73a58ad97c971dbf5ad5932997f1e790663184b3
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-03-21 14:11:44 +0000

    * Forgot to commit this one.
    svn path=/nix/trunk/test/; revision=12

--- tmpl/pan-0.13.4.nix
+++ tmpl/pan-0.13.4.nix
@@ -5,6 +5,6 @@ pango <- ./pango-1.2.1.nix
 gtk <- ./gtk+-2.2.1.nix
 gnet <- ./gnet-1.1.8.nix
 
-src = ../dist/pan-0.13.4.tar.bz2
+src = url(http://pan.rebelbase.com/download/releases/0.13.4/SOURCE/pan-0.13.4.tar.bz2)
 
 build = ../build/pan-build.sh
