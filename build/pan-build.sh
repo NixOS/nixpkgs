24699af6ae424693c51385759cfd9258de207886
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-03-21 15:58:40 +0000

    * Added a command to list installed packages.
    svn path=/nix/trunk/test/; revision=14

--- build/pan-build.sh
+++ build/pan-build.sh
@@ -2,7 +2,7 @@
 
 export PATH=$pkgconfig/bin:$gnet/bin:/bin:/usr/bin
 export PKG_CONFIG_PATH=$glib/lib/pkgconfig:$atk/lib/pkgconfig:$pango/lib/pkgconfig:$gtk/lib/pkgconfig
-export LD_LIBRARY_PATH=$gnet/lib:$atk/lib:$pango/lib:$gtk/lib:$gnet/lib
+export LD_LIBRARY_PATH=$glib/lib:$atk/lib:$pango/lib:$gtk/lib:$gnet/lib
 
 top=`pwd`
 tar xvfj $src
