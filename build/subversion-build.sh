955ae188b200dc5020003dcfc7d98a82f8d0994f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-04-23 09:02:53 +0000

    * Fix descriptor for Subversion 0.21.0.
    svn path=/nix/trunk/test/; revision=45

--- build/subversion-build.sh
+++ build/subversion-build.sh
@@ -5,10 +5,10 @@ export PATH=/bin:/usr/bin
 export LDFLAGS=-s
 
 top=`pwd`
-tar xvfz $src
-cd subversion-*
-./configure --prefix=$top --with-ssl
-make
-make install
-cd ..
-rm -rf subversion-*
+tar xvfz $src || exit 1
+cd subversion-* || exit 1
+./configure --prefix=$top --with-ssl || exit 1
+make || exit 1
+make install || exit 1
+cd $top || exit 1
+rm -rf subversion-* || exit 1
