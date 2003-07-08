151016ab8531d576d48c04f9305eb55b1037fff4
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-07-08 15:34:10 +0000

    * Build in the current directory, not in the target directory. * `id' -> `name'.
    svn path=/nixpkgs/trunk/; revision=132

--- pkgs/aterm/aterm-build.sh
+++ pkgs/aterm/aterm-build.sh
@@ -2,12 +2,9 @@
 
 export PATH=/bin:/usr/bin
 
-mkdir $out || exit 1
-cd $out || exit 1
 tar xvfz $src || exit 1
 cd aterm-* || exit 1
 ./configure --prefix=$out || exit 1
 make || exit 1
+mkdir $out || exit 1
 make install || exit 1
-cd $out || exit 1
-rm -rf aterm-* || exit 1
--- pkgs/pkgconfig/pkgconfig-build.sh
+++ pkgs/pkgconfig/pkgconfig-build.sh
@@ -2,12 +2,9 @@
 
 export PATH=/bin:/usr/bin
 
-mkdir $out || exit 1
-cd $out || exit 1
 tar xvfz $src || exit 1
 cd pkgconfig-* || exit 1
 ./configure --prefix=$out || exit 1
 make || exit 1
+mkdir $out || exit 1
 make install || exit 1
-cd $out || exit 1
-rm -rf pkgconfig-* || exit 1
