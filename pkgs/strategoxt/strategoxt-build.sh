3980a39bd015833170a9d643fb152bdc0eac6fc6
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-07-13 21:27:09 +0000

    * Packages for SDF2, StrategoXT.
    svn path=/nixpkgs/trunk/; revision=157

--- pkgs/strategoxt/strategoxt-build.sh
+++ pkgs/strategoxt/strategoxt-build.sh
@@ -3,7 +3,7 @@
 export PATH=/bin:/usr/bin
 
 tar xvfz $src || exit 1
-cd aterm-* || exit 1
-./configure --prefix=$out --with-gcc || exit 1
+cd strategoxt-* || exit 1
+./configure --prefix=$out --with-aterm=$aterm --with-sdf=$sdf2 || exit 1
 make || exit 1
 make install || exit 1
