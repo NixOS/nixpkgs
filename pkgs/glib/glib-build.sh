a119076f9e6f8ad59848bcb0e219cc75118e5d5c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-07-17 22:33:05 +0000

    * Check for failure.
    svn path=/nixpkgs/trunk/; revision=175

--- pkgs/glib/glib-build.sh
+++ pkgs/glib/glib-build.sh
@@ -2,8 +2,8 @@
 
 export PATH=$pkgconfig/bin:/bin:/usr/bin
 
-tar xvfj $src
-cd glib-*
-LDFLAGS=-s ./configure --prefix=$out
-make
-make install
+tar xvfj $src || exit 1
+cd glib-* || exit 1
+LDFLAGS=-s ./configure --prefix=$out || exit 1
+make || exit 1
+make install || exit 1
