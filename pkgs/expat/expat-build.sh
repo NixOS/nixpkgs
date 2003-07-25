2c2cae89e644a43e0456bf62983755ffa0e03990
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-07-25 19:03:24 +0000

    * Added a package for expat. * Don't use GDBM in Subversion.
    svn path=/nixpkgs/trunk/; revision=205

---
+++ pkgs/expat/expat-build.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+. $stdenv/setup || exit 1
+
+tar xvfz $src || exit 1
+cd expat-* || exit 1
+LDFLAGS=-Wl,-S ./configure --prefix=$out || exit 1
+make || exit 1
+make install || exit 1
--- pkgs/subversion/subversion-build.sh
+++ pkgs/subversion/subversion-build.sh
@@ -26,7 +26,7 @@ echo "extra flags: $extraflags"
 
 tar xvfz $src || exit 1
 cd subversion-* || exit 1
-LDFLAGS=-Wl,-S ./configure --prefix=$out $extraflags || exit 1
+LDFLAGS=-Wl,-S ./configure --prefix=$out $extraflags --without-gdbm || exit 1
 make $extramakeflags || exit 1
 make install $extramakeflags || exit 1
 
