cec4d0dc6233c3b8f71998a978dc25a257dc47f9
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-07-26 21:00:24 +0000

    * Strip debug info.
    svn path=/nixpkgs/trunk/; revision=208

--- pkgs/libxml2/libxml2-build.sh
+++ pkgs/libxml2/libxml2-build.sh
@@ -7,3 +7,4 @@ cd libxml2-* || exit 1
 LDFLAGS=-Wl,-S ./configure --prefix=$out || exit 1
 make || exit 1
 make install || exit 1
+strip -S $out/lib/*.a || exit 1
--- pkgs/libxslt/libxslt-build.sh
+++ pkgs/libxslt/libxslt-build.sh
@@ -10,5 +10,6 @@ cd libxslt-* || exit 1
 LDFLAGS=-Wl,-S ./configure --prefix=$out || exit 1
 make || exit 1
 make install || exit 1
+strip -S $out/lib/*.a || exit 1
 
 echo $envpkgs > $out/envpkgs || exit 1
