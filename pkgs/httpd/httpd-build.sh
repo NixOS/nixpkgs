9f94d884b774f78ba92601ababe18f2ef5d96d98
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-08-06 14:52:32 +0000

    * Force use of threads. * Use /dev/urandom, not /dev/random.
    svn path=/nixpkgs/trunk/; revision=262

--- pkgs/httpd/httpd-build.sh
+++ pkgs/httpd/httpd-build.sh
@@ -10,7 +10,8 @@ envpkgs="$ssl $db4 $expat"
 tar xvfz $src || exit 1
 cd httpd-* || exit 1
 ./configure --prefix=$out --enable-ssl --with-ssl=$ssl --with-berkeley-db=$db4 \
- --with-expat=$expat --enable-mods-shared=all --without-gdbm || exit 1
+ --with-expat=$expat --enable-mods-shared=all --without-gdbm \
+ --enable-threads --with-devrandom=/dev/urandom || exit 1
 make || exit 1
 make install || exit 1
 strip -S $out/lib/*.a || exit 1
