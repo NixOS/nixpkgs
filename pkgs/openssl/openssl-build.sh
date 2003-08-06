5da5a72830c7e4a18f19cea671e78afb84be315d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-08-06 11:30:45 +0000

    * OpenSSL's rpath should include itself.
    svn path=/nixpkgs/trunk/; revision=258

--- pkgs/openssl/openssl-build.sh
+++ pkgs/openssl/openssl-build.sh
@@ -4,9 +4,14 @@ export NIX_LDFLAGS=-Wl,-s
 
 . $stdenv/setup || exit 1
 
+envpkgs=""
+. $setenv || exit 1
+
 tar xvfz $src || exit 1
 cd openssl-* || exit 1
 ./config --prefix=$out shared || exit 1
 make || exit 1
 mkdir $out || exit 1
 make install || exit 1
+
+echo $envpkgs > $out/envpkgs || exit 1
