6b08880f80ca82a0d8fa78259152977752efc305
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-08-13 17:29:52 +0000

    * Added Mozilla Firebird (with gtk2 and antialiasing).
    svn path=/nixpkgs/trunk/; revision=280

---
+++ pkgs/firebird/firebird-build.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+
+export NIX_LDFLAGS=-Wl,-s
+
+. $stdenv/setup || exit 1
+
+export PATH=$pkgconfig/bin:$PATH
+envpkgs="$gtk $libIDL"
+. $setenv || exit 1
+
+tar xvfj $src || exit 1
+cd mozilla || exit 1
+
+cat > .mozconfig <<EOF
+export MOZ_PHOENIX=1
+mk_add_options MOZ_PHOENIX=1
+ac_add_options --enable-crypto
+ac_add_options --disable-tests
+ac_add_options --disable-debug
+ac_add_options --disable-mailnews
+ac_add_options --disable-composer
+ac_add_options --enable-optimize=-O2
+ac_add_options --disable-ldap
+ac_add_options --disable-mailnews
+ac_add_options --enable-extensions=default,-inspector,-irc,-venkman,-content-packs,-help
+ac_add_options --enable-xft
+ac_add_options --enable-swg
+ac_add_options --enable-strip
+ac_add_options --enable-default-toolkit=gtk2
+EOF
+echo "ac_add_options --prefix=$out" >> .mozconfig
+
+./configure || exit 1
+make -f client.mk build || exit 1
+make install || exit 1
