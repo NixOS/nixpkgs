f6e0983d443d0ed6a87cc6d1eec0beb1b4ebbd46
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-08-18 16:33:38 +0000

    * Added GNU tar. * Some refactoring on stdenv-linux to prevent unnecessary rebuilds.
    svn path=/nixpkgs/trunk/; revision=308

--- pkgs/stdenv-linux/setup.sh
+++ pkgs/stdenv-linux/setup.sh
@@ -3,8 +3,4 @@
 export NIX_CFLAGS="-isystem @GLIBC@/include $NIX_CFLAGS"
 export NIX_LDFLAGS="-L @GLIBC@/lib -Wl,-dynamic-linker,@GLIBC@/lib/ld-linux.so.2,-rpath,@GLIBC@/lib $NIX_LDFLAGS"
 
-if test "@NATIVETOOLS@"; then
-    export PATH=$PATH:/usr/local/bin:/usr/bin:/bin
-else
-    export PATH=@COREUTILS@/bin:$PATH
-fi
+export PATH=@PATH@
--- pkgs/stdenv-linux/stdenv-build.sh
+++ pkgs/stdenv-linux/stdenv-build.sh
@@ -4,9 +4,26 @@ export PATH=/bin:/usr/bin
 
 mkdir $out || exit 1
 
+if test "$nativeTools" == 1; then
+    p='$PATH:/usr/local/bin:/usr/bin:/bin'
+else
+    p=
+    first=1
+    for i in $tools; do
+	if test "$first" == 1; then
+	    first=
+	else
+	    p=$p:
+	fi
+	p=$p$i/bin
+    done
+    p=$p':$PATH'
+fi
+
+echo "########## $p"
+
 sed \
  -e s^@GLIBC\@^$glibc^g \
  -e s^@BASEENV\@^$baseenv^g \
- -e s^@NATIVETOOLS\@^$nativeTools^g \
- -e s^@COREUTILS\@^$coreutils^g \
+ -e s^@PATH\@^$p^g \
  < $setup > $out/setup || exit 1
