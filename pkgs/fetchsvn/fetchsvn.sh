397901e36f9d066ae791f67e18a60860cf94c69f
Eelco Visser <visser@acm.org>
2003-08-18 20:47:12 +0000

    creating tar after obtaining exported subversion directory
    svn path=/nixpkgs/trunk/; revision=318

--- pkgs/fetchsvn/fetchsvn.sh
+++ pkgs/fetchsvn/fetchsvn.sh
@@ -1,5 +1,17 @@
 #! /bin/sh
 
-echo "exporting svn repository $url (at rev $rev) into $out..."
+set -e
 
-$svn/bin/svn export -r $rev $url $out || exit 1
+echo "exporting svn repository $url/$dir (at rev $rev) into $out..."
+
+svn export -r $rev $url/$dir $dir
+
+# touch bootstrapped sources because subversion doesn't sets the mtime of files
+# to checkout time, not to the last mtime in the repository.
+MTIME=`date +%Y%m%d%H%M.%S`
+echo "** INFO -- Modification time: $MTIME"
+find $dir -print | xargs touch -t $MTIME
+# end of touch
+
+mkdir $out
+tar zcf $out/$dir.tar.gz $dir
