23d90c2324872082e0af639c739b2a409777b9a3
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-08-25 19:26:46 +0000

    * Patched binutils so that ld no longer searches any of the "default" directories   (/lib, ...).  So unless a directory is explicitly added using `-L' it won't we   searched.  This has already revealed that libxml has a hidden dependance on libz.
    svn path=/nixpkgs/trunk/; revision=357

--- pkgs/binutils/binutils-build.sh
+++ pkgs/binutils/binutils-build.sh
@@ -4,6 +4,12 @@
 
 tar xvfj $src || exit 1
 cd binutils-* || exit 1
+
+# Clear the default library search path.
+echo 'NATIVE_LIB_DIRS=' >> ld/configure.tgt || exit 1
+
 ./configure --prefix=$out || exit 1
 make || exit 1
 make install || exit 1
+
+strip -S $out/lib/*.a || exit 1
