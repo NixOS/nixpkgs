1edcca4322cc47a4a0e632fc915c12ed0e94b3d3
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-08-26 20:05:56 +0000

    * Build version.h before installing the header files.
    svn path=/nixpkgs/trunk/; revision=362

--- pkgs/linux-headers/linux-headers-build.sh
+++ pkgs/linux-headers/linux-headers-build.sh
@@ -2,10 +2,11 @@
 
 . $stdenv/setup || exit 1
 
+tar xvfj $src || exit 1
+cd linux-* || exit 1
+make include/linux/version.h || exit 1
 mkdir $out || exit 1
-cd $out || exit 1
-tar xvfj $src 'linux-*/include/linux' 'linux-*/include/asm-i386' || exit 1
-mv linux-*/include . || exit 1
-rmdir linux-* || exit 1
-cd include || exit 1
+mkdir $out/include || exit 1
+cp -prvd include/linux include/asm-i386 $out/include || exit 1
+cd $out/include || exit 1
 ln -s asm-i386 asm || exit 1
