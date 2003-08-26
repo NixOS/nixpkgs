3638af3cab98d5cd6a2e485d31d6e5d7c7bbe6e5
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-08-26 13:55:30 +0000

    * gcc: ensure that the compilers past stage-1 and the libraries are linked   against our glibc.
    svn path=/nixpkgs/trunk/; revision=360

--- pkgs/gcc/gcc-build.sh
+++ pkgs/gcc/gcc-build.sh
@@ -1,10 +1,26 @@
 #! /bin/sh
 
 . $stdenv/setup || exit 1
+export PATH=$binutils/bin:$PATH
 
 tar xvfj $src || exit 1
 mkdir build || exit 1
 cd build || exit 1
 ../gcc-*/configure --prefix=$out --enable-languages=c,c++ || exit 1
+
+extraflags="$NIX_CFLAGS $NIX_LDFLAGS -Wl,-s"
+
+mf=Makefile
+sed \
+ -e "s^FLAGS_FOR_TARGET =\(.*\)^FLAGS_FOR_TARGET = \1 $extraflags^" \
+ < $mf > $mf.tmp || exit 1
+mv $mf.tmp $mf
+
+mf=gcc/Makefile
+sed \
+ -e "s^X_CFLAGS =\(.*\)^X_CFLAGS = \1 $extraflags^" \
+ < $mf > $mf.tmp || exit 1
+mv $mf.tmp $mf
+
 make bootstrap || exit 1
 make install || exit 1
