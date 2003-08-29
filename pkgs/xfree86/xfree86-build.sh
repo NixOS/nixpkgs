d60e11c4e2fab23922d0e9ff337e284835f8190b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-08-29 13:18:56 +0000

    * Quick fix to allow fontconfig to find client-side fonts.
    svn path=/nixpkgs/trunk/; revision=375

--- pkgs/xfree86/xfree86-build.sh
+++ pkgs/xfree86/xfree86-build.sh
@@ -13,3 +13,6 @@ cd xc || exit 1
 sed "s^@OUT@^$out^" < $hostdef > config/cf/host.def
 make World || exit 1
 make install || exit 1
+
+# Hack to get fontconfig to work.
+ln -s /usr/X11R6/lib/X11/fonts $out/lib/X11/fonts
