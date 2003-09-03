ff7acae84bb46c3c13ab4f9e95a054de01bc0344
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-09-03 15:07:50 +0000

    * Added gqview to system * Enabled build of dotty/lefty in graphviz.
    svn path=/nixpkgs/trunk/; revision=383

--- pkgs/graphviz/graphviz-build.sh
+++ pkgs/graphviz/graphviz-build.sh
@@ -7,6 +7,6 @@ NIX_CFLAGS_COMPILE="-I$zlib/include -I$libpng/include -I$libjpeg/include -I$expa
 
 tar xvfz $src || exit 1
 cd graphviz-* || exit 1
-./configure --prefix=$out --without-x || exit 1
+./configure --prefix=$out --x-includes=$x11/include --x-libraries=$x11/lib || exit 1
 make || exit 1
 make install || exit 1
