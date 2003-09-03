d883e86e1c2d6d5adf98d1b805c3dfbec9181f41
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-09-03 10:32:30 +0000

    * Added graphviz.
    svn path=/nixpkgs/trunk/; revision=378

---
+++ pkgs/graphviz/graphviz-build.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+envpkgs="$zlib $libpng $libjpeg $expat"
+. $stdenv/setup || exit 1
+
+NIX_CFLAGS_COMPILE="-I$zlib/include -I$libpng/include -I$libjpeg/include -I$expat/include $NIX_CFLAGS_COMPILE"
+
+tar xvfz $src || exit 1
+cd graphviz-* || exit 1
+./configure --prefix=$out --without-x || exit 1
+make || exit 1
+make install || exit 1
