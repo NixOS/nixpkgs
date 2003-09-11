443b81595a4484b888d74c53b249bbd17ab4f869
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-09-11 12:24:19 +0000

    * Fixed stdenv-native.
    svn path=/nixpkgs/trunk/; revision=386

---
+++ pkgs/stdenv-native/setup.sh
@@ -0,0 +1,8 @@
+export PATH=/usr/local/bin:/usr/bin:/bin
+export SHELL=/bin/sh
+
+. @BASEENV@/setup
+
+export NIX_CC=/usr/bin/gcc
+export NIX_CXX=/usr/bin/g++
+export NIX_LD=/usr/bin/ld
--- pkgs/stdenv-native/stdenv-build.sh
+++ pkgs/stdenv-native/stdenv-build.sh
@@ -1,26 +1,9 @@
 #! /bin/sh
 
-export PATH=/bin:/usr/bin
+export PATH=/usr/local/bin:/usr/bin:/bin
 
 mkdir $out || exit 1
-mkdir $out/bin || exit 1
-
-echo "export PATH=$out/bin:/bin:/usr/bin" >> $out/setup || exit 1
-
-gcc=/usr/bin/gcc
 
 sed \
- -e s^@GCC\@^$gcc^g \
- < $gccwrapper > $out/bin/gcc || exit 1
-chmod +x $out/bin/gcc || exit 1
-
-ln -s gcc $out/bin/cc
-
-gplusplus=/usr/bin/g++
-
-sed \
- -e s^@GCC\@^$gplusplus^g \
- < $gccwrapper > $out/bin/g++ || exit 1
-chmod +x $out/bin/g++ || exit 1
-
-ln -s g++ $out/bin/c++
+ -e s^@BASEENV\@^$baseenv^g \
+ < $setup > $out/setup || exit 1
