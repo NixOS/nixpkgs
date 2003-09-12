a9f88dae647c648f5de84fe2291542e81d3fd4b2
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-09-12 10:20:12 +0000

    * User Mode Linux.
    svn path=/nixpkgs/trunk/; revision=390

---
+++ pkgs/uml/uml-build.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+. $stdenv/setup || exit 1
+export PATH=$perl/bin:$m4/bin:$PATH
+
+export NIX_CC=$gcc/bin/gcc
+
+tar xvfj $linuxSrc || exit 1
+cd linux-* || exit 1
+bunzip2 < $umlSrc | $patch/bin/patch -p1 || exit 1
+cp $config .config || exit 1
+make oldconfig ARCH=um || exit 1
+
+make linux ARCH=um || exit 1
+strip linux || exit 1
+make modules ARCH=um || exit 1
+
+mkdir $out || exit 1
+mkdir $out/bin || exit 1
+cp -p linux $out/bin || exit 1
+make modules_install INSTALL_MOD_PATH=$out ARCH=um || exit 1
+
