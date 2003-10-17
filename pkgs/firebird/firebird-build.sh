95bf9df10d31967f10d72b5174265614c7cf6fd5
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-10-17 10:13:18 +0000

    * Upgraded to Mozilla Firebird 0.7.
    svn path=/nixpkgs/trunk/; revision=426

--- pkgs/firebird/firebird-build.sh
+++ pkgs/firebird/firebird-build.sh
@@ -4,7 +4,7 @@ envpkgs="$gtk $libIDL"
 . $stdenv/setup || exit 1
 export PATH=$pkgconfig/bin:$perl/bin:$zip/bin:$unzip/bin:$PATH
 
-tar xvfj $src || exit 1
+tar xvfz $src || exit 1
 cd mozilla || exit 1
 
 cat > .mozconfig <<EOF
@@ -23,7 +23,6 @@ ac_add_options --enable-xft
 ac_add_options --enable-swg
 ac_add_options --enable-strip
 ac_add_options --enable-default-toolkit=gtk2
-ac_add_options --disable-pedantic
 EOF
 echo "ac_add_options --prefix=$out" >> .mozconfig
 
