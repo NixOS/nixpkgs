33f773c97ad36a2b9f9c9599f12ca12d655517f1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-10-31 13:12:20 +0000

    * Improved prototype.
    svn path=/nixpkgs/trunk/; revision=447

---
+++ pkgs-ng/system/populate-linkdirs.pl
@@ -0,0 +1,72 @@
+#! /usr/bin/perl -w
+
+use strict;
+use Cwd;
+
+my $selfdir = $ENV{"out"};
+mkdir "$selfdir", 0755 || die "error creating $selfdir";
+
+# For each activated package, create symlinks.
+
+sub createLinks {
+    my $srcdir = shift;
+    my $dstdir = shift;
+
+    my @srcfiles = glob("$srcdir/*");
+
+    foreach my $srcfile (@srcfiles) {
+        my $basename = $srcfile;
+        $basename =~ s/^.*\///g; # strip directory
+        my $dstfile = "$dstdir/$basename";
+	if ($srcfile =~ /\/envpkgs$/) {
+	} elsif (-d $srcfile) {
+            # !!! hack for resolving name clashes
+            if (!-e $dstfile) {
+                mkdir $dstfile, 0755 || 
+                    die "error creating directory $dstfile";
+            }
+            -d $dstfile or die "$dstfile is not a directory";
+            createLinks($srcfile, $dstfile);
+        } elsif (-l $dstfile) {
+            my $target = readlink($dstfile);
+            die "collission between $srcfile and $target";
+        } else {
+#            print "linking $dstfile to $srcfile\n";
+            symlink($srcfile, $dstfile) ||
+                die "error creating link $dstfile";
+        }
+    }
+}
+
+my %done;
+
+sub addPkg {
+    my $pkgdir = shift;
+
+    return if (defined $done{$pkgdir});
+    $done{$pkgdir} = 1;
+
+    print "merging $pkgdir\n";
+
+    createLinks("$pkgdir", "$selfdir");
+
+#    if (-f "$pkgdir/envpkgs") {
+#	my $envpkgs = `cat $pkgdir/envpkgs`;
+#	chomp $envpkgs;
+#	my @envpkgs = split / +/, $envpkgs;
+#	foreach my $envpkg (@envpkgs) {
+#	    addPkg($envpkg);
+#	}
+#    }
+}
+
+
+foreach my $name (keys %ENV) {
+
+    next unless $name =~ /^act.*$/;
+
+    my $pkgdir = $ENV{$name};
+    next unless $pkgdir =~ /^\//;
+
+    addPkg($pkgdir);
+}
