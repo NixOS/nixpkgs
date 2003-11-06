f0f5b6297e87df9dd07250bf1f3ba104e0a5a292
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-11-06 12:43:25 +0000

    * Converted more packages.
    svn path=/nixpkgs/trunk/; revision=484

---
+++ pkgs-ng/servers/x11/xfree86/builder.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+export buildinputs="$bison $flex"
+. $stdenv/setup || exit 1
+
+tar xvfz $src1 || exit 1
+tar xvfz $src2 || exit 1
+tar xvfz $src3 || exit 1
+cd xc || exit 1
+sed "s^@OUT@^$out^" < $hostdef > config/cf/host.def
+make World || exit 1
+make install || exit 1
+
+# !!! Hack to get fontconfig to work.
+ln -s /usr/X11R6/lib/X11/fonts $out/lib/X11/fonts
