5ce30ac868f05d04a8f7f68a84b6a6b0563ac9b6
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-11-11 16:13:13 +0000

    * Added gqview.
    svn path=/nixpkgs/trunk/; revision=497

---
+++ pkgs-ng/applications/graphics/gqview/builder.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+buildinputs="$pkgconfig $gtk $libpng"
+. $stdenv/setup || exit 1
+
+tar xvfz $src || exit 1
+cd gqview-* || exit 1
+./configure --prefix=$out || exit 1
+make || exit 1
+make install || exit 1
