e6db9b1caa9acc6ebd1a1e219ec084f2209ad68f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-11-13 13:11:38 +0000

    * Browser plugin for MPlayer.
    svn path=/nixpkgs/trunk/; revision=498

---
+++ pkgs-ng/applications/video/mplayerplug-in/builder.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+buildinputs="$x11"
+. $stdenv/setup || exit 1
+
+tar xvfz $src || exit 1
+cd mplayer* || exit 1
+./configure || exit 1
+make || exit 1
+mkdir -p $out/lib/mozilla/plugins || exit 1
+cp mplayerplug-in.so $out/lib/mozilla/plugins || exit 1
