fe1c5cb1e1e21aeb890105558fe463a52fa2bee5
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-12-21 21:25:38 +0000

    * Regression fix.
    svn path=/nixpkgs/trunk/; revision=587

--- pkgs/servers/http/apache-httpd/default.nix
+++ pkgs/servers/http/apache-httpd/default.nix
@@ -1,6 +1,11 @@
 { stdenv, fetchurl, openssl, db4, expat, perl
 , sslSupport, db4Support
 }:
+
+assert sslSupport -> !isNull openssl;
+assert db4Support -> !isNull db4;
+assert !isNull expat && !isNull perl;
+
 derivation {
   name = "apache-httpd-2.0.48";
   system = stdenv.system;
@@ -10,6 +15,9 @@ derivation {
     url = ftp://ftp.cs.uu.nl/mirror/apache.org/dist/httpd/httpd-2.0.48.tar.gz;
     md5 = "466c63bb71b710d20a5c353df8c1a19c";
   };
+
+  sslSupport = sslSupport;
+  db4Support = db4Support;
 
   stdenv = stdenv;
   perl = perl;
