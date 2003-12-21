38bc744a0b7bd38f556a9db46f8acb30e918e95f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-12-21 16:00:17 +0000

    * Use md5sum instead of nix-hash.
    svn path=/nixpkgs/trunk/; revision=584

--- pkgs/build-support/fetchurl/builder.sh
+++ pkgs/build-support/fetchurl/builder.sh
@@ -4,17 +4,9 @@
 
 echo "downloading $url into $out..."
 
-#prefetch=@prefix@/store/nix-prefetch-url-$md5
-prefetch=/nix/store/nix-prefetch-url-$md5
-if test -f "$prefetch"; then
-    echo "using prefetched $prefetch";
-    mv $prefetch $out || exit 1
-else
-    wget --passive-ftp "$url" -O "$out" || exit 1
-fi
+wget --passive-ftp "$url" -O "$out" || exit 1
 
-#actual=$(@bindir@/nix-hash --flat $out)
-actual=$(/nix/bin/nix-hash --flat $out)
+actual=$(md5sum -b $out | cut -c1-32)
 if test "$actual" != "$md5"; then
     echo "hash is $actual, expected $md5"
     exit 1
