e2570fd7b70273c4347d3b45307b0c4be57ceb82
Eelco Visser <visser@acm.org>
2003-12-24 15:58:48 +0000

    built StrategoXT from subversion; not sure whether there are problems with shared libraries.
    svn path=/nixpkgs/trunk/; revision=601

--- pkgs/development/compilers/strategoxt/trunk.nix
+++ pkgs/development/compilers/strategoxt/trunk.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchsvn, autotools, which, aterm, sdf}: derivation {
-  name = "strategoxt-0.9.4-4785";
+  name = "strategoxt-0.9.4-4792";
   system = stdenv.system;
   builder = ./svnbuilder.sh;
   src = fetchsvn {
     url = https://svn.cs.uu.nl:12443/repos/StrategoXT/trunk/StrategoXT;
-    rev = "4785";
+    rev = "4792";
   };
   stdenv = stdenv;
 
