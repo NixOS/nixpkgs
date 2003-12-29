68361ac1991767b73e7858fe1e744dc0953e5aa3
Eelco Visser <visser@acm.org>
2003-12-29 17:01:32 +0000

    clone nix-dist to create strategoxt-dist
    svn path=/nixpkgs/trunk/; revision=603

---
+++ strategoxt-dist/build+upload.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+url="https://svn.cs.uu.nl:12443/repos/trace/nix/trunk/"
+
+if ! rev=$(curl --silent -k https://svn.cs.uu.nl:12443/repos/trace/nix/trunk/ \
+ | grep '<h2>Revision' \
+ | sed 's/.*Revision \(.*\):.*/\1/'); \
+ then exit 1; fi
+
+echo "building revision $rev of $url"
+
+echo $rev > head-revision.nix
+
+if ! storeexprs=($(nix-instantiate -vvv do-it.nix)); then exit 1; fi
+
+srcexpr=${storeexprs[0]}
+testexpr=${storeexprs[1]}
+
+if ! nix-store -vvvv -r "$srcexpr" > /dev/null; then exit 1; fi
+
+if ! outpath=$(nix-store -qn "$srcexpr"); then exit 1; fi
+
+uploader="http://losser.st-lab.cs.uu.nl/~eelco/cgi-bin/upload.pl/"
+
+curl --silent -T "$outpath/manual.html" "$uploader" || exit 1
+curl --silent -T "$outpath/style.css" "$uploader" || exit 1
+curl --silent -T "$outpath"/nix-*.tar.bz2 "$uploader" || exit 1
+
+if ! nix-store -vvvv -r "$testexpr" > /dev/null; then exit 1; fi
---
+++ strategoxt-dist/do-it.nix
@@ -0,0 +1,26 @@
+let {
+  system = "i686-linux";
+  pkgs = (import ../pkgs/system/all-packages.nix) {system = system;};
+  stdenv = pkgs.stdenv_;
+
+  sourcedist = (import ./nix-source-dist.nix) {
+    stdenv = stdenv;
+    autoconf = pkgs.autoconf;
+    automake = pkgs.automake;
+    libxml2 = pkgs.libxml2;
+    libxslt = pkgs.libxslt;
+    docbook_dtd = pkgs.docbook_xml_dtd;
+    docbook_xslt = pkgs.docbook_xml_xslt;
+    fetchurl = pkgs.fetchurl;
+    fetchsvn = pkgs.fetchsvn;
+    rev = import ./head-revision.nix;
+  };
+
+  testbuild = (import ./nix-test-build.nix) {
+    stdenv = stdenv;
+    getopt = pkgs.getopt;
+    src = sourcedist;
+  };
+
+  body = [sourcedist testbuild];
+}
