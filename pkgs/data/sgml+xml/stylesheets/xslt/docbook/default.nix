a11739140030c2af138500a1f4ed4c607e5b22f2
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-01-08 14:35:37 +0000

    * Upgraded to 1.64.1.
    svn path=/nixpkgs/trunk/; revision=624

--- pkgs/data/sgml+xml/stylesheets/xslt/docbook/default.nix
+++ pkgs/data/sgml+xml/stylesheets/xslt/docbook/default.nix
@@ -1,12 +1,12 @@
 {stdenv, fetchurl}:
 
 derivation {
-  name = "docbook-xsl-1.62.4";
+  name = "docbook-xsl-1.64.1";
   system = stdenv.system;
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://belnet.dl.sourceforge.net/sourceforge/docbook/docbook-xsl-1.62.4.tar.gz;
-    md5 = "4f33db39db7fa95b50143ad609d734a0";
+    url = http://cesnet.dl.sourceforge.net/sourceforge/docbook/docbook-xsl-1.64.1.tar.gz;
+    md5 = "ed766902e8381f6206d12f5c326fbd47";
   };
   stdenv = stdenv;
 }
