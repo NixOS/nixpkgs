7b141f7f5a2d18dfa8e76ddb3f2766505446f4f3
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-01-15 16:17:58 +0000

    * Build more packages in the distribution of nixpkgs.
    svn path=/nixpkgs/trunk/; revision=654

--- pkgs/system/populate-cache.nix
+++ pkgs/system/populate-cache.nix
@@ -1,12 +1,24 @@
 let {
   pkgs = import ./i686-linux.nix;
   body = 
-    [ pkgs.subversion
+    [ pkgs.zip
+      pkgs.unzip
+      pkgs.par2cmdline
+      pkgs.graphviz
+      pkgs.strategoxt
+      pkgs.libxml2
+      pkgs.libxslt
+      pkgs.docbook_xml_dtd
+      pkgs.docbook_xml_xslt
+      pkgs.subversion
       pkgs.pan
       pkgs.sylpheed
+      pkgs.firebird
       pkgs.MPlayer
       pkgs.MPlayerPlugin
-      pkgs.gqview
       pkgs.vlc
+      pkgs.gqview
+      pkgs.hello
+      pkgs.nxml
     ];
 }
