73c7b4a1119fdc8d5f4417e76b8d22252164368a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-02-18 08:17:12 +0000

    * Pass parameters correctly.
    svn path=/nixpkgs/trunk/; revision=799

--- pkgs/tools/networking/bittorrent/builder.sh
+++ pkgs/tools/networking/bittorrent/builder.sh
@@ -12,7 +12,8 @@ mkdir $out/bin
 for i in $(cd $out/bin-orig && ls); do
   cat > $out/bin/$i <<EOF
 #! /bin/sh
-PYTHONPATH=$out/lib/python2.3/site-packages:$wxPython/lib/python2.3/site-packages exec $out/bin-orig/$i \$*
+IFS=
+PYTHONPATH=$out/lib/python2.3/site-packages:$wxPython/lib/python2.3/site-packages exec $out/bin-orig/$i \$@
 EOF
   chmod +x $out/bin/$i
 done
