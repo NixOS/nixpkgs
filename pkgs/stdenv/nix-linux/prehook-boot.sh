1999676d30c6a443cb0e3fab9ff469cd576a5e51
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-02-20 10:03:46 +0000

    * Bug fix: don't pass -B to the linker.
    svn path=/nixpkgs/trunk/; revision=811

--- pkgs/stdenv/nix-linux/prehook-boot.sh
+++ pkgs/stdenv/nix-linux/prehook-boot.sh
@@ -4,12 +4,14 @@ export NIX_CC=/usr/bin/gcc
 export NIX_CXX=/usr/bin/g++
 export NIX_LD=/usr/bin/ld
 
-export NIX_CFLAGS_COMPILE="-isystem $param4/include $NIX_CFLAGS_COMPILE"
 # The "-B$param4/lib" is a quick hack to force gcc to link against the
 # crt1.o from our own glibc, rather than the one in /usr/lib.  The
 # real solution is of course to prevent those paths from being used by
 # gcc in the first place.
-export NIX_CFLAGS_LINK="-B$param4/lib -L$param4/lib $NIX_CFLAGS_LINK"
+# !!! -B should really be in NIX_CFLAGS_LINK, but ld-wrapper also uses
+# that one (incorrectly?).
+export NIX_CFLAGS_COMPILE="-B$param4/lib -isystem $param4/include $NIX_CFLAGS_COMPILE"
+export NIX_CFLAGS_LINK="-L$param4/lib $NIX_CFLAGS_LINK"
 export NIX_LDFLAGS="-dynamic-linker $param4/lib/ld-linux.so.2 -rpath $param4/lib $NIX_LDFLAGS"
 
 export NIX_LIBC_INCLUDES="$param4/include"
