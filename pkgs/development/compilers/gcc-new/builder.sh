ab73ec398bb3b0baac73196df2b8bbcf6171cb56
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-03-09 17:10:23 +0000

    * Removed the old gcc.
    svn path=/nixpkgs/trunk/; revision=825

--- pkgs/development/compilers/gcc-new/builder.sh
+++ pkgs/development/compilers/gcc-new/builder.sh
@@ -5,7 +5,7 @@ buildinputs="$binutils"
 
 tar xvfj $src
 
-if test "$noSysDirs" == "1"; then
+if test "$enforcePurity" = "1"; then
     # Disable the standard include directories.
     cd gcc-*
     cat >> ./gcc/cppdefault.h <<EOF
@@ -29,8 +29,9 @@ mkdir build
 cd build
 ../gcc-*/configure --prefix=$out --enable-languages="$langs"
 
-if test "$noSysDirs" == "1"; then
+if test "$enforcePurity" = "1"; then
     # Patch some of the makefiles to force linking against our own glibc.
+    . $NIX_GCC/nix-support/add-flags # add glibc/gcc flags
     extraflags="-Wl,-s $NIX_CFLAGS_COMPILE $NIX_CFLAGS_LINK"
     for i in $NIX_LDFLAGS; do
         extraflags="$extraflags -Wl,$i"
--- pkgs/development/compilers/gcc-new/default.nix
+++ pkgs/development/compilers/gcc-new/default.nix
@@ -1,16 +1,20 @@
-{ stdenv, fetchurl, noSysDirs ? true
+{ stdenv, fetchurl, enforcePurity ? true
 , langC ? true, langCC ? true, langF77 ? false
 }:
 
 assert langC;
 
 derivation {
-  name = "gcc-3.3.3";
+  name = "gcc-3.3.2";
   system = stdenv.system;
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/mirror/languages/gcc/releases/gcc-3.3.3/gcc-3.3.3.tar.bz2;
-    md5 = "3c6cfd9fcd180481063b4058cf6faff2";
+    url = ftp://ftp.nluug.nl/pub/gnu/gcc/gcc-3.3.2/gcc-3.3.2.tar.bz2;
+    md5 = "65999f654102f5438ac8562d13a6eced";
   };
-  inherit stdenv noSysDirs langC langCC langF77;
+#  src = fetchurl {
+#    url = ftp://ftp.nluug.nl/mirror/languages/gcc/releases/gcc-3.3.3/gcc-3.3.3.tar.bz2;
+#    md5 = "3c6cfd9fcd180481063b4058cf6faff2";
+#  };
+  inherit stdenv enforcePurity langC langCC langF77;
 }
