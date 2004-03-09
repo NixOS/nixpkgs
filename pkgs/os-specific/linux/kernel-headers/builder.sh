1b39a96d3749102a12f43fb96f8a35124215da78
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-03-09 10:28:31 +0000

    * Create a dummy linux/autoconf.h, which is needed by linux/config.h. * Updated to 2.4.25.
    svn path=/nixpkgs/trunk/; revision=819

--- pkgs/os-specific/linux/kernel-headers/builder.sh
+++ pkgs/os-specific/linux/kernel-headers/builder.sh
@@ -1,12 +1,17 @@
 #! /bin/sh
 
-. $stdenv/setup || exit 1
+. $stdenv/setup
 
-tar xvfj $src || exit 1
-cd linux-* || exit 1
-make include/linux/version.h || exit 1
-mkdir $out || exit 1
-mkdir $out/include || exit 1
-cp -prvd include/linux include/asm-i386 $out/include || exit 1
-cd $out/include || exit 1
-ln -s asm-i386 asm || exit 1
+tar xvfj $src
+cd linux-*
+
+make include/linux/version.h
+
+mkdir $out
+mkdir $out/include
+cp -prvd include/linux include/asm-i386 $out/include
+cd $out/include
+ln -s asm-i386 asm
+
+# config.h includes autoconf.h, which doesn't exist.
+echo -n > $out/include/linux/autoconf.h
--- pkgs/os-specific/linux/kernel-headers/default.nix
+++ pkgs/os-specific/linux/kernel-headers/default.nix
@@ -1,11 +1,14 @@
-{stdenv, fetchurl}: derivation {
-  # !!! check that this is a i386
-  name = "linux-headers-2.4.22-i386";
+{stdenv, fetchurl}:
+
+assert stdenv.system == "i686-linux";
+
+derivation {
+  name = "linux-headers-2.4.25-i386";
   system = stdenv.system;
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.nl.kernel.org/pub/linux/kernel/v2.4/linux-2.4.22.tar.bz2;
-    md5 = "75dc85149b06ac9432106b8941eb9f7b";
+    url = ftp://ftp.nl.kernel.org/pub/linux/kernel/v2.4/linux-2.4.25.tar.bz2;
+    md5 = "5fc8e9f43fa44ac29ddf9a9980af57d8";
   };
-  stdenv = stdenv;
+  inherit stdenv;
 }
