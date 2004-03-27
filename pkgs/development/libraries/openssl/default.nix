e8295a80b4d126d3af40a3b13768051477a467ee
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-03-27 15:49:20 +0000

    * Upgrade to 0.9.7d (another security fix).
    svn path=/nixpkgs/trunk/; revision=855

--- pkgs/development/libraries/openssl/default.nix
+++ pkgs/development/libraries/openssl/default.nix
@@ -1,11 +1,10 @@
 {stdenv, fetchurl, perl}: derivation {
-  name = "openssl-0.9.7c";
+  name = "openssl-0.9.7d";
   system = stdenv.system;
   builder = ./builder.sh;
   src = fetchurl {
-      url = http://www.openssl.org/source/openssl-0.9.7c.tar.gz;
-      md5 = "c54fb36218adaaaba01ef733cd88c8ec";
-    };
-  stdenv = stdenv;
-  perl = perl;
+    url = http://www.openssl.org/source/openssl-0.9.7d.tar.gz;
+    md5 = "1b49e90fc8a75c3a507c0a624529aca5";
+  };
+  inherit stdenv perl;
 }
