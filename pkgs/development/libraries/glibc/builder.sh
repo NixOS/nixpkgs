085e197ea34e80cd7f3b6c37e603525b7f76a059
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-03-30 12:35:31 +0000

    * Toss out all the linker flags, since any --rpath switch causes the   glibc build to fail.
    svn path=/nixpkgs/trunk/; revision=876

--- pkgs/development/libraries/glibc/builder.sh
+++ pkgs/development/libraries/glibc/builder.sh
@@ -4,6 +4,13 @@ export NIX_NO_SELF_RPATH=1
 . $stdenv/setup
 
 
+# !!! Toss the linker flags.  Any sort of rpath is fatal.
+# This probably will cause a failure when building in a pure Nix
+# environment.
+export NIX_LDFLAGS=
+export NIX_GLIBC_FLAGS_SET=1
+
+
 postUnpack() {
     cd $sourceRoot
     unpackFile $linuxthreadsSrc
@@ -17,7 +24,8 @@ preConfigure() {
     mkdir ../build
     cd ../build
     configureScript=../$sourceRoot/configure
-    configureFlags="--enable-add-ons --disable-profile"
+    configureFlags="--enable-add-ons --disable-profile \
+      --with-headers=$kernelHeaders/include"
 }
 
 preConfigure=preConfigure
