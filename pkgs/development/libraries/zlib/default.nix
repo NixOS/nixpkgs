68327c3a9dbbea3cd647c509d8cfd2db8ccd0c72
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-03-30 12:40:55 +0000

    * Fix brokenness due to automatic transformations.
    svn path=/nixpkgs/trunk/; revision=879

--- pkgs/development/libraries/zlib/default.nix
+++ pkgs/development/libraries/zlib/default.nix
@@ -2,7 +2,6 @@
 
 stdenv.mkDerivation {
   name = "zlib-1.2.1";
-  builder = ./builder.sh;
   src = fetchurl {
     url = http://www.gzip.org/zlib/zlib-1.2.1.tar.gz;
     md5 = "ef1cb003448b4a53517b8f25adb12452";
