0376334dde8ba130e6a53b308567a0838c6207c3
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-03-31 10:19:01 +0000

    * New Nix release.
    svn path=/nixpkgs/trunk/; revision=886

--- pkgs/misc/nix/builder.sh
+++ pkgs/misc/nix/builder.sh
@@ -1,10 +1,7 @@
-buildinputs="$aterm $bdb"
 . $stdenv/setup
 
-tar xvfj $src
-cd nix-*
-./configure --prefix=$out \
- --with-store-dir=/nix/store --localstatedir=/nix/var \
- --with-aterm=$aterm --with-bdb=$bdb
-make
-make install
+configureFlags="\
+  --with-store-dir=/nix/store --localstatedir=/nix/var \
+  --with-aterm=$aterm --with-bdb=$bdb"
+
+genericBuild
--- pkgs/misc/nix/default.nix
+++ pkgs/misc/nix/default.nix
@@ -1,15 +1,19 @@
-{stdenv, fetchurl, aterm, bdb}:
+{ stdenv, fetchurl, aterm, bdb, perl
+, storeDir ? "/nix/store"
+, stateDir ? "/nix/var"
+}:
 
-assert aterm != null && bdb != null;
+assert aterm != null && bdb != null && perl != null;
 # assert bdb.version >= 4.2
 # assert aterm.version >= 2.0
 
 stdenv.mkDerivation {
-  name = "nix-0.5pre807";
+  name = "nix-0.5pre881";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://catamaran.labs.cs.uu.nl/dist/nix/nix-0.5pre807/nix-0.5pre807.tar.bz2;
-    md5 = "fa64bfc39de3e8903954328e4a90d530";
+    url = http://catamaran.labs.cs.uu.nl/dist/nix/nix-0.5pre881/nix-0.5pre881.tar.bz2;
+    md5 = "427a4add7b374ea392ec81a3494cfb23";
   };
-  inherit aterm bdb;
+  buildInputs = [aterm bdb perl];
+  inherit storeDir stateDir aterm bdb;
 }
