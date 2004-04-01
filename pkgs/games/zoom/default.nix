57c5759250458381ebb8375b9fbeb1acceb65442
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-04-01 20:20:09 +0000

    * Missing file.
    svn path=/nixpkgs/trunk/; revision=902

---
+++ pkgs/games/zoom/default.nix
@@ -0,0 +1,14 @@
+{stdenv, fetchurl, perl, expat, xlibs, freetype}:
+
+# !!! assert freetype == xlibs.freetype
+
+stdenv.mkDerivation {
+  name = "zoom-1.0.2alpha1";
+  src = fetchurl {
+    url = http://www.logicalshift.demon.co.uk/unix/zoom/zoom-1.0.2alpha1.tar.gz;
+    md5 = "91b2fe444028178aa3b23bd0e3ae1a61";
+  };
+  buildInputs = [perl expat xlibs freetype];
+  # Zoom doesn't add the right directory in the include path.
+  CFLAGS = ["-I" (freetype ~ /include/freetype2)];
+}
