9c55897afc8589040682521e2954147045f9ce2d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-04-07 14:15:54 +0000

    * Accept relative paths such as `-I ..' (`-I..' was already being   accepted).
    svn path=/nixpkgs/trunk/; revision=924

--- pkgs/build-support/gcc-wrapper/utils.sh
+++ pkgs/build-support/gcc-wrapper/utils.sh
@@ -4,8 +4,18 @@ skip () {
     fi
 }
 
+
+# Checks whether a path is impure.  E.g., `/lib/foo.so' is impure, but
+# `/nix/store/.../lib/foo.so' isn't.
 badPath() {
     local p=$1
+    
+    # Relative paths are okay (since they're presumably relative to
+    # the temporary build directory).
+    if test "${p:0:1}" != "/"; then return 1; fi
+    
+    # Otherwise, the path should refer to the store or some temporary
+    # directory (including the build directory).
     test \
         "${p:0:${#NIX_STORE}}" != "$NIX_STORE" -a \
         "${p:0:4}" != "/tmp" -a \
