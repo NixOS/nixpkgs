1049ba5906de5f46f36ab4ea73c27a9995d572e9
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-04-14 12:55:37 +0000

    * Specified MD5 hash.
    svn path=/nixpkgs/trunk/; revision=940

--- pkgs/development/compilers/strategoxt/trunk.nix
+++ pkgs/development/compilers/strategoxt/trunk.nix
@@ -7,6 +7,7 @@ stdenv.mkDerivation {
   src = fetchsvn {
     url = https://svn.cs.uu.nl:12443/repos/StrategoXT/trunk/StrategoXT;
     rev = "4792";
+    md5 = "0a7bba2cabfc7a9e2b9b5b43ddb3b7ad";
   };
 
   buildInputs = [autoconf automake libtool which aterm sdf];
