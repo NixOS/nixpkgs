9ba11d2be68130c5e082a8d6df2e75d700d2c3b3
Martin Bravenboer <martin.bravenboer@logicblox.com>
2004-05-12 15:35:06 +0000

    Upgrade to generic build
    svn path=/nixpkgs/trunk/; revision=1015

--- pkgs/development/tools/parsing/toolbuslib/builder.sh
+++ pkgs/development/tools/parsing/toolbuslib/builder.sh
@@ -1,7 +1,3 @@
-buildinputs="$aterm"
-. $stdenv/setup || exit 1
-
-tar zxf $src || exit 1
-cd toolbuslib-* || exit 1
-./configure --prefix=$out --with-aterm=$aterm || exit 1
-make install || exit 1
+. $stdenv/setup
+configureFlags="--with-aterm=$aterm"
+genericBuild
--- pkgs/development/tools/parsing/toolbuslib/toolbuslib-0.5.1.nix
+++ pkgs/development/tools/parsing/toolbuslib/toolbuslib-0.5.1.nix
@@ -1,11 +1,12 @@
-{stdenv, fetchurl, aterm}: derivation {
+{stdenv, fetchurl, aterm}:
+
+stdenv.mkDerivation {
   name = "toolbuslib-0.5.1";
-  system = stdenv.system;
   builder = ./builder.sh;
   src = fetchurl {
     url = http://www.cwi.nl/projects/MetaEnv/toolbuslib/toolbuslib-0.5.1.tar.gz;
     md5 = "1c7c7cce870f813bef60bbffdf061c90";
   };
-  stdenv = stdenv;
-  aterm = aterm;
+  buildInputs = [aterm];
 }
+
