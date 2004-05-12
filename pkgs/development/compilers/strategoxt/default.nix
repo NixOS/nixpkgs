ecdac2b80476a52f76d11bb3dcad3aeca7db67dc
Martin Bravenboer <martin.bravenboer@logicblox.com>
2004-05-12 14:34:12 +0000

    Upgrade strategoxt to 0.10
    svn path=/nixpkgs/trunk/; revision=1013

--- pkgs/development/compilers/strategoxt/default.nix
+++ pkgs/development/compilers/strategoxt/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, aterm, sdf}:
 
 stdenv.mkDerivation {
-  name = "strategoxt-0.9.5";
+  name = "strategoxt-0.10";
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.stratego-language.org/pub/stratego/StrategoXT/strategoxt-0.9.5.tar.gz;
-    md5 = "c3caea5c05f8d8439450866b6d5664df";
+    url = ftp://ftp.stratego-language.org/pub/stratego/StrategoXT/strategoxt-0.10.tar.gz;
+    md5 = "526a28e84248b649bb098b22d227cd26";
   };
   inherit aterm sdf;
 }
