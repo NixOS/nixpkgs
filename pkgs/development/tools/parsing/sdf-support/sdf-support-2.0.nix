100e0387e516aca949cb5e183e59af6b3be237b8
Martin Bravenboer <martin.bravenboer@logicblox.com>
2004-05-12 15:57:23 +0000

    More upgrades
    svn path=/nixpkgs/trunk/; revision=1017

--- pkgs/development/tools/parsing/sdf-support/sdf-support-2.0.nix
+++ pkgs/development/tools/parsing/sdf-support/sdf-support-2.0.nix
@@ -1,13 +1,12 @@
-{stdenv, fetchurl, aterm, toolbuslib, ptsupport}: derivation {
+{stdenv, fetchurl, aterm, toolbuslib, ptsupport}:
+
+stdenv.mkDerivation {
   name = "sdf-support-2.0";
-  system = stdenv.system;
   builder = ./builder.sh;
   src = fetchurl {
     url = http://www.cwi.nl/projects/MetaEnv/sdf-support/sdf-support-2.0.tar.gz;
     md5 = "2987b89ed1d73e34e128b895ff44264c";
   };
-  stdenv = stdenv;
-  aterm = aterm;
-  ptsupport = ptsupport;
-  toolbuslib = toolbuslib;
+  inherit stdenv aterm ptsupport toolbuslib;
+  buildInputs = [stdenv aterm ptsupport toolbuslib];
 }
