56fd464a72101b6ee7a754170406aa995a394e5f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-06-29 17:33:54 +0000

    * GCC 3.4 now seems to work.
    svn path=/nixpkgs/trunk/; revision=1105

--- pkgs/development/compilers/gcc-3.4/builder.sh
+++ pkgs/development/compilers/gcc-3.4/builder.sh
@@ -1,8 +1,48 @@
 . $stdenv/setup
 
 
-FIXINC_DUMMY=$NIX_BUILD_TOP/dummy
-mkdir $FIXINC_DUMMY
+export NIX_FIXINC_DUMMY=$NIX_BUILD_TOP/dummy
+mkdir $NIX_FIXINC_DUMMY
+
+
+if test "$noSysDirs" = "1"; then
+
+    if test "$noSysDirs" = "1"; then
+        # Figure out what extra flags to pass to the gcc compilers
+        # being generated to make sure that they use our glibc.
+        if test -e $NIX_GCC/nix-support/orig-glibc; then
+            glibc=$(cat $NIX_GCC/nix-support/orig-glibc)
+            # Ugh.  Copied from gcc-wrapper/builder.sh.  We can't just
+            # source in $NIX_GCC/nix-support/add-flags, since that
+            # would cause *this* GCC to be linked against the
+            # *previous* GCC.  Need some more modularity there.
+            extraCFlags="-B$glibc/lib -isystem $glibc/include"
+            extraLDFlags="-B$glibc/lib -L$glibc/lib -Wl,-s \
+              -Wl,-dynamic-linker,$glibc/lib/ld-linux.so.2"
+
+            # Oh, what a hack.  I should be shot for this.
+            # In stage 1, we should link against the previous GCC, but
+            # not afterwards.  Otherwise we retain a dependency.
+            # However, ld-wrapper, which adds the linker flags for the
+            # previous GCC, is also used in stage 2/3.  We can prevent
+            # it from adding them by NIX_GLIBC_FLAGS_SET, but then
+            # gcc-wrapper will also not add them, thereby causing
+            # stage 1 to fail.  So we use a trick to only set the
+            # flags in gcc-wrapper.
+            hook=$(pwd)/ld-wrapper-hook
+            echo "NIX_GLIBC_FLAGS_SET=1" > $hook
+            export NIX_LD_WRAPPER_START_HOOK=$hook
+        fi
+
+        export NIX_EXTRA_CFLAGS=$extraCFlags
+        export NIX_EXTRA_LDFLAGS=$extraLDFlags
+        export CFLAGS=$extraCFlags
+        export LDFLAGS=$extraLDFlags
+    fi
+
+else
+    patches=""
+fi
 
 
 preConfigure() {
@@ -25,58 +65,6 @@ preConfigure() {
 }
 
 preConfigure=preConfigure
-
-
-postConfigure() {
-    if test "$noSysDirs" = "1"; then
-        # Patch some of the makefiles to force linking against our own
-        # glibc.
-        if test -e $NIX_GCC/nix-support/orig-glibc; then
-            glibc=$(cat $NIX_GCC/nix-support/orig-glibc)
-            # Ugh.  Copied from gcc-wrapper/builder.sh.  We can't just
-            # source in $NIX_GCC/nix-support/add-flags, since that
-            # would cause *this* GCC to be linked against the
-            # *previous* GCC.  Need some more modularity there.
-            extraFlags="-Wl,-s -B$glibc/lib -isystem $glibc/include \
-                -L$glibc/lib -Wl,-dynamic-linker -Wl,$glibc/lib/ld-linux.so.2"
-
-            # Oh, what a hack.  I should be shot for this.
-            # In stage 1, we should link against the previous GCC, but
-            # not afterwards.  Otherwise we retain a dependency.
-            # However, ld-wrapper, which adds the linker flags for the
-            # previous GCC, is also used in stage 2/3.  We can prevent
-            # it from adding them by NIX_GLIBC_FLAGS_SET, but then
-            # gcc-wrapper will also not add them, thereby causing
-            # stage 1 to fail.  So we use a trick to only set the
-            # flags in gcc-wrapper.
-            hook=$(pwd)/ld-wrapper-hook
-            echo "NIX_GLIBC_FLAGS_SET=1" > $hook
-            export NIX_LD_WRAPPER_START_HOOK=$hook
-        fi
-
-#        mf=Makefile
-#        sed \
-#            -e "s^FLAGS_FOR_TARGET =\(.*\)^FLAGS_FOR_TARGET = \1 $extraFlags^" \
-#            < $mf > $mf.tmp
-#        mv $mf.tmp $mf
-
-#        mf=gcc/Makefile
-#        sed \
-#            -e "s^X_CFLAGS =\(.*\)^X_CFLAGS = \1 $extraFlags^" \
-#            < $mf > $mf.tmp
-#        mv $mf.tmp $mf
-
-        # Patch gcc/Makefile to prevent fixinc.sh from "fixing" system
-        # header files from /usr/include.
-#        mf=gcc/Makefile
-#        sed \
-#            -e "s^NATIVE_SYSTEM_HEADER_DIR =\(.*\)^NATIVE_SYSTEM_HEADER_DIR = $FIXINC_DUMMY^" \
-#            < $mf > $mf.tmp
-#        mv $mf.tmp $mf
-    fi
-}
-
-postConfigure=postConfigure
 
 
 makeFlags="bootstrap"
