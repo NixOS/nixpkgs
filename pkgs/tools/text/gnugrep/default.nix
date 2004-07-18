0a32c85cb66c673935f16fe1043aee7cfb034611
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-07-18 21:16:45 +0000

    * Use a mirror (losser), since gnugrep-2.5.1 has been removed from the   FSF server.
    svn path=/nixpkgs/trunk/; revision=1175

--- pkgs/tools/text/gnugrep/default.nix
+++ pkgs/tools/text/gnugrep/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation {
   name = "gnugrep-2.5.1";
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/gnu/grep/grep-2.5.1.tar.bz2;
+    url = http://losser.st-lab.cs.uu.nl/~eelco/mirror/grep-2.5.1.tar.bz2;
     md5 = "ddd99e2d5d4f4611357e31e97f080cf2";
   };
   buildInputs = [pcre];
