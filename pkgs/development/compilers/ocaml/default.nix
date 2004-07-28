417aa140f6e54fdaa65fefa783fc39f89f816bfe
Eelco Visser <visser@acm.org>
2004-07-28 11:23:35 +0000

    don't use patched configure script for ocaml
    svn path=/nixpkgs/trunk/; revision=1191

--- pkgs/development/compilers/ocaml/default.nix
+++ pkgs/development/compilers/ocaml/default.nix
@@ -7,7 +7,6 @@ stdenv.mkDerivation {
     url = http://caml.inria.fr/distrib/ocaml-3.08/ocaml-3.08.0.tar.gz;
     md5 = "c6ef478362295c150101cdd2efcd38e0";
   };
-  configureScript = ./configure;
   dontAddPrefix = "True";
   configureFlags = ["-no-tk" "-x11lib" x11];
   makeFlags = ["world" "bootstrap" "opt"];
