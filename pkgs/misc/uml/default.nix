b9b9e8b29ec0851aadf8d74c571aaaf1e358d62b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-07-31 17:29:10 +0000

    * UML 2.4.24-2 is less buggy.
    svn path=/nixpkgs/trunk/; revision=1209

--- pkgs/misc/uml/default.nix
+++ pkgs/misc/uml/default.nix
@@ -3,17 +3,21 @@
 assert patch != null && perl != null && m4 != null;
 
 stdenv.mkDerivation {
-  name = "uml-2.4.25-1";
+  name = "uml-2.4.24-2";
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.nl.kernel.org/pub/linux/kernel/v2.4/linux-2.4.25.tar.bz2;
-    md5 = "5fc8e9f43fa44ac29ddf9a9980af57d8";
+    url = ftp://ftp.nl.kernel.org/pub/linux/kernel/v2.4/linux-2.4.24.tar.bz2;
+    md5 = "1e055c42921b2396a559d84df4c3d9aa";
+#    url = ftp://ftp.nl.kernel.org/pub/linux/kernel/v2.4/linux-2.4.26.tar.bz2;
+#    md5 = "88d7aefa03c92739cb70298a0b486e2c";
   };
   umlPatch = fetchurl {
-    url = http://heanet.dl.sourceforge.net/sourceforge/user-mode-linux/uml-patch-2.4.25-1.bz2;
-    md5 = "aacbb7b19ec7599119313a31328e1912";
+    url = http://heanet.dl.sourceforge.net/sourceforge/user-mode-linux/uml-patch-2.4.24-2.bz2;
+    md5 = "f2aeb4d44f5734d63e98e6d66cc256de";
   };
   noAioPatch = ./no-aio.patch;
+#  hostfsPatch = ./hostfs.patch;
+#  hostfsAccessPatch = ./hostfs-access.patch;
   config = ./config;
   buildInputs = [patch perl m4];
 }
