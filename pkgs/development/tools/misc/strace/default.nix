a53a262a0c19845aaf7e9380f6af9d9d8e7c0b3b
Armijn Hemel <armijn@gpl-violations.org>
2004-08-04 10:12:26 +0000

    add strace (for real)
    svn path=/nixpkgs/trunk/; revision=1219

---
+++ pkgs/development/tools/misc/strace/default.nix
@@ -0,0 +1,11 @@
+{stdenv, fetchurl}:
+
+stdenv.mkDerivation {
+  name = "strace-4.5.6";
+
+  builder = ./builder.sh;
+  src = fetchurl {
+    url = http://heanet.dl.sourceforge.net/sourceforge/strace/strace-4.5.6.tar.bz2;
+    md5 = "2dd9d23430957a7ee0221efb28c66d1e";
+  };
+}
