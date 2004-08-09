2c61f2f1affb78bbb98335e7f19aed600f403b3c
Martin Bravenboer <martin.bravenboer@logicblox.com>
2004-08-09 21:27:04 +0000

    Update ofJ2SDK  1.4.2
    svn path=/nixpkgs/trunk/; revision=1253

--- pkgs/development/compilers/j2sdk/j2sdk-sun-linux.nix
+++ pkgs/development/compilers/j2sdk/j2sdk-sun-linux.nix
@@ -14,11 +14,11 @@ assert stdenv.system == "i686-linux";
 
 derivation {
   name = "j2sdk-1.4.2";
-  filename = "j2sdk-1.4.2_03";
-  dirname = "j2sdk1.4.2_03";
+  filename = "j2sdk-1.4.2_05";
+  dirname = "j2sdk1.4.2_05";
   system = stdenv.system;
   builder = ./builder.sh;
-  pathname = "/tmp/j2sdk-1_4_2_03-linux-i586.bin";
-  md5 = "859aa6750161477acec83b7bb5d9998d";
+  pathname = "/tmp/j2sdk-1_4_2_05-linux-i586.bin";
+  md5 = "825ff134f3e370f6e677638d32962082";
   stdenv = stdenv;
 }
