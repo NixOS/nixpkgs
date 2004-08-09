7418420edc6b88cf6ffee8a85f92d9c231e0a3df
Martin Bravenboer <martin.bravenboer@logicblox.com>
2004-08-09 21:40:24 +0000

    BCEL. Java packages are an incredible mess
    svn path=/nixpkgs/trunk/; revision=1255

---
+++ pkgs/development/libraries/java/jakarta-bcel/builder.sh
@@ -0,0 +1,6 @@
+. $stdenv/setup
+
+tar zxvf $src
+cd bcel-5.1
+mkdir -p $out/share/java/
+cp bcel-5.1.jar $out/share/java/
---
+++ pkgs/development/libraries/java/jakarta-bcel/default.nix
@@ -0,0 +1,14 @@
+{stdenv, fetchurl, regexp}:
+
+stdenv.mkDerivation {
+  name = "jakarta-bcel-5.1";
+  builder = ./builder.sh;
+
+  src = fetchurl {
+    url = http://apache.cs.uu.nl/dist/jakarta/bcel/binaries/bcel-5.1.tar.gz;
+    md5 = "318f22e4f5f59b68cd004db83a7d65dc";
+  };
+
+  inherit regexp;
+  buildInputs = [stdenv];
+}
