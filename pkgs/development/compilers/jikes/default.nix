ec899b9f2b95d34bd78b0e0b3c2c9d8be44ec4d7
Martin Bravenboer <martin.bravenboer@logicblox.com>
2004-08-20 10:54:20 +0000

    Mirror jikes at losser
    svn path=/nixpkgs/trunk/; revision=1300

--- pkgs/development/compilers/jikes/default.nix
+++ pkgs/development/compilers/jikes/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "jikes-1.21";
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://www-126.ibm.com/pub/jikes/1.21/jikes-1.21.tar.bz2;
+    url = http://losser.st-lab.cs.uu.nl/~mbravenb/mirror/jikes-1.21.tar.bz2;
     md5 = "4e45eeab4c75918174e16ea2b695d812";
   };
 }
