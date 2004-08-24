3a2231718da67c28fe679d3b93a52f60145710a8
Martin Bravenboer <martin.bravenboer@logicblox.com>
2004-08-24 12:09:32 +0000

    More stable URL
    svn path=/nixpkgs/trunk/; revision=1329

--- pkgs/servers/sql/postgresql/default.nix
+++ pkgs/servers/sql/postgresql/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
   builder = ./builder.sh;
 
   src = fetchurl {
-    url = ftp://ftp2.nl.postgresql.org/mirror/postgresql/latest/postgresql-7.4.5.tar.bz2;
+    url = ftp://ftp2.nl.postgresql.org/mirror/postgresql/src/7.4.5/postgresql-7.4.5.tar.bz2;
     md5 = "97e750c8e69c208b75b6efedc5a36efb";
   };
 
