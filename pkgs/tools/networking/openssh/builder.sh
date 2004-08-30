4812b512f155da72960da60a58a65787356c7a61
Armijn Hemel <armijn@gpl-violations.org>
2004-08-30 13:53:48 +0000

    remove the generated keys (good? bad? not sure) and change the permissions of the keysign binary. This is because of suid-nix on which the NixOS scripts barf
    svn path=/nixpkgs/trunk/; revision=1366

--- pkgs/tools/networking/openssh/builder.sh
+++ pkgs/tools/networking/openssh/builder.sh
@@ -1,3 +1,9 @@
 . $stdenv/setup
+
+configureFlags="--with-privsep-path=$out/empty"
  
 genericBuild
+
+rm $out/etc/ssh_host_dsa_key $out/etc/ssh_host_dsa_key.pub $out/etc/ssh_host_key $out/etc/ssh_host_key.pub $out/etc/ssh_host_rsa_key $out/etc/ssh_host_rsa_key.pub
+
+chmod +r $out/libexec/ssh-keysign
