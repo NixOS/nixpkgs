80f1579d53d99fc8f99830d5053bc73a2955f79e
Rob Vermaas <rob.vermaas@gmail.com>
2004-09-08 14:23:18 +0000

    svn path=/nixpkgs/trunk/; revision=1380

--- pkgs/development/interpreters/octave/default.nix
+++ pkgs/development/interpreters/octave/default.nix
@@ -10,5 +10,5 @@ stdenv.mkDerivation {
     md5 = "b781f5d0cd750cb9bb3ef23523f5f339";
   };
   buildInputs = [g77 readline ncurses];
-  configureFlags = "--enable-readline --enable-dl --disable-static";
+  configureFlags = "--enable-readline --enable-dl --disable-static --enable-shared";
 }
