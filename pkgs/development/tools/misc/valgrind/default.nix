edda6f73a93b1d329aaf860b5fcfb333b897d3bc
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-09-10 09:55:34 +0000

    * Valgrind 2.2.0.
    svn path=/nixpkgs/trunk/; revision=1388

--- pkgs/development/tools/misc/valgrind/default.nix
+++ pkgs/development/tools/misc/valgrind/default.nix
@@ -3,12 +3,13 @@
 # Note: I added the Perl dependency for Valgrind 2.1.1.  It's needed
 # to generate some files.  Maybe in stable releases we won't need
 # Perl.
+# Update: 2.2.0 still needs it.
 
 stdenv.mkDerivation {
-  name = "valgrind-2.1.1";
+  name = "valgrind-2.2.0";
   src = fetchurl {
-    url = http://developer.kde.org/~sewardj/valgrind-2.1.1.tar.bz2;
-    md5 = "0010c3e8f054ecc633151c62044b646d";
+    url = http://developer.kde.org/~sewardj/valgrind-2.2.0.tar.bz2;
+    md5 = "30dc51f6fc94751b90b04af9c2e2c656";
   };
   buildInputs = [perl];
 }
