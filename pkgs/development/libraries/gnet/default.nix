e72d99e24bff0f4538a6ee3e890a832e3e93aeb1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-09-18 21:18:56 +0000

    * Gnet updated to 2.0.5.
    svn path=/nixpkgs/trunk/; revision=1458

--- pkgs/development/libraries/gnet/default.nix
+++ pkgs/development/libraries/gnet/default.nix
@@ -3,10 +3,10 @@
 assert pkgconfig != null && glib != null;
 
 stdenv.mkDerivation {
-  name = "gnet-2.0.4";
+  name = "gnet-2.0.5";
   src = fetchurl {
-    url = http://www.gnetlibrary.org/src/gnet-2.0.4.tar.gz;
-    md5 = "b43e728391143214e2cfd0b835b6fd2a";
+    url = http://www.gnetlibrary.org/src/gnet-2.0.5.tar.gz;
+    md5 = "126f140618de34801933d192302ed0b9";
   };
   buildInputs = [pkgconfig glib];
 }
