ca6d6323add72673760ae1ea4a2d0d8498a45665
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-09-18 17:11:16 +0000

    * Glibc updated to 2.3.3 (which requires some patches for gcc 3.4 compatibility).
    svn path=/nixpkgs/trunk/; revision=1441

--- pkgs/development/libraries/glibc/builder.sh
+++ pkgs/development/libraries/glibc/builder.sh
@@ -7,6 +7,7 @@ export NIX_NO_SELF_RPATH=1
 postUnpack() {
     cd $sourceRoot
     unpackFile $linuxthreadsSrc
+    rm -rf nptl
     cd ..
 }
 
@@ -17,15 +18,23 @@ preConfigure() {
     mkdir ../build
     cd ../build
     configureScript=../$sourceRoot/configure
+    # `--with-tls --without-__thread' is required when for
+    # linuxthreads.  See
+    # http://sources.redhat.com/bugzilla/show_bug.cgi?id=317.  Be sure
+    # to read Drepper's comment for another classic example of glibc's
+    # release management strategy.
     configureFlags="--enable-add-ons --disable-profile \
-      --with-headers=$kernelHeaders/include"
+      --with-headers=$kernelHeaders/include \
+      --with-tls --without-__thread"
 }
 
 preConfigure=preConfigure
 
 
 postInstall() {
-    make localedata/install-locales
+    if test -n "$installLocales"; then
+        make localedata/install-locales
+    fi
     rm $out/etc/ld.so.cache
     (cd $out/include && ln -s $kernelHeaders/include/* .) || exit 1
     # `glibcbug' causes a retained dependency on the C compiler.
--- pkgs/development/libraries/glibc/default.nix
+++ pkgs/development/libraries/glibc/default.nix
@@ -1,24 +1,38 @@
-{stdenv, fetchurl, kernelHeaders, patch}:
-
-assert patch != null;
+{stdenv, fetchurl, kernelHeaders, installLocales ? true}:
 
 stdenv.mkDerivation {
-  name = "glibc-2.3.2";
+  name = "glibc-2.3.3";
   builder = ./builder.sh;
 
   src = fetchurl {
-    url = ftp://ftp.nl.net/pub/gnu/glibc/glibc-2.3.2.tar.bz2;
-    md5 = "ede969aad568f48083e413384f20753c";
+    url = http://ftp.gnu.org/pub/gnu/glibc/glibc-2.3.3.tar.bz2;
+    md5 = "e825807b98042f807799ccc9dd96d31b";
   };
   linuxthreadsSrc = fetchurl {
-    url = ftp://ftp.nl.net/pub/gnu/glibc/glibc-linuxthreads-2.3.2.tar.bz2;
-    md5 = "894b8969cfbdf787c73e139782167607";
+    url = http://ftp.gnu.org/pub/gnu/glibc/glibc-linuxthreads-2.3.3.tar.bz2;
+    md5 = "8149ea62922e75bd692bc3b92e5e766b";
   };
 
-  # This is a patch to make glibc compile under GCC 3.3.  Presumably
-  # later releases of glibc won't need this.
-  patches = [./glibc-2.3.2-sscanf-1.patch];
+  patches = [
+    # This patch fixes the bug
+    # http://sources.redhat.com/bugzilla/show_bug.cgi?id=312.  Note
+    # that this bug was marked as `WORKSFORME' with the comment to
+    # just use glibc from CVS.  This and the unholy Linuxthreads/NPTL
+    # mess proves that glibc, together with the Linux kernel,
+    # constitutes an AXIS OF EVIL wrt release management.  Patch
+    # obtained from
+    # http://www.pengutronix.de/software/ptxdist/patches-cvs/glibc-2.3.2/generic/fixup.patch.
+    ./fixup.patch
 
-  buildInputs = [patch];
-  inherit kernelHeaders;
+    # Likewise, this fixes the bug reported in
+    # http://sources.redhat.com/ml/libc-alpha/2003-07/msg00117.html.
+    # Die, glibc, die.
+    ./no-unit-at-a-time.patch
+
+    # This is a patch to make glibc compile under GCC 3.3.  Presumably
+    # later releases of glibc won't need this.
+#    ./glibc-2.3.2-sscanf-1.patch
+  ];
+
+  inherit kernelHeaders installLocales;
 }
