698967c01d38a0bcc90fc2ee8d2a68e9481236b0
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-09-18 21:10:44 +0000

    * Fontconfig updated to 2.2.3.
    svn path=/nixpkgs/trunk/; revision=1448

--- pkgs/development/libraries/xlibs/fontconfig/default.nix
+++ pkgs/development/libraries/xlibs/fontconfig/default.nix
@@ -3,11 +3,11 @@
 assert freetype != null && expat != null;
 
 stdenv.mkDerivation {
-  name = "fontconfig-2.2.2";
+  name = "fontconfig-2.2.3";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://freedesktop.org/~fontconfig/release/fontconfig-2.2.2.tar.gz;
-    md5 = "af6e9a8addfa89aa68d703d5eb004185";
+    url = http://freedesktop.org/~fontconfig/release/fontconfig-2.2.3.tar.gz;
+    md5 = "2466a797d645cda5eb466080fdaec416";
   };
   buildInputs = [freetype];
   propagatedBuildInputs = [expat]; # !!! shouldn't be necessary, but otherwise pango breaks
