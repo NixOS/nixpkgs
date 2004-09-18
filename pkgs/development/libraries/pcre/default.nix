e31d1dd9604033e0d5af6230e41c2a52d53e9a41
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-09-18 17:14:22 +0000

    * Pcre updated to 4.5.
    svn path=/nixpkgs/trunk/; revision=1443

--- pkgs/development/libraries/pcre/default.nix
+++ pkgs/development/libraries/pcre/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "pcre-4.3";
+  name = "pcre-4.5";
   src = fetchurl {
-    url = ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-4.3.tar.bz2;
-    md5 = "7bc7d5b590a41e6f9ede30f272002a02";
+    url = ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-4.5.tar.bz2;
+    md5 = "c51bd34197008b128046f0799d2242e4";
   };
 }
