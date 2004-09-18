54c7304dbe0b5a173ca8b3f0c422278ed39f5f35
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-09-18 17:09:17 +0000

    * Binutils updated to 2.15 (required for glibc 2.3.3).
    svn path=/nixpkgs/trunk/; revision=1440

--- pkgs/development/tools/misc/binutils/default.nix
+++ pkgs/development/tools/misc/binutils/default.nix
@@ -1,11 +1,12 @@
 {stdenv, fetchurl, noSysDirs}:
 
 stdenv.mkDerivation {
-  name = "binutils-2.14";
+  name = "binutils-2.15";
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.nl.net/pub/gnu/binutils/binutils-2.14.tar.bz2;
-    md5 = "2da8def15d28af3ec6af0982709ae90a";
+    url = http://ftp.gnu.org/gnu/binutils/binutils-2.15.tar.bz2;
+    md5 = "624e6b74983ac6b2960edaf2d522ca58";
   };
+  patches = [./no-lex.patch];
   inherit noSysDirs;
 }
