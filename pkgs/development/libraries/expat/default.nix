0e8d792804cbc88026f071ebf0553fab1f9aa6f1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-09-18 21:12:53 +0000

    * Expat updated to 1.95.8.
    svn path=/nixpkgs/trunk/; revision=1452

--- pkgs/development/libraries/expat/default.nix
+++ pkgs/development/libraries/expat/default.nix
@@ -1,8 +1,7 @@
 {stdenv, fetchurl}: stdenv.mkDerivation {
-  name = "expat-1.95.7";
-  builder = ./builder.sh;
+  name = "expat-1.95.8";
   src = fetchurl {
-    url = http://heanet.dl.sourceforge.net/sourceforge/expat/expat-1.95.7.tar.gz;
-    md5 = "2ff59c2a5cbdd21a285c5f343e214fa9";
+    url = http://heanet.dl.sourceforge.net/sourceforge/expat/expat-1.95.8.tar.gz;
+    md5 = "aff487543845a82fe262e6e2922b4c8e";
   };
 }
