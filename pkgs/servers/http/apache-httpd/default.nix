78d30da09fa05bc24f944df90bf6e8fb51e81204
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-09-18 21:08:55 +0000

    * Apache updated to 2.0.51.
    svn path=/nixpkgs/trunk/; revision=1446

--- pkgs/servers/http/apache-httpd/default.nix
+++ pkgs/servers/http/apache-httpd/default.nix
@@ -7,12 +7,12 @@ assert db4Support -> db4 != null;
 assert expat != null && perl != null;
 
 stdenv.mkDerivation {
-  name = "apache-httpd-2.0.49";
+  name = "apache-httpd-2.0.51";
 
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.cs.uu.nl/mirror/apache.org/dist/httpd/httpd-2.0.49.tar.gz;
-    md5 = "275d3d37eed1b070f333d3618f7d1954";
+    url = ftp://ftp.cs.uu.nl/mirror/apache.org/dist/httpd/httpd-2.0.51.tar.gz;
+    md5 = "ab93c56b88ca1b9ce71ba12e74bbc5de";
   };
 
   inherit sslSupport db4Support;
