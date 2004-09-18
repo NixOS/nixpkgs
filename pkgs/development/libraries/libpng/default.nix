4088f5118847dd1d7e114016ef9d1c815be1bc08
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-09-18 21:13:33 +0000

    * Libpng updated to 1.2.7 (security fix!).
    svn path=/nixpkgs/trunk/; revision=1453

--- pkgs/development/libraries/libpng/default.nix
+++ pkgs/development/libraries/libpng/default.nix
@@ -3,11 +3,11 @@
 assert zlib != null;
 
 stdenv.mkDerivation {
-  name = "libpng-1.2.5";
+  name = "libpng-1.2.7";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://heanet.dl.sourceforge.net/sourceforge/png-mng/libpng-1.2.5.tar.bz2;
-    md5 = "3fc28af730f12ace49b14568de4ad934";
+    url = http://heanet.dl.sourceforge.net/sourceforge/png-mng/libpng-1.2.7.tar.bz2;
+    md5 = "21030102f99f81c37276403e5956d198";
   };
   propagatedBuildInputs = [zlib];
   inherit zlib;
