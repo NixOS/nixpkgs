4e98bfc5a7a709778075df79273412f319c659b8
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-09-19 10:10:58 +0000

    * Freetype reverted to 2.1.5 since 2.1.9 broke Firefox.
    svn path=/nixpkgs/trunk/; revision=1461

--- pkgs/development/libraries/freetype/default.nix
+++ pkgs/development/libraries/freetype/default.nix
@@ -1,7 +1,9 @@
 {stdenv, fetchurl}: stdenv.mkDerivation {
-  name = "freetype-2.1.9";
+  name = "freetype-2.1.5";
   src = fetchurl {
-    url = http://heanet.dl.sourceforge.net/sourceforge/freetype/freetype-2.1.9.tar.bz2;
-    md5 = "ec1b903e4be5f073caa72458ea58c29c";
+    url = http://heanet.dl.sourceforge.net/sourceforge/freetype/freetype-2.1.5.tar.bz2;
+    md5 = "54537b518b84d04190a1eccd393a29df";
+#    url = http://cesnet.dl.sourceforge.net/sourceforge/freetype/freetype-2.1.7.tar.bz2;
+#    md5 = "d71723948d7c0e44c401b5733c50857e";
   };
 }
