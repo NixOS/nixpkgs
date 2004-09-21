aeee7551f0edb344db30acabb43d5dc7108504ac
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-09-21 15:27:43 +0000

    * Explicitly set POSIX compliance level to work around coreutils   maintainer fascism.
    svn path=/nixpkgs/trunk/; revision=1482

--- pkgs/development/compilers/blackdown/builder.sh
+++ pkgs/development/compilers/blackdown/builder.sh
@@ -6,6 +6,11 @@ cp $src .
 bin=`basename $src`
 chmod u+x $bin
 
+# This is required because those GNU coreutils fuckers suddenly
+# removed the `+N' syntax (due to a misguided desire for "standards
+# compliance"), which the Blackdown installer uses.
+export _POSIX2_VERSION=199209
+
 alias more=cat
 yes yes | ./$bin
 
