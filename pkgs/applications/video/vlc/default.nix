9c4736c4f3cab524f9c5f9fbe30bd8431c4aeaab
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-09-21 16:00:09 +0000

    * VLC updated to 0.7.2.
    svn path=/nixpkgs/trunk/; revision=1484

--- pkgs/applications/video/vlc/default.nix
+++ pkgs/applications/video/vlc/default.nix
@@ -2,18 +2,15 @@
 , stdenv, fetchurl, x11, libXv, wxGTK, libdvdcss, libdvdplay
 , mpeg2dec, a52dec, libmad, alsa}:
 
-assert x11 != null && wxGTK != null && libdvdcss != null
-  && libdvdplay != null && mpeg2dec != null && a52dec != null
-  && libmad != null && alsa != null;
 assert libdvdplay.libdvdread.libdvdcss == libdvdcss;
 assert xvSupport -> libXv != null;
 
 stdenv.mkDerivation {
-  name = "vlc-0.7.1";
+  name = "vlc-0.7.2";
 
   src = fetchurl {
-    url = http://download.videolan.org/pub/videolan/vlc/0.7.1/vlc-0.7.1.tar.gz;
-    md5 = "faa5e3162a3e9b3a3d8c3dcc06f70911";
+    url = http://download.videolan.org/pub/videolan/vlc/0.7.2/vlc-0.7.2.tar.gz;
+    md5 = "25dfcc804cb92f46c0b64ce1466515cc";
   };
 
   buildInputs = [
