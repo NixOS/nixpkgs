301683096803f21ed532d8779cea04adc6eea492
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-09-27 19:36:44 +0000

    * Patchelf updated to latest prerelease.
    svn path=/nixpkgs/trunk/; revision=1522

--- pkgs/development/tools/misc/patchelf/default.nix
+++ pkgs/development/tools/misc/patchelf/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "patchelf-0.1";
+  name = "patchelf-0.1pre1514";
   src = fetchurl {
-    url = http://catamaran.labs.cs.uu.nl/dist/nix/patchelf-0.1pre1501/patchelf-0.1pre1501.tar.gz;
+    url = http://catamaran.labs.cs.uu.nl/dist/nix/patchelf-0.1pre1514/patchelf-0.1pre1514.tar.gz;
     md5 = "b3d82ce1dc68304770fe5411ed718e3a";
   };
 #  src = /home/eelco/Dev/patchelf/patchelf-0.1.tar.gz;
