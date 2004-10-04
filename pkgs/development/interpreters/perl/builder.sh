4211520f76129b1a9e7b21a596d7745f2b0694cd
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-10-04 10:49:47 +0000

    * Removed impurity in Perl (if certain libraries were present in   /usr/lib, it would find them, then fail when using them; maybe this   is a regression introduced when we upgraded to 5.8.5?). * Remove some retained dependencies in Perl (on ar and sed).
    svn path=/nixpkgs/trunk/; revision=1538

--- pkgs/development/interpreters/perl/builder.sh
+++ pkgs/development/interpreters/perl/builder.sh
@@ -1,23 +1,5 @@
 . $stdenv/setup
 
-postUnpack=postUnpack
-postUnpack() {
-    # Perl's Configure messes with PATH.  We can't have that, so we
-    # patch it.  Yeah, this is an ugly hack.
-    if test "$NIX_ENFORCE_PURITY" = "1" -a -n "$NIX_STORE"; then
-        cat Configure | \
-            grep -v '^paths=' | \
-            grep -v '^locincpth=' | \
-            grep -v '^xlibpth=' | \
-            grep -v '^glibpth=' | \
-            grep -v '^loclibpth=' | \
-            grep -v '^locincpth=' | \
-            cat > Configure.tmp
-        mv Configure.tmp Configure
-        chmod +x Configure
-    fi
-}
-
 if test "$NIX_ENFORCE_PURITY" = "1" -a -n "$NIX_STORE"; then
     GLIBC=$(cat $NIX_GCC/nix-support/orig-glibc)
     extraflags="-Dlocincpth=$GLIBC/include -Dloclibpth=$GLIBC/lib"
