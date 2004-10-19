d133b324c04258b112437998d2b346f672606aa3
Armijn Hemel <armijn@gpl-violations.org>
2004-10-19 11:50:04 +0000

    don't let bash depend on tar and gzip
    svn path=/nixpkgs/trunk/; revision=1578

--- pkgs/stdenv/nix-linux-static/bash-static/builder.sh
+++ pkgs/stdenv/nix-linux-static/bash-static/builder.sh
@@ -1,5 +1,4 @@
 . $stdenv/setup
  
-tar zxvf $src
 mkdir $out
-cp -a bash-2.05b/* $out
+cp -a $src $out
--- pkgs/stdenv/nix-linux-static/bash-static/default.nix
+++ pkgs/stdenv/nix-linux-static/bash-static/default.nix
@@ -3,5 +3,5 @@
 stdenv.mkDerivation {
   name = "bash-static-2.05b";
   builder = ./builder.sh;
-  src = ./bash-2.05b-static.tar.gz;
+  src = ./bash;
 }
