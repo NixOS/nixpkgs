99b011dc3098b3b0db4fde671b033f3ee6f77845
Armijn Hemel <armijn@gpl-violations.org>
2004-10-19 15:10:38 +0000

    don't make bzip2 an initial package, but download it instead. After all, we've got gzip :)
    svn path=/nixpkgs/trunk/; revision=1583

--- pkgs/stdenv/nix-linux-static/bzip2-static/builder.sh
+++ pkgs/stdenv/nix-linux-static/bzip2-static/builder.sh
@@ -1,5 +1,8 @@
 . $stdenv/setup
- 
-tar zxvf $src
+
+PATH=$gzip:$PATH
+
+$curl/bin/curl $src > $tarball
+$tar/bin/tar zxvf $tarball
 mkdir $out
 cp -a bzip2-1.0.2/* $out
--- pkgs/stdenv/nix-linux-static/bzip2-static/default.nix
+++ pkgs/stdenv/nix-linux-static/bzip2-static/default.nix
@@ -1,7 +1,9 @@
-{stdenv}:
+{stdenv, gnutar, gzip, curl}:
  
 stdenv.mkDerivation {
+  inherit gnutar gzip curl;
   name = "bzip2-static-1.0.2";
   builder = ./builder.sh;
-  src = ./bzip2-1.0.2-static.tar.gz;
+  src = http://losser.st-lab.cs.uu.nl/~armijn/.nix/bzip2-1.0.2-static.tar.gz;
+  tarball = bzip2-1.0.2-static.tar.gz;
 }
