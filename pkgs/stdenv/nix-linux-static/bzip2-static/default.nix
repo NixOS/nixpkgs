69ef6a1d5e9f14e7f86fca26ead5bf992fdf333b
Armijn Hemel <armijn@gpl-violations.org>
2004-10-19 15:32:53 +0000

    add quotes for tarball variable
    svn path=/nixpkgs/trunk/; revision=1586

--- pkgs/stdenv/nix-linux-static/bzip2-static/default.nix
+++ pkgs/stdenv/nix-linux-static/bzip2-static/default.nix
@@ -5,5 +5,5 @@ stdenv.mkDerivation {
   name = "bzip2-static-1.0.2";
   builder = ./builder.sh;
   src = http://losser.st-lab.cs.uu.nl/~armijn/.nix/bzip2-1.0.2-static.tar.gz;
-  tarball = bzip2-1.0.2-static.tar.gz;
+  tarball = "bzip2-1.0.2-static.tar.gz";
 }
