a44af445554edd6d5f0bc0e79ab43aa8b82cd9ca
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-11-05 20:54:19 +0000

    * Example 2.
    svn path=/nixpkgs/trunk/; revision=1703

---
+++ pkgs/applications/misc/hello/ex-2/default.nix
@@ -0,0 +1,10 @@
+{stdenv, fetchurl, perl}:
+
+stdenv.mkDerivation {
+  name = "hello-2.1.1";
+  src = fetchurl {
+    url = ftp://ftp.nluug.nl/pub/gnu/hello/hello-2.1.1.tar.gz;
+    md5 = "70c9ccf9fac07f762c24f2df2290784d";
+  };
+  buildInputs = [perl];
+}
