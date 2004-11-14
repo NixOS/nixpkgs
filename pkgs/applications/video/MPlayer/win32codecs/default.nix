39d167caacfbaea2f7d9cee3e7de6d9b7fc09328
Eelco Dolstra <eelco.dolstra@logicblox.com>
2004-11-14 14:56:56 +0000

    * Fix url.
    svn path=/nixpkgs/trunk/; revision=1772

--- pkgs/applications/video/MPlayer/win32codecs/default.nix
+++ pkgs/applications/video/MPlayer/win32codecs/default.nix
@@ -2,7 +2,7 @@
   name = "MPlayer-codecs-essential-20040427";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://www2.mplayerhq.hu/MPlayer/releases/codecs/essential-20040427.tar.bz2;
+    url = http://losser.st-lab.cs.uu.nl/~eelco/dist/essential-20040427.tar.bz2;
     md5 = "4ffc1682448aa870aec9d8efc1321a09";
   };
 }
