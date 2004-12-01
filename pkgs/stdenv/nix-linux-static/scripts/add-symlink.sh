98ce16d2baed7307593205fcbd2417399d37a804
Armijn Hemel <armijn@gpl-violations.org>
2004-12-01 18:56:22 +0000

    addd sutff
    svn path=/nixpkgs/branches/nixos-pkgs/; revision=1830

--- pkgs/stdenv/nix-linux-static/scripts/add-symlink.sh
+++ pkgs/stdenv/nix-linux-static/scripts/add-symlink.sh
@@ -2,4 +2,4 @@ $extra2/bin/chmod u+w $out/include
 (cd $out/include && $extra2/bin/ln -s $extra/include/* .) || exit 1
 
 cd $out
-$extra3/bin/find . | $extra3/bin/xargs $patchelf --interpreter $out/lib/ld-linux.so.2 --shrink-rpath
+$extra3/bin/find . -exec $patchelf --interpreter $out/lib/ld-linux.so.2 --shrink-rpath {} \;
