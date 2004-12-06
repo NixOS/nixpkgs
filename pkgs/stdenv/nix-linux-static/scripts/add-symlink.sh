505a857e5503df7fc6a142570a3234eaca65aab0
Armijn Hemel <armijn@gpl-violations.org>
2004-12-06 19:00:06 +0000

    don't match a trailing slash
    svn path=/nixpkgs/branches/nixos-pkgs/; revision=1838

--- pkgs/stdenv/nix-linux-static/scripts/add-symlink.sh
+++ pkgs/stdenv/nix-linux-static/scripts/add-symlink.sh
@@ -4,5 +4,5 @@ $extra2/bin/chmod u+w $out/include
 cd $out
 $extra3/bin/find . -not -type l -exec $extra2/bin/chmod u+w {} \;
 $extra3/bin/find . -exec $patchelf --interpreter $out/lib/ld-linux.so.2 --shrink-rpath {} \; || true
-$extra2/bin/cat ./bin/catchsegv | $extra4/bin/sed "s|/nix/store/[a-z0-9]*-glibc/|$out|g" > ./bin/catchsegv
+$extra2/bin/cat ./bin/catchsegv | $extra4/bin/sed "s|/nix/store/[a-z0-9]*-glibc|$out|g" > ./bin/catchsegv
 $extra3/bin/find . -not -type l -exec $extra2/bin/chmod u-w {} \;
