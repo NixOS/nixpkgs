e656dad03f5f4561fa5e59a8b7303275a2b4bb29
Armijn Hemel <armijn@gpl-violations.org>
2004-12-06 19:20:15 +0000

    remove all other occurences of old glibc path
    svn path=/nixpkgs/branches/nixos-pkgs/; revision=1839

--- pkgs/stdenv/nix-linux-static/scripts/add-symlink.sh
+++ pkgs/stdenv/nix-linux-static/scripts/add-symlink.sh
@@ -4,5 +4,13 @@ $extra2/bin/chmod u+w $out/include
 cd $out
 $extra3/bin/find . -not -type l -exec $extra2/bin/chmod u+w {} \;
 $extra3/bin/find . -exec $patchelf --interpreter $out/lib/ld-linux.so.2 --shrink-rpath {} \; || true
+
 $extra2/bin/cat ./bin/catchsegv | $extra4/bin/sed "s|/nix/store/[a-z0-9]*-glibc|$out|g" > ./bin/catchsegv
+$extra2/bin/cat ./bin/tzselect | $extra4/bin/sed "s|/nix/store/[a-z0-9]*-glibc|$out|g" > ./bin/tzselect
+$extra2/bin/cat ./bin/xtrace | $extra4/bin/sed "s|/nix/store/[a-z0-9]*-glibc|$out|g" > ./bin/xtrace
+$extra2/bin/cat ./bin/ldd | $extra4/bin/sed "s|/nix/store/[a-z0-9]*-glibc|$out|g" > ./bin/ldd
+
+$extra2/bin/cat ./lib/libc.so | $extra4/bin/sed "s|/nix/store/[a-z0-9]*-glibc|$out|g" > ./lib/libc.so
+$extra2/bin/cat ./lib/libpthread.so | $extra4/bin/sed "s|/nix/store/[a-z0-9]*-glibc|$out|g" > ./lib/libpthread.so
+
 $extra3/bin/find . -not -type l -exec $extra2/bin/chmod u-w {} \;
