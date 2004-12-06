fd3077a9842de0da8c57151b6da7eed9bfb647c1
Armijn Hemel <armijn@gpl-violations.org>
2004-12-06 18:30:52 +0000

    replace old glibc reference with $out
    svn path=/nixpkgs/branches/nixos-pkgs/; revision=1836

--- pkgs/stdenv/nix-linux-static/scripts/add-symlink.sh
+++ pkgs/stdenv/nix-linux-static/scripts/add-symlink.sh
@@ -3,5 +3,6 @@ $extra2/bin/chmod u+w $out/include
 
 cd $out
 $extra3/bin/find . -not -type l -exec $extra2/bin/chmod u+w {} \;
-$extra3/bin/find . -exec $patchelf --interpreter $out/lib/ld-linux.so.2 --shrink-rpath {} \;
+$extra3/bin/find . -exec $patchelf --interpreter $out/lib/ld-linux.so.2 --shrink-rpath {} \; || true
+$extra2/bin/cat ./bin/catchsegv | $extra4/bin/sed "s|/nix/store/[a-z0-9]*-glibc/|$out|g" > ./bin/catchsegv
 $extra3/bin/find . -not -type l -exec $extra2/bin/chmod u-w {} \;
