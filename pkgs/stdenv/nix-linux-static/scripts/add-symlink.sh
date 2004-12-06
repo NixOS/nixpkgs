fddd91238a9ed4503d6c9cb599f0615bf5d56625
Armijn Hemel <armijn@gpl-violations.org>
2004-12-06 16:49:55 +0000

    first make all patchable files writable before patching, then change them back (yes, I've checked it, none of the files are writeable, so that all should go well :) )
    svn path=/nixpkgs/branches/nixos-pkgs/; revision=1833

--- pkgs/stdenv/nix-linux-static/scripts/add-symlink.sh
+++ pkgs/stdenv/nix-linux-static/scripts/add-symlink.sh
@@ -2,4 +2,6 @@ $extra2/bin/chmod u+w $out/include
 (cd $out/include && $extra2/bin/ln -s $extra/include/* .) || exit 1
 
 cd $out
+$extra3/bin/find . -not -type l -exec chmod u+w {} \;
 $extra3/bin/find . -exec $patchelf --interpreter $out/lib/ld-linux.so.2 --shrink-rpath {} \;
+$extra3/bin/find . -not -type l -exec chmod u-w {} \;
