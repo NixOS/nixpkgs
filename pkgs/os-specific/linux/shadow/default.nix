1eb961902f37dd00c74c72e209bcc45ed919d0e6
Armijn Hemel <armijn@gpl-violations.org>
2004-12-16 17:45:31 +0000

    fix download location
    svn path=/nixpkgs/branches/nixos-pkgs/; revision=1887

--- pkgs/os-specific/linux/shadow/default.nix
+++ pkgs/os-specific/linux/shadow/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
    
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.pld.org.pl/software/shadow/shadow-4.0.4.1.tar.bz2;
+    url = ftp://ftp.pld.org.pl/software/shadow/old/shadow-4.0.4.1.tar.bz2;
     md5 = "3a3d17d3d7c630b602baf66ae7434c61";
   };
    
