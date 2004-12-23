ba4721190009777bbb2f9f92c80e7008e6f4d4e3
Martin Bravenboer <martin.bravenboer@logicblox.com>
2004-12-23 08:51:15 +0000

    ATerm update
    svn path=/nixpkgs/trunk/; revision=1928

--- pkgs/development/libraries/aterm/default.nix
+++ pkgs/development/libraries/aterm/default.nix
@@ -1,10 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "aterm-2.2";
+  name = "aterm-2.3.1";
   configureFlags = "--with-gcc";
+
   src = fetchurl {
-    url = http://www.cwi.nl/projects/MetaEnv/aterm/aterm-2.2.tar.gz;
-    md5 = "e1098f4cb84dbfce095cb4c14303ec16";
+    url = http://www.cwi.nl/projects/MetaEnv/aterm/aterm-2.3.1.tar.gz;
+    md5 = "5a2d70acc45a9d301e0dba12fcaf77e7";
   };
 }
