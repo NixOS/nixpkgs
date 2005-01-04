583ef5d157dfdd8bc4447f2d1fd47521fbf6e1a9
Rob Vermaas <rob.vermaas@gmail.com>
2005-01-04 13:35:13 +0000

    * octave 2.1.64
    svn path=/nixpkgs/trunk/; revision=1987

--- pkgs/development/interpreters/octave/default.nix
+++ pkgs/development/interpreters/octave/default.nix
@@ -4,10 +4,10 @@ assert readline != null && ncurses != null;
 assert g77.langF77;
 
 stdenv.mkDerivation {
-  name = "octave-2.1.61";
+  name = "octave-2.1.64";
   src = fetchurl {
-    url = ftp://ftp.octave.org/pub/octave/bleeding-edge/octave-2.1.61.tar.bz2;
-    md5 = "66416e4c219dd1f2a83ec45c6958396b";
+    url = ftp://ftp.octave.org/pub/octave/bleeding-edge/octave-2.1.64.tar.bz2;
+    md5 = "01ec8b13bd850123b190129be93adc1f";
   };
   buildInputs = [g77 readline ncurses perl];
   configureFlags = "--enable-readline --enable-dl --disable-static --enable-shared";
