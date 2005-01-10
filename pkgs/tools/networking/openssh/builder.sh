a8d8a8f82c44ccc64210cdc07543561004b243fc
Armijn Hemel <armijn@gpl-violations.org>
2005-01-10 15:33:43 +0000

    don't install keys. The Nix scripts empty the whole environment, including some variables that are used by ssh-keygen.
    svn path=/nixpkgs/branches/nixos-pkgs/; revision=2001

--- pkgs/tools/networking/openssh/builder.sh
+++ pkgs/tools/networking/openssh/builder.sh
@@ -3,11 +3,9 @@
 
 configureFlags="--with-privsep-path=$out/empty"
  
-postInstall() {
-   rm $out/etc/ssh_host_dsa_key $out/etc/ssh_host_dsa_key.pub $out/etc/ssh_host_key $out/etc/ssh_host_key.pub $out/etc/ssh_host_rsa_key $out/etc/ssh_host_rsa_key.pub
-
-   chmod +r $out/libexec/ssh-keysign
+installPhase() {
+   make install-nokeys
 }
-#postInstall=postInstall
+installPhase=installPhase
 
 genericBuild
