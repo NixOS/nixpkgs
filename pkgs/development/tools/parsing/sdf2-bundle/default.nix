c8c0c50eb27365485675b004ea3299c011dd7599
Martin Bravenboer <martin.bravenboer@logicblox.com>
2005-01-10 17:24:39 +0000

    Update of sdf2-bundle 2.3
    svn path=/nixpkgs/trunk/; revision=2003

--- pkgs/development/tools/parsing/sdf2-bundle/default.nix
+++ pkgs/development/tools/parsing/sdf2-bundle/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   builder = ./builder.sh;
   src = fetchurl {
     url = ftp://ftp.stratego-language.org/pub/stratego/sdf2/sdf2-bundle-2.3/sdf2-bundle-2.3.tar.gz;
-    md5 = "523b9b9e4f73ef6abad65d61e9635389";
+    md5 = "79e305c2bdb2a5627719c890fb050bfa";
   };
 
   buildInputs = [aterm];
