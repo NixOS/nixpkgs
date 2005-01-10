5be8b1821a72c2d9257222a82cad74760bb3b2ff
Armijn Hemel <armijn@gpl-violations.org>
2005-01-10 14:05:14 +0000

    upgrade to a newer version of shadowutils
    2000th commit!!!
    
    svn path=/nixpkgs/branches/nixos-pkgs/; revision=2000

--- pkgs/os-specific/linux/shadow/default.nix
+++ pkgs/os-specific/linux/shadow/default.nix
@@ -1,12 +1,12 @@
 {stdenv, fetchurl}:
    
 stdenv.mkDerivation {
-  name = "shadow-4.0.4.1";
+  name = "shadow-4.0.6";
    
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.pld.org.pl/software/shadow/old/shadow-4.0.4.1.tar.bz2;
-    md5 = "3a3d17d3d7c630b602baf66ae7434c61";
+    url = ftp://ftp.pld.org.pl/software/shadow/shadow-4.0.6.tar.bz2;
+    md5 = "3ca79b02c0aaa82128f4c32cb68ffe4f";
   };
    
 }
