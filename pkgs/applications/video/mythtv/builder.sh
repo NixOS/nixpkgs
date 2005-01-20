34326de3c22a62951a86cc77d9534542aa490fe9
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-01-20 22:07:08 +0000

    * MythTV now installs properly, but it doesn't work yet since Qt has   to be built with MySql client support.
    svn path=/nixpkgs/trunk/; revision=2078

--- pkgs/applications/video/mythtv/builder.sh
+++ pkgs/applications/video/mythtv/builder.sh
@@ -2,10 +2,14 @@
 
 export QTDIR=$qt3
 
+
 buildPhase=myBuilder
 myBuilder() {
     qmake mythtv.pro || fail
     make || fail
 }
 
+
 genericBuild
+
+exit 1
--- pkgs/applications/video/mythtv/default.nix
+++ pkgs/applications/video/mythtv/default.nix
@@ -9,6 +9,8 @@ stdenv.mkDerivation {
     md5 = "0eba17cf64c96ea3ead23e7e15419cc0";
   };
 
+  patches = [./settings.patch];
+
   buildInputs = [which qt3 x11 libXinerama libXv libXxf86vm lame];
   inherit qt3;
 }
