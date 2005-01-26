37d2e5519e0acaeeec7f91fbd731f18e6033bc83
Martin Bravenboer <martin.bravenboer@logicblox.com>
2005-01-26 09:36:29 +0000

    Separate files for different Automake versions
    svn path=/nixpkgs/trunk/; revision=2094

---
+++ pkgs/development/tools/misc/automake/automake-1.7.9.nix
@@ -0,0 +1,12 @@
+{stdenv, fetchurl, perl, autoconf}:
+
+stdenv.mkDerivation {
+  name = "automake-1.7.9";
+  builder = ./builder.sh;
+  setupHook = ./setup-hook.sh;
+  src = fetchurl {
+    url = ftp://ftp.nluug.nl/pub/gnu/automake/automake-1.7.9.tar.bz2;
+    md5 = "571fd0b0598eb2a27dcf68adcfddfacb";
+  };
+  buildInputs = [perl autoconf];
+}
