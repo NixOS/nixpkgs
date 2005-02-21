3e0253618474df1e5a334d4f84ff49d33c2d13c1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-02-21 20:42:07 +0000

    * Move the stdenv-linux bootstrap from stdenvs.nix to the   nix-linux-static directory. * Get rid of the old nix-linux. * Commented the bootstrap process.
    svn path=/nixpkgs/trunk/; revision=2263

--- pkgs/stdenv/nix-linux-static/default.nix
+++ pkgs/stdenv/nix-linux-static/default.nix
@@ -1,5 +1,20 @@
+# This file constructs the standard build environment for the
+# Linux/i686 platform.  It's completely pure; that is, it relies on no
+# external (non-Nix) tools, such as /usr/bin/gcc, and it contains a C
+# compiler and linker that do not search in default locations,
+# ensuring purity of components produced by it.
+
+{allPackages}:
+
 rec {
 
+  # The bootstrap process proceeds in several steps.
+
+  # 1) Create a standard environment by downloading pre-built
+  # statically linked binaries of coreutils, gcc, etc.
+
+  # To fetch the pre-built binaries, we use a statically linked `curl'
+  # binary which is unpacked here.
   curl = derivation {
     name = "curl";
     builder = ./bash-static/bash;
@@ -11,6 +26,7 @@ rec {
     args = [ ./scripts/curl-unpack ];
   };
 
+  # This function downloads and unpacks a file.
   download =
   { url, pkgname, postProcess ? [], addToPath ? []
   , extra ? null, extra2 ? null
@@ -27,6 +43,8 @@ rec {
     inherit postProcess addToPath extra extra2 extra3 extra4 patchelf;
   };
 
+  # The various statically linked components that make up the standard
+  # environment.
   coreutils = download {
     url = http://losser.st-lab.cs.uu.nl/~armijn/.nix/coreutils-5.0-static.tar.gz;
     pkgname = "coreutils";
@@ -99,9 +117,11 @@ rec {
     extra = linuxHeaders;
   };
 
-  
-  stdenvInitial = let {
 
+  # The "fake" standard environment used to build "real" standard
+  # environments.  It consists of just bash, coreutils, and sed, which
+  # is all that is needed by ../generic/builder.sh.
+  stdenvInitial = let {
     body = derivation {
       name = "stdenv-linux-static-initial";
       system = "i686-linux";
@@ -115,12 +135,13 @@ rec {
         stdenv = body;
         system = body.system;
       });
-
       shell = ./bash-static/bash;
     };
   };
 
 
+  # This function builds the various standard environments used during
+  # the bootstrap.
   stdenvBootFun = {glibc, gcc, binutils, staticGlibc}: (import ../generic) {
     name = "stdenv-linux-static-boot";
     param1 = if staticGlibc then "static" else "dynamic";
@@ -148,4 +169,95 @@ rec {
       ./patchelf-static
     ];
   };
+
+
+  # Create the first "real" standard environment.  This one consists
+  # of statically linked components only, and a minimal glibc to keep
+  # the gcc configure script happy.
+  stdenvLinuxBoot1 = stdenvBootFun {
+    # Use the statically linked, downloaded glibc/gcc/binutils.
+    inherit glibc gcc binutils;
+    staticGlibc = true;
+  };
+
+  # 2) These are the packages that we can build with the first
+  #    stdenv.  We only need Glibc (in step 3).
+  stdenvLinuxBoot1Pkgs = allPackages {
+    stdenv = stdenvLinuxBoot1;
+    bootCurl = curl;
+  };
+
+  # 3) Build Glibc with the statically linked tools.  The result is the
+  #    full, dynamically linked, final Glibc.
+  stdenvLinuxGlibc = stdenvLinuxBoot1Pkgs.glibc;
+
+  # 4) Construct a second stdenv identical to the first, except that
+  #    this one uses the Glibc built in step 3.  It still uses
+  #    statically linked tools.
+  stdenvLinuxBoot2 = stdenvBootFun {
+    glibc = stdenvLinuxGlibc;
+    staticGlibc = false;
+    inherit gcc binutils;
+  };
+
+  # 5) The packages that can be built using the second stdenv.
+  stdenvLinuxBoot2Pkgs = allPackages {
+    stdenv = stdenvLinuxBoot2;
+    bootCurl = curl;
+  };
+
+  # 6) Construct a third stdenv identical to the second, except that
+  #    this one uses the dynamically linked GCC and Binutils from step
+  #    5.  The other tools (e.g. coreutils) are still static.
+  stdenvLinuxBoot3 = stdenvBootFun {
+    glibc = stdenvLinuxGlibc;
+    staticGlibc = false;
+    inherit (stdenvLinuxBoot2Pkgs) gcc binutils;
+  };
+
+  # 7) The packages that can be built using the third stdenv.
+  stdenvLinuxBoot3Pkgs = allPackages {
+    stdenv = stdenvLinuxBoot3;
+    bootCurl = curl;
+  };
+
+  # 8) Construct the final stdenv.  It uses the Glibc, GCC and
+  #    Binutils built above, and adds in dynamically linked versions
+  #    of all other tools.
+  stdenvLinux = (import ../generic) {
+    name = "stdenv-nix-linux";
+    preHook = ./prehook.sh;
+    initialPath = [
+      ((import ../nix/path.nix) {pkgs = stdenvLinuxBoot3Pkgs;})
+      stdenvLinuxBoot3Pkgs.patchelf
+    ];
+
+    stdenv = stdenvInitial;
+
+    gcc = (import ../../build-support/gcc-wrapper) {
+      stdenv = stdenvInitial;
+      nativeTools = false;
+      nativeGlibc = false;
+      inherit (stdenvLinuxBoot2Pkgs) gcc binutils;
+      glibc = stdenvLinuxGlibc;
+      shell = stdenvLinuxBoot3Pkgs.bash ~ /bin/sh;
+    };
+
+    shell = stdenvLinuxBoot3Pkgs.bash ~ /bin/sh;
+  };
+
+  # 8) Finally, the set of components built using the Linux stdenv.
+  #    Reuse the tools built in the previous steps.
+  stdenvLinuxPkgs =
+    allPackages {
+      stdenv = stdenvLinux;
+      bootCurl = stdenvLinuxBoot3Pkgs.curl;
+    } //
+    {inherit (stdenvLinuxBoot2Pkgs) binutils gcc;} //
+    {inherit (stdenvLinuxBoot3Pkgs)
+      gzip bzip2 bash coreutils diffutils findutils gawk
+      gnumake gnused gnutar gnugrep curl patch patchelf;
+    } //
+    {glibc = stdenvLinuxGlibc;};
+    
 }
--- pkgs/stdenv/nix-linux/gcc-static/builder.sh
+++
@@ -1,5 +0,0 @@
-. $stdenv/setup
- 
-tar zxvf $src
-mkdir $out
-cp -a gcc-3.3.4/* $out
