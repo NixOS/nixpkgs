4eba4a211e649b4f27e17a69e91bee4611253bb2
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-02-22 15:23:56 +0000

    * Check the hash, but only on Nix <= 0.7.
    svn path=/nixpkgs/trunk/; revision=2272

--- pkgs/build-support/fetchurl/builder.sh
+++ pkgs/build-support/fetchurl/builder.sh
@@ -4,4 +4,12 @@ header "downloading $out from $url"
 
 curl --fail --location --max-redirs 20 "$url" > "$out"
 
+if test "$NIX_OUTPUT_CHECKED" != "1"; then
+    actual=$(md5sum -b "$out" | cut -c1-32)
+    if test "$actual" != "$md5"; then
+        echo "hash is $actual, expected $md5"
+        exit 1
+    fi
+fi
+
 stopNest
