5ab3aa73c4409567fda778ff5ea7c893800a21a7
Martin Bravenboer <martin.bravenboer@logicblox.com>
2005-02-22 20:08:24 +0000

    Added the latest Mono release
    svn path=/nixpkgs/trunk/; revision=2276

---
+++ pkgs/development/compilers/mono/default.nix
@@ -0,0 +1,11 @@
+{stdenv, fetchurl, bison, pkgconfig, glib}:
+
+stdenv.mkDerivation {
+  name = "mono-1.1.4";
+  src = fetchurl {
+    url = http://www.go-mono.com/archive/1.1.4/mono-1.1.4.tar.gz;
+    md5 = "66755e5f201e912cecdd19807ba62487";
+  };
+
+  buildInputs = [bison pkgconfig glib];
+}
