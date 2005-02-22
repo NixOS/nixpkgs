44a1f6078742e2da695420ed84f3ef68a7554442
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-02-22 15:34:58 +0000

    * `md5' -> `id' in compatibility mode.
    svn path=/nixpkgs/trunk/; revision=2273

--- pkgs/build-support/fetchurl/builder.sh
+++ pkgs/build-support/fetchurl/builder.sh
@@ -5,9 +5,13 @@ header "downloading $out from $url"
 curl --fail --location --max-redirs 20 "$url" > "$out"
 
 if test "$NIX_OUTPUT_CHECKED" != "1"; then
+    if test "$outputHashAlgo" != "md5"; then
+        echo "hashes other than md5 are unsupported in Nix <= 0.7, upgrade to Nix 0.8"
+        exit 1
+    fi
     actual=$(md5sum -b "$out" | cut -c1-32)
-    if test "$actual" != "$md5"; then
-        echo "hash is $actual, expected $md5"
+    if test "$actual" != "$id"; then
+        echo "hash is $actual, expected $id"
         exit 1
     fi
 fi
