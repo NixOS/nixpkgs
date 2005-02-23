8d24f2179f182625c1754a5c60740de5a713bf1f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-02-23 20:56:08 +0000

    * Some (hopefully temporary) hacks to get Valgrind to compile with   2.6.10 headers.  Now everything in populate-cache.nix builds again :-)
    svn path=/nixpkgs/trunk/; revision=2291

--- pkgs/development/tools/misc/valgrind/default.nix
+++ pkgs/development/tools/misc/valgrind/default.nix
@@ -12,4 +12,14 @@ stdenv.mkDerivation {
     md5 = "30dc51f6fc94751b90b04af9c2e2c656";
   };
   buildInputs = [perl];
+
+  # Hack to get Valgrind to compile with Linux 2.6.10 headers.  The
+  # file `include/asm/processor.h' indirectly needs
+  # CONFIG_X86_L1_CACHE_SHIFT (for the alignment of some type that
+  # probably isn't relevant here anyway).
+  # !!! maybe this should be done in linux-headers?
+  NIX_CFLAGS_COMPILE = "-DCONFIG_X86_L1_CACHE_SHIFT=7";
+
+  # Another kernel header problem.
+  patches = [./pgoff_t.patch];
 }
