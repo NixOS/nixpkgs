25ab1079f958ade8e56be72119eadfed2468d7ff
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-02-23 14:13:27 +0000

    * Use Linux 2.6.10 headers.  2.6.9 gives parse errors in some   applications (e.g.,   `...glibc-2.3.4/include/linux/byteorder/little_endian.h:43: error:   parse error before "__cpu_to_le64p"').
    svn path=/nixpkgs/trunk/; revision=2289

--- pkgs/os-specific/linux/kernel-headers/default.nix
+++ pkgs/os-specific/linux/kernel-headers/default.nix
@@ -3,10 +3,10 @@
 assert stdenv.system == "i686-linux";
 
 stdenv.mkDerivation {
-  name = "linux-headers-2.6.9-i386";
+  name = "linux-headers-2.6.10-i386";
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/os/Linux/system/kernel/v2.6/linux-2.6.9.tar.bz2;
-    md5 = "e921200f074ca97184e150ef5a4af825";
+    url = ftp://ftp.nluug.nl/pub/os/Linux/system/kernel/v2.6/linux-2.6.10.tar.bz2;
+    md5 = "cffcd2919d9c8ef793ce1ac07a440eda";
   };
 }
