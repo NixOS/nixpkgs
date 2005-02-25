ce52a3635d3bbfbc04698d37486aa117a2ce08a1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-02-25 12:05:16 +0000

    * Nix updated to 0.7.
    svn path=/nixpkgs/trunk/; revision=2300

--- pkgs/misc/nix/default.nix
+++ pkgs/misc/nix/default.nix
@@ -8,11 +8,11 @@ assert aterm != null && bdb != null && perl != null;
 # assert aterm.version >= 2.0
 
 stdenv.mkDerivation {
-  name = "nix-0.6";
+  name = "nix-0.7";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://catamaran.labs.cs.uu.nl/dist/nix/nix-0.6/nix-0.6.tar.gz;
-    md5 = "c4a40960d7b00390b24f6f16e87153d3";
+    url = http://catamaran.labs.cs.uu.nl/dist/nix/nix-0.7/nix-0.7.tar.gz;
+    md5 = "36ca1e13ddd45745e2a5975f8a0209ff";
   };
   buildInputs = [aterm bdb perl curl];
   inherit storeDir stateDir aterm bdb;
