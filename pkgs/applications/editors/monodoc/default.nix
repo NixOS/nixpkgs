8853b55a2ef44e5646788793696faab0913933f9
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-03-08 14:39:00 +0000

    * Generate wrappers to add Mono to PATH and to add Monodoc and GTK# to   the GAC search path.  Unfortunately Mono doesn't yet seem to   implement the .NET mechanisms for specifying locations for module   dependencies.
    svn path=/nixpkgs/trunk/; revision=2343

--- pkgs/applications/editors/monodoc/default.nix
+++ pkgs/applications/editors/monodoc/default.nix
@@ -3,10 +3,15 @@
 stdenv.mkDerivation {
   name = "monodoc-1.0.6";
 
+  builder = ./builder.sh;
   src = fetchurl {
     url = http://www.go-mono.com/archive/1.0.6/monodoc-1.0.6.tar.gz;
     md5 = "f2fc27e8e4717d90dc7efa2450625693";
   };
 
+  makeWrapper = ../../../build-support/make-wrapper/make-wrapper.sh;
+
   buildInputs = [mono gtksharp pkgconfig];
+
+  inherit gtksharp;
 }
