fc2318ca6373a699040d94c1802cf868277aa93d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-03-09 17:37:37 +0000

    * Autoconf updated to 2.59.
    svn path=/nixpkgs/trunk/; revision=2350

--- pkgs/development/tools/misc/autoconf/default.nix
+++ pkgs/development/tools/misc/autoconf/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, m4, perl}:
 
 stdenv.mkDerivation {
-  name = "autoconf-2.58";
+  name = "autoconf-2.59";
   src = fetchurl {
-    url = http://catamaran.labs.cs.uu.nl/dist/tarballs/autoconf-2.58.tar.bz2;
-    md5 = "db3fa3069c6554b3505799c7e1022e2b";
+    url = http://ftp.gnu.org/gnu/autoconf/autoconf-2.59.tar.bz2;
+    md5 = "1ee40f7a676b3cfdc0e3f7cd81551b5f";
   };
   buildInputs = [m4 perl];
 }
