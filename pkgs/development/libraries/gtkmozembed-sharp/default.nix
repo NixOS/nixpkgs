dd5ef003fc59df481e24555a24e782926f183f12
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-03-09 17:39:11 +0000

    * Latest gtkmozembed# (replaces gecko#).
    svn path=/nixpkgs/trunk/; revision=2352

--- pkgs/development/libraries/gtkmozembed-sharp/default.nix
+++ pkgs/development/libraries/gtkmozembed-sharp/default.nix
@@ -1,13 +1,10 @@
 {stdenv, fetchurl, pkgconfig, mono, gtksharp, gtk, monoDLLFixer}:
 
 stdenv.mkDerivation {
-  name = "gecko-sharp-0.5";
+  name = "gtkmozembed-sharp-0.7-pre41601";
 
   builder = ./builder.sh;
-  src = fetchurl {
-    url = http://www.go-mono.com/archive/1.0/gecko-sharp-0.5.tar.gz;
-    md5 = "71e75186b2ee5c644d5dd1560ce27357";
-  };
+  src = /home/eelco/gtkmozembed-sharp-0.7-pre41601.tar.bz2;
 
   buildInputs = [
     pkgconfig mono gtksharp gtk
