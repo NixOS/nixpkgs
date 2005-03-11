579b8893a088b41137c29d5ca5f1d9ef40fbc92e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-03-11 13:15:16 +0000

    * Graphviz 2.2.
    svn path=/nixpkgs/trunk/; revision=2382

---
+++ pkgs/tools/graphics/graphviz/builder.sh
@@ -0,0 +1,12 @@
+. $stdenv/setup
+
+configureFlags="\
+    --with-pngincludedir=$libpng/include \
+    --with-pnglibdir=$libpng/lib \
+    --with-jpegincludedir=$libjpeg/include \
+    --with-jpeglibdir=$libjpeg/lib \
+    --with-expatincludedir=$expat/include \
+    --with-expatlibdir=$expat/lib \
+    "
+
+genericBuild
--- pkgs/tools/graphics/graphviz/default.nix
+++ pkgs/tools/graphics/graphviz/default.nix
@@ -3,15 +3,18 @@
 assert libpng != null && libjpeg != null && expat != null;
 
 stdenv.mkDerivation {
-  name = "graphviz-1.12";
+  name = "graphviz-2.2";
 
+  builder = ./builder.sh;
   src = fetchurl {
-    url = http://catamaran.labs.cs.uu.nl/dist/tarballs/graphviz-1.12.tar.gz;
-    md5 = "a5c004c42f58c957f772060d0889059c";
+    url = http://www.graphviz.org/pub/graphviz/ARCHIVE/graphviz-2.2.tar.gz;
+    md5 = "9275d30695a5c22f360acbef7b85acd3";
   };
 
   buildInputs = [x11 libpng libjpeg expat libXaw yacc];
   configureFlags = [
     (if x11 == null then "--without-x" else "")
   ];
+  
+  inherit libpng libjpeg expat;
 }
