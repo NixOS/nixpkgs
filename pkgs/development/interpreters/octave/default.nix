305f12283a58bf9a90c5a43331e7768aae7fb80d
Rob Vermaas <rob.vermaas@gmail.com>
2005-03-16 08:17:02 +0000

    octave: corrected hash
    svn path=/nixpkgs/trunk/; revision=2421

--- pkgs/development/interpreters/octave/default.nix
+++ pkgs/development/interpreters/octave/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
   name = "octave-2.9.0";
   src = fetchurl {
     url = ftp://ftp.octave.org/pub/octave/bleeding-edge/octave-2.9.0.tar.bz2;
-    md5 = "f0fbf6995241b957f078962f7c1148df";
+    md5 = "687a09033bc68f09810e947010bc8f29";
   };
   buildInputs = [g77 readline ncurses perl];
   configureFlags = "--enable-readline --enable-dl --disable-static --enable-shared";
