013300cb0ec3fadea478c707d93b26304c4166d8
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-04-11 08:54:32 +0000

    * Libtiff updated to 3.7.2.
    svn path=/nixpkgs/trunk/; revision=2519

--- pkgs/development/libraries/libtiff/default.nix
+++ pkgs/development/libraries/libtiff/default.nix
@@ -3,11 +3,11 @@
 assert zlib != null && libjpeg != null;
 
 stdenv.mkDerivation {
-  name = "libtiff-3.7.1";
+  name = "libtiff-3.7.2";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://catamaran.labs.cs.uu.nl/dist/tarballs/tiff-3.7.1.tar.gz;
-    md5 = "37d222df12eb23691614cd40b7b1f215";
+    url = ftp://ftp.remotesensing.org/pub/libtiff/tiff-3.7.2.tar.gz;
+    md5 = "9d7123bd0dbde2a3853fb758346adb78";
   };
   propagatedBuildInputs = [zlib libjpeg];
   inherit zlib libjpeg;
