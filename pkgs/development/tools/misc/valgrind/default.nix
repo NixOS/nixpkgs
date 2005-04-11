5c72ac825226909201f8a494cad4b52b8bb336d7
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-04-11 08:55:05 +0000

    * Valgrind updated to 2.4.0.
    svn path=/nixpkgs/trunk/; revision=2520

--- pkgs/development/tools/misc/valgrind/default.nix
+++ pkgs/development/tools/misc/valgrind/default.nix
@@ -3,23 +3,13 @@
 # Note: I added the Perl dependency for Valgrind 2.1.1.  It's needed
 # to generate some files.  Maybe in stable releases we won't need
 # Perl.
-# Update: 2.2.0 still needs it.
+# Update: 2.4.0 still needs it.
 
 stdenv.mkDerivation {
-  name = "valgrind-2.2.0";
+  name = "valgrind-2.4.0";
   src = fetchurl {
-    url = http://catamaran.labs.cs.uu.nl/dist/tarballs/valgrind-2.2.0.tar.bz2;
-    md5 = "30dc51f6fc94751b90b04af9c2e2c656";
+    url = http://valgrind.org/downloads/valgrind-2.4.0.tar.bz2;
+    md5 = "1d0bd81d368789946d32d18a468ea0cf";
   };
   buildInputs = [perl];
-
-  # Hack to get Valgrind to compile with Linux 2.6.10 headers.  The
-  # file `include/asm/processor.h' indirectly needs
-  # CONFIG_X86_L1_CACHE_SHIFT (for the alignment of some type that
-  # probably isn't relevant here anyway).
-  # !!! maybe this should be done in linux-headers?
-  NIX_CFLAGS_COMPILE = "-DCONFIG_X86_L1_CACHE_SHIFT=7";
-
-  # Another kernel header problem.
-  patches = [./pgoff_t.patch];
 }
