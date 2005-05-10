25584bc99dc2854975c5a37df9dd0cce3a461439
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-05-10 14:28:36 +0000

    * The hash of the Acrobat Reader tarball changed.
    svn path=/nixpkgs/trunk/; revision=3024

--- pkgs/applications/misc/acrobat-reader/default.nix
+++ pkgs/applications/misc/acrobat-reader/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   builder = ./builder.sh;
   src = fetchurl {
     url = ftp://ftp.adobe.com/pub/adobe/reader/unix/7x/7.0/enu/AdbeRdr70_linux_enu.tar.gz;
-    md5 = "f847ce21e5d871837f2dc1d2f1baf9a9";
+    md5 = "0ce9b4fc702f831db97a627ef2629675";
   };
   
   libPath = [libXt libXp libXext libX11 glib pango atk gtk libstdcpp5 zlib];
