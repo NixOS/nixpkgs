6485d5fc09a8afd28c1a33aa2d267fdc1b759123
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-05-12 14:42:06 +0000

    * Firefox updated to 1.0.4.
    svn path=/nixpkgs/trunk/; revision=3039

--- pkgs/applications/networking/browsers/firefox/default.nix
+++ pkgs/applications/networking/browsers/firefox/default.nix
@@ -3,12 +3,12 @@
 # !!! assert libIDL.glib == gtk.glib;
 
 (stdenv.mkDerivation {
-  name = "firefox-1.0.3";
+  name = "firefox-1.0.4";
 
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/source/firefox-1.0.3-source.tar.bz2;
-    md5 = "f1a9a8da0547564a0f8a249f9d56bdf4";
+    url = http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.4/source/firefox-1.0.4-source.tar.bz2;
+    md5 = "0f5d0586750fde79ba98ecf3ee4425a7";
   };
 
   buildInputs = [pkgconfig gtk perl zip libIDL libXi];
