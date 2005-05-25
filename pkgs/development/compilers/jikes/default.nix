286a0aa881845b160a28431c5e81e5eff9828a26
Martin Bravenboer <martin.bravenboer@logicblox.com>
2005-05-25 09:21:36 +0000

    Custom builder not necessary
    svn path=/nixpkgs/trunk/; revision=3134

--- pkgs/development/compilers/jikes/default.nix
+++ pkgs/development/compilers/jikes/default.nix
@@ -2,7 +2,6 @@
 
 stdenv.mkDerivation {
   name = "jikes-1.22";
-  builder = ./builder.sh;
   src = fetchurl {
     url = http://mesh.dl.sourceforge.net/sourceforge/jikes/jikes-1.22.tar.bz2;
     md5 = "cda958c7fef6b43b803e1d1ef9afcb85";
