b899b28c733845d124bb1197b0c9ff27eaa98b0e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-06-17 22:06:59 +0000

    * Oops, 1.15 was not the most recent version (and a very bad one, too).
    svn path=/nixpkgs/trunk/; revision=3202

--- pkgs/tools/archivers/gnutar/default.nix
+++ pkgs/tools/archivers/gnutar/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "gnutar-1.15";
+  name = "gnutar-1.15.1";
   src = fetchurl {
-    url = http://ftp.gnu.org/gnu/tar/tar-1.15.tar.bz2;
-    md5 = "412695e2c2ecbe2753d128f303b3ccf4";
+    url = http://ftp.gnu.org/gnu/tar/tar-1.15.1.tar.bz2;
+    md5 = "57da3c38f8e06589699548a34d5a5d07";
   };
 }
