8bef2f3f8263e8cc760c71f332af7c7d1d1cd92b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-06-17 13:51:33 +0000

    * Use the generic builder.
    svn path=/nixpkgs/trunk/; revision=3196

--- pkgs/development/tools/misc/gnum4/builder.sh
+++
@@ -1,7 +0,0 @@
-. $stdenv/setup || exit 1
-
-tar xvfz $src || exit 1
-cd m4-* || exit 1
-./configure --prefix=$out || exit 1
-make || exit 1
-make install || exit 1
--- pkgs/development/tools/misc/gnum4/default.nix
+++ pkgs/development/tools/misc/gnum4/default.nix
@@ -1,7 +1,6 @@
 {stdenv, fetchurl}:
 stdenv.mkDerivation {
   name = "gnum4-1.4.3";
-  builder = ./builder.sh;
   src = fetchurl {
     url = http://ftp.gnu.org/gnu/m4/m4-1.4.3.tar.bz2;
     md5 = "1f7d7eba70a0525c44c2edc3998925c7";
