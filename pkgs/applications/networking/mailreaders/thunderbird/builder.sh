0f77007f1a64aa673695a972d1375aefab1f2cfa
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-06-18 07:20:10 +0000

    * Disable freetype2 because Thunderbird (like Firefox) doesn't like   Freetype >= 2.1.8.
      https://bugzilla.mozilla.org/show_bug.cgi?id=234035
    
    svn path=/nixpkgs/trunk/; revision=3203

--- pkgs/applications/networking/mailreaders/thunderbird/builder.sh
+++ pkgs/applications/networking/mailreaders/thunderbird/builder.sh
@@ -7,6 +7,7 @@ ac_add_options --disable-debug
 ac_add_options --enable-optimize=-O2
 ac_add_options --disable-ldap
 ac_add_options --enable-xft
+ac_add_options --disable-freetype2
 ac_add_options --enable-swg
 ac_add_options --enable-strip
 ac_add_options --enable-default-toolkit=gtk2
