cf09d1933d268f4f9e0a930c0730c2ca19d9a9cc
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-06-30 16:03:44 +0000

    * Revert.
    svn path=/nixpkgs/trunk/; revision=3266

--- pkgs/development/tools/parsing/flex/default.nix
+++ pkgs/development/tools/parsing/flex/default.nix
@@ -4,7 +4,6 @@ assert yacc != null;
 
 stdenv.mkDerivation {
   name = "flex-2.5.4a";
-  builder = ./builder.sh;
   src = fetchurl {
     url = http://catamaran.labs.cs.uu.nl/dist/tarballs/flex-2.5.4a.tar.gz;
     md5 = "bd8753d0b22e1f4ec87a553a73021adf";
