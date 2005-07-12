0fc44c5e5ec8f849864ec1fcd9c721cfbe4b897a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-07-12 11:28:43 +0000

    * SDL updated to 1.2.8.
    svn path=/nixpkgs/trunk/; revision=3315

--- pkgs/development/libraries/SDL/default.nix
+++ pkgs/development/libraries/SDL/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, x11}:
 
 stdenv.mkDerivation {
-  name = "SDL-1.2.7";
+  name = "SDL-1.2.8";
   src = fetchurl {
-    url = http://catamaran.labs.cs.uu.nl/dist/tarballs/SDL-1.2.7.tar.gz;
-    md5 = "d29b34b6ba3ed213893fc9d8d35e357a";
+    url = http://www.libsdl.org/release/SDL-1.2.8.tar.gz;
+    md5 = "37aaf9f069f9c2c18856022f35de9f8c";
   };
   buildInputs = [x11];
-  patches = [./gcc-3.4.patch];
+  patches = [./no-cxx.patch];
 }
