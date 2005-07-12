023e5023409f276e8f6331404a4ee5e07dc9e16a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-07-12 11:40:16 +0000

    * The latest Generator (Genesis / Mega Drive emulator).  No longer   crashes when not started in full-screen mode.
    svn path=/nixpkgs/trunk/; revision=3316

--- pkgs/games/generator/default.nix
+++ pkgs/games/generator/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, gtk, SDL, nasm}:
 
 stdenv.mkDerivation {
-  name = "generator-0.35-cbiere";
+  name = "generator-0.35-cbiere-20050503";
   src = fetchurl {
-    url = http://catamaran.labs.cs.uu.nl/dist/tarballs/generator-0.35-cbiere.tar.bz2;
-    md5 = "6ec4379d8c6c794ec59b9d61e73fb73d";
+    url = http://www.ghostwhitecrab.com/generator/generator-0.35-cbiere.tar.bz2;
+    md5 = "bce3326c165d74e8a00e50355b653e08";
   };
   configureFlags = "--with-gtk --with-raze --with-sdl-audio";
   buildInputs = [gtk SDL nasm];
