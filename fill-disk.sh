62882b6d0a788e63b6122f37e7659f554f193baf
Armijn Hemel <armijn@gpl-violations.org>
2005-07-19 16:02:17 +0000

    use right path
    svn path=/nixu/trunk/; revision=3368

--- fill-disk.sh
+++ fill-disk.sh
@@ -75,7 +75,7 @@ echo copying nixpkgs...
 cp -fa ../pkgs $root/nixpkgs/trunk
 
 make_dir 0755 /tmp/scripts
-cp -fa ../scripts $root/tmp/scripts
+cp -fa ../scripts $root/tmp
 
 echo adding manifest
 $NIX_CMD_PATH/nix-pull $manifest
