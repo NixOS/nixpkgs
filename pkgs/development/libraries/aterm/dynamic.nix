e1084ff4006426932e9d50805be69cef41fc6d22
Martin Bravenboer <martin.bravenboer@logicblox.com>
2005-07-20 19:41:29 +0000

    Upgrade of aterm-dynamic
    svn path=/nixpkgs/trunk/; revision=3388

--- pkgs/development/libraries/aterm/dynamic.nix
+++ pkgs/development/libraries/aterm/dynamic.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation {
   name = "aterm-dynamic-2.3.2";
   src = fetchurl {
-    url = http://catamaran.labs.cs.uu.nl/dist/stratego/aterm-2.3.2pre11645/aterm-2.3.2pre11645.tar.gz;
-    md5 = "f2feeeee6498388911d69133bff83926";
+    url = http://catamaran.labs.cs.uu.nl/dist/stratego/aterm-2.3.2pre11665/aterm-2.3.2pre11665.tar.gz;
+    md5 = "2eb01c044d765ded55f10e05756fec98";
   };
 }
