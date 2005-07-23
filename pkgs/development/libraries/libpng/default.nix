de58e076cb20cbf5dce24e5e8b96079558a3e313
Martin Bravenboer <martin.bravenboer@logicblox.com>
2005-07-23 20:55:52 +0000

    Use the autoconf distribution of libpng
    svn path=/nixpkgs/trunk/; revision=3414

--- pkgs/development/libraries/libpng/default.nix
+++ pkgs/development/libraries/libpng/default.nix
@@ -4,10 +4,9 @@ assert zlib != null;
 
 stdenv.mkDerivation {
   name = "libpng-1.2.8";
-  builder = ./builder.sh;
   src = fetchurl {
-    url = http://catamaran.labs.cs.uu.nl/dist/tarballs/libpng-1.2.8.tar.bz2;
-    md5 = "00cea4539bea4bd34cbf8b82ff9589cd";
+    url = http://surfnet.dl.sourceforge.net/sourceforge/libpng/libpng-1.2.8-config.tar.gz;
+    md5 = "e5a39003eed16103cbbd3b6a8bc6b1f9";
   };
   propagatedBuildInputs = [zlib];
   inherit zlib;
