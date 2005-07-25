ea1f32f822be013f5140e132022671c49a0ccb7b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-07-25 10:05:44 +0000

    * Move to a more stable location.
    svn path=/nixpkgs/trunk/; revision=3420

--- pkgs/development/libraries/aterm/dynamic.nix
+++ pkgs/development/libraries/aterm/dynamic.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation {
   name = "aterm-dynamic-2.3.2";
   src = fetchurl {
-    url = http://catamaran.labs.cs.uu.nl/dist/stratego/aterm-2.3.2pre11665/aterm-2.3.2pre11665.tar.gz;
+    url = http://catamaran.labs.cs.uu.nl/dist/tarballs/aterm-2.3.2pre11665.tar.gz;
     md5 = "2eb01c044d765ded55f10e05756fec98";
   };
 }
