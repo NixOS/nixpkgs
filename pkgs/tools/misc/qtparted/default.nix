869259ba996eca21714dbc71a7cc31bb9116953a
Armijn Hemel <armijn@gpl-violations.org>
2005-07-31 22:20:46 +0000

    preliminary qtparted support for rob, untested (might fail build)
    svn path=/nixpkgs/trunk/; revision=3490

---
+++ pkgs/tools/misc/qtparted/default.nix
@@ -0,0 +1,11 @@
+{stdenv, fetchurl, e2fsprogs, ncurses, readline, parted, libXext, libX11, zlib, qt3}:
+
+stdenv.mkDerivation {
+  name = "qtparted-0.4.4";
+  src = fetchurl {
+    url = http://surfnet.dl.sourceforge.net/sourceforge/qtparted/qtparted-0.4.4.tar.bz2;
+    md5 = "b8253bf21eaebe1f2c22b50462e8046c";
+  };
+  buildInputs = [e2fsprogs ncurses readline parted libXext libX11 zlib qt3];
+  configureFlags = "--disable-reiserfs --disable-jfs --disable-xfs";
+}
