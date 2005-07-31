e60679df476d0f46d57f0ea39ceba9bef692b0d2
Armijn Hemel <armijn@gpl-violations.org>
2005-07-31 20:39:05 +0000

    build libraries too (needed for tools like parted)
    svn path=/nixpkgs/trunk/; revision=3486

--- pkgs/os-specific/linux/e2fsprogs/default.nix
+++ pkgs/os-specific/linux/e2fsprogs/default.nix
@@ -6,6 +6,6 @@ stdenv.mkDerivation {
     url = http://catamaran.labs.cs.uu.nl/dist/tarballs/e2fsprogs-1.36.tar.gz;
     md5 = "1804ee96b76e5e7113fe3cecd6fe582b";
   };
-  configureFlags = "--enable-dynamic-e2fsck";
+  configureFlags = "--enable-dynamic-e2fsck --enable-elf-shlibs";
   buildInputs = [gettext];
 }
