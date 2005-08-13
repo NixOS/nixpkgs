29005ceca7774e80b1cf87886efc6248b33bf1be
Armijn Hemel <armijn@gpl-violations.org>
2005-08-13 19:19:57 +0000

    remove a file that was accidentily added, remove some cruft
    svn path=/nixpkgs/trunk/; revision=3573

--- pkgs/tools/misc/grub-wrapper/builder.sh
+++ pkgs/tools/misc/grub-wrapper/builder.sh
@@ -3,10 +3,3 @@
 
 makeWrapper "$grub/sbin/grub-install" "$out/sbin/grub-install" \
 --suffix-each PATH ':' "$diffutils/bin $gnused/bin $gnugrep/bin"
-
-#echo "#! $SHELL -e" > $out/sbin/grub-install
-#echo PATH=$diffutils/bin:$gnused/bin:$gnugrep/bin:$PATH >> $out/sbin/grub-install
-#echo "exec \"$grub/sbin/grub-install\" \"\$@\"" >> $out/sbin/grub-install
-#
-#chmod +x $out/sbin/grub-install
-
