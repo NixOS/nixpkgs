36c969565fe21928820700f3378b9714951301e7
Armijn Hemel <armijn@gpl-violations.org>
2005-08-27 17:58:07 +0000

    add
    svn path=/nixpkgs/trunk/; revision=3721

---
+++ pkgs/os-specific/linux/mingetty-wrapper/builder.sh
@@ -0,0 +1,10 @@
+. $stdenv/setup
+#. $makeWrapper
+
+ensureDir "$(dirname $out)"
+ensureDir "$(dirname $out/sbin/mingetty)"
+
+cat > $out/sbin/mingetty << END
+#! $SHELL -e
+exec $mingetty/sbin/mingetty --loginprog=$shadowutils/bin/login "\$@"
+END
