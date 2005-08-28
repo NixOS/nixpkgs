3ccc0014b62676204c53b7b843650c30ff13f5fb
Armijn Hemel <armijn@gpl-violations.org>
2005-08-28 10:18:30 +0000

    remove debugline
    svn path=/nixpkgs/trunk/; revision=3735

--- pkgs/os-specific/linux/e2fsprogs-diet/default.nix
+++ pkgs/os-specific/linux/e2fsprogs-diet/default.nix
@@ -7,7 +7,6 @@ stdenv.mkDerivation {
     url = http://nix.cs.uu.nl/dist/tarballs/e2fsprogs-1.36.tar.gz;
     md5 = "1804ee96b76e5e7113fe3cecd6fe582b";
   };
-  #configureFlags = "--with-diet-libc";
   buildInputs = [gettext];
   NIX_GCC = dietgcc;
 }
