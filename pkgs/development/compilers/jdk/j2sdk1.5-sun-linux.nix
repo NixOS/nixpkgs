1122e263eeb9b8e42caed09bfa74f655e5e1bc44
Martin Bravenboer <martin.bravenboer@logicblox.com>
2005-09-17 23:24:40 +0000

    Cleaning up the java naming conventions
    svn path=/nixpkgs/trunk/; revision=3867

--- pkgs/development/compilers/jdk/j2sdk1.5-sun-linux.nix
+++ pkgs/development/compilers/jdk/j2sdk1.5-sun-linux.nix
@@ -1,6 +1,6 @@
 /**
  * This Nix expression requires the user to download the j2sdk
- * distribution to /tmp. Please obtain j2sdk-1_5_0-beta-linux-i586.bin
+ * distribution to /tmp. Please obtain j2sdk-1_5_0_05-linux-i586.bin
  * from java.sun.com by hand and place it in /tmp. Blame Sun, not me.
  *
  * Note that this is not necessary if someone has already pushed a
@@ -14,11 +14,11 @@ assert stdenv.system == "i686-linux";
 
 derivation {
   name = "jdk-1.5.0";
-  filename = "jdk-1_5_0_03";
-  dirname = "jdk1.5.0_03";
+  filename = "jdk-1_5_0_05";
+  dirname = "jdk1.5.0_05";
   system = stdenv.system;
   builder = ./builder.sh;
-  pathname = "/tmp/jdk-1_5_0_03-linux-i586.bin";
-  md5 = "bc221641fcfdc9268499001326fc8ebb";
+  pathname = "/tmp/jdk-1_5_0_05-linux-i586.bin";
+  md5 = "";
   stdenv = stdenv;
 }
