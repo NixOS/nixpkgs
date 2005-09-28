b9a0811148f5b883956f086ab2dd0cf23ae56eb5
Armijn Hemel <armijn@gpl-violations.org>
2005-09-28 14:06:10 +0000

    add fix for braindead module.h, so it can compile with our glibc kernel headers
    svn path=/nixpkgs/trunk/; revision=3953

--- pkgs/os-specific/linux/sysklogd/builder.sh
+++ pkgs/os-specific/linux/sysklogd/builder.sh
@@ -7,4 +7,6 @@ ensureDir "$out/usr/share/man/man8/"
 ensureDir "$out/usr/share/man/man5/"
 ensureDir "$out/usr/sbin"
 
+NIX_CFLAGS_COMPILE="-DCONFIG_X86_L1_CACHE_SHIFT=0 $NIX_CFLAGS_COMPILE"
+
 genericBuild
--- pkgs/os-specific/linux/sysklogd/default.nix
+++ pkgs/os-specific/linux/sysklogd/default.nix
@@ -7,5 +7,5 @@ stdenv.mkDerivation {
     url = http://www.infodrom.org/projects/sysklogd/download/sysklogd-1.4.1.tar.gz;
     md5 = "d214aa40beabf7bdb0c9b3c64432c774";
   };
-  patches = [./sysklogd-1.4.1-cvs-20050525.diff];
+  patches = [./sysklogd-1.4.1-cvs-20050525.diff ./sysklogd-1.4.1-asm.patch];
 }
