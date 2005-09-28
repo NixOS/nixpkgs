d217b912d2e824541c640b6ac28371a9eb4426f8
Roy van den Broek <rbroek@cs.uu.nl>
2005-09-28 10:03:15 +0000

    Hack to prevent `java.lang.UnsatisfiedLinkError: no svnjavahl in java.library.path' from happening when using the javahl bindings for Subversion.
    svn path=/nixpkgs/trunk/; revision=3949

--- pkgs/applications/version-management/subversion-1.2.x/builder.sh
+++ pkgs/applications/version-management/subversion-1.2.x/builder.sh
@@ -38,6 +38,10 @@ postInstall() {
         mkdir -p subversion/bindings/java/javahl/classes # bug fix
         make javahl
         make install-javahl
+        # Hack to prevent java.lang.UnsatisfiedLinkError: no svnjavahl in java.library.path
+        cd $out/lib
+        ln -s libsvnjavahl-1.so libsvnjavahl.so
+        cd -
         mkdir -p $out/share/doc/$name
         $jdk/bin/javadoc -d $out/share/doc/$name \
             -windowtitle "JavaHL Subversion Bindings" \
