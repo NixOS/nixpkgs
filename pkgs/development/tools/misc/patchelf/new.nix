bbbf730cb0f3c12366bc78d307858dde740680bd
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-10-04 13:21:33 +0000

    * Use the latest patchelf.
    svn path=/nixpkgs/trunk/; revision=4006

--- pkgs/development/tools/misc/patchelf/new.nix
+++ pkgs/development/tools/misc/patchelf/new.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "patchelf-0.1pre3994";
+  name = "patchelf-0.1pre4005";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/nix/patchelf-0.1pre3994/patchelf-0.1pre3994.tar.gz;
-    md5 = "26d335b0b1c335c19b1a1b36d1002e09";
+    url = http://nix.cs.uu.nl/dist/nix/patchelf-0.1pre4005/patchelf-0.1pre4005.tar.gz;
+    md5 = "95f0a2025d72a8499d608cc706c097b3";
   };
 }
