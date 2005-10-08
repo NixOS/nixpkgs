1ca2ca9457de18b7af2e9239233fa06a6a23dc10
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-10-08 15:52:03 +0000

    * Firefox 1.5beta2.
    svn path=/nixpkgs/trunk/; revision=4035

--- pkgs/applications/networking/browsers/firefox-1.5beta/default.nix
+++ pkgs/applications/networking/browsers/firefox-1.5beta/default.nix
@@ -2,16 +2,17 @@
 
 # !!! assert libIDL.glib == gtk.glib;
 
-(stdenv.mkDerivation {
-  name = "firefox-1.5beta1";
+stdenv.mkDerivation {
+  name = "firefox-1.5beta2";
 
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5b1/source/firefox-1.5b1-source.tar.bz2;
-    md5 = "a1b2549a31c74e7366213bb2ba76876f";
+    url = http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5b2/source/firefox-1.5b2-source.tar.bz2;
+    md5 = "08eb51cbab8050ebfd3d53e6592fc0f0";
   };
 
   buildInputs = [pkgconfig gtk perl zip libIDL libXi];
+  inherit gtk;
 
   patches = [./writable-copies.patch];
-}) // {inherit gtk;}
+}
