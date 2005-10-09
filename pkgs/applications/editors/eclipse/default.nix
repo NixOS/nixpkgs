4f21598f2f7fbd5cfe9f9e148cda3a1d17686d89
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-10-09 16:36:35 +0000

    * Eclipse updated to 3.1.1.
    svn path=/nixpkgs/trunk/; revision=4038

--- pkgs/applications/editors/eclipse/default.nix
+++ pkgs/applications/editors/eclipse/default.nix
@@ -3,7 +3,7 @@
 let {
   body =
     stdenv.mkDerivation {
-      name = "eclipse-sdk-3.1";
+      name = "eclipse-sdk-3.1.1";
       builder = ./builder.sh;
       src = bindist;
       inherit makeWrapper jdk;
@@ -13,7 +13,7 @@ let {
 
   bindist = 
     fetchurl {
-      url = ftp://sunsite.informatik.rwth-aachen.de/pub/mirror/eclipse/R-3.1-200506271435/eclipse-SDK-3.1-linux-gtk.tar.gz;
-      md5 = "0441c11cc5af1e84ed3be322929899e8";
+      url = http://sunsite.informatik.rwth-aachen.de/eclipse/downloads/drops/R-3.1.1-200509290840/eclipse-SDK-3.1.1-linux-gtk.tar.gz;
+      md5 = "a2ae61431657e2ed247867b9a9948290";
     };
 }
