1cc9ec7f6d051bc7d668598a1c2d9ef84da205b8
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-10-11 12:42:48 +0000

    * ATerm updated to 2.4.2.
    svn path=/nixpkgs/trunk/; revision=4051

--- pkgs/development/libraries/aterm/aterm-2.4.nix
+++ pkgs/development/libraries/aterm/aterm-2.4.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "aterm-2.4";
+  name = "aterm-2.4.2";
   src = fetchurl {
-    url = http://www.cwi.nl/projects/MetaEnv/aterm/aterm-2.4.tar.gz;
-    md5 = "baf12d71372cfaa0d39145234e262fef";
+    url = http://www.cwi.nl/projects/MetaEnv/aterm/aterm-2.4.2.tar.gz;
+    md5 = "18617081dd112d85e6c4b1b552628114";
   };
 }
