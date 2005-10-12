20c68ab323ebe5020af7c82e89b0e149e0c87a27
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-10-12 09:06:02 +0000

    * Use the right hash.
    svn path=/nixpkgs/trunk/; revision=4071

--- pkgs/development/libraries/gtk-libs-2.6/glib/default.nix
+++ pkgs/development/libraries/gtk-libs-2.6/glib/default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation {
   name = "glib-2.6.6";
   src = fetchurl {
     url = ftp://ftp.gtk.org/pub/gtk/v2.6/glib-2.6.6.tar.bz2;
-    md5 = "dba15cceeaea39c5a61b6844d2b7b920";
+    md5 = "6e22b0639d314536f23ee118f29b43de";
   };
   buildInputs = [pkgconfig gettext perl];
 }
