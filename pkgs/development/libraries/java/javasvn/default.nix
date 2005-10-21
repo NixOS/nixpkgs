6011c99f48ae0ac99e154cf5cca8b45368e1b2fe
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-10-21 15:52:31 +0000

    * JavaSVN updated to 0.9.3 (because the 0.9.2 zipfile had disappeared).
    svn path=/nixpkgs/trunk/; revision=4115

--- pkgs/development/libraries/java/javasvn/default.nix
+++ pkgs/development/libraries/java/javasvn/default.nix
@@ -1,12 +1,12 @@
 {stdenv, fetchurl, unzip} :
 
 stdenv.mkDerivation {
-  name = "javasvn-0.9.2";
+  name = "javasvn-0.9.3";
   builder = ./builder.sh;
 
   src = fetchurl {
-    url = http://tmate.org/svn/org.tmatesoft.svn_0.9.2.standalone.zip;
-    md5 = "0f51c8a5daadccd0a7c301b265fda893";
+    url = http://nix.cs.uu.nl/dist/tarballs/org.tmatesoft.svn_0.9.3.standalone.zip;
+    md5 = "8c8a1e4e3b7306ee4d933e26a5aab2ab";
   };
   
   inherit unzip;
