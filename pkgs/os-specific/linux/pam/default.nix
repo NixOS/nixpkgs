e4453149f9c29692ebee066888a18214837ff173
Armijn Hemel <armijn@gpl-violations.org>
2005-10-24 16:36:03 +0000

    add a patch that replaces `/bin/pwd` with `pwd` and rely on stdenv to supply pwd
    svn path=/nixpkgs/trunk/; revision=4125

--- pkgs/os-specific/linux/pam/default.nix
+++ pkgs/os-specific/linux/pam/default.nix
@@ -6,4 +6,5 @@ stdenv.mkDerivation {
     url = http://www.nl.kernel.org/pub/linux/libs/pam/pre/library/Linux-PAM-0.80.tar.bz2;
     md5 = "ccff87fe639efdfc22b1ba4a0f08ec57";
   };
+  patches = [./pam-pwd.patch];
 }
