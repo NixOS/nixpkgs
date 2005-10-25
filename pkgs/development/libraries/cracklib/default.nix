0b6462aee7cd16abc7749ee1d7741a4fa7ade7da
Armijn Hemel <armijn@gpl-violations.org>
2005-10-25 17:13:56 +0000

    also add cracklib dictionairies
    svn path=/nixpkgs/trunk/; revision=4128

--- pkgs/development/libraries/cracklib/default.nix
+++ pkgs/development/libraries/cracklib/default.nix
@@ -2,8 +2,13 @@
 
 stdenv.mkDerivation {
   name = "cracklib-2.8.5";
+  builder = ./builder.sh;
   src = fetchurl {
     url = http://surfnet.dl.sourceforge.net/sourceforge/cracklib/cracklib-2.8.5.tar.gz;
     md5 = "68674db41be7569099b7aa287719b248";
+  };
+  dicts = fetchurl {
+    url = http://surfnet.dl.sourceforge.net/sourceforge/cracklib/cracklib-words.gz;
+    md5 = "d18e670e5df560a8745e1b4dede8f84f";
   };
 }
