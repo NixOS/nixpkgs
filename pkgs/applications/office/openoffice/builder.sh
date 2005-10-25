9d7df2c6ad2dccf16abd834551d74884fea9d542
Armijn Hemel <armijn@gpl-violations.org>
2005-10-25 23:29:16 +0000

    more dependencies, use a lot of system packages instead of using the tarballs that are in the OOo2 source dist. It seems the only things missing in the tarball are a kernel (but it has to be cross platform) and emacs...
    Gah, BLOAT!
    
    svn path=/nixpkgs/trunk/; revision=4135

--- pkgs/applications/office/openoffice/builder.sh
+++ pkgs/applications/office/openoffice/builder.sh
@@ -2,7 +2,7 @@ source $stdenv/setup
 
 configurePhase() {
   cd config_office/;
-  ./configure;
+  ./configure --disable-epm --disable-odk --with-java=no --disable-cups --with-system-libs --with-system-python --disable-mozilla;
 }
 
 configurePhase=configurePhase;
