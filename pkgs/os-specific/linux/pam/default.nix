0fb522de9584a1708bbd54ae1fc6a1b5a72c6fdc
Armijn Hemel <armijn@gpl-violations.org>
2005-10-25 17:57:05 +0000

    don't use /bin/ls, but rely on the build environment to supply ls
    svn path=/nixpkgs/trunk/; revision=4131

--- pkgs/os-specific/linux/pam/default.nix
+++ pkgs/os-specific/linux/pam/default.nix
@@ -7,6 +7,6 @@ stdenv.mkDerivation {
     url = http://www.nl.kernel.org/pub/linux/libs/pam/pre/library/Linux-PAM-0.80.tar.bz2;
     md5 = "ccff87fe639efdfc22b1ba4a0f08ec57";
   };
-  patches = [./pam-pwd.patch ./pam-cracklib.patch];
+  patches = [./pam-pwd.patch ./pam-cracklib.patch ./pam-modules.patch];
   inherit cracklib;
 }
