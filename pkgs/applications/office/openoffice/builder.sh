9cf98d31f77a0acb1c8488536d2ae70bceb01f9f
Armijn Hemel <armijn@gpl-violations.org>
2005-10-26 20:42:12 +0000

    we need getopt too
    svn path=/nixpkgs/trunk/; revision=4148

--- pkgs/applications/office/openoffice/builder.sh
+++ pkgs/applications/office/openoffice/builder.sh
@@ -3,8 +3,17 @@ source $stdenv/setup
 configurePhase() {
   cd config_office/;
   ./configure --disable-epm --disable-odk --with-java=no --disable-cups --with-system-libs --with-system-python --disable-mozilla --without-nas --disable-pasf --disable-gnome-vfs;
+  cd ..
 }
 
-configurePhase=configurePhase;
+configurePhase=configurePhase
+
+buildPhase() {
+  ./bootstrap
+  source LinuxIntelEnv.Set.sh 
+  dmake
+}
+
+buildPhase=buildPhase
 
 genericBuild
