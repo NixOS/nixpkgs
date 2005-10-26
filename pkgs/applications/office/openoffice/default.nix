617cbc2b12fe3727badf6733445fbb452903bb78
Armijn Hemel <armijn@gpl-violations.org>
2005-10-26 00:08:36 +0000

    OOo needs Perl too...
    svn path=/nixpkgs/trunk/; revision=4137

--- pkgs/applications/office/openoffice/default.nix
+++ pkgs/applications/office/openoffice/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, pam, python, tcsh, libxslt}:
+{stdenv, fetchurl, pam, python, tcsh, libxslt, perl}:
 
 stdenv.mkDerivation {
   name = "openoffice.org-2.0.0";
@@ -7,5 +7,5 @@ stdenv.mkDerivation {
     url = ftp://ftp.nluug.nl/pub/office/openoffice/stable/2.0.0/OOo_2.0.0_src.tar.gz;
     md5 = "a68933afc2bf432d11b2043ac99ba0aa";
   };
-  buildInputs = [pam python tcsh libxslt];
+  buildInputs = [pam python tcsh libxslt perl];
 }
