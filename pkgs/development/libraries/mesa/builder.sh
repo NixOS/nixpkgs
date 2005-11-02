6928e1cc2dd93df130488c3502d2cd927e67bd14
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-11-02 20:58:38 +0000

    * Mesa updated to 6.4.
    svn path=/nixpkgs/trunk/; revision=4196

--- pkgs/development/libraries/mesa/builder.sh
+++ pkgs/development/libraries/mesa/builder.sh
@@ -4,11 +4,8 @@ makeFlags="linux-x86"
 
 installPhase=installPhase
 installPhase() {
-    mkdir $out
-    mkdir $out/lib
-    cp -pvd lib/* $out/lib
-    mkdir $out/include
-    cp -rv include/GL $out/include
+    ensureDir $out
+    ./bin/installmesa $out
 }
 
 genericBuild
--- pkgs/development/libraries/mesa/default.nix
+++ pkgs/development/libraries/mesa/default.nix
@@ -3,16 +3,20 @@
 assert stdenv.system == "i686-linux";
 
 stdenv.mkDerivation {
-  name = "mesa-6.0.1";
+  name = "mesa-6.4";
   srcs = [
     (fetchurl {
-      url = http://nix.cs.uu.nl/dist/tarballs/MesaLib-6.0.1.tar.bz2;
-      md5 = "b7f14088c5c2f14490d2739a91102112";
+      url = http://surfnet.dl.sourceforge.net/sourceforge/mesa3d/MesaLib-6.4.tar.bz2;
+      md5 = "85a84e47a3f718f752f306b9e0954ef6";
+    })
+/*    (fetchurl {
+      url = http://surfnet.dl.sourceforge.net/sourceforge/mesa3d/MesaGLUT-6.4.tar.bz2;
+      md5 = "1a8c4d4fc699233f5fdb902b8753099e";
     })
     (fetchurl {
-      url = http://nix.cs.uu.nl/dist/tarballs/MesaDemos-6.0.1.tar.bz2;
-      md5 = "dd6aadfd9ca8e1cfa90c6ee492bc6f43";
-    })
+      url = http://surfnet.dl.sourceforge.net/sourceforge/mesa3d/MesaDemos-6.4.tar.bz2;
+      md5 = "1a8c4d4fc699233f5fdb902b8753099e";
+    }) */
   ];
   builder = ./builder.sh;
   buildInputs = [xlibs.xlibs xlibs.libXmu xlibs.libXi];
