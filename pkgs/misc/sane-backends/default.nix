c2732d31c756138d9f62ca5fe5e9d5542918c415
Armijn Hemel <armijn@gpl-violations.org>
2005-11-03 17:20:38 +0000

    add sane-backends
    svn path=/nixpkgs/trunk/; revision=4208

---
+++ pkgs/misc/sane-backends/default.nix
@@ -0,0 +1,9 @@
+{stdenv, fetchurl}:
+
+stdenv.mkDerivation {
+  name = "sane-backends-1.0.16";
+  src = fetchurl {
+    url = ftp://ftp3.sane-project.org/pub/sane/sane-backends-1.0.16/sane-backends-1.0.16.tar.gz;
+    md5 = "bec9b9262246316b4ebfe2bc7451aa28";
+  };
+}
