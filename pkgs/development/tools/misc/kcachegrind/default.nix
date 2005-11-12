d7cd4fbb081bddc8ba9563bb9b92f65d509f7d22
Martin Bravenboer <martin.bravenboer@logicblox.com>
2005-11-12 17:09:29 +0000

    Added kcachegrind
    svn path=/nixpkgs/trunk/; revision=4265

--- pkgs/development/tools/misc/kcachegrind/default.nix
+++ pkgs/development/tools/misc/kcachegrind/default.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchurl, kdelibs}:
 
 stdenv.mkDerivation {
-  name = "/kcachegrind-0.4.6";
+  name = "kcachegrind-0.4.6";
   src = fetchurl {
     url = http://kcachegrind.sourceforge.net/kcachegrind-0.4.6.tar.gz;
     md5 = "4ed60028dcefd6bf626635d5f2f50273";
