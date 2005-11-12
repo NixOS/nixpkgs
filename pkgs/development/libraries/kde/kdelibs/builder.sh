770261b78efde19c753ed9fa31a56d9332e83a07
Armijn Hemel <armijn@gpl-violations.org>
2005-11-12 22:18:08 +0000

    fix passing of extra includes
    svn path=/nixpkgs/trunk/; revision=4276

--- pkgs/development/libraries/kde/kdelibs/builder.sh
+++ pkgs/development/libraries/kde/kdelibs/builder.sh
@@ -3,7 +3,7 @@ source $stdenv/setup
 configureFlags="\
   --without-arts \
   --with-ssl-dir=$openssl \
-  --with-extra-includes=$libjpeg \
+  --with-extra-includes=$libjpeg/include \
   --x-includes=$libX11/include \
   --x-libraries=$libX11/lib"
 
