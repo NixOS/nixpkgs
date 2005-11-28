0cf7a938271ffa927ac9520f098e647f20fa7a7c
Armijn Hemel <armijn@gpl-violations.org>
2005-11-28 22:03:13 +0000

    add kernel headers for ARM
    svn path=/nixpkgs/trunk/; revision=4316

---
+++ pkgs/os-specific/linux/kernel-headers-arm/builder.sh
@@ -0,0 +1,24 @@
+. $stdenv/setup
+
+
+buildPhase() {
+    make include/linux/version.h
+}
+
+buildPhase=buildPhase
+
+
+installPhase() {
+    mkdir $out
+    mkdir $out/include
+    #cd $out/include
+    #ln -s asm-arm asm
+    make include/asm ARCH=arm
+    cp -prvd include/linux include/asm include/asm-arm include/asm-generic $out/include
+    echo -n > $out/include/linux/autoconf.h
+}
+
+installPhase=installPhase
+
+
+genericBuild
---
+++ pkgs/os-specific/linux/kernel-headers-arm/default.nix
@@ -0,0 +1,12 @@
+{stdenv, fetchurl}:
+
+assert stdenv.system == "i686-linux";
+
+stdenv.mkDerivation {
+  name = "linux-headers-2.6.13.4-arm";
+  builder = ./builder.sh;
+  src = fetchurl {
+    url = http://www.kernel.org/pub/linux/kernel/v2.6/linux-2.6.13.4.tar.bz2;
+    md5 = "94768d7eef90a9d8174639b2a7d3f58d";
+  };
+}
