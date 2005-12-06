20426f9825416d88f4b57dc97f314d81429e8b19
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-12-06 21:35:07 +0000

    * Latest QT 3.x.
    svn path=/nixpkgs/trunk/; revision=4345

--- pkgs/development/libraries/qt-3/default.nix
+++ pkgs/development/libraries/qt-3/default.nix
@@ -11,14 +11,14 @@ assert xrenderSupport -> xftSupport && libXft != null;
 assert mysqlSupport -> mysql != null;
 
 stdenv.mkDerivation {
-  name = "qt-3.3.3";
+  name = "qt-3.3.5";
 
   builder = ./builder.sh;
   substitute = ../../../build-support/substitute/substitute.sh;
   hook = ./setup-hook.sh;  
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/qt-x11-free-3.3.3.tar.bz2;
-    md5 = "3e0a0c8429b0a974b39b5f535ddff01c";
+    url = http://wftp.tu-chemnitz.de/pub/Qt/qt/source/qt-x11-free-3.3.5.tar.bz2;
+    md5 = "05d04688c0c0230ed54e89102d689ca4";
   };
 
   buildInputs = [x11 libXft libXrender zlib libjpeg libpng which];
