47dcd9819fc9148c653aa8d7d40d75fa8e0bb863
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-12-12 16:20:49 +0000

    * Updated Berkeley DB to 4.4.16.
    svn path=/nixpkgs/trunk/; revision=4354

--- pkgs/development/libraries/db4/default.nix
+++ pkgs/development/libraries/db4/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, cxxSupport ? true, compat185 ? true}:
 
 stdenv.mkDerivation {
-  name = "db4-4.3.28";
+  name = "db4-4.4.16";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://downloads.sleepycat.com/db-4.3.28.NC.tar.gz;
-    md5 = "6efcf5f4f30c7170f68d8952739771cd";
+    url = http://downloads.sleepycat.com/db-4.4.16.NC.tar.gz;
+    md5 = "1466026e67b5c3eb60c8c16b7f472c17";
   };
   configureFlags = [
     (if cxxSupport then "--enable-cxx" else "--disable-cxx")
