ec790cdf85f1535ea1a119b184ad8ef6bda40e5c
Armijn Hemel <armijn@gpl-violations.org>
2005-12-13 14:28:55 +0000

    add a seperate dir for SSH-keys. Remove the /etc-secret dir, we probably won't be using it anyway...
    svn path=/nixu/trunk/; revision=4362

--- fill-disk.sh
+++ fill-disk.sh
@@ -140,8 +140,8 @@ make_dir 00755 /boot
 make_dir 00755 /dev
 make_dir 00755 /dev/pts
 make_dir 00755 /etc # global non-constant configuration
+make_dir 00755 /etc/ssh
 make_dir 00755 /etc/sysconfig
-make_dir 00755 /etc-secret
 make_dir 00755 /home
 make_dir 00755 /lib
 make_dir 00755 /lib/modules
