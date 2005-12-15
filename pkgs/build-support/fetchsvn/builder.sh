07687a8514451f0934485e4658a078a2c6f96b89
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-12-15 17:21:20 +0000

    * Pipe the "p" character into Subversion to force it to accept the   server's certificate.  This is perfectly safe: we don't care whether   the server is being spoofed --- only the cryptographic hash of the   output matters.
    svn path=/nixpkgs/trunk/; revision=4377

--- pkgs/build-support/fetchsvn/builder.sh
+++ pkgs/build-support/fetchsvn/builder.sh
@@ -7,7 +7,11 @@ echo $prefetch
 if test -e "$prefetch"; then
     mv $prefetch $out
 else
-    svn export -r "$rev" "$url" $out
+    # Pipe the "p" character into Subversion to force it to accept the
+    # server's certificate.  This is perfectly safe: we don't care
+    # whether the server is being spoofed --- only the cryptographic
+    # hash of the output matters.
+    echo 'p' | svn export -r "$rev" "$url" $out
 fi
 
 actual=$(nix-hash $out)
