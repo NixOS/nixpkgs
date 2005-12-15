784605f8de509918b19905788e856b55ed88cb84
Eelco Dolstra <eelco.dolstra@logicblox.com>
2005-12-15 17:45:40 +0000

    * Bittorrent 4.2.1.
    svn path=/nixpkgs/trunk/; revision=4378

--- pkgs/tools/networking/bittorrent/default.nix
+++ pkgs/tools/networking/bittorrent/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, python, pygtk, makeWrapper}:
 
 stdenv.mkDerivation {
-  name = "bittorrent-4.0.1";
+  name = "bittorrent-4.2.1";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://www.bittorrent.com/dl/BitTorrent-4.0.1.tar.gz;
-    md5 = "e890d856d43b3d0af14b28714bc5801a";
+    url = http://www.bittorrent.com/dl/BitTorrent-4.2.1.tar.gz;
+    md5 = "0deb2e083e95206a9e601ff6ca35b826";
   };
   buildInputs = [python pygtk];
   inherit python pygtk makeWrapper;
