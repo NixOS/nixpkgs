41108750ba8bd6279bcdbed12d4cb8da3baefc71
Armijn Hemel <armijn@gpl-violations.org>
2005-12-19 17:17:06 +0000

    add beecrypt
    svn path=/nixpkgs/trunk/; revision=4401

---
+++ pkgs/development/libraries/beecrypt/default.nix
@@ -0,0 +1,11 @@
+{stdenv, fetchurl, m4}:
+
+stdenv.mkDerivation {
+  name = "beecrypt-4.1.2";
+  src = fetchurl {
+    url = http://surfnet.dl.sourceforge.net/sourceforge/beecrypt/beecrypt-4.1.2.tar.gz;
+    md5 = "820d26437843ab0a6a8a5151a73a657c";
+  };
+  buildInputs = [m4];
+  configureFlags = "--disable-optimized --enable-static";
+}
