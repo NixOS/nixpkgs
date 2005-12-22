57067bba9e7ad60da73d8b68a2ff026cf981869d
Armijn Hemel <armijn@gpl-violations.org>
2005-12-22 18:10:47 +0000

    we depend on coreutils (chmod, etc.)
    svn path=/nixpkgs/trunk/; revision=4410

--- pkgs/servers/ssh-script/builder.sh
+++ pkgs/servers/ssh-script/builder.sh
@@ -5,4 +5,5 @@ ensureDir $out
 sed -e "s^@bash\@^$bash^g" \
     -e "s^@sshd\@^$ssh^g" \
     -e "s^@initscripts\@^$initscripts^g" \
+    -e "s^@coreutils\@^$coreutils^g" \
     < $script > $out/control
--- pkgs/servers/ssh-script/default.nix
+++ pkgs/servers/ssh-script/default.nix
@@ -1,9 +1,9 @@
-{stdenv, ssh, bash, initscripts, key ? null}:
+{stdenv, ssh, bash, initscripts, coreutils, key ? null}:
 
 stdenv.mkDerivation {
   name = "ssh-script-0.0.1";
   server = "ssh";
   builder = ./builder.sh ;
-  inherit bash ssh initscripts;
+  inherit bash ssh initscripts coreutils;
   script = [./sshd];
 }
