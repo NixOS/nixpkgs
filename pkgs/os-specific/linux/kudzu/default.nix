7e6f127abb03421f192d0bf40de6634d1d3457d5
Armijn Hemel <armijn@gpl-violations.org>
2005-12-23 18:56:33 +0000

    fix python includes
    svn path=/nixpkgs/trunk/; revision=4419

--- pkgs/os-specific/linux/kudzu/default.nix
+++ pkgs/os-specific/linux/kudzu/default.nix
@@ -2,10 +2,12 @@
 
 stdenv.mkDerivation {
   name = "kudzu-1.2.16";
+  builder = ./builder.sh;
   src = fetchurl {
     url = http://losser.labs.cs.uu.nl/~armijn/.nix/kudzu-1.2.16.tar.gz;
     md5 = "5fc786dd558064fd9c9cb3e5be10e799";
   };
   buildInputs = [pciutils python popt gettext];
+  inherit python;
   patches = [./kudzu-python.patch];
 }
