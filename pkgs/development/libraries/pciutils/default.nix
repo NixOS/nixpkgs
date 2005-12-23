9a3162f1ff27e1d31887812ccc2478b07ef8e66b
Armijn Hemel <armijn@gpl-violations.org>
2005-12-23 15:26:05 +0000

    rip a patch from the Fedora RPM. This is needed to let kudzu compile...
    svn path=/nixpkgs/trunk/; revision=4417

--- pkgs/development/libraries/pciutils/default.nix
+++ pkgs/development/libraries/pciutils/default.nix
@@ -7,5 +7,5 @@ stdenv.mkDerivation {
     url = ftp://ftp.kernel.org/pub/software/utils/pciutils/pciutils-2.2.1.tar.bz2;
     md5 = "483a08dc864ec42497ad95310bb1a8ee";
   };
-  patches = [./pciutils-path.patch];
+  patches = [./pciutils-path.patch ./pciutils-devicetype.patch];
 }
