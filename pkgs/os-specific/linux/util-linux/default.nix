6e80370dfeb48acbf2f053c6cfdb18f2415be874
Armijn Hemel <armijn@gpl-violations.org>
2005-12-23 22:59:33 +0000

    update util-linux to 2.12r
    svn path=/nixpkgs/trunk/; revision=4424

--- pkgs/os-specific/linux/util-linux/default.nix
+++ pkgs/os-specific/linux/util-linux/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "util-linux-2.12q";
+  name = "util-linux-2.12r";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/util-linux-2.12q.tar.bz2;
-    md5 = "54320aa1abbce00c0dc030e2c3afe5d7";
+    url = http://www.kernel.org/pub/linux/utils/util-linux/util-linux-2.12r.tar.bz2;
+    md5 = "af9d9e03038481fbf79ea3ac33f116f9";
   };
   patches = [./MCONFIG.patch];
 }
