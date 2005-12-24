53c0ae3af3d3e9f613f8eae8ef201bd3c4044be5
Armijn Hemel <armijn@gpl-violations.org>
2005-12-24 00:25:18 +0000

    add a static version of module-init-tools for NixOS
    svn path=/nixpkgs/trunk/; revision=4427

---
+++ pkgs/os-specific/linux/module-init-tools-static/builder.sh
@@ -0,0 +1,5 @@
+source $stdenv/setup
+
+export LDFLAGS=-static
+
+genericBuild
---
+++ pkgs/os-specific/linux/module-init-tools-static/default.nix
@@ -0,0 +1,13 @@
+{stdenv, fetchurl}:
+
+stdenv.mkDerivation {
+  name = "module-init-tools-3.2.2";
+  builder = ./builder.sh;
+  src = fetchurl {
+    url = ftp://ftp.kernel.org/pub/linux/utils/kernel/module-init-tools/module-init-tools-3.2.2.tar.bz2;
+    md5 = "a1ad0a09d3231673f70d631f3f5040e9";
+  };
+  patches = [./module-init-tools-static.patch];
+}
+
+
