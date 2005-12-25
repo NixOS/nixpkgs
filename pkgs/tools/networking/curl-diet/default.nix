c067f85ab12cfbcc139a9755b88fa84b9ba59e9e
Armijn Hemel <armijn@gpl-violations.org>
2005-12-25 00:49:21 +0000

    upgrade the statically linked curl to 7.15.1 from 7.14.1.
    It would be good to use the output from this Nix expression for the initial
    static stdenv.  Right now it is present, but we have not on record how it
    exactly was compiled. If we use this package, we do know that in the future.
    
    However, if we update this package it will cause a *MASSIVE* rebuild, namely
    everything.
    
    I won't be doing that...for now...even though some downtime for some people
    would be a very good idea ;)
    
    svn path=/nixpkgs/trunk/; revision=4433

--- pkgs/tools/networking/curl-diet/default.nix
+++ pkgs/tools/networking/curl-diet/default.nix
@@ -3,14 +3,13 @@
 assert sslSupport -> openssl != null;
 
 stdenv.mkDerivation {
-  name = "curl-7.14.1";
+  name = "curl-7.15.1";
   src = fetchurl {
-    url = http://curl.haxx.se/download/curl-7.14.1.tar.bz2;
-    md5 = "8b8723f3c0e7acfb30c5215e6cffde32";
+    url = http://curl.haxx.se/download/curl-7.15.1.tar.bz2;
+    md5 = "d330d48580bfade58c82d4f295f171f0";
   };
   buildInputs = [zlib (if sslSupport then openssl else null)];
   patches = [./configure-cxxcpp.patch];
-  #configureFlags = (if sslSupport then "--with-ssl" else "--without-ssl");
   configureFlags = "--enable-shared=no" + (if sslSupport then "--with-ssl" else "--without-ssl");
   NIX_GCC = dietgcc;
   NIX_GLIBC_FLAGS_SET=1;
