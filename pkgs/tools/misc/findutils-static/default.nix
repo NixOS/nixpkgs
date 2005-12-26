40c9f1a764081de22fb32908700fd3b85aa75d6e
Armijn Hemel <armijn@gpl-violations.org>
2005-12-26 17:13:37 +0000

    add a statically compiled version of findutils for the inital stdenv and NixOS
    svn path=/nixpkgs/trunk/; revision=4441

---
+++ pkgs/tools/misc/findutils-static/default.nix
@@ -0,0 +1,13 @@
+{stdenv, fetchurl, coreutils}:
+
+stdenv.mkDerivation {
+  name = "findutils-4.2.26";
+  src = fetchurl {
+    url = http://ftp.gnu.org/pub/gnu/findutils/findutils-4.2.26.tar.gz;
+    md5 = "9ac4e62937b1fdc4eb643d1d4bf117d3";
+  };
+  buildInputs = [coreutils];
+  patches = [./findutils-path.patch];
+  LDFLAGS="-static";
+  configureFlags="--disable-nls --without-included-regex";
+}
