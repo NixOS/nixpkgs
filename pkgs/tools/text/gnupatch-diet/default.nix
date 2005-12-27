cecd284522a102d2b7b44e8e927076ac27c2ec13
Armijn Hemel <armijn@gpl-violations.org>
2005-12-27 02:47:59 +0000

    add static version for initial stdenv...and maybe NixOS...
    svn path=/nixpkgs/trunk/; revision=4443

---
+++ pkgs/tools/text/gnupatch-diet/default.nix
@@ -0,0 +1,10 @@
+{stdenv, fetchurl, dietgcc}:
+
+stdenv.mkDerivation {
+  name = "gnupatch-2.5.4";
+  src = fetchurl {
+    url = http://nix.cs.uu.nl/dist/tarballs/patch-2.5.4.tar.gz;
+    md5 = "ee5ae84d115f051d87fcaaef3b4ae782";
+  };
+  NIX_GCC=dietgcc;
+}
