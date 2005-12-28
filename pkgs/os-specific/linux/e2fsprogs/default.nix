0ee2f324ccd7f9d23a8592b3c3fe262004ff7c3c
Armijn Hemel <armijn@gpl-violations.org>
2005-12-28 02:53:56 +0000

    update to 1.38. Weird: this version doesn't want to build with dietlibc, should find out why...
    svn path=/nixpkgs/trunk/; revision=4452

--- pkgs/os-specific/linux/e2fsprogs/default.nix
+++ pkgs/os-specific/linux/e2fsprogs/default.nix
@@ -1,12 +1,15 @@
 {stdenv, fetchurl, gettext}:
 
 stdenv.mkDerivation {
-  name = "e2fsprogs-1.36";
+  name = "e2fsprogs-1.38";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/e2fsprogs-1.36.tar.gz;
-    md5 = "1804ee96b76e5e7113fe3cecd6fe582b";
+    #url = http://nix.cs.uu.nl/dist/tarballs/e2fsprogs-1.36.tar.gz;
+    url = http://surfnet.dl.sourceforge.net/sourceforge/e2fsprogs/e2fsprogs-1.38.tar.gz;
+    md5 = "d774d4412bfb80d12cf3a4fdfd59de5a";
   };
   configureFlags = "--enable-dynamic-e2fsck --enable-elf-shlibs";
   buildInputs = [gettext];
 }
+
+
