cca5dc0b57fe39d0ad494be69e7a900ad8e7aeb3
Armijn Hemel <armijn@gpl-violations.org>
2005-12-31 16:28:49 +0000

    add sparc-linux
    svn path=/nixpkgs/trunk/; revision=4471

--- pkgs/os-specific/linux/kernel-headers-cross/builder.sh
+++ pkgs/os-specific/linux/kernel-headers-cross/builder.sh
@@ -15,9 +15,10 @@ installPhase() {
     #ln -s asm-arm asm
     if test $cross = "arm-linux"; then
        arch=arm
-    else if test $cross = "mips-linux"; then
+    elif test $cross = "mips-linux"; then
            arch=mips
-         fi
+    elif test $cross = "sparc-linux"; then
+           arch=sparc
     fi
     make include/asm ARCH=$arch
     cp -prvd include/linux include/asm include/asm-$arch include/asm-generic $out/include
