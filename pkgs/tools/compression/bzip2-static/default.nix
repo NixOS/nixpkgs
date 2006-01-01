dde6370c2a7d341b27b0b80806413f94ccaa96dc
Armijn Hemel <armijn@gpl-violations.org>
2006-01-01 13:34:35 +0000

    add a statically linked bzip2. This is the last of the tools we need for a fullystatic initial stdenv...
    svn path=/nixpkgs/trunk/; revision=4477

---
+++ pkgs/tools/compression/bzip2-static/default.nix
@@ -0,0 +1,11 @@
+{stdenv, fetchurl}:
+
+stdenv.mkDerivation {
+  name = "bzip2-1.0.3";
+  builder = ./builder.sh;
+  src = fetchurl {
+    url = http://www.bzip.org/1.0.3/bzip2-1.0.3.tar.gz;
+    md5 = "8a716bebecb6e647d2e8a29ea5d8447f";
+  };
+  patches = [./bzip2-1.0.3-static.patch];
+}
