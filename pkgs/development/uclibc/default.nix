4655f9a723078ba64f06095ee75e8a3b844b972d
Armijn Hemel <armijn@gpl-violations.org>
2006-01-01 01:55:26 +0000

    add configuration for powerpc. Due to some bug we use a post-0.9.28 snapshot. Scary ;)
    svn path=/nixpkgs/trunk/; revision=4475

--- pkgs/development/uclibc/default.nix
+++ pkgs/development/uclibc/default.nix
@@ -4,8 +4,10 @@ stdenv.mkDerivation {
   builder = ./builder.sh;
   name = "uClibc-0.9.28";
   src = fetchurl {
-    url = http://www.uclibc.org/downloads/uClibc-0.9.28.tar.bz2;
-    md5 = "1ada58d919a82561061e4741fb6abd29";
+    url = http://www.uclibc.org/downloads/snapshots/uClibc-20051001.tar.bz2;
+    md5 = "5442033ed956d506f9a810cf70dc3744";
+    #url = http://www.uclibc.org/downloads/uClibc-0.9.28.tar.bz2;
+    #md5 = "1ada58d919a82561061e4741fb6abd29";
   };
   config = if cross == "mips-linux"
              then ./config-mips-linux
@@ -13,6 +15,8 @@ stdenv.mkDerivation {
                   then ./config-arm-linux
                 else if cross == "sparc-linux"
                     then ./config-sparc-linux
+                else if cross == "powerpc-linux"
+                    then ./config-powerpc-linux
                       else "";
 
   inherit kernelHeadersCross;
