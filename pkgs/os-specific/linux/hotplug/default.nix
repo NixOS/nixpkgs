55068cef8a19a6a4d6bbe18a051b5d872f6cae4a
Armijn Hemel <armijn@gpl-violations.org>
2006-01-04 23:08:50 +0000

    we don't have /usr/bin/install on NixOS, so remove this path
    svn path=/nixpkgs/trunk/; revision=4489

--- pkgs/os-specific/linux/hotplug/default.nix
+++ pkgs/os-specific/linux/hotplug/default.nix
@@ -6,5 +6,5 @@ stdenv.mkDerivation {
     url = http://surfnet.dl.sourceforge.net/sourceforge/linux-hotplug/hotplug-2004_03_29.tar.gz;
     md5 = "167bd479a1ca30243c51ca088e0942b3";
   };
-  patches = [./hotplug-install-path.patch];
+  patches = [./hotplug-install-path.patch ./hotplug-install.patch];
 }
