d36b6a18988a3abd5916a417e60698d18d339d28
Armijn Hemel <armijn@gpl-violations.org>
2006-01-05 16:31:29 +0000

    patch hotplug so we won't need /bin/bash. I'm not sure how free of bashisms these scripts are, so using /bin/sh instead is no option...
    svn path=/nixpkgs/trunk/; revision=4492

---
+++ pkgs/os-specific/linux/hotplug/builder.sh
@@ -0,0 +1,19 @@
+source $stdenv/setup
+
+preBuild=preBuild
+
+preBuild() {
+  sed -e "s^@bash\@^$bash^g" \
+      < etc/hotplug/dasd.agent > etc/hotplug/dasd.agent.tmp
+  mv etc/hotplug/dasd.agent.tmp etc/hotplug/dasd.agent
+
+  sed -e "s^@bash\@^$bash^g" \
+      < etc/hotplug/tape.agent > etc/hotplug/tape.agent.tmp
+  mv etc/hotplug/tape.agent.tmp etc/hotplug/tape.agent
+
+  sed -e "s^@bash\@^$bash^g" \
+      < etc/hotplug.d/default/default.hotplug > etc/hotplug.d/default/default.hotplug.tmp
+  mv etc/hotplug.d/default/default.hotplug.tmp etc/hotplug.d/default/default.hotplug
+}
+
+genericBuild
--- pkgs/os-specific/linux/hotplug/default.nix
+++ pkgs/os-specific/linux/hotplug/default.nix
@@ -1,10 +1,12 @@
-{stdenv, fetchurl}:
+{stdenv, fetchurl, bash}:
 
 stdenv.mkDerivation {
   name = "hotplug-2004_03_29";
+  builder = ./builder.sh;
   src = fetchurl {
     url = http://surfnet.dl.sourceforge.net/sourceforge/linux-hotplug/hotplug-2004_03_29.tar.gz;
     md5 = "167bd479a1ca30243c51ca088e0942b3";
   };
-  patches = [./hotplug-install-path.patch ./hotplug-install.patch];
+  patches = [./hotplug-install-path.patch ./hotplug-install.patch ./hotplug-2004_03_29-bash.patch];
+  inherit bash;
 }
