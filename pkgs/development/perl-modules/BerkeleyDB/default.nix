b9854645d33efa48ed850dda1ae1540ec6b845e8
Rob Vermaas <rob.vermaas@gmail.com>
2006-01-08 15:10:23 +0000

    0.26 -> 0.27
    svn path=/nixpkgs/trunk/; revision=4517

--- pkgs/development/perl-modules/BerkeleyDB/default.nix
+++ pkgs/development/perl-modules/BerkeleyDB/default.nix
@@ -1,10 +1,10 @@
 {fetchurl, perl, db4}:
 
 import ../generic perl {
-  name = "BerkeleyDB-0.26";
+  name = "BerkeleyDB-0.27";
   src = fetchurl {
-    url = ftp://ftp.cs.uu.nl/mirror/CPAN/authors/id/P/PM/PMQS/BerkeleyDB-0.26.tar.gz;
-    md5 = "6e9882f4e4bac48b24079d082af30e6c";
+    url = ftp://ftp.cs.uu.nl/mirror/CPAN/authors/id/P/PM/PMQS/BerkeleyDB-0.27.tar.gz;
+    md5 = "43aa72c0c6941af0d656d749ad543e96";
   };
   perlPreHook = ./hook.sh;
   inherit db4;
