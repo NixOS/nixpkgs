54f4af6054e53acb7fc9282ceca0bd4ab5982705
Armijn Hemel <armijn@gpl-violations.org>
2006-01-16 17:30:56 +0000

    don't forget findutils
    svn path=/nixpkgs/trunk/; revision=4567

--- pkgs/os-specific/linux/kernelscripts/default.nix
+++ pkgs/os-specific/linux/kernelscripts/default.nix
@@ -1,9 +1,9 @@
-{ stdenv, coreutils, nix}:
+{ stdenv, coreutils, nix, findutils}:
 
 derivation {
   name = "kernelscripts";
   system = stdenv.system;
   builder = ./builder.sh;
   createModules = ./create-modules.sh;
-  inherit stdenv coreutils nix;
+  inherit stdenv coreutils nix findutils;
 }
