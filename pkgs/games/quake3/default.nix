691e719ef02138aa373689b88c9c8f5f5cabecd0
Armijn Hemel <armijn@gpl-violations.org>
2006-01-26 18:50:25 +0000

    fix the Nix expression, so it evaluates correctly...
    svn path=/nixpkgs/trunk/; revision=4599

--- pkgs/games/quake3/default.nix
+++ pkgs/games/quake3/default.nix
@@ -29,8 +29,8 @@ stdenv.mkDerivation {
   name = "quake3-icculus-1.33pre526";
   src = fetchurl {
     url = http://losser.st-lab.cs.uu.nl/~eelco/dist/quake3-icculus-r526.tar.bz2;
-    md5 = 63429347b918052c27cdb5c1d15939ad;
-  }
-  builder = ./builder.sh;
+    md5 = "63429347b918052c27cdb5c1d15939ad";
+  };
+  builder = "./builder.sh";
   buildInputs = [x11 SDL mesa openal];
 }
