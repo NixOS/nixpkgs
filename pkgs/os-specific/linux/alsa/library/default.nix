14fbcfa64c1dd3f0305d99089b3cda439c388b27
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-01-27 23:48:28 +0000

    * ALSA library updated to 1.0.10.
    svn path=/nixpkgs/trunk/; revision=4610

--- pkgs/os-specific/linux/alsa/library/default.nix
+++ pkgs/os-specific/linux/alsa/library/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "alsa-lib-1.0.9";
+  name = "alsa-lib-1.0.10";
   src = fetchurl {
-    url = ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.0.9.tar.bz2;
-    md5 = "114af3304619920ffe2b147b760700b9";
+    url = ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.0.10.tar.bz2;
+    md5 = "b1a4e15c9ff81798507de470a92fcc43";
   };
 }
