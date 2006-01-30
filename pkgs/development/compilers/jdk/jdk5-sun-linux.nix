accbe1d1a74acbf2d56a55b141bb566068064994
Armijn Hemel <armijn@gpl-violations.org>
2006-01-30 14:25:19 +0000

    update to jdk-1_5_0_06-linux-i586.bin
    svn path=/nixpkgs/trunk/; revision=4618

--- pkgs/development/compilers/jdk/jdk5-sun-linux.nix
+++ pkgs/development/compilers/jdk/jdk5-sun-linux.nix
@@ -1,6 +1,6 @@
 /**
  * This Nix expression requires the user to download the jdk
- * distribution to /tmp. Please obtain jdk-1_5_0_05-linux-i586.bin
+ * distribution to /tmp. Please obtain jdk-1_5_0_06-linux-i586.bin
  * from java.sun.com by hand and place it in /tmp. Blame Sun, not me.
  *
  * Note that this is not necessary if someone has already pushed a
@@ -19,10 +19,10 @@ assert swingSupport -> libX11 != null && libXext != null;
 (stdenv.mkDerivation {
   name = "jdk-1.5.0";
   builder = ./builder.sh;
-  filename = "jdk-1_5_0_05";
-  dirname = "jdk1.5.0_05";
-  pathname = "/tmp/jdk-1_5_0_05-linux-i586.bin";
-  md5 = "2f83bf2a38fff1f8ac51b02ec7391ca3";
+  filename = "jdk-1_5_0_06";
+  dirname = "jdk1.5.0_06";
+  pathname = "/tmp/jdk-1_5_0_06-linux-i586.bin";
+  md5 = "3cdad4a383b93680f02f6f06198c2227";
   buildInputs = [unzip];
   libraries =
     (if swingSupport then [libX11 libXext] else []);
