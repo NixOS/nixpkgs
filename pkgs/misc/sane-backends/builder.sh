9cb897da18dc4ce4f4fe9309f35f8074463faaf7
Armijn Hemel <armijn@gpl-violations.org>
2006-01-30 15:29:26 +0000

    update to 1.0.17, put some stuff in place for hotplugging support...
    svn path=/nixpkgs/trunk/; revision=4620

---
+++ pkgs/misc/sane-backends/builder.sh
@@ -0,0 +1,3 @@
+source $stdenv/setup
+
+genericBuild
--- pkgs/misc/sane-backends/default.nix
+++ pkgs/misc/sane-backends/default.nix
@@ -1,9 +1,14 @@
-{stdenv, fetchurl}:
+{stdenv, fetchurl, hotplug ? null, hotplugSupport ? false}:
+
+assert hotplugSupport -> hotplug != null;
+assert hotplugSupport -> stdenv.system == "i686-linux";
 
 stdenv.mkDerivation {
-  name = "sane-backends-1.0.16";
+  name = "sane-backends-1.0.17";
+  builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp3.sane-project.org/pub/sane/old-versions/sane-backends-1.0.16/sane-backends-1.0.16.tar.gz;
-    md5 = "bec9b9262246316b4ebfe2bc7451aa28";
+    url = ftp://ftp3.sane-project.org/pub/sane/sane-backends-1.0.17/sane-backends-1.0.17.tar.gz;
+    md5 = "b51c10da8a81a04e1bae88c9e6556df2";
   };
+  inherit (if hotplugSupport then hotplug else null);
 }
