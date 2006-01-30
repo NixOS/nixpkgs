ce92a4d1b10dc766a7916c33d97e580195f5f623
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-01-30 21:59:28 +0000

    * NVidia support.
    svn path=/nixpkgs/trunk/; revision=4628

--- pkgs/os-specific/linux/opengl/xorg-sys/builder.sh
+++ pkgs/os-specific/linux/opengl/xorg-sys/builder.sh
@@ -3,6 +3,8 @@ source $stdenv/setup
 ensureDir $out/lib
 
 ln -s /usr/lib/libGL.so.1 $out/lib/
+ln -s /usr/lib/libGLcore.so.1 $out/lib/
+ln -s /usr/lib/libnvidia-tls.so.1 $out/lib/
 
 for i in $neededLibs; do
     ln -s $i/lib/*.so* $out/lib/
