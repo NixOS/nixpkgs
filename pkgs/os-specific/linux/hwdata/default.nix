37d1db32f08f421a0e60ba666686b8c3c0619496
Armijn Hemel <armijn@gpl-violations.org>
2006-01-30 15:42:01 +0000

    upgrade to 0.172
    svn path=/nixpkgs/trunk/; revision=4621

--- pkgs/os-specific/linux/hwdata/default.nix
+++ pkgs/os-specific/linux/hwdata/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "hwdata-0.159";
+  name = "hwdata-0.172";
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/os/Linux/distr/debian/pool/main/h/hwdata/hwdata_0.159.orig.tar.gz;
-    md5 = "5756e42e70955b15dc62cfcdbaa1c885";
+    url = ftp://ftp.nluug.nl/pub/os/Linux/distr/debian/pool/main/h/hwdata/hwdata_0.172.orig.tar.gz;
+    md5 = "1c6b7f4dfe489f881702176c5f8e5a2e";
   };
 }
