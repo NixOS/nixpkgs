3ce62c7a5efd38a3240fdc90c7e018a2eb72202f
Armijn Hemel <armijn@gpl-violations.org>
2006-01-31 12:59:06 +0000

    add hotplugging stuff conditionally
    svn path=/nixpkgs/trunk/; revision=4633

--- pkgs/misc/sane-backends/builder.sh
+++ pkgs/misc/sane-backends/builder.sh
@@ -1,3 +1,12 @@
 source $stdenv/setup
 
+postInstall() {
+  if test $hotplugSupport = "1" ; then
+    ensureDir $out/etc/hotplug/usb/
+    cp tools/hotplug/* $out/etc/hotplug/usb/
+  fi
+}
+
+postInstall=postInstall
+
 genericBuild
