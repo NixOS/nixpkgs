13398fe348ec313b2ccc279b96e8c3c8e34f2187
Armijn Hemel <armijn@gpl-violations.org>
2006-01-31 13:01:58 +0000

    remove some assertions
    svn path=/nixpkgs/trunk/; revision=4634

--- pkgs/misc/sane-backends/default.nix
+++ pkgs/misc/sane-backends/default.nix
@@ -1,14 +1,13 @@
-{stdenv, fetchurl, hotplug ? null, hotplugSupport ? false}:
+{stdenv, fetchurl, hotplugSupport ? false}:
 
-assert hotplugSupport -> hotplug != null;
 assert hotplugSupport -> stdenv.system == "i686-linux";
 
 stdenv.mkDerivation {
   name = "sane-backends-1.0.17";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/sane-backends-1.0.17.tar.gz;
+    url = ftp://ftp3.sane-project.org/pub/sane/sane-backends-1.0.17/sane-backends-1.0.17.tar.gz;
     md5 = "b51c10da8a81a04e1bae88c9e6556df2";
   };
-  inherit (if hotplugSupport then hotplug else null);
+  inherit hotplugSupport;
 }
