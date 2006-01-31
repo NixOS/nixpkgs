72b0c9444f279ae1b967e1d0951aa524c8a19b89
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-01-31 16:40:21 +0000

    * Doh!
    svn path=/nixpkgs/trunk/; revision=4647

--- pkgs/development/libraries/java/jakarta-regexp/java-bin-builder.sh
+++ pkgs/development/libraries/java/jakarta-regexp/java-bin-builder.sh
@@ -1,6 +1,6 @@
 source $stdenv/setup
 
 tar zxvf $src
-cd jakarta-regexp-1.3
+cd jakarta-regexp-1.4
 mkdir -p $out/share/java/
-cp jakarta-regexp-1.3.jar $out/share/java/
+cp jakarta-regexp-1.4.jar $out/share/java/
