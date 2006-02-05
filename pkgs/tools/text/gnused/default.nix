5472ca11e8efe22ffe380e1e78aa9aaae4498b6b
Martin Bravenboer <martin.bravenboer@logicblox.com>
2006-02-05 01:55:03 +0000

    Revert sed update
    svn path=/nixpkgs/trunk/; revision=4723

--- pkgs/tools/text/gnused/default.nix
+++ pkgs/tools/text/gnused/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "gnused-4.1.5";
+  name = "gnused-4.1.4";
   src = fetchurl {
-    url = ftp://ftp.gnu.org/gnu/sed/sed-4.1.5.tar.gz;
-    md5 = "7a1cbbbb3341287308e140bd4834c3ba";
+    url = http://nix.cs.uu.nl/dist/tarballs/sed-4.1.4.tar.gz;
+    md5 = "2a62ceadcb571d2dac006f81df5ddb48";
   };
 }
