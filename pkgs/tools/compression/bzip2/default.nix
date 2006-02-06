e12399051a1e2bdb1a268b415a7590cb2a0b32a6
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-02-06 12:44:34 +0000

    * Grmbl.
    svn path=/nixpkgs/trunk/; revision=4738

--- pkgs/tools/compression/bzip2/default.nix
+++ pkgs/tools/compression/bzip2/default.nix
@@ -2,7 +2,10 @@
 
 stdenv.mkDerivation {
   name = "bzip2-1.0.3";
-  builder = if stdenv.system == "powerpc-darwin" then ./builder-darwin.sh else ./builder.sh;
+  builder =
+    if stdenv.system == "powerpc-darwin" then ./builder-darwin.sh
+    if stdenv.system == "i686-freebsd" then ./builder-freebsd.sh
+    else ./builder.sh;
   src = fetchurl {
     url = http://nix.cs.uu.nl/dist/tarballs/bzip2-1.0.3.tar.gz;
     md5 = "8a716bebecb6e647d2e8a29ea5d8447f";
