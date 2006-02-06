4d91fa9c45cd344cdc8ff8e10ac4a092305227ab
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-02-06 12:29:36 +0000

    * Quick hack to get bzip2 to build on Darwin.
    svn path=/nixpkgs/trunk/; revision=4737

--- pkgs/tools/compression/bzip2/default.nix
+++ pkgs/tools/compression/bzip2/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation {
   name = "bzip2-1.0.3";
-  builder = ./builder.sh;
+  builder = if stdenv.system == "powerpc-darwin" then ./builder-darwin.sh else ./builder.sh;
   src = fetchurl {
     url = http://nix.cs.uu.nl/dist/tarballs/bzip2-1.0.3.tar.gz;
     md5 = "8a716bebecb6e647d2e8a29ea5d8447f";
