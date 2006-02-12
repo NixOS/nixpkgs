5e4075947c92a2055c890c2d501b0230cd53571d
Martin Bravenboer <martin.bravenboer@logicblox.com>
2006-02-12 12:23:47 +0000

    Update of spoofax
    svn path=/nixpkgs/trunk/; revision=4795

--- pkgs/applications/editors/eclipse/plugins/spoofax/default.nix
+++ pkgs/applications/editors/eclipse/plugins/spoofax/default.nix
@@ -1,6 +1,6 @@
 {stdenv, fetchurl}:
 
 fetchurl {
-  url = http://nix.cs.uu.nl/dist/tarballs/org.spoofax.editor_0.3.10.jar;
-  md5 = "ff77853e750e19a9b8d380c17ea27f3d";
+  url = http://www.ii.uib.no/~karltk/spoofax/plugins/org.spoofax.editor_0.3.11.jar;
+  md5 = "c36941afcb0e538e16fafd594eae128e";
 }
