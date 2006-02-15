bbda87fc306fdbdf22e8fe934505bbb90d420e46
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-02-15 17:15:16 +0000

    * Fix the title bar icon.
    svn path=/nixpkgs/trunk/; revision=4818

--- pkgs/applications/networking/browsers/firefox/builder.sh
+++ pkgs/applications/networking/browsers/firefox/builder.sh
@@ -9,17 +9,22 @@ postInstall() {
     # This fixes starting Firefox when there already is a running
     # instance.  The `firefox' wrapper script actually expects to be
     # in the same directory as `run-mozilla.sh', apparently.
+    libDir=$(cd $out/lib && ls -d firefox-*)
+    test -n "$libDir"
     cd $out/bin
-    mv firefox ../lib/firefox-*/
-    ln -s ../lib/firefox-*/firefox .
+    mv firefox ../lib/$libDir/
+    ln -s ../lib/$libDir/firefox .
 
     # Register extensions etc.
     echo "running firefox -register..."
-    (cd $out/lib/firefox-* && LD_LIBRARY_PATH=. ./firefox-bin -register) || false
+    (cd $out/lib/$libDir && LD_LIBRARY_PATH=. ./firefox-bin -register) || false
 
     echo "running regxpcom..."
-    (cd $out/lib/firefox-* && LD_LIBRARY_PATH=. ./regxpcom) || false
-    
+    (cd $out/lib/$libDir && LD_LIBRARY_PATH=. ./regxpcom) || false
+
+    # Put the Firefox icon in the right place.
+    ensureDir $out/lib/$libDir/chrome/icons/default
+    ln -s ../../../icons/default.xpm  $out/lib/$libDir/chrome/icons/default/
 }
 
 genericBuild
