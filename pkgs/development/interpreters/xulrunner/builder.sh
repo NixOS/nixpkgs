2cbcd0bd7afd0875f39c330862b24469ee501dbf
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-02-15 09:33:54 +0000

    * "ensureDir ./lib/xulrunner-*" doesn't work because ensureDir quotes   its argument, causing a directory named xulrunner-* to be created.   As a result xulrunner couldn't find the extensions directory and   wouldn't start.
    svn path=/nixpkgs/trunk/; revision=4815

--- pkgs/development/interpreters/xulrunner/builder.sh
+++ pkgs/development/interpreters/xulrunner/builder.sh
@@ -7,17 +7,18 @@ postInstall() {
     # This fixes starting Firefox when there already is a running
     # instance.  The `firefox' wrapper script actually expects to be
     # in the same directory as `run-mozilla.sh', apparently.
+    libDir=$(cd $out/lib && ls xulrunner-*)
+    test -n "$libDir"
     cd $out/bin
-    mv xulrunner ../lib/xulrunner-*/
-    ln -s ../lib/xulrunner-*/xulrunner .
+    mv xulrunner ../lib/$libDir/
+    ln -s ../lib/$libDir/xulrunner .
 
     echo "running xulrunner --register-global..."
     $out/bin/xulrunner --register-global || true
 
     # xulrunner wants to create these directories at the first startup
-    cd $out
-    ensureDir ./lib/xulrunner-*/extensions
-    ensureDir ./lib/xulrunner-*/updates
+    ensureDir $out/lib/$libDir/extensions
+    ensureDir $out/lib/$libDir/updates
 }
 
 genericBuild
