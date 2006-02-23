90666e6eff1c30129dd926cc1824911cfba0480e
Merijn de Jonge <merijn.de.jonge@sorama.eu>
2006-02-23 09:30:40 +0000

    Updated ocaml to version 3.09.1
    svn path=/nixpkgs/trunk/; revision=4882

--- pkgs/development/compilers/ocaml/default.nix
+++ pkgs/development/compilers/ocaml/default.nix
@@ -4,8 +4,8 @@ stdenv.mkDerivation {
   name = "ocaml-3.08.0";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/ocaml-3.08.0.tar.gz;
-    md5 = "c6ef478362295c150101cdd2efcd38e0";
+    url = http://caml.inria.fr/pub/distrib/ocaml-3.09/ocaml-3.09.1.tar.gz;
+    md5 = "c73f4b093e27ba5bf13d62923f89befc";
   };
   configureScript = ./configure;
   dontAddPrefix = "True";
