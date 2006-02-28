f08241e5feb2bd6e81fc5556b85883c4fbf10af0
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-02-28 21:04:14 +0000

    * Fix for i915 after X.org upgrade.
    svn path=/nixpkgs/trunk/; revision=4934

--- pkgs/os-specific/linux/opengl/xorg-sys/builder.sh
+++ pkgs/os-specific/linux/opengl/xorg-sys/builder.sh
@@ -5,6 +5,7 @@ ensureDir $out/lib
 ln -s /usr/lib/libGL.so.1 $out/lib/
 ln -s /usr/lib/libGLcore.so.1 $out/lib/
 ln -s /usr/lib/libnvidia-tls.so.1 $out/lib/
+ln -s /usr/lib/libdrm.so.2 $out/lib/
 
 for i in $neededLibs; do
     ln -s $i/lib/*.so* $out/lib/
