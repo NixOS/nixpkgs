fe46e2a9b02fb8b02afb1dfdbfc0be846205deef
Armijn Hemel <armijn@gpl-violations.org>
2006-03-08 19:25:44 +0000

    d'oh, don't actually gzip it
    svn path=/nixpkgs/trunk/; revision=5008

--- pkgs/configs/etc/protocols/default.nix
+++ pkgs/configs/etc/protocols/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "etc-protocols-1.3";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://losser.labs.cs.uu.nl/~armijn/.nix/protocols.gz;
-    md5 = "3c7dcc6c6d30fadec21829574116171a";
+    url = http://losser.labs.cs.uu.nl/~armijn/.nix/protocols;
+    md5 = "6583a05c20dfb3784cd48fef2c59aa05";
   };
 }
--- pkgs/configs/etc/services/default.nix
+++ pkgs/configs/etc/services/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "etc-services-1.42";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://losser.labs.cs.uu.nl/~armijn/.nix/services.gz;
-    md5 = "093dbece9828e5e979081f3722858fb9";
+    url = http://losser.labs.cs.uu.nl/~armijn/.nix/services;
+    md5 = "dde31302b080df6ec5fcacee0d56dc90";
   };
 }
