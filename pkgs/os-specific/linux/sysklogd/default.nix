920ff11c96adb56b389be31ff0042e136c4f61c2
Armijn Hemel <armijn@gpl-violations.org>
2006-03-14 15:30:00 +0000

    fix the impurity error. However, this introduces another error. Apparently there is some difference between "install" on my FC3 machine and "install" in coreutils in Nix:
    building install_man
    install -o root -g root -m 644 sysklogd.8 /nix/store/x03w5xpzcya04b4ax47lic2ahnf479nz-sysklogd-1.4.1/usr/share/man/man8/sysklogd.8
    install: cannot change ownership of `/nix/store/x03w5xpzcya04b4ax47lic2ahnf479nz-sysklogd-1.4.1/usr/share/man/man8/sysklogd.8': Operation not permitted
    make: *** [install_man] Error 1
    
    
    svn path=/nixpkgs/trunk/; revision=5035

--- pkgs/os-specific/linux/sysklogd/default.nix
+++ pkgs/os-specific/linux/sysklogd/default.nix
@@ -7,5 +7,5 @@ stdenv.mkDerivation {
     url = http://nix.cs.uu.nl/dist/tarballs/sysklogd-1.4.1.tar.gz;
     md5 = "d214aa40beabf7bdb0c9b3c64432c774";
   };
-  patches = [./sysklogd-1.4.1-cvs-20050525.diff ./sysklogd-1.4.1-asm.patch];
+  patches = [./sysklogd-1.4.1-cvs-20050525-local.diff ./sysklogd-1.4.1-asm.patch];
 }
