ae736de422722bbe992fcc4e547ec2a7df2ad8d9
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-03-16 13:02:03 +0000

    * Valgrind 3.1.1.
    svn path=/nixpkgs/trunk/; revision=5047

--- pkgs/development/tools/misc/valgrind/default.nix
+++ pkgs/development/tools/misc/valgrind/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "valgrind-3.1.0";
+  name = "valgrind-3.1.1";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/valgrind-3.1.0.tar.bz2;
-    md5 = "d92156e9172dc6097e56c69ea9c88013";
+    url = http://www.valgrind.org/downloads/valgrind-3.1.1.tar.bz2;
+    md5 = "3bbfafedb59c19bf75977381ce2eb6d7";
   };
 }
