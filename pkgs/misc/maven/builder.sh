6d47926badb7ad978e0051592f20d72a163511cb
Roy van den Broek <rbroek@cs.uu.nl>
2006-04-02 17:12:52 +0000

    By default install the axis-maven and jira-soap plugin. This should probably be done with some sort of wrapper, such that the plugins aren't installed by default.
    svn path=/nixpkgs/trunk/; revision=5133

--- pkgs/misc/maven/builder.sh
+++ pkgs/misc/maven/builder.sh
@@ -11,4 +11,14 @@ cp -r $out/bin $out/bin-orig
 # Remove the original mvn from the bin directory
 rm $out/bin/$mavenBinary
 # Set the JAVA_HOME variable when using Maven
-makeWrapper "$out/bin-orig/$mavenBinary" "$out/bin/$mavenBinary"  --set JAVA_HOME "$jdk"
+makeWrapper "$out/bin-orig/$mavenBinary" "$out/bin/$mavenBinary" --set JAVA_HOME "$jdk"
+
+# Add the maven-axis and JIRA plugin by default when using maven 1.x
+if [ -e $out/bin/maven ]
+then
+  export OLD_HOME=$HOME
+  export HOME=.
+  $out/bin/maven plugin:download -DgroupId=maven-plugins -DartifactId=maven-axis-plugin -Dversion=0.7
+  $out/bin/maven plugin:download -Dmaven.repo.remote=http://repository.atlassian.com,http://www.ibiblio.org/maven -DartifactId=maven-axis-plugin -DgroupId=atlassian -Dversion=0.7AXIS1.3
+  export HOME=OLD_HOME
+fi
