144c22b2fe692169d1f6fbd1a29a1fc1a4b2c784
Merijn de Jonge <merijn.de.jonge@sorama.eu>
2006-04-04 13:10:05 +0000

    addid nix expression for toolbus
    svn path=/nixpkgs/trunk/; revision=5141

--- pkgs/development/interpreters/toolbus/default.nix
+++ pkgs/development/interpreters/toolbus/default.nix
@@ -1,11 +1,1 @@
-{stdenv, fetchurl, toolbuslib, atermjava, aterm, yacc, flex} :
-
-stdenv.mkDerivation {
-   name = "toolbus-1.2.2";
-   src = fetchurl {
-            url = http://www.cwi.nl/projects/MetaEnv/toolbus/toolbus-1.2.2.tar.gz;
-            md5 = "887349b097006c0883e1948797349a50";
-         };
-   inherit toolbuslib atermjava aterm yacc flex;
-   buildInputs = [toolbuslib atermjava aterm yacc flex];
-}
+import ./toolbus-1.2.2.nix
