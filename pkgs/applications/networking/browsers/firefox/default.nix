b3b9118eab4898db3f342d6b0c80a70369de13d6
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-04-14 16:08:35 +0000

    * Firefox 1.5.0.2.
    svn path=/nixpkgs/trunk/; revision=5161

--- pkgs/applications/networking/browsers/firefox/default.nix
+++ pkgs/applications/networking/browsers/firefox/default.nix
@@ -10,12 +10,12 @@
 }:
 
 stdenv.mkDerivation {
-  name = "firefox-1.5.0.1";
+  name = "firefox-1.5.0.2";
 
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://releases.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/source/firefox-1.5.0.1-source.tar.bz2;
-    md5 = "c76f02956645bc823241379e27f76bb5";
+    url = http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/source/firefox-1.5.0.2-source.tar.bz2;
+    md5 = "6bd65d899e9bcb16c28225899cc61a1e";
   };
 
   buildInputs = [
