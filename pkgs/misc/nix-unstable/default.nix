285756b5b0829e7451198abc1cb57be36ed2d421
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-05-01 19:16:12 +0000

    * Latest version.
    svn path=/nixpkgs/trunk/; revision=5247

--- pkgs/misc/nix-unstable/default.nix
+++ pkgs/misc/nix-unstable/default.nix
@@ -8,11 +8,11 @@ assert aterm != null && bdb != null && perl != null;
 # assert aterm.version >= 2.0
 
 stdenv.mkDerivation {
-  name = "nix-0.10pre5214";
+  name = "nix-0.10pre5245";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/nix/nix-0.10pre5214/nix-0.10pre5214.tar.bz2;
-    md5 = "a6e9e551a5d45d5850e4b94f8c1c8f0a";
+    url = http://nix.cs.uu.nl/dist/nix/nix-0.10pre5245/nix-0.10pre5245.tar.bz2;
+    md5 = "12aa99b0ed20ebc3e37529bb01a60bc3";
   };
   buildInputs = [aterm bdb perl curl];
   #patches = [./nix-profile-0.9.2.patch];
