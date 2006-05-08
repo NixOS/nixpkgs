6a1a282635d95f635590c1a0948fa5ff41573db5
Armijn Hemel <armijn@gpl-violations.org>
2006-05-08 21:05:07 +0000

    vim 7.0
    svn path=/nixpkgs/trunk/; revision=5289

--- pkgs/applications/editors/vim/default.nix
+++ pkgs/applications/editors/vim/default.nix
@@ -1,12 +1,12 @@
 {stdenv, fetchurl, ncurses}:
  
 stdenv.mkDerivation {
-  name = "vim-6.3";
+  name = "vim-7.0";
  
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/vim-6.3.tar.bz2;
-    md5 = "821fda8f14d674346b87e3ef9cb96389";
+    url = ftp://ftp.vim.org/pub/vim/unix/vim-7.0.tar.bz2;
+    md5 = "4ca69757678272f718b1041c810d82d8";
   };
  
   inherit ncurses;
