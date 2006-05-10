93957f48c31bd1911ba733ba3bcb8903419584ba
Armijn Hemel <armijn@gpl-violations.org>
2006-05-10 22:56:03 +0000

    ethereal 0.99
    svn path=/nixpkgs/trunk/; revision=5291

--- pkgs/applications/networking/sniffers/ethereal/default.nix
+++ pkgs/applications/networking/sniffers/ethereal/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, perl, pkgconfig, glib, libpcap}:
 
 stdenv.mkDerivation {
-  name = "ethereal-0.10.14";
+  name = "ethereal-0.99";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/ethereal-0.10.14.tar.bz2;
-    md5 = "297f678c037f88429250830e924b8fa0";
+    url = ftp://ftp.uni-kl.de/pub/ethereal/ethereal-0.99.0.tar.bz2;
+    md5 = "f9905b9d347acdc05af664a7553f7f76";
   };
   buildInputs = [perl pkgconfig glib libpcap];
 }
