046aeb12b0be90e51dec3ba437cff244d7aa109e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-05-30 12:43:45 +0000

    * We no longer need the prefetch stuff.
    svn path=/nixpkgs/trunk/; revision=5340

--- pkgs/build-support/fetchsvn/builder.sh
+++ pkgs/build-support/fetchsvn/builder.sh
@@ -2,17 +2,11 @@ source $stdenv/setup
 
 header "exporting $url (r$rev) into $out"
 
-prefetch=$(dirname $out)/svn-checkout-tmp-$outputHash
-echo $prefetch
-if test -e "$prefetch"; then
-    mv $prefetch $out
-else
-    # Pipe the "p" character into Subversion to force it to accept the
-    # server's certificate.  This is perfectly safe: we don't care
-    # whether the server is being spoofed --- only the cryptographic
-    # hash of the output matters.
-    echo 'p' | svn export -r "$rev" "$url" $out
-fi
+# Pipe the "p" character into Subversion to force it to accept the
+# server's certificate.  This is perfectly safe: we don't care
+# whether the server is being spoofed --- only the cryptographic
+# hash of the output matters.
+echo 'p' | svn export -r "$rev" "$url" $out
 
 actual=$(nix-hash $out)
 if test "$actual" != "$outputHash"; then
