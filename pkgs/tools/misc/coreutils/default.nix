42c513626509e38bcebb7e422fbc321f71f08f9c
Armijn Hemel <armijn@gpl-violations.org>
2006-06-17 11:58:14 +0000

    new coreutils
    svn path=/nixpkgs/trunk/; revision=5460

--- pkgs/tools/misc/coreutils/default.nix
+++ pkgs/tools/misc/coreutils/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "coreutils-5.94";
+  name = "coreutils-5.96";
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/gnu/coreutils/coreutils-5.94.tar.bz2;
-    md5 = "11985c8345371546da8ff13f7efae359";
+    url = ftp://ftp.nluug.nl/pub/gnu/coreutils/coreutils-5.96.tar.bz2;
+    md5 = "bf55d069d82128fd754a090ce8b5acff";
   };
 }
