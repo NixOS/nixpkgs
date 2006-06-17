3d789d45dcc2f9a462689d720cc5f75b4d70580c
Armijn Hemel <armijn@gpl-violations.org>
2006-06-17 12:21:12 +0000

    new version of parted
    svn path=/nixpkgs/trunk/; revision=5461

--- pkgs/tools/misc/parted/default.nix
+++ pkgs/tools/misc/parted/default.nix
@@ -1,10 +1,12 @@
 {stdenv, fetchurl, e2fsprogs, ncurses, readline}:
 
 stdenv.mkDerivation {
-  name = "parted-1.6.23";
+  #name = "parted-1.6.23";
+  name = "parted-1.7.1";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/parted-1.6.23.tar.gz;
-    md5 = "7e46a32def60ea355c193d9225691742";
+    #url = http://nix.cs.uu.nl/dist/tarballs/parted-1.6.23.tar.gz;
+    url = ftp://ftp.nluug.nl/pub/gnu/parted/parted-1.7.1.tar.bz2;
+    md5 = "baa6771273c8362d735086d52a0d6efe";
   };
   buildInputs = [e2fsprogs ncurses readline];
   patches = [./parted-trailingcomma.patch];
