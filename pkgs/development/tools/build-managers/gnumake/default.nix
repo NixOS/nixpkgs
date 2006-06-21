2c1e5d0cfa9996e814f6b5e2807f3b7853ac52e7
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-06-21 15:53:46 +0000

    * Latest GNU Make.
    svn path=/nixpkgs/trunk/; revision=5480

--- pkgs/development/tools/build-managers/gnumake/default.nix
+++ pkgs/development/tools/build-managers/gnumake/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "gnumake-3.80";
+  name = "gnumake-3.81";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/make-3.80.tar.bz2;
-    md5 = "0bbd1df101bc0294d440471e50feca71";
+    url = http://ftp.gnu.org/pub/gnu/make/make-3.81.tar.bz2;
+    md5 = "354853e0b2da90c527e35aabb8d6f1e6";
   };
   patches = [./log.diff];
 }
