450bdbe28a1254f14153a8d25c4e4f16e154a690
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-06-21 15:55:14 +0000

    * Latest Python.
    svn path=/nixpkgs/trunk/; revision=5483

--- pkgs/development/interpreters/python/default.nix
+++ pkgs/development/interpreters/python/default.nix
@@ -3,10 +3,10 @@
 assert zlibSupport -> zlib != null;
 
 stdenv.mkDerivation {
-  name = "python-2.4.2";
+  name = "python-2.4.3";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/Python-2.4.2.tar.bz2;
-    md5 = "98db1465629693fc434d4dc52db93838";
+    url = http://www.python.org/ftp/python/2.4.3/Python-2.4.3.tar.bz2;
+    md5 = "141c683447d5e76be1d2bd4829574f02";
   };
   buildInputs = [
     (if zlibSupport then zlib else null)
