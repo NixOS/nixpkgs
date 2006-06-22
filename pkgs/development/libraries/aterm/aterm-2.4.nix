ac41a46f6e387a637b9fe49c6d15594ac5e7e524
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-06-22 09:02:54 +0000

    * ATerm aliasing patch (from   http://bugzilla.sen.cwi.nl:8080/show_bug.cgi?id=470).
    svn path=/nixpkgs/trunk/; revision=5486

--- pkgs/development/libraries/aterm/aterm-2.4.nix
+++ pkgs/development/libraries/aterm/aterm-2.4.nix
@@ -6,6 +6,7 @@ stdenv.mkDerivation {
     url = http://nix.cs.uu.nl/dist/tarballs/aterm-2.4.2.tar.gz;
     md5 = "18617081dd112d85e6c4b1b552628114";
   };
+  patches = [./aterm-alias-fix-2.patch];
   meta = {
     homepage = http://www.cwi.nl/htbin/sen1/twiki/bin/view/SEN1/ATerm;
     license = "LGPL";
