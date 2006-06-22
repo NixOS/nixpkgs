530657fb19dfe64555acdda450292140141d595a
Martin Bravenboer <martin.bravenboer@logicblox.com>
2006-06-22 10:27:35 +0000

    The real gnumake 3.80
    svn path=/nixpkgs/trunk/; revision=5491

--- pkgs/development/tools/build-managers/gnumake-3.80/default.nix
+++ pkgs/development/tools/build-managers/gnumake-3.80/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "gnumake-3.81";
+  name = "gnumake-3.80";
   src = fetchurl {
-    url = http://ftp.gnu.org/pub/gnu/make/make-3.81.tar.bz2;
-    md5 = "354853e0b2da90c527e35aabb8d6f1e6";
+    url = http://nix.cs.uu.nl/dist/tarballs/make-3.80.tar.bz2;
+    md5 = "0bbd1df101bc0294d440471e50feca71";
   };
   patches = [./log.diff];
 }
