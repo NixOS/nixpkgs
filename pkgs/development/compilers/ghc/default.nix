2bbb426a7d60941ef2eae7c52c8748da814cbd2f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-06-22 15:22:55 +0000

    * GHC 6.4.2.
    svn path=/nixpkgs/trunk/; revision=5493

--- pkgs/development/compilers/ghc/default.nix
+++ pkgs/development/compilers/ghc/default.nix
@@ -1,10 +1,10 @@
 {stdenv, gcc, fetchurl, perl, ghc, m4, readline, ncurses}:
 
 stdenv.mkDerivation {
-  name = "ghc-6.4.1";
+  name = "ghc-6.4.2";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/ghc-6.4.1-src.tar.bz2;
-    md5 = "fd289bc7c3afa272ff831a71a50b5b00";
+    url = http://www.haskell.org/ghc/dist/6.4.2/ghc-6.4.2-src.tar.bz2;
+    md5 = "a394bf14e94c3bca5507d568fcc03375";
   };
   buildInputs = [perl ghc m4];
   propagatedBuildInputs = [readline ncurses];
