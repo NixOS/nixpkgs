3be153c62c7ec10744e594128a98109cb3d658e8
Armijn Hemel <armijn@gpl-violations.org>
2006-06-23 15:09:14 +0000

    new version of syslinux, 3.10 already fixed gcc 4 fixes...
    svn path=/nixpkgs/trunk/; revision=5503

--- pkgs/os-specific/linux/syslinux/default.nix
+++ pkgs/os-specific/linux/syslinux/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, nasm, perl}:
 
 stdenv.mkDerivation {
-  name = "syslinux-3.09";
+  name = "syslinux-3.11";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/syslinux-3.09.tar.bz2;
-    md5 = "dd403b15ef18bb0e5d78d3f552f822a5";
+    url = http://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-3.11.tar.bz2;
+    md5 = "513ff71287a4397d507879a1a836c2e8";
   };
  buildInputs = [nasm perl];
  patches = [./syslinux-installpath.patch];
