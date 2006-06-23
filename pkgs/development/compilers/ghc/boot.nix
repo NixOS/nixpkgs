45e1edfda8a1aedae43492c616c0f5f053e235c9
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-06-23 20:08:48 +0000

    * Give the GHC builder a temporary $HOME.
    svn path=/nixpkgs/trunk/; revision=5504

--- pkgs/development/compilers/ghc/boot.nix
+++ pkgs/development/compilers/ghc/boot.nix
@@ -3,11 +3,11 @@
 assert stdenv.system == "i686-linux";
 
 stdenv.mkDerivation {
-  name = "ghc-6.4.1";
+  name = "ghc-6.4.2";
   builder = ./boot.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/ghc-6.4.1-i386-unknown-linux.tar.bz2;
-    md5 = "9cd18a8e946da91b373b8ec855cd842e";
+    url = http://www.haskell.org/ghc/dist/6.4.2/ghc-6.4.2-i386-unknown-linux.tar.bz2;
+    md5 = "092fe2e25dab22b926babe97cc77db1f";
   };
   buildInputs = [perl];
   propagatedBuildInputs = [readline ncurses];
--- pkgs/development/compilers/ghc/builder.sh
+++ pkgs/development/compilers/ghc/builder.sh
@@ -2,4 +2,8 @@ source $stdenv/setup
 
 configureFlags="--with-gcc=$gcc/bin/gcc"
 
+# Don't you hate build processes that write in $HOME? :-(
+export HOME=$(pwd)/fake-home
+mkdir -p $HOME
+
 genericBuild
