9273065b0511fb7b1f3c332b5635c02648ae7516
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-06-24 10:32:02 +0000

    * Doh!
    svn path=/nixpkgs/trunk/; revision=5511

--- pkgs/development/libraries/chmlib/default.nix
+++ pkgs/development/libraries/chmlib/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   builder = ./builder.sh;
   src = fetchurl {
     url = http://66.93.236.84/~jedwin/projects/chmlib/chmlib-0.38.tar.bz2;
-    md5 = "8fa5e9a1af13084ca465d9ee09e1946e";
+    md5 = "d72661526aaea377ed30e9f58a086964";
   };
   buildInputs = [libtool];
 }
