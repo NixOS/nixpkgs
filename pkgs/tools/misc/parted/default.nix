c21a8b149247c5118cc5e02461ca39c36a7b92fa
Armijn Hemel <armijn@gpl-violations.org>
2006-06-24 17:29:14 +0000

    cosmetic fix
    svn path=/nixpkgs/trunk/; revision=5518

--- pkgs/tools/misc/parted/default.nix
+++ pkgs/tools/misc/parted/default.nix
@@ -1,10 +1,8 @@
 {stdenv, fetchurl, e2fsprogs, ncurses, readline}:
 
 stdenv.mkDerivation {
-  #name = "parted-1.6.23";
   name = "parted-1.7.1";
   src = fetchurl {
-    #url = http://nix.cs.uu.nl/dist/tarballs/parted-1.6.23.tar.gz;
     url = ftp://ftp.nluug.nl/pub/gnu/parted/parted-1.7.1.tar.bz2;
     md5 = "baa6771273c8362d735086d52a0d6efe";
   };
