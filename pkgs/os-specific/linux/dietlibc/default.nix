d4643c2a7fc19efcb427731a0b2bd16e82c872ce
Armijn Hemel <armijn@gpl-violations.org>
2006-06-25 00:44:53 +0000

    update to 0.30
    svn path=/nixpkgs/trunk/; revision=5526

--- pkgs/os-specific/linux/dietlibc/default.nix
+++ pkgs/os-specific/linux/dietlibc/default.nix
@@ -3,10 +3,10 @@
 assert stdenv.system == "i686-linux";
 
 stdenv.mkDerivation {
-  name = "dietlibc-0.29";
+  name = "dietlibc-0.30";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/dietlibc-0.29.tar.bz2;
-    md5 = "16d31dd7b5f9124e8ea8280c3f646e13";
+    url = http://www.kernel.org/pub/linux/libs/dietlibc/dietlibc-0.30.tar.bz2;
+    md5 = "2465d652fff6f1fad3da3b98e60e83c9";
   };
   patches = [./dietlibc-install.patch];
 }
