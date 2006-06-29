9a67a5ca5a76272ba672e18fd0a71f6db22082b6
Armijn Hemel <armijn@gpl-violations.org>
2006-06-29 11:52:23 +0000

    upgrade to 4.5.14
    svn path=/nixpkgs/trunk/; revision=5537

--- pkgs/development/tools/misc/strace/default.nix
+++ pkgs/development/tools/misc/strace/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "strace-4.5.12";
+  name = "strace-4.5.14";
 
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/strace-4.5.12.tar.bz2;
-    md5 = "c9dc77b9bd7f144f317e8289e0f6d40b";
+    url = http://surfnet.dl.sourceforge.net/sourceforge/strace/strace-4.5.14.tar.bz2;
+    md5 = "09bcd5d00ece28f8154dec11cadfce3c";
   };
 }
