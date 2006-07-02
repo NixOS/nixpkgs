cb66528886011acdb94eb05b4df96704f77eb361
Armijn Hemel <armijn@gpl-violations.org>
2006-07-02 22:42:33 +0000

    mktemp uses the generic builder, so the builder can be removed
    svn path=/nixpkgs/trunk/; revision=5560

--- pkgs/tools/security/mktemp/default.nix
+++ pkgs/tools/security/mktemp/default.nix
@@ -3,7 +3,6 @@
 stdenv.mkDerivation {
   name = "mktemp-1.5";
   
-  builder = ./builder.sh;
   src = fetchurl {
     url = http://nix.cs.uu.nl/dist/tarballs/mktemp-1.5.tar.gz;
     md5 = "9a35c59502a228c6ce2be025fc6e3ff2";
