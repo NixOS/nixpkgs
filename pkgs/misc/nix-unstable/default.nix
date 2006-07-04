6d5a7bd6dfdbb7a9fd97610bb409d1410dc5f712
Armijn Hemel <armijn@gpl-violations.org>
2006-07-04 18:42:58 +0000

    update to a newer unstable Nix
    svn path=/nixpkgs/trunk/; revision=5578

--- pkgs/misc/nix-unstable/default.nix
+++ pkgs/misc/nix-unstable/default.nix
@@ -8,11 +8,11 @@ assert aterm != null && bdb != null && perl != null;
 # assert aterm.version >= 2.0
 
 stdenv.mkDerivation {
-  name = "nix-0.10pre5245";
+  name = "nix-0.10pre5429";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/nix/nix-0.10pre5245/nix-0.10pre5245.tar.bz2;
-    md5 = "12aa99b0ed20ebc3e37529bb01a60bc3";
+    url = http://nix.cs.uu.nl/dist/nix/nix-0.10pre5479/nix-0.10pre5479.tar.bz2;
+    md5 = "20055c95996a694235d09a47aefa24c1";
   };
   buildInputs = [aterm bdb perl curl];
   #patches = [./nix-profile-0.9.2.patch];
