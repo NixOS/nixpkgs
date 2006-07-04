87613a856a492ca805d080b0675ce727fcf49dc7
Armijn Hemel <armijn@gpl-violations.org>
2006-07-04 20:26:50 +0000

    gimp dev version 1.3.4 updated to 1.3.9
    svn path=/nixpkgs/trunk/; revision=5581

--- pkgs/applications/graphics/gimp/default.nix
+++ pkgs/applications/graphics/gimp/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, pkgconfig, gtk, libgtkhtml, glib, pango, atk, freetype, fontconfig, libart_lgpl, libtiff, libjpeg, libpng, libexif, zlib, perl, perlXMLParser, python, pygtk}:
 
 stdenv.mkDerivation {
-  name = "gimp-2.3.6";
+  name = "gimp-2.3.9";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/gimp-2.3.6.tar.bz2;
-    md5 = "ce8ad77f4eb47abb868e6b4eb1f97943" ;
+    url = ftp://ftp.gimp.org/pub/gimp/v2.3/gimp-2.3.9.tar.bz2;
+    md5 = "4299e81e1824e08a90b50dc8beb46151" ;
   };
   
   buildInputs = [ pkgconfig gtk libgtkhtml glib pango atk freetype fontconfig libart_lgpl libtiff libjpeg libpng libexif zlib perl perlXMLParser python pygtk] ;
