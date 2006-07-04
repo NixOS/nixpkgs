7b1649c62af37353256eba9a83d790aa8e47c4f1
Armijn Hemel <armijn@gpl-violations.org>
2006-07-04 12:28:16 +0000

    fix download location for libpng
    svn path=/nixpkgs/trunk/; revision=5568

--- pkgs/development/libraries/libpng/default.nix
+++ pkgs/development/libraries/libpng/default.nix
@@ -5,7 +5,7 @@ assert zlib != null;
 stdenv.mkDerivation {
   name = "libpng-1.2.10";
   src = fetchurl {
-    url = ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.2.10.tar.bz2;
+    url = ftp://ftp.simplesystems.org/pub/libpng/png/src/history/libpng-1.2.10.tar.bz2;
     md5 = "4f23eebd59ddd01a8f91ff8c823dd7d6";
   };
   propagatedBuildInputs = [zlib];
