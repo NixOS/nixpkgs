569bd692f31382b830940b2d4d0c6c6aef665a26
Armijn Hemel <armijn@gpl-violations.org>
2006-07-04 13:41:23 +0000

    update to 2.6.17.3 (security bugs)
    svn path=/nixpkgs/trunk/; revision=5573

--- pkgs/os-specific/linux/kernel/default.nix
+++ pkgs/os-specific/linux/kernel/default.nix
@@ -3,11 +3,11 @@
 assert stdenv.system == "i686-linux";
 
 stdenv.mkDerivation {
-  name = "linux-2.6.17.1";
+  name = "linux-2.6.17.3";
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/os/Linux/system/kernel/v2.6/linux-2.6.17.1.tar.bz2;
-    md5 = "0a8f1a66646bc6ac7b3ec3e8f51652a0";
+    url = ftp://ftp.nluug.nl/pub/os/Linux/system/kernel/v2.6/linux-2.6.17.3.tar.bz2;
+    md5 = "14bcbbbb4206ff8ec863f9f3aa21293d";
   };
   config = ./config-2.6.17.1;
   inherit perl;
