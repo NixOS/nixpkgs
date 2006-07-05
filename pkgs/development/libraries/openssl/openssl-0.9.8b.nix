228fd19ffbb6c01d019f1df2bd69f68efd3fd201
Armijn Hemel <armijn@gpl-violations.org>
2006-07-05 15:54:53 +0000

    add openssl 0.9.8b
    svn path=/nixpkgs/trunk/; revision=5592

---
+++ pkgs/development/libraries/openssl/openssl-0.9.8b.nix
@@ -0,0 +1,10 @@
+{stdenv, fetchurl, perl}: stdenv.mkDerivation {
+  name = "openssl-0.9.8b";
+  builder = ./builder.sh;
+  src = fetchurl {
+    url = ftp://ftp.nluug.nl/pub/security/openssl/openssl-0.9.8b.tar.gz;
+    sha1 = "99565db630a044fa484d4f91006a31908f262246";
+  };
+  buildInputs = [perl];
+#  patches = [./darwin-makefile.patch];
+}
