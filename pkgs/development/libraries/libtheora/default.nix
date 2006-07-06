3af28ab76e1dd34a5924936240abb68916da89ea
Armijn Hemel <armijn@gpl-violations.org>
2006-07-06 22:40:39 +0000

    1.0alpha4 -> 1.0alpha7
    svn path=/nixpkgs/trunk/; revision=5636

--- pkgs/development/libraries/libtheora/default.nix
+++ pkgs/development/libraries/libtheora/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, libogg, libvorbis}:
 
 stdenv.mkDerivation {
-  name = "libtheora-1.0alpha4";
+  name = "libtheora-1.0alpha7";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/libtheora-1.0alpha4.tar.bz2;
-    md5 = "a71ac42ec0f848da327930841a80ff2b";
+    url = http://downloads.xiph.org/releases/theora/libtheora-1.0alpha7.tar.bz2;
+    md5 = "1bc851e39e4b16977131d5e5f769f48b";
   };
   propagatedBuildInputs = [libogg libvorbis];
 }
