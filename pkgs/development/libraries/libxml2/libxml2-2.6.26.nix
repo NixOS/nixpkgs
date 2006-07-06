c4d7d5811b5401d1d89c8385e8460f78fbc6c914
Armijn Hemel <armijn@gpl-violations.org>
2006-07-06 22:31:05 +0000

    libxml2 2.6.26
    svn path=/nixpkgs/trunk/; revision=5635

---
+++ pkgs/development/libraries/libxml2/libxml2-2.6.26.nix
@@ -0,0 +1,20 @@
+{stdenv, fetchurl, zlib, python ? null, pythonSupport ? true}:
+
+assert zlib != null;
+assert pythonSupport -> python != null;
+
+stdenv.mkDerivation {
+  name = "libxml2-2.6.26";
+  builder = ./builder.sh;
+
+  src = fetchurl {
+    url = ftp://xmlsoft.org/libxml2/libxml2-2.6.26.tar.gz;
+    md5 = "2d8d3805041edab967368b497642f981";
+  };
+
+  python = if pythonSupport then python else null;
+  inherit pythonSupport;
+
+  buildInputs =  if pythonSupport then [python] else [];
+  propagatedBuildInputs = [zlib];
+}
