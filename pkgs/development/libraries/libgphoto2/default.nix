1789bf99440b00437e6e749e7ae274d2273a093f
Armijn Hemel <armijn@gpl-violations.org>
2006-07-06 21:11:59 +0000

    update to 2.2.1. Also include a patch to fix a SNAFU by the maintainers, which are not willing to fix it (apparently)
    svn path=/nixpkgs/trunk/; revision=5626

--- pkgs/development/libraries/libgphoto2/default.nix
+++ pkgs/development/libraries/libgphoto2/default.nix
@@ -1,11 +1,13 @@
 {stdenv, fetchurl, pkgconfig, libusb}:
 
 stdenv.mkDerivation {
-  name = "libgphoto2-2.1.99";
+  name = "libgphoto2-2.2.1";
 
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/libgphoto2-2.1.99.tar.bz2;
-    md5 = "3c6d9cb65661915e07491a6f9215d5a9";
+    url = http://surfnet.dl.sourceforge.net/sourceforge/gphoto/libgphoto2-2.2.1.tar.bz2;
+    md5 = "69827311733e39fafa9f77bb05e55b77";
   };
   buildInputs = [pkgconfig libusb];
+
+  patches = [./libgphoto2-2.2.1.patch];
 }
