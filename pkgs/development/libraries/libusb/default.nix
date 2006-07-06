c806a402e7445350d6c77920a6b008bfa08680b1
Armijn Hemel <armijn@gpl-violations.org>
2006-07-06 21:37:45 +0000

    update to 0.1.12
    svn path=/nixpkgs/trunk/; revision=5631

--- pkgs/development/libraries/libusb/default.nix
+++ pkgs/development/libraries/libusb/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "libusb-0.1.10a";
+  name = "libusb-0.1.12";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/libusb-0.1.10a.tar.gz;
-    md5 = "c6062b29acd2cef414bcc34e0decbdd1";
+    url = http://surfnet.dl.sourceforge.net/sourceforge/libusb/libusb-0.1.12.tar.gz;
+    md5 = "caf182cbc7565dac0fd72155919672e6";
   };
 }
