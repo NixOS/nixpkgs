381a915353f3ba33f195867bb7b4d5ac0d7cf471
Armijn Hemel <armijn@gpl-violations.org>
2006-07-06 22:56:04 +0000

    1.1.0 -> 1.1.2
    svn path=/nixpkgs/trunk/; revision=5639

--- pkgs/development/libraries/libvorbis/default.nix
+++ pkgs/development/libraries/libvorbis/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, libogg}:
 
 stdenv.mkDerivation {
-  name = "libvorbis-1.1.0";
+  name = "libvorbis-1.1.2";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/libvorbis-1.1.0.tar.gz;
-    md5 = "bb764aeabde613d1a424a29b1f15e7e6";
+    url = http://downloads.xiph.org/releases/vorbis/libvorbis-1.1.2.tar.gz;
+    md5 = "37847626b8e1b53ae79a34714c7b3211";
   };
   buildInputs = [libogg];
 }
