9ecd455774b5547c6bcd8bb322ea0740aeb4a54d
Armijn Hemel <armijn@gpl-violations.org>
2006-07-06 21:12:18 +0000

    update to 2.2.0
    svn path=/nixpkgs/trunk/; revision=5627

--- pkgs/applications/misc/gphoto2/default.nix
+++ pkgs/applications/misc/gphoto2/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, pkgconfig, libgphoto2, libexif, popt}:
 
 stdenv.mkDerivation {
-  name = "gphoto2-2.1.99";
+  name = "gphoto2-2.2.0";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/gphoto2-2.1.99.tar.bz2;
-    md5 = "549a9dfae6910ab6456b194ea86b55a2";
+    url = http://surfnet.dl.sourceforge.net/sourceforge/gphoto/gphoto2-2.2.0.tar.bz2;
+    md5 = "f5c1f83185db598b4ca52889964a5e84";
   };
   buildInputs = [pkgconfig libgphoto2 libexif popt];
 }
