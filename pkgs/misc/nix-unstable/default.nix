563624911c2e7e209810b5490229925bfcfbc0fc
Armijn Hemel <armijn@gpl-violations.org>
2006-07-07 13:14:07 +0000

    update to pre5609
    svn path=/nixpkgs/trunk/; revision=5653

--- pkgs/misc/nix-unstable/default.nix
+++ pkgs/misc/nix-unstable/default.nix
@@ -8,11 +8,11 @@ assert aterm != null && bdb != null && perl != null;
 # assert aterm.version >= 2.0
 
 stdenv.mkDerivation {
-  name = "nix-0.10pre5529";
+  name = "nix-0.10pre5609";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/nix/nix-0.10pre5529/nix-0.10pre5529.tar.bz2;
-    md5 = "583a92e4df47a20d237d2a89bdad9296";
+    url = http://nix.cs.uu.nl/dist/nix/nix-0.10pre5609/nix-0.10pre5609.tar.bz2;
+    md5 = "83a7059062189161b9c1687080f837dd";
   };
   buildInputs = [aterm bdb perl curl];
   inherit storeDir stateDir aterm bdb;
