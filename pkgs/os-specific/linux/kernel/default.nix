96ec0645aa5209498a8789ed6123a2d14ed8ea56
Armijn Hemel <armijn@gpl-violations.org>
2006-07-07 13:12:41 +0000

    another security update -> 2.6.17.4
    svn path=/nixpkgs/trunk/; revision=5652

--- pkgs/os-specific/linux/kernel/default.nix
+++ pkgs/os-specific/linux/kernel/default.nix
@@ -3,11 +3,11 @@
 assert stdenv.system == "i686-linux";
 
 stdenv.mkDerivation {
-  name = "linux-2.6.17.3";
+  name = "linux-2.6.17.4";
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/os/Linux/system/kernel/v2.6/linux-2.6.17.3.tar.bz2;
-    md5 = "14bcbbbb4206ff8ec863f9f3aa21293d";
+    url = ftp://ftp.nluug.nl/pub/os/Linux/system/kernel/v2.6/linux-2.6.17.4.tar.bz2;
+    md5 = "d33826d07134d33e526f3e828af24287";
   };
   config = ./config-2.6.17.1;
   inherit perl;
