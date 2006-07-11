4d7f947bcbf051ddf202add2f86dbe53f3f568cd
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-07-11 07:55:38 +0000

    * GCC 4.0.3.
    svn path=/nixpkgs/trunk/; revision=5676

--- pkgs/development/compilers/gcc-4.0/default.nix
+++ pkgs/development/compilers/gcc-4.0/default.nix
@@ -6,11 +6,11 @@
 assert langC;
 
 stdenv.mkDerivation {
-  name = "gcc-4.0.2";
+  name = "gcc-4.0.3";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/gcc-4.0.2.tar.bz2;
-    md5 = "a659b8388cac9db2b13e056e574ceeb0";
+    url = http://ftp.gnu.org/pub/gnu/gcc/gcc-4.0.3/gcc-4.0.3.tar.bz2;
+    md5 = "6ff1af12c53cbb3f79b27f2d6a9a3d50";
   };
   # !!! apply only if noSysDirs is set
   patches = [./no-sys-dirs.patch];
