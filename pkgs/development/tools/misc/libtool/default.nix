1410af25b794eddb4852aeef3dd0b988e7d52bcc
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-07-14 15:57:52 +0000

    * Latest Libtool.
    svn path=/nixpkgs/trunk/; revision=5710

--- pkgs/development/tools/misc/libtool/default.nix
+++ pkgs/development/tools/misc/libtool/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, m4, perl}:
 
 stdenv.mkDerivation {
-  name = "libtool-1.5.20";
+  name = "libtool-1.5.22";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/libtool-1.5.20.tar.gz;
-    md5 = "fc3b564700aebcb19de841c1c2d66e99";
+    url = http://ftp.gnu.org/gnu/libtool/libtool-1.5.22.tar.gz;
+    md5 = "8e0ac9797b62ba4dcc8a2fb7936412b0";
   };
   buildInputs = [m4 perl];
 }
