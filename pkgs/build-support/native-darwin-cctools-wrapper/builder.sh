b3936b7062884cd7500276dd544d6da4ae22f8db
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-07-14 17:17:01 +0000

    * Add nmedit.
    svn path=/nixpkgs/trunk/; revision=5713

--- pkgs/build-support/native-darwin-cctools-wrapper/builder.sh
+++ pkgs/build-support/native-darwin-cctools-wrapper/builder.sh
@@ -1,6 +1,6 @@
 source $stdenv/setup
 
 ensureDir $out/bin
-for i in ar as c++filt gprof ld nm ranlib size strings strip; do 
+for i in ar as c++filt gprof ld nm nmedit ranlib size strings strip; do 
     ln -s /usr/bin/$i $out/bin/
 done
