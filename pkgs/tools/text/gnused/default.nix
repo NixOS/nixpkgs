ea2da73c8dd4fa20971fb01bbe5925bf20d60c38
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-07-14 10:59:14 +0000

    * Doh! Idem.
    svn path=/nixpkgs/trunk/; revision=5704

--- pkgs/tools/text/gnused/default.nix
+++ pkgs/tools/text/gnused/default.nix
@@ -1,9 +1,11 @@
 {stdenv, fetchurl}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation ({
   name = "gnused-4.1.5";
   src = fetchurl {
     url = ftp://ftp.nluug.nl/pub/gnu/sed/sed-4.1.5.tar.gz;
     md5 = "7a1cbbbb3341287308e140bd4834c3ba";
   };
-}
+} // 
+  # !!! hack: this should go away in gnused > 4.1.5
+  (if stdenv.system != "i686-linux" then {patches = [./gettext-fix.patch];} else {}))
