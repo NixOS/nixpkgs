fee4d3f929e074d3c33e1af16cfc82ff5f0d0155
Armijn Hemel <armijn@gpl-violations.org>
2006-07-16 23:03:02 +0000

    update to 0.6.4
    svn path=/nixpkgs/trunk/; revision=5722

--- pkgs/os-specific/linux/qc-usb/default.nix
+++ pkgs/os-specific/linux/qc-usb/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, kernel}:
 
 stdenv.mkDerivation {
-  name = "qc-usb-0.6.3";
+  name = "qc-usb-0.6.4";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/qc-usb-0.6.3.tar.gz;
-    md5 = "3d33380a29a7f92c4eef1f82d61b4ee0";
+    url = http://heanet.dl.sourceforge.net/sourceforge/qce-ga/qc-usb-0.6.4.tar.gz;
+    md5 = "7e91c3a633382c99100e3ef4f1d9f50a";
   };
   inherit kernel;
   patches = [./quickcam-install.patch];
