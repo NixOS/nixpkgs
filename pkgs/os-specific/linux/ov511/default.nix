d169043264eb2982dd2cb744ee298ce92ce06762
Armijn Hemel <armijn@gpl-violations.org>
2006-07-17 17:57:26 +0000

    readd ov511 2.30
    svn path=/nixpkgs/trunk/; revision=5745

--- pkgs/os-specific/linux/ov511/default.nix
+++ pkgs/os-specific/linux/ov511/default.nix
@@ -1,13 +1,1 @@
-{stdenv, fetchurl, kernel}:
-
-stdenv.mkDerivation {
-  name = "ov511-2.32";
-  builder = ./builder.sh;
-  src = fetchurl {
-    url = http://www.ovcam.org/ov511/download/2.xx/distros/ov511-2.32.tar.bz2;
-    md5 = "6a08025311649356242761641a1df0f2";
-  };
-  patches = [./ov511-kernel.patch ./ov511-2.32.patch ./ov511-2.32-kdir.patch];
-  inherit kernel;
-  NIX_GLIBC_FLAGS_SET=1;
-}
+import ./ov511-2.30-default.nix
---
+++ pkgs/os-specific/linux/ov511/ov511-2.30-default.nix
@@ -0,0 +1,12 @@
+{stdenv, fetchurl, kernel}:
+
+stdenv.mkDerivation {
+  name = "ov511-2.30";
+  builder = ./ov511-2.30-builder.sh;
+  src = fetchurl {
+    url = http://nix.cs.uu.nl/dist/tarballs/ov511-2.30.tar.bz2;
+    md5 = "9eacf9e54f2f54a59ddbf14221a53f2a";
+  };
+  patches = [./ov511-kernel.patch];
+  inherit kernel;
+}
