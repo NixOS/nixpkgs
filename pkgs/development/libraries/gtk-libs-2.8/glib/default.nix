daefa7f60de76932642a4c1c3ed0d70ee0b73406
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-07-20 15:22:10 +0000

    * Propagate the gettext dependency.
    svn path=/nixpkgs/trunk/; revision=5831

--- pkgs/development/libraries/gtk-libs-2.8/glib/default.nix
+++ pkgs/development/libraries/gtk-libs-2.8/glib/default.nix
@@ -8,5 +8,6 @@ stdenv.mkDerivation {
     url = ftp://ftp.gtk.org/pub/gtk/v2.8/glib-2.8.6.tar.bz2;
     md5 = "fce6835fd8c99ab4c3e5213bc5bcd0ed";
   };
-  buildInputs = [pkgconfig gettext perl];
+  buildInputs = [pkgconfig perl];
+  propagatedBuildInputs = [gettext];
 }
