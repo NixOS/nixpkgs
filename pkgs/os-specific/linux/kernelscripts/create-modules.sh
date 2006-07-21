80b7adab70bc95133a90d27ea90a6a967d83c448
Armijn Hemel <armijn@gpl-violations.org>
2006-07-21 16:13:56 +0000

    cleanups
    svn path=/nixpkgs/trunk/; revision=5866

--- pkgs/os-specific/linux/kernelscripts/create-modules.sh
+++ pkgs/os-specific/linux/kernelscripts/create-modules.sh
@@ -1,19 +1,12 @@
 #! /bin/sh -e
 
-#archivesDir=$(@mktemp@/bin/mktemp -d)
 archivesDir=/
-
-cpwd=@coreutils@/bin/pwd
-
-#storeExpr=$(@nix@/bin/nix-store -qR $(@nix@/bin/nix-store -r $(echo '(import @kernelpkgs@).everything' | @nix@/bin/nix-instantiate -)))
 
 kernel=$(@nix@/bin/nix-store -r $(echo '(import @kernelpkgs@).kernel' | @nix@/bin/nix-instantiate -))
 
 ov511=$(@nix@/bin/nix-store -r $(echo '(import @kernelpkgs@).ov511' | @nix@/bin/nix-instantiate -))
 
 module_init_tools=$(@nix@/bin/nix-store -r $(echo '(import @kernelpkgs@).module_init_tools' | @nix@/bin/nix-instantiate -))
-
-#echo making kernel stuff
 
 kernelVersion=$(cd $kernel/lib/modules/; @coreutils@/bin/ls -d *)
 
@@ -25,15 +18,13 @@ echo making new kernel tree
 
 @coreutils@/bin/mkdir -p $archivesDir/lib/modules/$kernelVersion
 
-#echo $kernelVersion
-
 cd $kernel
 
 echo making kernel directories
 
 @findutils@/bin/find . -not -path "./lib/modules/$kernelVersion/build*" -type d | @findutils@/bin/xargs -n 1 -i% @coreutils@/bin/mkdir -p $archivesDir/%
 
-echo linking kernel modules
+echo symlinking kernel modules
 
 @findutils@/bin/find . -not -path "./lib/modules/$kernelVersion/build*" \
   -a -not -path "./System*" -a -not -path "./vmlinuz*" \
@@ -44,7 +35,7 @@ echo making ov511 directories
 cd $ov511
 @findutils@/bin/find . -not -path "./lib/modules/$kernelVersion/build*" -type d | @findutils@/bin/xargs -n 1 -i% @coreutils@/bin/mkdir -p $archivesDir/%
 
-echo linking ov511 modules
+echo symlinking ov511 modules
 
 @findutils@/bin/find . -not -path "./lib/modules/$kernelVersion/build*" -type f | @findutils@/bin/xargs -n 1 -i% @coreutils@/bin/ln -s $ov511/% $archivesDir/%
 
