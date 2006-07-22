0d620b8890478c48bccde573cbdb3b0ccab5f627
Armijn Hemel <armijn@gpl-violations.org>
2006-07-22 22:31:45 +0000

    1.4.2.2 -> 1.4.4
    svn path=/nixpkgs/trunk/; revision=5878

--- pkgs/tools/security/gnupg/default.nix
+++ pkgs/tools/security/gnupg/default.nix
@@ -7,11 +7,11 @@
 }:
 
 stdenv.mkDerivation {
-  name = "gnupg-1.4.2.2";
+  name = "gnupg-1.4.4";
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-1.4.2.2.tar.bz2;
-    sha1 = "f5559ddb004e0638f6bd9efe2bac00134c5065ba";
+    url = ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-1.4.4.tar.bz2;
+    sha1 = "3414d67f22973576f31e354f44859bafbccb7eee";
   };
   idea = if ideaSupport then fetchurl {
     url = ftp://ftp.gnupg.dk/pub/contrib-dk/idea.c.gz;
