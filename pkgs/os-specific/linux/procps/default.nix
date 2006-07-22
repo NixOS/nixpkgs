5ae0cc2c5e4bc0604f571cbf1d2945181ac52a0c
Armijn Hemel <armijn@gpl-violations.org>
2006-07-22 19:49:29 +0000

    new procps
    svn path=/nixpkgs/trunk/; revision=5877

--- pkgs/os-specific/linux/procps/default.nix
+++ pkgs/os-specific/linux/procps/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, ncurses}:
 
 stdenv.mkDerivation {
-  name = "procps-3.2.6";
+  name = "procps-3.2.7";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/procps-3.2.6.tar.gz;
-    md5 = "7ce39ea27d7b3da0e8ad74dd41d06783";
+    url = http://procps.sourceforge.net/procps-3.2.7.tar.gz;
+    md5 = "f490bca772b16472962c7b9f23b1e97d";
   };
   patches = [./makefile.patch ./procps-build.patch];
   buildInputs = [ncurses];
