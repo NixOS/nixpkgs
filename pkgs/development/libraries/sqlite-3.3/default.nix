e082ae115140b0cd1c3c25ad82c7362acffbc575
Armijn Hemel <armijn@gpl-violations.org>
2006-07-22 22:51:58 +0000

    3.3.5 -> 3.3.6
    svn path=/nixpkgs/trunk/; revision=5879

--- pkgs/development/libraries/sqlite-3.3/default.nix
+++ pkgs/development/libraries/sqlite-3.3/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-	name = "sqlite-3.3.5";
+	name = "sqlite-3.3.6";
 	src = fetchurl {
-		url = "http://www.sqlite.org/sqlite-3.3.5.tar.gz";
-		md5 = "dd2a7b6f2a07a4403a0b5e17e8ed5b88";
+		url = "http://www.sqlite.org/sqlite3-3.3.6.bin.gz";
+		md5 = "7e0b5e1bf989419c662c7955fdf47ab3";
 	};
 	configureFlags = "--enable-threadsafe";
 	inherit stdenv;
