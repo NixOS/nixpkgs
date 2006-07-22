ecae366a77fa8814f279ca0468a3a9f5af942923
Armijn Hemel <armijn@gpl-violations.org>
2006-07-22 11:42:53 +0000

    pass the right values for tk. Spotted on NixOS ;)
    svn path=/nixpkgs/trunk/; revision=5874

--- pkgs/applications/networking/instant-messengers/amsn/builder.sh
+++ pkgs/applications/networking/instant-messengers/amsn/builder.sh
@@ -1,5 +1,5 @@
 source $stdenv/setup
 
-configureFlags="--with-tcl=$tcl/lib"
+configureFlags="--with-tcl=$tcl/lib --with-tk=$tk/lib"
 
 genericBuild
--- pkgs/applications/networking/instant-messengers/amsn/default.nix
+++ pkgs/applications/networking/instant-messengers/amsn/default.nix
@@ -8,6 +8,6 @@ stdenv.mkDerivation {
     md5 = "1b90fdbb0a51c7646f4d2e6b22f18711";
   };
 
-  inherit tcl;
+  inherit tcl tk;
   buildInputs = [which tcl tk x11];
 }
