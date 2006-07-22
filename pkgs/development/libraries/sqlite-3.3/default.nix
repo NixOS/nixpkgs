e65725487120d73a80ec27e9171d52618ec15e90
Armijn Hemel <armijn@gpl-violations.org>
2006-07-22 22:54:22 +0000

    don't forget the closing ". This heat is killing my braaaaaaaain...
    svn path=/nixpkgs/trunk/; revision=5881

--- pkgs/development/libraries/sqlite-3.3/default.nix
+++ pkgs/development/libraries/sqlite-3.3/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation {
 	name = "sqlite-3.3.6";
 	src = fetchurl {
-		url = "http://www.sqlite.org/sqlite-3.3.6.tar.gz;
+		url = "http://www.sqlite.org/sqlite-3.3.6.tar.gz";
 		md5 = "a2cb1fafad5c2587e513dcbd18ace097";
 	};
 	configureFlags = "--enable-threadsafe";
