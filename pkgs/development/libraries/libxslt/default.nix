f6440357157cb58824346f3fa590dcfbfbfe086e
Armijn Hemel <armijn@gpl-violations.org>
2006-07-24 23:55:32 +0000

    prepare for multiple versions
    svn path=/nixpkgs/trunk/; revision=5930

--- pkgs/development/libraries/libxslt/default.nix
+++ pkgs/development/libraries/libxslt/default.nix
@@ -1,12 +1,1 @@
-{stdenv, fetchurl, libxml2}:
-
-assert libxml2 != null;
-
-stdenv.mkDerivation {
-  name = "libxslt-1.1.15";
-  src = fetchurl {
-    url = ftp://xmlsoft.org/libxml2/libxslt-1.1.15.tar.gz;
-    md5 = "238de9eda71b570ff7b78aaf65308fc6";
-  };
-  buildInputs = [libxml2];
-}
+import ./libxslt-1.1.15.nix
