55e322ca6c31e6d927d8ab358a45170a4006099e
Armijn Hemel <armijn@gpl-violations.org>
2006-07-25 08:24:22 +0000

    variability, add a static variant, default is to use shared libraries
    svn path=/nixpkgs/trunk/; revision=5932

--- pkgs/development/libraries/zlib/default.nix
+++ pkgs/development/libraries/zlib/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl}:
+{stdenv, fetchurl, static ? false}:
 
 stdenv.mkDerivation {
   name = "zlib-1.2.3";
@@ -6,5 +6,5 @@ stdenv.mkDerivation {
     url = http://nix.cs.uu.nl/dist/tarballs/zlib-1.2.3.tar.gz;
     md5 = "debc62758716a169df9f62e6ab2bc634";
   };
-  configureFlags = "--shared";
+  configureFlags = if static then "" else "--shared";
 }
