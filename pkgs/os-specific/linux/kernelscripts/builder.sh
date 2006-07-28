6aca0d8f9df45f60a5ef8ae9cfedb2036fb4edfe
Armijn Hemel <armijn@gpl-violations.org>
2006-07-28 13:29:33 +0000

    more fixes
    svn path=/nixpkgs/trunk/; revision=5985

--- pkgs/os-specific/linux/kernelscripts/builder.sh
+++ pkgs/os-specific/linux/kernelscripts/builder.sh
@@ -8,24 +8,27 @@ mkdir -p $out/lib/modules/$kernelVersion
 
 cd $kernel
 
-# making kernel directories
+echo making kernel directories
 
 find . -not -path "./lib/modules/$kernelVersion/build*" -type d | xargs -n 1 -i% mkdir -p $out/%
 
-# symlinking kernel modules
+echo symlinking kernel modules
 
 find . -not -path "./lib/modules/$kernelVersion/build*" -a -not -path \
   "./System*" -a -not -path "./vmlinuz*" -type f | xargs -n 1 -i% \
   ln -s $kernel/% $out/%
 
-# echo making ov511 directories
+echo $modules
+for i in $modules; do
+  echo making directories for $i
+  cd $i
+  find . -not -path "./lib/modules/$kernelVersion/build*" -type d | xargs -n 1 -i% mkdir -p $out/%
 
-# cd $ov511
-# @findutils@/bin/find . -not -path "./lib/modules/$kernelVersion/build*" -type d | @findutils@/bin/xargs -n 1 -i% @coreutils@/bin/mkdir -p $out/%
-# 
-# echo symlinking ov511 modules
-# 
-# @findutils@/bin/find . -not -path "./lib/modules/$kernelVersion/build*" -type f | @findutils@/bin/xargs -n 1 -i% @coreutils@/bin/ln -s $ov511/% $out/%
+  echo symlinking modules for $i
 
-# running depmod
+  find . -not -path "./lib/modules/$kernelVersion/build*" -type f | xargs -n 1 -i% ln -s $i/% $out/%
+
+done
+
+echo running depmod
 $module_init_tools/sbin/depmod -ae $kernelVersion
