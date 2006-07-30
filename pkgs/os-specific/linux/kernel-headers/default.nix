7930f906dcea7d017a8fb7e803370b693bdbfada
Armijn Hemel <armijn@gpl-violations.org>
2006-07-30 22:47:39 +0000

    allow for variability, don't change the default
    svn path=/nixpkgs/trunk/; revision=5999

--- pkgs/os-specific/linux/kernel-headers/default.nix
+++ pkgs/os-specific/linux/kernel-headers/default.nix
@@ -1,12 +1,1 @@
-{stdenv, fetchurl}:
-
-assert stdenv.system == "i686-linux";
-
-stdenv.mkDerivation {
-  name = "linux-headers-2.6.14.3-i386";
-  builder = ./builder.sh;
-  src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/linux-2.6.14.3.tar.bz2;
-    md5 = "982717a9cb246e3c427cc45e3fc86097";
-  };
-}
+import ./kernel-headers-2.6.14.3.nix
