2018e992bedce4040a129ce2134dde20cd6371a6
Armijn Hemel <armijn@gpl-violations.org>
2006-07-31 14:45:53 +0000

    patch the binaries to nuke paths to glibc. Ripped from stdenv/linux/scripts/fix-outpath.sh
    svn path=/nixpkgs/trunk/; revision=6003

--- pkgs/shells/bash-static/builder.sh
+++ pkgs/shells/bash-static/builder.sh
@@ -1,5 +1,12 @@
 source $stdenv/setup
 genericBuild
 cd $out/bin
+
+find . -type f | while read fn; do
+    cat $fn | sed "s|/nix/store/[a-z0-9]*-|/nix/store/ffffffffffffffffffffffffffffffff-|g" > $fn.tmp
+    if test -x $fn; then chmod +x $fn.tmp; fi
+    mv $fn.tmp $fn
+done
+
 strip $out/bin/bash
 ln -s bash sh
