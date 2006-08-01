f36368c749fb6786c2b6cce9d4590fb5639fe5d6
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-08-01 12:29:07 +0000

    * Boehm GC 6.8, needed on Darwin-x86.
    svn path=/nixpkgs/trunk/; revision=6011

--- pkgs/development/libraries/boehm-gc/default.nix
+++ pkgs/development/libraries/boehm-gc/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "boehm-gc-6.5";
+  name = "boehm-gc-6.8";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/gc6.5.tar.gz;
-    md5 = "00bf95cdcbedfa7321d14e0133b31cdb";
+    url = http://www.hpl.hp.com/personal/Hans_Boehm/gc/gc_source/gc6.8.tar.gz;
+    md5 = "418d38bd9c66398386a372ec0435250e";
   };
 }
