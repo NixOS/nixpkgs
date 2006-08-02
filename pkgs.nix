2c87da322d2781fee2f6cd65d1924651318f2dd8
Armijn Hemel <armijn@gpl-violations.org>
2006-08-02 22:34:13 +0000

    use less packages
    svn path=/nixu/trunk/; revision=6024

--- pkgs.nix
+++ pkgs.nix
@@ -2,10 +2,10 @@ rec {
   inherit (import /nixpkgs/trunk/pkgs/top-level/all-packages.nix {})
     stdenv kernel bash bashStatic coreutils coreutilsDiet findutilsWrapper utillinux utillinuxStatic sysvinit
    e2fsprogsDiet e2fsprogs
-    nettools nixUnstable subversion gcc wget which vim less screen openssh binutils
+    nettools nixUnstable subversion gcc wget which vim less screen openssh binutils nixStatic
     strace shadowutils iputils gnumake curl gnused gnutar gnugrep gzip
     mingettyWrapper grubWrapper syslinux parted module_init_tools module_init_toolsStatic udev
-    dhcpWrapper man nano eject sysklogd kudzu xawtv mktemp cdrtools cpio;
+    dhcpWrapper man nano eject sysklogd mktemp cdrtools cpio;
 
   boot = (import ./boot) {inherit stdenv bash bashStatic coreutils findutilsWrapper
     utillinux utillinuxStatic sysvinit e2fsprogs nettools subversion gcc wget which vim
@@ -20,5 +20,5 @@ rec {
     nix = nixUnstable;
     };
 
-  everything = [boot sysvinit sysklogd kernel xawtv];
+  everything = [boot sysvinit sysklogd kernel ];
 }
