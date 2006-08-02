2f5ec262a859afcc9750b0790a08b839105d2abd
Armijn Hemel <armijn@gpl-violations.org>
2006-08-02 10:52:32 +0000

    nuke references to glibc...this should become a generic function somewhere...
    svn path=/nixpkgs/trunk/; revision=6017

--- pkgs/misc/nix-static/builder.sh
+++ pkgs/misc/nix-static/builder.sh
@@ -11,4 +11,16 @@ preConfigure() {
 
 preConfigure=preConfigure
 
+postInstall() {
+  cd $out/bin
+
+  find . -type f | while read fn; do
+    cat $fn | sed "s|/nix/store/[a-z0-9]*-|/nix/store/ffffffffffffffffffffffffffffffff-|g" > $fn.tmp
+    if test -x $fn; then chmod +x $fn.tmp; fi
+    mv $fn.tmp $fn
+  done
+}
+
+postInstall=postInstall
+
 genericBuild
