68e7009fcc16c3b73a7711c16e7d64fa99c19d41
Armijn Hemel <armijn@gpl-violations.org>
2006-08-05 09:44:26 +0000

    update to pre6052, remove patches, since these are in Nix now
    svn path=/nixpkgs/trunk/; revision=6053

--- pkgs/misc/nix-unstable/default.nix
+++ pkgs/misc/nix-unstable/default.nix
@@ -8,17 +8,16 @@ assert aterm != null && bdb != null && perl != null;
 # assert aterm.version >= 2.0
 
 stdenv.mkDerivation {
-  name = "nix-0.10pre6047";
+  name = "nix-0.10pre6052";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/nix/nix-0.10pre6047/nix-0.10pre6047.tar.bz2;
-    md5 = "40f68dd3555dbb12478a468df301c3a9";
+    url = http://nix.cs.uu.nl/dist/nix/nix-0.10pre6052/nix-0.10pre6052.tar.bz2;
+    md5 = "89c131d6c11da091067a14d1eddaf3aa";
   };
   buildInputs = [aterm bdb perl curl];
   inherit storeDir stateDir aterm bdb;
 
   # uncomment if you want to be able to use nix-prefetch-url when NIX_ROOT
-  # is set
+  # is set. Not needed to build or install NixOS.
   #patches = [./nix-0.10pre5679.patch];
-  patches = [./nix-0.10pre5896-paths.patch ./nix-0.10pre5896-chroot-once.patch];
 }
