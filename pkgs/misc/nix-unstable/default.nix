0345f37f1ca3ccdd731dd5bf4f2e892e4d245530
Armijn Hemel <armijn@gpl-violations.org>
2006-08-05 00:28:06 +0000

    update to nix-0.10pre6047, remove patch, since it is no longer needed
    svn path=/nixpkgs/trunk/; revision=6050

--- pkgs/misc/nix-unstable/default.nix
+++ pkgs/misc/nix-unstable/default.nix
@@ -8,11 +8,11 @@ assert aterm != null && bdb != null && perl != null;
 # assert aterm.version >= 2.0
 
 stdenv.mkDerivation {
-  name = "nix-0.10pre5896";
+  name = "nix-0.10pre6047";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/nix/nix-0.10pre5896/nix-0.10pre5896.tar.bz2;
-    md5 = "509d4a452cbf2894a86184b5eaf45abd";
+    url = http://nix.cs.uu.nl/dist/nix/nix-0.10pre6047/nix-0.10pre6047.tar.bz2;
+    md5 = "40f68dd3555dbb12478a468df301c3a9";
   };
   buildInputs = [aterm bdb perl curl];
   inherit storeDir stateDir aterm bdb;
@@ -20,5 +20,5 @@ stdenv.mkDerivation {
   # uncomment if you want to be able to use nix-prefetch-url when NIX_ROOT
   # is set
   #patches = [./nix-0.10pre5679.patch];
-  patches = [./nix-0.10pre5896-date.patch ./nix-0.10pre5896-paths.patch ./nix-0.10pre5896-chroot-once.patch];
+  patches = [./nix-0.10pre5896-paths.patch ./nix-0.10pre5896-chroot-once.patch];
 }
