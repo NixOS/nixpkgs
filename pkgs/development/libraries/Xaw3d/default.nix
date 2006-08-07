a2c118e0e3806d1662beda70d9788008ce501c21
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-08-07 10:17:39 +0000

    * Remove redundant definition of LayYY_prev_more_offset; flex already   generates it.
    svn path=/nixpkgs/trunk/; revision=6064

--- pkgs/development/libraries/Xaw3d/default.nix
+++ pkgs/development/libraries/Xaw3d/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
     url = http://nix.cs.uu.nl/dist/tarballs/Xaw3d-1.5E.tar.gz;
     md5 = "29ecfdcd6bcf47f62ecfd672d31269a1";
   };
-  patches = [./config.patch];
+  patches = [./config.patch ./laylex.patch];
   buildInputs = [x11 imake gccmakedep libXmu libXpm libXp bison flex];
   propagatedBuildInputs = [x11 libXmu];
 }
