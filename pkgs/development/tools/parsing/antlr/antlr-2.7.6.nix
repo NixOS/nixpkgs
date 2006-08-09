1cb5a30f3453e9ac30744d544f8bd3166ba805cd
Martin Bravenboer <martin.bravenboer@logicblox.com>
2006-08-09 15:09:50 +0000

    Added ANTLR 2.7.6
    svn path=/nixpkgs/trunk/; revision=6082

---
+++ pkgs/development/tools/parsing/antlr/antlr-2.7.6.nix
@@ -0,0 +1,11 @@
+{stdenv, fetchurl, jre}:
+
+stdenv.mkDerivation {
+  name = "antlr-2.7.6";
+  builder = ./builder2.sh;
+  src = fetchurl {
+    url = http://www.antlr.org/download/antlr-2.7.6.tar.gz;
+    md5 = "17d8bf2e814f0a26631aadbbda8d7324";
+  };
+  inherit jre;
+}
