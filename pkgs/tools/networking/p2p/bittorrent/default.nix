38ee36175780aa8862af95aae280d0d235e8375e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-08-09 15:49:02 +0000

    * Backout accidental commit.
    svn path=/nixpkgs/trunk/; revision=6088

--- pkgs/tools/networking/p2p/bittorrent/default.nix
+++ pkgs/tools/networking/p2p/bittorrent/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, python, pygtk, makeWrapper}:
 
 stdenv.mkDerivation {
-  name = "bittorrent-4.9.8";
+  name = "bittorrent-4.4.0";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://download.bittorrent.com/dl/BitTorrent-4.9.8.tar.gz;
-    md5 = "30d14135a8c6869976cd807db019dfd9";
+    url = http://www.bittorrent.com/dl/BitTorrent-4.4.0.tar.gz;
+    md5 = "74d4b48202c28f0b27e989b6d5f5b214";
   };
   buildInputs = [python pygtk];
   inherit python pygtk makeWrapper;
