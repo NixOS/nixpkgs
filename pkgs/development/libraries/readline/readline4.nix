4bfffeee5233e84fdb980a0e6060a66eb946f015
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-08-21 10:35:01 +0000

    * Propagate readline's dependency on ncurses.
    svn path=/nixpkgs/trunk/; revision=6197

--- pkgs/development/libraries/readline/readline4.nix
+++ pkgs/development/libraries/readline/readline4.nix
@@ -6,6 +6,5 @@ stdenv.mkDerivation {
     url = http://nix.cs.uu.nl/dist/tarballs/readline-4.3.tar.gz;
     md5 = "f86f7cb717ab321fe15f1bbcb058c11e";
   };
-  inherit ncurses;
-  buildInputs = [ncurses];
+  propagatedBuildInputs = [ncurses];
 }
--- pkgs/development/libraries/readline/readline5.nix
+++ pkgs/development/libraries/readline/readline5.nix
@@ -6,6 +6,5 @@ stdenv.mkDerivation {
     url = ftp://ftp.gnu.org/gnu/readline/readline-5.1.tar.gz;
     md5 = "7ee5a692db88b30ca48927a13fd60e46";
   };
-  inherit ncurses;
-  buildInputs = [ncurses];
+  propagatedBuildInputs = [ncurses];
 }
