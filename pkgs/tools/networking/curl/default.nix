193edaab22a521721ad761097d1071ba4a0ae26d
Armijn Hemel <armijn@gpl-violations.org>
2006-08-25 21:01:42 +0000

    7.15.4 -> 7.15.5
    svn path=/nixpkgs/trunk/; revision=6243

--- pkgs/tools/networking/curl/default.nix
+++ pkgs/tools/networking/curl/default.nix
@@ -3,11 +3,11 @@
 assert sslSupport -> openssl != null;
 
 stdenv.mkDerivation {
-  name = "curl-7.15.4";
+  name = "curl-7.15.5";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://curl.haxx.se/download/curl-7.15.4.tar.bz2;
-    md5 = "d9345a55c8bc67eafcd37fa1b728e00e";
+    url = http://curl.haxx.se/download/curl-7.15.5.tar.bz2;
+    md5 = "594142c7d53bbdd988e8cef6354eeeff";
   };
   buildInputs = [zlib (if sslSupport then openssl else null)];
   patches = [./configure-cxxcpp.patch];
