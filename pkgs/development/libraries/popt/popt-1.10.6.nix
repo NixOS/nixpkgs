ac298dd69846a99df730d663769cb2d116e3eb45
Armijn Hemel <armijn@gpl-violations.org>
2006-08-27 19:59:23 +0000

    fix sources stuff
    svn path=/nixpkgs/trunk/; revision=6278

--- pkgs/development/libraries/popt/popt-1.10.6.nix
+++ pkgs/development/libraries/popt/popt-1.10.6.nix
@@ -1,12 +1,13 @@
-{stdenv, fetchurl, gettext}:
+{stdenv, fetchurl, gettext, libtool, autoconf, automake}:
 
 assert gettext != null;
 
 stdenv.mkDerivation {
+  builder = ./builder-1.10.6.sh;
   name = "popt-1.10.6";
   src = fetchurl {
     url = http://losser.st-lab.cs.uu.nl/~armijn/.nix/popt-1.10.6.tar.gz;
-    md5 = "76b4bbcda13eb7fa86b9af893c648202";
+    md5 = "130ee632bd4c677d898b0ef5efa67666";
   };
-  buildInputs = [gettext];
+  buildInputs = [gettext libtool automake autoconf];
 }
