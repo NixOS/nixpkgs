4a8b316cf6f14b5e1f7e6bee04070b685c69881b
Armijn Hemel <armijn@gpl-violations.org>
2006-08-27 22:31:35 +0000

    * show installation instructions at install time * update manual to reflect new method better
    svn path=/nixu/trunk/; revision=6285

--- login.sh
+++ login.sh
@@ -8,6 +8,18 @@ export PATH=@bash@/bin:@coreutilsdiet@/bin:@coreutils@/bin:@findutils@/bin:@util
 
 echo
 echo "=== Welcome to Nix! ==="
+echo "NixOS Installation instructions"
+echo ""
+echo "* edit the file called 'disklayout' (vi is provided) and provide"
+echo "  the following name=value pairs:"
+echo "  * INSTALLDEVICE (root device, for example /dev/hda1)"
+echo "  * SWAP (swap device, for example /dev/hda2)"
+echo "  * TARGETDRIVE (target drive to install grub, for example /dev/hda)"
+echo "* run: sh fill-disk.sh"
+echo ""
+echo ""
+echo ""
+echo ""
 
 export HOME=/
 cd $HOME
