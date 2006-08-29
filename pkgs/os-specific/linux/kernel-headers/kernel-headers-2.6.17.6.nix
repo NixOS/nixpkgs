a6409626a4266c9fd6045b25032049d5ca164782
Armijn Hemel <armijn@gpl-violations.org>
2006-08-29 18:01:48 +0000

    fix download location
    svn path=/nixpkgs/trunk/; revision=6312

--- pkgs/os-specific/linux/kernel-headers/kernel-headers-2.6.17.6.nix
+++ pkgs/os-specific/linux/kernel-headers/kernel-headers-2.6.17.6.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation {
   name = "linux-headers-2.6.17.6";
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/os/Linux/system/kernel/v2.6/linux-2.6.17.6.tar.bz2;
+    url = ftp://ftp.nl.kernel.org/pub/linux/kernel/v2.6/linux-2.6.17.6.tar.bz2;
     md5 = "5013fbe6049e32675187c203aef92218";
   };
 }
