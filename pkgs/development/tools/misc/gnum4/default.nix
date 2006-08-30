39eb66f644c649b107c8738c24ca981482c3ddfc
Armijn Hemel <armijn@gpl-violations.org>
2006-08-30 17:05:47 +0000

    set default to 1.4.6 (was 1.4.3)
    svn path=/nixpkgs/trunk/; revision=6378

--- pkgs/development/tools/misc/gnum4/default.nix
+++ pkgs/development/tools/misc/gnum4/default.nix
@@ -1,8 +1,1 @@
-{stdenv, fetchurl}:
-stdenv.mkDerivation {
-  name = "gnum4-1.4.3";
-  src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/m4-1.4.3.tar.bz2;
-    md5 = "1f7d7eba70a0525c44c2edc3998925c7";
-  };
-}
+import ./m4-1.4.6.nix
