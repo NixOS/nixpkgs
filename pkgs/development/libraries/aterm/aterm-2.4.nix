61391a87217de91cb32423cc4d74a5cc5939bd8d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-08-30 17:43:08 +0000

    * Grmbl.
    svn path=/nixpkgs/trunk/; revision=6382

--- pkgs/development/libraries/aterm/aterm-2.4.nix
+++ pkgs/development/libraries/aterm/aterm-2.4.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "aterm-2.4.2-test";
+  name = "aterm-2.4.2";
   src = fetchurl {
     url = http://nix.cs.uu.nl/dist/tarballs/aterm-2.4.2.tar.gz;
     md5 = "18617081dd112d85e6c4b1b552628114";
