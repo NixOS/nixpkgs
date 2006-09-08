0e0bc82f769b0ac660bf13b1c768db9ae4c5f9a0
Armijn Hemel <armijn@gpl-violations.org>
2006-09-08 11:58:52 +0000

    update nix-unstable to the latest successful build from teh buildfarm. Mooh!
    svn path=/nixpkgs/trunk/; revision=6475

--- pkgs/misc/nix-unstable/default.nix
+++ pkgs/misc/nix-unstable/default.nix
@@ -8,11 +8,11 @@ assert aterm != null && bdb != null && perl != null;
 # assert aterm.version >= 2.0
 
 stdenv.mkDerivation {
-  name = "nix-0.10pre6453";
+  name = "nix-0.10pre6474";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/nix/nix-0.10pre6453/nix-0.10pre6453.tar.bz2;
-    md5 = "0ed14840864bfb4d68f78b7717a406e3";
+    url = http://nix.cs.uu.nl/dist/nix/nix-0.10pre6474/nix-0.10pre6474.tar.bz2;
+    md5 = "aed25ae83ada6012a3d924d3747b4c23";
   };
   buildInputs = [aterm bdb perl curl];
   inherit storeDir stateDir aterm bdb;
