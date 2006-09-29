4e2994a237c7ab25eb4770e9bbed19db1b78e0d2
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-09-29 21:17:10 +0000

    * OpenSSL 0.9.8d.
    svn path=/nixpkgs/trunk/; revision=6629

--- pkgs/development/libraries/openssl/default.nix
+++ pkgs/development/libraries/openssl/default.nix
@@ -1,1 +1,9 @@
-import ./openssl-0.9.8b.nix
+{stdenv, fetchurl, perl}: stdenv.mkDerivation {
+  name = "openssl-0.9.8d";
+  builder = ./builder.sh;
+  src = fetchurl {
+    url = ftp://ftp.nluug.nl/pub/security/openssl/openssl-0.9.8d.tar.gz;
+    sha1 = "4136fba00303a3d319d2052bfa8e1f09a2e12fc2";
+  };
+  buildInputs = [perl];
+}
