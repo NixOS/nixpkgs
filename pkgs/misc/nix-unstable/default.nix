0f68b0c9e1a8e3727af1928d39629384ad8ca842
Armijn Hemel <armijn@gpl-violations.org>
2006-10-01 14:13:08 +0000

    new unstable Nix
    svn path=/nixpkgs/trunk/; revision=6630

--- pkgs/misc/nix-unstable/default.nix
+++ pkgs/misc/nix-unstable/default.nix
@@ -8,11 +8,11 @@ assert aterm != null && bdb != null && perl != null;
 # assert aterm.version >= 2.0
 
 stdenv.mkDerivation {
-  name = "nix-0.10pre6613";
+  name = "nix-0.10pre6619";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/nix/nix-0.10pre6613/nix-0.10pre6613.tar.bz2;
-    md5 = "1214f921fe971b7526bb710e75dbb8c4";
+    url = http://nix.cs.uu.nl/dist/nix/nix-0.10pre6619/nix-0.10pre6619.tar.bz2;
+    md5 = "551a2ab48b87c0e7d95adc69a8f57e4e";
   };
   buildInputs = [aterm bdb perl curl];
   inherit storeDir stateDir aterm bdb;
