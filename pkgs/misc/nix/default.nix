22dbea916126bf6698529056fa6ac3ffdb2fe2a4
Armijn Hemel <armijn@gpl-violations.org>
2006-10-08 22:04:44 +0000

    upgrade to 0.10
    svn path=/nixpkgs/trunk/; revision=6679

--- pkgs/misc/nix/default.nix
+++ pkgs/misc/nix/default.nix
@@ -8,13 +8,12 @@ assert aterm != null && bdb != null && perl != null;
 # assert aterm.version >= 2.0
 
 stdenv.mkDerivation {
-  name = "nix-0.9.2";
+  name = "nix-0.10";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/nix/nix-0.9.2/nix-0.9.2.tar.bz2;
-    md5 = "fc3a24f72760e357ac29935b2aebce0b";
+    url = http://nix.cs.uu.nl/dist/nix/nix-0.10/nix-0.10.tar.bz2;
+    md5 = "2e5db8be894b28873b5bb1b25194955f";
   };
   buildInputs = [aterm bdb perl curl];
-  patches = [./nix-profile-0.9.2.patch];
   inherit storeDir stateDir aterm bdb;
 }
