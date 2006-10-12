8c0c0b2ad8d98a0a3c38d04d9db75a33902726a1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-10-12 15:59:15 +0000

    * If the file already exists in dist/tarballs, check that the hashes match.
    svn path=/nixpkgs/trunk/; revision=6719

--- maintainers/scripts/evacuate-urls.sh
+++ maintainers/scripts/evacuate-urls.sh
@@ -6,27 +6,32 @@ find . -name "*.nix" | while read fn; do
 
         if oldURL=$(echo "$line" | sed 's^url = \(.*\);^\1^'); then
 
-            if ! echo "$oldURL" | grep -q -E "www.cs.uu.nl|nix.cs.uu.nl|.stratego-language.org|java.sun.com|ut2004|linuxq3a|RealPlayer|Adbe|belastingdienst|microsoft|armijn/.nix"; then
+            if ! echo "$oldURL" | grep -q -E "www.cs.uu.nl|nix.cs.uu.nl|.stratego-language.org|java.sun.com|ut2004|linuxq3a|RealPlayer|Adbe|belastingdienst|microsoft|armijn/.nix|sun.com|archive.eclipse.org"; then
                 base=$(basename $oldURL)
                 newURL="http://nix.cs.uu.nl/dist/tarballs/$base"
                 newPath="/data/webserver/dist/tarballs/$base"
                 echo "$fn: $oldURL -> $newURL"
 
+		isSafe=1
+
 		if test -e "$newPath"; then
-		    echo "WARNING: $newPath exists!"
-		else
-
-		    if test -n "$doMove"; then
-
-			if ! test -e "$newPath"; then
-			    curl --fail --location --max-redirs 20 "$oldURL" > "$newPath".tmp
-			    mv -f "$newPath".tmp "$newPath"
-			fi
-
-			sed "s^$oldURL^$newURL^" < "$fn" > "$fn".tmp
-			mv -f "$fn".tmp "$fn"
-
+		    hash=$(fgrep -A 1 "$oldURL" "$fn" | grep md5 | sed 's^.*md5 = \"\(.*\)\";.*^\1^')
+		    echo "HASH = $hash"
+		    if ! test "$(nix-hash --type md5 --flat "$newPath")" = "$hash"; then
+			echo "WARNING: $newPath exists and differs!"
+			isSafe=
 		    fi
+		fi
+
+		if test -n "$doMove" -a -n "$isSafe"; then
+
+		    if ! test -e "$newPath"; then
+			curl --disable-epsv --fail --location --max-redirs 20 "$oldURL" > "$newPath".tmp
+			mv -f "$newPath".tmp "$newPath"
+		    fi
+
+		    sed "s^$oldURL^$newURL^" < "$fn" > "$fn".tmp
+		    mv -f "$fn".tmp "$fn"
 
 		fi
 
