20d42ae673cf2a4432c89443e06d22feb9185e94
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-10-18 12:45:46 +0000

    * Fake Glibc's crti.o and crtn.o to make the linker happy.
    svn path=/nixpkgs/trunk/; revision=6760

--- pkgs/os-specific/linux/dietlibc/builder.sh
+++ pkgs/os-specific/linux/dietlibc/builder.sh
@@ -7,6 +7,11 @@ postInstall=postInstall
 postInstall() {
     (cd $out && ln -s lib-* lib)
     (cd $out/lib && ln -s start.o crt1.o)
+
+    # Fake crti.o and crtn.o.
+    touch empty.c
+    gcc -c empty.c -o $out/lib/crti.o
+    gcc -c empty.c -o $out/lib/crtn.o
 }
 
 genericBuild
