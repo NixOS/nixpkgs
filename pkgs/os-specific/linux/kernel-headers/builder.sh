ed5f6e55e8558a5543e9bb91294a2a8d1c255ae0
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-10-23 17:42:17 +0000

    * Support x86_64.
    svn path=/nixpkgs/trunk/; revision=6805

--- pkgs/os-specific/linux/kernel-headers/builder.sh
+++ pkgs/os-specific/linux/kernel-headers/builder.sh
@@ -11,15 +11,16 @@ buildPhase=buildPhase
 installPhase() {
     mkdir $out
     mkdir $out/include
+    cp -prvd include/linux include/asm-generic $out/include
     if test $system = "i686-linux"; then
-        cp -prvd include/linux include/asm-i386 include/asm-generic $out/include
-        cd $out/include
-        ln -s asm-i386 asm
+        platform=i386
+    elif test $system = "x86_64-linux"; then
+        platform=x86_64
     elif test $system = "powerpc-linux"; then
-        cp -prvd include/linux include/asm-ppc include/asm-generic $out/include
-        cd $out/include
-        ln -s asm-ppc asm
+        platform=ppw
     fi
+    cp -prvd include/asm-$platform $out/include
+    ln -s asm-$platform $out/include/asm
     echo -n > $out/include/linux/autoconf.h
 }
 
