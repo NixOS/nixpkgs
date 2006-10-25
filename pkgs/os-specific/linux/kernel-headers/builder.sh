26a11fbf545c27ded5335aefdb6ed8a4c7db8342
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-10-25 10:14:27 +0000

    * Fix a typo, and put the platform-specific header directory in the   Nix expression rather than the builder.
    svn path=/nixpkgs/trunk/; revision=6843

--- pkgs/os-specific/linux/kernel-headers/builder.sh
+++ pkgs/os-specific/linux/kernel-headers/builder.sh
@@ -12,13 +12,6 @@ installPhase() {
     mkdir $out
     mkdir $out/include
     cp -prvd include/linux include/asm-generic $out/include
-    if test $system = "i686-linux"; then
-        platform=i386
-    elif test $system = "x86_64-linux"; then
-        platform=x86_64
-    elif test $system = "powerpc-linux"; then
-        platform=ppw
-    fi
     cp -prvd include/asm-$platform $out/include
     ln -s asm-$platform $out/include/asm
     echo -n > $out/include/linux/autoconf.h
--- pkgs/os-specific/linux/kernel-headers/default.nix
+++ pkgs/os-specific/linux/kernel-headers/default.nix
@@ -9,4 +9,10 @@ stdenv.mkDerivation {
     url = ftp://ftp.nl.kernel.org/pub/linux/kernel/v2.6/linux-2.6.18.1.tar.bz2;
     md5 = "38f00633b02f07819d17bcd87d03eb3a";
   };
+
+  platform = 
+    if stdenv.system == "i686-linux" then "i386" else
+    if stdenv.system == "x86_64-linux" then "x86_64" else
+    if stdenv.system == "powerpc-linux" then "ppc" else
+    abort "don't know what the kernel include directory is called for this platform";
 }
