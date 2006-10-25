47d7e73abb11e7dfdf41615a39c386e280ab076f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-10-25 17:27:32 +0000

    * Real URLs.
    svn path=/nixpkgs/trunk/; revision=6853

--- pkgs/stdenv/linux/bootstrap/x86_64/default.nix
+++ pkgs/stdenv/linux/bootstrap/x86_64/default.nix
@@ -6,22 +6,22 @@
   tar = ./tar.bz2;
 
   staticToolsURL = {
-    url = file:///tmp/tarballs/static-tools.tar.bz2;
+    url = http://nix.cs.uu.nl/dist/tarballs/stdenv-linux/x86_64/r6842/static-tools.tar.bz2;
     sha1 = "46899b48cba2f7a53d6c283f4a3706c046900924";
   };
 
   binutilsURL = {
-    url = file:///tmp/tarballs/binutils.tar.bz2;
+    url = http://nix.cs.uu.nl/dist/tarballs/stdenv-linux/x86_64/r6842/binutils.tar.bz2;
     sha1 = "b76ff6427f6930059a548c507fda9860ab31affb";
   };
 
   gccURL = {
-    url = file:///tmp/tarballs/gcc.tar.bz2;
+    url = http://nix.cs.uu.nl/dist/tarballs/stdenv-linux/x86_64/r6842/gcc.tar.bz2;
     sha1 = "ba8e05b80f0fd779c96d597135961f028b4fabcf";
   };
 
   glibcURL = {
-    url = file:///tmp/tarballs/glibc.tar.bz2;
+    url = http://nix.cs.uu.nl/dist/tarballs/stdenv-linux/x86_64/r6842/glibc.tar.bz2;
     sha1 = "76774fdc2d73e78d6b63f53507feb01070628dbe";
   };
 }
