0e776c19d2f057670353384d79ba0df9631f67ab
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-10-26 22:55:04 +0000

    * Real URLs.
    svn path=/nixpkgs/trunk/; revision=6869

--- pkgs/stdenv/linux/bootstrap/powerpc/default.nix
+++ pkgs/stdenv/linux/bootstrap/powerpc/default.nix
@@ -6,22 +6,22 @@
   tar = ./tar.bz2;
 
   staticToolsURL = {
-    url = file:///tmp/tarballs/static-tools.tar.bz2;
+    url = http://nix.cs.uu.nl/dist/tarballs/stdenv-linux/powerpc/r6860/static-tools.tar.bz2;
     sha1 = "90ec30bbdac515e03c90b0909ee09a4cdcfe5214";
   };
 
   binutilsURL = {
-    url = file:///tmp/tarballs/binutils.tar.bz2;
+    url = http://nix.cs.uu.nl/dist/tarballs/stdenv-linux/powerpc/r6860/binutils.tar.bz2;
     sha1 = "577b256dcb5297a001acb8b49ce36e9c78ff8fc8";
   };
 
   gccURL = {
-    url = file:///tmp/tarballs/gcc.tar.bz2;
+    url = http://nix.cs.uu.nl/dist/tarballs/stdenv-linux/powerpc/r6860/gcc.tar.bz2;
     sha1 = "853d570c3419bddcf18d4340722880d2a80e2a3f";
   };
 
   glibcURL = {
-    url = file:///tmp/tarballs/glibc.tar.bz2;
+    url = http://nix.cs.uu.nl/dist/tarballs/stdenv-linux/powerpc/r6860/glibc.tar.bz2;
     sha1 = "d34e78fb4a0aa282318b1465e195bc2d4e6e7315";
   };
 }
