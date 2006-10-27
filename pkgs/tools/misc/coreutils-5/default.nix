0f7883d12fd46c2173fe80e28ab1a3bb484de0ae
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-10-27 19:53:23 +0000

    * Revive the old coreutils, since coreutils 6 is broken beyond repair   with dietlibc (it does some incredibly nasty hackery with the system   header files...).
    svn path=/nixpkgs/trunk/; revision=6878

---
+++ pkgs/tools/misc/coreutils-5/default.nix
@@ -0,0 +1,10 @@
+{stdenv, fetchurl}:
+
+stdenv.mkDerivation {
+  name = "coreutils-5.97";
+  src = fetchurl {
+    url = http://nix.cs.uu.nl/dist/tarballs/coreutils-5.97.tar.gz;
+    md5 = "bdec4b75c76ac9bf51b6dd1747d3b06e";
+  };
+  patches = [./dietlibc.patch];
+}
