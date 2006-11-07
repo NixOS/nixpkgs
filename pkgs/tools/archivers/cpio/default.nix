908b2bf0277959d19e16cc294f6786321687b4a8
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-11-07 10:48:08 +0000

    * Latest cpio; fixes a problem with symlinks.
    svn path=/nixpkgs/trunk/; revision=6965

--- pkgs/tools/archivers/cpio/default.nix
+++ pkgs/tools/archivers/cpio/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "cpio-2.6";
+  name = "cpio-2.7";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/cpio-2.6.tar.bz2;
-    md5 = "25e0e8725bc60ed3460c9cde92752674";
+    url = http://ftp.gnu.org/gnu/cpio/cpio-2.7.tar.bz2;
+    md5 = "69ad6cb3d288aafe5f969f68d9fd0fb7";
   };
 }
