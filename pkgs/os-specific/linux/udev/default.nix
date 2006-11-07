986ec9c8a12cef0c38d8f39edd1117e74d8b0f45
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-11-07 21:13:57 +0000

    * Fix some paths.
    svn path=/nixpkgs/trunk/; revision=6974

--- pkgs/os-specific/linux/udev/default.nix
+++ pkgs/os-specific/linux/udev/default.nix
@@ -6,5 +6,6 @@ stdenv.mkDerivation {
     url = http://www.us.kernel.org/pub/linux/utils/kernel/hotplug/udev-103.tar.bz2;
     md5 = "7360ce47b5ec3f9fa71f18071ecb8b97";
   };
-  patches = [./udev-091-installpath.patch];
+  preBuild = "makeFlagsArray=(etcdir=$out/etc sbindir=$out/sbin usrbindir=$out/bin usrsbindir=$out/sbin mandir=$out/share/man)";
+  preInstall = "installFlagsArray=(\"\${makeFlagsArray[@]}\" udevdir=dummy)";
 }
