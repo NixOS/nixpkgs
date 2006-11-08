aca43b766b13e0b0fb630c7cb800f543234a6db0
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-11-08 16:11:51 +0000

    * Sheesh, Subversion is picky.
    svn path=/nixpkgs/trunk/; revision=6981

--- pkgs/development/libraries/neon/default.nix
+++ pkgs/development/libraries/neon/default.nix
@@ -7,11 +7,11 @@ assert compressionSupport -> zlib != null;
 assert sslSupport -> openssl != null;
 
 (stdenv.mkDerivation {
-  name = "neon-0.26.2";
+  name = "neon-0.26.1";
   
   src = fetchurl {
-    url = http://www.webdav.org/neon/neon-0.26.2.tar.gz;
-    md5 = "65ff7f6d962305b984a60d6ec5b189b5";
+    url = http://www.webdav.org/neon/neon-0.26.1.tar.gz;
+    md5 = "3bb7a82bddfc1c56d2f9dba849aecd1f";
   };
   
   buildInputs = [libxml2] ++ (if compressionSupport then [zlib] else []);
