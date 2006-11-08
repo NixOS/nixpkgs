36b1473e09f52ecae7e32c0fa2f21dcc5bfaa237
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-11-08 09:26:18 +0000

    * Allow the result to be stored somewhere else than $out.
    svn path=/nixpkgs/trunk/; revision=6978

--- pkgs/build-support/substitute/generic-substituter.sh
+++ pkgs/build-support/substitute/generic-substituter.sh
@@ -3,14 +3,20 @@ source $substitute
 
 args=
 
+target=$out
+if test -n "$dir"; then
+    target=$out/$dir/$name
+    ensureDir $out/$dir
+fi
+
 # Select all environment variables that start with a lowercase character.
 for envVar in $(env | sed "s/^[^a-z].*//" | sed "s/^\([^=]*\)=.*/\1/"); do
     echo "$envVar -> ${!envVar}"
     args="$args --subst-var $envVar"
 done
 
-substitute $src $out $args
+substitute $src $target $args
 
 if test -n "$isExecutable"; then
-    chmod +x $out
+    chmod +x $target
 fi
