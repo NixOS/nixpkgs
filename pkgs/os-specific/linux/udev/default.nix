5474c6518f963d31ebcc61f77e8d44eebcdf88af
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-11-13 10:33:29 +0000

    * Impurity in udev: don't call /usr/bin/install.
    svn path=/nixpkgs/trunk/; revision=7013

--- pkgs/os-specific/linux/udev/default.nix
+++ pkgs/os-specific/linux/udev/default.nix
@@ -6,6 +6,6 @@ stdenv.mkDerivation {
     url = http://www.us.kernel.org/pub/linux/utils/kernel/hotplug/udev-103.tar.bz2;
     md5 = "7360ce47b5ec3f9fa71f18071ecb8b97";
   };
-  preBuild = "makeFlagsArray=(etcdir=$out/etc sbindir=$out/sbin usrbindir=$out/bin usrsbindir=$out/sbin mandir=$out/share/man)";
+  preBuild = "makeFlagsArray=(etcdir=$out/etc sbindir=$out/sbin usrbindir=$out/bin usrsbindir=$out/sbin mandir=$out/share/man INSTALL='install -c')";
   preInstall = "installFlagsArray=(\"\${makeFlagsArray[@]}\" udevdir=dummy)";
 }
