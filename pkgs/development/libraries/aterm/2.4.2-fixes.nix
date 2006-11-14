82888b7a201e0303f20863b274dc5e0512ad6042
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-11-14 18:50:17 +0000

    * New version that integrates the MinGW patch.
    svn path=/nixpkgs/trunk/; revision=7034

--- pkgs/development/libraries/aterm/2.4.2-fixes.nix
+++ pkgs/development/libraries/aterm/2.4.2-fixes.nix
@@ -4,10 +4,8 @@ stdenv.mkDerivation {
   name = "aterm-2.4.2-fixes";
   src = fetchurl {
     url = http://losser.st-lab.cs.uu.nl/~eelco/dist/aterm-2.4.2-fixes.tar.bz2;
-    md5 = "0622feaaa97c1e411e16f480f64e75fa";
+    md5 = "9ad8ed141d3e66a7689817789431c0cd";
   };
-  patches = 
-    (if stdenv ? isMinGW && stdenv.isMinGW then [./mingw-asm.patch] else []);
   meta = {
     homepage = http://www.cwi.nl/htbin/sen1/twiki/bin/view/SEN1/ATerm;
     license = "LGPL";
