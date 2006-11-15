3803cfdff287599922a3f12efd58dfb7bb1ad79e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-11-15 15:25:32 +0000

    * lhs2TeX updated to 1.11.
    svn path=/nixpkgs/trunk/; revision=7055

--- pkgs/tools/typesetting/lhs2tex/default.nix
+++ pkgs/tools/typesetting/lhs2tex/default.nix
@@ -3,12 +3,12 @@
 assert tetex == polytable.tetex;
 
 stdenv.mkDerivation {
-  name = "lhs2tex-1.10pre";
+  name = "lhs2tex-1.11";
   builder = ./builder.sh;
   
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/lhs2tex-1.10pre.tar.bz2;
-    md5 = "4fb875cdc0ba8daacc18b97f76aab4bf";
+    url = http://www.informatik.uni-bonn.de/~loeh/lhs2tex/lhs2tex-1.11.tar.bz2;
+    md5 = "16fb6a150bc1ab5e22c66f52f11cec54";
   };
 
   buildInputs = [tetex ghc];
