298f1ffad6fe912f49a3f9d0790aa090fdcdd172
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-11-17 11:45:56 +0000

    * Revert r7041.  I think we're better off with gensplash than with   splashy.
    svn path=/nixpkgs/trunk/; revision=7059

--- pkgs/development/libraries/directfb/default.nix
+++ pkgs/development/libraries/directfb/default.nix
@@ -1,6 +1,4 @@
-{ stdenv, fetchurl, perl
-, enableStaticLibraries ? true
-}:
+{stdenv, fetchurl, perl}:
 
 stdenv.mkDerivation {
   name = "directfb-1.0.0-pre-rc2";
@@ -9,5 +7,4 @@ stdenv.mkDerivation {
     md5 = "1996c8e90075b1177b847cd594122401";
   };
   buildInputs = [perl];
-  configureFlags = "${if enableStaticLibraries then "--enable-static" else ""}";
 }
