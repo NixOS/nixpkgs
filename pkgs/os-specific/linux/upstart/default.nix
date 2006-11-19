518bce117f3f5b6dffff543f898613205cf83853
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-11-19 15:58:58 +0000

    * Turn on the compatibility tools.
    svn path=/nixpkgs/trunk/; revision=7072

--- pkgs/os-specific/linux/upstart/default.nix
+++ pkgs/os-specific/linux/upstart/default.nix
@@ -6,4 +6,5 @@ stdenv.mkDerivation {
     url = http://upstart.ubuntu.com/download/upstart-0.3.0.tar.bz2;
     md5 = "269046f41c6418225306280044a799eb";
   };
+  configureFlags = "--enable-compat";
 }
