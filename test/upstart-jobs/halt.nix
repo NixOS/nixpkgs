54f6e4fc713ff2fbddf66ae0df578a5eeb01300c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-11-20 21:19:34 +0000

    * Unmount file systems.
    svn path=/nixu/trunk/; revision=7084

--- test/upstart-jobs/halt.nix
+++ test/upstart-jobs/halt.nix
@@ -15,8 +15,20 @@ script
     echo \"<<< SYSTEM SHUTDOWN >>>\"
     echo \"\"
 
+    # Do an initial sync just in case.
+    sync || true
+
+    # Unmount file systems.
+    umount -n -a || true
+
+    # Remount / read-only
+    mount -n -o remount,ro /dontcare / || true
+
+    # Final sync.
+    sync || true
+
     # Right now all events above power off the system.
-    exec halt -f -p
+    exec halt -f
 end script
   ";
   
