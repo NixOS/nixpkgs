89ba1cba24202f2ca65ae482359b39aee6c8ba97
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-11-28 15:45:41 +0000

    * We don't need groups from shadow.
    svn path=/nixpkgs/trunk/; revision=7159

--- pkgs/os-specific/linux/shadow/default.nix
+++ pkgs/os-specific/linux/shadow/default.nix
@@ -7,5 +7,8 @@ stdenv.mkDerivation {
     url = http://losser.labs.cs.uu.nl/~armijn/.nix/shadow-4.0.16.tar.bz2;
     md5 = "1d91f7479143d1d705b94180c0d4874b";
   };
+  
   configureFlags = "--with-selinux=no";
+
+  postInstall = "rm $out/bin/groups"; # coreutils provides `groups'
 }
