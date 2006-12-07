3613f5568cdaf6175322ed749a80fb846080c7aa
Armijn Hemel <armijn@gpl-violations.org>
2006-12-07 19:31:23 +0000

    1.1 -> 1.2
    svn path=/nixpkgs/trunk/; revision=7268

--- pkgs/tools/archivers/cabextract/default.nix
+++ pkgs/tools/archivers/cabextract/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "cabextract-1.1";
+  name = "cabextract-1.2";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/cabextract-1.1.tar.gz;
-    md5 = "f4b729c0be7d288660f4fc167de199a1";
+    url = http://www.kyz.uklinux.net/downloads/cabextract-1.2.tar.gz;
+    md5 = "dc421a690648b503265c82ade84e143e";
   };
 }
