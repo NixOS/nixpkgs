cca1ba037283a1f3b4196db19dbd6bae5f500bb4
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-12-09 17:27:59 +0000

    * Latest Flash beta.
    svn path=/nixpkgs/trunk/; revision=7292

--- pkgs/applications/networking/browsers/mozilla-plugins/flashplayer-9-beta/default.nix
+++ pkgs/applications/networking/browsers/mozilla-plugins/flashplayer-9-beta/default.nix
@@ -1,12 +1,12 @@
 {stdenv, fetchurl, zlib}:
 
 (stdenv.mkDerivation {
-  name = "flashplayer-9.0.21.55-pre-beta-101806";
+  name = "flashplayer-9.0.21.78-pre-beta-112006";
 
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://download.macromedia.com/pub/labs/flashplayer9_update/FP9_plugin_beta_101806.tar.gz;
-    md5 = "0b234c5d0eaf254ef8af364fb9ed97f2";
+    url = http://download.macromedia.com/pub/labs/flashplayer9_update/FP9_plugin_beta_112006.tar.gz;
+    md5 = "3ab408f85ae6d8180cc913edf97bf3eb";
   };
 
   inherit zlib;
