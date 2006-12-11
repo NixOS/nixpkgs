b80769d5aed53e747d8d5ec169fbc4df37e86882
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-12-11 02:55:28 +0000

    * Hm, "set" doesn't seem to do the right thing.
    svn path=/nixos/trunk/; revision=7308

--- upstart-jobs/nix-daemon.nix
+++ upstart-jobs/nix-daemon.nix
@@ -6,8 +6,10 @@
   job = "
     start on startup
     stop on shutdown
-    set NIX_CONF_DIR=/nix/etc/nix
-    respawn ${nix}/bin/nix-worker --daemon > /dev/null 2>&1
+    script
+      export NIX_CONF_DIR=/nix/etc/nix
+      exec ${nix}/bin/nix-worker --daemon > /dev/null 2>&1
+    end script
   ";
 
 }
