a6ca6b0612861e99ad3a0faf2d43048f9ff25f15
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-12-13 21:15:31 +0000

    * Doh!
    svn path=/nixpkgs/trunk/; revision=7342

--- pkgs/development/libraries/aterm/2.4.2-fixes.nix
+++ pkgs/development/libraries/aterm/2.4.2-fixes.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "aterm-2.4.2-fixes-2";
+  name = "aterm-2.4.2-fixes";
   src = fetchurl {
     url = http://losser.st-lab.cs.uu.nl/~eelco/dist/aterm-2.4.2-fixes.tar.bz2;
     md5 = "9ad8ed141d3e66a7689817789431c0cd";
@@ -11,5 +11,4 @@ stdenv.mkDerivation {
     license = "LGPL";
     description = "Library for manipulation of term data structures in C";
   };
-#  unpackPhase = "while true; do sleep 1; echo y; done";
 }
