84f1e01646220cc1cf790da3456fea78f1e1f093
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-12-18 15:52:43 +0000

    * Doh.
    svn path=/nixos/trunk/; revision=7384

--- upstart-jobs/sshd.nix
+++ upstart-jobs/sshd.nix
@@ -18,7 +18,7 @@ start script
         ${openssh}/bin/ssh-keygen -t dsa -b 1024 -f /etc/ssh/ssh_host_dsa_key -N ''
     fi
 
-    if ! {glibc}/bin/getent passwd sshd > /dev/null; then
+    if ! ${glibc}/bin/getent passwd sshd > /dev/null; then
         ${pwdutils}/sbin/useradd -g nogroup -d /var/empty -s /noshell \\
             -c 'SSH privilege separation user' sshd
     fi
