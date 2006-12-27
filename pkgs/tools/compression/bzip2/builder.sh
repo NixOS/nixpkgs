f6d923febc594c7a1fc3295e01972b049569e292
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-12-27 17:58:32 +0000

    * Merge builder-cygwin.sh.
    svn path=/nixpkgs/trunk/; revision=7497

--- pkgs/tools/compression/bzip2/builder.sh
+++ pkgs/tools/compression/bzip2/builder.sh
@@ -18,7 +18,7 @@ fi
 
 postInstall=postInstall
 postInstall() {
-    rm $out/bin/bunzip2 $out/bin/bzcat
+    rm $out/bin/bunzip2* $out/bin/bzcat*
     ln -s bzip2 $out/bin/bunzip2
     ln -s bzip2 $out/bin/bzcat
 }
