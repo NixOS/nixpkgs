5d72829c18e50609cb92668be221a6b430a898ea
Eelco Dolstra <eelco.dolstra@logicblox.com>
2006-12-27 20:40:25 +0000

    * Doh.
    svn path=/nixpkgs/trunk/; revision=7505

---
+++ pkgs/applications/graphics/xfig/builder.sh
@@ -0,0 +1,25 @@
+source $stdenv/setup
+source $makeWrapper
+
+makeFlags="XAWLIB=-lXaw3d BINDIR=$out/bin XAPPLOADDIR=$out/etc/X11/app-defaults LIBDIR=$out/lib/X11 XFIGDOCDIR=$out/share/doc/xfig MANPATH=$out/man"
+
+preBuild=preBuild
+preBuild() {
+    echo "#define XAW3D" >> Imakefile.tmp
+    echo "#define XAW3D1_5E" >> Imakefile.tmp
+    cat Imakefile >> Imakefile.tmp
+    mv Imakefile.tmp Imakefile
+    xmkmf
+}
+
+installCommand=myInstall
+myInstall() {
+    make install.all $makeFlags
+
+    mv $out/bin/xfig $out/bin/.xfig
+
+    makeWrapper $out/bin/.xfig $out/bin/xfig \
+        --set XAPPLRESDIR $out/etc/X11/app-defaults
+}
+
+genericBuild
