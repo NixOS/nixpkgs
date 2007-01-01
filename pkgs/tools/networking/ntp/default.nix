6b8b7566fbdc2ac682e06db0da1b294040b00600
Armijn Hemel <armijn@gpl-violations.org>
2007-01-01 16:16:54 +0000

    location moved
    svn path=/nixpkgs/trunk/; revision=7527

--- pkgs/tools/networking/ntp/default.nix
+++ pkgs/tools/networking/ntp/default.nix
@@ -5,7 +5,7 @@ assert stdenv.isLinux -> libcap != null;
 stdenv.mkDerivation {
   name = "ntp-4.2.2p4";
   src = fetchurl {
-    url = http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2.2p4.tar.gz;
+    url = http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/ntp-4.2.2p4.tar.gz;
     md5 = "916fe57525f8327f340b203f129088fa";
   };
   configureFlags = "
