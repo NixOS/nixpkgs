403d766a59247810252f89ee69e7d9ef6fe7967b
Armijn Hemel <armijn@gpl-violations.org>
2007-01-01 18:49:23 +0000

    new version
    svn path=/nixpkgs/trunk/; revision=7528

--- pkgs/tools/networking/ntp/default.nix
+++ pkgs/tools/networking/ntp/default.nix
@@ -3,10 +3,10 @@
 assert stdenv.isLinux -> libcap != null;
  
 stdenv.mkDerivation {
-  name = "ntp-4.2.2p4";
+  name = "ntp-4.2.4";
   src = fetchurl {
-    url = http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/ntp-4.2.2p4.tar.gz;
-    md5 = "916fe57525f8327f340b203f129088fa";
+    url = http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2.4.tar.gz;
+    md5 = "eb9147d26cbe18bd8fbec07f1df55aef";
   };
   configureFlags = "
     --without-crypto
