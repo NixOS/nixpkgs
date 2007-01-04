b6b5bafa221ee9eb8a4a22929afd1845ee7e660b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-01-04 10:29:40 +0000

    * Refactoring.
    svn path=/nixos/trunk/; revision=7533

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 if test -z "$NIXOS_CONFIG"; then
     NIXOS_CONFIG=/etc/nixos/configuration.nix
 fi
-nix-build configuration/system.nix \
+nix-build system/system.nix \
     --arg configuration "import $NIXOS_CONFIG" \
     -A system -K -k
 ./result/bin/switch-to-configuration test
--- upgrade.sh
+++ upgrade.sh
@@ -2,7 +2,7 @@
 if test -z "$NIXOS_CONFIG"; then
     NIXOS_CONFIG=/etc/nixos/configuration.nix
 fi
-nix-env -p /nix/var/nix/profiles/system -f configuration/system.nix \
+nix-env -p /nix/var/nix/profiles/system -f system/system.nix \
     --arg configuration "import $NIXOS_CONFIG" \
     --set -A system
 /nix/var/nix/profiles/system/bin/switch-to-configuration switch
