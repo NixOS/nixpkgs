b96c1601a18541ca6bec25002ad4835821e6ce95
Armijn Hemel <armijn@gpl-violations.org>
2007-01-06 22:07:50 +0000

    update to 1.8.1
    svn path=/nixpkgs/trunk/; revision=7545

--- pkgs/tools/misc/parted/default.nix
+++ pkgs/tools/misc/parted/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, e2fsprogs, ncurses, readline}:
 
 stdenv.mkDerivation {
-  name = "parted-1.7.1";
+  name = "parted-1.8.1";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/parted-1.7.1.tar.bz2;
-    md5 = "baa6771273c8362d735086d52a0d6efe";
+    url = ftp://ftp.nluug.nl/pub/gnu/parted/parted-1.8.1.tar.bz2;
+    md5 = "c430b38fd5f3c7530e2c3a3bdf605a29";
   };
   buildInputs = [e2fsprogs ncurses readline];
   patches = [./parted-trailingcomma.patch];
