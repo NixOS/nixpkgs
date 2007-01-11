7bc63c5312f7505a5a881b69b9f67a0ab2593571
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-01-11 12:41:35 +0000

    * Apache 2.2.4.
    svn path=/nixpkgs/trunk/; revision=7632

--- pkgs/servers/http/apache-httpd/default.nix
+++ pkgs/servers/http/apache-httpd/default.nix
@@ -7,12 +7,12 @@ assert db4Support -> db4 != null;
 assert expat != null && perl != null;
 
 stdenv.mkDerivation {
-  name = "apache-httpd-2.2.3";
+  name = "apache-httpd-2.2.4";
 
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/httpd-2.2.3.tar.bz2;
-    md5 = "887bf4a85505e97babe1d90635361c0d";
+    url = http://archive.apache.org/dist/httpd/httpd-2.2.4.tar.bz2;
+    md5 = "536c86c7041515a25dd8bad3611da9a3";
   };
 
   inherit sslSupport db4Support;
