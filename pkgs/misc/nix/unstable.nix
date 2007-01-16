617abe91e8a5ded59e075cd1f0cab7eaac2e2484
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-01-16 14:11:20 +0000

    * Latest unstable Nix.
    svn path=/nixpkgs/trunk/; revision=7695

--- pkgs/misc/nix/unstable.nix
+++ pkgs/misc/nix/unstable.nix
@@ -4,11 +4,11 @@
 }:
 
 stdenv.mkDerivation {
-  name = "nix-0.11pre7667";
+  name = "nix-0.11pre7681";
   
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/nix/nix-0.11pre7667/nix-0.11pre7667.tar.bz2;
-    md5 = "f1228dbf6dc6a1fce41fb881850a5a82";
+    url = http://nix.cs.uu.nl/dist/nix/nix-0.11pre7681/nix-0.11pre7681.tar.bz2;
+    md5 = "e62140ecdf7ed7203341004c4704e310";
   };
   
   buildInputs = [perl curl];
