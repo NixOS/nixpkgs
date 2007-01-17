b53d7f2c6146d8a6e0243f3d72a91029fbe6e832
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-01-17 14:36:24 +0000

    * Create install-source.nix.
    svn path=/nixos/trunk/; revision=7707

--- checkout-nixos.sh
+++ checkout-nixos.sh
@@ -5,4 +5,5 @@ nix-env -i subversion
 cd /etc/nixos
 svn co https://svn.cs.uu.nl:12443/repos/trace/nixos/trunk nixos
 svn co https://svn.cs.uu.nl:12443/repos/trace/nixpkgs/trunk nixpkgs
-ln -s ../nixpkgs/pkgs nixos/pkgs
+ln -sf ../nixpkgs/pkgs nixos/pkgs
+ln -sf nixpkgs/pkgs/top-level/all-packages.nix install-source.nix
