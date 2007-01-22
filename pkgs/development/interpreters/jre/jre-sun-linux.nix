e67a0ca2130219a3f61f8ae2224b08fb2cfcd688
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-01-22 15:17:13 +0000

    * Let JRE provide its Mozilla plugin.
    svn path=/nixpkgs/trunk/; revision=7746

--- pkgs/development/interpreters/jre/jre-sun-linux.nix
+++ pkgs/development/interpreters/jre/jre-sun-linux.nix
@@ -2,12 +2,14 @@
 
 assert stdenv.system == "i686-linux";
 
-stdenv.mkDerivation {
-  name    = "jre-1.5.0";
+(stdenv.mkDerivation {
+  name = "jre-1.5.0";
   version = "jre1.5.0_07";
   builder = ./builder.sh;
   src = fetchurl {
-      url = http://jdl.sun.com/webapps/download/AutoDL?BundleId=10542;
-      md5 = "e2ad1c9e47f3e34d1efae059b9e2a2d9";
-    };
-}
+    url = http://jdl.sun.com/webapps/download/AutoDL?BundleId=10542;
+    md5 = "e2ad1c9e47f3e34d1efae059b9e2a2d9";
+  };
+})
+
+// {mozillaPlugin = "/plugin/i386/ns7";}
