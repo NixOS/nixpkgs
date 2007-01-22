c219e77af77d4ec66d65d1962e19e567c4b7507d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-01-22 19:14:50 +0000

    * Syslinux updated to 3.31. * Some uber-ugly hacks to get it to build on x86_64 with a non-32-bit   enabled GCC.
    svn path=/nixpkgs/trunk/; revision=7756

--- pkgs/os-specific/linux/syslinux/default.nix
+++ pkgs/os-specific/linux/syslinux/default.nix
@@ -1,12 +1,23 @@
 {stdenv, fetchurl, nasm, perl}:
 
 stdenv.mkDerivation {
-  name = "syslinux-3.20";
-  builder = ./builder.sh;
+  name = "syslinux-3.31";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/syslinux-3.20.tar.bz2;
-    md5 = "0701e0de1de6d31bdd889384b041e5b7";
+    url = http://www.nl.kernel.org/pub/linux/utils/boot/syslinux/syslinux-3.31.tar.bz2;
+    sha256 = "1w0hw28w97mj27h4w64wn9bi24zcff1i8ifcxnbh4iing1pcmi2p";
   };
- buildInputs = [nasm perl];
- patches = [./syslinux-3.20-installpath.patch];
+  buildInputs = [nasm perl];
+
+  preBuild = "
+    makeFlagsArray=(BINDIR=$out/bin SBINDIR=$out/sbin LIBDIR=$out/lib INCDIR=$out/include)
+
+    # Really dirty hack to get syslinux to build without needing a GCC
+    # with 32-bit libraries and headers.
+    if test \"$system\" = x86_64-linux; then
+        substituteInPlace memdisk/Makefile \\
+            --replace 'all: memdisk e820test' 'all: memdisk'
+        mkdir gnu
+        touch gnu/stubs-32.h
+    fi
+  ";
 }
