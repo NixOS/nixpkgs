66706c592e2ad7ef8696817815b4e7eb335ccda3
Mart Kolthof <skolthof@cs.uu.nl>
2007-01-23 13:29:11 +0000

    Altered download location to sourceforge.
    svn path=/nixpkgs/trunk/; revision=7769

--- pkgs/development/libraries/cil-aterm/default.nix
+++ pkgs/development/libraries/cil-aterm/default.nix
@@ -1,9 +1,9 @@
-{ stdenv, fetchurl, ocaml, perl }:
+ls{ stdenv, fetchurl, ocaml, perl }:
 
 stdenv.mkDerivation {
   name    = "cil-aterm-1.3.4";
   src     = fetchurl {
-		url = http://manju.cs.berkeley.edu/cil/distrib/cil-1.3.4.tar.gz;
+		url = http://surfnet.dl.sourceforge.net/sourceforge/cil/cil-1.3.4.tar.gz;
                 md5 = "a7fa54f19844a20562efd37f67c391da";
             };
   patches = [./cil-aterm-1.3.4.patch];
