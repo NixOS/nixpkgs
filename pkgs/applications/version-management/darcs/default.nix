6f0ba86b9701455b7aa3b30e8653cc0c0ee751e1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-01-24 14:27:10 +0000

    * Darcs 1.0.9rc2.
    svn path=/nixpkgs/trunk/; revision=7791

--- pkgs/applications/version-management/darcs/default.nix
+++ pkgs/applications/version-management/darcs/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, ghc, zlib, ncurses, curl}:
 
 stdenv.mkDerivation {
-  name = "darcs-1.0.5";
+  name = "darcs-1.0.9rc2";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/darcs-1.0.5.tar.gz;
-    md5 = "9a9a4f84ed5b6258f7ab321713adf20b";
+    url = http://abridgegame.org/darcs/darcs-1.0.9rc2.tar.gz;
+    sha256 = "1as9xc9x7245nf1qw4smfwny08l04lmrr7cag9a32qglssxp528c";
   };
   buildInputs = [ghc zlib ncurses curl];
 
