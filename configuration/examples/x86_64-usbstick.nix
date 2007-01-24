a1f512ede348aed01d27053253a1c0a25de9cafc
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-01-24 11:50:38 +0000

    * Another example: NixOS on x86_64 on a USB stick.
    svn path=/nixos/trunk/; revision=7787

---
+++ configuration/examples/x86_64-usbstick.nix
@@ -0,0 +1,27 @@
+# Configuration file used to install NixOS-x86_64 on a USB stick.
+
+{
+  boot = {
+    grubDevice = "/dev/sda";
+    initrd = {
+      extraKernelModules = ["usb_storage" "ehci_hcd" "ohci_hcd"];
+      enableSplashScreen = false;
+    };
+  };
+
+  fileSystems = [
+    { mountPoint = "/";
+      label = "nixos-usb";
+    }
+  ];
+
+  services = {
+    ttyBackgrounds = {
+      enable = false;
+    };
+  };
+
+  fonts = {
+    enableFontConfig = false;
+  };
+}
