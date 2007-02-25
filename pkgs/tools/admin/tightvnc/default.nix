8cfe4d82759719a6ca6a16ac214a18c1b1b0e94f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-02-25 09:45:12 +0000

    * Latest tightvnc.
    svn path=/nixpkgs/trunk/; revision=8037

--- pkgs/tools/admin/tightvnc/default.nix
+++ pkgs/tools/admin/tightvnc/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, x11, zlib, libjpeg, imake, gccmakedep, libXmu, libXaw, libXpm, libXp}:
 
 stdenv.mkDerivation {
-  name = "tightvnc-1.3dev7";
+  name = "tightvnc-1.3.8";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/tightvnc-1.3dev7_unixsrc.tar.bz2;
-    md5 = "8e9e63f19d8351a5359c0cc15d96c18c";
+    url = http://belnet.dl.sourceforge.net/sourceforge/vnc-tight/tightvnc-1.3.8_unixsrc.tar.bz2;
+    sha256 = "0y0ii1w21zcvm3kwxbjcxyqjwr5krmc32jawg8mfqgw4qv2y2xnd";
   };
   buildInputs = [x11 zlib libjpeg imake gccmakedep libXmu libXaw libXpm libXp];
 }
