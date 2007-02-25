3ab4c587fb219fb470f62d922fe35d528996af56
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-02-25 11:29:00 +0000

    * Fixed URL.
    svn path=/nixpkgs/trunk/; revision=8040

--- pkgs/tools/admin/tightvnc/default.nix
+++ pkgs/tools/admin/tightvnc/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "tightvnc-1.3.8";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://belnet.dl.sourceforge.net/sourceforge/vnc-tight/tightvnc-1.3.8_unixsrc.tar.bz2;
+    url = http://surfnet.dl.sourceforge.net/sourceforge/vnc-tight/tightvnc-1.3.8_unixsrc.tar.bz2;
     sha256 = "0y0ii1w21zcvm3kwxbjcxyqjwr5krmc32jawg8mfqgw4qv2y2xnd";
   };
   buildInputs = [x11 zlib libjpeg imake gccmakedep libXmu libXaw libXpm libXp];
