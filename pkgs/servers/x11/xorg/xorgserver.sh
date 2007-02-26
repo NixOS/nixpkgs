26a710ad676f1e19e00c3bbebc08ca01417b88ba
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-02-26 19:11:19 +0000

    * Hack to address a GNU Make impurity (NIXPKGS-45).
    svn path=/nixpkgs/trunk/; revision=8054

--- pkgs/servers/x11/xorg/xorgserver.sh
+++ pkgs/servers/x11/xorg/xorgserver.sh
@@ -1,3 +1,8 @@
+# There is a dependency somewhere on `-ldl', which Make treats
+# specially by mapping it to /usr/lib/libdl.so.  That won't work on
+# NixOS, so force Make to search in our own Glibc.
+export VPATH=$(cat ${NIX_GCC}/nix-support/orig-libc)/lib
+
 preConfigure=preConfigure
 preConfigure() {
     unpackFile $mesaSrc
