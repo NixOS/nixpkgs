7cda27b6ec983cb717fb7c7adc66cf9f9829dd1f
Martin Bravenboer <martin.bravenboer@logicblox.com>
2007-02-27 10:40:15 +0000

    Fix for building valgrind on x86_64-linux.
    If building on x86_64-linux, pass --enable-only64bit to the configure
    script of valgrind. This disables the 32-bit build, which uses gcc
    -m32, which is not supported by the standard gcc for x86_64-linux.
    
    
    svn path=/nixpkgs/trunk/; revision=8073

--- pkgs/development/tools/misc/valgrind/default.nix
+++ pkgs/development/tools/misc/valgrind/default.nix
@@ -7,6 +7,9 @@ stdenv.mkDerivation {
     sha256 = "0hf48y13mm1c1zg59bvkbr0lzcwng5mb33lgiv3d0gzl4w2r5jhv";
   };
 
+  configureFlags =
+    if stdenv.system == "x86_64-linux" then ["--enable-only64bit"] else [];
+
   meta = {
     description = "Award-winning suite of tools for debugging and profiling Linux programs";
   };
