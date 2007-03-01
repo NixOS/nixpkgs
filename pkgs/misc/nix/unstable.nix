c467d6e5e62568c1f6146dbbc2ad63134d011761
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-03-01 15:50:14 +0000

    * Doh doh doh.
    svn path=/nixpkgs/trunk/; revision=8138

--- pkgs/misc/nix/unstable.nix
+++ pkgs/misc/nix/unstable.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation {
   
   src = fetchurl {
     url = http://nix.cs.uu.nl/dist/nix/nix-0.11pre8133/nix-0.11pre8133.tar.bz2;
-    md5 = "33c28fcd97d5ae64e9219897b36cc6e4)";
+    md5 = "33c28fcd97d5ae64e9219897b36cc6e4";
   };
   
   buildInputs = [perl curl openssl];
