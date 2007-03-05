12b6b77f966b7b17a531295829460ae47b4f8b54
Mart Kolthof <skolthof@cs.uu.nl>
2007-03-05 16:12:08 +0000

    upgraded to version 1.3.6
    svn path=/nixpkgs/trunk/; revision=8185

--- pkgs/development/libraries/cil-aterm/default.nix
+++ pkgs/development/libraries/cil-aterm/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ocaml, perl }:
 
 stdenv.mkDerivation {
-  name    = "cil-aterm-1.3.4";
+  name    = "cil-aterm-1.3.6";
   src     = fetchurl {
-		url = http://surfnet.dl.sourceforge.net/sourceforge/cil/cil-1.3.4.tar.gz;
-                md5 = "a7fa54f19844a20562efd37f67c391da";
+		url = http://surfnet.dl.sourceforge.net/sourceforge/cil/cil-1.3.6.tar.gz;
+                md5 = "112dfbabdd0e1280800d62ba4449ab45";
             };
-  patches = [./cil-aterm-1.3.4.patch];
+  patches = [./cil-aterm-1.3.6.patch];
   buildInputs = [ ocaml perl ];
   inherit ocaml perl;
 }  
