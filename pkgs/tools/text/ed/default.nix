15036d306bd8b6801c28903ca7028907c3d4c2a5
Armijn Hemel <armijn@gpl-violations.org>
2007-03-12 23:59:48 +0000

    0.4 -> 0.5
    svn path=/nixpkgs/trunk/; revision=8270

--- pkgs/tools/text/ed/default.nix
+++ pkgs/tools/text/ed/default.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchurl}: stdenv.mkDerivation {
-  name = "ed-0.4";
+  name = "ed-0.5";
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/gnu/ed/ed-0.4.tar.bz2;
-    md5 = "b5c8606bb306671bbbb2bd708d937bcb";
+    url = ftp://ftp.nluug.nl/pub/gnu/ed/ed-0.5.tar.bz2;
+    sha256 = "0gjzsdpha28q7fddwvwxa6h87x13sgg0wakq6a1diw7vw2yjpvzd";
   };
 }
