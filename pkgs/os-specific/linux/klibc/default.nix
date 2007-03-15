a2db89608575eac1fa9d8c1299689729539a5532
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-03-15 14:38:39 +0000

    * klibc 1.5.
    svn path=/nixpkgs/trunk/; revision=8295

--- pkgs/os-specific/linux/klibc/default.nix
+++ pkgs/os-specific/linux/klibc/default.nix
@@ -3,11 +3,11 @@
 assert stdenv.isLinux;
 
 stdenv.mkDerivation {
-  name = "klibc-1.4.33";
+  name = "klibc-1.5";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://www.kernel.org/pub/linux/libs/klibc/Testing/klibc-1.4.33.tar.bz2;
-    sha256 = "1831bphb4z1x8vkhqmvxkb617pb4dixq33bm0nc6qrxrwix0ylag";
+    url = http://www.nl.kernel.org/pub/linux/libs/klibc/klibc-1.5.tar.bz2;
+    sha256 = "1izhf8kscjymsvsvhcqw9awnmp94vwv70zdj09srg9bkpjj0n017";
   };
   inherit kernel;
   buildInputs = [perl bison mktemp];
