d8cd9746cb85855864f435dbbf6cb866fde3e538
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-03-16 14:58:28 +0000

    * Fix URL.
    svn path=/nixpkgs/trunk/; revision=8304

--- pkgs/os-specific/linux/libnscd/default.nix
+++ pkgs/os-specific/linux/libnscd/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "libnscd-2.0.2";
    
   src = fetchurl {
-    url = http://suse.osuosl.org/people/kukuk/libnscd/libnscd-2.0.2.tar.bz2;
+    url = http://ftp.suse.com/pub/people/kukuk/libnscd/libnscd-2.0.2.tar.bz2;
     sha256 = "0nxhwy42x44jlpdb5xq1prbvfjmds4hplmwv3687z0c4r9rn506l";
   };
 }
