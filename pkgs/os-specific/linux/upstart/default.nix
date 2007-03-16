4d1c976a9d2328ad2e936d3d34b4ae886d8b6480
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-03-16 12:58:49 +0000

    * Copied Upstart 0.3.0.
    svn path=/nixpkgs/trunk/; revision=8299

--- pkgs/os-specific/linux/upstart/default.nix
+++ pkgs/os-specific/linux/upstart/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation {
   name = "upstart-0.3.0";
   src = fetchurl {
-    url = http://upstart.ubuntu.com/download/upstart-0.3.0.tar.bz2;
+    url = http://losser.st-lab.cs.uu.nl/~eelco/dist/upstart-0.3.0.tar.bz2;
     md5 = "269046f41c6418225306280044a799eb";
   };
   configureFlags = "--enable-compat";
