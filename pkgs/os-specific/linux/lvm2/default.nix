8dab939a7fd2e7d5a0fa0d581be5e8dd51dd827b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-03-16 15:19:19 +0000

    * New lvm2.
    svn path=/nixpkgs/trunk/; revision=8305

--- pkgs/os-specific/linux/lvm2/default.nix
+++ pkgs/os-specific/linux/lvm2/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, devicemapper}:
 
 stdenv.mkDerivation {
-  name = "lvm2-2.02.17";
+  name = "lvm2-2.02.22";
   src = fetchurl {
-    url = ftp://sources.redhat.com/pub/lvm2/LVM2.2.02.17.tgz;
-    md5 = "a1bebdabb0dace2b9dd98579625ce53c";
+    url = ftp://sources.redhat.com/pub/lvm2/old/LVM2.2.02.22.tgz;
+    sha256 = "1izcjv3g2xrma79xswdk8n1bm3rg1h70ccdp167wnwjca8rs56i8";
   };
   buildInputs = [devicemapper];
   # To prevent make install from failing.
