6837f1e46589fa2cc61bd167a8111fb36f118538
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-04-18 20:16:06 +0000

    * Updated the tfp-server-extension patch for 0.5.
    svn path=/nixpkgs/trunk/; revision=8596

--- pkgs/applications/window-managers/compiz/default.nix
+++ pkgs/applications/window-managers/compiz/default.nix
@@ -4,15 +4,14 @@
 }:
 
 stdenv.mkDerivation {
-  name = "compiz-0.3.6";
+  name = "compiz-0.5.0";
   src = fetchurl {
-    url = http://xorg.freedesktop.org/archive/individual/app/compiz-0.3.6.tar.bz2;
-    sha256 = "0z7cprg510x1sjzsj8h02l1q5h7qvhcn7z94b7a48pxv124z7qpg";
+    url = http://xorg.freedesktop.org/archive/individual/app/compiz-0.5.0.tar.gz;
+    sha256 = "1fac5fc37b218k34lpxqlhs7srqxm7jly0hfncs3ghmjmxdlj03y";
   };
-  patches = fetchurl {
-    url = http://gandalfn.club.fr/ubuntu/compiz-patch/02-tfp-server-extension.patch;
-    sha256 = "1hi53ajypmgsyfz7cziccdk9f8mn3pfl255yjzl0v15nv5kacmiq";
-  };
+  patches = [
+    ./tfp-server-extension.patch
+  ];
   buildInputs = [
     pkgconfig libXrender xextproto gtk libwnck GConf
   ];
