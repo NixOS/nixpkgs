a268c294b5da3fe14f14d8b06bc058b256649fc7
Armijn Hemel <armijn@gpl-violations.org>
2007-05-13 21:15:41 +0000

    make pygobject an inherited attribute
    svn path=/nixpkgs/trunk/; revision=8669

--- pkgs/development/python-modules/pygtk/default.nix
+++ pkgs/development/python-modules/pygtk/default.nix
@@ -7,5 +7,7 @@ stdenv.mkDerivation {
     url = http://ftp.gnome.org/pub/GNOME/sources/pygtk/2.10/pygtk-2.10.4.tar.bz2;
     sha256 = "1xg8vng42lql29aa5ryva8icc8dwdc7h2y3yn96qjdgl394d96mb";
   };
-  buildInputs = [python pkgconfig glib gtk pygobject pycairo];
+  buildInputs = [python pkgconfig glib gtk pycairo];
+
+  propagatedBuildInputs = [pygobject];
 }
