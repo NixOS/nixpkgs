16fc4c08388afeddc9aaf693f1438d5947635fcf
Armijn Hemel <armijn@gpl-violations.org>
2007-05-13 23:24:34 +0000

    ethereal -> wireshark update to 0.99.5
    svn path=/nixpkgs/trunk/; revision=8675

---
+++ pkgs/applications/networking/sniffers/wireshark/default.nix
@@ -0,0 +1,10 @@
+{stdenv, fetchurl, perl, pkgconfig, glib, libpcap}:
+
+stdenv.mkDerivation {
+  name = "wireshark-0.99.5";
+  src = fetchurl {
+    url = http://www.wireshark.org/download/src/wireshark-0.99.5.tar.gz;
+    sha256 = "0a6fkfdypfp73h1zxva2qxsm2mpkp1jv7p3d42xv5ghfwjqxyi0i";
+  };
+  buildInputs = [perl pkgconfig glib libpcap];
+}
