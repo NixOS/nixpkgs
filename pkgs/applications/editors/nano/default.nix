0f087ce9a06109328933f15a37a1995595a32c9c
Armijn Hemel <armijn@gpl-violations.org>
2007-05-13 17:59:15 +0000

    2.0.3 -> 2.0.6
    svn path=/nixpkgs/trunk/; revision=8663

--- pkgs/applications/editors/nano/default.nix
+++ pkgs/applications/editors/nano/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, ncurses, gettext}:
 
 stdenv.mkDerivation {
-  name = "nano-2.0.3";
+  name = "nano-2.0.6";
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/gnu/nano/nano-2.0.3.tar.gz;
-    md5 = "b8e2c1450b36d21f9a82509da3e4d9b1";
+    url = ftp://ftp.nluug.nl/pub/gnu/nano/nano-2.0.6.tar.gz;
+    sha256 = "0p2xfs4jzj7dvp208qdrxij7x8gbwxgnrdm7zafgpbbg1bvxh40d";
   };
   buildInputs = [ncurses gettext];
   configureFlags = "--enable-tiny";
