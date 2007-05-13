aee37f4c04118e08512e1ad0fb8563d66a00dc22
Armijn Hemel <armijn@gpl-violations.org>
2007-05-13 22:43:15 +0000

    update to 1.8.7: * remove patch (no longer needed) * disable readline support
    svn path=/nixpkgs/trunk/; revision=8672

--- pkgs/tools/misc/parted/default.nix
+++ pkgs/tools/misc/parted/default.nix
@@ -1,11 +1,12 @@
-{stdenv, fetchurl, e2fsprogs, ncurses, readline}:
+{stdenv, fetchurl, e2fsprogs, readline}:
 
 stdenv.mkDerivation {
-  name = "parted-1.8.1";
+  name = "parted-1.8.7";
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/gnu/parted/parted-1.8.1.tar.bz2;
-    md5 = "c430b38fd5f3c7530e2c3a3bdf605a29";
+    url = ftp://ftp.nluug.nl/pub/gnu/parted/parted-1.8.7.tar.bz2;
+    sha256 = "0njabfinn1kbkdx80gayqanpammnl28pklli34bymhx1sxn82kk3";
   };
-  buildInputs = [e2fsprogs ncurses readline];
-  patches = [./parted-trailingcomma.patch];
+  buildInputs = [e2fsprogs readline];
+
+  configureFlags = "--without-readline";
 }
