7e4289df4743c9049fd3b6aec9742853412db106
Armijn Hemel <armijn@gpl-violations.org>
2007-05-14 00:18:13 +0000

    2.6.8 -> 2.6.9
    svn path=/nixpkgs/trunk/; revision=8680

--- pkgs/applications/networking/sync/rsync/default.nix
+++ pkgs/applications/networking/sync/rsync/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "rsync-2.6.8";
+  name = "rsync-2.6.9";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/rsync-2.6.8.tar.gz;
-    md5 = "082a9dba1f741e6591e5cd748a1233de";
+    url = http://rsync.samba.org/ftp/rsync/rsync-2.6.9.tar.gz;
+    sha256 = "1y9kwsyxfgcv2kzlh6bnm95mdwcz30wrmihb61rhx2fdpq0p6hya";
   };
 }
