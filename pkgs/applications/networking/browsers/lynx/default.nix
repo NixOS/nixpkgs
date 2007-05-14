d2086b5e5349cedb2ce97a2bf6b74b1cc38d7467
Armijn Hemel <armijn@gpl-violations.org>
2007-05-14 01:36:50 +0000

    lynx 2.8.6
    svn path=/nixpkgs/trunk/; revision=8687

--- pkgs/applications/networking/browsers/lynx/default.nix
+++ pkgs/applications/networking/browsers/lynx/default.nix
@@ -5,10 +5,10 @@
 assert sslSupport -> openssl != null;
 
 stdenv.mkDerivation {
-  name = "lynx-2.8.5";
+  name = "lynx-2.8.6";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/lynx2.8.5.tar.bz2;
-    md5 = "d1e5134e5d175f913c16cb6768bc30eb";
+    url = http://lynx.isc.org/lynx2.8.6/lynx2.8.6.tar.bz2;
+    sha256 = "0kbnxg01w1hczii6yqkx05dcx6yfcpqadrsavjyq3h68birc366w";
   };
   configureFlags = (if sslSupport then "--with-ssl" else "");
   buildInputs = [ncurses (if sslSupport then openssl else null)];
