fc9a530666ab1662c82af7220f34f4785fac207e
Armijn Hemel <armijn@gpl-violations.org>
2007-05-14 01:05:53 +0000

    1.3.29 -> 1.3.31
    svn path=/nixpkgs/trunk/; revision=8684

--- pkgs/development/tools/misc/swig/default.nix
+++ pkgs/development/tools/misc/swig/default.nix
@@ -6,12 +6,12 @@ assert pythonSupport -> python != null;
 assert javaSupport -> jdk != null;
 
 stdenv.mkDerivation {
-  name = "swig-1.3.29";
+  name = "swig-1.3.31";
 
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/swig-1.3.29.tar.gz;
-    md5 = "44c6f6d8d724cf7fa4a5109e6f63c9b1";
+    url = http://surfnet.dl.sourceforge.net/sourceforge/swig/swig-1.3.31.tar.gz;
+    sha256 = "01fqf6xvhaqh76kh13bknn5j7axpm1pdnjpzwxsa9d1lbq4n7bf2";
   };
 
   inherit perlSupport pythonSupport javaSupport;
