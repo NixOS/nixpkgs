c068774f59cde80c90db73c2591eb6df8ada4b52
Armijn Hemel <armijn@gpl-violations.org>
2007-05-14 00:04:39 +0000

    cleanups, build graphical frontend too
    svn path=/nixpkgs/trunk/; revision=8677

--- pkgs/applications/networking/sniffers/wireshark/default.nix
+++ pkgs/applications/networking/sniffers/wireshark/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, perl, pkgconfig, glib, libpcap}:
+{stdenv, fetchurl, perl, pkgconfig, gtk, libpcap}:
 
 stdenv.mkDerivation {
   name = "wireshark-0.99.5";
@@ -6,5 +6,5 @@ stdenv.mkDerivation {
     url = http://www.wireshark.org/download/src/wireshark-0.99.5.tar.gz;
     sha256 = "0a6fkfdypfp73h1zxva2qxsm2mpkp1jv7p3d42xv5ghfwjqxyi0i";
   };
-  buildInputs = [perl pkgconfig glib libpcap];
+  buildInputs = [perl pkgconfig gtk libpcap];
 }
