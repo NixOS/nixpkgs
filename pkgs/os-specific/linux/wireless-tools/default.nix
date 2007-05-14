42eb388fa28553cbaabdf6747775782e113401c3
Armijn Hemel <armijn@gpl-violations.org>
2007-05-14 11:39:01 +0000

    pre17 -> pre21
    svn path=/nixpkgs/trunk/; revision=8690

--- pkgs/os-specific/linux/wireless-tools/default.nix
+++ pkgs/os-specific/linux/wireless-tools/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "wireless-tools-29-pre17";
+  name = "wireless-tools-29-pre21";
 
   src = fetchurl {
-    url = http://www.hpl.hp.com/personal/Jean_Tourrilhes/Linux/wireless_tools.29.pre17.tar.gz;
-    sha256 = "13488mk5q8zdb6z933287kf2sf2narkn4khni5z3x7y87jvrs127";
+    url = http://www.hpl.hp.com/personal/Jean_Tourrilhes/Linux/wireless_tools.29.pre21.tar.gz;
+    sha256 = "1agk4i3jvwzdiin7c19ixn8ipi4f2vg71lp3mzcjqmghph9lhwch";
   };
 
   preBuild = "
