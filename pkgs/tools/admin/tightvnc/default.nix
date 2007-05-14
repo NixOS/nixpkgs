35bc49bc3ad6142906a3200fa2a409c534e0d4e5
Armijn Hemel <armijn@gpl-violations.org>
2007-05-14 01:31:43 +0000

    vncviewer 1.3.9
    svn path=/nixpkgs/trunk/; revision=8686

--- pkgs/tools/admin/tightvnc/default.nix
+++ pkgs/tools/admin/tightvnc/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, x11, zlib, libjpeg, imake, gccmakedep, libXmu, libXaw, libXpm, libXp}:
 
 stdenv.mkDerivation {
-  name = "tightvnc-1.3.8";
+  name = "tightvnc-1.3.9";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://surfnet.dl.sourceforge.net/sourceforge/vnc-tight/tightvnc-1.3.8_unixsrc.tar.bz2;
-    sha256 = "0y0ii1w21zcvm3kwxbjcxyqjwr5krmc32jawg8mfqgw4qv2y2xnd";
+    url = http://surfnet.dl.sourceforge.net/sourceforge/vnc-tight/tightvnc-1.3.9_unixsrc.tar.bz2;
+    sha256 = "0nij6kyzwxf7nblwd6riwqhzh8b8xwdffpj379zi5y9mcmiwmalr";
   };
   buildInputs = [x11 zlib libjpeg imake gccmakedep libXmu libXaw libXpm libXp];
 }
