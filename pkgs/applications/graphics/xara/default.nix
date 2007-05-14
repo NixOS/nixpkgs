1a879a423a12648daf36807594712f433d67d199
Armijn Hemel <armijn@gpl-violations.org>
2007-05-14 16:59:15 +0000

    update to newer version, still has rpath problems
    svn path=/nixpkgs/trunk/; revision=8692

--- pkgs/applications/graphics/xara/default.nix
+++ pkgs/applications/graphics/xara/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, autoconf, automake, gettext, libtool, cvs, wxGTK, gtk, pkgconfig, libxml2, zip, libpng, libjpeg}:
 
 stdenv.mkDerivation {
-  name = "xaralx-0.5r1405";
+  name = "xaralx-0.7r1766";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://downloads2.xara.com/opensource/XaraLX-0.5r1405.tar.bz2;
-    md5 = "1b087819e4b10b21da8c267ed56a45a4";
+    url = http://downloads2.xara.com/opensource/XaraLX-0.7r1766.tar.bz2;
+    sha256 = "1rcl7hqvcai586jky7hvzxhnq8q0ka2rsmgiq5ijwclgr5d4ah7n";
   };
   
   buildInputs = [automake autoconf gettext libtool cvs wxGTK gtk pkgconfig libxml2 zip libpng libjpeg];
