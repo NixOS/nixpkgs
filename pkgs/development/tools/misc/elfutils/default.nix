2f670d1e0646f416796e3cbeb64f80505836315c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-05-15 13:33:39 +0000

    * Copy elfutils before it disappears again.
    svn path=/nixpkgs/trunk/; revision=8703

--- pkgs/development/tools/misc/elfutils/default.nix
+++ pkgs/development/tools/misc/elfutils/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation {
   name = "elfutils-0.127";
   src = fetchurl {
-    url = http://ftp.cs.pu.edu.tw/Linux/sourceware/systemtap/elfutils/elfutils-0.127.tar.gz;
+    url = http://nix.cs.uu.nl/dist/tarballs/elfutils-0.127.tar.gz;
     sha256 = "12n3h5r3c24a6l2wxz0w2dqq072bvgms0dzckivrwp5vdn22lpdv";
   };
 }
