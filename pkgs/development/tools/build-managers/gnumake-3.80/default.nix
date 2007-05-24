45c7aa04d777f7cf4c727d664576c4420bc8105f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-05-24 15:20:40 +0000

    * Don't look in /usr/lib and /usr/include for certain kinds of   dependencies.  (NIXPKGS-45)
    svn path=/nixpkgs/trunk/; revision=8755

--- pkgs/development/tools/build-managers/gnumake-3.80/default.nix
+++ pkgs/development/tools/build-managers/gnumake-3.80/default.nix
@@ -6,5 +6,5 @@ stdenv.mkDerivation {
     url = http://nix.cs.uu.nl/dist/tarballs/make-3.80.tar.bz2;
     md5 = "0bbd1df101bc0294d440471e50feca71";
   };
-  patches = [./log.diff];
+  patches = [./log.patch];
 }
--- pkgs/development/tools/build-managers/gnumake/default.nix
+++ pkgs/development/tools/build-managers/gnumake/default.nix
@@ -6,5 +6,16 @@ stdenv.mkDerivation {
     url = http://nix.cs.uu.nl/dist/tarballs/make-3.81.tar.bz2;
     md5 = "354853e0b2da90c527e35aabb8d6f1e6";
   };
-  patches = [./log.diff];
+  
+  patches = [
+    # Provide nested log output for subsequent pretty-printing by
+    # nix-log2xml.
+    ./log.patch
+
+    # Purity: don't look for library dependencies (of the form
+    # `-lfoo') in /lib and /usr/lib.  It's a stupid feature anyway.
+    # Likewise, when searching for included Makefiles, don't look in
+    # /usr/include and friends.
+    ./impure-dirs.patch
+  ];
 }
