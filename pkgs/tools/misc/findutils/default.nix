e85fb3ce90e85430ae6819e0700bc14fe2b3804b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-05-24 13:49:40 +0000

    * findutils updated to 4.2.30.
    svn path=/nixpkgs/trunk/; revision=8750

--- pkgs/tools/misc/findutils/default.nix
+++ pkgs/tools/misc/findutils/default.nix
@@ -1,14 +1,14 @@
 {stdenv, fetchurl, coreutils}:
 
 stdenv.mkDerivation {
-  name = "findutils-4.2.29";
+  name = "findutils-4.2.30";
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/gnu/findutils/findutils-4.2.29.tar.gz;
-    md5 = "24e76434ca74ba3c2c6ad621eb64e1ff";
+    url = ftp://ftp.nluug.nl/pub/gnu/findutils/findutils-4.2.30.tar.gz;
+    sha256 = "1x1s0h1gf4hxh6xi6vq336sz8zsh4hvnsslc7607z41l82xrqjrl";
   };
   buildInputs = [coreutils];
   patches = [./findutils-path.patch]
     # Note: the dietlibc patch is just to get findutils to compile.
     # The locate command probably won't work though.
-    ++ (if stdenv ? isDietLibC then [./dietlibc-hack.patch] else []);
+    ++ stdenv.lib.optional (stdenv ? isDietLibC) ./dietlibc-hack.patch;
 }
