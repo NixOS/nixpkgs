056209410defb685a434a41171f6a9ca0c5d15ec
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-05-24 13:42:28 +0000

    * GNU M4 updated to 1.4.9 (NIXPKGS-51).
    svn path=/nixpkgs/trunk/; revision=8748

--- pkgs/development/tools/misc/gnum4/default.nix
+++ pkgs/development/tools/misc/gnum4/default.nix
@@ -1,8 +1,8 @@
 {stdenv, fetchurl}:
 stdenv.mkDerivation {
-  name = "gnum4-1.4.8";
+  name = "gnum4-1.4.9";
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/gnu/m4/m4-1.4.8.tar.bz2;
-    md5 = "6bbf917e5d8fab20b38d43868c3944d3";
+    url = ftp://ftp.nluug.nl/pub/gnu/m4/m4-1.4.9.tar.bz2;
+    sha256 = "0a9xgknp29zdhpp7xn3bfcxdc0wn2rzvjfdksz5ym82b6y397qm8";
   };
 }
