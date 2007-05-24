c44f77bbdf6f898ed7f4f393fe37dd5d674d4bd3
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-05-24 13:34:34 +0000

    * curl 7.16.2 (NIXPKGS-55).  The CXXCPP patch doesn't seem to be   necessary anymore.
    svn path=/nixpkgs/trunk/; revision=8742

--- pkgs/tools/networking/curl/default.nix
+++ pkgs/tools/networking/curl/default.nix
@@ -4,18 +4,19 @@ assert zlibSupport -> zlib != null;
 assert sslSupport -> openssl != null;
 
 stdenv.mkDerivation {
-  name = "curl-7.15.5";
+  name = "curl-7.16.2";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/curl-7.15.5.tar.bz2;
-    md5 = "594142c7d53bbdd988e8cef6354eeeff";
+    url = http://curl.haxx.se/download/curl-7.16.2.tar.bz2;
+    sha256 = "18mzp56y8qhlvi27av7866mvsiyiigb7c5qdppjr8qizsj0kx0rf";
   };
   buildInputs =
-    (if zlibSupport then [zlib] else [])
-    ++ (if sslSupport then [openssl] else []);
-  patches = [./configure-cxxcpp.patch];
+    stdenv.lib.optional zlibSupport zlib ++
+    stdenv.lib.optional sslSupport openssl;
   configureFlags = "
     ${if sslSupport then "--with-ssl=${openssl}" else "--without-ssl"}
   ";
   CFLAGS = if stdenv ? isDietLibC then "-DHAVE_INET_NTOA_R_2_ARGS=1" else "";
+  CXX = "g++";
+  CXXCPP = "g++ -E";
   inherit sslSupport openssl;
 }
