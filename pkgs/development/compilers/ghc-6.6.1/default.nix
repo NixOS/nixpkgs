838879a4acc91680c8ee4df4bddb94453b606239
Armijn Hemel <armijn@gpl-violations.org>
2007-06-02 17:15:19 +0000

    fix hash for ghc
    svn path=/nixpkgs/trunk/; revision=8818

--- pkgs/development/compilers/ghc-6.6.1/default.nix
+++ pkgs/development/compilers/ghc-6.6.1/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
 
   src = map fetchurl [
     { url = http://www.haskell.org/ghc/dist/6.6.1/ghc-6.6.1-src.tar.bz2;
-      md5 = "dea271503463bd28c27f25ab90998633";
+      md5 = "ba9f4aec2fde5ff1e1548ae69b78aeb0";
     }
     { url = http://www.haskell.org/ghc/dist/6.6.1/ghc-6.6.1-src-extralibs.tar.bz2;
       md5 = "43a26b81608b206c056adc3032f7da2a";
