d6835dbeb41fa58a3ba355677785c3a981743e5c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-06-02 17:03:11 +0000

    * FUSE 2.6.5.
    svn path=/nixpkgs/trunk/; revision=8817

--- pkgs/os-specific/linux/fuse/default.nix
+++ pkgs/os-specific/linux/fuse/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "fuse-2.6.1";
+  name = "fuse-2.6.5";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://heanet.dl.sourceforge.net/sourceforge/fuse/fuse-2.6.1.tar.gz;
-    md5 = "13e1873086a1d7a95f470bbc7428c528";
+    url = http://heanet.dl.sourceforge.net/sourceforge/fuse/fuse-2.6.5.tar.gz;
+    sha256 = "0901hrhi1z6dwlgvgn75cg2268wvaz53x0knn7jplk6acwir54db";
   };
   configureFlags = [ "--disable-kernel-module" ];
 }
