ce0fdeb20aec6eb7dc546fee240608d360a4d6fe
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-06-03 20:17:15 +0000

    * Latest AEFS.
    svn path=/nixpkgs/trunk/; revision=8822

--- pkgs/tools/security/aefs/default.nix
+++ pkgs/tools/security/aefs/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, fuse}:
   
 stdenv.mkDerivation {
-  name = "aefs-0.3pre279";
+  name = "aefs-0.3pre285";
   
   src = fetchurl {
-    url = http://losser.st-lab.cs.uu.nl/~eelco/dist/aefs-0.3pre279.tar.bz2;
-    sha256 = "1gsa55cmhgj124m2laxcxcrgh45rsmp9jwz9wc9q515np9s7h11c";
+    url = http://losser.st-lab.cs.uu.nl/~eelco/dist/aefs-0.3pre285.tar.bz2;
+    sha256 = "1psciqllzm08c21h6k2zxmvmi0grkvaiq177giv1avzzzfhq0z3c";
   };
 
   buildInputs = [fuse];
