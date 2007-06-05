80c7cce4e9d48b1e9c0f4603ace70191e11292c3
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-06-05 16:56:10 +0000

    * Use /var.
    svn path=/nixpkgs/trunk/; revision=8836

--- pkgs/development/libraries/dbus/default.nix
+++ pkgs/development/libraries/dbus/default.nix
@@ -7,6 +7,16 @@ stdenv.mkDerivation {
     sha256 = "1jn652zb81mczsx4rdcwrrzj3lfhx9d107zjfnasc4l5yljl204a";
   };
   buildInputs = [pkgconfig expat];
-  configureFlags = "--without-x";
-  #configureFlags = "--localstatedir=/var";
+  configureFlags = "--without-x --localstatedir=/var";
+
+  # Awful hack: `make install' wants to write in /var, but it
+  # can't.  So redirect it with a DESTDIR.
+  preInstall = "
+    installFlagsArray=(DESTDIR=$out/destdir)
+  ";
+
+  postInstall = "
+    mv $out/destdir/$out/* $out
+    rm -rf $out/destdir
+  ";
 }
