de2733c1c920507819428ebdf57e29ba0d8e15f8
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-06-09 20:23:49 +0000

    * Latest udev.
    svn path=/nixpkgs/trunk/; revision=8855

--- pkgs/os-specific/linux/udev/default.nix
+++ pkgs/os-specific/linux/udev/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "udev-103";
+  name = "udev-112";
   src = fetchurl {
-    url = http://www.us.kernel.org/pub/linux/utils/kernel/hotplug/udev-103.tar.bz2;
-    md5 = "7360ce47b5ec3f9fa71f18071ecb8b97";
+    url = http://www.us.kernel.org/pub/linux/utils/kernel/hotplug/udev-112.tar.bz2;
+    sha256 = "0vhqnli4va3yzvr90rcpbgs42sphpk9prdis9x2qmlmdynzangw2";
   };
   preBuild = "makeFlagsArray=(etcdir=$out/etc sbindir=$out/sbin usrbindir=$out/bin usrsbindir=$out/sbin mandir=$out/share/man INSTALL='install -c')";
   preInstall = "installFlagsArray=(udevdir=dummy)";
