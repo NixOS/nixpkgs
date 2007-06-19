c1e37ee94dc88458d46831b5b0d2dae46f6bd5d0
Michael Raskin <7c6f434c@mail.ru>
2007-06-19 17:40:42 +0000

    svn path=/nixpkgs/trunk/; revision=8896

--- pkgs/development/tools/misc/lsof/default.nix
+++ pkgs/development/tools/misc/lsof/default.nix
@@ -1,15 +1,15 @@
 
 {stdenv, fetchurl}:
 stdenv.mkDerivation {
-  name = "lsof";
+  name = "lsof-4.78";
 
   src = fetchurl {
-    url = ftp://ftp.tux.org/pub/sites/vic.cc.purdue.edu/tools/unix/lsof/lsof_4.78.tar.bz2;
+    url =ftp://sunsite.ualberta.ca/pub/Mirror/lsof/lsof_4.78.tar.bz2;
     sha256 = "0azvl43niqkq94drx52p6dvp70r38f25fqw181ywmvqn80dbb3c9";
   };
 
   unpackPhase = "tar xvjf $src; cd lsof_*; tar xvf lsof_*.tar; sourceRoot=lsof_*; ";
-  configurePhase = "./Configure -n linux;";
   preBuild = "sed -i Makefile -e 's/^CFGF=/&	-DHASIPv6=1/;';";
+  configurePhase = "./Configure -n linux;";
   installPhase = " mkdir -p $out/bin $out/man/man8; cp lsof.8 $out/man/man8/; cp lsof $out/bin";
 }
