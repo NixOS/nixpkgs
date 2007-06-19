49c274d6a67e3b07cbbb51267251f905a20c7b1b
Michael Raskin <7c6f434c@mail.ru>
2007-06-19 16:06:53 +0000

    Fixed ftp source for those with IPs without reverse DNS.
    svn path=/nixpkgs/trunk/; revision=8895

--- pkgs/development/tools/misc/lsof/default.nix
+++ pkgs/development/tools/misc/lsof/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "lsof";
 
   src = fetchurl {
-    url = ftp://lsof.itap.purdue.edu/pub/tools/unix/lsof/lsof_4.78.tar.bz2;
+    url = ftp://ftp.tux.org/pub/sites/vic.cc.purdue.edu/tools/unix/lsof/lsof_4.78.tar.bz2;
     sha256 = "0azvl43niqkq94drx52p6dvp70r38f25fqw181ywmvqn80dbb3c9";
   };
 
