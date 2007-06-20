f8db720230bd460c1744bb7f8bf718f704bcc6b3
Michael Raskin <7c6f434c@mail.ru>
2007-06-20 13:01:59 +0000

    Refreshed iproute package.
    svn path=/nixpkgs/trunk/; revision=8906

--- pkgs/os-specific/linux/iproute/default.nix
+++ pkgs/os-specific/linux/iproute/default.nix
@@ -1,12 +1,16 @@
-{stdenv, fetchurl, bison, flex, db4}:
 
-stdenv.mkDerivation {
-  name = "iproute2-2.6.15-060110";
-  src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/iproute2-2.6.15-060110.tar.gz;
-    md5 = "04f57a6d366d36426d276178b600f5c5";
+pkgs:
+pkgs.stdenv.mkDerivation {
+  name = "iproute";
+
+  src = pkgs.fetchurl {
+    url = http://ftp.debian.org/debian/pool/main/i/iproute/iproute_20070313.orig.tar.gz;
+    sha256 = "1j7cmlr7p9xcg9ys8fhjnynwrp475rbkr2j2c5jqm1xzczw60f9v";
   };
-  buildInputs = [bison flex db4];
-  patches = [./iproute2-2.6.15-060110-path.patch];
 
+  preConfigure = "for script in $(find . -type f); do sed -e 's@#! /bin/bash@#! /bin/sh@' -i $script; done;";
+
+  makeFlags = " SBINDIR=\\$(out)/sbin CONFDIR=\\$(out)/etc DOCDIR=\\$(out)/doc MANDIR=\\$(out)/man ";
+ 
+  buildInputs = [pkgs.bison pkgs.flex pkgs.db4];
 }
