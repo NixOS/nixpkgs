9c542effbefac3bc08edb532042ab0afee2aeadd
Armijn Hemel <armijn@gpl-violations.org>
2007-07-02 20:26:49 +0000

    ed 0.6: * few fixes * GPLv3
    svn path=/nixpkgs/trunk/; revision=8942

--- pkgs/tools/text/ed/default.nix
+++ pkgs/tools/text/ed/default.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchurl}: stdenv.mkDerivation {
-  name = "ed-0.5";
+  name = "ed-0.6";
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/gnu/ed/ed-0.5.tar.bz2;
-    sha256 = "0gjzsdpha28q7fddwvwxa6h87x13sgg0wakq6a1diw7vw2yjpvzd";
+    url = ftp://ftp.nluug.nl/pub/gnu/ed/ed-0.6.tar.bz2;
+    sha256 = "1fzp9nj8gm4z0d2v486zwqqy8nqx730ygad82zvj0akcnlq7csax";
   };
 }
