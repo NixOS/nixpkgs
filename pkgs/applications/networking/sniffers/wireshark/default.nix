9b43958df58b773c11af086ab2664564fa7ca71d
Michael Raskin <7c6f434c@mail.ru>
2007-07-06 13:07:14 +0000

    Version bump. Wireshark.org has already pulled old version off site...
    svn path=/nixpkgs/trunk/; revision=8950

--- pkgs/applications/networking/sniffers/wireshark/default.nix
+++ pkgs/applications/networking/sniffers/wireshark/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, perl, pkgconfig, gtk, libpcap}:
 
 stdenv.mkDerivation {
-  name = "wireshark-0.99.5";
+  name = "wireshark-0.99.6";
   src = fetchurl {
-    url = http://www.wireshark.org/download/src/wireshark-0.99.5.tar.gz;
-    sha256 = "0a6fkfdypfp73h1zxva2qxsm2mpkp1jv7p3d42xv5ghfwjqxyi0i";
+    url = http://www.wireshark.org/download/src/wireshark-0.99.6.tar.gz;
+    sha256 = "1f2i84dk4nw05nh78b2j4n9pbwdngcqx0grrja5831r6paj35x1y";
   };
   buildInputs = [perl pkgconfig gtk libpcap];
 }
