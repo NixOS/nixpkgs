1253a722f561f8ebb665ea5bd65f84d3e21abd1f
Michael Raskin <7c6f434c@mail.ru>
2007-07-07 13:34:39 +0000

    Updated ImageMagick version. Old version seems to be unavailable, and earlier had a checksum change.
    svn path=/nixpkgs/trunk/; revision=8956

--- pkgs/applications/graphics/ImageMagick/default.nix
+++ pkgs/applications/graphics/ImageMagick/default.nix
@@ -1,13 +1,11 @@
-
 {stdenv, fetchurl, bzip2, freetype, graphviz, ghostscript,
 libjpeg, libpng, libtiff, libX11, libxml2, zlib}:
 stdenv.mkDerivation {
-  name = "ImageMagick";
+  name = "ImageMagick-6.3.5";
 
   src = fetchurl {
-    url = ftp://ftp.carnet.hr/misc/imagemagick/ImageMagick-6.3.4-7.tar.bz2;
-    sha1 = "bead7b27d951cb9c780c76771562e9e7c7c23d16";
-# cdc388e20580de2a0f91f68e1228c68b0735f385 
+    url = ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.3.5-0.tar.bz2;
+    sha256 = "0yp3srha2h21qsnhfsfczjqw8x1qy1bdlc06qsrj1as0bn2br7b4";
   };
 
   configureFlags = " --with-dots --with-gs-font-dir="+ ghostscript +
