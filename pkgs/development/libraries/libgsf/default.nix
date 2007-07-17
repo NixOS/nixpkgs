74df4c50c6e1f556f8c44ae8e20482e4175cc328
Wouter den Breejen <uu@denbreejen.net>
2007-07-17 14:44:40 +0000

    Fixed undeclared gettext dependency for libgsf
    svn path=/nixpkgs/trunk/; revision=8988

--- pkgs/development/libraries/libgsf/default.nix
+++ pkgs/development/libraries/libgsf/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, perl, perlXMLParser, pkgconfig, libxml2, glib}:
+{stdenv, fetchurl, perl, perlXMLParser, pkgconfig, libxml2, glib, gettext}:
 
 stdenv.mkDerivation {
   name = "libgsf-1.14.1";
@@ -6,5 +6,5 @@ stdenv.mkDerivation {
     url = http://nix.cs.uu.nl/dist/tarballs/libgsf-1.14.1.tar.bz2;
     md5 = "00de00b99382d0b7e034e0fffd8951d4";
   };
-  buildInputs = [perl perlXMLParser pkgconfig libxml2 glib];
+  buildInputs = [perl perlXMLParser pkgconfig libxml2 glib gettext];
 }
