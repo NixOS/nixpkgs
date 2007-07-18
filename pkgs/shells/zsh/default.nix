6296d1a64f2b7536c526928ad150e1e250e528f5
Michael Raskin <7c6f434c@mail.ru>
2007-07-18 20:34:18 +0000

    Version bump. Old one is unavailable.
    svn path=/nixpkgs/trunk/; revision=9001

--- pkgs/shells/zsh/default.nix
+++ pkgs/shells/zsh/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, coreutils, ncurses}:
 stdenv.mkDerivation {
-  name = "zsh-4.2.3";
+  name = "zsh-4.3.4";
 
   src = fetchurl {
-    url = ftp://nephtys.lip6.fr/pub/unix/shells/zsh/zsh-4.3.2.tar.bz2;
-    sha256 = "1lyzh68h69iarnmrrnd2gy9ssmns4w0abbr3hfz98lhik762f3na";
+    url = ftp://nephtys.lip6.fr/pub/unix/shells/zsh/zsh-4.3.4.tar.bz2;
+    sha256 = "1inypy60h7hir8hwidid85pbajrb5w09fl222p0h4fnsn0nf583g";
   };
 
   configureFlags = "--with-tcsetpgrp --enable-maildir-support --enable-multibyte";
