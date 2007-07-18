fa88a699361495ec8e63582a1d57a75fb4228902
Wouter den Breejen <uu@denbreejen.net>
2007-07-18 16:45:07 +0000

    Worked on openoffice
    svn path=/nixpkgs/trunk/; revision=8997

--- pkgs/applications/office/openoffice/builder.sh
+++ pkgs/applications/office/openoffice/builder.sh
@@ -1,8 +1,19 @@
 source $stdenv/setup
 
 configurePhase() {
+
   cd config_office/;
-  ./configure --disable-epm --disable-odk --with-java=no --disable-cups --with-system-libs --with-system-python --disable-mozilla --without-nas --disable-pasf --disable-gnome-vfs;
+  ./configure --disable-epm --disable-odk --with-java=no --disable-cups --with-system-python \
+  --disable-mozilla --without-nas --disable-pasf --disable-gnome-vfs \
+  --with-hsqldb-jar=$hsqldb/lib/hsqldb.jar \
+  --with-beanshell-jar=$beanshell/bsh.jar \
+  --with-xml-apis-jar=$xerces/xml-apis.jar \
+  --with-xerces-jar=$xerces/xercesImpl.jar \
+  --with-xalan-jar=$xalan/xalan.jar \
+  --with-xt-jar=$xt --with-system-libs;
+	
+  #--with-system-libs
+
   cd ..
 }
 
--- pkgs/applications/office/openoffice/default.nix
+++ pkgs/applications/office/openoffice/default.nix
@@ -1,4 +1,7 @@
-{stdenv, fetchurl, pam, python, tcsh, libxslt, perl, perlArchiveZip, perlCompressZlib, zlib, libjpeg, expat, pkgconfig, freetype, libwpd, libxml2, db4, sablotron, curl, libXaw, fontconfig, libsndfile, neon, bison, flex, zip, unzip, gtk, libmspack, getopt, file}:
+{ stdenv, fetchurl, pam, python, tcsh, libxslt, perl, perlArchiveZip
+, perlCompressZlib, zlib, libjpeg, expat, pkgconfig, freetype, libwpd
+, libxml2, db4, sablotron, curl, libXaw, fontconfig, libsndfile, neon
+, bison, flex, zip, unzip, gtk, libmspack, getopt, file, jre }:
 
 stdenv.mkDerivation {
   name = "openoffice.org-2.0.0";
@@ -6,6 +9,11 @@ stdenv.mkDerivation {
   src = fetchurl {
     url = http://nix.cs.uu.nl/dist/tarballs/OOo_2.0.0_src.tar.gz;
     md5 = "a68933afc2bf432d11b2043ac99ba0aa";
+    #url = http://ftp.snt.utwente.nl/pub/software/openoffice/stable/2.2.1/OOo_2.2.1_src_core.tar.bz2;
+    #sha256 = "adc54c88892f5ced9887945709856efeb628fe5f7b5b2f2aa7797c5391b9c7d6";
   };
-  buildInputs = [pam python tcsh libxslt perl perlArchiveZip perlCompressZlib zlib libjpeg expat pkgconfig freetype libwpd libxml2 db4 sablotron curl libXaw fontconfig libsndfile neon bison flex zip unzip gtk libmspack getopt file];
+  buildInputs = [ pam python tcsh libxslt perl perlArchiveZip perlCompressZlib zlib 
+                  libjpeg expat pkgconfig freetype libwpd libxml2 db4 sablotron curl 
+                  libXaw fontconfig libsndfile neon bison flex zip unzip gtk libmspack getopt file jre ];
+
 }
