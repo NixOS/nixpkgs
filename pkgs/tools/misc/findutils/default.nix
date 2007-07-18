eb1bfefa91ac2528cf302e0d9c6ec8ea8aac2113
Wouter den Breejen <uu@denbreejen.net>
2007-07-18 10:32:20 +0000

    Fixed a /bin/echo reference
    svn path=/nixpkgs/trunk/; revision=8995

--- pkgs/tools/misc/findutils/default.nix
+++ pkgs/tools/misc/findutils/default.nix
@@ -7,7 +7,8 @@ stdenv.mkDerivation {
     sha256 = "1x1s0h1gf4hxh6xi6vq336sz8zsh4hvnsslc7607z41l82xrqjrl";
   };
   buildInputs = [coreutils];
-  patches = [./findutils-path.patch]
+  patches = [ ./findutils-path.patch ./change_echo_path.patch ]
+  
     # Note: the dietlibc patch is just to get findutils to compile.
     # The locate command probably won't work though.
     ++ stdenv.lib.optional (stdenv ? isDietLibC) ./dietlibc-hack.patch;
