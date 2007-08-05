b90010367ed66c1501e70bb836707bb753b846c5
Andres Löh <mail@andres-loeh.de>
2007-08-05 18:21:28 +0000

    * update sudo version
    svn path=/nixpkgs/trunk/; revision=9050

--- pkgs/tools/security/sudo/default.nix
+++ pkgs/tools/security/sudo/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, coreutils, pam}:
 stdenv.mkDerivation {
-  name = "sudo-1.6.8";
+  name = "sudo-1.6.9";
 
   src = fetchurl {
-    url = ftp://sunsite.ualberta.ca/pub/Mirror/sudo/sudo-1.6.8p12.tar.gz;
-    md5 = "b29893c06192df6230dd5f340f3badf5";
+    url = ftp://sunsite.ualberta.ca/pub/Mirror/sudo/sudo-1.6.9p3.tar.gz;
+    md5 = "21791b0bfb14fe1dc508fdcfaae9bacc";
   };
 
   postConfigure = "sed -e '/_PATH_MV/d; /_PATH_VI/d' -i config.h ; echo '#define _PATH_MV \"/var/run/current-system/sw/bin/mv\"' >> config.h; echo '#define _PATH_VI \"/var/run/current-system/sw/bin/vi\"' >> config.h; echo '#define EDITOR _PATH_VI' >>config.h ";
