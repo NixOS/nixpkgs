db0e9e16c901ec228aba68c8af2860e9901c422b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-08-07 23:27:38 +0000

    * Use a patched ATerm library that fixes another GC bug.
    svn path=/nixpkgs/trunk/; revision=9065

--- pkgs/development/libraries/aterm/2.4.2-fixes.nix
+++ pkgs/development/libraries/aterm/2.4.2-fixes.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "aterm-2.4.2-fixes";
+  name = "aterm-2.4.2-fixes-r2";
   src = fetchurl {
-    url = http://losser.st-lab.cs.uu.nl/~eelco/dist/aterm-2.4.2-fixes.tar.bz2;
-    md5 = "9ad8ed141d3e66a7689817789431c0cd";
+    url = http://losser.st-lab.cs.uu.nl/~eelco/dist/aterm-2.4.2-fixes-r2.tar.bz2;
+    sha256 = "1w3bxdpc2hz29li5ssmdcz3x0fn47r7g62ns0v8nazxwf40vff4j";
   };
   meta = {
     homepage = http://www.cwi.nl/htbin/sen1/twiki/bin/view/SEN1/ATerm;
