822a3e4ead51f86bfb0df10c0d31dfbb2daab8ae
Michael Raskin <7c6f434c@mail.ru>
2007-08-09 18:11:57 +0000

    Added ThePenguinMachine. Build technically broken (but dumps compile dir to /tmp, and it runs from there.. I want to fix it first), and it crashes in a very strage way.
    svn path=/nixpkgs/trunk/; revision=9084

---
+++ pkgs/games/thePenguinMachine/default.nix
@@ -0,0 +1,38 @@
+args:
+args.stdenv.mkDerivation {
+  name = "thePenguinMachine";
+
+  src = args.
+	fetchurl {
+		url = http://www.migniot.com/matrix/projects/thepenguinmachine/ThePenguinMachine.tar.gz;
+		sha256 = "09ljks8vj75g00h3azc83yllbfsrxwmv1c9g32gylcmsshik0dqv";
+	};
+
+  buildInputs =(with args; [python24 pil pygame SDL]);
+
+  configurePhase = (with args; "
+		sed -e \"/includes = /aincludes.append('${SDL}/include/SDL')\" -i setup.py;
+		sed -e \"/includes = /aincludes.append('${pygame}/include/python2.4')\" -i setup.py;
+		cat setup.py;
+	");
+  buildPhase = "
+		python setup.py build;
+		python setup.py build_clib;
+		python setup.py build_ext;
+		python setup.py build_py;
+		python setup.py build_scripts;
+		";
+  installPhase = (with args ; "
+		python setup.py install --prefix=\${out}
+		cp -r . /tmp/tpm-build
+		echo 'export PYTHONPATH=$PYTHONPATH:${pygame}/lib/python2.4/site-packages:${pil}/lib/python2.4/site-packages/PIL
+		python ThePenguinMachine.py' >/tmp/tpm-build/tpm.sh; 
+		chmod a+rx /tmp/tpm-build/tpm.sh
+		");
+
+  meta = {
+    description = "
+	The Penguin Machine - an Incredible Machine clone.
+";
+  };
+}
