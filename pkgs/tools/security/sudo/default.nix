f3b3d536002ef56db5bbfc19ccd804bf1bab9c6b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-08-17 13:37:28 +0000

    * Sudo 1.6.9p4.
    svn path=/nixpkgs/trunk/; revision=9148

--- pkgs/tools/security/sudo/default.nix
+++ pkgs/tools/security/sudo/default.nix
@@ -1,17 +1,27 @@
 {stdenv, fetchurl, coreutils, pam}:
+
 stdenv.mkDerivation {
-  name = "sudo-1.6.9";
+  name = "sudo-1.6.9p4";
 
   src = fetchurl {
-    url = ftp://sunsite.ualberta.ca/pub/Mirror/sudo/sudo-1.6.9p3.tar.gz;
-    md5 = "21791b0bfb14fe1dc508fdcfaae9bacc";
+    url = ftp://sunsite.ualberta.ca/pub/Mirror/sudo/sudo-1.6.9p4.tar.gz;
+    md5 = "0y65f77wxsl285yr1krfh5djcwm95n39p0yb3r1xgg98gir5z7w4";
   };
 
-  postConfigure = "sed -e '/_PATH_MV/d; /_PATH_VI/d' -i config.h ; echo '#define _PATH_MV \"/var/run/current-system/sw/bin/mv\"' >> config.h; echo '#define _PATH_VI \"/var/run/current-system/sw/bin/vi\"' >> config.h; echo '#define EDITOR _PATH_VI' >>config.h ";
+  # `--with-stow' allows /etc/sudoers to be a symlink.  Only it
+  # doesn't really help because the target still has to have mode 0440,
+  # while files in the Nix store all have mode 0444.
+  #configureFlags = "--with-stow";
 
-  makeFlags = " install_gid=nixbld install_uid=nixbld1 ";
+  postConfigure = "
+    sed -e '/_PATH_MV/d; /_PATH_VI/d' -i config.h
+    echo '#define _PATH_MV \"/var/run/current-system/sw/bin/mv\"' >> config.h
+    echo '#define _PATH_VI \"/var/run/current-system/sw/bin/vi\"' >> config.h
+    echo '#define EDITOR _PATH_VI' >>config.h
 
-  installFlags = " sudoers_uid=nixbld1 sudoers_gid=nixbld sysconfdir=$(prefix)/etc ";
+    makeFlags=\"install_uid=$(id -u) install_gid=$(id -g)\"
+    installFlags=\"sudoers_uid=$(id -u) sudoers_gid=$(id -g) sysconfdir=$out/etc\"
+  ";
 
   buildInputs = [coreutils pam];
 }
