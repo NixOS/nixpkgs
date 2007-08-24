c5ac83d13813925de0bbf85a02b8ab828fda545b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-08-24 13:06:07 +0000

    * Docbook 5 XSL updated to 1.73.1 (1.73.0 was broken).
    svn path=/nixpkgs/trunk/; revision=9193

--- pkgs/data/sgml+xml/stylesheets/xslt/docbook5/default.nix
+++ pkgs/data/sgml+xml/stylesheets/xslt/docbook5/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "docbook5-xsl-1.73.0";
+  name = "docbook5-xsl-1.73.1";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://kent.dl.sourceforge.net/sourceforge/docbook/docbook-xsl-ns-1.73.0.tar.bz2;
-    sha256 = "1ldg8jqbwqy2il7ghvyv49nn45n9aj0l5w5l04sh305z6bww67nk";
+    url = http://kent.dl.sourceforge.net/sourceforge/docbook/docbook-xsl-ns-1.73.1.tar.bz2;
+    sha256 = "105irc94p04j0fj5vf5fschyxv9azkh2bsa69a796jax5ngpbahn";
   };
 }
