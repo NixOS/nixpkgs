606dba5d0a7eb3a353965f164dbe19261e53fbc1
Marc Weber <marco-oweber@gmx.de>
2007-08-30 14:44:02 +0000

    update nix version to latest nix-0.11pre9217
    svn path=/nixpkgs/trunk/; revision=9218

--- pkgs/tools/package-management/nix/unstable.nix
+++ pkgs/tools/package-management/nix/unstable.nix
@@ -4,11 +4,11 @@
 }:
 
 stdenv.mkDerivation {
-  name = "nix-0.11pre9121";
+  name = "nix-0.11pre9217";
   
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/nix/nix-0.11pre9121/nix-0.11pre9121.tar.bz2;
-    sha256 = "0yd84g8gyl1xnb0ki0ssy270ff6bs4xy3mjf2ixa7xj9ch3hbyhm";
+    url = http://nix.cs.uu.nl/dist/nix/nix-0.11pre9217/nix-0.11pre9217.tar.bz2;
+    sha256 = "79707d04e7a2bd0869b55fbfe05dcb673a2c864fb1fb72c390ae18db5ba633e5";
   };
 
   buildInputs = [perl curl openssl];
