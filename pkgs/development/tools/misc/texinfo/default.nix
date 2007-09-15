ef783b3c88ee31423362445e08acdaec99467dfc
Armijn Hemel <armijn@gpl-violations.org>
2007-09-15 23:54:01 +0000

    update to 4.11
    svn path=/nixpkgs/trunk/; revision=9322

--- pkgs/development/tools/misc/texinfo/default.nix
+++ pkgs/development/tools/misc/texinfo/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, ncurses}:
 
 stdenv.mkDerivation {
-  name = "texinfo-4.8a";
+  name = "texinfo-4.11";
   src = fetchurl {
-    url = mirror://gnu/texinfo/texinfo-4.8a.tar.bz2;
-    md5 = "0f429f87de9f20d6c0d952e63bf8e3fa";
+    url = mirror://gnu/texinfo/texinfo-4.11.tar.bz2;
+    sha256 = "1xzmjbrgf5l9c5ckglgsclalmz32m5nfdkn71b4adiwafx43s0v1";
   };
   buildInputs = [ncurses];
 }
