b67ce21440f651e75acca2cf24a2a7c2a870a5bc
Wouter den Breejen <uu@denbreejen.net>
2007-09-20 11:32:20 +0000

    Added auctex from mailing list
    svn path=/nixpkgs/trunk/; revision=9345

---
+++ pkgs/misc/tex/auctex/default.nix
@@ -0,0 +1,14 @@
+{ stdenv, fetchurl, emacs, tetex }:
+
+stdenv.mkDerivation {
+  name = "auctex-11.84";
+  meta = {
+    description = "AUCTeX is an extensible package for writing and formatting TeX files in GNU Emacs and XEmacs.";
+    homepage = http://www.gnu.org/software/auctex;
+  };
+  src = fetchurl {
+    url = http://ftp.gnu.org/pub/gnu/auctex/auctex-11.84.tar.gz;
+    md5 = "73970c51221524442c11cde13d0584e9";
+  };
+  buildInputs = [ emacs tetex ];
+}
