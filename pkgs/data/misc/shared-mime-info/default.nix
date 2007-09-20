ad5847f250a9c13371c45fd43e39fa9b8dc78233
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-09-20 19:18:55 +0000

    shared-mime-info: URL corrected & upgrade to 0.22
    svn path=/nixpkgs/trunk/; revision=9348

--- pkgs/data/misc/shared-mime-info/default.nix
+++ pkgs/data/misc/shared-mime-info/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, perl, perlXMLParser, pkgconfig, gettext, libxml2, glib}:
 
 stdenv.mkDerivation {
-  name = "shared-mime-info-0.21";
+  name = "shared-mime-info-0.22";
 
   src = fetchurl {
-	url = http://FIX_ME.org/shared-mime-info-0.21.tar.bz2;
-    sha256 = "050jyvnhwv0fdyfmdb8sdxkryw0vqnkkzz7ld6jl4ixiv2ikcnhk";
+	url = http://freedesktop.org/~hadess/shared-mime-info-0.22.tar.bz2;
+    sha256 = "1chz63v9jr009z9jhs07klybmhyf58i8vxipigf5gkdabjiclcyr";
   };
 
   buildInputs = [perl perlXMLParser pkgconfig gettext libxml2 glib];
