2c25696b762c4f36dfd5522c0e3cd0331f2c180d
Michael Raskin <7c6f434c@mail.ru>
2007-09-23 16:28:56 +0000

    Returned timidity and its expression with a last-minute-fix..
    svn path=/nixpkgs/trunk/; revision=9362

---
+++ pkgs/tools/misc/timidity/default.nix
@@ -0,0 +1,31 @@
+args: with args;
+stdenv.mkDerivation {
+  name = "timidity-2.13.0";
+
+  src = fetchurl {
+    url = http://ovh.dl.sourceforge.net/sourceforge/timidity/TiMidity++-2.13.0.tar.bz2;
+    sha256 = "1jbmk0m375fh5nj2awqzns7pdjbi7dxpjdwcix04zipfcilppbmf";
+  };
+
+  instruments = 
+	fetchurl {
+		url = http://www.csee.umbc.edu/pub/midia/instruments.tar.gz;
+		sha256 = "0lsh9l8l5h46z0y8ybsjd4pf6c22n33jsjvapfv3rjlfnasnqw67";
+	};
+
+  buildInputs =[alsaLib];
+
+  config=./timidity.cfg;
+
+  postInstall = "mkdir -p \$out/share/timidity/; 
+		cp \$config \$out/share/timidity/timidity.cfg; 
+		tar -xf \$instruments -C \$out/share/timidity/; 
+	";
+
+  meta = {
+    description = "
+	TiMidity - software MIDI renderer.
+";
+  };
+}
+
