d4d6b5424daeeed52fec5a249e682e324408f99d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-09-26 18:46:43 +0000

    * Chatzilla 0.9.78.1.
    svn path=/nixpkgs/trunk/; revision=9374

--- pkgs/applications/networking/irc/chatzilla/default.nix
+++ pkgs/applications/networking/irc/chatzilla/default.nix
@@ -1,17 +1,20 @@
 {stdenv, fetchurl, unzip}:
 
 stdenv.mkDerivation {
-  name = "chatzilla-0.9.74";
+  name = "chatzilla-0.9.78.1";
+  
   builder = ./builder.sh;
+  
   src = fetchurl {
     # Obtained from http://chatzilla.rdmsoft.com/xulrunner/.
-    url = http://chatzilla.rdmsoft.com/xulrunner/download/chatzilla-0.9.74-xr.zip;
-    md5 = "a1eada15b172eab6a771afa5f8670f7a";
+    url = http://chatzilla.rdmsoft.com/xulrunner/download/chatzilla-0.9.78.1-xr.zip;
+    sha256 = "1f7fgi1dvpzjsiai0vc986vd481i77hcafvnzm73bc2q8pa5g5mb";
   };
 
   buildInputs = [unzip];
 
   meta = {
+    homepage = http://chatzilla.hacksrus.com/;
     description = "Stand-alone version of Chatzilla, an IRC client";
   };
 }
