7841a05eb1730171cb416932f4d1dbf3df812041
Armijn Hemel <armijn@gpl-violations.org>
2007-10-03 15:38:19 +0000

    bump to 2.2.1
    svn path=/nixpkgs/trunk/; revision=9400

--- pkgs/applications/networking/instant-messengers/pidgin/default.nix
+++ pkgs/applications/networking/instant-messengers/pidgin/default.nix
@@ -17,10 +17,10 @@
 } :
 
 stdenv.mkDerivation {
-  name = "pidgin-2.2.0";
+  name = "pidgin-2.2.1";
   src = fetchurl {
-    url = mirror://sourceforge/pidgin/pidgin-2.2.0.tar.bz2;
-    sha256 = "1fisidcj2sp6lyqf2w9ib565ln3h9bgp0iswxy9bgqyknjqw3djc";
+    url = mirror://sourceforge/pidgin/pidgin-2.2.1.tar.bz2;
+    sha256 = "08sq0yc8vz6c24pdv64clv6s91a5idslpp7vll8vgspn4piyzyvf";
   };
 
   inherit nss ncurses;
