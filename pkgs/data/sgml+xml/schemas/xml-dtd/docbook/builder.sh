54111f87ba30427a8197c72217b1282a432581d7
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-10-19 09:07:01 +0000

    * Standards compliance.
    svn path=/nixpkgs/trunk/; revision=9476

--- pkgs/data/sgml+xml/schemas/xml-dtd/docbook/builder.sh
+++ pkgs/data/sgml+xml/schemas/xml-dtd/docbook/builder.sh
@@ -3,5 +3,5 @@ source $stdenv/setup
 mkdir -p $out/xml/dtd/docbook
 cd $out/xml/dtd/docbook
 unpackFile $src
-find -type f -exec chmod -x {} \;
+find . -type f -exec chmod -x {} \;
 eval "$postInstall"
