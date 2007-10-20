614a3b47c6ddd481088f83fa18e14a4fec95c39f
Armijn Hemel <armijn@gpl-violations.org>
2007-10-20 17:39:28 +0000

    update to 2.17
    svn path=/nixpkgs/trunk/; revision=9499

--- pkgs/tools/system/which/default.nix
+++ pkgs/tools/system/which/default.nix
@@ -1,8 +1,8 @@
 {stdenv, fetchurl}: stdenv.mkDerivation {
   name = "which-2.16";
   src = fetchurl {
-    url = http://ftp.gnu.org/gnu/which/which-2.16.tar.gz;
-    md5 = "830b83af48347a9a3520f561e47cbc9b";
+    url = http://ftp.gnu.org/gnu/which/which-2.17.tar.gz;
+    sha256 = "1nnnbn83da8481blmcyv96gm15ccsilr93fmgmwdlzj8a72fjvqp";
   };
 }
 
