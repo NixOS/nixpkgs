183d29946f04b2e82ff9885076522d0240d9008d
Armijn Hemel <armijn@gpl-violations.org>
2007-10-22 20:14:22 +0000

    security update 0.9.8g
    svn path=/nixpkgs/trunk/; revision=9510

--- pkgs/development/libraries/openssl/default.nix
+++ pkgs/development/libraries/openssl/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl, perl}: stdenv.mkDerivation {
-  name = "openssl-0.9.8e";
+  name = "openssl-0.9.8g";
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/security/openssl/openssl-0.9.8e.tar.gz;
-    sha256 = "0h03lrj99d44w2zjcr1mj210aza9j09s7zb5g5q53g2zp4l88kj1";
+    url = ftp://ftp.nluug.nl/pub/security/openssl/openssl-0.9.8g.tar.gz;
+    sha256 = "1w0wj3pgg9ga0hay3jdxs9sl17bfw307b6qvkxn735fy8ml8h9hf";
   };
   buildInputs = [perl];
 }
