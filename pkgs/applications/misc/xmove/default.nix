004057ea50b225f0585a969510b6ec7c09eba67d
Michael Raskin <7c6f434c@mail.ru>
2007-10-26 05:44:38 +0000

    Fixed xmove to work with unrecognizable hostnames as with localhost
    svn path=/nixpkgs/trunk/; revision=9530

--- pkgs/applications/misc/xmove/default.nix
+++ pkgs/applications/misc/xmove/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
     sha256 = "0q310k3bi39vdk0kqqvsahnb1k6lx9hlx80iyxnkq59l6jxnhyhf";
   };
 
-  buildPhase = "cd xmove; cp ../man/man1/xmove.1 xmove.man ; xmkmf; make; cd .. ; cd xmovectrl ; cp ../man/man1/xmovectrl.1 xmovectrl.man; xmkmf; make ; cd ..";
+  buildPhase = "cd xmove; sed -e 's/.*No address for our host.*/{hp = gethostbyname(\"localhost\");};/' -i main.c; cp ../man/man1/xmove.1 xmove.man ; xmkmf; make; cd .. ; cd xmovectrl ; cp ../man/man1/xmovectrl.1 xmovectrl.man; xmkmf; make ; cd ..";
   installPhase = "cd xmove; make install install.man MANDIR=\${out}/man/man1 BINDIR=\${out}/bin; cd .. ; cd xmovectrl ; make install install.man MANDIR=\${out}/man/man1 BINDIR=\${out}/bin; cd ..";
 
   buildInputs = [libX11 libXi imake xauth libXau];
