1bc214fd59019aefd60d6ea23c79fdc3293f2845
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-11-04 17:16:52 +0000

    * Latest MPlayer plugin.
    svn path=/nixpkgs/trunk/; revision=9576

--- pkgs/applications/networking/browsers/mozilla-plugins/mplayerplug-in/default.nix
+++ pkgs/applications/networking/browsers/mozilla-plugins/mplayerplug-in/default.nix
@@ -4,16 +4,25 @@
 # are the include files so that we can access the plugin API (I
 # think).
 
-(stdenv.mkDerivation {
-  name = "mplayerplug-in-3.35";
+stdenv.mkDerivation {
+  name = "mplayerplug-in-3.45";
 
   builder = ./builder.sh;
   src = fetchurl {
-    url = mirror://sourceforge/mplayerplug-in/mplayerplug-in-3.35.tar.gz;
-    sha256 = "0zxd2nnmj4n9rkndd614ljv7ylz4f4jqvx1wswqfw5j7hwxm34dw";
+    url = mirror://sourceforge/mplayerplug-in/mplayerplug-in-3.45.tar.gz;
+    sha256 = "0cfl0s7v1bgdak39x7s2hnx968qs3vlv09gmwms27czimn8vijiz";
   };
 
   buildInputs = [pkgconfig firefox (firefox.gtk) libXpm gettext];
   
   inherit firefox;
-}) // {mozillaPlugin = "/lib/mozilla/plugins";}
+
+  passthru = {
+    mozillaPlugin = "/lib/mozilla/plugins";
+  };
+
+  meta = {
+    description = "A browser plugin that uses mplayer to play digital media from websites";
+    homepage = http://mplayerplug-in.sourceforge.net/;
+  };
+}
