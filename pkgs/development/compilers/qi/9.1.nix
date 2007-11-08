b7a3dcdb9448e55535299acad23937f69f02cacf
Michael Raskin <7c6f434c@mail.ru>
2007-11-08 14:51:41 +0000

    Added missing Qi
    svn path=/nixpkgs/trunk/; revision=9602

---
+++ pkgs/development/compilers/qi/9.1.nix
@@ -0,0 +1,37 @@
+args : with args;
+	with builderDefs {
+		src = /* put a fetchurl here */
+		fetchurl {
+			url = http://www.lambdassociates.org/Download/Qi9.1.zip;
+			sha256 = "1j584i7pj38rnlf7v9njfdwc6gc296v5friw2887dsw34dmwyg3f";
+		};
+		buildInputs = [ unzip clisp];
+		configureFlags = [];
+	} null; /* null is a terminator for sumArgs */
+let 
+	bash=stdenv.bash ;
+in
+let 
+	allBuild = FullDepEntry ("
+		(sleep 0.1; echo ) | clisp install.txt;
+		(sleep 0.1; echo -e '1\n(quit)\n' ) | sh Qi-Linux-CLisp 
+		ensureDir \$out/share
+		ensureDir \$out/bin
+		cp -r . \$out/share/Qi-9.1
+		echo -e '#! ${bash}/bin/bash
+		arg1=\${1:-'\$out'/share/Qi-9.1/startup.txt}
+		shift
+		clisp -M '\$out'/share/Qi-9.1/lispinit.mem \$arg1 \"\$@\"\\n' > \$out/bin/qi
+		chmod a+x \$out/bin/qi
+	") [ addInputs minInit doUnpack defEnsureDir];
+in
+stdenv.mkDerivation rec {
+	name = "Qi-"+version;
+	builder = writeScript (name + "-builder")
+		(textClosure [allBuild doForceShare doPropagate]);
+	meta = {
+		description = "
+		Qi - next generation on top of Common Lisp.
+";
+	};
+}
