86959dbe0509172a2ed087d50876eeb71ff37598
Michael Raskin <7c6f434c@mail.ru>
2007-11-11 17:20:06 +0000

    urkud asked to fix a broken meta..
    svn path=/nixpkgs/trunk/; revision=9643

--- pkgs/development/libraries/libidn/default.nix
+++ pkgs/development/libraries/libidn/default.nix
@@ -7,9 +7,9 @@ stdenv.mkDerivation {
 		sha256 = "0cip97xskrsfp6v1v966jb921srl1s65a5d8s0l73s85yww55n73";
 	};
 
-	meta = [
+	meta = {
 		homepage = http://www.gnu.org/software/libidn;
 		description = "GNU Libidn library for internationalized domain names";
 		license = "LGPL";
-	];
+	};
 }
