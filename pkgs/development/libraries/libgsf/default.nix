3ae88cb1f361f6c96cbe43923aeb95790bfc7a44
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-11-14 23:44:21 +0000

    libgsf: meta written
    0.14.7 in my previous commit was a typo: 1.14.7 of course
    
    svn path=/nixpkgs/trunk/; revision=9681

--- pkgs/development/libraries/libgsf/default.nix
+++ pkgs/development/libraries/libgsf/default.nix
@@ -1,11 +1,17 @@
 args: with args;
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "libgsf-1.14.7";
   src = fetchurl {
-    url = http://ftp.gnome.org/pub/gnome/sources/libgsf/1.14/libgsf-1.14.7.tar.bz2;
+    url = "http://ftp.gnome.org/pub/gnome/sources/libgsf/1.14/${name}.tar.bz2";
 	sha256 = "0vd8arjaavb3qywd9cm2gdn6ngrlyd99nlsj72par8fm60k48bhq";
   };
   buildInputs = [perl perlXMLParser pkgconfig libxml2 glib gettext bzip2
   gnomevfs libbonobo];
+
+  meta = {
+	  homepage = http://www.gnome.org/projects/libgsf;
+	  license = "LGPL";
+	  description = "GNOME Structured File Library";
+  };
 }
