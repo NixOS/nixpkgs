16a5a77587576d500d30889334060d02cd7a9738
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-11-14 23:23:36 +0000

    dbus-glib upgraded to 0.74, meta written
    svn path=/nixpkgs/trunk/; revision=9677

--- pkgs/development/libraries/dbus-glib/default.nix
+++ pkgs/development/libraries/dbus-glib/default.nix
@@ -1,12 +1,18 @@
-{stdenv, fetchurl, pkgconfig, gettext, dbus, glib, expat}:
+args: with args;
 
 stdenv.mkDerivation {
-  name = "dbus-glib-0.73";
+  name = "dbus-glib-0.74";
   src = fetchurl {
-    url = http://dbus.freedesktop.org/releases/dbus-glib/dbus-glib-0.73.tar.gz;
-    sha256 = "14ndjhbn6q4m7wrml8s57wghnjbm6a6fqb5jgazjxcn6748gkmyn";
+    url = http://dbus.freedesktop.org/releases/dbus-glib/dbus-glib-0.74.tar.gz;
+    sha256 = "1qmbnd9xgg7vahlfywd8sfd9yqhx8jdyczz3cshfsd4qc76xhw78";
   };
   inherit dbus glib;
   buildInputs = [pkgconfig gettext glib expat];
   propagatedBuildInputs = [dbus];
+
+  meta = {
+	  homepage = http://dbus.freedesktop.org;
+	  license = "AFL-2.1 or GPL-2";
+	  description = "GLib bindings for D-Bus lightweight IPC mechanism";
+  };
 }
