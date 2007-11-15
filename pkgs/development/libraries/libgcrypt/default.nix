67ba3f1d4d3f9833a3bcf06cb5403c186a35cf65
Michael Raskin <7c6f434c@mail.ru>
2007-11-15 04:23:46 +0000

    Added a currently more functional URL for libgcrypt
    svn path=/nixpkgs/trunk/; revision=9688

--- pkgs/development/libraries/libgcrypt/default.nix
+++ pkgs/development/libraries/libgcrypt/default.nix
@@ -3,7 +3,10 @@ args: with args;
 stdenv.mkDerivation {
   name = "libgcrypt-1.3.1";
   src = fetchurl {
-    url = ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.3.1.tar.bz2;
+    urls = [
+      ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.3.1.tar.bz2
+      ftp://ftp.gnupg.org/gcrypt/alpha/libgcrypt/libgcrypt-1.3.1.tar.bz2
+    ];
     sha256 = "0ip0bjhnn12lvb050j91x64wfhmpk7xlc3p93mxi9g2qczg413nz";
   };
 
