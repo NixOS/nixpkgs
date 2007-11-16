8294a83e5df6538cf8b2b749e91f0e3727448071
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-11-16 17:28:17 +0000

    gphoto2 upgraded to 2.4.0
    svn path=/nixpkgs/trunk/; revision=9722

--- pkgs/applications/misc/gphoto2/default.nix
+++ pkgs/applications/misc/gphoto2/default.nix
@@ -1,10 +1,12 @@
-{stdenv, fetchurl, pkgconfig, libgphoto2, libexif, popt}:
+args: with args;
 
-stdenv.mkDerivation {
-  name = "gphoto2-2.2.0";
+stdenv.mkDerivation rec {
+  name = "gphoto2-2.4.0";
   src = fetchurl {
-    url = mirror://sourceforge/gphoto/gphoto2-2.2.0.tar.bz2;
-    md5 = "f5c1f83185db598b4ca52889964a5e84";
+    url = "mirror://sourceforge/gphoto/${name}.tar.bz2";
+    sha256 = "1rf4w5m35dsi8dkwwnh4wg70xivdi9j79f2dy3rq90p1v8sar9ca";
   };
-  buildInputs = [pkgconfig libgphoto2 libexif popt];
+  buildInputs = [pkgconfig libgphoto2 libexif popt gettext];
+# There is a bug in 2.4.0 configure.ac (in their m4 macroses)
+  patchPhase = "sed -e 's@_tmp=true@_tmp=false@' -i configure configure.ac";
 }
