0dcad9f5abaafec7c1df1cbfe3ea2ced3575fa4b
Wouter den Breejen <uu@denbreejen.net>
2007-11-16 14:33:36 +0000

    added libnotify
    svn path=/nixpkgs/trunk/; revision=9712

---
+++ pkgs/development/libraries/libnotify/default.nix
@@ -0,0 +1,20 @@
+{ stdenv, fetchurl
+, pkgconfig, dbus, dbus_glib
+, gtk, glib
+}:
+ 
+stdenv.mkDerivation {
+  name = "libnotify-0.4.4";
+  #builder = ./builder.sh;
+
+  src = fetchurl {
+    url = http://www.galago-project.org/files/releases/source/libnotify/libnotify-0.4.4.tar.gz;
+    sha256 = "2389a9b8220f776033f728a8d46352cfee5c8705066e34887bfb188f9f0d3856";
+  };
+
+  buildInputs = [
+    pkgconfig dbus dbus_glib gtk glib
+  ];
+
+  configureFlags="";
+}
