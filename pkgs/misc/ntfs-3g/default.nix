c1836bbafcd458a8206840e4ad6f7b2e4e56fec2
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-11-19 22:29:01 +0000

    ntfs-3g upgraded to 1.1104
    svn path=/nixpkgs/branches/stdenv-updates/; revision=9760

--- pkgs/misc/ntfs-3g/default.nix
+++ pkgs/misc/ntfs-3g/default.nix
@@ -1,15 +1,16 @@
 args: with args;
-stdenv.mkDerivation {
-  name = "ntfs3g-1.826";
+stdenv.mkDerivation rec {
+  name = "ntfs-3g-1.1104";
   src = fetchurl {
-    url = http://www.ntfs-3g.org/ntfs-3g-1.826.tgz;
-    sha256 = "0anxg4nzhc8d8wvxgw78bc2pb2ciim8mflxgcac9p8d3djwdsxyp";
+    url = "${meta.homepage}/${name}.tgz";
+    sha256 = "1m96c3vxm051lpy3kyik9s0m390rj6ngm11xmahfhw61794jzbyp";
   };
   buildInputs = [fuse pkgconfig];
   preConfigure="sed -e 's:/sbin:@sbindir@:' -i src/Makefile.in";
   configureFlags="--enable-shared --disable-static --disable-ldconfig --exec-prefix=\${prefix}";
 
   meta = {
+    homepage = http://www.ntfs-3g.org;
     description = "FUSE-base ntfs driver with full write support";
   };
 }
