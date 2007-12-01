a1f7a312c0e91a8f02a7e409460ba54e205674d9
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-12-01 20:04:21 +0000

    cpio: unneeded patches removed
    svn path=/nixpkgs/branches/stdenv-updates/; revision=9817

--- pkgs/tools/archivers/cpio/default.nix
+++ pkgs/tools/archivers/cpio/default.nix
@@ -6,5 +6,4 @@ stdenv.mkDerivation {
     url = mirror://gnu/cpio/cpio-2.9.tar.bz2;
     sha256 = "01s7f9hg8kgpis96j99hgkiqgdy53pm7qi7bhm3fzx58jfk5z6mv";
   };
-  patches = [./symlink.patch ./gnulib-futimens.patch];
 }
