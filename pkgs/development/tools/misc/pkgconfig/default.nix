216657f62ef53ce8cc8ee6e5a63bba91f4b4bf99
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-12-01 20:29:14 +0000

    setup-hooks using addToSearchPath
    I've accidentially reverted setup-hooks while merging with trunk. Correcting it.
    
    svn path=/nixpkgs/branches/stdenv-updates/; revision=9824

--- pkgs/development/tools/misc/pkgconfig/default.nix
+++ pkgs/development/tools/misc/pkgconfig/default.nix
@@ -7,15 +7,5 @@ stdenv.mkDerivation {
     url = http://pkgconfig.freedesktop.org/releases/pkg-config-0.22.tar.gz;
     sha256 = "1rpb5wygmp0f8nal7y3ga4556i7hkjdslv3wdq04fj30gns621vy";
   };
-
-  patches = [
-    # Process Requires.private properly, see
-    # http://bugs.freedesktop.org/show_bug.cgi?id=4738.
-    (fetchurl {
-      name = "pkgconfig-8494.patch";
-      url = http://bugs.freedesktop.org/attachment.cgi?id=8494;
-      sha256 = "1pcrdbb7dypg2biy0yqc7bdxak5zii8agqljdvk7j4wbyghpqzws";
-    })
-  ];
 }
 
