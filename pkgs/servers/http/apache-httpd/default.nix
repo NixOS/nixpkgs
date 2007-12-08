0e7e524cf30b663aa84ba72e461baa9031daa9eb
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-12-08 01:07:25 +0000

    * Work around an impurity in APR: don't detect the epoll() system   call, because then an Apache built on a 2.6 kernel won't work   on 2.4 kernel.  In fact Apache will get into a loop segfaulting.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=9881

--- pkgs/servers/http/apache-httpd/default.nix
+++ pkgs/servers/http/apache-httpd/default.nix
@@ -21,6 +21,13 @@ stdenv.mkDerivation {
   openssl = if sslSupport then openssl else null;
   db4 = if db4Support then db4 else null;
 
+  # For now, disable detection of epoll to ensure that Apache still
+  # runs on Linux 2.4 kernels.  Once we've dropped support for 2.4 in
+  # Nixpkgs, this can go.  In general, it's a problem that APR
+  # detects characteristics of the build system's kernel to decide
+  # what to use at runtime, since it's impure.
+  apr_cv_epoll = "no";
+  
   meta = {
     description = "Apache HTTPD, the world's most popular web server";
   };
