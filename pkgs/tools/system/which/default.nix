6281ec3eb81c6f2596536ec2934a093fc53f7169
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-12-08 01:09:57 +0000

    fix name (was 2.16, should have been 2.17)
    svn path=/nixpkgs/branches/stdenv-updates/; revision=9890

--- pkgs/tools/system/which/default.nix
+++ pkgs/tools/system/which/default.nix
@@ -1,5 +1,5 @@
 {stdenv, fetchurl}: stdenv.mkDerivation {
-  name = "which-2.16";
+  name = "which-2.17";
   src = fetchurl {
     url = http://ftp.gnu.org/gnu/which/which-2.17.tar.gz;
     sha256 = "1nnnbn83da8481blmcyv96gm15ccsilr93fmgmwdlzj8a72fjvqp";
