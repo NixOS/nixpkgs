ec6a614c1ec8639baf7987788315b6acd6c9a9c5
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-12-08 01:08:37 +0000

    Added indent
    svn path=/nixpkgs/branches/stdenv-updates/; revision=9885

---
+++ pkgs/development/tools/misc/indent/2.2.9.nix
@@ -0,0 +1,28 @@
+args : with args;
+	with builderDefs {
+		src = /* put a fetchurl here */
+		fetchurl {
+			url = ftp://ftp.gnu.org/gnu/indent/indent-2.2.9.tar.gz;
+			sha256 = "0rp4divzv5xyflm8ixpp2isa092n0nmhgmhvz56db7k3lnyqg4qb";
+		};
+
+		buildInputs = [];
+		configureFlags = [];
+	} null; /* null is a terminator for sumArgs */
+let 
+	preBuild = FullDepEntry ("
+		sed -e '/extern FILE [*]output/i#ifndef OUTPUT_DEFINED_ELSEWHERE' -i src/indent.h
+		sed -e '/extern FILE [*]output/a#endif' -i src/indent.h
+		sed -e '1i#define OUTPUT_DEFINED_ELSEWHERE 1' -i src/output.c
+	") [minInit doUnpack];
+in
+stdenv.mkDerivation rec {
+	name = "indent";
+	builder = writeScript (name + "-builder")
+		(textClosure [doConfigure preBuild doMakeInstall doForceShare doPropagate]);
+	meta = {
+		description = "
+	GNU Indent - a source text formatter.
+";
+	};
+}
