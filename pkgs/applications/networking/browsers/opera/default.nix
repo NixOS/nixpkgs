f72d74bafc8d346d12e6f17eb767e62be5db4739
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-12-08 01:09:41 +0000

    Opera updated - works now on x64-linux as well
    svn path=/nixpkgs/branches/stdenv-updates/; revision=9889

--- pkgs/applications/networking/browsers/opera/default.nix
+++ pkgs/applications/networking/browsers/opera/default.nix
@@ -1,7 +1,6 @@
-{ stdenv, fetchurl, qt, zlib, libX11, libXext, libSM, libICE, libstdcpp5
+{ stdenv, fetchurl, qt, zlib, libX11, libXext, libSM, libICE, libstdcpp5, glibc
 , motif ? null, libXt ? null}:
 
-assert stdenv.system == "i686-linux";
 assert motif != null -> libXt != null;
 
 # !!! Add Xinerama and Xrandr dependencies?  Or should those be in Qt?
@@ -12,13 +11,22 @@ stdenv.mkDerivation rec {
   version = "9.24-20071015.5";
   name = "opera-${version}";
 
-  builder = ./builder.sh;
-  src = fetchurl {
-    url = ftp://ftp.task.gda.pl/pub/opera/linux/924/final/en/i386/shared/opera-9.24-20071015.5-shared-qt.i386-en.tar.bz2;
-    sha256 = "1frhnrp63k4lz29a8z9c99h383xrsrby432xp20hxrylh0zypzb5";
-  };
+  inherit libstdcpp5;
 
+  builder = ./builder.sh;
+    src = if (stdenv.system == "i686-linux") then
+      fetchurl {
+        url = ftp://ftp.task.gda.pl/pub/opera/linux/950b/final/en/i386/shared/opera-9.50b-20071024.5-shared-qt.i386-en.tar.bz2;
+        sha256 = "0vv1q86is9x6vw8fx92wrnvlyn4x29zgk9zjn66rcx37n6grqqah";
+      } else if (stdenv.system == "x86_64-linux") then
+      fetchurl {
+        url = ftp://ftp.task.gda.pl/pub/opera/linux/950b/final/en/x86_64/opera-9.50-20071024.2-shared-qt.x86_64-1643.tar.bz2;
+        sha256 = "1gv1r18ar3vz1l24nf8qixjlba1yb5d3xvg3by41i4dy0vlznqn6";
+      } else throw "unsupported platform ${stdenv.system} (only i686-linux and x86_64 linux supported yet)";
+
+  # operapluginwrapper seems to require libXt ?
+  # Adding it makes startup faster and omits error messages (on x68)
   libPath =
-    [qt motif zlib libX11 libXext libSM libICE libstdcpp5]
-    ++ (if motif != null then [motif libXt ] else []);
+    [glibc qt motif zlib libX11 libXt libXext libSM libICE libstdcpp5]
+    ++ (if motif != null then [motif ] else []);
 }
