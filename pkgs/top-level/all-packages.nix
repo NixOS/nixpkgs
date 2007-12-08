f1f5b31c0c0a30fb496a09b9d1b78e1773ad4a99
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-12-08 01:07:38 +0000

    * Enable GD support in Nagios.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=9882

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2746,7 +2746,8 @@ rec {
   };
 
   nagios = import ../servers/monitoring/nagios {
-    inherit fetchurl stdenv perl;
+    inherit fetchurl stdenv perl gd libpng zlib;
+    gdSupport = true;
   };
 
   nagiosPluginsOfficial = import ../servers/monitoring/nagios/plugins/official {
