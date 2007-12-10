67b8350ee6e755dabde1ea763dcddf905c7882e2
Andres Löh <mail@andres-loeh.de>
2007-12-10 10:50:59 +0000

    * ghc-6.8 is no longer a snapshot * fixed typo in application of x86_64 patch, i.e.,     now really seems to work on x86_64 ...
    svn path=/nixpkgs/trunk/; revision=9898

--- pkgs/development/compilers/ghc-6.8/default.nix
+++ pkgs/development/compilers/ghc-6.8/default.nix
@@ -1,15 +1,15 @@
 {stdenv, fetchurl, readline, ghc, perl, m4, gmp, ncurses}:
 
 stdenv.mkDerivation (rec {
-  name = "ghc-6.8.0.20071018";
+  name = "ghc-6.8.1";
   homepage = "http://www.haskell.org/ghc";
 
   src = map fetchurl [
     { url = "${homepage}/dist/stable/dist/${name}-src.tar.bz2";
-      md5 = "7e61bd2a55c2d6ed5a6d996d19d3f6bf";
+      md5 = "8d47d4dcde96c31fe8bedcee7f99eaf1";
     }
     { url = "${homepage}/dist/stable/dist/${name}-src-extralibs.tar.bz2";
-      md5 = "7b155c1d1e7daa492cc2161b3828a377";
+      md5 = "f91de87e7c0a3fe2f27c5a83212d9743";
     }
   ];
 
@@ -18,7 +18,7 @@ stdenv.mkDerivation (rec {
   setupHook = ./setup-hook.sh;
 
   meta = {
-    description = "The Glasgow Haskell Compiler v6.8 (snapshot)";
+    description = "The Glasgow Haskell Compiler v6.8.1";
   };
 
   postInstall = "
@@ -32,7 +32,7 @@ stdenv.mkDerivation (rec {
   patch64 = ./x86_64-linux_patch;
 
   # Thanks to Ian Lynagh ghc now works on x86_64-linux as well 
-  patchPhase = if (stdenv.system == "x86_64-linx") then "patch -p2 < $patch64" else "";
+  patchPhase = if (stdenv.system == "x86_64-linux") then "patch -p2 < $patch64" else "";
 
   configureFlags="--with-gmp-libraries=$gmp/lib --with-readline-libraries=\"$readline/lib\"";
 
