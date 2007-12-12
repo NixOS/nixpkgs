0c8d30be0f52395505db757b4e829309d0045f93
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-12-12 00:24:48 +0000

    qt-4: removed unused files
    svn path=/nixpkgs/branches/stdenv-updates/; revision=9919

--- pkgs/development/libraries/qt-4/builder.sh
+++
@@ -1,9 +0,0 @@
-source $stdenv/setup
-
-ensureDir $out/nix-support
-substitute "$hook" "$out/nix-support/setup-hook" --subst-var out
-
-# !!! TODO: -system-libmng
-configureFlags="-prefix $out $configureFlags"
-
-genericBuild
