ccb13fd637b24bf04000857f4513c2c747705ae1
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-12-12 00:20:58 +0000

    openalSoft: nix-expr cleanup
    svn path=/nixpkgs/branches/stdenv-updates/; revision=9914

--- pkgs/development/libraries/openalSoft/default.nix
+++ pkgs/development/libraries/openalSoft/default.nix
@@ -1,20 +1,17 @@
-args:
-args.stdenv.mkDerivation {
+args: with args;
+stdenv.mkDerivation {
 #The current release is still in a testing phase, though it should be stable
 # (neither the ABI or API will break). Please try it out and let me know how it
 #  works. :-)
 
   name = "openal-soft-testing";
 
-  src = args.fetchurl {
-    url = http://kcat.strangesoft.net/OpenAL.tar.bz2?2;
+  src = fetchurl {
+    url = http://kcat.strangesoft.net/OpenAL.tar.bz2;
     sha256 = "7b53c3e6eda1a71010651eb058c71c9b0c86b3c15cae5f0ffeeb7222531aa97d";
   };
 
-  buildInputs =(with args; [cmake alsaLib]);
-  #phases = "buildPhase installPhase";
-
-  buildPhase = "ensureDir \$out; cmake -DCMAKE_INSTALL_PREFIX:PATH=\$out .; make; make install;";
+  buildInputs = [cmake alsaLib];
   
   meta = {
       description = "openal alternative";
