e19b3d7f79b332fa81802973e0025f2050d7971b
Yury G. Kudryashov <urkud.urkud@gmail.com>
2007-12-12 00:20:50 +0000

    clisp: upgraded to 2.43 (both versions do not work on stdenv-updates branch)
    svn path=/nixpkgs/branches/stdenv-updates/; revision=9913

--- pkgs/development/interpreters/clisp/default.nix
+++ pkgs/development/interpreters/clisp/default.nix
@@ -1,15 +1,14 @@
-args:
-args.stdenv.mkDerivation {
-  name = "clisp-2.41a";
+args: with args;
+stdenv.mkDerivation {
+  name = "clisp-2.43";
   builder = ./builder.sh;
-  src = args.
+  src =
 	fetchurl {
-		url = mirror://gnu/clisp/release/2.41/clisp-2.41a.tar.bz2;
-		sha256 = "08z35bni42dhlqlsg5rr5p025961fl82gqvaadrf0jh20jdqspqy";
+		url = mirror://gnu/clisp/release/2.43/clisp-2.43.tar.bz2;
+		sha256 = "10qyn6wccnayf1cyvrcanay6c6laar6z1r608w7ijp6nb763q8dm";
 	};
 
-  inherit (args) libsigsegv gettext coreutils;
-  buildInputs = (with args;
- [libsigsegv gettext ncurses readline libX11 libXau
-	libXt pcre zlib]);
+  inherit libsigsegv gettext coreutils;
+  buildInputs = [libsigsegv gettext ncurses readline libX11 libXau
+	libXt pcre zlib];
 }
