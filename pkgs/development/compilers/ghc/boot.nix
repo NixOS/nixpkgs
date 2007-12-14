65c1809b67369027ae75df71df735423ca60a13b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-12-14 17:51:11 +0000

    * Support for building a bootstrap GHC (ghcboot) on i686-darwin.   Building of the bootstrapped compiler isn't tested yet.
    svn path=/nixpkgs/trunk/; revision=9940

--- pkgs/development/compilers/ghc/boot.nix
+++ pkgs/development/compilers/ghc/boot.nix
@@ -1,20 +1,57 @@
 {stdenv, fetchurl, perl, readline, ncurses, gmp}:
 
-assert stdenv.system == "i686-linux" || stdenv.system == "x86_64-linux";
-
 stdenv.mkDerivation {
-  name = "ghc-6.4.2";
+  name = if stdenv.system == "i686-darwin" then "ghc-6.6.1" else "ghc-6.4.2";
   builder = ./boot.sh;
-  src = if stdenv.system == "i686-linux" then
-        (fetchurl {
-             url = http://nix.cs.uu.nl/dist/tarballs/ghc-6.4.2-i386-unknown-linux.tar.bz2;
-             md5 = "092fe2e25dab22b926babe97cc77db1f";
-        }) else
-        (fetchurl {
-             url = http://haskell.org/ghc/dist/6.4.2/ghc-6.4.2-x86_64-unknown-linux.tar.bz2;
-             md5 = "8f5fe48798f715cd05214a10987bf6d5";
-        });             
+
+  src =
+    if stdenv.system == "i686-linux" then
+      fetchurl {
+        url = http://nix.cs.uu.nl/dist/tarballs/ghc-6.4.2-i386-unknown-linux.tar.bz2;
+        md5 = "092fe2e25dab22b926babe97cc77db1f";
+      }
+    else if stdenv.system == "x86_64-linux" then
+      fetchurl {
+        url = http://haskell.org/ghc/dist/6.4.2/ghc-6.4.2-x86_64-unknown-linux.tar.bz2;
+        md5 = "8f5fe48798f715cd05214a10987bf6d5";
+      }
+    else if stdenv.system == "i686-darwin" then
+      fetchurl {
+        url = http://www.haskell.org/ghc/dist/6.6.1/ghc-6.6.1-i386-apple-darwin.tar.bz2;
+        sha256 = "1drbsicanr6jlykvs4vs6gbi2q9ac1bcaxz2vzwh3pfv3lfibwia";
+      }
+    else throw "cannot bootstrap GHC on this platform"; 
+
   buildInputs = [perl];
-  propagatedBuildInputs = [readline ncurses gmp];
-  inherit readline ncurses gmp;
+
+  # On Linux, use patchelf to modify the executables so that they can
+  # find readline/gmp.
+  postBuild = if stdenv.isLinux then "
+    find . -type f -perm +100 \\
+        -exec patchelf --interpreter \"$(cat $NIX_GCC/nix-support/dynamic-linker)\" \\
+        --set-rpath \"${readline}/lib:${ncurses}/lib:${gmp}/lib\" {} \;
+  " else "";
+
+  # The binaries for Darwin use frameworks, so fake those frameworks,
+  # and create some wrapper scripts that set DYLD_FRAMEWORK_PATH so
+  # that the executables work with no special setup.
+  postInstall = if stdenv.isDarwin then "
+
+    ensureDir $out/frameworks/GMP.framework/Versions/A
+    ln -s ${gmp}/lib/libgmp.dylib $out/frameworks/GMP.framework/GMP
+    ln -s ${gmp}/lib/libgmp.dylib $out/frameworks/GMP.framework/Versions/A/GMP
+    ensureDir $out/frameworks/GNUreadline.framework/Versions/A
+    ln -s ${readline}/lib/libreadline.dylib $out/frameworks/GNUreadline.framework/GNUreadline
+    ln -s ${readline}/lib/libreadline.dylib $out/frameworks/GNUreadline.framework/Versions/A/GNUreadline
+
+    mv $out/bin $out/bin-orig
+    mkdir $out/bin
+    for i in $(cd $out/bin-orig && ls); do
+        echo \"#! $SHELL -e\" >> $out/bin/$i
+        echo \"DYLD_FRAMEWORK_PATH=$out/frameworks exec $out/bin-orig/$i -framework-path $out/frameworks \\\"\\$@\\\"\" >> $out/bin/$i
+        chmod +x $out/bin/$i
+    done
+
+  " else "";
+
 }
