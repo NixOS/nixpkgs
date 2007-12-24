5c6cb19b3d1f6c19b2e9da65ede7736a90d9a823
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-12-24 13:27:07 +0000

    * Latest FUSE, sshfs.
    svn path=/nixpkgs/trunk/; revision=9964

--- pkgs/os-specific/linux/fuse/default.nix
+++ pkgs/os-specific/linux/fuse/default.nix
@@ -1,11 +1,16 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "fuse-2.6.5";
+  name = "fuse-2.7.2";
   builder = ./builder.sh;
   src = fetchurl {
-    url = mirror://sourceforge/fuse/fuse-2.6.5.tar.gz;
-    sha256 = "0901hrhi1z6dwlgvgn75cg2268wvaz53x0knn7jplk6acwir54db";
+    url = mirror://sourceforge/fuse/fuse-2.7.2.tar.gz;
+    sha256 = "1zxssdiirf19mihbnxpy0kiix35d4256b9lani7qwqi7m940jfgv";
   };
   configureFlags = [ "--disable-kernel-module" ];
+
+  meta = {
+    homepage = http://fuse.sourceforge.net/;
+    description = "Kernel module and library that allows filesystems to be implemented in user space";
+  };
 }
--- pkgs/tools/networking/sshfs-fuse/default.nix
+++ pkgs/tools/networking/sshfs-fuse/default.nix
@@ -1,10 +1,15 @@
 {stdenv, fetchurl, pkgconfig, glib, fuse}:
 
 stdenv.mkDerivation {
-  name = "sshfs-fuse-1.7";
+  name = "sshfs-fuse-1.9";
   src = fetchurl {
-    url = mirror://sourceforge/fuse/sshfs-fuse-1.7.tar.gz;
-    md5 = "e91a2fed1da952a375798408dc6e41a0";
+    url = mirror://sourceforge/fuse/sshfs-fuse-1.9.tar.gz;
+    sha256 = "10ishsghdwd6a1cd36gp26qpch6z8d6wjs7aw8vs0ffnvrs4hdza";
   };
   buildInputs = [pkgconfig glib fuse];
+
+  meta = {
+    homepage = http://fuse.sourceforge.net/sshfs.html;
+    description = "FUSE-based filesystem that allows remote filesystems to be mounted over SSH";
+  };
 }
