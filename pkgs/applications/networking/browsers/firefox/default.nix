78e7401dd19485e8fd7e3455f13ea8704d9585e3
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-12-30 21:47:12 +0000

    * Firefox 2.0.0.11.
    svn path=/nixpkgs/trunk/; revision=9971

--- pkgs/applications/networking/browsers/firefox/default.nix
+++ pkgs/applications/networking/browsers/firefox/default.nix
@@ -10,12 +10,12 @@
 }:
 
 stdenv.mkDerivation {
-  name = "firefox-2.0.0.9";
+  name = "firefox-2.0.0.11";
 
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://releases.mozilla.org/pub/mozilla.org/firefox/releases/2.0.0.9/source/firefox-2.0.0.9-source.tar.bz2;
-    sha1 = "3b39d4128534d18f7e2c4d76a14561c18556eff0";
+    url = http://releases.mozilla.org/pub/mozilla.org/firefox/releases/2.0.0.11/source/firefox-2.0.0.11-source.tar.bz2;
+    sha1 = "6d070bf04f1a037b621237d02ef9be7802a51bf1";
   };
 
   buildInputs = [
@@ -49,6 +49,7 @@ stdenv.mkDerivation {
 
   meta = {
     description = "Mozilla Firefox - the browser, reloaded";
+    homepage = http://www.mozilla.com/en-US/firefox/;
   };
 
   passthru = {inherit gtk;};
