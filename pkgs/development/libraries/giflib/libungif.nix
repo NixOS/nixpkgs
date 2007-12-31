317ebab2c24d5e45a84cbf4d7758b509aebce479
Armijn Hemel <armijn@gpl-violations.org>
2007-12-31 21:53:40 +0000

    add libungif expression for backwards compat
    svn path=/nixpkgs/trunk/; revision=10027

---
+++ pkgs/development/libraries/giflib/libungif.nix
@@ -0,0 +1,10 @@
+{stdenv, fetchurl}:
+
+stdenv.mkDerivation {
+  name = "libungif-4.1.4";
+  src = fetchurl {
+    url = mirror://sourceforge/giflib/libungif-4.1.4.tar.gz;
+    md5 = "efdfcf8e32e35740288a8c5625a70ccb";
+  };
+}
+
