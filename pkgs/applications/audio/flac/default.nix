9a2e58073ed942bd91bfaab4b3391713bc103a1c
Armijn Hemel <armijn@gpl-violations.org>
2007-12-31 20:49:10 +0000

    update to 1.2.1
    svn path=/nixpkgs/trunk/; revision=10020

--- pkgs/applications/audio/flac/default.nix
+++ pkgs/applications/audio/flac/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, libogg}:
 
 stdenv.mkDerivation {
-  name = "flac-1.1.2";
+  name = "flac-1.2.1";
   src = fetchurl {
-    url = http://downloads.xiph.org/releases/flac/flac-1.1.2.tar.gz;
-    md5 = "2bfc127cdda02834d0491ab531a20960";
+    url = http://downloads.xiph.org/releases/flac/flac-1.2.1.tar.gz;
+    sha256 = "1pry5lgzfg57pga1zbazzdd55fkgk3v5qy4axvrbny5lrr5s8dcn";
   };
 
   buildInputs = [libogg] ;
