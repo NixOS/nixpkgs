7f91ba32ec1bd3b734ee9ed3d579ccbc559d53b0
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-12-31 17:56:13 +0000

    * file 4.23.
    svn path=/nixpkgs/trunk/; revision=10002

--- pkgs/tools/misc/file/default.nix
+++ pkgs/tools/misc/file/default.nix
@@ -1,9 +1,14 @@
 {stdenv, fetchurl}:
  
 stdenv.mkDerivation {
-  name = "file-4.17";
+  name = "file-4.23";
   src = fetchurl {
-    url = ftp://ftp.astron.com/pub/file/file-4.17.tar.gz;
-    md5 = "50919c65e0181423d66bb25d7fe7b0fd";
+    url = ftp://ftp.astron.com/pub/file/file-4.23.tar.gz;
+    sha256 = "0iyiyzcs88k6r881l11zrg86ys3rnwjyh1bgx7hnfyjv8zk9db9g";
+  };
+
+  meta = {
+    description = "A program that shows the type of files";
+    homepage = ftp://ftp.astron.com/pub/file;
   };
 }
