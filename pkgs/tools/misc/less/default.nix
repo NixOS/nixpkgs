e0a14721b1bda16cf76ff003c31d091bc6ea733a
Armijn Hemel <armijn@gpl-violations.org>
2007-12-31 23:10:32 +0000

    update to 416
    svn path=/nixpkgs/trunk/; revision=10033

--- pkgs/tools/misc/less/default.nix
+++ pkgs/tools/misc/less/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, ncurses}:
  
 stdenv.mkDerivation {
-  name = "less-394";
+  name = "less-416";
  
   src = fetchurl {
-    url = http://www.greenwoodsoftware.com/less/less-394.tar.gz;
-    md5 = "a9f072ccefa0d315b325f3e9cdbd4b97";
+    url = http://www.greenwoodsoftware.com/less/less-416.tar.gz;
+    sha256 = "1nsmnczqqkiir1gi11dzrh1gssn2zmxrzqd692qy92dh86bmfilv";
   };
  
   buildInputs = [ncurses];
