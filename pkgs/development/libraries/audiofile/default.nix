306ecab2076385ce0e5edc12925b7c3722d03809
Armijn Hemel <armijn@gpl-violations.org>
2007-12-31 22:56:10 +0000

    update to 0.2.6
    svn path=/nixpkgs/trunk/; revision=10032

--- pkgs/development/libraries/audiofile/default.nix
+++ pkgs/development/libraries/audiofile/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "audiofile-0.2.3";
+  name = "audiofile-0.2.6";
   src = fetchurl {
-    url = http://www.68k.org/~michael/audiofile/audiofile-0.2.5.tar.gz;
-    md5 = "fd07c62a17ceafa317929e55e51e26c5";
+    url = http://www.68k.org/~michael/audiofile/audiofile-0.2.6.tar.gz;
+    sha256 = "1a921w6jwcnkmx3vm091qrj7109jzri6kw4ygjq6ym91dssnfqab";
   };
 }
