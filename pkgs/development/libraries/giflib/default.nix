21a0b76f5e51c2c95b88876073dfe96bc8192f79
Armijn Hemel <armijn@gpl-violations.org>
2007-12-31 21:52:09 +0000

    libungif has moved due to LZW patent expirations:
    http://sourceforge.net/forum/forum.php?forum_id=753553
    
    
    svn path=/nixpkgs/trunk/; revision=10025

--- pkgs/development/libraries/giflib/default.nix
+++ pkgs/development/libraries/giflib/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation {
   name = "libungif-4.1.4";
   src = fetchurl {
-    url = mirror://sourceforge/libungif/libungif-4.1.4.tar.gz;
+    url = mirror://sourceforge/giflib/libungif-4.1.4.tar.gz;
     md5 = "efdfcf8e32e35740288a8c5625a70ccb";
   };
 }
