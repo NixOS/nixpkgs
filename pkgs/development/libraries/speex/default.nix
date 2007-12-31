c032b571bcefe64275cb5e13600105765878e786
Armijn Hemel <armijn@gpl-violations.org>
2007-12-31 18:06:15 +0000

    1.2beta2 -> 1.2beta3
    svn path=/nixpkgs/trunk/; revision=10003

--- pkgs/development/libraries/speex/default.nix
+++ pkgs/development/libraries/speex/default.nix
@@ -1,10 +1,10 @@
 args: with args;
 
 stdenv.mkDerivation {
-  name = "speex-1.2beta2";
+  name = "speex-1.2beta3";
   src = fetchurl {
-    url = http://downloads.us.xiph.org/releases/speex/speex-1.2beta2.tar.gz;
-	sha256 = "1np34q5i7yswkgknb8pa6ngqb4l4jv84c9yqnn0215vncbl76xg5";
+    url = http://downloads.us.xiph.org/releases/speex/speex-1.2beta3.tar.gz;
+	sha256 = "1az7kiwa8mzi1x7j01gcakx854qcbm4g67n0c4s56bvny6dn18vp";
   };
   buildInputs = [libogg];
 }
