8ae848454a05983ff32000a212dcafd893ec6259
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-12-31 21:56:38 +0000

    * Fix GnuPG download location.
    svn path=/nixpkgs/trunk/; revision=10028

--- pkgs/tools/security/gnupg/default.nix
+++ pkgs/tools/security/gnupg/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
   name = "gnupg-1.4.8";
   builder = ./builder.sh;
   src = fetchurl {
-    url = ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-1.4.8.tar.bz2;
+    url = ftp://ftp.cert.dfn.de/pub/tools/crypt/gcrypt/gnupg/gnupg-1.4.8.tar.bz2;
     sha1 = "4b63267358e5c70f05b48e27d6877bad2636cabd";
   };
   buildInputs = [readline];
@@ -22,5 +22,5 @@ stdenv.mkDerivation {
   meta = {
     description = "A free implementation of the OpenPGP standard for encrypting and signing data";
     homepage = http://www.gnupg.org/;
-  };  
+  };
 }
