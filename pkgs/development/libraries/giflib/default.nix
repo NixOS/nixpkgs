8c0e2ba0c0fc9a056304bd8b264b866b8cac2c03
Armijn Hemel <armijn@gpl-violations.org>
2007-12-31 22:10:46 +0000

    giflib 4.1.6
    svn path=/nixpkgs/trunk/; revision=10029

--- pkgs/development/libraries/giflib/default.nix
+++ pkgs/development/libraries/giflib/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "libungif-4.1.4";
+  name = "giflib-4.1.6";
   src = fetchurl {
-    url = mirror://sourceforge/giflib/libungif-4.1.4.tar.gz;
-    md5 = "efdfcf8e32e35740288a8c5625a70ccb";
+    url = mirror://sourceforge/giflib/giflib-4.1.6.tar.bz2;
+    sha256 = "1v9b7ywz7qg8hli0s9vv1b8q9xxb2xvqq2mg1zpr73xwqpcwxhg1";
   };
 }
 
