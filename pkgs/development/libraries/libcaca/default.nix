346f103a1d83709cc103d60b7d4b028ee0cdb7e5
Armijn Hemel <armijn@gpl-violations.org>
2007-12-31 19:02:49 +0000

    update to 0.99.beta13b
    svn path=/nixpkgs/trunk/; revision=10009

--- pkgs/development/libraries/libcaca/default.nix
+++ pkgs/development/libraries/libcaca/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, ncurses}:
 
 stdenv.mkDerivation {
-  name = "libcaca-0.99-beta11";
+  name = "libcaca-0.99-beta13b";
   src = fetchurl {
-    url = http://libcaca.zoy.org/files/libcaca-0.99.beta11.tar.gz;
-    sha256 = "1kj0rkfbmq8kc3pi3p323ifx5yp9pcmbnxln7phxj5k4v7ngyld7";
+    url = http://libcaca.zoy.org/files/libcaca-0.99.beta13b.tar.gz;
+    sha256 = "0xy8pcnljnj5la97bzbwwyzyqa7dr3v9cyw8gdjzdfgqywvac1vg";
   };
   configureFlags = "--disable-x11 --disable-imlib2 --disable-doc";
   propagatedBuildInputs = [ncurses];
