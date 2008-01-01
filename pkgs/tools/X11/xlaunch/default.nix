9784b59b8b3423655247e6b9aa20efb3e6c967d9
Michael Raskin <7c6f434c@mail.ru>
2008-01-01 11:49:18 +0000

    Another attempt to fix xlaunch
    svn path=/nixpkgs/trunk/; revision=10038

--- pkgs/tools/X11/xlaunch/default.nix
+++ pkgs/tools/X11/xlaunch/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
 	buildCommand = "
 		mkdir -p \$out/bin
 		echo '
-			(egrep \"^ +env\" /etc/event.d/xserver | sed -e s/env// ; echo X;) | bash &
+			(egrep \"^ +env\" /etc/event.d/xserver | sed -e s/env/ export / ; echo X;) | bash &
 			sleep 15; 
 			\"$@\";
 		' >\$out/bin/xlaunch
