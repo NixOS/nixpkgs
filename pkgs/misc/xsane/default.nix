12de73494919dc033d20bd0b311f3dd655a9d4ea
Armijn Hemel <armijn@gpl-violations.org>
2008-01-01 00:15:19 +0000

    update to 0.995
    svn path=/nixpkgs/trunk/; revision=10035

--- pkgs/misc/xsane/default.nix
+++ pkgs/misc/xsane/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, saneBackends, saneFrontends,
 	libX11, gtk, pkgconfig, libusb ? null}:
 stdenv.mkDerivation {
-  name = "xsane-0.97";
+  name = "xsane-0.995";
 
   src = fetchurl {
-    url = ftp://ftp.sane-project.org/pub/sane/xsane/xsane-0.97.tar.gz;
-    md5 = "3d1f889d88c3462594febd53be58c561";
+    url = ftp://ftp.sane-project.org/pub/sane/xsane/xsane-0.995.tar.gz;
+    sha256 = "02rl5bkk3z3k6s04ki14l12vsl5jnx4mri66m1hl799m7hjl5836";
   };
 
   buildInputs = [saneBackends saneFrontends libX11 gtk pkgconfig ] ++
