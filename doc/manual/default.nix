e9a7f80681e3005c6edfdfcdf11099d544cc515a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-01-04 14:24:42 +0000

    * Build the Nix manual in a Nix expression instead of a Makefile so   that it becomes easier to include it on the CD.
    svn path=/nixos/trunk/; revision=10070

---
+++ doc/manual/default.nix
@@ -0,0 +1,42 @@
+let
+
+  pkgs = import ../../pkgs/top-level/all-packages.nix {};
+
+  options = builtins.toFile "options.xml" (builtins.unsafeDiscardStringContext
+    (builtins.toXML (pkgs.lib.optionAttrSetToDocList ""
+      (import ../../system/options.nix {pkgs = pkgs; mkOption = pkgs.lib.mkOption;}))));
+
+  optionsDocBook = pkgs.runCommand "options-db.xml" {} ''
+    ${pkgs.libxslt}/bin/xsltproc -o $out ${./options-to-docbook.xsl} ${options} 
+  '';
+    
+  manual = pkgs.stdenv.mkDerivation {
+    name = "nixos-manual";
+
+    sources = pkgs.lib.sourceFilesBySuffices ./. [".xml"];
+
+    buildInputs = [pkgs.libxslt];
+
+    xsltFlags = ''
+      --param section.autolabel 1
+      --param section.label.includes.component.label 1
+      --param html.stylesheet 'style.css'
+      --param xref.with.number.and.title 1
+      --param toc.section.depth 3
+      --param admon.style '''
+      --param callout.graphics.extension '.gif'
+    '';
+
+    buildCommand = ''
+      ensureDir $out
+      ln -s $sources/*.xml .
+      ln -s ${optionsDocBook} options-db.xml
+      xsltproc $xsltFlags --nonet --xinclude \
+        --output $out/manual.html \
+        ${pkgs.docbook5_xsl}/xml/xsl/docbook/html/docbook.xsl \
+        ./manual.xml
+      cp ${./style.css} $out/style.css
+    '';
+  };
+
+in manual
