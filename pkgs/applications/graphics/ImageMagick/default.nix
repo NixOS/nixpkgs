0966b36824b5706282ef9ff9bdeba8e071c34d89
Armijn Hemel <armijn@gpl-violations.org>
2008-01-07 13:49:25 +0000

    update
    svn path=/nixpkgs/trunk/; revision=10088

--- pkgs/applications/graphics/ImageMagick/default.nix
+++ pkgs/applications/graphics/ImageMagick/default.nix
@@ -1,10 +1,10 @@
 args: with args;
 (stdenv.mkDerivation ({
-  name = "ImageMagick-6.3.5";
+  name = "ImageMagick-6.3.7-9";
 
   src = fetchurl {
-    url = ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.3.5-5.tar.bz2;
-    sha256 = "0avq6kllxw552krxgsa72c1b44zwyhwi38dk4a4ij3fqy0svy9zh";
+    url = ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.3.7-9.tar.bz2;
+    sha256 = "1s38s78xvzm20ib22zypsb9vvzva0kbqjf0pf4c1q89jzg205pk0";
   };
 
   configureFlags = " --with-dots --with-gs-font-dir="+ ghostscript +
