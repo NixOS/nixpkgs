8a71f361503c28227b0aa3a93ee1049beb64a362
Wouter den Breejen <uu@denbreejen.net>
2008-01-14 15:18:11 +0000

    The sources of xterm have changed again...
    svn path=/nixpkgs/trunk/; revision=10142

--- pkgs/applications/misc/xterm/default.nix
+++ pkgs/applications/misc/xterm/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "xterm-208";
   src = fetchurl {
     url = ftp://invisible-island.net/xterm/xterm.tar.gz;
-    md5 = "f7b04a66dc401dc22f5ddb7f345be229"; /* was a062d0b398918015d07c31ecdcc5111a  */
+    md5 = "b767d702e1464e16802b90c2187252c6"; /* was f7b04a66dc401dc22f5ddb7f345be229 */ /* was a062d0b398918015d07c31ecdcc5111a  */
   };
   buildInputs = [libXaw xproto libXt libX11 libSM libICE ncurses];
 }
