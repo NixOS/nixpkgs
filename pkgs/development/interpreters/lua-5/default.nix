453dcda812556c4e48ef74cffbd22c28532d9eb1
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-01-15 01:41:37 +0000

    lua-5: url correted
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10148

--- pkgs/development/interpreters/lua-5/default.nix
+++ pkgs/development/interpreters/lua-5/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation {
   name = "lua-5.1.2";
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/lua-5.1.2.tar.gz;
+    url = http://www.lua.org/ftp/lua-5.1.2.tar.gz;
     sha256 = "17ixifwgjva5592s2rn1ki56wa7hgw0z210r4bcx5lv8zv39iw2w";
   };
   buildFlags = "linux"; # TODO: support for non-linux systems
