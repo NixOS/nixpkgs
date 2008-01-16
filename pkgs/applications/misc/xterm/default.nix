55b660a13a9da5f8cd385374e8bf171bce4e6a74
Andres Löh <mail@andres-loeh.de>
2008-01-16 09:13:52 +0000

    for xterm: * update version number * enable unicode support
    svn path=/nixpkgs/trunk/; revision=10167

--- pkgs/applications/misc/xterm/default.nix
+++ pkgs/applications/misc/xterm/default.nix
@@ -1,11 +1,12 @@
 {stdenv, fetchurl, libXaw, xproto, libXt, libX11, libSM, libICE, ncurses}:
 
 stdenv.mkDerivation {
-  name = "xterm-208";
+  name = "xterm-231";
   src = fetchurl {
     url = ftp://invisible-island.net/xterm/xterm-231.tgz;
     md5 = "b767d702e1464e16802b90c2187252c6"; /* was f7b04a66dc401dc22f5ddb7f345be229 */ /* was a062d0b398918015d07c31ecdcc5111a  */
   };
   buildInputs = [libXaw xproto libXt libX11 libSM libICE ncurses];
+  configureFlags = ["--enable-wide-chars"];
 }
 
