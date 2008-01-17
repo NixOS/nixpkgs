817d45cb232d941b5cf09e14757ad2dd5ee092be
Marc Weber <marco-oweber@gmx.de>
2008-01-17 00:34:19 +0000

    fix (now installs into  ;)
    svn path=/nixpkgs/trunk/; revision=10183

--- pkgs/development/libraries/glew/default.nix
+++ pkgs/development/libraries/glew/default.nix
@@ -7,11 +7,13 @@ args.stdenv.mkDerivation {
     sha256 = "1kjr1fchnl785wsg11vzc03q3pm12lh20n1i593zr1xqfjgx2b4h";
   };
 
-  buildInputs =(with args; [mesa x11 libXmu libXi libtool]);
+  buildInputs =(with args; [mesa x11 libXmu libXi]);
 
   meta = { 
       description = "cross-platform open-source C/C++ extension loading library";
       homepage = http://glew.sourceforge.net/;
       license = ["BSD" "GLX" "SGI-B" "GPL2"]; # License description copied from gentoo-1.4.0 
   };
+
+  installPhase="GLEW_DEST=\$out make install";
 }
