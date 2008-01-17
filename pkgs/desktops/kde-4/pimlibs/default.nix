ed035b32993fdcb5de5b43738d8ed879248897bb
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-01-17 13:02:41 +0000

    pimlibs are compilable now
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10199

--- pkgs/desktops/kde-4/pimlibs/default.nix
+++ pkgs/desktops/kde-4/pimlibs/default.nix
@@ -1,12 +1,13 @@
 args: with args;
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "kdepimlibs-4.0.0";
   
   src = fetchurl {
-    url = mirror://kde/stable/4.0/src/kdepimlibs-4.0.0.tar.bz2;
+    url = "mirror://kde/stable/4.0/src/${name}.tar.bz2";
 	sha256 = "0vixx2vh7qgysnbzvykf20362p22jzvl8snpqaknay3v3b2k0br0";
   };
 
   propagatedBuildInputs = [kdelibs boost gpgme cyrus_sasl openldap];
+  buildInputs = [cmake];
 }
