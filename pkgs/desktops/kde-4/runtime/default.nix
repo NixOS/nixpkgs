2412e8364b3bca62d021fb1b87bf2a86cad3ab99
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-01-17 13:03:21 +0000

    Added samba support to kde4-runtime
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10200

--- pkgs/desktops/kde-4/runtime/default.nix
+++ pkgs/desktops/kde-4/runtime/default.nix
@@ -1,14 +1,16 @@
 args: with args;
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "kdebase-runtime-4.0.0";
   
   src = fetchurl {
-    url = mirror://kde/stable/4.0/src/kdebase-runtime-4.0.0.tar.bz2;
+    url = "mirror://kde/stable/4.0/src/${name}.tar.bz2";
     sha256 = "0svsn9gzg3ka77j7z71fy502a09w9gp9jd2q2y1w07ahpdil5p7h";
   };
 
-  propagatedBuildInputs = [kdepimlibs libusb xineLib];
-  patchPhase = "fixCmakeDbusCalls";
+  propagatedBuildInputs = [kdepimlibs libusb xineLib samba];
+  buildInputs = [cmake];
+  phononPatch = ./phonon.patch;
+  patchPhase = "fixCmakeDbusCalls; patch -p0 < ${phononPatch}";
 }
 
