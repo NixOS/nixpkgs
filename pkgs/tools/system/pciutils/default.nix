8215b6c953ed2c89db616702d788ffaea6186ba3
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-01-18 14:39:24 +0000

    * pciutils 2.2.8, with the latest pci.ids.
    svn path=/nixpkgs/trunk/; revision=10218

--- pkgs/tools/system/pciutils/default.nix
+++ pkgs/tools/system/pciutils/default.nix
@@ -1,16 +1,16 @@
 {stdenv, fetchurl, zlib}:
 
 stdenv.mkDerivation {
-  name = "pciutils-2.2.5";
+  name = "pciutils-2.2.8";
   builder = ./builder.sh;
   src = fetchurl {
-    url = mirror://kernel/software/utils/pciutils/pciutils-2.2.5.tar.bz2;
-    sha256 = "1zlamdax1i8cf3kvzg7dh7nshvvzlhvkwlxdmym0hbnmbf0p100p";
+    url = mirror://kernel/software/utils/pciutils/pciutils-2.2.8.tar.bz2;
+    sha256 = "0hgri2ancnjl56ld2flb9w606dyvr5gly8gsz3bzl71r8s464qsq";
   };
   buildInputs = [zlib];
 
   pciids = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/pci.ids.20070226.bz2;
-    sha256 = "1wrpq4dxm03v5jvvdlvwl8nrkj3hspgifkw5czmd647lzikp13qc";
+    url = http://nix.cs.uu.nl/dist/tarballs/pci.ids.20080118.bz2;
+    sha256 = "0dl6psdac62llbklxn4dvkzbw1j1sdadw9i4l36vpd6mvqa7lz0a";
   };
 }
