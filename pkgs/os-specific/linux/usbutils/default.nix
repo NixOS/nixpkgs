4bbd0d9aad5293cd9c7764632872e870a43b5365
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-01-18 15:18:02 +0000

    * Latest usbutils, usb.ids.
    svn path=/nixpkgs/trunk/; revision=10219

--- pkgs/os-specific/linux/usbutils/default.nix
+++ pkgs/os-specific/linux/usbutils/default.nix
@@ -3,17 +3,17 @@
 let
 
   usbids = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/usb.ids.20061212.bz2;
-    sha256 = "112l4fzjn5p3y6fv3x10vbrd36n2v5n04s7pjdlkb2yqv4crp84m";
+    url = http://nix.cs.uu.nl/dist/tarballs/usb.ids.20080115.bz2;
+    sha256 = "0xymp8fpp9pnkj2i4ry8zpsvy18zw14sx03pnz316lpgwc6dx12n";
   };
 
 in
 
 stdenv.mkDerivation {
-  name = "usbutils-0.72";
+  name = "usbutils-0.73";
   src = fetchurl {
-    url = mirror://sourceforge/linux-usb/usbutils-0.72.tar.gz;
-    sha256 = "08s4g4sz7p3a1afvphxd7h5bbfywvp0j611y85wbhwr14i9m6f00";
+    url = mirror://sourceforge/linux-usb/usbutils-0.73.tar.gz;
+    sha256 = "1x27mc2apyipf8fa2ac49rfnkm7f5dwv784b1ncgc5yjiz4prp1f";
   };
   buildInputs = [libusb];
   preBuild = "bunzip2 < ${usbids} > usb.ids";
