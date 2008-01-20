e2c0809777a2ced9f83fda44b11a283e1a59a9ae
Michael Raskin <7c6f434c@mail.ru>
2008-01-20 22:55:43 +0000

    fixed "which" src
    svn path=/nixpkgs/trunk/; revision=10235

--- pkgs/tools/system/which/default.nix
+++ pkgs/tools/system/which/default.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchurl, readline}: stdenv.mkDerivation {
   name = "which-2.19";
   src = fetchurl {
-    url = mirror://gnu/pub/gnu/which/which-2.19.tar.gz;
+    url = mirror://gnu/which/which-2.19.tar.gz;
     sha256 = "0lnd8mfpc0r1r2ch54vl3vc6r0fnzfl33sqdda2aq62iyrsbhybx";
   };
 
