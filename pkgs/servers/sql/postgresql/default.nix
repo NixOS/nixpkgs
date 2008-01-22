0885dcb0a25e02ebc4de8a1061f01a5ac24920d0
Marc Weber <marco-oweber@gmx.de>
2008-01-22 14:29:38 +0000

    postgresql updated to 8.2.6
    svn path=/nixpkgs/trunk/; revision=10244

--- pkgs/servers/sql/postgresql/default.nix
+++ pkgs/servers/sql/postgresql/default.nix
@@ -5,12 +5,12 @@ assert ncurses != null;
 assert readline != null;
 
 stdenv.mkDerivation {
-  name = "postgresql-8.0.6";
+  name = "postgresql-8.2.6";
   builder = ./builder.sh;
 
   src = fetchurl {
-    url = ftp://ftp2.nl.postgresql.org/mirror/postgresql/source/v8.0.6/postgresql-8.0.6.tar.bz2;
-    md5 = "f3b27b8171267f9a87592f931c09f0ee";
+    url = ftp://ftp.de.postgresql.org/mirror/postgresql/source/v8.2.6/postgresql-8.2.6.tar.bz2;
+    sha256="056ixbsfmdwhniryc0mr1kl66jywkqqhqvjdi7i3v4qzh9z34hgf";
   };
 
   inherit readline;
