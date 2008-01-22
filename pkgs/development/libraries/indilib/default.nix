9ba0ec1be90b2c339bd6ca6fbbf8eb4892c2ed5c
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-01-22 13:08:59 +0000

    indilib added
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10239

---
+++ pkgs/development/libraries/indilib/default.nix
@@ -0,0 +1,16 @@
+args: with args;
+stdenv.mkDerivation {
+  name = "indilib-0.5";
+
+  src = fetchurl {
+    url = mirror://sf/indi/indilib-0.5.tar.gz;
+    sha256 = "02km37m3d2l8c9wnab24zm2k6a3l8h2fali74jhm4z3khwr277ad";
+  };
+
+  propagatedBuildInputs = [cfitsio libusb zlib];
+  configureFlags = "--enable-shared --disable-static";
+
+  meta = {
+    homepage = http://indi.sf.net;
+  };
+}
