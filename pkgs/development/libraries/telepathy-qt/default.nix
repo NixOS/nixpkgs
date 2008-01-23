4de89f61b00307debd0aebc36658a384e320bf30
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-01-23 16:34:33 +0000

    Added some telepathy+tapioca stuff.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10266

---
+++ pkgs/development/libraries/telepathy-qt/default.nix
@@ -0,0 +1,16 @@
+args: with args;
+let rev = "2031"; in
+stdenv.mkDerivation {
+	name = "telepathy-qt-r${rev}";
+	src = fetchsvn {
+		url = "https://tapioca-voip.svn.sourceforge.net/svnroot/tapioca-voip/trunk/telepathy-qt";
+		inherit rev;
+		sha256 = "0d7psgc8nr5bryrjgfg92622hbilp0qwx0pya3836bz2l6x3msnb";
+	};
+
+	buildInputs = [cmake];
+	propagatedBuildInputs = [qt];
+	meta = {
+		description = "Qt bindings for telepathy";
+	};
+}
