6f6477d60fb8d3433a08fbe777bd4fbbf3cfb7c5
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-01-24 16:56:56 +0000

    * Latest manpages.
    svn path=/nixpkgs/trunk/; revision=10277

--- pkgs/data/documentation/man-pages/default.nix
+++ pkgs/data/documentation/man-pages/default.nix
@@ -1,14 +1,19 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "man-pages-2.43";
+  name = "man-pages-2.74";
   
   src = fetchurl {
-    url = ftp://ftp.win.tue.nl/pub/linux-local/manpages/man-pages-2.43.tar.gz;
-    sha256 = "01dibzkssaq0ssq61adhmri29ws9jbhbn2yxmjvb3gg8q7gjah9w";
+    url = ftp://ftp.win.tue.nl/pub/linux-local/manpages/man-pages-2.74.tar.gz;
+    sha256 = "1k6hf6va29gnf2c9kpwd6w555gp1vimf73ac2ij2j7dqx64hy7s7";
   };
 
   preBuild = "
     makeFlagsArray=(MANDIR=$out/share/man)
   ";
+
+  meta = {
+    description = "Linux development manual pages";
+    homepage = ftp://ftp.win.tue.nl/pub/linux-local/manpages/;
+  };
 }
