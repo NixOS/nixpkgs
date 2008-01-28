de6ab6354f4215431269189384f4dacb3e3721a8
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-01-28 19:50:39 +0000

    Clisp is compilable
    svn path=/nixpkgs/trunk/; revision=10363

--- pkgs/development/interpreters/clisp/default.nix
+++ pkgs/development/interpreters/clisp/default.nix
@@ -1,14 +1,36 @@
 args: with args;
-stdenv.mkDerivation {
-  name = "clisp-2.43";
-  builder = ./builder.sh;
+stdenv.mkDerivation rec {
+  v = "2.43";
+  name = "clisp-${v}";
   src =
 	fetchurl {
-		url = mirror://gnu/clisp/release/2.43/clisp-2.43.tar.bz2;
+		url = "mirror://gnu/clisp/release/${v}/${name}.tar.bz2";
 		sha256 = "10qyn6wccnayf1cyvrcanay6c6laar6z1r608w7ijp6nb763q8dm";
 	};
 
   inherit libsigsegv gettext coreutils;
   buildInputs = [libsigsegv gettext ncurses readline libX11 libXau
 	libXt pcre zlib];
+  
+  patchPhase = ''
+  sed -i 's@/bin/pwd@${coreutils}&@' src/clisp-link.in
+  find . -type f | xargs sed -e 's/-lICE/-lXau &/' -i
+  '';
+
+  configureFlags = "--with-readline builddir --with-dynamic-ffi
+  --with-module=clx/new-clx --with-module=i18n --with-module=bindings/glibc
+  --with-module=pcre --with-module=rawsock --with-module=readline
+  --with-module=syscalls --with-module=wildcard --with-module=zlib";
+
+  preBuild = "cd builddir";
+
+  NIX_CFLAGS_COMPILE="-O0";
+
+  # TODO : make mod-check fails
+  doCheck = 1;
+
+  meta = {
+	  description = "ANSI Common Lisp Implementation";
+	  homepage = http://clisp.cons.org;
+  };
 }
