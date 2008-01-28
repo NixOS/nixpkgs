a76f4ff7e20158b8b57a9ea192ace2c14fc9dc5b
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-01-28 20:01:12 +0000

    Reverted default gettext to 0.14.6
    svn path=/nixpkgs/trunk/; revision=10366

--- pkgs/development/libraries/gettext/default.nix
+++ pkgs/development/libraries/gettext/default.nix
@@ -4,5 +4,5 @@ args.stdenv.lib.listOfListsToAttrs [
 	[ "0.14.6" (import ./0.14.6.nix args) ]
 	[ "0.15" (import ./0.15.nix args) ]
 	[ "0.16.x" (import ./0.16.x.nix args) ]
-	[ "default" (import ./0.16.x.nix args) ]
+	[ "default" (import ./0.14.6.nix args) ]
 ]
