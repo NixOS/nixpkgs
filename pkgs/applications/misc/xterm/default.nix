6f8379d1756bd490287acad61e88645560128ba2
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-01-28 19:40:08 +0000

    xterm: using versioned tarball, upgrade go 231
    svn path=/nixpkgs/trunk/; revision=10333

--- pkgs/applications/misc/xterm/default.nix
+++ pkgs/applications/misc/xterm/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, libXaw, xproto, libXt, libX11, libSM, libICE, ncurses}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "xterm-231";
   src = fetchurl {
-    url = ftp://invisible-island.net/xterm/xterm-231.tgz;
-    md5 = "b767d702e1464e16802b90c2187252c6"; /* was f7b04a66dc401dc22f5ddb7f345be229 */ /* was a062d0b398918015d07c31ecdcc5111a  */
+    url = "ftp://invisible-island.net/xterm/${name}.tgz";
+    sha256 = "0qlz5nkdqkahdg9kbd1ni96n69srj1pd9yggwrw3z0kghaajb2sr";
   };
   buildInputs = [libXaw xproto libXt libX11 libSM libICE ncurses];
   configureFlags = ["--enable-wide-chars"];
