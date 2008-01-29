9eb8065a74d5fb22f5ba77126c638c24c79032eb
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-01-29 13:16:06 +0000

    * added rdesktop
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10378

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4477,6 +4477,11 @@ rec {
     inherit fetchurl stdenv;
   };
 
+  rdesktop = import ../applications/networking/remote/rdesktop {
+    inherit fetchurl stdenv openssl;
+    inherit (xlibs) libX11;
+  };
+
   RealPlayer = import ../applications/video/RealPlayer {
     inherit fetchurl stdenv;
     inherit (gtkLibs) glib pango atk gtk;
