b1b8c4f258dbe305f0d2b5d7a94151d6bb7087b6
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-01-31 10:40:21 +0000

    * Added ms-sys, a program for writing Microsoft compatible boot records.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10428

---
+++ pkgs/tools/misc/mssys/default.nix
@@ -0,0 +1,22 @@
+{stdenv, fetchurl, gettext}:
+
+stdenv.mkDerivation {
+  name = "ms-sys-2.1.3";
+  
+  src = fetchurl {
+    url = mirror://sourceforge/ms-sys/ms-sys-2.1.3.tgz;
+    md5 = "6fad0a69ac89440ad4f696dbbbf11497";
+  };
+
+  buildInputs = [gettext];
+
+  preBuild = ''
+    makeFlags=(PREFIX=$out)
+  '';
+
+  meta = {
+    homepage = http://ms-sys.sourceforge.net/;
+    license = "GPL";
+    description = "A program for writing Microsoft compatible boot records";
+  };
+}
