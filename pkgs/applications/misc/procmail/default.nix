ecea9e1e0d0a39557e20dae63fe1edf5f7c94d98
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-01-31 17:32:06 +0000

    procmail: syntax changed, mailstat should work
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10437

--- pkgs/applications/misc/procmail/default.nix
+++ pkgs/applications/misc/procmail/default.nix
@@ -1,18 +1,13 @@
 args: with args;
 stdenv.mkDerivation {
   name="procmail-3.22";
-  buildInputs = [stdenv.gcc.libc];
-  installPhase = "
-    ensureDir \$out/bin
-    find . -exec sed -e \"s%^RM[ \\t]*=%RM=`type -f rm | awk '{print $3;}'`%\" -i '{}' ';'
-    sed -e 's%\\(LDFLAGS = \$(LDFLAGS1) -lnsl -ldl -lc\\)%\\1 -m%' -i src/Makefile
-    sed -e \"s%^BASENAME.*%\BASENAME=$out%\" -i Makefile
-    make DESTDIR=\$out install
-   ";
-  phases ="installPhase";
+  patchPhase = "find . -type f | xargs sed -i 's@/bin/rm@rm@g' ";
+  configurePhase= "make init";
+  makeFlags = ''BASENAME=$(out) LIBS=-lm'';
+
+  postInstall = '' sed -i /^PATH/d $out/bin/mailstat '';
   src = fetchurl {
     url = ftp://ftp.fu-berlin.de/pub/unix/mail/procmail/procmail-3.22.tar.gz;
     sha256 = "05z1c803n5cppkcq99vkyd5myff904lf9sdgynfqngfk9nrpaz08";
   };
-  o=62;
 }
