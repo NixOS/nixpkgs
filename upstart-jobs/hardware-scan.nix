9802ed58eed0175fea39bc93ed0af521c43b965e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-02-01 21:50:29 +0000

    * hardware-scan: load modules for all supported USB devices   automatically.  Of course this should be triggered when   a USB device is plugged in (from udev).
    svn path=/nixos/trunk/; revision=10451

--- upstart-jobs/hardware-scan.nix
+++ upstart-jobs/hardware-scan.nix
@@ -3,27 +3,26 @@
 {
   name = "hardware-scan";
   
-  job = "
-start on udev
+  job = ''
+    start on udev
 
-script
-    for i in ${toString kernelModules}; do
-        echo \"Loading kernel module $i...\"
-        ${modprobe}/sbin/modprobe $i || true
-    done
-
-    if test -n \"${toString doHardwareScan}\" -a ! -e /var/run/safemode; then
-    
-        # Try to load modules for all PCI devices.
-        for i in /sys/bus/pci/devices/*/modalias; do
-            echo \"Trying to load a module for $(basename $(dirname $i))...\"
-            ${modprobe}/sbin/modprobe $(cat $i) || true
-            echo \"\"
+    script
+        for i in ${toString kernelModules}; do
+            echo "Loading kernel module $i..."
+            ${modprobe}/sbin/modprobe $i || true
         done
 
-    fi
-end script
+        if test -n "${toString doHardwareScan}" -a ! -e /var/run/safemode; then
 
-  ";
+            # Try to load modules for all PCI and USB devices.
+            for i in /sys/bus/pci/devices/*/modalias /sys/bus/usb/devices/*/modalias; do
+                echo "Trying to load a module for $(basename $(dirname $i))..."
+                ${modprobe}/sbin/modprobe $(cat $i) || true
+                echo ""
+            done
+
+        fi
+    end script
+  '';
 
 }
