d52ed394ae143e260cdb8fa9b3132bf67feeb951
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-02-02 20:39:21 +0000

    gettext: removed old versions, added 0.17
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10454

--- pkgs/development/libraries/gettext/0.14.6.nix
+++
@@ -1,9 +0,0 @@
-args: with args;
-stdenv.mkDerivation {
-  name = "gettext-0.14.6";
-  src = fetchurl {
-    url = mirror://gnu/gettext/gettext-0.14.6.tar.gz;
-    md5 = "c26fc7f0a493c5c7c39bbc4e7ed42790";
-  };
-  configureFlags = "--disable-csharp";
-}
