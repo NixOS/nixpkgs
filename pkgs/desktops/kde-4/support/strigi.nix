b4c440e72e8f81c46c5e8511506ee2de8cb4d321
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-02-03 16:42:28 +0000

    Typo in strigi
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10485

--- pkgs/desktops/kde-4/support/strigi.nix
+++ pkgs/desktops/kde-4/support/strigi.nix
@@ -4,6 +4,6 @@ stdenv.mkDerivation {
   name = "strigi-svn";
   src = svnSrc "strigi" "0zmfy7ga4y49hl3rgw8ypqag36k78k2wfkkxka8xskrd249516ha";
   CLUCENE_HOME=cluceneCore;
-  buildInputs = [ cmake zlib cluceneCore bzip2 libxml2 qt dbus
+  buildInputs = [ cmake zlib cluceneCore bzip2 libxml2 qt dbus.libs
   log4cxx stdenv.gcc.libc exiv2 bison cppunit perl ];
 }
