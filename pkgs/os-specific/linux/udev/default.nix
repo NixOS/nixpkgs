6b7dd4cac71dc8087393f00f5f36451d26a7f75d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-02-04 13:04:16 +0000

    * udev: get rid of silly warnings in make install.
    svn path=/nixpkgs/trunk/; revision=10494

--- pkgs/os-specific/linux/udev/default.nix
+++ pkgs/os-specific/linux/udev/default.nix
@@ -2,10 +2,24 @@
 
 stdenv.mkDerivation {
   name = "udev-118";
+
   src = fetchurl {
     url = mirror://kernel/linux/utils/kernel/hotplug/udev-118.tar.bz2;
     sha256 = "1i488wqm7i6nz6gidbkxkb47hr427ika48i8imwrvvnpg1kzhska";
   };
-  preBuild = "makeFlagsArray=(etcdir=$out/etc sbindir=$out/sbin usrbindir=$out/bin usrsbindir=$out/sbin mandir=$out/share/man INSTALL='install -c')";
-  preInstall = "installFlagsArray=(udevdir=dummy)";
+
+  # "DESTDIR=/" is a hack to prevent "make install" from trying to
+  # mess with /dev.
+  preBuild = ''
+    makeFlagsArray=(etcdir=$out/etc sbindir=$out/sbin usrbindir=$out/bin usrsbindir=$out/sbin mandir=$out/share/man INSTALL='install -c' DESTDIR=/)
+  '';
+
+  preInstall = ''
+    installFlagsArray=(udevdir=$TMPDIR/dummy)
+  '';
+
+  meta = {
+    homepage = http://www.kernel.org/pub/linux/utils/kernel/hotplug/udev.html;
+    description = "Udev manages the /dev filesystem";
+  };
 }
