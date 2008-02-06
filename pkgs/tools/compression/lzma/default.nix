bf95691d1c88c907d0a61e2a9005098cb23f2109
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-02-06 14:19:58 +0000

    svn path=/nixpkgs/trunk/; revision=10514

--- pkgs/tools/compression/lzma/default.nix
+++ pkgs/tools/compression/lzma/default.nix
@@ -8,6 +8,9 @@ stdenv.mkDerivation {
     sha256 = "1mqy1biy46gqky6n3gyr2l395hwckh0xyi96waz5p5x8mgp372ch";
   };
 
+  CFLAGS = "-O3";
+  CXXFLAGS = "-O3";
+
   meta = {
     homepage = http://tukaani.org/lzma/;
     description = "The LZMA compression program";
