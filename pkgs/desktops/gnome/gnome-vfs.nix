503d0bfc6532fb2b487fe0a1c178416389d17ee7
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-02-06 19:00:30 +0000

    gnome-vfs: propagating GConf
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10518

--- pkgs/desktops/gnome/gnome-vfs.nix
+++ pkgs/desktops/gnome/gnome-vfs.nix
@@ -3,9 +3,11 @@ args: with args;
 stdenv.mkDerivation {
   inherit (input) name src;
   buildInputs = [
-    pkgconfig perl gnome.glib libxml2 gnome.GConf gnome.libbonobo
+    pkgconfig perl gnome.glib libxml2 gnome.libbonobo
     gnome.gnomemimedata popt perlXMLParser gettext bzip2
     dbus_glib hal openssl samba fam
   ];
+
+  propagatedBuildInputs = [gnome.GConf];
   patches = [./no-kerberos.patch];
 }
