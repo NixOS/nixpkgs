734a1b39e0b85e376cd2671bf4274f8de4cd6c18
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-02-10 17:40:00 +0000

    added SDL_net, some minor modifications to SDL_image and SDL_mixer
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10595

--- pkgs/development/libraries/SDL_image/default.nix
+++ pkgs/development/libraries/SDL_image/default.nix
@@ -1,11 +1,14 @@
 args: with args;
-stdenv.mkDerivation {
-  name = "SDL_image-1.2.6";
+stdenv.mkDerivation (rec {
+  pname = "SDL_image";
+  version = "1.2.6";
+
+  name = "${pname}-${version}";
 
   src = fetchurl {
-		url = http://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.6.tar.gz;
-		sha256 = "1i3f72dw3i3l6d77dk81gw57sp0629rng9k76qb37brlz7dv3z48";
-	};
+    url = "http://www.libsdl.org/projects/${pname}/release/${name}.tar.gz";
+    sha256 = "1i3f72dw3i3l6d77dk81gw57sp0629rng9k76qb37brlz7dv3z48";
+  };
 
   buildInputs = [SDL libpng libjpeg libtiff libungif libXpm];
 
@@ -13,7 +16,7 @@ stdenv.mkDerivation {
 
   meta = {
     description = "
-	SDL image library.
+      SDL image library.
 ";
   };
-}
+})
