0a270d49e751926118acf9477493d497a0dc50a7
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-02-10 17:41:29 +0000

    libsigsegv: shared
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10607

--- pkgs/development/libraries/libsigsegv/2.5.nix
+++ pkgs/development/libraries/libsigsegv/2.5.nix
@@ -7,6 +7,8 @@ stdenv.mkDerivation rec {
     sha256 = "0fvcsq9msi63vrbpvks6mqkrnls5cfy6bzww063sqhk2h49vsyyg";
   };
 
+  configureFlags = "--enable-shared --disable-static";
+
   meta = {
     homepage = http://libsigsegv.sf.net;
     description = "A library for handling page faults in user mode";
