71dd78fe64eb011a217c6f45db53624999450a2c
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-02-10 17:40:39 +0000

    * man-db 2.5.1.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10600

--- pkgs/tools/misc/man/default.nix
+++ pkgs/tools/misc/man/default.nix
@@ -1,15 +1,32 @@
 {stdenv, fetchurl, db4, groff}:
  
 stdenv.mkDerivation {
-  name = "man-2.4.3";
+  name = "man-db-2.5.1";
+  
   src = fetchurl {
-    url = http://savannah.nongnu.org/download/man-db/man-db-2.4.3.tar.gz;
-    md5 = "30814a47f209f43b152659ba51fc7937";
+    url = http://download.savannah.nongnu.org/releases/man-db/man-db-2.5.1.tar.gz;
+    sha256 = "178w1fk23ffh8vabj29cn0yyg5ps7bwy1zrrrcsw8aypbh3sfjy3";
   };
+  
   buildInputs = [db4 groff];
-  configureFlags = "--disable-setuid";
+  
+  configureFlags = ''
+    --disable-setuid
+    --with-nroff=${groff}/bin/nroff
+    --with-tbl=${groff}/bin/tbl
+    --with-eqn=${groff}/bin/eqn
+    --with-neqn=${groff}/bin/neqn
+  '';
+
+  troff = "${groff}/bin/troff";
+  
   patches = [
     # Search in "share/man" relative to each path in $PATH (in addition to "man").
     ./share.patch
   ];
+
+  meta = {
+    homepage = http://www.nongnu.org/man-db/;
+    description = "An implementation of the standard Unix documentation system accessed using the man command";
+  };
 }
