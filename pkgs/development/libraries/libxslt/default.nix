6e337bd44dea8ef8267b2787f932983de78e2820
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-02-10 17:41:40 +0000

    libxslt: propagating libxml2
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10609

--- pkgs/development/libraries/libxslt/default.nix
+++ pkgs/development/libraries/libxslt/default.nix
@@ -1,6 +1,4 @@
-{stdenv, fetchurl, libxml2}:
-
-assert libxml2 != null;
+args: with args;
 
 stdenv.mkDerivation {
   name = "libxslt-1.1.22";
@@ -8,6 +6,6 @@ stdenv.mkDerivation {
     url = ftp://xmlsoft.org/libxml2/libxslt-1.1.22.tar.gz;
     sha256 = "1nj9pvn4ibhwxpl3ry9n6d7jahppcnqc7mi87nld4vsr2vp3j7sf";
   };
-  buildInputs = [libxml2];
-  postInstall = "ensureDir $out/nix-support; ln -s ${libxml2}/nix-support/setup-hook $out/nix-support/";
+  propagatedBuildInputs = [libxml2];
+  configureFlags = "--enable-shared --disable-static";
 }
