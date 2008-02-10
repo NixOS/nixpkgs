cc0430ca4b93204a0f7f9b997bd6cd6b3904b5b3
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-02-10 17:41:35 +0000

    gettext: shared
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10608

--- pkgs/development/libraries/gettext/0.17.nix
+++ pkgs/development/libraries/gettext/0.17.nix
@@ -5,5 +5,5 @@ stdenv.mkDerivation rec {
     url = "mirror://gnu/gettext/${name}.tar.gz";
     sha256 = "1fipjpaxxwifdw6cbr7mkxp1yvy643i38nhlh7124bqnisxki5i0";
   };
-  configureFlags = "--disable-csharp";
+  configureFlags = "--disable-csharp --enable-shared --disable-static";
 }
