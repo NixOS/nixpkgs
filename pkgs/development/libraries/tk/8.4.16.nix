63e46d78d03edbafc8dcea1bcf41c71f1b6347ea
Yury G. Kudryashov <urkud.urkud@gmail.com>
2008-02-10 17:36:23 +0000

    Fixed stable socat (applied patch by Nicolas Pierron); updated Tk installation step for XMaxima compatibility
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10576

---
+++ pkgs/development/libraries/tk/8.4.16.nix
@@ -0,0 +1,18 @@
+args: with args;
+
+stdenv.mkDerivation {
+  name = "tk-${version}";
+  src = fetchurl {
+    url = "mirror://sourceforge/tcl/tk${version}-src.tar.gz";
+    sha256 = "0cciavzd05bpm5yfppid0s0vsf8kabwia9620vgvi26sv1gjgwhb";
+  };
+  postInstall = ''
+    echo -e '#! /bin/sh \n $( readlink -f $( which wish${__substring 0 3 version}) ) "$@"' >$out/bin/wish
+    chmod a+x $out/bin/wish
+  ''; 
+  configureFlags="--with-tcl=${tcl}/lib";
+  preConfigure = "cd unix";
+
+  buildInputs = [tcl x11];
+  inherit tcl;
+}
