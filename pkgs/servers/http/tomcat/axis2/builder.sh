74adfd62395b4dde3ad04ab17ec6c30d205c5dfb
Sander van der Burg <s.vanderburg@tudelft.nl>
2008-02-13 09:27:19 +0000

    Also install the axis2 libraries
    svn path=/nixpkgs/trunk/; revision=10654

--- pkgs/servers/http/tomcat/axis2/builder.sh
+++ pkgs/servers/http/tomcat/axis2/builder.sh
@@ -2,7 +2,10 @@ buildInputs="$unzip $apacheAnt $jdk"
 source $stdenv/setup
 
 unzip $src
-cd axis2-*/webapp
+cd axis2-*
+ensureDir $out/share/java/axis2
+cp lib/* $out/share/java/axis2
+cd webapp
 ant
 ensureDir $out/webapps
 cp ../dist/axis2.war $out/webapps
