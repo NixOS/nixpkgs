4b009da5fd775ed54fd15b1e01250dd8892cc8f6
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-02-15 10:56:31 +0000

    * Jetty 5.1.12 (couldn't get 5.1.14 to work quickly).
    svn path=/nixpkgs/trunk/; revision=10702

--- pkgs/servers/http/jetty/default.nix
+++ pkgs/servers/http/jetty/default.nix
@@ -1,13 +1,19 @@
 {stdenv, fetchurl, unzip}:
 
 stdenv.mkDerivation {
-  name = "jetty-5.1.4";
-
-  builder = ./bin-builder.sh;
-  buildInputs = [unzip];
+  name = "jetty-5.1.12";
 
   src = fetchurl {
-    url = mirror://sourceforge/jetty/jetty-5.1.4.zip;
-    md5 = "5d16bb1ea4a62dff93c0b7f7de00430f";
+    url = ftp://ftp.mortbay.org/pub/jetty-5/jetty-5.1.12.zip;
+    sha256 = "04nysajgrlyvfh810jpyr8iay38kwjrbmh6bgs10mwd30qhj4rd1";
   };
+
+  buildInputs = [unzip];
+
+  buildPhase = "true";
+
+  installPhase = ''
+    ensureDir $out
+    cp -pr * $out/
+  '';
 }
