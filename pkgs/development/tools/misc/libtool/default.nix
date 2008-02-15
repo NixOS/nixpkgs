a2339db1b1437d73459343314b333d3c07646f73
Ludovic Courtès <ludo@gnu.org>
2008-02-15 21:47:55 +0000

    Libtool: Use `mirror://'.
    svn path=/nixpkgs/trunk/; revision=10712

--- pkgs/development/tools/misc/libtool/default.nix
+++ pkgs/development/tools/misc/libtool/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation rec {
   name = "libtool-1.5.26";
   src = fetchurl {
-    url = "http://ftp.gnu.org/gnu/libtool/" + name + ".tar.gz";
+    url = "mirror://gnu/libtool/" + name + ".tar.gz";
     sha256 = "029ggq5kri1gjn6nfqmgw4w920gyfzscjjxbsxxidal5zqsawd8w";
   };
   buildInputs = [m4 perl];
