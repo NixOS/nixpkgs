b67cb39b7dadb9d38507b86a515a7b13667779c2
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-02-20 17:22:45 +0000

    svn path=/nixpkgs/branches/stdenv-updates/; revision=10789

--- pkgs/development/libraries/aterm/2.4.2-fixes.nix
+++ pkgs/development/libraries/aterm/2.4.2-fixes.nix
@@ -6,6 +6,7 @@ stdenv.mkDerivation {
     url = http://losser.st-lab.cs.uu.nl/~eelco/dist/aterm-2.4.2-fixes-r2.tar.bz2;
     sha256 = "1w3bxdpc2hz29li5ssmdcz3x0fn47r7g62ns0v8nazxwf40vff4j";
   };
+  doCheck = true;
   meta = {
     homepage = http://www.cwi.nl/htbin/sen1/twiki/bin/view/SEN1/ATerm;
     license = "LGPL";
