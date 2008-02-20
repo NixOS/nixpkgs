96aa40524e42cf5f52ccb86b4356baee18107e37
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-02-20 17:22:32 +0000

    * Hack hack hack.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10788

--- pkgs/stdenv/generic/builder.sh
+++ pkgs/stdenv/generic/builder.sh
@@ -3,6 +3,10 @@ p2=$param2
 p3=$param3
 p4=$param4
 p5=$param5
+_preHook="$preHook"
+_postHook="$postHook"
+preHook=
+postHook=
 
 source $stdenv/setup
 
@@ -11,8 +15,8 @@ mkdir $out
 # Can't use substitute() here, because replace may not have been
 # built yet (in the bootstrap).
 sed \
-    -e "s^@preHook@^$preHook^g" \
-    -e "s^@postHook@^$postHook^g" \
+    -e "s^@preHook@^$_preHook^g" \
+    -e "s^@postHook@^$_postHook^g" \
     -e "s^@initialPath@^$initialPath^g" \
     -e "s^@gcc@^$gcc^g" \
     -e "s^@shell@^$shell^g" \
