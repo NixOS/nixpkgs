3bcc70349b822d29d06600f66ce5d2deeb8fa0fb
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-02-21 17:26:46 +0000

    svn path=/nixpkgs/branches/stdenv-updates-merge/; revision=10814

--- pkgs/stdenv/nix/prehook.sh
+++ pkgs/stdenv/nix/prehook.sh
@@ -6,4 +6,5 @@ if test "$system" = "i686-darwin" -o "$system" = "powerpc-darwin"; then
     export NIX_NO_SELF_RPATH=1
     dontFixLibtool=1
     NIX_STRIP_DEBUG=0 # !!! do we still need this?
+    stripAllFlags=" " # the Darwin "strip" command doesn't know "-s" 
 fi
