89a360738fa0a654f2b3eaee483a2bda92fc6b54
Armijn Hemel <armijn@gpl-violations.org>
2008-02-26 21:02:36 +0000

    add stuff for 2.20
    svn path=/nixpkgs/trunk/; revision=10868

---
+++ pkgs/desktops/gnome/src-gnome-desktop-2.20.3.nix
@@ -0,0 +1,543 @@
+# Note: this file was generated automatically by make-listing.pl!
+
+{fetchurl} : {
+  alacarte = {
+    name = "alacarte-0.11.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/alacarte-0.11.3.tar.bz2;
+      md5 = "5ca9f07e839f64b7fcefc84cfb05aa17";
+    };
+  };
+  bugbuddy = {
+    name = "bug-buddy-2.20.1";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/bug-buddy-2.20.1.tar.bz2;
+      md5 = "6b9ea4c067674120bd93711b3b9a9e63";
+    };
+  };
+  dasher = {
+    name = "dasher-4.6.1";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/dasher-4.6.1.tar.bz2;
+      md5 = "1457df5075477d757f7df5693947464d";
+    };
+  };
+  deskbarapplet = {
+    name = "deskbar-applet-2.20.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/deskbar-applet-2.20.3.tar.bz2;
+      md5 = "f84abfede0bbe5b172b24f581550e4a0";
+    };
+  };
+  eel = {
+    name = "eel-2.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/eel-2.20.0.tar.bz2;
+      md5 = "691734dc1078e05e726b4f5b8646dadf";
+    };
+  };
+  ekiga = {
+    name = "ekiga-2.0.11";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/ekiga-2.0.11.tar.bz2;
+      md5 = "c4b18fdd5f392545f4c8498fb8874882";
+    };
+  };
+  eog = {
+    name = "eog-2.20.4";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/eog-2.20.4.tar.bz2;
+      md5 = "f9c923e1e09caa8e6f764ae8242f8d61";
+    };
+  };
+  epiphany = {
+    name = "epiphany-2.20.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/epiphany-2.20.3.tar.bz2;
+      md5 = "df2861b3e91bef6d365f50187b6170cb";
+    };
+  };
+  evince = {
+    name = "evince-2.20.2";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/evince-2.20.2.tar.bz2;
+      md5 = "15214f5211d9f98ffa0afd8158b7d021";
+    };
+  };
+  evolution = {
+    name = "evolution-2.12.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/evolution-2.12.3.tar.bz2;
+      md5 = "554698d7387aee82c5f4d4520760503d";
+    };
+  };
+  evolutiondataserver = {
+    name = "evolution-data-server-1.12.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/evolution-data-server-1.12.3.tar.bz2;
+      md5 = "9a04e379c34ad538764866dfd4eb2931";
+    };
+  };
+  evolutionexchange = {
+    name = "evolution-exchange-2.12.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/evolution-exchange-2.12.3.tar.bz2;
+      md5 = "1bdda76724a17fb7606abcdfdb6ec170";
+    };
+  };
+  evolutionwebcal = {
+    name = "evolution-webcal-2.12.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/evolution-webcal-2.12.0.tar.bz2;
+      md5 = "21e77fa081decb87ce641f3a77245864";
+    };
+  };
+  fastuserswitchapplet = {
+    name = "fast-user-switch-applet-2.18.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/fast-user-switch-applet-2.18.0.tar.bz2;
+      md5 = "aefe9ea3aecc908336ae909aeee385b5";
+    };
+  };
+  fileroller = {
+    name = "file-roller-2.20.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/file-roller-2.20.3.tar.bz2;
+      md5 = "7af6bac2e96ad9c728d791ce21dba8f5";
+    };
+  };
+  gcalctool = {
+    name = "gcalctool-5.20.2";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gcalctool-5.20.2.tar.bz2;
+      md5 = "bcde10b8029d3a513183e8aac9506c1f";
+    };
+  };
+  gconfeditor = {
+    name = "gconf-editor-2.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gconf-editor-2.20.0.tar.bz2;
+      md5 = "b686677878b5754ffa8d0a2cc1129988";
+    };
+  };
+  gdm = {
+    name = "gdm-2.20.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gdm-2.20.3.tar.bz2;
+      md5 = "0074b163986cfe48d3998518084c4d00";
+    };
+  };
+  gedit = {
+    name = "gedit-2.20.4";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gedit-2.20.4.tar.bz2;
+      md5 = "6c7d74f9b4339c9ec9e81f6ea3eb19c9";
+    };
+  };
+  gnomeapplets = {
+    name = "gnome-applets-2.20.1";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-applets-2.20.1.tar.bz2;
+      md5 = "e49402e7323bdd9595dc5cbfdba6ba5b";
+    };
+  };
+  gnomebackgrounds = {
+    name = "gnome-backgrounds-2.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-backgrounds-2.20.0.tar.bz2;
+      md5 = "391c0c64407c5bd2a52207b0298beac4";
+    };
+  };
+  gnomecontrolcenter = {
+    name = "gnome-control-center-2.20.1";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-control-center-2.20.1.tar.bz2;
+      md5 = "d7420f0fc28937573a13bc5c1633b23b";
+    };
+  };
+  gnomedesktop = {
+    name = "gnome-desktop-2.20.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-desktop-2.20.3.tar.bz2;
+      md5 = "d59ddca5e9bc4e4d1717538ceae57501";
+    };
+  };
+  gnomedocutils = {
+    name = "gnome-doc-utils-0.12.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-doc-utils-0.12.0.tar.bz2;
+      md5 = "5934c08d12407d8233416343cd73df24";
+    };
+  };
+  gnomegames = {
+    name = "gnome-games-2.20.2";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-games-2.20.2.tar.bz2;
+      md5 = "3cfd717e7cf1478ce9cba7b712ede219";
+    };
+  };
+  gnomeicontheme = {
+    name = "gnome-icon-theme-2.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-icon-theme-2.20.0.tar.bz2;
+      md5 = "56857a6d5f26c236b48fcf6760549d1b";
+    };
+  };
+  gnomekeyring = {
+    name = "gnome-keyring-2.20.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-keyring-2.20.3.tar.bz2;
+      md5 = "2205177a168e745fa803c96a4fb62102";
+    };
+  };
+  gnomekeyringmanager = {
+    name = "gnome-keyring-manager-2.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-keyring-manager-2.20.0.tar.bz2;
+      md5 = "61d701888f00c02490c0cd551bf3fcb1";
+    };
+  };
+  gnomemag = {
+    name = "gnome-mag-0.14.10";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-mag-0.14.10.tar.bz2;
+      md5 = "e3e498c7df873ecc7acabd52a7b4ddc7";
+    };
+  };
+  gnomemedia = {
+    name = "gnome-media-2.20.1";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-media-2.20.1.tar.bz2;
+      md5 = "75036a6990725c4323812afd4cd734f5";
+    };
+  };
+  gnomemenus = {
+    name = "gnome-menus-2.20.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-menus-2.20.3.tar.bz2;
+      md5 = "b8ab05f5edfdc3e6a46662a504934657";
+    };
+  };
+  gnomenetstatus = {
+    name = "gnome-netstatus-2.12.1";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-netstatus-2.12.1.tar.bz2;
+      md5 = "8651ca1694a6c222ae5cad6e21814d24";
+    };
+  };
+  gnomenettool = {
+    name = "gnome-nettool-2.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-nettool-2.20.0.tar.bz2;
+      md5 = "8f16e4207655ccf1e7af679fc0f58bbd";
+    };
+  };
+  gnomepanel = {
+    name = "gnome-panel-2.20.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-panel-2.20.3.tar.bz2;
+      md5 = "0ddf04cea0859570216319d6f1f8c4d8";
+    };
+  };
+  gnomepowermanager = {
+    name = "gnome-power-manager-2.20.2";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-power-manager-2.20.2.tar.bz2;
+      md5 = "8a38e3a169b80c44639f4383a64835d0";
+    };
+  };
+  gnomepythondesktop = {
+    name = "gnome-python-desktop-2.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-python-desktop-2.20.0.tar.bz2;
+      md5 = "02b177f488091d27bf0648ec4660d8f7";
+    };
+  };
+  gnomescreensaver = {
+    name = "gnome-screensaver-2.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-screensaver-2.20.0.tar.bz2;
+      md5 = "d166a3ebf69289331b2ebcf5c0187fd9";
+    };
+  };
+  gnomesession = {
+    name = "gnome-session-2.20.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-session-2.20.3.tar.bz2;
+      md5 = "f9d80e12c8fd79efa9786f617ba18bc7";
+    };
+  };
+  gnomesharp = {
+    name = "gnome-sharp-2.16.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-sharp-2.16.0.tar.bz2;
+      md5 = "09034669acec52f9d9818d9a31c032d5";
+    };
+  };
+  gnomespeech = {
+    name = "gnome-speech-0.4.17";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-speech-0.4.17.tar.bz2;
+      md5 = "a202dbc9df89e1c91e66cbf0fd21f26b";
+    };
+  };
+  gnomesystemmonitor = {
+    name = "gnome-system-monitor-2.20.2";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-system-monitor-2.20.2.tar.bz2;
+      md5 = "d5e52f923b0b725b3a49e4bb7a296436";
+    };
+  };
+  gnomesystemtools = {
+    name = "gnome-system-tools-2.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-system-tools-2.20.0.tar.bz2;
+      md5 = "382ed1f5ad4cb6ce7b88985611e2be18";
+    };
+  };
+  gnometerminal = {
+    name = "gnome-terminal-2.18.4";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-terminal-2.18.4.tar.bz2;
+      md5 = "e4429d66d0d2ede8e14756cfbc15d30a";
+    };
+  };
+  gnomethemes = {
+    name = "gnome-themes-2.20.2";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-themes-2.20.2.tar.bz2;
+      md5 = "52d018251ef8164deaf9343cbfb7af16";
+    };
+  };
+  gnomeuserdocs = {
+    name = "gnome-user-docs-2.20.1";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-user-docs-2.20.1.tar.bz2;
+      md5 = "d6bba44acaed20117769ec8dce378f71";
+    };
+  };
+  gnomeutils = {
+    name = "gnome-utils-2.20.0.1";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-utils-2.20.0.1.tar.bz2;
+      md5 = "2d4385df2fb566b437b283bd2ce70190";
+    };
+  };
+  gnomevolumemanager = {
+    name = "gnome-volume-manager-2.17.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gnome-volume-manager-2.17.0.tar.bz2;
+      md5 = "104cec26e721e0bba69debd392367195";
+    };
+  };
+  gok = {
+    name = "gok-1.3.7";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gok-1.3.7.tar.bz2;
+      md5 = "14b2407604ceec0ffcbf4866fef35502";
+    };
+  };
+  gstpluginsbase = {
+    name = "gst-plugins-base-0.10.15";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gst-plugins-base-0.10.15.tar.bz2;
+      md5 = "8449359b465074433fec626778bcf36f";
+    };
+  };
+  gstpluginsgood = {
+    name = "gst-plugins-good-0.10.6";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gst-plugins-good-0.10.6.tar.bz2;
+      md5 = "25f111360c2930705b91b4fcf93ae5c5";
+    };
+  };
+  gstreamer = {
+    name = "gstreamer-0.10.15";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gstreamer-0.10.15.tar.bz2;
+      md5 = "09f1c4ea025faef88b566fa26ec24ae0";
+    };
+  };
+  gtkengines = {
+    name = "gtk-engines-2.12.2";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gtk-engines-2.12.2.tar.bz2;
+      md5 = "a73506d8262eb52e0e4bd8e04517ea4e";
+    };
+  };
+  gtkhtml = {
+    name = "gtkhtml-3.16.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gtkhtml-3.16.3.tar.bz2;
+      md5 = "bba94aecc723946e2a4e68e2dcf59722";
+    };
+  };
+  gtksourceview = {
+    name = "gtksourceview-2.0.2";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gtksourceview-2.0.2.tar.bz2;
+      md5 = "8a9daf3fa9ddeed92c18572ee16cd1f5";
+    };
+  };
+  gucharmap = {
+    name = "gucharmap-1.10.2";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/gucharmap-1.10.2.tar.bz2;
+      md5 = "0a81695c916b2dbae2392f4bcb8c4e22";
+    };
+  };
+  libgailgnome = {
+    name = "libgail-gnome-1.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/libgail-gnome-1.20.0.tar.bz2;
+      md5 = "5a7662e3d637c86baa95a50adb225e56";
+    };
+  };
+  libgnomekbd = {
+    name = "libgnomekbd-2.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/libgnomekbd-2.20.0.tar.bz2;
+      md5 = "0b495d355e25c3531419cb3996be7e28";
+    };
+  };
+  libgnomeprint = {
+    name = "libgnomeprint-2.18.2";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/libgnomeprint-2.18.2.tar.bz2;
+      md5 = "b1f6c95a8dbb8ce17ab00fc2eea2aa91";
+    };
+  };
+  libgnomeprintui = {
+    name = "libgnomeprintui-2.18.1";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/libgnomeprintui-2.18.1.tar.bz2;
+      md5 = "4f9a09372a5fa2cd5c35be30dff3fcd6";
+    };
+  };
+  libgtop = {
+    name = "libgtop-2.20.1";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/libgtop-2.20.1.tar.bz2;
+      md5 = "b6fa671e8325bd8dbce684527791225a";
+    };
+  };
+  liboobs = {
+    name = "liboobs-2.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/liboobs-2.20.0.tar.bz2;
+      md5 = "c0562e115af4777da471182b9d462b40";
+    };
+  };
+  librsvg = {
+    name = "librsvg-2.18.2";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/librsvg-2.18.2.tar.bz2;
+      md5 = "4691ad687281e943260021272c28043e";
+    };
+  };
+  libsoup = {
+    name = "libsoup-2.2.104";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/libsoup-2.2.104.tar.bz2;
+      md5 = "ab3b10b1c97de5abe38a748a3656da4c";
+    };
+  };
+  libwnck = {
+    name = "libwnck-2.20.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/libwnck-2.20.3.tar.bz2;
+      md5 = "0f0f54764993497ed02f34a44056efb2";
+    };
+  };
+  metacity = {
+    name = "metacity-2.20.2";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/metacity-2.20.2.tar.bz2;
+      md5 = "3b986030629860674b219fc35c226b1b";
+    };
+  };
+  nautilus = {
+    name = "nautilus-2.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/nautilus-2.20.0.tar.bz2;
+      md5 = "3856ec2ffeba786d12f8f6622e398c33";
+    };
+  };
+  nautiluscdburner = {
+    name = "nautilus-cd-burner-2.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/nautilus-cd-burner-2.20.0.tar.bz2;
+      md5 = "a7ddb02c33be107f6c9c1874b7703e9e";
+    };
+  };
+  orca = {
+    name = "orca-2.20.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/orca-2.20.3.tar.bz2;
+      md5 = "a270df4640493e0a96dc6d363ae800a6";
+    };
+  };
+  pygtksourceview = {
+    name = "pygtksourceview-2.0.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/pygtksourceview-2.0.0.tar.bz2;
+      md5 = "a4947108e7a669b67ff0ff157a132728";
+    };
+  };
+  seahorse = {
+    name = "seahorse-2.20.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/seahorse-2.20.3.tar.bz2;
+      md5 = "6f03b38cb0a52e682c73be49f244f83b";
+    };
+  };
+  soundjuicer = {
+    name = "sound-juicer-2.20.1";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/sound-juicer-2.20.1.tar.bz2;
+      md5 = "cea3c67215c99acf513ee7ea4b5eba7b";
+    };
+  };
+  tomboy = {
+    name = "tomboy-0.8.2";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/tomboy-0.8.2.tar.bz2;
+      md5 = "cca5b43cd1eb6bc6d277b307944ae884";
+    };
+  };
+  totem = {
+    name = "totem-2.20.3";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/totem-2.20.3.tar.bz2;
+      md5 = "7360e0856042615c7c1c3baf03d50bbf";
+    };
+  };
+  vino = {
+    name = "vino-2.20.1";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/vino-2.20.1.tar.bz2;
+      md5 = "c45379bd177e0bc9a937a3388fc81650";
+    };
+  };
+  vte = {
+    name = "vte-0.16.12";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/vte-0.16.12.tar.bz2;
+      md5 = "9816c35be2ff1b0cf57bd831e1344d1d";
+    };
+  };
+  yelp = {
+    name = "yelp-2.20.0";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/yelp-2.20.0.tar.bz2;
+      md5 = "8902618c343093b1c64ee6d647ec41ed";
+    };
+  };
+  zenity = {
+    name = "zenity-2.20.1";
+    src = fetchurl {
+      url = http://ftp.gnome.org/pub/GNOME/desktop/2.20/2.20.3/sources/zenity-2.20.1.tar.bz2;
+      md5 = "b9989582ea43f8fd58819d85ef9c9bc5";
+    };
+  };
+}
