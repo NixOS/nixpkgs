f03708fd979862b9277ebf298023ffc2c12c0cd3
Armijn Hemel <armijn@gpl-violations.org>
2008-02-29 08:49:08 +0000

    0.8 -> 0.9
    svn path=/nixpkgs/trunk/; revision=10893

--- pkgs/tools/text/ed/default.nix
+++ pkgs/tools/text/ed/default.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchurl}: stdenv.mkDerivation {
-  name = "ed-0.8";
+  name = "ed-0.9";
   src = fetchurl {
-    url = mirror://gnu/ed/ed-0.8.tar.bz2;
-    sha256 = "07zaw9w6vrcvpc2srd0zkm7948j262zx2xwj5qbb3lhvry3kr61p";
+    url = mirror://gnu/ed/ed-0.9.tar.bz2;
+    sha256 = "1xy746g7ai9gmv6iq2x1ll8x6wy4fi9anfh7gj5ifsdnaiahgyi2";
   };
 }
