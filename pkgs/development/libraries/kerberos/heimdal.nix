73051b79b720ff94e3071b65c2e862db0e7e60e1
Armijn Hemel <armijn@gpl-violations.org>
2008-03-02 22:12:27 +0000

    force a build without X for now. This should be made an option...soon.
    svn path=/nixpkgs/trunk/; revision=10921

--- pkgs/development/libraries/kerberos/heimdal.nix
+++ pkgs/development/libraries/kerberos/heimdal.nix
@@ -7,6 +7,7 @@ stdenv.mkDerivation rec {
 		sha256 = "1h4x41lpv2abpv5l3yjd58pfzs0kkp5sbnih9iykhwd6sii1iig5";
 	};
 
-	configureFlags = "--enable-shared --disable-static --with-openldap=${openldap}";
+	## ugly, X should be made an option
+	configureFlags = "--enable-shared --disable-static --with-openldap=${openldap} --without-x";
 	propagatedBuildInputs = [ readline db4 openssl openldap cyrus_sasl ];
 }
