9ee09f4a554a15608bd3b7f6f5c2c1679b82b14c
Michael Raskin <7c6f434c@mail.ru>
2008-03-02 21:59:32 +0000

    Added JUnicode
    svn path=/nixpkgs/trunk/; revision=10917

---
+++ pkgs/data/fonts/junicode/0.6.15.nix
@@ -0,0 +1,32 @@
+args : with args; with builderDefs {src="";} null;
+	let localDefs = builderDefs (rec {
+		src = /* put a fetchurl here */
+		fetchurl {
+		  url = http://prdownloads.sourceforge.net/junicode/junicode-0.6.15.zip;
+		  sha256 = "0p16r5s6qwyz0hayb6k61s5r2sfachlx7r6gpqqx5myx6ipbfdns";
+		};
+
+		buildInputs = [unzip];
+		configureFlags = [];
+		doInstall = FullDepEntry (''
+			unzip ${src}
+			ensureDir $out/share/fonts/junicode-ttf
+			cp *.ttf $out/share/fonts/junicode-ttf
+		'') ["minInit" "addInputs" "defEnsureDir"];
+	}) null; /* null is a terminator for sumArgs */
+	in with localDefs;
+stdenv.mkDerivation rec {
+	name = "junicode-"+version;
+	builder = writeScript (name + "-builder")
+		(textClosure localDefs 
+			[doInstall doForceShare doPropagate]);
+	meta = {
+		description = "
+		A Unicode font.
+";
+		inherit src;
+	};
+}
+
+
+
