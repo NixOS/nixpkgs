27540fd36e4e2a9b6ae286e8ed2f4b6008243fec
Armijn Hemel <armijn@gpl-violations.org>
2008-03-03 22:07:25 +0000

    update to 0.8.14 (warning: triggers OOo rebuild :-P )
    svn path=/nixpkgs/trunk/; revision=10933

--- pkgs/development/libraries/libwpd/default.nix
+++ pkgs/development/libraries/libwpd/default.nix
@@ -1,9 +1,9 @@
 args: with args;
 stdenv.mkDerivation {
-  name = "libwpd-0.8.13";
+  name = "libwpd-0.8.14";
   src = fetchurl {
-    url = mirror://sourceforge/libwpd/libwpd-0.8.13.tar.gz;
-    sha256 = "08mb8bp0d3387l1snii4c0ighfhkby7qx2b3wymqb4a0l76rlzfn";
+    url = mirror://sourceforge/libwpd/libwpd-0.8.14.tar.gz;
+    sha256 = "1syli6i5ma10cwzpa61a18pyjmianjwsf6pvmvzsh5md6yk4yx01";
   };
   buildInputs = [pkgconfig glib libgsf libxml2 bzip2];
 }
