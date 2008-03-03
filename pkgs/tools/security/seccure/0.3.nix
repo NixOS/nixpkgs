25f81f6438d973d57bedbab7db5295ae2ddb6ac4
Michael Raskin <7c6f434c@mail.ru>
2008-03-03 10:48:29 +0000

    Added SECCURE - Elliptic Curve Cryptography zero-configuration command-line tool. Note: needs to be setuid, because loves to lock memory.
    svn path=/nixpkgs/trunk/; revision=10926

---
+++ pkgs/tools/security/seccure/0.3.nix
@@ -0,0 +1,28 @@
+args : with args; with builderDefs {src="";} null;
+	let localDefs = builderDefs (rec {
+		src = /* put a fetchurl here */
+		fetchurl {
+			url = http://freshmeat.net/redir/seccure/65485/url_tgz/seccure-0.3.tar.gz;
+			sha256 = "0isah96p35yxm86dklmgmdkvpflqi2aj4k344jp57chrhg5av74d";
+		};
+
+		buildInputs = [libgcrypt];
+		configureFlags = [];
+		doPatch = FullDepEntry (''
+			sed -e s@/usr/@$out/@g -i Makefile
+			ensureDir $out/bin $out/share/man/man1
+		'') ["minInit" "doUnpack" "defEnsureDir"];
+	}) null; /* null is a terminator for sumArgs */
+	in with localDefs;
+stdenv.mkDerivation rec {
+	name = "seccure-"+version;
+	builder = writeScript (name + "-builder")
+		(textClosure localDefs 
+			["doPatch" doMakeInstall doForceShare doPropagate]);
+	meta = {
+		description = "
+		Elliptic Curve Cryptography. Zero-configuration.
+";
+		inherit src;
+	};
+}
