bde6dd899084eaf438869b83dac062538636e6df
Armijn Hemel <armijn@gpl-violations.org>
2008-03-03 21:44:17 +0000

    new powertop
    svn path=/nixpkgs/trunk/; revision=10932

--- pkgs/os-specific/linux/powertop/default.nix
+++ pkgs/os-specific/linux/powertop/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, ncurses}:
 
 stdenv.mkDerivation {
-  name = "powertop-1.8";
+  name = "powertop-1.9";
   src = fetchurl {
-    url = http://linuxpowertop.org/download/powertop-1.8.tar.gz;
-    sha256 = "0fmdbg00yfzhw5ldc5g6il602lvpmhi9dri4l0pc2ndgwm3fl9bk";
+    url = http://www.lesswatts.org/projects/powertop/download/powertop-1.9.tar.gz;
+    sha256 = "15150ra7n0q1nfij4ax3dnlplyjakm2ipx246xi3wsj3qc99m2a1";
   };
   patches = [./powertop-1.8.patch];
   buildInputs = [ncurses];
