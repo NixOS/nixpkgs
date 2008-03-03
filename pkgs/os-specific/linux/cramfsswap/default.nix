635b721180820052d4046712ba99fc576c975efd
Armijn Hemel <armijn@gpl-violations.org>
2008-03-03 21:37:55 +0000

    update to 1.4.1
    svn path=/nixpkgs/trunk/; revision=10931

--- pkgs/os-specific/linux/cramfsswap/default.nix
+++ pkgs/os-specific/linux/cramfsswap/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, zlib}:
 
 stdenv.mkDerivation {
-  name = "cramfsswap-1.3.1";
+  name = "cramfsswap-1.4.1";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://ftp.debian.org/debian/pool/main/c/cramfsswap/cramfsswap_1.3.1.tar.gz;
-    sha256 = "1dh1z50spxgi0nn33ag7zzm2svl8h1w32kfhy1lzc2vxvxdfyq9r";
+    url = http://ftp.debian.org/debian/pool/main/c/cramfsswap/cramfsswap_1.4.1.tar.gz;
+    sha256 = "0c6lbx1inkbcvvhh3y6fvfaq3w7d1zv7psgpjs5f3zjk1jysi9qd";
   };
 
   buildInputs = [zlib];
