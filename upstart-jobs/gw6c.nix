e48a01523fecc47556ab2cd4e90554296549b5e5
Michael Raskin <7c6f434c@mail.ru>
2008-03-04 15:30:54 +0000

    Gateway6 upstart job update.
    svn path=/nixos/trunk/; revision=10953

--- upstart-jobs/gw6c.nix
+++ upstart-jobs/gw6c.nix
@@ -5,7 +5,7 @@ let
 	gw6cService = import ../services/gw6c {
 		inherit (pkgs) stdenv gw6c coreutils 
 		procps upstart iputils gnused 
-		gnugrep;
+		gnugrep seccureUser writeScript;
 		username = cfg.username;
 		password = cfg.password;
 		server = cfg.server;
