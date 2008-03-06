173ae78b855852dd02c4bd60e907e43a7ad65c54
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-03-06 16:22:41 +0000

    * libpng 1.2.25.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=10994

--- pkgs/development/libraries/libpng/default.nix
+++ pkgs/development/libraries/libpng/default.nix
@@ -3,11 +3,16 @@
 assert zlib != null;
 
 stdenv.mkDerivation {
-  name = "libpng-1.2.24";
+  name = "libpng-1.2.25";
   src = fetchurl {
-    url = mirror://sourceforge/libpng/libpng-1.2.24.tar.bz2;
-    sha256 = "0kd0qkakc5zh2inrzw5r0h02761v1s9q223lv7za7iaxyl4byash";
+    url = mirror://sourceforge/libpng/libpng-1.2.25.tar.bz2;
+    sha256 = "1q3w2lfr7vkzvfxdgf54vj5s29xw8fg3hzb1lnfy9m9j1kdfm4if";
   };
   propagatedBuildInputs = [zlib];
   inherit zlib;
+
+  meta = {
+    description = "The official reference implementation for the PNG file format";
+    homepage = http://www.libpng.org/pub/png/libpng.html;
+  };
 }
