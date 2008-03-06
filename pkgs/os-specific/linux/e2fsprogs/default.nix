282c8421d5c3d787c795fae84ed2d71cb34c6e94
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-03-06 12:08:19 +0000

    * e2fsprogs 1.40.7.
    svn path=/nixpkgs/trunk/; revision=10981

--- pkgs/os-specific/linux/e2fsprogs/default.nix
+++ pkgs/os-specific/linux/e2fsprogs/default.nix
@@ -1,19 +1,18 @@
 {stdenv, fetchurl, gettext}:
 
 stdenv.mkDerivation {
-  name = "e2fsprogs-1.39";
+  name = "e2fsprogs-1.40.7";
   src = fetchurl {
-    url = mirror://sourceforge/e2fsprogs/e2fsprogs-1.39.tar.gz;
-    md5 = "06f7806782e357797fad1d34b7ced0c6";
+    url = mirror://sourceforge/e2fsprogs/e2fsprogs-1.40.7.tar.gz;
+    sha256 = "019czxrdz7f2nlcx4n9rb4cmjia7n038lhgnpamqdclxf0imfxjr";
   };
   configureFlags =
     if stdenv ? isDietLibC
     then ""
-    else "--enable-dynamic-e2fsck --enable-elf-shlibs";
-  buildInputs = [gettext];
-  patches = [./e2fsprogs-1.39_etc.patch];
+    else "--enable-elf-shlibs";
+  #buildInputs = [gettext];
   preInstall = "installFlagsArray=('LN=ln -s')";
-  postInstall = "make install-libs";
+  #postInstall = "make install-libs";
   NIX_CFLAGS_COMPILE =
     if stdenv ? isDietLibC then
       "-UHAVE_SYS_PRCTL_H " +
