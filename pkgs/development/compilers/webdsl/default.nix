7eb1759d37522155b5a4c21c89bd278e3e4233f2
Sander van der Burg <s.vanderburg@tudelft.nl>
2008-03-06 12:31:39 +0000

    Upgraded WebDSL
    svn path=/nixpkgs/trunk/; revision=10982

--- pkgs/development/compilers/webdsl/default.nix
+++ pkgs/development/compilers/webdsl/default.nix
@@ -1,12 +1,12 @@
 {stdenv, fetchurl, aterm, sdf, strategoxt, pkgconfig, javafront}:
 
 stdenv.mkDerivation {
-  name = "webdsl-7.12pre876";
+  name = "webdsl-8.2pre1006";
   builder = ./builder.sh;
 
   src = fetchurl {
-    url = http://buildfarm.st.ewi.tudelft.nl/releases/strategoxt/webdsl-7.12pre876-g60njq3p/webdsl-7.12pre876.tar.gz;
-    md5 = "7cd8709b02e03da74d90f8f8388e8d01";
+    url = http://buildfarm.st.ewi.tudelft.nl/releases/strategoxt/webdsl-8.2pre1006-c8h623yn/webdsl-8.2.tar.gz
+    md5 = "07c2471c961acc6467c55594e49da7c1";
   };
 
   inherit aterm sdf strategoxt javafront;
