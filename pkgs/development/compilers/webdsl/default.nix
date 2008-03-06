7c266891bea994e25823970e671d727df7ba3212
Sander van der Burg <s.vanderburg@tudelft.nl>
2008-03-06 12:39:38 +0000

    Fixed typo
    svn path=/nixpkgs/trunk/; revision=10983

--- pkgs/development/compilers/webdsl/default.nix
+++ pkgs/development/compilers/webdsl/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   builder = ./builder.sh;
 
   src = fetchurl {
-    url = http://buildfarm.st.ewi.tudelft.nl/releases/strategoxt/webdsl-8.2pre1006-c8h623yn/webdsl-8.2.tar.gz
+    url = http://buildfarm.st.ewi.tudelft.nl/releases/strategoxt/webdsl-8.2pre1006-c8h623yn/webdsl-8.2.tar.gz;
     md5 = "07c2471c961acc6467c55594e49da7c1";
   };
 
