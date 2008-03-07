4bd753eaebd325a0b41007f0590362bd7b3a2e4c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-03-07 15:40:07 +0000

    * Revert to pkg-config 0.22, even with the patch it changes the   Requires.private interpretation and I don't actually have a reason   to update to 0.23 anyway.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=11015

--- pkgs/development/tools/misc/pkgconfig/default.nix
+++ pkgs/development/tools/misc/pkgconfig/default.nix
@@ -1,25 +1,29 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "pkg-config-0.23";
+  name = "pkg-config-0.22";
   
   setupHook = ./setup-hook.sh;
   
   src = fetchurl {
-    url = http://pkgconfig.freedesktop.org/releases/pkg-config-0.23.tar.gz;
-    sha256 = "0lrvk17724mc2nzpaa0vwybarrl50r7qdnr4h6jijm50srrf1808";
+    url = http://pkgconfig.freedesktop.org/releases/pkg-config-0.22.tar.gz;
+    sha256 = "1rpb5wygmp0f8nal7y3ga4556i7hkjdslv3wdq04fj30gns621vy";
   };
 
   patches = [
     # Process Requires.private properly, see
     # http://bugs.freedesktop.org/show_bug.cgi?id=4738.
-    ./private.patch
+    (fetchurl {
+      name = "pkgconfig-8494.patch";
+      url = http://bugs.freedesktop.org/attachment.cgi?id=8494;
+      sha256 = "1pcrdbb7dypg2biy0yqc7bdxak5zii8agqljdvk7j4wbyghpqzws";
+    })
   ];
-
-  patchFlags = "-p0";
 
   meta = {
     description = "A tool that allows packages to find out information about other packages";
     homepage = http://pkg-config.freedesktop.org/wiki/;
-  };  
+  };
+
 }
+
