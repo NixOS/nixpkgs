00e0e9ac37871c3e9dde2f94bdc78a2e8e1e3b14
Ludovic Courtès <ludo@gnu.org>
2008-03-10 13:24:11 +0000

    Gnash: Install Mozilla plug-in.
    svn path=/nixpkgs/trunk/; revision=11034

--- pkgs/applications/video/gnash/default.nix
+++ pkgs/applications/video/gnash/default.nix
@@ -8,11 +8,12 @@
 stdenv.mkDerivation rec {
   name = "gnash-0.8.2";
 
-  builder = ./builder.sh;
   src = fetchurl {
     url = "mirror://gnu/gnash/0.8.2/${name}.tar.bz2";
     sha256 = "1akbs0wkgiawrjwwgp5w0cqn0qn3fcnfv40scjlrvdqrcqgfg0ac";
   };
+
+  builder = ./builder.sh;
 
   patchPhase = ''
     # Add all libs to `macros/libslist', a list of library search paths.
@@ -29,6 +30,9 @@ stdenv.mkDerivation rec {
                  libogg libxml2 libjpeg mesa libpng boost freetype agg
 		 dbus curl pkgconfig glib gtk];
   inherit SDL_mixer SDL;
+
+  preInstall = ''ensureDir $out/plugins'';
+  postInstall = ''make install-plugins'';
 
   meta = {
     homepage = http://www.gnu.org/software/gnash/;
