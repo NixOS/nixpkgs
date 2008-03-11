54a46e4c506d0c014875bc1519a0bbd73e44c67b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-03-11 17:01:55 +0000

    * Samba updated to 3.0.28a.  Removed the static config file.
    svn path=/nixpkgs/trunk/; revision=11062

--- pkgs/servers/samba/default.nix
+++ pkgs/servers/samba/default.nix
@@ -1,23 +1,34 @@
 args: with args;
 
 stdenv.mkDerivation rec {
-  name = "samba-3.0.28";
+  name = "samba-3.0.28a";
 
   src = fetchurl {
-    url = "http://us1.samba.org/samba/ftp/stable/${name}.tar.gz";
-    sha256 = "13nr4mvh6vxgl7nb94qnqx3njcyd10cf4ji18srlkizpp49r5byw";
+    url = http://us3.samba.org/samba/ftp/stable/samba-3.0.28a.tar.gz;
+    sha256 = "1pnrh4qlapsqgdrzq4v5vxv622wcxghi5cp0n4f87c8pc2rfrjcx";
   };
 
   buildInputs = [readline pam openldap kerberos popt iniparser libunwind fam];
-  configureFlags = ''--with-pam --with-smbmount --datadir=$out/share
-  --with-aio-support ''
-  + (if (stdenv.gcc.libc != null) then "--with-libiconv=${stdenv.gcc.libc}" else "");
-  postUnpack = "sourceRoot=\$sourceRoot/source";
+
+  preConfigure = "cd source";
   
-  configFile = ./smb.conf;
-  postInstall = ''
-  rm -rf $out/var
-  ln -s /var/samba $out/var
-  cp ${configFile} $out/lib/smb.conf
+  configureFlags = ''
+    --with-pam
+    --with-smbmount
+    --with-aio-support
+    ${if (stdenv.gcc.libc != null) then "--with-libiconv=${stdenv.gcc.libc}" else ""}
   '';
+
+
+  #  --datadir=$out/share
+    
+  #postUnpack = "sourceRoot=\$sourceRoot/source";
+  
+  #configFile = ./smb.conf;
+  
+  #postInstall = ''
+  #  rm -rf $out/var
+  #  ln -s /var/samba $out/var
+  #  cp ${configFile} $out/lib/smb.conf
+  #'';
 }
