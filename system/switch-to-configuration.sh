8389cc378a42baa1be52a03e89f7560d099c6b7e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-03-13 10:15:12 +0000

    * Hack: don't restart dbus automatically, it kills the X server.   Apparantly the cause was not the upgrade to DBus 1.1.20 but   something else.
    svn path=/nixos/trunk/; revision=11095

--- system/switch-to-configuration.sh
+++ system/switch-to-configuration.sh
@@ -73,7 +73,7 @@ if test "$action" = "switch" -o "$action" = "test"; then
 
         # Hack: skip the sys-* and ctrl-alt-delete events.
         # Another hack: don't restart the X server (that would kill all the clients).
-        if echo "$event" | grep -q "^sys-\|^ctrl-\|^xserver"; then continue; fi
+        if echo "$event" | grep -q "^sys-\|^ctrl-\|^xserver|^dbus"; then continue; fi
     
         if ! test -e "$oldEvents/$event"; then
             echo "starting $event..."
