572e6a28c6fb633ac847253722944bd032a968e8
Tobias Hammerschmidt <tobias.hammerschmidt@gmx.de>
2008-03-16 12:38:59 +0000

    version update
    svn path=/nixpkgs/trunk/; revision=11145

--- pkgs/misc/tex/auctex/default.nix
+++ pkgs/misc/tex/auctex/default.nix
@@ -1,15 +1,24 @@
 { stdenv, fetchurl, emacs, tetex }:
  
-stdenv.mkDerivation {
-  name = "auctex-11.84";
+stdenv.mkDerivation ( rec {
+  pname = "auctex";
+  version = "11.85";
+  name = "${pname}-${version}";
+
   meta = {
     description = "AUCTeX is an extensible package for writing and formatting TeX files in GNU Emacs and XEmacs.";
     homepage = http://www.gnu.org/software/auctex;
   };
+
   src = fetchurl {
-    url = http://ftp.gnu.org/pub/gnu/auctex/auctex-11.84.tar.gz;
-    md5 = "73970c51221524442c11cde13d0584e9";
+    url = "http://ftp.gnu.org/pub/gnu/${pname}/${name}.tar.gz";
+    sha256 = "aebbea00431f8fd1e6be6519d9cc28e974942000737f956027da2c952a6d304e";
   };
-  configureFlags="--with-lispdir=\${out}/emacs/site-lisp --disable-preview";
+
   buildInputs = [ emacs tetex ];
-}
+
+  configureFlags = [
+    "--with-lispdir=\${out}/emacs/site-lisp"
+    "--disable-preview"
+  ];
+})
