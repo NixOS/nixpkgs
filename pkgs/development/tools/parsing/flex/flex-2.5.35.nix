a3ea4c135b621de706a492b427ed5bfc7fb8a5f4
Michael Raskin <7c6f434c@mail.ru>
2008-03-20 09:50:15 +0000

    Newer flex.
    svn path=/nixpkgs/trunk/; revision=11213

---
+++ pkgs/development/tools/parsing/flex/flex-2.5.35.nix
@@ -0,0 +1,19 @@
+# This should be moved to default.nix eventually (?)
+
+{stdenv, fetchurl, yacc, m4}:
+
+assert yacc != null && m4 != null;
+
+stdenv.mkDerivation {
+  name = "flex-2.5.35";
+  src = fetchurl {
+    url = mirror://sourceforge/flex/flex-2.5.35.tar.bz2;
+    sha256 = "0ysff249mwhq0053bw3hxh58djc0gy7vjan2z1krrf9n5d5vvv0b";
+  };
+  buildInputs = [yacc];
+  propagatedBuildInputs = [m4];
+
+  meta = {
+    description = "A fast lexical analyser generator";
+  };
+}
