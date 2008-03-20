2cf00550927658eb08c16bd772134e844d82ba1a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-03-20 18:51:07 +0000

    * lcov 1.6.
    svn path=/nixpkgs/trunk/; revision=11245

--- pkgs/development/tools/analysis/lcov/builder.sh
+++ pkgs/development/tools/analysis/lcov/builder.sh
@@ -1,19 +1,7 @@
 source $stdenv/setup
 
-installFlags="PREFIX=$out"
 
-preInstall=preInstall
 preInstall() {
-    substituteInPlace bin/install.sh --replace /bin/bash $shell
-
-    for i in bin/*; do
-        echo "fixing $i..."
-        sed -e "s^@PREFIX@^$out^" \
-            -e "s^@PERL@^$perl/bin/perl^" \
-            < $i > $i.tmp
-        mv $i.tmp $i
-        chmod +x $i
-    done
 }
 
 genericBuild
--- pkgs/development/tools/analysis/lcov/default.nix
+++ pkgs/development/tools/analysis/lcov/default.nix
@@ -1,12 +1,35 @@
 {stdenv, fetchurl, perl}:
 
 stdenv.mkDerivation {
-  name = "lcov-1.4pre-cvs-20041021";
-  builder = ./builder.sh;
+  name = "lcov-1.6";
+  
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/lcov-1.4pre-cvs-20041021.tar.gz;
-    md5 = "5150da759b4047b19d026c6ab1216841";
+    url = mirror://sourceforge/ltp/lcov-1.6.tar.gz;
+    sha256 = "0d6lb0vlj3lvqmm678jic9h25q4dnlkbv37wg5yj311hdr9ls1kx";
   };
-  patches = [./lcov.patch];
-  inherit perl;
+
+  patches = [
+    # http://ltp.cvs.sourceforge.net/ltp/utils/analysis/lcov/bin/geninfo?revision=1.33&view=markup&pathrev=HEAD
+    ./string.patch
+  ];
+
+  preBuild = ''
+    makeFlagsArray=(PREFIX=$out BIN_DIR=$out/bin MAN_DIR=$out/share/man)
+  '';
+
+  preInstall = ''
+    substituteInPlace bin/install.sh --replace /bin/bash $shell
+  '';
+
+  postInstall = ''
+    for i in $out/bin/*; do
+      substituteInPlace $i --replace /usr/bin/perl ${perl}/bin/perl
+    done
+  ''; # */
+
+  meta = {
+    description = "A code coverage tool for Linux";
+    homepage = http://ltp.sourceforge.net/coverage/lcov.php;
+  };
+
 }
