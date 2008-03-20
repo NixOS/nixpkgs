91dd7e2b86afdd10782024eaa511a73de26417b1
Marc Weber <marco-oweber@gmx.de>
2008-03-20 11:36:31 +0000

    fix: tunctl is build again (also added check)
    svn path=/nixpkgs/trunk/; revision=11222

--- pkgs/os-specific/linux/uml-utilities/builder.sh
+++ pkgs/os-specific/linux/uml-utilities/builder.sh
@@ -3,11 +3,11 @@ source $stdenv/setup
 
 tar xvfj $src
 cd tools
-sed s/mconsole// < Makefile > tmp
-mv tmp Makefile
+sed -e 's/mconsole//' -e '1s/.*/TUNCTL = \$(shell [ -n tunctl ] \&\& echo tunctl)/' -i Makefile
 mkdir $out
 mkdir $out/bin
 mkdir $out/lib
 mkdir $out/lib/uml
 make BIN_DIR=$out/bin LIB_DIR=$out/lib/uml
 make BIN_DIR=$out/bin LIB_DIR=$out/lib/uml install
+[ -n $tunctl ] && [ -f $out/bin/tunctl ] || fail_no_tunctl
