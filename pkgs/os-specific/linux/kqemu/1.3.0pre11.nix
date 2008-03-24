0dfa801a82b2361f4fd2a2f03c021caa518c49ec
Michael Raskin <7c6f434c@mail.ru>
2008-03-24 19:39:12 +0000

    Updated builderDefs style. Now more things are overridable. Needed to fix build with custom kernels.
    svn path=/nixpkgs/trunk/; revision=11274

--- pkgs/os-specific/linux/kqemu/1.3.0pre11.nix
+++ pkgs/os-specific/linux/kqemu/1.3.0pre11.nix
@@ -1,33 +1,26 @@
 args : with args;
-	let localDefs = builderDefs {
-		src = /* put a fetchurl here */
-		fetchurl {
-			url = http://fabrice.bellard.free.fr/qemu/kqemu-1.3.0pre11.tar.gz;
-			sha256 = "03svg2x52ziglf9r9irf6ziiz8iwa731fk1mdskwdip5jxbyy6jl";
-		};
-		buildInputs = [];
-		configureFlags = [''--prefix=$out'' ''--kernel-path=$(ls -d ${kernel}/lib/modules/*/build)''];
-	} null; /* null is a terminator for sumArgs */
-	in with localDefs;
-let 
+rec {
+  name = "kqemu-"+version;
+  src = fetchurl {
+    url = http://fabrice.bellard.free.fr/qemu/kqemu-1.3.0pre11.tar.gz;
+    sha256 = "03svg2x52ziglf9r9irf6ziiz8iwa731fk1mdskwdip5jxbyy6jl";
+  };
+
+  buildInputs = [];
+  configureFlags = [''--PREFIx=$out'' ''--kernel-path=$(ls -d ${kernel}/lib/modules/*/build)''];
   debugStep = FullDepEntry (''
   	cat config-host.mak
-  '') [minInit];
+  '') ["minInit"];
   preConfigure = FullDepEntry ('' 
-  	sed -e 's/`uname -r`/'"$(basename ${kernel}/lib/modules/*)"'/' -i install.sh
-  	sed -e '/kernel_path=/akernel_path=$out$kernel_path' -i install.sh
-	sed -e '/depmod/d' -i install.sh
-	cat install.sh
-  '') [minInit doUnpack];
-in
-stdenv.mkDerivation rec {
-	name = "kqemu-"+version;
-	builder = writeScript (name + "-builder")
-		(textClosure localDefs [preConfigure doConfigure debugStep doMakeInstall doForceShare doPropagate]);
-	meta = {
-		description = "
-		Kernel module for Qemu acceleration
-";
-		inherit src;
-	};
+    sed -e 's/`uname -r`/'"$(basename ${kernel}/lib/modules/*)"'/' -i install.sh
+    sed -e '/kernel_path=/akernel_path=$out$kernel_path' -i install.sh
+    sed -e '/depmod/d' -i install.sh
+    cat install.sh
+  '') ["minInit" "doUnpack"];
+
+  phaseNames = ["preConfigure" "doConfigure" "debugStep" "doMakeInstall"];
+
+  meta = {
+    description = " Kernel module for Qemu acceleration ";
+  }; 
 }
