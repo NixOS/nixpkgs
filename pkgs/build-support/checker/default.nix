c150dc2cf3c5e498225c05f7fa11d4a6f52faf36
Marc Weber <marco-oweber@gmx.de>
2008-03-24 23:47:40 +0000

    ignoring config option servicesProposal (is checked in upstart-jobs/default.nix)
    svn path=/nixpkgs/trunk/; revision=11278

--- pkgs/build-support/checker/default.nix
+++ pkgs/build-support/checker/default.nix
@@ -18,7 +18,9 @@ let
 	if (! isAttrs b) then s else
 	if (lib.getAttr ["_type"] "" b) == "option" then "" else
 	findInList (x : x != "") 
-		(map (x: checkAttrInclusion 
+		( map (x: if (x == "servicesProposal") # this attr will be checked at another place ( -> upstart-jobs/default.nix )
+                       then ""
+                       else checkAttrInclusion 
 			(s + "." + x) 
 			(__getAttr x a)
 			(lib.getAttr [x] null b)) 
