9f0f6cb5ecf152ff3167af7d944bc5f9f41db654
Tobias Hammerschmidt <tobias.hammerschmidt@gmx.de>
2008-03-27 09:46:03 +0000

    version update
    svn path=/nixpkgs/trunk/; revision=11297

--- pkgs/development/libraries/java/hsqldb/default.nix
+++ pkgs/development/libraries/java/hsqldb/default.nix
@@ -2,12 +2,12 @@
 }:
 
 stdenv.mkDerivation {
-  name = "hsqldb-1.8.0.8";
+  name = "hsqldb-1.8.0.9";
   builder = ./builder.sh;
 
   src = fetchurl {
-    url = mirror://sourceforge/hsqldb/hsqldb_1_8_0_8.zip;
-    sha256 = "1428pkizd19i2p7iiha0nkgypa3hznj4vbw503ayyhnnhbgw4ama";
+    url = mirror://sourceforge/hsqldb/hsqldb_1_8_0_9.zip;
+    sha256 = "e98d1d8bca15059f4ef4f0d3dde2d75778a5e1bbe8bc12abd4ec2cac39d5adec";
   };
 
   buildInputs = [ unzip
