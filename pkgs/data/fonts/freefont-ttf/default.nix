553a3af3ceafd4c5e0d5a63444630cbc3aaf9437
Ludovic Courtès <ludo@gnu.org>
2008-03-29 22:07:15 +0000

    Update GNU Freefont.
    svn path=/nixpkgs/trunk/; revision=11344

--- pkgs/data/fonts/freefont-ttf/default.nix
+++ pkgs/data/fonts/freefont-ttf/default.nix
@@ -1,13 +1,27 @@
 {stdenv, fetchurl}:
 
-stdenv.mkDerivation {
-  name = "freefont-ttf-20060126";
+stdenv.mkDerivation rec {
+  name = "freefont-ttf-20080323";
   src = fetchurl {
-    url = http://download.savannah.gnu.org/releases/freefont/freefont-ttf-20060126.tar.gz;
-    md5 = "822aba4e2ed065d9d3ded6e26e495854";
+    url = "mirror://gnu/freefont/${name}.tar.gz";
+    sha256 = "17wkasp5bgzw8n1dxpfcgj5zawfb743ff3cq24qh4brkyfrf2jkr";
   };
+
   installPhase = ''
     ensureDir $out/share/fonts/truetype
     cp *.ttf $out/share/fonts/truetype
   '';
+
+  meta = {
+    description = "GNU Free UCS Outline Fonts";
+
+    longDescription = ''
+      The GNU Freefont project aims to provide a set of free outline
+      (PostScript Type0, TrueType, OpenType...) fonts covering the ISO
+      10646/Unicode UCS (Universal Character Set).
+    '';
+
+    homepage = http://www.gnu.org/software/freefont/;
+    license = "GPLv2+";
+  };
 }
