b5e0ee02aaf33b7d5735a712f8b82d1e669644f2
Ludovic Courtès <ludo@gnu.org>
2008-04-06 20:43:39 +0000

    cvs: Use SHA256 instead of MD5.
    svn path=/nixpkgs/trunk/; revision=11473

--- pkgs/applications/version-management/cvs/default.nix
+++ pkgs/applications/version-management/cvs/default.nix
@@ -1,11 +1,12 @@
-{stdenv, fetchurl, nano}:
+{ stdenv, fetchurl, nano }:
 
 stdenv.mkDerivation {
   name = "cvs-1.12.13";
 
   src = fetchurl {
     url = http://ftp.gnu.org/non-gnu/cvs/source/feature/1.12.13/cvs-1.12.13.tar.bz2;
-    md5 = "956ab476ce276c2d19d583e227dbdbea";
+    sha256 = "0pjir8cwn0087mxszzbsi1gyfc6373vif96cw4q3m1x6p49kd1bq";
   };
-  buildInputs = [nano];
+
+  buildInputs = [ nano ];
 }
