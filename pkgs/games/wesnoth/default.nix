ff87b2ffa99eb8ba057b4966870132cf88bafb5f
Tobias Hammerschmidt <tobias.hammerschmidt@gmx.de>
2008-04-08 18:19:15 +0000

    version update
    svn path=/nixpkgs/trunk/; revision=11495

--- pkgs/games/wesnoth/default.nix
+++ pkgs/games/wesnoth/default.nix
@@ -1,13 +1,13 @@
 args: with args;
 stdenv.mkDerivation (rec {
   pname = "wesnoth";
-  version = "1.4";
+  version = "1.4.1";
 
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "http://surfnet.dl.sourceforge.net/sourceforge/${pname}/${name}.tar.bz2";
-    sha256 = "67ad0509567d9496f15f1a3888c9e2001776411ffdd7007bdb8f324cdce5895d";
+    sha256 = "c5b7b478553dc8994d99024e14b48b3f64e2c328631bd5b05904509b499fa68c";
   };
 
   buildInputs = [SDL SDL_image SDL_mixer SDL_net gettext zlib boost freetype];
