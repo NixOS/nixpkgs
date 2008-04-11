b9e87b09aae77c74e7bbe7035508fddca6058350
Marc Weber <marco-oweber@gmx.de>
2008-04-11 15:39:27 +0000

    updated cabal version
    svn path=/nixpkgs/trunk/; revision=11576

--- pkgs/misc/bleeding-edge-fetch-info/cabal.nix
+++ pkgs/misc/bleeding-edge-fetch-info/cabal.nix
@@ -1,3 +1,3 @@
  args: with args; fetchurl { 
   url = http://mawercer.de/~nix/repos/cabal.tar.gz;
-  sha256 = "62b0059cd57e1fb96930858cb575c379bb129f9f2ba94d9e65a8f2e96b4db04e";}
+  sha256 = "33bdbe8f0c213dcc28da88a2775f81f72e89209806e26b412699220c3db206ec";}
