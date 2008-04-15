b6c405fac66fd0f1520057711536747b9159fdb0
Marc Weber <marco-oweber@gmx.de>
2008-04-15 11:46:43 +0000

    bazaar update 1.3.1
    svn path=/nixpkgs/trunk/; revision=11633

--- pkgs/applications/version-management/bazaar/default.nix
+++ pkgs/applications/version-management/bazaar/default.nix
@@ -4,8 +4,8 @@ stdenv.mkDerivation {
   name = "bazaar-1.1";
 
   src = fetchurl {
-    url = https://launchpad.net/bzr/1.1/1.1/+download/bzr-1.1.tar.gz;
-    sha256 = "1qpkw580r22yxybdghx2ha0kyk22brbhd1kg9wwjh209dqy2gqzc";
+    url = http://launchpad.net/bzr/1.3/1.3.1/+download/bzr-1.3.1.tar.gz;
+    sha256 = "1d81bm8cdim6a7x02z88m594zymsg4kh73njq6q30k5656smjpym";
   };
 
   buildInputs = [python makeWrapper];
