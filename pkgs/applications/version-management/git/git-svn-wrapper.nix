ec9950c07af98a666ecc1ce72caa55009d5dc7f7
Ludovic Courtès <ludo@gnu.org>
2008-04-15 14:49:36 +0000

    git-svn: Don't produce any binary other than `git-svn'.
    svn path=/nixpkgs/trunk/; revision=11635

--- pkgs/applications/version-management/git/git-svn-wrapper.nix
+++ pkgs/applications/version-management/git/git-svn-wrapper.nix
@@ -1,22 +1,24 @@
-args: with args;
-if (!args.subversion.perlBindings) then throw "svn perl bindings required to install gitsvnwrapper"
-else args.stdenv.mkDerivation {
-  inherit git perlLibs subversion;
+{ stdenv, subversion, git, perlLibs }:
+
+if (!subversion.perlBindings)
+  then throw "svn perl bindings required to install gitsvnwrapper"
+else stdenv.mkDerivation {
+  inherit perlLibs;
   name = "git-svn-wrapper";
   phases = "buildPhase";
   buildPhase = "
-    gitperllib=\$git/lib/site_perl
+    gitperllib=${git}/lib/site_perl
     for i in \$perlLibs; do
       gitperllib=\$gitperllib:\$i/lib/site_perl
     done
 
     ensureDir \$out/bin
-    for a in \$git/bin/*; do
+    for a in ${git}/bin/git-svn; do
       target=\$out/bin/\$(basename $a)
       target=\$out/bin/\$(basename $a)
       echo \"#!/bin/sh
         export GITPERLLIB=\$gitperllib
-        PATH=\\\$PATH:$subversion/bin
+        PATH=\\\$PATH:${subversion}/bin
         $a \\\"\\\$@\\\"
       \" > \$target
       chmod +x \$target
@@ -24,6 +26,8 @@ else args.stdenv.mkDerivation {
   ";
 
   meta = { 
-      description = "simple wrapper around git adding env variables so that git-svn works without extra work";
+    description = "git-svn, a bidirectional Git/Subversion gateway";
+    license = "GPLv2";
+    homepage = http://git.or.cz/;
   };
 }
