578b598fbb77366f98053fd24cd2a48a97b2775e
Ludovic Courtès <ludo@gnu.org>
2008-04-25 16:14:08 +0000

    Remove former GNU ed expression.
    svn path=/nixpkgs/trunk/; revision=11716

--- pkgs/tools/text/ed/default.nix
+++
@@ -1,7 +0,0 @@
-{stdenv, fetchurl}: stdenv.mkDerivation {
-  name = "ed-0.9";
-  src = fetchurl {
-    url = mirror://gnu/ed/ed-0.9.tar.bz2;
-    sha256 = "1xy746g7ai9gmv6iq2x1ll8x6wy4fi9anfh7gj5ifsdnaiahgyi2";
-  };
-}
