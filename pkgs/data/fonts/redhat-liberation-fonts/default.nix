d05d1e3af51b2be3e067141cf6123bba4768194b
Michael Raskin <7c6f434c@mail.ru>
2008-04-26 15:54:37 +0000

    Stupid package name typo
    svn path=/nixpkgs/trunk/; revision=11729

--- pkgs/data/fonts/redhat-liberation-fonts/default.nix
+++ pkgs/data/fonts/redhat-liberation-fonts/default.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "freefont-ttf-20060126";
+  name = "redhat-liberation";
   src = fetchurl {
     url = http://www.redhat.com/f/fonts/liberation-fonts-ttf-3.tar.gz;
     sha256 = "13zzyqyi3mf676aj3fg9122asyykpx94mk689r1p2ab1axyg4k0p";
