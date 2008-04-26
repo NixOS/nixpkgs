07d8e537749dd1b82d8ebcc804d713b52eb09ad0
Michael Raskin <7c6f434c@mail.ru>
2008-04-26 15:22:48 +0000

    Added LModern TeX Fonts
    svn path=/nixpkgs/trunk/; revision=11726

---
+++ pkgs/data/fonts/lmodern/1.010.nix
@@ -0,0 +1,25 @@
+args : with args; 
+rec {
+  src = fetchurl {
+    url = http://ftp.de.debian.org/debian/pool/main/l/lmodern/lmodern_1.010x.orig.tar.gz;
+    sha256 = "0nwxj1ng7rvnp16jxcs25hbc5in65mdk4a3g3rlaq91i5qpq7mxj";
+  };
+
+  buildInputs = [];
+  configureFlags = [];
+
+  /* doConfigure should be specified separately */
+  phaseNames = ["doCopy"];
+
+  doCopy = FullDepEntry(''
+    ensureDir $out/share/texmf/
+
+    cp -r ./* $out/share/texmf/
+  '') ["minInit" "defEnsureDir" "doUnpack"];
+
+  name = "lmodern-" + version;
+  meta = {
+    description = "Latin Modern font";
+  };
+}
+
