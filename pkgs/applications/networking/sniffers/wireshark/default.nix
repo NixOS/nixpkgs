10ca296d82ca4168af17f06096bc16fb0ad59540
Armijn Hemel <armijn@gpl-violations.org>
2008-04-27 21:47:21 +0000

    change 'name' to version number of the tarball (was still at 0.99.7)
    svn path=/nixpkgs/trunk/; revision=11732

--- pkgs/applications/networking/sniffers/wireshark/default.nix
+++ pkgs/applications/networking/sniffers/wireshark/default.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchurl, perl, pkgconfig, gtk, libpcap, flex, bison}:
 
 stdenv.mkDerivation {
-  name = "wireshark-0.99.7";
+  name = "wireshark-1.0.0";
   src = fetchurl {
     url = http://www.wireshark.org/download/src/all-versions/wireshark-1.0.0.tar.bz2;
     sha256 = "1l4zrmxf3i2i8a5f953pbbpy1c44d1q3r79hbzq7q9x78vhi6ixm";
