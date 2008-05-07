8fa23b53af1a840cec056a539ecaea767b38c4b9
Michael Raskin <7c6f434c@mail.ru>
2008-05-07 07:22:41 +0000

    Added tailor
    svn path=/nixpkgs/trunk/; revision=11766

---
+++ pkgs/applications/version-management/tailor/0.9.31.nix
@@ -0,0 +1,19 @@
+args : with args; 
+rec {
+  src = fetchurl {
+    url = http://darcs.arstecnica.it/tailor-0.9.31.tar.gz;
+    sha256 = "1apzd6mfmhgmxffzgzwsr17gnyqj6bycn783l9105cihsfcv9v3j";
+  };
+
+  buildInputs = [python];
+  configureFlags = [];
+
+  /* doConfigure should be specified separately */
+  phaseNames = ["installPythonPackage"];
+      
+  name = "tailor-" + version;
+  meta = {
+    description = "Version control tools integration tool";
+  };
+}
+ 
