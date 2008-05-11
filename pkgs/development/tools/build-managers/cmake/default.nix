1f15a6abfdaf8fb77e7ae8171c1b8e707c4ae86e
Marc Weber <marco-oweber@gmx.de>
2008-05-11 14:52:09 +0000

    updated cmake to 2.6.0 (Pierre on #cmake told me it's almost compatible with 2.4) so I don't expect any trouble
    svn path=/nixpkgs/trunk/; revision=11814

--- pkgs/development/tools/build-managers/cmake/default.nix
+++ pkgs/development/tools/build-managers/cmake/default.nix
@@ -1,11 +1,10 @@
 args: with args;
 stdenv.mkDerivation {
-  name = "cmake-2.4.7";
+  name = "cmake-2.6.0";
   setupHook = ./setup-hook.sh;
 
-  src = fetchurl {
-    url = http://www.cmake.org/files/v2.4/cmake-2.4.7.tar.gz;
-    sha256 = "0mkx23s7zq48hzzzw3vbzsfzfz3rjsiwgf3i00xawcxrjjrgxm9g";
+  src = fetchurl { url=http://www.cmake.org/files/v2.6/cmake-2.6.0.tar.gz;
+                   sha256 = "09qgk5gk0pnihzf2mmqz5cayd64y5viic8x78x4czrh4982x76a9";
   };
 
   propagatedBuildInputs = [replace];
