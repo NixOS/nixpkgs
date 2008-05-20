a4e77530c8074271d78899f9e18b03132f4591db
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-05-20 09:19:17 +0000

    * nss_ldap: purity fix: don't try to install /etc/ldap.conf.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=11850

--- pkgs/os-specific/linux/nss_ldap/default.nix
+++ pkgs/os-specific/linux/nss_ldap/default.nix
@@ -1,18 +1,20 @@
 {stdenv, fetchurl, openldap}:
    
 stdenv.mkDerivation {
-  name = "nss_ldap-254";
+  name = "nss_ldap-260";
    
   src = fetchurl {
-    url = http://www.padl.com/download/nss_ldap-254.tar.gz;
-    md5 = "00475b790d3aff3ccd40a1ab4520965e";
+    url = http://www.padl.com/download/nss_ldap-260.tar.gz;
+    sha256 = "0kn022js39mqmy7g5ba911q46223vk7vcf51x28rbl86lp32zv4v";
   };
 
-  preInstall = "
+  preInstall = ''
     installFlagsArray=(INST_UID=$(id -u) INST_GID=$(id -g) LIBC_VERS=2.5 NSS_VERS=2)
-    substituteInPlace Makefile --replace '/usr$(libdir)' $TMPDIR
+    substituteInPlace Makefile \
+      --replace '/usr$(libdir)' $TMPDIR \
+      --replace 'install-data-local:' 'install-data-local-disabled:'
     ensureDir $out/etc
-  ";
+  '';
 
   buildInputs = [openldap];
 }
