c741baeb4e1743aee549676fd4a357eb33b3737b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-05-22 11:39:30 +0000

    * Proper name attribute.
    svn path=/nixpkgs/trunk/; revision=11879

--- pkgs/tools/security/nmap/default.nix
+++ pkgs/tools/security/nmap/default.nix
@@ -1,17 +1,21 @@
-{stdenv, fetchurl, libpcap, libX11, gtk, pkgconfig,
-  openssl, python, pygtk, makeWrapper, pygobject,
-  pycairo, pysqlite}:
+{ stdenv, fetchurl, libpcap, libX11, gtk, pkgconfig
+, openssl, python, pygtk, makeWrapper, pygobject
+, pycairo, pysqlite
+}:
+  
 stdenv.mkDerivation {
-  name = "Nmap";
+  name = "nmap-4.60";
 
   src = fetchurl {
     url = http://download.insecure.org/nmap/dist/nmap-4.60.tar.bz2;
-    sha256 = "1jhway86lmrnyzvwi24ama1vrz89f9nmln29vr92kb31aw2nl30w";  };
+    sha256 = "1jhway86lmrnyzvwi24ama1vrz89f9nmln29vr92kb31aw2nl30w";
+  };
 
   postInstall =''
     wrapProgram $out/bin/zenmap --prefix PYTHONPATH : "$(toPythonPath $out)" --prefix PYTHONPATH : "$PYTHONPATH" --prefix PYTHONPATH : $(toPythonPath ${pygtk})/gtk-2.0 --prefix PYTHONPATH : $(toPythonPath ${pygobject})/gtk-2.0 --prefix PYTHONPATH : $(toPythonPath ${pycairo})/gtk-2.0
   '';
 
-  buildInputs = [libpcap libX11 gtk pkgconfig openssl python 
-    pygtk makeWrapper pysqlite];
+  buildInputs = [
+    libpcap libX11 gtk pkgconfig openssl python pygtk makeWrapper pysqlite
+  ];
 }
