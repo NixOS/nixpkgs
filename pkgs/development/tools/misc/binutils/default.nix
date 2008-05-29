25245042c2671054a8229aaa90f9050777174c00
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-05-29 14:16:35 +0000

    * Turn on ld's --enable-new-dtags option by default to get RUNPATHs   instead of RPATHs.  Borrowed from Gentoo.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=11930

--- pkgs/development/tools/misc/binutils/default.nix
+++ pkgs/development/tools/misc/binutils/default.nix
@@ -2,12 +2,28 @@
 
 stdenv.mkDerivation {
   name = "binutils-2.18";
-  builder = ./builder.sh;
+  
   src = fetchurl {
     url = mirror://gnu/binutils/binutils-2.18.tar.bz2;
     sha256 = "16zfc7llbjdn69bbdy7kqgg2xa67ypgj7z5qicgwzvghaaj36yj8";
   };
+
+  patches = [
+    # Turn on --enable-new-dtags by default to make the linker set
+    # RUNPATH instead of RPATH on binaries.  This is important because
+    # RUNPATH can be overriden using LD_LIBRARY_PATH at runtime.
+    ./new-dtags.patch
+  ];
+  
   inherit noSysDirs;
+
+  preConfigure = ''
+    # Clear the default library search path.
+    if test "$noSysDirs" = "1"; then
+        echo 'NATIVE_LIB_DIRS=' >> ld/configure.tgt
+    fi
+  '';
+  
   configureFlags = "--disable-werror"; # needed for dietlibc build
 
   meta = {
