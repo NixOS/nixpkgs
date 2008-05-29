eeb82eb2df7a8ed6a580036a35ee47c2e6deb2ff
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-05-29 15:36:15 +0000

    * Wine 1.0rc2.
    svn path=/nixpkgs/trunk/; revision=11932

--- pkgs/misc/emulators/wine/default.nix
+++ pkgs/misc/emulators/wine/default.nix
@@ -8,11 +8,11 @@ assert stdenv.isLinux;
 let lib = import ../../../lib/default.nix; in
 
 stdenv.mkDerivation {
-  name = "wine-0.9.60";
+  name = "wine-1.0-pre-rc2";
 
   src = fetchurl {
-    url = mirror://sourceforge/wine/wine-0.9.60.tar.bz2;
-    sha256 = "fb5bd9d70746c1c0a0d5fb506bf2d0fdb36bceaf1a790964890e934148722948";
+    url = mirror://sourceforge/wine/wine-1.0-rc2.tar.bz2;
+    sha256 = "1f0qi4qv8scfgq9angikimr8zl65jl9jxqpikmcmxq4gwqc265h6";
   };
 
   buildInputs = [
@@ -26,7 +26,7 @@ stdenv.mkDerivation {
   # Wine locates a lot of libraries dynamically through dlopen().  Add
   # them to the RPATH so that the user doesn't have to set them in
   # LD_LIBRARY_PATH.
-  NIX_LDFLAGS = map (path: "-rpath ${path}/lib") [
+  NIX_LDFLAGS = map (path: "-rpath ${path}/lib ") [
     freetype fontconfig stdenv.gcc.gcc mesa mesa.libdrm
     xlibs.libXinerama xlibs.libXrender xlibs.libXrandr
     xlibs.libXcursor xlibs.libXcomposite
