5fadd928bcc4bd013ed0c7883714852350df54fe
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-06-03 14:29:14 +0000

    * Use a fixed patchelf.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=11958

--- pkgs/development/tools/misc/patchelf/default.nix
+++ pkgs/development/tools/misc/patchelf/default.nix
@@ -1,15 +1,15 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "patchelf-0.4pre11926";
+  name = "patchelf-0.4pre11957";
   
   src = fetchurl {
-    url = http://nixos.org/releases/patchelf/patchelf-0.4pre11926-l7bh3lls/patchelf-0.4pre11926.tar.bz2;
+    url = http://nixos.org/releases/patchelf/patchelf-0.4pre11957-lngvn112/patchelf-0.4pre11957.tar.bz2;
     sha256 = "39cb2a277bb9cb6fe216694b0db3b7e364e2ec0597e437e5cb0fd4378fd172f3";
   };
 
   meta = {
-    homepage = "http://nixos.org/patchelf.html";
+    homepage = http://nixos.org/patchelf.html;
     license = "GPL";
     description = "A small utility to modify the dynamic linker and RPATH of ELF executables";
   };
