c6e98aeba3b3ce8148a6133e3b360f990db66130
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-06-03 14:36:15 +0000

    * Doh!
    svn path=/nixpkgs/branches/stdenv-updates/; revision=11959

--- pkgs/development/tools/misc/patchelf/default.nix
+++ pkgs/development/tools/misc/patchelf/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   
   src = fetchurl {
     url = http://nixos.org/releases/patchelf/patchelf-0.4pre11957-lngvn112/patchelf-0.4pre11957.tar.bz2;
-    sha256 = "39cb2a277bb9cb6fe216694b0db3b7e364e2ec0597e437e5cb0fd4378fd172f3";
+    sha256 = "3e9a72f17cfddcc0fbb6e2433aea7147d54c5c986c8cb3fce0dd985936efa7f3";
   };
 
   meta = {
