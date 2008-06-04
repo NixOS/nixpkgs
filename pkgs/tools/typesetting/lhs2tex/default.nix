b89a5bfa33cbe673212dfed871fbe755973174f2
Andres Löh <mail@andres-loeh.de>
2008-06-04 09:59:13 +0000

    lhs2tex version bump to 1.13, removed the external builder.sh
    svn path=/nixpkgs/trunk/; revision=11971

--- pkgs/tools/typesetting/lhs2tex/default.nix
+++ pkgs/tools/typesetting/lhs2tex/default.nix
@@ -3,17 +3,23 @@
 assert tetex == polytable.tetex;
 
 stdenv.mkDerivation {
-  name = "lhs2tex-1.13pre3";
-  builder = ./builder.sh;
+  name = "lhs2tex-1.13";
   
   src = fetchurl {
-    url = "http://www.cs.uu.nl/~andres/lhs2tex/lhs2tex-1.13pre3.tar.bz2";
-    sha256 = "8ddc9bd150c20c33518d747fee95577ec8f587146532cda12b8034adc847826c";
+    url = "http://people.cs.uu.nl/andres/lhs2tex/lhs2tex-1.13.tar.gz";
+    sha256 = "28282cb4afcc71785b092d358ffb33f5ec7585e50b392ae4fb6391d495a0836b";
   };
 
   buildInputs = [tetex ghc];
   propagatedBuildInputs = [polytable];
   propagatedUserEnvPackages = [polytable];
+
+  postInstall = ''
+    ensureDir "$out/share/doc/$name"
+    cp doc/Guide2.pdf $out/share/doc/$name
+    ensureDir "$out/nix-support"
+    echo "$propagatedUserEnvPackages" > $out/nix-support/propagated-user-env-packages
+  '';
 
   inherit tetex;
 }
