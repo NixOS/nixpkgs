2991f9adefa06bf59a0e84a04ab363a4a2154d52
Andres Löh <mail@andres-loeh.de>
2008-06-04 14:24:28 +0000

    * layout changes
    svn path=/nixpkgs/trunk/; revision=11981

--- pkgs/applications/networking/sync/unison/default.nix
+++ pkgs/applications/networking/sync/unison/default.nix
@@ -1,15 +1,18 @@
 {stdenv, fetchurl, ocaml, lablgtk, fontschumachermisc, xset, makeWrapper}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (rec {
+
   name = "unison-2.13.16";
   src = fetchurl {
-    url = http://www.seas.upenn.edu/~bcpierce/unison//download/releases/stable/unison-2.13.16.tar.gz;
+    url = "http://www.seas.upenn.edu/~bcpierce/unison//download/releases/stable/${name}.tar.gz";
     sha256 = "808400a933aeb67654edc770822cd186d1b2adc92e7cb5836996c71c69ffe656";
   };
 
   buildInputs = [ocaml makeWrapper];
 
-  preBuild = "sed -i \"s|\\(OCAMLOPT=.*\\)$|\\1 -I ${lablgtk}/lib/ocaml/lablgtk2|\" Makefile.OCaml";
+  preBuild = ''
+    sed -i "s|\(OCAMLOPT=.*\)$|\1 -I ${lablgtk}/lib/ocaml/lablgtk2|" Makefile.OCaml
+  '';
   makeFlags = "UISTYLE=gtk2 INSTALLDIR=$(out)/bin/";
   preInstall = "ensureDir $out/bin";
   postInstall = ''
@@ -18,4 +21,5 @@ stdenv.mkDerivation {
         --run "${xset}/bin/xset q | grep -q \"${fontschumachermisc}\" || ${xset}/bin/xset +fp \"${fontschumachermisc}/lib/X11/fonts/misc\""
     done
   '';
-}
+
+})
