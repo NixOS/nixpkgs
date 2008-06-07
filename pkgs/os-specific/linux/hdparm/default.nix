7478e316b7bd73ba27779023f960e548fba2322c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-06-07 12:20:19 +0000

    * hdparm 8.7.
    svn path=/nixpkgs/trunk/; revision=12007

--- pkgs/os-specific/linux/hdparm/default.nix
+++ pkgs/os-specific/linux/hdparm/default.nix
@@ -1,14 +1,19 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "hdparm-6.9";
+  name = "hdparm-8.7";
 
   src = fetchurl {
-    url = mirror://sourceforge/hdparm/hdparm-6.9.tar.gz;
-    sha256 = "01pyb9jmcv9nl1ig39s1i58fwgijqhjc5q1vxscbw0bd563fvrna";
+    url = mirror://sourceforge/hdparm/hdparm-8.7.tar.gz;
+    sha256 = "1gbyywsam769fdsgcy2clkfik9h2drad78h1xnh55b9c6fjlacmq";
   };
 
   preBuild = "
     makeFlagsArray=(sbindir=$out/sbin manprefix=$out)
   ";
+
+  meta = {
+    description = "A tool to get/set ATA/SATA drive parameters under Linux";
+    homepage = http://sourceforge.net/projects/hdparm/;
+  };
 }
