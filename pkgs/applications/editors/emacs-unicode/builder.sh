2813fb46ad7f435722346f379995a328b6ab1613
Andres Löh <mail@andres-loeh.de>
2008-06-09 14:21:36 +0000

    more pervasive crt-patching in emacs snapshot to make it work on my machine
    svn path=/nixpkgs/trunk/; revision=12021

--- pkgs/applications/editors/emacs-unicode/builder.sh
+++ pkgs/applications/editors/emacs-unicode/builder.sh
@@ -5,10 +5,12 @@ preConfigure() {
     libc=$(cat ${NIX_GCC}/nix-support/orig-libc)
     echo "libc: $libc"
 
-    substituteInPlace src/s/gnu-linux.h \
-        --replace /usr/lib/crt1.o $libc/lib/crt1.o \
-        --replace /usr/lib/crti.o $libc/lib/crti.o \
-        --replace /usr/lib/crtn.o $libc/lib/crtn.o
+    for i in src/s/*.h src/m/*.h; do
+        substituteInPlace $i \
+            --replace /usr/lib/crt1.o $libc/lib/crt1.o \
+            --replace /usr/lib/crti.o $libc/lib/crti.o \
+            --replace /usr/lib/crtn.o $libc/lib/crtn.o
+    done
     
     for i in Makefile.in ./src/Makefile.in ./lib-src/Makefile.in ./leim/Makefile.in ./admin/unidata/Makefile; do
         substituteInPlace $i --replace /bin/pwd pwd
