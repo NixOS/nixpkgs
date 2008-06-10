18099d1ad54e2f3dc43c1326e67c3f7e8ca2e4c4
Nicolas Pierron <nicolas.b.pierron@gmail.com>
2008-06-10 18:15:10 +0000

    Fix the url of libcaca.
    svn path=/nixpkgs/trunk/; revision=12033

--- pkgs/development/libraries/libcaca/default.nix
+++ pkgs/development/libraries/libcaca/default.nix
@@ -1,11 +1,18 @@
 {stdenv, fetchurl, ncurses}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "libcaca-0.99-beta13b";
   src = fetchurl {
-    url = http://libcaca.zoy.org/files/libcaca-0.99.beta13b.tar.gz;
+	  name = "${name}.tar.gz";
+    url = http://libcaca.zoy.org/attachment/wiki/libcaca/libcaca-0.99.beta13b.tar.gz?format=raw;
     sha256 = "0xy8pcnljnj5la97bzbwwyzyqa7dr3v9cyw8gdjzdfgqywvac1vg";
   };
   configureFlags = "--disable-x11 --disable-imlib2 --disable-doc";
   propagatedBuildInputs = [ncurses];
+
+	meta = {
+	  homepage = http://libcaca.zoy.org/;
+		description = "A graphics library that outputs text instead of pixels.";
+	  license = "WTFPL"; # http://sam.zoy.org/wtfpl/
+	};
 }
