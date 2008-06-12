bcc44390eb146f5bac5783dfcfb524ab023fb606
Michael Raskin <7c6f434c@mail.ru>
2008-06-12 10:08:10 +0000

    Added cheetah-template templating engine
    svn path=/nixpkgs/trunk/; revision=12054

---
+++ pkgs/tools/text/cheetah-template/2.0.1.nix
@@ -0,0 +1,18 @@
+args : with args; 
+rec {
+  src = fetchurl {
+    url = http://downloads.sourceforge.net/cheetahtemplate/Cheetah-2.0.1.tar.gz;
+    sha256 = "134k4s5f116k23vb7wf9bynlx3gf0wwl7y0zp9ciz0q66nh1idkh";
+  };
+
+  buildInputs = [python makeWrapper];
+  configureFlags = [];
+
+  /* doConfigure should be specified separately */
+  phaseNames = ["installPythonPackage" (makeManyWrappers ''$out/bin/*'' ''--prefix PYTHONPATH : $(toPythonPath $out)'')];
+      
+  name = "cheetah-template-" + version;
+  meta = {
+    description = "Templating engine";
+  };
+}
