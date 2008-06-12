179e15203a1faa1603193390ebae950fe3d183ea
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-06-12 14:04:06 +0000

    * x86_64 binaries.  Don't work yet though.
    svn path=/nixpkgs/trunk/; revision=12058

--- pkgs/os-specific/linux/nvidia/default.nix
+++ pkgs/os-specific/linux/nvidia/default.nix
@@ -8,12 +8,21 @@ in
 
 stdenv.mkDerivation {
   name = "nvidiaDrivers-${versionNumber}-${kernel.version}";
+  
   builder = ./builder.sh;
   
-  src = fetchurl {
-    url = "http://us.download.nvidia.com/XFree86/Linux-x86/${versionNumber}/NVIDIA-Linux-x86-${versionNumber}-pkg1.run";
-    sha256 = "14r3zddrppd0zxq76dd08dlj4qqncr7fj9cnrny4f0b5d0qgrd3f";
-  };
+  src =
+    if stdenv.system == "i686-linux" then
+      fetchurl {
+        url = "http://us.download.nvidia.com/XFree86/Linux-x86/${versionNumber}/NVIDIA-Linux-x86-${versionNumber}-pkg1.run";
+       sha256 = "14r3zddrppd0zxq76dd08dlj4qqncr7fj9cnrny4f0b5d0qgrd3f";
+      }
+    else if stdenv.system == "x86_64-linux" then
+      fetchurl {
+        url = "http://us.download.nvidia.com/XFree86/Linux-x86_64/${versionNumber}/NVIDIA-Linux-x86_64-${versionNumber}-pkg2.run";
+       sha256 = "0dmgn5a0432zkhgh0d5xs2h0sq5di2iaqybrj5f1vb2ghayams1y";
+      }
+    else throw "nvidia-x11 does not support platform ${stdenv.system}";
 
   #xenPatch = ./nvidia-2.6.24-xen.patch;
 
