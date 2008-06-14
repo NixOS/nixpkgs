80b2254cbb90bfb93dbe1bb5a58866db68e17ae4
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-06-14 20:46:34 +0000

    * SDL 1.2.13.
    svn path=/nixpkgs/trunk/; revision=12092

--- pkgs/development/libraries/SDL/default.nix
+++ pkgs/development/libraries/SDL/default.nix
@@ -6,20 +6,28 @@ assert openglSupport -> mesa != null;
 assert alsaSupport -> alsaLib != null;
 
 stdenv.mkDerivation {
-  name = "SDL-1.2.11";
+  name = "SDL-1.2.13";
+  
   src = fetchurl {
-    url = http://www.libsdl.org/release/SDL-1.2.11.tar.gz;
-    md5 = "418b42956b7cd103bfab1b9077ccc149";
+    url = http://www.libsdl.org/release/SDL-1.2.13.tar.gz;
+    sha256 = "0cp155296d6fy3w31jj481jxl9b43fkm01klyibnna8gsvqrvycl";
   };
+  
   propagatedBuildInputs = [x11 libXrandr];
-  buildInputs = 
-    (if openglSupport then [mesa] else []) ++
-    (if alsaSupport then [alsaLib] else []);
-  configureFlags = "
+  
+  buildInputs =
+    stdenv.lib.optional openglSupport mesa ++
+    stdenv.lib.optional alsaSupport alsaLib;
+    
+  configureFlags = ''
     --disable-x11-shared --disable-alsa-shared --enable-rpath
     ${if alsaSupport then "--with-alsa-prefix=${alsaLib}/lib" else ""}
-  ";
-#  patches = [./no-cxx.patch];
-#  NIX_CFLAGS_COMPILE = "-DBITS_PER_LONG=32"; /* !!! hack around kernel header bug */
-  inherit openglSupport;
+  '';
+
+  passthru = {inherit openglSupport;};
+
+  meta = {
+    description = "A cross-platform multimedia library";
+    homepage = http://www.libsdl.org/;
+  };
 }
