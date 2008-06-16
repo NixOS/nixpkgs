4577779af651c564221c5a732f5e85795328f459
Michael Raskin <7c6f434c@mail.ru>
2008-06-16 09:09:58 +0000

    Fixed Inkscape build
    svn path=/nixpkgs/trunk/; revision=12114

--- pkgs/applications/graphics/inkscape/default.nix
+++ pkgs/applications/graphics/inkscape/default.nix
@@ -1,15 +1,16 @@
 args: with args;
 
 stdenv.mkDerivation rec {
-  name = "inkscape-0.45.1";
+  name = "inkscape-0.46";
 
   src = fetchurl {
     url = "mirror://sf/inkscape/${name}.tar.gz";
-    sha256 = "1y0b9bm8chn6a2ip99dj4dhg0188yn67v571ha0x38wrlmvn4k0d";
+    sha256 = "0flrjqa68vnnn8lrhj86xpa6h2cyzrvjy6873v9id092f86ix1li";
   };
 
   # Work around Python misdetection and set `PYTHON_LIBS' to
   # "-L/nix/store/... -lpython2.4" instead of "/nix/store/.../libpython2.4.so".
+  # Changed to 2.5
   patches = [ ./configure-python-libs.patch ];
 
   propagatedBuildInputs = [
@@ -26,13 +27,16 @@ stdenv.mkDerivation rec {
 
   configureFlags = "--with-python";
 
+  preBuild = ''
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I./extension/script"
+  '';
+
   postInstall = ''
     # Make sure PyXML modules can be found at run-time.
     for i in "$out/bin/"*
     do
-      # FIXME: We're assuming Python 2.4.
       wrapProgram "$i" --prefix PYTHONPATH :      \
-       "${pyxml}/lib/python2.4/site-packages" ||  \
+       "$(toPythonPath ${pyxml})" ||  \
         exit 2
     done
   '';
