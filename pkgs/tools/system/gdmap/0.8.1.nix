c888ed7499ddcac51cc47085fe5f7137fa86a425
Michael Raskin <7c6f434c@mail.ru>
2008-06-16 11:11:56 +0000

    Added updated gdmap
    svn path=/nixpkgs/trunk/; revision=12115

---
+++ pkgs/tools/system/gdmap/0.8.1.nix
@@ -0,0 +1,23 @@
+args : with args;
+	let localDefs = builderDefs {
+		src = /* put a fetchurl here */
+		fetchurl {
+		    url = http://downloads.sourceforge.net/gdmap/gdmap-0.8.1.tar.gz;
+		    sha256 = "0nr8l88cg19zj585hczj8v73yh21k7j13xivhlzl8jdk0j0cj052";
+		};
+
+		buildInputs = [gtk pkgconfig libxml2 intltool gettext];
+		configureFlags = [];
+	} null; /* null is a terminator for sumArgs */
+	in with localDefs;
+stdenv.mkDerivation rec {
+	name = "gdmap"+version;
+	builder = writeScript (name + "-builder")
+		(textClosure localDefs [doConfigure doMakeInstall doForceShare doPropagate]);
+	meta = {
+		description = "
+	Recursive rectangle map of disk usage.	
+";
+		inherit src;
+	};
+}
