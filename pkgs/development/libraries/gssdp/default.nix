7ab5540a4a58dacea8f30b5256181e8dedf63205
Armijn Hemel <armijn@gpl-violations.org>
2008-06-18 19:03:58 +0000

    add gssdp and gupnp, some Universal Plug and Play thingies
    svn path=/nixpkgs/trunk/; revision=12149

--- pkgs/development/libraries/gssdp/default.nix
+++ pkgs/development/libraries/gssdp/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, pkgconfig, libsoup, glib, libxml2}:
 
 stdenv.mkDerivation {
-  name = "gssdp-0.4.2";
+  name = "gssdp-0.6.1";
   src = fetchurl {
-    url = http://www.gupnp.org/sources/gssdp/gssdp-0.4.2.tar.gz;
-    sha256 = "0d4h494qkls3hl4cc8pjhlv34nwysa9bf7xsffbd59r4dxbqziwy";
+    url = http://www.gupnp.org/sources/gssdp/gssdp-0.6.1.tar.gz;
+    sha256 = "1mla3s0p4vabrn4m7il02f1d1ily3712fjw4k9l3x89rqyi2qh7f";
   };
 
   buildInputs = [pkgconfig libsoup glib libxml2];
---
+++ pkgs/development/libraries/gupnp/default.nix
@@ -0,0 +1,17 @@
+args: with args;
+ 
+stdenv.mkDerivation {
+  name = "gupnp-0.12";
+
+  src = fetchurl {
+    url = http://www.gupnp.org/sources/gupnp/gupnp-0.12.tar.gz;
+    sha256 = "1sm1rqvx752nb3j1yl7h30kx2ymndkji8m73fxshjssmc6z40ayg";
+  };
+
+  buildInputs = [
+    pkgconfig glib libxml2 gssdp libsoup e2fsprogs
+ #dbus.libs dbus_glib gtk glib
+  ];
+
+  #configureFlags="";
+}
