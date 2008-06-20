1ec810f84453533db989688933c69d9692fd96f4
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-06-20 16:09:48 +0000

    * Clean up indentation.
    svn path=/nixos/trunk/; revision=12169

--- upstart-jobs/udev.nix
+++ upstart-jobs/udev.nix
@@ -51,45 +51,45 @@ in
 {
   name = "udev";
   
-  job = "
-start on startup
-stop on shutdown
+  job = ''
+    start on startup
+    stop on shutdown
 
-env UDEV_CONFIG_FILE=${conf}
+    env UDEV_CONFIG_FILE=${conf}
 
-start script
-    echo '' > /proc/sys/kernel/hotplug
+    start script
+	echo "" > /proc/sys/kernel/hotplug
 
-    # Get rid of possible old udev processes.
-    ${procps}/bin/pkill -u root '^udevd$' || true
+	# Get rid of possible old udev processes.
+	${procps}/bin/pkill -u root "^udevd$" || true
 
-    # Start udev.
-    ${udev}/sbin/udevd --daemon
+	# Start udev.
+	${udev}/sbin/udevd --daemon
 
-    # Let udev create device nodes for all modules that have already
-    # been loaded into the kernel (or for which support is built into
-    # the kernel).
-    if ! test -e ${devicesCreated}; then
-        ${udev}/sbin/udevtrigger
-        ${udev}/sbin/udevsettle # wait for udev to finish
-        touch ${devicesCreated}
-    fi
+	# Let udev create device nodes for all modules that have already
+	# been loaded into the kernel (or for which support is built into
+	# the kernel).
+	if ! test -e ${devicesCreated}; then
+	    ${udev}/sbin/udevtrigger
+	    ${udev}/sbin/udevsettle # wait for udev to finish
+	    touch ${devicesCreated}
+	fi
 
-    # Kill udev, let Upstart restart and monitor it.  (This is nasty,
-    # but we have to run udevtrigger first.  Maybe we can use
-    # Upstart's `binary' keyword, but it isn't implemented yet.)
-    if ! ${procps}/bin/pkill -u root '^udevd$'; then
-        echo \"couldn't stop udevd\"
-    fi
+	# Kill udev, let Upstart restart and monitor it.  (This is nasty,
+	# but we have to run udevtrigger first.  Maybe we can use
+	# Upstart's `binary' keyword, but it isn't implemented yet.)
+	if ! ${procps}/bin/pkill -u root "^udevd$"; then
+	    echo "couldn't stop udevd"
+	fi
 
-    while ${procps}/bin/pgrep -u root '^udevd$'; do
-        sleep 1
-    done
+	while ${procps}/bin/pgrep -u root "^udevd$"; do
+	    sleep 1
+	done
 
-    initctl emit new-devices
-end script
+	initctl emit new-devices
+    end script
 
-respawn ${udev}/sbin/udevd
-  ";
+    respawn ${udev}/sbin/udevd
+  '';
 
 }
