de4901c4feacaa478e6c1309b55dd8f13a6806a3
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-06-25 13:08:09 +0000

    * lsof 4.80.
    svn path=/nixpkgs/trunk/; revision=12186

--- pkgs/development/tools/misc/lsof/default.nix
+++ pkgs/development/tools/misc/lsof/default.nix
@@ -1,20 +1,35 @@
-
 {stdenv, fetchurl}:
+
 stdenv.mkDerivation {
-  name = "lsof-4.78";
+  name = "lsof-4.80";
 
   src = fetchurl {
-    url =ftp://sunsite.ualberta.ca/pub/Mirror/lsof/lsof_4.78.tar.bz2;
-    sha256 = "0azvl43niqkq94drx52p6dvp70r38f25fqw181ywmvqn80dbb3c9";
+    urls = [
+      ftp://lsof.itap.purdue.edu/pub/tools/unix/lsof/lsof_4.80.tar.bz2
+      ftp://lsof.itap.purdue.edu/pub/tools/unix/lsof/OLD/lsof_4.80.tar.bz2
+    ];
+    sha256 = "1q0k3c9ajpdxkhqq793pl4fdfnrwl5hgwk9556gvcj96hllssgbr";
   };
 
   unpackPhase = "tar xvjf $src; cd lsof_*; tar xvf lsof_*.tar; sourceRoot=$( echo lsof_*/); ";
+  
   preBuild = "sed -i Makefile -e 's/^CFGF=/&	-DHASIPv6=1/;';";
+  
   configurePhase = "./Configure -n linux;";
-  installPhase = " mkdir -p $out/bin $out/man/man8; cp lsof.8 $out/man/man8/; cp lsof $out/bin";
+  
+  installPhase = ''
+    ensureDir $out/bin $out/man/man8
+    cp lsof.8 $out/man/man8/
+    cp lsof $out/bin
+  '';
 
   meta = {
-    description = "List open files. Can show what process has opened some file, 
-socket (IPv6/IPv4/UNIX local), or partition (by opening a file from it).";
+    homepage = ftp://lsof.itap.purdue.edu/pub/tools/unix/lsof/;
+    description = "A tool to list open files";
+    longDescription = ''
+      List open files. Can show what process has opened some file,
+      socket (IPv6/IPv4/UNIX local), or partition (by opening a file
+      from it).
+    '';
   };
 }
