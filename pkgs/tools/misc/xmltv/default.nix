2e8df9e7226626ae9b9449ee9e2036151cdec23d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-06-26 08:43:14 +0000

    svn path=/nixpkgs/trunk/; revision=12193

--- pkgs/tools/misc/xmltv/default.nix
+++ pkgs/tools/misc/xmltv/default.nix
@@ -9,7 +9,7 @@ import ../../../development/perl-modules/generic perl {
     url = mirror://sourceforge/xmltv/xmltv-0.5.51.tar.bz2;
     sha256 = "0vgc167y6y847m18vg3qwjy3df12bryjy9par01n5b9mjalx9jpd";
   };
-  makeMakerFlags = "-components tv_grab_nl";
+  #makeMakerFlags = "-components tv_grab_nl";
   buildInputs = [
     perlTermReadKey perlXMLTwig perlXMLWriter perlDateManip
     perlHTMLTree perlHTMLParser perlHTMLTagset perlURI perlLWP
