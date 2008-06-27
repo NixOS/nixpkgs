f960020e955285964bb7e5af7c7dc7d74045b47b
Ludovic Courtès <ludo@gnu.org>
2008-06-27 18:18:38 +0000

    GnuTLS 2.4.0 (new stable series).
    svn path=/nixpkgs/trunk/; revision=12202

--- pkgs/development/libraries/gnutls/default.nix
+++ pkgs/development/libraries/gnutls/default.nix
@@ -5,11 +5,11 @@ assert guileBindings -> guile != null;
 
 stdenv.mkDerivation rec {
 
-  name = "gnutls-2.2.5";
+  name = "gnutls-2.4.0";
 
   src = fetchurl {
     url = "mirror://gnu/gnutls/${name}.tar.bz2";
-    sha256 = "0mxf4pwv17lf4c2b3bh70wn35y9f45325am1ywkmw2azbfyqj9ng";
+    sha256 = "11sdwj994lbd8n5icxdj2xr10a0b7s4nh4bm2pf8phncy6kbr0n6";
   };
 
   patches = [ ./tmpdir.patch ];
@@ -25,6 +25,21 @@ stdenv.mkDerivation rec {
   
   meta = {
     description = "The GNU Transport Layer Security Library";
+
+    longDescription = ''
+       GnuTLS is a project that aims to develop a library which
+       provides a secure layer, over a reliable transport
+       layer. Currently the GnuTLS library implements the proposed
+       standards by the IETF's TLS working group.
+
+       Quoting from the TLS protocol specification:
+
+       "The TLS protocol provides communications privacy over the
+       Internet. The protocol allows client/server applications to
+       communicate in a way that is designed to prevent eavesdropping,
+       tampering, or message forgery."
+    '';
+
     homepage = http://www.gnu.org/software/gnutls/;
     license = "LGPLv2.1+";
   };
