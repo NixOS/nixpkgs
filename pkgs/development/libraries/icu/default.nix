c8f3bf4e2487f152e9708e5d9bbe62841239b3c2
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-07-04 05:53:41 +0000

    * Export a symbol that OpenOffice needs.
    svn path=/nixpkgs/trunk/; revision=12254

--- pkgs/development/libraries/icu/default.nix
+++ pkgs/development/libraries/icu/default.nix
@@ -2,13 +2,25 @@
 
 stdenv.mkDerivation {
   name = "icu4c-3.6";
+  
   src = fetchurl {
     url = ftp://ftp.software.ibm.com/software/globalization/icu/3.6/icu4c-3_6-src.tgz;
     sha256 = "0hdh8sbpmabijprdpn7rmsqilw97f3paxxsxa4hd61k2kpbfhdai";
   };
-  postUnpack="
+
+  patchFlags = "-p0";
+
+  patches = [
+    (fetchurl {
+      url = "http://sources.gentoo.org/viewcvs.py/*checkout*/gentoo-x86/dev-libs/icu/files/icu-3.8-setBreakType-public.diff?rev=1.1";
+      sha256 = "09g39rzj3bdf2q9n47rzdlpcjyipip42swbjpb0gjzp439jv3wmk";
+    })
+  ];
+  
+  postUnpack = "
     sourceRoot=\${sourceRoot}/source
     echo Source root reset to \${sourceRoot}
   ";
-  configureFlags="--disable-debug";
+  
+  configureFlags = "--disable-debug";
 }
