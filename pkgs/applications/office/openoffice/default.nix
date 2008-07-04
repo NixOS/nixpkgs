c37db61834d3b82841a11339df73a722db10fc06
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-07-04 05:54:04 +0000

    * OpenOffice 2.3.1.
    svn path=/nixpkgs/trunk/; revision=12255

--- pkgs/applications/office/openoffice/default.nix
+++ pkgs/applications/office/openoffice/default.nix
@@ -7,7 +7,7 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "openoffice.org-2.3.0";
+  name = "openoffice.org-2.3.1";
   builder = ./builder.sh;
 
   src =
@@ -21,9 +21,9 @@ stdenv.mkDerivation rec {
         #sha256 = "";
     #} else 
     fetchurl {
-        url = http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=src_bzip&lang=core&version=2.3.0;
-        name = "OOo_2.3.0_src_core.tar.bz2";
-        sha256 = "0mkxn9qj3f03rjkmxc4937gr2w429hnxzb9j5j2grdknfga5a1c3";
+      url = http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=src_bzip&lang=core&version=2.3.1;
+      name = "OOo_2.3.1_src_core.tar.bz2";
+      md5 = "9555a2d5fae9a25c788cc732921ef57a";
     };
 
   configureFlags = "
@@ -73,14 +73,7 @@ stdenv.mkDerivation rec {
 
   inherit icu fontconfig libjpeg jdk cups;
 
-  # libawt_problem see http://www.openoffice.org/issues/show_bug.cgi?id=74751 
-  # Can be removed in newer releases than 2.3.0
-  patch_file = ./libawt_problem;
   patches = [./ooo-libtextcat.patch ];
-  patchPhase = " 
-  patch config_office/set_soenv.in \$patch_file
-  unset patchPhase; patchPhase;
-  ";
 
   meta = {
     description = "OpenOffice.org is a multiplatform and multilingual office suite";
