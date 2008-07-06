70b50b9f52d1e628e051f4f688850ba40e6f4bd8
Wouter den Breejen <uu@denbreejen.net>
2008-07-06 18:32:42 +0000

    Another version bump (because of another 404)
    svn path=/nixpkgs/trunk/; revision=12281

--- pkgs/servers/squid/3.head.nix
+++ pkgs/servers/squid/3.head.nix
@@ -9,8 +9,8 @@ args:
       name = "squid-3.0-stable5";
 
       src = args.fetchurl {
-        url = http://www.squid-cache.org/Versions/v3/HEAD/squid-3.HEAD-20080424.tar.bz2;
-        sha256 = "0mqjq8112rjgn3nbpkg8iql32cnk5kiw8fmhj5gbqzbycbhaxjgz";
+	url = http://www.squid-cache.org/Versions/v3/HEAD/squid-3.HEAD-20080706.tar.bz2;
+        sha256 = "20f5994dd2189faa07edaef2c529f385e4590266d283745027b35d140eefe1ab";
       };
 
       configureFlags = ["--enable-ipv6"];
