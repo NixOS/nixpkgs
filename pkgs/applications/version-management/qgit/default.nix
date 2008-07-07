821cb7ccdc3fe0dea5b74e449ea083383d4f9884
Peter Simons <simons@cryp.to>
2008-07-07 11:54:45 +0000

    qgit: download this package from sourceforge
    I hope the mirror syntax is right.
    
    svn path=/nixpkgs/trunk/; revision=12306

--- pkgs/applications/version-management/qgit/default.nix
+++ pkgs/applications/version-management/qgit/default.nix
@@ -5,12 +5,12 @@ stdenv.mkDerivation rec {
   meta =
   {
     license = "GPLv2";
-    homepage = http://git.or.cz;
+    homepage = "http://digilander.libero.it/mcostalba/";
     description = "Graphical front-end to Git";
   };
   src = fetchurl
   {
-    url = "http://todo/${name}.tar.bz2";
+    url = "mirror://sourceforge/qgit/${name}.tar.bz2";
     sha256 = "0qmgd1cjny5aciljpvn2bczgdvlpgd2f7wzafda24zj4mzqnppsq";
   };
   buildInputs = [qt libXext libX11];
