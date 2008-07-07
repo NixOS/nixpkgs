4793092374fa9615bf3b072e4028e7092f9e8465
Ludovic Courtès <ludo@gnu.org>
2008-07-07 22:02:36 +0000

    GNU Make: Make `nix-log2xml' support optional.
    One question remains: how to make the option available to the user.
    `getConfig' is nice but useless as long as `useFromStdenv' prevails.
    
    svn path=/nixpkgs/trunk/; revision=12311

--- pkgs/development/tools/build-managers/gnumake/default.nix
+++ pkgs/development/tools/build-managers/gnumake/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl}:
+{stdenv, fetchurl, log2xmlSupport ? true}:
 
 stdenv.mkDerivation {
   name = "gnumake-3.81";
@@ -7,15 +7,18 @@ stdenv.mkDerivation {
     md5 = "354853e0b2da90c527e35aabb8d6f1e6";
   };
   
-  patches = [
-    # Provide nested log output for subsequent pretty-printing by
-    # nix-log2xml.
-    ./log.patch
-
-    # Purity: don't look for library dependencies (of the form
-    # `-lfoo') in /lib and /usr/lib.  It's a stupid feature anyway.
-    # Likewise, when searching for included Makefiles, don't look in
-    # /usr/include and friends.
-    ./impure-dirs.patch
-  ];
+  patches =
+    (if log2xmlSupport
+        # Provide nested log output for subsequent pretty-printing by
+        # nix-log2xml.
+        then [ ./log.patch ]
+	else [])
+    ++
+    [
+      # Purity: don't look for library dependencies (of the form
+      # `-lfoo') in /lib and /usr/lib.  It's a stupid feature anyway.
+      # Likewise, when searching for included Makefiles, don't look in
+      # /usr/include and friends.
+      ./impure-dirs.patch
+    ];
 }
