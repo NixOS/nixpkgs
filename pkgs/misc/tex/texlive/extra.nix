5245bad31a03af6dcfdb2be1a42fed629419ecb3
Michael Raskin <7c6f434c@mail.ru>
2008-07-07 05:53:21 +0000

    Updated texlive-extra source
    svn path=/nixpkgs/trunk/; revision=12293

--- pkgs/misc/tex/texlive/extra.nix
+++ pkgs/misc/tex/texlive/extra.nix
@@ -2,8 +2,8 @@ args: with args;
 rec {
   name = "texlive-extra-2007";
   src = fetchurl {
-    url = mirror://debian/pool/main/t/texlive-extra/texlive-extra_2007.dfsg.1.orig.tar.gz;
-    sha256 = "1440495dcsrwhnz1p1prs4rf84ca0v7fjwg7sdw7isnprnpiq7w5";
+    url = mirror://debian/pool/main/t/texlive-extra/texlive-extra_2007.dfsg.2.orig.tar.gz;
+    sha256 = "1sdhidjafv5cls7r9g60qq3cw655kw91ms4may39pcm8wdbhqs77";
   };
 
   buildInputs = [texLive];
