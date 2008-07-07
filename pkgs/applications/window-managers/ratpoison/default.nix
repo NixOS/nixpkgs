d94a0f3064f0774a702dbe3a6d8cfc57a14ef7b8
Peter Simons <simons@cryp.to>
2008-07-07 11:47:05 +0000

    ratpoison: updated to version 1.4.3.
    svn path=/nixpkgs/trunk/; revision=12297

--- pkgs/applications/window-managers/ratpoison/default.nix
+++ pkgs/applications/window-managers/ratpoison/default.nix
@@ -1,14 +1,14 @@
 {stdenv, fetchurl, libX11, inputproto, libXt, libXpm, libXft, fontconfig,
 	libXtst, xextproto, readline}:
 stdenv.mkDerivation {
-  name = "RatPoison-1.4.1";
+  name = "ratpoison-1.4.3";
 
   src = fetchurl {
-    url = http://download.savannah.gnu.org/releases/ratpoison/ratpoison-1.4.1.tar.gz;
-    md5 = "fcbdcc84cfad9b18518074f676eba270";
+    url = http://download.savannah.gnu.org/releases/ratpoison/ratpoison-1.4.3.tar.gz;
+    sha256 = "15y3hi4dc7f98mhhpms22ahmh8lbzhyqli878z3fgrix4z7vr4fz";
   };
 
   buildInputs = [libX11 inputproto libXt
-	 libXpm libXft fontconfig libXtst 
+	 libXpm libXft fontconfig libXtst
 	xextproto readline];
 }
