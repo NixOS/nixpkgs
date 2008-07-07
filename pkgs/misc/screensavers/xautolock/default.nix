8ff43a16363a59aad8ca3e5e33c4a95a8cfaf020
Peter Simons <simons@cryp.to>
2008-07-07 23:11:43 +0000

    xautolock: updated to version 2.2
    svn path=/nixpkgs/trunk/; revision=12314

--- pkgs/misc/screensavers/xautolock/default.nix
+++ pkgs/misc/screensavers/xautolock/default.nix
@@ -2,14 +2,13 @@
 
 stdenv.mkDerivation rec
 {
-  name = "xautolock-2.1";
+  name = "xautolock-2.2";
   src = fetchurl
   {
-    url = "http://www.ibiblio.org.org/pub/Linux/X11/screensavers/${name}.tgz";
-    sha256 = "1ylc6589ck88jmp8bxccs0xay1nyrnlw6cajzihk5m0pkfwwk92b";
+    url = "http://www.ibiblio.org/pub/Linux/X11/screensavers/${name}.tgz";
+    sha256 = "11f0275175634e6db756e96f5713ec91b8b1c41f8663df54e8a5d27dc71c4da2";
   };
-
-  makeFlags="BINDIR=$$out/bin MANPATH=$$out/man";
+  makeFlags="BINDIR=\${out}/bin MANPATH=\${out}/man";
   preBuild = "xmkmf";
   installTargets = "install install.man";
   buildInputs = [x11 imake libXScrnSaver scrnsaverproto];
