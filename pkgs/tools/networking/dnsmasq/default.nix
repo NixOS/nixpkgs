5f044d6f3fd89b0fb5f194bc6196cde76c2db4af
Marc Weber <marco-oweber@gmx.de>
2008-07-07 11:40:16 +0000

    fix: dnsmasq can now be started after installing.. What has gone wrong by my first commit attempt?
    svn path=/nixpkgs/trunk/; revision=12294

--- pkgs/tools/networking/dnsmasq/default.nix
+++ pkgs/tools/networking/dnsmasq/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
     sha256 = "1q346l403rvvmvr14fk2l201p8fl3p5417vkp95zlx00jdb7hl8n";
   };
 
-  installPhase = "ensureDir \$out; make DESTDIR=\$out PREFIX=ôônstall"; 
+  installPhase = "ensureDir \$out/bin; make DESTDIR= BINDIR=\$out/bin MANDIR=\$out/man LOCALEDIR=\$out/share/locale install"; 
 
   meta = { 
       description = "DNS forwarder and DHCP server";
