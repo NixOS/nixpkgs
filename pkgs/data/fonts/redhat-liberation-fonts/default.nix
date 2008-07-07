4650c3ddc57b432c90af5a9299a36f0973d292ec
Wouter den Breejen <uu@denbreejen.net>
2008-07-07 19:02:57 +0000

    Redhat n00bs calls their source xxx.tar.gz but it is not gzipped . .  the generic builder can also not figure this out . . .
    svn path=/nixpkgs/trunk/; revision=12310

--- pkgs/data/fonts/redhat-liberation-fonts/default.nix
+++ pkgs/data/fonts/redhat-liberation-fonts/default.nix
@@ -3,9 +3,11 @@
 stdenv.mkDerivation {
   name = "redhat-liberation";
   src = fetchurl {
-    url = https://www.redhat.com/f/fonts/liberation-fonts.tar.gz;
+    url = http://www.redhat.com/f/fonts/liberation-fonts.tar.gz;
     sha256 = "5749c27f3deb5da323961e86efed1306fc072bea9065790d0047ad61471be6a5";
   };
+  
+  unpackPhase = ''tar -xvf "$src" && sourceRoot="$PWD/liberation-fonts"'';
   installPhase = ''
     ensureDir $out/share/fonts/truetype
     cp *.ttf $out/share/fonts/truetype
