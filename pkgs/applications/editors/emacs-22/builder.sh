9cb86e27bbab02cd1460db92f8be6d114087fdef
Michael Raskin <7c6f434c@mail.ru>
2008-07-09 13:03:25 +0000

    Trying to fix x86_64 build..
    svn path=/nixpkgs/trunk/; revision=12328

--- pkgs/applications/editors/emacs-22/builder.sh
+++ pkgs/applications/editors/emacs-22/builder.sh
@@ -14,7 +14,10 @@ preConfigure() {
         substituteInPlace $i \
             --replace /usr/${glibclibdir}/crt1.o $libc/${glibclibdir}/crt1.o \
             --replace /usr/${glibclibdir}/crti.o $libc/${glibclibdir}/crti.o \
-            --replace /usr/${glibclibdir}/crtn.o $libc/${glibclibdir}/crtn.o
+            --replace /usr/${glibclibdir}/crtn.o $libc/${glibclibdir}/crtn.o \
+            --replace /usr/lib/crt1.o $libc/${glibclibdir}/crt1.o \
+            --replace /usr/lib/crti.o $libc/${glibclibdir}/crti.o \
+            --replace /usr/lib/crtn.o $libc/${glibclibdir}/crtn.o
     done
 
     for i in Makefile.in ./src/Makefile.in ./lib-src/Makefile.in ./leim/Makefile.in; do
