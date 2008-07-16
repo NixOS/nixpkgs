a0f8f11e87681b83e8059aa3fe068c6344930b2e
Armijn Hemel <armijn@gpl-violations.org>
2008-07-16 23:02:53 +0000

    update to 2.6.4
    svn path=/nixpkgs/trunk/; revision=12368

--- pkgs/applications/office/abiword/default.nix
+++ pkgs/applications/office/abiword/default.nix
@@ -4,10 +4,10 @@ enchant, wv
 }:
 
 stdenv.mkDerivation {
-  name = "abiword-2.6.3";
+  name = "abiword-2.6.4";
   src = fetchurl {
-    url = http://www.abisource.com/downloads/abiword/2.6.3/source/abiword-2.6.3.tar.gz;
-    sha256 = "1v4hvnlf5x9q4w3w4yvv712hsajmhv8dpndbni623ag24g2frzz5";
+    url = http://www.abisource.com/downloads/abiword/2.6.4/source/abiword-2.6.4.tar.gz;
+    sha256 = "1zp9p2dfrskn7r827ivvii2477ysxkvrsshk79hgw3xhd5mplbad";
   };
 
   buildInputs = [pkgconfig gtk libglade libgnomeprint libgnomeprintui
