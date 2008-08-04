d0aa54c8fa4686517d4de5a16536b0ada06d203c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-08-04 18:00:49 +0000

    * Pan 0.133.
    svn path=/nixpkgs/trunk/; revision=12496

--- pkgs/applications/networking/newsreaders/pan/default.nix
+++ pkgs/applications/networking/newsreaders/pan/default.nix
@@ -6,20 +6,12 @@
 assert spellChecking -> gtkspell != null;
 
 stdenv.mkDerivation {
-  name = "pan-0.132";
+  name = "pan-0.133";
 
   src = fetchurl {
-    url = http://pan.rebelbase.com/download/releases/0.132/source/pan-0.132.tar.bz2;
-    sha1 = "dc0bf586145b9d617039f49101874fbc76a9dc18";
+    url = http://pan.rebelbase.com/download/releases/0.133/source/pan-0.133.tar.bz2;
+    sha1 = "a0bd98ea1ba174800896611e3305a6b6d8dbde2f";
   };
-
-  patches = [
-    # Patch to build Pan on recent glibs.
-    (fetchurl {
-      url = "http://sources.gentoo.org/viewcvs.py/*checkout*/gentoo-x86/net-nntp/pan/files/pan-0.132-glib-compat.patch?rev=1.1";
-      sha256 = "0bmgnc3l1pz763xns8057qbsjirc0njr8dn34fhy4ig9yk4kp9k7";
-    })
-  ];
 
   buildInputs = [pkgconfig gtk perl pcre gmime gettext]
     ++ stdenv.lib.optional spellChecking gtkspell;
