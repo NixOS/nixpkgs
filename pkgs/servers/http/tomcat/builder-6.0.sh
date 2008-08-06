63b9500b882d06fbaa9d31a72e0c9117c3e55f63
Sander van der Burg <s.vanderburg@tudelft.nl>
2008-08-06 14:59:45 +0000

    Added support for custom context.xml for Tomcat
    svn path=/nixpkgs/trunk/; revision=12512

--- pkgs/servers/http/tomcat/builder-6.0.sh
+++ pkgs/servers/http/tomcat/builder-6.0.sh
@@ -4,5 +4,10 @@ tar zxf $src
 
 mkdir $out
 mv apache-tomcat*/* $out
+
+# Rename the context.xml to context.xml.default in order to allow a custom context.xml
+mv $out/conf/context.xml $out/conf/context.xml.default
+
+# Change all references from CATALINA_HOME to CATALINA_BASE and add support for shared libraries
 sed -i -e 's|${catalina.home}|${catalina.base}|g' \
        -e 's|shared.loader=|shared.loader=${catalina.base}/shared/lib/*.jar|' $out/conf/catalina.properties
