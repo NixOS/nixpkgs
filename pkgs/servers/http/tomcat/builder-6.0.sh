c282368077c2f447f654c1113c3b90cb5185c1c7
Sander van der Burg <s.vanderburg@tudelft.nl>
2008-08-06 13:25:51 +0000

    Some tomcat common lib fixes
    svn path=/nixpkgs/trunk/; revision=12510

--- pkgs/servers/http/tomcat/builder-6.0.sh
+++ pkgs/servers/http/tomcat/builder-6.0.sh
@@ -4,4 +4,5 @@ tar zxf $src
 
 mkdir $out
 mv apache-tomcat*/* $out
-sed -i -e 's|shared.loader=|shared.loader=${catalina.base}/shared/lib/*.jar|' $out/conf/catalina.properties
+sed -i -e 's|${catalina.home}|${catalina.base}|g' \
+       -e 's|shared.loader=|shared.loader=${catalina.base}/shared/lib/*.jar|' $out/conf/catalina.properties
--- pkgs/servers/http/tomcat/jdbc/mysql/builder.sh
+++ pkgs/servers/http/tomcat/jdbc/mysql/builder.sh
@@ -1,4 +1,4 @@
 source $stdenv/setup
 
-ensureDir $out/shared/lib
-ln -s $mysql_jdbc/share/java/mysql-connector-java.jar $out/shared/lib/mysql-connector-java.jar
+ensureDir $out/lib
+ln -s $mysql_jdbc/share/java/mysql-connector-java.jar $out/lib/mysql-connector-java.jar
