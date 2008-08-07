6e8d8a16142f5077956c2a18b943aba2a43cf625
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-08-07 11:15:11 +0000

    * sysstat 8.0.4.1.
    svn path=/nixpkgs/trunk/; revision=12533

--- pkgs/os-specific/linux/sysstat/default.nix
+++ pkgs/os-specific/linux/sysstat/default.nix
@@ -1,18 +1,18 @@
 {stdenv, fetchurl, gettext}:
    
 stdenv.mkDerivation {
-  name = "sysstat-8.0.0";
+  name = "sysstat-8.0.4.1";
    
   src = fetchurl {
-    url = http://perso.orange.fr/sebastien.godard/sysstat-8.0.0.tar.bz2;
-    md5 = "cb579d5c5d5bc1386cc09193e15765bf";
+    url = http://perso.orange.fr/sebastien.godard/sysstat-8.0.4.1.tar.bz2;
+    sha256 = "17bzyz6bp63br4pns40ypc0qac0299lh90p7fhis5sn31sx811rf";
   };
 
   buildInputs = [gettext];
 
-  preConfigure = "
+  preConfigure = ''
     export PATH_CP=$(type -tp cp)
     export PATH_CHKCONFIG=/no-such-program
     makeFlagsArray=(SA_DIR=$out/var/log/sa SYSCONFIG_DIR=$out/etc CHOWN=true IGNORE_MAN_GROUP=y)
-  ";
+  '';
 }
