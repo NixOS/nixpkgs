0589d477603d702885c461fb531439e6fa2c3116
Marc Weber <marco-oweber@gmx.de>
2008-08-10 12:41:53 +0000

    added compatibility patch making reiserfsck ignore -v flag
    svn path=/nixpkgs/trunk/; revision=12571

--- pkgs/os-specific/linux/reiserfsprogs/default.nix
+++ pkgs/os-specific/linux/reiserfsprogs/default.nix
@@ -1,14 +1,14 @@
 args: with args;
 
 stdenv.mkDerivation {
-  name = "reiserfsprogs-3.6.19";
+  name = "reiserfsprogs-3.6.19-patched";
 
   src = fetchurl {
     url = http://chichkin_i.zelnet.ru/namesys/reiserfsprogs-3.6.19.tar.gz;
     sha256 = "1gv8gr0l5l2j52540y2wj9c9h7fn0r3vabykf95748ydmr9jr1n0";
   };
 
-  patches = [./headers-fix.patch];
+  patches = [./headers-fix.patch ./verbose-flag-ignore-for-compatibility.patch ];
 
   meta = {
     homepage = http://www.namesys.com/;
