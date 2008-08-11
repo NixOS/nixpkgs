b65d095ad7d266b9b1a9dae0b96a0cc7dc181030
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-08-11 11:31:57 +0000

    * module-init-tools updated to 3.4.
    svn path=/nixpkgs/trunk/; revision=12581

--- pkgs/os-specific/linux/module-init-tools/default.nix
+++ pkgs/os-specific/linux/module-init-tools/default.nix
@@ -1,15 +1,32 @@
 {stdenv, fetchurl}:
 
-stdenv.mkDerivation ({
-  name = "module-init-tools-3.2.2";
-  src = fetchurl {
-    url = mirror://kernel/linux/utils/kernel/module-init-tools/module-init-tools-3.2.2.tar.bz2;
-    md5 = "a1ad0a09d3231673f70d631f3f5040e9";
-  };
+stdenv.mkDerivation {
+  name = "module-init-tools-3.4";
+  
+  src = [
+    (fetchurl {
+      url = mirror://kernel/linux/utils/kernel/module-init-tools/module-init-tools-3.4.tar.bz2;
+      sha256 = "11rxcdr915skc1m6dcavavw8dhcsy24wpi56sw1m4akj2frs3iwn";
+    })
+
+    # Upstream forgot to include the generated manpages.  Thankfully
+    # the Gentoo people fixed this for us :-)
+    (fetchurl {
+      url = mirror://gentoo/distfiles/module-init-tools-3.4-manpages.tar.bz2;
+      sha256 = "0jid24girjhr30mrdckylkcz11v4in46nshhrqv18yaxm6506v6j";
+    })
+  ];
+
   patches = [./module-dir.patch];
+  
   postInstall = "rm $out/sbin/insmod.static"; # don't need it
-} // (if stdenv ? isDietLibC then {
+
   # We don't want bash (and therefore glibc) in the closure of the
   # output, since we want to put this in a initrd.
-  dontPatchShebangs = true;
-} else {}))
+  dontPatchShebangs = stdenv ? isDietLibC;
+
+  meta = {
+    homepage = http://www.kernel.org/pub/linux/utils/kernel/module-init-tools/;
+    description = "Tools for loading and managing Linux kernel modules";
+  };
+}
