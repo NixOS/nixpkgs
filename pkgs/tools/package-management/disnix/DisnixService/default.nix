7bb4aae65fe422a351f16e66e64fe6fd62533968
Sander van der Burg <s.vanderburg@tudelft.nl>
2008-08-12 13:09:14 +0000

    Updated to later revision
    svn path=/nixpkgs/trunk/; revision=12595

--- pkgs/tools/package-management/disnix/DisnixService/default.nix
+++ pkgs/tools/package-management/disnix/DisnixService/default.nix
@@ -4,8 +4,8 @@ stdenv.mkDerivation {
     name = "DisnixService-0.1";
     src = fetchsvn {
 	url = https://svn.nixos.org/repos/nix/disnix/DisnixService/trunk;
-	md5 = "13e0a12292a2e1fb1049ef102863e64b";
-	rev = 12590;
+	md5 = "a39b65b7f56f94534e0b7e2ca318b4d2";
+	rev = 12594;
     };
     
     buildInputs = [ jdk apacheAnt axis2 shebangfix ];
