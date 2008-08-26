1a1592715899856af7729d3f780c03bbc4e2bcb8
Sander van der Burg <s.vanderburg@tudelft.nl>
2008-08-26 22:12:03 +0000

    Changed GWT widgets mirror to heanet
    svn path=/nixpkgs/trunk/; revision=12731

--- pkgs/development/libraries/java/gwt-widgets/default.nix
+++ pkgs/development/libraries/java/gwt-widgets/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   builder = ./builder.sh;
   
   src = fetchurl {
-    url = http://kent.dl.sourceforge.net/sourceforge/gwt-widget/gwt-widgets-0.1.5-bin.tar.gz;
+    url = http://heanet.dl.sourceforge.net/sourceforge/gwt-widget/gwt-widgets-0.1.5-bin.tar.gz;
     md5 = "daf59b3bc28a9045b6165f185e3e77a0";
   };  
 }
