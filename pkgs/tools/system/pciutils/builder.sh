562345e5950a60a0a689d78bc9e82095135b9ba8
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-08-30 09:56:21 +0000

    * pciutils updated to 3.0.0.  Also latest pci.ids.
    svn path=/nixpkgs/trunk/; revision=12773

--- pkgs/tools/system/pciutils/builder.sh
+++ pkgs/tools/system/pciutils/builder.sh
@@ -5,14 +5,6 @@ preBuild() {
   bunzip2 < $pciids > pci.ids
 }
 
-postInstall=postInstall
-postInstall() {
-  ensureDir $out/lib
-  ensureDir $out/include/pci
-  cp lib/*.h $out/include/pci
-  cp lib/libpci.a $out/lib
-}
-
-makeFlags="PREFIX=$out"
+makeFlags="PREFIX=$out $makeFlags"
 
 genericBuild
--- pkgs/tools/system/pciutils/default.nix
+++ pkgs/tools/system/pciutils/default.nix
@@ -1,16 +1,29 @@
 {stdenv, fetchurl, zlib}:
 
 stdenv.mkDerivation {
-  name = "pciutils-2.2.8";
+  name = "pciutils-3.0.0";
+  
   builder = ./builder.sh;
+  
   src = fetchurl {
-    url = mirror://kernel/software/utils/pciutils/pciutils-2.2.8.tar.bz2;
-    sha256 = "0hgri2ancnjl56ld2flb9w606dyvr5gly8gsz3bzl71r8s464qsq";
+    url = mirror://kernel/software/utils/pciutils/pciutils-3.0.0.tar.bz2;
+    sha256 = "1q9j7w95ysy6c02j7p3z58y23n5v2cdjwy6hz8s9xzvnlr0ynpnh";
   };
+  
   buildInputs = [zlib];
 
   pciids = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/pci.ids.20080118.bz2;
-    sha256 = "0dl6psdac62llbklxn4dvkzbw1j1sdadw9i4l36vpd6mvqa7lz0a";
+    url = http://nixos.org/tarballs/pci.ids.20080830.bz2;
+    sha256 = "0nfjj9lsifmm6js9w0isrscirr1a7dj9ynppbc0g5i19rzrmwafy";
+  };
+
+  # Override broken auto-detect logic.
+  makeFlags = "ZLIB=yes DNS=yes";
+
+  installTargets = "install install-lib";
+
+  meta = {
+    homepage = http://mj.ucw.cz/pciutils.shtml;
+    description = "A collection of programs for inspecting and manipulating configuration of PCI devices";
   };
 }
