d22149a3b78f9abeceb919f8bbf3fcbfc52a3e5d
Ludovic Courtès <ludo@gnu.org>
2008-09-01 13:00:24 +0000

    Use tcpdump mirror scheme.
    svn path=/nixpkgs/trunk/; revision=12780

--- pkgs/development/libraries/libpcap/default.nix
+++ pkgs/development/libraries/libpcap/default.nix
@@ -1,9 +1,12 @@
 {stdenv, fetchurl, flex, bison}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "libpcap-0.9.8";
   src = fetchurl {
-      url = [ http://www.tcpdump.org/release/libpcap-0.9.8.tar.gz http://www.sfr-fresh.com/unix/misc/libpcap-0.9.8.tar.gz ];
+      url = [
+       "mirror://tcpdump/release/${name}.tar.gz"
+       "http://www.sfr-fresh.com/unix/misc/${name}.tar.gz"
+      ];
       sha256 = "1yb2hg8jd1bzq3lbrff1sps4757krvj2c9pm2ixn44a4vsc865f4";
   };
   buildInputs = [flex bison];
--- pkgs/tools/networking/tcpdump/default.nix
+++ pkgs/tools/networking/tcpdump/default.nix
@@ -1,10 +1,18 @@
 {stdenv, fetchurl, libpcap}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "tcpdump-3.9.8";
+
   src = fetchurl {
-    url = http://www.tcpdump.org/release/tcpdump-3.9.8.tar.gz;
+    url = "mirror://tcpdump/release/${name}.tar.gz";
     sha256 = "16fjm1ih56mwqniffp63adbxwfj5n10x1a7l22j3cx683pmwh293";
   };
+
   buildInputs = [libpcap];
+
+  meta = {
+    description = "tcpdump, a famous network sniffer";
+    homepage = http://www.tcpdump.org/;
+    license = "BSD-style";
+  };
 }
