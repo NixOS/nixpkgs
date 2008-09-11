e89fed21f918d72e568f545ac577d3f80b2e15c2
Armijn Hemel <armijn@gpl-violations.org>
2008-09-11 22:04:27 +0000

    update to 1.0.3
    svn path=/nixpkgs/trunk/; revision=12845

--- pkgs/applications/networking/sniffers/wireshark/default.nix
+++ pkgs/applications/networking/sniffers/wireshark/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, perl, pkgconfig, gtk, libpcap, flex, bison}:
 
 stdenv.mkDerivation {
-  name = "wireshark-1.0.2";
+  name = "wireshark-1.0.3";
   src = fetchurl {
-    url = http://www.wireshark.org/download/src/wireshark-1.0.2.tar.gz;
-    sha256 = "0lv5agsb6ivqz2271yyiq5lsq7xvr35cxzikciq82rn7v8bj0qc9";
+    url = http://www.wireshark.org/download/src/wireshark-1.0.3.tar.bz2;
+    sha256 = "1wmkbq0rgy7rz8mqggyay98z4qd3s9bnv5lmvx1r55sndcq6z2bp";
   };
   buildInputs = [perl pkgconfig gtk libpcap flex bison];
 }
