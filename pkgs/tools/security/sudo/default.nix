0233672dc776fc0402cd421c15bb8ae7be860055
Ludovic Courtès <ludo@gnu.org>
2008-09-11 14:31:37 +0000

    Sudo 1.6.9p17.
    svn path=/nixpkgs/trunk/; revision=12841

--- pkgs/tools/security/sudo/default.nix
+++ pkgs/tools/security/sudo/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, coreutils, pam}:
 
-stdenv.mkDerivation {
-  name = "sudo-1.6.9p4";
+stdenv.mkDerivation rec {
+  name = "sudo-1.6.9p17";
 
   src = fetchurl {
-    url = ftp://sunsite.ualberta.ca/pub/Mirror/sudo/sudo-1.6.9p4.tar.gz;
-    sha256 = "0y65f77wxsl285yr1krfh5djcwm95n39p0yb3r1xgg98gir5z7w4";
+    url = "http://www.sudo.ws/sudo/dist/${name}.tar.gz";
+    sha256 = "15j5qzwn1nl9fr6ss3b2fk803cin3w11081rgqmm8vscd3zx8b0y";
   };
 
   # `--with-stow' allows /etc/sudoers to be a symlink.  Only it
@@ -16,7 +16,7 @@ stdenv.mkDerivation {
   postConfigure = "
     sed -e '/_PATH_MV/d; /_PATH_VI/d' -i config.h
     echo '#define _PATH_MV \"/var/run/current-system/sw/bin/mv\"' >> config.h
-    echo '#define _PATH_VI \"/var/run/current-system/sw/bin/vi\"' >> config.h
+    echo '#define _PATH_VI \"/var/run/current-system/sw/bin/nano\"' >> config.h
     echo '#define EDITOR _PATH_VI' >>config.h
 
     makeFlags=\"install_uid=$(id -u) install_gid=$(id -g)\"
@@ -24,4 +24,19 @@ stdenv.mkDerivation {
   ";
 
   buildInputs = [coreutils pam];
+
+  meta = {
+    description = "sudo, a command to run commands as root";
+
+    longDescription = ''
+      Sudo (su "do") allows a system administrator to delegate
+      authority to give certain users (or groups of users) the ability
+      to run some (or all) commands as root or another user while
+      providing an audit trail of the commands and their arguments.
+    '';
+
+    homepage = http://www.sudo.ws/;
+
+    license = http://www.sudo.ws/sudo/license.html;
+  };
 }
