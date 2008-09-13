1b5dbd6d9cb69c7377c57b9f7af06d7ed8ee0e93
Armijn Hemel <armijn@gpl-violations.org>
2008-09-13 10:25:20 +0000

    0.5 -> 0.5.1
    svn path=/nixpkgs/trunk/; revision=12859

--- pkgs/tools/archivers/unshield/default.nix
+++ pkgs/tools/archivers/unshield/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, zlib}:
 
 stdenv.mkDerivation {
-  name = "unshield-0.5";
+  name = "unshield-0.5.1";
   src = fetchurl {
-    url = mirror://sourceforge/synce/unshield-0.5.tar.gz;
-    sha256 = "1apdj4qphf6m21lqj9l8sg8rpn85qkp5592w5np9xbhr8776cg3p";
+    url = mirror://sourceforge/synce/unshield-0.5.1.tar.gz;
+    sha256 = "0zc6kllw54m925zyh33jwjhk9c7rwcrkyydvhdsixr72fdqacjx3";
   };
   configureFlags = "--with-zlib=${zlib}";
 }
