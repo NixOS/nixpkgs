263b3d8f1e793e4cba6a2d40cf18e8f106335093
Armijn Hemel <armijn@gpl-violations.org>
2008-09-13 10:11:39 +0000

    3.0.0 -> 3.0.4
    svn path=/nixpkgs/trunk/; revision=12858

--- pkgs/applications/networking/sync/rsync/default.nix
+++ pkgs/applications/networking/sync/rsync/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "rsync-3.0.0";
+  name = "rsync-3.0.4";
   src = fetchurl {
-    url = http://rsync.samba.org/ftp/rsync/src/rsync-3.0.0.tar.gz;
-    sha256 = "0sd11rb5cpa5a2dl3h7cn1q4nmdkc07hf9vr3yvrkqwxpfspss8p";
+    url = http://rsync.samba.org/ftp/rsync/src/rsync-3.0.4.tar.gz;
+    sha256 = "0lfyrs8vj47p1p19b0f5grxhxcn00hpb1yvvprbwzr6j077ljfkl";
   };
 }
