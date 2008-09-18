0548c19dbee4dd9137174b41412b12c7468e5546
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-09-18 21:15:14 +0000

    * NTP 4.2.4p5 (and the old url was broken).
    svn path=/nixpkgs/trunk/; revision=12883

--- pkgs/tools/networking/ntp/default.nix
+++ pkgs/tools/networking/ntp/default.nix
@@ -3,14 +3,22 @@
 assert stdenv.isLinux -> libcap != null;
  
 stdenv.mkDerivation {
-  name = "ntp-4.2.4p4";
+  name = "ntp-4.2.4p5";
+  
   src = fetchurl {
-    url = http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2.4p4.tar.gz;
-    sha256 = "0im89i51ap7aqlhxq5isz5xg4h8w8ahwdhir8la3c83h3p47fcmv";
+    url = http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/ntp-4.2.4p5.tar.gz;
+    sha256 = "066x8gm55cziyc86ciwdq68y2xqfbbqqh8417nkwd1jmrihfmjvl";
   };
+  
   configureFlags = ''
     --without-crypto
     ${if stdenv.isLinux then "--enable-linuxcaps" else ""}
   '';
+  
   buildInputs = if stdenv.isLinux then [libcap] else [];
+
+  meta = {
+    homepage = http://www.ntp.org/;
+    description = "An implementation of the Network Time Protocol";
+  };
 }
