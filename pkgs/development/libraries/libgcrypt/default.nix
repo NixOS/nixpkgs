cfef9ba3adc31a88d6432f39fa8259cd01ba2b0c
Ludovic Courtès <ludo@gnu.org>
2008-09-18 19:03:42 +0000

    GNU Libgcrypt 1.4.3.
    svn path=/nixpkgs/trunk/; revision=12882

--- pkgs/development/libraries/libgcrypt/default.nix
+++ pkgs/development/libraries/libgcrypt/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, libgpgerror }:
 
 stdenv.mkDerivation rec {
-  name = "libgcrypt-1.4.2";
+  name = "libgcrypt-1.4.3";
 
   src = fetchurl {
     url = "mirror://gnupg/libgcrypt/${name}.tar.bz2";
-    sha256 = "1hsj26c68lffhz00mi5i70kc5mg9l0dxlf6g48ms4fvzdy218x5s";
+    sha256 = "02r31dycxvlld9mn1ah78g1phkh42ajk7qcqi20x9mq1kyyikj48";
   };
 
   propagatedBuildInputs = [ libgpgerror ];
@@ -19,10 +19,10 @@ stdenv.mkDerivation rec {
   '';
 
   meta = {
-    description = "libgcrypt, a general-pupose cryptographic library";
+    description = "GNU Libgcrypt, a general-pupose cryptographic library";
 
     longDescription = ''
-      Libgcrypt is a general purpose cryptographic library based on
+      GNU Libgcrypt is a general purpose cryptographic library based on
       the code from GnuPG.  It provides functions for all
       cryptographic building blocks: symmetric ciphers, hash
       algorithms, MACs, public key algorithms, large integer
