8d7d8fc4bde41ee0fcfcc9bd87a6c3cbcf4bdddd
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-09-18 09:53:12 +0000

    * Linux 2.6.25.17.
    svn path=/nixpkgs/trunk/; revision=12879

--- pkgs/os-specific/linux/kernel/linux-2.6.25.nix
+++ pkgs/os-specific/linux/kernel/linux-2.6.25.nix
@@ -28,7 +28,7 @@ let
 
   lib = stdenv.lib;
 
-  version = "2.6.25.16";
+  version = "2.6.25.17";
 
   baseFeatures = {
     iwlwifi = true;
@@ -49,7 +49,7 @@ stdenv.mkDerivation {
   
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v2.6/linux-${version}.tar.bz2";
-    sha256 = "0xzkyfiym2ylasj862yh8zk0yhbgwlrvy5xcs14c8gq1fvrd4jcn";
+    sha256 = "15jx163rryvvdy65wgfpws8l5cqrczfygsz6v5280i5glhy1dh77";
   };
   
   patches = map (p: p.patch) kernelPatches;
