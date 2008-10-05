6c772d627ffc2dccecab61a47fcd8ee28dce9fdd
Pjotr Prins <pjotr.public01@thebird.nl>
2008-10-05 08:48:49 +0000

    rq (a cluster runner) works
    svn path=/nixpkgs/trunk/; revision=12950

--- pkgs/applications/networking/cluster/rq/default.nix
+++ pkgs/applications/networking/cluster/rq/default.nix
@@ -1,5 +1,8 @@
 {stdenv, fetchurl, sqlite, ruby }:
 
+# Package builds rq with all dependencies into one blob. This to ascertain
+# the combination of packages works.
+
 stdenv.mkDerivation {
   name = "rq-3.4.0";
   src = fetchurl {
@@ -9,11 +12,25 @@ stdenv.mkDerivation {
 
   buildInputs = [ ruby ];
 
-  installPhase = "ruby install.rb";
-  
+  # patch checks for existing stdin file - sent it upstream
+  patches = [ ./rq.patch ];
+
+  buildPhase = ''
+    cd all
+    ./install.sh $out
+    cd ..
+  '';
+
+  installPhase = ''
+  '';
+
   meta = {
     license = "Ruby";
     homepage = "http://www.codeforpeople.com/lib/ruby/rq/";
-    description = "rq is a tool used to create instant linux clusters by managing sqlite databases as nfs mounted priority work queues";
+    description = "Simple cluster queue runner";
+    longDescription = "rq creates instant linux clusters by managing priority work queues, even on a multi-core single machine. This cluster runner is easy to install and easy to manage, contrasting with the common complicated solutions.";
+    pkgMaintainer = "Pjotr Prins";
+    # rq installs a separate Ruby interpreter, which has lower priority
+    priority = "10";
   };
 }
