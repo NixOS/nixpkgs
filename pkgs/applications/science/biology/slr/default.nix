034f28e268d49d0b49de9d13ce90fcfe247d81d0
Pjotr Prins <pjotr.public01@thebird.nl>
2008-10-05 09:00:19 +0000

    Biology package
    svn path=/nixpkgs/trunk/; revision=12953

---
+++ pkgs/applications/science/biology/slr/default.nix
@@ -0,0 +1,29 @@
+{stdenv, fetchurl, liblapack}:
+
+stdenv.mkDerivation {
+  name = "slr";
+  src = fetchurl {
+    url = http://www.ebi.ac.uk/goldman-srv/SLR/download/current/slr_source.tgz;
+    sha256 = "0i81fv201p187mim4zakipxnhzqdvd3p5a9qa59xznc6458r2zsn";
+  };
+
+  buildInputs = [ liblapack ];
+  buildPhase = ''
+    cd src
+    ls
+    make -fMakefile.linux
+  '';
+  installPhase = ''
+    ensureDir $out/bin
+    cp -v ../Slr $out/bin 
+  '';
+
+  meta = {
+    description     = "Phylogenetic Analysis by Maximum Likelihood (PAML)";
+    longDescription = ''
+SLR is a program to detect sites in coding DNA that are unusually conserved and/or unusually variable (that is, evolving under purify or positive selection) by analysing the pattern of changes for an alignment of sequences on an evolutionary tree.     
+'';
+    license     = "GPL3";
+    homepage    = http://www.ebi.ac.uk/goldman/SLR/;
+  };
+}
