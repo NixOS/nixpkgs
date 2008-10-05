1587e34e0a43be7420320b5f7c65193cbdc322e0
Pjotr Prins <pjotr.public01@thebird.nl>
2008-10-05 08:57:39 +0000

    XML validating parser for Arb
    svn path=/nixpkgs/trunk/; revision=12951

---
+++ pkgs/tools/text/xml/rxp/default.nix
@@ -0,0 +1,18 @@
+{stdenv, fetchurl} :
+
+stdenv.mkDerivation {
+  name = "rxp-1.2.3";
+  src = fetchurl {
+    url = http://ftp.de.debian.org/debian/pool/main/r/rxp/rxp_1.2.3.orig.tar.gz;
+    sha256 = "1r4khvmnl5231y37ji8f3mikxy0dhdz155wi3qihfi27mc1yv534";
+  };
+  installPhase = ''
+    ensureDir $out/bin
+    cp rxp $out/bin
+  '';
+  meta = {
+    license = "GPL";
+    description = "a validating XML parser written in C";
+    homepage = "http://www.cogsci.ed.ac.uk/~richard/rxp.html";
+  };
+}
