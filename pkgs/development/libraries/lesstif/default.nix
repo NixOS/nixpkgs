cb546965bc1435653bbf71f52b1b9973b0cc79be
Pjotr Prins <pjotr.public01@thebird.nl>
2008-10-05 08:59:26 +0000

    Lesstif patches - lesstif stable has not been updated in a long time and these are recognized patches (5 out of 6 are in Debian stable). Required for the arb package.
    svn path=/nixpkgs/trunk/; revision=12952

--- pkgs/development/libraries/lesstif/default.nix
+++ pkgs/development/libraries/lesstif/default.nix
@@ -1,12 +1,26 @@
 {stdenv, fetchurl, x11, libXp, libXau}:
 
 stdenv.mkDerivation {
-  name = "lesstif-0.95";
+  name = "lesstif-0.95-p2";
   src = fetchurl {
     url = mirror://sourceforge/lesstif/lesstif-0.95.0.tar.bz2;
     md5 = "ab895165c149d7f95843c7584b1c7ad4";
   };
   buildInputs = [x11];
   propagatedBuildInputs = [libXp libXau];
-  patches = [./c-linkage.patch];
+
+  # The last stable release of lesstif was in June 2006. These
+  # patches fix a number of later issues - in particular the 
+  # render_table_crash shows up in 'arb'. The same patches appear
+  # in Debian, so we assume they have been sent upstream.
+  #
+  patches = [
+    ./c-bad_integer_cast.patch    
+    ./c-linkage.patch             
+    ./c-unsigned_int.patch
+    ./c-missing_xm_h.patch        
+    ./c-xim_chained_list_crash.patch
+    ./c-render_table_crash.patch 
+    ./c-xpmpipethrough.patch
+    ];
 }
