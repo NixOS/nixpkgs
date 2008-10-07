51aa939b218f1991023bc7944b813bba9c159a0d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-10-07 17:34:00 +0000

    * GNU M4 1.4.11.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=12993

--- pkgs/development/tools/misc/gnum4/default.nix
+++ pkgs/development/tools/misc/gnum4/default.nix
@@ -1,8 +1,15 @@
 {stdenv, fetchurl}:
+
 stdenv.mkDerivation {
-  name = "gnum4-1.4.9";
+  name = "gnum4-1.4.11";
+  
   src = fetchurl {
-    url = mirror://gnu/m4/m4-1.4.9.tar.bz2;
-    sha256 = "0a9xgknp29zdhpp7xn3bfcxdc0wn2rzvjfdksz5ym82b6y397qm8";
+    url = mirror://gnu/m4/m4-1.4.11.tar.bz2;
+    sha256 = "1bcakymxddxykg5vbll3d9xq17m5sa3r6cprf1k27x5k4mjnhz0b";
+  };
+
+  meta = {
+    homepage = http://www.gnu.org/software/m4/;
+    description = "An implementation of the traditional Unix macro processor";
   };
 }
