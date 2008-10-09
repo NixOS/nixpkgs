201d20db206667ba7c5bd5ec88ba03741a7fa24d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-10-09 14:29:06 +0000

    * openssl-0.9.8i.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=13024

--- pkgs/development/libraries/openssl/default.nix
+++ pkgs/development/libraries/openssl/default.nix
@@ -1,11 +1,21 @@
 {stdenv, fetchurl, perl}:
 
 stdenv.mkDerivation {
-  name = "openssl-0.9.8h";
-  builder = ./builder.sh;
+  name = "openssl-0.9.8i";
+  
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/security/openssl/openssl-0.9.8h.tar.gz;
-    sha1 = "ced4f2da24a202e01ea22bef30ebc8aee274de86";
+    url = http://www.openssl.org/source/openssl-0.9.8i.tar.gz;
+    sha1 = "b2e029cfb68bf32eae997d60317a40945db5a65f";
   };
+  
   buildInputs = [perl];
+
+  configureScript = "./config";
+  
+  configureFlags = "shared";
+
+  meta = {
+    homepage = http://www.openssl.org/;
+    description = "A cryptographic library that implements the SSL and TLS protocols";
+  };
 }
