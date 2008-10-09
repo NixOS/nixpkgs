a644a08a25a4d53093d53ab921582c7a39d0889c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-10-09 12:55:55 +0000

    * Latest version.
    svn path=/nixpkgs/trunk/; revision=13021

--- pkgs/tools/package-management/nix/unstable.nix
+++ pkgs/tools/package-management/nix/unstable.nix
@@ -4,14 +4,14 @@
 , supportOldDBs ? true
 }:
 
-let version = "0.12pre12824"; in
+let version = "0.12pre12876"; in
 
 stdenv.mkDerivation {
   name = "nix-${version}";
   
   src = fetchurl {
-    url = "http://nixos.org/releases/nix/nix-${version}-dr98xr6q/nix-${version}.tar.bz2";
-    sha256 = "cd6176fbcb677435218d5e8067b9c3a4dee172e6ede44a28080edef5e69ccd75";
+    url = "http://nixos.org/releases/nix/nix-${version}-mlwc8b59/nix-${version}.tar.bz2";
+    sha256 = "480767060934ce4866bd2e6975de4964dc9519169e7512369b12750af4bb0238";
   };
 
   buildInputs = [perl curl openssl];
