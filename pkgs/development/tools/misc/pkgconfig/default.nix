b43d4f87db55fe32716e1f174c0a375248546754
Ludovic Courtès <ludo@gnu.org>
2008-10-12 19:15:29 +0000

    Pkgconfig 0.23.
    Untested!
    
    svn path=/nixpkgs/branches/stdenv-updates/; revision=13054

--- pkgs/development/tools/misc/pkgconfig/default.nix
+++ pkgs/development/tools/misc/pkgconfig/default.nix
@@ -1,24 +1,14 @@
 {stdenv, fetchurl}:
 
-stdenv.mkDerivation {
-  name = "pkg-config-0.22";
+stdenv.mkDerivation rec {
+  name = "pkg-config-0.23";
   
   setupHook = ./setup-hook.sh;
   
   src = fetchurl {
-    url = http://pkgconfig.freedesktop.org/releases/pkg-config-0.22.tar.gz;
-    sha256 = "1rpb5wygmp0f8nal7y3ga4556i7hkjdslv3wdq04fj30gns621vy";
+    url = "http://pkgconfig.freedesktop.org/releases/${name}.tar.gz";
+    sha256 = "0lrvk17724mc2nzpaa0vwybarrl50r7qdnr4h6jijm50srrf1808";
   };
-
-  patches = [
-    # Process Requires.private properly, see
-    # http://bugs.freedesktop.org/show_bug.cgi?id=4738.
-    (fetchurl {
-      name = "pkgconfig-8494.patch";
-      url = http://bugs.freedesktop.org/attachment.cgi?id=8494;
-      sha256 = "1pcrdbb7dypg2biy0yqc7bdxak5zii8agqljdvk7j4wbyghpqzws";
-    })
-  ];
 
   meta = {
     description = "A tool that allows packages to find out information about other packages";
