1e6622f4d5d500d6e701bd81dd4a22977d10637d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-10-13 12:51:04 +0000

    * The patch for proper Requires.private processing is still necessary,   unfortunately.  For instance, libxcb's xcb.pc declares:
        Requires.private: pthread-stubs ...
    
      Thus libxcb has to declare the pthread-stubs package as a
      propagatedBuildInput, otherwise packages that check for libxcb using
      pkg-config (such as libX11) will fail.  But then we get a totally
      unnecessary additional runtime dependency, which is clearly a bad
      thing.
    
    svn path=/nixpkgs/branches/stdenv-updates/; revision=13058

--- pkgs/development/tools/misc/pkgconfig/default.nix
+++ pkgs/development/tools/misc/pkgconfig/default.nix
@@ -10,6 +10,12 @@ stdenv.mkDerivation rec {
     sha256 = "0lrvk17724mc2nzpaa0vwybarrl50r7qdnr4h6jijm50srrf1808";
   };
 
+  patches = [
+    # Process Requires.private properly, see
+    # http://bugs.freedesktop.org/show_bug.cgi?id=4738.
+    ./requires-private.patch
+  ];
+
   meta = {
     description = "A tool that allows packages to find out information about other packages";
     homepage = http://pkg-config.freedesktop.org/wiki/;
