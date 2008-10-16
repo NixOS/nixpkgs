777b377582cf8c8b5728553664d93c54033c497f
Marc Weber <marco-oweber@gmx.de>
2008-10-16 23:24:37 +0000

    dooh - mjpegtools forgotten to bump name version as well
    svn path=/nixpkgs/trunk/; revision=13090

--- pkgs/tools/video/mjpegtools/default.nix
+++ pkgs/tools/video/mjpegtools/default.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchurl, libjpeg, libX11}:
 
 stdenv.mkDerivation {
-  name = "mjpegtools-1.6.2";
+  name = "mjpegtools-1.9.0rc3";
   src = fetchurl {
     url = mirror://sourceforge/mjpeg/mjpegtools-1.9.0rc3.tar.gz;
     sha256 = "1xvgqzdb2rw6j4ss65k4hrzrbsl74p7k5l4qgf5dbfcw522kw7lb";
