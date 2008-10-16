17a9d60d01e5a83eb27b450a8f68a7caaa1ea5f6
Peter Simons <simons@cryp.to>
2008-10-16 12:42:07 +0000

    neon-0.28: add support for a static library build (unused by default)
    svn path=/nixpkgs/trunk/; revision=13082

--- pkgs/development/libraries/neon/0.28.nix
+++ pkgs/development/libraries/neon/0.28.nix
@@ -1,23 +1,27 @@
 { stdenv, fetchurl, libxml2
 , compressionSupport ? true, zlib ? null
 , sslSupport ? true, openssl ? null
+, static ? false
+, shared ? true
 }:
 
 assert compressionSupport -> zlib != null;
 assert sslSupport -> openssl != null;
+assert static || shared;
 
 stdenv.mkDerivation {
   name = "neon-0.28.3";
-  
+
   src = fetchurl {
     url = http://www.webdav.org/neon/neon-0.28.3.tar.gz;
     sha256 = "1hnd9wlbnfpppx6rvalhdkc1rf29afacl1m15z751g3h9hdybplh";
   };
-  
+
   buildInputs = [libxml2] ++ stdenv.lib.optional compressionSupport zlib;
 
   configureFlags = ''
-    --enable-shared --disable-static
+    ${if shared then "--enable-shared" else "--disable-shared"}
+    ${if static then "--enable-static" else "--disable-static"}
     ${if compressionSupport then "--with-zlib" else "--without-zlib"}
     ${if sslSupport then "--with-ssl --with-libs=${openssl}" else "--without-ssl"}
   '';
