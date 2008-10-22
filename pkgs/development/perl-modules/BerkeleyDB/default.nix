cd48c1b4960a39927e957d9c6435d2b9c10ace35
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-10-22 19:25:01 +0000

    * Unbreak the DB_File / BerkeleyDB Perl modules.
    svn path=/nixpkgs/trunk/; revision=13102

--- pkgs/development/perl-modules/BerkeleyDB/default.nix
+++ pkgs/development/perl-modules/BerkeleyDB/default.nix
@@ -2,10 +2,14 @@
 
 import ../generic perl {
   name = "BerkeleyDB-0.27";
+  
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/BerkeleyDB-0.27.tar.gz;
+    url = http://nixos.org/tarballs/BerkeleyDB-0.27.tar.gz;
     md5 = "43aa72c0c6941af0d656d749ad543e96";
   };
-  perlPreHook = "source ${./hook.sh}";
-  inherit db4;
+
+  preConfigure = ''
+    echo "LIB = ${db4}/lib" > config.in
+    echo "INCLUDE = ${db4}/include" >> config.in
+  '';
 }
--- pkgs/development/perl-modules/BerkeleyDB/hook.sh
+++
@@ -1,7 +0,0 @@
-oldPreConfigure=$preConfigure
-preConfigure=myPreConfigure
-myPreConfigure() {
-    echo "LIB = $db4/lib" > config.in
-    echo "INCLUDE = $db4/include" >> config.in
-    $oldPreConfigure
-}
