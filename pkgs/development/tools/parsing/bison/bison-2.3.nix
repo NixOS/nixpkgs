c0960419b25830a1bf0c787d8c14130d718029de
Ludovic Courtès <ludo@gnu.org>
2008-11-03 18:27:35 +0000

    GNU Bison 2.3: Update `meta' attribute.
    svn path=/nixpkgs/trunk/; revision=13181

--- pkgs/development/tools/parsing/bison/bison-2.3.nix
+++ pkgs/development/tools/parsing/bison/bison-2.3.nix
@@ -11,7 +11,25 @@ stdenv.mkDerivation {
   buildInputs = [m4];
 
   meta = {
-    description = "A yacc-compatible parser generator";
+    description = "GNU Bison, a Yacc-compatible parser generator";
+
+    longDescription = ''
+      Bison is a general-purpose parser generator that converts an
+      annotated context-free grammar into an LALR(1) or GLR parser for
+      that grammar.  Once you are proficient with Bison, you can use
+      it to develop a wide range of language parsers, from those used
+      in simple desk calculators to complex programming languages.
+
+      Bison is upward compatible with Yacc: all properly-written Yacc
+      grammars ought to work with Bison with no change.  Anyone
+      familiar with Yacc should be able to use Bison with little
+      trouble.  You need to be fluent in C or C++ programming in order
+      to use Bison.
+    '';
+
+    homepage = http://www.gnu.org/software/bison/;
+
+    license = "GPLv3+";
   };
 } // {
   glrSupport = true;
