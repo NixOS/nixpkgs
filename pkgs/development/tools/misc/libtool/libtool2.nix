fccfff91dd5fff2b2fe7e35e5700dfa9c48e0eec
Ludovic Courtès <ludo@gnu.org>
2008-11-03 10:22:41 +0000

    GNU Libtool 2.x: Add `meta' attribute.
    svn path=/nixpkgs/trunk/; revision=13177

--- pkgs/development/tools/misc/libtool/libtool2.nix
+++ pkgs/development/tools/misc/libtool/libtool2.nix
@@ -15,4 +15,22 @@ stdenv.mkDerivation rec {
   # Don't fixup "#! /bin/sh" in Libtool, otherwise it will use the
   # "fixed" path in generated files!
   dontPatchShebangs = true;
+
+  meta = {
+    description = "GNU Libtool, a generic library support script";
+
+    longDescription = ''
+      GNU libtool is a generic library support script.  Libtool hides
+      the complexity of using shared libraries behind a consistent,
+      portable interface.
+
+      To use libtool, add the new generic library building commands to
+      your Makefile, Makefile.in, or Makefile.am.  See the
+      documentation for details.
+    '';
+
+    homepage = http://www.gnu.org/software/libtool/;
+
+    license = "GPLv2+";
+  };
 }
