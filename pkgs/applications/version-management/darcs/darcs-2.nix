5333a271a32516e02b4a868853cf006b0eda1bc8
Marc Weber <marco-oweber@gmx.de>
2008-11-05 20:20:42 +0000

    bump darcs to 2.1.0 (release) - patch provided by Ben Franksen
    svn path=/nixpkgs/trunk/; revision=13201

--- pkgs/applications/version-management/darcs/darcs-2.nix
+++ pkgs/applications/version-management/darcs/darcs-2.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, ghc, zlib, ncurses, curl, perl}:
 
 stdenv.mkDerivation {
-  name = "darcs-2.1.0pre3";
+  name = "darcs-2.1.0";
   src = fetchurl {
-    url = http://www.darcs.net/darcs-2.1.0pre3.tar.gz;
-    sha256 = "a4b63c16a20edef3e1dc06db29211b7272b51e6ef10e12924e6515fb71f58303";
+    url = http://www.darcs.net/darcs-2.1.0.tar.gz;
+    sha256 = "d5a63e62bceb45905163d508c6b25158dab6aca367015566d8c539ec37107ab4";
   };
 
   buildInputs = [ghc zlib ncurses curl perl];
