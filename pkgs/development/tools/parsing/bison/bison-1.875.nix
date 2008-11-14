c1cbd437dcbd5a58c04b36dcb9d179c221d2e6e9
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-11-14 16:28:15 +0000

    * Doh.
    svn path=/nixpkgs/trunk/; revision=13297

--- pkgs/development/tools/parsing/bison/bison-1.875.nix
+++ pkgs/development/tools/parsing/bison/bison-1.875.nix
@@ -5,7 +5,7 @@ assert m4 != null;
 stdenv.mkDerivation {
   name = "bison-1.875";
   src = fetchurl {
-    urls = mirror://gnu/bison/bison-1.875.tar.bz2;
+    url = mirror://gnu/bison/bison-1.875.tar.bz2;
     md5 = "b7f8027b249ebd4dd0cc948943a71af0";
   };
   buildInputs = [m4];
