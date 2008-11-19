96aade5a27391add8936249ff1fbe8f5a2413a43
Marc Weber <marco-oweber@gmx.de>
2008-11-19 21:45:43 +0000

    hackish fix to build cdrtools again
    svn path=/nixpkgs/trunk/; revision=13343

--- pkgs/applications/misc/cdrtools/default.nix
+++ pkgs/applications/misc/cdrtools/default.nix
@@ -3,6 +3,15 @@
 stdenv.mkDerivation {
   name = "cdrtools-2.01";
   builder = ./builder.sh;
+  #hack, I'm getting "chown: invalid user: `bin" error, so replace chown by a nop dummy script
+  preInstall = ''
+    mkdir "$TMP/bin"
+    for i in chown chgrp; do
+      echo '#!/bin/sh' >> "$TMP/bin/$i"
+      chmod +x "$TMP/bin/$i"
+      PATH="$TMP/bin:$PATH"
+    done
+  '';
   src = fetchurl {
     url = ftp://ftp.berlios.de/pub/cdrecord/cdrtools-2.01.tar.bz2;
     md5 = "d44a81460e97ae02931c31188fe8d3fd";
