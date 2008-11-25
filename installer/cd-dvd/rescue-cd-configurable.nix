23551d0a4dbf2419a3a4534bc77411c1acdb9e07
Michael Raskin <7c6f434c@mail.ru>
2008-11-25 18:00:25 +0000

    some modules..
    svn path=/nixos/trunk/; revision=13419

--- installer/cd-dvd/rescue-cd-configurable.nix
+++ installer/cd-dvd/rescue-cd-configurable.nix
@@ -153,7 +153,6 @@ rec {
       kernelModules = bootKernelModules;
       extraModulePackages = ((extraModulePackages pkgs)
       ++(if aufs then [(kernelPackages pkgs).aufs] else [])
-      ++(pkgs.lib.optional intel3945FWEnable (kernelPackages pkgs).iwlwifi)
       );
     };
     
