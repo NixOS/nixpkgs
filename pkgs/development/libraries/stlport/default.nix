0720af543a3f0766c5c2e696250cdbe7ad379fae
Valentin David <valentin.david@gmail.com>
2008-12-02 16:51:39 +0000

    Update stlport to 5.2.0.
    svn path=/nixpkgs/trunk/; revision=13558

--- pkgs/development/libraries/stlport/default.nix
+++ pkgs/development/libraries/stlport/default.nix
@@ -1,11 +1,10 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "STLport-5.0.0";
-  builder = ./builder.sh;
+  name = "STLport-5.2.0";
 
   src = fetchurl {
-    url = mirror://sourceforge/stlport/STLport-5.0.0.tar.bz2;
-    md5 = "5aefcbb0c30a91d50bb2d6c7b30e8393";
+    url = mirror://sourceforge/stlport/STLport-5.2.0.tar.bz2;
+    md5 = "448d74859407912c0087adcf51bf109a";
   };
 }
