01ed35dc5fdae139434ae705366e616123364a07
Ludovic Courtès <ludo@gnu.org>
2008-12-18 09:18:36 +0000

    GSL 1.12.
    svn path=/nixpkgs/trunk/; revision=13656

--- pkgs/development/libraries/gsl/default.nix
+++ pkgs/development/libraries/gsl/default.nix
@@ -1,15 +1,32 @@
-args: with args;
-stdenv.mkDerivation {
-  name = "gsl-1.11";
+{ fetchurl, stdenv }:
+
+stdenv.mkDerivation rec {
+  name = "gsl-1.12";
 
   src = fetchurl {
-    url = ftp://ftp.gnu.org/gnu/gsl/gsl-1.11.tar.gz;
-    sha256 = "1c8ijbykgm6w8q0a1j3bfjdd9764fcw9v709bv7pqrgimq3ya4bn";
+    url = "mirror://gnu/gsl/${name}.tar.gz";
+    sha256 = "1fdpqw981gcc0wkvcacm16mrrsq5f4jdq395zk59lxjcsa492092";
   };
 
+  doCheck = true;
+
+  configureFlags = "--disable-static";
+
   meta = { 
-      description = "numerical library (>1000 functions)";
-      homepage = http://www.gnu.org/software/gsl;
-      license = "GPL2";
+    description = "The GNU Scientific Library, a large numerical library";
+
+    longDescription = ''
+      The GNU Scientific Library (GSL) is a numerical library for C
+      and C++ programmers.  It is free software under the GNU General
+      Public License.
+
+      The library provides a wide range of mathematical routines such
+      as random number generators, special functions and least-squares
+      fitting.  There are over 1000 functions in total with an
+      extensive test suite.
+    '';
+
+    homepage = http://www.gnu.org/software/gsl/;
+    license = "GPLv3+";
   };
 }
