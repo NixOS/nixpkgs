f87ad9b6630a61bb56da622cf8b39bd4bbbc2642
Michael Raskin <7c6f434c@mail.ru>
2009-01-03 13:42:36 +0000

    Autoconf 2.13 (older version used by Mozilla)
    svn path=/nixpkgs/trunk/; revision=13691

---
+++ pkgs/development/tools/misc/autoconf/2.13.nix
@@ -0,0 +1,37 @@
+{stdenv, fetchurl, m4, perl, lzma}:
+
+stdenv.mkDerivation rec {
+  name = "autoconf-2.13";
+  
+  src = fetchurl {
+    url = "mirror://gnu/autoconf/${name}.tar.gz";
+    sha256 = "07krzl4czczdsgzrrw9fiqx35xcf32naf751khg821g5pqv12qgh";
+  };
+
+  buildInputs = [m4 perl lzma];
+  
+  unpackCmd = "lzma -d < $src | tar -x ";
+
+  doCheck = true;
+
+  # Don't fixup "#! /bin/sh" in Autoconf, otherwise it will use the
+  # "fixed" path in generated files!
+  dontPatchShebangs = true;
+
+  meta = {
+    homepage = http://www.gnu.org/software/autoconf/;
+    description = "GNU Autoconf, a part of the GNU Build System";
+
+    longDescription = ''
+      GNU Autoconf is an extensible package of M4 macros that produce
+      shell scripts to automatically configure software source code
+      packages.  These scripts can adapt the packages to many kinds of
+      UNIX-like systems without manual user intervention.  Autoconf
+      creates a configuration script for a package from a template
+      file that lists the operating system features that the package
+      can use, in the form of M4 macro calls.
+    '';
+
+    license = "GPLv2+";
+  };
+}
