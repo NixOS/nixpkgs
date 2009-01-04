c1ae54a6b3159d0443c0294eb485fafbe6dc33aa
Andres Löh <mail@andres-loeh.de>
2009-01-04 13:48:58 +0000

    version bump to 0.9.7
    svn path=/nixpkgs/trunk/; revision=13697

--- pkgs/development/tools/haskell/uuagc/default.nix
+++ pkgs/development/tools/haskell/uuagc/default.nix
@@ -2,9 +2,9 @@
 
 cabal.mkDerivation (self : {
   pname = "uuagc";
-  version = "0.9.6";
+  version = "0.9.7";
   name = self.fname;
-  sha256 = "10e148bdf052e9a80e52c54a94314df9d1772e68416e5dfac289c47fd1ba8558";
+  sha256 = "7479ddbc8dc4b04cae278a942a50d7d76f06011aca06c56bcd26bdeba6eeb2d6";
   extraBuildInputs = [uulib];
   meta = {
     description = "Attribute Grammar System of Universiteit Utrecht";
