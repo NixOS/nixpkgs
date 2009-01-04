9d67550148321daca17c886742cbc7706d47d3fa
Andres Löh <mail@andres-loeh.de>
2009-01-04 17:30:03 +0000

    version bump to 0.8
    svn path=/nixpkgs/trunk/; revision=13701

--- pkgs/applications/graphics/pqiv/default.nix
+++ pkgs/applications/graphics/pqiv/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, getopt, which, pkgconfig, gtk } :
 
 stdenv.mkDerivation (rec {
-  name = "pqiv-0.7.1";
+  name = "pqiv-0.8";
 
   src = fetchurl {
-    url = "http://www.pberndt.com/raw//Programme/Linux/pqiv/_download/pqiv-0.7.1.tbz";
-    sha256 = "659c5d1d12c080dff5152325a2391e01a2c9a6ea225d1f755a98e8766f318eef";
+    url = "http://www.pberndt.com/raw//Programme/Linux/pqiv/_download/pqiv-0.8.tbz";
+    sha256 = "365332bab4b13ca56da6935e7155af20658e67d323808942dce23e880466f66d";
   };
 
   buildInputs = [ getopt which pkgconfig gtk ];
