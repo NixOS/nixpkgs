e1281affb558008c68b82a535e8bba0c418c321a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-01-06 09:43:16 +0000

    * man-pages 3.15.
    svn path=/nixpkgs/trunk/; revision=13713

--- pkgs/data/documentation/man-pages/default.nix
+++ pkgs/data/documentation/man-pages/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation rec {
-  name = "man-pages-3.11";
+  name = "man-pages-3.15";
   
   src = fetchurl {
     url = "mirror://kernel/linux/docs/man-pages/${name}.tar.bz2";
-    sha256 = "1pl3jdp7vh6vl2drqdal3ggdc22icdgfkfbswh21k8jlcxf00dn8";
+    sha256 = "0pr29ziz2d2zl2iii16372x2bqvx1a5g8xbb6wga4nxiz4w6ixhh";
   };
 
   preBuild = "
