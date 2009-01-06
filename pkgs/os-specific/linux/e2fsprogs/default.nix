a36a329e57133cc0e2e14791bcbd0289c872e9a0
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-01-06 09:16:41 +0000

    * e2fsprogs 1.41.3.  This adds userspace support for ext4.
    svn path=/nixpkgs/trunk/; revision=13711

--- pkgs/os-specific/linux/e2fsprogs/default.nix
+++ pkgs/os-specific/linux/e2fsprogs/default.nix
@@ -1,20 +1,30 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "e2fsprogs-1.40.11";
+  name = "e2fsprogs-1.41.3";
+  
   src = fetchurl {
-    url = mirror://sourceforge/e2fsprogs/e2fsprogs-1.40.11.tar.gz;
-    sha256 = "0yw97irzwrra09wijjn8ig5j2fkyqwxl3hqyspsm3cmk2cshrsry";
+    url = mirror://sourceforge/e2fsprogs/e2fsprogs-1.41.3.tar.gz;
+    sha256 = "0yldax5z1d1gfxpvzmr8y2z5zg5xhbi9pjjy4yw0q28dd2pfsxyf";
   };
+  
   configureFlags =
     if stdenv ? isDietLibC
     then ""
     else "--enable-elf-shlibs";
+    
   preInstall = "installFlagsArray=('LN=ln -s')";
+  
   postInstall = "make install-libs";
+  
   NIX_CFLAGS_COMPILE =
     if stdenv ? isDietLibC then
       "-UHAVE_SYS_PRCTL_H " +
       (if stdenv.system == "x86_64-linux" then "-DHAVE_LSEEK64_PROTOTYPE=1 -Dstat64=stat" else "")
     else "";
+
+  meta = {
+    homepage = http://e2fsprogs.sourceforge.net/;
+    description = "Tools for creating and checking ext2/ext3/ext4 filesystems";
+  };
 }
