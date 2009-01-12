abfb6c220d449c8d0c9f255eb15983dc07983ee8
Ludovic Courtès <ludo@gnu.org>
2009-01-12 20:20:18 +0000

    GnuPG 2.0.10.
    svn path=/nixpkgs/trunk/; revision=13749

--- pkgs/tools/security/gnupg2/default.nix
+++ pkgs/tools/security/gnupg2/default.nix
@@ -1,22 +1,37 @@
-# remmeber to
-# echo "pinentry-program `which pinentry-gtk-2`" >> ~/.gnupg/gpg-agent.conf
-# and install pinentry as well
+# Remember to install Pinentry and
+# 'echo "pinentry-program `which pinentry-gtk-2`" >> ~/.gnupg/gpg-agent.conf'.
 
-args: with args;
+{ fetchurl, stdenv, readline, openldap, bzip2, zlib, libgpgerror
+, pth, libgcrypt, libassuan, libksba, libusb, curl }:
+
 stdenv.mkDerivation rec {
-  name = "gnupg-2.0.8";
+  name = "gnupg-2.0.10";
 
   src = fetchurl {
     url = "mirror://gnupg/gnupg/${name}.tar.bz2";
-    sha256 = "04v9s92xph1hrhac49yyrgzdwjqshs2zawvjbi3jc2klwjpi1wqn";
+    sha256 = "08yz2kgcnphjml5mhq4bm4dg64jrz79p97nlrlb88ym6p6ybg26l";
   };
 
   buildInputs = [ readline openldap bzip2 zlib libgpgerror pth libgcrypt
     libassuan libksba libusb curl ];
 
+  doCheck = true;
+
   meta = {
-    description = "A free implementation of the OpenPGP standard for encrypting
-	and signing data, v2";
-    homepage = http://www.gnupg.org/;
+    description = "GNU Privacy Guard (GnuPG), GNU Project's implementation of the OpenPGP standard";
+
+    longDescription = ''
+      GnuPG is the GNU project's complete and free implementation of
+      the OpenPGP standard as defined by RFC4880.  GnuPG allows to
+      encrypt and sign your data and communication, features a
+      versatile key managment system as well as access modules for all
+      kind of public key directories.  GnuPG, also known as GPG, is a
+      command line tool with features for easy integration with other
+      applications.  A wealth of frontend applications and libraries
+      are available.  Version 2 of GnuPG also provides support for
+      S/MIME.
+    '';
+
+    homepage = http://gnupg.org/;
   };
 }
