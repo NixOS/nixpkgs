a0670a904eeba5f4100de68d9c06c6cba4e5e267
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-01-15 15:40:23 +0000

    * Reduce duplication between kernel expressions.
    svn path=/nixpkgs/trunk/; revision=13778

--- pkgs/os-specific/linux/kernel/generic.nix
+++ pkgs/os-specific/linux/kernel/generic.nix
@@ -1,17 +1,25 @@
-{ stdenv, fetchurl, perl, mktemp, module_init_tools, lib
+{ stdenv, fetchurl, perl, mktemp, module_init_tools
 
-  # The base source file
-, src
+, # The kernel source tarball.
+  src
 
-  # A list of patches to apply to the kernel.  Each element of this list
+, # The kernel version.
+  version
+
+, # The kernel configuration.
+  config
+
+, # An attribute set whose attributes express the availability of
+  # certain features in this kernel.  E.g. `{iwlwifi = true;}'
+  # indicates a kernel that provides Intel wireless support.  Used in
+  # NixOS to implement kernel-specific behaviour.
+  features ? {}
+
+, # A list of patches to apply to the kernel.  Each element of this list
   # should be an attribute set {name, patch} where `name' is a
   # symbolic name and `patch' is the actual patch.  The patch may
   # optionally be compressed with gzip or bzip2.
-, kernelPatches ? []
-
-  # A list of commands to run on patched kernel before 
-  # mke oldconfig
-, preConfigure? ""
+  kernelPatches ? []
 
 , # Whether to build a User-Mode Linux kernel.
   userModeLinux ? false
@@ -19,34 +27,25 @@
 , # Whether to build a Xen kernel.
   xen ? false
 
-, # Allows you to set your own kernel version for output
-  version ? "unknown"
-
-  # To change how kernel thinks of itself
-, localVersion ? ""
-
-, # Your own kernel configuration file, if you don't want to use the
-  # default. 
-  kernelConfig ? null
+, # Allows you to set your own kernel version suffix (e.g.,
+  # "-my-kernel").
+  localVersion ? ""
 
 , # A list of additional statements to be appended to the
   # configuration file.
   extraConfig ? []
-
-, features ? []
 }:
 
 assert stdenv.system == "i686-linux" || stdenv.system == "x86_64-linux";
 
 let
 
-  lib = import ../../../lib;
+  lib = stdenv.lib;
 
 in
 
 stdenv.mkDerivation {
   name = if userModeLinux then "user-mode-linux-${version}" else "linux-${version}";
-  builder = ./builder-custom.sh;
 
   passthru = {
     inherit version;
@@ -54,28 +53,23 @@ stdenv.mkDerivation {
     features = lib.fold (x: y: (if x ? features then x.features else {}) // y) features kernelPatches;
   };
   
-  inherit src;
-  preConfigure = preConfigure;
+  builder = ./builder.sh;
 
+  inherit src config;
+  
   patches = map (p: p.patch) kernelPatches;
+  
   extraConfig =
     let addNewlines = map (s: "\n" + s + "\n");
         configFromPatches =
           map (p: if p ? extraConfig then p.extraConfig else "") kernelPatches;
     in lib.concatStrings (addNewlines (configFromPatches ++ extraConfig));
 
-  config =
-    if kernelConfig != null then kernelConfig else
-    if userModeLinux then ./config-2.6.23-uml else
-    if stdenv.system == "i686-linux" then ./config-2.6.23-i686-smp else
-    if stdenv.system == "x86_64-linux" then ./config-2.6.23-x86_64-smp else
-    abort "No kernel configuration for your platform!";
-  
   buildInputs = [perl mktemp];
   
   arch =
-    if userModeLinux then "um" else
     if xen then "xen" else
+    if userModeLinux then "um" else
     if stdenv.system == "i686-linux" then "i386" else
     if stdenv.system == "x86_64-linux" then "x86_64" else
     abort "Platform ${stdenv.system} is not supported.";
