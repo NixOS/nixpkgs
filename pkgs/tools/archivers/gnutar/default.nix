4998f96209a7de8289c4b54aeab0cd947f6bb31b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-01-16 16:11:22 +0000

    * GNU tar 1.21.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=13787

--- pkgs/tools/archivers/gnutar/default.nix
+++ pkgs/tools/archivers/gnutar/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "gnutar-1.20";
+  name = "gnutar-1.21";
   
   src = fetchurl {
-    url = mirror://gnu/tar/tar-1.20.tar.bz2;
-    sha256 = "1swx3whm2vh0qzq8v04vgwk5zds6zlznk52xwivj7p2szcxg72xy";
+    url = mirror://gnu/tar/tar-1.21.tar.bz2;
+    sha256 = "0l5kmq3s6rbps6h62li5a1yycchaa2mnhv8b8qlak90w0z970v6w";
   };
   
   patches = [./implausible.patch];
