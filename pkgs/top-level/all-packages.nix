0604fa4ed8795eb8684deb482b370e6fafaf13a7
Armijn Hemel <armijn@gpl-violations.org>
2009-01-28 17:16:27 +0000

    pass intltool to new pidgin
    svn path=/nixpkgs/trunk/; revision=13898

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7835,7 +7835,7 @@ let
 
   pidgin = import ../applications/networking/instant-messengers/pidgin {
     inherit fetchurl stdenv pkgconfig perl perlXMLParser libxml2 nss
-      gtkspell aspell gettext ncurses avahi dbus dbus_glib lib;
+      gtkspell aspell gettext ncurses avahi dbus dbus_glib lib intltool;
     openssl = if (getConfig ["pidgin" "openssl"] true) then openssl else null;
     gnutls = if (getConfig ["pidgin" "gnutls"] false) then gnutls else null;
     GStreamer = gst_all.gstreamer;
