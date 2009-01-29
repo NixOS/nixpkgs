7da93ed73833388cc071af463e49cfb3e762d726
Ludovic Courtès <ludo@gnu.org>
2009-01-29 21:24:15 +0000

    libglade: Add meta-information.
    svn path=/nixpkgs/trunk/; revision=13912

--- pkgs/desktops/gnome/libglade.nix
+++ pkgs/desktops/gnome/libglade.nix
@@ -6,4 +6,27 @@ stdenv.mkDerivation {
   inherit (input) name src;
   buildInputs = [pkgconfig];
   propagatedBuildInputs = [gtk libxml2];
+
+  meta = {
+    description = "Glade, a User Interface Designer for GTK+ and GNOME";
+
+    longDescription = ''
+      Glade is a RAD tool to enable quick & easy development of user
+      interfaces for the GTK+ toolkit and the GNOME desktop
+      environment, released under the GNU GPL License.
+
+      The user interfaces designed in Glade are saved as XML, and by
+      using the libglade library these can be loaded by applications
+      dynamically as needed.
+
+      By using libglade, Glade XML files can be used in numerous
+      programming languages including C, C++, Java, Perl, Python, C#,
+      Pike, Ruby, Haskell, Objective Caml and Scheme.  Adding support
+      for other languages is easy too.
+    '';
+
+    license = "LGPLv2+";
+
+    homepage = http://glade.gnome.org/;
+  };
 }
