4fc2a64242cec1d9a83715463eb165c1e186ad86
Armijn Hemel <armijn@gpl-violations.org>
2009-02-01 18:51:15 +0000

    version bump to 0.0.22
    svn path=/nixpkgs/trunk/; revision=13927

--- pkgs/development/libraries/gegl/default.nix
+++ pkgs/development/libraries/gegl/default.nix
@@ -1,10 +1,10 @@
 args:
 args.stdenv.mkDerivation {
-  name = "gegl-0.0.20";
+  name = "gegl-0.0.22";
 
   src = args.fetchurl {
-    url = ftp://ftp.gimp.org/pub/gegl/0.0/gegl-0.0.20.tar.bz2;
-    sha256 = "1dqdammp2jv6cwycwx5pwn5m9n3xss5j6656xb59dj4xxypvd2vh";
+    url = ftp://ftp.gimp.org/pub/gegl/0.0/gegl-0.0.22.tar.bz2;
+    sha256 = "0nx6r9amzhw5d2ghlw3z8qnry18rwz1ymvl2cm31b8p49z436wl5";
   };
 
   configureFlags = "--disable-docs"; # needs fonts otherwise  don't know how to pass them
