db6973610ca557ad6a3777cc472a9384d8cbbdb8
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2009-02-02 21:56:29 +0000

    Updating cromfs to the just released 1.5.7.
    svn path=/nixpkgs/trunk/; revision=13964

--- pkgs/tools/archivers/cromfs/default.nix
+++ pkgs/tools/archivers/cromfs/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, pkgconfig, fuse, perl}:
 
 stdenv.mkDerivation {
-  name = "cromfs-1.5.6.2";
+  name = "cromfs-1.5.7";
   src = fetchurl {
-    url = http://bisqwit.iki.fi/src/arch/cromfs-1.5.6.2.tar.bz2;
-    sha256 = "bbe5db623d3c0d2b92fe877d8c8e22a8f8d84210739313bf691d42c05406464d";
+    url = http://bisqwit.iki.fi/src/arch/cromfs-1.5.7.tar.bz2;
+    sha256 = "7df900cd5d2656e0d2a9cdfb6da7dd194eef2e2a34537f7f69a56dc08a0c0deb";
   };
 
   patchPhase = ''sed -i 's@/bin/bash@/bin/sh@g' configure; set -x'';
