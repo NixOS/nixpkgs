205aa93e924c623ae38a4a4a3cb86015f6c90d28
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-02-03 16:14:23 +0000

    * libxml2 updated to 2.7.3.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=13972

--- pkgs/development/libraries/libxml2/default.nix
+++ pkgs/development/libraries/libxml2/default.nix
@@ -1,22 +1,34 @@
 {stdenv, fetchurl, zlib, python ? null, pythonSupport ? true}:
 
-assert zlib != null;
 assert pythonSupport -> python != null;
 
 stdenv.mkDerivation {
-  name = "libxml2-2.6.32";
-  builder = ./builder.sh;
+  name = "libxml2-2.7.3";
 
   src = fetchurl {
-    url = ftp://xmlsoft.org/libxml2/libxml2-2.6.32.tar.gz;
-    sha256 = "0lsxr0akvp1sx29yh0nmzdhhc15dpa1i5chk40yaxjmgg6w2hi0v";
+    url = ftp://xmlsoft.org/libxml2/libxml2-sources-2.7.3.tar.gz;
+    sha256 = "01bgxgvl0gcx97zmlz9f2ivgbiv86kqbs9l93n2cbxywv1pc4jd5";
   };
 
-  python = if pythonSupport then python else null;
-  inherit pythonSupport zlib;
+  configureFlags = ''
+    ${if pythonSupport then "--with-python=${python}" else ""}
+  '';
 
-  buildInputs =  if pythonSupport then [python] else [];
+  patchPhase = ''
+    sed -e "s^pythondir=.*$^pythondir=$out/lib/python2.4/site-packages^" -i configure
+  '';
+
+  passthru = {inherit pythonSupport;};
+
   propagatedBuildInputs = [zlib];
 
-  postInstall = "ensureDir $out/nix-support; cp ${./setup-hook.sh} $out/nix-support/setup-hook";
+  postInstall = ''
+    ensureDir $out/nix-support
+    cp ${./setup-hook.sh} $out/nix-support/setup-hook
+  '';
+
+  meta = {
+    homepage = http://xmlsoft.org/;
+    description = "A XML parsing library for C";
+  };
 }
