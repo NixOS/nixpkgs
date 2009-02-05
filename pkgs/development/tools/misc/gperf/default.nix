6aa15fcfb056b5d16ed7716c88ba28bc83d25497
Ludovic Courtès <ludo@gnu.org>
2009-02-05 16:49:41 +0000

    GNU gperf 3.0.4.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=13987

--- pkgs/development/tools/misc/gperf/default.nix
+++ pkgs/development/tools/misc/gperf/default.nix
@@ -1,9 +1,32 @@
 {stdenv, fetchurl}:
 
-stdenv.mkDerivation {
-  name = "gperf-3.0.3";
+stdenv.mkDerivation rec {
+  name = "gperf-3.0.4";
+
   src = fetchurl {
-    url = mirror://gnu/gperf/gperf-3.0.3.tar.gz;
-    sha256 = "0mchz9rawhm9sb6rvm05vdlxajs9ycv4907h3j07xqnrr0kpaa33";
+    url = "mirror://gnu/gperf/${name}.tar.gz";
+    sha256 = "0gnnm8iqcl52m8iha3sxrzrl9mcyhg7lfrhhqgdn4zj00ji14wbn";
+  };
+
+  meta = {
+    description = "GNU gperf, a perfect hash function generator";
+
+    longDescription = ''
+      GNU gperf is a perfect hash function generator.  For a given
+      list of strings, it produces a hash function and hash table, in
+      form of C or C++ code, for looking up a value depending on the
+      input string.  The hash function is perfect, which means that
+      the hash table has no collisions, and the hash table lookup
+      needs a single string comparison only.
+
+      GNU gperf is highly customizable.  There are options for
+      generating C or C++ code, for emitting switch statements or
+      nested ifs instead of a hash table, and for tuning the algorithm
+      employed by gperf.
+    '';
+
+    license = "GPLv3+";
+
+    homepage = http://www.gnu.org/software/gperf/;
   };
 }
