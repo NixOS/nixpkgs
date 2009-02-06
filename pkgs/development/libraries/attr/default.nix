c84c92d3e8f96f8feda659a7d14f86e1053b2b3d
Peter Simons <simons@cryp.to>
2009-02-06 09:48:20 +0000

    ftp://oss.sgi.com/projects/xfs/cmd_tars/attr_2.4.41-1.tar.gz no longer exists. There is a newer version on the server, but it cannot be downloaded, apparently because of misconfigured permissions.
    svn path=/nixpkgs/trunk/; revision=13995

--- pkgs/development/libraries/attr/default.nix
+++ pkgs/development/libraries/attr/default.nix
@@ -4,13 +4,15 @@ stdenv.mkDerivation {
   name = "attr-2.4.41";
 
   src = fetchurl {
-    url = ftp://oss.sgi.com/projects/xfs/cmd_tars/attr_2.4.41-1.tar.gz;
+    # The file cannot be downloaded from sgi.com.
+    #url = ftp://oss.sgi.com/projects/xfs/cmd_tars/attr_2.4.41-1.tar.gz;
+    url = "http://gentoo.chem.wisc.edu/gentoo/distfiles/attr_2.4.41-1.tar.gz";
     sha256 = "0dc286g8vr402aca6wg945sdm92bys8a142vrkwx6bkjz4bwz6gp";
   };
 
   buildInputs = [libtool gettext];
-  
+
   configureFlags = "MAKE=make LIBTOOL=libtool MSGFMT=msgfmt MSGMERGE=msgmerge XGETTEXT=xgettext ECHO=echo SED=sed AWK=gawk";
-  
+
   installTargets = "install install-lib install-dev";
 }
