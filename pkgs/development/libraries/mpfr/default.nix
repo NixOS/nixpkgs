8e6f58086d0f735faa0d04780e558e2b3e03dce6
Ludovic Courtès <ludo@gnu.org>
2009-02-12 15:26:48 +0000

    Update description for MPFR, now a GNU project.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=14040

--- pkgs/development/libraries/mpfr/default.nix
+++ pkgs/development/libraries/mpfr/default.nix
@@ -15,6 +15,20 @@ stdenv.mkDerivation {
 
   meta = {
     homepage = http://www.mpfr.org/;
-    description = "Library for multiple-precision floating-point arithmetic";
+    description = "GNU MPFR, a library for multiple-precision floating-point arithmetic";
+
+    longDescription = ''
+      The GNU MPFR library is a C library for multiple-precision
+      floating-point computations with correct rounding.  MPFR is
+      based on the GMP multiple-precision library.
+
+      The main goal of MPFR is to provide a library for
+      multiple-precision floating-point computation which is both
+      efficient and has a well-defined semantics.  It copies the good
+      ideas from the ANSI/IEEE-754 standard for double-precision
+      floating-point arithmetic (53-bit mantissa).
+    '';
+
+    license = "LGPLv2+";
   };
 }
