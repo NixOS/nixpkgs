498be42912a2fc31af19e15a602aa9acc4a0702f
Marc Weber <marco-oweber@gmx.de>
2009-02-12 20:28:01 +0000

    version bump: wireshark-1.1.2, libpcap-1.0.0
    svn path=/nixpkgs/trunk/; revision=14054

--- pkgs/development/libraries/libpcap/default.nix
+++ pkgs/development/libraries/libpcap/default.nix
@@ -1,16 +1,23 @@
 {stdenv, fetchurl, flex, bison}:
 
 stdenv.mkDerivation rec {
-  name = "libpcap-0.9.4";
+  name = "libpcap-1.0.0";
   src = fetchurl {
       url = [
        "mirror://tcpdump/release/${name}.tar.gz"
        "http://www.sfr-fresh.com/unix/misc/${name}.tar.gz"
       ];
-      sha256 = "0q0cnn607kfa4y4rbz3glg5lfr8r08s8l08w8fwrr3d6njjzd71p";
+      sha256 = "1h3kmj485qz1i08xs4sc3a0bmhs1rvq0h7gycs7paap2szhw8552";
   };
   buildInputs = [flex bison];
-  configureFlags = "
-    ${if stdenv.system == "i686-linux" then "--with-pcap=linux" else ""}
-  ";
+  configureFlags = [
+      "${if stdenv.system == "i686-linux" then "--with-pcap=linux" else ""}"
+       "--with-pcap=linux"
+  ];
+
+  preInstall = ''ensureDir $out/bin'';
+  patches = if stdenv.system == "i686-linux"
+      then []
+      else [ ./libpcap_amd64.patch ];
+
 }
