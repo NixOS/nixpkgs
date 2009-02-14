394be4b2e16be623da76729588fd201be6762391
Marc Weber <marco-oweber@gmx.de>
2009-02-14 23:44:08 +0000

    parted version bump to 1.8.8 and passing --disable-Werror
    to get rid of compile time warnings causing build failure
    
    svn path=/nixpkgs/trunk/; revision=14082

--- pkgs/tools/misc/parted/default.nix
+++ pkgs/tools/misc/parted/default.nix
@@ -1,12 +1,22 @@
 {stdenv, fetchurl, e2fsprogs, readline}:
 
 stdenv.mkDerivation {
-  name = "parted-1.8.7";
+  name = "parted-1.8.8";
   src = fetchurl {
-    url = mirror://gnu/parted/parted-1.8.7.tar.bz2;
-    sha256 = "0njabfinn1kbkdx80gayqanpammnl28pklli34bymhx1sxn82kk3";
+    url = mirror://gnu/parted/parted-1.8.8.tar.bz2;
+    sha256 = "1sn5qcdi4pvxnxz8ryh5p52qmqd72qbk0d0a65pksxf7khd83kfz";
   };
   buildInputs = [e2fsprogs readline];
 
+  preConfigure=''
+    export CFLAGS=-fgnu89-inline
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -fgnu89-inline"
+  '';
   configureFlags = "--without-readline";
+
+  meta = {
+    description = "industrial-strength package for creating, destroying, resizing, checking and copying partitions";
+    homepage = http://www.gnu.org/software/parted/;
+    license = "GPLv3";
+  };
 }
