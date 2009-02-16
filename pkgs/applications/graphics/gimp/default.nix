1ff26614144d103dfb31dcaaf758d977640b6473
Armijn Hemel <armijn@gpl-violations.org>
2009-02-16 21:28:00 +0000

    version bump
    svn path=/nixpkgs/trunk/; revision=14088

--- pkgs/applications/graphics/gimp/default.nix
+++ pkgs/applications/graphics/gimp/default.nix
@@ -4,11 +4,11 @@
 }:
 
 stdenv.mkDerivation {
-  name = "gimp-2.6.4";
+  name = "gimp-2.6.5";
   
   src = fetchurl {
-    url = ftp://ftp.gtk.org/pub/gimp/v2.6/gimp-2.6.4.tar.bz2;
-    sha256 = "1qlpszdaskh575xjibkxvvqxmgp8j7v1i1z2dnzz6gazni2pmll6";
+    url = ftp://ftp.gtk.org/pub/gimp/v2.6/gimp-2.6.5.tar.bz2;
+    sha256 = "08g5rc383ijcdfqr9ybrn6qazxqqfq0pndknsgh25z19bhghh2b2";
   };
   
   buildInputs = [
