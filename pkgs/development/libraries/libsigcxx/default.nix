3c6ef4bc75ca2b70cd35ed80a0d5278899f32c4c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-02-17 16:09:58 +0000

    * libsigc++: updated to the latest version to make it build on GCC 4.3.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=14103

--- pkgs/development/libraries/libsigcxx/default.nix
+++ pkgs/development/libraries/libsigcxx/default.nix
@@ -1,12 +1,17 @@
-{ stdenv, fetchurl, pkgconfig}:
+{stdenv, fetchurl, pkgconfig}:
 
-stdenv.mkDerivation {
-  name = "libsigc++-2.0.16";
+stdenv.mkDerivation rec {
+  name = "libsigc++-2.2.3";
+  
   src = fetchurl {
-    url = http://ftp.gnome.org/pub/GNOME/sources/libsigc++/2.0/libsigc++-2.0.16.tar.gz;
-    md5 = "49b8c091b1be84d9f9801c4c81cd98b8";
+    url = "http://ftp.gnome.org/pub/GNOME/sources/libsigc++/2.2/${name}.tar.bz2";
+    sha256 = "0hjh7834mbp2n5qnc7n1r3l70j9g06ibv7kbmhix9b101w6ypnak";
   };
 
   buildInputs = [pkgconfig];
-}
 
+  meta = {
+    homepage = http://libsigc.sourceforge.net/;
+    description = "A typesafe callback system for standard C++";
+  };
+}
