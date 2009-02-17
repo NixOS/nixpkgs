7ba68d934f8f9a9a967791b5f33e415dc4d8415d
Pjotr Prins <pjotr.public01@thebird.nl>
2009-02-17 08:23:21 +0000

    Added note on package status for 64 bits
    svn path=/nixpkgs/trunk/; revision=14090

--- pkgs/applications/science/biology/ncbi-tools/default.nix
+++ pkgs/applications/science/biology/ncbi-tools/default.nix
@@ -1,6 +1,11 @@
 {stdenv, fetchurl, cpio}:
 
-# Note: you may want the older C-libs at ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools++/2008/Mar_17_2008/NCBI_C_Toolkit/ncbi_c--Mar_17_2008.tar.gz
+# The NCBI package only builds on 32bits - on 64bits it breaks because
+# of position dependent code. Debian packagers have written replacement
+# make files(!). Either we use these, or negotiate a version which can
+# be pushed upstream to NCBI.
+#
+# Another note: you may want the older and deprecated C-libs at ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools++/2008/Mar_17_2008/NCBI_C_Toolkit/ncbi_c--Mar_17_2008.tar.gz
 
 stdenv.mkDerivation rec {
   name = "ncbi_tools";
