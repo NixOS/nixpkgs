1fadce397e5f14cb5197d4ebe12d64054b275b80
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-02-18 10:38:51 +0000

    * slim updated to 1.3.1.  This version has PAM support built-in, but it has   a bug that prevents logins when using pam_unix2.  So I've fixed it.
    svn path=/nixpkgs/trunk/; revision=14108

--- pkgs/applications/display-managers/slim/default.nix
+++ pkgs/applications/display-managers/slim/default.nix
@@ -1,25 +1,33 @@
 {stdenv, fetchurl, x11, libjpeg, libpng, libXmu, freetype, pam}:
 
-stdenv.mkDerivation {
-  name = "slim-1.2.6";
+stdenv.mkDerivation rec {
+  name = "slim-1.3.1";
+
   src = fetchurl {
-    url = http://download.berlios.de/slim/slim-1.2.6.tar.gz;
-    sha256 = "0plcmm955rnv67sx67ka6dccanr4rfzwzvsj6lnr8kqdip4522jg";
+    url = "http://download.berlios.de/slim/${name}.tar.gz";
+    sha256 = "0xqgzvg6h1bd29140mcgg9r16vcmskz7zmym7i7jlz7x9c1a9mxc";
   };
+
   patches = [
     # Allow the paths of the configuration file and theme directory to
     # be set at runtime.
     ./runtime-paths.patch
-    # PAM support from
-    # http://developer.berlios.de/patch/?func=detailpatch&patch_id=1979&group_id=2663
+
+    # Fix a bug in slim's PAM support: the "resp" argument to the
+    # conversation function is a pointer to a pointer to an array of
+    # pam_response structures, not a pointer to an array of pointers to
+    # pam_response structures.  Of course C can't tell the difference...
     ./pam.patch
   ];
+
   buildInputs = [x11 libjpeg libpng libXmu freetype pam];
+
   NIX_CFLAGS_COMPILE = "-I${freetype}/include/freetype2";
-  preBuild = "
+
+  preBuild = ''
     substituteInPlace Makefile --replace /usr /no-such-path
-    makeFlagsArray=(CC=gcc CXX=g++ PREFIX=$out MANDIR=$out/share/man CFGDIR=$out/etc)
-  ";
+    makeFlagsArray=(CC=gcc CXX=g++ PREFIX=$out MANDIR=$out/share/man CFGDIR=$out/etc USE_PAM=1)
+  '';
 
   meta = {
     homepage = http://slim.berlios.de;
