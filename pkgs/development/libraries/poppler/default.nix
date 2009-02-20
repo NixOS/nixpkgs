d622b7a1f83cdd049e13e5a68f02999b9e1dd88b
Ludovic Courtès <ludo@gnu.org>
2009-02-20 23:43:29 +0000

    Poppler 0.10.4.
    svn path=/nixpkgs/trunk/; revision=14149

--- pkgs/development/libraries/poppler/default.nix
+++ pkgs/development/libraries/poppler/default.nix
@@ -5,34 +5,34 @@
 assert qt4Support -> (qt4 != null);
 
 stdenv.mkDerivation rec {
-	name = "poppler-0.8.4";
+  name = "poppler-0.10.4";
 
-	src = fetchurl {
-		url = "http://poppler.freedesktop.org/${name}.tar.gz";
-		sha256 = "0yi590vgqwjqmqspflxycbnfxjdcwa1fx9ark3diav3yn105gga5";
-	};
+  src = fetchurl {
+    url = "http://poppler.freedesktop.org/${name}.tar.gz";
+    sha256 = "15c53cn8ljhas0fbirp9s8sbgmaxpzg697c48zsdnz8zwm797gs1";
+  };
 
-	buildInputs = [pkgconfig zlib glib cairo freetype fontconfig libjpeg gtk]
+  buildInputs = [pkgconfig zlib glib cairo freetype fontconfig libjpeg gtk]
     ++ (if qt4Support then [qt4] else []);
 
-	configureFlags = "--enable-shared --disable-static --enable-exceptions
-	--enable-cairo --enable-splash --enable-poppler-glib --enable-zlib "
-  + (if qt4Support then "--enable-qt-poppler" else "--disable-qt-poppler");
+  configureFlags = "--enable-shared --disable-static --enable-exceptions
+    --enable-cairo --enable-splash --enable-poppler-glib --enable-zlib "
+    + (if qt4Support then "--enable-qt-poppler" else "--disable-qt-poppler");
 
-	patches = [ ./GDir-const.patch
+  patches = [ ./GDir-const.patch ./use_exceptions.patch ];
 
-              # XXX: This patch no longer applies, what was the point of it?
-              # ./datadir_env.patch
+  preConfigure = "sed -e '/jpeg_incdirs/s@/usr@${libjpeg}@' -i configure";
 
-              ./use_exceptions.patch ];
-
-	preConfigure = "sed -e '/jpeg_incdirs/s@/usr@${libjpeg}@' -i configure";
-
-#  doCheck = true;
+  doCheck = true;
 
   meta = {
     homepage = http://poppler.freedesktop.org/;
     description = "Poppler, a PDF rendering library";
+
+    longDescription = ''
+      Poppler is a PDF rendering library based on the xpdf-3.0 code base.
+    '';
+
     license = "GPLv2";
   };
 }
