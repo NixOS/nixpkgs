3157e4e829838f12494b15338e24e2f9d7dda384
Michael Raskin <7c6f434c@mail.ru>
2009-02-21 18:58:56 +0000

    Extreme Tux Racer
    svn path=/nixpkgs/trunk/; revision=14152

---
+++ pkgs/games/extremetuxracer/default.nix
@@ -0,0 +1,31 @@
+a :  
+let 
+  fetchurl = a.fetchurl;
+
+  version = a.lib.getAttr ["version"] "0.5beta" a; 
+  buildInputs = with a; [
+    mesa libX11 xproto tcl freeglut
+    SDL SDL_mixer libXi inputproto
+    libXmu libXext xextproto libXt libSM libICE
+    libpng pkgconfig gettext intltool
+  ];
+in
+rec {
+  src = fetchurl {
+    url = "http://downloads.sourceforge.net/extremetuxracer/extremetuxracer-${version}.tar.gz";
+    sha256 = "04d99fsfna5mc9apjxsiyw0zgnswy33kwmm1s9d03ihw6rba2zxs";
+  };
+
+  inherit buildInputs;
+  configureFlags = [
+  		"--with-tcl=${a.tcl}/lib"
+  	];
+
+  /* doConfigure should be removed if not needed */
+  phaseNames = ["doConfigure" "doMakeInstall"];
+
+  name = "extremetuxracer-" + version;
+  meta = {
+    description = "ExtremeTuxRacer - Tux lies on his belly and accelerates down ice slopes..";
+  };
+}
