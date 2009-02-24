cb40899b8d4b4ca096bcdf9798b665b32dc65ea2
Sander van der Burg <s.vanderburg@tudelft.nl>
2009-02-24 13:40:11 +0000

    Fixed compilation bug of kdepim with qgpgme
    svn path=/nixpkgs/trunk/; revision=14226

--- pkgs/desktops/kde-4.2/pim/default.nix
+++ pkgs/desktops/kde-4.2/pim/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
   };
   CMAKE_PREFIX_PATH=kdepimlibs;
   builder = ./builder.sh;  
-  buildInputs = [ cmake qt4 perl boost gpgme libassuan libgpgerror libxslt
+  buildInputs = [ cmake qt4 perl boost gpgme stdenv.gcc.libc libassuan libgpgerror libxslt
                   shared_mime_info libXScrnSaver
                   kdelibs kdepimlibs automoc4 phonon akonadi strigi soprano qca2 ];
 }
