f468a5597a6a3e0c15f1f441ca9637459223396b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-03-03 10:42:51 +0000

    * Install a libbz2.so symlink.  Otherwise, programs that link against   -lbz2 will get libbz2.a, which is generally not what we want.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=14307

--- pkgs/tools/compression/bzip2/builder.sh
+++ pkgs/tools/compression/bzip2/builder.sh
@@ -3,20 +3,18 @@ installFlags="PREFIX=$out"
 
 if test -n "$sharedLibrary"; then
 
-    preBuild=preBuild
     preBuild() {
         make -f Makefile-libbz2_so
     }
 
-    preInstall=preInstall
     preInstall() {
         ensureDir $out/lib
         mv libbz2.so* $out/lib
+        ln -s libbz2.so.1.0 $out/lib/libbz2.so
     }
     
 fi
 
-postInstall=postInstall
 postInstall() {
     rm $out/bin/bunzip2* $out/bin/bzcat*
     ln -s bzip2 $out/bin/bunzip2
