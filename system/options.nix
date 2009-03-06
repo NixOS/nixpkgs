a86ae923d7c660cba592c58cec38c32faa2f545b
Marc Weber <marco-oweber@gmx.de>
2009-03-06 12:25:35 +0000

    Convert "nixos-environment"
    in particular: (override) nix, extraPackages, checkConfigurationOptions
    
    svn path=/nixos/branches/fix-style/; revision=14360

--- system/options.nix
+++ system/options.nix
@@ -2116,64 +2116,6 @@ in
   };
 
 
-  environment = {
-
-    pathsToLink = mkOption {
-      default = ["/bin" "/sbin" "/lib" "/share" "/man" "/info" "/etc"];
-      example = ["/"];
-      description = "
-        Lists directories to be symlinked in `/var/run/current-system/sw'.
-      ";
-    };
-
-    cleanStart = mkOption {
-      default = false;
-      example = true;
-      description = "
-        There are some times when you want really small system for specific 
-        purpose and do not want default package list. Setting 
-        <varname>cleanStart</varname> to <literal>true</literal> allows you 
-        to create a system with empty path - only extraPackages will be 
-        included.
-      ";
-    };
-
-    extraPackages = mkOption {
-      default = [];
-      example = [pkgs.firefox pkgs.thunderbird];
-      description = "
-        This option allows you to add additional packages to the system
-        path.  These packages are automatically available to all users,
-        and they are automatically updated every time you rebuild the
-        system configuration.  (The latter is the main difference with
-        installing them in the default profile,
-        <filename>/nix/var/nix/profiles/default</filename>.  The value
-        of this option must be a function that returns a list of
-        packages.  The function will be called with the Nix Packages
-        collection as its argument for convenience.
-      ";
-    };
-
-    nix = mkOption {
-      default = pkgs.nixUnstable;
-      example = pkgs.nixCustomFun /root/nix.tar.gz;
-      merge = mergeOneOption;
-      description = "
-        Use non-default Nix easily. Be careful, though, not to break everything.
-      ";
-    };
-
-    checkConfigurationOptions = mkOption {
-      default = true;
-      example = false;
-      description = "
-        If all configuration options must be checked. Non-existing options fail build.
-      ";
-    };
-
-  };
-
-    
   nesting = {
     children = mkOption {
       default = [];
@@ -2214,6 +2156,8 @@ in
 
     # environment
     (import ../etc/default.nix)
+
+    (import ../system/nixos-environment.nix)
 
     # users
     (import ../system/users-groups.nix)
