e97aa4bb014d041ed528cf5e25d25acb564f23bc
Ludovic Courtès <ludo@gnu.org>
2009-03-07 11:47:40 +0000

    GNU libidn 1.13.
    svn path=/nixpkgs/trunk/; revision=14434

--- pkgs/development/libraries/libidn/default.nix
+++ pkgs/development/libraries/libidn/default.nix
@@ -1,15 +1,37 @@
-args: with args;
+{ fetchurl, stdenv }:
 
-stdenv.mkDerivation {
-	name = "libidn-1.2";
-	src = fetchurl {
-		url = mirror://gnu/libidn/libidn-1.2.tar.gz;
-		sha256 = "0cip97xskrsfp6v1v966jb921srl1s65a5d8s0l73s85yww55n73";
-	};
+stdenv.mkDerivation rec {
+  name = "libidn-1.13";
 
-	meta = {
-		homepage = http://www.gnu.org/software/libidn;
-		description = "GNU Libidn library for internationalized domain names";
-		license = "LGPL";
-	};
+  src = fetchurl {
+    url = "mirror://gnu/libidn/${name}.tar.gz";
+    sha256 = "07p7cnmfnq7ds8a56iqmxps46bzznn92xxpdlnnp3n6pwcnidmn7";
+  };
+
+  configureFlags = "--disable-static";
+
+  doCheck = true;
+
+  meta = {
+    homepage = http://www.gnu.org/software/libidn/;
+    description = "GNU Libidn library for internationalized domain names";
+
+    longDescription = ''
+      GNU Libidn is a fully documented implementation of the
+      Stringprep, Punycode and IDNA specifications.  Libidn's purpose
+      is to encode and decode internationalized domain names.  The
+      native C, C\# and Java libraries are available under the GNU
+      Lesser General Public License version 2.1 or later.
+
+      The library contains a generic Stringprep implementation.
+      Profiles for Nameprep, iSCSI, SASL, XMPP and Kerberos V5 are
+      included.  Punycode and ASCII Compatible Encoding (ACE) via IDNA
+      are supported.  A mechanism to define Top-Level Domain (TLD)
+      specific validation tables, and to compare strings against those
+      tables, is included.  Default tables for some TLDs are also
+      included.
+    '';
+
+    license = "LGPLv2+";
+  };
 }
