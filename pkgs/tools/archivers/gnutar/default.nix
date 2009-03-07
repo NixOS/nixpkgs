01a662d3e089b74982f84457a90c89cff2e338b5
Ludovic Courtès <ludo@gnu.org>
2009-03-07 11:35:18 +0000

    GNU Tar 1.22 (untested).
    svn path=/nixpkgs/branches/stdenv-updates/; revision=14433

--- pkgs/tools/archivers/gnutar/default.nix
+++ pkgs/tools/archivers/gnutar/default.nix
@@ -1,17 +1,34 @@
 {stdenv, fetchurl}:
 
-stdenv.mkDerivation {
-  name = "gnutar-1.21";
+stdenv.mkDerivation rec {
+  name = "gnutar-1.22";
   
   src = fetchurl {
-    url = mirror://gnu/tar/tar-1.21.tar.bz2;
-    sha256 = "0l5kmq3s6rbps6h62li5a1yycchaa2mnhv8b8qlak90w0z970v6w";
+    url = "mirror://gnu/tar/${name}.tar.bz2";
+    sha256 = "0kdaadflxa6wznbbrp0xlxk9926hrr4yg7wr6m98ygvs35zvdvrw";
   };
   
   patches = [./implausible.patch];
 
   meta = {
-    homepage = http://www.gnu.org/software/grep/;
-    description = "GNU implementation of the tar archiver";
+    homepage = http://www.gnu.org/software/tar/;
+    description = "GNU implementation of the `tar' archiver";
+
+    longDescription = ''
+      The Tar program provides the ability to create tar archives, as
+      well as various other kinds of manipulation.  For example, you
+      can use Tar on previously created archives to extract files, to
+      store additional files, or to update or list files which were
+      already stored.
+
+      Initially, tar archives were used to store files conveniently on
+      magnetic tape.  The name "Tar" comes from this use; it stands
+      for tape archiver.  Despite the utility's name, Tar can direct
+      its output to available devices, files, or other programs (using
+      pipes), it can even access remote devices or files (as
+      archives).
+    '';
+
+    license = "GPLv3+";
   };
 }
