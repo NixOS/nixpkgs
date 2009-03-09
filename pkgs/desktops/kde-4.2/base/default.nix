b3c8857f605d743b6b6a82199183bc26465f671c
Sander van der Burg <s.vanderburg@tudelft.nl>
2009-03-09 15:43:05 +0000

    Fixed detection of PCI utils
    svn path=/nixpkgs/trunk/; revision=14467

--- pkgs/desktops/kde-4.2/base/default.nix
+++ pkgs/desktops/kde-4.2/base/default.nix
@@ -7,6 +7,6 @@ stdenv.mkDerivation {
     url = mirror://kde/stable/4.2.1/src/kdebase-4.2.1.tar.bz2;
     sha1 = "c500024294a7621d176d26bdabdd138d18ec827d";
   };
-  buildInputs = [ cmake perl qt4 kdelibs pciutils libraw1394
+  buildInputs = [ cmake perl qt4 kdelibs pciutils stdenv.gcc.libc libraw1394
                   automoc4 phonon strigi qimageblitz soprano ];
 }
