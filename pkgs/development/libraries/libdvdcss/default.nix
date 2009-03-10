9ab4720b8581745cdf38f33ec260042e6ec1390b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-03-10 08:06:50 +0000

    * Latest libdvdcss.
    svn path=/nixpkgs/trunk/; revision=14483

--- pkgs/development/libraries/libdvdcss/default.nix
+++ pkgs/development/libraries/libdvdcss/default.nix
@@ -1,9 +1,15 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "libdvdcss-1.2.9";
+  name = "libdvdcss-1.2.10";
+  
   src = fetchurl {
-    url = http://download.videolan.org/pub/libdvdcss/1.2.9/libdvdcss-1.2.9.tar.bz2;
-    md5 = "553383d898826c285afb2ee453b07868";
+    url = http://download.videolan.org/pub/libdvdcss/1.2.10/libdvdcss-1.2.10.tar.bz2;
+    sha256 = "0812zxg4b6yjkckzwdzfzb4jnffykr9567f9v29barmb2d8ag513";
+  };
+
+  meta = {
+    homepage = http://www.videolan.org/developers/libdvdcss.html;
+    description = "A library for decrypting DVDs";
   };
 }
