e629ce0e0787fb12cd7e087c8760ea84021ec47b
Sander van der Burg <s.vanderburg@tudelft.nl>
2009-03-10 14:41:50 +0000

    Updated openbabel to 2.2.1 and fixed style so that kdeedu can use it. Enabled ocaml support in kdeedu
    svn path=/nixpkgs/trunk/; revision=14494

--- pkgs/development/libraries/openbabel/default.nix
+++ pkgs/development/libraries/openbabel/default.nix
@@ -1,13 +1,14 @@
-args: with args;
+{stdenv, fetchurl, zlib, libxml2}:
+
 stdenv.mkDerivation {
-	name = "openbabel-2.1.1";
-	src = fetchurl { 
-		url = mirror://sf/openbabel/openbabel-2.1.1.tar.gz;
-		sha256 = "1rgvci796a7bmc49ih26ma7c248d32w6drs3cwljpjk0dllsqdif";
-	};
+  name = "openbabel-2.2.1";
+  src = fetchurl { 
+    url = mirror://sf/openbabel/openbabel-2.2.1.tar.gz;
+    sha256 = "822345d70778de1d2d9afe65a659f1719b8ca300066fb1fa2f473bc97c457e80";
+  };
 # TODO : perl & python bindings;
 # TODO : wxGTK: I have no time to compile
 # TODO : separate lib and apps
-	buildInputs = [zlib libxml2];
-	configureFlags = "--enable-shared --disable-static";
+  buildInputs = [ zlib libxml2 ];
+  configureFlags = "--enable-shared --disable-static";
 }
