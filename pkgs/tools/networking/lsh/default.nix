d9cfe67b9fa4c6e967882d3218ef72815d7dffc7
Ludovic Courtès <ludo@gnu.org>
2009-03-15 14:11:21 +0000

    lsh: Change the PAM service name in accordance with NixOS (had forgotten to commit it).
    svn path=/nixpkgs/trunk/; revision=14542

--- pkgs/tools/networking/lsh/default.nix
+++ pkgs/tools/networking/lsh/default.nix
@@ -7,11 +7,20 @@ stdenv.mkDerivation {
     sha256 = "614b9d63e13ad3e162c82b6405d1f67713fc622a8bc11337e72949d613713091";
   };
 
+  patches = [ ./pam-service-name.patch ];
+
   buildInputs = [gperf guile gmp zlib liboop gnum4 pam];
 
   meta = {
     description = "GNU lsh, a GPL'd implementation of the SSH protocol";
+
+    longDescription = ''
+      lsh is a free implementation (in the GNU sense) of the ssh
+      version 2 protocol, currently being standardised by the IETF
+      SECSH working group.
+    '';
+    
     homepage = http://www.lysator.liu.se/~nisse/lsh/;
-    license = "GPL";
+    license = "GPLv2+";
   };
 }
