32154f3dac2cd31a0e779140f38e94c0f818ed2e
Michael Raskin <7c6f434c@mail.ru>
2009-03-16 21:30:43 +0000

    Updated monotone
    svn path=/nixpkgs/trunk/; revision=14565

--- pkgs/applications/version-management/monotone/default.nix
+++ pkgs/applications/version-management/monotone/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, boost, zlib}:
 
 stdenv.mkDerivation {
-  name = "monotone-0.40";
+  name = "monotone-0.42";
   src = fetchurl {
-    url = http://monotone.ca/downloads/0.40/monotone-0.40.tar.gz;
-    sha256 = "0zs1zxvdn0m27w1830qbbh5vqk948hkpdkxwnlghszcbzz49151h";
+    url = http://monotone.ca/downloads/0.42/monotone-0.42.tar.gz;
+    sha256 = "0i6srfx0ps8hlgdbn0y7iy9gi33a7vpiwdm5rhxjxgvhn5j9svdr";
   };
   buildInputs = [boost zlib];
 }
