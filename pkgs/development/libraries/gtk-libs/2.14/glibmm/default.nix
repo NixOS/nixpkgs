edec8307f355ebcc7826ff768fa8bc4743497f66
Ludovic Courtès <ludo@gnu.org>
2009-03-25 21:57:17 +0000

    Glibmm: Fix URL.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=14717

--- pkgs/development/libraries/gtk-libs/2.14/glibmm/default.nix
+++ pkgs/development/libraries/gtk-libs/2.14/glibmm/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "glibmm-2.18.1";
 
   src = fetchurl {
-    url = "mirror://gnome/GNOME/sources/glibmm/2.18/${name}.tar.bz2";
+    url = "mirror://gnome/sources/glibmm/2.18/${name}.tar.bz2";
     sha256 = "0jg65hv6pwxqk4fabsjjz2zwn5hb6rgy3szj956avliarbliyr3r";
   };
 
