51760b17b3dffa4c1ec6e0abfd87feb8154bb076
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-03-25 17:41:02 +0000

    * PCRE 7.8.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=14710

--- pkgs/development/libraries/pcre/default.nix
+++ pkgs/development/libraries/pcre/default.nix
@@ -1,13 +1,20 @@
 {stdenv, fetchurl, unicodeSupport ? false, cplusplusSupport ? true}:
 
 stdenv.mkDerivation {
-  name = "pcre-7.4";
+  name = "pcre-7.8";
+  
   src = fetchurl {
-    url = mirror://sourceforge/pcre/pcre-7.4.tar.bz2;
-    sha256 = "1rdks2h5f3p2d71c4jnxaic1c9gmgsfky80djnafcdbdrhzkiyx5";
+    url = mirror://sourceforge/pcre/pcre-7.8.tar.bz2;
+    sha256 = "1zsqk352mx2zklf9bgpg9d88ckfdssbbbiyslhrycfckw8m3qpvr";
   };
+  
   configureFlags = ''
     ${if unicodeSupport then "--enable-unicode-properties --enable-shared --disable-static" else ""}
     ${if !cplusplusSupport then "--disable-cpp" else ""}
   '';
+
+  meta = {
+    homepage = http://www.pcre.org/;
+    description = "A library for Perl Compatible Regular Expressions";
+  };
 }
