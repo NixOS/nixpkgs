8bbdba6af3e01a0c1dd38bd2152ff9ca33ba1141
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-03-25 17:39:02 +0000

    * mpfr updated to 2.4.1.  Removed the INRIA link since the tarball   there has a different hash (!).
    svn path=/nixpkgs/branches/stdenv-updates/; revision=14709

--- pkgs/development/libraries/mpfr/default.nix
+++ pkgs/development/libraries/mpfr/default.nix
@@ -1,13 +1,10 @@
 {stdenv, fetchurl, gmp}:
 
 stdenv.mkDerivation {
-  name = "mpfr-2.4.0";
+  name = "mpfr-2.4.1";
 
   src = fetchurl {
-    urls = [
-      http://gforge.inria.fr/frs/download.php/16015/mpfr-2.4.0.tar.bz2
-      http://www.mpfr.org/mpfr-2.4.0/mpfr-2.4.0.tar.bz2
-    ];
+    url = http://www.mpfr.org/mpfr-2.4.1/mpfr-2.4.1.tar.bz2;
     sha256 = "17ajw12jfs721igsr6ny3wxz9j1nm618iplc82wyzins5gn52gdy";
   };
 
