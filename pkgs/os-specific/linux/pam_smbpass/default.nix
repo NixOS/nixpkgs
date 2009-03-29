2dce53aa55a1e8a0f668cf4aff85efaaa2b0bcec
Wouter den Breejen <uu@denbreejen.net>
2009-03-29 20:55:05 +0000

    pam-smbpass
    svn path=/nixpkgs/trunk/; revision=14763

--- pkgs/os-specific/linux/pam_smbpass/default.nix
+++ pkgs/os-specific/linux/pam_smbpass/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation {
   name = "pam_smbpass-2.2.8a";
    
-  src = ./clr9p5ar0nc76ls15ifgf87wmlp3px2n-pam-smbpass-2.2.8a.tar;
+  src = ./pam-smbpass-2.2.8a.tar;
 
   buildInputs = [pam libxcrypt];
 }
