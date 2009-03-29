857a0a378427b6d01abed24074a6d9c4eb41590c
Wouter den Breejen <uu@denbreejen.net>
2009-03-29 20:56:32 +0000

    pam-smbpass
    svn path=/nixpkgs/trunk/; revision=14764

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7308,6 +7308,10 @@ let
     inherit stdenv fetchurl pam libxcrypt;
   };
 
+  pam_smbpass = import ../os-specific/linux/pam_smbpass {
+    inherit stdenv fetchurl pam libxcrypt;
+  };
+
   pcmciaUtils = composedArgsAndFun (import ../os-specific/linux/pcmciautils) {
     inherit stdenv fetchurl udev yacc flex;
     inherit sysfsutils module_init_tools;
