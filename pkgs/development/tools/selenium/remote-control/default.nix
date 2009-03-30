93f9b0f45e54c0d951c2387a98996ddd8d7b5338
Marc Weber <marco-oweber@gmx.de>
2009-03-30 14:51:30 +0000

    updating selenium to SNAPSHOT version
    to run firefox from selenium remote control you either have to use the
    binary, write a binary wrapper or use latest version (which is what I've
    done now)
    
    svn path=/nixpkgs/trunk/; revision=14786

--- pkgs/development/tools/selenium/remote-control/default.nix
+++ pkgs/development/tools/selenium/remote-control/default.nix
@@ -1,13 +1,18 @@
 args: with args;
-let version = "1.0-beta-2";
+# let version = "1.0-beta-2";
+let version = "1.0-SNAPSHOT-standalone";
 in stdenv.mkDerivation {
+    /*
+
+    Use this if there is another release..
+
     name = "selenium-remote-control-${version}-dist";
     src = fetchurl {
       url = "http://release.seleniumhq.org/selenium-remote-control/${version}/selenium-remote-control-${version}-dist.zip";
       sha256 = "0ciyfqvnv0117l2rhw9dclv85mcf3czpimvybj38v3syl7m7yk41";
     };
-    phases = "unpackPhase buildPhase";
     buildInputs = [unzip];
+    phases = "unpackPhase buildPhase";
     buildPhase = ''
       ensureDir $out/{bin,lib}
       mv * $out/lib
@@ -16,7 +21,27 @@ in stdenv.mkDerivation {
       #!/bin/sh
       exec ${jre}/bin/java -jar $out/lib/selenium-server-${version}/selenium-server.jar "\$@"
       EOF
-      echo chmod +x "$bin"
+      chmod +x "$bin" 
+    '';
+    */
+
+    # this snapshot version starts a firefox from a script file. It only issues a warning about it
+    # you still have to pass -DfirefoxDefaultPath=/home/marc/.nix-profile/bin/firefox or such..
+    name = "selenium-remote-control-${version}-dist";
+    # this dist file has been created using  mvn package  -Dmaven.test.skip=true based on svn rev 2639 
+    src = fetchurl {
+      url = "http://mawercer.de/~nix/selenium-server-1.0-SNAPSHOT-standalone.jar";
+      sha256 = "1g59g5y8lyjjj3fda30cm9gmjncdz70slrp03rqmnrkzp7z6khnc";
+    };
+    phases = "buildPhase";
+    buildPhase = ''
+      ensureDir $out/{bin,lib}
+      cp $src $out/lib/
+      bin="$out/bin/selenium-remote-control"
+      cat >> "$bin" << EOF
+      #!/bin/sh
+      exec ${jre}/bin/java -jar "$out/lib/$(basename $src)" "\$@"
+      EOF
       chmod +x "$bin" 
     '';
 }
