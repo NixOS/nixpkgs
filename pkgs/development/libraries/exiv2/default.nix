5a03361f2dac7ce2786b087893a592c0fff590ff
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-04-01 09:04:01 +0000

    * Updated exiv2, the old one doesn't compile on GCC 4.3.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=14819

--- pkgs/development/libraries/exiv2/default.nix
+++ pkgs/development/libraries/exiv2/default.nix
@@ -1,11 +1,19 @@
-args: with args;
+{stdenv, fetchurl, zlib}:
 
-stdenv.mkDerivation {
-	name = "exiv2-0.15";
-	src = fetchurl {
-		url = http://www.exiv2.org/exiv2-0.15.tar.gz;
-		sha256 = "0gjak1849rbw5azz4ggckmcw0r40wlr3hgwrf4s0c23k27lq4bdp";
-	};
-	buildInputs = [zlib];
-	configureFlags = "--with-zlib=${zlib} --enable-shared --disable-static";
+stdenv.mkDerivation rec {
+  name = "exiv2-0.18";
+  
+  src = fetchurl {
+    url = "http://www.exiv2.org/${name}.tar.gz";
+    sha256 = "1kg4bdlcqqhw9gcfs68i55sz4hvlf94xxxmqb255hhvhfj692rz5";
+  };
+  
+  buildInputs = [zlib];
+  
+  configureFlags = "--with-zlib=${zlib} --enable-shared --disable-static --disable-xmp";
+
+  meta = {
+    homepage = http://www.exiv2.org/;
+    description = "A library and command-line utility to manage image metadata";
+  };
 }
