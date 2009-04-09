bec0e32034493c6bbf08d70a9342d09565e098a9
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-04-09 14:11:55 +0000

    * Add the programs provided by the acl, attr and libcap libraries to   the system path.  (They were in the closure already anyway so it   doesn't cost us anything.)
    svn path=/nixos/trunk/; revision=14973

--- system/system.nix
+++ system/system.nix
@@ -182,6 +182,8 @@ rec {
     setuidWrapper
   ]
   ++ pkgs.lib.optionals (!config.environment.cleanStart) [
+    pkgs.acl
+    pkgs.attr
     pkgs.bashInteractive # bash with ncurses support
     pkgs.bzip2
     pkgs.coreutils
@@ -197,6 +199,7 @@ rec {
     pkgs.gzip
     pkgs.iputils
     pkgs.less
+    pkgs.libcap
     pkgs.lvm2
     pkgs.man
     pkgs.mdadm
