6a864c50406f2ef0996e29500ebefca80168e321
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-04-15 09:54:55 +0000

    * libgphoto2 updated to 2.4.5.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=15057

--- pkgs/development/libraries/libgphoto2/default.nix
+++ pkgs/development/libraries/libgphoto2/default.nix
@@ -1,16 +1,18 @@
 {stdenv, fetchurl, pkgconfig, libusb, libtool, libexif, libjpeg, gettext}:
 
 stdenv.mkDerivation rec {
-  name = "libgphoto2-2.4.2";
+  name = "libgphoto2-2.4.5";
 
   src = fetchurl {
     url = "mirror://sourceforge/gphoto/${name}.tar.bz2";
-    sha256 = "09jjfh9c7s02dxkwwr8j3kaqffsyiiz7ncbkmdvav1i14xdna6gk";
+    sha256 = "1pipdwjxbjg7y9n5ldz6qlpiiiqyba6jx315277ams5d8jxg2bfk";
   };
   
   buildInputs = [pkgconfig libusb libtool libexif libjpeg gettext];
 
   meta = {
+    homepage = http://www.gphoto.org/proj/libgphoto2/;
+    description = "A library for accessing digital cameras";
     license = "LGPL-2";
   };
 }
