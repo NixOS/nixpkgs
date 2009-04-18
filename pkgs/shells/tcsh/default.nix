306b6e87a5a0e772ad34749ce8744e1bd05b79cd
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2009-04-18 23:31:45 +0000

    Updating tcsh, so its url even works.
    svn path=/nixpkgs/trunk/; revision=15137

--- pkgs/shells/tcsh/default.nix
+++ pkgs/shells/tcsh/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, ncurses}:
 
 stdenv.mkDerivation {
-  name = "tcsh-6.15.00";
+  name = "tcsh-6.16.00";
   src = fetchurl {
-    url = ftp://ftp.funet.fi/pub/unix/shells/tcsh/tcsh-6.15.00.tar.gz;
-    sha256 = "1p5chgvj87m2dv4ci9qf4i81gav0lzr7rkcm320sj62z09ckxa6w";
+    url = ftp://ftp.funet.fi/pub/unix/shells/tcsh/tcsh-6.16.00.tar.gz;
+    sha256 = "1m0p8lqqna3vpf2k4x3hia3rlrz38av67x7hb4qsiq2kfpbbh0vn";
   };
   buildInputs = [ncurses];
 }
