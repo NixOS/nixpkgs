b1e2bb54f2defa7bd5a44f8c4700e3c81a1df500
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-04-20 16:01:57 +0000

    * Doh!  This broke pam_console (and probably system-config-printer).
    svn path=/nixpkgs/branches/stdenv-updates/; revision=15190

--- pkgs/development/tools/misc/automake/setup-hook.sh
+++ pkgs/development/tools/misc/automake/setup-hook.sh
@@ -1,5 +1,5 @@
 addAclocals () {
-    addToSearchPathWithCustomDelimiter ACLOCAL_PATH $1/share/aclocal
+    addToSearchPathWithCustomDelimiter : ACLOCAL_PATH $1/share/aclocal
 }
 
 envHooks=(${envHooks[@]} addAclocals)
