a6b2ffd5f2596f3fc88aece492aa741d17ab4424
Ludovic Courtès <ludo@gnu.org>
2009-04-22 12:11:21 +0000

    GNU Find Utilities 4.4.1.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=15242

--- pkgs/tools/misc/findutils/default.nix
+++ pkgs/tools/misc/findutils/default.nix
@@ -1,11 +1,13 @@
 {stdenv, fetchurl, coreutils}:
 
-stdenv.mkDerivation {
-  name = "findutils-4.2.33";
+stdenv.mkDerivation rec {
+  name = "findutils-4.4.1";
+
   src = fetchurl {
-    url = mirror://gnu/findutils/findutils-4.2.33.tar.gz;
-    sha256 = "0y0gmdc55kknf5438c1da5xsvpch3v800r79rgz5rv6fb90djg41";
+    url = "mirror://gnu/findutils/${name}.tar.gz";
+    sha256 = "0f61phan4q8w5i1lz768q973c1spfqgvc470jc89rpg0gxfvi9bp";
   };
+
   buildInputs = [coreutils];
 
   patches = [ ./findutils-path.patch ./change_echo_path.patch ]
@@ -13,8 +15,27 @@ stdenv.mkDerivation {
     # The locate command probably won't work though.
     ++ stdenv.lib.optional (stdenv ? isDietLibC) ./dietlibc-hack.patch;
 
+  doCheck = true;
+
   meta = {
     homepage = http://www.gnu.org/software/findutils/;
-    description = "The basic directory searching utilities of the GNU operating system";
+    description = "GNU Find Utilities, the basic directory searching utilities of the GNU operating system";
+
+    longDescription = ''
+      The GNU Find Utilities are the basic directory searching
+      utilities of the GNU operating system.  These programs are
+      typically used in conjunction with other programs to provide
+      modular and powerful directory search and file locating
+      capabilities to other commands.
+
+      The tools supplied with this package are:
+
+          * find - search for files in a directory hierarchy;
+          * locate - list files in databases that match a pattern;
+          * updatedb - update a file name database;
+          * xargs - build and execute command lines from standard input.
+    '';
+
+    license = "GPLv3+";
   };
 }
