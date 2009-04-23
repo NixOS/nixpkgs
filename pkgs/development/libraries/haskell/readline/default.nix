2bb2373cf98145d7190896785c59f9eb1a2479d5
Andres Löh <mail@andres-loeh.de>
2009-04-23 17:48:06 +0000

    Try to link readline Haskell package against ncursesw.
    svn path=/nixpkgs/trunk/; revision=15284

--- pkgs/development/libraries/haskell/readline/default.nix
+++ pkgs/development/libraries/haskell/readline/default.nix
@@ -5,6 +5,11 @@ cabal.mkDerivation (self : {
   version = "1.0.1.0";
   sha256 = "07f2f039f32bf18838a4875d0f3caa3ed9436dd52b962b2061f0bb8a3316fa1d";
   propagatedBuildInputs = [ readline ];
+  # experimentally link with ncursesw because ghci can't interpret ld scripts,
+  # and ncurses sometimes seems to be a script pointing to ncursesw
+  postConfigure = ''
+    sed -i -e "/^extra-libraries/ s/ncurses/ncursesw/" readline.buildinfo
+  '';
   meta = {
     description = "An interface to the GNU readline library";
   };
