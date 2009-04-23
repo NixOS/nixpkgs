e13bce06c45275d8d0295389fe254365abae89cb
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-04-23 10:11:02 +0000

    * Latest irssi.  This also fixes --disable-static breakage.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=15264

--- pkgs/applications/networking/irc/irssi/default.nix
+++ pkgs/applications/networking/irc/irssi/default.nix
@@ -1,13 +1,17 @@
 {stdenv, fetchurl, pkgconfig, ncurses, glib, openssl}:
 
-stdenv.mkDerivation {
-  name = "irssi-0.8.12";
+stdenv.mkDerivation rec {
+  name = "irssi-0.8.13";
+  
   src = fetchurl {
-    url = http://irssi.org/files/irssi-0.8.12.tar.bz2;
-    sha256 = "1w7zkfs6j7xdcbqh8x0vf9rk2ps9d6rcgr8fapfjpk09nm5n6ba6";
+    url = "http://irssi.org/files/${name}.tar.bz2";
+    sha256 = "0dfp0lmnw5ndl2a9lj2rc8rg1lylcjrqlrg26h4jj8blhfn42rc9";
   };
+  
   buildInputs = [pkgconfig ncurses glib openssl];
+  
   NIX_LDFLAGS = "-lncurses";
+  
   configureFlags = "--with-proxy --with-ncurses --enable-ssl";
 
   meta = {
