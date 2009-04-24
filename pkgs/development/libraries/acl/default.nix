96443f794ef87b50c603dd21d71a7c1b606810f6
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-04-24 20:27:54 +0000

    * Unfortunately, the Gentoo mirror of the attr library is no more   stable than the SGI source.  So we'll mirror it ourselves.  Thanks   to Matthew Curry for reporting this.
    svn path=/nixpkgs/trunk/; revision=15289

--- pkgs/development/libraries/acl/default.nix
+++ pkgs/development/libraries/acl/default.nix
@@ -4,9 +4,7 @@ stdenv.mkDerivation {
   name = "acl-2.2.47";
 
   src = fetchurl {
-    # The file cannot be downloaded from sgi.com.
-    #url = ftp://oss.sgi.com/projects/xfs/cmd_tars/acl_2.2.47-1.tar.gz;
-    url = "http://gentoo.chem.wisc.edu/gentoo/distfiles/acl_2.2.47-1.tar.gz";
+    url = http://nixos.org/tarballs/acl_2.2.47-1.tar.gz;
     sha256 = "1j39g62fki0iyji9s62slgwdfskpkqy7rmjlqcnmsvsnxbxhc294";
   };
 
--- pkgs/development/libraries/attr/default.nix
+++ pkgs/development/libraries/attr/default.nix
@@ -4,9 +4,7 @@ stdenv.mkDerivation {
   name = "attr-2.4.41";
 
   src = fetchurl {
-    # The file cannot be downloaded from sgi.com.
-    #url = ftp://oss.sgi.com/projects/xfs/cmd_tars/attr_2.4.41-1.tar.gz;
-    url = "http://gentoo.chem.wisc.edu/gentoo/distfiles/attr_2.4.41-1.tar.gz";
+    url = http://nixos.org/tarballs/attr_2.4.41-1.tar.gz;
     sha256 = "0dc286g8vr402aca6wg945sdm92bys8a142vrkwx6bkjz4bwz6gp";
   };
 
