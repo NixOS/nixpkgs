f7454eae9cc9255883dd5afb6beb945fb49737f2
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-04-25 22:36:33 +0000

    * Be able to cope with future changes to Upstart.
    svn path=/nixpkgs/trunk/; revision=15311

--- pkgs/os-specific/linux/upstart/default.nix
+++ pkgs/os-specific/linux/upstart/default.nix
@@ -14,6 +14,16 @@ stdenv.mkDerivation {
   
   preBuild = "export NIX_CFLAGS_COMPILE=\"$NIX_CFLAGS_COMPILE -DSHELL=\\\"$SHELL\\\"\"";
 
+  # The interface version prevents NixOS from switching to an
+  # incompatible Upstart at runtime.  (Switching across reboots is
+  # fine, of course.)  It should be increased whenever Upstart changes
+  # in a backwards-incompatible way.  If the interface version of two
+  # Upstart builds is the same, then we can switch between them at
+  # runtime; otherwise we can't and we need to reboot.
+  passthru = {
+    interfaceVersion = 0;
+  };
+
   meta = {
     homepage = "http://upstart.ubuntu.com/";
     description = "An event-based replacement for the /sbin/init daemon";
