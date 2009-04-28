fa9ceac86273f7fec7deb480b18de6e0efc1cd2c
Marc Weber <marco-oweber@gmx.de>
2009-04-28 13:51:58 +0000

    * Synced with trunk @ 15346
    svn path=/nixos/branches/modular-nixos/; revision=15373

--- upstart-jobs/xserver/default.nix
+++ upstart-jobs/xserver/default.nix
@@ -315,7 +315,7 @@ let
       xorg.xf86inputevdev
     ] 
     ++ getAttr ["modules"] [] videoDriverModules
-    ++ (optional cfg.synaptics.enable ["${pkgs.synaptics}/${xorg.xorgserver}" /*xorg.xf86inputevdev*/]);
+    ++ (optional cfg.synaptics.enable ["${xorg.xf86inputsynaptics}"]);
 
 
   fontsForXServer =
@@ -560,7 +560,7 @@ mkIf cfg.enable {
         }
 
         ${if videoDriver != "nvidia"
-          then "env XORG_DRI_DRIVER_PATH=${pkgs.mesa}/lib/modules/dri"
+          then "env XORG_DRI_DRIVER_PATH=${pkgs.mesa}/lib/dri"
           else ""
         }
 
