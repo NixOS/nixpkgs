621b56e233b534a0a216a83e78247e198a78ed1f
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2009-04-28 10:31:16 +0000

    Radare version bump
    svn path=/nixpkgs/trunk/; revision=15349

--- pkgs/development/tools/analysis/radare/default.nix
+++ pkgs/development/tools/analysis/radare/default.nix
@@ -2,11 +2,11 @@
   python, ruby, libewf, vte, perl}:
 
 stdenv.mkDerivation {
-  name = "radare-1.2.2";
+  name = "radare-1.3";
 
   src = fetchurl {
-    url = http://radare.org/get/radare-1.2.2.tar.gz;
-    sha256 = "0624ic97s1b70ijbr16b33p76mls8rziqwla6bq29l554dh2hfn4";
+    url = http://radare.org/get/radare-1.3.tar.gz;
+    sha256 = "0r2yl24ywyqzi7wn82hr6rkn3dgf9bl9m662hswszx44pd0cxarx";
   };
 
   patches = [ ./lua.patch ];
