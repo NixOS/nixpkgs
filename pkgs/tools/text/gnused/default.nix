39195f42ff1aba45d64dc287fa1f4b3f2f0c6faa
Ludovic Courtès <ludo@gnu.org>
2009-04-30 19:30:35 +0000

    GNU sed: Update/fix `meta'.
    svn path=/nixpkgs/trunk/; revision=15417

--- pkgs/tools/text/gnused/default.nix
+++ pkgs/tools/text/gnused/default.nix
@@ -12,7 +12,18 @@ stdenv.mkDerivation {
   patches = [./gettext-fix.patch];
 
   meta = {
-    homepage = http://www.gnu.org/software/grep/;
-    description = "GNU implementation of the Unix sed command";
+    homepage = http://www.gnu.org/software/sed/;
+    description = "GNU sed, a batch stream editor";
+
+    longDescription = ''
+      Sed (stream editor) isn't really a true text editor or text
+      processor.  Instead, it is used to filter text, i.e., it takes
+      text input and performs some operation (or set of operations) on
+      it and outputs the modified text.  Sed is typically used for
+      extracting part of a file using pattern matching or substituting
+      multiple occurrences of a string within a file.
+    '';
+
+    license = "GPLv2+";
   };
 }
