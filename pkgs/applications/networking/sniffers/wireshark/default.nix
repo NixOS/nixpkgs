2eb7441069233ef7c3bc9967b789042c9a39d178
Michael Raskin <7c6f434c@mail.ru>
2009-05-04 11:01:59 +0000

    Wireshark update
    svn path=/nixpkgs/trunk/; revision=15439

--- pkgs/applications/networking/sniffers/wireshark/default.nix
+++ pkgs/applications/networking/sniffers/wireshark/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, perl, pkgconfig, gtk, libpcap, flex, bison}:
 
 stdenv.mkDerivation {
-  name = "wireshark-1.1.2";
+  name = "wireshark-1.1.3";
   src = fetchurl {
-    url = http://www.wireshark.org/download/src/wireshark-1.1.2.tar.gz;
-    sha256 = "0fsf84czzxg0gpyf525lx2c9i8la26fkhqv4visz5bz2r0911yd4";
+    url = http://www.wireshark.org/download/src/wireshark-1.1.3.tar.gz;
+    sha256 = "0j2ng84di0lyjax80mw74lmcipxkmlbld4w2yms93932wbxij3s1";
   };
   configureFlags = "--with-pcap=${libpcap}";
   buildInputs = [perl pkgconfig gtk libpcap flex bison];
