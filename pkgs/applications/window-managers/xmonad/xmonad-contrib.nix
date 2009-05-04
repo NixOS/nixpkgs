ab6c19e6a34c3cfa0cc9b19ab6d88c6da8b18c20
Andres Löh <mail@andres-loeh.de>
2009-05-04 13:32:56 +0000

    Minor fix to xmonad-contrib expression.
    svn path=/nixpkgs/trunk/; revision=15441

--- pkgs/applications/window-managers/xmonad/xmonad-contrib.nix
+++ pkgs/applications/window-managers/xmonad/xmonad-contrib.nix
@@ -5,7 +5,7 @@ cabal.mkDerivation (self : {
   name = "${self.fname}";
   version = "0.8.1";
   sha256 = "dedbd2e7718f7383e403f5f0b40d411f27099625e7e1dcddd42f7c12bf6488a9";
-  extraBuildInputs = [X11 xmonad];
+  propagatedBuildInputs = [X11 xmonad];
   meta = {
     description = "a huge extension library for xmonad";
   };
