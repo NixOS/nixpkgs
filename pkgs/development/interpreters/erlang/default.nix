67066d1c567d4abfe6b4707372bf4decf33449d3
Michael Raskin <7c6f434c@mail.ru>
2009-05-05 15:46:03 +0000

    Update erlang
    svn path=/nixpkgs/trunk/; revision=15452

--- pkgs/development/interpreters/erlang/default.nix
+++ pkgs/development/interpreters/erlang/default.nix
@@ -1,12 +1,18 @@
 args: with args;
 
+let version = "R13B"; in
+
 stdenv.mkDerivation {
   name = "erlang-" + version;
   src = fetchurl {
     url = "http://www.erlang.org/download/otp_src_" + version + ".tar.gz";
-    sha256 = "16w7snhbjzwiywppsp04yiy2bkncff8pf4i643kqzkqx578jhaqz";
+    sha256 = "112889v9axhrk9x9swcgql5kpj19p14504m06h4n7b99irzxf4rg";
   };
   buildInputs = [perl gnum4 ncurses openssl];
   patchPhase = '' sed -i "s@/bin/rm@rm@" lib/odbc/configure erts/configure '';
+  preConfigure = ''
+    export HOME=$PWD/../
+    sed -e s@/bin/pwd@pwd@g -i otp_build
+  '';
   configureFlags = "--with-ssl=${openssl}";
 }
