e65f6da64456f3956b0d209f50623c15b3bc87cd
Michael Raskin <7c6f434c@mail.ru>
2009-05-06 07:30:13 +0000

    Update DejaVu fonts
    svn path=/nixpkgs/trunk/; revision=15463

--- pkgs/data/fonts/dejavu-fonts/default.nix
+++ pkgs/data/fonts/dejavu-fonts/default.nix
@@ -1,20 +1,22 @@
 {fetchurl, stdenv, fontforge, perl, fontconfig, FontTTF}:
 
+let version = "2.29" ; in
+
 stdenv.mkDerivation {
-  name = "dejavu-fonts-2.23";
+  name = "dejavu-fonts-${version}";
   #fontconfig is needed only for fc-lang (?)
   buildInputs = [fontforge perl FontTTF];
   src = fetchurl {
-    url = mirror://sourceforge/dejavu/dejavu-fonts-2.23.tar.bz2;
-    sha256 = "0gifaxiianls54i05yw5gxhi2a0j9jmy5p0q58ym4l9fxv5drnhn";
+    url = "mirror://sourceforge/dejavu/dejavu-fonts-${version}.tar.bz2";
+    sha256 = "1h8x0bnbh9awwsxiwjpp73iczk1y4d5y0as1f4zb4pbk6l2m7v60";
   };
   preBuild = ''
     sed -e s@/usr/bin/env@$(type -tP env)@ -i scripts/*
     sed -e s@/usr/bin/perl@$(type -tP perl)@ -i scripts/*
     mkdir resources
     tar xf ${fontconfig.src} --wildcards '*/fc-lang'
-    ln -s $PWD/fontconfig-*/fc-lang resources/
-    ln -s ${perl}/lib/*/unicore/* resources/
+    ln -s $PWD/fontconfig-*/fc-lang -t resources/
+    ln -s ${perl}/lib/*/*/unicore/* -t resources/
   '';
   installPhase = '' 
     ensureDir $out/share/fonts/truetype
