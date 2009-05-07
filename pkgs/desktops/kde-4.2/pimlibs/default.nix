68ab70cbdf9c2b39aae5555b69e44a3ba9065806
Sander van der Burg <s.vanderburg@tudelft.nl>
2009-05-07 13:14:09 +0000

    Fixed typo in version number
    svn path=/nixpkgs/trunk/; revision=15493

--- pkgs/desktops/kde-4.2/pimlibs/default.nix
+++ pkgs/desktops/kde-4.2/pimlibs/default.nix
@@ -2,7 +2,7 @@
  kdelibs, automoc4, phonon, akonadi}:
 
 stdenv.mkDerivation {
-  name = "kdepimlibs-4.2.";
+  name = "kdepimlibs-4.2.3";
   src = fetchurl {
     url = mirror://kde/stable/4.2.3/src/kdepimlibs-4.2.3.tar.bz2;
     sha1 = "3b12d5974bfbc384e3c986328a7bb5b1b6b50361";
