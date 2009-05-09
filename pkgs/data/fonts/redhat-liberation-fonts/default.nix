e29972b12972e77021005657225320225b2e61fa
Michael Raskin <7c6f434c@mail.ru>
2009-05-09 16:26:59 +0000

    Update RedHat Liberation fonts
    svn path=/nixpkgs/trunk/; revision=15516

--- pkgs/data/fonts/redhat-liberation-fonts/default.nix
+++ pkgs/data/fonts/redhat-liberation-fonts/default.nix
@@ -3,11 +3,10 @@
 stdenv.mkDerivation {
   name = "redhat-liberation";
   src = fetchurl {
-    url = http://www.redhat.com/f/fonts/liberation-fonts.tar.gz;
-    sha256 = "5749c27f3deb5da323961e86efed1306fc072bea9065790d0047ad61471be6a5";
+    url = https://fedorahosted.org/releases/l/i/liberation-fonts/liberation-fonts-1.04.tar.gz;
+    sha256 = "189i6pc4jqhhmsb9shi8afg9af9crpmz9bnlldhqaxavr1bhj38f";
   };
   
-  unpackPhase = ''tar -xvf "$src" && sourceRoot="$PWD/liberation-fonts"'';
   installPhase = ''
     ensureDir $out/share/fonts/truetype
     cp *.ttf $out/share/fonts/truetype
