e420d8f5d43e7de92255731af3274c626ba05860
Michael Raskin <7c6f434c@mail.ru>
2009-05-10 21:22:36 +0000

    Update kQemu source location
    svn path=/nixpkgs/trunk/; revision=15543

--- pkgs/os-specific/linux/kqemu/1.4.0pre1.nix
+++ pkgs/os-specific/linux/kqemu/1.4.0pre1.nix
@@ -2,7 +2,7 @@ args : with args;
 rec {
   name = "kqemu-"+version;
   src = fetchurl {
-    url = http://bellard.org/qemu/kqemu-1.4.0pre1.tar.gz;
+    url = http://www.nongnu.org/qemu/kqemu-1.4.0pre1.tar.gz;
     sha256 = "14dlmawn3gia1j401ag5si5k1a1vav7jpv86rl37p1hwmr7fihxs";
   };
 
