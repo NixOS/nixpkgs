f616199fb569bc9153a929575d07767deb71f449
Michael Raskin <7c6f434c@mail.ru>
2009-05-12 16:01:38 +0000

    Fix relfs build
    svn path=/nixpkgs/trunk/; revision=15574

--- pkgs/tools/misc/relfs/cvs.2008.03.05.nix
+++ pkgs/tools/misc/relfs/cvs.2008.03.05.nix
@@ -12,38 +12,37 @@ args : with args;
 		buildInputs = [ocaml fuse postgresql pcre
 			e2fsprogs gnomevfs pkgconfig GConf];
 		configureFlags = [];
+		    build = builderDefs.stringsWithDeps.fullDepEntry ("
+			cd deps 
+			sed -e 's/^CPP/#&/ ; s/^# CPP=gcc/CPP=gcc/' -i Makefile.camlidl
+			make 
+			cd ../src
+			sed -e 's/NULL\\|FALSE/0/g' -i Mimetype_lib.c
+			sed -e 's@/usr/local/@'\$out/'@' -i Makefile
+			sed -e '/install:/a\\\tmkdir -p '\$out'/share' -i Makefile
+			make
+			mkdir -p \$out/bin
+			echo '
+				createuser -A -D \$1
+				dropdb relfs_\$1 ; 
+				rm -rf /tmp/relfs-\$1-tmp;  
+				mkdir /tmp/relfs-\$1-tmp;  
+				USER=\$1 relfs -f -s /tmp/relfs-\$1-tmp  & 
+				sleep 1 && 
+				kill -15 \${!};
+				rm -rf /tmp/relfs-\$1-tmp ; 
+				psql -d relfs_\$1 <<< \"ALTER DATABASE relfs_\$1 OWNER TO \$1; 
+					ALTER TABLE obj OWNER TO \$1; 
+					ALTER TABLE obj_mimetype OWNER TO \$1; 
+					ALTER TABLE membership OWNER TO \$1;\"' > \$out/bin/relfs_grant;
+			chmod a+x \$out/bin/relfs_grant;
+		") ["minInit" "doUnpack" "addInputs"];
 	};
 	in with localDefs;
-let build = FullDepEntry ("
-	cd deps 
-	sed -e 's/^CPP/#&/ ; s/^# CPP=gcc/CPP=gcc/' -i Makefile.camlidl
-	make 
-	cd ../src
-	sed -e 's/NULL\\|FALSE/0/g' -i Mimetype_lib.c
-	sed -e 's@/usr/local/@'\$out/'@' -i Makefile
-	sed -e '/install:/a\\\tmkdir -p '\$out'/share' -i Makefile
-	make
-	mkdir -p \$out/bin
-	echo '
-		createuser -A -D \$1
-		dropdb relfs_\$1 ; 
-		rm -rf /tmp/relfs-\$1-tmp;  
-		mkdir /tmp/relfs-\$1-tmp;  
-		USER=\$1 relfs -f -s /tmp/relfs-\$1-tmp  & 
-		sleep 1 && 
-		kill -15 \${!};
-		rm -rf /tmp/relfs-\$1-tmp ; 
-		psql -d relfs_\$1 <<< \"ALTER DATABASE relfs_\$1 OWNER TO \$1; 
-			ALTER TABLE obj OWNER TO \$1; 
-			ALTER TABLE obj_mimetype OWNER TO \$1; 
-			ALTER TABLE membership OWNER TO \$1;\"' > \$out/bin/relfs_grant;
-	chmod a+x \$out/bin/relfs_grant;
-") [minInit doUnpack addInputs];
-in
 stdenv.mkDerivation rec {
 	name = "relfs-"+version;
 	builder = writeScript (name + "-builder")
-		(textClosure localDefs [build doMakeInstall doForceShare doPropagate]);
+		(textClosure localDefs ["build" "doMakeInstall" "doForceShare" "doPropagate"]);
 	meta = {
 		description = "A relational filesystem on top of FUSE";
 		inherit src;
