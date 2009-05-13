895587ef61b78a607f7123f2f29550b5c571bb63
Sander van der Burg <s.vanderburg@tudelft.nl>
2009-05-13 14:11:46 +0000

    Upgraded kdesvn to 1.3.0
    svn path=/nixpkgs/trunk/; revision=15592

--- pkgs/desktops/kde-4.2/extragear/kdesvn/default.nix
+++ pkgs/desktops/kde-4.2/extragear/kdesvn/default.nix
@@ -2,10 +2,10 @@
  kdelibs, automoc4, phonon}:
 
 stdenv.mkDerivation {
-  name = "kdesvn-1.2.4";
+  name = "kdesvn-1.3.0";
   src = fetchurl {
-    url = http://kdesvn.alwins-world.de/downloads/kdesvn-1.2.4.tar.bz2;
-    sha256 = "aa81addf5f86b4c59026debc555d7aac783002331942847c32d2feb277a81620";
+    url = http://kdesvn.alwins-world.de/downloads/kdesvn-1.3.0.tar.bz2;
+    sha256 = "d219c430c98d75d07258057e78c29042777e0368eded7494499361031ac63649";
   };
   builder = ./builder.sh;
   inherit subversion;
