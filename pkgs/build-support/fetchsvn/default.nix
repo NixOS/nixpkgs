754eef0250ec928fc447c84030f0c74f350df7e8
Michael Raskin <7c6f434c@mail.ru>
2009-05-19 17:07:20 +0000

    Update fetchsvn for proxy support
    svn path=/nixpkgs/trunk/; revision=15654

--- pkgs/build-support/fetchsvn/default.nix
+++ pkgs/build-support/fetchsvn/default.nix
@@ -11,4 +11,12 @@ stdenv.mkDerivation {
   outputHash = if sha256 == "" then md5 else sha256;
   
   inherit url rev sshSupport openssh;
+
+  impureEnvVars = [
+    # We borrow these environment variables from the caller to allow
+    # easy proxy configuration.  This is impure, but a fixed-output
+    # derivation like fetchurl is allowed to do so since its result is
+    # by definition pure.
+    "http_proxy" "https_proxy" "ftp_proxy" "all_proxy" "no_proxy"
+    ];
 }
