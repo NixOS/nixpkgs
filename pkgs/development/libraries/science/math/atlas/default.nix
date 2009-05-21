94ba737704e8097142e24fd7fbe88facbfcfa729
Marc Weber <marco-oweber@gmx.de>
2009-05-21 17:39:17 +0000

    fix atlas by Tobias Neumann
    svn path=/nixpkgs/trunk/; revision=15685

--- pkgs/development/libraries/science/math/atlas/default.nix
+++ pkgs/development/libraries/science/math/atlas/default.nix
@@ -1,16 +1,17 @@
 args: with args;
 
 stdenv.mkDerivation {
-  name = "atlas-3.9.9";
+  name = "atlas-3.9.11";
   src = fetchurl {
-    url = http://kent.dl.sourceforge.net/sourceforge/math-atlas/atlas3.9.9.tar.bz2;
-    sha256 = "0apbiqr0hlb38mrnlij0szsraxvxqfqainmp59xqj94pndx5s3yk";
+    url = mirror://sf/math-atlas/atlas3.9.11.tar.bz2;
+    sha256 = "d91e593a772cf540ff693f7d8c43d10c3037eb334c5c77572ea1b6a64a0b9677";
   };
 
   # configure outside of the source directory
   preConfigure = '' mkdir build; cd build; configureScript=../configure; '';
 
-  NIX_CFLAGS_COMPILE = if stdenv.system == "x86_64-linux" then "-fPIC" else "";
+  # the manual says you should pass -fPIC as configure arg .. It works
+  configureFlags = "-Fa alg -fPIC";
 
   buildInputs = [gfortran];
 
