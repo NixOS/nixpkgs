5b7525c631891a51529795fc037eb6a98ba1afde
Ludovic Courtès <ludo@gnu.org>
2009-05-24 21:25:29 +0000

    Move Twisted to `buildPythonPackage'.
    svn path=/nixpkgs/trunk/; revision=15709

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -67,6 +67,30 @@ rec {
     };
   });
 
+  twisted = buildPythonPackage {
+    name = "twisted-8.1.0";
+
+    src = fetchurl {
+      url = http://tmrc.mit.edu/mirror/twisted/Twisted/8.1/Twisted-8.1.0.tar.bz2;
+      sha256 = "0q25zbr4xzknaghha72mq57kh53qw1bf8csgp63pm9sfi72qhirl";
+    };
+
+    propagatedBuildInputs = [ pkgs.ZopeInterface ];
+
+    meta = {
+      homepage = http://twistedmatrix.com/;
+
+      description = "Twisted, an event-driven networking engine written in Python";
+
+      longDescription = ''
+        Twisted is an event-driven networking engine written in Python
+        and licensed under the MIT license.
+      '';
+
+      license = "MIT";
+    };
+  };
+
   foolscap = buildPythonPackage (rec {
     name = "foolscap-0.3.2";
 
