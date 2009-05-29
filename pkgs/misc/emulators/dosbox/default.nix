ec871e64000bcd720baf23d67620dae8dde405f3
Sander van der Burg <s.vanderburg@tudelft.nl>
2009-05-29 12:14:56 +0000

    Upgraded DOSBox to 0.73 and removed glibc fixes which are obsolete now
    svn path=/nixpkgs/trunk/; revision=15786

--- pkgs/misc/emulators/dosbox/default.nix
+++ pkgs/misc/emulators/dosbox/default.nix
@@ -1,28 +1,15 @@
 {stdenv, fetchurl, SDL, makeDesktopItem}:
 
 stdenv.mkDerivation rec { 
-  name = "dosbox-0.72";
+  name = "dosbox-0.73";
   
   src = fetchurl {
     url = "mirror://sourceforge/dosbox/${name}.tar.gz";
-    sha256 = "0ydck7jgvdwnpxakg2y83dmk2dnwx146cgidbmdn7h75y7cxfiqp";
+    sha256 = "b0a94c46164391a9c32d9571e4d0b61ff238908ff0b77e09157c22dc98a93765";
   };
   
   buildInputs = [SDL];    
-  
-  # Add missing includes in order to fix compilation with glibc 2.9
-  patchPhase = ''
-    echo "#include <string.h>" > tmp.cpp
-    for i in src/hardware/gameblaster.cpp src/hardware/tandy_sound.cpp
-    do
-      cat tmp.cpp $i > $i.new
-      mv $i.new $i
-    done
-    echo "#include <stdlib.h>" > tmp.cpp
-    cat tmp.cpp src/shell/shell_cmds.cpp > src/shell/shell_cmds.cpp.new
-    mv src/shell/shell_cmds.cpp.new src/shell/shell_cmds.cpp
-  '';
-   
+    
   desktopItem = makeDesktopItem {
     name = "dosbox";
     exec = "dosbox";
