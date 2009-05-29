0da38f14f52f494122b3f8e5b555de15d9e4de23
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-05-29 12:43:29 +0000

    * Forgot to commit.
    svn path=/nixos/branches/modular-nixos/; revision=15789

---
+++ modules/installer/tools/tools.nix
@@ -0,0 +1,85 @@
+# This module generates nixos-install, nixos-rebuild,
+# nixos-hardware-scan, etc.
+
+{config, pkgs, ...}:
+
+let
+
+  ### interface
+
+  options = {
+  
+    installer.nixpkgsURL = pkgs.lib.mkOption {
+      default = "";
+      example = http://nixos.org/releases/nix/nixpkgs-0.11pre7577;
+      description = ''
+        URL of the Nixpkgs distribution to use when building the
+        installation CD.
+      '';
+    };
+
+    installer.manifests = pkgs.lib.mkOption {
+      default = [http://nixos.org/releases/nixpkgs/channels/nixpkgs-unstable/MANIFEST];
+      example =
+        [ http://nixos.org/releases/nixpkgs/channels/nixpkgs-unstable/MANIFEST
+          http://nixos.org/releases/nixpkgs/channels/nixpkgs-stable/MANIFEST
+        ];
+      description = ''
+        URLs of manifests to be downloaded when you run
+        <command>nixos-rebuild</command> to speed up builds.
+      '';
+    };
+    
+  };
+
+
+  ### implementation
+
+  makeProg = args: pkgs.substituteAll (args // {
+    dir = "bin";
+    isExecutable = true;
+  });
+  
+  nixosInstall = makeProg {
+    name = "nixos-install";
+    src = ./nixos-install.sh;
+
+    inherit (pkgs) perl;
+    nix = config.environment.nix;
+    nixpkgsURL = config.installer.nixpkgsURL;
+
+    pathsFromGraph = "${pkgs.path}/build-support/kernel/paths-from-graph.pl";
+
+    nixClosure = pkgs.runCommand "closure"
+      {exportReferencesGraph = ["refs" config.environment.nix];}
+      "cp refs $out";
+  };
+
+  nixosRebuild = makeProg {
+    name = "nixos-rebuild";
+    src = ./nixos-rebuild.sh;
+  };
+
+  nixosGenSeccureKeys = makeProg {
+    name = "nixos-gen-seccure-keys";
+    src = ./nixos-gen-seccure-keys.sh;
+  };
+
+  nixosHardwareScan = makeProg {
+    name = "nixos-hardware-scan";
+    src = ./nixos-hardware-scan.pl;
+    inherit (pkgs) perl;
+  };
+
+in
+
+{
+  require = options;
+
+  environment.extraPackages =
+    [ nixosInstall
+      nixosRebuild
+      nixosHardwareScan
+      nixosGenSeccureKeys
+    ];
+}
