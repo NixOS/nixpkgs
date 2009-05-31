d7e96a4e5869e9388dd87f76f8e70df636007979
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2009-05-31 08:35:00 +0000

    Fixing the qt4 setup hook, where "[ -n $blabla ]" always evaluated to true.
    svn path=/nixpkgs/trunk/; revision=15808

--- pkgs/development/libraries/qt-4/setup-hook.sh
+++ pkgs/development/libraries/qt-4/setup-hook.sh
@@ -1,6 +1,6 @@
 export QTDIR=@out@
 
-if [ -n $qt4BadIncludes ]; then
+if [ -n "$qt4BadIncludes" ]; then
     for d in @out@/include/*; do
 	export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I$d";
     done;
