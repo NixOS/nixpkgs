9d25c4d4e236c7f5c8b5dd35671f5452a3771afe
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-06-02 12:27:27 +0000

    * Lots of Qt packages fail to build if the subdirectories of   $out/include aren't explicitly included in the include path.
    svn path=/nixpkgs/trunk/; revision=15820

--- pkgs/development/libraries/qt-4/setup-hook.sh
+++ pkgs/development/libraries/qt-4/setup-hook.sh
@@ -1,7 +1,5 @@
 export QTDIR=@out@
 
-if [ -n "$qt4BadIncludes" ]; then
-    for d in @out@/include/*; do
-	export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I$d";
-    done;
-fi
+for d in @out@/include/*; do
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I$d"
+done
