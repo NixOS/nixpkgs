655675611585e62e852548a8c54d4f4dc270ccc3
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-06-02 19:35:26 +0000

    * ntp 4.2.4p7.
    svn path=/nixpkgs/trunk/; revision=15828

--- pkgs/tools/networking/ntp/default.nix
+++ pkgs/tools/networking/ntp/default.nix
@@ -3,11 +3,11 @@
 assert stdenv.isLinux -> libcap != null;
  
 stdenv.mkDerivation rec {
-  name = "ntp-4.2.4p6";
+  name = "ntp-4.2.4p7";
   
   src = fetchurl {
     url = "http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/${name}.tar.gz";
-    sha256 = "0rqhcmb9rjdl0hkvxyabnmlrzgvvw2f16k7mi9b4qskwgh340xk4";
+    sha256 = "1hq1iz54md8imc4a60jcmljlm5jk8ql7x40v7kbnc1ndwly8i0an";
   };
   
   configureFlags = ''
