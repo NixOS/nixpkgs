889bf6bfd8b6424e6bb6e510de8303f8b7c6d08f
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2009-06-02 19:07:50 +0000

    Made the setup-hook more friendly to packages which use qt4 more normally (that is, they are aware that the header files are in $QTDIR/include/*/)
    svn path=/nixpkgs/trunk/; revision=15827

--- pkgs/development/libraries/qt-4/setup-hook.sh
+++ pkgs/development/libraries/qt-4/setup-hook.sh
@@ -1,5 +1,10 @@
 export QTDIR=@out@
 
-for d in @out@/include/*; do
-    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I$d"
-done
+if [ -z "$normalQt" ]; then
+  # This helps for g++, but not for moc. And no qt4 package should expect
+  # having all qt4 header files dirs into -I. But the KDE nix expressions want
+  # this.
+  for d in @out@/include/*; do
+      export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I$d"
+  done
+fi
