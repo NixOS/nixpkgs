3f036ae7ce3ce5e693d9d345e5ab9e01eca8bb33
Michael Raskin <7c6f434c@mail.ru>
2009-06-05 08:34:30 +0000

    SGT Puzzles tarball changed back (!).
    svn path=/nixpkgs/trunk/; revision=15858

--- pkgs/games/sgt-puzzles/default.nix
+++ pkgs/games/sgt-puzzles/default.nix
@@ -10,7 +10,7 @@ in
 rec {
   src = fetchurl {
     url = "http://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles-${version}.tar.gz";
-    sha256 = "08p1r8r6v3c7lgc1spnji89axr76b859alnw04sg4m7hbd6s2n7v";
+    sha256 = "1m6fybbvlx33786hmgraqxgm1hakfj9bqqszzzpi2ka4spfzj3xl";
   };
 
   inherit buildInputs;
