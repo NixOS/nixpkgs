43cc5950fa73313eec1c6185b0f4eee6f37b6bec
Sander van der Burg <s.vanderburg@tudelft.nl>
2009-06-09 15:04:57 +0000

    Fixed compilation of kdewebdev
    svn path=/nixpkgs/trunk/; revision=15911

--- pkgs/desktops/kde-4.2/webdev/default.nix
+++ pkgs/desktops/kde-4.2/webdev/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
     url = mirror://kde/stable/4.2.4/src/kdewebdev-4.2.4.tar.bz2;
     sha1 = "9e3667c994793232177a70ff0b6fb2caa252757f";
   };
-  includeAllQtLibs=true;
+  includeAllQtDirs=true;
   CMAKE_PREFIX_PATH=kdepimlibs;
   buildInputs = [ cmake qt4 perl libxml2 libxslt boost kdelibs kdepimlibs automoc4 phonon ];
 }
