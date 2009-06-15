588eb7a3517b448cabfb96e6d14e1390c95f76e7
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-06-15 15:05:42 +0000

    * Linux 2.6.27.25.
    svn path=/nixpkgs/trunk/; revision=15962

--- pkgs/os-specific/linux/kernel/linux-2.6.27.nix
+++ pkgs/os-specific/linux/kernel/linux-2.6.27.nix
@@ -5,11 +5,11 @@ assert !userModeLinux;
 import ./generic.nix (
 
   rec {
-    version = "2.6.27.24";
+    version = "2.6.27.25";
   
     src = fetchurl {
       url = "mirror://kernel/linux/kernel/v2.6/linux-${version}.tar.bz2";
-      sha256 = "0db4ddnqv7gyps95xz3vn3p7w6zlsm2l4c5vn9pga6gpxzipvpmg";
+      sha256 = "1a23513dkv127ql8f14i28p404cyyyfh581m4lvnj6biy38m2k8a";
     };
 
     features = {
