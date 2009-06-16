b7032e7fc436150b3401809070bddfad3348715f
Andres Löh <mail@andres-loeh.de>
2009-06-16 08:59:19 +0000

    Version bump for uu-parsinglib.
    svn path=/nixpkgs/trunk/; revision=15970

--- pkgs/development/libraries/haskell/uu-parsinglib/default.nix
+++ pkgs/development/libraries/haskell/uu-parsinglib/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self : {
   pname = "uu-parsinglib";
-  version = "2.1.0";
-  sha256 = "e00c0c0b4df515ffa60706cf79ab71a78a071fe77799bfa7b0240b01d5ff962d";
+  version = "2.2.0";
+  sha256 = "f66b16d112d8a013eaf14823d2f3d228b476dfd117558a97a949a29961db5290";
   meta = {
     description = "New version of the Utrecht University parser combinator library";
   };
