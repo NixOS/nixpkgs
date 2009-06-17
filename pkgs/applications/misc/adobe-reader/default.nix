e16fe2a1ebfeea7f139665148a11cecf55bc7f75
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-06-17 13:56:46 +0000

    svn path=/nixpkgs/trunk/; revision=15984

--- pkgs/applications/misc/adobe-reader/default.nix
+++ pkgs/applications/misc/adobe-reader/default.nix
@@ -3,13 +3,13 @@
 assert stdenv.system == "i686-linux";
 
 stdenv.mkDerivation {
-  name = "adobe-reader-9.1.0-1";
+  name = "adobe-reader-9.1.1-1";
   
   builder = ./builder.sh;
   
   src = fetchurl {
-    url = http://ardownload.adobe.com/pub/adobe/reader/unix/9.x/9.1/enu/AdbeRdr9.1.0-1_i486linux_enu.tar.bz2;
-    sha256 = "0liynlmpgmb23fpmbh83mjzfmq798q1rbpymxjgasc2sn76vd0y7";
+    url = http://ardownload.adobe.com/pub/adobe/reader/unix/9.x/9.1.1/enu/AdbeRdr9.1.1-1_i486linux_enu.tar.bz2;
+    sha256 = "1j3f7d5s7yfm6vg1628f6as462sbm1ika79v0flq0zgljc111vna";
   };
 
   # !!! Adobe Reader contains copies of OpenSSL, libcurl, and libicu.
