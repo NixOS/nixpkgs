93f767d897d263aae835d8479be6c5095c51a291
Rob Vermaas <rob.vermaas@gmail.com>
2009-06-22 11:41:14 +0000

    * bumped version for file
    svn path=/nixpkgs/trunk/; revision=16018

--- pkgs/tools/misc/file/default.nix
+++ pkgs/tools/misc/file/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl}:
  
 stdenv.mkDerivation {
-  name = "file-4.25";
+  name = "file-5.03";
   src = fetchurl {
-    url = ftp://ftp.astron.com/pub/file/file-4.25.tar.gz;
-    sha256 = "abcd5bc7f9a604b5966463eac4a3f54de180a08adb513d902bb44152ebad6c8a";
+    url = ftp://ftp.astron.com/pub/file/file-5.03.tar.gz;
+    sha256 = "1fwmpplwc6h2g89ribq7w8x2np0yn5k7bw042815rv7jkrzv9nhy";
   };
 
   meta = {
