e7edaef8734c06215e18fef3cdd9c6173a7363f1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-06-22 11:14:51 +0000

    * Latest patchelf.
    svn path=/nixpkgs/trunk/; revision=16017

--- pkgs/development/tools/misc/patchelf/0.5.nix
+++ pkgs/development/tools/misc/patchelf/0.5.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "patchelf-0.5pre15500";
+  name = "patchelf-0.5pre15975";
   
   src = fetchurl {
-    url = http://hydra.nixos.org/build/31016/download/1/patchelf-0.5pre15500.tar.bz2;
-    sha256 = "acc9acfc3756f8ec314b21efcd62d296ab2f74c057c2b715d3b13d521e24f5c5";
+    url = http://hydra.nixos.org/build/35275/download/1/patchelf-0.5pre15975.tar.bz2;
+    sha256 = "fa945392386c484b670c1182e354f0738b03db54d51ed6cc7ff9ebd645a20ecb";
   };
 
   meta = {
