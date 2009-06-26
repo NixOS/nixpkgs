c59fb90f629ef7b143fe01fabf32c1a0c4908b18
Marc Weber <marco-oweber@gmx.de>
2009-06-26 00:56:54 +0000

    make fetchgit and nix-prefetch-git use --depth 1 to receive only a shallow clone cutting history
    svn path=/nixpkgs/trunk/; revision=16047

--- pkgs/build-support/fetchgit/builder.sh
+++ pkgs/build-support/fetchgit/builder.sh
@@ -2,7 +2,7 @@ source $stdenv/setup
 
 header "exporting $url (rev $rev) into $out"
 
-git clone "$url" $out
+git clone --depth 1 "$url" $out
 if test -n "$rev"; then
   cd $out
   git checkout $rev
