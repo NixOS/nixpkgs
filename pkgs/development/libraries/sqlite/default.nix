d62a585e152e2783641b63033661f2a5e81aff01
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-07-02 11:21:22 +0000

    * sqlite 3.6.16.
    svn path=/nixpkgs/trunk/; revision=16135

--- pkgs/development/libraries/sqlite/default.nix
+++ pkgs/development/libraries/sqlite/default.nix
@@ -1,13 +1,13 @@
 {stdenv, fetchurl, readline, static ? false}:
 
 stdenv.mkDerivation rec {
-  name = "sqlite-3.6.12";
+  name = "sqlite-3.6.16";
 
   # Note: don't use the "amalgamation" source release, since it
   # doesn't install sqlite3.pc.
   src = fetchurl {
     url = "http://www.sqlite.org/${name}.tar.gz";
-    sha256 = "00cj6bda0kqqn6m3g8k4n4w1rnj76mgk47hf03j0d3w0j0g3rhln";
+    sha256 = "1kadzd5c82x3i7vd0cfqxc3r8a2smc04fhsxpl07jxjlva4khvqc";
   };
 
   buildInputs = [readline];
