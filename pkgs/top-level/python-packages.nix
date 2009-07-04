88b53a7d732044a7ec6d8a61f4e0c27a4d132444
Ludovic Courtès <ludo@gnu.org>
2009-07-04 21:33:52 +0000

    Twisted 8.2.0.
    svn path=/nixpkgs/trunk/; revision=16174

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -215,11 +215,11 @@ rec {
   });
 
   twisted = buildPythonPackage {
-    name = "twisted-8.1.0";
+    name = "twisted-8.2.0";
 
     src = fetchurl {
-      url = http://tmrc.mit.edu/mirror/twisted/Twisted/8.1/Twisted-8.1.0.tar.bz2;
-      sha256 = "0q25zbr4xzknaghha72mq57kh53qw1bf8csgp63pm9sfi72qhirl";
+      url = http://tmrc.mit.edu/mirror/twisted/Twisted/8.2/Twisted-8.2.0.tar.bz2;
+      sha256 = "1c6zplisjdnjzkfs0ld3a0f7m7xbjgx5rcwsdw5i1xiibsq2nq70";
     };
 
     propagatedBuildInputs = [ pkgs.ZopeInterface ];
@@ -235,6 +235,8 @@ rec {
       '';
 
       license = "MIT";
+
+      maintainers = [ stdenv.lib.maintainers.ludo ];
     };
   };
 
