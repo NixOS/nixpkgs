e9dddc50ab64c5d8815b0a8729bdfdb227a44560
Michael Raskin <7c6f434c@mail.ru>
2009-07-07 10:03:26 +0000

    Adding dadadodo - a markov chain-based text generator
    svn path=/nixpkgs/trunk/; revision=16206

---
+++ pkgs/tools/text/dadadodo/default.nix
@@ -0,0 +1,32 @@
+a :  
+let 
+  fetchurl = a.fetchurl;
+
+  version = a.lib.attrByPath ["version"] "1.04" a; 
+  buildInputs = with a; [
+    
+  ];
+in
+rec {
+  src = fetchurl {
+    url = "http://www.jwz.org/dadadodo/dadadodo-${version}.tar.gz";
+    sha256 = "1pzwp3mim58afjrc92yx65mmgr1c834s1v6z4f4gyihwjn8bn3if";
+  };
+
+  inherit buildInputs;
+  configureFlags = [];
+
+  /* doConfigure should be removed if not needed */
+  phaseNames = ["doMake" "doDeploy"];
+  installFlags = "PREFIX=$out";
+
+  doDeploy = a.fullDepEntry (''
+    ensureDir $out/bin
+    cp dadadodo $out/bin
+  '') [ "minInit" "doMake" "defEnsureDir"];
+      
+  name = "dadadodo-" + version;
+  meta = {
+    description = "Markov chain-based text generator";
+  };
+}
