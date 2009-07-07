47d20d5322f6169de5fa4d1d174ba58b7efebdc7
Michael Raskin <7c6f434c@mail.ru>
2009-07-07 10:00:55 +0000

    Update lablgtk
    svn path=/nixpkgs/trunk/; revision=16202

--- pkgs/development/libraries/lablgtk/default.nix
+++ pkgs/development/libraries/lablgtk/default.nix
@@ -1,14 +1,15 @@
 {stdenv, fetchurl, ocaml, pkgconfig, gtk, libgnomecanvas}:
 
-stdenv.mkDerivation {
-  name = "lablgtk-2.6.0";
+stdenv.mkDerivation (rec {
+  version = "2.12.0";
+  name = "lablgtk-${version}";
   src = fetchurl {
-    url = http://wwwfun.kurims.kyoto-u.ac.jp/soft/olabl/dist/lablgtk-2.6.0.tar.gz;
-    sha256 = "3694bc1e288ce0903af6c96a2790d2340ba38fa51b18090062ede75137d97876";
+    url = "http://wwwfun.kurims.kyoto-u.ac.jp/soft/olabl/dist/${name}.tar.gz";
+    sha256 = "1kflrg4rqqzrz0ffdzlhz510m82k21m5x78yr5nxd98avcd39qxk";
   };
 
   buildInputs = [ocaml pkgconfig gtk libgnomecanvas];
 
   configureFlags = "--with-libdir=$(out)/lib/ocaml";
   buildFlags = "world";
-}
+})
