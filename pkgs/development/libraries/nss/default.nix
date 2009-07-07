cc28087b5a2790e2abb5b4fe02d367584c79794c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-07-07 10:59:18 +0000

    * Make doesn't like newlines.
    svn path=/nixpkgs/trunk/; revision=16210

--- pkgs/development/libraries/nss/default.nix
+++ pkgs/development/libraries/nss/default.nix
@@ -27,10 +27,8 @@ stdenv.mkDerivation {
   BUILD_OPT = "1";
 
   makeFlags =
-    ''
-      NSPR_CONFIG_STATUS= NSDISTMODE=copy BUILD_OPT=1 SOURCE_PREFIX=\$(out)
-      ${if stdenv.is64bit then "USE_64=1" else ""}
-    '';
+    ["NSPR_CONFIG_STATUS=" "NSDISTMODE=copy" "BUILD_OPT=1" "SOURCE_PREFIX=\$(out)"]
+    ++ stdenv.lib.optionals stdenv.is64bit "USE_64=1";
 
   buildFlags = "nss_build_all";
 
