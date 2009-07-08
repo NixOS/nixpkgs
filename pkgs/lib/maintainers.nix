edd198ee8bd328394bb84de755c689f8f80f9515
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-07-08 16:14:51 +0000

    * Doh.
    svn path=/nixpkgs/trunk/; revision=16259

--- pkgs/lib/maintainers.nix
+++ pkgs/lib/maintainers.nix
@@ -4,7 +4,7 @@
   /* Add your name and email address here.  Keep the list
      alphabetically sorted.  */
 
-  eelco = "Eelco Dolstra <e.dolstra@tudelft.nl";
+  eelco = "Eelco Dolstra <e.dolstra@tudelft.nl>";
   ludo = "Ludovic Courtès <ludo@gnu.org>";
   raskin = "Michael Raskin <7c6f434c@mail.ru>";
 }
