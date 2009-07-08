1dbe3d5b8752355d9dfcc41b54d92f7c94fd0586
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-07-08 11:49:09 +0000

    * man: use UTF-8 instead of latin1 by default.  Fixes broken special   characters in man output.
    svn path=/nixpkgs/trunk/; revision=16244

--- pkgs/tools/misc/man/default.nix
+++ pkgs/tools/misc/man/default.nix
@@ -19,6 +19,15 @@ stdenv.mkDerivation {
     ./share.patch
   ];
 
+  postInstall =
+    ''
+      # Use UTF-8 by default.  Otherwise man won't know how to deal
+      # with certain characters.
+      substituteInPlace $out/lib/man.conf \
+        --replace "nroff -Tlatin1" "nroff" \
+        --replace "eqn -Tlatin1" "eqn -Tutf8"
+    '';
+
   meta = {
     homepage = http://primates.ximian.com/~flucifredi/man/;
     description = "Tool to read online Unix documentation";
