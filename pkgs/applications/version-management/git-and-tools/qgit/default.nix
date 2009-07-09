1c45eda25a60a9db86ad913d13c30ea9dc8aa241
Peter Simons <simons@cryp.to>
2009-07-09 09:08:22 +0000

    Updated qgit to version 2.3.
    svn path=/nixpkgs/trunk/; revision=16264

--- pkgs/applications/version-management/git-and-tools/qgit/default.nix
+++ pkgs/applications/version-management/git-and-tools/qgit/default.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchurl, qt, libXext, libX11}:
 
 stdenv.mkDerivation rec {
-  name = "qgit-2.2";
+  name = "qgit-2.3";
   meta =
   {
     license = "GPLv2";
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
   src = fetchurl
   {
     url = "mirror://sourceforge/qgit/${name}.tar.bz2";
-    sha256 = "82adcc59b2a9d3a3e54eef9e6a76ac6583e459b6f5c97050d26b0593d11c3d32";
+    sha256 = "a5fdd7e27fea376790eed787e22f4863eb9d2fe0217fd98b9fdbcf47a45bdc64";
   };
   buildInputs = [qt libXext libX11];
   configurePhase = "qmake PREFIX=$out";
