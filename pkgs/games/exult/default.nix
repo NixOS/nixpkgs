9611cf20487c553eeeb196075b860140b8a0d746
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-07-09 10:47:17 +0000

    * Refactoring.
    svn path=/nixpkgs/trunk/; revision=16269

--- pkgs/games/exult/default.nix
+++ pkgs/games/exult/default.nix
@@ -1,8 +1,24 @@
 {stdenv, fetchurl, SDL, SDL_mixer, zlib, libpng, unzip}:
 
+let
+
+  # Digital recordings of the music on an original Roland MT-32.  So
+  # we don't need actual MIDI playback capability.
+  musicFiles =
+    [ (fetchurl {
+        url = mirror://sourceforge/exult/U7MusicOGG_1of2.zip;
+        md5 = "7746d1a9164fd67509107797496553bf";
+      })
+      (fetchurl {
+        url = mirror://sourceforge/exult/U7MusicOGG_2of2.zip;
+        md5 = "cdae5956d7c52f35e90317913a660123";
+      })
+    ];
+
+in
+
 stdenv.mkDerivation {
   name = "exult-1.2";
-  builder = ./builder.sh;
   
   src = fetchurl {
     url = mirror://sourceforge/exult/exult-1.2.tar.gz;
@@ -23,22 +39,18 @@ stdenv.mkDerivation {
   buildInputs = [SDL SDL_mixer zlib libpng unzip];
   
   NIX_CFLAGS_COMPILE = "-I${SDL_mixer}/include/SDL";
-  
-  # Digital recordings of the music on an original Roland MT-32.  So
-  # we don't need actual MIDI playback capability.
-  musicFiles = [
-    (fetchurl {
-      url = mirror://sourceforge/exult/U7MusicOGG_1of2.zip;
-      md5 = "7746d1a9164fd67509107797496553bf";
-    })
-    (fetchurl {
-      url = mirror://sourceforge/exult/U7MusicOGG_2of2.zip;
-      md5 = "cdae5956d7c52f35e90317913a660123";
-    })
-  ];
 
+  postInstall =
+    ''
+      ensureDir $out/share/exult/music
+      for i in $musicFiles; do
+          unzip -o -d $out/share/exult/music $i
+      done
+    '';
+  
   meta = {
     homepage = http://exult.sourceforge.net/;
     description = "A reimplementation of the Ultima VII game engine";
+    maintainers = [stdenv.lib.maintainers.eelco];
   };
 }
