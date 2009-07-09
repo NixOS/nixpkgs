86d88db042096fe07192c17c261d28db6620ae22
Ludovic Courtès <ludo@gnu.org>
2009-07-09 12:07:45 +0000

    Liberation Fonts: Fix name; install doc; add `meta'.
    svn path=/nixpkgs/trunk/; revision=16273

--- pkgs/data/fonts/redhat-liberation-fonts/default.nix
+++ pkgs/data/fonts/redhat-liberation-fonts/default.nix
@@ -1,15 +1,48 @@
 {stdenv, fetchurl}:
 
-stdenv.mkDerivation {
-  name = "redhat-liberation";
+stdenv.mkDerivation rec {
+  name = "liberation-fonts-1.04";
   src = fetchurl {
-    url = https://fedorahosted.org/releases/l/i/liberation-fonts/liberation-fonts-1.04.tar.gz;
+    url = "https://fedorahosted.org/releases/l/i/liberation-fonts/${name}.tar.gz";
     sha256 = "189i6pc4jqhhmsb9shi8afg9af9crpmz9bnlldhqaxavr1bhj38f";
   };
   
   installPhase = ''
     ensureDir $out/share/fonts/truetype
-    cp *.ttf $out/share/fonts/truetype
+    cp -v *.ttf $out/share/fonts/truetype
+
+    ensureDir "$out/doc/${name}"
+    cp -v AUTHORS ChangeLog COPYING License.txt README "$out/doc/${name}"
   '';
+
+  meta = {
+    description = "Liberation Fonts, replacements for Times New Roman, Arial, and Courier New.";
+
+    longDescription = ''
+      The Liberation Fonts are intended to be replacements for the three most
+      commonly used fonts on Microsoft systems: Times New Roman, Arial, and
+      Courier New.
+
+      There are three sets: Sans (a substitute for Arial, Albany, Helvetica,
+      Nimbus Sans L, and Bitstream Vera Sans), Serif (a substitute for Times
+      New Roman, Thorndale, Nimbus Roman, and Bitstream Vera Serif) and Mono
+      (a substitute for Courier New, Cumberland, Courier, Nimbus Mono L, and
+      Bitstream Vera Sans Mono).
+
+      You are free to use these fonts on any system you would like.  You are
+      free to redistribute them under the GPL+exception license found in the
+      download.  Using these fonts does not subject your documents to the
+      GPL---it liberates them from any proprietary claim.
+    '';
+
+    # See `License.txt' for details.
+    license = "GPLv2 + exception";
+
+    homepage = https://fedorahosted.org/liberation-fonts/;
+
+    maintainers = [
+      stdenv.lib.maintainers.raskin
+      stdenv.lib.maintainers.ludo
+    ];
+  };
 }
-  
