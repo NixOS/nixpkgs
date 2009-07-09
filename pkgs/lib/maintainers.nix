07df330fd5c036ede96423dee4af112b37bad195
Marc Weber <marco-oweber@gmx.de>
2009-07-09 22:23:56 +0000

    maintainers + marcweber
    svn path=/nixpkgs/trunk/; revision=16294

--- pkgs/lib/maintainers.nix
+++ pkgs/lib/maintainers.nix
@@ -6,6 +6,7 @@
 
   eelco = "Eelco Dolstra <e.dolstra@tudelft.nl>";
   ludo = "Ludovic Courtès <ludo@gnu.org>";
+  marcweber = "Marc Weber <marco-oweber@gmx.de>";
   raskin = "Michael Raskin <7c6f434c@mail.ru>";
   sander = "Sander van der Burg <s.vanderburg@tudelft.nl>";
 }
