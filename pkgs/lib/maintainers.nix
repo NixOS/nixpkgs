9f9ec8d2c68cb65d97287f3ef136c85a773d3106
Andres Löh <mail@andres-loeh.de>
2009-07-10 19:50:04 +0000

    Added myself as a maintainer.
    svn path=/nixpkgs/trunk/; revision=16327

--- pkgs/lib/maintainers.nix
+++ pkgs/lib/maintainers.nix
@@ -4,6 +4,7 @@
   /* Add your name and email address here.  Keep the list
      alphabetically sorted.  */
 
+  andres = "Andres Loeh <andres@cs.uu.nl>";
   eelco = "Eelco Dolstra <e.dolstra@tudelft.nl>";
   ludo = "Ludovic Courtès <ludo@gnu.org>";
   marcweber = "Marc Weber <marco-oweber@gmx.de>";
