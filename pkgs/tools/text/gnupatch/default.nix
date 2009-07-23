c100551503b3cb3039fd7a4f950320ad26799c3b
Rob Vermaas <rob.vermaas@gmail.com>
2009-07-23 22:07:45 +0000

    * darwin patch to make sure gnupatch doesn't get dSYM extension
    svn path=/nixpkgs/trunk/; revision=16447

--- pkgs/tools/text/gnupatch/default.nix
+++ pkgs/tools/text/gnupatch/default.nix
@@ -1,6 +1,6 @@
 {stdenv, fetchurl}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation ( {
   name = "gnupatch-2.5.4";
   src = fetchurl {
     url = mirror://gnu/patch/patch-2.5.4.tar.gz;
@@ -10,6 +10,6 @@ stdenv.mkDerivation {
   # Hack around ancient configure script: doesn't build on many newer
   # platforms unless a platform is specified.
   configureFlags = "dummy";
-  
+
   patches = if stdenv.isDarwin then [./setmode.patch] else [];
-}
+} // (if stdenv.isDarwin then { ac_cv_exeext = "" ; } else {} ) )
