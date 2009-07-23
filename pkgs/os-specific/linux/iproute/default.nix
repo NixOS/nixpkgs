aaca21fcffce3de9710234c6bdbc12ffa9139e5a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-07-23 21:34:53 +0000

    * iproute2 updated to 2.6.29-1.
    svn path=/nixpkgs/trunk/; revision=16446

--- pkgs/os-specific/linux/iproute/default.nix
+++ pkgs/os-specific/linux/iproute/default.nix
@@ -1,25 +1,19 @@
 {fetchurl, stdenv, flex, bison, db4}:
 
-stdenv.mkDerivation {
-  name = "iproute-2.6.22-070710";
+stdenv.mkDerivation rec {
+  name = "iproute2-2.6.29-1";
 
   src = fetchurl {
-    url = http://developer.osdl.org/dev/iproute2/download/iproute2-2.6.22-070710.tar.gz;
-    sha256 = "3c6b48af9e655e4f0a34c7718e288960a1dc84a3ac7eb726e855adb45fbd953a";
+    url = "http://devresources.linux-foundation.org/dev/iproute2/download/${name}.tar.bz2";
+    sha256 = "16zvhdzv7yqyvmwxyqa6shzsld6r0wpnk37dig69sk20wpzv1zqk";
   };
  
-  unpackPhase = ''
-      mkdir tmp; cd tmp
-      unpackFile "$src"
-  '';
+  preConfigure =
+    ''
+      patchShebangs ./configure
+    '';
 
-  patchPhase = ''
-    for script in $(find . -type f); do sed -e 's@#! /bin/bash@#! /bin/sh@' -i $script;
-    done;
-    sed -e s@/usr/lib@$out/lib@ -i tc/Makefile
-  '';
-
-  makeFlags = " SBINDIR=\\$(out)/sbin CONFDIR=\\$(out)/etc DOCDIR=\\$(out)/doc MANDIR=\\$(out)/man ";
+  makeFlags = "DESTDIR= LIBDIR=$(out)/lib SBINDIR=$(out)/sbin CONFDIR=$(out)/etc DOCDIR=$(out)/share/doc MANDIR=$(out)/share/man";
 
   buildInputs = [bison flex db4];
 }
