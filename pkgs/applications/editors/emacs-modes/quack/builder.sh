7f22bca190ff3418d5679f2c09d165ae550dd524
Ludovic Courtès <ludo@gnu.org>
2009-07-24 12:41:47 +0000

    Quack 0.37.
    svn path=/nixpkgs/trunk/; revision=16453

--- pkgs/applications/editors/emacs-modes/quack/builder.sh
+++
@@ -1,7 +0,0 @@
-source "$stdenv/setup" || exit 1
-
-emacsDir="$out/share/emacs/site-lisp"
-
-ensureDir "$emacsDir" &&					\
-cp "$src" "$emacsDir/quack.el" &&				\
-emacs --batch -f batch-byte-compile "$emacsDir/quack.el"
--- pkgs/applications/editors/emacs-modes/quack/default.nix
+++ pkgs/applications/editors/emacs-modes/quack/default.nix
@@ -1,21 +1,32 @@
 { fetchurl, stdenv, emacs }:
 
 stdenv.mkDerivation {
-  name = "quack-0.36";
+  name = "quack-0.37";
 
   src = fetchurl {
     # XXX: Upstream URL is not versioned, which might eventually break this.
     url = "http://www.neilvandyke.org/quack/quack.el";
-    sha256 = "0y9l35a8v56ldy4dap0816i80q9lnfpp27pl2a12d5hzb84hq8nr";
+    sha256 = "1q5442cpvw2i0qhmhn7mh45jnmzg0cmd01k5zp4gvg1526c0hbcc";
   };
 
   buildInputs = [ emacs ];
 
-  builder = ./builder.sh;
+  unpackPhase = "true";
+  configurePhase = "true";
+  installPhase = "true";
+
+  buildPhase = ''
+    emacsDir="$out/share/emacs/site-lisp"
+
+    ensureDir "$emacsDir"
+    cp -v "$src" "$emacsDir/quack.el"
+    emacs --batch -f batch-byte-compile "$emacsDir/quack.el"
+  '';
 
   meta = {
     description = "Enhanced Emacs support for editing and running Scheme code";
     homepage = http://www.neilvandyke.org/quack/;
     license = "GPLv2+";
+    maintainers = [ stdenv.lib.maintainers.ludo ];
   };
 }
