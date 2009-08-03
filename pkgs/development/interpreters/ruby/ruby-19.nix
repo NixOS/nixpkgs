cebbdff56c26eafb68eea956acc86920b23da8b7
Pjotr Prins <pjotr.public01@thebird.nl>
2009-08-03 11:53:58 +0000

    Updated ruby interpreter
    svn path=/nixpkgs/trunk/; revision=16556

--- pkgs/development/interpreters/ruby/ruby-19.nix
+++ pkgs/development/interpreters/ruby/ruby-19.nix
@@ -1,10 +1,10 @@
 { ruby18, fetchurl }:
 
 ruby18.override rec {
-  version = "1.9.1-p129";
+  version = "1.9.1-p243";
   name = "ruby-${version}";
   src = fetchurl {
-    url = "ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.1-p129.tar.gz";
-    sha256 = "0fr52j6xbrq3x4nr93azhz1zhlqb4dar7bi0f0iyqz6iw6naidr7";
+    url = "ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-${version}.tar.gz";
+    sha256 = "1r4bls76dg97lqgwkxi6kbxzirkvjm21d4i2qyz469lnncvqwn9i";
   };
 }
