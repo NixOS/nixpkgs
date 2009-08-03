8abae778b8253b6d175a5fdf3aa251c63fc5d2e8
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-08-03 12:18:50 +0000

    * Latest NVIDIA driver.
    svn path=/nixpkgs/trunk/; revision=16558

--- pkgs/os-specific/linux/nvidia-x11/default.nix
+++ pkgs/os-specific/linux/nvidia-x11/default.nix
@@ -2,7 +2,7 @@
 
 let 
 
-  versionNumber = "185.18.14";
+  versionNumber = "185.18.29";
 
 in
 
@@ -15,12 +15,12 @@ stdenv.mkDerivation {
     if stdenv.system == "i686-linux" then
       fetchurl {
         url = "http://us.download.nvidia.com/XFree86/Linux-x86/${versionNumber}/NVIDIA-Linux-x86-${versionNumber}-pkg0.run";
-        sha256 = "0q7p7329r8ivsri0ldzrwi08976iqjvz34ximkzksyh60q6xlsf5";
+        sha256 = "0pgmxp592hkxx3k68af48jpvzywaxlkmls2rn68f6y871wb8akks";
       }
     else if stdenv.system == "x86_64-linux" then
       fetchurl {
         url = "http://us.download.nvidia.com/XFree86/Linux-x86_64/${versionNumber}/NVIDIA-Linux-x86_64-${versionNumber}-pkg0.run";
-        sha256 = "1bz854bhqxdjmhsa3agrgmpddql1xkc5frip1z6pdwmy1sh0754q";
+        sha256 = "1lpykpf0swv9rdql1hcgikqdw9jb7735f4y3p0qk50xx6j4sfwl4";
       }
     else throw "nvidia-x11 does not support platform ${stdenv.system}";
 
