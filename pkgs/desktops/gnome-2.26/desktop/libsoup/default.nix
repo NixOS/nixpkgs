2f8c8d39e8e7a72cb6463f180c85e87b7fce7110
Michael Raskin <7c6f434c@mail.ru>
2009-08-24 05:31:15 +0000

    OOps, fixed wrong file
    svn path=/nixpkgs/trunk/; revision=16825

--- pkgs/desktops/gnome-2.26/desktop/libsoup/default.nix
+++ pkgs/desktops/gnome-2.26/desktop/libsoup/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "libsoup-2.27.4";
   src = fetchurl {
     url = mirror://gnome/desktop/2.27/2.27.4/sources/libsoup-2.27.4.tar.bz2;
-    md5 = "4d67aa8d2d3c719b67bde58ca3b8a94aa1d172bc242672401d7f3d22685065b9";
+    sha256 = "4d67aa8d2d3c719b67bde58ca3b8a94aa1d172bc242672401d7f3d22685065b9";
   };
   buildInputs = [ pkgconfig libxml2 gnutls libproxy sqlite curl glib GConf ];
 }
--- pkgs/desktops/gnome-2.26/desktop/libsoup/git-head.nix
+++ pkgs/desktops/gnome-2.26/desktop/libsoup/git-head.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   src = fetchgit {
     url = git://git.gnome.org/libsoup;
     rev = "3d0441b3f0c402447306f53789a47abdc573f8f3";
-    sha256 = "22379acc77cb6a381bd0abf69ae75ca8";
+    md5 = "22379acc77cb6a381bd0abf69ae75ca8";
   };
   buildInputs = [ pkgconfig libxml2 gnutls libproxy sqlite curl glib GConf ];
 }
