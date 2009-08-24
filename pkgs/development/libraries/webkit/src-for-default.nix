d436b3a8f24e35ac2b240044b3e76317ecacd318
Michael Raskin <7c6f434c@mail.ru>
2009-08-24 03:54:25 +0000

    Try downgrading WebKit further
    svn path=/nixpkgs/trunk/; revision=16819

--- pkgs/development/libraries/webkit/src-for-default.nix
+++ pkgs/development/libraries/webkit/src-for-default.nix
@@ -1,6 +1,6 @@
 rec {
-  advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r47409.tar.bz2";
-  version = "r47409";
+  advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r47388.tar.bz2";
+  version = "r47388";
   url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-${version}.tar.bz2";
-  hash = "1lyl8ksdca2a5869yhrvb8sdd3ymnia0jhgaabhfm04saygn6sqx";
+  hash = "0apq5fg254wckpfqxgy61qd13k8srss1822dllmwcfsfk15m7mcx";
 }
