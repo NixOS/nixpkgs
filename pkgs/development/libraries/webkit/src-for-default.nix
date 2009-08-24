98a62231d6fd94e2a86f1f98ccb39dfb704425b3
Michael Raskin <7c6f434c@mail.ru>
2009-08-24 18:59:29 +0000

    The chosen tarball failed; trying the previous one
    svn path=/nixpkgs/trunk/; revision=16838

--- pkgs/development/libraries/webkit/src-for-default.nix
+++ pkgs/development/libraries/webkit/src-for-default.nix
@@ -1,6 +1,6 @@
 rec {
-  advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r47060.tar.bz2";
-  version = "r47060";
+  advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r47011.tar.bz2";
+  version = "r47011";
   url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-${version}.tar.bz2";
-  hash = "0fxyr3p8j3c110nsl15n4wm90aai6hwz5hwc43g1cpxd966qi0cj";
+  hash = "03lzw3hh7nnbwb8vyv98lgqqgpmd4pc536z7xfn47yg4q76069wq";
 }
