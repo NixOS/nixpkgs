0e6cd4b7944043ff159f0aad51756574d93966b0
Michael Raskin <7c6f434c@mail.ru>
2009-08-24 05:19:30 +0000

    Fix typo
    svn path=/nixpkgs/trunk/; revision=16821

--- pkgs/desktops/gnome-2.26/desktop/libsoup/git-head.nix
+++ pkgs/desktops/gnome-2.26/desktop/libsoup/git-head.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "libsoup-2.27.git";
   src = fetchgit {
     url = git://git.gnome.org/libsoup;
-    revision = "3d0441b3f0c402447306f53789a47abdc573f8f3";
+    rev = "3d0441b3f0c402447306f53789a47abdc573f8f3";
     sha256 = "22379acc77cb6a381bd0abf69ae75ca8";
   };
   buildInputs = [ pkgconfig libxml2 gnutls libproxy sqlite curl glib GConf ];
