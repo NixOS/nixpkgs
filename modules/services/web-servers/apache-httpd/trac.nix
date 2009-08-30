8fd9fa36c465a66835b1170704d2aa6ab30a76f0
Sander van der Burg <s.vanderburg@tudelft.nl>
2009-08-30 19:11:47 +0000

    Added an initial Trac subservice. Trac projects still have to be created by using the trac-admin tool
    svn path=/nixos/trunk/; revision=16894

---
+++ modules/services/web-servers/apache-httpd/trac.nix
@@ -0,0 +1,38 @@
+{config, pkgs, serverInfo}:
+
+let
+  inherit (pkgs.lib) mkOption;
+in
+{
+  options = {
+    projectsLocation = mkOption {
+      description = "URL path in which Trac projects can be accessed";
+      default = "/projects";
+    };
+  };
+
+  extraModules = [
+    { name = "python"; path = "${pkgs.mod_python}/modules/mod_python.so"; }
+  ];
+  
+  extraConfig = ''
+      <Location ${config.projectsLocation}>
+        SetHandler mod_python
+        PythonHandler trac.web.modpython_frontend
+	PythonOption TracEnvParentDir /var/trac/projects
+        PythonOption TracUriRoot ${config.projectsLocation}
+        PythonOption PYTHON_EGG_CACHE /var/trac/egg-cache
+      </Location>
+  '';
+  
+  globalEnvVars = [
+    { name = "PYTHONPATH";
+      value = 
+        "${pkgs.mod_python}/lib/python2.5/site-packages:" +
+	"${pkgs.pythonPackages.trac}/lib/python2.5/site-packages:" +
+	"${pkgs.setuptools}/lib/python2.5/site-packages:" +
+	"${pkgs.pythonPackages.genshi}/lib/python2.5/site-packages:" +
+	"${pkgs.pythonPackages.psycopg2}/lib/python2.5/site-packages";
+    }
+  ];
+}
