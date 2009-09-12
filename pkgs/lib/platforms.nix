b7fa5f13c95b4349d4193cd0e6e2c9b8b8a799c4
Ludovic Courtès <ludo@gnu.org>
2009-09-12 18:46:04 +0000

    Add `gnu' as an alias for `linux'.
    The rationale is that usually applications don't care about the kernel
    but rather about the C library, tool chain, and standard utilities,
    which are GNU, not Linux.
    
    svn path=/nixpkgs/trunk/; revision=17069

--- pkgs/lib/platforms.nix
+++ pkgs/lib/platforms.nix
@@ -2,6 +2,7 @@ let
   lists = import ./lists.nix;
 in
 rec {
+  gnu = linux;  /* ++ hurd ++ kfreebsd ++ ... */
   linux = ["i686-linux" "x86_64-linux" "powerpc-linux"];
   darwin = ["i686-darwin" "powerpc-darwin"];
   freebsd = ["i686-freebsd" "x86_64-freebsd" "powerpc-freebsd"];
