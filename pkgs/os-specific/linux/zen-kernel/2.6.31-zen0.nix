07ed9e4611c1349aa038ab8da73d04fb8a650c2b
Michael Raskin <7c6f434c@mail.ru>
2009-09-15 12:29:15 +0000

    Enable FB_CON_DECOR
    svn path=/nixpkgs/trunk/; revision=17159

--- pkgs/os-specific/linux/zen-kernel/2.6.31-zen0.nix
+++ pkgs/os-specific/linux/zen-kernel/2.6.31-zen0.nix
@@ -25,26 +25,24 @@ in
 
   preConfigure = '' 
     killOption () {
-      sed -re 's/^('"$1"')=[ym]/# \1 is not set/' -i .config
-    }
-    setOptionMod () {
-      sed -re 's/^# ('"$1"') is not set/\1=m/' -i .config
-      sed -re "1i$1=m" -i .config
-    }
-    setOptionYes () {
-      sed -re 's/^# ('"$1"') is not set/\1=y/' -i .config
-      sed -re "1i$1=y" -i .config
+      sed -re 's/^('"$1"')=.*/# \1 is not set/' -i .config
     }
     setOptionVal () {
       sed -re 's/^('"$1"')=.*/\1='"$2"'/' -i .config
+      sed -re 's/^# ('"$1"') is not set/\1='"$2"'/' -i .config
       sed -re "1i$1=$2" -i .config
+    }
+    setOptionMod () {
+      setOptionVal "$1" m
+    }
+    setOptionYes () {
+      setOptionVal "$1" y
     }
 
     make allmodconfig
 
     killOption CONFIG_CMDLINE_OVERRIDE
 
-    killOption CONFIG_IMA
     killOption 'CONFIG_.*_DEBUG.*'
     killOption 'CONFIG_DEBUG.*'
     killOption CONFIG_AUDIT_ARCH
@@ -52,14 +50,7 @@ in
     killOption 'CONFIG_GCOV.*'
     killOption 'CONFIG_KGDB.*'
     killOption 'CONFIG_.*_TEST'
-    
-    killOption CONFIG_KERNEL_BZIP2
-    killOption CONFIG_KERNEL_LZMA
-    setOptionYes CONFIG_KERNEL_GZIP
-
     killOption CONFIG_TASKSTATS
-    killOption CONFIG_PREEMPT_NONE
-    setOptionYes CONFIG_PREEMPT_VOLUNTARY
 
     killOption CONFIG_SLQB
     killOption CONFIG_SLQB_ALLOCATOR
@@ -68,11 +59,28 @@ in
     killOption CONFIG_ACPI_CUSTOM_DSDT_INITRD
     killOption CONFIG_DEVTMPFS
     killOption CONFIG_DEVTMPFS_MOUNT
+
+    killOption CONFIG_IMA
   '' +
-  (''
+  ''
+    killOption CONFIG_KERNEL_BZIP2
+    killOption CONFIG_KERNEL_LZMA
+    setOptionYes CONFIG_KERNEL_GZIP
+  ''+
+  ''
     killOption CONFIG_FB_TILEBLITTING
+    killOption CONFIG_FB_S3
+    killOption CONFIG_FB_VT8623
+    killOption CONFIG_FB_ARK
+    setOptionYes CONFIG_FRAMEBUFFER_CONSOLE
+    setOptionYes CONFIG_FB
+    make oldconfig
     setOptionYes CONFIG_FB_CON_DECOR
-  '')+
+  ''+
+  ''
+    killOption CONFIG_PREEMPT_NONE
+    setOptionYes CONFIG_PREEMPT_VOLUNTARY
+  ''+
   (if a.lib.attrByPath ["ckSched"] false a then ''
     killOption CONFIG_CPU_CFS
     setOptionYes CONFIG_CPU_BFS
