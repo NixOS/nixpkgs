08cc42f378c97c6eab307e935c26925d9116f8e2
Ludovic Courtès <ludo@gnu.org>
2009-09-16 12:39:57 +0000

    libgc: Add meta-info.
    svn path=/nixpkgs/trunk/; revision=17187

--- pkgs/development/libraries/boehm-gc/default.nix
+++ pkgs/development/libraries/boehm-gc/default.nix
@@ -12,7 +12,30 @@ stdenv.mkDerivation {
   doCheck = true;
 
   meta = {
-    description = "A garbage collector for C and C++";
+    description = "The Boehm-Demers-Weiser conservative garbage collector for C and C++";
+
+    longDescription = ''
+      The Boehm-Demers-Weiser conservative garbage collector can be used as a
+      garbage collecting replacement for C malloc or C++ new.  It allows you
+      to allocate memory basically as you normally would, without explicitly
+      deallocating memory that is no longer useful.  The collector
+      automatically recycles memory when it determines that it can no longer
+      be otherwise accessed.
+
+      The collector is also used by a number of programming language
+      implementations that either use C as intermediate code, want to
+      facilitate easier interoperation with C libraries, or just prefer the
+      simple collector interface.
+
+      Alternatively, the garbage collector may be used as a leak detector for
+      C or C++ programs, though that is not its primary goal.
+    '';
+
     homepage = http://www.hpl.hp.com/personal/Hans_Boehm/gc/;
+
+    # non-copyleft, X11-style license
+    license = "http://www.hpl.hp.com/personal/Hans_Boehm/gc/license.txt";
+
+    maintainers = [ stdenv.lib.maintainers.ludo ];
   };
 }
