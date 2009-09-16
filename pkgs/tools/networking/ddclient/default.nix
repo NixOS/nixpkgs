5d6ee123ed7cea0ba4df0e78ddf8a54d4b9040c8
Rob Vermaas <rob.vermaas@gmail.com>
2009-09-16 11:27:18 +0000

    ddclient
    svn path=/nixpkgs/trunk/; revision=17181

---
+++ pkgs/tools/networking/ddclient/default.nix
@@ -0,0 +1,24 @@
+{buildPerlPackage, fetchurl, perl}:
+
+buildPerlPackage {
+  name = "ddclient-3.8.0";
+
+  src = fetchurl {
+    url = mirror://sourceforge/ddclient/ddclient-3.8.0.tar.gz ;
+    sha256 = "1cqz6fwx8bcl7zdrvm6irh3bzs8858gkyficww9simyjmz7z3w48";
+  };
+
+  patches = [ ./ddclient-foreground.patch ];
+
+  preConfigure = '' 
+    touch Makefile.PL
+  ''; 
+
+  installPhase = ''
+    ensureDir $out/bin
+    cp ddclient $out/bin
+  '';
+
+  doCheck = false;
+
+}
