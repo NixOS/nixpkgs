1d7de3605fcbbd38fd5edce9e3b2e16c0ea98425
Marc Weber <marco-oweber@gmx.de>
2009-09-21 15:55:24 +0000

    ardour: fix source using a copy
    svn path=/nixpkgs/trunk/; revision=17339

--- pkgs/applications/audio/ardour/default.nix
+++ pkgs/applications/audio/ardour/default.nix
@@ -1,9 +1,16 @@
 args: with args;
+
 stdenv.mkDerivation {
+
   name = "ardour-2.8.2";
 
-  # svn is the source to get official releases from their site.. :-(
-  src = /tmp/ardour-2.8.2.tar.bz2;
+  # svn is the source to get official releases from their site?
+  # alternative: wget  --data-urlencode 'key=7c4b2e1df903aae5ff5cc4077cda801e' http://ardour.org/downloader
+  # but hash is changing ?
+  src = fetchurl {
+    url = http://mawercer.de/~nix/ardour-2.8.2.tar.bz2;
+    sha256 = "1igwv1r6rlybdac24qady5asaf34f9k7kawkkgyvsifhl984m735";
+  };
 
   buildInputs = [
     scons boost
