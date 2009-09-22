8a9c1f6781bfcc073b2d2f1f933390917492f8e9
Peter Simons <simons@cryp.to>
2009-09-22 19:37:18 +0000

    htop: updated to version 0.8.3
    svn path=/nixpkgs/trunk/; revision=17355

--- pkgs/os-specific/linux/htop/default.nix
+++ pkgs/os-specific/linux/htop/default.nix
@@ -1,10 +1,10 @@
 {fetchurl, stdenv, ncurses}:
 
 stdenv.mkDerivation rec {
-  name = "htop-0.8.1";
+  name = "htop-0.8.3";
   src = fetchurl {
     url = "mirror://sourceforge/htop/${name}.tar.gz";
-    sha256 = "0a2x28ibz7bg18nnb75gdssxwys0xvzd760j1vnq5dx45wh2ibi5";
+    sha256 = "f03bac8999c57c399cbf4332831bcce905d0393d0f97f7e32a1407b48890dd9d";
   };
   buildInputs = [ncurses];
   meta = {
