16583ec56628cae92b5c7e6beb9a02390e621ef2
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-09-26 19:08:49 +0000

    * Latest manpages.
    svn path=/nixpkgs/trunk/; revision=17449

--- pkgs/data/documentation/man-pages/default.nix
+++ pkgs/data/documentation/man-pages/default.nix
@@ -1,16 +1,17 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation rec {
-  name = "man-pages-3.19";
+  name = "man-pages-3.22";
   
   src = fetchurl {
     url = "mirror://kernel/linux/docs/man-pages/Archive/${name}.tar.bz2";
-    sha256 = "1xzazq6wknyi2bjw39z6jswifn3gq3f2wr0mp06xgz7fzr0d266q";
+    sha256 = "0b6x5jajbrqls2yvy1xzgr1c4jhs1cp8pid07a34m6wipjr6b5kg";
   };
 
-  preBuild = "
-    makeFlagsArray=(MANDIR=$out/share/man)
-  ";
+  preBuild =
+    ''
+      makeFlagsArray=(MANDIR=$out/share/man)
+    '';
 
   meta = {
     description = "Linux development manual pages";
