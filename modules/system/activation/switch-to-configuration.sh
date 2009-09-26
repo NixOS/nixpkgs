c5d45514a1ec69043cebd30469833d5466bf24c9
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-09-26 21:00:47 +0000

    * Don't restart dbus, since that causes ConsoleKit to forget about   current sessions.
    svn path=/nixos/trunk/; revision=17452

--- modules/system/activation/switch-to-configuration.sh
+++ modules/system/activation/switch-to-configuration.sh
@@ -85,7 +85,9 @@ EOF
 
         # Hack: skip the sys-* and ctrl-alt-delete events.
         # Another hack: don't restart the X server (that would kill all the clients).
-        if echo "$event" | grep -q "^sys-\|^ctrl-\|^xserver"; then continue; fi
+        # And don't restart dbus, since that causes ConsoleKit to
+        # forget about current sessions.
+        if echo "$event" | grep -q "^sys-\|^ctrl-\|^xserver$\|^dbus$"; then continue; fi
     
         if ! test -e "$oldEvents/$event"; then
             echo "starting $event..."
