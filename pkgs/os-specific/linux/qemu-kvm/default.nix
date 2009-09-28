9bc85fda471211628ebf0e8348878d0f1da2538c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-09-28 14:28:20 +0000

    * qemu-kvm 0.11.0.
    svn path=/nixpkgs/trunk/; revision=17468

--- pkgs/os-specific/linux/qemu-kvm/default.nix
+++ pkgs/os-specific/linux/qemu-kvm/default.nix
@@ -3,11 +3,11 @@
 assert stdenv.isLinux;
    
 stdenv.mkDerivation rec {
-  name = "qemu-kvm-0.11.0-rc2";
+  name = "qemu-kvm-0.11.0";
    
   src = fetchurl {
     url = "mirror://sourceforge/kvm/${name}.tar.gz";
-    sha256 = "0mdcbiiypa76cvjpns36hlnqg6rgwbk07jjqyylva7vaxhzqrsyq";
+    sha256 = "1jcx0jak528ifa7v3040zyzlny9gyjmwycx6m2dv1wsllzrp2w34";
   };
 
   patches = [ ./unix-domain.patch ];
