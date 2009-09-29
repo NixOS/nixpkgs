8177f0f0420b591ba5193b1702f820e413073665
Michael Raskin <7c6f434c@mail.ru>
2009-09-29 12:52:47 +0000

    Updating ICU library..
    svn path=/nixpkgs/trunk/; revision=17496

--- pkgs/development/libraries/icu/default.nix
+++ pkgs/development/libraries/icu/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "icu4c-3.6";
+  name = "icu4c-4.2";
   
   src = fetchurl {
-    url = ftp://ftp.software.ibm.com/software/globalization/icu/3.6/icu4c-3_6-src.tgz;
-    sha256 = "0hdh8sbpmabijprdpn7rmsqilw97f3paxxsxa4hd61k2kpbfhdai";
+    url = http://download.icu-project.org/files/icu4c/4.2.1/icu4c-4_2_1-src.tgz;
+    sha256 = "0qw050msb34wr522s7s83i6skxsc9i19p4rlvmf99pqk2hgf6kc1";
   };
 
   patchFlags = "-p0";
@@ -23,4 +23,10 @@ stdenv.mkDerivation {
   ";
   
   configureFlags = "--disable-debug";
+
+  meta = {
+    description = "Unicode and globalization support library";
+    maintainers = [stdenv.lib.maintainers.raskin];
+    platforms = stdenv.lib.platforms.all;
+  };
 }
