176c0e32b18139e83efaf71c516bcd9dee733192
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-09-29 15:33:03 +0000

    * net-tools: install the manpages in $out/share/man/manN instead of   $out/share/manN.  Cleaned up the expression at the same time.
    svn path=/nixpkgs/trunk/; revision=17509

--- pkgs/os-specific/linux/net-tools/default.nix
+++ pkgs/os-specific/linux/net-tools/default.nix
@@ -1,12 +1,26 @@
-{stdenv, fetchurl}:
+{ stdenv, fetchurl }:
 
 stdenv.mkDerivation {
   name = "net-tools-1.60";
-  builder = ./builder.sh;
+  
   src = fetchurl {
     url = http://www.tazenda.demon.co.uk/phil/net-tools/net-tools-1.60.tar.bz2;
     md5 = "888774accab40217dde927e21979c165";
   };
-  config = ./config.h;
-  patch = ./net-tools-labels.patch;
+
+  patches = [ ./net-tools-labels.patch ];
+  
+  preBuild =
+    ''
+      cp ${./config.h} config.h
+    '';
+
+  makeFlags = "BASEDIR=$(out) mandir=/share/man";
+
+  meta = {
+    homepage = http://www.tazenda.demon.co.uk/phil/net-tools/;
+    description = "A set of tools for controlling the network subsystem in Linux";
+    license = "GPLv2+";
+    platforms = [ stdenv.lib.platforms.linux ];
+  };
 }
