b595a17822e37390f496705e500ea501f1940306
Michael Raskin <7c6f434c@mail.ru>
2009-09-29 22:03:39 +0000

    Update monotone
    svn path=/nixpkgs/trunk/; revision=17517

--- pkgs/applications/version-management/monotone/default.nix
+++ pkgs/applications/version-management/monotone/default.nix
@@ -2,12 +2,12 @@
   lua, pcre, sqlite, lib}:
 
 let 
-  version = "0.44";
+  version = "0.45";
 in stdenv.mkDerivation {
   name = "monotone-${version}";
   src = fetchurl {
     url = "http://monotone.ca/downloads/${version}/monotone-${version}.tar.gz";
-    sha256 = "1d1jck5dw210q99km5akz1bsk447sybypdwwi07v1836jkgk0wll";
+    sha256 = "64c734274715f392eb4a879172a11c0606d37c02b4a6f23045772af5f8e2a9ec";
   };
   buildInputs = [boost zlib botan libidn lua pcre sqlite];
   preConfigure = ''
