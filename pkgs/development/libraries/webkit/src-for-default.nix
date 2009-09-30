de014f5bb3c69f2df6f6c94cc571ec6311486748
Michael Raskin <7c6f434c@mail.ru>
2009-09-30 14:20:19 +0000

    Hm, fetchurl failed due to transient network problem previous time..
    svn path=/nixpkgs/trunk/; revision=17558

--- pkgs/development/libraries/webkit/src-for-default.nix
+++ pkgs/development/libraries/webkit/src-for-default.nix
@@ -1,7 +1,7 @@
 rec {
    version="r48908";
    name="webkit-r48908";
-   hash="";
+   hash="18hymchzix56yhl830mxfiayxxjhd5kgm3ga4jc9k627ypqsdycg";
    url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r48908.tar.bz2";
    advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r48908.tar.bz2";
   
