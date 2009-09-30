36742ba9651f911394259a6103d91d7590a0b904
Michael Raskin <7c6f434c@mail.ru>
2009-09-30 06:50:32 +0000

    GConf was wrong about include file location..
    svn path=/nixpkgs/trunk/; revision=17532

--- pkgs/desktops/gnome-2.28/platform/GConf/default.nix
+++ pkgs/desktops/gnome-2.28/platform/GConf/default.nix
@@ -7,6 +7,11 @@ stdenv.mkDerivation {
     url = mirror://gnome/sources/GConf/2.28/GConf-2.28.0.tar.bz2;
     sha256 = "1j3ah0f71yv4di3fvv1aahcjvqfwsxw2m71ljbjq0apv5gzdqmyh";
   };
+
+  preConfigure = ''
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${dbus_glib}/include/dbus-1.0 -I${policykit}/include/PolicyKit -I${glib}/include/glib-2.0"
+  '';
+
   buildInputs = [ pkgconfig glib gtk dbus_glib ORBit2 libxml2
                   expat policykit intltool ];
 }
