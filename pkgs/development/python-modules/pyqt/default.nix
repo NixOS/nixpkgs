7d808c4229d4fdb0df0e43ca1d41a8e5ae631469
Sander van der Burg <s.vanderburg@tudelft.nl>
2009-09-30 09:43:49 +0000

    Upgraded PyQt to 4.6
    svn path=/nixpkgs/trunk/; revision=17537

--- pkgs/development/python-modules/pyqt/default.nix
+++ pkgs/development/python-modules/pyqt/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, lib, python, sip, qt4}:
 
 stdenv.mkDerivation {
-  name = "pyqt-x11-gpl-4.5.4";
+  name = "pyqt-x11-gpl-4.6";
   src = fetchurl {
-    url = http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4/PyQt-x11-gpl-4.5.4.tar.gz;
-    sha256 = "1a55zng6yhnbk5swc02bkbyccdgf0f0v94yxk9v5a43hv9xnrl5k";
+    url = http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4/PyQt-x11-gpl-4.6.tar.gz;
+    sha256 = "0an7fadypsz4wyrss4f6ysb6gjgl3ygrchx94vbnc146jpfy7p19";
   };
   configurePhase = "python ./configure.py --confirm-license -b $out/bin -d $out/lib/python2.5/site-packages -v $out/share/sip -p $out/plugins";
   buildInputs = [ python sip qt4 ];
