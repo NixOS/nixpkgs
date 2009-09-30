fbda797a0b00b1dacd1b122130943085bf16c675
Michael Raskin <7c6f434c@mail.ru>
2009-09-30 13:17:29 +0000

    Current Webkit trunk build seems to be broken..
    svn path=/nixpkgs/trunk/; revision=17556

--- pkgs/development/libraries/webkit/src-for-default.nix
+++ pkgs/development/libraries/webkit/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="r48908";
-   name="webkit-r48908";
-   hash="18hymchzix56yhl830mxfiayxxjhd5kgm3ga4jc9k627ypqsdycg";
-   url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r48908.tar.bz2";
-   advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r48908.tar.bz2";
+   version="r48794";
+   name="webkit-r48794";
+   hash="1spdhrvm7k4gmd12lwbx5whi0l3qznaxpsjyki5ldpcm2b0s2cp1";
+   url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r48794.tar.bz2";
+   advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r48794.tar.bz2";
   
   
 }
