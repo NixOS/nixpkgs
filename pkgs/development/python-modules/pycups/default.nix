d68835524d21e774c76685d84f9b6b54b8b385b9
Sander van der Burg <s.vanderburg@tudelft.nl>
2009-10-02 11:13:03 +0000

    Upgraded pycups to 1.9.46
    svn path=/nixpkgs/trunk/; revision=17593

--- pkgs/development/python-modules/pycups/default.nix
+++ pkgs/development/python-modules/pycups/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, python, cups}:
 
 stdenv.mkDerivation {
-  name = "pycups-1.9.45";
+  name = "pycups-1.9.46";
   src = fetchurl {
-    url = http://cyberelk.net/tim/data/pycups/pycups-1.9.45.tar.bz2;
-    md5 = "ff634a6751f8a859ed26751bf03abef0";
+    url = http://cyberelk.net/tim/data/pycups/pycups-1.9.46.tar.bz2;
+    sha256 = "1zn85gkpdzvkiwbmn466cc5yzz2810030dyg5hns5f55n7bd3avl";
   };
   installPhase = "python ./setup.py install --prefix $out";
   buildInputs = [ python cups ];
