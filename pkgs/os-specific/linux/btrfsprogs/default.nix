e4672f42e54130f10e1986c86586ffbda5539eab
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-10-02 15:06:07 +0000

    svn path=/nixpkgs/trunk/; revision=17613

--- pkgs/os-specific/linux/btrfsprogs/default.nix
+++ pkgs/os-specific/linux/btrfsprogs/default.nix
@@ -7,6 +7,9 @@ let
     zlib libuuid acl 
   ];
 in
+
+assert libuuid != null;
+
 rec {
   src = fetchurl {
     url = "http://www.kernel.org/pub/linux/kernel/people/mason/btrfs/btrfs-progs-${version}.tar.bz2";
