cd2e2948a8cd17696a5ad58e3f8281db7ff38f39
Michael Raskin <7c6f434c@mail.ru>
2009-10-06 06:12:08 +0000

    Adding a zen-stable head version
    svn path=/nixpkgs/trunk/; revision=17663

---
+++ pkgs/os-specific/linux/zen-kernel/src-for-zen-stable.nix
@@ -0,0 +1,9 @@
+rec {
+   version="5692deda2c65ef19b303c96c8524dcacb310bc70";
+   name="zen-linux-5692deda2c65ef19b303c96c8524dcacb310bc70";
+   hash="4a89d26c44897babe4f7f69890c1045bdc49d94a39c42534418e2d20dbc3b809";
+   rev="5692deda2c65ef19b303c96c8524dcacb310bc70";
+   url="git://zen-sources.org/kernel/zen-stable.git";
+  
+  
+}
---
+++ pkgs/os-specific/linux/zen-kernel/src-info-for-zen-stable.nix
@@ -0,0 +1,6 @@
+{
+  repoUrl = "git://zen-sources.org/kernel/zen-stable.git";
+  rev = "origin/master";
+  baseName = "zen-linux";
+  method = "fetchgit";
+}
