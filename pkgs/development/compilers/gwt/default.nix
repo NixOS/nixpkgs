49124816ccd94aa33b4139dc14c8b8174ee74b2f
Sander van der Burg <s.vanderburg@tudelft.nl>
2009-10-07 13:03:38 +0000

    Upgraded GWT to 1.5.3
    svn path=/nixpkgs/trunk/; revision=17698

--- pkgs/development/compilers/gwt/default.nix
+++ pkgs/development/compilers/gwt/default.nix
@@ -1,12 +1,12 @@
 {stdenv, fetchurl, glib, gtk, pango, atk, libX11, libXt, libstdcpp5}:
 
 stdenv.mkDerivation {
-  name = "gwt-linux-1.4.61";
+  name = "gwt-linux-1.5.3";
   builder = ./builder.sh;
 
   src = fetchurl {
-    url = http://google-web-toolkit.googlecode.com/files/gwt-linux-1.4.61.tar.bz2;
-    md5 = "5aa5d630716817f7cf22dc2a36c0fcbd";
+    url = http://google-web-toolkit.googlecode.com/files/gwt-linux-1.5.3.tar.bz2;
+    sha1 = "5d7d3295cef4d0eb06a991138e9f538409146027";
   };
 
   inherit glib gtk pango atk libX11 libXt libstdcpp5;
