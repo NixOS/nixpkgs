8cb83682c455b7ed8df6b8d70a936a32d1187655
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-10-07 11:28:35 +0000

    * p7zip updated to 9.04.
    svn path=/nixpkgs/trunk/; revision=17688

--- pkgs/tools/archivers/p7zip/default.nix
+++ pkgs/tools/archivers/p7zip/default.nix
@@ -1,19 +1,22 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "p7zip-4.57";
+  name = "p7zip-9.04";
   
   src = fetchurl {
-    url = mirror://sourceforge/p7zip/p7zip_4.57_src_all.tar.bz2;
-    sha256 = "0lsvw1bh6dzpxn8kyl7s79w2drpfxaa1c79khqm56gfmdlw27s80";
+    url = mirror://sourceforge/p7zip/p7zip_9.04_src_all.tar.bz2;
+    sha256 = "1azr73vlj602hl6siagnqd1rn8sw73lny292jqgspg0lb9wvdyyx";
   };
   
-  preConfigure = "
-    makeFlagsArray=(DEST_HOME=$out)
-  ";
+  preConfigure =
+    ''
+      makeFlagsArray=(DEST_HOME=$out)
+    '';
 
   meta = {
     homepage = http://p7zip.sourceforge.net/;
     description = "A port of the 7-zip archiver";
+    # license = "LGPLv2.1+"; + "unRAR restriction"
+    platforms = [ stdenv.lib.platforms.unix ];
   };
 }
