dc242687b210cfbec6bc0fe52d5ffa81356b795a
Rob Vermaas <rob.vermaas@gmail.com>
2009-10-08 09:26:47 +0000

    updated pgadmin to 1.10.0
    svn path=/nixpkgs/trunk/; revision=17713

--- pkgs/applications/misc/pgadmin/default.nix
+++ pkgs/applications/misc/pgadmin/default.nix
@@ -1,11 +1,10 @@
 args: with args;
-stdenv.mkDerivation {
-  name = "pgadmin3-1.8.1";
+stdenv.mkDerivation rec {
+  name = "pgadmin3-1.10.0";
 
   src = fetchurl {
-    name = "pgadmin3-v1.8.1.tar.gz";
-    url = "http://ftp3.de.postgresql.org/pub/Mirrors/ftp.postgresql.org//pgadmin3/release/v1.8.1/src/pgadmin3-1.8.1.tar.gz";
-    sha256 = "1vnpbgb2ksvcgbzab4jjspwvs5cvam53azinfavjad4kpjczdywb";
+    url = "http://ftp3.de.postgresql.org/pub/Mirrors/ftp.postgresql.org/pgadmin3/release/v1.10.0/src/pgadmin3-1.10.0.tar.gz";
+    sha256 = "1ndi951da3jw5800fjdgkbvl8n6k71x7x16ghihi1l88bilf2a16";
   };
 
   buildInputs = [postgresql wxGTK libxml2 libxslt openssl];
