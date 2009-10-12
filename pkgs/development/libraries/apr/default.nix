fcf92435e310935db14ca50cf0eeecc0ef6a72b7
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-10-12 17:57:27 +0000

    * apr 1.3.9.
    svn path=/nixpkgs/trunk/; revision=17767

--- pkgs/development/libraries/apr/default.nix
+++ pkgs/development/libraries/apr/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation rec {
-  name = "apr-1.3.8";
+  name = "apr-1.3.9";
   
   src = fetchurl {
     url = "mirror://apache/apr/${name}.tar.bz2";
-    sha256 = "0mzsr6pv2gcp1cvppfzsd2c7zqgbw0rakjndcna49gv1dq0zgdvx";
+    sha256 = "1qicxnk62d9mjza8vch2wxy4xlq8sa76chwi5cp6bs4cyj9s61ap";
   };
 
   meta = {
