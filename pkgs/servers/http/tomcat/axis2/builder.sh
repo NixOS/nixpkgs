223789d4118e3feccb5e88faa54f3ed3567e633c
Sander van der Burg <s.vanderburg@tudelft.nl>
2009-10-13 13:21:51 +0000

    Cleaned the expression of tomcat a bit
    svn path=/nixpkgs/trunk/; revision=17777

--- pkgs/servers/http/tomcat/axis2/builder.sh
+++ pkgs/servers/http/tomcat/axis2/builder.sh
@@ -1,14 +1,14 @@
-buildInputs="$unzip $apacheAnt $jdk"
 source $stdenv/setup
 
 unzip $src
 cd axis2-*
-ensureDir $out/share/java/axis2
-cp lib/* $out/share/java/axis2
+ensureDir $out
+cp -av * $out
 cd webapp
 ant
+cd ..
 ensureDir $out/webapps
-cp ../dist/axis2.war $out/webapps
+cp dist/axis2.war $out/webapps
 cd $out/webapps
 mkdir axis2
 cd axis2
--- pkgs/servers/http/tomcat/axis2/default.nix
+++ pkgs/servers/http/tomcat/axis2/default.nix
@@ -1,15 +1,13 @@
 {stdenv, fetchurl, apacheAnt, jdk, unzip}:
 
 stdenv.mkDerivation {
-
   name = "axis2-1.5";
-
-  builder = ./builder.sh;
 
   src = fetchurl {
     url = http://apache.mirror.easycolocate.nl/ws/axis2/1_5/axis2-1.5-bin.zip;
     sha256 = "0f0a471xfsjx7s3i9awhajl1kli8y8pd8aiki7cwb9n4g467rwmc";
   };
 
-  inherit apacheAnt jdk unzip;
+  buildInputs = [ unzip apacheAnt jdk ];
+  builder = ./builder.sh;
 }
