d1e55d0210bc588d16845dc63e77929b186a3f66
Michael Raskin <7c6f434c@mail.ru>
2009-10-15 11:13:21 +0000

    Install monotone Perl API
    svn path=/nixpkgs/trunk/; revision=17818

--- pkgs/applications/version-management/monotone/default.nix
+++ pkgs/applications/version-management/monotone/default.nix
@@ -1,10 +1,11 @@
 {stdenv, fetchurl, boost, zlib, botan, libidn,
-  lua, pcre, sqlite, lib}:
+  lua, pcre, sqlite, perl, lib}:
 
 let 
   version = "0.45";
-in stdenv.mkDerivation {
+in stdenv.mkDerivation rec {
   name = "monotone-${version}";
+  inherit perl;
   src = fetchurl {
     url = "http://monotone.ca/downloads/${version}/monotone-${version}.tar.gz";
     sha256 = "64c734274715f392eb4a879172a11c0606d37c02b4a6f23045772af5f8e2a9ec";
@@ -13,6 +14,12 @@ in stdenv.mkDerivation {
   preConfigure = ''
     export sqlite_LIBS=-lsqlite3
     export NIX_LDFLAGS="$NIX_LDFLAGS -ldl"
+  '';
+  postInstall = ''
+    ensureDir $out/share/${name}
+    cp -r contrib/ $out/share/${name}/contrib
+    ensureDir $out/lib/perl5/site_perl/''${perl##*-perl-}
+    cp contrib/Monotone.pm $out/lib/perl5/site_perl/''${perl##*-perl-}
   '';
   meta = {
     maintainers = [lib.maintainers.raskin];
