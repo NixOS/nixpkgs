ffe3d659862801466002ed710405b89df7230f43
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2009-10-15 22:08:23 +0000

    Making perl DB_File follow the lib/perl5/site_perl convention for the *.pm files, so all hooks on PERL5LIB include this module properly. I don't know why this doesn't install the files to that */site_perl/* by default.
    svn path=/nixpkgs/trunk/; revision=17837

--- pkgs/development/perl-modules/DB_File/default.nix
+++ pkgs/development/perl-modules/DB_File/default.nix
@@ -1,11 +1,11 @@
 {fetchurl, buildPerlPackage, db4}:
 
 buildPerlPackage {
-  name = "DB_File-1.816";
+  name = "DB_File-1.820";
   
   src = fetchurl {
-    url = mirror://cpan/authors/id/P/PM/PMQS/DB_File-1.816.tar.gz;
-    sha256 = "1a668hk5v0l180kbqss2hq9khl756cmrykn8fz1rl4qzsp6lq284";
+    url = mirror://cpan/authors/id/P/PM/PMQS/DB_File-1.820.tar.gz;
+    sha256 = "0jnz5lsrad67j42sdw5bqpkmgiyj45rpiqgkff3i21252k9d5s7a";
   };
 
   preConfigure = ''
@@ -15,5 +15,13 @@ buildPerlPackage {
     LIB = ${db4}/lib
     INCLUDE = ${db4}/include
     EOF
+  '';
+
+  # I don't know about perl paths, but PERL5LIB env var is managed through
+  # lib/perl5/site_perl, and the *.pm should be inside lib/perl5/site_perl/...
+  # for other packages to get that in the PERL5LIB env var.
+  postInstall = ''
+    ensureDir $out/lib/perl5/site_perl/
+    cp -R $out/lib/perl5/5* $out/lib/perl5/site_perl
   '';
 }
