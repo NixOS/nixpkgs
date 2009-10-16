8e9ea2c2093724532072d1fdf9da7f9fd3cb951a
Ludovic Courtès <ludo@gnu.org>
2009-10-16 08:00:36 +0000

    GNU Guile 1.9.4 (alpha).
    svn path=/nixpkgs/trunk/; revision=17843

--- pkgs/development/interpreters/guile/1.9.nix
+++ pkgs/development/interpreters/guile/1.9.nix
@@ -2,39 +2,25 @@
 , gawk, makeWrapper }:
 
 stdenv.mkDerivation rec {
-  name = "guile-1.9.3";  # This is an alpha release!
+  name = "guile-1.9.4";  # This is an alpha release!
   src = fetchurl {
     url = "ftp://alpha.gnu.org/gnu/guile/${name}.tar.gz";
-    sha256 = "10q0k4884b68nba272bg1ym4djpvq35r9m8xw444mrh1jqfz9gsj";
+    sha256 = "1p136fb0s46q1cycfsnd7nny14ji43xva58cz39szvq36p9kjbbg";
   };
 
-  buildInputs = [ makeWrapper gawk readline libtool libunistring ];
-  propagatedBuildInputs = [
-    /* XXX: Temporary Hack so that guile-config(1) gets to see `pkg-config',
-       until this is fixed upstream.  */
-    pkgconfig
-
-    gmp boehmgc
-  ];
+  buildInputs = [ makeWrapper gawk readline libtool libunistring pkgconfig ];
+  propagatedBuildInputs = [ gmp boehmgc ];
 
   postInstall = ''
     wrapProgram $out/bin/guile-snarf --prefix PATH : "${gawk}/bin"
 
-    # XXX: Hack until fixed upstream.
-    # See http://thread.gmane.org/gmane.comp.lib.gnulib.bugs/18903 .
+    # XXX: See http://thread.gmane.org/gmane.comp.lib.gnulib.bugs/18903 for
+    # why `--with-libunistring-prefix' and similar options coming from
+    # `AC_LIB_LINKFLAGS_BODY' don't work on NixOS/x86_64.
     sed -i "$out/lib/pkgconfig/guile-2.0.pc"    \
-        -e 's|-Wl,-z -Wl,relro||g ;
-            s|-lunistring|-L${libunistring}/lib -lunistring|g ;
+        -e 's|-lunistring|-L${libunistring}/lib -lunistring|g ;
             s|^Cflags:\(.*\)$|Cflags: -I${libunistring}/include \1|g ;
             s|-lltdl|-L${libtool}/lib -lltdl|g'
-  '';
-
-  preBuild = ''
-    sed -e '/lt_dlinit/a  lt_dladdsearchdir("'$out/lib'");' -i libguile/dynl.c
-
-    # XXX: Hack until fixed upstream.
-    sed -i "meta/guile-config" \
-        -e "/^exec guile/i export GUILE_AUTO_COMPILE=0"
   '';
 
   doCheck = true;
