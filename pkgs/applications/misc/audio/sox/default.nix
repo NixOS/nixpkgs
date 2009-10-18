523d1b8216215883ea76d1703f63c5ce87f9f890
Marc Weber <marco-oweber@gmx.de>
2009-10-18 04:43:56 +0000

    updating sox, adding maintainer, enabling mp3 support
    svn path=/nixpkgs/trunk/; revision=17864

--- pkgs/applications/misc/audio/sox/default.nix
+++ pkgs/applications/misc/audio/sox/default.nix
@@ -2,11 +2,11 @@ args: with args;
 let inherit (args.composableDerivation) composableDerivation edf; in
 composableDerivation {} {
 
-    name = "sox-14.0.0";
+    name = "sox-14.3.0";
 
     src = args.fetchurl {
-      url = mirror://sourceforge/sox/sox-14.0.0.tar.gz;
-      sha256 = "1l7v04nlvb96y0w9crvm6nq8g50yxp3bkv6nb1c205s982inlalc";
+      url = mirror://sourceforge/sox/sox-14.3.0.tar.gz;
+      sha256 = "15r39dq9nlwrypm0vpxmbxyqqv0bd6284djbi1fdfrlkjhf43gws";
     };
 
     flags =
@@ -17,7 +17,12 @@ composableDerivation {} {
     edf { name = "alsa"; enable = { buildInputs = [alsaLib]; }; }
     // edf { name = "libao"; enable = { buildInputs = [libao]; }; }
     // edf { name = "oss"; }
-    // edf { name = "sun_audio"; };
+    // edf { name = "sun_audio"; }
+    // edf { name = "dl-lame"; enable.buildInputs = [ lame ]; } # use shared library
+    // edf { name = "lame";    enable.buildInputs = [ lame ]; }
+    // edf { name = "dl-mad"; enable.buildInputs = [ libmad ]; } # use shared library
+    // edf { name = "mad";    enable.buildInputs =[ libmad ]; }
+    ;
     # These options should be autodetected by the configure script
     /*
     --without-sndfile       Don't try to use libsndfile
@@ -38,7 +43,12 @@ composableDerivation {} {
   cfg = {
     ossSupport = false;
     sun_audioSupport = false;
-  };
+  } // lib.listToAttrs [
+      { name = "dl-lameSupport"; value = true; }
+      { name = "dl-madSupport"; value = true; }
+  ];
+
+  configureFlags = ["-enable-dl-lame"];
 
 
   optionals = [ "libsndfile" "libogg" "flac" "ffmpeg" "libmad" "lame"
@@ -49,6 +59,7 @@ composableDerivation {} {
   meta = {
     description = "Sample Rate Converter for audio";
     homepage = http://www.mega-nerd.com/SRC/index.html;
+    maintainers = [lib.maintainers.marcweber];
     # you can choose one of the following licenses:
     license = [
       "GPL"
