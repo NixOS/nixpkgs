5b3c7c6b7abf180412668ff56a9ca438022821d3
Marc Weber <marco-oweber@gmx.de>
2009-10-18 04:43:59 +0000

    wireshark update - old source vanished
    svn path=/nixpkgs/trunk/; revision=17865

--- pkgs/applications/networking/sniffers/wireshark/default.nix
+++ pkgs/applications/networking/sniffers/wireshark/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, perl, pkgconfig, gtk, libpcap, flex, bison}:
 
 stdenv.mkDerivation {
-  name = "wireshark-1.1.3";
+  name = "wireshark-1.3.0";
   src = fetchurl {
-    url = http://www.wireshark.org/download/src/wireshark-1.1.3.tar.gz;
-    sha256 = "0j2ng84di0lyjax80mw74lmcipxkmlbld4w2yms93932wbxij3s1";
+    url = http://www.wireshark.org/download/src/wireshark-1.3.0.tar.gz;
+    sha256 = "06vn6yklbg2ajh0gs0j58d4fwkkjxz8xn5f8xlpfkffs2m80aw2r";
   };
   configureFlags = "--with-pcap=${libpcap}";
   buildInputs = [perl pkgconfig gtk libpcap flex bison];
