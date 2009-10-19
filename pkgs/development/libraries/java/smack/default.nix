d4e8a731dfb3e8152b9f4975500fe07bb42b726e
Sander van der Burg <s.vanderburg@tudelft.nl>
2009-10-19 15:02:24 +0000

    Upgraded smack library to 3.1.0
    svn path=/nixpkgs/trunk/; revision=17885

--- pkgs/development/libraries/java/smack/default.nix
+++ pkgs/development/libraries/java/smack/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "smack-3_0_4";
+  name = "smack-3_1_0";
   builder = ./builder.sh;
   
   src = fetchurl {
-    url = http://www.igniterealtime.org/downloadServlet?filename=smack/smack_3_0_4.tar.gz;
-    md5 = "a7eb7df35ed8ab959badd370f995c671";
+    url = http://www.igniterealtime.org/downloadServlet?filename=smack/smack_3_1_0.tar.gz;
+    sha256 = "02kn3i7py6ilnchz0yn4v2g0sh8msxcw61kankqrz2a65852i28i";
   };  
 }
