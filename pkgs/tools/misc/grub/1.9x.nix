26d2ee22e2458910160aaa43bf7042c66a0e66cb
Ludovic Courtès <ludo@gnu.org>
2009-10-26 13:58:58 +0000

    GNU GRUB 1.97.
    svn path=/nixpkgs/trunk/; revision=17965

--- pkgs/tools/misc/grub/1.9x.nix
+++ pkgs/tools/misc/grub/1.9x.nix
@@ -7,12 +7,11 @@ let unifont_bdf = fetchurl {
 in
 
 stdenv.mkDerivation rec {
-  name = "grub-1.97beta3";
+  name = "grub-1.97";
 
   src = fetchurl {
-    url = "ftp://alpha.gnu.org/gnu/grub/grub-1.97~beta3.tar.gz";
-    sha256 = "1drbv8157xs5v76smls1n14i5c9lahybgwdqvk9w4imcakfnsfca";
-    name = "${name}.tar.gz";
+    url = "ftp://alpha.gnu.org/gnu/grub/${name}.tar.gz";
+    sha256 = "0wsnwrng8h3d3mkwyxpc9a4c5vrsjc6c7lxfxmrpqvdb4yb2wn4d";
   };
 
   buildInputs = [ bison ncurses libusb freetype ];
