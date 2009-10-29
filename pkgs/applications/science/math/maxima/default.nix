5dd325255706ea104ecc9c96968b62bef2851047
Peter Simons <simons@cryp.to>
2009-10-29 11:57:45 +0000

    Updated Maxima to version 5.19.2.
    svn path=/nixpkgs/trunk/; revision=18014

--- pkgs/applications/science/math/maxima/default.nix
+++ pkgs/applications/science/math/maxima/default.nix
@@ -1,13 +1,18 @@
-args: with args;
+{ stdenv, fetchurl, clisp }:
+
+let
+    name    = "maxima";
+    version = "5.19.2";
+in
 stdenv.mkDerivation {
-  name = "maxima-5.17.0";
+  name = "${name}-${version}";
 
   src = fetchurl {
-    url = "mirror://sourceforge/maxima/maxima-5.17.0.tar.gz";
-    sha256 = "1nzphlm4al0j8jcgalscdqfcgkbscl68qz2mkm8n8804mss32alj";
+    url = "mirror://sourceforge/${name}/${name}-${version}.tar.gz";
+    sha256 = "4b9d592cb5c5b49acf10c894aa4e899bd47f079b315ee22542122a2e64589072";
   };
 
-  buildInputs =[clisp];
+  buildInputs = [clisp];
 
   meta = {
     description = "Maxima computer algebra system";
