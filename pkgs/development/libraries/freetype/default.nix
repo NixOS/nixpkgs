1b25d01703414cc61f3e540905bedad92612e82c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-10-29 12:48:47 +0000

    * FreeType updated to 2.3.11.  Also added an option to enable the   patent-encumbered hinting and sub-pixel rendering.  It's disabled by   default.  (Or should it be enabled by default?)
    svn path=/nixpkgs/branches/xorg-7.5/; revision=18019

--- pkgs/development/libraries/freetype/default.nix
+++ pkgs/development/libraries/freetype/default.nix
@@ -1,17 +1,29 @@
-{stdenv, fetchurl}:
+{ stdenv, fetchurl
+
+, # FreeType supports hinting using a TrueType bytecode interpreter,
+  # as well as sub-pixel rendering.  These are patented by Apple and
+  # Microsoft, respectively, so they are disabled by default.  This
+  # option allows them to be enabled.  See
+  # http://www.freetype.org/patents.html.
+  useEncumberedCode ? false
+}:
 
 stdenv.mkDerivation rec {
-  name = "freetype-2.3.9";
+  name = "freetype-2.3.11";
   
   src = fetchurl {
     url = "mirror://sourceforge/freetype/${name}.tar.bz2";
-    sha256 = "1dia4j01aqdcrkpfkcniswcrccdx4jx2p3hyhbh76kchx6y3782i";
+    sha256 = "1j9f3q7vkdhlcxmfhkkyvxmniih2gcsb428v73mfk88qc0g3n0wa";
   };
 
   configureFlags = "--disable-static";
 
+  NIX_CFLAGS_COMPILE = stdenv.lib.optionalString useEncumberedCode
+    "-DFT_CONFIG_OPTION_SUBPIXEL_RENDERING=1 -DTT_CONFIG_OPTION_BYTECODE_INTERPRETER=1";
+
   meta = {
     description = "A font rendering engine";
     homepage = http://www.freetype.org/;
+    license = "GPLv2+"; # or the FreeType License (BSD + advertising clause)
   };
 }
