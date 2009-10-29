71f6b12135d33b2ca2aabba31db60f4173182b7f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-10-29 14:52:10 +0000

    * libdrm updated to 2.4.15. * libpthread_stubs updated to 0.3.
    svn path=/nixpkgs/branches/xorg-7.5/; revision=18029

--- pkgs/development/libraries/pthread-stubs/default.nix
+++ pkgs/development/libraries/pthread-stubs/default.nix
@@ -1,8 +1,15 @@
-{stdenv, fetchurl}:
-stdenv.mkDerivation {
-  name = "pthread-stubs-0.1";
+{ stdenv, fetchurl }:
+
+stdenv.mkDerivation rec {
+  name = "libpthread-stubs-0.3";
+  
   src = fetchurl {
-    url = http://xcb.freedesktop.org/dist/libpthread-stubs-0.1.tar.bz2;
+    url = "http://xcb.freedesktop.org/dist/${name}.tar.bz2";
     sha256 = "0raxl73kmviqinp00bfa025d0j4vmfjjcvfn754mi60mw48swk80";
+  };
+
+  meta = {
+    homepage = http://xcb.freedesktop.org/;
+    license = "bsd";
   };
 }
