f210a41f844b7442f69300b9980db141e1cf5190
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-10-29 14:13:07 +0000

    * Shocking: a new version of libjpeg!
    svn path=/nixpkgs/branches/xorg-7.5/; revision=18024

--- pkgs/development/libraries/libjpeg/default.nix
+++ pkgs/development/libraries/libjpeg/default.nix
@@ -1,29 +1,18 @@
-{stdenv, fetchurl, libtool, static ? false, ...}: 
+{ stdenv, fetchurl, libtool, static ? false }: 
 
 stdenv.mkDerivation {
-  name = "libjpeg-6b";
-  
-  builder = ./builder.sh;
+  name = "libjpeg-7";
   
   src = fetchurl {
-    url = http://www.ijg.org/files/jpegsrc.v6b.tar.gz;
-    md5 = "dbd5f3b47ed13132f04c685d608a7547";
+    url = http://www.ijg.org/files/jpegsrc.v7.tar.gz;
+    sha256 = "1gvy6f83pskxrxwnxqah3g9mhnlgi6aph39b99609gn50ri8ddsh";
   };
   
-  inherit libtool;
-
   configureFlags = "--enable-shared ${if static then " --enable-static" else ""}";
-    
-  # Required for building of dynamic libraries on Darwin.
-  patches = [
-    (fetchurl {
-      url = http://svn.macports.org/repository/macports/trunk/dports/graphics/jpeg/files/patch-ltconfig;
-      md5 = "e6725fa4a09aa1de4ca75343fd0f61d5";
-    })
-    (fetchurl {
-      url = http://svn.macports.org/repository/macports/trunk/dports/graphics/jpeg/files/patch-ltmain.sh;
-      #md5 = "489986ad8e7a93aef036766b25f321d5";
-      md5 = "092a12aeb0c386dd7dae059109d950ba";
-    })
-  ];
+
+  meta = {
+    homepage = http://www.ijg.org/;
+    description = "A library that implements the JPEG image file format";
+    license = "free";
+  };
 }
