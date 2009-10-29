7133275ca0db2b22cb2a68f430eee8b59664c3b7
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-10-29 18:17:45 +0000

    * Purity: don't do a mkdir /var/cache/fontconfig.
    svn path=/nixpkgs/branches/xorg-7.5/; revision=18036

--- pkgs/development/libraries/fontconfig/default.nix
+++ pkgs/development/libraries/fontconfig/default.nix
@@ -13,7 +13,8 @@ stdenv.mkDerivation rec {
 
   configureFlags = "--with-confdir=/etc/fonts --with-cache-dir=/var/cache/fontconfig --disable-docs --with-default-fonts=";
 
-  installFlags = "CONFDIR=$(out)/etc RUN_FC_CACHE_TEST=false";
+  # Don't try to write to /etc/fonts or /var/cache/fontconfig at install time.
+  installFlags = "CONFDIR=$(out)/etc RUN_FC_CACHE_TEST=false fc_cachedir=$(out)/dummy";
 
   meta = {
     description = "A library for font customization and configuration";
