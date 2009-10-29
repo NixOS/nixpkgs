e9a6fc3bc1a0d388b2b2057bc50edf36c2b7fa83
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-10-29 17:19:25 +0000

    * Mesa 7.5.2.
    svn path=/nixpkgs/branches/xorg-7.5/; revision=18033

--- pkgs/development/libraries/mesa/default.nix
+++ pkgs/development/libraries/mesa/default.nix
@@ -1,26 +1,27 @@
-{stdenv, fetchurl, pkgconfig, x11, xlibs, libdrm, expat}:
+{ stdenv, fetchurl, pkgconfig, x11, xlibs, libdrm, expat }:
 
 if stdenv.system != "i686-linux" && stdenv.system != "x86_64-linux" && stdenv.system != "i686-darwin" && stdenv.system != "i686-freebsd" then
   throw "unsupported platform for Mesa"
 else
 
 stdenv.mkDerivation {
-  name = "mesa-7.4.1";
+  name = "mesa-7.5.2";
   
   src = fetchurl {
-    url = mirror://sourceforge/mesa3d/MesaLib-7.4.1.tar.bz2;
-    md5 = "423260578b653818ba66c2fcbde6d7ad";
+    url = ftp://ftp.freedesktop.org/pub/mesa/7.5.2/MesaLib-7.5.2.tar.bz2;
+    md5 = "94e47a499f1226803869c2e37a6a8e3a";
   };
   
-  buildInputs = [
-    pkgconfig expat x11 libdrm xlibs.glproto
-    xlibs.libXxf86vm xlibs.libXfixes xlibs.libXdamage xlibs.dri2proto
-  ];
+  buildInputs =
+    [ pkgconfig expat x11 libdrm xlibs.glproto
+      xlibs.libXxf86vm xlibs.libXfixes xlibs.libXdamage xlibs.dri2proto
+    ];
   
-  passthru = {inherit libdrm;};
+  passthru = { inherit libdrm; };
   
   meta = {
     description = "An open source implementation of OpenGL";
     homepage = http://www.mesa3d.org/;
+    license = "bsd";
   };
 }
