18e5da0f60c09100928f99e329cba35a73d2d29a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-11-01 14:14:27 +0000

    * Delete the old Gnome.
    svn path=/nixpkgs/branches/xorg-7.5/; revision=18062

--- pkgs/desktops/gnome/libIDL.nix
+++
@@ -1,8 +0,0 @@
-{input, stdenv, fetchurl, pkgconfig, glib, lex, yacc}:
-
-assert pkgconfig != null && glib != null && lex != null && yacc != null;
-
-stdenv.mkDerivation {
-  inherit (input) name src;
-  buildInputs = [pkgconfig glib lex yacc];
-}
