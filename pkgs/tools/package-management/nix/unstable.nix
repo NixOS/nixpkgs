7b8a7854972f5c9fae4d7d3e80ac9b548e82aeae
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-11-01 14:01:05 +0000

    svn path=/nixpkgs/trunk/; revision=18061

--- pkgs/tools/package-management/nix/unstable.nix
+++ pkgs/tools/package-management/nix/unstable.nix
@@ -7,11 +7,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "nix-0.13pre17232${nameSuffix}";
+  name = "nix-0.13pre17922${nameSuffix}";
   
   src = fetchurl {
-    url = "http://hydra.nixos.org/build/75293/download/4/${name}.tar.bz2";
-    sha256 = "aaea96d6dd87f8cceb2973e561d1cd0ca1beeaa0384eb91f4db09ac75d42148f";
+    url = "http://hydra.nixos.org/build/105957/download/4/${name}.tar.bz2";
+    sha256 = "11735f2d01ed1c4a7dd345690cd6bbfec175626a6bf2c0d76a27da3c5f51c187";
   };
 
   buildInputs = [perl curl openssl];
