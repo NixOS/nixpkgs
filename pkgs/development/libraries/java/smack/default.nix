248fcdc3bb6a6460bc90ad5845577a783729238f
Sander van der Burg <s.vanderburg@tudelft.nl>
2009-11-02 15:12:17 +0000

    Downgraded smack to 3.0.4. It seems that 3.1.0 contains some annoying bugs
    svn path=/nixpkgs/trunk/; revision=18071

--- pkgs/development/libraries/java/smack/default.nix
+++ pkgs/development/libraries/java/smack/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "smack-3_1_0";
+  name = "smack-3_0_4";
   builder = ./builder.sh;
   
   src = fetchurl {
-    url = http://www.igniterealtime.org/downloadServlet?filename=smack/smack_3_1_0.tar.gz;
-    sha256 = "02kn3i7py6ilnchz0yn4v2g0sh8msxcw61kankqrz2a65852i28i";
+    url = http://www.igniterealtime.org/downloadServlet?filename=smack/smack_3_0_4.tar.gz;
+    sha256 = "075nn7vwfjr2a9j6ycikkbssxhai82vpvll9123r83rar3ds3li6";
   };  
 }
