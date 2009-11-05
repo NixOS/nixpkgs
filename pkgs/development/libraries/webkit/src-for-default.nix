5638a3360031d27c7ec83deddbd9db1b85d4e751
Michael Raskin <7c6f434c@mail.ru>
2009-11-05 23:11:34 +0000

    Update WebKit
    svn path=/nixpkgs/trunk/; revision=18172

--- pkgs/development/libraries/webkit/src-for-default.nix
+++ pkgs/development/libraries/webkit/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="r49957";
-   name="webkit-r49957";
-   hash="0nq6hyzxx7xyi85200yb2dgvfz2lrkwl1fxyiaic27f4b9mmdp1c";
-   url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r49957.tar.bz2";
-   advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r49957.tar.bz2";
+   version="r50540";
+   name="webkit-r50540";
+   hash="07gjazgkjnvigkl7pa9r8hzw74dkx9m7m7q2xfnayp9lv79r2wp4";
+   url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r50540.tar.bz2";
+   advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r50540.tar.bz2";
   
   
 }
