194a24bcf1afd384a588ba78b672ebda7c0ce186
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-11-06 01:00:44 +0000

    * Support a `system' argument for if you want to do hacky things like   change a NixOS installation from x86_64-linux to i686-linux (or vice   versa if you have distributed builds enabled - I used this upgrade   from 32-bit to 64-bit NixOS).
    svn path=/nixos/trunk/; revision=18177

--- default.nix
+++ default.nix
@@ -1,13 +1,18 @@
 { configuration ? import ./lib/from-env.nix "NIXOS_CONFIG" /etc/nixos/configuration.nix
+, system ? builtins.currentSystem
 }:
 
 let
   
-  eval = import ./lib/eval-config.nix { modules = [ configuration ]; };
+  eval = import ./lib/eval-config.nix {
+    inherit system;
+    modules = [ configuration ];
+  };
 
   inherit (eval) config pkgs;
 
   vmConfig = (import ./lib/eval-config.nix {
+    inherit system;
     modules = [ configuration ./modules/virtualisation/qemu-vm.nix ];
   }).config;
     
