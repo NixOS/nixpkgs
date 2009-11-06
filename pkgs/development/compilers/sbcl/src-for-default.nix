e78e88ffcf7edad05f44654915de52a24ed2bd5b
Michael Raskin <7c6f434c@mail.ru>
2009-11-06 11:23:06 +0000

    Blacklist more versions of SBCL?
    svn path=/nixpkgs/trunk/; revision=18194

--- pkgs/development/compilers/sbcl/src-for-default.nix
+++ pkgs/development/compilers/sbcl/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="1.0.31";
-   name="sbcl-1.0.31";
-   hash="1p49l9ycpqz6x3xs2x6dr5h9ih1kwxyz0j3kqa0x3rj999p4s9nh";
-   url="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.31/sbcl-1.0.31-source.tar.bz2";
-   advertisedUrl="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.31/sbcl-1.0.31-source.tar.bz2";
+   version="1.0.30";
+   name="sbcl-1.0.30";
+   hash="0cs9671jgmn50835m8ccssmq86ssj3pvpd9cjkkq5c3ggm1zddnq";
+   url="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.30/sbcl-1.0.30-source.tar.bz2";
+   advertisedUrl="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.30/sbcl-1.0.30-source.tar.bz2";
   
   
 }
