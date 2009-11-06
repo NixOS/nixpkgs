bc2ba3677bc6b8dab77dcdbdf0c59e2a55d88b0d
Michael Raskin <7c6f434c@mail.ru>
2009-11-06 19:27:32 +0000

    I think I rolled back to original sbcl-1.0.29 state
    svn path=/nixpkgs/trunk/; revision=18223

--- pkgs/development/compilers/sbcl/src-for-default.nix
+++ pkgs/development/compilers/sbcl/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="1.0.29.54.rc1";
-   name="sbcl-1.0.29.54.rc1";
-   hash="0lmawwf1p3qiyyfd98bryq2k6l1kfhx88pvpavwy3m082dmk66gm";
-   url="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.29/sbcl-1.0.29.54.rc1-source.tar.bz2";
-   advertisedUrl="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.29/sbcl-1.0.29.54.rc1-source.tar.bz2";
+   version="1.0.29";
+   name="sbcl-1.0.29";
+   hash="0ajgkisqxdcc0zivvaqgr48k9b94adm6s11hl061glw43vlh2bff";
+   url="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.29/sbcl-1.0.29-source-r2.tar.bz2";
+   advertisedUrl="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.29/sbcl-1.0.29-source-r2.tar.bz2";
   
   
 }
--- pkgs/development/compilers/sbcl/src-info-for-default.nix
+++ pkgs/development/compilers/sbcl/src-info-for-default.nix
@@ -2,7 +2,7 @@
   baseName = "sbcl";
   downloadPage = "http://sourceforge.net/projects/sbcl/files/";
   choiceCommand = "head -1 | sed -e 's@/download@@;'\"$skipRedirectSF\"";
-  sourceRegexp = "source[.]tar";
+  sourceRegexp = "source[.-].*tar";
   versionExtractorSedScript = "s/.*-([0-9.rc]+)-.*/\\1/";
-  blacklistRegexp = "1[.]0[.]3[012]";
+  blacklistRegexp = "1[.]0[.]3[012]|1[.]0[.]29[.]54[.]rc1";
 }
