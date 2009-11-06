1dce71822eda6f6fc70de67f2193d99d6d5702ac
Michael Raskin <7c6f434c@mail.ru>
2009-11-06 18:36:16 +0000

    Missed some added letters in SBCL version number..
    svn path=/nixpkgs/trunk/; revision=18217

--- pkgs/development/compilers/sbcl/src-for-default.nix
+++ pkgs/development/compilers/sbcl/src-for-default.nix
@@ -1,8 +1,8 @@
 rec {
-   version="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.29/sbcl-1.0.29.54.rc1-source.tar.bz2";
-   name="sbcl-http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.29/sbcl-1.0.29.54.rc1-source.tar.bz2";
+   version="1.0.29.54.rc1";
+   name="sbcl-1.0.29.54.rc1";
    hash="0lmawwf1p3qiyyfd98bryq2k6l1kfhx88pvpavwy3m082dmk66gm";
-   url="";
+   url="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.29/sbcl-1.0.29.54.rc1-source.tar.bz2";
    advertisedUrl="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.29/sbcl-1.0.29.54.rc1-source.tar.bz2";
   
   
--- pkgs/development/compilers/sbcl/src-info-for-default.nix
+++ pkgs/development/compilers/sbcl/src-info-for-default.nix
@@ -3,6 +3,6 @@
   downloadPage = "http://sourceforge.net/projects/sbcl/files/";
   choiceCommand = "head -1 | sed -e 's@/download@@;'\"$skipRedirectSF\"";
   sourceRegexp = "source[.]tar";
-  versionExtractorSedScript = "s/.*-([0-9.]+)-.*/\\1/";
+  versionExtractorSedScript = "s/.*-([0-9.rc]+)-.*/\\1/";
   blacklistRegexp = "1[.]0[.]3[012]";
 }
