1114086eba6747989740724d2ed843d05021e399
Andres Löh <mail@andres-loeh.de>
2009-11-06 10:30:51 +0000

    Version bump for MazesOfMonad.
    svn path=/nixpkgs/trunk/; revision=18191

--- pkgs/games/MazesOfMonad/default.nix
+++ pkgs/games/MazesOfMonad/default.nix
@@ -2,9 +2,9 @@
 
 cabal.mkDerivation (self : {
   pname = "MazesOfMonad";
-  version = "1.0.2";
+  version = "1.0.5";
   name = self.fname;
-  sha256 = "cb5833d509a96fe7411b5eba981bd939da2942b47595d99b861028b9328a4748";
+  sha256 = "a068df09975e7a3060cbd69191cbf99cb3a7b0ee524deb61eef4c52e7fada3f3";
   propagatedBuildInputs = [HUnit mtl regexPosix time];
   meta = {
     description = "Console-based Role Playing Game";
