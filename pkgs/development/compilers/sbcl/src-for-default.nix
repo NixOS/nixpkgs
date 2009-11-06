fe775f8c71822fd49303195db8ddd989978bb0dd
Michael Raskin <7c6f434c@mail.ru>
2009-11-06 10:57:42 +0000

    Oops, forgot to use blacklist-enabled updater
    svn path=/nixpkgs/trunk/; revision=18193

--- pkgs/development/compilers/sbcl/src-for-default.nix
+++ pkgs/development/compilers/sbcl/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="1.0.32";
-   name="sbcl-1.0.32";
-   hash="00qwj4wfpk4d5a2mskpr3l9ahnqbvz2av878lcmrc7yj1y64vdqs";
-   url="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.32/sbcl-1.0.32-source.tar.bz2";
-   advertisedUrl="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.32/sbcl-1.0.32-source.tar.bz2";
+   version="1.0.31";
+   name="sbcl-1.0.31";
+   hash="1p49l9ycpqz6x3xs2x6dr5h9ih1kwxyz0j3kqa0x3rj999p4s9nh";
+   url="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.31/sbcl-1.0.31-source.tar.bz2";
+   advertisedUrl="http://downloads.sourceforge.net/sbcl/files/sbcl/1.0.31/sbcl-1.0.31-source.tar.bz2";
   
   
 }
