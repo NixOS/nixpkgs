1c8f15b0aa1e7abaf9f38651fd44dd1de6bed2cf
Ludovic Courtès <ludo@gnu.org>
2009-11-06 22:44:18 +0000

    LZO: Build the shared library.
    svn path=/nixpkgs/trunk/; revision=18232

--- pkgs/development/libraries/lzo/default.nix
+++ pkgs/development/libraries/lzo/default.nix
@@ -7,7 +7,9 @@ stdenv.mkDerivation rec {
     url = "${meta.homepage}/download/${name}.tar.gz";
     sha256 = "8b1b0da8f757b9ac318e1c15a0eac8bdb56ca902a2dd25beda06c0f265f22591";
   };
-  
+
+  configureFlags = "--enable-shared";
+
   meta = {
     description = "A data compresion library suitable for real-time data de-/compression";
     homepage = http://www.oberhumer.com/opensource/lzo;
