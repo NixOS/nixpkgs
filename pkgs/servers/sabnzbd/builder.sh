d24e7434ec3f35ab123c78252686cff411a591dc
Wouter den Breejen <uu@denbreejen.net>
2009-11-08 19:59:56 +0000

    Added sabnzbd usenet download tool :)
    svn path=/nixpkgs/trunk/; revision=18295

--- pkgs/servers/sabnzbd/builder.sh
+++ pkgs/servers/sabnzbd/builder.sh
@@ -3,10 +3,10 @@ source $stdenv/setup
 tar xvfz $src
 mv SABnzbd-* $out
 
--- Create a start script and let wrapProgram with toPythonPath wrap it so that python is started with cheetahTemplate in its importpath (classpath)
+# Create a start script and let wrapProgram with toPythonPath wrap it so that python is started with cheetahTemplate in its importpath (classpath)
 mkdir $out/bin
-echo "$python/bin/python $out/SABnzbd.py" > $out/bin/sabnzbd.sh
-chmod +x $out/bin/sabnzbd.sh
+echo "$python/bin/python $out/SABnzbd.py" > $out/bin/sabnzbd
+chmod +x $out/bin/sabnzbd
 
 for i in $(cd $out/bin && ls); do
   wrapProgram $out/bin/$i \
