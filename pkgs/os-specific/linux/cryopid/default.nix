e3204aa4073479ba659872b3e96c762e1f5e2bc1
Marco Maggesi <maggesi@math.unifi.it>
2009-11-08 10:38:07 +0000

    New version of cryopid.
    Update package cryopid to a new (unstable) version.
    The old (stable) version of cryopid works only with old linux kernels.
    
    svn path=/nixpkgs/trunk/; revision=18286

--- pkgs/os-specific/linux/cryopid/default.nix
+++ pkgs/os-specific/linux/cryopid/default.nix
@@ -2,36 +2,25 @@
 
 let
 
-  src32 = fetchurl {
-    url = http://dagobah.ucc.asn.au/wacky/cryopid-0.5.9.1-i386.tar.gz;
-    sha256 = "14175pc87x932c09cl1n5iwc5v1086gd4xpb4pz7d5fvqpaxca3h";
-  };
+  pname = "cryopid";
+  version = "20090908";
+  revision = "7da69201d50e";
 
-  src64 = fetchurl {
-    url = http://dagobah.ucc.asn.au/wacky/cryopid-0.5.9.1-x86_64.tar.gz;
-    sha256 = "0y3h9fvb59c8i07das5srhprnsbj1i9m93fp37mzqcjxi2gwjw3b";
-  };
-  
 in
 
-stdenv.mkDerivation {
-  name = "cryopid-0.5.9.1";
+stdenv.mkDerivation rec {
+  name = "${pname}-${version}";
 
-  src =
-    if stdenv.system == "i686-linux" then src32
-    else if stdenv.system == "x86_64-linux" then src64
-    else throw "unsupported platform for CryoPID";
+  src = fetchurl {
+    url = "https://sharesource.org/hg/cryopid/archive/${revision}.tar.bz2";
+    sha256 = "908a4b1cb26322ee25afe13ff59e0d86f669538cb4583766b15ca79fda6c69ca";
+  };
 
-  buildInputs = [zlibStatic];
+  buildInputs = [ zlibStatic ];
 
-  buildPhase = ''
-    make -C src ARCH=i386
-  '';
+  preBuild = "cd src";
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp src/freeze $out/bin
-  '';
+  installPhase = "mkdir -p $out/bin; cp cryopid $out/bin";
 
   meta = {
     description = "A process freezer for Linux";
@@ -40,7 +29,7 @@ stdenv.mkDerivation {
       and save it to a file.  This file can then be used to resume the process
       later on, either after a reboot or even on another machines.
     '';
-    homepage = http://cryopid.berlios.de;
+    homepage = http://sharesource.org/project/cryopid;
     license = ''
       Modified BSD license (without advertising clause).  CryoPID ships with
       and links against the dietlibc library, which is distributed under the
