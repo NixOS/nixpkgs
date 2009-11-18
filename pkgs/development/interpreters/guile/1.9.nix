1f9e87871ea9f9db2c05ce1e9a59a101d5beb2fe
Ludovic Courtès <ludo@gnu.org>
2009-11-18 13:41:17 +0000

    GNU Guile 1.9: Remove unnecessary patch.
    svn path=/nixpkgs/trunk/; revision=18418

--- pkgs/development/interpreters/guile/1.9.nix
+++ pkgs/development/interpreters/guile/1.9.nix
@@ -19,8 +19,8 @@ rec {
   propagatedBuildInputs = [ gmp boehmgc ];
 
   patches =
-    stdenv.lib.optionals (coverageAnalysis != null)
-      [ ./gcov-file-name.patch ./disable-gc-sensitive-tests.patch ];
+    stdenv.lib.optional (coverageAnalysis != null)
+      ./disable-gc-sensitive-tests.patch;
 
   postInstall = ''
     wrapProgram $out/bin/guile-snarf --prefix PATH : "${gawk}/bin"
