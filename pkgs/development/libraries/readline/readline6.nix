5c14f92b303cb22ea92969a3313a7519fe05f8bb
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2009-11-19 22:15:13 +0000

    Reverting a wrong name change I once did, when I thought that we would update all the naming in nixpkgs to match the new build/host cross compilation stdenv. Nevertheless, we decided not to do the renaming, but I forgot this change in readline until ludo told me about it.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=18474

--- pkgs/development/libraries/readline/readline6.nix
+++ pkgs/development/libraries/readline/readline6.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
     sha256 = "1pn13j6f9376kwki69050x3zh62yb1w31l37rws5nwr5q02xk68i";
   };
 
-  propagatedHostInputs = [ncurses];
+  propagatedBuildInputs = [ncurses];
 
   patchFlags = "-p0";
   patches =
