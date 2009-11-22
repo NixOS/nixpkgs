04a15b29b08913b4e7025fec445ac8a2c7b8ac1d
Ludovic Courtès <ludo@gnu.org>
2009-11-22 16:03:56 +0000

    glibc 2.11: Factorize the libc/info/locales expressions.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=18526

--- pkgs/development/libraries/glibc-2.11/default.nix
+++ pkgs/development/libraries/glibc-2.11/default.nix
@@ -5,116 +5,49 @@
 , gccCross ? null
 }:
 
-/* FIXME: Update `locales.nix' and `info.nix'.  */
-let version = "2.11"; in
+let build = import ./common.nix;
+in
+  build ({
+    name = "glibc" +
+      stdenv.lib.optionalString (cross != null) "-${cross.config}";
 
-stdenv.mkDerivation rec {
-  name = "glibc-${version}" +
-    stdenv.lib.optionalString (cross != null) "-${cross.config}";
+    inherit fetchurl stdenv kernelHeaders installLocales profilingLibraries
+      cross gccCross;
 
-  builder = ./builder.sh;
+    builder = ./builder.sh;
 
-  src = fetchurl {
-    url = "mirror://gnu/glibc/${name}.tar.bz2";
-    sha256 = "0b6nbr89qmqcvzz26ggnw7gcxhvnzbc8z299h12wqjmcix4hxwcy";
-  };
+    preInstall = ''
+      ensureDir $out/lib
+      ln -s ${stdenv.gcc.gcc}/lib/libgcc_s.so.1 $out/lib/libgcc_s.so.1
+    '';
 
-  inherit kernelHeaders installLocales;
-  crossConfig = if (cross != null) then cross.config else null;
+    postInstall = ''
+      rm $out/lib/libgcc_s.so.1
+    '';
 
-  inherit (stdenv) is64bit;
+    meta.description = "The GNU C Library";
+  }
 
-  patches = [
-    /* Fix for NIXPKGS-79: when doing host name lookups, when
-       nsswitch.conf contains a line like
+  //
 
-         hosts: files mdns4_minimal [NOTFOUND=return] dns mdns4
+  (if cross != null
+   then {
+      preConfigure = ''
+        sed -i s/-lgcc_eh//g "../$sourceRoot/Makeconfig"
 
-       don't return an error when mdns4_minimal can't be found.  This
-       is a bug in Glibc: when a service can't be found, NSS should
-       continue to the next service unless "UNAVAIL=return" is set.
-       ("NOTFOUND=return" refers to the service returning a NOTFOUND
-       error, not the service itself not being found.)  The reason is
-       that the "status" variable (while initialised to UNAVAIL) is
-       outside of the loop that iterates over the services, the
-       "files" service sets status to NOTFOUND.  So when the call to
-       find "mdns4_minimal" fails, "status" will still be NOTFOUND,
-       and it will return instead of continuing to "dns".  Thus, the
-       line
+        cat > config.cache << "EOF"
+libc_cv_forced_unwind=yes
+libc_cv_c_cleanup=yes
+libc_cv_gnu89_inline=yes
+EOF
+        export BUILD_CC=gcc
+        export CC="$crossConfig-gcc"
+        export AR="$crossConfig-ar"
+        export RANLIB="$crossConfig-ranlib"
 
-         hosts: mdns4_minimal [NOTFOUND=return] dns mdns4
-
-       does work because "status" will contain UNAVAIL after the
-       failure to find mdns4_minimal. */
-    ./nss-skip-unavail.patch
-
-    /* Make it possible to override the locale-archive in NixOS. */
-    ./locale-override.patch
-
-    /* Have rpcgen(1) look for cpp(1) in $PATH.  */
-    ./rpcgen-path.patch
-
-    /* Make sure `nscd' et al. are linked against `libssp'.  */
-    ./stack-protector-link.patch
-  ];
-
-  configureFlags = [
-    "--enable-add-ons"
-    "--with-headers=${kernelHeaders}/include"
-    (if profilingLibraries then "--enable-profile" else "--disable-profile")
-  ] ++ stdenv.lib.optionals (cross != null) [
-    "--host=${cross.config}"
-    "--build=${stdenv.system}"
-    "--with-tls"
-    "--enable-kernel=2.6.0"
-    "--without-fp"
-    "--with-__thread"
-  ] ++ (if (stdenv.system == "armv5tel-linux") then [
-    "--host=arm-linux-gnueabi"
-    "--build=arm-linux-gnueabi"
-    "--without-fp"
-  ] else []);
-
-  buildInputs = stdenv.lib.optionals (cross != null) [ gccCross ];
-
-  preInstall = ''
-    ensureDir $out/lib
-    ln -s ${stdenv.gcc.gcc}/lib/libgcc_s.so.1 $out/lib/libgcc_s.so.1
-  '';
-
-  postInstall = ''
-    rm $out/lib/libgcc_s.so.1
-  '';
-
-  meta = {
-    homepage = http://www.gnu.org/software/libc/;
-    description = "The GNU C Library";
-
-    longDescription =
-      '' Any Unix-like operating system needs a C library: the library which
-         defines the "system calls" and other basic facilities such as
-         open, malloc, printf, exit...
-
-         The GNU C library is used as the C library in the GNU system and
-         most systems with the Linux kernel.
+        # The host strip will destroy everything in the target binaries
+        # otherwise.
+        dontStrip=1
       '';
-
-    license = "LGPLv2+";
-
-    maintainers = [ stdenv.lib.maintainers.ludo ];
-    platforms = stdenv.lib.platforms.linux;
-  };
-}
-
-//
-
-(if (stdenv.system == "i686-linux")
- then {
-   # Workaround for this bug:
-   #   http://sourceware.org/bugzilla/show_bug.cgi?id=411
-   # I.e. when gcc is compiled with --with-arch=i686, then the
-   # preprocessor symbol `__i686' will be defined to `1'.  This causes
-   # the symbol __i686.get_pc_thunk.dx to be mangled.
-   NIX_CFLAGS_COMPILE = "-U__i686";
- }
- else {})
+   }
+   else {}))
--- pkgs/development/libraries/glibc-2.11/locales.nix
+++ pkgs/development/libraries/glibc-2.11/locales.nix
@@ -8,60 +8,35 @@
 
 { stdenv, fetchurl, allLocales ? true, locales ? ["en_US.UTF-8/UTF-8"] }:
 
-stdenv.mkDerivation rec {
-  name = "glibc-locales-2.9";
+let build = import ./common.nix;
+in
+  build {
+    name = "glibc-locales";
 
-  builder = ./localesbuilder.sh;
+    inherit fetchurl stdenv;
+    installLocales = true;
 
-  src = fetchurl {
-    url = http://ftp.gnu.org/gnu/glibc/glibc-2.9.tar.bz2;
-    sha256 = "0v53m7flx6qcx7cvrvvw6a4dx4x3y6k8nvpc4wfv5xaaqy2am2q9";
-  };
+    builder = ./localesbuilder.sh;
 
-  srcPorts = fetchurl {
-    url = http://ftp.gnu.org/gnu/glibc/glibc-ports-2.9.tar.bz2;
-    sha256 = "0r2sn527wxqifi63di7ns9wbjh1cainxn978w178khhy7yw9fk42";
-  };
+    # Awful hack: `localedef' doesn't allow the path to `locale-archive'
+    # to be overriden, but you *can* specify a prefix, i.e. it will use
+    # <prefix>/<path-to-glibc>/lib/locale/locale-archive.  So we use
+    # $TMPDIR as a prefix, meaning that the locale-archive is placed in
+    # $TMPDIR/nix/store/...-glibc-.../lib/locale/locale-archive.
+    buildPhase =
+      ''
+        mkdir -p $TMPDIR/"$(dirname $(readlink -f $(type -p localedef)))/../lib/locale"
+        make localedata/install-locales \
+            LOCALEDEF="localedef --prefix=$TMPDIR" \
+            localedir=$out/lib/locale \
+            ${if allLocales then "" else "SUPPORTED-LOCALES=\"${toString locales}\""}
+      '';
 
-  inherit (stdenv) is64bit;
+    installPhase =
+      ''
+        ensureDir "$out/lib/locale"
+        cp -v "$TMPDIR/nix/store/"*"/lib/locale/locale-archive" "$out/lib/locale"
+      '';
 
-  configureFlags = [
-    "--enable-add-ons"
-    "--without-headers"
-    "--disable-profile"
-  ] ++ (if (stdenv.system == "armv5tel-linux") then [
-    "--host=arm-linux-gnueabi"
-    "--build=arm-linux-gnueabi"
-    "--without-fp"
-  ] else []);
-
-  patches = [
-    /* Support GNU Binutils 2.20 and above.  */
-    ./binutils-2.20.patch
-  ];
-
-  # Awful hack: `localedef' doesn't allow the path to `locale-archive'
-  # to be overriden, but you *can* specify a prefix, i.e. it will use
-  # <prefix>/<path-to-glibc>/lib/locale/locale-archive.  So we use
-  # $TMPDIR as a prefix, meaning that the locale-archive is placed in
-  # $TMPDIR/nix/store/...-glibc-.../lib/locale/locale-archive.
-  buildPhase =
-    ''
-      mkdir -p $TMPDIR/"$(dirname $(readlink -f $(type -p localedef)))/../lib/locale"
-      make localedata/install-locales \
-          LOCALEDEF="localedef --prefix=$TMPDIR" \
-          localedir=$out/lib/locale \
-          ${if allLocales then "" else "SUPPORTED-LOCALES=\"${toString locales}\""}
-    '';
-
-  installPhase =
-    ''
-      ensureDir $out/lib/locale
-      cp $TMPDIR/nix/store/*/lib/locale/locale-archive $out/lib/locale/
-    '';
-
-  meta = {
-    homepage = http://www.gnu.org/software/libc/;
-    description = "Locale information for the GNU C Library";
-  };
-}
+    meta.description = "Locale information for the GNU C Library";
+  }
