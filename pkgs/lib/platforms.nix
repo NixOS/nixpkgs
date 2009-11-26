0db7aaca02419df01e1d85ec5bcbf6e978fd24fc
Rob Vermaas <rob.vermaas@gmail.com>
2009-11-26 15:03:42 +0000

    add x86_64-darwin as platform
    svn path=/nixpkgs/trunk/; revision=18646

--- pkgs/lib/platforms.nix
+++ pkgs/lib/platforms.nix
@@ -3,7 +3,7 @@ let lists = import ./lists.nix; in
 rec {
   gnu = linux; /* ++ hurd ++ kfreebsd ++ ... */
   linux = ["i686-linux" "x86_64-linux" "powerpc-linux"];
-  darwin = ["i686-darwin" "powerpc-darwin"];
+  darwin = ["i686-darwin" "powerpc-darwin" "x86_64-darwin"];
   freebsd = ["i686-freebsd" "x86_64-freebsd" "powerpc-freebsd"];
   openbsd = ["i686-openbsd" "x86_64-openbsd"];
   netbsd = ["i686-netbsd" "x86_64-netbsd"];
