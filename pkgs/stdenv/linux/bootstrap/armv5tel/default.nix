6089b48c03416f2cfe73dddfb8609178d71d3b1e
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2009-11-29 21:30:46 +0000

    Updating the url to the armv5tel-linux bootstrap files, once niksnut updated the svn checkout published in http://nixos.org/tarballs/stdenv-linux/
    svn path=/nixpkgs/branches/stdenv-updates/; revision=18700

--- pkgs/stdenv/linux/bootstrap/armv5tel/default.nix
+++ pkgs/stdenv/linux/bootstrap/armv5tel/default.nix
@@ -7,7 +7,7 @@
   curl = ./curl.bz2;
 
   bootstrapTools = {
-    url = "http://vicerveza.homeunix.net/~viric/tmp/bootstrap-tools-armv5tel-linux.cpio.bz2";
+    url = "http://nixos.org/tarballs/stdenv-linux/armv5tel/r17267/bootstrap-tools.cpio.bz2";
     sha256 = "0b7mrcl7naj1xpqx1qnlmd825dxzikzhxir3mw4pr3dy28n0b2ka";
   };
 }
