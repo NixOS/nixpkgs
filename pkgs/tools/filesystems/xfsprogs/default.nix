ab708d5b6e069418347ab81b29fc3eff012efeb1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-12-08 16:58:22 +0000

    * xfsprogs updated to 3.0.3. * xfsprogs: don't use shared libraries because libxfs.so isn't   installed for some reason, so mkfs.xfs doesn't work.
    svn path=/nixpkgs/trunk/; revision=18842

--- pkgs/tools/filesystems/xfsprogs/default.nix
+++ pkgs/tools/filesystems/xfsprogs/default.nix
@@ -1,21 +1,16 @@
 { stdenv, fetchurl, libtool, gettext, libuuid }:
 
-stdenv.mkDerivation {
-  name = "xfsprogs-2.9.7-1";
+stdenv.mkDerivation rec {
+  name = "xfsprogs-3.0.3";
 
   src = fetchurl {
-    urls = [ ftp://oss.sgi.com/projects/xfs/previous/cmd_tars/xfsprogs_2.9.7-1.tar.gz http://ftp.lfs-matrix.org/pub/blfs/svn/x/xfsprogs_2.9.7-1.tar.gz ];
-    sha256 = "0g4pr1rv4lgc7vab18wiwrcr6jq40fs1krb2vfkgh779p7gf3il7";
+    url = "ftp://oss.sgi.com/projects/xfs/previous/${name}.tar.gz";
+    sha256 = "158ddibsnwcihfvskwc9rknd28p81jk8z463slafp1gf355kmcsq";
   };
 
-  buildInputs = [libtool gettext libuuid];
+  buildInputs = [ libtool gettext libuuid ];
 
-  preConfigure =
-    ''
-      sp_path=$(echo $PATH | sed -e 's/:/ /g');
-      sed -e 's@/usr/bin@'"$PATH: $sp_path"'@g' -i configure
-      sed -e 's@/usr/local/bin@'"$PATH: sp_path"'@g' -i configure
-    '';
+  configureFlags = "MAKE=make MSGFMT=msgfmt MSGMERGE=msgmerge XGETTEXT=xgettext ZIP=gzip AWK=gawk --disable-shared";
 
   meta = {
     description = "SGI XFS utilities";
