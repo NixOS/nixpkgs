60a5f44759e056c039db98d3d0fcf166ea8ba5c7
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2009-12-08 19:34:25 +0000

    Updating the url for the latest armv5tel linux bootstrap-tools.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=18844

--- pkgs/stdenv/linux/bootstrap/armv5tel/default.nix
+++ pkgs/stdenv/linux/bootstrap/armv5tel/default.nix
@@ -7,7 +7,7 @@
   curl = ./curl.bz2;
 
   bootstrapTools = {
-    url = "http://vicerveza.homeunix.net/~viric/tmp/bootstrap-tools.cpio.bz2";
+    url = "http://nixos.org/tarballs/stdenv-linux/armv5tel/r18744/bootstrap-tools.cpio.bz2";
     sha256 = "1rn4n5kilqmv62dfjfcscbsm0w329k3gyb2v9155fsi1sl2cfzcb";
   };
 }
