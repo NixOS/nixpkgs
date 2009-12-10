19d02b6ca4a12a18de7b1a96f6a7ebc8d1755151
Ludovic Courtès <ludo@gnu.org>
2009-12-10 21:27:19 +0000

    ocaml: Add description.
    svn path=/nixpkgs/trunk/; revision=18882

--- pkgs/development/compilers/ocaml/3.11.1.nix
+++ pkgs/development/compilers/ocaml/3.11.1.nix
@@ -21,8 +21,27 @@ stdenv.mkDerivation (rec {
 
   meta = {
     homepage = http://caml.inria.fr/ocaml;
-    license = "QPL, LGPL2 (library part)";
-    desctiption = "Most popular variant of the Caml language";
+    licenses = [ "QPL" /* compiler */ "LGPLv2" /* library */ ]
+    description = "Objective Caml, the most popular variant of the Caml language";
+
+    longDescription =
+      '' Objective Caml is the most popular variant of the Caml language.
+         From a language standpoint, it extends the core Caml language with a
+         fully-fledged object-oriented layer, as well as a powerful module
+         system, all connected by a sound, polymorphic type system featuring
+         type inference.
+
+         The Objective Caml system is an industrial-strength implementation
+         of this language, featuring a high-performance native-code compiler
+         (ocamlopt) for 9 processor architectures (IA32, PowerPC, AMD64,
+         Alpha, Sparc, Mips, IA64, HPPA, StrongArm), as well as a bytecode
+         compiler (ocamlc) and an interactive read-eval-print loop (ocaml)
+         for quick development and portability.  The Objective Caml
+         distribution includes a comprehensive standard library, a replay
+         debugger (ocamldebug), lexer (ocamllex) and parser (ocamlyacc)
+         generators, a pre-processor pretty-printer (camlp4) and a
+         documentation generator (ocamldoc).
+       '';
   };
 
 })
