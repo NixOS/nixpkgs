1030a6bb68fb3c77d153a92d32f9f94dcd53b58f
Marc Weber <marco-oweber@gmx.de>
2009-12-11 13:58:17 +0000

    fix mjpegtools
    svn path=/nixpkgs/trunk/; revision=18893

--- pkgs/tools/video/mjpegtools/default.nix
+++ pkgs/tools/video/mjpegtools/default.nix
@@ -7,4 +7,9 @@ stdenv.mkDerivation {
     sha256 = "1xvgqzdb2rw6j4ss65k4hrzrbsl74p7k5l4qgf5dbfcw522kw7lb";
   };
   buildInputs = [libjpeg libX11];
+  patches = [ ( fetchurl {
+      url = "http://bugs.gentoo.org/attachment.cgi?id=145622";
+      sha256 = "0c3bdrkr0qsrd3jybzz84z9gs4bq90rvxg87ffw08149v5qjz7a1";
+      name = "patch.patch";
+  } ) ]; # from gentoo. Don't know why it broke. Make it compile again.
 }
