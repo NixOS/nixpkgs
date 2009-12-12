5cbd24426598bba78b4af3c03f72e3c21bb39921
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2009-12-12 19:48:12 +0000

    Updating ntp.
    svn path=/nixpkgs/trunk/; revision=18916

--- pkgs/tools/networking/ntp/default.nix
+++ pkgs/tools/networking/ntp/default.nix
@@ -3,11 +3,11 @@
 assert stdenv.isLinux -> libcap != null;
  
 stdenv.mkDerivation rec {
-  name = "ntp-4.2.4p7";
+  name = "ntp-4.2.6";
   
   src = fetchurl {
     url = "http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/${name}.tar.gz";
-    sha256 = "1hq1iz54md8imc4a60jcmljlm5jk8ql7x40v7kbnc1ndwly8i0an";
+    sha256 = "04cam5l804ws6cs0ihlmx26n39vpap5wb39i1vxfff01mri4q38b";
   };
   
   configureFlags = ''
