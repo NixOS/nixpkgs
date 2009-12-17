56e8f65d44641765877cc413a77635dc74a271b1
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2009-12-17 08:08:17 +0000

    Updating 'dar'.
    svn path=/nixpkgs/trunk/; revision=19010

--- pkgs/tools/archivers/dar/default.nix
+++ pkgs/tools/archivers/dar/default.nix
@@ -1,16 +1,18 @@
 {stdenv, fetchurl, zlib, bzip2, openssl}:
 
-stdenv.mkDerivation {
-  name = "dar-2.3.8";
+stdenv.mkDerivation rec {
+  name = "dar-2.3.9";
   src = fetchurl {
-    url = mirror://sourceforge/dar/dar-2.3.8.tar.gz;
-    md5 = "6c0d383c22df0e1816e860153f60d746";
+    url = "mirror://sourceforge/dar/dar-2.3.9.tar.gz";
+    sha256 = "037ak6wckqyn52000m8bapgnf70rw776dl3s61i9bdq548slq6yc";
   };
+
+  buildInputs = [zlib bzip2 openssl];
 
   meta = {
 	  homepage = http://dar.linux.free.fr/;
 	  description = "Disk ARchiver, allows backing up files into indexed archives";
+    maintainers = with stdenv.lib.maintainers; [viric];
+    platforms = with stdenv.lib.platforms; linux;
   };
-
-  buildInputs = [zlib bzip2 openssl];
 }
