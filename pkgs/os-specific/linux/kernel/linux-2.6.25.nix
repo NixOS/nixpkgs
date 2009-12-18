c122c7a5baf74ebc424b8455c5178cddb34d9f4b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-12-18 12:23:13 +0000

    * Hm, I thought I tested this...
    svn path=/nixpkgs/trunk/; revision=19032

--- pkgs/os-specific/linux/kernel/linux-2.6.25.nix
+++ pkgs/os-specific/linux/kernel/linux-2.6.25.nix
@@ -26,18 +26,6 @@ import ./generic.nix (
         # Optimize with -O2, not -Os.
         CC_OPTIMIZE_FOR_SIZE n
 
-        # Virtualisation (KVM, Xen...).
-        PARAVIRT_GUEST y
-        KVM_CLOCK y
-        KVM_GUEST y
-        XEN y
-
-        # We need 64 GB (PAE) support for Xen guest support.
-        HIGHMEM64G? y
-
-        # Enable the kernel's built-in memory tester.
-        MEMTEST y
-
         # Include the CFQ I/O scheduler in the kernel, rather than as a
         # module, so that the initrd gets a good I/O scheduler.
         IOSCHED_CFQ y
@@ -46,10 +34,8 @@ import ./generic.nix (
         MARKERS n
         KPROBES n
         NUMA? n
-        PM_TRACE_RTC n
 
         # Enable various subsystems.
-        ACCESSIBILITY y # Accessibility support
         AUXDISPLAY y # Auxiliary Display support
         DONGLE y # Serial dongle support
         HIPPI y
@@ -64,28 +50,7 @@ import ./generic.nix (
         # Networking options.
         IP_PNP n
         IPV6_PRIVACY y
-        NETFILTER_ADVANCED y
-        IP_VS_PROTO_TCP y
-        IP_VS_PROTO_UDP y
-        IP_VS_PROTO_ESP y
-        IP_VS_PROTO_AH y
         IP_DCCP_CCID3 n # experimental
-        CLS_U32_PERF y
-        CLS_U32_MARK y
-
-        # Wireless networking.
-        IPW2100_MONITOR y # support promiscuous mode
-        IPW2200_MONITOR y # support promiscuous mode
-        IWLWIFI_LEDS? y
-        IWLWIFI_RFKILL y
-        IWLAGN_SPECTRUM_MEASUREMENT y
-        IWLAGN_LEDS y
-        IWL4965 y # Intel Wireless WiFi 4965AGN
-        IWL5000 y # Intel Wireless WiFi 5000AGN
-        IWL3945_RFKILL y
-        IWL3945_LEDS y
-        HOSTAP_FIRMWARE y # Support downloading firmware images with Host AP driver
-        HOSTAP_FIRMWARE_NVRAM y
 
         # Some settings to make sure that fbcondecor works - in particular,
         # disable tileblitting and the drivers that need it.
@@ -117,9 +82,7 @@ import ./generic.nix (
         USB_STORAGE_SDDR09 y
         USB_STORAGE_SDDR55 y
         USB_STORAGE_JUMPSHOT y
-        USB_STORAGE_ONETOUCH y
         USB_STORAGE_KARMA y
-        USB_STORAGE_CYPRESS_ATACB y
 
         # USB serial devices.
         USB_SERIAL_GENERIC y # USB Generic Serial Driver
@@ -151,8 +114,6 @@ import ./generic.nix (
         XFS_POSIX_ACL y
         XFS_RT y # XFS Realtime subvolume support
         OCFS2_DEBUG_MASKLOG n
-        UBIFS_FS_XATTR y
-        UBIFS_FS_ADVANCED_COMPR y
         NFSD_V2_ACL y
         NFSD_V3 y
         NFSD_V3_ACL y
@@ -160,23 +121,16 @@ import ./generic.nix (
         CIFS_XATTR y
         CIFS_POSIX y
 
-        # Security related features.
-        STRICT_DEVMEM y # Filter access to /dev/mem
-        SECURITY_SELINUX_BOOTPARAM_VALUE 0 # disable SELinux by default
-
         # Misc. options.
         8139TOO_8129 y
         8139TOO_PIO n # PIO is slower
         AIC79XX_DEBUG_ENABLE n
         AIC7XXX_DEBUG_ENABLE n
         AIC94XX_DEBUG n
-        B43_PCMCIA y
         BLK_DEV_BSG n
         BLK_DEV_CMD640_ENHANCED y # CMD640 enhanced support
         BLK_DEV_IDEACPI y # IDE ACPI support
-        BLK_DEV_INTEGRITY y
         BLK_DEV_IO_TRACE n
-        BSD_PROCESS_ACCT_V3 y
         BT_HCIUART_BCSP y
         BT_HCIUART_H4 y # UART (H4) protocol support
         BT_HCIUART_LL y
@@ -186,19 +140,10 @@ import ./generic.nix (
         DMAR? n # experimental
         FUSION y # Fusion MPT device support
         IRDA_ULTRA y # Ultra (connectionless) protocol
-        JOYSTICK_IFORCE_232 y # I-Force Serial joysticks and wheels
-        JOYSTICK_IFORCE_USB y # I-Force USB joysticks and wheels
-        JOYSTICK_XPAD_FF y # X-Box gamepad rumble support
-        JOYSTICK_XPAD_LEDS y # LED Support for Xbox360 controller 'BigX' LED
         KALLSYMS_EXTRA_PASS n
-        LDM_PARTITION y # Windows Logical Disk Manager (Dynamic Disk) support
-        LEDS_TRIGGER_IDE_DISK y # LED IDE Disk Trigger
-        LOGIRUMBLEPAD2_FF y # Logitech Rumblepad 2 force feedback
         LOGO n # not needed
-        MEDIA_ATTACH y
         MEGARAID_NEWGEN y
         MODVERSIONS y
-        MTRR_SANITIZER y
         NET_FC y # Fibre Channel driver support
         PCI_LEGACY y
         PPP_MULTILINK y # PPP multilink support
@@ -206,7 +151,6 @@ import ./generic.nix (
         SERIAL_8250 y # 8250/16550 and compatible serial support
         SLIP_COMPRESSED y # CSLIP compressed headers
         SLIP_SMART y
-        THERMAL_HWMON y # Hardware monitoring support
         USB_DEBUG n
         USB_EHCI_ROOT_HUB_TT y # Root Hub Transaction Translators
         X86_MCE y
