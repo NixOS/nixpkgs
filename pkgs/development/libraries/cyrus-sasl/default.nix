405bbecba137aaf92f984f51e06721fec1c5539d
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2009-12-19 08:59:36 +0000

    Updating cyrus-sasl (hoping for it to build with gcc 4.4)
    svn path=/nixpkgs/trunk/; revision=19043

--- pkgs/development/libraries/cyrus-sasl/default.nix
+++ pkgs/development/libraries/cyrus-sasl/default.nix
@@ -1,11 +1,11 @@
 args:
 with args;
 stdenv.mkDerivation {
-  name = "cyrus-sasl-2.1.22";
+  name = "cyrus-sasl-2.1.23";
 
   src = fetchurl {
-    url = ftp://ftp.andrew.cmu.edu/pub/cyrus-mail/cyrus-sasl-2.1.22.tar.gz;
-    sha256 = "c69e3853f35b14ee2c3f6e876e42d880927258ff4678aa052e5f0853db209962";
+    url = ftp://ftp.andrew.cmu.edu/pub/cyrus-mail/cyrus-sasl-2.1.23.tar.gz;
+    sha256 = "0dmi41hfy015pzks8n93qsshgvi0az7pv81nls4nxayb810crvr0";
   };
   configureFlags="--with-openssl=${openssl} --with-plugindir=\${out}/lib/sasl2 --with-configdir=\${out}/lib/sasl2";
   buildInputs = [ openssl db4 gettext ];
