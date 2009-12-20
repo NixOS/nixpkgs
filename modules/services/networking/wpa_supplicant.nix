57f70db611cde0bd5aa26a32df4f0bfc6378eb19
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-12-20 16:51:22 +0000

    * Revert r19003 for now, because wpa_supplicant 0.6.9 (in the Nixpkgs   trunk) doesn't have a -s flag.  0.7.0 in the stdenv branch   presumably does, but it's marked as unstable upstream.
    svn path=/nixos/trunk/; revision=19047

--- modules/services/networking/wpa_supplicant.nix
+++ modules/services/networking/wpa_supplicant.nix
@@ -56,8 +56,7 @@ in
 
         exec =
           "${pkgs.wpa_supplicant}/sbin/wpa_supplicant " +
-          "-s -C /var/run/wpa_supplicant " +
-          "-c ${configFile} -i${config.networking.WLANInterface}";
+          "-C /var/run/wpa_supplicant -c ${configFile} -i${config.networking.WLANInterface}";
       };
   
   };
