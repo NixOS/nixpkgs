fc8a7ab83aacdd6f03e45a28fe84c1eadffc5fec
Armijn Hemel <armijn@gpl-violations.org>
2009-12-24 22:18:28 +0000

    sync name with actual version
    svn path=/nixpkgs/trunk/; revision=19099

--- pkgs/desktops/kde-4.3/support/qca2/default.nix
+++ pkgs/desktops/kde-4.3/support/qca2/default.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchurl, lib, which, qt4}:
 
 stdenv.mkDerivation {
-  name = "qca-2.0.1";
+  name = "qca-2.0.2";
   src = fetchurl {
     url = http://delta.affinix.com/download/qca/2.0/qca-2.0.2.tar.bz2;
     sha256 = "49b5474450104a2298747c243de1451ab7a6aeed4bf7df43ffa4b7128a2837b8";
