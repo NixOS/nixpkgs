91d825d620a4f1174ae11378ee2ae2465544aa81
Ludovic Courtès <ludo@gnu.org>
2010-01-05 11:16:26 +0000

    libmpeg2: Build the shared library.
    svn path=/nixpkgs/trunk/; revision=19215

--- pkgs/development/libraries/mpeg2dec/default.nix
+++ pkgs/development/libraries/mpeg2dec/default.nix
@@ -8,6 +8,8 @@ stdenv.mkDerivation {
     sha256 = "1vny7rg0p2rmic71hli2l2612i5yaw8vy0wsnm5nvhwfiw37cjn7";
   };
 
+  configureFlags = "--enable-shared --disable-static";
+
   meta = {
     homepage = http://libmpeg2.sourceforge.net/;
     description = "A free library for decoding mpeg-2 and mpeg-1 video streams";
