9dcfc5263bce27d6d8abd35170740c56caee8437
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-01-07 20:39:52 +0000

    Updating poppler
    svn path=/nixpkgs/trunk/; revision=19299

--- pkgs/development/libraries/poppler/default.nix
+++ pkgs/development/libraries/poppler/default.nix
@@ -5,11 +5,11 @@
 assert qt4Support -> (qt4 != null);
 
 stdenv.mkDerivation rec {
-  name = "poppler-0.10.6";
+  name = "poppler-0.12.3";
 
   src = fetchurl {
     url = "http://poppler.freedesktop.org/${name}.tar.gz";
-    sha256 = "bcd78d674c4166af069afdb27af810c012e13cfd2b7b21f9dce63dd3f62bded1";
+    sha256 = "1k7vilpz0ipnmw9dfpb3rqkhlm4rqcnkn3bhhp14di2h55nzwkvs";
   };
 
   buildInputs = [pkgconfig zlib glib cairo freetype fontconfig libjpeg gtk]
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
     ''
     + (if qt4Support then "--enable-qt-poppler" else "--disable-qt-poppler");
 
-  patches = [ ./GDir-const.patch ./use_exceptions.patch ];
+  patches = [ ./GDir-const.patch ];
 
   preConfigure = "sed -e '/jpeg_incdirs/s@/usr@${libjpeg}@' -i configure";
 
