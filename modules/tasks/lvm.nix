cac502e35b4f23da844f30a3955e7a387d21cb58
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-01-10 19:14:42 +0000

    * Okay, so we do need an "lvm" task to run vgscan/vgchange.  It's only   needed though for devices that were not already found and activated   in the initrd.
    svn path=/nixos/trunk/; revision=19337

--- modules/tasks/lvm.nix
+++ modules/tasks/lvm.nix
@@ -6,6 +6,25 @@
 
   config = {
 
+    jobs.lvm =
+      { startOn = "started udev or new-devices";
+
+        script =
+          ''
+            # Scan for block devices that might contain LVM physical volumes
+            # and volume groups.
+            ${pkgs.lvm2}/sbin/vgscan --mknodes
+
+            # Make all logical volumes on all volume groups available, i.e.,
+            # make them appear in /dev.
+            ${pkgs.lvm2}/sbin/vgchange --available y
+
+            initctl emit -n new-devices
+          '';
+
+        task = true;
+      };
+
     environment.systemPackages = [ pkgs.lvm2 ];
 
     services.udev.packages = [ pkgs.lvm2 ];
