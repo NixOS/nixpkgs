ccde4002078a6405a7e0f1c0491a34b22002f7b3
Andres Löh <mail@andres-loeh.de>
2010-01-12 10:27:25 +0000

    Updated lhs2TeX.
    svn path=/nixpkgs/trunk/; revision=19366

--- pkgs/tools/typesetting/lhs2tex/default.nix
+++ pkgs/tools/typesetting/lhs2tex/default.nix
@@ -4,18 +4,22 @@
 
 cabal.mkDerivation (self : {
   pname = "lhs2tex";
-  version = "1.14";
+  version = "1.15";
   name = self.fname;
-  sha256 = "1667acce394a0d4852f8ad07fa85397e43873fd98a219db794e4773883288687";
+  sha256 = "77f25c1f22823587ceca6eead133a403540319a0ae3bf03a369b3e8c86baf124";
   extraBuildInputs = [tetex regexCompat utf8String];
   propagatedBuildInputs = [polytable]; # automatically in user-env now with cabal
-
-  configureFlags = ''--constraint=base<4'';
 
   postInstall = ''
     ensureDir "$out/share/doc/$name"
     cp doc/Guide2.pdf $out/share/doc/$name
     ensureDir "$out/nix-support"
   '';
+
+  meta = {
+    description = "Preprocessor for typesetting Haskell sources with LaTeX";
+    license = "GPLv2";
+    maintainers = [self.stdenv.lib.maintainers.andres];
+  };
 })
 
