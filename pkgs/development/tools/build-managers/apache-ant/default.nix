5a787b0ab78db52afa08e9d102d3ed11e7397a5c
Rob Vermaas <rob.vermaas@gmail.com>
2010-01-13 15:20:24 +0000

    ant version 1.8.0RC1
    svn path=/nixpkgs/trunk/; revision=19400

--- pkgs/development/tools/build-managers/apache-ant/default.nix
+++ pkgs/development/tools/build-managers/apache-ant/default.nix
@@ -11,8 +11,8 @@ let {
       inherit antContrib jdk;
 
       src = fetchurl {
-        url = http://apache.mirror.transip.nl/ant/binaries/apache-ant-1.7.1-bin.tar.bz2 ;
-        sha256 = "15rgkini0g100jygp7z9hgc3yfb9m62q4nk989rin7dqj2flrr94";
+        url = http://apache.mirror.transip.nl/ant/binaries/apache-ant-1.8.0RC1-bin.tar.bz2 ;
+        sha256 = "0xvmrsghibq7p3wvfkmvmkkg0zzfmw32lrfjl5f6cfzchjjnw9wx";
       };
     };
 
