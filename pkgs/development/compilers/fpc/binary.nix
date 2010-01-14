8706bd4591adb649c4f974c7d67b9786ef25b2d4
Michael Raskin <7c6f434c@mail.ru>
2010-01-14 08:00:50 +0000

    Update FPC
    svn path=/nixpkgs/trunk/; revision=19420

--- pkgs/development/compilers/fpc/binary.nix
+++ pkgs/development/compilers/fpc/binary.nix
@@ -1,12 +1,19 @@
 args: with args;
 
 stdenv.mkDerivation {
-  name = "fpc-2.2.2-binary";
+  name = "fpc-2.4.0-binary";
 
-  src = fetchurl {
-    url = ftp://ftp.chg.ru/pub/lang/pascal/fpc/dist/i386-linux-2.2.2/fpc-2.2.2.i386-linux.tar;
-    sha256 = "8c18f63b36a76eee673f96ca254c49c5a42bcf3e36279abe8774f961792449a5";
-  };
+  src = if stdenv.system == "i686-linux" then
+  fetchurl {
+    url = "ftp://ftp.chg.ru/pub/lang/pascal/fpc/dist/2.4.0/i386-linux/fpc-2.4.0.i386-linux.tar";
+    sha256 = "1zas9kp0b36zxqvb9i4idh2l0nb6qpmgah038l77w6las7ghh0dv";
+  }
+  else if stdenv.system == "x86_64-linux" then
+  fetchurl {
+    url = "ftp://ftp.chg.ru/pub/lang/pascal/fpc/dist/2.4.0/x86_64-linux/fpc-2.4.0.x86_64-linux.tar";
+    sha256 = "111d11g5ra55hjywx64ldwwflpimsy8zryvap68v0309nyd23f0z";
+  }
+  else null;
 
   builder = ./binary-builder.sh;
 
