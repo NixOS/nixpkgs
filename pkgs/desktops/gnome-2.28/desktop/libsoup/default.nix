d263076f5a6b384b317630e56ac1d6cdda91b07c
Michael Raskin <7c6f434c@mail.ru>
2010-01-14 09:23:38 +0000

    Minor update of libsoup
    svn path=/nixpkgs/trunk/; revision=19424

--- pkgs/desktops/gnome-2.28/desktop/libsoup/default.nix
+++ pkgs/desktops/gnome-2.28/desktop/libsoup/default.nix
@@ -2,10 +2,10 @@
   glib, GConf, gnome_keyring}:
 
 stdenv.mkDerivation {
-  name = "libsoup-2.28.0";
+  name = "libsoup-2.28.2";
   src = fetchurl {
-    url = mirror://gnome/sources/libsoup/2.28/libsoup-2.28.0.tar.bz2;
-    sha256 = "1dkgih5im81lqc0y2qv3xcjd8hvmd4fjjvh5a5akfq6mjp9ifwk4";
+    url = mirror://gnome/sources/libsoup/2.28/libsoup-2.28.2.tar.bz2;
+    sha256 = "002kxjh6dwpps4iwly1bazxlzgqhkfszqqy26mp1gy2il3lzrlcx";
   };
   buildInputs = [ pkgconfig libxml2 gnutls libproxy sqlite curl 
     glib GConf gnome_keyring ];
