f573c323276f3290371967b85b412ece1367b6b2
Michael Raskin <7c6f434c@mail.ru>
2010-01-14 20:35:04 +0000

    Make fpc binary builder more explicit and more verbose
    svn path=/nixpkgs/trunk/; revision=19445

--- pkgs/development/compilers/fpc/binary-builder.sh
+++ pkgs/development/compilers/fpc/binary-builder.sh
@@ -2,9 +2,11 @@ source $stdenv/setup
 
 tar xf $src
 tarballdir=$(pwd)
-for i in *.tar; do tar xf $i; done
+for i in *.tar; do tar xvf $i; done
+echo "Deploying binaries.."
 mkdir $out
 cd $out
-for i in $tarballdir/*.gz; do tar xf $i; done
-ln -fs $out/lib/fpc/*/ppc386 $out/bin
+for i in $tarballdir/*.gz; do tar xvf $i; done
+echo "Creating ppc386 symlink.."
+ln -fs $out/lib/fpc/*/ppc386 $out/bin/ppc386
 
