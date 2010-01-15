241b80a1d38871d09fc5eea196e8ccb2d758ac29
Michael Raskin <7c6f434c@mail.ru>
2010-01-15 08:59:45 +0000

    Steal patch idea from Debian to build with fresher backend
    svn path=/nixpkgs/trunk/; revision=19456

--- pkgs/misc/xsane/default.nix
+++ pkgs/misc/xsane/default.nix
@@ -8,6 +8,10 @@ stdenv.mkDerivation {
     sha256 = "0zddar0y76iv4c55qzfmnm819z5hzisr2jwjb60x36v6bdrhcjx5";
   };
 
+  preConfigure = ''
+    sed -e '/SANE_CAP_ALWAYS_SETTABLE/d' -i src/xsane-back-gtk.c
+  '';
+
   buildInputs = [saneBackends saneFrontends libX11 gtk pkgconfig ] ++
 	(if (libusb != null) then [libusb] else []);
 }
