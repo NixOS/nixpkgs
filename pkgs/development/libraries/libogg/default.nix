e2a24424ece733b0f18397fcb865c15906e730f0
Armijn Hemel <armijn@gpl-violations.org>
2010-01-17 23:04:20 +0000

    update to 1.1.4
    svn path=/nixpkgs/branches/stdenv-updates/; revision=19510

--- pkgs/development/libraries/libogg/default.nix
+++ pkgs/development/libraries/libogg/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "libogg-1.1.3";
+  name = "libogg-1.1.4";
   src = fetchurl {
-    url = http://downloads.xiph.org/releases/ogg/libogg-1.1.3.tar.gz;
-    md5 = "eaf7dc6ebbff30975de7527a80831585";
+    url = http://downloads.xiph.org/releases/ogg/libogg-1.1.4.tar.gz;
+    sha256 = "00z15ha73yfv7zn9z2z5yvc8g53ci6vn12vpc0l7qhc8zn1w2m4k";
   };
 }
