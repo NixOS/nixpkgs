dc137c54eb16955dec3e7b9bdbc3d0dd6173221e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-01-20 21:03:53 +0000

    * Configuration used to generate an Amazon image.  This doesn't belong   in `tests' but I don't have a better place right now.
    svn path=/nixos/trunk/; revision=19583

---
+++ tests/amazon.nix
@@ -0,0 +1,33 @@
+rec {
+
+  machine =
+    { config, pkgs, ... }:
+    
+    {
+      services.httpd.enable = true;
+      services.httpd.adminAddr = "e.dolstra@tudelft.nl";
+      services.httpd.documentRoot = "${pkgs.valgrind}/share/doc/valgrind/html";
+
+      nixpkgs.config.packageOverrides = pkgsOld:
+        { dhcp = pkgs.lib.overrideDerivation pkgsOld.dhcp (oldAttrs:
+            { configureFlags = "--disable-dhcpv6";
+            });
+        };
+
+      fileSystems = [ ];
+
+      swapDevices =
+        [ { device = "/dev/sda2"; } ];
+
+      services.sshd.enable = true;
+      services.sshd.permitRootLogin = "without-password";
+
+      services.mingetty.ttys = [ ];
+    };
+
+  config = (import ./lib/eval-config.nix {
+    system = "i686-linux";
+    modules = [ machine ./modules/virtualisation/amazon-image.nix ];
+  }).config;
+
+}
