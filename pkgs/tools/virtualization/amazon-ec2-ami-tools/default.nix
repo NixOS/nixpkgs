2feb60424384c917fde753647d8aed39d3d4e7ef
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-01-22 16:42:54 +0000

    * A patch to ec2-bundle-image to make it work properly on read-only   images (in the Nix store).
    svn path=/nixpkgs/trunk/; revision=19616

--- pkgs/tools/virtualization/amazon-ec2-ami-tools/default.nix
+++ pkgs/tools/virtualization/amazon-ec2-ami-tools/default.nix
@@ -10,6 +10,14 @@ stdenv.mkDerivation {
     sha256 = "2a7c848abea286234adcbb08938cfad50b844ecdfc7770e781289d9d396a1972";
   };
 
+  # Amazon EC2 requires that disk images are writable.  If they're
+  # not, the VM immediately terminates with a mysterious
+  # "Server.InternalError" message.  Since disk images generated in
+  # the Nix store are read-only, they must be made writable in the
+  # tarball uploaded to Amazon S3.  So add a `--mode=0755' flag to the
+  # tar invocation.
+  patches = [ ./writable.patch ];
+
   installPhase =
     ''
       ensureDir $out
