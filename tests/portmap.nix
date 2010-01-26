31f2d8ffbc1ff0da7f1e5aabbc5f505da6db2055
Ludovic Courtès <ludo@gnu.org>
2010-01-26 23:55:22 +0000

    Add simple portmap test.
    svn path=/nixos/trunk/; revision=19700

---
+++ tests/portmap.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+
+  machine =
+    { config, pkgs, ... }:
+
+    { services.portmap.enable = true; };
+
+  testScript =
+    ''
+       # The `portmap' service must be registered once for TCP and once for
+       # UDP.
+       $machine->mustSucceed("test `rpcinfo -p | grep portmapper | wc -l` -eq 2");
+    '';
+
+}
