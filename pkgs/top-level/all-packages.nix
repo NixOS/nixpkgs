6e6f057d1889426cb9cee93c7e5ca79cf7f857a0
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-01-27 10:46:58 +0000

    * icecatXulrunner3 needs a xz argument.
    svn path=/nixpkgs/trunk/; revision=19704

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7144,7 +7144,7 @@ let
 
   icecatXulrunner3 = lowPrio (import ../applications/networking/browsers/icecat-3 {
     application = "xulrunner";
-    inherit fetchurl stdenv pkgconfig perl zip libjpeg libpng zlib cairo
+    inherit fetchurl stdenv xz pkgconfig perl zip libjpeg libpng zlib cairo
       python dbus dbus_glib freetype fontconfig bzip2 xlibs alsaLib;
     inherit (gnome) libIDL libgnomeui gnomevfs gtk pango;
     inherit (pythonPackages) ply;
