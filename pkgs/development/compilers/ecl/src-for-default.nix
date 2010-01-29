0c13a208038701c47831cda150a878e9a82bc7ae
Michael Raskin <7c6f434c@mail.ru>
2010-01-29 06:13:22 +0000

    Update ECL
    svn path=/nixpkgs/trunk/; revision=19725

--- pkgs/development/compilers/ecl/src-for-default.nix
+++ pkgs/development/compilers/ecl/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="9.6.2";
-   name="ecl-9.6.2";
-   hash="1847ppfrgfpjwc9qlfcvvsfhkarjz339qzh9mb0fwnqfihrbqpfy";
-   url="http://downloads.sourceforge.net/ecls/files/ecls/9.6.0/ecl-${version}.tar.gz";
-   advertisedUrl="http://downloads.sourceforge.net/ecls/files/ecls/9.6.0/ecl-9.6.2.tar.gz";
+   version="9.12.3";
+   name="ecl-9.12.3";
+   hash="10p6qncsx8jh3dwfv5l0rbqx1ib7mmvac2dxc6xvj38khkaykwpl";
+   url="http://downloads.sourceforge.net/ecls/files/ecls/9.12/ecl-${version}.tgz";
+   advertisedUrl="http://downloads.sourceforge.net/ecls/files/ecls/9.12/ecl-9.12.3.tgz";
   
   
 }
