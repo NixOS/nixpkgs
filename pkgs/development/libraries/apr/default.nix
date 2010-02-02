a066167a9e5fde279ef30d29dbe56fa757c3199c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-02-02 16:49:07 +0000

    * Fix building apr on Cygwin.
    svn path=/nixpkgs/trunk/; revision=19766

--- pkgs/development/libraries/apr/default.nix
+++ pkgs/development/libraries/apr/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl}:
+{ stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
   name = "apr-1.3.9";
@@ -8,6 +8,11 @@ stdenv.mkDerivation rec {
     sha256 = "1qicxnk62d9mjza8vch2wxy4xlq8sa76chwi5cp6bs4cyj9s61ap";
   };
 
+  configureFlags =
+    # Including the Windows headers breaks unistd.h.
+    # Based on ftp://sourceware.org/pub/cygwin/release/libapr1/libapr1-1.3.8-2-src.tar.bz2
+    stdenv.lib.optionalString (stdenv.system == "i686-cygwin") "ac_cv_header_windows_h=no";
+  
   meta = {
     homepage = http://apr.apache.org/;
     description = "The Apache Portable Runtime library";
