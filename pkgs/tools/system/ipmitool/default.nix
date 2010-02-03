c261d7a8472d003a6dd6bcf331cab609311e2383
Peter Simons <simons@cryp.to>
2010-02-03 12:47:23 +0000

    pkgs/tools/system/ipmitool: updated to version 1.8.11
    svn path=/nixpkgs/trunk/; revision=19777

--- pkgs/tools/system/ipmitool/default.nix
+++ pkgs/tools/system/ipmitool/default.nix
@@ -2,14 +2,14 @@
 
 let
   pkgname = "ipmitool";
-  version = "1.8.9";
+  version = "1.8.11";
 in
 stdenv.mkDerivation {
   name = "${pkgname}-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/${pkgname}/${pkgname}-${version}.tar.gz";
-    sha256 = "1d6bf2595d1fd0dbef206c300cc666d3d079548ba97f727077d61c4736a7e63a";
+    sha256 = "5612f4835d89a6f2cede588eef978a05d63435cf2646256300d9785d8020a13e";
   };
 
   buildInputs = [ openssl ];
