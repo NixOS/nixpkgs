637746b9cca5f313171dba955265b9fd5e1fb2fa
Ludovic Courtès <ludo@gnu.org>
2010-02-08 13:53:16 +0000

    WordNet: Fix URL; add `meta.maintainers'.
    svn path=/nixpkgs/trunk/; revision=19863

--- pkgs/applications/misc/wordnet/default.nix
+++ pkgs/applications/misc/wordnet/default.nix
@@ -1,10 +1,11 @@
 {stdenv, fetchurl, tcl, tk, x11, makeWrapper}:
 
+let version = "3.0"; in
 stdenv.mkDerivation {
-  name = "wordnet-3.0";
+  name = "wordnet-${version}";
   src = fetchurl {
-    url = http://wordnet.princeton.edu/3.0/WordNet-3.0.tar.bz2;
-    sha256 = "6c492d0c7b4a40e7674d088191d3aa11f373bb1da60762e098b8ee2dda96ef22";
+    url = "http://wordnetcode.princeton.edu/${version}/WordNet-${version}.tar.bz2";
+    sha256 = "08pgjvd2vvmqk3h641x63nxp7wqimb9r30889mkyfh2agc62sjbc";
   };
 
   buildInputs = [tcl tk x11 makeWrapper];
@@ -19,7 +20,22 @@ stdenv.mkDerivation {
   '';
 
   meta = {
-    description = "WordNet, a lexical database for the English language.";
+    description = "WordNet, a lexical database for the English language";
+
+    longDescription =
+      '' WordNet® is a large lexical database of English.  Nouns, verbs,
+         adjectives and adverbs are grouped into sets of cognitive synonyms
+         (synsets), each expressing a distinct concept.  Synsets are
+         interlinked by means of conceptual-semantic and lexical relations.
+         The resulting network of meaningfully related words and concepts can
+         be navigated with the browser.  WordNet is also freely and publicly
+         available for download.  WordNet's structure makes it a useful tool
+         for computational linguistics and natural language processing.
+      '';
+
     homepage = http://wordnet.princeton.edu/;
+
+    maintainers = [ stdenv.lib.maintainers.ludo ];
+    platforms = stdenv.lib.platforms.gnu;  # arbitrary choice
   };
 }
