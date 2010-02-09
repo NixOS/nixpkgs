6478e925568ca6c78b8be0528c9e4a39b5c5d216
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-02-09 10:42:36 +0000

    * Symlink propagated-user-env-packages to   propagated-build-native-inputs rather than propagated-build-inputs.
    svn path=/nixpkgs/trunk/; revision=19878

--- pkgs/development/perl-modules/generic/builder.sh
+++ pkgs/development/perl-modules/generic/builder.sh
@@ -29,12 +29,14 @@ preConfigure() {
 }
 
 postFixup() {
+    echo FIXUP
+    
     # If a user installs a Perl package, she probably also wants its
     # dependencies in the user environment (since Perl modules don't
     # have something like an RPATH, so the only way to find the
     # dependencies is to have them in the PERL5LIB variable).
-    if test -e $out/nix-support/propagated-build-inputs; then
-        ln -s $out/nix-support/propagated-build-inputs $out/nix-support/propagated-user-env-packages
+    if test -e $out/nix-support/propagated-build-native-inputs; then
+        ln -s $out/nix-support/propagated-build-native-inputs $out/nix-support/propagated-user-env-packages
     fi
 }
 
