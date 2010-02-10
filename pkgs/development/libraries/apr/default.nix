31b1ba6b113e36b327d599086cde3ac87e33ff97
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-02-10 13:30:57 +0000

    * apr: don't use accept4(), because it's not support on Linux <   2.6.28.  It's rather unfortunate that the result of building apr   depends on the capabilities of the kernel on which it's built...
    svn path=/nixpkgs/trunk/; revision=19903

--- pkgs/development/libraries/apr/default.nix
+++ pkgs/development/libraries/apr/default.nix
@@ -2,16 +2,18 @@
 
 stdenv.mkDerivation rec {
   name = "apr-1.3.9";
-  
+
   src = fetchurl {
     url = "mirror://apache/apr/${name}.tar.bz2";
     sha256 = "1qicxnk62d9mjza8vch2wxy4xlq8sa76chwi5cp6bs4cyj9s61ap";
   };
 
   configureFlags =
+    # Don't use accept4 because it's only supported on Linux >= 2.6.28.
+    [ " apr_cv_accept4=no" ]
     # Including the Windows headers breaks unistd.h.
     # Based on ftp://sourceware.org/pub/cygwin/release/libapr1/libapr1-1.3.8-2-src.tar.bz2
-    stdenv.lib.optionalString (stdenv.system == "i686-cygwin") "ac_cv_header_windows_h=no";
+    ++ stdenv.lib.optional (stdenv.system == "i686-cygwin") "ac_cv_header_windows_h=no";
   
   meta = {
     homepage = http://apr.apache.org/;
