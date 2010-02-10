3beabfeb59779729a9545f2639fc38a81d3f6e14
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-02-10 13:37:28 +0000

    svn path=/nixpkgs/trunk/; revision=19904

--- pkgs/development/libraries/apr/default.nix
+++ pkgs/development/libraries/apr/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
 
   configureFlags =
     # Don't use accept4 because it's only supported on Linux >= 2.6.28.
-    [ " apr_cv_accept4=no" ]
+    [ "apr_cv_accept4=no" ]
     # Including the Windows headers breaks unistd.h.
     # Based on ftp://sourceware.org/pub/cygwin/release/libapr1/libapr1-1.3.8-2-src.tar.bz2
     ++ stdenv.lib.optional (stdenv.system == "i686-cygwin") "ac_cv_header_windows_h=no";
