9501307305e4ad66558983f0e8119441c04e41d6
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-02-11 22:22:00 +0000

    Updating the url for shared desktop ontologies.
    svn path=/nixpkgs/trunk/; revision=19943

--- pkgs/data/misc/shared-desktop-ontologies/default.nix
+++ pkgs/data/misc/shared-desktop-ontologies/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation {
   name = "shared-desktop-ontologies-0.2";
   src = fetchurl {
-    url = mirror://sourceforge/shared-desktop-ontologies/shared-desktop-ontologies-0.2.tar.bz2;
+    url = mirror://sourceforge/oscaf/shared-desktop-ontologies-0.2.tar.bz2;
     sha256 = "1w9gfprrp518hb7nm5wspxjd7xx0h08bph6asrx5vrx7j7fzg4m7";
   };
   buildInputs = [ cmake ];
