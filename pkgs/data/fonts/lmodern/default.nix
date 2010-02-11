f71d6f9f91746aec49dc8005f188d93264dd7ff2
Michael Raskin <7c6f434c@mail.ru>
2010-02-11 09:44:48 +0000

    Update lmodern
    svn path=/nixpkgs/trunk/; revision=19925

--- pkgs/data/fonts/lmodern/default.nix
+++ pkgs/data/fonts/lmodern/default.nix
@@ -1,19 +1,21 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation {
-  name = "lmodern-1.010x";
+  name = "lmodern-2.004.1";
   
   src = fetchurl {
-    url = http://ftp.de.debian.org/debian/pool/main/l/lmodern/lmodern_1.010x.orig.tar.gz;
-    sha256 = "0nwxj1ng7rvnp16jxcs25hbc5in65mdk4a3g3rlaq91i5qpq7mxj";
+    url = mirror://debian/pool/main/l/lmodern/lmodern_2.004.1.orig.tar.gz;
+    sha256 = "1bvlf8p39667q58pvyfzy3yl0mylf0ak96flwp8vj01vqbi3rfaz";
   };
 
   installPhase = ''
-    ensureDir $out/share/texmf/
+    ensureDir $out/texmf/
     ensureDir $out/share/fonts/
 
-    cp -r ./* $out/share/texmf/
+    cp -r ./* $out/texmf/
     cp -r fonts/{opentype,type1} $out/share/fonts/
+
+    ln -s $out/texmf* $out/share/
   '';
 
   meta = {
