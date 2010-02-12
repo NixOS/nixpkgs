9cddc91b91dded35d48551f76258e169542a27af
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-02-12 23:19:31 +0000

    Fixing a hash in kde 4.4 artwork
    svn path=/nixpkgs/trunk/; revision=19967

--- pkgs/desktops/kde-4.4/artwork/default.nix
+++ pkgs/desktops/kde-4.4/artwork/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   name = "kdeartwork-4.4.0";
   src = fetchurl {
     url = mirror://kde/stable/4.4.0/src/kdeartwork-4.4.0.tar.bz2;
-    sha1 = "08djygxycrpgn09506lmkm86d1jbl8m0bbi79jdd4q3c1w76dbi4";
+    sha256 = "08djygxycrpgn09506lmkm86d1jbl8m0bbi79jdd4q3c1w76dbi4";
   };
   buildInputs = [ cmake qt4 perl xscreensaver
                   kdelibs kdebase_workspace automoc4 phonon strigi eigen ];
