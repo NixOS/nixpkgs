4e0ccd63b3ec8fa1b933ab9ffae97fb0316af98d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-02-12 14:27:26 +0000

    * In NixOS chroot builds, there is no root account.  So rcs' configure   should not rely on its existence in deciding whether to use   getpwuid().  Not using getpwuid() causes rcs to fail with "ci:   setuid not supported".
    svn path=/nixpkgs/trunk/; revision=19957

--- pkgs/applications/version-management/rcs/default.nix
+++ pkgs/applications/version-management/rcs/default.nix
@@ -2,8 +2,18 @@
 
 stdenv.mkDerivation {
   name = "rcs-5.7";
+  
   src = fetchurl {
     url = ftp://ftp.cs.purdue.edu/pub/RCS/rcs-5.7.tar;
     md5 = "f7b3f106bf87ff6344df38490f6a02c5";
+  };
+
+  patches = [ ./no-root.patch ];
+
+  meta = {
+    homepage = http://www.cs.purdue.edu/homes/trinkle/RCS/;
+    description = "Revision Control System, a version management system";
+    maintainers = [ stdenv.lib.maintainers.eelco ];
+    platforms = [ stdenv.lib.platforms.all ];
   };
 }
