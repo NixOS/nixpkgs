66e7aaa5c7d6eea52606aa06c807fb9fe0617c82
Ludovic Courtès <ludo@gnu.org>
2010-02-12 09:07:19 +0000

    GNU Acct 6.5.4.
    svn path=/nixpkgs/trunk/; revision=19945

--- pkgs/tools/system/acct/default.nix
+++ pkgs/tools/system/acct/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv }:
 
 stdenv.mkDerivation rec {
-  name = "acct-6.5.3";
+  name = "acct-6.5.4";
 
   src = fetchurl {
     url = "mirror://gnu/acct/${name}.tar.gz";
-    sha256 = "0a48z9wb1cfb7nnmr0y4pbyqbx6qim2hqwl9bm4iyfy1p2r06mml";
+    sha256 = "01yh0cqa4mn0165qx3c59bc9madmp5qmjssxsm7mnbz7bhhwf0qg";
   };
 
   doCheck = true;
