911dfd89fce7f59dec6f5e9d0bb419509a610243
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-02-13 16:06:25 +0000

    vtk had a wrong version version in its 'name'
    svn path=/nixpkgs/trunk/; revision=19978

--- pkgs/development/libraries/vtk/default.nix
+++ pkgs/development/libraries/vtk/default.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl, cmake, mesa, libX11, xproto, libXt }:
 
-stdenv.mkDerivation {
-  name = "vtk-5.2.1";
+stdenv.mkDerivation rec {
+  name = "vtk-5.4.2";
   src = fetchurl {
-    url = http://www.vtk.org/files/release/5.4/vtk-5.4.2.tar.gz;
+    url = "http://www.vtk.org/files/release/5.4/${name}.tar.gz";
     sha256 = "0gd7xlxiqww6xxcs2kicz0g6k147y3200np4jnsf10vlxs10az03";
   };
   buildInputs = [ cmake mesa libX11 xproto libXt ];
