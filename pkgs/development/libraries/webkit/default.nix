dac3633341ac5a60f0a35c2a867f0fd0591ebf58
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-02-15 08:42:45 +0000

    Adding 'glib' as buildInput to webkit (its configure script wanteed it). Naming all the buildInputs as for the target system (s/buildNativeInputs/buildInputs).
    svn path=/nixpkgs/trunk/; revision=20002

--- pkgs/development/libraries/webkit/default.nix
+++ pkgs/development/libraries/webkit/default.nix
@@ -9,7 +9,7 @@ rec {
     sha256 = s.hash;
   };
 
-  buildNativeInputs = [gtk atk cairo curl fontconfig freetype
+  buildInputs = [gtk glib atk cairo curl fontconfig freetype
     gettext libjpeg libpng libtiff libxml2 libxslt pango
     sqlite icu gperf bison flex autoconf automake libtool 
     perl intltool pkgconfig libsoup gtkdoc libXt libproxy
