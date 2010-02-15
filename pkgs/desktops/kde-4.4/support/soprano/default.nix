a928ee6ea03d56b8cc8bbdef297940142b20e8c7
Sander van der Burg <s.vanderburg@tudelft.nl>
2010-02-15 18:05:47 +0000

    Upgraded soprano to 2.4.0.1 hotfix release
    svn path=/nixpkgs/trunk/; revision=20022

--- pkgs/desktops/kde-4.4/support/soprano/default.nix
+++ pkgs/desktops/kde-4.4/support/soprano/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, lib, cmake, qt4, cluceneCore, redland}:
 
 stdenv.mkDerivation {
-  name = "soprano-2.4.0";
+  name = "soprano-2.4.0.1";
   
   src = fetchurl {
-    url = mirror://sourceforge/soprano/soprano-2.4.0.tar.bz2;
-    sha256 = "1630wax4baab5cl40xlvq6pj4scgmspmgb7wyyp7ppph4b0a8dkz";
+    url = mirror://sourceforge/soprano/soprano-2.4.0.1.tar.bz2;
+    sha256 = "0124i92g3vky9wm8ripy3x4gnf1c4pz2hklisds9vld6ylj8gsa6";
   };
   
   # We disable the Java backends, since we do not need them and they make the closure size much bigger
