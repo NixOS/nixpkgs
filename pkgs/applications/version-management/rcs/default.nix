7fd8b772db18c892fd99defa1c87284faa9c20c9
Peter Simons <simons@cryp.to>
2010-02-16 14:19:07 +0000

    pkgs/applications/version-management/rcs/default.nix: meta.platforms is not a list
    svn path=/nixpkgs/trunk/; revision=20053

--- pkgs/applications/version-management/rcs/default.nix
+++ pkgs/applications/version-management/rcs/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation {
   name = "rcs-5.7";
-  
+
   src = fetchurl {
     url = ftp://ftp.cs.purdue.edu/pub/RCS/rcs-5.7.tar;
     md5 = "f7b3f106bf87ff6344df38490f6a02c5";
@@ -14,6 +14,6 @@ stdenv.mkDerivation {
     homepage = http://www.cs.purdue.edu/homes/trinkle/RCS/;
     description = "Revision Control System, a version management system";
     maintainers = [ stdenv.lib.maintainers.eelco ];
-    platforms = [ stdenv.lib.platforms.all ];
+    platforms = stdenv.lib.platforms.all;
   };
 }
