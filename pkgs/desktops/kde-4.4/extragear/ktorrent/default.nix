c8555cfd1b79ae406d46d56b5f419575d96bd545
Sander van der Burg <s.vanderburg@tudelft.nl>
2010-02-16 11:22:54 +0000

    Upgraded ktorrent to 3.3.4
    svn path=/nixpkgs/trunk/; revision=20047

--- pkgs/desktops/kde-4.4/extragear/ktorrent/default.nix
+++ pkgs/desktops/kde-4.4/extragear/ktorrent/default.nix
@@ -2,10 +2,10 @@
  kdelibs, kdepimlibs, kdebase_workspace, automoc4, phonon, qca2}:
 
 stdenv.mkDerivation {
-  name = "ktorrent-3.3.2";
+  name = "ktorrent-3.3.4";
   src = fetchurl {
-    url = http://ktorrent.org/downloads/3.3.2/ktorrent-3.3.2.tar.bz2;
-    sha256 = "08s67nz6wml5bx595czw2gcvrfb09hw1n1rzbj1n8iprr1abcpz1";
+    url = http://ktorrent.org/downloads/3.3.4/ktorrent-3.3.4.tar.bz2;
+    sha256 = "09lq5140135v9919k4wfmqww5jm17yvyqny8hlk10zyzd42vh7zk";
   };
   cmakeFlags = "-DTASKMANAGER_INCLUDE_DIR=${kdebase_workspace}/include";
   buildInputs = [ cmake qt4 perl gmp taglib boost gettext stdenv.gcc.libc
