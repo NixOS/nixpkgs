95c065f33fe255b917f12463e585d6434b144357
Michael Raskin <7c6f434c@mail.ru>
2010-02-21 07:20:26 +0000

    Find some mirror for ArkPandora
    svn path=/nixpkgs/trunk/; revision=20144

--- pkgs/data/fonts/arkpandora/default.nix
+++ pkgs/data/fonts/arkpandora/default.nix
@@ -2,7 +2,10 @@ args : with args;
 let version = lib.attrByPath ["version"] "2.04" args; in
 rec {
   src = fetchurl {
-    url = "http://www.users.bigpond.net.au/gavindi/ttf-arkpandora-${version}.tgz";
+    urls = [
+      "ftp://ftp.FreeBSD.org/pub/FreeBSD/ports/distfiles/ttf-arkpandora-${version}.tgz"
+      "http://www.users.bigpond.net.au/gavindi/ttf-arkpandora-${version}.tgz"
+      ];
     sha256 = "16mfxwlgn6vs3xn00hha5dnmz6bhjiflq138y4zcq3yhk0y9bz51";
   };
 
