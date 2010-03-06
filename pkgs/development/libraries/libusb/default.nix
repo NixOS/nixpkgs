329d46cbd62da804fee673d0d9be4da2f553d4d1
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-03-06 22:34:21 +0000

    Making libusb build on non-linux. It was built with -Werror, and I removed that. There were compiler warnings in non-linux.
    svn path=/nixpkgs/trunk/; revision=20456

--- pkgs/development/libraries/libusb/default.nix
+++ pkgs/development/libraries/libusb/default.nix
@@ -2,6 +2,13 @@
 
 stdenv.mkDerivation {
   name = "libusb-0.1.12";
+
+  # On non-linux, we get warnings compiling, and we don't want the
+  # build to break.
+  patchPhase = ''
+    sed -i s/-Werror// Makefile.in
+  '';
+
   src = fetchurl {
     url = mirror://sourceforge/libusb/libusb-0.1.12.tar.gz;
     md5 = "caf182cbc7565dac0fd72155919672e6";
