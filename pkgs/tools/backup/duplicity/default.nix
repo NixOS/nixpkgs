387216d7fa9196105a3ce3fc7a4e8153c54368a5
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-03-09 15:40:58 +0000

    * Duplicity updated to 0.6.07.
    svn path=/nixpkgs/trunk/; revision=20498

--- pkgs/tools/backup/duplicity/default.nix
+++ pkgs/tools/backup/duplicity/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, python, librsync, gnupg, boto, makeWrapper }:
 
 stdenv.mkDerivation {
-  name = "duplicity-0.6.06";
+  name = "duplicity-0.6.07";
 
   src = fetchurl {
-    url = http://code.launchpad.net/duplicity/0.6-series/0.6.06/+download/duplicity-0.6.06.tar.gz;
-    sha256 = "1g284y24061krigs386x5s7vs7cnwhah7g1mfk9jfn3gzsidv70g";
+    url = http://code.launchpad.net/duplicity/0.6-series/0.6.07/+download/duplicity-0.6.07.tar.gz;
+    sha256 = "11rwr47cgr652m3qb583qlhfjbagnhsidbn8y09fyfi75iifqrjs";
   };
 
   installPhase = ''
