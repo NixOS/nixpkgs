4a44d3244429c35444938bec2307a321966fa90f
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-03-10 22:22:43 +0000

    Made tcpdump crossbuild.
    svn path=/nixpkgs/trunk/; revision=20543

--- pkgs/development/libraries/libpcap/default.nix
+++ pkgs/development/libraries/libpcap/default.nix
@@ -8,11 +8,17 @@ stdenv.mkDerivation rec {
     sha256 = "1h3kmj485qz1i08xs4sc3a0bmhs1rvq0h7gycs7paap2szhw8552";
   };
   
-  buildInputs = [ flex bison ];
+  buildNativeInputs = [ flex bison ];
   
   configureFlags = "--with-pcap=linux";
 
   preInstall = ''ensureDir $out/bin'';
   
   patches = [ ./libpcap_amd64.patch ];
+
+  crossAttrs = {
+    # Stripping hurts in static libraries
+    dontStrip = true;
+    configureFlags = [ "--with-pcap=linux" "ac_cv_linux_vers=2" ];
+  };
 }
--- pkgs/tools/networking/tcpdump/default.nix
+++ pkgs/tools/networking/tcpdump/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, libpcap }:
+{ stdenv, fetchurl, libpcap, enableStatic ? false }:
 
 stdenv.mkDerivation rec {
   name = "tcpdump-4.0.0";
@@ -9,6 +9,14 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [ libpcap ];
+
+  patches = [ ./disable-ipv6.patch ];
+
+  crossAttrs = {
+    LDFLAGS = if enableStatic then "-static" else "";
+    configureFlags = [ "ac_cv_linux_vers=2" ] ++ (stdenv.lib.optional
+      (stdenv.cross.platform.kernelMajor == "2.4") "--disable-ipv6");
+  };
 
   meta = {
     description = "tcpdump, a famous network sniffer";
