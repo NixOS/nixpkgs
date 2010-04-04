355413e0a5b272185d163cb55d52bb1340727a58
Armijn Hemel <armijn@gpl-violations.org>
2010-04-04 15:00:56 +0000

    update to 436
    svn path=/nixpkgs/trunk/; revision=20939

--- pkgs/tools/misc/less/default.nix
+++ pkgs/tools/misc/less/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, ncurses}:
  
 stdenv.mkDerivation {
-  name = "less-418";
+  name = "less-436";
  
   src = fetchurl {
-    url = http://www.greenwoodsoftware.com/less/less-418.tar.gz;
-    sha256 = "1d03n4wx8y1gmf2m8yawzg6ixmfrixcai5l14p9pj8q13gbgqcpm";
+    url = http://www.greenwoodsoftware.com/less/less-436.tar.gz;
+    sha256 = "1lilcx6qrfr2dqahv7r10j9h2v86w5sb7m8wrx2sza9ifkq6z8ap";
   };
  
   buildInputs = [ncurses];
