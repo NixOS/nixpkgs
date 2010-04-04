155515f0767a8cb824441779e4c98275fa7f5d61
Armijn Hemel <armijn@gpl-violations.org>
2010-04-04 14:51:37 +0000

    update to 2.0.19
    svn path=/nixpkgs/trunk/; revision=20937

--- pkgs/tools/networking/gftp/default.nix
+++ pkgs/tools/networking/gftp/default.nix
@@ -1,10 +1,10 @@
 args: with args;
 stdenv.mkDerivation {
-  name = "gftp-2.0.18";
+  name = "gftp-2.0.19";
 
   src = fetchurl {
-    url = http://www.gftp.org/gftp-2.0.18.tar.bz2;
-    sha256 = "8145e18d1edf13e8cb6cd7a69bb69de5c46307086997755654488fb8282d38a2";
+    url = http://www.gftp.org/gftp-2.0.19.tar.bz2;
+    sha256 = "1z8b26n23k0sjbxgrix646b06cnpndpq7cbcj0ilsvvdx5ms81jk";
   };
 
   buildInputs = [ gtk readline ncurses gettext openssl pkgconfig ];
@@ -12,6 +12,6 @@ stdenv.mkDerivation {
   meta = { 
       description = "GTK based FTP client";
       homepage = http://www.gftp.org;
-      license = "GPL";
+      license = "GPLv2+";
   };
 }
