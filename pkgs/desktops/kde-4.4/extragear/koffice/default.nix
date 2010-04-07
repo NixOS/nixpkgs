7e2ce26e0958248164b9a71c6ea69d5f3abfe672
Sander van der Burg <s.vanderburg@tudelft.nl>
2010-04-07 13:59:34 +0000

    Upgraded koffice to 2.1.2
    svn path=/nixpkgs/trunk/; revision=20970

--- pkgs/desktops/kde-4.4/extragear/koffice/default.nix
+++ pkgs/desktops/kde-4.4/extragear/koffice/default.nix
@@ -3,10 +3,10 @@
 , kdelibs, kdepimlibs, automoc4, phonon, qimageblitz, qca2, eigen}:
 
 stdenv.mkDerivation {
-  name = "koffice-2.1.1";
+  name = "koffice-2.1.2";
   src = fetchurl {
-    url = mirror://kde/stable/koffice-2.1.1/koffice-2.1.1.tar.bz2;
-    sha256 = "00dcdb68ykcf7yblq68rxh1ldi70irsxzbcgh36aadzgk0k8r4pz";
+    url = mirror://kde/stable/koffice-2.1.2/koffice-2.1.2.tar.bz2;
+    sha256 = "1nmp2kmksk6vjw9bk04lnl8ld43hf4s0ras6vali725innvkn8v4";
   }; 
   buildInputs = [ cmake qt4 perl lcms exiv2 libxml2 libxslt boost glew shared_mime_info 
                   poppler gsl gmm wv2 libwpd
