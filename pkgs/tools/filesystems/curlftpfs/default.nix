6c37624153d57c8061befae00eb9c6e68f367997
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-04-09 15:40:12 +0000

    Updating curlftpfs
    svn path=/nixpkgs/trunk/; revision=20994

--- pkgs/tools/filesystems/curlftpfs/default.nix
+++ pkgs/tools/filesystems/curlftpfs/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, fuse, curl, pkgconfig, glib, zlib}:
 
 stdenv.mkDerivation {
-  name = "curlftpfs-0.9.1";
+  name = "curlftpfs-0.9.2";
   src = fetchurl {
-    url = mirror://sourceforge/curlftpfs/curlftpfs-0.9.1.tar.gz;
-    sha256 = "9f50cdf02c0dc31ef148410345b2374d294d8853d2dae11775e36b0268ad227d";
+    url = mirror://sourceforge/curlftpfs/curlftpfs-0.9.2.tar.gz;
+    sha256 = "0n397hmv21jsr1j7zx3m21i7ryscdhkdsyqpvvns12q7qwwlgd2f";
   };
   buildInputs = [fuse curl pkgconfig glib zlib];
 }
