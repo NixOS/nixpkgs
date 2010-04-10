86a0b21de87b116f94e63572916c9d5f5385bf85
Armijn Hemel <armijn@gpl-violations.org>
2010-04-10 19:06:59 +0000

    update to 2.8.3
    svn path=/nixpkgs/trunk/; revision=21000

--- pkgs/applications/office/abiword/default.nix
+++ pkgs/applications/office/abiword/default.nix
@@ -3,10 +3,10 @@ enchant, wv, librsvg, bzip2
 }:
 
 stdenv.mkDerivation {
-  name = "abiword-2.8.2";
+  name = "abiword-2.8.3";
   src = fetchurl {
-    url = http://www.abisource.org/downloads/abiword/2.8.2/source/abiword-2.8.2.tar.gz;
-    sha256 = "176v0c41453qspz958s9jlsbh1sdg61j7cpdp86x93lsci3qyrnb";
+    url = http://www.abisource.org/downloads/abiword/2.8.3/source/abiword-2.8.3.tar.gz;
+    sha256 = "1jz3w1rp5wyhv2sk62r14kxljcd3x9kf4axm3zfjaqifhnjpnkxp";
   };
 
   buildInputs = [pkgconfig gtk libglade librsvg bzip2
