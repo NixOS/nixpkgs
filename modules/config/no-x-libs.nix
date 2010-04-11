eea16605325cbde61c7454c10c25dd79689330f9
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-04-11 14:47:06 +0000

    no-x-libs: s/sshd/openssh/
    svn path=/nixos/trunk/; revision=21009

--- modules/config/no-x-libs.nix
+++ modules/config/no-x-libs.nix
@@ -7,14 +7,14 @@
       example = true;
       description = ''
         Switch off the options in the default configuration that require X libraries.
-	Currently this includes: sshd.forwardX11, dbus, hal, fonts.enableCoreFonts,
+	Currently this includes: openssh.forwardX11, dbus, hal, fonts.enableCoreFonts,
 	fonts.enableFontConfig
       '';
     };
   };
   config = pkgs.lib.mkIf config.environment.noXlibs {
     services = {
-      sshd = {
+      openssh = {
         forwardX11 = false;
       };
       dbus.enable = false;
