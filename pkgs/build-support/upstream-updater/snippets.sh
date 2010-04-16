71f2fc2324b23a242220b0e0c7024b8504f6f25a
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-04-16 17:34:01 +0000

    Fix the upstream-update snippets.sh for sourceforge URLs (sourceforge changed the url pattern)
    svn path=/nixpkgs/trunk/; revision=21133

--- pkgs/build-support/upstream-updater/snippets.sh
+++ pkgs/build-support/upstream-updater/snippets.sh
@@ -2,7 +2,7 @@
 
 #http://sourceforge.net/projects/webdruid/files/webdruid/0.6.0-alpha5/webdruid-0.6.0-alpha5.tar.gz/download
 #http://downloads.sourceforge.net/webdruid/files/webdruid/0.6.0-alpha5/webdruid-0.6.0-alpha5.tar.gz
-skipRedirectSF='s@^http://sourceforge.net/projects/@http://downloads.sourceforge.net/@; s@/download$@@'
+skipRedirectSF='s@sourceforge.net/projects@downloads.sourceforge.net/project@; s@/files@@; s@/download$@@;'
 extractReleaseSF='s@.*/([^/]+)/[^/]+@\1@'
 apacheMirror='s@http://www.apache.org/dist/@mirror://apache/@'
 skipRedirectApache='s@/dyn/closer.cgi[?]path=@/dist@'
