21d2c514e5a6b87941980684e1880b6100170c95
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-04-16 19:23:25 +0000

    * libcdio updated to 0.82.
    svn path=/nixpkgs/trunk/; revision=21136

--- pkgs/development/libraries/libcdio/default.nix
+++ pkgs/development/libraries/libcdio/default.nix
@@ -1,20 +1,20 @@
-{fetchurl, stdenv, libcddb, pkgconfig, ncurses, help2man}:
+{ fetchurl, stdenv, libcddb, pkgconfig, ncurses, help2man }:
 
 stdenv.mkDerivation rec {
-  name = "libcdio-0.80";
+  name = "libcdio-0.82";
   
   src = fetchurl {
     url = "mirror://gnu/libcdio/${name}.tar.gz";
-    sha256 = "0rnayw7gcq6jii2xrgg2n4wa8bsf5rw2hcww214lc0ssvcs1m95i";
+    sha256 = "0fax1dzy84dzs20bmpq2gfw6hc1x2x9mhk53wynhcycjw3l3vjqs";
   };
 
-  buildInputs = [libcddb pkgconfig ncurses help2man];
+  buildInputs = [ libcddb pkgconfig ncurses help2man ];
 
   # Disabled because one test (check_paranoia.sh) fails.
   #doCheck = true;
 
   meta = {
-    description = "A library for OS-idependent CD-ROM and CD image access";
+    description = "A library for OS-independent CD-ROM and CD image access";
     longDescription = ''
       GNU libcdio is a library for OS-idependent CD-ROM and
       CD image access.  It includes a library for working with
