d75d0cbb6a351fa6ccbdcfe4abe96c8c582ea968
Marc Weber <marco-oweber@gmx.de>
2010-04-19 16:47:41 +0000

    Eclipse update
    svn path=/nixpkgs/trunk/; revision=21169

--- pkgs/applications/editors/eclipse/default.nix
+++ pkgs/applications/editors/eclipse/default.nix
@@ -6,18 +6,18 @@
 assert stdenv ? glibc;
 
 stdenv.mkDerivation rec {
-  name = "eclipse-3.5.1";
+  name = "eclipse-3.5.2";
   
   src =
     if stdenv.system == "x86_64-linux" then
       fetchurl {
-        url = http://ftp.ing.umu.se/mirror/eclipse/eclipse/downloads/drops/R-3.5.1-200909170800/eclipse-SDK-3.5.1-linux-gtk-x86_64.tar.gz;
-        sha256 = "132zd7q9q29h978wnlsfbrlszc85r1wj30yqs2aqbv3l5xgny1kk";
+        url = http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/eclipse/downloads/drops/R-3.5.2-201002111343/eclipse-SDK-3.5.2-linux-gtk-x86_64.tar.gz;
+        md5 = "54e2ce0660b2b1b0eb4267acf70ea66d";
       }
     else
       fetchurl {
-        url = http://mirrors.linux-bg.org/eclipse/eclipse/downloads/drops/R-3.5.1-200909170800/eclipse-SDK-3.5.1-linux-gtk.tar.gz;
-        sha256 = "0a0lpa7gxg91zswpahi6fvg3csl4csvlym4z2ad5cc1d4yvicp56";
+        url = http://mirror.selfnet.de/eclipse/eclipse/downloads/drops/R-3.5.2-201002111343/eclipse-SDK-3.5.2-linux-gtk.tar.gz;
+        md5 = "bde55a2354dc224cf5f26e5320e72dac";
       };
 
   desktopItem = makeDesktopItem {
