e18b352ce000a76b6dd8341f5773a6d73ec28a98
Rob Vermaas <rob.vermaas@gmail.com>
2010-04-19 07:42:27 +0000

    remove obsolete option from machine spec
    svn path=/nixos/trunk/; revision=21151

--- lib/testing.nix
+++ lib/testing.nix
@@ -145,7 +145,6 @@ rec {
         { config, pkgs, ... }:
         {
           virtualisation.memorySize = 1024;
-          virtualisation.readonlyHostFS = false;
           services.xserver.enable = true;
           services.xserver.displayManager.slim.enable = false;
           services.xserver.displayManager.auto.enable = true;
