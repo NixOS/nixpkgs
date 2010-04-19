d64fc6e17cbe61501f157f3f7641b7cebfd0f63c
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-04-19 14:21:52 +0000

    Commenting at release.nix how hydra will evaluate it.
    svn path=/nixpkgs/trunk/; revision=21167

--- pkgs/top-level/release.nix
+++ pkgs/top-level/release.nix
@@ -1,3 +1,12 @@
+/*
+  This file will be evaluated by hydra with a call like this:
+  hydra_eval_jobs --gc-roots-dir \
+    /nix/var/nix/gcroots/per-user/hydra/hydra-roots --argstr \
+    system i686-linux --argstr system x86_64-linux --arg \
+    nixpkgs "{outPath = ./}" .... release.nix
+    
+  Hydra can be installed with "nix-env -i hydra".
+*/
 with (import ./release-lib.nix);
 
 {
