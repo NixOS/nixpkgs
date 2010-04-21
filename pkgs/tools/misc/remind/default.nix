85c1a2b7ff95f5ccd5f06a72a2ae3f373434eebb
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-04-21 07:00:49 +0000

    "Remind" update.
    svn path=/nixpkgs/trunk/; revision=21193

--- pkgs/tools/misc/remind/default.nix
+++ pkgs/tools/misc/remind/default.nix
@@ -1,15 +1,17 @@
 {stdenv, fetchurl} :
 
 stdenv.mkDerivation {
-  name = "remind-3.1.6";
+  name = "remind-3.1.8";
   src = fetchurl {
-    url = http://www.roaringpenguin.com/files/download/remind-03.01.06.tar.gz;
-    sha256 = "acdf73904c95de55b615d80c7c007abe58d75e41978a16a43333a22583ac7738";
+    url = http://www.roaringpenguin.com/files/download/remind-03.01.08.tar.gz;
+    sha256 = "0gvizrpkbanm515bhd6mq9xxs4g4ji9pplswaj4plaqsk3yw0qjw";
   };
 
   meta = {
     homepage = http://www.roaringpenguin.com/products/remind;
     description = "Sophisticated calendar and alarm program for the console";
     license = "GPLv2";
+    maintainers = with stdenv.lib.maintainers; [viric];
+    platforms = with stdenv.lib.platforms; linux;
   };
 }
