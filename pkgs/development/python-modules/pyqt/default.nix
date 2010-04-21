0de19dec3bb660d8220baabe9a310d42c10dca67
Sander van der Burg <s.vanderburg@tudelft.nl>
2010-04-21 12:06:28 +0000

    - Upgraded SIP to 4.10.2 - Upgraded PyQt to 4.7.3
    svn path=/nixpkgs/trunk/; revision=21203

--- pkgs/development/python-modules/pyqt/default.nix
+++ pkgs/development/python-modules/pyqt/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, python, sip, qt4}:
 
 stdenv.mkDerivation {
-  name = "pyqt-x11-gpl-4.7.2";
+  name = "pyqt-x11-gpl-4.7.3";
   
   src = fetchurl {
-    url = http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4/PyQt-x11-gpl-4.7.2.tar.gz;
-    sha256 = "097gxdr7jqv5a58z89djylm2b21x83crqn2fjsr5zmwhc0zwj5yv";
+    url = http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4/PyQt-x11-gpl-4.7.3.tar.gz;
+    sha256 = "0p12m2rjsm3igpqqmgn79my2h16vhxq8dma9ih1744hpnirdcjjp";
   };
   
   configurePhase = "python ./configure.py --confirm-license -b $out/bin -d $out/lib/${python.libPrefix}/site-packages -v $out/share/sip -p $out/plugins";
--- pkgs/development/python-modules/python-sip/default.nix
+++ pkgs/development/python-modules/python-sip/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, python }:
 
 stdenv.mkDerivation rec {
-  name = "sip-4.10.1";
+  name = "sip-4.10.2";
   
   src = fetchurl {
     url = "http://www.riverbankcomputing.co.uk/static/Downloads/sip4/${name}.tar.gz";
-    sha256 = "16pdk86amcl4hnc9vv2y1ihl8ym9hjkh49andm4jahv4630qhc9h";
+    sha256 = "1vw810fa3g59r3cnfizxykq7dg84zcgs278cwrksk7rsgz8gdn9q";
   };
   
   configurePhase = "python ./configure.py -d $out/lib/${python.libPrefix}/site-packages -b $out/bin -e $out/include";
