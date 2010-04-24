8e905083ae1029131de3054245cb04d526244c18
Ludovic Courtès <ludo@gnu.org>
2010-04-24 21:02:37 +0000

    pwdutils 3.2.6.
    svn path=/nixpkgs/trunk/; revision=21299

--- pkgs/os-specific/linux/pwdutils/default.nix
+++ pkgs/os-specific/linux/pwdutils/default.nix
@@ -1,14 +1,43 @@
 {stdenv, fetchurl, pam, openssl, libnscd}:
-   
-stdenv.mkDerivation {
-  name = "pwdutils-3.1.3";
-   
+
+stdenv.mkDerivation rec {
+  name = "pwdutils-3.2.6";
+
   src = fetchurl {
-    url = mirror://kernel/linux/utils/net/NIS/pwdutils-3.1.3.tar.bz2;
-    md5 = "b18c601e282d8695cbb5ddd87eaa473c";
+    url = "mirror://kernel/linux/utils/net/NIS/${name}.tar.bz2";
+    sha256 = "1pyawvv9x0hiachn1mb257s6hm92dh1ykczgp7ik8z6jl020z3n7";
   };
 
   buildInputs = [pam openssl libnscd];
 
-  configureFlags = "--disable-ldap";
+  patchPhase =
+    '' for i in src/tst-*
+       do
+         sed -i "$i" -e's|/bin/bash|/bin/sh|g'
+       done
+    '';
+
+  configurePhase =
+    '' ./configure --prefix="$out" --exec-prefix="$out/bin" \
+                   --libdir="$out/lib" --disable-static     \
+                   --disable-ldap
+    '';
+
+  # FIXME: The test suite seems to make assumptions that don't hold in Nix
+  # chroots.
+  doCheck = false;
+
+  meta = {
+    description = "Linux pwdutils, utilities to manage passwd information";
+
+    longDescription =
+      '' Pwdutils is a collection of utilities to manage the passwd
+         information stored in local files, NIS, NIS+ or LDAP and can replace
+         the shadow suite complete.
+      '';
+
+    license = "GPLv2";
+
+    platforms = stdenv.lib.platforms.linux;
+  };
 }
