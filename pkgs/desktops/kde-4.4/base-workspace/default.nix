a4842e0987f28debc34dedeeecec455e77fe3477
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-04-26 20:39:08 +0000

    Really add policykit support to kdebase-workspace
    svn path=/nixpkgs/trunk/; revision=21329

--- pkgs/desktops/kde-4.4/base-workspace/default.nix
+++ pkgs/desktops/kde-4.4/base-workspace/default.nix
@@ -2,7 +2,7 @@
 , qt4, sip, pyqt4, kdelibs, kdepimlibs, kdebindings
 , libXi, libXau, libXdmcp, libXtst, libXcomposite, libXdamage, libXScrnSaver
 , lm_sensors, libxklavier, libusb, libpthreadstubs, boost
-, automoc4, phonon, strigi, soprano, qimageblitz, akonadi, polkit_qt
+, automoc4, phonon, strigi, soprano, qimageblitz, akonadi, polkit_qt, policykit
 }:
 
 stdenv.mkDerivation {
@@ -11,10 +11,15 @@ stdenv.mkDerivation {
     url = mirror://kde/stable/4.4.2/src/kdebase-workspace-4.4.2.tar.bz2;
     sha256 = "1g8rnpwpihk6sgvils9cafxz017d23ksg5i9x9275ilydslbsclc";
   };
-  buildInputs = [ cmake perl python qt4 pam consolekit sip pyqt4 kdelibs kdepimlibs kdebindings libpthreadstubs boost libusb stdenv.gcc.libc
-                  libXi libXau libXdmcp libXtst libXcomposite libXdamage libXScrnSaver
-                  lm_sensors libxklavier automoc4 phonon strigi soprano qimageblitz akonadi polkit_qt ];
-  builder = ./builder.sh;
+  buildInputs = [ cmake perl python qt4 pam consolekit sip pyqt4 kdelibs libXtst
+    kdepimlibs kdebindings boost libusb stdenv.gcc.libc libXi libXau libXdmcp
+    libXcomposite libXdamage libXScrnSaver lm_sensors libxklavier automoc4
+    phonon strigi soprano qimageblitz akonadi polkit_qt libpthreadstubs
+    policykit ];
+  patchPhase=''
+    sed -i -e 's|''${PYTHON_SITE_PACKAGES_DIR}|''${CMAKE_INSTALL_PREFIX}/lib/python2.6/site-packages|' \
+      plasma/generic/scriptengines/python/CMakeLists.txt
+  '';
   meta = {
     description = "KDE Workspace";
     longDescription = "KDE base components that are only required to work with X11 such KDM and KWin";
