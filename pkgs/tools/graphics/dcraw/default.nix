311dc9299e4ba74a76875e8a4a0bd84cfdb6162a
Armijn Hemel <armijn@gpl-violations.org>
2010-04-27 14:42:49 +0000

    update to 8.99
    svn path=/nixpkgs/trunk/; revision=21347

--- pkgs/tools/graphics/dcraw/default.nix
+++ pkgs/tools/graphics/dcraw/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, libjpeg, lcms, gettext }:
 
 stdenv.mkDerivation {
-  name = "dcraw-8.98";
+  name = "dcraw-8.99";
 
   src = fetchurl {
-    url = http://www.cybercom.net/~dcoffin/dcraw/archive/dcraw-8.98.tar.gz;
-    sha256 = "0vb375m1s5b1d72d9a5apcyxz2b9zl0pycj700yhy2zsfx804kmp";
+    url = http://www.cybercom.net/~dcoffin/dcraw/archive/dcraw-8.99.tar.gz;
+    sha256 = "0w557lsrj8fs666cxzhhv1rigmy705bnkn94rn856dwck59l3ipq";
   };
 
   buildInputs = [ libjpeg lcms gettext ];
