7d0cd3dbb779c2944f2a3fb0d2c6c7731a2fb123
Peter Simons <simons@cryp.to>
2010-04-29 14:57:13 +0000

    pkgs/development/libraries/freeglut: disable building with "-Wall -Werror" on Darwin
    The build generates warnings on MacOS X because of unused variables, and we
    don't want those trivial problems to lead to a build failure.
    
    svn path=/nixpkgs/trunk/; revision=21430

--- pkgs/development/libraries/freeglut/default.nix
+++ pkgs/development/libraries/freeglut/default.nix
@@ -8,6 +8,8 @@ stdenv.mkDerivation {
     sha256 = "0lmhh5p19rw4wisr0jsl7nsa2hxdaasj0vxk5ri83crhp982v7r6";
   };
 
+  configureFlags = "--" + (if stdenv.isDarwin then "disable" else "enable") + "-warnings";
+
   buildInputs = [x11 mesa];
   patches = ./freeglut-gcc-4.2.patch;
 }
