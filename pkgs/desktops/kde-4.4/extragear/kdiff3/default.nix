f85992530fd1622a120d7b0f6bb1eaaf55de1747
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-04-29 21:31:35 +0000

    Adding meta information to kdiff3 so it gets built by hydra
    svn path=/nixpkgs/trunk/; revision=21443

--- pkgs/desktops/kde-4.4/extragear/kdiff3/default.nix
+++ pkgs/desktops/kde-4.4/extragear/kdiff3/default.nix
@@ -11,4 +11,12 @@ stdenv.mkDerivation {
 
   # kdebase allows having a konqueror plugin built
   buildInputs = [ cmake qt4 perl kdelibs automoc4 phonon gettext kdebase ];
+
+  meta = {
+    homepage = http://kdiff3.sourceforge.net/;
+    license = "GPLv2+";
+    description = "Compares and merges 2 or 3 files or directories";
+    maintainers = with stdenv.lib.maintainers; [viric];
+    platforms = with stdenv.lib.platforms; linux;
+  };
 }
