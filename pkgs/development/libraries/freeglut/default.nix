5d6399548511089cdd286ecf992f26a009de9c42
Peter Simons <simons@cryp.to>
2010-04-29 14:57:17 +0000

    pkgs/development/libraries/freeglut/default.nix: strip trailing white space
    svn path=/nixpkgs/trunk/; revision=21431

--- pkgs/development/libraries/freeglut/default.nix
+++ pkgs/development/libraries/freeglut/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation {
   name = "freeglut-2.4.0";
-  
+
   src = fetchurl {
     url = mirror://sourceforge/freeglut/freeglut-2.4.0.tar.gz;
     sha256 = "0lmhh5p19rw4wisr0jsl7nsa2hxdaasj0vxk5ri83crhp982v7r6";
