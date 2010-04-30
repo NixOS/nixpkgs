4ba1c06c894953562e4542a702f90568d8c6c8d7
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-04-30 20:40:11 +0000

    Support nepomuk and marble in digikam.
    Also stop passing lib.
    
    svn path=/nixpkgs/trunk/; revision=21465

--- pkgs/desktops/kde-4.4/extragear/digikam/default.nix
+++ pkgs/desktops/kde-4.4/extragear/digikam/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, fetchurl, lib, cmake, qt4, kdelibs, automoc4, phonon, qimageblitz, qca2, eigen,
-kdegraphics, lcms, jasper, libgphoto2, kdepimlibs, gettext}:
+{ stdenv, fetchurl, cmake, qt4, kdelibs, automoc4, phonon, qimageblitz, qca2, eigen,
+kdegraphics, lcms, jasper, libgphoto2, kdepimlibs, gettext, soprano, kdeedu }:
 
 stdenv.mkDerivation rec {
   name = "digikam-1.2.0";
@@ -10,13 +10,13 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [ cmake qt4 kdelibs kdegraphics automoc4 phonon qimageblitz qca2 eigen
-    lcms jasper libgphoto2 kdepimlibs gettext ];
+    lcms jasper libgphoto2 kdepimlibs gettext soprano kdeedu ];
   cmakeFlags = [ "-DGETTEXT_INCLUDE_DIR=${gettext}/include" ];
   meta = {
     description = "Photo Management Program";
     license = "GPL";
     homepage = http://www.digikam.org;
-    maintainers = [ lib.maintainers.viric ];
-    platforms = with lib.platforms; linux;
+    maintainers = [ stdenv.lib.maintainers.viric ];
+    platforms = stdenv.lib.platforms.linux;
   };
 }
