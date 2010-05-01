bf926196812a8a2d0c09e700705a4864ecd18bd8
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-05-01 07:07:31 +0000

    Upgrade virtuoso to 6.1.1
    svn path=/nixpkgs/trunk/; revision=21473

--- pkgs/servers/sql/virtuoso/default.nix
+++ pkgs/servers/sql/virtuoso/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, libxml2, openssl, readline }:
 
 stdenv.mkDerivation rec {
-  name = "virtuoso-opensource-6.1.0";
+  name = "virtuoso-opensource-6.1.1";
 
   src = fetchurl {
     url = "mirror://sf/virtuoso/${name}.tar.gz";
-    sha256 = "1kwsb4j7k1a4mnbpjzn4hpg3bwx0bjndan5x8ffpagk5ardqi5b4";
+    sha256 = "1sd70j9i26ml16lig9r9lmrdf5q0kybq71r6vzzzc5v5jxjz0l7w";
   };
 
   buildInputs = [ libxml2 openssl readline ];
@@ -24,4 +24,10 @@ stdenv.mkDerivation rec {
     mv $out/share/virtuoso/doc $out/share/doc/${name}
     find $out -name "*.a" -delete -o -name "*.jar" -delete -o -type d -empty -delete
     '';
+  
+  meta = with stdenv.lib; {
+    homepage = http://virtuoso.openlinksw.com/dataspace/dav/wiki/Main/;
+    platforms = platforms.all;
+    maintainers = [ maintainers.urkud ];
+  };
 }
