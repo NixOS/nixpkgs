20fcfdef64f8cfcc9d47b04caa645a194ecfa644
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-05-04 15:26:34 +0000

    * Revert to Samba 3.3.3 because never versions (including 3.3.12)   break our VM builds.  The child smbd process sends a TERM signal to   its process group when it exists (e.g. on unmount), causing qemu-kvm   to die.
      Reported upstream: https://sourceforge.net/tracker/?func=detail&aid=2996643&group_id=180599&atid=893831
    
    svn path=/nixpkgs/trunk/; revision=21600

--- pkgs/servers/samba/default.nix
+++ pkgs/servers/samba/default.nix
@@ -14,23 +14,22 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "samba-3.5.2";
+  name = "samba-3.3.3";
 
   src = fetchurl {
     url = "http://us3.samba.org/samba/ftp/stable/${name}.tar.gz";
-    sha256 = "0pi946lyn57larvada77pkal48hc0rn07bjxpg2ahz0c389cknl2";
+    sha256 = "08x3ng7ls5c1a95v7djx362i55wdlmnvarpr7rhng5bb55s9n5qn";
   };
 
   buildInputs = [readline pam openldap popt iniparser libunwind fam acl]
     ++ stdenv.lib.optional useKerberos kerberos;
 
-  preConfigure = "cd source3";
+  preConfigure = "cd source";
 
   postInstall = ''
     mkdir -pv $out/lib/cups/backend
     ln -sv ../../../bin/smbspool $out/lib/cups/backend/smb
   '' + stdenv.lib.optionalString (configDir == "") "touch $out/lib/smb.conf";
-
 
   # Don't pass --with-private-dir=/var/samba/private
   #            --with-lockdir=/var/samba/lock
