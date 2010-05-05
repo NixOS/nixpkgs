1edabb560c14770c09674e0fe330f0df9bd6effc
Ludovic Courtès <ludo@gnu.org>
2010-05-05 19:48:32 +0000

    Make `nixUnstable' the default.
    svn path=/nixpkgs/trunk/; revision=21616

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9289,21 +9289,19 @@ let
     inherit stdenv fetchurl jdk makeWrapper;
   };
 
-  nix = makeOverridable (import ../tools/package-management/nix) {
+  nix = nixUnstable;
+
+  nixStable = makeOverridable (import ../tools/package-management/nix) {
     inherit fetchurl stdenv perl curl bzip2 openssl aterm;
     storeDir = getPkgConfig "nix" "storeDir" "/nix/store";
     stateDir = getPkgConfig "nix" "stateDir" "/nix/var";
   };
 
-  # The bleeding edge.
-  nixUnstable = nix;
-  /*
   nixUnstable = makeOverridable (import ../tools/package-management/nix/unstable.nix) {
     inherit fetchurl stdenv perl curl bzip2 openssl aterm;
     storeDir = getPkgConfig "nix" "storeDir" "/nix/store";
     stateDir = getPkgConfig "nix" "stateDir" "/nix/var";
   };
-  */
 
   # The SQLite branch.
   nixSqlite = lowPrio (makeOverridable (import ../tools/package-management/nix/sqlite.nix) {
