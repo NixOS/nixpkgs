e367687e2982637ebc755c114c9ed90fdca1a613
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-05-16 20:25:57 +0000

    * mdadm: Enable incremental activation of swraid arrays from udev.
    svn path=/nixpkgs/trunk/; revision=21805

--- pkgs/os-specific/linux/mdadm/default.nix
+++ pkgs/os-specific/linux/mdadm/default.nix
@@ -1,19 +1,25 @@
-{stdenv, fetchurl, groff}:
+{ stdenv, fetchurl, groff }:
 
 stdenv.mkDerivation rec {
   name = "mdadm-3.1.2";
+  
   src = fetchurl {
     url = "mirror://kernel/linux/utils/raid/mdadm/${name}.tar.bz2";
     sha256 = "0s2d2a01j8cizxqvbgd0sn5bpa1j46q8976078b3jq1q7i1ir0zz";
   };
 
-  buildNativeInputs = [groff];
+  # Enable incremental activation of swraid arrays from udev.
+  patches = [ ./udev.patch ];
 
-  patchPhase = "sed -e 's@/lib/udev@\${out}/lib/udev@' -i Makefile";
+  buildNativeInputs = [ groff ];
 
+  preConfigure = "sed -e 's@/lib/udev@\${out}/lib/udev@' -i Makefile";
+
+  # Force mdadm to use /var/run/mdadm.map for its map file (or
+  # /dev/.mdadm/map as a fallback).
   preBuild =
     ''
-      makeFlagsArray=(INSTALL=install BINDIR=$out/sbin MANDIR=$out/share/man)
+      makeFlagsArray=(INSTALL=install BINDIR=$out/sbin MANDIR=$out/share/man VAR_RUN=/var/run/mdadm ALT_RUN=/dev/.mdadm)
       if [[ -n "$crossConfig" ]]; then
         makeFlagsArray+=(CROSS_COMPILE=$crossConfig-)
       fi
@@ -21,5 +27,6 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "Programs for managing RAID arrays under Linux";
+    homepage = http://neil.brown.name/blog/mdadm;
   };
 }
