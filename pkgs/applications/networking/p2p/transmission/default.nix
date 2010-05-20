0619aaf2ac03d9a4b1e6d570868ce0b680cbd349
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-05-20 13:39:16 +0000

    * Transmission updated to 1.93.
    svn path=/nixpkgs/trunk/; revision=21923

--- pkgs/applications/networking/p2p/transmission/default.nix
+++ pkgs/applications/networking/p2p/transmission/default.nix
@@ -1,14 +1,19 @@
 { stdenv, fetchurl, pkgconfig, openssl, curl, intltool, gtkClient ? true, gtk }:
+
 stdenv.mkDerivation rec {
-  name = "transmission-1.92";
+  name = "transmission-1.93";
+  
   src = fetchurl {
     url = "http://mirrors.m0k.org/transmission/files/${name}.tar.bz2";
-    sha256 = "1vdvl3aza5cip4skhd2y4ip2vjci7q3y3qi3008ykk28ga93gw6s";
+    sha256 = "0w0nsyw10h4lm57qc09ja4iqqwvzcjldnqxi4zp0ha5dkbxv3dz9";
   };
+  
   buildInputs = [ pkgconfig openssl curl intltool ] ++ stdenv.lib.optional gtkClient gtk;
+  
   configureFlags = if gtkClient then "--enable-gtk" else "--disable-gtk";
+  
   meta = {
-    description = "A fast, easy and free BitTorrent client.";
+    description = "A fast, easy and free BitTorrent client";
     longDescription = ''
       Transmission is a BitTorrent client which features a simple interface
       on top of a cross-platform back-end.
@@ -22,7 +27,7 @@ stdenv.mkDerivation rec {
     '';
     homepage = http://www.transmissionbt.com/;
     license = [ "GPLv2" ];
-    maintainers = with stdenv.lib.maintainers; [ astsmtl ];
-    platforms = with stdenv.lib.platforms; linux;
+    maintainers = [ stdenv.lib.maintainers.astsmtl ];
+    platforms = stdenv.lib.platforms.linux;
   };
 }
