5a928cc58431e0099d9070a66bf1a721c7324fd3
Peter Simons <simons@cryp.to>
2010-05-20 10:32:56 +0000

    pkgs/development/libraries/pcre: updated to version 8.02
    svn path=/nixpkgs/branches/stdenv-updates/; revision=21908

--- pkgs/development/libraries/pcre/default.nix
+++ pkgs/development/libraries/pcre/default.nix
@@ -1,13 +1,13 @@
 {stdenv, fetchurl, unicodeSupport ? false, cplusplusSupport ? true}:
 
 stdenv.mkDerivation {
-  name = "pcre-7.8";
-  
+  name = "pcre-8.02";
+
   src = fetchurl {
-    url = mirror://sourceforge/pcre/pcre-7.8.tar.bz2;
-    sha256 = "1zsqk352mx2zklf9bgpg9d88ckfdssbbbiyslhrycfckw8m3qpvr";
+    url = mirror://sourceforge/pcre/pcre-8.02.tar.bz2;
+    sha256 = "1gafmkmkbpdqjbdl85q2z5774gw4gfqjf238icz7gqf3v4v90xd4";
   };
-  
+
   configureFlags = ''
     ${if unicodeSupport then "--enable-unicode-properties" else ""}
     ${if !cplusplusSupport then "--disable-cpp" else ""}
