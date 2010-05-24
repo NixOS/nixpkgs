c293598da8fa12348db4d211f6210ebcc7d07872
Ludovic Courtès <ludo@gnu.org>
2010-05-24 21:28:54 +0000

    tor: Add self as co-maintainer; add to Hydra; improve formatting.
    svn path=/nixpkgs/trunk/; revision=21957

--- pkgs/tools/security/tor/default.nix
+++ pkgs/tools/security/tor/default.nix
@@ -1,10 +1,10 @@
-{stdenv, fetchurl, libevent, openssl, zlib}:
+{ stdenv, fetchurl, libevent, openssl, zlib }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "tor-0.2.1.26";
 
   src = fetchurl {
-    url = "http://www.torproject.org/dist/tor-0.2.1.26.tar.gz";
+    url = "http://www.torproject.org/dist/${name}.tar.gz";
     sha256 = "18kz1hs6lvckkddy4y36gi7ly03ar2il2xzkrf8bfgif1gnn1p3c";
   };
 
@@ -13,23 +13,30 @@ stdenv.mkDerivation {
     '' sed -i "src/or/test.c" -es/localhost/127.0.0.1/g
     '';
 
-  buildInputs = [libevent openssl zlib];
+  buildInputs = [ libevent openssl zlib ];
 
   doCheck = true;
 
   meta = {
     homepage = http://www.torproject.org/;
-    description = "Tor is an onion router enabling Internet anonymity by thwarting network traffic analysis";
-    longDescription=''
-     Tor protects you by bouncing your communications around a distributed 
-     network of relays run by volunteers all around the world: it prevents 
-     somebody watching your Internet connection from learning what sites you 
-     visit, and it prevents the sites you visit from learning your physical 
-     location. Tor works with many of your existing applications, including 
-     web browsers, instant messaging clients, remote login, and other 
-     applications based on the TCP protocol. 
-    '';
-    license="bsd";
-  };
+    description = "Tor, an anonymous network router to improve privacy on the Internet";
 
+    longDescription=''
+      Tor protects you by bouncing your communications around a distributed
+      network of relays run by volunteers all around the world: it prevents
+      somebody watching your Internet connection from learning what sites you
+      visit, and it prevents the sites you visit from learning your physical
+      location. Tor works with many of your existing applications, including
+      web browsers, instant messaging clients, remote login, and other
+      applications based on the TCP protocol.
+    '';
+
+    license="mBSD";
+
+    maintainers =
+      [ # Russell O’Connor <roconnor@theorem.ca> ?
+	stdenv.lib.maintainers.ludo
+      ];
+    platforms = stdenv.lib.platforms.gnu;  # arbitrary choice
+  };
 }
