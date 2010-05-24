e1f009b9dff84a77cec6596d00ed28eb617b7a5d
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-05-24 08:19:41 +0000

    Add yakuake
    svn path=/nixpkgs/trunk/; revision=21954

---
+++ pkgs/desktops/kde-4.4/extragear/yakuake/default.nix
@@ -0,0 +1,19 @@
+{ stdenv, fetchurl, kdelibs, cmake, gettext, perl, automoc4, qt4, phonon }:
+
+stdenv.mkDerivation rec {
+  name = "yakuake-2.9.6";
+
+  src = fetchurl {
+    url = "http://download.berlios.de/yakuake/${name}.tar.bz2";
+    sha256 = "08n6kdzk205rq1bs4yx5f2qawz6xigdhn9air0pbjsi1j630yfzq";
+  };
+
+  buildInputs = [ kdelibs cmake gettext perl automoc4 qt4 phonon stdenv.gcc.libc ];
+
+  meta = with stdenv.lib; {
+    homepage = http://yakuake.kde.org;
+    description = "Quad-style terminal emulator for KDE";
+    maintainers = [ maintainers.urkud ];
+    platforms = linux;
+  };
+}
