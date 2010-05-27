894435ce5e783badca4e63d36860337b16bedeb4
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-05-27 10:05:17 +0000

    * Slight cleanup.
    svn path=/nixos/trunk/; revision=21998

--- tests/openssh.nix
+++ tests/openssh.nix
@@ -2,31 +2,29 @@
 
 {
   nodes = {
+  
     server = 
       {pkgs, config, ...}:
       
       {
         services.openssh.enable = true;
-      }
-    ;
+      };
+      
     client = 
       {pkgs, config, ...}:
       
       {
-      }      
-    ;
+      };
   };
   
   testScript =
   ''
     my $key=`${pkgs.openssh}/bin/ssh-keygen -t dsa -f key -N ""`;
     
-    $server->mustSucceed("mkdir /root/.ssh");
-    $server->mustSucceed("chmod 700 /root/.ssh");
+    $server->mustSucceed("mkdir -m 700 /root/.ssh");
     $server->copyFileFromHost("key.pub", "/root/.ssh/authorized_keys");
     
-    $client->mustSucceed("mkdir /root/.ssh");
-    $client->mustSucceed("chmod 700 /root/.ssh");
+    $client->mustSucceed("mkdir -m 700 /root/.ssh");
     $client->copyFileFromHost("key", "/root/.ssh/id_dsa");
     $client->mustSucceed("chmod 600 /root/.ssh/id_dsa");
     
