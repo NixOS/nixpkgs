6c526f16957eeef9dafe9113c6950176788b7079
David Guibert <david.guibert@gmail.com>
2010-05-27 19:33:16 +0000

    add myself to the maintainers list
    svn path=/nixpkgs/trunk/; revision=22007

--- pkgs/lib/maintainers.nix
+++ pkgs/lib/maintainers.nix
@@ -9,12 +9,13 @@
   astsmtl = "Alexander Tsamutali <astsmtl@yandex.ru>";
   bjg = "Brian Gough <bjg@gnu.org>";
   eelco = "Eelco Dolstra <e.dolstra@tudelft.nl>";
+  guibert = "David Guibert <david.guibert@gmail.com>";
   ludo = "Ludovic Courtès <ludo@gnu.org>";
   marcweber = "Marc Weber <marco-oweber@gmx.de>";
   pierron = "Nicolas B. Pierron <nixos@nbp.name>";
   raskin = "Michael Raskin <7c6f434c@mail.ru>";
-  simons = "Peter Simons <simons@cryp.to>";
   sander = "Sander van der Burg <s.vanderburg@tudelft.nl>";
+  simons = "Peter Simons <simons@cryp.to>";
   urkud = "Yury G. Kudryashov <urkud+nix@ya.ru>";
   viric = "Lluís Batlle i Rossell <viriketo@gmail.com>";
   z77z = "Marco Maggesi <maggesi@math.unifi.it>";
