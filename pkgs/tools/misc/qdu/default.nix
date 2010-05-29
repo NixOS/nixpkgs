95683bb7c7013c1051160f2493ca97fb6823ac5b
David Guibert <david.guibert@gmail.com>
2010-05-29 12:33:24 +0000

    qdu: fix bad formatting.
    svn path=/nixpkgs/trunk/; revision=22054

--- pkgs/tools/misc/qdu/default.nix
+++ pkgs/tools/misc/qdu/default.nix
@@ -1,25 +1,27 @@
 {stdenv, fetchurl, qt3, libXext, libX11}:
+
 stdenv.mkDerivation {
+  name = "qdu-2.2";
   src = fetchurl {
     url = http://artis.imag.fr/~Gilles.Debunne/Code/QDU/qdu-2.2.tar.gz;
     sha256 = "0nn13lcw7bpasdn5xd0ydkyzirz9zamgl8lizi3ncqdzv8bjm7xl";
   };
 
-  buildInputs = [ qt3 libXext libX11];
- 
+  buildInputs = [ qt3 libXext libX11 ];
+
   patchPhase = ''
-  	sed -i "s@/usr/bin@$out/bin@" qdu.pro
-  	sed -i "s@hint>directoryview@hint>directoryView@g" qduInterface.ui
+    sed -i "s@/usr/bin@$out/bin@" qdu.pro
+    sed -i "s@hint>directoryview@hint>directoryView@g" qduInterface.ui
   '';
   buildPhase = ''
-	qmake
-  	make
-	make install
+    qmake
+    make
+    make install
   '';
- 
-  name = "qdu-2.2";
-  meta = { homepage = "http://freshmeat.net/redir/qdu/38383/url_homepage/QDU";
-           description = "A graphical disk usage tool based on Qt";
-           license="GPL";
-         };
+
+  meta = {
+    homepage = "http://artis.imag.fr/~Gilles.Debunne/Code/QDU";
+    description = "A graphical disk usage tool based on Qt";
+    license="GPL";
+  };
 }
