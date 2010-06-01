25e05e4cf9547cebeece26ac4d1754e56a66ab2c
Ludovic Courtès <ludo@gnu.org>
2010-06-01 10:13:29 +0000

    Add SimGrid, a simulator for distributed applications in heterogeneous environments.
    svn path=/nixpkgs/trunk/; revision=22078

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8520,6 +8520,11 @@ let
     };
   };
 
+  simgrid = import ../applications/misc/simgrid {
+    inherit fetchurl cmake ruby;
+    stdenv = stdenv2;
+  };
+
   skype_linux = import ../applications/networking/skype {
     inherit fetchurl stdenv;
     inherit alsaLib freetype fontconfig zlib;
