9f9f58ead931435d371eef7bfa5f6154b4267a5e
Armijn Hemel <armijn@gpl-violations.org>
2010-06-03 21:32:28 +0000

    update to 2.3.1
    svn path=/nixpkgs/trunk/; revision=22134

--- pkgs/desktops/kde-4.4/extragear/amarok/default.nix
+++ pkgs/desktops/kde-4.4/extragear/amarok/default.nix
@@ -2,10 +2,10 @@
 , kdelibs, automoc4, phonon, strigi, soprano, qca2}:
 
 stdenv.mkDerivation {
-  name = "amarok-2.3.0";
+  name = "amarok-2.3.1";
   src = fetchurl {
-    url = mirror://kde/stable/amarok/2.3.0/src/amarok-2.3.0.tar.bz2;
-    sha256 = "1a2c6qy2ik9l7r1lxm82h49acvzxsxxlnlgzwhlrhi27p3sk0xpw";
+    url = mirror://kde/stable/amarok/2.3.1/src/amarok-2.3.1.tar.bz2;
+    sha256 = "0wjaic35bpv6dnnv2wwrbbsqbpng5cn7xfd3ykx25yjg9d6kzvrz";
   };
   inherit mysql loudmouth;
   QT_PLUGIN_PATH="${qtscriptgenerator}/lib/qt4/plugins";
