9709be4f1631f603b1f784619aca1a773a02877a
Michael Raskin <7c6f434c@mail.ru>
2010-06-03 11:30:05 +0000

    Update gnokii
    svn path=/nixpkgs/trunk/; revision=22119

--- pkgs/tools/misc/gnokii/src-for-default.nix
+++ pkgs/tools/misc/gnokii/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="0.6.28.1";
-   name="gnokii-0.6.28.1";
-   hash="0l8ifslvzcwm70pycv8gycdr35g4qk1wsjxbk7b4r5z53jhc6hdz";
+   version="0.6.29";
+   name="gnokii-0.6.29";
+   hash="0w94balyy9l0xy8x3z20f127b5ijpm9a9fbbskz147jyzhprgw0l";
    url="http://www.gnokii.org/download/gnokii/gnokii-${version}.tar.gz";
-   advertisedUrl="http://www.gnokii.org/download/gnokii/gnokii-0.6.28.1.tar.gz";
+   advertisedUrl="http://www.gnokii.org/download/gnokii/gnokii-0.6.29.tar.gz";
   
   
 }
