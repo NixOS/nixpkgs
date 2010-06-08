e4d8ca690c7d7938ad3caaa4dca43143c610a914
Michael Raskin <7c6f434c@mail.ru>
2010-06-08 12:52:36 +0000

    Updating Asymptote
    svn path=/nixpkgs/trunk/; revision=22183

--- pkgs/tools/graphics/asymptote/src-for-default.nix
+++ pkgs/tools/graphics/asymptote/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="1.91";
-   name="asymptote-1.91";
-   hash="1bchzl8czhhjz47mr68m0ysj0dp09ks69qx3l46is3h6rj4bj1sp";
-   url="http://downloads.sourceforge.net/asymptote/files/asymptote/${version}/asymptote-${version}.src.tgz";
-   advertisedUrl="http://downloads.sourceforge.net/asymptote/files/asymptote/1.91/asymptote-1.91.src.tgz";
+   version="1.96";
+   name="asymptote-1.96";
+   hash="0rs2h77j3nq36w0vr131yvlp8lqsanidhf9fvd4bggz5j5r8r850";
+   url="http://downloads.sourceforge.net/project/asymptote/asymptote/${version}/asymptote-${version}.src.tgz";
+   advertisedUrl="http://downloads.sourceforge.net/project/asymptote/asymptote/1.96/asymptote-1.96.src.tgz";
   
   
 }
