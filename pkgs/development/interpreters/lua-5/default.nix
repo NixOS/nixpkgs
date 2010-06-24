029299e7107c277de53fa6dbde19ba20722645a6
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-06-24 23:05:15 +0000

    Making lua build with -fPIC. This allows building vlc on linux-x86_64.
    svn path=/nixpkgs/trunk/; revision=22411

--- pkgs/development/interpreters/lua-5/default.nix
+++ pkgs/development/interpreters/lua-5/default.nix
@@ -6,6 +6,7 @@ stdenv.mkDerivation {
     url = http://www.lua.org/ftp/lua-5.1.4.tar.gz;
     sha256 = "0fmgk100ficm1jbm4ga9xy484v4cm89wsdfckdybb9gjx8jy4f5h";
   };
+  makeFlags = [ "MYCFLAGS=-fPIC" ];
   buildFlags = "linux"; # TODO: support for non-linux systems
   installFlags = "install INSTALL_TOP=\${out}";
   buildInputs = [ ncurses readline ];
