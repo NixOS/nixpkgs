3327ef177fbaff7bb3e40f316243341d30a91c81
Sander van der Burg <s.vanderburg@tudelft.nl>
2010-07-01 15:20:14 +0000

    Upgraded konversation to version 1.3
    svn path=/nixpkgs/trunk/; revision=22439

--- pkgs/desktops/kde-4.4/extragear/konversation/default.nix
+++ pkgs/desktops/kde-4.4/extragear/konversation/default.nix
@@ -2,17 +2,12 @@
 , kdelibs, kdepimlibs, automoc4, phonon, qca2}:
 
 stdenv.mkDerivation {
-  name = "konversation-1.2.3";
+  name = "konversation-1.3";
   src = fetchurl {
-    url = mirror://kde/stable/konversation/1.2.3/src/konversation-1.2.3.tar.bz2;
-    sha256 = "06h0j6clgb7b208i7y9n93zfqajgd7y0wf853r535rd1ysi3kjmg";
+    url = mirror://kde/stable/konversation/1.3/src/konversation-1.3.tar.bz2;
+    sha256 = "05gs75j4qza7i7yydy7rcqhp70r6zblbai5k1fygyhsd23ryqq9n";
   };
   buildInputs = [ cmake qt4 perl gettext stdenv.gcc.libc libXScrnSaver kdelibs kdepimlibs automoc4 phonon qca2 ];
-  patchPhase = ''
-    echo "include_directories(${qt4}/include/QtGui)" > tmp
-    cp src/CMakeLists.txt src/CMakeLists.bak
-    cat tmp src/CMakeLists.bak > src/CMakeLists.txt
-  '';
   meta = {
     description = "Integrated IRC client for KDE";
     license = "GPL";
