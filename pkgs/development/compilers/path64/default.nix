cf30574d4e1396481f8f9a31dc168e5b5b704c87
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-07-04 11:10:58 +0000

    Quite fixing the path64 compiler. It builds programs in C even finding the libc header files.
    Nevertheless, although all stages work, pathcc keeps on saying "INTERNAL ERROR happened",
    but resultign binaries work fine.
    
    It needs some upstream fixing - they have all too much hardcoded to /usr, for example.
    
    svn path=/nixpkgs/trunk/; revision=22449

--- pkgs/development/compilers/path64/default.nix
+++ pkgs/development/compilers/path64/default.nix
@@ -1,5 +1,7 @@
 {stdenv, fetchgit, perl, flex, bison, gmp, mpfr, cmake}:
 
+assert stdenv.system == "x86_64-linux";
+
 stdenv.mkDerivation {
   name = "path64-2010-07-02";
 
@@ -9,15 +11,19 @@ stdenv.mkDerivation {
     sha256 = "fa1320959e1131557d5f82e98f4621a222ec45e1d6e0e7f271d2c13de4fd0dd1";
   };
 
-  cmakeFlags = "
+  patchPhase = ''
+    sed -i s,/usr/bin/ld,$(type -P ld), src/driver/phases.c
+    sed -i s,/lib64/ld-linux-x86-64.so.2,${stdenv.glibc}/lib/ld-linux-x86-64.so.2, src/include/main_defs.h.in
+  '';
+
+  cmakeFlags = ''
     -DPATH64_ENABLE_HUGEPAGES=ON 
     -DPATH64_ENABLE_MATHLIBS=ON -DPATH64_ENABLE_OPENMP=ON 
     -DPATH64_ENABLE_PSCRUNTIME=OFF -DPSC_CRT_PATH=/usr/lib64 
     -DPATH64_ENABLE_PROFILING=OFF -DPATH64_ENABLE_TARGETS=x8664 
     -DCMAKE_BUILD_TYPE=Debug -DPATH64_ENABLE_FORTRAN=OFF
-  ";
-
-  makeFlags = "-j4";
+    -DPSC_CRT_PATH=${stdenv.glibc}/lib
+  '';
 
   buildInputs = [ perl flex bison gmp mpfr cmake ];
 }
