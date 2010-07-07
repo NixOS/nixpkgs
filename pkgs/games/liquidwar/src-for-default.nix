2e6ffe7b135f41551420c251ac6b920b8f324283
Michael Raskin <7c6f434c@mail.ru>
2010-07-07 06:26:57 +0000

    Update LiquidWar
    svn path=/nixpkgs/trunk/; revision=22502

--- pkgs/games/liquidwar/src-for-default.nix
+++ pkgs/games/liquidwar/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="0.0.7beta";
-   name="liquidwar-0.0.7beta";
-   hash="0wl54nfrcfkyacg14y0653xyy2wrlmfwlmfiqrrifv08kzgw6pgb";
+   version="0.0.8beta";
+   name="liquidwar-0.0.8beta";
+   hash="1qcwms44i9x2s38hy64w7xxjkb2j0bh7ril9hldkjy3z208s3wff";
    url="http://download.savannah.gnu.org/releases/liquidwar6/${version}/liquidwar6-${version}.tar.gz";
-   advertisedUrl="http://download.savannah.gnu.org/releases/liquidwar6/0.0.7beta/liquidwar6-0.0.7beta.tar.gz";
+   advertisedUrl="http://download.savannah.gnu.org/releases/liquidwar6/0.0.8beta/liquidwar6-0.0.8beta.tar.gz";
   
   
 }
