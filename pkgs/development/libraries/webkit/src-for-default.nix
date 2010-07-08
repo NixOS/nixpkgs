6be9a7692a52e1e2563f62c8165e4921e1da6639
Michael Raskin <7c6f434c@mail.ru>
2010-07-08 08:21:13 +0000

    Update WebKit back to latest nightly - the SSL problem is not in WebKit version
    svn path=/nixpkgs/trunk/; revision=22521

--- pkgs/development/libraries/webkit/src-for-default.nix
+++ pkgs/development/libraries/webkit/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="r62241";
-   name="webkit-r62241";
-   hash="0ymymv6ls6r1yb75r7il9j1b2kc62nqbdwmivphgpms6idpbrx46";
-   url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r62241.tar.bz2";
-   advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r62241.tar.bz2";
+   version="r62632";
+   name="webkit-r62632";
+   hash="138b1iyp97v2sy8cqqkbgr9gpj954as0ndb13s738raj3yn5bc5g";
+   url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r62632.tar.bz2";
+   advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r62632.tar.bz2";
   
   
 }
