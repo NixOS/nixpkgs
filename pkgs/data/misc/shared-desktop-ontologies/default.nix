d0de407886646679bc5f290927a2b7c6807f0012
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-07-17 22:11:59 +0000

    SDO-0.5
    svn path=/nixpkgs/trunk/; revision=22621

--- pkgs/data/misc/shared-desktop-ontologies/default.nix
+++ pkgs/data/misc/shared-desktop-ontologies/default.nix
@@ -1,14 +1,27 @@
-{stdenv, fetchurl, cmake}:
+{stdenv, fetchurl, cmake, v ? "0.2"}:
 
-stdenv.mkDerivation {
-  name = "shared-desktop-ontologies-0.2";
+stdenv.mkDerivation rec {
+  name = "shared-desktop-ontologies-${v}";
   src = fetchurl {
-    url = mirror://sourceforge/oscaf/shared-desktop-ontologies-0.2.tar.bz2;
-    sha256 = "1w9gfprrp518hb7nm5wspxjd7xx0h08bph6asrx5vrx7j7fzg4m7";
+    url = "mirror://sf/oscaf/${name}.tar.bz2";
+    sha256 =
+      if v == "0.2" then
+        "1w9gfprrp518hb7nm5wspxjd7xx0h08bph6asrx5vrx7j7fzg4m7"
+      else if v == "0.5" then
+        "1a1gs2b314133rg7vzwvnqbxchf7xgs0jpkydid5l2wz98m7j17r"
+      else throw "Unknown version";
   };
   buildInputs = [ cmake ];
-  meta = {
+  meta = with stdenv.lib; {
     description = "Ontologies necessary for the Nepomuk semantic desktop";
-    maintainers = [ stdenv.lib.maintainers.sander ];
+    longDescription = ''
+      The shared-desktop-ontologies package brings the semantic web to the
+      desktop in terms of vocabulary. It contains the well known core
+      ontologies such as RDF and RDFS as well as the Nepomuk ontologies which
+      are used by projects like KDE or Strigi.
+    '';
+    platforms = platforms.all;
+    maintainers = [ maintainers.sander maintainers.urkud ];
   };
 }
+
