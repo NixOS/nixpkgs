ab0d010869eedcdee35935571528158e6e0118c9
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-07-21 12:02:12 +0000

    * Udev updated to 160.
    svn path=/nixpkgs/branches/x-updates/; revision=22688

--- pkgs/os-specific/linux/udev/default.nix
+++ pkgs/os-specific/linux/udev/default.nix
@@ -1,27 +1,27 @@
-{stdenv, fetchurl, gperf, pkgconfig, glib, acl, 
-  libusb, usbutils, pciutils, ...}:
+{ stdenv, fetchurl, gperf, pkgconfig, glib, acl
+, libusb, usbutils, pciutils }:
 
 assert stdenv ? glibc;
 
 stdenv.mkDerivation rec {
-  name = "udev-154";
+  name = "udev-160";
 
   src = fetchurl {
     url = "mirror://kernel/linux/utils/kernel/hotplug/${name}.tar.bz2";
-    sha256 = "1aw5vn96gasvkjh6nfivaf68l6ph33y63zf7gnp3mc7c7zfncm0c";
+    sha256 = "0wx43bxiff8i80k7w3j2iszksdzw1hz6sanx8cjsiypgppi4fr12";
   };
 
-  buildInputs = [gperf pkgconfig glib acl libusb usbutils];
+  buildInputs = [ gperf pkgconfig glib acl libusb usbutils ];
 
-  configureFlags = "--with-pci-ids-path=${pciutils}/share/pci.ids
-    --disable-introspection
-    --with-firmware-path=/root/test-firmware:/var/run/current-system/firmware";
+  configureFlags =
+    ''
+      --with-pci-ids-path=${pciutils}/share/pci.ids
+      --disable-introspection
+      --with-firmware-path=/root/test-firmware:/var/run/current-system/firmware
+    '';
 
   postInstall =
     ''
-      # Install some rules that really should be installed by default.
-      cp rules/packages/40-pilot-links.rules $out/libexec/rules.d/
-
       # The path to rule_generator.functions in write_cd_rules and
       # write_net_rules is broken.  Also, don't store the mutable
       # persistant rules in /etc/udev/rules.d but in
