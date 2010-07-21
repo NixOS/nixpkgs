2f6c7cd3dd2ce0cd39d92f47048a489419be7686
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-07-21 12:01:57 +0000

    * Latest pciutils and usbutils, along with the latest PCI and USB IDs.
    svn path=/nixpkgs/branches/x-updates/; revision=22687

--- pkgs/os-specific/linux/usbutils/default.nix
+++ pkgs/os-specific/linux/usbutils/default.nix
@@ -1,24 +1,24 @@
-{stdenv, fetchurl, pkgconfig, libusb}:
+{ stdenv, fetchurl, pkgconfig, libusb }:
 
 let
 
   # Obtained from http://www.linux-usb.org/usb.ids.bz2.
   usbids = fetchurl {
-    url = http://nixos.org/tarballs/usb.ids.20090808.bz2;
-    sha256 = "1007p5cm3k6vdsfrkwy92yvscazycz71k8wx32jygr50qsv1advj";
+    url = http://nixos.org/tarballs/usb.ids.20100720.bz2;
+    sha256 = "0krncssk0b10z6grw305824zma953l3g2rb7jkk25mb78pw5fd5d";
   };
 
 in
 
 stdenv.mkDerivation rec {
-  name = "usbutils-0.84";
+  name = "usbutils-0.86";
   
   src = fetchurl {
     url = "mirror://sourceforge/linux-usb/${name}.tar.gz";
-    sha256 = "1aksgfsnxq43q3aas4cwhmy34ik3h2sg4iv2al9vz2v9aqnib83n";
+    sha256 = "1x0jkiwrgdb8qwy21iwhxpc8k61apxqp1901h866d1ydsakbxcmk";
   };
   
-  buildInputs = [pkgconfig libusb];
+  buildInputs = [ pkgconfig libusb ];
   
   preBuild = "bunzip2 < ${usbids} > usb.ids";
 
