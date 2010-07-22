b33d41ef084dff09e4e79aaf583b9ae2ac2efc61
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-07-22 14:08:44 +0000

    * clutter-gtk updated to 0.10.4 so that it builds against the current   GTK+ (http://hydra.nixos.org/build/495170).
    svn path=/nixpkgs/branches/x-updates/; revision=22711

--- pkgs/development/libraries/clutter-gtk/default.nix
+++ pkgs/development/libraries/clutter-gtk/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, pkgconfig, clutter, gtk }:
 
 stdenv.mkDerivation rec {
-  name = "clutter-gtk-0.10.2";
+  name = "clutter-gtk-0.10.4";
 
   src = fetchurl {
     url = "http://source.clutter-project.org/sources/clutter-gtk/0.10/${name}.tar.bz2";
-    sha256 = "1jdhhqin4wi8gk6i1m701vrhm6lrvywq3q3yy88ph8yj8h6z4any";
+    sha256 = "04mrq5ddj24zhh7dvyp9bl8510wq7qpmy3rl3wiqwl9c43gahd01";
   };
 
   buildInputs = [ pkgconfig clutter gtk ];
