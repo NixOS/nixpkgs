4e872b84eecb3b2428a35a8b83c2bee2b7a42a29
Peter Simons <simons@cryp.to>
2010-07-27 23:52:14 +0000

    pyOpenSSL: updated to version 0.10
    svn path=/nixpkgs/trunk/; revision=22773

--- pkgs/development/python-modules/pyopenssl/default.nix
+++ pkgs/development/python-modules/pyopenssl/default.nix
@@ -1,16 +1,16 @@
-a :  
-let 
+a :
+let
   fetchurl = a.fetchurl;
 
-  version = a.lib.attrByPath ["version"] "0.8" a; 
+  version = a.lib.attrByPath ["version"] "0.10" a;
   propagatedBuildInputs = with a; [
     openssl python
   ];
 in
 rec {
   src = fetchurl {
-    url = "http://prdownloads.sourceforge.net/sourceforge/pyopenssl/pyOpenSSL-${version}.tar.gz";
-    sha256 = "1qzzycjyp1qsw87msj9kg2q3h7il1bf4jkrwy841y0zi44fl3112";
+    url = "http://pypi.python.org/packages/source/p/pyOpenSSL/pyOpenSSL-0.10.tar.gz";
+    sha256 = "4514f8960389042ca2587f9cb801a13f7990387753fc678680b0c084719b5b60";
   };
 
   inherit propagatedBuildInputs;
@@ -18,7 +18,7 @@ rec {
 
   /* doConfigure should be removed if not needed */
   phaseNames = ["installPythonPackage"];
-      
+
   name = "pyOpenSSL-" + version;
   meta = {
     description = "Python OpenSSL wrapper capable of checking certificates";
