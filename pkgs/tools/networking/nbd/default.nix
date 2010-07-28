4b248a36b03e8be05d46c1de25535673d051560d
Peter Simons <simons@cryp.to>
2010-07-28 21:28:23 +0000

    pkgs/tools/networking/nbd: updated to version 2.9.15
    svn path=/nixpkgs/trunk/; revision=22799

--- pkgs/tools/networking/nbd/default.nix
+++ pkgs/tools/networking/nbd/default.nix
@@ -1,20 +1,25 @@
 {stdenv, fetchurl, pkgconfig, glib}:
 
 stdenv.mkDerivation rec {
-  name = "nbd-2.9.13";
-  meta = {
-    homepage = "http://nbd.sourceforge.net";
-    description = "map arbitrary files as block devices over the network";
-    license = "GPLv2";
-  };
+  name = "nbd-2.9.15";
+
   src = fetchurl {
     url = "mirror://sourceforge/nbd/${name}.tar.bz2";
-    sha256 = "d83da56a97ae70c6c52c994a64e960eaae0664fcadf1fc30f4c9f68c00d19da1";
+    sha256 = "26e2ef18cc2f214d666eef1cfb31df1ba8ee6eaa78390afd6d18f2cce9c0c704";
   };
+
   buildInputs = [pkgconfig glib];
 
   # Link this package statically to generate an nbd-server binary that
   # has no dynamic dependencies and that can be used on (non-Nix) remote
   # machines that have a different setup than the local one.
   configureFlags = "LDFLAGS=-static";
+
+  meta = {
+    homepage = "http://nbd.sourceforge.net";
+    description = "map arbitrary files as block devices over the network";
+    license = "GPLv2";
+    maintainers = [ stdenv.lib.maintainers.simons  ];
+    platforms = stdenv.lib.platforms.gnu;
+  };
 }
