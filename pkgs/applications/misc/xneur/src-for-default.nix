ef3606a0ae783df10b82f094cff8bafb790858b8
Michael Raskin <7c6f434c@mail.ru>
2010-08-02 19:28:15 +0000

    Update XNeur to 0.9.9
    svn path=/nixpkgs/trunk/; revision=22906

--- pkgs/applications/misc/xneur/src-for-default.nix
+++ pkgs/applications/misc/xneur/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="0.9.8";
-   name="xneur-0.9.8";
-   hash="11xpw5vj4yccyq54jnx63kv6lkxa3fqr3kjncmgdc8xqk1pa7jmd";
+   version="0.9.9";
+   name="xneur-0.9.9";
+   hash="07ircm7ngc84h6fpy8q01444vy9cm08q4mbl3q3ya0dg00cq64z4";
    url="http://dists.xneur.ru/release-${version}/tgz/xneur-${version}.tar.bz2";
-   advertisedUrl="http://dists.xneur.ru/release-0.9.8/tgz/xneur-0.9.8.tar.bz2";
+   advertisedUrl="http://dists.xneur.ru/release-0.9.9/tgz/xneur-0.9.9.tar.bz2";
   
   
 }
