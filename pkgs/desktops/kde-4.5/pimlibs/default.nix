c21bd4a0c667ccc935c0d37243a64cb1364ed05d
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-08-02 17:17:41 +0000

    Add kdepimlibs
    svn path=/nixpkgs/trunk/; revision=22894

--- pkgs/desktops/kde-4.5/pimlibs/default.nix
+++ pkgs/desktops/kde-4.5/pimlibs/default.nix
@@ -1,16 +1,16 @@
 { stdenv, fetchurl, cmake, qt4, perl, boost, cyrus_sasl, gpgme, libical, openldap, shared_mime_info
-, kdelibs, automoc4, phonon, akonadi, soprano}:
+, kdelibs, automoc4, akonadi, soprano}:
 
 stdenv.mkDerivation rec {
-  name = "kdepimlibs-4.4.92";
+  name = "kdepimlibs-4.4.95";
 
   src = fetchurl {
-    url = "mirror://kde/unstable/4.4.92/src/${name}.tar.bz2";
-    sha256 = "08y1fvacahv565vfvjjkg10cg660zbfqbqi8n280nxz73qgjmlj7";
+    url = "mirror://kde/unstable/4.4.95/src/${name}.tar.bz2";
+    sha256 = "06ap22p9x7l698skkrnsh7k3h1z0v6h3h3fwjrv4y6lv5vygccny";
   };
 
   buildInputs = [ cmake qt4 perl boost cyrus_sasl gpgme libical openldap
-    shared_mime_info kdelibs automoc4 phonon akonadi soprano ];
+    shared_mime_info kdelibs automoc4 akonadi soprano ];
 
   meta = {
     description = "KDE PIM libraries";
