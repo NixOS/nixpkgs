a162c597e156760559a7e5a7eba0e173592c8ea5
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-08-02 17:20:40 +0000

    akonadi-1.4.0
    svn path=/nixpkgs/trunk/; revision=22898

--- pkgs/desktops/kde-4.5/support/akonadi/default.nix
+++ pkgs/desktops/kde-4.5/support/akonadi/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, cmake, qt4, shared_mime_info, libxslt, boost, mysql, automoc4, soprano}:
 
 stdenv.mkDerivation rec {
-  name = "akonadi-1.3.85";
+  name = "akonadi-1.4.0";
   src = fetchurl {
     url = "http://download.akonadi-project.org/${name}.tar.bz2";
-    sha256 = "1d2ancspavp4qg717hj56j1likb0ifdr65q1awbc2ghqqgd9znck";
+    sha256 = "199fh5yqygr0xdwcnjqqms8vskigbzvwb3071r979606rrsnpnl5";
   };
   buildInputs = [ cmake qt4 shared_mime_info libxslt boost mysql automoc4 soprano ];
   meta = with stdenv.lib; {
