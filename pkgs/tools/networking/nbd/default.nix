c820859eac76c39123396493c8c72420e7879152
Peter Simons <simons@cryp.to>
2010-08-03 18:12:38 +0000

    pkgs/tools/networking/nbd: revert "enable regression tests"
    test suite failure on hydra: http://hydra.nixos.org/build/518375
    
    svn path=/nixpkgs/trunk/; revision=22935

--- pkgs/tools/networking/nbd/default.nix
+++ pkgs/tools/networking/nbd/default.nix
@@ -17,8 +17,6 @@ stdenv.mkDerivation rec {
 
   postInstall = ''install -D -m 444 README "$out/share/doc/nbd/README"'';
 
-  doCheck = true;
-
   meta = {
     homepage = "http://nbd.sourceforge.net";
     description = "map arbitrary files as block devices over the network";
