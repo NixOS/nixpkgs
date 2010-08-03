a7adc46a3b7506f6420ad6e9dff7ae2b03a2522a
Michael Raskin <7c6f434c@mail.ru>
2010-08-03 07:35:29 +0000

    Update parts of QCA to build with fresher OpenSSL
    svn path=/nixpkgs/trunk/; revision=22913

--- pkgs/desktops/kde-4.5/support/qca2/default.nix
+++ pkgs/desktops/kde-4.5/support/qca2/default.nix
@@ -7,6 +7,13 @@ stdenv.mkDerivation {
     sha256 = "49b5474450104a2298747c243de1451ab7a6aeed4bf7df43ffa4b7128a2837b8";
   };
   buildInputs = [ which qt4 ];
+  preBuild = ''
+    sed -i include/QtCrypto/qca_publickey.h -e '/EMSA3_Raw/a,\
+            EMSA3_SHA224,     ///< SHA224, with EMSA3 (ie PKCS#1 Version 1.5) encoding\
+            EMSA3_SHA256,     ///< SHA256, with EMSA3 (ie PKCS#1 Version 1.5) encoding\
+            EMSA3_SHA384,     ///< SHA384, with EMSA3 (ie PKCS#1 Version 1.5) encoding\
+            EMSA3_SHA512      ///< SHA512, with EMSA3 (ie PKCS#1 Version 1.5) encoding'
+  '';
   meta = with stdenv.lib; {
     description = "Qt Cryptographic Architecture";
     license = "LGPL";
--- pkgs/desktops/kde-4.5/support/qca2/ossl.nix
+++ pkgs/desktops/kde-4.5/support/qca2/ossl.nix
@@ -1,19 +1,28 @@
-{stdenv, fetchurl, qt4, qca2, openssl}:
+{stdenv, fetchurl, fetchsvn, qt4, qca2, openssl}:
 
 stdenv.mkDerivation rec {
-  name = "qca-ossl-2.0.0-beta3";
+  version = "2.0.0-beta3";
+  name = "qca-ossl-${version}";
   src = fetchurl {
     url = "http://delta.affinix.com/download/qca/2.0/plugins/${name}.tar.bz2";
     sha256 = "0yy68racvx3clybry2i1bw5bz9yhxr40p3xqagxxb15ihvsrzq08";
+  };
+  # SVN version has stabilized and has a lot of fixes for fresh OpenSSL
+  # Take the main source from there
+  svn_src = fetchsvn {
+    url = svn://anonsvn.kde.org/home/kde/trunk/kdesupport/qca/plugins/qca-ossl ; 
+    rev = 1115936;
+    sha256 =  "ef2c0307e8834e1e7cb23b6fea1cc22486328a37186301a6c11161b1c93d834b";
   };
   buildInputs = [ qt4 qca2 openssl ];
   dontAddPrefix = true;
   configureFlags="--no-separate-debug-info --with-qca=${qca2}
     --with-openssl-inc=${openssl}/include --with-openssl-lib=${openssl}/lib";
   preConfigure=''
+    cp ${svn_src}/qca-ossl.cpp .
+
     configureFlags="$configureFlags --plugins-path=$out/lib/qt4/plugins"
   '';
-  patches = [ ./ossl-remove-whirlpool.patch ];
   meta = with stdenv.lib; {
     description = "Qt Cryptographic Architecture OpenSSL plugin";
     license = "LGPL";
