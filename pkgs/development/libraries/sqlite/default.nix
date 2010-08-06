8852e08ebf01272431af5bdfb5c66cdc34ce9497
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-08-06 07:45:54 +0000

    * Update to SQLite 3.7.0.1, which fixes a corruption bug when the   database is also accessed using SQLite 3.6.23.
    svn path=/nixpkgs/trunk/; revision=22981

--- pkgs/development/libraries/sqlite/default.nix
+++ pkgs/development/libraries/sqlite/default.nix
@@ -3,11 +3,11 @@
 assert readline != null -> ncurses != null;
 
 stdenv.mkDerivation {
-  name = "sqlite-3.7.0";
+  name = "sqlite-3.7.0.1";
 
   src = fetchurl {
-    url = "http://www.sqlite.org/sqlite-amalgamation-3.7.0.tar.gz";
-    sha256 = "13krs46f6fx83hd13i1a6ndbqs12wv4nhgi2dg3aas58r40p7a3w";
+    url = "http://www.sqlite.org/sqlite-amalgamation-3.7.0.1.tar.gz";
+    sha256 = "0nx506wmp32l6sh2p82bd2fscak1b402fp86cxl3r9pfs1yvychj";
   };
 
   buildInputs = [ readline ncurses ];
