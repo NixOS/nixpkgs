ea1ba40374ea824e887862a2ad399486e0f22b24
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-08-09 11:58:26 +0000

    svn path=/nixos/trunk/; revision=23056

--- tests/kde4.nix
+++ tests/kde4.nix
@@ -11,7 +11,6 @@
       services.httpd.adminAddr = "foo@example.org";
       services.httpd.documentRoot = "${pkgs.valgrind}/share/doc/valgrind/html";
 
-      services.xserver.displayManager.slim.enable = false;
       services.xserver.displayManager.kdm.enable = true;
       services.xserver.displayManager.kdm.extraConfig =
         ''
@@ -21,7 +20,6 @@
           AutoLoginPass=foobar
         '';
 
-      services.xserver.desktopManager.default = "kde4";
       services.xserver.desktopManager.kde4.enable = true;
 
       users.extraUsers = pkgs.lib.singleton
