153577d0f3511dd7e81641f64c3424b07a73d17b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-08-09 21:14:08 +0000

    * Add GTK+ as a dependency to generate the icon cache.
    svn path=/nixpkgs/trunk/; revision=23071

--- pkgs/desktops/xfce-4/art/xfce4-icon-theme.nix
+++ pkgs/desktops/xfce-4/art/xfce4-icon-theme.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, intltool }:
+{ stdenv, fetchurl, pkgconfig, intltool, gtk }:
 
 stdenv.mkDerivation rec {
   name = "xfce4-icon-theme-4.4.3";
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
     sha1 = "0c0d0c45cd4a7f609310db8e9d17c1c4a131a6e7";
   };
 
-  buildInputs = [ pkgconfig intltool ];
+  buildInputs = [ pkgconfig intltool gtk ];
 
   meta = {
     homepage = http://www.xfce.org/;
