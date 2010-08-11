53d79f6f9c02cb0d7e34d18895340a8b0475b10d
Rob Vermaas <rob.vermaas@gmail.com>
2010-08-11 17:55:31 +0000

    updated libvirt to 0.8.3
    svn path=/nixpkgs/trunk/; revision=23124

--- pkgs/development/libraries/libvirt/default.nix
+++ pkgs/development/libraries/libvirt/default.nix
@@ -1,19 +1,16 @@
 { stdenv, fetchurl, libxml2, gnutls, devicemapper, perl }:
 
-let version = "0.7.5"; in
+let version = "0.8.3"; in
 
 stdenv.mkDerivation {
   name = "libvirt-${version}";
 
   src = fetchurl {
     url = "http://libvirt.org/sources/libvirt-${version}.tar.gz";
-    sha256 = "922481aadf72a74cf14012fe3967c60d01e70f7e88908410d57428943ab4eb8b";
+    sha256 = "07vsk4g1nxvxc8yr6cdvwp9kvwgm2g7lh6aaggfkxb2775n87q9m";
   };
 
   buildInputs = [ libxml2 gnutls devicemapper perl ];
-
-  # fix for redhat bz 531496
-  patches = [ ./non-absolute-ld.patch ];
 
   # xen currently disabled in nixpkgs
   configureFlags = ''                                                  
