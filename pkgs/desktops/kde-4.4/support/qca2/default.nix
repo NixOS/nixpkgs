05da3db77fe2b7c1300d6a047070a6a4dd6ad7eb
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-08-12 10:39:29 +0000

    Copy qca from kde-4.5 to make qca2_ossl build
    svn path=/nixpkgs/trunk/; revision=23137

--- pkgs/desktops/kde-4.4/support/qca2/default.nix
+++ pkgs/desktops/kde-4.4/support/qca2/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, lib, which, qt4}:
+{stdenv, fetchurl, which, qt4}:
 
 stdenv.mkDerivation {
   name = "qca-2.0.2";
@@ -7,10 +7,17 @@ stdenv.mkDerivation {
     sha256 = "49b5474450104a2298747c243de1451ab7a6aeed4bf7df43ffa4b7128a2837b8";
   };
   buildInputs = [ which qt4 ];
-  meta = {
+  preBuild = ''
+    sed -i include/QtCrypto/qca_publickey.h -e '/EMSA3_Raw/a,\
+            EMSA3_SHA224,     ///< SHA224, with EMSA3 (ie PKCS#1 Version 1.5) encoding\
+            EMSA3_SHA256,     ///< SHA256, with EMSA3 (ie PKCS#1 Version 1.5) encoding\
+            EMSA3_SHA384,     ///< SHA384, with EMSA3 (ie PKCS#1 Version 1.5) encoding\
+            EMSA3_SHA512      ///< SHA512, with EMSA3 (ie PKCS#1 Version 1.5) encoding'
+  '';
+  meta = with stdenv.lib; {
     description = "Qt Cryptographic Architecture";
     license = "LGPL";
     homepage = http://delta.affinix.com/qca;
-    maintainers = [ lib.maintainers.sander ];
+    maintainers = [ maintainers.sander maintainers.urkud ];
   };
 }
