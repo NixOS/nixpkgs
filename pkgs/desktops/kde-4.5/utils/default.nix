501117b1c5ee4c9702b3a2c0b6c2ece6d5c6eb24
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-08-16 20:37:18 +0000

    Rewrite builsystem fixes in upstream-supported manner
    svn path=/nixpkgs/trunk/; revision=23195

--- pkgs/desktops/kde-4.5/utils/default.nix
+++ pkgs/desktops/kde-4.5/utils/default.nix
@@ -1,30 +1,28 @@
 { kdePackage, cmake, qt4, perl, gmp, python, libzip, libarchive, xz
 , sip, pyqt4, pycups, rhpl, system_config_printer, qjson, shared_mime_info
+, kdebase_workspace
 , kdelibs, kdepimlibs, kdebase, kdebindings, automoc4, qimageblitz, qca2}:
 
 kdePackage {
   pn = "kdeutils";
   v = "4.5.0";
   
-  inherit system_config_printer;
-  preConfigure = ''
+  postPatch = ''
+    cp -vn ${qjson}/share/apps/cmake/modules/FindQJSON.cmake cmake/modules
     sed -e "s@/usr\(/share/system-config-printer\)@${system_config_printer}\1@" -i \
       printer-applet/cmake-modules/FindSystemConfigPrinter.py \
       printer-applet/printer-applet.py
     sed -i -e "s|import cupshelpers.ppds, cupshelpers.cupshelpers|import ppds, cupshelpers|" printer-applet/cmake-modules/FindSystemConfigPrinter.py
     '';
 
-  cmakeFlags = "-DCMAKE_MODULE_PATH=${qjson}/share/apps/cmake/modules";
-  patches = [ ./cmake-module-path.diff ];
-  
   buildInputs = [ cmake qt4 perl gmp python libzip libarchive xz sip pyqt4
     pycups rhpl system_config_printer kdelibs kdepimlibs kdebase kdebindings
-    automoc4 qimageblitz qca2 qjson shared_mime_info ];
+    automoc4 qimageblitz qca2 qjson shared_mime_info kdebase_workspace ];
+
+  patches = [ ./log-feature.diff ];
                   
   meta = {
     description = "KDE Utilities";
     license = "GPL";
-    homepage = http://www.kde.org;
-    inherit (kdelibs.meta) maintainers platforms;
   };
 }
