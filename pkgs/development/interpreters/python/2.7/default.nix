0b529ff4d7ed63647a46be5c4b185c368fa8eff8
Peter Simons <simons@cryp.to>
2010-08-18 15:03:34 +0000

    pkgs/development/interpreters/python/2.7: added various meta attributes
    svn path=/nixpkgs/trunk/; revision=23228

--- pkgs/development/interpreters/python/2.7/default.nix
+++ pkgs/development/interpreters/python/2.7/default.nix
@@ -92,6 +92,18 @@ stdenv.mkDerivation ( {
   enableParallelBuilding = true;
 
   meta = {
+    homepage = "http://python.org";
+    description = "Python -- a high-level dynamically-typed programming language";
+    longDescription = ''
+      Python is a remarkably powerful dynamic programming language that
+      is used in a wide variety of application domains. Some of its key
+      distinguishing features include: clear, readable syntax; strong
+      introspection capabilities; intuitive object orientation; natural
+      expression of procedural code; full modularity, supporting
+      hierarchical packages; exception-based error handling; and very
+      high level dynamic data types.
+    '';
+    license = "GPLv2";
     platforms = stdenv.lib.platforms.all;
     maintainers = [ stdenv.lib.maintainers.simons ];
   };
