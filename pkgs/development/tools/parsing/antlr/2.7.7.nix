e9fd810997aadfcc857a19b792c26a2c82cb6ac2
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-08-18 18:56:03 +0000

    antlr-2: compile library
    svn path=/nixpkgs/trunk/; revision=23235

--- pkgs/development/tools/parsing/antlr/2.7.7.nix
+++ pkgs/development/tools/parsing/antlr/2.7.7.nix
@@ -6,5 +6,6 @@ stdenv.mkDerivation {
     url = "http://www.antlr2.org/download/antlr-2.7.7.tar.gz";
     sha256 = "1ffvcwdw73id0dk6pj2mlxjvbg0662qacx4ylayqcxgg381fnfl5";
   };
+  patches = [ ./2.7.7-fixes.patch ];
   buildInputs = [jdk python];
 }
