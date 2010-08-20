7120017365f8b7e0334b0bc34cde75025d25646f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-08-20 09:18:53 +0000

    * Add meta info to xpf.
    svn path=/nixpkgs/trunk/; revision=23269

--- pkgs/tools/text/xml/xpf/default.nix
+++ pkgs/tools/text/xml/xpf/default.nix
@@ -4,9 +4,16 @@ assert libxml2.pythonSupport == true;
 
 stdenv.mkDerivation {
   name = "xpf-0.2";
+  
   src = fetchurl {
     url = http://nixos.org/tarballs/xpf-0.2.tar.gz;
     md5 = "d92658828139e1495e052d0cfe25d312";
   };
+  
   buildInputs = [python libxml2];
+
+  meta = {
+    description = "XML Pipes and Filters - command line tools for manipulating and querying XML data";
+    homepage = http://www.cs.uu.nl/wiki/bin/view/Martin/XmlPipesAndFilters;
+  };
 }
