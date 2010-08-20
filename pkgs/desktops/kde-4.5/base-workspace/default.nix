b97b22220fcc12dfe1d0133a34f905864ff20ec5
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-08-20 21:36:56 +0000

    kdebase-workspace: more dependencies, list TODO
    svn path=/nixpkgs/trunk/; revision=23293

--- pkgs/desktops/kde-4.5/base-workspace/default.nix
+++ pkgs/desktops/kde-4.5/base-workspace/default.nix
@@ -3,17 +3,19 @@
 , libXi, libXau, libXdmcp, libXtst, libXcomposite, libXdamage, libXScrnSaver
 , lm_sensors, libxklavier, libusb, libpthreadstubs, boost
 , automoc4, strigi, soprano, qimageblitz, akonadi
-, libdbusmenu_qt
+, libdbusmenu_qt, libqalculate, pciutils, libraw1394, bluez
 }:
 
 kdePackage {
   pn = "kdebase-workspace";
   v = "4.5.0";
 
+# TODO: qedje, qzion, ggadgets, libgps
   buildInputs = [ cmake perl python qt4 pam consolekit sip pyqt4 kdelibs libXtst
-    kdepimlibs kdebindings boost libusb libXi libXau libXdmcp
+    kdepimlibs kdebindings boost libusb libXi libXau libXdmcp libraw1394
     libXcomposite libXdamage libXScrnSaver lm_sensors libxklavier automoc4
-    strigi soprano qimageblitz akonadi libpthreadstubs libdbusmenu_qt ];
+    strigi soprano qimageblitz akonadi libpthreadstubs libdbusmenu_qt libqalculate
+    pciutils bluez ];
 
   meta = {
     description = "KDE Workspace";
