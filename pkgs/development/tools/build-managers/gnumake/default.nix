58b1dfe0fe9c67f02c3eb4d76f97b8a6db8de677
Ludovic Courtès <ludo@gnu.org>
2010-08-20 15:26:41 +0000

    GNU Make 3.82.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=23281

--- pkgs/development/tools/build-managers/gnumake/default.nix
+++ pkgs/development/tools/build-managers/gnumake/default.nix
@@ -1,19 +1,22 @@
 {stdenv, fetchurl}:
 
+let version = "3.82"; in
 stdenv.mkDerivation {
-  name = "gnumake-3.81";
-  
+  name = "gnumake-${version}";
+
   src = fetchurl {
-    url = mirror://gnu/make/make-3.81.tar.bz2;
-    md5 = "354853e0b2da90c527e35aabb8d6f1e6";
+    url = "mirror://gnu/make/make-${version}.tar.bz2";
+    sha256 = "0ri98385hsd7li6rh4l5afcq92v8l2lgiaz85wgcfh4w2wzsghg2";
   };
-  
+
+  doCheck = true;
+
   patches =
     [
       # Provide nested log output for subsequent pretty-printing by
       # nix-log2xml.
       ./log.patch
-      
+
       # Purity: don't look for library dependencies (of the form
       # `-lfoo') in /lib and /usr/lib.  It's a stupid feature anyway.
       # Likewise, when searching for included Makefiles, don't look in
@@ -22,8 +25,22 @@ stdenv.mkDerivation {
     ];
 
   meta = {
-    description = "A program for automatically building non-source files from sources";
+    description = "GNU Make, a program controlling the generation of non-source files from sources";
+
+    longDescription =
+      '' Make is a tool which controls the generation of executables and
+         other non-source files of a program from the program's source files.
+
+         Make gets its knowledge of how to build your program from a file
+         called the makefile, which lists each of the non-source files and
+         how to compute it from other files. When you write a program, you
+         should write a makefile for it, so that it is possible to use Make
+         to build and install the program.
+      '';
+
     homepage = http://www.gnu.org/software/make/;
-    license = "GPL";
+
+    license = "GPLv3+";
+    maintainers = [ stdenv.lib.maintainers.ludo ];
   };
 }
