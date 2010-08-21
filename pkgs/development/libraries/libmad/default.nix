c68d102286ef21680f1984efeb606f48f8bd59c2
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-08-21 16:53:25 +0000

    Patching libmad for mips (Ben Nanonote)
    svn path=/nixpkgs/branches/stdenv-updates/; revision=23312

--- pkgs/development/libraries/libmad/default.nix
+++ pkgs/development/libraries/libmad/default.nix
@@ -8,11 +8,13 @@ stdenv.mkDerivation {
     sha256 = "bbfac3ed6bfbc2823d3775ebb931087371e142bb0e9bb1bee51a76a6e0078690";
   };
 
-  patches = [ ./pkgconfig.patch ];
+  patches = [ ./001-mips_removal_h_constraint.patch ./pkgconfig.patch ];
+
+  buildNativeInputs = [ autoconf ];
 
   # The -fforce-mem flag has been removed in GCC 4.3.
   preConfigure = ''
-    ${autoconf}/bin/autoconf
+    autoconf
     substituteInPlace configure --replace "-fforce-mem" ""
   '';
 
