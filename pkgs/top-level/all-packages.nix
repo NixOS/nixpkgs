8d7045e1ffb4155514c41e8d8f797bc1dd592cb5
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-08-21 09:56:01 +0000

    Disable fbcondecor patch - doesn't work with 2.6.35.2
    svn path=/nixpkgs/trunk/; revision=23301

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4539,7 +4539,7 @@ let
   linux_2_6_35 = makeOverridable (import ../os-specific/linux/kernel/linux-2.6.35.nix) {
     inherit fetchurl stdenv perl mktemp module_init_tools ubootChooser;
     kernelPatches =
-      [ kernelPatches.fbcondecor_2_6_35
+      [ #kernelPatches.fbcondecor_2_6_35
         kernelPatches.sec_perm_2_6_24
         # kernelPatches.aufs2_2_6_35
       ];
