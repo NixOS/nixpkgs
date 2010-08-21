6bb40f5d0876b2bb214334a62e4ace0e38b178e2
Michael Raskin <7c6f434c@mail.ru>
2010-08-21 07:13:18 +0000

    Step back WebKit - latest nightly fails
    svn path=/nixpkgs/trunk/; revision=23299

--- pkgs/development/libraries/webkit/src-for-default.nix
+++ pkgs/development/libraries/webkit/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="r65727";
-   name="webkit-r65727";
-   hash="1cpwcivzdl9phladzn8sfpavz5l0hwdy0ssnxrpi2rpi15nfy5zd";
-   url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r65727.tar.bz2";
-   advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r65727.tar.bz2";
+   version="r65707";
+   name="webkit-r65707";
+   hash="18nm0774zv8pknz2vifs30z4nq394qk0m8mjvzanlh3gamv4zlsh";
+   url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r65707.tar.bz2";
+   advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r65707.tar.bz2";
   
   
 }
