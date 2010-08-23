a65aab017161d450e74526b4d7e4c96eb79acf76
Peter Simons <simons@cryp.to>
2010-08-23 14:27:33 +0000

    pkgs/development/interpreters/lua-5: cosmetic changes
    svn path=/nixpkgs/trunk/; revision=23358

--- pkgs/development/interpreters/lua-5/default.nix
+++ pkgs/development/interpreters/lua-5/default.nix
@@ -1,11 +1,13 @@
-{stdenv, fetchurl, ncurses, readline}: 
+{stdenv, fetchurl, ncurses, readline}:
 
 stdenv.mkDerivation {
   name = "lua-5.1.4";
+
   src = fetchurl {
-    url = http://www.lua.org/ftp/lua-5.1.4.tar.gz;
+    url = "http://www.lua.org/ftp/lua-5.1.4.tar.gz";
     sha256 = "0fmgk100ficm1jbm4ga9xy484v4cm89wsdfckdybb9gjx8jy4f5h";
   };
+
   makeFlags = [ "CFLAGS=-fPIC" ];
   buildFlags = "linux"; # TODO: support for non-linux systems
   installFlags = "install INSTALL_TOP=\${out}";
