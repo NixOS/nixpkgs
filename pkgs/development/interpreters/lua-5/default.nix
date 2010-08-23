d3a8a3f7d00b8c586d0e29bce6c2d8b4cd7790d1
Peter Simons <simons@cryp.to>
2010-08-23 14:26:26 +0000

    pkgs/development/interpreters/lua-5: added meta section to the expression
    svn path=/nixpkgs/trunk/; revision=23357

--- pkgs/development/interpreters/lua-5/default.nix
+++ pkgs/development/interpreters/lua-5/default.nix
@@ -15,4 +15,20 @@ stdenv.mkDerivation {
     install -D -m 644 etc/lua.pc $out/lib/pkgconfig/lua.pc
   '';
   buildInputs = [ ncurses readline ];
+
+  meta = {
+    homepage = "http://www.lua.org";
+    description = "Lua is a powerful, fast, lightweight, embeddable scripting language.";
+    longDescription = ''
+      Lua combines simple procedural syntax with powerful data
+      description constructs based on associative arrays and extensible
+      semantics. Lua is dynamically typed, runs by interpreting bytecode
+      for a register-based virtual machine, and has automatic memory
+      management with incremental garbage collection, making it ideal
+      for configuration, scripting, and rapid prototyping.
+    '';
+    license = "MIT";
+    platforms = stdenv.lib.platforms.linux;
+    maintainers = [];
+  };
 }
