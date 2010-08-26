24e71fa8fc75a7d2e670151f7f03905d98c0f74c
Peter Simons <simons@cryp.to>
2010-08-26 11:44:43 +0000

    pkgs/applications/science/math/maxima: updated to version 5.22.1
    svn path=/nixpkgs/trunk/; revision=23448

--- pkgs/applications/science/math/maxima/default.nix
+++ pkgs/applications/science/math/maxima/default.nix
@@ -2,21 +2,29 @@
 
 let
     name    = "maxima";
-    version = "5.21.1";
+    version = "5.22.1";
 in
 stdenv.mkDerivation {
   name = "${name}-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/${name}/${name}-${version}.tar.gz";
-    sha256 = "1dae887e1787871437d699a6b1acc1c1f7428729487492a07c6a31e26bf53a1b";
+    sha256 = "0sdrv3lra6j3ylaqsblnd3x7rq4ybafyj7rb114ycadpx2qf06lq";
   };
 
   buildInputs = [clisp];
 
   meta = {
     description = "Maxima computer algebra system";
-    homepage = http://maxima.sourceforge.net;
+    homepage = "http://maxima.sourceforge.net";
+
+    longDescription = ''
+      Maxima is a fairly complete computer algebra system written in
+      lisp with an emphasis on symbolic computation. It is based on
+      DOE-MACSYMA and licensed under the GPL. Its abilities include
+      symbolic integration, 3D plotting, and an ODE solver.
+    '';
+
     platforms = stdenv.lib.platforms.all;
     maintainers = [ stdenv.lib.maintainers.simons ];
   };
