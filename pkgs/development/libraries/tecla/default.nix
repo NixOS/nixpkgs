c1469fba93a2ac42068f49dbc4ca74c124f417b9
Peter Simons <simons@cryp.to>
2010-08-26 18:43:37 +0000

    pkgs/development/libraries/tecla: enable Hydra build on all platforms
    svn path=/nixpkgs/trunk/; revision=23470

--- pkgs/development/libraries/tecla/default.nix
+++ pkgs/development/libraries/tecla/default.nix
@@ -1,18 +1,21 @@
 { stdenv, fetchurl }:
 
-stdenv.mkDerivation rec
-{
+stdenv.mkDerivation rec {
   name = "tecla-1.6.1";
-  meta =
-  {
-    homepage = "http://www.astro.caltech.edu/~mcs/tecla/";
-    description = "Tecla command-line editing library";
-    license = "as-is";
-  };
-  src = fetchurl
-  {
+
+  src = fetchurl {
     url = "http://www.astro.caltech.edu/~mcs/tecla/lib${name}.tar.gz";
     sha256 = "18crv6q5f9g0cg6ry5h9dsa10inhpslklrv20h70f58lpm3jknr1";
   };
+
   configureFlags = "CFLAGS=-O3 CXXFLAGS=-O3";
+
+  meta = {
+    homepage = "http://www.astro.caltech.edu/~mcs/tecla/";
+    description = "Tecla command-line editing library";
+    license = "as-is";
+
+    platforms = stdenv.lib.platforms.all;
+    maintainers = [ stdenv.lib.maintainers.simons ];
+  };
 }
