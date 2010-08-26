7b9b8f9e2e0aa0e51738a015a2cc752763433f7a
Peter Simons <simons@cryp.to>
2010-08-26 09:41:22 +0000

    pkgs/tools/admin/tightvnc/default.nix: added meta section (plus various cosmetic changes)
    svn path=/nixpkgs/trunk/; revision=23438

--- pkgs/tools/admin/tightvnc/default.nix
+++ pkgs/tools/admin/tightvnc/default.nix
@@ -2,19 +2,32 @@
 
 stdenv.mkDerivation {
   name = "tightvnc-1.3.10";
-  builder = ./builder.sh;
-  gcc=stdenv.gcc.gcc;
-	inherit perl;
+
   src = fetchurl {
     url = mirror://sourceforge/vnc-tight/tightvnc-1.3.10_unixsrc.tar.bz2;
     sha256 = "f48c70fea08d03744ae18df6b1499976362f16934eda3275cead87baad585c0d";
   };
 
   # for the builder script
-  inherit xauth;
+  inherit xauth fontDirectories perl;
+  gcc = stdenv.gcc.gcc;
 
-  inherit fontDirectories;
+  buildInputs = [x11 zlib libjpeg imake gccmakedep libXmu libXaw libXpm libXp xauth];
+  builder = ./builder.sh;
 
-  buildInputs = [x11 zlib libjpeg imake gccmakedep libXmu libXaw libXpm libXp
-  	xauth];
+  meta = {
+    license = "GPLv2+";
+    homepage = "http://vnc-tight.sourceforge.net/";
+    description = "TightVNC is an improved version of VNC";
+
+    longDescription = ''
+      TightVNC is an improved version of VNC, the great free
+      remote-desktop tool. The improvements include bandwidth-friendly
+      "tight" encoding, file transfers in the Windows version, enhanced
+      GUI, many bugfixes, and more.
+    '';
+
+    maintainers = [];
+    platforms = stdenv.lib.platforms.unix;
+  };
 }
