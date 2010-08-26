eb37b385f90994a483bab2ec81a54d8693e94f13
Peter Simons <simons@cryp.to>
2010-08-26 12:03:17 +0000

    pkgs/top-level/python-packages.nix: added pexpect version 2.3
    svn path=/nixpkgs/trunk/; revision=23452

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -509,6 +509,40 @@ rec {
     };
   });
 
+  pexpect = buildPythonPackage {
+    name = "pexpect-2.3";
+
+    src = fetchurl {
+      url = "http://pexpect.sourceforge.net/pexpect-2.3.tar.gz";
+      sha256 = "0x8bfjjqygriry1iyygm5048ykl5qpbpzqfp6i8dhkslm3ryf5fk";
+    };
+
+    meta = {
+      homepage = "http://www.noah.org/wiki/Pexpect";
+      description = "Automate interactive console applications such as ssh, ftp, etc.";
+      license = "MIT";
+
+      longDescription = ''
+        Pexpect is similar to the Don Libes "Expect" system, but Pexpect
+        as a different interface that is easier to understand. Pexpect
+        is basically a pattern matching system. It runs programs and
+        watches output. When output matches a given pattern Pexpect can
+        respond as if a human were typing responses. Pexpect can be used
+        for automation, testing, and screen scraping. Pexpect can be
+        used for automating interactive console applications such as
+        ssh, ftp, passwd, telnet, etc. It can also be used to control
+        web applications via "lynx", "w3m", or some other text-based web
+        browser. Pexpect is pure Python. Unlike other Expect-like
+        modules for Python Pexpect does not require TCL or Expect nor
+        does it require C extensions to be compiled. It should work on
+        any platform that supports the standard Python pty module.
+      '';
+
+      maintainers = [ stdenv.lib.maintainers.simons ];
+      platforms = python.meta.platforms;
+    };
+  };
+
   psycopg2 = buildPythonPackage rec {
     name = "psycopg2-2.0.13";
 
