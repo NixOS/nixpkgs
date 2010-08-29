733b2893a747074b4a4e1f409181b2a158f4a08c
Armijn Hemel <armijn@gpl-violations.org>
2010-08-29 13:40:02 +0000

    update to 2.4.10.1, add correct license
    svn path=/nixpkgs/trunk/; revision=23506

--- pkgs/development/libraries/libgphoto2/default.nix
+++ pkgs/development/libraries/libgphoto2/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, pkgconfig, libusb, libtool, libexif, libjpeg, gettext}:
 
 stdenv.mkDerivation rec {
-  name = "libgphoto2-2.4.5";
+  name = "libgphoto2-2.4.10.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/gphoto/${name}.tar.bz2";
-    sha256 = "1pipdwjxbjg7y9n5ldz6qlpiiiqyba6jx315277ams5d8jxg2bfk";
+    sha256 = "12m99hcak59ppq1ijnfsrcm09mdj102f1xjmz4myggwjdymwc3lq";
   };
   
   buildInputs = [pkgconfig libtool libjpeg gettext];
@@ -16,6 +16,6 @@ stdenv.mkDerivation rec {
   meta = {
     homepage = http://www.gphoto.org/proj/libgphoto2/;
     description = "A library for accessing digital cameras";
-    license = "LGPL-2";
+    license = "LGPL 2.1";
   };
 }
