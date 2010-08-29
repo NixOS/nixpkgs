fe7e41324c73ee0ffee4835cf1f62b3ff8b95db5
Armijn Hemel <armijn@gpl-violations.org>
2010-08-29 13:47:59 +0000

    update to 0.6.19, add meta information
    svn path=/nixpkgs/trunk/; revision=23507

--- pkgs/development/libraries/libexif/default.nix
+++ pkgs/development/libraries/libexif/default.nix
@@ -1,12 +1,19 @@
 { stdenv, fetchurl, gettext }:
 
 stdenv.mkDerivation rec {
-  name = "libexif-0.6.16";
+  name = "libexif-0.6.19";
 
   src = fetchurl {
     url = "mirror://sourceforge/libexif/${name}.tar.bz2";
-    sha256 = "0hxc3aik3sn8xq4mbmxxb8ycx2lwffmhi5xvz0zjffhfwkaqas6v";
+    sha256 = "1gfa07bzs2lk0n887f1lvz5b9a7abyc3f5zaf39w4sf23hk9axpr";
   };
 
   buildInputs = [ gettext ];
+
+  meta = {
+    homepage = http://libexif.sourceforge.net/;
+    description = "A library to read and manipulate EXIF data in digital photographs;
+    license = "LGPL 2.1";
+  };
+
 }
