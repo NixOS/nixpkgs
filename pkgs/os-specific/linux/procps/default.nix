94324d95b39ff04dd5cf0f4d0eb0c36ce7c6eaf6
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-08-29 09:58:53 +0000

    Fix to make procps build with gnumake 3.82
    svn path=/nixpkgs/branches/stdenv-updates/; revision=23492

--- pkgs/os-specific/linux/procps/default.nix
+++ pkgs/os-specific/linux/procps/default.nix
@@ -7,6 +7,6 @@ stdenv.mkDerivation {
     url = http://procps.sourceforge.net/procps-3.2.8.tar.gz;
     sha256 = "0d8mki0q4yamnkk4533kx8mc0jd879573srxhg6r2fs3lkc6iv8i";
   };
-  patches = [./makefile.patch ./procps-build.patch];
+  patches = [./makefile.patch ./procps-build.patch ./gnumake3.82.patch];
   buildInputs = [ncurses];
 }
