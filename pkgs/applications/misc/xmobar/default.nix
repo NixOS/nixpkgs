fec59669329d148a1a46a0f7d19bbe6a4d71774c
Andres Löh <mail@andres-loeh.de>
2010-08-31 10:40:28 +0000

    Fix renamed argument problem.
    svn path=/nixpkgs/trunk/; revision=23556

--- pkgs/applications/misc/xmobar/default.nix
+++ pkgs/applications/misc/xmobar/default.nix
@@ -1,11 +1,11 @@
-{cabal, X11, mtl, parsec, stm, utf8String, X11_xft}:
+{cabal, X11, mtl, parsec, stm, utf8String, X11Xft}:
 
 cabal.mkDerivation (self : {
   pname = "xmobar";
   name = "${self.fname}";
   version = "0.9.2";
   sha256 = "361295f5dc912512a2eb644ecd331562a271243192be6215cb071e44f50c7c66";
-  extraBuildInputs = [X11 mtl parsec stm utf8String X11_xft];
+  extraBuildInputs = [X11 mtl parsec stm utf8String X11Xft];
   meta = {
     description = "xmobar is a minimalistic text based status bar";
   };
