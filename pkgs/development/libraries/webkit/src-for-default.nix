a33c1d27881aba13338e1c706f7764d41af0c81d
Michael Raskin <7c6f434c@mail.ru>
2010-08-31 18:47:07 +0000

    Stepping back roughly halfway to our previous version of Webkit
    svn path=/nixpkgs/trunk/; revision=23572

--- pkgs/development/libraries/webkit/src-for-default.nix
+++ pkgs/development/libraries/webkit/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="r66329";
-   name="webkit-r66329";
-   hash="1kfigk95lyfgjjks9c62gnb334zxnz44zcirxlx23sq3vrkqgs84";
-   url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r66329.tar.bz2";
-   advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r66329.tar.bz2";
+   version="r66052";
+   name="webkit-r66052";
+   hash="1c8wmdiq8y38isfyb1dxx6qbi8bs3kmws5vxm0q22gy171rmkjai";
+   url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r66052.tar.bz2";
+   advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r66052.tar.bz2";
   
   
 }
