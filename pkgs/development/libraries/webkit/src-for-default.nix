26b3c63e32272a7bc3c03fc5e442ba658aba35a8
Michael Raskin <7c6f434c@mail.ru>
2010-09-01 08:32:59 +0000

    Going further back with WebKit
    svn path=/nixpkgs/trunk/; revision=23573

--- pkgs/development/libraries/webkit/src-for-default.nix
+++ pkgs/development/libraries/webkit/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="r66052";
-   name="webkit-r66052";
-   hash="1c8wmdiq8y38isfyb1dxx6qbi8bs3kmws5vxm0q22gy171rmkjai";
-   url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r66052.tar.bz2";
-   advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r66052.tar.bz2";
+   version="r65727";
+   name="webkit-r65727";
+   hash="1cpwcivzdl9phladzn8sfpavz5l0hwdy0ssnxrpi2rpi15nfy5zd";
+   url="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r65727.tar.bz2";
+   advertisedUrl="http://builds.nightly.webkit.org/files/trunk/src/WebKit-r65727.tar.bz2";
   
   
 }
