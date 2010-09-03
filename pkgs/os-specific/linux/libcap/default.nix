9a0adf069fcad9a5c3b4c95408cbec286a8fce6d
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-09-03 12:11:33 +0000

    Updating libcap (i686-linux kde programs failed because libcap did not build there)
    svn path=/nixpkgs/branches/stdenv-updates/; revision=23615

--- pkgs/os-specific/linux/libcap/default.nix
+++ pkgs/os-specific/linux/libcap/default.nix
@@ -1,15 +1,16 @@
-{stdenv, fetchurl, attr}:
+{stdenv, fetchurl, attr, perl}:
 
 assert stdenv.isLinux;
 
 stdenv.mkDerivation {
-  name = "libcap-2.09";
+  name = "libcap-2.19";
   
   src = fetchurl {
-    url = mirror://kernel/linux/libs/security/linux-privs/kernel-2.6/libcap-2.09.tar.bz2;
-    sha256 = "0sq15y8yfm7knf6jhqcycb9wz52n3r1sriii66xk0djvd4hw69jr";
+    url = mirror://kernel/linux/libs/security/linux-privs/kernel-2.6/libcap-2.19.tar.gz;
+    sha256 = "0fdsz9j741npvh222f8p1y6l516z9liibiwdpdr3a4zg53m0pw45";
   };
   
+  buildNativeInputs = [perl];
   buildInputs = [attr];
 
   preBuild = ''
