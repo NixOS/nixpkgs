da11152e6e5960126b605dd9b60ece9e9ec5649c
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-09-05 06:00:26 +0000

    Updating 'icu' so it builds on mips too
    svn path=/nixpkgs/branches/stdenv-updates/; revision=23641

--- pkgs/development/libraries/icu/default.nix
+++ pkgs/development/libraries/icu/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation {
-  name = "icu4c-4.2";
+  name = "icu4c-4.5.1";
   
   src = fetchurl {
-    url = http://download.icu-project.org/files/icu4c/4.2.1/icu4c-4_2_1-src.tgz;
-    sha256 = "0qw050msb34wr522s7s83i6skxsc9i19p4rlvmf99pqk2hgf6kc1";
+    url = http://download.icu-project.org/files/icu4c/4.5.1/icu4c-4_5_1-src.tgz;
+    sha256 = "1cbjwz99rqy6r3rb3022qlcrfvncvgigpb7n9824jadz9m17lmfm";
   };
 
   patchFlags = "-p0";
@@ -13,13 +13,6 @@ stdenv.mkDerivation {
   CFLAGS = "-O0";
   CXXFLAGS = "-O0";
 
-  patches = [
-    (fetchurl {
-      url = "http://sources.gentoo.org/viewcvs.py/*checkout*/gentoo-x86/dev-libs/icu/files/icu-3.8-setBreakType-public.diff?rev=1.1";
-      sha256 = "09g39rzj3bdf2q9n47rzdlpcjyipip42swbjpb0gjzp439jv3wmk";
-    })
-  ];
-  
   postUnpack = "
     sourceRoot=\${sourceRoot}/source
     echo Source root reset to \${sourceRoot}
