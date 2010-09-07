d460ffc55493adb8a6907e27c1cee7ff5b2f3e3b
Ludovic Courtès <ludo@gnu.org>
2010-09-07 08:02:36 +0000

    uucp: Fetch from mirror://gnu; add `meta'.
    svn path=/nixpkgs/trunk/; revision=23661

--- pkgs/tools/misc/uucp/default.nix
+++ pkgs/tools/misc/uucp/default.nix
@@ -1,14 +1,30 @@
 { stdenv, fetchurl }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "uucp-1.07";
 
   src = fetchurl {
-    url = http://ftp.de.debian.org/debian/pool/main/u/uucp/uucp_1.07.orig.tar.gz;
+    url = "mirror://gnu/uucp/${name}.tar.gz";
     sha256 = "0b5nhl9vvif1w3wdipjsk8ckw49jj1w85xw1mmqi3zbcpazia306";
   };
 
+  doCheck = true;
+
   meta = {
     description = "Unix-unix cp over serial line, also includes cu program";
+
+    longDescription =
+      '' Taylor UUCP is a free implementation of UUCP and is the standard
+         UUCP used on the GNU system.  If you don't know what UUCP is chances
+         are, nowadays, that you won't need it.  If you do need it, you've
+         just found one of the finest UUCP implementations available.
+      '';
+
+    homepage = http://www.gnu.org/software/uucp/uucp.html;
+
+    license = "GPLv2+";
+
+    platforms = stdenv.lib.platforms.all;
+    maintainers = [ stdenv.lib.maintainers.ludo ];
   };
 }
