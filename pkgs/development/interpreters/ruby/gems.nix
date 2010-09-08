17c70d3efdf63c7ca70286b2cdce72846f4ddce1
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-09-08 09:56:53 +0000

    Mass rubygems update
    * Upgrade to rubygems-1.3.7
    * Upgrade all gems
    * Add rails3
    
    svn path=/nixpkgs/trunk/; revision=23682

--- pkgs/development/interpreters/ruby/gems.nix
+++ pkgs/development/interpreters/ruby/gems.nix
@@ -4,10 +4,10 @@ rec {
   # some packages (eg ruby-debug) still require 1.8. So let's stick to that for
   # now if nobody has different requirements
 
-  version = "1.3.5";
+  version = "1.3.7";
   src = fetchurl {
-    url = "http://rubyforge.org/frs/download.php/60718/rubygems-1.3.5.tgz";
-    sha256 = "1b26fn9kmyd6394m1gqppi10xyf1hac85lvsynsxzpjlmv0qr4n0";
+    url = "http://production.cf.rubygems.org/rubygems/${name}.tgz";
+    sha256 = "17bwlqxqrjrial111rn395yjx9wyxrmvmj0hgd85bxkkcap912rq";
   };
 
 
