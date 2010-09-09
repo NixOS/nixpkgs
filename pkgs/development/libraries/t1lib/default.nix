3bb0a18f0c76570da44b7c5c2c37e598cba84ada
Peter Simons <simons@cryp.to>
2010-09-09 09:43:33 +0000

    pkgs/development/libraries/t1lib: updated to version 5.1.2
    svn path=/nixpkgs/trunk/; revision=23693

--- pkgs/development/libraries/t1lib/default.nix
+++ pkgs/development/libraries/t1lib/default.nix
@@ -1,11 +1,13 @@
 {stdenv, fetchurl, x11, libXaw, libXpm}:
 
 stdenv.mkDerivation {
-  name = "t1lib-5.1.0";
+  name = "t1lib-5.1.2";
+
   src = fetchurl {
-    url = ftp://ftp.nluug.nl/pub/metalab/libs/graphics/t1lib-5.1.1.tar.gz;
-    sha256 = "0r1wb94kjd8jwym9na2k2snikizrnyc4cf4mf92v89r4yy0apph8";
+    url = "ftp://ftp.nluug.nl/pub/metalab/libs/graphics/t1lib-5.1.2.tar.gz";
+    sha256 = "0nbvjpnmcznib1nlgg8xckrmsw3haa154byds2h90y2g0nsjh4w2";
   };
+
   buildInputs = [x11 libXaw libXpm];
   buildFlags = "without_doc";
 }
