6fd70d9c4749f8a3526c5c4e1764a54b416d5de8
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-09-09 17:47:37 +0000

    Updating xfsprogs and making it build.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=23704

--- pkgs/tools/filesystems/xfsprogs/default.nix
+++ pkgs/tools/filesystems/xfsprogs/default.nix
@@ -1,16 +1,20 @@
 { stdenv, fetchurl, libtool, gettext, libuuid }:
 
 stdenv.mkDerivation rec {
-  name = "xfsprogs-3.0.3";
+  name = "xfsprogs-3.1.3";
 
   src = fetchurl {
     url = "ftp://oss.sgi.com/projects/xfs/previous/${name}.tar.gz";
-    sha256 = "158ddibsnwcihfvskwc9rknd28p81jk8z463slafp1gf355kmcsq";
+    sha256 = "1mazg6sl4gbm204ndgw585xvcsxg3hg22d989ww6lgmycp635l7s";
   };
 
   buildInputs = [ libtool gettext libuuid ];
 
   configureFlags = "MAKE=make MSGFMT=msgfmt MSGMERGE=msgmerge XGETTEXT=xgettext ZIP=gzip AWK=gawk --disable-shared";
+  preConfigure = ''
+    configureFlags="$configureFlags root_sbindir=$out/sbin root_libdir=$out/lib"
+  '';
+  disableStatic = false;
 
   meta = {
     description = "SGI XFS utilities";
