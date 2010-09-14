6236de33808690dd0afb44b326d980ba59f95961
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-09-14 08:45:15 +0000

    Blindly commiting a fix for dmidecode to build with gnumake 3.82
    Thank you niksnut.
    
    svn path=/nixpkgs/branches/stdenv-updates/; revision=23777

--- pkgs/os-specific/linux/dmidecode/default.nix
+++ pkgs/os-specific/linux/dmidecode/default.nix
@@ -8,6 +8,12 @@ stdenv.mkDerivation rec {
     sha256 = "1h72r5scrpvgw60hif5msjg6vw2x0jd6z094lhbh6cjk6gls6x2d";
   };
 
+  # Taken from gentoo, to build with gnumake 3.82
+  # http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/sys-apps/dmidecode/dmidecode-2.10.ebuild?r1=1.5&r2=1.6
+  patchPhase = ''
+    sed -i -e '/^PROGRAMS !=/d' Makefile
+  '';
+
   makeFlags = "prefix=$(out)";
 
   meta = {
