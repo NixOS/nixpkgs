4163c5c98a1b8e80517abb53cc00a74feb8b89ca
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-09-14 08:40:05 +0000

    Trying to fix the urls for xfsprogs
    svn path=/nixpkgs/branches/stdenv-updates/; revision=23776

--- pkgs/tools/filesystems/xfsprogs/default.nix
+++ pkgs/tools/filesystems/xfsprogs/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "xfsprogs-3.1.3";
 
   src = fetchurl {
-    url = "ftp://oss.sgi.com/projects/xfs/previous/${name}.tar.gz";
+    urls = [ "ftp://oss.sgi.com/projects/xfs/cmd_tars/${name}.tar.gz" "ftp://oss.sgi.com/projects/xfs/previous/${name}.tar.gz" ];
     sha256 = "1mazg6sl4gbm204ndgw585xvcsxg3hg22d989ww6lgmycp635l7s";
   };
 
