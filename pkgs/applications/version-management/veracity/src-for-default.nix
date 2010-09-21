9effe4ad771639789d9328b39b29a29b08900d83
Michael Raskin <7c6f434c@mail.ru>
2010-09-21 20:25:59 +0000

    Update Veracity nightly
    svn path=/nixpkgs/trunk/; revision=23885

--- pkgs/applications/version-management/veracity/src-for-default.nix
+++ pkgs/applications/version-management/veracity/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="0.3.1.10286";
-   name="veracity-0.3.1.10286";
-   hash="0cihzf2m4bxwf8z1va550q8yqpjwc2rs013rx0q8azl0idx0akah";
+   version="0.3.1.10290";
+   name="veracity-0.3.1.10290";
+   hash="15j708yxzgyzvnydy0ab9h68caw95yls6f1a0qndfw4givgkjw05";
    url="http://download-us.sourcegear.com/Veracity/nightly/veracity-source-${version}.tar.gz";
-   advertisedUrl="http://download-us.sourcegear.com/Veracity/nightly/veracity-source-0.3.1.10286.tar.gz";
+   advertisedUrl="http://download-us.sourcegear.com/Veracity/nightly/veracity-source-0.3.1.10290.tar.gz";
   
   
 }
