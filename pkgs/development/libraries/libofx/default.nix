f64ba49369598fb72c5aa0a1d6fb5b4bcea57ef1
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-09-26 19:40:41 +0000

    metadata
    svn path=/nixpkgs/trunk/; revision=23958

--- pkgs/development/libraries/libofx/default.nix
+++ pkgs/development/libraries/libofx/default.nix
@@ -17,6 +17,8 @@ stdenv.mkDerivation rec {
     description = "Opensource implementation of the Open Financial eXchange specification";
     homepage = http://libofx.sourceforge.net/;
     license = "LGPL";
+    platforms = stdenv.lib.platforms.all;
+    maintainers = [ stdenv.lib.maintainers.urkud ];
   };
 }
 
