1171bb1dcaa8c57bc983093ccef7acc770e64ba2
Peter Simons <simons@cryp.to>
2010-10-05 13:16:53 +0000

    pkgs/development/libraries/pcre: updated to version 8.10
    svn path=/nixpkgs/branches/stdenv-updates/; revision=24070

--- pkgs/development/libraries/pcre/default.nix
+++ pkgs/development/libraries/pcre/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, unicodeSupport ? false, cplusplusSupport ? true}:
 
 stdenv.mkDerivation {
-  name = "pcre-8.02";
+  name = "pcre-8.10";
 
   src = fetchurl {
-    url = mirror://sourceforge/pcre/pcre-8.02.tar.bz2;
-    sha256 = "1gafmkmkbpdqjbdl85q2z5774gw4gfqjf238icz7gqf3v4v90xd4";
+    url = mirror://sourceforge/pcre/pcre-8.10.tar.bz2;
+    sha256 = "7ac4e016f6bad8c7d990e6de9bce58c04ff5dd8838be0c5ada0afad1d6a07480";
   };
 
   configureFlags = ''
@@ -14,7 +14,19 @@ stdenv.mkDerivation {
   '';
 
   meta = {
-    homepage = http://www.pcre.org/;
+    homepage = "http://www.pcre.org/";
     description = "A library for Perl Compatible Regular Expressions";
+    license = "BSD-3";
+
+    longDescription = ''
+      The PCRE library is a set of functions that implement regular
+      expression pattern matching using the same syntax and semantics as
+      Perl 5. PCRE has its own native API, as well as a set of wrapper
+      functions that correspond to the POSIX regular expression API. The
+      PCRE library is free, even for building proprietary software.
+    '';
+
+    platforms = stdenv.lib.platforms.all;
+    maintainers = [ stdenv.lib.maintainers.simons ];
   };
 }
