5d962bc1b2a47251316f79d9a10b6aae62717b7a
Michael Raskin <7c6f434c@mail.ru>
2010-10-06 08:49:35 +0000

    Update SBCL
    svn path=/nixpkgs/trunk/; revision=24106

--- pkgs/development/compilers/sbcl/src-for-default.nix
+++ pkgs/development/compilers/sbcl/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="1.0.42";
-   name="sbcl-1.0.42";
-   hash="0j699rb3nw7akcmpqjlzwsrc0yg6kj6nf9bzfhnw19q1m4kl4pm5";
-   url="http://downloads.sourceforge.net/project/sbcl/sbcl/1.0.42/sbcl-1.0.42-source.tar.bz2";
-   advertisedUrl="http://downloads.sourceforge.net/project/sbcl/sbcl/1.0.42/sbcl-1.0.42-source.tar.bz2";
+   version="1.0.43";
+   name="sbcl-1.0.43";
+   hash="0csykjcmy0gjlvxx7pg7kd2b7h610zpys3xla85qjj9ngjzpmbcq";
+   url="http://downloads.sourceforge.net/project/sbcl/sbcl/1.0.43/sbcl-1.0.43-source.tar.bz2";
+   advertisedUrl="http://downloads.sourceforge.net/project/sbcl/sbcl/1.0.43/sbcl-1.0.43-source.tar.bz2";
   
   
 }
