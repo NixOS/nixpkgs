4e9457501420fb9d5a0dec0d87a6e4cec8ed4fcf
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-10-06 16:02:44 +0000

    * NTP updated to 4.2.6p2.
    svn path=/nixpkgs/trunk/; revision=24118

--- pkgs/tools/networking/ntp/default.nix
+++ pkgs/tools/networking/ntp/default.nix
@@ -3,11 +3,11 @@
 assert stdenv.isLinux -> libcap != null;
  
 stdenv.mkDerivation rec {
-  name = "ntp-4.2.6";
+  name = "ntp-4.2.6p2";
   
   src = fetchurl {
     url = "http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/${name}.tar.gz";
-    sha256 = "04cam5l804ws6cs0ihlmx26n39vpap5wb39i1vxfff01mri4q38b";
+    sha256 = "1n79scfvgjk8hn1fr4q2kkk6xm83k68r4p488ai09nm20dwqp2a2";
   };
   
   configureFlags = ''
