113075207e09d4e1e919f3b84b29940f2a936603
Peter Simons <simons@cryp.to>
2010-10-07 13:14:21 +0000

    pkgs/tools/networking/x11-ssh-askpass: strip trailing whitespace
    svn path=/nixpkgs/trunk/; revision=24141

--- pkgs/tools/networking/x11-ssh-askpass/default.nix
+++ pkgs/tools/networking/x11-ssh-askpass/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation {
   name = "x11-ssh-askpass-1.2.4.1";
- 
+
   src = fetchurl {
     url = http://www.jmknoble.net/software/x11-ssh-askpass/x11-ssh-askpass-1.2.4.1.tar.gz;
     sha1 = "78c992951685d4dbffb77536f37b83ae2a6eafc7";
