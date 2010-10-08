d61a0a42ca2a1e2021d0e2ef8bb6f21912b09a74
Michael Raskin <7c6f434c@mail.ru>
2010-10-08 05:33:59 +0000

    Update wine
    svn path=/nixpkgs/trunk/; revision=24166

--- pkgs/misc/emulators/wine/src-for-default.nix
+++ pkgs/misc/emulators/wine/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="1.3.3";
-   name="wine-1.3.3";
-   hash="1kpwcm3zqwxzwm9npipq80yfp7la6429vpg6sf4lvxc1z316wns5";
+   version="1.3.4";
+   name="wine-1.3.4";
+   hash="1p2zh9c3k0b2ryy8cdldcz6x7mdg0c2gkvj0hxa3bi9cczsqg4s4";
    url="http://prdownloads.sourceforge.net/wine/wine-${version}.tar.bz2";
-   advertisedUrl="http://prdownloads.sourceforge.net/wine/wine-1.3.3.tar.bz2";
+   advertisedUrl="http://prdownloads.sourceforge.net/wine/wine-1.3.4.tar.bz2";
   
   
 }
