48229f2b0d79cb30fa0cebeb80dfdab0bdd781dd
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-10-11 19:30:54 +0000

    * dnsmasq updated to 2.55.
    svn path=/nixpkgs/trunk/; revision=24222

--- pkgs/tools/networking/dnsmasq/default.nix
+++ pkgs/tools/networking/dnsmasq/default.nix
@@ -1,17 +1,17 @@
 { stdenv, fetchurl }:
 
-stdenv.mkDerivation {
-  name = "dnsmasq-2.40";
+stdenv.mkDerivation rec {
+  name = "dnsmasq-2.55";
 
   src = fetchurl {
-    url = http://www.thekelleys.org.uk/dnsmasq/dnsmasq-2.40.tar.gz;
-    sha256 = "1q346l403rvvmvr14fk2l201p8fl3p5417vkp95zlx00jdb7hl8n";
+    url = "http://www.thekelleys.org.uk/dnsmasq/${name}.tar.gz";
+    sha256 = "0agrz7lvqdvh7ps173nr5yl00dblv2lpd0x9pm64f03zjzsyqqyg";
   };
 
-  installPhase = "ensureDir \$out/bin; make DESTDIR= BINDIR=\$out/bin MANDIR=\$out/man LOCALEDIR=\$out/share/locale install"; 
+  makeFlags = "DESTDIR= BINDIR=$(out)/bin MANDIR=$(out)/man LOCALEDIR=$(out)/share/locale";
 
   meta = { 
-    description = "DNS forwarder and DHCP server";
+    description = "An integrated DNS, DHCP and TFTP server for small networks";
     homepage = http://www.thekelleys.org.uk/dnsmasq/doc.html;
     license = "GPL";
   };
