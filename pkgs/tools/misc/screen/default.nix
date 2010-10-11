143a5749a3440f6e1b8add732fc0740c734687ad
Ludovic Courtès <ludo@gnu.org>
2010-10-11 22:11:54 +0000

    GNU Screen: Add the built-in telnet client; add meta-data.
    svn path=/nixpkgs/trunk/; revision=24228

--- pkgs/tools/misc/screen/default.nix
+++ pkgs/tools/misc/screen/default.nix
@@ -1,19 +1,47 @@
 {stdenv, fetchurl, ncurses}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "screen-4.0.3";
+
   src = fetchurl {
-    url = mirror://gnu/screen/screen-4.0.3.tar.gz;
+    url = "mirror://gnu/screen/${name}.tar.gz";
     sha256 = "0xvckv1ia5pjxk7fs4za6gz2njwmfd54sc464n8ab13096qxbw3q";
   };
 
-  buildInputs = [ncurses];
+  configureFlags = [ "--enable-telnet" ];
+  buildInputs = [ ncurses ];
+
+  doCheck = true;
 
   meta = {
-    homepage = "http://www.gnu.org/software/screen/";
-    description = "Full-screen window manager that multiplexes a physical terminal";
+    homepage = http://www.gnu.org/software/screen/;
+    description = "GNU Screen, a window manager that multiplexes a physical terminal";
+
+    longDescription =
+      '' GNU Screen is a full-screen window manager that multiplexes a physical
+         terminal between several processes, typically interactive shells.
+         Each virtual terminal provides the functions of the DEC VT100
+         terminal and, in addition, several control functions from the ANSI
+         X3.64 (ISO 6429) and ISO 2022 standards (e.g., insert/delete line
+         and support for multiple character sets).  There is a scrollback
+         history buffer for each virtual terminal and a copy-and-paste
+         mechanism that allows the user to move text regions between windows.
+         When screen is called, it creates a single window with a shell in it
+         (or the specified command) and then gets out of your way so that you
+         can use the program as you normally would.  Then, at any time, you
+         can create new (full-screen) windows with other programs in them
+         (including more shells), kill the current window, view a list of the
+         active windows, turn output logging on and off, copy text between
+         windows, view the scrollback history, switch between windows, etc.
+         All windows run their programs completely independent of each other.
+         Programs continue to run when their window is currently not visible
+         and even when the whole screen session is detached from the users
+         terminal.
+      '';
+
+    license = stdenv.lib.licenses.gpl2Plus;
 
     platforms = stdenv.lib.platforms.unix;
-    maintainers = [];
+    maintainers = [ stdenv.lib.maintainers.ludo ];
   };
 }
