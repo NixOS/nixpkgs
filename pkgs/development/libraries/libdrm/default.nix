40c9c602111d7e7e608c08884fcfdf8b9c74156d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-10-12 22:12:54 +0000

    * libdrm updated to 2.4.22.
    svn path=/nixpkgs/trunk/; revision=24256

--- pkgs/development/libraries/libdrm/default.nix
+++ pkgs/development/libraries/libdrm/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, pkgconfig, libpthreadstubs}:
 
 stdenv.mkDerivation rec {
-  name = "libdrm-2.4.21";
+  name = "libdrm-2.4.22";
   
   src = fetchurl {
     url = "http://dri.freedesktop.org/libdrm/${name}.tar.bz2";
-    sha256 = "0jx1idd2ycglx81b0nlawh07lgi8k5h9854c9apdxcdnlcmn26sf";
+    sha256 = "0gbb7i282i6gf2wzbzkcz5j662v4ixpfjf0gv0090k89wjafbc0b";
   };
 
   buildInputs = [ pkgconfig libpthreadstubs ];
