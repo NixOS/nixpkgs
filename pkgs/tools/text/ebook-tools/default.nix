f0390804510b27f5579b3a53e8888f039cf2722d
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-10-12 07:49:13 +0000

    ebook-tools-0.2.1
    svn path=/nixpkgs/trunk/; revision=24233

--- pkgs/tools/text/ebook-tools/default.nix
+++ pkgs/tools/text/ebook-tools/default.nix
@@ -5,11 +5,11 @@ let
 in
 
 stdenv.mkDerivation rec {
-  name = "${pn}-0.2.0";
+  name = "${pn}-0.2.1";
 
   src = fetchurl {
     url = "mirror://sf/${pn}/${name}.tar.gz";
-    sha256 = "18jb6v20pzk0kxv2dgjqgzzrcg7qid569apr63phfq1as1h09x67";
+    sha256 = "0wgwdsd3jwwfg36jyr5j0wayqjli3ia80lxzk10byd4cmkywnhy2";
   };
 
   buildInputs = [ cmake libxml2 libzip ];
