e82053ac947797f0bf49481a1acfc9aaa712b09c
Michael Raskin <7c6f434c@mail.ru>
2010-10-13 15:48:37 +0000

    Fix XawTV build
    svn path=/nixpkgs/trunk/; revision=24267

--- pkgs/applications/video/xawtv/default.nix
+++ pkgs/applications/video/xawtv/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, ncurses, libjpeg, libX11, libXt, libXft, xproto, libFS, fontsproto, libXaw, libXpm, libXext, libSM, libICE, perl, xextproto}:
+{stdenv, fetchurl, ncurses, libjpeg, libX11, libXt, libXft, xproto, libFS, fontsproto, libXaw, libXpm, libXext, libSM, libICE, perl, xextproto, linux}:
 
 stdenv.mkDerivation {
   name = "xawtv-3.95";
@@ -6,7 +6,10 @@ stdenv.mkDerivation {
     url = http://dl.bytesex.org/releases/xawtv/xawtv-3.95.tar.gz;
     md5 = "ad25e03f7e128b318e392cb09f52207d";
   };
+  preConfigure = ''
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I$(echo ${linux}/lib/modules/*/build/include)"
+  '';
   buildInputs = [ncurses libjpeg libX11 libXt libXft xproto libFS fontsproto libXaw libXpm libXext libSM libICE perl xextproto];
-  patches = [./xawtv-3.95-libfs.patch ./xawtv-3.95-makefile.patch];
+  patches = [./xawtv-3.95-libfs.patch ./xawtv-3.95-makefile.patch ./xawtv-3.95-page-mask.patch ];
   
 }
