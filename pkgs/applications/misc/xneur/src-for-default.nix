686802b8e1dc0c894386329eb7074047190026e9
Michael Raskin <7c6f434c@mail.ru>
2010-10-14 11:49:07 +0000

    Update XNeur
    svn path=/nixpkgs/trunk/; revision=24275

--- pkgs/applications/misc/xneur/src-for-default.nix
+++ pkgs/applications/misc/xneur/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="0.9.9";
-   name="xneur-0.9.9";
-   hash="07ircm7ngc84h6fpy8q01444vy9cm08q4mbl3q3ya0dg00cq64z4";
+   version="0.10.0";
+   name="xneur-0.10.0";
+   hash="1jh40awnc5bvdj07jv99ymiln8nzr5ac5ahz9v4w0m560b7qq48f";
    url="http://dists.xneur.ru/release-${version}/tgz/xneur-${version}.tar.bz2";
-   advertisedUrl="http://dists.xneur.ru/release-0.9.9/tgz/xneur-0.9.9.tar.bz2";
+   advertisedUrl="http://dists.xneur.ru/release-0.10.0/tgz/xneur-0.10.0.tar.bz2";
   
   
 }
