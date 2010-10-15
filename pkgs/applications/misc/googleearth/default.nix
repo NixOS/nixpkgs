ebbbb77e6c4ebc3d5feb838b3e930512c70ac998
Evgeny Egorochkin <phreedom.stdin@gmail.com>
2010-10-15 20:06:28 +0000

    Google-earth: version bump
    svn path=/nixpkgs/trunk/; revision=24308

--- pkgs/applications/misc/googleearth/default.nix
+++ pkgs/applications/misc/googleearth/default.nix
@@ -6,11 +6,11 @@ zlib }:
 assert stdenv.system == "i686-linux";
 
 stdenv.mkDerivation {
-  name = "googleearth-5.1.3535.3218";
+  name = "googleearth-5.2.0001";
 
   src = fetchurl {
     url = http://dl.google.com/earth/client/current/GoogleEarthLinux.bin;
-    sha256 = "f721e4e8db3a7351c77a8aea425ec334ff01e163481cbcf6cdda9dbb0ad422ac";
+    sha256 = "2e6fcbd2384446e2a6eed8ca23173e32c5f3f9ae4d1168e2e348c3924fd2bf30";
   };
 
   buildNativeInputs = [
