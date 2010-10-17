7e03aecb57ecf929c5a458ba79bc220d48caf448
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-10-17 18:43:31 +0000

    Add kde-4.5.2 to manifest
    svn path=/nixpkgs/trunk/; revision=24327

--- pkgs/desktops/kde-4.5/kde-package/manifest.nix
+++ pkgs/desktops/kde-4.5/kde-package/manifest.nix
@@ -99,4 +99,85 @@ builtins.listToAttrs
 {
   name = "kdepim-runtime-4.4.6.tar.bz2";
   value = "1y0xxwsq9wlwbkd661i4f1iqvpzg3sbi3lq9dir45fjx406chgmd";
-}]
+}
+{
+  name = "kdeaccessibility-4.5.2.tar.bz2";
+  value = "1iv94vvngxs94vl3ssjq1y2sd50wcc7s7gvngwqj534qjd1fj3a2";
+}
+{
+  name = "kdeadmin-4.5.2.tar.bz2";
+  value = "1gy0p9vfx8k1ni92n6p8fhq32i7dazs2vls7f5na8hc6s5wsn861";
+}
+{
+  name = "kdeartwork-4.5.2.tar.bz2";
+  value = "0y7rj70x861a07r80skldzkkbqhh41gsv2vh8cg1n4hqq4l5nj5y";
+}
+{
+  name = "kdebase-4.5.2.tar.bz2";
+  value = "1hwcd3k25bhi73hps4rqc3rh87sdf8syvv0jx4prc6qd2cpjxvp6";
+}
+{
+  name = "kdebase-runtime-4.5.2.tar.bz2";
+  value = "04dbjhkl1zfys6rhjrk3dmqqwzw4hh0hyrm6rn8f1gbiqfcgrkfy";
+}
+{
+  name = "kdebase-workspace-4.5.2.tar.bz2";
+  value = "17jd2ixvsk9f0z30ksmjaasdfza39c80d0ylzkpdjf9nkiiv1pn8";
+}
+{
+  name = "kdebindings-4.5.2.tar.bz2";
+  value = "077n2g4mmfkg7xvz7v8f0m79sp2f72f56ari14j4x073vgf51hfs";
+}
+{
+  name = "kdeedu-4.5.2.tar.bz2";
+  value = "1ljp5jm5iml2s1jdi1n5h8hzqsl7pq98vcdj9bi1pjq3l87gh4i5";
+}
+{
+  name = "kdegames-4.5.2.tar.bz2";
+  value = "0vld69dz2src7ya92a5nqi0739whldxvgphyxm9bavk69x6g129c";
+}
+{
+  name = "kdegraphics-4.5.2.tar.bz2";
+  value = "0zl1df2srnf7xsivyws3zicrj6bginjn6rgi0iq6hx3q9l5lah2d";
+}
+{
+  name = "kdelibs-4.5.2.tar.bz2";
+  value = "1hxlqvcwys1snhahrv1lryv95sh042jikscqmm6vind03pcqrq09";
+}
+{
+  name = "kdemultimedia-4.5.2.tar.bz2";
+  value = "1gxmal28m83r9imasv3nw4c091kllpkivfws8aal63r0dg5n5fy2";
+}
+{
+  name = "kdenetwork-4.5.2.tar.bz2";
+  value = "0k59zf8m34b7xp4h9mj328gjnr73l2d7vf4b4aikv0m2damryp7a";
+}
+{
+  name = "kdepimlibs-4.5.2.tar.bz2";
+  value = "0bag9jhr1azadcz01rfdhdl8nfk6w4cjw3w2zxardar5ri129v2b";
+}
+{
+  name = "kdeplasma-addons-4.5.2.tar.bz2";
+  value = "1pdlwbadk3qyh9rxj4j2b2wjfn7dmi9jcargahc7p7pb8mf0pizw";
+}
+{
+  name = "kdesdk-4.5.2.tar.bz2";
+  value = "0ii7gmshw4207i0hlzz3fl5kc21i4vx2v29jw10ri1vz7pmv9v9q";
+}
+{
+  name = "kdetoys-4.5.2.tar.bz2";
+  value = "01kn14lh00ig33r98xjly37ad7w1cwvc4l02hswbqr99smksxn33";
+}
+{
+  name = "kdeutils-4.5.2.tar.bz2";
+  value = "1rh4s60wx253adw7z1j0sp29l43y7xvd2516bwxq0sn8bmp3w7nr";
+}
+{
+  name = "kdewebdev-4.5.2.tar.bz2";
+  value = "02baf8h4v688x03hxr8kliglia1chlarnmzc0gbpnpry9zhwpp5p";
+}
+{
+  name = "oxygen-icons-4.5.2.tar.bz2";
+  value = "0c8hy3vsqpm6n52v6w3igj78s0qy6cb3m6fm8qh795rpw495d5yj";
+}
+]
