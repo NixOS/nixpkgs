8846577c78d1bd8030bf6b35b43ccfeb87be8662
Ludovic Courtès <ludo@gnu.org>
2010-10-19 08:09:10 +0000

    GNU mtools 4.0.15.
    svn path=/nixpkgs/trunk/; revision=24356

--- pkgs/tools/filesystems/mtools/default.nix
+++ pkgs/tools/filesystems/mtools/default.nix
@@ -1,17 +1,18 @@
-{ stdenv, fetchurl, texinfo }:
+{ stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "mtools-4.0.13";
+  name = "mtools-4.0.15";
 
   src = fetchurl {
     url = "mirror://gnu/mtools/${name}.tar.bz2";
-    sha256 = "1nj7lc2q1g66l3ma8z1c95nglf9himnr6k85b5rry99f9za7npbg";
+    sha256 = "169qjy3fmmhxiy0jljh84jvjh8mh1p8gglwqgjhq7hbw235fy399";
   };
 
-  buildInputs = [ texinfo ];
+  doCheck = true;
 
   meta = {
     homepage = http://www.gnu.org/software/mtools/;
-    description = "Utilities to access MS-DOS disks without mounting them";
+    description = "GNU mtools, utilities to access MS-DOS disks";
+    platforms = stdenv.lib.platforms.gnu;  # arbitrary choice
   };
 }
