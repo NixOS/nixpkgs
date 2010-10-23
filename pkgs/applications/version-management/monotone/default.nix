ba662a84e7c20dbb1aed22e0367ddce486d81432
Michael Raskin <7c6f434c@mail.ru>
2010-10-23 15:28:18 +0000

    Update Monotone
    svn path=/nixpkgs/trunk/; revision=24431

--- pkgs/applications/version-management/monotone/default.nix
+++ pkgs/applications/version-management/monotone/default.nix
@@ -2,13 +2,13 @@
   lua, pcre, sqlite, perl, pkgconfig}:
 
 let 
-  version = "0.48";
+  version = "0.48.1";
 in stdenv.mkDerivation rec {
   name = "monotone-${version}";
   inherit perl;
   src = fetchurl {
     url = "http://monotone.ca/downloads/${version}/monotone-${version}.tar.gz";
-    sha256 = "3149abf0e4433a0e14c5da805a04dbbc45b16086bc267d473b17e933407d839d";
+    sha256 = "e5ab4917866d2c597f99577554549b7c4cda9895be7a5dd0469f1c7798fe3c10";
   };
   buildInputs = [boost zlib botan libidn lua pcre sqlite pkgconfig];
   postInstall = ''
