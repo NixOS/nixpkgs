93129530a973d275666f9401f787ccd7612ba608
Evgeny Egorochkin <phreedom.stdin@gmail.com>
2010-10-26 01:15:42 +0000

    Rake: Fix build. Patch by Ilya Cassina.
    svn path=/nixpkgs/trunk/; revision=24480

--- pkgs/development/ruby-modules/rake/default.nix
+++ pkgs/development/ruby-modules/rake/default.nix
@@ -7,13 +7,16 @@ stdenv.mkDerivation {
     sha256 = "1kggvkkj609hj1xvpadzchki66i7ynz3qq4nc2hmfkf536fx8c03";
   };
   buildInputs = [ruby];
+  patchPhase = ''
+    sed -i install.rb \
+        -e 's/$bindir  = destdir + $bindir/prefix = CONFIG["prefix"];$bindir = $bindir.slice(prefix.length..$bindir.length);$bindir = destdir + $bindir/' \
+        -e 's/$sitedir = destdir + $sitedir/$sitedir = $sitedir.slice(prefix.length..$sitedir.length);$sitedir = destdir + $sitedir/'
+  '';
   buildPhase = "true";
   installPhase = ''
     ensureDir $out/lib
     ensureDir $out/bin
-    ruby setup.rb config --prefix=$out 
-    # --bindir $out/bin --libdir $out/lib
-    ruby setup.rb setup
-    ruby setup.rb install
+    export DESTDIR=$out
+    ruby install.rb
   '';
 }
