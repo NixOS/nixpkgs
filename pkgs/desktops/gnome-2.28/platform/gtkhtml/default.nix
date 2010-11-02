7ce8c4b77ea6897cff6be8e43d16bc5efee1106d
Piotr Pietraszkiewicz <ppietrasa@googlemail.com>
2010-11-02 20:09:15 +0000

    fixed compilation error, debian bug #577368 (implicit decl. of function 'GTK_WIDGET_SENSITIVE'), by bumping gtkhtml to version 3.29.92.1
    svn path=/nixpkgs/trunk/; revision=24581

--- pkgs/desktops/gnome-2.28/platform/gtkhtml/default.nix
+++ pkgs/desktops/gnome-2.28/platform/gtkhtml/default.nix
@@ -2,11 +2,11 @@
 GConf, enchant, isocodes, gnome_icon_theme }:
 
 stdenv.mkDerivation rec {
-  name = "gtkhtml-3.29.5";
+  name = "gtkhtml-3.29.92.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gtkhtml/3.29/${name}.tar.bz2";
-    sha256 = "0abd91isqbriq9nclq14275v2xd0r9vrr3sxhxwxxp02m8gskwvd";
+    sha256 = "a34fe24af0f591db95010475c21a461985ef4479b2e91602bc745a9accfeef77";
   };
 
   buildInputs = [pkgconfig gtk intltool GConf enchant isocodes gnome_icon_theme ];
