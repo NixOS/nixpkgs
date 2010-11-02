5e5ce5a987bd044459c19b393884a348e9d1f0b3
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-11-02 13:24:08 +0000

    Updating kdesvn. It still does not build to some docbook problems I don't know how to solve, related to kde 4.5 I guess.
    It runs some /nix/store/wlbri8r6fzavvq554bm04666aphhkxxr-kdelibs-4.5.2/bin/meinproc4 --check --cache /tmp/nix-build-rjcm9mri08cpbvh7gcjcvra2iqzn20ch-kdesvn-1.5.5.drv-0/kdesvn-1.5.5/build/doc/nl/index.cache.bz2 /tmp/nix-build-rjcm9mri08cpbvh7gcjcvra2iqzn20ch-kdesvn-1.5.5.drv-0/kdesvn-1.5.5/doc/nl/index.docbook
    
    and this fails.
    
    svn path=/nixpkgs/trunk/; revision=24569

--- pkgs/applications/version-management/kdesvn/default.nix
+++ pkgs/applications/version-management/kdesvn/default.nix
@@ -1,15 +1,21 @@
 { stdenv, fetchurl, lib, cmake, qt4, perl, gettext, apr, aprutil, subversion, db4
-, kdelibs, automoc4, phonon}:
+, kdelibs, automoc4, phonon, kdebase}:
 
 stdenv.mkDerivation {
-  name = "kdesvn-1.5.2";
+  name = "kdesvn-1.5.5";
   src = fetchurl {
-    url = http://kdesvn.alwins-world.de/downloads/kdesvn-1.5.2.tar.bz2;
-    sha256 = "1svblxi70ks816zj1w4cc87x72b628g7xjx4hvx57zw8d9hr463h";
+    url = http://kdesvn.alwins-world.de/downloads/kdesvn-1.5.5.tar.bz2;
+    sha256 = "02sb34p04dyd88ksxvpiffhxqwmhs3yv1wif9m8w0fly9hvy1zk7";
   };
-  builder = ./builder.sh;
-  inherit subversion;
+
+  patchPhase = ''
+    sed -i -e "s|/usr|${subversion}|g" src/svnqt/cmakemodules/FindSubversion.cmake
+  '';
+
+  makeFlags = [ "VERBOSE=1" ];
+
   buildInputs = [ cmake qt4 perl gettext apr aprutil subversion db4 kdelibs automoc4 phonon ];
+
   meta = {
     description = "KDE SVN front-end";
     license = "GPL";
