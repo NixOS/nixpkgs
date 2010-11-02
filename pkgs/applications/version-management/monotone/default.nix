24d011bfbc04757446025a1dbc29a893e936eba8
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-11-02 11:20:24 +0000

    monotone-0.99.1, less hacky way to get perl version
    svn path=/nixpkgs/trunk/; revision=24565

--- pkgs/applications/version-management/monotone/default.nix
+++ pkgs/applications/version-management/monotone/default.nix
@@ -2,22 +2,24 @@
   lua, pcre, sqlite, perl, pkgconfig}:
 
 let 
-  version = "0.99";
+  version = "0.99.1";
+  perlVersion = builtins.substring 5 10 perl.name;
 in stdenv.mkDerivation rec {
   name = "monotone-${version}";
-  inherit perl;
   src = fetchurl {
     url = "http://monotone.ca/downloads/${version}/monotone-${version}.tar.gz";
-    sha256 = "fa677f09169afb71452598ce92ea376fe06037d17bfe650fb6aed17cead11453";
+    sha256 = "189h5f6gqd4ng0qmzi3xwnj17nnpxm2vzras216ar6b5yc9bnki0";
   };
   buildInputs = [boost zlib botan libidn lua pcre sqlite pkgconfig];
   postInstall = ''
     ensureDir $out/share/${name}
     cp -rv contrib/ $out/share/${name}/contrib
-    ensureDir $out/lib/perl5/site_perl/''${perl##*-perl-}
-    cp -v contrib/Monotone.pm $out/lib/perl5/site_perl/''${perl##*-perl-}
+    ensureDir $out/lib/perl5/site_perl/${perlVersion}
+    cp -v contrib/Monotone.pm $out/lib/perl5/site_perl/${perlVersion}
   '';
   meta = {
+    description = "A free distributed version control system";
     maintainers = [stdenv.lib.maintainers.raskin];
+    platforms = stdenv.lib.platforms.all;
   };
 }
