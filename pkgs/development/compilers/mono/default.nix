f4a73e2dddc4532a9d2450dbae50ddc674d8d47a
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-11-07 21:25:28 +0000

    Updating mono from 2.6 to 2.8
    svn path=/nixpkgs/trunk/; revision=24614

--- pkgs/development/compilers/mono/default.nix
+++ pkgs/development/compilers/mono/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, bison, pkgconfig, glib, gettext, perl}:
 
 stdenv.mkDerivation {
-  name = "mono-2.6.4";
+  name = "mono-2.8";
   src = fetchurl {
-    url = http://ftp.novell.com/pub/mono/sources/mono/mono-2.6.4.tar.bz2;
-    sha256 = "08krrl9zdigzxxa10c9scy518301l20xmw6dc2w8s0rvrhxryny0";
+    url = http://ftp.novell.com/pub/mono/sources/mono/mono-2.8.tar.bz2;
+    sha256 = "04bivxg90mmihkp72sjshl4ijbjcbl9f6hdgm476zy794g5rwd78";
   };
 
   buildInputs = [bison pkgconfig glib gettext perl];
@@ -12,9 +12,14 @@ stdenv.mkDerivation {
 
   NIX_LDFLAGS = "-lgcc_s" ;
 
+  # To overcome the bug https://bugzilla.novell.com/show_bug.cgi?id=644723
+  dontDisableStatic = true;
+
   # Attempt to fix this error when running "mcs --version":
   # The file /nix/store/xxx-mono-2.4.2.1/lib/mscorlib.dll is an invalid CIL image
   dontStrip = true;
+
+  enableParallelBuilding = true;
 
   preBuild = "
     makeFlagsArray=(INSTALL=`type -tp install`)
