df8db9a701d6eee1d5c4b16dbad5430a8f371e1c
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-11-23 20:46:45 +0000

    Making the nanonote uboot not start the default console in ttyS0, but in tty0 instead. It's more comfortable for those not having a serial line attached to it.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=24828

--- pkgs/misc/uboot/nanonote.nix
+++ pkgs/misc/uboot/nanonote.nix
@@ -16,7 +16,7 @@ stdenv.mkDerivation {
   srcPatches = fetchgit {
     url = "git://projects.qi-hardware.com/openwrt-xburst.git";
     rev = "3244d5ef9f93802f9b9b6f4405636424abf6fa83";
-    sha256 = "8ae25853028930b418fad90637e0e68f9888285ba3ad1f720be45f08823ee855";
+    sha256 = "99dde9c3c4c66722d3ee2223d306bc84289a8aa77d0b5918ca49c2f4032ab38f";
   };
 
   patchPhase = ''
@@ -25,6 +25,7 @@ stdenv.mkDerivation {
         patch -p1 < $a
     done
     chmod +w -R *
+    sed -i -e 's/console=ttyS0,57600n8//' include/configs/qi_lb60.h
   '';
 
   # Remove the cross compiler prefix, and add reiserfs support
