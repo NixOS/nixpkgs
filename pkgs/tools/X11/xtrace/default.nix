c50a240e3d1d0cc7c6a781a64e049f5d33529397
Ludovic Courtès <ludo@gnu.org>
2010-11-25 09:38:28 +0000

    xtrace 1.0.2.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=24854

--- pkgs/tools/X11/xtrace/default.nix
+++ pkgs/tools/X11/xtrace/default.nix
@@ -1,17 +1,24 @@
-{stdenv, fetchurl, libX11}:
+{ stdenv, fetchurl, libX11, xauth, makeWrapper }:
 
+let version = "1.0.2"; in
 stdenv.mkDerivation {
-  name = "xtrace-1.0.1";
+  name = "xtrace-${version}";
   src = fetchurl {
-    url = "https://alioth.debian.org/frs/download.php/3149/xtrace_1.0.1.orig.tar.gz";
-    sha256 = "042rifm93mws7xbw86z0m1rmdijprlkijsi2882as1yf6gdbdqbm";
+    url = "https://alioth.debian.org/frs/download.php/3201/xtrace_${version}.orig.tar.gz";
+    sha256 = "0czywk2iwj9vifml0qjlbz8n9jnqjsm4zz22haii82bf4l5w3y04";
   };
-  buildInputs = [libX11];
+
+  buildInputs = [ libX11 makeWrapper ];
+
+  postInstall =
+    '' wrapProgram "$out/bin/xtrace" \
+         --prefix PATH ':' "${xauth}/bin"
+    '';
 
   meta = {
     homepage = http://xtrace.alioth.debian.org/;
-    description = "Trace X protocol connections";
-    license = "free";
+    description = "xtrace, a tool to trace X11 protocol connections";
+    license = "GPLv2";
     maintainers = with stdenv.lib.maintainers; [viric];
     platforms = with stdenv.lib.platforms; linux;
   };
