fca73ee251d58abbe41d22c50836f45516cd345a
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-11-29 18:00:26 +0000

    Making directfb with more options, so it can be cross-built without X for example.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=24926

--- pkgs/development/libraries/directfb/default.nix
+++ pkgs/development/libraries/directfb/default.nix
@@ -1,6 +1,7 @@
-{stdenv, fetchurl, perl, zlib, libjpeg, freetype, 
-  SDL, libX11, xproto, xextproto, libXext, libpng,
-  renderproto, libXrender, giflib}:
+{stdenv, fetchurl, perl, zlib, libjpeg, freetype, libpng, giflib
+, enableX11 ? true, libX11, xproto, xextproto, libXext, renderproto, libXrender
+, enableSDL ? true, SDL }:
+
 let s = import ./src-for-default.nix; in
 stdenv.mkDerivation {
   inherit (s) name;
@@ -8,22 +9,30 @@ stdenv.mkDerivation {
     url = s.url;
     sha256 = s.hash;
   };
-  buildInputs = [perl zlib libjpeg freetype SDL
-    xproto libX11 libXext xextproto libpng
-    renderproto libXrender giflib
+
+  buildNativeInputs = [ perl ];
+
+  buildInputs = [ zlib libjpeg freetype giflib libpng ]
+    ++ stdenv.lib.optional enableSDL SDL
+    ++ stdenv.lib.optionals enableX11 [
+      xproto libX11 libXext xextproto
+      renderproto libXrender
     ];
+
   NIX_LDFLAGS="-lgcc_s";
+
   configureFlags = [
     "--enable-sdl"
     "--enable-zlib"
     "--with-gfxdrivers=all"
     "--enable-devmem"
     "--enable-fbdev"
-    "--enable-x11"
     "--enable-mmx"
     "--enable-sse"
     "--enable-sysfs"
     "--with-software"
     "--with-smooth-scaling"
+    ] ++ stdenv.lib.optionals enableX11 [
+      "--enable-x11"
     ];
 }
