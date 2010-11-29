9d4fb7d641e176f99813598db8d45f57ccd80fa5
Peter Simons <simons@cryp.to>
2010-11-29 11:12:43 +0000

    pkgs/development/interpreters/r-lang: updated to version 2.12.0
    svn path=/nixpkgs/trunk/; revision=24913

--- pkgs/development/interpreters/r-lang/default.nix
+++ pkgs/development/interpreters/r-lang/default.nix
@@ -4,14 +4,20 @@ withBioconductor ? false
 
 stdenv.mkDerivation {
   name = "r-lang";
-  version = "2.7.0";
+  version = "2.12.0";
   src = fetchurl {
-    url = http://cran.r-project.org/src/base/R-2/R-2.7.0.tar.gz;
-    sha256 = "17ql1j5d9rfpxs04j9v9qyxiysc9nh6yr43lgfdamayzjpia5jqm";
+    url = http://cran.r-project.org/src/base/R-2/R-2.12.0.tar.gz;
+    sha256 = "93d72d845b01c6cd00e58f04b5e78fd6c83de96a8620505ad2a016772af02179";
   };
 
   bioconductor = if withBioconductor then import ../development/libraries/science/biology/bioconductor { inherit fetchurl stdenv readline; } else null;
-  
+
+  postUnpack = ''
+    gunzip R-2.12.0/src/library/Recommended/Matrix_0.999375-44.tar.gz
+    tar --file=R-2.12.0/src/library/Recommended/Matrix_0.999375-44.tar --delete Matrix/src/dummy.cpp
+    gzip R-2.12.0/src/library/Recommended/Matrix_0.999375-44.tar
+  '';
+
   buildInputs = [readline perl gfortran libpng libX11 libXt zlib];
   configureFlags = ["--enable-R-shlib"] ;
 
