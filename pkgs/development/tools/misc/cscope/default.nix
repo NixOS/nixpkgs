cd9d91306c7f4f3d00c4f21ebbd864a186c8d4a3
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-12-04 21:43:24 +0000

    Fixing the dependency of cscope on 'emacs'.
    On the other hand, cross-building emacs fails even to evaluate. I did not investigate.
    
    
    svn path=/nixpkgs/branches/stdenv-updates/; revision=24970

--- pkgs/development/tools/misc/cscope/default.nix
+++ pkgs/development/tools/misc/cscope/default.nix
@@ -17,8 +17,8 @@ stdenv.mkDerivation rec {
 
   configureFlags = "--with-ncurses=${ncurses}";
 
-  buildInputs = [ ncurses emacs ];
-  buildNativeInputs = [ pkgconfig ];
+  buildInputs = [ ncurses ];
+  buildNativeInputs = [ pkgconfig emacs ];
 
   postInstall = ''
     # Install Emacs mode.
