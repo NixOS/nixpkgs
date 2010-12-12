209522e365569d895c7e1c560e59381b33ba79f7
Ludovic Courtès <ludo@gnu.org>
2010-12-12 20:19:13 +0000

    GNU Common C++ 1.8.1.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=25077

--- pkgs/development/libraries/commoncpp2/default.nix
+++ pkgs/development/libraries/commoncpp2/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv }:
 
 stdenv.mkDerivation rec {
-  name = "commoncpp2-1.8.0";
+  name = "commoncpp2-1.8.1";
 
   src = fetchurl {
     url = "mirror://gnu/commoncpp/${name}.tar.gz";
-    sha256 = "0a7arpm9l3s5qics5m77lyx1yl7998lkypydqwx11nj730034nmc";
+    sha256 = "0kmgr5w3b1qwzxnsnw94q6rqs0hr8nbv9clf07ca2a2fyypx9kjk";
   };
 
   doCheck = true;
