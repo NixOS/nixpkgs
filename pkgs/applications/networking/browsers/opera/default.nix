24640ac0ec77c9c13b940920a4ec1c60410c224b
Michael Raskin <7c6f434c@mail.ru>
2010-12-14 15:56:22 +0000

    Update Opera
    svn path=/nixpkgs/trunk/; revision=25130

--- pkgs/applications/networking/browsers/opera/default.nix
+++ pkgs/applications/networking/browsers/opera/default.nix
@@ -5,20 +5,20 @@
 assert stdenv.isLinux && stdenv.gcc.gcc != null;
 
 stdenv.mkDerivation rec {
-  name = "opera-10.10";
+  name = "opera-10.63";
 
   builder = ./builder.sh;
   
   src =
     if stdenv.system == "i686-linux" then
       fetchurl {
-        url = "http://mirror.liteserver.nl/pub/opera/linux/1010/final/en/i386/shared/opera-10.10.gcc4-shared-qt3.i386.tar.bz2";
-        sha256 = "0y8xahwgx5jw83ky4zkw8ixyfgnd2xg9k0zq15yivhimi60fsppc";
+        url = "http://mirror.liteserver.nl/pub/opera/linux/1063/opera-10.63-6450.i386.linux.tar.bz2";
+        sha256 = "dd105d602a4b8897749a4cb9610f8bfe2d07d4f4cc9bf3905930c65592737259";
       }
     else if stdenv.system == "x86_64-linux" then
       fetchurl {
-        url = "http://mirror.liteserver.nl/pub/opera/linux/1010/final/en/x86_64/opera-10.10.gcc4-shared-qt3.x86_64.tar.bz2";
-        sha256 = "1z0zgalqv9lnf1jsg3zg9diqfyszh75r7m1dbkifkdawn4zv4q3s";
+        url = "http://mirror.liteserver.nl/pub/opera/linux/1063/opera-10.63-6450.x86_64.linux.tar.bz2";
+        sha256 = "da8ae14cf317364ab0295102220246b205bf30c59c00cadb571395c90dda7c74";
       }
     else throw "Opera is not supported on ${stdenv.system} (only i686-linux and x86_64 linux are supported)";
 
