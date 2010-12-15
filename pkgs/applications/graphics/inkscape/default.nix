83e25f83c83e329ddff35a88a87a3b875ab6c9c3
Russell O'Connor <roconnor@theorem.ca>
2010-12-15 15:56:04 +0000

    Patch Inkscape to support PDF importing.
    svn path=/nixpkgs/trunk/; revision=25151

--- pkgs/applications/graphics/inkscape/default.nix
+++ pkgs/applications/graphics/inkscape/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, pkgconfig, perl, perlXMLParser, gtk, libXft
 , libpng, zlib, popt, boehmgc, libxml2, libxslt, glib, gtkmm
 , glibmm, libsigcxx, lcms, boost, gettext, makeWrapper, intltool
-, gsl, python, pyxml, lxml }:
+, gsl, python, pyxml, lxml, poppler }:
 
 stdenv.mkDerivation rec {
   name = "inkscape-0.48.0";
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
   buildInputs = [
     pkgconfig perl perlXMLParser gtk libXft libpng zlib popt boehmgc
     libxml2 libxslt glib gtkmm glibmm libsigcxx lcms boost gettext
-    makeWrapper intltool gsl
+    makeWrapper intltool gsl poppler
   ];
 
   configureFlags = "--with-python";
