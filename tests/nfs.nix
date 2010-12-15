6039c22da407ec96e36e6712cf450b5d83f13c40
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-12-15 12:10:17 +0000

    svn path=/nixos/trunk/; revision=25141

--- tests/nfs.nix
+++ tests/nfs.nix
@@ -4,7 +4,7 @@ let
 
   client = 
     { config, pkgs, ... }:
-    { fileSystems = pkgs.lib.mkOverrideTemplate 50 {} 
+    { fileSystems = pkgs.lib.mkOverride 50 
         [ { mountPoint = "/data";
             device = "server:/data";
             fsType = "nfs";
