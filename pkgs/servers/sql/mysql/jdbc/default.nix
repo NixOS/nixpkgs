4e0c2cc3c64e4d7d962b150732494e6fdbbf4026
Sander van der Burg <s.vanderburg@tudelft.nl>
2010-12-16 21:04:13 +0000

    Seems that this URL gives some trouble, fixed it to a better one...
    svn path=/nixpkgs/trunk/; revision=25161

--- pkgs/servers/sql/mysql/jdbc/default.nix
+++ pkgs/servers/sql/mysql/jdbc/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   builder = ./builder.sh;
 
   src = fetchurl {
-    url = http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.14.tar.gz/from/http://mirror.leaseweb.com/mysql/;
+    url = ftp://mirror.leaseweb.com/mysql/Downloads/Connector-J/mysql-connector-java-5.1.14.tar.gz;
     sha256 = "1l1jgaf498pvmvls3ilwyxpcafywfabf5kjc8qgzx7559lx8fvya";
   };
 
