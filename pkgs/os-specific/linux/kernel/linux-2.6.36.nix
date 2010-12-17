dc94479f5f31d7437a1f33e06960020e5ddb2e5d
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2010-12-17 20:28:50 +0000

    Updating the linux 2.6.36 to 2.6.36.2
    svn path=/nixpkgs/trunk/; revision=25185

--- pkgs/os-specific/linux/kernel/linux-2.6.36.nix
+++ pkgs/os-specific/linux/kernel/linux-2.6.36.nix
@@ -192,11 +192,11 @@ in
 import ./generic.nix (
 
   rec {
-    version = "2.6.36";
+    version = "2.6.36.2";
   
     src = fetchurl {
       url = "mirror://kernel/linux/kernel/v2.6/linux-${version}.tar.bz2";
-      sha256 = "15a076d1a435a6bf8e92834eba4b390b4ec094ce06d47f89d071ca9e5788ce04";
+      sha256 = "06wshmpgwfswdfqx5cc5nsqaa8vjyg3wab2zpzmvmc1gw564b8ll";
     };
 
     config = configWithPlatform stdenv.platform;
