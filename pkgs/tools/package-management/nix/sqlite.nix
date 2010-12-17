229b94b30cbf28d54bd1f218fb92db598847f735
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-12-17 08:56:53 +0000

    svn path=/nixpkgs/trunk/; revision=25165

--- pkgs/tools/package-management/nix/sqlite.nix
+++ pkgs/tools/package-management/nix/sqlite.nix
@@ -5,11 +5,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "nix-1.0pre25100";
+  name = "nix-1.0pre25121";
 
   src = fetchurl {
-    url = "http://hydra.nixos.org/build/800113/download/4/${name}.tar.bz2";
-    sha256 = "466f044c02c69f4eb8c572c762a057d7ee24e90e8836eab336bb5188a3901d56";
+    url = "http://hydra.nixos.org/build/805927/download/4/${name}.tar.bz2";
+    sha256 = "94e619e8b44f1172e71dc86d0aec1a557e76d49609e9a1fccbcea752360e3e97";
   };
 
   buildInputs = [ perl curl openssl pkgconfig boehmgc ];
