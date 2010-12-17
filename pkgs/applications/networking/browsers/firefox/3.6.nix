a6f1f87697768e39c5ed7f1e475b3002e1418d46
Karn Kallio <tierpluspluslists@gmail.com>
2010-12-17 20:28:51 +0000

    Upstream reports bug has been fixed https://bugzilla.mozilla.org/show_bug.cgi?id=551152 and the patch did not cleanly apply.
    svn path=/nixpkgs/trunk/; revision=25186

--- pkgs/applications/networking/browsers/firefox/3.6.nix
+++ pkgs/applications/networking/browsers/firefox/3.6.nix
@@ -46,10 +46,6 @@ rec {
     
     inherit src;
 
-    # To be removed when the change gets upstream. I don't know if the patch
-    # affects xulrunner or firefox.
-    patches = [ ./symlinks-bug551152.patch ];
-
     buildInputs =
       [ pkgconfig gtk perl zip libIDL libjpeg libpng zlib cairo bzip2
         python dbus dbus_glib pango freetype fontconfig xlibs.libXi
