c04f8e236c503e30183dad8e95fea070755a0735
Karn Kallio <tierpluspluslists@gmail.com>
2010-12-17 23:18:21 +0000

    Bump kmplayer to latest release 0.11.2.c
    This also fixes build failure related to docbook versions.
    
    svn path=/nixpkgs/trunk/; revision=25190

--- pkgs/applications/video/kmplayer/default.nix
+++ pkgs/applications/video/kmplayer/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, lib, cmake, qt4, perl, gettext, pango, gtk, dbus_glib, kdelibs, automoc4, phonon}:
 
 stdenv.mkDerivation {
-  name = "kmplayer-0.11.2b";
+  name = "kmplayer-0.11.2c";
   src = fetchurl {
-    url = http://kmplayer.kde.org/pkgs/kmplayer-0.11.2b.tar.bz2;
-    sha256 = "00a1pw31p849cbgskyfi8jni9ar6yi2ivr625vza2za6apdxvkr7";
+    url = http://kmplayer.kde.org/pkgs/kmplayer-0.11.2c.tar.bz2;
+    sha256 = "1qhafq865bzpz6m9k7cjdv4884qfpn481ak77ly0nidpq2ab0l9m";
   };
   builder = ./builder.sh;
   buildInputs = [ cmake qt4 perl gettext stdenv.gcc.libc pango gtk dbus_glib kdelibs automoc4 phonon ];
