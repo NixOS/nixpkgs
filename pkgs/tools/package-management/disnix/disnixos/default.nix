c93a60d7a395f80dfb68197c4f0417c38b8b9781
Sander van der Burg <s.vanderburg@tudelft.nl>
2010-12-19 15:28:11 +0000

    Bumped version of disnixos again
    svn path=/nixpkgs/trunk/; revision=25201

--- pkgs/tools/package-management/disnix/disnixos/default.nix
+++ pkgs/tools/package-management/disnix/disnixos/default.nix
@@ -3,8 +3,8 @@
 stdenv.mkDerivation {
   name = "disnixos";
   src = fetchurl {
-    url = http://hydra.nixos.org/build/812969/download/1/disnixos-0.1pre25198.tar.gz;
-    sha256 = "1bvn89177cb2ab3vl1lva99hy3r6dznmv7x5hnxmmqncw5kmd42j";
+    url = http://hydra.nixos.org/build/812999/download/1/disnixos-0.1pre25200.tar.gz;
+    sha256 = "1cw0wg5x65nanrjfj67nk1jxngaqh2l7msx03s536hssm9g2mv7r";
   };
   buildInputs = [ socat pkgconfig disnix ];
   dontStrip = true;
