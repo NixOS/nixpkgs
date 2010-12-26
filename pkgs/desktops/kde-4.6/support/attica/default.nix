b0cbe093ba9e3af8a1cc0cd350f072c5e5eba9bf
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-12-26 15:27:15 +0000

    attica-0.2.0
    svn path=/nixpkgs/trunk/; revision=25284

--- pkgs/desktops/kde-4.6/support/attica/default.nix
+++ pkgs/desktops/kde-4.6/support/attica/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, cmake, qt4}:
 
 stdenv.mkDerivation rec {
-  name = "attica-0.1.4";
+  name = "attica-0.2.0";
   src = fetchurl {
     url = "mirror://kde/stable/attica/${name}.tar.bz2";
-    sha256 = "0frarnrnbli3f5ji90swgw05g88w1f5777ais345wc8lkvqg9ix1";
+    sha256 = "0g2la91fgdr185ah15vc91plvdwvbm6kpsyz0vk0da7ggiyg3y9a";
   };
   buildInputs = [ cmake qt4 ];
   meta = with stdenv.lib; {
