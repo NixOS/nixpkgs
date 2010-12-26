4f140371a508b492b0dec53b658ed943ae4d36a9
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-12-26 15:27:20 +0000

    akonadi-1.4.90
    svn path=/nixpkgs/trunk/; revision=25285

--- pkgs/desktops/kde-4.6/support/akonadi/default.nix
+++ pkgs/desktops/kde-4.6/support/akonadi/default.nix
@@ -1,14 +1,12 @@
 {stdenv, fetchurl, cmake, qt4, shared_mime_info, libxslt, boost, mysql, automoc4, soprano}:
 
 stdenv.mkDerivation rec {
-  name = "akonadi-1.4.0";
+  name = "akonadi-1.4.90";
   src = fetchurl {
     url = "http://download.akonadi-project.org/${name}.tar.bz2";
-    sha256 = "199fh5yqygr0xdwcnjqqms8vskigbzvwb3071r979606rrsnpnl5";
+    sha256 = "0am4m81zhq343a42s1ig5jxx47i6g1d97r546qqzdm5w542r6c00";
   };
   buildInputs = [ cmake qt4 shared_mime_info libxslt boost mysql automoc4 soprano ];
-  patches = [ ./fix-broken-datadir-parameter.patch ];
-  postPatch = "sed -e '/Q_ASSERT.*SQLITE/d' -i qsqlite/src/qsql_sqlite.cpp";
   meta = with stdenv.lib; {
     description = "KDE PIM Storage Service";
     license = "LGPL";
