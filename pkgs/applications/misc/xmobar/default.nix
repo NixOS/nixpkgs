d3bb1b263001b3035f779ffcdb4588b7a8284eae
Alexander Tsamutali <astsmtl@yandex.ru>
2010-12-30 22:36:46 +0000

    applications/misc/xmobar: Updated to 0.12.
    svn path=/nixpkgs/trunk/; revision=25327

--- pkgs/applications/misc/xmobar/default.nix
+++ pkgs/applications/misc/xmobar/default.nix
@@ -3,9 +3,10 @@
 cabal.mkDerivation (self : {
   pname = "xmobar";
   name = "${self.fname}";
-  version = "0.9.2";
-  sha256 = "361295f5dc912512a2eb644ecd331562a271243192be6215cb071e44f50c7c66";
+  version = "0.12";
+  sha256 = "633b7985dbaebd58864f591ea7ff8b44f5a69b4e3d0a592df01daf8fe11a5d31";
   extraBuildInputs = [X11 mtl parsec stm utf8String X11Xft];
+  configureFlags = "--flags=with_xft";
   meta = {
     description = "xmobar is a minimalistic text based status bar";
   };
