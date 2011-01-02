8af7cabe215fcaa803b3866e3301528d07c69149
Karn Kallio <tierpluspluslists@gmail.com>
2011-01-02 22:35:02 +0000

    Update patchelf 0.6 to latest prerelease 23458.
    The older prerelease did not download; Hydra said the product had disappeared.
    
    svn path=/nixpkgs/trunk/; revision=25350

--- pkgs/development/tools/misc/patchelf/0.6.nix
+++ pkgs/development/tools/misc/patchelf/0.6.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation rec {
-  name = "patchelf-0.6pre22275";
+  name = "patchelf-0.6pre23458";
   
   src = fetchurl {
-    url = "http://hydra.nixos.org/build/504657/download/2/patchelf-0.6pre22813.tar.bz2";
-    sha256 = "1ml86wyl35ik3lixkcz2vlzvbcancj0dygwfh5ambjmazp2q19mh";
+    url = "http://hydra.nixos.org/build/838169/download/2/patchelf-0.6pre23458.tar.bz2";
+    sha256 = "18d74n14s4xh8aqwisvz63gx9j4d5b9bzb8k1jnp8whvvwzasdq5";
   };
 
   meta = {
