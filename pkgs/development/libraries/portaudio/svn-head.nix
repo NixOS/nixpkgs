660792741dbabd16968711d6b20c06600d7f2ac7
Michael Raskin <7c6f434c@mail.ru>
2011-01-02 14:52:32 +0000

    Adding a portaudio svn-head expression
    svn path=/nixpkgs/trunk/; revision=25336

---
+++ pkgs/development/libraries/portaudio/svn-head.nix
@@ -0,0 +1,25 @@
+{ stdenv, fetchsvn, alsaLib, pkgconfig }:
+
+stdenv.mkDerivation rec {
+  revision = "1564";
+  name = "portaudio-svn-r${revision}";
+	
+  src = fetchsvn {
+    url = "https://www.portaudio.com/repos/portaudio/trunk";
+    rev = revision;
+    sha256 = "0vhiy4lkmv0flhvkbbra71z5cfr3gbh27bbfcqqdc939b4z35lsi";
+  };
+
+  buildInputs = [ alsaLib pkgconfig ];
+  
+  meta = {
+    description = "Portable cross-platform Audio API";
+    homepage = http://www.portaudio.com/;
+    # Not exactly a bsd license, but alike
+    license = "BSD";
+  };
+
+  passthru = {
+    api_version = 19;
+  };
+}
