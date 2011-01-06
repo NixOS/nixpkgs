def604cc6248a584892b24318f0dbdbdf462b380
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-01-06 10:17:18 +0000

    * Spotify: add the 32-bit version and provide a longDescription.
    svn path=/nixpkgs/trunk/; revision=25436

--- pkgs/applications/audio/spotify/default.nix
+++ pkgs/applications/audio/spotify/default.nix
@@ -1,14 +1,24 @@
 { fetchurl, stdenv, dpkg, xlibs, qt4, alsaLib }:
 
-assert stdenv.system == "x86_64-linux";
+assert stdenv.system == "i686-linux" || stdenv.system == "x86_64-linux";
+
+let version = "0.4.9.302"; in
 
 stdenv.mkDerivation {
-  name = "spotify-0.4.9.302";
+  name = "spotify-${version}";
 
-  src = fetchurl {
-    url = http://repository.spotify.com/pool/non-free/s/spotify/spotify-client-qt_0.4.9.302.g604b4fb-1_amd64.deb;
-    sha256 = "1cghs3hwmqnd7g62g1h2bf3yvxgjq8b94vzhp1w9ysb5rswyjkyv";
-  };
+  src =
+    if stdenv.system == "i686-linux" then 
+      fetchurl {
+        url = "http://repository.spotify.com/pool/non-free/s/spotify/spotify-client-qt_${version}.g604b4fb-1_i386.deb";
+        sha256 = "1kw3jfvz8a9v6zl3yh6f51vsick35kmcf7vkbjb6wl0nk1a8q8gg";
+      }
+    else if stdenv.system == "x86_64-linux" then 
+      fetchurl {
+        url = "http://repository.spotify.com/pool/non-free/s/spotify/spotify-client-qt_${version}.g604b4fb-1_amd64.deb";
+        sha256 = "1cghs3hwmqnd7g62g1h2bf3yvxgjq8b94vzhp1w9ysb5rswyjkyv";
+      }
+    else throw "Spotify not supported on this platform.";
 
   buildInputs = [ dpkg ];
 
@@ -34,5 +44,22 @@ stdenv.mkDerivation {
     homepage = https://www.spotify.com/download/previews/;
     description = "Spotify for Linux allows you to play music from the Spotify music service";
     license = "unfree";
+    maintainers = [ stdenv.lib.maintainers.eelco ];
+
+    longDescription =
+      ''
+        Spotify is a digital music streaming service.  This package
+        provides the Spotify client for Linux.  At present, it does not
+        work with free Spotify accounts; it requires a Premium or
+        Unlimited account.
+
+        Currently, the Spotify client requires a symlink from
+        /usr/share/spotify to its resources.  Thus, you should do
+        something like:
+
+          $ nix-env -i spotify
+          $ mkdir -p /usr/share
+          $ ln -s ~/.nix-profile/share/spotify /usr/share/
+      '';
   };
 }
