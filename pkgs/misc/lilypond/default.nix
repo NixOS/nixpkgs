c05a95f8f6874ffdd5b8f4879d4b74789c4010d1
Michael Raskin <7c6f434c@mail.ru>
2011-01-12 18:19:43 +0000

    Update Lilypond - this also fixes its build
    svn path=/nixpkgs/trunk/; revision=25525

--- pkgs/misc/lilypond/default.nix
+++ pkgs/misc/lilypond/default.nix
@@ -2,12 +2,15 @@
 , python, gettext, flex, perl, bison, pkgconfig, texLive
 , fontconfig, freetype, pango, fontforge, help2man }:
 
-stdenv.mkDerivation {
-  name = "lilypond-2.13.9";
+stdenv.mkDerivation rec{
+  majorVersion="2.13";
+  minorVersion="46";
+  version="${majorVersion}.${minorVersion}";
+  name = "lilypond-${version}";
 
   src = fetchurl {
-    url = http://download.linuxaudio.org/lilypond/sources/v2.13/lilypond-2.13.9.tar.gz;
-    sha256 = "1x3jz0zbhly4rc07nry3ia3ydd6vislz81gg0ivwfm6f6q0ssk57";
+    url = "http://download.linuxaudio.org/lilypond/sources/v${majorVersion}/lilypond-${version}.tar.gz";
+    sha256 = "370f59d10a3fc74c5790111f7a69e04304eda5384513c95838dda3cc087904e6";
   };
 
   configureFlags = [ "--disable-documentation" "--with-ncsb-dir=${ghostscript}/share/ghostscript/fonts"];
