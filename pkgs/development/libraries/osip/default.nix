e437b85040b9371d9abbedcfa593b1ca5e2eecd6
Ludovic Courtès <ludo@gnu.org>
2011-01-13 22:36:07 +0000

    GNU oSIP: Slightly improve `meta'.
    svn path=/nixpkgs/trunk/; revision=25549

--- pkgs/development/libraries/osip/default.nix
+++ pkgs/development/libraries/osip/default.nix
@@ -6,15 +6,12 @@ stdenv.mkDerivation rec {
     sha256 = "14csf6z7b802bahxd560ibx3mg2fq3ki734vf3k2vknr4jm5v5fx";
   };
   name = "libosip2-${version}";
-      
+
   meta = {
     license = "LGPLv2.1+";
-    description = "GNU oSIP library ";
-    maintainers = with stdenv.lib.maintainers;
-    [
-      raskin
-    ];
-    platforms = with stdenv.lib.platforms;
-      linux;
+    homepage = http://www.gnu.org/software/osip/;
+    description = "The GNU oSIP library, an implementation of the Session Initiation Protocol (SIP)";
+    maintainers = with stdenv.lib.maintainers; [ raskin ];
+    platforms = stdenv.lib.platforms.linux;
   };
 }
