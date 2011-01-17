b7f536027dfa147131b856781fa10a68ba2c2661
Rob Vermaas <rob.vermaas@gmail.com>
2011-01-17 19:41:05 +0000

    enable lzma support in squashfs-tools
    svn path=/nixpkgs/trunk/; revision=25602

--- pkgs/tools/filesystems/squashfs/default.nix
+++ pkgs/tools/filesystems/squashfs/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, zlib, attr}:
+{stdenv, fetchurl, zlib, attr, xz}:
 
 stdenv.mkDerivation rec {
   name = "squashfs-4.1";
@@ -8,16 +8,17 @@ stdenv.mkDerivation rec {
     sha256 = "0sh40r7gz81fg7ivgr7rld8spvqb6hsfvnf6gd3gbcr5b830v1rs";
   };
   
-  buildInputs = [zlib attr];
+  buildInputs = [zlib attr xz];
 
   preBuild = ''
     cd squashfs-tools
   '';
+  IUSE="+gzip +lzma";
 
   NIX_LDFLAGS = "-lgcc_s"; # for pthread_cancel
 
   installFlags = "INSTALL_DIR=\${out}/bin";
-
+  makeFlags = "XZ_SUPPORT=1";
   meta = {
     homepage = http://squashfs.sourceforge.net/;
     description = "Tool for creating and unpacking squashfs filesystems";
