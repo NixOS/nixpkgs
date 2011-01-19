45f6a2587f3d58b24f8865d32012298a6a730ab9
Sander van der Burg <s.vanderburg@tudelft.nl>
2011-01-19 13:46:13 +0000

    bump disnixos
    svn path=/nixpkgs/trunk/; revision=25629

--- pkgs/tools/package-management/disnix/disnixos/default.nix
+++ pkgs/tools/package-management/disnix/disnixos/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, disnix, socat, pkgconfig}:
 
 stdenv.mkDerivation {
-  name = "disnixos-0.1pre25606";
+  name = "disnixos-0.1pre25628";
   src = fetchurl {
-    url = http://hydra.nixos.org/build/860037/download/1/disnixos-0.1pre25606.tar.gz;
-    sha256 = "1rsajzn79ca1624zsj9zr72247hh3gdda53l4vzqb8hzm2vhh0yc";
+    url = http://hydra.nixos.org/build/861183/download/1/disnixos-0.1pre25628.tar.gz;
+    sha256 = "1wbd8x3j8mspv5v2w5vmfadby52337xpyhjll15a2wik1d8f19bk";
   };
   buildInputs = [ socat pkgconfig disnix ];
   dontStrip = true;
