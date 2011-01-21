8361444f9b0eb10e1595a57b69ac5fa4147b1c8b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-01-21 13:38:30 +0000

    * Disable compositing for now in the graphical installation CD.  It   seems to cause problems with KDE 4.5 on some graphics drivers (such   as Cirrus and VGA in QEMU).  (NixOS/121)
    svn path=/nixos/trunk/; revision=25659

--- modules/installer/cd-dvd/installation-cd-graphical.nix
+++ modules/installer/cd-dvd/installation-cd-graphical.nix
@@ -1,11 +1,20 @@
 # This module defines a NixOS installation CD that contains X11 and
 # KDE 4.
 
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
   require = [
     ./installation-cd-base.nix
     ../../profiles/graphical.nix
   ];
+
+  # Disable compositing for now.  It seems to cause problems with KDE
+  # 4.5 on some graphics drivers (such as Cirrus and VGA in QEMU).
+  services.xserver.config =
+    ''
+      Section "Extensions"
+        Option "Composite" "Disable"
+      EndSection
+    '';
 }
