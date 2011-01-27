a393fe50ad2882f58d8060072221fb124ed33dc1
Cillian de Roiste <cillian.deroiste@gmail.com>
2011-01-27 21:10:41 +0000

    Bumping xf86-input-wacom to version 0.10.10, updating udev rules for modern udev
    svn path=/nixpkgs/trunk/; revision=25706

--- pkgs/os-specific/linux/xf86-input-wacom/default.nix
+++ pkgs/os-specific/linux/xf86-input-wacom/default.nix
@@ -1,28 +1,28 @@
 { stdenv, fetchurl, libX11, libXi, inputproto, file
-, xproto, ncurses, pkgconfig, xorgserver }:
+, xproto, ncurses, pkgconfig, xorgserver, 
+libXext, libXrandr, randrproto, libXrender }:
 
 stdenv.mkDerivation rec {
-  name = "xf86-input-wacom-0.10.5";
+  name = "xf86-input-wacom-0.10.10";
 
   src = fetchurl {
     url = "mirror://sourceforge/linuxwacom/${name}.tar.bz2";
-    sha256 = "07rg9a9n1dyjff4awlc5imy44y0lg59qs8h4rr56lgjg612wkmy0";
+    sha256 = "03yggp2ww64va6gmasl0gy0rbfcyb1zlj9kapp9kvhk2j4458fdr";
   };
 
   buildInputs = [ libX11 libXi inputproto xproto ncurses pkgconfig xorgserver
-    file ];
-
-  patchPhase="sed -e s@/usr/bin/file@${file}/bin/file@g -i configure";
+    file libXext libXrandr libXrender randrproto ];
 
   preConfigure = ''
-    configureFlags="--with-xorg-module-dir=$out/lib/xorg/modules/input
-    --with-sdkdir=$out/include/xorg"
+    ensureDir $out/share/X11/xorg.conf.d
+    configureFlags="--with-xorg-module-dir=$out/lib/xorg/modules
+    --with-sdkdir=$out/include/xorg --with-xorg-conf-dir=$out/share/X11/xorg.conf.d"
   '';
 
   postInstall =
     ''
-      ensureDir $out/etc/udev/rules.d
-      cp ${./10-wacom.rules} $out/etc/udev/rules.d/10-wacom.rules
+      ensureDir $out/lib/udev/rules.d
+      cp ${./10-wacom.rules} $out/lib/udev/rules.d/10-wacom.rules
     '';
 
   meta = with stdenv.lib; {
