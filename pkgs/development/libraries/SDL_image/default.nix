5c6389f69c766f7cdba227837fd1c78951fdc0bb
Yury G. Kudryashov <urkud.urkud@gmail.com>
2011-02-09 21:10:05 +0000

    SDL_image-1.2.10
    svn path=/nixpkgs/trunk/; revision=25865

--- pkgs/development/libraries/SDL_image/default.nix
+++ pkgs/development/libraries/SDL_image/default.nix
@@ -2,18 +2,18 @@
 
 stdenv.mkDerivation rec {
   pname = "SDL_image";
-  version = "1.2.6";
+  version = "1.2.10";
 
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "http://www.libsdl.org/projects/${pname}/release/${name}.tar.gz";
-    sha256 = "1i3f72dw3i3l6d77dk81gw57sp0629rng9k76qb37brlz7dv3z48";
+    sha256 = "0xhqw56xgc0rn3ziccirib8ai2whbbidjmvig527n9znjlg5vq3m";
   };
 
   buildInputs = [SDL libpng libjpeg libtiff libungif libXpm];
 
-  postInstall = "ln -s $out/include/SDL/SDL_image.h $out/include/";
+  postInstall = "ln -sv $out/include/SDL/SDL_image.h $out/include/";
 
   meta = {
     description = "SDL image library";
