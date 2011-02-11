2c72c03de589720dd75f996f24f5d22740463bda
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-02-11 14:48:30 +0000

    * Mesa 7.9.1.
    svn path=/nixpkgs/branches/x-updates/; revision=25905

--- pkgs/development/libraries/mesa/default.nix
+++ pkgs/development/libraries/mesa/default.nix
@@ -1,27 +1,29 @@
-{ stdenv, fetchurl, pkgconfig, x11, xlibs, libdrm, expat, lipo ? null }:
+{ stdenv, fetchurl, pkgconfig, x11, xlibs, libdrm, expat, lipo ? null, talloc }:
 
 if ! stdenv.lib.lists.elem stdenv.system stdenv.lib.platforms.mesaPlatforms then
   throw "unsupported platform for Mesa"
 else
 
+let version = "7.9.1"; in
+
 stdenv.mkDerivation {
-  name = "mesa-7.8.2";
+  name = "mesa-${version}";
 
   src = fetchurl {
-    url = ftp://ftp.freedesktop.org/pub/mesa/7.8.2/MesaLib-7.8.2.tar.bz2;
-    md5 = "6be2d343a0089bfd395ce02aaf8adb57";
+    url = "ftp://ftp.freedesktop.org/pub/mesa/${version}/MesaLib-${version}.tar.bz2";
+    md5 = "07dc6cfb5928840b8b9df5bd1b3ae434";
   };
 
   configureFlags =
     "--disable-gallium"
-    + (if stdenv.system == "mips64-linux" then
-      " --with-dri-drivers=swrast --with-driver=dri" else "")
-    + (if stdenv.isDarwin then " --disable-egl" else "");
+    + stdenv.lib.optionalString (stdenv.system == "mips64-linux")
+      " --with-dri-drivers=swrast --with-driver=dri"
+    + stdenv.lib.optionalString stdenv.isDarwin " --disable-egl";
 
   buildInputs =
     [ pkgconfig expat x11 libdrm xlibs.glproto
       xlibs.libXxf86vm xlibs.libXfixes xlibs.libXdamage xlibs.dri2proto
-      lipo
+      lipo talloc
     ];
 
   passthru = { inherit libdrm; };
