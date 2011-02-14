4621178b75ca1c49b6ea7541e41386e77d72353b
Florian Friesdorf <flo@chaoflow.net>
2011-02-14 22:40:51 +0000

    aircrack-1.0-rc2 -> 1.1-1859 (current trunk)
    latest stable does not compile with gcc 4.5
    
    svn path=/nixpkgs/trunk/; revision=25972

--- pkgs/tools/networking/aircrack-ng/default.nix
+++ pkgs/tools/networking/aircrack-ng/default.nix
@@ -1,11 +1,15 @@
-{stdenv, fetchurl, libpcap, openssl, zlib, wirelesstools}:
+{stdenv, fetchsvn, libpcap, openssl, zlib, wirelesstools}:
 
-stdenv.mkDerivation {
-  name = "aircrack-ng-1.0-rc2";
+let
+  rev = "1859";
+in
+stdenv.mkDerivation rec {
+  name = "aircrack-ng-1.1-${rev}";
 
-  src = fetchurl {
-    url = http://download.aircrack-ng.org/aircrack-ng-1.0-rc2.tar.gz;
-    sha256 = "9d52f15f3fca52775ecb9cfc1f0aeb04c3c3bd3101665d5760d395f7d2a87d8b";
+  src = fetchsvn {
+    url = "http://trac.aircrack-ng.org/svn/trunk";
+    inherit rev;
+    sha256 = "6ca98321ef3f14af9c78b2fe25091c4e79e3c28679f240b80f8aeda71b84ab4a";
   };
 
   buildInputs = [libpcap openssl zlib];
