a35dcdb2819e0ed429fe4a2204803a7bdfcd0e3b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-02-24 11:59:30 +0000

    * libvirt updated to 0.8.8.
    svn path=/nixpkgs/trunk/; revision=26088

--- pkgs/development/libraries/libvirt/default.nix
+++ pkgs/development/libraries/libvirt/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, libxml2, gnutls, devicemapper, perl, python }:
 
-let version = "0.8.3"; in
+let version = "0.8.8"; in
 
 stdenv.mkDerivation {
   name = "libvirt-${version}";
 
   src = fetchurl {
     url = "http://libvirt.org/sources/libvirt-${version}.tar.gz";
-    sha256 = "07vsk4g1nxvxc8yr6cdvwp9kvwgm2g7lh6aaggfkxb2775n87q9m";
+    sha256 = "04z1757qpi3ssnjv5h2qnw1sds2m50yxk67cbdam6w4i50vyl2h3";
   };
 
   buildInputs = [ libxml2 gnutls devicemapper perl python ];
@@ -21,5 +21,6 @@ stdenv.mkDerivation {
     homepage = http://libvirt.org/;
     description = "A toolkit to interact with the virtualization capabilities of recent versions of Linux (and other OSes).";
     license = "LGPLv2+";
+    platforms = stdenv.lib.platforms.linux;
   };
 }
