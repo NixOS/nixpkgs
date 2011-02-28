e5e98b9984eaa3d85faecf72a9816e1bf8416c24
Michael Raskin <7c6f434c@mail.ru>
2011-02-28 20:56:01 +0000

    Updating veracity; adding parts of needed infrastructure for tests. Currently, a test run would fail
    svn path=/nixpkgs/trunk/; revision=26126

--- pkgs/applications/version-management/veracity/src-for-default.nix
+++ pkgs/applications/version-management/veracity/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="0.5.7.10383";
-   name="veracity-0.5.7.10383";
-   hash="1826zr2nwd6qknvv6mw7a2x93fwi4wpabfq4ijy6am3cl55hdy51";
+   version="0.5.7.10397";
+   name="veracity-0.5.7.10397";
+   hash="09w1qj4wklaf7mw0vavzyqpagcd0cwqppdl8vaqqi0irddgivnq8";
    url="http://download-us.sourcegear.com/Veracity/nightly/veracity-source-${version}.tar.gz";
-   advertisedUrl="http://download-us.sourcegear.com/Veracity/nightly/veracity-source-0.5.7.10383.tar.gz";
+   advertisedUrl="http://download-us.sourcegear.com/Veracity/nightly/veracity-source-0.5.7.10397.tar.gz";
   
   
 }
