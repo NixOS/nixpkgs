6381040f0a1ab31265c19ed7b76a0f53761fce4b
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2011-03-02 17:18:20 +0000

    Adding coin3d and soqt
    svn path=/nixpkgs/trunk/; revision=26133

---
+++ pkgs/development/libraries/soqt/default.nix
@@ -0,0 +1,22 @@
+{ fetchurl, stdenv, coin3d, qt4 }:
+
+stdenv.mkDerivation rec {
+  name = "soqt-${version}";
+  version = "1.5.0";
+
+  src = fetchurl {
+    url = "http://ftp.coin3d.org/coin/src/all/SoQt-${version}.tar.gz";
+    sha256 = "14dbh8ynzjcgwgxjc6530c5plji7vn62kbdf447w0dp53564p8zn";
+  };
+
+  buildInputs = [ coin3d qt4 ];
+
+  meta = {
+    homepage = http://www.coin3d.org/;
+    license = "GPLv2+";
+    description = "Glue between Coin high-level 3D visualization library and Qt";
+
+    maintainers = [ stdenv.lib.maintainers.viric ];
+    platforms = stdenv.lib.platforms.linux;
+  };
+}
