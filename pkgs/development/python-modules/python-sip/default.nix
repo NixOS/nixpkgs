31c38d7dd2c5b3683687ad172b0fd3ada4a887f9
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2011-03-04 09:48:29 +0000

    Updating pyqt and sip because the 'src' there mentioned is not at the urls anymore.
    svn path=/nixpkgs/trunk/; revision=26152

--- pkgs/development/python-modules/python-sip/default.nix
+++ pkgs/development/python-modules/python-sip/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, python }:
 
 stdenv.mkDerivation rec {
-  name = "sip-4.11.2";
+  name = "sip-4.12.1";
   
   src = fetchurl {
     url = "http://www.riverbankcomputing.co.uk/static/Downloads/sip4/${name}.tar.gz";
-    sha256 = "0g1pj203m491rhy111ayr4k4lsbcqd8sa1np503xv94a90b05l6f";
+    sha256 = "1dc16f8m52qc824ksvyfhkdmsjbxyq82g5dr2xn8x9f26246xmp9";
   };
   
   configurePhase = "python ./configure.py -d $out/lib/${python.libPrefix}/site-packages -b $out/bin -e $out/include";
