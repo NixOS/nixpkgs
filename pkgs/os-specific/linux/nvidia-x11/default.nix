b8138544887cbce1b9cafc92c2ba5065390c60a1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-03-07 16:51:54 +0000

    * NVIDIA driver updated to 260.19.44.
    svn path=/nixpkgs/branches/x-updates/; revision=26199

--- pkgs/os-specific/linux/nvidia-x11/default.nix
+++ pkgs/os-specific/linux/nvidia-x11/default.nix
@@ -7,7 +7,7 @@
 
 with stdenv.lib;
 
-let versionNumber = "260.19.36"; in
+let versionNumber = "260.19.44"; in
 
 stdenv.mkDerivation {
   name = "nvidia-x11-${versionNumber}${optionalString (!libsOnly) "-${kernel.version}"}";
@@ -18,12 +18,12 @@ stdenv.mkDerivation {
     if stdenv.system == "i686-linux" then
       fetchurl {
         url = "http://us.download.nvidia.com/XFree86/Linux-x86/${versionNumber}/NVIDIA-Linux-x86-${versionNumber}.run";
-        sha256 = "0vfcfvn59qg53kqkij83pidssfnafd720hrx62cm7jmqzp5vgyvc";
+        sha256 = "06sp7wi1q2sh00r0nm340zpfjhngf4qifflbnk2ai88gb6zrpi54";
       }
     else if stdenv.system == "x86_64-linux" then
       fetchurl {
         url = "http://us.download.nvidia.com/XFree86/Linux-x86_64/${versionNumber}/NVIDIA-Linux-x86_64-${versionNumber}-no-compat32.run";
-        sha256 = "18wqfnmaiz0smf9xbzc3389nwz5lhisq8330bi0z9skwfjlpai2f";
+        sha256 = "0hqp56sf61wc9fn3zdvc0lr8w170p36mmf9irgpz0g96qwnic2z9";
       }
     else throw "nvidia-x11 does not support platform ${stdenv.system}";
 
