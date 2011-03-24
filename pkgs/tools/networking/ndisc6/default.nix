608018188208d98d0ea4e2537b85a181ad3f5845
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-03-24 15:26:11 +0000

    * dnssort requires Perl.
    svn path=/nixpkgs/trunk/; revision=26494

--- pkgs/tools/networking/ndisc6/default.nix
+++ pkgs/tools/networking/ndisc6/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, perl }:
 
 stdenv.mkDerivation rec {
   name = "ndisc6-1.0.1";
@@ -7,6 +7,8 @@ stdenv.mkDerivation rec {
     url = "http://www.remlab.net/files/ndisc6/archive/${name}.tar.bz2";
     sha256 = "1pggc9x3zki1sv08rs8x4fm7pmd3sn1nwkan3szax19xg049xbqx";
   };
+
+  buildInputs = [ perl ];
 
   meta = {
     homepage = http://www.remlab.net/ndisc6/;
