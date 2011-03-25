f477fe9d3d051452612e90fa2539d1259a0de023
Karn Kallio <tierpluspluslists@gmail.com>
2011-03-25 23:14:01 +0000

    Update libssh version to latest release 0.4.8
    svn path=/nixpkgs/trunk/; revision=26519

--- pkgs/development/libraries/libssh/default.nix
+++ pkgs/development/libraries/libssh/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, cmake, zlib, openssl}:
 
 stdenv.mkDerivation {
-  name = "libssh-0.4.1";
+  name = "libssh-0.4.8";
   src = fetchurl {
-    url = http://www.libssh.org/files/0.4/libssh-0.4.1.tar.gz;
-    sha256 = "0f12iyzwc2w5m5y1b6jzr08516vpfwwwrqqd4dkb6b0q2a1axlm6";
+    url = http://www.libssh.org/files/0.4/libssh-0.4.8.tar.gz;
+    sha256 = "05d8i8hwya2gry3lky9pmjpvr9f4wvggszqjjzgxvyy74sj3khhm";
   };
   buildInputs = [ cmake zlib openssl ];
   meta = {
