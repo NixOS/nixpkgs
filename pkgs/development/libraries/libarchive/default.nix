0e5593b0f60aa89e2bc85e9fe7ed26177cf703f4
Peter Simons <simons@cryp.to>
2011-03-25 12:55:22 +0000

    libarchive: don't try to build e2fsprogs, attr, or acl unless running on Linux
    These packages give errors when built on, say, Darwin.
    
    svn path=/nixpkgs/trunk/; revision=26510

--- pkgs/development/libraries/libarchive/default.nix
+++ pkgs/development/libraries/libarchive/default.nix
@@ -9,10 +9,11 @@ stdenv.mkDerivation rec {
     sha256 = "16095d15334b3c8dbb02db5af3d415f12c1c3bdd4eb43af7bbc36ab7572c0b7a";
   };
 
-  propagatedBuildInputs = [acl libxml2 zlib bzip2 e2fsprogs xz attr openssl];
-  
+  propagatedBuildInputs = [libxml2 zlib bzip2 xz openssl] ++
+    (if stdenv.isLinux then [e2fsprogs attr acl] else []);
+
   buildInputs = [sharutils];
-  
+
   meta = {
     description = "A library for reading and writing streaming archives";
     homepage = http://people.freebsd.org/~kientzle/libarchive;
