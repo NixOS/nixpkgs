446b09c477aa1412c8344f566ceed6cd05737aa9
Ludovic Courtès <ludo@gnu.org>
2011-03-25 10:54:17 +0000

    Give less work to Hydra.
    svn path=/nixpkgs/trunk/; revision=26507

--- pkgs/top-level/release.nix
+++ pkgs/top-level/release.nix
@@ -13,7 +13,7 @@ with (import ./release-lib.nix);
 
   tarball = import ./make-tarball.nix;
 
-} // (mapTestOn ((packagesWithMetaPlatform pkgs) // rec {
+} // (mapTestOn (/* (packagesWithMetaPlatform pkgs) // */ rec {
 
   MPlayer = linux;
   abcde = linux;
@@ -380,6 +380,7 @@ with (import ./release-lib.nix);
     jdee = linux;
   };
 
+  /*
   firefox36Pkgs.firefox = linux;
   firefox40Pkgs.firefox = linux;
 
@@ -541,6 +542,7 @@ with (import ./release-lib.nix);
     xfdesktop = linux;
     xfwm4 = linux;
   };
+  */
 
 } ))
 
