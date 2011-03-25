9eb80e4f944a8c9d3ef5ab43542565d2cdef96ab
Karn Kallio <tierpluspluslists@gmail.com>
2011-03-25 15:27:30 +0000

    URL to libssh has changed.
    svn path=/nixpkgs/trunk/; revision=26516

--- pkgs/development/libraries/libssh/default.nix
+++ pkgs/development/libraries/libssh/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation {
   name = "libssh-0.4.1";
   src = fetchurl {
-    url = http://www.libssh.org/files/libssh-0.4.1.tar.gz;
+    url = http://www.libssh.org/files/0.4/libssh-0.4.1.tar.gz;
     sha256 = "0f12iyzwc2w5m5y1b6jzr08516vpfwwwrqqd4dkb6b0q2a1axlm6";
   };
   buildInputs = [ cmake zlib openssl ];
