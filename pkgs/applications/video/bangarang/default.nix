94e19387f3acf673604d40f75a0795aebf8b1bbd
Evgeny Egorochkin <phreedom.stdin@gmail.com>
2011-03-26 18:18:49 +0000

    Bangarang: version bump
    svn path=/nixpkgs/trunk/; revision=26529

--- pkgs/applications/video/bangarang/default.nix
+++ pkgs/applications/video/bangarang/default.nix
@@ -1,10 +1,10 @@
 { stdenv, fetchurl, cmake, qt4, kdelibs, automoc4, phonon, soprano, kdemultimedia, taglib, glibc, gettext }:
 stdenv.mkDerivation rec {
-  name = "bangarang-1.0.1";
+  name = "bangarang-2.0";
 
   src = fetchurl {
     url = "http://bangarangissuetracking.googlecode.com/files/${name}.tar.gz";
-    sha256 = "0a89w6zqyzcb34vp3qmyp1mdm2k0igm71b5sh11xnikjvs3k7c33";
+    sha256 = "1fixqx56k0mk0faz35rzpdg6zaa0mvm4548rg0g7fhafl35fxzlz";
   };
 
   buildInputs = [ cmake qt4 kdelibs automoc4 phonon soprano kdemultimedia taglib glibc gettext ];
