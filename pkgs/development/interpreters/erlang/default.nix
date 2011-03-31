4585c62aec5c62ca031d293ba99dbc77e14fecc0
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-03-31 08:44:11 +0000

    * Erlang updated to R14B02.
    svn path=/nixpkgs/branches/modular-python/; revision=26619

--- pkgs/development/interpreters/erlang/default.nix
+++ pkgs/development/interpreters/erlang/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, perl, gnum4, ncurses, openssl }:
 
-let version = "R14A"; in
+let version = "R14B02"; in
 
 stdenv.mkDerivation {
   name = "erlang-" + version;
   
   src = fetchurl {
     url = "http://www.erlang.org/download/otp_src_" + version + ".tar.gz";
-    sha256 = "170n5p6al1bxwngdmafm1c6892xjxppb96gzgki9gfb0mla6li73";
+    sha256 = "1g85a85w031jr5pmz9b0x3p11d44glkf7qpy64l9y7l2b45hb7c4";
   };
   
   buildInputs = [ perl gnum4 ncurses openssl ];
