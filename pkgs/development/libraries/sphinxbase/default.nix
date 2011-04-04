f1a3b5e937f65b42eb0e2ee9a6fa98abd68f4169
Shea Levy <shea@shealevy.com>
2011-04-04 18:50:30 +0000

    Use pkgconfig and a shorter sourceforge url
    svn path=/nixpkgs/trunk/; revision=26685

--- pkgs/development/libraries/sphinxbase/default.nix
+++ pkgs/development/libraries/sphinxbase/default.nix
@@ -1,12 +1,14 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, pkgconfig }:
 
 stdenv.mkDerivation rec {
   name = "sphinxbase-0.6.1";
 
   src = fetchurl {
-    url = "mirror://sourceforge/cmusphinx/sphinxbase/${name}.tar.gz";
+    url = "mirror://sourceforge/cmusphinx/${name}.tar.gz";
     sha256 = "1blra8igkqbqr7m2izbis1h3kkzblsqj9rkbw0f00025li8i1z55";
   };
+
+  buildInputs = [ pkgconfig ];
 
   meta = {
     description = "Support Library for Pocketsphinx";
