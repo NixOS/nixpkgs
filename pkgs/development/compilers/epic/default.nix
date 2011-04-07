742e6e9e0be79fd902b3e1b680a6beee36d6b140
Andres Löh <mail@andres-loeh.de>
2011-04-07 07:47:22 +0000

    Updated epic.
    svn path=/nixpkgs/trunk/; revision=26728

--- pkgs/development/compilers/epic/default.nix
+++ pkgs/development/compilers/epic/default.nix
@@ -2,10 +2,11 @@
 
 cabal.mkDerivation (self : {
   pname = "epic";
-  version = "0.1.5";
-  sha256 = "5a3d94e88cb85beb3c13f3b9f3c00c6768e1b067ff88d40ea63d9961a92347ff";
+  version = "0.1.10";
+  sha256 = "0iaq0mswf370jk4r2xyby8qa5ihcydnbkr478p35x2i56axp60ji";
   propagatedBuildInputs = [mtl];
   extraBuildInputs = [happy gmp boehmgc];
+  noHaddock = true;
   meta = {
     description = "An experimental language with full dependent types";
     license = "BSD";
