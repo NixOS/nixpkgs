e88ae7fa7d53670721a80b5f277389c3dfa8709b
Karn Kallio <tierpluspluslists@gmail.com>
2011-04-12 16:31:51 +0000

    Pass libgcrypt and gmp to network manager.
    svn path=/nixpkgs/trunk/; revision=26808

--- pkgs/tools/networking/network-manager/default.nix
+++ pkgs/tools/networking/network-manager/default.nix
@@ -1,4 +1,6 @@
-{ stdenv, fetchurl, intltool, wirelesstools, pkgconfig, dbus, dbus_glib, udev, libnl1, libuuid, polkit, gnutls, ppp, dhcp, iptables, libtasn1 }:
+{ stdenv, fetchurl, intltool, wirelesstools, pkgconfig, dbus, dbus_glib
+, udev, libnl1, libuuid, polkit, gnutls, ppp, dhcp, iptables, libtasn1
+, libgcrypt, gmp }:
 stdenv.mkDerivation rec {
 
   name = "network-manager-${version}";
@@ -14,7 +16,7 @@ stdenv.mkDerivation rec {
     "--with-crypto=gnutls" "--disable-more-warnings"
     "--with-udev-dir=\${out}/lib/udev" ];
 
-  buildInputs = [ intltool wirelesstools pkgconfig dbus dbus_glib udev libnl1 libuuid polkit gnutls ppp libtasn1 ];
+  buildInputs = [ intltool wirelesstools pkgconfig dbus dbus_glib udev libnl1 libuuid polkit gnutls ppp libtasn1 libgcrypt gmp ];
 
   meta = with stdenv.lib; {
     homepage = http://projects.gnome.org/NetworkManager/;
