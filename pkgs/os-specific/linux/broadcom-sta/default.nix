564edafa1a0c3c56a9dc49fbccb1c25da8b6cd0c
Shea Levy <shea@shealevy.com>
2011-04-12 15:54:39 +0000

    Get broadcom-sta to work on kernels >= 2.6.37
    Patch from http://forums.linuxmint.com/viewtopic.php?f=141&t=57056&start=20#p378103
    
    svn path=/nixpkgs/trunk/; revision=26806

--- pkgs/os-specific/linux/broadcom-sta/default.nix
+++ pkgs/os-specific/linux/broadcom-sta/default.nix
@@ -15,7 +15,10 @@ stdenv.mkDerivation {
   };
 
   buildInputs = [ kernel ];
-  patches = [ ./makefile.patch ];
+  patches = [ ./makefile.patch ]
+    ++ stdenv.lib.optional
+    (! builtins.lessThan (builtins.compareVersions kernel.version "2.6.37") 0)
+      [ ./mutex-sema.patch ];
 
   makeFlags = "KDIR=${kernel}/lib/modules/${kernel.version}/build";
 
