2eb6b0742794ca462c14026af9bf674acfc697c7
Michael Raskin <7c6f434c@mail.ru>
2011-04-13 06:31:45 +0000

    Update Gambit
    svn path=/nixpkgs/trunk/; revision=26816

--- pkgs/development/compilers/gambit/src-for-default.nix
+++ pkgs/development/compilers/gambit/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="v4_6_0";
-   name="gambit-v4_6_0";
-   hash="1hnczygq25r6hi1l6lh1sd4g382j2z0ji5bdd01lhr4i07dcvipd";
+   version="v4_6_1";
+   name="gambit-v4_6_1";
+   hash="0ad6c63dg5ggaamixxinvlf3128mra8xzss5chh66lkii4dc3m7g";
    url="http://www.iro.umontreal.ca/~gambit/download/gambit/v4.6/source/gambc-${version}-devel.tgz";
-   advertisedUrl="http://www.iro.umontreal.ca/~gambit/download/gambit/v4.6/source/gambc-v4_6_0-devel.tgz";
+   advertisedUrl="http://www.iro.umontreal.ca/~gambit/download/gambit/v4.6/source/gambc-v4_6_1-devel.tgz";
   
   
 }
