57fee7d6791bdb03b37bb357b988f09f7d8f179c
Sander van der Burg <s.vanderburg@tudelft.nl>
2011-04-14 15:37:04 +0000

    Bumped tomcat to 6.0.32
    svn path=/nixpkgs/trunk/; revision=26845

--- pkgs/servers/http/tomcat/6.0.nix
+++ pkgs/servers/http/tomcat/6.0.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl }:
 
-let version = "6.0.29"; in
+let version = "6.0.32"; in
 
 stdenv.mkDerivation rec {
   name = "apache-tomcat-${version}";
 
   src = fetchurl {
     url = "mirror://apache/tomcat/tomcat-6/v${version}/bin/${name}.tar.gz";
-    sha256 = "0v96wmd4fnk3qskw32k8mb77f7yssqqinsrf9sir672l5ggmmcjc";
+    sha256 = "505e649d1ffcf746e66be8295c8244a2949349dedf678e9f2a659c4736968c5e";
   };
 
   installPhase =
