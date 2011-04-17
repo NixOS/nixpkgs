250bdbbdb24174622789b9a2e42f250b1972c3a0
Michael Raskin <7c6f434c@mail.ru>
2011-04-17 17:39:17 +0000

    Update veracity
    svn path=/nixpkgs/trunk/; revision=26868

--- pkgs/applications/version-management/veracity/src-for-default.nix
+++ pkgs/applications/version-management/veracity/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="0.7.0.10414";
-   name="veracity-0.7.0.10414";
-   hash="0kaqh2d1zh2vskwz9fw2yrx396knhbjq63h4r72y7cc2izgly21j";
+   version="0.7.0.10439";
+   name="veracity-0.7.0.10439";
+   hash="0baa2zlif6m5m0l0dlmdkzgda6cfvc6mi30c1066cfdz1axa7zjl";
    url="http://download-us.sourcegear.com/Veracity/nightly/veracity-source-${version}.tar.gz";
-   advertisedUrl="http://download-us.sourcegear.com/Veracity/nightly/veracity-source-0.7.0.10414.tar.gz";
+   advertisedUrl="http://download-us.sourcegear.com/Veracity/nightly/veracity-source-0.7.0.10439.tar.gz";
   
   
 }
