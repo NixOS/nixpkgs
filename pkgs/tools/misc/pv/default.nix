1a83a071237c471ba7a9c0d2c8246fde8d48213b
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2011-04-23 20:58:15 +0000

    Updating pv
    svn path=/nixpkgs/trunk/; revision=26937

--- pkgs/tools/misc/pv/default.nix
+++ pkgs/tools/misc/pv/default.nix
@@ -1,16 +1,18 @@
 { stdenv, fetchurl } :
 
 stdenv.mkDerivation {
-  name = "pv-1.1.4";
+  name = "pv-1.2.0";
 
   src = fetchurl {
-    url = http://pipeviewer.googlecode.com/files/pv-1.1.4.tar.bz2;
-    sha256 = "c8613c240ab4297f6ad346f0047138f551a093c603eeb581d5e83091cad3a559";
+    url = http://pipeviewer.googlecode.com/files/pv-1.2.0.tar.bz2;
+    sha256 = "0rn6rpiw7c16pgkhcslax9p1mxkxkmk6ivc9hjmsis7r69niyag3";
   };
 
   meta = {
     homepage = http://www.ivarch.com/programs/pv;
     description = "Tool for monitoring the progress of data through a pipeline";
     license = "free";
+    maintainers = with stdenv.lib.maintainers; [viric];
+    platforms = with stdenv.lib.platforms; all;
   };
 }
