3ae283e8c33a06c24c674a979d2d40bedad71c09
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-04-27 16:16:47 +0000

    * Remove installer2 and nixos-bootstrap-archive for lack of   documentation / maintenance / clear use cases.
    svn path=/nixos/trunk/; revision=27008

--- modules/installer/tools/tools.nix
+++ modules/installer/tools/tools.nix
@@ -35,48 +35,6 @@ let
       "cp refs $out";
   };
 
-  # rewrite of nixosInstall: each tool does exactly one job.
-  # So they get more useful.
-  installer2 =
-  let nixClosure = pkgs.runCommand "closure"
-        {exportReferencesGraph = ["refs" config.environment.nix];}
-        "cp refs $out";
-
-      nix = config.environment.nix;
-  in rec {
-
-    nixosPrepareInstall = makeProg {
-      name = "nixos-prepare-install";
-      src = ./installer2/nixos-prepare-install.sh;
-
-      inherit nix nixClosure nixosBootstrap;
-    };
-
-    runInChroot = makeProg {
-     name = "run-in-chroot";
-       src = ./installer2/run-in-chroot.sh;
-    };
-
-    nixosBootstrap = makeProg {
-      name = "nixos-bootstrap";
-      src = ./installer2/nixos-bootstrap.sh;
-
-      inherit (pkgs) coreutils;
-      inherit nixClosure nix;
-
-      # TODO shell ?
-      nixpkgsURL = cfg.nixpkgsURL;
-    };
-
-    # see ./nixos-bootstrap-archive/README-BOOTSTRAP-NIXOS
-    # TODO refactor: It should *not* depend on configuration.nix
-    # maybe even move this in nixpkgs?
-    minimalInstallArchive = import ./nixos-bootstrap-archive {
-      inherit (pkgs) stdenv runCommand perl pathsFromGraph gnutar coreutils bzip2;
-      inherit nixosPrepareInstall runInChroot nixosBootstrap nixClosure;
-    };
-  };
-
   nixosRebuild = makeProg {
     name = "nixos-rebuild";
     src = ./nixos-rebuild.sh;
@@ -170,19 +128,13 @@ in
         nixosDeployNetwork
         nixosInstall
         nixosRebuild
-         nixosHardwareScan
-         nixosGenSeccureKeys
-         nixosOption
-
-         installer2.runInChroot
-         installer2.nixosPrepareInstall
+        nixosHardwareScan
+        nixosGenSeccureKeys
+        nixosOption
       ] ++ pkgs.lib.optional cfg.enableGraphicalTools nixosGui;
 
     system.build = {
       inherit nixosInstall nixosHardwareScan nixosOption;
-
-      # expose scripts
-      inherit (installer2) nixosPrepareInstall runInChroot nixosBootstrap minimalInstallArchive;
     };
   };
 }
