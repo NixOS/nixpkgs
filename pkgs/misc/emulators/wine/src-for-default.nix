9926941bf27a05ce14e376430117d9551e25b08d
Michael Raskin <7c6f434c@mail.ru>
2011-04-30 19:42:48 +0000

    Fixing forgotten hash for Wine update
    svn path=/nixpkgs/trunk/; revision=27065

--- pkgs/misc/emulators/wine/src-for-default.nix
+++ pkgs/misc/emulators/wine/src-for-default.nix
@@ -1,7 +1,7 @@
 rec {
    version="1.3.19";
    name="wine-1.3.19";
-   hash="";
+   hash="13g7wk4xx5vxr5hq3jn69iyrscvvbz5r58l9y11ffsyp11y7g1l6";
    url="http://prdownloads.sourceforge.net/wine/wine-${version}.tar.bz2";
    advertisedUrl="http://prdownloads.sourceforge.net/wine/wine-1.3.19.tar.bz2";
   
