c2c9b1d0119517da7baf363b2331528d99b59832
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-05-03 15:43:08 +0000

    * SQLite updated to 3.7.6.2.  Also enabled the SECURE_DELETE feature   (required by Firefox).
    svn path=/nixpkgs/trunk/; revision=27116

--- pkgs/development/libraries/sqlite/default.nix
+++ pkgs/development/libraries/sqlite/default.nix
@@ -3,18 +3,18 @@
 assert readline != null -> ncurses != null;
 
 stdenv.mkDerivation {
-  name = "sqlite-3.7.5";
+  name = "sqlite-3.7.6.2";
 
   src = fetchurl {
-    url = http://www.sqlite.org/sqlite-autoconf-3070500.tar.gz;
-    sha256 = "151gdb7lhzppccc42wzc27ckf0v25m7j9pfxna15ixn9ds98cnyb";
+    url = http://www.sqlite.org/sqlite-autoconf-3070602.tar.gz;
+    sha256 = "10gwjdjzf8r2k42vlzx79l4bhcj4h5wpjfqpshq75y4g1prndajk";
   };
 
   buildInputs = [ readline ncurses ];
   
   configureFlags = "--enable-threadsafe";
 
-  NIX_CFLAGS_COMPILE = "-DSQLITE_ENABLE_COLUMN_METADATA=1";
+  NIX_CFLAGS_COMPILE = "-DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_SECURE_DELETE=1";
   NIX_CFLAGS_LINK = if readline != null then "-lncurses" else "";
 
   meta = {
