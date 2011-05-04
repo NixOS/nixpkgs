e88567a0dbc4bbcb4ca8d930b2e356b377b335ef
Peter Simons <simons@cryp.to>
2011-05-04 13:21:26 +0000

    git-annex: added missing dataenc dependency
    svn path=/nixpkgs/trunk/; revision=27144

--- pkgs/applications/version-management/git-and-tools/default.nix
+++ pkgs/applications/version-management/git-and-tools/default.nix
@@ -47,7 +47,7 @@ rec {
 
   gitAnnex = lib.makeOverridable (import ./git-annex) {
     inherit stdenv fetchurl libuuid rsync findutils curl perl;
-    inherit (haskellPackages) ghc MissingH utf8String QuickCheck2 pcreLight SHA;
+    inherit (haskellPackages) ghc MissingH utf8String QuickCheck2 pcreLight SHA dataenc;
   };
 
   qgit = import ./qgit {
