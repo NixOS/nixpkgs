2ff77d4134d480afabd7680daa8e1c938c5de634
Rob Vermaas <rob.vermaas@gmail.com>
2011-05-04 10:14:02 +0000

    fixed hash
    svn path=/nixpkgs/trunk/; revision=27137

--- pkgs/applications/version-management/git-and-tools/svn2git/default.nix
+++ pkgs/applications/version-management/git-and-tools/svn2git/default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
   src = fetchgit {
     url = http://git.gitorious.org/svn2git/svn2git.git;
     rev = "197979b6a641b8b5fa4856c700b1235491c73a41";
-    sha256 = "fce3749b3db4940060c6065e927248afe18fd698f30ded6cddba201b67e5351c";
+    sha256 = "7be1a8f5822aff2d4ea7f415dce0b4fa8c6a82310acf24e628c5f1ada2d2d613";
   };
 
   buildPhase = ''
