ae4ea00df0c0a57ce57eb1e1d62d90f7e9f3c3a1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-05-04 14:09:44 +0000

    * obex-data-server updated to 0.4.6.
    svn path=/nixpkgs/trunk/; revision=27145

--- pkgs/tools/bluetooth/obex-data-server/default.nix
+++ pkgs/tools/bluetooth/obex-data-server/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, libusb, glib, dbus_glib, bluez, openobex }:
    
 stdenv.mkDerivation rec {
-  name = "obex-data-server-0.4.5";
+  name = "obex-data-server-0.4.6";
    
   src = fetchurl {
     url = "http://tadas.dailyda.com/software/${name}.tar.gz";
-    sha256 = "0qy7mrwr3xfplcxlrq97hiiyda7r9jn24015y9azahi7q5xjfhg7";
+    sha256 = "0kq940wqs9j8qjnl58d6l3zhx0jaszci356xprx23l6nvdfld6dk";
   };
 
   buildInputs = [ pkgconfig libusb glib dbus_glib bluez openobex ];
