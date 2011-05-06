45859265b0638905352166a06518cfd87fea77c6
Florian Friesdorf <flo@chaoflow.net>
2011-05-06 21:49:12 +0000

    gmime 2.4.24
    svn path=/nixpkgs/trunk/; revision=27174

--- pkgs/development/libraries/gmime/default.nix
+++ pkgs/development/libraries/gmime/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, glib, zlib }:
 
 stdenv.mkDerivation rec {
-  name = "gmime-2.4.22";
+  name = "gmime-2.4.24";
   
   src = fetchurl {
     url = "mirror://gnome/sources/gmime/2.4/${name}.tar.gz";
-    sha256 = "0s93amrj7fh3p8lv770p7mrml07m6dps6srwi1wn76d2rfb944xn";
+    sha256 = "2f538d68e215f075d16575a6da9acb87983db9e2df0d7d403858048881a0dd15";
   };
   
   buildInputs = [ pkgconfig glib zlib ];
@@ -13,5 +13,6 @@ stdenv.mkDerivation rec {
   meta = {
     homepage = http://spruce.sourceforge.net/gmime/;
     description = "A C/C++ library for manipulating MIME messages";
+    maintainers = [ stdenv.lib.maintainers.chaoflow ];
   };
 }
