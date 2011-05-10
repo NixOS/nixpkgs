d72ba421879e55c9fa81083943848f75a5593635
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-05-10 10:35:20 +0000

    * Revert to GRUB 1 as the default.  The switch to GRUB 2 seems to   cause more problems than expected, so we should probably think about   the proper migration path for a while.  In the meantime, of course,   everybody is encouraged to set boot.loader.grub.version = 2.
    svn path=/nixos/trunk/; revision=27209

--- modules/installer/grub/grub.nix
+++ modules/installer/grub/grub.nix
@@ -35,8 +35,8 @@ in
       };
 
       version = mkOption {
-        default = 2;
-        example = 1;
+        default = 1;
+        example = 2;
         description = ''
           The version of GRUB to use: <literal>1</literal> for GRUB Legacy
           (versions 0.9x), or <literal>2</literal> for GRUB 2.
