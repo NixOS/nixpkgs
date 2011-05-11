faf10f25b56da617c94a187ed05c014c4120215d
Peter Simons <simons@cryp.to>
2011-05-11 12:28:23 +0000

    nbd: enable doCheck
    The test suite runs fine on my Linux machine, but let's wait and see what Hydra
    has to say about that.
    
    svn path=/nixpkgs/trunk/; revision=27221

--- pkgs/tools/networking/nbd/default.nix
+++ pkgs/tools/networking/nbd/default.nix
@@ -13,6 +13,7 @@ stdenv.mkDerivation {
 
   buildInputs = [pkgconfig glib];
   postInstall = ''install -D -m 444 README "$out/share/doc/nbd/README"'';
+  doCheck = true;
 
   # Glib calls `clock_gettime', which is in librt.  Since we're using
   # a static Glib, we need to pass it explicitly.
