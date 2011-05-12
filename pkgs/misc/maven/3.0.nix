77db7530655624df55b612dd8bc1f181d35b9aae
Rob Vermaas <rob.vermaas@gmail.com>
2011-05-12 08:39:09 +0000

    update maven3
    svn path=/nixpkgs/trunk/; revision=27233

--- pkgs/misc/maven/3.0.nix
+++ pkgs/misc/maven/3.0.nix
@@ -3,11 +3,11 @@
 assert jdk != null;
 
 stdenv.mkDerivation {
-	name = "apache-maven-3.0";
+	name = "apache-maven-3.0.3";
 	builder = ./builder.sh;
 	src = fetchurl {
-		url = mirror://apache/maven/binaries/apache-maven-3.0-bin.tar.gz;
-		sha256 = "18i7vf7w79pvga4k0plixv2ppdvm476cgikaxxnar1fac5v0qsh4";
+		url = mirror://apache/maven/binaries/apache-maven-3.0.3-bin.tar.gz;
+		sha256 = "b845479bd5d6d821210d3530c65da628a118abedd176492741e1d9bc5e400e2a";
 	};
 	
         buildInputs = [makeWrapper]; 
