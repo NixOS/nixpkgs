e2897bdd5209f9678b9a4fc21458ea185344ff54
Sander van der Burg <s.vanderburg@tudelft.nl>
2011-05-13 13:23:13 +0000

    Bumped versions of disnix and disnixos to deal with recent NixOS changes
    svn path=/nixpkgs/trunk/; revision=27248

--- pkgs/tools/package-management/disnix/default.nix
+++ pkgs/tools/package-management/disnix/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, pkgconfig, dbus_glib, libxml2, libxslt, getopt, nixUnstable, gettext, libiconv}:
 
 stdenv.mkDerivation {
-  name = "disnix-0.2.1";
+  name = "disnix-0.3pre27244";
   src = fetchurl {
-    url = http://hydra.nixos.org/build/923387/download/4/disnix-0.2.1.tar.gz;
-    sha256 = "1kablxlg7mxajg2r96im2xr3xjgwmqi2gahxyhn2mvb6555dn5h8";
+    url = http://hydra.nixos.org/build/1083290/download/4/disnix-0.3pre27244.tar.gz;
+    sha256 = "1x7y34mxs26k019y9y8fsnzdk7wmjqfmybw99qgqqxy5kblvlzns";
   };
   buildInputs = [ pkgconfig dbus_glib libxml2 libxslt getopt nixUnstable ]
                 ++ stdenv.lib.optional (!stdenv.isLinux) libiconv
--- pkgs/tools/package-management/disnix/disnixos/default.nix
+++ pkgs/tools/package-management/disnix/disnixos/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, disnix, socat, pkgconfig}:
 
 stdenv.mkDerivation {
-  name = "disnixos-0.1";
+  name = "disnixos-0.2pre27247";
   src = fetchurl {
-    url = http://hydra.nixos.org/build/910925/download/3/disnixos-0.1.tar.gz;
-    sha256 = "0gd0jnc8n50g55lv4ha9nim9s2gv7mi4qdz4j3rnaws86sfgh8x2";
+    url = http://hydra.nixos.org/build/1083317/download/3/disnixos-0.2pre27247.tar.gz;
+    sha256 = "0m13zkfkssqalk9z67n1d5dg0xyfjbkb8v033dc2da07d4pdh3vq";
   };
   buildInputs = [ socat pkgconfig disnix ];
   dontStrip = true;
