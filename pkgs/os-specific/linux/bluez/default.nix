d1a44b3a8c6ae02d2a47285725c624dc064b0d65
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2011-05-20 22:21:34 +0000

    Making bluez install some of its test tools. Mainly, the bluez-simple-agent, that can do a pincode handshake with the user.
    svn path=/nixpkgs/trunk/; revision=27284

--- pkgs/os-specific/linux/bluez/default.nix
+++ pkgs/os-specific/linux/bluez/default.nix
@@ -1,6 +1,12 @@
-{ stdenv, fetchurl, pkgconfig, dbus, glib, libusb, alsaLib }:
+{ stdenv, fetchurl, pkgconfig, dbus, glib, libusb, alsaLib, python, makeWrapper
+, pythonDBus, pygobject }:
 
 assert stdenv.isLinux;
+
+let
+  pythonpath = "${pythonDBus}/lib/${python.libPrefix}/site-packages:"
+    + "${pygobject}/lib/${python.libPrefix}/site-packages";
+in
    
 stdenv.mkDerivation rec {
   name = "bluez-4.69";
@@ -10,12 +16,21 @@ stdenv.mkDerivation rec {
     sha256 = "1h4fp6l1sflc0l5vg90hzvgldlwv7rqc4cbn2z6axmxv969pmrhh";
   };
 
-  buildInputs = [ pkgconfig dbus.libs glib libusb alsaLib ];
+  buildInputs = [ pkgconfig dbus.libs glib libusb alsaLib python makeWrapper ];
 
   configureFlags = "--localstatedir=/var";
 
   # Work around `make install' trying to create /var/lib/bluetooth.
   installFlags = "statedir=$(TMPDIR)/var/lib/bluetooth";
+
+  postInstall = ''
+    pushd test
+    for a in simple-agent test-adapter test-device; do
+      cp $a $out/bin/bluez-$a
+      wrapProgram $out/bin/bluez-$a --prefix PYTHONPATH : ${pythonpath}
+    done
+    popd
+  '';
 
   meta = {
     homepage = http://www.bluez.org/;
