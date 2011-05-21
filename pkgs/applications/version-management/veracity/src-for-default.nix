f2788f28376aa98d6fd71d5af8277591cfd2ac64
Michael Raskin <7c6f434c@mail.ru>
2011-05-21 04:50:01 +0000

    missing hash
    svn path=/nixpkgs/trunk/; revision=27286

--- pkgs/applications/version-management/veracity/src-for-default.nix
+++ pkgs/applications/version-management/veracity/src-for-default.nix
@@ -1,7 +1,7 @@
 rec {
    version="0.9.0.10466";
    name="veracity-0.9.0.10466";
-   hash="";
+   hash="19mi6pw4cy4f0w7v7myx0i1ajy40i9pgkmgr9rvfilcyi9rlvj6q";
    url="http://download-us.sourcegear.com/Veracity/nightly/veracity-source-${version}.tar.gz";
    advertisedUrl="http://download-us.sourcegear.com/Veracity/nightly/veracity-source-0.9.0.10466.tar.gz";
   
