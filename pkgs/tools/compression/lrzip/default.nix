074ef3fd859449434953eb7a73addefc8ef1faed
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2011-05-25 22:19:53 +0000

    Updating lrzip
    svn path=/nixpkgs/trunk/; revision=27304

--- pkgs/tools/compression/lrzip/default.nix
+++ pkgs/tools/compression/lrzip/default.nix
@@ -1,16 +1,14 @@
-{stdenv, fetchurl, zlib, lzo, bzip2, nasm}:
+{stdenv, fetchurl, zlib, lzo, bzip2, nasm, perl}:
 
 stdenv.mkDerivation rec {
-  name = "lrzip-0.44";
+  name = "lrzip-0.606";
 
   src = fetchurl {
     url = "http://ck.kolivas.org/apps/lrzip/${name}.tar.bz2";
-    sha256 = "1ncr6igs8v6yxp60sgb9h4ra8wb7jzbxiyj4a9m4nrxyw8fwm2iv";
+    sha256 = "03qxqs7y868w2kfpa84xmhdnh3b4j9x29g4hkzyrg8f4cxgkcv8k";
   };
 
-  NIX_CFLAGS_COMPILE = "-isystem ${zlib}/include";
-
-  buildInputs = [ zlib lzo bzip2 nasm ];
+  buildInputs = [ zlib lzo bzip2 nasm perl ];
 
   meta = {
     homepage = http://ck.kolivas.org/apps/lrzip/;
