bd1459a55785a0438b036c3602a2617bce6b17a4
Ludovic Courtès <ludo@gnu.org>
2011-05-30 21:49:50 +0000

    GNU SIP Witch 1.0.2.
    svn path=/nixpkgs/trunk/; revision=27329

--- pkgs/servers/sip/sipwitch/default.nix
+++ pkgs/servers/sip/sipwitch/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, pkgconfig, ucommon, libosip, libexosip, gnutls, zlib }:
 
 stdenv.mkDerivation rec {
-  name = "sipwitch-0.10.5";
+  name = "sipwitch-1.0.2";
 
   src = fetchurl {
     url = "mirror://gnu/sipwitch/${name}.tar.gz";
-    sha256 = "0zsdlrri6nph8da9rb2rmr4xgj79hn2ysrfxpnil699qv7qnpb0v";
+    sha256 = "0pwq20f9g3hfgxrpvj7k28qy4lmzigvwbh50p5h22gqmn0hgh7ls";
   };
 
   buildInputs = [ pkgconfig ucommon libosip libexosip gnutls zlib ];
