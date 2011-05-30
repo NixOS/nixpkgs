1323580c9a85da93d04bd5e8eb726eb1e3120620
Ludovic Courtès <ludo@gnu.org>
2011-05-30 21:49:12 +0000

    GNU Scientific Library 1.15.
    svn path=/nixpkgs/trunk/; revision=27323

--- pkgs/development/libraries/gsl/default.nix
+++ pkgs/development/libraries/gsl/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv }:
 
 stdenv.mkDerivation rec {
-  name = "gsl-1.14";
+  name = "gsl-1.15";
 
   src = fetchurl {
     url = "mirror://gnu/gsl/${name}.tar.gz";
-    sha256 = "1yzsazwz7w9pckh7y0mnbl75ykfcv20426vrjwxwgrx1v6plfjix";
+    sha256 = "18qf6jzz1r3mzb5qynywv4xx3z9g61hgkbpkdrhbgqh2g7jhgfc5";
   };
 
   doCheck = true;
