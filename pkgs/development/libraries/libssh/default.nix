ee7583d68544bf98556f1a988b982da15cd1185d
Yury G. Kudryashov <urkud.urkud@gmail.com>
2011-06-07 21:49:42 +0000

    libssh-0.4.8
    svn path=/nixpkgs/trunk/; revision=27378

--- pkgs/development/libraries/libssh/default.nix
+++ pkgs/development/libraries/libssh/default.nix
@@ -1,15 +1,16 @@
 {stdenv, fetchurl, cmake, zlib, openssl}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "libssh-0.4.8";
   src = fetchurl {
-    url = http://www.libssh.org/files/0.4/libssh-0.4.8.tar.gz;
+    url = "http://www.libssh.org/files/0.4/${name}.tar.gz";
     sha256 = "05d8i8hwya2gry3lky9pmjpvr9f4wvggszqjjzgxvyy74sj3khhm";
   };
   buildInputs = [ cmake zlib openssl ];
   meta = {
     description = "SSH client library";
     license = "LGPL";
-    maintainers = [ stdenv.lib.maintainers.sander ]; 
+    maintainers = with stdenv.lib.maintainers; [ sander urkud ];
+    platforms = stdenv.lib.platforms.all;
   };
 }
