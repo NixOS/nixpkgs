0ccfc552f5b04f954d8feeb739933ba674056a28
Rob Vermaas <rob.vermaas@gmail.com>
2011-06-09 13:43:02 +0000

    update hydra
    svn path=/nixpkgs/trunk/; revision=27403

--- pkgs/development/tools/misc/hydra/default.nix
+++ pkgs/development/tools/misc/hydra/default.nix
@@ -34,10 +34,10 @@ in
 
 stdenv.mkDerivation rec {
   name = "hydra-${version}";
-  version = "0.1pre26321";
+  version = "0.1pre27262";
   src = fetchurl {
-    url = http://hydra.nixos.org/build/976452/download/2/hydra-0.1pre26321.tar.gz;
-    sha256 = "1fzvvyad3ggms7qyy6x0xgcpj9w5zq11nmfpsx7m6z0xgilg3lil";
+    url = http://hydra.nixos.org/build/1084383/download/2/hydra-0.1pre27262.tar.gz;
+    sha256 = "5f33bcec5ea31970673ade07afd411ef466c6540d0584ed7fd84a945e1867b73";
   };
 
   configureFlags = "--with-nix=${nix}";
