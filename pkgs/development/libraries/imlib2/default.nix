6dd9fd054e39d19087f52c9fb2bf263f5ec0fa98
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2011-06-15 10:30:08 +0000

    updating imlib2. This makes feh load tiffs correctly.
    svn path=/nixpkgs/trunk/; revision=27467

--- pkgs/development/libraries/imlib2/default.nix
+++ pkgs/development/libraries/imlib2/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, x11, libjpeg, libtiff, libungif, libpng, bzip2 }:
 
 stdenv.mkDerivation {
-  name = "imlib2-1.3.0";
+  name = "imlib2-1.4.4";
   
   src = fetchurl {
-    url = mirror://sourceforge/enlightenment/imlib2-1.3.0.tar.gz;
-    sha256 = "1lrg7haqhmzpdb14cgp9vny5fanlwlyhf5n017v130in297gv1qj";
+    url = mirror://sourceforge/enlightenment/imlib2-1.4.4.tar.gz;
+    sha256 = "163162aifak8ya17brzqwjlr8ywz40s2s3573js5blcc1g4m5pm4";
   };
   
   buildInputs = [ x11 libjpeg libtiff libungif libpng bzip2 ];
