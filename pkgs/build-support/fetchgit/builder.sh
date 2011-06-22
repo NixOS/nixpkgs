c8f12bd8be0fa2682ac013f8d22698f2ae68d3fb
Florian Friesdorf <flo@chaoflow.net>
2011-06-22 21:01:36 +0000

    fetchgit builder comment: can handle remotes without HEAD
    thx civodul for bringing it up and testing
    
    svn path=/nixpkgs/trunk/; revision=27523

--- pkgs/build-support/fetchgit/builder.sh
+++ pkgs/build-support/fetchgit/builder.sh
@@ -1,6 +1,7 @@
 # tested so far with:
 # - no revision specified and remote has a HEAD which is used
 # - revision specified and remote has a HEAD
+# - revision specified and remote without HEAD
 source $stdenv/setup
 
 header "exporting $url (rev $rev) into $out"
