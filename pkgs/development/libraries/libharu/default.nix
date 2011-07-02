4f854226f5fb4496e8bb817a02c1a37bf1438025
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2011-07-02 14:16:06 +0000

    Updating libharu. I renamed it from haru to libharu; I prefer libharu for the name.
    svn path=/nixpkgs/trunk/; revision=27578

--- pkgs/development/libraries/libharu/default.nix
+++ pkgs/development/libraries/libharu/default.nix
@@ -1,19 +1,19 @@
 { stdenv, fetchurl, zlib, libpng }:
 
 stdenv.mkDerivation {
-  name = "haru-2.1.0";
+  name = "libharu-2.2.1";
 
   src = fetchurl {
-    url = http://libharu.org/files/libharu-2.1.0.tar.bz2;
-    sha256 = "07mbvw41jwrapc8jwhi385jpr5b3wx6kah41mkxkifvc06y2141r";
+    url = http://libharu.org/files/libharu-2.2.1.tar.bz2;
+    sha256 = "04493rjb4z8f04p3kjvnya8phg4b0vzy3mbdbp8jfy0dhvqg4h4j";
   };
 
   configureFlags = "--with-zlib=${zlib} --with-png=${libpng}";
 
   buildInputs = [ zlib libpng ];
 
-  meta = { 
-    description = "cross platform, open source library for generating PDF files";
+  meta = {
+    description = "Cross platform, open source library for generating PDF files";
     homepage = http://libharu.org/wiki/Main_Page;
     license = "ZLIB/LIBPNG"; # see README.
     maintainers = [ stdenv.lib.maintainers.marcweber ];
