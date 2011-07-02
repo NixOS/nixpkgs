cbb0e6278021cfdd2663bbc8749cab1b2dcccc8a
Karn Kallio <tierpluspluslists@gmail.com>
2011-07-02 19:19:48 +0000

    Advance lftp to version 4.3.1
    svn path=/nixpkgs/trunk/; revision=27579

--- pkgs/tools/networking/lftp/default.nix
+++ pkgs/tools/networking/lftp/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, gnutls, libtasn1, pkgconfig, readline, zlib, xz}:
 
 stdenv.mkDerivation rec {
-  name = "lftp-4.2.3";
+  name = "lftp-4.3.1";
 
   src = fetchurl {
     url = "ftp://ftp.cs.tu-berlin.de/pub/net/ftp/lftp/${name}.tar.xz";
-    sha256 = "0l58lw2xgx9y64kz6mjd8ggdx92yr05rp08zssvsn8wx1vlvg6bh";
+    sha256 = "0v3591fknmimarzk5icm0qxdcfzfckwi2drh165vsiggmj590iyx";
   };
 
   buildInputs = [gnutls libtasn1 pkgconfig readline zlib xz];
