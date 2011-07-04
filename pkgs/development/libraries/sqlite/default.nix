cefbc72a356cc5a998e1b138ec46d204ac34aa85
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-07-04 15:05:12 +0000

    * Oops.
    svn path=/nixpkgs/trunk/; revision=27601

--- pkgs/development/libraries/sqlite/default.nix
+++ pkgs/development/libraries/sqlite/default.nix
@@ -14,7 +14,7 @@ stdenv.mkDerivation {
   
   configureFlags = "--enable-threadsafe";
 
-  NIX_CFLAGS_COMPILE = "-DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_SECURE_DELETE=1 -DSQLITE_SECURE_DELETE=1";
+  NIX_CFLAGS_COMPILE = "-DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_SECURE_DELETE=1 -DSQLITE_ENABLE_UNLOCK_NOTIFY=1";
   NIX_CFLAGS_LINK = if readline != null then "-lncurses" else "";
 
   meta = {
