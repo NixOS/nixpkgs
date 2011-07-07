389fc6e32a1f87a517ebd4d22bb399df885d8cce
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-07-07 20:03:36 +0000

    * libdbusmenu-qt updated to 0.8.2.
    svn path=/nixpkgs/branches/kde-4.7/; revision=27642

--- pkgs/development/libraries/libdbusmenu-qt/default.nix
+++ pkgs/development/libraries/libdbusmenu-qt/default.nix
@@ -1,18 +1,19 @@
-{ stdenv, fetchurl, qt4, cmake }:
+{ stdenv, fetchurl, qt4, cmake, doxygen }:
 
 let
   baseName = "libdbusmenu-qt";
-  v = "0.7.0";
+  v = "0.8.2";
 in
+
 stdenv.mkDerivation rec {
   name = "${baseName}-${v}";
 
   src = fetchurl {
     url = "http://launchpad.net/${baseName}/trunk/${v}/+download/${name}.tar.bz2";
-    sha256 = "0zn3w57xjk34j08fx4n757kakkd4y07halrnaj4a0x0c9dhyjf14";
+    sha256 = "0fazwj4nhh9lr7zwz9ih20i6w60zlni3hcdwj6kz6521bkr8zg2s";
   };
 
-  buildInputs = [ cmake qt4 ];
+  buildInputs = [ cmake qt4 doxygen ];
   
   meta = with stdenv.lib; {
     description = "Provides a Qt implementation of the DBusMenu spec";
