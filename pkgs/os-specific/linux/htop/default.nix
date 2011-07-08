f15dca4c9c711a07a4187588338686258821b2bf
Peter Simons <simons@cryp.to>
2011-07-08 22:45:28 +0000

    htop: updated to version 0.9
    svn path=/nixpkgs/trunk/; revision=27680

--- pkgs/os-specific/linux/htop/default.nix
+++ pkgs/os-specific/linux/htop/default.nix
@@ -1,12 +1,18 @@
 {fetchurl, stdenv, ncurses}:
 
-stdenv.mkDerivation rec {
-  name = "htop-0.8.3";
+let
+  name = "htop-0.9";
+in
+stdenv.mkDerivation {
+  inherit name;
+
   src = fetchurl {
     url = "mirror://sourceforge/htop/${name}.tar.gz";
-    sha256 = "f03bac8999c57c399cbf4332831bcce905d0393d0f97f7e32a1407b48890dd9d";
+    sha256 = "4de65c38e1886bccd30ed692b30eb9bf195240680781bfe1eaf5faf84ee6fbfd";
   };
+
   buildInputs = [ncurses];
+
   meta = {
     description = "An interactive process viewer for Linux";
     homepage = "http://htop.sourceforge.net";
