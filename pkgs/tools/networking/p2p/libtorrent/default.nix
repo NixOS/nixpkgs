21691b47d70cbdcb5ff5a97b184b0025fe066fdc
Peter Simons <simons@cryp.to>
2011-07-18 14:34:33 +0000

    libtorrent: updated to version 0.12.9
    svn path=/nixpkgs/trunk/; revision=27823

--- pkgs/tools/networking/p2p/libtorrent/default.nix
+++ pkgs/tools/networking/p2p/libtorrent/default.nix
@@ -1,19 +1,23 @@
 { stdenv, fetchurl, pkgconfig, openssl, libsigcxx }:
 
-stdenv.mkDerivation rec {
-  pname = "libtorrent";
-  version = "0.12.6";
+let
+  version = "0.12.9";
+in
+stdenv.mkDerivation {
+  name = "libtorrent-${version}";
 
-  name = "${pname}-${version}";
-  
   src = fetchurl {
-    url = "http://libtorrent.rakshasa.no/downloads/${name}.tar.gz";
-    sha256 = "0abisz3jgfv4zmyzbsknzvz9hwakxxpdgfl33qk0aslnchqz60kv";
+    url = "http://libtorrent.rakshasa.no/downloads/libtorrent-${version}.tar.gz";
+    sha256 = "25dc9e8dd45d070f447e599bef08ef0ca421bac6e7f55e608dcd19360594af64";
   };
 
   buildInputs = [ pkgconfig openssl libsigcxx ];
-  
+
   meta = {
+    homepage = "http://libtorrent.rakshasa.no/";
     description = "A BitTorrent library written in C++ for *nix, with a focus on high performance and good code";
+
+    platforms = stdenv.lib.platforms.unix;
+    maintainers = [ stdenv.lib.maintainers.simons ];
   };
 }
