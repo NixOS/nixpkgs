37e07ad933bb2c2f2cb36ab78b659ca9c8b62b07
Peter Simons <simons@cryp.to>
2011-07-20 18:20:28 +0000

    pandoc: updated to version 1.8.1.2
    svn path=/nixpkgs/trunk/; revision=27850

--- pkgs/development/libraries/haskell/pandoc/default.nix
+++ pkgs/development/libraries/haskell/pandoc/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self : {
   pname = "pandoc";
-  version = "1.8.1.1";
-  sha256 = "0g680j8x3v19wcb9k2dxgrdrjr48w0vhvlspfzgw0sgzrgfmaqsj";
+  version = "1.8.1.2";
+  sha256 = "93b23b7ff91ac6d91c8b3945175967fa4da5a5587c1147a19a1a20c8d61b734d";
   propagatedBuildInputs =
     [HTTP citeprocHs dlist tagsoup texmath xhtml zipArchive];
   meta = {
