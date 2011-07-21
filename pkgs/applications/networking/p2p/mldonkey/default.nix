2758c07f1ee596cdbb56c856588ed5820d6e9663
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2011-07-21 18:34:41 +0000

    Updating mldonkey. I remove the patch needed for mips, as they put it upstream already.
    svn path=/nixpkgs/trunk/; revision=27885

--- pkgs/applications/networking/p2p/mldonkey/default.nix
+++ pkgs/applications/networking/p2p/mldonkey/default.nix
@@ -1,19 +1,17 @@
 {stdenv, fetchurl, ocaml, zlib, bzip2, ncurses, file, gd, libpng }:
 
 stdenv.mkDerivation (rec {
-  name = "mldonkey-3.0.4";
+  name = "mldonkey-3.0.7";
   
   src = fetchurl {
     url = "mirror://sourceforge/mldonkey/${name}.tar.bz2";
-    sha256 = "0wnyi2m7126dq6r5zbqplgknnypzcx0p3q2mxcdfhp94ln7vn3f6";
+    sha256 = "1m5cfx79jiym93bx789lqc1bmwrkz1b1jilhga7d994jsjcw6c2n";
   };
   
   meta = {
     description = "Client for many p2p networks, with multiple frontends";
     homepage = http://mldonkey.sourceforge.net/;
   };
-
-  patches = [ ./gcc44mips64.patch ];
 
   buildInputs = [ ocaml zlib ncurses bzip2 file gd libpng ];
   configureFlags = [ "--disable-gui" ];
