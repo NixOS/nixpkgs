6c6968cf07edc746fb7239ec31553fedec91d2c4
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-07-25 14:33:38 +0000

    * x264 updated to the latest snapshot.
    svn path=/nixpkgs/branches/kde-4.7/; revision=27941

--- pkgs/development/libraries/x264/default.nix
+++ pkgs/development/libraries/x264/default.nix
@@ -1,12 +1,12 @@
 {stdenv, fetchurl, yasm}:
 
 stdenv.mkDerivation rec {
-  version = "snapshot-20110416-2245-stable";
+  version = "snapshot-20110724-2245-stable";
   name = "x264-${version}";
 
   src = fetchurl {
     url = "ftp://ftp.videolan.org/pub/videolan/x264/snapshots/x264-${version}.tar.bz2";
-    sha256 = "17bbczmsln6wmw7vwjmmr18bhngj1b2xfr9fq3a3n54706df9370";
+    sha256 = "07bylkh8cwcmj01sr41hhrvfbciyixhw1irdpj01kz9d0h8dhhpz";
   };
 
   patchPhase = ''
