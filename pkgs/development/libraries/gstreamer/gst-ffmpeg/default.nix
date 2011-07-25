ac9579626ff7c000f879f8355c450cb7512a5671
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-07-25 20:10:13 +0000

    * Updated gst-plugins-good and gst-ffmpeg.
    svn path=/nixpkgs/branches/kde-4.7/; revision=27951

--- pkgs/development/libraries/gstreamer/gst-ffmpeg/default.nix
+++ pkgs/development/libraries/gstreamer/gst-ffmpeg/default.nix
@@ -1,27 +1,29 @@
-{ fetchurl, stdenv, pkgconfig, gstPluginsBase, bzip2, liboil }:
+{ fetchurl, stdenv, pkgconfig, gstPluginsBase, bzip2, yasm
+, useInternalFfmpeg ? false, ffmpeg ? null }:
 
 stdenv.mkDerivation rec {
-  name = "gst-ffmpeg-0.10.11";
+  name = "gst-ffmpeg-0.10.12";
 
   src = fetchurl {
     urls = [
       "http://gstreamer.freedesktop.org/src/gst-ffmpeg/${name}.tar.bz2"
       "mirror://gentoo/distfiles/${name}.tar.bz2"
       ];
-    sha256 = "0bk9k9sccx9nvhjakacvq8gd6vp63x9ddmjrqkfdhkmgwlwa2dpz";
+    sha256 = "0fyppl8q18g71jd2r0mbiqk8hhrdxq43dglma06mxyjb5c80fxxi";
   };
 
-  propagatedBuildInputs = [ gstPluginsBase ];
-  
-  buildInputs = [ pkgconfig bzip2 liboil ];
+  # Upstream strongly recommends against using --with-system-ffmpeg,
+  # but we do it anyway because we're so hardcore (and we don't want
+  # multiple copies of ffmpeg).
+  configureFlags = stdenv.lib.optionalString (!useInternalFfmpeg) "--with-system-ffmpeg";
 
-  configureFlags = "--enable-ladspa";
+  buildInputs =
+    [ pkgconfig bzip2 gstPluginsBase ]
+    ++ (if useInternalFfmpeg then [ yasm ] else [ ffmpeg ]);
 
   meta = {
     homepage = "http://gstreamer.freedesktop.org/releases/gst-ffmpeg";
-
     description = "GStreamer's plug-in using FFmpeg";
-
     license = "GPLv2+";
   };
 }
--- pkgs/development/libraries/gstreamer/gst-plugins-good/default.nix
+++ pkgs/development/libraries/gstreamer/gst-plugins-good/default.nix
@@ -1,26 +1,29 @@
 { fetchurl, stdenv, pkgconfig, gstPluginsBase, aalib, cairo
-, flac, hal, libjpeg, zlib, speex, libpng, libdv, libcaca, dbus
-, libiec61883, libavc1394, ladspaH, taglib, gdbm, pulseaudio
-, gnome, libcap, libtasn1, liboil
+, flac, libjpeg, zlib, speex, libpng, libdv, libcaca
+, libiec61883, libavc1394, taglib, pulseaudio
+, glib, gstreamer, bzip2
 }:
 
 stdenv.mkDerivation rec {
-  name = "gst-plugins-good-0.10.25";
+  name = "gst-plugins-good-0.10.30";
 
   src = fetchurl {
     urls = [
       "${meta.homepage}/src/gst-plugins-good/${name}.tar.bz2"
       "mirror://gentoo/distfiles/${name}.tar.bz2"
       ];
-    sha256 = "1xlr8rzb6pxi91g6jxhqa7cwl0kg5y21qfd9bgc4fb212867bmdm";
+    sha256 = "1xlmw211fcn60y2m5gxrryb3knqril4kk2c01b6j713xna8blb5i";
   };
 
-  propagatedBuildInputs = [gstPluginsBase aalib cairo flac hal libjpeg
-    zlib speex libpng libdv libcaca dbus.libs libiec61883 libavc1394 ladspaH
-    taglib gdbm pulseaudio gnome.libsoup libcap libtasn1 liboil];
-  buildInputs = [pkgconfig];
+  configureFlags = "--disable-oss";
 
-  configureFlags = "--enable-ladspa";
+  buildInputs =
+    [ pkgconfig glib gstreamer gstPluginsBase libavc1394 libiec61883
+      aalib libcaca cairo libdv flac libjpeg libpng pulseaudio speex
+      taglib bzip2
+    ];
+
+  enableParallelBuilding = true;
 
   meta = {
     homepage = http://gstreamer.freedesktop.org;
