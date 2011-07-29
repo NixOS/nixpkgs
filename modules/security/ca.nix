7d69a82b55416bd708ac9f19b291eb24a241d97e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-07-29 19:06:27 +0000

    * Put the CA certificate bundle in /etc/ssl/certs because Qt expects   them there.
    svn path=/nixos/trunk/; revision=28009

--- modules/security/ca.nix
+++ modules/security/ca.nix
@@ -6,15 +6,21 @@ with pkgs.lib;
 
   config = {
 
-    environment.etc = singleton
-      { source = "${pkgs.cacert}/etc/ca-bundle.crt";
-        target = "ca-bundle.crt";
-      };
+    environment.etc =
+      [ { source = "${pkgs.cacert}/etc/ca-bundle.crt";
+          target = "ssl/certs/ca-bundle.crt";
+        }
+
+        # Backward compatibility; may remove at some point.
+        { source = "${pkgs.cacert}/etc/ca-bundle.crt";
+          target = "ca-bundle.crt";
+        }
+      ];
 
     environment.shellInit =
       ''
-        export CURL_CA_BUNDLE=/etc/ca-bundle.crt
-        export GIT_SSL_CAINFO=/etc/ca-bundle.crt
+        export CURL_CA_BUNDLE=/etc/ssl/certs/ca-bundle.crt
+        export GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt
       '';
       
   };
