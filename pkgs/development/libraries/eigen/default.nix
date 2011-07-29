f966079e300c593ea454bcf7874df37cd778eced
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-07-29 16:05:25 +0000

    * Eigen updated to 2.0.16
    svn path=/nixpkgs/branches/kde-4.7/; revision=28004

--- pkgs/development/libraries/eigen/default.nix
+++ pkgs/development/libraries/eigen/default.nix
@@ -1,16 +1,19 @@
 {stdenv, fetchurl, cmake}:
 
 let
-  v = "2.0.15";
+  v = "2.0.16";
 in
 stdenv.mkDerivation {
   name = "eigen-${v}";
+  
   src = fetchurl {
     url = "http://bitbucket.org/eigen/eigen/get/${v}.tar.bz2";
     name = "eigen-${v}.tar.bz2";
-    sha256 = "0frgmkwsgmqaw88qpgvfalisq0wjpcbgqbj6jhpk39nbrs3zcq83";
+    sha256 = "04lzlhl7r1yqkpq9xabm1v5vgz0z9xqf1k7indgwdw8nfyma3qar";
   };
+  
   buildNativeInputs = [ cmake ];
+  
   meta = with stdenv.lib; {
     description = "C++ template library for linear algebra: vectors, matrices, and related algorithms";
     license = licenses.lgpl3Plus;
