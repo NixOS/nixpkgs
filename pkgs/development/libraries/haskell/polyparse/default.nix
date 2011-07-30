4aca0cf2179784df73aa40fbfeb5ac36aa52284d
Peter Simons <simons@cryp.to>
2011-07-30 21:43:39 +0000

    haskell-polyparse: updated to version 1.7
    svn path=/nixpkgs/trunk/; revision=28012

--- pkgs/development/libraries/haskell/polyparse/default.nix
+++ pkgs/development/libraries/haskell/polyparse/default.nix
@@ -1,9 +1,10 @@
-{cabal}:
+{cabal, text}:
 
 cabal.mkDerivation (self : {
   pname = "polyparse";
-  version = "1.4";
-  sha256 = "6e599fb0771e8ce2e1d3a3bbe5eddc2d77b2b4bbb54602f01005dc55dc039d44";
+  version = "1.7";
+  sha256 = "de8ed0ce54f1f81bb0783dd97b7b22eca28df4a238684a26b37c5af2d17a364b";
+  propagatedBuildInputs = [text];
   meta = {
     description = "A variety of alternative parser combinator libraries";
   };
