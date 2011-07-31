53b9044b19cbd35ec556b7e387b244ec1ca24337
Cillian de Roiste <cillian.deroiste@gmail.com>
2011-07-31 10:49:19 +0000

    Updating darktable to 0.9.1
    svn path=/nixpkgs/trunk/; revision=28016

--- pkgs/applications/graphics/darktable/default.nix
+++ pkgs/applications/graphics/darktable/default.nix
@@ -1,27 +1,47 @@
 { stdenv, fetchurl,
-  GConf, cairo, curl, dbus_glib, exiv2, gnome_keyring, gphoto2, gtk,
-  intltool, lcms, lensfun, libexif, libglade, libgphoto2, libjpeg,
-  libpng, libraw1394, librsvg, libtiff, openexr, pkgconfig, sqlite, }:
+  GConf, atk, cairo, cmake, curl, dbus_glib, exiv2, glib,
+  gnome_keyring, gphoto2, gtk, ilmbase, intltool, lcms, lcms2,
+  lensfun, libXau, libXdmcp, libexif, libglade, libgphoto2, libjpeg,
+  libpng, libpthreadstubs, libraw1394, librsvg, libtiff, libxcb,
+  openexr, pixman, pkgconfig, sqlite}:
 
 stdenv.mkDerivation rec {
-  version = "0.7.1";
+  version = "0.9.1";
   name = "darktable-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/darktable/darktable-${version}.tar.gz";
-    sha256 = "080gvf5gp3rb0vlsvdgnzrzky9dfpqw5cwnj6z1k8lvryd9fzahm";
+    sha256 = "b687a5f1b2a6c8aa230c1dc3ef83bf74a103e3ebe1c61cdea95a612a7375f21e";
   };
 
-  patches = [ ./exif.patch ];
+  buildInputs = [
+    GConf atk cairo cmake curl dbus_glib exiv2 glib gnome_keyring gtk
+    ilmbase intltool lcms lcms2 lensfun libXau libXdmcp libexif
+    libglade libgphoto2 libjpeg libpng libpthreadstubs libraw1394
+    librsvg libtiff libxcb openexr pixman pkgconfig sqlite];
 
-  buildInputs = [ GConf cairo curl dbus_glib exiv2 gnome_keyring gtk
-                        intltool lcms lensfun libexif libglade
-                        libgphoto2 libjpeg libpng libraw1394 librsvg
-                        libtiff openexr pkgconfig sqlite ];
+  preConfigure = ''
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${gtk}/include/gtk-2.0"
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${gtk}/lib/gtk-2.0/include"
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${cairo}/include/cairo"
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${atk}/include/atk-1.0"
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${ilmbase}/include/OpenEXR"
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${openexr}/include/OpenEXR"
+  '';
 
-  meta = {
-    description = "a virtual lighttable and darkroom for photographers";
+  cmakeFlags = [
+    "-DPTHREAD_INCLUDE_DIR=${stdenv.glibc}/include"
+    "-DPTHREAD_LIBRARY=${stdenv.glibc}/lib/libpthread.so"
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DGTK2_GLIBCONFIG_INCLUDE_DIR=${glib}/lib/glib-2.0/include"
+    "-DGTK2_GDKCONFIG_INCLUDE_DIR=${gtk}/lib/gtk-2.0/include"
+  ];
+
+  meta = with stdenv.lib; {
+    description = "Virtual lighttable and darkroom for photographers";
     homepage = http://darktable.sourceforge.net;
-    license = stdenv.lib.licenses.gpl3Plus;
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+    maintainers = [ maintainers.goibhniu ];
   };
 }
