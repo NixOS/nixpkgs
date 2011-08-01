6a34912bfb572403c816601332fba9d05bc263e0
Shea Levy <shea@shealevy.com>
2011-08-01 01:10:36 +0000

    zip: Folded builder into derivation
    svn path=/nixpkgs/trunk/; revision=28047

--- pkgs/tools/archivers/zip/default.nix
+++ pkgs/tools/archivers/zip/default.nix
@@ -1,10 +1,13 @@
 {stdenv, fetchurl}: stdenv.mkDerivation {
   name = "zip-2.32";
-  builder = ./builder.sh;
   src = fetchurl {
     url = ftp://ftp.info-zip.org/pub/infozip/src/zip232.tgz;
     md5 = "8a4da4460386e324debe97f3b7fe4d96";
   };
+
+  buildFlags="-f unix/Makefile generic";
+
+  installFlags="-f unix/Makefile prefix=$out INSTALL=cp";
 
   meta = {
     homepage = http://www.info-zip.org;
