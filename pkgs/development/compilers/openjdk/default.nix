11f0ca932042b0e9097580106bb09958c581bd8c
Shea Levy <shea@shealevy.com>
2011-08-01 01:08:07 +0000

    openjdk: Add 'free' to $PATH for the build
    svn path=/nixpkgs/trunk/; revision=28029

--- pkgs/development/compilers/openjdk/default.nix
+++ pkgs/development/compilers/openjdk/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, unzip}:
+{stdenv, fetchurl, unzip, procps}:
 
 stdenv.mkDerivation {
   name = "openjdk-7b127";
@@ -8,5 +8,5 @@ stdenv.mkDerivation {
     sha256 = "1qhwlz9y5qmwmja4qnxg6sn3pgsg1i11fb9j41w8l26acyhk34rs";
   };
 
-  buildInputs = [ unzip ];
+  buildInputs = [ unzip procps ];
 }
