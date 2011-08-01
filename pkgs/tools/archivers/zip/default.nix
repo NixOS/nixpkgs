92e792f17bd428321b2f4e1cc3f2b847424e821f
Shea Levy <shea@shealevy.com>
2011-08-01 01:10:44 +0000

    zip: Change formatting
    svn path=/nixpkgs/trunk/; revision=28048

--- pkgs/tools/archivers/zip/default.nix
+++ pkgs/tools/archivers/zip/default.nix
@@ -1,5 +1,8 @@
-{stdenv, fetchurl}: stdenv.mkDerivation {
+{stdenv, fetchurl}:
+
+stdenv.mkDerivation {
   name = "zip-2.32";
+
   src = fetchurl {
     url = ftp://ftp.info-zip.org/pub/infozip/src/zip232.tgz;
     md5 = "8a4da4460386e324debe97f3b7fe4d96";
@@ -13,3 +16,4 @@
     homepage = http://www.info-zip.org;
   };
 }
+
