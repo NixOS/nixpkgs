3fb602619121f895a47877efc71a77fd9defeb02
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-08-02 06:53:09 +0000

    * Add smartmontools.
    svn path=/nixos/trunk/; revision=28121

--- modules/profiles/base.nix
+++ modules/profiles/base.nix
@@ -69,6 +69,7 @@
     pkgs.sdparm
     pkgs.hdparm
     pkgs.dmraid
+    pkgs.smartmontools # for diagnosing hard disks
 
     # Tools to create / manipulate filesystems.
     pkgs.ntfsprogs # for resizing NTFS partitions
