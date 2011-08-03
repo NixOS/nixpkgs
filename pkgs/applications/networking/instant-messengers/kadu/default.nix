8d546fbdb871c3c67f5c3d3f1d804254c8f642ec
Piotr Pietraszkiewicz <ppietrasa@googlemail.com>
2011-08-03 18:36:56 +0000

    upgraded kadu to v0.9.2 + fixed typos in description
    svn path=/nixpkgs/trunk/; revision=28143

--- pkgs/applications/networking/instant-messengers/kadu/default.nix
+++ pkgs/applications/networking/instant-messengers/kadu/default.nix
@@ -1,33 +1,33 @@
-{ stdenv, fetchurl, cmake, qt4, libgadu, libXScrnSaver, libsndfile, qca2
-, libX11, alsaLib }:
+{ stdenv, fetchurl, cmake, qt4, libgadu, libXScrnSaver, libsndfile, libX11,
+  alsaLib, aspell, libidn, qca2 }:
 
 stdenv.mkDerivation {
 
-  name = "kadu-0.6.5.4";
+  name = "kadu-0.9.2";
 
   src = fetchurl {
-    url = http://www.kadu.net/download/stable/kadu-0.6.5.4.tar.bz2;
-    sha256 = "09bdcyx75lca1pl6x3vfpawmdjn5pfxvwivb9xdhk4bdvswxgv4x";
+    url = http://www.kadu.net/download/stable/kadu-0.9.2.tar.bz2;
+    sha256 = "05lpx7m2adx8vv4h62rsiwlgay24m6cxdbibg7vzl4xkq9ybv30b";
   };
-  
+
   buildInputs = [
-    cmake qt4 libgadu libXScrnSaver libsndfile qca2 libX11 alsaLib
+    cmake qt4 libgadu libXScrnSaver libsndfile libX11 alsaLib aspell libidn qca2
   ];
 
-  cmakeFlags = "-DENABLE_AUTODOWNLOAD=OFF";
+  configureFlags = "CPPFLAGS=-DQT_NO_DEBUG";
+
+  cmakeFlags = "-DENABLE_AUTODOWNLOAD=OFF -DBUILD_DESCRIPTION='NixOS' -DCMAKE_BUILD_TYPE=Release";
 
   prePatch = ''
     patchShebangs .
   '';
-  
-  patches = [ 
-    ./includes.patch
-  ];
+
+  patches = [ ./config.patch ];
 
   NIX_LDFLAGS="-lX11";
 
-  meta = { 
-    description = "An instance messanger client for the gadu-gadu network (most popular polish IM network)";
+  meta = {
+    description = "An instant-messenger client for the gadu-gadu network (most popular polish IM network)";
     homepage = http://www.kadu.net/w/English:Main_Page;
     license = "GPLv2";
     platforms = stdenv.lib.platforms.linux;
