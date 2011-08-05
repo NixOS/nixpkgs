f55fc207e7a874fd94cb6c4eedf6eab9173a0234
Peter Simons <simons@cryp.to>
2011-08-05 18:01:56 +0000

    funcmp: updated build instructions with cabal2nix
    svn path=/nixpkgs/trunk/; revision=28173

--- pkgs/development/libraries/haskell/funcmp/default.nix
+++ pkgs/development/libraries/haskell/funcmp/default.nix
@@ -6,8 +6,10 @@ cabal.mkDerivation (self : {
   sha256 = "f68807833f39178c99877321f0f335cfde12a5c4b38e6c51f33f8cab94b9e12e";
   propagatedBuildInputs = [];
   meta = {
+    homepage = "http://savannah.nongnu.org/projects/funcmp/";
     description = "Functional MetaPost";
     license = "GPL";
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
     maintainers = [self.stdenv.lib.maintainers.simons];
   };
 })
