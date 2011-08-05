bb26c3d1836fae6424c3ee9e3384f83dd317b919
Peter Simons <simons@cryp.to>
2011-08-05 18:02:00 +0000

    hsdns: updated build instructions with cabal2nix
    svn path=/nixpkgs/trunk/; revision=28174

--- pkgs/development/libraries/haskell/hsdns/default.nix
+++ pkgs/development/libraries/haskell/hsdns/default.nix
@@ -5,10 +5,12 @@ cabal.mkDerivation (self : {
   version = "1.5";
   sha256 = "2788d1ad5ef07ae5f356a460fb92316118f3a4d9c779ec27fb8243602bcf6399";
   propagatedBuildInputs = [adns network];
+  noHaddock = true; /* the build fails for reasons I don't understand. */
   meta = {
-    description = "an asynchronous DNS resolver on top of GNU ADNS";
-    license = "LGPL";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    homepage = "http://gitorious.org/hsdns";
+    description = "Asynchronous DNS Resolver";
+    license = self.stdenv.lib.licenses.lgpl3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [self.stdenv.lib.maintainers.simons];
   };
 })
-
