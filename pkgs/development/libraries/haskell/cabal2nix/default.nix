24979c3495d8c303ac8e378b061f9619a4559213
Peter Simons <simons@cryp.to>
2011-08-05 19:41:54 +0000

    cabal2nix: updated to version 1.7
    The new version can read Cabal files via HTTP, and it can also determine
    the hash automatically. Hence, generating a build expression is now a
    matter of running:
    
      cabal2nix http://hackage.haskell.org/packages/archive/cabal2nix/1.7/cabal2nix.cabal
    
    svn path=/nixpkgs/trunk/; revision=28183

--- pkgs/development/libraries/haskell/cabal2nix/default.nix
+++ pkgs/development/libraries/haskell/cabal2nix/default.nix
@@ -1,13 +1,15 @@
-{cabal}:
+{cabal,HTTP}:
 
 cabal.mkDerivation (self : {
   pname = "cabal2nix";
-  version = "1.2";
-  sha256 = "f60dbb36dc73351e1891d2b31ae66c465955c46b87eedb0928bcf61b9a7eff54";
-  propagatedBuildInputs = [];
+  version = "1.7";
+  sha256 = "1inb1rv11dphgvg72zyfmzkcmw7dac1jrc40s7frhvwjhrnr4syv";
+  propagatedBuildInputs = [HTTP];
   meta = {
     homepage = "http://github.com/peti/cabal2nix";
     description = "Convert Cabal files into Nix build instructions";
     license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
