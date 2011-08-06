a751ada1cea6c5afb3f79a61359dd9e0cd28308e
Cillian de Roiste <cillian.deroiste@gmail.com>
2011-08-06 10:33:52 +0000

    Updating oxygen-gtk to 1.1.1
    svn path=/nixpkgs/trunk/; revision=28191

--- pkgs/misc/themes/gtk2/oxygen-gtk/default.nix
+++ pkgs/misc/themes/gtk2/oxygen-gtk/default.nix
@@ -1,17 +1,19 @@
-{ stdenv, fetchurl, 
-  cmake, glib, gtk, pkgconfig }:
+{ stdenv, fetchurl,
+  cmake, dbus_glib, glib, gtk, gtkLibs, pkgconfig, xorg }:
 
 stdenv.mkDerivation rec {
-  version = "1.0.1";
-  name = "oxygen-gtk-${version}";  
+  version = "1.1.1";
+  name = "oxygen-gtk-${version}";
 
   src = fetchurl {
     url = "mirror://kde/stable/oxygen-gtk/${version}/src/${name}.tar.bz2";
-    sha256 = "0ki8qllr5ai48bl2pz8rxzf5cax08ckhgrn0nlf815ba83jfar32";
+    sha256 = "66d571f08ec999f56de412f42a0395c9dc60b73adaaece96c6da6e98353fe379";
   };
-  
-  buildInputs = [ cmake glib gtk pkgconfig ];
-  
+
+  buildInputs = [ cmake dbus_glib glib gtk gtkLibs.gdk_pixbuf
+   pkgconfig xorg.libXau xorg.libXdmcp xorg.libpthreadstubs
+   xorg.libxcb xorg.pixman ];
+
   meta = with stdenv.lib; {
     description = "Port of the default KDE widget theme (Oxygen), to gtk";
     homepage = https://projects.kde.org/projects/playground/artwork/oxygen-gtk;
