ae6fa8071f9d15ced5c98601870876f8dc4f809c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-08-06 23:26:38 +0000

    * Wine updated to 1.3.26.
    svn path=/nixpkgs/trunk/; revision=28209

--- pkgs/misc/emulators/wine/src-for-default.nix
+++ pkgs/misc/emulators/wine/src-for-default.nix
@@ -1,9 +1,9 @@
 rec {
-   version="1.3.25";
-   name="wine-1.3.25";
-   hash="1ahwgq6j8js9gkj8ynar5r1ifn1y1lfkkmhgk8y9fbzxdsb5llpm";
+   version="1.3.26";
+   name="wine-1.3.26";
+   hash="0x2frnggg2nlva1qqx4ysdnhcq9gld0p0516z922rl8c1kcb5bbc";
    url="http://prdownloads.sourceforge.net/wine/wine-${version}.tar.bz2";
-   advertisedUrl="http://prdownloads.sourceforge.net/wine/wine-1.3.25.tar.bz2";
+   advertisedUrl="http://prdownloads.sourceforge.net/wine/wine-1.3.26.tar.bz2";
   
   
 }
