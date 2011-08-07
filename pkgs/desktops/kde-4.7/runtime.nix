b2a20db52274ac4adf61bcfb30b878f5df90013a
Karn Kallio <tierpluspluslists@gmail.com>
2011-08-07 17:50:32 +0000

    Copy kde45 wrap of nepomukservicestub with virtuoso.
    svn path=/nixpkgs/trunk/; revision=28223

--- pkgs/desktops/kde-4.7/runtime.nix
+++ pkgs/desktops/kde-4.7/runtime.nix
@@ -1,5 +1,5 @@
 { automoc4, cmake, kde, kdelibs, qt4, strigi, soprano, shared_desktop_ontologies
-, bzip2, xz, shared_mime_info, libssh, exiv2, attica, phonon, libcanberra
+, bzip2, xz, shared_mime_info, libssh, exiv2, attica, phonon, libcanberra, virtuoso
 }:
 
 kde.package {
@@ -7,9 +7,15 @@ kde.package {
   buildInputs =
     [ phonon cmake kdelibs qt4 automoc4 strigi soprano
       shared_desktop_ontologies bzip2 xz shared_mime_info libssh
-      exiv2 attica
+      exiv2 attica virtuoso
       (libcanberra.override { gtk = null; })
     ];
+
+# Copied from kde45, Nepomuk needs it.
+  postInstall = ''
+    wrapProgram "$out/bin/nepomukservicestub" --prefix LD_LIBRARY_PATH : "${virtuoso}/lib" \
+        --prefix PATH : "${virtuoso}/bin"
+  '';
 
   meta = {
     license = "LGPL";
