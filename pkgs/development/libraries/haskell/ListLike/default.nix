477a7ef458c2448c60ec322a6c36f4f0acb4cfc5
Peter Simons <simons@cryp.to>
2011-08-07 20:25:19 +0000

    haskell-ListLike: updated to version 3.1.1
    svn path=/nixpkgs/trunk/; revision=28284

--- pkgs/development/libraries/haskell/ListLike/default.nix
+++ pkgs/development/libraries/haskell/ListLike/default.nix
@@ -1,12 +1,17 @@
-{cabal}:
+{cabal} :
 
 cabal.mkDerivation (self : {
   pname = "ListLike";
-  version = "3.0.1";
-  sha256 = "1366ipy33fphjjk583c62rsyfwh36i5lbnip1v8r089c9glvwkxf";
+  version = "3.1.1";
+  sha256 = "16q1rsjr9rjlm8iwmr32s1yfcpw0xj34hvb8jfjqyfsail6nh9fh";
   meta = {
+    homepage = "http://software.complete.org/listlike";
     description = "Generic support for list-like structures";
-    license = "BSD";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
