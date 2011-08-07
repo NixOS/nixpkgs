dc3535b0c98659d5935fb221e7a353e26f021ed3
Peter Simons <simons@cryp.to>
2011-08-07 22:00:51 +0000

    SDL-mixer: updated to version 0.6.1
    svn path=/nixpkgs/trunk/; revision=28315

--- pkgs/development/libraries/haskell/SDL-mixer/default.nix
+++ pkgs/development/libraries/haskell/SDL-mixer/default.nix
@@ -1,12 +1,14 @@
-{cabal, SDL, SDL_mixer}:
+{cabal, SDL} :
 
 cabal.mkDerivation (self : {
   pname = "SDL-mixer";
-  version = "0.5.5";
-  sha256 = "0e1b07a0cbe22eafff7b36f53e4ea840abad50131a867cb70d47beaf902fd185";
-  propagatedBuildInputs = [SDL SDL_mixer];
+  version = "0.6.1";
+  sha256 = "1fxp5sz0w6pr5047jjvh81wkljxsl7fca239364i50m44mpcsyn1";
+  propagatedBuildInputs = [ SDL ];
   meta = {
     description = "Binding to libSDL_mixer";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
