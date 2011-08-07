75dfd9d3c6a399be63f4747ff3b8df94aa1508f4
Peter Simons <simons@cryp.to>
2011-08-07 22:16:26 +0000

    wxHaskell: cosmetic, no functional change
    svn path=/nixpkgs/trunk/; revision=28363

--- pkgs/development/libraries/haskell/wxHaskell/wx.nix
+++ pkgs/development/libraries/haskell/wxHaskell/wx.nix
@@ -1,12 +1,13 @@
-{cabal, stm, wxcore}:
+{cabal, stm, wxcore} :
 
 cabal.mkDerivation (self : {
   pname = "wx";
   version = "0.12.1.6";
   sha256 = "1p0gn46gk1abin095va22n9bycxhm2cq1vyvwiypcdq7jq541lhk";
-  propagatedBuildInputs = [stm wxcore];
+  propagatedBuildInputs = [ stm wxcore ];
   meta = {
+    homepage = "http://haskell.org/haskellwiki/WxHaskell";
     description = "wxHaskell";
+    license = "LGPL";
   };
 })
-
