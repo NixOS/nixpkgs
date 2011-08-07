15fbcc11d794d0e11ace3c626f1a4c7f3609ab31
Peter Simons <simons@cryp.to>
2011-08-07 19:34:28 +0000

    haskell-MemoTrie: updated to version 0.4.10
    svn path=/nixpkgs/trunk/; revision=28270

--- pkgs/development/libraries/haskell/MemoTrie/default.nix
+++ pkgs/development/libraries/haskell/MemoTrie/default.nix
@@ -1,13 +1,17 @@
-{cabal}:
+{cabal} :
 
 cabal.mkDerivation (self : {
   pname = "MemoTrie";
-  version = "0.4.9";
-  sha256 = "f17dd0b73c1a11a6edb38fb6f457b9687f2e93aa4677e90f7ec482a528ac02e7";
+  version = "0.4.10";
+  sha256 = "1hkraq33sai046gwqlabc9nkz6jbl6vgj0c6lc6j4j5h5d8v08kk";
   meta = {
+    homepage = "http://haskell.org/haskellwiki/MemoTrie";
     description = "Trie-based memo functions";
-    license = "BSD";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
