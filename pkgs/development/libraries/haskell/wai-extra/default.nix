8174450e0a43c788a65ee257b5ff10f7b439d1ee
Peter Simons <simons@cryp.to>
2011-08-07 22:01:36 +0000

    haskell-wai-extra: updated to version 0.4.0.3
    svn path=/nixpkgs/trunk/; revision=28325

--- pkgs/development/libraries/haskell/wai-extra/default.nix
+++ pkgs/development/libraries/haskell/wai-extra/default.nix
@@ -1,18 +1,23 @@
-{cabal, blazeBuilder, blazeBuilderEnumerator, caseInsensitive, enumerator,
- httpTypes, network, text, transformers, wai, zlibBindings}:
+{cabal, blazeBuilder, blazeBuilderEnumerator, caseInsensitive,
+ enumerator, httpTypes, network, text, transformers, wai,
+ zlibBindings} :
 
 cabal.mkDerivation (self : {
   pname = "wai-extra";
-  version = "0.4.0.1";
-  sha256 = "0xj7dkwqa9axgm6cizf2kd97fakbmq5580mian888i5f21jn1n2z";
+  version = "0.4.0.3";
+  sha256 = "1mfjn9rxzcfdwvimjw57j7vpr1y64ia7905c8nxa9968sdy0dhsy";
   propagatedBuildInputs = [
     blazeBuilder blazeBuilderEnumerator caseInsensitive enumerator
     httpTypes network text transformers wai zlibBindings
   ];
   meta = {
-    description = "Provides some basic WAI handlers and middleware";
-    license = "BSD3";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    homepage = "http://github.com/snoyberg/wai-extra";
+    description = "Provides some basic WAI handlers and middleware.";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
