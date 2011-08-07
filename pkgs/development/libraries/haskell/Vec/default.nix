388ceb5e33f2ee9746c85523566dc395bd1c2874
Peter Simons <simons@cryp.to>
2011-08-07 22:01:45 +0000

    haskell-Vec: updated to version 0.9.8
    svn path=/nixpkgs/trunk/; revision=28327

--- pkgs/development/libraries/haskell/Vec/default.nix
+++ pkgs/development/libraries/haskell/Vec/default.nix
@@ -1,14 +1,18 @@
-{cabal, QuickCheck}:
+{cabal, QuickCheck} :
 
 cabal.mkDerivation (self : {
   pname = "Vec";
-  version = "0.9.7";
-  sha256 = "a67197f4dc022d6119a790e029a9475a17bb04ad812006bb154e5da9cd8f7ac7";
-  propagatedBuildInputs = [QuickCheck];
+  version = "0.9.8";
+  sha256 = "0yaqy0p2jh2ajf8897vsxl5w6bmavixn6n5cc7w8kdnybpbdms6v";
+  propagatedBuildInputs = [ QuickCheck ];
   meta = {
-    description = "Fixed-length lists and low-dimensional linear algebra";
-    license = "BSD";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    homepage = "http://graphics.cs.ucdavis.edu/~sdillard/Vec";
+    description = "Fixed-length lists and low-dimensional linear algebra.";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
