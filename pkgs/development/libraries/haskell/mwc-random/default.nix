84fef0f665eab080c593872a0a26a04189805931
Peter Simons <simons@cryp.to>
2011-08-07 22:02:56 +0000

    haskell-mwc-random: updated to version 0.10.0.0
    svn path=/nixpkgs/trunk/; revision=28345

--- pkgs/development/libraries/haskell/mwc-random/default.nix
+++ pkgs/development/libraries/haskell/mwc-random/default.nix
@@ -1,14 +1,18 @@
-{cabal, primitive, vector}:
+{cabal, primitive, vector} :
 
 cabal.mkDerivation (self : {
   pname = "mwc-random";
-  version = "0.8.0.3";
-  sha256 = "1sjjayfhfkfixcwdp21cfqld9pjikdsvlb956c1a7hcs82b68xp3";
-  propagatedBuildInputs = [primitive vector];
+  version = "0.10.0.0";
+  sha256 = "0pbzwq7jckjx0q7rpzhvjd2iam30yml0mwkz1jff8vl832z5xa8v";
+  propagatedBuildInputs = [ primitive vector ];
   meta = {
+    homepage = "https://github.com/bos/mwc-random";
     description = "Fast, high quality pseudo random number generation";
-    license = "BSD";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
