3623617019e7ecac0d629b4cfcaf5d5ee2bea274
Peter Simons <simons@cryp.to>
2011-08-07 18:22:01 +0000

    derive: updated to version 2.5.2
    svn path=/nixpkgs/trunk/; revision=28228

--- pkgs/development/libraries/haskell/derive/default.nix
+++ pkgs/development/libraries/haskell/derive/default.nix
@@ -1,11 +1,17 @@
-{cabal, haskellSrcExts, mtl, uniplate}:
+{cabal, haskellSrcExts, syb, transformers, uniplate} :
 
 cabal.mkDerivation (self : {
   pname = "derive";
-  version = "2.3.0.2";
-  sha256 = "bb8f62d93742d0f27c742bf09fdad73111057d9b531dda45d7f0c894b447809e";
-  propagatedBuildInputs = [haskellSrcExts mtl uniplate];
+  version = "2.5.2";
+  sha256 = "03qmwy47xxpdwpfyf509bsh7ysh4y5lwxsn15jpbnz6mnssxn07y";
+  propagatedBuildInputs = [
+    haskellSrcExts syb transformers uniplate
+  ];
   meta = {
+    homepage = "http://community.haskell.org/~ndm/derive/";
     description = "A program and library to derive instances for data types";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
