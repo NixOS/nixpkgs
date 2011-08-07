396bbe33ff63e641b2c4a6b296aab36e27a11d01
Peter Simons <simons@cryp.to>
2011-08-07 22:01:12 +0000

    MonadCatchIO-transformers: updated to version 0.2.2.2
    svn path=/nixpkgs/trunk/; revision=28319

--- pkgs/development/libraries/haskell/MonadCatchIO-transformers/default.nix
+++ pkgs/development/libraries/haskell/MonadCatchIO-transformers/default.nix
@@ -2,11 +2,13 @@
 
 cabal.mkDerivation (self : {
   pname = "MonadCatchIO-transformers";
-  version = "0.2.2.0";
-  sha256 = "7f3b45ac01ac98476d1305333435005a6876e5c04c562e94ad2426ee7ab6936d";
-  propagatedBuildInputs = [transformers extensibleExceptions];
+  version = "0.2.2.2";
+  sha256 = "083c0abwja447j0p8q0j15iv7bshchy83rfqw07b2hfy38467h9g";
+  propagatedBuildInputs = [ transformers ];
   meta = {
     description = "Monad-transformer compatible version of the Control.Exception module";
+    license = self.stdenv.lib.licenses.publicDomain;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
