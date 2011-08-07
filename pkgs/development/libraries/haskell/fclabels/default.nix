d3aeee395d98e91a43fbc80a8014b5c36e091373
Peter Simons <simons@cryp.to>
2011-08-07 20:25:32 +0000

    haskell-fclabels: updated to version 0.11.1.1
    svn path=/nixpkgs/trunk/; revision=28287

--- pkgs/development/libraries/haskell/fclabels/default.nix
+++ pkgs/development/libraries/haskell/fclabels/default.nix
@@ -1,14 +1,17 @@
-{cabal, monadsFd}:
+{cabal, mtl} :
 
 cabal.mkDerivation (self : {
   pname = "fclabels";
-  version = "0.4.2";
-  sha256 = "d1abfd94f8970e55ed839645a58697b8beeacc5e5ea4a5861001539aa8fc28d7";
-  propagatedBuildInputs = [monadsFd];
+  version = "0.11.1.1";
+  sha256 = "09zs9ddhhz57r2fm6ayc95xgsfjcympmgiify2p6f42w9hs34dmf";
+  propagatedBuildInputs = [ mtl ];
   meta = {
-    description = "First class accessor labels";
-    license = "BSD";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    description = "First class accessor labels implemented as lenses.";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
