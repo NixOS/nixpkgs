afc7e7523f69686bda386cd7a8168dd340174588
Peter Simons <simons@cryp.to>
2011-08-07 22:03:40 +0000

    haskell-WebBits: cosmetic, no functional change
    svn path=/nixpkgs/trunk/; revision=28356

--- pkgs/development/libraries/haskell/WebBits/default.nix
+++ pkgs/development/libraries/haskell/WebBits/default.nix
@@ -1,13 +1,15 @@
-{cabal, mtl, parsec, syb}:
+{cabal, mtl, parsec, syb} :
 
 cabal.mkDerivation (self : {
   pname = "WebBits";
   version = "2.0";
   sha256 = "14a1rqlq925f6rdbi8yx44xszj5pvskcmw1gi1bj8hbilgmlwi7f";
-
   propagatedBuildInputs = [ mtl parsec syb ];
-
   meta = {
-    description = "WebBits is a collection of libraries for working with JavaScript.";
+    homepage = "http://www.cs.brown.edu/research/plt/";
+    description = "JavaScript analysis tools";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
