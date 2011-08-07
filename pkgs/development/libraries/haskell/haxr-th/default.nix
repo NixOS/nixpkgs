d5fbf640a3fab64ed2fbfdb69b1f153ea35cc64c
Peter Simons <simons@cryp.to>
2011-08-07 21:59:55 +0000

    haskell-haxr-th: updated to version 3000.5
    Unfortunately, even the latest version will not compile because it
    expects an obsolete version of the base libraries.
    
    svn path=/nixpkgs/trunk/; revision=28302

--- pkgs/development/libraries/haskell/haxr-th/default.nix
+++ pkgs/development/libraries/haskell/haxr-th/default.nix
@@ -1,11 +1,15 @@
-{cabal, haxr, HaXml, HTTP}:
+{cabal, haxr} :
 
 cabal.mkDerivation (self : {
   pname = "haxr-th";
-  version = "3000.0.0";
-  sha256 = "00wqhri2fljjds6rwws0hgk7z27ii1lgvxr8db2li0780q5fa6ic";
+  version = "3000.5";
+  sha256 = "1h1g4r7c5k3rja49ip4m21f2sscn06xjxharnlyazvvs6mzfysif";
+  propagatedBuildInputs = [ haxr ];
   meta = {
+    homepage = "http://www.haskell.org/haxr/";
     description = "Automatic deriving of XML-RPC structs for Haskell records.";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
-  propagatedBuildInputs = [HaXml HTTP haxr];
 })
