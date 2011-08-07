332e97cc9daa745eeea3f68f8ee25ec62537d901
Peter Simons <simons@cryp.to>
2011-08-07 18:22:24 +0000

    pandoc-types: updated to version 1.8.2
    svn path=/nixpkgs/trunk/; revision=28233

--- pkgs/development/libraries/haskell/pandoc-types/default.nix
+++ pkgs/development/libraries/haskell/pandoc-types/default.nix
@@ -1,11 +1,15 @@
-{cabal, syb}:
+{cabal, syb} :
 
 cabal.mkDerivation (self : {
   pname = "pandoc-types";
-  version = "1.8";
-  sha256 = "1ikr1dmmdag31hgcswrnhzqacv4kl7z6dc8za2cjdq0cpd2mla98";
-  propagatedBuildInputs = [syb];
+  version = "1.8.2";
+  sha256 = "04whkqld2pnfz25i9rcq7d4pi9zkn6c1rpz95vdlg9r5xkhhnn3a";
+  propagatedBuildInputs = [ syb ];
   meta = {
+    homepage = "http://johnmacfarlane.net/pandoc";
     description = "Types for representing a structured document";
+    license = "GPL";
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
