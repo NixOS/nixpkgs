ff6f8891d762af31ba85ec27667727340901655a
Peter Simons <simons@cryp.to>
2011-08-07 22:02:09 +0000

    cautious-file: updated to version 1.0
    svn path=/nixpkgs/trunk/; revision=28333

--- pkgs/development/libraries/haskell/cautious-file/default.nix
+++ pkgs/development/libraries/haskell/cautious-file/default.nix
@@ -1,13 +1,16 @@
-{cabal}:
+{cabal} :
 
 cabal.mkDerivation (self : {
   pname = "cautious-file";
-  version = "0.1.5";
-  sha256 = "0d7b7bf530476b89ffc22bf6e586402b0fd6fd5571cc941df08838c5a890ad01";
+  version = "1.0";
+  sha256 = "1s2la91vk9c99bj0ipzc4r6w81rcs4jfmn0xr1cgjab00bzj880q";
   meta = {
-    description = "Ways to write a file cautiously";
-    license = "BSD";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    description = "Ways to write a file cautiously, to reduce the chances of problems such as data loss due to crashes or power failures";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
