6099f6ba8fc99c0fcd592d55a913493e88ffc2f8
Peter Simons <simons@cryp.to>
2011-08-07 19:33:26 +0000

    base64-string: updated to version 0.2
    svn path=/nixpkgs/trunk/; revision=28256

--- pkgs/development/libraries/haskell/base64-string/default.nix
+++ pkgs/development/libraries/haskell/base64-string/default.nix
@@ -1,14 +1,14 @@
-{cabal}:
+{cabal} :
 
 cabal.mkDerivation (self : {
   pname = "base64-string";
-  version = "0.1";
-
-  sha256 = "f9a6f050f5d9993313a1ceb49eba827ecf1046af51266d10b0dc899b53f13d8c";
-
-  propagatedBuildInputs = [];
+  version = "0.2";
+  sha256 = "0pkhrimabacsjalzq0y3a197fqfbspsbv8xszzg4vbb1fb59dj1y";
   meta = {
-    description = "";
+    homepage = "http://urchin.earth.li/~ian/cabal/base64-string/";
+    description = "Base64 implementation for String's.";
+    license = "unknown";
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
