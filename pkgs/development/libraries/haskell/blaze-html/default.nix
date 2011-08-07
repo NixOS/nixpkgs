d2a2d365c92e414a4ff8f606c61dec6f800f2da7
Peter Simons <simons@cryp.to>
2011-08-07 18:23:23 +0000

    blaze-html: updated to version 0.4.1.6
    svn path=/nixpkgs/trunk/; revision=28245

--- pkgs/development/libraries/haskell/blaze-html/default.nix
+++ pkgs/development/libraries/haskell/blaze-html/default.nix
@@ -1,13 +1,17 @@
-{cabal, text, blazeBuilder}:
+{cabal, blazeBuilder, text} :
 
 cabal.mkDerivation (self : {
   pname = "blaze-html";
-  version = "0.4.1.4";
-  sha256 = "1xf302dapwmmlxj9alfbdv6rcrxhr9p305s4jz2d6ckq2xzz8yjf";
-  propagatedBuildInputs = [text blazeBuilder];
+  version = "0.4.1.6";
+  sha256 = "084phxxdy12vi2q084k8w693m94v0pjf29zx2fk1y0n80k05ii4z";
+  propagatedBuildInputs = [ blazeBuilder text ];
   meta = {
-    description = "A blazingly fast HTML combinator library";
-    license = "BSD";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    homepage = "http://jaspervdj.be/blaze";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
