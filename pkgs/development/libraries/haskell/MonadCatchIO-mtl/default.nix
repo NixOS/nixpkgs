341f137c8e4254d3ad35b79987781ee88b028196
Peter Simons <simons@cryp.to>
2011-08-07 22:01:08 +0000

    MonadCatchIO-mtl: updated to version 0.3.0.3
    svn path=/nixpkgs/trunk/; revision=28318

--- pkgs/development/libraries/haskell/MonadCatchIO-mtl/default.nix
+++ pkgs/development/libraries/haskell/MonadCatchIO-mtl/default.nix
@@ -1,12 +1,15 @@
-{cabal, mtl, extensibleExceptions} :
+{cabal, mtl} :
 
 cabal.mkDerivation (self : {
   pname = "MonadCatchIO-mtl";
-  version = "0.3.0.1";
-  sha256 = "56113319439a10e338b2e3169e1df575024fbaf97827511f4856e46efbac9a07";
-  propagatedBuildInputs = [mtl extensibleExceptions];
+  version = "0.3.0.3";
+  sha256 = "0sqvdrvl1z6y176jwxv02aam4fz2awmqyjba803w816pp9m9a5pr";
+  propagatedBuildInputs = [ mtl ];
   meta = {
+    homepage = "http://code.haskell.org/~jcpetruzza/MonadCatchIO-mtl";
     description = "Monad-transformer version of the Control.Exception module";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
