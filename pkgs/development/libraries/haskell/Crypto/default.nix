0c8a434cf0be7ad97d632a13074419290c889b16
Peter Simons <simons@cryp.to>
2011-08-07 20:25:15 +0000

    haskell-Crypto: updated to version 4.2.3
    svn path=/nixpkgs/trunk/; revision=28283

--- pkgs/development/libraries/haskell/Crypto/default.nix
+++ pkgs/development/libraries/haskell/Crypto/default.nix
@@ -1,12 +1,14 @@
-{cabal, HUnit, QuickCheck}:
+{cabal, HUnit, QuickCheck} :
 
 cabal.mkDerivation (self : {
   pname = "Crypto";
-  version = "4.1.0";
-  sha256 = "0984c833c5dfa6f4d56fd6fb284db7b7cef6676dc7999a1436aa856becba2b8f";
-  propagatedBuildInputs = [HUnit QuickCheck];
+  version = "4.2.3";
+  sha256 = "02wi8lyi27i8cdj8vclrl7vcng38srdzz9qpqwsc4y4lmvgg82br";
+  propagatedBuildInputs = [ HUnit QuickCheck ];
   meta = {
-    description = "Several encryption algorithms for Haskell";
+    description = "Collects together existing Haskell cryptographic functions into a package";
+    license = "unknown";
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
