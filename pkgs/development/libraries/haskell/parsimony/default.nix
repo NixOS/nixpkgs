56301c5324507370a504c6e0cbac8a0a9c854eb9
Peter Simons <simons@cryp.to>
2011-08-07 22:02:32 +0000

    haskell-parsimony: updated to version 1.1
    svn path=/nixpkgs/trunk/; revision=28339

--- pkgs/development/libraries/haskell/parsimony/default.nix
+++ pkgs/development/libraries/haskell/parsimony/default.nix
@@ -1,12 +1,14 @@
-{cabal, utf8String}:
+{cabal, utf8String} :
 
 cabal.mkDerivation (self : {
   pname = "parsimony";
-  version = "1";
-  sha256 = "8196029dc346470301f4535f678724412060a0aafd81b017211b57635a25a378";
-  propagatedBuildInputs = [utf8String];
+  version = "1.1";
+  sha256 = "0476zmsjyjf58lh85806baqblq8hjxcrrnqc6ddxxq17lmvsd5ic";
+  propagatedBuildInputs = [ utf8String ];
   meta = {
     description = "Monadic parser combinators derived from Parsec";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
