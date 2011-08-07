0df3b896488e090a28e80f09719ac47c47a8faf6
Peter Simons <simons@cryp.to>
2011-08-07 22:01:49 +0000

    haskell-Hint: updated to version 0.3.3.2
    svn path=/nixpkgs/trunk/; revision=28328

--- pkgs/development/libraries/haskell/hint/default.nix
+++ pkgs/development/libraries/haskell/hint/default.nix
@@ -1,16 +1,18 @@
-{cabal, extensibleExceptions, filepath, ghcMtl,
- ghcPaths, haskellSrc, MonadCatchIOMtl, mtl, utf8String} :
+{cabal, MonadCatchIOMtl, ghc, ghcMtl, ghcPaths, haskellSrc, mtl,
+ utf8String} :
 
 cabal.mkDerivation (self : {
   pname = "hint";
-  version = "0.3.2.3";
-  sha256 = "1cc01037cfd32eb1a299ce625487411a97ce70178778d7bbd1d5fcef7d3d40c4";
+  version = "0.3.3.2";
+  sha256 = "1qm74hjz8cxypvavcw7s094zg9ic3r1ll2lj3y159ipc79cw2sn1";
   propagatedBuildInputs = [
-    extensibleExceptions filepath ghcMtl ghcPaths haskellSrc
-    MonadCatchIOMtl mtl utf8String
+    MonadCatchIOMtl ghc ghcMtl ghcPaths haskellSrc mtl utf8String
   ];
   meta = {
-    description = "An mtl compatible version of the Ghc-Api monads and monad-transformers";
+    homepage = "http://projects.haskell.org/hint";
+    description = "Runtime Haskell interpreter (GHC API wrapper)";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
