a04acc1c5d222ff8a0027c1d3e7de45b248329b4
Peter Simons <simons@cryp.to>
2011-08-07 20:24:41 +0000

    haskell-HGL: updated to version 3.2.0.2
    svn path=/nixpkgs/trunk/; revision=28275

--- pkgs/development/libraries/haskell/HGL/default.nix
+++ pkgs/development/libraries/haskell/HGL/default.nix
@@ -1,13 +1,14 @@
-{cabal, X11}:
+{cabal, X11} :
 
 cabal.mkDerivation (self : {
   pname = "HGL";
-  version = "3.2.0.0";
-  sha256 = "fa7cb1981f6e5a89b35e0fc2593c0945175a0d97fc3bc356cc8724fa1c881e86";
-  propagatedBuildInputs = [X11];
-  configureFlags = ''--constraint=base<4'';
+  version = "3.2.0.2";
+  sha256 = "13wcvf6bfii9pihr8m08b81fyslf5n587ds4zzgizbd8m38k81vz";
+  propagatedBuildInputs = [ X11 ];
   meta = {
     description = "A simple graphics library based on X11 or Win32";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
