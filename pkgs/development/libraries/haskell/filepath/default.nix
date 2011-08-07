1015675c78c06435facf1656312438a9c7006c70
Peter Simons <simons@cryp.to>
2011-08-07 22:03:16 +0000

    haskell-filepath: updated to version 1.2.0.0
    svn path=/nixpkgs/trunk/; revision=28350

--- pkgs/development/libraries/haskell/filepath/default.nix
+++ pkgs/development/libraries/haskell/filepath/default.nix
@@ -1,12 +1,14 @@
-{ cabal} :
+{cabal} :
 
 cabal.mkDerivation (self : {
   pname = "filepath";
-  version = "1.1.0.2";
-  sha256 = "1mkckshbpmgp7z97vc923x1qph8brqh9wcnb90kbv46bhiza277q";
-  propagatedBuildInputs = [];
+  version = "1.2.0.0";
+  sha256 = "14jji7byjlkzsylsnqwfsiw5vsc7nlaisqabzcw9f7nhrxkq2n20";
   meta = {
-    description = "Library for manipulating FilePath's in a cross platform way";
+    homepage = "http://www-users.cs.york.ac.uk/~ndm/filepath/";
+    description = "Library for manipulating FilePaths in a cross platform way.";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
