457335b4555ef1683dd533d614ec44812138a26e
Peter Simons <simons@cryp.to>
2011-08-07 22:02:13 +0000

    haskell-data-accessor: updated to version 0.2.2
    svn path=/nixpkgs/trunk/; revision=28334

--- pkgs/development/libraries/haskell/data-accessor/data-accessor.nix
+++ pkgs/development/libraries/haskell/data-accessor/data-accessor.nix
@@ -1,14 +1,18 @@
-{cabal, transformers}:
+{cabal, transformers} :
 
 cabal.mkDerivation (self : {
   pname = "data-accessor";
-  version = "0.2.1.7";
-  sha256 = "05wi8wm4vq4j4ldx1925q7v31rnh4gj5891gqc5hdf0x8cgkxbii";
-  propagatedBuildInputs = [transformers];
+  version = "0.2.2";
+  sha256 = "1jqd0qlv1yab83d5pdbzjw6q4a2kvbsar6kgczq0f0xn9gxm0qci";
+  propagatedBuildInputs = [ transformers ];
   meta = {
-    description = "Utilities for accessing and manipulating the fields of records";
-    license = "BSD";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    homepage = "http://www.haskell.org/haskellwiki/Record_access";
+    description = "Utilities for accessing and manipulating fields of records";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
