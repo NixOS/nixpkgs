f728e6564b01e57c26fc592518c5eda33ec7b3ef
Peter Simons <simons@cryp.to>
2011-08-07 19:34:00 +0000

    haskell-warp: updated to version 0.4.3
    svn path=/nixpkgs/trunk/; revision=28264

--- pkgs/development/libraries/haskell/warp/default.nix
+++ pkgs/development/libraries/haskell/warp/default.nix
@@ -1,18 +1,23 @@
-{cabal, blazeBuilder, blazeBuilderEnumerator, caseInsensitive, enumerator,
- httpTypes, network, simpleSendfile, transformers, unixCompat, wai}:
+{cabal, blazeBuilder, blazeBuilderEnumerator, caseInsensitive,
+ enumerator, httpTypes, network, simpleSendfile, transformers,
+ unixCompat, wai} :
 
 cabal.mkDerivation (self : {
   pname = "warp";
-  version = "0.4.1.1";
-  sha256 = "0qck4mpg4p6v2yx2r6qchqd3lvsn8n5ys6xsm11hhznc2r50aayh";
+  version = "0.4.3";
+  sha256 = "1g1fyys4j5cd2lp8818060i970f6fpxnjgyvb5m4r9asn7p1z4yc";
   propagatedBuildInputs = [
     blazeBuilder blazeBuilderEnumerator caseInsensitive enumerator
     httpTypes network simpleSendfile transformers unixCompat wai
   ];
   meta = {
-    description = "A fast, light-weight web server for WAI applications";
-    license = "BSD3";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    homepage = "http://github.com/snoyberg/warp";
+    description = "A fast, light-weight web server for WAI applications.";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
