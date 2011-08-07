981db911364432a891c65b68645803955282fd3a
Peter Simons <simons@cryp.to>
2011-08-07 18:22:43 +0000

    tagsoup: updated to version 0.12.2
    svn path=/nixpkgs/trunk/; revision=28238

--- pkgs/development/libraries/haskell/tagsoup/default.nix
+++ pkgs/development/libraries/haskell/tagsoup/default.nix
@@ -1,13 +1,18 @@
-{cabal}:
+{cabal, text} :
 
 cabal.mkDerivation (self : {
   pname = "tagsoup";
-  version = "0.12";
-  sha256 = "0jaqr6q8asn7gd336xsblcc55lzm8glzlhvs61mhzjvk4hg9pmg7";
+  version = "0.12.2";
+  sha256 = "066nmp4fd5pcx9xiq7lhxcrhmnhkxk8z7zx5laa19bbb8jbjjl4y";
+  propagatedBuildInputs = [ text ];
   meta = {
+    homepage = "http://community.haskell.org/~ndm/tagsoup/";
     description = "Parsing and extracting information from (possibly malformed) HTML/XML documents";
-    license = "BSD";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
