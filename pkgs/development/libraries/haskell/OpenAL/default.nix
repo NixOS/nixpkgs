6e2657f4b9dc8a38e827c9d4c25f92edacc6149a
Peter Simons <simons@cryp.to>
2011-08-07 22:00:08 +0000

    haskell-OpenAL: updated to version 1.4.0.0
    svn path=/nixpkgs/trunk/; revision=28305

--- pkgs/development/libraries/haskell/OpenAL/default.nix
+++ pkgs/development/libraries/haskell/OpenAL/default.nix
@@ -1,12 +1,15 @@
-{cabal, OpenGL, openal}:
+{cabal, ObjectName, StateVar, Tensor, openal} :
 
 cabal.mkDerivation (self : {
   pname = "OpenAL";
-  version = "1.3.1.1"; # Haskell Platform 2009.0.0
-  sha256 = "4875e015770feb93d5127d4b6f2338eb3957a0c36f64ccb8b3846010619f89b0";
-  propagatedBuildInputs = [OpenGL openal];
+  version = "1.4.0.0";
+  sha256 = "1vn9r8fd4zwqm8a9d8bgvi9vs1lmygn8sw1rlv819b5wmjwz3ms2";
+  propagatedBuildInputs = [ ObjectName StateVar Tensor openal ];
   meta = {
+    homepage = "http://connect.creativelabs.com/openal/";
     description = "A binding to the OpenAL cross-platform 3D audio API";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
