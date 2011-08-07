48d7977feb180505e5c4deaff809f97361390b29
Peter Simons <simons@cryp.to>
2011-08-07 22:01:57 +0000

    web-routes: updated to version 0.25.2
    svn path=/nixpkgs/trunk/; revision=28330

--- pkgs/development/libraries/haskell/web-routes/default.nix
+++ pkgs/development/libraries/haskell/web-routes/default.nix
@@ -1,13 +1,17 @@
-{cabal, network, parsec, utf8String}:
+{cabal, mtl, network, parsec, utf8String} :
 
 cabal.mkDerivation (self : {
   pname = "web-routes";
-  version = "0.22.0";
-  sha256 = "6482ecba585cf7b1f32c29bfd5cb6f5e06dba72231e38ae5baa15b6b95f7c6c8";
-  propagatedBuildInputs = [network parsec utf8String];
+  version = "0.25.2";
+  sha256 = "0gspjvk5859zwg55q8fjyz4a0d2p6lf2qwa41b8s6kcqi38nnp08";
+  propagatedBuildInputs = [ mtl network parsec utf8String ];
   meta = {
-    description = "Library for maintaining correctness and composability of URLs within an application";
-    license = "BSD";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    description = "Library for maintaining correctness and composability of URLs within an application.";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
