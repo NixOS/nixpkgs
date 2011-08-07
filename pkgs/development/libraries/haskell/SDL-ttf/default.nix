122b3831db6855cf431cedf5bac3c1b2b7a732f8
Peter Simons <simons@cryp.to>
2011-08-07 22:00:47 +0000

    SDL-ttf: updated to version 0.6.1
    svn path=/nixpkgs/trunk/; revision=28314

--- pkgs/development/libraries/haskell/SDL-ttf/default.nix
+++ pkgs/development/libraries/haskell/SDL-ttf/default.nix
@@ -1,12 +1,14 @@
-{cabal, SDL, SDL_ttf}:
+{cabal, SDL, SDL_ttf} :
 
 cabal.mkDerivation (self : {
   pname = "SDL-ttf";
-  version = "0.5.5";
-  sha256 = "577109d01fca61c103520e7bf33e45af118bcb6d15fcd6da7a8b72c43c69cbb6";
-  propagatedBuildInputs = [SDL SDL_ttf];
+  version = "0.6.1";
+  sha256 = "0n6vbigkjfvvk98bp7ys14snpd1zmbz69ndhhpnrn02h363vwkal";
+  propagatedBuildInputs = [ SDL SDL_ttf ];
   meta = {
     description = "Binding to libSDL_ttf";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
