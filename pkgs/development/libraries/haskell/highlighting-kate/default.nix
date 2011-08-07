e2ca9813d4e4e7ff59457015b1fdaf4aeabd6c38
Peter Simons <simons@cryp.to>
2011-08-07 18:23:27 +0000

    highlighting-kate: updated to version 0.2.10
    svn path=/nixpkgs/trunk/; revision=28246

--- pkgs/development/libraries/haskell/highlighting-kate/default.nix
+++ pkgs/development/libraries/haskell/highlighting-kate/default.nix
@@ -1,11 +1,15 @@
-{cabal, parsec, pcreLight, xhtml}:
+{cabal, parsec, regexPcreBuiltin, xhtml} :
 
 cabal.mkDerivation (self : {
   pname = "highlighting-kate";
-  version = "0.2.9";
-  sha256 = "0mk8m01mqw6vnjldr5idc6611475j4m292cm18kn3wa83000mbgk";
-  propagatedBuildInputs = [parsec pcreLight xhtml];
+  version = "0.2.10";
+  sha256 = "0cw89qsslrp4zh47ics7bg79fkqnxpnyz1a9xws0xzd9xmg3zrhh";
+  propagatedBuildInputs = [ parsec regexPcreBuiltin xhtml ];
   meta = {
+    homepage = "http://github.com/jgm/highlighting-kate";
     description = "Syntax highlighting";
+    license = "GPL";
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
