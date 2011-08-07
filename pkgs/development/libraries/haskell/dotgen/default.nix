2aabd7b12bc54977e9d10efe77523ae78896bd19
Peter Simons <simons@cryp.to>
2011-08-07 22:01:28 +0000

    haskell-dotgen: updated to version 0.4.1
    svn path=/nixpkgs/trunk/; revision=28323

--- pkgs/development/libraries/haskell/dotgen/default.nix
+++ pkgs/development/libraries/haskell/dotgen/default.nix
@@ -1,11 +1,13 @@
-{cabal}:
+{cabal} :
 
 cabal.mkDerivation (self : {
   pname = "dotgen";
-  version = "0.3";
-  sha256 = "1c486ffa782c4a46e66df12f5acf776a808b654d96fe0d0071d5c41111b1b7ed";
+  version = "0.4.1";
+  sha256 = "1g5ds0mqkz0lzhcp42hin08azschs3p083ikdk4d5jil8rzl7d8k";
   meta = {
-    description = "A simple interface for building .dot graph files";
+    description = "A simple interface for building .dot graph files.";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
