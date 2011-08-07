cebb321c48445cb000da1350a7a3002322622608
Peter Simons <simons@cryp.to>
2011-08-07 22:03:00 +0000

    haskell-mpppc: updated to version 0.1.2
    svn path=/nixpkgs/trunk/; revision=28346

--- pkgs/development/libraries/haskell/mpppc/default.nix
+++ pkgs/development/libraries/haskell/mpppc/default.nix
@@ -1,12 +1,14 @@
-{cabal, ansiTerminal, split, text}:
+{cabal, ansiTerminal, split, text} :
 
 cabal.mkDerivation (self : {
   pname = "mpppc";
-  version = "0.1.0";
-  sha256 = "73796138cc10df77217568d59fb999567436bedefaa8579ed6648c6cfb841c86";
-  propagatedBuildInputs = [ansiTerminal split text];
+  version = "0.1.2";
+  sha256 = "1zms71wx5a6rd60xy1pv6g1kxlx0hzh36pbr5a5lkfflc583z1k5";
+  propagatedBuildInputs = [ ansiTerminal split text ];
   meta = {
     description = "Multi-dimensional parametric pretty-printer with color";
+    license = "GPL";
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
