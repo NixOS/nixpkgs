e2e79e13629eab3878a68696f1eeee9c320fa097
Peter Simons <simons@cryp.to>
2011-08-07 19:34:12 +0000

    haskell-pool: updated to version 0.1.0.4
    svn path=/nixpkgs/trunk/; revision=28266

--- pkgs/development/libraries/haskell/pool/default.nix
+++ pkgs/development/libraries/haskell/pool/default.nix
@@ -1,14 +1,18 @@
-{cabal, monadControl, stm, transformers}:
+{cabal, monadControl, transformers} :
 
 cabal.mkDerivation (self : {
   pname = "pool";
-  version = "0.1.0.2";
-  sha256 = "1w2z3p3iriw43g655rhd5b70r3cbzl4jf8bybyk5d04x6mcg3wfq";
-  propagatedBuildInputs = [monadControl stm transformers];
+  version = "0.1.0.4";
+  sha256 = "11jsls81njkwhn48xdyrqydhr4yz82g7a6pji80ckplkdyycgx6p";
+  propagatedBuildInputs = [ monadControl transformers ];
   meta = {
-    description = "Thread-safe resource pools";
-    license = "BSD3";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    homepage = "http://github.com/snoyberg/pool";
+    description = "Thread-safe resource pools.";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
