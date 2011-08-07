8569a4b22045f3b6b78294ec8a6c9d1970b8708f
Peter Simons <simons@cryp.to>
2011-08-07 18:22:28 +0000

    pandoc: updated to version 1.8.2.1
    svn path=/nixpkgs/trunk/; revision=28234

--- pkgs/development/libraries/haskell/pandoc/default.nix
+++ pkgs/development/libraries/haskell/pandoc/default.nix
@@ -1,12 +1,20 @@
-{cabal, HTTP, citeprocHs, dlist, tagsoup, texmath, xhtml, zipArchive}:
+{cabal, HTTP, base64Bytestring, citeprocHs, dlist, json, mtl,
+ network, pandocTypes, parsec, syb, tagsoup, texmath, utf8String,
+ xhtml, xml, zipArchive} :
 
 cabal.mkDerivation (self : {
   pname = "pandoc";
-  version = "1.8.1.2";
-  sha256 = "93b23b7ff91ac6d91c8b3945175967fa4da5a5587c1147a19a1a20c8d61b734d";
-  propagatedBuildInputs =
-    [HTTP citeprocHs dlist tagsoup texmath xhtml zipArchive];
+  version = "1.8.2.1";
+  sha256 = "0cwly0j2rj46h654iwl04l6jkhk6rrhynqvrdnq47067n9vm60pi";
+  propagatedBuildInputs = [
+    HTTP base64Bytestring citeprocHs dlist json mtl network pandocTypes
+    parsec syb tagsoup texmath utf8String xhtml xml zipArchive
+  ];
   meta = {
+    homepage = "http://johnmacfarlane.net/pandoc";
     description = "Conversion between markup formats";
+    license = "GPL";
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
