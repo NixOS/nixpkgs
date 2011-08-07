ec1c158d754066365847a9a3fb0e9f50c0ebddbc
Peter Simons <simons@cryp.to>
2011-08-07 22:01:16 +0000

    haskell-yesod-form: updated to version 0.2.0.1
    svn path=/nixpkgs/trunk/; revision=28320

--- pkgs/development/libraries/haskell/yesod-form/default.nix
+++ pkgs/development/libraries/haskell/yesod-form/default.nix
@@ -1,21 +1,24 @@
-{cabal, blazeBuilder, blazeHtml, dataDefault, emailValidate, hamlet,
- network, persistent, text, transformers, wai, webRoutesQuasi,
- xssSanitize, yesodCore, yesodPersistent}:
+{cabal, blazeBuilder, blazeHtml, dataDefault, emailValidate,
+ hamlet, network, persistent, text, transformers, wai,
+ webRoutesQuasi, xssSanitize, yesodCore, yesodPersistent} :
 
 cabal.mkDerivation (self : {
   pname = "yesod-form";
-  version = "0.2.0";
-  sha256 = "1dwwndrb09bqfzrarxpiw29xqvxk70iy8b2vcy0rl6g2wvrcm8an";
+  version = "0.2.0.1";
+  sha256 = "17ap5jf433s8a3k97vskvc7wxa8qdx03fynh3881zg5s1kqj3b3r";
   propagatedBuildInputs = [
-    blazeBuilder blazeHtml dataDefault emailValidate hamlet
-    network persistent text transformers wai webRoutesQuasi
-    xssSanitize yesodCore yesodPersistent
+    blazeBuilder blazeHtml dataDefault emailValidate hamlet network
+    persistent text transformers wai webRoutesQuasi xssSanitize
+    yesodCore yesodPersistent
   ];
-  noHaddock = true;
   meta = {
+    homepage = "http://www.yesodweb.com/";
     description = "Form handling support for Yesod Web Framework";
-    license = "BSD3";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
