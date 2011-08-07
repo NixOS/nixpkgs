45ae546d937bc83eb84b9ddcad31f5559a018521
Peter Simons <simons@cryp.to>
2011-08-07 22:01:40 +0000

    haskell-data-reify: updated to version 0.6
    svn path=/nixpkgs/trunk/; revision=28326

--- pkgs/development/libraries/haskell/data-reify/default.nix
+++ pkgs/development/libraries/haskell/data-reify/default.nix
@@ -1,11 +1,14 @@
-{cabal}:
+{cabal} :
 
 cabal.mkDerivation (self : {
   pname = "data-reify";
-  version = "0.5";
-  sha256 = "ca501cbde57d3b53b4def9ce3de9ecccc5136fb0575877e98e163668ebe23055";
+  version = "0.6";
+  sha256 = "0mif89mpj5zvw8czc51mfj27jw2ipxd2awnm9q13s46k6s5pv6a7";
   meta = {
-    description = "Reify a recursive data structure into an explicit graph";
+    homepage = "http://www.ittc.ku.edu/csdl/fpg/Tools/IOReification";
+    description = "Reify a recursive data structure into an explicit graph.";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
