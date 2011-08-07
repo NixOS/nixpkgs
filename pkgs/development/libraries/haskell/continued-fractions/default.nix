0b6775521c4053fe476540a1993b68b5b3a40094
Peter Simons <simons@cryp.to>
2011-08-07 20:25:57 +0000

    haskell-packages.nix: added continued-fractions library, required by random-fu
    svn path=/nixpkgs/trunk/; revision=28293

---
+++ pkgs/development/libraries/haskell/continued-fractions/default.nix
@@ -0,0 +1,12 @@
+{cabal} :
+
+cabal.mkDerivation (self : {
+  pname = "continued-fractions";
+  version = "0.9.1.0";
+  sha256 = "07g6ms68xfzd25zr2k2fpg4f4pynmq4mn7djkzrg3gbfh9gfq37q";
+  meta = {
+    homepage = "/dev/null";
+    description = "Continued fractions.";
+    license = self.stdenv.lib.licenses.publicDomain;
+  };
+})
