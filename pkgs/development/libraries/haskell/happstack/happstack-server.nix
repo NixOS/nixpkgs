11da59c3bcefb1618a10fe7af7e893462583abe2
Peter Simons <simons@cryp.to>
2011-08-07 18:23:44 +0000

    happstack-server: updated to version 6.1.6
    There is a newer version available, but Gitit won't accept that one.
    
    svn path=/nixpkgs/trunk/; revision=28250

--- pkgs/development/libraries/haskell/happstack/happstack-server.nix
+++ pkgs/development/libraries/haskell/happstack/happstack-server.nix
@@ -1,16 +1,23 @@
-{cabal, HUnit, HaXml, MaybeT, parsec, sendfile, utf8String, mtl, network, hslogger, happstackData, happstackUtil, xhtml, html, zlib}:
+{cabal, MaybeT, blazeHtml, happstackData, happstackUtil, hslogger,
+ html, mtl, network, parsec, sendfile, syb, text, utf8String, xhtml,
+ zlib} :
 
 cabal.mkDerivation (self : {
-    pname = "happstack-server";
-    version = "0.5.0.4";
-    sha256 = "1iyjrlg5x6rlx8bfhn62a0ckjap0zv22hb6yazqph53jx6vn9b7q";
-    propagatedBuildInputs = [
-      HUnit HaXml MaybeT happstackData happstackUtil hslogger html
-      mtl network parsec sendfile utf8String xhtml zlib
+  pname = "happstack-server";
+  version = "6.1.6";
+  sha256 = "1z4c2bymyyvhs47ynrlp4d2cwqws2d2isiwj82c33qcmk4znargg";
+  propagatedBuildInputs = [
+    MaybeT blazeHtml happstackData happstackUtil hslogger html mtl
+    network parsec sendfile syb text utf8String xhtml zlib
+  ];
+  meta = {
+    homepage = "http://happstack.com";
+    description = "Web related tools and services.";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
     ];
-    meta = {
-        description = "Web related tools and services";
-        license = "BSD";
-        maintainers = [self.stdenv.lib.maintainers.andres];
-    };
+  };
 })
