c37b3ffcdfa278789e048ece29e585c9613406ff
Peter Simons <simons@cryp.to>
2011-08-07 20:24:50 +0000

    haskell-aeson: updated to version 0.3.2.10 in an attempt to fix build failures
    That attempt was not successful, unfortunately, the package still
    doesn't compile. :-(
    
    svn path=/nixpkgs/trunk/; revision=28277

--- pkgs/development/libraries/haskell/aeson/default.nix
+++ pkgs/development/libraries/haskell/aeson/default.nix
@@ -1,18 +1,22 @@
-{cabal, attoparsec, blazeBuilder, blazeTextual, deepseq, hashable, mtl,
- syb, text, unorderedContainers, vector}:
+{cabal, attoparsec, blazeBuilder, blazeTextual, deepseq, hashable,
+ mtl, syb, text, unorderedContainers, vector} :
 
 cabal.mkDerivation (self : {
   pname = "aeson";
-  version = "0.3.2.9";
-  sha256 = "1qaajk797zpickw4ik5lc03wnmxkrcmv3zik7n1bjqx6h37h0zqw";
+  version = "0.3.2.10";
+  sha256 = "003bd6nyayd7rd9j4ncjgv7kvsncv4sb84yskqjwiq7y0b36shj8";
   propagatedBuildInputs = [
-    attoparsec blazeBuilder blazeTextual deepseq hashable mtl
-    syb text unorderedContainers vector
+    attoparsec blazeBuilder blazeTextual deepseq hashable mtl syb text
+    unorderedContainers vector
   ];
   meta = {
+    homepage = "http://github.com/mailrank/aeson";
     description = "Fast JSON parsing and encoding";
-    license = "BSD3";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
