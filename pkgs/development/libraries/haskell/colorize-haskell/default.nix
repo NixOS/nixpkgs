3f665a39ee57f5981160fff15973a21d37bfc9c9
Peter Simons <simons@cryp.to>
2011-08-07 19:34:20 +0000

    colorize-haskell: updated to version 1.0.1
    svn path=/nixpkgs/trunk/; revision=28268

--- pkgs/development/libraries/haskell/colorize-haskell/default.nix
+++ pkgs/development/libraries/haskell/colorize-haskell/default.nix
@@ -1,12 +1,15 @@
-{cabal, ansiTerminal, haskellLexer}:
+{cabal, ansiTerminal, haskellLexer} :
 
 cabal.mkDerivation (self : {
   pname = "colorize-haskell";
-  version = "1.0.0";
-  sha256 = "14c180ea3e8beb12dd289c51453bd2e3583f306799db4630c8f86cf09bbb3763";
-  propagatedBuildInputs = [ansiTerminal haskellLexer];
+  version = "1.0.1";
+  sha256 = "1v4spa6vw9igjpd1dr595z5raz5fr8f485q5w9imrv8spms46xh3";
+  propagatedBuildInputs = [ ansiTerminal haskellLexer ];
   meta = {
-    description = "Highlight Haskell source";
+    homepage = "http://github.com/yav/colorize-haskell";
+    description = "Highligt Haskell source";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
