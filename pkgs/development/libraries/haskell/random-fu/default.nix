6738bc8e93d6e462206974e13b79499804654515
Peter Simons <simons@cryp.to>
2011-08-07 20:26:06 +0000

    haskell-random-fu: updated to version 0.2
    svn path=/nixpkgs/trunk/; revision=28295

--- pkgs/development/libraries/haskell/random-fu/default.nix
+++ pkgs/development/libraries/haskell/random-fu/default.nix
@@ -1,17 +1,22 @@
-{cabal, erf, mtl, mersenneRandomPure64, monadLoops, MonadPrompt,
- mwcRandom, randomShuffle, stateref, tagged, vector, syb}:
+{cabal, erf, gamma, monadLoops, mtl, randomShuffle, randomSource,
+ rvar, syb, transformers, vector} :
 
 cabal.mkDerivation (self : {
   pname = "random-fu";
-  version = "0.1.3";
-  sha256 = "1l7czlll6y02m5xzdky95m78806gnj5y3nk3cxs5zqgxwskq73bk";
-  propagatedBuildInputs =
-    [erf mtl mersenneRandomPure64 monadLoops MonadPrompt
-     mwcRandom randomShuffle stateref tagged vector syb];
+  version = "0.2";
+  sha256 = "1w5bqhhh07xr377whgfbzn57p77j8ng5nmy2rx8qnqyw8smlkxzm";
+  propagatedBuildInputs = [
+    erf gamma monadLoops mtl randomShuffle randomSource rvar syb
+    transformers vector
+  ];
   meta = {
+    homepage = "https://github.com/mokus0/random-fu";
     description = "Random number generation";
-    license = "Public Domain";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    license = self.stdenv.lib.licenses.publicDomain;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
