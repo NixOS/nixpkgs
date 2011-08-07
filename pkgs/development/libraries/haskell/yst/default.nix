56bf74372b106e49615cd04fd9471aaf37333e2a
Peter Simons <simons@cryp.to>
2011-08-07 19:33:49 +0000

    yst: updated to version 0.2.4.1
    svn path=/nixpkgs/trunk/; revision=28261

--- pkgs/development/libraries/haskell/yst/default.nix
+++ pkgs/development/libraries/haskell/yst/default.nix
@@ -1,14 +1,19 @@
-{cabal, HStringTemplate, csv, pandoc, parsec, split, utf8String, xhtml, HsSyck, time}:
+{cabal, HStringTemplate, HsSyck, csv, pandoc, parsec, split,
+ utf8String, xhtml} :
 
 cabal.mkDerivation (self : {
   pname = "yst";
-  version = "0.2.3.2";
-  sha256 = "b857e70db67d708e2edb61a1d6bc4eaff3abd2bc252b3605f66bf1760da4da4b";
+  version = "0.2.4.1";
+  sha256 = "0y620p6kn1mky30fia63na5idppfjfmc828jcaa0ads08rmj5wgy";
   propagatedBuildInputs = [
-    HStringTemplate csv pandoc parsec split utf8String xhtml HsSyck time
+    HStringTemplate HsSyck csv pandoc parsec split utf8String xhtml
   ];
   meta = {
-    description =
-      "Builds a static website from templates and data in YAML or CSV files";
+    homepage = "http://github.com/jgm/yst";
+    description = "Builds a static website from templates and data in YAML or
+CSV files.";
+    license = "GPL";
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
