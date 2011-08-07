ea6188240a0f38571c34c28f5d7f716b04299000
Peter Simons <simons@cryp.to>
2011-08-07 18:23:19 +0000

    blaze-textual: updated to version 0.2.0.3
    svn path=/nixpkgs/trunk/; revision=28244

--- pkgs/development/libraries/haskell/blaze-textual/default.nix
+++ pkgs/development/libraries/haskell/blaze-textual/default.nix
@@ -1,14 +1,20 @@
-{cabal, blazeBuilder, text, vector}:
+{cabal, blazeBuilder, doubleConversion, text, vector}:
 
 cabal.mkDerivation (self : {
   pname = "blaze-textual";
-  version = "0.1.0.0";
-  sha256 = "0ql25b0r4xbshqsjfndl7glq0hp2ncxb3h5zd541vsqrqrf8y4gk";
-  propagatedBuildInputs = [blazeBuilder text vector];
+  version = "0.2.0.3";
+  sha256 = "0x1a8qiqd4q6qlq2lf4v31wzsbrj7403p4zf74lfl7grjgvbsnfj";
+  propagatedBuildInputs = [
+    blazeBuilder doubleConversion text vector
+  ];
   meta = {
+    homepage = "http://github.com/mailrank/blaze-textual";
     description = "Fast rendering of common datatypes";
-    license = "BSD3";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
