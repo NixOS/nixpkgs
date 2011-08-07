b5cfebc23e2f8b7e5501306302cae23792f8c399
Peter Simons <simons@cryp.to>
2011-08-07 11:14:30 +0000

    cabal2nix: disable hydra builds for the time being, it seems that fetchgit doesn't work there
    svn path=/nixpkgs/trunk/; revision=28221

--- pkgs/development/libraries/haskell/cabal2nix/default.nix
+++ pkgs/development/libraries/haskell/cabal2nix/default.nix
@@ -2,11 +2,11 @@
 
 cabal.mkDerivation (self : {
   pname = "cabal2nix";
-  version = "v1.8-beta-1";
+  version = "v1.8-beta-3";
 
   src = fetchgit {
     url = "git://github.com/peti/cabal2nix.git";
-    rev = "5ad59780b88208e20f2ba086572a2ba8783fc56f";
+    rev = "26fb761aee1b3398b3ed18e3be89507964320e4f";
   };
 
   propagatedBuildInputs = [HTTP];
@@ -14,7 +14,6 @@ cabal.mkDerivation (self : {
     homepage = "http://github.com/peti/cabal2nix";
     description = "Convert Cabal files into Nix build instructions";
     license = self.stdenv.lib.licenses.bsd3;
-    platforms = self.stdenv.lib.platforms.haskellPlatforms;
     maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
