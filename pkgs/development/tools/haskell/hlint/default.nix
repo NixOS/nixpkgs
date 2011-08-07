e29935f9b3089e214323d28168e94dd2ff7b62d4
Peter Simons <simons@cryp.to>
2011-08-07 18:22:13 +0000

    hlint: updated to version 1.8.13
    svn path=/nixpkgs/trunk/; revision=28231

--- pkgs/development/tools/haskell/hlint/default.nix
+++ pkgs/development/tools/haskell/hlint/default.nix
@@ -1,13 +1,17 @@
-{cabal, haskellSrcExts, mtl, uniplate, hscolour, parallel, transformers}:
+{cabal, cpphs, haskellSrcExts, hscolour, transformers, uniplate} :
 
 cabal.mkDerivation (self : {
   pname = "hlint";
-  version = "1.8.7";
-  name = self.fname;
-  sha256 = "0k2fwwwmq0qqb5nw5acsjr2gqnsmqcf3ckb6wdrkqsqp8g2k14mn";
-  extraBuildInputs =
-    [haskellSrcExts mtl uniplate hscolour parallel transformers];
+  version = "1.8.13";
+  sha256 = "125hvljx70b1zai3xdrarjl9fji2fq2g390rlffq428ifjrl9nk2";
+  propagatedBuildInputs = [
+    cpphs haskellSrcExts hscolour transformers uniplate
+  ];
   meta = {
+    homepage = "http://community.haskell.org/~ndm/hlint/";
     description = "Source code suggestions";
+    license = "GPL";
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
