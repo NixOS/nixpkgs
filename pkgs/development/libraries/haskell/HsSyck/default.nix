db896148184a50807815aeaa9f6ebb62886ce8cd
Peter Simons <simons@cryp.to>
2011-08-07 19:33:56 +0000

    haskell-HsSyck: updated to version 0.50
    svn path=/nixpkgs/trunk/; revision=28263

--- pkgs/development/libraries/haskell/HsSyck/default.nix
+++ pkgs/development/libraries/haskell/HsSyck/default.nix
@@ -1,10 +1,14 @@
-{cabal}:
+{cabal, syb, utf8String} :
 
 cabal.mkDerivation (self : {
   pname = "HsSyck";
-  version = "0.45";
-  sha256 = "67d02a892cd4535a02cc3ebf7b6e9f898a64428ec6783488875ac694d7fafb79";
+  version = "0.50";
+  sha256 = "0ap675i2fngvd1nw1dk8p2fz4nbd2aq5ci8dsvpcjbp28y9j2blm";
+  propagatedBuildInputs = [ syb utf8String ];
   meta = {
     description = "Fast, lightweight YAML loader and dumper";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
