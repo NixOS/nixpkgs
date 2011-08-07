e877d8c673f39f229cbe4d2570fada8ccb78155d
Peter Simons <simons@cryp.to>
2011-08-07 22:01:53 +0000

    http-types: 0.6.5.1
    svn path=/nixpkgs/trunk/; revision=28329

--- pkgs/development/libraries/haskell/http-types/default.nix
+++ pkgs/development/libraries/haskell/http-types/default.nix
@@ -1,14 +1,15 @@
-{cabal, blazeBuilder, caseInsensitive, text}:
+{cabal, blazeBuilder, caseInsensitive, text} :
 
 cabal.mkDerivation (self : {
   pname = "http-types";
-  version = "0.6.5";
-  sha256 = "1z2y219170n6rrmmffkg8xa450xzl42zpwahv7m71bxlz4cvxjc1";
-  propagatedBuildInputs = [blazeBuilder caseInsensitive text];
+  version = "0.6.5.1";
+  sha256 = "1gmf5ghfm8hzifinknyk10m7ayxkn48h1l0mchi2vl6h5rg0nnca";
+  propagatedBuildInputs = [ blazeBuilder caseInsensitive text ];
   meta = {
-    description = "Generic HTTP types for Haskell (for both client and server code)";
-    license = "BSD3";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    homepage = "https://github.com/aristidb/http-types";
+    description = "Generic HTTP types for Haskell (for both client and server code).";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
