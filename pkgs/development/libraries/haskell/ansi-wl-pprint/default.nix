ef1d541c3b23cf98e04c8e1157ab5504434e5e9f
Peter Simons <simons@cryp.to>
2011-08-07 22:02:44 +0000

    haskell-ansi-wl-pprint: cosmenic, no functional change
    svn path=/nixpkgs/trunk/; revision=28342

--- pkgs/development/libraries/haskell/ansi-wl-pprint/default.nix
+++ pkgs/development/libraries/haskell/ansi-wl-pprint/default.nix
@@ -1,12 +1,15 @@
-{cabal, ansiTerminal}:
+{cabal, ansiTerminal} :
 
 cabal.mkDerivation (self : {
   pname = "ansi-wl-pprint";
   version = "0.6.3";
   sha256 = "1cpkfn1ld0sjysksdsxxwwy1b17s4smmzk8y88y9mb81vgwlalkl";
-  propagatedBuildInputs = [ansiTerminal];
+  propagatedBuildInputs = [ ansiTerminal ];
   meta = {
+    homepage = "http://github.com/batterseapower/ansi-wl-pprint";
     description = "The Wadler/Leijen Pretty Printer for colored ANSI terminal output";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
