883c69943dfa24ac16c2bf733669bbd9da50afa8
Peter Simons <simons@cryp.to>
2011-08-07 20:25:36 +0000

    haskell-test-framework: updated to version 0.4.1.1
    svn path=/nixpkgs/trunk/; revision=28288

--- pkgs/development/libraries/haskell/test-framework/default.nix
+++ pkgs/development/libraries/haskell/test-framework/default.nix
@@ -1,14 +1,20 @@
-{cabal, ansiTerminal, ansiWLPprint, hostname, regexPosix, xml}:
+{cabal, ansiTerminal, ansiWlPprint, hostname, regexPosix, xml} :
 
 cabal.mkDerivation (self : {
   pname = "test-framework";
-  version = "0.4.0";
-  sha256 = "0zxrdndycr63kzfibk0c2n4j39x6b8s4332sgqm54g1vdl3fxzbl";
-  propagatedBuildInputs = [ansiTerminal ansiWLPprint hostname regexPosix xml];
+  version = "0.4.1.1";
+  sha256 = "1ig4v2y8xba2rg6pc8yjf0j20cwa3czknnfqn1sgpqhmip2961pc";
+  propagatedBuildInputs = [
+    ansiTerminal ansiWlPprint hostname regexPosix xml
+  ];
   meta = {
-    description = "Framework for running and organising tests";
-    license = "BSD3";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    homepage = "http://batterseapower.github.com/test-framework/";
+    description = "Framework for running and organising tests, with HUnit and QuickCheck support";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
