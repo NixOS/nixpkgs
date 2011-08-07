3ec87421dbf059d88fd6f23be842313735dd996e
Peter Simons <simons@cryp.to>
2011-08-07 22:16:18 +0000

    wxHaskell-wxdirect.nix: updated to version 0.12.1.4
    svn path=/nixpkgs/trunk/; revision=28361

--- pkgs/development/libraries/haskell/wxHaskell/wxdirect.nix
+++ pkgs/development/libraries/haskell/wxHaskell/wxdirect.nix
@@ -1,16 +1,15 @@
-{cabal, parsec, time}:
+{cabal, parsec} :
 
 cabal.mkDerivation (self : {
   pname = "wxdirect";
-  version = "0.12.1.3";
-  sha256 = "da59315339dc78b3bfbe08c1681d53c74a56e7c3de0f41a90099bd289b1bfb11";
-  preConfigure = ''
-    sed -i 's|\(containers.*\) && < 0.4|\1|' ${self.pname}.cabal
-    sed -i 's|\(time.*\) && < 1.2|\1|' ${self.pname}.cabal
-  '';
-  propagatedBuildInputs = [parsec time];
+  version = "0.12.1.4";
+  sha256 = "0v1blh3l02h58cvsngfax5knmg51lil1kj6pr5iqrbcrivp2nh7f";
+  propagatedBuildInputs = [ parsec ];
   meta = {
+    homepage = "http://haskell.org/haskellwiki/WxHaskell";
     description = "helper tool for building wxHaskell";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
