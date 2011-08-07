ac4fa33ad366af5c111394e84820cd513796a124
Peter Simons <simons@cryp.to>
2011-08-07 22:03:12 +0000

    haskell-haskeline: updated to version 0.6.4.0
    svn path=/nixpkgs/trunk/; revision=28349

--- pkgs/development/libraries/haskell/haskeline/default.nix
+++ pkgs/development/libraries/haskell/haskeline/default.nix
@@ -1,12 +1,15 @@
-{cabal, extensibleExceptions, mtl, utf8String}:
+{cabal, mtl, terminfo, utf8String} :
 
 cabal.mkDerivation (self : {
   pname = "haskeline";
-  version = "0.6.3.2";
-  sha256 = "0ragimzrilsk5r8n0fbq3lyjww28bmc1r1vgjf9pb4kpfpbz0cq8";
-  propagatedBuildInputs = [extensibleExceptions mtl utf8String];
+  version = "0.6.4.0";
+  sha256 = "0p2qbckvdhzid6zrcgjwr8b5h8vxd7wdswsm2qp3hiyg48v4y5hd";
+  propagatedBuildInputs = [ mtl terminfo utf8String ];
   meta = {
-    description = "A command-line interface for user input, written in Haskell";
+    homepage = "http://trac.haskell.org/haskeline";
+    description = "A command-line interface for user input, written in Haskell.";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
-
