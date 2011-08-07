0d787c8a2efffda712aa4f788cdc29aa41ba90c2
Peter Simons <simons@cryp.to>
2011-08-07 20:24:30 +0000

    haskell-monads-fd: updated to version 0.2.1
    svn path=/nixpkgs/trunk/; revision=28273

--- pkgs/development/libraries/haskell/monads-fd/default.nix
+++ pkgs/development/libraries/haskell/monads-fd/default.nix
@@ -1,14 +1,17 @@
-{cabal, transformers}:
+{cabal, mtl, transformers} :
 
 cabal.mkDerivation (self : {
   pname = "monads-fd";
-  version = "0.0.0.1";
-  sha256 = "7eaac97b2c91a670171f18ebeb3f73b1a45e16576851279f73ea2e1f5ec63b25";
-  propagatedBuildInputs = [transformers];
+  version = "0.2.0.0";
+  sha256 = "1iqr5p3va5sxmpvydwqz2src54j5njcyrzn9p5apc60nv7yv6x4c";
+  propagatedBuildInputs = [ mtl transformers ];
   meta = {
     description = "Monad classes, using functional dependencies";
-    license = "BSD";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.stdenv.lib.platforms.haskellPlatforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
-
