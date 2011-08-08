9049a3000d800826f0f1b600077cc2caa38f7cb7
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-08-08 19:55:14 +0000

    * Update nixUnstable.
    svn path=/nixpkgs/trunk/; revision=28403

--- pkgs/tools/package-management/nix/unstable.nix
+++ pkgs/tools/package-management/nix/unstable.nix
@@ -5,11 +5,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "nix-1.0pre27764";
+  name = "nix-1.0pre28391";
 
   src = fetchurl {
-    url = "http://hydra.nixos.org/build/1162354/download/4/${name}.tar.bz2";
-    sha256 = "fe8117644a67947dd8aa8594181ff893b09806ea7803c842cc0b5be86a31a9c6";
+    url = "http://hydra.nixos.org/build/1232663/download/4/${name}.tar.bz2";
+    sha256 = "c990f0ad564960b1d82fdbed32bee0db64c26b57400f027f01434237edd1be0f";
   };
 
   buildNativeInputs = [ perl pkgconfig ];
