595a6d59e6231eee3ae4df06593fa43914e2d200
Andres Löh <mail@andres-loeh.de>
2011-08-08 09:47:38 +0000

    Updated haskell-src-exts.
    svn path=/nixpkgs/trunk/; revision=28378

--- pkgs/development/libraries/haskell/haskell-src-exts/1.11.1.nix
+++ pkgs/development/libraries/haskell/haskell-src-exts/1.11.1.nix
@@ -1,15 +1,19 @@
-{cabal, cpphs, happy} :
+{cabal, happy, cpphs} :
 
 cabal.mkDerivation (self : {
   pname = "haskell-src-exts";
   version = "1.11.1";
   sha256 = "1jqf8l81zw7x5ryf8h2n0b2636yhxkfp3j4ndbqw6hc7i5q581m6";
-  propagatedBuildInputs = [ cpphs happy ];
+  extraBuildInputs = [ happy ];
+  propagatedBuildInputs = [ cpphs ];
   meta = {
     homepage = "http://code.haskell.org/haskell-src-exts";
     description = "Manipulating Haskell source: abstract syntax, lexer, parser, and pretty-printer";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.stdenv.lib.platforms.haskellPlatforms;
-    maintainers = [ self.stdenv.lib.maintainers.simons ];
+    maintainers = [
+      self.stdenv.lib.maintainers.simons
+      self.stdenv.lib.maintainers.andres
+    ];
   };
 })
