80d817fcb79b382c5ef3a3205295ee24fe35d8fe
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-08-08 17:16:49 +0000

    * Verify whether Xfce users get ownership of devices.
    svn path=/nixos/trunk/; revision=28396

--- tests/xfce.nix
+++ tests/xfce.nix
@@ -17,8 +17,12 @@
 
   testScript =
     ''
-      $machine->waitForWindow(qr/Tips/);
+      $machine->waitForWindow(qr/Tips/);      
       $machine->sleep(10);
+
+      # Check that logging in has given the user ownership of devices.
+      $machine->succeed("getfacl /dev/snd/timer | grep -q alice");
+      
       $machine->succeed("su - alice -c 'DISPLAY=:0.0 Terminal &'");
       $machine->waitForWindow(qr/Terminal/);
       $machine->sleep(10);
