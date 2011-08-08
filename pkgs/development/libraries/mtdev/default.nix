d66debd06a3cdb42fb92bf0cf9e276d93d05ec6d
Shea Levy <shea@shealevy.com>
2011-08-08 00:25:46 +0000

    mtdev: Added meta information
    svn path=/nixpkgs/trunk/; revision=28367

--- pkgs/development/libraries/mtdev/default.nix
+++ pkgs/development/libraries/mtdev/default.nix
@@ -7,5 +7,23 @@ stdenv.mkDerivation {
     url = "http://bitmath.org/code/mtdev/mtdev-1.1.0.tar.gz";
     sha256 = "14mky2vrzgy3x6k3rwkkpqkqyivbr6ym99gj5jmil9fqa9644lw4";
   };
+
+  meta = {
+    homepage = http://bitmath.org/code/mtdev/;
+
+    description = "Multitouch Protocol Translation Library";
+
+    longDescription = ''
+      The mtdev is a stand-alone library which transforms all variants of
+      kernel MT events to the slotted type B protocol. The events put into
+      mtdev may be from any MT device, specifically type A without contact
+      tracking, type A with contact tracking, or type B with contact tracking.
+      See the kernel documentation for further details. 
+    '';
+
+    license = "MIT/X11";
+
+    maintainers = [ stdenv.lib.maintainers.shlevy ];
+  };
 }
 
