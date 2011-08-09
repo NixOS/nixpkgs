ff510a051697c59faa5621dc32e405483a15491b
Peter Simons <simons@cryp.to>
2011-08-09 08:17:35 +0000

    pkgs/development/libraries/haskell/cabal2nix: updated to latest version
    svn path=/nixpkgs/trunk/; revision=28413

--- pkgs/development/libraries/haskell/cabal2nix/default.nix
+++ pkgs/development/libraries/haskell/cabal2nix/default.nix
@@ -2,11 +2,11 @@
 
 cabal.mkDerivation (self : {
   pname = "cabal2nix";
-  version = "v1.8-beta-9";
+  version = "v1.8-beta-10";
 
   src = fetchgit {
     url = "git://github.com/peti/cabal2nix.git";
-    rev = "8ba91f9b69851c55a4414e40e27b132754be4885";
+    rev = "3c91f4d1c5a881ebf5bf7eef3771f0267c08bad4";
   };
 
   propagatedBuildInputs = [HTTP];
