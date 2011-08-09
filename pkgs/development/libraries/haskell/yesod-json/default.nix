1eb2c6573199ad5146f472c2e59477b7994abc57
Andres Löh <mail@andres-loeh.de>
2011-08-09 20:13:22 +0000

    Update yesod-json.
    svn path=/nixpkgs/trunk/; revision=28439

--- pkgs/development/libraries/haskell/yesod-json/default.nix
+++ pkgs/development/libraries/haskell/yesod-json/default.nix
@@ -1,16 +1,15 @@
-{cabal, aeson, blazeTextual, hamlet, text, vector, yesodCore}:
+{ cabal, aeson, blazeTextual, hamlet, text, vector, yesodCore }:
 
-cabal.mkDerivation (self : {
+cabal.mkDerivation (self: {
   pname = "yesod-json";
-  version = "0.1.1.1";
-  sha256 = "02mly02c6z49s0gznx47w919gcn7qz1qvr8704sab1sjk87cyjwl";
-  propagatedBuildInputs = [
-    aeson blazeTextual hamlet text vector yesodCore
-  ];
+  version = "0.1.1.2";
+  sha256 = "0d6dkhzjpxp3687x914h67swm4lgsalb1xr13gz53ddb0qj18n7j";
+  buildDepends = [ aeson blazeTextual hamlet text vector yesodCore ];
   meta = {
-    description = "Generate content for Yesod using the aeson package";
-    license = "BSD3";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    homepage = "http://www.yesodweb.com/";
+    description = "Generate content for Yesod using the aeson package.";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+    maintainers = [ self.stdenv.lib.maintainers.andres ];
   };
 })
-
