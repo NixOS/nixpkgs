799d82249bf9a5f1c1b1980bf0da47b9bd2485ee
Yury G. Kudryashov <urkud.urkud@gmail.com>
2011-08-10 20:19:49 +0000

    ntrack-014, use libnl-3.x
    svn path=/nixpkgs/trunk/; revision=28469

--- pkgs/development/libraries/ntrack/default.nix
+++ pkgs/development/libraries/ntrack/default.nix
@@ -1,7 +1,7 @@
-{ stdenv, fetchurl, glib, qt4, pkgconfig, libnl2, pygobject, python }:
+{ stdenv, fetchurl, glib, qt4, pkgconfig, libnl, pygobject, python }:
 
 let
-  version = "011";
+  version = "014";
 in
 
 stdenv.mkDerivation rec {
@@ -9,16 +9,15 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "http://launchpad.net/ntrack/main/${version}/+download/${name}.tar.gz";
-    sha256 = "0qi6nhymsv7w6hfxnz9jbxk311wb6x9jci7a3gcr4cc5nwkl7sxy";
+    sha256 = "1aqn3q0dj2kk0j9rf02qgbfghlykaas7q0g8wxyz7nd6zg4qhyj2";
   };
 
-  buildInputs = [ libnl2 qt4 ];
+  buildInputs = [ libnl qt4 ];
 
   buildNativeInputs = [ pkgconfig python ];
 
   configureFlags = "--without-gobject CFLAGS=--std=gnu99";
 
-  patches = [ ./libnl2.patch ];
   postPatch = ''
     sed -e "s@/usr\(/lib/ntrack/modules/\)@$out&@" -i common/ntrack.c
     '';
