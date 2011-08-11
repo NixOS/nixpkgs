622a12a4053633cc104291a934e1ccfcd686af73
Peter Simons <simons@cryp.to>
2011-08-11 10:24:39 +0000

    haskell-MazesOfMonad: updated to version 1.0.7
    svn path=/nixpkgs/trunk/; revision=28484

--- pkgs/games/MazesOfMonad/default.nix
+++ pkgs/games/MazesOfMonad/default.nix
@@ -1,13 +1,19 @@
-{cabal, HUnit, mtl, regexPosix, time}:
+{ cabal, HUnit, mtl, regexPosix, time }:
 
-cabal.mkDerivation (self : {
+cabal.mkDerivation (self: {
   pname = "MazesOfMonad";
-  version = "1.0.5";
-  name = self.fname;
-  sha256 = "a068df09975e7a3060cbd69191cbf99cb3a7b0ee524deb61eef4c52e7fada3f3";
-  propagatedBuildInputs = [HUnit mtl regexPosix time];
+  version = "1.0.7";
+  sha256 = "1zk6bckll03b40iq8z13753glkmcan6439w8cc6rn5h2fhp189v9";
+  isLibrary = false;
+  isExecutable = true;
+  buildDepends = [ HUnit mtl regexPosix time ];
   meta = {
     description = "Console-based Role Playing Game";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.andres
+      self.stdenv.lib.maintainers.simons
+    ];
   };
-})  
-
+})
