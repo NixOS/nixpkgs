fa2e5927bd61e683e135bcbe22c23b7eec062938
Peter Simons <simons@cryp.to>
2011-08-11 10:45:06 +0000

    haskell-fclabels: updated to version 0.11.2
    svn path=/nixpkgs/trunk/; revision=28488

--- pkgs/development/libraries/haskell/fclabels/default.nix
+++ pkgs/development/libraries/haskell/fclabels/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "fclabels";
-  version = "0.11.1.1";
-  sha256 = "09zs9ddhhz57r2fm6ayc95xgsfjcympmgiify2p6f42w9hs34dmf";
+  version = "0.11.2";
+  sha256 = "0ish0gy2f3q65c9f4ix0nhcid9kpr7faijxkmwcy9bymjfg441kf";
   buildDepends = [ mtl ];
   meta = {
     description = "First class accessor labels implemented as lenses.";
