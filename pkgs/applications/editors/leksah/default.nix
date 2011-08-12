493e36669c11f81a35a4cafb016017fdb0e9c59f
Peter Simons <simons@cryp.to>
2011-08-12 11:02:44 +0000

    haskell-leksah: updated build expression for recent versions of cabal.nix
    svn path=/nixpkgs/trunk/; revision=28512

--- pkgs/applications/editors/leksah/default.nix
+++ pkgs/applications/editors/leksah/default.nix
@@ -1,15 +1,18 @@
-{cabal, binary, binaryShared, deepseq, glib, gtk,
- gtksourceview2, hslogger, leksahServer, ltk, mtl, network,
- parsec, processLeksah, regexBase, regexTDFA, strict, utf8String} :
+{ cabal, binary, binaryShared, deepseq, glib, gtk, gtksourceview2
+, hslogger, leksahServer, ltk, mtl, network, parsec, processLeksah
+, regexBase, regexTdfa, strict, time, utf8String
+}:
 
-cabal.mkDerivation (self : {
+cabal.mkDerivation (self: {
   pname = "leksah";
   version = "0.10.0.4";
   sha256 = "1g12w1kl63fxzz1c2x237yrqkaja9awiqyyipkdms5iql0ini7bw";
-  propagatedBuildInputs = [
+  isLibrary = true;
+  isExecutable = true;
+  buildDepends = [
     binary binaryShared deepseq glib gtk gtksourceview2 hslogger
     leksahServer ltk mtl network parsec processLeksah regexBase
-    regexTDFA strict utf8String
+    regexTdfa strict time utf8String
   ];
   noHaddock = true;
   meta = {
@@ -17,6 +20,9 @@ cabal.mkDerivation (self : {
     description = "Haskell IDE written in Haskell";
     license = "GPL";
     platforms = self.stdenv.lib.platforms.linux;
-    maintainers = [ self.stdenv.lib.maintainers.andres ];
+    maintainers = [
+      self.stdenv.lib.maintainers.andres
+      self.stdenv.lib.maintainers.simons
+    ];
   };
 })
