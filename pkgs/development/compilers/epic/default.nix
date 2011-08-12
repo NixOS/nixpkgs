0891479ca6b821bff8ee5c5672f3bb1016d4164b
Peter Simons <simons@cryp.to>
2011-08-12 11:02:48 +0000

    haskell-epic: updated to version 0.1.11
    svn path=/nixpkgs/trunk/; revision=28513

--- pkgs/development/compilers/epic/default.nix
+++ pkgs/development/compilers/epic/default.nix
@@ -1,15 +1,23 @@
-{cabal, mtl, happy, gmp, boehmgc}:
+{ cabal, mtl, happy, gmp, boehmgc }:
 
-cabal.mkDerivation (self : {
+cabal.mkDerivation (self: {
   pname = "epic";
-  version = "0.1.10";
-  sha256 = "0iaq0mswf370jk4r2xyby8qa5ihcydnbkr478p35x2i56axp60ji";
-  propagatedBuildInputs = [mtl];
-  extraBuildInputs = [happy gmp boehmgc];
+  version = "0.1.11";
+  sha256 = "12dz1wjaf3n8fqk46vhpnxq9z633wi6wyihcmif7amxmqv3l8zn9";
+  isLibrary = true;
+  isExecutable = true;
+  buildDepends = [ mtl ];
+  extraLibraries = [ gmp boehmgc ];
   noHaddock = true;
+  buildTools = [ happy ];
   meta = {
-    description = "An experimental language with full dependent types";
-    license = "BSD";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    homepage = "http://www.dcs.st-and.ac.uk/~eb/epic.php";
+    description = "Compiler for a simple functional language";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.andres
+      self.stdenv.lib.maintainers.simons
+    ];
   };
 })
