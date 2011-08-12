3cb574c748af098b4c2ef24b4478e10585e314a2
Peter Simons <simons@cryp.to>
2011-08-12 12:40:03 +0000

    haskell-uuagc: added missing meta.platforms
    svn path=/nixpkgs/trunk/; revision=28516

--- pkgs/development/tools/haskell/uuagc/cabal.nix
+++ pkgs/development/tools/haskell/uuagc/cabal.nix
@@ -9,6 +9,7 @@ cabal.mkDerivation (self: {
     homepage = "http://www.cs.uu.nl/wiki/HUT/WebHome";
     description = "Cabal plugin for the Universiteit Utrecht Attribute Grammar System";
     license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
     maintainers = [
       self.stdenv.lib.maintainers.andres
       self.stdenv.lib.maintainers.simons
