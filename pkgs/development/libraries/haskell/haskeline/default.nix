ac71f2adb8fc885d2030130d850885fc6faf6ea2
Peter Simons <simons@cryp.to>
2011-08-12 00:23:12 +0000

    haskell-haskeline: fixed build with ghc 7.2.1
    svn path=/nixpkgs/trunk/; revision=28501

--- pkgs/development/libraries/haskell/haskeline/default.nix
+++ pkgs/development/libraries/haskell/haskeline/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.6.4.0";
   sha256 = "0p2qbckvdhzid6zrcgjwr8b5h8vxd7wdswsm2qp3hiyg48v4y5hd";
   buildDepends = [ extensibleExceptions mtl terminfo utf8String ];
+  patchPhase = "sed -i -e 's|base >=4.1 && < 4.4|base >=4.1 \\&\\& < 5|g' -e 's|unix>=2.0 && < 2.5|unix>=2.0 \\&\\& < 3|g' haskeline.cabal";
   meta = {
     homepage = "http://trac.haskell.org/haskeline";
     description = "A command-line interface for user input, written in Haskell";
