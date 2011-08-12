7a0ad70b63fda1af040a5cbbb6f50ee688e8e500
Peter Simons <simons@cryp.to>
2011-08-12 18:43:20 +0000

    haskell-dimensional: updated to version 0.10.1
    svn path=/nixpkgs/trunk/; revision=28526

--- pkgs/development/libraries/haskell/dimensional/default.nix
+++ pkgs/development/libraries/haskell/dimensional/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "dimensional";
-  version = "0.10";
-  sha256 = "5d0ab2a0ca566f7d9a4fe1ec180a1bcf4138a3647a2c287a908506c8911cd385";
+  version = "0.10.1";
+  sha256 = "0pnrqm6qny09g8iy191844d0mvvyzg1j637pvwwwhci8lzq3hfyd";
   buildDepends = [ numtype time ];
   meta = {
     homepage = "http://dimensional.googlecode.com/";
