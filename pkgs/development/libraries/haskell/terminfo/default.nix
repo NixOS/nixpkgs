c4e4a6057394fe9ff323254e21745413f1e74372
Peter Simons <simons@cryp.to>
2011-08-13 23:38:52 +0000

    haskell-terminfo: updated to version 0.3.2.2
    svn path=/nixpkgs/trunk/; revision=28556

--- pkgs/development/libraries/haskell/terminfo/default.nix
+++ pkgs/development/libraries/haskell/terminfo/default.nix
@@ -1,13 +1,14 @@
-{cabal, extensibleExceptions, ncurses}:
+{ cabal, extensibleExceptions, ncurses }:
 
-cabal.mkDerivation (self : {
+cabal.mkDerivation (self: {
   pname = "terminfo";
-  version = "0.3.2";
-  sha256 = "0sfb6p6gj29wahp45plai7bygyyhhcaw6ld5xf90clkxy5pcsw1a";
-  propagatedBuildInputs = [extensibleExceptions ncurses];
+  version = "0.3.2.2";
+  sha256 = "1370vxvv32aarmk64yvwb8rav523xk7fg5h65cgxvn4ppjqv0f51";
+  buildDepends = [ extensibleExceptions ];
+  extraLibraries = [ ncurses ];
   meta = {
     homepage = "http://code.haskell.org/terminfo";
-    description = "Haskell bindings to the terminfo library.";
+    description = "Haskell bindings to the terminfo library";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [ self.stdenv.lib.maintainers.simons ];
