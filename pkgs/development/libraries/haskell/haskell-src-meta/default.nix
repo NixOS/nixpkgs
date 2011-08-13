f15bdc50c4ff597f2da6e1106745e18d2e3fb374
Peter Simons <simons@cryp.to>
2011-08-13 13:57:39 +0000

    haskell-haskell-src-meta: updated to version 0.4.0.2
    svn path=/nixpkgs/trunk/; revision=28539

--- pkgs/development/libraries/haskell/haskell-src-meta/default.nix
+++ pkgs/development/libraries/haskell/haskell-src-meta/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "haskell-src-meta";
-  version = "0.4.0.1";
-  sha256 = "10g74sax8x45lphy133717y9xb43d8a684mm9qv0arjwn5v4np7s";
+  version = "0.4.0.2";
+  sha256 = "1i3lhik0sa34x6rnkfa9scai9cdgx05h6mrbhwsfc7p2jlsixk92";
   buildDepends = [ haskellSrcExts syb thLift ];
   meta = {
     description = "Parse source to template-haskell abstract syntax";
