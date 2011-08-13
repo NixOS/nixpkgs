b0b297ecb706e560a79911ef74e433939fb66291
Peter Simons <simons@cryp.to>
2011-08-13 23:38:23 +0000

    Regenerated haskell-wx and haskell-wxdirect expressions; no functional change.
    svn path=/nixpkgs/trunk/; revision=28550

--- pkgs/development/libraries/haskell/wxHaskell/wx.nix
+++ pkgs/development/libraries/haskell/wxHaskell/wx.nix
@@ -1,13 +1,18 @@
-{cabal, stm, wxcore} :
+{ cabal, stm, wxcore }:
 
-cabal.mkDerivation (self : {
+cabal.mkDerivation (self: {
   pname = "wx";
   version = "0.12.1.6";
   sha256 = "1p0gn46gk1abin095va22n9bycxhm2cq1vyvwiypcdq7jq541lhk";
-  propagatedBuildInputs = [ stm wxcore ];
+  buildDepends = [ stm wxcore ];
   meta = {
     homepage = "http://haskell.org/haskellwiki/WxHaskell";
     description = "wxHaskell";
     license = "LGPL";
+    platforms = self.ghc.meta.platforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.andres
+      self.stdenv.lib.maintainers.simons
+    ];
   };
 })
--- pkgs/development/libraries/haskell/wxHaskell/wxdirect.nix
+++ pkgs/development/libraries/haskell/wxHaskell/wxdirect.nix
@@ -1,15 +1,20 @@
-{cabal, parsec} :
+{ cabal, parsec, time }:
 
-cabal.mkDerivation (self : {
+cabal.mkDerivation (self: {
   pname = "wxdirect";
   version = "0.12.1.4";
   sha256 = "0v1blh3l02h58cvsngfax5knmg51lil1kj6pr5iqrbcrivp2nh7f";
-  propagatedBuildInputs = [ parsec ];
+  isLibrary = true;
+  isExecutable = true;
+  buildDepends = [ parsec time ];
   meta = {
     homepage = "http://haskell.org/haskellwiki/WxHaskell";
     description = "helper tool for building wxHaskell";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
-    maintainers = [ self.stdenv.lib.maintainers.simons ];
+    maintainers = [
+      self.stdenv.lib.maintainers.andres
+      self.stdenv.lib.maintainers.simons
+    ];
   };
 })
