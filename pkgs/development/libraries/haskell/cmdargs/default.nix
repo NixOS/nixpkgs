54acaed0c371ec30f2d1d9386fd2c376403751fe
Peter Simons <simons@cryp.to>
2011-08-13 13:57:22 +0000

    haskell-cmdargs: updated to version 0.8
    svn path=/nixpkgs/trunk/; revision=28535

--- pkgs/development/libraries/haskell/cmdargs/default.nix
+++ pkgs/development/libraries/haskell/cmdargs/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "cmdargs";
-  version = "0.7";
-  sha256 = "0qijfdc66f0r2k272sl41nxfymmsk7naw5is3b4zyxsgm147c0vq";
+  version = "0.8";
+  sha256 = "0yhjg6rck1aj8gq07i9dcfxyjffxlr4lxnir2brkyahpfx5iyw9k";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [ transformers ];
