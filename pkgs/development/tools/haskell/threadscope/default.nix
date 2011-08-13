26dce48e24c960b7f3067515f8290a0dfc5124ed
Peter Simons <simons@cryp.to>
2011-08-13 23:38:40 +0000

    haskell-threadscope: updated to version 0.1.3
    svn path=/nixpkgs/trunk/; revision=28553

--- pkgs/development/tools/haskell/threadscope/default.nix
+++ pkgs/development/tools/haskell/threadscope/default.nix
@@ -1,18 +1,20 @@
-{cabal, binary, cairo, ghcEvents, glade, gtk, mtl}:
+{ cabal, binary, cairo, ghcEvents, glade, gtk, mtl }:
 
-cabal.mkDerivation (self : {
+cabal.mkDerivation (self: {
   pname = "threadscope";
-  version = "0.1.2";
-  sha256 = "ce1116016f6b2d38e6063ba3dd147f38147a9c4399160f37aba9c50c96d00a90";
-  propagatedBuildInputs = [binary cairo ghcEvents glade gtk mtl];
-  preConfigure = ''
-    sed -i 's|\(containers.*\) && < 0.4|\1|' ${self.pname}.cabal
-  '';
+  version = "0.1.3";
+  sha256 = "1vak3624vrnkfvwxzfw5hkc0552v213jb874f6q536g5vhjjxpih";
+  isLibrary = false;
+  isExecutable = true;
+  buildDepends = [ binary cairo ghcEvents glade gtk mtl ];
   configureFlags = "--ghc-options=-rtsopts";
   meta = {
     description = "A graphical thread profiler";
-    license = "BSD";
-    maintainers = [self.stdenv.lib.maintainers.andres];
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.andres
+      self.stdenv.lib.maintainers.simons
+    ];
   };
-})  
-
+})
