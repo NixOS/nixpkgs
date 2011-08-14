db565e9a8bd98b80e474f1401a6f8d81515fae6e
Peter Simons <simons@cryp.to>
2011-08-14 10:45:02 +0000

    haskell-citeproc-hs: updated to version 0.3.3
    svn path=/nixpkgs/trunk/; revision=28567

--- pkgs/development/libraries/haskell/citeproc-hs/default.nix
+++ pkgs/development/libraries/haskell/citeproc-hs/default.nix
@@ -1,13 +1,18 @@
-{cabal, json, mtl, pandocTypes, parsec, syb, utf8String, xml}:
+{ cabal, HTTP, hsBibutils, json, mtl, network, pandocTypes, parsec
+, syb, time, utf8String, xml
+}:
 
-cabal.mkDerivation (self : {
+cabal.mkDerivation (self: {
   pname = "citeproc-hs";
-  version = "0.3.2";
-  sha256 = "04lq0w1yjasn4i9siqpw41ia9f67xlv7vqwhs2a87hr1jnr09pgf";
-  propagatedBuildInputs =
-    [json mtl pandocTypes parsec syb utf8String xml];
+  version = "0.3.3";
+  sha256 = "0yhzqxrr7jn1h0r2vy1jmlrf0z64qcr2fl37i04rpiwzb3nc16r4";
+  buildDepends = [
+    HTTP hsBibutils json mtl network pandocTypes parsec syb time
+    utf8String xml
+  ];
   meta = {
+    homepage = "http://gorgias.mine.nu/repos/citeproc-hs/";
     description = "A Citation Style Language implementation in Haskell";
+    license = self.stdenv.lib.licenses.bsd3;
   };
 })
-
