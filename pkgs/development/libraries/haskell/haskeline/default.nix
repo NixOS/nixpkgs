b19f73027d225df4ab8d5e4613d0d24519c78e4a
Peter Simons <simons@cryp.to>
2011-08-14 10:45:11 +0000

    haskell-haskeline: updated to version 0.6.4.3
    svn path=/nixpkgs/trunk/; revision=28569

--- pkgs/development/libraries/haskell/haskeline/default.nix
+++ pkgs/development/libraries/haskell/haskeline/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "haskeline";
-  version = "0.6.4.2";
-  sha256 = "0za3n0kyfnya9lc6p07702rn393ghhsb3b08i1lpxz6587fbdzjb";
+  version = "0.6.4.3";
+  sha256 = "1dlrsazprvn6xcd12k5ln536rv9sljjccrjgpq6jj6b9ziadwiwr";
   buildDepends = [ extensibleExceptions mtl terminfo utf8String ];
   meta = {
     homepage = "http://trac.haskell.org/haskeline";
