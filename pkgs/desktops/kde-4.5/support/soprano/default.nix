06a34e58252de1bf6617be87f1d918379b808d48
Yury G. Kudryashov <urkud.urkud@gmail.com>
2011-08-15 20:45:48 +0000

    Use soprano-2.7.0 in kde-4.5.5
    svn path=/nixpkgs/trunk/; revision=28601

--- pkgs/desktops/kde-4.5/support/soprano/default.nix
+++ pkgs/desktops/kde-4.5/support/soprano/default.nix
@@ -1,15 +1,15 @@
-{stdenv, fetchurl, cmake, qt4, cluceneCore, redland, libiodbc}:
+{ stdenv, fetchurl, cmake, qt4, clucene_core, redland, libiodbc }:
 
 stdenv.mkDerivation rec {
-  name = "soprano-2.5.3";
+  name = "soprano-2.7.0";
 
   src = fetchurl {
     url = "mirror://sourceforge/soprano/${name}.tar.bz2";
-    sha256 = "0hxc6jnbh0529jsc0ixvy8pshnffrpgsadinhk9navkpyn5xg4l9";
+    sha256 = "1ki92wg0i9nhn1fh5mdcls5h9h3lf2k5r66snsags4x7zw0dmv2z";
   };
 
   # We disable the Java backend, since we do not need them and they make the closure size much bigger
-  buildInputs = [ cmake qt4 cluceneCore redland libiodbc ];
+  buildInputs = [ cmake qt4 clucene_core redland libiodbc ];
 
   meta = {
     homepage = http://soprano.sourceforge.net/;
