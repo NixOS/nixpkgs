acbc9e63dc52e83915a9d6f0b1f45ebf8ef571eb
Peter Simons <simons@cryp.to>
2011-08-15 10:55:01 +0000

    haskell-extensible-exceptions: added version 0.1.1.2
    svn path=/nixpkgs/trunk/; revision=28579

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -544,7 +544,8 @@ let result = let callPackage = x : y : modifyPrio (newScope result.final x y);
 
   emgm = callPackage ../development/libraries/haskell/emgm {};
 
-  extensibleExceptions_0_1_1_0 = callPackage ../development/libraries/haskell/extensible-exceptions {};
+  extensibleExceptions_0_1_1_0 = callPackage ../development/libraries/haskell/extensible-exceptions/0.1.1.0.nix {};
+  extensibleExceptions_0_1_1_2 = callPackage ../development/libraries/haskell/extensible-exceptions/0.1.1.2.nix {};
   extensibleExceptions = null; # a core package in recent GHCs
 
   failure = callPackage ../development/libraries/haskell/failure {};
