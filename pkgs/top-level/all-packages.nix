ea5a0a00a020027ea84eaa60124e1ca78a2988bf
Peter Simons <simons@cryp.to>
2011-08-16 15:20:44 +0000

    Renamed boost's default.nix to 1.47.0.nix for sake of consistency.
    svn path=/nixpkgs/trunk/; revision=28615

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3019,11 +3019,11 @@ let
 
   boolstuff = callPackage ../development/libraries/boolstuff { };
 
-  boost = callPackage ../development/libraries/boost { };
-
   boost142 = callPackage ../development/libraries/boost/1.42.nix { };
   boost144 = callPackage ../development/libraries/boost/1.44.nix { };
   boost146 = callPackage ../development/libraries/boost/1.46.nix { };
+  boost147 = callPackage ../development/libraries/boost/1.47.nix { };
+  boost = boost147;
 
   # A Boost build with all library variants enabled.  Very large (about 250 MB).
   boostFull = appendToName "full" (boost.override {
