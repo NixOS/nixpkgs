8db5491040489d8badda1692df4e8a60733a088b
Marco Maggesi <maggesi@math.unifi.it>
2011-08-17 16:52:38 +0000

    Ooops, over-simplified
    svn path=/nixpkgs/trunk/; revision=28647

--- pkgs/os-specific/linux/blcr/default.nix
+++ pkgs/os-specific/linux/blcr/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, kernel, perl }:
+{ stdenv, fetchurl, kernel, perl, makeWrapper }:
 
 assert stdenv.isLinux;
 
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
     sha256 = "c8243c9a7a215d4fc4e8f2199045711cf711a6f2e0b39a94413478ffae110eac";
   };
 
-  buildInputs = [ perl ];
+  buildInputs = [ perl makeWrapper ];
 
   preConfigure = ''
     configureFlagsArray=(
@@ -18,6 +18,13 @@ stdenv.mkDerivation {
       --with-kmod-dir=$out/lib/modules/${kernel.version}
       --with-system-map=${kernel}/System.map
     )
+  '';
+
+  postInstall = ''
+    for prog in "$out/bin/"*
+    do
+      wrapProgram "$prog" --prefix LD_LIBRARY_PATH ":" "$out/lib"
+    done
   '';
       
   meta = {
