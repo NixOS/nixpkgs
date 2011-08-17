93829bfe4714e75d395e778853d3927f46ac7f54
Marco Maggesi <maggesi@math.unifi.it>
2011-08-17 14:25:56 +0000

    Update BLCR to version 0.8.3
    svn path=/nixpkgs/trunk/; revision=28642

--- pkgs/os-specific/linux/blcr/default.nix
+++ pkgs/os-specific/linux/blcr/default.nix
@@ -2,33 +2,13 @@
 
 assert stdenv.isLinux;
 
-let
-  version = "0.8.2";
-  #website = https://ftg.lbl.gov/CheckpointRestart;
-in
-
 stdenv.mkDerivation {
-  name = "blcr-${version}-${kernel.version}";
+  name = "blcr-0.8.3-${kernel.version}";
 
   src = fetchurl {
-    url = https://ftg.lbl.gov/assets/projects/CheckpointRestart/downloads/blcr-0.8.2.tar.gz;
-    #url = "${website}/downloads/blcr-${version}.tar.gz";
-    sha256 = "1ldvzrajljkm318z5ix1p27n0gmv7gqxij6ck7p5fz4ncdbb01x8";
+    url = https://ftg.lbl.gov/assets/projects/CheckpointRestart/downloads/blcr-0.8.3.tar.gz;
+    sha256 = "c8243c9a7a215d4fc4e8f2199045711cf711a6f2e0b39a94413478ffae110eac";
   };
-  
-  patchFlags = "-p0";
-
-  patches = map fetchurl [
-    { url = http://upc-bugs.lbl.gov/blcr-dist/blcr-0.8.2+kernel-2.6.31.patch01;
-      sha256 = "0jnz18kbrm64hahvhk35zakcpylad1khsp5kjxkj19j0lkjv3m4h";
-    }
-    { url = http://upc-bugs.lbl.gov/blcr-dist/blcr-0.8.2+kernel-2.6.32.patch02;
-      sha256 = "1f5s9c7iiaxd67ki3bmz09mf66shzbl97hvwaq4nmk6miq94k1fw";
-    }
-    { url = http://upc-bugs.lbl.gov/blcr-dist/blcr-0.8.2+kernel-2.6.34.patch03;
-      sha256 = "0g2v67n40ikx5hja7zgnc2axif16r2sibi5vf0r4wjzc5z9gdfrh";
-    }
-  ];
 
   buildInputs = [ perl makeWrapper autoconf automake libtool ];
 
