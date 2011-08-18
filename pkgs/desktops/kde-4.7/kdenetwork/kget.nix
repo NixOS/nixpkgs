c6458b5269814e8afe562ef6cd703a8074c0f847
Yury G. Kudryashov <urkud.urkud@gmail.com>
2011-08-18 12:52:29 +0000

    Do not pass kdebase to kget as buildInput
    svn path=/nixpkgs/trunk/; revision=28653

--- pkgs/desktops/kde-4.7/kdenetwork/kget.nix
+++ pkgs/desktops/kde-4.7/kdenetwork/kget.nix
@@ -1,9 +1,11 @@
-{ kde, kdelibs, libktorrent, kde_workspace, kdepimlibs, kdebase,
+{ kde, kdelibs, libktorrent, kde_workspace, kdepimlibs,
   shared_desktop_ontologies, kde_baseapps, gpgme, boost, libmms, qca2 }:
 
 kde {
-  buildInputs = [ kdelibs libktorrent kde_workspace kdebase
-    shared_desktop_ontologies kdepimlibs kde_baseapps gpgme boost libmms qca2 ];
+  buildInputs =
+    [ kdelibs libktorrent kde_workspace shared_desktop_ontologies kdepimlibs
+      kde_baseapps gpgme boost libmms qca2
+    ];
 
   KDEDIRS = libktorrent;
 
