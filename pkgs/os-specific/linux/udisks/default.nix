f7a51ce2ac8821a0b1a5485ea5b2168bb7fa5089
Yury G. Kudryashov <urkud.urkud@gmail.com>
2011-08-18 12:53:07 +0000

    Fix udisks udev rules
    svn path=/nixpkgs/trunk/; revision=28658

--- pkgs/os-specific/linux/udisks/default.nix
+++ pkgs/os-specific/linux/udisks/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchurl, pkgconfig, sg3_utils, udev, glib, dbus, dbus_glib
 , polkit, parted, lvm2, libatasmart, intltool, libuuid, mdadm
-, libxslt, docbook_xsl, utillinux }:
+, libxslt, docbook_xsl, utillinux, automake, autoconf }:
 
 stdenv.mkDerivation rec {
   name = "udisks-1.0.3";
@@ -10,10 +10,21 @@ stdenv.mkDerivation rec {
     sha256 = "0jwavs2ag0cv46517j17943s16a8fw2lqk4k3cljgivh5aswwnyr";
   };
 
+  # Move 80-udisks.rules manually to make the patch smaller
+  prePatch = "mv -v data/80-udisks.rules{,.in}";
+
+  # Not written a patch that can be accepted upstream yet
+  postPatch = "sed -e 's@/sbin/mdadm@${mdadm}&@' -i data/80-udisks.rules.in";
+
+  patches = [ ./purity.patch ];
+
+
   buildInputs =
-    [ pkgconfig sg3_utils udev glib dbus dbus_glib polkit parted
+    [ sg3_utils udev glib dbus dbus_glib polkit parted
       lvm2 libatasmart intltool libuuid libxslt docbook_xsl
     ];
+
+  buildNativeInputs = [ automake autoconf pkgconfig ];
 
   configureFlags = "--localstatedir=/var";
 
@@ -23,6 +34,8 @@ stdenv.mkDerivation rec {
       substituteInPlace src/main.c --replace \
         "/sbin:/bin:/usr/sbin:/usr/bin" \
         "${utillinux}/bin:${mdadm}/sbin:/var/run/current-system/sw/bin:/var/run/current-system/sw/sbin"
+
+      automake
     '';
 
   meta = {
