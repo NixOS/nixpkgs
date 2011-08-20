d99e939015fb1a46a52ecf9ca088feecf2452aa3
Cillian de Roiste <cillian.deroiste@gmail.com>
2011-08-20 23:26:59 +0000

    Upgrading Kdenlive to 0.8
    svn path=/nixpkgs/trunk/; revision=28715

--- pkgs/applications/video/kdenlive/default.nix
+++ pkgs/applications/video/kdenlive/default.nix
@@ -1,20 +1,19 @@
-{stdenv, fetchurl, lib, cmake, qt4, perl, kdelibs, automoc4, phonon, mlt, gettext,
-shared_mime_info, soprano}:
+{ stdenv, fetchurl, lib, cmake, qt4, perl, kdelibs, automoc4, phonon, mlt, gettext
+, qimageblitz, qjson, shared_mime_info, soprano }:
 
-stdenv.mkDerivation {
-  name = "kdenlive-0.7.8";
+stdenv.mkDerivation rec {
+  name = "kdenlive-${version}";
+  version = "0.8";
+
   src = fetchurl {
-    url = mirror://sourceforge/kdenlive/kdenlive-0.7.8.tar.gz;
-    sha256 = "10bwmhh3kzdbq1nzq8s5ln7ydrzg41d1rihj5kdmf5hb91az8mvx";
+    url = "mirror://sourceforge/kdenlive/${name}.tar.gz";
+    sha256 = "18e3390c9eb7124af5cd43819c679374aec46dcaf1fc5cdb43918db470c1076f";
   };
 
-  prePatch = ''
-    # For Qt47 compatibility.
-    sed -i 's@class QImage@#include <QImage>@' src/colorcorrection/vectorscopegenerator.h
-  '';
+  patches = [ ./kdenlive-newmlt.patch ];
 
   buildInputs = [ cmake qt4 perl kdelibs automoc4 phonon mlt gettext
-    shared_mime_info soprano ];
+    qimageblitz qjson shared_mime_info soprano ];
 
   meta = {
     description = "Free and open source video editor";
