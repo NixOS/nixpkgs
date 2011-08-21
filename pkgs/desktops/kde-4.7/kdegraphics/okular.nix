0b99612c180b1f92eb4dcc859571a65e11d03326
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-08-21 00:13:34 +0000

    * Layout.
    svn path=/nixpkgs/trunk/; revision=28717

--- pkgs/desktops/kde-4.7/kdegraphics/okular.nix
+++ pkgs/desktops/kde-4.7/kdegraphics/okular.nix
@@ -1,9 +1,9 @@
-{ chmlib, djvulibre, ebook_tools, kde, kdelibs, libspectre, popplerQt4, qca2,
-  qimageblitz }:
+{ chmlib, djvulibre, ebook_tools, kde, kdelibs, libspectre, popplerQt4, qca2
+, qimageblitz }:
 
 kde {
-  buildInputs = [ chmlib djvulibre ebook_tools kdelibs libspectre popplerQt4
-    qca2 qimageblitz ];
+  buildInputs =
+    [ chmlib djvulibre ebook_tools kdelibs libspectre popplerQt4 qca2 qimageblitz ];
 
   meta = {
     description = "Okular, the KDE document viewer";
