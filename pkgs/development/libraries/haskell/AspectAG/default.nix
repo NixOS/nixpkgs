56477a99f5b28b51c71693138eee289aeb3f15a2
Peter Simons <simons@cryp.to>
2011-08-21 16:09:41 +0000

    haskell-AspectAG: updated to version 0.3.4.1
    svn path=/nixpkgs/trunk/; revision=28723

--- pkgs/development/libraries/haskell/AspectAG/default.nix
+++ pkgs/development/libraries/haskell/AspectAG/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "AspectAG";
-  version = "0.3.4";
-  sha256 = "1icbmqygjp9xrsbc22g2y7pcvq193w5av7rk8pi2cbfgsimcl8gs";
+  version = "0.3.4.1";
+  sha256 = "12iaf27crynwnnd7qm1zvvaj6zw6i6c05mb4dsq55dnhph2l356g";
   buildDepends = [ HList mtl ];
   meta = {
     homepage = "http://www.cs.uu.nl/wiki/bin/view/Center/AspectAG";
