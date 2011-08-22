d81cab00035f5629247e1d16ba753607e4315786
Peter Simons <simons@cryp.to>
2011-08-22 11:54:23 +0000

    haskell-hledger-interest: updated to version 1.2
    svn path=/nixpkgs/trunk/; revision=28737

--- pkgs/applications/office/hledger-interest/default.nix
+++ pkgs/applications/office/hledger-interest/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "hledger-interest";
-  version = "1.0";
-  sha256 = "082cyyyina7w5zvzsy1b9m9a7vb12ccxd351s8ajk11pg628zb80";
+  version = "1.2";
+  sha256 = "1r5x6z0k33fpvqba6zlkhpm7b0rwwnli4q100psms89x0rrj7y2d";
   isLibrary = false;
   isExecutable = true;
   buildDepends = [ hledgerLib mtl time ];
@@ -12,6 +12,9 @@ cabal.mkDerivation (self: {
     description = "computes interest for a given account";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
-    maintainers = [ self.stdenv.lib.maintainers.simons ];
+    maintainers = [
+      self.stdenv.lib.maintainers.andres
+      self.stdenv.lib.maintainers.simons
+    ];
   };
 })
