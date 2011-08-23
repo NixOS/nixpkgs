1c79ecc5af8961212788e409dbe14eea2b06d260
Peter Simons <simons@cryp.to>
2011-08-23 08:44:59 +0000

    git2cl: added meta data for the package and updated the build to take advantage of stdenv features such as automatic updating of the shebang path to perl, etc.
    svn path=/nixpkgs/trunk/; revision=28768

--- pkgs/applications/version-management/git-and-tools/git2cl/default.nix
+++ pkgs/applications/version-management/git-and-tools/git2cl/default.nix
@@ -1,19 +1,22 @@
-{ fetchgit
-, stdenv
-, perl
-}:
-stdenv.mkDerivation rec {
+{ fetchgit, stdenv, perl }:
+
+stdenv.mkDerivation {
   name = "git2cl";
 
   src = fetchgit {
-    url = git://git.sv.gnu.org/git2cl.git;
+    url = "git://repo.or.cz/git2cl.git";
     rev = "8373c9f74993e218a08819cbcdbab3f3564bbeba";
     sha256 = "b0d39379640c8a12821442431e2121f7908ce1cc88ec8ec6bede218ea8c21f2f";
   };
 
-  buildCommand = ''
-    ensureDir $out/bin
-    cp ${src}/git2cl $out/bin
-    sed -i 's|/usr/bin/perl|${perl}/bin/perl|' $out/bin/git2cl
+  buildInputs = [ perl ];
+  installPhase = ''
+    install -D -m755 git2cl $out/bin/git2cl
+    install -D -m644 README $out/share/doc/git2cl/README
   '';
+
+  meta = {
+    homepage = "http://josefsson.org/git2cl/";
+    description = "convert git logs to GNU style ChangeLog files";
+  };
 }
