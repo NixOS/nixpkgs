1e83b9c896bba6610f27f2444698cf7a29774cf3
Peter Simons <simons@cryp.to>
2011-08-23 08:45:08 +0000

    haskell-enumerator: updated to version 0.4.14
    svn path=/nixpkgs/trunk/; revision=28770

--- pkgs/development/libraries/haskell/enumerator/default.nix
+++ pkgs/development/libraries/haskell/enumerator/default.nix
@@ -2,11 +2,11 @@
 
 cabal.mkDerivation (self: {
   pname = "enumerator";
-  version = "0.4.13.1";
-  sha256 = "5fcafe316444adfb66c213ffb71359560f48eafe03bb2df99bfba17d2e3153c8";
+  version = "0.4.14";
+  sha256 = "06bvrr76y12r3i1xi1s98fc1v6mcmn8nzhbp80wgyi743ksqcr1d";
   buildDepends = [ text transformers ];
   meta = {
-    homepage = "http://john-millikin.com/software/enumerator/";
+    homepage = "https://john-millikin.com/software/enumerator/";
     description = "Reliable, high-performance processing with left-fold enumerators";
     license = self.stdenv.lib.licenses.mit;
     platforms = self.ghc.meta.platforms;
