25594961ad13ee0ad85a3aafc95ce9b1ed38e7d6
Shea Levy <shea@shealevy.com>
2011-08-24 23:37:40 +0000

    modprobe.nix: Blacklist snd_pcsp by default
    svn path=/nixos/trunk/; revision=28805

--- modules/system/boot/modprobe.nix
+++ modules/system/boot/modprobe.nix
@@ -83,6 +83,11 @@ with pkgs.lib;
         # of log output, so it should never be loaded automatically.
         "evbug"
 
+        # This module causes ALSA to occassionally select the wrong
+        # default sound device, and is little more than an annoyance
+        # on modern machines.
+        "snd_pcsp"
+
         # !!! Hm, Ubuntu blacklists all framebuffer devices because
         # they're "buggy" and cause suspend problems.  Maybe we should
         # too?
