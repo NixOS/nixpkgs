5412c585bc7a2abf92f58b5c16906717540d60e2
Peter Simons <simons@cryp.to>
2011-08-26 10:29:17 +0000

    haskell-cabal2nix: updated to version 1.13
    svn path=/nixpkgs/trunk/; revision=28828

--- pkgs/development/tools/haskell/cabal2nix/default.nix
+++ pkgs/development/tools/haskell/cabal2nix/default.nix
@@ -1,12 +1,12 @@
-{ cabal, HTTP, mtl, nixosTypes, regexPosix }:
+{ cabal, hackageDb, HTTP, mtl, nixosTypes, regexPosix }:
 
 cabal.mkDerivation (self: {
   pname = "cabal2nix";
-  version = "1.12";
-  sha256 = "14bijci07hkm3ksbqpzbnmwiysy5s3ll89r2iqkr1rbmj7bqxdwy";
+  version = "1.13";
+  sha256 = "1wjxaz3yyc41y4plwixd6p2g7higvjzpi8znbgvnzcpfj3hlh616";
   isLibrary = false;
   isExecutable = true;
-  buildDepends = [ HTTP mtl nixosTypes regexPosix ];
+  buildDepends = [ hackageDb HTTP mtl nixosTypes regexPosix ];
   meta = {
     homepage = "http://github.com/haskell4nix/cabal2nix";
     description = "Convert Cabal files into Nix build instructions";
