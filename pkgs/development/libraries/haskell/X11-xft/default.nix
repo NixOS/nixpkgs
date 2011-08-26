ea5a993ac4ed79aa0afa03a51be925d21504052c
Peter Simons <simons@cryp.to>
2011-08-26 10:29:06 +0000

    haskell-X11-xft: cabal2nix can re-generate this expression now
    svn path=/nixpkgs/trunk/; revision=28826

--- pkgs/development/libraries/haskell/X11-xft/default.nix
+++ pkgs/development/libraries/haskell/X11-xft/default.nix
@@ -1,11 +1,14 @@
-{ ghc, cabal, X11, utf8String, pkgconfig, libXft, freetype, fontconfig }:
-cabal.mkDerivation (self : {
+{ cabal, fontconfig, freetype, libXft, pkgconfig, utf8String, X11
+}:
+
+cabal.mkDerivation (self: {
   pname = "X11-xft";
   version = "0.3";
   sha256 = "48892d0d0a90d5b47658877facabf277bf8466b7388eaf6ce163b843432a567d";
-  buildInputs = [ ghc pkgconfig libXft freetype fontconfig ];
-  propagatedBuildInputs = [ X11 utf8String ];
-  configureFlags=["--extra-include-dirs=${freetype}/include/freetype2"];
+  buildDepends = [ utf8String X11 ];
+  extraLibraries = [ fontconfig freetype pkgconfig ];
+  pkgconfigDepends = [ libXft ];
+  configureFlags = "--extra-include-dirs=${freetype}/include/freetype2";
   meta = {
     description = "Bindings to the Xft, X Free Type interface library, and some Xrender parts";
     license = "LGPL";
