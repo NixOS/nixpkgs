dc7e7b7dcfe065a5de135c2d75b58e72b59814db
Peter Simons <simons@cryp.to>
2011-08-26 09:20:42 +0000

    haskell-extensible-exceptions: added version 0.1.1.3
    svn path=/nixpkgs/trunk/; revision=28817

---
+++ pkgs/development/libraries/haskell/extensible-exceptions/0.1.1.3.nix
@@ -0,0 +1,16 @@
+{ cabal }:
+
+cabal.mkDerivation (self: {
+  pname = "extensible-exceptions";
+  version = "0.1.1.3";
+  sha256 = "1i8rjfczsx1wjfaq423a7cp7qrnxh053865z7bg6hwhk2pxsrxkm";
+  meta = {
+    description = "Extensible exceptions";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.andres
+      self.stdenv.lib.maintainers.simons
+    ];
+  };
+})
