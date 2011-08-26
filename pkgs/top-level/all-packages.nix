5d6cc2ef8442d91f8d373e322601abda94c076bb
Yury G. Kudryashov <urkud.urkud@gmail.com>
2011-08-26 16:02:43 +0000

    Add note about drop-kde4.5 branch
    svn path=/nixpkgs/trunk/; revision=28841

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7794,6 +7794,8 @@ let
 
   kde4 = recurseIntoAttrs pkgs.kde45;
 
+  # TODO: merge with branches/drop-kde4.5 if you want to remove KDE SC 4.5
+  # This branch removes kde45 and quite a few compatibility hacks
   kde45 = kdePackagesFor pkgs.kde45 "4.5";
   kde47 = kdePackagesFor pkgs.kde47 "4.7";
 
