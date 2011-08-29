ba9cc549fa52ea698d5c65351197926aafa2bd5e
Peter Simons <simons@cryp.to>
2011-08-29 20:27:20 +0000

    haskell-data-default: updated to version 0.3.0
    svn path=/nixpkgs/trunk/; revision=28891

--- pkgs/development/libraries/haskell/data-default/default.nix
+++ pkgs/development/libraries/haskell/data-default/default.nix
@@ -1,9 +1,10 @@
-{ cabal }:
+{ cabal, dlist }:
 
 cabal.mkDerivation (self: {
   pname = "data-default";
-  version = "0.2.0.1";
-  sha256 = "0hhrzaykwybqpig0kss4iq1i93ygb80g8i1chpr84akmvdr07w0i";
+  version = "0.3.0";
+  sha256 = "0nbvknfa9kkc46fl1ffji0ghqb41wwsccmc55mya5zavfq9g05g9";
+  buildDepends = [ dlist ];
   meta = {
     description = "A class for types with a default value";
     license = self.stdenv.lib.licenses.bsd3;
