5b948f85e5c3dba869e2890936ca8fcb9ba43e98
Peter Simons <simons@cryp.to>
2011-08-29 20:28:14 +0000

    haskell-yesod-form: updated to version 0.3.1
    svn path=/nixpkgs/trunk/; revision=28898

--- pkgs/development/libraries/haskell/yesod-form/default.nix
+++ pkgs/development/libraries/haskell/yesod-form/default.nix
@@ -1,16 +1,16 @@
 { cabal, blazeBuilder, blazeHtml, dataDefault, emailValidate
-, hamlet, network, persistent, text, time, transformers
-, webRoutesQuasi, xssSanitize, yesodCore, yesodPersistent
+, hamlet, network, persistent, shakespeareCss, shakespeareJs, text
+, time, transformers, wai, xssSanitize, yesodCore, yesodPersistent
 }:
 
 cabal.mkDerivation (self: {
   pname = "yesod-form";
-  version = "0.1.0.1";
-  sha256 = "108652256g82xapsn0w4cbficq4ikwlsa8yis2zq7izxk48livlm";
+  version = "0.3.1";
+  sha256 = "0xa9950michbwlp90xcr8ybpig7f026zwq28lg03zavnaqisqkfj";
   buildDepends = [
     blazeBuilder blazeHtml dataDefault emailValidate hamlet network
-    persistent text time transformers webRoutesQuasi xssSanitize
-    yesodCore yesodPersistent
+    persistent shakespeareCss shakespeareJs text time transformers wai
+    xssSanitize yesodCore yesodPersistent
   ];
   meta = {
     homepage = "http://www.yesodweb.com/";
