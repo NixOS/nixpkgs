5836af8dc2b4f52d9922b7e03d673fb0dec166b1
Peter Simons <simons@cryp.to>
2011-08-29 20:28:02 +0000

    haskell-wai-extra: updated to version 0.4.2
    svn path=/nixpkgs/trunk/; revision=28895

--- pkgs/development/libraries/haskell/wai-extra/default.nix
+++ pkgs/development/libraries/haskell/wai-extra/default.nix
@@ -5,14 +5,14 @@
 
 cabal.mkDerivation (self: {
   pname = "wai-extra";
-  version = "0.4.0.3";
-  sha256 = "1mfjn9rxzcfdwvimjw57j7vpr1y64ia7905c8nxa9968sdy0dhsy";
+  version = "0.4.2";
+  sha256 = "1k2dsjramy14rfd1j8k7zgirdfl2zybd0z0pxkvxdrgr9s2pk51y";
   buildDepends = [
     blazeBuilder blazeBuilderEnumerator caseInsensitive enumerator
     httpTypes network text time transformers wai zlibBindings
   ];
   meta = {
-    homepage = "http://github.com/snoyberg/wai-extra";
+    homepage = "http://github.com/yesodweb/wai";
     description = "Provides some basic WAI handlers and middleware";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
