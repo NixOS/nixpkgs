d84962624e9415a2f7698e054a35bb4d2e3ab556
Peter Simons <simons@cryp.to>
2011-08-29 21:46:22 +0000

    Dropped obsolete haskell-emgm.
    The package doesn't build with any of our compilers.
    
    svn path=/nixpkgs/trunk/; revision=28906

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -543,8 +543,6 @@ let result = let callPackage = x : y : modifyPrio (newScope result.final x y);
 
   filepath = callPackage ../development/libraries/haskell/filepath {};
 
-  emgm = callPackage ../development/libraries/haskell/emgm {};
-
   extensibleExceptions_0_1_1_0 = callPackage ../development/libraries/haskell/extensible-exceptions/0.1.1.0.nix {};
   extensibleExceptions_0_1_1_2 = callPackage ../development/libraries/haskell/extensible-exceptions/0.1.1.2.nix {};
   extensibleExceptions_0_1_1_3 = callPackage ../development/libraries/haskell/extensible-exceptions/0.1.1.3.nix {};
