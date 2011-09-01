006c7c854a95db3497b61c3f80ba58ed29ed5d85
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2011-09-01 17:38:55 +0000

    Adding a patch for libxcrypt that could make it build for gcc 4.6. Untested; I only looked at the hydra log.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=28961

--- pkgs/development/libraries/libxcrypt/default.nix
+++ pkgs/development/libraries/libxcrypt/default.nix
@@ -11,4 +11,7 @@ stdenv.mkDerivation {
     url = ftp://ftp.suse.com/pub/people/kukuk/libxcrypt/libxcrypt-3.0.2.tar.bz2;
     sha256 = "15l2xvhi3r3b40x4665c101ikylh5xsbpw03gnszypfjgn1jkcii";
   };
+
+  patches = [ ./gcc46.patch ];
+
 }
