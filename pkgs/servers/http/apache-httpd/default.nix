573e554e2c4ba7e8984aee568e8475c5e7e87811
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-09-02 15:59:12 +0000

    * Apache httpd updated to 2.2.20, with a fix for CVE-2011-3192.
    svn path=/nixpkgs/trunk/; revision=28976

--- pkgs/servers/http/apache-httpd/default.nix
+++ pkgs/servers/http/apache-httpd/default.nix
@@ -8,12 +8,12 @@ assert sslSupport -> openssl != null;
 assert ldapSupport -> aprutil.ldapSupport && openldap != null;
 
 stdenv.mkDerivation rec {
-  version = "2.2.19";
+  version = "2.2.20";
   name = "apache-httpd-${version}";
 
   src = fetchurl {
     url = "mirror://apache/httpd/httpd-${version}.tar.bz2";
-    sha1 = "5676da63f3203129287d7c09a16cf523c00ec6cf";
+    sha1 = "c8f00a505af6ed3f89f45b640217c388f5cd32b0";
   };
 
   buildInputs = [perl apr aprutil pcre] ++
