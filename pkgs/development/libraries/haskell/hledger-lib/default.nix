232483a379315bd38f8cc7b564d3193465b325e9
Peter Simons <simons@cryp.to>
2011-09-02 18:02:16 +0000

    haskell-hledger-lib: updated to version 0.15
    svn path=/nixpkgs/trunk/; revision=28978

--- pkgs/development/libraries/haskell/hledger-lib/default.nix
+++ pkgs/development/libraries/haskell/hledger-lib/default.nix
@@ -1,16 +1,17 @@
-{ cabal, HUnit, mtl, parsec, regexpr, safe, split, time, utf8String
+{ cabal, cmdargs, HUnit, mtl, parsec, regexpr, safe, split, time
+, utf8String
 }:
 
 cabal.mkDerivation (self: {
   pname = "hledger-lib";
-  version = "0.14";
-  sha256 = "9a6d6ab4383800279e135e9bbcd886e95cea45232d093202c5d43e6edd1f927c";
+  version = "0.15";
+  sha256 = "1bsn02pgz38ivk6z24niwab2ibmysngc1hghw5d5n2xa862ffrgb";
   buildDepends = [
-    HUnit mtl parsec regexpr safe split time utf8String
+    cmdargs HUnit mtl parsec regexpr safe split time utf8String
   ];
   meta = {
     homepage = "http://hledger.org";
-    description = "Reusable types and utilities for the hledger accounting tool and financial apps in general";
+    description = "Core data types, parsers and utilities for the hledger accounting tool";
     license = "GPL";
     platforms = self.ghc.meta.platforms;
     maintainers = [
