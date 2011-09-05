1e6eeb9ad7750336dfcd1bdc47ef02c5ebd148c5
Andres Löh <mail@andres-loeh.de>
2011-09-05 09:52:34 +0000

    Updated ghc-events and threadscope.
    svn path=/nixpkgs/trunk/; revision=29023

--- pkgs/development/tools/haskell/threadscope/default.nix
+++ pkgs/development/tools/haskell/threadscope/default.nix
@@ -1,15 +1,15 @@
-{ cabal, binary, cairo, ghcEvents, glade, gtk, mtl }:
+{ cabal, binary, cairo, ghcEvents, glib, gtk, mtl, pango }:
 
 cabal.mkDerivation (self: {
   pname = "threadscope";
-  version = "0.1.3";
-  sha256 = "1vak3624vrnkfvwxzfw5hkc0552v213jb874f6q536g5vhjjxpih";
+  version = "0.2.0";
+  sha256 = "0b8lc8han4d90wgzliy80l1gbkm09gg6qxsn37blj41wzl6yzr9k";
   isLibrary = false;
   isExecutable = true;
-  buildDepends = [ binary cairo ghcEvents glade gtk mtl ];
+  buildDepends = [ binary cairo ghcEvents glib gtk mtl pango ];
   configureFlags = "--ghc-options=-rtsopts";
   meta = {
-    description = "A graphical thread profiler";
+    description = "A graphical tool for profiling parallel Haskell programs";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [
