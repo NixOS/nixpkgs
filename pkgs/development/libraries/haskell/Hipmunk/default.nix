051326b8c762e3757e72a607db09ca5de4fc47d4
Peter Simons <simons@cryp.to>
2011-09-06 17:17:38 +0000

    haskell-Hipmunk: updated to version 5.2.0.3
    svn path=/nixpkgs/trunk/; revision=29065

--- pkgs/development/libraries/haskell/Hipmunk/default.nix
+++ pkgs/development/libraries/haskell/Hipmunk/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "Hipmunk";
-  version = "5.2.0.2";
-  sha256 = "18ly05q953x59smqqrhwjlfa1v6lqma0xiarmr689i63zzb7ji8z";
+  version = "5.2.0.3";
+  sha256 = "1as00372hz0ngy6pc5l0w7qgw5flcyag07zk3dxkbj5kv24xk49m";
   buildDepends = [ StateVar transformers ];
   meta = {
     homepage = "http://patch-tag.com/r/felipe/hipmunk/home";
