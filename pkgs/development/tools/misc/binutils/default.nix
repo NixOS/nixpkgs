dcd147a1411753d5acb9a22027002bdea9932dcb
Shea Levy <shea@shealevy.com>
2011-09-06 20:47:27 +0000

    Add note in binutils/default.nix to warn about the problematic source tarball
    svn path=/nixpkgs/trunk/; revision=29067

--- pkgs/development/tools/misc/binutils/default.nix
+++ pkgs/development/tools/misc/binutils/default.nix
@@ -6,6 +6,22 @@ in
 stdenv.mkDerivation rec {
   name = basename + stdenv.lib.optionalString (cross != null) "-${cross.config}";
 
+  # WARNING: Upstream made a mistake in packaging that may mean anyone
+  # but the FSF hosting this tarball is accidentally in violation of
+  # the GPL. We can't update binutils until the next stdenv-updates,
+  # so we are stuck with this version. The issue is discussed in
+  # this email: http://sourceware.org/ml/binutils/2011-08/msg00198.html
+  # The tarball for this minor version will not be fixed, as only the
+  # tarballs for the latest minor version of each major version will
+  # be repackaged. The fixed sources for the closest version to this one
+  # can be found at mirror://gnu/binutils/binutils-2.21.1a.tar.bz2
+  # or http://ftp.gnu.org/gnu/binutils/binutils-2.21.1a.tar.bz2
+  # The sources missing from this tarball come from cgen. It is unclear
+  # WHICH sources should be included, but the cvs tree can be checked out
+  # by:
+  # cvs -z 9 -d :pserver:anoncvs@sourceware.org:/cvs/src login
+  # {enter "anoncvs" as the password}
+  # cvs -z 9 -d :pserver:anoncvs@sourceware.org:/cvs/src co cgen
   src = fetchurl {
     url = "http://nixos.org/tarballs/${basename}.tar.bz2";
     sha256 = "1iyhc42zfa0j2gaxy4zvpk47sdqj4rqvib0mb8597ss8yidyrav0";
