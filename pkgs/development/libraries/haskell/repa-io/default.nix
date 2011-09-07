05db155ad3041531583f070546850b84b17f09f2
Peter Simons <simons@cryp.to>
2011-09-07 17:29:54 +0000

    haskell-repa-io: updated to version 2.2.0.1
    svn path=/nixpkgs/trunk/; revision=29093

--- pkgs/development/libraries/haskell/repa-io/default.nix
+++ pkgs/development/libraries/haskell/repa-io/default.nix
@@ -1,10 +1,10 @@
-{ cabal, bmp, repa, repaBytestring }:
+{ cabal, binary, bmp, repa, repaBytestring, vector }:
 
 cabal.mkDerivation (self: {
   pname = "repa-io";
-  version = "2.1.0.1";
-  sha256 = "1mjv90rr1vymrnv5kz8i4kvjal6mwhb2042ylbdggvv8hjsc8awq";
-  buildDepends = [ bmp repa repaBytestring ];
+  version = "2.2.0.1";
+  sha256 = "1akp6xkfvbc7jmnzlrz4y5fncqrv9d06y981dndcv6lgxz4hv4j1";
+  buildDepends = [ binary bmp repa repaBytestring vector ];
   meta = {
     homepage = "http://repa.ouroborus.net";
     description = "Read and write Repa arrays in various formats";
