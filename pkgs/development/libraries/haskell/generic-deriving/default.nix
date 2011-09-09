6a754d3119d9b2a2fb25184edb60b803dc3f5ffc
Andres Löh <mail@andres-loeh.de>
2011-09-09 19:35:31 +0000

    Minor fix.
    svn path=/nixpkgs/trunk/; revision=29138

--- pkgs/development/libraries/haskell/generic-deriving/default.nix
+++ pkgs/development/libraries/haskell/generic-deriving/default.nix
@@ -5,7 +5,7 @@ cabal.mkDerivation (self: {
   version = "1.0.2";
   sha256 = "0qj4ql44f87rgg5l512mh863c1zjfklw1w5k8x4nyw37s7qx9hvq";
   meta = {
-    description = "Generic programming library for generalized deriving.";
+    description = "Generic programming library for generalized deriving";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [
