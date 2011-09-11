8993dabdf40d2ab814e51ca6a35837c3975967c5
Peter Simons <simons@cryp.to>
2011-09-11 10:34:26 +0000

    haskell-yesod: updated to version 0.9.2
    svn path=/nixpkgs/trunk/; revision=29178

--- pkgs/development/libraries/haskell/yesod/default.nix
+++ pkgs/development/libraries/haskell/yesod/default.nix
@@ -1,20 +1,21 @@
 { cabal, attoparsecText, blazeBuilder, blazeHtml, hamlet, httpTypes
-, monadControl, parsec, shakespeareCss, shakespeareJs, text, time
-, transformers, unixCompat, wai, waiExtra, warp, yesodAuth
-, yesodCore, yesodForm, yesodJson, yesodPersistent
+, monadControl, parsec, shakespeareCss, shakespeareJs
+, shakespeareText, text, time, transformers, unixCompat, wai
+, waiExtra, warp, yesodAuth, yesodCore, yesodForm, yesodJson
+, yesodPersistent
 }:
 
 cabal.mkDerivation (self: {
   pname = "yesod";
-  version = "0.9.1.1";
-  sha256 = "1f1l9davhqdf6fnkcrclf2dvszqsw2jby7y865y4sgcb73jb32fp";
+  version = "0.9.2";
+  sha256 = "1074vbrab8c0nrmkv1fn4rm65fyfw08b3mj498cxala3nm382whd";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
     attoparsecText blazeBuilder blazeHtml hamlet httpTypes monadControl
-    parsec shakespeareCss shakespeareJs text time transformers
-    unixCompat wai waiExtra warp yesodAuth yesodCore yesodForm
-    yesodJson yesodPersistent
+    parsec shakespeareCss shakespeareJs shakespeareText text time
+    transformers unixCompat wai waiExtra warp yesodAuth yesodCore
+    yesodForm yesodJson yesodPersistent
   ];
   meta = {
     homepage = "http://www.yesodweb.com/";
