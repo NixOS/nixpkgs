6a73467eac6fe9d37c024b959a9a8a5fde150bcf
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-09-11 20:48:02 +0000

    * Chromium updated to the latest version.  Because Chromium now used   Gold, this requires a new patchelf. * Changed the package name from ‘chrome’ to ‘chromium’.
    svn path=/nixpkgs/trunk/; revision=29197

--- pkgs/applications/networking/browsers/chromium/default.nix
+++ pkgs/applications/networking/browsers/chromium/default.nix
@@ -2,23 +2,28 @@
 , fetchurl, ffmpeg, fontconfig, freetype, gtkLibs, libX11
 , libXScrnSaver, libXdamage, libXext, libXrender, libXt, libXtst
 , libgcrypt, libjpeg, libpng, makeWrapper, nspr, nss, patchelf
-, stdenv, unzip, zlib, pam }:
+, stdenv, unzip, zlib, pam, pcre }:
 
 assert stdenv.system == "i686-linux" || stdenv.system == "x86_64-linux" ;
 
 stdenv.mkDerivation rec {
-  name = "chrome-${version}";
-  version = "88807";
+  name = "chromium-16.0.879.0-pre${version}";
+
+  # To determine the latest revision, get
+  # ‘http://commondatastorage.googleapis.com/chromium-browser-continuous/Linux/LAST_CHANGE’.
+  # For the version number, see ‘about:config’.
+  version = "100626";
+  
   src =
     if stdenv.system == "x86_64-linux" then
       fetchurl {
         url = "http://commondatastorage.googleapis.com/chromium-browser-continuous/Linux_x64/${version}/chrome-linux.zip";
-        sha256 = "c158f58fa8220782ec8dec4170f90c564b978d1c6ead298cc2f67e84613f17b1";
+        sha256 = "1dymz7h9v5hkivn6qn26bnj1waw60z3mngh8g46yvvc5xn4npc3l";
       }
     else if stdenv.system == "i686-linux" then
       fetchurl {
         url = "http://commondatastorage.googleapis.com/chromium-browser-continuous/Linux/${version}/chrome-linux.zip";
-        sha256 = "01sr882c7hr53001p8bnk5vyj8zfjm6r3i4a6wxzxd17xjh1bcxb";
+        sha256 = "0zqaj90lfzdddbs6sjygmyxlh8nw3xfr9xw450g9cabg6a2sh7ca";
       }
     else throw "Chromium is not supported on this platform.";
 
@@ -31,7 +36,7 @@ stdenv.mkDerivation rec {
        [ GConf alsaLib bzip2 cairo cups dbus dbus_glib expat
          ffmpeg fontconfig freetype libX11 libXScrnSaver
          libXdamage libXext libXrender libXt libXtst libgcrypt libjpeg
-         libpng nspr nss stdenv.gcc.gcc zlib stdenv.gcc.libc
+         libpng nspr stdenv.gcc.gcc zlib stdenv.gcc.libc
          gtkLibs.glib gtkLibs.gtk gtkLibs.gdk_pixbuf gtkLibs.pango
          pam
        ];
@@ -41,6 +46,8 @@ stdenv.mkDerivation rec {
     ensureDir $out/libexec/chrome
 
     cp -R * $out/libexec/chrome
+
+    strip $out/libexec/chrome/chrome
     
     ${patchelf}/bin/patchelf \
       --interpreter "$(cat $NIX_GCC/nix-support/dynamic-linker)" \
@@ -48,7 +55,7 @@ stdenv.mkDerivation rec {
       $out/libexec/chrome/chrome
 
     makeWrapper $out/libexec/chrome/chrome $out/bin/chrome \
-      --prefix LD_LIBRARY_PATH : "${nss}/lib"
+      --prefix LD_LIBRARY_PATH : "${pcre}/lib:${nss}/lib"
   '';
 
   meta =  with stdenv.lib; {
