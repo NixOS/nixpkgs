6557b25b370d873d9e4e610c4c38429539c73957
Peter Simons <simons@cryp.to>
2011-09-13 17:35:12 +0000

    haskell-io-storage: added version 0.3
    svn path=/nixpkgs/trunk/; revision=29242

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -773,6 +773,8 @@ let result = let callPackage = x : y : modifyPrio (newScope result.final x y);
 
   instantGenerics = callPackage ../development/libraries/haskell/instant-generics {};
 
+  ioStorage = callPackage ../development/libraries/haskell/io-storage {};
+
   irc = callPackage ../development/libraries/haskell/irc {
     parsec = self.parsec2;
   };
