2f736e0ca01b0a0c0543c0bf080e81503f30560d
Peter Simons <simons@cryp.to>
2011-09-16 18:19:15 +0000

    haskell-Hipmunk: avoid build error in the haddock documentation
    svn path=/nixpkgs/trunk/; revision=29309

--- pkgs/development/libraries/haskell/Hipmunk/default.nix
+++ pkgs/development/libraries/haskell/Hipmunk/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "5.2.0.3";
   sha256 = "1as00372hz0ngy6pc5l0w7qgw5flcyag07zk3dxkbj5kv24xk49m";
   buildDepends = [ StateVar transformers ];
+  noHaddock = true;
   meta = {
     homepage = "http://patch-tag.com/r/felipe/hipmunk/home";
     description = "A Haskell binding for Chipmunk";
