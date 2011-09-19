b0f43454674e8646db458cb72e343d579a7aef4d
Peter Simons <simons@cryp.to>
2011-09-19 18:25:55 +0000

    pkgs/os-specific/linux/cpufrequtils: download tarball from any of the "kernel" mirrors
    svn path=/nixpkgs/trunk/; revision=29361

--- pkgs/os-specific/linux/cpufrequtils/default.nix
+++ pkgs/os-specific/linux/cpufrequtils/default.nix
@@ -4,9 +4,9 @@ assert stdenv.isLinux && stdenv.system != "powerpc-linux";
 
 stdenv.mkDerivation {
   name = "cpufrequtils-008";
-  
+
   src = fetchurl {
-    url = http://kernel.org/pub/linux/utils/kernel/cpufreq/cpufrequtils-008.tar.gz;
+    url = "mirror://kernel/linux/utils/kernel/cpufreq/cpufrequtils-008.tar.gz";
     md5 = "52d3e09e47ffef634833f7fab168eccf";
   };
 
