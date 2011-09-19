ef664e90a081d0ac66fe0f43007de53a60e3f812
Yury G. Kudryashov <urkud.urkud@gmail.com>
2011-09-19 05:16:11 +0000

    Re-enable ntrack support in KDE-4.7
    It seems that the patches applied to ntrack in r29105 fixed the problem.
    
    svn path=/nixpkgs/trunk/; revision=29338

--- pkgs/desktops/kde-4.7/kde-runtime.nix
+++ pkgs/desktops/kde-4.7/kde-runtime.nix
@@ -1,13 +1,11 @@
 { kde, kdelibs, shared_desktop_ontologies, bzip2, xz, libssh, exiv2, attica
-, libcanberra, virtuoso, samba
+, libcanberra, virtuoso, samba, ntrack
 }:
-
-# TODO: Re-enable ntrack once it is fixed upstream
 
 kde {
   buildInputs =
     [ kdelibs shared_desktop_ontologies bzip2 xz libssh exiv2 attica
-      samba (libcanberra.override { gtk = null; })
+      samba (libcanberra.override { gtk = null; }) ntrack
     ];
 
   passthru.propagatedUserEnvPackages = [ virtuoso ];
