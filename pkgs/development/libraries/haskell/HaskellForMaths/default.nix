1d3a00e51a1495a885c4c7e48925c484f16f2955
Peter Simons <simons@cryp.to>
2011-09-19 16:38:22 +0000

    haskell-HaskellForMaths: updated to version 0.4.0
    svn path=/nixpkgs/trunk/; revision=29351

--- pkgs/development/libraries/haskell/HaskellForMaths/default.nix
+++ pkgs/development/libraries/haskell/HaskellForMaths/default.nix
@@ -1,10 +1,10 @@
-{ cabal, QuickCheck, random }:
+{ cabal, random }:
 
 cabal.mkDerivation (self: {
   pname = "HaskellForMaths";
-  version = "0.3.4";
-  sha256 = "1cmhzcybv3kwvs058sjihdkqr72rygv2nmbcy8i485pk35yq31sm";
-  buildDepends = [ QuickCheck random ];
+  version = "0.4.0";
+  sha256 = "1x6kac1im07cpb1014ci8v222q66w2g3gr1wdcv2yvdi5g24ymbp";
+  buildDepends = [ random ];
   meta = {
     homepage = "http://haskellformaths.blogspot.com/";
     description = "Combinatorics, group theory, commutative algebra, non-commutative algebra";
