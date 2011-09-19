6e0c05923545c0239e377ed272b2ef90e08661a2
Rob Vermaas <rob.vermaas@gmail.com>
2011-09-19 21:37:05 +0000

    use older libpng for tetex
    svn path=/nixpkgs/trunk/; revision=29368

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8349,7 +8349,9 @@ let
 
   splix = callPackage ../misc/cups/drivers/splix { };
 
-  tetex = callPackage ../misc/tex/tetex { };
+  tetex = callPackage ../misc/tex/tetex { 
+    libpng = libpng12;
+  };
 
   tex4ht = callPackage ../misc/tex/tex4ht { };
 
