12a38dd5c674e0347bb49b8c844c03d0fbc4bbb3
Florian Friesdorf <flo@chaoflow.net>
2011-09-21 20:04:43 +0000

    notmuch-0.8 all tests pass
    svn path=/nixpkgs/trunk/; revision=29427

--- pkgs/applications/networking/mailreaders/notmuch/default.nix
+++ pkgs/applications/networking/mailreaders/notmuch/default.nix
@@ -1,70 +1,75 @@
-{ fetchgit, stdenv, bash, emacs, gdb, git, glib, gmime, gnupg1, pkgconfig, talloc, xapian }:
+{ fetchurl, stdenv, bash, emacs, gdb, git, glib, gmime, gnupg1, pkgconfig, talloc, xapian }:
 
 stdenv.mkDerivation rec {
-  name = "notmuch-0.6-rc4";
+  name = "notmuch-0.8";
 
-  src = fetchgit {
-    url = "git://github.com/chaoflow/notmuch";
-    rev = "9f8ef78e0c6b28918f3edda06b59a9e8f9bef8e3";
-    sha256 = "c7c425c10a695ca22dfbdf5fe8e0dcb6a888edc0b3388023e7ff35f69acc0085";
+  src = fetchurl {
+    url = "http://notmuchmail.org/releases/${name}.tar.gz";
+    sha256 = "f40bcdc6447cae9f76d5b4e70ab70d87e4a813cd123b524c1dc3155a3371a949";
   };
 
   buildInputs = [ bash emacs gdb git glib gmime gnupg1 pkgconfig talloc xapian ];
 
   # XXX: Make me a loop
   patchPhase = ''
-    substituteInPlace "test/atomicity" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+    # substituteInPlace "test/atomicity" \
+    #   --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
+    substituteInPlace "test/aggregate-results.sh" \
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/author-order" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/basic" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/crypto" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/dump-restore" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/emacs" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/emacs-large-search-buffer" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/encoding" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/from-guessing" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/json" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/long-id" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/maildir-sync" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/new" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/notmuch-test" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/raw" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/reply" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/search" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/search-by-folder" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/search-insufficient-from-quoting" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
+    substituteInPlace "test/search-folder-coherence" \
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/search-output" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/search-position-overlap-bug" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
+    substituteInPlace "test/symbol-hiding" \
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/test-lib.sh" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/test-verbose" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/thread-naming" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/thread-order" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
     substituteInPlace "test/uuencode" \
-      --replace "#!/bin/bash" "#!${bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${bash}/bin/bash"
   '';
 
   postBuild = ''
