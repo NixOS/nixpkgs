62a77eb641f79da2f77b7d594340c3cb8d1883e5
Andres Löh <mail@andres-loeh.de>
2011-09-24 02:48:40 +0000

    Added maintainers for haskell-numInstances.
    svn path=/nixpkgs/trunk/; revision=29464

--- pkgs/development/libraries/haskell/NumInstances/default.nix
+++ pkgs/development/libraries/haskell/NumInstances/default.nix
@@ -8,5 +8,9 @@ cabal.mkDerivation (self: {
     description = "Instances of numeric classes for functions and tuples";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.andres
+      self.stdenv.lib.maintainers.simons
+    ];
   };
 })
