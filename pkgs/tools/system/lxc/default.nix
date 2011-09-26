a004ca1a7d7f19a7732fc12bbb0b100fb794ad1d
Peter Simons <simons@cryp.to>
2011-09-26 08:12:10 +0000

    lxc: fixed build to include the SGML documentation
    svn path=/nixpkgs/trunk/; revision=29490

--- pkgs/tools/system/lxc/default.nix
+++ pkgs/tools/system/lxc/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, libcap, perl }:
+{ stdenv, fetchurl, libcap, perl, docbook2x, docbook_xml_dtd_45 }:
 
 let
   name = "lxc-0.7.5";
@@ -11,9 +11,15 @@ stdenv.mkDerivation{
     sha256 = "019ec63f250c874bf7625b1f1bf555b1a6e3a947937a4fca73100abddf829b1c";
   };
 
-  buildInputs = [ libcap perl ];
+  buildInputs = [ libcap perl docbook2x ];
 
-  patchPhase = "sed -i -e 's|/sbin/ldconfig|:|' src/lxc/Makefile.in";
+  patches = [
+   ./dont-run-ldconfig.patch
+   ./fix-documentation-build.patch
+   ./fix-sgml-documentation.patch
+  ];
+
+  preConfigure = "export XML_CATALOG_FILES=${docbook_xml_dtd_45}/xml/dtd/docbook/catalog.xml";
 
   configureFlags = "--localstatedir=/var";
 
@@ -24,9 +30,19 @@ stdenv.mkDerivation{
   '';
 
   meta = {
-    homepage = http://lxc.sourceforge.net;
-    description = "lxc Linux Containers userland tools";
+    homepage = "http://lxc.sourceforge.net";
+    description = "lightweight virtual system mechanism";
     license = stdenv.lib.licenses.lgpl21Plus;
+
+    longDescription = ''
+      LXC is the userspace control package for Linux Containers, a
+      lightweight virtual system mechanism sometimes described as
+      "chroot on steroids". LXC builds up from chroot to implement
+      complete virtual systems, adding resource management and isolation
+      mechanisms to Linux’s existing process management infrastructure.
+    '';
+
+
     platforms = stdenv.lib.platforms.linux;
     maintainers = [ stdenv.lib.maintainers.simons ];
   };
