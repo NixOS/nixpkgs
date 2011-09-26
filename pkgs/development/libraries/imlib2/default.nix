5da4cd69fc527e0f8355b78394d55d150b513640
Yury G. Kudryashov <urkud.urkud@gmail.com>
2011-09-26 13:13:45 +0000

    imlib2-1.4.5
    svn path=/nixpkgs/trunk/; revision=29496

--- pkgs/development/libraries/imlib2/default.nix
+++ pkgs/development/libraries/imlib2/default.nix
@@ -1,12 +1,18 @@
 { stdenv, fetchurl, x11, libjpeg, libtiff, libungif, libpng, bzip2, pkgconfig }:
 
-stdenv.mkDerivation {
-  name = "imlib2-1.4.4";
+stdenv.mkDerivation rec {
+  name = "imlib2-1.4.5";
   
   src = fetchurl {
-    url = mirror://sourceforge/enlightenment/imlib2-1.4.4.tar.gz;
-    sha256 = "163162aifak8ya17brzqwjlr8ywz40s2s3573js5blcc1g4m5pm4";
+    url = "mirror://sourceforge/enlightenment/${name}.tar.bz2";
+    sha256 = "0nllbhf8vfwdm40z35yj27n83k2mjf5vbd62khad4f0qjf9hsw14";
   };
   
-  buildInputs = [ x11 libjpeg libtiff libungif libpng bzip2 pkgconfig ];
+  buildInputs = [ x11 libjpeg libtiff libungif libpng bzip2 ];
+
+  buildNativeInputs = [ pkgconfig ];
+
+  meta = {
+    platforms = stdenv.lib.platforms.gnu; # random choice
+  };
 }
