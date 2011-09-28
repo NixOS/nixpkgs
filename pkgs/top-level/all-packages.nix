0f2e4160c0072d55a4d77fe19ceb94196fd650ca
Shea Levy <shea@shealevy.com>
2011-09-28 14:18:26 +0000

    Linux 3.0: Don't use aufs2.1, there's aufs 3.x for Linux 3.x kernels
    svn path=/nixpkgs/trunk/; revision=29520

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5550,7 +5550,7 @@ let
     kernelPatches =
       [ #kernelPatches.fbcondecor_2_6_38
         kernelPatches.sec_perm_2_6_24
-        kernelPatches.aufs2_1_3_0
+        #kernelPatches.aufs2_1_3_0
         #kernelPatches.mips_restart_2_6_36
       ];
   };
