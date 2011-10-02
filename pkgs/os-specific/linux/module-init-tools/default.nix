4b795e225b257d7c08ca242b72a87f56715e9e53
Shea Levy <shea@shealevy.com>
2011-10-02 04:03:02 +0000

    module-init-tools: 3.16
    svn path=/nixpkgs/trunk/; revision=29556

--- pkgs/os-specific/linux/module-init-tools/default.nix
+++ pkgs/os-specific/linux/module-init-tools/default.nix
@@ -1,23 +1,16 @@
-{stdenv, fetchurl}:
+{stdenv, fetchurl, docbook2x}:
 
-stdenv.mkDerivation {
-  name = "module-init-tools-3.4";
+stdenv.mkDerivation rec {
+  name = "module-init-tools-3.16";
   
-  src = [
-    (fetchurl {
-      url = mirror://kernel/linux/utils/kernel/module-init-tools/module-init-tools-3.4.tar.bz2;
-      sha256 = "11rxcdr915skc1m6dcavavw8dhcsy24wpi56sw1m4akj2frs3iwn";
-    })
+  src = fetchurl {
+    url = "mirror://kernel/linux/utils/kernel/module-init-tools/${name}.tar.bz2";
+    sha256 = "0jxnz9ahfic79rp93l5wxcbgh4pkv85mwnjlbv1gz3jawv5cvwp1";
+  };
 
-    # Upstream forgot to include the generated manpages.  Thankfully
-    # the Gentoo people fixed this for us :-)
-    (fetchurl {
-      url = mirror://gentoo/distfiles/module-init-tools-3.4-manpages.tar.bz2;
-      sha256 = "0jid24girjhr30mrdckylkcz11v4in46nshhrqv18yaxm6506v6j";
-    })
-  ];
+  buildInputs = [ docbook2x ];
 
-  patches = [./module-dir.patch];
+  patches = [ ./module-dir.patch ./docbook2man.patch ];
   
   postInstall = "rm $out/sbin/insmod.static"; # don't need it
 
