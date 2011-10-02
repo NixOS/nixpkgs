c073bf9c2261ad1ff6ad945555b5f6a990d0ed6c
Shea Levy <shea@shealevy.com>
2011-10-02 14:02:06 +0000

    Update the shadow tarball url
    svn path=/nixpkgs/trunk/; revision=29564

--- pkgs/os-specific/linux/shadow/default.nix
+++ pkgs/os-specific/linux/shadow/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "shadow-4.1.4.2";
    
   src = fetchurl {
-    url = "ftp://pkg-shadow.alioth.debian.org/pub/pkg-shadow/${name}.tar.bz2";
+    url = "http://pkg-shadow.alioth.debian.org/releases/${name}.tar.bz2";
     sha256 = "1449ny7pdnwkavg92wvibapnkgdq5pas38nvl1m5xa37g5m7z64p";
   };
 
