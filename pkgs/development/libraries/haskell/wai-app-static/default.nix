717d0a154df9c6ae4a260937341e78e770be8416
Peter Simons <simons@cryp.to>
2011-10-03 20:21:31 +0000

    haskell-wai-app-static: updated to version 0.3.4
    svn path=/nixpkgs/trunk/; revision=29615

--- pkgs/development/libraries/haskell/wai-app-static/default.nix
+++ pkgs/development/libraries/haskell/wai-app-static/default.nix
@@ -5,8 +5,8 @@
 
 cabal.mkDerivation (self: {
   pname = "wai-app-static";
-  version = "0.3.3";
-  sha256 = "04b4cw93agw136xjh9rssiw96vz3kxji7zh209brwfnwh13bra17";
+  version = "0.3.4";
+  sha256 = "0jxlm0qfv59c3q6ngvr94rzrn01byfn3wak208ws18xhk14cv058";
   buildDepends = [
     base64Bytestring blazeBuilder blazeHtml cryptohash fileEmbed
     httpDate httpTypes text time transformers unixCompat wai
