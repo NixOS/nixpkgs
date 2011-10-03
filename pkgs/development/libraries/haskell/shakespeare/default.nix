8855e4e69bb8fda84e4ce11bba85566c0945a90a
Peter Simons <simons@cryp.to>
2011-10-03 20:21:27 +0000

    haskell-shakespeare: updated to version 0.10.1.1
    svn path=/nixpkgs/trunk/; revision=29614

--- pkgs/development/libraries/haskell/shakespeare/default.nix
+++ pkgs/development/libraries/haskell/shakespeare/default.nix
@@ -1,10 +1,10 @@
-{ cabal, blazeBuilder, blazeHtml, failure, parsec, text }:
+{ cabal, parsec, text }:
 
 cabal.mkDerivation (self: {
   pname = "shakespeare";
-  version = "0.10.1";
-  sha256 = "1ckzfxrs65n8cx0hm64c3jd4hbw48x1vi0cvnkz39k500jpq874f";
-  buildDepends = [ blazeBuilder blazeHtml failure parsec text ];
+  version = "0.10.1.1";
+  sha256 = "1qd5wrcr4ss5zigbb7s6c7y7qbvrnbvgdpwq985yyy71i5hwxv0a";
+  buildDepends = [ parsec text ];
   meta = {
     homepage = "http://www.yesodweb.com/book/templates";
     description = "A toolkit for making compile-time interpolated templates";
