fc77db6d2125326a276b214c2b8f4e919db6d081
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-10-03 20:42:45 +0000

    * Wine 1.3.29.
    svn path=/nixpkgs/trunk/; revision=29617

--- pkgs/misc/emulators/wine/src-info-for-default.nix
+++
@@ -1,6 +0,0 @@
-{
-  downloadPage = "http://www.winehq.org/";
-  baseName = "wine";
-  versionExtractorSedScript = ''s/[^-]*-(.+)[.]tar[.].*/\1/'';
-  versionReferenceCreator = ''$(replaceAllVersionOccurences)'';
-}
