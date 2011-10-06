e0c4e42d8cb0052b642f0e4b0a9654b7d31c775e
Peter Simons <simons@cryp.to>
2011-10-06 21:30:27 +0000

    haskell-cmdlib: updated to version 0.3.4
    svn path=/nixpkgs/trunk/; revision=29705

--- pkgs/development/libraries/haskell/cmdlib/default.nix
+++ pkgs/development/libraries/haskell/cmdlib/default.nix
@@ -1,12 +1,12 @@
-{ cabal, mtl, split, syb }:
+{ cabal, mtl, split, syb, transformers }:
 
 cabal.mkDerivation (self: {
   pname = "cmdlib";
-  version = "0.3.3";
-  sha256 = "0gryz70d69r9pscwxmn5yr02r0zvvgj1vwc3g9klgbkipbsa7xvk";
+  version = "0.3.4";
+  sha256 = "1f8vb681xd2v7hv0s84x032yf8x2jlxc0j302irv20fkc1w1vbrr";
   isLibrary = true;
   isExecutable = true;
-  buildDepends = [ mtl split syb ];
+  buildDepends = [ mtl split syb transformers ];
   meta = {
     description = "a library for command line parsing & online help";
     license = self.stdenv.lib.licenses.bsd3;
