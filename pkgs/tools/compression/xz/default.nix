0baf36471578a960588c78eb69d89dc7623029ee
Ludovic Courtès <ludo@gnu.org>
2011-10-14 21:56:07 +0000

    XZ Utils 5.0.3.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=29846

--- pkgs/tools/compression/xz/default.nix
+++ pkgs/tools/compression/xz/default.nix
@@ -1,25 +1,35 @@
-{stdenv, fetchurl, lib}:
+{ stdenv, fetchurl }:
 
-stdenv.mkDerivation ({
-  name = "xz-5.0.0";
-  
+stdenv.mkDerivation rec {
+  name = "xz-5.0.3";
+
   src = fetchurl {
-    url = http://tukaani.org/xz/xz-5.0.0.tar.bz2;
-    sha256 = "1n2nc00d83di0jl5d9qwpngkmlk3wqhs4w9q2ah680v9qijrxa27";
+    url = "http://tukaani.org/xz/${name}.tar.bz2";
+    sha256 = "1sgaq4gdh8llz3gnlgvd65x610fwc8h4m32skhqn5npwgghvj4as";
   };
+
+  doCheck = true;
 
   meta = {
     homepage = http://tukaani.org/xz/;
-    description = "Successor of the LZMA Utils package";
-    license = "GPL/LGPL";
-    maintainers = [ lib.maintainers.sander ];
+    description = "XZ, general-purpose data compression software, successor of LZMA";
+
+    longDescription =
+      '' XZ Utils is free general-purpose data compression software with high
+         compression ratio.  XZ Utils were written for POSIX-like systems,
+         but also work on some not-so-POSIX systems.  XZ Utils are the
+         successor to LZMA Utils.
+
+         The core of the XZ Utils compression code is based on LZMA SDK, but
+         it has been modified quite a lot to be suitable for XZ Utils.  The
+         primary compression algorithm is currently LZMA2, which is used
+         inside the .xz container format.  With typical files, XZ Utils
+         create 30 % smaller output than gzip and 15 % smaller output than
+         bzip2.
+      '';
+
+    licenses = [ "GPLv2+" "LGPLv2.1+" ];
+    maintainers = with stdenv.lib.maintainers; [ sander ludo ];
+    platforms = stdenv.lib.platforms.all;
   };
 }
-
-//
-
-(if stdenv.system == "x86_64-darwin"
- # Work around assembler misconfiguration as `x86'.  This appears to be fixed
- # by commit b9b5c54cd438b3ae47b44cc211b71f3bc53e35ef (Nov 22 12:00:30 2009 # +0200).
- then { configureFlags = "--enable-assembler=x86_64"; }
- else {}))
