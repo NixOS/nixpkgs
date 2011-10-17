491c6e43b182a189f27455dcc17e64ca7b7dbd7d
Russell O'Connor <roconnor@theorem.ca>
2011-10-17 22:18:21 +0000

    Package for picosat.
    svn path=/nixpkgs/trunk/; revision=29869

---
+++ pkgs/applications/science/logic/picosat/default.nix
@@ -0,0 +1,41 @@
+{stdenv, fetchurl }:
+
+let
+  version = "936";
+  pname = "picosat";
+
+in
+
+stdenv.mkDerivation rec {
+  name = "${pname}-${version}";
+
+  src = fetchurl {
+    url = "http://fmv.jku.at/picosat/${name}.tar.gz";
+    sha256 = "02hq68fmfjs085216wsj13ff6i1rhc652yscl16w9jzpfqzly91n";
+  };
+
+  dontAddPrefix = true;
+
+  # configureFlags = "--shared"; the ./configure file is broken and doesn't accept this parameter :(
+  patchPhase = ''
+   sed -e 's/^shared=no/shared=yes/' -i configure
+  '';
+
+  installPhase = ''
+   ensureDir "$out"/bin
+   cp picomus "$out"/bin
+   cp picosat "$out"/bin
+   ensureDir "$out"/lib
+   cp libpicosat.a "$out"/lib
+   cp libpicosat.so "$out"/lib
+   ensureDir "$out"/include/picosat
+   cp picosat.h "$out"/include/picosat
+  '';
+
+  meta = {
+    homepage = http://fmv.jku.at/picosat/;
+    description = "SAT solver with proof and core support";
+    license = "MIT";
+    maintainers = [ stdenv.lib.maintainers.roconnor ];
+  };
+}
