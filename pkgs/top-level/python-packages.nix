42fd7e5d64b79eec1a3c75e01f8377ed92071e84
Peter Simons <simons@cryp.to>
2011-10-19 10:27:57 +0000

    python-ldap: added version 2.4.3
    svn path=/nixpkgs/trunk/; revision=29886

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1358,6 +1358,17 @@ let pythonPackages = python.modules // rec {
     };
   };
 
+  ldap = buildPythonPackage rec {
+    name = "python-ldap-2.4.3";
+
+    src = fetchurl {
+      url = "http://pypi.python.org/packages/source/p/python-ldap/${name}.tar.gz";
+      sha256 = "17aysa9b4zjw00ikjirf4m37xbp2ifj1g0zjs14xzqqib3nh1yw8";
+    };
+
+    NIX_CFLAGS_COMPILE = "-I${pkgs.cyrus_sasl}/include/sasl";
+    propagatedBuildInputs = [pkgs.openldap pkgs.cyrus_sasl pkgs.openssl];
+  };
 
   pylint = buildPythonPackage rec {
     name = "pylint-0.23.0";
