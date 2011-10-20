631f071df875d861974387d587aa76cccbf2009b
Sander van der Burg <s.vanderburg@tudelft.nl>
2011-10-20 11:07:47 +0000

    Bumped version of disnixos
    svn path=/nixpkgs/trunk/; revision=29929

--- pkgs/tools/package-management/disnix/disnixos/default.nix
+++ pkgs/tools/package-management/disnix/disnixos/default.nix
@@ -1,17 +1,17 @@
 {stdenv, fetchurl, disnix, socat, pkgconfig}:
 
 stdenv.mkDerivation {
-  name = "disnixos-0.2pre29885";
+  name = "disnixos-0.2pre29928";
   src = fetchurl {
-    url = http://hydra.nixos.org/build/1461383/download/3/disnixos-0.2pre29885.tar.gz;
-    sha256 = "1lyxpadykk7nbb7gad12mbcsz29c6fqihhv3k3vkh32fimbq7bma";
+    url = http://hydra.nixos.org/build/1464065/download/3/disnixos-0.2pre29928.tar.gz;
+    sha256 = "0l7kszbpi8lzq95khi45fbq71l5csf8kamds4va6bxsyfwxrx6sm";
   };
   buildInputs = [ socat pkgconfig disnix ];
   dontStrip = true;
   NIX_STRIP_DEBUG = true;
   
   meta = {
-    description = "Provides complementary NixOS infrastructure deployment to NixOS";
+    description = "Provides complementary NixOS infrastructure deployment to Disnix";
     license = "LGPLv2.1+";
     maintainers = [ stdenv.lib.maintainers.sander ];
   };
