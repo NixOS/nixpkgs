bba184d3753e5353a159ab9d05f984b4ed603fa8
Peter Simons <simons@cryp.to>
2011-10-21 16:13:56 +0000

    haskell-ConfigFile: updated to version 1.1.0
    svn path=/nixpkgs/trunk/; revision=29960

--- pkgs/development/libraries/haskell/ConfigFile/default.nix
+++ pkgs/development/libraries/haskell/ConfigFile/default.nix
@@ -2,11 +2,12 @@
 
 cabal.mkDerivation (self: {
   pname = "ConfigFile";
-  version = "1.0.6";
-  sha256 = "339722184b86d53d8b361933e572b6e1478824c7eba3fb66b67d0eb5245cd038";
+  version = "1.1.0";
+  sha256 = "0m5p56if711qi69lxw78746sb0jr5gqbmip5hdbb7lk4z5drgvhc";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [ MissingH mtl parsec ];
+  noHaddock = true;
   meta = {
     homepage = "http://software.complete.org/configfile";
     description = "Configuration file reading & writing";
