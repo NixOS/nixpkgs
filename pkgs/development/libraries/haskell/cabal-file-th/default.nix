e09dbeb845188f7dfbc5f39c6cba927ef359af9a
Peter Simons <simons@cryp.to>
2011-10-25 16:43:13 +0000

    haskell-cabal-file-th: updated to version 0.2.1
    svn path=/nixpkgs/trunk/; revision=30015

--- pkgs/development/libraries/haskell/cabal-file-th/default.nix
+++ pkgs/development/libraries/haskell/cabal-file-th/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "cabal-file-th";
-  version = "0.1";
-  sha256 = "1i0k8c9kf2av0vs9qhd15kqrkzi5z89va4gp2cfkssq66y98k8p8";
+  version = "0.2.1";
+  sha256 = "0nczwicgf6kx3lk5m7wqf1wj6ghn8jfx112dzh7jh4f4xs66nsd1";
   meta = {
     homepage = "http://github.com/nkpart/cabal-file-th";
     description = "Template Haskell expressions for reading fields from a project's cabal file";
