b74d8fb90f7847ad488f56c4cfc1cdc7ccbad053
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2011-10-25 17:46:26 +0000

    Making gpgme optional in mutt. A dependency of gpgme (pth) does not build on arm.
    svn path=/nixpkgs/trunk/; revision=30019

--- pkgs/applications/networking/mailreaders/mutt/default.nix
+++ pkgs/applications/networking/mailreaders/mutt/default.nix
@@ -1,11 +1,13 @@
-{ stdenv, fetchurl, ncurses, which, perl, gpgme
+{ stdenv, fetchurl, ncurses, which, perl
 , sslSupport ? true
 , imapSupport ? true
 , headerCache ? true
 , saslSupport ? true
+, gpgmeSupport ? true
 , gdbm ? null
 , openssl ? null
 , cyrus_sasl ? null
+, gpgme ? null
 }:
 
 assert headerCache -> gdbm != null;
@@ -21,17 +23,20 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [
-    ncurses which perl gpgme
+    ncurses which perl
     (if headerCache then gdbm else null)
     (if sslSupport then openssl else null)
     (if saslSupport then cyrus_sasl else null)
+    (if gpgmeSupport then gpgme else null)
   ];
   
   configureFlags = [
     "--with-mailpath=" "--enable-smtp"
 
     # This allows calls with "-d N", that output debug info into ~/.muttdebug*
-    "--enable-debug" "--enable-pop" "--enable-imap" "--enable-gpgme"
+    "--enable-debug"
+
+    "--enable-pop" "--enable-imap"
 
     # The next allows building mutt without having anything setgid
     # set by the installer, and removing the need for the group 'mail'
@@ -41,6 +46,7 @@ stdenv.mkDerivation rec {
     (if sslSupport then "--with-ssl" else "--without-ssl")
     (if imapSupport then "--enable-imap" else "--disable-imap")
     (if saslSupport then "--with-sasl" else "--without-sasl")
+    (if gpgmeSupport then "--enable-gpgme" else "--disable-gpgme")
   ];
 
   meta = {
