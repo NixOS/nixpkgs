9de4823ff5ea0da120230ffd913f6c1cb571a442
Peter Simons <simons@cryp.to>
2011-10-26 17:37:15 +0000

    haskell-text: updated to version 0.11.1.6
    svn path=/nixpkgs/trunk/; revision=30037

--- pkgs/development/libraries/haskell/text/0.11.1.6.nix
+++ pkgs/development/libraries/haskell/text/0.11.1.6.nix
@@ -2,11 +2,11 @@
 
 cabal.mkDerivation (self: {
   pname = "text";
-  version = "0.11.1.5";
-  sha256 = "0fxxhw932gdvaqafsbw7dfzccc43hv92yhxppzp6jrg0npbyz04l";
+  version = "0.11.1.6";
+  sha256 = "1c4vzxwjcmdlb8nj71g6rqdw4nhz0l17saazhw1vv8cbizmdm4m7";
   buildDepends = [ deepseq ];
   meta = {
-    homepage = "https://bitbucket.org/bos/text";
+    homepage = "https://github.com/bos/text";
     description = "An efficient packed Unicode text type";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
