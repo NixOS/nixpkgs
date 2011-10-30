e93ae189aba7d41565918a799952c0f6cff0131e
Peter Simons <simons@cryp.to>
2011-10-30 16:06:45 +0000

    haskell-skein: updated to version 0.1.0.3
    svn path=/nixpkgs/trunk/; revision=30136

--- pkgs/development/libraries/haskell/skein/default.nix
+++ pkgs/development/libraries/haskell/skein/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "skein";
-  version = "0.1.0.2";
-  sha256 = "1ay7ri011vwvl74b9calbnav90d0r08gzqgdk8nvw1qx4slk1ibs";
+  version = "0.1.0.3";
+  sha256 = "1lag86db793l7n6zg97kn5wv31dal5sb8wig4sr7kqschxszq44d";
   buildDepends = [ cereal cryptoApi tagged ];
   meta = {
     description = "Skein, a family of cryptographic hash functions. Includes Skein-MAC as well.";
