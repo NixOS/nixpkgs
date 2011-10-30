652a24f21a2865976465fc3d44270993c90555c8
Peter Simons <simons@cryp.to>
2011-10-30 16:06:47 +0000

    haskell-curl: added meta.maintainers
    svn path=/nixpkgs/trunk/; revision=30137

--- pkgs/development/libraries/haskell/curl/default.nix
+++ pkgs/development/libraries/haskell/curl/default.nix
@@ -9,5 +9,9 @@ cabal.mkDerivation (self: {
     description = "Haskell binding to libcurl";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.andres
+      self.stdenv.lib.maintainers.simons
+    ];
   };
 })
