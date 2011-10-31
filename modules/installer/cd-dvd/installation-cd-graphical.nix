b024940384090772f6793ccd68dadca24a1342a9
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-10-31 21:08:22 +0000

    * Enable upower on the graphical ISO.
    svn path=/nixos/trunk/; revision=30156

--- modules/installer/cd-dvd/installation-cd-graphical.nix
+++ modules/installer/cd-dvd/installation-cd-graphical.nix
@@ -11,4 +11,8 @@
 
   # Provide wicd for easy wireless configuration.
   networking.wicd.enable = true;
+
+  # KDE complains if power management is disabled (to be precise, if
+  # there is no power management backend such as upower).
+  powerManagement.enable = true;
 }
