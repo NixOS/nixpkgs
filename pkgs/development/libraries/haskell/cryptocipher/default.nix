5e5a7ea1677bb4cf25a3bb51953b8ef6593db7c0
Peter Simons <simons@cryp.to>
2011-11-02 01:42:15 +0000

    haskell-cryptocipher: updated to version 0.3.0
    svn path=/nixpkgs/trunk/; revision=30181

--- pkgs/development/libraries/haskell/cryptocipher/default.nix
+++ pkgs/development/libraries/haskell/cryptocipher/default.nix
@@ -16,5 +16,9 @@ cabal.mkDerivation (self: {
     description = "Symmetrical Block, Stream and PubKey Ciphers";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.andres
+      self.stdenv.lib.maintainers.simons
+    ];
   };
 })
