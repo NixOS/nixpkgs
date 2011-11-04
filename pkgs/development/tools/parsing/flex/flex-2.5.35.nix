ca92853163eca46b480fd0f6f6a38f4891fc33fd
Michael Raskin <7c6f434c@mail.ru>
2011-11-04 21:31:07 +0000

    Make cross-buildable
    svn path=/nixpkgs/trunk/; revision=30238

--- pkgs/development/tools/parsing/flex/flex-2.5.35.nix
+++ pkgs/development/tools/parsing/flex/flex-2.5.35.nix
@@ -13,6 +13,13 @@ stdenv.mkDerivation {
   buildInputs = [yacc];
   propagatedBuildNativeInputs = [m4];
 
+  crossAttrs = {
+    preConfigure = ''
+      export ac_cv_func_malloc_0_nonnull=yes
+      export ac_cv_func_realloc_0_nonnull=yes
+    '';
+  };
+
   meta = {
     description = "A fast lexical analyser generator";
   };
