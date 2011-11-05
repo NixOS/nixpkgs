70035096a596bbfba7e10b81cf2aaa28c21760df
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2011-11-05 18:50:43 +0000

    Updating our super-old postfix. 2.8.6 builds. I don't know if it serves well.
    svn path=/nixpkgs/trunk/; revision=30244

--- pkgs/servers/mail/postfix/default.nix
+++ pkgs/servers/mail/postfix/default.nix
@@ -1,15 +1,15 @@
 { stdenv, fetchurl, db4, glibc, openssl, cyrus_sasl
-, coreutils, findutils, gnused, gnugrep
+, coreutils, findutils, gnused, gnugrep, bison, perl
 }:
 
 assert stdenv.isLinux;
 
 stdenv.mkDerivation {
-  name = "postfix-2.2.11";
+  name = "postfix-2.8.6";
   
   src = fetchurl {
-    url = ftp://ftp.cs.uu.nl/mirror/postfix/postfix-release/official/postfix-2.2.11.tar.gz;
-    sha256 = "04hxpyd3h1f48fnppjwqqxbil13bcwidzpfkra2pgm7h42d9blq7";
+    url = ftp://ftp.cs.uu.nl/mirror/postfix/postfix-release/official/postfix-2.8.6.tar.gz;
+    sha256 = "1rfsfhna5hy5lc6hkg1zc2862pdc5c1y9z6aiy8rinlmzrfplhlb";
   };
 
   installTargets = ["non-interactive-package"];
@@ -43,12 +43,12 @@ stdenv.mkDerivation {
     export sample_directory=$out/share/postfix/doc/samples
     export readme_directory=$out/share/postfix/doc
 
-    make makefiles CCARGS='-DUSE_TLS -DUSE_SASL_AUTH -DUSE_CYRUS_SASL -DHAS_DB -I${cyrus_sasl}/include/sasl' AUXLIBS='-lssl -lcrypto -lsasl2 -ldb'
+    make makefiles CCARGS='-DUSE_TLS -DUSE_SASL_AUTH -DUSE_CYRUS_SASL -I${cyrus_sasl}/include/sasl' AUXLIBS='-lssl -lcrypto -lsasl2 -ldb -lnsl'
   '';
 
-  buildInputs = [db4 openssl cyrus_sasl];
+  buildInputs = [db4 openssl cyrus_sasl bison perl];
   
-  patches = [./postfix-2.2.9-db.patch ./postfix-2.2.9-lib.patch];
+  patches = [ ./postfix-2.2.9-db.patch  ./postfix-2.2.9-lib.patch ./db-linux3.patch ];
   
   inherit glibc;
 }
