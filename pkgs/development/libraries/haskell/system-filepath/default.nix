e57615635495103bb7ec993a54a2d1ee312be4b4
Peter Simons <simons@cryp.to>
2011-11-06 18:36:36 +0000

    haskell-system-filepath: updated to version 0.4.3
    svn path=/nixpkgs/trunk/; revision=30270

--- pkgs/development/libraries/haskell/system-filepath/default.nix
+++ pkgs/development/libraries/haskell/system-filepath/default.nix
@@ -1,10 +1,10 @@
-{ cabal, text }:
+{ cabal, deepseq, text }:
 
 cabal.mkDerivation (self: {
   pname = "system-filepath";
-  version = "0.4.2";
-  sha256 = "070srsvqqjix0afy5ch1zcmpnrrszkds83rv0dp0izqrlzl038mr";
-  buildDepends = [ text ];
+  version = "0.4.3";
+  sha256 = "16a57dipz3aid5n22gzyd9yqmsxm98c3s6vb7minj82q9rbl5z67";
+  buildDepends = [ deepseq text ];
   meta = {
     homepage = "https://john-millikin.com/software/hs-filepath/";
     description = "High-level, byte-based file and directory path manipulations";
