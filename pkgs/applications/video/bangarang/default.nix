6d51717fcfb10d83434df784adf1957393dba321
Evgeny Egorochkin <phreedom.stdin@gmail.com>
2011-11-09 13:01:51 +0000

    Bangarang: fix deps to get it to compile against kde 4.7
    svn path=/nixpkgs/trunk/; revision=30353

--- pkgs/applications/video/bangarang/default.nix
+++ pkgs/applications/video/bangarang/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, cmake, qt4, kdelibs, automoc4, phonon, soprano, kdemultimedia, taglib, glibc, gettext }:
+{ stdenv, fetchurl, cmake, qt4, kdelibs, automoc4, phonon, soprano, shared_desktop_ontologies, kdemultimedia, taglib, glibc, gettext }:
 stdenv.mkDerivation rec {
   name = "bangarang-2.0";
 
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec {
     sha256 = "1fixqx56k0mk0faz35rzpdg6zaa0mvm4548rg0g7fhafl35fxzlz";
   };
 
-  buildInputs = [ cmake qt4 kdelibs automoc4 phonon soprano kdemultimedia taglib glibc gettext ];
+  buildInputs = [ cmake qt4 kdelibs automoc4 phonon soprano shared_desktop_ontologies kdemultimedia taglib glibc gettext ];
 
   meta = with stdenv.lib; {
     description = "A media player for KDE based on Nepomuk and Phonon";
