11426750a0ccd3a247d3a24ae9f53c35666fb1d7
Evgeny Egorochkin <phreedom.stdin@gmail.com>
2011-11-09 14:24:14 +0000

    Revert "DigiKam: fix deps" This commit breaks hydra evaluation for some reason, but it works locally for some reason... This reverts commit 3ba002080ab1d8e28ca1b824de01a5156487735f.
    svn path=/nixpkgs/trunk/; revision=30355

--- pkgs/applications/graphics/digikam/default.nix
+++ pkgs/applications/graphics/digikam/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, cmake, qt4, kdelibs, automoc4, phonon, qimageblitz, qca2, eigen,
-kdegraphics, lcms, jasper, libgphoto2, kdepimlibs, gettext, soprano, kdeedu, libkipi, libkdcraw, libkexiv2,
+kdegraphics, lcms, jasper, libgphoto2, kdepimlibs, gettext, soprano, kdeedu,
 liblqr1, lensfun, pkgconfig }:
 
 stdenv.mkDerivation rec {
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [ cmake qt4 kdelibs kdegraphics automoc4 phonon qimageblitz qca2 eigen
-    lcms jasper libgphoto2 kdepimlibs gettext soprano kdeedu liblqr1 lensfun libkipi libkdcraw libkexiv2
+    lcms jasper libgphoto2 kdepimlibs gettext soprano kdeedu liblqr1 lensfun
     pkgconfig ];
 
   KDEDIRS=kdeedu;
