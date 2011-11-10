5b622e20a161503f1142549a26aeaf7317442d60
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-11-10 23:35:58 +0000

    * KDE 4.7.3.
    svn path=/nixpkgs/trunk/; revision=30384

--- pkgs/desktops/kde-4.7/default.nix
+++ pkgs/desktops/kde-4.7/default.nix
@@ -1,7 +1,7 @@
 { callPackage, callPackageOrig, stdenv, qt47 }:
 
 let
-  release = "4.7.2";
+  release = "4.7.3";
 
   # Need callPackageOrig to avoid infinite cycle
   kde = callPackageOrig ./kde-package {
