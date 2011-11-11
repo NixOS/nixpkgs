718a4ec1c597dc464a91426a6651fdac71d8df42
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-11-11 02:28:12 +0000

    svn path=/nixpkgs/trunk/; revision=30386

--- pkgs/top-level/release.nix
+++ pkgs/top-level/release.nix
@@ -383,8 +383,8 @@ with (import ./release-lib.nix);
 
   firefox36Pkgs.firefox = linux;
   firefox50Pkgs.firefox = linux;
-  firefox60Pkgs.firefox = linux;
   firefox70Pkgs.firefox = linux;
+  firefox80Pkgs.firefox = linux;
 
   gnome = {
     gnome_panel = linux;
