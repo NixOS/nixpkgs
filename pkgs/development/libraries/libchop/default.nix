ac443637fec5f9f363146c9dd14279d93f1890e4
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2011-11-13 14:56:14 +0000

    Fixing the description of libchop; it had the reminiscences of the gnutls default.nix I used as base.
    svn path=/nixpkgs/trunk/; revision=30413

--- pkgs/development/libraries/libchop/default.nix
+++ pkgs/development/libraries/libchop/default.nix
@@ -15,25 +15,12 @@ stdenv.mkDerivation rec {
   doCheck = true;
 
   meta = {
-    description = "The GNU Transport Layer Security Library";
-
-    longDescription = ''
-       GnuTLS is a project that aims to develop a library which
-       provides a secure layer, over a reliable transport
-       layer. Currently the GnuTLS library implements the proposed
-       standards by the IETF's TLS working group.
-
-       Quoting from the TLS protocol specification:
-
-       "The TLS protocol provides communications privacy over the
-       Internet. The protocol allows client/server applications to
-       communicate in a way that is designed to prevent eavesdropping,
-       tampering, or message forgery."
-    '';
+    description = "Set of utilities and library for data backup and distributed storage";
 
     homepage = http://nongnu.org/libchop/;
     license = "GPLv3+";
 
-    maintainers = [ stdenv.lib.maintainers.ludo stdenv.lib.maintainers.viric ];
+    maintainers = [ stdenv.lib.maintainers.ludo
+      stdenv.lib.maintainers.viric ];
   };
 }
