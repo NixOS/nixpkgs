ce87cf8033871015cea92ca60e639804f1da4469
Peter Simons <simons@cryp.to>
2011-11-15 17:55:12 +0000

    haskell-safe: updated to version 0.3.2
    svn path=/nixpkgs/trunk/; revision=30440

--- pkgs/development/libraries/haskell/safe/default.nix
+++ pkgs/development/libraries/haskell/safe/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "safe";
-  version = "0.3.1";
-  sha256 = "0sq4fcwh9bmhi79rnxh70hqwa2k3sipqnvq0f9a78j4wg6nmm6li";
+  version = "0.3.2";
+  sha256 = "1gblidpsz2zyr5aw5c9ggxzx3firdz7s7iai9vf9gc5mv9vnnggv";
   meta = {
     homepage = "http://community.haskell.org/~ndm/safe/";
     description = "Library for safe (pattern match free) functions";
