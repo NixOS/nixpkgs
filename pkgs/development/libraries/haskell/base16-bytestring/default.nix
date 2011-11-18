ca45069a2309a32e8092bd3574c918857243ec4e
Peter Simons <simons@cryp.to>
2011-11-18 18:30:12 +0000

    haskell-base16-bytestring: updated to version 0.1.1.3
    svn path=/nixpkgs/trunk/; revision=30468

--- pkgs/development/libraries/haskell/base16-bytestring/default.nix
+++ pkgs/development/libraries/haskell/base16-bytestring/default.nix
@@ -2,11 +2,11 @@
 
 cabal.mkDerivation (self: {
   pname = "base16-bytestring";
-  version = "0.1.1.2";
-  sha256 = "1isxyl52vh0lg195wq9nkr3hlmbw3d3c9aymxlz8hynz0hh1q1z0";
+  version = "0.1.1.3";
+  sha256 = "0v08fnkykvd6y6in6f9a808vk2gfd9pf0wd7rr28z6wwxm5d2x6l";
   meta = {
-    homepage = "http://github.com/mailrank/base16-bytestring";
-    description = "Fast base16 (hex) encoding and deconding for ByteStrings";
+    homepage = "http://github.com/bos/base16-bytestring";
+    description = "Fast base16 (hex) encoding and decoding for ByteStrings";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [
