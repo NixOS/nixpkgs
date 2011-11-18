0d7b744419c83c0bb7bb2eef664e888b888668c7
Peter Simons <simons@cryp.to>
2011-11-18 18:30:24 +0000

    haskell-double-conversion: updated to version 0.2.0.3
    svn path=/nixpkgs/trunk/; revision=30471

--- pkgs/development/libraries/haskell/double-conversion/default.nix
+++ pkgs/development/libraries/haskell/double-conversion/default.nix
@@ -2,11 +2,11 @@
 
 cabal.mkDerivation (self: {
   pname = "double-conversion";
-  version = "0.2.0.1";
-  sha256 = "146ijcv55k0lqlw8y4nz7p5kjpwry2jzbqmkan904pnlcfv4a60a";
+  version = "0.2.0.3";
+  sha256 = "17ny1gvd622rnqjvlrmcpgw3wlabrsc6d046d4ii6xv299z97qw9";
   buildDepends = [ text ];
   meta = {
-    homepage = "https://github.com/mailrank/double-conversion";
+    homepage = "https://github.com/bos/double-conversion";
     description = "Fast conversion between double precision floating point and text";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
