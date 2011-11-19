9b2bd7479f4376292fd66cee36fed4d12498f31c
Ludovic Courtès <ludo@gnu.org>
2011-11-19 18:28:16 +0000

    Shadow: Make Linux-PAM optional; allow cross-compilation.
    svn path=/nixpkgs/trunk/; revision=30482

--- pkgs/os-specific/linux/shadow/default.nix
+++ pkgs/os-specific/linux/shadow/default.nix
@@ -1,22 +1,31 @@
-{ stdenv, fetchurl, pam, glibc }:
-   
+{ stdenv, fetchurl, pam ? null, glibcCross ? null }:
+
+let
+  glibc = if stdenv ? cross
+          then glibcCross
+          else stdenv.glibc;
+in
 stdenv.mkDerivation rec {
   name = "shadow-4.1.4.2";
-   
+
   src = fetchurl {
     url = "http://pkg-shadow.alioth.debian.org/releases/${name}.tar.bz2";
     sha256 = "1449ny7pdnwkavg92wvibapnkgdq5pas38nvl1m5xa37g5m7z64p";
   };
 
-  buildInputs = [ pam ];
+  buildInputs = stdenv.lib.optional (pam != null && stdenv.isLinux) pam;
 
   patches = [ ./no-sanitize-env.patch ./su-name.patch ./keep-path.patch ];
 
-  preBuild =
+  # Assume System V `setpgrp (void)', which is the default on GNU variants
+  # (`AC_FUNC_SETPGRP' is not cross-compilation capable.)
+  preConfigure = "export ac_cv_func_setpgrp_void=yes";
+
+  preBuild = assert glibc != null;
     ''
       substituteInPlace lib/nscd.c --replace /usr/sbin/nscd ${glibc}/sbin/nscd
     '';
-  
+
   meta = {
     homepage = http://pkg-shadow.alioth.debian.org/;
     description = "Suite containing authentication-related tools such as passwd and su";
