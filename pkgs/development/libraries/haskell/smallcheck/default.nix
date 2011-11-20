0a872860d77fbb0feda1dfaab71ab4aec0c2a1d8
Peter Simons <simons@cryp.to>
2011-11-20 10:13:48 +0000

    haskell-smallcheck: updated to version 0.5
    svn path=/nixpkgs/trunk/; revision=30487

--- pkgs/development/libraries/haskell/smallcheck/default.nix
+++ pkgs/development/libraries/haskell/smallcheck/default.nix
@@ -2,9 +2,10 @@
 
 cabal.mkDerivation (self: {
   pname = "smallcheck";
-  version = "0.4";
-  sha256 = "0nq13jm3akrmgk6n2clisip16v0jf1xkm0hm678v63s87hxqb1ma";
+  version = "0.5";
+  sha256 = "10bmaifpkd5h3fp76cgbbc21q6nd0v8b3f80fjnb1ggg5zqs10dv";
   meta = {
+    homepage = "https://github.com/feuerbach/smallcheck";
     description = "Another lightweight testing library in Haskell";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
