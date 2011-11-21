80dc05cc9a7148099af7beb88900a096a530b20c
Peter Simons <simons@cryp.to>
2011-11-21 17:54:29 +0000

    haskell-xmonad-extras: updated to version 0.10
    svn path=/nixpkgs/trunk/; revision=30513

--- pkgs/applications/window-managers/xmonad/xmonad-extras.nix
+++ pkgs/applications/window-managers/xmonad/xmonad-extras.nix
@@ -1,13 +1,14 @@
-{ cabal, hint, mtl, network, parsec, random, split, X11, xmonad
-, xmonadContrib
+{ cabal, hint, mtl, network, parsec, random, regexPosix, split, X11
+, xmonad, xmonadContrib
 }:
 
 cabal.mkDerivation (self: {
   pname = "xmonad-extras";
-  version = "0.9.2";
-  sha256 = "54b41a4c59ff3d68b3a214d727fb5675fa7c1b90090d99e58ecae62b3dfdd701";
+  version = "0.10";
+  sha256 = "0pxvzqcwva64xlrw21wxxc8zq8w36rgg58a12j6kz47ivwkin27g";
   buildDepends = [
-    hint mtl network parsec random split X11 xmonad xmonadContrib
+    hint mtl network parsec random regexPosix split X11 xmonad
+    xmonadContrib
   ];
   configureFlags = "-f-with_hlist -f-with_mpd";
   noHaddock = true;
