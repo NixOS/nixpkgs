050ea140223dfb8b290f7078707163c93339bbea
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-11-22 12:59:22 +0000

    * DBLaTeX updated to 0.3.2.
    svn path=/nixpkgs/trunk/; revision=30523

--- pkgs/misc/tex/dblatex/default.nix
+++ pkgs/misc/tex/dblatex/default.nix
@@ -1,20 +1,18 @@
-{stdenv, fetchurl, python, libxslt, tetex}:
+{ stdenv, fetchurl, python, libxslt, tetex }:
 
 stdenv.mkDerivation rec {
-  name = "dblatex-0.2.12";
+  name = "dblatex-0.3.2";
 
   src = fetchurl {
     url = "mirror://sourceforge/dblatex/${name}.tar.bz2";
-    sha256 = "1wjghrlcn7hkr70nnyzzag1z57l5b1ck8i3r8zl7bw2rsrvqmyz2";
+    sha256 = "0jg2acv2lyrbw06l8rr0id75lj1pws7xbsmryq58r3n13xzb1p7b";
   };
 
   buildPhase = "true";
   
-  installPhase = "
-    python ./setup.py install --prefix=$out
-  ";
+  installPhase = "python ./setup.py install --prefix=$out";
 
-  buildInputs = [python libxslt tetex];
+  buildInputs = [ python libxslt tetex ];
 
   meta = {
     description = "A program to convert DocBook to DVI, PostScript or PDF via LaTeX or ConTeXt";
