53510bff9a73c40189b8c5b1ae172205b1b49912
Peter Simons <simons@cryp.to>
2011-11-25 13:44:48 +0000

    haskell-hakyll: updated to version 3.2.2.0
    svn path=/nixpkgs/trunk/; revision=30563

--- pkgs/development/libraries/haskell/hakyll/default.nix
+++ pkgs/development/libraries/haskell/hakyll/default.nix
@@ -1,14 +1,15 @@
-{ cabal, binary, blazeHtml, cryptohash, hamlet, mtl, pandoc, parsec
-, regexBase, regexPcre, snapCore, snapServer, tagsoup, time
+{ cabal, binary, blazeHtml, citeprocHs, cryptohash, hamlet, mtl
+, pandoc, parsec, regexBase, regexPcre, snapCore, snapServer
+, tagsoup, time
 }:
 
 cabal.mkDerivation (self: {
   pname = "hakyll";
-  version = "3.2.0.10";
-  sha256 = "1hdivw1smfkxza5jl8gm84gnzb5a9sgc2lcas3hikv968p9c1yry";
+  version = "3.2.2.0";
+  sha256 = "0z7q99j3zqhck085s99sbj1qksra3rsmwimf44j49b59nqrn50va";
   buildDepends = [
-    binary blazeHtml cryptohash hamlet mtl pandoc parsec regexBase
-    regexPcre snapCore snapServer tagsoup time
+    binary blazeHtml citeprocHs cryptohash hamlet mtl pandoc parsec
+    regexBase regexPcre snapCore snapServer tagsoup time
   ];
   meta = {
     homepage = "http://jaspervdj.be/hakyll";
