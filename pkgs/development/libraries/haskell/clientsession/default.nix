23d4fe544f9d332886fb7eb03357d74e6335735b
Peter Simons <simons@cryp.to>
2011-11-27 12:48:44 +0000

    haskell-clientsession: updated to version 0.7.3.4
    svn path=/nixpkgs/trunk/; revision=30577

--- pkgs/development/libraries/haskell/clientsession/default.nix
+++ pkgs/development/libraries/haskell/clientsession/default.nix
@@ -1,12 +1,13 @@
-{ cabal, base64Bytestring, cereal, cryptoApi, cryptocipher, skein
+{ cabal, base64Bytestring, cereal, cryptoApi, cryptocipher, entropy
+, skein
 }:
 
 cabal.mkDerivation (self: {
   pname = "clientsession";
-  version = "0.7.3.3";
-  sha256 = "0cfj225hzn8fsffwnq5zq55dh9m5av1i58b4njhna7miiz6b4jsq";
+  version = "0.7.3.4";
+  sha256 = "16blm8p0c2hk06yn5f5qmrrxwsrsdv7l7x1s07ygn8s9jmb9xyqr";
   buildDepends = [
-    base64Bytestring cereal cryptoApi cryptocipher skein
+    base64Bytestring cereal cryptoApi cryptocipher entropy skein
   ];
   meta = {
     homepage = "http://github.com/snoyberg/clientsession/tree/master";
