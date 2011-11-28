f5ac7019cc89ec987accc0b7189456e9f307bff0
Peter Simons <simons@cryp.to>
2011-11-28 18:12:21 +0000

    haskell-yesod-json: updated to version 0.2.2
    svn path=/nixpkgs/trunk/; revision=30604

--- pkgs/development/libraries/haskell/yesod-json/default.nix
+++ pkgs/development/libraries/haskell/yesod-json/default.nix
@@ -1,10 +1,10 @@
-{ cabal, aesonNative, shakespeareJs, text, vector, yesodCore }:
+{ cabal, aeson, shakespeareJs, text, vector, yesodCore }:
 
 cabal.mkDerivation (self: {
   pname = "yesod-json";
-  version = "0.2.1";
-  sha256 = "1d710pqrdafyz8s0spd19vwvx5v9kwy44wb0byz3445jhi3kwn88";
-  buildDepends = [ aesonNative shakespeareJs text vector yesodCore ];
+  version = "0.2.2";
+  sha256 = "11n34wg0jjamghd93ild48rzganbwzbqf6shv8lyy1lsm2jl8q0v";
+  buildDepends = [ aeson shakespeareJs text vector yesodCore ];
   meta = {
     homepage = "http://www.yesodweb.com/";
     description = "Generate content for Yesod using the aeson package";
