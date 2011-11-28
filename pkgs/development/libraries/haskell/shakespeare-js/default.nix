e60933dbf0d25b3fbef52840c1f71866d920b562
Peter Simons <simons@cryp.to>
2011-11-28 18:11:54 +0000

    haskell-shakespeare-js: updated to version 0.10.3
    svn path=/nixpkgs/trunk/; revision=30595

--- pkgs/development/libraries/haskell/shakespeare-js/default.nix
+++ pkgs/development/libraries/haskell/shakespeare-js/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "shakespeare-js";
-  version = "0.10.2";
-  sha256 = "1yyqfp2lj8vq7x2a74wrgm7kx6zp1djp67bls9ip74kxi24dbznv";
+  version = "0.10.3";
+  sha256 = "1xz4jf91dfz60dji31d3hyy1k01hk0v41pb8qmi598jylk9iz1r2";
   buildDepends = [ shakespeare text ];
   meta = {
     homepage = "http://www.yesodweb.com/book/templates";
