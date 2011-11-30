e3323c43111cda7810c9a6df804c8995a9d15e5c
Ludovic Courtès <ludo@gnu.org>
2011-11-30 11:19:07 +0000

    ISL 0.08, with some simplifications.
    svn path=/nixpkgs/trunk/; revision=30632

--- pkgs/development/libraries/isl/default.nix
+++ pkgs/development/libraries/isl/default.nix
@@ -1,31 +1,24 @@
 {stdenv, fetchurl, gmp, static ? false}:
 
-let 
-  version = "0.06";
-  staticFlags = if static then " --enable-static --disable-shared" else "";
-in
-
 stdenv.mkDerivation rec {
-  name = "isl-${version}";
-  
+  name = "isl-0.08";
+
   src = fetchurl {
-    url = "http://www.kotnet.org/~skimo/isl/isl-${version}.tar.bz2";
-    sha256 = "0w1i1m94w0jkmm0bzlp08c4r97j7yp0d7crxf28524b9mgbg0mwk";
+    url = "http://www.kotnet.org/~skimo/isl/${name}.tar.bz2";
+    sha256 = "16rqvajcp9x6j76mg9q6bprqkgsm1zprx50j90s6v996y7ww3j9l";
   };
 
   buildInputs = [ gmp ];
 
-  dontDisableStatic = if static then true else false;
-  configureFlags = "--with-gmp-prefix=${gmp}" + staticFlags;
-  crossAttrs = {
-    configureFlags = configureFlags + " --with-gmp-prefix=${gmp.hostDrv} ";
-  };
+  dontDisableStatic = static;
+  configureFlags =
+    stdenv.lib.optionals static [ " --enable-static" "--disable-shared" ];
 
   meta = {
     homepage = http://www.kotnet.org/~skimo/isl/;
     license = "LGPLv2.1";
     description = "A library for manipulating sets and relations of integer points bounded by linear constraints.";
     maintainers = [ stdenv.lib.maintainers.shlevy ];
+    platforms = stdenv.lib.platforms.all;
   };
 }
-
