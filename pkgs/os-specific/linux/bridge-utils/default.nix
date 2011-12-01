d7cac3194e9677d8da27298b13aea0cd69bcc0c8
Peter Simons <simons@cryp.to>
2011-12-01 11:26:49 +0000

    bridge-utils: updated to version 1.5
    svn path=/nixpkgs/trunk/; revision=30643

--- pkgs/os-specific/linux/bridge-utils/default.nix
+++ pkgs/os-specific/linux/bridge-utils/default.nix
@@ -1,18 +1,21 @@
 { stdenv, fetchurl, autoconf, automake }:
 
-stdenv.mkDerivation rec {
-  name = "bridge-utils-1.4";
+let
+  name = "bridge-utils-1.5";
+in
+stdenv.mkDerivation {
+  inherit name;
 
   src = fetchurl {
     url = "mirror://sourceforge/bridge/${name}.tar.gz";
-    sha256 = "0csrvpjx1n5fzscdrc0xky3rnaxi90rylqciha5sl0n3pklpasc7";
+    sha256 = "42f9e5fb8f6c52e63a98a43b81bd281c227c529f194913e1c51ec48a393b6688";
   };
 
   buildInputs = [ autoconf automake ];
 
   preConfigure = "autoreconf";
 
-  meta = { 
+  meta = {
     description = "http://sourceforge.net/projects/bridge/";
     homepage = [ "http://www.linux-foundation.org/en/Net:Bridge/" "http://sourceforge.net/projects/bridge/" ];
     license = "GPL";
