787d6926672f3a957af9a47811900f36a8fa6e51
Peter Simons <simons@cryp.to>
2011-12-02 11:46:51 +0000

    haskell-attoparsec: updated to version 0.10.0.3
    svn path=/nixpkgs/trunk/; revision=30687

--- pkgs/development/libraries/haskell/attoparsec/default.nix
+++ pkgs/development/libraries/haskell/attoparsec/default.nix
@@ -1,12 +1,12 @@
-{ cabal, deepseq }:
+{ cabal, deepseq, text }:
 
 cabal.mkDerivation (self: {
   pname = "attoparsec";
-  version = "0.9.1.2";
-  sha256 = "0h9j4gn376k6j3v9l6pk7a4vxabkwk80043x6xlyxsh8p77jgj3v";
-  buildDepends = [ deepseq ];
+  version = "0.10.0.3";
+  sha256 = "0qlmjv8fhbx0xk8vkhlm01qmqlbk7xl98vfhcnlcjjrc5wkj0pjc";
+  buildDepends = [ deepseq text ];
   meta = {
-    homepage = "https://bitbucket.org/bos/attoparsec";
+    homepage = "https://github.com/bos/attoparsec";
     description = "Fast combinator parsing for bytestrings";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
