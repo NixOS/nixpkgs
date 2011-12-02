7bbeed560c3c9c3074cedb261fab02998f45e5d5
Peter Simons <simons@cryp.to>
2011-12-02 11:47:42 +0000

    haskell-xss-sanitize: updated to version 0.3.1
    svn path=/nixpkgs/trunk/; revision=30698

--- pkgs/development/libraries/haskell/xss-sanitize/default.nix
+++ pkgs/development/libraries/haskell/xss-sanitize/default.nix
@@ -1,13 +1,11 @@
-{ cabal, attoparsecText, cssText, network, tagsoup, text
-, utf8String
-}:
+{ cabal, attoparsec, cssText, network, tagsoup, text, utf8String }:
 
 cabal.mkDerivation (self: {
   pname = "xss-sanitize";
-  version = "0.3.0.1";
-  sha256 = "1rycdjl7b7bk100vgdwy6iighdqsbsyvrklp0zqbl1x45abph9pc";
+  version = "0.3.1";
+  sha256 = "0s8nqqx5f5b07xxlda4gh0w6vmlkhbqbz36cf6glhbhhyw27jkx5";
   buildDepends = [
-    attoparsecText cssText network tagsoup text utf8String
+    attoparsec cssText network tagsoup text utf8String
   ];
   meta = {
     homepage = "http://github.com/gregwebs/haskell-xss-sanitize";
