72e5c8176e7a1ac2e9d2094ec21e90af225fe5bf
Peter Simons <simons@cryp.to>
2011-12-03 16:11:57 +0000

    rsync: stripped trailing whitespace
    svn path=/nixpkgs/trunk/; revision=30715

--- pkgs/applications/networking/sync/rsync/default.nix
+++ pkgs/applications/networking/sync/rsync/default.nix
@@ -6,7 +6,7 @@ assert enableACLs -> acl != null;
 
 stdenv.mkDerivation {
   name = "rsync-3.0.9";
-  
+
   src = fetchurl {
     url = http://rsync.samba.org/ftp/rsync/src/rsync-3.0.9.tar.gz;
     sha256 = "01bw4klqsrlhh3i9lazd485sd9qx5djvnwa21lj2h3a9sn6hzw9h";
