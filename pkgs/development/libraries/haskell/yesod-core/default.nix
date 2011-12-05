36c3aabd5690d6d65441678de11dfe90bd235e84
Peter Simons <simons@cryp.to>
2011-12-05 16:00:10 +0000

    haskell-yesod-core: updated to version 0.9.3.5
    svn path=/nixpkgs/trunk/; revision=30755

--- pkgs/development/libraries/haskell/yesod-core/default.nix
+++ pkgs/development/libraries/haskell/yesod-core/default.nix
@@ -2,20 +2,20 @@
 , clientsession, cookie, dataObject, dataObjectYaml, enumerator
 , failure, hamlet, httpTypes, monadControl, parsec, pathPieces
 , random, shakespeare, shakespeareCss, shakespeareJs
-, strictConcurrency, text, time, transformers, vector, wai
-, waiExtra
+, strictConcurrency, text, time, transformers, transformersBase
+, vector, wai, waiExtra
 }:
 
 cabal.mkDerivation (self: {
   pname = "yesod-core";
-  version = "0.9.3.4";
-  sha256 = "1hnzmbspdj15480n0r4r1gayapwwz3d5r3hzj5k4qd9975lyxcw1";
+  version = "0.9.3.5";
+  sha256 = "0gnn0lm52qk0lmjh51kvwf645icfdrvy0ck5kg9dpznk5i3n2g13";
   buildDepends = [
     aeson blazeBuilder blazeHtml caseInsensitive cereal clientsession
     cookie dataObject dataObjectYaml enumerator failure hamlet
     httpTypes monadControl parsec pathPieces random shakespeare
     shakespeareCss shakespeareJs strictConcurrency text time
-    transformers vector wai waiExtra
+    transformers transformersBase vector wai waiExtra
   ];
   meta = {
     homepage = "http://www.yesodweb.com/";
