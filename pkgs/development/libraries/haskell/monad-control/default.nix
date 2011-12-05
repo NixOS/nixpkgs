1a479e8c02a88fb76725e46fa2b5f8ff66e27026
Peter Simons <simons@cryp.to>
2011-12-05 15:59:51 +0000

    haskell-monad-control: updated to version 0.3.0.1
    svn path=/nixpkgs/trunk/; revision=30748

--- pkgs/development/libraries/haskell/monad-control/default.nix
+++ pkgs/development/libraries/haskell/monad-control/default.nix
@@ -1,12 +1,14 @@
-{ cabal, baseUnicodeSymbols, transformers }:
+{ cabal, baseUnicodeSymbols, transformers, transformersBase }:
 
 cabal.mkDerivation (self: {
   pname = "monad-control";
-  version = "0.2.0.3";
-  sha256 = "0z7wjilrx6phqs2gxwv65dy1n3mc0j8hj3adshkwy6z8ggj283nh";
-  buildDepends = [ baseUnicodeSymbols transformers ];
+  version = "0.3.0.1";
+  sha256 = "0aa73cw82yssias0a9y237h9r4v4abvs0lv2lmhxaa4a4mdrj3ms";
+  buildDepends = [
+    baseUnicodeSymbols transformers transformersBase
+  ];
   meta = {
-    homepage = "https://github.com/basvandijk/monad-control/";
+    homepage = "https://github.com/basvandijk/monad-control";
     description = "Lift control operations, like exception catching, through monad transformers";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
