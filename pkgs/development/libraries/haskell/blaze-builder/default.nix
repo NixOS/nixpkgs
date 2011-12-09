a0504e732f1b9b85163039c726693d858c435afd
Peter Simons <simons@cryp.to>
2011-12-09 11:51:16 +0000

    haskell-blaze-builder: updated to version 0.3.0.2
    svn path=/nixpkgs/trunk/; revision=30806

--- pkgs/development/libraries/haskell/blaze-builder/default.nix
+++ pkgs/development/libraries/haskell/blaze-builder/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "blaze-builder";
-  version = "0.3.0.1";
-  sha256 = "1p3xlifcr7v987zx8l2sppn9yydph332mn1xxk0yfi78a6386nfb";
+  version = "0.3.0.2";
+  sha256 = "009njfm9g0fs7kg6q9c7qqfl69msw9q3d07xh3xg7layh09khsdb";
   buildDepends = [ text ];
   meta = {
     homepage = "http://github.com/meiersi/blaze-builder";
