50fd91efa92a047ae2474fb94284cc4db2b8c13a
Peter Simons <simons@cryp.to>
2011-12-10 22:30:53 +0000

    haskell-hakyll: updated to version 3.2.3.2
    svn path=/nixpkgs/trunk/; revision=30824

--- pkgs/development/libraries/haskell/hakyll/default.nix
+++ pkgs/development/libraries/haskell/hakyll/default.nix
@@ -5,8 +5,8 @@
 
 cabal.mkDerivation (self: {
   pname = "hakyll";
-  version = "3.2.3.1";
-  sha256 = "1mmzi8d5a3k4s81h5ciis32g8khxvpafps6pql65lvpp9yhicxdz";
+  version = "3.2.3.2";
+  sha256 = "0410gg8sdnw0iyhqrw1wfv9abbrv7r6awgvlhqds17vnrdxxk2w8";
   buildDepends = [
     binary blazeHtml citeprocHs cryptohash hamlet mtl pandoc parsec
     regexBase regexPcre snapCore snapServer tagsoup time
