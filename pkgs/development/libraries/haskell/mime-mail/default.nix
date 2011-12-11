88b400f927252bd6322aa33c4a628b5072b45757
Peter Simons <simons@cryp.to>
2011-12-11 18:01:07 +0000

    haskell-mime-mail: updated to version 0.4.1.0
    svn path=/nixpkgs/trunk/; revision=30830

--- pkgs/development/libraries/haskell/mime-mail/default.nix
+++ pkgs/development/libraries/haskell/mime-mail/default.nix
@@ -1,10 +1,10 @@
-{ cabal, blazeBuilder, dataenc, random, text }:
+{ cabal, base64Bytestring, blazeBuilder, random, text }:
 
 cabal.mkDerivation (self: {
   pname = "mime-mail";
-  version = "0.4";
-  sha256 = "0hlyk3mypn9iw7g8hhn528ycbm3qayiczwf4paw0sxd6xsg9a28s";
-  buildDepends = [ blazeBuilder dataenc random text ];
+  version = "0.4.1.0";
+  sha256 = "1czjxxpv2w8zvwm6nkv0rr47rdl84s5f5xvf3r4kjaw44a6jfgg0";
+  buildDepends = [ base64Bytestring blazeBuilder random text ];
   meta = {
     homepage = "http://github.com/snoyberg/mime-mail";
     description = "Compose MIME email messages";
