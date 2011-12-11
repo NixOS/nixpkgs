c561cbe8c874f794cba04d5c2805497b9ecf05ed
Peter Simons <simons@cryp.to>
2011-12-11 19:39:06 +0000

    upgraded mc
    svn path=/nixpkgs/trunk/; revision=30838

--- pkgs/tools/misc/mc/default.nix
+++ pkgs/tools/misc/mc/default.nix
@@ -2,11 +2,11 @@
 , libX11, libICE, perl, zip, unzip, gettext, slang}:
 
 stdenv.mkDerivation rec {
-  name = "mc-4.7.5.3";
+  name = "mc-4.8.0";
   
   src = fetchurl {
-    url = http://www.midnight-commander.org/downloads/mc-4.7.5.3.tar.bz2;
-    sha256 = "1di8fsdg6y98iq3846j145qjk4bf5rgjj2hxnxdbcwfp8p3v8x88";
+    url = http://www.midnight-commander.org/downloads/mc-4.8.0.tar.bz2;
+    sha256 = "dbf077b318c13fc6d465dc67bd43958f067b9ff7e21041975bd14927dfa31b52";
   };
   
   buildInputs = [ pkgconfig perl glib gpm slang zip unzip file gettext libX11 libICE e2fsprogs ];
