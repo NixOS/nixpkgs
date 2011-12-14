5c507048ddc836585961e4fc175fc531a701196a
Yury G. Kudryashov <urkud.urkud@gmail.com>
2011-12-14 18:22:48 +0000

    Review dbus-glib expression
    * Move pkgconfig and gettext to buildNativeInputs
    * Add deprecation warning
    
    svn path=/nixpkgs/branches/stdenv-updates/; revision=30894

--- pkgs/development/libraries/dbus-glib/default.nix
+++ pkgs/development/libraries/dbus-glib/default.nix
@@ -7,17 +7,19 @@ stdenv.mkDerivation rec {
     url = "${meta.homepage}/releases/dbus-glib/${name}.tar.gz";
     sha256 = "04fiwld5yaxyggxlvdmbaqkngh4fn8gfkkqckcp3274bpgb82z19";
   };
-  
-  buildInputs = [ pkgconfig expat gettext ]
-                ++ stdenv.lib.optional (!stdenv.isLinux) libiconv;
-  
-  propagatedBuildInputs = [ dbus.libs glib ];
-  
+
+  buildNativeInputs = [ pkgconfig gettext ];
+
+  buildInputs = [ expat ] ++ stdenv.lib.optional (!stdenv.isLinux) libiconv;
+
+  propagatedBuildInputs = [ dbus glib ];
+
   passthru = { inherit dbus glib; };
 
   meta = {
     homepage = http://dbus.freedesktop.org;
     license = "AFL-2.1 or GPL-2";
-    description = "GLib bindings for D-Bus lightweight IPC mechanism";
+    description = "Obsolete glib bindings for D-Bus lightweight IPC mechanism";
+    maintainers = [ stdenv.lib.maintainers.urkud ];
   };
 }
