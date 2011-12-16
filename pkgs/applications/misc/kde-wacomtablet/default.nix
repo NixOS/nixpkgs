1e74801dc454471e5142c0e2bad4b6168aa7fa91
Yury G. Kudryashov <urkud.urkud@gmail.com>
2011-12-16 22:33:28 +0000

    wacomtablet-1.3.5
    svn path=/nixpkgs/trunk/; revision=30946

--- pkgs/applications/misc/kde-wacomtablet/default.nix
+++ pkgs/applications/misc/kde-wacomtablet/default.nix
@@ -1,18 +1,20 @@
-{ stdenv, fetchurl, automoc4, cmake, kdelibs }:
+{ stdenv, fetchurl, kdelibs, gettext }:
 
 stdenv.mkDerivation rec {
-  name = "wacomtablet-1.2.5";
+  name = "wacomtablet-1.3.5";
 
   src = fetchurl {
-    url = "http://kde-apps.org/CONTENT/content-files/114856-${name}.tar.gz";
-    sha256 = "11hfab6sqmhvd0m1grc9m9yfi0p7rk0bycj9wqgkgbc8cwgps6sf";
+    url = "http://kde-apps.org/CONTENT/content-files/114856-wacomtablet-v1.3.5.tar.bz2";
+    sha256 = "0dgsp3izx2v44f6j8mhxc6zybjn5sj9038w6b4v2fgix47fri0ja";
   };
 
-  buildInputs = [ automoc4 cmake kdelibs ];
+  buildInputs = [ kdelibs ];
+
+  buildNativeInputs = [ gettext ];
 
   meta = with stdenv.lib; {
     description = "KDE Wacom graphic tablet configuration tool";
-    license = "GPLv2";
+    license = licenses.gpl2Plus;
     homepage = http://kde-apps.org/content/show.php/wacom+tablet?content=114856;
     platforms = platforms.linux;
     maintainers = [ maintainers.goibhniu maintainers.urkud ];
