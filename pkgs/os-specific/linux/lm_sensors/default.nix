5f0536dc27d3cbb86306321be950623906965d4c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-12-21 10:28:09 +0000

    * lm-sensors updated to 3.3.1.  Also fixed the name to match upstream.
    svn path=/nixpkgs/trunk/; revision=30996

--- pkgs/os-specific/linux/lm_sensors/default.nix
+++ pkgs/os-specific/linux/lm_sensors/default.nix
@@ -1,14 +1,16 @@
-{stdenv, fetchurl, bison, flex, perl}:
+{ stdenv, fetchurl, bison, flex, which, perl }:
+
+let version = "3.3.1"; in
 
 stdenv.mkDerivation rec {
-  name = "lm_sensors-3.1.1";
+  name = "lm-sensors-3.3.1";
   
   src = fetchurl {
-    url = "http://dl.lm-sensors.org/lm-sensors/releases/${name}.tar.bz2";
-    sha256 = "1vsrs2cl35h2gry03lp0pwbzpw0nbpsbpds5w4mdmx16clm3ynnr";
+    url = "http://dl.lm-sensors.org/lm-sensors/releases/lm_sensors-${version}.tar.bz2";
+    sha256 = "13v2gszagmx8hwjyzh2k47rdpc2kyg9zky3kdqhdbgzp8lwpik6g";
   };
 
-  buildInputs = [bison flex perl];
+  buildInputs = [ bison flex which perl ];
 
   preBuild = ''
     makeFlagsArray=(PREFIX=$out ETCDIR=$out/etc)
