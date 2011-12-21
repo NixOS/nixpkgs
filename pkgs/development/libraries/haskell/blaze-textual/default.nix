e368ea1e8562e095acc7fc7996a77a0bbef3c51a
Peter Simons <simons@cryp.to>
2011-12-21 10:49:17 +0000

    haskell-blaze-textual: updated to version 0.2.0.6
    svn path=/nixpkgs/trunk/; revision=31004

--- pkgs/development/libraries/haskell/blaze-textual/default.nix
+++ pkgs/development/libraries/haskell/blaze-textual/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "blaze-textual";
-  version = "0.2.0.5";
-  sha256 = "0rl41idjmrw227yi2x6nk2rlm93rgzz2y7jvz2yvmya2ihrppzjf";
+  version = "0.2.0.6";
+  sha256 = "1699fj9zig6ids9bdjn5v0gqhnyx5dkzi542gkx1gs8943c94737";
   buildDepends = [ blazeBuilder text time vector ];
   meta = {
     homepage = "http://github.com/bos/blaze-textual";
