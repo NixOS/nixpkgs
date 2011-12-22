2f02b3d8e053875f600840e78b9498dcaf156786
Peter Simons <simons@cryp.to>
2011-12-22 12:19:14 +0000

    haskell-unordered-containers: updated to version 0.1.4.6
    svn path=/nixpkgs/trunk/; revision=31037

--- pkgs/development/libraries/haskell/unordered-containers/default.nix
+++ pkgs/development/libraries/haskell/unordered-containers/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "unordered-containers";
-  version = "0.1.4.5";
-  sha256 = "065701zdbxpn70zl142jxmv30m0162mg3z95rvpfa593ggq688m4";
+  version = "0.1.4.6";
+  sha256 = "1azwxbrzlzaw54idp3z2xx1xlywzsf1r893blbz51nnwcj9v550d";
   buildDepends = [ deepseq hashable ];
   meta = {
     description = "Efficient hashing-based container types";
