1b722dcc80d121953be1e453cc5eaeb1256857eb
Peter Simons <simons@cryp.to>
2011-12-25 16:06:28 +0000

    haskell-repr: updated to version 0.4.1.2
    svn path=/nixpkgs/trunk/; revision=31073

--- pkgs/development/libraries/haskell/repr/default.nix
+++ pkgs/development/libraries/haskell/repr/default.nix
@@ -12,5 +12,9 @@ cabal.mkDerivation (self: {
     description = "Render overloaded expressions to their textual representation";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.andres
+      self.stdenv.lib.maintainers.simons
+    ];
   };
 })
