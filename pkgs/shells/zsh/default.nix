ecba799e780734eaf9087fc7444ae554d7f59e46
Florian Friesdorf <flo@chaoflow.net>
2011-12-29 19:46:43 +0000

    zsh meta
    svn path=/nixpkgs/trunk/; revision=31147

--- pkgs/shells/zsh/default.nix
+++ pkgs/shells/zsh/default.nix
@@ -26,5 +26,14 @@ stdenv.mkDerivation {
     tar xf ${documentation} -C $out/share
   '';
 
-  buildInputs = [ncurses coreutils];
+  buildInputs = [ ncurses coreutils ];
+
+  meta = {
+    description = "the Z shell";
+    longDescription = "Zsh is a UNIX command interpreter (shell) usable as an interactive login shell and as a shell script command processor.  Of the standard shells, zsh most closely resembles ksh but includes many enhancements.  Zsh has command line editing, builtin spelling correction, programmable command completion, shell functions (with autoloading), a history mechanism, and a host of other features.";
+    license = "MIT-like";
+    homePage = "http://www.zsh.org/";
+    maintainers = with stdenv.lib.maintainers; [ chaoflow ];
+    platforms = stdenv.lib.platforms.gnu;  # arbitrary choice
+  };
 }
