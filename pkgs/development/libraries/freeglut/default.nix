2c9461ebddfb19633c1266aed9e2cb6cb47e239e
Florian Friesdorf <flo@chaoflow.net>
2012-01-04 23:05:41 +0000

    freeglut-2.8.0
    svn path=/nixpkgs/trunk/; revision=31304

--- pkgs/development/libraries/freeglut/default.nix
+++ pkgs/development/libraries/freeglut/default.nix
@@ -1,15 +1,15 @@
-{stdenv, fetchurl, x11, mesa}:
+{ stdenv, fetchurl, libXi, libXrandr, libXxf86vm, mesa, x11 }:
 
 stdenv.mkDerivation {
-  name = "freeglut-2.4.0";
+  name = "freeglut-2.8.0";
 
   src = fetchurl {
-    url = mirror://sourceforge/freeglut/freeglut-2.4.0.tar.gz;
-    sha256 = "0lmhh5p19rw4wisr0jsl7nsa2hxdaasj0vxk5ri83crhp982v7r6";
+    url = mirror://sourceforge/freeglut/freeglut-2.8.0.tar.gz;
+    sha256 = "197293ff886abe613bc9eb4a762d9161b0c9e64b3e8e613ed7c5e353974fba05";
   };
 
   configureFlags = "--" + (if stdenv.isDarwin then "disable" else "enable") + "-warnings";
 
-  buildInputs = [x11 mesa];
-  patches = ./freeglut-gcc-4.2.patch;
+  buildInputs = [ libXi libXrandr libXxf86vm mesa x11 ];
+  patches = [ ./0001-remove-typedefs-now-living-in-mesa.patch ];
 }
