d0321537d93b896a8a893ea079b739835285d521
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2012-01-04 22:05:26 +0000

    Updating dwm to 6.0.
    svn path=/nixpkgs/trunk/; revision=31301

--- pkgs/applications/window-managers/dwm/default.nix
+++ pkgs/applications/window-managers/dwm/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, libX11, libXinerama, patches ? []}:
 
 stdenv.mkDerivation rec {
-  name = "dwm-5.9";
+  name = "dwm-6.0";
  
   src = fetchurl {
     url = "http://dl.suckless.org/dwm/${name}.tar.gz";
-    sha256 = "0cp25zqgaqj5k1mlvgxnc5jqi252chqjc5v0fzpqbhrklaidbk9d";
+    sha256 = "0mpbivy9j80l1jqq4bd4g4z8s5c54fxrjj44avmfwncjwqylifdj";
   };
  
   buildInputs = [ libX11 libXinerama ];
@@ -19,8 +19,8 @@ stdenv.mkDerivation rec {
  
   meta = {
     homepage = "www.suckless.org";
-    description = "dynamic window manager for X";
-    license="MIT";
+    description = "Dynamic window manager for X";
+    license = "MIT";
     maintainers = with stdenv.lib.maintainers; [viric];
     platforms = with stdenv.lib.platforms; all;
   };
