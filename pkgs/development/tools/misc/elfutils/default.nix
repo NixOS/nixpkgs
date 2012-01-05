b00d19d77593120f7dbe5378c821a6eb46432495
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-05 23:15:48 +0000

    elfutils-0.152, pass zlib, bzip2 and xz
    svn path=/nixpkgs/trunk/; revision=31323

--- pkgs/development/tools/misc/elfutils/default.nix
+++ pkgs/development/tools/misc/elfutils/default.nix
@@ -1,17 +1,21 @@
-{stdenv, fetchurl, m4}:
+{stdenv, fetchurl, zlib, bzip2, xz, m4}:
 
+# TODO: Look at the hardcoded paths to kernel, modules etc.
 stdenv.mkDerivation rec {
-  name = "elfutils-0.148";
+  name = "elfutils-${version}";
+  version = "0.152";
   
   src = fetchurl {
     urls = [
-      "https://fedorahosted.org/releases/e/l/elfutils/0.143/${name}.tar.bz2"
+      "https://fedorahosted.org/releases/e/l/elfutils/${version}/${name}.tar.bz2"
       "mirror://gentoo/distfiles/${name}.tar.bz2"
       ];
-    sha256 = "1nl7x2gidd2i048yjlvyzhpbca9793z435cw8bsircjxfi5gmswa";
+    sha256 = "19mlgxyzcwiv64ynj2cibgkiw4qkm3n37kizvy6555dsmlaqfybq";
   };
 
-  buildInputs = [m4];
+  buildInputs = [ zlib bzip2 xz ];
+
+  buildNativeInputs = [m4];
   
   dontAddDisableDepTrack = true;
 
