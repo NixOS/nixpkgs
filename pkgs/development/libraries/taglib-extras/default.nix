ba80f4337f3d7310b89b9505d080b4e68bdfacd6
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-05 23:43:22 +0000

    taglib{,-extras}: use buildNativeInputs, meta
    svn path=/nixpkgs/trunk/; revision=31329

--- pkgs/development/libraries/taglib-extras/default.nix
+++ pkgs/development/libraries/taglib-extras/default.nix
@@ -1,10 +1,11 @@
 {stdenv, fetchurl, cmake, taglib}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "taglib-extras-1.0.1";
   src = fetchurl {
-    url = http://www.kollide.net/~jefferai/taglib-extras-1.0.1.tar.gz;
+    url = "http://www.kollide.net/~jefferai/${name}.tar.gz";
     sha256 = "0cln49ws9svvvals5fzxjxlzqm0fzjfymn7yfp4jfcjz655nnm7y";
   };
-  buildInputs = [ cmake taglib ];
+  buildInputs = [ taglib ];
+  buildNativeInputs = [ cmake ];
 }
--- pkgs/development/libraries/taglib/default.nix
+++ pkgs/development/libraries/taglib/default.nix
@@ -1,19 +1,22 @@
 {stdenv, fetchurl, zlib, cmake}:
 
 stdenv.mkDerivation rec {
-  name = "taglib-1.7.0";
+  name = "taglib-1.7";
   
   src = fetchurl {
-    url = "https://github.com/downloads/taglib/taglib/taglib-1.7.tar.gz";
+    url = "http://developer.kde.org/~wheeler/files/src/${name}.tar.gz";
     sha256 = "0gvpmfrrh4wgdpyc14zq9mk3hivp8kbmfdxjk8bi2nf3py6zpph9";
   };
   
   cmakeFlags = "-DWITH_ASF=ON -DWITH_MP4=ON";
 
-  buildInputs = [zlib cmake];
+  buildInputs = [zlib];
+  buildNativeInputs = [cmake];
 
   meta = {
     homepage = http://developer.kde.org/~wheeler/taglib.html;
     description = "A library for reading and editing the meta-data of several popular audio formats";
+    inherit (cmake.meta) platforms;
+    maintainers = [ stdenv.lib.maintainers.urkud ];
   };
 }
