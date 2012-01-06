09060a0a19512a43be98aa5a0e1b58128181339e
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-06 18:12:38 +0000

    Add gnome3.{glib_networking,gsettings_desktop_schemas}
    Cherry-pick from stdenv-updates
    
    svn path=/nixpkgs/trunk/; revision=31356

--- pkgs/desktops/gnome-3/default.nix
+++ pkgs/desktops/gnome-3/default.nix
@@ -4,5 +4,9 @@
   # Ensure that we use dbus-glib built with gtkLibs3x.glib
   dbus_glib = callPackage ../../development/libraries/dbus-glib { };
 
+  glib_networking = callPackage ./platform/glib-networking.nix {};
+
+  gsettings_desktop_schemas = callPackage ./platform/gsettings-desktop-schemas.nix {};
+
   GConf = callPackage ./platform/GConf.nix { };
 }
---
+++ pkgs/desktops/gnome-3/platform/gsettings-desktop-schemas.nix
@@ -0,0 +1,13 @@
+{ stdenv, fetchurl, xz, glib, pkgconfig, intltool }:
+
+stdenv.mkDerivation rec {
+  name = "gsettings-desktop-schemas-3.2.0";
+
+  src = fetchurl {
+    url = mirror://gnome/sources/gsettings-desktop-schemas/3.2/gsettings-desktop-schemas-3.2.0.tar.xz;
+    sha256 = "0772axkd1nlf3j1lcg0zi5x5jh4zmr25k98dhn7pzppahljaj3hi";
+  };
+
+  buildInputs = [ glib ];
+  buildNativeInputs = [ pkgconfig xz intltool ];
+}
