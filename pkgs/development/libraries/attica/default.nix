dc9bf79b545d97216739e37aa2e02025d2f80019
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-06 00:55:48 +0000

    attica-0.3.0
    svn path=/nixpkgs/trunk/; revision=31336

--- pkgs/development/libraries/attica/default.nix
+++ pkgs/development/libraries/attica/default.nix
@@ -1,19 +1,20 @@
 { stdenv, fetchurl, cmake, qt4 }:
 
 stdenv.mkDerivation rec {
-  name = "attica-0.2.0";
+  name = "attica-0.3.0";
   
   src = fetchurl {
     url = "mirror://kde/stable/attica/${name}.tar.bz2";
-    sha256 = "0g2la91fgdr185ah15vc91plvdwvbm6kpsyz0vk0da7ggiyg3y9a";
+    sha256 = "1wfd37mvskn77ppzjdh3x6cb5p9na81ibh05ardfvbwqqn523gd0";
   };
   
-  buildInputs = [ cmake qt4 ];
+  buildInputs = [ qt4 ];
+  buildNativeInputs = [ cmake ];
   
   meta = with stdenv.lib; {
     description = "A library to access Open Collaboration Service providers";
     license = "LGPL";
     maintainers = [ maintainers.sander maintainers.urkud ];
-    platforms = qt4.meta.platforms;
+    inherit (qt4.meta) platforms;
   };
 }
