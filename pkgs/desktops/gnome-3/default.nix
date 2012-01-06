265b149db17d4a2b6a374f91832b955e41240c4c
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-06 18:13:19 +0000

    Add gnome3.libgnome_keyring
    svn path=/nixpkgs/trunk/; revision=31359

--- pkgs/desktops/gnome-3/default.nix
+++ pkgs/desktops/gnome-3/default.nix
@@ -4,7 +4,9 @@
   # Ensure that we use dbus-glib built with gtkLibs3x.glib
   dbus_glib = callPackage ../../development/libraries/dbus-glib { };
 
-  glib_networking = callPackage ./platform/glib-networking.nix {};
+  glib_networking = callPackage ./platform/glib-networking.nix { };
+
+  libgnome_keyring = callPackage ./platform/libgnome-keyring.nix { };
 
   gsettings_desktop_schemas = callPackage ./platform/gsettings-desktop-schemas.nix {};
 
---
+++ pkgs/desktops/gnome-3/platform/libgnome-keyring.nix
@@ -0,0 +1,14 @@
+{ stdenv, fetchurl, glib, dbus_libs, libgcrypt, pkgconfig, xz,
+intltool }:
+
+stdenv.mkDerivation rec {
+  name = "libgnome-keyring-3.2.2";
+
+  src = fetchurl {
+    url = mirror://gnome/sources/libgnome-keyring/3.2/libgnome-keyring-3.2.2.tar.xz;
+    sha256 = "1cxd2vb1lzm8smq1q45dsn13s6kdqdb60lashdk7hwv035xy9jrb";
+  };
+
+  propagatedBuildInputs = [ glib dbus_libs libgcrypt ];
+  buildNativeInputs = [ pkgconfig xz intltool ];
+}
