20973a9280836635d631aa985201254d33c88641
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-06 00:55:14 +0000

    GConf-2.28.1, policykit->polkit
    svn path=/nixpkgs/trunk/; revision=31333

--- pkgs/desktops/gnome-2/platform/GConf/default.nix
+++ pkgs/desktops/gnome-2/platform/GConf/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, pkgconfig, dbus_glib, glib, ORBit2, libxml2
-, policykit, intltool, dbus_libs }:
+, polkit, intltool, dbus_libs }:
 
 stdenv.mkDerivation {
-  name = "GConf-2.26.2";
-  
+  name = "GConf-2.28.1";
+
   src = fetchurl {
-    url = mirror://gnome/platform/2.26/2.26.2/sources/GConf-2.26.2.tar.bz2;
-    sha256 = "1vb7hjxddy54g4vch936621g66n0mhi3wkhm9lwqh449vdqg4yki";
+    url = mirror://gnome/sources/GConf/2.28/GConf-2.28.1.tar.bz2;
+    sha256 = "001h9gngz31gnvs6mjyazdibhdqmw1wwk88n934b0mv013wpgi2k";
   };
 
-  buildInputs = [ pkgconfig ORBit2 dbus_libs dbus_glib libxml2 policykit intltool ];
+  buildInputs = [ pkgconfig ORBit2 dbus_libs dbus_glib libxml2 polkit intltool ];
   propagatedBuildInputs = [ glib ];
 }
