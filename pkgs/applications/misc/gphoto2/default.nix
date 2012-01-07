3b672f9b7520294b1b0bf327316fcc5fe3bbfa2a
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-07 19:41:59 +0000

    (lib)gphoto-2.4.11
    svn path=/nixpkgs/trunk/; revision=31412

--- pkgs/applications/misc/gphoto2/default.nix
+++ pkgs/applications/misc/gphoto2/default.nix
@@ -3,18 +3,16 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "gphoto2-2.4.5";
+  name = "gphoto2-2.4.11";
   
   src = fetchurl {
     url = "mirror://sourceforge/gphoto/${name}.tar.bz2";
-    sha256 = "0bjbgz9n7ijf8167i1bm7q3pg366axgx5zydck13d2znhd30x069";
+    sha256 = "0ah42b7rnqh0z8bb96z7cdycxdh5k19h6lmfc02kdhrhqdr3q81y";
   };
   
-  buildInputs = [pkgconfig libgphoto2 libexif popt gettext libjpeg readline libtool];
+  buildNativeInputs = [ pkgconfig gettext ];
+  buildInputs = [ libgphoto2 libexif popt libjpeg readline libtool ];
   
-  # There is a bug in 2.4.0 configure.ac (in their m4 macroses)
-  #patchPhase = "sed -e 's@_tmp=true@_tmp=false@' -i configure configure.ac";
-
   meta = {
     homepage = http://www.gphoto.org/;
   };
