735de44b72f9b1f2f3310f211dc51b46efc2a7f9
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-07 00:10:22 +0000

    KDE SC 4.8: latest unstable akonadi
    svn path=/nixpkgs/trunk/; revision=31391

--- pkgs/desktops/kde-4.8/support/akonadi/default.nix
+++ pkgs/desktops/kde-4.8/support/akonadi/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, cmake, qt4, shared_mime_info, libxslt, boost, automoc4, soprano }:
 
 stdenv.mkDerivation rec {
-  name = "akonadi-1.6.1";
+  name = "akonadi-1.6.90";
   
   src = fetchurl {
     url = "mirror://kde/stable/akonadi/src/${name}.tar.bz2";
-    sha256 = "0r8sw7m1pwqc7qkaczm0r8adqi1wvlhdp32gy3q5p5plq50xhgra";
+    sha256 = "0a35wkrrdk4k7kw1d4rvq4w4wwlmz9vk2nb4c2yibpn9rmc6ighn";
   };
 
   buildInputs = [ qt4 soprano libxslt boost ];
