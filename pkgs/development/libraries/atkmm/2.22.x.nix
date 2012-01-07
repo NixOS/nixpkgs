4ae99c2e02df7843308f35b20e1aac0de0c14ee1
Karn Kallio <tierpluspluslists@gmail.com>
2012-01-07 13:31:06 +0000

    atkmm build was failing.
    svn path=/nixpkgs/trunk/; revision=31398

--- pkgs/development/libraries/atkmm/2.22.x.nix
+++ pkgs/development/libraries/atkmm/2.22.x.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, atk, glibmm, pkgconfig }:
+{ stdenv, fetchurl, atk, glibmm, pkgconfig, xz }:
 
 stdenv.mkDerivation rec {
   name = "atkmm-2.22.6";
@@ -8,6 +8,7 @@ stdenv.mkDerivation rec {
     sha256 = "1dmf72i7jv2a2gavjiah2722bf5qk3hb97hn5dasxqxr0r8jjx0a";
   };
 
+  buildInputs = [ xz ];
   propagatedBuildInputs = [ atk glibmm ];
 
   buildNativeInputs = [ pkgconfig ];
