f4a079a76c36459b62dea0da7daaa7ad55050345
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-09 10:25:56 +0000

    Upgrade xine-lib and xine-ui
    svn path=/nixpkgs/trunk/; revision=31451

--- pkgs/development/libraries/xine-lib/default.nix
+++ pkgs/development/libraries/xine-lib/default.nix
@@ -1,29 +1,30 @@
 { stdenv, fetchurl, pkgconfig, xorg, alsaLib, mesa, aalib
 , libvorbis, libtheora, speex, zlib, libdvdcss, perl, ffmpeg
-, flac, libcaca, pulseaudio, libmng
+, flac, libcaca, pulseaudio, libmng, libcdio, libv4l, vcdimager
+, libmpcdec
 }:
 
 stdenv.mkDerivation rec {
-  name = "xine-lib-1.1.19";
+  name = "xine-lib-1.2.0";
   
   src = fetchurl {
     url = "mirror://sourceforge/xine/${name}.tar.bz2";
-    sha256 = "0x47kmsaxx1bv8w2cacvzls3sjw9y4vk82rd94km1m1s6k2wcxv2";
+    sha256 = "1yss9cxxkcb6dzrv78xvi845ls6lhhbv6g8yfm6zjjl07v7jbm6c";
   };
 
-  patches =
-    [ (fetchurl {
-        url = "http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/media-libs/xine-lib/files/xine-lib-1.1.19-ffmpeg.patch?revision=1.1";
-        sha256 = "0dqr0kc829djfn0wvk4jg84v61pxynqbp4s4phvywd7x9caf092b";
-      })
-    ];
+#  patches =
+#    [ (fetchurl {
+#        url = "http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/media-libs/xine-lib/files/xine-lib-1.1.19-ffmpeg.patch?revision=1.1";
+#        sha256 = "0dqr0kc829djfn0wvk4jg84v61pxynqbp4s4phvywd7x9caf092b";
+#      })
+#    ];
   
   buildNativeInputs = [ pkgconfig perl ];
 
   buildInputs = [
     xorg.libX11 xorg.libXv xorg.libXinerama xorg.libxcb xorg.libXext
     alsaLib mesa aalib libvorbis libtheora speex perl ffmpeg flac
-    libcaca pulseaudio libmng
+    libcaca pulseaudio libmng libcdio libv4l vcdimager libmpcdec
   ];
 
   NIX_LDFLAGS = "-rpath ${libdvdcss}/lib -L${libdvdcss}/lib -ldvdcss";
