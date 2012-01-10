acf6a9c5e4f4b2f6c11812364e93fe9cd5d1ce1b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-01-10 23:49:24 +0000

    * Cleanup.
    svn path=/nixpkgs/trunk/; revision=31476

--- pkgs/development/interpreters/ruby/patches.nix
+++ pkgs/development/interpreters/ruby/patches.nix
@@ -1,46 +1,56 @@
-{fetchurl, writeScript, ruby, ncurses, sqlite, libxml2, libxslt, libffi, zlib,
-  libuuid, gems}:
+{ fetchurl, writeScript, ruby, ncurses, sqlite, libxml2, libxslt, libffi
+, zlib, libuuid, gems, jdk }:
 
 let
+
   patchUsrBinEnv = writeScript "path-usr-bin-env" ''
     #!/bin/sh
-    set -x
     echo "==================="
     find "$1" -type f -name "*.rb" | xargs sed -i "s@/usr/bin/env@$(type -p env)@g"
     find "$1" -type f -name "*.mk" | xargs sed -i "s@/usr/bin/env@$(type -p env)@g"
   '';
+  
 in
+
 {
   sup = { buildInputs = [ gems.ncursesw ]; };
+  
   sqlite3 = { propagatedBuildInputs = [ sqlite ]; };
+  
   rails = { gemFlags = "--no-ri --no-rdoc"; };
+  
   ncurses = { propagatedBuildInputs = [ ncurses ]; };
+  
   ncursesw = { propagatedBuildInputs = [ ncurses ]; };
+  
   nokogiri = {
-    buildFlags=["--with-xml2-dir=${libxml2} --with-xml2-include=${libxml2}/include/libxml2"
-                "--with-xslt-dir=${libxslt}" ];
+    buildFlags =
+      [ "--with-xml2-dir=${libxml2} --with-xml2-include=${libxml2}/include/libxml2"
+        "--with-xslt-dir=${libxslt}"
+      ];
   };
 
   ffi = {
     postUnpack = "onetuh";
-    buildFlags=["--with-ffi-dir=${libffi}"];
+    buildFlags = ["--with-ffi-dir=${libffi}"];
     NIX_POST_EXTRACT_FILES_HOOK = patchUsrBinEnv;
   };
 
   xrefresh_server =
-  let patch = fetchurl {
-      url = "http://mawercer.de/~nix/xrefresh.diff.gz";
-      sha256 = "1f7bnmn1pgkmkml0ms15m5lx880hq2sxy7vsddb3sbzm7n1yyicq";
-    };
-  in {
-    propagatedBuildInputs = [ gems.rb_inotify ];
+    let
+      patch = fetchurl {
+        url = "http://mawercer.de/~nix/xrefresh.diff.gz";
+        sha256 = "1f7bnmn1pgkmkml0ms15m5lx880hq2sxy7vsddb3sbzm7n1yyicq";
+      };
+    in {
+      propagatedBuildInputs = [ gems.rb_inotify ];
 
-    # monitor implementation for Linux
-    postInstall = ''
-      cd $out/${ruby.gemPath}/gems/*
-      zcat ${patch} | patch -p 1
-    '';
-  };
+      # monitor implementation for Linux
+      postInstall = ''
+        cd $out/${ruby.gemPath}/gems/*
+        zcat ${patch} | patch -p 1
+      ''; # */
+    };
 
   xapian_full = {
     buildInputs = [ gems.rake zlib libuuid ];
