c9fc73ae45c1ca5e17444857f5a8987acf6f0a32
Peter Simons <simons@cryp.to>
2012-01-12 15:21:11 +0000

    haskell-hxt-charproperties: updated to version 9.1.1
    svn path=/nixpkgs/trunk/; revision=31496

--- pkgs/development/libraries/haskell/hxt-charproperties/default.nix
+++ pkgs/development/libraries/haskell/hxt-charproperties/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "hxt-charproperties";
-  version = "9.1.0";
-  sha256 = "1a227czzbbw8pigc2dk5fyyc4x1rpx82mb5c4hpjjz97l0sdlc23";
+  version = "9.1.1";
+  sha256 = "14xv0q1hh0k8lgispc4fa49cvyg9s7936kp42vr9b0pyd1q4zid8";
   meta = {
     homepage = "http://www.fh-wedel.de/~si/HXmlToolbox/index.html";
     description = "Character properties and classes for XML and Unicode";
