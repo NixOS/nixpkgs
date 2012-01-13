e495d3345f39e00793d391f6b000d93c349a2fdb
Peter Simons <simons@cryp.to>
2012-01-13 10:32:06 +0000

    haskell-wx: updated to version 0.13.2
    svn path=/nixpkgs/trunk/; revision=31517

--- pkgs/development/libraries/haskell/wxHaskell/wx.nix
+++ pkgs/development/libraries/haskell/wxHaskell/wx.nix
@@ -2,13 +2,13 @@
 
 cabal.mkDerivation (self: {
   pname = "wx";
-  version = "0.12.1.6";
-  sha256 = "1p0gn46gk1abin095va22n9bycxhm2cq1vyvwiypcdq7jq541lhk";
+  version = "0.13.2";
+  sha256 = "19k0sa16dr63bgl9j37zrxnknlnq3c2927xccwc2vq19vl7n52nd";
   buildDepends = [ stm wxcore ];
   meta = {
     homepage = "http://haskell.org/haskellwiki/WxHaskell";
     description = "wxHaskell";
-    license = "LGPL";
+    license = "unknown";
     platforms = self.ghc.meta.platforms;
     maintainers = [
       self.stdenv.lib.maintainers.andres
