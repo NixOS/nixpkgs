d5b73eadeb26b88694ce2d87addcf17c65fda434
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-14 18:04:25 +0000

    Fix kalzium build
    svn path=/nixpkgs/trunk/; revision=31564

--- pkgs/desktops/kde-4.8/kdeedu/kalzium.nix
+++ pkgs/desktops/kde-4.8/kdeedu/kalzium.nix
@@ -2,12 +2,7 @@
 kde {
   buildInputs = [ kdelibs facile ocaml eigen openbabel avogadro ];
 
-  prePatch = ''
-    cp -v ${./FindLibfacile.cmake} cmake/modules/FindLibfacile.cmake
-    sed -e 's/\+facile/''${LIBFACILE_INCLUDE_DIR}/' -i src/CMakeOCamlInstructions.cmake
-    '';
-
-  patches = [ ./kalzium-feature-log.patch ];
+  patches = [ ./kalzium-find-libfacile.patch ];
 
   meta = {
     description = "Periodic Table of Elements";
