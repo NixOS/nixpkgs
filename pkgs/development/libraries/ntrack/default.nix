51956f085ddec9ca03bd8d7af483abf617aae54a
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-14 18:03:34 +0000

    ntrack-016, add meta
    svn path=/nixpkgs/trunk/; revision=31550

--- pkgs/development/libraries/ntrack/default.nix
+++ pkgs/development/libraries/ntrack/default.nix
@@ -1,8 +1,7 @@
-{ stdenv, fetchurl, glib, qt4, pkgconfig, libnl, pygobject, python, automake
-, autoconf }:
+{ stdenv, fetchurl, glib, qt4, pkgconfig, libnl, pygobject, python }:
 
 let
-  version = "014";
+  version = "016";
 in
 
 stdenv.mkDerivation rec {
@@ -10,32 +9,21 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "http://launchpad.net/ntrack/main/${version}/+download/${name}.tar.gz";
-    sha256 = "1aqn3q0dj2kk0j9rf02qgbfghlykaas7q0g8wxyz7nd6zg4qhyj2";
+    sha256 = "037ig5y0mp327m0hh4pnfr3vmsk3wrxgfjy3645q4ws9vdhx807w";
   };
 
   buildInputs = [ libnl qt4 ];
 
-  buildNativeInputs = [ pkgconfig python automake autoconf ];
+  buildNativeInputs = [ pkgconfig python ];
 
   configureFlags = "--without-gobject CFLAGS=--std=gnu99";
 
-  patchP0 = fetchurl {
-    url = http://bazaar.launchpad.net/~asac/ntrack/main/diff/312/309;
-    name = "ntrack-bzr-309-to-312.patch";
-    sha256 = "1bpjpikln2i7nsmd2gl82g08yzaqac311sgsva7z7pqccxz0vsj5";
+  patchPhase = ''sed -e "s@/usr\(/lib/ntrack/modules/\)@$out&@" -i common/ntrack.c'';
+
+  meta = {
+    description = "Network Connectivity Tracking library for Desktop Applications";
+    homepage = https://launchpad.net/ntrack;
+    platforms = stdenv.lib.platforms.linux;
+    maintainers = [ stdenv.lib.maintainers.urkud ];
   };
-
-  patchP1 = fetchurl {
-    url = "https://bugs.launchpad.net/ntrack/+bug/750554/+attachment/2291609/+files/ntrack_libnl_link.diff";
-    sha256 = "1al6wfjph7nmck1q2q2z98cnzcrwpln2wwh45xynsxr6wgczwck6";
-  };
-
-  patchPhase =
-    ''
-      patch -p0 < ${patchP0}
-      patch -p1 < ${patchP1}
-      sed -e "s@/usr\(/lib/ntrack/modules/\)@$out&@" -i common/ntrack.c
-    '';
-
-  preConfigure = "automake"; # The second patch changes Makefile.am files
 }
