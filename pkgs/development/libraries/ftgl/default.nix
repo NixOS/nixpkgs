116a7c4ac2bf2930c8294262d84a40f4cbaf17d6
Peter Simons <simons@cryp.to>
2012-01-15 11:44:05 +0000

    ftgl: fixed download URL
    svn path=/nixpkgs/trunk/; revision=31571

--- pkgs/development/libraries/ftgl/default.nix
+++ pkgs/development/libraries/ftgl/default.nix
@@ -1,13 +1,16 @@
 {stdenv, fetchurl, freetype, mesa}:
- 
-stdenv.mkDerivation {
+
+let
   name = "ftgl-2.1.3-rc5";
-  
+in
+stdenv.mkDerivation {
+  inherit name;
+
   src = fetchurl {
-    url = mirror://sourceforge/ftgl/files/FTGL%20Source/2.1.3%7Erc5/ftgl-2.1.3-rc5.tar.gz ;
+    url = "mirror://sourceforge/ftgl/${name}.tar.gz";
     sha256 = "0nsn4s6vnv5xcgxcw6q031amvh2zfj2smy1r5mbnjj2548hxcn2l";
   };
-  
+
   buildInputs = [freetype mesa];
 
 }
