bd7fe12cefe07b03a81e724de3bf55f12c4822e8
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-16 17:39:40 +0000

    Use qt-4.8 by default
    svn path=/nixpkgs/branches/stdenv-updates/; revision=31596

--- pkgs/desktops/kde-4.7/default.nix
+++ pkgs/desktops/kde-4.7/default.nix
@@ -1,4 +1,4 @@
-{ callPackage, callPackageOrig, stdenv, qt47 }:
+{ callPackage, callPackageOrig, stdenv, qt48 }:
 
 let
   release = "4.7.4";
@@ -44,7 +44,7 @@ kde.modules // kde.individual //
 
   akonadi = callPackage ./support/akonadi { };
 
-  qt4 = qt47;
+  qt4 = qt48;
 
   kdebase_workspace = kde.modules.kde_workspace;
 
