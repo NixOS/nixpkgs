09dee8994ff6b391f0bd7bb81bf418f946004c67
Peter Simons <simons@cryp.to>
2012-01-18 15:04:09 +0000

    haskell-idris: updated to version 0.9.0
    svn path=/nixpkgs/trunk/; revision=31622

--- pkgs/development/compilers/idris/default.nix
+++ pkgs/development/compilers/idris/default.nix
@@ -1,16 +1,17 @@
-{ cabal, binary, epic, happy, ivor, mtl, parsec, readline }:
+{ cabal, binary, epic, happy, haskeline, mtl, parsec, transformers
+}:
 
 cabal.mkDerivation (self: {
   pname = "idris";
-  version = "0.1.7.1";
-  sha256 = "1449fy7ld2p6ksn43bvhpa5z7j8vx4wc2szwq85wzpwfaw10d8wb";
-  isLibrary = true;
+  version = "0.9.0";
+  sha256 = "03zbdcl3v90zv0ibzq9fa8z2qrrdsilh5m509mczwrcmlzbzsmrl";
+  isLibrary = false;
   isExecutable = true;
-  buildDepends = [ binary epic ivor mtl parsec readline ];
+  buildDepends = [ binary epic haskeline mtl parsec transformers ];
   buildTools = [ happy ];
   noHaddock = true;
   meta = {
-    homepage = "http://www.cs.st-andrews.ac.uk/~eb/Idris/";
+    homepage = "http://www.idris-lang.org/";
     description = "Dependently Typed Functional Programming Language";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
