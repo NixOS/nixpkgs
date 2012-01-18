8cc90001d95843d4260ca7b9c3a995da4ef700b1
Peter Simons <simons@cryp.to>
2012-01-18 15:04:14 +0000

    haskell-blaze-builder-conduit: updated to version 0.0.1
    svn path=/nixpkgs/trunk/; revision=31624

--- pkgs/development/libraries/haskell/blaze-builder-conduit/default.nix
+++ pkgs/development/libraries/haskell/blaze-builder-conduit/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "blaze-builder-conduit";
-  version = "0.0.0";
-  sha256 = "022j78sj9cs4xcbvxz0f2lga0rhxcwaj1mkxn0019rbx3lix1nh3";
+  version = "0.0.1";
+  sha256 = "1k9is6dknfqjnkqrvxs9z8cx0ffnnz4q02is9plndyg8pccgjfa9";
   buildDepends = [ blazeBuilder conduit text transformers ];
   meta = {
     homepage = "http://github.com/snoyberg/conduit";
