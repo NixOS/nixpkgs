01f48c7fc1b73a544017bb2b0303b8b59d26cafb
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-01-18 19:44:16 +0000

    * Fix "most".  Not clear how passing "-ncurses" to the linker ever   worked.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=31641

--- pkgs/tools/misc/most/default.nix
+++ pkgs/tools/misc/most/default.nix
@@ -1,22 +1,23 @@
-{stdenv, fetchurl, slang}:
+{ stdenv, fetchurl, slang, ncurses }:
 
 stdenv.mkDerivation {
   name = "most-5.0.0";
 
   src = fetchurl {
-      url = ftp://space.mit.edu/pub/davis/most/most-5.0.0.tar.bz2;
-      sha256 = "1f5x7rvjg89b5klfqs1gb91jmbnd3fy08d8rwgdvgg0plqkxr7ja";
+    url = ftp://space.mit.edu/pub/davis/most/most-5.0.0.tar.bz2;
+    sha256 = "1f5x7rvjg89b5klfqs1gb91jmbnd3fy08d8rwgdvgg0plqkxr7ja";
   };
 
   preConfigure = ''
-    sed -i -e "s|-ltermcap|-ncurses|" configure
+    sed -i -e "s|-ltermcap|-lncurses|" configure
     sed -i autoconf/Makefile.in src/Makefile.in \
       -e "s|/bin/cp|cp|"  \
       -e "s|/bin/rm|rm|"
   '';
+  
   configureFlags = "--with-slang=${slang}";
 
-  buildInputs = [ slang ];
+  buildInputs = [ slang ncurses ];
 
   meta = {
     description = ''
