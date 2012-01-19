40a684701affafa17cfabe7cb9a29db1f51a1e15
Peter Simons <simons@cryp.to>
2012-01-19 14:45:10 +0000

    haskell-blaze-textual-native: updated to version 0.2.1.1
    svn path=/nixpkgs/trunk/; revision=31685

--- pkgs/development/libraries/haskell/blaze-textual-native/default.nix
+++ pkgs/development/libraries/haskell/blaze-textual-native/default.nix
@@ -2,12 +2,12 @@
 
 cabal.mkDerivation (self: {
   pname = "blaze-textual-native";
-  version = "0.2.1";
-  sha256 = "12cnl76qld19x6zlhxcsx2b27mfr9v7sc2xq6af9h77wqb98fkvn";
+  version = "0.2.1.1";
+  sha256 = "1q3gdf4ljc5xhw8f72qkvi6insk2nwdfk28a00y1b58jmk8003sd";
   buildDepends = [ blazeBuilder text time vector ];
   meta = {
     homepage = "http://github.com/mailrank/blaze-textual";
-    description = "Fast rendering of common datatypes";
+    description = "Fast rendering of common datatypes (deprecated)";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [
