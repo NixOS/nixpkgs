5856dbaa537eab2adcfe903d149756430a388db0
Peter Simons <simons@cryp.to>
2012-01-19 14:45:14 +0000

    haskell-control-monad-attempt: updated to version 0.3.0.1
    svn path=/nixpkgs/trunk/; revision=31686

--- pkgs/development/libraries/haskell/control-monad-attempt/default.nix
+++ pkgs/development/libraries/haskell/control-monad-attempt/default.nix
@@ -2,12 +2,12 @@
 
 cabal.mkDerivation (self: {
   pname = "control-monad-attempt";
-  version = "0.3.0";
-  sha256 = "1l0bqb5h2fs7vx2r8nd6kscyyrrqc0gshnxwdz9p6clfnknyqbqw";
+  version = "0.3.0.1";
+  sha256 = "140n27vdbyjz5qycrwlrmyd7s48fxcl6msl16g7czg40k5y23j5s";
   buildDepends = [ attempt transformers ];
   meta = {
     homepage = "http://github.com/snoyberg/control-monad-attempt";
-    description = "Monad transformer for attempt";
+    description = "Monad transformer for attempt. (deprecated)";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [
