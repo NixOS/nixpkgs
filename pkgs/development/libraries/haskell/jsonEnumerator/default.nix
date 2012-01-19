d1645c819d4e389a768b8502a37a628a3ebc1fc5
Peter Simons <simons@cryp.to>
2012-01-19 14:45:47 +0000

    haskell-json-enumerator: updated to version 0.0.1.2
    svn path=/nixpkgs/trunk/; revision=31691

--- pkgs/development/libraries/haskell/jsonEnumerator/default.nix
+++ pkgs/development/libraries/haskell/jsonEnumerator/default.nix
@@ -4,15 +4,15 @@
 
 cabal.mkDerivation (self: {
   pname = "json-enumerator";
-  version = "0.0.1.1";
-  sha256 = "0k94x9vwwaprqbc8gay5l0vg6hjmjpjp852yncncb8kr0r344z7l";
+  version = "0.0.1.2";
+  sha256 = "08gwrm15pvvhhrkrncy6wr4fi5v55fdhc8byfrw5zd62hmx8xm9d";
   buildDepends = [
     blazeBuilder blazeBuilderEnumerator enumerator jsonTypes text
     transformers
   ];
   meta = {
     homepage = "http://github.com/snoyberg/json-enumerator";
-    description = "Pure-Haskell utilities for dealing with JSON with the enumerator package";
+    description = "Pure-Haskell utilities for dealing with JSON with the enumerator package. (deprecated)";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [
