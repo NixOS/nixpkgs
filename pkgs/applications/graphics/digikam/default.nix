8deba0e4f80bb28448a39de10c4a5aa9b7a3c189
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-19 20:28:09 +0000

    Revert digikam change
    svn path=/nixpkgs/trunk/; revision=31714

--- pkgs/applications/graphics/digikam/default.nix
+++ pkgs/applications/graphics/digikam/default.nix
@@ -1,26 +1,28 @@
 { stdenv, fetchurl, cmake, qt4, kdelibs, automoc4, phonon, qimageblitz, qca2, eigen,
-lcms, jasper, libgphoto2, kdepimlibs, gettext, soprano, libjpeg, libtiff,
+kdegraphics, lcms, jasper, libgphoto2, kdepimlibs, gettext, soprano, kdeedu,
 liblqr1, lensfun, pkgconfig, qjson, libkdcraw, opencv, libkexiv2, libkipi, boost,
-shared_desktop_ontologies, marble, clapack, mysql }:
+shared_desktop_ontologies, marble }:
 
 stdenv.mkDerivation rec {
-  name = "digikam-2.5.0";
+  name = "digikam-2.4.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/digikam/${name}.tar.bz2";
-    sha256 = "06l52j8i45vyfj3b81ivifqsqdjlcj4g68d8w06c5lhzniwjqaam";
+    sha256 = "0fyyhc26syd1d1m8jqyg2i66hwd523mh419ln8y944jkrjj6gadc";
   };
 
-  buildNativeInputs = [ cmake automoc4 pkgconfig ];
+  buildInputs = [ cmake qt4 kdelibs kdegraphics automoc4 phonon qimageblitz qca2 eigen
+    lcms jasper libgphoto2 kdepimlibs gettext soprano kdeedu liblqr1 lensfun
+    pkgconfig qjson libkdcraw opencv libkexiv2 libkipi boost shared_desktop_ontologies
+    marble ];
 
-  buildInputs = [ qt4 kdelibs phonon qimageblitz qca2 eigen lcms libjpeg libtiff
-    jasper libgphoto2 kdepimlibs gettext soprano liblqr1 lensfun qjson libkdcraw
-    opencv libkexiv2 libkipi boost shared_desktop_ontologies marble mysql ];
+  KDEDIRS=kdeedu;
 
   # Make digikam find some FindXXXX.cmake
-  KDEDIRS="${marble}:${qjson}";
-
-  patches = [ ./libkvkontakte-not-topdir.patch ./ftbfs-libkipi.patch ];
+  preConfigure = ''
+    cp ${qjson}/share/apps/cmake/modules/FindQJSON.cmake cmake/modules;
+    cp ${marble}/share/apps/cmake/modules/FindMarble.cmake cmake/modules;
+  '';
 
   meta = {
     description = "Photo Management Program";
