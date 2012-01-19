c037e74e611d6089d9597f24f0988e0d9b60264c
Peter Simons <simons@cryp.to>
2012-01-19 14:45:51 +0000

    haskell-neither: updated to version 0.3.0.1
    svn path=/nixpkgs/trunk/; revision=31692

--- pkgs/development/libraries/haskell/neither/default.nix
+++ pkgs/development/libraries/haskell/neither/default.nix
@@ -2,12 +2,12 @@
 
 cabal.mkDerivation (self: {
   pname = "neither";
-  version = "0.3.0";
-  sha256 = "0lak4y0k4cisr27vw2bnpd0pa1kkgv8r96z7vf19wg7brzarx71l";
+  version = "0.3.0.1";
+  sha256 = "1vr8zap3vp28dr48s510lfrbfhw5yz25vng6wyk20582lv4j2mz8";
   buildDepends = [ failure transformers ];
   meta = {
     homepage = "http://github.com/snoyberg/neither";
-    description = "Provide versions of Either with good monad and applicative instances";
+    description = "Provide versions of Either with good monad and applicative instances. (deprecated)";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [
