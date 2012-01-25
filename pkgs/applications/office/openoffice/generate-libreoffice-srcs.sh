7fc001306c8d17cb3a723236b62ad24ac9db8681
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2012-01-25 19:12:27 +0000

    I add some little code to fetch the srcs of libreoffice.
    svn path=/nixpkgs/trunk/; revision=31838

---
+++ pkgs/applications/office/openoffice/generate-libreoffice-srcs.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+VERSIONBASE=3.4.5
+
+VERSION=3.4.5.2
+
+if [ $# -gt 2 ]; then
+  VERSIONBASE=$1
+  VERSION=$2
+fi
+
+echo '{fetchurl} : ['
+
+# I skip, as I hope I'll not need: sdk testing 
+for a in artwork base bootstrap calc components extensions extras filters \
+  help impress libs-core libs-extern libs-extern-sys libs-gui postprocess \
+  translations ure writer; do
+
+  URL=http://download.documentfoundation.org/libreoffice/src/$VERSIONBASE/libreoffice-$a-$VERSION.tar.bz2
+
+  echo '(fetchurl {'
+  echo "  url = \"$URL\";"
+  echo "  sha256 = \"`nix-prefetch-url $URL`\";"
+  echo '})'
+done
+
+echo ']'
---
+++ pkgs/applications/office/openoffice/libreoffice-srcs.nix
@@ -0,0 +1,74 @@
+{fetchurl} : [
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-artwork-3.4.5.2.tar.bz2";
+  sha256 = "01vi1njw0agpvxi0141k2arzhgq1kjbda7sj1l0l304qj9n78b1q";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-base-3.4.5.2.tar.bz2";
+  sha256 = "0f5rsyrn33iz0sny3wxsghv7c7yrbhas42yarb5rq958i3kpm6wz";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-bootstrap-3.4.5.2.tar.bz2";
+  sha256 = "05xz6ykddrm6mrgl9jssr2xpg2ir0x6c1c3n1cph0mvd0hiz58x9";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-calc-3.4.5.2.tar.bz2";
+  sha256 = "1l00y0qj1s9m31jmqg7q7bczxa5xsng388nqc9zc3vd6zjf4xy9i";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-components-3.4.5.2.tar.bz2";
+  sha256 = "1sq6yl06x006wxj1i89lv75sskb1bkxws82rcj1gvih6r8czih52";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-extensions-3.4.5.2.tar.bz2";
+  sha256 = "18xhk9h9vl9qhwlgv8ks3xi6hls2vgnkygnz1yhpf4p2g2y6nv7z";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-extras-3.4.5.2.tar.bz2";
+  sha256 = "0zxm30na6hzpjfasi1g890magf63kmg39y6y56wz9hv2z9p5q8jv";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-filters-3.4.5.2.tar.bz2";
+  sha256 = "002qxqav39530csraq16ga4fnvw8v9wqbvp88b6lzi2m5yqpbl46";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-help-3.4.5.2.tar.bz2";
+  sha256 = "1agsqzz73dmsy2jsw63qq1ljx4h0ppz7zwqllc7617yiak0rqsqq";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-impress-3.4.5.2.tar.bz2";
+  sha256 = "0ivabx4wl7dkfalr0z4gqm5976phc6mkidnv1i580ixrf0zdz483";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-libs-core-3.4.5.2.tar.bz2";
+  sha256 = "0blwd58hf51iixikrjrlqdm6n40wv5k2c9c8krfzxfcvk20mvv4b";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-libs-extern-3.4.5.2.tar.bz2";
+  sha256 = "01cm3fjfj08a7b8i6iszml2ly1wa2hnkqqq1irvjb0wb1fjjxq3s";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-libs-extern-sys-3.4.5.2.tar.bz2";
+  sha256 = "1528n6mnlhpdhi9zfvqziwr6knsak212ghjww0jlfh31ky3whlh0";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-libs-gui-3.4.5.2.tar.bz2";
+  sha256 = "0win8si2yjkbfrwx46dw6as82iw7x4wd53cirncsrwvzksdqphcr";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-postprocess-3.4.5.2.tar.bz2";
+  sha256 = "1rspqy1drfvj3rdfx8d7wbwn17vw08n26qmb98xwkgf5najdpfnp";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-translations-3.4.5.2.tar.bz2";
+  sha256 = "0ngynrskq6h58gybswcpklj49cyvh0igksngbr0gz6n1yq3471iz";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-ure-3.4.5.2.tar.bz2";
+  sha256 = "0wg06gbaq3ynqzaxaaqgxkjssxnggw6xqgfm63rmn9c1cd4q0mzy";
+})
+(fetchurl {
+  url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-writer-3.4.5.2.tar.bz2";
+  sha256 = "0k7xf94abavdzpns1z13phl3vjz0xkfv2h5sn7rgpr3dy184klxh";
+})
+]
