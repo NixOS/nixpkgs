9f2be8d6b113de165f6d5de9fcfa239224b5d3f9
Michael Raskin <7c6f434c@mail.ru>
2012-01-26 18:04:02 +0000

    Update GLEW
    svn path=/nixpkgs/trunk/; revision=31867

--- pkgs/development/libraries/glew/default.nix
+++ pkgs/development/libraries/glew/default.nix
@@ -1,16 +1,20 @@
 { stdenv, fetchurl, mesa, x11, libXmu, libXi }:
 
-stdenv.mkDerivation {
-  name = "glew-1.5.2";
+stdenv.mkDerivation rec {
+  name = "glew-1.7.0";
 
   src = fetchurl {
-    url = mirror://sourceforge/glew/glew-1.5.2.tgz;
-    sha256 = "0dh5wpfq7aaldkiwcqmm9w59c2qcglkjv8zazmnm8n5771n3caj8";
+    url = "mirror://sourceforge/glew/${name}.tgz";
+    sha256 = "02aw6sc4v1434m7awfcxqb0v987i2ykg3fncnp21i9g1n4zsclqn";
   };
 
   buildInputs = [ mesa x11 libXmu libXi ];
 
-  installPhase = "GLEW_DEST=\$out make install";
+  installPhase = "
+    GLEW_DEST=\$out make install
+    mkdir -pv \$out/share/doc/glew
+    cp -r README.txt LICENSE.txt doc  \$out/share/doc/glew
+  ";
   
   meta = { 
     description = "Cross-platform open-source C/C++ extension loading library";
