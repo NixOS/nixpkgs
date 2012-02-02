57c1279ce8a6f0dbdad7d0cae9d3173d7bdd071b
Peter Simons <simons@cryp.to>
2012-02-02 13:33:54 +0000

    haskell-clientsession: updated to version 0.7.4
    svn path=/nixpkgs/trunk/; revision=31980

--- pkgs/development/libraries/haskell/clientsession/default.nix
+++ pkgs/development/libraries/haskell/clientsession/default.nix
@@ -4,14 +4,14 @@
 
 cabal.mkDerivation (self: {
   pname = "clientsession";
-  version = "0.7.3.6";
-  sha256 = "04cvws7h2pm3rk8p2yj1pqkf9vjqq65hxg9sjldg7zhxdjgq1hgc";
+  version = "0.7.4";
+  sha256 = "050mg3rzyld4v2b9v1pc1q563sp7sffiapvr8ks8f46ibl17lvss";
   buildDepends = [
     base64Bytestring cereal cprngAes cryptoApi cryptocipher entropy
     skein tagged
   ];
   meta = {
-    homepage = "http://github.com/snoyberg/clientsession/tree/master";
+    homepage = "http://github.com/yesodweb/clientsession/tree/master";
     description = "Securely store session data in a client-side cookie";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
