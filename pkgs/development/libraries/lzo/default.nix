f113ee27ad1befdff3eef7cbd714065f167b946b
Ludovic Courtès <ludo@gnu.org>
2012-02-02 20:49:42 +0000

    LZO 2.06.
    svn path=/nixpkgs/trunk/; revision=31988

--- pkgs/development/libraries/lzo/default.nix
+++ pkgs/development/libraries/lzo/default.nix
@@ -1,18 +1,32 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation rec {
-  name = "lzo-2.03";
-  
+  name = "lzo-2.06";
+
   src = fetchurl {
     url = "${meta.homepage}/download/${name}.tar.gz";
-    sha256 = "8b1b0da8f757b9ac318e1c15a0eac8bdb56ca902a2dd25beda06c0f265f22591";
+    sha256 = "0wryshs446s7cclrbjykyj766znhcpnr7s3cxy33ybfn6vwfcygz";
   };
 
-  configureFlags = "--enable-shared";
+  configureFlags = [ "--enable-shared" ];
+
+  doCheck = true;
 
   meta = {
     description = "A data compresion library suitable for real-time data de-/compression";
+    longDescription =
+      '' LZO is a data compression library which is suitable for data
+	 de-/compression in real-time.  This means it favours speed over
+	 compression ratio.
+
+	 LZO is written in ANSI C.  Both the source code and the compressed
+	 data format are designed to be portable across platforms.
+      '';
+
     homepage = http://www.oberhumer.com/opensource/lzo;
     license = "GPLv2+";
+
+    platforms = stdenv.lib.platforms.all;
+    maintainers = [ stdenv.lib.maintainers.ludo ];
   };
 }
