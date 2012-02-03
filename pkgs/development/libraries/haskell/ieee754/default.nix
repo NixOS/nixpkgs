88f110cb756f56f51bec15f084ea885fc395c57a
Peter Simons <simons@cryp.to>
2012-02-03 21:00:24 +0000

    haskell-ieee754: updated to version 0.7.3
    svn path=/nixpkgs/trunk/; revision=32018

--- pkgs/development/libraries/haskell/ieee754/default.nix
+++ pkgs/development/libraries/haskell/ieee754/default.nix
@@ -9,5 +9,9 @@ cabal.mkDerivation (self: {
     description = "Utilities for dealing with IEEE floating point numbers";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.andres
+      self.stdenv.lib.maintainers.simons
+    ];
   };
 })
