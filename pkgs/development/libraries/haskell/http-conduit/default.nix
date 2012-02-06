df0f103e86fb06529bde8fcc49ddc0dbe77019b8
Peter Simons <simons@cryp.to>
2012-02-06 14:27:21 +0000

    haskell-http-conduit: downgrade to version 1.2.0
    The latest version has requirements version that we cannot fulfill
    without duplicating a lot of packages.
    
    svn path=/nixpkgs/trunk/; revision=32053

--- pkgs/development/libraries/haskell/http-conduit/default.nix
+++ pkgs/development/libraries/haskell/http-conduit/default.nix
@@ -1,19 +1,19 @@
 { cabal, asn1Data, attoparsec, attoparsecConduit, base64Bytestring
 , blazeBuilder, blazeBuilderConduit, caseInsensitive, certificate
 , conduit, cprngAes, dataDefault, failure, httpTypes, liftedBase
-, monadControl, network, socks, text, time, tls, tlsExtra
-, transformers, transformersBase, utf8String, zlibConduit
+, monadControl, network, text, time, tls, tlsExtra, transformers
+, transformersBase, utf8String, zlibConduit
 }:
 
 cabal.mkDerivation (self: {
   pname = "http-conduit";
-  version = "1.2.3";
-  sha256 = "0kygmbcvv0j020ml9jgmg3yzda3k066s2h8g3c135cmad6jc8hnd";
+  version = "1.2.0";
+  sha256 = "1s3qrvglhymwvr0cp32mi95dijic40lphhsfr4hm63qjphfc8dpi";
   buildDepends = [
     asn1Data attoparsec attoparsecConduit base64Bytestring blazeBuilder
     blazeBuilderConduit caseInsensitive certificate conduit cprngAes
-    dataDefault failure httpTypes liftedBase monadControl network socks
-    text time tls tlsExtra transformers transformersBase utf8String
+    dataDefault failure httpTypes liftedBase monadControl network text
+    time tls tlsExtra transformers transformersBase utf8String
     zlibConduit
   ];
   meta = {
