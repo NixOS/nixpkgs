d5d4995b03c3aec94bc691b9e09fccc360a9fa02
Peter Simons <simons@cryp.to>
2012-02-07 10:34:30 +0000

    haskell-pandoc: updated to version 1.9.0.5
    svn path=/nixpkgs/trunk/; revision=32102

--- pkgs/development/libraries/haskell/pandoc/default.nix
+++ pkgs/development/libraries/haskell/pandoc/default.nix
@@ -1,23 +1,19 @@
-{ cabal, ansiTerminal, base64Bytestring, blazeHtml, citeprocHs
-, Diff, extensibleExceptions, highlightingKate, HTTP, HUnit, json
-, mtl, network, pandocTypes, parsec, QuickCheck, random, syb
-, tagsoup, temporary, testFramework, testFrameworkHunit
-, testFrameworkQuickcheck2, texmath, time, utf8String, xml
-, zipArchive, zlib
+{ cabal, base64Bytestring, blazeHtml, citeprocHs
+, extensibleExceptions, highlightingKate, HTTP, json, mtl, network
+, pandocTypes, parsec, random, syb, tagsoup, temporary, texmath
+, time, utf8String, xml, zipArchive, zlib
 }:
 
 cabal.mkDerivation (self: {
   pname = "pandoc";
-  version = "1.9.0.3";
-  sha256 = "1p5054sdvvgl38rr0ajfavr79rwr2l8jdrpzai329ksskkh1acdp";
+  version = "1.9.0.5";
+  sha256 = "0haszw0khz47k2cfhzz38ia1zznwwmixhaf0jyr6l2gdpfq59b0p";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
-    ansiTerminal base64Bytestring blazeHtml citeprocHs Diff
-    extensibleExceptions highlightingKate HTTP HUnit json mtl network
-    pandocTypes parsec QuickCheck random syb tagsoup temporary
-    testFramework testFrameworkHunit testFrameworkQuickcheck2 texmath
-    time utf8String xml zipArchive zlib
+    base64Bytestring blazeHtml citeprocHs extensibleExceptions
+    highlightingKate HTTP json mtl network pandocTypes parsec random
+    syb tagsoup temporary texmath time utf8String xml zipArchive zlib
   ];
   configureFlags = "-fhighlighting -fthreaded";
   meta = {
