3f5643417a699f7acb1922fd08c0c27ad8bbb31e
Arie Middelkoop <amiddelk@gmail.com>
2012-02-07 09:14:34 +0000

    linux-firmware-nonfree 0.33 -> 0.35
    svn path=/nixpkgs/trunk/; revision=32097

--- pkgs/os-specific/linux/firmware/rtl8192c/default.nix
+++ pkgs/os-specific/linux/firmware/rtl8192c/default.nix
@@ -4,7 +4,10 @@ stdenv.mkDerivation {
   src = firmwareLinuxNonfree;
 
   phases = [ "installPhase" ];
-  installPhase = "ensureDir $out/rtlwifi && cp $src/realtek/rtlwifi/rtl8192cfw.bin $out/rtlwifi/rtl8192cfw.bin";
+  installPhase = ''
+    ensureDir $out/rtlwifi
+    cp "$src/rtlwifi/rtl8192cfw.bin" "$out/rtlwifi/rtl8192cfw.bin"
+  '';
 
   meta = {
     description = "Firmware for the Realtek RTL8192c wireless cards";
