07bf4c6f764dfc72bdca127f5361bd9e6cce093a
Peter Simons <simons@cryp.to>
2012-02-08 09:22:28 +0000

    haskell-asn1-data: updated to version 0.6.1.3
    svn path=/nixpkgs/trunk/; revision=32136

--- pkgs/development/libraries/haskell/asn1-data/default.nix
+++ pkgs/development/libraries/haskell/asn1-data/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "asn1-data";
-  version = "0.6.1.2";
-  sha256 = "1655fp71l8qjif4p6c1y2xk8r0gj58djg7np5zwwm6jlj780773r";
+  version = "0.6.1.3";
+  sha256 = "0vly55dpqrm9vb8ayp72na1nf2rr5ini3d81qqvxflmjgpksrgwq";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
