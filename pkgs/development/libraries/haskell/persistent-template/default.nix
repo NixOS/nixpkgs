c78b13be70449c786fdf146d68043dc176fe47c1
Peter Simons <simons@cryp.to>
2012-02-10 11:32:57 +0000

    haskell-persistent-template: updated to version 0.8.1
    svn path=/nixpkgs/trunk/; revision=32181

--- pkgs/development/libraries/haskell/persistent-template/default.nix
+++ pkgs/development/libraries/haskell/persistent-template/default.nix
@@ -1,10 +1,10 @@
-{ cabal, monadControl, persistent, text, transformers }:
+{ cabal, aeson, monadControl, persistent, text, transformers }:
 
 cabal.mkDerivation (self: {
   pname = "persistent-template";
-  version = "0.6.3.1";
-  sha256 = "0hppxd80q7bnxz9vm08i278d9k95w6z139y73dy53ncqkp72ahrg";
-  buildDepends = [ monadControl persistent text transformers ];
+  version = "0.8.1";
+  sha256 = "0av1rzqz80lzirk9l45jiwj4zar6ncy0q9x7dqhsrklyxk4jzv6v";
+  buildDepends = [ aeson monadControl persistent text transformers ];
   meta = {
     homepage = "http://www.yesodweb.com/book/persistent";
     description = "Type-safe, non-relational, multi-backend persistence";
