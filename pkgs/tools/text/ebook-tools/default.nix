761785c4ab0728b677db03cc9988538dd81ae51e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-02-10 18:33:14 +0000

    * Fix ebook-tools.  It broke because of the libzip update.
    svn path=/nixpkgs/trunk/; revision=32215

--- pkgs/tools/text/ebook-tools/default.nix
+++ pkgs/tools/text/ebook-tools/default.nix
@@ -1,22 +1,23 @@
-{ stdenv, fetchurl, cmake, libxml2, libzip }:
-
-let
-  pn = "ebook-tools";
-in
+{ stdenv, fetchurl, cmake, pkgconfig, libxml2, libzip }:
 
 stdenv.mkDerivation rec {
-  name = "${pn}-0.2.1";
+  name = "ebook-tools-0.2.1";
 
   src = fetchurl {
-    url = "mirror://sf/${pn}/${name}.tar.gz";
+    url = "mirror://sourceforge/ebook-tools/${name}.tar.gz";
     sha256 = "0wgwdsd3jwwfg36jyr5j0wayqjli3ia80lxzk10byd4cmkywnhy2";
   };
 
-  buildInputs = [ cmake libxml2 libzip ];
+  buildInputs = [ cmake pkgconfig libxml2 libzip ];
+
+  preConfigure = 
+    ''
+      NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE $(pkg-config --cflags libzip)"
+    '';
 
   meta = {
-    homepage = "http://${pn}.sourceforge.net";
-    description = "Tools and libs for dealing with various ebook file formats";
+    homepage = "http://ebook-tools.sourceforge.net";
+    description = "Tools and library for dealing with various ebook file formats";
     maintainers = [ stdenv.lib.maintainers.urkud ];
     platforms = stdenv.lib.platforms.all;
   };
