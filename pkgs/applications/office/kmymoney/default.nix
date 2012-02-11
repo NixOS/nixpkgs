af62434159aea3b910a59e1a734f8f20415d0570
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-02-11 21:15:16 +0000

    kmymoney-4.6.2, fix build with qt-4.8
    svn path=/nixpkgs/trunk/; revision=32230

--- pkgs/applications/office/kmymoney/default.nix
+++ pkgs/applications/office/kmymoney/default.nix
@@ -2,11 +2,11 @@
 , shared_mime_info, perl, boost, gpgme, gmpxx, libalkimia, libofx, libical }:
 
 stdenv.mkDerivation rec {
-  name = "kmymoney-4.6.1";
+  name = "kmymoney-4.6.2";
 
   src = fetchurl {
     url = "mirror://sourceforge/kmymoney2/${name}.tar.bz2";
-    sha256 = "0ympq70z0a9zrs623jznj6hnjph2rdhpra0np2mqm1mmy72bkjjz";
+    sha256 = "0x9bl4h2mk8dv49nnn7drv528fnl5ynvvyy7q4m22k0d5yxarn5d";
   };
 
   buildInputs = [ kdepimlibs perl boost gpgme gmpxx libalkimia libofx libical ];
@@ -14,7 +14,7 @@ stdenv.mkDerivation rec {
 
   KDEDIRS = libalkimia;
 
-  patches = [ ./qgpgme.patch ];
+  patches = [ ./qgpgme.patch ./qt-4.8.patch ];
 
   meta = {
     homepage = http://kmymoney2.sourceforge.net/;
