7d51d5249938880daa115ba6fa48fb2a91a159c4
Peter Simons <simons@cryp.to>
2012-02-13 09:01:51 +0000

    haskell-HDBC: updated to version 2.3.1.1
    svn path=/nixpkgs/trunk/; revision=32239

--- pkgs/development/libraries/haskell/HDBC/HDBC.nix
+++ pkgs/development/libraries/haskell/HDBC/HDBC.nix
@@ -2,13 +2,13 @@
 
 cabal.mkDerivation (self: {
   pname = "HDBC";
-  version = "2.3.1.0";
-  sha256 = "1y3qcc0ids9k3af4qkhabwg82q03a21wl4vdqhj1h0zmf08b3m6k";
+  version = "2.3.1.1";
+  sha256 = "1gqihvsf5w7j8xbn1xy93mdrsh77qwxbhfmyjivbm6i95w1i0kb7";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [ convertible mtl text time utf8String ];
   meta = {
-    homepage = "https://github.com/jgoerzen/hdbc/wiki";
+    homepage = "https://github.com/hdbc/hdbc";
     description = "Haskell Database Connectivity";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
