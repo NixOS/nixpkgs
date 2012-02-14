c81f618a343bbd8ee0473cbfd7217ee711b5ab58
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-02-14 14:33:10 +0000

    svn merge ^/nixpkgs/trunk
    svn path=/nixpkgs/branches/stdenv-updates/; revision=32273

--- pkgs/desktops/kde-4.8/kdepim-runtime.nix
+++ pkgs/desktops/kde-4.8/kdepim-runtime.nix
@@ -5,6 +5,13 @@ kde {
   buildInputs = [ kdepimlibs akonadi boost shared_desktop_ontologies libxml2
     libxslt ];
 
+  patches = map fetchurl [ {
+    # See https://bugs.kde.org/show_bug.cgi?id=289932
+    url = "http://bugsfiles.kde.org/attachment.cgi?id=68763";
+    name = "delays-in-nepomuk-indexing.patch";
+    sha256 = "0j3ndqwvi4bg789jb0wqsh2zpwi1gwdyng9gdmng189dx409jf85";
+  } ];
+
   meta = {
     description = "KDE PIM runtime";
     license = "GPL";
