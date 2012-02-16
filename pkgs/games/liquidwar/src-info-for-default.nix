4a260f082e3a2f535df4ef7d627f9af5a235d720
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-02-16 12:24:13 +0000

    svn merge ^/nixpkgs/trunk
    svn path=/nixpkgs/branches/libpng15/; revision=32325

--- pkgs/games/liquidwar/src-info-for-default.nix
+++
@@ -1,8 +0,0 @@
-{
-  downloadPage = "http://download.savannah.gnu.org/releases/liquidwar6/";
-  choiceCommand = '' tail -1 | sed -r -e 's@(.*)/@http://download.savannah.gnu.org/releases/liquidwar6/\1/liquidwar6-\1.tar.gz@' '';
-  sourceRegexp = ".*";
-  baseName = "liquidwar";
-  versionExtractorSedScript = ''s/.*-([-0-9a-z.]+)[.]tar[.]gz/\1/'';
-  versionReferenceCreator = "$(replaceAllVersionOccurences)";
-}
