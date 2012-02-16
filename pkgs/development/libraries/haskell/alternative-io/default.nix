0fea240440ab4bcaf7e0a207de9c7ccd304872ce
Peter Simons <simons@cryp.to>
2012-02-16 09:05:47 +0000

    haskell-alternative-io: added meta.maintainers section
    svn path=/nixpkgs/trunk/; revision=32318

--- pkgs/development/libraries/haskell/alternative-io/default.nix
+++ pkgs/development/libraries/haskell/alternative-io/default.nix
@@ -13,5 +13,9 @@ cabal.mkDerivation (self: {
     description = "IO as Alternative instance";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    maintainers = [
+      self.stdenv.lib.maintainers.andres
+      self.stdenv.lib.maintainers.simons
+    ];
   };
 })
