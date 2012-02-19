26f5bdfde1aa754196b8c02fd62b672fc311e517
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-02-19 09:22:20 +0000

    xplanet: pass netpbm, edit meta
    svn path=/nixpkgs/trunk/; revision=32390

--- pkgs/applications/science/astronomy/xplanet/default.nix
+++ pkgs/applications/science/astronomy/xplanet/default.nix
@@ -1,4 +1,5 @@
-{stdenv, fetchurl, lib, pkgconfig, freetype, pango, libpng, libtiff, giflib, libjpeg}:
+{stdenv, fetchurl, pkgconfig, freetype, pango, libpng, libtiff, giflib
+, libjpeg, netpbm}:
 
 stdenv.mkDerivation rec {
   name = "xplanet-1.2.2";
@@ -8,12 +9,13 @@ stdenv.mkDerivation rec {
     sha256 = "1jnkrly9njkibxqbg5im4pq9cqjzwmki6jzd318dvlfmnicqr3vg";
   };
 
-  buildInputs = [ pkgconfig freetype pango libpng libtiff giflib libjpeg ];
+  buildInputs = [ pkgconfig freetype pango libpng libtiff giflib libjpeg netpbm ];
 
   meta = {
     description = "Renders an image of the earth or other planets into the X root window";
     homepage = http://xplanet.sourceforge.net;
     license = "GPL";
-    maintainers = [ lib.maintainers.sander ];
+    maintainers = [ stdenv.lib.maintainers.sander stdenv.lib.maintainers.urkud ];
+    platforms = stdenv.lib.platforms.all;
   };
 }
