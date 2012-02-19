045d0a3ea0c58807622fe6ac74f455bcea6314aa
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-02-19 09:22:14 +0000

    xplanet-1.2.2; builds with libpng-1.5
    svn path=/nixpkgs/trunk/; revision=32389

--- pkgs/applications/science/astronomy/xplanet/default.nix
+++ pkgs/applications/science/astronomy/xplanet/default.nix
@@ -1,20 +1,12 @@
 {stdenv, fetchurl, lib, pkgconfig, freetype, pango, libpng, libtiff, giflib, libjpeg}:
 
-stdenv.mkDerivation {
-  name = "xplanet-1.2.1";
+stdenv.mkDerivation rec {
+  name = "xplanet-1.2.2";
 
   src = fetchurl {
-    url = mirror://sourceforge/xplanet/xplanet-1.2.1.tar.gz;
-    sha256 = "1pp55a1rgjkfcrwc00y3l48fhpqcp3qagd1zbym6zg27fzi5fbgm";
+    url = "mirror://sourceforge/xplanet/${name}.tar.gz";
+    sha256 = "1jnkrly9njkibxqbg5im4pq9cqjzwmki6jzd318dvlfmnicqr3vg";
   };
-
-  patches = 
-    [ # Build on GCC 4.4.
-      (fetchurl {
-        url = "http://sources.gentoo.org/viewcvs.py/*checkout*/gentoo-x86/x11-misc/xplanet/files/xplanet-1.2.1-gentoo.patch?rev=1.1";
-        sha256 = "0mmagjizj4hj057qmpi45w95zlrqda32x96xy44f6126xzj02yd5";
-      })
-    ];
 
   buildInputs = [ pkgconfig freetype pango libpng libtiff giflib libjpeg ];
 
