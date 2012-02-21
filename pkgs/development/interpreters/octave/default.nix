47e9932cd283c877c7e56e7a0af32ac247bbb6b8
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2012-02-21 15:45:22 +0000

    I attempted to get octave 3.4.3 working with atlas, but it also builds and crashes at plot(1), as 3.6.0 also did. I don't know what's wrong there.
    Program received signal SIGSEGV, Segmentation fault.
    0x00007ffff694e3ad in dtrtri_
    
    Exact as 3.6.0. I'll revert to the old octave version that worked without any
    blas or lapack implementation.
    
    --This line, and those below, will be ignored-e-
    
    M    pkgs/development/interpreters/octave/default.nix
    
    svn path=/nixpkgs/trunk/; revision=32460

--- pkgs/development/interpreters/octave/default.nix
+++ pkgs/development/interpreters/octave/default.nix
@@ -1,19 +1,21 @@
 {stdenv, fetchurl, gfortran, readline, ncurses, perl, flex, texinfo, qhull,
-libX11, graphicsmagick, pcre, blas, clapack, texLive }:
+libX11, graphicsmagick, pcre, atlas, clapack, texLive }:
 
 stdenv.mkDerivation rec {
-  name = "octave-3.6.0";
+  name = "octave-3.4.3";
   src = fetchurl {
     url = "mirror://gnu/octave/${name}.tar.bz2";
-    sha256 = "1mwj5pbbdzfbmcqyk0vx6si7mh8yhayppwnb1i63v871gxy775z5";
+    sha256 = "0j61kpfbv8l8rw3r9cwcmskvvav3q2f6plqdq3lnb153jg61klcl";
   };
 
   buildInputs = [ gfortran readline ncurses perl flex texinfo qhull libX11
-    graphicsmagick pcre blas clapack texLive ];
-
-  NIX_LDFLAGS = "-lf2c"; # For clapack
+    graphicsmagick pcre clapack atlas texLive ];
 
   enableParallelBuilding = true;
 
-  configureFlags = "--enable-readline --enable-dl --disable-docs";
+  preConfigure = ''
+    configureFlagsArray=('--with-blas=-L${atlas}/lib -lf77blas -latlas'
+      '--with-lapack=-L${clapack}/lib -llapack -lf2c')
+  '';
+  configureFlags = [ "--enable-readline" "--enable-dl" ];
 }
