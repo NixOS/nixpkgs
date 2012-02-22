371b34e49a8e66e105019f233aad213397ca203c
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2012-02-22 22:42:16 +0000

    Making octave build with qrupdate, and keep the big log of tests results of the check phase in the out store path.
    svn path=/nixpkgs/trunk/; revision=32496

--- pkgs/development/interpreters/octave/default.nix
+++ pkgs/development/interpreters/octave/default.nix
@@ -1,6 +1,6 @@
 {stdenv, fetchurl, gfortran, readline, ncurses, perl, flex, texinfo, qhull,
 libX11, graphicsmagick, pcre, liblapack, texLive, pkgconfig, mesa, fltk,
-fftw, fftwSinglePrec, zlib, curl }:
+fftw, fftwSinglePrec, zlib, curl, qrupdate }:
 
 stdenv.mkDerivation rec {
   name = "octave-3.4.3";
@@ -11,11 +11,17 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ gfortran readline ncurses perl flex texinfo qhull libX11
     graphicsmagick pcre liblapack texLive pkgconfig mesa fltk zlib curl
-    fftw fftwSinglePrec ];
+    fftw fftwSinglePrec qrupdate ];
 
   doCheck = true;
 
   enableParallelBuilding = true;
 
   configureFlags = [ "--enable-readline" "--enable-dl" ];
+
+  # Keep a copy of the octave tests detailed results in the output
+  # derivation, because someone may care
+  postInstall = ''
+    cp test/fntests.log $out/share/octave/${name}-fntests.log
+  '';
 }
