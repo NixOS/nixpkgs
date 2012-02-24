c24800a05b5e862167954e8a4f953b28fe0b5c52
Peter Simons <simons@cryp.to>
2012-02-24 11:38:57 +0000

    haskell-wai: updated to version 1.1.0.1
    svn path=/nixpkgs/trunk/; revision=32542

--- pkgs/development/libraries/haskell/wai/default.nix
+++ pkgs/development/libraries/haskell/wai/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "wai";
-  version = "1.1.0";
-  sha256 = "1kmmivcak9v13rgivs2vhr543dfdx19wncwlpda453570ywam7vh";
+  version = "1.1.0.1";
+  sha256 = "0klyi3d4rivgvbasamxccvkz1w44ak0rcbx840ypdcjq8mbcb85f";
   buildDepends = [
     blazeBuilder conduit httpTypes network text transformers vault
   ];
