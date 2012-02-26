7a5a0c24df24bd43c02ecd0e98a3e77809d3d706
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2012-02-26 21:23:59 +0000

    Updating itk to 4.0.0, so they may also build in stdenv-updates.
    svn path=/nixpkgs/trunk/; revision=32603

--- pkgs/development/libraries/itk/default.nix
+++ pkgs/development/libraries/itk/default.nix
@@ -1,18 +1,19 @@
-{stdenv, fetchurl, cmake, libX11, libuuid}:
+{stdenv, fetchurl, cmake, libX11, libuuid, xz}:
 
 stdenv.mkDerivation rec {
-  name = "itk-3.16.0";
+  name = "itk-4.0.0";
 
   src = fetchurl {
-    url = mirror://sourceforge/itk/InsightToolkit-3.16.0.tar.gz;
-    sha256 = "18r021ib2g94qlajjsny0r6cpc61rmr0zrpb2l0mx1y7j9ckr6ks";
+    url = mirror://sourceforge/itk/InsightToolkit-4.0.0.tar.xz;
+    sha256 = "05z49sw612cbyiaghcsda0xylrkf06jh81ql79si5632w1hpgbd9";
   };
 
   cmakeFlags = [ "-DBUILD_TESTING=OFF" "-DBUILD_EXAMPLES=OFF" ];
 
-  # makeFlags = [ "VERBOSE=1" ];
+  enableParallelBuilding = true;
 
-  buildInputs = [ cmake libX11 libuuid ];
+  buildNativeInputs = [ cmake xz ];
+  buildInputs = [ libX11 libuuid ];
 
   meta = {
     description = "Insight Segmentation and Registration Toolkit";
