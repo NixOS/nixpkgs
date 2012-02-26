0f81888e091f3e5788bdb711809abd7acde080a3
Florian Friesdorf <flo@chaoflow.net>
2012-02-26 17:23:20 +0000

    meta for offline-distutils
    svn path=/nixpkgs/branches/stdenv-updates/; revision=32586

--- pkgs/development/python-modules/offline-distutils/default.nix
+++ pkgs/development/python-modules/offline-distutils/default.nix
@@ -18,4 +18,17 @@ stdenv.mkDerivation {
 allow-hosts = None
 EOF
   '';
+
+  meta = {
+    description = "distutils configured to disallow downloads";
+    longDescription = ''
+      A normal distutils with added distutils.cfg to set allow-hosts
+      to None. This dissallows easy_install to download any packages.
+      It is used by buildPythonPackage to ensure that no packages are
+      downloaded during build/test/install.
+    '';
+    license = stdenv.lib.licenses.psfl;
+    platforms = stdenv.lib.platforms.all;
+    maintainers = with stdenv.lib.maintainers; [ simons chaoflow ];
+  };
 }
