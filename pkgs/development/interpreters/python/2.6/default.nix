1c504b49b486e0dd3ae15a2066618cc283e5aed2
Florian Friesdorf <flo@chaoflow.net>
2012-02-26 17:23:31 +0000

    remove empty readonly site-packages
    svn path=/nixpkgs/branches/stdenv-updates/; revision=32590

--- pkgs/development/interpreters/python/2.6/default.nix
+++ pkgs/development/interpreters/python/2.6/default.nix
@@ -64,6 +64,8 @@ let
     postInstall =
       ''
         rm -rf "$out/lib/python${majorVersion}/test"
+        rm "$out/lib/python${majorVersion}/site-packages/README"
+        rmdir "$out/lib/python${majorVersion}/site-packages/"
       '';
 
     passthru = {
--- pkgs/development/interpreters/python/3.1/default.nix
+++ pkgs/development/interpreters/python/3.1/default.nix
@@ -54,6 +54,8 @@ stdenv.mkDerivation {
 
   postInstall = ''
     rm -rf "$out/lib/python${majorVersion}/test"
+    rm "$out/lib/python${majorVersion}/site-packages/README"
+    rmdir "$out/lib/python${majorVersion}/site-packages/"
   '';
 
   passthru = {
