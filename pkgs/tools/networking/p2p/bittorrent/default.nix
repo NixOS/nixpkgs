2eb54f5062f171fbb686b900fbb347dcfe8958a2
Florian Friesdorf <flo@chaoflow.net>
2012-02-27 22:53:44 +0000

    fix bittorrent by using buildPythonPackage
    svn path=/nixpkgs/branches/stdenv-updates/; revision=32636

--- pkgs/tools/networking/p2p/bittorrent/default.nix
+++ pkgs/tools/networking/p2p/bittorrent/default.nix
@@ -1,20 +1,25 @@
 { gui ? false
 , stdenv, fetchurl, makeWrapper
-, python, wxPython ? null, pycrypto, twisted
+, python, wxPython ? null, pycrypto, twisted, buildPythonPackage
 }:
 
 assert gui -> wxPython != null;
 
-stdenv.mkDerivation {
+buildPythonPackage rec {
   name = "bittorrent-5.2.2";
-  
-  builder = ./builder.sh;
   
   src = fetchurl {
     url = http://download.bittorrent.com/dl/archive/BitTorrent-5.2.2.tar.gz;
     sha256 = "05k803hbwsyn51j4aibzdsnqxz24kw4rvr60v2c0wji8gcvy3kx9";
   };
   
+  preConfigure = ''
+    export HOME=$TMP
+  '';
+
+  # accesses network
+  doCheck = false;
+
   buildInputs = [ python pycrypto twisted makeWrapper ]
     ++ stdenv.lib.optional gui wxPython;
 
