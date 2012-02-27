9e646d4689139e200bc399b7855633899ab5cc99
Florian Friesdorf <flo@chaoflow.net>
2012-02-27 17:33:55 +0000

    added unzip to buildPythonPackage's buildInputs - used quite often
    authored by: chaoflow, goibhniu
    
    svn path=/nixpkgs/branches/stdenv-updates/; revision=32624

--- pkgs/development/python-modules/generic/default.nix
+++ pkgs/development/python-modules/generic/default.nix
@@ -3,7 +3,7 @@
    (http://pypi.python.org/pypi/setuptools/), which represents a large
    number of Python packages nowadays.  */
 
-{ python, setuptools, wrapPython, lib, site, offlineDistutils }:
+{ python, setuptools, wrapPython, lib, site, offlineDistutils, unzip }:
 
 { name, namePrefix ? "python-"
 
@@ -71,7 +71,7 @@ python.stdenv.mkDerivation (attrs // {
 
   name = namePrefix + name;
 
-  buildInputs = [ python wrapPython setuptools ] ++ buildInputs ++ pythonPath;
+  buildInputs = [ python wrapPython setuptools unzip ] ++ buildInputs ++ pythonPath;
 
   preConfigure = ''
     # do not allow distutils to make downloads, whatever install command is used
