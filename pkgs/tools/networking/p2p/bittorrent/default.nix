ee15dc232784394f95d3f3076af1ed016639abd2
Florian Friesdorf <flo@chaoflow.net>
2012-02-28 00:06:36 +0000

    Revert "fix bittorrent by using buildPythonPackage"
    This reverts commit c029f9f7fe86867cfd94461996dc6a6415e0e8d0.
    
    svn path=/nixpkgs/branches/stdenv-updates/; revision=32639

--- pkgs/tools/networking/p2p/bittorrent/default.nix
+++ pkgs/tools/networking/p2p/bittorrent/default.nix
@@ -1,25 +1,20 @@
 { gui ? false
 , stdenv, fetchurl, makeWrapper
-, python, wxPython ? null, pycrypto, twisted, buildPythonPackage
+, python, wxPython ? null, pycrypto, twisted
 }:
 
 assert gui -> wxPython != null;
 
-buildPythonPackage rec {
+stdenv.mkDerivation {
   name = "bittorrent-5.2.2";
+  
+  builder = ./builder.sh;
   
   src = fetchurl {
     url = http://download.bittorrent.com/dl/archive/BitTorrent-5.2.2.tar.gz;
     sha256 = "05k803hbwsyn51j4aibzdsnqxz24kw4rvr60v2c0wji8gcvy3kx9";
   };
   
-  preConfigure = ''
-    export HOME=$TMP
-  '';
-
-  # accesses network
-  doCheck = false;
-
   buildInputs = [ python pycrypto twisted makeWrapper ]
     ++ stdenv.lib.optional gui wxPython;
 
