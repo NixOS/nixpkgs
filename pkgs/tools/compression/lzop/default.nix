f20a947a5d13c68ab89f0c07db0ffecb80147b3d
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2012-03-01 22:03:34 +0000

    Updating lzop
    svn path=/nixpkgs/trunk/; revision=32730

--- pkgs/tools/compression/lzop/default.nix
+++ pkgs/tools/compression/lzop/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, lzo}:
 
 stdenv.mkDerivation {
-  name = "lzop-1.02rc1";
+  name = "lzop-1.03";
   src = fetchurl {
-    url = http://www.lzop.org/download/lzop-1.02rc1.tar.gz;
-    sha256 = "1dc32bfd82b130727bcec1de3b8a7cf090b78b3f14981d375ceb862b1e0e6873";
+    url = http://www.lzop.org/download/lzop-1.03.tar.gz;
+    sha256 = "1jdjvc4yjndf7ihmlcsyln2rbnbaxa86q4jskmkmm7ylfy65nhn1";
   };
 
   buildInputs = [ lzo ];
