f750ad3871169db3e9b3ea4d2a98e5a76fdb2b13
Peter Simons <simons@cryp.to>
2012-03-06 20:57:39 +0000

    nss: updated to version 3.13.3 to fix build errors on Linux 3.2
    svn path=/nixpkgs/trunk/; revision=32834

--- pkgs/development/libraries/nss/default.nix
+++ pkgs/development/libraries/nss/default.nix
@@ -10,11 +10,11 @@ let
 in
 
 stdenv.mkDerivation {
-  name = "nss-3.12.8";
-  
+  name = "nss-3.13.3";
+
   src = fetchurl {
-    url = http://ftp.mozilla.org/pub/mozilla.org/security/nss/releases/NSS_3_12_8_RTM/src/nss-3.12.8.tar.gz;
-    sha256 = "050c175l5zyzqxcp5fxj4q4n641c3j7w6w6fjg5hk3cyfhlwwy4i";
+    url = http://ftp.mozilla.org/pub/mozilla.org/security/nss/releases/NSS_3_13_3_RTM/src/nss-3.13.3.tar.gz;
+    sha256 = "efa10f2c70da4bddabf1a6081964969bb23359b93d6eadbf4739274a77bc3587";
   };
 
   buildInputs = [nspr perl zlib];
@@ -23,11 +23,9 @@ stdenv.mkDerivation {
 
   # Based on the build instructions at
   # http://www.mozilla.org/projects/security/pki/nss/nss-3.11.4/nss-3.11.4-build.html
-  
+
   postPatch = ''
-    sed -i -e "/^PREFIX =/s:= /usr:= $out:" \
-                "mozilla/security/nss/config/Makefile"
-    ln -s Linux2.6.mk mozilla/security/coreconf/Linux3.0.mk
+    sed -i -e "/^PREFIX =/s:= /usr:= $out:" mozilla/security/nss/config/Makefile
   '';
 
   preConfigure = "cd mozilla/security/nss";
@@ -64,7 +62,7 @@ stdenv.mkDerivation {
 
       # Borrowed from Gentoo.  Firefox expects an nss-config script,
       # but NSS doesn't provide it.
-      
+
       NSS_VMAJOR=`cat lib/nss/nss.h | grep "#define.*NSS_VMAJOR" | awk '{print $3}'`
       NSS_VMINOR=`cat lib/nss/nss.h | grep "#define.*NSS_VMINOR" | awk '{print $3}'`
       NSS_VPATCH=`cat lib/nss/nss.h | grep "#define.*NSS_VPATCH" | awk '{print $3}'`
