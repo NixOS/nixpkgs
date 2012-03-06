ce5a0fdf687ee122b93b3bbcd5601b71f09263ad
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-06 13:08:46 +0000

    rekonq-0.9.0-1
    svn path=/nixpkgs/trunk/; revision=32825

--- pkgs/applications/networking/browsers/rekonq/default.nix
+++ pkgs/applications/networking/browsers/rekonq/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, kdelibs, gettext}:
 
 stdenv.mkDerivation rec {
-  name = "rekonq-0.8.1";
+  name = "rekonq-0.9.0-1";
 
   src = fetchurl {
     url = "mirror://sf/rekonq/${name}.tar.bz2";
-    sha256 = "0zwvdk66q9iphdkrhn850p9f0h8lads9qx3v6dnq15gfk793nz4h";
+    sha256 = "0vri6wdxxi7qkcjpgvscwa7m3ysy62jns924d07arvy8bmg5whc5";
   };
 
   buildInputs = [ kdelibs ];
