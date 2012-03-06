f7896f322e4d3d6d1e65a7cf50ffdc1801e95e43
Peter Simons <simons@cryp.to>
2012-03-06 20:04:58 +0000

    libgnomeprint: fix gcc 4.6 compilation error
    svn path=/nixpkgs/trunk/; revision=32832

--- pkgs/desktops/gnome-2/platform/libgnomeprint/default.nix
+++ pkgs/desktops/gnome-2/platform/libgnomeprint/default.nix
@@ -12,6 +12,8 @@ stdenv.mkDerivation {
     sha256 = "1034ec8651051f84d2424e7a1da61c530422cc20ce5b2d9e107e1e46778d9691";
   };
 
+  patches = [ ./bug653388.patch ];
+
   buildInputs = [ pkgconfig gtk gettext intltool libart_lgpl libgnomecups bison flex ];
   propagatedBuildInputs = [ libxml2 ];
 }
