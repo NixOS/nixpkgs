a919e1274fa1105b83b3309a9c0955253d488208
Peter Simons <simons@cryp.to>
2012-03-07 06:52:02 +0000

    perl-Compress-Raw-Zlib: updated to version 2.051 to (hopefully) fix test suite failures on Darwin
    svn path=/nixpkgs/trunk/; revision=32842

--- pkgs/development/perl-modules/Compress-Raw-Zlib/default.nix
+++ pkgs/development/perl-modules/Compress-Raw-Zlib/default.nix
@@ -1,13 +1,13 @@
 { fetchurl, buildPerlPackage, zlib }:
 
 buildPerlPackage rec {
-  name = "Compress-Raw-Zlib-2.037";
-    
+  name = "Compress-Raw-Zlib-2.051";
+
   src = fetchurl {
     url = "mirror://cpan/modules/by-module/Compress/${name}.tar.gz";
-    sha256 = "18grvxjlsqlqiwxgdf26s4z4q9ag0vacrswxbyaqf11a03sciw7d";
+    sha256 = "16c7e0d2ed339c0b5ffe787bbcc9fc063ce6f2145d8cd6a18d0c79fa68d36c09";
   };
-    
+
   preConfigure = ''
     cat > config.in <<EOF
       BUILD_ZLIB   = False
