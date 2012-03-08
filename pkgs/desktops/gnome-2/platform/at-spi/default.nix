5d42d15d15425f38216b45b1f20bed004bd87c6f
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-08 22:45:02 +0000

    at-spi-1.32.0
    svn path=/nixpkgs/trunk/; revision=32915

--- pkgs/desktops/gnome-2/platform/at-spi/default.nix
+++ pkgs/desktops/gnome-2/platform/at-spi/default.nix
@@ -1,12 +1,14 @@
 { stdenv, fetchurl, python, pkgconfig, popt, atk, gtk, libX11, libICE, libXtst, libXi
-, intltool, libbonobo, ORBit2}:
+, intltool, libbonobo, ORBit2, GConf, dbus_glib }:
 
 stdenv.mkDerivation {
-  name = "at-spi-1.28.0";
+  name = "at-spi-1.32.0";
+
   src = fetchurl {
-    url = mirror://gnome/sources/at-spi/1.28/at-spi-1.28.0.tar.bz2;
-    sha256 = "0rv616drqpk58vybi3kalzyx06dxg26iwkbcrzk5563avhhj5qpb";
+    url = mirror://gnome/sources/at-spi/1.32/at-spi-1.32.0.tar.bz2;
+    sha256 = "0fbh0afzw1gm4r2w68b8l0vhnia1qyzdl407vyxfw4v4fkm1v16c";
   };
+
   buildInputs = [ python pkgconfig popt atk gtk libX11 libICE libXtst libXi
-                  intltool libbonobo ORBit2 ];
+                  intltool libbonobo ORBit2 GConf dbus_glib ];
 }
