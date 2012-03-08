a85944b6658c6cc716d9dc62664ebdd30d368be1
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-08 22:45:32 +0000

    gnome-desktop-2.32.1
    svn path=/nixpkgs/trunk/; revision=32922

--- pkgs/desktops/gnome-2/desktop/gnome-desktop/default.nix
+++ pkgs/desktops/gnome-2/desktop/gnome-desktop/default.nix
@@ -2,11 +2,13 @@
 , intltool, GConf, gnome_doc_utils}:
 
 stdenv.mkDerivation {
-  name = "gnome-desktop-2.28.0";
+  name = "gnome-desktop-2.32.1";
+
   src = fetchurl {
-    url = mirror://gnome/sources/gnome-desktop/2.28/gnome-desktop-2.28.0.tar.bz2;
-    sha256 = "1raag5c74pyy0f1i37fjxyxcnk9ck4mh6c1hcdmv5dv40xndwvwp";
+    url = mirror://gnome/sources/gnome-desktop/2.32/gnome-desktop-2.32.1.tar.bz2;
+    sha256 = "17bkng6ay37n3492lr9wpb49kms6gh554rn9gbjs27zygvvfrjsm";
   };
+
   configureFlags = "--disable-scrollkeeper";
   buildInputs = [ pkgconfig python libxml2Python libxslt which libX11 gtk
                   intltool GConf gnome_doc_utils ];
