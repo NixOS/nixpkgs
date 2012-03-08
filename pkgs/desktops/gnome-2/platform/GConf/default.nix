dff711a9db0bb92aa21b84c3aa4f775770a57a5d
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-08 22:45:25 +0000

    GConf: depend on gtk
    It needs gtk only for 'gtk-sanity-check-2' program. We should split this program
    to a dedicated package.
    
    svn path=/nixpkgs/trunk/; revision=32920

--- pkgs/desktops/gnome-2/platform/GConf/default.nix
+++ pkgs/desktops/gnome-2/platform/GConf/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, pkgconfig, dbus_glib, glib, ORBit2, libxml2
-, polkit, intltool, dbus_libs }:
+, polkit, intltool, dbus_libs, gtk }:
 
 stdenv.mkDerivation {
   name = "GConf-2.32.4";
@@ -9,6 +9,10 @@ stdenv.mkDerivation {
     sha256 = "09ch709cb9fniwc4221xgkq0jf0x0lxs814sqig8p2dcll0llvzk";
   };
 
-  buildInputs = [ pkgconfig ORBit2 dbus_libs dbus_glib libxml2 polkit intltool ];
+  buildInputs = [ ORBit2 dbus_libs dbus_glib libxml2 polkit gtk ];
   propagatedBuildInputs = [ glib ];
+
+  buildNativeInputs = [ pkgconfig intltool ];
+
+  configureFlags = "--with-gtk=2.0";
 }
