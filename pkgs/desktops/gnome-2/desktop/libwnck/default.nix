396fcedaedc3f0550c6285ec8a1382215ac219c4
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-08 22:45:38 +0000

    libwnck-2.30.7
    svn path=/nixpkgs/trunk/; revision=32924

--- pkgs/desktops/gnome-2/desktop/libwnck/default.nix
+++ pkgs/desktops/gnome-2/desktop/libwnck/default.nix
@@ -1,10 +1,12 @@
 {stdenv, fetchurl, pkgconfig, libX11, gtk, intltool}:
 
 stdenv.mkDerivation {
-  name = "libwnck-2.28.0";
+  name = "libwnck-2.30.7";
+
   src = fetchurl {
-    url = mirror://gnome/sources/libwnck/2.28/libwnck-2.28.0.tar.bz2;
-    sha256 = "0ixr2sffbcm6gn29vmli7x30cfi1848w8vgdhy93m8zg5xny62yr";
+    url = mirror://gnome/sources/libwnck/2.30/libwnck-2.30.7.tar.xz;
+    sha256 = "15713yl0f8f3p99jzqqfmbicrdswd3vwpx7r3bkf1bgh6d9lvs4b";
   };
+
   buildInputs = [ pkgconfig libX11 gtk intltool ];
 }
