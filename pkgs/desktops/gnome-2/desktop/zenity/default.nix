9fb93537bb1354244c823aa3f504fd1c6755bdd3
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-08 22:45:15 +0000

    zenity-2.32.1, remove CPPFLAGS magic
    svn path=/nixpkgs/trunk/; revision=32917

--- pkgs/desktops/gnome-2/desktop/zenity/default.nix
+++ pkgs/desktops/gnome-2/desktop/zenity/default.nix
@@ -1,16 +1,16 @@
-{stdenv, fetchurl, pkgconfig, cairo, libxml2, libxslt, glib, gtk, pango, atk, gnome_doc_utils, intltool, libglade,
-  libX11}:
+{ stdenv, fetchurl, pkgconfig, cairo, libxml2, libxslt, glib, gtk, pango, atk
+, gnome_doc_utils, intltool, libglade, libX11, which }:
 
 stdenv.mkDerivation {
-  name = "zenity-2.28.0";
+  name = "zenity-2.32.1";
+
   src = fetchurl {
-    url = mirror://gnome/sources/zenity/2.28/zenity-2.28.0.tar.bz2;
-    sha256 = "0qwcrkgqsldxmh29xlbakh6lc3qz8sp6kmk1ca7fc3kbwhya4irp";
+    url = mirror://gnome/sources/zenity/2.32/zenity-2.32.1.tar.bz2;
+    sha256 = "1b0qxb07wif0ds1pl8xk3fq9p874j89rf718lii4ndh7382bwf48";
   };
+
   configureFlags = "--disable-scrollkeeper";
-  buildInputs = [ pkgconfig gtk gnome_doc_utils intltool libglade libxml2 libxslt libX11];
-  CPPFLAGS = "-I${cairo}/include/cairo -I${glib}/include/glib-2.0 -I${glib}/lib/glib-2.0/include "+
-             "-I${gtk}/include/gtk-2.0 -I${gtk}/lib/gtk-2.0/include -I${pango}/include/pango-1.0 "+
-	     "-I${atk}/include/atk-1.0 -I${libglade}/include/libglade-2.0";
-  LIBS = "-lgtk-x11-2.0 -lglade-2.0 -lX11";
+  buildInputs = [ gtk libglade libxml2 libxslt libX11 ];
+
+  buildNativeInputs = [ pkgconfig intltool gnome_doc_utils which ];
 }
