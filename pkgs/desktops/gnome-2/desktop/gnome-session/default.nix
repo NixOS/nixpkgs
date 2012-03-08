cfe4427418a628712301508e82aa92d8020fd191
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-08 22:45:28 +0000

    gnome-session-2.32.1
    svn path=/nixpkgs/trunk/; revision=32921

--- pkgs/desktops/gnome-2/desktop/gnome-session/default.nix
+++ pkgs/desktops/gnome-2/desktop/gnome-session/default.nix
@@ -1,16 +1,17 @@
 { stdenv, fetchurl, pkgconfig, dbus_glib, cairo, dbus, gtk, pango, atk, libXau, libXtst, inputproto
-, intltool, libglade, startup_notification, GConf}:
+, intltool, libglade, startup_notification, GConf, upower }:
 
 stdenv.mkDerivation {
-  name = "gnome-session-2.28.0";
+  name = "gnome-session-2.32.1";
+
   src = fetchurl {
-    url = mirror://gnome/sources/gnome-session/2.28/gnome-session-2.28.0.tar.bz2;
-    sha256 = "032wfr70z61fnfk7snw8kck914z6y4wxr6v0dcgil3q9zc29ms43";
+    url = mirror://gnome/sources/gnome-session/2.32/gnome-session-2.32.1.tar.bz2;
+    sha256 = "0sk8qclarpar27va1ahzwjh2wsafys0xsdjzdg7cgygw6gj3rn92";
   };
-  buildInputs = [ pkgconfig dbus_glib gtk libXau libXtst inputproto intltool libglade startup_notification GConf ];
-  CPPFLAGS = "-I${gtk}/include/gtk-2.0 -I${gtk}/lib/gtk-2.0/include -I${dbus_glib}/include/dbus-1.0 "+
-             "-I${pango}/include/pango-1.0 -I${atk}/include/atk-1.0 -I${cairo}/include/cairo "+
-	     "-I${dbus.libs}/include/dbus-1.0 -I${dbus.libs}/lib/dbus-1.0/include "+
-	     "-I${libglade}/include/libglade-2.0";
-  LIBS = "-lglade-2.0 -ldbus-glib-1";
+
+  buildInputs =
+    [ dbus_glib gtk libXau libXtst inputproto libglade startup_notification
+      GConf upower
+    ];
+  buildNativeInputs = [ pkgconfig intltool ];
 }
