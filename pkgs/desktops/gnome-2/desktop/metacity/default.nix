86080768f10fb8c8a20b4e78dbd8b37ea44f96c6
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-08 22:46:03 +0000

    metacity-2.30.3
    svn path=/nixpkgs/trunk/; revision=32930

--- pkgs/desktops/gnome-2/desktop/metacity/default.nix
+++ pkgs/desktops/gnome-2/desktop/metacity/default.nix
@@ -1,12 +1,18 @@
 { stdenv, fetchurl, pkgconfig, glib, gtk, libXcomposite, libXcursor, libXdamage
-, libcanberra, intltool, GConf, startup_notification, zenity, gnome_doc_utils}:
+, libcanberra, intltool, GConf, startup_notification, zenity, gnome_doc_utils
+, gsettings_desktop_schemas }:
 
 stdenv.mkDerivation {
-  name = "metacity-2.28.0";
+  name = "metacity-2.30.3";
+
   src = fetchurl {
-    url = mirror://gnome/sources/metacity/2.28/metacity-2.28.0.tar.bz2;
-    sha256 = "0iamb6gw6gl6bfs7nqxpwr9xiij5axxr1iy4bl6g9z11dwx5a886";
+    url = mirror://gnome/sources/metacity/2.30/metacity-2.30.3.tar.bz2;
+    sha256 = "1p8qzj967mmlwdl6gv9vb2vzs19czvivl0sd337lgr55iw0qgy08";
   };
-  buildInputs = [ pkgconfig glib gtk libXcomposite libXcursor libXdamage libcanberra
-                  intltool GConf startup_notification zenity gnome_doc_utils ];
+
+  buildInputs =
+    [ pkgconfig glib gtk libXcomposite libXcursor libXdamage libcanberra
+      intltool GConf startup_notification zenity gnome_doc_utils
+      gsettings_desktop_schemas
+    ];
 }
