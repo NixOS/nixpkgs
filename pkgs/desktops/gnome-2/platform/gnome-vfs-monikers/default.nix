e7e3a28ac6776419c392802562744d91c7607e01
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-08 22:45:11 +0000

    gnome-vfs-monikers: cleanup CPPFLAGS
    svn path=/nixpkgs/trunk/; revision=32916

--- pkgs/desktops/gnome-2/platform/gnome-vfs-monikers/default.nix
+++ pkgs/desktops/gnome-2/platform/gnome-vfs-monikers/default.nix
@@ -7,7 +7,4 @@ stdenv.mkDerivation {
     sha256 = "0gpgk5vwhgqfhrd8pf1314kh7sv3jfqll2xbdbrs5s5sxy3v7b15";
   };
   buildInputs = [ pkgconfig glib intltool gnome_vfs libbonobo ];
-  CPPFLAGS = "-I${libbonobo}/include/libbonobo-2.0 -I${libbonobo}/include/bonobo-activation-2.0 "+
-             "-I${glib}/include/glib-2.0 -I${glib}/lib/glib-2.0/include -I${ORBit2}/include/orbit-2.0 "+
-	     "-I${gnome_vfs}/include/gnome-vfs-2.0";
 }
