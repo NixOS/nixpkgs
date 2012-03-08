d2747b12e98a91b734528cb29eab3a3af288c7b9
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2012-03-08 13:34:42 +0000

    Updating and fixing physfs, so more videogames can build.
    svn path=/nixpkgs/trunk/; revision=32891

--- pkgs/development/libraries/physfs/default.nix
+++ pkgs/development/libraries/physfs/default.nix
@@ -1,14 +1,18 @@
 {stdenv, fetchurl, cmake}:
 
 stdenv.mkDerivation rec {
-  name = "physfs-2.0.0";
+  name = "physfs-2.0.2";
 
   src = fetchurl {
     url = "${meta.homepage}/downloads/${name}.tar.gz";
-    sha256 = "072hqprni4vf4ax6b659s2xxrbz0y6iziarsczawbhi69m4azpyb";
+    sha256 = "02dwy0vsn2dp31f15vxd3yxxr1rgy25ab7ncavyh73i290qnsadf";
   };
 
   buildNativeInputs = [ cmake ];
+
+  patchPhase = ''
+    sed s,-Werror,, -i CMakeLists.txt
+  '';
 
   meta = {
     homepage = http://icculus.org/physfs/;
