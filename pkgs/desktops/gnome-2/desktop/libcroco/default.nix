ec84a1f0b8b7fb1dfb369589a273e03499818236
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-08 22:45:54 +0000

    libcroco-0.6.4
    svn path=/nixpkgs/trunk/; revision=32928

--- pkgs/desktops/gnome-2/desktop/libcroco/default.nix
+++ pkgs/desktops/gnome-2/desktop/libcroco/default.nix
@@ -1,10 +1,11 @@
 {stdenv, fetchurl, pkgconfig, libxml2, glib}:
 
 stdenv.mkDerivation {
-  name = "libcroco-0.6.2";
+  name = "libcroco-0.6.4";
+
   src = fetchurl {
-    url = mirror://gnome/sources/libcroco/0.6/libcroco-0.6.2.tar.bz2;
-    sha256 = "0j8p6xlpdhhbzjznr7rx7jiy3fi95qib0gsnkv9n76y0chzqa95y";
+    url = mirror://gnome/sources/libcroco/0.6/libcroco-0.6.4.tar.xz;
+    sha256 = "1sij88na1skd4d5dx75l803fww3v9872q8m2hj6sjlkc839vl5n8";
   };
   buildInputs = [ pkgconfig libxml2 glib ];
 }
