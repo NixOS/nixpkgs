065d71635de22881c16fb167798d71d0fa8675b7
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-08 15:37:23 +0000

    gnome-icon-theme-2.30.3
    svn path=/nixpkgs/trunk/; revision=32900

--- pkgs/desktops/gnome-2/desktop/gnome-icon-theme/default.nix
+++ pkgs/desktops/gnome-2/desktop/gnome-icon-theme/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, pkgconfig, intltool, iconnamingutils }:
 
 stdenv.mkDerivation rec {
-  name = "gnome-icon-theme-2.28.0";
-  
+  name = "gnome-icon-theme-2.30.3";
+
   src = fetchurl {
-    url = "mirror://gnome/sources/gnome-icon-theme/2.28/${name}.tar.bz2";
-    sha256 = "111q1yijm8mjvz600nfa49gbjz4988vpfv74jcknfng0k92vvv1i";
+    url = mirror://gnome/sources/gnome-icon-theme/2.30/gnome-icon-theme-2.30.3.tar.bz2;
+    sha256 = "1iysjfw3rajv9skdhshwcbjsm4jrsl6sfvqzrfynsfl4fyfjyzj1";
   };
   
-  buildInputs = [ pkgconfig intltool iconnamingutils ];
+  buildNativeInputs = [ pkgconfig intltool iconnamingutils ];
 }
