b409732c69e9a1c41adc1301b16c3eb39360b38b
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-08 22:45:18 +0000

    gnome-menus-2.30.5
    svn path=/nixpkgs/trunk/; revision=32918

--- pkgs/desktops/gnome-2/desktop/gnome-menus/default.nix
+++ pkgs/desktops/gnome-2/desktop/gnome-menus/default.nix
@@ -1,10 +1,12 @@
 {stdenv, fetchurl, pkgconfig, python, glib, intltool}:
 
 stdenv.mkDerivation {
-  name = "gnome-menus-2.28.0";
+  name = "gnome-menus-2.30.5";
+
   src = fetchurl {
-    url = mirror://gnome/sources/gnome-menus/2.28/gnome-menus-2.28.0.tar.bz2;
-    sha256 = "1lgkqa5gn0g61mfmr2xj2yfg4qjpdavj8rgbdg4bldznphmhp11s";
+    url = mirror://gnome/sources/gnome-menus/2.30/gnome-menus-2.30.5.tar.bz2;
+    sha256 = "1ajckii51spmkgfc0168c56x0syz5vwb2fp8b81c5s6n0r85dk3d";
   };
+
   buildInputs = [ pkgconfig python glib intltool ];
 }
