711fada6eea7021241699ecdfe479c2727875ba7
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-08 22:46:06 +0000

    gnome-settings-daemon-2.32.1
    svn path=/nixpkgs/trunk/; revision=32931

--- pkgs/desktops/gnome-2/desktop/gnome-settings-daemon/default.nix
+++ pkgs/desktops/gnome-2/desktop/gnome-settings-daemon/default.nix
@@ -1,11 +1,18 @@
 { stdenv, fetchurl, pkgconfig, dbus_glib, libxklavier, gtk
-, intltool, GConf, gnome_desktop, libglade, libgnomekbd}:
+, intltool, GConf, gnome_desktop, libglade, libgnomekbd, polkit, pulseaudio }:
 
 stdenv.mkDerivation {
-  name = "gnome-settings-daemon-2.28.0";
+  name = "gnome-settings-daemon-2.32.1";
+
   src = fetchurl {
-    url = mirror://gnome/sources/gnome-settings-daemon/2.28/gnome-settings-daemon-2.28.0.tar.bz2;
-    sha256 = "1md46vs3m36czwjdkz084facanjr03cxgr50frf2yln60kc06cnz";
+    url = mirror://gnome/sources/gnome-settings-daemon/2.32/gnome-settings-daemon-2.32.1.tar.bz2;
+    sha256 = "11jyn10w2p2a76pjrkd0pjl1w406df821p053awklvmdqgzb6x00";
   };
-  buildInputs = [ pkgconfig intltool dbus_glib libxklavier gtk GConf gnome_desktop libglade libgnomekbd ];
+
+  buildInputs =
+    [ dbus_glib libxklavier gtk GConf gnome_desktop libglade libgnomekbd polkit
+      pulseaudio
+    ];
+
+  buildNativeInputs = [ pkgconfig intltool ];
 }
