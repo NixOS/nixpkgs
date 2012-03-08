b196e5f1253d33d9a9de6eacf1b84c69fb09f88f
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-08 22:45:51 +0000

    libgnomekbd-2.32.0
    svn path=/nixpkgs/trunk/; revision=32927

--- pkgs/desktops/gnome-2/desktop/libgnomekbd/default.nix
+++ pkgs/desktops/gnome-2/desktop/libgnomekbd/default.nix
@@ -1,10 +1,12 @@
 {stdenv, fetchurl, pkgconfig, dbus_glib, libxklavier, glib, gtk, intltool, GConf, libglade}:
 
 stdenv.mkDerivation {
-  name = "libgnomekbd-2.28.0";
+  name = "libgnomekbd-2.32.0";
+
   src = fetchurl {
-    url = mirror://gnome/sources/libgnomekbd/2.28/libgnomekbd-2.28.0.tar.bz2;
-    sha256 = "0s1664nwsavwjfmg4wkhvrpz9qxw04nsx5l8z87nlrny3312rnkj";
+    url = mirror://gnome/sources/libgnomekbd/2.32/libgnomekbd-2.32.0.tar.bz2;
+    sha256 = "0mnjhdryx94c106fghzz01dyc1vlp16wn6sajvpxffnqqx62rmfx";
   };
+
   buildInputs = [ pkgconfig dbus_glib libxklavier glib gtk intltool GConf libglade ];
 }
