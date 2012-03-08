7390600a6e3e56e7f8f6529b2f40d7c8a77e128a
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-08 10:14:37 +0000

    libsoup-2.34.3
    svn path=/nixpkgs/trunk/; revision=32883

--- pkgs/desktops/gnome-2/desktop/libsoup/default.nix
+++ pkgs/desktops/gnome-2/desktop/libsoup/default.nix
@@ -2,11 +2,15 @@
   glib, GConf, gnome_keyring}:
 
 stdenv.mkDerivation rec {
-  name = "libsoup-2.33.6";
+  name = "libsoup-2.34.3";
+
   src = fetchurl {
-    url = "mirror://gnome/sources/libsoup/2.33/${name}.tar.bz2";
-    sha256 = "988f7897fe125a77a5946b2fd6d47d7374fd94a1406e810482cfff6a52a6a923";
+    url = mirror://gnome/sources/libsoup/2.34/libsoup-2.34.3.tar.xz;
+    sha256 = "072af1iqcky5vm6akm450qhdjrgav4yyl6s8idhnq0gpm5jqhgy4";
   };
+
   buildInputs = [ pkgconfig libxml2 gnutls libproxy sqlite curl 
     glib GConf gnome_keyring ];
+
+  configureFlags = "--disable-tls-check";
 }
