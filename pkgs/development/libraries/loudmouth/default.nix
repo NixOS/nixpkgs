469d3f46bee28098929f780dc9bd7bb17af073c0
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-09 15:09:11 +0000

    loudmouth: use mirror://gnome
    svn path=/nixpkgs/trunk/; revision=32955

--- pkgs/development/libraries/loudmouth/default.nix
+++ pkgs/development/libraries/loudmouth/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "loudmouth-1.4.3";
     
   src = fetchurl {
-    url = http://ftp.gnome.org/pub/GNOME/sources/loudmouth/1.4/loudmouth-1.4.3.tar.bz2;
+    url = mirror://gnome/sources/loudmouth/1.4/loudmouth-1.4.3.tar.bz2;
     md5 = "55339ca42494690c3942ee1465a96937";
   };
     
