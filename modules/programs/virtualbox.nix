de6968c1630f22590b104718fa49baf83b044df5
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-03-10 14:34:40 +0000

    * Virtualbox: use the right set of kernel packages.
    svn path=/nixos/trunk/; revision=32971

--- modules/programs/virtualbox.nix
+++ modules/programs/virtualbox.nix
@@ -2,16 +2,18 @@
 
 with pkgs.lib;
 
+let virtualbox = config.boot.kernelPackages.virtualbox; in
+
 {
   boot.kernelModules = [ "vboxdrv" "vboxnetadp" "vboxnetflt" ];
-  boot.extraModulePackages = [ pkgs.linuxPackages.virtualbox ];
-  environment.systemPackages = [ pkgs.linuxPackages.virtualbox ];
+  boot.extraModulePackages = [ virtualbox ];
+  environment.systemPackages = [ virtualbox ];
 
   # ‘VBoxNetAdpCtl’ needs to be setuid root to allow users to create
   # host-only networks (https://www.virtualbox.org/ticket/4014).
   security.setuidOwners = singleton
     { program = "VBoxNetAdpCtl";
-      source = "${pkgs.linuxPackages.virtualbox}/virtualbox/VBoxNetAdpCtl";
+      source = "${virtualbox}/virtualbox/VBoxNetAdpCtl";
       owner = "root";
       group = "root";
       setuid = true;
