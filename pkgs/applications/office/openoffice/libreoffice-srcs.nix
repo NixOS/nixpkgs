251b9b6472e396b2a7d6a33d7920b3fba13eb5fa
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2012-03-10 21:18:07 +0000

    I made libreoffice build further... Let's see if further enough. I make hydra build it regardless of 'force'. Tomorrow we'll see the result.
    svn path=/nixpkgs/trunk/; revision=32977

--- pkgs/applications/office/openoffice/libreoffice-srcs.nix
+++ pkgs/applications/office/openoffice/libreoffice-srcs.nix
@@ -75,4 +75,80 @@
   url = "http://download.documentfoundation.org/libreoffice/src/3.4.5/libreoffice-testing-3.4.5.2.tar.bz2";
   sha256 = "05sk3li5jgmjv722ncsr2i7arhmsvkkvmh4qw7xl70p8dpxhla8q";
 })
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/26b3e95ddf3d9c077c480ea45874b3b8-lp_solve_5.5.tar.gz";
+  md5 = "26b3e95ddf3d9c077c480ea45874b3b8";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/ea91f2fb4212a21d708aced277e6e85a-vigra1.4.0.tar.gz";
+  md5 = "ea91f2fb4212a21d708aced277e6e85a";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/9ed97fce60a9a65852402248a6659492-hunspell-1.3.1.tar.gz";
+  md5 = "9ed97fce60a9a65852402248a6659492";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/17410483b5b5f267aa18b7e00b65e6e0-hsqldb_1_8_0.zip";
+  md5 = "17410483b5b5f267aa18b7e00b65e6e0";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/0ff7d225d087793c8c2c680d77aac3e7-mdds_0.5.3.tar.bz2";
+  md5 = "0ff7d225d087793c8c2c680d77aac3e7";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/ea570af93c284aa9e5621cd563f54f4d-bsh-2.0b1-src.tar.gz";
+  md5 = "ea570af93c284aa9e5621cd563f54f4d";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/ada24d37d8d638b3d8a9985e80bc2978-source-9.0.0.7-bj.zip";
+  md5 = "ada24d37d8d638b3d8a9985e80bc2978";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/48a9f787f43a09c0a9b7b00cd1fddbbf-hyphen-2.7.1.tar.gz";
+  md5 = "48a9f787f43a09c0a9b7b00cd1fddbbf";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/798b2ffdc8bcfe7bca2cf92b62caf685-rhino1_5R5.zip";
+  md5 = "798b2ffdc8bcfe7bca2cf92b62caf685";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/3bf481ca95109b14435125c0dd1f2217-graphite2-1.0.3.tgz";
+  md5 = "3bf481ca95109b14435125c0dd1f2217";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/284e768eeda0e2898b0d5bf7e26a016e-raptor-1.4.18.tar.gz";
+  md5 = "284e768eeda0e2898b0d5bf7e26a016e";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/128cfc86ed5953e57fe0f5ae98b62c2e-libtextcat-2.2.tar.gz";
+  md5 = "128cfc86ed5953e57fe0f5ae98b62c2e";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/48d8169acc35f97e05d8dcdfd45be7f2-lucene-2.3.2.tar.gz";
+  md5 = "48d8169acc35f97e05d8dcdfd45be7f2";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/5ba6a61a2f66dfd5fee8cdd4cd262a37-libwpg-0.2.0.tar.bz2";
+  md5 = "5ba6a61a2f66dfd5fee8cdd4cd262a37";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/83d4029aebf290c0a9a8fee9c99638d3-libwps-0.2.1.tar.bz2";
+  md5 = "83d4029aebf290c0a9a8fee9c99638d3";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/fdb27bfe2dbe2e7b57ae194d9bf36bab-SampleICC-1.3.2.tar.gz";
+  md5 = "fdb27bfe2dbe2e7b57ae194d9bf36bab";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/067201ea8b126597670b5eff72e1f66c-mythes-1.2.0.tar.gz";
+  md5 = "067201ea8b126597670b5eff72e1f66c";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/fca8706f2c4619e2fa3f8f42f8fc1e9d-rasqal-0.9.16.tar.gz";
+  md5 = "fca8706f2c4619e2fa3f8f42f8fc1e9d";
+})
+(fetchurl {
+  url = "http://dev-www.libreoffice.org/src/ca66e26082cab8bb817185a116db809b-redland-1.0.8.tar.gz";
+  md5 = "ca66e26082cab8bb817185a116db809b";
+})
 ]
