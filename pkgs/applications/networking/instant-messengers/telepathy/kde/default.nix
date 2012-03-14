7a3187226182d45630f64415c63a6fcaf4f712b0
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-14 19:35:30 +0000

    Add kde4.telepathy.call_ui
    Very experimental, fails to build (missing deps)
    
    svn path=/nixpkgs/trunk/; revision=33077

--- pkgs/applications/networking/instant-messengers/telepathy/kde/default.nix
+++ pkgs/applications/networking/instant-messengers/telepathy/kde/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, telepathy_qt, kdelibs, gettext, pkgconfig }:
+{ stdenv, fetchurl, fetchgit, telepathy_qt, kdelibs, gettext, pkgconfig }:
 
 let
   version = "0.3.0";
@@ -27,10 +27,25 @@ let
     );
   };
 
-  pkgs = builtins.listToAttrs (map ktpFun manifest);
+  stable = builtins.listToAttrs (map ktpFun manifest);
+  unstable = {
+    call_ui = stdenv.mkDerivation {
+      name = "ktp-call-ui-20120314";
+
+      src = fetchgit {
+        url = git://anongit.kde.org/ktp-call-ui;
+        rev = "3587166d1ace83b115e113853514a7acc04d9d86";
+        sha256 = "0yv386rqy4vkwmd38wvvsrbam59sbv5k2lwimv96kf93xgkp5g0l";
+      };
+
+      buildInputs = [ kdelibs telepathy_qt common_internals ];
+      buildNativeInputs = [ gettext pkgconfig ];
+    };
+  };
   common_internals = pkgs.common_internals;
+  pkgs = unstable // stable;
 in
-pkgs //{
+pkgs // {
   inherit version;
   recurseForDerivations = true;
   full = stdenv.lib.attrValues pkgs;
