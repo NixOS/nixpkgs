6ff954a5e8ff7e7cc1c80602cd692c9550e40b4c
Peter Simons <simons@cryp.to>
2012-03-15 18:07:49 +0000

    git-annex: updated to version 3.20120315
    svn path=/nixpkgs/trunk/; revision=33116

--- pkgs/applications/version-management/git-and-tools/default.nix
+++ pkgs/applications/version-management/git-and-tools/default.nix
@@ -48,7 +48,8 @@ rec {
   gitAnnex = lib.makeOverridable (import ./git-annex) {
     inherit stdenv fetchurl libuuid rsync findutils curl perl git ikiwiki which coreutils;
     inherit (haskellPackages_ghc741) ghc MissingH utf8String pcreLight SHA dataenc
-      HTTP testpack hS3 mtl network hslogger hxt json liftedBase monadControl IfElse QuickCheck2;
+      HTTP testpack hS3 mtl network hslogger hxt json liftedBase monadControl IfElse
+      QuickCheck2 bloomfilter;
   };
 
   qgit = import ./qgit {
