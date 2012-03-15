b0d0278e82febd897bbc086a2243e75c5f40b858
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-03-15 17:08:26 +0000

    libssh-0.5.2, switch to libgcrypt
    svn path=/nixpkgs/trunk/; revision=33114

--- pkgs/development/libraries/libssh/default.nix
+++ pkgs/development/libraries/libssh/default.nix
@@ -1,12 +1,14 @@
-{stdenv, fetchurl, cmake, zlib, openssl}:
+{stdenv, fetchurl, cmake, zlib, libgcrypt}:
 
 stdenv.mkDerivation rec {
-  name = "libssh-0.4.8";
+  name = "libssh-0.5.2";
   src = fetchurl {
-    url = "http://www.libssh.org/files/0.4/${name}.tar.gz";
-    sha256 = "05d8i8hwya2gry3lky9pmjpvr9f4wvggszqjjzgxvyy74sj3khhm";
+    url = "http://www.libssh.org/files/0.5/${name}.tar.gz";
+    sha256 = "09xjb8ja8k3j3a0y3zjphhxwrnh1y618afcbbri48x87mz0vks21";
   };
-  buildInputs = [ cmake zlib openssl ];
+  buildInputs = [ zlib libgcrypt ];
+  buildNativeInputs = [ cmake ];
+  cmakeFlags = "-DWITH_GCRYPT=ON";
   meta = {
     description = "SSH client library";
     license = "LGPL";
