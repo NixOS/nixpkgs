a315eefca145ee5aace6b3ee9a5bf381c5f535d4
Shea Levy <shea@shealevy.com>
2012-03-16 11:22:05 +0000

    Restore old installation-cd-new-kernel.nix and create a new installation-cd-efi.nix
    svn path=/nixos/trunk/; revision=33146

---
+++ modules/installer/cd-dvd/installation-cd-efi.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  require = [ ./installation-cd-graphical.nix ];
+
+  boot.kernelPackages = pkgs.linuxPackages_3_3;
+  boot.vesa = false;
+
+  # What follows should probably move into base once the base kernel has the
+  # efi boot stub
+
+  # Get a console as soon as the initrd loads fbcon on EFI boot
+  boot.initrd.kernelModules = [ "fbcon" ];
+
+  # Enable reading EFI variables via sysfs
+  boot.kernelModules = [ "efivars" ];
+
+  # efi-related tools
+  environment.systemPackages = [ pkgs.efibootmgr ];
+
+  isoImage.makeEfiBootable = true;
+}
