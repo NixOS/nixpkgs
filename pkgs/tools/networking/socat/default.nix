5521faad6520f65ac77cff59e9b499b207d40e7e
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2012-03-18 18:33:10 +0000

    Updating socat. I thought it could have improvements in performance for nixos build-vm in my computer, but does not.
    svn path=/nixpkgs/trunk/; revision=33239

--- pkgs/tools/networking/socat/default.nix
+++ pkgs/tools/networking/socat/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, openssl }:
 
 stdenv.mkDerivation rec {
-  name = "socat-1.7.1.2";
+  name = "socat-1.7.2.0";
   
   src = fetchurl {
     url = "http://www.dest-unreach.org/socat/download/${name}.tar.bz2";
-    sha256 = "0rz12l36id4sfzbr2mpral1ddcqgm71al0snh14smg8l94amnfgp";
+    sha256 = "00hq0ia1fs4sy0qpavzlpf4qmnhh2ybq5is2kqzvqky14zlvvcsr";
   };
 
   buildInputs = [ openssl ];
