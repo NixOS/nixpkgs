836532a54b527771ffc4472b3c32f55fb7110a51
Shea Levy <shea@shealevy.com>
2012-03-20 01:51:03 +0000

    Whoops, forgot meta
    svn path=/nixpkgs/trunk/; revision=33284

--- pkgs/os-specific/linux/xf86-input-mtrack/default.nix
+++ pkgs/os-specific/linux/xf86-input-mtrack/default.nix
@@ -34,4 +34,14 @@ stdenv.mkDerivation {
     url = "https://github.com/BlueDragonX/xf86-input-mtrack/tarball/v0.2.0";
     sha256 = "1zvd68dxpjn44ys7ysi3yc95xdjw1rz0s3xwlh3fzpw1ib3wrr3x";
   };
+
+  meta = {
+    homepage = https://github.com/BlueDragonX/xf86-input-mtrack;
+
+    description = "An Xorg driver for multitouch trackpads";
+
+    license = "gplv2";
+
+    maintainers = [ stdenv.lib.maintainers.shlevy ];
+  };
 }
