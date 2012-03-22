3a86557b7c3f500d2310d7d07ca8131573d8ca92
Cillian de Roiste <cillian.deroiste@gmail.com>
2012-03-22 10:36:48 +0000

    Tidy up: style
    svn path=/nixpkgs/trunk/; revision=33351

--- pkgs/applications/audio/qtractor/default.nix
+++ pkgs/applications/audio/qtractor/default.nix
@@ -1,6 +1,6 @@
-{ alsaLib, autoconf, automake, dssi, fetchurl, gtk, jackaudio,
-ladspaH, ladspaPlugins, liblo, libmad, libsamplerate, libsndfile,
-libtool, libvorbis, pkgconfig, qt4, rubberband, stdenv }:
+{ alsaLib, autoconf, automake, dssi, fetchurl, gtk, jackaudio
+, ladspaH, ladspaPlugins, liblo, libmad, libsamplerate, libsndfile
+, libtool, libvorbis, pkgconfig, qt4, rubberband, stdenv }:
 
 stdenv.mkDerivation rec {
   version = "0.5.0";
@@ -11,9 +11,11 @@ stdenv.mkDerivation rec {
     sha256 = "de5991d2d29b2713d73a90ab29efc24db0be68d8e9ca328062d53d229e902e89";
   };
 
-  buildInputs = [ alsaLib autoconf automake dssi gtk jackaudio ladspaH
-    ladspaPlugins liblo libmad libsamplerate libsndfile libtool
-    libvorbis pkgconfig qt4 rubberband ];
+  buildInputs =
+    [ alsaLib autoconf automake dssi gtk jackaudio ladspaH
+      ladspaPlugins liblo libmad libsamplerate libsndfile libtool
+      libvorbis pkgconfig qt4 rubberband
+    ];
 
   meta = with stdenv.lib; {
     description = "Audio/MIDI multi-track sequencer";
