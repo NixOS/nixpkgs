21fd15f4559d5c3bfe76f4842265db23b29e6453
Mathijs Kwik <mathijs@bluescreen303.nl>
2012-04-01 10:49:42 +0000

    mongodb: minor upgrade
    svn path=/nixpkgs/trunk/; revision=33506

--- pkgs/servers/nosql/mongodb/default.nix
+++ pkgs/servers/nosql/mongodb/default.nix
@@ -3,11 +3,11 @@
 assert useV8 -> v8 != null;
 
 stdenv.mkDerivation rec {
-  name = "mongodb-2.0.2";
+  name = "mongodb-2.0.4";
 
   src = fetchurl {
-    url = "http://downloads.mongodb.org/src/mongodb-src-r2.0.2.tar.gz";
-    sha256 = "13xcwaz9rqn0xgh5jijpqxhz1q2qjl8x18bymbl7092k6p29i974";
+    url = "http://downloads.mongodb.org/src/mongodb-src-r2.0.4.tar.gz";
+    sha256 = "1y9qd94qfrp7h1mng7f8hfb36wgj93886d2ifadg8y3pfzr6sab5";
   };
 
   buildInputs = [scons which boost] ++ stdenv.lib.optional useV8 v8;
