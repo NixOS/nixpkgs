4df1d122c45606be2dde869e431e4489f91487c6
Sander van der Burg <s.vanderburg@tudelft.nl>
2012-04-04 22:07:31 +0000

    Bumped revision of DisnixOS
    svn path=/nixpkgs/trunk/; revision=33598

--- pkgs/tools/package-management/disnix/disnixos/default.nix
+++ pkgs/tools/package-management/disnix/disnixos/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, disnix, socat, pkgconfig }:
 
 stdenv.mkDerivation {
-  name = "disnixos-0.2pre33556";
+  name = "disnixos-0.2pre33586";
   
   src = fetchurl {
-    url = http://hydra.nixos.org/build/2363206/download/3/disnixos-0.2pre33556.tar.gz;
-    sha256 = "0fq96cqd0hfa6cwz7phq7ccm935hlcwmjd59vfsp6bwp0wyjzpzl";
+    url = http://hydra.nixos.org/build/2368187/download/3/disnixos-0.2pre33586.tar.gz;
+    sha256 = "110vn4390447dws343py8ss6s8jizx8yg7yl38i64nlqh0bcn4ny";
   };
   
   buildInputs = [ socat pkgconfig disnix ];
