78d9ee096aa5824b8ba192d6dc8d6519b94752f2
Sander van der Burg <s.vanderburg@tudelft.nl>
2012-04-05 10:45:53 +0000

    Bumped revision of disnix
    svn path=/nixpkgs/trunk/; revision=33610

--- pkgs/tools/package-management/disnix/default.nix
+++ pkgs/tools/package-management/disnix/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, dbus_glib, libxml2, libxslt, getopt, nixUnstable, gettext, libiconv }:
 
 stdenv.mkDerivation {
-  name = "disnix-0.3pre30527";
+  name = "disnix-0.3pre32254";
   
   src = fetchurl {
-    url = http://hydra.nixos.org/build/1926928/download/4/disnix-0.3pre30527.tar.gz;
-    sha256 = "1mdcxyrz60nxcyn116i41nhh94r9hacvyilkjyjfiyf8d58pji1y";
+    url = http://hydra.nixos.org/build/2368541/download/4/disnix-0.3pre32254.tar.gz;
+    sha256 = "1jznx4mb6vwpzzpbk4c16j73hjgng7v1nraq8yya7f7m1s2gyhcw";
   };
   
   buildInputs = [ pkgconfig dbus_glib libxml2 libxslt getopt nixUnstable ]
