f04ca020f32edb111ac15cb62b2349bbfbbce34f
Peter Simons <simons@cryp.to>
2012-04-06 18:56:00 +0000

    xlockmore: updated to version 5.38
    svn path=/nixpkgs/trunk/; revision=33652

--- pkgs/misc/screensavers/xlockmore/default.nix
+++ pkgs/misc/screensavers/xlockmore/default.nix
@@ -1,23 +1,22 @@
-{stdenv, fetchurl, pam ? null, x11, freetype}:
+{ stdenv, fetchurl, pam ? null, x11 }:
 
 stdenv.mkDerivation rec {
-  # FIXME: Password authentication doesn't work!
 
-  name = "xlockmore-5.29";
+  name = "xlockmore-5.38";
   src = fetchurl {
     url = "http://www.tux.org/~bagleyd/xlock/${name}/${name}.tar.bz2";
-    sha256 = "47700f74cdd6ada80717358fd9cbb4316a0b2350fd527cfcd1e9b018d3818db2";
+    sha256 = "15x5l43zdjn881xf2m9psz9s2hvd2l8py2kzdhdh1v9m4ml20nf4";
   };
 
-  # Optionally, it can use GTK+ as well.
-  buildInputs = [pam x11 freetype];
+  # Optionally, it can use GTK+.
+  buildInputs = [ pam x11 ];
 
   # The `xlock' program needs to be linked against Glibc's
   # `libgcrypt', which contains `crypt(3)'.
   patches = [ ./makefile-libcrypt.patch ];
 
-  # Don't try to install `xlock' setuid.  Instead, the user should add
-  # it to `security.extraSetuidPrograms'.
+  # Don't try to install `xlock' setuid. Password authentication works
+  # fine via PAM without super user privileges.
   configureFlags =
       " --with-crypt"		# TODO: set --enable-appdefaultdir to a suitable value
     + " --disable-setuid"
@@ -41,7 +40,7 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "Xlockmore, a screen locker for the X Window System.";
-    homepage = http://www.tux.org/~bagleyd/xlockmore.html;
+    homepage = "http://www.tux.org/~bagleyd/xlockmore.html";
     license = "GPL";
   };
 }
