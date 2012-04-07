285af645e67f6e817afeef4f1122b65d1702077f
Cillian de Roiste <cillian.deroiste@gmail.com>
2012-04-07 23:57:30 +0000

    Adding extra requirements, fixing some of the ffado utilities
    svn path=/nixpkgs/trunk/; revision=33664

--- pkgs/os-specific/linux/ffado/default.nix
+++ pkgs/os-specific/linux/ffado/default.nix
@@ -1,9 +1,9 @@
-{stdenv, fetchsvn, dbus_glib, dbus_libs, expat, glibmm, jackaudio
-, libconfig, libiec61883, libraw1394, libxmlxx, pkgconfig, pyqt4, python
-, pythonDBus, qt4, scons }:
+{ stdenv, fetchsvn, dbus, dbus_cplusplus, expat, glibmm, libconfig
+, libavc1394, libiec61883, libraw1394, libxmlxx, makeWrapper, pkgconfig
+, pyqt4, python, pythonDBus, qt4, scons }:
 
 stdenv.mkDerivation rec {
-  name = "libffado-svn";
+  name = "libffado-svn-2117";
 
   src = fetchsvn {
     url = "http://subversion.ffado.org/ffado/trunk/libffado";
@@ -12,18 +12,31 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs =
-    [ dbus_glib dbus_libs expat glibmm jackaudio libconfig libiec61883
-      libraw1394 libxmlxx pkgconfig pyqt4 python pythonDBus qt4 scons
+    [ dbus dbus_cplusplus expat glibmm libavc1394 libconfig
+      libiec61883 libraw1394 libxmlxx makeWrapper pkgconfig pyqt4
+      python pythonDBus qt4 scons
     ];
 
-  buildPhase = "scons";
+  patches = [ ./enable-mixer-and-dbus.patch ];
+
+  preBuild = "export PYLIBSUFFIX=lib/${python.libPrefix}/site-packages";
+
+  # TODO fix ffado-diag, it doesn't seem to use PYPKGDIR
+  buildPhase = "scons PYPKGDIR=$out/$PYLIBSUFFIX";
   installPhase = ''
-    scons PREFIX=$out LIBDIR=$out/lib SHAREDIR=$out/share/libffado install
+    scons PREFIX=$out LIBDIR=$out/lib SHAREDIR=$out/share/libffado \
+      PYPKGDIR=$out/$PYLIBSUFFIX install
+
+    PYDIR=$out/$PYLIBSUFFIX
+    wrapProgram $out/bin/ffado-mixer --prefix PYTHONPATH : \
+      $PYTHONPATH:$PYDIR:${pyqt4}/$LIBSUFFIX:${pythonDBus}/$LIBSUFFIX:
+    wrapProgram $out/bin/ffado-diag --prefix PYTHONPATH : \
+      $PYTHONPATH:$PYDIR:$out/share/libffado/python:${pyqt4}/$LIBSUFFIX:${pythonDBus}/$LIBSUFFIX:
     '';
 
   meta = with stdenv.lib; {
     homepage = http://www.ffado.org;
-    description = "Firewire audio drivers";
+    description = "FireWire audio drivers";
     license = licenses.gpl3;
     maintainers = [ maintainers.goibhniu ];
   };
