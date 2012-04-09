6ef610851eb08d704e826799f7281e06f3a5e090
Mathijs Kwik <mathijs@bluescreen303.nl>
2012-04-09 10:17:20 +0000

    kde okular: include pkgconfig, otherwise libspectre isn't found (.ps viewing)
    svn path=/nixpkgs/trunk/; revision=33676

--- pkgs/desktops/kde-4.7/kdegraphics/okular.nix
+++ pkgs/desktops/kde-4.7/kdegraphics/okular.nix
@@ -1,9 +1,9 @@
 { chmlib, djvulibre, ebook_tools, kde, kdelibs, libspectre, popplerQt4, qca2
-, qimageblitz }:
+, qimageblitz, pkgconfig }:
 
 kde {
   buildInputs =
-    [ chmlib djvulibre ebook_tools kdelibs libspectre popplerQt4 qca2 qimageblitz ];
+    [ chmlib djvulibre ebook_tools kdelibs libspectre popplerQt4 qca2 qimageblitz pkgconfig ];
 
   meta = {
     description = "Okular, the KDE document viewer";
