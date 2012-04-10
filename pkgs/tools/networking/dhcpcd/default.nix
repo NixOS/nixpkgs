e7dcb5c31f14434c699fa3139c532b8811001b0b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-04-10 12:56:00 +0000

    * dhcpcd updated to 5.5.6.
    svn path=/nixpkgs/trunk/; revision=33719

--- pkgs/tools/networking/dhcpcd/default.nix
+++ pkgs/tools/networking/dhcpcd/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "dhcpcd-5.5.5";
+  name = "dhcpcd-5.5.6";
 
   src = fetchurl {
     url = "http://roy.marples.name/downloads/dhcpcd/${name}.tar.bz2";
-    sha256 = "0arypxvyk0hbaln2plc9r4naij8pzj2yhah5rslmdd49sd0h94k8";
+    sha256 = "1rz8n6crw6yh9hzqwdqjpl37v60i1szr7dbhf6gvm374gpf10zv5";
   };
 
   configureFlags = "--sysconfdir=/etc";
