1d0c23bffb6979f76bec910b767052429bdc804b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-04-11 10:05:29 +0000

    * Use a raw disk for the installer test.  This speeds it up by almost   two minutes on my laptop.  Note that due to sparse allocation, raw   disks don't actually take up more space than qcow2 disks (and   they're temporary anyway).
    svn path=/nixos/trunk/; revision=33746

--- lib/test-driver/test-driver.pl
+++ lib/test-driver/test-driver.pl
@@ -143,11 +143,11 @@ sub runTests {
 }
 
 
-# Create an empty qcow2 virtual disk with the given name and size (in
+# Create an empty raw virtual disk with the given name and size (in
 # MiB).
 sub createDisk {
     my ($name, $size) = @_;
-    system("qemu-img create -f qcow2 $name ${size}M") == 0
+    system("qemu-img create -f raw $name ${size}M") == 0
         or die "cannot create image of size $size";
 }
 
