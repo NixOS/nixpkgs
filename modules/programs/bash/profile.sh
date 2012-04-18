6abf3c8a971de1db003fcbf85baaafe7878609e9
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-04-18 10:59:57 +0000

    * Change $NIXPKGS_CONFIG to a better location (/etc/nix instead of   /nix/etc).  Not sure the default profile should be setting this at   all though.
    svn path=/nixos/trunk/; revision=33824

--- modules/programs/bash/profile.sh
+++ modules/programs/bash/profile.sh
@@ -14,7 +14,7 @@ export __ETC_PROFILE_DONE=1
 # Initialise a bunch of environment variables.
 export LOCALE_ARCHIVE=/var/run/current-system/sw/lib/locale/locale-archive
 export LD_LIBRARY_PATH=/var/run/opengl-driver/lib:/var/run/opengl-driver-32/lib # !!! only set if needed
-export NIXPKGS_CONFIG=/nix/etc/config.nix
+export NIXPKGS_CONFIG=/etc/nix/nixpkgs-config.nix
 export NIX_PATH=/nix/var/nix/profiles/per-user/root/channels/nixos:nixpkgs=/etc/nixos/nixpkgs:nixos=/etc/nixos/nixos:nixos-config=/etc/nixos/configuration.nix:services=/etc/nixos/services
 export PAGER="less -R"
 export EDITOR=nano
