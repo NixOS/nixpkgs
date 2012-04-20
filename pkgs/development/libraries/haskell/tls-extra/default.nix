cbbf85d83970ade6ccfcdf5c960e8ebaaf5ccb05
Peter Simons <simons@cryp.to>
2012-04-20 11:35:13 +0000

    haskell-tls-extra: updated to version 0.4.6
    svn path=/nixpkgs/trunk/; revision=33854

--- pkgs/development/libraries/haskell/tls-extra/default.nix
+++ pkgs/development/libraries/haskell/tls-extra/default.nix
@@ -1,16 +1,16 @@
 { cabal, certificate, cryptoApi, cryptocipher, cryptohash, mtl
-, network, text, time, tls, vector
+, network, pem, text, time, tls, vector
 }:
 
 cabal.mkDerivation (self: {
   pname = "tls-extra";
-  version = "0.4.5";
-  sha256 = "0pra0ah086y214hs4zvgkv3p4g6iara27im7x7z1djbj96ikah8a";
+  version = "0.4.6";
+  sha256 = "1xl55i4nr7kyc3qxi8zmq18m0xhlwlrx9fwkck22krshqgq2i6nn";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
-    certificate cryptoApi cryptocipher cryptohash mtl network text time
-    tls vector
+    certificate cryptoApi cryptocipher cryptohash mtl network pem text
+    time tls vector
   ];
   meta = {
     homepage = "http://github.com/vincenthz/hs-tls-extra";
