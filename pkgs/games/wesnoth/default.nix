e81f9de12174e3a681ab5d1b63cf618afca1625b
Karn Kallio <tierpluspluslists@gmail.com>
2012-04-22 05:31:22 +0000

    Advance wesnoth version to 1.10.2; fixes old version failing build.
    svn path=/nixpkgs/trunk/; revision=33874

--- pkgs/games/wesnoth/default.nix
+++ pkgs/games/wesnoth/default.nix
@@ -1,34 +1,34 @@
-{ stdenv, fetchurl, automake, autoconf, SDL, SDL_image, SDL_mixer, SDL_net, SDL_ttf, pango
+{ stdenv, fetchurl, cmake, SDL, SDL_image, SDL_mixer, SDL_net, SDL_ttf, pango
 , gettext, zlib, boost, freetype, libpng, pkgconfig, lua, dbus, fontconfig, libtool
 , fribidi, asciidoc }:
 
 stdenv.mkDerivation rec {
   pname = "wesnoth";
-  version = "1.8.6";
+  version = "1.10.2";
 
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/sourceforge/${pname}/${name}.tar.bz2";
-    sha256 = "10c01ys846zsy831gprdy2nx3qlzv65s4jy99lw6misak3x07rjg";
+    sha256 = "0hawzq85sq0kdddj0s7y6kpx043g1hsl7cvyw1xcxqh45hkgw2ix";
   };
 
   buildInputs = [ SDL SDL_image SDL_mixer SDL_net SDL_ttf pango gettext zlib boost fribidi
-                  automake autoconf freetype libpng pkgconfig lua dbus fontconfig libtool ];
+                  cmake freetype libpng pkgconfig lua dbus fontconfig libtool ];
 
   # The preInstall sed substitution fix errors which I
   # believe arise from autotools version mismatches.  Rather than
   # hunt for the correct automake and autoconf versions these changes
   # make the build work with the versions current in Nixpkgs.
-  preInstall = ''
-    sed -i -e s,@MKINSTALLDIRS@,`pwd`/config/mkinstalldirs, po/*/Makefile
-  '';
+#  preInstall = ''
+#    sed -i -e s,@MKINSTALLDIRS@,`pwd`/config/mkinstalldirs, po/*/Makefile
+#  '';
 
-  configurePhase = ''
-    ./autogen.sh --prefix=$out --with-boost=${boost} \
-                 --with-preferences-dir=.${pname} \
-                 --with-datadir-name=${pname}
-  '';
+#  configurePhase = ''
+#    ./autogen.sh --prefix=$out --with-boost=${boost} \
+#                 --with-preferences-dir=.${pname} \
+#                 --with-datadir-name=${pname}
+#  '';
 
   # Make the package build with the gcc currently available in Nixpkgs.
   NIX_CFLAGS_COMPILE = "-Wno-ignored-qualifiers";
