771697634573f07f7e7372e1552b3e8c7a35488b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-04-24 13:54:33 +0000

    * Linux 3.2.16.
    svn path=/nixpkgs/trunk/; revision=33908

--- pkgs/os-specific/linux/kernel/linux-3.2.nix
+++ pkgs/os-specific/linux/kernel/linux-3.2.nix
@@ -234,7 +234,7 @@ in
 import ./generic.nix (
 
   rec {
-    version = "3.2.15";
+    version = "3.2.16";
     testing = false;
 
     modDirVersion = version;
@@ -245,7 +245,7 @@ import ./generic.nix (
 
     src = fetchurl {
       url = "mirror://kernel/linux/kernel/v3.0/${if testing then "testing/" else ""}linux-${version}.tar.xz";
-      sha256 = "19cqk1da9b1pzwxxj4k1ri38c164160pnjxf4nky2g3xryafl784";
+      sha256 = "0wkfg57xkpnmlcp8g1xkj8jf0x66s3acs9w8snfndcxbw225hqz1";
     };
 
     config = configWithPlatform stdenv.platform;
