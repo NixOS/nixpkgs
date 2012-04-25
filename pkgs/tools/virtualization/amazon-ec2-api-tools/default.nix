b222e1b61ff314313c3ac0d0aeaac6c5e053a535
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-04-25 13:33:25 +0000

    * Updated the EC2 API tools to 1.5.3.0.
    svn path=/nixpkgs/trunk/; revision=33916

--- pkgs/tools/virtualization/amazon-ec2-api-tools/default.nix
+++ pkgs/tools/virtualization/amazon-ec2-api-tools/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, unzip, makeWrapper, jre }:
 
 stdenv.mkDerivation {
-  name = "ec2-api-tools-1.5.2.5";
+  name = "ec2-api-tools-1.5.3.0";
   
   src = fetchurl {
     url = http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip;
-    sha256 = "1p4126hcxz0c1kcrhdlsgx31klh3kv7kp8dn9sgjivmgxgillyyc";
+    sha256 = "0p0rk3bhk35b3dxdy8wjnbbj5zh1va63yfvdk5jxnzdzplds8v65";
   };
 
   buildInputs = [ unzip makeWrapper ];
