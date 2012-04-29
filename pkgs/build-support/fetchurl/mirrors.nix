5c1b0582d24433b498b902befd1f3cc77544283b
Peter Simons <simons@cryp.to>
2012-04-29 18:17:50 +0000

    build-support/fetchurl/mirrors.nix: add funet.fi to more mirrors
    Committed on behalf of Ilja Honkonen <ilja.honkonen@helsinki.fi>.
    
    svn path=/nixpkgs/trunk/; revision=33946

--- pkgs/build-support/fetchurl/mirrors.nix
+++ pkgs/build-support/fetchurl/mirrors.nix
@@ -36,6 +36,7 @@ rec {
     ftp://mirror.cict.fr/gnu/
     ftp://ftp.cs.tu-berlin.de/pub/gnu/
     ftp://ftp.chg.ru/pub/gnu/
+    ftp://ftp.funet.fi/pub/mirrors/ftp.gnu.org/gnu/
   ];
 
   # GCC.
@@ -99,6 +100,7 @@ rec {
     http://ramses.wh2.tu-dresden.de/pub/mirrors/kernel.org/
     http://linux-kernel.uio.no/pub/
     http://kernel.osuosl.org/pub/
+    ftp://ftp.funet.fi/pub/mirrors/ftp.kernel.org/pub/
   ];
 
   # Mirrors of ftp://ftp.kde.org/pub/kde/.
@@ -107,6 +109,7 @@ rec {
     http://ftp.gwdg.de/pub/x11/kde/
     ftp://ftp.heanet.ie/mirrors/ftp.kde.org/
     ftp://ftp.kde.org/pub/kde/
+    ftp://ftp.funet.fi/pub/mirrors/ftp.kde.org/pub/kde/
   ];
 
   # Gentoo files.
@@ -166,6 +169,7 @@ rec {
     ftp://ftp.ru.debian.org/debian/
     ftp://ftp.debian.org/debian/
     http://archive.debian.org/debian-archive/debian/
+    ftp://ftp.funet.fi/pub/mirrors/ftp.debian.org/debian/
   ];
 
   # Ubuntu.
@@ -240,6 +244,7 @@ rec {
     http://mirror.cc.columbia.edu/pub/software/apache/
     http://www.apache.org/dist/
     http://archive.apache.org/dist/ # fallback for old releases
+    ftp://ftp.funet.fi/pub/mirrors/apache.org/
   ];
 
   postgresql = [
