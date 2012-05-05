69c66055b7dd4a67510314b66584decc78a2218b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-05-05 14:42:17 +0000

    * OpenSSH 6.0.
    svn path=/nixpkgs/trunk/; revision=33988

--- pkgs/tools/networking/openssh/default.nix
+++ pkgs/tools/networking/openssh/default.nix
@@ -13,11 +13,11 @@ let
 in
 
 stdenv.mkDerivation rec {
-  name = "openssh-5.9p1";
+  name = "openssh-6.0p1";
 
   src = fetchurl {
     url = "ftp://ftp.nl.uu.net/pub/OpenBSD/OpenSSH/portable/${name}.tar.gz";
-    sha256 = "1mbpfyq3kjc4p8pc631iqgvcqhwhm6vdwvzsdmd54jzhdxmqngld";
+    sha1 = "f691e53ef83417031a2854b8b1b661c9c08e4422";
   };
 
   prePatch = stdenv.lib.optionalString hpnSupport
