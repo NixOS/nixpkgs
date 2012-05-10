e3ccebccec96466761ecedb87c508f4c99a23033
Ludovic Courtès <ludo@gnu.org>
2012-05-10 20:23:45 +0000

    GNU libc 2.13: Skip `dont_use_system_ld_so_cache.patch' on GNU.
    svn path=/nixpkgs/trunk/; revision=34053

--- pkgs/development/libraries/glibc/2.13/common.nix
+++ pkgs/development/libraries/glibc/2.13/common.nix
@@ -75,10 +75,15 @@ stdenv.mkDerivation ({
 
     /* Allow nixos and nix handle the locale-archive. */
     ./nix-locale-archive.patch
+  ]
 
-    /* don't use /etc/ld.so.cache, for non-nixos systems */
-    ./dont_use_system_ld_so_cache.patch
+  ++ (stdenv.lib.optional (hurdHeaders == null)
 
+      /* Don't use /etc/ld.so.cache, for non-NixOS systems.  Currently
+         disabled on GNU/Hurd, which uses a more recent libc snapshot. */
+         ./dont_use_system_ld_so_cache.patch )
+
+  ++ [
     /* Without this patch many KDE binaries crash. */
     ./glibc-elf-localscope.patch
   ] ++ stdenv.lib.optional installLocales ./catalan-firstdays.patch;
