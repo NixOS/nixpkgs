767db7397c1f2a044dd724bd11ec05acdb189e31
Peter Simons <simons@cryp.to>
2012-05-11 13:04:28 +0000

    haskell-packages: downgrade 'mtl' and 'transformers' in GHC 7.4.1 environment to fix build errors
    The new versions of mtl and transformers break many important packages, such as
    monad-par, graphviz, pandoc, and all other packages that depend on any of those.
    This situation causes serious problems for me, because I depend on some of those
    packages for my daily work. IMHO, it is an overreaction to have all those builds
    fail, because some day in the future a new version of Haskell Platform *may* be
    released that *may* recommend the latest versions of 'transformers' and 'mtl'. As
    long as those changes have such profound negative effects on our packages, those
    upgrades should be deferred. This approach seems consistent with the way we've
    handled these matters things in the past, too. For example, we happily break
    conformance with older versions of HP, when those changes are beneficial for
    users. In other words, we have usually valued usability over strict conformance
    before, and IMHO that is a sensible policy.
    
    I agree that it's nice to test what kind of trouble these upgrades cause, but I
    don't believe that 'trunk' is the right place to perform those tests. The breakage
    these changes cause affect users who rely on Nixpkgs to provide a stable working
    environment.
    
    svn path=/nixpkgs/trunk/; revision=34059

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -157,8 +157,8 @@ let result = let callPackage = x : y : modifyPrio (newScope result.final x y);
     zlib         = self.zlib_0_5_3_3;           # 7.4.1 ok
     HTTP         = self.HTTP_4000_2_3;          # 7.4.1 ok
     text         = self.text_0_11_2_0;          # 7.4.1 ok
-    transformers = self.transformers_0_3_0_0;   # 7.4.1 ok
-    mtl          = self.mtl_2_1_1;              # 7.4.1 ok
+    transformers = self.transformers_0_2_2_0;   # 7.4.1 ok
+    mtl          = self.mtl_2_0_1_0;            # 7.4.1 ok
     random       = self.random_1_0_1_1;         # 7.4.1 ok
     cabalInstall = self.cabalInstall_0_14_0;    # 7.4.1 ok
     alex         = self.alex_3_0_1;             # 7.4.1 ok
