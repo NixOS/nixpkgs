e2edc964fd2631bdc7111af54716db07fbb0216c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-05-15 21:13:46 +0000

    * Updated libtiff to 3.9.6 and applied a fix for CVE-2012-1173.
    svn path=/nixpkgs/branches/x-updates/; revision=34124

--- pkgs/development/libraries/libtiff/default.nix
+++ pkgs/development/libraries/libtiff/default.nix
@@ -1,17 +1,30 @@
 { stdenv, fetchurl, zlib, libjpeg }:
 
-stdenv.mkDerivation {
-  name = "libtiff-3.9.4";
+let version = "3.9.6"; in
+
+stdenv.mkDerivation rec {
+  name = "libtiff-${version}";
   
   src = fetchurl {
     urls =
-      [ ftp://ftp.remotesensing.org/pub/libtiff/tiff-3.9.4.tar.gz
-        http://download.osgeo.org/libtiff/tiff-3.9.4.tar.gz
+      [ "ftp://ftp.remotesensing.org/pub/libtiff/tiff-${version}.tar.gz"
+        "http://download.osgeo.org/libtiff/tiff-${version}.tar.gz"
       ];
-    sha256 = "19hxd773yxcs4lxlc3zfdkz5aiv705vj2jvy5srpqkxpbw3nvdv7";
+    sha256 = "0cv8ml3fnkjx60163j69a9cklzlh8wxbvbql78s78q13as8i3fhg";
   };
+
+  patchFlags = "-p0";
+
+  patches =
+    [ (fetchurl {
+        url = "http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/media-libs/tiff/files/tiff-3.9.5-CVE-2012-1173.patch?revision=1.2";
+        sha256 = "07v22lbx9vlqj1f5r2fzcjcr37b97mw5ayjnisgmk4nd1yjxv5qn";
+      })
+    ];
   
   propagatedBuildInputs = [ zlib libjpeg ];
+
+  enableParallelBuilding = true;
 
   meta = {
     description = "Library and utilities for working with the TIFF image file format";
