cd55352e467f2456b117d67f86189c86f608a335
Rickard Nilsson <rickard.nilsson@telia.com>
2012-05-15 21:45:35 +0000

    Updated the Xfce URLs.
    The upstream releases have moved from http://www.xfce.org/archive to
    http://archive.xfce.org.
    
    svn path=/nixpkgs/trunk/; revision=34128

--- pkgs/desktops/xfce-4.6/core/xfce4-settings.nix
+++ pkgs/desktops/xfce-4.6/core/xfce4-settings.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
   name = "xfce4-settings-4.6.5";
   
   src = fetchurl {
-    url = "http://www.xfce.org/archive/xfce/4.6.2/src/${name}.tar.bz2";
+    url = "http://archive.xfce.org/xfce/4.6.2/src/${name}.tar.bz2";
     sha1 = "c036cc2f3100a46b2649e678dff7c0106b219263";
   };
 
