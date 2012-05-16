a9a507eeded5538a2617ea2315dfdabf0e45a6f6
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-05-16 15:08:44 +0000

    * On headless systems, disable the emergency shell and redirect   mountall output to the standard Upstart job log file   (/var/log/upstart/mountall).
    svn path=/nixos/trunk/; revision=34138

--- modules/profiles/headless.nix
+++ modules/profiles/headless.nix
@@ -1,6 +1,10 @@
 # Common configuration for headless machines (e.g., Amazon EC2
 # instances).
 
+{ config, pkgs, ... }:
+
+with pkgs.lib;
+
 {
   sound.enable = false;
   boot.vesa = false;
@@ -10,4 +14,15 @@
 
   # Since we can't manually respond to a panic, just reboot.
   boot.kernelParams = [ "panic=1" "stage1panic=1" ];
+
+  # Since we don't have an (interactive) console, disable the
+  # emergency shell (started if mountall fails).
+  jobs."mount-failed".script = mkOverride 50
+    ''
+      ${pkgs.utillinux}/bin/logger -p user.emerg -t mountall "filesystem ‘$DEVICE’ could not be mounted on ‘$MOUNTPOINT’"
+    '';
+
+  # Likewise, redirect mountall output from the console to the default
+  # Upstart job log file.
+  jobs."mountall".console = mkOverride 50 "";
 }
