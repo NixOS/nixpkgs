503576d27708c1196bed931d78de8ab848aa4246
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-05-16 20:04:56 +0000

    * Pan 0.137.
    svn path=/nixpkgs/trunk/; revision=34139

--- pkgs/applications/networking/newsreaders/pan/default.nix
+++ pkgs/applications/networking/newsreaders/pan/default.nix
@@ -1,21 +1,21 @@
 { spellChecking ? true
 , stdenv, fetchurl, pkgconfig, gtk, gtkspell ? null
-, perl, pcre, gmime, gettext, intltool
+, perl, pcre, gmime, gettext, intltool, dbus_glib, libnotify
 }:
 
 assert spellChecking -> gtkspell != null;
 
-let version = "0.135"; in
+let version = "0.137"; in
 
 stdenv.mkDerivation {
   name = "pan-${version}";
 
   src = fetchurl {
     url = "http://pan.rebelbase.com/download/releases/${version}/source/pan-${version}.tar.bz2";
-    sha1 = "6cd93facf86615761279113badd7462e59399ae4";
+    sha1 = "372f1f6406e2fcd9ce413774730975560f546fcf";
   };
 
-  buildInputs = [ pkgconfig gtk perl gmime gettext intltool ]
+  buildInputs = [ pkgconfig gtk perl gmime gettext intltool dbus_glib libnotify ]
     ++ stdenv.lib.optional spellChecking gtkspell;
 
   enableParallelBuilding = true;
