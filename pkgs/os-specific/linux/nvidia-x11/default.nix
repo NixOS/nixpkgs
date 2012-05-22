a1d64b8082ff494ba5dd6d887257078e9abc671b
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2012-05-22 18:12:46 +0000

    Updating the nvidia 295 driver, so it builds with linux 3.4.
    svn path=/nixpkgs/trunk/; revision=34211

--- pkgs/os-specific/linux/nvidia-x11/default.nix
+++ pkgs/os-specific/linux/nvidia-x11/default.nix
@@ -8,7 +8,7 @@
 
 with stdenv.lib;
 
-let versionNumber = "295.40"; in
+let versionNumber = "295.53"; in
 
 stdenv.mkDerivation {
   name = "nvidia-x11-${versionNumber}${optionalString (!libsOnly) "-${kernel.version}"}";
@@ -19,12 +19,12 @@ stdenv.mkDerivation {
     if stdenv.system == "i686-linux" then
       fetchurl {
         url = "http://us.download.nvidia.com/XFree86/Linux-x86/${versionNumber}/NVIDIA-Linux-x86-${versionNumber}.run";
-        sha256 = "0anffkx1p1yhcflmz6jv9b57xb9mnvqadnjhgwr50fl4vms6bm6c";
+        sha256 = "185hy1d4yixc9drz4r3wgya30zbqcyh2949wfjaqcvxc2ri0jh00";
       }
     else if stdenv.system == "x86_64-linux" then
       fetchurl {
         url = "http://us.download.nvidia.com/XFree86/Linux-x86_64/${versionNumber}/NVIDIA-Linux-x86_64-${versionNumber}-no-compat32.run";
-        sha256 = "0rs9nfkw8f9z52ippadap8r50zrrgldv9d9hdkm8g08cyif5g95w";
+        sha256 = "06fdx3iwqcscwkl14rdb5n1wzscm0gvdxfywr5bzf7y591w4yl7y";
       }
     else throw "nvidia-x11 does not support platform ${stdenv.system}";
 
