4157166f8a4d9471663b9ced3fbb83601e05cc00
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2012-05-25 07:52:12 +0000

    Making the sheevaplug system tarball evaluate fine again. It had some pieces not up to date: fileSystems definition, kernel version, ...
    svn path=/nixos/trunk/; revision=34237

--- modules/installer/cd-dvd/system-tarball-sheevaplug.nix
+++ modules/installer/cd-dvd/system-tarball-sheevaplug.nix
@@ -40,7 +40,7 @@ in
 
 {
   require =
-    [ options
+    [
       ./system-tarball.nix
       ../../hardware/network/rt73.nix
     ];
@@ -120,7 +120,17 @@ in
       # "console=ttyS0,115200n8"  # serial console
     ];
 
-  boot.kernelPackages = pkgs.linuxPackages_2_6_35;
+  boot.kernelPackages = pkgs.linuxPackages_3_4;
+
+  boot.supportedFilesystems = [ "reiserfs" ];
+
+  /* fake entry, just to have a happy stage-1. Users
+     may boot without having stage-1 though */
+  fileSystems = [
+    { mountPoint = "/";
+      device = "/dev/something";
+      }
+  ];
 
   services.mingetty = {
     ttys = [ "ttyS0" ];
@@ -165,6 +175,9 @@ in
   services.openssh.enable = true;
   services.ttyBackgrounds.enable = false;
   jobs.openssh.startOn = pkgs.lib.mkOverrideTemplate 50 {} "";
+
+  # Needed for nixos to evaluate
+  system.build.menuBuilder = "true";
 
   nixpkgs.config = {
     platform = pkgs.platforms.sheevaplug;
