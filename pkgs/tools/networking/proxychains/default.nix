46809b6094391bbc7fe5271ffe54b16676169f76
Evgeny Egorochkin <phreedom.stdin@gmail.com>
2012-05-27 11:28:19 +0000

    ProxyChains: version bump
    svn path=/nixpkgs/trunk/; revision=34252

--- pkgs/tools/networking/proxychains/default.nix
+++ pkgs/tools/networking/proxychains/default.nix
@@ -1,12 +1,12 @@
-{ stdenv, fetchurl } :
+{ stdenv, fetchgit } :
 stdenv.mkDerivation {
-  name = "proxychains-3.1";
-  src = fetchurl {
-    url = mirror://sourceforge/proxychains/proxychains-3.1.tar.gz;
-    sha256 = "9a27657fe9f6e17de9e402ba5c60f9954e7e79fb270c1ef242770f3c01d8515a";
+  name = "proxychains-4.0";
+  src = fetchgit {
+    url = https://github.com/haad/proxychains.git;
+#    sha256 = "9a27657fe9f6e17de9e402ba5c60f9954e7e79fb270c1ef242770f3c01d8515a";
   };
 
-  patchPhase = "sed -e s@libproxychains.so@$out/lib/libproxychains.so@ -i proxychains/proxychains";
+#  patchPhase = "sed -e s@libproxychains.so@$out/lib/libproxychains.so@ -i proxychains/proxychains";
 
   meta = {
     description = "Proxifier for SOCKS proxies.";
