62b353e60541c5100f2c7ff1dbc7a42dd635665b
Evgeny Egorochkin <phreedom.stdin@gmail.com>
2012-05-27 12:37:59 +0000

    proxychains: add revision and hash
    svn path=/nixpkgs/trunk/; revision=34253

--- pkgs/tools/networking/proxychains/default.nix
+++ pkgs/tools/networking/proxychains/default.nix
@@ -1,12 +1,11 @@
 { stdenv, fetchgit } :
 stdenv.mkDerivation {
-  name = "proxychains-4.0";
+  name = "proxychains-4.0.1-head";
   src = fetchgit {
     url = https://github.com/haad/proxychains.git;
-#    sha256 = "9a27657fe9f6e17de9e402ba5c60f9954e7e79fb270c1ef242770f3c01d8515a";
+    rev = "c9b8ce35b24f9d4e80563242b759dff54867163f";
+    sha256 = "4ab73e14c5db6d32d88e0710a9f1b7c9c77b59574a7cf0e9f69f34d8ec9fb643";
   };
-
-#  patchPhase = "sed -e s@libproxychains.so@$out/lib/libproxychains.so@ -i proxychains/proxychains";
 
   meta = {
     description = "Proxifier for SOCKS proxies.";
