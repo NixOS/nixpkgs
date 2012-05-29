71c305740665b667c99debf330367641ca844528
Mathijs Kwik <mathijs@bluescreen303.nl>
2012-05-29 07:43:58 +0000

    mongodb: upgraded to 2.0.5
    svn path=/nixpkgs/trunk/; revision=34267

--- pkgs/servers/nosql/mongodb/default.nix
+++ pkgs/servers/nosql/mongodb/default.nix
@@ -3,11 +3,11 @@
 assert useV8 -> v8 != null;
 
 stdenv.mkDerivation rec {
-  name = "mongodb-2.0.4";
+  name = "mongodb-2.0.5";
 
   src = fetchurl {
-    url = "http://downloads.mongodb.org/src/mongodb-src-r2.0.4.tar.gz";
-    sha256 = "1y9qd94qfrp7h1mng7f8hfb36wgj93886d2ifadg8y3pfzr6sab5";
+    url = "http://downloads.mongodb.org/src/mongodb-src-r2.0.5.tar.gz";
+    sha256 = "0vnwphjn0iqgjrvfk18vridx5324zmmbrapp2d9rbqc9xg6jrpav";
   };
 
   buildInputs = [scons which boost] ++ stdenv.lib.optional useV8 v8;
