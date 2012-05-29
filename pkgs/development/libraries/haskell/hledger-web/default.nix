bb2d3311a257d53a4ef59c179fc50d5d3261eecc
Peter Simons <simons@cryp.to>
2012-05-29 20:16:06 +0000

    haskell-hledger-web: updated to version 0.18
    svn path=/nixpkgs/trunk/; revision=34282

--- pkgs/development/libraries/haskell/hledger-web/default.nix
+++ pkgs/development/libraries/haskell/hledger-web/default.nix
@@ -1,23 +1,21 @@
-{ cabal, aeson, blazeHtml, cabalFileTh, clientsession, cmdargs
-, dataObject, dataObjectYaml, failure, fileEmbed, filepath, hamlet
-, hledger, hledgerLib, httpEnumerator, HUnit, ioStorage, parsec
-, regexpr, safe, shakespeareCss, shakespeareJs, shakespeareText
-, text, time, tlsExtra, transformers, wai, waiExtra, warp, yesod
-, yesodCore, yesodForm, yesodJson, yesodStatic
+{ cabal, blazeHtml, cabalFileTh, clientsession, cmdargs, filepath
+, hamlet, hledger, hledgerLib, HUnit, ioStorage, networkConduit
+, parsec, regexpr, safe, shakespeareText, text, time, transformers
+, wai, waiExtra, warp, yaml, yesod, yesodCore, yesodDefault
+, yesodStatic
 }:
 
 cabal.mkDerivation (self: {
   pname = "hledger-web";
-  version = "0.17.1";
-  sha256 = "0cix0k2skbrcbjhj876cx4mizjvkqnr91s092a3lg4rv40dhgwa9";
+  version = "0.18";
+  sha256 = "1cxlyw9rs1pg0ympig9svkfi9fikpbvfzm6jc7hijkg215l617np";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
-    aeson blazeHtml cabalFileTh clientsession cmdargs dataObject
-    dataObjectYaml failure fileEmbed filepath hamlet hledger hledgerLib
-    httpEnumerator HUnit ioStorage parsec regexpr safe shakespeareCss
-    shakespeareJs shakespeareText text time tlsExtra transformers wai
-    waiExtra warp yesod yesodCore yesodForm yesodJson yesodStatic
+    blazeHtml cabalFileTh clientsession cmdargs filepath hamlet hledger
+    hledgerLib HUnit ioStorage networkConduit parsec regexpr safe
+    shakespeareText text time transformers wai waiExtra warp yaml yesod
+    yesodCore yesodDefault yesodStatic
   ];
   meta = {
     homepage = "http://hledger.org";
