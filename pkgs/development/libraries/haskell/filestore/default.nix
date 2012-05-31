0deabf5eaa80e049aedecc87b0601f51f4e1f306
Peter Simons <simons@cryp.to>
2012-05-31 07:24:40 +0000

    haskell-filestore: updated to version 0.5
    svn path=/nixpkgs/trunk/; revision=34295

--- pkgs/development/libraries/haskell/filestore/default.nix
+++ pkgs/development/libraries/haskell/filestore/default.nix
@@ -4,15 +4,14 @@
 
 cabal.mkDerivation (self: {
   pname = "filestore";
-  version = "0.4.2";
-  sha256 = "1zv5c1r82a77p6dadabj8853a0z7p8qrk0fdxvr9sr02zd95cg16";
+  version = "0.5";
+  sha256 = "0cppm8iksz4dnh4kafyfy0cqbidw83rdpgc1mksiwh9c9gaxrlq7";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
     Diff filepath HUnit mtl parsec split time utf8String xml
   ];
   meta = {
-    homepage = "http://johnmacfarlane.net/repos/filestore";
     description = "Interface for versioning file stores";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
