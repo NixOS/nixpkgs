b89f433951358209d30bd2aecd3403c152c9f36b
Peter Simons <simons@cryp.to>
2012-06-03 18:52:43 +0000

    haskell-reactive-banana-wx: re-generated expression with the latest version of Cabal2Nix
    The package won't compile when 'buildExamples' is enabled. First of all,
    it depends on an ancient version of the 'executable-path' library. That
    problem can be remedied by adding
    
      patchPhase = ''
        sed -i -e 's|executable-path == 0.0.\*|executable-path|' reactive-banana-wx.cabal
      '';
    
    ..., but even then it won't build because the Cabal file doesn't declare
    all dependencies properly. Both GHC 7.0.4 and 7.4.1 fail complaining
    about the absence of Prelude.
    
    svn path=/nixpkgs/trunk/; revision=34328

--- pkgs/development/libraries/haskell/reactive-banana-wx/default.nix
+++ pkgs/development/libraries/haskell/reactive-banana-wx/default.nix
@@ -1,22 +1,18 @@
-{ cabal, cabalMacosx, reactiveBanana, wx, wxcore, buildExamples ? false, executablePath ? null}:
+{ cabal, cabalMacosx, reactiveBanana, wx, wxcore }:
 
-assert buildExamples -> executablePath != null;
-
-cabal.mkDerivation (self:
-  let lib = self.stdenv.lib;
-  in
-  {
+cabal.mkDerivation (self: {
   pname = "reactive-banana-wx";
   version = "0.6.0.0";
   sha256 = "1pxcymh6xpmbkbc8i2hvjbki9s81mx69wrp8nl1i0y4pppzi8ihp";
   isLibrary = true;
   isExecutable = true;
-  buildDepends = [ cabalMacosx reactiveBanana wx wxcore ] ++ lib.optional buildExamples executablePath;
-  configureFlags = lib.optionalString buildExamples "-fbuildExamples";
+  buildDepends = [ cabalMacosx reactiveBanana wx wxcore ];
+  configureFlags = "-f-buildExamples";
   meta = {
     homepage = "http://haskell.org/haskellwiki/Reactive-banana";
     description = "Examples for the reactive-banana library, using wxHaskell";
-    license = lib.licenses.bsd3;
+    license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    maintainers = [ self.stdenv.lib.maintainers.andres ];
   };
 })
