73a1d5e27face256c81af586de52b0062b5ce948
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-06-05 15:24:16 +0000

    * PostgreSQL updates.
    svn path=/nixpkgs/trunk/; revision=34361

--- pkgs/servers/sql/postgresql/8.4.x.nix
+++ pkgs/servers/sql/postgresql/8.4.x.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, zlib, ncurses, readline }:
 
-let version = "8.4.11"; in
+let version = "8.4.12"; in
 
 stdenv.mkDerivation rec {
   name = "postgresql-${version}";
   
   src = fetchurl {
     url = "mirror://postgresql/source/v${version}/${name}.tar.bz2";
-    sha256 = "13ww30xv2pwd5hy1x5lsmba4jc59n6f6nz0dc29bb0k2s7qrzg2v";
+    sha256 = "0xqgzi6qrx4g7g8f38hp02qj8grrfnvc33kyijc2hf0qxhqb7dwr";
   };
 
   buildInputs = [ zlib ncurses readline ];
