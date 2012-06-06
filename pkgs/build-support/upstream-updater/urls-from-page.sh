43bc3880f6ebe557b31b378ae07b37d7e9cb370d
Michael Raskin <7c6f434c@mail.ru>
2012-06-06 13:26:48 +0000

    Process relative URLs
    svn path=/nixpkgs/trunk/; revision=34367

--- pkgs/build-support/upstream-updater/urls-from-page.sh
+++ pkgs/build-support/upstream-updater/urls-from-page.sh
@@ -4,9 +4,11 @@ url="$1"
 protocol="${url%%:*}"
 path="${url#$protocol://}"
 server="${path%%/*}"
+basepath="${path%/*}"
 relpath="${path#$server}"
  
 echo "URL: $url" >&2
 
 curl -L -k "$url" | sed -re 's/^/-/;s/[hH][rR][eE][fF]="([^"]*)"/\n+\1\n-/g' | \
-  sed -e '/^-/d; s/^[+]//; /^#/d;'"s/^\\//$protocol:\\/\\/$server\\//g"
+  sed -e '/^-/d; s/^[+]//; /^#/d;'"s/^\\//$protocol:\\/\\/$server\\//g" | \
+  sed -re 's~^[^:]*$~'"$protocol://$basepath/&~"
