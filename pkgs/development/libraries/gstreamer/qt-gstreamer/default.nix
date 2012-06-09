1629524bb23cf412e364b8718735aebe53b16f35
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-06-09 13:09:33 +0000

    qt-gstreamer-0.10.2
    svn path=/nixpkgs/trunk/; revision=34401

--- pkgs/development/libraries/gstreamer/qt-gstreamer/default.nix
+++ pkgs/development/libraries/gstreamer/qt-gstreamer/default.nix
@@ -2,17 +2,19 @@
 , automoc4, flex, bison, pkgconfig }:
 
 stdenv.mkDerivation rec {
-  name = "${pname}-0.10.1";
+  name = "${pname}-0.10.2";
   pname = "qt-gstreamer";
 
   src = fetchurl {
     url = "http://gstreamer.freedesktop.org/src/${pname}/${name}.tar.bz2";
-    sha256 = "0g377jlzlwgywgk7nbv9fd0aimv8wpzrymwzdiaffczxv5xvip5h";
+    sha256 = "1laryg5vgc3prdi7dmpfwa71nsrc3ngv27sgax44c4qfrgpsgwbf";
   };
 
   buildInputs = [ gstreamer gst_plugins_base glib qt4 ];
   propagatedBuildInputs = [ boost ];
   buildNativeInputs = [ cmake automoc4 flex bison pkgconfig ];
+
+  cmakeFlags = "-DUSE_QT_PLUGIN_DIR=OFF -DUSE_GST_PLUGIN_DIR=OFF";
 
   patches = [ ./boost1.48.patch ];
 }
