c9f95fa0c2bc07936748f76aa11ea0f808571075
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-06-10 17:06:35 +0000

    * net-snmp: enable Perl support.
    svn path=/nixpkgs/trunk/; revision=34424

--- pkgs/tools/networking/net-snmp/default.nix
+++ pkgs/tools/networking/net-snmp/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, file, openssl }:
+{ stdenv, fetchurl, file, openssl, perl }:
 
 stdenv.mkDerivation rec {
   name = "net-snmp-5.7.1";
@@ -8,6 +8,13 @@ stdenv.mkDerivation rec {
     sha256 = "07qqdgs3flraqccwry4a4x23jcg6vfi0rqj7clsibdv51ijwjwbw";
   };
 
+  preConfigure =
+    ''
+      perlversion=$(perl -e 'use Config; print $Config{version};')
+      perlarchname=$(perl -e 'use Config; print $Config{archname};')
+      installFlags="INSTALLSITEARCH=$out/lib/perl5/site_perl/$perlversion/$perlarchname INSTALLSITEMAN3DIR=$out/share/man/man3"
+    '';
+
   configureFlags =
     [ "--with-default-snmp-version=3"
       "--with-sys-location=Unknown"
@@ -16,7 +23,7 @@ stdenv.mkDerivation rec {
       "--with-persistent-directory=/var/lib/net-snmp"
     ];
 
-  buildInputs = [ file openssl ];
+  buildInputs = [ file openssl perl ];
 
   enableParallelBuilding = true;
 
