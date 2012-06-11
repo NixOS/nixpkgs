ac3df043d635333d59662e553d659199f6cb25dd
Peter Simons <simons@cryp.to>
2012-06-11 17:15:52 +0000

    haskell-happstack-data: obsolete package removed
    svn path=/nixpkgs/trunk/; revision=34463

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -770,8 +770,6 @@ let result = let callPackage = x : y : modifyPrio (newScope result.final x y);
 
   hamlet = callPackage ../development/libraries/haskell/hamlet {};
 
-  happstackData = callPackage ../development/libraries/haskell/happstack/happstack-data.nix {};
-
   happstackUtil = callPackage ../development/libraries/haskell/happstack/happstack-util.nix {};
 
   happstackServer = callPackage ../development/libraries/haskell/happstack/happstack-server.nix {};
