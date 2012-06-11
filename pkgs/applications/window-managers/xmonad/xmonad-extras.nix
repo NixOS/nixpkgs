edb10988b35042873a9d45f7492fc773696f4c3b
Peter Simons <simons@cryp.to>
2012-06-11 14:55:56 +0000

    xmonad: fixed build with GHC 7.4.1
    svn path=/nixpkgs/trunk/; revision=34457

--- pkgs/applications/window-managers/xmonad/xmonad-extras.nix
+++ pkgs/applications/window-managers/xmonad/xmonad-extras.nix
@@ -1,5 +1,5 @@
-{ cabal, hint, HList, libmpd, mtl, network, parsec, random
-, regexPosix, split, X11, xmonad, xmonadContrib
+{ cabal, hint, libmpd, mtl, network, parsec, random, regexPosix
+, split, X11, xmonad, xmonadContrib
 }:
 
 cabal.mkDerivation (self: {
@@ -7,9 +7,10 @@ cabal.mkDerivation (self: {
   version = "0.10.1";
   sha256 = "17rac0xjw1zw1jlc1rpq54vg50xscb3b98knk4gkb8bv1khpgz27";
   buildDepends = [
-    hint HList libmpd mtl network parsec random regexPosix split X11
-    xmonad xmonadContrib
+    hint libmpd mtl network parsec random regexPosix split X11 xmonad
+    xmonadContrib
   ];
+  configureFlags = "-f-with_hlist";
   meta = {
     homepage = "http://projects.haskell.org/xmonad-extras";
     description = "Third party extensions for xmonad with wacky dependencies";
