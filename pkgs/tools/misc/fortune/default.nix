e0b38f88db9ca6f1fadda29965439315926ba44b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-06-12 23:41:05 +0000

    * Fortune: use a "-" instead of a "_" to separate the package name   from the version.  Otherwise "nix-env -i fortune-mod" doesn't work.   (Reported on IRC.)
    svn path=/nixpkgs/trunk/; revision=34487

--- pkgs/tools/misc/fortune/default.nix
+++ pkgs/tools/misc/fortune/default.nix
@@ -1,20 +1,24 @@
 { stdenv, fetchurl, recode }:
 
 stdenv.mkDerivation {
-  name = "fortune-mod_1.99.1";
+  name = "fortune-mod-1.99.1";
+  
   src = fetchurl {
     url = http://ftp.de.debian.org/debian/pool/main/f/fortune-mod/fortune-mod_1.99.1.orig.tar.gz;
     sha256 = "1kpa2hgbglj5dbfasvl9wc1q3xpl91mqn3sfby46r4rwyzhswlgw";
   };
-  buildInputs = [recode];
+  
+  buildInputs = [ recode ];
+  
   preConfigure = ''
     sed -i "s|/usr/|$out/|" Makefile 
   '';
+  
   postInstall = ''
     ln -s $out/games/fortune $out/bin/fortune
   '';
 
   meta = {
-    description = "fortune is a simple program that displays a pseudorandom message from a database of quotations that first appeared in Version 7 Unix.";
+    description = "A program that displays a pseudorandom message from a database of quotations";
   };
 }
