dc32b4caef14a232629f453b4e396bfa2fa0d326
aszlig <aszlig@redmoonstudios.org>
2012-06-15 10:27:14 +0200

    chromium: Generate the latest source.nix.

---
+++ pkgs/applications/networking/browsers/chromium/source.nix
@@ -0,0 +1,5 @@
+{
+  version = "21.0.1174.1";
+  url = "http://commondatastorage.googleapis.com/chromium-browser-official/chromium-21.0.1174.1.tar.bz2";
+  sha256 = "00jd3lzdbxm4rlqvxf0wfz9pvsza85rhlb0pzdzrdjy45kn06a75";
+}
