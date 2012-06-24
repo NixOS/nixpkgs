061a9988405e280757c4eea3730647ada90edb36
Mathijs Kwik <mathijs@bluescreen303.nl>
2012-06-24 10:06:28 +0200

    luks root: c-style for-loop -> seq
    The ash shell no longer supports this bash-specific syntax.
    This left systems that use luksroot unable to boot.

--- modules/system/boot/luksroot.nix
+++ modules/system/boot/luksroot.nix
@@ -11,7 +11,7 @@ let
     # available as a function.
     if ! test -e ${device}; then
         echo -n "waiting 10 seconds for device ${device} to appear..."
-        for ((try = 0; try < 10; try++)); do
+        for try in $(seq 0 9); do
             sleep 1
             if test -e ${device}; then break; fi
             echo -n .
