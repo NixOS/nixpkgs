dbe2325603420a89aa030994138bb54167fab81d
David Guibert <david.guibert@gmail.com>
2012-06-27 20:41:07 +0200

    fix the grep pattern finding programs called by absolute paths in udev rules.

--- modules/services/hardware/udev.nix
+++ modules/services/hardware/udev.nix
@@ -73,7 +73,7 @@ let
       echo "OK"
 
       echo -n "Checking that all programs call by absolute paths in udev rules exist ... "
-      import_progs=$(grep 'IMPORT{program}="/' $out/* |
+      import_progs=$(grep 'IMPORT{program}="\/' $out/* |
         sed -e 's/.*IMPORT{program}="\([^ "]*\)[ "].*/\1/' | uniq)
       run_progs=$(grep -v '^[[:space:]]*#' $out/* | grep 'RUN+="/' |
         sed -e 's/.*RUN+="\([^ "]*\)[ "].*/\1/' | uniq)
