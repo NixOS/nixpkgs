4608426fbadc6de97480f7e38bace4f2ace06c51
Cillian de Róiste <goibhniu@fsfe.org>
2012-07-01 22:12:38 +0200

    Fix compilation error when using boost 1.5 by pinning the boost version to 1.49

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6281,6 +6281,7 @@ let
 
   ardour3 =  lowPrio (callPackage ../applications/audio/ardour/ardour3.nix {
     inherit (gnome) libgnomecanvas libgnomecanvasmm;
+    boost = boost149;
   });
 
   arora = callPackage ../applications/networking/browsers/arora { };
