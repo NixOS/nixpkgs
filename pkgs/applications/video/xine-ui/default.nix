8a764b7891cbb8d4753c420254608814de428905
Lluís Batlle i Rossell <viric@viric.name>
2012-07-02 23:01:00 +0200

    xine-ui: update to 0.99.7

--- pkgs/applications/video/xine-ui/default.nix
+++ pkgs/applications/video/xine-ui/default.nix
@@ -1,18 +1,18 @@
-{stdenv, fetchurl, pkgconfig, xlibs, xineLib, libpng12, readline, ncurses, curl
-, lirc, shared_mime_info }:
+{stdenv, fetchurl, pkgconfig, xlibs, libpng, xineLib, readline, ncurses, curl
+, lirc, shared_mime_info, libjpeg }:
 
 stdenv.mkDerivation rec {
-  name = "xine-ui-0.99.6";
+  name = "xine-ui-0.99.7";
   
   src = fetchurl {
     url = "mirror://sourceforge/xine/${name}.tar.xz";
-    sha256 = "1wwylnckm5kfq5fi154w8jqf5cwvp7c1ani15q7sgfrfdkmy7caf";
+    sha256 = "1i3byriqav06b55kwzs4dkzrjw7mmmcv0rc7jzb52hn8qp8xz34x";
   };
   
   buildNativeInputs = [ pkgconfig shared_mime_info ];
 
   buildInputs =
-    [ xineLib libpng12 readline ncurses curl lirc
+    [ xineLib libpng readline ncurses curl lirc libjpeg
       xlibs.xlibs xlibs.libXext xlibs.libXv xlibs.libXxf86vm xlibs.libXtst xlibs.inputproto
       xlibs.libXinerama xlibs.libXi xlibs.libXft
     ];
