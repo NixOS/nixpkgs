950c73271ae35d4211f4f64d834c059f941084b7
aszlig <aszlig@redmoonstudios.org>
2012-07-04 14:14:00 +0200

    chromium: Remove obsolete source.nix.
    As already promised, the old single-channel source.nix is now obsolete as we're
    using Omahaproxy now and the build of the stable version finishes successful and
    the browser runs fine.

--- pkgs/applications/networking/browsers/chromium/source.nix
+++
@@ -1,5 +0,0 @@
-{
-  version = "21.0.1179.1";
-  url = "http://commondatastorage.googleapis.com/chromium-browser-official/chromium-21.0.1179.1.tar.bz2";
-  sha256 = "1ynm1dv8nwjg6a0absid1g3r62y0mpb74pmal8g9nmqb92rlkdnc";
-}
