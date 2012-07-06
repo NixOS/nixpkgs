5cf889967374a49f0af8eb974c1d14b255e4fa14
aszlig <aszlig@redmoonstudios.org>
2012-07-06 03:33:52 +0200

    xpra: Don't include cython into store requisites.
    Cython is not required in order to run XPRA, so we now explicitly specify what
    should be put into PYTHONPATH instead of using the PYTHONPATH which is set
    during build time.
    
    That way we don't get unnecessary stuff in /nix/store, like the mentioned
    cython compiler/interpreter.

--- pkgs/tools/X11/xpra/default.nix
+++ pkgs/tools/X11/xpra/default.nix
@@ -1,6 +1,8 @@
-{stdenv, fetchurl, pkgconfig, python, cython, pygtk, xlibs, gtk, ffmpeg, x264, libvpx, makeWrapper}:
+{ stdenv, fetchurl, pkgconfig, python, cython
+, pygtk, pygobject, pycairo, xlibs, gtk
+, ffmpeg, x264, libvpx, makeWrapper}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "xpra-0.3.2";
   
   src = fetchurl {
@@ -8,8 +10,10 @@ stdenv.mkDerivation {
     sha256 = "1s1z6r0r78qvf59ci3vxammjz7lj5m64jyk0bfn7yxd5jl3sy41y";
   };
 
+  buildNativeInputs = [ cython ];
+
   buildInputs = [
-    pkgconfig python cython pygtk gtk ffmpeg x264 libvpx makeWrapper
+    pkgconfig python pygtk gtk ffmpeg x264 libvpx makeWrapper
     xlibs.inputproto xlibs.libXcomposite xlibs.libXdamage xlibs.libXtst
   ];
 
@@ -19,13 +23,20 @@ stdenv.mkDerivation {
     ./do-build
   '';
 
+  pythonPaths = [
+    "$out/lib/python"
+    "$(toPythonPath ${pygtk})/gtk-2.0"
+  ] ++ map (i: "$(toPythonPath ${i})") [
+    pygobject pycairo notify
+  ];
+
   installPhase = ''
     mkdir -p $out
     cp -r install/* $out
 
     for i in $(cd $out/bin && ls); do
         wrapProgram $out/bin/$i \
-            --set PYTHONPATH "$out/lib/python:$(toPythonPath ${pygtk})/gtk-2.0:$PYTHONPATH" \
+            --set PYTHONPATH "${stdenv.lib.concatStringsSep ":" pythonPaths}" \
             --prefix PATH : "${xlibs.xauth}/bin:${xlibs.xorgserver}/bin:${xlibs.xmodmap}/bin"
     done
   '';
