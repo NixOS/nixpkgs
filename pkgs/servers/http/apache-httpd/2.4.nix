32449b5ae6ac66a5bd0470ca81c80650620116fe
Peter Simons <simons@cryp.to>
2012-07-07 08:57:53 +0200

    apache-httpd-2.4: replace use of tabs with blanks

--- pkgs/servers/http/apache-httpd/2.4.nix
+++ pkgs/servers/http/apache-httpd/2.4.nix
@@ -23,8 +23,8 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [perl] ++
-    optional ldapSupport openldap ++	# there is no --with-ldap flag
-    optional libxml2Support libxml2;	# there is --with-libxml2, but it doesn't work
+    optional ldapSupport openldap ++    # there is no --with-ldap flag
+    optional libxml2Support libxml2;    # there is --with-libxml2, but it doesn't work
 
   # Required for ‘pthread_cancel’.
   NIX_LDFLAGS = "-lgcc_s";
