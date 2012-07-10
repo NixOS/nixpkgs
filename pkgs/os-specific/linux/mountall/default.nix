cfb08fc6002670aaa02c2abc5315156c17e93219
Peter Simons <simons@cryp.to>
2012-07-10 22:20:12 +0200

    mountall: strip trailing whitespace

--- pkgs/os-specific/linux/mountall/default.nix
+++ pkgs/os-specific/linux/mountall/default.nix
@@ -1,8 +1,8 @@
 { stdenv, fetchurl, pkgconfig, libnih, dbus, udev, autoconf, automake, libtool, gettext }:
-   
+
 stdenv.mkDerivation {
   name = "mountall-2.35";
-   
+
   src = fetchurl {
     url = https://launchpad.net/ubuntu/+archive/primary/+files/mountall_2.35.tar.gz;
     sha256 = "1k52d4x75balnwcsqgznvzrdqgbp2dqnrzw0n25kajdcwr192wwy";
@@ -15,10 +15,10 @@ stdenv.mkDerivation {
   buildInputs = [ pkgconfig libnih dbus.libs udev autoconf automake libtool gettext ];
 
   makeFlags = "initramfshookdir=$(out)/share/initramfs-tools/hooks upstart_jobs_initramfs_configdir=$(out)/share/initramfs-tools/event-driven/upstart-jobs";
-  
+
   meta = {
     homepage = https://launchpad.net/ubuntu/+source/mountall;
     description = "Utility to mount all filesystems and emit Upstart events";
-    platforms = stdenv.lib.platforms.linux;    
+    platforms = stdenv.lib.platforms.linux;
   };
 }
