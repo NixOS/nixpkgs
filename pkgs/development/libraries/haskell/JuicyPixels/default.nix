b9e1a3f109806809f1a845c9cc32663276460582
Alexander Foremny <alexanderforemny@gmail.com>
2012-07-11 21:55:39 +0200

    Add JuicyPixels Haskell package
    Forgot to attach the Nix expression for that package. It is generated by
    cabal2nix and works fine on my machine (i686, ghc7.4.1).

---
+++ pkgs/development/libraries/haskell/JuicyPixels/default.nix
@@ -0,0 +1,18 @@
+{ cabal, cereal, deepseq, mtl, primitive, transformers, vector
+, zlib
+}:
+
+cabal.mkDerivation (self: {
+  pname = "JuicyPixels";
+  version = "1.3";
+  sha256 = "07wljfag4ylw16wdi7znjb61pfihdik5d7p4h2lmz6xirm4mjzrm";
+  buildDepends = [
+    cereal deepseq mtl primitive transformers vector zlib
+  ];
+  meta = {
+    homepage = "https://github.com/Twinside/Juicy.Pixels";
+    description = "Picture loading/serialization (in png, jpeg and bitmap)";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+  };
+})
