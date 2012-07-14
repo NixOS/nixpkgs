f405ab64670c6229035b4b0fc3305c4f5a1d31bf
Peter Simons <simons@cryp.to>
2012-07-14 11:14:39 +0200

    haskell-largeword: update to version 1.0.2

--- pkgs/development/libraries/haskell/largeword/default.nix
+++ pkgs/development/libraries/haskell/largeword/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "largeword";
-  version = "1.0.1";
-  sha256 = "0kkkck220wap3ad2k6461ylhshiqbizv0qh35i2325fhqx892gyr";
+  version = "1.0.2";
+  sha256 = "0ylbl9rffhqw9ahasn23n00h7v5yqmghmqkrq446zdr72bq23hk2";
   meta = {
     homepage = "http://trac.haskell.org/largeword/wiki";
     description = "Provides Word128, Word192 and Word256 and a way of producing other large words if required";
