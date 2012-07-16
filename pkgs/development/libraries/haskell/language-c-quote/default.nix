3be2d8d9cd3fc765c98d4a517e082d40aee07134
Peter Simons <simons@cryp.to>
2012-07-16 11:21:11 +0200

    haskell-language-c-quote: update to version 0.4.0

--- pkgs/development/libraries/haskell/language-c-quote/default.nix
+++ pkgs/development/libraries/haskell/language-c-quote/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "language-c-quote";
-  version = "0.3.2";
-  sha256 = "1pxrag9ws93rsqrsxcyxg07pvp2qw0f19yj2b40ynwzk112nkd16";
+  version = "0.4.0";
+  sha256 = "1qbl81c8fw2gz96n8505yxhw1d3zhxq436ikv173x1zz0b4z26g1";
   buildDepends = [
     exceptionMtl exceptionTransformers filepath haskellSrcMeta
     mainlandPretty mtl srcloc syb symbol
