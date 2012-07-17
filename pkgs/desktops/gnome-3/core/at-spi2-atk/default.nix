cd7027bdaecda32b83805b0f336bbfee0f2fadc2
Antono Vasiljev <self@antono.info>
2012-07-18 01:46:29 +0300

    GNOME3: at-spi2-atk (TODO: deal with gschemas.compiled conflict)

--- pkgs/desktops/gnome-3/core/at-spi2-atk/default.nix
+++ pkgs/desktops/gnome-3/core/at-spi2-atk/default.nix
@@ -1,14 +1,17 @@
 { stdenv, fetchurl, python, pkgconfig, popt, atk, libX11, libICE, xlibs, libXi
-, intltool, dbus_glib }:
+, intltool, dbus_glib, gnome3, libSM }:
 
-stdenv.mkDerivation {
-  name = "at-spi2-atk-2.5.3";
+stdenv.mkDerivation rec {
+  versionMajor = "2.5";
+  versionMinor = "4";
+  moduleName   = "at-spi2-atk";
+  name = "${moduleName}-${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = mirror://gnome/sources/at-spi2-atk/2.5/at-spi2-atk-2.5.3.tar.xz;
-    sha256 = "16y6q0v3va7r77ns1r6w4mg3rvyxmnyzx1b3n0wqjzmqkd8avgmx";
+    url = "mirror://gnome/sources/${moduleName}/${versionMajor}/${name}.tar.xz";
+    sha256 = "1bl5jl644slf5mmnr4z7xj433prvjjpgmv9rdmxdny23j08qa8vs";
   };
 
   buildInputs = [ python pkgconfig popt atk libX11 libICE xlibs.libXtst libXi
-                  intltool dbus_glib ];
+                  intltool dbus_glib gnome3.at_spi2_core libSM ];
 }
