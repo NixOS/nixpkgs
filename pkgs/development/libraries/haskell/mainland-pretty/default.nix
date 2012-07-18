bdc0530f93b757a1e22eb73a567665d1b08cd583
Peter Simons <simons@cryp.to>
2012-07-18 16:04:55 +0200

    Revert updates to srcloc, mainland-pretty, and language-c-quote to un-break accelerate-cuda.

--- pkgs/development/libraries/haskell/mainland-pretty/default.nix
+++ pkgs/development/libraries/haskell/mainland-pretty/default.nix
@@ -1,10 +1,10 @@
-{ cabal, srcloc, text }:
+{ cabal, srcloc, symbol }:
 
 cabal.mkDerivation (self: {
   pname = "mainland-pretty";
-  version = "0.2.1";
-  sha256 = "1pl96m92bsrghkp5ixaqlhi2hybc4hafy64zlmsgjlmdvslxhs3h";
-  buildDepends = [ srcloc text ];
+  version = "0.1.3.0";
+  sha256 = "1grfsn066z11737dxdk1bdhyvz9vjxxn6krfgx9bc8jin6n9h6aq";
+  buildDepends = [ srcloc symbol ];
   meta = {
     homepage = "http://www.eecs.harvard.edu/~mainland/";
     description = "Pretty printing designed for printing source code";
