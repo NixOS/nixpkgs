5750566c2af11d31a705fb687cf6a0ce300b5252
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-07-18 09:49:27 -0400

    Fix Pyrex build

--- pkgs/development/interpreters/pyrex/0.9.5.nix
+++ pkgs/development/interpreters/pyrex/0.9.5.nix
@@ -1,26 +1,19 @@
-{ stdenv, fetchurl, builderDefs, python }:
+{ stdenv, fetchurl, pythonPackages }:
 
-let
+let version = "0.9.5.1.1"; in
 
-  localDefs = builderDefs.passthru.function {
+pythonPackages.buildPythonPackage rec {
+  name = "pyrex-${version}";
 
-    src = fetchurl {
-      url = http://www.cosc.canterbury.ac.nz/greg.ewing/python/Pyrex/oldtar/Pyrex-0.9.5.1.1.tar.gz;
-      sha256 = "0lxxvn4mjfb83swcbqb5908q4iy53w4ip5i0f9angm2va1jyhd3z";
-    };
+  src = fetchurl {
+    url = "http://www.cosc.canterbury.ac.nz/greg.ewing/python/Pyrex/oldtar/Pyrex-${version}.tar.gz";
+    sha256 = "0lxxvn4mjfb83swcbqb5908q4iy53w4ip5i0f9angm2va1jyhd3z";
+  };
 
-    buildInputs = [python];
+  doCheck = false;
 
-  }; 
-
-in with localDefs;
-        
-stdenv.mkDerivation rec {
-  name = "pyrex-0.9.5.1.1";
-  builder = writeScript (name + "-builder")
-    (textClosure localDefs [installPythonPackage doForceShare]);
   meta = {
-    description = "Python package compiler or something like that";
-    inherit src;
+    homepage = http://www.cosc.canterbury.ac.nz/greg.ewing/python/Pyrex/;
+    description = "A language for writing Python extension modules";
   };
 }
