7bb60c640da5fe0fad420ef83eb41521e10925aa
Michael Raskin <7c6f434c@mail.ru>
2012-07-19 09:07:57 +0400

    LMMath: add Latin Modern Math font

---
+++ pkgs/data/fonts/lmodern/lmmath.nix
@@ -0,0 +1,30 @@
+{ stdenv, fetchurl, unzip }:
+
+stdenv.mkDerivation {
+  name = "lmmath-0.903";
+  
+  src = fetchurl {
+    url = "http://www.gust.org.pl/projects/e-foundry/lm-math/download/lmmath0903otf";
+    sha256 = "ee96cb14f5c746d6c6b9ecfbdf97dafc2f535be3dd277e15e8ea6fb594995d64";
+    name = "lmmath-0.903.zip";
+  };
+
+  buildInputs = [unzip];
+
+  sourceRoot = ".";
+
+  installPhase = ''
+    mkdir -p $out/texmf/fonts/opentype
+    mkdir -p $out/share/fonts/opentype
+
+    cp *.{OTF,otf} $out/texmf/fonts/opentype/lmmath-regular.otf
+    cp *.{OTF,otf} $out/share/fonts/opentype/lmmath-regular.otf
+
+    ln -s $out/texmf* $out/share/
+  '';
+
+  meta = {
+    description = "Latin Modern font";
+  };
+}
+
