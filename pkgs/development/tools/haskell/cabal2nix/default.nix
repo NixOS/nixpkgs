03d98589e29d0eed775d178aca64876f162f2d83
Peter Simons <simons@cryp.to>
2012-07-19 12:18:40 +0200

    haskell-cabal2nix: update to version 1.38

--- pkgs/development/tools/haskell/cabal2nix/default.nix
+++ pkgs/development/tools/haskell/cabal2nix/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "cabal2nix";
-  version = "1.37";
-  sha256 = "0gd20nzr3bqscd4pn0j3vsmnhxiypbv5ppncmi0ck5n4ii8c6m94";
+  version = "1.38";
+  sha256 = "1kybxrkddbzr1cqpqplbflhakf9njb9hvamhdwvlanlk8985h8jg";
   isLibrary = false;
   isExecutable = true;
   buildDepends = [ Cabal filepath hackageDb HTTP mtl regexPosix ];
