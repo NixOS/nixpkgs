fe42f8add91b1616de94f878351f3dd5b44dbcdd
Cillian de Róiste <goibhniu@fsfe.org>
2012-07-19 04:37:33 +0200

    ffmpeg: Add optional Jack audio support.
    The author of this commit is @cillianderoiste because he made the patch and I'm
    just committing it.
    
    This change adds support for Jack audio "while we're at it", see discussion at
    pull request #54.

--- pkgs/development/libraries/ffmpeg/default.nix
+++ pkgs/development/libraries/ffmpeg/default.nix
@@ -1,4 +1,5 @@
 { stdenv, fetchurl, pkgconfig, yasm, zlib, bzip2, alsaLib
+, jackSupport ? false, jackaudio ? null
 , mp3Support ? true, lame ? null
 , speexSupport ? true, speex ? null
 , theoraSupport ? true, libtheora ? null
@@ -12,6 +13,7 @@
 , x11grabSupport ? false, libXext ? null, libXfixes ? null
 }:
 
+assert jackSupport -> jackaudio != null;
 assert speexSupport -> speex != null;
 assert theoraSupport -> libtheora != null;
 assert vorbisSupport -> libvorbis != null;
@@ -54,6 +56,7 @@ stdenv.mkDerivation rec {
     ++ stdenv.lib.optional x11grabSupport "--enable-x11grab";
 
   buildInputs = [ pkgconfig lame yasm zlib bzip2 alsaLib ]
+    ++ stdenv.lib.optional jackSupport jackaudio
     ++ stdenv.lib.optional mp3Support lame
     ++ stdenv.lib.optional speexSupport speex
     ++ stdenv.lib.optional theoraSupport libtheora
