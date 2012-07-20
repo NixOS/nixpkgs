77510eaa9917eef42fe73bf3a0c802c0e71bbfe8
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-07-20 17:38:36 -0400

    dbus.nix: Fix path to dbus-send

--- modules/services/system/dbus.nix
+++ modules/services/system/dbus.nix
@@ -138,7 +138,7 @@ in
         ExecStartPre=${pkgs.dbus_tools}/bin/dbus-uuidgen --ensure
         ExecStartPre=-${pkgs.coreutils}/bin/rm -f /var/run/dbus/pid
         ExecStart=${pkgs.dbus_daemon}/bin/dbus-daemon --system --address=systemd: --nofork --systemd-activation
-        ExecReload=${pkgs.dbus_tools}/dbus-send --print-reply --system --type=method_call --dest=org.freedesktop.DBus / org.freedesktop.DBus.ReloadConfig
+        ExecReload=${pkgs.dbus_tools}/bin/dbus-send --print-reply --system --type=method_call --dest=org.freedesktop.DBus / org.freedesktop.DBus.ReloadConfig
         OOMScoreAdjust=-900
       '';
 
