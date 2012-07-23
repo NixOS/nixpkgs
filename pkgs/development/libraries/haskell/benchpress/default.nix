f110ebe38e483a473fc967761b455253863db6f9
Peter Simons <simons@cryp.to>
2012-07-23 11:56:21 +0200

    haskell-benchpress: update to version 0.2.2.5

--- pkgs/development/libraries/haskell/benchpress/default.nix
+++ pkgs/development/libraries/haskell/benchpress/default.nix
@@ -2,11 +2,11 @@
 
 cabal.mkDerivation (self: {
   pname = "benchpress";
-  version = "0.2.2.4";
-  sha256 = "0cabjx0gkbk5blqkm9pmnz8kmi3573367365gny4r3m431iwxgnf";
+  version = "0.2.2.5";
+  sha256 = "0md66nnn9ad8f3y0dd411plnza59fvhfd6nibag3a556bpfa0kgs";
   buildDepends = [ mtl time ];
   patchPhase = ''
-    sed -i -e 's|base .*,|base,|' -e 's|mtl .*,|mtl,|' benchpress.cabal
+    sed -i -e 's|mtl .*,|mtl,|' benchpress.cabal
   '';
   meta = {
     homepage = "http://github.com/tibbe/benchpress";
