e2576899c16f389d27410f47db79d3340a04140a
Shea Levy <shea@shealevy.com>
2012-07-25 17:51:19 -0400

    Add coffee-script node package

--- pkgs/top-level/node-packages.nix
+++ pkgs/top-level/node-packages.nix
@@ -7,6 +7,18 @@ let buildNodePackage = import ../development/web/nodejs/build-node-package.nix {
 with pkgs;
 
 let self = {
+  "coffee-script" = self."coffee-script-1.3.3";
+
+  "coffee-script-1.3.3" = buildNodePackage rec {
+    name = "coffee-script-1.3.3";
+    src = fetchurl {
+      url = "http://registry.npmjs.org/coffee-script/-/${name}.tgz";
+      sha256 = "deedd2cf9d5abe2bad724e6809bec40efa07215dae85f44d78cd37736bb50bc5";
+    };
+    deps = [
+
+    ];
+  };
 };
 
 in self
