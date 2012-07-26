6c7132093190f401706efc6b95e973f6bb0f9361
Petr Rockai <me@mornfall.net>
2012-07-26 14:44:16 +0200

    Allow build-support RPM VMs to be booted with their native kernels.

--- pkgs/build-support/vm/default.nix
+++ pkgs/build-support/vm/default.nix
@@ -14,13 +14,14 @@ rec {
   # The 15 second CIFS timeout is too short if the host if heavily
   # loaded (e.g., in the Hydra build farm when it's running many jobs
   # in parallel).  So apply a patch to increase the timeout to 120s.
-  kernel = assert pkgs.linux.features.cifsTimeout; linuxKernel;
+  nixpkgsKernel = assert pkgs.linux.features.cifsTimeout; linuxKernel;
 
   kvm = pkgs.qemu_kvm;
 
 
-  modulesClosure = makeModulesClosure {
+  modulesClosure = kernel: makeModulesClosure {
     inherit kernel rootModules;
+    allowMissing = true;
   };
 
 
@@ -29,7 +30,7 @@ rec {
 
   initrdUtils = runCommand "initrd-utils"
     { buildInputs = [ nukeReferences ];
-      allowedReferences = [ "out" modulesClosure ]; # prevent accidents like glibc being included in the initrd
+      allowedReferences = [ "out" ]; # prevent accidents like glibc being included in the initrd
     }
     ''
       mkdir -p $out/bin
@@ -76,8 +77,9 @@ rec {
     halt -d -p -f
   '';
 
-  stage1Init = writeScript "vm-run-stage1" ''
+  stage1Init = kernel: writeScript "vm-run-stage1" ''
     #! ${initrdUtils}/bin/ash -e
+    echo START
 
     export PATH=${initrdUtils}/bin
 
@@ -103,7 +105,7 @@ rec {
       esac
     done
 
-    for i in $(cat ${modulesClosure}/insmod-list); do
+    for i in $(cat ${modulesClosure kernel}/insmod-list); do
       args=
       case $i in
         */cifs.ko)
@@ -157,10 +159,9 @@ rec {
     ${teardown} $?
   '';
 
-
-  initrd = makeInitrd {
+  initrd = kernel: makeInitrd {
     contents = [
-      { object = stage1Init;
+      { object = stage1Init kernel;
         symlink = "/init";
       }
     ];
@@ -198,7 +199,7 @@ rec {
   '';
 
 
-  qemuCommandLinux = ''
+  qemuCommandLinux = kernel: ''
     ${kvm}/bin/qemu-kvm \
       ${lib.optionalString (pkgs.stdenv.system == "x86_64-linux") "-cpu kvm64"} \
       -nographic -no-reboot \
@@ -207,7 +208,7 @@ rec {
       -net user,guestfwd=tcp:10.0.2.4:445-chardev:samba \
       -drive file=$diskImage,if=virtio,cache=writeback,werror=report \
       -kernel ${kernel}/${img} \
-      -initrd ${initrd}/initrd \
+      -initrd ${initrd kernel}/initrd \
       -append "console=ttyS0 panic=1 command=${stage2Init} out=$out mountDisk=$mountDisk" \
       $QEMU_OPTS
   '';
@@ -332,14 +333,16 @@ rec {
      `run-vm' will be left behind in the temporary build directory
      that allows you to boot into the VM and debug it interactively. */
 
-  runInLinuxVM = drv: lib.overrideDerivation drv (attrs: {
-    requiredSystemFeatures = [ "kvm" ];
-    builder = "${bash}/bin/sh";
-    args = ["-e" (vmRunCommand qemuCommandLinux)];
-    origArgs = attrs.args;
-    origBuilder = attrs.builder;
-    QEMU_OPTS = "-m ${toString (if attrs ? memSize then attrs.memSize else 256)}";
-  });
+  runInLinuxVM = drv: let
+        kernel = if drv ? kernel then drv.kernel else nixpkgsKernel;
+    in lib.overrideDerivation drv (attrs: {
+        requiredSystemFeatures = [ "kvm" ];
+        builder = "${bash}/bin/sh";
+        args = ["-e" (vmRunCommand (qemuCommandLinux kernel))];
+        origArgs = attrs.args;
+        origBuilder = attrs.builder;
+        QEMU_OPTS = "-m ${toString (if attrs ? memSize then attrs.memSize else 256)}";
+      });
 
 
   extractFs = {file, fs ? null} :
@@ -559,7 +562,7 @@ rec {
     mkdir $TMPDIR/xchg
     export > $TMPDIR/xchg/saved-env
     mountDisk=1
-    ${qemuCommandLinux}
+    ${qemuCommandLinux nixpkgsKernel}
   '';
 
 
@@ -746,6 +749,41 @@ rec {
         packages = packages ++ extraPackages;
       }) { inherit fetchurl; };
     };
+
+  rpm2kernel = rpms: stdenv.mkDerivation {
+    name = "rpm2kernel";
+    inherit rpms;
+    builder = writeScript "rpm2kernel-builder" ''
+      source $stdenv/setup
+      ensureDir $out
+      cd $out
+      for r in $rpms; do
+        echo $r | grep "kernel-[0-9]" || continue
+        ${rpm}/bin/rpm2cpio $r | ${cpio}/bin/cpio -i --make-directories
+      done
+      cd $out
+      ln -s boot/vmlinuz* bzImage
+      ln -s boot/System.map* System.map
+      ln -s boot/config* config
+      cd lib/modules
+      ver=`echo *`
+      cd $ver
+      ${module_init_tools}/sbin/depmod -ae -F $out/System.map $ver -b $out
+    '';
+  };
+
+  makeKernelFromRPMDist =
+    { urlPrefix ? "", urlPrefixes ? [urlPrefix]
+    , packagesList ? "", packagesLists ? [packagesList]
+    , archs ? ["noarch" "i386"]
+    , ...}:
+
+    rpm2kernel (
+      import (rpmClosureGenerator {
+        inherit packagesLists urlPrefixes archs;
+        name = "kernel";
+        packages = "kernel";
+      }) { inherit fetchurl; });
 
 
   /* Like `rpmClosureGenerator', but now for Debian/Ubuntu releases
