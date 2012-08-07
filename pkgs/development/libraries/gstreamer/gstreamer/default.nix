778d69c4ea0d98b62fde05feea3673048488eeb6
aszlig <aszlig@redmoonstudios.org>
2012-08-07 03:48:37 +0200

    gstreamer: Update to version 0.11.92.
    This already includes the version 1.0 API, so we'll expect things to break badly
    very soon. According to an official announcement, the API is final and won't
    change (except minor and absolutely necessary) until versions aufter 1.0.
    
    See the release announcement here:
    
    http://gstreamer.freedesktop.org/news/#2012-06-07T16:43:00Z
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/development/libraries/gstreamer/gstreamer/default.nix
+++ pkgs/development/libraries/gstreamer/gstreamer/default.nix
@@ -1,14 +1,14 @@
 { fetchurl, stdenv, perl, bison, flex, pkgconfig, glib, libxml2 }:
 
 stdenv.mkDerivation rec {
-  name = "gstreamer-0.10.35";
+  name = "gstreamer-0.11.92";
 
   src = fetchurl {
     urls =
-      [ "${meta.homepage}/src/gstreamer/${name}.tar.bz2"
-        "mirror://gentoo/distfiles/${name}.tar.bz2"
+      [ "${meta.homepage}/src/gstreamer/${name}.tar.xz"
+        "mirror://gentoo/distfiles/${name}.tar.xz"
       ];
-    sha256 = "11lp13lig3c6qys80phyvsik56r9y0c95vg2jxxliqj6rnigwyw1";
+    sha256 = "5e41428eea3f62711bede6fd9457ac836874215fcfe5d6775c3f2000452da281";
   };
 
   buildInputs = [ perl bison flex pkgconfig ];
