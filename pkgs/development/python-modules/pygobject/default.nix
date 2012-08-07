7f1cddcad33fd3fcfb67fd2e260cca79e2a0ddc0
aszlig <aszlig@redmoonstudios.org>
2012-08-07 11:10:51 +0200

    pygobject: Update to version 3.0.4.
    This replaces the former gst-python derivation and is the way how GStreamer
    should be used in API version 1.0, for reference please see:
    
    https://wiki.ubuntu.com/Novacut/GStreamer1.0#Python_Porting_Guide
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/development/python-modules/pygobject/default.nix
+++ pkgs/development/python-modules/pygobject/default.nix
@@ -1,28 +1,18 @@
-{ stdenv, fetchurl, python, pkgconfig, glib }:
+{ stdenv, fetchurl, python, pkgconfig, glib, gobjectIntrospection
+, cairo, pycairo, expat }:
 
 stdenv.mkDerivation rec {
-  name = "pygobject-2.27.0";
+  name = "pygobject-3.0.4";
   
   src = fetchurl {
-    url = "http://ftp.gnome.org/pub/GNOME/sources/pygobject/2.27/${name}.tar.bz2";
-    sha256 = "18mq4mj9s9sw12m6gbbc4iffrq993c7q09v9yahlnamrqn3bv53m";
+    url = "http://ftp.gnome.org/pub/GNOME/sources/pygobject/3.0/${name}.tar.xz";
+    sha256 = "f457b1d7f6b8bfa727593c3696d2b405da66b4a8d34cd7d3362ebda1221f0661";
   };
 
-  configureFlags = "--disable-introspection";
-
-  buildInputs = [ python pkgconfig glib ];
-
-  postInstall = ''
-    # All python code is installed into a "gtk-2.0" sub-directory. That
-    # sub-directory may be useful on systems which share several library
-    # versions in the same prefix, i.e. /usr/local, but on Nix that directory
-    # is useless. Furthermore, its existence makes it very hard to guess a
-    # proper $PYTHONPATH that allows "import gtk" to succeed.
-    cd $(toPythonPath $out)/gtk-2.0
-    for n in *; do
-      ln -s "gtk-2.0/$n" "../$n"
-    done
-  '';
+  buildInputs = [
+    python pkgconfig glib gobjectIntrospection
+    cairo pycairo expat
+  ];
 
   meta = {
     homepage = http://live.gnome.org/PyGObject;
