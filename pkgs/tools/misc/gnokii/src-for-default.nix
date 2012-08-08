bee287597a31d330e7d9669a9aa6967beb8d642b
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-08-08 13:42:41 +0400

    Fix gnokii URL

--- pkgs/tools/misc/gnokii/src-for-default.nix
+++ pkgs/tools/misc/gnokii/src-for-default.nix
@@ -2,7 +2,7 @@ rec {
    version="0.6.30";
    name="gnokii-0.6.30";
    hash="0k14dymifk7dx3z58cqrc4mgxrm17s4jk12chwdfjhjarz390x8p";
-   url="http://www.gnokii.org/download/gnokii/gnokii-${version}.tar.gz";
+   url="http://www.gnokii.org/download/gnokii/0.6.x/gnokii-${version}.tar.gz";
    advertisedUrl="http://www.gnokii.org/download/gnokii/gnokii-0.6.30.tar.gz";
   
   
