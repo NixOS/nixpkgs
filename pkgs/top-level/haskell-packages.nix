7aa98d2f5e0f352755f3545bcf922a2fcbcfa774
Peter Simons <simons@cryp.to>
2012-08-09 11:36:45 +0200

    haskell-monad-logger: add version 0.2.0

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -1056,6 +1056,8 @@ let result = let callPackage = x : y : modifyPrio (newScope result.final x y);
 
   monadLoops = callPackage ../development/libraries/haskell/monad-loops {};
 
+  monadLogger = callPackage ../development/libraries/haskell/monad-logger {};
+
   monadPar_0_1_0_3 = callPackage ../development/libraries/haskell/monad-par/0.1.0.3.nix {};
   monadPar_0_3 = callPackage ../development/libraries/haskell/monad-par/0.3.nix {};
   monadPar = self.monadPar_0_3;
