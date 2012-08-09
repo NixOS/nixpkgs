19dca37062a57c94b9fd9353451d34083dda21fd
Peter Simons <simons@cryp.to>
2012-08-09 12:10:54 +0200

    haskell-yesod-core: update to version 1.1.0

--- pkgs/development/libraries/haskell/yesod-core/default.nix
+++ pkgs/development/libraries/haskell/yesod-core/default.nix
@@ -1,21 +1,22 @@
 { cabal, aeson, blazeBuilder, blazeHtml, blazeMarkup
 , caseInsensitive, cereal, clientsession, conduit, cookie, failure
-, fastLogger, hamlet, httpTypes, liftedBase, monadControl, parsec
-, pathPieces, random, resourcet, shakespeare, shakespeareCss
-, shakespeareI18n, shakespeareJs, text, time, transformers
-, transformersBase, vector, wai, waiExtra, waiLogger, yesodRoutes
+, fastLogger, hamlet, httpTypes, liftedBase, monadControl
+, monadLogger, parsec, pathPieces, random, resourcet, shakespeare
+, shakespeareCss, shakespeareI18n, shakespeareJs, text, time
+, transformers, transformersBase, vector, wai, waiExtra
+, yesodRoutes
 }:
 
 cabal.mkDerivation (self: {
   pname = "yesod-core";
-  version = "1.0.1.3";
-  sha256 = "1rdj622zi12v9d7cxkn8w4q43k8c6gfz8wrpmvya76gqmg6h2gil";
+  version = "1.1.0";
+  sha256 = "01jxb6skpwckvkgar847limqrcl91l50pr7iza2sfp0a6j5a2hmk";
   buildDepends = [
     aeson blazeBuilder blazeHtml blazeMarkup caseInsensitive cereal
     clientsession conduit cookie failure fastLogger hamlet httpTypes
-    liftedBase monadControl parsec pathPieces random resourcet
-    shakespeare shakespeareCss shakespeareI18n shakespeareJs text time
-    transformers transformersBase vector wai waiExtra waiLogger
+    liftedBase monadControl monadLogger parsec pathPieces random
+    resourcet shakespeare shakespeareCss shakespeareI18n shakespeareJs
+    text time transformers transformersBase vector wai waiExtra
     yesodRoutes
   ];
   meta = {
