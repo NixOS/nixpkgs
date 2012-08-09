f061cfe2f32a2a60a7c23a9514ee6b66287657b5
Peter Simons <simons@cryp.to>
2012-08-09 12:10:49 +0200

    haskell-attoparsec-conduit: update to version 0.5.0

--- pkgs/development/libraries/haskell/attoparsec-conduit/default.nix
+++ pkgs/development/libraries/haskell/attoparsec-conduit/default.nix
@@ -2,12 +2,12 @@
 
 cabal.mkDerivation (self: {
   pname = "attoparsec-conduit";
-  version = "0.4.0.1";
-  sha256 = "1r01xaaw439qpbfnanv2yfw9fz6id8cy17yqdm6x6vk35hmv59kc";
+  version = "0.5.0";
+  sha256 = "17l98kbv2pxcchacy7r4ja0czdklc7r4j8vzv3pi0pjb2s9ih6sq";
   buildDepends = [ attoparsec conduit text transformers ];
   meta = {
     homepage = "http://github.com/snoyberg/conduit";
-    description = "Turn attoparsec parsers into sinks";
+    description = "Consume attoparsec parsers via conduit";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [ self.stdenv.lib.maintainers.andres ];
