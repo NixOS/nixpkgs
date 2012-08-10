986f04c87efed848a27f5b571c15937acf12923b
Ludovic Courtès <ludo@gnu.org>
2012-08-10 23:16:02 +0200

    guile: disable thread support on Solaris

--- pkgs/development/interpreters/guile/default.nix
+++ pkgs/development/interpreters/guile/default.nix
@@ -99,10 +99,10 @@
 
 //
 
-(if stdenv.isFreeBSD
+(if (stdenv.isFreeBSD || stdenv.isSunOS)
  then {
-   # XXX: Thread support is currently broken on FreeBSD (namely the
-   # `SCM_I_IS_THREAD' assertion in `scm_spawn_thread' is hit.)
+   # XXX: Thread support is currently broken on FreeBSD and Solaris (namely
+   # the `SCM_I_IS_THREAD' assertion in `scm_spawn_thread' is hit.)
    configureFlags = [ "--without-threads" ];
  }
  else {}))
