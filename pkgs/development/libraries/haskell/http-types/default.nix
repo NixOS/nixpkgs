3999d82a405e2c3d2229aa8416fb2c1f02ede202
Peter Simons <simons@cryp.to>
2012-08-16 15:41:22 +0200

    haskell-http-types: update to version 0.7.2

--- pkgs/development/libraries/haskell/http-types/default.nix
+++ pkgs/development/libraries/haskell/http-types/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "http-types";
-  version = "0.7.1";
-  sha256 = "1vfaffraiqdsjbw4b0xwk8h8d4pl94mbxlwyqk8ry786v9aafyqx";
+  version = "0.7.2";
+  sha256 = "06yvjq4246sp6gfillwbk6xz1d9l0zq06qy70a5zdyaw6viz2h76";
   buildDepends = [ blazeBuilder caseInsensitive text ];
   meta = {
     homepage = "https://github.com/aristidb/http-types";
