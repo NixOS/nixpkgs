d2347755e0df89fb9b66a91fcd3a4f735178e65e
Peter Simons <simons@cryp.to>
2012-08-16 15:41:21 +0200

    haskell-certificate: update to version 1.2.5

--- pkgs/development/libraries/haskell/certificate/default.nix
+++ pkgs/development/libraries/haskell/certificate/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "certificate";
-  version = "1.2.4";
-  sha256 = "079364567cshfj2bc0lbj933jcgdqisvv8m0czlyscb4hd10vfg7";
+  version = "1.2.5";
+  sha256 = "1zxi1hflq973m7a1c9wvxdx8aqapx1kqy8j7nn7k67l9gpjb1jbc";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [ asn1Data cryptoPubkeyTypes mtl pem time ];
