5e19db2ce5ca3786b87fb31839d51af82b36ab69
Peter Simons <simons@cryp.to>
2012-08-20 10:25:18 +0200

    haskell-semigroups: update to version 0.8.4

--- pkgs/development/libraries/haskell/semigroups/default.nix
+++ pkgs/development/libraries/haskell/semigroups/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "semigroups";
-  version = "0.8.3.2";
-  sha256 = "0g433l4rinc6r2yr91jnl6wh5b2kn5vsrp08cmznkgaz45lb7n7c";
+  version = "0.8.4";
+  sha256 = "0xphwxxzddgcw2hr4h60l3y27f1x032w049wkjs71f5hdypwapv4";
   meta = {
     homepage = "http://github.com/ekmett/semigroups/";
     description = "Haskell 98 semigroups";
