ec6926bf7e20cca717feb5d8c8ee173c357995fa
aszlig <aszlig@redmoonstudios.org>
2012-08-22 09:59:01 +0200

    ncdu: New package, starting at version 1.8.
    `ncdu` is a little ncurses tool to browse the filesystem tree sorted by disk
    usage.

---
+++ pkgs/tools/misc/ncdu/default.nix
@@ -0,0 +1,19 @@
+{ stdenv, fetchurl, ncurses }:
+
+stdenv.mkDerivation rec {
+  name = "ncdu-${version}";
+  version = "1.8";
+
+  src = fetchurl {
+    url = "http://dev.yorhel.nl/download/${name}.tar.gz";
+    sha256 = "42aaf0418c05e725b39b220166a9c604a9c54c0fbf7692c9c119b36d0ed5d099";
+  };
+
+  buildInputs = [ ncurses ];
+
+  meta = {
+    description = "An ncurses disk usage analyzer.";
+    homepage = http://dev.yorhel.nl/ncdu;
+    license = stdenv.lib.licenses.mit;
+  };
+}
