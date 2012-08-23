5318c4652cbfa1673a219d50b48130919be8b78d
Peter Simons <simons@cryp.to>
2012-08-23 17:37:40 +0200

    Patch overzealous dependency restrictions in Haskell packages.

--- pkgs/development/libraries/haskell/clientsession/default.nix
+++ pkgs/development/libraries/haskell/clientsession/default.nix
@@ -10,6 +10,9 @@ cabal.mkDerivation (self: {
     base64Bytestring cereal cprngAes cryptoApi cryptocipher entropy
     skein tagged
   ];
+  patchPhase = ''
+    sed -i -e 's|, base64-bytestring.*|, base64-bytestring|' clientsession.cabal
+  '';
   meta = {
     homepage = "http://github.com/yesodweb/clientsession/tree/master";
     description = "Securely store session data in a client-side cookie";
