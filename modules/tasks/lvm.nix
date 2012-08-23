0280aa2dc4c6a3cf9ddd84e93242611d4945320c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-08-23 10:23:41 -0400

    Remove the lvm job
    There is a generator in lvm2 that takes care of this.

--- modules/tasks/lvm.nix
+++ modules/tasks/lvm.nix
@@ -6,19 +6,6 @@
 
   config = {
 
-    jobs.lvm =
-      { startOn = "started udev or new-devices";
-
-        script =
-          ''
-            # Make all logical volumes on all volume groups available, i.e.,
-            # make them appear in /dev.
-            ${pkgs.lvm2}/sbin/vgchange --available y
-          '';
-
-        task = true;
-      };
-
     environment.systemPackages = [ pkgs.lvm2 ];
 
     services.udev.packages = [ pkgs.lvm2 ];
