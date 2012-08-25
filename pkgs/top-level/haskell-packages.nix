ba83178aed6a5af45ae4e453e62494312829183c
Mathijs Kwik <mathijs@bluescreen303.nl>
2012-08-25 20:16:55 +0200

    xmonad-extras: pass old version of split
    optional features depend on it.

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -1811,6 +1811,7 @@ let result = let callPackage = x : y : modifyPrio (newScope result.final x y);
 
   xmonadExtras = callPackage ../applications/window-managers/xmonad/xmonad-extras.nix {
     X11 = self.X11_1_5_0_1;
+    split = self.split_0_1;
   };
 
   # Tools.
