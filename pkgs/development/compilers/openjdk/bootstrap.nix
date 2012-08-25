84e1da4b09e2332447e5a2d9fc4e45b2499d7cc0
Shea Levy <shea@shealevy.com>
2012-08-25 17:55:21 -0400

    openjdk bootstrap tarballs are on nixos.org now.

--- pkgs/development/compilers/openjdk/bootstrap.nix
+++ pkgs/development/compilers/openjdk/bootstrap.nix
@@ -4,12 +4,12 @@ let
   # !!! These should be on nixos.org
   src = if glibc.system == "x86_64-linux" then
     fetchurl {
-      url = ftp://linode.shealevy.com/openjdk-bootstrap-x86_64-linux-2012-08-24.tar.xz;
+      url = http://nixos.org/tarballs/openjdk-bootstrap-x86_64-linux-2012-08-24.tar.xz;
       sha256 = "0gla9dxrfq2w1hvgsnn8jg8a60k27im6z43a6iidi0qmwa0wah32";
     }
   else if glibc.system == "i686-linux" then
     fetchurl {
-      url = ftp://linode.shealevy.com/openjdk-bootstrap-i686-linux-2012-08-24.tar.xz;
+      url = http://nixos.org/tarballs/openjdk-bootstrap-i686-linux-2012-08-24.tar.xz;
       sha256 = "184wq212bycwbbq4ix8cc6jwjxkrqw9b01zb86q95kqpa8zy5206";
     }
   else throw "No bootstrap for system";
