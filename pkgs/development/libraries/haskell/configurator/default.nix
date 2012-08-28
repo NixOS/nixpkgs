e01aa340f6896ab5553397eb4b290b4069bd4f2f
Peter Simons <simons@cryp.to>
2012-08-28 12:37:14 +0200

    haskell-configurator: update to version 0.2.0.1

--- pkgs/development/libraries/haskell/configurator/default.nix
+++ pkgs/development/libraries/haskell/configurator/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "configurator";
-  version = "0.2.0.0";
-  sha256 = "0zkcmziyfq2sm9i75ysi5nxd21fynp88m0safhmn3jld7plj03la";
+  version = "0.2.0.1";
+  sha256 = "02w6f5q2xkpc3kgqz6a58g7yr0q4xd8ck1b6lr64ahvqwsjbxy6p";
   buildDepends = [
     attoparsec hashable text unixCompat unorderedContainers
   ];
