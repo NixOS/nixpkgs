d11f3d5551b613b4e6c87749c69a8bcd15c2c1e4
Peter Simons <simons@cryp.to>
2012-08-30 13:02:00 +0200

    haskell-http-conduit: update to version 1.6.0.1

--- pkgs/development/libraries/haskell/http-conduit/default.nix
+++ pkgs/development/libraries/haskell/http-conduit/default.nix
@@ -8,8 +8,8 @@
 
 cabal.mkDerivation (self: {
   pname = "http-conduit";
-  version = "1.6.0";
-  sha256 = "1vr4kglmlqn6s31ks231qx4b8034p6yi0501h3zswpqyf5cimhl7";
+  version = "1.6.0.1";
+  sha256 = "0v6r57pliaam3i69826bf07iqmkm2s2x6f19mk0amh7fvdfjhgyb";
   buildDepends = [
     asn1Data attoparsec attoparsecConduit base64Bytestring blazeBuilder
     blazeBuilderConduit caseInsensitive certificate conduit cookie
@@ -17,9 +17,6 @@ cabal.mkDerivation (self: {
     monadControl mtl network regexCompat resourcet socks text time tls
     tlsExtra transformers transformersBase utf8String void zlibConduit
   ];
-  patchPhase = ''
-    sed -i http-conduit.cabal -e 's|, base64-bytestring.*|, base64-bytestring|' -e 's|, asn1-data.*|, asn1-data|'
-  '';
   meta = {
     homepage = "http://www.yesodweb.com/book/http-conduit";
     description = "HTTP client package with conduit interface and HTTPS support";
