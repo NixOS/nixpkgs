39760a2ad6cdb5a6ad9d657b6c64e3f349dc9fb5
Mathijs Kwik <mathijs@bluescreen303.nl>
2012-09-04 00:45:17 +0200

    davfs2: upgrade to 1.4.7
    - add option to disable the default user/group in the config file.
      If you only want to mount filesystems as root (don't use setuid
      root), the default is not very useful. This setting is not
      overridable from an external config file, so this needs to be done
      here. Perhaps it's possible to tell the package to look in system
      /etc, but that makes the install-phase try to put files there too.
      Would take quite some patching.

--- pkgs/tools/filesystems/davfs2/default.nix
+++ pkgs/tools/filesystems/davfs2/default.nix
@@ -1,16 +1,21 @@
-{ stdenv, fetchurl, neon, zlib }:
+{ stdenv, fetchurl, neon, zlib, noDavsUser ? false}:
 
 stdenv.mkDerivation rec {
-  name = "davfs2-1.4.5";
+  name = "davfs2-1.4.7";
 
   src = fetchurl {
     url = "mirror://savannah/davfs2/${name}.tar.gz";
-    sha256 = "1pkl2braggp2qg4c68dwfv399l9jz7cvi7gkm4xbj6mgvl0cxw18";
+    sha256 = "0i7hrwlfzisb4l2mza1kjj9q9xxixggjplsjm339zl7828mfxh2h";
   };
 
+  prePatch = stdenv.lib.optional noDavsUser ''
+    echo "dav_user root" >> etc/davfs2.conf
+    echo "dav_group root" >> etc/davfs2.conf
+  '';
+
   buildInputs = [ neon zlib ];
-  
-  patches = [ ./davfs2-install.patch ./isdir.patch ]; 
+
+  patches = [ ./davfs2-install.patch ./isdir.patch ];
 
   meta = {
     longDescription = "Web Distributed Authoring and Versioning (WebDAV), an extension to the HTTP-protocol, allows authoring of resources on a remote web server. davfs2 provides the ability to access such resources like a typical filesystem, allowing for use by standard applications with no built-in support for WebDAV.";
