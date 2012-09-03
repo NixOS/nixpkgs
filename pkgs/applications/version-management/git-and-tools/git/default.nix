14da2bf31b4bc29e729f265b6e863b7209e67758
Peter Simons <simons@cryp.to>
2012-09-03 21:33:33 +0200

    git: update to version 1.7.12

--- pkgs/applications/version-management/git-and-tools/git/default.nix
+++ pkgs/applications/version-management/git-and-tools/git/default.nix
@@ -9,7 +9,7 @@
 
 let
 
-  version = "1.7.11.4";
+  version = "1.7.12";
 
   svn = subversionClient.override { perlBindings = true; };
 
@@ -20,7 +20,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "http://git-core.googlecode.com/files/git-${version}.tar.gz";
-    sha256 = "16a1gm256w82j9ardzyfyqi0f35l3x92xsqz8ghz1pnja8jns7g9";
+    sha256 = "3b8661782dc280d3d4be5193bcb8a1895c1ba272cf02efb70857bbcc4415f505";
   };
 
   patches = [ ./docbook2texi.patch ];
