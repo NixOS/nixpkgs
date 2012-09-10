3692a2e83d9012b4f3493c15e93c1e5af0c3e8ea
aszlig <aszlig@redmoonstudios.org>
2012-09-10 22:06:02 +0200

    conky: Update to version 1.9.0.
    Version 1.8.1 seems to be quite dated, in addition I'd like to use the weather
    plugin, which isn't available in 1.8.1 as well. If you work in your hermetically
    sealed environment, you surely want to know what's going on outside.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/os-specific/linux/conky/default.nix
+++ pkgs/os-specific/linux/conky/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, pkgconfig, libxml2, curl, wirelesstools, glib, openssl}:
 
 stdenv.mkDerivation rec {
-  name = "conky-1.8.1";
+  name = "conky-1.9.0";
 
   src = fetchurl {
     url = "mirror://sourceforge/conky/${name}.tar.bz2";
-    sha256 = "0hsybra4qzaqzny6n66m7768vbwkikajcvcbsqgnnnb9527jfnpq";
+    sha256 = "0vxvjmi3cdvnp994sv5zcdyncfn0mlxa71p2wm9zpyrmy58bbwds";
   };
 
   buildInputs = [ pkgconfig libxml2 curl wirelesstools glib openssl ];
