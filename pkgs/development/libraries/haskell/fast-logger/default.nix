5c75c45f26c3269e438d97f57dd11b815035c718
Peter Simons <simons@cryp.to>
2012-09-11 12:15:34 +0200

    haskell-fast-logger: update to version 0.3.1

--- pkgs/development/libraries/haskell/fast-logger/default.nix
+++ pkgs/development/libraries/haskell/fast-logger/default.nix
@@ -1,10 +1,10 @@
-{ cabal, blazeBuilder, filepath, text, unixTime }:
+{ cabal, blazeBuilder, dateCache, filepath, text, unixTime }:
 
 cabal.mkDerivation (self: {
   pname = "fast-logger";
-  version = "0.2.2";
-  sha256 = "1r1fk0lqmh49v24wnx236x9cz122c8806y9mrxnaihxggw4anj0x";
-  buildDepends = [ blazeBuilder filepath text unixTime ];
+  version = "0.3.1";
+  sha256 = "0sjn3vad0fbchv1fhap71wfnihlwnfhk6p9h9hpnbr0i4b32f1ks";
+  buildDepends = [ blazeBuilder dateCache filepath text unixTime ];
   meta = {
     description = "A fast logging system";
     license = self.stdenv.lib.licenses.bsd3;
