cc22e43f5aea4a478b74e72fcde2a18ecd0486f8
Peter Simons <simons@cryp.to>
2012-09-11 12:15:36 +0200

    haskell-yesod-static: update to version 1.1.0.1

--- pkgs/development/libraries/haskell/yesod-static/default.nix
+++ pkgs/development/libraries/haskell/yesod-static/default.nix
@@ -5,14 +5,13 @@
 
 cabal.mkDerivation (self: {
   pname = "yesod-static";
-  version = "1.1.0";
-  sha256 = "07aynfgr5hbm98yimlbw9scph387rkxh4mdij4is1mcv2rdhvhxm";
+  version = "1.1.0.1";
+  sha256 = "1hlg32bhmhc5npd0bm317mssrddf7jzhwr0z3ns0dpiynz9mh0vy";
   buildDepends = [
     base64Bytestring cereal conduit cryptoConduit cryptohash fileEmbed
     httpTypes systemFilepath text transformers unixCompat wai
     waiAppStatic yesodCore
   ];
-  jailbreak = true;
   meta = {
     homepage = "http://www.yesodweb.com/";
     description = "Static file serving subsite for Yesod Web Framework";
