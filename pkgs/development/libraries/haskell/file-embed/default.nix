6cff725ee9c96409e4c6bec88228d705a7fe6ebf
Peter Simons <simons@cryp.to>
2012-09-11 12:05:51 +0200

    haskell-file-embed: update to version 0.0.4.5

--- pkgs/development/libraries/haskell/file-embed/default.nix
+++ pkgs/development/libraries/haskell/file-embed/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "file-embed";
-  version = "0.0.4.4";
-  sha256 = "1czwa5vpafhvif4gv7bwa7hrxkrbrvvybgyjckd0hdpl6bpd4nhp";
+  version = "0.0.4.5";
+  sha256 = "18rhcjll5gj790g5balk3xhnmmgjh2bixik8vna5drs7y9i0innp";
   meta = {
     homepage = "https://github.com/snoyberg/file-embed";
     description = "Use Template Haskell to embed file contents directly";
