20518a9556e4af2c8d608f5ba204298efbe681e9
Peter Simons <simons@cryp.to>
2012-09-11 12:05:52 +0200

    haskell-yaml: update to version 0.8.0.2

--- pkgs/development/libraries/haskell/yaml/default.nix
+++ pkgs/development/libraries/haskell/yaml/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "yaml";
-  version = "0.8.0.1";
-  sha256 = "0frps30spsxkr479h4hpzsc9gkaazgmdjp5p4pmzy9mf174qacs8";
+  version = "0.8.0.2";
+  sha256 = "14blcsylbf9wx4yw8fsk8ddjvg844x97xfc1h7r4ls9l9ar7k95j";
   buildDepends = [
     aeson attoparsec conduit resourcet text transformers
     unorderedContainers vector
