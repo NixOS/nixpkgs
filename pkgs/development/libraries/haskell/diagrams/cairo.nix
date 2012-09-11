d23a9edde14273dfea5205216ad394b8bdebff59
Peter Simons <simons@cryp.to>
2012-09-11 12:13:55 +0200

    haskell-diagrams: cosmetic

--- pkgs/development/libraries/haskell/diagrams/cairo.nix
+++ pkgs/development/libraries/haskell/diagrams/cairo.nix
@@ -6,10 +6,10 @@ cabal.mkDerivation (self: {
   pname = "diagrams-cairo";
   version = "0.5.0.2";
   sha256 = "1wwk65c2cx7rkhmai5spms791fjhl3snwhj0w9399q8pgj6g4lj8";
-  jailbreak = true;
   buildDepends = [
     cairo cmdargs diagramsCore diagramsLib filepath gtk mtl split
   ];
+  jailbreak = true;
   meta = {
     homepage = "http://projects.haskell.org/diagrams";
     description = "Cairo backend for diagrams drawing EDSL";
