027faa30455ba8ca4831230ad25b999db8242c80
Peter Simons <simons@cryp.to>
2012-09-11 12:05:52 +0200

    haskell-wai-app-static: update to version 1.3.0.1

--- pkgs/development/libraries/haskell/wai-app-static/default.nix
+++ pkgs/development/libraries/haskell/wai-app-static/default.nix
@@ -6,14 +6,13 @@
 
 cabal.mkDerivation (self: {
   pname = "wai-app-static";
-  version = "1.3.0";
-  sha256 = "1xsjq0xip9anbd4cpami8w6d2p0pdp375hzw1jx6pvajjmkjksqn";
+  version = "1.3.0.1";
+  sha256 = "0rgbqbcj4jd6xpjm3nqa5hdf3an7208in536dl6x9n88w9a6qngp";
   buildDepends = [
     base64Bytestring blazeBuilder blazeHtml blazeMarkup cereal
     cryptoConduit cryptohash fileEmbed httpDate httpTypes mimeTypes
     systemFileio systemFilepath text time transformers unixCompat wai
   ];
-  jailbreak = true;
   meta = {
     homepage = "http://www.yesodweb.com/book/wai";
     description = "WAI application for static serving";
