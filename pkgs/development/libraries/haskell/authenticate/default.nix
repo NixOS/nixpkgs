b4102299e5d2b1ab2a3779ad2b45f6c741d531b6
Peter Simons <simons@cryp.to>
2012-09-11 12:05:51 +0200

    haskell-authenticate: update to version 1.3.1.1

--- pkgs/development/libraries/haskell/authenticate/default.nix
+++ pkgs/development/libraries/haskell/authenticate/default.nix
@@ -6,8 +6,8 @@
 
 cabal.mkDerivation (self: {
   pname = "authenticate";
-  version = "1.3.1";
-  sha256 = "1ad3vzfa7nvp8h8wk5370d2qyri0nywq1wjdvqas2mg4iv7v7271";
+  version = "1.3.1.1";
+  sha256 = "120n7z22x4y4ngxqxsi65zn992f1lksaawcd7rmjvf8m0fysbb4n";
   buildDepends = [
     aeson attoparsec blazeBuilder blazeBuilderConduit caseInsensitive
     conduit httpConduit httpTypes monadControl network resourcet
