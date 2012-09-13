68f6866ab66d952df4983b20d4f9ca51d6621102
Mathijs Kwik <mathijs@bluescreen303.nl>
2012-09-13 12:32:34 +0200

    node.js: upgrade to 0.8.9
    - espipe patch no longer needed
    - linkV8 headers replaced by buildNodePackage's upcoming binary support

--- pkgs/development/web/nodejs/default.nix
+++ pkgs/development/web/nodejs/default.nix
@@ -1,21 +1,12 @@
-{ stdenv, fetchurl, openssl, python, zlib, v8, linkV8Headers ? false, utillinux }:
-
-let
-
-  espipe_patch = fetchurl {
-    url = https://github.com/joyent/libuv/commit/0ac2fdc55455794e057e4999a2e785ca8fbfb1b2.patch;
-    sha256 = "0mqgbsz23b3zp19dwk12ys14b031hssmlp40dylb7psj937qcpzi";
-  };
-
-in
+{ stdenv, fetchurl, openssl, python, zlib, v8, utillinux }:
 
 stdenv.mkDerivation rec {
-  version = "0.8.8";
+  version = "0.8.9";
   name = "nodejs-${version}";
 
   src = fetchurl {
     url = "http://nodejs.org/dist/v${version}/node-v${version}.tar.gz";
-    sha256 = "0cri5r191l5pw8a8pf3gs7hfjm3rrz6kdnm3l8wghmp9p12p0aq9";
+    sha256 = "1finh64yra4lqy5jzqif3hhq8hzjvkbc8xkw8364sjsygj3hc3rj";
   };
 
   configureFlags = [
@@ -30,15 +21,11 @@ stdenv.mkDerivation rec {
 
   prePatch = ''
     sed -e 's|^#!/usr/bin/env python$|#!${python}/bin/python|g' -i tools/{*.py,waf-light,node-waf} configure
-    pushd deps/uv
-    patch -p 1 < ${espipe_patch}
-    popd
   '';
 
   postInstall = ''
+
     sed -e 's|^#!/usr/bin/env node$|#!'$out'/bin/node|' -i $out/lib/node_modules/npm/bin/npm-cli.js
-  '' + stdenv.lib.optionalString linkV8Headers '' # helps binary npms
-    ln -s ${v8}/include/* $out/include
   '' + stdenv.lib.optionalString stdenv.isDarwin ''
     install_name_tool -change libv8.dylib ${v8}/lib/libv8.dylib $out/bin/node
   '';
