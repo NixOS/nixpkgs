3c627e290c2711c35847c3d84d2e999bb1452cc8
Peter Simons <simons@cryp.to>
2012-09-17 16:17:55 +0200

    haskell-JuicyPixels: update to version 2.0

--- pkgs/development/libraries/haskell/JuicyPixels/default.nix
+++ pkgs/development/libraries/haskell/JuicyPixels/default.nix
@@ -4,14 +4,14 @@
 
 cabal.mkDerivation (self: {
   pname = "JuicyPixels";
-  version = "1.3";
-  sha256 = "07wljfag4ylw16wdi7znjb61pfihdik5d7p4h2lmz6xirm4mjzrm";
+  version = "2.0";
+  sha256 = "1qvdp0b2fn0cgp9vrm2p35jx8qcz1ikpvjzwkdkc8q84yr1x0457";
   buildDepends = [
     cereal deepseq mtl primitive transformers vector zlib
   ];
   meta = {
     homepage = "https://github.com/Twinside/Juicy.Pixels";
-    description = "Picture loading/serialization (in png, jpeg and bitmap)";
+    description = "Picture loading/serialization (in png, jpeg, bitmap and gif)";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [ self.stdenv.lib.maintainers.andres ];
