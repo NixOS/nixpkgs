27ab1bab7f248170ab95729d49002cd443daab12
Peter Simons <simons@cryp.to>
2012-09-17 16:17:56 +0200

    haskell-generic-deriving: update to version 1.2.2

--- pkgs/development/libraries/haskell/generic-deriving/default.nix
+++ pkgs/development/libraries/haskell/generic-deriving/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "generic-deriving";
-  version = "1.2.1";
-  sha256 = "0ld4kh3p3lmavxi4y25fpxvq75qk7bd87yvwcbj63j6af1v60h2z";
+  version = "1.2.2";
+  sha256 = "1k64c3wqvgcvwarv55v8c303l959rs01znq443wynzi7kz7xcfl9";
   meta = {
     description = "Generic programming library for generalised deriving";
     license = self.stdenv.lib.licenses.bsd3;
