76eaa4c4f011dd6e1fc7e37393a76a4e77983aa6
Peter Simons <simons@cryp.to>
2012-09-20 14:34:52 +0200

    haskell-HStringTemplate: update to version 0.6.10

--- pkgs/development/libraries/haskell/HStringTemplate/default.nix
+++ pkgs/development/libraries/haskell/HStringTemplate/default.nix
@@ -1,13 +1,14 @@
-{ cabal, deepseq, filepath, mtl, parsec, syb, sybWithClass, text
-, time, utf8String
+{ cabal, blazeBuilder, deepseq, filepath, mtl, parsec, syb
+, sybWithClass, text, time, utf8String
 }:
 
 cabal.mkDerivation (self: {
   pname = "HStringTemplate";
-  version = "0.6.9";
-  sha256 = "0xa665q5gya51vjkg1i6f6qk67jx28xcbxs5v1d9yr1f8djh5d9v";
+  version = "0.6.10";
+  sha256 = "0asv8n4l2j819cngxgdk5p0b4bqcxnmdc2mlp9b3s7mrs15ljxkk";
   buildDepends = [
-    deepseq filepath mtl parsec syb sybWithClass text time utf8String
+    blazeBuilder deepseq filepath mtl parsec syb sybWithClass text time
+    utf8String
   ];
   meta = {
     description = "StringTemplate implementation in Haskell";
