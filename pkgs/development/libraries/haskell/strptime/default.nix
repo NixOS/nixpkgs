5e3e55ea087c12ac96f12c02dd912e9e2066bb5a
Peter Simons <simons@cryp.to>
2012-09-20 14:34:53 +0200

    haskell-strptime: update to version 1.0.7

--- pkgs/development/libraries/haskell/strptime/default.nix
+++ pkgs/development/libraries/haskell/strptime/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "strptime";
-  version = "1.0.6";
-  sha256 = "1brzh22nrs2mg5h815vj8vlz0qn2jwm4y4sdp5zlszjxfsqc2hp7";
+  version = "1.0.7";
+  sha256 = "1x8z7ib66v8xif7gxdzsxi7ifnz75f4k2c1h2jsjq308srdzxjcr";
   buildDepends = [ time ];
   meta = {
     description = "Efficient parsing of LocalTime using a binding to C's strptime, with some extra features (i.e. fractional seconds)";
