7347039d0d7d7dfc804c9f86f7713e178f381d75
Peter Simons <simons@cryp.to>
2012-09-20 14:34:52 +0200

    haskell-binary: update to version 0.6.1.0

--- pkgs/development/libraries/haskell/binary/default.nix
+++ pkgs/development/libraries/haskell/binary/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "binary";
-  version = "0.6.0.0";
-  sha256 = "0p72w7f9nn19g2wggsh8x4z7y9s174f3drz9a5ln4x7h554swcxv";
+  version = "0.6.1.0";
+  sha256 = "0d423k37973f5v9mz9401zmsfdgspnf9h6s9xgr3zh19giz7c3js";
   meta = {
     homepage = "https://github.com/kolmodin/binary";
     description = "Binary serialisation for Haskell values using lazy ByteStrings";
