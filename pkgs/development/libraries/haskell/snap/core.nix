ddaea62f91e55517c318bbb07340e578855e78f9
Peter Simons <simons@cryp.to>
2012-09-21 13:46:17 +0200

    haskell-snap-core: update to version 0.9.2.1

--- pkgs/development/libraries/haskell/snap/core.nix
+++ pkgs/development/libraries/haskell/snap/core.nix
@@ -7,15 +7,14 @@
 
 cabal.mkDerivation (self: {
   pname = "snap-core";
-  version = "0.9.2";
-  sha256 = "04c1i5ccsb76yw7nyj8sxxnwq3ym14iygc3immdn2lqrm424vkkp";
+  version = "0.9.2.1";
+  sha256 = "18i0hfzc37q12nyp7g4sc87s4xcqz1crjwpnhh4y5fw66glzmq3q";
   buildDepends = [
     attoparsec attoparsecEnumerator blazeBuilder blazeBuilderEnumerator
     bytestringMmap caseInsensitive deepseq enumerator filepath HUnit
     MonadCatchIOTransformers mtl random regexPosix text time
     transformers unixCompat unorderedContainers vector zlibEnum
   ];
-  jailbreak = true;
   meta = {
     homepage = "http://snapframework.com/";
     description = "Snap: A Haskell Web Framework (core interfaces and types)";
