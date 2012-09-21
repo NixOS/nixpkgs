882ccb0f5280d96d0a3e01efa8d876a47cd8b6c0
Alexander Foremny <alexanderforemny@gmail.com>
2012-09-21 16:33:24 +0200

    Patch attoparsec-binary to work with GHC 7.6.1

--- pkgs/development/libraries/haskell/attoparsec-binary/default.nix
+++ pkgs/development/libraries/haskell/attoparsec-binary/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.1.0.1";
   sha256 = "1d3zjr8bh6d44v1vid0cvrrbyhn7xj4bn96vy36dzk7h7p87bzxa";
   buildDepends = [ attoparsec ];
+  patches = [ ./attoparsec-binary-ghc7.6.1.patch ];
   meta = {
     description = "Binary processing extensions to Attoparsec";
     license = self.stdenv.lib.licenses.bsd3;
