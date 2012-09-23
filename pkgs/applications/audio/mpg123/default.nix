72551c8ede4f18e712b512d9c4fd69609d456380
Song Wenwu <iyzsong@gmail.com>
2012-09-23 10:34:49 +0800

    mpg123: update to 1.14.4

--- pkgs/applications/audio/mpg123/default.nix
+++ pkgs/applications/audio/mpg123/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, alsaLib }:
 
 stdenv.mkDerivation {
-  name = "mpg123-1.12.3";
+  name = "mpg123-1.14.4";
 
   src = fetchurl {
-    url = mirror://sourceforge/mpg123/mpg123-1.12.3.tar.bz2;
-    sha256 = "1ij689s7jch3d4g0ja3jylaphallc8vgrsrm9b12254phnyy23xf";
+    url = mirror://sourceforge/mpg123/mpg123-1.14.4.tar.bz2;
+    sha256 = "04jw7sfnmk853awviq8c63mdk7s3r31p4kdn8r86pv5l9vr8k8cw";
   };
 
   buildInputs = [ alsaLib ];
