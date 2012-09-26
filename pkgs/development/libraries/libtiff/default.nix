99df2b42a719d9729508c1e2e6f59835f5d7c743
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-09-26 14:59:48 -0400

    libtiff: Update to 3.9.7

--- pkgs/development/libraries/libtiff/default.nix
+++ pkgs/development/libraries/libtiff/default.nix
@@ -1,27 +1,18 @@
 { stdenv, fetchurl, zlib, libjpeg }:
 
-let version = "3.9.6"; in
+let version = "3.9.7"; in
 
 stdenv.mkDerivation rec {
   name = "libtiff-${version}";
-  
+
   src = fetchurl {
     urls =
       [ "ftp://ftp.remotesensing.org/pub/libtiff/tiff-${version}.tar.gz"
         "http://download.osgeo.org/libtiff/tiff-${version}.tar.gz"
       ];
-    sha256 = "0cv8ml3fnkjx60163j69a9cklzlh8wxbvbql78s78q13as8i3fhg";
+    sha256 = "0spg1hr5rsrmg88sfzb05qnf0haspq7r5hvdkxg5zib1rva4vmpm";
   };
 
-  patchFlags = "-p0";
-
-  patches =
-    [ (fetchurl {
-        url = "http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/media-libs/tiff/files/tiff-3.9.5-CVE-2012-1173.patch?revision=1.2";
-        sha256 = "07v22lbx9vlqj1f5r2fzcjcr37b97mw5ayjnisgmk4nd1yjxv5qn";
-      })
-    ];
-  
   propagatedBuildInputs = [ zlib libjpeg ];
 
   enableParallelBuilding = true;
