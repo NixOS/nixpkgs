ced4dd874a75cd99e073ce8f95a5d5a7520c3b31
Peter Simons <simons@cryp.to>
2012-09-27 12:21:07 +0200

    haskell-hxt: update to version 9.3.1.0

--- pkgs/development/libraries/haskell/hxt/default.nix
+++ pkgs/development/libraries/haskell/hxt/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "hxt";
-  version = "9.2.2";
-  sha256 = "0ichjpshq10b11dyfv1q7rs2m190x3gplx6k54amlxv45nwd1s6r";
+  version = "9.3.1.0";
+  sha256 = "0nv7d7ffwq81671c7gyzaqx7xgrgs42svbq5xraij4jbq5406719";
   buildDepends = [
     binary deepseq filepath HUnit hxtCharproperties hxtRegexXmlschema
     hxtUnicode mtl network parsec
