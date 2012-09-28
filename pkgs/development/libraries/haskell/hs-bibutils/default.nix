be459eddd8b8f33070e3bbe513a7ce867a232dc3
Peter Simons <simons@cryp.to>
2012-09-28 10:46:39 +0200

    haskell-hs-bibutils: update to version 4.15

--- pkgs/development/libraries/haskell/hs-bibutils/default.nix
+++ pkgs/development/libraries/haskell/hs-bibutils/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "hs-bibutils";
-  version = "4.14";
-  sha256 = "1icdaayahwgfmckd93k26kic2wdgyl65lv08rnwydsi5vlqww32x";
+  version = "4.15";
+  sha256 = "1h2vqi6ymhx9wpfv5qzvq4fhc4iand93shsncp8nszk64acmz9z9";
   buildDepends = [ syb ];
   meta = {
     homepage = "http://gorgias.mine.nu/repos/hs-bibutils/";
