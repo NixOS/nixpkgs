3cf0b00b5a9d409a622ee5b9b04ed065910c043a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-10-02 11:48:54 -0400

    bind: Update to 9.7.6-P3
    Fixes CVE-2012-4244.

--- pkgs/servers/dns/bind/default.nix
+++ pkgs/servers/dns/bind/default.nix
@@ -1,15 +1,14 @@
 { stdenv, fetchurl, openssl, libtool, perl, libxml2 }:
 
-let
-  version = "9.7.6-P1";
-in
+let version = "9.7.6-P3"; in
+
 stdenv.mkDerivation rec {
 
   name = "bind-${version}";
 
   src = fetchurl {
     url = "http://ftp.isc.org/isc/bind9/${version}/${name}.tar.gz";
-    sha256 = "1xp7c3fpi3b6y1bz77mf7c98ic7rxp5lpwlmzqwsdrllip33qw1k";
+    sha256 = "0xx23dcy9rbky8ar89pg3s07njqy7667sfrnbqc334pvd2qxanjx";
   };
 
   patchPhase = ''
@@ -21,7 +20,7 @@ stdenv.mkDerivation rec {
   /* Why --with-libtool? */
   configureFlags = [ "--with-libtool" "--with-openssl=${openssl}"
     "--localstatedir=/var" ];
-      
+
   meta = {
     homepage = http://www.isc.org/software/bind;
     description = "ISC BIND: a domain name server";
