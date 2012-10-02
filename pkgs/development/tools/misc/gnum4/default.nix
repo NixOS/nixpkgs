b63305721d39107c04ef9fa1e74f3eb8f32beb79
Rickard Nilsson <rickynils@gmail.com>
2012-10-02 08:49:33 +0200

    gnum4: Disable tests for ARM.
    I can't get the `diversions' to pass on SheevaPlug. It builds fine otherwise.

--- pkgs/development/tools/misc/gnum4/default.nix
+++ pkgs/development/tools/misc/gnum4/default.nix
@@ -10,7 +10,8 @@ stdenv.mkDerivation {
 
   doCheck = !stdenv.isDarwin
     && !stdenv.isCygwin                    # XXX: `test-dup2' fails on Cygwin
-    && !stdenv.isSunOS;                    # XXX: `test-setlocale2.sh' fails
+    && !stdenv.isSunOS                     # XXX: `test-setlocale2.sh' fails
+    && !stdenv.isArm;                      # XXX: `diversions' fails on SheevaPlug
 
   # Upstream is aware of it; it may be in the next release.
   patches = [ ./s_isdir.patch ./readlink-EINVAL.patch ];
