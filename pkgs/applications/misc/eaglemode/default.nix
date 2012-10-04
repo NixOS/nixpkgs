73f79401b1d656ece1e39af240bf4f1233d510c8
Mathijs Kwik <mathijs@bluescreen303.nl>
2012-10-04 13:15:01 +0200

    eaglemode: remove unused xineLib dependency
    It seems the resulting output path has no reference to libxine, so it
    does not get used. Probably it needs some hard-coded link-paths as
    eaglemode wants to use dlopen for some things.
    
    If anyone wants to use eaglemode's xine support and fix this issue,
    please make it optional.

--- pkgs/applications/misc/eaglemode/default.nix
+++ pkgs/applications/misc/eaglemode/default.nix
@@ -1,20 +1,20 @@
-{ stdenv, fetchurl, perl, libX11, xineLib, libjpeg, libpng, libtiff, pkgconfig,
+{ stdenv, fetchurl, perl, libX11, libjpeg, libpng, libtiff, pkgconfig,
 librsvg, glib, gtk, libXext, libXxf86vm, poppler }:
 
 stdenv.mkDerivation {
   name = "eaglemode-0.84.0";
- 
+
   src = fetchurl {
     url = mirror://sourceforge/eaglemode/eaglemode-0.84.0.tar.bz2;
     sha256 = "0n20b419j0l7h7jr4s3f3n09ka0ysg9nqs8mcwsrx24rcq7nv0cs";
   };
- 
-  buildInputs = [ perl libX11 xineLib libjpeg libpng libtiff pkgconfig 
+
+  buildInputs = [ perl libX11 libjpeg libpng libtiff pkgconfig
     librsvg glib gtk libXxf86vm libXext poppler ];
- 
+
   # The program tries to dlopen both Xxf86vm and Xext, so we use the
   # trick on NIX_LDFLAGS and dontPatchELF to make it find them.
-  # I use 'yes y' to skip a build error linking with xineLib, 
+  # I use 'yes y' to skip a build error linking with xineLib,
   # because xine stopped exporting "_x_vo_new_port"
   #  http://sourceforge.net/projects/eaglemode/forums/forum/808824/topic/5115261
   buildPhase = ''
@@ -30,7 +30,7 @@ stdenv.mkDerivation {
     # Run 'eaglemode.sh', not 'eaglemode'.
     ln -s $out/eaglemode.sh $out/bin/eaglemode.sh
   '';
- 
+
   meta = {
     homepage = "http://eaglemode.sourceforge.net";
     description = "Zoomable User Interface";
