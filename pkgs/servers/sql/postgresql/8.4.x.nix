925aeb98d5fdbc3bc027ad71364110fdbb67d59d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-10-04 11:26:19 -0400

    PostgreSQL updates

--- pkgs/servers/sql/postgresql/8.4.x.nix
+++ pkgs/servers/sql/postgresql/8.4.x.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, zlib, ncurses, readline }:
 
-let version = "8.4.13"; in
+let version = "8.4.14"; in
 
 stdenv.mkDerivation rec {
   name = "postgresql-${version}";
 
   src = fetchurl {
     url = "mirror://postgresql/source/v${version}/${name}.tar.bz2";
-    sha256 = "1fccqkni64vg1pi4zzcl67bm9g2brrlzjn1vh6qlyfpsld139p90";
+    sha256 = "0fhk3mmk95p5gwmg2skqv1rfi7ylk8gw195hx8rska7fbdryfwhi";
   };
 
   buildInputs = [ zlib ncurses readline ];
