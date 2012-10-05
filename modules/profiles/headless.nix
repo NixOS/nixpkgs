0ddd147cfc26d84cb2ff70c3de20c5ece0d2abc2
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-10-04 23:25:33 -0400

    headless.nix: Mountall is gone

--- modules/profiles/headless.nix
+++ modules/profiles/headless.nix
@@ -14,15 +14,4 @@ with pkgs.lib;
 
   # Since we can't manually respond to a panic, just reboot.
   boot.kernelParams = [ "panic=1" "stage1panic=1" ];
-
-  # Since we don't have an (interactive) console, disable the
-  # emergency shell (started if mountall fails).
-  jobs."mount-failed".script = mkOverride 50
-    ''
-      ${pkgs.utillinux}/bin/logger -p user.emerg -t mountall "filesystem ‘$DEVICE’ could not be mounted on ‘$MOUNTPOINT’"
-    '';
-
-  # Likewise, redirect mountall output from the console to the default
-  # Upstart job log file.
-  jobs."mountall".console = mkOverride 50 "";
 }
