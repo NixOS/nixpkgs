df5abaf8b48ae89b0caa8e546119d523a1bd3571
Cillian de Róiste <goibhniu@fsfe.org>
2012-10-07 22:30:47 +0200

    Update the calf plugin pack to 0.0.19-rc7
    This is a significant update with loads of new features, plugins and GUIs

--- pkgs/applications/audio/calf/default.nix
+++ pkgs/applications/audio/calf/default.nix
@@ -1,17 +1,17 @@
-{ stdenv, fetchurl, cairo, expat, glib, gtk, jackaudio, ladspaH
+{ stdenv, fetchurl, cairo, expat, fftwSinglePrec, fluidsynth, glib, gtk, jackaudio, ladspaH
 , libglade, lv2, pkgconfig }:
 
 stdenv.mkDerivation rec {
   name = "calf-${version}";
-  version = "0.0.18.6";
+  version = "0.0.19-rc7";
 
   src = fetchurl {
     url = "mirror://sourceforge/calf/${name}.tar.gz";
-    sha256 = "03w6jjkrr6w8da6qzd0x4dlkg295c6jxby500x4cj07wpbpk6irh";
+    sha256 = "0515pzc7ishrq0j5hza83s0yp3x34r977h776lpky389whcyf45j";
   };
 
   buildInputs =
-    [ cairo jackaudio gtk glib expat libglade ladspaH lv2 pkgconfig ];
+    [ cairo jackaudio fftwSinglePrec fluidsynth gtk glib expat libglade ladspaH lv2 pkgconfig ];
 
   meta = with stdenv.lib; {
     homepage = http://calf.sourceforge.net;
