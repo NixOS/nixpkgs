318202951e8396e0fc1c8c6e49fcba0c1d2e27bc
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-10-08 14:43:47 -0400

    Revert "Remove cups-pdf-filter"
    This reverts commit 006002d16f24068668fef77ea8b2f3c653d22b4d.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8691,6 +8691,8 @@ let
 
   cups = callPackage ../misc/cups { };
 
+  cups_pdf_filter = callPackage ../misc/cups/pdf-filter.nix { };
+
   gutenprint = callPackage ../misc/drivers/gutenprint { };
 
   gutenprintBin = callPackage ../misc/drivers/gutenprint/bin.nix { };
