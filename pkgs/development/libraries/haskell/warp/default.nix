2112958f8a957d3d6d343963090e955d7d3b94ca
Peter Simons <simons@cryp.to>
2012-10-11 10:18:50 +0200

    haskell-warp: update to version 1.3.3.2

--- pkgs/development/libraries/haskell/warp/default.nix
+++ pkgs/development/libraries/haskell/warp/default.nix
@@ -5,14 +5,13 @@
 
 cabal.mkDerivation (self: {
   pname = "warp";
-  version = "1.3.3.1";
-  sha256 = "0nz5n574lgaii6i9ncc6dkzw2aw6g7hwjjq7v8njly2vpwygxn7s";
+  version = "1.3.3.2";
+  sha256 = "1w2i5nf3qh7p0xfrrp6x34rw3sxa48v703w1vlsikhyjdqs2crq8";
   buildDepends = [
     blazeBuilder blazeBuilderConduit caseInsensitive conduit hashable
     httpTypes liftedBase network networkConduit simpleSendfile
     transformers unixCompat void wai
   ];
-  patchPhase = "sed -i -e 's|, lifted-base.*|, lifted-base|' warp.cabal";
   meta = {
     homepage = "http://github.com/yesodweb/wai";
     description = "A fast, light-weight web server for WAI applications";
