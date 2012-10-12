74196058eeb32f655ea0cdafe40f9b601bcf213d
Joachim Schiele <js@lastlog.de>
2012-10-12 00:45:42 +0000

    added gsmartcontrol 0.8.7 to nixpkgs

---
+++ pkgs/tools/misc/gsmartcontrol/default.nix
@@ -0,0 +1,23 @@
+{ fetchurl, stdenv, smartmontools, gtk, gtkmm, libglademm, pkgconfig, pcre }:
+
+stdenv.mkDerivation rec {
+  version="0.8.7";
+  name = "gsmartcontrol";
+
+  src = fetchurl {
+    url = "http://artificialtime.com/gsmartcontrol/gsmartcontrol-${version}.tar.bz2";
+    sha256 = "1ipykzqpfvlr84j38hr7q2cag4imrn1gql10slp8bfrs4h1si3vh";
+  };
+
+  buildInputs = [ smartmontools gtk gtkmm libglademm pkgconfig pcre ];
+
+  #installTargets = "install datainstall";
+
+  meta = {
+    description = "GSmartControl is a graphical user interface for smartctl (from smartmontools package), which is a tool for querying and controlling SMART (Self-Monitoring, Analysis, and Reporting Technology) data on modern hard disk drives.";
+    homepage = http://gsmartcontrol.berlios.de;
+    license = "GPLv2+";
+    maintainers = with stdenv.lib.maintainers; [qknight];
+    platforms = with stdenv.lib.platforms; linux;
+  };
+}
