640a13c04c7b656bd9153c08c8aeba41c11b477b
Michael Raskin <7c6f434c@mail.ru>
2012-10-17 22:59:41 +0400

    Update Psi

--- pkgs/applications/networking/instant-messengers/psi/default.nix
+++ pkgs/applications/networking/instant-messengers/psi/default.nix
@@ -1,31 +1,32 @@
 { stdenv, fetchurl, aspell, qt4, zlib, sox, libX11, xproto, libSM
-, libICE, qca2, pkgconfig, qca2_ossl, liboil, speex, callPackage, which, glib }:
+, libICE, qca2, pkgconfig, qca2_ossl, liboil, speex, callPackage, which, glib
+, libXScrnSaver, scrnsaverproto
+}:
 
 stdenv.mkDerivation rec {
-  name = "psi-0.14";
+  name = "psi-0.15";
 
   src = fetchurl {
     url = "mirror://sourceforge/psi/${name}.tar.bz2";
-    sha256 = "1h54a1qryfva187sw9qnb4lv1d3h3lysqgw55v727swvslh4l0da";
+    sha256 = "593b5ddd7934af69c245afb0e7290047fd7dedcfd8765baca5a3a024c569c7e6";
   };
 
   buildInputs =
     [ aspell qt4 zlib sox libX11 xproto libSM libICE
-      qca2 qca2_ossl pkgconfig which glib
+      qca2 qca2_ossl pkgconfig which glib scrnsaverproto libXScrnSaver
     ];
 
   NIX_CFLAGS_COMPILE="-I${qca2}/include/QtCrypto";
 
   NIX_LDFLAGS="-lqca";
 
-  configureFlags =
-    [ " --with-zlib-inc=${zlib}/include "
-      " --disable-bundled-qca"
-    ];
-
   psiMedia = callPackage ./psimedia.nix { };
 
   enableParallelBuilding = true;
+
+  configureFlags = [
+    "--with-aspell-inc=${aspell}/include"
+    ];
 
   postInstall = ''
     PSI_PLUGINS="$out/lib/psi/plugins"
