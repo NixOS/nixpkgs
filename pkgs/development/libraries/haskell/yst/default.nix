5ff29b0e3de99d431bf5e251ee0ee7e12b96c8a0
Peter Simons <simons@cryp.to>
2012-10-23 18:29:59 +0200

    haskell-yst: update to version 0.3

--- pkgs/development/libraries/haskell/yst/default.nix
+++ pkgs/development/libraries/haskell/yst/default.nix
@@ -1,16 +1,16 @@
-{ cabal, csv, filepath, HsSyck, HStringTemplate, pandoc, parsec
-, split, time, utf8String, xhtml
+{ cabal, csv, filepath, HDBC, HDBCSqlite3, HsSyck, HStringTemplate
+, pandoc, parsec, split, time, utf8String, xhtml
 }:
 
 cabal.mkDerivation (self: {
   pname = "yst";
-  version = "0.2.4.1";
-  sha256 = "0y620p6kn1mky30fia63na5idppfjfmc828jcaa0ads08rmj5wgy";
+  version = "0.3";
+  sha256 = "1f09qcf2kqgq9w7irvzlwhv5sy0q3ml82ksza72hj0f5rfbyvfla";
   isLibrary = false;
   isExecutable = true;
   buildDepends = [
-    csv filepath HsSyck HStringTemplate pandoc parsec split time
-    utf8String xhtml
+    csv filepath HDBC HDBCSqlite3 HsSyck HStringTemplate pandoc parsec
+    split time utf8String xhtml
   ];
   meta = {
     homepage = "http://github.com/jgm/yst";
