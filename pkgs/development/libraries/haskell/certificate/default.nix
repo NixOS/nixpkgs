42d80326d43bf66d2fd8df404bbee2025953dbad
Peter Simons <simons@cryp.to>
2012-10-24 18:06:11 +0200

    haskell-certificate: update to version 1.3.0

--- pkgs/development/libraries/haskell/certificate/default.nix
+++ pkgs/development/libraries/haskell/certificate/default.nix
@@ -1,12 +1,16 @@
-{ cabal, asn1Data, cryptoPubkeyTypes, mtl, pem, time }:
+{ cabal, asn1Data, cryptohash, cryptoPubkeyTypes, filepath, mtl
+, pem, time
+}:
 
 cabal.mkDerivation (self: {
   pname = "certificate";
-  version = "1.2.8";
-  sha256 = "0lcyvkr0iqywwid9zlpzfvzpg70gvw7y0ij51ljadrwp882nj0ws";
+  version = "1.3.0";
+  sha256 = "1vhqbwbk0xpq74f01lkqifq7pgxdbfgq193gy57b9rwsnxvzqip1";
   isLibrary = true;
   isExecutable = true;
-  buildDepends = [ asn1Data cryptoPubkeyTypes mtl pem time ];
+  buildDepends = [
+    asn1Data cryptohash cryptoPubkeyTypes filepath mtl pem time
+  ];
   meta = {
     homepage = "http://github.com/vincenthz/hs-certificate";
     description = "Certificates and Key Reader/Writer";
