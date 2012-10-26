ebf80609bc8b8dc7f1bb495dc0ba6fc58b0df537
James Cook <jcook@cs.berkeley.edu>
2012-10-25 19:47:19 -0700

    mesa:  Use autoreconfHook.

--- pkgs/development/libraries/mesa/default.nix
+++ pkgs/development/libraries/mesa/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, fetchurl, autoconf, automake, libtool, flex, bison, pkgconfig, libdrm, file, expat, makedepend
-, llvm
+{ stdenv, fetchurl, autoconf, automake, autoreconfHook, libtool
+, flex, bison, pkgconfig, libdrm, file, expat, makedepend, llvm
 , libXxf86vm, libXfixes, libXdamage, glproto, dri2proto, libX11, libxcb, libXext
 , libXt, udev, enableTextureFloats ? false
 , python, libxml2Python }:
@@ -29,13 +29,11 @@ stdenv.mkDerivation {
     # Texture floats are patented, see docs/patents.txt
     + stdenv.lib.optionalString enableTextureFloats " --enable-texture-float";
 
-  buildInputs = [ autoconf automake libtool expat libdrm libXxf86vm libXfixes libXdamage glproto dri2proto
-    llvm
+  buildInputs = [ autoconf automake autoreconfHook libtool
+    expat libdrm libXxf86vm libXfixes libXdamage glproto dri2proto llvm
     libxml2Python libX11 libXext libxcb libXt udev ];
 
   buildNativeInputs = [ pkgconfig python makedepend file flex bison ];
-
-  preConfigure = "./autogen.sh";
 
   enableParallelBuilding = true;
 
