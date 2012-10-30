0dd3996ab4f2921f7f436329305fe02397794e57
Mathijs Kwik <mathijs@bluescreen303.nl>
2012-10-30 10:17:52 +0100

    node-packages: upgrade coffee-script to 1.4.0
    I'm not sure yet how to handle these upgrades.
    Should we keep the old version around?
    Should we only keep it around if other pkgs in nixpkgs depend on it?
    
    Probably people develop their own projects (that are not in nixpkgs)
    on top of these and we don't want to break these.
    
    For now, I think it's best just to keep old versions around, but
    update the (unversioned) link to the latest.
    
    External projects should then depend on explicit version numbers if
    they don't want stuff to break.
    
    If a certain package has +5 versions in nixpkgs, we can clear out the
    unuseful ones.

--- pkgs/top-level/node-packages.nix
+++ pkgs/top-level/node-packages.nix
@@ -178,8 +178,8 @@ let self = {
     ];
   };
 
-  "coffee-script" = self."coffee-script-1.3.3";
-  "coffee-script-~1" = self."coffee-script-1.3.3";
+  "coffee-script" = self."coffee-script-1.4.0";
+  "coffee-script-~1" = self."coffee-script-1.4.0";
 
   "coffee-script-~1.1.2" = self.buildNodePackage rec {
     name = "coffee-script-1.1.3";
@@ -197,6 +197,17 @@ let self = {
     src = fetchurl {
       url = "http://registry.npmjs.org/coffee-script/-/${name}.tgz";
       sha256 = "deedd2cf9d5abe2bad724e6809bec40efa07215dae85f44d78cd37736bb50bc5";
+    };
+    deps = [
+
+    ];
+  };
+
+  "coffee-script-1.4.0" = self.buildNodePackage rec {
+    name = "coffee-script-1.4.0";
+    src = fetchurl {
+      url = "http://registry.npmjs.org/coffee-script/-/${name}.tgz";
+      sha256 = "146e8985d89210b63dae83378fd851ccf54d38d7d11cadcdca01520d50882613";
     };
     deps = [
 
