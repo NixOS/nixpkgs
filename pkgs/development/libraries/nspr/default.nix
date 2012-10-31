db6abc4569d9732d4d9bbd716a8207a0fc500880
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-10-31 13:49:25 +0100

    nspr: Update to 4.9.3

--- pkgs/development/libraries/nspr/default.nix
+++ pkgs/development/libraries/nspr/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl }:
 
-let version = "4.9.2"; in
+let version = "4.9.3"; in
 
 stdenv.mkDerivation {
   name = "nspr-${version}";
 
   src = fetchurl {
     url = "http://ftp.mozilla.org/pub/mozilla.org/nspr/releases/v${version}/src/nspr-${version}.tar.gz";
-    sha1 = "455eab365f8fd732a4b014ca0af685f3c2dec598";
+    sha256 = "1igkfb7dm3fd59swa5m961rgc53z98l3jadk42c4yy76b85z78ww";
   };
 
   preConfigure = "cd mozilla/nsprpub";
