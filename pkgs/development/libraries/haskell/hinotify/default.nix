23604072cf18cd269bb9be980727682c6667d7f9
Peter Simons <simons@cryp.to>
2012-11-01 10:08:28 +0100

    haskell-hinotify: update to version 0.3.3

--- pkgs/development/libraries/haskell/hinotify/default.nix
+++ pkgs/development/libraries/haskell/hinotify/default.nix
@@ -2,11 +2,11 @@
 
 cabal.mkDerivation (self: {
   pname = "hinotify";
-  version = "0.3.2";
-  sha256 = "0gr9rv1af6w7g2hbjhz1livi5zfhzdswjyapvjz3d7cga906bj48";
+  version = "0.3.3";
+  sha256 = "0z8pd5zva25zii5kkh807kdkn4j9w9z74f2dw4kyflwidn0063fr";
   meta = {
-    homepage = "http://code.haskell.org/hinotify/README.html";
-    description = "Haskell binding to INotify";
+    homepage = "https://github.com/kolmodin/hinotify.git";
+    description = "Haskell binding to inotify";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [ self.stdenv.lib.maintainers.andres ];
