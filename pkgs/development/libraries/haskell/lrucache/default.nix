cada9e93e0ef5df293079746e07ea7782d81341b
Peter Simons <simons@cryp.to>
2012-11-01 10:08:28 +0100

    haskell-lrucache: update to version 1.1.1.1

--- pkgs/development/libraries/haskell/lrucache/default.nix
+++ pkgs/development/libraries/haskell/lrucache/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "lrucache";
-  version = "1.1.1";
-  sha256 = "05y0b2dbbp017hkbr0pz943956dm31g5xhma4bqnh49yd4lyn5l9";
+  version = "1.1.1.1";
+  sha256 = "0w310wsvin5hw3awpicnzddyifcq0844h52jwjcqnin81l2lqrfw";
   meta = {
     homepage = "http://github.com/chowells79/lrucache";
     description = "a simple, pure LRU cache";
