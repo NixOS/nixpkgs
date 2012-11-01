b74182cb9b3b02f8933a878de720e47f56b0942c
Peter Simons <simons@cryp.to>
2012-11-01 10:08:29 +0100

    haskell-unlambda: update to version 0.1.3

--- pkgs/development/libraries/haskell/unlambda/default.nix
+++ pkgs/development/libraries/haskell/unlambda/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "unlambda";
-  version = "0.1.2";
-  sha256 = "12x7jc5nwbhkhnylg9l29nr5y8p322avhb9zc0w2apy3fvrq7y93";
+  version = "0.1.3";
+  sha256 = "0clcpkhg23a7ma72rjjpl2w8jpg2mdn4rgm3vf0vqr7lbyma1h89";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [ mtl ];
