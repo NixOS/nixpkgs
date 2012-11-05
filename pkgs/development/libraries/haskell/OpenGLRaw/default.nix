eb1dff71ebd4034e9bc0cb37fdc2c40e443da2b9
Peter Simons <simons@cryp.to>
2012-11-05 16:19:51 +0100

    haskell-OpenGLRaw: update to version 1.3.0.0

--- pkgs/development/libraries/haskell/OpenGLRaw/default.nix
+++ pkgs/development/libraries/haskell/OpenGLRaw/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "OpenGLRaw";
-  version = "1.2.0.0";
-  sha256 = "1nwk93wlwh7gz2lb1dc88frmwik71g61a7k8xfiib2q5a8a8kf9r";
+  version = "1.3.0.0";
+  sha256 = "0ifp5inrm48hzpq0x9hlk5cxh2k64y05phmsdb5hydb7r6dcla32";
   extraLibraries = [ mesa ];
   meta = {
     homepage = "http://www.haskell.org/haskellwiki/Opengl";
