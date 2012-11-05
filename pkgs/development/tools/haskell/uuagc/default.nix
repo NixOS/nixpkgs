5ce4e11e40e20f47e8e8556acced2df934308910
Peter Simons <simons@cryp.to>
2012-11-05 16:19:52 +0100

    haskell-uuagc: update to version 0.9.42.1

--- pkgs/development/tools/haskell/uuagc/default.nix
+++ pkgs/development/tools/haskell/uuagc/default.nix
@@ -1,16 +1,12 @@
-{ cabal, filepath, haskellSrcExts, mtl, uuagcBootstrap, uuagcCabal
-, uulib
-}:
+{ cabal, filepath, haskellSrcExts, mtl, uuagcCabal, uulib }:
 
 cabal.mkDerivation (self: {
   pname = "uuagc";
-  version = "0.9.40.3";
-  sha256 = "053p7cbis843zn0qg8imc77xnfj4kna8wwfanxbj8kcapcqvwihl";
+  version = "0.9.42.1";
+  sha256 = "1xyzvv9rqzqdc7kd2rncafnglfg699sn3f4987kbsxzhilvmhw78";
   isLibrary = true;
   isExecutable = true;
-  buildDepends = [
-    filepath haskellSrcExts mtl uuagcBootstrap uuagcCabal uulib
-  ];
+  buildDepends = [ filepath haskellSrcExts mtl uuagcCabal uulib ];
   meta = {
     homepage = "http://www.cs.uu.nl/wiki/HUT/WebHome";
     description = "Attribute Grammar System of Universiteit Utrecht";
