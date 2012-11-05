1392f6166a0e0d32cfa3f3b7ab98e11fd97810f6
Peter Simons <simons@cryp.to>
2012-11-05 16:19:51 +0100

    haskell-dbus: update to version 0.10.4

--- pkgs/development/libraries/haskell/dbus/default.nix
+++ pkgs/development/libraries/haskell/dbus/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "dbus";
-  version = "0.10.3";
-  sha256 = "1l74whkrznlycl6rc1h63rc1vmvp6q2g8g92imycf8f4sizmigfq";
+  version = "0.10.4";
+  sha256 = "0cv4sgk1mdxc81jlky21k0y3zg7qii585xiapr1m589r5528gj2f";
   buildDepends = [
     cereal libxmlSax network parsec random text transformers vector
     xmlTypes
