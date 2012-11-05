efeadc8d3a0d94bc12eaa3de07e74507fbd0c544
Peter Simons <simons@cryp.to>
2012-11-05 16:19:52 +0100

    haskell-shakespeare: update to version 1.0.2

--- pkgs/development/libraries/haskell/shakespeare/default.nix
+++ pkgs/development/libraries/haskell/shakespeare/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "shakespeare";
-  version = "1.0.1.4";
-  sha256 = "01gw31g75v8r8hjvkv7mxvx7736ib9x9nqpxavpbrfplyxgk2y31";
+  version = "1.0.2";
+  sha256 = "1n7qymrn2f83a11zdh2950pf8rsqnc52blwi02amjxsvy2piidzm";
   buildDepends = [ parsec text ];
   meta = {
     homepage = "http://www.yesodweb.com/book/shakespearean-templates";
