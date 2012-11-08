8165e2ddb05185a5dcc8582d7973b69626da156a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-11-08 15:18:09 +0100

    Document that Java packages should install JARs in $out/lib/java

--- pkgs/tools/misc/jdiskreport/builder.sh
+++ pkgs/tools/misc/jdiskreport/builder.sh
@@ -2,13 +2,14 @@ source $stdenv/setup
 
 unzip $src
 
-mkdir -p $out/jars
-mv */*.jar $out/jars
-jar=$(ls $out/jars/jdiskreport-*.jar)
+jar=$(ls */*.jar)
+
+mkdir -p $out/lib/java
+mv $jar $out/lib/java
 
 mkdir -p $out/bin
 cat > $out/bin/jdiskreport <<EOF
 #! $SHELL -e
-exec $jdk/bin/java -jar $jar
+exec $jre/bin/java -jar $out/lib/java/$(basename $jar)
 EOF
 chmod +x $out/bin/jdiskreport
