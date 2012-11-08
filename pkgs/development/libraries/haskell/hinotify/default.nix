ede08a7c4abde936c3eaa19f2b95d243cdc0c0f4
Peter Simons <simons@cryp.to>
2012-11-08 14:56:39 +0100

    haskell-hinotify: downgrade to version 0.3.2
    This down-grade is necessary to make fsnotify happy, which in turn is
    required by Yesod: <https://github.com/mdittmer/hfsnotify/issues/24>.

--- pkgs/development/libraries/haskell/hinotify/default.nix
+++ pkgs/development/libraries/haskell/hinotify/default.nix
@@ -2,13 +2,12 @@
 
 cabal.mkDerivation (self: {
   pname = "hinotify";
-  version = "0.3.4";
-  sha256 = "05iqy4llf42k20a4hdc7p3hx30v030ljwi469ps8xxx36c9c5kmf";
+  version = "0.3.2";
+  sha256 = "0gr9rv1af6w7g2hbjhz1livi5zfhzdswjyapvjz3d7cga906bj48";
   meta = {
-    homepage = "https://github.com/kolmodin/hinotify.git";
-    description = "Haskell binding to inotify";
+    homepage = "http://code.haskell.org/hinotify/README.html";
+    description = "Haskell binding to INotify";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
-    maintainers = [ self.stdenv.lib.maintainers.andres ];
   };
 })
