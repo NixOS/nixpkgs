39e5c66df2daf1e3ef79b11dd2b04f84a5111511
Peter Simons <simons@cryp.to>
2012-11-08 15:03:39 +0100

    haskell-fsnotify: patch to support recent versions of hinotify

--- pkgs/development/libraries/haskell/fsnotify/default.nix
+++ pkgs/development/libraries/haskell/fsnotify/default.nix
@@ -5,6 +5,9 @@ cabal.mkDerivation (self: {
   version = "0.0.2";
   sha256 = "14pvyky3wg1k1lrv41rymi4mihvdpkx8vfv43wa0z6g4a6456ayz";
   buildDepends = [ hinotify systemFileio systemFilepath text time ];
+  patchPhase = ''
+    sed -i -e 's@hinotify == 0.3.2@hinotify >= 0.3.2@g' fsnotify.cabal
+  '';
   meta = {
     description = "Cross platform library for file creation, modification, and deletion notification";
     license = self.stdenv.lib.licenses.bsd3;
