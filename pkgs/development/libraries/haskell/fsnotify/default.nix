c867db9a14d864cd61c432cf9c4817dca4c9586b
Peter Simons <simons@cryp.to>
2012-11-08 14:57:36 +0100

    haskell-fsnotify: update to version 0.0.2

--- pkgs/development/libraries/haskell/fsnotify/default.nix
+++ pkgs/development/libraries/haskell/fsnotify/default.nix
@@ -5,7 +5,6 @@ cabal.mkDerivation (self: {
   version = "0.0.2";
   sha256 = "14pvyky3wg1k1lrv41rymi4mihvdpkx8vfv43wa0z6g4a6456ayz";
   buildDepends = [ hinotify systemFileio systemFilepath text time ];
-  jailbreak = true;
   meta = {
     description = "Cross platform library for file creation, modification, and deletion notification";
     license = self.stdenv.lib.licenses.bsd3;
