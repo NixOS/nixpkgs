aeb8d4c406f9c7f393713eaca9248259dbc5e3ff
Rob Vermaas <rob.vermaas@gmail.com>
2012-11-11 13:21:33 +0100

    Oops, should have at least parsed the expression when committing before having had coffee...

--- pkgs/applications/editors/sublime/default.nix
+++ pkgs/applications/editors/sublime/default.nix
@@ -6,11 +6,19 @@ assert stdenv.system == "i686-linux" || stdenv.system == "x86_64-linux";
 
 stdenv.mkDerivation rec {
   name = "sublimetext-2.0.1";
-  src = fetchurl {
-    name = "sublimetext-2.0.1.tar.bz2";
-    url = http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2;
-    sha256 = "0174dnp9zika5as25mcls5y0qzhh8mnc5ajxsxz7qjrk4lrzk3c5";
-  };
+  src = 
+    if stdenv.system == "i686-linux" then
+      fetchurl {
+        name = "sublimetext-2.0.1.tar.bz2";
+        url = http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.tar.bz2;
+        sha256 = "1x6vmlfn5mdbf23nyfr7dhhi6y60lnpcmqj59svl3bzvayijsxaf";
+      }
+    else
+      fetchurl {
+        name = "sublimetext-2.0.1.tar.bz2";
+        url = http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2;
+        sha256 = "0174dnp9zika5as25mcls5y0qzhh8mnc5ajxsxz7qjrk4lrzk3c5";
+      };
   buildCommand = ''
     tar xvf ${src}
     mkdir -p $out/bin
