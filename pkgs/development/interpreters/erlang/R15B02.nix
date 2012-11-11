73533ce2a083ea760a89f28d67f753dc612cfd78
Peter Simons <simons@cryp.to>
2012-11-11 20:44:37 +0100

    erlang: drop the leading 'R' from the Erlang version number so that nix-env recognizes it and allows "nix-env -i erlang" to work

--- pkgs/development/interpreters/erlang/R15B02.nix
+++ pkgs/development/interpreters/erlang/R15B02.nix
@@ -3,26 +3,26 @@
 
 assert wxSupport -> mesa != null && wxGTK != null && xlibs != null;
 
-let version = "R15B02"; in
+let version = "15B02"; in
 
 stdenv.mkDerivation {
   name = "erlang-" + version;
-  
+
   src = fetchurl {
-    url = "http://www.erlang.org/download/otp_src_" + version + ".tar.gz";
+    url = "http://www.erlang.org/download/otp_src_R{version}.tar.gz";
     sha256 = "03eb0bd640916666ff83df1330912225fbf555e0c8cf58bb35d8307a314f1158";
   };
-  
-  buildInputs = 
+
+  buildInputs =
     [ perl gnum4 ncurses openssl
     ] ++ stdenv.lib.optional wxSupport [ mesa wxGTK xlibs.libX11 ];
-  
+
   patchPhase = '' sed -i "s@/bin/rm@rm@" lib/odbc/configure erts/configure '';
-  
+
   preConfigure = ''
     export HOME=$PWD/../
     sed -e s@/bin/pwd@pwd@g -i otp_build
   '';
-  
+
   configureFlags = "--with-ssl=${openssl}";
 }
