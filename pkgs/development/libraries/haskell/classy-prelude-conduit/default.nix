7a066dccf5bdfdac42c1b8f0acade97a61834080
Peter Simons <simons@cryp.to>
2012-11-13 12:12:58 +0100

    haskell-classy-prelude-conduit: update to version 0.4.1

--- pkgs/development/libraries/haskell/classy-prelude-conduit/default.nix
+++ pkgs/development/libraries/haskell/classy-prelude-conduit/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "classy-prelude-conduit";
-  version = "0.4.0.1";
-  sha256 = "00cp39pm7jwijd2ykbvjxvg1nyhmb2rzpp4v5srazf0vbbbzgvl2";
+  version = "0.4.1";
+  sha256 = "0llir0xnnyhgxifay019x64jw7mnn9p1sqs1xwm14gjcqr2nqqg8";
   buildDepends = [ classyPrelude conduit xmlConduit ];
   meta = {
     homepage = "https://github.com/snoyberg/classy-prelude";
