b71948d34d5d8a18f411f1f3748c4b16e151aa2e
Peter Simons <simons@cryp.to>
2012-11-15 12:09:58 +0100

    haskell-yesod-core: update to version 1.1.4.2

--- pkgs/development/libraries/haskell/yesod-core/default.nix
+++ pkgs/development/libraries/haskell/yesod-core/default.nix
@@ -9,8 +9,8 @@
 
 cabal.mkDerivation (self: {
   pname = "yesod-core";
-  version = "1.1.4.1";
-  sha256 = "0ckgncxmlkiihyqddphpihijgypsdink5nlg6amjgp9a6y8qcwyn";
+  version = "1.1.4.2";
+  sha256 = "0gbkjd6r2srw41hfnibp8sfzzlck0bnyrwkf4bbjgzivr6iy1lkf";
   buildDepends = [
     aeson blazeBuilder blazeHtml blazeMarkup caseInsensitive cereal
     clientsession conduit cookie failure fastLogger hamlet httpTypes
