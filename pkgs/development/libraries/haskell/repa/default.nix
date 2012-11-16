2646e975f8862815b02d53ce2b8b518babf04130
Peter Simons <simons@cryp.to>
2012-11-16 15:59:15 +0100

    haskell-repa: update to version 3.2.2.3

--- pkgs/development/libraries/haskell/repa/default.nix
+++ pkgs/development/libraries/haskell/repa/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "repa";
-  version = "3.2.2.2";
-  sha256 = "1a7rgpy2qc42zc9si7p1zgi1m9b06dz533d2mvhizzac9k3fb5vq";
+  version = "3.2.2.3";
+  sha256 = "04y8yrfh0khk7w9wv61fh2rfygw8r6g9c3spzki6kw63byr01m5c";
   buildDepends = [ QuickCheck vector ];
   jailbreak = true;
   meta = {
