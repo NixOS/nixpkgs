3aef242cc02e0146bde9ebc948d3a1a436f623b9
Cillian de Róiste <goibhniu@fsfe.org>
2012-11-17 20:02:48 +0100

    Update xf86-video-nested to work on current NixOS Xorg

--- pkgs/os-specific/linux/xf86-video-nested/default.nix
+++ pkgs/os-specific/linux/xf86-video-nested/default.nix
@@ -1,30 +1,31 @@
 { stdenv, fetchgit, autoconf, automake, fontsproto, libX11, libXext
-, libtool, pkgconfig, renderproto, utilmacros, xorgserver
+, libtool, pixman, pkgconfig, renderproto, utilmacros, xorgserver
 }:
 
 stdenv.mkDerivation {
-  name = "xf86-video-nested-2011-09-12";
+  name = "xf86-video-nested-2012-06-15";
 
-  # Breaks at 8d80f2e415e9e58ca481fe52ae8f2290e25de898 for Xorg 7.6
   src = fetchgit {
     url = git://anongit.freedesktop.org/xorg/driver/xf86-video-nested;
-    rev = "fceee1716625badf698ca27dd5788a4deb8533bc";
-    sha256 = "6b3544ddcf40602364fd0e528f6e677c37ef8d08f6c4e756caea7e29abf200f7"; 
+    rev = "ad48dc6eb98776a8a886f26f31c0110352fa1da4";
+    sha256 = "43a102405acdcdb346ab197b33c8fa724d2140f33754f8ee3941a0eea152735c";
   };
 
-  # Fixed in e123277d10337a1c3b853118df0d1becdddf3b77
-  patchPhase = "sed -e 's/Werror/Werror -Wno-extra-portability/g' -i configure.ac";
-
-  buildInputs = 
-    [ autoconf automake fontsproto libX11 libXext libtool pkgconfig
-      renderproto utilmacros xorgserver
+  buildInputs =
+    [ autoconf automake fontsproto libX11 libXext libtool pixman
+      pkgconfig renderproto utilmacros xorgserver
     ];
 
-  configureScript = "./autogen.sh";
+
+  configurePhase = ''
+    autoreconf -fvi
+    ./configure --prefix=$out CFLAGS="-I${pixman}/include/pixman-1"
+  '';
 
   meta = {
     homepage = git://anongit.freedesktop.org/xorg/driver/xf86-video-nested;
     description = "Driver to run Xorg on top of Xorg or something else";
     maintainers = [ stdenv.lib.maintainers.goibhniu ];
+    platforms = stdenv.lib.platforms.linux;
   };
 }
