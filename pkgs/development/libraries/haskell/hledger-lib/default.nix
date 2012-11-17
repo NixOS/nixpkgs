623c57b5c7e79dccfdd2fda3ac581045c3c6f6c0
Peter Simons <simons@cryp.to>
2012-11-17 11:39:25 +0100

    haskell-hledger-lib: update to version 0.19.1

--- pkgs/development/libraries/haskell/hledger-lib/default.nix
+++ pkgs/development/libraries/haskell/hledger-lib/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "hledger-lib";
-  version = "0.19";
-  sha256 = "1kbjal838b3k0rmvdrndmyjngvyfwpmzh6y8kir4l2nf31jxwjbs";
+  version = "0.19.1";
+  sha256 = "19ccbf9g1garwg56ig4qckz1zky89g1z71nwfbwi4v57bjw53ab4";
   buildDepends = [
     cmdargs csv filepath HUnit mtl parsec regexpr safe shakespeareText
     split time transformers utf8String
