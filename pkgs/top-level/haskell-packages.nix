9fa9e0520fa1ae68debba5fedcb226743c2f5245
Peter Simons <simons@cryp.to>
2012-11-20 14:39:37 +0100

    haskell-regex-pcre-builtin: removed from Nixpkgs -- use the superior regex-pcre library instead

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -1427,8 +1427,6 @@ let result = let callPackage = x : y : modifyPrio (newScope result.final x y);
   regexPCRE = callPackage ../development/libraries/haskell/regex-pcre {};
   regexPcre = self.regexPCRE;
 
-  regexPcreBuiltin = callPackage ../development/libraries/haskell/regex-pcre-builtin {};
-
   regexpr = callPackage ../development/libraries/haskell/regexpr {};
 
   regular = callPackage ../development/libraries/haskell/regular {};
