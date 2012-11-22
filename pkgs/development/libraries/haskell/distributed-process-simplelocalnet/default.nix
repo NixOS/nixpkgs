763739107ec0d295a90ae745abdecce51dc334eb
Peter Simons <simons@cryp.to>
2012-11-22 16:59:32 +0100

    haskell-distributed-process-simplelocalnet: update to version 0.2.0.8

--- pkgs/development/libraries/haskell/distributed-process-simplelocalnet/default.nix
+++ pkgs/development/libraries/haskell/distributed-process-simplelocalnet/default.nix
@@ -5,8 +5,8 @@
 
 cabal.mkDerivation (self: {
   pname = "distributed-process-simplelocalnet";
-  version = "0.2.0.7";
-  sha256 = "0jxbxacvdg4pf65s6r48nck45g8dfsarks3m2pdn73gjn4cd81c7";
+  version = "0.2.0.8";
+  sha256 = "01kzqfbrkl9zzknw0gbdh1c1lss911lphagn2sw6nzl9xpnhjqk0";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
