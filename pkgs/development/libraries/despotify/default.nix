3d49da0c0a99ab6d6df283d66b3dac417521b5cf
Alexander Foremny <alexanderforemny@gmail.com>
2012-11-25 13:56:45 +0100

    Add complete meta attribute

--- pkgs/development/libraries/despotify/default.nix
+++ pkgs/development/libraries/despotify/default.nix
@@ -21,7 +21,14 @@ stdenv.mkDerivation rec {
   installPhase = "make LDCONFIG=true INSTALL_PREFIX=$out install";
 
   meta = {
-    homepage = "despotify.se";
+    description = "Open source Spotify client and library";
+    longDescription = ''
+      despotify is a open source implementation of the Spotify API.  This
+      package provides both a library and a few already quite useful,
+      proof-of-concept clients.
+    '';
+    homepage = "http://despotify.se";
+    license = stdenv.lib.licenses.bsd2;
   };
 
 }
