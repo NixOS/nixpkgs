7e4d8632529ff02cc759c12b83fb87da823acf47
Peter Simons <simons@cryp.to>
2012-11-26 17:36:35 +0100

    haskell-liblastfm: update to version 0.0.3.8

--- pkgs/development/libraries/haskell/liblastfm/default.nix
+++ pkgs/development/libraries/haskell/liblastfm/default.nix
@@ -4,14 +4,11 @@
 
 cabal.mkDerivation (self: {
   pname = "liblastfm";
-  version = "0.0.3.7";
-  sha256 = "004mi6515gd6vhk7fjd63rwwnf3micm3z1kwcn1f73hi70p27ma0";
+  version = "0.0.3.8";
+  sha256 = "0icx86x3w85z0pqdxcch583j6jk5id5aw9gf24266mgfg5k6iwdy";
   buildDepends = [
     aeson attoparsec curl mtl pureMD5 urlencoded utf8String xml
   ];
-  patchPhase = ''
-    sed -i -e 's|curl == .*,|curl,|' -e 's|urlencoded .*,|urlencoded,|' liblastfm.cabal
-  '';
   meta = {
     description = "Wrapper to Lastfm API";
     license = self.stdenv.lib.licenses.mit;
