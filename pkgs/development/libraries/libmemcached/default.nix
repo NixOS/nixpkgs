6f4c88c7b9d3f19a3aeb291e45cc5f8aa1bbbb1a
Rob Vermaas <rob.vermaas@gmail.com>
2012-12-02 15:19:14 +0100

    Use older libmemcached 1.0.7

--- pkgs/development/libraries/libmemcached/default.nix
+++ pkgs/development/libraries/libmemcached/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, cyrus_sasl, libevent }:
 
 stdenv.mkDerivation rec {
-  name = "libmemcached-1.0.14";
+  name = "libmemcached-1.0.7";
   
   src = fetchurl {
-    url = https://launchpad.net/libmemcached/1.0/1.0.14/+download/libmemcached-1.0.14.tar.gz;
-    sha256 = "0swl3r7m35rx7abkfycpcknbf83y7l2azq9zscms2rc99cnfmsij";
+    url = https://launchpad.net/libmemcached/1.0/1.0.7/+download/libmemcached-1.0.7.tar.gz;
+    sha256 = "10cdczkgqiirjy7jwfdk49namqi4jiv1djqrf5fxbaiyfg4qdyiy";
   };
   
   buildInputs = [ cyrus_sasl libevent ];
