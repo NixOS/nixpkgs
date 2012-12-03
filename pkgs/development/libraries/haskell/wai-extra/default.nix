2b4569f528d356bbf7e823b01ad0553e42e632b6
Peter Simons <simons@cryp.to>
2012-12-03 16:11:07 +0100

    haskell-wai-extra: update to version 1.3.0.5

--- pkgs/development/libraries/haskell/wai-extra/default.nix
+++ pkgs/development/libraries/haskell/wai-extra/default.nix
@@ -6,8 +6,8 @@
 
 cabal.mkDerivation (self: {
   pname = "wai-extra";
-  version = "1.3.0.4";
-  sha256 = "1i909qipmnwx161c0xlqzrwcpbv6si2rrbhfs524dicgr1jdijcc";
+  version = "1.3.0.5";
+  sha256 = "0xzsnsf4sdbzkw92xyzmyi6qp2qpbh5dj3579sppcihdq9djj8l8";
   buildDepends = [
     ansiTerminal blazeBuilder blazeBuilderConduit caseInsensitive
     conduit dataDefault dateCache fastLogger httpTypes network
