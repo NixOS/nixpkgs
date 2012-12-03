57631b141e20874f1597c7764fece86bd3f2673f
Peter Simons <simons@cryp.to>
2012-12-03 16:11:04 +0100

    haskell-conduit: update to version 0.5.5

--- pkgs/development/libraries/haskell/conduit/default.nix
+++ pkgs/development/libraries/haskell/conduit/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "conduit";
-  version = "0.5.4.2";
-  sha256 = "0v79bjdn3pvfsyy48h1wjpzyds2sa963wqii9qz7dfkdfq0s825y";
+  version = "0.5.5";
+  sha256 = "0av12gaxsrfmsbs70y532wfwnpz9v6ymn182dr8phpqn8d9lx2iq";
   buildDepends = [
     liftedBase monadControl resourcet text transformers
     transformersBase void
