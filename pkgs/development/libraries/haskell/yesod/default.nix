e9fc18f1e1772eee647b146796c3bef590d32c1a
Peter Simons <simons@cryp.to>
2012-12-03 16:11:07 +0100

    haskell-yesod: update to version 1.1.5

--- pkgs/development/libraries/haskell/yesod/default.nix
+++ pkgs/development/libraries/haskell/yesod/default.nix
@@ -1,8 +1,8 @@
-{ cabal, attoparsec, base64Bytestring, blazeBuilder, blazeHtml
-, blazeMarkup, Cabal, conduit, fileEmbed, filepath, fsnotify
-, ghcPaths, hamlet, httpConduit, httpReverseProxy, httpTypes
-, liftedBase, monadControl, network, optparseApplicative, parsec
-, projectTemplate, resourcet, shakespeare, shakespeareCss
+{ cabal, aeson, attoparsec, base64Bytestring, blazeBuilder
+, blazeHtml, blazeMarkup, Cabal, conduit, fileEmbed, filepath
+, fsnotify, ghcPaths, hamlet, httpConduit, httpReverseProxy
+, httpTypes, liftedBase, monadControl, network, optparseApplicative
+, parsec, projectTemplate, resourcet, shakespeare, shakespeareCss
 , shakespeareJs, shakespeareText, split, systemFileio
 , systemFilepath, tar, text, time, transformers, unixCompat
 , unorderedContainers, wai, waiExtra, warp, yaml, yesodAuth
@@ -12,19 +12,19 @@
 
 cabal.mkDerivation (self: {
   pname = "yesod";
-  version = "1.1.4";
-  sha256 = "1na50j7cd89mxk7ss67xlj703spvkpzcwgp7qs1pn2x3xsm7vrm2";
+  version = "1.1.5";
+  sha256 = "1w34cwnqd92hjrjhqikpgzkjd4fnv3n844zziprk5kg0mlrylpvq";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
-    attoparsec base64Bytestring blazeBuilder blazeHtml blazeMarkup
-    Cabal conduit fileEmbed filepath fsnotify ghcPaths hamlet
-    httpConduit httpReverseProxy httpTypes liftedBase monadControl
-    network optparseApplicative parsec projectTemplate resourcet
-    shakespeare shakespeareCss shakespeareJs shakespeareText split
-    systemFileio systemFilepath tar text time transformers unixCompat
-    unorderedContainers wai waiExtra warp yaml yesodAuth yesodCore
-    yesodDefault yesodForm yesodJson yesodPersistent zlib
+    aeson attoparsec base64Bytestring blazeBuilder blazeHtml
+    blazeMarkup Cabal conduit fileEmbed filepath fsnotify ghcPaths
+    hamlet httpConduit httpReverseProxy httpTypes liftedBase
+    monadControl network optparseApplicative parsec projectTemplate
+    resourcet shakespeare shakespeareCss shakespeareJs shakespeareText
+    split systemFileio systemFilepath tar text time transformers
+    unixCompat unorderedContainers wai waiExtra warp yaml yesodAuth
+    yesodCore yesodDefault yesodForm yesodJson yesodPersistent zlib
   ];
   meta = {
     homepage = "http://www.yesodweb.com/";
