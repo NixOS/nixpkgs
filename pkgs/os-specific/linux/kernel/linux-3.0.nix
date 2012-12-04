47362f67ea5a358d80ccf2c94ed1756a00e32c24
Mathijs Kwik <mathijs@bluescreen303.nl>
2012-12-04 10:38:39 +0100

    linux-3.0: upgrade to 3.0.54

--- pkgs/os-specific/linux/kernel/linux-3.0.nix
+++ pkgs/os-specific/linux/kernel/linux-3.0.nix
@@ -230,7 +230,7 @@ in
 import ./generic.nix (
 
   rec {
-    version = "3.0.53";
+    version = "3.0.54";
 
     preConfigure = ''
       substituteInPlace scripts/depmod.sh --replace '-b "$INSTALL_MOD_PATH"' ""
@@ -238,7 +238,7 @@ import ./generic.nix (
 
     src = fetchurl {
       url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-      sha256 = "0nrlfsi3y3cw398yz4qqh54ymzwxj3jm85g74zm8z2bhqnwmd28d";
+      sha256 = "0jqdqp5rx05ph2wkj431rdcm9s8jpbzlbl53pbb6vp7yr3nichvg";
     };
 
     config = configWithPlatform stdenv.platform;
