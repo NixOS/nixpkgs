c4038db19df17b2e3ff8f82d691b67c1fcf52d3f
Peter Simons <simons@cryp.to>
2012-12-05 16:43:49 +0100

    dlx: install both hp.m and new.m microcode

--- pkgs/misc/emulators/dlx/default.nix
+++ pkgs/misc/emulators/dlx/default.nix
@@ -14,7 +14,7 @@ stdenv.mkDerivation {
     mkdir -p $out/include/dlx $out/share/dlx/{examples,doc} $out/bin
     mv -v masm mon dasm $out/bin/
     mv -v *.i auto.a $out/include/dlx/
-    mv -v *.a hp.m $out/share/dlx/examples/
+    mv -v *.a *.m $out/share/dlx/examples/
     mv -v README.txt MANUAL.TXT $out/share/dlx/doc/
   '';
 
