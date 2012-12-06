d1ea6e9f24e53def3035c80e10ee00390938e923
Peter Simons <simons@cryp.to>
2012-12-06 11:55:11 +0100

    haskell-cipher-aes: update to version 0.1.5

--- pkgs/development/libraries/haskell/cipher-aes/default.nix
+++ pkgs/development/libraries/haskell/cipher-aes/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "cipher-aes";
-  version = "0.1.4";
-  sha256 = "0yidq4swwhn1ldm16faraa00gpy946r9ndjkwhfps4pb3h96z0hz";
+  version = "0.1.5";
+  sha256 = "0n0qbq2hwyksdbr6fn7yj5vwicmdrn58mfz0dprl8fj456r4j3kn";
   meta = {
     homepage = "http://github.com/vincenthz/hs-cipher-aes";
     description = "Fast AES cipher implementation with advanced mode of operations";
