47321f0da921c69c13e0e3d57ca5fa45ccc55fac
Peter Simons <simons@cryp.to>
2012-12-08 11:35:02 +0100

    haskell-test-framework: update to version 0.8

--- pkgs/development/libraries/haskell/test-framework/default.nix
+++ pkgs/development/libraries/haskell/test-framework/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "test-framework";
-  version = "0.7.0";
-  sha256 = "1v2kv59j98lmmgggxq8i3yq8v750l3c5xp7aq1az7k6n224yblab";
+  version = "0.8";
+  sha256 = "1w895nq357zpc4v6vr5nbszyrw7cpsjq5bj38vdd10bfpjjmijcl";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
