32ee67fb5c57c230f1c30c910c347cd397468503
Peter Simons <simons@cryp.to>
2012-12-08 11:35:01 +0100

    haskell-iproute: update to version 1.2.8

--- pkgs/development/libraries/haskell/iproute/default.nix
+++ pkgs/development/libraries/haskell/iproute/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "iproute";
-  version = "1.2.7";
-  sha256 = "07ixxq45w5wzvfrvsv2b206kygiqn1v3bcclkd98afjpc6mv3ld3";
+  version = "1.2.8";
+  sha256 = "0vialraqr8r5d4bvknp3hd9412vpva43nqyv6y7bj0505xxr06l2";
   buildDepends = [ appar byteorder network ];
   meta = {
     homepage = "http://www.mew.org/~kazu/proj/iproute/";
