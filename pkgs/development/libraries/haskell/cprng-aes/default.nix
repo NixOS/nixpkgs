d0eb00b5785bd403f50fb5b161154b87862b9173
Peter Simons <simons@cryp.to>
2012-12-10 12:43:39 +0100

    haskell-cprng-aes: update to version 0.3.1

--- pkgs/development/libraries/haskell/cprng-aes/default.nix
+++ pkgs/development/libraries/haskell/cprng-aes/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "cprng-aes";
-  version = "0.3.0";
-  sha256 = "1a8imapda8k0rf0bvag1iin66f2r97pfgip7dkvpvmdkp3j1212h";
+  version = "0.3.1";
+  sha256 = "0z1kpgy9d4yp1vmcparsv3r5g1khv2yqqkr99ac3mgvr6pyh24dk";
   buildDepends = [
     cipherAes cryptoApi cryptoRandomApi entropy random
   ];
