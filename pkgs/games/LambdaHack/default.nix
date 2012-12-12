de81ec924b8a5b38d4fdbdd490d916a46135f3c6
Peter Simons <simons@cryp.to>
2012-12-12 11:54:27 +0100

    haskell-LambdaHack: update to version 0.2.6.5

--- pkgs/games/LambdaHack/default.nix
+++ pkgs/games/LambdaHack/default.nix
@@ -1,12 +1,16 @@
-{ cabal, binary, ConfigFile, filepath, gtk, mtl, random, zlib }:
+{ cabal, binary, ConfigFile, filepath, gtk, miniutter, mtl, random
+, text, zlib
+}:
 
 cabal.mkDerivation (self: {
   pname = "LambdaHack";
-  version = "0.2.6";
-  sha256 = "03adjwzbql1k1ky05vivry7waa8p41ha3lsnv9j9mdgpwqldypwd";
+  version = "0.2.6.5";
+  sha256 = "114s3adqs5mh566dbn0bb20v088wgg8arsm6m8hs9vx8j3jc8nx5";
   isLibrary = true;
   isExecutable = true;
-  buildDepends = [ binary ConfigFile filepath gtk mtl random zlib ];
+  buildDepends = [
+    binary ConfigFile filepath gtk miniutter mtl random text zlib
+  ];
   meta = {
     homepage = "http://github.com/kosmikus/LambdaHack";
     description = "A roguelike game engine in early and active development";
