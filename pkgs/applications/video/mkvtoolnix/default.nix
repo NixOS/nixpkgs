6776756882e244f9fcd1eb018e515790e70374eb
Mathijs Kwik <mathijs@bluescreen303.nl>
2012-12-13 13:35:39 +0100

    mkvtoolnix: upgrade to 5.9.0

--- pkgs/applications/video/mkvtoolnix/default.nix
+++ pkgs/applications/video/mkvtoolnix/default.nix
@@ -16,11 +16,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "mkvtoolnix-5.8.0";
+  name = "mkvtoolnix-5.9.0";
 
   src = fetchurl {
     url = "http://www.bunkus.org/videotools/mkvtoolnix/sources/${name}.tar.bz2";
-    sha256 = "0q294zk5cpfh1s89n70d9b2fs14rlacdlnhchlsjmf1mq3jcg7iw";
+    sha256 = "1qdxzi72w5p77brlpp7y7llsgzlvl4p8fk1kzg934cqw6cqza4yr";
   };
 
   buildInputs = [ libmatroska flac libvorbis file boost lzo xdg_utils expat wxGTK zlib ruby gettext pkgconfig curl ];
