3f1cc9ba24815d657700c3db91811c609b6df896
Peter Simons <simons@cryp.to>
2012-12-23 20:14:09 +0100

    haskell-diagrams-contrib: update to version 0.6.0.1

--- pkgs/development/libraries/haskell/diagrams/contrib.nix
+++ pkgs/development/libraries/haskell/diagrams/contrib.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "diagrams-contrib";
-  version = "0.6";
-  sha256 = "059ljwsbrkzj2wvx9q4viinz46nw5lf4yjmx2c1dmwaqfz3i7j7i";
+  version = "0.6.0.1";
+  sha256 = "1wrfdsfb2yj93wq2ykxjyg6g0q56bmxd2rc6r3gd0zcs8kiimaar";
   buildDepends = [
     colour dataDefault diagramsLib forceLayout lens mtl vectorSpace
   ];
