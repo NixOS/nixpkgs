764ebc29ab3efdd708c51c98168e0a86254c80d1
Lluís Batlle i Rossell <viric@viric.name>
2012-12-24 15:10:40 +0000

    Adding the armv6l bootstrap; I forgot that file in previous commits.

---
+++ pkgs/stdenv/linux/bootstrap/armv6l/default.nix
@@ -0,0 +1,13 @@
+{
+  sh = ../armv5tel/sh;
+  bzip2 = ../armv5tel/bzip2;
+  mkdir = ../armv5tel/mkdir;
+  cpio = ../armv5tel/cpio;
+  ln = ../armv5tel/ln;
+  curl = ../armv5tel/curl.bz2;
+
+  bootstrapTools = {
+    url = "file:///root/v6/bootstrap-tools.cpio.bz2";
+    sha256 = "01s4z461jv9plsxwkspjakdvjxzd5pd84i73nc2ynag5hmjyj63d";
+  };
+}
