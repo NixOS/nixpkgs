cfdc532102c52ebe63fe51012362963ee7c0039b
Lluís Batlle i Rossell <viric@viric.name>
2012-12-30 13:50:31 +0000

    Updating bootstrap tools to ones with glibc 2.17
    This solves the problem of the change of name in ld.so between
    glibc 2.13 and 2.17 (at least for armhf). Some comments about it in
    https://github.com/NixOS/nixpkgs/issues/234#issuecomment-11764352
    
    As there says, without this, gmp (after glibc built) tests crashed
    - segfault.

--- pkgs/stdenv/linux/bootstrap/armv6l/default.nix
+++ pkgs/stdenv/linux/bootstrap/armv6l/default.nix
@@ -8,8 +8,8 @@
 
   bootstrapTools = {
     # Built from make-bootstrap-tools-crosspi.nix
-    # nixpkgs rev 87ec7b49b120950a260d9733de7f34d7c2bffb98
+    # nixpkgs rev eb0422e4c1263a65a9b2b954fe10a1e03d67db3e
     url = http://viric.name/tmp/nix/pi/bootstrap-tools.cpio.bz2;
-    sha256 = "04qy8cqd30pqhil62b9sx67ijdspf9npx2snwwrcwvk3zbyhcll3";
+    sha256 = "1zb27x5h54k51yrvn3sy4wb1qprx8iv2kfbgklxwc0mcxp9b7ccd";
   };
 }
