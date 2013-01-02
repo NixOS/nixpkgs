9cc65f6893077883ee97f1462a156235ba422427
Lluís Batlle i Rossell <viric@viric.name>
2013-01-02 20:01:49 +0100

    Setting coreutils to 8.20 for linux, 8.19 for darwin
    As Eelco noted in 0ecd1f62c799dffbdbd4bc1ba8120bfb4a4c7a6f,
    Darwin can't build 8.20. But linux can't build 8.19 now.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -570,10 +570,15 @@ let
 
   convmv = callPackage ../tools/misc/convmv { };
 
-  coreutils = callPackage ../tools/misc/coreutils {
-    # TODO: Add ACL support for cross-Linux.
-    aclSupport = crossSystem == null && stdenv.isLinux;
-  };
+  coreutils = if stdenv.isDarwin then
+      # 8.20 doesn't build on Darwin
+      (callPackage ../tools/misc/coreutils/8.19) 
+    else
+      (callPackage ../tools/misc/coreutils)
+    {
+      # TODO: Add ACL support for cross-Linux.
+      aclSupport = crossSystem == null && stdenv.isLinux;
+    };
 
   cpio = callPackage ../tools/archivers/cpio { };
 
