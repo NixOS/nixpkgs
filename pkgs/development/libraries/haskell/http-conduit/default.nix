35fbdd0e8ce85d327bbaa6eae19134f3f4cf731f
Peter Simons <simons@cryp.to>
2013-01-04 19:17:22 +0100

    haskell-http-conduit: update to version 1.8.6.1

--- pkgs/development/libraries/haskell/http-conduit/default.nix
+++ pkgs/development/libraries/haskell/http-conduit/default.nix
@@ -8,8 +8,8 @@
 
 cabal.mkDerivation (self: {
   pname = "http-conduit";
-  version = "1.8.5.2";
-  sha256 = "0c1a6iknvi34sh97j7cfzwyikcz0kdz4vgsc47lr7c2a75gl0via";
+  version = "1.8.6.1";
+  sha256 = "1vkfld5kn8fql78mw8zwsp524m07kr4a10c411rzpv6xqx92azy7";
   buildDepends = [
     asn1Data attoparsec attoparsecConduit base64Bytestring blazeBuilder
     blazeBuilderConduit caseInsensitive certificate conduit cookie
