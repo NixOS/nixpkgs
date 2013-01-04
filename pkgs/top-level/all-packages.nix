440bc6ec2a17697b379a637b9e84a274bec3a719
Peter Simons <simons@cryp.to>
2013-01-04 20:55:41 +0100

    all-packages.nix: strip trailing whitespace

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5001,7 +5001,7 @@ let
   telepathy_qt = callPackage ../development/libraries/telepathy/qt { };
 
   tinyxml = tinyxml2;
-  
+
   tinyxml2 = callPackage ../development/libraries/tinyxml/2.6.2.nix { };
 
   tk = callPackage ../development/libraries/tk { };
