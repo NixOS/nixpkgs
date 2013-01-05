11eb687e572f1a799753d202b97b0ba6846f5e88
Lluís Batlle i Rossell <viric@viric.name>
2013-01-05 21:50:59 +0000

    Fixing nettools with headers >=3.6 (no more strip)
    I remove the strip (Metricom radio) support

--- pkgs/os-specific/linux/net-tools/config.h
+++ pkgs/os-specific/linux/net-tools/config.h
@@ -50,7 +50,7 @@
 #define HAVE_HWSLIP 1
 #define HAVE_HWPPP 1
 #define HAVE_HWTUNNEL 1
-#define HAVE_HWSTRIP 1
+#define HAVE_HWSTRIP 0
 #define HAVE_HWTR 0
 #define HAVE_HWAX25 0
 #define HAVE_HWROSE 0
