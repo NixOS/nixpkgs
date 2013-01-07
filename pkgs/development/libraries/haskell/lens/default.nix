7025e237100d1d97821c95a90dba7c0142e678f8
Peter Simons <simons@cryp.to>
2013-01-07 12:18:34 +0100

    haskell-lens: update to version 3.7.2

--- pkgs/development/libraries/haskell/lens/default.nix
+++ pkgs/development/libraries/haskell/lens/default.nix
@@ -5,8 +5,8 @@
 
 cabal.mkDerivation (self: {
   pname = "lens";
-  version = "3.7.1.2";
-  sha256 = "1hapcnmyqyc3645gsy1ikwzm2srbbznps6yrfr02y2lcbnjpn3g6";
+  version = "3.7.2";
+  sha256 = "14kc9yhq7niv49gpdcl55priwnvisawa9jsp8hnplk48p11i2xs3";
   buildDepends = [
     comonad comonadsFd comonadTransformers filepath hashable mtl
     parallel semigroups split text transformers unorderedContainers
