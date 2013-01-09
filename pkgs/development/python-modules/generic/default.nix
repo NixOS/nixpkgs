0f52f5a945798957037850377fe5da94fe8485c7
Florian Friesdorf <flo@chaoflow.net>
2013-01-09 22:29:46 +0100

    Revert "don't propagate recursivePthLoader, it is needed to process it already"
    This reverts commit edd160452e0c17bb5b0f07693dc1a1a4dd2dd511.
    
    If we propagate it, derivations that use a python package will also
    get the recursivePthLoader needed to process the pth files of the
    package.

--- pkgs/development/python-modules/generic/default.nix
+++ pkgs/development/python-modules/generic/default.nix
@@ -53,6 +53,9 @@ python.stdenv.mkDerivation (attrs // {
 
   buildInputs = [ python wrapPython setuptools ] ++ buildInputs ++ pythonPath;
 
+  # recursivePthLoader is responsible for loading pth files
+  propagatedBuildInputs = propagatedBuildInputs ++ [ recursivePthLoader ];
+
   buildInputStrings = map toString buildInputs;
 
   pythonPath = [ setuptools ] ++ pythonPath;
