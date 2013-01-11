0cfd455a67accd8c1617532f8e2377889f8789bc
Jack Cummings <jack@mudshark.org>
2013-01-11 09:25:32 -0800

    Update x86info description. Add Homepage.

--- pkgs/os-specific/linux/x86info/default.nix
+++ pkgs/os-specific/linux/x86info/default.nix
@@ -17,8 +17,17 @@ stdenv.mkDerivation rec {
   '';
 
   meta = {
+    description = "An identification utility for the x86 series of processors.";
+    longDescription = 
+    ''
+      x86info will identify all Intel/AMD/Centaur/Cyrix/VIA CPUs. It leverages
+      the cpuid kernel module where possible.  it supports parsing model specific
+      registers (MSRs) via the msr kernel module.  it will approximate processor
+      frequency, and identify the cache sizes and layout. 
+    '';
     platforms = stdenv.lib.platforms.linux;
+    license = stdenv.lib.licenses.gpl2;
+    homepage = http://codemonkey.org.uk/projects/x86info/;
     maintainers = with stdenv.lib.maintainers; [jcumming];
-    description = "x86info, a CPU identification utility.";
   };
 }
