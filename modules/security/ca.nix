ac8080b83c8bcaaadd35f7430ccc0add0eade15a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-01-15 16:53:17 +0100

    Remove obsolete environment variables

--- modules/security/ca.nix
+++ modules/security/ca.nix
@@ -20,10 +20,6 @@ with pkgs.lib;
     environment.shellInit =
       ''
         export OPENSSL_X509_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
-
-        # !!! Remove the following as soon as OpenSSL 1.0.0e is the default.
-        export CURL_CA_BUNDLE=/etc/ssl/certs/ca-bundle.crt
-        export GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt
       '';
 
   };
