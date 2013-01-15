0b399d8e49e064842265c0b5f6b4e0346ad097eb
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-01-15 17:34:01 +0100

    Revert "Remove obsolete environment variables"
    This reverts commit ac8080b83c8bcaaadd35f7430ccc0add0eade15a.

--- modules/security/ca.nix
+++ modules/security/ca.nix
@@ -20,6 +20,10 @@ with pkgs.lib;
     environment.shellInit =
       ''
         export OPENSSL_X509_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
+
+        # !!! Remove the following as soon as OpenSSL 1.0.0e is the default.
+        export CURL_CA_BUNDLE=/etc/ssl/certs/ca-bundle.crt
+        export GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt
       '';
 
   };
