ee19b41203a75aabaafa8093f3b6ef487a9febc8
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-01-15 11:18:42 +0100

    Update the tarball mirroring scripts

--- maintainers/scripts/copy-tarballs.sh
+++ maintainers/scripts/copy-tarballs.sh
@@ -8,7 +8,19 @@ urls=$(nix-instantiate --eval-only --xml --strict '<nixpkgs/maintainers/scripts/
 
 for url in $urls; do
     if echo "$url" | grep -q -E "www.cs.uu.nl|nixos.org|.stratego-language.org|java.sun.com|ut2004|linuxq3a|RealPlayer|Adbe|belastingdienst|microsoft|armijn/.nix|sun.com|archive.eclipse.org"; then continue; fi
-    if ! echo "$url" | grep -q -E "\-.*[0-9].*"; then echo "skipping $url"; continue; fi
+
+    # Check the URL scheme.
+    if ! echo "$url" | grep -q -E "^[a-z]+://"; then echo "skipping $url (no URL scheme)"; continue; fi
+
+    # Check the basename.  It should include something resembling a version.
+    base="$(basename "$url")"
+    #if ! echo "$base" | grep -q -E "[-_].*[0-9].*"; then echo "skipping $url (no version)"; continue; fi
+    if ! echo "$base" | grep -q -E "[a-zA-Z]"; then echo "skipping $url (no letter in name)"; continue; fi
+    if ! echo "$base" | grep -q -E "[0-9]"; then echo "skipping $url (no digit in name)"; continue; fi
+    if ! echo "$base" | grep -q -E "[-_\.]"; then echo "skipping $url (no dot/underscore in name)"; continue; fi
+    if echo "$base" | grep -q -E "[&?=]"; then echo "skipping $url (bad character in name)"; continue; fi
+    if [ "${base:0:1}" = "." ]; then echo "skipping $url (starts with a dot)"; continue; fi
+
     $(dirname $0)/copy-tarball.sh "$url"
 done
 
