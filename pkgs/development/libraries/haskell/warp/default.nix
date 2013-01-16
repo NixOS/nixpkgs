9386ecd23b4480631b15e6f967b492422a94f8b3
Peter Simons <simons@cryp.to>
2013-01-16 12:42:13 +0100

    haskell-warp: update to version 1.3.7.1

--- pkgs/development/libraries/haskell/warp/default.nix
+++ pkgs/development/libraries/haskell/warp/default.nix
@@ -5,8 +5,8 @@
 
 cabal.mkDerivation (self: {
   pname = "warp";
-  version = "1.3.7";
-  sha256 = "06648wqiwlcsvd41qdqdbgn1zcq890iq39zsxi24vf4s8q7jnzyf";
+  version = "1.3.7.1";
+  sha256 = "0kabcxzjc686iayhxln2cmnxm3vk3h0685n4d4ybprv5h1nc6ghl";
   buildDepends = [
     blazeBuilder blazeBuilderConduit caseInsensitive conduit hashable
     httpTypes liftedBase network networkConduit simpleSendfile
