d54c8c4b8360aaedbb5b1220d09ef3a2bef9efdc
David Guibert <david.guibert@gmail.com>
2013-01-17 21:42:22 +0100

    update kernels: 3.0.59, 3.4.26 and 3.7.3

--- pkgs/os-specific/linux/kernel/linux-3.0.nix
+++ pkgs/os-specific/linux/kernel/linux-3.0.nix
@@ -230,7 +230,7 @@ in
 import ./generic.nix (
 
   rec {
-    version = "3.0.58";
+    version = "3.0.59";
 
     preConfigure = ''
       substituteInPlace scripts/depmod.sh --replace '-b "$INSTALL_MOD_PATH"' ""
@@ -238,7 +238,7 @@ import ./generic.nix (
 
     src = fetchurl {
       url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-      sha256 = "10ahi60ass970vjlhnzcf0y5fk3g7ck1a1lnwzy0kkywp6nb2c10";
+      sha256 = "0w0l0vsky921j6wvhydl8ggzwkwkhr0lk2lkd7lshhmv9c6bdg86";
     };
 
     config = configWithPlatform stdenv.platform;
