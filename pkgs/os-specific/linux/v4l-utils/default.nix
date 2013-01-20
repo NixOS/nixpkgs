ced57493ee6853fd05cec18c5f9abeaa530e5370
Peter Simons <simons@cryp.to>
2013-01-20 16:23:28 +0100

    v4l-utils: update to version 0.9.3
    This update fixes the build of the SANE back and front-ends.

--- pkgs/os-specific/linux/v4l-utils/default.nix
+++ pkgs/os-specific/linux/v4l-utils/default.nix
@@ -4,28 +4,22 @@
 assert withQt4 -> qt4 != null;
 
 stdenv.mkDerivation rec {
-  name = "v4l-utils-0.8.8";
+  name = "v4l-utils-0.9.3";
 
   src = fetchurl {
     url = "http://linuxtv.org/downloads/v4l-utils/${name}.tar.bz2";
-    sha256 = "0zx8f1npsl6g5vjah1gwydg1j5azl74kr83ifbjhshgmnvscd92z";
+    sha256 = "0gaag38x47wlvmp4j60wgf9ma1rxzfyg7i12zxxxi4m3cpcb0bah";
   };
 
-  buildInputs = [ libjpeg which ] ++ stdenv.lib.optional withQt4 qt4;
+  buildInputs = [ which ];
+  propagatedBuildInputs = [ libjpeg ] ++ stdenv.lib.optional withQt4 qt4;
 
-  configureFlags = [
-    "--with-udevdir=$(out)/lib/udev"
-  ];
-
-  installPhase = ''
-    make PREFIX=$out install
-  '';
+  preConfigure = ''configureFlags="--with-udevdir=$out/lib/udev"'';
 
   meta = {
     homepage = http://linuxtv.org/projects.php;
     description = "V4L utils and libv4l, that provides common image formats regardless of the v4l device";
-    # (The libs are of LGPLv2.1+, some other pieces are GPL)
-    license = "free";
+    license = "free"; # The libs are of LGPLv2.1+, some other pieces are GPL.
     maintainers = with stdenv.lib.maintainers; [viric];
     platforms = with stdenv.lib.platforms; linux;
   };
