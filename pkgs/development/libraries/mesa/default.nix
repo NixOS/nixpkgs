099ccef98219287d013aa4e392cfd8060db4de7f
Arseniy Alekseyev <rotsor@gmail.com>
2013-01-26 19:20:56 +0000

    Updated Mesa to 9.0.2
    Also updated libdrm to 2.4.40 (not 2.4.41 because they have broken tars for that)
    
    Removed the Mesa patch for sis video on mips64el-linux because sis is not
    supported in Mesa anyway (since version 8)

--- pkgs/development/libraries/mesa/default.nix
+++ pkgs/development/libraries/mesa/default.nix
@@ -1,37 +1,38 @@
 { stdenv, fetchurl, flex, bison, pkgconfig, libdrm, file, expat, makedepend
 , libXxf86vm, libXfixes, libXdamage, glproto, dri2proto, libX11, libxcb, libXext
 , libXt, udev, enableTextureFloats ? false
-, python, libxml2Python }:
+, python, libxml2Python, autoconf, automake, libtool, llvm, writeText }:
 
 if ! stdenv.lib.lists.elem stdenv.system stdenv.lib.platforms.mesaPlatforms then
   throw "unsupported platform for Mesa"
 else
 
-let version = "8.0.5"; in
+let version = "9.0.2"; in
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "mesa-${version}";
 
   src = fetchurl {
     url = "ftp://ftp.freedesktop.org/pub/mesa/${version}/MesaLib-${version}.tar.bz2";
-    md5 = "01305591073a76b65267f69f27d635a3";
+    sha256="0knnb8srr1xbin25js4y5kmpw7xys5gh1h8hwqamayvfdkr33gvm";
   };
-
-  patches =
-    stdenv.lib.optional (stdenv.system == "mips64el-linux") ./mips_wmb.patch;
 
   prePatch = "patchShebangs .";
 
+  preConfigure = "./autogen.sh";
+
   configureFlags =
-      " --enable-gles1 --enable-gles2 --enable-gallium-egl"
-    + " --with-gallium-drivers=i915,nouveau,r600,svga,swrast"
+    ""
+    + " --enable-gles1 --enable-gles2 --enable-gallium-egl"
+#    + " --enable-r600-llvm-compiler"
+    + " --with-gallium-drivers=i915,nouveau,r300,r600,svga,swrast"
     # Texture floats are patented, see docs/patents.txt
     + stdenv.lib.optionalString enableTextureFloats " --enable-texture-float";
 
   buildInputs = [ expat libdrm libXxf86vm libXfixes libXdamage glproto dri2proto
-    libxml2Python libX11 libXext libxcb libXt udev ];
+    libxml2Python libX11 libXext libxcb libXt udev llvm ];
 
-  buildNativeInputs = [ pkgconfig python makedepend file flex bison ];
+  buildNativeInputs = [ pkgconfig python makedepend file flex bison automake autoconf libtool ];
 
   enableParallelBuilding = true;
 
