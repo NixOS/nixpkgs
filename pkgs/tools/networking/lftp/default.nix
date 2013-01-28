08b8512ce41fbecfb5223a55927d55624b9e7bc0
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-01-28 14:12:29 +0100

    lftp: Update to 4.4.0 and fix build
    http://hydra.nixos.org/build/3896385

--- pkgs/tools/networking/lftp/default.nix
+++ pkgs/tools/networking/lftp/default.nix
@@ -1,12 +1,20 @@
-{stdenv, fetchurl, gnutls, libtasn1, pkgconfig, readline, zlib, xz}:
+{ stdenv, fetchurl, gnutls, pkgconfig, readline, zlib, xz }:
 
 stdenv.mkDerivation rec {
-  name = "lftp-4.3.1";
+  name = "lftp-4.4.0";
 
   src = fetchurl {
     url = "ftp://ftp.cs.tu-berlin.de/pub/net/ftp/lftp/${name}.tar.xz";
-    sha256 = "0v3591fknmimarzk5icm0qxdcfzfckwi2drh165vsiggmj590iyx";
+    sha256 = "0cg4gabya2sygbwh2b0cdr8v719q9gv929hdb5g1mxgj8npjd4y7";
   };
 
-  buildInputs = [gnutls libtasn1 pkgconfig readline zlib];
+  patches = [ ./no-gets.patch ];
+
+  buildInputs = [ gnutls pkgconfig readline zlib ];
+
+  meta = {
+    homepage = http://lftp.yar.ru/;
+    description = "A file transfer program supporting a number of network protocols";
+    license = "GPL";
+  };
 }
