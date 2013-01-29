4b3e2531c965947e823e085f083ef4d81929ca31
Vladimír Čunát <vcunat@gmail.com>
2013-01-29 05:10:07 +0100

    update at-spi

--- pkgs/desktops/gnome-3/core/at-spi2-atk/default.nix
+++ pkgs/desktops/gnome-3/core/at-spi2-atk/default.nix
@@ -1,17 +1,17 @@
 { stdenv, fetchurl, python, pkgconfig, popt, atk, libX11, libICE, xlibs, libXi
-, intltool, dbus_glib, gnome3, libSM }:
+, intltool, dbus_glib, at_spi2_core, libSM }:
 
 stdenv.mkDerivation rec {
-  versionMajor = "2.5";
-  versionMinor = "4";
+  versionMajor = "2.6";
+  versionMinor = "2";
   moduleName   = "at-spi2-atk";
   name = "${moduleName}-${versionMajor}.${versionMinor}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${moduleName}/${versionMajor}/${name}.tar.xz";
-    sha256 = "1bl5jl644slf5mmnr4z7xj433prvjjpgmv9rdmxdny23j08qa8vs";
+    sha256 = "1ymwm50qdl4iw1z54kn9yf5p16wy412mvbzr8m8p70mbx0r88v29";
   };
 
   buildInputs = [ python pkgconfig popt atk libX11 libICE xlibs.libXtst libXi
-                  intltool dbus_glib gnome3.at_spi2_core libSM ];
+                  intltool dbus_glib at_spi2_core libSM ];
 }
--- pkgs/desktops/gnome-3/core/at-spi2-core/default.nix
+++ pkgs/desktops/gnome-3/core/at-spi2-core/default.nix
@@ -3,14 +3,14 @@
 
 stdenv.mkDerivation rec {
 
-  versionMajor = "2.5";
-  versionMinor = "4";
+  versionMajor = "2.6";
+  versionMinor = "3";
   moduleName   = "at-spi2-core";
   name = "${moduleName}-${versionMajor}.${versionMinor}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${moduleName}/${versionMajor}/${name}.tar.xz";
-    sha256 = "167zm1a36rd09wg161fsq8swnzdk3wv23kq49nd0l7gr89flf9ig";
+    sha256 = "1snh2lmry33756gw5pfjxir3g4lrrx9bhadkaz8cyiz88sp8fi7w";
   };
 
   buildInputs = [ python pkgconfig popt libX11 xextproto libSM libICE libXtst libXi intltool dbus_glib ];
