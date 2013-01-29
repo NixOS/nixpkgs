713b420be8926ae13a90ddd4fdafe33f0a5c2923
Vladimír Čunát <vcunat@gmail.com>
2013-01-29 05:08:33 +0100

    removed some libraries that are up-to-date on the top level

--- pkgs/desktops/gnome-3/default.nix
+++ pkgs/desktops/gnome-3/default.nix
@@ -1,6 +1,9 @@
-{ callPackage, lib, self, stdenv, gettext, overrides ? {} }:
+{ callPackage, lib, self, stdenv, gettext, overrides ? {}, pkgs }:
 
-{
+rec {
+  inherit (pkgs) fetchurl_gnome glib gtk3 atk pango;
+  gtk = gtk3;
+  orbit = pkgs.gnome2.ORBit2;
 
   inherit (lib) lowPrio hiPrio appendToName makeOverridable;
 
@@ -12,23 +15,11 @@
 
   at_spi2_core = callPackage ./core/at-spi2-core { };
 
-  atk = callPackage ./core/atk { };
-
   gconf = callPackage ./core/gconf { };
-
-  gdk_pixbuf = callPackage ./core/gdk-pixbuf { };
-
-  glib = callPackage ./core/glib { };
-
-  gobject_introspection = callPackage ./core/gobject-introspection { };
-
-  gtk = callPackage ./core/gtk { };
 
   gnome_terminal = callPackage ./core/gnome-terminal { };
 
   gsettings_desktop_schemas = lib.lowPrio (callPackage ./core/gsettings-desktop-schemas { });
-
-  pango = callPackage ./core/pango { };
 
   vte = callPackage ./core/vte { };
 
