78487562836179a0d61eee26d44a921f0792c61b
Peter Simons <simons@cryp.to>
2013-01-30 17:30:25 +0100

    Parallel building is enabled again in GHC 7.6.2, because we hope the issues we've had with earlier versions have been remedied. If anyone experiences build errors with this package, please file issue at Github!

--- pkgs/development/compilers/ghc/7.6.2.nix
+++ pkgs/development/compilers/ghc/7.6.2.nix
@@ -12,19 +12,6 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ ghc perl gmp ncurses ];
 
-  # The comments below applied to GHC 7.6.1, left for if somebody experiences
-  # similar problems. If so, I argue we should selectively disable parallel
-  # building JUST for that platform. ~aristidb
-  #
-  ## My attempts to compile GHC with parallel build support enabled, failed
-  ## 4 consecutive times with the following error:
-  ##
-  ##    building rts/dist/build/AutoApply.debug_o
-  ##    building rts/dist/build/AutoApply.thr_o
-  ##      rts_dist_HC rts/dist/build/AutoApply.debug_o
-  ##    /nix/store/1iigiim5855m8j7pmwf5xrnpf705s4dh-binutils-2.21.1a/bin/ld: cannot find libraries/integer-gmp/dist-install/build/cbits/gmp-wrappers_o_split/gmp-wrappers__1.o
-  ##    collect2: ld returned 1 exit status
-  ##    make[1]: *** [libraries/integer-gmp/dist-install/build/cbits/gmp-wrappers.p_o] Error 1
   enableParallelBuilding = true;
 
   buildMK = ''
