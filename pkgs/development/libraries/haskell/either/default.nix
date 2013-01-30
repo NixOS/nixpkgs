eb2adf9acd2c7e2b5178156e4756d527b0a81451
Peter Simons <simons@cryp.to>
2013-01-30 11:29:40 +0100

    haskell-either: update to version 3.1

--- pkgs/development/libraries/haskell/either/default.nix
+++ pkgs/development/libraries/haskell/either/default.nix
@@ -1,13 +1,16 @@
-{ cabal, semigroupoids, semigroups, transformers }:
+{ cabal, MonadRandom, mtl, semigroupoids, semigroups, transformers
+}:
 
 cabal.mkDerivation (self: {
   pname = "either";
-  version = "3.0.3";
-  sha256 = "02kpb8xd19n00ll61haqr6k3hy8qmbdf73gr4zs59q9xh0739qxc";
-  buildDepends = [ semigroupoids semigroups transformers ];
+  version = "3.1";
+  sha256 = "1paglfhd2xjr32sh5npl3kvamv4nps9fpy0wk9ya0l26w4c3bdsm";
+  buildDepends = [
+    MonadRandom mtl semigroupoids semigroups transformers
+  ];
   meta = {
     homepage = "http://github.com/ekmett/either/";
-    description = "Haskell 98 either monad transformer";
+    description = "An either monad transformer";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
   };
