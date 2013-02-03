2889242ff5ccd03e4464bde1813774fa5b96e000
Shea Levy <shea@shealevy.com>
2013-02-02 19:42:56 -0500

    Maintain iso_efi

--- release.nix
+++ release.nix
@@ -168,11 +168,12 @@ let
       type = "new-kernel";
     };
 
-    # A variant with experimental efi booting support. Currently requires
-    # an RC kernel. Eventually this should probably be merged into cd-minimal
+    # A variant with efi booting support. Once cd-minimal has a newer kernel,
+    # this should be enabled by default.
     iso_efi = makeIso {
       module = ./modules/installer/cd-dvd/installation-cd-efi.nix;
       type = "efi";
+      maintainers = [ "shlevy" ];
     };
 
     # Provide a tarball that can be unpacked into an SD card, and easily
