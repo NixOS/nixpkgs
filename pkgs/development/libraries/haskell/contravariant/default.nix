4548458a3e9bf743f971c2a31790f16e097f702b
Peter Simons <simons@cryp.to>
2013-02-06 12:27:09 +0100

    haskell-contravariant: update to version 0.4

--- pkgs/development/libraries/haskell/contravariant/default.nix
+++ pkgs/development/libraries/haskell/contravariant/default.nix
@@ -1,10 +1,10 @@
-{ cabal, transformers, transformersCompat }:
+{ cabal, tagged, transformers, transformersCompat }:
 
 cabal.mkDerivation (self: {
   pname = "contravariant";
-  version = "0.3";
-  sha256 = "025rmangj0g8vls1ymh1dz4xq6ljnn8fsbcfrds3030s325v4zl9";
-  buildDepends = [ transformers transformersCompat ];
+  version = "0.4";
+  sha256 = "1nqslc9vcx6v6c33fn7mvb1xhr33yl3ja92p788zx8z1drx9cqc3";
+  buildDepends = [ tagged transformers transformersCompat ];
   meta = {
     homepage = "http://github.com/ekmett/contravariant/";
     description = "Haskell 98 contravariant functors";
