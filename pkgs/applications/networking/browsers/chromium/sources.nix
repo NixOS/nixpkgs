9b726e94a82b735fd9e64c23410e245af0dbbca7
aszlig <aszlig@redmoonstudios.org>
2013-02-07 06:46:17 +0100

    chromium: Update stable channel to v24.0.1312.69.
    Let's begin with the most trivial one: The stable version.
    This version just contains a few bug fixes and builds fine so far.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/applications/networking/browsers/chromium/sources.nix
+++ pkgs/applications/networking/browsers/chromium/sources.nix
@@ -11,8 +11,8 @@
     sha256 = "1pn7qv1s6lcx8k26h89x9zdy43rzdq12f92s2l6cfdhr9ls9wv0s";
   };
   stable = {
-    version = "24.0.1312.52";
-    url = "http://commondatastorage.googleapis.com/chromium-browser-official/chromium-24.0.1312.52.tar.bz2";
-    sha256 = "04fp04591dszx07wwdsgxf0wb2sxm863z1qxn5dii6f9yjqgh3gk";
+    version = "24.0.1312.69";
+    url = "http://commondatastorage.googleapis.com/chromium-browser-official/chromium-24.0.1312.69.tar.bz2";
+    sha256 = "1nvnhkky72nywk601vx5bbjp1m2f5dygza9h34y20inz3jgg8nbr";
   };
 }
