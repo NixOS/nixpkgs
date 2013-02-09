37db55f3ec61481fa6a465b539552f8f3435cde2
Peter Simons <simons@cryp.to>
2013-02-09 21:03:26 +0100

    boost: remove obsolete version 1.51.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3441,13 +3441,11 @@ let
   boost144 = callPackage ../development/libraries/boost/1.44.nix { };
   boost147 = callPackage ../development/libraries/boost/1.47.nix { };
   boost149 = callPackage ../development/libraries/boost/1.49.nix { };
-  boost151 = callPackage ../development/libraries/boost/1.51.nix { };
   boost152 = callPackage ../development/libraries/boost/1.52.nix { };
   boost153 = callPackage ../development/libraries/boost/1.53.nix { };
   boost = boost152;
 
   boostHeaders149 = callPackage ../development/libraries/boost/1.49-headers.nix { };
-  boostHeaders151 = callPackage ../development/libraries/boost/1.51-headers.nix { };
   boostHeaders152 = callPackage ../development/libraries/boost/1.52-headers.nix { };
   boostHeaders153 = callPackage ../development/libraries/boost/1.53-headers.nix { };
   boostHeaders = boostHeaders152;
