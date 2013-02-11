8fa1ce00f25eadcfae992f368dacbace44e28bed
Peter Simons <simons@cryp.to>
2013-02-11 14:37:15 +0100

    petidomo: update to version 4.3

--- pkgs/servers/mail/petidomo/default.nix
+++ pkgs/servers/mail/petidomo/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, flex, bison, sendmailPath ? "/var/setuid-wrappers/sendmail" }:
 
 stdenv.mkDerivation rec {
-  name = "petidomo-4.2";
+  name = "petidomo-4.3";
 
   src = fetchurl {
     url = "mirror://sourceforge/petidomo/${name}.tar.gz";
-    sha256 = "0rckzsiqg819ids5784gmlf5p1lbjvavz0f5mwn10h34kclhi8bz";
+    sha256 = "0x4dbxc4fcfg1rw5ywpcypvylnzn3y4rh0m6fz4h4cdnzb8p1lvm";
   };
 
   buildInputs = [ flex bison ];
