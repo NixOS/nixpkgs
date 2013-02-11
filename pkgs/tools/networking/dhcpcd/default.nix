7c2e9cf2bf781e588a02794145dab49cdf9380d4
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-02-11 17:45:42 +0100

    dhcpcd: Update to 5.6.7

--- pkgs/tools/networking/dhcpcd/default.nix
+++ pkgs/tools/networking/dhcpcd/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "dhcpcd-5.5.6";
+  name = "dhcpcd-5.6.7";
 
   src = fetchurl {
     url = "http://roy.marples.name/downloads/dhcpcd/${name}.tar.bz2";
-    sha256 = "1rz8n6crw6yh9hzqwdqjpl37v60i1szr7dbhf6gvm374gpf10zv5";
+    sha256 = "144cjcjnr85jiwbw5iv3hvn97sc0z25ya3r31cn0wv11jrsw6b0h";
   };
 
   configureFlags = "--sysconfdir=/etc";
