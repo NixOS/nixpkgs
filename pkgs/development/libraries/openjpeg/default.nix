fe0976b9a94ac63c698fbaddda924010df762720
Vladimír Čunát <vcunat@gmail.com>
2013-02-12 21:41:09 +0100

    openjpeg: use dependencies

--- pkgs/development/libraries/openjpeg/default.nix
+++ pkgs/development/libraries/openjpeg/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, pkgconfig, libpng, libtiff, lcms2 }:
 
 stdenv.mkDerivation rec {
   name = "openjpeg-1.5.1";
@@ -7,6 +7,8 @@ stdenv.mkDerivation rec {
     url = "http://openjpeg.googlecode.com/files/${name}.tar.gz";
     sha256 = "13dbyf3jwr4h2dn1k11zph3jgx17z7d66xmi640mbsf8l6bk1yvc";
   };
+
+  buildInputs = [ pkgconfig libpng libtiff lcms2 ];
 
   meta = {
     homepage = http://www.openjpeg.org/;
