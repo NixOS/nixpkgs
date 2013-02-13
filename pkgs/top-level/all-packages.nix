4c4e3f0ada5f876dd79703a7beafc01b24e7839d
Vladimír Čunát <vcunat@gmail.com>
2013-02-13 17:45:27 +0100

    meshlab: fix forgotten qt47

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7482,9 +7482,7 @@ let
 
   merkaartor = callPackage ../applications/misc/merkaartor { };
 
-  meshlab = callPackage ../applications/graphics/meshlab {
-    qt = qt47;
-  };
+  meshlab = callPackage ../applications/graphics/meshlab { };
 
   mhwaveedit = callPackage ../applications/audio/mhwaveedit {};
 
