52531cef2132c7aa1173ea9d770a2f81381d8562
Bjørn Forsman <bjorn.forsman@gmail.com>
2013-02-16 23:17:18 +0100

    file: bump from 5.11 to 5.12

--- pkgs/tools/misc/file/default.nix
+++ pkgs/tools/misc/file/default.nix
@@ -1,17 +1,17 @@
-{stdenv, fetchurl, zlib}:
+{ stdenv, fetchurl, zlib }:
 
 stdenv.mkDerivation rec {
-  name = "file-5.11";
+  name = "file-5.12";
 
   buildInputs = [ zlib ];
 
   src = fetchurl {
     url = "ftp://ftp.astron.com/pub/file/${name}.tar.gz";
-    sha256 = "c70ae29a28c0585f541d5916fc3248c3e91baa481f63d7ccec53d1534cbcc9b7";
+    sha256 = "08ix4xrvan0k80n0l5lqfmc4azjv5lyhvhwdxny4r09j5smhv78r";
   };
 
   meta = {
-    description = "A program that shows the type of files";
     homepage = "http://darwinsys.com/file";
+    description = "A program that shows the type of files";
   };
 }
