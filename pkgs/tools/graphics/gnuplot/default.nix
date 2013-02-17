f2bff3d088b3c869c1a642e8f226e83c9f3e0972
Peter Simons <simons@cryp.to>
2013-02-18 00:00:48 +0100

    gnuplot: configure GDFONTPATH using 'fontconfig'
    This patch adds a wrapper script around gnuplot that uses fontconfig's
    fc-list(1) utility to determine the set of available fonts and makes that list
    available to Gnuplot in the $GDFONTPATH variable.

--- pkgs/tools/graphics/gnuplot/default.nix
+++ pkgs/tools/graphics/gnuplot/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, zlib, gd, texinfo
+{ stdenv, fetchurl, zlib, gd, texinfo, makeWrapper
 , texLive ? null
 , lua ? null
 , emacs ? null
@@ -11,7 +11,10 @@
 , cairo ? null
 , pkgconfig ? null
 , readline
+, fontconfig ? null, gnused ? null, coreutils ? null
 }:
+
+assert libX11 != null -> (fontconfig != null && gnused != null && coreutils != null);
 
 stdenv.mkDerivation rec {
   name = "gnuplot-4.4.4";
@@ -21,12 +24,20 @@ stdenv.mkDerivation rec {
     sha256 = "1zfv3npsxfn743wl65ibh11djxrc8fxzi2mgg75ppy6m12fmja6j";
   };
 
-  configureFlags = if libX11 != null then ["--with-x"] else ["--without-x"];
-
   buildInputs =
     [ zlib gd texinfo readline emacs lua texLive libX11 libXt libXpm libXaw
-      wxGTK pango cairo pkgconfig
+      wxGTK pango cairo pkgconfig makeWrapper
     ];
+
+  configureFlags = if libX11 != null then ["--with-x"] else ["--without-x"];
+
+  postInstall = stdenv.lib.optionalString (libX11 != null) ''
+    wrapProgram $out/bin/gnuplot \
+       --prefix PATH : '${gnused}/bin' \
+       --prefix PATH : '${coreutils}/bin' \
+       --prefix PATH : '${fontconfig}/bin' \
+       --run '. ${./set-gdfontpath-from-fontconfig.sh}'
+  '';
 
   meta = {
     homepage = "http://www.gnuplot.info";
