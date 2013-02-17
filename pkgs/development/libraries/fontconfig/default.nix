5b6a2bc2fa397c50e407499c21a38db676fb9ef4
Corey O'Connor <coreyoconnor@gmail.com>
2013-02-17 02:11:37 -0800

    cherry pick in infinality patch from x-updates plus others

--- pkgs/development/libraries/fontconfig/default.nix
+++ pkgs/development/libraries/fontconfig/default.nix
@@ -1,16 +1,24 @@
 { stdenv, fetchurl, pkgconfig, freetype, expat }:
 
 stdenv.mkDerivation rec {
-  name = "fontconfig-2.10.1";
+  name = "fontconfig-2.10.2";
 
   src = fetchurl {
-    url = "http://fontconfig.org/release/${name}.tar.gz";
-    sha256 = "08h252crb3aqciwdk81jypmz2i7618dzqn3zlr87w1f017wjp4f3";
+    url = "http://fontconfig.org/release/${name}.tar.bz2";
+    sha256 = "0llraqw86jmw4vzv7inskp3xxm2gc64my08iwq5mzncgfdbfza4f";
   };
 
-  buildInputs = [ pkgconfig freetype ];
+  infinality_patch = with freetype.infinality; if useInfinality
+    then let subvers = "1";
+      in fetchurl {
+        url = "${base_URL}/fontconfig-infinality-1-${vers}_${subvers}.tar.bz2";
+        sha256 = "1fm5xx0mx2243jrq5rxk4v0ajw2nawpj23399h710bx6hd1rviq7";
+      }
+    else null;
 
-  propagatedBuildInputs = [ expat ]; # !!! shouldn't be necessary, but otherwise pango breaks
+  buildInputs = [ pkgconfig freetype expat ];
+
+  #propagatedBuildInputs = [ expat ]; # !!! shouldn't be necessary, but otherwise pango breaks
 
   configureFlags = "--with-confdir=/etc/fonts --with-cache-dir=/var/cache/fontconfig --disable-docs --with-default-fonts=";
 
@@ -27,6 +35,10 @@ stdenv.mkDerivation rec {
 
   # Don't try to write to /etc/fonts or /var/cache/fontconfig at install time.
   installFlags = "CONFDIR=$(out)/etc/fonts RUN_FC_CACHE_TEST=false fc_cachedir=$(TMPDIR)/dummy";
+
+  postInstall = if !freetype.infinality.useInfinality then "" else ''
+    cd "$out/etc/fonts" && tar xvf ${infinality_patch}
+  '';
 
   meta = {
     description = "A library for font customization and configuration";
