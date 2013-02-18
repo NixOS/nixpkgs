bd8428efa13764f3bf6ac7de538afa5f50023aa2
Carles Pagès <page@cubata.homelinux.net>
2013-02-18 19:57:27 +0100

    wayland: update to 1.0.5.
    Also updated weston to 1.0.5, and got rid of the patch that's no longer needed.
    They fixed it upstream even if it was actually caused by our pkg-config patch.

--- pkgs/applications/window-managers/weston/default.nix
+++ pkgs/applications/window-managers/weston/default.nix
@@ -2,19 +2,15 @@
 , cairo, libxcb, libXcursor, x11, udev, libdrm2_4_40, mtdev
 , libjpeg, pam, autoconf, automake, libtool }:
 
-let version = "1.0.4"; in
+let version = "1.0.5"; in
 
 stdenv.mkDerivation rec {
   name = "weston-${version}";
 
   src = fetchurl {
     url = "http://wayland.freedesktop.org/releases/${name}.tar.xz";
-    sha256 = "1841sd6i8nq9gs1xkx8lwcg4lzy6yfcs95nnsfaf0y1ppd6c9sj2";
+    sha256 = "0g2k82pnlxl8b70ykazj7kn8xffjfsmgcgx427qdrm4083z2hgm0";
   };
-
-  patches = [
-    ./makefile.patch
-  ];
 
   buildInputs = [ pkgconfig wayland mesa90x libxkbcommon pixman
     cairo libxcb libXcursor x11 udev libdrm2_4_40 mtdev
--- pkgs/development/libraries/wayland/default.nix
+++ pkgs/development/libraries/wayland/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, pkgconfig, libffi, expat, doxygen }:
 
-let version = "1.0.4"; in
+let version = "1.0.5"; in
 
 stdenv.mkDerivation rec {
   name = "wayland-${version}";
 
   src = fetchurl {
     url = "http://wayland.freedesktop.org/releases/${name}.tar.xz";
-    sha256 = "074n94d79bqvihf9wmszvar8r9v4g4n1h9m8vs8ki6xjhsk07s4d";
+    sha256 = "130n7v5i7rfsrli2n8vdzfychlgd8v7by7sfgp8vfqdlss5km34w";
   };
 
   buildInputs = [ pkgconfig libffi expat doxygen ];
