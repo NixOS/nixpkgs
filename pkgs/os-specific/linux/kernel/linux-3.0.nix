f4a51bbdd6755bc7d1d79d58711c20a3612023ca
Shea Levy <shea@shealevy.com>
2013-02-18 11:34:19 -0500

    Linux 3.0.65

--- pkgs/os-specific/linux/kernel/linux-3.0.nix
+++ pkgs/os-specific/linux/kernel/linux-3.0.nix
@@ -230,7 +230,7 @@ in
 import ./generic.nix (
 
   rec {
-    version = "3.0.64";
+    version = "3.0.65";
 
     preConfigure = ''
       substituteInPlace scripts/depmod.sh --replace '-b "$INSTALL_MOD_PATH"' ""
@@ -238,7 +238,7 @@ import ./generic.nix (
 
     src = fetchurl {
       url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-      sha256 = "0vya753ayxzjk7ymnw5i2rwcpxp8lss7xf1gl7fzakn6kz780nky";
+      sha256 = "1vmk0bmg0djwbh9scrhpyljan01ygkwn4q0j1ydn8snag7sn4x8y";
     };
 
     config = configWithPlatform stdenv.platform;
