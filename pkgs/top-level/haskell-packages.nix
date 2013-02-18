9ff93714d769ab8c3427635a283beedfe3c470a9
Peter Simons <simons@cryp.to>
2013-02-18 10:48:59 +0100

    haskell-monad-par: update to version 0.3.4

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -1218,8 +1218,8 @@ let result = let callPackage = x : y : modifyPrio (newScope result.final x y);
   monadLogger = callPackage ../development/libraries/haskell/monad-logger {};
 
   monadPar_0_1_0_3 = callPackage ../development/libraries/haskell/monad-par/0.1.0.3.nix {};
-  monadPar_0_3 = callPackage ../development/libraries/haskell/monad-par/0.3.nix {};
-  monadPar = self.monadPar_0_3;
+  monadPar_0_3_4 = callPackage ../development/libraries/haskell/monad-par/0.3.4.nix {};
+  monadPar = self.monadPar_0_3_4;
 
   monadParExtras = callPackage ../development/libraries/haskell/monad-par-extras {};
 
