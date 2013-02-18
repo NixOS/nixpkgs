c1729b434e7bf9bea5e7be247d6ddd6799fbec36
Peter Simons <simons@cryp.to>
2013-02-18 11:11:16 +0100

    haskell-wai-test: update to version 1.3.0.2

--- pkgs/development/libraries/haskell/wai-test/default.nix
+++ pkgs/development/libraries/haskell/wai-test/default.nix
@@ -4,14 +4,14 @@
 
 cabal.mkDerivation (self: {
   pname = "wai-test";
-  version = "1.3.0.1";
-  sha256 = "0yy0bvkrny4kj77wvn0cflwha6yijfxvnj530ps7xnzv1qm8qn1l";
+  version = "1.3.0.2";
+  sha256 = "0awr1wwhky0mbllfdan42shfckmnnf66dji5zx7rhwjdfqvbbhzn";
   buildDepends = [
     blazeBuilder blazeBuilderConduit caseInsensitive conduit cookie
     httpTypes HUnit text transformers wai
   ];
   meta = {
-    homepage = "http://www.yesodweb.com/book/wai";
+    homepage = "http://www.yesodweb.com/book/web-application-interface";
     description = "Unit test framework (built on HUnit) for WAI applications";
     license = self.stdenv.lib.licenses.mit;
     platforms = self.ghc.meta.platforms;
