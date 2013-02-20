4287ca08adf3c344ac3c495a06bfb64f95c61a48
Peter Simons <simons@cryp.to>
2013-02-20 11:58:04 +0100

    haskell-simple-sendfile: update to version 0.2.11

--- pkgs/development/libraries/haskell/simple-sendfile/default.nix
+++ pkgs/development/libraries/haskell/simple-sendfile/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "simple-sendfile";
-  version = "0.2.10";
-  sha256 = "07js189yyya2vs3wsc49b7c2zbh63f48kywmz88g7s394bp3n9am";
+  version = "0.2.11";
+  sha256 = "1q9m9lxv9jfkn6a1lf07jcl4li3n5996df1qrfkfjq2n0bvn4qfj";
   buildDepends = [ network ];
   meta = {
     description = "Cross platform library for the sendfile system call";
