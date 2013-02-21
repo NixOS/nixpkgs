d81088839f366b3191f1ff2f26713980e78c59b7
Peter Simons <simons@cryp.to>
2013-02-21 16:13:05 +0100

    haskell-authenticate: update to version 1.3.2.6

--- pkgs/development/libraries/haskell/authenticate/default.nix
+++ pkgs/development/libraries/haskell/authenticate/default.nix
@@ -1,17 +1,16 @@
-{ cabal, aeson, attoparsec, blazeBuilder, blazeBuilderConduit
-, caseInsensitive, conduit, httpConduit, httpTypes, monadControl
-, network, resourcet, tagsoup, text, transformers
+{ cabal, aeson, attoparsec, blazeBuilder, caseInsensitive, conduit
+, httpConduit, httpTypes, network, tagsoup, text, transformers
 , unorderedContainers, xmlConduit
 }:
 
 cabal.mkDerivation (self: {
   pname = "authenticate";
-  version = "1.3.2.4";
-  sha256 = "1xakp54g5dyppzpm1q3bbx6g4pvs7p2903pyq6bya1n82zsk7yg7";
+  version = "1.3.2.6";
+  sha256 = "12sgi6q6kajjhh8mns9nklxj0kwn32xs5kzi7wmw50shx0smnjrz";
   buildDepends = [
-    aeson attoparsec blazeBuilder blazeBuilderConduit caseInsensitive
-    conduit httpConduit httpTypes monadControl network resourcet
-    tagsoup text transformers unorderedContainers xmlConduit
+    aeson attoparsec blazeBuilder caseInsensitive conduit httpConduit
+    httpTypes network tagsoup text transformers unorderedContainers
+    xmlConduit
   ];
   meta = {
     homepage = "http://github.com/yesodweb/authenticate";
