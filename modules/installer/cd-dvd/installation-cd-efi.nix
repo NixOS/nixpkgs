fb7b7a28872584a74550b1245e1f5930b70cf8e8
Shea Levy <shea@shealevy.com>
2013-02-21 13:30:42 -0500

    Update EFI installation CD to meet the requirements of the new gummiboot

--- modules/installer/cd-dvd/installation-cd-efi.nix
+++ modules/installer/cd-dvd/installation-cd-efi.nix
@@ -3,17 +3,16 @@
 {
   require = [ ./installation-cd-minimal.nix ];
 
-  boot.kernelPackages = pkgs.linuxPackages_3_7;
+  boot.kernelPackages = pkgs.linuxPackages_3_8;
   boot.vesa = false;
 
   # What follows should probably move into base once the base kernel has the
   # efi boot stub
 
   # Get a console as soon as the initrd loads fbcon on EFI boot
-  boot.initrd.kernelModules = [ "fbcon" ];
-
   # Enable reading EFI variables via sysfs
-  boot.kernelModules = [ "efivars" ];
+  # !!! Needing efivars will only be necessary until http://article.gmane.org/gmane.linux.kernel.efi/773 is merged
+  boot.initrd.kernelModules = [ "fbcon" "efivars" ];
 
   # efi-related tools
   environment.systemPackages = [ pkgs.efibootmgr ];
