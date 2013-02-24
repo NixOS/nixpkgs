86b96bd0e9e0b143e3ddeb8f6f764103dfae4d09
Peter Simons <simons@cryp.to>
2013-02-24 22:42:15 +0100

    haskell-cautious-file: disable 'doCheck' because of test suite failure

--- pkgs/development/libraries/haskell/cautious-file/default.nix
+++ pkgs/development/libraries/haskell/cautious-file/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "1.0.2";
   sha256 = "1sw5ngwrarq1lsd4c6v2wdmgbhkkq6kpybb62r8ccm11ddgn3yiq";
   buildDepends = [ filepath ];
+  doCheck = false;
   meta = {
     description = "Ways to write a file cautiously, to reduce the chances of problems such as data loss due to crashes or power failures";
     license = self.stdenv.lib.licenses.bsd3;
