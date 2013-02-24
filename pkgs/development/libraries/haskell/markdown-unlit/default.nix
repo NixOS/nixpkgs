9a96a358f09b4be96e419ef7d43f732e369ce4c1
Peter Simons <simons@cryp.to>
2013-02-24 22:16:38 +0100

    haskell-markdown-unlit: add version 0.2.0.1

---
+++ pkgs/development/libraries/haskell/markdown-unlit/default.nix
@@ -0,0 +1,15 @@
+{ cabal, hspec, QuickCheck, silently, stringbuilder }:
+
+cabal.mkDerivation (self: {
+  pname = "markdown-unlit";
+  version = "0.2.0.1";
+  sha256 = "1bc3vcifv2xcddh8liq380c6sxarrs5pf21pfs9i4dx9rfl3hvhq";
+  isLibrary = true;
+  isExecutable = true;
+  testDepends = [ hspec QuickCheck silently stringbuilder ];
+  meta = {
+    description = "Literate Haskell support for Markdown";
+    license = self.stdenv.lib.licenses.mit;
+    platforms = self.ghc.meta.platforms;
+  };
+})
