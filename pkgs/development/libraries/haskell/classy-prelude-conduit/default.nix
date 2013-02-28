007ca863dc8ee8701a4511f7b71a8f62da3f06b3
Peter Simons <simons@cryp.to>
2013-02-28 11:31:49 +0100

    haskell-classy-prelude-conduit: update to version 0.5.2

--- pkgs/development/libraries/haskell/classy-prelude-conduit/default.nix
+++ pkgs/development/libraries/haskell/classy-prelude-conduit/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "classy-prelude-conduit";
-  version = "0.5.1";
-  sha256 = "1vwcxwrbnczchq2b773kjjr3ysc47widak8qj0kwi26nf3jics4k";
+  version = "0.5.2";
+  sha256 = "11krzhy78z0srjy5g6h8ssv5n3ml8ryx92x0zdjigqxw4zq9ic72";
   buildDepends = [
     classyPrelude conduit monadControl resourcet transformers void
     xmlConduit
