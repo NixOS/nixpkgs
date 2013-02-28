93876deae2101ac0887b090ae7137703b125cf7f
Peter Simons <simons@cryp.to>
2013-02-28 11:31:48 +0100

    haskell-DAV: update to version 0.3.1

--- pkgs/development/libraries/haskell/DAV/default.nix
+++ pkgs/development/libraries/haskell/DAV/default.nix
@@ -5,15 +5,14 @@
 
 cabal.mkDerivation (self: {
   pname = "DAV";
-  version = "0.3";
-  sha256 = "16qbq59g79a0a1n6vblndj1fknj9fvd0anhrsz9czwl3k3lk5cx8";
+  version = "0.3.1";
+  sha256 = "0ql6sf61gq55iyn189papnid91n4ab5s2i24zvkqrgixjz7998rd";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
     caseInsensitive cmdargs httpConduit httpTypes lens liftedBase mtl
     network resourcet transformers xmlConduit xmlHamlet
   ];
-  jailbreak = true;
   meta = {
     homepage = "http://floss.scru.org/hDAV";
     description = "RFC 4918 WebDAV support";
