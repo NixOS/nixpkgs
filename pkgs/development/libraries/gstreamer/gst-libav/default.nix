4990ae1836ea03376d523cda7aca9838ff4772c6
Jack Cummings <jack@mudshark.org>
2013-03-01 01:16:49 -0800

    gst-libav no longer supports using the system ffmpeg.

--- pkgs/development/libraries/gstreamer/gst-libav/default.nix
+++ pkgs/development/libraries/gstreamer/gst-libav/default.nix
@@ -1,5 +1,4 @@
-{ fetchurl, stdenv, pkgconfig, gst_plugins_base, bzip2, yasm
-, useInternalFfmpeg ? false, ffmpeg ? null }:
+{ fetchurl, stdenv, pkgconfig, gst_plugins_base, bzip2, yasm }:
 
 stdenv.mkDerivation rec {
   name = "gst-libav-1.0.5";
@@ -12,17 +11,13 @@ stdenv.mkDerivation rec {
     sha256 = "1m9jzga9ahml3dnfm7syz6mglmysqpbkkyr48kka9cwna1kbxy5f";
   };
 
-  # Upstream strongly recommends against using --with-system-ffmpeg,
-  # but we do it anyway because we're so hardcore (and we don't want
-  # multiple copies of ffmpeg).
-  configureFlags = stdenv.lib.optionalString (!useInternalFfmpeg) "--with-system-ffmpeg";
-
   buildInputs =
-    [ pkgconfig bzip2 gst_plugins_base ]
-    ++ (if useInternalFfmpeg then [ yasm ] else [ ffmpeg ]);
+    [ pkgconfig bzip2 gst_plugins_base yasm ];
+
+  enableParallelBuilding = true; 
 
   meta = {
-    homepage = "http://gstreamer.freedesktop.org/releases/gst-ffmpeg";
+    homepage = "http://gstreamer.freedesktop.org/releases/gst-libav";
     description = "GStreamer's plug-in using FFmpeg";
     license = "GPLv2+";
   };
