da71b6d579a7d1ebd9a2a96c7e25b8d13e6d4c9a
Vladimír Čunát <vcunat@gmail.com>
2013-03-02 14:44:51 +0100

    rekonq: update
    Note it won't build with kde 4.7 and newer versions even require 4.9
    which we don't have yet (4.10 is the current upstream).

--- pkgs/applications/networking/browsers/rekonq/default.nix
+++ pkgs/applications/networking/browsers/rekonq/default.nix
@@ -1,14 +1,16 @@
-{ stdenv, fetchurl, kdelibs, gettext, pkgconfig, shared_desktop_ontologies, qca2, qoauth }:
+{ stdenv, fetchurl, kde4, gettext, pkgconfig, shared_desktop_ontologies, qca2, qoauth }:
+
+assert builtins.compareVersions "4.8.3" kde4.release != 1; # https://bugs.kde.org/show_bug.cgi?id=306077
 
 stdenv.mkDerivation rec {
-  name = "rekonq-1.1";
+  name = "rekonq-1.70"; # >=1.80 need kde >=4.9.0
 
   src = fetchurl {
     url = "mirror://sourceforge/rekonq/${name}.tar.bz2";
-    sha256 = "1bs733mwyfb7bxnial8n49b82ip04sark2mxwlq7ixxsbgq7972l";
+    sha256 = "05zqm329vajsn32l5b5nlbw92gl3irdcx8kifr4y921i9bkzigad";
   };
 
-  buildInputs = [ kdelibs qca2 qoauth ];
+  buildInputs = [ kde4.kdelibs qca2 qoauth ];
 
   nativeBuildInputs = [ gettext pkgconfig shared_desktop_ontologies ];
 
