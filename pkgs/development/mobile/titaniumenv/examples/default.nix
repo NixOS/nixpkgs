69a13e4e924b31d96708e2786fa2781e8af21faa
Sander van der Burg <svanderburg@gmail.com>
2013-03-05 15:57:54 +0100

    We don't need a reference here to titaniumenv, so remove it

--- pkgs/development/mobile/titaniumenv/examples/default.nix
+++ pkgs/development/mobile/titaniumenv/examples/default.nix
@@ -2,38 +2,31 @@
 
 let
   pkgs = import nixpkgs {};
-  pkgs_i686 = import nixpkgs { system = "i686-linux"; };
 in
 rec {
-  titaniumenv = import ./.. {
-    inherit pkgs pkgs_i686;
-  };
-  
   kitchensink_android = import ./kitchensink {
-    inherit titaniumenv;
-    inherit (pkgs) fetchgit;
+    inherit (pkgs) fetchgit titaniumenv;
     target = "android";
   };
   
   kitchensink_iphone = import ./kitchensink {
-    inherit titaniumenv;
-    inherit (pkgs) fetchgit;
+    inherit (pkgs) fetchgit titaniumenv;
     target = "iphone";
   };
   
   emulate_kitchensink = import ./emulate-kitchensink {
-    inherit (titaniumenv) androidenv;
+    inherit (pkgs.titaniumenv) androidenv;
     kitchensink = kitchensink_android;
   };
   
   simulate_kitchensink_iphone = import ./simulate-kitchensink {
-    inherit (titaniumenv) xcodeenv;
+    inherit (pkgs.titaniumenv) xcodeenv;
     kitchensink = kitchensink_iphone;
     device = "iPhone";
   };
   
   simulate_kitchensink_ipad = import ./simulate-kitchensink {
-    inherit (titaniumenv) xcodeenv;
+    inherit (pkgs.titaniumenv) xcodeenv;
     kitchensink = kitchensink_iphone;
     device = "iPad";
   };
