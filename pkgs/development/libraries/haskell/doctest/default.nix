e459a7514e82e85f925249ea0065d6b199114550
Peter Simons <simons@cryp.to>
2013-03-07 17:03:00 +0100

    haskell-doctest: jailbreak to support syb 0.4.0

--- pkgs/development/libraries/haskell/doctest/default.nix
+++ pkgs/development/libraries/haskell/doctest/default.nix
@@ -13,6 +13,7 @@ cabal.mkDerivation (self: {
     baseCompat deepseq filepath ghcPaths hspec HUnit QuickCheck setenv
     silently stringbuilder syb transformers
   ];
+  jailbreak = true;
   doCheck = false;
   meta = {
     homepage = "https://github.com/sol/doctest-haskell#readme";
