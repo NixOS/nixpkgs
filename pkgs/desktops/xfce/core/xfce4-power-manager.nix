96a0d0cc6fca7a4675654441e8add76197b164d9
Vladimír Čunát <vcunat@gmail.com>
2013-03-09 22:30:06 +0100

    xfce4-power-manager: update this forgotten package

--- pkgs/desktops/xfce/core/xfce4-power-manager.nix
+++ pkgs/desktops/xfce/core/xfce4-power-manager.nix
@@ -3,12 +3,12 @@
 
 stdenv.mkDerivation rec {
   p_name  = "xfce4-power-manager";
-  ver_maj = "1.0";
-  ver_min = "10";
+  ver_maj = "1.2";
+  ver_min = "0";
 
   src = fetchurl {
     url = "mirror://xfce/src/xfce/${p_name}/${ver_maj}/${name}.tar.bz2";
-    sha256 = "1w120k1sl4s459ijaxkqkba6g1p2sqrf9paljv05wj0wz12bpr40";
+    sha256 = "1sc4f4wci5yl3l9lk7vcsbwj6hdjshbxw9qm43s64jr882jriyyp";
   };
   name = "${p_name}-${ver_maj}.${ver_min}";
 
