29dac4ea3989245270fdf7c4f060418ab6015118
Peter Simons <simons@cryp.to>
2013-03-10 00:26:26 +0100

    haskell-stylish-haskell: update to version 0.5.6.1

--- pkgs/development/libraries/haskell/stylish-haskell/default.nix
+++ pkgs/development/libraries/haskell/stylish-haskell/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "stylish-haskell";
-  version = "0.5.6.0";
-  sha256 = "1cy40b7csna3fwq0bm5mx9d09x52vj517mf38yn8ymd0afff67sb";
+  version = "0.5.6.1";
+  sha256 = "0fxncnl9bvb7qjha3r06qli9qlzfljism6k688hrr9y6l06jdc2c";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
