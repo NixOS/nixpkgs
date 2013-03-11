c3dc9fbe6174af126314ab7d30ebedfde703f93d
Peter Simons <simons@cryp.to>
2013-03-11 11:20:20 +0100

    haskell-yesod-json: update to version 1.1.2.2

--- pkgs/development/libraries/haskell/yesod-json/default.nix
+++ pkgs/development/libraries/haskell/yesod-json/default.nix
@@ -1,16 +1,17 @@
-{ cabal, aeson, attoparsecConduit, blazeBuilder, conduit, safe
-, shakespeareJs, text, transformers, vector, wai, waiExtra
-, yesodCore, yesodRoutes
+{ cabal, aeson, attoparsecConduit, blazeBuilder, conduit, hspec
+, safe, shakespeareJs, text, transformers, vector, wai, waiExtra
+, waiTest, yesodCore, yesodRoutes
 }:
 
 cabal.mkDerivation (self: {
   pname = "yesod-json";
-  version = "1.1.2.1";
-  sha256 = "0vrhn3v26q7cqwds99hqyibq09vyxnviqzgyn7w5ldnl3ycg7dqd";
+  version = "1.1.2.2";
+  sha256 = "1bl4rs3biv2man0n8ijldil32lyswjqk5ykz0nv06qsaaafjpc3g";
   buildDepends = [
     aeson attoparsecConduit blazeBuilder conduit safe shakespeareJs
     text transformers vector wai waiExtra yesodCore yesodRoutes
   ];
+  testDepends = [ hspec text waiTest yesodCore ];
   meta = {
     homepage = "http://www.yesodweb.com/";
     description = "Generate content for Yesod using the aeson package";
