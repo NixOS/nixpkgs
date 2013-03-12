8d1ff12cdcb2d714515517417b9f3a4d4bdb2171
Lluís Batlle i Rossell <viric@viric.name>
2013-03-12 20:01:07 +0100

    mktorrent: fixing the usual pthread_cancel problem

--- pkgs/tools/misc/mktorrent/default.nix
+++ pkgs/tools/misc/mktorrent/default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation {
     sha256 = "17qi3nfky240pq6qcmf5qg324mxm83vk9r3nvsdhsvinyqm5d3kg";
   };
 
-  makeFlags = "USE_PTHREADS=1 USE_OPENSSL=1 USE_LONG_OPTIONS=1" +
+  makeFlags = "CFLAGS=-lgcc_s USE_PTHREADS=1 USE_OPENSSL=1 USE_LONG_OPTIONS=1" +
     stdenv.lib.optionalString stdenv.isi686 " USE_LARGE_FILES=1";
 
   preInstall = ''
