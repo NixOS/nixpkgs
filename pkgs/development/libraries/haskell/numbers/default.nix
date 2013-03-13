9d52ffce0288ebe3bff1ded360a75111debc5e15
Peter Simons <simons@cryp.to>
2013-03-13 11:58:45 +0100

    haskell-numbers: update to version 3000.1.0.3

--- pkgs/development/libraries/haskell/numbers/default.nix
+++ pkgs/development/libraries/haskell/numbers/default.nix
@@ -2,14 +2,13 @@
 
 cabal.mkDerivation (self: {
   pname = "numbers";
-  version = "3000.1.0.1";
-  sha256 = "0r2s47nfdxasnp8j7giwpxls9v48f6ld0gc2hg2p7y2ar5xfrcc4";
+  version = "3000.1.0.3";
+  sha256 = "0w2m2m3vp3lpvnc7wkw6pqfz741a68dma4s0asl7cryykwf94xgz";
   testDepends = [
     QuickCheck testFramework testFrameworkQuickcheck2
   ];
-  doCheck = false;
   meta = {
-    homepage = "https://github.com/DanBurton/numbers";
+    homepage = "https://github.com/DanBurton/numbers#readme";
     description = "Various number types";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
