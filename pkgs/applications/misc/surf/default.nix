9ac8a16277705dd43bc8509c9589910f8ca7abc5
Vladimír Čunát <vcunat@gmail.com>
2013-03-14 16:01:53 +0100

    vimprobable2, surf: fix the settings-schemas problem
    Note that they still segfault.

--- pkgs/applications/misc/surf/default.nix
+++ pkgs/applications/misc/surf/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, makeWrapper, gtk, webkit, pkgconfig, glib, glib_networking, libsoup, patches ? null}:
+{stdenv, fetchurl, makeWrapper, gtk, webkit, pkgconfig, glib, glib_networking, libsoup, gsettings_desktop_schemas, patches ? null}:
 
 stdenv.mkDerivation rec {
   name = "surf-${version}";
@@ -19,13 +19,14 @@ stdenv.mkDerivation rec {
 # `-lX11' to make sure libX11's store path is in the RPATH
   NIX_LDFLAGS = "-lX11";
   preConfigure = [ ''sed -i "s@PREFIX = /usr/local@PREFIX = $out@g" config.mk'' ];
- installPhase = ''
+  installPhase = ''
     make PREFIX=/ DESTDIR=$out install
-    wrapProgram "$out/bin/surf" --prefix GIO_EXTRA_MODULES : \
-      ${glib_networking}/lib/gio/modules
+    wrapProgram "$out/bin/surf" \
+      --prefix GIO_EXTRA_MODULES : ${glib_networking}/lib/gio/modules \
+      --prefix XDG_DATA_DIRS : "${gsettings_desktop_schemas}/share"
   '';
 
-  meta = { 
+  meta = {
       description = "surf is a simple web browser based on WebKit/GTK+. It is able to display websites and follow links. It supports the XEmbed protocol which makes it possible to embed it in another application. Furthermore, one can point surf to another URI by setting its XProperties.";
       homepage = http://surf.suckless.org;
       license = "MIT";
