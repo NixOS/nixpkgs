0236cc5d8826e3429fe8eb5e8b2851f68dfdad16
Lluís Batlle i Rossell <viric@viric.name>
2013-03-18 15:07:03 +0100

    iputils: build and install the manpages

--- pkgs/os-specific/linux/iputils/default.nix
+++ pkgs/os-specific/linux/iputils/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, fetchurl, libsysfs, gnutls, openssl, libcap }:
+{ stdenv, fetchurl, libsysfs, gnutls, openssl, libcap, sp, docbook_sgml_dtd_31
+, SGMLSpm }:
 
 assert stdenv ? glibc;
 
@@ -13,15 +14,28 @@ stdenv.mkDerivation rec {
     sha256 = "17riqp8dh8dvx32zv3hyrghpxz6xnxa6vai9b4yc485nqngm83s5";
   };
 
-  buildInputs = [ libsysfs gnutls openssl libcap ];
+  prePatch = ''
+    sed -i s/sgmlspl/sgmlspl.pl/ doc/Makefile
+  '';
 
-  buildFlags = "all ninfod";
+  buildInputs = [ libsysfs gnutls openssl libcap sp docbook_sgml_dtd_31 SGMLSpm ];
+
+  buildFlags = "man all ninfod";
+
+  # Stdenv doesn't handle symlinks well for that
+  dontGzipMan = true;
 
   installPhase =
     ''
       mkdir -p $out/sbin $out/bin
       cp -p ping ping6 tracepath tracepath6 traceroute6 $out/bin/
       cp -p clockdiff arping rdisc ninfod/ninfod $out/sbin/
+
+      mkdir -p $out/share/man/man8
+      cp -p doc/clockdiff.8 doc/arping.8 doc/ping.8 doc/rdisc.8 \
+        doc/tracepath.8 doc/ninfod.8 $out/share/man/man8
+      ln -s $out/share/man/man8/{ping,ping6}.8
+      ln -s $out/share/man/man8/{tracepath,tracepath6}.8
     '';
     
   meta = {
