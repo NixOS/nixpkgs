a5c4b2e236bbc029326d02c0e30565a4161eee4b
Peter Simons <simons@cryp.to>
2013-03-18 12:02:04 +0100

    haskell-tagged: update to version 0.4.5

--- pkgs/development/libraries/haskell/tagged/default.nix
+++ pkgs/development/libraries/haskell/tagged/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "tagged";
-  version = "0.4.4";
-  sha256 = "0bc88q33pyz4sx429awhxmnjzvmsg7vwwsqi2w7yf8lrmz5ph08x";
+  version = "0.4.5";
+  sha256 = "1ipj7ff2ya3r3w39clcrxs766hm568lj2kb2kd3npg87jj05wgv0";
   meta = {
     homepage = "http://github.com/ekmett/tagged";
     description = "Haskell 98 phantom types to avoid unsafely passing dummy arguments";
