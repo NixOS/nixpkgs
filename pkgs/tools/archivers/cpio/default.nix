32a503a5d34d22987d4c2a723a0155da543043bb
Vladimír Čunát <vcunat@gmail.com>
2013-03-20 23:01:36 +0100

    cpio: fix build without gets

--- pkgs/tools/archivers/cpio/default.nix
+++ pkgs/tools/archivers/cpio/default.nix
@@ -2,11 +2,13 @@
 
 stdenv.mkDerivation {
   name = "cpio-2.11";
-  
+
   src = fetchurl {
     url = mirror://gnu/cpio/cpio-2.11.tar.bz2;
     sha256 = "bb820bfd96e74fc6ce43104f06fe733178517e7f5d1cdee553773e8eff7d5bbd";
   };
+
+  patches = [ ./no-gets.patch ];
 
   meta = {
     homepage = http://www.gnu.org/software/cpio/;
