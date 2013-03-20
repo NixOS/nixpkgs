4d54ec04443a132a09b02c9c43125fb6c451ac89
Peter Simons <simons@cryp.to>
2013-03-20 10:44:55 +0100

    haskell-cereal-conduit: update to version 0.7

--- pkgs/development/libraries/haskell/cereal-conduit/default.nix
+++ pkgs/development/libraries/haskell/cereal-conduit/default.nix
@@ -1,10 +1,11 @@
-{ cabal, cereal, conduit, transformers, HUnit }:
+{ cabal, cereal, conduit, HUnit, mtl, resourcet, transformers }:
 
 cabal.mkDerivation (self: {
   pname = "cereal-conduit";
   version = "0.7";
   sha256 = "0cf0lp47qiilrdrzmn58hwh3q7fh7g55f2a1r1bw31xapp1cvbix";
-  buildDepends = [ cereal conduit transformers HUnit ];
+  buildDepends = [ cereal conduit transformers ];
+  testDepends = [ cereal conduit HUnit mtl resourcet transformers ];
   meta = {
     homepage = "https://github.com/litherum/cereal-conduit";
     description = "Turn Data.Serialize Gets and Puts into Sources, Sinks, and Conduits";
