bbc516c59e42759201e998a8aad8546d8c656a09
Peter Simons <simons@cryp.to>
2013-03-21 11:15:42 +0100

    haskell-gitit: update to version 0.10.3.1

--- pkgs/development/libraries/haskell/gitit/default.nix
+++ pkgs/development/libraries/haskell/gitit/default.nix
@@ -7,8 +7,8 @@
 
 cabal.mkDerivation (self: {
   pname = "gitit";
-  version = "0.10.3";
-  sha256 = "1hz4ddym2vn01nd22gb995dya48fzc7nsmsqywdc8hjczmn93qyg";
+  version = "0.10.3.1";
+  sha256 = "1sm6rryfyqr0nd4flbc5d520xyw2ajnkylvqf4fi4dhl6fnbpam5";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
