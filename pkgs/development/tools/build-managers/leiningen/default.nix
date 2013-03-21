3fdae2e5d94d044bb579f19e724d93266d7570ce
Moritz Ulrich <moritz@tarn-vedra.de>
2013-03-21 23:55:32 +0100

    Push leiningen to 2.1.1.
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- pkgs/development/tools/build-managers/leiningen/default.nix
+++ pkgs/development/tools/build-managers/leiningen/default.nix
@@ -2,20 +2,20 @@
 
 stdenv.mkDerivation rec {
   pname = "leiningen";
-  version = "2.1.0";
+  version = "2.1.1";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://raw.github.com/technomancy/leiningen/${version}/bin/lein-pkg";
-    sha256 = "1k1d1dr11jmm166r12wkw6j4wnlxk3p7wkvlk3cxhdrmyacqfrzk";
+    sha256 = "07asjk1pr47mgzf8m1igci9zhik49ycijhliq1mw001n9qqxlf74";
   };
 
   jarsrc = fetchurl {
     url = "https://leiningen.s3.amazonaws.com/downloads/leiningen-${version}-standalone.jar";
-    sha256 = "0y1llnbsgimxg3zpy070j98k1i6y1r0asn4h8aal0db8x3sj49j2";
+    sha256 = "1rzvkc0v66gxv6i5x4w7dn6bvd0dxylsvy7fhp84k9rd7cikk89j";
   };
 
-  patches = ./lein_2.1.0.patch;
+  patches = ./lein_2.1.1.patch;
 
   inherit rlwrap clojure;
 
