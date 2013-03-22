fb127d28cb02e2bf5a84e06f6749b48562d971a2
Rok Garbas <rok@garbas.si>
2013-03-23 00:38:18 +0100

    ipython: adding depenencies for ipython's notebook

--- pkgs/shells/ipython/default.nix
+++ pkgs/shells/ipython/default.nix
@@ -9,7 +9,7 @@ buildPythonPackage rec {
     sha256 = "1m4m0zf3llnicfgrbnl2h08p3662px7v2pzbhq4fq24vnyz6x5w2";
   };
 
-  propagatedBuildInputs = [ pythonPackages.readline pythonPackages.sqlite3 ];
+  propagatedBuildInputs = [ pythonPackages.readline pythonPackages.sqlite3 pythonPackages.tornado pythonPackages.pyzmq ];
 
   doCheck = false;
 
