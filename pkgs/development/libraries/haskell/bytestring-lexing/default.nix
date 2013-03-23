3aab8525ed82a4738e75d5cbd443e65d98f0f4f7
Peter Simons <simons@cryp.to>
2013-03-23 14:14:40 +0100

    haskell-bytestring-lexing: update to version 0.4.3

--- pkgs/development/libraries/haskell/bytestring-lexing/default.nix
+++ pkgs/development/libraries/haskell/bytestring-lexing/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "bytestring-lexing";
-  version = "0.4.2";
-  sha256 = "0s9rip1ik7f29n2i88277vhy7dqhc2bb5bb9l6fd47disb78ic9h";
+  version = "0.4.3";
+  sha256 = "0dynfrf8ym01v2dl57422h8r75kdlqh2qzqi9yc0f1bmbfqxap6r";
   buildTools = [ alex ];
   meta = {
     homepage = "http://code.haskell.org/~wren/";
