9e5910620f70e726b80130bea2fa55738bcb5e1b
Peter Simons <simons@cryp.to>
2013-03-23 14:14:40 +0100

    haskell-void: update to version 0.6

--- pkgs/development/libraries/haskell/void/default.nix
+++ pkgs/development/libraries/haskell/void/default.nix
@@ -1,10 +1,10 @@
-{ cabal, semigroups }:
+{ cabal, hashable, semigroups }:
 
 cabal.mkDerivation (self: {
   pname = "void";
-  version = "0.5.12";
-  sha256 = "03fqcap94saj7mx3y4pvvfj4z8dy6rsk2kvhgbnk2wvz5xm7xvci";
-  buildDepends = [ semigroups ];
+  version = "0.6";
+  sha256 = "0g1dja7qcp2d9a4m8j1f4ddyvbl003znyk7yn5w5qiiqr1pacs1n";
+  buildDepends = [ hashable semigroups ];
   meta = {
     homepage = "http://github.com/ekmett/void";
     description = "A Haskell 98 logically uninhabited data type";
