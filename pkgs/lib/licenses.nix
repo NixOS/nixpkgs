3388c3433e10149da9bdef4f871452744f586ebc
Peter Simons <simons@cryp.to>
2013-03-24 22:45:48 +0100

    pkgs/lib/licenses.nix: strip trailing whitespace

--- pkgs/lib/licenses.nix
+++ pkgs/lib/licenses.nix
@@ -9,7 +9,7 @@
     fullName = "Apache Software License 2.0";
     url = http://www.apache.org/licenses/LICENSE-2.0;
   };
-  
+
   boost = {
     shortName = "boost";
     fullName = "Boost Software License";
@@ -57,13 +57,13 @@
     fullName = "GNU General Public License version 2 only";
     url = http://www.gnu.org/licenses/old-licenses/gpl-2.0.html;
   };
-    
+
   gpl2Oss = {
     shortName = "GPLv2+OSS";
     fullName = "GNU General Public License version 2 only (with OSI approved licenses linking exception)";
     url = http://www.mysql.com/about/legal/licensing/foss-exception;
   };
-  
+
   gpl2Plus = {
     shortName = "GPLv2+";
     fullName = "GNU General Public License version 2 or later";
@@ -75,7 +75,7 @@
     fullName = "GNU General Public License version 3 only";
     url = http://www.fsf.org/licensing/licenses/gpl.html;
   };
-  
+
   gpl3Plus = {
     shortName = "GPLv3+";
     fullName = "GNU General Public License version 3 or later";
@@ -111,66 +111,66 @@
     fullName = "libtiff license";
     url = https://fedoraproject.org/wiki/Licensing/libtiff;
   };
-  
+
   lgpl2 = {
     shortName = "LGPLv2";
     fullName = "GNU Library General Public License version 2";
     url = http://www.gnu.org/licenses/old-licenses/lgpl-2.0.html;
   };
-  
+
   lgpl2Plus = {
     shortName = "LGPLv2+";
     fullName = "GNU Library General Public License version 2 or later";
     url = http://www.gnu.org/licenses/old-licenses/lgpl-2.0.html;
   };
-  
+
   lgpl21 = {
     shortName = "LGPLv2.1";
     fullName = "GNU Lesser General Public License version 2.1 only";
     url = http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html;
   };
-  
+
   lgpl21Plus = {
     shortName = "LGPLv2.1+";
     fullName = "GNU Lesser General Public License version 2.1 or later";
     url = http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html;
   };
-  
+
   lgpl3 = {
     shortName = "LGPLv3";
     fullName = "GNU Lesser General Public License version 3 only";
     url = http://www.fsf.org/licensing/licenses/lgpl.html;
   };
-  
+
   lgpl3Plus = {
     shortName = "LGPLv3+";
     fullName = "GNU Lesser General Public License version 3 or later";
     url = http://www.fsf.org/licensing/licenses/lgpl.html;
   };
-  
+
   mit = {
     shortName = "MIT";
     fullName = "MIT/X11 license";
     url = http://www.opensource.org/licenses/mit-license.php;
   };
-  
+
   mpl11 = {
     shortName = "MPL1.1";
     fullName = "Mozilla Public License version 1.1";
     url = http://www.mozilla.org/MPL/MPL-1.1.html;
   };
-  
+
   openssl = {
     shortName = "openssl";
     fullName = "OpenSSL license";
     url = http://www.openssl.org/source/license.html;
   };
-  
+
   publicDomain = {
     shortName = "Public Domain";
     fullname = "Public Domain";
   };
-  
+
   psfl = {
     shortName = "PSFL";
     fullName = "Python Software Foundation License";
