9497164f619f09c9ccf7f06612cd9d1689e2e124
Peter Simons <simons@cryp.to>
2013-03-24 17:33:53 +0100

    haskell-conduit: update to version 1.0.4.1

--- pkgs/development/libraries/haskell/conduit/default.nix
+++ pkgs/development/libraries/haskell/conduit/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "conduit";
-  version = "1.0.4";
-  sha256 = "1y97wc273i3qvq4nqp9kr3bgl8mca257hv92f3lbq2wzqkr5vahk";
+  version = "1.0.4.1";
+  sha256 = "1fihn6ixs6cmim5y605w2mzjrcwplr58r835wq9k3arb25d5wnys";
   buildDepends = [
     liftedBase mmorph monadControl resourcet text transformers
     transformersBase void
@@ -13,7 +13,6 @@ cabal.mkDerivation (self: {
   testDepends = [
     doctest hspec QuickCheck resourcet text transformers void
   ];
-  jailbreak = true;
   meta = {
     homepage = "http://github.com/snoyberg/conduit";
     description = "Streaming data processing library";
