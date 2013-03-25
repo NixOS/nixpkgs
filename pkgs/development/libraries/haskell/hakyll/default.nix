67c9b39b60f030c6940a106ad375f26b0e3de04f
Peter Simons <simons@cryp.to>
2013-03-25 11:40:29 +0100

    haskell-hakyll: update to version 4.2.2.0

--- pkgs/development/libraries/haskell/hakyll/default.nix
+++ pkgs/development/libraries/haskell/hakyll/default.nix
@@ -1,26 +1,29 @@
 { cabal, binary, blazeHtml, blazeMarkup, citeprocHs, cmdargs
-, cryptohash, deepseq, filepath, httpConduit, httpTypes, HUnit
-, lrucache, mtl, pandoc, parsec, QuickCheck, random, regexBase
-, regexTdfa, snapCore, snapServer, tagsoup, testFramework
-, testFrameworkHunit, testFrameworkQuickcheck2, text, time
+, cryptohash, dataDefault, deepseq, filepath, httpConduit
+, httpTypes, HUnit, lrucache, mtl, pandoc, parsec, QuickCheck
+, random, regexBase, regexTdfa, snapCore, snapServer, tagsoup
+, testFramework, testFrameworkHunit, testFrameworkQuickcheck2, text
+, time
 }:
 
 cabal.mkDerivation (self: {
   pname = "hakyll";
-  version = "4.2.1.2";
-  sha256 = "0b2jmi9hi5l72lkyjn2w3gwn52zvnvv7c10x5329hp000gzmwbvi";
+  version = "4.2.2.0";
+  sha256 = "0kz8v2ip0hmvqnrxgv44g2863z1dql88razl7aa3fw01q56ihz0y";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
-    binary blazeHtml blazeMarkup citeprocHs cmdargs cryptohash deepseq
-    filepath httpConduit httpTypes lrucache mtl pandoc parsec random
-    regexBase regexTdfa snapCore snapServer tagsoup text time
+    binary blazeHtml blazeMarkup citeprocHs cmdargs cryptohash
+    dataDefault deepseq filepath httpConduit httpTypes lrucache mtl
+    pandoc parsec random regexBase regexTdfa snapCore snapServer
+    tagsoup text time
   ];
   testDepends = [
-    binary blazeHtml blazeMarkup citeprocHs cmdargs cryptohash deepseq
-    filepath httpConduit httpTypes HUnit lrucache mtl pandoc parsec
-    QuickCheck random regexBase regexTdfa snapCore snapServer tagsoup
-    testFramework testFrameworkHunit testFrameworkQuickcheck2 text time
+    binary blazeHtml blazeMarkup citeprocHs cmdargs cryptohash
+    dataDefault deepseq filepath httpConduit httpTypes HUnit lrucache
+    mtl pandoc parsec QuickCheck random regexBase regexTdfa snapCore
+    snapServer tagsoup testFramework testFrameworkHunit
+    testFrameworkQuickcheck2 text time
   ];
   doCheck = false;
   meta = {
