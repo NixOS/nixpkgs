9ce23dfa6830e7b7dd8bbe3c3fcd16ec08385ee4
Shea Levy <shea@shealevy.com>
2013-03-25 17:20:54 -0400

    e1000e: Use kernel.dev instead of kernelDev
    Doesn't build, due to outdated header expectations
    
    Signed-off-by: Shea Levy <shea@shealevy.com>

--- pkgs/os-specific/linux/e1000e/default.nix
+++ pkgs/os-specific/linux/e1000e/default.nix
@@ -1,24 +1,21 @@
-{ stdenv, fetchurl, kernelDev }:
+{ stdenv, fetchurl, kernel }:
 
 stdenv.mkDerivation {
-  name = "e1000e-1.5.1-${kernelDev.version}";
+  name = "e1000e-1.5.1-${kernel.version}";
 
   src = fetchurl {
     url = "http://downloads.sourceforge.net/e1000/e1000e-1.5.1.tar.gz";
     sha256 = "0nzjlarpqcpm5y112n3vzra4qv32hiygpfkk10y8g4nln4adhqsw";
   };
 
-  buildInputs = [ kernelDev ];
-
   configurePhase = ''
     cd src
-    kernel_version=$( cd ${kernelDev}/lib/modules && echo * )
-    sed -i -e 's|/lib/modules|${kernelDev}/lib/modules|' Makefile
-    export makeFlags="BUILD_KERNEL=$kernel_version"
+    sed -i -e 's|/lib/modules|${kernel.dev}/lib/modules|' Makefile
+    export makeFlags="BUILD_KERNEL=${kernel.modDirVersion}"
   '';
 
   installPhase = ''
-    install -v -D -m 644 e1000e.ko "$out/lib/modules/$kernel_version/kernel/drivers/net/e1000e/e1000e.ko"
+    install -v -D -m 644 e1000e.ko "$out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/e1000e/e1000e.ko"
   '';
 
   dontStrip = true;
