1b0acab1e50048d431644a68ee5c66aaac47d810
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-03-26 13:13:52 +0100

    Only include Linux builds in the NixOS channel

--- release-combined.nix
+++ release-combined.nix
@@ -1,4 +1,11 @@
+{ nixpkgs, officialRelease }:
+
 {
   nixos = import ./release.nix;
-  nixpkgs = import <nixpkgs/pkgs/top-level/release.nix>;
+  nixpkgs =
+    import <nixpkgs/pkgs/top-level/release.nix> {
+      inherit nixpkgs officialRelease;
+      # Only do Linux builds.
+      supportedSystems = [ "x86_64-linux" "i686-linux" ];
+    };
 }
