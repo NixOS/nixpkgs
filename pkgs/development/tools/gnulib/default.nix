96dbbc7e211ab56c701340c279428d2c95e10c7c
Peter Simons <simons@cryp.to>
2013-03-26 01:12:27 +0100

    gnulib: update to version 0.0-7899-g34f8464 and provide a proper meta section

--- pkgs/development/tools/gnulib/default.nix
+++ pkgs/development/tools/gnulib/default.nix
@@ -1,7 +1,21 @@
 { stdenv, fetchgit }:
 
-stdenv.lib.setName "gnulib-0.0-7898-gdb9cad7" (fetchgit {
-  url = "http://git.savannah.gnu.org/r/gnulib.git";
-  rev = "db9cad7b8b59a010ff9158513a29cb002a2b8ae1";
-  sha256 = "1azdz7mhx1jf0l17wn7zp591wxry0ys4g29ycij5mai7vzhddjn6";
-})
+stdenv.mkDerivation {
+  name = "gnulib-0.0-7899-g34f8464";
+
+  src = fetchgit {
+    url = "http://git.savannah.gnu.org/r/gnulib.git";
+    rev = "34f84640cd015eee3d9aed3b1eddf6f361576890";
+    sha256 = "9267827d6b3f0eb97957eae7851afd4e5ac8aa714a83e3be52e7b660cc27851d";
+  };
+
+  buildPhase = ":";
+
+  installPhase = "mkdir -p $out; mv * $out/";
+
+  meta = {
+    homepage = "http://www.gnu.org/software/gnulib/";
+    description = "central location for code to be shared among GNU packages";
+    license = stdenv.lib.licenses.gpl3Plus;
+  };
+}
