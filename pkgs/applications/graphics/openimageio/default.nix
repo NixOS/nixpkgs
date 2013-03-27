67a1becfdca20d51cc97d99103e6978b209844d2
Cillian de Róiste <goibhniu@fsfe.org>
2013-03-27 23:29:17 +0100

    OpenImageIO Tidy Up: moved to applications/graphics
    oiio includes CLI tools for manipulating graphics

--- pkgs/applications/graphics/openimageio/default.nix
+++ pkgs/applications/graphics/openimageio/default.nix
@@ -1,4 +1,6 @@
-{ stdenv, fetchurl, cmake, unzip, boost, libjpeg, libtiff, libpng, openexr, ilmbase }:
+{ stdenv, fetchurl, boost, cmake, ilmbase, libjpeg, libpng, libtiff, openexr
+, unzip
+}:
 
 stdenv.mkDerivation rec {
   name = "oiio-${version}";
@@ -9,13 +11,16 @@ stdenv.mkDerivation rec {
     sha256 = "08a6qhplzs8kianqb1gjgrndg81h3il5531jn9g6i4940b1xispg";
   };
 
-  buildInputs = [ cmake unzip boost libjpeg libtiff libpng openexr ilmbase ];
+  buildInputs = [ boost cmake ilmbase libjpeg libpng libtiff openexr unzip ];
 
   configurePhase = "";
 
-  buildPhase = "make ILMBASE_HOME=${ilmbase} OPENEXR_HOME=${openexr} USE_PYTHON=0 INSTALLDIR=$out dist_dir=";
+  buildPhase = ''
+    make ILMBASE_HOME=${ilmbase} OPENEXR_HOME=${openexr} USE_PYTHON=0 \
+      INSTALLDIR=$out dist_dir=
+  '';
 
-  installPhase = "echo hallo";
+  installPhase = "";
 
   meta = with stdenv.lib; {
     homepage = http://www.openimageio.org;
