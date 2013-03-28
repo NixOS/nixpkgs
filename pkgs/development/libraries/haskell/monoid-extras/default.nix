b5bda573d5fd9fde2c657624d6ce5e9100a35af2
Peter Simons <simons@cryp.to>
2013-03-28 14:40:12 +0100

    haskell-monoid-extras: update to version 0.2.2.3

--- pkgs/development/libraries/haskell/monoid-extras/default.nix
+++ pkgs/development/libraries/haskell/monoid-extras/default.nix
@@ -2,10 +2,9 @@
 
 cabal.mkDerivation (self: {
   pname = "monoid-extras";
-  version = "0.2.2.2";
-  sha256 = "1fz93hm0sswisvwvbygxvbwmmnzqcxmz9h82i4361wzychf2si22";
+  version = "0.2.2.3";
+  sha256 = "00yj7wdyznsis82fb7i07s0vz8vsn2mpqk7jkgl9xxa57gk1rsax";
   buildDepends = [ semigroups ];
-  jailbreak = true;
   meta = {
     description = "Various extra monoid-related definitions and utilities";
     license = self.stdenv.lib.licenses.bsd3;
