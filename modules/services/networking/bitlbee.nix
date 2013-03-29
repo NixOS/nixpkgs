8d0a7cb6d24900a6c8f8001a6da0029f89179c8c
Pascal Wittmann <mail@pascal-wittmann.de>
2013-03-29 12:51:47 +0100

    Bitlbee: tabs to spaces

--- modules/services/networking/bitlbee.nix
+++ modules/services/networking/bitlbee.nix
@@ -23,59 +23,58 @@ in
     services.bitlbee = {
 
       enable = mkOption {
-	default = false;
-	description = ''
-	  Whether to run the BitlBee IRC to other chat network gateway.
-	  Running it allows you to access the MSN, Jabber, Yahoo! and ICQ chat
-	  networks via an IRC client.
-	'';
+        default = false;
+        description = ''
+          Whether to run the BitlBee IRC to other chat network gateway.
+          Running it allows you to access the MSN, Jabber, Yahoo! and ICQ chat
+          networks via an IRC client.
+        '';
       };
 
       interface = mkOption {
-	default = "127.0.0.1";
-	description = ''
-	  The interface the BitlBee deamon will be listening to.  If `127.0.0.1',
-	  only clients on the local host can connect to it; if `0.0.0.0', clients
-	  can access it from any network interface.
-	'';
+        default = "127.0.0.1";
+        description = ''
+          The interface the BitlBee deamon will be listening to.  If `127.0.0.1',
+          only clients on the local host can connect to it; if `0.0.0.0', clients
+          can access it from any network interface.
+        '';
       };
 
       portNumber = mkOption {
-	default = 6667;
-	description = ''
-	  Number of the port BitlBee will be listening to.
-	'';
+        default = 6667;
+        description = ''
+          Number of the port BitlBee will be listening to.
+        '';
       };
 
       authMode = mkOption {
-	default = "Open";
-	check = authModeCheck;
-	description = ''
-	  The following authentication modes are available:
-	  	Open -- Accept connections from anyone, use NickServ for user authentication.
-	  	Closed -- Require authorization (using the PASS command during login) before allowing the user to connect at all.
-	  	Registered -- Only allow registered users to use this server; this disables the register- and the account command until the user identifies himself.
-	'';	
+        default = "Open";
+        check = authModeCheck;
+        description = ''
+          The following authentication modes are available:
+            Open -- Accept connections from anyone, use NickServ for user authentication.
+            Closed -- Require authorization (using the PASS command during login) before allowing the user to connect at all.
+            Registered -- Only allow registered users to use this server; this disables the register- and the account command until the user identifies himself.
+        ''; 
       };
 
       extraSettings = mkOption {
-	default = "";
-	description = ''
-	  Will be inserted in the Settings section of the config file.
-	'';	
+        default = "";
+        description = ''
+          Will be inserted in the Settings section of the config file.
+        ''; 
       };
 
       extraDefaults = mkOption {
-	default = "";
-	description = ''
-	  Will be inserted in the Default section of the config file.
-	'';	
+        default = "";
+        description = ''
+          Will be inserted in the Default section of the config file.
+        ''; 
       };
 
     };
 
   };
-
 
   ###### implementation
 
@@ -83,44 +82,43 @@ in
 
     users.extraUsers = singleton
       { name = "bitlbee";
-	uid = bitlbeeUid;
-	description = "BitlBee user";
-	home = "/var/lib/bitlbee";
+        uid = bitlbeeUid;
+        description = "BitlBee user";
+        home = "/var/lib/bitlbee";
       };
 
     users.extraGroups = singleton
       { name = "bitlbee";
-	gid = config.ids.gids.bitlbee;
+        gid = config.ids.gids.bitlbee;
       };
 
     systemd.services.bitlbee = 
       { description = "BitlBee IRC to other chat networks gateway";
         after = [ "network.target" ];
         wantedBy = [ "multi-user.target" ];
-	serviceConfig.ExecStart = "${pkgs.bitlbee}/sbin/bitlbee -F -n -c /etc/bitlbee.conf";
+        serviceConfig.ExecStart = "${pkgs.bitlbee}/sbin/bitlbee -F -n -c /etc/bitlbee.conf";
       };
 
     environment.systemPackages = [ pkgs.bitlbee ];
 
     environment.etc =
       [
-	{ source = pkgs.writeText "bitlbee.conf"
-		''
-		[settings]
-		RunMode = Daemon
-		User = bitlbee	
-		ConfigDir = /var/lib/bitlbee			
-		DaemonInterface = ${cfg.interface}
-		DaemonPort = ${toString cfg.portNumber} 
-		AuthMode = ${cfg.authMode}
-		${cfg.extraSettings}
-		[defaults]
-		${cfg.extraDefaults}
-		'';
+        { source = pkgs.writeText "bitlbee.conf"
+            ''
+            [settings]
+            RunMode = Daemon
+            User = bitlbee  
+            ConfigDir = /var/lib/bitlbee      
+            DaemonInterface = ${cfg.interface}
+            DaemonPort = ${toString cfg.portNumber} 
+            AuthMode = ${cfg.authMode}
+            ${cfg.extraSettings}
+            [defaults]
+            ${cfg.extraDefaults}
+            '';
           target = "bitlbee.conf";
         }
       ];
-
   };
 
 }
