13d40ae54db8cf5f39c350d14e2a6a013a9ec34b
Lluís Batlle i Rossell <viric@viric.name>
2013-03-30 21:05:29 +0100

    otpw: fixing the entropy source (PATH variables)
    I don't know why it set some hardcoded PATH.

--- pkgs/os-specific/linux/otpw/default.nix
+++ pkgs/os-specific/linux/otpw/default.nix
@@ -1,15 +1,17 @@
 { stdenv, fetchurl, pam }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "otpw-1.3";
 
   src = fetchurl {
-    url = http://www.cl.cam.ac.uk/~mgk25/download/otpw-1.3.tar.gz;
+    url = "http://www.cl.cam.ac.uk/~mgk25/download/${name}.tar.gz";
     sha256 = "1k3hc7xbxz6hkc55kvddi3cibafwf93ivn58sy1l888d3l5dwmrk";
   };
 
   patchPhase = ''
     sed -i 's/^CFLAGS.*/CFLAGS=-O2 -fPIC/' Makefile
+    sed -i -e 's,PATH=.*;,,' conf.h
+    sed -i -e '/ENTROPY_ENV/d' otpw-gen.c
   '';
 
   installPhase = ''
