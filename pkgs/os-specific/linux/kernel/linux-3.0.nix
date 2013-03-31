37de48e43d18dce5e88f7fb7815f51be2e2916cc
Mathijs Kwik <mathijs@bluescreen303.nl>
2013-03-31 11:10:48 +0200

    linux-3.0: upgrade to 3.0.71

--- pkgs/os-specific/linux/kernel/linux-3.0.nix
+++ pkgs/os-specific/linux/kernel/linux-3.0.nix
@@ -231,7 +231,7 @@ in
 import ./generic.nix (
 
   rec {
-    version = "3.0.70";
+    version = "3.0.71";
 
     preConfigure = ''
       substituteInPlace scripts/depmod.sh --replace '-b "$INSTALL_MOD_PATH"' ""
@@ -239,7 +239,7 @@ import ./generic.nix (
 
     src = fetchurl {
       url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-      sha256 = "0hxb457mixpcq43dg0lnbkfdjnzqw4ajfcfkyyfgdzn5496li6va";
+      sha256 = "1qavk6kp84h02bs0clhpri148lfj7zdzj7jjy07vw0h48lmj1405";
     };
 
     config = configWithPlatform stdenv.platform;
