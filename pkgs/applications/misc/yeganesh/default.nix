f3a16d0dd53134f29636210e3587695c586f92bc
Peter Simons <simons@cryp.to>
2013-04-01 17:18:37 +0200

    update meta.maintainers section

--- pkgs/applications/misc/yeganesh/default.nix
+++ pkgs/applications/misc/yeganesh/default.nix
@@ -12,5 +12,6 @@ cabal.mkDerivation (self: {
     description = "small dmenu wrapper";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
