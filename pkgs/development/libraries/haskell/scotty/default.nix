c3cad285f50f050f1a1f496ed0f4c27ff5238e6d
Peter Simons <simons@cryp.to>
2013-04-04 11:04:20 +0200

    haskell-scotty: update to version 0.4.6

--- pkgs/development/libraries/haskell/scotty/default.nix
+++ pkgs/development/libraries/haskell/scotty/default.nix
@@ -1,6 +1,5 @@
-{ cabal, aeson, blazeBuilder, caseInsensitive, conduit
-, dataDefault, httpTypes, mtl, regexCompat, resourcet
-, text, wai, waiExtra, warp
+{ cabal, aeson, blazeBuilder, caseInsensitive, conduit, dataDefault
+, httpTypes, mtl, regexCompat, resourcet, text, wai, waiExtra, warp
 }:
 
 cabal.mkDerivation (self: {
@@ -8,25 +7,14 @@ cabal.mkDerivation (self: {
   version = "0.4.6";
   sha256 = "0g83kgqr1p03z7dks6x00id2gz95kkw00wmwp5vyz4zvx1mmmvk8";
   buildDepends = [
-    aeson
-    blazeBuilder
-    caseInsensitive
-    conduit
-    dataDefault
-    httpTypes
-    mtl
-    regexCompat
-    resourcet
-    text
-    wai
-    waiExtra
-    warp
+    aeson blazeBuilder caseInsensitive conduit dataDefault httpTypes
+    mtl regexCompat resourcet text wai waiExtra warp
   ];
   meta = {
-    homepage = "https://github.com/xich/scotty";
-    description =
-      "Haskell web framework inspired by Ruby's Sinatra, using WAI and Warp";
-    license = self.stdenv.lib.licenses.bsd;
+    homepage = "https://github.com/ku-fpg/scotty";
+    description = "Haskell web framework inspired by Ruby's Sinatra, using WAI and Warp";
+    license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    maintainers = [ self.stdenv.lib.maintainers.simons ];
   };
 })
