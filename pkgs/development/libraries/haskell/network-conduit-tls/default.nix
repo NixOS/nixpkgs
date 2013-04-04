fbe9c7138b41b8845fc2c7ee2cc775c7c671c354
Peter Simons <simons@cryp.to>
2013-04-04 16:22:47 +0200

    haskell-network-conduit-tls: update to version 1.0.0.2

--- pkgs/development/libraries/haskell/network-conduit-tls/default.nix
+++ pkgs/development/libraries/haskell/network-conduit-tls/default.nix
@@ -5,8 +5,8 @@
 
 cabal.mkDerivation (self: {
   pname = "network-conduit-tls";
-  version = "1.0.0.1";
-  sha256 = "1bfb888j7raan764sgq50xxmckgqg3cnz3fcmvpqdjp7lclh313z";
+  version = "1.0.0.2";
+  sha256 = "1vzhalz6hxal73rxm6f2l9m7j34mldamz16wrb6ay67wg6giq55z";
   buildDepends = [
     aeson certificate conduit cryptoApi cryptoRandomApi network
     networkConduit pem systemFileio systemFilepath tls tlsExtra
