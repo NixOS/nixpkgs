ad5b5bcf0a7e0624bf2b50b41d872d52d013ccc5
Lluís Batlle i Rossell <viric@viric.name>
2013-04-05 16:11:03 +0200

    skype-call-recorder: fix expression and improve conferences
    I missed a semicolon; I also add an upstream patch about
    improving the hosted conferences support:
    http://repo.or.cz/w/skype-call-recorder.git/commit/abd67f1d44eef81baf2e9729f95e002c4ecc7350

--- pkgs/applications/networking/instant-messengers/skype-call-recorder/default.nix
+++ pkgs/applications/networking/instant-messengers/skype-call-recorder/default.nix
@@ -7,16 +7,20 @@ stdenv.mkDerivation {
     sha256 = "1iijkhq3aj9gr3bx6zl8ryvzkqcdhsm9yisimakwq0lnw0lgf5di";
   };
 
-  patchPhase = ''
+  # Keep an rpath reference to the used libogg
+  prePatch = ''
     sed -i -e '/ADD_EXECUTABLE/aSET(LIBRARIES ''${LIBRARIES} ogg)' CMakeLists.txt
   '';
+
+  # Better support for hosted conferences
+  patches = [ ./conference.patch ];
 
   buildInputs = [ cmake lame id3lib libvorbis qt4 libogg ];
 
   meta = {
     homepage = http://atdot.ch/scr/;
     description = "Open source tool to record your Skype calls on Linux";
-    license = "GPLv2+"
+    license = "GPLv2+";
     platforms = with stdenv.lib.platforms; linux;
     maintainers = with stdenv.lib.maintainers; [viric];
   };
