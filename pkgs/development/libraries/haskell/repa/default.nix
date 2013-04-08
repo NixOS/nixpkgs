203fd72f8fccabf1ca9aa5fe8a35c6658bdc3152
Peter Simons <simons@cryp.to>
2013-04-08 12:14:02 +0200

    haskell-repa: update to version 3.2.3.2

--- pkgs/development/libraries/haskell/repa/default.nix
+++ pkgs/development/libraries/haskell/repa/default.nix
@@ -2,10 +2,9 @@
 
 cabal.mkDerivation (self: {
   pname = "repa";
-  version = "3.2.3.1";
-  sha256 = "0r5z781h9v6ri2m3ih7jbymvc3i2m26qaf29pxwmaks0sdlp4qmr";
+  version = "3.2.3.2";
+  sha256 = "0xwaqkir4gjlhnvd1r69ycswg4p65ig38d2np69hv195zr47pmvh";
   buildDepends = [ QuickCheck vector ];
-  jailbreak = true;
   meta = {
     homepage = "http://repa.ouroborus.net";
     description = "High performance, regular, shape polymorphic parallel arrays";
