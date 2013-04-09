a679422a7df74f70710b4e89e9a34790bed9932d
Peter Simons <simons@cryp.to>
2013-04-09 12:20:07 +0200

    haskell-fsnotify: update to version 0.0.7

--- pkgs/development/libraries/haskell/fsnotify/default.nix
+++ pkgs/development/libraries/haskell/fsnotify/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "fsnotify";
-  version = "0.0.6";
-  sha256 = "0ib6ansj3vaq9hxxbsq5jw14w2b61wp4jfahzb3c3x46mdb1bqw5";
+  version = "0.0.7";
+  sha256 = "0yx69kdy67pjkwfrd5r00lhprbf98j9za31f7vaxxf2lbgqlmk9q";
   buildDepends = [ hinotify systemFileio systemFilepath text time ];
   testDepends = [
     Cabal Glob hinotify hspec QuickCheck random systemFileio
