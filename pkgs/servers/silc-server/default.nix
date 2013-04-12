5e6c0de6c9f1dfd31e58668ea04e08c41d23a452
Lluís Batlle i Rossell <viric@viric.name>
2013-04-12 21:10:30 +0200

    silc-server: Nicer fetchurl src.

--- pkgs/servers/silc-server/default.nix
+++ pkgs/servers/silc-server/default.nix
@@ -1,10 +1,10 @@
 { stdenv, fetchurl }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "silc-server-1.1.18";
 
   src = fetchurl {
-    url = "http://silcnet.org/download/server/sources/silc-server-1.1.18.tar.bz2";
+    url = "http://silcnet.org/download/server/sources/${name}.tar.bz2";
     sha256 = "0nr0hrwr4kbi611qazmrify7a27nzxb5n7d97f5i9cw3avxlw38s";
   };
 
