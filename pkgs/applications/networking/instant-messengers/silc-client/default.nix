acf2979d9ca58c822923dac323ba8dd1ec7a83e6
Lluís Batlle i Rossell <viric@viric.name>
2013-04-12 23:41:09 +0200

    silc_client: Missing file

---
+++ pkgs/applications/networking/instant-messengers/silc-client/default.nix
@@ -0,0 +1,29 @@
+{ stdenv, fetchurl, perl, pkgconfig, glib, ncurses
+, enablePlugin ? true }:
+
+stdenv.mkDerivation rec {
+  name = "silc-client-1.1.8" + stdenv.lib.optionalString enablePlugin "-irssi-plugin";
+
+  src = fetchurl {
+    url = "http://silcnet.org/download/server/sources/${name}.tar.bz2";
+    sha256 = "1qnk35g8sbnfps3bq2k9sy0ymlsijh5n8z59m2ccq4pkmqbfqgv2";
+  };
+
+  dontDisableStatic = true;
+
+  patches = [ ./server_setup.patch ];
+
+  preConfigure = stdenv.lib.optionalString enablePlugin ''
+    configureFlags="$configureFlags --with-silc-plugin=$out/lib/irssi"
+  '';
+
+  buildInputs = [ perl pkgconfig glib ncurses ];
+
+  meta = {
+    homepage = http://silcnet.org/;
+    description = "Secure Internet Live Conferencing server";
+    license = "GPLv2";
+    maintainers = with stdenv.lib.maintainers; [viric];
+    platforms = with stdenv.lib.platforms; linux;
+  };
+}
