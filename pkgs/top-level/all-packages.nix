93c39d24b5b3563c3751bc2bc5fd473718be4e52
Jonas Hoersch <coroa@online.de>
2013-04-12 13:13:32 +0200

    offlineimap: enable support for the sqlite status backend

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1296,7 +1296,9 @@ let
 
   odt2txt = callPackage ../tools/text/odt2txt { };
 
-  offlineimap = callPackage ../tools/networking/offlineimap { };
+  offlineimap = callPackage ../tools/networking/offlineimap {
+    inherit (pythonPackages) sqlite3;
+  };
 
   opendbx = callPackage ../development/libraries/opendbx { };
 
