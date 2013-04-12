2080202a34ee79798fa1531ede3c514b94a0eae5
Lluís Batlle i Rossell <viric@viric.name>
2013-04-12 23:48:29 +0200

    silc_client: more url problems
    It was harder to test, after nix-prefetch-url. Damn it.

--- pkgs/applications/networking/instant-messengers/silc-client/default.nix
+++ pkgs/applications/networking/instant-messengers/silc-client/default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation {
   name = basename + stdenv.lib.optionalString enablePlugin "-irssi-plugin";
 
   src = fetchurl {
-    url = "http://silcnet.org/download/server/sources/${basename}.tar.bz2";
+    url = "http://silcnet.org/download/client/sources/${basename}.tar.bz2";
     sha256 = "1qnk35g8sbnfps3bq2k9sy0ymlsijh5n8z59m2ccq4pkmqbfqgv2";
   };
 
