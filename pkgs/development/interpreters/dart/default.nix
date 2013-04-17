237192ce6e16f143c7b0c317c08e1f175567ad38
Zef Hemel <zef@zef.me>
2013-04-17 16:50:02 +0200

    Adjusted to point to version URL (hosted by me -- had to).

--- pkgs/development/interpreters/dart/default.nix
+++ pkgs/development/interpreters/dart/default.nix
@@ -12,6 +12,8 @@ stdenv.mkDerivation {
     patchelf --interpreter "$(cat $NIX_GCC/nix-support/dynamic-linker)" \
              --set-rpath $libPath \
              $out/bin/dart
+    
+    # Hack around weird dart2js resolving bug
     mv $out/bin/dart2js $out/bin/.dart2js
     echo "#!/bin/sh" > $out/bin/dart2js
     echo "$out/bin/.dart2js \$*" >> $out/bin/dart2js
@@ -21,14 +23,14 @@ stdenv.mkDerivation {
   
   src =
     if stdenv.system == "x86_64-linux" then
-     fetchurl {
-       url = https://storage.googleapis.com/dart-editor-archive-integration/latest/dartsdk-linux-64.tar.gz;
-       sha256 = "1riwxxczskfsaax7n03m7isnbxf3walky0cac1w8j5apr1xvg5ma";
+      fetchurl {
+        url = http://download.zef.s3.amazonaws.com/dartsdk-m4-linux-64.tar.gz;
+        sha256 = "1riwxxczskfsaax7n03m7isnbxf3walky0cac1w8j5apr1xvg5ma";
       }
     else
-     fetchurl {
-       url = https://storage.googleapis.com/dart-editor-archive-integration/latest/dartsdk-linux-32.tar.gz;
-       sha256 = "00935c4vxfj2h3x354g75qdazswwissbwc7kj5k05l1m3lizikf6";
+      fetchurl {
+        url = http://download.zef.s3.amazonaws.com/dartsdk-m4-linux-32.tar.gz;
+        sha256 = "00935c4vxfj2h3x354g75qdazswwissbwc7kj5k05l1m3lizikf6";
       };
  
   libPath = stdenv.lib.makeLibraryPath [ stdenv.gcc.gcc ];
