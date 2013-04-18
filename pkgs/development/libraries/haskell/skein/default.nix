ca70807262a64584727b39c71b06bd1eaef9b696
Peter Simons <simons@cryp.to>
2013-04-18 15:29:22 +0200

    haskell-skein: jailbreak to support current version of 'tagged'

--- pkgs/development/libraries/haskell/skein/default.nix
+++ pkgs/development/libraries/haskell/skein/default.nix
@@ -6,6 +6,7 @@ cabal.mkDerivation (self: {
   sha256 = "1mvyq6wp03fs6sjppgalaacargvi69yf5d59k473pgr1g89v4ni1";
   buildDepends = [ cereal cryptoApi tagged ];
   testDepends = [ cereal cryptoApi filepath hspec tagged ];
+  jailbreak = true;
   meta = {
     homepage = "https://github.com/meteficha/skein";
     description = "Skein, a family of cryptographic hash functions. Includes Skein-MAC as well.";
