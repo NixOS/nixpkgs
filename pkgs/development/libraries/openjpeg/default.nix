808a5bd2d83b153bebd61606936b05b957769816
Vladimír Čunát <vcunat@gmail.com>
2013-04-18 20:54:23 +0200

    openjpeg: flatten includes to make it better detectable

--- pkgs/development/libraries/openjpeg/default.nix
+++ pkgs/development/libraries/openjpeg/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, libpng, libtiff, lcms2 }:
+{ stdenv, fetchurl, pkgconfig, libpng, libtiff, lcms2, glib/*passthru only*/ }:
 
 stdenv.mkDerivation rec {
   name = "openjpeg-1.5.1";
@@ -11,7 +11,10 @@ stdenv.mkDerivation rec {
     sha256 = "13dbyf3jwr4h2dn1k11zph3jgx17z7d66xmi640mbsf8l6bk1yvc";
   };
 
-  buildInputs = [ pkgconfig libpng libtiff lcms2 ];
+  nativebuildInputs = [ pkgconfig ];
+  propagatedBuildInputs = [ libpng libtiff lcms2 ]; # in closure anyway
+
+  postInstall = glib.flattenInclude;
 
   meta = {
     homepage = http://www.openjpeg.org/;
