3c3b5b61999ef141103db222b610c345f8637214
Peter Simons <simons@cryp.to>
2013-04-18 10:51:41 +0200

    haskell-statistics: update to version 0.10.3.1

--- pkgs/development/libraries/haskell/statistics/default.nix
+++ pkgs/development/libraries/haskell/statistics/default.nix
@@ -1,13 +1,13 @@
 { cabal, deepseq, erf, HUnit, ieee754, mathFunctions, monadPar
 , mwcRandom, primitive, QuickCheck, testFramework
 , testFrameworkHunit, testFrameworkQuickcheck2, vector
-, vectorAlgorithms, fetchurl
+, vectorAlgorithms
 }:
 
 cabal.mkDerivation (self: {
   pname = "statistics";
-  version = "0.10.3.0";
-  sha256 = "1ay03y9z84mc4ai6i4g81v129rhg4146kad4ggb2gimbj6851fw1";
+  version = "0.10.3.1";
+  sha256 = "12abfqxsriqlncr60wwcsm0q41hmqc6vp9p1hmnv2l3qqcisk60s";
   buildDepends = [
     deepseq erf mathFunctions monadPar mwcRandom primitive vector
     vectorAlgorithms
@@ -18,10 +18,6 @@ cabal.mkDerivation (self: {
     vectorAlgorithms
   ];
   doCheck = false;
-  patches = [
-    (fetchurl { url = "https://github.com/bos/statistics/commit/6b88143c9f931dd4fa95c1f083999ae12dd01b55.patch";
-                sha256 = "0bl10qhp73cijxlpkjfmnkbl49jlj85xxvyhlca6jw7awcka6d2l"; })
-  ];
   meta = {
     homepage = "https://github.com/bos/statistics";
     description = "A library of statistical types, data, and functions";
