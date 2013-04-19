b6c7c4a142357f8fb90eef6b4beee5df749a9e4d
Peter Simons <simons@cryp.to>
2013-04-19 10:47:36 +0200

    haskell-acid-state, haskell-safecopy: cosmetic updates by hackage4nix

--- pkgs/development/libraries/haskell/safecopy/default.nix
+++ pkgs/development/libraries/haskell/safecopy/default.nix
@@ -1,20 +1,14 @@
-{ cabal, cereal, text }:
+{ cabal, cereal, text, time }:
 
 cabal.mkDerivation (self: {
   pname = "safecopy";
   version = "0.8.2";
   sha256 = "0l2kqymsxv244fahxcpxlrspk6xipz3br6j854ipbfh8b0bfvr4m";
-  isLibrary = true;
-  isExecutable = true;
-  buildDepends = [
-    cereal text
-  ];
+  buildDepends = [ cereal text time ];
   meta = {
     homepage = "http://acid-state.seize.it/safecopy";
-    description = "Binary serialization with version control.";
+    description = "Binary serialization with version control";
     license = self.stdenv.lib.licenses.publicDomain;
     platforms = self.ghc.meta.platforms;
-    maintainers = [
-    ];
   };
 })
