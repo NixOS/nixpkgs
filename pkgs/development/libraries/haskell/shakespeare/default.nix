0345b1df709d87bd484068dd3e1fae997aedd44f
Peter Simons <simons@cryp.to>
2013-04-20 19:19:56 +0200

    haskell-shakespeare: patch to fix build with GHC 7.0.4

--- pkgs/development/libraries/haskell/shakespeare/default.nix
+++ pkgs/development/libraries/haskell/shakespeare/default.nix
@@ -1,4 +1,4 @@
-{ cabal, hspec, parsec, text }:
+{ cabal, fetchurl, hspec, parsec, text }:
 
 cabal.mkDerivation (self: {
   pname = "shakespeare";
@@ -6,6 +6,11 @@ cabal.mkDerivation (self: {
   sha256 = "0aqcgfx3y9sbp7wvjmx6rxwi4r13qrfxs9a40gc00np03bpk1hxb";
   buildDepends = [ parsec text ];
   testDepends = [ hspec parsec text ];
+  patchFlags = "-p2";
+  patches = [ (fetchurl { url = "https://github.com/yesodweb/shakespeare/pull/102.patch";
+                          sha256 = "02fp87sw7k8zyn8kgmjg8974gi7pp5fyvb4f84i983qycmlmh8xq";
+                        })
+            ];
   meta = {
     homepage = "http://www.yesodweb.com/book/shakespearean-templates";
     description = "A toolkit for making compile-time interpolated templates";
