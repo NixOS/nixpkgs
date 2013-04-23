7ca16e51151dab8d7a21c81b90f458d617f62d5b
Lluís Batlle i Rossell <viric@viric.name>
2013-04-23 11:05:46 +0400

    hatari: fix pthread_cancel+gcc_s thing, and some spacing

--- pkgs/misc/emulators/hatari/default.nix
+++ pkgs/misc/emulators/hatari/default.nix
@@ -1,15 +1,18 @@
-{stdenv, fetchurl, zlib, SDL, cmake}:
+{ stdenv, fetchurl, zlib, SDL, cmake }:
 
-let
+stdenv.mkDerivation rec {
   name = "hatari-1.6.2";
-in
-stdenv.mkDerivation {
-  inherit name; 
+
   src = fetchurl {
     url = "http://download.tuxfamily.org/hatari/1.6.2/${name}.tar.bz2";
     sha256 = "0gqvfqqd0lg3hi261rwh6gi2b5kmza480kfzx43d4l49xcq09pi0";
   };
-  buildInputs = [zlib SDL cmake];
+
+  # For pthread_cancel
+  cmakeFlags = "-DCMAKE_EXE_LINKER_FLAGS=-lgcc_s";
+
+  buildInputs = [ zlib SDL cmake ];
+
   meta = {
     homepage = "http://hatari.tuxfamily.org/";
     description = "Hatari is an Atari ST/STE/TT/Falcon emulator.";
