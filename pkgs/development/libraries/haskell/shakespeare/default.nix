e21708320c6af3d4acc13e6b0f68c46455b25841
Peter Simons <simons@cryp.to>
2013-04-23 11:35:25 +0200

    haskell-shakespeare: update to version 1.0.5

--- pkgs/development/libraries/haskell/shakespeare/default.nix
+++ pkgs/development/libraries/haskell/shakespeare/default.nix
@@ -1,16 +1,13 @@
-{ cabal, fetchurl, hspec, parsec, text }:
+{ cabal, hspec, parsec, systemFileio, systemFilepath, text, time }:
 
 cabal.mkDerivation (self: {
   pname = "shakespeare";
-  version = "1.0.4";
-  sha256 = "0aqcgfx3y9sbp7wvjmx6rxwi4r13qrfxs9a40gc00np03bpk1hxb";
-  buildDepends = [ parsec text ];
-  testDepends = [ hspec parsec text ];
-  patchFlags = "-p2";
-  patches = [ (fetchurl { url = "https://github.com/yesodweb/shakespeare/pull/102.patch";
-                          sha256 = "02fp87sw7k8zyn8kgmjg8974gi7pp5fyvb4f84i983qycmlmh8xq";
-                        })
-            ];
+  version = "1.0.5";
+  sha256 = "1dc1yg35pxh45fv20fvnlpas0svqi18h6bdalpjaqjb164s114vf";
+  buildDepends = [ parsec systemFileio systemFilepath text time ];
+  testDepends = [
+    hspec parsec systemFileio systemFilepath text time
+  ];
   meta = {
     homepage = "http://www.yesodweb.com/book/shakespearean-templates";
     description = "A toolkit for making compile-time interpolated templates";
