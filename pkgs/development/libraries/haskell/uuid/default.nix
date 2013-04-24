7d383131903cc6d36fd3715e2c6efe7c823de858
Peter Simons <simons@cryp.to>
2013-04-24 11:18:26 +0200

    haskell-uuid: update to version 1.2.11

--- pkgs/development/libraries/haskell/uuid/default.nix
+++ pkgs/development/libraries/haskell/uuid/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "uuid";
-  version = "1.2.10";
-  sha256 = "17njmmh190pg9bpb8iaw2kniyn7z7j70mzdmxr9h29kydl1xmky6";
+  version = "1.2.11";
+  sha256 = "0fr91w8jahb7ryhgl8dm6w8jg9sykn3m1jnfvyrfdpkpwkc0kpxx";
   buildDepends = [ binary cryptohash maccatcher random time ];
   testDepends = [
     criterion deepseq HUnit mersenneRandomPure64 QuickCheck random
