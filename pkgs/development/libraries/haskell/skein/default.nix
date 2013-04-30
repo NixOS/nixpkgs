dc52f9b8a176295f3a16f91af877e68b1c59c375
Peter Simons <simons@cryp.to>
2013-04-30 14:11:00 +0200

    haskell-skein: update to version 1.0.3

--- pkgs/development/libraries/haskell/skein/default.nix
+++ pkgs/development/libraries/haskell/skein/default.nix
@@ -2,11 +2,10 @@
 
 cabal.mkDerivation (self: {
   pname = "skein";
-  version = "1.0.2";
-  sha256 = "1wzy586lcr0pxsdf4lvqhbzq8bsm26gm71jlmv64i22f99n5s9h2";
+  version = "1.0.3";
+  sha256 = "15vzydywhwjdgybabvv6lfk1vjs7blvs3k2apwxjdjh2q7jmgkam";
   buildDepends = [ cereal cryptoApi tagged ];
   testDepends = [ cereal cryptoApi filepath hspec tagged ];
-  jailbreak = true;
   meta = {
     homepage = "https://github.com/meteficha/skein";
     description = "Skein, a family of cryptographic hash functions. Includes Skein-MAC as well.";
