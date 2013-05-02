cb842f97176afadb4ec0adcc6fef4903dc41350d
Peter Simons <simons@cryp.to>
2013-05-02 12:35:22 +0200

    haskell-snap: update to version 0.11.3

--- pkgs/development/libraries/haskell/snap/snap.nix
+++ pkgs/development/libraries/haskell/snap/snap.nix
@@ -1,25 +1,24 @@
 { cabal, aeson, attoparsec, cereal, clientsession, comonad
 , configurator, directoryTree, dlist, errors, filepath, hashable
 , heist, lens, logict, MonadCatchIOTransformers, mtl, mwcRandom
-, pwstoreFast, regexPosix, snapCore, snapServer, stm, syb, tagged
-, text, time, transformers, unorderedContainers, vector
-, vectorAlgorithms, xmlhtml
+, pwstoreFast, regexPosix, snapCore, snapServer, stm, syb, text
+, time, transformers, unorderedContainers, vector, vectorAlgorithms
+, xmlhtml
 }:
 
 cabal.mkDerivation (self: {
   pname = "snap";
-  version = "0.11.2.2";
-  sha256 = "1fwm60rx2ginlphi00kn3ghs9rxdx4br8p4f3prpfmhvjikwgv8n";
+  version = "0.11.3";
+  sha256 = "1v4qx9kfxyx3kc2k4cpr8pjf20mfqmmzi7rzdlaf32frh38hp5ac";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
     aeson attoparsec cereal clientsession comonad configurator
     directoryTree dlist errors filepath hashable heist lens logict
     MonadCatchIOTransformers mtl mwcRandom pwstoreFast regexPosix
-    snapCore snapServer stm syb tagged text time transformers
+    snapCore snapServer stm syb text time transformers
     unorderedContainers vector vectorAlgorithms xmlhtml
   ];
-  jailbreak = true;
   meta = {
     homepage = "http://snapframework.com/";
     description = "Top-level package for the Snap Web Framework";
