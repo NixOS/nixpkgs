986f36194650e2a41451cbfb9f29ce1c66a62df3
Shea Levy <shea@shealevy.com>
2013-05-05 11:30:34 -0400

    linux/bootstrap: Use <nix/fetchurl> to get the static binaries
    Since nix-1.4, nix's corepkgs contain a fetchurl suitable for
    downloading the bootstrap binaries. Doing this will allow us to have a
    nixpkgs with no in-tree binaries without breaking the purity of the
    bootstrap (though for now, they are fetched in-tree until the binaries
    are added to nixos.org somewhere). As an additional small benefit, the
    in-tree binaries do not have to be hashed on every instantiation as they
    do now.
    
    The fetchurl in nix-1.2 and 1.3 isn't able to make binaries executable,
    so it can't be used for this case. In that case, attempting to build the
    bootstrap will show a message asking the user to manually download each
    file and add it to the store with nix-store --add (but the hash is
    ultimately the same, of course).
    
    Signed-off-by: Shea Levy <shea@shealevy.com>

--- pkgs/stdenv/linux/bootstrap/armv5tel/default.nix
+++ pkgs/stdenv/linux/bootstrap/armv5tel/default.nix
@@ -1,10 +1,65 @@
-{
-  sh = ./sh;
-  bzip2 = ./bzip2;
-  mkdir = ./mkdir;
-  cpio = ./cpio;
-  ln = ./ln;
-  curl = ./curl.bz2;
+let fetch = { file, sha256 }:
+    let
+      nixFetchurl = import <nix/fetchurl.nix>;
+      args = {
+        url = "file://${builtins.toString ./.}/${file}";
+        inherit sha256;
+        executable = true;
+      };
+    in if (builtins.functionArgs nixFetchurl) ? executable
+      then nixFetchurl args
+      else derivation {
+        name = file;
+        builder = "/bin/sh";
+
+        system = builtins.currentSystem;
+
+        args = [ "-c" "echo $message; exit 1" ];
+
+        message = ''
+          Sorry, this version of nix cannot download all of the bootstrap tools.
+          Please download ${args.url}, make it executable, add it to the store
+          with `nix-store --add', and try again.
+        '';
+
+        outputHashAlgo = "sha256";
+
+        outputHash = args.sha256;
+
+        outputHashMode = "recursive";
+
+        preferLocalBuild = true;
+      };
+in {
+  sh = fetch {
+    file = "sh";
+    sha256 = "1kv3gc8h209rjc5i0rzzc4pjxq23kzq25iff89in05c9vl20mn4n";
+  };
+
+  bzip2 = fetch {
+    file = "bzip2";
+    sha256 = "090jrj099wl33q0iq092iljilam39pv8acli59m2af0fa9z0d9f0";
+  };
+
+  mkdir = fetch {
+    file = "mkdir";
+    sha256 = "01s8myfvi559dsxvbanxw71vxzjv49k4gi1qh0ak6l0qx0xq602b";
+  };
+
+  cpio = fetch {
+    file = "cpio";
+    sha256 = "07snc8l0mn84w2xrvdmc5yfpmlbrvl2bar8wipbpvm43nzz2qbzs";
+  };
+
+  ln = fetch {
+    file = "ln";
+    sha256 = "0m9fz02bashpfgwfkxmrp4wa8a5r3il52bclvf6z36vsam0vx56d";
+  };
+
+  curl = fetch {
+    file = "curl.bz2";
+    sha256 = "19yqdjqi31zlnqn8ss2ml60iq2a1vrwfw6dmvjqp6qbxmh7yb8n8";
+  };
 
   bootstrapTools = {
     url = "http://nixos.org/tarballs/stdenv-linux/armv5tel/r18744/bootstrap-tools.cpio.bz2";
--- pkgs/stdenv/linux/bootstrap/powerpc/default.nix
+++ pkgs/stdenv/linux/bootstrap/powerpc/default.nix
@@ -1,9 +1,61 @@
-{
-  bash = ./bash;
-  bzip2 = ./bzip2;
-  cp = ./cp;
-  curl = ./curl.bz2;
-  tar = ./tar.bz2;
+let
+  fetch = { file, sha256 }:
+    let
+      nixFetchurl = import <nix/fetchurl.nix>;
+      args = {
+        url = "file://${builtins.toString ./.}/${file}";
+        inherit sha256;
+        executable = true;
+      };
+    in if (builtins.functionArgs nixFetchurl) ? executable
+      then nixFetchurl args
+      else derivation {
+        name = file;
+        builder = "/bin/sh";
+
+        system = builtins.currentSystem;
+
+        args = [ "-c" "echo $message; exit 1" ];
+
+        message = ''
+          Sorry, this version of nix cannot download all of the bootstrap tools.
+          Please download ${args.url}, make it executable, add it to the store
+          with `nix-store --add', and try again.
+        '';
+
+        outputHashAlgo = "sha256";
+
+        outputHash = args.sha256;
+
+        outputHashMode = "recursive";
+
+        preferLocalBuild = true;
+      };
+in {
+  bash = fetch {
+    file = "bash";
+    sha256 = "0zss8im6hbx6z2i2wxn1554kd7ggdqdli4xk39cy5fchlnz9bqpp";
+  };
+
+  bzip2 = fetch {
+    file = "bzip2";
+    sha256 = "01ylj8x7albv6k9sqx2h1prsazh4d8y22nga0pwai2bnns0q9qdg";
+  };
+
+  cp = fetch {
+    file = "cp";
+    sha256 = "0d7xbzrv22bxgw7w9b03rakirna5zfvr9gzwm7ichd2fh634hvgl";
+  };
+
+  curl = fetch {
+    file = "curl.bz2";
+    sha256 = "17c25dfslw3qkjlcmihpbhn3x4kj9pgkslizv89ggnki7iiy4jgh";
+  };
+
+  tar = fetch {
+    file = "tar.bz2";
+    sha256 = "132ylqwz02hw5njqx7wvj4sxpcrllx8b8b3a00rlv6iad671ayyr";
+  };
 
   staticToolsURL = {
     url = http://nixos.org/tarballs/stdenv-linux/powerpc/r9828/static-tools.tar.bz2;
