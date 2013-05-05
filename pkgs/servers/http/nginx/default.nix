e39ddf321c0966fa6a740c2fddef87eef97a13ca
Shea Levy <shea@shealevy.com>
2013-05-05 15:48:07 -0400

    nginx: Don't fetch the full WebDAV extension unless needed
    Signed-off-by: Shea Levy <shea@shealevy.com>

--- pkgs/servers/http/nginx/default.nix
+++ pkgs/servers/http/nginx/default.nix
@@ -1,17 +1,19 @@
 { stdenv, fetchurl, fetchgit, openssl, zlib, pcre, libxml2, libxslt, expat, fullWebDAV ? false }:
 
+let
+  dav-ext = fetchgit {
+    url = git://github.com/arut/nginx-dav-ext-module.git;
+    rev = "54cebc1f21fc13391aae692c6cce672fa7986f9d";
+    sha256 = "1dvpq1fg5rslnl05z8jc39sgnvh3akam9qxfl033akpczq1bh8nq";
+  };
+in
+
 stdenv.mkDerivation rec {
   name = "nginx-1.2.4";
 
   src = fetchurl {
     url = "http://nginx.org/download/${name}.tar.gz";
     sha256 = "0hvcv4lgfcrsl40azkd3rxhf73l05jzzgflclpkdvjd95xgw51y5";
-  };
-
-  dav-ext = fetchgit {
-    url = git://github.com/arut/nginx-dav-ext-module.git;
-    rev = "54cebc1f21fc13391aae692c6cce672fa7986f9d";
-    sha256 = "1dvpq1fg5rslnl05z8jc39sgnvh3akam9qxfl033akpczq1bh8nq";
   };
 
   buildInputs = [ openssl zlib pcre libxml2 libxslt ] ++ stdenv.lib.optional fullWebDAV expat;
