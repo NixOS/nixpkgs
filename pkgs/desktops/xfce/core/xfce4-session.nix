80e4a647406433c4f737f18c9cdaa9656288fe63
Vladimír Čunát <vcunat@gmail.com>
2013-05-08 10:58:03 +0200

    xfce4session: bugfix update, superseding the patches

--- pkgs/desktops/xfce/core/xfce4-session.nix
+++ pkgs/desktops/xfce/core/xfce4-session.nix
@@ -1,34 +1,26 @@
 { stdenv, fetchurl, pkgconfig, intltool, gtk, libxfce4util, libxfce4ui, xfce4panel
 , libwnck, dbus_glib, xfconf, libglade, xorg
-, polkit, xfce4_dev_tools }:
+, polkit }:
 
 #TODO: gnome stuff: gconf (assistive?), keyring
 
 stdenv.mkDerivation rec {
   p_name  = "xfce4-session";
   ver_maj = "4.10";
-  ver_min = "0";
+  ver_min = "1";
 
   src = fetchurl {
     url = "mirror://xfce/src/xfce/${p_name}/${ver_maj}/${name}.tar.bz2";
-    sha256 = "1kj65jkjhd0ysf0yxsf88wzpyv6n8i8qgd3gb502hf1x9jksk2mv";
+    sha256 = "10zwki7v55a325abr57wczcb5g7ml99cqk1p8ls8qycqqfyzlm01";
   };
   name = "${p_name}-${ver_maj}.${ver_min}";
-
-  patch_crash = fetchurl {
-    url = "http://git.xfce.org/xfce/xfce4-session/patch/?id=ab391138cacc62ab184a338e237c4430356b41f9";
-    sha256 = "1kydj52hm883rdanpcqzf5qphj0ws2v28g8fim8jv2pm72d33day";
-  };
-
-  patches = [ ./xfce4-session-systemd.patch patch_crash ];
 
   buildInputs =
     [ pkgconfig intltool gtk libxfce4util libxfce4ui libwnck dbus_glib
       xfconf xfce4panel libglade xorg.iceauth
-      polkit xfce4_dev_tools
+      polkit
     ];
 
-  preConfigure = "xdt-autogen";
   configureFlags = [ "--with-xsession-prefix=$(out)" ];
 
   preFixup = "rm $out/share/icons/hicolor/icon-theme.cache";
