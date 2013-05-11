f3c909143a6953c26a2aa653ee01e7374b53248b
Peter Simons <simons@cryp.to>
2013-05-11 15:17:47 +0200

    haskell-accelerate-io: update to version 0.13.0.0

--- pkgs/development/libraries/haskell/accelerate-io/default.nix
+++ pkgs/development/libraries/haskell/accelerate-io/default.nix
@@ -1,12 +1,12 @@
-{ cabal, accelerate, repa, vector }:
+{ cabal, accelerate, bmp, repa, vector }:
 
 cabal.mkDerivation (self: {
   pname = "accelerate-io";
-  version = "0.12.1.0";
-  sha256 = "1hcyshzfh7ldswv7sjklxlw5h1hx7spx6dy23bvdryrkq929gb8a";
-  buildDepends = [ accelerate repa vector ];
+  version = "0.13.0.0";
+  sha256 = "08mlmb7ipdyh4nzv6dsfszack5glm0ihr34capa0ilq4c7cvwlv5";
+  buildDepends = [ accelerate bmp repa vector ];
   meta = {
-    homepage = "http://www.cse.unsw.edu.au/~chak/project/accelerate/";
+    homepage = "https://github.com/AccelerateHS/accelerate-io";
     description = "Read and write Accelerate arrays in various formats";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
