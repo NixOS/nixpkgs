0d175b7c60558869e8b4d4256682fa4c8e849c54
Peter Simons <simons@cryp.to>
2013-05-11 15:47:34 +0200

    haskell-accelerate: disable haddock phase to avoid build error
        Running Haddock for accelerate-0.13.0.0...
        Warning: The documentation for the following packages are not installed. No
        links will be generated to these packages: rts-1.0
        Preprocessing library accelerate-0.13.0.0...
        Setup: dist/doc/html/accelerate/haddock-prolog16263.txt: invalid argument

--- pkgs/development/libraries/haskell/accelerate/default.nix
+++ pkgs/development/libraries/haskell/accelerate/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.13.0.0";
   sha256 = "07ph5brvxwi8k5calqmgiyafll2w88679rnpcv6bk5z57bsb5jli";
   buildDepends = [ fclabels hashable hashtables ];
+  noHaddock = true;
   meta = {
     homepage = "https://github.com/AccelerateHS/accelerate/";
     description = "An embedded language for accelerated array processing";
