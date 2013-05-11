e7bf966cd8680aa10a881bca3345c4a37e9a2e4a
Peter Simons <simons@cryp.to>
2013-05-11 15:17:47 +0200

    haskell-accelerate-cuda: update to version 0.13.0.0

--- pkgs/development/libraries/haskell/accelerate-cuda/default.nix
+++ pkgs/development/libraries/haskell/accelerate-cuda/default.nix
@@ -1,19 +1,19 @@
 { cabal, accelerate, binary, cryptohash, cuda, fclabels, filepath
-, hashable, hashtables, languageCQuote, mainlandPretty, mtl, srcloc
-, text, transformers, unorderedContainers
+, hashable, hashtables, languageCQuote, mainlandPretty, mtl
+, SafeSemaphore, srcloc, text, transformers, unorderedContainers
 }:
 
 cabal.mkDerivation (self: {
   pname = "accelerate-cuda";
-  version = "0.12.1.2";
-  sha256 = "184rxdp9idvhrqa0k3fqcm3nrgjcs3f53dz7wrhhpfa3iqrr6vd4";
+  version = "0.13.0.0";
+  sha256 = "1wlvnbafdbjs8s1ds1zip2wqag96y1nj03zhr0sikgccyszfadf1";
   buildDepends = [
     accelerate binary cryptohash cuda fclabels filepath hashable
-    hashtables languageCQuote mainlandPretty mtl srcloc text
-    transformers unorderedContainers
+    hashtables languageCQuote mainlandPretty mtl SafeSemaphore srcloc
+    text transformers unorderedContainers
   ];
   meta = {
-    homepage = "http://www.cse.unsw.edu.au/~chak/project/accelerate/";
+    homepage = "https://github.com/AccelerateHS/accelerate-cuda/";
     description = "Accelerate backend for NVIDIA GPUs";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.stdenv.lib.platforms.none;
