aec64e12d3a11debd96983898b9c2d77e983c14a
Peter Simons <simons@cryp.to>
2013-05-12 15:43:06 +0200

    haskell-ChasingBottoms: jailbreak to fix build with recent versions of QuickCheck

--- pkgs/development/libraries/haskell/ChasingBottoms/default.nix
+++ pkgs/development/libraries/haskell/ChasingBottoms/default.nix
@@ -7,6 +7,7 @@ cabal.mkDerivation (self: {
   isLibrary = true;
   isExecutable = true;
   buildDepends = [ mtl QuickCheck random syb ];
+  jailbreak = true;
   meta = {
     description = "For testing partial and infinite values";
     license = "unknown";
