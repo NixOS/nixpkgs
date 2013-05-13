82759ee4c08eec084effbdd12b6f95f7cb71d655
Peter Simons <simons@cryp.to>
2013-05-13 11:21:36 +0200

    haskell-accelerate-io: update to version 0.13.0.1

--- pkgs/development/libraries/haskell/accelerate-io/default.nix
+++ pkgs/development/libraries/haskell/accelerate-io/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "accelerate-io";
-  version = "0.13.0.0";
-  sha256 = "08mlmb7ipdyh4nzv6dsfszack5glm0ihr34capa0ilq4c7cvwlv5";
+  version = "0.13.0.1";
+  sha256 = "0wjprbhcddnjqbhmpxiwq73hazdnhafhjj7mpvpxhs9pz1dbv89h";
   buildDepends = [ accelerate bmp repa vector ];
   meta = {
     homepage = "https://github.com/AccelerateHS/accelerate-io";
