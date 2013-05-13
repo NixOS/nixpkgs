6b33985fc7e4cef746e8f1f749505fdbeb970cb6
Peter Simons <simons@cryp.to>
2013-05-13 11:21:37 +0200

    haskell-unix-time: update to version 0.1.8

--- pkgs/development/libraries/haskell/unix-time/default.nix
+++ pkgs/development/libraries/haskell/unix-time/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "unix-time";
-  version = "0.1.6";
-  sha256 = "0l8k42n67qwc1ljxw2ksmdnj630q1ql0im0j1z7yv9kak9pmqfy6";
+  version = "0.1.8";
+  sha256 = "1s9r2i4hl7nm1f4zya03q0a2ayliby51caziz1w79schhplfa5sv";
   testDepends = [ doctest hspec QuickCheck time ];
   meta = {
     description = "Unix time parser/formatter and utilities";
