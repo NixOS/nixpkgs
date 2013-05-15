027712669973008d7330d03a2629483643ba2535
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-05-15 12:47:17 +0200

    Return exit code 127 if a command is not found

--- modules/programs/bash/command-not-found.nix
+++ modules/programs/bash/command-not-found.nix
@@ -34,9 +34,12 @@ in
           # Retry the command if we just installed it.
           if [ $? = 126 ]; then
             "$@"
+          else
+            return 127
           fi
         else
           echo "$1: command not found" >&2
+          return 127
         fi
       }
     '';
