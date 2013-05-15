501833cf5539133841ac101ff16d373ceb2b0d4a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-05-15 14:52:02 +0200

    linux: Re-enable the memory resource controller
    We accidentally lost this in Linux >= 3.5 because the kernel options
    were renamed.  (Probably an argument for not using "?" in the kernel
    config...)

--- pkgs/os-specific/linux/kernel/linux-3.9.nix
+++ pkgs/os-specific/linux/kernel/linux-3.9.nix
@@ -215,8 +215,8 @@ let
       # Linux Containers
       RT_GROUP_SCHED? y
       CGROUP_DEVICE? y
-      CGROUP_MEM_RES_CTLR? y
-      CGROUP_MEM_RES_CTLR_SWAP? y
+      MEMCG? y
+      MEMCG_SWAP? y
       DEVPTS_MULTIPLE_INSTANCES? y
 
       # Enable staging drivers.  These are somewhat experimental, but
