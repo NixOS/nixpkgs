77724cdd5fffc2402e6d2ac939a774fc44ded954
Peter Simons <simons@cryp.to>
2013-05-16 13:02:38 +0200

    haskell-uu-parsinglib: update to version 2.8.1

--- pkgs/development/libraries/haskell/uu-parsinglib/default.nix
+++ pkgs/development/libraries/haskell/uu-parsinglib/default.nix
@@ -1,10 +1,10 @@
-{ cabal, ListLike, time }:
+{ cabal, ListLike, time, uuInterleaved }:
 
 cabal.mkDerivation (self: {
   pname = "uu-parsinglib";
-  version = "2.7.4.3";
-  sha256 = "0q907gfxia7i63wc1p0zfmp102f5xpanhrq5pa259mp1nwqq6lyg";
-  buildDepends = [ ListLike time ];
+  version = "2.8.1";
+  sha256 = "10phjwm8dm45rms2vfpx9vzm56v7b9wp0308xkfbalh5zbvxmv35";
+  buildDepends = [ ListLike time uuInterleaved ];
   meta = {
     homepage = "http://www.cs.uu.nl/wiki/bin/view/HUT/ParserCombinators";
     description = "Fast, online, error-correcting, monadic, applicative, merging, permuting, idiomatic parser combinators";
