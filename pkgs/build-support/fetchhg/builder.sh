83e45309fda47d08b7e9ea77d59e9b3b3339005a
Jack Cummings <jack@mudshark.org>
2013-05-17 09:46:27 -0700

    fetchhg: allow untrusted certs (merge #540)
    It's for fixed-output derivations, security provided by output hash.

--- pkgs/build-support/fetchhg/builder.sh
+++ pkgs/build-support/fetchhg/builder.sh
@@ -2,7 +2,7 @@ source $stdenv/setup
 
 header "getting $url${tag:+ ($tag)} into $out"
 
-hg clone ${tag:+-r "$tag"} "$url" "$out"
+hg clone --insecure ${tag:+-r "$tag"} "$url" "$out"
 
 rm -rf "$out/.hg"
 
