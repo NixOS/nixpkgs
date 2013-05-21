6899c57dbef7ed5c5e7f07fc7d2460eb602dd207
Peter Simons <simons@cryp.to>
2013-05-21 10:58:34 +0200

    haskell-fgl-visualize: update to version 0.1.0.1

--- pkgs/development/libraries/haskell/fgl-visualize/default.nix
+++ pkgs/development/libraries/haskell/fgl-visualize/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "fgl-visualize";
-  version = "0.1";
-  sha256 = "0ri6ywg7rj8qfyngjxvihw43s2h2l3w03fwq1ipn59cdcnah08bc";
+  version = "0.1.0.1";
+  sha256 = "0vwafx0rggksg5i7cx4r2bs5wa6csb5p39vpix425zr3l6vggrxq";
   buildDepends = [ dotgen fgl ];
   meta = {
     description = "Convert FGL graphs to dot (graphviz) files";
