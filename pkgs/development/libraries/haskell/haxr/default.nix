b23d134019082c3d0750887240a6c1ec3804c9ae
Peter Simons <simons@cryp.to>
2013-05-21 10:58:34 +0200

    haskell-haxr: update to version 3000.9.3

--- pkgs/development/libraries/haskell/haxr/default.nix
+++ pkgs/development/libraries/haskell/haxr/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "haxr";
-  version = "3000.9.2.1";
-  sha256 = "065kkqa8wjh5qncjx7x83nxvmk338g8xzl4rjbczga6l3dpy0f24";
+  version = "3000.9.3";
+  sha256 = "1jg7abgbykxjjpmakmfm6zcwxn0hf9q53430ibr4m9n6alh7nglq";
   buildDepends = [
     base64Bytestring blazeBuilder HaXml HTTP mtl network time
     utf8String
