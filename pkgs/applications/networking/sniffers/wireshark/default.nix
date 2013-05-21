af6503b2af316fc00125de5e15a35996ed3eeb83
Bjørn Forsman <bjorn.forsman@gmail.com>
2013-05-21 22:48:50 +0200

    wireshark: add desktop item
    And fix a small "ethereal" typo.

--- pkgs/applications/networking/sniffers/wireshark/default.nix
+++ pkgs/applications/networking/sniffers/wireshark/default.nix
@@ -1,6 +1,7 @@
 { stdenv, fetchurl, perl, pkgconfig, gtk, libpcap, flex, bison
 , gnutls, libgcrypt, glib, zlib, libxml2, libxslt, adns, geoip
 , heimdal, python, lynx, lua5
+, makeDesktopItem
 }:
 
 let version = "1.8.6"; in
@@ -20,13 +21,30 @@ stdenv.mkDerivation {
 
   configureFlags = "--disable-usr-local --with-ssl --enable-threads --enable-packet-editor";
 
+  desktopItem = makeDesktopItem {
+    name = "Wireshark";
+    exec = "wireshark";
+    icon = "wireshark";
+    comment = "Powerful network protocol analysis suite";
+    desktopName = "Wireshark";
+    genericName = "Network packet analyzer";
+    categories = "Network;System";
+  };
+
+  postInstall = ''
+    mkdir -p "$out"/share/applications/
+    mkdir -p "$out"/share/icons/
+    cp "$desktopItem"/share/applications/* "$out"/share/applications/
+    cp image/wsicon.svg "$out"/share/icons/wireshark.svg
+  '';
+
   meta = {
     homepage = http://www.wireshark.org/;
     description = "a powerful network protocol analyzer";
     license = stdenv.lib.licenses.gpl2;
 
     longDescription = ''
-      Wireshark (formerly known as "Etherreal") is a powerful network
+      Wireshark (formerly known as "Ethereal") is a powerful network
       protocol analyzer developed by an international team of networking
       experts. It runs on UNIX, OS X and Windows.
     '';
