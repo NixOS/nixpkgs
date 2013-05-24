7e7b669b3e499ee62967d54af8fd69982a0243a0
Lluís Batlle i Rossell <viric@viric.name>
2013-05-25 00:08:42 +0200

    megatools: fixing the network part
    It requires some env vars to work

--- pkgs/tools/networking/megatools/default.nix
+++ pkgs/tools/networking/megatools/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, fetchurl, glib, curl, pkgconfig, fuse }:
+{ stdenv, fetchurl, glib, curl, pkgconfig, fuse, glib_networking, makeWrapper
+, gsettings_desktop_schemas }:
 
 stdenv.mkDerivation rec {
   name = "megatools-1.9.91";
@@ -8,7 +9,16 @@ stdenv.mkDerivation rec {
     sha256 = "0hb83wqsn6mggcmk871hl8cski5x0hxz9dhaka42115s4mdfbl1i";
   };
 
-  buildInputs = [ glib curl pkgconfig fuse ];
+  buildInputs = [ glib curl pkgconfig fuse makeWrapper ];
+
+  postInstall = ''
+    for a in $out/bin/*; do
+      wrapProgram "$a" \
+            --prefix GIO_EXTRA_MODULES : "${glib_networking}/lib/gio/modules" \
+            --prefix XDG_DATA_DIRS : "${gsettings_desktop_schemas}/share"
+
+    done
+  '';
 
   meta = {
     description = "Command line client for Mega.co.nz";
