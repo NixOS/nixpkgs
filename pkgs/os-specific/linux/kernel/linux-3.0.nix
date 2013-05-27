39e0eb94204f3c05edca88878028855980930333
Mathijs Kwik <mathijs@bluescreen303.nl>
2013-05-27 07:43:50 +0200

    linux-3.0: upgrade to 3.0.80

--- pkgs/os-specific/linux/kernel/linux-3.0.nix
+++ pkgs/os-specific/linux/kernel/linux-3.0.nix
@@ -231,7 +231,7 @@ in
 import ./generic.nix (
 
   rec {
-    version = "3.0.78";
+    version = "3.0.80";
 
     preConfigure = ''
       substituteInPlace scripts/depmod.sh --replace '-b "$INSTALL_MOD_PATH"' ""
@@ -239,7 +239,7 @@ import ./generic.nix (
 
     src = fetchurl {
       url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-      sha256 = "0jgarhjllxygxsj2kph61pf9jvlab4pnp7n4kb9l0lfj3y0crx04";
+      sha256 = "0f3md117bh8n5izkhjd2jp096jqmwz6wpxn7rf8x2x9cz4jz0cqx";
     };
 
     config = configWithPlatform stdenv.platform;
