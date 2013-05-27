e7b61ea77de5960f0c3969f86776ddfaf308fccc
Vladimír Čunát <vcunat@gmail.com>
2013-05-27 12:24:04 +0200

    xorg.xf86videointel: fix bad hash (from #556)

--- pkgs/servers/x11/xorg/default.nix
+++ pkgs/servers/x11/xorg/default.nix
@@ -1514,7 +1514,7 @@ let
     builder = ./builder.sh;
     src = fetchurl {
       url = mirror://xorg/individual/driver/xf86-video-intel-2.21.7.tar.bz2;
-      sha256 = "0mpdd7am813gv7nxxvbllcph1xg7czy0bd1kn3q6bc489h53wzbp";
+      sha256 = "1nb3hc1h6scja89rwf2fvadj5z8ckcnm94lryf895i3r02jbpsps";
     };
     buildInputs = [pkgconfig dri2proto fontsproto libdrm udev libpciaccess randrproto renderproto libX11 xcbutil libxcb libXext xextproto xf86driproto libXfixes xorgserver xproto libXrender libXvMC ];
   })) // {inherit dri2proto fontsproto libdrm udev libpciaccess randrproto renderproto libX11 xcbutil libxcb libXext xextproto xf86driproto libXfixes xorgserver xproto libXrender libXvMC ;};
