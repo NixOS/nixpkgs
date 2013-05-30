496c7cfa6243b0d0f75efcae4f0354ed9e594315
Peter Simons <simons@cryp.to>
2013-05-30 11:01:49 +0200

    haskell-xdot: update to version 0.2.3.1

--- pkgs/development/libraries/haskell/xdot/default.nix
+++ pkgs/development/libraries/haskell/xdot/default.nix
@@ -2,10 +2,9 @@
 
 cabal.mkDerivation (self: {
   pname = "xdot";
-  version = "0.2.3";
-  sha256 = "0xb8igsqydiw1w00frn4mxkflhxkayif2vivxmq5fk53am2f43wy";
+  version = "0.2.3.1";
+  sha256 = "1gricrnssxgzaq1z7nnyppmz284nix0m89477x22mal125pkcf7n";
   buildDepends = [ cairo graphviz gtk mtl polyparse text ];
-  jailbreak = true;
   meta = {
     description = "Parse Graphviz xdot files and interactively view them using GTK and Cairo";
     license = self.stdenv.lib.licenses.bsd3;
