982c74a555653b4ee46ac28894ec48463de318c7
Peter Simons <simons@cryp.to>
2013-05-30 11:01:50 +0200

    haskell-yesod: update to version 1.2.0.1

--- pkgs/development/libraries/haskell/yesod/default.nix
+++ pkgs/development/libraries/haskell/yesod/default.nix
@@ -1,31 +1,18 @@
-{ cabal, aeson, attoparsec, base64Bytestring, blazeBuilder
-, blazeHtml, blazeMarkup, Cabal, conduit, fileEmbed, filepath
-, fsnotify, ghcPaths, hamlet, httpConduit, httpReverseProxy
-, httpTypes, liftedBase, monadControl, network, networkConduit
-, optparseApplicative, parsec, projectTemplate, resourcet
-, shakespeare, shakespeareCss, shakespeareJs, shakespeareText
-, split, systemFileio, systemFilepath, tar, text, time
-, transformers, unixCompat, unorderedContainers, wai, waiExtra
-, warp, yaml, yesodAuth, yesodCore, yesodDefault, yesodForm
-, yesodJson, yesodPersistent, zlib
+{ cabal, aeson, blazeHtml, blazeMarkup, dataDefault, hamlet
+, monadControl, networkConduit, safe, shakespeareCss, shakespeareJs
+, text, transformers, unorderedContainers, wai, waiExtra, warp
+, yaml, yesodAuth, yesodCore, yesodForm, yesodPersistent
 }:
 
 cabal.mkDerivation (self: {
   pname = "yesod";
-  version = "1.1.9.3";
-  sha256 = "16w96lyd0z9slhzggbvmb23yfr3jkwywpmp15kbilwn7ghjlkm41";
-  isLibrary = true;
-  isExecutable = true;
+  version = "1.2.0.1";
+  sha256 = "1whkw0lmkyja2j6vbfcf5rjmmhmc85r4arjwjrvdmz6jkjyqham3";
   buildDepends = [
-    aeson attoparsec base64Bytestring blazeBuilder blazeHtml
-    blazeMarkup Cabal conduit fileEmbed filepath fsnotify ghcPaths
-    hamlet httpConduit httpReverseProxy httpTypes liftedBase
-    monadControl network networkConduit optparseApplicative parsec
-    projectTemplate resourcet shakespeare shakespeareCss shakespeareJs
-    shakespeareText split systemFileio systemFilepath tar text time
-    transformers unixCompat unorderedContainers wai waiExtra warp yaml
-    yesodAuth yesodCore yesodDefault yesodForm yesodJson
-    yesodPersistent zlib
+    aeson blazeHtml blazeMarkup dataDefault hamlet monadControl
+    networkConduit safe shakespeareCss shakespeareJs text transformers
+    unorderedContainers wai waiExtra warp yaml yesodAuth yesodCore
+    yesodForm yesodPersistent
   ];
   meta = {
     homepage = "http://www.yesodweb.com/";
