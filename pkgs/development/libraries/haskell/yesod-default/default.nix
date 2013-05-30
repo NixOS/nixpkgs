6dbbd1822b86158f7b0fdbd1a79082e9bb0405d3
Peter Simons <simons@cryp.to>
2013-05-30 11:01:49 +0200

    haskell-yesod-default: update to version 1.2.0

--- pkgs/development/libraries/haskell/yesod-default/default.nix
+++ pkgs/development/libraries/haskell/yesod-default/default.nix
@@ -1,20 +1,13 @@
-{ cabal, dataDefault, hamlet, networkConduit, safe, shakespeareCss
-, shakespeareJs, text, transformers, unorderedContainers, wai
-, waiExtra, warp, yaml, yesodCore
-}:
+{ cabal, yesodCore }:
 
 cabal.mkDerivation (self: {
   pname = "yesod-default";
-  version = "1.1.3.2";
-  sha256 = "07gm9sj4c231wdmfmb7df1s8wvqa6fw7nhcq554h16h2ibv5pcqg";
-  buildDepends = [
-    dataDefault hamlet networkConduit safe shakespeareCss shakespeareJs
-    text transformers unorderedContainers wai waiExtra warp yaml
-    yesodCore
-  ];
+  version = "1.2.0";
+  sha256 = "15nsknnxnfbkpg4pswxcpgfb2y0hz0xxj56jknd93hcm7aay36pk";
+  buildDepends = [ yesodCore ];
   meta = {
     homepage = "http://www.yesodweb.com/";
-    description = "Default config and main functions for your yesod application";
+    description = "Default config and main functions for your yesod application (deprecated)";
     license = self.stdenv.lib.licenses.mit;
     platforms = self.ghc.meta.platforms;
     maintainers = [ self.stdenv.lib.maintainers.andres ];
