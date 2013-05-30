41f9d7d83cce13c181aca03dd3e08b3116f3a801
Peter Simons <simons@cryp.to>
2013-05-30 12:17:20 +0200

    benchpress: jailbreak to fix build with Haskell Platform 2013.2.0.0

--- pkgs/development/libraries/haskell/benchpress/default.nix
+++ pkgs/development/libraries/haskell/benchpress/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.2.2.6";
   sha256 = "19ygaf2g4yqkfbc6bw6fmf9jsymbj1iallzvl0zw3vjx860rchfg";
   buildDepends = [ mtl time ];
+  jailbreak = true;
   meta = {
     homepage = "http://github.com/tibbe/benchpress";
     description = "Micro-benchmarking with detailed statistics";
