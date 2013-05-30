e539c38a21902c6245763eabe1af137abc4e9590
Peter Simons <simons@cryp.to>
2013-05-30 11:01:48 +0200

    haskell-persistent-postgresql: update to version 1.2.0

--- pkgs/development/libraries/haskell/persistent-postgresql/default.nix
+++ pkgs/development/libraries/haskell/persistent-postgresql/default.nix
@@ -1,16 +1,15 @@
-{ cabal, aeson, conduit, monadControl, persistent, postgresqlLibpq
-, postgresqlSimple, text, time, transformers
+{ cabal, aeson, blazeBuilder, conduit, monadControl, persistent
+, postgresqlLibpq, postgresqlSimple, text, time, transformers
 }:
 
 cabal.mkDerivation (self: {
   pname = "persistent-postgresql";
-  version = "1.1.3.1";
-  sha256 = "1lbr4x45hzk73439x6is9zw6y3mqy1ivmblncvg70kzw0kmjhpnk";
+  version = "1.2.0";
+  sha256 = "0i5ha1vs91makwrw4qqwdzp92i24ynmmsmjyqpik6q80fxxiwps6";
   buildDepends = [
-    aeson conduit monadControl persistent postgresqlLibpq
+    aeson blazeBuilder conduit monadControl persistent postgresqlLibpq
     postgresqlSimple text time transformers
   ];
-  jailbreak = true;
   meta = {
     homepage = "http://www.yesodweb.com/book/persistent";
     description = "Backend for the persistent library using postgresql";
