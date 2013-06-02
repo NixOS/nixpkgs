71f3c07548d7d08e44906d4090dc3cf05aad8487
Peter Simons <simons@cryp.to>
2013-06-02 18:07:02 +0200

    haskell-hledger-lib: update to version 0.21

--- pkgs/development/libraries/haskell/hledger-lib/default.nix
+++ pkgs/development/libraries/haskell/hledger-lib/default.nix
@@ -1,14 +1,20 @@
 { cabal, cmdargs, csv, filepath, HUnit, mtl, parsec, prettyShow
-, regexCompat, regexpr, safe, split, time, transformers, utf8String
+, regexCompat, regexpr, safe, split, testFramework
+, testFrameworkHunit, time, transformers, utf8String
 }:
 
 cabal.mkDerivation (self: {
   pname = "hledger-lib";
-  version = "0.20.0.1";
-  sha256 = "1skm2jii7d8b6a6i98xwxckxwhikgj44823wqzdk8f3bq9zlwhcg";
+  version = "0.21";
+  sha256 = "1j57f05ja8v2igykij3vnvl4z89cppnnbf9bz1i3m7c61xapawg7";
   buildDepends = [
     cmdargs csv filepath HUnit mtl parsec prettyShow regexCompat
     regexpr safe split time transformers utf8String
+  ];
+  testDepends = [
+    cmdargs csv filepath HUnit mtl parsec prettyShow regexCompat
+    regexpr safe split testFramework testFrameworkHunit time
+    transformers
   ];
   meta = {
     homepage = "http://hledger.org";
