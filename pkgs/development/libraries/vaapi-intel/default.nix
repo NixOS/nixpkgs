7717bc1573a021cd38250297ad27540bddec22c3
Mathijs Kwik <mathijs@bluescreen303.nl>
2013-06-03 06:01:12 +0200

    vaapi-intel: fix compilation

--- pkgs/development/libraries/vaapi-intel/default.nix
+++ pkgs/development/libraries/vaapi-intel/default.nix
@@ -3,16 +3,15 @@
 
 stdenv.mkDerivation rec {
   name = "libva-intel-driver-1.0.20";
-  
+
   src = fetchurl {
     url = "http://www.freedesktop.org/software/vaapi/releases/libva-intel-driver/${name}.tar.bz2";
     sha256 = "1jfl8909j3a3in6m8b5bx3dn7pzr8a1sw3sk4vzm7h3j2dkgpzhj";
   };
 
-  buildInputs = [ autoconf automake libtool mesa libva pkgconfig libdrm libX11 intelgen4asm ];
+  buildInputs = [ pkgconfig libdrm libva libX11 ];
 
   preConfigure = ''
-    sh autogen.sh
     sed -i -e "s,LIBVA_DRIVERS_PATH=.*,LIBVA_DRIVERS_PATH=$out/lib/dri," configure
   '';
 
