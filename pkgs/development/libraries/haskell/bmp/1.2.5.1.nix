6cacb34fec15df3b1a98061b12528e48df0b940b
Peter Simons <simons@cryp.to>
2013-06-05 11:32:11 +0200

    haskell-bmp: jailbreak version 1.2.5.1 to support binary 0.7.x

--- pkgs/development/libraries/haskell/bmp/1.2.5.1.nix
+++ pkgs/development/libraries/haskell/bmp/1.2.5.1.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "1.2.5.1";
   sha256 = "1q1g5p7kan9hqb4s50fz989c4p8vmfrs7qvwiqx9bcic8k7jqld4";
   buildDepends = [ binary ];
+  jailbreak = true;
   meta = {
     homepage = "http://code.ouroborus.net/bmp";
     description = "Read and write uncompressed BMP image files";
