f724a5314ba8e9059bd5bf17a8f63e2abeafb5ff
Vladimír Čunát <vcunat@gmail.com>
2013-06-11 14:17:56 +0200

    flac: update, enable tests
    Fix includes in VLC.
    Tried a few other programs using it, seems fine.

--- pkgs/applications/video/vlc/default.nix
+++ pkgs/applications/video/vlc/default.nix
@@ -10,12 +10,17 @@
 
 stdenv.mkDerivation rec {
   name = "vlc-${version}";
-  version = "2.0.6";
+  version = "2.0.7";
 
   src = fetchurl {
     url = "http://download.videolan.org/pub/videolan/vlc/${version}/${name}.tar.xz";
-    sha256 = "0qqrpry41vawihhggcx00vibbn73hxdal1gim1qnrqrcbq1rik1i";
+    sha256 = "052kfkpd0r2fwkyz97qhz2a368xqxa905qacrd1bkl2bkvahfc94";
   };
+
+  postPatch = /* flac 1.3.0 fix */ ''
+    sed -i -e 's:stream_decoder.h:FLAC/stream_decoder.h:' modules/codec/flac.c
+    sed -i -e 's:stream_encoder.h:FLAC/stream_encoder.h:' modules/codec/flac.c
+  '';
 
   buildInputs =
     [ xz bzip2 perl zlib a52dec libmad faad2 ffmpeg alsaLib libdvdnav libdvdnav.libdvdread
