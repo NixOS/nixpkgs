962e2aede8df63d94d2ca9f973380111efcebcf7
Peter Simons <simons@cryp.to>
2013-06-19 09:27:55 +0200

    haskell-postgresql-simple: update to version 0.3.3.2

--- pkgs/development/libraries/haskell/postgresql-simple/default.nix
+++ pkgs/development/libraries/haskell/postgresql-simple/default.nix
@@ -5,8 +5,8 @@
 
 cabal.mkDerivation (self: {
   pname = "postgresql-simple";
-  version = "0.3.3.1";
-  sha256 = "0fqndncfy6yg27ndfaqyqd2q9wrbabkdq388s4q9m7maj222xlb9";
+  version = "0.3.3.2";
+  sha256 = "1gh2ih1n6g17jry12g7nv344sfzrhfc1assslx0cjlsryhbz25lp";
   buildDepends = [
     attoparsec blazeBuilder blazeTextual postgresqlLibpq text time
     transformers vector
