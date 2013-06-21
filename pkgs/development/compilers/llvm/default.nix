9b502a9ae1f48f50daa2109baaf935ed1e7f8225
Shea Levy <shea@shealevy.com>
2013-06-21 19:18:52 -0400

    llvm: Update to 3.3
    Signed-off-by: Shea Levy <shea@shealevy.com>

--- pkgs/development/compilers/llvm/default.nix
+++ pkgs/development/compilers/llvm/default.nix
@@ -1,30 +1,30 @@
 { stdenv, fetchurl, perl, groff, cmake, python, libffi }:
 
-let version = "3.2"; in
+let version = "3.3"; in
 
 stdenv.mkDerivation {
   name = "llvm-${version}";
 
   src = fetchurl {
     url    = "http://llvm.org/releases/${version}/llvm-${version}.src.tar.gz";
-    sha256 = "0hv30v5l4fkgyijs56sr1pbrlzgd674pg143x7az2h37sb290l0j";
+    sha256 = "0y3mfbb5qzcpw3v5qncn69x1hdrrrfirgs82ypi2annhf0g6nxk8";
   };
 
-  patches = [ ./set_soname.patch ]; # http://llvm.org/bugs/show_bug.cgi?id=12334
-  patchFlags = "-p0";
+  # The default rlimits are too low for shared libraries.
+  patches = [ ./more-memory-for-bugpoint.patch ];
 
-  preConfigure = "patchShebangs .";
-
-  propagatedBuildInputs = [ libffi ];
-  buildInputs = [ perl groff cmake python ]; # ToDo: polly, libc++; enable cxx11?
+  # libffi was propagated before, but it wasn't even being used, so
+  # unless something needs it just an input is fine.
+  buildInputs = [ perl groff cmake python libffi ]; # ToDo: polly, libc++; enable cxx11?
 
   # created binaries need to be run before installation... I coudn't find a better way
   preBuild = ''export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:"`pwd`/lib'';
 
-  cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" "-DBUILD_SHARED_LIBS=ON" ];
+  cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" "-DBUILD_SHARED_LIBS=ON" "-DLLVM_ENABLE_FFI=ON" ];
 
   enableParallelBuilding = true;
-  #doCheck = true; # tests are broken, don't know why
+
+  doCheck = true;
 
   meta = {
     homepage = http://llvm.org/;
