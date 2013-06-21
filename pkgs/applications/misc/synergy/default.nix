57dee2a27e99bc9187e2c832f3e63082a05542d7
aszlig <aszlig@redmoonstudios.org>
2013-06-21 06:20:45 +0200

    synergy: Update to new upstream version 1.4.12.
    This brings in support for encryption and thus requires the crypto++ library as
    an additional dependency. Unfortunately the upstream integration isn't quite the
    way we'd like it to be, so we need to add a small patch to ignore the bundled
    version and use the package from nixpkgs.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/applications/misc/synergy/default.nix
+++ pkgs/applications/misc/synergy/default.nix
@@ -1,19 +1,20 @@
-{ stdenv, fetchurl, cmake, x11, libX11, libXi, libXtst }:
+{ stdenv, fetchurl, cmake, x11, libX11, libXi, libXtst, cryptopp }:
 
 stdenv.mkDerivation rec {
-  name = "synergy-1.4.10";
+  name = "synergy-1.4.12";
 
   src = fetchurl {
   	url = "http://synergy.googlecode.com/files/${name}-Source.tar.gz";
-  	sha256 = "1ghgf96gbk4sdw8sqlc3pjschkmmqybihi12mg6hi26gnk7a5m86";
+  	sha256 = "0j884skwqy8r8ckj9a4rlwsbjwb1yrj9wqma1nwhr2inff6hrdim";
   };
 
-  buildInputs = [ cmake x11 libX11 libXi libXtst ];
-  
+  patches = [ ./cryptopp.patch ];
+
+  buildInputs = [ cmake x11 libX11 libXi libXtst cryptopp ];
+
   # At this moment make install doesn't work for synergy
   # http://synergy-foss.org/spit/issues/details/3317/
 
-  
   installPhase = ''
     ensureDir $out/bin
     cp ../bin/synergyc $out/bin
@@ -21,7 +22,7 @@ stdenv.mkDerivation rec {
     cp ../bin/synergyd $out/bin
   '';
 
-  meta = { 
+  meta = {
     description = "Tool to share the mouse keyboard and the clipboard between computers";
     homepage = http://synergy-foss.org;
     license = stdenv.lib.licenses.gpl2;
