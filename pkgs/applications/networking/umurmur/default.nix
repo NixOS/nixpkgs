1b8548fc725ea75a5ea5bf931e0fd70b685e54af
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2013-06-21 12:15:12 +0200

    Updating umurmur to 0.2.13
    Now opus works.

--- pkgs/applications/networking/umurmur/default.nix
+++ pkgs/applications/networking/umurmur/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, openssl, protobufc, libconfig }:
 
 stdenv.mkDerivation rec {
-  name = "umurmur-0.2.12";
+  name = "umurmur-0.2.13";
   
   src = fetchurl {
     url = "http://umurmur.googlecode.com/files/${name}.tar.gz";
-    sha1 = "5be3c765af3c5f518d1e1bbd828b3582ad4097cd";
+    sha1 = "c9345b67213f52688fef2113132c62d2edbf4bea";
   };
   
   buildInputs = [ openssl protobufc libconfig ];
