14a44600ca69ea5dea61a268843665d605c6104e
Moritz Ulrich <moritz@tarn-vedra.de>
2013-06-22 15:05:32 +0200

    Add stm32flash, flash program for stm32 bootloader.
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3516,6 +3516,8 @@ let
 
   splint = callPackage ../development/tools/analysis/splint { };
 
+  stm32flash = callPackage ../development/tools/misc/stm32flash { };
+
   strace = callPackage ../development/tools/misc/strace { };
 
   swig = callPackage ../development/tools/misc/swig { };
