48e1e1cae649231b533c03a4f8ebda59b2c6424f
Song Wenwu <iyzsong@gmail.com>
2013-06-23 17:48:21 +0800

    add smplayer 0.8.5

---
+++ pkgs/applications/video/smplayer/default.nix
@@ -0,0 +1,22 @@
+{ stdenv, fetchurl, qt4 }:
+
+stdenv.mkDerivation rec {
+  name = "smplayer-0.8.5";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/smplayer/${name}.tar.bz2";
+    sha256 = "0vbfvaqg5c25vabq1mf9xg6kzgvxnpd0i172y1gjznnlpcw2fxrw";
+  };
+
+  buildInputs = [ qt4 ];
+
+  preConfigure = ''
+    makeFlags="PREFIX=$out"
+  '';
+
+  meta = {
+    description = "A complete front-end for MPlayer";
+    homepage = "http://smplayer.sourceforge.net/";
+    license = "GPLv3+";
+  };
+}
