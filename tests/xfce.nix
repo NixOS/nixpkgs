fde8e002ce3470e58a10518098fa930f5d7f4bdf
Domen Kozar <domen@dev.si>
2013-06-23 13:13:03 +0200

    tests.xfce: attempt to fix terminal command

--- tests/xfce.nix
+++ tests/xfce.nix
@@ -23,7 +23,7 @@
       # Check that logging in has given the user ownership of devices.
       $machine->succeed("getfacl /dev/snd/timer | grep -q alice");
 
-      $machine->succeed("su - alice -c 'DISPLAY=:0.0 Terminal &'");
+      $machine->succeed("su - alice -c 'DISPLAY=:0.0 xfce4-terminal &'");
       $machine->waitForWindow(qr/Terminal/);
       $machine->sleep(10);
       $machine->screenshot("screen");
