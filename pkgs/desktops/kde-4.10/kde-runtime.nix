bf61d25dacf03502efd0c9e5acc0a18acccafc11
Mathijs Kwik <mathijs@bluescreen303.nl>
2013-06-26 13:31:21 +0200

    kde 4.10: option to use a libcanberra with gtk support
    by default (as it was before), kde depends on a libcanberra with its
    gtk dependency removed so a kde system can be gtk-free.
    
    However, both firefox and chromium depend on gtk+ and libcanberra too.
    So this leads to 2 versions being installed. this option combats that
    by letting the kde use the other libcanberra

--- pkgs/desktops/kde-4.10/kde-runtime.nix
+++ pkgs/desktops/kde-4.10/kde-runtime.nix
@@ -1,12 +1,21 @@
 { kde, kdelibs, shared_desktop_ontologies, bzip2, libssh, exiv2, attica
 , libcanberra, virtuoso, samba, libjpeg, ntrack, pkgconfig, qca2, xz, pulseaudio
-, networkmanager, kactivities, kdepimlibs, openexr, ilmbase
+, networkmanager, kactivities, kdepimlibs, openexr, ilmbase, config
 }:
+
+let libcanberraWithoutGTK = if (config ? kde_runtime && config.kde_runtime ? libcanberraWithoutGTK)
+                            then config.kde_runtime.libcanberraWithoutGTK
+                            else true;
+
+    canberra = if libcanberraWithoutGTK
+               then libcanberra.override { gtk = null; }
+               else libcanberra;
+in
 
 kde {
   buildInputs = [
     kdelibs attica xz bzip2 libssh libjpeg exiv2 ntrack
-    qca2 samba (libcanberra.override { gtk = null; }) pulseaudio
+    qca2 samba canberra pulseaudio
     networkmanager kactivities kdepimlibs openexr
 #todo: add openslp
   ];
