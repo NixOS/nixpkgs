b2973d3b07475bf4d1562bf3c7dcacddff1f41ec
aszlig <aszlig@redmoonstudios.org>
2013-06-28 04:15:27 +0200

    python-packages: Add nixpart version 0.1.
    This version is preliminary because it quite heavily depends on pykickstart
    (through blivet) and the roadmap is to have a nice NixOS attrset-based
    specification of partitions.
    
    Currently the main purpose for this is in preparation for the Hetzner nixops
    backend, but we might want to make this part of the standard NixOS installer.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2778,6 +2778,26 @@ pythonPackages = python.modules // rec {
     };
   });
 
+  nixpart = buildPythonPackage rec {
+    name = "nixpart-${version}";
+    version = "0.1";
+
+    src = fetchurl {
+      url = "https://github.com/aszlig/nixpart/archive/v${version}.tar.gz";
+      sha256 = "1gyk260qnv14w94f0ljfra4mmigq9i6cx0gy4ppf3fia5wfj2a6j";
+    };
+
+    propagatedBuildInputs = [ blivet ];
+
+    doCheck = false;
+
+    meta = {
+      description = "NixOS Anaconda Kickstart helper";
+      license = pkgs.lib.licenses.gpl2Plus;
+      maintainers = [ stdenv.lib.maintainers.aszlig ];
+    };
+  };
+
   nose = buildPythonPackage rec {
     name = "nose-1.2.1";
 
