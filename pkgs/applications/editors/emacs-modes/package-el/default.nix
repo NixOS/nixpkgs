bd80ae6cdb844c68164de4d3cbfbdab89a476270
Moritz Ulrich <moritz@tarn-vedra.de>
2013-06-28 09:38:15 +0200

    Add propagatedUserEnvPkgs and try to run ert tests.
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- pkgs/applications/editors/emacs-modes/package-el/default.nix
+++ pkgs/applications/editors/emacs-modes/package-el/default.nix
@@ -11,8 +11,36 @@ let
   packageFile = if packageElSupported
     then concatStringsSep "." [name (last (splitString "." src.name))]
     else "";
+  emacsInitialization = cmds: ''
+     (condition-case err
+        (progn
+          (setq debug-on-error t)
+          (load "'${emacs}/share/emacs/site-lisp/site-start.el'")
+          (setq package-user-dir (expand-file-name "'./share/emacs/elpa/'"))
+          (require (quote package))
+          (package-initialize)
+          ${cmds}
+          (setq kill-emacs-hook nil))
+        (error (progn
+                 (setq kill-emacs-hook nil)
+                 (error (error-message-string err)))))
+  '';
 in
 stdenv.mkDerivation ({
+  name = args.name;
+
+  # Run the emacs setup hook when this package is a build input
+  propagatedBuildInputs = (args.propagatedBuildInputs or []) ++ deps ++ [ emacs ];
+
+  # The user wants the package and it's dependencies in his profile.
+  # TODO: Should we automatically add emacs too?
+  propagatedUserEnvPkgs = (args.propagatedUserEnvPkgs or []) ++ deps;
+
+  # Make buildEmacsPackage Package useful with --run-env
+  buildInputs = (args.buildInputs or []) ++ deps;
+
+  phases = [ "unpackPhase" "buildPhase" "installPhase" "fixupPhase" "testPhase" ];
+
   unpackPhase = ''
     # This is actually a hack. package.el needs the correct filename,
     # not a nix-store filename.
@@ -23,20 +51,9 @@ stdenv.mkDerivation ({
     runHook preBuild
 
     mkdir -pv $out/share/emacs/elpa/
-    ${emacs}/bin/emacs --batch -Q --eval '
-      (condition-case err
-        (progn
-          (setq debug-on-error t)
-          (load "'${emacs}/share/emacs/site-lisp/site-start.el'")
-          (setq package-user-dir (expand-file-name "'./share/emacs/elpa/'"))
-          (require (quote package))
-          (package-initialize)
-          (package-install-file "./${packageFile}")
-          (setq kill-emacs-hook nil))
-        (error (progn
-                 (setq kill-emacs-hook nil)
-                 (error (error-message-string err)))))'
-    rm ./${packageFile}
+    ${emacs}/bin/emacs --batch --eval \ '
+      ${emacsInitialization ''(package-install-file "./${packageFile}")''};
+      '
 
     runHook postBuild
   '';
@@ -49,10 +66,14 @@ stdenv.mkDerivation ({
 
   preFixup = ''
   '';
-} // args // {
-  # Run the emacs setup hook when this package is a build input
-  propagatedBuildInputs = (args.propagatedBuildInputs or []) ++ [ emacs ];
 
-  # # Make buildEmacsPackage Package useful with --run-env
-  buildInputs = (args.buildInputs or []) ++ deps;
+  testPhase = ''
+    ${emacs}/bin/emacs --batch '
+      ${emacsInitialization ''
+        ;; Load any file where name contains test
+        (cl-dolist (f (directory-files "." (quote full-path) "test.*\\.el"))
+          (load-file f))
+        (ert-run-tests-batch-and-exit)
+      ''}'
+  '';
 } )
