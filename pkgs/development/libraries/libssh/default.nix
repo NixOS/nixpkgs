9ce21dab8d7f02a806929eabf98de9380cea0ce6
Vladimír Čunát <vcunat@gmail.com>
2013-06-29 17:07:11 +0200

    libssh: non-critical security update CVE-2013-0176

--- pkgs/development/libraries/libssh/default.nix
+++ pkgs/development/libraries/libssh/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, cmake, zlib, libgcrypt }:
 
 stdenv.mkDerivation rec {
-  name = "libssh-0.5.3";
+  name = "libssh-0.5.4";
 
   src = fetchurl {
-    url = "https://red.libssh.org/attachments/download/38/${name}.tar.gz";
-    sha256 = "1w6s217vjq0w3v5i0c5ql6m0ki1yz05g9snah3azxfkl9k4schpd";
+    url = "https://red.libssh.org/attachments/download/41/libssh-0.5.4.tar.gz";
+    sha256 = "106a9nzmr8q5kw809p5zdhmjrqv7p6609ai5c1n7xi7pxg52af2z";
   };
 
   buildInputs = [ zlib libgcrypt ];
