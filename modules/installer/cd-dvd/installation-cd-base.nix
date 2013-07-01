59bab9048a8eb238d735920cc27ea31f18ad341d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-07-01 23:13:28 +0200

    Move initial channel generation into a separate module

--- modules/installer/cd-dvd/installation-cd-base.nix
+++ modules/installer/cd-dvd/installation-cd-base.nix
@@ -5,31 +5,11 @@
 
 with pkgs.lib;
 
-let
-
-  # We need a copy of the Nix expressions for Nixpkgs and NixOS on the
-  # CD.  These are installed into the "nixos" channel of the root
-  # user, as expected by nixos-rebuild/nixos-install.
-  channelSources = pkgs.runCommand "nixos-${config.system.nixosVersion}"
-    { expr = builtins.readFile ../../../lib/channel-expr.nix; }
-    ''
-      mkdir -p $out/nixos
-      cp -prd ${cleanSource ../../..} $out/nixos/nixos
-      cp -prd ${cleanSource <nixpkgs>} $out/nixos/nixpkgs
-      chmod -R u+w $out/nixos/nixos
-      echo -n ${config.system.nixosVersion} > $out/nixos/nixos/.version
-      echo -n "" > $out/nixos/nixos/.version-suffix
-      echo "$expr" > $out/nixos/default.nix
-    '';
-
-  includeSources = true;
-
-in
-
 {
   require =
     [ ./memtest.nix
       ./iso-image.nix
+      ./channel.nix
 
       # Profiles of this basic installation CD.
       ../../profiles/all-hardware.nix
@@ -41,17 +21,6 @@ in
   isoImage.isoName = "${config.isoImage.isoBaseName}-${config.system.nixosVersion}-${pkgs.stdenv.system}.iso";
 
   isoImage.volumeID = "NIXOS_${config.system.nixosVersion}";
-
-  # Provide the NixOS/Nixpkgs sources in /etc/nixos.  This is required
-  # for nixos-install.
-  boot.postBootCommands = optionalString includeSources
-    ''
-      echo "unpacking the NixOS/Nixpkgs sources..."
-      mkdir -p /nix/var/nix/profiles/per-user/root
-      ${config.environment.nix}/bin/nix-env -p /nix/var/nix/profiles/per-user/root/channels -i ${channelSources} --quiet
-      mkdir -m 0700 -p /root/.nix-defexpr
-      ln -s /nix/var/nix/profiles/per-user/root/channels /root/.nix-defexpr/channels
-    '';
 
   # Make the installer more likely to succeed in low memory
   # environments.  The kernel's overcommit heustistics bite us
