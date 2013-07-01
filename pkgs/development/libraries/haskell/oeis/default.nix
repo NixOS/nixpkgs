88034ca8196adaad5138ed1892d40f4ca8a67233
Peter Simons <simons@cryp.to>
2013-07-01 11:04:04 +0200

    haskell-oeis: update to version 0.3.2

--- pkgs/development/libraries/haskell/oeis/default.nix
+++ pkgs/development/libraries/haskell/oeis/default.nix
@@ -1,12 +1,13 @@
-{ cabal, HTTP, network }:
+{ cabal, HTTP, HUnit, network, testFramework, testFrameworkHunit }:
 
 cabal.mkDerivation (self: {
   pname = "oeis";
-  version = "0.3.1";
-  sha256 = "0kxs25b1z0b807vhrn8v7chsdsw8civqiym8767fy2rk5si0i4w2";
+  version = "0.3.2";
+  sha256 = "1lp4mbsh98vnyfbnq9224n98hajv8q5prpzgbcw90bih0rbiw4w4";
   buildDepends = [ HTTP network ];
+  testDepends = [ HUnit testFramework testFrameworkHunit ];
   meta = {
-    description = "Interface to the Online Encyclopedia of Integer Sequences";
+    description = "Interface to the Online Encyclopedia of Integer Sequences (OEIS)";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [ self.stdenv.lib.maintainers.andres ];
