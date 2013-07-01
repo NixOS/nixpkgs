2cb552f2dc8726a402237f2ddef914350edb5e1d
Peter Simons <simons@cryp.to>
2013-07-01 11:04:03 +0200

    haskell-conduit: update to version 1.0.7

--- pkgs/development/libraries/haskell/conduit/default.nix
+++ pkgs/development/libraries/haskell/conduit/default.nix
@@ -1,17 +1,17 @@
-{ cabal, doctest, hspec, liftedBase, mmorph, monadControl
+{ cabal, doctest, hspec, liftedBase, mmorph, monadControl, mtl
 , QuickCheck, resourcet, text, transformers, transformersBase, void
 }:
 
 cabal.mkDerivation (self: {
   pname = "conduit";
-  version = "1.0.6";
-  sha256 = "0da5wxhsfjgcn850iimcp15vhg5lw0fa8i7c3hdw4yvrpza14dcn";
+  version = "1.0.7";
+  sha256 = "0nail476sz4dmr052sl5s14hkk336zp22hpgmr2qf141zzy8i2m1";
   buildDepends = [
-    liftedBase mmorph monadControl resourcet text transformers
+    liftedBase mmorph monadControl mtl resourcet text transformers
     transformersBase void
   ];
   testDepends = [
-    doctest hspec QuickCheck resourcet text transformers void
+    doctest hspec mtl QuickCheck resourcet text transformers void
   ];
   meta = {
     homepage = "http://github.com/snoyberg/conduit";
