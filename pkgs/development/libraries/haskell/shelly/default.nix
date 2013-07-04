cfbae2de737f3ac95e107bdf599b2095e3df24f7
Peter Simons <simons@cryp.to>
2013-07-04 17:38:57 +0200

    haskell-shelly: update to version 1.3.0.3

--- pkgs/development/libraries/haskell/shelly/default.nix
+++ pkgs/development/libraries/haskell/shelly/default.nix
@@ -3,8 +3,8 @@
 
 cabal.mkDerivation (self: {
   pname = "shelly";
-  version = "1.3.0.2";
-  sha256 = "1vhjqm2y6hfaq88k4qz0gdjic1316c5p1gyzgz19r0iwry6msrh2";
+  version = "1.3.0.3";
+  sha256 = "0izfxv45b5j9p3q2nrlxdn9g4vfmzghl4ab7zisn07gx381wk9qq";
   buildDepends = [
     mtl systemFileio systemFilepath text time unixCompat
   ];
