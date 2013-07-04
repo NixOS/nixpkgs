d199fe766a6f96d8739b5e8d1a9d9211d5f795e8
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-07-04 17:44:44 +0200

    qemu: Merge stuff from qemu-kvm

--- pkgs/applications/virtualization/qemu/default.nix
+++ pkgs/applications/virtualization/qemu/default.nix
@@ -1,5 +1,9 @@
-{ stdenv, fetchurl, python, zlib, pkgconfig, glib, SDL, ncurses, perl, pixman
-, attr, libcap, vde2 }:
+{ stdenv, fetchurl, python, zlib, pkgconfig, glib, ncurses, perl, pixman
+, attr, libcap, vde2, alsaLib, texinfo, libuuid
+, sdlSupport ? true, SDL
+, vncSupport ? true, libjpeg, libpng
+, spiceSupport ? false, spice, spice_protocol
+}:
 
 stdenv.mkDerivation rec {
   name = "qemu-1.5.1";
@@ -9,17 +13,21 @@ stdenv.mkDerivation rec {
     sha256 = "1s7316pgizpayr472la8p8a4vhv7ymmzd5qlbkmq6y9q5zpa25ac";
   };
 
-  buildInputs = [
-    python zlib pkgconfig glib SDL ncurses perl pixman attr libcap
-    vde2
-  ];
+  buildInputs =
+    [ python zlib pkgconfig glib ncurses perl pixman attr libcap
+      vde2 alsaLib texinfo libuuid
+    ]
+    ++ stdenv.lib.optionals sdlSupport [ SDL ]
+    ++ stdenv.lib.optionals vncSupport [ libjpeg libpng ]
+    ++ stdenv.lib.optionals spiceSupport [ spice_protocol spice ];
 
   enableParallelBuilding = true;
 
-  configureFlags = [
-    "--enable-virtfs"
-    "--enable-vde"
-  ];
+  configureFlags =
+    [ "--audio-drv-list=alsa"
+      "--smbd=smbd"  # use `smbd' from $PATH
+    ]
+    ++ stdenv.lib.optional spiceSupport "--enable-spice";
 
   meta = {
     homepage = http://www.qemu.org/;
