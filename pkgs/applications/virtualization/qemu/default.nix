9da1586d7529af32337b3ec8de261d60d1312a4a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-07-04 16:52:43 +0200

    qemu: Update to 1.5.1

--- pkgs/applications/virtualization/qemu/default.nix
+++ pkgs/applications/virtualization/qemu/default.nix
@@ -2,11 +2,11 @@
 , attr, libcap, vde2 }:
 
 stdenv.mkDerivation rec {
-  name = "qemu-1.4.0";
+  name = "qemu-1.5.1";
 
   src = fetchurl {
     url = "http://wiki.qemu.org/download/${name}.tar.bz2";
-    sha256 = "1a7d11vjs1p6i1ck2ff9annmkhpkbjl73hl9i1cbg3s0fznrfqh6";
+    sha256 = "1s7316pgizpayr472la8p8a4vhv7ymmzd5qlbkmq6y9q5zpa25ac";
   };
 
   buildInputs = [
@@ -22,9 +22,10 @@ stdenv.mkDerivation rec {
   ];
 
   meta = {
-    description = "QEmu processor emulator";
+    homepage = http://www.qemu.org/;
+    description = "A generic and open source machine emulator and virtualizer";
     license = "GPLv2+";
-    maintainers = with stdenv.lib.maintainers; [ viric shlevy ];
-    platforms = with stdenv.lib.platforms; linux;
+    maintainers = with stdenv.lib.maintainers; [ viric shlevy eelco ];
+    platforms = stdenv.lib.platforms.linux;
   };
 }
