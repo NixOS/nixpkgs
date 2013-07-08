7b89bb2da4e3a854210706d9d4996ae2cfe3f70d
Peter Simons <simons@cryp.to>
2013-07-08 11:53:10 +0200

    haskell-attoparsec-conduit: update to version 1.0.1.1

--- pkgs/development/libraries/haskell/attoparsec-conduit/default.nix
+++ pkgs/development/libraries/haskell/attoparsec-conduit/default.nix
@@ -3,14 +3,14 @@
 
 cabal.mkDerivation (self: {
   pname = "attoparsec-conduit";
-  version = "1.0.1";
-  sha256 = "14b6ym5sjvg1x82ijydhrjk5445kg0fvwqzqwqld59akbqb6fpg5";
+  version = "1.0.1.1";
+  sha256 = "0v6d5a720fksvgaysbhqfzsq9a9h0l37yw3dbskxljbdy66gqsh0";
   buildDepends = [ attoparsec conduit text transformers ];
   testDepends = [ attoparsec conduit hspec resourcet text ];
   meta = {
     homepage = "http://github.com/snoyberg/conduit";
     description = "Consume attoparsec parsers via conduit";
-    license = self.stdenv.lib.licenses.bsd3;
+    license = self.stdenv.lib.licenses.mit;
     platforms = self.ghc.meta.platforms;
     maintainers = [ self.stdenv.lib.maintainers.andres ];
   };
