eda01e3439f0098d2572984335dbae59c03b4457
Peter Simons <simons@cryp.to>
2013-07-10 09:20:34 +0200

    haskell-atomic-primops: update to version 0.2.2

--- pkgs/development/libraries/haskell/atomic-primops/default.nix
+++ pkgs/development/libraries/haskell/atomic-primops/default.nix
@@ -1,10 +1,10 @@
-{ cabal, Cabal, primitive }:
+{ cabal, bitsAtomic, Cabal, primitive }:
 
 cabal.mkDerivation (self: {
   pname = "atomic-primops";
-  version = "0.1.0.2";
-  sha256 = "1qjm97hi2wr658yk7f5cfppizaawmrkvs2q7qzq00h14fr71xxca";
-  buildDepends = [ Cabal primitive ];
+  version = "0.2.2";
+  sha256 = "1a3svsh96pl6915g70sf9zhqby0ahhifww6m13cn0zr4za32vl7n";
+  buildDepends = [ bitsAtomic Cabal primitive ];
   meta = {
     homepage = "https://github.com/rrnewton/haskell-lockfree-queue/wiki";
     description = "A safe approach to CAS and other atomic ops in Haskell";
