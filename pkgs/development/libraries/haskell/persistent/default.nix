a7c66776cb922eead0a3a0339744f9029ba6daa7
Peter Simons <simons@cryp.to>
2013-07-10 09:20:34 +0200

    haskell-persistent: update to version 1.2.2.0

--- pkgs/development/libraries/haskell/persistent/default.nix
+++ pkgs/development/libraries/haskell/persistent/default.nix
@@ -7,8 +7,8 @@
 
 cabal.mkDerivation (self: {
   pname = "persistent";
-  version = "1.2.1.1";
-  sha256 = "12gn84svbi72122si9q61987wmb8zbam0x7jy2x20r2bbdfh50rv";
+  version = "1.2.2.0";
+  sha256 = "18p3yz683ks0gdn1l4qys29jj7iqhlwh7i0s50bkl4x1w0mpa1kh";
   buildDepends = [
     aeson attoparsec base64Bytestring blazeHtml blazeMarkup conduit
     liftedBase monadControl monadLogger pathPieces poolConduit
