649dd8377ec85928cd93fb6cf37ef48f39daa7c2
Evgeny Egorochkin <phreedom@yandex.ru>
2013-07-13 23:33:06 +0300

    perl-net-amazon-s3-policy: fix version to match tarball

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -4790,7 +4790,7 @@ rec {
   };
 
   NetAmazonS3Policy = buildPerlPackage {
-    name = "Net-Amazon-S3-Policy-0.001002";
+    name = "Net-Amazon-S3-Policy-0.1.2";
     src = fetchurl {
       url = mirror://cpan/authors/id/P/PO/POLETTIX/Net-Amazon-S3-Policy-0.1.2.tar.gz;
       sha256 = "1kkzimvxwxx0mypf75dalksxmvwsb8b575yx2nzmvjkvjg3j9na0";
