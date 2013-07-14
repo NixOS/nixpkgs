b71c382f6fdb2d7f23c0729efa878d784ea0897e
Peter Simons <simons@cryp.to>
2013-07-14 21:22:57 +0200

    haskell-bindings-posix: fix bogus change committed earlier

--- pkgs/development/libraries/haskell/bindings-posix/default.nix
+++ pkgs/development/libraries/haskell/bindings-posix/default.nix
@@ -1,11 +1,10 @@
-{ cabal, bindingsDSL, pthread }:
+{ cabal, bindingsDSL }:
 
 cabal.mkDerivation (self: {
   pname = "bindings-posix";
   version = "1.2.3";
   sha256 = "0nj18lfpn8hmlaa7cmvdkjnk8fi2f6ysjbigkx7zbrpqnvbi63ba";
   buildDepends = [ bindingsDSL ];
-  extraLibraries = [ pthread ];
   meta = {
     description = "Low level bindings to posix";
     license = self.stdenv.lib.licenses.bsd3;
