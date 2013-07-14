1ae62db2e0acd4f83c98f3a05e8f0471d4a6572e
Peter Simons <simons@cryp.to>
2013-07-14 20:19:41 +0200

    haskell-bindings-posix: update to version 1.2.3

--- pkgs/development/libraries/haskell/bindings-posix/default.nix
+++ pkgs/development/libraries/haskell/bindings-posix/default.nix
@@ -1,10 +1,11 @@
-{ cabal, bindingsDSL }:
+{ cabal, bindingsDSL, pthread }:
 
 cabal.mkDerivation (self: {
   pname = "bindings-posix";
   version = "1.2.3";
   sha256 = "0nj18lfpn8hmlaa7cmvdkjnk8fi2f6ysjbigkx7zbrpqnvbi63ba";
   buildDepends = [ bindingsDSL ];
+  extraLibraries = [ pthread ];
   meta = {
     description = "Low level bindings to posix";
     license = self.stdenv.lib.licenses.bsd3;
