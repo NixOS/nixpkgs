567fbce9beaf6a126f13cdd2bd5ccd6be18f897e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-07-16 13:59:41 +0200

    NFS test: Set a fixed fsid
    Otherwise clients get a "Stale NFS handle" error after the server
    reboots.  Not clear why this is the case, since the fsid shouldn't
    change across reboots...

--- tests/nfs.nix
+++ tests/nfs.nix
@@ -26,7 +26,7 @@ in
         { services.nfs.server.enable = true;
           services.nfs.server.exports =
             ''
-              /data 192.168.1.0/255.255.255.0(rw,no_root_squash)
+              /data 192.168.1.0/255.255.255.0(rw,no_root_squash,no_subtree_check,fsid=0)
             '';
           services.nfs.server.createMountPoints = true;
         };
