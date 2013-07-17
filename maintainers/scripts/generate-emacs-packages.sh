32557d987886860b8d9db78763e5a603f95ce727
Moritz Ulrich <moritz@tarn-vedra.de>
2013-07-18 01:20:29 +0200

    Fix generate-emacs-packages.sh when called outside of the directory.
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- maintainers/scripts/generate-emacs-packages.sh
+++ maintainers/scripts/generate-emacs-packages.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-emacs --batch --load generate-emacs-packages.el --eval '(nix-generate-emacs-packages)'
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd $DIR
+emacs --batch --load "$DIR/generate-emacs-packages.el" --eval '(nix-generate-emacs-packages)'
+popd
