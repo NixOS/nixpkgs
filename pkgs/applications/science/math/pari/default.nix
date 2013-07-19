6ae39a424236b0ce1fec6ba17b25bad2a55da326
Ertugrul Söylemez <es@ertes.de>
2013-07-19 17:20:37 +0200

    pari: Updated to 2.5.4, fixed GMP and readline support.

--- pkgs/applications/science/math/pari/default.nix
+++ pkgs/applications/science/math/pari/default.nix
@@ -1,50 +1,24 @@
-x@{builderDefsPackage
-  , perl, zlib, gmp, readline
-  , ...}:
-builderDefsPackage
-(a :  
-let 
-  helperArgNames = ["stdenv" "fetchurl" "builderDefsPackage"] ++ 
-    [];
+{ fetchurl, gmp, readline, stdenv }:
 
-  buildInputs = map (n: builtins.getAttr n x)
-    (builtins.attrNames (builtins.removeAttrs x helperArgNames));
-  sourceInfo = rec {
-    baseName="pari";
-    version="2.5.0";
-    name="${baseName}-${version}";
-    url="http://pari.math.u-bordeaux.fr/pub/pari/unix/${name}.tar.gz";
-    hash="18ipxj4hzj7s3fqz878fiypkzrkbjj8wvbygz9j8c3ya06q27jax";
-  };
-in
-rec {
-  src = a.fetchurl {
-    url = sourceInfo.url;
-    sha256 = sourceInfo.hash;
-  };
-
-  inherit (sourceInfo) name version;
-  inherit buildInputs;
-
-  /* doConfigure should be removed if not needed */
-  phaseNames = ["doConfigure" "doMakeInstall"];
-  configureCommand="./Configure";
-      
-  meta = {
-    description = "Computer algebra system for high-performance number theory computations";
-    maintainers = with a.lib.maintainers;
-    [
-      raskin
-    ];
-    platforms = with a.lib.platforms;
-      linux;
-    license = "GPLv2+";
-    homepage = "http://pari.math.u-bordeaux.fr/";
-  };
-  passthru = {
-    updateInfo = {
-      downloadPage = "http://pari.math.u-bordeaux.fr/download.html";
+stdenv.mkDerivation rec {
+    name = "pari-2.5.4";
+    meta = {
+        description = "Computer algebra system for high-performance number theory computations";
+        homepage    = "http://pari.math.u-bordeaux.fr/";
+        license     = "GPLv2+";
+        maintainers = with stdenv.lib.maintainers; [ertes raskin];
+        platforms   = stdenv.lib.platforms.linux;
     };
-  };
-}) x
 
+    src = fetchurl {
+        url = "http://pari.math.u-bordeaux.fr/pub/pari/unix/${name}.tar.gz";
+        sha256 = "0gpsj5n8d1gyl7nq2y915sscs3d334ryrv8qgjdwqf3cr95f2dwz";
+    };
+
+    buildInputs = [gmp readline];
+
+    configureScript = "./Configure";
+    configureFlags =
+        "--with-gmp=${gmp} " +
+        "--with-readline=${readline}";
+}
