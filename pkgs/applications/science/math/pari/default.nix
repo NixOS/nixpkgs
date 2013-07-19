481116b5ed6930b4d22f4f2db399a522c7af7414
Ertugrul Söylemez <es@ertes.de>
2013-07-19 19:39:13 +0200

    pari: Coding style.

--- pkgs/applications/science/math/pari/default.nix
+++ pkgs/applications/science/math/pari/default.nix
@@ -1,24 +1,25 @@
-{ fetchurl, gmp, readline, stdenv }:
+{ stdenv, fetchurl, gmp, readline }:
 
 stdenv.mkDerivation rec {
-    name = "pari-2.5.4";
-    meta = {
-        description = "Computer algebra system for high-performance number theory computations";
-        homepage    = "http://pari.math.u-bordeaux.fr/";
-        license     = "GPLv2+";
-        maintainers = with stdenv.lib.maintainers; [ertes raskin];
-        platforms   = stdenv.lib.platforms.linux;
-    };
+  name = "pari-2.5.4";
 
-    src = fetchurl {
-        url = "http://pari.math.u-bordeaux.fr/pub/pari/unix/${name}.tar.gz";
-        sha256 = "0gpsj5n8d1gyl7nq2y915sscs3d334ryrv8qgjdwqf3cr95f2dwz";
-    };
+  src = fetchurl {
+    url = "http://pari.math.u-bordeaux.fr/pub/pari/unix/${name}.tar.gz";
+    sha256 = "0gpsj5n8d1gyl7nq2y915sscs3d334ryrv8qgjdwqf3cr95f2dwz";
+  };
 
-    buildInputs = [gmp readline];
+  buildInputs = [gmp readline];
 
-    configureScript = "./Configure";
-    configureFlags =
-        "--with-gmp=${gmp} " +
-        "--with-readline=${readline}";
+  configureScript = "./Configure";
+  configureFlags =
+    "--with-gmp=${gmp} " +
+    "--with-readline=${readline}";
+
+  meta = {
+    description = "Computer algebra system for high-performance number theory computations";
+    homepage    = "http://pari.math.u-bordeaux.fr/";
+    license     = "GPLv2+";
+    maintainers = with stdenv.lib.maintainers; [ertes raskin];
+    platforms   = stdenv.lib.platforms.linux;
+  };
 }
