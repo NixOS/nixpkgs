8740b48d862a3d739e0f261b7a4833c3f994eddc
Peter Simons <simons@cryp.to>
2013-07-21 15:37:56 +0200

    haskell-classy-prelude: update to version 0.5.9

--- pkgs/development/libraries/haskell/classy-prelude/default.nix
+++ pkgs/development/libraries/haskell/classy-prelude/default.nix
@@ -1,15 +1,15 @@
-{ cabal, basicPrelude, hashable, hspec, liftedBase, monadControl
-, QuickCheck, systemFilepath, text, transformers
+{ cabal, async, basicPrelude, deepseq, hashable, hspec, liftedBase
+, monadControl, QuickCheck, systemFilepath, text, transformers
 , unorderedContainers, vector
 }:
 
 cabal.mkDerivation (self: {
   pname = "classy-prelude";
-  version = "0.5.8";
-  sha256 = "1yq2x3mfkasprmsx1gracjhih9l9x0dsq6pdf90khlcl11qh57ir";
+  version = "0.5.9";
+  sha256 = "1qqmip3ynqdxlwynm60wsn82dcyymcfql79k039iablanj4mic61";
   buildDepends = [
-    basicPrelude hashable liftedBase monadControl systemFilepath text
-    transformers unorderedContainers vector
+    async basicPrelude deepseq hashable liftedBase monadControl
+    systemFilepath text transformers unorderedContainers vector
   ];
   testDepends = [ hspec QuickCheck transformers ];
   meta = {
