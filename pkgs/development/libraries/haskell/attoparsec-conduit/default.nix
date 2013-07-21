16581965df0b2c92d5132673a8a81946522a391d
Peter Simons <simons@cryp.to>
2013-07-21 15:37:56 +0200

    haskell-attoparsec-conduit: update to version 1.0.1.2

--- pkgs/development/libraries/haskell/attoparsec-conduit/default.nix
+++ pkgs/development/libraries/haskell/attoparsec-conduit/default.nix
@@ -3,8 +3,8 @@
 
 cabal.mkDerivation (self: {
   pname = "attoparsec-conduit";
-  version = "1.0.1.1";
-  sha256 = "0v6d5a720fksvgaysbhqfzsq9a9h0l37yw3dbskxljbdy66gqsh0";
+  version = "1.0.1.2";
+  sha256 = "1j05r7mvm83wgnka7asmwd1dj4ajkg548mryyhpr7dd53vn5lbx0";
   buildDepends = [ attoparsec conduit text transformers ];
   testDepends = [ attoparsec conduit hspec resourcet text ];
   meta = {
