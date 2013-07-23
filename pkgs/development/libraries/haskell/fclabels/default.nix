025995edafc2001d4c7cabbc2cd09d83b606b65e
Peter Simons <simons@cryp.to>
2013-07-23 11:56:01 +0200

    haskell-fclabels: disable the Haddock documentation to fix the build
    https://github.com/sebastiaanvisser/fclabels/issues/20

--- pkgs/development/libraries/haskell/fclabels/default.nix
+++ pkgs/development/libraries/haskell/fclabels/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "1.1.7";
   sha256 = "0mk75fk3c4ilb2hmz972ymv1pkzxp6010mlma7hw1l13dfy0hy9v";
   buildDepends = [ mtl transformers ];
+  noHaddock = true;
   meta = {
     homepage = "https://github.com/sebastiaanvisser/fclabels";
     description = "First class accessor labels";
