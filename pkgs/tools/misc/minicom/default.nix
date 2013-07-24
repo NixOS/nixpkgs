63f9aed9ebe20b926f221997eb237cd96076fb5a
Bjørn Forsman <bjorn.forsman@gmail.com>
2013-07-24 14:36:00 +0200

    minicom: 2.4 -> 2.6.2
    This also fixes the annoying issue that minicom doesn't work out of the
    box:
    
      $ minicom
      minicom: there is no global configuration file /etc/minirc.dfl
      Ask your sysadmin to create one (with minicom -s).
    
      $ sudo minicom -s
      minicom: there is no global configuration file /etc/minirc.dfl
      Ask your sysadmin to create one (with minicom -s).
    
    minicom 2.4 basically refuses to enter setup unless /etc/minirc.dfl
    already exists. sudo touch /etc/minirc.dfl is enough to fix that though,
    but with this commit "sudo minicom -s" will work out of the box.

--- pkgs/tools/misc/minicom/default.nix
+++ pkgs/tools/misc/minicom/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ncurses }:
 
 stdenv.mkDerivation rec {
-  name = "minicom-2.4";
+  name = "minicom-2.6.2";
 
   src = fetchurl {
-    url = "http://alioth.debian.org/frs/download.php/3195/${name}.tar.gz";
-    sha256 = "0j0ayimh3389pciqs60fsfafn87p9gnmmmqz15xq9fkkn10g4ykb";
+    url = "http://alioth.debian.org/frs/download.php/file/3869/${name}.tar.gz";
+    sha256 = "0s4ibk8scspm8a0raf5s4zgp9b82c4bn529rir9abzqlg5gj3kzk";
   };
 
   buildInputs = [ncurses];
