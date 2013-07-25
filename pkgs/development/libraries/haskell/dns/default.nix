88cb67fc889ef7b57fdd09b68b0962a3f26f61a1
Peter Simons <simons@cryp.to>
2013-07-25 10:29:22 +0200

    haskell-dns: update to version 0.3.7

--- pkgs/development/libraries/haskell/dns/default.nix
+++ pkgs/development/libraries/haskell/dns/default.nix
@@ -1,14 +1,18 @@
 { cabal, attoparsec, attoparsecConduit, binary, blazeBuilder
-, conduit, iproute, mtl, network, networkConduit, random
+, conduit, hspec, iproute, mtl, network, networkConduit, random
 }:
 
 cabal.mkDerivation (self: {
   pname = "dns";
-  version = "0.3.6";
-  sha256 = "0dpwy94id9rxxjpji47nazinm8i1ihm0606dmi5iqqhbl5h2jara";
+  version = "0.3.7";
+  sha256 = "1wly3h36j9gjyx6p2vzand5019m6rs0qkcf1h4q61igks65xs674";
   buildDepends = [
     attoparsec attoparsecConduit binary blazeBuilder conduit iproute
     mtl network networkConduit random
+  ];
+  testDepends = [
+    attoparsec attoparsecConduit binary blazeBuilder conduit hspec
+    iproute mtl network networkConduit random
   ];
   meta = {
     description = "DNS library in Haskell";
