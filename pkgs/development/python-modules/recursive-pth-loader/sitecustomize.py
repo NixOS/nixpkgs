12218c371a2c43e3e212a8be29479c93cb4649b2
Bjørn Forsman <bjorn.forsman@gmail.com>
2013-07-30 13:08:08 +0200

    python: append paths from PYTHON<VERSION>PATH env var to sys.path
    Use of this (admittedly non-standard) variable enables a safer way to
    extend Python interpreters with modules that are installed in a nix
    profile.
    
    Instead of the unsafe
    
      export PYTHONPATH=~/.nix-profile/lib/python2.7/site-packages
    
    (which probably breaks if you start python3) you can now
    
      export PYTHON27PATH=~/.nix-profile/lib/python2.7/site-packages
      export PYTHON33PATH=~/.nix-profile/lib/python3.3/site-packages
    
    and there will be no interference between python2.7 and python3.3.

--- pkgs/development/python-modules/recursive-pth-loader/sitecustomize.py
+++ pkgs/development/python-modules/recursive-pth-loader/sitecustomize.py
@@ -1,5 +1,6 @@
 """Recursively load pth files in site-packages of sys.path
 
+- Append paths from PYTHON<VERSION>PATH environment variable to sys.path
 - iterate over sys.path
 - check for pth in dirs that end in site-packages
 - ignore import statements in pth files
@@ -11,6 +12,19 @@ import os
 import site
 import sys
 
+# Append paths from PYTHON<VERSION>PATH environment variable to sys.path. Use
+# of this (admittedly non-standard) variable enables a safer way to extend
+# Python interpreters with modules that are installed in a nix profile.
+#
+# Instead of the unsafe
+#   export PYTHONPATH=~/.nix-profile/lib/python2.7/site-packages
+# which probably breaks if you start python3, you can now
+#   export PYTHON27PATH=~/.nix-profile/lib/python2.7/site-packages
+#   export PYTHON33PATH=~/.nix-profile/lib/python3.3/site-packages
+# and there will be no interference between python2.7 and python3.3.
+pythonpath = os.getenv("PYTHON%d%dPATH" % (sys.version_info.major, sys.version_info.minor))
+if pythonpath:
+    sys.path.extend(pythonpath.split(os.pathsep))
 
 for path_idx, sitedir in enumerate(sys.path):
     # ignore non-site-packages
