2208c360ac3ed2633b568520803d2887e18d9133
Ricardo M. Correia <rcorreia@wizy.org>
2013-08-05 02:29:04 +0000

    linux: Update to 3.2.50

--- pkgs/os-specific/linux/kernel/linux-3.2.nix
+++ pkgs/os-specific/linux/kernel/linux-3.2.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.2.49";
+  version = "3.2.50";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "06xiwcgk6hbcp6g1dpmxb95dzx94s29vzmh1pz4lsglcj1yfrkry";
+    sha256 = "0yg936syhay9x0qxqxdqrgi6ijdqklhqdrd8zk7l4zvgxaayaj68";
   };
 
   features.iwlwifi = true;
