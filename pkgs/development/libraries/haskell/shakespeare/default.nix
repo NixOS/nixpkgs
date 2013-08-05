0b93f032305f3579cc46fe2139a75621710995e0
Peter Simons <simons@cryp.to>
2013-08-05 10:19:57 +0200

    haskell-shakespeare: update to version 1.0.5.1

--- pkgs/development/libraries/haskell/shakespeare/default.nix
+++ pkgs/development/libraries/haskell/shakespeare/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "shakespeare";
-  version = "1.0.5";
-  sha256 = "1dc1yg35pxh45fv20fvnlpas0svqi18h6bdalpjaqjb164s114vf";
+  version = "1.0.5.1";
+  sha256 = "1qsg23jqv6lzwqk42yapqydx3fn6crkrzim8wr7ds55z6iblxbq6";
   buildDepends = [ parsec systemFileio systemFilepath text time ];
   testDepends = [
     hspec parsec systemFileio systemFilepath text time
