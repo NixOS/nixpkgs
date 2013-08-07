dbfcdc9066df81257097985ec7eff318dc6c803f
Moritz Ulrich <moritz@tarn-vedra.de>
2013-08-07 16:09:08 +0200

    Add meta for elixir & rebar.
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- pkgs/development/interpreters/elixir/default.nix
+++ pkgs/development/interpreters/elixir/default.nix
@@ -17,4 +17,19 @@ stdenv.mkDerivation {
       --replace '$(shell echo `pwd`/rebar)' ${rebar}/bin/rebar \
       --replace "/usr/local" $out
   '';
+
+  meta = {
+    homepage = "http://elixir-lang.org/";
+    description = "Elixir is a functional, meta-programming aware language built on top of the Erlang VM.";
+
+    longDescription = ''
+      Elixir is a functional, meta-programming
+      aware language built on top of the Erlang VM. It is a dynamic
+      language with flexible syntax and macro support that leverages
+      Erlang's abilities to build concurrent, distributed and
+      fault-tolerant applications with hot code upgrades.p
+    '';
+
+    platforms = stdenv.lib.platforms.linux;
+  };
 }
