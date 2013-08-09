0656eff7360e72ff03d3831f5bdcdf9107773076
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-08-09 16:31:39 +0200

    pixman: Update to 0.30.2

--- pkgs/development/libraries/pixman/default.nix
+++ pkgs/development/libraries/pixman/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, pkgconfig, perl, withPNG ? true, libpng, glib /*just passthru*/ }:
 
 stdenv.mkDerivation rec {
-  name = "pixman-0.28.2";
+  name = "pixman-0.30.2";
 
   src = fetchurl {
     url = "http://cairographics.org/releases/${name}.tar.gz";
-    sha256 = "0mcvxd5gx3w1wzgph91l2vaiic91jmx7s01hi2igphyvd80ckyia";
+    sha256 = "1sgnpx34pj3245a9v8056jddc4cg4xxkqdjvvw6k2hnprhh8k65x";
   };
 
   nativeBuildInputs = [ pkgconfig perl ];
