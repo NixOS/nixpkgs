a0cb696441d9085228db42cda765a39675e8217e
Peter Simons <simons@cryp.to>
2013-08-11 18:37:14 +0200

    haskell-hastache: update to version 0.5.1

--- pkgs/development/libraries/haskell/hastache/default.nix
+++ pkgs/development/libraries/haskell/hastache/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "hastache";
-  version = "0.5.0";
-  sha256 = "1c1pphw7qx5l5fdfqchihvp2yrwwb0ln8dfshkvd1giv8cjmbyn8";
+  version = "0.5.1";
+  sha256 = "05lm7mjzc1hamxcj8akq06081bhp907hrjdkhas3wzm6ran6rwn3";
   buildDepends = [
     blazeBuilder filepath ieee754 mtl syb text transformers utf8String
   ];
