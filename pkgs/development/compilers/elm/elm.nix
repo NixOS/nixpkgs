8a093803834666f6a213c29ab7ac7a24eb79ac6e
Peter Simons <simons@cryp.to>
2013-08-11 18:37:13 +0200

    haskell-Elm: update to version 0.9.0.1

--- pkgs/development/compilers/elm/elm.nix
+++ pkgs/development/compilers/elm/elm.nix
@@ -1,17 +1,16 @@
-{ cabal, blazeHtml, blazeMarkup, cmdargs, deepseq, filepath, hjsmin
-, indents, json, mtl, pandoc, parsec, shakespeare, text
-, transformers
+{ cabal, binary, blazeHtml, blazeMarkup, cmdargs, filepath, hjsmin
+, indents, mtl, pandoc, parsec, transformers, unionFind, uniplate
 }:
 
 cabal.mkDerivation (self: {
   pname = "Elm";
-  version = "0.8.0.3";
-  sha256 = "0zai8glmkiqramivgz405zh385cz166gpry2yl29g37dxpwxffzb";
+  version = "0.9.0.1";
+  sha256 = "0p6sqfrf11xpgj7y81hsjbvsyyyfvc4nzcg6gmfwyqkg3qc3yg6v";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
-    blazeHtml blazeMarkup cmdargs deepseq filepath hjsmin indents json
-    mtl pandoc parsec shakespeare text transformers
+    binary blazeHtml blazeMarkup cmdargs filepath hjsmin indents mtl
+    pandoc parsec transformers unionFind uniplate
   ];
   meta = {
     homepage = "http://elm-lang.org";
