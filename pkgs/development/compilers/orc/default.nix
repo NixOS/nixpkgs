094ffb2b08d20f1b20aa62dd87921dc3ed7cbb4e
Song Wenwu <iyzsong@gmail.com>
2013-08-11 11:00:45 +0800

    add orc 0.4.17

---
+++ pkgs/development/compilers/orc/default.nix
@@ -0,0 +1,16 @@
+{ stdenv, fetchurl }:
+
+stdenv.mkDerivation rec {
+  name = "orc-0.4.17";
+
+  src = fetchurl {
+    url = "http://code.entropywave.com/download/orc/${name}.tar.gz";
+    sha256 = "1s6psp8phrd1jmxz9j01cksh3q5xrm1bd3z7zqxg5zsrijjcrisg";
+  };
+
+  meta = {
+    description = "The Oil Runtime Compiler";
+    homepage = "http://code.entropywave.com/orc/";
+    platform = stdenv.lib.platforms.linux;
+  };
+}
