5060160634e565feda706921fb0537b921cffbc4
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-08-14 03:32:28 +0200

    not-detected.nix: Enable all firmware
    Note that since the configuration produced by nixos-hardware-scan
    includes not-detected.nix, you get all firmware when following the
    regular installation procedure.
    
    Issue #87.

--- modules/installer/scan/not-detected.nix
+++ modules/installer/scan/not-detected.nix
@@ -5,20 +5,5 @@
 with pkgs.lib;
 
 {
-  imports =
-    [
-      ../../hardware/network/intel-4965agn.nix
-      ../../hardware/network/intel-5000.nix
-      ../../hardware/network/intel-5150.nix
-      ../../hardware/network/intel-6000.nix
-      ../../hardware/network/intel-6000g2a.nix
-      ../../hardware/network/intel-6000g2b.nix
-      ../../hardware/network/broadcom-43xx.nix
-    ];
-
-  config = mkDefault {
-    # That wireless card firmware not enabled because the corresponding
-    # build expression 'rt73fw' is broken.
-    networking.enableRalinkFirmware = false;
-  };
+  hardware.enableAllFirmware = true;
 }
