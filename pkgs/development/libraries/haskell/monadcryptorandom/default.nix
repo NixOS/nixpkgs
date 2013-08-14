be4cb037ff97357cf0d47fe0147b649f22eee66c
Peter Simons <simons@cryp.to>
2013-08-14 11:51:54 +0200

    haskell-monadcryptorandom: fix build with recent versions of tagged

--- pkgs/development/libraries/haskell/monadcryptorandom/default.nix
+++ pkgs/development/libraries/haskell/monadcryptorandom/default.nix
@@ -1,10 +1,11 @@
-{ cabal, cryptoApi, mtl, tagged, transformers }:
+{ cabal, fetchurl, cryptoApi, mtl, tagged, transformers }:
 
 cabal.mkDerivation (self: {
   pname = "monadcryptorandom";
   version = "0.5.2";
   sha256 = "0a0qx331c1kvhmwwam7pbbrnq8ky3spfnw6zsz6rz7g1lk1hfawn";
   buildDepends = [ cryptoApi mtl tagged transformers ];
+  patches = [ (fetchurl { url = "https://github.com/TomMD/monadcryptorandom/pull/5.patch"; sha256 = "1mcil5w40gfy3hjnrpbgzciz65aygqpghgma0hckjb6xa694a71l"; }) ];
   meta = {
     homepage = "https://github.com/TomMD/monadcryptorandom";
     description = "A monad for using CryptoRandomGen";
