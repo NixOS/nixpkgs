f15af7c8e6e938ee54dc060b8c77deeb5e7f9fdf
Moritz Ulrich <moritz@tarn-vedra.de>
2013-08-14 23:37:17 +0200

    avra-1.3.0: Assember for the Atmel AVR microcontroller family
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2107,6 +2107,8 @@ let
 
   aspectj = callPackage ../development/compilers/aspectj { };
 
+  avra = callPackage ../development/compilers/avra { };
+
   bigloo = callPackage ../development/compilers/bigloo { };
 
   chicken = callPackage ../development/compilers/chicken { };
