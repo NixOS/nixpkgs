2272af9994b99abe39aff23f142a502c5731632b
Peter Simons <simons@cryp.to>
2013-08-14 11:52:02 +0200

    haskell-crypto-cipher-tests: update to version 0.0.2

--- pkgs/development/libraries/haskell/crypto-cipher-tests/default.nix
+++ pkgs/development/libraries/haskell/crypto-cipher-tests/default.nix
@@ -5,11 +5,15 @@
 
 cabal.mkDerivation (self: {
   pname = "crypto-cipher-tests";
-  version = "0.0.1";
-  sha256 = "1g5rxx07s23pr0vam0bjlp3qwxzqa3m6x6gbv88fhkmbqwxiq2fh";
+  version = "0.0.2";
+  sha256 = "1jzci2a6827jgiklj8sh7pjl7g4igk2j6mim20619i4rk6x0lhgz";
   buildDepends = [
     byteable cryptoCipherTypes HUnit mtl QuickCheck securemem
     testFramework testFrameworkHunit testFrameworkQuickcheck2
+  ];
+  testDepends = [
+    byteable cryptoCipherTypes HUnit mtl QuickCheck testFramework
+    testFrameworkHunit testFrameworkQuickcheck2
   ];
   meta = {
     homepage = "http://github.com/vincenthz/hs-crypto-cipher";
