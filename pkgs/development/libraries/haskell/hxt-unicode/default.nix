21e8b7f337599d8ae5c73997005dad441925ccb4
Peter Simons <simons@cryp.to>
2013-08-17 11:50:38 +0200

    haskell-hxt-unicode: update to version 9.0.2.1

--- pkgs/development/libraries/haskell/hxt-unicode/default.nix
+++ pkgs/development/libraries/haskell/hxt-unicode/default.nix
@@ -2,13 +2,13 @@
 
 cabal.mkDerivation (self: {
   pname = "hxt-unicode";
-  version = "9.0.2";
-  sha256 = "1ri3198j0bavgam861yiiisl43rh4pbkmji7g6v3jnnch7834hdd";
+  version = "9.0.2.1";
+  sha256 = "1ng3qaiwkaav1kmf0yxkm44887xphbx6slva3fskzx0sgkd1v0vp";
   buildDepends = [ hxtCharproperties ];
   meta = {
     homepage = "http://www.fh-wedel.de/~si/HXmlToolbox/index.html";
     description = "Unicode en-/decoding functions for utf8, iso-latin-* and other encodings";
-    license = "unknown";
+    license = self.stdenv.lib.licenses.mit;
     platforms = self.ghc.meta.platforms;
     maintainers = [ self.stdenv.lib.maintainers.andres ];
   };
