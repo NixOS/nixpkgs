273689bcbd798e70fabef9815ec3dd77b3401952
Mathijs Kwik <mathijs@bluescreen303.nl>
2013-08-17 21:10:30 +0200

    linux-3.10: remove the btrfs send patch
    it helps, but is incomplete.
    more fixes are coming, but including these would change too much
    generic btrfs code, which might cause trouble for others.
    
    so the best advice is not to use btrfs send yet and wait for 3.11 or 3.12

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6451,7 +6451,6 @@ let
     kernelPatches =
       [
         kernelPatches.sec_perm_2_6_24
-        kernelPatches.btrfs_send_backport
       ] ++ lib.optionals (platform.kernelArch == "mips")
       [ kernelPatches.mips_fpureg_emu
         kernelPatches.mips_fpu_sigill
