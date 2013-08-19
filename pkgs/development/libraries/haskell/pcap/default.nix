6d547d2888f3eb65cc308ff3d986749e8cfa5974
Peter Simons <simons@cryp.to>
2013-08-19 13:48:32 +0200

    haskell-pcap: re-generate expression with cabal2nix

--- pkgs/development/libraries/haskell/pcap/default.nix
+++ pkgs/development/libraries/haskell/pcap/default.nix
@@ -1,10 +1,11 @@
-{ cabal, network, time, libpcap }:
+{ cabal, libpcap, network, time }:
 
 cabal.mkDerivation (self: {
   pname = "pcap";
   version = "0.4.5.2";
   sha256 = "0pydw62qqw61sxfd8x9vvwgpgl3zp6mqv8rm4c825ymzyipjxsg7";
-  buildDepends = [ network time libpcap ];
+  buildDepends = [ network time ];
+  extraLibraries = [ libpcap ];
   meta = {
     homepage = "https://github.com/bos/pcap";
     description = "A system-independent interface for user-level packet capture";
