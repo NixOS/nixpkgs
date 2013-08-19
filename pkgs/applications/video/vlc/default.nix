a8ada47af142a41aab1bb725bbb07c3025a75331
Jonas Hoersch <coroa@online.de>
2013-08-19 16:10:56 +0200

    vlc: update to version 2.0.8

--- pkgs/applications/video/vlc/default.nix
+++ pkgs/applications/video/vlc/default.nix
@@ -10,17 +10,12 @@
 
 stdenv.mkDerivation rec {
   name = "vlc-${version}";
-  version = "2.0.7";
+  version = "2.0.8";
 
   src = fetchurl {
     url = "http://download.videolan.org/pub/videolan/vlc/${version}/${name}.tar.xz";
-    sha256 = "052kfkpd0r2fwkyz97qhz2a368xqxa905qacrd1bkl2bkvahfc94";
+    sha256 = "00hpbm0v424yhfzqyxrvrvfjkbvf3f43yqk6h1qhwmnl8n1z4am0";
   };
-
-  postPatch = /* flac 1.3.0 fix */ ''
-    sed -i -e 's:stream_decoder.h:FLAC/stream_decoder.h:' modules/codec/flac.c
-    sed -i -e 's:stream_encoder.h:FLAC/stream_encoder.h:' modules/codec/flac.c
-  '';
 
   buildInputs =
     [ xz bzip2 perl zlib a52dec libmad faad2 ffmpeg alsaLib libdvdnav libdvdnav.libdvdread
