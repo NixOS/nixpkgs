8216bc5bc4d8133c6e490130ef0e82244afc2215
Peter Simons <simons@cryp.to>
2013-08-21 10:18:58 +0200

    haskell-contravariant: update to version 0.4.4

--- pkgs/development/libraries/haskell/contravariant/default.nix
+++ pkgs/development/libraries/haskell/contravariant/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "contravariant";
-  version = "0.4.3";
-  sha256 = "1hhcsy5bshi2yx8618wxa40gax5wfapnbgdmv1acgjyxb6vbmsp6";
+  version = "0.4.4";
+  sha256 = "1b4422wpcyvrf4r7rr8c8wa008f5ykaxs17vr4plp91rcczr87s4";
   buildDepends = [ tagged transformers transformersCompat ];
   meta = {
     homepage = "http://github.com/ekmett/contravariant/";
