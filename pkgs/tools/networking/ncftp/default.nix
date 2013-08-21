d65cea26dc763d8e4b5ba59481841690097b2ff2
Bjørn Forsman <bjorn.forsman@gmail.com>
2013-08-21 20:22:23 +0200

    ncftp: 3.2.4 -> 3.2.5
    Also, add missing ncurses buildInput and add/update meta attributes. The
    homepage is back up!

--- pkgs/tools/networking/ncftp/default.nix
+++ pkgs/tools/networking/ncftp/default.nix
@@ -1,14 +1,15 @@
 {stdenv, fetchurl, ncurses, coreutils}:
 
-let version = "3.2.4"; in
+let version = "3.2.5"; in
 stdenv.mkDerivation {
   name = "ncftp-${version}";
 
   src = fetchurl {
-    # `ncftp.com' got stolen, apparently, so resort to Debian.
-    url = "mirror://debian/pool/main/n/ncftp/ncftp_${version}.orig.tar.gz";
-    sha256 = "6f26e7891f3eab27eebd2bbbe2bc87d5ae872e610eaf0bc5652aec520adcf68a";
+    url = "ftp://ftp.ncftp.com/ncftp/ncftp-${version}-src.tar.bz2";
+    sha256 = "0hlx12i0lwi99qsrx7nccf4nvwjj2gych4yks5y179b1ax0y5sxl";
   };
+
+  buildInputs = [ ncurses ];
 
   preConfigure = ''
     find . -name "*.sh" -type f | xargs sed 's@/bin/ls@${coreutils}/bin/ls@g' -i
@@ -22,10 +23,10 @@ stdenv.mkDerivation {
     sed 's@/bin/rm@${coreutils}/bin/rm@g' -i configure
   '';
 
-  meta = {
-    description = "NcFTP Client (also known as just NcFTP) is a set of FREE application programs implementing the File Transfer Protocol (FTP).";
-
-    # Homeless!
-    # homepage = http://www.ncftp.com/ncftp/;
+  meta = with stdenv.lib; {
+    description = "Command line FTP (File Transfer Protocol) client";
+    homepage = http://www.ncftp.com/ncftp/;
+    platforms = platforms.linux;
+    maintainers = [ maintainers.bjornfor ];
   };
 }
