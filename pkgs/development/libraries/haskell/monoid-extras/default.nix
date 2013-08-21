fb720f8caadab5e5652d81e902f6f12eaf93517e
Peter Simons <simons@cryp.to>
2013-08-21 10:18:59 +0200

    haskell-monoid-extras: update to version 0.3.1.0

--- pkgs/development/libraries/haskell/monoid-extras/default.nix
+++ pkgs/development/libraries/haskell/monoid-extras/default.nix
@@ -1,10 +1,10 @@
-{ cabal, semigroups }:
+{ cabal, semigroupoids, semigroups }:
 
 cabal.mkDerivation (self: {
   pname = "monoid-extras";
-  version = "0.3.0.0";
-  sha256 = "1bb8yq2vja80177h3wfadkjkwvcrszx0nq6m5n10f4lh9spvr087";
-  buildDepends = [ semigroups ];
+  version = "0.3.1.0";
+  sha256 = "0nxdkx0j67nb41ingp1yl0appfx64ikf5709f48469qbaf3pgax3";
+  buildDepends = [ semigroupoids semigroups ];
   meta = {
     description = "Various extra monoid-related definitions and utilities";
     license = self.stdenv.lib.licenses.bsd3;
