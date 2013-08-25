946f219a32c8e776f10e698144427c0a2879d14a
Mathijs Kwik <mathijs@bluescreen303.nl>
2013-08-25 23:41:15 +0200

    calibre: really fix building on master
    It turns out I hardcoded the output path that qt's tarball extracts.
    But that path is versioned (4.8.5 for example).
    As I've already merged x-updates on my own system, my qt version was
    different (4.8.4 vs 4.8.5). Made the path-guessing more flexible, so
    now it should work with any 4.8.*

--- pkgs/applications/misc/calibre/default.nix
+++ pkgs/applications/misc/calibre/default.nix
@@ -18,8 +18,9 @@ stdenv.mkDerivation rec {
 
   patchPhase = ''
     tar xf ${qt48.src}
+    qtdir=$(realpath $(ls | grep qt | grep 4.8 | grep src))
     sed -i setup/build_environment.py \
-        -e "s|^qt_private_inc = .*|qt_private_inc = ['../qt-everywhere-opensource-src-4.8.5/include/%s'%(m) for m in ('QtGui', 'QtCore')]|"
+        -e "s|^qt_private_inc = .*|qt_private_inc = ['$qtdir/include/%s\'%(m) for m in ('QtGui', 'QtCore')]|"
   '';
 
   buildInputs =
