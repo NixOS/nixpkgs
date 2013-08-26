1f60a7c4bcdbab38260681c77c007d28793d4e4d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-08-26 14:50:45 +0200

    less: Update to 458
    For some crazy reason, less is a stdenv dependency.

--- pkgs/tools/misc/less/default.nix
+++ pkgs/tools/misc/less/default.nix
@@ -1,13 +1,22 @@
-{stdenv, fetchurl, ncurses}:
- 
-stdenv.mkDerivation {
-  name = "less-451";
- 
+{ stdenv, fetchurl, ncurses }:
+
+stdenv.mkDerivation rec {
+  name = "less-458";
+
   src = fetchurl {
-    url = http://www.greenwoodsoftware.com/less/less-451.tar.gz;
-    sha256 = "9fe8287c647afeafb4149c5dedaeacfd20971ed7c26c7553794bb750536b5f57";
+    url = "http://www.greenwoodsoftware.com/less/${name}.tar.gz";
+    sha256 = "1b7wn1nk8qlzx20jmn9l6zcbw81n9g0w9zzhhzab6m6yks0wfdp5";
   };
- 
-  buildInputs = [ncurses];
- 
+
+  # Look for ‘sysless’ in /etc.
+  configureFlags = "--sysconfdir=/etc";
+
+  buildInputs = [ ncurses ];
+
+  meta = {
+    homepage = http://www.greenwoodsoftware.com/less/;
+    description = "A more advanced file pager than ‘more’";
+    platforms = stdenv.lib.platforms.linux;
+    maintainers = [ stdenv.lib.maintainers.eelco ];
+  };
 }
