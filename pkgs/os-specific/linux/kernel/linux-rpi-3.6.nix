c44d8970ea61f898d59d44ed0c78bd8b3a049daa
Rickard Nilsson <rickynils@gmail.com>
2013-08-27 12:05:13 +0200

    raspberrypi-kernel: Updated to latest version as of 20130826 (rev 7849605f5a)

--- pkgs/os-specific/linux/kernel/linux-rpi-3.6.nix
+++ pkgs/os-specific/linux/kernel/linux-rpi-3.6.nix
@@ -1,14 +1,16 @@
 { stdenv, fetchurl, ... } @ args:
 
-let rev = "91a3be5b2b"; in
+let
 
-import ./generic.nix (args // rec {
+  rev = "7849605f5a";
+
+in import ./generic.nix (args // rec {
   version = "3.6.y-${rev}";
 
   src = fetchurl {
     url = "https://api.github.com/repos/raspberrypi/linux/tarball/${rev}";
     name = "linux-raspberrypi-${version}.tar.gz";
-    sha256 = "04370b1da7610622372940decdc13ddbba2a58c9da3c3bd3e7df930a399f140d";
+    sha256 = "1diwc5p6az6ipcldwmkq7hb5f15nvdgwzmypixc2vmzmc4ylarxl";
   };
 
   features.iwlwifi = true;
