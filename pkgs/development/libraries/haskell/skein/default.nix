6ac5f660650d8f488d26cef2363a715f2a7d4517
Peter Simons <simons@cryp.to>
2013-08-29 10:48:45 +0200

    haskell-skein: update to version 1.0.6

--- pkgs/development/libraries/haskell/skein/default.nix
+++ pkgs/development/libraries/haskell/skein/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "skein";
-  version = "1.0.3";
-  sha256 = "15vzydywhwjdgybabvv6lfk1vjs7blvs3k2apwxjdjh2q7jmgkam";
+  version = "1.0.6";
+  sha256 = "0jdh618k66bhiwrxb9i2yckxz0w3cpc7q15ilz49lqjjpl86bndk";
   buildDepends = [ cereal cryptoApi tagged ];
   testDepends = [ cereal cryptoApi filepath hspec tagged ];
   jailbreak = true;
