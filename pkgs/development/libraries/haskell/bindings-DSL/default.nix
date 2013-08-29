96628bff7a0fd4c34e939c6bcae609964848fc02
Peter Simons <simons@cryp.to>
2013-08-29 10:48:45 +0200

    haskell-bindings-DSL: update to version 1.0.17

--- pkgs/development/libraries/haskell/bindings-DSL/default.nix
+++ pkgs/development/libraries/haskell/bindings-DSL/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "bindings-DSL";
-  version = "1.0.16";
-  sha256 = "1sly88585f94dsnhyw6nagnr4jfjixnn61my85x05987flf325px";
+  version = "1.0.17";
+  sha256 = "1203n6wzdp21hd7zdhvhppxhkz4xr3qykwkb8j5mb2s4kijx01bn";
   meta = {
     homepage = "http://bitbucket.org/mauricio/bindings-dsl";
     description = "FFI domain specific language, on top of hsc2hs";
