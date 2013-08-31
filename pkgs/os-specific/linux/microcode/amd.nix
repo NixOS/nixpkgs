9dc7678852f2b0c1a0f2194066c34158ffbb3aae
danbst <abcz2.uprola@gmail.com>
2013-08-31 21:57:19 +0200

    Added mirrot url for AMD microcode

--- pkgs/os-specific/linux/microcode/amd.nix
+++ pkgs/os-specific/linux/microcode/amd.nix
@@ -5,7 +5,13 @@ stdenv.mkDerivation rec {
   name = "amd-ucode-2012-09-10";
 
   src = fetchurl {
-    url = "http://www.amd64.org/pub/microcode/${name}.tar";
+    #url = "http://www.amd64.org/pub/microcode/${name}.tar"; # www.amd64.org is often down
+    #url="http://pkgs.fedoraproject.org/repo/pkgs/microcode_ctl/amd-ucode-2012-09-10.tar/559bc355d3799538584add80df2996f0/${name}.tar";
+
+    urls = [ "http://www.amd64.org/pub/microcode/${name}.tar"
+             "http://pkgs.fedoraproject.org/repo/pkgs/microcode_ctl/amd-ucode-2012-09-10.tar/559bc355d3799538584add80df2996f0/${name}.tar"
+           ];
+
     sha256 = "065phvhx5hx5ssdd1x2p5m1yv26ak7l5aaw6yk6h95x9mxn5r111";
   };
 
