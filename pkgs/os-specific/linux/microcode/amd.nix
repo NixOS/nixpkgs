2a798997a3189f86f13e60a057381e54e7c02250
danbst <abcz2.uprola@gmail.com>
2013-08-31 21:57:19 +0200

    Add mirror url for AMD microcode
    www.amd64.org is often down, so provide a 2nd url to download from.

--- pkgs/os-specific/linux/microcode/amd.nix
+++ pkgs/os-specific/linux/microcode/amd.nix
@@ -5,7 +5,10 @@ stdenv.mkDerivation rec {
   name = "amd-ucode-2012-09-10";
 
   src = fetchurl {
-    url = "http://www.amd64.org/pub/microcode/${name}.tar";
+    urls =
+      [ "http://www.amd64.org/pub/microcode/${name}.tar"
+        "http://pkgs.fedoraproject.org/repo/pkgs/microcode_ctl/${name}.tar/559bc355d3799538584add80df2996f0/${name}.tar"
+      ];
     sha256 = "065phvhx5hx5ssdd1x2p5m1yv26ak7l5aaw6yk6h95x9mxn5r111";
   };
 
