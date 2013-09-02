jcumming/nixos

fixiup zfs binaries in initrd
Previously, the zfs binaries were put in $out/sbin where the stage-1
patchelf wouldn't fix them up. This would fail the allowedReferences
test.

Move the zfs binaries to $out/bin.

Add a few config.boot.initrd.extraUtilsCommandsTest to make sure the
binaries didn't get cooked.

modules/tasks/filesystems/zfs.nix
@@ -52,10 +52,20 @@ in
       kernelModules = [ "spl" "zfs" ] ;
       extraUtilsCommands =
         ''
-          cp -v ${kernel.zfs}/sbin/zfs $out/sbin
-          cp -v ${kernel.zfs}/sbin/zdb $out/sbin
-          cp -v ${kernel.zfs}/sbin/zpool $out/sbin
+          cp -v ${kernel.zfs}/bin/zfs $out/sbin
+          cp -v ${kernel.zfs}/bin/zdb $out/sbin
+          cp -v ${kernel.zfs}/bin/zpool $out/sbin
+          cp -pdv ${kernel.zfs}/lib/lib*.so.* $out/lib
+          cp -pdv ${pkgs.zlib}/lib/lib*.so.* $out/lib
         '';
+
+      extraUtilsCommandsTest = 
+        ''
+          zfs -?
+          zpool -?
+          zdb -?
+        '';
+
       postDeviceCommands =
         ''
           zpool import -f -a -d /dev

NixOS/nixpkgs

modules/tasks/filesystems/zfs.nix
@@ -52,9 +52,9 @@ in
       kernelModules = [ "spl" "zfs" ] ;
       extraUtilsCommands =
         ''
-          cp -v ${kernel.zfs}/sbin/zfs $out/sbin
-          cp -v ${kernel.zfs}/sbin/zdb $out/sbin
-          cp -v ${kernel.zfs}/sbin/zpool $out/sbin
+          cp -v ${kernel.zfs}/sbin/zfs $out/bin
+          cp -v ${kernel.zfs}/sbin/zdb $out/bin
+          cp -v ${kernel.zfs}/sbin/zpool $out/bin
         '';
       postDeviceCommands =
         ''