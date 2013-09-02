bd8c82b76dafbd000d4c2b41e0fcad1b9f1e98c3
Peter Simons <simons@cryp.to>
2013-09-02 13:20:16 +0200

    haskell-hoogle: update to version 4.2.21

--- pkgs/development/libraries/haskell/hoogle/default.nix
+++ pkgs/development/libraries/haskell/hoogle/default.nix
@@ -7,8 +7,8 @@
 
 cabal.mkDerivation (self: {
   pname = "hoogle";
-  version = "4.2.20";
-  sha256 = "0sff230qc9lk3kqr9azg399fsaybwqpic9pj52jyw61ffasnl2dd";
+  version = "4.2.21";
+  sha256 = "167iw0rp37c1bixmaa5l06c943h33b457symllh8rcbmf880z09i";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
