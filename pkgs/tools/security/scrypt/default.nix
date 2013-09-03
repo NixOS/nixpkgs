a1bb7c1cfa3553af8477c849b59f83981b952a38
Ricardo M. Correia <rcorreia@wizy.org>
2013-09-03 01:49:08 +0000

    scrypt: New package

---
+++ pkgs/tools/security/scrypt/default.nix
@@ -0,0 +1,20 @@
+{ stdenv, fetchurl, openssl }:
+
+stdenv.mkDerivation rec {
+  name = "scrypt-${version}";
+  version = "1.1.6";
+
+  src = fetchurl {
+    url = "https://www.tarsnap.com/scrypt/scrypt-1.1.6.tgz";
+    sha256 = "dfd0d1a544439265bbb9b58043ad3c8ce50a3987b44a61b1d39fd7a3ed5b7fb8";
+  };
+
+  buildInputs = [ openssl ];
+
+  meta = {
+    description = "The scrypt encryption utility";
+    homepage = https://www.tarsnap.com/scrypt.html;
+    license = "BSD";
+    platforms = stdenv.lib.platforms.all;
+  };
+}
