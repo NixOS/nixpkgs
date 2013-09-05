847b9ec38eadde7f5f1292111be29c9abfbe92c1
Edward Tjörnhammar <ed@cflags.cc>
2013-09-05 19:03:28 +0200

    Added abook 0.6.0pre2
    Abook is a text-based addressbook program designed to use with mutt mail
    client.

---
+++ pkgs/applications/misc/abook/default.nix
@@ -0,0 +1,21 @@
+{ stdenv, fetchurl, pkgconfig, ncurses, readline }:
+
+let version = "0.6.0pre2"; in
+stdenv.mkDerivation rec {
+  name = "abook-${version}";
+
+  src = fetchurl {
+    url = "http://abook.sourceforge.net/devel/${name}.tar.gz";
+    sha256 = "59d444504109dd96816e003b3023175981ae179af479349c34fa70bc12f6d385";
+  };
+
+  buildInputs = [ pkgconfig ncurses readline ];
+
+  meta = {
+    homepage = "http://abook.sourceforge.net/";
+    description = "Abook is a text-based addressbook program designed to use with mutt mail client.";
+    license = "GPLv2";
+    maintainers = [ stdenv.lib.maintainers.edwtjo ];
+    platforms = with stdenv.lib.platforms; linux;
+  };
+}
