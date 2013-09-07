523ac0d82b523360159f47bb7ad34bfc02d46209
Jason "Don" O'Conal <lovek323@gmail.com>
2013-09-07 10:49:38 +1000

    fail2ban: fix build on darwin

--- pkgs/tools/security/fail2ban/default.nix
+++ pkgs/tools/security/fail2ban/default.nix
@@ -7,47 +7,46 @@ pythonPackages.buildPythonPackage {
   namePrefix = "";
 
   src = fetchurl {
-    url = "https://github.com/fail2ban/fail2ban/zipball/${version}";
-    name = "fail2ban-${version}.zip";
-    sha256 = "1linfz5qxmm4225lzi9vawsa79y41d3rcdahvrzlyqlhb02ipd55";
+    url    = "https://github.com/fail2ban/fail2ban/zipball/${version}";
+    name   = "fail2ban-${version}.zip";
+    sha256 = "0lbanfshr8kasa1bb7861w3mrm2d0c1bvv4s5703265s8zp5m284";
   };
 
   buildInputs = [ unzip ];
 
   pythonPath = [ gamin ];
 
-  preConfigure =
-    ''
-      substituteInPlace setup.cfg \
-        --replace /usr $out
+  preConfigure = ''
+    substituteInPlace setup.cfg \
+      --replace /usr $out
 
-      substituteInPlace setup.py \
-        --replace /etc $out/etc \
-        --replace /var $TMPDIR/var \
+    substituteInPlace setup.py \
+      --replace /etc $out/etc \
+      --replace /var $TMPDIR/var \
 
-      for i in fail2ban-client fail2ban-regex fail2ban-server; do
-        substituteInPlace $i \
-          --replace /usr/share/fail2ban $out/share/fail2ban
-      done
-      
-      for i in config/action.d/sendmail*.conf; do
-        substituteInPlace $i \
-          --replace /usr/sbin/sendmail sendmail \
-          --replace /usr/bin/whois whois
-      done
-    '';
+    for i in fail2ban-client fail2ban-regex fail2ban-server; do
+      substituteInPlace $i \
+        --replace /usr/share/fail2ban $out/share/fail2ban
+    done
+    
+    for i in config/action.d/sendmail*.conf; do
+      substituteInPlace $i \
+        --replace /usr/sbin/sendmail sendmail \
+        --replace /usr/bin/whois whois
+    done
+  '';
 
   doCheck = false;
   
-  installCommand =
-    ''
-      python setup.py install --prefix=$out
-    '';
+  installCommand = ''
+    python setup.py install --prefix=$out
+  '';
 
-  meta = {
-    homepage = http://www.fail2ban.org/;
+  meta = with stdenv.lib; {
+    homepage    = http://www.fail2ban.org/;
     description = "A program that scans log files for repeated failing login attempts and bans IP addresses";
-    license = stdenv.lib.licenses.gpl2Plus;
-    maintainers = [ stdenv.lib.maintainers.eelco ];
+    license     = licenses.gpl2Plus;
+    maintainers = with maintainers; [ eelco lovek323 ];
+    platforms   = platforms.unix;
   };
 }
