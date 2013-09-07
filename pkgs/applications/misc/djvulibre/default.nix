c2bc80f25d245ea4a7dc8f31640b8c8b2cec2020
Jason "Don" O'Conal <lovek323@gmail.com>
2013-09-07 10:09:11 +1000

    djvulibre: fix build on darwin

--- pkgs/applications/misc/djvulibre/default.nix
+++ pkgs/applications/misc/djvulibre/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, libjpeg, libtiff, librsvg }:
+{ stdenv, fetchurl, libjpeg, libtiff, librsvg, libintlOrEmpty }:
 
 stdenv.mkDerivation rec {
   name = "djvulibre-3.5.25.3";
@@ -8,7 +8,9 @@ stdenv.mkDerivation rec {
     sha256 = "1q5i5ha4zmj2ahjfhi8cv1rah80vm43m9ads46ji38rgvpb7x3c9";
   };
 
-  buildInputs = [ libjpeg libtiff librsvg ];
+  buildInputs = [ libjpeg libtiff librsvg ] ++ libintlOrEmpty;
+
+  NIX_LDFLAGS = stdenv.lib.optionalString stdenv.isDarwin "-lintl";
 
   meta = {
     description = "A library and viewer for the DJVU file format for scanned images";
