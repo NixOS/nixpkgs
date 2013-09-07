9c1cbd4dcd41aa6ee9561e8a8bff63b78fb9535d
Sergey Mironov <grrwlf@gmail.com>
2013-09-08 02:03:44 +0400

    xfce4-power-manager: fix an brightness plugin issue

--- pkgs/desktops/xfce/core/xfce4-power-manager.nix
+++ pkgs/desktops/xfce/core/xfce4-power-manager.nix
@@ -10,6 +10,12 @@ stdenv.mkDerivation rec {
     url = "mirror://xfce/src/xfce/${p_name}/${ver_maj}/${name}.tar.bz2";
     sha256 = "1sc4f4wci5yl3l9lk7vcsbwj6hdjshbxw9qm43s64jr882jriyyp";
   };
+
+  brightness_patch = fetchurl {
+    url = "http://git.xfce.org/xfce/xfce4-power-manager/patch/?id=05d12e12596512f7a31d3cdb4845a69dc2d4c611";
+    sha256 = "0rbldvjwpj93hx59xrmvbdql1pgkbqzjh4vp6gkavn4z6sv535v8";
+  };
+
   name = "${p_name}-${ver_maj}.${ver_min}";
 
   buildInputs =
@@ -17,6 +23,8 @@ stdenv.mkDerivation rec {
       libnotify xfce4panel
     ];
   preFixup = "rm $out/share/icons/hicolor/icon-theme.cache";
+
+  patches = [ brightness_patch ];
 
   meta = {
     homepage = http://goodies.xfce.org/projects/applications/xfce4-power-manager;
