a0ab379c00beea50d76547a1d2e00cd87fa06183
Oliver Charles <ollie@ocharles.org.uk>
2013-09-08 12:40:37 +0100

    pipes: Add Haskell 'pipes' family of libraries

--- pkgs/development/libraries/haskell/pipes-concurrency/default.nix
+++ pkgs/development/libraries/haskell/pipes-concurrency/default.nix
@@ -1,13 +1,15 @@
-{ cabal, pipes, stm }:
+{ cabal, async, pipes, stm }:
 
 cabal.mkDerivation (self: {
   pname = "pipes-concurrency";
-  version = "1.2.1";
-  sha256 = "036cn6pafqpf2811iigablks3zk747bnzji9ykrgwhpja427vlbl";
+  version = "2.0.0";
+  sha256 = "1f9l6qlaf8dyldzwaavj3k5akm74ycga5j173ypdna3pv0jbzfrk";
   buildDepends = [ pipes stm ];
+  testDepends = [ async pipes stm ];
   meta = {
     description = "Concurrency for the pipes ecosystem";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    maintainers = [ self.stdenv.lib.maintainers.ocharles ];
   };
 })
