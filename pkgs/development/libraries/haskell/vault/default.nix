b2a54d51b555ccaa4c4f27bdefe8db62462c395b
Peter Simons <simons@cryp.to>
2013-09-09 10:20:27 +0200

    haskell-vault: jailbreak to fix build with older versions of GHC

--- pkgs/development/libraries/haskell/vault/default.nix
+++ pkgs/development/libraries/haskell/vault/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.3.0.0";
   sha256 = "1lvv2sw5j48jbxniw55bxq88dhn46l7lk0blv2cnaf1vw6wms4m8";
   buildDepends = [ hashable unorderedContainers ];
+  jailbreak = true;
   meta = {
     homepage = "https://github.com/HeinrichApfelmus/vault";
     description = "a persistent store for values of arbitrary types";
