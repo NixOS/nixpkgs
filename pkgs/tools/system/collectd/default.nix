eac934c83affc067d46e0f9c6640210ebead1df4
Rob Vermaas <rob.vermaas@gmail.com>
2013-09-09 13:05:50 +0200

    Fix collectd, couldn't run due to missing pthread_cancel.

--- pkgs/tools/system/collectd/default.nix
+++ pkgs/tools/system/collectd/default.nix
@@ -8,6 +8,8 @@ stdenv.mkDerivation rec {
     sha256 = "0gljf5c60q6i0nrii6addxy1p76qqixww8zy17a7a1zil6a3i5wh";
   };
 
+  NIX_LDFLAGS = "-lgcc_s"; # for pthread_cancel
+
   meta = {
     homepage = http://collectd.org;
     description = "collectd is a daemon which collects system performance statistics periodically";
