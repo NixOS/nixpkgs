a1cca8226aa9d86229f8302cda255909abb54111
Peter Simons <simons@cryp.to>
2013-09-10 11:46:55 +0200

    happy: add version 1.18.11

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -176,7 +176,7 @@ let result = let callPackage = x : y : modifyPrio (newScope result.finalReturn x
     cabalInstall = self.cabalInstall_1_18_0_1;
     alex         = self.alex_3_0_5;
     haddock      = self.haddock_2_13_2;
-    happy        = self.happy_1_18_10;
+    happy        = self.happy_1_18_11;
     primitive    = self.primitive_0_5_0_1;      # semi-official, but specified
   };
 
@@ -2343,7 +2343,8 @@ let result = let callPackage = x : y : modifyPrio (newScope result.finalReturn x
   happy_1_18_8 = callPackage ../development/tools/parsing/happy/1.18.8.nix {};
   happy_1_18_9 = callPackage ../development/tools/parsing/happy/1.18.9.nix {};
   happy_1_18_10 = callPackage ../development/tools/parsing/happy/1.18.10.nix {};
-  happy = self.happy_1_18_10;
+  happy_1_18_11 = callPackage ../development/tools/parsing/happy/1.18.11.nix {};
+  happy = self.happy_1_18_11;
 
   happyMeta = callPackage ../development/tools/haskell/happy-meta {};
 
