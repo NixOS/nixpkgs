39043ce87e5839fcdae9906f4cccf06747570825
Peter Simons <simons@cryp.to>
2013-09-11 20:43:04 +0200

    yap: add 'zlib' and 'gmp' build inputs to enable additional features

--- pkgs/development/compilers/yap/default.nix
+++ pkgs/development/compilers/yap/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, readline }:
+{ stdenv, fetchurl, readline, gmp, zlib }:
 
 stdenv.mkDerivation rec {
   version = "6.2.2";
@@ -9,11 +9,14 @@ stdenv.mkDerivation rec {
     sha256 = "0l6p0vy667wws64cvwf74ssl6h9gypjzrsl3b2d32hs422186pzi";
   };
 
-  buildInputs = [ readline ];
+  buildInputs = [ readline gmp zlib ];
 
-  meta = { 
+  meta = {
+    homepage = "http://www.dcc.fc.up.pt/~vsc/Yap/";
     description = "Yap Prolog System is a ISO-compatible high-performance Prolog compiler";
-    homepage = http://yap.sourceforge.net/;
     license = "artistic";
+
+    maintainers = [ stdenv.lib.maintainers.simons ];
+    platforms = stdenv.lib.platforms.linux;
   };
 }
