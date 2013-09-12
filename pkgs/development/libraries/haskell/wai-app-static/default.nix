535819ccd3f919d2e4e03538bf9627a78dd48631
Peter Simons <simons@cryp.to>
2013-09-12 18:11:43 +0200

    haskell-wai-app-static: update to version 1.3.2.1

--- pkgs/development/libraries/haskell/wai-app-static/default.nix
+++ pkgs/development/libraries/haskell/wai-app-static/default.nix
@@ -1,23 +1,23 @@
 { cabal, base64Bytestring, blazeBuilder, blazeHtml, blazeMarkup
 , cereal, cryptoApi, cryptoConduit, cryptohashCryptoapi, fileEmbed
 , hspec, httpDate, httpTypes, mimeTypes, network, systemFileio
-, systemFilepath, text, time, transformers, unixCompat, wai
-, waiTest
+, systemFilepath, text, time, transformers, unixCompat
+, unorderedContainers, wai, waiTest, zlib
 }:
 
 cabal.mkDerivation (self: {
   pname = "wai-app-static";
-  version = "1.3.1.4";
-  sha256 = "1457643xkigqnacg1fw25jp9kjqiy55d22ll8fml07bxs37hlr63";
+  version = "1.3.2.1";
+  sha256 = "1iw2b53p08c38fdh3d0js9j8lyy0i8qszp3jd736kzxxiig6ah79";
   buildDepends = [
     base64Bytestring blazeBuilder blazeHtml blazeMarkup cereal
     cryptoApi cryptoConduit cryptohashCryptoapi fileEmbed httpDate
     httpTypes mimeTypes systemFileio systemFilepath text time
-    transformers unixCompat wai
+    transformers unixCompat unorderedContainers wai zlib
   ];
   testDepends = [
     hspec httpDate httpTypes mimeTypes network text time transformers
-    unixCompat wai waiTest
+    unixCompat wai waiTest zlib
   ];
   meta = {
     homepage = "http://www.yesodweb.com/book/web-application-interface";
