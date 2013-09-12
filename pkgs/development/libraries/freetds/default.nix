6c2d4f98627ee6104e8990a1481e7a3df1e893a2
Vladimir Still <xstill@fi.muni.cz>
2013-09-12 20:54:11 +0200

    freetds: Update expression with working url.

--- pkgs/development/libraries/freetds/default.nix
+++ pkgs/development/libraries/freetds/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "freetds-0.91";
 
   src = fetchurl {
-    url = ftp://ftp.ibiblio.org/pub/Linux/ALPHA/freetds/stable/freetds-stable.tgz;
+    url = ftp://ftp.astron.com/pub/freetds/stable/freetds-stable.tgz;
     sha256 = "0r946axzxs0czsmr7283w7vmk5jx3jnxxc32d2ncxsrsh2yli0ba";
   };
 
