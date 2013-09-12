38bd86b4240564500042f17627580a7b2bfb4ea8
Peter Simons <simons@cryp.to>
2013-09-12 18:11:43 +0200

    haskell-postgresql-simple: update to version 0.3.7.1

--- pkgs/development/libraries/haskell/postgresql-simple/default.nix
+++ pkgs/development/libraries/haskell/postgresql-simple/default.nix
@@ -1,15 +1,15 @@
-{ cabal, attoparsec, base16Bytestring, blazeBuilder, blazeTextual
-, cryptohash, HUnit, postgresqlLibpq, text, time, transformers
-, vector
+{ cabal, aeson, attoparsec, base16Bytestring, blazeBuilder
+, blazeTextual, cryptohash, HUnit, postgresqlLibpq, text, time
+, transformers, vector
 }:
 
 cabal.mkDerivation (self: {
   pname = "postgresql-simple";
-  version = "0.3.6.0";
-  sha256 = "1qszr3k7cihizbaq3naj134gavkpamk8q3g02rsilzvn0ivq8wb8";
+  version = "0.3.7.1";
+  sha256 = "1xrgwpg58srmzv1d0jdknyh5vwdq2c40fyqy0wvgppisxzq469wh";
   buildDepends = [
-    attoparsec blazeBuilder blazeTextual postgresqlLibpq text time
-    transformers vector
+    aeson attoparsec blazeBuilder blazeTextual postgresqlLibpq text
+    time transformers vector
   ];
   testDepends = [
     base16Bytestring cryptohash HUnit text time vector
