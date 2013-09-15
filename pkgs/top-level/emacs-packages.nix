10bed918eb01d7fc6b7283eefa0e1152d20c0086
Moritz Ulrich <moritz@tarn-vedra.de>
2013-09-15 16:45:04 +0200

    Use dash-notation instead of camelCase in emacsPackages.otherPackages.
    Package.el's convention is haskell-mode, not haskellMode. Rename the
    'manually' managed packages in Nix so we don't have duplicates in case
    a package is in package.el and in `otherPackages' (in which case the
    newer version will be picked automatically).
    
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- pkgs/top-level/emacs-packages.nix
+++ pkgs/top-level/emacs-packages.nix
@@ -37,23 +37,23 @@ in rec {
   # Legacy emacs packages, not yet built via package.el or newer
   # version
   otherPackages = {
-    autoComplete = callPackage ../applications/editors/emacs-modes/auto-complete { };
+    auto-complete = callPackage ../applications/editors/emacs-modes/auto-complete { };
     bbdb = callPackage ../applications/editors/emacs-modes/bbdb { texinfo = pkgs.texinfo5; };
     cedet = callPackage ../applications/editors/emacs-modes/cedet { python = pkgs.python; };
     calfw = callPackage ../applications/editors/emacs-modes/calfw { };
     coffee = callPackage ../applications/editors/emacs-modes/coffee { };
-    colorTheme = callPackage ../applications/editors/emacs-modes/color-theme { };
+    color-theme = callPackage ../applications/editors/emacs-modes/color-theme { };
     cua = callPackage ../applications/editors/emacs-modes/cua { };
     # ecb = callPackage ../applications/editors/emacs-modes/ecb { };
     jabber = callPackage ../applications/editors/emacs-modes/jabber { };
     emacsClangCompleteAsync = callPackage ../applications/editors/emacs-modes/emacs-clang-complete-async { };
     emacsSessionManagement = callPackage ../applications/editors/emacs-modes/session-management-for-emacs { };
-    emacsw3m = callPackage ../applications/editors/emacs-modes/emacs-w3m { texinfo = pkgs.texinfo5; };
+    emacs-w3m = callPackage ../applications/editors/emacs-modes/emacs-w3m { texinfo = pkgs.texinfo5; };
     emms = callPackage ../applications/editors/emacs-modes/emms { texinfo = pkgs.texinfo5; };
     ess = callPackage ../applications/editors/emacs-modes/ess { };
-    flymakeCursor = callPackage ../applications/editors/emacs-modes/flymake-cursor { };
+    flymake-cursor = callPackage ../applications/editors/emacs-modes/flymake-cursor { };
     gh = callPackage ../applications/editors/emacs-modes/gh { };
-    graphvizDot = callPackage ../applications/editors/emacs-modes/graphviz-dot { };
+    graphviz-dot-mode = callPackage ../applications/editors/emacs-modes/graphviz-dot { };
     gist = callPackage ../applications/editors/emacs-modes/gist { };
     jade = callPackage ../applications/editors/emacs-modes/jade { };
     jdee = callPackage ../applications/editors/emacs-modes/jdee {
@@ -61,32 +61,32 @@ in rec {
     };
     js2 = callPackage ../applications/editors/emacs-modes/js2 { };
     stratego = callPackage ../applications/editors/emacs-modes/stratego { };
-    haskellMode = callPackage ../applications/editors/emacs-modes/haskell { };
+    haskell-mode = callPackage ../applications/editors/emacs-modes/haskell { };
     ocamlMode = callPackage ../applications/editors/emacs-modes/ocaml { };
-    tuaregMode = callPackage ../applications/editors/emacs-modes/tuareg { };
+    tuareg = callPackage ../applications/editors/emacs-modes/tuareg { };
     hol_light_mode = callPackage ../applications/editors/emacs-modes/hol_light { };
     htmlize = callPackage ../applications/editors/emacs-modes/htmlize { };
     logito = callPackage ../applications/editors/emacs-modes/logito { };
-    loremIpsum = callPackage ../applications/editors/emacs-modes/lorem-ipsum { };
+    lorem-ipsum = callPackage ../applications/editors/emacs-modes/lorem-ipsum { };
     magit = callPackage ../applications/editors/emacs-modes/magit { texinfo = pkgs.texinfo5; };
     maudeMode = callPackage ../applications/editors/emacs-modes/maude { };
     notmuch = callPackage ../applications/networking/mailreaders/notmuch { };
     org = (callPackage ../applications/editors/emacs-modes/org { texinfo = pkgs.texinfo5; });
     org2blog = callPackage ../applications/editors/emacs-modes/org2blog { };
     pcache = callPackage ../applications/editors/emacs-modes/pcache { };
-    phpMode = callPackage ../applications/editors/emacs-modes/php { };
-    prologMode = callPackage ../applications/editors/emacs-modes/prolog { };
+    php-mode = callPackage ../applications/editors/emacs-modes/php { };
+    prolog = callPackage ../applications/editors/emacs-modes/prolog { };
     proofgeneral = callPackage ../applications/editors/emacs-modes/proofgeneral {
       texLive = pkgs.texLiveAggregationFun {
         paths = [ pkgs.texLive pkgs.texLiveCMSuper ];
       };
     };
     quack = callPackage ../applications/editors/emacs-modes/quack { };
-    rectMark = callPackage ../applications/editors/emacs-modes/rect-mark { };
+    rect-mark = callPackage ../applications/editors/emacs-modes/rect-mark { };
     remember = callPackage ../applications/editors/emacs-modes/remember { };
     rudel = callPackage ../applications/editors/emacs-modes/rudel { };
-    scalaMode = callPackage ../applications/editors/emacs-modes/scala-mode { };
+    scala-mode = callPackage ../applications/editors/emacs-modes/scala-mode { };
     sunriseCommander = callPackage ../applications/editors/emacs-modes/sunrise-commander { };
-    xmlRpc = callPackage ../applications/editors/emacs-modes/xml-rpc { };
+    xml-rpc = callPackage ../applications/editors/emacs-modes/xml-rpc { };
   };
 }
