f2c492ec6602856d5ada184569569aba36626a02
Peter Simons <simons@cryp.to>
2013-09-16 10:50:04 +0200

    haskell-multiarg: update to version 0.24.0.0

--- pkgs/development/libraries/haskell/multiarg/default.nix
+++ pkgs/development/libraries/haskell/multiarg/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "multiarg";
-  version = "0.22.0.0";
-  sha256 = "1fswkgrn8mc92lrzmrxhv6hbgch2lqdvmjn88k4ajqc0gpmpb750";
+  version = "0.24.0.0";
+  sha256 = "0vdhrsqwa2wq9cvf96x3hqml2vbjcvik9mpz1kbbhb61f9lbhas6";
   buildDepends = [ utf8String ];
   meta = {
     homepage = "https://github.com/massysett/multiarg";
