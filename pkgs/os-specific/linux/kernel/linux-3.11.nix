1751f77b6a0c70d02a0d6ee4f0f4ceeb67d62fcc
Mathijs Kwik <mathijs@bluescreen303.nl>
2013-09-16 22:44:40 +0200

    linux-3.11: upgrade to 3.11.1

--- pkgs/os-specific/linux/kernel/linux-3.11.nix
+++ pkgs/os-specific/linux/kernel/linux-3.11.nix
@@ -1,12 +1,11 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.11";
-  modDirVersion = "3.11.0";
+  version = "3.11.1";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1yfpa4fzhsn4r5dwkcf3azy0vqdms865jaikn3fdwbabmpqchgl0";
+    sha256 = "16wblz06129lxvxsl3rhmdj4b31yzmwv3rxnjmrlj3c3qlzph29c";
   };
 
   features.iwlwifi = true;
