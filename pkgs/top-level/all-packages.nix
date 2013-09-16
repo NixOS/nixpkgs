80e7c8bc8f3b237915de9fddfd8767e22764e578
Jack Cummings <jack.cummings@intel.com>
2013-09-15 21:07:11 -0700

    Gratuitous Space Battles. Needs libcurl.so.3, so put an old version of that in, too

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -690,6 +690,11 @@ let
     scpSupport = zlibSupport && !stdenv.isSunOS && !stdenv.isCygwin;
   };
 
+  curl3 = callPackage ../tools/networking/curl/7.15.nix rec {
+    zlibSupport = true;
+    sslSupport = zlibSupport;
+  };
+
   cunit = callPackage ../tools/misc/cunit { };
 
   curlftpfs = callPackage ../tools/filesystems/curlftpfs { };
@@ -4064,6 +4069,8 @@ let
   gamin = callPackage ../development/libraries/gamin { };
 
   gav = callPackage ../games/gav { };
+
+  gsb = callPackage ../games/gsb { };
 
   gdome2 = callPackage ../development/libraries/gdome2 {
     inherit (gnome) gtkdoc;
