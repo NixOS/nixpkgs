1d8a998c691bc8df2409b21e04e7956ba741e076
Evgeny Egorochkin <phreedom@yandex.ru>
2013-09-17 01:41:35 +0300

    quassel: update to 0.9.0

--- pkgs/applications/networking/irc/quassel/default.nix
+++ pkgs/applications/networking/irc/quassel/default.nix
@@ -11,11 +11,11 @@ let
 
 in with stdenv; mkDerivation rec {
 
-  name = "quassel-0.7.4";
+  name = "quassel-0.9.0";
 
   src = fetchurl {
     url = "http://quassel-irc.org/pub/${name}.tar.bz2";
-    sha256 = "08f4m35bkmp9p1n560a3fg711s9izb25ddx2az03xzf1jl8qdrg7";
+    sha256 = "09v0igjkzan3hllk47w39hkav6v1419vpxn2lfd8473kwdmf0grf";
   };
 
   buildInputs = [ cmake qt4 ]
@@ -48,6 +48,7 @@ in with stdenv; mkDerivation rec {
     '';
     license = "GPLv3";
     maintainers = [ maintainers.phreedom ];
+    repositories.git = https://github.com/quassel/quassel.git;
     inherit (qt4.meta) platforms;
   };
 }
