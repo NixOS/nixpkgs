16e633d2556f4bc02230dcdea9fc0e555ca2c6e8
Peter Simons <simons@cryp.to>
2013-09-18 10:24:40 +0200

    haskell-nats: update to version 0.1.2

--- pkgs/development/libraries/haskell/nats/default.nix
+++ pkgs/development/libraries/haskell/nats/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "nats";
-  version = "0.1";
-  sha256 = "08gh7jjmws70919hmqqmvnfqcpxr34f03jmg3lzmmhqvr15gm1vy";
+  version = "0.1.2";
+  sha256 = "1r4083p8sbnqs74l8faqfs1i97k8bql762l55pbmapy0p1xrkzka";
   meta = {
     homepage = "http://github.com/ekmett/nats/";
     description = "Haskell 98 natural numbers";
