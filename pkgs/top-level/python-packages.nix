cf6ff3a8a3fdb36864c10e85a9a964ef331db025
Jaka Hudoklin <jakahudoklin@gmail.com>
2013-09-20 18:02:04 +0200

    pythonPackages: add wand, ctypes-based simple MagickWand API binding for Python

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6233,6 +6233,24 @@ pythonPackages = modules // import ./python-packages-generated.nix {
   };
 
 
+  wand = buildPythonPackage rec {
+    name = "Wand-0.3.5";
+
+    src = fetchurl {
+      url = "https://pypi.python.org/packages/source/W/Wand/${name}.tar.gz";
+      md5 = "10bab03bf86ce8da2a95a3b15197ae2e";
+    };
+
+    buildInputs = [ pkgs.imagemagick pytest psutil memory_profiler pytest_xdist ];
+
+    meta = {
+      description = "Ctypes-based simple MagickWand API binding for Python";
+      homepage = http://wand-py.org/;
+      platforms = stdenv.lib.platforms.all;
+    };
+  };
+
+
   webob = buildPythonPackage rec {
     version = "1.2.3";
     name = "webob-${version}";
