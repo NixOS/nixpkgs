514a095b7b0b0b45a419cba71cae091fb776efd2
Peter Simons <simons@cryp.to>
2013-09-21 10:01:10 +0200

    haskell-rfc5051: update to version 0.1.0.3

--- pkgs/development/libraries/haskell/rfc5051/default.nix
+++ pkgs/development/libraries/haskell/rfc5051/default.nix
@@ -1,13 +1,11 @@
-{ cabal, glibcLocales }:
+{ cabal }:
 
 cabal.mkDerivation (self: {
   pname = "rfc5051";
-  version = "0.1.0.2";
-  sha256 = "0j009h78gw92bl61g0js6xkah76lb6gm0lv55z2sw6gxm4a1ygml";
+  version = "0.1.0.3";
+  sha256 = "0av4c3qvwbkbzrjrrg601ay9pds7wscqqp2lc2z78mv2lllap3g3";
   isLibrary = true;
   isExecutable = true;
-  preConfigure = "export LANG=en_US.UTF-8"
-               + self.stdenv.lib.optionalString self.stdenv.isLinux " LOCALE_ARCHIVE=${glibcLocales}/lib/locale/locale-archive";
   meta = {
     description = "Simple unicode collation as per RFC5051";
     license = self.stdenv.lib.licenses.bsd3;
