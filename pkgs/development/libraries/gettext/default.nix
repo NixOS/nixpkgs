46a7b1ea0a8cfc636207f1f66ac2a95662dd064a
Peter Simons <simons@cryp.to>
2013-09-23 11:14:27 +0200

    gettext: fix minor errors after merge

--- pkgs/development/libraries/gettext/default.nix
+++ pkgs/development/libraries/gettext/default.nix
@@ -70,11 +70,11 @@ stdenv.mkDerivation (rec {
   };
 }
 
-// optionalAttrs stdenv.isDarwin {
+// stdenv.lib.optionalAttrs stdenv.isDarwin {
   makeFlags = "CFLAGS=-D_FORTIFY_SOURCE=0";
 }
 
-// optionalAttrs stdenv.isCygwin {
+// stdenv.lib.optionalAttrs stdenv.isCygwin {
   patchPhase =
    # Make sure `error.c' gets compiled and is part of `libgettextlib.la'.
    # This fixes:
