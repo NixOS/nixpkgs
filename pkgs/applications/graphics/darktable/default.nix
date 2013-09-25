70a5abd44060cb1b902695d326ce9692cd3edea6
Rickard Nilsson <rickynils@gmail.com>
2013-09-25 23:25:55 +0200

    darktable: Add several missing dependencies. This makes the map and tethering modules available

--- pkgs/applications/graphics/darktable/default.nix
+++ pkgs/applications/graphics/darktable/default.nix
@@ -1,9 +1,10 @@
-{ stdenv, fetchurl
+{ stdenv, fetchurl, libsoup, graphicsmagick, SDL, json_glib
 , GConf, atk, cairo, cmake, curl, dbus_glib, exiv2, glib
 , libgnome_keyring, gtk, ilmbase, intltool, lcms, lcms2
 , lensfun, libXau, libXdmcp, libexif, libglade, libgphoto2, libjpeg
 , libpng, libpthreadstubs, libraw1394, librsvg, libtiff, libxcb
-, openexr, pixman, pkgconfig, sqlite, bash, libxslt }:
+, openexr, pixman, pkgconfig, sqlite, bash, libxslt, openjpeg
+, mesa }:
 
 assert stdenv ? glibc;
 
@@ -21,6 +22,7 @@ stdenv.mkDerivation rec {
       ilmbase intltool lcms lcms2 lensfun libXau libXdmcp libexif
       libglade libgphoto2 libjpeg libpng libpthreadstubs libraw1394
       librsvg libtiff libxcb openexr pixman pkgconfig sqlite libxslt
+      libsoup graphicsmagick SDL json_glib openjpeg mesa
     ];
 
   preConfigure = ''
@@ -30,16 +32,16 @@ stdenv.mkDerivation rec {
     export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${atk}/include/atk-1.0"
     export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${ilmbase}/include/OpenEXR"
     export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${openexr}/include/OpenEXR"
-
-    substituteInPlace tools/create_preferences.sh.in --replace '#!/usr/bin/env bash' '#!${bash}/bin/bash'
   '';
 
   cmakeFlags = [
     "-DPTHREAD_INCLUDE_DIR=${stdenv.glibc}/include"
     "-DPTHREAD_LIBRARY=${stdenv.glibc}/lib/libpthread.so"
     "-DCMAKE_BUILD_TYPE=Release"
+    "-DBINARY_PACKAGE_BUILD=1"
     "-DGTK2_GLIBCONFIG_INCLUDE_DIR=${glib}/lib/glib-2.0/include"
     "-DGTK2_GDKCONFIG_INCLUDE_DIR=${gtk}/lib/gtk-2.0/include"
+    "-DBUILD_USERMANUAL=False"
   ];
 
   meta = with stdenv.lib; {
@@ -47,6 +49,6 @@ stdenv.mkDerivation rec {
     homepage = http://darktable.sourceforge.net;
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
-    maintainers = [ maintainers.goibhniu ];
+    maintainers = [ maintainers.goibhniu maintainers.rickynils ];
   };
 }
