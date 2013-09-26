36fca9480535c08220bdd7838bbd1dbf42661d3e
Moritz Ulrich <moritz@tarn-vedra.de>
2013-09-26 21:37:49 +0200

    Fix some issues in my packages reported by nixpkgs-lint.
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- pkgs/development/tools/misc/stm32flash/default.nix
+++ pkgs/development/tools/misc/stm32flash/default.nix
@@ -8,19 +8,17 @@ stdenv.mkDerivation {
     sha256 = "04k631g9lzvp9xr4sw51xpq1g542np61s1l8fpwx9rbsc8m5l0i6";
   };
 
-  buildInputs = [];
-
   installPhase = ''
     # Manually copy, make install copies to /usr/local/bin
     mkdir -pv $out/bin/
     cp stm32flash $out/bin/
   '';
 
-  meta = { 
-    description = "Open source flash program for the STM32 ARM processors using the ST bootloader.";
+  meta = with stdenv.lib; {
+    description = "Open source flash program for the STM32 ARM processors using the ST bootloader";
     homepage = https://code.google.com/p/stm32flash/;
     license = "GPLv2";
-    platforms = stdenv.lib.platforms.all; # Should work on all platforms
-    maintainers = [ stdenv.lib.maintainers.the-kenny ];
+    platforms = platforms.all; # Should work on all platforms
+    maintainers = [ maintainers.the-kenny ];
   };
 }
