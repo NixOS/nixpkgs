bb69f658eac0ff6da3ea35cec21532892750d072
Sander van der Burg <svanderburg@gmail.com>
2013-09-27 17:56:01 +0200

    Bump titaniumsdk to 3.1.4

--- pkgs/development/mobile/titaniumenv/titaniumsdk.nix
+++ pkgs/development/mobile/titaniumenv/titaniumsdk.nix
@@ -1,14 +1,14 @@
 {stdenv, fetchurl, unzip, makeWrapper, python, jdk}:
 
 stdenv.mkDerivation {
-  name = "titanium-mobilesdk-3.1.1.v20130612114553";
+  name = "mobilesdk-3.1.4.v20130926144546";
   src = if (stdenv.system == "i686-linux" || stdenv.system == "x86_64-linux") then fetchurl {
-    url = http://builds.appcelerator.com.s3.amazonaws.com/mobile/3_1_X/mobilesdk-3.1.1.v20130612114553-linux.zip;
-    sha1 = "410ba7e8171a887b6a4b3173116430657c3d84aa";
+    url = http://builds.appcelerator.com.s3.amazonaws.com/mobile/3_1_X/mobilesdk-3.1.4.v20130926144546-linux.zip;
+    sha1 = "da4a03ced67f0e8f442d551bbd41ea01fceeee00";
   }
   else if stdenv.system == "x86_64-darwin" then fetchurl {
-    url = http://builds.appcelerator.com.s3.amazonaws.com/mobile/3_1_X/mobilesdk-3.1.1.v20130612114553-osx.zip;
-    sha1 = "0893a1560ac6fb63369fc9f6ea9550b6649438fa";
+    url = http://builds.appcelerator.com.s3.amazonaws.com/mobile/3_1_X/mobilesdk-3.1.4.v20130926144546-osx.zip;
+    sha1 = "55f604c8edb989ba214c8ed7538d1b416df0419e";
   }
   else throw "Platform: ${stdenv.system} not supported!";
   
