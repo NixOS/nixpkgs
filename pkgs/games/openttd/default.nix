296dbc8279c96da93ccbbbf917b8599ee88fcc1f
Moritz Ulrich <moritz@tarn-vedra.de>
2013-09-29 00:29:26 +0200

    OpenTTD: 1.3.2 & some coding-style fixes.
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- pkgs/games/openttd/default.nix
+++ pkgs/games/openttd/default.nix
@@ -1,21 +1,21 @@
-{stdenv, fetchurl, pkgconfig, SDL, libpng, zlib, xz, freetype, fontconfig}:
+{ stdenv, fetchurl, pkgconfig, SDL, libpng, zlib, xz, freetype, fontconfig }:
 
 stdenv.mkDerivation rec {
   name = "openttd-${version}";
-  version = "1.2.2";
+  version = "1.3.2";
 
   src = fetchurl {
     url = "http://binaries.openttd.org/releases/${version}/${name}-source.tar.xz";
-    sha256 = "158znfx389bhs9gd2hadnbc2a32z4ma1vz8704cmw9yh0fmhbcap";
+    sha256 = "02r7xfq9a5x1y2wpdhqyczaj48z0qan33hs4i2liahsg1k6w1vzn";
   };
 
-  buildInputs = [SDL libpng pkgconfig xz zlib freetype fontconfig];
+  buildInputs = [ SDL libpng pkgconfig xz zlib freetype fontconfig ];
   prefixKey = "--prefix-dir=";
 
-  configureFlags = ''
-    --with-zlib=${zlib}/lib/libz.a 
-    --without-liblzo2
-  '';
+  configureFlags = [
+    "--with-zlib=${zlib}/lib/libz.a"
+    "--without-liblzo2"
+  ];
 
   makeFlags = "INSTALL_PERSONAL_DIR=";
 
@@ -24,11 +24,11 @@ stdenv.mkDerivation rec {
   '';
 
   meta = {
-    description = ''OpenTTD is an open source clone of the Microprose game "Transport Tycoon Deluxe".'';
+    description = ''OpenTTD is an open source clone of the Microprose game "Transport Tycoon Deluxe"'';
     longDescription = ''
       OpenTTD is a transportation economics simulator. In single player mode,
-      players controll a transportation business, and use rail, road, sea, and air
-      transport to move goods and people around the simulated world. 
+      players control a transportation business, and use rail, road, sea, and air
+      transport to move goods and people around the simulated world.
 
       In multiplayer networked mode, players may:
         - play competitively as different businesses
@@ -37,6 +37,7 @@ stdenv.mkDerivation rec {
     '';
     homepage = http://www.openttd.org/;
     license = "GPLv2";
-    maintainers = with stdenv.lib.maintainers; [ jcumming ];
+    platforms = stdenv.lib.platforms.unix;
+    maintainers = with stdenv.lib.maintainers; [ jcumming the-kenny ];
   };
 }
