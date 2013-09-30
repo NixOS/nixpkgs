02c11554d7037fd9d3518705a8b339fc77ed9d6c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-09-30 11:29:27 +0200

    linux: Update to 3.4.63

--- pkgs/os-specific/linux/kernel/linux-3.4.nix
+++ pkgs/os-specific/linux/kernel/linux-3.4.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.4.62";
+  version = "3.4.63";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1cfi7125xdb1b9mrabrxyr86ixyb8jik21vg5r8d355r0zgz124m";
+    sha256 = "0wphxk0lzbcd7vplvnx4b2h446pmdv2gxc0gnjbrzfl0v4lgz1y3";
   };
 
   features.iwlwifi = true;
