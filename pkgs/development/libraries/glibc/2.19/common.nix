1472989242fc4c22e9b86e3860a317f72ad77003
Alexander Kjeldaas <ak@formalprivacy.com>
2013-10-01 13:10:40 +0200

    Make glibc compilation more pure. Remove datetime from nscd.

--- pkgs/development/libraries/glibc/2.19/common.nix
+++ pkgs/development/libraries/glibc/2.19/common.nix
@@ -60,6 +60,14 @@ stdenv.mkDerivation ({
       ./fix-math.patch
 
       ./cve-2014-0475.patch
+
+      /* Remove references to the compilation date.  */
+      ./glibc-remove-date-from-compilation-banner.patch
+
+      /* Remove the date and time from nscd.  It is used as a protocol
+         compatibility check, but we assume nix takes care of that for
+         us. */
+      ./glibc-remove-datetime-from-nscd.patch
     ];
 
   postPatch = ''
