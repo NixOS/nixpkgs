dc7ed72066175f0f311df98e467dc7c4ab2a865a
Moritz Ulrich <moritz@tarn-vedra.de>
2013-10-02 13:55:17 +0200

    mu: Install msg2pdf (render html mail to pdf) and mug (gtk gui)
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- pkgs/tools/networking/mu/default.nix
+++ pkgs/tools/networking/mu/default.nix
@@ -1,4 +1,5 @@
-{ fetchurl, stdenv, sqlite, pkgconfig, xapian, glib, gmime, texinfo, emacs, guile }:
+{ fetchurl, stdenv, sqlite, pkgconfig, xapian, glib, gmime, texinfo, emacs, guile
+, gtk3, webkit, libsoup, icu }:
 
 stdenv.mkDerivation rec {
   version = "0.9.9.5";
@@ -9,7 +10,24 @@ stdenv.mkDerivation rec {
     sha256 = "1hwkliyb8fjrz5sw9fcisssig0jkdxzhccw0ld0l9a10q1l9mqhp";
   };
 
-  buildInputs = [ sqlite pkgconfig xapian glib gmime texinfo emacs guile ];
+  buildInputs = [ sqlite pkgconfig xapian glib gmime texinfo emacs guile
+                  gtk3 webkit libsoup icu ];
+
+  preBuild = ''
+    # Fix mu4e-builddir (set it to $out)
+    substituteInPlace mu4e/mu4e-meta.el.in \
+      --replace "@abs_top_builddir@" "$out"
+
+    # We install msg2pdf to bin/msg2pdf, fix its location in elisp
+    substituteInPlace mu4e/mu4e-actions.el \
+      --replace "/toys/msg2pdf/msg2pdf" "/bin/msg2pdf"
+  '';
+
+  # Install mug and msg2pdf
+  postInstall = ''
+    cp -v toys/msg2pdf/msg2pdf $out/bin/
+    cp -v toys/mug/mug $out/bin/
+  '';
 
   meta = {
     description = "A collection of utilties for indexing and searching Maildirs";
