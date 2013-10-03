be67ab572ced2e0d563a1e21e41d0edba2d76f02
Peter Simons <simons@cryp.to>
2013-10-03 17:55:28 +0200

    haskell-regex-compat-tdfa: work around broken release tarball on Hackage

--- pkgs/development/libraries/haskell/regex-compat-tdfa/default.nix
+++ pkgs/development/libraries/haskell/regex-compat-tdfa/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.95.1.3";
   sha256 = "0wl5sqbb3rl5dai3qni8w09wlipc4n1mn9kh5zgb9xl0lcd59pjx";
   buildDepends = [ regexBase regexTdfa ];
+  patchPhase = "find . -type f -exec touch {} ';'";
   meta = {
     homepage = "http://hub.darcs.net/shelarcy/regex-compat-tdfa";
     description = "Unicode Support version of Text.Regex, using regex-tdfa";
