e902ca15ab1e24f4e64cb7dd2040311ba39558e7
Vladimír Čunát <vcunat@gmail.com>
2013-10-05 21:21:21 +0200

    update wayland deps needed for gtk+ 3.10

--- pkgs/development/libraries/libxkbcommon/default.nix
+++ pkgs/development/libraries/libxkbcommon/default.nix
@@ -1,14 +1,14 @@
-{ stdenv, fetchurl, yacc, flex, xkeyboard_config }:
+{ stdenv, fetchurl, pkgconfig, yacc, flex, xkeyboard_config }:
 
 stdenv.mkDerivation rec {
-  name = "libxkbcommon-0.2.0";
+  name = "libxkbcommon-0.3.1";
 
   src = fetchurl {
-    url = "http://xkbcommon.org/download/${name}.tar.bz2";
-    sha256 = "0hpvfa8p4bhvhc1gcb578m354p5idd192xb8zlaq16d33h90msvl";
+    url = "http://xkbcommon.org/download/${name}.tar.xz";
+    sha256 = "13mk335r4dhi9qglzbp46ina1wz4qgcp8r7s06iq7j50pf0kb5ww";
   };
 
-  buildInputs = [ yacc flex xkeyboard_config ];
+  buildInputs = [ pkgconfig yacc flex xkeyboard_config ];
 
   configureFlags = ''
     --with-xkb-config-root=${xkeyboard_config}/etc/X11/xkb
--- pkgs/development/libraries/wayland/default.nix
+++ pkgs/development/libraries/wayland/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, libffi, expat, pkgconfig, libxslt, docbook_xsl, doxygen }:
 
-let version = "1.0.5"; in
+let version = "1.2.1"; in
 
 stdenv.mkDerivation rec {
   name = "wayland-${version}";
 
   src = fetchurl {
     url = "http://wayland.freedesktop.org/releases/${name}.tar.xz";
-    sha256 = "130n7v5i7rfsrli2n8vdzfychlgd8v7by7sfgp8vfqdlss5km34w";
+    sha256 = "0vqngxpavcxvjwlw7afgrbv7qvmd489sj555wrnxjc8p10mjw1kq";
   };
 
   buildInputs = [ pkgconfig libffi expat libxslt docbook_xsl doxygen ];
