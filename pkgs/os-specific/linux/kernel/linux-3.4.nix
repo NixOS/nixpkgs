c242863da8f1125f5f16b4739c5808320cebc196
Mathijs Kwik <mathijs@bluescreen303.nl>
2013-10-06 20:16:06 +0200

    linux-3.4: upgrade to 3.4.65

--- pkgs/os-specific/linux/kernel/linux-3.4.nix
+++ pkgs/os-specific/linux/kernel/linux-3.4.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.4.63";
+  version = "3.4.65";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0wphxk0lzbcd7vplvnx4b2h446pmdv2gxc0gnjbrzfl0v4lgz1y3";
+    sha256 = "1amy6gdqnk0klqmghkdfn2fv1rd30pqvqwx6ix27gf3hmn4s823z";
   };
 
   features.iwlwifi = true;
