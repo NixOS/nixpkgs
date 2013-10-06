07fd8c87ab30970fce6139feb087c9ff331b2f48
Mathijs Kwik <mathijs@bluescreen303.nl>
2013-10-06 20:15:54 +0200

    linux-3.10: upgrade to 3.10.15

--- pkgs/os-specific/linux/kernel/linux-3.10.nix
+++ pkgs/os-specific/linux/kernel/linux-3.10.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.10.14";
+  version = "3.10.15";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0iqvwjhhmqvb7wbqifczd7bd9a4iq8xwyl61sijbwlarn5ghsm1n";
+    sha256 = "07wjh58sylbbw9hwxd5xvbz3dxd05iar8ahzk90lki38m5157ffk";
   };
 
   features.iwlwifi = true;
