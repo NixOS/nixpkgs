e855ac5070fa594e1db5ebdf1ea9caef65163c4f
Vladimír Čunát <vcunat@gmail.com>
2013-10-08 20:52:19 +0200

    gcc: don't use profiling on darwin
    Darwin seems to have problems on Hydra, maybe because of this.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2480,7 +2480,7 @@ let
     inherit noSysDirs;
 
     # PGO seems to speed up compilation by gcc by ~10%, see #445 discussion
-    profiledCompiler = with stdenv; (isi686 || isx86_64);
+    profiledCompiler = with stdenv; (!isDarwin && (isi686 || isx86_64));
 
     # When building `gcc.crossDrv' (a "Canadian cross", with host == target
     # and host != build), `cross' must be null but the cross-libc must still
