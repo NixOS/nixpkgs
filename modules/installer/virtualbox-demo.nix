87bea0b09eaa99a663c4d8a32b6b3da1c5352bfc
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-10-09 13:33:43 +0200

    Add more video drivers to the VM image
    This might allow it to work in VMware and QEMU.

--- modules/installer/virtualbox-demo.nix
+++ modules/installer/virtualbox-demo.nix
@@ -10,4 +10,8 @@
 
   # Allow mounting of shared folders.
   users.extraUsers.demo.extraGroups = [ "vboxsf" ];
+
+  # Add some more video drivers to give X11 a shot at working in
+  # VMware and QEMU.
+  services.xserver.videoDrivers = mkOverride 40 [ "virtualbox" "vmware" "cirrus" "vesa" ];
 }
