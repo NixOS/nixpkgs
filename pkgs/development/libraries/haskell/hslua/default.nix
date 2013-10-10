d8993dd96c184833c91e96f617881837054ab191
Peter Simons <simons@cryp.to>
2013-10-10 11:50:47 +0200

    haskell-hslua: update to version 0.3.7

--- pkgs/development/libraries/haskell/hslua/default.nix
+++ pkgs/development/libraries/haskell/hslua/default.nix
@@ -1,10 +1,12 @@
-{ cabal, mtl }:
+{ cabal, lua, mtl }:
 
 cabal.mkDerivation (self: {
   pname = "hslua";
-  version = "0.3.6.1";
-  sha256 = "0c60gnf0mp6kx2z2149icl7hdwvigibvxd091a3vc6zkl5c5r41p";
+  version = "0.3.7";
+  sha256 = "1q5s2b7x9idvdvp31yl86mmy476gfq6rg8f0r8faqxrm45jwgv2q";
   buildDepends = [ mtl ];
+  pkgconfigDepends = [ lua ];
+  configureFlags = "-fsystem-lua";
   meta = {
     description = "A Lua language interpreter embedding in Haskell";
     license = self.stdenv.lib.licenses.bsd3;
