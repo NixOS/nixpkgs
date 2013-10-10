304b837c4156fa78e9af4e45f36c12b4160ec14c
Peter Simons <simons@cryp.to>
2013-10-10 11:50:47 +0200

    haskell-persistent-template: update to version 1.2.0.3

--- pkgs/development/libraries/haskell/persistent-template/default.nix
+++ pkgs/development/libraries/haskell/persistent-template/default.nix
@@ -1,13 +1,14 @@
 { cabal, aeson, hspec, monadControl, monadLogger, persistent
-, QuickCheck, text, transformers
+, QuickCheck, text, thOrphans, transformers
 }:
 
 cabal.mkDerivation (self: {
   pname = "persistent-template";
-  version = "1.2.0.2";
-  sha256 = "0zj35mg7fzyk4b98s3s8m5i064s0wznz9aixgxa4kzm4xps7hj4z";
+  version = "1.2.0.3";
+  sha256 = "10scyrfa8g79v8ra79bp0bg7q6iwqjw6jpm06g11pngv4x9zx880";
   buildDepends = [
-    aeson monadControl monadLogger persistent text transformers
+    aeson monadControl monadLogger persistent text thOrphans
+    transformers
   ];
   testDepends = [ aeson hspec persistent QuickCheck text ];
   meta = {
