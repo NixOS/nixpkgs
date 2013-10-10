d2f719644baaa37605550c546efa03d5f58720ef
Peter Simons <simons@cryp.to>
2013-10-10 11:50:47 +0200

    haskell-regex-compat-tdfa: update to version 0.95.1.4

--- pkgs/development/libraries/haskell/regex-compat-tdfa/default.nix
+++ pkgs/development/libraries/haskell/regex-compat-tdfa/default.nix
@@ -2,10 +2,9 @@
 
 cabal.mkDerivation (self: {
   pname = "regex-compat-tdfa";
-  version = "0.95.1.3";
-  sha256 = "0wl5sqbb3rl5dai3qni8w09wlipc4n1mn9kh5zgb9xl0lcd59pjx";
+  version = "0.95.1.4";
+  sha256 = "1p90fn90yhp7fvljjdqjp41cszidcfz4pw7fwvzyx4739b98x8sg";
   buildDepends = [ regexBase regexTdfa ];
-  patchPhase = "find . -type f -exec touch {} ';'";
   meta = {
     homepage = "http://hub.darcs.net/shelarcy/regex-compat-tdfa";
     description = "Unicode Support version of Text.Regex, using regex-tdfa";
