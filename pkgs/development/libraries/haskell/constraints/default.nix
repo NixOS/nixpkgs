1df8a24935eb02e40682ee9f649626bcafb94304
Peter Simons <simons@cryp.to>
2013-10-13 10:14:30 +0200

    haskell-constraints: update to version 0.3.4.1

--- pkgs/development/libraries/haskell/constraints/default.nix
+++ pkgs/development/libraries/haskell/constraints/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "constraints";
-  version = "0.3.3";
-  sha256 = "0mglqd6l6bc333i7gymbm8q037hj5fny6jzyg1zmw5kg6r3xcwdi";
+  version = "0.3.4.1";
+  sha256 = "13jxh2cgcfyiqhx7j5063k8k60wz9h4hd5lf2mw2skbcryg6csmb";
   buildDepends = [ newtype ];
   meta = {
     homepage = "http://github.com/ekmett/constraints/";
