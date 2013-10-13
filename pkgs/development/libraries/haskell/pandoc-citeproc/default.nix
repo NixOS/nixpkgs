1dc9738c340e5d5478d2bb474742f9a63212b3ba
Peter Simons <simons@cryp.to>
2013-10-13 10:14:31 +0200

    haskell-pandoc-citeproc: update to version 0.1.2

--- pkgs/development/libraries/haskell/pandoc-citeproc/default.nix
+++ pkgs/development/libraries/haskell/pandoc-citeproc/default.nix
@@ -1,19 +1,19 @@
 { cabal, aeson, aesonPretty, attoparsec, Diff, filepath, hexpat
 , hsBibutils, HTTP, json, mtl, network, pandoc, pandocTypes, parsec
-, rfc5051, syb, tagsoup, texmath, text, time, utf8String, vector
-, yaml
+, rfc5051, split, syb, tagsoup, texmath, text, time, utf8String
+, vector, yaml
 }:
 
 cabal.mkDerivation (self: {
   pname = "pandoc-citeproc";
-  version = "0.1.1.2";
-  sha256 = "02bs9wb3x1p9fs4kixchmvyyrhrkmx0qkwv22qmy4gsp90sc8q8i";
+  version = "0.1.2";
+  sha256 = "055msvrcqjkijkhzws48scpc4z90g0qjjsdcd0fhy309da6vax57";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
     aeson attoparsec filepath hexpat hsBibutils HTTP json mtl network
-    pandocTypes parsec rfc5051 syb tagsoup texmath text time utf8String
-    vector yaml
+    pandoc pandocTypes parsec rfc5051 split syb tagsoup texmath text
+    time utf8String vector yaml
   ];
   testDepends = [ aeson aesonPretty Diff pandoc pandocTypes ];
   doCheck = false;
