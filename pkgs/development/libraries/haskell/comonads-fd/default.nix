93727ab86143d8872048403abb54189425804808
Peter Simons <simons@cryp.to>
2013-10-14 23:54:21 +0200

    haskell-comonads-fd: don't try to build documentation; this package is empty

--- pkgs/development/libraries/haskell/comonads-fd/default.nix
+++ pkgs/development/libraries/haskell/comonads-fd/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "4.0";
   sha256 = "19xpv0dsz7w3a1sq1gdxwzglfal45vj2s22zb12g9mpk5rp3hw1s";
   buildDepends = [ comonad ];
+  noHaddock = true;
   meta = {
     homepage = "http://github.com/ekmett/comonads-fd/";
     description = "This package has been merged into comonad 4.0";
