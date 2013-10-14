a8c81206269bc1a2dc8b5980a20bdf4736e6c26f
Peter Simons <simons@cryp.to>
2013-10-14 08:47:48 +0200

    haskell-parsers: update to version 0.10

--- pkgs/development/libraries/haskell/parsers/default.nix
+++ pkgs/development/libraries/haskell/parsers/default.nix
@@ -1,12 +1,14 @@
-{ cabal, charset, doctest, filepath, text, transformers
+{ cabal, charset, doctest, filepath, parsec, text, transformers
 , unorderedContainers
 }:
 
 cabal.mkDerivation (self: {
   pname = "parsers";
-  version = "0.9";
-  sha256 = "04lbayvdv2hax4s9sqlnia7jpzv1sgls41ylql0xbi2zhz5rvyyi";
-  buildDepends = [ charset text transformers unorderedContainers ];
+  version = "0.10";
+  sha256 = "090dvmdb1kmnc3k2x170y9fdifxi16hzkij1gzc51flx3bpx40i1";
+  buildDepends = [
+    charset parsec text transformers unorderedContainers
+  ];
   testDepends = [ doctest filepath ];
   meta = {
     homepage = "http://github.com/ekmett/parsers/";
