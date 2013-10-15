347132478b4782b15831c332150542318d9efc0d
Peter Simons <simons@cryp.to>
2013-10-15 11:28:36 +0200

    haskell-HTTP: jailbreak to fix build with GHC 7.7

--- pkgs/development/libraries/haskell/HTTP/4000.2.8.nix
+++ pkgs/development/libraries/haskell/HTTP/4000.2.8.nix
@@ -13,6 +13,7 @@ cabal.mkDerivation (self: {
     network pureMD5 split testFramework testFrameworkHunit wai warp
   ];
   doCheck = false;
+  jailbreak = true;
   meta = {
     homepage = "https://github.com/haskell/HTTP";
     description = "A library for client-side HTTP";
