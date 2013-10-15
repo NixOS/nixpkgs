7edfd67fe00be0963ce8c8d03c8a206f3ac77a92
Peter Simons <simons@cryp.to>
2013-10-15 10:53:54 +0200

    haskell-accelerate: jailbreak to fix build with recent versions of fclables

--- pkgs/development/libraries/haskell/accelerate/default.nix
+++ pkgs/development/libraries/haskell/accelerate/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.13.0.5";
   sha256 = "1vqkv3k0w1zy0111a786npf3hypbcg675lbdkv2cf3zx5hqcnn6j";
   buildDepends = [ fclabels hashable hashtables ];
+  jailbreak = true;
   meta = {
     homepage = "https://github.com/AccelerateHS/accelerate/";
     description = "An embedded language for accelerated array processing";
