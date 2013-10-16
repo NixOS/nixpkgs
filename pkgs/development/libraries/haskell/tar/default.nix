b9136ed171ef3689311f20723699646d82e26abb
Peter Simons <simons@cryp.to>
2013-10-16 17:09:26 +0200

    haskell-tar: cosmetic

--- pkgs/development/libraries/haskell/tar/default.nix
+++ pkgs/development/libraries/haskell/tar/default.nix
@@ -6,7 +6,7 @@ cabal.mkDerivation (self: {
   sha256 = "0vbsv7h3zgp30mlgsw156jkv1rqy5zbm98as9haf7x15hd6jf254";
   buildDepends = [ filepath time ];
   meta = {
-    description = "Reading, writing and manipulating \".tar\" archive files";
+    description = "Reading, writing and manipulating \".tar\" archive files.";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [ self.stdenv.lib.maintainers.andres ];
