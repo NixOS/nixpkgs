b2868afaa42b5e3a4f3f513537a9ce47f9a24776
Peter Simons <simons@cryp.to>
2013-10-16 17:16:33 +0200

    haskell-entropy: update to version 0.2.2.4

--- pkgs/development/libraries/haskell/entropy/default.nix
+++ pkgs/development/libraries/haskell/entropy/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "entropy";
-  version = "0.2.2.3";
-  sha256 = "04kzl7j73g5ibbc28b4llmnrvhg7q2fji1akx5ii81sfxdpnxy45";
+  version = "0.2.2.4";
+  sha256 = "1cjmpb0rh1ib4j9mwmf1irn401vmjawxkshxdmmb4643rmcgx1gm";
   meta = {
     homepage = "https://github.com/TomMD/entropy";
     description = "A platform independent entropy source";
