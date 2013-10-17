4836a75fe0bf31dc31ff0783dd69f4ad2c6f46f2
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-10-17 12:20:07 +0200

    Make more tests release-critical

--- nixos/release-combined.nix
+++ nixos/release-combined.nix
@@ -43,16 +43,22 @@ in rec {
         (all nixos.iso_graphical)
         (all nixos.ova)
 
+        (all nixos.tests.efi-installer.simple)
         (all nixos.tests.firefox)
         (all nixos.tests.firewall)
+        (all nixos.tests.installer.grub1)
         (all nixos.tests.installer.lvm)
         (all nixos.tests.installer.separateBoot)
         (all nixos.tests.installer.simple)
+        (all nixos.tests.ipv6)
         (all nixos.tests.kde4)
         (all nixos.tests.login)
         (all nixos.tests.misc)
+        (all nixos.tests.nat)
+        (all nixos.tests.nfs3)
         (all nixos.tests.openssh)
         (all nixos.tests.printing)
+        (all nixos.tests.proxy)
         (all nixos.tests.xfce)
 
         nixpkgs.tarball
