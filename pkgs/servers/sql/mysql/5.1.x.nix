de10c1b9be3bbc6d7942a0058d062a51db1cf6fb
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-10-17 13:25:45 +0200

    mysql: Update to 5.1.72

--- pkgs/servers/sql/mysql/5.1.x.nix
+++ pkgs/servers/sql/mysql/5.1.x.nix
@@ -3,11 +3,11 @@
 # Note: zlib is not required; MySQL can use an internal zlib.
 
 stdenv.mkDerivation rec {
-  name = "mysql-5.1.69";
+  name = "mysql-5.1.72";
 
   src = fetchurl {
     url = "http://cdn.mysql.com/Downloads/MySQL-5.1/${name}.tar.gz";
-    md5 = "06bbb6a11a2cbe042f80dbd333ff9f12";
+    md5 = "ed79cd48e3e7402143548917813cdb80";
   };
 
   buildInputs = [ncurses zlib perl openssl] ++ stdenv.lib.optional stdenv.isLinux ps;
