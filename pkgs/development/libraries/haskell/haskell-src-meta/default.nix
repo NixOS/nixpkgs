06d6c3ed5d061e95d1db92f7eff08d5c1a2181d8
Peter Simons <simons@cryp.to>
2013-10-18 10:53:59 +0200

    haskell-src-meta: jailbreak to fix build with latest th-orphans

--- pkgs/development/libraries/haskell/haskell-src-meta/default.nix
+++ pkgs/development/libraries/haskell/haskell-src-meta/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.6.0.4";
   sha256 = "10dixf2abk0canwikf3wdp1ahc51400wxa7x4g59pygv8a3c1c1x";
   buildDepends = [ haskellSrcExts syb thOrphans uniplate ];
+  jailbreak = true;
   meta = {
     description = "Parse source to template-haskell abstract syntax";
     license = self.stdenv.lib.licenses.bsd3;
