73449533bafee31b1a9e1ccd077af687809733bf
Evgeny Egorochkin <phreedom@yandex.ru>
2013-10-19 10:52:16 +0300

    linux 3.12: fix url to match hash

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -4,7 +4,7 @@ import ./generic.nix (args // rec {
   version = "3.12-rc3";
 
   src = fetchurl {
-    url = "https://www.kernel.org/pub/linux/kernel/v3.0/testing/linux-${version}.tar.gz";
+    url = "https://www.kernel.org/pub/linux/kernel/v3.0/testing/linux-${version}.tar.xz";
     sha256 = "1rayb0f4n81yp9ghcws0v36dpqyl9ks3naf37p2qy7jvrwagmj28";
   };
 
