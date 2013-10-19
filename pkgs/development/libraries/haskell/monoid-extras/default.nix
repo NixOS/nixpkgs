cbc31042f86f4106330caced243fcf84803e410e
Peter Simons <simons@cryp.to>
2013-10-19 11:26:04 +0200

    haskell-monoid-extras: update to version 0.3.2.3

--- pkgs/development/libraries/haskell/monoid-extras/default.nix
+++ pkgs/development/libraries/haskell/monoid-extras/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "monoid-extras";
-  version = "0.3.2.2";
-  sha256 = "1fy0fk2mzan6n5chc11x303hz3iq3kpx6ma6c8xsi8va1b9ikpda";
+  version = "0.3.2.3";
+  sha256 = "1q7aw4adg082rszkc3skdvidcn86n06xvr3x8qarpjb285znsmc4";
   buildDepends = [ groupoids groups semigroupoids semigroups ];
   jailbreak = true;
   meta = {
