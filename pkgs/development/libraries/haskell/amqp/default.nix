7e3384d9a87aa1d9d9f0caf91db71cc891f7c6db
Peter Simons <simons@cryp.to>
2013-10-19 11:26:04 +0200

    haskell-amqp: update to version 0.7.0

--- pkgs/development/libraries/haskell/amqp/default.nix
+++ pkgs/development/libraries/haskell/amqp/default.nix
@@ -1,12 +1,19 @@
-{ cabal, binary, dataBinaryIeee754, network, text, xml }:
+{ cabal, binary, clock, dataBinaryIeee754, hspec, hspecExpectations
+, network, split, text, xml
+}:
 
 cabal.mkDerivation (self: {
   pname = "amqp";
-  version = "0.6.0";
-  sha256 = "0rfczmhf46sc3pxxb5gwv4ggsxkqkvdi4kkqdnrdpdhs7p41im2n";
+  version = "0.7.0";
+  sha256 = "09zazmbdw8nphbjkmixn2dpwdgkjqjfbn6jv522ykvrcnsn35kc4";
   isLibrary = true;
   isExecutable = true;
-  buildDepends = [ binary dataBinaryIeee754 network text xml ];
+  buildDepends = [
+    binary clock dataBinaryIeee754 network split text xml
+  ];
+  testDepends = [
+    binary dataBinaryIeee754 hspec hspecExpectations network split text
+  ];
   meta = {
     homepage = "https://github.com/hreinhardt/amqp";
     description = "Client library for AMQP servers (currently only RabbitMQ)";
