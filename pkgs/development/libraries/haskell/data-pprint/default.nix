cd5a60d590ff926db6699422ee0f8a6d7a51da15
Peter Simons <simons@cryp.to>
2013-10-19 13:02:54 +0200

    haskell-data-pprint: patch to fix build with older versions of 'pretty'

--- pkgs/development/libraries/haskell/data-pprint/default.nix
+++ pkgs/development/libraries/haskell/data-pprint/default.nix
@@ -5,6 +5,10 @@ cabal.mkDerivation (self: {
   version = "0.2.2";
   sha256 = "0cr69qv2j8fmmlir8rzlafcxk1cg3lg1z0zrwkz0lb7idm25fy36";
   buildDepends = [ deepseq mtl parallel time ];
+  patches = [ (fetchurl { url = "https://github.com/divipp/ActiveHs-misc/pull/3.patch";
+                          sha256 = "0wxvc7cbv4qpjl5zxy41863qpzda2ma75pmnnqx29qyh3rxp7biw";
+                        })
+            ];
   meta = {
     description = "Prettyprint and compare Data values";
     license = self.stdenv.lib.licenses.bsd3;
