655ba1e418f72317b86f540dc75ffc9dfc6fdebe
Lluís Batlle i Rossell <viric@viric.name>
2013-10-20 18:08:15 +0200

    freeciv: update to 2.4.0

--- pkgs/games/freeciv/default.nix
+++ pkgs/games/freeciv/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, zlib, bzip2, pkgconfig
+{ stdenv, fetchurl, zlib, bzip2, pkgconfig, curl, lzma, gettext
 , sdlClient ? true, SDL, SDL_mixer, SDL_image, SDL_ttf, SDL_gfx, freetype
 , gtkClient ? false, gtk
 , server ? true, readline }:
@@ -8,16 +8,16 @@ let
   client = sdlClient || gtkClient;
 in
 stdenv.mkDerivation rec {
-  name = "freeciv-2.3.1";
+  name = "freeciv-2.4.0";
 
   src = fetchurl {
     url = "mirror://sourceforge/freeciv/${name}.tar.bz2";
-    sha256 = "1n3ak0y9hj9kha0r3cdbi8zb47vrgal1jsbblamqgwwwgzy8cri3";
+    sha256 = "1bc01pyihsrby6w95n49gi90ggp40dyxsy4kmlmwcakxfxprwakv";
   };
 
   nativeBuildInputs = [ pkgconfig ];
 
-  buildInputs = [ zlib bzip2 ]
+  buildInputs = [ zlib bzip2 curl lzma gettext ]
     ++ optionals sdlClient [ SDL SDL_mixer SDL_image SDL_ttf SDL_gfx freetype ]
     ++ optional gtkClient gtk
     ++ optional server readline;
