a21f0b604a6c853bc83491219f8bda9025692010
Oliver Charles <ollie@ocharles.org.uk>
2013-10-23 17:15:37 +0100

    haskellPackages.linear: Update to 1.3.1

--- pkgs/development/libraries/haskell/linear/default.nix
+++ pkgs/development/libraries/haskell/linear/default.nix
@@ -1,19 +1,19 @@
-{ cabal, distributive, doctest, filepath, hashable, HUnit, lens
-, reflection, semigroupoids, semigroups, simpleReflect, tagged
-, testFramework, testFrameworkHunit, transformers
+{ cabal, binary, distributive, doctest, filepath, hashable, HUnit
+, lens, reflection, semigroupoids, semigroups, simpleReflect
+, tagged, testFramework, testFrameworkHunit, transformers
 , unorderedContainers, vector
 }:
 
 cabal.mkDerivation (self: {
   pname = "linear";
-  version = "1.3";
-  sha256 = "0b5qjsbdkqv0h1236lv2nisjh9yz7gc5bd6xv6i8q5jryzs43pi9";
+  version = "1.3.1";
+  sha256 = "1s07qbdi12rc4djk4s0ds5sh79qcqfmgrbwfj1ygskq3ra88qqsa";
   buildDepends = [
-    distributive hashable reflection semigroupoids semigroups tagged
-    transformers unorderedContainers vector
+    binary distributive hashable reflection semigroupoids semigroups
+    tagged transformers unorderedContainers vector
   ];
   testDepends = [
-    doctest filepath HUnit lens simpleReflect testFramework
+    binary doctest filepath HUnit lens simpleReflect testFramework
     testFrameworkHunit
   ];
   meta = {
@@ -21,5 +21,6 @@ cabal.mkDerivation (self: {
     description = "Linear Algebra";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    maintainers = [ self.stdenv.lib.maintainers.ocharles ];
   };
 })
