6d059b2ee5bff0f7ef679056dc9877e18256e711
Rok Garbas <rok@garbas.si>
2013-11-02 01:46:30 +0100

    ejabberd: updated to 2.1.13

--- pkgs/servers/xmpp/ejabberd/default.nix
+++ pkgs/servers/xmpp/ejabberd/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, expat, erlang, zlib, openssl, pam, lib}:
 
 stdenv.mkDerivation rec {
-  version = "2.1.10";
-  name = "ejabberd-2.1.10";
+  version = "2.1.13";
+  name = "ejabberd-${version}";
   src = fetchurl {
-    url = http://www.process-one.net/downloads/ejabberd/2.1.10/ejabberd-2.1.10.tar.gz;
-    sha256 = "1gijv6d90w9fq0as2l0mp0fn65jihgd86nxry98pv6liks4fbhlx";
+    url = "http://www.process-one.net/downloads/ejabberd/${version}/${name}.tgz";
+    sha256 = "0vf8mfrx7vr3c5h3nfp3qcgwf2kmzq20rjv1h9sk3nimwir1q3d8";
   };
   buildInputs = [ expat erlang zlib openssl pam ];
   patchPhase = ''
@@ -13,9 +13,9 @@ stdenv.mkDerivation rec {
   '';
   preConfigure = ''
     cd src
-  '';  
+  '';
   configureFlags = ["--enable-pam"];
-  
+
   meta = {
     description = "Open-source XMPP application server written in Erlang";
     license = "GPLv2";
