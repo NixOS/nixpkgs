fd8d833a8674853219413746369ae0c48de576e6
Mathijs Kwik <mathijs@bluescreen303.nl>
2013-11-05 16:57:11 +0100

    switch default gccgo to 4.8

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2670,8 +2670,7 @@ let
     inherit fetchurl stdenv;
   });
 
-  # gccgo46 does not work. I set 4.7 then.
-  gccgo = gccgo47;
+  gccgo = gccgo48;
 
   gccgo47 = wrapGCC (gcc47_real.gcc.override {
     name = "gccgo";
