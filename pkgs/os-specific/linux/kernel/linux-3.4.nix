30abda84a0d116b934068b8cff9f69c687af1095
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-11-06 10:29:27 +0100

    linux: Update to 3.4.68
    (cherry picked from commit 07ccfe6af87d05c372b10b730f9ee799305d86dc)

--- pkgs/os-specific/linux/kernel/linux-3.4.nix
+++ pkgs/os-specific/linux/kernel/linux-3.4.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.4.67";
+  version = "3.4.68";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1ndzlwhxvlm4lr86240h3ysxhmdkgdcp0n6qhid5nwrmxf5fpb1d";
+    sha256 = "0z1gbz13px37ny7652cy1s5az5g6yh3bnz4zayab72rzmhgbpsyh";
   };
 
   features.iwlwifi = true;
