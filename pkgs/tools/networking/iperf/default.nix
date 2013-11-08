9b35dac0c0e0364559a094313d0708f73abef055
Domen Kožar <domen@dev.si>
2013-11-08 16:02:51 +0100

    iperf: 2.0.4 -> 2.0.5

--- pkgs/tools/networking/iperf/default.nix
+++ pkgs/tools/networking/iperf/default.nix
@@ -1,16 +1,17 @@
 { stdenv, fetchurl }:
 
-stdenv.mkDerivation {
-  name = "iperf-2.0.4";
+stdenv.mkDerivation rec {
+  name = "iperf-2.0.5";
 
   src = fetchurl {
-    url = mirror://sourceforge/iperf/iperf-2.0.4.tar.gz;
-    sha256 = "0i3r75prbyxs56rngjbrag8rg480ki3daaa924krrafng30z2liv";
+    url = "mirror://sourceforge/iperf/${name}.tar.gz";
+    sha256 = "0nr6c81x55ihs7ly2dwq19v9i1n6wiyad1gacw3aikii0kzlwsv3";
   };
 
-  meta = {
-    homepage = ""; 
-    description = "tool to measure IP bandwidth using UDP or TCP";
+  meta = with stdenv.lib; {
+    homepage = "http://sourceforge.net/projects/iperf/"; 
+    description = "Tool to measure IP bandwidth using UDP or TCP";
+    platforms = platforms.linux;
     license = "as-is";
   };
 }
