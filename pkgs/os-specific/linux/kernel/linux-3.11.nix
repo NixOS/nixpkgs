321bb99a72cc9ea3e751c6e674b96b9d682f5be3
Ricardo M. Correia <rcorreia@wizy.org>
2013-11-08 17:30:00 +0100

    linux: Update to 3.11.7

--- pkgs/os-specific/linux/kernel/linux-3.11.nix
+++ pkgs/os-specific/linux/kernel/linux-3.11.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.11.6";
+  version = "3.11.7";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0klbyx6qy3ifwrwh5w7yzk6m6jd32flkk73z95bih3ihmbnbzlvs";
+    sha256 = "1nb93pchv72c7jibv1kvkmgkg2556gb9a0xx21nf9mclf46a9dx9";
   };
 
   features.iwlwifi = true;
