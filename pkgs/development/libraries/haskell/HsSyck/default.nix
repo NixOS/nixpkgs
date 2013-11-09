cbaf1138582b61f44658b4566c4f9170096ba31b
Peter Simons <simons@cryp.to>
2013-11-09 19:28:24 +0100

    haskell-HsSyck: update to version 0.51

--- pkgs/development/libraries/haskell/HsSyck/default.nix
+++ pkgs/development/libraries/haskell/HsSyck/default.nix
@@ -1,10 +1,10 @@
-{ cabal, syb, utf8String }:
+{ cabal, hashtables, syb, utf8String }:
 
 cabal.mkDerivation (self: {
   pname = "HsSyck";
-  version = "0.50";
-  sha256 = "0ap675i2fngvd1nw1dk8p2fz4nbd2aq5ci8dsvpcjbp28y9j2blm";
-  buildDepends = [ syb utf8String ];
+  version = "0.51";
+  sha256 = "13y5vbzrix33dzwhdrvng2ds2dnipkxn1h22jvbf47rwnkyh2g49";
+  buildDepends = [ hashtables syb utf8String ];
   meta = {
     description = "Fast, lightweight YAML loader and dumper";
     license = self.stdenv.lib.licenses.bsd3;
