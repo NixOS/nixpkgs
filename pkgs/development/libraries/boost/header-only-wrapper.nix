f923c3220485b057cacbabb3396ba42d1dbb2b97
Aristid Breitkreuz <aristidb@gmail.com>
2013-11-12 23:28:27 +0100

    boost: fix hardcoded hash
    Closes #1209
    
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- pkgs/development/libraries/boost/header-only-wrapper.nix
+++ pkgs/development/libraries/boost/header-only-wrapper.nix
@@ -8,10 +8,7 @@ in
 stdenv.mkDerivation {
   name = "boost-headers-${version}";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/boost/${pkgid}.tar.bz2";
-    sha256 = "07df925k56pbz3gvhxpx54aij34qd40a7sxw4im11brnwdyr4zh4";
-  };
+  src = boost.src;
 
   phases = [ "installPhase" ];
 
