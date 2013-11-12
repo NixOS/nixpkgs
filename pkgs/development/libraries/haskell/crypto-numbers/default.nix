f646bb6c8bdff30c27d50ffb7456d59c86ead268
Peter Simons <simons@cryp.to>
2013-11-12 11:16:02 +0100

    haskell-crypto-numbers: update to version 0.2.3

--- pkgs/development/libraries/haskell/crypto-numbers/default.nix
+++ pkgs/development/libraries/haskell/crypto-numbers/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "crypto-numbers";
-  version = "0.2.2";
-  sha256 = "1ia39al01hb65h23ql0mr5vwzj8slv98i7a22cix8p0b6an1w3vv";
+  version = "0.2.3";
+  sha256 = "0nx2mlf40127j7vas7liqy2yzfg4alfaxcjilcxk99kavpaanzgp";
   buildDepends = [ cryptoRandom vector ];
   testDepends = [
     byteable cryptoRandom HUnit QuickCheck testFramework
