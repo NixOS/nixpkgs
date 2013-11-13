e815e4026a6815fbd54c1a41769bbb38d3d3e17a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-11-13 11:08:19 +0100

    mediawiki: Update to 1.20.7

--- nixos/modules/services/web-servers/apache-httpd/mediawiki.nix
+++ nixos/modules/services/web-servers/apache-httpd/mediawiki.nix
@@ -72,11 +72,11 @@ let
 
   # Unpack Mediawiki and put the config file in its root directory.
   mediawikiRoot = pkgs.stdenv.mkDerivation rec {
-    name= "mediawiki-1.20.5";
+    name= "mediawiki-1.20.7";
 
     src = pkgs.fetchurl {
       url = "http://download.wikimedia.org/mediawiki/1.20/${name}.tar.gz";
-      sha256 = "0ix6khrilfdncjqnh41xjs0bd49i1q0rywycjaixjfpwj6vjbqbl";
+      sha256 = "0cdl2mq3nw1jymanlxn7pi3qbf5y5003q53kmc8dip73nvrwnfxm";
     };
 
     skins = config.skins;
