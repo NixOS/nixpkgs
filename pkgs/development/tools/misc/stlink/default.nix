fbfa569efddd383509c9346b6d577192f0f386d0
Bjørn Forsman <bjorn.forsman@gmail.com>
2013-11-13 23:28:24 +0100

    stlink: fix chroot build
    Add sha256 attribute to fetchgit to make it a fixed-output derivation
    (needed for chroot builds).

--- pkgs/development/tools/misc/stlink/default.nix
+++ pkgs/development/tools/misc/stlink/default.nix
@@ -13,6 +13,7 @@ stdenv.mkDerivation {
   src = fetchgit {
     url = git://github.com/texane/stlink.git;
     rev = "5be889e3feb75fc7f594012c4855b4dc16940050";
+    sha256 = "1xbfr1kz4k6hhl0xpqn1vi83fdynjxx3ymn9gb7b0pb7h6ncjcyr";
   };
 
   buildInputs = [ autoconf automake libtool pkgconfig libusb1 ];
