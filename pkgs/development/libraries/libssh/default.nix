1812522f2a574bde1871e5691fcb645a02f9e7e6
Vladimír Čunát <vcunat@gmail.com>
2013-11-17 14:48:42 +0100

    libssh: bugfix update + CVE-2013-0176
    It seems an old DoS-only problem.

--- pkgs/development/libraries/libssh/default.nix
+++ pkgs/development/libraries/libssh/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, cmake, zlib, libgcrypt }:
 
 stdenv.mkDerivation rec {
-  name = "libssh-0.5.3";
+  name = "libssh-0.5.5";
 
   src = fetchurl {
-    url = "https://red.libssh.org/attachments/download/38/${name}.tar.gz";
-    sha256 = "1w6s217vjq0w3v5i0c5ql6m0ki1yz05g9snah3azxfkl9k4schpd";
+    url = "https://red.libssh.org/attachments/download/51/${name}.tar.gz";
+    sha256 = "17cfdff4hc0ijzrr15biq29fiabafz0bw621zlkbwbc1zh2hzpy0";
   };
 
   buildInputs = [ zlib libgcrypt ];
