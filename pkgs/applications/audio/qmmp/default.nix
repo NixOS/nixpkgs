452b25b6c33c04a745008457fcb9f8f45de054c0
Bjørn Forsman <bjorn.forsman@gmail.com>
2013-11-17 12:02:37 +0100

    qmmp: add meta.respositories
    This metadata is used by the tool @phreedom is writing to automatically
    find package updates.

--- pkgs/applications/audio/qmmp/default.nix
+++ pkgs/applications/audio/qmmp/default.nix
@@ -55,5 +55,6 @@ stdenv.mkDerivation rec {
     license = licenses.gpl2;
     platforms = platforms.linux;
     maintainers = [maintainers.bjornfor];
+    repositories.svn = http://qmmp.googlecode.com/svn/;
   };
 }
