017b36a5dfff7d53521a7d13dbef13b733d45ff7
Vladimír Čunát <vcunat@gmail.com>
2013-11-17 15:10:46 +0100

    dbus_python: update 1.1.1 -> 1.2.0

--- pkgs/development/python-modules/dbus/default.nix
+++ pkgs/development/python-modules/dbus/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, python, pkgconfig, dbus, dbus_glib, dbus_tools }:
 
 stdenv.mkDerivation rec {
-  name = "dbus-python-1.1.1";
+  name = "dbus-python-1.2.0";
 
   src = fetchurl {
     url = "http://dbus.freedesktop.org/releases/dbus-python/${name}.tar.gz";
-    sha256 = "122yj5y0mndk9axh735qvwwckck6s6x0q84dw6p97mplp17wl5w9";
+    sha256 = "1py62qir966lvdkngg0v8k1khsqxwk5m4s8nflpk1agk5f5nqb71";
   };
 
   postPatch = "patchShebangs .";
