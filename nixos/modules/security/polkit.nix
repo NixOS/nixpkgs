bc56bb7546dc49178100eab4b82fcc2bb4e87e48
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-11-18 18:01:07 +0100

    polkit: Add some examples

--- nixos/modules/security/polkit.nix
+++ nixos/modules/security/polkit.nix
@@ -23,7 +23,15 @@ in
       default = "";
       example =
         ''
-          TODO
+          /* Log authorization checks. */
+          polkit.addRule(function(action, subject) {
+            polkit.log("user " +  subject.user + " is attempting action " + action.id + " from PID " + subject.pid);
+          });
+
+          /* Allow any local user to do anything (dangerous!). */
+          polkit.addRule(function(action, subject) {
+            if (subject.local) return "yes";
+          });
         '';
       description =
         ''
