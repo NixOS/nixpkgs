c853841c8c69b938378cdd00edc9afa683402364
aszlig <aszlig@redmoonstudios.org>
2013-11-22 15:28:25 +0100

    log4cxx: Fix build by adding missing includes.
    This adds a bit too many includes but in the end it won't hurt on the
    size of the build output because those are optimized away. But having a
    whole bunch of sed expressions just for selectively adding includes just
    clutters up the package expression.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/development/libraries/log4cxx/default.nix
+++ pkgs/development/libraries/log4cxx/default.nix
@@ -9,7 +9,18 @@ stdenv.mkDerivation {
     url = http://apache.mirrors.hoobly.com/logging/log4cxx/0.10.0/apache-log4cxx-0.10.0.tar.gz;    
     sha256 = "130cjafck1jlqv92mxbn47yhxd2ccwwnprk605c6lmm941i3kq0d";
   };
-  
+
+  postPatch = ''
+    sed -i -e '1,/^#include/ {
+      /^#include/i \
+        #include <cstdio> \
+        #include <cstdlib> \
+        #include <cstring>
+    }' src/examples/cpp/console.cpp \
+       src/main/cpp/inputstreamreader.cpp \
+       src/main/cpp/socketoutputstream.cpp
+  '';
+
   buildInputs = [autoconf automake libtool libxml2 cppunit boost apr aprutil db45 expat];
 
   meta = {
