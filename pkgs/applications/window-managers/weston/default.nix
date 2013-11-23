821cfa2c736cd50341fbbcb05742875ec21b4640
Vladimír Čunát <vcunat@gmail.com>
2013-11-23 23:30:47 +0100

    wayland, weston: update to 1.3.1, fix weston build
    Also make Hydra build weston, so we can see the breakages in future.
    
    (cherry picked from commit 30666ed5ad0169611cb7a6ab80a9e941fe7dd9ca)
    
    Conflicts (take later = master):
    	pkgs/development/libraries/wayland/default.nix

--- pkgs/applications/window-managers/weston/default.nix
+++ pkgs/applications/window-managers/weston/default.nix
@@ -2,30 +2,32 @@
 , cairo, libxcb, libXcursor, x11, udev, libdrm, mtdev
 , libjpeg, pam, autoconf, automake, libtool }:
 
-let version = "1.0.5"; in
+let version = "1.3.1"; in
 
 stdenv.mkDerivation rec {
   name = "weston-${version}";
 
   src = fetchurl {
     url = "http://wayland.freedesktop.org/releases/${name}.tar.xz";
-    sha256 = "0g2k82pnlxl8b70ykazj7kn8xffjfsmgcgx427qdrm4083z2hgm0";
+    sha256 = "1isvh66irrz707r69495767n5yxp07dvy0xx6mj1mbj1n4s1657p";
   };
 
   buildInputs = [ pkgconfig wayland mesa libxkbcommon
     cairo libxcb libXcursor x11 udev libdrm mtdev
-    libjpeg pam autoconf automake libtool ];
+    libjpeg pam /*autoconf automake libtool*/ ];
 
-  preConfigure = "autoreconf -vfi";
+  #preConfigure = "autoreconf -vfi";
 
-  # prevent install target to chown root weston-launch, which fails
-  configureFlags = ''
-    --disable-setuid-install
-  '';
+  NIX_CFLAGS_COMPILE = "-I${libdrm}/include/libdrm";
+
+  configureFlags = [
+    "--disable-setuid-install" # prevent install target to chown root weston-launch, which fails
+  ];
 
   meta = {
     description = "Reference implementation of a Wayland compositor";
     homepage = http://wayland.freedesktop.org/;
     license = stdenv.lib.licenses.mit;
+    platforms = stdenv.lib.platforms.linux;
   };
 }
