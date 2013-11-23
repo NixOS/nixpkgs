e228e4b2ccb8ddbf2f71f1241b94fe5795f7b177
Vladimír Čunát <vcunat@gmail.com>
2013-11-23 10:17:52 +0100

    pixman: update to 0.32.4
    (cherry picked from commit f7b39cce8c27c021037042f7c5a9d8d92866bd7a)
    
    Conflicts (taken later = master):
    	pkgs/development/libraries/pixman/default.nix

--- pkgs/development/libraries/pixman/default.nix
+++ pkgs/development/libraries/pixman/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, pkgconfig, perl, withPNG ? true, libpng, glib /*just passthru*/ }:
 
 stdenv.mkDerivation rec {
-  name = "pixman-0.30.2";
+  name = "pixman-0.32.4";
 
   src = fetchurl {
     url = "http://cairographics.org/releases/${name}.tar.gz";
-    sha256 = "1sgnpx34pj3245a9v8056jddc4cg4xxkqdjvvw6k2hnprhh8k65x";
+    sha256 = "113ycngcssbrps217dyajq96hm9xghsfch82h14yffla1r1fviw0";
   };
 
   nativeBuildInputs = [ pkgconfig perl ];
