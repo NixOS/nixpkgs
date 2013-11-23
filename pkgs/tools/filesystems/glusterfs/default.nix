e1b0d0d8edc3e5ed0b558be9f8ad82d9654b8c2f
Danny Wilson <danny@prime.vc>
2013-11-23 23:12:08 +0100

    GlusterFS (server only) compiles on SmartOS

--- pkgs/tools/filesystems/glusterfs/default.nix
+++ pkgs/tools/filesystems/glusterfs/default.nix
@@ -9,8 +9,8 @@ let
     url="http://download.gluster.org/pub/gluster/glusterfs/3.4/3.4.2/glusterfs-3.4.2.tar.gz";
     sha256="1vzdihsy4da11jsa46n1n2xk6d40g7v0zrlqvs3pb9k07fql5kag";
   };
-  buildInputs = [
-    fuse bison flex openssl python ncurses readline
+  buildInputs = (stdenv.lib.optional (!stdenv.isSunOS) [fuse]) ++ [
+    bison flex openssl python ncurses readline
   ];
 in
 stdenv.mkDerivation
@@ -19,10 +19,31 @@ rec {
   inherit buildInputs;
   configureFlags = [
     ''--with-mountutildir="$out/sbin"''
+    (stdenv.lib.optionalString stdenv.isSunOS "--disable-fusermount")
     ];
   src = fetchurl {
     inherit (s) url sha256;
   };
+
+  patches = stdenv.lib.optional stdenv.isSunOS [./disable-non-std-dir-testing.diff];
+
+  preConfigure = stdenv.lib.optionalString stdenv.isSunOS ''
+    find . -name \*.c -or -name \*.h -exec sed -i \
+      -e 's|\su_int| uint|g'\
+      -e 's|<sys/cdefs.h>|<sys/types.h>|g' {} \;
+
+    mv ./api/src/glfs.h ./api/src/glfs.h.orig
+    cat > ./api/src/glfs.h <<-EOF
+      #ifdef        __cplusplus
+      # define __BEGIN_DECLS  extern "C" {
+      # define __END_DECLS    }
+      #else
+      # define __BEGIN_DECLS
+      # define __END_DECLS
+      #endif
+    EOF
+    cat ./api/src/glfs.h.orig >> ./api/src/glfs.h
+  '';
 
   meta = {
     inherit (s) version;
