d809fd135f56f9eabbe2d8d7bd08ddabd88ba7a5
aszlig <aszlig@redmoonstudios.org>
2013-11-24 07:29:48 +0100

    protobuf: Update to new upstream version 2.5.0.
    Starting from version 32, Chromium now relies on protobuf version 2.5.0,
    so let's update it, especially because it doesn't have backwards
    compatible changes in the release notes.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/development/libraries/protobuf/default.nix
+++ pkgs/development/libraries/protobuf/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, zlib }:
 
 stdenv.mkDerivation rec {
-  name = "protobuf-2.4.1";
+  name = "protobuf-2.5.0";
 
   src = fetchurl {
     url = "http://protobuf.googlecode.com/files/${name}.tar.bz2";
-    sha256 = "1gxhfhk37jyjq1z4y3d4bz4i1fk2an5ydhk5kjzlp0rhfcs5516g";
+    sha256 = "0xxn9gxhvsgzz2sgmihzf6pf75clr05mqj6218camwrwajpcbgqk";
   };
 
   buildInputs = [ zlib ];
