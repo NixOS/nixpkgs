55f40dfbec7df7ac2cf0e8e9ecb4691165c08363
Vladimír Čunát <vcunat@gmail.com>
2013-11-24 12:43:22 +0100

    graphite2: clean unneeded dependencies
    We almost created cycles in harfbuzz.

--- pkgs/development/libraries/silgraphite/graphite2.nix
+++ pkgs/development/libraries/silgraphite/graphite2.nix
@@ -1,17 +1,15 @@
-{ stdenv, fetchurl, pkgconfig, freetype, libXft, pango, fontconfig, cmake }:
+{ stdenv, fetchurl, pkgconfig, freetype, cmake }:
 
 stdenv.mkDerivation rec {
   version = "1.2.3";
   name = "graphite2-${version}";
-  
+
   src = fetchurl {
     url = "mirror://sourceforge/silgraphite/graphite2/${name}.tgz";
     sha256 = "1xgwnd81gm6p293x8paxb3yisnvpj5qnv1dzr7bjdi7b7h00ls7g";
   };
 
-  buildInputs = [pkgconfig freetype libXft pango fontconfig cmake];
-
-  NIX_CFLAGS_COMPILE = "-I${freetype}/include/freetype2";
+  buildInputs = [ pkgconfig freetype cmake ];
 
   meta = {
     description = "An advanced font engine";
