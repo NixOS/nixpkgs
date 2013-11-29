26953e0b6b3e6885228630988891fbc3738a8a35
Jason "Don" O'Conal <lovek323@gmail.com>
2013-11-29 09:36:15 +0000

    texlive-extras: update to latest version

--- pkgs/tools/typesetting/tex/texlive/extra.nix
+++ pkgs/tools/typesetting/tex/texlive/extra.nix
@@ -1,24 +1,26 @@
 args: with args;
 rec {
-  name = "texlive-extra-2013";
+  name = "texlive-extra_2013.20131112";
+
   src = fetchurl {
-    url = mirror://debian/pool/main/t/texlive-extra/texlive-extra_2013.20131010.orig.tar.xz;
-    sha256 = "1wciyjwp0swny22amwcnr6vvdwjy423856q7c3l1sd5b31xfbc18";
+    url    = "mirror://debian/pool/main/t/texlive-extra/texlive-extra/${name}.orig.tar.xz";
+    sha256 = "0qpiig9sz8wx3dhy1jha7rkxrhvpf2cmfx424h68s3ql05nnw65i";
   };
 
-  buildInputs = [texLive xz];
-  phaseNames = ["doCopy"];
+  buildInputs = [ texLive xz ];
+
+  phaseNames = [ "doCopy" ];
+
   doCopy = fullDepEntry (''
     mkdir -p $out/share
     cp -r texmf* $out/
     ln -s $out/texmf* $out/share
-  '') ["minInit" "doUnpack" "defEnsureDir" "addInputs"];
+  '') [ "minInit" "doUnpack" "defEnsureDir" "addInputs" ];
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Extra components for TeXLive";
-    maintainers = [ args.lib.maintainers.raskin ];
-
-    # Actually, arch-independent.. 
-    hydraPlatforms = [];
+    license     = licenses.publicDomain;
+    maintainers = with maintainers; [ lovek323 raskin ];
+    platforms   = platforms.all;
   };
 }
