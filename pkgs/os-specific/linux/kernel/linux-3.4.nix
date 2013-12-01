9ade293be32b0f12913fe8889b504bee7c2c4694
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-12-01 18:20:39 -0500

    linux: Update to 3.4.71
    (cherry picked from commit 3b94410d866c44e8fdd92dbfea25e2a6eb5db4c2)

--- pkgs/os-specific/linux/kernel/linux-3.4.nix
+++ pkgs/os-specific/linux/kernel/linux-3.4.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.4.70";
+  version = "3.4.71";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0l0ln1jmrknvg9ibv13s8b9rzn3ck789y13hpdv04as5wp738v20";
+    sha256 = "0l9bv40mwk45ccmb2pfy574h6zrclrz5vhvmnn61q3z16j269dpp";
   };
 
   features.iwlwifi = true;
