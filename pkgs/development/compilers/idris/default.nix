1d191dd56fce6e2eccef04bd3b0b53471a330a4d
Peter Simons <simons@cryp.to>
2013-12-01 23:29:08 +0100

    haskell-idris: update to version 0.9.10

--- pkgs/development/compilers/idris/default.nix
+++ pkgs/development/compilers/idris/default.nix
@@ -1,21 +1,21 @@
 { cabal, ansiTerminal, ansiWlPprint, binary, boehmgc, Cabal
-, filepath, gmp, happy, haskeline, languageJava, libffi
-, llvmGeneral, llvmGeneralPure, mtl, parsec, parsers, split, text
-, time, transformers, trifecta, unorderedContainers, utf8String
-, vector, vectorBinaryInstances
+, deepseq, filepath, gmp, happy, haskeline, languageJava, mtl
+, network, parsers, split, text, time, transformers, trifecta
+, unorderedContainers, utf8String, vector, vectorBinaryInstances
+, xml
 }:
 
 cabal.mkDerivation (self: {
   pname = "idris";
-  version = "0.9.9.3";
-  sha256 = "1l19xx0xbcwlnnh2w0rmri7wwixffzfrafpbji64nwyx1awz4iab";
+  version = "0.9.10";
+  sha256 = "0sbadjc4kj59f5240036pryxr4b6k6y2zkmszv99wq660mm7a3d3";
   isLibrary = false;
   isExecutable = true;
   buildDepends = [
-    ansiTerminal ansiWlPprint binary Cabal filepath haskeline
-    languageJava libffi llvmGeneral llvmGeneralPure mtl parsec parsers
-    split text time transformers trifecta unorderedContainers
-    utf8String vector vectorBinaryInstances
+    ansiTerminal ansiWlPprint binary Cabal deepseq filepath haskeline
+    languageJava mtl network parsers split text time transformers
+    trifecta unorderedContainers utf8String vector
+    vectorBinaryInstances xml
   ];
   buildTools = [ happy ];
   extraLibraries = [ boehmgc gmp ];
