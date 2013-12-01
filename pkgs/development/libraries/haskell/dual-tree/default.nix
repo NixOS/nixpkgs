a14ba391b8082ba004dd6719514932a0f292e217
Peter Simons <simons@cryp.to>
2013-12-01 23:29:09 +0100

    haskell-dual-tree: update to version 0.2.0.1

--- pkgs/development/libraries/haskell/dual-tree/default.nix
+++ pkgs/development/libraries/haskell/dual-tree/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "dual-tree";
-  version = "0.2";
-  sha256 = "0wasnjkixl6zkskjp18qj3jym3yv3a85i3w5qphgjr3xifbzwnjf";
+  version = "0.2.0.1";
+  sha256 = "0v9kdhnwq8nv30ci5q7n43abl0wag21i06wp8pv1xgrva4lhswm5";
   buildDepends = [ monoidExtras newtype semigroups ];
   jailbreak = true;
   meta = {
