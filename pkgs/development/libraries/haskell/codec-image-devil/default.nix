159649aeb46981b66d4f896f2260804398465c03
Peter Simons <simons@cryp.to>
2013-12-02 20:02:04 +0100

    haskell-codec-image-devil: re-generate with cabal2nix

--- pkgs/development/libraries/haskell/codec-image-devil/default.nix
+++ pkgs/development/libraries/haskell/codec-image-devil/default.nix
@@ -4,10 +4,9 @@ cabal.mkDerivation (self: {
   pname = "Codec-Image-DevIL";
   version = "0.2.3";
   sha256 = "1kv3hns9f0bhfb723nj9szyz3zfqpvy02azzsiymzjz4ajhqmrsz";
-  buildDepends = [ libdevil ];
+  extraLibraries = [ libdevil ];
   meta = {
-    homepage = "http://hackage.haskell.org/package/Codec-Image-DevIL";
-    description = "Simple FFI interface to the DevIL image library";
+    description = "An FFI interface to the DevIL library";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
   };
