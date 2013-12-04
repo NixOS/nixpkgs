86a82ac7973f9937f75446d45c4a784bb625b37c
Nixpkgs Monitor <none@none>
2013-12-04 04:25:26 +0200

    privoxy: update from 3.0.12 to 3.0.21, potentially fixes CVE-2013-2503
    (cherry picked from commit 3837210feddf342c33f99de59dc4a95b36308af2)

--- pkgs/tools/networking/privoxy/default.nix
+++ pkgs/tools/networking/privoxy/default.nix
@@ -1,16 +1,14 @@
 {stdenv, fetchurl, autoconf, automake, zlib, pcre, w3m}:
 
 stdenv.mkDerivation {
-  name = "privoxy-3.0.12";
+  name = "privoxy-3.0.21";
 
   src = fetchurl {
-    url = mirror://sourceforge/ijbswa/Sources/3.0.12%20%28stable%29/privoxy-3.0.12-stable-src.tar.gz;
-    sha256 = "16ngim1p4pb4zk8h7js4zjw280fxqxamasfngixikp2ivmzxl469";
+    url = mirror://sourceforge/ijbswa/Sources/3.0.21%20%28stable%29/privoxy-3.0.21-stable-src.tar.gz;
+    sha256 = "1f6xb7aa47p90c26vqaw74y6drs9gpnhxsgby3mx0awdjh0ydisy";
   };
 
   buildInputs = [automake autoconf zlib pcre w3m];
-
-  patches = [./disable-user-error.patch];
 
   preConfigure = ''
     autoheader
