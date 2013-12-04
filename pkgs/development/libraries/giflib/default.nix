7d6fec63c6b3953fb8de2c6a18e9f8c2c8c6c02b
Evgeny Egorochkin <phreedom@yandex.ru>
2013-12-04 16:56:05 +0200

    giflib: fix documentation

--- pkgs/development/libraries/giflib/default.nix
+++ pkgs/development/libraries/giflib/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl}:
+{stdenv, fetchurl, xmlto, docbook_xml_dtd_412, docbook_xsl, libxml2 }:
 
 stdenv.mkDerivation {
   name = "giflib-4.2.3";
@@ -6,5 +6,7 @@ stdenv.mkDerivation {
     url = mirror://sourceforge/giflib/giflib-4.2.3.tar.bz2;
     sha256 = "0rmp7ipzk42r841bggd7bfqk4p8qsssbp4wcck4qnz7p4rkxbj0a";
   };
+
+  buildInputs = [ xmlto docbook_xml_dtd_412 docbook_xsl libxml2 ];
 }
 
