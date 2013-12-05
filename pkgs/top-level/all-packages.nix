9e81121a133535808ea1af2e732832ec6e1f25d0
Edward Tjörnhammar <ed@cflags.cc>
2013-12-05 23:09:41 +0100

    Adding IntelliJ IDEA13 Community and Ultimate

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8077,6 +8077,8 @@ let
     slibGuile = slibGuile.override { scheme = guile_1_8; };
   };
 
+  ideas = recurseIntoAttrs (callPackage ../applications/editors/idea { });
+
   libquvi = callPackage ../applications/video/quvi/library.nix { };
 
   mi2ly = callPackage ../applications/audio/mi2ly {};
