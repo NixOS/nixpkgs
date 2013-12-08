c1df906704397c2712bb9d1edffda6f97ee1323c
Domen Kožar <domen@dev.si>
2013-12-08 19:19:11 +0100

    vlc: specify meta.platforms

--- pkgs/applications/video/vlc/default.nix
+++ pkgs/applications/video/vlc/default.nix
@@ -45,8 +45,9 @@ stdenv.mkDerivation rec {
       ${freefont_ttf}/share/fonts/truetype/FreeSerifBold.ttf
   '';
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Cross-platform media player and streaming server";
     homepage = http://www.videolan.org/vlc/;
+    platforms = platforms.linux;
   };
 }
