b1c6aabdb16fa26b1f2ece03bddaa1d9df310cde
Peter Simons <simons@cryp.to>
2013-12-09 13:03:23 +0100

    haskell-tasty-ant-xml: update to version 1.0.0.2

--- pkgs/development/libraries/haskell/tasty-ant-xml/default.nix
+++ pkgs/development/libraries/haskell/tasty-ant-xml/default.nix
@@ -4,14 +4,14 @@
 
 cabal.mkDerivation (self: {
   pname = "tasty-ant-xml";
-  version = "1.0.0.1";
-  sha256 = "1yn337dr9clzrkr8kpvm7x07lyb3v8pcijrddqah08k0ds8zpzcj";
+  version = "1.0.0.2";
+  sha256 = "1c13gmqsp33cfqim7aqqjypabkx42rmmhmjj8fpcr0ainpmvwkwi";
   buildDepends = [
     genericDeriving mtl reducers stm tagged tasty transformers xml
   ];
   meta = {
     homepage = "http://github.com/ocharles/tasty-ant-xml";
-    description = "A tasty ingredient to output test results in XML, using the Ant schema. This XML can be consumed by the Jenkins continuous integration framework.";
+    description = "Render tasty output to XML for Jenkins";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [ self.stdenv.lib.maintainers.ocharles ];
