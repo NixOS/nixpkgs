27bdaa32690b828e5c86755e2baed986596b516a
Peter Simons <simons@cryp.to>
2013-12-09 13:03:24 +0100

    haskell-warp: update to version 2.0.0.1

--- pkgs/development/libraries/haskell/warp/default.nix
+++ pkgs/development/libraries/haskell/warp/default.nix
@@ -1,23 +1,23 @@
 { cabal, blazeBuilder, blazeBuilderConduit, caseInsensitive
-, conduit, hashable, hspec, HTTP, httpAttoparsec, httpTypes, HUnit
-, liftedBase, network, networkConduit, QuickCheck, simpleSendfile
-, transformers, unixCompat, void, wai
+, conduit, doctest, hashable, hspec, HTTP, httpAttoparsec, httpDate
+, httpTypes, HUnit, liftedBase, network, networkConduit, QuickCheck
+, simpleSendfile, time, transformers, unixCompat, void, wai
 }:
 
 cabal.mkDerivation (self: {
   pname = "warp";
-  version = "1.3.10.1";
-  sha256 = "1pi2x0gi4r6qy151a9gmfq223yiy53j7prj2pyn00cprr0m4mk2v";
+  version = "2.0.0.1";
+  sha256 = "12n1r1qahngfhrqhlbfjl11rmy5la9mpd0h4ai0lzdxkakb89pxl";
   buildDepends = [
     blazeBuilder blazeBuilderConduit caseInsensitive conduit hashable
-    httpAttoparsec httpTypes liftedBase network networkConduit
+    httpAttoparsec httpDate httpTypes liftedBase network networkConduit
     simpleSendfile transformers unixCompat void wai
   ];
   testDepends = [
-    blazeBuilder blazeBuilderConduit caseInsensitive conduit hashable
-    hspec HTTP httpAttoparsec httpTypes HUnit liftedBase network
-    networkConduit QuickCheck simpleSendfile transformers unixCompat
-    void wai
+    blazeBuilder blazeBuilderConduit caseInsensitive conduit doctest
+    hashable hspec HTTP httpAttoparsec httpDate httpTypes HUnit
+    liftedBase network networkConduit QuickCheck simpleSendfile time
+    transformers unixCompat void wai
   ];
   meta = {
     homepage = "http://github.com/yesodweb/wai";
