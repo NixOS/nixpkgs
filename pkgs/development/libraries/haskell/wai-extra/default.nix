40a309e038d39cca527548e9f8a20c5afef59170
Peter Simons <simons@cryp.to>
2013-12-09 13:03:23 +0100

    haskell-wai-extra: update to version 2.0.0.1

--- pkgs/development/libraries/haskell/wai-extra/default.nix
+++ pkgs/development/libraries/haskell/wai-extra/default.nix
@@ -1,23 +1,23 @@
 { cabal, ansiTerminal, base64Bytestring, blazeBuilder
 , blazeBuilderConduit, caseInsensitive, conduit, dataDefault
-, dateCache, fastLogger, hspec, httpTypes, HUnit, liftedBase
-, network, resourcet, stringsearch, text, time, transformers, void
-, wai, waiLogger, waiTest, word8, zlib, zlibBindings, zlibConduit
+, fastLogger, hspec, httpTypes, HUnit, liftedBase, network
+, resourcet, stringsearch, text, time, transformers, void, wai
+, waiLogger, waiTest, word8, zlib, zlibBindings, zlibConduit
 }:
 
 cabal.mkDerivation (self: {
   pname = "wai-extra";
-  version = "1.3.4.6";
-  sha256 = "09rk9i2fkk4haiq1c6rhcp1p72zw34j9cxsmqnm4jzh6fdsrkq2k";
+  version = "2.0.0.1";
+  sha256 = "16m6scz36yzvyhz098lv6ppxdyk6kmavzyzb42iq0bpyk9m0x2qx";
   buildDepends = [
     ansiTerminal base64Bytestring blazeBuilder blazeBuilderConduit
-    caseInsensitive conduit dataDefault dateCache fastLogger httpTypes
-    liftedBase network resourcet stringsearch text time transformers
-    void wai waiLogger word8 zlibConduit
+    caseInsensitive conduit dataDefault fastLogger httpTypes liftedBase
+    network resourcet stringsearch text time transformers void wai
+    waiLogger word8 zlibConduit
   ];
   testDepends = [
     blazeBuilder conduit dataDefault fastLogger hspec httpTypes HUnit
-    text transformers wai waiTest zlib zlibBindings
+    resourcet text transformers wai waiTest zlib zlibBindings
   ];
   meta = {
     homepage = "http://github.com/yesodweb/wai";
