3242e0d495d3ff12d30c229ba3d2807f61f1e83b
Peter Simons <simons@cryp.to>
2013-12-10 11:03:13 +0100

    haskell-wai-app-static: update to version 2.0.0.1

--- pkgs/development/libraries/haskell/wai-app-static/default.nix
+++ pkgs/development/libraries/haskell/wai-app-static/default.nix
@@ -1,18 +1,18 @@
 { cabal, base64Bytestring, blazeBuilder, blazeHtml, blazeMarkup
 , cereal, cryptoApi, cryptoConduit, cryptohashCryptoapi, fileEmbed
-, hspec, httpDate, httpTypes, mimeTypes, network, systemFileio
-, systemFilepath, text, time, transformers, unixCompat
-, unorderedContainers, wai, waiTest, zlib
+, filepath, hspec, httpDate, httpTypes, mimeTypes, network
+, systemFileio, systemFilepath, text, time, transformers
+, unixCompat, unorderedContainers, wai, waiTest, zlib
 }:
 
 cabal.mkDerivation (self: {
   pname = "wai-app-static";
-  version = "2.0.0";
-  sha256 = "0zbjwghkg4xsjfxq74s6c041mvkj168r8nhh946yz6n660bkisai";
+  version = "2.0.0.1";
+  sha256 = "0c02j61a62dpl9i736q8cv0b9kw17akkws7waqa4zcbad1wyiqps";
   buildDepends = [
     base64Bytestring blazeBuilder blazeHtml blazeMarkup cereal
-    cryptoApi cryptoConduit cryptohashCryptoapi fileEmbed httpDate
-    httpTypes mimeTypes systemFileio systemFilepath text time
+    cryptoApi cryptoConduit cryptohashCryptoapi fileEmbed filepath
+    httpDate httpTypes mimeTypes systemFileio systemFilepath text time
     transformers unixCompat unorderedContainers wai zlib
   ];
   testDepends = [
