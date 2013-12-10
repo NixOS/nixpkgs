9f3dc64b9085e2b91a07062d6b2cdd44034fd9fc
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-12-10 12:55:55 +0100

    apr: Update to 1.5.0

--- pkgs/development/libraries/apr/default.nix
+++ pkgs/development/libraries/apr/default.nix
@@ -1,26 +1,26 @@
 { stdenv, fetchurl }:
 
-let
-   inherit (stdenv.lib) optionals;
-in
-
 stdenv.mkDerivation rec {
-  name = "apr-1.4.8";
+  name = "apr-1.5.0";
 
   src = fetchurl {
     url = "mirror://apache/apr/${name}.tar.bz2";
-    md5 = "ce2ab01a0c3cdb71cf0a6326b8654f41";
+    md5 = "cc93bd2c12d0d037f68e21cc6385dc31";
   };
 
-  patches = optionals stdenv.isDarwin [ ./darwin_fix_configure.patch ];
+  patches = stdenv.lib.optionals stdenv.isDarwin [ ./darwin_fix_configure.patch ];
 
   configureFlags =
     # Including the Windows headers breaks unistd.h.
     # Based on ftp://sourceware.org/pub/cygwin/release/libapr1/libapr1-1.3.8-2-src.tar.bz2
     stdenv.lib.optional (stdenv.system == "i686-cygwin") "ac_cv_header_windows_h=no";
 
+  enableParallelBuilding = true;
+
   meta = {
     homepage = http://apr.apache.org/;
     description = "The Apache Portable Runtime library";
+    platforms = stdenv.lib.platforms.all;
+    maintainers = [ stdenv.lib.maintainers.eelco ];
   };
 }
