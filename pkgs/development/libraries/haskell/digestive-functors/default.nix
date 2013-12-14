9fe0d3696346f41d821b36cc8b1a2cc6e0eb5f0a
Peter Simons <simons@cryp.to>
2013-12-14 11:30:18 +0100

    haskell-digestive-functors: update to version 0.6.2.0

--- pkgs/development/libraries/haskell/digestive-functors/default.nix
+++ pkgs/development/libraries/haskell/digestive-functors/default.nix
@@ -1,15 +1,15 @@
 { cabal, HUnit, mtl, QuickCheck, testFramework, testFrameworkHunit
-, testFrameworkQuickcheck2, text
+, testFrameworkQuickcheck2, text, time
 }:
 
 cabal.mkDerivation (self: {
   pname = "digestive-functors";
-  version = "0.6.1.1";
-  sha256 = "05px6xal6kzppph5nm9w60vsdz0d9gng8zp26ipwpxzk57jg4jjw";
-  buildDepends = [ mtl text ];
+  version = "0.6.2.0";
+  sha256 = "1d07ws5s34x9sviq7mfkl6fh1rl28r5x1rmgbkcxil5h6gxn5mi7";
+  buildDepends = [ mtl text time ];
   testDepends = [
     HUnit mtl QuickCheck testFramework testFrameworkHunit
-    testFrameworkQuickcheck2 text
+    testFrameworkQuickcheck2 text time
   ];
   jailbreak = true;
   meta = {
