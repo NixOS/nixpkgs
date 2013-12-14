60e76fd0c221827db6aa4cc7489de6db31bb8690
Arseniy Seroka <ars.seroka@gmail.com>
2013-12-15 00:27:22 +0400

    BerkeleyDB: update to 0.54

--- pkgs/development/perl-modules/BerkeleyDB/default.nix
+++ pkgs/development/perl-modules/BerkeleyDB/default.nix
@@ -1,11 +1,11 @@
 {buildPerlPackage, fetchurl, db4}:
 
 buildPerlPackage rec {
-  name = "BerkeleyDB-0.36";
+  name = "BerkeleyDB-0.54";
   
   src = fetchurl {
     url = "mirror://cpan/authors/id/P/PM/PMQS/${name}.tar.gz";
-    sha256 = "07xf50riarb60l1h6m2dqmql8q5dij619712fsgw7ach04d8g3z1";
+    sha256 = "010e66d0034b93a8397c600da320611149aef7861eaf1f93b95e49ae37b825b8";
   };
 
   preConfigure = ''
