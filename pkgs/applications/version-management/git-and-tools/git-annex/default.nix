f449007adecfce312ea87c54b2f9880ddf65b041
Peter Simons <simons@cryp.to>
2013-12-14 11:30:18 +0100

    haskell-git-annex: update to version 5.20131213

--- pkgs/applications/version-management/git-and-tools/git-annex/default.nix
+++ pkgs/applications/version-management/git-and-tools/git-annex/default.nix
@@ -4,18 +4,18 @@
 , extensibleExceptions, feed, filepath, git, gnupg1, gnutls, hamlet
 , hinotify, hS3, hslogger, HTTP, httpConduit, httpTypes, IfElse
 , json, lsof, MissingH, MonadCatchIOTransformers, monadControl, mtl
-, network, networkInfo, networkMulticast, networkProtocolXmpp
-, openssh, perl, QuickCheck, random, regexTdfa, rsync
-, SafeSemaphore, SHA, stm, tasty, tastyHunit, tastyQuickcheck, text
-, time, transformers, unixCompat, utf8String, uuid, wai, waiLogger
-, warp, which, xmlConduit, xmlTypes, yesod, yesodCore, yesodDefault
-, yesodForm, yesodStatic
+, network, networkConduit, networkInfo, networkMulticast
+, networkProtocolXmpp, openssh, perl, QuickCheck, random, regexTdfa
+, rsync, SafeSemaphore, SHA, stm, tasty, tastyHunit
+, tastyQuickcheck, text, time, transformers, unixCompat, utf8String
+, uuid, wai, waiLogger, warp, which, xmlConduit, xmlTypes, yesod
+, yesodCore, yesodDefault, yesodForm, yesodStatic
 }:
 
 cabal.mkDerivation (self: {
   pname = "git-annex";
-  version = "5.20131130";
-  sha256 = "0px918wzv9zqxz7wc6rx2ay8rizbckw79yinhisjvp3y5lldyjj1";
+  version = "5.20131213";
+  sha256 = "0mwslkz0kklp4phlsf8hibh97sabdnigls7hr9725wb0ncfa85yn";
   isLibrary = false;
   isExecutable = true;
   buildDepends = [
@@ -24,11 +24,11 @@ cabal.mkDerivation (self: {
     editDistance extensibleExceptions feed filepath gnutls hamlet
     hinotify hS3 hslogger HTTP httpConduit httpTypes IfElse json
     MissingH MonadCatchIOTransformers monadControl mtl network
-    networkInfo networkMulticast networkProtocolXmpp QuickCheck random
-    regexTdfa SafeSemaphore SHA stm tasty tastyHunit tastyQuickcheck
-    text time transformers unixCompat utf8String uuid wai waiLogger
-    warp xmlConduit xmlTypes yesod yesodCore yesodDefault yesodForm
-    yesodStatic
+    networkConduit networkInfo networkMulticast networkProtocolXmpp
+    QuickCheck random regexTdfa SafeSemaphore SHA stm tasty tastyHunit
+    tastyQuickcheck text time transformers unixCompat utf8String uuid
+    wai waiLogger warp xmlConduit xmlTypes yesod yesodCore yesodDefault
+    yesodForm yesodStatic
   ];
   buildTools = [ bup curl git gnupg1 lsof openssh perl rsync which ];
   configureFlags = "-fS3
