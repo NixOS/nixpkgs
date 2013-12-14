f5587ef7519ba95f8c3da08cf5f86eb2b2cd25f2
Peter Simons <simons@cryp.to>
2013-12-14 11:30:19 +0100

    haskell-yesod-form: update to version 1.3.4.1

--- pkgs/development/libraries/haskell/yesod-form/default.nix
+++ pkgs/development/libraries/haskell/yesod-form/default.nix
@@ -6,8 +6,8 @@
 
 cabal.mkDerivation (self: {
   pname = "yesod-form";
-  version = "1.3.4";
-  sha256 = "1ajdbr7lz6jxh5r0iapc7c70pmyrac8rw8qzv4vh8lbvmx0z4ck4";
+  version = "1.3.4.1";
+  sha256 = "01h9k7qwp26i57akbm9236r8gbil4mh2317caa4bcva7m1dclvpj";
   buildDepends = [
     aeson attoparsec blazeBuilder blazeHtml blazeMarkup cryptoApi
     dataDefault emailValidate hamlet network persistent resourcet
