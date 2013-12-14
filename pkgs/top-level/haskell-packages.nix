a9a5bde463e59b1a186de15fff3be5e1943efe61
Peter Simons <simons@cryp.to>
2013-12-14 11:28:54 +0100

    haskell-HTTP: update to version 4000.2.10

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -151,7 +151,7 @@ let result = let callPackage = x : y : modifyPrio (newScope result.finalReturn x
     haskellSrc   = self.haskellSrc_1_0_1_5;
     hashable     = self.hashable_1_2_1_0;
     html         = self.html_1_0_1_2;
-    HTTP         = self.HTTP_4000_2_9;
+    HTTP         = self.HTTP_4000_2_10;
     HUnit        = self.HUnit_1_2_5_2;
     mtl          = self.mtl_2_1_2;
     network      = self.network_2_4_2_1;
@@ -1217,8 +1217,8 @@ let result = let callPackage = x : y : modifyPrio (newScope result.finalReturn x
   HTTP_4000_2_3 = callPackage ../development/libraries/haskell/HTTP/4000.2.3.nix {};
   HTTP_4000_2_5 = callPackage ../development/libraries/haskell/HTTP/4000.2.5.nix {};
   HTTP_4000_2_8 = callPackage ../development/libraries/haskell/HTTP/4000.2.8.nix {};
-  HTTP_4000_2_9 = callPackage ../development/libraries/haskell/HTTP/4000.2.9.nix {};
-  HTTP = self.HTTP_4000_2_9;
+  HTTP_4000_2_10 = callPackage ../development/libraries/haskell/HTTP/4000.2.10.nix {};
+  HTTP = self.HTTP_4000_2_10;
 
   httpAttoparsec = callPackage ../development/libraries/haskell/http-attoparsec {};
 
