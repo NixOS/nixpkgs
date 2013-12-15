e4df13e956650e7069a1b0af15d3bb67659ed5a0
Nixpkgs Monitor <none@none>
2013-12-15 12:35:47 +0200

    enscript: update from 1.6.5.2 to 1.6.6

--- pkgs/tools/text/enscript/default.nix
+++ pkgs/tools/text/enscript/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, gettext }:
 
 stdenv.mkDerivation rec {
-  name = "enscript-1.6.5.2";
+  name = "enscript-1.6.6";
 
   src = fetchurl {
     url = "mirror://gnu/enscript/${name}.tar.gz";
-    sha256 = "0xfblj3liwf1zxpx8bdcl2dwqi2lkfm1zsl6ld9757kgnfyk6gnv";
+    sha256 = "1fy0ymvzrrvs889zanxcaxjfcxarm2d3k43c9frmbl1ld7dblmkd";
   };
 
   buildInputs = [ gettext ];
