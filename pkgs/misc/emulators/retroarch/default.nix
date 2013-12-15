88b9e1f191c1c48a285fdb17dc2f5a85860e80c0
Song Wenwu <iyzsong@gmail.com>
2013-12-15 23:47:25 +0800

    retroarch: update to 0.9.9.7

--- pkgs/misc/emulators/retroarch/default.nix
+++ pkgs/misc/emulators/retroarch/default.nix
@@ -1,27 +1,31 @@
-{ stdenv, fetchurl, pkgconfig, which
+{ stdenv, fetchgit, pkgconfig, which
 , SDL, mesa, alsaLib
+, libXxf86vm, libXinerama, libXv
 }:
 
 stdenv.mkDerivation rec {
-  name = "retroarch-0.9.9";
+  name = "retroarch-0.9.9.7";
 
-  src = fetchurl {
-    url = "http://themaister.net/retroarch-dl/${name}.tar.gz";
-    sha256 = "08xlndpl14c4ccgp752ixx3a7ajf3xp93nawhinwxq0cw801prda";
+  src = fetchgit {
+    url = "https://github.com/libretro/RetroArch.git";
+    rev = "ea0c4880556e0f9d1fe8253ddc713bc743b00e1b";
+    sha256 = "1jhyh7f8ijy67fxslxqsp8pjl2lwayjljp06hp4n5cn33yajpbd7";
   };
 
   buildInputs = [
     pkgconfig which SDL mesa alsaLib
+    libXxf86vm libXinerama libXv
   ];
 
   preConfigure = ''
     configureFlags="--global-config-dir=$out/etc"
   '';
-
+ 
   meta = {
-    description = "A cross-platform multi-system emulator";
-    homepage = "http://themaister.net/retroarch.html";
+    description = "Modular multi-system game/emulator system";
+    homepage = "http://www.libretro.com/";
     license = stdenv.lib.licenses.gpl3Plus;
-    platform = stdenv.lib.platforms.linux;
+    platforms = stdenv.lib.platforms.linux;
+    maintainers = with stdenv.lib.maintainers; [ iyzsong ];
   };
 }
