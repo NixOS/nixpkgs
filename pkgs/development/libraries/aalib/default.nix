46f338d55deee4b20ecd4327847592a2c54ad7dc
Evgeny Egorochkin <phreedom@yandex.ru>
2013-12-18 13:33:12 +0200

    aalib: update builder

--- pkgs/development/libraries/aalib/default.nix
+++ pkgs/development/libraries/aalib/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation {
   name = "aalib-1.4rc5";
-  builder = ./builder.sh;
+
   src = fetchurl {
     url = mirror://sourceforge/aa-project/aalib-1.4rc5.tar.gz;
     sha256 = "1vkh19gb76agvh4h87ysbrgy82hrw88lnsvhynjf4vng629dmpgv";
@@ -13,8 +13,9 @@ stdenv.mkDerivation {
     cp ${automake}/share/automake*/config.{sub,guess} .
   '';
 
-  buildInputs = [ncurses];
-  inherit ncurses;
+  buildInputs = [ ncurses ];
+
+  configureFlags = "--without-x --with-ncurses=${ncurses}";
 
   meta = {
     description = "ASCII art graphics library";
