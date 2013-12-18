01a051292f718bd387f0644efdafbb28a3895a2a
Shea Levy <shea@shealevy.com>
2013-12-18 12:35:07 -0500

    Linux 3.12.5
    Signed-off-by: Shea Levy <shea@shealevy.com>

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.12.4";
+  version = "3.12.5";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "c56317810e5716dd2be7ad947a6bd174460f7cf9afb33b700a052aa91f73f9bb";
+    sha256 = "48181474f3943f77c1e62bed676b09a9fed64364d5847ce14a45b8d947112719";
   };
 
   features.iwlwifi = true;
