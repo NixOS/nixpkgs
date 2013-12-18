947f0a2282ea794e8bed2e7594943c2c333f8fce
Nixpkgs Monitor <none@none>
2013-12-18 17:58:12 +0200

    lmodern: update from 2.004.1 to 2.004.4

--- pkgs/data/fonts/lmodern/default.nix
+++ pkgs/data/fonts/lmodern/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation {
-  name = "lmodern-2.004.1";
+  name = "lmodern-2.004.4";
   
   src = fetchurl {
-    url = mirror://debian/pool/main/l/lmodern/lmodern_2.004.1.orig.tar.gz;
-    sha256 = "1bvlf8p39667q58pvyfzy3yl0mylf0ak96flwp8vj01vqbi3rfaz";
+    url = mirror://debian/pool/main/l/lmodern/lmodern_2.004.4.orig.tar.gz;
+    sha256 = "1g1fmi9asw6x9arm5sy3r4jwz7zrrbcw6q4waj3iqs0iq525i1rw";
   };
 
   installPhase = ''
