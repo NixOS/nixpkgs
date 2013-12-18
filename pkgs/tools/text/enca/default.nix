74c3965b92f88e73484ec05f5973c903015ff1c7
Nixpkgs Monitor <none@none>
2013-12-18 15:37:12 +0200

    enca: update from 1.13 to 1.15

--- pkgs/tools/text/enca/default.nix
+++ pkgs/tools/text/enca/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, libiconv ? null }:
 
 stdenv.mkDerivation rec {
-  name = "enca-1.13";
+  name = "enca-1.15";
 
   src = fetchurl {
     url = "http://dl.cihar.com/enca/${name}.tar.bz2";
-    sha256 = "6c091455d524b3ddae3533fe9bb375092f60ad6fe252e0777c873bd7869c7432";
+    sha256 = "1iibfl2s420x7bc1hvxr1ys4cpz579brg2m2ln4rp677azmrr8mv";
   };
 
   buildInputs = (stdenv.lib.optional (libiconv != null) libiconv);
