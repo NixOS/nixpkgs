ede8d010a54270ea5fd3eff55348709fee52186c
Peter Simons <simons@cryp.to>
2013-12-19 11:35:26 +0100

    haskell-haskell-names: update to version 0.3.2.4

--- pkgs/development/libraries/haskell/haskell-names/default.nix
+++ pkgs/development/libraries/haskell/haskell-names/default.nix
@@ -6,8 +6,8 @@
 
 cabal.mkDerivation (self: {
   pname = "haskell-names";
-  version = "0.3.2.3";
-  sha256 = "0vh88qkvlljqviy5mqjz8gbpwnl2wq65c2rm9kyjnc2cpyp4i8zx";
+  version = "0.3.2.4";
+  sha256 = "1m3p64kkm8zv2diwila0zdincykavf2fvxpqzpsmfndwb24f5b3p";
   buildDepends = [
     aeson Cabal dataLens dataLensTemplate filepath haskellPackages
     haskellSrcExts hseCpp mtl tagged transformers traverseWithClass
