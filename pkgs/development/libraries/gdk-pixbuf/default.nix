51c83e927d52f0e72dc2d22cc7a2919e193af41f
Vladimír Čunát <vcunat@gmail.com>
2013-12-21 15:14:55 +0100

    gdk_pixbuf: minor update (mainly JPEG fixes)

--- pkgs/development/libraries/gdk-pixbuf/default.nix
+++ pkgs/development/libraries/gdk-pixbuf/default.nix
@@ -3,14 +3,14 @@
 
 let
   ver_maj = "2.30";
-  ver_min = "1";
+  ver_min = "2";
 in
 stdenv.mkDerivation rec {
   name = "gdk-pixbuf-${ver_maj}.${ver_min}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gdk-pixbuf/${ver_maj}/${name}.tar.xz";
-    sha256 = "0c4dxsnpqc46liqjlh3w2qmrzv0b89ksn15z2f9h13362lg3n9m3";
+    sha256 = "1gzczsv41h28is4rrxjfyj1qx8ifp23fq2ckh0k099m9fnhbzfna";
   };
 
   # !!! We might want to factor out the gdk-pixbuf-xlib subpackage.
