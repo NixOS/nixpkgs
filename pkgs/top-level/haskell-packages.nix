5b37e9b543d9955ca41edc64b916ee6564fdc9b8
Peter Simons <simons@cryp.to>
2013-12-24 18:07:21 +0100

    haskell-prettyclass: add version 1.0.0.0

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -1793,6 +1793,8 @@ let result = let callPackage = x : y : modifyPrio (newScope result.finalReturn x
 
   ppm = callPackage ../development/libraries/haskell/ppm {};
 
+  prettyclass = callPackage ../development/libraries/haskell/prettyclass {};
+
   prettyShow_1_2 = callPackage ../development/libraries/haskell/pretty-show/1.2.nix {};
   prettyShow_1_6_3 = callPackage ../development/libraries/haskell/pretty-show/1.6.3.nix {
     happy = self.happy_1_19_2;
