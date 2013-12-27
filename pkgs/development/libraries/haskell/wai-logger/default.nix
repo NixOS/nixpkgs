ec33405820afcdc6761f9f8957aabf18b134bbfc
Peter Simons <simons@cryp.to>
2013-12-27 08:46:36 +0100

    haskell-wai-logger: update to version 2.1.0

--- pkgs/development/libraries/haskell/wai-logger/default.nix
+++ pkgs/development/libraries/haskell/wai-logger/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "wai-logger";
-  version = "2.0.2";
-  sha256 = "0wpx01sizl17ln1npia1c9hx1ph5qv8zwmikgmv0hkgx7w9bifaa";
+  version = "2.1.0";
+  sha256 = "1vb2nih78qw7ha1v67hsyyplarxxg5zh82pmh85sdbdykp5cwz0c";
   buildDepends = [
     blazeBuilder byteorder caseInsensitive fastLogger httpTypes network
     unixTime wai
