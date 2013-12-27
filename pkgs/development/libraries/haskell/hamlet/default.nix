453668d04e6fd55412e1295c0e814385687bb7d9
Peter Simons <simons@cryp.to>
2013-12-27 08:46:36 +0100

    haskell-hamlet: update to version 1.1.7.6

--- pkgs/development/libraries/haskell/hamlet/default.nix
+++ pkgs/development/libraries/haskell/hamlet/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "hamlet";
-  version = "1.1.7.5";
-  sha256 = "1ph92n5l63qr5hpjcvl37j1w857dwwzzgsxn8mdadi2pz35lxm82";
+  version = "1.1.7.6";
+  sha256 = "1b0y7imdihm87nkz32bmh2gbalidy9rzp85x677lvxc99c1m9d1d";
   buildDepends = [
     blazeBuilder blazeHtml blazeMarkup failure parsec shakespeare text
   ];
