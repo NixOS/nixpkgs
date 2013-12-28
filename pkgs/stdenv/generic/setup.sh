d978c35ff966b64e7c2e5f251e4ed35a24a3e80d
Vladimír Čunát <vcunat@gmail.com>
2013-12-28 08:17:14 +0100

    stdenv/setup.sh: fix previous change...
    for the case that there is no makefile. CC #1354

--- pkgs/stdenv/generic/setup.sh
+++ pkgs/stdenv/generic/setup.sh
@@ -109,9 +109,6 @@ runHook preHook
 # Check that the pre-hook initialised SHELL.
 if [ -z "$SHELL" ]; then echo "SHELL not set"; exit 1; fi
 
-# see https://github.com/NixOS/nixpkgs/pull/1354#issuecomment-31260409
-makeFlags="${makeFlags} SHELL=${SHELL}"
-
 # Hack: run gcc's setup hook.
 envHooks=()
 crossEnvHooks=()
@@ -616,6 +613,9 @@ buildPhase() {
         echo "no Makefile, doing nothing"
         return
     fi
+
+    # see https://github.com/NixOS/nixpkgs/pull/1354#issuecomment-31260409
+    makeFlags="${makeFlags} SHELL=${SHELL}"
 
     echo "make flags: $makeFlags ${makeFlagsArray[@]} $buildFlags ${buildFlagsArray[@]}"
     make ${makefile:+-f $makefile} \
