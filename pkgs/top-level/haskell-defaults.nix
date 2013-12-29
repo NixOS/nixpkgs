7e51f66bea71c1651c50f9060564930186c307f9
Peter Simons <simons@cryp.to>
2013-12-29 11:55:24 +0100

    Fix GHC 7.2.2 build of Haskell Platform 2012.2.0.0.

--- pkgs/top-level/haskell-defaults.nix
+++ pkgs/top-level/haskell-defaults.nix
@@ -47,7 +47,9 @@
     self : self.haskellPlatformArgs_2012_2_0_0 self // {
       haskellPlatform = self.haskellPlatform_2012_2_0_0;
       deepseq = self.deepseq_1_3_0_2;
+      cabalInstall_0_14_0 = self.cabalInstall_0_14_0.override { Cabal = self.Cabal_1_14_0; };
       cabalInstall_1_16_0_2 = self.cabalInstall_1_16_0_2.override { Cabal = self.Cabal_1_16_0_3; };
+      cabalInstall = self.cabalInstall_0_14_0.override { Cabal = self.Cabal_1_14_0; };
       cabal2nix = self.cabal2nix.override { Cabal = self.Cabal_1_16_0_3; hackageDb = self.hackageDb.override { Cabal = self.Cabal_1_16_0_3; }; };
       binary = self.binary_0_6_0_0;
     };
