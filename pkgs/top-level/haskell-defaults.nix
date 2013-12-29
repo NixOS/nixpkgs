da712872814dc28fe5f2024235eee7521eb031fa
Peter Simons <simons@cryp.to>
2013-12-29 13:14:00 +0100

    haskell-defaults.nix: use the old version of pretty-show with ghc 7.2.2
    The old compiler cannot build Happy 1.19.2.

--- pkgs/top-level/haskell-defaults.nix
+++ pkgs/top-level/haskell-defaults.nix
@@ -52,6 +52,7 @@
       cabalInstall = self.cabalInstall_0_14_0.override { Cabal = self.Cabal_1_14_0; };
       cabal2nix = self.cabal2nix.override { Cabal = self.Cabal_1_16_0_3; hackageDb = self.hackageDb.override { Cabal = self.Cabal_1_16_0_3; }; };
       binary = self.binary_0_6_0_0;
+      prettyShow = self.prettyShow_1_2;
     };
 
   ghc721Prefs = ghc722Prefs;
