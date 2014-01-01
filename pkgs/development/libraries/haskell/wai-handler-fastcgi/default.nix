27425f638d6a8c71749271d1222a9026e64e3e3d
Peter Simons <simons@cryp.to>
2014-01-02 00:20:06 +0100

    haskell-wai-handler-fastcgi: re-generate with cabal2nix

--- pkgs/development/libraries/haskell/wai-handler-fastcgi/default.nix
+++ pkgs/development/libraries/haskell/wai-handler-fastcgi/default.nix
@@ -1,14 +1,14 @@
-{ cabal, wai, waiExtra, fcgi }:
+{ cabal, fcgi, wai, waiExtra }:
 
 cabal.mkDerivation (self: {
   pname = "wai-handler-fastcgi";
   version = "2.0.0";
   sha256 = "1pqiqx1wq2iv705f8bd4sxmjmmkkxiw4g6a9dpwnawwb5n0d88nl";
-  buildDepends = [
-    wai waiExtra fcgi
-  ];
+  buildDepends = [ wai waiExtra ];
+  extraLibraries = [ fcgi ];
   meta = {
-    description = "A WAI handler that out to the libfcgi C library";
+    homepage = "http://www.yesodweb.com/book/web-application-interface";
+    description = "Wai handler to fastcgi";
     license = self.stdenv.lib.licenses.mit;
     platforms = self.ghc.meta.platforms;
   };
