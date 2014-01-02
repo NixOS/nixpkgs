3394d3172686511a361f66e9c6338528e89f6839
Song Wenwu <iyzsong@gmail.com>
2014-01-02 10:33:36 +0800

    gstreamer: update to 1.2.2, add myself as maintainer

--- pkgs/development/libraries/gstreamer/base/default.nix
+++ pkgs/development/libraries/gstreamer/base/default.nix
@@ -4,18 +4,19 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "gst-plugins-base-1.2.1";
+  name = "gst-plugins-base-1.2.2";
 
   meta = {
     description = "Base plugins and helper libraries";
     homepage = "http://gstreamer.freedesktop.org";
     license = stdenv.lib.licenses.lgpl2Plus;
     platforms = stdenv.lib.platforms.linux;
+    maintainers = with stdenv.lib.maintainers; [ iyzsong ];
   };
 
   src = fetchurl {
     url = "${meta.homepage}/src/gst-plugins-base/${name}.tar.xz";
-    sha256 = "de2444a5c150d4e4b680364d7c0414cd8b015d95b305ff65d65a17683379532f";
+    sha256 = "fa90cf21eac0a77f9393100356aef99ae42072c31dc218d3ae2e7f86cd5ced69";
   };
 
   nativeBuildInputs = [
--- pkgs/development/libraries/gstreamer/ugly/default.nix
+++ pkgs/development/libraries/gstreamer/ugly/default.nix
@@ -5,17 +5,18 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "gst-plugins-ugly-1.2.1";
+  name = "gst-plugins-ugly-1.2.2";
 
   meta = {
     homepage = "http://gstreamer.freedesktop.org";
     license = stdenv.lib.licenses.lgpl2Plus;
     platforms = stdenv.lib.platforms.linux;
+    maintainers = with stdenv.lib.maintainers; [ iyzsong ];
   };
 
   src = fetchurl {
     url = "${meta.homepage}/src/gst-plugins-ugly/${name}.tar.xz";
-    sha256 = "35ae5d45de54827604e24f57f54ab30a5ab2245b4c4120977273eb11d19c2395";
+    sha256 = "4b6aac272a5be0d68f365ef6fba0f829fc5c1d1d601bb4dd9e85f5289b2b56c3";
   };
 
   nativeBuildInputs = [ pkgconfig python ];
