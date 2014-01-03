9ea0efdebea488f1038d6f7a3ccb3d8f18a9e4ed
Peter Simons <simons@cryp.to>
2014-01-03 11:56:20 +0100

    haskell-cipher-aes128: update to version 0.6

--- pkgs/development/libraries/haskell/cipher-aes128/default.nix
+++ pkgs/development/libraries/haskell/cipher-aes128/default.nix
@@ -2,12 +2,14 @@
 
 cabal.mkDerivation (self: {
   pname = "cipher-aes128";
-  version = "0.5";
-  sha256 = "14rwnz0nwmy1zch1ywjxf2fgfs1xj84l4n785rhb6npmx6k7rmqd";
+  version = "0.6";
+  sha256 = "1zpxg14csb52rjsvvfcyhpl9yfyidx73zxpdsipxvb1w26p8sl7y";
+  isLibrary = true;
+  isExecutable = true;
   buildDepends = [ cereal cryptoApi tagged ];
   meta = {
     homepage = "https://github.com/TomMD/cipher-aes128";
-    description = "AES128 using AES-NI when available";
+    description = "AES and common modes using AES-NI when available";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
   };
