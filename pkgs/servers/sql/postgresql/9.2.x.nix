9f22c446e9260f0ac736d39dd767686adee26845
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-01-03 11:54:52 +0100

    postgresql: Update to 8.4.19, 9.0.15, 9.1.11, 9.2.6
    This is a critical update fixing several data corruption issues:
    
      http://www.postgresql.org/about/news/1492/
    
    (cherry picked from commit 0c93c758bfc711edba57b765e9d6889c796af90b)

--- pkgs/servers/sql/postgresql/9.2.x.nix
+++ pkgs/servers/sql/postgresql/9.2.x.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, zlib, readline }:
 
-let version = "9.2.5"; in
+let version = "9.2.6"; in
 
 stdenv.mkDerivation rec {
   name = "postgresql-${version}";
 
   src = fetchurl {
     url = "mirror://postgresql/source/v${version}/${name}.tar.bz2";
-    sha256 = "22c1edfd6a404bb15fba655863e94f09a10716ded1910a8bc98ee85f413007a4";
+    sha256 = "4ba98053a66e5678af93dbc2956e8b04623f759e174f48940c41f4251cf0f886";
   };
 
   buildInputs = [ zlib readline ];
