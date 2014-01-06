150981ec0e250ce7671acc66b09025101a9c9896
Peter Simons <simons@cryp.to>
2014-01-06 12:21:12 +0100

    haskell-pipes: update to version 4.0.2

--- pkgs/development/libraries/haskell/pipes/default.nix
+++ pkgs/development/libraries/haskell/pipes/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "pipes";
-  version = "4.0.1";
-  sha256 = "0q2i58w4nlf23di6rjrv543nmj5d5nkz29q9aciw89zx5x5m22h9";
+  version = "4.0.2";
+  sha256 = "18hcpklryyq9f6iwycxzi3sd6gyd9h0gy0ckg4rl7rhgy73hzgcz";
   buildDepends = [ mmorph mtl transformers void ];
   testDepends = [
     mtl QuickCheck testFramework testFrameworkQuickcheck2 transformers
