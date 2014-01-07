320498d5297f28a636d0afe5df3cf768aa472d50
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-01-07 14:24:59 +0100

    flex: Clean up expression

--- pkgs/development/tools/parsing/flex/default.nix
+++ pkgs/development/tools/parsing/flex/default.nix
@@ -1,17 +1,16 @@
-# This should be moved to default.nix eventually (?)
-
-{stdenv, fetchurl, yacc, m4}:
-
-assert yacc != null && m4 != null;
+{ stdenv, fetchurl, bison, m4 }:
 
 stdenv.mkDerivation {
   name = "flex-2.5.35";
+
   src = fetchurl {
     url = mirror://sourceforge/flex/flex-2.5.35.tar.bz2;
     sha256 = "0ysff249mwhq0053bw3hxh58djc0gy7vjan2z1krrf9n5d5vvv0b";
   };
-  buildInputs = [yacc];
-  propagatedNativeBuildInputs = [m4];
+
+  buildInputs = [ bison ];
+
+  propagatedNativeBuildInputs = [ m4 ];
 
   crossAttrs = {
     preConfigure = ''
@@ -21,6 +20,7 @@ stdenv.mkDerivation {
   };
 
   meta = {
+    homepage = http://flex.sourceforge.net/;
     description = "A fast lexical analyser generator";
   };
 }
