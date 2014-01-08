eed630797e6938006ff74e55a67fc2555c596a57
Peter Simons <simons@cryp.to>
2014-01-08 17:31:53 +0100

    haskell-poppler: re-generate expression with cabal2nix

--- pkgs/development/libraries/haskell/poppler/default.nix
+++ pkgs/development/libraries/haskell/poppler/default.nix
@@ -1,4 +1,6 @@
-{ cabal, cairo, glib, gtk, gtk2hsBuildtools, libc, mtl, popplerGlib, pkgconfig }:
+{ cabal, cairo, gdk_pixbuf, glib, gtk, gtk2hsBuildtools, libc, mtl
+, pango, popplerGlib
+}:
 
 cabal.mkDerivation (self: {
   pname = "poppler";
@@ -6,11 +8,10 @@ cabal.mkDerivation (self: {
   sha256 = "1ln5akiarv1ng5gjrzf8bnkm556xzl50m209qvi5nk98g7fyhqs7";
   buildDepends = [ cairo glib gtk mtl ];
   buildTools = [ gtk2hsBuildtools ];
-  extraLibraries = [ libc pkgconfig ];
-  pkgconfigDepends = [ glib gtk popplerGlib ];
-  #jailbreak = true;
+  extraLibraries = [ libc ];
+  pkgconfigDepends = [ cairo gdk_pixbuf glib gtk pango popplerGlib ];
   meta = {
-    homepage = "http://www.haskell.org/gtk2hs";
+    homepage = "http://www.haskell.org/gtk2hs/";
     description = "Binding to the Poppler";
     license = self.stdenv.lib.licenses.gpl2;
     platforms = self.ghc.meta.platforms;
