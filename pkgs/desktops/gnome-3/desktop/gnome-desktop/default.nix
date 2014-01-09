00ff5466d3408efba24e02b54d74b1da53409c81
Domen Kožar <domen@dev.si>
2014-01-09 14:14:06 +0100

    gnome3.gnome-desktop: 3.5 -> 3.11

--- pkgs/desktops/gnome-3/desktop/gnome-desktop/default.nix
+++ pkgs/desktops/gnome-3/desktop/gnome-desktop/default.nix
@@ -1,20 +1,28 @@
-{ stdenv, fetchurl, pkgconfig, python, libxml2Python, libxslt, which, libX11, gnome3
-, intltool, gnome_doc_utils }:
+{ stdenv, fetchurl, pkgconfig, python, libxml2Python, libxslt, which, libX11, gnome3, gtk3, glib
+, intltool, gnome_doc_utils, libxkbfile, xkeyboard_config, isocodes, itstool, wayland
+, gobjectIntrospection }:
 
 stdenv.mkDerivation rec {
 
-  majorVersion = "3.5";
-  minorVersion = "3";
+  majorVersion = "3.11";
+  minorVersion = "2";
   name = "gnome-desktop-${majorVersion}.${minorVersion}";
+
+  # this should probably be setuphook for glib
+  NIX_CFLAGS_COMPILE = "-I${glib}/include/gio-unix-2.0";
 
   enableParallelBuilding = true;
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-desktop/${majorVersion}/${name}.tar.xz";
-    sha256 = "1nrqcp1p5cxhfjjy5hjpvkqmzsgl2353a08fg0b11c932v95bsba";
+    sha256 = "0vsxgp77s0pmcq177lz310ync4c1n7cwb63163zm5g5xw69qc5d7";
   };
 
   configureFlags = "--disable-scrollkeeper";
-  buildInputs = [ pkgconfig python libxml2Python libxslt which libX11
-                  gnome3.gtk gnome3.glib intltool gnome_doc_utils ];
+  buildInputs = [ pkgconfig python libxml2Python libxslt which libX11 xkeyboard_config isocodes itstool wayland
+                  gtk3 glib intltool gnome_doc_utils libxkbfile gnome3.gsettings_desktop_schemas gobjectIntrospection ];
+
+  meta = with stdenv.lib; {
+    platforms = platforms.linux;
+  };
 }
