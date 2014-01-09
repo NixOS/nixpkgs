5040ae0ccfe01e2094e3909bdf59662240d60391
Domen Kožar <domen@dev.si>
2014-01-09 18:31:48 +0100

    fix typo in gnome3.libpeas

--- pkgs/desktops/gnome-3/core/libpeas/default.nix
+++ pkgs/desktops/gnome-3/core/libpeas/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec {
   buildInputs = with gnome3; [ intltool pkgconfig glib gobjectIntrospection gtk3 ];
 
   src = fetchurl {
-    url = "mirror://gnome/sources/eog/1.9/${name}.tar.xz";
+    url = "mirror://gnome/sources/libpeas/1.9/${name}.tar.xz";
     sha256 = "13fzyzv6c0cfdj83z1s16lv8k997wpnzyzr0wfwcfkcmvz64g1q0";
   };
 
