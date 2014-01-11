215931f5d08d9f70f085ae1588eceaa87324c4f6
Domen Kožar <domen@dev.si>
2014-01-11 15:40:16 +0100

    gnome3.gnome_desktop: 3.11 -> 3.10 (use gnome3 stable)

--- pkgs/desktops/gnome-3/desktop/gnome-desktop/default.nix
+++ pkgs/desktops/gnome-3/desktop/gnome-desktop/default.nix
@@ -4,8 +4,8 @@
 
 stdenv.mkDerivation rec {
 
-  majorVersion = "3.11";
-  minorVersion = "2";
+  majorVersion = "3.10";
+  minorVersion = "1";
   name = "gnome-desktop-${majorVersion}.${minorVersion}";
 
   # this should probably be setuphook for glib
@@ -15,10 +15,9 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-desktop/${majorVersion}/${name}.tar.xz";
-    sha256 = "0vsxgp77s0pmcq177lz310ync4c1n7cwb63163zm5g5xw69qc5d7";
+    sha256 = "0hdvm909lbpnixqv11qdx9iaycx4dpxys46fa128bqp8alisgb0h";
   };
 
-  configureFlags = "--disable-scrollkeeper";
   buildInputs = [ pkgconfig python libxml2Python libxslt which libX11 xkeyboard_config isocodes itstool wayland
                   gtk3 glib intltool gnome_doc_utils libxkbfile gnome3.gsettings_desktop_schemas gobjectIntrospection ];
 
