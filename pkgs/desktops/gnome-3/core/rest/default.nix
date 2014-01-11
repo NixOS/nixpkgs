db7a05e3ad1da90381d127d6e70b207ebfa9a58d
Rob Vermaas <rob.vermaas@gmail.com>
2014-01-11 23:10:08 +0100

    Gnome1: add gobjectIntrospection to rest

--- pkgs/desktops/gnome-3/core/rest/default.nix
+++ pkgs/desktops/gnome-3/core/rest/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, glib, libsoup }:
+{ stdenv, fetchurl, pkgconfig, glib, libsoup, gobjectIntrospection }:
 
 stdenv.mkDerivation rec {
   name = "rest-0.7.90";
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
     sha256 = "08n0cvz44l4b1gkmjryap3ysd0wcbbbdjbcar73nr52dmk52ls0x";
   };
 
-  buildInputs = [ pkgconfig glib libsoup ];
+  buildInputs = [ pkgconfig glib libsoup gobjectIntrospection];
 
   configureFlags = "--with-ca-certificates=/etc/ssl/certs/ca-bundle.crt";
 
