b7ac6b3f51ff31427fdb3289decf1e3c261ab176
Jaka Hudoklin <jakahudoklin@gmail.com>
2014-01-12 23:39:14 +0100

    Add node-webkit, app runtime based on Chromium and node.js

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3799,6 +3799,10 @@ let
 
   ninja = callPackage ../development/tools/build-managers/ninja { };
 
+  node_webkit = callPackage ../development/tools/node-webkit {
+    gconf = pkgs.gnome.GConf;
+  };
+
   noweb = callPackage ../development/tools/literate-programming/noweb { };
 
   omake = callPackage ../development/tools/ocaml/omake { };
