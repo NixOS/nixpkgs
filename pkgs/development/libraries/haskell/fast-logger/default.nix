476c776b8b200c304e1b0aeec1e0c5ac88602b2b
Peter Simons <simons@cryp.to>
2014-01-13 15:40:06 +0100

    haskell-fast-logger: update to version 2.1.4

--- pkgs/development/libraries/haskell/fast-logger/default.nix
+++ pkgs/development/libraries/haskell/fast-logger/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "fast-logger";
-  version = "2.1.2";
-  sha256 = "0kz31jfhc40wlw3b49zcsn97y6vc9qqnaci743mgjskv4zbqaf34";
+  version = "2.1.4";
+  sha256 = "1fb75wx1v9h7690x43kd85lq1h9zi8nq438pqclzzrcfidsnm6z5";
   buildDepends = [ blazeBuilder filepath text ];
   testDepends = [ hspec ];
   meta = {
