7a6255e4e1747550bd811ef00a3e1bc953f11b40
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-01-14 14:20:47 +0100

    patchelf: Update to 0.8
    This fixes a segfault in building Eclipse and the Android SDK.

--- pkgs/development/tools/misc/patchelf/default.nix
+++ pkgs/development/tools/misc/patchelf/default.nix
@@ -1,16 +1,18 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "patchelf-0.7";
+  name = "patchelf-0.8";
 
   src = fetchurl {
     url = "http://nixos.org/releases/patchelf/${name}/${name}.tar.bz2";
-    sha256 = "61b96f455e6ccd1c1d7d159df7199c85ff6e8f9622d795a5420e418acfb8b808";
+    sha256 = "c99f84d124347340c36707089ec8f70530abd56e7827c54d506eb4cc097a17e7";
   };
 
   meta = {
     homepage = http://nixos.org/patchelf.html;
     license = "GPL";
     description = "A small utility to modify the dynamic linker and RPATH of ELF executables";
+    maintainers = [ stdenv.lib.maintainers.eelco ];
+    platforms = stdenv.lib.platforms.all;
   };
 }
