10998e8af54b623c1455c215f2f0efc95d222672
Peter Simons <simons@cryp.to>
2014-01-15 16:45:01 +0100

    syslogng: update from 3.1.2 to 3.5.3

--- pkgs/tools/system/syslog-ng/default.nix
+++ pkgs/tools/system/syslog-ng/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, eventlog, pkgconfig, glib}:
 
 stdenv.mkDerivation {
-  name = "syslog-ng-3.1.2";
+  name = "syslog-ng-3.5.3";
 
   src = fetchurl {
-    url = "http://www.balabit.com/downloads/files?path=/syslog-ng/sources/3.1.2/source/syslog-ng_3.1.2.tar.gz";
-    sha256 = "0a508l4j11336jn5kg65l70rf7xbpdxi2n477rvp5p48cc1adcg2";
+    url = "http://www.balabit.com/downloads/files?path=/syslog-ng/sources/3.5.3/source/syslog-ng_3.5.3.tar.gz";
+    sha256 = "1l3424qn9bf9z742pqba8x3dj7g729asimmhlizv1rvjlaxa2jd3";
   };
 
   buildInputs = [eventlog pkgconfig glib];
@@ -15,7 +15,5 @@ stdenv.mkDerivation {
     homepage = "http://www.balabit.com/network-security/syslog-ng/";
     description = "Next-generation syslogd with advanced networking and filtering capabilities";
     license = "GPLv2";
-    platforms = stdenv.lib.platforms.linux;
-    maintainers = [ stdenv.lib.maintainers.simons ];
   };
 }
