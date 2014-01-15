c056a69fdda8308ce1fb7548aab0350ea40fad2e
Shea Levy <shea@shealevy.com>
2014-01-15 01:06:04 -0500

    darwin openjdk: set jre attribute.
    Fixes tarball
    
    Signed-off-by: Shea Levy <shea@shealevy.com>

--- pkgs/development/compilers/openjdk-darwin/default.nix
+++ pkgs/development/compilers/openjdk-darwin/default.nix
@@ -1,16 +1,19 @@
 {stdenv, fetchurl, ...}:
+let
+jdk = stdenv.mkDerivation {
+    name = "openjdk6-b16-24_apr_2009-r1";
 
-stdenv.mkDerivation {
-  name = "openjdk6-b16-24_apr_2009-r1";
+    src = fetchurl {
+      url = http://hg.bikemonkey.org/archive/openjdk6_darwin/openjdk6-b16-24_apr_2009-r1.tar.bz2;
+      sha256 = "14pbv6jjk95k7hbgiwyvjdjv8pccm7m8a130k0q7mjssf4qmpx1v";
+    };
 
-  src = fetchurl {
-    url = http://hg.bikemonkey.org/archive/openjdk6_darwin/openjdk6-b16-24_apr_2009-r1.tar.bz2;
-    sha256 = "14pbv6jjk95k7hbgiwyvjdjv8pccm7m8a130k0q7mjssf4qmpx1v";
+    installPhase = ''
+      mkdir -p $out
+      cp -vR * $out/
+    '';
+
+    passthru.jre = jdk;
+
   };
-
-  installPhase = ''
-    mkdir -p $out
-    cp -vR * $out/
-  '';
-
-}
+in jdk
