a39490d709ec8f09b04f91530219c343ce041412
Rok Garbas <rok@garbas.si>
2014-01-16 18:07:16 +0000

    fixing munin (broken by latest version update) also switch to fetchgit

--- pkgs/servers/monitoring/munin/default.nix
+++ pkgs/servers/monitoring/munin/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, makeWrapper, which, coreutils, rrdtool, perl, perlPackages
+{ stdenv, fetchgit, makeWrapper, which, coreutils, rrdtool, perl, perlPackages
 , python, ruby, openjdk, nettools
 }:
 
@@ -6,9 +6,10 @@ stdenv.mkDerivation rec {
   version = "2.0.19";
   name = "munin-${version}";
 
-  src = fetchurl {
-    url = "https://github.com/munin-monitoring/munin/archive/${version}.tar.gz";
-    sha256 = "10y9kmygd27mygvfkvn01dirb8glna9jzh140dcbci7yz7rrhdqh";
+  src = fetchgit {
+    url = "git://github.com/munin-monitoring/munin.git";
+    rev = "refs/tags/${version}";
+    sha256 = "0027rrdrmcql68b475jlxnfgkijbfngynkjpdii6fgaszswqz3ay";
   };
 
   buildInputs = [ 
@@ -68,7 +69,8 @@ stdenv.mkDerivation rec {
 
   preBuild = ''
     substituteInPlace "Makefile" \
-      --replace "/bin/pwd" "pwd"
+      --replace "/bin/pwd" "pwd" \
+      --replace "HTMLOld.3pm" "HTMLOld.3"
 
     # munin checks at build time if user/group exists, unpure
     sed -i '/CHECKUSER/d' Makefile
