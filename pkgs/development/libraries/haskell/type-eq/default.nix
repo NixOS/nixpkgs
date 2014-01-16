5f16eeea96e65ca90bd3b41a136ed1fcbe4831ca
Peter Simons <simons@cryp.to>
2014-01-16 11:54:50 +0100

    haskell-type-eq: update to version 0.4.1

--- pkgs/development/libraries/haskell/type-eq/default.nix
+++ pkgs/development/libraries/haskell/type-eq/default.nix
@@ -1,9 +1,10 @@
-{ cabal }:
+{ cabal, cpphs }:
 
 cabal.mkDerivation (self: {
   pname = "type-eq";
-  version = "0.4";
-  sha256 = "1cvbqxwkiybxbpzr98yl2pnx5w4zrr340z86q40zirgr1f0ch674";
+  version = "0.4.1";
+  sha256 = "0l8nkrdn1hs8ddhh85qm176f9v42fdck9iscn4swd92vj7bfci7k";
+  buildTools = [ cpphs ];
   meta = {
     homepage = "http://github.com/glaebhoerl/type-eq";
     description = "Type equality evidence you can carry around";
