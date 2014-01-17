930150bf4bfece6177dd33ffed337ecc86944216
Peter Simons <simons@cryp.to>
2014-01-17 10:40:21 +0100

    haskell-http-common: update to version 0.7.1.1

--- pkgs/development/libraries/haskell/http-common/default.nix
+++ pkgs/development/libraries/haskell/http-common/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "http-common";
-  version = "0.7.0.1";
-  sha256 = "1brflygyf8y60jilrl6p8jdr5k5zwcqpdhj6j51vj9n4gsnr6a8d";
+  version = "0.7.1.1";
+  sha256 = "1a0szaqs1halrv4kx57g2hd4vcdhks7pfal0hyq19af2pncaz1h8";
   buildDepends = [
     base64Bytestring blazeBuilder caseInsensitive mtl network text
     transformers unorderedContainers
