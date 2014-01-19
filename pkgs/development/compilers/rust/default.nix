cb245301350147edfec6867f0955e7e1379e3439
Shea Levy <shea@shealevy.com>
2014-01-19 12:40:31 -0500

    Fix whitespace
    Signed-off-by: Shea Levy <shea@shealevy.com>

--- pkgs/development/compilers/rust/default.nix
+++ pkgs/development/compilers/rust/default.nix
@@ -3,7 +3,7 @@
 let snapshotName = "rust-stage0-2014-01-05-a6d3e57-linux-x86_64-aa8fbbacdb1d8a078f3a3fe3478dcbc506bd4090.tar.bz2"; in
 stdenv.mkDerivation {
   name = "rust-0.9";
-  
+
   src = fetchurl {
     url = http://static.rust-lang.org/dist/rust-0.9.tar.gz;
     sha256 = "1lfmgnn00wrc30nf5lgg52w58ir3xpsnpmzk2v5a35xp8lsir4f0";
@@ -36,16 +36,16 @@ stdenv.mkDerivation {
     done
   '';
 
-  buildInputs = [ which file perl curl python27 makeWrapper];
+  buildInputs = [ which file perl curl python27 makeWrapper ];
   enableParallelBuilding = true;
 
   meta = {
     homepage = http://www.rust-lang.org/;
     description = "A safe, concurrent, practical language";
     maintainers = [ stdenv.lib.maintainers.madjar ];
-    license = map (builtins.getAttr "shortName") [stdenv.lib.licenses.mit stdenv.lib.licenses.asl20 ];
+    license = map (builtins.getAttr "shortName") [ stdenv.lib.licenses.mit stdenv.lib.licenses.asl20 ];
     # platforms as per http://static.rust-lang.org/doc/master/tutorial.html#getting-started
-    platforms = [ "i686-linux" "x86_64-linux" "x86_64-darwin"];
+    platforms = [ "i686-linux" "x86_64-linux" "x86_64-darwin" ];
   };
 }
 
