fecb119e5e8984ae5fd43797bb234916512d4960
Peter Simons <simons@cryp.to>
2014-01-20 18:05:00 +0100

    haskell-coroutine-object: re-generate with cabal2nix

--- pkgs/development/libraries/haskell/coroutine-object/default.nix
+++ pkgs/development/libraries/haskell/coroutine-object/default.nix
@@ -1,13 +1,16 @@
-{ cabal, cereal, either, lens, mtl, safecopy, transformers, transformersFree, uuid }:
+{ cabal, cereal, either, lens, mtl, safecopy, transformers
+, transformersFree, uuid
+}:
 
 cabal.mkDerivation (self: {
   pname = "coroutine-object";
   version = "0.2.0.0";
   sha256 = "1jl5glnk4ildjrxyxscxd0v7xfqbd9vpv5gaxygsfsbfr1zizp3s";
-  buildDepends = [ cereal either either lens mtl safecopy transformers transformersFree uuid ];
-  #jailbreak = true;
+  buildDepends = [
+    cereal either lens mtl safecopy transformers transformersFree uuid
+  ];
+  jailbreak = true;
   meta = {
-    homepage = "http://ianwookim.org/hoodle";
     description = "Object-oriented programming realization using coroutine";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
