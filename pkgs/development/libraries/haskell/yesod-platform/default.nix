9c065f77d8fdb816546b9fc9d1c27bc64fa48f26
Peter Simons <simons@cryp.to>
2014-01-20 12:44:51 +0100

    haskell-yesod-platform: update to version 1.2.6

--- pkgs/development/libraries/haskell/yesod-platform/default.nix
+++ pkgs/development/libraries/haskell/yesod-platform/default.nix
@@ -2,65 +2,67 @@
 , attoparsecConduit, authenticate, base64Bytestring
 , baseUnicodeSymbols, blazeBuilder, blazeBuilderConduit, blazeHtml
 , blazeMarkup, byteable, byteorder, caseInsensitive, cereal
-, certificate, cipherAes, cipherRc4, clientsession, conduit
-, connection, controlMonadLoop, cookie, cprngAes, cryptoApi
-, cryptoCipherTypes, cryptoConduit, cryptohash, cryptohashCryptoapi
-, cryptoNumbers, cryptoPubkey, cryptoPubkeyTypes, cryptoRandom
+, certificate, cipherAes, cipherBlowfish, cipherCamellia, cipherDes
+, cipherRc4, clientsession, conduit, connection, controlMonadLoop
+, cookie, cprngAes, cryptoApi, cryptocipher, cryptoCipherTypes
+, cryptoConduit, cryptohash, cryptohashCryptoapi, cryptoNumbers
+, cryptoPubkey, cryptoPubkeyTypes, cryptoRandom, cryptoRandomApi
 , cssText, dataDefault, dataDefaultClass, dataDefaultInstancesBase
 , dataDefaultInstancesContainers, dataDefaultInstancesDlist
 , dataDefaultInstancesOldLocale, dlist, emailValidate, entropy
-, failure, fastLogger, fileEmbed, filesystemConduit, hamlet, hjsmin
-, hspec, hspecExpectations, htmlConduit, httpAttoparsec, httpClient
-, httpClientConduit, httpClientTls, httpConduit, httpDate
-, httpTypes, languageJavascript, liftedBase, mimeMail, mimeTypes
-, mmorph, monadControl, monadLogger, monadLoops, networkConduit
-, pathPieces, pem, persistent, persistentTemplate, poolConduit
-, primitive, processConduit, publicsuffixlist, pureMD5, pwstoreFast
-, quickcheckIo, resourcePool, resourcet, safe, securemem
-, semigroups, setenv, SHA, shakespeare, shakespeareCss
-, shakespeareI18n, shakespeareJs, shakespeareText, silently
-, simpleSendfile, skein, socks, stmChans, stringsearch
-, systemFileio, systemFilepath, tagged, tagsoup, tagstreamConduit
-, tls, tlsExtra, transformersBase, unixCompat, unorderedContainers
-, utf8Light, utf8String, vector, void, wai, waiAppStatic, waiExtra
-, waiLogger, waiTest, warp, word8, xmlConduit, xmlTypes
-, xssSanitize, yaml, yesod, yesodAuth, yesodCore, yesodForm
-, yesodPersistent, yesodRoutes, yesodStatic, yesodTest
-, zlibBindings, zlibConduit
+, esqueleto, failure, fastLogger, fileEmbed, filesystemConduit
+, hamlet, hjsmin, hspec, hspecExpectations, htmlConduit
+, httpAttoparsec, httpClient, httpClientConduit, httpClientTls
+, httpConduit, httpDate, httpTypes, languageJavascript, liftedBase
+, mimeMail, mimeTypes, mmorph, monadControl, monadLogger
+, monadLoops, networkConduit, pathPieces, pem, persistent
+, persistentTemplate, poolConduit, primitive, processConduit
+, publicsuffixlist, pureMD5, pwstoreFast, quickcheckIo
+, resourcePool, resourcet, safe, scientific, securemem, semigroups
+, setenv, SHA, shakespeare, shakespeareCss, shakespeareI18n
+, shakespeareJs, shakespeareText, silently, simpleSendfile, skein
+, socks, stmChans, stringsearch, systemFileio, systemFilepath
+, tagged, tagsoup, tagstreamConduit, tls, tlsExtra
+, transformersBase, unixCompat, unorderedContainers, utf8Light
+, utf8String, vector, void, wai, waiAppStatic, waiExtra, waiLogger
+, waiTest, warp, warpTls, word8, xmlConduit, xmlTypes, xssSanitize
+, yaml, yesod, yesodAuth, yesodCore, yesodForm, yesodPersistent
+, yesodRoutes, yesodStatic, yesodTest, zlibBindings, zlibConduit
 }:
 
 cabal.mkDerivation (self: {
   pname = "yesod-platform";
-  version = "1.2.5.3";
-  sha256 = "0k9srgsnz9cgpxhdk04qz27lqp1xm97bprxjv93j1sxny92v6122";
+  version = "1.2.6";
+  sha256 = "15ixhxim14672hl9cl92sbi94yzv6g6zgg07jvkciixg0hd8xr6p";
   buildDepends = [
     aeson ansiTerminal asn1Data asn1Types attoparsec attoparsecConduit
     authenticate base64Bytestring baseUnicodeSymbols blazeBuilder
     blazeBuilderConduit blazeHtml blazeMarkup byteable byteorder
-    caseInsensitive cereal certificate cipherAes cipherRc4
-    clientsession conduit connection controlMonadLoop cookie cprngAes
-    cryptoApi cryptoCipherTypes cryptoConduit cryptohash
-    cryptohashCryptoapi cryptoNumbers cryptoPubkey cryptoPubkeyTypes
-    cryptoRandom cssText dataDefault dataDefaultClass
+    caseInsensitive cereal certificate cipherAes cipherBlowfish
+    cipherCamellia cipherDes cipherRc4 clientsession conduit connection
+    controlMonadLoop cookie cprngAes cryptoApi cryptocipher
+    cryptoCipherTypes cryptoConduit cryptohash cryptohashCryptoapi
+    cryptoNumbers cryptoPubkey cryptoPubkeyTypes cryptoRandom
+    cryptoRandomApi cssText dataDefault dataDefaultClass
     dataDefaultInstancesBase dataDefaultInstancesContainers
     dataDefaultInstancesDlist dataDefaultInstancesOldLocale dlist
-    emailValidate entropy failure fastLogger fileEmbed
+    emailValidate entropy esqueleto failure fastLogger fileEmbed
     filesystemConduit hamlet hjsmin hspec hspecExpectations htmlConduit
     httpAttoparsec httpClient httpClientConduit httpClientTls
     httpConduit httpDate httpTypes languageJavascript liftedBase
     mimeMail mimeTypes mmorph monadControl monadLogger monadLoops
     networkConduit pathPieces pem persistent persistentTemplate
     poolConduit primitive processConduit publicsuffixlist pureMD5
-    pwstoreFast quickcheckIo resourcePool resourcet safe securemem
-    semigroups setenv SHA shakespeare shakespeareCss shakespeareI18n
-    shakespeareJs shakespeareText silently simpleSendfile skein socks
-    stmChans stringsearch systemFileio systemFilepath tagged tagsoup
-    tagstreamConduit tls tlsExtra transformersBase unixCompat
-    unorderedContainers utf8Light utf8String vector void wai
-    waiAppStatic waiExtra waiLogger waiTest warp word8 xmlConduit
-    xmlTypes xssSanitize yaml yesod yesodAuth yesodCore yesodForm
-    yesodPersistent yesodRoutes yesodStatic yesodTest zlibBindings
-    zlibConduit
+    pwstoreFast quickcheckIo resourcePool resourcet safe scientific
+    securemem semigroups setenv SHA shakespeare shakespeareCss
+    shakespeareI18n shakespeareJs shakespeareText silently
+    simpleSendfile skein socks stmChans stringsearch systemFileio
+    systemFilepath tagged tagsoup tagstreamConduit tls tlsExtra
+    transformersBase unixCompat unorderedContainers utf8Light
+    utf8String vector void wai waiAppStatic waiExtra waiLogger waiTest
+    warp warpTls word8 xmlConduit xmlTypes xssSanitize yaml yesod
+    yesodAuth yesodCore yesodForm yesodPersistent yesodRoutes
+    yesodStatic yesodTest zlibBindings zlibConduit
   ];
   jailbreak = true;
   meta = {
