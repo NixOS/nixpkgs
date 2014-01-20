b26ab6c8f0c4e3e21dc0acb1b254368ba1a68eb9
Peter Simons <simons@cryp.to>
2014-01-20 18:05:00 +0100

    haskell-hoodle-render: re-generate with cabal2nix

--- pkgs/development/libraries/haskell/hoodle-render/default.nix
+++ pkgs/development/libraries/haskell/hoodle-render/default.nix
@@ -1,13 +1,17 @@
-{ cabal, base64Bytestring, cairo, gd, hoodleTypes, lens, monadLoops, poppler, strict, svgcairo, uuid }:
+{ cabal, base64Bytestring, cairo, filepath, gd, hoodleTypes, lens
+, monadLoops, mtl, poppler, strict, svgcairo, uuid
+}:
 
 cabal.mkDerivation (self: {
   pname = "hoodle-render";
   version = "0.3.2";
   sha256 = "1mmx27g1vqpndk26nz2hy7rckcgg68clvr5x31cqz9f8sifd8rsg";
-  buildDepends = [ base64Bytestring cairo gd hoodleTypes lens monadLoops poppler strict svgcairo uuid];
-  #jailbreak = true;
+  buildDepends = [
+    base64Bytestring cairo filepath gd hoodleTypes lens monadLoops mtl
+    poppler strict svgcairo uuid
+  ];
+  jailbreak = true;
   meta = {
-    homepage = "http://ianwookim.org/hoodle";
     description = "Hoodle file renderer";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
