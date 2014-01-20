a2d0ec2f4b97b070fb975f35b0f4ddbde566877b
Oliver Charles <ollie@ocharles.org.uk>
2014-01-20 11:58:22 +0000

    Update siege to 3.0.5 and add meta information

--- pkgs/tools/networking/siege/default.nix
+++ pkgs/tools/networking/siege/default.nix
@@ -1,12 +1,18 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, lib }:
 let
-  version = "3.0.2";
+  version = "3.0.5";
   baseName = "siege";
 in stdenv.mkDerivation rec {
   name = "${baseName}-${version}";
   src = fetchurl {
     url = "http://www.joedog.org/pub/siege/${name}.tar.gz";
-    sha256 = "0b86rvcrjxy6h9w32bhpcm1gwmn223mf9f30dfsnaw51w90kn716";
+    sha256 = "16faa6kappg23bdriyiy3ym94rmddpvw8cl8xgv5nxq2v17n4gi8";
+  };
+  meta = {
+    description = "HTTP load tester";
+    maintainers = with lib.maintainers; [ ocharles raskin ];
+    platforms = with lib.platforms; linux;
+    license = with lib.licenses; gpl2Plus;
   };
   NIX_LDFLAGS = stdenv.lib.optionalString stdenv.isLinux "-lgcc_s";
 }
