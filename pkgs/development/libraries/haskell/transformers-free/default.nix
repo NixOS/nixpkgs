4350308ec74de7dfb69451e2b8f449ece19d6009
Peter Simons <simons@cryp.to>
2014-01-20 18:05:00 +0100

    haskell-transformers-free: re-generate with cabal2nix

--- pkgs/development/libraries/haskell/transformers-free/default.nix
+++ pkgs/development/libraries/haskell/transformers-free/default.nix
@@ -6,7 +6,6 @@ cabal.mkDerivation (self: {
   sha256 = "0fbzkr7ifvqng8wqi3332vwvmx36f8z167angyskfdd0a5rik2z0";
   buildDepends = [ transformers ];
   meta = {
-    homepage = "https://github.com/Gabriel439/Haskell-Transformers-Free-Library";
     description = "Free monad transformers";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
