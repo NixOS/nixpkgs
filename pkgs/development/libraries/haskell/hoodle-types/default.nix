22727d9507dac459d6247cdce4ea44ad8913cc8a
Peter Simons <simons@cryp.to>
2014-01-20 18:05:00 +0100

    haskell-hoodle-types: re-generate with cabal2nix

--- pkgs/development/libraries/haskell/hoodle-types/default.nix
+++ pkgs/development/libraries/haskell/hoodle-types/default.nix
@@ -5,13 +5,11 @@ cabal.mkDerivation (self: {
   version = "0.2.2";
   sha256 = "0dw2ji676nq3idb7izzzfnxzhyngf84wkapc0la43g4w4hzv1zxz";
   buildDepends = [ cereal lens mtl strict uuid ];
-  #jailbreak = true;
+  jailbreak = true;
   meta = {
-    homepage = "http://ianwookim.org/hoodle";
     description = "Data types for programs for hoodle file format";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = [ self.stdenv.lib.maintainers.ianwookim ];
   };
 })
-
