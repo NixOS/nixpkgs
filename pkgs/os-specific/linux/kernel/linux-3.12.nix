8bc7c9f66d6a3b23b0a7a69fbd33882ca5f54bfc
William A. Kennington III <william@wkennington.com>
2014-01-19 19:11:25 -0600

    linux: Update from 3.12.7 -> 3.12.8

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.12.7";
+  version = "3.12.8";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1pmz1g919kbhf8mq23d25187gwzr8l85xj7bf9dvn3vpai7spafb";
+    sha256 = "15smxcn4q5c94jvdkgpz2r2dy4wgla67rp6s6r1yamz250xmc2xm";
   };
 
   features.iwlwifi = true;
