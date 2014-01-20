a1adb429ecaf094f28c6ce00bdfe79a4660245b9
Peter Simons <simons@cryp.to>
2014-01-20 18:05:00 +0100

    haskell-hoodle-builder: re-generate with cabal2nix

--- pkgs/development/libraries/haskell/hoodle-builder/default.nix
+++ pkgs/development/libraries/haskell/hoodle-builder/default.nix
@@ -1,13 +1,15 @@
-{ cabal, blazeBuilder, doubleConversion, hoodleTypes, lens, strict}:
+{ cabal, blazeBuilder, doubleConversion, hoodleTypes, lens, strict
+}:
 
 cabal.mkDerivation (self: {
   pname = "hoodle-builder";
   version = "0.2.2";
   sha256 = "0gagfpjihf6lafi90r883n9agaj1pw4gygaaxv4xxfsc270855bq";
-  buildDepends = [ blazeBuilder doubleConversion hoodleTypes lens strict ];
-  #jailbreak = true;
+  buildDepends = [
+    blazeBuilder doubleConversion hoodleTypes lens strict
+  ];
+  jailbreak = true;
   meta = {
-    homepage = "http://ianwookim.org/hoodle";
     description = "text builder for hoodle file format";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
