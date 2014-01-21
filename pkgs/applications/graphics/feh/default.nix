4e983788cdb14c182964fb9e451e63db4bc12605
Peter Simons <simons@cryp.to>
2014-01-21 20:19:21 +0100

    feh: update from 2.8 to 2.9.3

--- pkgs/applications/graphics/feh/default.nix
+++ pkgs/applications/graphics/feh/default.nix
@@ -2,11 +2,11 @@
 , libXinerama, curl }:
 
 stdenv.mkDerivation rec {
-  name = "feh-2.8";
+  name = "feh-2.9.3";
 
   src = fetchurl {
     url = "http://feh.finalrewind.org/${name}.tar.bz2";
-    sha256 = "0zmslchnzvi9ydxj2mgci4x8zpv5mdfkf7kyny3nibbpajibqmrx";
+    sha256 = "1wlhfbglzc1jzsh80s4s1fawclgzyjy2105ffzx2mw9s0c1xds5l";
   };
 
   buildInputs = [x11 imlib2 giblib libjpeg libpng libXinerama curl ];
