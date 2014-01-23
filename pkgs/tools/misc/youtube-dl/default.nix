f0081dcc02b240ed9a6fd8717ba298b4223622c2
Evgeny Egorochkin <phreedom@yandex.ru>
2014-01-23 20:52:36 +0200

    youtubeDL: update from 2014.01.08 to 2014.01.20

--- pkgs/tools/misc/youtube-dl/default.nix
+++ pkgs/tools/misc/youtube-dl/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, python, zip }:
 
 let
-  version = "2014.01.08";
+  version = "2014.01.20";
 in
 stdenv.mkDerivation rec {
   name = "youtube-dl-${version}";
 
   src = fetchurl {
     url = "http://youtube-dl.org/downloads/${version}/${name}.tar.gz";
-    sha256 = "0a83j62w2k5fdxp3fvs0zr6r81w7jkzzzr8r8wim1yyi6k8cjcwv";
+    sha256 = "1krqkq21pgzkfsw70b0fp7xpxsh1ipgr2bgkf1y30ckf9nkpp9ny";
   };
 
   buildInputs = [ python ];
