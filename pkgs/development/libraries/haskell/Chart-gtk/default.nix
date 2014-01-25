20929da52cb41b7d2ca0b58ea4f0cf986f991b4a
Peter Simons <simons@cryp.to>
2014-01-25 16:52:41 +0100

    haskell-Chart-gtk: update to version 1.2

--- pkgs/development/libraries/haskell/Chart-gtk/default.nix
+++ pkgs/development/libraries/haskell/Chart-gtk/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "Chart-gtk";
-  version = "1.1";
-  sha256 = "1394h7jd8pk55396nz1xjisz4v7brqcf9fwdnw9g4q3x1b7dcgs8";
+  version = "1.2";
+  sha256 = "0qq72cf1m2gvcksa1jj5g9qi6b47pmpzh3grhs7kh3m7qyq0a56g";
   buildDepends = [ cairo Chart ChartCairo colour gtk mtl time ];
   meta = {
     homepage = "https://github.com/timbod7/haskell-chart/wiki";
