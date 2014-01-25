00eaf038ac41fb12771ad7639855de8dd3776f30
Domen Kožar <domen@dev.si>
2014-01-26 00:17:49 +0100

    gnome3.at_spi2_core: add gobject introspection support

--- pkgs/desktops/gnome-3/core/at-spi2-core/default.nix
+++ pkgs/desktops/gnome-3/core/at-spi2-core/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, python, pkgconfig, popt, intltool, dbus_glib
-, libX11, xextproto, libSM, libICE, libXtst, libXi }:
+, libX11, xextproto, libSM, libICE, libXtst, libXi, gobjectIntrospection }:
 
 stdenv.mkDerivation (rec {
   versionMajor = "2.10";
@@ -15,6 +15,7 @@ stdenv.mkDerivation (rec {
   buildInputs = [
     python pkgconfig popt  intltool dbus_glib
     libX11 xextproto libSM libICE libXtst libXi
+    gobjectIntrospection
   ];
 
   # ToDo: on non-NixOS we create a symlink from there?
