c18cc5d6e893cdf975bbf801c930c8444de79eb7
Peter Simons <simons@cryp.to>
2014-01-25 16:52:42 +0100

    haskell-heist: update to version 0.13.0.6

--- pkgs/development/libraries/haskell/heist/default.nix
+++ pkgs/development/libraries/haskell/heist/default.nix
@@ -6,8 +6,8 @@
 
 cabal.mkDerivation (self: {
   pname = "heist";
-  version = "0.13.0.5";
-  sha256 = "17lpqiidy1s6yzhh865y7dhkcv34p7pxzljpn64yyfa2pc8885dj";
+  version = "0.13.0.6";
+  sha256 = "1h34bmcb9bqkagcx3iqnp4l8z8qhngf00mki4hpk905znja6hib9";
   buildDepends = [
     aeson attoparsec blazeBuilder blazeHtml directoryTree dlist errors
     filepath hashable MonadCatchIOTransformers mtl random text time
