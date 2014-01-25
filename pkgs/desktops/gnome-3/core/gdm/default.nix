7546695dbb5fefb3f2914ab315ff2e8062946c38
Domen Kožar <domen@dev.si>
2014-01-26 00:19:06 +0100

    gdm: add gobject introspection support

--- pkgs/desktops/gnome-3/core/gdm/default.nix
+++ pkgs/desktops/gnome-3/core/gdm/default.nix
@@ -1,16 +1,16 @@
 { stdenv, fetchurl, pkgconfig, glib, itstool, libxml2, intltool, accountservice, libX11
-, gtk, libcanberra_gtk3, pam, libtool
-}:
+, gtk, libcanberra_gtk3, pam, libtool, gobjectIntrospection }:
 
 stdenv.mkDerivation rec {
   name = "gdm-3.10.0.1";
 
   src = fetchurl {
-    url = "http://ftp.acc.umu.se/pub/GNOME/core/3.10/3.10.2/sources/${name}.tar.xz";
+    url = "mirror://gnome/sources/gdm/3.10/${name}.tar.xz";
     sha256 = "1rva3djas48m8w1gyv3nds3jxfkirdfl0bk30x79mizrk80456jl";
   };
 
-  buildInputs = [ pkgconfig glib itstool libxml2 intltool accountservice libX11 gtk libcanberra_gtk3 pam libtool ];
+  buildInputs = [ pkgconfig glib itstool libxml2 intltool accountservice
+                  gobjectIntrospection libX11 gtk libcanberra_gtk3 pam libtool ];
 
   meta = with stdenv.lib; {
     platforms = platforms.linux;
