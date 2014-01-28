5878ffa55b31317e8284cad8d5bae0f240d406bd
Petr Rockai <me@mornfall.net>
2014-01-28 21:45:52 +0100

    quvi: Update to current upstream (0.9.4).

--- pkgs/applications/video/quvi/tool.nix
+++ pkgs/applications/video/quvi/tool.nix
@@ -1,22 +1,25 @@
-{stdenv, fetchurl, pkgconfig, lua5, curl, quvi_scripts, libquvi}:
+{stdenv, fetchurl, pkgconfig, lua5, curl, quvi_scripts, libquvi, lua5_sockets, glib, makeWrapper}:
 
 stdenv.mkDerivation rec {
   name = "quvi-${version}";
-  version="0.4.0";
+  version="0.9.4";
 
   src = fetchurl {
-    url = "mirror://sourceforge/quvi/quvi-${version}.tar.gz";
-    sha256 = "0qzyj55py4z7pg97794jjycq8nvrlr02072rgjzg8jgknw49hgfv";
+    url = "mirror://sourceforge/quvi/quvi-${version}.tar.xz";
+    sha256 = "047rrwnnp72624z2px5nzn5wxi5fyckkddh2pj8j5xs6kdim429m";
   };
 
-  buildInputs = [ pkgconfig lua5 curl quvi_scripts libquvi ];
+  buildInputs = [ pkgconfig lua5 curl quvi_scripts libquvi glib makeWrapper ];
+  postInstall = ''
+      wrapProgram $out/bin/quvi --set LUA_PATH "${lua5_sockets}/share/lua/${lua5.luaversion}/?.lua"
+  '';
 
-  meta = { 
+  meta = {
     description = "Web video downloader";
     homepage = http://quvi.sf.net;
     license = "LGPLv2.1+";
     platforms = stdenv.lib.platforms.linux;
-    maintainers = [ stdenv.lib.maintainers.mornfall ]; 
+    maintainers = [ stdenv.lib.maintainers.mornfall ];
   };
 }
 
