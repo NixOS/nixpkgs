048029493750a9f8d253054f10fe6158f59835b3
Peter Simons <simons@cryp.to>
2014-01-28 10:31:43 +0100

    haskell-network-conduit: update to version 1.0.2.1

--- pkgs/development/libraries/haskell/network-conduit/default.nix
+++ pkgs/development/libraries/haskell/network-conduit/default.nix
@@ -3,13 +3,12 @@
 
 cabal.mkDerivation (self: {
   pname = "network-conduit";
-  version = "1.0.2";
-  sha256 = "00x9m4lsh4hkvw6z6kqd3q7hpy2q905vcnj9x1wbn6swz621h4rw";
+  version = "1.0.2.1";
+  sha256 = "1dq7pwimjkr0wpyjphbvjy3klkcjl8jin76am5jbz3cxk1dr20jk";
   buildDepends = [
     conduit liftedBase monadControl network transformers
   ];
   testDepends = [ conduit ];
-  doCheck = false;
   meta = {
     homepage = "http://github.com/snoyberg/conduit";
     description = "Stream socket data using conduits";
