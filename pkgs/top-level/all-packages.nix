31fa2cd52bef42d670c569cb8951509c89c143b0
Ricardo M. Correia <rcorreia@wizy.org>
2014-01-29 19:06:45 +0100

    grsecurity: Fix building grsec-3.x.0 kernels

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6660,13 +6660,17 @@ let
   # config options you need (e.g. by overriding extraConfig). See list of options here:
   # https://en.wikibooks.org/wiki/Grsecurity/Appendix/Grsecurity_and_PaX_Configuration_Options
   linux_3_2_grsecurity = lowPrio (lib.overrideDerivation (linux_3_2.override (args: {
-    modDirVersion = "${linux_3_2.version}-grsec";
     kernelPatches = args.kernelPatches ++ [ kernelPatches.grsecurity_3_0_3_2_54 kernelPatches.grsec_path ];
+    argsOverride = {
+      modDirVersion = "${linux_3_2.modDirVersion}-grsec";
+    };
   })) (args: grsecurityOverrider args));
 
   linux_3_12_grsecurity = lowPrio (lib.overrideDerivation (linux_3_12.override (args: {
-    modDirVersion = "${linux_3_12.version}-grsec";
     kernelPatches = args.kernelPatches ++ [ kernelPatches.grsecurity_3_0_3_12_8 kernelPatches.grsec_path ];
+    argsOverride = {
+      modDirVersion = "${linux_3_12.modDirVersion}-grsec";
+    };
   })) (args: grsecurityOverrider args));
 
   linux_3_2_apparmor = lowPrio (linux_3_2.override {
