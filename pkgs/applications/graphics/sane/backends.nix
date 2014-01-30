e28c0da32e87f4c957d1e5312e56930686bc0d12
Rickard Nilsson <rickynils@gmail.com>
2014-01-30 12:26:11 +0100

    sane-backends: Add extra source url
    It seems alioth.debian.org has some problem (login needed for download), so
    I've added a pkgs.fedoraproject.org url.

--- pkgs/applications/graphics/sane/backends.nix
+++ pkgs/applications/graphics/sane/backends.nix
@@ -12,7 +12,10 @@ stdenv.mkDerivation rec {
   name = "sane-backends-${version}";
 
   src = fetchurl {
-    url = "https://alioth.debian.org/frs/download.php/file/3958/${name}.tar.gz";
+    urls = [
+      "http://pkgs.fedoraproject.org/repo/pkgs/sane-backends/sane-backends-1.0.24.tar.gz/1ca68e536cd7c1852322822f5f6ac3a4/${name}.tar.gz"
+      "https://alioth.debian.org/frs/download.php/file/3958/${name}.tar.gz"
+    ];
     curlOpts = "--insecure";
     sha256 = "0ba68m6bzni54axjk15i51rya7hfsdliwvqyan5msl7iaid0iir7";
   };
