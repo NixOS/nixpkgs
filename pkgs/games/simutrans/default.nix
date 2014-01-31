f06c3ef47f031fb9eb158ce600ced6c2dc99ff04
Linquize <linquize@yahoo.com.hk>
2014-01-31 21:27:55 +0800

    Fix previous simutrans 112.3 build
    This fixes commit b5d19fad6710e786a80f9584e65f4f17e9678c24

--- pkgs/games/simutrans/default.nix
+++ pkgs/games/simutrans/default.nix
@@ -26,8 +26,8 @@ let
     sha256 = "1ng963n2gvnwmsj73iy3gp9i5iqf5g6qk1gh1jnfm86gnjrsrq4m";
   };
   pak128 = fetchurl {
-    url = "mirror://sourceforge/simutrans/pak128/pak128%20for%20${ver_1}/pak128-2.2.0--${ver_1}.0.zip";
-    sha256 = "13rwv9q3fa3ac0k11ds7zkpd00k4mn14rb0cknknvyz46icb9n80";
+    url = "mirror://sourceforge/simutrans/pak128/pak128%20for%20${ver_1}/pak128-2.3.0--${ver_1}.2.zip";
+    sha256 = "0jcif6mafsvpvxh1njyd6z2f6sab0fclq3f3nlg765yp3i1bfgff";
   };
 
   withPak = pak: stdenv.mkDerivation {
@@ -45,6 +45,12 @@ let
     src = fetchurl {
       url = "mirror://sourceforge/simutrans/simutrans/${ver_h2}/simutrans-src-${ver_h2}.zip";
       sha256 = "0jdq2krfj3qsh8dks9ixsdvpyjq9yi80p58b0xjpsn35mkbxxaca";
+    };
+
+    # this resource is needed since 112.2 because the folders in simutrans directory has been removed from source code
+    resources = fetchurl {
+      url = "mirror://sourceforge/simutrans/simutrans/${ver_h2}/simulinux-${ver_h2}.zip";
+      sha256 = "14ly341pdkr8r3cd0q49w424m79iz38iaxfi9l1yfcxl8idkga1c";
     };
     sourceRoot = ".";
 
@@ -72,6 +78,7 @@ let
     installPhase = ''
       mkdir -p $out/share/
       mv simutrans $out/share/
+      unzip -o ${resources} -d $out/share/
 
       mkdir -p $out/bin/
       mv build/default/sim $out/bin/simutrans
