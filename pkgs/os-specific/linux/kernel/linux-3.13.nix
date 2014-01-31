f9f065a17809c3b8c4998df0160d6262357626fd
William A. Kennington III <william@wkennington.com>
2014-01-31 15:28:50 -0600

    kernel: Upgrade from 3.13.0 -> 3.13.1

--- pkgs/os-specific/linux/kernel/linux-3.13.nix
+++ pkgs/os-specific/linux/kernel/linux-3.13.nix
@@ -1,13 +1,11 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.13";
-
-  modDirVersion = "3.13.0";
+  version = "3.13.1";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "4d5e5eee5f276424c32e9591f1b6c971baedc7b49f28ce03d1f48b1e5d6226a2";
+    sha256 = "0gkikbz676h79rv0aaihl90mw7fqhjx3rkhcqrmzggjb54048jvq";
   };
 
   features.iwlwifi = true;
