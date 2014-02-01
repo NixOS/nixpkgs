586cf4ee42e98130b97355a70df8d6705b5cd632
James Cook <james.cook@utoronto.ca>
2014-02-01 01:07:00 -0800

    libyaml: Patch to fix CVE-2013-6393 (close #1658)
    (cherry picked from commit 21a220900ad72cebc08286f2f9d936db0da7fa6f)

--- pkgs/development/libraries/libyaml/default.nix
+++ pkgs/development/libraries/libyaml/default.nix
@@ -8,6 +8,9 @@ stdenv.mkDerivation {
     sha256 = "a8bbad7e5250b3735126b7e3bd9f6fce9db19d6be7cc13abad17a24b59ec144a";
   };
 
+  # Downloaded on 2014-02-01 from https://bugzilla.redhat.com/show_bug.cgi?id=1033990
+  patches = [ ./cve-2013-6393_a.patch ./cve-2013-6393_b.patch ./cve-2013-6393_c.patch ];
+
   meta = {
     homepage = http://pyyaml.org/;
     description = "A YAML 1.1 parser and emitter written in C";
