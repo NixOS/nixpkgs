b409b23fa0ecea780cc194d8e648966b3bf0d46e
Peter Simons <simons@cryp.to>
2014-02-02 20:16:13 +0100

    haskell-shelly: update to version 1.4.3.1

--- pkgs/development/libraries/haskell/shelly/default.nix
+++ pkgs/development/libraries/haskell/shelly/default.nix
@@ -1,12 +1,13 @@
-{ cabal, mtl, systemFileio, systemFilepath, text, time, unixCompat
+{ cabal, async, mtl, systemFileio, systemFilepath, text, time
+, unixCompat
 }:
 
 cabal.mkDerivation (self: {
   pname = "shelly";
-  version = "1.4.1";
-  sha256 = "04yi5kc9jfcpc1rq3prypc832xgv94lrzv3i8saf1rg8a6najwbx";
+  version = "1.4.3.1";
+  sha256 = "04q22wprp0x075xyx6lnr8r792izvfigi4zryz2lxr5kqnzjki6r";
   buildDepends = [
-    mtl systemFileio systemFilepath text time unixCompat
+    async mtl systemFileio systemFilepath text time unixCompat
   ];
   meta = {
     homepage = "https://github.com/yesodweb/Shelly.hs";
