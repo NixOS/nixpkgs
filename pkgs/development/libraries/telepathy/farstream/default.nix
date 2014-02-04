5da2c77fbd61a8924028e06163d5a572fe81c590
Shea Levy <shea@shealevy.com>
2014-02-04 07:05:21 -0500

    telepathy-farstream: Bump.
    Hopefully fixes the tested job
    
    Signed-off-by: Shea Levy <shea@shealevy.com>

--- pkgs/development/libraries/telepathy/farstream/default.nix
+++ pkgs/development/libraries/telepathy/farstream/default.nix
@@ -2,12 +2,12 @@
 , pkgconfig, libxslt, python, gstreamer, gst_python, pygobject }:
 
 stdenv.mkDerivation rec {
-  name = "${pname}-0.4.0";
+  name = "${pname}-0.6.0";
   pname = "telepathy-farstream";
 
   src = fetchurl {
     url = "http://telepathy.freedesktop.org/releases/${pname}/${name}.tar.gz";
-    sha256 = "13mllgfx4b1hh1vpzq8lk5qfr3ivkkkmwbxgi6d47avgk746kznd";
+    sha256 = "04v414jij2ba71krk7vh4avsz07sfp5w38dmvjwig5isdygy0905";
   };
 
   buildInputs = [ gst_plugins_base gst_python pygobject ];
