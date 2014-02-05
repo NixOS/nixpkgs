2f17d51ce8df8f394cc5f7a3e6a872441ebd5aa5
Peter Simons <simons@cryp.to>
2014-02-05 10:42:41 +0100

    haskell-blaze-html: update to version 0.7.0.1

--- pkgs/development/libraries/haskell/blaze-html/default.nix
+++ pkgs/development/libraries/haskell/blaze-html/default.nix
@@ -4,16 +4,13 @@
 
 cabal.mkDerivation (self: {
   pname = "blaze-html";
-  version = "0.7.0.0";
-  sha256 = "1k8mxq3hmf2s7qab67jz3yaan7wdc4mn5sa00rw5zk4mjh722w86";
+  version = "0.7.0.1";
+  sha256 = "05z0a6x49f56bazkcdxpdi2a7pyzsiv7qc72grcz9sqjz1d6yagh";
   buildDepends = [ blazeBuilder blazeMarkup text ];
   testDepends = [
     blazeBuilder blazeMarkup HUnit QuickCheck testFramework
     testFrameworkHunit testFrameworkQuickcheck2 text
   ];
-  patchPhase = ''
-    sed -i -e 's|blaze-markup.*>=.*,|blaze-markup,|' blaze-html.cabal
-  '';
   meta = {
     homepage = "http://jaspervdj.be/blaze";
     description = "A blazingly fast HTML combinator library for Haskell";
