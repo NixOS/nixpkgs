ebe7e96078131c272f41c84fa4988ae15e53eab2
Peter Simons <simons@cryp.to>
2014-02-05 10:42:58 +0100

    haskell-murmur-hash: update to version 0.1.0.7

--- pkgs/development/libraries/haskell/murmur-hash/default.nix
+++ pkgs/development/libraries/haskell/murmur-hash/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "murmur-hash";
-  version = "0.1.0.6";
-  sha256 = "0wnkwl3a9x0f4rvsj4wf129n03vpw8qk4kzx6vmrapwwb4r80npz";
+  version = "0.1.0.7";
+  sha256 = "125v4ypiv8n2m8zd1yi46prz96yy79ap0yzhm4vhrws4cf1zapkp";
   meta = {
     homepage = "http://github.com/nominolo/murmur-hash";
     description = "MurmurHash2 implementation for Haskell";
