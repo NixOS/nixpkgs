24029ec478b1755eb375f05714a311d9b1d94541
Vladimír Čunát <vcunat@gmail.com>
2014-02-08 11:54:16 +0100

    linux: minor updates 3.12.10, 3.10.29, 3.4.79

--- pkgs/os-specific/linux/kernel/linux-3.4.nix
+++ pkgs/os-specific/linux/kernel/linux-3.4.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.4.78";
+  version = "3.4.79";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1n9avgjy3qpr28n1rq80kc1gn33w9nz6bvwds6i4d5z793fp7qpk";
+    sha256 = "07xd01b5vl6gl4p2cs75fsn295jvwmlq2j9jw582b2ii8vsaavvv";
   };
 
   features.iwlwifi = true;
