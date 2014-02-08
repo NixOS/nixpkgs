c7b781ca129befdda451590b291c1761402b8645
PkmX <pkmx.tw@gmail.com>
2014-02-09 05:20:21 +0800

    Bump wqy-zenhei to 0.9.45.

--- pkgs/data/fonts/wqy-zenhei/default.nix
+++ pkgs/data/fonts/wqy-zenhei/default.nix
@@ -1,17 +1,20 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "wqy-zenhei-0.4.23-1";
+  name = "wqy-zenhei-${version}";
+  version = "0.9.45";
 
   src = fetchurl {
-    url = mirror://sourceforge/wqy/wqy-zenhei-0.4.23-1.tar.gz;
-    sha256 = "138nn81ai240av0xvcq4ab3rl73n0qlj3gwr3a36i63ry8vdj5qm";
+    url = "mirror://sourceforge/wqy/${name}.tar.gz";
+    sha256 = "1mkmxq8g2hjcglb3zajfqj20r4r88l78ymsp2xyl5yav8w3f7dz4";
   };
+
+  dontBuild = true;
 
   installPhase =
     ''
       mkdir -p $out/share/fonts
-      cp *.ttf $out/share/fonts
+      install -m644 *.ttc $out/share/fonts/
     '';
 
   meta = {
