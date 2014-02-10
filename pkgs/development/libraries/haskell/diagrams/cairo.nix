643c8e1aba330e51bdf2685ee23ced10550b97b0
Peter Simons <simons@cryp.to>
2014-02-10 11:05:54 +0100

    haskell-diagrams-cairo: update to version 1.0.1.2

--- pkgs/development/libraries/haskell/diagrams/cairo.nix
+++ pkgs/development/libraries/haskell/diagrams/cairo.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "diagrams-cairo";
-  version = "1.0.1.1";
-  sha256 = "0mjc24sir0mm1kqhkk26mfbz90kc71hdylral4bjymxs6fpx7crh";
+  version = "1.0.1.2";
+  sha256 = "040x3zdrr70kg10isby6xp8mswvjd84xiz2rf7w1y66g9izdgfmc";
   buildDepends = [
     cairo colour dataDefaultClass diagramsCore diagramsLib filepath
     hashable lens mtl split statestack time
