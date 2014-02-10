8e8b35c910b8c30b9ee17c5767c8fdaeddcb7581
Vladimír Čunát <vcunat@gmail.com>
2014-02-10 19:24:25 +0100

    libyaml: minor update 0.1.3 -> .5
    The CVE patches are in the release now.
    
    (cherry picked from commit 334a911ace888ae82d0c34b99c237f6f0632ff8d)
    
    Conflicts:
    	pkgs/development/libraries/libyaml/default.nix

--- pkgs/development/libraries/libyaml/default.nix
+++ pkgs/development/libraries/libyaml/default.nix
@@ -1,19 +1,18 @@
-{stdenv, fetchurl}:
-
+{ stdenv, fetchurl }:
+let
+  version = "0.1.5";
+in
 stdenv.mkDerivation {
-  name = "libyaml-0.1.3";
+  name = "libyaml-${version}";
 
   src = fetchurl {
-    url = http://pyyaml.org/download/libyaml/yaml-0.1.3.tar.gz;
-    sha256 = "a8bbad7e5250b3735126b7e3bd9f6fce9db19d6be7cc13abad17a24b59ec144a";
+    url = "http://pyyaml.org/download/libyaml/yaml-${version}.tar.gz";
+    sha256 = "1vrv5ly58bkmcyc049ad180f2m8iav6l9h3v8l2fqdmrny7yx1zs";
   };
 
-  # Downloaded on 2014-02-01 from https://bugzilla.redhat.com/show_bug.cgi?id=1033990
-  patches = [ ./cve-2013-6393_a.patch ./cve-2013-6393_b.patch ./cve-2013-6393_c.patch ];
-
-  meta = {
+  meta = with stdenv.lib; {
     homepage = http://pyyaml.org/;
     description = "A YAML 1.1 parser and emitter written in C";
-    license = "free";
+    license = licenses.mit;
   };
 }
