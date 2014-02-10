589d40db12585f2ee6320fc561ab26c7cf3fac96
Peter Simons <simons@cryp.to>
2014-02-10 11:06:40 +0100

    haskell-sqlite-simple: update to version 0.4.5.0

--- pkgs/development/libraries/haskell/sqlite-simple/default.nix
+++ pkgs/development/libraries/haskell/sqlite-simple/default.nix
@@ -1,12 +1,15 @@
-{ cabal, base16Bytestring, directSqlite, HUnit, text, time
-, transformers
+{ cabal, attoparsec, base16Bytestring, blazeBuilder, blazeTextual
+, directSqlite, HUnit, text, time, transformers
 }:
 
 cabal.mkDerivation (self: {
   pname = "sqlite-simple";
-  version = "0.4.4.0";
-  sha256 = "09vgy3hji0bjb3bwxwkwhmgf50q442dqr3d86g5l5s3xiw3hca0r";
-  buildDepends = [ directSqlite text time transformers ];
+  version = "0.4.5.0";
+  sha256 = "0y4w8rj46lawz3hbi8dz80fbvxxgzj85094dxkwvpzgrf33py4y4";
+  buildDepends = [
+    attoparsec blazeBuilder blazeTextual directSqlite text time
+    transformers
+  ];
   testDepends = [ base16Bytestring directSqlite HUnit text time ];
   meta = {
     homepage = "http://github.com/nurpax/sqlite-simple";
