c7767f32661ba6c3937c9eb1bafcd385b281a298
Peter Simons <simons@cryp.to>
2014-02-10 11:06:20 +0100

    haskell-ixdopp: update to version 0.1.3

--- pkgs/development/libraries/haskell/ixdopp/default.nix
+++ pkgs/development/libraries/haskell/ixdopp/default.nix
@@ -6,10 +6,10 @@ cabal.mkDerivation (self: {
   sha256 = "1vknwznk42b33q4pmh6z620g761yf3cmsmrmhilgq42i5qhll4d4";
   isLibrary = false;
   isExecutable = true;
-  buildDepends = [ preprocessorTools ];
+  buildDepends = [ preprocessorTools syb ];
   meta = {
     homepage = "http://www.eecs.harvard.edu/~tov/pubs/haskell-session-types/";
-    description = "Expands a Haskell program using ixdo notation into a Haskell program using the indexed monad bind operator (>>>=)";
+    description = "A preprocessor for expanding \"ixdo\" notation for indexed monads";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
   };
