b2d0c27d0acd3e8d4270038f93e672b1e63a79d6
Shea Levy <shea@shealevy.com>
2014-02-11 13:47:34 -0500

    Revert "Remove now-unneeded old version of haskell-tls"
    It's used by smtps-gmail
    
    This reverts commit 70d73236e866a9157aaa65c0aba7a3e4880a823c.

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -2386,7 +2386,13 @@ let result = let callPackage = x : y : modifyPrio (newScope result.finalReturn x
 
   timeCompat = callPackage ../development/libraries/haskell/time-compat {};
 
-  tls = callPackage ../development/libraries/haskell/tls {};
+  tls_1_1_5 = callPackage ../development/libraries/haskell/tls/1.1.5.nix {};
+  tls_1_2_2 = callPackage ../development/libraries/haskell/tls/1.2.2.nix {};
+  tls = self.tls_1_2_2;
+
+  tlsExtra = callPackage ../development/libraries/haskell/tls-extra {
+    tls = self.tls_1_1_5;
+  };
 
   transformers_0_2_2_0 = callPackage ../development/libraries/haskell/transformers/0.2.2.0.nix {};
   transformers_0_3_0_0 = if (pkgs.stdenv.lib.versionOlder ghc.version "7.7") then
