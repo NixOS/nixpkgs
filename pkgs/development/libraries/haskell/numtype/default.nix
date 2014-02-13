6ff746c698e75277314455122f62c919f6fb2654
Peter Simons <simons@cryp.to>
2014-02-13 10:58:01 +0100

    haskell-numtype: update to version 1.1

--- pkgs/development/libraries/haskell/numtype/default.nix
+++ pkgs/development/libraries/haskell/numtype/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "numtype";
-  version = "1.0.1";
-  sha256 = "130qchi9dplpg7pxf4pz7nz4mnprngw16mizqycp5pdlawbcp5js";
+  version = "1.1";
+  sha256 = "1az10xcfl6qpyy9qnh8g2iqx53rxnjxzc1h8kl1gira6yv7g6857";
   meta = {
     homepage = "http://dimensional.googlecode.com/";
     description = "Type-level (low cardinality) integers";
