6038c78c252a39709bf8ae8ade0116ecfc2114f9
Peter Simons <simons@cryp.to>
2014-02-13 10:57:53 +0100

    haskell-hledger-web: update to version 0.22.4

--- pkgs/development/libraries/haskell/hledger-web/default.nix
+++ pkgs/development/libraries/haskell/hledger-web/default.nix
@@ -8,8 +8,8 @@
 
 cabal.mkDerivation (self: {
   pname = "hledger-web";
-  version = "0.22.1";
-  sha256 = "0nqw7scnhcip2bg832p9v0rqk01gn4xwj9bqsvsvmh31fh9ldchw";
+  version = "0.22.4";
+  sha256 = "07xz6ijg3nzzjair5gdjjryv5hs2rxws4maz22rrqnpf8wwjjc54";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
@@ -21,9 +21,6 @@ cabal.mkDerivation (self: {
   ];
   testDepends = [ hspec yesod yesodTest ];
   doCheck = false;
-  patchPhase = ''
-    sed -i -e 's|blaze-html.*0.7|blaze-html|' -e 's|blaze-markup.*0.7|blaze-markup|' hledger-web.cabal
-  '';
   meta = {
     homepage = "http://hledger.org";
     description = "A web interface for the hledger accounting tool";
