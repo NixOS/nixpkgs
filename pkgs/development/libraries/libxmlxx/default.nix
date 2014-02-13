5857e5c4f8f0c8185ff616d2f5ec7e5530bc160f
Evgeny Egorochkin <phreedom@yandex.ru>
2014-02-13 19:06:20 +0200

    libxmlxx: update from 2.30.1 to 2.37.1

--- pkgs/development/libraries/libxmlxx/default.nix
+++ pkgs/development/libraries/libxmlxx/default.nix
@@ -1,10 +1,10 @@
 { stdenv, fetchurl
 , pkgconfig, libxml2, glibmm, perl }:
 stdenv.mkDerivation rec {
-  name = "libxml++-2.30.1";
+  name = "libxml++-2.37.1";
   src = fetchurl {
-    url = "mirror://gnome/sources/libxml++/2.30/${name}.tar.bz2";
-    sha256 = "02mrib11cjx5fshdr8p1biwvyl0xfkf86b6nh8ashwv590v0wgs3";
+    url = "mirror://gnome/sources/libxml++/2.37/${name}.tar.xz";
+    sha256 = "17xkdndcambij33k25cb5c4mg2457wi114kiaprjma9j0mh87cgk";
   };
 
   buildInputs = [ pkgconfig glibmm perl ];
