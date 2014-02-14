ad4e2bd4995555d1d3b05a47f134046917a4f93c
Evgeny Egorochkin <phreedom@yandex.ru>
2014-02-14 16:50:39 +0200

    linux_3_4: update from 3.4.79 to 3.4.80

--- pkgs/os-specific/linux/kernel/linux-3.4.nix
+++ pkgs/os-specific/linux/kernel/linux-3.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.4.79";
+  version = "3.4.80";
   extraMeta.branch = "3.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "07xd01b5vl6gl4p2cs75fsn295jvwmlq2j9jw582b2ii8vsaavvv";
+    sha256 = "1vma3bxydryhcg7iimndq4rbpzbpjvnq7qa5md6wm6iill011pil";
   };
 
   features.iwlwifi = true;
