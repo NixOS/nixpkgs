46d531bc3fb654fc74a46f4c693ee8b3c6075e34
David Guibert <david.guibert@gmail.com>
2014-02-17 20:15:01 +0100

    vaapi-vdpau: apply a patch to delete dropped types from VA API

--- pkgs/development/libraries/vaapi-vdpau/default.nix
+++ pkgs/development/libraries/vaapi-vdpau/default.nix
@@ -8,13 +8,18 @@ stdenv.mkDerivation rec {
     sha256 = "1fcvgshzyc50yb8qqm6v6wn23ghimay23ci0p8sm8gxcy211jp0m";
   };
 
+  patches = [ ./glext85.patch
+              (fetchurl { url = "http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/x11-libs/libva-vdpau-driver/files/libva-vdpau-driver-0.7.4-VAEncH264VUIBufferType.patch?revision=1.1";
+                          name = "libva-vdpau-driver-0.7.4-VAEncH264VUIBufferType.patch";
+                          sha256 = "166svcav6axkrlb3i4rbf6dkwjnqdf69xw339az1f5yabj72pqqs";
+                        }) ];
+
   buildInputs = [ libvdpau mesa libva pkgconfig ];
 
   preConfigure = ''
     sed -i -e "s,LIBVA_DRIVERS_PATH=.*,LIBVA_DRIVERS_PATH=$out/lib/dri," configure
   '';
 
-  patches = [ ./glext85.patch ];
 
   meta = {
     homepage = http://cgit.freedesktop.org/vaapi/vdpau-driver/;
