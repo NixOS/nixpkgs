d41c4a0b8dd83c8753f878572e992cf4700d2b22
Jaka Hudoklin <jakahudoklin@gmail.com>
2014-02-18 12:06:08 +0100

    build-support: Added makeSymlinks function
    I have refractored things a little bit. We need a function that would symlink
    a bunch of files (ex. created with writeText) to a folder. What `environemnt.etc`
    uses looks like a fit, because it has really nice syntax where you can specify
    `source`, `target` and `mode` so i generalize its functionality. I also added
    support for providing only derivation in a list of files to symlink
    where name is used as a `target` (usefull in a case if you use `writeText`).

--- pkgs/build-support/make-symlinks/builder.sh
+++ pkgs/build-support/make-symlinks/builder.sh
@@ -1,9 +1,43 @@
 source $stdenv/setup
 
-mkdir $out
-for file in $files
-do
-  subdir=`dirname $file`
-  mkdir -p $out/$subdir
-  ln -s $dir/$file $out/$file
+set -f
+sources_=($sources)
+targets_=($targets)
+modes_=($modes)
+path_=($path)
+set +f
+
+mkdir -p $out/$path_
+
+for ((i = 0; i < ${#targets_[@]}; i++)); do
+    source="${sources_[$i]}"
+    target="${targets_[$i]}"
+
+    if [[ "$source" =~ '*' ]]; then
+
+        # If the source name contains '*', perform globbing.
+        mkdir -p $out/$path_/$target
+        for fn in $source; do
+            ln -s "$fn" $out/$path_/$target/
+        done
+
+    else
+        
+        mkdir -p $out/$path_/$(dirname $target)
+        if ! [ -e $out/$path_/$target ]; then
+            ln -s $source $out/$path_/$target
+        else
+            echo "duplicate entry $target -> $source"
+            if test "$(readlink $out/$path_/$target)" != "$source"; then
+                echo "mismatched duplicate entry $(readlink $out/$path_/$target) <-> $source"
+                exit 1
+            fi
+        fi
+       
+        if test "${modes_[$i]}" != symlink; then
+            echo "${modes_[$i]}" > $out/$path_/$target.mode
+        fi
+        
+    fi
 done
+
