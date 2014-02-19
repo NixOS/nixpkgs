ad0732313d1d512f0cc504e7a5b354236ced49f2
Domen Kožar <domen@dev.si>
2014-02-19 16:53:08 +0100

    gnome3: typo

--- nixos/modules/services/x11/desktop-managers/gnome3.nix
+++ nixos/modules/services/x11/desktop-managers/gnome3.nix
@@ -34,7 +34,7 @@ in {
           export GTK_DATA_PREFIX=${config.system.path}
 
           # find theme engines
-          export GTK_PATH=${config.system.path}/lib/gtk-3.0:{config.system.path}/lib/gtk-2.0
+          export GTK_PATH=${config.system.path}/lib/gtk-3.0:${config.system.path}/lib/gtk-2.0
 
           export XDG_MENU_PREFIX=gnome
 
