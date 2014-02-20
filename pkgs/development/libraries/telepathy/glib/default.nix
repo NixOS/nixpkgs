668a0bbfc762e61a2b501e86253585a4102ed69d
Luca Bruno <luca.bruno@immobiliare.it>
2014-02-20 10:27:10 +0100

    telepathy-glib: Generate vala bindings

--- pkgs/development/libraries/telepathy/glib/default.nix
+++ pkgs/development/libraries/telepathy/glib/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, dbus_glib, glib, python, pkgconfig, libxslt
-, gobjectIntrospection }:
+, gobjectIntrospection, valaSupport ? true, vala }:
 
 stdenv.mkDerivation rec {
   name = "telepathy-glib-0.22.0";
@@ -9,9 +9,11 @@ stdenv.mkDerivation rec {
     sha256 = "0mqrq2azw70rm50vy21acfnzn8mmh0w7dxh87mwr1lyk0jn1n232";
   };
 
+  configureFlags = stdenv.lib.optional valaSupport "--enable-vala-bindings";
+
   propagatedBuildInputs = [dbus_glib glib python gobjectIntrospection];
 
-  buildInputs = [pkgconfig libxslt];
+  buildInputs = [pkgconfig libxslt] ++ stdenv.lib.optional valaSupport vala;
 
   meta = {
     homepage = http://telepathy.freedesktop.org;
