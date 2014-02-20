94591762fed83447e68548bd4da281070ae2f865
Lluís Batlle i Rossell <viric@viric.name>
2014-02-20 12:27:38 +0100

    Updating rdesktop to 1.8.1.

--- pkgs/applications/networking/remote/rdesktop/default.nix
+++ pkgs/applications/networking/remote/rdesktop/default.nix
@@ -2,17 +2,21 @@
 
 stdenv.mkDerivation (rec {
   pname = "rdesktop";
-  version = "1.7.1";
+  version = "1.8.1";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/${pname}/${name}.tar.gz";
-    sha256 = "0yc4xz95w40m8ailpjgqp9h7bkc758vp0dlq4nj1pvr3xfnl7sni";
+    sha256 = "0il248cdsxvwjsl4bswf27ld9r1a7d48jf6bycr86kf3i55q7k3n";
   };
 
   buildInputs = [openssl libX11];
 
-  configureFlags = [ "--with-openssl=${openssl}" ];
+  configureFlags = [
+    "--with-openssl=${openssl}"
+    "--disable-credssp"
+    "--disable-smartcard"
+  ];
 
   meta = {
     description = "rdesktop is an open source client for Windows Terminal Services";
