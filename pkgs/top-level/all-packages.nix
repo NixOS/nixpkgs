cab9ac7bbf2e84791e9b3fef06a233a201b728a0
Matej Cotman <cotman.matej@gmail.com>
2014-02-22 18:13:26 +0100

    twmn: add twmn to all-packages.nix

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8967,6 +8967,8 @@ let
 
   tribler = callPackage ../applications/networking/p2p/tribler { };
 
+  twmn = callPackage ../applications/misc/twmn { };
+
   twinkle = callPackage ../applications/networking/instant-messengers/twinkle {
     ccrtp = ccrtp_1_8;
     libzrtpcpp = libzrtpcpp_1_6;
