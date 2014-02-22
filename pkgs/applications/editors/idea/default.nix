8af6b20856fe6523463b54154b072c4f4347df07
Edward Tjörnhammar <ed@cflags.cc>
2014-02-22 23:42:11 +0100

    idea: update 13.0.2 -> 13.1.1
    And use buildnumbers only in the buildscript and use more familiar
    version numbers in the package name.
    
    [bjorn.forsman@gmail.com: modify commit message]

--- pkgs/applications/editors/idea/default.nix
+++ pkgs/applications/editors/idea/default.nix
@@ -5,10 +5,10 @@
 let
 
   buildIdea =
-  { name, src, description, license }:
+  { name, version, build, src, description, license }:
 
   stdenv.mkDerivation rec {
-    inherit name src license;
+    inherit name build src license;
     ideaItem = makeDesktopItem {
       name = "IDEA";
       exec = "idea";
@@ -23,26 +23,26 @@ let
     buildCommand = ''
       tar xvzf $src
       mkdir -p $out
-      cp -a $name $out
+      cp -a idea-$build $out
 
       interpreter=$(echo ${stdenv.glibc}/lib/ld-linux*.so.2)
 
-      7z x $out/$name/lib/snappy-java-1.0.5.jar
-      rm $out/$name/lib/snappy-java-1.0.5.jar
+      7z x $out/idea-$build/lib/snappy-java-1.0.5.jar
+      rm $out/idea-$build/lib/snappy-java-1.0.5.jar
       if [ "${stdenv.system}" == "x86_64-linux" ];then
-        patchelf --set-interpreter $interpreter $out/$name/bin/fsnotifier64
+        patchelf --set-interpreter $interpreter $out/idea-$build/bin/fsnotifier64
         patchelf --set-rpath ${stdenv.gcc.gcc}/lib64/ org/xerial/snappy/native/Linux/amd64/libsnappyjava.so
       else
-        patchelf --set-interpreter $interpreter $out/$name/bin/fsnotifier
+        patchelf --set-interpreter $interpreter $out/idea-$build/bin/fsnotifier
         patchelf --set-rpath ${stdenv.gcc.gcc}/lib/ org/xerial/snappy/native/Linux/i386/libsnappyjava.so
       fi
-      7z a -tzip $out/$name/lib/snappy-java-1.0.5.jar .
+      7z a -tzip $out/idea-$build/lib/snappy-java-1.0.5.jar .
 
       mkdir -p $out/bin
 
       jdk=${jdk}/lib/openjdk
 
-      makeWrapper $out/$name/bin/idea.sh $out/bin/idea \
+      makeWrapper $out/idea-$build/bin/idea.sh $out/bin/idea \
         --prefix PATH : ${jdk}/bin:${coreutils}/bin:${gnugrep}/bin:${which}/bin:${git}/bin \
         --prefix LD_RUN_PATH : ${stdenv.gcc.gcc}/lib/ \
         --prefix JDK_HOME : $jdk \
@@ -64,23 +64,27 @@ let
 
 in {
 
-  idea_community_1302 = buildIdea {
-    name = "idea-IC-133.696";
+  idea_community_1311 = buildIdea rec {
+    name = "idea-community-${version}";
+    version = "13.1.1";
+    build = "IC-135.480";
     description = "IntelliJ IDEA 13 Community Edition";
     license = stdenv.lib.licenses.asl20.shortName;
     src = fetchurl {
-      url = http://download-ln.jetbrains.com/idea/ideaIC-13.0.2.tar.gz;
-      sha256 = "a776878c2dba877761b74d2886d006f64eec3c9694dbd1578380287a9c8cbd53";
+      url = "http://download-ln.jetbrains.com/idea/ideaIC-${version}.tar.gz";
+      sha256 = "9e28d3e5682b037c9d6190622ab2a47112fa792539083cc7a4cb24f3f7bf7d22";
     };
   };
 
-  idea_ultimate_1302 = buildIdea {
-    name = "idea-IU-133.696";
+  idea_ultimate_1311 = buildIdea rec {
+    name = "idea-ultimate-${version}";
+    version = "13.1.1";
+    build = "IU-135.480";
     description = "IntelliJ IDEA 13 Ultimate Edition";
     license = stdenv.lib.licenses.unfree;
     src = fetchurl {
-      url = http://download-ln.jetbrains.com/idea/ideaIU-13.0.2.tar.gz;
-      sha256 = "7ba9499ecc82029470712d6ead520db1e2cf520e4f86d382cbdea1d0052712d0";
+      url = "http://download-ln.jetbrains.com/idea/ideaIU-${version}.tar.gz";
+      sha256 = "d699abcdcace387105a465049e015c1367dedf42f7a5f5a1f7b3d840e98b2658";
     };
   };
 
