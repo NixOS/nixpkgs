3ceadf7dc385eb51a7a04db96469f6b73dc36fda
Artyom Shalkhakov <artyom.shalkhakov@gmail.com>
2014-02-23 12:11:00 +0100

    Fixed SHA256 of ATS 0.2.11 as well as package name.

--- pkgs/development/compilers/ats/default.nix
+++ pkgs/development/compilers/ats/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, gmp }:
 
 let version = "0.2.11"; in stdenv.mkDerivation {
-  name = "ats2-postiats-${version}";
+  name = "ats-anairiats-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/ats-lang/ats-lang-anairiats-${version}.tgz";
-    sha256 = "1rzcqc7fwqf0y4cc14lr282r25s66jygf6cxrnf5l8p5p550l0dl";
+    sha256 = "0rqykyx5whichx85jr4l4c9fdan0qsdd4kwd7a81k3l07zbd9fc6";
   };
 
   buildInputs = [ gmp ];
