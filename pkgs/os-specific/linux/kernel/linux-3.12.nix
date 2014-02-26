05c19ced9b6997edb00fca5a96282921303564c9
Evgeny Egorochkin <phreedom@yandex.ru>
2014-02-26 20:49:27 +0200

    linuxPackages_3_12.perf: update from 3.12.12 to 3.12.13

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.12.12";
+  version = "3.12.13";
   extraMeta.branch = "3.12";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0wndjj5bdjfl4i9s5mj9wylhspygsvnl3pnwps6d65q2jm813lrv";
+    sha256 = "08r4k64v0nkr0dnmir3n3x7f4i83akl3ahx9cl7rbj29zc4ninmd";
   };
 
   features.iwlwifi = true;
