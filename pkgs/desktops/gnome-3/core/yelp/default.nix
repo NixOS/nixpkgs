036bb1330c89d97726179cb4e1e5648fa0a9c2d1
Luca Bruno <lucabru@src.gnome.org>
2014-02-27 20:37:28 +0100

    gnome3: Setup hook for gdk-pixbuf and gnome-icon-theme
    Add necessary paths to the environment so that
    applications can find icons.

--- pkgs/desktops/gnome-3/core/yelp/default.nix
+++ pkgs/desktops/gnome-3/core/yelp/default.nix
@@ -1,18 +1,18 @@
 { stdenv, intltool, fetchurl, webkitgtk, pkgconfig, gtk3, glib
 , file, librsvg, hicolor_icon_theme, gnome3, gdk_pixbuf
-, bash, makeWrapper, itstool, libxml2, libxslt, icu  }:
+, bash, makeWrapper, itstool, libxml2, libxslt, icu }:
 
 stdenv.mkDerivation rec {
   name = "yelp-3.10.1";
 
   src = fetchurl {
-    url = "https://download.gnome.org/sources/yelp/3.10/${name}.tar.xz";
+    url = "mirror://gnome/sources/yelp/3.10/${name}.tar.xz";
     sha256 = "17736479b7d0b1128c7d6cb3073f2b09e4bbc82670731b2a0d3a3219a520f816";
   };
 
-  propagatedUserEnvPkgs = [ librsvg gdk_pixbuf gnome3.gnome_themes_standard
-                            gnome3.gnome_icon_theme hicolor_icon_theme
-                            gnome3.gnome_icon_theme_symbolic ];
+  propagatedUserEnvPkgs = [ gnome3.gnome_themes_standard  ];
+  propagatedBuildInputs = [ librsvg gdk_pixbuf gnome3.gnome_icon_theme
+                            hicolor_icon_theme gnome3.gnome_icon_theme_symbolic ];
 
   preConfigure = "substituteInPlace ./configure --replace /usr/bin/file ${file}/bin/file";
 
@@ -20,14 +20,12 @@ stdenv.mkDerivation rec {
                   libxml2 libxslt icu file makeWrapper gnome3.yelp_xsl
                   gnome3.gsettings_desktop_schemas ];
 
-  installFlags = "gsettingsschemadir=\${out}/share/${name}/glib-2.0/schemas/";
+  installFlags = "gsettingsschemadir=\${out}/share/yelp/glib-2.0/schemas/";
 
   postInstall = ''
-    mkdir -p $out/lib/yelp/gdk-pixbuf-2.0/2.10.0
-    cat ${gdk_pixbuf}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache ${librsvg}/lib/gdk-pixbuf/loaders.cache > $out/lib/yelp/gdk-pixbuf-2.0/2.10.0/loaders.cache
     wrapProgram "$out/bin/yelp" \
-      --set GDK_PIXBUF_MODULE_FILE `readlink -e $out/lib/yelp/gdk-pixbuf-2.0/2.10.0/loaders.cache` \
-      --prefix XDG_DATA_DIRS : "${gtk3}/share:${gnome3.gnome_themes_standard}/:${gnome3.gnome_themes_standard}/share:${gnome3.gnome_icon_theme_symbolic}/share:${gnome3.yelp_xsl}/share/yelp-xsl:${gnome3.gnome_icon_theme}/share:${hicolor_icon_theme}/share:${gnome3.gsettings_desktop_schemas}/share:$out/share:$out/share/${name}"
+      --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE" \
+      --prefix XDG_DATA_DIRS : "${gtk3}/share:${gnome3.gnome_themes_standard}/:${gnome3.gnome_themes_standard}/share:${gnome3.yelp_xsl}/share/yelp-xsl:${gnome3.gsettings_desktop_schemas}/share:$out/share:$out/share/yelp:$XDG_ICON_DIRS"
   '';
 
   meta = with stdenv.lib; {
