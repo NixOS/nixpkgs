6eecf5f03f0237a601b10471e60034472d1413c3
Evgeny Egorochkin <phreedom@yandex.ru>
2014-02-27 18:52:05 +0200

    parallel: update from 20130422 to 20140222

--- pkgs/tools/misc/parallel/default.nix
+++ pkgs/tools/misc/parallel/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, perl }:
 
 stdenv.mkDerivation rec {
-  name = "parallel-20130422";
+  name = "parallel-20140222";
 
   src = fetchurl {
     url = "mirror://gnu/parallel/${name}.tar.bz2";
-    sha256 = "0aygc6d2sf2xrnyldv62a17masfs5am4zfm68k4fhvymwcfbp41h";
+    sha256 = "0zb3hg92br6a53jn0pzfl16ffc1hfw81jk7nzw5spkshsdrcqx3y";
   };
 
   patchPhase =
