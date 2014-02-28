8cd17a8b0312fcb455195a4c1f45f9a652184e12
Peter Simons <simons@cryp.to>
2014-02-28 10:43:49 +0100

    haskell-git-annex: update to version 5.20140227

--- pkgs/applications/version-management/git-and-tools/git-annex/default.nix
+++ pkgs/applications/version-management/git-and-tools/git-annex/default.nix
@@ -2,35 +2,35 @@
 , caseInsensitive, clientsession, cryptoApi, cryptohash, curl
 , dataDefault, dataenc, DAV, dbus, dlist, dns, editDistance
 , extensibleExceptions, feed, filepath, git, gnupg1, gnutls, hamlet
-, hinotify, hS3, hslogger, HTTP, httpConduit, httpTypes, IfElse
-, json, lsof, MissingH, MonadCatchIOTransformers, monadControl, mtl
-, network, networkConduit, networkInfo, networkMulticast
+, hinotify, hS3, hslogger, HTTP, httpClient, httpConduit, httpTypes
+, IfElse, json, liftedBase, lsof, MissingH
+, MonadCatchIOTransformers, monadControl, mtl, network
+, networkConduit, networkInfo, networkMulticast
 , networkProtocolXmpp, openssh, optparseApplicative, perl
 , QuickCheck, random, regexTdfa, rsync, SafeSemaphore, SHA, stm
 , tasty, tastyHunit, tastyQuickcheck, tastyRerun, text, time
 , transformers, unixCompat, utf8String, uuid, wai, waiLogger, warp
-, which, xmlConduit, xmlTypes, yesod, yesodCore, yesodDefault
-, yesodForm, yesodStatic
+, which, xmlTypes, yesod, yesodCore, yesodDefault, yesodForm
+, yesodStatic
 }:
 
 cabal.mkDerivation (self: {
   pname = "git-annex";
-  version = "5.20140221";
-  sha256 = "0z6awqbk6kagssx26ddixbrl381c1kmsmlgaqmx087g4rwp43wqb";
+  version = "5.20140227";
+  sha256 = "0zdaryvfpbmc454798z3z3sshf0br8v3sw0vnksqng3zm3w73dah";
   isLibrary = false;
   isExecutable = true;
   buildDepends = [
     aeson async blazeBuilder bloomfilter caseInsensitive clientsession
     cryptoApi cryptohash dataDefault dataenc DAV dbus dlist dns
     editDistance extensibleExceptions feed filepath gnutls hamlet
-    hinotify hS3 hslogger HTTP httpConduit httpTypes IfElse json
-    MissingH MonadCatchIOTransformers monadControl mtl network
-    networkConduit networkInfo networkMulticast networkProtocolXmpp
-    optparseApplicative QuickCheck random regexTdfa SafeSemaphore SHA
-    stm tasty tastyHunit tastyQuickcheck tastyRerun text time
-    transformers unixCompat utf8String uuid wai waiLogger warp
-    xmlConduit xmlTypes yesod yesodCore yesodDefault yesodForm
-    yesodStatic
+    hinotify hS3 hslogger HTTP httpClient httpConduit httpTypes IfElse
+    json liftedBase MissingH MonadCatchIOTransformers monadControl mtl
+    network networkConduit networkInfo networkMulticast
+    networkProtocolXmpp optparseApplicative QuickCheck random regexTdfa
+    SafeSemaphore SHA stm tasty tastyHunit tastyQuickcheck tastyRerun
+    text time transformers unixCompat utf8String uuid wai waiLogger
+    warp xmlTypes yesod yesodCore yesodDefault yesodForm yesodStatic
   ];
   buildTools = [ bup curl git gnupg1 lsof openssh perl rsync which ];
   configureFlags = "-fS3
