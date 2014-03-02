d2dd22651d1ffcc5a248bb6930ee691644538a9f
Mathijs Kwik <mathijs@bluescreen303.nl>
2014-03-02 08:17:22 +0100

    idris-mode: upgrade to current version

--- pkgs/applications/editors/emacs-modes/idris/default.nix
+++ pkgs/applications/editors/emacs-modes/idris/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchgit, emacs }:
 
 stdenv.mkDerivation rec {
-  name = "idris-mode-20140106";
+  name = "idris-mode-20140223";
 
   src = fetchgit {
     url = https://github.com/idris-hackers/idris-mode.git;
-    rev = "47df65dd5b554c1d7cf70a07c3bd06d80867f870";
-    sha256 = "55df66d1bace134bea83f0547e01daf068fc96dc080cf88ea8945ddcb2d08ea4";
+    rev = "486470533e74c55192e92a1afa050475915ee1e7";
+    sha256 = "ff2e6bd8fbf421e8f2db0789d2ff56c5103775b911b99bab64e4652d332bad43";
   };
 
   buildInputs = [ emacs ];
