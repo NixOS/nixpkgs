65cce5c38bacb66eac2aa35f5aa8b420d7ed22da
Peter Simons <simons@cryp.to>
2014-03-03 11:08:41 +0100

    opensmtpd: update from 201307151923p1 to 201402281146p1

--- pkgs/servers/mail/opensmtpd/default.nix
+++ pkgs/servers/mail/opensmtpd/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "opensmtpd-${version}";
-  version = "201307151923p1";
+  version = "201402281146p1";
 
   buildInputs = [ libevent zlib openssl db bison pam ];
 
   src = fetchurl {
     url = "http://www.opensmtpd.org/archives/${name}.tar.gz";
-    sha256 = "0cggq60zzz5mgj093pmgwjp4bm6znnhyv6ibp1vhkba7cxjavr4g";
+    sha256 = "0q287lx6pcz1bz6b334fb977dpbcg4rpyli3zrxj27wql4x0dg6b";
   };  
 
   configureFlags = [ 
