c4873add803cde6cb50f29f5ac896ef8b16a6958
Peter Simons <simons@cryp.to>
2014-03-03 11:08:43 +0100

    youtubeDL: update from 2014.02.26 to 2014.02.28

--- pkgs/tools/misc/youtube-dl/default.nix
+++ pkgs/tools/misc/youtube-dl/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, python, zip }:
 
 let
-  version = "2014.02.26";
+  version = "2014.02.28";
 in
 stdenv.mkDerivation rec {
   name = "youtube-dl-${version}";
 
   src = fetchurl {
     url = "http://youtube-dl.org/downloads/${version}/${name}.tar.gz";
-    sha256 = "069p3iyh96xc7y9ixzl4v5smq8i6d4gnhyy2yyzphjlfnpvwhb7j";
+    sha256 = "1xizqk47f3wd1g1mqs87iand4v3pjxxdzh4z7s6525hlkb8z5813";
   };
 
   buildInputs = [ python ];
