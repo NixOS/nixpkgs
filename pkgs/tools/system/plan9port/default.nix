d14df722a71f3d6c5d6c4ef545960e26fc18294b
Peter Simons <simons@cryp.to>
2014-03-03 11:08:41 +0100

    plan9port: update from 20110103 to 20140228

--- pkgs/tools/system/plan9port/default.nix
+++ pkgs/tools/system/plan9port/default.nix
@@ -1,13 +1,13 @@
 {stdenv, fetchurl, libX11, xproto, libXt, xextproto, libXext}:
 
 stdenv.mkDerivation rec {
-  name = "plan9port-20110103";
+  name = "plan9port-20140228";
   
   builder = ./builder.sh;
 
   src = fetchurl {
     url = "http://swtch.com/plan9port/${name}.tgz";
-    sha256 = "12hq3k03jgva72498qa1dyndakbhbfg0sc1jhcap9cxqj04xf0dc";
+    sha256 = "1l7nsjfrrcq0l43kw0f1437jz3nyl9qw7i2vn0sbmcsv5vmsj0cr";
   };
 
   buildInputs = [ libX11 xproto libXt xextproto libXext ];
