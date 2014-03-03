be693f08cbf24e4e1f65e837b54793cee8f1971d
Peter Simons <simons@cryp.to>
2014-03-03 11:53:01 +0100

    haskell-mono-traversable: update to version 0.3.1

--- pkgs/development/libraries/haskell/mono-traversable/default.nix
+++ pkgs/development/libraries/haskell/mono-traversable/default.nix
@@ -1,19 +1,19 @@
-{ cabal, comonad, hashable, hspec, QuickCheck, semigroupoids
+{ cabal, comonad, foldl, hashable, hspec, QuickCheck, semigroupoids
 , semigroups, text, transformers, unorderedContainers, vector
 , vectorInstances
 }:
 
 cabal.mkDerivation (self: {
   pname = "mono-traversable";
-  version = "0.3.0.3";
-  sha256 = "0n8bgynapx43f4agbp596ywnfv0cm12x3ihifr3vzv78iixnl0xh";
+  version = "0.3.1";
+  sha256 = "0a0vy0hp34sw1q7153jd614mqydzg79pw645kfxlihs3j7ac2b3j";
   buildDepends = [
     comonad hashable semigroupoids semigroups text transformers
     unorderedContainers vector vectorInstances
   ];
   testDepends = [
-    hspec QuickCheck semigroups text transformers unorderedContainers
-    vector
+    foldl hspec QuickCheck semigroups text transformers
+    unorderedContainers vector
   ];
   meta = {
     homepage = "https://github.com/snoyberg/mono-traversable";
