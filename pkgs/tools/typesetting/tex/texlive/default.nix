f0680440191f3dcd774d9f2a1971defc2b139e64
Jason "Don" O'Conal <lovek323@gmail.com>
2014-03-03 16:34:53 +1100

    texLiveFull: fix build on Darwin
    Added --with-system-icu=no, --with-system-harfbuzz=no (looks like the
    omission of --with-system-icu was not sufficient to force the builder
    not to look for system ICU libs).
    
    Disable building bibtex-x: complains about missing ICU directory.
    
    Disable building dvisvgm: a test fails (and I can't work out how to
    prevent the tests from running just for this).
    
    Disable building devnag: failing tests.

--- pkgs/tools/typesetting/tex/texlive/default.nix
+++ pkgs/tools/typesetting/tex/texlive/default.nix
@@ -45,7 +45,7 @@ rec {
 
     NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${icu}/include/layout";
 
-    ./Build --prefix="$out" --datadir="$out/share" --mandir "$out/share/man" --infodir "$out/share/info" \
+    ./Build --prefix="$out" --datadir="$out/share" --mandir="$out/share/man" --infodir="$out/share/info" \
       ${args.lib.concatStringsSep " " configureFlags}
     cd Work
   '' ) [ "minInit" "doUnpack" "addInputs" "defEnsureDir" ];
@@ -116,14 +116,22 @@ rec {
 
   configureFlags = [ "--with-x11" "--enable-ipc" "--with-mktexfmt"
     "--enable-shared" "--disable-native-texlive-build" "--with-system-zziplib"
-    "--with-system-libgs" "--with-system-t1lib" "--with-system-freetype2"
-    "--with-system-freetype=no" "--disable-ttf2pk" "--enable-ttf2pk2"
-    ]
-    ++ ( if stdenv.isDarwin
-         # ironically, couldn't get xetex compiling on darwin
-         then [ "--disable-xetex" "--disable-xdv2pdf" "--disable-xdvipdfmx" ]
-         # couldn't seem to get system icu working on darwin
-         else [ "--with-system-icu" ] );
+    "--with-system-libgs" "--with-system-t1lib" "--with-system-freetype2" 
+    "--with-system-freetype=no" "--disable-ttf2pk" "--enable-ttf2pk2" ]
+    ++ stdenv.lib.optionals stdenv.isDarwin [
+      # Complains about a missing ICU directory
+      "--disable-bibtex-x"
+
+      # TODO: We should be able to fix these tests
+      "--disable-devnag"
+      "--disable-dvisvgm"
+      "--disable-xdv2pdf"
+      "--disable-xdvipdfmx"
+      "--disable-xetex"
+
+      "--with-system-harfbuzz=no"
+      "--with-system-icu=no"
+    ];
 
   phaseNames = [ "addInputs" "doMainBuild" "doMakeInstall" "doPostInstall" ];
 
