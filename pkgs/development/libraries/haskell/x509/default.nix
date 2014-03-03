63a72b6ea04970393548c8473d1980744622f2b1
Peter Simons <simons@cryp.to>
2014-03-03 11:53:22 +0100

    haskell-x509: update to version 1.4.10

--- pkgs/development/libraries/haskell/x509/default.nix
+++ pkgs/development/libraries/haskell/x509/default.nix
@@ -5,8 +5,8 @@
 
 cabal.mkDerivation (self: {
   pname = "x509";
-  version = "1.4.9";
-  sha256 = "0yrlkmrc0ygki61wlfvavvrm5aklircrj5w1nbvjlimrc4j8vq9g";
+  version = "1.4.10";
+  sha256 = "1xsq0g7f5ki6l9yx604j6bsl1k7s3p1xr6hh2086hsgl9fp1f9ap";
   buildDepends = [
     asn1Encoding asn1Parse asn1Types cryptohash cryptoPubkeyTypes
     filepath mtl pem time
