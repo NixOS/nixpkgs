212fdb0afe429d3ae9bb54201ffe66f0d8557771
Peter Simons <simons@cryp.to>
2014-03-04 11:29:39 +0100

    haskell-postgresql-libpq: update to version 0.9.0.0

--- pkgs/development/libraries/haskell/postgresql-libpq/default.nix
+++ pkgs/development/libraries/haskell/postgresql-libpq/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "postgresql-libpq";
-  version = "0.8.2.6";
-  sha256 = "0n3lqffscwc6pq0rfy4yjild9hcgnkpq9a8icbvgxv4si13ipsgh";
+  version = "0.9.0.0";
+  sha256 = "09bi0npvly02zjhp463bmzm1n8w1cqsln676z82xi2in86317pv3";
   extraLibraries = [ postgresql ];
   meta = {
     homepage = "http://github.com/lpsmith/postgresql-libpq";
