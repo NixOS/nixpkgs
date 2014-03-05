5446f60e8af7a30c9a13963b01e049cc75d06b57
Peter Simons <simons@cryp.to>
2014-03-05 15:21:58 +0100

    haskell-time: update to version 1.4.2

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -2432,7 +2432,7 @@ let result = let callPackage = x : y : modifyPrio (newScope result.finalReturn x
   threepennyGui = callPackage ../development/libraries/haskell/threepenny-gui {};
 
   time_1_1_2_4 = callPackage ../development/libraries/haskell/time/1.1.2.4.nix {};
-  time_1_4_1 = callPackage ../development/libraries/haskell/time/1.4.1.nix {};
+  time_1_4_2 = callPackage ../development/libraries/haskell/time/1.4.2.nix {};
   # time is in the core package set. It should only be necessary to
   # pass it explicitly in rare circumstances.
   time = null;
