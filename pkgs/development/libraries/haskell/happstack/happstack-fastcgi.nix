88c2fd564aab672f3724660100b857f81f9a5fb7
Thomas Bereknyei <tomberek@gmail.com>
2014-03-06 23:21:08 -0500

    New maintainer (me) uploaded patched package

--- pkgs/development/libraries/haskell/happstack/happstack-fastcgi.nix
+++ pkgs/development/libraries/haskell/happstack/happstack-fastcgi.nix
@@ -2,15 +2,9 @@
 
 cabal.mkDerivation (self: {
   pname = "happstack-fastcgi";
-  version = "0.1.4";
-  sha256 = "0jgrwvqlskphp0ixvyvg09d7xfm1brxyjr4dlaq235n6v7f0kvmb";
+  version = "0.1.5";
+  sha256 = "0rvb041nx2f8azvfy1yysisjqrmsfbxnccn992v5q7zhlglcvj8h";
   buildDepends = [ cgi fastcgi happstackServer mtl utf8String ];
-  patches = [ ./HttpVersion.patch ];
-  /*patchPhase = ''*/
-    /*substituteInPlace src/Happstack/Server/FastCGI.hs --replace "Happstack.Server.HTTP.Types" "Happstack.Server.Types" \*/
-        /*--replace "*/
-
-    /*'';*/
   meta = {
     description = "Happstack extension for use with FastCGI";
     license = self.stdenv.lib.licenses.bsd3;
