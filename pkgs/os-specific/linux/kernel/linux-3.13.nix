9b650b074b4f80794565150feb40a26d8b4620aa
Ricardo M. Correia <rcorreia@wizy.org>
2014-03-08 21:50:02 +0100

    linux: Update to 3.13.6

--- pkgs/os-specific/linux/kernel/linux-3.13.nix
+++ pkgs/os-specific/linux/kernel/linux-3.13.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.13.5";
+  version = "3.13.6";
   extraMeta.branch = "3.13";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "03yggix59k2i2lf0m3kkqslcvvfcg19xx96ywbrfkfbb7vplw67w";
+    sha256 = "10j1rc63y0rcrnc2rx8jyczqg42gcbijd83qqd795qrrp5wnf7qw";
   };
 
   features.iwlwifi = true;
