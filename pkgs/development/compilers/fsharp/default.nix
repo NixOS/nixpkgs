f277d6f4a060ea98d85ffa55d5dea28b01b887d5
Austin Seipp <aseipp@pobox.com>
2014-03-08 15:45:52 -0600

    fsharp: specify exact mono path, install fsi/fsc binaries
    fsc/fsi are just symlinks for backwards compatibility.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/development/compilers/fsharp/default.nix
+++ pkgs/development/compilers/fsharp/default.nix
@@ -13,6 +13,16 @@ stdenv.mkDerivation rec {
   buildInputs = [ mono pkgconfig autoconf automake which ];
   configurePhase = "./autogen.sh --prefix $out";
 
+  # Make sure the executables use the right mono binary,
+  # and set up some symlinks for backwards compatibility.
+  postInstall = ''
+    substituteInPlace $out/bin/fsharpc --replace " mono " " ${mono}/bin/mono "
+    substituteInPlace $out/bin/fsharpi --replace " mono " " ${mono}/bin/mono "
+    substituteInPlace $out/bin/fsharpiAnyCpu --replace " mono " " ${mono}/bin/mono "
+    ln -s $out/bin/fsharpc $out/bin/fsc
+    ln -s $out/bin/fsharpi $out/bin/fsi
+  '';
+
   # To fix this error when running:
   # The file "/nix/store/path/whatever.exe" is an not a valid CIL image
   dontStrip = true;
