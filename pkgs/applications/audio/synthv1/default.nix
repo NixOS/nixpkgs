dc80186449660acf10a4bd7a1e3e2fe017139b29
Cillian de Róiste <cillian.deroiste@gmail.com>
2014-03-08 12:46:59 +0100

    synthv1: update from 0.3.6 to 0.4.0

--- pkgs/applications/audio/synthv1/default.nix
+++ pkgs/applications/audio/synthv1/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "synthv1-${version}";
-  version = "0.3.6";
+  version = "0.4.0";
 
   src = fetchurl {
     url = "mirror://sourceforge/synthv1/${name}.tar.gz";
-    sha256 = "1xj4dk1g546f9fv2c4i7g3f1axrxfrxzk9w1nidhj3686j79nyry";
+    sha256 = "0zyxja1s74fr95qbwsyykggs3af2ndm2hz1l0avb645xgm93vcfv";
   };
 
   buildInputs = [ qt4 jackaudio lv2 ];
