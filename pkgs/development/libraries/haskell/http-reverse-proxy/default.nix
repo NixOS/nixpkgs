902d57fe40d4a69c6aad893475574405805a0e61
Peter Simons <simons@cryp.to>
2014-03-11 13:26:10 +0100

    haskell-http-reverse-proxy: update to version 0.3.1

--- pkgs/development/libraries/haskell/http-reverse-proxy/default.nix
+++ pkgs/development/libraries/haskell/http-reverse-proxy/default.nix
@@ -1,17 +1,17 @@
-{ cabal, basicPrelude, blazeBuilder, caseInsensitive, conduit
-, dataDefaultClass, hspec, httpClient, httpConduit, httpTypes
-, liftedBase, monadControl, network, networkConduit, resourcet
-, text, transformers, wai, waiLogger, warp, word8
+{ cabal, async, basicPrelude, blazeBuilder, caseInsensitive
+, conduit, dataDefaultClass, hspec, httpClient, httpConduit
+, httpTypes, liftedBase, monadControl, network, networkConduit
+, resourcet, text, transformers, wai, waiLogger, warp, word8
 }:
 
 cabal.mkDerivation (self: {
   pname = "http-reverse-proxy";
-  version = "0.3.0.1";
-  sha256 = "01rbczzf300ivb08wncm19wq64m7n6r5xfbgh82phjxjmmx9h6wj";
+  version = "0.3.1";
+  sha256 = "1xayhjrs6nm7gm5xc6z6vhzi9iq5czdvpywamkncivjsxjlc50b8";
   buildDepends = [
-    basicPrelude blazeBuilder caseInsensitive conduit dataDefaultClass
-    httpClient httpTypes liftedBase monadControl network networkConduit
-    resourcet text wai waiLogger word8
+    async basicPrelude blazeBuilder caseInsensitive conduit
+    dataDefaultClass httpClient httpTypes liftedBase monadControl
+    network networkConduit resourcet text wai waiLogger word8
   ];
   testDepends = [
     blazeBuilder conduit hspec httpConduit httpTypes liftedBase network
