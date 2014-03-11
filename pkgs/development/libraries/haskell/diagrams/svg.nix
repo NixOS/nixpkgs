220150c6cf505aad58e64f26cb46d811bf7506d0
Peter Simons <simons@cryp.to>
2014-03-11 11:49:14 +0100

    haskell-diagrams-svg: update to version 1.0.2

--- pkgs/development/libraries/haskell/diagrams/svg.nix
+++ pkgs/development/libraries/haskell/diagrams/svg.nix
@@ -5,8 +5,8 @@
 
 cabal.mkDerivation (self: {
   pname = "diagrams-svg";
-  version = "1.0.1.3";
-  sha256 = "0brbvzwh7yi3400wrdpkmw6jfd2nhi238zddhid76lmx2q9zxvvx";
+  version = "1.0.2";
+  sha256 = "1pbkr5qg3cjk2c23aas5y9f0bk5nsding4r4c4fdkxj49ipi4ys1";
   buildDepends = [
     blazeMarkup blazeSvg colour diagramsCore diagramsLib filepath
     hashable lens monoidExtras mtl split time vectorSpace
