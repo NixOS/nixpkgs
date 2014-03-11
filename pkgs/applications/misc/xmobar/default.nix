423b2e2cd495678893128bece3f2949cf98bc510
Peter Simons <simons@cryp.to>
2014-03-11 11:48:51 +0100

    haskell-xmobar: update to version 0.20

--- pkgs/applications/misc/xmobar/default.nix
+++ pkgs/applications/misc/xmobar/default.nix
@@ -1,15 +1,16 @@
-{ cabal, alsaCore, alsaMixer, filepath, libXrandr, mtl, parsec
-, regexCompat, stm, time, utf8String, wirelesstools, X11, X11Xft
+{ cabal, alsaCore, alsaMixer, filepath, HTTP, libXrandr, mtl
+, parsec, regexCompat, stm, time, utf8String, wirelesstools, X11
+, X11Xft
 }:
 
 cabal.mkDerivation (self: {
   pname = "xmobar";
-  version = "0.19";
-  sha256 = "1lwbww9vpqscip16lqiax2qvfyksxms5xx4n0s61mzw7v61hyxq2";
+  version = "0.20";
+  sha256 = "06ra5nx53rlijkb3hhp5p5a0b3bx14921jgkkp1xqciscnspj2nv";
   isLibrary = false;
   isExecutable = true;
   buildDepends = [
-    alsaCore alsaMixer filepath mtl parsec regexCompat stm time
+    alsaCore alsaMixer filepath HTTP mtl parsec regexCompat stm time
     utf8String X11 X11Xft
   ];
   extraLibraries = [ libXrandr wirelesstools ];
