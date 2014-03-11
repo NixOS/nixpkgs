856ed85f3cc594b1b9d41efdd9ad083d1132af6f
Peter Simons <simons@cryp.to>
2014-03-11 11:49:23 +0100

    haskell-hint: update to version 0.4.0.0

--- pkgs/development/libraries/haskell/hint/default.nix
+++ pkgs/development/libraries/haskell/hint/default.nix
@@ -1,17 +1,20 @@
-{ cabal, extensibleExceptions, filepath, ghcMtl, ghcPaths
-, haskellSrc, MonadCatchIOMtl, mtl, random, utf8String
+{ cabal, exceptions, extensibleExceptions, filepath, ghcMtl
+, ghcPaths, HUnit, mtl, random, utf8String
 }:
 
 cabal.mkDerivation (self: {
   pname = "hint";
-  version = "0.3.3.7";
-  sha256 = "1aba9dfkxlpmvbvllw4qnlrd300vnr0ismkn3kva1pv1cay5pifk";
+  version = "0.4.0.0";
+  sha256 = "0xs56ws7vwdjgvg7d31iqa111342an6rxvwhh7c3h4z1bq5l1l61";
   buildDepends = [
-    extensibleExceptions filepath ghcMtl ghcPaths haskellSrc
-    MonadCatchIOMtl mtl random utf8String
+    exceptions extensibleExceptions filepath ghcMtl ghcPaths mtl random
+    utf8String
+  ];
+  testDepends = [
+    exceptions extensibleExceptions filepath HUnit mtl
   ];
   meta = {
-    homepage = "http://darcsden.com/jcpetruzza/hint";
+    homepage = "http://hub.darcs.net/jcpetruzza/hint";
     description = "Runtime Haskell interpreter (GHC API wrapper)";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
