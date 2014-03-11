c7823d47f428529247b4cc5e28d05e8063f5a519
Peter Simons <simons@cryp.to>
2014-03-11 11:49:02 +0100

    haskell-cryptohash-cryptoapi: update to version 0.1.2

--- pkgs/development/libraries/haskell/cryptohash-cryptoapi/default.nix
+++ pkgs/development/libraries/haskell/cryptohash-cryptoapi/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "cryptohash-cryptoapi";
-  version = "0.1.0";
-  sha256 = "06b62ddwx2mp71dzaj8h88vw2c6nv3rj8n6d3d9vmqa7cws3mjkx";
+  version = "0.1.2";
+  sha256 = "1i2qxyq5qk4jzgkl7kndy10sbmzmagamfnqvl300qm3msi9k0kfy";
   buildDepends = [ cereal cryptoApi cryptohash tagged ];
   meta = {
     homepage = "http://github.com/vincenthz/hs-cryptohash-cryptoapi";
