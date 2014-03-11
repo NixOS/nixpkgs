4f5a93fc99f67f220174c9979f81086b98f94110
Peter Simons <simons@cryp.to>
2014-03-11 11:49:12 +0100

    haskell-diagrams-postscript: update to version 1.0.2

--- pkgs/development/libraries/haskell/diagrams/postscript.nix
+++ pkgs/development/libraries/haskell/diagrams/postscript.nix
@@ -1,14 +1,15 @@
-{ cabal, diagramsCore, diagramsLib, dlist, filepath, hashable, lens
-, monoidExtras, mtl, semigroups, split, vectorSpace
+{ cabal, dataDefaultClass, diagramsCore, diagramsLib, dlist
+, filepath, hashable, lens, monoidExtras, mtl, semigroups, split
+, vectorSpace
 }:
 
 cabal.mkDerivation (self: {
   pname = "diagrams-postscript";
-  version = "1.0.1.2";
-  sha256 = "0im1w70qi8qs2z8x41v7pwvk1alfaw1h8k0683njzd5sfz2m1gny";
+  version = "1.0.2";
+  sha256 = "14y8wivgxs3qvybzqk1bfqsrs5457qd5br7nk1924si5gpsgp1xx";
   buildDepends = [
-    diagramsCore diagramsLib dlist filepath hashable lens monoidExtras
-    mtl semigroups split vectorSpace
+    dataDefaultClass diagramsCore diagramsLib dlist filepath hashable
+    lens monoidExtras mtl semigroups split vectorSpace
   ];
   meta = {
     homepage = "http://projects.haskell.org/diagrams/";
