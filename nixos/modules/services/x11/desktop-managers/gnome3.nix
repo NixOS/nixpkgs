75793d6a3f4c4fd91e7a196918e55ea8e3ea800e
José Romildo Malaquias <malaquias@gmail.com>
2014-03-12 10:16:11 -0300

    environment.variables.GIO_EXTRA_MODULES is list of paths instead of a single path

--- nixos/modules/services/x11/desktop-managers/gnome3.nix
+++ nixos/modules/services/x11/desktop-managers/gnome3.nix
@@ -43,7 +43,7 @@ in {
         '';
       };
 
-    environment.variables.GIO_EXTRA_MODULES = "${gnome3.dconf}/lib/gio/modules";
+    environment.variables.GIO_EXTRA_MODULES = [ "${gnome3.dconf}/lib/gio/modules" ];
     environment.systemPackages =
       [ gnome3.evince
         gnome3.eog
