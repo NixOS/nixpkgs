dcb705ed62e5aa39017016e99fc7a3e2dedcd408
Peter Simons <simons@cryp.to>
2014-03-12 11:27:02 +0100

    haskell-hsini: update to version 0.3.1

--- pkgs/development/libraries/haskell/hsini/default.nix
+++ pkgs/development/libraries/haskell/hsini/default.nix
@@ -4,14 +4,13 @@
 
 cabal.mkDerivation (self: {
   pname = "hsini";
-  version = "0.2";
-  sha256 = "0d9dhzaw2v9r0hb4lywzw4f0inijbcw5brc5dh45zfkalmn3aqam";
+  version = "0.3.1";
+  sha256 = "06cys4i1nsic13dkp5jgammm3qykzizlnp6wdka2vl699rvnzaaf";
   buildDepends = [ mtl parsec ];
   testDepends = [
     HUnit mtl parsec QuickCheck testFramework testFrameworkHunit
     testFrameworkQuickcheck2 testFrameworkTh
   ];
-  doCheck = false;
   meta = {
     description = "Package for user configuration files (INI)";
     license = self.stdenv.lib.licenses.bsd3;
