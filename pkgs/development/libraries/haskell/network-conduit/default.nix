3267f1ee048f1b9de7a91d53200250c50fd8a581
Peter Simons <simons@cryp.to>
2014-03-12 11:27:04 +0100

    haskell-network-conduit: update to version 1.0.4

--- pkgs/development/libraries/haskell/network-conduit/default.nix
+++ pkgs/development/libraries/haskell/network-conduit/default.nix
@@ -3,8 +3,8 @@
 
 cabal.mkDerivation (self: {
   pname = "network-conduit";
-  version = "1.0.3";
-  sha256 = "0l5r0iws4zbqvkb2nlzxq0zspaz9vhl2a5r43jrxh4cvqb6lbn3q";
+  version = "1.0.4";
+  sha256 = "1a7p3gs0rczx0rhz27410rr6fzy3l0nsj6kk5wi0nqvqfyh7jb9c";
   buildDepends = [
     conduit liftedBase monadControl network transformers
   ];
