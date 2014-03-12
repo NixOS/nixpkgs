9b1dbc008725edb9d4d1e2edbc16f48f62954221
Peter Simons <simons@cryp.to>
2014-03-12 11:27:07 +0100

    haskell-persistent-sqlite: update to version 1.3.0.3

--- pkgs/development/libraries/haskell/persistent-sqlite/default.nix
+++ pkgs/development/libraries/haskell/persistent-sqlite/default.nix
@@ -4,8 +4,10 @@
 
 cabal.mkDerivation (self: {
   pname = "persistent-sqlite";
-  version = "1.3.0.2";
-  sha256 = "0mxnmbqsj67mfsp7d09wbziwfwcfdqr0rln0qcyi3wjwgybjmkzk";
+  version = "1.3.0.3";
+  sha256 = "0b7dp2hiza02rnnph44rd2vls06jipmixi32icbijmcqk83hfglq";
+  isLibrary = true;
+  isExecutable = true;
   buildDepends = [
     aeson conduit monadControl monadLogger persistent text transformers
   ];
