a5d87ea10357a8e412e6bbc8546a7794c545fffc
Christoph Hrdinka <c.github@hrdinka.at>
2014-03-13 19:59:58 +0100

    haskell-leveldb-haskell: add version 0.3.0

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -1590,6 +1590,8 @@ let result = let callPackage = x : y : modifyPrio (newScope result.finalReturn x
 
   lenses = callPackage ../development/libraries/haskell/lenses {};
 
+  leveldb-haskell = callPackage ../development/libraries/haskell/leveldb-haskell {};
+
   libffi = callPackage ../development/libraries/haskell/libffi {
     libffi = pkgs.libffi;
   };
