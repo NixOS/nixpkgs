05b667d619e905894f5d76d996463e85b6314fd5
Evgeny Egorochkin <phreedom@yandex.ru>
2014-03-13 09:09:19 +0200

    socat: update from 1.7.2.3 to 1.7.2.4

--- pkgs/tools/networking/socat/default.nix
+++ pkgs/tools/networking/socat/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, openssl }:
 
 stdenv.mkDerivation rec {
-  name = "socat-1.7.2.3";
+  name = "socat-1.7.2.4";
 
   src = fetchurl {
     url = "http://www.dest-unreach.org/socat/download/${name}.tar.bz2";
-    sha256 = "1l5ajqwfpxy35mvqlihzncmfngn61k7in51wkvd8q4vvmxaar605";
+    sha256 = "028yjka2zr6j1i8pmfmvzqki8ajczdl1hnry1x31xbbg3j83jxsb";
   };
 
   buildInputs = [ openssl ];
@@ -13,6 +13,7 @@ stdenv.mkDerivation rec {
   meta = {
     description = "A utility for bidirectional data transfer between two independent data channels";
     homepage = http://www.dest-unreach.org/socat/;
+    repositories.git = git://repo.or.cz/socat.git;
     platforms = stdenv.lib.platforms.linux;
     license = stdenv.lib.licenses.gpl2;
     maintainers = stdenv.lib.maintainers.eelco;
