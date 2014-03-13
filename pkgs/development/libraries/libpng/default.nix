f5464c298232c179508ee77ff35a187393c71264
Vladimír Čunát <vcunat@gmail.com>
2014-03-13 22:00:41 +0100

    libpng: update 1.6.9 -> .10 to fix CVE-2014-0333

--- pkgs/development/libraries/libpng/default.nix
+++ pkgs/development/libraries/libpng/default.nix
@@ -3,11 +3,11 @@
 assert zlib != null;
 
 let
-  version = "1.6.9";
-  sha256 = "0ji7488fp08b3xa6605zzghzpxawkhhg6jbqzrdw2y38zrvadqzx";
+  version = "1.6.10";
+  sha256 = "0mjsfxc18478y1jxrs3snmx7mvckmghvki9gfhmhl49n1vyz00s0";
   patch_src = fetchurl {
     url = "mirror://sourceforge/libpng-apng/libpng-${version}-apng.patch.gz";
-    sha256 = "0l61y6b03avyxrx8c72x8m73j3b8mj0wx80wdl6w82wgnr2fw4x1";
+    sha256 = "0512q27c26ajzqm2qzmmc7q1frj7cjylls2hxy3y3wg2r6ryizw8";
   };
   whenPatched = stdenv.lib.optionalString apngSupport;
 
