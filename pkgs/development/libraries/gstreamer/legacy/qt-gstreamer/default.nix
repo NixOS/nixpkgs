8de867c95869626d1615154877798ed622521842
Vladimír Čunát <vcunat@gmail.com>
2014-03-14 09:20:42 +0100

    qt_gstreamer: update from 0.10.2 to 0.10.3

--- pkgs/development/libraries/gstreamer/legacy/qt-gstreamer/default.nix
+++ pkgs/development/libraries/gstreamer/legacy/qt-gstreamer/default.nix
@@ -2,12 +2,12 @@
 , automoc4, flex, bison, pkgconfig }:
 
 stdenv.mkDerivation rec {
-  name = "${pname}-0.10.2";
+  name = "${pname}-0.10.3";
   pname = "qt-gstreamer";
 
   src = fetchurl {
     url = "http://gstreamer.freedesktop.org/src/${pname}/${name}.tar.bz2";
-    sha256 = "1laryg5vgc3prdi7dmpfwa71nsrc3ngv27sgax44c4qfrgpsgwbf";
+    sha256 = "1pqg9sxzk8sdrf7pazb5v21hasqai9i4l203gbdqz29w2ll1ybsl";
   };
 
   buildInputs = [ gstreamer gst_plugins_base glib qt4 ];
