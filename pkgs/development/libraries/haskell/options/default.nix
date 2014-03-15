dd701b4c51dd1e9b32d7cf0e01059c83bfa7a91c
Peter Simons <simons@cryp.to>
2014-03-15 09:47:28 +0100

    haskell-options: we cannot easily run the test suite because it requires 'chell', which requires 'options'

--- pkgs/development/libraries/haskell/options/default.nix
+++ pkgs/development/libraries/haskell/options/default.nix
@@ -6,6 +6,7 @@ cabal.mkDerivation (self: {
   sha256 = "0d40d6k1c8v2b0bgchgl54sk9wx28kysp8bjws8bwjcmmd57775f";
   buildDepends = [ monadsTf transformers ];
   testDepends = [ chell chellQuickcheck monadsTf transformers ];
+  doCheck = false;
   meta = {
     homepage = "https://john-millikin.com/software/haskell-options/";
     description = "A powerful and easy-to-use command-line option parser";
