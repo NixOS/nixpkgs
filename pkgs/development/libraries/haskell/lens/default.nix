b976817bd7eb9149aa0aa2077ffd8624884b4724
Peter Simons <simons@cryp.to>
2014-03-15 09:26:15 +0100

    haskell-lens: update to version 4.0.7

--- pkgs/development/libraries/haskell/lens/default.nix
+++ pkgs/development/libraries/haskell/lens/default.nix
@@ -1,23 +1,22 @@
-{ cabal, aeson, bifunctors, comonad, constraints, contravariant
-, deepseq, distributive, doctest, exceptions, filepath
-, genericDeriving, hashable, hlint, HUnit, mtl, nats, parallel
-, primitive, profunctors, QuickCheck, reflection, scientific
-, semigroupoids, semigroups, simpleReflect, split, tagged
-, testFramework, testFrameworkHunit, testFrameworkQuickcheck2
-, testFrameworkTh, text, transformers, transformersCompat
-, unorderedContainers, utf8String, vector, void, zlib
+{ cabal, aeson, bifunctors, comonad, contravariant, deepseq
+, distributive, doctest, exceptions, filepath, genericDeriving
+, hashable, hlint, HUnit, mtl, nats, parallel, primitive
+, profunctors, QuickCheck, reflection, scientific, semigroupoids
+, semigroups, simpleReflect, split, tagged, testFramework
+, testFrameworkHunit, testFrameworkQuickcheck2, testFrameworkTh
+, text, transformers, transformersCompat, unorderedContainers
+, utf8String, vector, void, zlib
 }:
 
 cabal.mkDerivation (self: {
   pname = "lens";
-  version = "4.0.5";
-  sha256 = "1kjmi76zpkhkwn514inhpfhfh3pg5na1f2zw1faqnk3bylpp9h6g";
+  version = "4.0.7";
+  sha256 = "0n4xw182yamgaplyshbhyjw9i7n6rvaszr4011mdbd6yvprl5v29";
   buildDepends = [
-    aeson bifunctors comonad constraints contravariant distributive
-    exceptions filepath hashable mtl parallel primitive profunctors
-    reflection scientific semigroupoids semigroups split tagged text
-    transformers transformersCompat unorderedContainers utf8String
-    vector void zlib
+    aeson bifunctors comonad contravariant distributive exceptions
+    filepath hashable mtl parallel primitive profunctors reflection
+    scientific semigroupoids semigroups split tagged text transformers
+    transformersCompat unorderedContainers utf8String vector void zlib
   ];
   testDepends = [
     deepseq doctest filepath genericDeriving hlint HUnit mtl nats
