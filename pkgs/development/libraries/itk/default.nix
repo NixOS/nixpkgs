d1e88c462587647675089a8b9270b2861833006c
Shea Levy <shea@shealevy.com>
2014-03-14 20:18:17 -0400

    Unconditionally compile itk with shared libs

--- pkgs/development/libraries/itk/default.nix
+++ pkgs/development/libraries/itk/default.nix
@@ -1,17 +1,18 @@
-{stdenv, fetchurl, cmake, libX11, libuuid, xz, sharedLibs ? false}:
+{stdenv, fetchurl, cmake, libX11, libuuid, xz}:
 
 stdenv.mkDerivation rec {
-  name = "itk${stdenv.lib.optionalString sharedLibs ''-shared''}-4.0.0";
+  name = "itk-4.0.0";
 
   src = fetchurl {
     url = mirror://sourceforge/itk/InsightToolkit-4.0.0.tar.xz;
     sha256 = "05z49sw612cbyiaghcsda0xylrkf06jh81ql79si5632w1hpgbd9";
   };
 
-  cmakeFlags = [ "-DBUILD_TESTING=OFF" "-DBUILD_EXAMPLES=OFF" ]
-    ++ stdenv.lib.optional sharedLibs [
-  "-DBUILD_SHARED_LIBS=ON"
-  "-DCMAKE_CXX_FLAGS=-fPIC"
+  cmakeFlags = [
+    "-DBUILD_TESTING=OFF"
+    "-DBUILD_EXAMPLES=OFF"
+    "-DBUILD_SHARED_LIBS=ON"
+    "-DCMAKE_CXX_FLAGS=-fPIC"
   ];
 
   enableParallelBuilding = true;
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4569,8 +4569,6 @@ let
 
   itk = callPackage ../development/libraries/itk { };
 
-  itkShared = itk.override { sharedLibs = true; };
-
   jamp = builderDefsPackage ../games/jamp {
     inherit mesa SDL SDL_image SDL_mixer;
   };
