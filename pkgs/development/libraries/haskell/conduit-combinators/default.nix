053391eba5e42ba34680df9e7a0fefdfdcf0d0a5
Peter Simons <simons@cryp.to>
2014-03-19 11:45:01 +0100

    haskell-conduit-combinators: update to version 0.2.1

--- pkgs/development/libraries/haskell/conduit-combinators/default.nix
+++ pkgs/development/libraries/haskell/conduit-combinators/default.nix
@@ -1,19 +1,20 @@
 { cabal, basicPrelude, chunkedData, conduit, hspec, monoTraversable
-, primitive, silently, systemFileio, systemFilepath, text
-, transformers, transformersBase, vector
+, mwcRandom, primitive, silently, systemFileio, systemFilepath
+, text, transformers, transformersBase, vector, void
 }:
 
 cabal.mkDerivation (self: {
   pname = "conduit-combinators";
-  version = "0.2.0.1";
-  sha256 = "0hmy398kk37n5l4pacb2a0z9h9f1kl6vva9gsph1kiqnnz7sbr4r";
+  version = "0.2.1";
+  sha256 = "0v3b9wiziyynk00fk07nwrk3c032wyr0adrwlkjl89ma1ix220sv";
   buildDepends = [
-    chunkedData conduit monoTraversable primitive systemFileio
-    systemFilepath text transformers transformersBase vector
+    chunkedData conduit monoTraversable mwcRandom primitive
+    systemFileio systemFilepath text transformers transformersBase
+    vector void
   ];
   testDepends = [
-    basicPrelude chunkedData hspec monoTraversable silently text
-    transformers vector
+    basicPrelude chunkedData hspec monoTraversable mwcRandom silently
+    text transformers vector
   ];
   meta = {
     homepage = "https://github.com/fpco/conduit-combinators";
