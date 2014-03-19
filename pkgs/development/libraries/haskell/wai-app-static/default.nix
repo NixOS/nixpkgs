6b5721c2b6fbe51d68b7a3f573306137fd759822
Peter Simons <simons@cryp.to>
2014-03-19 11:45:36 +0100

    haskell-wai-app-static: update to version 2.0.0.4

--- pkgs/development/libraries/haskell/wai-app-static/default.nix
+++ pkgs/development/libraries/haskell/wai-app-static/default.nix
@@ -1,19 +1,19 @@
 { cabal, base64Bytestring, blazeBuilder, blazeHtml, blazeMarkup
-, cereal, cryptoApi, cryptoConduit, cryptohashCryptoapi, fileEmbed
-, filepath, hspec, httpDate, httpTypes, mimeTypes, network
-, systemFileio, systemFilepath, text, time, transformers
-, unixCompat, unorderedContainers, wai, waiTest, zlib
+, byteable, cryptohash, cryptohashConduit, fileEmbed, filepath
+, hspec, httpDate, httpTypes, mimeTypes, network, systemFileio
+, systemFilepath, text, time, transformers, unixCompat
+, unorderedContainers, wai, waiTest, zlib
 }:
 
 cabal.mkDerivation (self: {
   pname = "wai-app-static";
-  version = "2.0.0.3";
-  sha256 = "0anbmp6wjk1qba1skr08fkm5y9lm58kfghzcgzgx8hrid7w85fb7";
+  version = "2.0.0.4";
+  sha256 = "1dk1s2q8w2rvknknw54ja2jhm1nayp8zpyis1zhgnl4yjgwr5kld";
   buildDepends = [
-    base64Bytestring blazeBuilder blazeHtml blazeMarkup cereal
-    cryptoApi cryptoConduit cryptohashCryptoapi fileEmbed filepath
-    httpDate httpTypes mimeTypes systemFileio systemFilepath text time
-    transformers unixCompat unorderedContainers wai zlib
+    base64Bytestring blazeBuilder blazeHtml blazeMarkup byteable
+    cryptohash cryptohashConduit fileEmbed filepath httpDate httpTypes
+    mimeTypes systemFileio systemFilepath text time transformers
+    unixCompat unorderedContainers wai zlib
   ];
   testDepends = [
     hspec httpDate httpTypes mimeTypes network text time transformers
