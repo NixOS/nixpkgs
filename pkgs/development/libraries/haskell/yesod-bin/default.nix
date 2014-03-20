cf29fe8205884784b029505b4b17453cae1dcd08
Peter Simons <simons@cryp.to>
2014-03-20 20:55:15 +0100

    haskell-yesod-bin: update to version 1.2.7.3

--- pkgs/development/libraries/haskell/yesod-bin/default.nix
+++ pkgs/development/libraries/haskell/yesod-bin/default.nix
@@ -1,26 +1,26 @@
 { cabal, attoparsec, base64Bytestring, blazeBuilder, Cabal, conduit
-, dataDefaultClass, fileEmbed, filepath, filesystemConduit
-, fsnotify, ghcPaths, httpConduit, httpReverseProxy, httpTypes
-, liftedBase, network, networkConduit, optparseApplicative, parsec
-, projectTemplate, resourcet, shakespeare, shakespeareCss
-, shakespeareJs, shakespeareText, split, systemFileio
-, systemFilepath, tar, text, time, transformers, unixCompat
-, unorderedContainers, wai, warp, yaml, zlib
+, dataDefaultClass, fileEmbed, filepath, fsnotify, ghcPaths
+, httpConduit, httpReverseProxy, httpTypes, liftedBase, network
+, networkConduit, optparseApplicative, parsec, projectTemplate
+, resourcet, shakespeare, shakespeareCss, shakespeareJs
+, shakespeareText, split, systemFileio, systemFilepath, tar, text
+, time, transformers, unixCompat, unorderedContainers, wai, warp
+, yaml, zlib
 }:
 
 cabal.mkDerivation (self: {
   pname = "yesod-bin";
-  version = "1.2.7.2";
-  sha256 = "13mn0b144a4cfwlpq25r72a4ffngjgfdzk8rd5yk37mcsqn7yagy";
+  version = "1.2.7.3";
+  sha256 = "05kbfnkigk4p1vjw0h1pkp7jfczjq99vgrsaz45faajadnhffnpz";
   isLibrary = false;
   isExecutable = true;
   buildDepends = [
     attoparsec base64Bytestring blazeBuilder Cabal conduit
-    dataDefaultClass fileEmbed filepath filesystemConduit fsnotify
-    ghcPaths httpConduit httpReverseProxy httpTypes liftedBase network
-    networkConduit optparseApplicative parsec projectTemplate resourcet
-    shakespeare shakespeareCss shakespeareJs shakespeareText split
-    systemFileio systemFilepath tar text time transformers unixCompat
+    dataDefaultClass fileEmbed filepath fsnotify ghcPaths httpConduit
+    httpReverseProxy httpTypes liftedBase network networkConduit
+    optparseApplicative parsec projectTemplate resourcet shakespeare
+    shakespeareCss shakespeareJs shakespeareText split systemFileio
+    systemFilepath tar text time transformers unixCompat
     unorderedContainers wai warp yaml zlib
   ];
   meta = {
