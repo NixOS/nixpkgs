048824a55bc498dde844ee6a75a47d1e35f31f29
Peter Simons <simons@cryp.to>
2014-03-20 20:55:10 +0100

    haskell-tasty-quickcheck: update to version 0.8.0.3

--- pkgs/development/libraries/haskell/tasty-quickcheck/default.nix
+++ pkgs/development/libraries/haskell/tasty-quickcheck/default.nix
@@ -1,10 +1,10 @@
-{ cabal, QuickCheck, random, tagged, tasty }:
+{ cabal, QuickCheck, tagged, tasty }:
 
 cabal.mkDerivation (self: {
   pname = "tasty-quickcheck";
-  version = "0.8";
-  sha256 = "10d7chqrlp1fjphnkiykxd22g4mfp69kmihd705sxb0y0mrdfh8x";
-  buildDepends = [ QuickCheck random tagged tasty ];
+  version = "0.8.0.3";
+  sha256 = "0dng415dsdg86rliwmz5hpn2111cn0x494c0vmdmzv5qgvx5naf6";
+  buildDepends = [ QuickCheck tagged tasty ];
   meta = {
     description = "QuickCheck support for the Tasty test framework";
     license = self.stdenv.lib.licenses.mit;
