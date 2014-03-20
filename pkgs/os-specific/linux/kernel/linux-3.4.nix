7552196127d10877791f7def8087791899c4ef1a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-03-20 15:40:24 +0100

    linux: Downgrade to 3.4.82
    3.4.83 breaks iwlwifi, see https://lkml.org/lkml/2014/3/12/420.

--- pkgs/os-specific/linux/kernel/linux-3.4.nix
+++ pkgs/os-specific/linux/kernel/linux-3.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.4.83";
+  version = "3.4.82";
   extraMeta.branch = "3.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1jammd31cgzymaqd2qwrm1shx5xwpaxmpnv658fix275fn35a8gb";
+    sha256 = "1z1kdbc4s3lpzpxfgcqkj6da3vp2xgk6q1314z0lamcaqhfsrr1x";
   };
 
   features.iwlwifi = true;
