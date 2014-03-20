08fed7fab54328402112b78ecd70e513becf7360
Peter Simons <simons@cryp.to>
2014-03-20 09:38:42 +0100

    haskell-ghcjs-dom: update to version 0.0.6

--- pkgs/development/libraries/haskell/ghcjs-dom/default.nix
+++ pkgs/development/libraries/haskell/ghcjs-dom/default.nix
@@ -1,10 +1,10 @@
-{ cabal, glib, gtk3, mtl, transformers, webkitgtk3 }:
+{ cabal, ghcjsBase, mtl }:
 
 cabal.mkDerivation (self: {
   pname = "ghcjs-dom";
-  version = "0.0.4";
-  sha256 = "1a3dv2l3s3aifcpivmnv308k2a1kb7r4x0z9gi83wc4xr9a8f08w";
-  buildDepends = [ glib gtk3 mtl transformers webkitgtk3 ];
+  version = "0.0.6";
+  sha256 = "0c27mf5cjvw5q4lwbmi245q4y09b61y5s6hxsfzgdn4lhfbvdma6";
+  buildDepends = [ ghcjsBase mtl ];
   meta = {
     description = "DOM library that supports both GHCJS and WebKitGTK";
     license = self.stdenv.lib.licenses.mit;
