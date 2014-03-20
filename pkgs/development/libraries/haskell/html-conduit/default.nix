94a591deedc0de733a832df3d2e0c6d9e62c37fe
Peter Simons <simons@cryp.to>
2014-03-20 20:55:06 +0100

    haskell-html-conduit: update to version 1.1.0.2

--- pkgs/development/libraries/haskell/html-conduit/default.nix
+++ pkgs/development/libraries/haskell/html-conduit/default.nix
@@ -1,15 +1,14 @@
-{ cabal, conduit, filesystemConduit, hspec, HUnit, resourcet
-, systemFilepath, tagstreamConduit, text, transformers, xmlConduit
-, xmlTypes
+{ cabal, conduit, hspec, HUnit, resourcet, systemFilepath
+, tagstreamConduit, text, transformers, xmlConduit, xmlTypes
 }:
 
 cabal.mkDerivation (self: {
   pname = "html-conduit";
-  version = "1.1.0.1";
-  sha256 = "0v3zlassakc34i7kzajx63s1rxn516xv7lrib0a4kn4cdlqn7kxs";
+  version = "1.1.0.2";
+  sha256 = "12a5hb9sf4sd11sjhwwp84k8whkxs7hqfyni2hi247fii2ldkfax";
   buildDepends = [
-    conduit filesystemConduit resourcet systemFilepath tagstreamConduit
-    text transformers xmlConduit xmlTypes
+    conduit resourcet systemFilepath tagstreamConduit text transformers
+    xmlConduit xmlTypes
   ];
   testDepends = [ hspec HUnit xmlConduit ];
   meta = {
