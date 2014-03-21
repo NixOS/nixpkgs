2560f2e78b3fe32c03813ed56d666459c90f06cb
Austin Seipp <aseipp@pobox.com>
2014-03-21 04:08:59 -0500

    fsharp: 3.0 -> 3.1, and attempt to fix build
    Hopefully this fix will appease hydra - the distributed autogen.sh
    script is annoying.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/development/compilers/fsharp/default.nix
+++ pkgs/development/compilers/fsharp/default.nix
@@ -2,16 +2,19 @@
 
 stdenv.mkDerivation rec {
   name = "fsharp-${version}";
-  version = "3.0";
+  version = "3.1";
 
   src = fetchgit {
     url = "https://github.com/fsharp/fsharp";
-    rev = "refs/heads/fsharp_30";
-    sha256 = "59639c76ff401c9ddb1af7a2f5a53a5aef4ec0d62317aeb33429f3eb009f771f";
+    rev = "refs/heads/fsharp_31";
+    sha256 = "0d41ae31c57ec9ac8a4ea149b615ae085f3774b8877d8e53ddbf68856c32eda0";
   };
 
   buildInputs = [ mono pkgconfig autoconf automake which ];
-  configurePhase = "./autogen.sh --prefix $out";
+  configurePhase = ''
+    substituteInPlace ./autogen.sh "/usr/bin/env sh" "/bin/sh"
+    ./autogen.sh --prefix $out
+  '';
 
   # Make sure the executables use the right mono binary,
   # and set up some symlinks for backwards compatibility.
