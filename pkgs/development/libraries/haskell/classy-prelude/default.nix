60358214278fd647c9e1fe2fb4692e963794e0de
Peter Simons <simons@cryp.to>
2014-03-21 18:46:59 +0100

    haskell-classy-prelude: update to version 0.8.1.1

--- pkgs/development/libraries/haskell/classy-prelude/default.nix
+++ pkgs/development/libraries/haskell/classy-prelude/default.nix
@@ -6,8 +6,8 @@
 
 cabal.mkDerivation (self: {
   pname = "classy-prelude";
-  version = "0.8.1";
-  sha256 = "04rgsw656vjzaibargw5ibzagy74mlsd25wpw6hn1phz8qkgls4l";
+  version = "0.8.1.1";
+  sha256 = "14iq0zdmw4f2i3c282hs89c4a763wcm7vn5n0f6kcvcpjgjyahgi";
   buildDepends = [
     basicPrelude chunkedData enclosedExceptions hashable liftedBase
     monoTraversable semigroups systemFilepath text time transformers
