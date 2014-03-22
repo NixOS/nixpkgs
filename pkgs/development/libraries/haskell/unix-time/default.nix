1bc1ddcda0d7dec6d9fea739651903fecc705d71
Peter Simons <simons@cryp.to>
2014-03-22 12:28:31 +0100

    haskell-unix-time: fix test suite in shared library build
    https://github.com/kazu-yamamoto/unix-time/issues/20

--- pkgs/development/libraries/haskell/unix-time/default.nix
+++ pkgs/development/libraries/haskell/unix-time/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.2.2";
   sha256 = "0xnl12mscc5nwjl9s2lx4xr8q8agzcpxh3bmxxidfjrg19drfwrm";
   testDepends = [ doctest hspec QuickCheck time ];
+  configureFlags = self.stdenv.lib.optionalString self.enableSharedLibraries "--ghc-option=-fPIC";
   meta = {
     description = "Unix time parser/formatter and utilities";
     license = self.stdenv.lib.licenses.bsd3;
