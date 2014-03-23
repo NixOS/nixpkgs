e7172cad9aec8cec889eee3a0c21476b7b88c521
Shea Levy <shea@shealevy.com>
2014-03-22 22:40:14 -0400

    haskell-postgresql-simple: bump

--- pkgs/development/libraries/haskell/postgresql-simple/default.nix
+++ pkgs/development/libraries/haskell/postgresql-simple/default.nix
@@ -1,15 +1,15 @@
 { cabal, aeson, attoparsec, base16Bytestring, blazeBuilder
-, blazeTextual, cryptohash, HUnit, postgresqlLibpq, text, time
-, transformers, uuid, vector
+, blazeTextual, cryptohash, hashable, HUnit, postgresqlLibpq
+, scientific, text, time, transformers, uuid, vector
 }:
 
 cabal.mkDerivation (self: {
   pname = "postgresql-simple";
-  version = "0.4.0.2";
-  sha256 = "0gx9jmmzv6aaa6z88i3j51f5hp153dbwzw3x7jx329c5pjp536g9";
+  version = "0.4.2.0";
+  sha256 = "0g31a7s2h9d6f3igvrddbr357sk4vabvg88mmvb194pps66y8zzn";
   buildDepends = [
-    aeson attoparsec blazeBuilder blazeTextual postgresqlLibpq text
-    time transformers uuid vector
+    aeson attoparsec blazeBuilder blazeTextual hashable postgresqlLibpq
+    scientific text time transformers uuid vector
   ];
   testDepends = [
     aeson base16Bytestring cryptohash HUnit text time vector
