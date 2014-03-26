0fc10d7a733e7106b38f5b54fe4e675b349c7d82
Emery Hemingway <emery@vfemail.net>
2014-03-26 18:12:00 -0400

    rippled: initial expression
    rippled is the Ripple P2P payment network reference server
    https://ripple.com

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6469,6 +6469,8 @@ let
 
   rethinkdb = callPackage ../servers/nosql/rethinkdb { };
 
+  rippled = callPackage ../servers/rippled { };
+
   spamassassin = callPackage ../servers/mail/spamassassin {
     inherit (perlPackages) HTMLParser NetDNS NetAddrIP DBFile
       HTTPDate MailDKIM LWP IOSocketSSL;
