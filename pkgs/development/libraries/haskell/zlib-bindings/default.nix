0312c15071c33b25b99dd678ef6868ad87012979
Peter Simons <simons@cryp.to>
2014-03-26 09:51:08 +0100

    haskell-zlib-bindings: update to version 0.1.1.4

--- pkgs/development/libraries/haskell/zlib-bindings/default.nix
+++ pkgs/development/libraries/haskell/zlib-bindings/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "zlib-bindings";
-  version = "0.1.1.3";
-  sha256 = "18jhav7chbapakm9mwn7bn9lgvip7qaz61dw5gwv2nyalvm96qfr";
+  version = "0.1.1.4";
+  sha256 = "02ln0gv9kkq68s3n195q2mpqimxb6k4wqs731b1gg8wrbbkaxb6w";
   buildDepends = [ zlib ];
   testDepends = [ hspec QuickCheck zlib ];
   meta = {
