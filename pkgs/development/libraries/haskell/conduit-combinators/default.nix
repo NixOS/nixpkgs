43de5f42c7ca5d4e34f2da73ccba53430ffc36d1
Peter Simons <simons@cryp.to>
2014-03-27 13:17:24 +0100

    haskell-conduit-combinators: update to version 0.2.3.1

--- pkgs/development/libraries/haskell/conduit-combinators/default.nix
+++ pkgs/development/libraries/haskell/conduit-combinators/default.nix
@@ -1,21 +1,21 @@
-{ cabal, base16Bytestring, base64Bytestring, basicPrelude
-, chunkedData, conduit, hspec, monoTraversable, mwcRandom
-, primitive, silently, systemFileio, systemFilepath, text
-, transformers, transformersBase, unixCompat, vector, void
+{ cabal, base16Bytestring, base64Bytestring, chunkedData, conduit
+, hspec, monoTraversable, mwcRandom, primitive, silently
+, systemFileio, systemFilepath, text, transformers
+, transformersBase, unixCompat, vector, void
 }:
 
 cabal.mkDerivation (self: {
   pname = "conduit-combinators";
-  version = "0.2.3";
-  sha256 = "05sb1v6rciaj7cj6lxv6pf9ai0k3q6cvvflcb4a7q6ql9xr3j7pr";
+  version = "0.2.3.1";
+  sha256 = "078i0727nhy75y6bxav6sxr1gz9cq04nvskdnzwabljppd34dqg4";
   buildDepends = [
     base16Bytestring base64Bytestring chunkedData conduit
     monoTraversable mwcRandom primitive systemFileio systemFilepath
     text transformers transformersBase unixCompat vector void
   ];
   testDepends = [
-    base16Bytestring base64Bytestring basicPrelude chunkedData hspec
-    monoTraversable mwcRandom silently text transformers vector
+    base16Bytestring base64Bytestring chunkedData hspec monoTraversable
+    mwcRandom silently systemFilepath text transformers vector
   ];
   meta = {
     homepage = "https://github.com/fpco/conduit-combinators";
