cbca108acd2e35c65a9b93b33006b5cd44de9ce3
Peter Simons <simons@cryp.to>
2014-03-27 13:17:58 +0100

    haskell-yesod-core: update to version 1.2.9.1

--- pkgs/development/libraries/haskell/yesod-core/default.nix
+++ pkgs/development/libraries/haskell/yesod-core/default.nix
@@ -1,23 +1,23 @@
 { cabal, aeson, async, attoparsecConduit, blazeBuilder, blazeHtml
 , blazeMarkup, caseInsensitive, cereal, clientsession, conduit
-, cookie, dataDefault, failure, fastLogger, hamlet, hspec
-, httpTypes, HUnit, liftedBase, monadControl, monadLogger, network
-, networkConduit, parsec, pathPieces, QuickCheck, random, resourcet
-, safe, shakespeare, shakespeareCss, shakespeareI18n, shakespeareJs
-, text, time, transformers, transformersBase, unixCompat, vector
-, wai, waiExtra, waiLogger, waiTest, warp, yesodRoutes
+, cookie, dataDefault, fastLogger, hamlet, hspec, httpTypes, HUnit
+, liftedBase, monadControl, monadLogger, network, networkConduit
+, parsec, pathPieces, QuickCheck, random, resourcet, safe
+, shakespeare, shakespeareCss, shakespeareI18n, shakespeareJs, text
+, time, transformers, transformersBase, unixCompat, vector, wai
+, waiExtra, waiLogger, waiTest, warp, yesodRoutes
 }:
 
 cabal.mkDerivation (self: {
   pname = "yesod-core";
-  version = "1.2.9";
-  sha256 = "0qd9x2pca2nx7yqz9whlvb0rjgf1zf9vw19j6z0p149yddgrrcyq";
+  version = "1.2.9.1";
+  sha256 = "1j37jf82csdvjgb36034c0pqrmzd5r131hfqh43jp4m1wqsrib5k";
   buildDepends = [
     aeson attoparsecConduit blazeBuilder blazeHtml blazeMarkup
     caseInsensitive cereal clientsession conduit cookie dataDefault
-    failure fastLogger hamlet httpTypes liftedBase monadControl
-    monadLogger parsec pathPieces random resourcet safe shakespeare
-    shakespeareCss shakespeareI18n shakespeareJs text time transformers
+    fastLogger hamlet httpTypes liftedBase monadControl monadLogger
+    parsec pathPieces random resourcet safe shakespeare shakespeareCss
+    shakespeareI18n shakespeareJs text time transformers
     transformersBase unixCompat vector wai waiExtra waiLogger warp
     yesodRoutes
   ];
