8470a695eda8b506e71ad494117b1b6270708d82
Peter Simons <simons@cryp.to>
2014-03-27 13:17:42 +0100

    haskell-project-template: update to version 0.1.4

--- pkgs/development/libraries/haskell/project-template/default.nix
+++ pkgs/development/libraries/haskell/project-template/default.nix
@@ -1,19 +1,18 @@
-{ cabal, base64Bytestring, base64Conduit, basicPrelude, conduit
-, hspec, mtl, QuickCheck, resourcet, systemFileio, systemFilepath
-, text, transformers
+{ cabal, base64Bytestring, conduit, hspec, mtl, QuickCheck
+, resourcet, systemFileio, systemFilepath, text, transformers
 }:
 
 cabal.mkDerivation (self: {
   pname = "project-template";
-  version = "0.1.3.2";
-  sha256 = "076xq5hwi7bfn3cmx8zd19vnj6lj2p7qm2waam94qqw2m466xq50";
+  version = "0.1.4";
+  sha256 = "1fmpb7jrn7mry8wq5jyxhkwyr61fknhi7p8mmqs7xn8lxwbj5904";
   buildDepends = [
-    base64Bytestring base64Conduit basicPrelude conduit mtl resourcet
-    systemFileio systemFilepath text transformers
+    base64Bytestring conduit mtl resourcet systemFileio systemFilepath
+    text transformers
   ];
   testDepends = [
-    base64Bytestring basicPrelude conduit hspec QuickCheck
-    systemFilepath text transformers
+    base64Bytestring conduit hspec QuickCheck systemFilepath text
+    transformers
   ];
   meta = {
     homepage = "https://github.com/fpco/haskell-ide";
