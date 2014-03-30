11679b331a305881772da762772634e122963b0e
Peter Simons <simons@cryp.to>
2014-03-30 22:07:24 +0200

    haskell-yesod-core: update to version 1.2.9.2

--- pkgs/development/libraries/haskell/yesod-core/default.nix
+++ pkgs/development/libraries/haskell/yesod-core/default.nix
@@ -1,30 +1,32 @@
 { cabal, aeson, async, attoparsecConduit, blazeBuilder, blazeHtml
 , blazeMarkup, caseInsensitive, cereal, clientsession, conduit
-, cookie, dataDefault, fastLogger, hamlet, hspec, httpTypes, HUnit
-, liftedBase, monadControl, monadLogger, network, networkConduit
-, parsec, pathPieces, QuickCheck, random, resourcet, safe
-, shakespeare, shakespeareCss, shakespeareI18n, shakespeareJs, text
-, time, transformers, transformersBase, unixCompat, vector, wai
-, waiExtra, waiLogger, waiTest, warp, yesodRoutes
+, conduitExtra, cookie, dataDefault, fastLogger, hamlet, hspec
+, httpTypes, HUnit, liftedBase, monadControl, monadLogger, network
+, networkConduit, parsec, pathPieces, QuickCheck, random, resourcet
+, safe, shakespeare, shakespeareCss, shakespeareI18n, shakespeareJs
+, streamingCommons, text, time, transformers, transformersBase
+, unixCompat, vector, wai, waiExtra, waiLogger, waiTest, warp
+, yesodRoutes
 }:
 
 cabal.mkDerivation (self: {
   pname = "yesod-core";
-  version = "1.2.9.1";
-  sha256 = "1j37jf82csdvjgb36034c0pqrmzd5r131hfqh43jp4m1wqsrib5k";
+  version = "1.2.9.2";
+  sha256 = "1ip5rra426agqv5v2hmxcp1g3xjvi9wnixrpdnxvifflhr7gchfl";
   buildDepends = [
     aeson attoparsecConduit blazeBuilder blazeHtml blazeMarkup
-    caseInsensitive cereal clientsession conduit cookie dataDefault
-    fastLogger hamlet httpTypes liftedBase monadControl monadLogger
-    parsec pathPieces random resourcet safe shakespeare shakespeareCss
-    shakespeareI18n shakespeareJs text time transformers
+    caseInsensitive cereal clientsession conduit conduitExtra cookie
+    dataDefault fastLogger hamlet httpTypes liftedBase monadControl
+    monadLogger parsec pathPieces random resourcet safe shakespeare
+    shakespeareCss shakespeareI18n shakespeareJs text time transformers
     transformersBase unixCompat vector wai waiExtra waiLogger warp
     yesodRoutes
   ];
   testDepends = [
-    async blazeBuilder conduit hamlet hspec httpTypes HUnit liftedBase
-    network networkConduit QuickCheck random resourcet shakespeareCss
-    shakespeareJs text transformers wai waiTest
+    async blazeBuilder conduit conduitExtra hamlet hspec httpTypes
+    HUnit liftedBase network networkConduit QuickCheck random resourcet
+    shakespeare shakespeareCss shakespeareJs streamingCommons text
+    transformers wai waiTest
   ];
   jailbreak = true;
   meta = {
