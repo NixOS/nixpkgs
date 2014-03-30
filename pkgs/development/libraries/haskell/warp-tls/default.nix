3e2eb89cb6bd7a5a290e1d679eb08cce15884aa0
Peter Simons <simons@cryp.to>
2014-03-30 22:07:15 +0200

    haskell-warp-tls: update to version 2.0.3.2

--- pkgs/development/libraries/haskell/warp-tls/default.nix
+++ pkgs/development/libraries/haskell/warp-tls/default.nix
@@ -1,14 +1,15 @@
-{ cabal, conduit, cprngAes, dataDefaultClass, network
-, networkConduit, tls, transformers, wai, warp
+{ cabal, conduit, conduitExtra, cprngAes, dataDefaultClass, network
+, networkConduit, resourcet, streamingCommons, tls, transformers
+, wai, warp
 }:
 
 cabal.mkDerivation (self: {
   pname = "warp-tls";
-  version = "2.0.3.1";
-  sha256 = "1cyf4syblisi5hana7h2g72yyrjln40v3b6jq2253nglqip79l5w";
+  version = "2.0.3.2";
+  sha256 = "1dqaq1z4gb3sya41hiwsxgl4f0v7sqza7aazf2vc7dd5x5izp02w";
   buildDepends = [
-    conduit cprngAes dataDefaultClass network networkConduit tls
-    transformers wai warp
+    conduit conduitExtra cprngAes dataDefaultClass network
+    networkConduit resourcet streamingCommons tls transformers wai warp
   ];
   meta = {
     homepage = "http://github.com/yesodweb/wai";
