0a150f97c8c26df1ae95c2325c6d0df0c51de4a0
Peter Simons <simons@cryp.to>
2014-03-30 22:51:46 +0200

    haskell-conduit-extra: this package doesn't contain any modules, so we cannot have documentation
    Apparently, Michael Snoyman made all his code depend on an empty package
    just in case it turns out that the extra dependency might come in useful
    in the future. Go figure.

--- pkgs/development/libraries/haskell/conduit-extra/default.nix
+++ pkgs/development/libraries/haskell/conduit-extra/default.nix
@@ -4,6 +4,7 @@ cabal.mkDerivation (self: {
   pname = "conduit-extra";
   version = "1.0.0";
   sha256 = "120c3zay8svdw3b9nqgxlrj45a1d4xf0sijkg367m7hp22szvz8a";
+  noHaddock = true;
   meta = {
     homepage = "http://github.com/snoyberg/conduit";
     description = "Temporary placeholder package";
