1149154e0e26b8acb78ce4c4f1f729d39e9ed1a9
Bjørn Forsman <bjorn.forsman@gmail.com>
2014-03-30 15:45:00 +0200

    openocd: update 0.7.0 -> 0.8.0
    * Remove unneeded --enable-<JLINK_BASED_DEBUGGER> configure flags.
      configure auto selects support for them now (and they're all enabled).
    
    * Not everything is auto-detected; I asked on the openocd mailing list
      and they suggested a set of ./configure flags for a "distro build"
      (add them).
    
    * Remove --enable-ft2232_libftdi because configure says that it's
      deprecated and we should use libftdi (which we are using when *not*
      passing --enable-ft2232_libftdi (or --enable-legacy-ft2232_libftdi as
      the option is now known as)).
    
    * Add needed pkgconfig build input.
    
    * Udev rules file has been renamed in source archive: openocd.rules =>
      99-openocd.rules.

--- pkgs/development/tools/misc/openocd/default.nix
+++ pkgs/development/tools/misc/openocd/default.nix
@@ -1,25 +1,36 @@
-{ stdenv, fetchurl, libftdi, libusb1 }:
+{ stdenv, fetchurl, libftdi, libusb1, pkgconfig }:
+
+# TODO: Add "hidapi" as dependency to gain access to CMSIS-DAP debuggers.
+# Support should be auto-detected, but if not, pass "--enable-cmsis-dap" to
+# configure.
 
 stdenv.mkDerivation rec {
   name = "openocd-${version}";
-  version = "0.7.0";
+  version = "0.8.0";
 
   src = fetchurl {
     url = "mirror://sourceforge/openocd/openocd-${version}.tar.bz2";
-    sha256 = "0qwfyd821sy5p0agz0ybgn5nd7vplipw4mhm485ldj1hcmw7n8sj";
+    sha256 = "0byk7hnccgmhw0f84qlkfhps38gp2xp628bfrsc03vq08hr6q1sv";
   };
 
-  configureFlags = [ "--enable-ft2232_libftdi"
-                     "--enable-jlink"
-                     "--enable-rlink"
-                     "--enable-ulink"
-                     "--enable-stlink" ];
+  buildInputs = [ libftdi libusb1 pkgconfig ];
 
-  buildInputs = [ libftdi libusb1 ];
+  configureFlags = [
+    "--enable-jtag_vpi"
+    "--enable-usb_blaster_libftdi"
+    "--enable-amtjtagaccel"
+    "--enable-gw16012"
+    "--enable-presto_libftdi"
+    "--enable-openjtag_ftdi"
+    "--enable-oocd_trace"
+    "--enable-buspirate"
+    "--enable-sysfsgpio"
+    "--enable-remote-bitbang"
+  ];
 
   postInstall = ''
     mkdir -p "$out/etc/udev/rules.d"
-    ln -s "$out/share/openocd/contrib/openocd.udev" "$out/etc/udev/rules.d/99-openocd.rules"
+    ln -s "$out/share/openocd/contrib/99-openocd.udev" "$out/etc/udev/rules.d/99-openocd.rules"
   '';
 
   meta = {
