4dd4f85430982eb2a775492d2bc55757be1ecb25
Shea Levy <shea@shealevy.com>
2014-03-31 02:51:14 -0400

    Revert "haskell-smtps-gmail: update to version 1.1.0"
    The smtps-gmail upgrade causes a build failure:
    
    /nix/store/8h8mravvbcm09rhybxq7hi4459lkssg8-stdenv/setup: line 937: ./Setup: No such file or directory
    
    This reverts commit a7c80c11e5cfee6fc847fa7b228e11b4a0188060.

--- pkgs/development/libraries/haskell/smtps-gmail/default.nix
+++ pkgs/development/libraries/haskell/smtps-gmail/default.nix
@@ -1,19 +1,23 @@
-{ cabal, base64Bytestring, cprngAes, dataDefault, filepath
-, mimeMail, network, stringsearch, text, tls
+{ cabal, base64String, cprngAes, network, tlsExtra, utf8String
 }:
 
 cabal.mkDerivation (self: {
   pname = "smtps-gmail";
-  version = "1.1.0";
-  sha256 = "0zr2ndpwfnb9pkv69vx94p0vpghl0khd0wbmccjpk6hlrp6fsj66";
+  version = "1.0.0";
+  sha256 = "0kv5m8rg5z1iic10av3bscdygnph1iab4b22sq3hmx6a93abqkc2";
+  isLibrary = true;
+  isExecutable = true;
   buildDepends = [
-    base64Bytestring cprngAes dataDefault filepath mimeMail network
-    stringsearch text tls
+    base64String cprngAes network tlsExtra utf8String
   ];
   meta = {
     homepage = "https://github.com/enzoh/smtps-gmail";
-    description = "Gmail SMTP Client";
+    description = "Gmail API";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
   };
+  # adding a Setup script as it's needed by nix
+  preConfigure = ''
+    printf "import Distribution.Simple\nmain = defaultMain\n" > Setup.hs
+  '';
 })
