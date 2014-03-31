65cc76c44ac81191cc0b82cef58d1ad698a24412
Jaka Hudoklin <jakahudoklin@gmail.com>
2014-04-01 01:55:32 +0200

    libjpeg_original: add optional static builds

--- pkgs/development/libraries/libjpeg/default.nix
+++ pkgs/development/libraries/libjpeg/default.nix
@@ -1,4 +1,6 @@
-{ stdenv, fetchurl }: 
+{ stdenv, fetchurl, static ? false }: 
+
+with stdenv.lib;
 
 stdenv.mkDerivation {
   name = "libjpeg-8d";
@@ -7,6 +9,8 @@ stdenv.mkDerivation {
     url = http://www.ijg.org/files/jpegsrc.v8d.tar.gz;
     sha256 = "1cz0dy05mgxqdgjf52p54yxpyy95rgl30cnazdrfmw7hfca9n0h0";
   };
+
+  configureFlags = optional static "--enable-static --disable-shared";
   
   meta = {
     homepage = http://www.ijg.org/;
