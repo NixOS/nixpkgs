7288f25bd1ecaf19182ba2a7bfa4b488303e17ad
Austin Seipp <aseipp@pobox.com>
2014-04-01 03:00:17 -0500

    kernel: stable/longterm updates
     - longterm: 3.4.83  -> 3.4.85
     - longterm: 3.10.33 -> 3.10.35
     - longterm: 3.12.14 -> 3.12.15
     - stable:   3.13.7  -> 3.13.8
    
    NOTE: This will break the testing grsec kernel at the moment (there's
    not a 3.18.8 patch yet), but it's destined to be upgraded to 3.14 soon
    anyway.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/os-specific/linux/kernel/linux-3.4.nix
+++ pkgs/os-specific/linux/kernel/linux-3.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.4.83";
+  version = "3.4.85";
   extraMeta.branch = "3.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1jammd31cgzymaqd2qwrm1shx5xwpaxmpnv658fix275fn35a8gb";
+    sha256 = "1ldbq9qka6bdgic4ydl9pnny734gyi2vxzdsnfmfi8mx7bnar29y";
   };
 
   features.iwlwifi = true;
