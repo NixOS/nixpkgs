1a29fcae69f4be51411df46477d317a829217659
Luca Bruno <luca.bruno@immobiliare.it>
2014-04-02 18:21:00 +0200

    gdm: Add very experimental display manager

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -139,6 +139,7 @@
       polipo = 129;
       mopidy = 130;
       unifi = 131;
+      gdm = 132;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -252,6 +253,7 @@
       polipo = 129;
       mopidy = 130;
       docker = 131;
+      gdm = 132;
 
       # When adding a gid, make sure it doesn't match an existing uid. And don't use gids above 399!
 
