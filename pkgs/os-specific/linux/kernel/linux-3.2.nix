e8c6c60b93f7d3d113f0ffbcc232d19b9a5bdc44
Ricardo M. Correia <rcorreia@wizy.org>
2014-04-02 04:21:42 +0200

    linux: Update to 3.2.56

--- pkgs/os-specific/linux/kernel/linux-3.2.nix
+++ pkgs/os-specific/linux/kernel/linux-3.2.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.2.55";
+  version = "3.2.56";
   extraMeta.branch = "3.2";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "15fj7kd3ba52in1siqbdq45i7xzb53yy88l9k4bgfgds3j8wxj9m";
+    sha256 = "08858sx63bsay185ywwyh01wnms7lyh9rvgwznwnzmjpnfi3hihm";
   };
 
   features.iwlwifi = true;
