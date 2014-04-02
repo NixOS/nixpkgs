53ce02b7c002a44e7c8a402f5cf824a1da2281f2
Gergely Risko <errge@nilcons.com>
2014-04-02 17:29:33 +0200

    Jailbreak the whole haskell/Chart "ecosystem", not just Chart

--- pkgs/development/libraries/haskell/Chart-gtk/default.nix
+++ pkgs/development/libraries/haskell/Chart-gtk/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "1.2";
   sha256 = "0qq72cf1m2gvcksa1jj5g9qi6b47pmpzh3grhs7kh3m7qyq0a56g";
   buildDepends = [ cairo Chart ChartCairo colour gtk mtl time ];
+  jailbreak = true;
   meta = {
     homepage = "https://github.com/timbod7/haskell-chart/wiki";
     description = "Utility functions for using the chart library with GTK";
