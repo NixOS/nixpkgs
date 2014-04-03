4bdf7f4bea20023b54a9c0f80e298edef5927cc4
Evgeny Egorochkin <phreedom@yandex.ru>
2014-04-03 07:52:32 +0300

    liboauth: update from 1.0.1 to 1.0.2

--- pkgs/development/libraries/liboauth/default.nix
+++ pkgs/development/libraries/liboauth/default.nix
@@ -2,11 +2,11 @@
 
 
 stdenv.mkDerivation rec {
-  name = "liboauth-1.0.1";
+  name = "liboauth-1.0.2";
 
   src = fetchurl {
     url = "mirror://sourceforge/liboauth/${name}.tar.gz";
-    sha256 = "12wdwq09nba8dzzcgcpbzmgcjr141ky69pm78s15hyyvw4px71sh";
+    sha256 = "1qs58yzydw20dmzvx22i541w641kwd6ja80s9na1az32n1krh6zv";
   };
 
   buildInputs = [ nss openssl ];
@@ -15,6 +15,7 @@ stdenv.mkDerivation rec {
     platforms = platforms.linux;
     description = "C library implementing the OAuth secure authentication protocol";
     homepage = http://liboauth.sourceforge.net/;
+    repositories.git = https://github.com/x42/liboauth.git;
   };
 
 }
