d89138f6ac1c7047c0ef496d8fd37f07bbab9dba
Arseniy Seroka <ars.seroka@gmail.com>
2014-04-04 17:04:32 +0400

    vacuum-graphviz: add jailbreak

--- pkgs/development/libraries/haskell/vacuum-graphviz/default.nix
+++ pkgs/development/libraries/haskell/vacuum-graphviz/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "2.1.0.1";
   sha256 = "093ba6n30a6gyifnk3bd50rkx8qldjqq9vsk92pnq152ibs36b2m";
   buildDepends = [ filepath graphviz vacuum ];
+  jailbreak = true;
   meta = {
     description = "A library for transforming vacuum graphs into GraphViz output";
     license = self.stdenv.lib.licenses.gpl3;
