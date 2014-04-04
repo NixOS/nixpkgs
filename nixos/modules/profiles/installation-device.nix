c6797b373f379f7a7f8a7da01bdf3f6751f11f2e
Shea Levy <shea@shealevy.com>
2014-04-04 17:05:57 -0400

    Turn on user-controlled wpa-cli on the livecd
    Fixes #1204

--- nixos/modules/profiles/installation-device.nix
+++ nixos/modules/profiles/installation-device.nix
@@ -45,6 +45,7 @@ with pkgs.lib;
 
     # Enable wpa_supplicant, but don't start it by default.
     networking.wireless.enable = true;
+    networking.wireless.userControlled.enable = true;
     jobs.wpa_supplicant.startOn = pkgs.lib.mkOverride 50 "";
 
     # Tell the Nix evaluator to garbage collect more aggressively.
