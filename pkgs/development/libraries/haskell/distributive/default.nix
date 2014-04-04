7ccdd41c27ecc2ec98924d1948ab07f576ae2bb8
Peter Simons <simons@cryp.to>
2014-04-04 23:29:43 +0200

    haskell-distributive: update to version 0.4.3.1

--- pkgs/development/libraries/haskell/distributive/default.nix
+++ pkgs/development/libraries/haskell/distributive/default.nix
@@ -4,13 +4,13 @@
 
 cabal.mkDerivation (self: {
   pname = "distributive";
-  version = "0.4.1";
-  sha256 = "0izsgasml3didklkk6z274fzfd1c6qnn0wlzprcz0bnd07zkh814";
+  version = "0.4.3.1";
+  sha256 = "17ny7nlxg6x08f88dyl15wsdhgi0cgafxdsl5wdw5vv6y0jsrx27";
   buildDepends = [ tagged transformers transformersCompat ];
   testDepends = [ doctest filepath ];
   meta = {
     homepage = "http://github.com/ekmett/distributive/";
-    description = "Haskell 98 Distributive functors -- Dual to Traversable";
+    description = "Distributive functors -- Dual to Traversable";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
   };
