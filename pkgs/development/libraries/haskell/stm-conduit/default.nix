7f143d564660d33b221f09f6cc4f7aa0dfa3fb50
Peter Simons <simons@cryp.to>
2014-04-04 23:30:05 +0200

    haskell-stm-conduit: update to version 2.4.0

--- pkgs/development/libraries/haskell/stm-conduit/default.nix
+++ pkgs/development/libraries/haskell/stm-conduit/default.nix
@@ -1,16 +1,17 @@
-{ cabal, async, cereal, cerealConduit, conduit, HUnit, liftedAsync
-, liftedBase, monadControl, monadLoops, QuickCheck, resourcet, stm
-, stmChans, testFramework, testFrameworkHunit
+{ cabal, async, cereal, cerealConduit, conduit, conduitExtra, HUnit
+, liftedAsync, liftedBase, monadControl, monadLoops, QuickCheck
+, resourcet, stm, stmChans, testFramework, testFrameworkHunit
 , testFrameworkQuickcheck2, transformers
 }:
 
 cabal.mkDerivation (self: {
   pname = "stm-conduit";
-  version = "2.3.0";
-  sha256 = "1qvzqzk822sc0sjblgqb5y73mggsvh70zpzz908isfiqcwp7llc4";
+  version = "2.4.0";
+  sha256 = "03ifxr3pspwmzf0xdh7mj3q1wiz13d86w9pdhiqa6b1d1qw6rvha";
   buildDepends = [
-    async cereal cerealConduit conduit liftedAsync liftedBase
-    monadControl monadLoops resourcet stm stmChans transformers
+    async cereal cerealConduit conduit conduitExtra liftedAsync
+    liftedBase monadControl monadLoops resourcet stm stmChans
+    transformers
   ];
   testDepends = [
     conduit HUnit QuickCheck resourcet stm stmChans testFramework
