481df7e6cdfb2ba8e7e4a1eb2954c242ef35c9c0
Peter Simons <simons@cryp.to>
2014-04-04 23:23:15 +0200

    haskell-hoogle: update to version 4.2.31

--- pkgs/development/libraries/haskell/hoogle/default.nix
+++ pkgs/development/libraries/haskell/hoogle/default.nix
@@ -1,19 +1,19 @@
 { cabal, aeson, binary, blazeBuilder, Cabal, caseInsensitive
 , cmdargs, conduit, deepseq, filepath, haskellSrcExts, httpTypes
-, parsec, QuickCheck, random, safe, shake, tagsoup, text, time
-, transformers, uniplate, vector, vectorAlgorithms, wai, warp
+, parsec, QuickCheck, random, resourcet, safe, shake, tagsoup, text
+, time, transformers, uniplate, vector, vectorAlgorithms, wai, warp
 }:
 
 cabal.mkDerivation (self: {
   pname = "hoogle";
-  version = "4.2.30";
-  sha256 = "0vw0chqsq8wmi1mpdxj1c9g4ah7lqxm8rwh85j2vyp56vfscw9q1";
+  version = "4.2.31";
+  sha256 = "176lyj236ncq0lg7vbl4bbm1a98ficj82aranrhyxdjzmbk3i67x";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
     aeson binary blazeBuilder Cabal caseInsensitive cmdargs conduit
     deepseq filepath haskellSrcExts httpTypes parsec QuickCheck random
-    safe shake tagsoup text time transformers uniplate vector
+    resourcet safe shake tagsoup text time transformers uniplate vector
     vectorAlgorithms wai warp
   ];
   testDepends = [ filepath ];
