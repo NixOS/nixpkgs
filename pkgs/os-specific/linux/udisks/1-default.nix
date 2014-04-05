fa6b9baea9d2364cac8dffda04cb3bf4f7f77ee7
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-04-05 18:56:45 +0200

    Revert "udisks1: bump to fix CVE-2014-0004"
    This reverts commit 0194a44d63c613065bb5c55d50470881c00563c2 because
    it breaks udisks on 13.10 (e.g. running "udisks --enumerate" will
    print "Unit udisks.service failed to load").
    
    (cherry picked from commit d7daf1a47f0d3d759555a3f0a0f09398c69c6b28)

--- pkgs/os-specific/linux/udisks/1-default.nix
+++ pkgs/os-specific/linux/udisks/1-default.nix
@@ -3,11 +3,11 @@
 , libxslt, docbook_xsl, utillinux }:
 
 stdenv.mkDerivation rec {
-  name = "udisks-1.0.5";
+  name = "udisks-1.0.4";
 
   src = fetchurl {
     url = "http://hal.freedesktop.org/releases/${name}.tar.gz";
-    sha256 = "0wbg3jrv8limdgvcygf4dqin3y6d30y9pcmmk711vq571vmq5v7j";
+    sha256 = "1xgqifddwaavmjc8c30i0mdffyirsld7c6qhfyjw7f9khwv8jjw5";
   };
 
   patches = [ ./purity.patch ./no-pci-db.patch ];
@@ -33,11 +33,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ pkgconfig ];
 
-  configureFlags = [
-    "--localstatedir=/var"
-    "--enable-lvm2"
-    "--with-systemdsystemunitdir=$(out)/etc/systemd/system"
-  ];
+  configureFlags = "--localstatedir=/var --enable-lvm2";
 
   meta = {
     homepage = http://www.freedesktop.org/wiki/Software/udisks;
