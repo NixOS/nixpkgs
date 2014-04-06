5fcc9d2b680aeaeb869bb33cca4ebd7a988fd131
Peter Simons <simons@cryp.to>
2014-04-06 20:31:19 +0200

    haskell-network-conduit: there is no code in this module to generate documentation for

--- pkgs/development/libraries/haskell/network-conduit/default.nix
+++ pkgs/development/libraries/haskell/network-conduit/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "1.1.0";
   sha256 = "06amxl8rg4zfnmgc1iyq5mxy9qihcqddqgqkbfvaf25mwr43992p";
   buildDepends = [ conduit ];
+  noHaddock = true;
   meta = {
     homepage = "http://github.com/snoyberg/conduit";
     description = "Stream socket data using conduits. (deprecated)";
