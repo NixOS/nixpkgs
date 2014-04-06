ea916f0b323d29172fb547f33b493b7974d7dc9b
Peter Simons <simons@cryp.to>
2014-04-06 18:29:36 +0200

    haskell-blaze-builder-conduit: there is no code in this module to generate documentation for

--- pkgs/development/libraries/haskell/blaze-builder-conduit/default.nix
+++ pkgs/development/libraries/haskell/blaze-builder-conduit/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "1.1.0";
   sha256 = "0xxyn3lhcn1bkybhrl5dx68d0adf26ilf34gv0mxkwpfj7m7d3k3";
   buildDepends = [ conduit ];
+  noHaddock = true;
   meta = {
     homepage = "http://github.com/snoyberg/conduit";
     description = "Convert streams of builders to streams of bytestrings. (deprecated)";
