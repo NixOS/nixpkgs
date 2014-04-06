4b57acece18be9ee5d86349500f6afe01e3da3bd
Peter Simons <simons@cryp.to>
2014-04-06 20:52:50 +0200

    haskell-http-client-conduit: there is no code in this module to generate documentation for

--- pkgs/development/libraries/haskell/http-client-conduit/default.nix
+++ pkgs/development/libraries/haskell/http-client-conduit/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.3.0";
   sha256 = "0k2vq9y7kfbkhcsszjr74ahq5nw5z7dbzjhw1ixbigcr56axsd19";
   buildDepends = [ httpClient ];
+  noHaddock = true;
   meta = {
     homepage = "https://github.com/snoyberg/http-client";
     description = "Frontend support for using http-client with conduit (deprecated)";
