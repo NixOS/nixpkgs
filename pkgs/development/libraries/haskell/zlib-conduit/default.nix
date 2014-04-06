acd673be4b1efe37ed5fa10c70d24215e2e1ae39
Peter Simons <simons@cryp.to>
2014-04-06 20:32:09 +0200

    haskell-zlib-conduit: there is no code in this module to generate documentation for

--- pkgs/development/libraries/haskell/zlib-conduit/default.nix
+++ pkgs/development/libraries/haskell/zlib-conduit/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "1.1.0";
   sha256 = "1b22mca8bbg7f84h8y0qsb5ckzg2dw1b26y27x7b7xdxqbwpz93a";
   buildDepends = [ conduit ];
+  noHaddock = true;
   meta = {
     homepage = "http://github.com/snoyberg/conduit";
     description = "Streaming compression/decompression via conduits. (deprecated)";
