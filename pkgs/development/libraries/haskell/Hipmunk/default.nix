e235f6b6081341eb36b7d7dad7c0559504c8114f
Peter Simons <simons@cryp.to>
2014-04-07 12:43:14 +0200

    haskell-Hipmunk: update to version 5.2.0.13

--- pkgs/development/libraries/haskell/Hipmunk/default.nix
+++ pkgs/development/libraries/haskell/Hipmunk/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "Hipmunk";
-  version = "5.2.0.12";
-  sha256 = "0gybmwwij6gs3gsklcvck0nc1niyh6pvirnxgrcwclrz94ivpj42";
+  version = "5.2.0.13";
+  sha256 = "0ddf7cbwaswyszq9rs5jq353npbry8l2cc7p5wq9wq97yplz10bc";
   buildDepends = [ StateVar transformers ];
   meta = {
     homepage = "https://github.com/meteficha/Hipmunk";
