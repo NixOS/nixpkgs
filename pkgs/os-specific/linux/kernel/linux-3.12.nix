05ec85105009cbd92af15e6fe8168861ba52580f
Austin Seipp <aseipp@pobox.com>
2014-04-07 13:52:01 -0500

    kernel: longterm updates
     - longterm: 3.4.85  -> 3.4.86
     - longterm: 3.10.35 -> 3.10.36
     - longterm: 3.12.15 -> 3.12.17
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.12.15";
+  version = "3.12.17";
   extraMeta.branch = "3.12";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "08y42y5piibql0v6pwdp19nxvw5jngs4nw404lwh412zk0cnh4gn";
+    sha256 = "1knjmzk5gyab0b1prmna2bb8zz19sjmmjm24h6agmpq6q4bbnc5q";
   };
 
   features.iwlwifi = true;
