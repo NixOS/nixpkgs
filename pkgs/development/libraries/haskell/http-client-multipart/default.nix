b885ecf16c0e0bbc1a10451250c471e0180c32f3
Peter Simons <simons@cryp.to>
2014-04-08 13:29:57 +0200

    haskell-http-client-multipart: update to version 0.3.0.0

--- pkgs/development/libraries/haskell/http-client-multipart/default.nix
+++ pkgs/development/libraries/haskell/http-client-multipart/default.nix
@@ -5,12 +5,11 @@ cabal.mkDerivation (self: {
   version = "0.3.0.0";
   sha256 = "18za6s3658hgm95rrygghrz0b643c7nkzaimb14v2hv82w3k9crg";
   buildDepends = [ httpClient ];
+  noHaddock = true;
   meta = {
     homepage = "https://github.com/snoyberg/http-client";
     description = "Generate multipart uploads for http-client. (deprecated)";
     license = self.stdenv.lib.licenses.mit;
     platforms = self.ghc.meta.platforms;
   };
-  # This library now contains now modules as it is deprecated.
-  noHaddock = true;
 })
