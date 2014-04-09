29c0d0047faffed8055da6a7c84c0a36743f8ce0
Austin Seipp <aseipp@pobox.com>
2014-04-08 21:15:55 -0500

    luajit: adopt, upgrade, optimize
     - Build the 'amalgamation' by default - more RAM needed to build, but
       better overall performance
     - Upgrade to version 2.0.3
     - Adopt as maintainer
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/development/interpreters/luajit/default.nix
+++ pkgs/development/interpreters/luajit/default.nix
@@ -1,28 +1,31 @@
 { stdenv, fetchurl }:
 
-stdenv.mkDerivation rec{
-    version = "2.0.2";
-    name = "LuaJIT-${version}";
+stdenv.mkDerivation rec {
+  name    = "luajit-${version}";
+  version = "2.0.3";
 
-    src = fetchurl {
-        url="http://luajit.org/download/${name}.tar.gz";
-        sha256="0f3cykihfdn3gi6na9p0xjd4jnv26z18m441n5vyg42q9abh4ln0";
-    };
+  src = fetchurl {
+    url    = "http://luajit.org/download/LuaJIT-${version}.tar.gz";
+    sha256 = "0ydxpqkmsn2c341j4r2v6r5r0ig3kbwv3i9jran3iv81s6r6rgjm";
+  };
 
-    patchPhase = stdenv.lib.optionalString (stdenv.gcc.libc != null)
-    ''
-      substituteInPlace Makefile \
-        --replace ldconfig ${stdenv.gcc.libc}/sbin/ldconfig
-    '';
+  enableParallelBuilding = true;
 
-    installPhase = ''
-        make install PREFIX=$out
-    '';
+  patchPhase = stdenv.lib.optionalString (stdenv.gcc.libc != null)
+  ''
+    substituteInPlace Makefile \
+      --replace ldconfig ${stdenv.gcc.libc}/sbin/ldconfig
+  '';
 
-    meta = {
-        description= "Just-in-time compiler and interpreter for lua 5.1.";
-        homepage = http://luajit.org;
-        license = stdenv.lib.licenses.mit;
-	platorms = stdenv.lib.platforms.linux;
-    };
+  configurePhase = false;
+  buildFlags     = [ "amalg" ]; # Build highly optimized version
+  installPhase   = "make install PREFIX=$out";
+
+  meta = {
+    description = "high-performance JIT compiler for Lua 5.1";
+    homepage    = http://luajit.org;
+    license     = stdenv.lib.licenses.mit;
+    platforms   = stdenv.lib.platforms.linux;
+    maintainers = [ stdenv.lib.maintainers.thoughtpolice ];
+  };
 }
