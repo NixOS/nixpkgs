9b2b87379c76ddb9d49f022c9e728710ea2b5c31
Peter Simons <simons@cryp.to>
2014-04-10 12:56:39 +0200

    haskell-tf-random: update to version 0.5

--- pkgs/development/libraries/haskell/tf-random/default.nix
+++ pkgs/development/libraries/haskell/tf-random/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "tf-random";
-  version = "0.4";
-  sha256 = "0yi7699zswpsn9a39ccqkyd0117sppjnqggclnhs1wm7ql1glc27";
+  version = "0.5";
+  sha256 = "0445r2nns6009fmq0xbfpyv7jpzwv0snccjdg7hwj4xk4z0cwc1f";
   buildDepends = [ primitive random time ];
   meta = {
     description = "High-quality splittable pseudorandom number generator";
