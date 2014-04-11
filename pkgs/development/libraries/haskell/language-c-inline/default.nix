716cffc1bb97afd3ad6afe02889d224be841fd24
Peter Simons <simons@cryp.to>
2014-04-11 22:28:18 +0200

    haskell-language-c-inline: re-generate with cabal2nix

--- pkgs/development/libraries/haskell/language-c-inline/default.nix
+++ pkgs/development/libraries/haskell/language-c-inline/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.3.0.1";
   sha256 = "0dw1fqwg9hhwgvak0ykhafbxp4gnb2ww9lc83m8kzkyzn1ccb6hg";
   buildDepends = [ filepath languageCQuote mainlandPretty ];
+  testDepends = [ languageCQuote ];
   doCheck = false;
   meta = {
     homepage = "https://github.com/mchakravarty/language-c-inline/";
