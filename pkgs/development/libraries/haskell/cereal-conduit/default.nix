c203d9c5fa6af4b1cbc70d214c3c78b952b513a4
Peter Simons <simons@cryp.to>
2014-04-11 10:14:26 +0200

    haskell-cereal-conduit: update to version 0.7.2.1

--- pkgs/development/libraries/haskell/cereal-conduit/default.nix
+++ pkgs/development/libraries/haskell/cereal-conduit/default.nix
@@ -2,9 +2,9 @@
 
 cabal.mkDerivation (self: {
   pname = "cereal-conduit";
-  version = "0.7.2";
-  sha256 = "03jlhpz82a7j7n0351db0h7pkxihik3fv0wgjny7i0vlq7gyqdpl";
-  buildDepends = [ cereal conduit transformers ];
+  version = "0.7.2.1";
+  sha256 = "1qjx5y8hznpanchhjfrmi0r6vmiqsj0lh9x09n563gschs8dfisw";
+  buildDepends = [ cereal conduit resourcet transformers ];
   testDepends = [ cereal conduit HUnit mtl resourcet transformers ];
   meta = {
     homepage = "https://github.com/litherum/cereal-conduit";
