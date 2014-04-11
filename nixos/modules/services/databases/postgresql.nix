e9e60103de7fa4572c28d8fd1ecf5b9ed3178fb8
Shea Levy <shea@shealevy.com>
2014-04-11 19:22:39 -0400

    Revert "Create the 'postgres' superuser"
    Reverting postgres superuser changes until after stable.
    
    This reverts commit 7de29bd26f5e514e95d5d261c08f3029337eecb3.

--- nixos/modules/services/databases/postgresql.nix
+++ nixos/modules/services/databases/postgresql.nix
@@ -197,7 +197,6 @@ in
                 fi
                 rm -f ${cfg.dataDir}/*.conf
                 touch "${cfg.dataDir}/.first_startup"
-                touch "${cfg.dataDir}/postgresql-user-created"
             fi
 
             ln -sfn "${configFile}" "${cfg.dataDir}/postgresql.conf"
@@ -230,11 +229,6 @@ in
                 if ! kill -0 "$MAINPID"; then exit 1; fi
                 sleep 0.1
             done
-
-            if ! [ -e ${cfg.dataDir}/postgresql-user-created ]; then
-              createuser --superuser postgres
-              touch ${cfg.dataDir}/postgresql-user-created
-            fi
 
             if test -e "${cfg.dataDir}/.first_startup"; then
               ${optionalString (cfg.initialScript != null) ''
