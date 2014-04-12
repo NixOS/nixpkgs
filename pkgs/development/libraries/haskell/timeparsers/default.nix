9858c26f73cdc58a62ebf1a9b9cc30e1437b1e68
John Wiegley <johnw@newartisans.com>
2014-04-12 10:15:39 -0500

    Add recipes for a few Haskell libraries

---
+++ pkgs/development/libraries/haskell/timeparsers/default.nix
@@ -0,0 +1,13 @@
+{ cabal, attoparsec, convertible, mtl, time }:
+
+cabal.mkDerivation (self: {
+  pname = "timeparsers";
+  version = "0.3.2";
+  sha256 = "1dicp58f2amn5rgmnlfjpv4aj7ak6jrdlba2marglddvj4ycq1h7";
+  buildDepends = [ attoparsec convertible mtl time ];
+  meta = {
+    description = "Attoparsec parsers for various Date/Time formats";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+  };
+})
