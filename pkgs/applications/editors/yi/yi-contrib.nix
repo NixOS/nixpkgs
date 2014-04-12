2fd160f02731745ff1d258003793649b8ee182d1
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2014-04-12 17:18:50 +0200

    haskell: update yi to 0.8.1

--- pkgs/applications/editors/yi/yi-contrib.nix
+++ pkgs/applications/editors/yi/yi-contrib.nix
@@ -1,10 +1,12 @@
-{ cabal, dataAccessor, filepath, mtl, split, yi }:
+{ cabal, filepath, lens, mtl, split, time, transformersBase, yi }:
 
 cabal.mkDerivation (self: {
   pname = "yi-contrib";
-  version = "0.7.2";
-  sha256 = "074cq1y0pp66r2fqqszd8w2pws8jbfwq9g39w3rsgjnw83058fr8";
-  buildDepends = [ dataAccessor filepath mtl split yi ];
+  version = "0.8.1";
+  sha256 = "0jsbga30x302mr708vj5y7cpc961vh85dshpq2zlrf44dh0kmpvf";
+  buildDepends = [
+    filepath lens mtl split time transformersBase yi
+  ];
   meta = {
     homepage = "http://haskell.org/haskellwiki/Yi";
     description = "Add-ons to Yi, the Haskell-Scriptable Editor";
--- pkgs/applications/editors/yi/yi.nix
+++ pkgs/applications/editors/yi/yi.nix
@@ -1,31 +1,30 @@
 { cabal, alex, binary, Cabal, cautiousFile, concreteTyperep
-, dataAccessor, dataAccessorMtl, dataAccessorTemplate, derive, Diff
-, dlist, dyre, filepath, fingertree, glib, gtk, hashable, hint
-, HUnit, mtl, pango, parsec, pointedlist, QuickCheck, random
-, regexBase, regexTdfa, split, testFramework, testFrameworkHunit
-, time, uniplate, unixCompat, unorderedContainers, utf8String, vty
+, dataDefault, derive, Diff, dlist, dyre, filepath, fingertree
+, glib, gtk, hashable, hint, HUnit, lens, mtl, pango, parsec
+, pointedlist, QuickCheck, random, regexBase, regexTdfa, safe
+, split, tasty, tastyHunit, tastyQuickcheck, time, transformersBase
+, uniplate, unixCompat, unorderedContainers, utf8String, vty
 , xdgBasedir
 }:
 
 cabal.mkDerivation (self: {
   pname = "yi";
-  version = "0.7.2";
-  sha256 = "0g0hvr4zqcrmdl6mbdmrfxd5m51fhkhslvl9piwq83g2wirxqbvm";
+  version = "0.8.1";
+  sha256 = "1hyqlydc0na9pkb3fkbp13c6vnp4f80z8237bvrv12wkk5syyn23";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
-    binary Cabal cautiousFile concreteTyperep dataAccessor
-    dataAccessorMtl dataAccessorTemplate derive Diff dlist dyre
-    filepath fingertree glib gtk hashable hint mtl pango parsec
-    pointedlist QuickCheck random regexBase regexTdfa split time
-    uniplate unixCompat unorderedContainers utf8String vty xdgBasedir
+    binary Cabal cautiousFile concreteTyperep dataDefault derive Diff
+    dlist dyre filepath fingertree glib gtk hashable hint lens mtl
+    pango parsec pointedlist QuickCheck random regexBase regexTdfa safe
+    split time transformersBase uniplate unixCompat unorderedContainers
+    utf8String vty xdgBasedir
   ];
   testDepends = [
-    filepath HUnit QuickCheck testFramework testFrameworkHunit
+    filepath HUnit QuickCheck tasty tastyHunit tastyQuickcheck
   ];
   buildTools = [ alex ];
   configureFlags = "-fpango";
-  jailbreak = true;
   doCheck = false;
   meta = {
     homepage = "http://haskell.org/haskellwiki/Yi";
