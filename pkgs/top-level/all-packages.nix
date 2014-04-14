0ca9e1012b56d8ab05fa9b1b12b8a3caa7de26f5
Alexander Kjeldaas <ak@formalprivacy.com>
2014-04-14 16:03:08 +0200

    Disable profile-guided-optimization (PGO) for gcc.
    This only disables it for compiling gcc itself.
    PGO results in non-deterministic output because the timing information itself
    has small variations.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2837,7 +2837,8 @@ let
     inherit noSysDirs;
 
     # PGO seems to speed up compilation by gcc by ~10%, see #445 discussion
-    profiledCompiler = with stdenv; (!isDarwin && (isi686 || isx86_64));
+    # however, it results in non-deterministic output
+    profiledCompiler = false;
 
     # When building `gcc.crossDrv' (a "Canadian cross", with host == target
     # and host != build), `cross' must be null but the cross-libc must still
