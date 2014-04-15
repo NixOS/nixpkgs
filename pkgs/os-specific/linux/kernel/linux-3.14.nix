ba2f861f055a46b48eb001190f67c383fed7e220
Austin Seipp <aseipp@pobox.com>
2014-04-14 19:46:35 -0500

    kernel: stable/longterm updates
     - stable:   3.14    -> 3.14.1
     - longterm: 3.10.36 -> 3.10.37
     - longterm: 3.4.86  -> 3.4.86
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/os-specific/linux/kernel/linux-3.14.nix
+++ pkgs/os-specific/linux/kernel/linux-3.14.nix
@@ -1,13 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.14";
+  version = "3.14.1";
   extraMeta.branch = "3.14";
-  modDirVersion = "3.14.0";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "61558aa490855f42b6340d1a1596be47454909629327c49a5e4e10268065dffa";
+    sha256 = "1njm8gvlj7cq0m1051yxszl4f63383a7sv1na13hkqkv36kipgqx";
   };
 
   features.iwlwifi = true;
--- pkgs/os-specific/linux/kernel/linux-3.4.nix
+++ pkgs/os-specific/linux/kernel/linux-3.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.4.86";
+  version = "3.4.87";
   extraMeta.branch = "3.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1gb0pgpikl2fvwbkzpyh38ji0i656jgz902918kvxsip263w675q";
+    sha256 = "1b7isypwjnhvdqsjnq2gsylfasjk560fq0klx18gj3kvyx6ijmqd";
   };
 
   features.iwlwifi = true;
