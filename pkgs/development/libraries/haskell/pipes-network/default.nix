0ae505179bab43426533773dd02c77ced27bdb90
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2014-04-15 15:13:12 +0200

    haskell-pipes-network: add maintainers attribute again

--- pkgs/development/libraries/haskell/pipes-network/default.nix
+++ pkgs/development/libraries/haskell/pipes-network/default.nix
@@ -13,5 +13,6 @@ cabal.mkDerivation (self: {
     description = "Use network sockets together with the pipes library";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    maintainers = [ self.stdenv.lib.maintainers.ocharles ];
   };
 })
