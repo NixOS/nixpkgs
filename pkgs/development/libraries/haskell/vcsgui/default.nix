751fb11e26957fce07384db5ba2d5030a7f98ff7
Peter Simons <simons@cryp.to>
2014-04-17 21:13:33 +0200

    haskell-vcsgui: update to version 0.0.4

--- pkgs/development/libraries/haskell/vcsgui/default.nix
+++ pkgs/development/libraries/haskell/vcsgui/default.nix
@@ -1,12 +1,12 @@
-{ cabal, filepath, gtk3, MissingH, mtl, vcswrapper }:
+{ cabal, filepath, gtk3, mtl, vcswrapper }:
 
 cabal.mkDerivation (self: {
   pname = "vcsgui";
-  version = "0.0.2";
-  sha256 = "0kj06l9s4l791ffwdnh7j0nvjvgili18g2jl2ik81n28bzfyq1dx";
+  version = "0.0.4";
+  sha256 = "12hfdhnv3xc2dyqk76lyx5ggiygyap4hm50sd6qmwfjj3f2w6b98";
   isLibrary = true;
   isExecutable = true;
-  buildDepends = [ filepath gtk3 MissingH mtl vcswrapper ];
+  buildDepends = [ filepath gtk3 mtl vcswrapper ];
   meta = {
     homepage = "https://github.com/forste/haskellVCSGUI";
     description = "GUI library for source code management systems";
