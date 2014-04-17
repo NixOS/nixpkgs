17b1fd5652b336f9c009704a366783b24cca667f
Peter Simons <simons@cryp.to>
2014-04-17 21:13:32 +0200

    haskell-uulib: update to version 0.9.16

--- pkgs/development/libraries/haskell/uulib/default.nix
+++ pkgs/development/libraries/haskell/uulib/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "uulib";
-  version = "0.9.15";
-  sha256 = "0433shh493c04qmi0sd9mfzpy198zi11gxdmcs6nz2gcnxm812zm";
+  version = "0.9.16";
+  sha256 = "06d9i712flxj62j7rdxvy9b0ximhdfvdakwpmr886l6fi3xpajl3";
   meta = {
     homepage = "http://www.cs.uu.nl/wiki/HUT/WebHome";
     description = "Haskell Utrecht Tools Library";
