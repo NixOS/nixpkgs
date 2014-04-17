b16fcdcddf24bfc5420fb8e0b7b9760931390ce7
Peter Simons <simons@cryp.to>
2014-04-17 21:12:46 +0200

    haskell-dataenc: update to version 0.14.0.6

--- pkgs/development/libraries/haskell/dataenc/default.nix
+++ pkgs/development/libraries/haskell/dataenc/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "dataenc";
-  version = "0.14.0.5";
-  sha256 = "13gajqbayar7x8sq3rw93i277gqd0bx1i34spshlj4b41fraxc8w";
+  version = "0.14.0.6";
+  sha256 = "0635aspx65wwpky0kbnlj9ly4vjw5afzvdn9glnhfxq6m6yjzp8q";
   isLibrary = true;
   isExecutable = true;
   jailbreak = true;
