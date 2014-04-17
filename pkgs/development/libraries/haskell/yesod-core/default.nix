8ca9e5975d240ef26facb08405ad996d87796ad7
Peter Simons <simons@cryp.to>
2014-04-17 21:13:39 +0200

    haskell-yesod-core: update to version 1.2.14

--- pkgs/development/libraries/haskell/yesod-core/default.nix
+++ pkgs/development/libraries/haskell/yesod-core/default.nix
@@ -1,26 +1,26 @@
 { cabal, aeson, async, attoparsecConduit, blazeBuilder, blazeHtml
 , blazeMarkup, caseInsensitive, cereal, clientsession, conduit
-, conduitExtra, cookie, dataDefault, exceptions, fastLogger, hamlet
-, hspec, httpTypes, HUnit, liftedBase, monadControl, monadLogger
-, mtl, network, networkConduit, parsec, pathPieces, QuickCheck
-, random, resourcet, safe, shakespeare, shakespeareCss
-, shakespeareI18n, shakespeareJs, streamingCommons, text, time
-, transformers, transformersBase, unixCompat, vector, wai, waiExtra
-, waiLogger, waiTest, warp, yesodRoutes
+, conduitExtra, cookie, dataDefault, deepseq, exceptions
+, fastLogger, hamlet, hspec, httpTypes, HUnit, liftedBase
+, monadControl, monadLogger, mtl, network, networkConduit, parsec
+, pathPieces, QuickCheck, random, resourcet, safe, shakespeare
+, shakespeareCss, shakespeareI18n, shakespeareJs, streamingCommons
+, text, time, transformers, transformersBase, unixCompat, vector
+, wai, waiExtra, waiLogger, waiTest, warp, yesodRoutes
 }:
 
 cabal.mkDerivation (self: {
   pname = "yesod-core";
-  version = "1.2.12";
-  sha256 = "0bgi140v2z25cfvcydzaif7id8mv341r8iclkiz0lbqxs7d29zxz";
+  version = "1.2.14";
+  sha256 = "1xffs62nhibmz5v8d3jh2dqcz5kryj1n1mv26p1rljrwjxz9i2hb";
   buildDepends = [
     aeson attoparsecConduit blazeBuilder blazeHtml blazeMarkup
     caseInsensitive cereal clientsession conduit conduitExtra cookie
-    dataDefault exceptions fastLogger hamlet httpTypes liftedBase
-    monadControl monadLogger mtl parsec pathPieces random resourcet
-    safe shakespeare shakespeareCss shakespeareI18n shakespeareJs text
-    time transformers transformersBase unixCompat vector wai waiExtra
-    waiLogger warp yesodRoutes
+    dataDefault deepseq exceptions fastLogger hamlet httpTypes
+    liftedBase monadControl monadLogger mtl parsec pathPieces random
+    resourcet safe shakespeare shakespeareCss shakespeareI18n
+    shakespeareJs text time transformers transformersBase unixCompat
+    vector wai waiExtra waiLogger warp yesodRoutes
   ];
   testDepends = [
     async blazeBuilder conduit conduitExtra hamlet hspec httpTypes
