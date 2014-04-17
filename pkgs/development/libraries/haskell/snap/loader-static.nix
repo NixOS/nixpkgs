d7e5724db1459d92942abbda9de4d14cc98d244d
Peter Simons <simons@cryp.to>
2014-04-17 21:13:24 +0200

    haskell-snap-loader-static: update to version 0.9.0.2

--- pkgs/development/libraries/haskell/snap/loader-static.nix
+++ pkgs/development/libraries/haskell/snap/loader-static.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "snap-loader-static";
-  version = "0.9.0.1";
-  sha256 = "0xlb8611r9l9ld97rs27nji7k1qvjg5i62b4al38xj6i0f2skyyi";
+  version = "0.9.0.2";
+  sha256 = "0d6s7n6yryfs2jkw0hxvhvc79fhbj256askb1c6ksqhscxxxwz1m";
   meta = {
     homepage = "http://snapframework.com/";
     description = "Snap: A Haskell Web Framework: static loader";
