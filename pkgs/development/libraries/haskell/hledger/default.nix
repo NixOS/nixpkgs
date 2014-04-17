a80ec739b565321643290e04c2002fc5f9720b02
Peter Simons <simons@cryp.to>
2014-04-17 21:13:01 +0200

    haskell-hledger: update to version 0.22.2

--- pkgs/development/libraries/haskell/hledger/default.nix
+++ pkgs/development/libraries/haskell/hledger/default.nix
@@ -1,23 +1,23 @@
-{ cabal, cmdargs, csv, dataPprint, filepath, haskeline, hledgerLib
-, HUnit, mtl, parsec, prettyShow, regexpr, safe, shakespeareText
-, split, tabular, testFramework, testFrameworkHunit, text, time
+{ cabal, cmdargs, csv, filepath, haskeline, hledgerLib, HUnit, mtl
+, parsec, prettyShow, regexpr, safe, shakespeareText, split
+, tabular, testFramework, testFrameworkHunit, text, time
 , transformers, utf8String
 }:
 
 cabal.mkDerivation (self: {
   pname = "hledger";
-  version = "0.22.1";
-  sha256 = "1mlh5dlbfkqz41i7zaa4cybf75cg5mrp6sj48kwns0liaqyvav58";
+  version = "0.22.2";
+  sha256 = "08lk3gd332k5bhvr6db4z66f2rggzbdbhcb03sc5m0dbinawmib8";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
-    cmdargs dataPprint filepath haskeline hledgerLib HUnit mtl parsec
+    cmdargs filepath haskeline hledgerLib HUnit mtl parsec prettyShow
     regexpr safe shakespeareText split tabular text time utf8String
   ];
   testDepends = [
-    cmdargs csv dataPprint filepath haskeline hledgerLib HUnit mtl
-    parsec prettyShow regexpr safe shakespeareText split tabular
-    testFramework testFrameworkHunit text time transformers
+    cmdargs csv filepath haskeline hledgerLib HUnit mtl parsec
+    prettyShow regexpr safe shakespeareText split tabular testFramework
+    testFrameworkHunit text time transformers
   ];
   meta = {
     homepage = "http://hledger.org";
