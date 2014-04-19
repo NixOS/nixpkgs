30ac332563318be4e445dc3335e803326df84d3c
Vladimír Čunát <vcunat@gmail.com>
2014-04-19 14:30:48 +0200

    xorg: minor updates of three packages

--- pkgs/servers/x11/xorg/default.nix
+++ pkgs/servers/x11/xorg/default.nix
@@ -430,11 +430,11 @@ let
   })) // {inherit bdftopcf mkfontdir ;};
 
   fontsproto = (stdenv.mkDerivation ((if overrides ? fontsproto then overrides.fontsproto else x: x) {
-    name = "fontsproto-2.1.2";
+    name = "fontsproto-2.1.3";
     builder = ./builder.sh;
     src = fetchurl {
-      url = mirror://xorg/X11R7.7/src/everything/fontsproto-2.1.2.tar.bz2;
-      sha256 = "1ab8mbqxdwvdz4k5x4xb9c4n5w7i1xw276cbpk4z7a1nlpjrg746";
+      url = mirror://xorg/individual/proto/fontsproto-2.1.3.tar.bz2;
+      sha256 = "1f2sdsd74y34nnaf4m1zlcbhyv8xb6irnisc99f84c4ivnq4d415";
     };
     buildInputs = [pkgconfig ];
   })) // {inherit ;};
@@ -1140,11 +1140,11 @@ let
   })) // {inherit libX11 libXext libXft libXmu libXrender ;};
 
   xauth = (stdenv.mkDerivation ((if overrides ? xauth then overrides.xauth else x: x) {
-    name = "xauth-1.0.8";
+    name = "xauth-1.0.9";
     builder = ./builder.sh;
     src = fetchurl {
-      url = mirror://xorg/individual/app/xauth-1.0.8.tar.bz2;
-      sha256 = "1xylgy7qrf8w76gg1sa6x8ckv14q1nv0h554ndgrss8clpknlsd8";
+      url = mirror://xorg/individual/app/xauth-1.0.9.tar.bz2;
+      sha256 = "13y2invb0894b1in03jbglximbz6v31y2kr4yjjgica8xciibkjn";
     };
     buildInputs = [pkgconfig libX11 libXau libXext libXmu xproto ];
   })) // {inherit libX11 libXau libXext libXmu xproto ;};
@@ -2000,11 +2000,11 @@ let
   })) // {inherit libX11 xproto ;};
 
   xproto = (stdenv.mkDerivation ((if overrides ? xproto then overrides.xproto else x: x) {
-    name = "xproto-7.0.25";
+    name = "xproto-7.0.26";
     builder = ./builder.sh;
     src = fetchurl {
-      url = mirror://xorg/individual/proto/xproto-7.0.25.tar.bz2;
-      sha256 = "1zh977hrfxxdyhlxr9qjfa2xl8qrb4v43a2b708kdz2gvj2p894j";
+      url = mirror://xorg/individual/proto/xproto-7.0.26.tar.bz2;
+      sha256 = "0ksi8vhfd916bx2f3xlyhn6azf6cvvzrsdja26haa1cqfp0n4qb3";
     };
     buildInputs = [pkgconfig ];
   })) // {inherit ;};
