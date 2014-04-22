27a8cada791d3de1fd1c6992221ab485d9e4a349
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-04-22 13:08:00 +0200

    openvpn: Add systemd startup notification
    This causes OpenVPN services to reach the "active" state when the VPN
    connection is up (i.e., after OpenVPN prints "Initialization Sequence
    Completed"). This allows units to be ordered correctly after openvpn-*
    units, and makes systemctl present a password prompt:
    
      $ start openvpn-foo
      Enter Private Key Password: *************
    
    (I first tried to implement this by calling "systemd-notify --ready"
    from the "up" script, but systemd-notify is not reliable.)

--- pkgs/tools/networking/openvpn/default.nix
+++ pkgs/tools/networking/openvpn/default.nix
@@ -1,4 +1,6 @@
-{ stdenv, fetchurl, iproute, lzo, openssl, pam }:
+{ stdenv, fetchurl, iproute, lzo, openssl, pam, systemd }:
+
+with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "openvpn-2.3.3";
@@ -8,7 +10,9 @@ stdenv.mkDerivation rec {
     sha256 = "04xiwim56sb1vis93k9hhm1s29jdrlq7i2fa07jncnhh653d29gh";
   };
 
-  buildInputs = [ iproute lzo openssl pam ];
+  patches = optional stdenv.isLinux ./systemd-notify.patch;
+
+  buildInputs = [ iproute lzo openssl pam ] ++ optional stdenv.isLinux systemd;
 
   configureFlags = ''
     --enable-password-save
@@ -30,6 +34,8 @@ stdenv.mkDerivation rec {
   '';
 
   enableParallelBuilding = true;
+
+  NIX_LDFLAGS = optionalString stdenv.isLinux "-lsystemd-daemon"; # hacky
 
   meta = {
     description = "A robust and highly flexible tunneling application";
