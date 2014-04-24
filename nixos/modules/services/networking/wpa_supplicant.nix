109a2bee28f23c4a8d0a1db664e27043242da2ca
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-04-24 18:16:12 +0200

    Remove outdated remark
    (cherry picked from commit 2c70276d96fa911ecfcadbbd25837392d53fd81f)

--- nixos/modules/services/networking/wpa_supplicant.nix
+++ nixos/modules/services/networking/wpa_supplicant.nix
@@ -46,9 +46,7 @@ in
         example = [ "wlan0" "wlan1" ];
         description = ''
           The interfaces <command>wpa_supplicant</command> will use.  If empty, it will
-          automatically use all wireless interfaces. (Note that auto-detection is currently
-          broken on Linux 3.4.x kernels. See http://github.com/NixOS/nixos/issues/10 for
-          further details.)
+          automatically use all wireless interfaces.
         '';
       };
 
