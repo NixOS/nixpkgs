4001f85b0c7eb9fc0b21c52ea67f55fcb2bf22c0
Vladimír Čunát <vcunat@gmail.com>
2014-04-24 20:01:17 +0200

    linux-3.12: bump .17 -> .18
    (cherry picked from commit 116d52c6dfecb96860fc9f6f5e12a052df38e074)

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.12.17";
+  version = "3.12.18";
   extraMeta.branch = "3.12";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1knjmzk5gyab0b1prmna2bb8zz19sjmmjm24h6agmpq6q4bbnc5q";
+    sha256 = "0w0nwkhrbxa9yqnj6mvckkxdqn291mw4pxa309alcga3wn29mwis";
   };
 
   features.iwlwifi = true;
