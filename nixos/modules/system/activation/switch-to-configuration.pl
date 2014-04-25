4df1e5b0e4c089c226d3a2cfe1bc99e8a13d0da1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-04-25 17:42:09 +0200

    switch-to-configuration: Use systemctl's --no-legend flag
    (cherry picked from commit 37e6e08cdee082d9f60ab2900c960f2f594933a0)

--- nixos/modules/system/activation/switch-to-configuration.pl
+++ nixos/modules/system/activation/switch-to-configuration.pl
@@ -62,7 +62,7 @@ $SIG{PIPE} = "IGNORE";
 sub getActiveUnits {
     # FIXME: use D-Bus or whatever to query this, since parsing the
     # output of list-units is likely to break.
-    my $lines = `@systemd@/bin/systemctl list-units --full`;
+    my $lines = `LANG= @systemd@/bin/systemctl list-units --full --no-legend`;
     my $res = {};
     foreach my $line (split '\n', $lines) {
         chomp $line;
