ae1189379ef29e5a1c1d473c520be5146233014a
Linquize <linquize@yahoo.com.hk>
2014-04-25 18:21:58 +0200

    cpuminer: Upgrade to 2.3.3

--- pkgs/tools/misc/cpuminer/default.nix
+++ pkgs/tools/misc/cpuminer/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "cpuminer-${version}";
-  version = "2.3.2";
+  version = "2.3.3";
 
   src = fetchurl {
     url = "mirror://sourceforge/cpuminer/pooler-${name}.tar.gz";
-    sha256 = "10xnb58w90kq8xgnkk0z99c0b9p9pxhkhkcs9dml5pgxfrlakckg";
+    sha256 = "17pjnb1nb8c1akr07hgac2pqd5fcv34f12g7iykbn9hgig5r1sxc";
   };
 
   buildInputs = [ curl jansson ];
