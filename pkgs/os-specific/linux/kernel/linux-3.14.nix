a0b81c40a29c33b3a21f6cb63a6e6a1b8b190c3a
Austin Seipp <aseipp@pobox.com>
2014-04-27 08:41:40 -0500

    kernel/grsecurity: stable/longterm/testing updates
    kernels:
    
      - longterm: 3.4.87  -> 3.4.88
      - longterm: 3.10.37 -> 3.10.38
      - stable:   3.13.10 -> 3.13.11
      - stable:   3.14.1  -> 3.14.2
    
    grsecurity:
    
      - test: 3.0-3.14.1-201404241722 -> 3.0-3.14.2-201404270907
    
    NOTE: technically the 3.13 stable kernel is now EOL. However, it will
    become the long-term grsecurity stable kernel, and will have ongoing
    support from Canonical.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>
    (cherry picked from commit 92f7781f00f4fed209143b10b0e33ec3470af342)

--- pkgs/os-specific/linux/kernel/linux-3.14.nix
+++ pkgs/os-specific/linux/kernel/linux-3.14.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.14.1";
+  version = "3.14.2";
   extraMeta.branch = "3.14";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1njm8gvlj7cq0m1051yxszl4f63383a7sv1na13hkqkv36kipgqx";
+    sha256 = "0grf6hksx4gidqim05blsnvk5037y852smr8mn0hcnbwvvp1hxal";
   };
 
   features.iwlwifi = true;
--- pkgs/os-specific/linux/kernel/linux-3.4.nix
+++ pkgs/os-specific/linux/kernel/linux-3.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.4.87";
+  version = "3.4.88";
   extraMeta.branch = "3.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1b7isypwjnhvdqsjnq2gsylfasjk560fq0klx18gj3kvyx6ijmqd";
+    sha256 = "0l2ccrsr7ga6rhjb58vq7xxhym7n90m7n181hwpi707wikhz80rd";
   };
 
   features.iwlwifi = true;
