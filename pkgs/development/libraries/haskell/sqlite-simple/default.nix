4cc210e79370a94e8ff9c4eb851d3aa85d06f827
Peter Simons <simons@cryp.to>
2014-04-28 19:18:05 +0200

    haskell-sqlite-simple: update to version 0.4.6.1

--- pkgs/development/libraries/haskell/sqlite-simple/default.nix
+++ pkgs/development/libraries/haskell/sqlite-simple/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "sqlite-simple";
-  version = "0.4.5.2";
-  sha256 = "04080ak589n0abisb6bzsmycrh3l8sh0ipcl1gcsvvfd4x83c9yi";
+  version = "0.4.6.1";
+  sha256 = "0xmrqdwnkbfr4l610qs2fs2l00dyc00v9cphiwqfrqprljg6qb4y";
   buildDepends = [
     attoparsec blazeBuilder blazeTextual directSqlite text time
     transformers
