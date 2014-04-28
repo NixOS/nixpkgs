5b3a2abaa04f0aba3e42ae2c2607811220d0c353
Austin Seipp <aseipp@pobox.com>
2014-04-28 17:32:58 -0500

    lockdep: 3.14 -> 3.14.2
    Signed-off-by: Austin Seipp <aseipp@pobox.com>
    (cherry picked from commit 7faaa9e6daa79f40ae3725e97c463746d079f3ae)

--- pkgs/os-specific/linux/lockdep/default.nix
+++ pkgs/os-specific/linux/lockdep/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name    = "lockdep-${version}";
-  version = "3.14";
+  version = "3.14.2";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "61558aa490855f42b6340d1a1596be47454909629327c49a5e4e10268065dffa";
+    sha256 = "0grf6hksx4gidqim05blsnvk5037y852smr8mn0hcnbwvvp1hxal";
   };
 
   preConfigure = "cd tools/lib/lockdep";
