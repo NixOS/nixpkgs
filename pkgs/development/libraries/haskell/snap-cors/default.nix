8ebb8079850458c037544c9cb5a299b6ac68542a
Peter Simons <simons@cryp.to>
2014-04-28 19:18:03 +0200

    haskell-snap-cors: update to version 1.2.3

--- pkgs/development/libraries/haskell/snap-cors/default.nix
+++ pkgs/development/libraries/haskell/snap-cors/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "snap-cors";
-  version = "1.2.2";
-  sha256 = "1f32sj7y87lr0wjs3j3ynh95c4j4yx4fzizbgdfnjai1apcjkhcs";
+  version = "1.2.3";
+  sha256 = "0frm1jghm5n1pivcdx45cf7nymny25ijfslg52cm75m55d4l2lvv";
   buildDepends = [
     attoparsec caseInsensitive hashable network snap text transformers
     unorderedContainers
