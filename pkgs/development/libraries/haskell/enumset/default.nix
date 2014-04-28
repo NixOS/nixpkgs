9eff988bbf897541256590f9f79daf9f68f1e805
Peter Simons <simons@cryp.to>
2014-04-28 18:49:18 +0200

    haskell-enumset: re-generate with cabal2nix

--- pkgs/development/libraries/haskell/enumset/default.nix
+++ pkgs/development/libraries/haskell/enumset/default.nix
@@ -4,20 +4,11 @@ cabal.mkDerivation (self: {
   pname = "enumset";
   version = "0.0.4";
   sha256 = "1dzwxi7i757zdf68v470n8dwn1g8kg51w3c1mwqyxwq85650805w";
-
-  isExecutable = false;
-  isLibrary = true;
-
-  buildDepends = [
-    dataAccessor
-    storableRecord
-  ];
-
+  buildDepends = [ dataAccessor storableRecord ];
   meta = {
     description = "Sets of enumeration values represented by machine words";
-    license     = self.stdenv.lib.licenses.bsd3;
-    platforms   = self.ghc.meta.platforms;
-    maintainers = with self.stdenv.lib.maintainers; [ertes];
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+    maintainers = [ self.stdenv.lib.maintainers.ertes ];
   };
-
 })
