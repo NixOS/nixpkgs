045b7d77a1846fe6358981b717370a443830b395
Peter Simons <simons@cryp.to>
2014-04-28 19:18:14 +0200

    haskell-warp-tls: update to version 2.0.5

--- pkgs/development/libraries/haskell/warp-tls/default.nix
+++ pkgs/development/libraries/haskell/warp-tls/default.nix
@@ -5,8 +5,8 @@
 
 cabal.mkDerivation (self: {
   pname = "warp-tls";
-  version = "2.0.3.3";
-  sha256 = "03fjghsa9zvrvg7ickph577zzr62n91gsb99v9k47s4nd2xri2rj";
+  version = "2.0.5";
+  sha256 = "11nc5drys75mjfqww87rs2clhxpx485q008y42f2ymj7s5856db4";
   buildDepends = [
     conduit conduitExtra cprngAes dataDefaultClass network
     networkConduit resourcet streamingCommons tls transformers wai warp
