5ac35c0f3b019f6d9b871309ebe89046044b35b0
Peter Simons <simons@cryp.to>
2014-04-28 18:54:22 +0200

    haskell-tz: fix build (of the test suite)

--- pkgs/development/libraries/haskell/tz/default.nix
+++ pkgs/development/libraries/haskell/tz/default.nix
@@ -1,7 +1,6 @@
-{ cabal, binary, bindingsPosix, deepseq, HUnit, QuickCheck
-, testFramework, testFrameworkHunit, testFrameworkQuickcheck2
-, testFrameworkTh, time, tzdata, vector
-, pkgs_tzdata
+{ cabal, binary, bindingsPosix, deepseq, HUnit, pkgs_tzdata
+, QuickCheck, testFramework, testFrameworkHunit
+, testFrameworkQuickcheck2, testFrameworkTh, time, tzdata, vector
 }:
 
 cabal.mkDerivation (self: {
@@ -9,11 +8,11 @@ cabal.mkDerivation (self: {
   version = "0.0.0.5";
   sha256 = "03s5vs08dj3r7rq78ncya6x6dazvr93gfylyynwybpai09l2y89v";
   buildDepends = [ binary deepseq time tzdata vector ];
-  preCheck = "export TZDIR=${pkgs_tzdata}/share/zoneinfo";
   testDepends = [
     bindingsPosix HUnit QuickCheck testFramework testFrameworkHunit
     testFrameworkQuickcheck2 testFrameworkTh time tzdata
   ];
+  preConfigure = "export TZDIR=${pkgs_tzdata}/share/zoneinfo";
   meta = {
     homepage = "https://github.com/nilcons/haskell-tz";
     description = "Efficient time zone handling";
