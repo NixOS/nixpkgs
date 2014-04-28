0823af1708b8dfb4cf3f88b822aa7e814f1d61ce
Peter Simons <simons@cryp.to>
2014-04-28 19:17:49 +0200

    haskell-pointedlist: update to version 0.6.1

--- pkgs/development/libraries/haskell/pointedlist/default.nix
+++ pkgs/development/libraries/haskell/pointedlist/default.nix
@@ -1,10 +1,10 @@
-{ cabal, binary, derive }:
+{ cabal, binary }:
 
 cabal.mkDerivation (self: {
   pname = "pointedlist";
-  version = "0.6";
-  sha256 = "16sfw77w46f7rjd1lpdfzi1bdgf81siy2sj71xqkqbsz6cvkjakg";
-  buildDepends = [ binary derive ];
+  version = "0.6.1";
+  sha256 = "16xsrzqql7i4z6a3xy07sqnbyqdmcar1jiacla58y4mvkkwb0g3l";
+  buildDepends = [ binary ];
   meta = {
     description = "A zipper-like comonad which works as a list, tracking a position";
     license = self.stdenv.lib.licenses.bsd3;
