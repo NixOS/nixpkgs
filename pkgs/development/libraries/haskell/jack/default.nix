11fc857e30104949ef4b41fb6e8d703ab2a3ec50
Peter Simons <simons@cryp.to>
2014-04-28 18:57:09 +0200

    haskell-jack: re-generate with cabal2nix

--- pkgs/development/libraries/haskell/jack/default.nix
+++ pkgs/development/libraries/haskell/jack/default.nix
@@ -1,30 +1,22 @@
-{ cabal, enumset, eventList, explicitException, jackaudio, midi,
-nonNegative, transformers }:
+{ cabal, enumset, eventList, explicitException, jackaudio, midi
+, nonNegative, transformers
+}:
 
 cabal.mkDerivation (self: {
   pname = "jack";
   version = "0.7.0.2";
   sha256 = "06mf1sw1lp81b3d4hsgc199m30drdnxzzlmsxg4p1yvydjfdk4gj";
-
-  isExecutable = false;
   isLibrary = true;
-
-  pkgconfigDepends = [jackaudio];
-
+  isExecutable = true;
   buildDepends = [
-    enumset
-    eventList
-    explicitException
-    midi
-    nonNegative
-    transformers
+    enumset eventList explicitException midi nonNegative transformers
   ];
-
+  pkgconfigDepends = [ jackaudio ];
   meta = {
+    homepage = "http://www.haskell.org/haskellwiki/JACK";
     description = "Bindings for the JACK Audio Connection Kit";
-    homepage    = "http://www.haskell.org/haskellwiki/JACK";
-    license     = self.stdenv.lib.licenses.gpl2Plus;
-    platforms   = self.ghc.meta.platforms;
-    maintainers = with self.stdenv.lib.maintainers; [ertes];
+    license = "GPL";
+    platforms = self.ghc.meta.platforms;
+    maintainers = [ self.stdenv.lib.maintainers.ertes ];
   };
 })
