a1952b5e13fea9de119aace8e6b2bde47673c456
Peter Simons <simons@cryp.to>
2014-05-01 12:19:52 +0200

    haskell-hledger-web: update to version 0.22.8

--- pkgs/development/libraries/haskell/hledger-web/default.nix
+++ pkgs/development/libraries/haskell/hledger-web/default.nix
@@ -1,22 +1,23 @@
 { cabal, blazeHtml, blazeMarkup, clientsession, cmdargs
 , conduitExtra, dataDefault, filepath, hamlet, hjsmin, hledger
 , hledgerLib, hspec, httpClient, httpConduit, HUnit, json
-, networkConduit, parsec, regexpr, safe, shakespeareText, text
-, time, transformers, wai, waiExtra, waiHandlerLaunch, warp, yaml
-, yesod, yesodCore, yesodStatic, yesodTest
+, networkConduit, parsec, regexpr, safe, shakespeare
+, shakespeareText, text, time, transformers, wai, waiExtra
+, waiHandlerLaunch, warp, yaml, yesod, yesodCore, yesodStatic
+, yesodTest
 }:
 
 cabal.mkDerivation (self: {
   pname = "hledger-web";
-  version = "0.22.7";
-  sha256 = "0dxlprmymspjwxdr9sig483y8azd7z42sv6k6r35m74z7naga68y";
+  version = "0.22.8";
+  sha256 = "0588r4hgy7800jla5jma96a7pmm64b116w6ffid9v5j52qjxyhf3";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
     blazeHtml blazeMarkup clientsession cmdargs conduitExtra
     dataDefault filepath hamlet hjsmin hledger hledgerLib httpClient
     httpConduit HUnit json networkConduit parsec regexpr safe
-    shakespeareText text time transformers wai waiExtra
+    shakespeare shakespeareText text time transformers wai waiExtra
     waiHandlerLaunch warp yaml yesod yesodCore yesodStatic
   ];
   testDepends = [ hspec yesod yesodTest ];
