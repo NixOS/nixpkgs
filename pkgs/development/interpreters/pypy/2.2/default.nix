b89c5378d0be1b74fa4b23f349c8e445c9550fbd
Domen Kožar <domen@dev.si>
2014-05-01 18:36:48 +0200

    pypy: disable sqlite3 tests (failing due to sqlite bump)

--- pkgs/development/interpreters/pypy/2.2/default.nix
+++ pkgs/development/interpreters/pypy/2.2/default.nix
@@ -59,7 +59,8 @@ let
        # disable socket because it has two actual network tests that fail
        # disable test_mhlib because it fails for unknown reason
        # disable test_multiprocessing due to transient errors
-      ./pypy-c ./pypy/test_all.py --pypy=./pypy-c -k '-test_socket -test_shutil -test_mhlib -test_multiprocessing' lib-python
+       # disable sqlite3 due to https://bugs.pypy.org/issue1740
+      ./pypy-c ./pypy/test_all.py --pypy=./pypy-c -k '-test_sqlite -test_socket -test_shutil -test_mhlib -test_multiprocessing' lib-python
     '';
 
     installPhase = ''
