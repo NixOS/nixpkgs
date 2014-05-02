ab7e2f1dc52ea8e4c33434e8d29dd99d02414551
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-05-02 03:42:56 +0200

    haskell-HTTP: update to version 4000.2.13

--- pkgs/development/libraries/haskell/HTTP/4000.2.13.nix
+++ pkgs/development/libraries/haskell/HTTP/4000.2.13.nix
@@ -5,15 +5,13 @@
 
 cabal.mkDerivation (self: {
   pname = "HTTP";
-  version = "4000.2.12";
-  sha256 = "0zaynz8s70xzqxxrr8yjxwc7qspd4q2ig1vqjz1p4qqm72rs3jwl";
+  version = "4000.2.13";
+  sha256 = "1lff45nad6j2bc6waav1z95na0bif2figxiw86g2w741p7idxyy4";
   buildDepends = [ mtl network parsec ];
   testDepends = [
     caseInsensitive conduit deepseq httpdShed httpTypes HUnit mtl
     network pureMD5 split testFramework testFrameworkHunit wai warp
   ];
-  jailbreak = true;
-  doCheck = false;
   meta = {
     homepage = "https://github.com/haskell/HTTP";
     description = "A library for client-side HTTP";
