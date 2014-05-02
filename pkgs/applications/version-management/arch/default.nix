6548fbfb281c31658d22aaa0439287a77c9bf658
Michael Raskin <7c6f434c@mail.ru>
2014-05-03 00:19:55 +0400

    Use the working source for GNU ARch

--- pkgs/applications/version-management/arch/default.nix
+++ pkgs/applications/version-management/arch/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "tla-1.3.5";
   
   src = fetchurl {
-    url = "mirror://gnu/gnu-arch/" + name + ".tar.gz";
+    url = "ftp://ftp.gnu.org/old-gnu/gnu-arch/" + name + ".tar.gz";
     sha256 = "01mfzj1i6p4s8191cgd5850hds1zls88hkf9rb6qx1vqjv585aj0";
   };
 
