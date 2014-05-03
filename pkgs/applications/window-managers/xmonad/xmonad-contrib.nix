f9f19ee938117b9421bab65b277f3353ee549a60
Peter Simons <simons@cryp.to>
2014-05-03 11:44:29 +0200

    haskell-xmonad-contrib: update to version 0.11.3

--- pkgs/applications/window-managers/xmonad/xmonad-contrib.nix
+++ pkgs/applications/window-managers/xmonad/xmonad-contrib.nix
@@ -4,14 +4,11 @@
 
 cabal.mkDerivation (self: {
   pname = "xmonad-contrib";
-  version = "0.11.2";
-  sha256 = "0qlc732m6mhvx7g10r69hk5x460kjv2r04s91cnn5yfiia1qfpai";
+  version = "0.11.3";
+  sha256 = "14h9vr33yljymswj50wbimav263y9abdcgi07mvfis0zd08rxqxa";
   buildDepends = [
     extensibleExceptions mtl random utf8String X11 X11Xft xmonad
   ];
-  patchPhase = self.stdenv.lib.optional (self.stdenv.lib.versionOlder "7.8" self.ghc.version) ''
-    sed -i -e 's|ForeignFunctionInterface|AllowAmbiguousTypes, ForeignFunctionInterface|' xmonad-contrib.cabal
-  '';
   meta = {
     homepage = "http://xmonad.org/";
     description = "Third party extensions for xmonad";
