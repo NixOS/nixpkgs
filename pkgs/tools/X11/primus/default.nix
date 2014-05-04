7cb8c5c20a356889e510709778663ff9b9b7f1d9
Corey O'Connor <coreyoconnor@gmail.com>
2014-05-04 00:28:38 -0700

    use PRIMUS_UPLOAD to work around intel bug

--- pkgs/tools/X11/primus/default.nix
+++ pkgs/tools/X11/primus/default.nix
@@ -10,6 +10,10 @@ let
   primusrun = writeScript "primusrun"
 ''
   export LD_LIBRARY_PATH=${ld_path}
+  # see: https://github.com/amonakov/primus/issues/138
+  # I think the intel driver barfs when the pixel buffers try to read from the source memory
+  # directly. This causes an indirection through textures which appears to avoid issues.
+  export PRIMUS_UPLOAD=1
   exec "$@"
 '';
 in
