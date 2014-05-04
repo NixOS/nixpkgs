59b5aa05f9c05d33ff0a11918b291728744191a8
Peter Simons <simons@cryp.to>
2014-05-04 22:59:31 +0200

    haskell-persistent-postgresql: update to version 1.3.1.1

--- pkgs/development/libraries/haskell/persistent-postgresql/default.nix
+++ pkgs/development/libraries/haskell/persistent-postgresql/default.nix
@@ -5,8 +5,8 @@
 
 cabal.mkDerivation (self: {
   pname = "persistent-postgresql";
-  version = "1.3.1";
-  sha256 = "19wmz9xd1jdmysr7zhlxf6z9v7pfgy0d3g7szifk8wjq8ai57lny";
+  version = "1.3.1.1";
+  sha256 = "1qi19fm7waxrbh795jvcny7aaj6b64jqcwv772xjzl9dbv3q9qhc";
   buildDepends = [
     aeson blazeBuilder conduit monadControl persistent postgresqlLibpq
     postgresqlSimple resourcet text time transformers
