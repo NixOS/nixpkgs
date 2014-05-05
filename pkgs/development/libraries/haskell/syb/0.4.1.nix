efaac7c00639e4fd91ea2e86524c9927c4e27ab7
Peter Simons <simons@cryp.to>
2014-05-05 23:08:13 +0200

    haskell-syb-0.4.1: disable the test suite when compiling with GHC HEAD
    The test suite fails in a seemingly harmless way:
    
      ### Failure in: All:0
      expected: "(MyDataType Int,(DataType {tycon = \"MyDataType\", datarep = AlgRep [MyDataType]},(\"\",(\"MyDataType\",(\"\",\"MyDataType\")))))"
       but got: "(MyDataType Int,(DataType {tycon = \"Datatype.MyDataType\", datarep = AlgRep [MyDataType]},(\"\",(\"MyDataType\",(\"Datatype\",\"MyDataType\")))))"

--- pkgs/development/libraries/haskell/syb/0.4.1.nix
+++ pkgs/development/libraries/haskell/syb/0.4.1.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.4.1";
   sha256 = "1lkh4rrqdzvb8kyry07x2z88v478hrw5cp8wmhjgpg0ck8ywncma";
   testDepends = [ HUnit mtl ];
+  doCheck = self.stdenv.lib.versionOlder self.ghc.version "7.9";
   meta = {
     homepage = "http://www.cs.uu.nl/wiki/GenericProgramming/SYB";
     description = "Scrap Your Boilerplate";
