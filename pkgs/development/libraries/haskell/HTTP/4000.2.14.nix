6f5ee37d5c389b513df4a8c7a5af87faf6ca10b9
Peter Simons <simons@cryp.to>
2014-05-05 22:55:57 +0200

    haskell-HTTP: update to version 4000.2.14

--- pkgs/development/libraries/haskell/HTTP/4000.2.14.nix
+++ pkgs/development/libraries/haskell/HTTP/4000.2.14.nix
@@ -5,8 +5,8 @@
 
 cabal.mkDerivation (self: {
   pname = "HTTP";
-  version = "4000.2.13";
-  sha256 = "1lff45nad6j2bc6waav1z95na0bif2figxiw86g2w741p7idxyy4";
+  version = "4000.2.14";
+  sha256 = "1js5r3rpy5wx0h29qsd3sfazqj0svfdcv13nsvwqi9jnwik8041c";
   buildDepends = [ mtl network parsec ];
   testDepends = [
     caseInsensitive conduit deepseq httpdShed httpTypes HUnit mtl
@@ -14,14 +14,10 @@ cabal.mkDerivation (self: {
   ];
   jailbreak = true;
   doCheck = false;
-  preConfigure = self.stdenv.lib.optional (self.stdenv.lib.versionOlder "7.9" self.ghc.version) ''
-    sed -i -e 's|Extensions: |Extensions: FlexibleContexts, |' HTTP.cabal
-  '';
   meta = {
     homepage = "https://github.com/haskell/HTTP";
     description = "A library for client-side HTTP";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
-    maintainers = [ self.stdenv.lib.maintainers.andres ];
   };
 })
