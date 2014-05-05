4a08f3720635b7bb3b46dea80bba491302b16f68
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-05-05 15:54:38 +0200

    Don't start getty@tty1 on headless machines (like EC2)
    Backport: 14.04

--- nixos/modules/profiles/headless.nix
+++ nixos/modules/profiles/headless.nix
@@ -12,6 +12,8 @@ with lib;
   # Don't start a tty on the serial consoles.
   systemd.services."serial-getty@ttyS0".enable = false;
   systemd.services."serial-getty@hvc0".enable = false;
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@".enable = false;
 
   # Since we can't manually respond to a panic, just reboot.
   boot.kernelParams = [ "panic=1" "boot.panic_on_fail" ];
