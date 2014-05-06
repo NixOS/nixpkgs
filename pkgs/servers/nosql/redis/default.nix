11f1d0e377fbc951fa34b729400d1d190fc40299
Dario Bertini <berdario@gmail.com>
2014-05-06 17:05:08 +0200

    redis: update to 2.8.9 (close #2541)

--- pkgs/servers/nosql/redis/default.nix
+++ pkgs/servers/nosql/redis/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "redis-2.6.13";
+  name = "redis-2.8.9";
 
   src = fetchurl {
-    url = "http://redis.googlecode.com/files/${name}.tar.gz";
-    sha256 = "0j79a5vmdy0c1df89ymqk37kz8q2iqlzg81qwnz0djjqdiikk51v";
+    url = "http://download.redis.io/releases/${name}.tar.gz";
+    sha256 = "7834c37f2ff186c46aef8e4a066dfbf1d6772a285aa31c19c58162f264f1007f";
   };
 
   makeFlags = "PREFIX=$(out)";
