bb6695798767e6055e3f31f1db00a3e09131df5f
Peter Simons <simons@cryp.to>
2014-05-07 11:48:03 +0200

    haskell-shelly: update to version 1.5.3

--- pkgs/development/libraries/haskell/shelly/default.nix
+++ pkgs/development/libraries/haskell/shelly/default.nix
@@ -1,13 +1,16 @@
-{ cabal, async, mtl, systemFileio, systemFilepath, text, time
-, unixCompat
+{ cabal, async, enclosedExceptions, exceptions, liftedAsync
+, liftedBase, monadControl, mtl, systemFileio, systemFilepath, text
+, time, transformers, transformersBase, unixCompat
 }:
 
 cabal.mkDerivation (self: {
   pname = "shelly";
-  version = "1.5.2";
-  sha256 = "1iyn3xxmqbrx6xfay364m2qgflscmz9crr051jpzg0b629b8wssa";
+  version = "1.5.3";
+  sha256 = "1s4j2jp0nflv1nn29f4x2j1vfjf2mimjz0ljy4hg9hn7jp028s8h";
   buildDepends = [
-    async mtl systemFileio systemFilepath text time unixCompat
+    async enclosedExceptions exceptions liftedAsync liftedBase
+    monadControl mtl systemFileio systemFilepath text time transformers
+    transformersBase unixCompat
   ];
   meta = {
     homepage = "https://github.com/yesodweb/Shelly.hs";
