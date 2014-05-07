06d91b1f0ef5d5f0f4cedbd58565b63920ee677b
Peter Simons <simons@cryp.to>
2014-05-07 11:48:03 +0200

    haskell-yesod-static: update to version 1.2.3

--- pkgs/development/libraries/haskell/yesod-static/default.nix
+++ pkgs/development/libraries/haskell/yesod-static/default.nix
@@ -1,21 +1,22 @@
-{ cabal, async, base64Bytestring, byteable, conduit, conduitExtra
-, cryptohash, cryptohashConduit, dataDefault, fileEmbed, filepath
-, hjsmin, hspec, httpTypes, HUnit, mimeTypes, resourcet
-, shakespeareCss, systemFileio, systemFilepath, text, transformers
-, unixCompat, unorderedContainers, wai, waiAppStatic, waiTest
-, yesodCore, yesodTest
+{ cabal, async, attoparsec, base64Bytestring, blazeBuilder
+, byteable, conduit, conduitExtra, cryptohash, cryptohashConduit
+, cssText, dataDefault, fileEmbed, filepath, hashable, hjsmin
+, hspec, httpTypes, HUnit, mimeTypes, resourcet, shakespeareCss
+, systemFileio, systemFilepath, text, transformers, unixCompat
+, unorderedContainers, wai, waiAppStatic, waiTest, yesodCore
+, yesodTest
 }:
 
 cabal.mkDerivation (self: {
   pname = "yesod-static";
-  version = "1.2.2.5";
-  sha256 = "0n084dnvrswfqsvy5kdiq4ajfna2hwyzbb9sn2lj4s8jyiw1fs61";
+  version = "1.2.3";
+  sha256 = "093lvg7pl71dfnr7qyfmp9r1m3bs64849k0dw4w2qb618y8wb9jh";
   buildDepends = [
-    async base64Bytestring byteable conduit conduitExtra cryptohash
-    cryptohashConduit dataDefault fileEmbed filepath hjsmin httpTypes
-    mimeTypes resourcet shakespeareCss systemFileio systemFilepath text
-    transformers unixCompat unorderedContainers wai waiAppStatic
-    yesodCore
+    async attoparsec base64Bytestring blazeBuilder byteable conduit
+    conduitExtra cryptohash cryptohashConduit cssText dataDefault
+    fileEmbed filepath hashable hjsmin httpTypes mimeTypes resourcet
+    shakespeareCss systemFileio systemFilepath text transformers
+    unixCompat unorderedContainers wai waiAppStatic yesodCore
   ];
   testDepends = [
     async base64Bytestring byteable conduit conduitExtra cryptohash
