8a446a01d7b01b84140efe2eed3be5cff318345f
Peter Simons <simons@cryp.to>
2014-05-07 11:46:40 +0200

    haskell-HsSyck: update to version 0.52

--- pkgs/development/libraries/haskell/HsSyck/default.nix
+++ pkgs/development/libraries/haskell/HsSyck/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "HsSyck";
-  version = "0.51";
-  sha256 = "13y5vbzrix33dzwhdrvng2ds2dnipkxn1h22jvbf47rwnkyh2g49";
+  version = "0.52";
+  sha256 = "1hdckbm60pzmydaz4rsw3kz9byd205987jcddakyhmgfinzvqwvc";
   buildDepends = [ hashtables syb utf8String ];
   meta = {
     description = "Fast, lightweight YAML loader and dumper";
