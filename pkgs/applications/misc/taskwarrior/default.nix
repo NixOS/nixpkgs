07a5187063487d734d8bb2777bfa64621e61e0e8
Benjamin Podszun <dar@darklajid.de>
2014-05-08 11:18:02 +0200

    taskwarrior: bump to 2.3.0 (close #2567)
    vcunat fixed the evaluation

--- pkgs/applications/misc/taskwarrior/default.nix
+++ pkgs/applications/misc/taskwarrior/default.nix
@@ -1,17 +1,17 @@
-{stdenv, fetchurl, cmake}:
+{ stdenv, fetchurl, cmake, libuuid }:
 
 stdenv.mkDerivation rec {
   name = "taskwarrior-${version}";
-  version = "2.2.0";
+  version = "2.3.0";
 
   enableParallelBuilding = true;
 
   src = fetchurl {
     url = "http://www.taskwarrior.org/download/task-${version}.tar.gz";
-    sha256 = "057fh50qp9bd5s08rw51iybpamn55v5nhn3s6ds89g76hp95vqir";
+    sha256 = "0wxcfq0n96vmcbwrlk2x377k8cc5k4i64ca6p02y74g6168ji6ib";
   };
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [ cmake libuuid ];
 
   meta = {
     description = "GTD (getting things done) implementation";
