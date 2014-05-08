709ccd520bd3c9277ce97eb0dc96d06bae4deae3
aszlig <aszlig@redmoonstudios.org>
2014-05-08 14:59:42 +0200

    patchutils: Update to new upstream version 0.3.3.
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/tools/text/patchutils/default.nix
+++ pkgs/tools/text/patchutils/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl }:
 
-stdenv.mkDerivation {
-  name = "patchutils-0.3.0";
+stdenv.mkDerivation rec {
+  name = "patchutils-0.3.3";
 
   src = fetchurl {
-    url = http://cyberelk.net/tim/data/patchutils/stable/patchutils-0.3.0.tar.bz2;
-    sha256 = "08jzvprhpcgwvx0xlkwc8dbdd9ilvwyr3cwnq96xmbfipch69yi7";
+    url = "http://cyberelk.net/tim/data/patchutils/stable/${name}.tar.xz";
+    sha256 = "0g5df00cj4nczrmr4k791l7la0sq2wnf8rn981fsrz1f3d2yix4i";
   };
 
-  meta = { 
+  meta = {
     description = "Tools to manipulate patch files";
     homepage = http://cyberelk.net/tim/software/patchutils;
     license = "GPLv2";
