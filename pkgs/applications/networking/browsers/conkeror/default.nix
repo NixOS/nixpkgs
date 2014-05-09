06c8438bd8e9e3104a854211c78daceaad30edac
Mathijs Kwik <mathijs@bluescreen303.nl>
2014-05-09 07:15:32 +0200

    conkeror: upgrade to latest snapshot

--- pkgs/applications/networking/browsers/conkeror/default.nix
+++ pkgs/applications/networking/browsers/conkeror/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchgit, unzip, xulrunner, makeWrapper }:
 
 stdenv.mkDerivation {
-  name = "conkeror-1.0pre-20140212";
+  name = "conkeror-1.0pre-20140509";
 
   src = fetchgit {
     url = git://repo.or.cz/conkeror.git;
-    rev = "07064d76d10e0978c6de535e21f4597d44560fbd";
-    sha256 = "b03a7debee8583ff7a3f2d95474f60e956f0e24dbd1a8fd22412de1d6627f322";
+    rev = "eaf02dccf979d824667d177544103af000ae40bf";
+    sha256 = "1896db59d260f34a459402cb0c252b636aab9bf0378ae663a6d039f15af5b350";
   };
 
   buildInputs = [ unzip makeWrapper ];
