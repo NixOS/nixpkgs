cf1dbbbf7278be6b6c9a531d8c9b781836200697
Vladimír Čunát <vcunat@gmail.com>
2014-05-10 17:29:41 +0200

    llvm+clang: maintenance update 3.4 -> 3.4.1
    Slightly hacky for clang at some points, as directory names change strangely.

--- pkgs/development/compilers/llvm/3.4/default.nix
+++ pkgs/development/compilers/llvm/3.4/default.nix
@@ -4,15 +4,27 @@ let
 
   version = "3.4";
 
-  fetch = name: sha256: fetchurl {
-    url = "http://llvm.org/releases/${version}/${name}-${version}.src.tar.gz";
+  fetch = fetch_v version;
+  fetch_v = ver: name: sha256: fetchurl {
+    url = "http://llvm.org/releases/${ver}/${name}-${ver}.src.tar.gz";
     inherit sha256;
   };
 
-  self = {
-    llvm = callPackage ./llvm.nix {};
+  compiler-rt_src = fetch "compiler-rt" "0p5b6varxdqn7q3n77xym63hhq4qqxd2981pfpa65r1w72qqjz7k";
+  clang-tools-extra_src = fetch "clang-tools-extra" "1d1822mwxxl9agmyacqjw800kzz5x8xr0sdmi8fgx5xfa5sii1ds";
 
-    clang = callPackage ./clang.nix {};
+  self = {
+    llvm = callPackage ./llvm.nix rec {
+      version = "3.4.1";
+      fetch = fetch_v version;
+      inherit compiler-rt_src;
+    };
+
+    clang = callPackage ./clang.nix rec {
+      version = "3.4.1";
+      fetch = fetch_v version;
+      inherit clang-tools-extra_src;
+    };
 
     lld = callPackage ./lld.nix {};
 
