b0f62b8e915fd9c04dde03eb38d6b5d320f99c79
taku0 <mxxouy6x3m_github@tatapa.org>
2014-05-11 19:54:58 +0900

    scala: fix 2.9 URL

---
+++ pkgs/development/compilers/scala/base.nix
@@ -0,0 +1,40 @@
+{ name, sha256 }:
+
+{ stdenv, fetchurl, makeWrapper, jre }:
+
+stdenv.mkDerivation rec {
+  inherit name;
+
+  src = fetchurl {
+    url = "http://www.scala-lang.org/files/archive/${name}.tgz";
+    inherit sha256;
+  };
+
+  buildInputs = [ jre makeWrapper ] ;
+
+  installPhase = ''
+    mkdir -p $out
+    rm bin/*.bat
+    rm -f lib/scalacheck.jar # for 2.9
+    mv * $out
+
+    for p in $(ls $out/bin/) ; do
+      wrapProgram $out/bin/$p --prefix PATH ":" ${jre}/bin ;
+    done
+  '';
+
+  meta = {
+    description = "Scala is a general purpose programming language";
+    longDescription = ''
+      Scala is a general purpose programming language designed to express
+      common programming patterns in a concise, elegant, and type-safe way.
+      It smoothly integrates features of object-oriented and functional
+      languages, enabling Java and other programmers to be more productive.
+      Code sizes are typically reduced by a factor of two to three when
+      compared to an equivalent Java application.
+    '';
+    homepage = http://www.scala-lang.org/;
+    license = "BSD";
+    platforms = stdenv.lib.platforms.all;
+  };
+}
--- pkgs/development/compilers/scala/default.nix
+++ pkgs/development/compilers/scala/default.nix
@@ -1,37 +1,4 @@
-{ stdenv, fetchurl, makeWrapper, jre }:
-
-stdenv.mkDerivation rec {
+import ./base.nix {
   name = "scala-2.11.0";
-
-  src = fetchurl {
-    url = "http://www.scala-lang.org/files/archive/${name}.tgz";
-    sha256 = "00lap31c6rxvg7vipmj0j7f4mv6c58wpfyd3785bxwlhrzmmwgq7";
-  };
-
-  buildInputs = [ jre makeWrapper ] ;
-
-  installPhase = ''
-    mkdir -p $out
-    rm bin/*.bat
-    mv * $out
-
-    for p in $(ls $out/bin/) ; do
-      wrapProgram $out/bin/$p --prefix PATH ":" ${jre}/bin ;
-    done
-  '';
-
-  meta = {
-    description = "Scala is a general purpose programming language";
-    longDescription = ''
-      Scala is a general purpose programming language designed to express
-      common programming patterns in a concise, elegant, and type-safe way.
-      It smoothly integrates features of object-oriented and functional
-      languages, enabling Java and other programmers to be more productive.
-      Code sizes are typically reduced by a factor of two to three when
-      compared to an equivalent Java application.
-    '';
-    homepage = http://www.scala-lang.org/;
-    license = "BSD";
-    platforms = stdenv.lib.platforms.all;
-  };
+  sha256 = "073f5eebcf90f2be0a3aa37977392accec4adc9140d61bf779bb67c3c2b88a02";
 }
