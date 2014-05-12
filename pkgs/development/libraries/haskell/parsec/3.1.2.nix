62532defb1f6307760e4d9221d9f8478382dc16f
Peter Simons <simons@cryp.to>
2014-05-12 14:26:26 +0200

    haskell-parsec-3.1.2: jailbreak to fix build with text >= 1

--- pkgs/development/libraries/haskell/parsec/3.1.2.nix
+++ pkgs/development/libraries/haskell/parsec/3.1.2.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "3.1.2";
   sha256 = "0lhn9j2j5jlh7g0qx9f6ms63n9x1xlxg9isdvm6z0ksy3ywj9wch";
   buildDepends = [ mtl text ];
+  jailbreak = true;
   meta = {
     homepage = "http://www.cs.uu.nl/~daan/parsec.html";
     description = "Monadic parser combinators";
