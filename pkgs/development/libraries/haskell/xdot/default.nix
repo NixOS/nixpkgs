8560db44f771e586312ea865ca5cd4526260aff9
Peter Simons <simons@cryp.to>
2014-05-12 14:21:07 +0200

    haskell-xdot: jailbreak to fix build with text >= 1.1

--- pkgs/development/libraries/haskell/xdot/default.nix
+++ pkgs/development/libraries/haskell/xdot/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.2.4.2";
   sha256 = "0a5wmwcl3akw1n9xgdhvlrbvphvy9s528daax28137ixaphvrl0f";
   buildDepends = [ cairo graphviz gtk mtl polyparse text ];
+  jailbreak = true;
   meta = {
     description = "Parse Graphviz xdot files and interactively view them using GTK and Cairo";
     license = self.stdenv.lib.licenses.bsd3;
