a58455c024704a550fda79c8b69340c537348571
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-05-13 13:27:21 +0200

    linux: Update to 3.12.19
    Backport: 14.04
    (cherry picked from commit abbf643ae2dc03278133a65b3eb220a3293b3bbf)

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.12.18";
+  version = "3.12.19";
   extraMeta.branch = "3.12";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0w0nwkhrbxa9yqnj6mvckkxdqn291mw4pxa309alcga3wn29mwis";
+    sha256 = "1f59lyyyc4anaj50dzb9448gz6n9fdrcy2mvd7f193pp6018gdp6";
   };
 
   features.iwlwifi = true;
