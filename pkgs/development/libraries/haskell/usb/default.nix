481ec8ab9b46dc733dc74d500131417e971ecc41
Peter Simons <simons@cryp.to>
2014-05-13 18:36:12 +0200

    haskell-usb: jailbreak to fix build with text 1.x

--- pkgs/development/libraries/haskell/usb/default.nix
+++ pkgs/development/libraries/haskell/usb/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "1.2";
   sha256 = "1k73avkmpbmg6iq2kmwhg2ifibni5c1yp202afdb6v7w5akvmc0b";
   buildDepends = [ baseUnicodeSymbols bindingsLibusb text vector ];
+  jailbreak = true;
   meta = {
     homepage = "http://basvandijk.github.com/usb";
     description = "Communicate with USB devices";
