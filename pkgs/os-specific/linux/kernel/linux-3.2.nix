9c8ee7a7e55259d1c351c8343e00c6db9353eca8
Vladimír Čunát <vcunat@gmail.com>
2014-05-13 14:07:58 +0200

    linux: minor updates, probably often fixing CVE-2014-0196

--- pkgs/os-specific/linux/kernel/linux-3.2.nix
+++ pkgs/os-specific/linux/kernel/linux-3.2.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.2.57";
+  version = "3.2.58";
   extraMeta.branch = "3.2";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0njiqa5fi82a4dls9z1cp33lza00bg15x93c19bsyalp53xqbnab";
+    sha256 = "1mszzixiv4k61m241dl2n5s8rca26l6hc40v23lha814nrahjkn1";
   };
 
   features.iwlwifi = true;
--- pkgs/os-specific/linux/kernel/linux-3.4.nix
+++ pkgs/os-specific/linux/kernel/linux-3.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.4.88";
+  version = "3.4.89";
   extraMeta.branch = "3.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0l2ccrsr7ga6rhjb58vq7xxhym7n90m7n181hwpi707wikhz80rd";
+    sha256 = "1ldhdbfjvha1z9csnl518qdm3w8w284f59dgn4adg8a0ig6lybk6";
   };
 
   features.iwlwifi = true;
