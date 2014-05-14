1b4a5c52b0978fc12159b8c7da305939c7313843
Austin Seipp <aseipp@pobox.com>
2014-05-14 08:54:43 -0500

    linux-3.{4,10}: update
    Signed-off-by: Austin Seipp <aseipp@pobox.com>
    (cherry picked from commit 3efdeef6a31dca9dc7e76afd575fa43e78d6afea)

--- pkgs/os-specific/linux/kernel/linux-3.10.nix
+++ pkgs/os-specific/linux/kernel/linux-3.10.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.10.39";
+  version = "3.10.40";
   extraMeta.branch = "3.10";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1zl51dhcfyynii808mfvdjfq349wgfj1p4i8qbb786ancxam2ln4";
+    sha256 = "0a56hh47a9x8mg1rxlckfnrjcphwz9hraxj2fgran95vb33hsq5a";
   };
 
   features.iwlwifi = true;
--- pkgs/os-specific/linux/kernel/linux-3.4.nix
+++ pkgs/os-specific/linux/kernel/linux-3.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.4.89";
+  version = "3.4.90";
   extraMeta.branch = "3.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1ldhdbfjvha1z9csnl518qdm3w8w284f59dgn4adg8a0ig6lybk6";
+    sha256 = "0kh4y1sbsjm3awplfsd0i59rz7wc1dj23mcs5rwwhc0p7i8w4r75";
   };
 
   features.iwlwifi = true;
