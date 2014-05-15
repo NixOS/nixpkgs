7cba5944b4b59cc5ee14b5cba59462be986563ac
Peter Simons <simons@cryp.to>
2014-05-15 16:09:43 +0200

    haskell-thyme: update to version 0.3.5.0

--- pkgs/development/libraries/haskell/thyme/default.nix
+++ pkgs/development/libraries/haskell/thyme/default.nix
@@ -1,15 +1,15 @@
 { cabal, aeson, attoparsec, Cabal, deepseq, filepath, mtl
 , profunctors, QuickCheck, random, systemPosixRedirect, text, time
-, vector, vectorSpace
+, vector, vectorSpace, vectorThUnbox
 }:
 
 cabal.mkDerivation (self: {
   pname = "thyme";
-  version = "0.3.3.0";
-  sha256 = "0mpwwpc82xcdyawz87rcmvga1miw7cx538nnh379m2ibn0g71zaa";
+  version = "0.3.5.0";
+  sha256 = "0jc687hdxz97qjslminwqgj40pnnyhpfv73j6d8pmg064svdrg0q";
   buildDepends = [
     aeson attoparsec deepseq mtl profunctors QuickCheck random text
-    time vector vectorSpace
+    time vector vectorSpace vectorThUnbox
   ];
   testDepends = [
     attoparsec Cabal filepath mtl profunctors QuickCheck random
