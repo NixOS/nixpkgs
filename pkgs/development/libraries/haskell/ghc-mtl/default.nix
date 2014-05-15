9778f505de11776d6a36171fb6565b30fde05fca
Peter Simons <simons@cryp.to>
2014-05-15 16:08:43 +0200

    haskell-ghc-mtl: update to version 1.2.1.0

--- pkgs/development/libraries/haskell/ghc-mtl/default.nix
+++ pkgs/development/libraries/haskell/ghc-mtl/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "ghc-mtl";
-  version = "1.2.0.0";
-  sha256 = "0fk3adc56nhi46nf2w5ybp3cd9l13qqbpd5nbhfhbqj3g73j8p5a";
+  version = "1.2.1.0";
+  sha256 = "0h6r6gip9nsjlsq88wj105hhqliy7ac2dmmndsfzbjz07b03cklk";
   buildDepends = [ exceptions extensibleExceptions mtl ];
   meta = {
     homepage = "http://hub.darcs.net/jcpetruzza/ghc-mtl";
