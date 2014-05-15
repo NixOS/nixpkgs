8baf727bad4252923b643177e04b4b0292e798d1
Peter Simons <simons@cryp.to>
2014-05-15 19:56:16 +0200

    haskell-yap: jailbreak to fix build with recent versions of 'base'

--- pkgs/development/libraries/haskell/yap/default.nix
+++ pkgs/development/libraries/haskell/yap/default.nix
@@ -4,6 +4,7 @@ cabal.mkDerivation (self: {
   pname = "yap";
   version = "0.2";
   sha256 = "14lq549jhgnf51pgy1jv31ik8qx71yl7d53w8dpq1f9mlsn1g16i";
+  jailbreak = true;
   meta = {
     description = "yet another prelude - a simplistic refactoring with algebraic classes";
     license = self.stdenv.lib.licenses.bsd3;
