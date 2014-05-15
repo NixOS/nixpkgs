b1bbb8f7bc1b180feb3337d08113fa8b8ea5471b
Peter Simons <simons@cryp.to>
2014-05-15 16:09:34 +0200

    haskell-streaming-commons: update to version 0.1.2.4

--- pkgs/development/libraries/haskell/streaming-commons/default.nix
+++ pkgs/development/libraries/haskell/streaming-commons/default.nix
@@ -1,12 +1,14 @@
 { cabal, async, blazeBuilder, deepseq, hspec, network, QuickCheck
-, text, transformers, zlib
+, random, text, transformers, zlib
 }:
 
 cabal.mkDerivation (self: {
   pname = "streaming-commons";
-  version = "0.1.2.3";
-  sha256 = "1f8lngnxx0ml7bph8lfx3azv6fh6gwm86yhb8i5v9ghnflblaxxs";
-  buildDepends = [ blazeBuilder network text transformers zlib ];
+  version = "0.1.2.4";
+  sha256 = "1wy1cdmx3fhxg9xb7v5r3nyyjhr7irvcwy3l4g6br671zh8j8kcg";
+  buildDepends = [
+    blazeBuilder network random text transformers zlib
+  ];
   testDepends = [
     async blazeBuilder deepseq hspec network QuickCheck text zlib
   ];
