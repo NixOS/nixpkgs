cdbaa41f24cb6c6a8b734b87c65d10749bdbe189
Peter Simons <simons@cryp.to>
2014-05-16 21:28:18 +0200

    haskell-thyme: update to version 0.3.5.1

--- pkgs/development/libraries/haskell/thyme/default.nix
+++ pkgs/development/libraries/haskell/thyme/default.nix
@@ -5,8 +5,8 @@
 
 cabal.mkDerivation (self: {
   pname = "thyme";
-  version = "0.3.5.0";
-  sha256 = "0jc687hdxz97qjslminwqgj40pnnyhpfv73j6d8pmg064svdrg0q";
+  version = "0.3.5.1";
+  sha256 = "0v3aq7zv4fnkjhfc7mm6vx2wb5dh2npf2lsgpvcrdpy5zlfsrx50";
   buildDepends = [
     aeson attoparsec deepseq mtl profunctors QuickCheck random text
     time vector vectorSpace vectorThUnbox
