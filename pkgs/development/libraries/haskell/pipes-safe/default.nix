beb5d5fb84062730c4348e5c5b81b3369381e32b
Peter Simons <simons@cryp.to>
2014-05-16 21:28:14 +0200

    haskell-pipes-safe: update to version 2.2.0

--- pkgs/development/libraries/haskell/pipes-safe/default.nix
+++ pkgs/development/libraries/haskell/pipes-safe/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "pipes-safe";
-  version = "2.1.0";
-  sha256 = "0qm02hwmrqlncnlxix7mdgzbf0mzally4k7ydwg06nqi35bb7s3j";
+  version = "2.2.0";
+  sha256 = "1m44a2pbws73jbr2ca48i94mrfwzlsibyc22i2w3fqq159qfg6ca";
   buildDepends = [ exceptions pipes transformers ];
   meta = {
     description = "Safety for the pipes ecosystem";
