c8f41b2b4fdb476aa306516c31a2e39eb753f7ee
Corey O'Connor <coreyoconnor@gmail.com>
2014-05-16 00:23:37 -0700

    style cleanup

--- pkgs/tools/X11/primus/default.nix
+++ pkgs/tools/X11/primus/default.nix
@@ -11,8 +11,9 @@ let
 ''
   export LD_LIBRARY_PATH=${ld_path}
   # see: https://github.com/amonakov/primus/issues/138
-  # I think the intel driver barfs when the pixel buffers try to read from the source memory
-  # directly. This causes an indirection through textures which appears to avoid issues.
+  # I think the intel driver dies when the pixel buffers try to read from the source memory
+  # directly. Setting PRIMUS_UPLOAD causes an indirection through textures which appears to avoid
+  # this issue.
   export PRIMUS_UPLOAD=1
   exec "$@"
 '';
