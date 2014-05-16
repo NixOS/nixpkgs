65b6ac1d67561283f80a75f1c99c1ac07db48e65
Peter Simons <simons@cryp.to>
2014-05-16 21:28:17 +0200

    haskell-safecopy: update to version 0.8.3

--- pkgs/development/libraries/haskell/safecopy/default.nix
+++ pkgs/development/libraries/haskell/safecopy/default.nix
@@ -1,10 +1,15 @@
-{ cabal, cereal, text, time }:
+{ cabal, cereal, lens, quickcheckInstances, tasty, tastyQuickcheck
+, text, time, vector
+}:
 
 cabal.mkDerivation (self: {
   pname = "safecopy";
-  version = "0.8.2";
-  sha256 = "0l2kqymsxv244fahxcpxlrspk6xipz3br6j854ipbfh8b0bfvr4m";
-  buildDepends = [ cereal text time ];
+  version = "0.8.3";
+  sha256 = "10xd42a1i84fwiy3db0ji4bd8ssk9jqysqwc00wdcdjyp50c333n";
+  buildDepends = [ cereal text time vector ];
+  testDepends = [
+    cereal lens quickcheckInstances tasty tastyQuickcheck time vector
+  ];
   meta = {
     homepage = "http://acid-state.seize.it/safecopy";
     description = "Binary serialization with version control";
