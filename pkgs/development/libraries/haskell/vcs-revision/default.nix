1f1fa7cac498e6de5fecf8a9e98d580bef2c6f84
Peter Simons <simons@cryp.to>
2014-05-16 21:28:20 +0200

    haskell-vcs-revision: update to version 0.0.2

--- pkgs/development/libraries/haskell/vcs-revision/default.nix
+++ pkgs/development/libraries/haskell/vcs-revision/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "vcs-revision";
-  version = "0.0.1";
-  sha256 = "1zfv9b02ml8622kz755azhi4ajyxrqniiachd92znfrry4n8z1mn";
+  version = "0.0.2";
+  sha256 = "1lp1wf440n7kinmxz7la0gyfqfdlip6f0bn8pmwkxd1dqyrvg5cg";
   meta = {
     description = "Facilities for accessing the version control revision of the current directory";
     license = self.stdenv.lib.licenses.bsd3;
