6863dfeccc4296bba01a0a736878141545f54f14
Florent Becker <florent.becker@ens-lyon.org>
2014-05-17 12:51:18 +0200

    Add a derivation for ocaml-optcomp

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3381,6 +3381,8 @@ let
 
     ocaml_oasis = callPackage ../development/tools/ocaml/oasis { };
 
+    ocaml_optcomp = callPackage ../development/ocaml-modules/optcomp { };
+
     ocaml_pcre = callPackage ../development/ocaml-modules/pcre {
       inherit pcre;
     };
