f77e2dcb387637aea3a304c404867d27f91bccb8
Vladimír Čunát <vcunat@gmail.com>
2014-05-17 12:14:52 +0200

    mupdf: fix patch hash by fetchpatch
    Thanks to @kirelagin for reporting on IRC.

--- pkgs/applications/misc/mupdf/default.nix
+++ pkgs/applications/misc/mupdf/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, zlib, freetype, libjpeg, jbig2dec, openjpeg
+{ stdenv, fetchurl, fetchpatch, pkgconfig, zlib, freetype, libjpeg, jbig2dec, openjpeg
 , libX11, libXext }:
 stdenv.mkDerivation rec {
   name = "mupdf-1.3";
@@ -8,11 +8,11 @@ stdenv.mkDerivation rec {
     sha256 = "0y247nka5gkr1ajn47jrlp5rcnf6h4ff7dfsprma3h4wxqdv7a5b";
   };
 
-  patches = [(fetchurl {
+  patches = [(fetchpatch {
     name = "CVE-2014-2013.patch";
     url = "http://git.ghostscript.com/?p=mupdf.git;a=commitdiff_plain;"
       + "h=60dabde18d7fe12b19da8b509bdfee9cc886aafc";
-    sha256 = "1walj3wir9x50i6lph33bx14c8593r9xrn08gkd3v7r6d15lmjps";
+    sha256 = "0p721f3g2djz9fy6rcgj83c20f5k257wg2d0yvvmp02m7sp06l0g";
   })];
 
   buildInputs = [ pkgconfig zlib freetype libjpeg jbig2dec openjpeg libX11 libXext ];
