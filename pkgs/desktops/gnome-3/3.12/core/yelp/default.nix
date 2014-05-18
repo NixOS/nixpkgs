ae20e1c5f576f186f011cc3ac1a5d34f1cf96f52
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 23:22:30 +0200

    yelp: add 3.12 version

--- pkgs/desktops/gnome-3/3.12/core/yelp/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/yelp/default.nix
@@ -3,27 +3,27 @@
 , bash, makeWrapper, itstool, libxml2, libxslt, icu }:
 
 stdenv.mkDerivation rec {
-  name = "yelp-3.10.1";
+  name = "yelp-3.12.0";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/yelp/3.10/${name}.tar.xz";
-    sha256 = "17736479b7d0b1128c7d6cb3073f2b09e4bbc82670731b2a0d3a3219a520f816";
+    url = "mirror://gnome/sources/yelp/3.12/${name}.tar.xz";
+    sha256 = "0k2a1fggidmh98x2fv8zki2lbx7wx7p4b25iq11p6q8j9fwr2ff8";
   };
 
   propagatedUserEnvPkgs = [ gnome3.gnome_themes_standard  ];
-  propagatedBuildInputs = [ librsvg gdk_pixbuf gnome3.gnome_icon_theme
-                            hicolor_icon_theme gnome3.gnome_icon_theme_symbolic ];
 
   preConfigure = "substituteInPlace ./configure --replace /usr/bin/file ${file}/bin/file";
 
   buildInputs = [ pkgconfig gtk3 glib webkitgtk intltool itstool
                   libxml2 libxslt icu file makeWrapper gnome3.yelp_xsl
+                  librsvg gdk_pixbuf gnome3.gnome_icon_theme
+                  hicolor_icon_theme gnome3.gnome_icon_theme_symbolic
                   gnome3.gsettings_desktop_schemas ];
 
   preFixup = ''
     wrapProgram "$out/bin/yelp" \
       --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE" \
-      --prefix XDG_DATA_DIRS : "${gtk3}/share:${gnome3.gnome_themes_standard}/:${gnome3.gnome_themes_standard}/share:${gnome3.yelp_xsl}/share/yelp-xsl:${gnome3.gsettings_desktop_schemas}/share:$out/share:$out/share/yelp:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
+      --prefix XDG_DATA_DIRS : "${gnome3.gnome_themes_standard}/:${gnome3.gnome_themes_standard}/share:${gnome3.yelp_xsl}/share/yelp-xsl:${gnome3.gsettings_desktop_schemas}/share:$out/share:$out/share/yelp:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
   '';
 
   meta = with stdenv.lib; {
