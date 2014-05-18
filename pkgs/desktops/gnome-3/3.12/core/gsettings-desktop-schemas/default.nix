8afd771056345babb51b941d97938421f54acd6b
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 23:47:23 +0200

    gsettings-desktop-schemas: add 3.12.2 version and remove unused passthru

--- pkgs/desktops/gnome-3/3.12/core/gsettings-desktop-schemas/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/gsettings-desktop-schemas/default.nix
@@ -4,27 +4,18 @@
 
 stdenv.mkDerivation rec {
 
-  versionMajor = "3.10";
-  versionMinor = "1";
+  versionMajor = "3.12";
+  versionMinor = "2";
   moduleName   = "gsettings-desktop-schemas";
 
   name = "${moduleName}-${versionMajor}.${versionMinor}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${moduleName}/${versionMajor}/${name}.tar.xz";
-    sha256 = "04b8wy10l6pzs5928gnzaia73dz5fjlcdy39xi3mf50ajv27h8s5";
+    sha256 = "da75021e9c45a60d0a97ea3486f93444275d0ace86dbd1b97e5d09000d8c4ad1";
   };
 
   buildInputs = [ glib gobjectIntrospection ];
 
   nativeBuildInputs = [ pkgconfig intltool ];
-
-  passthru = {
-    doCompileSchemas = ''
-      for pkg in "${gsettings_desktop_schemas}" "${gtk3}"; do
-        cp -s $pkg/share/glib-2.0/schemas/*.gschema.xml $out/share/glib-2.0/schemas/
-      done
-      ${glib}/bin/glib-compile-schemas $out/share/glib-2.0/schemas/
-    '';
-  };
 }
