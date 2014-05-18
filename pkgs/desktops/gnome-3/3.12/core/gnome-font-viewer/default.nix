f7d8afcf2b22af6a65386ae5d3fedf42c832e515
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 23:39:49 +0200

    gnome-font-viewer: add 3.12.0 version and cleanup expr

--- pkgs/desktops/gnome-3/3.12/core/gnome-font-viewer/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/gnome-font-viewer/default.nix
@@ -4,11 +4,11 @@
 , gnome3, librsvg, gdk_pixbuf }:
 
 stdenv.mkDerivation rec {
-  name = "gnome-font-viewer-3.10.0";
+  name = "gnome-font-viewer-3.12.0";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gnome-font-viewer/3.10/${name}.tar.xz";
-    sha256 = "3928350f58ac6c95f44b64cba1a5f03437b19d9b2645a7b01176067504fdd652";
+    url = "mirror://gnome/sources/gnome-font-viewer/3.12/${name}.tar.xz";
+    sha256 = "fca50711b7bd4edd1213dd4f05a309911cd1e832974142944c06d52ae07cbe45";
   };
 
   doCheck = true;
@@ -16,16 +16,16 @@ stdenv.mkDerivation rec {
   NIX_CFLAGS_COMPILE = "-I${gnome3.glib}/include/gio-unix-2.0";
 
   propagatedUserEnvPkgs = [ gnome3.gnome_themes_standard ];
-  propagatedBuildInputs = [ gdk_pixbuf gnome3.gnome_icon_theme librsvg
-                            hicolor_icon_theme gnome3.gnome_icon_theme_symbolic ];
 
   buildInputs = [ pkgconfig gtk3 glib intltool itstool gnome3.gnome_desktop
+                  gdk_pixbuf gnome3.gnome_icon_theme librsvg
+                  hicolor_icon_theme gnome3.gnome_icon_theme_symbolic
                   gnome3.gsettings_desktop_schemas makeWrapper ];
 
   preFixup = ''
     wrapProgram "$out/bin/gnome-font-viewer" \
       --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE" \
-      --prefix XDG_DATA_DIRS : "${gtk3}/share:${gnome3.gnome_themes_standard}/share:$out/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
+      --prefix XDG_DATA_DIRS : "${gnome3.gnome_themes_standard}/share:$out/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
   '';
 
   meta = with stdenv.lib; {
