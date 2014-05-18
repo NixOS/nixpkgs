aa56842577ae3135699ded25437836ffe81e5633
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 23:48:21 +0200

    gcr: add 3.12.2 version

--- pkgs/desktops/gnome-3/3.12/core/gcr/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/gcr/default.nix
@@ -1,17 +1,17 @@
 { stdenv, fetchurl, pkgconfig, intltool, gnupg, p11_kit, glib
 , libgcrypt, libtasn1, dbus_glib, gtk, pango, gdk_pixbuf, atk
-, gobjectIntrospection, makeWrapper }:
+, gobjectIntrospection, makeWrapper, libxslt }:
 
 stdenv.mkDerivation rec {
-  name = "gcr-3.10.1";
+  name = "gcr-3.12.2";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gcr/3.10/${name}.tar.xz";
-    sha256 = "0nv470a8cvw4rw49hf5aqvll1rpkacmsr3pj8s1l205yaid4yvq0";
+    url = "mirror://gnome/sources/gcr/3.12/${name}.tar.xz";
+    sha256 = "456e20615ab178aa92eeabdea64dcce535c10d5af189171d9375291a2447d21c";
   };
 
   buildInputs = [
-    pkgconfig intltool gnupg p11_kit glib gobjectIntrospection
+    pkgconfig intltool gnupg p11_kit glib gobjectIntrospection libxslt
     libgcrypt libtasn1 dbus_glib gtk pango gdk_pixbuf atk makeWrapper
   ];
 
