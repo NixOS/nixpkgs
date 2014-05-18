11be39c23827de994b21e2b5c5bf76dc9eb4e5d4
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 23:11:18 +0200

    gnome-icon-theme: add 3.12.0 version

--- pkgs/desktops/gnome-3/3.12/core/gnome-icon-theme/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/gnome-icon-theme/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, intltool, iconnamingutils, gtk }:
 
 stdenv.mkDerivation rec {
-  name = "gnome-icon-theme-3.10.0";
+  name = "gnome-icon-theme-3.12.0";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gnome-icon-theme/3.10/${name}.tar.xz";
-    sha256 = "1xinbgkkvlhazj887ajcl13i7kdc1wcca02jwxzvjrvchjsp4m66";
+    url = "mirror://gnome/sources/gnome-icon-theme/3.12/${name}.tar.xz";
+    sha256 = "359e720b9202d3aba8d477752c4cd11eced368182281d51ffd64c8572b4e503a";
   };
 
   setupHook = ./setup-hook.sh;
