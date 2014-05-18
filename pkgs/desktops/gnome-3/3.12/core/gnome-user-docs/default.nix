5eb739fb1e6943dffe8950c50a2e0999b7448311
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 23:32:44 +0200

    gnome-user-docs: add 3.12.1 version

--- pkgs/desktops/gnome-3/3.12/core/gnome-user-docs/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/gnome-user-docs/default.nix
@@ -1,17 +1,17 @@
 { stdenv, fetchurl, pkgconfig, file, gnome3, itstool, libxml2, intltool }:
 
 stdenv.mkDerivation rec {
-  name = "gnome-user-docs-3.10.3";
+  name = "gnome-user-docs-3.12.1";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gnome-user-docs/3.10/${name}.tar.xz";
-    sha256 = "960b6373ea52e41e3deb3501930e024005b29d2cc958bfadc87450a291d2a905";
+    url = "mirror://gnome/sources/gnome-user-docs/3.12/${name}.tar.xz";
+    sha256 = "bfd084d72c688d6efb0c34bb572a704cc2ce093c97a33390eaffb5e42158d418";
   };
 
   buildInputs = [ pkgconfig gnome3.yelp itstool libxml2 intltool ];
 
   meta = with stdenv.lib; {
-    homepage = https://help.gnome.org/users/gnome-help/3.10;
+    homepage = https://help.gnome.org/users/gnome-help/3.12;
     description = "User and system administration help for the Gnome desktop";
     maintainers = with maintainers; [ lethalman ];
     license = licenses.cc-by-30;
