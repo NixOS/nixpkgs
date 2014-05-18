a1abb9a52599b74bb8158b0c4aec4cb9c3c9f64f
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 23:34:21 +0200

    gnome-disk-utility: add 3.12.1 version and cleanup expr

--- pkgs/desktops/gnome-3/3.12/core/gnome-disk-utility/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/gnome-disk-utility/default.nix
@@ -4,11 +4,11 @@
 , libcanberra_gtk3, libxslt, libtool, docbook_xsl, libpwquality }:
 
 stdenv.mkDerivation rec {
-  name = "gnome-disk-utility-3.10.0";
+  name = "gnome-disk-utility-3.12.1";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gnome-disk-utility/3.10/${name}.tar.xz";
-    sha256 = "1amqi2bribxn8r8k8mvxh3710rmdll9963smf0v59v0iwxi3mqil";
+    url = "mirror://gnome/sources/gnome-disk-utility/3.12/${name}.tar.xz";
+    sha256 = "5994bfae57063d74be45736050cf166cda5b1600a599703240b641b39375718e";
   };
 
   doCheck = true;
@@ -16,19 +16,19 @@ stdenv.mkDerivation rec {
   NIX_CFLAGS_COMPILE = "-I${gnome3.glib}/include/gio-unix-2.0";
 
   propagatedUserEnvPkgs = [ gnome3.gnome_themes_standard ];
-  propagatedBuildInputs = [ gdk_pixbuf gnome3.gnome_icon_theme
-                            librsvg udisks2 gnome3.gnome_settings_daemon
-                            hicolor_icon_theme gnome3.gnome_icon_theme_symbolic ];
 
   buildInputs = [ bash pkgconfig gtk3 glib intltool itstool
                   libxslt libtool libsecret libpwquality cracklib
                   libnotify libdvdread libcanberra_gtk3 docbook_xsl
+                  gdk_pixbuf gnome3.gnome_icon_theme
+                  librsvg udisks2 gnome3.gnome_settings_daemon
+                  hicolor_icon_theme gnome3.gnome_icon_theme_symbolic
                   gnome3.gsettings_desktop_schemas makeWrapper libxml2 ];
 
   preFixup = ''
     wrapProgram "$out/bin/gnome-disks" \
       --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE" \
-      --prefix XDG_DATA_DIRS : "${gtk3}/share:${gnome3.gnome_themes_standard}/share:$out/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
+      --prefix XDG_DATA_DIRS : "${gnome3.gnome_themes_standard}/share:$out/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
     rm $out/share/icons/hicolor/icon-theme.cache
   '';
 
