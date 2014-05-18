b01b15bb0bcb14588cb327f63d09dc3dba29e13a
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 21:19:56 +0200

    baobab: add 3.12.1 version and cleanup expr

--- pkgs/desktops/gnome-3/3.12/core/baobab/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/baobab/default.nix
@@ -4,14 +4,12 @@
 , gnome3, librsvg, gdk_pixbuf, file }:
 
 stdenv.mkDerivation rec {
-  name = "baobab-3.10.1";
+  name = "baobab-3.12.1";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/baobab/3.10/${name}.tar.xz";
-    sha256 = "23ce8e4847ce5f1c8230e757532d94c84e6e273d6ec8fca20eecaed5f96563f9";
+    url = "mirror://gnome/sources/baobab/3.12/${name}.tar.xz";
+    sha256 = "494808d8a5b1776377749a1dcd5b251eb399208a4c90380dc8b8c789811e514c";
   };
-
-  configureFlags = [ "--disable-static" ];
 
   doCheck = true;
 
@@ -22,14 +20,16 @@ stdenv.mkDerivation rec {
                             hicolor_icon_theme gnome3.gnome_icon_theme_symbolic ];
 
   buildInputs = [ vala pkgconfig gtk3 glib libgtop intltool itstool libxml2
-                  gnome3.gsettings_desktop_schemas makeWrapper file ];
+                  gnome3.gsettings_desktop_schemas makeWrapper file
+                  gdk_pixbuf gnome3.gnome_icon_theme librsvg
+                  hicolor_icon_theme gnome3.gnome_icon_theme_symbolic ];
 
   preFixup = ''
     rm $out/share/icons/hicolor/icon-theme.cache
     rm $out/share/icons/HighContrast/icon-theme.cache
     wrapProgram "$out/bin/baobab" \
       --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE" \
-      --prefix XDG_DATA_DIRS : "${gtk3}/share:${gnome3.gnome_themes_standard}/share:$out/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
+      --prefix XDG_DATA_DIRS : "${gnome3.gnome_themes_standard}/share:$out/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
   '';
 
   meta = with stdenv.lib; {
