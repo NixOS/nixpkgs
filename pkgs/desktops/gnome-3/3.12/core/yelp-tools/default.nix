9157babb2edc9cc9c28001e40fda7f5424f83f5f
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 23:55:16 +0200

    yelp-tools: add 3.12.1 version

--- pkgs/desktops/gnome-3/3.12/core/yelp-tools/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/yelp-tools/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, libxml2, libxslt, itstool, gnome3, pkgconfig }:
 
 stdenv.mkDerivation rec {
-  name = "yelp-tools-3.10.0";
+  name = "yelp-tools-3.12.1";
 
   src = fetchurl {
-    url = "https://download.gnome.org/sources/yelp-tools/3.10/${name}.tar.xz";
-    sha256 = "0496xyx1657db22ks3k92al64fp6236y5bgh7s7b0j8hcc112ppz";
+    url = "mirror://gnome/sources/yelp-tools/3.12/${name}.tar.xz";
+    sha256 = "7a5370d7adbec3b6e6b7b5e7e5ed966cb99c797907a186b94b93c184e97f0172";
   };
 
   buildInputs = [ libxml2 libxslt itstool gnome3.yelp_xsl pkgconfig ];
