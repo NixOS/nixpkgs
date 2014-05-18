a7a457e78a7330713e976cd3da8be7cc1c6dff22
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 23:30:45 +0200

    gnome-backgrounds: add 3.12.2 version

--- pkgs/desktops/gnome-3/3.12/core/gnome-backgrounds/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/gnome-backgrounds/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, intltool }:
 
 stdenv.mkDerivation rec {
-  name = "gnome-backgrounds-3.10.1";
+  name = "gnome-backgrounds-3.12.2";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gnome-backgrounds/3.10/${name}.tar.xz";
-    sha256 = "11rv03m4hznpx0brf47hil04199z3jjvl1aq7q0lnill3yrffiyc";
+    url = "mirror://gnome/sources/gnome-backgrounds/3.12/${name}.tar.xz";
+    sha256 = "ac4d3e0fffc5991865ca748e728a1ab87f167400105250ce2195b03502427180";
   };
 
   nativeBuildInputs = [ pkgconfig intltool ];
