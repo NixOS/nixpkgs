02934df3ee7b7e75fdaf19b2d2874b350165756a
Luca Bruno <lucabru@src.gnome.org>
2014-05-19 00:05:04 +0200

    libzapojit: use gnome mirror

--- pkgs/desktops/gnome-3/3.12/core/libzapojit/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/libzapojit/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "libzapojit-0.0.3";
 
   src = fetchurl {
-    url = "http://ftp.acc.umu.se/pub/GNOME/core/3.10/3.10.2/sources/${name}.tar.xz";
+    url = "mirror://gnome/sources//3.10/${name}.tar.xz";
     sha256 = "0zn3s7ryjc3k1abj4k55dr2na844l451nrg9s6cvnnhh569zj99x";
   };
 
