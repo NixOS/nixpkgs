f160950ad24fe76639bc4f9a0befa0037ec3f302
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 23:25:15 +0200

    gnome-common: add 3.12.0 version

--- pkgs/desktops/gnome-3/3.12/core/gnome-common/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/gnome-common/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, which, autoconf, automake }:
 
 stdenv.mkDerivation rec {
-  name = "gnome-common-3.10.0";
+  name = "gnome-common-3.12.0";
 
   src = fetchurl {
-    url = "https://download.gnome.org/sources/gnome-common/3.10/${name}.tar.xz";
-    sha256 = "aed69474a671e046523827f73ba5e936d57235b661db97900db7356e1e03b0a3";
+    url = "https://download.gnome.org/sources/gnome-common/3.12/${name}.tar.xz";
+    sha256 = "18712bc2df6b2dd88a11b9f7f874096d1c0c6e7ebc9cfc0686ef963bd590e1d8";
   };
 
   patches = [(fetchurl {
