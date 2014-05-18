2246827de15138da95f8a287eaa4cf041b5e0b7b
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 23:58:13 +0200

    at-spi2-atk: add 2.12.1 version

--- pkgs/desktops/gnome-3/3.12/core/at-spi2-atk/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/at-spi2-atk/default.nix
@@ -3,13 +3,13 @@
 
 stdenv.mkDerivation rec {
   versionMajor = "2.12";
-  versionMinor = "0";
+  versionMinor = "1";
   moduleName   = "at-spi2-atk";
   name = "${moduleName}-${versionMajor}.${versionMinor}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${moduleName}/${versionMajor}/${name}.tar.xz";
-    sha256 = "0crn3k6qvn6fjvgm68dj5ska1ppfgmdkaia2db1jp0b9y74nfm1v";
+    sha256 = "5fa9c527bdec028e06797563cd52d49bcf06f638549df983424d88db89bb1336";
   };
 
   buildInputs = [ python pkgconfig popt atk libX11 libICE xlibs.libXtst libXi
