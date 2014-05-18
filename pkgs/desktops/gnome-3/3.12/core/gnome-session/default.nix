b6286bf481176e700735e697353a4d10f56cb00d
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 23:23:21 +0200

    gnome-session: add 3.12.1 version

--- pkgs/desktops/gnome-3/3.12/core/gnome-session/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/gnome-session/default.nix
@@ -1,20 +1,20 @@
 { fetchurl, stdenv, pkgconfig, gnome3, glib, dbus_glib, json_glib, upower
-, libxslt, intltool, makeWrapper, systemd }:
+, libxslt, intltool, makeWrapper, systemd, xorg }:
 
 
 stdenv.mkDerivation rec {
-  name = "gnome-session-3.10.1";
+  name = "gnome-session-3.12.1";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gnome-session/3.10/${name}.tar.xz";
-    sha256 = "1k59yss7r748nvr0cdjrqmx0zy26b93rfn66lsdg9fz60x77087n";
+    url = "mirror://gnome/sources/gnome-session/3.12/${name}.tar.xz";
+    sha256 = "fa308771ac18bc5f77e5a5be3b2d93df1625168cb40167c1dfa898e9006e25d3";
   };
 
   configureFlags = "--enable-systemd";
 
   buildInputs = with gnome3;
     [ pkgconfig glib gnome_desktop gtk dbus_glib json_glib libxslt 
-      gnome3.gnome_settings_daemon
+      gnome3.gnome_settings_daemon xorg.xtrans
       gsettings_desktop_schemas upower intltool gconf makeWrapper systemd ];
 
   preFixup = ''
