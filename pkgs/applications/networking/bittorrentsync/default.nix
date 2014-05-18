1d24df4063fc3b543d158dd23260e569349602b4
Austin Seipp <aseipp@pobox.com>
2014-05-18 16:33:33 -0500

    btsync: fix my stupidity
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/applications/networking/bittorrentsync/default.nix
+++ pkgs/applications/networking/bittorrentsync/default.nix
@@ -5,8 +5,8 @@ let
     else if stdenv.system == "i686-linux" then "i386"
     else throw "Bittorrent Sync for: ${stdenv.system} not supported!";
 
-  sha256 = if stdenv.system == "x86_64-linux" then "1xba0wv45w4kg84i5sywkfk44vj9wxkr7f1vga2mpz86j8vq25iy";
-    else if stdenv.system == "i686-linux" then "10ml1wclix9236zcb4mh60hdm9vvfaarlb62z7fdrwk6ikqbm7fk";
+  sha256 = if stdenv.system == "x86_64-linux" then "1xba0wv45w4kg84i5sywkfk44vj9wxkr7f1vga2mpz86j8vq25iy"
+    else if stdenv.system == "i686-linux" then "10ml1wclix9236zcb4mh60hdm9vvfaarlb62z7fdrwk6ikqbm7fk"
     else throw "Bittorrent Sync for: ${stdenv.system} not supported!";
 
   libPath = stdenv.lib.makeLibraryPath [ stdenv.gcc.libc ];
