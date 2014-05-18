3bf7226d8c94e2be08da189da1786ddfc3682b5b
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 23:23:59 +0200

    gnome-shell: add 3.12.2 version

--- pkgs/desktops/gnome-3/3.12/core/gnome-shell/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/gnome-shell/default.nix
@@ -7,11 +7,11 @@
 # http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/gnome-base/gnome-shell/gnome-shell-3.10.2.1.ebuild?revision=1.3&view=markup
 
 stdenv.mkDerivation rec {
-  name = "gnome-shell-3.10.2.1";
+  name = "gnome-shell-3.12.2";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gnome-shell/3.10/${name}.tar.xz";
-    sha256 = "0k642y6h878v6mczx4z1zj4pjl7z4bvq02raxxwxkjyvyz2fv36j";
+    url = "mirror://gnome/sources/gnome-shell/3.12/${name}.tar.xz";
+    sha256 = "3ae230e8cb7a31e7b782c16ca178af5957858810788e26a6d630b69b3f85ce71";
   };
 
   buildInputs = with gnome3;
@@ -21,6 +21,8 @@ stdenv.mkDerivation rec {
       libXtst p11_kit networkmanagerapplet gjs mutter pulseaudio caribou evolution_data_server
       libical libtool nss gobjectIntrospection gtk gstreamer makeWrapper gdm gnome_control_center
       at_spi2_core upower ibus gnome_session gnome_desktop telepathy_logger gnome3.gnome_settings_daemon ];
+
+  installFlags = [ "keysdir=$(out)/share/gnome-control-center/keybindings" ];
 
   preBuild = ''
     patchShebangs src/data-to-c.pl
@@ -33,13 +35,11 @@ stdenv.mkDerivation rec {
       --prefix GI_TYPELIB_PATH : "$GI_TYPELIB_PATH" \
       --prefix LD_LIBRARY_PATH : "${accountservice}/lib:${ibus}/lib:${gdm}/lib" \
       --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE" \
-      --prefix XDG_DATA_DIRS : "${gnome_themes_standard}/share:${gtk}/share:$out/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
+      --prefix XDG_DATA_DIRS : "${gnome_themes_standard}/share:$out/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
 
     wrapProgram "$out/libexec/gnome-shell-calendar-server" \
       --prefix XDG_DATA_DIRS : "${evolution_data_server}/share:$out/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
   '';
-
-  patches = [ ./fix_background_corruption.patch ];
 
   meta = with stdenv.lib; {
     platforms = platforms.linux;
