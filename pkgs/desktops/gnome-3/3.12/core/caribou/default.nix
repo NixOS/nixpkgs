8ca85043156cbef81558453ce1132dfee6a3a0cd
Luca Bruno <lucabru@src.gnome.org>
2014-05-18 23:40:53 +0200

    caribou: add 0.4.13 version

--- pkgs/desktops/gnome-3/3.12/core/caribou/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/caribou/default.nix
@@ -3,11 +3,11 @@
 
 
 stdenv.mkDerivation rec {
-  name = "caribou-0.4.12";
+  name = "caribou-0.4.13";
 
   src = fetchurl {
     url = "mirror://gnome/sources/caribou/0.4/${name}.tar.xz";
-    sha256 = "0235sws58rg0kadxbp2nq5ha76zmhd4mr10n9qlbryf8p78qsvii";
+    sha256 = "953ba618621fda8a828d0d797fc916dbe35990dc01d7aa99d15e5e2241ee2782";
   };
 
   buildInputs = with gnome3;
