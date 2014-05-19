23ccdd979aafafc7443dc844d4a15e3392ef3bc9
Luca Bruno <lethalman88@gmail.com>
2014-05-19 22:06:18 +0200

    gnome-terminal: disable migration from gconf to gsettings

--- pkgs/desktops/gnome-3/3.12/core/gnome-terminal/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/gnome-terminal/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, pkgconfig, cairo, libxml2, gnome3, pango
-, gnome_doc_utils, intltool, libX11, which, gconf, libuuid
+, gnome_doc_utils, intltool, libX11, which, libuuid
 , desktop_file_utils, itstool, ncurses, makeWrapper, appdata-tools }:
 
 stdenv.mkDerivation rec {
@@ -15,15 +15,15 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [ gnome3.gtk gnome3.gsettings_desktop_schemas gnome3.vte appdata-tools
-                  gnome3.dconf gnome3.gconf itstool ncurses makeWrapper ];
+                  gnome3.dconf itstool ncurses makeWrapper ];
 
   nativeBuildInputs = [ pkgconfig intltool gnome_doc_utils which libuuid libxml2 desktop_file_utils ];
 
   # FIXME: enable for gnome3
-  configureFlags = [ "--disable-search-provider" "--without-nautilus-extension" ];
+  configureFlags = [ "--disable-search-provider" "--without-nautilus-extension" "--disable-migration" ];
 
   preFixup = ''
-    for f in "$out/libexec/gnome-terminal-migration" "$out/libexec/gnome-terminal-server"; do
+    for f in "$out/libexec/gnome-terminal-server"; do
       wrapProgram "$f" --prefix XDG_DATA_DIRS : "$out/share:$GSETTINGS_SCHEMAS_PATH"
     done
   '';
