c4ccc4afbc745e2ea6f6b22f21463830ef8efc7c
Luca Bruno <lethalman88@gmail.com>
2014-05-19 18:07:52 +0200

    bijiben: add 3.12.2 version

--- pkgs/desktops/gnome-3/3.12/apps/bijiben/default.nix
+++ pkgs/desktops/gnome-3/3.12/apps/bijiben/default.nix
@@ -1,14 +1,14 @@
 { stdenv, intltool, fetchurl, pkgconfig, glib
-, hicolor_icon_theme, makeWrapper, itstool
+, hicolor_icon_theme, makeWrapper, itstool, desktop_file_utils
 , clutter_gtk, libuuid, webkitgtk, zeitgeist
 , gnome3, librsvg, gdk_pixbuf, libxml2 }:
 
 stdenv.mkDerivation rec {
-  name = "bijiben-3.10.2";
+  name = "bijiben-3.12.2";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/bijiben/3.10/${name}.tar.xz";
-    sha256 = "81257f85218968b0ad386da6e1143586de478870ca74bb5387646a479999a7d4";
+    url = "mirror://gnome/sources/bijiben/3.12/${name}.tar.xz";
+    sha256 = "f319ef2a5b69ff9368e7488a28453da0f10eaa39a0f8e5d74623d0c07c824708";
   };
 
   doCheck = true;
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ pkgconfig glib intltool itstool libxml2
                   clutter_gtk libuuid webkitgtk gnome3.tracker
-                  gnome3.gnome_online_accounts zeitgeist
+                  gnome3.gnome_online_accounts zeitgeist desktop_file_utils
                   gnome3.gsettings_desktop_schemas makeWrapper
                   gdk_pixbuf gnome3.gnome_icon_theme librsvg
                   hicolor_icon_theme gnome3.gnome_icon_theme_symbolic ];
