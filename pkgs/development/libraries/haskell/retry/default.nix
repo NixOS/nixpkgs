a053a0cca726d140d92b6129f40d9d702a01d973
Peter Simons <simons@cryp.to>
2014-05-19 13:56:30 +0200

    haskell-retry: jailbreak to fix build with recent versions of 'exceptions'

--- pkgs/development/libraries/haskell/retry/default.nix
+++ pkgs/development/libraries/haskell/retry/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.4";
   sha256 = "16njq924b5n7jyfc059dbypp529gqlc9qnzd7wjk4m7dpm5bww67";
   buildDepends = [ dataDefault exceptions transformers ];
+  jailbreak = true;
   meta = {
     homepage = "http://github.com/Soostone/retry";
     description = "Retry combinators for monadic actions that may fail";
