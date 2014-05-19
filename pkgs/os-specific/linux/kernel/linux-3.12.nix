eb6309c87f020036ddac0d1d183f2c919e901eb7
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-05-19 16:03:37 +0200

    linux: Update to 3.12.20 (cherry picked from commit 2ee6c0c63e381c2afb3540261a353a7094fcf659)

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -1,15 +1,13 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.12.19";
+  version = "3.12.20";
   extraMeta.branch = "3.12";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1f59lyyyc4anaj50dzb9448gz6n9fdrcy2mvd7f193pp6018gdp6";
+    sha256 = "15pjvlqbbyif8s8mcpb7wf20fvbczhfp65mn6fd609j1p79nvqb9";
   };
-
-  kernelPatches = args.kernelPatches ++ [ { name = "cve-2014-0196"; patch = ./cve-2014-0196.patch; } ];
 
   features.iwlwifi = true;
   features.efiBootStub = true;
