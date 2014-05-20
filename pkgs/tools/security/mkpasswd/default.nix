d1a387e0ea60aa61605d2f9f4192ddc3809e6e61
Jaka Hudoklin <jakahudoklin@gmail.com>
2014-05-20 12:18:29 +0200

    mkpasswd: update to 5.1.2 and fix url

--- pkgs/tools/security/mkpasswd/default.nix
+++ pkgs/tools/security/mkpasswd/default.nix
@@ -1,15 +1,17 @@
-{ stdenv, fetchurl
+{ stdenv, fetchurl, perl
 }:
   
 stdenv.mkDerivation rec {
   name = "mkpasswd-${version}";
 
-  version = "5.1.1";
+  version = "5.1.2";
 
   src = fetchurl {
     url = "http://ftp.debian.org/debian/pool/main/w/whois/whois_${version}.tar.xz";
-    sha256 = "0i06a9mb9qcq272782mg6dffv3k7bqkw4cdr31yrc0s6jqylryv9";
+    sha256 = "00sl19b1p8l3gvdrx8lwvib0pcpfw8v0n8crw9lsabvp1s8pc7q2";
   };
+
+  buildInputs = [ perl ];
 
   preConfigure = ''
     substituteInPlace Makefile --replace "prefix = /usr" "prefix = $out"
