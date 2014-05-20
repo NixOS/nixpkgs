22f78aa3f47d08050855716467e0b9b53daeee6a
Jaka Hudoklin <jakahudoklin@gmail.com>
2014-05-20 12:37:20 +0200

    Add cutter, TCP/IP Connection cutting

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -827,6 +827,8 @@ let
 
   curlftpfs = callPackage ../tools/filesystems/curlftpfs { };
 
+  cutter = callPackage ../tools/networking/cutter { };
+
   dadadodo = builderDefsPackage (import ../tools/text/dadadodo) { };
 
   daq = callPackage ../applications/networking/ids/daq { };
