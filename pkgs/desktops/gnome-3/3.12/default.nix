84cafa5c5d45570094b0d67cdab04268389cf506
Luca Bruno <lucabru@src.gnome.org>
2014-05-20 16:02:54 +0200

    Fix infinite recursion in gnome3.gnome3...

--- pkgs/desktops/gnome-3/3.12/default.nix
+++ pkgs/desktops/gnome-3/3.12/default.nix
@@ -2,7 +2,7 @@
 
 rec {
   inherit (pkgs) glib gtk2 gtk3 gnome2;
-  gnome3 = pkgs.gnome3_12;
+  gnome3 = pkgs.gnome3_12 // { recurseForDerivations = false; };
   clutter = pkgs.clutter_1_18;
   clutter_gtk = pkgs.clutter_gtk.override { inherit clutter; };
   clutter-gst = pkgs.clutter-gst.override { inherit clutter; };
