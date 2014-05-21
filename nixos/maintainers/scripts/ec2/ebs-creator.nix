c9763e20e5e793be5eef155601f59dd7639c7ba3
Rob Vermaas <rob.vermaas@gmail.com>
2014-05-21 13:40:19 +0200

    Use HVM instance for EBS creator, guarantees everything is created in the correct zone.

--- nixos/maintainers/scripts/ec2/ebs-creator.nix
+++ nixos/maintainers/scripts/ec2/ebs-creator.nix
@@ -4,7 +4,7 @@
   machine =
     { config, pkgs, resources, ... }:
     { deployment.targetEnv = "ec2";
-      deployment.ec2.instanceType = "m1.large";
+      deployment.ec2.instanceType = "c3.large";
       deployment.ec2.securityGroups = [ "admin" ];
       deployment.ec2.ebsBoot = false;
       deployment.ec2.keyPair = resources.ec2KeyPairs.keypair.name;
