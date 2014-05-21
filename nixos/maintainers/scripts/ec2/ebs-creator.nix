044a24e58bcf4cf48df02df936c542839fb08d90
Rob Vermaas <rob.vermaas@gmail.com>
2014-05-21 16:31:52 +0200

    Better support for HVM instances. Now the NixOS images can be used on HVM instances without needing nixops. Previously the grub setup was incorrect, so a plain 'nixos-rebuild switch' and a reboot would result in a broken system.
    Also added growing of the partition of the root disk in the initrd,
    so you can run resize2fs after initial boot, without needing an
    extra reboot. This is useful especially for nixops'
    deployment.ec2.ebsInitialRootDiskSize option.

--- nixos/maintainers/scripts/ec2/ebs-creator.nix
+++ nixos/maintainers/scripts/ec2/ebs-creator.nix
@@ -8,6 +8,7 @@
       deployment.ec2.securityGroups = [ "admin" ];
       deployment.ec2.ebsBoot = false;
       deployment.ec2.keyPair = resources.ec2KeyPairs.keypair.name;
+      deployment.ec2.zone = "us-east-1e";
       environment.systemPackages = [ pkgs.parted ];
     };
 }
