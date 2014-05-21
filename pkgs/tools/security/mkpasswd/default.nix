97d9d26a7b0da7cf8abea62c49342021f61f55e3
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-05-21 10:46:48 +0200

    Revert "fix mkpasswd: use the git repository"
    This reverts commit ce13d178562c585bf1e10eb15350465460976294 since it
    breaks evaluation:
    
    in job ‘nixpkgs’:
    anonymous function at /nix/store/f73d4b03nsmccb1w5b1qfgc7dpvx35gw-git-export/pkgs/tools/security/mkpasswd/default.nix:1:1 called without required argument `fetchFromGitHub', at /nix/store/f73d4b03nsmccb1w5b1qfgc7dpvx35gw-git-export/lib/customisation.nix:58:12

--- pkgs/tools/security/mkpasswd/default.nix
+++ pkgs/tools/security/mkpasswd/default.nix
@@ -1,15 +1,14 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv, fetchurl
+}:
   
 stdenv.mkDerivation rec {
   name = "mkpasswd-${version}";
 
   version = "5.1.1";
 
-  src = fetchFromGitHub {
-    owner = "rfc1036";
-    repo = "whois";
-    rev = "v${version}";
-    sha256 = "026x8byx8pcpkdxca64368p0nlspk4phw18jg4p04di6cg6nc1m5";
+  src = fetchurl {
+    url = "http://ftp.debian.org/debian/pool/main/w/whois/whois_${version}.tar.xz";
+    sha256 = "0i06a9mb9qcq272782mg6dffv3k7bqkw4cdr31yrc0s6jqylryv9";
   };
 
   preConfigure = ''
@@ -20,12 +19,10 @@ stdenv.mkDerivation rec {
 
   installPhase = "make install-mkpasswd";
 
-  meta = with stdenv.lib; {
-    homepage = http://packages.qa.debian.org/w/whois.html;
+  meta = {
+    homepage = http://ftp.debian.org/debian/pool/main/w/whois/;
     description = ''
-      Overfeatured front end to crypt, from the Debian whois package
+      Overfeatured front end to crypt, from the Debian whois package.
     '';
-    license     = licenses.gpl2;
-    maintainers = with maintainers; [ cstrahan ];
   };
 }
