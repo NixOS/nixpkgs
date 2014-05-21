e135e6b35b4ac75cfee7c561b2ea864967568f81
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-05-21 11:20:33 +0200

    nixos-install: Copy /etc/hosts
    http://hydra.nixos.org/build/11130069

--- nixos/modules/installer/tools/nixos-install.sh
+++ nixos/modules/installer/tools/nixos-install.sh
@@ -80,7 +80,7 @@ mount -t tmpfs -o "mode=0755" none $mountPoint/run
 mount -t tmpfs -o "mode=0755" none $mountPoint/var/setuid-wrappers
 rm -rf $mountPoint/var/run
 ln -s /run $mountPoint/var/run
-cp -f /etc/resolv.conf $mountPoint/etc/resolv.conf
+cp -f /etc/resolv.conf /etc/hosts $mountPoint/etc/
 
 
 if [ -n "$runChroot" ]; then
