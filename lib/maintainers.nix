0f51ae90b93e8b5555e9077aa5e473ac6fa227f8
Luca Bruno <lethalman88@gmail.com>
2014-05-21 15:57:34 +0200

    Revert "typescript: add package. Closes #2712"
    This reverts commit 93a7cf74b938598447343c49d80392c2ca607cba.

--- lib/maintainers.nix
+++ lib/maintainers.nix
@@ -43,7 +43,6 @@
   iElectric = "Domen Kozar <domen@dev.si>";
   iyzsong = "Song Wenwu <iyzsong@gmail.com>";
   jcumming = "Jack Cummings <jack@mudshark.org>";
-  joamaki = "Jussi Maki <joamaki@gmail.com>";
   jwiegley = "John Wiegley <johnw@newartisans.com>";
   kkallio = "Karn Kallio <tierpluspluslists@gmail.com>";
   ktosiek = "Tomasz Kontusz <tomasz.kontusz@gmail.com>";
