3f0a45e88fb3b7e1cdf452f23b4bdb3658501d59
Peter Simons <simons@cryp.to>
2014-05-23 17:58:48 +0200

    haskell-codex: update to version 0.0.2

--- pkgs/development/tools/haskell/codex/default.nix
+++ pkgs/development/tools/haskell/codex/default.nix
@@ -1,21 +1,21 @@
-{ cabal, Cabal_1_20_0_0, downloadCurl, either, filepath, hackageDb, MissingH
+{ cabal, Cabal, downloadCurl, either, filepath, hackageDb, MissingH
 , monadLoops, tar, text, transformers, yaml, zlib
 }:
 
 cabal.mkDerivation (self: {
   pname = "codex";
-  version = "0.0.1.6";
-  sha256 = "0yr3qygxxj4k8ld7jh1h87jjawvixbz5wjssdxd5ix7askmm1dx2";
+  version = "0.0.2";
+  sha256 = "156830krsn1qczrx27bn3ihqlis698sjf563sa2njvc7v85plx55";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
-    Cabal_1_20_0_0 downloadCurl either filepath hackageDb MissingH 
-    monadLoops tar text transformers yaml zlib
+    Cabal downloadCurl either filepath hackageDb MissingH monadLoops
+    tar text transformers yaml zlib
   ];
   meta = {
     homepage = "http://github.com/aloiscochard/codex";
     description = "A ctags file generator for cabal project dependencies";
-    license = "unknown";
+    license = self.stdenv.lib.licenses.asl20;
     platforms = self.ghc.meta.platforms;
   };
 })
--- pkgs/top-level/haskell-defaults.nix
+++ pkgs/top-level/haskell-defaults.nix
@@ -21,6 +21,7 @@
 
   ghc782Prefs = self : super : ghcHEADPrefs self super // {
     cabalInstall_1_20_0_1 = super.cabalInstall_1_20_0_1.override { Cabal = self.Cabal_1_20_0_0; };
+    codex = super.codex.override { hackageDb = super.hackageDb.override { Cabal = self.Cabal_1_20_0_0; }; };
   };
 
   ghc763Prefs = self : super : ghc782Prefs self super // {
