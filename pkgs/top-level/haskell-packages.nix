363b35ac2c43ebf0cc5583904ca9bdac21057518
Peter Simons <simons@cryp.to>
2014-05-23 16:01:17 +0200

    haskell-tls: update to version 1.2.8.nix

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -2187,8 +2187,8 @@ self : let callPackage = x : y : modifyPrio (newScope self x y); in
   timeCompat = callPackage ../development/libraries/haskell/time-compat {};
 
   tls_1_1_5 = callPackage ../development/libraries/haskell/tls/1.1.5.nix {};
-  tls_1_2_7 = callPackage ../development/libraries/haskell/tls/1.2.7.nix {};
-  tls = self.tls_1_2_7;
+  tls_1_2_8 = callPackage ../development/libraries/haskell/tls/1.2.8.nix {};
+  tls = self.tls_1_2_8;
 
   tlsExtra = callPackage ../development/libraries/haskell/tls-extra {
     tls = self.tls_1_1_5;
