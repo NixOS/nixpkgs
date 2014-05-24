4c459974235f5230cdb3a39ff4ee4a0530a7888a
Arseniy Seroka <ars.seroka@gmail.com>
2014-05-24 22:34:27 +0400

    update moc

--- pkgs/applications/audio/moc/default.nix
+++ pkgs/applications/audio/moc/default.nix
@@ -1,17 +1,17 @@
-{ stdenv, fetchurl, ncurses, pkgconfig, alsaLib, flac, libmad, speex, ffmpeg_0_10, libvorbis, mpc, libsndfile, jackaudio, db, libmodplug, timidity, libid3tag, libtool }:
+{ stdenv, fetchurl, ncurses, pkgconfig, alsaLib, flac, libmad, speex, ffmpeg, libvorbis, mpc, libsndfile, jackaudio, db, libmodplug, timidity, libid3tag, libtool }:
 
 stdenv.mkDerivation rec {
   name = "moc-${version}";
-  version = "2.5.0-beta1";
+  version = "2.5.0-beta2";
 
   src = fetchurl {
     url = "http://ftp.daper.net/pub/soft/moc/unstable/moc-${version}.tar.bz2";
-    sha256 = "076816da9c6d1e61a386a1dda5f63ee2fc84bc31e9011ef70acc1d391d4c46a6";
+    sha256 = "486d50584c3fb0067b8c03af54e44351633a7740b18dc3b7358322051467034c";
   };
 
   configurePhase = "./configure prefix=$out";
 
-  buildInputs = [ ncurses pkgconfig alsaLib flac libmad speex ffmpeg_0_10 libvorbis mpc libsndfile jackaudio db libmodplug timidity libid3tag libtool ];
+  buildInputs = [ ncurses pkgconfig alsaLib flac libmad speex ffmpeg libvorbis mpc libsndfile jackaudio db libmodplug timidity libid3tag libtool ];
 
   meta = {
     description = "MOC (music on console) is a console audio player for LINUX/UNIX designed to be powerful and easy to use.";
