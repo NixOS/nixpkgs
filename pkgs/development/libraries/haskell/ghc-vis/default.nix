e08c94e8044d2f31a6459649db0c3e16fc54eb8a
Peter Simons <simons@cryp.to>
2014-05-24 10:55:28 +0200

    haskell-ghc-vis: update to version 0.7.2.2

--- pkgs/development/libraries/haskell/ghc-vis/default.nix
+++ pkgs/development/libraries/haskell/ghc-vis/default.nix
@@ -4,17 +4,17 @@
 
 cabal.mkDerivation (self: {
   pname = "ghc-vis";
-  version = "0.7.2.1";
-  sha256 = "160b45bgiz2ckva70dnmkf6i9bvffaavm5wzia2lk8c65pnvc9ih";
+  version = "0.7.2.2";
+  sha256 = "0abk76cy9qiblyways1r7jfsj996sj4laawzaz1j9p546plfkbnj";
   buildDepends = [
     cairo deepseq fgl ghcHeapView graphviz gtk mtl svgcairo text
     transformers xdot
   ];
+  jailbreak = true;
   postInstall = ''
     ensureDir "$out/share/ghci"
     ln -s "$out/share/$pname-$version/ghci" "$out/share/ghci/$pname"
   '';
-  jailbreak = true;
   meta = {
     homepage = "http://felsin9.de/nnis/ghc-vis";
     description = "Live visualization of data structures in GHCi";
