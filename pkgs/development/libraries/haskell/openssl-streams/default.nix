9f6469a1a1040a5af816f16b715cf711750f7a1d
Peter Simons <simons@cryp.to>
2014-05-24 10:55:31 +0200

    haskell-openssl-streams: update to version 1.1.0.2

--- pkgs/development/libraries/haskell/openssl-streams/default.nix
+++ pkgs/development/libraries/haskell/openssl-streams/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "openssl-streams";
-  version = "1.1.0.1";
-  sha256 = "1y3vj17951gz3wlf75j3ph3hlkjbrplrmnbd9rcyiipzv51vy8ym";
+  version = "1.1.0.2";
+  sha256 = "0h3jxxdls0p1xxr02rfag7j9y13ll3xgzx2ldv1nsfcv3rzw2pfy";
   buildDepends = [ HsOpenSSL ioStreams network ];
   testDepends = [
     HsOpenSSL HUnit ioStreams network testFramework testFrameworkHunit
