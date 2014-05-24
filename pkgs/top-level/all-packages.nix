956a01298828079012249c549773c19a89393260
AndersonTorres <torres.anderson.85@gmail.com>
2014-05-24 13:19:47 -0300

    Galculator: add version 2.3.1 Galculator is a GTK full-fledged calculator.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1070,6 +1070,10 @@ let
 
   g500-control = callPackage ../tools/misc/g500-control { };
 
+  galculator = callPackage ../applications/misc/galculator {
+    gtk = gtk3;
+  };
+
   gawk = callPackage ../tools/text/gawk { };
 
   gawkInteractive = appendToName "interactive"
