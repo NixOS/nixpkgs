a089dbfe8abdbd293652520ff002aff0bdaf85b8
Rob Vermaas <rob.vermaas@gmail.com>
2014-05-29 09:06:13 +0200

    Fix regression in fetchhg.

--- pkgs/build-support/fetchhg/builder.sh
+++ pkgs/build-support/fetchhg/builder.sh
@@ -1,8 +1,7 @@
 source $stdenv/setup
-
 header "getting $url${tag:+ ($tag)} into $out"
 
-hg clone --insecure ${tag:+-r "$tag"} "$url" hg-clone
+hg clone --insecure "$url" hg-clone
 
 hg archive -q -y ${tag:+-r "$tag"} --cwd hg-clone $out
 rm -f $out/.hg_archival.txt
