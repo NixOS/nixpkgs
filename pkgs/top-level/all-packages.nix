643d2af2cdde564dfa2908ecaeb7c66fc402ea5e
John Wiegley <johnw@newartisans.com>
2014-05-31 20:37:35 +0000

    emacs24: clangStdenv is in fact needed on darwin

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8200,6 +8200,11 @@ let
     librsvg = null;
     alsaLib = null;
     imagemagick = null;
+
+    # resolve unrecognized section __static_data in __DATA segment
+    stdenv = if stdenv.isDarwin
+      then clangStdenv
+      else stdenv;
   };
 
   emacs24-nox = lowPrio (appendToName "nox" (emacs24.override {
