693219f1b771b6b470b1a4a0ce367c562688af6c
Rok Garbas <rok@garbas.si>
2014-06-01 14:37:08 +0100

    weechatDevel: update to latest (few nice bugfixes)

--- pkgs/applications/networking/irc/weechat/devel.nix
+++ pkgs/applications/networking/irc/weechat/devel.nix
@@ -3,14 +3,14 @@
 , pythonPackages, cacert, cmake, makeWrapper }:
 
 stdenv.mkDerivation rec {
-  rev = "02eed97d977567b0fb16cfeeaeebb863eab1e509";
+  rev = "70d2b0ad8eb7854932d24c007a05b8627f751205";
   version = "1.0-rev${rev}";
   name = "weechat-${version}";
 
   src = fetchgit {
     inherit rev;
     url = "git://github.com/weechat/weechat.git";
-    sha256 = "0nk0p9chvn0h2pzq9793k2dz8h5iplz0zwqzyds55fbmsgzz51g2";
+    sha256 = "1k7kcrq46anq87r14sq3vrhij8acn6id7zxyhn0qnjj2wqgrjp5i";
   };
 
   buildInputs = 
