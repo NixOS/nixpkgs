27e2e4132bc00531a8477463e0b12e13c78bce16
Malcolm Matalka <mmatalka@gmail.com>
2014-06-01 15:13:04 +0200

    Add HaXe 3.1.3

--- pkgs/development/compilers/haxe/default.nix
+++ pkgs/development/compilers/haxe/default.nix
@@ -1,30 +1,28 @@
-{ stdenv, fetchsvn, ocaml, zlib, neko }:
+{ stdenv, fetchgit, ocaml, zlib, neko }:
 
 stdenv.mkDerivation {
-  name = "haxe-3.00";
+  name = "haxe-3.1.3";
 
   buildInputs = [ocaml zlib neko];
 
-  src = fetchsvn {
-    url = "http://haxe.googlecode.com/svn/trunk";
-    sha256 = "0hg8qailhgrcdk7r4k9kmwfl9d9ds0vy0l7wbv5wdrrc34qzifm4";
-    rev = 6706;
+  src = fetchgit {
+    url = "https://github.com/HaxeFoundation/haxe.git";
+    sha256 = "1p4yja6flv2r04q9lcrjxia3f3fsmhi3d88s0lz0nf0r4m61bjz0";
+    fetchSubmodules = true;
+
+    # Tag 3.1.3
+    rev = "7be30670b2f1f9b6082499c8fb9e23c0a6df6c28";
   };
 
   prePatch = ''
     sed -i -e 's|com.class_path <- \[|&"'"$out/lib/haxe/std/"'";|' main.ml
   '';
 
-  postBuild = ''
-    find std/tools -name '*.n' -delete
-    rm -f std/tools/haxedoc/haxedoc std/tools/haxelib/haxelib
-  '';
-
   buildFlags = [ "all" "tools" ];
 
   installPhase = ''
     install -vd "$out/bin" "$out/lib/haxe/std"
-    install -vt "$out/bin" haxe haxelib haxedoc
+    install -vt "$out/bin" haxe haxelib
     cp -vr std "$out/lib/haxe"
   '';
 
