7f9484967c47fa3d87a66a3f72b3c9da53124812
Peter Simons <simons@cryp.to>
2014-06-03 11:36:04 +0200

    haskell-statestack: update to version 0.2.0.3

--- pkgs/development/libraries/haskell/statestack/default.nix
+++ pkgs/development/libraries/haskell/statestack/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "statestack";
-  version = "0.2";
-  sha256 = "0j1axjwlh270qy6nlvm0hbc8nbd1ggm7klkjv553qf1rprz4zc2d";
+  version = "0.2.0.3";
+  sha256 = "0w5vw8jmnwbfyma4a3ggdm9jvxf3c18kpwbvcmvr5szifaqv9sgx";
   buildDepends = [ mtl transformers ];
   meta = {
     description = "Simple State-like monad transformer with saveable and restorable state";
