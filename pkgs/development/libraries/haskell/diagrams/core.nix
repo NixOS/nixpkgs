951296da65d3fe32faf3539c26047f4790dae9cb
Peter Simons <simons@cryp.to>
2014-06-03 11:34:49 +0200

    haskell-diagrams-core: update to version 1.2

--- pkgs/development/libraries/haskell/diagrams/core.nix
+++ pkgs/development/libraries/haskell/diagrams/core.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "diagrams-core";
-  version = "1.1.0.3";
-  sha256 = "0kl4bc5mvly227rzalzy9q6ki321drcdfsjqriv3ac70qmcfqyma";
+  version = "1.2";
+  sha256 = "0qlkick9ppwaignh179w65gmiq5as1nsi1ymficdw1p0jldi5f51";
   buildDepends = [
     dualTree lens MemoTrie monoidExtras newtype semigroups vectorSpace
     vectorSpacePoints
