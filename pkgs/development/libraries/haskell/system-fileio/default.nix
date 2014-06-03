fbfbaea156ad6606313fba3e392b98a4a06a7935
Peter Simons <simons@cryp.to>
2014-06-03 11:36:10 +0200

    haskell-system-fileio: update to version 0.3.14

--- pkgs/development/libraries/haskell/system-fileio/default.nix
+++ pkgs/development/libraries/haskell/system-fileio/default.nix
@@ -1,10 +1,14 @@
-{ cabal, systemFilepath, text, time }:
+{ cabal, chell, systemFilepath, temporary, text, time, transformers
+}:
 
 cabal.mkDerivation (self: {
   pname = "system-fileio";
-  version = "0.3.13";
-  sha256 = "12xsxcg2jk63x8aiikj5gx1an794zdfxzkx1sjnr2qyqyirk311v";
+  version = "0.3.14";
+  sha256 = "1x5cricx2n1wwvdad4i3q8s3gb28a129v3kkj9rn9803xh43zh29";
   buildDepends = [ systemFilepath text time ];
+  testDepends = [
+    chell systemFilepath temporary text time transformers
+  ];
   meta = {
     homepage = "https://john-millikin.com/software/haskell-filesystem/";
     description = "Consistent filesystem interaction across GHC versions";
