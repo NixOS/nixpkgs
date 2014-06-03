145bcc77e78b62e518c27c5318d7b8f45861efce
Peter Simons <simons@cryp.to>
2014-06-03 11:36:26 +0200

    haskell-utf8-string: update to version 0.3.8

--- pkgs/development/libraries/haskell/utf8-string/default.nix
+++ pkgs/development/libraries/haskell/utf8-string/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "utf8-string";
-  version = "0.3.7";
-  sha256 = "1s59xsw1i311rpxb7arnd280pjqab5mrlfjmxbabknka8wqlnnvq";
+  version = "0.3.8";
+  sha256 = "1h29dn0scsfkhmkg14ywq9178lw40ah1r36w249zfzqr02y7qxc0";
   meta = {
     homepage = "http://github.com/glguy/utf8-string/";
     description = "Support for reading and writing UTF8 Strings";
