404e05c4e5ab9b22a7b3ddfcebe6956b5bab3e24
Peter Simons <simons@cryp.to>
2014-06-05 11:04:12 +0200

    haskell-vinyl: update to version 0.4

--- pkgs/development/libraries/haskell/vinyl/default.nix
+++ pkgs/development/libraries/haskell/vinyl/default.nix
@@ -1,10 +1,10 @@
-{ cabal, doctest, lens }:
+{ cabal, doctest, lens, singletons }:
 
 cabal.mkDerivation (self: {
   pname = "vinyl";
-  version = "0.3";
-  sha256 = "0wa7pdk5ds1zq0yy6lbzhpjglpnz56hg36iwma09b6jk2x71sf5r";
-  testDepends = [ doctest lens ];
+  version = "0.4";
+  sha256 = "16v13bd5dvm9axngx1pvm7bq9412f6awz0cggsif1z0dy2kjpwgb";
+  testDepends = [ doctest lens singletons ];
   meta = {
     description = "Extensible Records";
     license = self.stdenv.lib.licenses.mit;
