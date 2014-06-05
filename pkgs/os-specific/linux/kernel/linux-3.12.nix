246edc3df2ade0701aabc53e41d6c8c85f51ac24
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-06-05 12:54:37 +0200

    linux: Update to 3.12.21

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.12.20";
+  version = "3.12.21";
   extraMeta.branch = "3.12";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "15pjvlqbbyif8s8mcpb7wf20fvbczhfp65mn6fd609j1p79nvqb9";
+    sha256 = "1pr4gr48bgxz28h790b2w41b1fc41xffz3drflsnz5736pisrhg7";
   };
 
   features.iwlwifi = true;
