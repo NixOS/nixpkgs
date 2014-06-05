5062c9147e7ba8ca1711f09db75cb332c9475478
Peter Simons <simons@cryp.to>
2014-06-05 12:12:39 +0200

    haskell-arithmoi: jailbreak to fix build with transformers 0.4.x

--- pkgs/development/libraries/haskell/arithmoi/default.nix
+++ pkgs/development/libraries/haskell/arithmoi/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.4.1.0";
   sha256 = "1xmwxmvl9l1fa2sgr4ff7al8b5d5136h4fq9r05abj3nfnx1a0iq";
   buildDepends = [ mtl random ];
+  jailbreak = true;
   meta = {
     homepage = "https://bitbucket.org/dafis/arithmoi";
     description = "Efficient basic number-theoretic functions. Primes, powers, integer logarithms.";
