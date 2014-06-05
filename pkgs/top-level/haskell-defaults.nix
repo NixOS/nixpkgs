c7a18e326fb06f66c6c0e127e6f3c7e3ae29cd72
Peter Simons <simons@cryp.to>
2014-06-05 14:39:03 +0200

    Stick to transformers 0.3.x everywhere; the new versions breaks just about every other package that we have.

--- pkgs/top-level/haskell-defaults.nix
+++ pkgs/top-level/haskell-defaults.nix
@@ -42,10 +42,8 @@
     gloss = null;                       # requires base >= 4.7
     haddock = self.haddock_2_13_2;
     modularArithmetic = null;           # requires base >= 4.7
-    mtl = self.mtl_2_2_1;
     pipesBinary = super.pipesBinary.override { binary = self.binary_0_7_2_1; };
-    transformers = self.transformers_0_4_1_0; # core packagen in ghc > 7.6.x
-    transformersCompat = super.transformersCompat;
+    transformers = self.transformers_0_3_0_0; # core packagen in ghc > 7.6.x
     zipArchive = super.zipArchive_0_2_2_1;    # works without binary 0.7.x
   };
 
