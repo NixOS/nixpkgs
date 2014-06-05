8fd61a33452cf1ee186c18a4ae63d9e1b9b1848a
Peter Simons <simons@cryp.to>
2014-06-05 11:04:06 +0200

    haskell-timerep: update to version 1.0.3

--- pkgs/development/libraries/haskell/timerep/default.nix
+++ pkgs/development/libraries/haskell/timerep/default.nix
@@ -1,9 +1,10 @@
-{ cabal }:
+{ cabal, time }:
 
 cabal.mkDerivation (self: {
   pname = "timerep";
   version = "1.0.3";
   sha256 = "14lz8nzfy1j7snvifbwjkk1fjc8wy4jk67xk9n87r25v3cva3x0p";
+  buildDepends = [ time ];
   meta = {
     description = "Parse and display time according to some RFCs (RFC3339, RFC2822)";
     license = self.stdenv.lib.licenses.bsd3;
