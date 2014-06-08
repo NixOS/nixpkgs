276a5e3b888355e7135f6f0f0f6a505becfddea0
William A. Kennington III <william@wkennington.com>
2014-06-07 19:10:31 -0500

    kernel: 3.14.5 -> 3.14.6 (close #2868)
    (cherry picked from commit d91eacd7200214de1349a1738fabe18bd0289c8d)

--- pkgs/os-specific/linux/kernel/linux-3.14.nix
+++ pkgs/os-specific/linux/kernel/linux-3.14.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.14.5";
+  version = "3.14.6";
   extraMeta.branch = "3.14";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0x38fyj86hrw7ns14m4kdpscqlj7ig889mxzvwsbhrsk0khllvyn";
+    sha256 = "1wk3zdka9n49ixl2s7djavbk0b2nd0772gbmiaxi9mljah9inlhw";
   };
 
   features.iwlwifi = true;
