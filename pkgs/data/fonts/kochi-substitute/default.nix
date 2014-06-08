0672cfcc5f8bf556905555e111e184bb6df28e7b
Jonathan Glines <auntieNeo@gmail.com>
2014-06-08 11:47:37 -0600

    Added mirrors for sourceforge.jp.

--- pkgs/data/fonts/kochi-substitute/default.nix
+++ pkgs/data/fonts/kochi-substitute/default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation {
   name = "kochi-substitute-${version}";
 
   src = fetchurl {
-    url = "http://jaist.dl.sourceforge.jp/efont/5411/kochi-substitute-${version}.tar.bz2";
+    url = "mirror://sourceforgejp/efont/5411/kochi-substitute-${version}.tar.bz2";
     sha256 = "f4d69b24538833bf7e2c4de5e01713b3f1440960a6cc2a5993cb3c68cd23148c";
   };
 
