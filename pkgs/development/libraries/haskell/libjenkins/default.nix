d72b09eaa910f1062cbe03db2f787654b0b54ae7
Peter Simons <simons@cryp.to>
2014-06-09 09:25:07 +0200

    haskell-libjenkins: update to version 0.4.3.0

--- pkgs/development/libraries/haskell/libjenkins/default.nix
+++ pkgs/development/libraries/haskell/libjenkins/default.nix
@@ -1,24 +1,23 @@
 { cabal, async, conduit, doctest, filepath, free, hspec
 , hspecExpectationsLens, httpClient, httpConduit, httpTypes, lens
-, monadControl, network, text, transformers, xmlConduit
+, monadControl, network, resourcet, text, transformers, xmlConduit
 }:
 
 cabal.mkDerivation (self: {
   pname = "libjenkins";
-  version = "0.4.2.0";
-  sha256 = "11013klk2gvcaf2d2gmi0bf3jg2m82li19szqlwb325kdjmdf546";
-  patches = [ ./new-conduit.patch ];
+  version = "0.4.3.0";
+  sha256 = "18z1yaf1a1ncvflxzv96b35d44933yrmsmxv5dr87iyfry28qbnv";
   buildDepends = [
     async conduit free httpClient httpConduit httpTypes lens
-    monadControl network text transformers xmlConduit
+    monadControl network resourcet text transformers xmlConduit
   ];
   testDepends = [
     async conduit doctest filepath free hspec hspecExpectationsLens
-    httpClient httpConduit httpTypes lens monadControl network text
-    transformers xmlConduit
+    httpClient httpConduit httpTypes lens monadControl network
+    resourcet text transformers xmlConduit
   ];
-  doCheck = false;
   jailbreak = true;
+  doCheck = false;
   meta = {
     description = "Jenkins API interface";
     license = self.stdenv.lib.licenses.bsd3;
