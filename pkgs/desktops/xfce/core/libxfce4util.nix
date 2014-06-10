5c72dac5487df7c230eea7a4559db54ce32106ef
Vladimír Čunát <vcunat@gmail.com>
2014-06-10 15:53:24 +0200

    libxfce4util: minor update 4.10.0 -> 4.10.1

--- pkgs/desktops/xfce/core/libxfce4util.nix
+++ pkgs/desktops/xfce/core/libxfce4util.nix
@@ -3,11 +3,11 @@
 stdenv.mkDerivation rec {
   p_name  = "libxfce4util";
   ver_maj = "4.10";
-  ver_min = "0";
+  ver_min = "1";
 
   src = fetchurl {
     url = "mirror://xfce/src/xfce/${p_name}/${ver_maj}/${name}.tar.bz2";
-    sha256 = "13k0wwbbqvdmbj4xmk4nxdlgvrdgr5y6r3dk380mzfw053hzwy89";
+    sha256 = "1fygyq9dywa989z1vb3d8hj4fg5ai75lcrngnf2s60jwf6nx2b78";
   };
   name = "${p_name}-${ver_maj}.${ver_min}";
 
