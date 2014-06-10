e76486c66273e6f62412fae9814e1ab8924f9b12
Peter Simons <simons@cryp.to>
2014-06-10 20:33:20 +0200

    haskell-hourglass: update to version 0.2.1

--- pkgs/development/libraries/haskell/hourglass/default.nix
+++ pkgs/development/libraries/haskell/hourglass/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "hourglass";
-  version = "0.2.0";
-  sha256 = "13zphy3gfj9p7vsa477qy30968fnz5kq7d0lzb1pyg5hxkx44rim";
+  version = "0.2.1";
+  sha256 = "0xb5g10mm427yagv5hzg5wgxpjjr6s9my675ik8rh5k1is4zr500";
   buildDepends = [ deepseq ];
   testDepends = [
     deepseq mtl tasty tastyHunit tastyQuickcheck time
