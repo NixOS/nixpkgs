8bc8dd90af7a237c7518f8b5b3260b2e3bb2e62f
Peter Simons <simons@cryp.to>
2014-06-10 20:33:17 +0200

    haskell-event-list: update to version 0.1.1

--- pkgs/development/libraries/haskell/event-list/default.nix
+++ pkgs/development/libraries/haskell/event-list/default.nix
@@ -1,12 +1,14 @@
-{ cabal, nonNegative, QuickCheck, transformers, utilityHt }:
+{ cabal, nonNegative, QuickCheck, random, transformers, utilityHt
+}:
 
 cabal.mkDerivation (self: {
   pname = "event-list";
-  version = "0.1.0.2";
-  sha256 = "01j48871nijhkbqdsfvbvq01yr9b5a056fn03ccgazikfsd368ri";
-  isLibrary = true;
-  isExecutable = true;
+  version = "0.1.1";
+  sha256 = "1qv7a4np8cr0chbvlz0kmcrvc95w1z7fqs2pq0mkwx22hnslsh5l";
   buildDepends = [ nonNegative QuickCheck transformers utilityHt ];
+  testDepends = [
+    nonNegative QuickCheck random transformers utilityHt
+  ];
   meta = {
     homepage = "http://code.haskell.org/~thielema/event-list/";
     description = "Event lists with relative or absolute time stamps";
