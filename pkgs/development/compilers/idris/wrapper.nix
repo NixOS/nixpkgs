8d708ba35ab63d59e4b8b8be854968bb856e24c6
Mathijs Kwik <mathijs@bluescreen303.nl>
2014-06-11 10:38:01 +0200

    idris: provide boehmgc to compile with llvm backend

--- pkgs/development/compilers/idris/wrapper.nix
+++ pkgs/development/compilers/idris/wrapper.nix
@@ -1,10 +1,10 @@
-{ gmp, makeWrapper, gcc, runCommand, idris_plain}:
+{ gmp, makeWrapper, gcc, runCommand, idris_plain, boehmgc}:
 
 runCommand "idris-wrapper" {} ''
   source ${makeWrapper}/nix-support/setup-hook
   mkdir -p $out/bin
   ln -s ${idris_plain}/bin/idris $out/bin
       wrapProgram $out/bin/idris \
-        --suffix NIX_CFLAGS_COMPILE : '"-I${gmp}/include -L${gmp}/lib"' \
+        --suffix NIX_CFLAGS_COMPILE : '"-I${gmp}/include -L${gmp}/lib -L${boehmgc}/lib"' \
         --suffix PATH : ${gcc}/bin
 ''
