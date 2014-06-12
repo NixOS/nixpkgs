eeb816771046cf7ab961ae4c9223eac092fcfcb8
Emery Hemingway <emery@vfemail.net>
2014-06-12 11:59:08 -0400

    libtoxcore: audio video support

--- pkgs/development/libraries/libtoxcore/default.nix
+++ pkgs/development/libraries/libtoxcore/default.nix
@@ -1,4 +1,8 @@
-{ stdenv, fetchurl, autoconf, automake, libtool, libsodium }:
+{ stdenv, fetchurl, autoconf, automake, libtool, libsodium, check
+, pkgconfig, libopus, libvpx
+, avSupport ? true }:
+
+assert avSupport -> pkgconfig != null && libopus != null && libvpx != null;
 
 let
   version = "63f25f86d";
@@ -13,9 +17,18 @@ stdenv.mkDerivation rec {
     sha256 = "194fddqpv40w4yk0sqh5wlpgrm06jmxvgk5530ziahjpf1m5gcw6";
   };
 
+  buildInputs = [
+    autoconf automake libtool libsodium 
+  ] ++ stdenv.lib.optional avSupport [ pkgconfig libopus libvpx ]
+    ++ stdenv.lib.optional doCheck [ check ];
+
   preConfigure = "./autogen.sh";
 
-  buildInputs = [ autoconf automake libtool libsodium ];
+  # Explicitly link against libgcc_s, to work around the infamous
+  # "libgcc_s.so.1 must be installed for pthread_cancel to work".
+  LDFLAGS = stdenv.lib.optionalString doCheck "-lgcc_s";
+  doCheck = false;
+  # important tests pass, others inevitably fail due to the build environment
 
   meta = {
     description = "P2P FOSS instant messaging library aimed to replace Skype with crypto";
