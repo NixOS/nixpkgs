40ca1149c912325831b30b725af9ac775abe552e
Peter Simons <simons@cryp.to>
2014-06-12 15:55:37 +0200

    haskell-io-memoize: update to version 1.1.0.0

--- pkgs/development/libraries/haskell/io-memoize/default.nix
+++ pkgs/development/libraries/haskell/io-memoize/default.nix
@@ -1,10 +1,10 @@
-{ cabal, spawn }:
+{ cabal, async }:
 
 cabal.mkDerivation (self: {
   pname = "io-memoize";
-  version = "1.0.0.0";
-  sha256 = "1z6aimyg7wasaqmacpch7skfm9iyl7khd54lfmb8iwghyfvah5d0";
-  buildDepends = [ spawn ];
+  version = "1.1.0.0";
+  sha256 = "1xnrzrvs5c3lrzdxm4hrqbh8chl8sxv2j98b28na73w8b7yv2agm";
+  buildDepends = [ async ];
   meta = {
     description = "Memoize IO actions";
     license = self.stdenv.lib.licenses.bsd3;
