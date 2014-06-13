213312b667c2f7a62b2d5fe9f136a5177fb33c13
Michael Raskin <7c6f434c@mail.ru>
2014-06-14 00:23:00 +0400

    Update GlusterFS to 3.4.3

--- pkgs/tools/filesystems/glusterfs/default.nix
+++ pkgs/tools/filesystems/glusterfs/default.nix
@@ -3,11 +3,11 @@ let
   s = # Generated upstream information 
   rec {
     baseName="glusterfs";
-    version="3.4.2";
+    version="3.4.3";
     name="${baseName}-${version}";
     hash="1vzdihsy4da11jsa46n1n2xk6d40g7v0zrlqvs3pb9k07fql5kag";
-    url="http://download.gluster.org/pub/gluster/glusterfs/3.4/3.4.2/glusterfs-3.4.2.tar.gz";
-    sha256="1vzdihsy4da11jsa46n1n2xk6d40g7v0zrlqvs3pb9k07fql5kag";
+    url="http://download.gluster.org/pub/gluster/glusterfs/3.4/3.4.3/glusterfs-3.4.3.tar.gz";
+    sha256="0j1yvpdb1bydsh3pqlyr23mfvra5bap9rxba910s9cn61mpy99bj";
   };
   buildInputs = [
     fuse bison flex openssl python ncurses readline
