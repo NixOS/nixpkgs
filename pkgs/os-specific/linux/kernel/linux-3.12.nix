27c72f337b2d5e247a513a553473b00f93eb7871
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-06-13 17:44:02 +0200

    linux: Update to 3.12.22
    Fixes CVE-2014-3153 (local privilege escalation via futex()).

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.12.21";
+  version = "3.12.22";
   extraMeta.branch = "3.12";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1pr4gr48bgxz28h790b2w41b1fc41xffz3drflsnz5736pisrhg7";
+    sha256 = "0c9rw11iihjimqb6mdmwn4q67ds7jg4qibj5ijj5pdmkdqzj7g54";
   };
 
   features.iwlwifi = true;
