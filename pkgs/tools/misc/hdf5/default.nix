e9fd88dab8074b9d50e19770c8aba07c45e4ac7a
Andreas Herrmann <andreash87@gmx.ch>
2014-06-15 11:12:03 +0200

    hdf5: Add zlib as optional dependency
    This adds addition compression modes to the hdf5 library.
    
    I am adding this, because pytables depends on it.

--- pkgs/tools/misc/hdf5/default.nix
+++ pkgs/tools/misc/hdf5/default.nix
@@ -1,6 +1,7 @@
 
 { stdenv
 , fetchurl
+, zlib ? null
 }:
 stdenv.mkDerivation {
   name = "hdf5-1.8.10-patch1";
@@ -8,7 +9,9 @@ stdenv.mkDerivation {
     url = http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.10-patch1.tar.gz;
     sha256 = "08ad32fhnci6rdfn6mn3w9v1wcaxdcd326n3ljwkcq4dzhkh28qz";  			
   };
-  buildInputs = [] ;
+
+  buildInputs = []
+    ++ stdenv.lib.optional (zlib != null) zlib;
   
   patches = [./bin-mv.patch];
   
