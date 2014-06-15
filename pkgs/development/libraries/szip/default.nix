1569ae15cf8e52976790ab0e280400b483d107c2
Andreas Herrmann <andreash87@gmx.ch>
2014-06-15 13:25:43 +0200

    szip: Fix source hash
    The hash code of the downloaded source tar-ball was not identical to the one
    specified in the nix-expression. This commit fixes that problem.

--- pkgs/development/libraries/szip/default.nix
+++ pkgs/development/libraries/szip/default.nix
@@ -4,6 +4,6 @@ stdenv.mkDerivation {
     name = "szip-2.1";
     src = fetchurl {
         url = ftp://ftp.hdfgroup.org/lib-external/szip/2.1/src/szip-2.1.tar.gz;
-        sha256 = "05707lrdhwp8mv0dgzh2b6m2mwamv1z6k29m2v1v7pz0c1w2gb6z";
+        sha256 = "1vym7r4by02m0yqj10023xyps5b21ryymnxb4nb2gs32arfxj5m8";
     };
 }
