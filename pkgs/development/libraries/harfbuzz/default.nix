336e06813e6d0181882fd6cfef2a12d5e5145c00
Vladimír Čunát <vcunat@gmail.com>
2014-06-15 19:45:23 +0200

    harfbuzz: minor update 0.9.28 -> .29

--- pkgs/development/libraries/harfbuzz/default.nix
+++ pkgs/development/libraries/harfbuzz/default.nix
@@ -8,11 +8,11 @@
 # (icu is a ~30 MB dependency, the rest is very small in comparison)
 
 stdenv.mkDerivation rec {
-  name = "harfbuzz-0.9.28";
+  name = "harfbuzz-0.9.29";
 
   src = fetchurl {
     url = "http://www.freedesktop.org/software/harfbuzz/release/${name}.tar.bz2";
-    sha256 = "0j70ppl2qrfx8yzdl8ki63wcxgrlhkyl31p1a6bhmbqqh32zfrx5";
+    sha256 = "14739pvc6bvddnj4rplhjdgiqwfvyincchlhvmig637sqqwfl730";
   };
 
   configureFlags = [
