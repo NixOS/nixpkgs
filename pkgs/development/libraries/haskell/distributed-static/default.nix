861c1ca445ba43380acd0ef831c4e658d48edd1d
Peter Simons <simons@cryp.to>
2014-06-16 16:10:53 +0200

    haskell-distributed-static: update to version 0.3.0.0

--- pkgs/development/libraries/haskell/distributed-static/default.nix
+++ pkgs/development/libraries/haskell/distributed-static/default.nix
@@ -2,11 +2,11 @@
 
 cabal.mkDerivation (self: {
   pname = "distributed-static";
-  version = "0.2.1.1";
-  sha256 = "08y9554x6avjwdmbf33fw1pw1wl8qmgfngmgb6vgad88krnixq1h";
+  version = "0.3.0.0";
+  sha256 = "1g8jr01jxlfshsz3mrpzl21q290whwz4va3zjp1h0d1pnfwz6jcj";
   buildDepends = [ binary rank1dynamic ];
   meta = {
-    homepage = "http://www.github.com/haskell-distributed/distributed-process";
+    homepage = "http://haskell-distributed.github.com";
     description = "Compositional, type-safe, polymorphic static values and closures";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
