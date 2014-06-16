195800e8816a53d5faafde4a0990a904dbac2510
John Wiegley <johnw@newartisans.com>
2014-06-16 14:26:40 -0700

    Do not use static-libgcc flag on Darwin

--- pkgs/development/libraries/zlib/default.nix
+++ pkgs/development/libraries/zlib/default.nix
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
 
   # As zlib takes part in the stdenv building, we don't want references
   # to the bootstrap-tools libgcc (as uses to happen on arm/mips)
-  NIX_CFLAGS_COMPILE = "-static-libgcc";
+  NIX_CFLAGS_COMPILE = stdenv.lib.optionalString (!stdenv.isDarwin) "-static-libgcc";
 
   crossAttrs = {
     dontStrip = static;
