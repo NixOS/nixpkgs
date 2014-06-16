c5b7e2a2f2f26b735afd185b34e7acf0210efbb8
Peter Simons <simons@cryp.to>
2014-06-16 16:10:38 +0200

    haskell-git-annex: update to version 5.20140613

--- pkgs/applications/version-management/git-and-tools/git-annex/default.nix
+++ pkgs/applications/version-management/git-and-tools/git-annex/default.nix
@@ -9,33 +9,41 @@
 , optparseApplicative, perl, QuickCheck, random, regexTdfa, rsync
 , SafeSemaphore, securemem, SHA, shakespeare, stm, tasty
 , tastyHunit, tastyQuickcheck, tastyRerun, text, time, transformers
-, unixCompat, utf8String, uuid, wai, waiLogger, warp, warpTls
-, which, xmlTypes, yesod, yesodCore, yesodDefault, yesodForm
-, yesodStatic, fsnotify
+, unixCompat, utf8String, uuid, wai, waiExtra, warp, warpTls, which
+, xmlTypes, yesod, yesodCore, yesodDefault, yesodForm, yesodStatic
 }:
 
 cabal.mkDerivation (self: {
   pname = "git-annex";
-  version = "5.20140606";
-  sha256 = "1b9hslkdv82lf8njwzy51yj8dgg2wn7g08wy73lk7pnddfh8qjpy";
+  version = "5.20140613";
+  sha256 = "01khqy68w1rqxic9lqal7902cv89d10xvbzmvlsx99g70ljqfafi";
   isLibrary = false;
   isExecutable = true;
   buildDepends = [
     aeson async blazeBuilder bloomfilter byteable caseInsensitive
-    clientsession cryptoApi cryptohash dataDefault dataenc DAV
-    dlist dns editDistance exceptions extensibleExceptions
-    feed filepath gnutls hamlet hS3 hslogger HTTP httpClient
+    clientsession cryptoApi cryptohash dataDefault dataenc DAV dbus
+    dlist dns editDistance exceptions extensibleExceptions fdoNotify
+    feed filepath gnutls hamlet hinotify hS3 hslogger HTTP httpClient
     httpConduit httpTypes IfElse json liftedBase MissingH monadControl
     mtl network networkConduit networkInfo networkMulticast
     networkProtocolXmpp optparseApplicative QuickCheck random regexTdfa
     SafeSemaphore securemem SHA shakespeare stm tasty tastyHunit
     tastyQuickcheck tastyRerun text time transformers unixCompat
-    utf8String uuid wai waiLogger warp warpTls xmlTypes yesod yesodCore
+    utf8String uuid wai waiExtra warp warpTls xmlTypes yesod yesodCore
     yesodDefault yesodForm yesodStatic
-  ] ++ (if !self.stdenv.isDarwin
-        then [ dbus fdoNotify hinotify ] else [ fsnotify ]);
+  ];
   buildTools = [ bup curl git gnupg1 lsof openssh perl rsync which ];
-  configureFlags = "-fAssistant -fProduction";
+  configureFlags = "-fS3
+                    -fWebDAV
+                    -fInotify
+                    -fDbus
+                    -fAssistant
+                    -fWebapp
+                    -fPairing
+                    -fXMPP
+                    -fDNS
+                    -fProduction
+                    -fTDFA";
   preConfigure = ''
     export HOME="$NIX_BUILD_TOP/tmp"
     mkdir "$HOME"
