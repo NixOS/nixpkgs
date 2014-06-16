a161d100718329572f31a4f939f597d14b18a60b
Peter Simons <simons@cryp.to>
2014-06-16 16:11:20 +0200

    haskell-rank1dynamic: update to version 0.2.0.0

--- pkgs/development/libraries/haskell/rank1dynamic/default.nix
+++ pkgs/development/libraries/haskell/rank1dynamic/default.nix
@@ -2,11 +2,11 @@
 
 cabal.mkDerivation (self: {
   pname = "rank1dynamic";
-  version = "0.1.0.2";
-  sha256 = "1341hhbdm6y0mj0qjda0ckqsla51fxiy1yfpbwfvsmpi2bkzgxn6";
+  version = "0.2.0.0";
+  sha256 = "09p3lggnsn0355440d9cazwijv9qm4siw99gg2xkk2hamp2sj42h";
   buildDepends = [ binary ];
   meta = {
-    homepage = "http://github.com/haskell-distributed/distributed-process";
+    homepage = "http://haskell-distributed.github.com";
     description = "Like Data.Dynamic/Data.Typeable but with support for rank-1 polymorphic types";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
