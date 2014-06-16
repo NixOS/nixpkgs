6509d2df2a2f8480097359a8d2e5703052cae11d
Peter Simons <simons@cryp.to>
2014-06-16 16:11:18 +0200

    haskell-network-transport: update to version 0.4.0.0

--- pkgs/development/libraries/haskell/network-transport/default.nix
+++ pkgs/development/libraries/haskell/network-transport/default.nix
@@ -1,12 +1,12 @@
-{ cabal, binary, transformers }:
+{ cabal, binary, hashable, transformers }:
 
 cabal.mkDerivation (self: {
   pname = "network-transport";
-  version = "0.3.0.1";
-  sha256 = "1iijcd864znik83smk1bjidinm199wri5fdyrhnffj0n35knrvas";
-  buildDepends = [ binary transformers ];
+  version = "0.4.0.0";
+  sha256 = "1485w86wzszlg4dvl0fkr7wa47snvpw825llrvdgrrkcxamhsmrz";
+  buildDepends = [ binary hashable transformers ];
   meta = {
-    homepage = "http://github.com/haskell-distributed/distributed-process";
+    homepage = "http://haskell-distributed.github.com";
     description = "Network abstraction layer";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
