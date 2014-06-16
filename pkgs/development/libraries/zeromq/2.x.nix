6b27a2789ef949f80917d6b1a769c7651bd0fe1d
Paul Colomiets <paul@colomiets.name>
2014-06-16 22:25:56 +0300

    Upgrade zeromq and nanomsg

--- pkgs/development/libraries/zeromq/2.x.nix
+++ pkgs/development/libraries/zeromq/2.x.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, libuuid}:
 
 stdenv.mkDerivation rec {
-  name = "zeromq-2.1.10";
+  name = "zeromq-2.2.0";
 
   src = fetchurl {
     url = "http://download.zeromq.org/${name}.tar.gz";
-    sha256 = "0yabbbgx9ajpq0hjzqjm6rmj7pkcj95d5zn7d59b4wmm6kipwwn6";
+    sha256 = "0ds6c244wik1lyfrfr2f4sha30fly4xybr02r9r4z156kvmlm423";
   };
 
   buildInputs = [ libuuid ];
--- pkgs/development/libraries/zeromq/3.x.nix
+++ pkgs/development/libraries/zeromq/3.x.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, libuuid}:
 
 stdenv.mkDerivation rec {
-  name = "zeromq-3.2.1-rc2";
+  name = "zeromq-3.2.4";
 
   src = fetchurl {
     url = "http://download.zeromq.org/${name}.tar.gz";
-    sha256 = "b0a70da77e39537120a1fa058c49434982741ecef5211edcd7aeab4caffb82b7";
+    sha256 = "0n9gfhwgkwq08kvvgk5zxjga08v628ij5chddk5w4ravr10s35nz";
   };
 
   buildInputs = [ libuuid ];
