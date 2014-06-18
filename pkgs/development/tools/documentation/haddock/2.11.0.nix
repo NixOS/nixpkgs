048ec2575791b1d1022889b607865fea5d94902b
Thomas Tuegel <ttuegel@gmail.com>
2014-06-18 17:14:45 -0500

    Wrap haddock 2.10+ to find GHC packages

--- pkgs/development/tools/documentation/haddock/2.11.0.nix
+++ pkgs/development/tools/documentation/haddock/2.11.0.nix
@@ -1,4 +1,4 @@
-{ cabal, alex, Cabal, filepath, ghcPaths, happy, xhtml }:
+{ cabal, alex, Cabal, filepath, ghcPaths, happy, xhtml, makeWrapper }:
 
 cabal.mkDerivation (self: {
   pname = "haddock";
@@ -6,10 +6,15 @@ cabal.mkDerivation (self: {
   sha256 = "0a29n6y9lmk5w78f6j8s7pg0m0k3wm7bx5r2lhk7bnzkr5f7rkcd";
   isLibrary = true;
   isExecutable = true;
-  buildDepends = [ Cabal filepath ghcPaths xhtml ];
+  buildDepends = [ Cabal filepath ghcPaths xhtml makeWrapper ];
   testDepends = [ Cabal filepath ];
   buildTools = [ alex happy ];
   doCheck = false;
+
+  postInstall = ''
+   wrapProgram $out/bin/haddock --add-flags "\$(${self.ghc.GHCGetPackages} ${self.ghc.version} \"\$(dirname \$0)\" \"--optghc=-package-conf --optghc=\")"
+  '';
+
   meta = {
     homepage = "http://www.haskell.org/haddock/";
     description = "A documentation-generation tool for Haskell libraries";
