8a16a950ba066ce017349e139817dfb4ecc03efe
Thomas Bereknyei <tomberek@gmail.com>
2014-06-18 01:01:45 -0400

    Adds tcache-AWS

---
+++ pkgs/development/libraries/haskell/tcache-AWS/default.nix
@@ -0,0 +1,14 @@
+{ cabal, aws, conduit, httpConduit, network, TCache, text }:
+
+cabal.mkDerivation (self: {
+  pname = "tcache-AWS";
+  version = "0.0.1";
+  sha256 = "18hi8jvc117pxjhpb891hqlsbi4wvmd6nr3vwnqqr7rcw2dsmnwv";
+  buildDepends = [ aws conduit httpConduit network TCache text ];
+  meta = {
+    description = "tcache using Amazon Web Services as default persistence mechanism";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+  };
+  maintainers = [ self.stdenv.lib.maintainers.tomberek ];
+})
--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -2166,6 +2166,8 @@ self : let callPackage = x : y : modifyPrio (newScope self x y); in
 
   TCache = callPackage ../development/libraries/haskell/TCache {};
 
+  tcacheAWS = callPackage ../development/libraries/haskell/tcache-AWS {};
+
   templateDefault = callPackage ../development/libraries/haskell/template-default {};
 
   temporary = callPackage ../development/libraries/haskell/temporary {};
