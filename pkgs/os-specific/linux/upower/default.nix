36e79e3b90b9fe5bdd6b9f5e966c33d6d8547281
Vladimír Čunát <vcunat@gmail.com>
2014-06-19 20:51:30 +0200

    upower: pull patches used in Ubuntu/trusty (fix #2884)

--- pkgs/os-specific/linux/upower/default.nix
+++ pkgs/os-specific/linux/upower/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, glib, dbus, dbus_glib, dbus_tools, polkit
+{ stdenv, fetchurl, fetchpatch, pkgconfig, glib, dbus, dbus_glib, dbus_tools, polkit
 , intltool, libxslt, docbook_xsl, udev, libusb1, pmutils
 , useSystemd ? true, systemd, gobjectIntrospection
 }:
@@ -12,6 +12,20 @@ stdenv.mkDerivation rec {
     url = "http://upower.freedesktop.org/releases/${name}.tar.xz";
     sha256 = "06wqhab2mn0j4biiwh7mn4kxbxnfnzjkxvhpgvnlpaz9m2q54cj3";
   };
+
+  patches = [
+    (fetchpatch rec {
+      url = "http://anonscm.debian.org/gitweb/?p=pkg-utopia/upower.git;"
+        + "a=blob_plain;f=debian/patches/${name};hb=b424b2763fbbba95df8c6ab3feeb57d072a9ddf7";
+      sha256 = "0iq991abrn745icyz6x0wyixrjli01vbmbd9lnwwgyil58h3z8sp";
+      name = "no_deprecation_define.patch";
+    })
+    (fetchpatch {
+      url = "http://cgit.freedesktop.org/upower/patch/?id=22da1a0bc5943b683189418d8b0f766e91b2bdbe";
+      sha256 = "0yfgg6pw4bwskannvdwjxr75lgdrjpxhsskwlzm0frp8v5jy4k4z";
+      name = "clamp-battery-percentages.patch";
+    })
+  ];
 
   buildInputs =
     [ dbus_glib polkit intltool libxslt docbook_xsl udev libusb1 gobjectIntrospection ]
