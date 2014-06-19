870a8a08337114bcaecc0557ec649b54ca9677eb
Rok Garbas <rok@garbas.si>
2014-06-19 15:59:54 +0100

    alsa-plugins: upgrade to 1.0.28
    adding libogg as dependency should fix the build

--- pkgs/os-specific/linux/alsa-plugins/default.nix
+++ pkgs/os-specific/linux/alsa-plugins/default.nix
@@ -1,18 +1,18 @@
-{ stdenv, fetchurl, lib, pkgconfig, alsaLib, pulseaudio ? null, jackaudio ? null }:
+{ stdenv, fetchurl, lib, pkgconfig, alsaLib, libogg, pulseaudio ? null, jackaudio ? null }:
 
 stdenv.mkDerivation rec {
-  name = "alsa-plugins-1.0.27";
+  name = "alsa-plugins-1.0.28";
 
   src = fetchurl {
     urls = [
       "ftp://ftp.alsa-project.org/pub/plugins/${name}.tar.bz2"
       "http://alsa.cybermirror.org/plugins/${name}.tar.bz2"
     ];
-    sha256 = "0ddbycq4cn9mc8xin0vh1af0zywz2rc2xyrs6qbayyyxq8vhrg8b";
+    sha256 = "081is33afhykb4ysll8s6gh0d6nm1cglslj9ck0disbyl3qqlvs2";
   };
 
   buildInputs =
-    [ pkgconfig alsaLib ]
+    [ pkgconfig alsaLib libogg ]
     ++ stdenv.lib.optional (pulseaudio != null) pulseaudio
     ++ stdenv.lib.optional (jackaudio != null) jackaudio;
 
