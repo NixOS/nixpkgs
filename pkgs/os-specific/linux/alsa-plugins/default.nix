1a1c83fa7756313618f29c26770bdee1ae3dfb88
Vladimír Čunát <vcunat@gmail.com>
2014-06-19 20:24:23 +0200

    alsa{Lib,Plugins}: update to 1.28.0
    Dropped unrecognized option, patch no longer applying, fixed licenses.

--- pkgs/os-specific/linux/alsa-plugins/default.nix
+++ pkgs/os-specific/linux/alsa-plugins/default.nix
@@ -1,26 +1,27 @@
 { stdenv, fetchurl, lib, pkgconfig, alsaLib, pulseaudio ? null, jackaudio ? null }:
 
 stdenv.mkDerivation rec {
-  name = "alsa-plugins-1.0.27";
+  name = "alsa-plugins-1.0.28";
 
   src = fetchurl {
     urls = [
       "ftp://ftp.alsa-project.org/pub/plugins/${name}.tar.bz2"
       "http://alsa.cybermirror.org/plugins/${name}.tar.bz2"
     ];
-    sha256 = "0ddbycq4cn9mc8xin0vh1af0zywz2rc2xyrs6qbayyyxq8vhrg8b";
+    sha256 = "081is33afhykb4ysll8s6gh0d6nm1cglslj9ck0disbyl3qqlvs2";
   };
 
+  # ToDo: a52, etc.?
   buildInputs =
     [ pkgconfig alsaLib ]
-    ++ stdenv.lib.optional (pulseaudio != null) pulseaudio
-    ++ stdenv.lib.optional (jackaudio != null) jackaudio;
+    ++ lib.optional (pulseaudio != null) pulseaudio
+    ++ lib.optional (jackaudio != null) jackaudio;
 
-  meta = { 
+  meta = with lib; {
     description = "Various plugins for ALSA";
     homepage = http://alsa-project.org/;
-    license = "GPL2.1";
-    maintainers = [lib.maintainers.marcweber];
-    platforms = lib.platforms.linux;
+    license = licenses.gpl2;
+    maintainers = [maintainers.marcweber];
+    platforms = platforms.linux;
   };
 }
