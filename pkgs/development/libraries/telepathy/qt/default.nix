5c482db0dd701004ab02acdf48484057667bf0cb
Vladimír Čunát <vcunat@gmail.com>
2014-06-19 23:02:18 +0200

    telepathy_qt: minor update, drop not applying patch
    Also enableParallelBuilding.

--- pkgs/development/libraries/telepathy/qt/default.nix
+++ pkgs/development/libraries/telepathy/qt/default.nix
@@ -1,18 +1,25 @@
-{ stdenv, fetchurl, cmake, qt4, pkgconfig, python, libxslt, dbus_glib
+{ stdenv, fetchurl, cmake, qt4, pkgconfig, python, libxslt, dbus_glib, dbus_daemon
 , telepathy_farstream, telepathy_glib, pythonDBus }:
 
 stdenv.mkDerivation rec {
-  name = "telepathy-qt-0.9.3";
+  name = "telepathy-qt-0.9.4";
 
   src = fetchurl {
     url = "http://telepathy.freedesktop.org/releases/telepathy-qt/${name}.tar.gz";
-    sha256 = "1yabyhsikw828ns7cf6hvzbxdxh53na1ck0q7qsav1lvlyz5gzy0";
+    sha256 = "1wk13rwpas1crj19xsbgl1c4qzri616xxa1hyhnykv4nkwxdpcgi";
   };
+
+  patches = [ ./farstream-0.2.diff ];
 
   nativeBuildInputs = [ cmake pkgconfig python libxslt ];
   propagatedBuildInputs = [ qt4 dbus_glib telepathy_farstream telepathy_glib pythonDBus ];
-  patches = [ ./cmake-2.8.12.diff ./farstream-0.2.diff ];
+
+  buildInputs = stdenv.lib.optional doCheck dbus_daemon;
+
   preBuild = ''
     NIX_CFLAGS_COMPILE+=" `pkg-config --cflags dbus-glib-1`"
   '';
+
+  enableParallelBuilding = true;
+  doCheck = false; # giving up for now
 }
