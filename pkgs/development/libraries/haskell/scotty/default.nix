aa599700425dce1b83b1947f71a7f8e2664309a2
Peter Simons <simons@cryp.to>
2014-06-20 15:06:05 +0200

    haskell-scotty: update to version 0.8.1

--- pkgs/development/libraries/haskell/scotty/default.nix
+++ pkgs/development/libraries/haskell/scotty/default.nix
@@ -1,17 +1,18 @@
 { cabal, aeson, blazeBuilder, caseInsensitive, conduit, dataDefault
-, hspec, httpTypes, mtl, regexCompat, text, transformers, wai
-, waiExtra, warp
+, hspec, httpTypes, liftedBase, monadControl, mtl, regexCompat
+, text, transformers, transformersBase, wai, waiExtra, warp
 }:
 
 cabal.mkDerivation (self: {
   pname = "scotty";
-  version = "0.8.0";
-  sha256 = "07198m8rsavdqr51abxsrmi8jail6h4ldzrr9s47il1djjba6lhh";
+  version = "0.8.1";
+  sha256 = "182iwsz5h7p08sqwfzb332gwj1wjx7fhhazm6gfdc0incab769m0";
   buildDepends = [
     aeson blazeBuilder caseInsensitive conduit dataDefault httpTypes
-    mtl regexCompat text transformers wai waiExtra warp
+    monadControl mtl regexCompat text transformers transformersBase wai
+    waiExtra warp
   ];
-  testDepends = [ hspec httpTypes wai waiExtra ];
+  testDepends = [ hspec httpTypes liftedBase wai waiExtra ];
   jailbreak = true;
   meta = {
     homepage = "https://github.com/scotty-web/scotty";
