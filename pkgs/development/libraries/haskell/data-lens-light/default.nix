2a88d3e56b12a1eb2c1e231f929dcf214e324743
Peter Simons <simons@cryp.to>
2014-06-20 15:05:46 +0200

    haskell-data-lens-light: update to version 0.1.2

--- pkgs/development/libraries/haskell/data-lens-light/default.nix
+++ pkgs/development/libraries/haskell/data-lens-light/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "data-lens-light";
-  version = "0.1.1";
-  sha256 = "03nsfwpcl7wmw2bgcb8z3w04krlbrbks7bjpnzfdz6cgdr5mnjrs";
+  version = "0.1.2";
+  sha256 = "1xxphcd36vw1ib48rfmrg207h0i1hlby01bm3xsxnq90ygizvxk7";
   buildDepends = [ mtl ];
   meta = {
     homepage = "https://github.com/feuerbach/data-lens-light";
