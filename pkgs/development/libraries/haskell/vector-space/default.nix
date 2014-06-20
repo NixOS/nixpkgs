b99b0f3ca22ed2d2a73bece9686610adc55cad2e
Peter Simons <simons@cryp.to>
2014-06-20 15:06:11 +0200

    haskell-vector-space: update to version 0.8.7

--- pkgs/development/libraries/haskell/vector-space/default.nix
+++ pkgs/development/libraries/haskell/vector-space/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "vector-space";
-  version = "0.8.6";
-  sha256 = "03kz2qhnynbgs4vk7348zjkkakzzwvxhbasl0lcazj1cx1ay7c4l";
+  version = "0.8.7";
+  sha256 = "1i3c34b3ngksmw4blhldap8fiw1jddm2h1qyr92csn3cllj6j1vm";
   buildDepends = [ Boolean MemoTrie NumInstances ];
   meta = {
     description = "Vector & affine spaces, linear maps, and derivatives";
