7179bdbcfe4b00245069e37228d55671a1281701
Peter Simons <simons@cryp.to>
2014-06-21 13:14:35 +0200

    haskell-network-transport-tcp: jailbreak to fix build with network 2.5

--- pkgs/development/libraries/haskell/network-transport-tcp/default.nix
+++ pkgs/development/libraries/haskell/network-transport-tcp/default.nix
@@ -1,4 +1,4 @@
-{ cabal, dataAccessor, network_2_4_1_2, networkTransport
+{ cabal, dataAccessor, network, networkTransport
 , networkTransportTests
 }:
 
@@ -6,11 +6,10 @@ cabal.mkDerivation (self: {
   pname = "network-transport-tcp";
   version = "0.4.0";
   sha256 = "1jjf1dj67a7l3jg3qgbg0hrjfnx1kr9n7hfvqssq7kr8sq1sc49v";
-  buildDepends = [ dataAccessor network_2_4_1_2 networkTransport ];
-  testDepends = [ network_2_4_1_2 networkTransport networkTransportTests ];
-  # tests should be enabled when network-transport-tests 0.2.0.0 released.
+  buildDepends = [ dataAccessor network networkTransport ];
+  testDepends = [ network networkTransport networkTransportTests ];
   doCheck = false;
-
+  jailbreak = true;
   meta = {
     homepage = "http://haskell-distributed.github.com";
     description = "TCP instantiation of Network.Transport";
