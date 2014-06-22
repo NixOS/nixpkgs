4a5cf73caac2d6f0d7ec021ec562173a33f762ef
Bjørn Forsman <bjorn.forsman@gmail.com>
2014-06-22 22:49:32 +0200

    cinelerra: don't repeat package name in meta.description

--- pkgs/applications/video/cinelerra/default.nix
+++ pkgs/applications/video/cinelerra/default.nix
@@ -41,7 +41,7 @@ stdenv.mkDerivation {
     ];
 
   meta = { 
-    description = "Cinelerra - Video Editor";
+    description = "Video Editor";
     homepage = http://www.cinelerra.org;
     maintainers = [ stdenv.lib.maintainers.marcweber ];
     license = "GPLv2";
