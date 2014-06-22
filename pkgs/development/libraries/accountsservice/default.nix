6693f6c3e35b6931ee99d60b940b09b02d23de6a
Bjørn Forsman <bjorn.forsman@gmail.com>
2014-06-22 21:29:26 +0200

    accountsservice: add meta.description

--- pkgs/development/libraries/accountsservice/default.nix
+++ pkgs/development/libraries/accountsservice/default.nix
@@ -24,4 +24,7 @@ stdenv.mkDerivation rec {
       --run "${coreutils}/bin/mkdir -p /var/lib/AccountsService/icons"
   '';
 
+  meta = {
+    description = "D-Bus interface for user account query and manipulation";
+  };
 }
