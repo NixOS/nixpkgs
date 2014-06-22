032cf360971a32e4c7ea06a593a38ba02ec9b28c
Bjørn Forsman <bjorn.forsman@gmail.com>
2014-06-22 22:45:53 +0200

    catdoc: add meta.description

--- pkgs/tools/text/catdoc/default.nix
+++ pkgs/tools/text/catdoc/default.nix
@@ -10,6 +10,7 @@ stdenv.mkDerivation rec {
   configureFlags = "--disable-wordview";
 
   meta = with stdenv.lib; {
+    description = "MS-Word/Excel/PowerPoint to text converter";
     platforms = platforms.all;
     license = "GPL2";
     maintainers = [ maintainers.urkud ];
