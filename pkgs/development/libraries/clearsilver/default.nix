ce4c20165b8191bd459e50e3d007cc8a5636af66
Bjørn Forsman <bjorn.forsman@gmail.com>
2014-06-22 22:51:14 +0200

    clearsilver: add description + homepage

--- pkgs/development/libraries/clearsilver/default.nix
+++ pkgs/development/libraries/clearsilver/default.nix
@@ -1,12 +1,19 @@
-{stdenv, fetchurl, python}:
+{ stdenv, fetchurl, python }:
 
 stdenv.mkDerivation {
   name = "clearsilver-0.10.3";
+
   src = fetchurl {
     url = http://www.clearsilver.net/downloads/clearsilver-0.10.3.tar.gz;
     md5 = "ff4104b0e58bca1b61d528edbd902769";
   };
+
   builder = ./builder.sh;
 
   inherit stdenv python;
+
+  meta = {
+    description = "Fast, powerful, and language-neutral HTML template system";
+    homepage = http://www.clearsilver.net/;
+  };
 }
