4b61edb390186593c701b51f24fc1cf5fa57c407
Bjørn Forsman <bjorn.forsman@gmail.com>
2014-06-22 23:08:11 +0200

    coredumper: mark as broken
    It doesn't build. Bumping to latest version (1.2.1) didn't fix it. It
    has no maintainers and the last time the expression was touched was in
    2007.

--- pkgs/development/libraries/coredumper/default.nix
+++ pkgs/development/libraries/coredumper/default.nix
@@ -6,4 +6,13 @@ stdenv.mkDerivation {
     url = http://google-coredumper.googlecode.com/files/coredumper-1.1.tar.gz;
     sha256 = "1phl1zg2n17rp595dyzz9iw01gfdpsdh0l6wy2hfb5shi71h63rx";
   };
+
+  # Doesn't build:
+  #
+  # src/elfcore.c: In function 'CreatePipeline':
+  # src/elfcore.c:1424:26: error: 'CLONE_VM' undeclared (first use in this function)
+  #                           CLONE_VM|CLONE_UNTRACED|SIGCHLD, &args, 0, 0, 0);
+  #                           ^
+  # src/elfcore.c:1424:26: note: each undeclared identifier is reported only once for each function it appears in
+  meta.broken = true;
 }
