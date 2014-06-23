40827f67e4c929d80e0e674012a9ed7343b8a574
William A. Kennington III <william@wkennington.com>
2014-06-23 03:32:58 -0500

    dnsmasq: Increase configurability

--- pkgs/tools/networking/dnsmasq/default.nix
+++ pkgs/tools/networking/dnsmasq/default.nix
@@ -1,5 +1,20 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, pkgconfig
+, withConntrack ? false, libnetfilter_conntrack ? null, libnfnetlink ? null
+, withDbus ? false, dbus ? null
+, withDnssec ? true, nettle ? null
+}:
 
+assert withDnssec -> true;
+assert withConntrack -> libnetfilter_conntrack != null && libnfnetlink != null;
+assert withDbus -> dbus != null;
+
+with stdenv.lib;
+let
+  copts = [ ]
+    ++ optional withDnssec "DNSSEC"
+    ++ optional withConntrack "CONNTRACK"
+    ++ optional withDbus "DBUS";
+in
 stdenv.mkDerivation rec {
   name = "dnsmasq-2.71";
 
@@ -8,13 +23,27 @@ stdenv.mkDerivation rec {
     sha256 = "1i1l2s2bcs6yx05wh1p2rf4bl0jfdgmg77b33gh44r1rdbv6933x";
   };
 
-  makeFlags = "DESTDIR= BINDIR=$(out)/bin MANDIR=$(out)/man LOCALEDIR=$(out)/share/locale";
+  buildInputs = [ pkgconfig ]
+    ++ optionals withConntrack [ libnetfilter_conntrack libnfnetlink ]
+    ++ optional withDbus dbus
+    ++ optional withDnssec nettle;
+
+  buildPhase = ''
+    make COPTS="${concatStringsSep " " (map (n: "-DHAVE_${n}") copts)}"
+  '';
+
+  installFlags = [
+    "DESTDIR= "
+    "BINDIR=$(out)/bin"
+    "MANDIR=$(out)/man"
+    "LOCALEDIR=$(out)/share/locale"
+  ];
 
   meta = {
     description = "An integrated DNS, DHCP and TFTP server for small networks";
     homepage = http://www.thekelleys.org.uk/dnsmasq/doc.html;
-    license = "GPL";
-    platforms = stdenv.lib.platforms.linux;
-    maintainers = [ stdenv.lib.maintainers.eelco ];
+    license = licenses.gpl2;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ eelco ];
   };
 }
