2d97211566b354a52c65b7f4eb5b38985fb623d1
Bjørn Forsman <bjorn.forsman@gmail.com>
2014-06-23 13:13:50 +0200

    zoom: add meta attributes
    And do some coding style / whitespace clean up.

--- pkgs/games/zoom/default.nix
+++ pkgs/games/zoom/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, perl, expat, x11, freetype}:
+{ stdenv, fetchurl, perl, expat, x11, freetype }:
 
 # !!! assert freetype == xlibs.freetype
 
@@ -10,8 +10,21 @@ stdenv.mkDerivation {
     sha256 = "1g6van7f7sg3zfcz80mncnnbccyg2hnm0hq4x558vpsm0lf7z5pj";
   };
   
-  buildInputs = [perl expat x11 freetype];
+  buildInputs = [ perl expat x11 freetype ];
   
   # Zoom doesn't add the right directory in the include path.
-  CFLAGS = ["-I" (freetype + "/include/freetype2")];
+  CFLAGS = [ "-I" (freetype + "/include/freetype2") ];
+
+  meta = with stdenv.lib; {
+    description = "Player for Z-Code, TADS and HUGO stories or games, usually text adventures ('interactive fiction')";
+    longDescription = ''
+      Zoom is a player for Z-Code, TADS and HUGO stories or games. These are
+      usually text adventures ('interactive fiction'), and were first created
+      by Infocom with the Zork series of games. Throughout the 80's, Infocom
+      released many interactive fiction stories before their ambitions to enter
+      the database market finally brought them low.
+    '';
+    license = licenses.gpl3;
+    platforms = platforms.linux;
+  };
 }
