c11d9daa588f3726bd53dee67600efc4c76777b4
Rickard Nilsson <rickynils@gmail.com>
2014-06-25 13:44:31 +0200

    syslog-ng: Add perl to buildInputs so perl scripts in output are fixed correctly

--- pkgs/tools/system/syslog-ng/default.nix
+++ pkgs/tools/system/syslog-ng/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, eventlog, pkgconfig, glib, python, systemd }:
+{ stdenv, fetchurl, eventlog, pkgconfig, glib, python, systemd, perl }:
 
 stdenv.mkDerivation {
   name = "syslog-ng-3.5.4.1";
@@ -8,7 +8,7 @@ stdenv.mkDerivation {
     sha256 = "0rkgrmnyx1x6m3jw5n49k7r1dcg79lxh900g74rgvd3j86g9dilj";
   };
 
-  buildInputs = [ eventlog pkgconfig glib python systemd ];
+  buildInputs = [ eventlog pkgconfig glib python systemd perl ];
 
   configureFlags = [
     "--enable-dynamic-linking"
