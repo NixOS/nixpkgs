abc3a27dbce17d7e076b0202fa746ab06b5c4c46
Charles Strahan <charles.c.strahan@gmail.com>
2014-06-25 15:01:13 -0400

    fix openjdk url for Darwin

--- pkgs/development/compilers/openjdk-darwin/default.nix
+++ pkgs/development/compilers/openjdk-darwin/default.nix
@@ -4,7 +4,7 @@ let
     name = "openjdk6-b16-24_apr_2009-r1";
 
     src = fetchurl {
-      url = http://hg.bikemonkey.org/archive/openjdk6_darwin/openjdk6-b16-24_apr_2009-r1.tar.bz2;
+      url = http://landonf.bikemonkey.org/static/soylatte/bsd-dist/openjdk6_darwin/openjdk6-b16-24_apr_2009-r1.tar.bz2;
       sha256 = "14pbv6jjk95k7hbgiwyvjdjv8pccm7m8a130k0q7mjssf4qmpx1v";
     };
 
