7f97fafe4fea45958b32a4a587975db827be8bab
Vladimír Čunát <vcunat@gmail.com>
2014-06-26 11:13:56 +0200

    linux-3.12: security update .22 ->.23, CVE-2014-0206
    CC #3090.

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.12.22";
+  version = "3.12.23";
   extraMeta.branch = "3.12";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0c9rw11iihjimqb6mdmwn4q67ds7jg4qibj5ijj5pdmkdqzj7g54";
+    sha256 = "0aa9vnyk4p6hg4fp4rm4a95cn4p16j2dfrhc3cfznpcdkpi5q9m7";
   };
 
   features.iwlwifi = true;
