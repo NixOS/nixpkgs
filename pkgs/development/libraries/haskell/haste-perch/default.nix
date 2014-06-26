b09f7cf39b0e8fda4b9ed87745cfae3aa110e2cd
tomberek <tomberek@gmail.com>
2014-06-26 13:04:17 -0400

    jailbreak for hastePerch

--- pkgs/development/libraries/haskell/haste-perch/default.nix
+++ pkgs/development/libraries/haskell/haste-perch/default.nix
@@ -5,6 +5,7 @@ cabal.mkDerivation (self: {
   version = "0.1.0.0";
   sha256 = "0g2ijb0mzqs2iq4i47biaxbsg4v15w9ky6yyz6wmngwf06rg4iwj";
   buildDepends = [ hasteCompiler mtl ];
+  jailbreak = true;
   meta = {
     homepage = "https://github.com/agocorona/haste-perch";
     description = "Create dynamic HTML in the browser using blaze-html-style notation with Haste";
