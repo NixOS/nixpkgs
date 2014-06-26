99da7b85cbf72ac50b5217e4c8b06acfd7ef4b1b
Vladimír Čunát <vcunat@gmail.com>
2014-06-26 11:04:30 +0200

    gnupg: security update 2.0.22 -> .24, CVE-2014-4617
    Close #3091.

--- pkgs/tools/security/gnupg/default.nix
+++ pkgs/tools/security/gnupg/default.nix
@@ -13,11 +13,11 @@ assert useUsb -> (libusb != null);
 assert useCurl -> (curl != null);
 
 stdenv.mkDerivation rec {
-  name = "gnupg-2.0.22";
+  name = "gnupg-2.0.24";
 
   src = fetchurl {
     url = "mirror://gnupg/gnupg/${name}.tar.bz2";
-    sha256 = "0lg210acj2rxq291q4cwamg9gx6gh2prb1xa93y5jhw5b6r0lza3";
+    sha256 = "0ch2hbindk832cy7ca00a7whw84ndm0nhqrl24a5fw4ldkca2x6r";
   };
 
   buildInputs
