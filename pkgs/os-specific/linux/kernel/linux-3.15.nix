e303e18608708eaa123ca72be92af3ce493207b2
Michael Raskin <7c6f434c@mail.ru>
2014-07-01 14:28:52 +0400

    Update Linux 3.15 to 3.15.3

--- pkgs/os-specific/linux/kernel/linux-3.15.nix
+++ pkgs/os-specific/linux/kernel/linux-3.15.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.15.2";
+  version = "3.15.3";
   extraMeta.branch = "3.15";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "09nq8q84xn6lwzdnn36pzfiqhn1lapi60yxn4hifb7v9ymhc5sv6";
+    sha256 = "0i8dmx1yiaraxb7gsjm7wpy0rv8038nbrwl2slzq749pgm4bd5bf";
   };
 
   features.iwlwifi = true;
