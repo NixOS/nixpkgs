adbb9ff7966c1c17588100d6afddda66eafc9453
Paul Colomiets <paul@colomiets.name>
2014-07-03 01:59:35 +0300

    dnsmasq: upgrade to 2.71, fixed dnsmasq module
    * The module now has systemd config
    
    * Add resolveLocalQueries option which sets up it as a dns server for
      local host (including reasonable setup of resolvconf)
    
    * Add "dnsmasq" user for running daemon
    
    * Enabled dbus and dnssec support for the package
    
    Conflicts:
    	nixos/modules/misc/ids.nix

--- pkgs/tools/networking/dnsmasq/default.nix
+++ pkgs/tools/networking/dnsmasq/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl }:
+{ pkgconfig, dbus_libs, nettle, stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
   name = "dnsmasq-2.71";
@@ -8,7 +8,29 @@ stdenv.mkDerivation rec {
     sha256 = "1fpzpzja7qr8b4kfdhh4i4sijp62c634yf0xvq2n4p7d5xbzn6a9";
   };
 
+  # Can't rely on make flags because of space in one of the parameters
+  buildPhase = ''
+    make COPTS="-DHAVE_DNSSEC -DHAVE_DBUS"
+  '';
+
+  # make flags used for installation only
   makeFlags = "DESTDIR= BINDIR=$(out)/bin MANDIR=$(out)/man LOCALEDIR=$(out)/share/locale";
+
+  postInstall = ''
+    install -Dm644 dbus/dnsmasq.conf $out/etc/dbus-1/system.d/dnsmasq.conf
+    install -Dm644 trust-anchors.conf $out/share/dnsmasq/trust-anchors.conf
+
+    ensureDir $out/share/dbus-1/system-services
+    cat <<END > $out/share/dbus-1/system-services/uk.org.thekelleys.dnsmasq.service
+    [D-BUS Service]
+    Name=uk.org.thekelleys.dnsmasq
+    Exec=$out/sbin/dnsmasq -k -1
+    User=root
+    SystemdService=dnsmasq.service
+    END
+  '';
+
+  buildInputs = [ pkgconfig dbus_libs nettle ];
 
   meta = {
     description = "An integrated DNS, DHCP and TFTP server for small networks";
