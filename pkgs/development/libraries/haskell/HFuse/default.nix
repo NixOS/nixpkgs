8aa3b656e53ee3b01666aafe51d8ad1ec0ba694f
Peter Simons <simons@cryp.to>
2014-07-03 16:59:40 +0200

    haskell-HFuse: update to version 0.2.4.2

--- pkgs/development/libraries/haskell/HFuse/default.nix
+++ pkgs/development/libraries/haskell/HFuse/default.nix
@@ -2,12 +2,11 @@
 
 cabal.mkDerivation (self: {
   pname = "HFuse";
-  version = "0.2.4.1";
-  sha256 = "12k04dvh92kk2i68bcb70xnk378qxmh46f241k06di5rkcgwyg1k";
+  version = "0.2.4.2";
+  sha256 = "043f5x807h1dajd0bvmhln9pjkviszx1279m298w711mrszapqrq";
   extraLibraries = [ fuse ];
   preConfigure = ''
     sed -i -e "s@  Extra-Lib-Dirs:         /usr/local/lib@  Extra-Lib-Dirs:         ${fuse}/lib@" HFuse.cabal
-    sed -i -e "s@  Include-Dirs:           /usr/include, /usr/local/include, .@  Include-Dirs:           ${fuse}/include@" HFuse.cabal
     sed -i -e "s/LANGUAGE FlexibleContexts/LANGUAGE FlexibleContexts, RankNTypes/" System/Fuse.hsc
     sed -i -e "s/E(Exception/E(catch, Exception, IOException/" System/Fuse.hsc
     sed -i -e "s/IO(catch,/IO(/" System/Fuse.hsc
@@ -15,7 +14,7 @@ cabal.mkDerivation (self: {
     sed -i -e "s/const exitFailure/\\\\(_ :: IOException) -> exitFailure/" System/Fuse.hsc
   '';
   meta = {
-    homepage = "https://github.com/toothbrush/hfuse";
+    homepage = "https://github.com/m15k/hfuse";
     description = "HFuse is a binding for the Linux FUSE library";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.stdenv.lib.platforms.linux;
