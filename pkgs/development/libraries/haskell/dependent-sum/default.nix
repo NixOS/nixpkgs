324cfb74473817300dc52e4ef3656569e791f3c1
Peter Simons <simons@cryp.to>
2014-07-03 17:00:06 +0200

    haskell-dependent-sum: update to version 0.2.1.0

--- pkgs/development/libraries/haskell/dependent-sum/default.nix
+++ pkgs/development/libraries/haskell/dependent-sum/default.nix
@@ -2,8 +2,8 @@
 
 cabal.mkDerivation (self: {
   pname = "dependent-sum";
-  version = "0.2.0.2";
-  sha256 = "06amnc50j246f2za0ri49j6vyq6admx03w3xxjhhfnfx9lp6zmhm";
+  version = "0.2.1.0";
+  sha256 = "1h6wsrh206k6q3jcfdxvlsswbm47x30psp6x30l2z0j9jyf7jpl3";
   meta = {
     homepage = "https://github.com/mokus0/dependent-sum";
     description = "Dependent sum type";
