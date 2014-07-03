485f73f6b38bd864be06f192a5d20681c49d0fde
Peter Simons <simons@cryp.to>
2014-07-03 16:59:42 +0200

    haskell-HTF: update to version 0.11.4.0

--- pkgs/development/libraries/haskell/HTF/default.nix
+++ pkgs/development/libraries/haskell/HTF/default.nix
@@ -1,17 +1,17 @@
-{ cabal, aeson, cpphs, Diff, filepath, haskellSrcExts, HUnit
-, liftedBase, monadControl, mtl, QuickCheck, random, regexCompat
-, temporary, text, unorderedContainers, xmlgen
+{ cabal, aeson, base64Bytestring, cpphs, Diff, filepath
+, haskellSrcExts, HUnit, liftedBase, monadControl, mtl, QuickCheck
+, random, regexCompat, temporary, text, unorderedContainers, xmlgen
 }:
 
 cabal.mkDerivation (self: {
   pname = "HTF";
-  version = "0.11.3.4";
-  sha256 = "0db47fvp33k83dnhvpygprm06p3z397f5ci154vqk7krjpxb2ynx";
+  version = "0.11.4.0";
+  sha256 = "0bg84x6xk359zby04xw62yy227fk85mgs7x5nkdbsxcajm7j0bs9";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
-    aeson cpphs Diff haskellSrcExts HUnit liftedBase monadControl mtl
-    QuickCheck random regexCompat text xmlgen
+    aeson base64Bytestring cpphs Diff haskellSrcExts HUnit liftedBase
+    monadControl mtl QuickCheck random regexCompat text xmlgen
   ];
   testDepends = [
     aeson filepath mtl random regexCompat temporary text
