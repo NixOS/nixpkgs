3329d681993dc9fddd6a74267b48b75aec7430ce
Michael Raskin <7c6f434c@mail.ru>
2014-07-04 21:42:31 +0400

    aria2: update from 1.18.1 to 1.18.5

--- pkgs/tools/networking/aria2/default.nix
+++ pkgs/tools/networking/aria2/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, openssl, libxml2, sqlite, zlib }:
 
 stdenv.mkDerivation rec {
-  name = "aria2-1.18.1";
+  name = "aria2-1.18.5";
 
   src = fetchurl {
     url = "mirror://sourceforge/aria2/stable/${name}/${name}.tar.bz2";
-    sha256 = "0fwm5d0i4nr9yzckmd8yc80yj4h1acrv6jca3h1vjji0rbgk49zy";
+    sha256 = "0gyfp4zw7rlaxcxhb402azazf1fi83kk3qg4w0j8d2i7pfa1zqi5";
   };
 
   buildInputs = [ pkgconfig openssl libxml2 sqlite zlib ];
