36426f2b49c9c709eba6363841c89674544502e4
tomberek <tomberek@gmail.com>
2014-07-04 09:10:57 -0400

    Initial additions

---
+++ pkgs/development/libraries/haskell/haste-compiler/default.nix
@@ -0,0 +1,25 @@
+{ cabal, binary, blazeBuilder, bzlib, dataBinaryIeee754
+, dataDefault, executablePath, filepath, ghcPaths, HTTP, monadsTf
+, mtl, network, random, shellmate, systemFileio, tar, temporary
+, time, transformers, utf8String, websockets, zipArchive
+}:
+
+cabal.mkDerivation (self: {
+  pname = "haste-compiler";
+  version = "0.3";
+  sha256 = "0a0hyra1h484c404d95d411l7gddaazy1ikwzlgkgzaqzd7j7dbd";
+  isLibrary = true;
+  isExecutable = true;
+  buildDepends = [
+    binary blazeBuilder bzlib dataBinaryIeee754 dataDefault
+    executablePath filepath ghcPaths HTTP monadsTf mtl network random
+    shellmate systemFileio tar temporary time transformers utf8String
+    websockets zipArchive
+  ];
+  meta = {
+    homepage = "http://github.com/valderman/haste-compiler";
+    description = "Haskell To ECMAScript compiler";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+  };
+})
