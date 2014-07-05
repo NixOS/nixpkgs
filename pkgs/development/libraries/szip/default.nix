0e1eac04bb4644b6287908ce70d53a138fafa797
Andreas Herrmann <andreash87@gmx.ch>
2014-07-05 21:05:23 +0200

    szip: Indentation

--- pkgs/development/libraries/szip/default.nix
+++ pkgs/development/libraries/szip/default.nix
@@ -1,9 +1,9 @@
 { stdenv, fetchurl }:
     
 stdenv.mkDerivation {
-    name = "szip-2.1";
-    src = fetchurl {
-        url = ftp://ftp.hdfgroup.org/lib-external/szip/2.1/src/szip-2.1.tar.gz;
-        sha256 = "1vym7r4by02m0yqj10023xyps5b21ryymnxb4nb2gs32arfxj5m8";
-    };
+  name = "szip-2.1";
+  src = fetchurl {
+    url = ftp://ftp.hdfgroup.org/lib-external/szip/2.1/src/szip-2.1.tar.gz;
+    sha256 = "1vym7r4by02m0yqj10023xyps5b21ryymnxb4nb2gs32arfxj5m8";
+  };
 }
