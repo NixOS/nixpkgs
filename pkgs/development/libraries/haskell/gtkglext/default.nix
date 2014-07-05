e658ae9694301671ec73acdfe588ddbf78fbae99
Bjørn Forsman <bjorn.forsman@gmail.com>
2014-07-05 16:09:27 +0200

    Revert "haskell-gtkglext: update to version 0.12.5.0"
    Because it breaks nixpkgs evaluation:
    
    $ nix-env -f . -qa '*' --meta --xml --drv-path --show-trace
    error: while querying the derivation named `haskell-gtkglext-ghc7.6.3-0.12.5.0':
    while evaluating the attribute `propagatedNativeBuildInputs' of the derivation `haskell-gtkglext-ghc7.6.3-0.12.5.0' at /home/bfo/nixpkgs/pkgs/build-support/cabal/default.nix:70:13:
    infinite recursion encountered
    
    This reverts commit 6ba32aa8bdfd73b6b55d07f270a3f344007ab7cb.

--- pkgs/development/libraries/haskell/gtkglext/default.nix
+++ pkgs/development/libraries/haskell/gtkglext/default.nix
@@ -1,12 +1,12 @@
-{ cabal, glib, gtk, gtk2hsBuildtools, gtkglext, pango }:
+{ pkgs, cabal, gtk, gtk2hsBuildtools, pangox_compat }:
 
 cabal.mkDerivation (self: {
   pname = "gtkglext";
   version = "0.12.5.0";
   sha256 = "1xhalcb85cpswdpqxx64fxpmyqq7iakgaczjrbr25fp0h1glshhk";
-  buildDepends = [ glib gtk pango ];
+  buildDepends = [ gtk ];
   buildTools = [ gtk2hsBuildtools ];
-  pkgconfigDepends = [ gtkglext ];
+  pkgconfigDepends = [ pkgs.gnome2.gtkglext pangox_compat ];
   meta = {
     homepage = "http://projects.haskell.org/gtk2hs/";
     description = "Binding to the GTK+ OpenGL Extension";
