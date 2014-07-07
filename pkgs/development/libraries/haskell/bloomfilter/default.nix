a8a57c3212d0389fe8c4a58d748e7d60ab92b8a4
Peter Simons <simons@cryp.to>
2014-07-07 11:25:53 +0200

    haskell-bloomfilter: update to version 2.0.0.0

--- pkgs/development/libraries/haskell/bloomfilter/default.nix
+++ pkgs/development/libraries/haskell/bloomfilter/default.nix
@@ -1,10 +1,15 @@
-{ cabal, deepseq }:
+{ cabal, deepseq, QuickCheck, random, testFramework
+, testFrameworkQuickcheck2
+}:
 
 cabal.mkDerivation (self: {
   pname = "bloomfilter";
-  version = "1.2.6.10";
-  sha256 = "162vp9riwf5q2l1hnw3g157fpwnw185fk41hkgyf8qaavcrz6slv";
+  version = "2.0.0.0";
+  sha256 = "07fif8i5rinysli1mpi92k405kvw8va7w9v9w4wd5bylb87zy77f";
   buildDepends = [ deepseq ];
+  testDepends = [
+    QuickCheck random testFramework testFrameworkQuickcheck2
+  ];
   meta = {
     homepage = "https://github.com/bos/bloomfilter";
     description = "Pure and impure Bloom Filter implementations";
