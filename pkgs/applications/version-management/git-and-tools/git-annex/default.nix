f7f1e3f842c4b6392d53ebb2eab10c9d130325f2
Peter Simons <simons@cryp.to>
2014-07-08 14:56:54 +0200

    haskell-git-annex: update to version 5.20140707

--- pkgs/applications/version-management/git-and-tools/git-annex/default.nix
+++ pkgs/applications/version-management/git-and-tools/git-annex/default.nix
@@ -4,41 +4,46 @@
 , exceptions, extensibleExceptions, fdoNotify, feed, filepath, git
 , gnupg1, gnutls, hamlet, hinotify, hS3, hslogger, HTTP, httpClient
 , httpConduit, httpTypes, IfElse, json, liftedBase, lsof, MissingH
-, monadControl, mtl, network, networkConduit, networkInfo
-, networkMulticast, networkProtocolXmpp, openssh
-, optparseApplicative, perl, QuickCheck, random, regexTdfa, rsync
-, SafeSemaphore, securemem, SHA, shakespeare, stm, tasty
-, tastyHunit, tastyQuickcheck, tastyRerun, text, time, transformers
-, unixCompat, utf8String, uuid, wai, waiExtra, warp, warpTls, which
-, xmlTypes, yesod, yesodCore, yesodDefault, yesodForm, yesodStatic
-, fsnotify
+, monadControl, mtl, network, networkInfo, networkMulticast
+, networkProtocolXmpp, openssh, optparseApplicative, perl
+, QuickCheck, random, regexTdfa, rsync, SafeSemaphore, securemem
+, SHA, shakespeare, stm, tasty, tastyHunit, tastyQuickcheck
+, tastyRerun, text, time, transformers, unixCompat, utf8String
+, uuid, wai, waiExtra, warp, warpTls, which, xmlTypes, yesod
+, yesodCore, yesodDefault, yesodForm, yesodStatic
 }:
 
 cabal.mkDerivation (self: {
   pname = "git-annex";
-  version = "5.20140613";
-  sha256 = "01khqy68w1rqxic9lqal7902cv89d10xvbzmvlsx99g70ljqfafi";
+  version = "5.20140707";
+  sha256 = "1m78125w6mq532ngfksrwj3s43qj7wyp756f6qxcqg1cl71xh34k";
   isLibrary = false;
   isExecutable = true;
   buildDepends = [
     aeson async blazeBuilder bloomfilter byteable caseInsensitive
-    clientsession cryptoApi cryptohash dataDefault dataenc DAV
-    dlist dns editDistance exceptions extensibleExceptions
-    feed filepath gnutls hamlet hS3 hslogger HTTP httpClient
+    clientsession cryptoApi cryptohash dataDefault dataenc DAV dbus
+    dlist dns editDistance exceptions extensibleExceptions fdoNotify
+    feed filepath gnutls hamlet hinotify hS3 hslogger HTTP httpClient
     httpConduit httpTypes IfElse json liftedBase MissingH monadControl
-    mtl network networkConduit networkInfo networkMulticast
-    networkProtocolXmpp optparseApplicative QuickCheck random regexTdfa
-    SafeSemaphore securemem SHA shakespeare stm tasty tastyHunit
-    tastyQuickcheck tastyRerun text time transformers unixCompat
-    utf8String uuid wai waiExtra warp warpTls xmlTypes yesod yesodCore
-    yesodDefault yesodForm yesodStatic
-  ] ++ (if (!self.stdenv.isDarwin) then [
-    dbus fdoNotify hinotify
-  ] else [
-    fsnotify
-  ]);
+    mtl network networkInfo networkMulticast networkProtocolXmpp
+    optparseApplicative QuickCheck random regexTdfa SafeSemaphore
+    securemem SHA shakespeare stm tasty tastyHunit tastyQuickcheck
+    tastyRerun text time transformers unixCompat utf8String uuid wai
+    waiExtra warp warpTls xmlTypes yesod yesodCore yesodDefault
+    yesodForm yesodStatic
+  ];
   buildTools = [ bup curl git gnupg1 lsof openssh perl rsync which ];
-  configureFlags = "-fAssistant -fProduction";
+  configureFlags = "-fS3
+                    -fWebDAV
+                    -fInotify
+                    -fDbus
+                    -fAssistant
+                    -fWebapp
+                    -fPairing
+                    -fXMPP
+                    -fDNS
+                    -fProduction
+                    -fTDFA";
   preConfigure = ''
     export HOME="$NIX_BUILD_TOP/tmp"
     mkdir "$HOME"
