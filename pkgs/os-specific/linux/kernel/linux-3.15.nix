eb659e89b41efae33b4b68a23c238b662d13e541
Vladimír Čunát <vcunat@gmail.com>
2014-07-09 22:49:12 +0200

    linux_*: update, including CVE-2014-4699 (most likely)
    CC #3196. No updates yet on 3.2 and 3.12 branches.

--- pkgs/os-specific/linux/kernel/linux-3.15.nix
+++ pkgs/os-specific/linux/kernel/linux-3.15.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.15.3";
+  version = "3.15.5";
   extraMeta.branch = "3.15";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0i8dmx1yiaraxb7gsjm7wpy0rv8038nbrwl2slzq749pgm4bd5bf";
+    sha256 = "09j0ga3nc90fbfl9g3i8x9vp70hq7ddnnlbcazahrz4vn6mngqv2";
   };
 
   features.iwlwifi = true;
--- pkgs/os-specific/linux/kernel/linux-3.4.nix
+++ pkgs/os-specific/linux/kernel/linux-3.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.4.95";
+  version = "3.4.98";
   extraMeta.branch = "3.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1rv2jsfc2m12nk293v12rvvqnicyh73pwp6bcb28jdqdhm95k6l6";
+    sha256 = "0kkd6q80w4rh1sjdd4j5a2fmib4mjwhsf5fm555jl2w3zsy0kr45";
   };
 
   kernelPatches = args.kernelPatches ++
