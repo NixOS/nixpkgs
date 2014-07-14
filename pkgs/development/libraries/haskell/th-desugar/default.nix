bd8330a9211888c478166ea759c5c327cad426b6
Peter Simons <simons@cryp.to>
2014-07-14 10:54:05 +0200

    haskell-th-desugar: update to version 1.4.2

--- pkgs/development/libraries/haskell/th-desugar/default.nix
+++ pkgs/development/libraries/haskell/th-desugar/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "th-desugar";
-  version = "1.4.1";
-  sha256 = "1pjv301bshdmn3s5nrmmcx5d1b80c410lml73sai68dhx7v64vw2";
+  version = "1.4.2";
+  sha256 = "16l0khjx2wppnm9spp6mg659m95hxjkzfv3pjw5ays3z6clhx8b9";
   buildDepends = [ mtl syb ];
   testDepends = [ hspec HUnit mtl syb ];
   meta = {
