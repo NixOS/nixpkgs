9f66dafa293f16ede205bebdb27e10454d0c84fa
Cray Elliott <MP2E@archlinux.us>
2014-07-14 05:01:06 -0700

    Import the Haskell library shellish 0.1.3 from Cabal

---
+++ pkgs/development/libraries/haskell/shellish/default.nix
@@ -0,0 +1,14 @@
+{ cabal, filepath, mtl, strict, time, unixCompat }:
+
+cabal.mkDerivation (self: {
+  pname = "shellish";
+  version = "0.1.4";
+  sha256 = "1ldwid270mwyky6zmggbvn72hvs4s39hhf2zj8r0jahxnwlpbfan";
+  buildDepends = [ filepath mtl strict time unixCompat ];
+  meta = {
+    homepage = "http://repos.mornfall.net/shellish";
+    description = "shell-/perl- like (systems) programming in Haskell";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+  };
+})
