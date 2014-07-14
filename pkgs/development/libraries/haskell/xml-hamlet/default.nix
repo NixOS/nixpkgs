c8a7d72eac0e3a86e3cb400e646b18add48e2d5f
Peter Simons <simons@cryp.to>
2014-07-14 10:54:20 +0200

    haskell-xml-hamlet: update to version 0.4.0.9

--- pkgs/development/libraries/haskell/xml-hamlet/default.nix
+++ pkgs/development/libraries/haskell/xml-hamlet/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "xml-hamlet";
-  version = "0.4.0.8";
-  sha256 = "10hc4a6lqifiinm9rf1ziw35bjnjgkd5mhagg9anvz35hhr7bbrr";
+  version = "0.4.0.9";
+  sha256 = "0vm4jhvjf53a33sisnhghg0m9pcv2fx676rl90vbm2xcd9rqlj82";
   buildDepends = [ parsec shakespeare text xmlConduit ];
   testDepends = [ hspec HUnit parsec shakespeare text xmlConduit ];
   meta = {
