12fb52b08a9e2e99664b9d5c5c34c1994e496700
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-07-15 23:30:55 +0200

    blender: Enable ocean modifier

--- pkgs/applications/misc/blender/default.nix
+++ pkgs/applications/misc/blender/default.nix
@@ -1,9 +1,11 @@
-{ stdenv, fetchurl, SDL, boost, cmake, ffmpeg, gettext, glew
+{ stdenv, lib, fetchurl, SDL, boost, cmake, ffmpeg, gettext, glew
 , ilmbase, libXi, libjpeg, libpng, libsamplerate, libsndfile
 , libtiff, mesa, openal, opencolorio, openexr, openimageio, openjpeg, python
-, zlib
+, zlib, fftw
 , jackaudioSupport ? false, jackaudio
 }:
+
+with lib;
 
 stdenv.mkDerivation rec {
   name = "blender-2.71";
@@ -16,8 +18,8 @@ stdenv.mkDerivation rec {
   buildInputs =
     [ SDL boost cmake ffmpeg gettext glew ilmbase libXi
       libjpeg libpng libsamplerate libsndfile libtiff mesa openal
-      opencolorio openexr openimageio openjpeg python zlib
-    ] ++ stdenv.lib.optional jackaudioSupport jackaudio;
+      opencolorio openexr openimageio openjpeg python zlib fftw
+    ] ++ optional jackaudioSupport jackaudio;
 
   postUnpack =
     ''
@@ -27,6 +29,7 @@ stdenv.mkDerivation rec {
   cmakeFlags =
     [ "-DOPENEXR_INC=${openexr}/include/OpenEXR"
       "-DWITH_OPENCOLLADA=OFF"
+      "-DWITH_MOD_OCEANSIM=ON"
       "-DWITH_CODEC_FFMPEG=ON"
       "-DWITH_CODEC_SNDFILE=ON"
       "-DWITH_INSTALL_PORTABLE=OFF"
@@ -34,7 +37,8 @@ stdenv.mkDerivation rec {
       "-DPYTHON_LIBPATH=${python}/lib"
       "-DPYTHON_INCLUDE_DIR=${python}/include/python${python.majorVersion}m"
       "-DPYTHON_VERSION=${python.majorVersion}"
-    ] ++ stdenv.lib.optional jackaudioSupport "-DWITH_JACK=ON";
+    ]
+    ++ optional jackaudioSupport "-DWITH_JACK=ON";
 
   NIX_CFLAGS_COMPILE = "-I${ilmbase}/include/OpenEXR -I${python}/include/${python.libPrefix}m";
 
