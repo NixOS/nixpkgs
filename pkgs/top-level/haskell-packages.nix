ebed71f440ec3f169849cab39397958bd46f00f8
Peter Simons <simons@cryp.to>
2014-07-18 14:34:28 +0200

    aeson: don't use version 0.8 yet because it breaks too many packages at the moment

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -142,7 +142,7 @@ self : let callPackage = x : y : modifyPrio (newScope self x y); in
   aeson_0_7_0_4 = callPackage ../development/libraries/haskell/aeson/0.7.0.4.nix { blazeBuilder = null; };
   aeson_0_7_0_6 = callPackage ../development/libraries/haskell/aeson/0.7.0.6.nix { blazeBuilder = null; };
   aeson_0_8_0_0 = callPackage ../development/libraries/haskell/aeson/0.8.0.0.nix { blazeBuilder = null; };
-  aeson = self.aeson_0_8_0_0;
+  aeson = self.aeson_0_7_0_6;   # 0.8.0.0 breaks too many packages at the moment
 
   aesonPretty = callPackage ../development/libraries/haskell/aeson-pretty {};
 
