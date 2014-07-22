1add807fdfa03c09a94f2bb55526d9385d6a9012
Peter Simons <simons@cryp.to>
2014-07-22 12:20:23 +0200

    haskell-parsers: switch to version 0.12

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -1745,9 +1745,8 @@ self : let callPackage = x : y : modifyPrio (newScope self x y); in
   parsec = self.parsec_3_1_5;
 
   parsers_0_10_3 = callPackage ../development/libraries/haskell/parsers/0.10.3.nix {};
-  parsers_0_11_0_3 = callPackage ../development/libraries/haskell/parsers/0.11.0.3.nix {};
   parsers_0_12 = callPackage ../development/libraries/haskell/parsers/0.12.nix {};
-  parsers = self.parsers_0_11_0_3; # the new version breaks trifecta
+  parsers = self.parsers_0_12;
 
   parsimony = callPackage ../development/libraries/haskell/parsimony {};
 
