bf0d518c6733873a67989c4781d56f8b0fccbc46
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-07-23 11:47:46 +0200

    stdenv-darwin: Shut up some warnings from SDK headers
    The Carbon headers spew out zillions of multichar / deprecations warnings,
    which isn't very helpful. So turn them off.

--- pkgs/stdenv/nix/default.nix
+++ pkgs/stdenv/nix/default.nix
@@ -15,9 +15,8 @@ import ../generic rec {
       stripAllFlags=" " # the Darwin "strip" command doesn't know "-s"
       xargsFlags=" "
       export MACOSX_DEPLOYMENT_TARGET=10.6
-      export NIX_CFLAGS_COMPILE+=" --sysroot=/var/empty"
       xcodePath=$(/usr/bin/xcrun --show-sdk-path 2> /dev/null || true)
-      export NIX_CFLAGS_COMPILE+=" -idirafter $xcodePath/usr/include -F$xcodePath/System/Library/Frameworks"
+      export NIX_CFLAGS_COMPILE+=" --sysroot=/var/empty -idirafter $xcodePath/usr/include -F$xcodePath/System/Library/Frameworks -Wno-multichar -Wno-deprecated-declarations"
       export NIX_LDFLAGS_AFTER+=" -L$xcodePath/usr/lib"
     '' else "");
 
