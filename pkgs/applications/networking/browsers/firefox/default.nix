d1fddb2698b37e0645f7e5bffdf964ca866fdb93
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-07-23 15:34:32 +0200

    firefox: Update to 31.0
    Also boldly re-enable parallel building.
    
    (cherry picked from commit 21c7ed54e3617d5561d5ff4c72a8c7505fdce060)

--- pkgs/applications/networking/browsers/firefox/default.nix
+++ pkgs/applications/networking/browsers/firefox/default.nix
@@ -17,14 +17,14 @@ assert stdenv.gcc ? libc && stdenv.gcc.libc != null;
 
 rec {
 
-  firefoxVersion = "29.0.1";
+  firefoxVersion = "31.0";
 
-  xulVersion = "29.0.1"; # this attribute is used by other packages
+  xulVersion = "31.0"; # this attribute is used by other packages
 
 
   src = fetchurl {
     url = "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/${firefoxVersion}/source/firefox-${firefoxVersion}.source.tar.bz2";
-    sha1 = "2819ef63403de2bcfff5496bd21a3b8cb5dfce82";
+    sha1 = "a6c3e25ee3aeb7da42db2aaeb50a385d63532beb";
   };
 
   commonConfigureFlags =
@@ -78,7 +78,7 @@ rec {
         "--disable-javaxpcom"
       ] ++ commonConfigureFlags;
 
-    #enableParallelBuilding = true; # cf. https://github.com/NixOS/nixpkgs/pull/1699#issuecomment-35196282
+    enableParallelBuilding = true;
 
     preConfigure =
       ''
