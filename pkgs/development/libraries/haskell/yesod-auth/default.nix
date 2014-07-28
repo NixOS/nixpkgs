ad0bd2a79596ac5631eaefb7838e19ea789905e1
Peter Simons <simons@cryp.to>
2014-07-28 12:18:57 +0200

    haskell-yesod-auth: update to version 1.3.2

--- pkgs/development/libraries/haskell/yesod-auth/default.nix
+++ pkgs/development/libraries/haskell/yesod-auth/default.nix
@@ -12,8 +12,8 @@
 
 cabal.mkDerivation (self: {
   pname = "yesod-auth";
-  version = "1.3.1.1";
-  sha256 = "0mw04v8gnhv9gnv7kv2v1x5df63zjdmi52r5bv1fzqay1s5b83ir";
+  version = "1.3.2";
+  sha256 = "0k0p5wjrczy2hiplz9wqav88sp4g24w9qvx7vzaii1zlslbmfnnf";
   buildDepends = [
     aeson attoparsecConduit authenticate base16Bytestring
     base64Bytestring binary blazeBuilder blazeHtml blazeMarkup byteable
