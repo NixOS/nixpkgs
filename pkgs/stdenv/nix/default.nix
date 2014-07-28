fb4e2e112fffa76d92433d8ffc7b7c5e19888c79
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-07-28 09:14:13 +0200

    Export SDKROOT

--- pkgs/stdenv/nix/default.nix
+++ pkgs/stdenv/nix/default.nix
@@ -15,7 +15,7 @@ import ../generic rec {
       stripAllFlags=" " # the Darwin "strip" command doesn't know "-s"
       xargsFlags=" "
       export MACOSX_DEPLOYMENT_TARGET=10.6
-      SDKROOT=$(/usr/bin/xcrun --show-sdk-path 2> /dev/null || true)
+      export SDKROOT=$(/usr/bin/xcrun --show-sdk-path 2> /dev/null || true)
       export NIX_CFLAGS_COMPILE+=" --sysroot=/var/empty -idirafter $SDKROOT/usr/include -F$xcodePath/System/Library/Frameworks -Wno-multichar -Wno-deprecated-declarations"
       export NIX_LDFLAGS_AFTER+=" -L$SDKROOT/usr/lib"
     '' else "");
