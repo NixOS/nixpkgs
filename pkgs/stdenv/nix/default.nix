3e4a382d6753a057256c7ef1e9f52ae9e07bd677
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-07-28 09:37:23 +0200

    Doh

--- pkgs/stdenv/nix/default.nix
+++ pkgs/stdenv/nix/default.nix
@@ -16,7 +16,7 @@ import ../generic rec {
       xargsFlags=" "
       export MACOSX_DEPLOYMENT_TARGET=10.6
       export SDKROOT=$(/usr/bin/xcrun --show-sdk-path 2> /dev/null || true)
-      export NIX_CFLAGS_COMPILE+=" --sysroot=/var/empty -idirafter $SDKROOT/usr/include -F$xcodePath/System/Library/Frameworks -Wno-multichar -Wno-deprecated-declarations"
+      export NIX_CFLAGS_COMPILE+=" --sysroot=/var/empty -idirafter $SDKROOT/usr/include -F$SDKROOT/System/Library/Frameworks -Wno-multichar -Wno-deprecated-declarations"
       export NIX_LDFLAGS_AFTER+=" -L$SDKROOT/usr/lib"
     '' else "");
 
