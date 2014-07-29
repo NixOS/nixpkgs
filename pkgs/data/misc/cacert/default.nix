b9c457ba1225f4f83aa6a299fadbcd80a23e4562
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-07-29 16:10:52 +0200

    cacert: Update to 20140704

--- pkgs/data/misc/cacert/default.nix
+++ pkgs/data/misc/cacert/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "cacert-20131205";
+  name = "cacert-20140704";
 
   src = fetchurl {
     url = "http://tarballs.nixos.org/${name}.pem.bz2";
-    sha256 = "049cm3nrhawkh9xpfjhgis6w58zji5ppi4d9yyjzrr7mpw0a34df";
+    sha256 = "05ymb7lrxavscbpx5xywlbya9q66r26fbngfif6zrvfpf3qskiza";
   };
 
   unpackPhase = "true";
