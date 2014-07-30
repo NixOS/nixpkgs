6077ef462e664f6d25838f13b45309ae91ed5ca4
Vladimír Čunát <vcunat@gmail.com>
2014-07-30 09:34:05 +0200

    Merge #3400: kernel updates
    (cherry picked from commit 33e4a7f6232c6ec17cd31370f453e704f949fea6)

--- pkgs/os-specific/linux/kernel/linux-3.10.nix
+++ pkgs/os-specific/linux/kernel/linux-3.10.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.10.48";
+  version = "3.10.50";
   extraMeta.branch = "3.10";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "14gz998vr9jb9blbk60phq3hwnl2yipd6fykkl5bd8gai5wph2l3";
+    sha256 = "1ay7hscaa4nyvpa4l10y8himyj5fp25n63nxvrccqqqir3b8l1gz";
   };
 
   features.iwlwifi = true;
