de12367e89aac43cc29d52342b270bddba1e7fdb
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-07-30 18:13:51 +0200

    postgresql: Update to 9.0.18, 9.1.14, 9.2.9, 9.3.5
    (cherry picked from commit e4e5502966636e1752005664c04baa06d13965a1)

--- pkgs/servers/sql/postgresql/9.2.x.nix
+++ pkgs/servers/sql/postgresql/9.2.x.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, zlib, readline }:
 
-let version = "9.2.8"; in
+let version = "9.2.9"; in
 
 stdenv.mkDerivation rec {
   name = "postgresql-${version}";
 
   src = fetchurl {
     url = "mirror://postgresql/source/v${version}/${name}.tar.bz2";
-    sha256 = "568ba482340219097475cce9ab744766889692ee7c9df886563e8292d66ed87c";
+    sha256 = "94ec6d330f125b6fc725741293073b07d7d20cc3e7b8ed127bc3d14ad2370197";
   };
 
   buildInputs = [ zlib readline ];
