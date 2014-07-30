6a5b3656893e92b73fba13b1b26716fcce2ddb60
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-07-30 13:17:34 +0200

    nixUnstable: Update to 1.8pre3711_45f9a91

--- pkgs/tools/package-management/nix/unstable.nix
+++ pkgs/tools/package-management/nix/unstable.nix
@@ -5,11 +5,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "nix-1.8pre3705_71a20d4";
+  name = "nix-1.8pre3711_45f9a91";
 
   src = fetchurl {
-    url = "http://hydra.nixos.org/build/12722887/download/5/${name}.tar.xz";
-    sha256 = "832905be494280c8dcb377e389fb9c6c77993508bb457e11720ba2b53dd580ae";
+    url = "http://hydra.nixos.org/build/12967850/download/5/${name}.tar.xz";
+    sha256 = "826d2cc0f6a140b7b93bb31f4ef67cb04bda6423239c58d0e9c33d70434023eb";
   };
 
   nativeBuildInputs = [ perl pkgconfig ];
