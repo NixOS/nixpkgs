36480116b412a10985644cd8100a30c4d827ea40
NOTtheMessiah <brian.cohen.88@gmail.com>
2014-08-05 10:58:24 -0400

    added license for gcolor2 and myself to maintainers

--- lib/maintainers.nix
+++ lib/maintainers.nix
@@ -65,6 +65,7 @@
   modulistic = "Pablo Costa <modulistic@gmail.com>";
   mornfall = "Petr Ročkai <me@mornfall.net>";
   msackman = "Matthew Sackman <matthew@wellquite.org>";
+  notthemessiah = "Brian Cohen <brian.cohen.88@gmail.com>";
   ocharles = "Oliver Charles <ollie@ocharles.org.uk>";
   offline = "Jaka Hudoklin <jakahudoklin@gmail.com>";
   orbitz = "Malcolm Matalka <mmatalka@gmail.com>";
