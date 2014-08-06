e592a0e94bb6753834cdcdfa753317a0febceb11
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-08-06 13:55:02 +0200

    test-instrumentation.nix: Set an empty root password
    This makes it easier to log in during interactive sessions.
    
    (cherry picked from commit 2b9ea7fd906b759115b76d7e0f08b92f1b316dbf)

--- nixos/modules/testing/test-instrumentation.nix
+++ nixos/modules/testing/test-instrumentation.nix
@@ -97,6 +97,9 @@ let kernel = config.boot.kernelPackages.kernel; in
 
     networking.usePredictableInterfaceNames = false;
 
+    # Make it easy to log in as root when running the test interactively.
+    security.initialRootPassword = "";
+
   };
 
 }
