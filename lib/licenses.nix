ae48ab82ad8022c7521cb4302d9c73293c4ff4c7
William A. Kennington III <william@wkennington.com>
2014-08-06 23:07:35 -0500

    license: Add isc

--- lib/licenses.nix
+++ lib/licenses.nix
@@ -121,6 +121,12 @@ rec {
     url = https://fedoraproject.org/wiki/Licensing/GPL_Classpath_Exception;
   };
 
+  isc = {
+    shortName = "ISC License";
+    fullName = "Internet Systems Consortium License";
+    url = http://www.isc.org/downloads/software-support-policy/isc-license/;
+  };
+
   inria = {
     shortName = "INRIA-NCLA";
     fullName  = "INRIA Non-Commercial License Agreement";
