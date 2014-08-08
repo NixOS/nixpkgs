50afd7f3e7589c20c546b0c30bae2ecf827d20ed
Peter Simons <simons@cryp.to>
2014-08-09 00:43:42 +0200

    haskell-network: don't build old versions of this package with a recent GHC

--- pkgs/development/libraries/haskell/network/2.2.1.4.nix
+++ pkgs/development/libraries/haskell/network/2.2.1.4.nix
@@ -11,6 +11,7 @@ cabal.mkDerivation (self: {
     description = "Networking-related facilities";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    hydraPlatforms = self.stdenv.lib.platforms.none;
     maintainers = [ self.stdenv.lib.maintainers.andres ];
   };
 })
