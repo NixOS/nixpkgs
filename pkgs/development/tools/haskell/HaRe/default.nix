faa33d58ce11a3487e7c599f465141a03ef7c0d1
Peter Simons <simons@cryp.to>
2014-08-09 00:29:42 +0200

    HaRe: don't build this with Hydra; the build is broken because of missing dependencies

--- pkgs/development/tools/haskell/HaRe/default.nix
+++ pkgs/development/tools/haskell/HaRe/default.nix
@@ -30,6 +30,7 @@ cabal.mkDerivation (self: {
     description = "the Haskell Refactorer";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    hydraPlatforms = self.stdenv.lib.platforms.none;
     maintainers = [ self.stdenv.lib.maintainers.andres ];
   };
 })
