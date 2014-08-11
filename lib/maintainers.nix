565b531f32663757e3bdf30fe0a481945d16e1bc
Christoph Hrdinka <c.github@hrdinka.at>
2014-08-11 21:33:08 +0200

    Add hrdinka to maintainers

--- lib/maintainers.nix
+++ lib/maintainers.nix
@@ -46,6 +46,7 @@
   goibhniu = "Cillian de Róiste <cillian.deroiste@gmail.com>";
   guibert = "David Guibert <david.guibert@gmail.com>";
   hinton = "Tom Hinton <t@larkery.com>";
+  hrdinka = "Christoph Hrdinka <c.nix@hrdinka.at>";
   ianwookim = "Ian-Woo Kim <ianwookim@gmail.com>";
   iElectric = "Domen Kozar <domen@dev.si>";
   iyzsong = "Song Wenwu <iyzsong@gmail.com>";
