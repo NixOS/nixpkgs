9217c7995cb6df487a1eb17ee8240c5a290454ce
Vladimír Čunát <vcunat@gmail.com>
2014-08-11 13:53:40 +0200

    makeFontsConf: give up fontDirectories checks for now
    This reverts e26188938c3142 and 77487fe6615f
    CC #3515.

--- pkgs/development/libraries/fontconfig/make-fonts-conf.nix
+++ pkgs/development/libraries/fontconfig/make-fonts-conf.nix
@@ -6,14 +6,6 @@ runCommand "fonts.conf"
     inherit fontDirectories;
   }
   ''
-    for fd in $fontDirectories;
-    do
-      if [ ! -d "$fd/share/fonts" ] && [ ! -d "$fd/lib/X11/fonts" ]; then
-        echo "ERROR: '$fd/' contains neither 'share/fonts/' nor 'lib/X11/fonts/'"
-        false
-      fi
-    done
-
     xsltproc --stringparam fontDirectories "$fontDirectories" \
       --stringparam fontconfig "${fontconfig}" \
       --path ${fontconfig}/share/xml/fontconfig \
