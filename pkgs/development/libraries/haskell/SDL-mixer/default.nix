793c7502ce13c946a44c230e0ff0749b0e0d0399
Peter Simons <simons@cryp.to>
2014-08-11 14:13:36 +0200

    haskell-SDL-mixer: disable Hydra builds as this package depends on broken 'SDL'

--- pkgs/development/libraries/haskell/SDL-mixer/default.nix
+++ pkgs/development/libraries/haskell/SDL-mixer/default.nix
@@ -12,5 +12,6 @@ cabal.mkDerivation (self: {
     description = "Binding to libSDL_mixer";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    hydraPlatforms = self.stdenv.lib.platforms.none;
   };
 })
