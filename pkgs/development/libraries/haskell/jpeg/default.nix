5b4ef079fb2a000b65918d594e131bc43fa6d3d0
Peter Simons <simons@cryp.to>
2014-08-12 10:47:56 +0200

    haskell-jpeg: update to version 0.0.1.1

--- pkgs/development/libraries/haskell/jpeg/default.nix
+++ pkgs/development/libraries/haskell/jpeg/default.nix
@@ -4,14 +4,12 @@
 
 cabal.mkDerivation (self: {
   pname = "jpeg";
-  version = "0.0.1";
-  sha256 = "848e047cfec5781a28f472e9cd27d016362211d88dd6adb4f826c37d29d8bba6";
+  version = "0.0.1.1";
+  sha256 = "1hnfapr21zpfyiywa4zzmwa518jzg73dnmaakrbvvpcmr4fvh9qx";
   buildDepends = [ mtl ];
   meta = {
     description = "A library for decoding JPEG files written in pure Haskell";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
-    hydraPlatforms = self.stdenv.lib.platforms.none;
-    broken = true;
   };
 })
