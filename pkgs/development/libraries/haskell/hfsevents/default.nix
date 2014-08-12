82f70114b8a0da74d487b94035a0074d8b1b5f75
Peter Simons <simons@cryp.to>
2014-08-12 11:03:28 +0200

    haskell-hfsevents: re-generate expression with cabal2nix

--- pkgs/development/libraries/haskell/hfsevents/default.nix
+++ pkgs/development/libraries/haskell/hfsevents/default.nix
@@ -7,7 +7,6 @@ cabal.mkDerivation (self: {
   version = "0.1.5";
   sha256 = "0hp9jjj59smfcs51d9zjhyvgdbn46l0rl0jr98wbzg3qya0vwj5k";
   buildDepends = [ cereal mtl text ];
-  buildTools = [ ];
   meta = {
     homepage = "http://github.com/luite/hfsevents";
     description = "File/folder watching for OS X";
