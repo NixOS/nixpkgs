f4a9d3c0d0f9b1bddd79b91e103fe91db9975422
aszlig <aszlig@redmoonstudios.org>
2014-08-13 04:53:31 +0200

    chromium: Clean up/remove old/unused stuff.
    We no longer need to supply compiler and binutils to the build process,
    se we can safely remove them. In addition, we're now passing the new
    options linux_use_gold_binary and linux_use_bundled_gold to gyp, for
    details, see:
    
    https://codereview.chromium.org/239163003
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/applications/networking/browsers/chromium/common.nix
+++ pkgs/applications/networking/browsers/chromium/common.nix
@@ -10,7 +10,7 @@
 , python, pythonPackages, perl, pkgconfig
 , nspr, udev, krb5
 , utillinux, alsaLib
-, gcc, bison, gperf
+, bison, gperf
 , glib, gtk, dbus_glib
 , libXScrnSaver, libXcursor, libXtst, mesa
 , protobuf, speechd, libXdamage
@@ -145,6 +145,8 @@ let
     '';
 
     gypFlags = mkGypFlags (gypFlagsUseSystemLibs // {
+      linux_use_bundled_binutils = false;
+      linux_use_bundled_gold = false;
       linux_use_gold_binary = false;
       linux_use_gold_flags = false;
       proprietary_codecs = false;
@@ -192,20 +194,15 @@ let
     '';
 
     buildPhase = let
-      CC = "${gcc}/bin/gcc";
-      CXX = "${gcc}/bin/g++";
       buildCommand = target: let
         # XXX: Only needed for version 36 and older!
         targetSuffix = optionalString
           (versionOlder source.version "37.0.0.0" && target == "mksnapshot")
           (if stdenv.is64bit then ".x64" else ".ia32");
       in ''
-        CC="${CC}" CC_host="${CC}"     \
-        CXX="${CXX}" CXX_host="${CXX}" \
-        LINK_host="${CXX}"             \
-          "${ninja}/bin/ninja" -C "${buildPath}"  \
-            -j$NIX_BUILD_CORES -l$NIX_BUILD_CORES \
-            "${target}${targetSuffix}"
+        "${ninja}/bin/ninja" -C "${buildPath}"  \
+          -j$NIX_BUILD_CORES -l$NIX_BUILD_CORES \
+          "${target}${targetSuffix}"
       '' + optionalString (target == "mksnapshot" || target == "chrome") ''
         paxmark m "${buildPath}/${target}${targetSuffix}"
       '';
