f0a363c910c4c05687feaad4416e551d66179db8
Vladimír Čunát <vcunat@gmail.com>
2014-08-14 23:04:40 +0200

    sync from master: llvm-3.4 maintenance update

--- pkgs/development/compilers/llvm/3.4/dragonegg.nix
+++ pkgs/development/compilers/llvm/3.4/dragonegg.nix
@@ -1,9 +1,17 @@
-{stdenv, fetch, llvm, gmp, mpfr, mpc, ncurses, zlib, version}:
+{stdenv, fetch, fetchpatch, llvm, gmp, mpfr, mpc, ncurses, zlib, version}:
 
 stdenv.mkDerivation rec {
   name = "dragonegg-${version}";
 
   src = fetch "dragonegg" "1733czbvby1ww3xkwcwmm0km0bpwhfyxvf56wb0zv5gksp3kbgrl";
+
+  patches = [(fetchpatch {
+    url = "https://llvm.org/viewvc/llvm-project/dragonegg/trunk/src/x86/ABIHack.inc"
+      + "?r1=208730&r2=208729&view=patch";
+    sha256 = "1al82gqz90hzjx24p0wls029lw2bgnlgd209kgvxsp82p4z1v1c1";
+    name = "bug-18548.patch";
+  })];
+  patchFlags = "-p2";
 
   # The gcc the plugin will be built for (the same used building dragonegg)
   GCC = "gcc";
