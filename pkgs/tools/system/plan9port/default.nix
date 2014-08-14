a149b1f31f6f1e4110d892ce50a0e48f2396cfa7
Vladimír Čunát <vcunat@gmail.com>
2014-08-15 00:11:28 +0200

    plan9port: enableParallelBuilding, improve meta

--- pkgs/tools/system/plan9port/default.nix
+++ pkgs/tools/system/plan9port/default.nix
@@ -12,17 +12,20 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://plan9port.googlecode.com/files/${name}.tgz";
-	# Google code is much faster than swtch
+    # Google code is much faster than swtch
     # url = "http://swtch.com/plan9port/${name}.tgz";
     sha256 = "1sza12j3db7i54r3pzli8wmby6aiyzmyfj8w0nidmawkwv6jdf6b";
   };
 
   buildInputs = stdenv.lib.optionals (!stdenv.isDarwin) [ libX11 xproto libXt xextproto libXext ];
 
-  meta = {
+  enableParallelBuilding = true;
+
+  meta = with stdenv.lib; {
     homepage = "http://swtch.com/plan9port/";
     description = "Plan 9 from User Space";
-    license="free";
+    license = licenses.lpl102;
+    platforms = platforms.unix;
   };
 
   inherit libXt;
