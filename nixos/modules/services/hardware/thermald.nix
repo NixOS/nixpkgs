1da35629cc4c21dcd45ea4d07df2b69325b526d3
Thomas Strobel <ts468@cam.ac.uk>
2014-08-14 12:42:16 +0200

    Cleanup: remove newlines.

--- nixos/modules/services/hardware/thermald.nix
+++ nixos/modules/services/hardware/thermald.nix
@@ -4,37 +4,25 @@ with lib;
 
 let
   cfg = config.services.thermald;
-
 in {
-
   ###### interface
-
   options = { 
-
     services.thermald = { 
-
       enable = mkOption {
         default = false;
         description = ''
           Whether to enable thermald, the temperature management daemon.
         ''; 
       };  
-
     };  
-
   };  
 
-
   ###### implementation
-
   config = mkIf cfg.enable {
-
     systemd.services.thermald = {
       description = "Thermal Daemon Service";
       wantedBy = [ "multi-user.target" ];
       script = "exec ${pkgs.thermald}/sbin/thermald --no-daemon --dbus-enable";
     };
-
   };
-
 }
