f9defef00d57de1037d76c5e2194d55dba54157c
Jaka Hudoklin <jakahudoklin@gmail.com>
2014-08-16 23:53:03 +0200

    Add neo4j database

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6913,6 +6913,8 @@ let
 
   nagiosPluginsOfficial = callPackage ../servers/monitoring/nagios/plugins/official-2.x.nix { };
 
+  neo4j = callPackage ../servers/nosql/neo4j { };
+
   net_snmp = callPackage ../servers/monitoring/net-snmp { };
 
   oidentd = callPackage ../servers/identd/oidentd { };
