e2644456dc98b30277e863322ad8f9f431387a36
Danny Wilson <danny@decube.net>
2014-08-18 18:46:32 +0200

    Add Oracle JDK 7 for Illumos

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2885,9 +2885,13 @@ let
     then pkgs.openjre
     else pkgs.oraclejre;
 
-  oraclejdk = pkgs.jdkdistro true false;
+  oraclejdk = if !stdenv.isSunOS
+    then pkgs.jdkdistro true false
+    else pkgs.oraclejdk7;
 
-  oraclejdk7 = pkgs.oraclejdk7distro true false;
+  oraclejdk7 = if !stdenv.isSunOS
+    then pkgs.oraclejdk7distro true false
+    else (callPackage ../development/compilers/jdk/jdk7-solaris.nix { });
 
   oraclejre = lowPrio (pkgs.jdkdistro false false);
 
