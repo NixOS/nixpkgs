87393f2f2bd70894729cef115318af0a09849292
Austin Seipp <aseipp@pobox.com>
2014-08-20 01:44:01 -0500

    lockdep: 3.14.4 -> 3.16.1
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/os-specific/linux/lockdep/default.nix
+++ pkgs/os-specific/linux/lockdep/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name    = "lockdep-${version}";
-  version = "3.14.4";
+  version = "3.16.1";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "05yp192pbjng92rpvq3fd4mhjc96iylwf6xb7as5lscwg660m1b5";
+    sha256 = "0wbxqlmk7w9047ir51dsz6vi7ww0hpycgrb43mk2a189xaldsdxy";
   };
 
   preConfigure = "cd tools/lib/lockdep";
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
     cp -R include/liblockdep $out/include
     make install DESTDIR=$out prefix=""
 
-    substituteInPlace $out/bin/lockdep --replace "./liblockdep.so" "$out/lib/liblockdep.so"
+    substituteInPlace $out/bin/lockdep --replace "./liblockdep.so" "$out/lib/liblockdep.so.$version"
   '';
 
   meta = {
