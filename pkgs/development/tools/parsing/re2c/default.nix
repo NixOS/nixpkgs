9f5eba94d801dd5ffd19a2922bcb9fff0654722e
Austin Seipp <aseipp@pobox.com>
2014-08-20 01:12:21 -0500

    re2c: 0.13.6 -> 0.13.7.4
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/development/tools/parsing/re2c/default.nix
+++ pkgs/development/tools/parsing/re2c/default.nix
@@ -1,13 +1,19 @@
 {stdenv, fetchurl }:
 
-stdenv.mkDerivation {
-  name = "re2c-0.13.6";
+stdenv.mkDerivation rec {
+  name = "re2c-${version}";
+  version = "0.13.7.4";
+
   src = fetchurl {
-    url = mirror://sourceforge/re2c/re2c/0.13.6/re2c-0.13.6.tar.gz;
-    sha256 = "1h3na1zacw3166k6wkdjzjs67frjca9wj07wgfas56c7m8wk0ilf";
+    url    = "mirror://sourceforge/re2c/re2c/${version}/${name}.tar.gz";
+    sha256 = "0j42s2gpz0rgiadwyb7ksqgc7i02l0q2qnmiyaj5f1w5rfa2c7yy";
   };
 
   meta = {
     description = "Tool for writing very fast and very flexible scanners";
+    homepage    = "http://re2c.org";
+    license     = stdenv.lib.license.publicDomain;
+    platforms   = stdenv.lib.platforms.all;
+    maintainers = [ stdenv.lib.maintainers.thoughtpolice ];
   };
 }
