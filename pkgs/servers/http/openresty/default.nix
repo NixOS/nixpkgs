b6a2f8eaa8019fa1b40e1ed160eddf3c3e2ae944
Michael Raskin <7c6f434c@mail.ru>
2014-08-21 08:42:00 +0400

    Fix a strange openresty build error

--- pkgs/servers/http/openresty/default.nix
+++ pkgs/servers/http/openresty/default.nix
@@ -46,6 +46,7 @@ stdenv.mkDerivation rec {
   preConfigure = ''
     export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${libxml2}/include/libxml2 $additionalFlags"
     export PATH="$PATH:${stdenv.gcc.libc}/sbin"
+    patchShebangs .
   '';
 
   meta = {
