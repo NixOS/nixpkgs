219983d6ad1bec55ab174d064d41d086654f9a17
Sam Griffin <sam.griffin@gmail.com>
2014-08-20 23:23:44 -0400

    adding tomcat version 8

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7023,6 +7023,8 @@ let
   tomcat6 = callPackage ../servers/http/tomcat/6.0.nix { };
 
   tomcat7 = callPackage ../servers/http/tomcat/7.0.nix { };
+	
+  tomcat8 = callPackage ../servers/http/tomcat/8.0.nix { };
 
   tomcat_mysql_jdbc = callPackage ../servers/http/tomcat/jdbc/mysql { };
 
