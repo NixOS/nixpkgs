e2db82874cf497c0dd156680d4c4a8f3b028125c
Vincent Laporte <vincent.laporte@gmail.com>
2014-08-22 14:56:33 +0200

    Updates merlin to 1.7.1

--- pkgs/development/tools/ocaml/merlin/default.nix
+++ pkgs/development/tools/ocaml/merlin/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, ocaml, findlib, yojson, menhir}:
 stdenv.mkDerivation {
 
-  name = "merlin-1.7";
+  name = "merlin-1.7.1";
 
   src = fetchurl {
-    url = "https://github.com/the-lambda-church/merlin/archive/v1.7.tar.gz";
-    sha256 = "0grprrykah02g8va63lidbcb6n8sd18l2k9spb5rwlcs3xhfw42b";
+    url = https://github.com/the-lambda-church/merlin/archive/v1.7.1.tar.gz;
+    sha256 = "c3b60c7b3fddaa2860e0d8ac0d4fed2ed60e319875734c7ac1a93df524c67aff";
   };
 
   buildInputs = [ ocaml findlib yojson menhir ];
