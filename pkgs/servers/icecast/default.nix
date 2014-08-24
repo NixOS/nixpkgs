e6f70fd2b3c74ded46a6c310e7d972f885242c37
Emery Hemingway <emery@vfemail.net>
2014-08-24 13:29:26 -0400

    icecast: update 2.3.3 to 2.4.0
    now with opus support

--- pkgs/servers/icecast/default.nix
+++ pkgs/servers/icecast/default.nix
@@ -1,28 +1,28 @@
 {stdenv, fetchurl
 , libxml2, libxslt, curl
-, libvorbis, libtheora, speex, libkate }:
+, libvorbis, libtheora, speex, libkate, libopus }:
 
 stdenv.mkDerivation rec {
-  name = "icecast-2.3.3";
+  name = "icecast-2.4.0";
 
   src = fetchurl {
     url = "http://downloads.xiph.org/releases/icecast/${name}.tar.gz";
-    sha256 = "0vf38mk13z1czpbj0g8va4rzjf201slqmiwcs8y9i6iwz3shc78v";
+    sha256 = "0chg8v3c0wkbakjcw73rsfccx13f28arrmmbz9p5fsmiw5bykdqp";
   };
 
-  buildInputs = [ libxml2 libxslt curl libvorbis libtheora speex libkate ];
+  buildInputs = [ libxml2 libxslt curl libvorbis libtheora speex libkate libopus ];
 
   meta = {
     description = "Server software for streaming multimedia";
 
     longDescription = ''
-      Icecast is a streaming media server which currently supports Ogg Vorbis and MP3
-      audio streams. It can be used to create an Internet radio station or a
-      privately running jukebox and many things in between. It is very versatile in
-      that new formats can be added relatively easily and supports open standards for
-      commuincation and interaction.
+      Icecast is a streaming media server which currently supports
+      Ogg (Vorbis and Theora), Opus, WebM and MP3 audio streams.
+      It can be used to create an Internet radio station or a privately
+      running jukebox and many things in between. It is very versatile
+      in that new formats can be added relatively easily and supports
+      open standards for commuincation and interaction.
     '';
-  
 
     homepage = http://www.icecast.org;
     license = stdenv.lib.licenses.gpl2;
