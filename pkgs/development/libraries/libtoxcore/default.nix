d2739c17ef622cd0d7ad77581202c9b75f854fa3
Domen Kožar <domen@dev.si>
2014-08-24 17:47:38 +0200

    libtoxcore: upgrade for a few rev to get i686-linux build working
    (cherry picked from commit be6ae818dcabf16068ff13ad397056547dd74cc8)

--- pkgs/development/libraries/libtoxcore/default.nix
+++ pkgs/development/libraries/libtoxcore/default.nix
@@ -2,8 +2,8 @@
 , libvpx, check, libconfig, pkgconfig }:
 
 let
-  version = "e1158be5a6";
-  date = "20140728";
+  version = "f83fcbb13c0";
+  date = "20140811";
 in
 stdenv.mkDerivation rec {
   name = "tox-core-${date}-${version}";
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "https://github.com/irungentoo/toxcore/tarball/${version}";
     name = "${name}.tar.gz";
-    sha256 = "1rsh1pbwvngsx5slmd6608b1zqs3jvq70bjr9zyziap9vxka3z1v";
+    sha256 = "09g74h3qnx9adyxxvzay8m2idbgbln7m4kkm7sg9925mvi5abb1w";
   };
 
   NIX_LDFLAGS = "-lgcc_s";
