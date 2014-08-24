9b6db1c9643a6bdfd0618c09b78b74875fde5358
Peter Simons <simons@cryp.to>
2014-08-24 11:45:56 +0200

    Re-generate Haskell packages with cabal2nix.

--- pkgs/development/libraries/haskell/hdaemonize/default.nix
+++ pkgs/development/libraries/haskell/hdaemonize/default.nix
@@ -12,6 +12,6 @@ cabal.mkDerivation (self: {
     description = "Library to handle the details of writing daemons for UNIX";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
-    maintainers = [ self.stdenv.lib.maintainers.aycanirican ];
+    maintainers = with self.stdenv.lib.maintainers; [ aycanirican ];
   };
 })
