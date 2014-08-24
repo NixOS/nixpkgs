72df3f37e55aa1e14733b03482c602114bf56dcd
Matthew Brown <matt@ederoyd.co.uk>
2014-08-24 11:17:31 +0100

    Updated definition file as per comments from the collaborator

--- pkgs/servers/search/sphinxsearch/default.nix
+++ pkgs/servers/search/sphinxsearch/default.nix
@@ -1,12 +1,10 @@
-{ stdenv, fetchurl, pkgconfig, mysql }:
-
-let
-  version = "2.1.9";
+{ stdenv, fetchurl, pkgconfig, mysql,
+  version = "2.1.9",
   mainSrc = fetchurl {
     url = "http://sphinxsearch.com/files/sphinx-${version}-release.tar.gz";
     sha256 = "00vwxf3zr0g1fq9mls1z2rd8nxw74b76pkl1j466lig1qc5am2b2";
-  };
-in
+  }
+}:
 
 stdenv.mkDerivation rec {
   name = "sphinxsearch-${version}";
@@ -22,7 +20,7 @@ stdenv.mkDerivation rec {
   ];
 
   meta = {
-    description = "Sphinx is an open source full text search server.";
+    description = "An open source full text search server";
     homepage    = http://sphinxsearch.com;
     license     = stdenv.lib.licenses.gpl2;
     platforms   = stdenv.lib.platforms.all;
