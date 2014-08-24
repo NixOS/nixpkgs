cbc67e17043adc8c019d30d961986233571b7aba
Peter Simons <simons@cryp.to>
2014-08-24 12:57:51 +0200

    hsyslog: build with a recent version of Cabal on GHC 6.10.4 (and don't run Haddock)

--- pkgs/top-level/haskell-defaults.nix
+++ pkgs/top-level/haskell-defaults.nix
@@ -119,6 +119,7 @@
     happy = super.happy.override { cabal = self.cabal.override { Cabal = self.Cabal_1_16_0_3; }; };
     hashable = super.hashable.override { cabal = self.cabal.override { Cabal = self.Cabal_1_16_0_3; }; };
     hashtables = super.hashtables.override { cabal = self.cabal.override { Cabal = self.Cabal_1_16_0_3; }; };
+    hsyslog = super.hsyslog.override { cabal = self.cabal.override { Cabal = self.Cabal_1_16_0_3; }; };
     HTTP = super.HTTP.override { cabal = self.cabal.override { Cabal = self.Cabal_1_16_0_3; }; };
     HUnit = super.HUnit.override { cabal = self.cabal.override { Cabal = self.Cabal_1_16_0_3; }; };
     network = super.network_2_2_1_7.override { cabal = self.cabal.override { Cabal = self.Cabal_1_16_0_3; }; };
