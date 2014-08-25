e7b2e13ff4205ece1a4c5655c8bb101b694cb63b
Oliver Charles <ollie@ocharles.org.uk>
2014-08-25 12:47:57 +0100

    haskellPackages.wreq: Patch to work with lens >= 4.4

--- pkgs/development/libraries/haskell/wreq/default.nix
+++ pkgs/development/libraries/haskell/wreq/default.nix
@@ -3,6 +3,7 @@
 { cabal, aeson, attoparsec, doctest, exceptions, filepath
 , httpClient, httpClientTls, httpTypes, HUnit, lens, mimeTypes
 , temporary, testFramework, testFrameworkHunit, text, time
+, fetchpatch
 }:
 
 cabal.mkDerivation (self: {
@@ -24,9 +25,11 @@ cabal.mkDerivation (self: {
     homepage = "http://www.serpentine.com/wreq";
     description = "An easy-to-use HTTP client library";
     license = self.stdenv.lib.licenses.bsd3;
-    broken = true;
-    hydraPlatforms = self.stdenv.lib.platforms.none;
     platforms = self.ghc.meta.platforms;
     maintainers = with self.stdenv.lib.maintainers; [ ocharles ];
   };
+  patches = [
+    (fetchpatch { url = https://github.com/bos/wreq/commit/e8e29b62006e39ab36ffbb1d18c3e9d5923158ac.patch; sha256 = "19kqy512sa4dbzqp7kmjpsnsmc63wqh5pkh6hcvkzsji15dmlqrg"; })
+    (fetchpatch { url = https://github.com/bos/wreq/pull/20.patch; sha256 = "1qfjwz5wlmmfcg8jy0yg7ixacq5fai3yscm552fba1ph66acyvg4"; })
+  ];
 })
