d5ea3b1e4491482e21efafc32ac91822d3ee9b55
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2014-08-28 12:55:34 +0200

    parallel: Update to 20140822.
    https://savannah.gnu.org/forum/forum.php?forum_id=8067

--- pkgs/tools/misc/parallel/default.nix
+++ pkgs/tools/misc/parallel/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, perl }:
 
 stdenv.mkDerivation rec {
-  name = "parallel-20140222";
+  name = "parallel-20140822";
 
   src = fetchurl {
     url = "mirror://gnu/parallel/${name}.tar.bz2";
-    sha256 = "0zb3hg92br6a53jn0pzfl16ffc1hfw81jk7nzw5spkshsdrcqx3y";
+    sha256 = "8a146a59bc71218921d561f2c801b85e06fe3a21571083b58e6e0966dd397fd4";
   };
 
   patchPhase =
