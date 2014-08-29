5a9cf400856f379d448c09d9abf5a64512cb0793
Jaka Hudoklin <jakahudoklin@gmail.com>
2014-08-29 17:56:07 +0200

    mongodb: add offline as maintainer

--- pkgs/servers/nosql/mongodb/default.nix
+++ pkgs/servers/nosql/mongodb/default.nix
@@ -1,5 +1,7 @@
 { stdenv, fetchurl, scons, boost, gperftools, pcre, snappy }:
 
+with stdenv.lib;
+
 let version = "2.6.4";
     system-libraries = [
       "pcre"
@@ -8,9 +10,9 @@ let version = "2.6.4";
       # "v8"      -- mongo still bundles 3.12 and does not work with 3.15+
       # "stemmer" -- not nice to package yet (no versioning, no makefile, no shared libs)
       # "yaml"    -- it seems nixpkgs' yamlcpp (0.5.1) is problematic for mongo
-    ] ++ stdenv.lib.optionals (!stdenv.isDarwin) [ "tcmalloc" ];
-    system-lib-args = stdenv.lib.concatStringsSep " "
-                          (map (lib: "--use-system-${lib}") system-libraries);
+    ] ++ optionals (!stdenv.isDarwin) [ "tcmalloc" ];
+    system-lib-args = concatStringsSep " "
+                        (map (lib: "--use-system-${lib}") system-libraries);
 
 in stdenv.mkDerivation rec {
   name = "mongodb-${version}";
@@ -39,9 +41,9 @@ in stdenv.mkDerivation rec {
   meta = {
     description = "a scalable, high-performance, open source NoSQL database";
     homepage = http://www.mongodb.org;
-    license = stdenv.lib.licenses.agpl3;
+    license = licenses.agpl3;
 
-    maintainers = [ stdenv.lib.maintainers.bluescreen303 ];
-    platforms = stdenv.lib.platforms.unix;
+    maintainers = with maintainers; [ bluescreen303 offline ];
+    platforms = platforms.unix;
   };
 }
