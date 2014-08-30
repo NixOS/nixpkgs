5c3c7574e248d36b2933b476f10652b6fb525f2d
Pascal Wittmann <mail@pascal-wittmann.de>
2014-08-31 00:32:37 +0200

    lirc: update from 0.9.0 to 0.9.1, add meta information and adopt it

--- pkgs/development/libraries/lirc/default.nix
+++ pkgs/development/libraries/lirc/default.nix
@@ -1,18 +1,30 @@
-{ stdenv, fetchurl, alsaLib }:
+{ stdenv, fetchurl, alsaLib, bash, help2man }:
 
 stdenv.mkDerivation rec {
-  name = "lirc-0.9.0";
+  name = "lirc-0.9.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/lirc/${name}.tar.bz2";
-    sha256 = "1zx4mcnjwzz6jsi6ln7a3dkgx05nvg1pxxvmjqvd966ldapay8v3";
+    sha256 = "0vakq9x10hyj9k7iv35sm5f4dhxvk0miwxvv6kn0bhwkr2mnapj6";
   };
 
-  buildInputs = [ alsaLib ];
+  preBuild = "patchShebangs .";
+
+  buildInputs = [ alsaLib help2man ];
 
   configureFlags = [
     "--with-driver=devinput"
     "--sysconfdir=$(out)/etc"
     "--enable-sandboxed"
   ];
+
+  makeFlags = [ "m4dir=$(out)/m4" ];
+
+  meta = with stdenv.lib; {
+    description = "Allows to receive and send infrared signals";
+    homepage = http://www.lirc.org/;
+    license = licenses.gpl2;
+    platforms = platforms.linux
+    maintainers = with maintainers; [ pSub ];
+  };
 }
