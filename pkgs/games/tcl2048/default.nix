327cab298eae1928d4ec0662e570002244ea5846
Danyil Bohdan <danyil.bohdan@gmail.com>
2014-08-31 12:32:14 +0300

    tcl2048: update to 0.2.6

--- pkgs/games/tcl2048/default.nix
+++ pkgs/games/tcl2048/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, tcl, tcllib }:
 
 stdenv.mkDerivation {
-  name = "tcl2048-0.2.5";
+  name = "tcl2048-0.2.6";
 
   src = fetchurl {
-    url = https://raw.githubusercontent.com/dbohdan/2048-tcl/v0.2.5/2048.tcl;
-    sha256 = "b0d6e8a31dce8c1ca8dbbb8c513b50fbfb9cd6a313201941fa15531165bf68ce";
+    url = https://raw.githubusercontent.com/dbohdan/2048-tcl/v0.2.6/2048.tcl;
+    sha256 = "3a6466a214c538daec8e2d08e0c1467f10f770c74e5897bea642134e22016730";
   };
 
   builder = ./builder.sh;
