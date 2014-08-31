0847250505aab535ecdc35c6b8f3c87d8f9dd0fe
AndersonTorres <torres.anderson.85@gmail.com>
2014-08-30 23:48:45 -0300

    Higan - a quick&dirty workaround
    Higan uses a lot of hardcoded paths when it looks for runtime files
    as shaders, icons and mainly BIOS files. So, we made a q&d wrapper
    script: it copies all these files to $HOME/.config/higan in the 1st
    invocation.

--- pkgs/misc/emulators/higan/builder.sh
+++ pkgs/misc/emulators/higan/builder.sh
@@ -18,3 +18,25 @@ install -m 644 ananke/libananke.so $out/lib/libananke.so.1
 (cd $out/lib && ln -s libananke.so.1 libananke.so)
 oldRPath=$(patchelf --print-rpath $out/bin/higan)
 patchelf --set-rpath $oldRPath:$out/lib $out/bin/higan
+
+# A dirty workaround, suggested by @cpages:
+# we create a wrapper script to set up
+# $HOME local configuration before higan runs
+
+mv $out/bin/higan $out/bin/.higan-wrapped
+cat <<EOF > $out/bin/higan 
+
+#!/bin/bash
+if [ ! -e \$HOME/.config/higan/.was_configured ]
+then
+    cp --update --recursive $out/share/higan \$HOME/.config
+    chmod --recursive u+w \$HOME/.config/higan
+    touch \$HOME/.config/higan/.was_configured
+fi
+# LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$out/lib
+$out/bin/.higan-wrapped "\$@"
+
+EOF
+
+patchShebangs $out/bin/higan
+chmod +x $out/bin/higan
