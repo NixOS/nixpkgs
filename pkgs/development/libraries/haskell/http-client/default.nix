536fd769bf8afb6615206e18a0ef4a082fed1061
Peter Simons <simons@cryp.to>
2014-08-31 13:13:23 +0200

    haskell-http-client: update to version 0.3.8

--- pkgs/development/libraries/haskell/http-client/default.nix
+++ pkgs/development/libraries/haskell/http-client/default.nix
@@ -9,8 +9,8 @@
 
 cabal.mkDerivation (self: {
   pname = "http-client";
-  version = "0.3.7.2";
-  sha256 = "1llrf2bfbh5z01pwg40zdgmz93h45h60mg2pv1k6b8pmzlwr6aaz";
+  version = "0.3.8";
+  sha256 = "0v5j6fcigjpksj1m0n15g3j680k7qkms3cqd0b1w0ma7k63kcibc";
   buildDepends = [
     base64Bytestring blazeBuilder caseInsensitive cookie
     dataDefaultClass deepseq exceptions filepath httpTypes mimeTypes
