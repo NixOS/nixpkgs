6ad299460caccd4c4ab928d7988f7a1f0460fea8
Longrin Wischnewski <robberer@freakmail.de>
2014-08-31 13:59:29 +0200

    rdesktop: update to version 1.8.2
    rdesktop: add meta fields

--- pkgs/applications/networking/remote/rdesktop/default.nix
+++ pkgs/applications/networking/remote/rdesktop/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation (rec {
   pname = "rdesktop";
-  version = "1.8.1";
+  version = "1.8.2";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/${pname}/${name}.tar.gz";
-    sha256 = "0il248cdsxvwjsl4bswf27ld9r1a7d48jf6bycr86kf3i55q7k3n";
+    sha256 = "0y0s0qjfsflp4drcn75ykx6as7mn13092bcvlp2ibhilkpa27gzv";
   };
 
   buildInputs = [openssl libX11];
@@ -20,5 +20,8 @@ stdenv.mkDerivation (rec {
 
   meta = {
     description = "Open source client for Windows Terminal Services";
+    homepage = http://www.rdesktop.org/;
+    platforms = stdenv.lib.platforms.linux;
+    license     = stdenv.lib.licenses.gpl2;
   };
 })
