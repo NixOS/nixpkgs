2a1a814e5356a93a5c477352a31b6239ae481492
Michael Raskin <7c6f434c@mail.ru>
2014-09-02 01:36:10 +0400

    Make console-getty only used inside container by default

--- nixos/modules/services/ttys/agetty.nix
+++ nixos/modules/services/ttys/agetty.nix
@@ -70,6 +70,7 @@ with lib;
       { serviceConfig.ExecStart = "@${pkgs.utillinux}/sbin/agetty agetty --noclear --login-program ${pkgs.shadow}/bin/login --keep-baud console 115200,38400,9600 $TERM";
         serviceConfig.Restart = "always";
         restartIfChanged = false;
+	enable = mkDefault config.boot.isContainer;
       };
 
     environment.etc = singleton
