9d8cbba8dc8b0896899db36effd2dccc08b37463
Cray Elliott <MP2E@archlinux.us>
2014-09-01 22:15:54 -0700

    ewemoa : Rewrite androidndk.nix after a few stylistic criticisms

--- pkgs/development/mobile/androidenv/androidndk.nix
+++ pkgs/development/mobile/androidenv/androidndk.nix
@@ -7,45 +7,22 @@
 }:
 
 let
-
-  version = "r10";
-  _name = "android-ndk-${version}";
-  #
-  _binPath = "$out/bin";
-  _pkgPath = "$out/libexec/${_name}";
-  #
-  _sedScript1 =
-    "'s|^PROGDIR=`dirname $0`|PROGDIR=`dirname $(readlink -f $(which $0))`|'";
-  _sedScript2 =
-    "'s|^MYNDKDIR=`dirname $0`|MYNDKDIR=`dirname $(readlink -f $(which $0))`|'";
-  #
-  _runtimeDeps = [
-    coreutils
-    file
-    findutils
-    gawk
-    gnugrep
-    gnused
-    jdk
-    which
-    platformTools
-  ];
-  _runtimePaths = (lib.concatStringsSep "/bin:" _runtimeDeps) + "/platform-tools";
+  the_version = "r10";
 
 in
 
 stdenv.mkDerivation rec {
-  name = "${_name}";
+  name = "android-ndk-${the_version}";
 
   src =
     if stdenv.system == "i686-linux" then fetchurl {
       url = "https://dl.google.com/android/ndk/" +
-            "android-ndk32-${version}-linux-x86.tar.bz2";
+            "android-ndk32-${the_version}-linux-x86.tar.bz2";
       sha256 =
         "7480eea8fe699cfc6a3fcfca9debe8d7e2cd6ef00e31e12b91dead49fcb782b4";
     } else if stdenv.system == "x86_64-linux" then fetchurl {
       url = "https://dl.google.com/android/ndk/" +
-            "android-ndk64-${version}-linux-x86_64.tar.bz2";
+            "android-ndk64-${the_version}-linux-x86_64.tar.bz2";
       sha256 =
         "b99bbc74973d0b2c17df22bc0ba9e61704d6f631deb036885fce05964d9ec921";
     } else throw "platform not ${stdenv.system} supported!";
@@ -54,12 +31,27 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ which makeWrapper ];
 
-  buildCommand = ''
+  buildCommand = let
+    bin_path = "$out/bin";
+    pkg_path = "$out/libexec/${name}";
+    sed_script_1 =
+      "'s|^PROGDIR=`dirname $0`" +
+      "|PROGDIR=`dirname $(readlink -f $(which $0))`|'";
+    sed_script_2 =
+      "'s|^MYNDKDIR=`dirname $0`" +
+      "|MYNDKDIR=`dirname $(readlink -f $(which $0))`|'";
+    runtime_paths = (lib.makeSearchPath "bin" [
+      coreutils file findutils
+      gawk gnugrep gnused
+      jdk
+      which
+    ]) + ":${platformTools}/platform-tools";
+  in ''
     set -x
-    ensureDir $out/libexec
+    mkdir -pv $out/libexec
     cd $out/libexec
     tar xf $src
-    cd ${_pkgPath}
+    cd ${pkg_path}
     # time to massage...
     # XXX: androidsdk.nix may have hints for appropriate patchelf-ing
     find \( \
@@ -70,22 +62,22 @@ stdenv.mkDerivation rec {
     # fix ineffective PROGDIR / MYNDKDIR determination
     for i in ndk-build ndk-gdb ndk-gdb-py
     do
-        sed -i -e ${_sedScript1} $i
+        sed -i -e ${sed_script_1} $i
     done
-    sed -i -e ${_sedScript2} ndk-which
+    sed -i -e ${sed_script_2} ndk-which
     # a bash script
     patchShebangs ndk-which
     # make some executables available in PATH
-    ensureDir ${_binPath}
+    mkdir -pv ${bin_path}
     for i in \
         ndk-build ndk-depends ndk-gdb ndk-gdb-py ndk-gdb.py ndk-stack ndk-which
     do
-        ln -sf ${_pkgPath}/$i ${_binPath}/$i
+        ln -sf ${pkg_path}/$i ${bin_path}/$i
     done
     # wrap
     for i in ndk-build ndk-gdb ndk-gdb-py ndk-which
     do
-        wrapProgram "${_binPath}/$i" --prefix PATH : "${_runtimePaths}"
+        wrapProgram "${bin_path}/$i" --prefix PATH : "${runtime_paths}"
     done
   '';
 }
