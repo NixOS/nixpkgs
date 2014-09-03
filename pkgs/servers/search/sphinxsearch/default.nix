428cb0ea7a53f5e6c0bd5eb4206061e24cb49172
Luca Bruno <luca.bruno@immobiliare.it>
2014-09-03 11:25:57 +0200

    sphinxsearch: fix evaluation error

--- pkgs/servers/search/sphinxsearch/default.nix
+++ pkgs/servers/search/sphinxsearch/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, pkgconfig, mysql,
-  version = "2.1.9",
+  version = "2.1.9";
   mainSrc = fetchurl {
     url = "http://sphinxsearch.com/files/sphinx-${version}-release.tar.gz";
     sha256 = "00vwxf3zr0g1fq9mls1z2rd8nxw74b76pkl1j466lig1qc5am2b2";
