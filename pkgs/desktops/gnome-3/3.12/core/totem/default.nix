1d0c2faf6b42f98d143c5758dfbe6c74b0a01ec5
Luca Bruno <luca.bruno@immobiliare.it>
2014-09-03 10:01:19 +0200

    totem: drop upstream patch after update

--- pkgs/desktops/gnome-3/3.12/core/totem/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/totem/default.nix
@@ -39,8 +39,6 @@ stdenv.mkDerivation rec {
     rm $out/share/icons/hicolor/icon-theme.cache
   '';
 
-  patches = [ ./fix_files_loss.patch ];
-
   meta = with stdenv.lib; {
     homepage = https://wiki.gnome.org/Apps/Videos;
     description = "Movie player for the GNOME desktop based on GStreamer";
