84b1072604b41901e9bc0dcd9a25a47d92fcfb6b
Peter Simons <simons@cryp.to>
2014-09-03 08:33:30 +0200

    haskell-network-uri: update to version 2.6.0.1

--- pkgs/development/libraries/haskell/network-uri/default.nix
+++ pkgs/development/libraries/haskell/network-uri/default.nix
@@ -6,8 +6,8 @@
 
 cabal.mkDerivation (self: {
   pname = "network-uri";
-  version = "2.6.0.0";
-  sha256 = "0dnprscb5nxidg56i7j6q783nwsrn3dabfsij3vjlvjn4f0sg11l";
+  version = "2.6.0.1";
+  sha256 = "09ymamb128jgqghpda4nixncr73all8qc6q53976aricm6a27p37";
   buildDepends = [ parsec ];
   testDepends = [
     HUnit network testFramework testFrameworkHunit
