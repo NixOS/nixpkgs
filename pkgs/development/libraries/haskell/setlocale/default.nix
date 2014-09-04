87bb484b9e34c8c8d0795fc1b2d2136ac2766376
Peter Simons <simons@cryp.to>
2014-09-04 11:02:44 +0200

    haskell-setlocale: jailbreak to fix build with GHC 7.8.3

--- pkgs/development/libraries/haskell/setlocale/default.nix
+++ pkgs/development/libraries/haskell/setlocale/default.nix
@@ -6,6 +6,7 @@ cabal.mkDerivation (self: {
   pname = "setlocale";
   version = "1.0.0";
   sha256 = "1bnxh09jjac8gyjl87w6v86dqc1xr398l28ili8283im141anpzi";
+  jailbreak = true;
   meta = {
     description = "Haskell bindings to setlocale";
     license = self.stdenv.lib.licenses.bsd3;
