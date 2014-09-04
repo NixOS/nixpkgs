3a38a9b127fd36db20e1a9b1f0eab2a5fd5086d9
Andreas Herrmann <andreash87@gmx.ch>
2014-09-04 18:47:57 +0200

    mpi4py: Change description

--- pkgs/development/python-modules/mpi4py/default.nix
+++ pkgs/development/python-modules/mpi4py/default.nix
@@ -47,7 +47,7 @@ buildPythonPackage rec {
 
   meta = {
     description =
-      "Provides Python bindings for the Message Passing Interface standard";
+      "Python bindings for the Message Passing Interface standard";
     homepage = "http://code.google.com/p/mpi4py/";
     license = stdenv.lib.licenses.bsd3;
   };
