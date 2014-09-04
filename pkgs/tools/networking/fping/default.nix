ff3ba4829471e51a72359b4f461fbe727f891d47
Moritz Ulrich <moritz@tarn-vedra.de>
2014-09-04 16:18:11 +0200

    fping: update from 3.8 to 3.10

--- pkgs/tools/networking/fping/default.nix
+++ pkgs/tools/networking/fping/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "fping-3.8";
+  name = "fping-3.10";
 
   src = fetchurl {
     url = "http://www.fping.org/dist/${name}.tar.gz";
-    sha256 = "04iwj4x3wns09wp777mb3kwfi7ypb4m9m73p0s2y699px77hcx67";
+    sha256 = "1n2psfxgww6wg5rz8rly06xkghgp8lshx2lx6rramrigyd1fhiyd";
   };
 
   meta = {
