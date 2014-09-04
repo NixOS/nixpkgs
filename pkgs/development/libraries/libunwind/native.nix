25b395bc8d8b1590723dc42edd97561611f8db68
Joel Taylor <me@joelt.io>
2014-09-03 18:46:47 -0700

    stop copying everything into the store

--- pkgs/development/libraries/libunwind/native.nix
+++ pkgs/development/libraries/libunwind/native.nix
@@ -6,19 +6,9 @@ stdenv.mkDerivation {
   unpackPhase = ":";
   dontBuild = true;
 
-  # TODO: figure out if these are different in Mavericks + older
   installPhase = ''
     mkdir -p $out/lib
     cat ${/usr/lib/system/libunwind.dylib} > $out/lib/libunwind.dylib
-    /usr/bin/install_name_tool \
-      -change /usr/lib/system/libunwind.dylib ${/usr/lib/system/libunwind.dylib} \
-      -change /usr/lib/system/libsystem_malloc.dylib ${/usr/lib/system/libsystem_malloc.dylib} \
-      -change /usr/lib/system/libsystem_pthread.dylib ${/usr/lib/system/libsystem_pthread.dylib} \
-      -change /usr/lib/system/libsystem_platform.dylib ${/usr/lib/system/libsystem_platform.dylib} \
-      -change /usr/lib/system/libsystem_c.dylib ${/usr/lib/system/libsystem_c.dylib} \
-      -change /usr/lib/system/libdyld.dylib ${/usr/lib/system/libdyld.dylib} \
-      -change /usr/lib/system/libkeymgr.dylib ${/usr/lib/system/libkeymgr.dylib} \
-      $out/lib/libunwind.dylib
   '';
 
   meta.platforms = stdenv.lib.platforms.darwin;
