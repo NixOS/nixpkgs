96adde8f6a37cf747996a31a39e2da5d09d3f7ed
Emery Hemingway <emery@vfemail.net>
2014-09-05 10:43:06 -0400

    rsync: update 3.1.0 to 3.1.1
    fix http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2014-2855

--- pkgs/applications/networking/sync/rsync/default.nix
+++ pkgs/applications/networking/sync/rsync/default.nix
@@ -7,37 +7,33 @@ assert enableACLs -> acl != null;
 
 stdenv.mkDerivation rec {
   name = "rsync-${version}";
-  version = "3.1.0";
+  version = "3.1.1";
 
   mainSrc = fetchurl {
+    # signed with key 0048 C8B0 26D4 C96F 0E58  9C2F 6C85 9FB1 4B96 A8C5
     url = "http://rsync.samba.org/ftp/rsync/src/rsync-${version}.tar.gz";
-    sha256 = "0kirw8wglqvwi1v8bwxp373g03xg857h59j5k3mmgff9gzvj7jl1";
+    sha256 = "0896iah6w72q5izpxgkai75bn40dqkqifi2ivcxjzr2zrx7kdr3x";
   };
 
   patchesSrc = fetchurl {
+    # signed with key 0048 C8B0 26D4 C96F 0E58  9C2F 6C85 9FB1 4B96 A8C5
     url = "http://rsync.samba.org/ftp/rsync/rsync-patches-${version}.tar.gz";
-    sha256 = "0sl8aadpjblvbb05vgais40z90yzhr09rwz0cykjdiv452gli75p";
+    sha256 = "0iij996xbyn20yr4w3kv3rw3cx4jwkg2k85x6w5hb5xlgsis8zjl";
   };
 
   srcs = [mainSrc] ++ stdenv.lib.optional enableCopyDevicesPatch patchesSrc;
-  patches = [(fetchurl {
-      url = "https://git.samba.org/?p=rsync.git;a=commitdiff_plain;h=0dedfbce2c1b851684ba658861fe9d620636c56a";
-      sha256 = "0j1pqmwsqc5mh815x28izi4baki2y2r5q8k7ma1sgs4xsgjc4rk8";
-      name = "CVE-2014-2855.patch";
-    })]
-    ++ stdenv.lib.optional enableCopyDevicesPatch "./patches/copy-devices.diff";
+  patches = stdenv.lib.optional enableCopyDevicesPatch "./patches/copy-devices.diff";
 
   buildInputs = stdenv.lib.optional enableACLs acl;
   nativeBuildInputs = [perl];
 
   configureFlags = "--with-nobody-group=nogroup";
 
-  meta = {
-    homepage = http://samba.anu.edu.au/rsync/;
+  meta = with stdenv.lib; {
+    homepage = http://rsync.samba.org/;
     description = "A fast incremental file transfer utility";
-    license = stdenv.lib.licenses.gpl3Plus;
-
-    platforms = stdenv.lib.platforms.unix;
-    maintainers = [ stdenv.lib.maintainers.simons stdenv.lib.maintainers.emery ];
+    license = licenses.gpl3Plus;
+    platforms = platforms.unix;
+    maintainers = with maintainers; [ simons emery ];
   };
 }
