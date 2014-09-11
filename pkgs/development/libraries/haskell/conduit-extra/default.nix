c7a684be23c6a98d393160bff68419ee9a1baa25
Peter Simons <simons@cryp.to>
2014-09-11 18:09:18 +0200

    haskell-conduit-extra: update to version 1.1.3.4

--- pkgs/development/libraries/haskell/conduit-extra/default.nix
+++ pkgs/development/libraries/haskell/conduit-extra/default.nix
@@ -7,8 +7,8 @@
 
 cabal.mkDerivation (self: {
   pname = "conduit-extra";
-  version = "1.1.3.2";
-  sha256 = "15flywjw5ha8cgn4yznwlnx31ls4y82gfmvbniby0d51y4x8dmgq";
+  version = "1.1.3.4";
+  sha256 = "0j27lzdklyq2kv10yfpd16a9sl00iyalmcy8jj9r87r7jpvj949p";
   buildDepends = [
     attoparsec blazeBuilder conduit filepath monadControl network
     primitive resourcet stm streamingCommons text transformers
