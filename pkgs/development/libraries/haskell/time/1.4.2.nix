0215ee15c306ea53b2bcc5806bafce12d7645f7d
Peter Simons <simons@cryp.to>
2014-09-11 18:10:06 +0200

    haskell-time: update to version 1.4.2

--- pkgs/development/libraries/haskell/time/1.4.2.nix
+++ pkgs/development/libraries/haskell/time/1.4.2.nix
@@ -18,5 +18,6 @@ cabal.mkDerivation (self: {
     description = "A time library";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
+    hydraPlatforms = self.stdenv.lib.platforms.none;
   };
 })
