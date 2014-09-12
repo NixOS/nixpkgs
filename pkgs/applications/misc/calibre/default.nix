245f34fef7f2afcc814c397738dbd604a01cd62f
Pascal Wittmann <mail@pascal-wittmann.de>
2014-09-12 20:10:27 +0200

    calibre: update from 2.2.0 to 2.3.0

--- pkgs/applications/misc/calibre/default.nix
+++ pkgs/applications/misc/calibre/default.nix
@@ -5,11 +5,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "calibre-2.2.0";
+  name = "calibre-2.3.0";
 
   src = fetchurl {
     url = "mirror://sourceforge/calibre/${name}.tar.xz";
-    sha256 = "03qljbb3995cq25azwy9qh087dq7m6igia5301v89y98vxxg2lxm";
+    sha256 = "13ny8d569hl7yv8m89216fmg0qdqqy6l9vgzf3cmbbb8i3l1lpxf";
   };
 
   inherit python;
