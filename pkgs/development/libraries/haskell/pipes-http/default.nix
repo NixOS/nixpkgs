b7db46f0e020da485b29eb4aef2b33ee0f10709e
Peter Simons <simons@cryp.to>
2014-09-12 10:56:16 +0200

    haskell-pipes-http: jailbreak to fix build with latest http-client library

--- pkgs/development/libraries/haskell/pipes-http/default.nix
+++ pkgs/development/libraries/haskell/pipes-http/default.nix
@@ -7,6 +7,7 @@ cabal.mkDerivation (self: {
   version = "1.0.1";
   sha256 = "15jmhf6lgqxv9zn08dky8biiv8cp4s3vf0xmp78pbllaqkvm4z9w";
   buildDepends = [ httpClient httpClientTls pipes ];
+  jailbreak = true;
   meta = {
     description = "HTTP client with pipes interface";
     license = self.stdenv.lib.licenses.bsd3;
