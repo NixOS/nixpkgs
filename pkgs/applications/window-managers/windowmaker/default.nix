b49416f2e921d2ad473b390c26bc8bed5d416df4
AndersonTorres <torres.anderson.85@gmail.com>
2014-09-13 15:21:56 -0300

    Windowmaker: adding system service file, and a long description

--- pkgs/applications/window-managers/windowmaker/default.nix
+++ pkgs/applications/window-managers/windowmaker/default.nix
@@ -3,18 +3,27 @@
 stdenv.mkDerivation rec {
   name = "windowmaker-${version}";
   version = "0.95.5";
+  srcName = "WindowMaker-${version}";
 
   src = fetchurl {
-    url = "http://windowmaker.org/pub/source/release/"
-        + "WindowMaker-${version}.tar.gz";
+    url = "http://windowmaker.org/pub/source/release/${srcName}.tar.gz";
     sha256 = "1l3hmx4jzf6vp0zclqx9gsqrlwh4rvqm1g1zr5ha0cp0zmsg89ab";
   };
 
   buildInputs = [ pkgconfig libX11 libXft libXmu ];
 
-  meta = {
-    homepage = "http://windowmaker.org/";
+  meta = with stdenv.lib; {
+    homepage = http://windowmaker.org/;
     description = "NeXTSTEP-like window manager";
-    license = stdenv.lib.licenses.gpl2Plus;
+    longDescription = ''
+      Window Maker is an X11 window manager originally designed to
+      provide integration support for the GNUstep Desktop
+      Environment. In every way possible, it reproduces the elegant look
+      and feel of the NEXTSTEP user interface. It is fast, feature rich,
+      easy to configure, and easy to use. It is also free software, with
+      contributions being made by programmers from around the world.
+    '';
+    license = licenses.gpl2Plus;
+    maintainers = [ maintainers.AndersonTorres ];
   };
 }
