e766f0b0d09006a450c35ca7888c85062f9aabbe
Gergely Risko <errge@nilcons.com>
2014-09-14 22:33:48 +0200

    Fix __DATE__ and __TIME__ purification in gcc-wrapper

--- pkgs/build-support/gcc-wrapper/gcc-wrapper.sh
+++ pkgs/build-support/gcc-wrapper/gcc-wrapper.sh
@@ -87,8 +87,8 @@ extraBefore=()
 # When enforcing purity, pretend gcc can't find the current date and
 # time
 if test "$NIX_ENFORCE_PURITY" = "1"; then
-    extraAfter+=('-D__DATE__=??? ?? ????'
-        '-D__TIME__=??:??:??'
+    extraAfter+=('-D__DATE__="??? ?? ????"'
+        '-D__TIME__="??:??:??"'
         -Wno-builtin-macro-redefined)
 fi
 
