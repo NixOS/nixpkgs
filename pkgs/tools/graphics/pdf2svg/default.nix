83604fa0324a22ea23150c4a2a0bc8c4b2935de7
Ian-Woo Kim <ianwookim@gmail.com>
2014-09-15 12:54:39 +0200

    pdf2svg: PDF converter utility to SVG format

---
+++ pkgs/tools/graphics/pdf2svg/default.nix
@@ -0,0 +1,20 @@
+{ stdenv, fetchurl, pkgconfig, cairo, gtk, poppler }:
+
+stdenv.mkDerivation {
+  name = "pdf2svg-0.2.2";
+
+  src = fetchurl {
+    url = "http://www.cityinthesky.co.uk/wp-content/uploads/2013/10/pdf2svg-0.2.2.tar.gz" ; 
+    sha256 = "1jy6iqwwvd7drcybmdlmnc8m970f82fd7fisa8ha5zh13p49r8n2";
+  };
+
+  buildInputs = [ cairo pkgconfig poppler.poppler_glib gtk ];
+
+  meta = { 
+    description = "PDF converter to SVG format";
+    homepage = http://www.cityinthesky.co.uk/opensource/pdf2svg;
+    license = stdenv.lib.licenses.gpl2;
+    maintainers = [ stdenv.lib.maintainers.ianwookim ];
+    platforms = stdenv.lib.platforms.linux;
+  };
+}
