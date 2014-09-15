36f9b9c284342574ce178571195c313fc1874cca
William A. Kennington III <william@wkennington.com>
2014-09-15 01:26:26 -0700

    nixos/consul: Add module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -213,6 +213,7 @@
   ./services/networking/cjdns.nix
   ./services/networking/cntlm.nix
   ./services/networking/connman.nix
+  ./services/networking/consul.nix
   ./services/networking/ddclient.nix
   ./services/networking/dhcpcd.nix
   ./services/networking/dhcpd.nix
