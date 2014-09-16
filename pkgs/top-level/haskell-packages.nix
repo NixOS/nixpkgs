79f605079ea887dfa71de25b2ccc75728fcd7908
Joachim Fasting <joachifm@fastmail.fm>
2014-09-16 12:42:24 +0200

    haskell-lens-family: new expression

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -1478,6 +1478,8 @@ self : let callPackage = x : y : modifyPrio (newScope self x y); in
 
   lensFamilyCore = callPackage ../development/libraries/haskell/lens-family-core {};
 
+  lensFamily = callPackage ../development/libraries/haskell/lens-family {};
+
   lenses = callPackage ../development/libraries/haskell/lenses {};
 
   leveldbHaskell = callPackage ../development/libraries/haskell/leveldb-haskell {};
