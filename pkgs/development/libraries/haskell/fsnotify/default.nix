dfaae12dc758e9029eaf6a79c2a0a5bbc3b59d74
Abhinav Gupta <mail@abhinavg.net>
2014-09-16 22:54:51 -0700

    fsnotify: 0.0.11 -> 0.1.0.3

--- pkgs/development/libraries/haskell/fsnotify/default.nix
+++ pkgs/development/libraries/haskell/fsnotify/default.nix
@@ -1,18 +1,16 @@
-{ stdenv, cabal, Cabal, Glob, hspec, QuickCheck, random
-, systemFileio, systemFilepath, text, time, uniqueid
-, hinotify, hfsevents
+{ stdenv, cabal, async, hinotify, hfsevents, systemFileio, systemFilepath
+, tasty, tastyHunit, temporaryRc, text, time
 }:
 
 cabal.mkDerivation (self: {
   pname = "fsnotify";
-  version = "0.0.11";
-  sha256 = "03m911pncyzgfdx4aj38azbbmj25fdm3s9l1w27zv0l730fy8ywq";
-  buildDepends = [ systemFileio systemFilepath text time ] ++
+  version = "0.1.0.3";
+  sha256 = "0m6jyg45azk377jklgwyqrx95q174cxd5znpyh9azznkh09wq58z";
+  buildDepends = [ async systemFileio systemFilepath text time ] ++
     (if stdenv.isDarwin then [ hfsevents ] else [ hinotify ]);
   testDepends = [
-    Cabal Glob hspec QuickCheck random systemFileio
-    systemFilepath text time uniqueid
-  ] ++ (if stdenv.isDarwin then [ hfsevents ] else [ hinotify ]);
+    async systemFileio systemFilepath tasty tastyHunit temporaryRc
+  ];
   doCheck = false;
   meta = {
     description = "Cross platform library for file change notification";
