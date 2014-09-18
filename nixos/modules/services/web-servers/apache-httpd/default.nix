0569cc408a5be6941c66a27905d7b9f70f74fe89
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-09-18 18:48:28 +0200

    httpd: Don't emit robots.txt if there are no robots entries
    (cherry picked from commit 837a0c05e5c9c5ae3a4bc29fb1cbc4b26bceb077)

--- nixos/modules/services/web-servers/apache-httpd/default.nix
+++ nixos/modules/services/web-servers/apache-httpd/default.nix
@@ -206,16 +206,11 @@ let
       </Directory>
     '';
 
-    robotsTxt = pkgs.writeText "robots.txt" ''
-      ${# If this is a vhost, the include the entries for the main server as well.
-        if isMainServer then ""
-        else concatMapStrings (svc: svc.robotsEntries) mainSubservices}
-      ${concatMapStrings (svc: svc.robotsEntries) subservices}
-    '';
-
-    robotsConf = ''
-      Alias /robots.txt ${robotsTxt}
-    '';
+    robotsTxt =
+      concatStringsSep "\n" (
+        # If this is a vhost, the include the entries for the main server as well.
+        (if isMainServer then [] else map (svc: svc.robotsEntries) mainSubservices)
+        ++ (map (svc: svc.robotsEntries) subservices));
 
   in ''
     ServerName ${serverInfo.canonicalName}
@@ -243,7 +238,9 @@ let
       CustomLog ${mainCfg.logDir}/access_log-${cfg.hostName} ${cfg.logFormat}
     '' else ""}
 
-    ${robotsConf}
+    ${optionalString (robotsTxt != "") ''
+      Alias /robots.txt ${pkgs.writeText "robots.txt" robotsTxt}
+    ''}
 
     ${if isMainServer || maybeDocumentRoot != null then documentRootConf else ""}
 
