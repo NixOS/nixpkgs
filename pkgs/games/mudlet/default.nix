cf0e9eb134909c55651d829df6ab5a7da9e3d920
wyvie <elijahrum@gmail.com>
2014-09-18 15:06:30 +0400

    Fixed package version, added makeWrapper and dep to luafs module

--- pkgs/games/mudlet/default.nix
+++ pkgs/games/mudlet/default.nix
@@ -1,15 +1,15 @@
-{ fetchurl, pkgs, stdenv, qt5, yajl, libzip, hunspell, lua5_1, boost }:
+{ fetchurl, pkgs, stdenv, makeWrapper, qt5, yajl, libzip, hunspell, lua5_1, boost, lua5_filesystem }:
 
 stdenv.mkDerivation rec {
   name = "mudlet-${version}";
-  version = "3.1";
+  version = "3.0";
 
   src = fetchurl {
     url = "https://github.com/Mudlet/Mudlet/archive/6bc55dde0499cffab48b0021f27dcff1d57b0b66.zip";
     sha256 = "c7b9a383d2cf393da730ce07ac8f06478eaec1fdf730054e837e58c598222d38";
   };
 
-  buildInputs = [ pkgs.unzip qt5 lua5_1 hunspell libzip yajl boost ];
+  buildInputs = [ pkgs.unzip qt5 lua5_1 hunspell libzip yajl boost makeWrapper lua5_filesystem ];
 
   configurePhase = "cd src && qmake";
 
@@ -18,7 +18,10 @@ stdenv.mkDerivation rec {
     mkdir -pv $out/bin
     cp mudlet $out
     cp -r mudlet-lua $out
-    ln -s $out/mudlet $out/bin/mudlet
+
+    # ln -s $out/mudlet $out/bin/mudlet
+    makeWrapper $out/mudlet $out/bin/mudlet \
+      --set LUA_CPATH "${lua5_filesystem}/lib/lua/5.2/?.so"
   '';
 
   patches = [ ./libs.patch ];
