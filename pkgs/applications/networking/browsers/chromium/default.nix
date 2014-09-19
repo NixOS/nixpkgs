62b20fc86ef14d5e5fa2697872a92a5a6e41068a
Dmitry V <savraz@gmail.com>
2014-09-19 09:51:11 +0400

    add symlink to chromium-browser for xdg-open to work as expected

--- pkgs/applications/networking/browsers/chromium/default.nix
+++ pkgs/applications/networking/browsers/chromium/default.nix
@@ -75,6 +75,7 @@ in stdenv.mkDerivation {
       --set CHROMIUM_SANDBOX_BINARY_PATH "${sandboxBinary}" \
       --add-flags "${chromium.plugins.flagsEnabled}"
 
+    ln -s "$out/bin/chromium" "$out/bin/chromium-browser"
     ln -s "${chromium.browser}/share/icons" "$out/share/icons"
     cp -v "${desktopItem}/share/applications/"* "$out/share/applications"
   '';
