df12cc6ad0d28181ff55086ca33ad2020bd7db2a
William A. Kennington III <william@wkennington.com>
2014-09-19 16:28:45 -0700

    kernel: 3.16.2 -> 3.16.3

--- pkgs/os-specific/linux/kernel/linux-3.16.nix
+++ pkgs/os-specific/linux/kernel/linux-3.16.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.16.2";
+  version = "3.16.3";
   extraMeta.branch = "3.16";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "16l5l099qv367d3gknpbycgrakli2mdklvgaifsn3hcrrjs44ybf";
+    sha256 = "1namhqsfr26pjwhrxh8agybdjk14337yyn6h12294dazjd7s74ak";
   };
 
   features.iwlwifi = true;
