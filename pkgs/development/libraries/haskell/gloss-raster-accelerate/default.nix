8b47088786dee853a0218592ea5abccc3fee15fc
Peter Simons <simons@cryp.to>
2014-09-19 12:51:21 +0200

    haskell-gloss-raster-accelerate: update to version 1.8.15.0

--- pkgs/development/libraries/haskell/gloss-raster-accelerate/default.nix
+++ pkgs/development/libraries/haskell/gloss-raster-accelerate/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "gloss-raster-accelerate";
-  version = "1.8.0.0";
-  sha256 = "17545lb99iszhmx8f8fan0dm76ps6bc977zs2ramycnybzbb3kyc";
+  version = "1.8.15.0";
+  sha256 = "1fs3ybrzkykslac1zzh6g73lfdfysn6y2fr1pra9hd0a7x5a8j10";
   buildDepends = [ accelerate accelerateCuda gloss glossAccelerate ];
   meta = {
     description = "Parallel rendering of raster images using Accelerate";
