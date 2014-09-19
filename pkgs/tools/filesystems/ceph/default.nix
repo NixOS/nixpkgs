3ff5532a827279042816c19f2c2895eba3eeca50
William A. Kennington III <william@wkennington.com>
2014-09-19 11:26:57 -0700

    ceph: 0.79 -> 0.85

--- pkgs/tools/filesystems/ceph/default.nix
+++ pkgs/tools/filesystems/ceph/default.nix
@@ -1,37 +1,55 @@
-{stdenv, fetchgit, libatomic_ops, autoconf, automake, boost, btrfsProgs, cryptopp, curl, expat,
- fcgi, fuse, gperftools, keyutils, leveldb, libaio, libedit, libtool,
- libuuid, linuxHeaders, openssl, pkgconfig, python, snappy, which, xfsprogs, xz}:
+{ stdenv, fetchgit, autoconf, automake, makeWrapper, pkgconfig, libtool, which
+, boost, btrfsProgs, cryptopp, curl, expat, fcgi, fuse, gperftools, keyutils
+, leveldb, libaio, libatomic_ops, libedit, libuuid, linuxHeaders, openssl
+, python, snappy, udev, xfsprogs, xz
+}:
 
+let
+  wrapArgs = "--prefix PYTHONPATH : \"$(toPythonPath $out)\""
+    + " --prefix PATH : \"$out/bin\""
+    + " --prefix LD_LIBRARY_PATH : \"$out/lib\"";
+in
 stdenv.mkDerivation rec {
-  baseName="ceph";
-  version="0.79";
-  name="${baseName}-${version}";
+  name="ceph-${version}";
+  version="0.85";
+
+  src = fetchgit {
+    url = "git://github.com/ceph/ceph.git";
+    rev = "refs/tags/v0.85";
+    sha256 = "0g98cgrs3gfsc8azg3k0n61bgna2w906qm69j4qbjkb61l83ld1z";
+  };
+
+  patches = [
+    ./0001-Cleanup-boost-optionals.patch # Remove in 0.86
+    ./0001-Makefile-env-Don-t-force-sbin.patch
+  ];
+
+  nativeBuildInputs = [ autoconf automake makeWrapper pkgconfig libtool which ];
   buildInputs = [
-    fuse linuxHeaders pkgconfig libatomic_ops autoconf automake boost btrfsProgs cryptopp expat
-    fcgi fuse gperftools keyutils leveldb libaio libedit libtool libuuid openssl pkgconfig
-    python snappy which xfsprogs.lib xz
+    boost boost.lib btrfsProgs cryptopp curl expat fcgi fuse gperftools keyutils
+    libatomic_ops leveldb libaio libedit libuuid linuxHeaders openssl python
+    snappy udev xfsprogs.lib xz
   ];
 
   preConfigure = ''
     ./autogen.sh
   '';
 
-  installFlags = "DESTDIR=\${out}";
+  configureFlags = [ "--exec_prefix=$(out)" ];
+
+  postInstall = ''
+    wrapProgram $out/bin/ceph ${wrapArgs}
+    wrapProgram $out/bin/ceph-brag ${wrapArgs}
+    wrapProgram $out/bin/ceph-rest-api ${wrapArgs}
+  '';
 
   enableParallelBuilding = true;
-  src = fetchgit {
-    url = "https://github.com/ceph/ceph";
-    rev = "4c2d73a5095f527c3a2168deb5fa54b3c8991a6e";
-    sha256 = "0850m817wqqmw2qdnwm5jvbdgifzlc7kcd05jv526pdvmq1x92rf";
-  };
 
-  meta = {
-    inherit version;
+  meta = with stdenv.lib; {
+    homepage = http://ceph.com/;
     description = "Distributed storage system";
-    maintainers = [
-      stdenv.lib.maintainers.ak
-    ];
-    platforms = with stdenv.lib.platforms; 
-      linux;
+    license = licenses.lgpl21;
+    maintainers = with maintainers; [ ak wkennington ];
+    platforms = with platforms; linux;
   };
 }
