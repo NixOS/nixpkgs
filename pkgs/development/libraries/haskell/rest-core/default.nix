9325c8b3b495d030ca03448df722eb75ed457df5
Peter Simons <simons@cryp.to>
2014-09-20 14:26:20 +0200

    haskell-rest-core: update to version 0.33

--- pkgs/development/libraries/haskell/rest-core/default.nix
+++ pkgs/development/libraries/haskell/rest-core/default.nix
@@ -8,8 +8,8 @@
 
 cabal.mkDerivation (self: {
   pname = "rest-core";
-  version = "0.32.0.2";
-  sha256 = "0iz3mp3dsjckwbqp6cf0vb5fr9srhszk0rrv080czqrswxh9sbxb";
+  version = "0.33";
+  sha256 = "05m8cvkm0nsi1yixk0dj5j6nvzzzs07i0pmyaj0ww6v5vkmcgmfb";
   buildDepends = [
     aeson aesonUtils either errors fclabels hxt hxtPickleUtils
     jsonSchema mtl multipart random restStringmap restTypes safe split
