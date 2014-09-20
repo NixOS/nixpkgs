6ec3a6976631453198f65057922b62db76fd4ac6
Peter Simons <simons@cryp.to>
2014-09-20 14:26:25 +0200

    haskell-rest-snap: update to version 0.1.17.14

--- pkgs/development/libraries/haskell/rest-snap/default.nix
+++ pkgs/development/libraries/haskell/rest-snap/default.nix
@@ -6,8 +6,8 @@
 
 cabal.mkDerivation (self: {
   pname = "rest-snap";
-  version = "0.1.17.13";
-  sha256 = "13c143dzxhfrshn19ylqfmhnxjirixfif8d1fmzagz1v893narkz";
+  version = "0.1.17.14";
+  sha256 = "0fd6d85gzp9mr7y7bgfk9wscrhrych9q7cssps8m5l03n83d8asp";
   buildDepends = [
     caseInsensitive restCore safe snapCore unorderedContainers
     uriEncode utf8String
