f63444abde0e42c023dcc1615380d31156416f8d
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-09-21 20:21:34 +0100

    sphinxbase: update from 0.7 to 0.8

--- pkgs/development/libraries/sphinxbase/default.nix
+++ pkgs/development/libraries/sphinxbase/default.nix
@@ -6,11 +6,11 @@
 }:
 
 stdenv.mkDerivation (rec {
-  name = "sphinxbase-0.7";
+  name = "sphinxbase-0.8";
 
   src = fetchurl {
     url = "mirror://sourceforge/cmusphinx/${name}.tar.gz";
-    sha256 = "1v3kfzw42ahxmr002i6wqigs832958vgghrv5dd62zazajdbk71q";
+    sha256 = "1a3c91g6rcfb2k8qyfhnd7s68ds6pxwv61xfp0ai1arbhx28jw2m";
   };
 
   buildInputs = [ pkgconfig bison ];
@@ -18,8 +18,11 @@ stdenv.mkDerivation (rec {
   meta = {
     description = "Support Library for Pocketsphinx";
     homepage = http://cmusphinx.sourceforge.net;
-    license = "free-non-copyleft";
+    license = stdenv.lib.licenses.bsd2;
+    platforms = stdenv.lib.platforms.unix;
+    maintainers = with stdenv.lib.maintainers; [ fuuzetsu ];
   };
+
 } // (stdenv.lib.optionalAttrs multipleOutputs {
   outputs = [ "out" "lib" "headers" ];
 
@@ -31,4 +34,3 @@ stdenv.mkDerivation (rec {
     cp -av $out/include $headers
   '';
 }))
-
