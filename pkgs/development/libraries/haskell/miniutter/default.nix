50352beb31b474693f373f18a78158ae771d035f
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-09-21 22:30:34 +0100

    haskell-miniutter: update to 0.4.4.1

--- pkgs/development/libraries/haskell/miniutter/default.nix
+++ pkgs/development/libraries/haskell/miniutter/default.nix
@@ -6,15 +6,14 @@
 
 cabal.mkDerivation (self: {
   pname = "miniutter";
-  version = "0.4.4.0";
-  sha256 = "1pfgbvnxh3c9b489i121p5bg785bp9ivfwizjfwc9w3r6spgppvk";
+  version = "0.4.4.1";
+  sha256 = "1raq5dq6mx56ddyq5hnh9r2yflry6prcjf17p15ds1w5r8cp8yx1";
   buildDepends = [ binary minimorph text ];
   testDepends = [ HUnit testFramework testFrameworkHunit text ];
   meta = {
     homepage = "https://github.com/Mikolaj/miniutter";
     description = "Simple English clause creation from arbitrary words";
     license = self.stdenv.lib.licenses.bsd3;
-    # https://github.com/Mikolaj/miniutter/issues/3
-    platforms = self.stdenv.lib.lists.filter (x: x != "i686-linux") self.ghc.meta.platforms;
+    platforms = self.ghc.meta.platforms;
   };
 })
