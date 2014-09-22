e27c77353eabd91f7340648c881dfcda8388850e
Georges Dubus <georges.dubus@compiletoi.net>
2014-09-22 17:13:14 +0200

    thunderbird: update from 31.1.0 to 31.1.1

--- pkgs/applications/networking/mailreaders/thunderbird/default.nix
+++ pkgs/applications/networking/mailreaders/thunderbird/default.nix
@@ -13,7 +13,7 @@
   enableOfficialBranding ? false
 }:
 
-let version = "31.1.0"; in
+let version = "31.1.1"; in
 let verName = "${version}"; in
 
 stdenv.mkDerivation rec {
@@ -21,7 +21,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "ftp://ftp.mozilla.org/pub/thunderbird/releases/${verName}/source/thunderbird-${verName}.source.tar.bz2";
-    sha1 = "08d231541a80a79180826f98dde732c5796191f4";
+    sha256 = "0xfxavc16qm0r27xpf8k3lxzzbhbp08g554n09ril30cx1zdjhs7";
   };
 
   buildInputs = # from firefox30Pkgs.xulrunner, but without gstreamer and libvpx
