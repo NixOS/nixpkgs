4e5c8b34f840ebbc46bebbbd5647c846803d0333
James Cook <james.cook@utoronto.ca>
2014-09-22 14:54:29 -0700

    firefox: Update to 32.0.2.
    (cherry picked from commit 27f3301650066cc70eff3fa56d17c4dc2e93c289)

--- pkgs/applications/networking/browsers/firefox/default.nix
+++ pkgs/applications/networking/browsers/firefox/default.nix
@@ -15,14 +15,14 @@
 
 assert stdenv.gcc ? libc && stdenv.gcc.libc != null;
 
-let version = "32.0.1"; in
+let version = "32.0.2"; in
 
 stdenv.mkDerivation rec {
   name = "firefox-${version}";
 
   src = fetchurl {
     url = "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/${version}/source/firefox-${version}.source.tar.bz2";
-    sha1 = "346754273e345e20f21b2d22cccb33e807507005";
+    sha1 = "k3kan11592xwnfn1k4wjw2a2hpa55yxz";
   };
 
   buildInputs =
