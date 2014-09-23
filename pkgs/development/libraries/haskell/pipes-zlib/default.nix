132b23134501925a754e1426ee1c571dfda32127
Peter Simons <simons@cryp.to>
2014-09-23 14:18:17 +0200

    haskell-pipes-zlib: update to version 0.4.1

--- pkgs/development/libraries/haskell/pipes-zlib/default.nix
+++ pkgs/development/libraries/haskell/pipes-zlib/default.nix
@@ -4,12 +4,12 @@
 
 cabal.mkDerivation (self: {
   pname = "pipes-zlib";
-  version = "0.4.0.1";
-  sha256 = "1k91q5hci4hk2kzaqfvg1nwbklqyg83wwhm3sdfhdn2famj0mls0";
+  version = "0.4.1";
+  sha256 = "0wjx51d3inhsjzqf16l46mhh0mdsa8fk7x1vvp2apg9s6zfw624k";
   buildDepends = [ pipes transformers zlib zlibBindings ];
   meta = {
     homepage = "https://github.com/k0001/pipes-zlib";
-    description = "Zlib compression and decompression for Pipes streams";
+    description = "Zlib and GZip compression and decompression for Pipes streams";
     license = self.stdenv.lib.licenses.bsd3;
     platforms = self.ghc.meta.platforms;
     maintainers = with self.stdenv.lib.maintainers; [ ocharles ];
