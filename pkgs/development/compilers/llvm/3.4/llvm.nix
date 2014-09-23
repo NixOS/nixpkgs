cb9f69c05272e638cf6ff14eab1216a06b61897d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-09-23 13:13:53 +0200

    llvm: Don't depend on Python on Darwin
    There already is a Python in /usr/bin, so we don't need to build
    another one.

--- pkgs/development/compilers/llvm/3.4/llvm.nix
+++ pkgs/development/compilers/llvm/3.4/llvm.nix
@@ -27,7 +27,10 @@ in stdenv.mkDerivation rec {
     mv compiler-rt-* $sourceRoot/projects/compiler-rt
   '';
 
-  buildInputs = [ perl groff cmake libxml2 python libffi ] ++ stdenv.lib.optional stdenv.isLinux valgrind;
+  buildInputs =
+    [ perl groff cmake libxml2 libffi ]
+    ++ stdenv.lib.optional (!stdenv.isDarwin) python
+    ++ stdenv.lib.optional stdenv.isLinux valgrind;
 
   propagatedBuildInputs = [ ncurses zlib ];
 
