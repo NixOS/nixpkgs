5aa986906600e0b3e47031a770c7b7aa1e9a9570
Thomas Strobel <ts468@cam.ac.uk>
2014-09-23 12:18:52 +0200

    Add python package: snapper-gui
    Graphical frontend for snapper.
    Not tested, yet.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10798,6 +10798,24 @@ let
     };
   };
 
+  snapperGUI = buildPythonPackage rec {
+    name = "Snapper-GUI";
+
+    src = fetchgit {
+      url = "https://github.com/ricardomv/snapper-gui";
+      rev = "11d98586b122180c75a86fccda45c4d7e3137591";
+      sha256 = "7a9f86fc17dbf130526e70c3e925eac30e2c74d6b932efbf7e7cd9fbba6dc4b1";
+    };
+
+    propagatedBuildInputs = with pythonPackages; [ pygobject3 dbus ];
+
+    meta = {
+      homepage = https://github.com/ricardomv/snapper-gui;
+      description = "Graphical frontend for snapper";
+      license = licenses.gpl2;
+      maintainers = [ stdenv.lib.maintainers.tstrobel ];
+    };
+  };
 
 # python2.7 specific packages
 } // optionalAttrs isPy27 (
@@ -10917,5 +10935,4 @@ let
       maintainers = [ stdenv.lib.maintainers.DamienCassou ];
     };
   };
-
 }); in pythonPackages
