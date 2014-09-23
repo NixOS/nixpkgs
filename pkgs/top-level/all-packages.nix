6bd2f75f1c67d2f8cdfd1b953228408c6fc401ba
wyvie <elijahrum@gmail.com>
2014-09-23 06:07:06 +0200

    Switched from import to callPackage

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3755,9 +3755,9 @@ let
   lua5 = lua5_2_compat;
   lua = lua5;
 
-  lua51Packages = recurseIntoAttrs (import ./lua-packages.nix pkgs lua5_1);
+  lua51Packages = recurseIntoAttrs (callPackage ./lua-packages.nix pkgs lua5_1);
 
-  lua52Packages = recurseIntoAttrs (import ./lua-packages.nix pkgs lua5_2);
+  lua52Packages = recurseIntoAttrs (callPackage ./lua-packages.nix pkgs lua5_2);
 
   luaPackages = lua52Packages;
 
