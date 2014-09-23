c7d625a65ed48d99cd22275d40f5e93d4be76997
stewart <stewart@chaatz.com>
2014-09-23 05:52:17 +0000

    changed sha256 for libcouchbase

--- pkgs/development/libraries/libcouchbase/default.nix
+++ pkgs/development/libraries/libcouchbase/default.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation {
   src = fetchgit {
     url = "https://github.com/couchbase/libcouchbase.git";
     rev = "bd3a20f9e18a69dca199134956fd4ad3e1b80ca8";
-    sha256 = "6bc4e9e8cea7d1b5e404457adb80905c39ae9a206d516c10d64597a330f069cb";
+    sha256 = "1jv9y0qa75s5sq86qlbd42dawfawj20dnyj50kjbbld7rvlfki3b";
     leaveDotGit = true;
   };
 
