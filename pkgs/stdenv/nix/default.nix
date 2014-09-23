9c1066336170f93fe5ec4a3b88336d479d50c6f3
John Wiegley <johnw@newartisans.com>
2014-09-23 11:49:16 -0500

    darwin: Use xcrun to determine the DEPLOYMENT_TARGET

--- pkgs/stdenv/nix/default.nix
+++ pkgs/stdenv/nix/default.nix
@@ -14,7 +14,7 @@ import ../generic rec {
       dontFixLibtool=1
       stripAllFlags=" " # the Darwin "strip" command doesn't know "-s"
       xargsFlags=" "
-      export MACOSX_DEPLOYMENT_TARGET=10.6
+      export MACOSX_DEPLOYMENT_TARGET=$(/usr/bin/xcrun --show-sdk-version 2> /dev/null || true)
       export SDKROOT=$(/usr/bin/xcrun --show-sdk-path 2> /dev/null || true)
       export NIX_CFLAGS_COMPILE+=" --sysroot=/var/empty -idirafter $SDKROOT/usr/include -F$SDKROOT/System/Library/Frameworks -Wno-multichar -Wno-deprecated-declarations"
       export NIX_LDFLAGS_AFTER+=" -L$SDKROOT/usr/lib"
