1e49070446ad7953461176a7e930f77dcd9e6ed8
Michel Kuhlmann <michel@kuhlmanns.info>
2014-09-23 16:49:48 +0200

    libLAS: initial expression

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5480,6 +5480,8 @@ let
 
   libgtop = callPackage ../development/libraries/libgtop {};
 
+  libLAS = callPackage ../development/libraries/libLAS { };
+
   liblo = callPackage ../development/libraries/liblo { };
 
   liblrdf = librdf;
