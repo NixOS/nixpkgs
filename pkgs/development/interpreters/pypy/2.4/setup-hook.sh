NixOS/nixpkgs

commit 428fdc879fbf629b2aac54482030ad2152786bca

pypy: 2.3 -> 2.4

pkgs/development/interpreters/pypy/2.4/setup-hook.sh
@@ -1,5 +1,5 @@
 addPythonPath() {
-    addToSearchPathWithCustomDelimiter : PYTHONPATH $1/lib/pypy2.3/site-packages
+    addToSearchPathWithCustomDelimiter : PYTHONPATH $1/lib/pypy2.4/site-packages
 }
 
 toPythonPath() {

pkgs/development/interpreters/pypy/2.4/default.nix
@@ -6,8 +6,8 @@ assert zlibSupport -> zlib != null;
 
 let
 
-  majorVersion = "2.3";
-  version = "${majorVersion}.1";
+  majorVersion = "2.4";
+  version = "${majorVersion}.0";
   pythonVersion = "2.7";
   libPrefix = "pypy${majorVersion}";
 
@@ -18,7 +18,7 @@ let
 
     src = fetchurl {
       url = "https://bitbucket.org/pypy/pypy/get/release-${version}.tar.bz2";
-      sha256 = "0fg4l48c7n59n5j3b1dgcsr927xzylkfny4a6pnk6z0pq2bhvl9z";
+      sha256 = "1lhk86clnkj305dxa6xr9wjib6ckf6xxl6qj0bq20vqh80nfq3by";
     };
 
     buildInputs = [ bzip2 openssl pkgconfig pythonFull libffi ncurses expat sqlite tk tcl x11 libX11 makeWrapper ]

pkgs/top-level/all-packages.nix
@@ -3878,7 +3878,7 @@ let
   python33 = callPackage ../development/interpreters/python/3.3 { };
   python34 = hiPrio (callPackage ../development/interpreters/python/3.4 { });
 
-  pypy = callPackage ../development/interpreters/pypy/2.3 { };
+  pypy = callPackage ../development/interpreters/pypy/2.4 { };
 
   python26Full = callPackage ../development/interpreters/python/wrapper.nix {
     extraLibs = [];

commit 25e726b28397a09bae2574cf13a38b269250a0c8

pypy: fix copy-paste error in setup-hook
Introduced in 428fdc879fbf629b2aac54482030ad2152786bca
("pypy: 2.3 -> 2.4").

pkgs/development/interpreters/pypy/2.4/setup-hook.sh
@@ -6,7 +6,7 @@ toPythonPath() {
     local paths="$1"
     local result=
     for i in $paths; do
-        p="$i/lib/pypy2.3/site-packages"
+        p="$i/lib/pypy2.4/site-packages"
         result="${result}${result:+:}$p"
     done
     echo $result