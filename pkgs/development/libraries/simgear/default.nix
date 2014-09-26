58168ed93b29c36f575c2adad19d113535de4848
Pascal Wittmann <mail@pascal-wittmann.de>
2014-09-26 12:35:49 +0200

    simgear: fix sha256sum and clean up

--- pkgs/development/libraries/simgear/default.nix
+++ pkgs/development/libraries/simgear/default.nix
@@ -1,51 +1,27 @@
-x@{builderDefsPackage
-  , plib, freeglut, xproto, libX11, libXext, xextproto, libXi , inputproto
-  , libICE, libSM, libXt, libXmu, mesa, boost, zlib, libjpeg , freealut
-  , openscenegraph, openal, expat, cmake, apr
-  , ...}:
-builderDefsPackage
-(a :
-let
-  helperArgNames = ["stdenv" "fetchurl" "builderDefsPackage"] ++
-    [];
+{ stdenv, fetchurl, plib, freeglut, xproto, libX11, libXext, xextproto, libXi
+, inputproto, libICE, libSM, libXt, libXmu, mesa, boost, zlib, libjpeg, freealut
+, openscenegraph, openal, expat, cmake, apr
+}:
 
-  buildInputs = map (n: builtins.getAttr n x)
-    (builtins.attrNames (builtins.removeAttrs x helperArgNames));
-  sourceInfo = rec {
-    baseName="simgear";
-    version="3.0.0";
-    name="${baseName}-${version}";
-    extension="tar.bz2";
-    url="http://mirrors.ibiblio.org/pub/mirrors/simgear/ftp/Source/${name}.${extension}";
-    hash="05l0wvi0s4a98ihmjbpcc66rj6qy3hrsqkjs388bddf2ws3qyi09";
-  };
-in
-rec {
-  src = a.fetchurl {
-    url = sourceInfo.url;
-    sha256 = sourceInfo.hash;
+stdenv.mkDerivation rec {
+  name = "simgear-${version}";
+  version = "3.0.0";
+
+  src = fetchurl {
+    url = "http://mirrors.ibiblio.org/pub/mirrors/simgear/ftp/Source/${name}.tar.bz2";
+    sha256 = "1ssrcmvk3a4dv0k4vjzndmirwmdgzk689d7wpmjm423nydmwajh0";
   };
 
-  inherit (sourceInfo) name version;
-  inherit buildInputs;
+  buildInputs = [ plib freeglut xproto libX11 libXext xextproto libXi inputproto
+                  libICE libSM libXt libXmu mesa boost zlib libjpeg freealut
+                  openscenegraph openal expat cmake apr ];
 
-  /* doConfigure should be removed if not needed */
-  phaseNames = [ "doCmake" "doMakeInstall" ];
-
-  meta = {
+  meta = with stdenv.lib; {
     description = "Simulation construction toolkit";
-    maintainers = with a.lib.maintainers;
-    [
-      raskin
-    ];
-    platforms = with a.lib.platforms;
-      linux;
-    license = a.lib.licenses.lgpl2;
+    homepage = https://gitorious.org/fg/simgear;
+    maintainers = with maintainers; [ raskin ];
+    platforms = platforms.linux;
+    license = licenses.lgpl2;
   };
-  passthru = {
-    updateInfo = {
-      downloadPage = "ftp://ftp.goflyflightgear.com/simgear/Source/";
-    };
-  };
-}) x
+}
 
