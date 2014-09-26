448f3229f81d760b09b9eb5c1f12e00228b91111
Siarhei Zirukin <ftrvxmtrx@gmail.com>
2014-09-26 10:16:31 +0200

    package update: go-1.3.1 -> go-1.3.2
    Closes #4279

--- pkgs/development/compilers/go/1.3.nix
+++ pkgs/development/compilers/go/1.3.nix
@@ -8,8 +8,8 @@ let
   loaderArm = "${glibc}/lib/ld-linux.so.3";
   srcs = {
     golang = fetchurl {
-      url = https://storage.googleapis.com/golang/go1.3.1.src.tar.gz;
-      sha256 = "fdfa148cc12f1e4ea45a5565261bf43d8a2e7d1fad4a16aed592d606223b93a8";
+      url = https://storage.googleapis.com/golang/go1.3.2.src.tar.gz;
+      sha256 = "1yq6xygc5lhn3b9d4sbddlf8dzi3wihyzjwyc8rhiwrb3hj8hx1y";
     };
     tools = fetchhg {
       url = https://code.google.com/p/go.tools/;
@@ -20,7 +20,7 @@ let
 in
 
 stdenv.mkDerivation {
-  name = "go-1.3.1";
+  name = "go-1.3.2";
 
   src = srcs.golang;
 
