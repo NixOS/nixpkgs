4ed56b013fd3e85937c32c6ab305c512ec4bde10
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-09-27 03:23:22 +0100

    agda-TotalParserCombinators: new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6801,6 +6801,8 @@ let
 
   categories = callPackage ../development/libraries/agda/categories {};
 
+  TotalParserCombinators = callPackage ../development/libraries/agda/TotalParserCombinators {};
+
   ### DEVELOPMENT / LIBRARIES / JAVA
 
   atermjava = callPackage ../development/libraries/java/aterm {
