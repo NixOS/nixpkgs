85fba5fdbda97c12ca0f992554a55d64cfedf365
Vladimír Čunát <vcunat@gmail.com>
2014-09-27 16:54:04 +0200

    kde-4.14: fix kactivities download
    It was not released with 4.14.
    http://osdir.com/ml/kde-commits/2014-07/msg02039.html

--- pkgs/desktops/kde-4.14/kactivities.nix
+++ pkgs/desktops/kde-4.14/kactivities.nix
@@ -1,6 +1,12 @@
-{ kde, kdelibs, nepomuk_core }:
+{ fetchurl, kde, kdelibs, nepomuk_core }:
 
 kde {
+
+  src = fetchurl {
+    url = "mirror://kde/stable/4.13.3/src/kactivities-4.13.3.tar.xz";
+    sha256 = "12l9brpq8mr9hqqmnlz9xfsfr8ry6283b32nfqfx0p3f7w19vjy7";
+  };
+
   propagatedBuildInputs = [ kdelibs nepomuk_core ];
 
   meta = {
