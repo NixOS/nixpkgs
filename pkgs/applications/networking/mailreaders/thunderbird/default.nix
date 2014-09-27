a5e8f410bfb81fe0755cd46fca67617ba51d3ffb
Vladimír Čunát <vcunat@gmail.com>
2014-09-27 18:24:45 +0200

    thunderbird: maintenance update
    The corresponding security problem should've been fixed by the latest nss update.

--- pkgs/applications/networking/mailreaders/thunderbird/default.nix
+++ pkgs/applications/networking/mailreaders/thunderbird/default.nix
@@ -13,7 +13,7 @@
   enableOfficialBranding ? false
 }:
 
-let version = "31.1.1"; in
+let version = "31.1.2"; in
 let verName = "${version}"; in
 
 stdenv.mkDerivation rec {
@@ -21,7 +21,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "ftp://ftp.mozilla.org/pub/thunderbird/releases/${verName}/source/thunderbird-${verName}.source.tar.bz2";
-    sha256 = "0xfxavc16qm0r27xpf8k3lxzzbhbp08g554n09ril30cx1zdjhs7";
+    sha1 = "a3983e7d29bd70e8117ca26f5b9873c68675caa9";
   };
 
   buildInputs = # from firefox30Pkgs.xulrunner, but without gstreamer and libvpx
