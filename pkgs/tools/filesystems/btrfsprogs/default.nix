03ab96520971512a18e894fc3ac44ded1000d321
William A. Kennington III <william@wkennington.com>
2014-09-30 11:35:58 -0700

    btrfsprogs: 3.16 -> 3.16.1

--- pkgs/tools/filesystems/btrfsprogs/default.nix
+++ pkgs/tools/filesystems/btrfsprogs/default.nix
@@ -1,22 +1,20 @@
 { stdenv, fetchurl, attr, acl, zlib, libuuid, e2fsprogs, lzo
 , asciidoc, xmlto, docbook_xml_dtd_45, docbook_xsl, libxslt }:
 
-let version = "3.16"; in
+let version = "3.16.1"; in
 
 stdenv.mkDerivation rec {
   name = "btrfs-progs-${version}";
 
   src = fetchurl {
-    url = "mirror://kernel/linux/kernel/people/mason/btrfs-progs/btrfs-progs-v${version}.tar.xz";
-    sha256 = "0phbrgipl04q8cdj9nnshik7b6p2bg51jxb3l1gvfc04dkgm2xls";
+    url = "mirror://kernel/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v${version}.tar.xz";
+    sha256 = "103ff31fn6x1r6y79hdzv5lqls68wqxmikwsl6q8mxllb5rqrwlk";
   };
 
   buildInputs = [
     attr acl zlib libuuid e2fsprogs lzo
     asciidoc xmlto docbook_xml_dtd_45 docbook_xsl libxslt
   ];
-
-  patches = [ ./multiple_mounts.patch ];
 
   # for btrfs to get the rpath to libgcc_s, needed for pthread_cancel to work
   NIX_CFLAGS_LINK = "-lgcc_s";
