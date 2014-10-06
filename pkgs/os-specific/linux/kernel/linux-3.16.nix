5b80f24b9d287b3c4eff5d336665bf5da8b9b75e
William A. Kennington III <william@wkennington.com>
2014-10-05 21:34:31 -0700

    kernel: 3.16.3 -> 3.16.4

--- pkgs/os-specific/linux/kernel/linux-3.16.nix
+++ pkgs/os-specific/linux/kernel/linux-3.16.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.16.3";
+  version = "3.16.4";
   extraMeta.branch = "3.16";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1namhqsfr26pjwhrxh8agybdjk14337yyn6h12294dazjd7s74ak";
+    sha256 = "0x8jh9j7kdfbxcf3f69p28j5dwjjdxf92sjnlhk3mp016yv02i99";
   };
 
   features.iwlwifi = true;
