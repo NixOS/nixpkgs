3cb125a92599c09a4ea95d722fd37c6db28e9725
Aristid Breitkreuz <aristidb@gmail.com>
2014-10-07 21:12:18 +0200

    gnupg1orig: update from 1.4.17 to 1.4.18

--- pkgs/tools/security/gnupg1/default.nix
+++ pkgs/tools/security/gnupg1/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, readline, bzip2 }:
 
 stdenv.mkDerivation rec {
-  name = "gnupg-1.4.17";
+  name = "gnupg-1.4.18";
 
   src = fetchurl {
     url = "mirror://gnupg/gnupg/${name}.tar.bz2";
-    sha256 = "0nvv1bd8v13gh2m1429azws7ks0ix9y1yv87ak9k9i1dsqcrvpg6";
+    sha256 = "1233bppjvdpbbl425ii6l7xvgy0879ghhnmwrph5b6c4g3dgvddp";
   };
 
   buildInputs = [ readline bzip2 ];
