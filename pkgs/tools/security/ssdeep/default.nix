9b391aadcee3f0825b3eeeae05faa06a25153167
Austin Seipp <aseipp@pobox.com>
2014-10-09 17:06:17 -0500

    ssdeep: 2.10 -> 2.11
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/tools/security/ssdeep/default.nix
+++ pkgs/tools/security/ssdeep/default.nix
@@ -2,16 +2,12 @@
 
 stdenv.mkDerivation rec {
   name    = "ssdeep-${version}";
-  version = "2.10";
+  version = "2.11";
 
   src = fetchurl {
     url    = "mirror://sourceforge/ssdeep/${name}.tar.gz";
-    sha256 = "1p7dgchq8hgadnxz5qh95ay17k5j74l4qyd15wspc54lb603p2av";
+    sha256 = "1izzkvrng4cc2p8gxp3w32k1v60l2yaq2y2hkifgq9s1yh30xk42";
   };
-
-  postFixup = stdenv.lib.optionalString stdenv.isLinux ''
-    patchelf --set-rpath "$(patchelf --print-rpath $out/bin/ssdeep):$out/lib" $out/bin/ssdeep
-  '';
 
   meta = {
     description = "A program for calculating fuzzy hashes";
