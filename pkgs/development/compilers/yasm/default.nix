3c45c8d53e699ddc784d42d626b708a570efb7c6
Vincent Laporte <Vincent.Laporte@gmail.com>
2014-10-10 21:33:26 +0100

    yasm: update from 1.2.0 to 1.3.0

--- pkgs/development/compilers/yasm/default.nix
+++ pkgs/development/compilers/yasm/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation rec {
-  name = "yasm-1.2.0";
+  name = "yasm-1.3.0";
 
   src = fetchurl {
     url = "http://www.tortall.net/projects/yasm/releases/${name}.tar.gz";
-    sha256 = "0cfg7ji3ia2in628w42wrfvw2ixmmm4rghwmv2k202mraysgm3vn";
+    sha256 = "0gv0slmm0qpq91za3v2v9glff3il594x5xsrbgab7xcmnh0ndkix";
   };
 
   meta = {
