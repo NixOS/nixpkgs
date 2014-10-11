7927e7cc539c9ce0cdcef6f39c74533db8dbe123
Oliver Charles <ollie@ocharles.org.uk>
2014-10-11 12:48:57 +0100

    haskellPackages.linuxInotify: New expression

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -1527,6 +1527,8 @@ self : let callPackage = x : y : modifyPrio (newScope self x y); in
 
   linear = callPackage ../development/libraries/haskell/linear {};
 
+  linuxInotify = callPackage ../development/libraries/haskell/linux-inotify {};
+
   List = callPackage ../development/libraries/haskell/List {};
 
   lists = callPackage ../development/libraries/haskell/lists {};
