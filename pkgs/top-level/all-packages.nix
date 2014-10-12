a2a7abc67cff23d6a76fd960e807c08f5c48ac8b
Domen Kožar <domen@dev.si>
2014-10-12 14:59:31 +0200

    pythonFull -> python with all modules, pythonFullWithPkgs -> buildEnv

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1875,7 +1875,7 @@ let
   openobex = callPackage ../tools/bluetooth/openobex { };
 
   openopc = callPackage ../tools/misc/openopc {
-    pythonFull = python27Full.override {
+    pythonFull = python27FullWithPkgs.override {
       extraLibs = [ python27Packages.pyro3 ];
     };
   };
@@ -3931,9 +3931,6 @@ let
   python2Packages = python27Packages;
   python3Packages = python34Packages;
 
-  pythonFull = python2Full;
-  python2Full = python27Full;
-
   python26 = callPackage ../development/interpreters/python/2.6 { db = db47; };
   python27 = callPackage ../development/interpreters/python/2.7 { };
   python32 = callPackage ../development/interpreters/python/3.2 { };
@@ -3942,18 +3939,24 @@ let
 
   pypy = callPackage ../development/interpreters/pypy/2.4 { };
 
-  python26Full = callPackage ../development/interpreters/python/wrapper.nix {
-    extraLibs = [];
-    postBuild = "";
-    python = python26;
+  pythonFull = python2Full;
+  python2Full = python27Full;
+  python26Full = python26.override {
+    includeModules = true;
+  };
+  python27Full = python27.override {
+    includeModules = true;
+  };
+  python26FullWithPkgs = callPackage ../development/interpreters/python/wrapper.nix {
+    python = python26Full;
     inherit (python26Packages) recursivePthLoader;
   };
-  python27Full = callPackage ../development/interpreters/python/wrapper.nix {
-    extraLibs = [];
-    postBuild = "";
-    python = python27;
+  python27FullWithPkgs = callPackage ../development/interpreters/python/wrapper.nix {
+    python = python27Full;
     inherit (python27Packages) recursivePthLoader;
   };
+  pythonFullWithPkgs = python2FullWithPkgs;
+  python2FullWithPkgs = python27FullWithPkgs;
 
   python2nix = callPackage ../tools/package-management/python2nix { };
 
