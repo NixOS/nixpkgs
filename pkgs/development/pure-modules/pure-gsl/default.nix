8fabf87fe132a4b93b71a858495ac8f21b481462
Alastair Pharo <asppsa@gmail.com>
2014-10-12 22:57:56 +1100

    Minor improvements to pure-gsl package

--- pkgs/development/pure-modules/pure-gsl/default.nix
+++ pkgs/development/pure-modules/pure-gsl/default.nix
@@ -1,10 +1,10 @@
-{ stdenv, fetchurl, pure, pkgconfig, gmp, gsl, mpfr }:
+{ stdenv, fetchurl, pure, pkgconfig, gsl }:
 
 stdenv.mkDerivation {
   name = "pure-gsl-0.12";
   src = fetchurl {
     url = https://bitbucket.org/purelang/pure-lang/downloads/pure-gsl-0.12.tar.gz;
-    md5 = "8e90a851ff4abd9cf4bf61a0acf5ddae";
+    sha256 = "06bdd873d5417d90ca35093056a060b77365123ed24c3ac583cd3922d4c78a75";
   };
 
   buildInputs = [ pkgconfig ];
