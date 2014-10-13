45e817b16fdf27e409d2bb44f1c036052dd53a0b
Peter Simons <simons@cryp.to>
2014-10-13 12:48:30 +0200

    haskell-monad-supply: update to version 0.5

--- pkgs/development/libraries/haskell/monad-supply/default.nix
+++ pkgs/development/libraries/haskell/monad-supply/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "monad-supply";
-  version = "0.4";
-  sha256 = "0qvv9j55hw1lrfqnz1aric4mvg62c2vqaf5wirn69gvh74slqyj8";
+  version = "0.5";
+  sha256 = "1f4l7sh0p7cxlg3wr5agpflycl4g9zh8chc4bilzglwx3xffbqqp";
   buildDepends = [ mtl ];
   meta = {
     description = "Stateful supply monad";
