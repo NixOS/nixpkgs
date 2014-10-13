702084a89e6594e0563ad167df826bc94aff2aec
Peter Simons <simons@cryp.to>
2014-10-13 12:46:23 +0200

    haskell-pipes-zlib: update to version 0.4.3

--- pkgs/development/libraries/haskell/pipes-zlib/default.nix
+++ pkgs/development/libraries/haskell/pipes-zlib/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "pipes-zlib";
-  version = "0.4.1";
-  sha256 = "0wjx51d3inhsjzqf16l46mhh0mdsa8fk7x1vvp2apg9s6zfw624k";
+  version = "0.4.3";
+  sha256 = "04iiw0r1mnxl4myyp87wqhff6jm0g2246gwismi7jnwy7xmllsmc";
   buildDepends = [ pipes transformers zlib zlibBindings ];
   meta = {
     homepage = "https://github.com/k0001/pipes-zlib";
