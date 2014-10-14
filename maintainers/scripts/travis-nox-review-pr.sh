e3563e6c23f4f99ff492bb106a46871c8ee7cb61
wmertens <Wout.Mertens@gmail.com>
2014-10-14 14:54:14 +0200

    travis build: More silence

--- maintainers/scripts/travis-nox-review-pr.sh
+++ maintainers/scripts/travis-nox-review-pr.sh
@@ -4,31 +4,32 @@ set -e
 export NIX_CURL_FLAGS=-sS
 
 # Install Nix
-bash <(curl https://nixos.org/nix/install)
+echo "=== Installing Nix..."
+bash <(curl -sS https://nixos.org/nix/install) >/dev/null 2>&1
 source $HOME/.nix-profile/etc/profile.d/nix.sh
 
 # Make sure we can use hydra's binary cache
 sudo mkdir /etc/nix
-sudo tee /etc/nix/nix.conf <<EOF
+sudo tee /etc/nix/nix.conf <<EOF >/dev/null
 binary-caches = http://cache.nixos.org http://hydra.nixos.org
 trusted-binary-caches = http://hydra.nixos.org
 build-max-jobs = 4
 EOF
 
-echo "First of all, checking evaluation, including meta"
-nix-env -f. -qa --json > /dev/null
+echo "=== Checking evaluation, including meta"
+nix-env -f. -qa --json >/dev/null
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    echo "Not a pull request, checking evaluation"
+    echo "===> Not a pull request, checking evaluation"
     nix-build pkgs/top-level/release.nix -A tarball
     exit 0
 fi
 
-echo "Installing nox"
-git clone https://github.com/madjar/nox
+echo "=== Installing nox"
+git clone https://github.com/madjar/nox >/dev/null
 pip --quiet install -e nox
 
-echo "Reviewing PR"
+echo "=== Reviewing PR"
 # The current HEAD is the PR merged into origin/master, so we compare
 # against origin/master
 nox-review wip --against origin/master
