9f408486fc86096a099508fda73376922b5f7fca
Peter Simons <simons@cryp.to>
2014-10-14 23:11:14 +0200

    haskell-HaTeX: update to version 3.14.0.0

--- pkgs/development/libraries/haskell/HaTeX/default.nix
+++ pkgs/development/libraries/haskell/HaTeX/default.nix
@@ -6,12 +6,12 @@
 
 cabal.mkDerivation (self: {
   pname = "HaTeX";
-  version = "3.13.1.0";
-  sha256 = "1ciifdb7yd79lvwdcxj8dq98dfmm9pg25rx9rc2wjn2jnp8j85r7";
+  version = "3.14.0.0";
+  sha256 = "0vbwhj031ny2vkp5hjxihlmpxaqy1far2nmxfzl1bv6rx0sqfjbg";
   buildDepends = [
     matrix parsec QuickCheck text transformers wlPprintExtras
   ];
-  testDepends = [ QuickCheck tasty tastyQuickcheck ];
+  testDepends = [ QuickCheck tasty tastyQuickcheck text ];
   meta = {
     homepage = "http://daniel-diaz.github.io/projects/hatex";
     description = "The Haskell LaTeX library";
