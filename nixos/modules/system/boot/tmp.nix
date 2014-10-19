6b61c8b1932e6efb78ad2cf72ea1e9ed4c5f401e
Emery Hemingway <emery@vfemail.net>
2014-10-19 11:18:46 -0400

    boot.tmpSize option to adjust tmpfs size at /tmp

--- nixos/modules/system/boot/tmp.nix
+++ nixos/modules/system/boot/tmp.nix
@@ -24,15 +24,46 @@ with lib;
       '';
     };
 
+    boot.tmpSize = mkOption {
+      default = "50%";
+      example = "512m";
+      type = types.str;
+      description = ''
+        Size limit for the /dev/shm tmpfs. Look at <command>mount(8)</command>, tmpfs size option,
+        for the accepted syntax.
+      '';
+    };
+
   };
 
   ###### implementation
 
   config = {
 
-    systemd.additionalUpstreamSystemUnits = optional config.boot.tmpOnTmpfs "tmp.mount";
+    systemd.mounts = mkIf config.boot.tmpOnTmpfs [
+      { description = "Temporary Directory";
+        unitConfig =
+          { Documentation = "http://www.freedesktop.org/wiki/Software/systemd/APIFileSystems";
+            DefaultDependencies = "no";
+          };
+        conflicts = [ "umount.target" ];
+        before = [ "local-fs.target" "umount.target" ];
+        wantedBy = [ "local-fs.target" ];
+
+        what = "tmpfs";
+        where = "/tmp";
+        type = "tmpfs";
+        options = "mode=1777,strictatime,size=${config.boot.tmpSize}";
+      }
+    ];
 
     systemd.tmpfiles.rules = optional config.boot.cleanTmpDir "D! /tmp 1777 root root";
+
+    assertions = [
+      { assertion = !(config.boot.tmpOnTmpfs && config.boot.cleanTmpDir);
+        message = "Both boot.tmpOnTmpfs and boot.cleanTmpDir enabled.";
+      }
+    ];
 
   };
 
