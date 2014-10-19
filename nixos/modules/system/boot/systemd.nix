f020a261646da736291fc4b398d48739bd34dbd9
William A. Kennington III <william@wkennington.com>
2014-10-19 16:34:53 -0700

    nixos/systemd: Also escape \ characters

--- nixos/modules/system/boot/systemd.nix
+++ nixos/modules/system/boot/systemd.nix
@@ -13,7 +13,7 @@ let
 
   makeUnit = name: unit:
     let
-      pathSafeName = lib.replaceChars ["@"] ["-"] name;
+      pathSafeName = lib.replaceChars ["@" "\\"] ["-" "-"] name;
     in
     if unit.enable then
       pkgs.runCommand "unit-${pathSafeName}" { preferLocalBuild = true; inherit (unit) text; }
