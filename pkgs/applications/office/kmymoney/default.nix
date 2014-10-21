f4f7b27cb20bb234324024ba6f05875c12c69539
Tobias Blaschke <code@tobiasblaschke.de>
2014-10-22 00:23:35 +0200

    KMyMoney: Update version to 4.7.0 - TODO: change mirrors!

--- pkgs/applications/office/kmymoney/default.nix
+++ pkgs/applications/office/kmymoney/default.nix
@@ -3,11 +3,14 @@
 , doxygen, oxygen_icons, kactivities }:
 
 stdenv.mkDerivation rec {
-  name = "kmymoney-4.6.4";
+  name = "kmymoney-4.7.0";
+  #name = "kmymoney-4.6.4";
 
   src = fetchurl {
-    url = "mirror://sourceforge/kmymoney2/${name}.tar.xz";
-    sha256 = "04n0lgi2yrx67bgjzbdbcm10pxs7l53srmp240znzw59njnjyll9";
+#    url = "mirror://sourceforge/kmymoney2/${name}.tar.xz";
+    url = http://ftp5.gwdg.de/pub/linux/kde/stable/kmymoney/4.7.0/src/kmymoney-4.7.0.tar.xz;
+    sha256 = "e4b3ce10e2fe7d84314b7e1c77a995f41488d161b716f7d67ca8de715e833e09"; # v. 4.7.0
+#    sha256 = "04n0lgi2yrx67bgjzbdbcm10pxs7l53srmp240znzw59njnjyll9"; # v 4.6.4
   };
 
   buildInputs = [ kdepimlibs perl boost gpgme gmpxx libalkimia libofx libical
