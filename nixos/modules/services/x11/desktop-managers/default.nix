50be0d544766eaa0e6fa4faa562073996ac352d7
Georges Dubus <georges.dubus@numergy.com>
2014-10-21 14:02:13 +0200

    Removed the kde4_next nixos module
    Merged its differences into kde4. This commit fixes #4519.

--- nixos/modules/services/x11/desktop-managers/default.nix
+++ nixos/modules/services/x11/desktop-managers/default.nix
@@ -18,7 +18,7 @@ in
   # determines the default: later modules (if enabled) are preferred.
   # E.g., if KDE is enabled, it supersedes xterm.
   imports = [
-    ./none.nix ./xterm.nix ./xfce.nix ./kde4.nix ./kde4_next.nix
+    ./none.nix ./xterm.nix ./xfce.nix ./kde4.nix
     ./e17.nix ./e18.nix ./e19.nix ./gnome3.nix ./xbmc.nix
   ];
 
