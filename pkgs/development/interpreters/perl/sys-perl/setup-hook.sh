7e315fa4cb5bcac82532a15863df129df148cbbb
Florian Friesdorf <flo@chaoflow.net>
2014-10-23 16:14:48 +0200

    cygwin: build perl as usual, no sysPerl

--- pkgs/development/interpreters/perl/sys-perl/setup-hook.sh
+++
@@ -1,5 +0,0 @@
-addPerlLibPath () {
-    addToSearchPath PERL5LIB $1/@libPrefix@
-}
-
-envHooks+=(addPerlLibPath)
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4738,9 +4738,7 @@ let
     fetchurl = fetchurlBoot;
   };
 
-  # CYGWINTODO
-
-  perl = if system != "i686-cygwin" then perl520 else sysPerl;
+  perl = perl520;
 
   php = php56;
 
@@ -4895,8 +4893,6 @@ let
   };
 
   supercollider_scel = supercollider.override { useSCEL = true; };
-
-  sysPerl = callPackage ../development/interpreters/perl/sys-perl { };
 
   tcl = tcl-8_6;
   tcl-8_5 = callPackage ../development/interpreters/tcl/8.5.nix { };
