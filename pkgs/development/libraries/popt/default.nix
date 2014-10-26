fde7faf7e126b8f617e40aade443468445837fce
U-user-PC\user <flo@chaoflow.net>
2014-10-26 15:27:39 +0100

    cygwin: popt patches

--- pkgs/development/libraries/popt/default.nix
+++ pkgs/development/libraries/popt/default.nix
@@ -2,11 +2,16 @@
 
 stdenv.mkDerivation rec {
   name = "popt-1.16";
-  
+
   src = fetchurl {
     url = "http://rpm5.org/files/popt/${name}.tar.gz";
     sha256 = "1j2c61nn2n351nhj4d25mnf3vpiddcykq005w2h6kw79dwlysa77";
   };
+
+  patches = stdenv.lib.optionals stdenv.isCygwin [
+    ./1.16-cygwin.patch
+    ./1.16-vpath.patch
+  ];
 
   meta = {
     description = "command line option parsing library";
