3a0a3893129d61badd11bda8c093e1553e46c99a
Moritz Ulrich <moritz@tarn-vedra.de>
2014-10-27 01:13:23 +0100

    Update egg2nix to 0.3.

--- pkgs/development/tools/egg2nix/default.nix
+++ pkgs/development/tools/egg2nix/default.nix
@@ -3,19 +3,22 @@
 # Note: This mostly reimplements the default.nix already contained in
 # the tarball. Is there a nicer way than duplicating code?
 
+let
+  version = "0.3";
+in
 eggDerivation {
   src = fetchurl {
-    url = "https://github.com/the-kenny/egg2nix/archive/0.2.tar.gz";
-    sha256 = "051nsy30diapcl687pyfrvcyqh5h55fijqjhykra2nah30bmf0k0";
+    url = "https://github.com/the-kenny/egg2nix/archive/${version}.tar.gz";
+    sha256 = "1sv6v5a3a17lsyx1i9ajlvix0v8yzl0nnvv9da9c1k349w0fdijv";
   };
 
-  name = "egg2nix-0.2";
+  name = "egg2nix-${version}";
   buildInputs = with chickenEggs; [
     matchable http-client
   ];
 
   meta = {
-    description = "Generate nix-expression from Chicken Scheme eggs";
+    description = "Generate nix-expression from CHICKEN scheme eggs";
     homepage = https://github.com/the-kenny/egg2nix;
     license = stdenv.lib.licenses.bsd3;
     platforms = stdenv.lib.platforms.unix;
