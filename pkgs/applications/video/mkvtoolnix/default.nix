03ee6621618b01d5674d8ef315babf51dcefdf69
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-10-27 00:54:13 +0000

    mkvtoolnix: update from 7.2.0 to 7.3.0

--- pkgs/applications/video/mkvtoolnix/default.nix
+++ pkgs/applications/video/mkvtoolnix/default.nix
@@ -15,12 +15,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "7.2.0";
+  version = "7.3.0";
   name = "mkvtoolnix-${version}";
 
   src = fetchurl {
     url = "http://www.bunkus.org/videotools/mkvtoolnix/sources/${name}.tar.xz";
-    sha256 = "1bpmd37y2v4icv9iqjv3p4kr62jbdng2ar8vpiij3bdgwrjc6gv1";
+    sha256 = "086lg64pki6mz00h0a735hgvz4347zbcp3wz384sigqndn99zc1c";
   };
 
   buildInputs = [
