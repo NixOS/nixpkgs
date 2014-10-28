b7eead88bd25f095d1fbd4cb721d0fc4538a0fbf
William A. Kennington III <william@wkennington.com>
2014-10-28 15:43:07 -0700

    libksba: 1.2.0 -> 1.3.1

--- pkgs/development/libraries/libksba/default.nix
+++ pkgs/development/libraries/libksba/default.nix
@@ -1,17 +1,20 @@
 { stdenv, fetchurl, libgpgerror }:
 
 stdenv.mkDerivation rec {
-  name = "libksba-1.2.0";
+  name = "libksba-1.3.1";
 
   src = fetchurl {
     url = "mirror://gnupg/libksba/${name}.tar.bz2";
-    sha256 = "0jwk7hm3x3g4hd7l12z3d79dy7359x7lc88dq6z7q0ixn1jwxbq9";
+    sha256 = "1ya6mcd6pk2v1pylvlk6wv3869aslz2mr2xw2gs6faxx2ravk5mw";
   };
 
-  propagatedBuildInputs = [libgpgerror];
+  propagatedBuildInputs = [ libgpgerror ];
 
-  meta = {
+  meta = with stdenv.lib; {
     homepage = http://www.gnupg.org;
     description = "CMS and X.509 access library under development";
+    platforms = platforms.all;
+    license = licenses.lgpl3;
+    maintainers = with maintainers; [ wkennington ];
   };
 }
