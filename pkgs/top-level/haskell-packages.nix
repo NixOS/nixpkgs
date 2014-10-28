2e48af098687642d842c9e94a4e9aa65b46838a8
Peter Simons <simons@cryp.to>
2014-10-28 12:44:31 +0100

    Add hsc3-db haskell package to tree.

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -1283,6 +1283,8 @@ self : let callPackage = x : y : modifyPrio (newScope self x y); in
 
   hsc3-process = callPackage ../development/libraries/haskell/hsc3-process {};
 
+  hsc3-db = callPackage ../development/libraries/haskell/hsc3-db {};
+
   hsdns = callPackage ../development/libraries/haskell/hsdns {};
 
   hsemail = callPackage ../development/libraries/haskell/hsemail {};
