61d7aaeea2b14a3d5d5f6d63d8b20fe585a35aea
stewart <stewart@chaatz.com>
2014-10-29 15:16:41 +0800

    format changes + meta data changes to hammer package

--- pkgs/development/tools/parsing/hammer/default.nix
+++ pkgs/development/tools/parsing/hammer/default.nix
@@ -1,32 +1,29 @@
 { stdenv, fetchgit, glib, pkgconfig, python, scons, pythonPackages }:
 
 stdenv.mkDerivation rec {
+  name = "hammer-${version}";
+  version = "1.0.0-rc3";
 
-name = "hammer-${version}";
-
-version = "1.0.0-rc3";
-
-src = fetchgit {
+  src = fetchgit {
     url = "git://github.com/UpstandingHackers/hammer";
     sha256 = "1v8f2a6bgjgdkhbqz751bqjlwb9lmqn5x63xcskwcl2b9n36vqi9";
     rev = "e7aa73446e23f4af2fce5f88572aae848f212c16";
-};
+  };
 
-buildInputs = [ glib pkgconfig python scons ];
+  buildInputs = [ glib pkgconfig python scons ];
+  buildPhase = "scons prefix=$out";
+  installPhase = "scons prefix=$out install";
 
-buildPhase = "scons prefix=$out";
-
-installPhase = "scons prefix=$out install";
-
-meta = with stdenv.lib; {
-     description = "Hammer is a parsing library. Like many modern parsing libraries,
+  meta = with stdenv.lib; {
+    description = "Hammer is a parsing library";
+    longDescription = "Hammer is a parsing library. Like many modern parsing libraries,
 	       it provides a parser combinator interface for writing grammars
 	       as inline domain-specific languages, but Hammer also provides a
 	       variety of parsing backends. It's also bit-oriented rather than
 	       character-oriented, making it ideal for parsing binary data such
 	       as images, network packets, audio, and executables.";
-     homepage = https://github.com/UpstandingHackers/hammer;
-     license = licenses.gpl2;
-     platforms = platforms.linux;
-     };
+    homepage = https://github.com/UpstandingHackers/hammer;
+    license = licenses.gpl2;
+    platforms = platforms.linux;
+    };
 }
