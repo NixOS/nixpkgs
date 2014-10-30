e5257409cdd35f720bf8374db9abd0acffd82866
Marko Durkovic <marko@miding.de>
2014-10-30 09:50:35 -0700

    cygwin: Use makeFlags var in zip

--- pkgs/tools/archivers/zip/default.nix
+++ pkgs/tools/archivers/zip/default.nix
@@ -13,7 +13,7 @@ stdenv.mkDerivation {
     sha256 = "0sb3h3067pzf3a7mlxn1hikpcjrsvycjcnj9hl9b1c3ykcgvps7h";
   };
 
-  buildFlags="-f unix/Makefile ${if stdenv.isCygwin then "cygwin" else "generic"}";
+  makeFlags="-f unix/Makefile ${if stdenv.isCygwin then "cygwin" else "generic"}";
 
   installFlags="-f unix/Makefile prefix=$(out) INSTALL=cp";
 
