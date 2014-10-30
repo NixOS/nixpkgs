5b37f998fdaf26cf1a7e459db79964b1938c1252
William A. Kennington III <william@wkennington.com>
2014-10-30 14:38:41 -0700

    kernel: 3.14.22 -> 3.14.23

--- pkgs/os-specific/linux/kernel/linux-3.14.nix
+++ pkgs/os-specific/linux/kernel/linux-3.14.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.14.22";
+  version = "3.14.23";
   extraMeta.branch = "3.14";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "115hcaxavgqz549y0ly5ijf7cwc0z549wlwwjlm27i0z3fjcg83p";
+    sha256 = "1zqh7fza318yv04qssw9lkcxpw590vrigbdrswcr885iwk561cb8";
   };
 
   features.iwlwifi = true;
