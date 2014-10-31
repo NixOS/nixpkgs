b94f27b846dd5573ce02e37de0157b9fc440e3b4
William A. Kennington III <william@wkennington.com>
2014-10-31 13:32:01 -0700

    ceph: 0.86 -> 0.87

--- pkgs/tools/filesystems/ceph/default.nix
+++ pkgs/tools/filesystems/ceph/default.nix
@@ -14,15 +14,18 @@ let
 in
 stdenv.mkDerivation rec {
   name="ceph-${version}";
-  version="0.86";
+  version="0.87";
 
   src = fetchgit {
     url = "git://github.com/ceph/ceph.git";
     rev = "refs/tags/v${version}";
-    sha256 = "19bl96z97kvsrliwid4g6dl7s3i0nw5z9nmg964i7jdwlsl98cfj";
+    sha256 = "0l3ig4kr50j78snjzj4h1lbmjz508yykvchqyfdcqv02dfklc8qd";
   };
 
-  patches = [ ./0001-Makefile-env-Don-t-force-sbin.patch ];
+  patches = [
+    ./0001-Cleanup-boost-optionals.patch # Remove in >0.87 patch is applied
+    ./0001-Makefile-env-Don-t-force-sbin.patch
+  ];
 
   nativeBuildInputs = [ autoconf automake makeWrapper pkgconfig libtool which ];
   buildInputs = [
