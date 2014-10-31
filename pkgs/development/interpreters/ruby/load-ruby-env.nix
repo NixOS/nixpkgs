143698555a7927c926d5fa4180c4349bf371798b
Charles Strahan <charles.c.strahan@gmail.com>
2014-10-31 02:03:37 +0000

    add documentation

--- pkgs/development/interpreters/ruby/load-ruby-env.nix
+++ pkgs/development/interpreters/ruby/load-ruby-env.nix
@@ -1,5 +1,31 @@
 { ruby, lib, callPackage, gemFixes, fetchurl, buildRubyGem }@defs:
 
+# This function builds a set of gems. You first convert your Gemfile to an attrset
+# called a "gemset", and then use this function to build the gemset.
+#
+# A gemset looks like the following:
+#
+#   {
+#     libv8 = {
+#       version = "3.16.14.7";
+#       src = {
+#         type = "gem";
+#         sha256 = "...";
+#       };
+#     };
+#     therubyracer = {
+#       version = "0.12.1";
+#       dependencies = [ "libv8" ];
+#       src = {
+#         type = "gem";
+#         sha256 = "...";
+#       };
+#     };
+#   }
+#
+# If you use these gems as build inputs, the GEM_PATH will be updated
+# appropriately, and command like `bundle exec` should work out of the box.
+
 { gemset, ruby ? defs.ruby, fixes ? gemFixes }@args:
 
 let
