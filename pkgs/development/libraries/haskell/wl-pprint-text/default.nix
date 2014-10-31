3ee6f3b3aad3d37a22dc095f73c29cf761d1d442
Peter Simons <simons@cryp.to>
2014-10-31 13:40:15 +0100

    haskell-wl-pprint-text: jailbreak to fix build with recent versions of 'text'

--- pkgs/development/libraries/haskell/wl-pprint-text/default.nix
+++ pkgs/development/libraries/haskell/wl-pprint-text/default.nix
@@ -7,6 +7,7 @@ cabal.mkDerivation (self: {
   version = "1.1.0.2";
   sha256 = "0wbfqp38as2qpn66sq4hvl3hzvj66v301cz9rmgnx2i62r0a3s81";
   buildDepends = [ text ];
+  jailbreak = true;
   meta = {
     description = "A Wadler/Leijen Pretty Printer for Text values";
     license = self.stdenv.lib.licenses.bsd3;
