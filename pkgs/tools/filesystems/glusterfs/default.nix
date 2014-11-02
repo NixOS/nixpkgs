815bbfb85244f752c43325e33dbc45b8ff5797b8
Michael Raskin <7c6f434c@mail.ru>
2014-11-02 23:24:51 +0300

    Update GlusterFS

--- pkgs/tools/filesystems/glusterfs/default.nix
+++ pkgs/tools/filesystems/glusterfs/default.nix
@@ -1,18 +1,18 @@
 {stdenv, fetchurl, fuse, bison, flex_2_5_35, openssl, python, ncurses, readline,
- autoconf, automake, libtool, pkgconfig, zlib, libaio}:
+ autoconf, automake, libtool, pkgconfig, zlib, libaio, libxml2}:
 let 
   s = # Generated upstream information 
   rec {
     baseName="glusterfs";
-    version="3.5.2";
+    version="3.6.0";
     name="${baseName}-${version}";
-    hash="1hvns9islr5jcy0r1cw5890ra246y12pl5nlhl3bvmhglrkv8n7g";
-    url="http://download.gluster.org/pub/gluster/glusterfs/3.5/3.5.2/glusterfs-3.5.2.tar.gz";
-    sha256="1hvns9islr5jcy0r1cw5890ra246y12pl5nlhl3bvmhglrkv8n7g";
+    hash="1c4lscqc5kvn5yj5pspvml59n1czspfqqdwhz73hbjd5lbqak9ml";
+    url="http://download.gluster.org/pub/gluster/glusterfs/3.6/3.6.0/glusterfs-3.6.0.tar.gz";
+    sha256="1c4lscqc5kvn5yj5pspvml59n1czspfqqdwhz73hbjd5lbqak9ml";
   };
   buildInputs = [
     fuse bison flex_2_5_35 openssl python ncurses readline
-    autoconf automake libtool pkgconfig zlib libaio
+    autoconf automake libtool pkgconfig zlib libaio libxml2
   ];
 in
 stdenv.mkDerivation
