9c458ad8d4c37e3c5786f477ee214e2176a6c1f4
Michael Raskin <7c6f434c@mail.ru>
2014-11-02 23:14:57 +0300

    Update Wine-unstable

--- pkgs/misc/emulators/wine/unstable.nix
+++ pkgs/misc/emulators/wine/unstable.nix
@@ -7,12 +7,12 @@ assert stdenv.isLinux;
 assert stdenv.gcc.gcc != null;
 
 let
-    version = "1.7.29";
+    version = "1.7.30";
     name = "wine-${version}";
 
     src = fetchurl {
       url = "mirror://sourceforge/wine/${name}.tar.bz2";
-      sha256 = "0qnyp1svzh2yyc3p34dg8hf21lwzxn4xkgpa3s0rfcak6l6ijj5f";
+      sha256 = "0v08w2av46y4wxrl8q4k9fhbi2cgawjaxclahqnpfw098bqcmxqh";
     };
 
     gecko = fetchurl {
