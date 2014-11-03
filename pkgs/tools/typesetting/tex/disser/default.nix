1042d595c4267ef5cf315f56c4beee9ddfbd88ed
Michael Raskin <7c6f434c@mail.ru>
2014-11-03 13:12:13 +0300

    Update TeX-disser thesis template for Russian styles

--- pkgs/tools/typesetting/tex/disser/default.nix
+++ pkgs/tools/typesetting/tex/disser/default.nix
@@ -12,11 +12,11 @@ let
     ++ [(a.texLiveAggregationFun {paths=[a.texLive a.texLiveCMSuper];})];
   sourceInfo = rec {
     baseName="disser";
-    version="1.1.8";
+    version="1.3.0";
     name="${baseName}-${version}";
     project="${baseName}";
     url="mirror://sourceforge/project/${project}/${baseName}/${version}/${name}.zip";
-    hash="15509hfcvkk5kfcza149c74qpamwgw88dg0ra749axs8xj8qmlw8";
+    hash="1iab3va6xw53l8xzmd83kbnzqah9f6imzzfd3c2054q53p0ndlim";
   };
 in
 rec {
