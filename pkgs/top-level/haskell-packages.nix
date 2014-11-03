984b0cbd8afdc07c55f83a31fe809113afd9641d
Peter Simons <simons@cryp.to>
2014-11-03 18:32:35 +0100

    uhc: remove broken package
    https://github.com/NixOS/nixpkgs/issues/3565

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -3007,8 +3007,6 @@ self : let callPackage = x : y : modifyPrio (newScope self x y); in
 
   Agda = callPackage ../development/compilers/agda { haskellSrcExts = self.haskellSrcExts_1_15_0_1; };
 
-  uhc = callPackage ../development/compilers/uhc {};
-
   epic = callPackage ../development/compilers/epic {};
 
   pakcs = callPackage ../development/compilers/pakcs {};
