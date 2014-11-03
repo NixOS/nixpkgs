e4655f0b4531b66e27de4ec45cb5b8230c8416a6
Vincent Laporte <Vincent.Laporte@gmail.com>
2014-11-03 20:04:48 +0000

    ocaml-lwt: fixes build with OCaml 3.12 and 4.02

--- pkgs/development/ocaml-modules/lwt/default.nix
+++ pkgs/development/ocaml-modules/lwt/default.nix
@@ -1,7 +1,9 @@
-{stdenv, fetchgit, which, cryptopp, ocaml, findlib, ocaml_react, ocaml_ssl, libev, pkgconfig, ncurses, ocaml_oasis, ocaml_text, glib}:
+{stdenv, fetchgit, which, cryptopp, ocaml, findlib, ocaml_react, ocaml_ssl, libev, pkgconfig, ncurses, ocaml_oasis, ocaml_text, glib, camlp4}:
 
 let
   version = "2.4.5";
+  inherit (stdenv.lib) optional getVersion versionAtLeast;
+  ocaml_version = getVersion ocaml;
 in
 
 stdenv.mkDerivation {
@@ -15,21 +17,22 @@ stdenv.mkDerivation {
     sha256 = "2bbf4f216dd62eeb765a89413f3b2b6d417a9c289ca49d595bb4d7a0545e343e";
   };
 
-  buildInputs = [ocaml_oasis pkgconfig which cryptopp ocaml findlib glib libev ncurses];
+  buildInputs = [ocaml_oasis pkgconfig which cryptopp ocaml findlib glib libev ncurses camlp4];
 
   propagatedBuildInputs = [ ocaml_react ocaml_ssl ocaml_text ];
 
-  configureFlags = [ "--enable-all" ];
+  configureFlags = [ "--enable-react" "--enable-glib" "--enable-ssl" "--enable-text" ]
+  ++ optional (versionAtLeast ocaml_version "4.0" && ! versionAtLeast ocaml_version "4.02") "--enable-toplevel";
 
   createFindlibDestdir = true;
 
-  meta = {
+  meta = with stdenv.lib; {
     homepage = http://ocsigen.org/lwt;
     description = "Lightweight thread library for Objective Caml";
-    license = "LGPL";
+    license = licenses.lgpl21;
     platforms = ocaml.meta.platforms;
-    maintainers = [
-      stdenv.lib.maintainers.z77z stdenv.lib.maintainers.gal_bolle
+    maintainers = with maintainers; [
+      z77z vbgl gal_bolle
     ];
   };
 }
