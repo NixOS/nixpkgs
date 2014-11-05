6e726e1348a4ab130b7c531b784b62b6dfa3dbb6
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-11-05 11:33:45 +0000

    sxiv: use https

--- pkgs/applications/graphics/sxiv/default.nix
+++ pkgs/applications/graphics/sxiv/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
   name = "sxiv-${version}";
 
   src = fetchgit {
-    url = "git@github.com:muennich/sxiv.git";
+    url = "https://github.com/muennich/sxiv.git";
     rev = "f55d9f4283f7133ab5a137fc04ee19d1df62fafb";
     sha256 = "85f734f40fdc837514b72694de12bac92fe130286fa6f1dc374e94d575ca8280";
   };
