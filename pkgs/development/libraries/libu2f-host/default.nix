9a7143918bab8609338676e5053589099387cba1
William A. Kennington III <william@wkennington.com>
2014-11-05 22:03:10 -0800

    libu2f-host: 0.0 -> 0.0.1

--- pkgs/development/libraries/libu2f-host/default.nix
+++ pkgs/development/libraries/libu2f-host/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, json_c, hidapi }:
 
 stdenv.mkDerivation rec {
-  name = "libu2f-host-0.0";
+  name = "libu2f-host-0.0.1";
 
   src = fetchurl {
     url = "https://developers.yubico.com/libu2f-host/Releases/${name}.tar.xz";
-    sha256 = "02pjald2j6syvxm5pszxcpqhpp7c80hblnzh6wrafkmpkpzi3rq5";
+    sha256 = "1cqgvbh2fim9r7pjazph64xnrhmsydqh8xrnxd4g16mc0k76s4mf";
   };
 
   buildInputs = [ pkgconfig json_c hidapi ];
