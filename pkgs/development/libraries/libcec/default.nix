e79cda5a2f2dcad0666a36d0e8aab821e9874273
Kirill Elagin <kirelagin@gmail.com>
2014-11-06 21:05:01 +0300

    libcec: Update to 2.2.0

--- pkgs/development/libraries/libcec/default.nix
+++ pkgs/development/libraries/libcec/default.nix
@@ -1,26 +1,25 @@
-{ stdenv, fetchurl, autoreconfHook, pkgconfig, udev }:
+{ stdenv, fetchurl, autoreconfHook, pkgconfig, udev, makeWrapper }:
 
-let version = "2.1.4"; in
+let version = "2.2.0"; in
 
 stdenv.mkDerivation {
   name = "libcec-${version}";
 
   src = fetchurl {
     url = "https://github.com/Pulse-Eight/libcec/archive/libcec-${version}.tar.gz";
-    sha256 = "0iz11zclbs3gk4ddq0pm4vyq015qmvy4nb9sra3vk6jw58izbgkr";
+    sha256 = "05r9ln52mlcsw0mf66klq9gjlazf0w12hcaqdyg4n3vfinhlfkzx";
   };
 
-  buildInputs = [ autoreconfHook pkgconfig udev ];
+  buildInputs = [ autoreconfHook pkgconfig udev makeWrapper ];
 
-  # Fix dlopen path
-  patchPhase = ''
-    substituteInPlace include/cecloader.h --replace "libcec.so" "$out/lib/libcec.so"
+  postInstall = ''
+    wrapProgram $out/bin/cec-client --prefix LD_LIBRARY_PATH : $out/lib
   '';
 
   meta = with stdenv.lib; {
-    description = "USB CEC adapter communication library";
-    homepage = "http://libcec.pulse-eight.com";
-    repositories.git = "https://github.com/Pulse-Eight/libcec.git";
+    description = "libCEC allows you in combination with the right hardware to control your device with your TV remote control. Utilising your existing HDMI cabling";
+    homepage = http://libcec.pulse-eight.com;
+    repositories.git = https://github.com/Pulse-Eight/libcec.git;
     license = stdenv.lib.licenses.gpl2Plus;
     platforms = platforms.linux;
     maintainers = [ maintainers.titanous ];
