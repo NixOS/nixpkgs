4c84621a7f55a6b1f64c391b64e0206195d65b6f
Peter Simons <simons@cryp.to>
2014-11-07 16:51:41 +0100

    Fix gnupgcompat1 which doesn't seem to work with gnupg version 2.1.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1269,7 +1269,7 @@ let
 
   gnupg1orig = callPackage ../tools/security/gnupg/1.nix { };
 
-  gnupg1compat = callPackage ../tools/security/gnupg/1compat.nix { };
+  gnupg1compat = callPackage ../tools/security/gnupg/1compat.nix { gnupg = gnupg20; };
 
   # use config.packageOverrides if you prefer original gnupg1
   gnupg1 = gnupg1compat;
