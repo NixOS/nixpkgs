49f0ff941d0ff224fcfde9b22c20fb5ba4aeaf2d
Peter Simons <simons@cryp.to>
2014-11-08 15:30:28 +0100

    haskell-text: drop obsolete versions 0.11.0.5, 0.11.0.6, 0.11.1.5, 0.11.2.0, and 0.11.3.1

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -2492,12 +2492,7 @@ self : let callPackage = x : y : modifyPrio (newScope self x y); in
 
   texmath = callPackage ../development/libraries/haskell/texmath {};
 
-  text_0_11_0_5 = callPackage ../development/libraries/haskell/text/0.11.0.5.nix {};
-  text_0_11_0_6 = callPackage ../development/libraries/haskell/text/0.11.0.6.nix {};
-  text_0_11_1_5 = callPackage ../development/libraries/haskell/text/0.11.1.5.nix {};
-  text_0_11_2_0 = callPackage ../development/libraries/haskell/text/0.11.2.0.nix {};
   text_0_11_2_3 = callPackage ../development/libraries/haskell/text/0.11.2.3.nix {};
-  text_0_11_3_1 = callPackage ../development/libraries/haskell/text/0.11.3.1.nix {};
   text_1_1_1_3 = callPackage ../development/libraries/haskell/text/1.1.1.3.nix {};
   text_1_2_0_0 = callPackage ../development/libraries/haskell/text/1.2.0.0.nix {};
   text = self.text_1_2_0_0;
