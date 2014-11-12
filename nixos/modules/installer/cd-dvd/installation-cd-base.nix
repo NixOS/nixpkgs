433a845081e5067995dc64ceea5bb5b33c16108b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-11-12 15:32:21 +0100

    Disable zfs in the ISO for now
    It's giving zpool-import.service errors.
    
    http://hydra.nixos.org/build/16883184

--- nixos/modules/installer/cd-dvd/installation-cd-base.nix
+++ nixos/modules/installer/cd-dvd/installation-cd-base.nix
@@ -43,7 +43,7 @@ with lib;
   boot.initrd.kernelModules = [ "fbcon" ];
 
   # Add support for cow filesystems and their utilities
-  boot.supportedFilesystems = [ "zfs" "btrfs" ];
+  boot.supportedFilesystems = [ /* "zfs" */ "btrfs" ];
 
   # Allow the user to log in as root without a password.
   users.extraUsers.root.initialHashedPassword = "";
