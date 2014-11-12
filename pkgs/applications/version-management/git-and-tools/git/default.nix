1b9f756e4e258e14fb1d4be9b492423f28a1bab0
Vladimir Kirillov <proger@wilab.org.ua>
2014-11-12 17:36:17 +0200

    git: do not do install-doc when !withManual

--- pkgs/applications/version-management/git-and-tools/git/default.nix
+++ pkgs/applications/version-management/git-and-tools/git/default.nix
@@ -55,7 +55,7 @@ stdenv.mkDerivation {
       # Install git-subtree.
       pushd contrib/subtree
       make
-      make install install-doc
+      make install ${stdenv.lib.optionalString withManual "install-doc"}
       popd
       rm -rf contrib/subtree
 
