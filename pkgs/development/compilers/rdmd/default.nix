959f706acec79ac314663b3faa7b0c75b6cf15a2
BlackEdder <edder@tkwsping.nl>
2014-11-13 20:51:32 +0000

    rdmd: update to new version
    Source of old version was not available anymore. Update also depends on
    newer version of dmd.

--- pkgs/development/compilers/rdmd/default.nix
+++ pkgs/development/compilers/rdmd/default.nix
@@ -1,29 +1,29 @@
-{ stdenv, fetchurl, writeText, lib, dmd }:
+{stdenv, lib, fetchgit, dmd}:
 
 stdenv.mkDerivation {
-  name = "rdmd-2.064";
-
-  src = fetchurl {
-    url = https://raw2.github.com/D-Programming-Language/tools/2.064/rdmd.d;
-    sha256 = "0b1g3ng6bkanvg00r6xb4ycpbh9x8b9dw589av665azxbcraqrs1";
-    name = "rdmd-src";
-  };
+  name = "rdmd-20141113";
 
   buildInputs = [ dmd ];
 
-  builder = writeText "drmd-builder.sh" ''
-      source $stdenv/setup
-      cp $src rdmd.d
-      dmd rdmd.d
-      mkdir -p $out/bin
-      cp rdmd $out/bin/
+  src = fetchgit {
+    url = git://github.com/D-Programming-Language/tools.git;
+    rev = "f496c68ee4e776597bd7382aa47f05da698a69e";
+    sha256 = "0vbhmz8nbh8ayml4vad0239kfg982vqfyqqrjv6wrlnjah97n5ms";
+  };
+
+  buildPhase = ''
+    dmd rdmd.d
   '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp rdmd $out/bin/
+	'';
 
   meta = {
     description = "Wrapper for D language compiler";
     homepage = http://dlang.org/rdmd.html;
     license = lib.licenses.boost;
-    maintainers = with stdenv.lib.maintainers; [ vlstill ];
     platforms = stdenv.lib.platforms.unix;
   };
 }
