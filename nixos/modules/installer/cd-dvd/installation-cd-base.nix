dda62c451cd0723f3cda6b81cc0d60a0da803596
wmertens <Wout.Mertens@gmail.com>
2014-11-13 10:31:15 +0100

    Re-enable ZFS in installer image
    Hopefully #4807 fixes http://hydra.nixos.org/build/16883184

--- nixos/modules/installer/cd-dvd/installation-cd-base.nix
+++ nixos/modules/installer/cd-dvd/installation-cd-base.nix
@@ -43,7 +43,7 @@ with lib;
   boot.initrd.kernelModules = [ "fbcon" ];
 
   # Add support for cow filesystems and their utilities
-  boot.supportedFilesystems = [ /* "zfs" */ "btrfs" ];
+  boot.supportedFilesystems = [ "zfs" "btrfs" ];
 
   # Configure host id for ZFS to work
   networking.hostId = "8425e349";
