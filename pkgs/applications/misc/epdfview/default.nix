237bf5443fba2f5565a4486f7cbc3145ea5b3bb7
Nikolay Amiantov <ab@fmap.me>
2014-11-14 16:30:38 +0300

    epdfview: fix building with cups 2.0

--- pkgs/applications/misc/epdfview/default.nix
+++ pkgs/applications/misc/epdfview/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, gtk, poppler }:
+{ stdenv, fetchurl, fetchpatch, pkgconfig, gtk, poppler }:
 stdenv.mkDerivation rec {
   name = "epdfview-0.1.8";
   src = fetchurl {
@@ -6,7 +6,17 @@ stdenv.mkDerivation rec {
     sha256 = "1w7qybh8ssl4dffi5qfajq8mndw7ipsd92vkim03nywxgjp4i1ll";
   };
   buildInputs = [ pkgconfig gtk poppler ];
-  patches = [ ./glib-top-level-header.patch ];
+  patches = [ (fetchpatch {
+                name = "epdfview-0.1.8-glib2-headers.patch";
+                url = "https://projects.archlinux.org/svntogit/community.git/plain/trunk/epdfview-0.1.8-glib2-headers.patch?h=packages/epdfview&id=40ba115c860bdec31d03a30fa594a7ec2864d634";
+                sha256 = "13ha8l3ysw065wr1gpi33dr3r23pv2w51iczzkx6pnxsqk9cda4f";
+              })
+              (fetchpatch {
+                name = "epdfview-0.1.8-modern-cups.patch";
+                url = "https://projects.archlinux.org/svntogit/community.git/plain/trunk/epdfview-0.1.8-modern-cups.patch?h=packages/epdfview&id=40ba115c860bdec31d03a30fa594a7ec2864d634";
+                sha256 = "15pjriq0ppnjgmr26r9zj9smv301w8szwps6n87sshm0yiw9sli7";
+              })
+            ];
   meta = {
     homepage = http://trac.emma-soft.com/epdfview/;
     description = "A lightweight PDF document viewer using Poppler and GTK+";
