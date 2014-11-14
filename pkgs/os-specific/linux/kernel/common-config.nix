557a3c92e3cb5f81e9d1a72ab9cfc1964a3dadd6
William A. Kennington III <william@wkennington.com>
2014-11-13 16:23:49 -0800

    kernel: Don't enable the iommu by default as this breaks for some hardware

--- pkgs/os-specific/linux/kernel/common-config.nix
+++ pkgs/os-specific/linux/kernel/common-config.nix
@@ -318,9 +318,6 @@ with stdenv.lib;
   ${optionalString (!stdenv.is64bit) ''
     HIGHMEM64G? y # We need 64 GB (PAE) support for Xen guest support.
   ''}
-  ${optionalString stdenv.is64bit ''
-    INTEL_IOMMU_DEFAULT_ON y
-  ''}
   ${optionalString (versionAtLeast version "3.9" && stdenv.is64bit) ''
     VFIO_PCI_VGA y
   ''}
