642a1611122533d6eb6d641ad8e0dac207afeebf
William A. Kennington III <william@wkennington.com>
2014-11-14 10:50:34 -0800

    kernel: 3.2.63 -> 3.2.64

--- pkgs/os-specific/linux/kernel/linux-3.2.nix
+++ pkgs/os-specific/linux/kernel/linux-3.2.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.2.63";
+  version = "3.2.64";
   extraMeta.branch = "3.2";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0fxfk1dppmgv5pwy7jjyih03i9rz5acp0pww4g5sy2d59rzfpbn8";
+    sha256 = "0niw4pbfc5dd46wzgvfnza8isyl01v1lva4njlwnrzag6x9fj0qp";
   };
 
   # We don't provide these patches if grsecurity is enabled, because
