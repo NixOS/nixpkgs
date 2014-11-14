25a52470f83394843226b018c729501f3c240aea
Vincent Laporte <Vincent.Laporte@gmail.com>
2014-11-14 23:10:17 +0000

    pcmanfm: update from 1.2.2 to 1.2.3

--- pkgs/applications/misc/pcmanfm/default.nix
+++ pkgs/applications/misc/pcmanfm/default.nix
@@ -1,10 +1,10 @@
 { stdenv, fetchurl, glib, gtk, intltool, libfm, libX11, pango, pkgconfig }:
 
 stdenv.mkDerivation {
-  name = "pcmanfm-1.2.2";
+  name = "pcmanfm-1.2.3";
   src = fetchurl {
-    url = "mirror://sourceforge/pcmanfm/pcmanfm-1.2.2.tar.xz";
-    sha256 = "00wmbqrbcrxk1mcdkgdl8ddf3gp28rm5mamqdq72sfr0q2d0287n";
+    url = "mirror://sourceforge/pcmanfm/pcmanfm-1.2.3.tar.xz";
+    sha256 = "1033rw5jd7nlzbcdpx3bik7347kyh1sg1gkla424gq9vqqpxia6g";
   };
 
   buildInputs = [ glib gtk intltool libfm libX11 pango pkgconfig ];
