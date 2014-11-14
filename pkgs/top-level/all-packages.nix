c3a8624ac2cde6c38fec606788f71c5bd4d5344b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-11-14 15:57:03 +0100

    firefox: Use regular linker
    It builds on Nix >= 1.8pre3890 due to the removal of the
    ADDR_LIMIT_3GB personality flag.
    
    (cherry picked from commit bf17f43fe96e454d748261f6d1bcf84a29ca2cf1)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8290,7 +8290,6 @@ let
   firefox13Wrapper = wrapFirefox { browser = firefox13Pkgs.firefox; };
 
   firefox = callPackage ../applications/networking/browsers/firefox {
-    stdenv = if stdenv.isLinux then useGoldLinker stdenv else stdenv;
     inherit (gnome) libIDL;
     inherit (pythonPackages) pysqlite;
   };
