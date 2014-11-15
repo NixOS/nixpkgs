2c19d082fbca4a216c7e816922e6a55d0d652f56
Pascal Wittmann <mail@pascal-wittmann.de>
2014-11-15 12:19:09 +0100

    opkg: update from 0.2.3 to 0.2.4

--- pkgs/tools/package-management/opkg/default.nix
+++ pkgs/tools/package-management/opkg/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, curl, gpgme }:
 
 stdenv.mkDerivation rec {
-  version = "0.2.3";
+  version = "0.2.4";
   name = "opkg-${version}";
   src = fetchurl {
     url = "http://downloads.yoctoproject.org/releases/opkg/opkg-${version}.tar.gz";
-    sha256 = "0qaczbw34az20vmh92h7fkswcgq7f6csajx35d4dljzfj85d8jcc";
+    sha256 = "11ih8lay1v7sk5czk3m3cfqil4cp8s7pfz60xnddy7nqazjcfh0g";
   };
 
   buildInputs = [ pkgconfig curl gpgme ];
