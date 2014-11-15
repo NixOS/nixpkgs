17140ebba89883113291bd8f7179e90f55dd8230
Michael Raskin <7c6f434c@mail.ru>
2014-11-16 00:12:21 +0400

    Fix tex4ht build: our new stdenv lacks CC (why, by the way?) so default to gcc if CC empty

--- pkgs/tools/typesetting/tex/tex4ht/default.nix
+++ pkgs/tools/typesetting/tex/tex4ht/default.nix
@@ -14,7 +14,7 @@ stdenv.mkDerivation rec {
     cd src
     for f in tex4ht t4ht htcmd ; do
       # -DENVFILE="$out/share/texmf-nix/tex4ht/base/unix/tex4ht.env"
-      $CC -o $f $f.c -I${tetex}/include -L${tetex}/lib  -DHAVE_DIRENT_H -DHAVE_DIRENT_H -DKPATHSEA -lkpathsea
+      ''${CC:-gcc} -o $f $f.c -I${tetex}/include -L${tetex}/lib  -DHAVE_DIRENT_H -DHAVE_DIRENT_H -DKPATHSEA -lkpathsea
     done
     cd -
   '';
