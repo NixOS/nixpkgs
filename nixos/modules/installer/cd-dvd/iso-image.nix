b45acd35a142f16cca7d759513761d83f9f6363c
Bob van der Linden <bobvanderlinden@gmail.com>
2014-11-16 20:13:49 +0100

    nixos: iso-image: removed com32 entries from syslinux menu
    These entries result in incorrect entries when UNetbootin writes
    the image to an USB disk.

--- nixos/modules/installer/cd-dvd/iso-image.nix
+++ nixos/modules/installer/cd-dvd/iso-image.nix
@@ -22,19 +22,6 @@ let
     LINUX /boot/bzImage
     APPEND init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams}
     INITRD /boot/initrd
-
-    LABEL chain
-    MENU LABEL Boot existing OS
-    COM32 chain.c32
-    APPEND hd0 0
-
-    LABEL reboot
-    MENU LABEL Reboot
-    COM32 reboot.c32
-
-    LABEL poweroff
-    MENU LABEL Power Off
-    COM32 poweroff.c32
     '';
 
   # The efi boot image
