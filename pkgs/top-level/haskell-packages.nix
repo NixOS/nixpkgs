7319cb58e1424637fae490043bfb5b94aff3de39
David Virgilio <drvirgilio@gmail.com>
2014-11-16 03:04:06 -0600

    new package: binary-bits

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -247,6 +247,8 @@ self : let callPackage = x : y : modifyPrio (newScope self x y); in
   binary_0_7_2_2 = callPackage ../development/libraries/haskell/binary/0.7.2.2.nix {};
   binary = null;                # core package since ghc >= 7.2.x
 
+  binaryBits = callPackage ../development/libraries/haskell/binary-bits {};
+
   binaryConduit = callPackage ../development/libraries/haskell/binary-conduit {};
 
   binaryShared = callPackage ../development/libraries/haskell/binary-shared {};
