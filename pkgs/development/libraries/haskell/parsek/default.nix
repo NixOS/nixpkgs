98a06ff71f057ffdfb3b5cd2e441b9dad62d01fd
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2014-11-17 15:52:00 +0100

    add myself as maintainer for parsek.

--- pkgs/development/libraries/haskell/parsek/default.nix
+++ pkgs/development/libraries/haskell/parsek/default.nix
@@ -8,7 +8,8 @@ cabal.mkDerivation (self: {
   sha256 = "197hjys71f9qgsw83w3wrmijimjzvb9cy4kywkqr95ack8a7pizn";
   meta = {
     description = "Parallel Parsing Processes";
-    license = "GPL";
+    license = self.stdenv.lib.licenses.gpl3;
     platforms = self.ghc.meta.platforms;
+    maintainers = with self.stdenv.lib.maintainers; [ jpbernardy ];
   };
 })
