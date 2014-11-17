88ea3e8c484961830715ee596d3b8149206201c7
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-11-17 18:07:17 +0000

    bsddb3: update from 6.0.1 to 6.1.0

--- pkgs/development/python-modules/bsddb3/default.nix
+++ pkgs/development/python-modules/bsddb3/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, python, db}:
 
 stdenv.mkDerivation rec {
-  name = "bsddb3-6.0.1";
+  name = "bsddb3-6.1.0";
   src = fetchurl {
     url = "https://pypi.python.org/packages/source/b/bsddb3/${name}.tar.gz";
-    md5 = "2b22ab1b4d896961c30e4106660e9139";
+    sha256 = "05gx3rfgq1qrgdmpd6hri6y5l97bh1wczvb6x853jchwi7in6cdi";
   };
   buildInputs = [python];
   buildPhase = "true";
