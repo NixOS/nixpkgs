818bd7dd1c28c6f702a3ad825e3b46f3aaa33db9
Pascal Wittmann <mail@pascal-wittmann.de>
2014-11-18 20:52:29 +0100

    cppcheck: update from 1.66 to 1.67

--- pkgs/development/tools/analysis/cppcheck/default.nix
+++ pkgs/development/tools/analysis/cppcheck/default.nix
@@ -4,14 +4,14 @@
 
 let
   name = "cppcheck";
-  version = "1.66";
+  version = "1.67";
 in
 stdenv.mkDerivation {
   name = "${name}-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/${name}/${name}-${version}.tar.bz2";
-    sha256 = "1dy1jd9ckqs565iwcv281pc3xmp9fnfw5ydh3wq06pyz74yw4scl";
+    sha256 = "1f9azv714mk37mjij29nfyd3hizsnj6wry1mmv7kxj0i1k7w0532";
   };
 
   configurePhase = ''
