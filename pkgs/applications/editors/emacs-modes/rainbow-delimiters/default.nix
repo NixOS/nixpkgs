546ece8f25575b66d3f3d4417a37c88b691d9f45
Cillian de Róiste <cillian.deroiste@gmail.com>
2014-11-18 21:23:41 +0100

    emacs24Packages.rainbowDelimiters: update from 1.3.12 to 1.3.13

--- pkgs/applications/editors/emacs-modes/rainbow-delimiters/default.nix
+++ pkgs/applications/editors/emacs-modes/rainbow-delimiters/default.nix
@@ -1,13 +1,13 @@
 {stdenv, fetchurl, emacs}:
 
-let version = "1.3.12";
+let version = "1.3.13";
 
 in stdenv.mkDerivation {
   name = "emacs-rainbow-delimiters-${version}";
 
   src = fetchurl {
     url = "https://github.com/jlr/rainbow-delimiters/archive/${version}.tar.gz";
-    sha256 = "0l65rqmnrc02q1b406kxc29w5cfpmrmq0glv493pjzhzc5m3r63z";
+    sha256 = "075j3nsk4jm0rs5671n28c1wksrfbvpl9a4f89kzcd7sk1h6ncvl";
   };
 
   buildInputs = [ emacs ];
