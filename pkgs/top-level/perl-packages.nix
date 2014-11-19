1f071d37efc56e0fbc0c81ed612fd83bedb3c02f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-11-19 12:07:16 +0100

    Add some Hydra dependencies to the channel

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -693,6 +693,7 @@ let self = _self // overrides; _self = with self; {
     meta = {
       description = "A storage class for Catalyst Authentication using DBIx::Class";
       license = with stdenv.lib.licenses; [ artistic1 gpl1Plus ];
+      platforms = stdenv.lib.platforms.linux;
     };
   };
 
@@ -992,6 +993,7 @@ let self = _self // overrides; _self = with self; {
     meta = {
       description = "Display a stack trace on the debug screen";
       license = with stdenv.lib.licenses; [ artistic1 gpl1Plus ];
+      platforms = stdenv.lib.platforms.linux;
     };
   };
 
@@ -1017,6 +1019,10 @@ let self = _self // overrides; _self = with self; {
     };
     buildInputs = [  TestWWWMechanizeCatalyst Testuseok ];
     propagatedBuildInputs = [ CatalystRuntime TextCSV XMLSimple ];
+    meta = {
+      license = "perl";
+      platforms = stdenv.lib.platforms.linux;
+    };
   };
 
   CatalystViewJSON = buildPerlPackage {
@@ -1068,14 +1074,19 @@ let self = _self // overrides; _self = with self; {
     propagatedBuildInputs = [ MooseXRelatedClassRoles CatalystRuntime ];
   };
 
-  CatalystTraitForRequestProxyBase = buildPerlPackage rec {
+  CatalystTraitForRequestProxyBase = buildPerlPackage {
     name = "Catalyst-TraitFor-Request-ProxyBase-0.000005";
     src = fetchurl {
-      url = "mirror://cpan/modules/by-module/Catalyst/${name}.tar.gz";
-      sha256 = "02kir63d5cs2ipj3fn1qlmmx3gqi1xqzrxfr4pv5vjhjgsm0zgx7";
+      url = mirror://cpan/authors/id/B/BO/BOBTFISH/Catalyst-TraitFor-Request-ProxyBase-0.000005.tar.gz;
+      sha256 = "a7bf0faa7e12ca5df625d9f5fc710f11bfd16ba5385837e48d42b3d286c9710a";
     };
-    buildInputs = [ CatalystRuntime ];
-    propagatedBuildInputs = [ Moose URI CatalystXRoleApplicator ];
+    buildInputs = [ CatalystRuntime HTTPMessage ];
+    propagatedBuildInputs = [ CatalystXRoleApplicator Moose URI namespaceautoclean ];
+    meta = {
+      description = "Replace request base with value passed by HTTP proxy";
+      license = "perl";
+      platforms = stdenv.lib.platforms.linux;
+    };
   };
 
   CatalystXScriptServerStarman = buildPerlPackage {
@@ -3195,6 +3206,7 @@ let self = _self // overrides; _self = with self; {
       homepage = https://github.com/rjbs/email-sender;
       description = "A library for sending email";
       license = with stdenv.lib.licenses; [ artistic1 gpl1Plus ];
+      platforms = stdenv.lib.platforms.linux;
     };
   };
 
