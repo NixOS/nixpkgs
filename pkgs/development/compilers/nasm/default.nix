885a29eefdb1205015e823e2ecdba7c304c5e159
Pascal Wittmann <mail@pascal-wittmann.de>
2014-11-19 08:40:18 +0100

    nasm: update from 2.11.05 to 2.11.06 and adopt it

--- pkgs/development/compilers/nasm/default.nix
+++ pkgs/development/compilers/nasm/default.nix
@@ -2,16 +2,17 @@
 
 stdenv.mkDerivation rec {
   name = "nasm-${version}";
-  version = "2.11.05";
+  version = "2.11.06";
   
   src = fetchurl {
     url = "http://www.nasm.us/pub/nasm/releasebuilds/${version}/${name}.tar.bz2";
-    sha256 = "1sgspnascc0asmwlv3jm1mq4vzx653sa7vlg48z20pfybk7pnhaa";
+    sha256 = "0v1y1kx09nzmk8w4v79jxhn15fmi3g7l9nmgkn7ldjl1d5yxkdl7";
   };
 
-  meta = {
+  meta = with stdenv.lib; {
     homepage = http://www.nasm.us/;
     description = "An 80x86 and x86-64 assembler designed for portability and modularity";
-    platforms = stdenv.lib.platforms.unix;
+    platforms = platforms.unix;
+    maintainers = with maintainers; [ pSub ];
   };
 }
