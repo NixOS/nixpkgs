e887fc72d32821818dac4a6b62201a5629d088ed
Vladimír Čunát <vcunat@gmail.com>
2014-11-20 17:58:10 +0100

    nasm: add a do-not-update comment

--- pkgs/development/compilers/nasm/default.nix
+++ pkgs/development/compilers/nasm/default.nix
@@ -2,8 +2,8 @@
 
 stdenv.mkDerivation rec {
   name = "nasm-${version}";
-  version = "2.11.05";
-  
+  version = "2.11.05"; # do not update until syslinux is fixed with that version
+
   src = fetchurl {
     url = "http://www.nasm.us/pub/nasm/releasebuilds/${version}/${name}.tar.bz2";
     sha256 = "1sgspnascc0asmwlv3jm1mq4vzx653sa7vlg48z20pfybk7pnhaa";
