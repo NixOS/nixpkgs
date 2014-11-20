e3388aea6e92bd41f862ed7479a80f33ba02d807
Vladimír Čunát <vcunat@gmail.com>
2014-11-20 11:31:25 +0100

    nasm: downgrade again until syslinux gets fixed
    Syslinux won't compile with the new version http://hydra.nixos.org/build/17236670
    We've already had one update+revert cycle before.
    CC @psub.

--- pkgs/development/compilers/nasm/default.nix
+++ pkgs/development/compilers/nasm/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "nasm-${version}";
-  version = "2.11.06";
+  version = "2.11.05";
   
   src = fetchurl {
     url = "http://www.nasm.us/pub/nasm/releasebuilds/${version}/${name}.tar.bz2";
-    sha256 = "0v1y1kx09nzmk8w4v79jxhn15fmi3g7l9nmgkn7ldjl1d5yxkdl7";
+    sha256 = "1sgspnascc0asmwlv3jm1mq4vzx653sa7vlg48z20pfybk7pnhaa";
   };
 
   meta = with stdenv.lib; {
