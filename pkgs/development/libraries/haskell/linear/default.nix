bd9df3f6b6cb9d42eb74224d5ad14b618cbb7a51
Peter Simons <simons@cryp.to>
2014-11-23 21:06:49 +0100

    haskell-linear: update to version 1.15.2

--- pkgs/development/libraries/haskell/linear/default.nix
+++ pkgs/development/libraries/haskell/linear/default.nix
@@ -8,8 +8,8 @@
 
 cabal.mkDerivation (self: {
   pname = "linear";
-  version = "1.13";
-  sha256 = "1gad6dvri7a21v8zx6m2m3ghcvb4zfrra3c4ghrc7ywccxvzmxpc";
+  version = "1.15.2";
+  sha256 = "1zikj932wbmjnq02msq5c2i3w2993lya0kvs326ajlpmq5q35gpj";
   buildDepends = [
     adjunctions binary distributive hashable lens reflection
     semigroupoids semigroups tagged transformers unorderedContainers
