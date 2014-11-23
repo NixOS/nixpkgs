bc2805a09f27ff293fc2364fe1df383ef39599ba
Peter Simons <simons@cryp.to>
2014-11-23 21:06:09 +0100

    haskell-diagrams-lib: update to version 1.2.0.5

--- pkgs/development/libraries/haskell/diagrams/lib.nix
+++ pkgs/development/libraries/haskell/diagrams/lib.nix
@@ -8,8 +8,8 @@
 
 cabal.mkDerivation (self: {
   pname = "diagrams-lib";
-  version = "1.2.0.4";
-  sha256 = "01gcbgxsnrcsysvpjhprym5ix10350x7l57f28nm0hbrfrsgidhz";
+  version = "1.2.0.5";
+  sha256 = "0km2j74riybmlhpb3jp4i3xilz4q2ngkifnsq6bsk8d0m3kqizd6";
   buildDepends = [
     active colour dataDefaultClass diagramsCore dualTree filepath
     fingertree hashable intervals JuicyPixels lens MemoTrie
