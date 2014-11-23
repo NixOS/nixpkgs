ceea18d4efb2a98a28d5313115e6782ff750fd0c
Peter Simons <simons@cryp.to>
2014-11-23 21:05:52 +0100

    haskell-cgrep: update to version 6.4.8

--- pkgs/development/libraries/haskell/cgrep/default.nix
+++ pkgs/development/libraries/haskell/cgrep/default.nix
@@ -6,8 +6,8 @@
 
 cabal.mkDerivation (self: {
   pname = "cgrep";
-  version = "6.4.7";
-  sha256 = "1937dvd69igx41sp1ljpghwqz0ki436pv0xmwz6bq1vny0axwmp7";
+  version = "6.4.8";
+  sha256 = "0xz2kzrvxxaa67x21dckv7l01s7jhmak7ka0rp8qygx6zjf836wy";
   isLibrary = false;
   isExecutable = true;
   buildDepends = [
