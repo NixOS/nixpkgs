b91ed78d79624d7f8cf38b1355a4f56e8aeb3969
Peter Simons <simons@cryp.to>
2014-11-23 21:06:34 +0100

    haskell-gtk3: update to version 0.13.1

--- pkgs/development/libraries/haskell/gtk3/default.nix
+++ pkgs/development/libraries/haskell/gtk3/default.nix
@@ -6,8 +6,8 @@
 
 cabal.mkDerivation (self: {
   pname = "gtk3";
-  version = "0.13.0.3";
-  sha256 = "12ywqxvmakcdmfj5y4jpn91rxa4xskd0w3hn2l679dk6lxyl15q7";
+  version = "0.13.1";
+  sha256 = "063my2y0fjsr2dgw066l6ck3mbcp1ahx5rqlzyb5xx1sbafrpl6a";
   isLibrary = true;
   isExecutable = true;
   buildDepends = [ cairo gio glib mtl pango text time transformers ];
