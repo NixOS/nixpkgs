0168c1ba194e35f618a311b6d07cc86461c9b77f
Bart Brouns <bart@magnetophon.nl>
2014-11-24 21:57:36 +0100

    faust: rename to faust-compiler (close #5113)
    @vcunat adjusted coding style slightly.

--- pkgs/applications/audio/faust-compiler/default.nix
+++ pkgs/applications/audio/faust-compiler/default.nix
@@ -3,25 +3,26 @@
 stdenv.mkDerivation rec {
 
   version = "0.9.67";
-  name = "faust-${version}";
+  name = "faust-compiler-${version}";
   src = fetchurl {
-    url = "http://downloads.sourceforge.net/project/faudiostream/${name}.zip";
+    url = "http://downloads.sourceforge.net/project/faudiostream/faust-${version}.zip";
     sha256 = "068vl9536zn0j4pknwfcchzi90rx5pk64wbcbd67z32w0csx8xm1";
   };
 
   buildInputs = [ unzip ];
 
-  patchPhase=''
+  patchPhase = ''
     sed -i '77,101d' Makefile
     sed -i 's#?= $(shell uname -s)#:= Linux#g'  architecture/osclib/oscpack/Makefile
     sed -e "s@\$FAUST_INSTALL /usr/local /usr /opt /opt/local@$out@g" -i tools/faust2appls/faustpath
-    '';
+  '';
 
-  postInstallPhase=''
+  postInstallPhase = ''
     rm -rf $out/include/
-    '';
-  
-  makeFlags="PREFIX=$(out)";
+  '';
+
+  makeFlags = "PREFIX=$(out)";
+  FPATH = "$out"; # <- where to search
 
   meta = with stdenv.lib; {
     description = "A functional programming language for realtime audio signal processing";
@@ -37,6 +38,8 @@ stdenv.mkDerivation rec {
       audio platforms and plugin formats (jack, alsa, ladspa, maxmsp,
       puredata, csound, supercollider, pure, vst, coreaudio) without
       any change to the FAUST code.
+      This package has just the compiler. Install faust for the full
+      set of faust2somethingElse tools.
     '';
     homepage = http://faust.grame.fr/;
     downloadPage = http://sourceforge.net/projects/faudiostream/files/;
