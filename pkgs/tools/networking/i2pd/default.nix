49e112c309ce68550e63d18a7d9ca56fedfb9abf
Edward Tjörnhammar <ed@cflags.cc>
2014-11-25 21:15:53 +0100

    i2pd: 0.3.0 -> 0.4.0

--- pkgs/tools/networking/i2pd/default.nix
+++ pkgs/tools/networking/i2pd/default.nix
@@ -3,11 +3,11 @@
 stdenv.mkDerivation rec {
 
   name = "i2pd-${version}";
-  version = "0.3.0";
+  version = "0.4.0";
 
   src = fetchurl {
-    url = "https://github.com/PrivacySolutions/i2pd/archive/v${version}-1stbinrelease.tar.gz";
-    sha256 = "015y5v6w1mmzmnylfdc1l3r4qbmax3nywyz0r0pd651xgvvvinrv";
+    url = "https://github.com/PrivacySolutions/i2pd/archive/${version}.tar.gz";
+    sha256 = "1gab0ams8bwkiwq0wjiclkm5ms5m5p3x06gzhi2dpdc6vbdkzmlp";
   };
 
   buildInputs = [ boost cryptopp ];
