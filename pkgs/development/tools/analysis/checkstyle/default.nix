a94da8c44c0e694450dbd6f30737cd254702251b
Pascal Wittmann <mail@pascal-wittmann.de>
2014-11-28 11:07:09 +0100

    checkstyle: update from 6.1 to 6.1.1

--- pkgs/development/tools/analysis/checkstyle/default.nix
+++ pkgs/development/tools/analysis/checkstyle/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  version = "6.1";
+  version = "6.1.1";
   name = "checkstyle-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/checkstyle/${version}/${name}-bin.tar.gz";
-    sha256 = "0g5ywnc64v2c4zlamm3l2x0l1z79199mcjz0xzv3j5wx7rsyasrc";
+    sha256 = "07qickjgayakzfq54nwb9rpxydjkymxw6lnhzxri36mxspq9pzda";
   };
 
   installPhase = ''
