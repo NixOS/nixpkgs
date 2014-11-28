a83eb0f2a141b0f7969b2cb905c4a4e8e5e15845
Ricardo M. Correia <rcorreia@wizy.org>
2014-11-28 21:00:58 +0100

    rustcMaster: Update from 0.12.0-pre-1336-g394269d -> 0.12.0-pre-1635-g29e928f

--- pkgs/development/compilers/rustc/head.nix
+++ pkgs/development/compilers/rustc/head.nix
@@ -18,19 +18,19 @@ assert stdenv.gcc.gcc != null;
 
 */
 
-with ((import ./common.nix) {inherit stdenv; version = "0.12.0-pre-1336-g394269d";});
+with ((import ./common.nix) {inherit stdenv; version = "0.12.0-pre-1635-g29e928f";});
 
 let snapshot = if stdenv.system == "i686-linux"
-      then "999ba4a0dfb70adca628138a7d5f491023621140"
+      then "c8342e762a1720be939ed7c6a39bdaa27892f66f"
       else if stdenv.system == "x86_64-linux"
-      then "55eaaed3bd6dd5a8d08e99aa4cd618d207f87d8c"
+      then "7a7fe6f5ed47b9cc66261f880e166c7c8738b73e"
       else if stdenv.system == "i686-darwin"
-      then "0581dff21a238343602ec0202a551bac93d21300"
+      then "63e8644512bd5665c14389a83d5af564c7c0b103"
       else if stdenv.system == "x86_64-darwin"
-      then "aad290cf3f8ac4aa0661984a9799c78161ea5a72"
+      then "7933ae0e974d1b897806138b7052cb2b4514585f"
       else abort "no-snapshot for platform ${stdenv.system}";
-    snapshotDate = "2014-11-18";
-    snapshotRev = "9c96a79";
+    snapshotDate = "2014-11-21";
+    snapshotRev = "c9f6d69";
     snapshotName = "rust-stage0-${snapshotDate}-${snapshotRev}-${platform}-${snapshot}.tar.bz2";
 
 in stdenv.mkDerivation {
@@ -40,8 +40,8 @@ in stdenv.mkDerivation {
 
   src = fetchgit {
     url = https://github.com/rust-lang/rust;
-    rev = "394269d16e3752a23ffa273e68f8aaefd2a510c4";
-    sha256 = "0zkz5f4gba4glhrf9f7v42qxk638q9ahgf6m0bjh8b7c3x52cv71";
+    rev = "29e928f2ba3501d37660314f6186d0e2ac18b9db";
+    sha256 = "1ndg920hnb8r9bblqvmqfflcrppzj68vzbhxf2zghmnx2z7svhxn";
   };
 
   # We need rust to build rust. If we don't provide it, configure will try to download it.
