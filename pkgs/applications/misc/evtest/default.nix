e6f59f2a228f541d167d5c2933d6c1f4a5237922
Bjørn Forsman <bjorn.forsman@gmail.com>
2014-12-02 22:59:49 +0100

    evtest: update 1.31 -> 1.32

--- pkgs/applications/misc/evtest/default.nix
+++ pkgs/applications/misc/evtest/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchgit, autoconf, automake, pkgconfig, libxml2 }:
 
 stdenv.mkDerivation rec {
-  name = "evtest-1.31";
+  name = "evtest-1.32";
 
   preConfigure = "autoreconf -iv";
 
@@ -9,8 +9,8 @@ stdenv.mkDerivation rec {
 
   src = fetchgit {
     url = "git://anongit.freedesktop.org/evtest";
-    rev = "871371806017301373b8b0e5b7e8f168ce1ea13f";
-    sha256 = "1hxldlldlrb9lnnybn839a97fpqd1cixbmci2wzgr0rzhjbwhcgp";
+    rev = "refs/tags/evtest-1.32";
+    sha256 = "150lb7d2gnkcqgfw1hcnb8lcvdb52fpig9j9qxjizp6irhlw2a31";
   };
 
   meta = with stdenv.lib; {
