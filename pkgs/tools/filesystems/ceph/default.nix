d2a2c776433d523704855acb862c70d28c5a0855
William A. Kennington III <william@wkennington.com>
2014-12-04 15:17:32 -0800

    ceph: 0.88 -> 0.89

--- pkgs/tools/filesystems/ceph/default.nix
+++ pkgs/tools/filesystems/ceph/default.nix
@@ -14,12 +14,12 @@ let
 in
 stdenv.mkDerivation rec {
   name="ceph-${version}";
-  version="0.88";
+  version="0.89";
 
   src = fetchgit {
     url = "git://github.com/ceph/ceph.git";
     rev = "refs/tags/v${version}";
-    sha256 = "1wf77mlx94h2mn75qqgb37xbdagl7zh0b579qh7gc1z4wcb8b8h0";
+    sha256 = "1ky1j8qb2nz3qzlhdcb6ppdjx7wb3icq7n0l5mm9x3q2ijbwvhlz";
   };
 
   patches = [
