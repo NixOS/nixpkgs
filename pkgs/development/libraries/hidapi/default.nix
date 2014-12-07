28d98c701bb25e270b670106c44800606deda87d
Bjørn Forsman <bjorn.forsman@gmail.com>
2014-12-07 16:15:52 +0100

    hidapi: add a comment on the license
    hidapi allows the user to chose between GPLv3, BSD or HIDAPI license
    (more liberal).

--- pkgs/development/libraries/hidapi/default.nix
+++ pkgs/development/libraries/hidapi/default.nix
@@ -15,6 +15,7 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     homepage = https://github.com/signal11/hidapi;
     description = "Library for communicating with USB and Bluetooth HID devices";
+    # Actually, you can chose between GPLv3, BSD or HIDAPI license (more liberal)
     license = licenses.bsd3;
     platforms = platforms.unix;
     maintainers = with maintainers; [ wkennington ];
