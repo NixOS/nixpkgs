a327bb6f1715893be0580cdcc42a80d2619c116d
Domen Kožar <domen@dev.si>
2014-12-09 11:12:24 +0100

    xfce4.xfce4-power-manager: 1.3.0 -> 1.3.2

--- pkgs/desktops/xfce/core/xfce4-power-manager.nix
+++ pkgs/desktops/xfce/core/xfce4-power-manager.nix
@@ -4,11 +4,11 @@
 stdenv.mkDerivation rec {
   p_name  = "xfce4-power-manager";
   ver_maj = "1.3";
-  ver_min = "0";
+  ver_min = "2";
 
   src = fetchurl {
     url = "mirror://xfce/src/xfce/${p_name}/${ver_maj}/${name}.tar.bz2";
-    sha256 = "1q0mnj9hjqw4zpk7wqnfayy6zzbjbhzd1mcplfjik2h753gaxgh5";
+    sha256 = "0lv29ycws185qr89xn01vcddkvpddk7q6hni0s6d0nqvjavycg0j";
   };
 
   name = "${p_name}-${ver_maj}.${ver_min}";
