6469098f1cc56f766eceecc12f04da2d885b7973
William A. Kennington III <william@wkennington.com>
2014-12-08 23:21:14 -0800

    kernel: 3.17.4 -> 3.17.6

--- pkgs/os-specific/linux/kernel/linux-3.17.nix
+++ pkgs/os-specific/linux/kernel/linux-3.17.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.17.4";
+  version = "3.17.6";
   # Remember to update grsecurity!
   extraMeta.branch = "3.17";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0ayh9y58iv38h76jl2r77856af2cazzkwcdhjqmccibajjf42maa";
+    sha256 = "0z4xai2m8s6zd4mkxsa8dw2ny378y6p9l835z4xj8xwgl30hjaa1";
   };
 
   features.iwlwifi = true;
