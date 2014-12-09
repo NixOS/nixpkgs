1f2d6a022e3641461dbab5d1280b78e04ac6ff63
William A. Kennington III <william@wkennington.com>
2014-12-08 23:21:07 -0800

    kernel: 3.12.33 -> 3.12.34
    (cherry picked from commit 845f647b8651b8f2469723ca2ff6db7ced2ca38a)

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.12.33";
+  version = "3.12.34";
   extraMeta.branch = "3.12";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "18vrlrda8xa6j7771x8bwl7vrhvi2kmrp91nlwhkqr4siwkh0jxp";
+    sha256 = "037dn7qraqpck36pla4qi60k58kh4h52wfgfnb3mhqy38x2fnrzh";
   };
 
   features.iwlwifi = true;
