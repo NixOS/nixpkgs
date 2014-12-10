8ca78f0376c8888886a959cc5cbb6f895b9956f2
Vladimír Čunát <vcunat@gmail.com>
2014-12-10 13:36:57 +0100

    luminance-hdr: fix build by Debian patch

--- pkgs/applications/graphics/luminance-hdr/default.nix
+++ pkgs/applications/graphics/luminance-hdr/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, cmake, fetchurl, pkgconfig, qt5, boost, exiv2, fftwFloat, gsl
+{ stdenv, cmake, fetchurl, fetchpatch, pkgconfig, qt5, boost, exiv2, fftwFloat, gsl
 , ilmbase, lcms2, libraw, libtiff, openexr
 }:
 
@@ -9,6 +9,13 @@ stdenv.mkDerivation rec {
     url = "mirror://sourceforge/qtpfsgui/${name}.tar.bz2";
     sha256 = "00fldbcizrx8jcnjgq74n3zmbm27dxzl96fxa7q49689mfnlw08l";
   };
+
+  patches = [(fetchpatch {
+    name = "fix-qt53-build.diff";
+    url = "http://anonscm.debian.org/cgit/pkg-phototools/luminance-hdr.git/"
+      + "plain/debian/patches/51_qt5_printsupport.diff?id=00c869a860062dac181303f2c03a3513c0e210bc";
+    sha256 = "0nzvfxd3ybxx61rj6vxcaaxfrsxrl9af3h8jj7pr3rncisnl9gkl";
+  })];
 
   NIX_CFLAGS_COMPILE = "-I${ilmbase}/include/OpenEXR";
 
