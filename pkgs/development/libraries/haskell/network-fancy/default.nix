cbe049de9429e7e29ae206ea49299546ba2f60e3
Peter Simons <simons@cryp.to>
2014-12-12 15:43:38 +0100

    haskell-network-fancy: update to version 0.2.2

--- pkgs/development/libraries/haskell/network-fancy/default.nix
+++ pkgs/development/libraries/haskell/network-fancy/default.nix
@@ -4,9 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "network-fancy";
-  version = "0.2.1";
-  sha256 = "1n8i7vyg1csdxxq7nip84ydkx3c8gykmqxg27ir0mmgj3p1jb42c";
-  doCheck = false;
+  version = "0.2.2";
+  sha256 = "12q2ifjwg4ss5l2pv42nfhsawncbwm6y4dwrspadkjv9b8idl91j";
   meta = {
     homepage = "http://github.com/taruti/network-fancy";
     description = "Networking support with a cleaner API";
