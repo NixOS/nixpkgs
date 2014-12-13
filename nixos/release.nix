69858d7743293d8f94b3d3160c9c3401722e4c10
aszlig <aszlig@redmoonstudios.org>
2014-12-13 11:15:48 +0100

    nixos: Add VM test for VirtualBox.
    Currently it pretty much tests starting up virtual machines and just
    shutting down afterwards, but for both VBoxManage and the VirtualBox
    GUI.
    
    This helps catching errors in hardened mode, however we still need to
    test whether networking works the way intended (and I fear that this is
    broken at the moment).
    
    The VirtualBox VM is _not_ using hardware virtualization support (thus
    we use system = "i686-linux", because x86_64 has no emulation support),
    because we're already within a qemu VM, which means it's going to be
    slow as hell (that's why I've written own subs just for testing
    startup/shutdown/whatnot with respective timeouts).
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- nixos/release.nix
+++ nixos/release.nix
@@ -306,6 +306,7 @@ in rec {
   tests.simple = callTest tests/simple.nix {};
   tests.tomcat = callTest tests/tomcat.nix {};
   tests.udisks2 = callTest tests/udisks2.nix {};
+  tests.virtualbox = callTest tests/virtualbox.nix {};
   tests.xfce = callTest tests/xfce.nix {};
 
 
