ca32d20d6e9b4ebe46d46a1b2e0c56ebf58962db
Jaka Hudoklin <jakahudoklin@gmail.com>
2014-12-14 13:05:21 +0100

    nixos/gitlab: enable tests in release

--- nixos/release.nix
+++ nixos/release.nix
@@ -251,6 +251,7 @@ in rec {
   tests.firefox = callTest tests/firefox.nix {};
   tests.firewall = callTest tests/firewall.nix {};
   tests.fleet = scrubDrv (import tests/fleet.nix { system = "x86_64-linux"; });
+  tests.gitlab = callTest tests/gitlab.nix {};
   tests.gnome3 = callTest tests/gnome3.nix {};
   tests.installer.grub1 = forAllSystems (system: scrubDrv (import tests/installer.nix { inherit system; }).grub1.test);
   tests.installer.lvm = forAllSystems (system: scrubDrv (import tests/installer.nix { inherit system; }).lvm.test);
