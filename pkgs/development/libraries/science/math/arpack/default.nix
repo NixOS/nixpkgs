84553e0aa0920c601fdae6fd419a48846d761da8
Thomas Tuegel <ttuegel@gmail.com>
2014-12-15 11:28:57 -0600

    arpack: build against atlasWithLapack

--- pkgs/development/libraries/science/math/arpack/default.nix
+++ pkgs/development/libraries/science/math/arpack/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, gfortran, openblas }:
+{ stdenv, fetchurl, gfortran, atlasWithLapack }:
 
 let
   version = "3.2.0";
@@ -10,23 +10,17 @@ stdenv.mkDerivation {
     sha256 = "1fwch6vipms1ispzg2djvbzv5wag36f1dmmr3xs3mbp6imfyhvff";
   };
 
-  buildInputs = [ gfortran ];
-  propagatedBuildInputs = [ openblas ];
-
-  preConfigure = ''
-    substituteInPlace arpack.pc.in \
-      --replace "@BLAS_LIBS@" "-L${openblas}/lib @BLAS_LIBS@"
-  '';
+  buildInputs = [ gfortran atlasWithLapack ];
 
   # Auto-detection fails because gfortran brings in BLAS by default
-  configureFlags="--with-blas=-lopenblas --with-lapack=-lopenblas";
+  configureFlags="--with-blas=-latlas --with-lapack=-latlas";
 
   meta = {
     homepage = "http://forge.scilab.org/index.php/p/arpack-ng/";
-    description = "A collection of Fortran77 subroutines to solve large scale eigenvalue problems";
-    # Looks like OpenBLAS is not that easy to build
-    # there is a sgemm_itcopy undefined reference on 32-bit, for example
-    platforms = ["x86_64-linux"];
+    description = ''
+      A collection of Fortran77 subroutines to solve large scale eigenvalue
+      problems.
+    '';
     license = stdenv.lib.licenses.bsd3;
     maintainers = [ stdenv.lib.maintainers.ttuegel ];
   };
