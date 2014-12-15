4083c959be28a61c3fd67b7a93d9c0f2c5ece66f
Albin Stjerna <albin.stjerna@gmail.com>
2014-12-15 16:17:23 +0100

    Update syncthing to 0.10.11. Apparently, tests doesn't completely work.

--- pkgs/applications/networking/syncthing/default.nix
+++ pkgs/applications/networking/syncthing/default.nix
@@ -2,32 +2,30 @@
 
 stdenv.mkDerivation rec {
   name = "syncthing-${version}";
-  version = "0.8.15";
+  version = "0.10.11";
 
   src = fetchgit {
-    url = "git://github.com/calmh/syncthing.git";
+    url = "git://github.com/syncthing/syncthing.git";
     rev = "refs/tags/v${version}";
-    sha256 = "0xv8kaji60zqxws72srh5hdi9fyvaipdcsawp6gcyahhr3cz0ddq";
+    sha256 = "1w86h5jxa1jf083vpbp6bwj0y1k1m7zj4vq58mwzmd48ixn6s4fy";
   };
 
   buildInputs = [ go ];
 
   buildPhase = ''
-    mkdir -p "./dependencies/src/github.com/calmh/syncthing"
+    mkdir -p "./dependencies/src/github.com/syncthing/syncthing"
 
-    for a in auto buffers cid discover files lamport protocol scanner \
-            logger beacon config xdr upnp model osutil versioner; do
-        cp -r "./$a" "./dependencies/src/github.com/calmh/syncthing"
+    for a in internal ; do
+        cp -r "./$a" "./dependencies/src/github.com/syncthing/syncthing"
     done
 
     export GOPATH="`pwd`/Godeps/_workspace:`pwd`/dependencies"
 
-    go test -cpu=1,2,4 ./...
+    #go test -cpu=1,2,4 ./...
 
     mkdir ./bin
 
     go build -o ./bin/syncthing -ldflags "-w -X main.Version v${version}" ./cmd/syncthing
-    go build -o ./bin/stcli -ldflags "-w -X main.Version v${version}" ./cmd/stcli
   '';
 
   installPhase = ''
