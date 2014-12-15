ccce6171e943c57a8a2265759edec21844277f27
Thomas Tuegel <ttuegel@gmail.com>
2014-12-15 08:57:30 -0600

    fontconfig-ultimate: do not replace terminus or proggy fonts

--- pkgs/development/libraries/fontconfig-ultimate/confd.nix
+++ pkgs/development/libraries/fontconfig-ultimate/confd.nix
@@ -16,6 +16,8 @@ stdenv.mkDerivation {
     rm $out/etc/fonts/conf.d/10-base-rendering.conf
 
     # Options controlled by NixOS module
+    rm $out/etc/fonts/conf.d/35-repl-custom.conf
+    rm $out/etc/fonts/conf.d/38-repl-*.conf
     rm $out/etc/fonts/conf.d/82-*.conf
     rm $out/etc/fonts/conf.d/83-*.conf
 
