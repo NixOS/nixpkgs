0aa452e1797c811adb3038f086908c7c03079a0b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-12-16 16:20:08 +0100

    cups-filters: Fix banners
    It was looking in cups' prefix for the PDF templates. So for instance
    printing a CUPS test page wouldn't work.

--- pkgs/misc/cups/filters.nix
+++ pkgs/misc/cups/filters.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
     sha256 = "1bq48nnrarlbf6qc93bz1n5wlh6j420gppbck3r45sinwhz5wa7m";
   };
 
-  buildInputs = [ 
+  buildInputs = [
     pkgconfig cups poppler fontconfig libjpeg libpng perl
     ijs qpdf dbus
   ];
@@ -18,8 +18,18 @@ stdenv.mkDerivation rec {
   preBuild = ''
     substituteInPlace Makefile --replace "/etc/rc.d" "$out/etc/rc.d"
   '';
+
   configureFlags = "--with-pdftops=pdftops --enable-imagefilters";
+
   makeFlags = "CUPS_SERVERBIN=$(out)/lib/cups CUPS_DATADIR=$(out)/share/cups CUPS_SERVERROOT=$(out)/etc/cups";
+
+  # Ensure that bannertopdf can find the PDF templates in $out. (By
+  # default, it assumes that cups and cups-filters are installed in
+  # the same prefix.)
+  postConfigure =
+    ''
+      substituteInPlace config.h --replace ${cups}/share/cups/data $out/share/cups/data
+    '';
 
   meta = {
     homepage = http://www.linuxfoundation.org/collaborate/workgroups/openprinting/cups-filters;
