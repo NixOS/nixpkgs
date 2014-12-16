b671a46f92f12cd606d8cb2a66a66bd43c97f17a
William A. Kennington III <william@wkennington.com>
2014-12-16 15:24:28 -0800

    gnupg: 2.1.0 -> 2.1.1

--- pkgs/tools/security/gnupg/21.nix
+++ pkgs/tools/security/gnupg/21.nix
@@ -5,11 +5,11 @@
 
 with stdenv.lib;
 stdenv.mkDerivation rec {
-  name = "gnupg-2.1.0";
+  name = "gnupg-2.1.1";
 
   src = fetchurl {
     url = "mirror://gnupg/gnupg/${name}.tar.bz2";
-    sha256 = "12q5shx6ldqad3rky154nv8f2cy57sxy9idivz93ggqm1bsw7a0n";
+    sha256 = "12q5shx6ldqad3rky154nv8f2cy57sxy9idivz93ggqm1bsw7a0a";
   };
 
   patches = [ ./socket-activate.patch ];
