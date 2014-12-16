9b581d16dbfc1257acb697890d88d29a7a708a7e
Vladimír Čunát <vcunat@gmail.com>
2014-12-16 21:54:03 +0100

    freetype: feature+security update
    http://savannah.nongnu.org/bugs/?43661
    New version of patches from bohoomil included.
    
    Untested on NixOS so far.

--- pkgs/development/libraries/freetype/default.nix
+++ pkgs/development/libraries/freetype/default.nix
@@ -7,30 +7,28 @@
 }:
 
 let
-  version = "2.5.3";
+  version = "2.5.4";
 
   fetch_bohoomil = name: sha256: fetchpatch {
-    url = https://raw.githubusercontent.com/bohoomil/fontconfig-ultimate/8a155db28f264520596cc3e76eb44824bdb30f8e/01_freetype2-iu/ + name;
+    url = https://raw.githubusercontent.com/bohoomil/fontconfig-ultimate/e4c99bcf5ac9595e2c64393c0661377685c0ad24/01_freetype2-iu/ + name;
     inherit sha256;
   };
 in
-with { inherit (stdenv.lib) optional optionalString; };
+with { inherit (stdenv.lib) optional optionals optionalString; };
 stdenv.mkDerivation rec {
   name = "freetype-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/freetype/${name}.tar.bz2";
-    sha256 = "0pppcn73b5pwd7zdi9yfx16f5i93y18q7q4jmlkwmwrfsllqp160";
+    sha256 = "1fxsbk4lp6ymifldzrb86g3x6mz771jmrzphkz92mcrkddk2qkiv";
   };
 
-  patches = [ ./enable-validation.patch ] # from Gentoo
-    ++ [
-      (fetch_bohoomil "freetype-2.5.3-pkgconfig.patch" "1dpfdh8kmka3gzv14glz7l79i545zizah6wma937574v5z2iy3nn")
-      (fetch_bohoomil "fix_segfault_with_harfbuzz.diff" "1nx36inqrw717b86cla2miprdb3hii4vndw95k0jbbhfmax9k6fy")
-    ]
-    ++ optional useEncumberedCode
-      (fetch_bohoomil "infinality-2.5.3.patch" "0mxiybcb4wwbicrjiinh1b95rv543bh05sdqk1v0ipr3fxfrb47q")
-    ;
+  patches = [ ./enable-validation.patch ] # from Gentoo, bohoomil has the same patch as well
+    ++ optionals useEncumberedCode [
+      (fetch_bohoomil "02-ftsmooth-2.5.4.patch" "11w4wb7gwgpijc788mpkxj92d7rfdwrdv7jzrpxwv5w5cgpx9iw9")
+      (fetch_bohoomil "03-upstream-2014.12.07.patch" "0gq7y63mg3gc5z69nfkv2kl7xad0bjzsvnl6j1j9q79jjbvaqdq0")
+      (fetch_bohoomil "04-infinality-2.5.4-2014.12.07.patch" "1gph7z9s2221gy5dxn01v3lga0m9yib8yqsaqj5km74bqx1vlalh")
+    ];
 
   propagatedBuildInputs = [ zlib bzip2 libpng ]; # needed when linking against freetype
   # dependence on harfbuzz is looser than the reverse dependence
