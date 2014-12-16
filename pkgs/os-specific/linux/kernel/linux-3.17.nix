66332cdee10081a6894c887257a2bcf042f53465
William A. Kennington III <william@wkennington.com>
2014-12-16 14:15:47 -0800

    Revert "kernel: 3.17.6 -> 3.17.7"
    This reverts commit d3a61d88aaad1e902e01b6f85c139f9c1e892c52.

--- pkgs/os-specific/linux/kernel/linux-3.17.nix
+++ pkgs/os-specific/linux/kernel/linux-3.17.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.17.7";
+  version = "3.17.6";
   # Remember to update grsecurity!
   extraMeta.branch = "3.17";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "074f56sz22wqnn21gamwf63llhsqc2hgb7dzkbkd5gqjx6pfdj53";
+    sha256 = "0z4xai2m8s6zd4mkxsa8dw2ny378y6p9l835z4xj8xwgl30hjaa1";
   };
 
   features.iwlwifi = true;
