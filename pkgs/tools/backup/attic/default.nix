a8af7f16ce997ebee115d44c16847ab1ea0688a1
Bjørn Forsman <bjorn.forsman@gmail.com>
2014-12-18 08:14:52 +0100

    attic: expression cleanup
    Clean up some whitespace errors and add namePrefix = "" to make package
    name like attribute name (no pythonX.Y- prefix).

--- pkgs/tools/backup/attic/default.nix
+++ pkgs/tools/backup/attic/default.nix
@@ -1,12 +1,13 @@
-{stdenv, fetchurl, python3Packages, openssl, acl }:
+{ stdenv, fetchurl, python3Packages, openssl, acl }:
 
 python3Packages.buildPythonPackage rec {
   name = "attic-0.13";
+  namePrefix = "";
 
   src = fetchurl {
-      url = "https://github.com/jborg/attic/archive/0.13.tar.gz";
-      sha256 = "da1c4c0759b541e72f6928341c863b406448351769113165d86d8393a5db98a3";
-      };
+    url = "https://github.com/jborg/attic/archive/0.13.tar.gz";
+    sha256 = "da1c4c0759b541e72f6928341c863b406448351769113165d86d8393a5db98a3";
+  };
 
   buildInputs = with python3Packages;
     [ cython msgpack openssl acl ];
@@ -18,6 +19,6 @@ python3Packages.buildPythonPackage rec {
   meta = with stdenv.lib; {
     description = "A deduplication backup program";
     homepage = "https://attic-backup.org";
-    license = stdenv.lib.licenses.bsd3;
- };
+    license = licenses.bsd3;
+  };
 }
