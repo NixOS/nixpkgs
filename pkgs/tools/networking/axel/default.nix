197ea06a73da85b1e0c9f7e40a0caa711c2de83e
Pascal Wittmann <mail@pascal-wittmann.de>
2014-12-19 22:57:16 +0100

    axel: add meta-information

--- pkgs/tools/networking/axel/default.nix
+++ pkgs/tools/networking/axel/default.nix
@@ -1,16 +1,18 @@
 {stdenv, fetchurl}:
 
-stdenv.mkDerivation {
-  name = "axel-2.4";
+stdenv.mkDerivation rec {
+  name = "axel-${version}";
+  version = "2.4";
+
   src = fetchurl {
-    urls = [
-      #https://alioth.debian.org/frs/download.php/3016/axel-2.4.tar.bz2
-      mirror://debian/pool/main/a/axel/axel_2.4.orig.tar.gz
-    ];
+    url = "mirror://debian/pool/main/a/axel/axel_${version}.orig.tar.gz";
     sha256 = "0dl0r9byd2ps90cq2nj1y7ib6gnkb5y9f3a3fmhcnjrm9smmg6im";
   };
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Console downloading program with some features for parallel connections for faster downloading";
+    homepage = http://axel.alioth.debian.org/;
+    maintainers = with maintainers; [ pSub ];
+    platforms = platforms.linux;
   };
 }
