5d30449d3da271a1b70c399e594d0262eb8e5b5a
Kurt Dietrich <kurto@mac.com>
2014-12-22 01:49:34 -0500

    Added game-music-emu, a decoder plugin for mpd.

---
+++ pkgs/applications/audio/game-music-emu/default.nix
@@ -0,0 +1,20 @@
+{ stdenv, fetchurl, cmake }:
+
+stdenv.mkDerivation rec {
+  version = "0.6.0";
+  name = "game-music-emu-${version}";
+
+  src = fetchurl {
+    url = "https://game-music-emu.googlecode.com/files/${name}.tar.bz2";
+    sha256 = "11s9l938nxbrk7qb2k1ppfgizcz00cakbxgv0gajc6hyqv882vjh";
+  };
+
+  buildInputs = [ cmake ];
+
+  meta = with stdenv.lib; {
+    homepage = https://code.google.com/p/game-music-emu/;
+    description = "Game_Music_Emu is a collection of video game music file emulators.";
+    platforms = platforms.all;
+    maintainers = [ ];
+  };
+}
