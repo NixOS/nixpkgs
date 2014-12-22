a2c14143e91bb76c69652b9a0bda15aca2b7fc62
Shea Levy <shea@shealevy.com>
2014-12-22 15:13:08 -0500

    Add newrelic-sysmond
    (cherry picked from commit 4e558732e7714ad69a6d0ad2b052f2efdde76376)

---
+++ pkgs/servers/monitoring/newrelic-sysmond/default.nix
@@ -0,0 +1,30 @@
+{ stdenv, fetchurl }:
+
+assert stdenv.system == "x86_64-linux";
+
+stdenv.mkDerivation rec {
+  name = "newrelic-sysmond-1.5.1.93";
+
+  src = fetchurl {
+    url = "http://download.newrelic.com/server_monitor/release/newrelic-sysmond-1.5.1.93-linux.tar.gz";
+
+    sha256 = "1bfwyczcf7pvji8lx566jxgy8dhyf1gmqmi64lj10673a86axnwz";
+  };
+
+  installPhase = ''
+    mkdir -p $out/bin
+    install -v -m755 daemon/nrsysmond.x64 $out/bin/nrsysmond
+    patchelf --set-interpreter "$(cat $NIX_GCC/nix-support/dynamic-linker)" \
+      $out/bin/nrsysmond
+  '';
+
+  meta = {
+    homepage = http://newrelic.com/;
+
+    description = "System-wide monitoring for newrelic";
+
+    license = stdenv.lib.licenses.unfree;
+
+    maintainers = [ stdenv.lib.maintainers.shlevy ];
+  };
+}
