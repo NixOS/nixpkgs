d940c7b4a881f5f48e5a58eaf375ee4a7562479d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-12-23 16:07:02 +0100

    pstack: Remove
    It doesn't work anymore. And anyway, this probably shouldn't have been
    a package in the first place (it's just some gdb options).
    
    (cherry picked from commit 4a63787b78cd0cf20d343e4bbc31a3057d7876c8)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4657,8 +4657,6 @@ let
 
   premake = premake4;
 
-  pstack = callPackage ../development/tools/misc/gdb/pstack.nix { };
-
   racerRust = callPackage ../development/tools/rust/racer { rust = rustcMaster; };
 
   radare = callPackage ../development/tools/analysis/radare {
