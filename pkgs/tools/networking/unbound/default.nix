676c462faab3277ac7dcb1c3934011e25727fe04
Matthew William Cox <sui@mattcox.ca>
2014-12-23 00:49:51 -0500

    nixpkgs: unbound 1.4.22 -> 1.5.1

--- pkgs/tools/networking/unbound/default.nix
+++ pkgs/tools/networking/unbound/default.nix
@@ -1,17 +1,22 @@
 { stdenv, fetchurl, openssl, expat, libevent }:
 
 stdenv.mkDerivation rec {
-  name = "unbound-1.4.22";
+  name = "unbound-${version}";
+  version = "1.5.1";
 
   src = fetchurl {
     url = "http://unbound.net/downloads/${name}.tar.gz";
-    sha256 = "17yjly9c00zfgbzvllqzjh668a4yk6vrinf47yrcs3hrna0m1bqw";
+    sha256 = "1v00k4b6m9wk0533s2jpg4rv9lhplh7zdp6vx2yyrmrbzc4jgy0g";
   };
- 
+
   buildInputs = [openssl expat libevent];
 
-  configureFlags = [ "--with-ssl=${openssl}" "--with-libexpat=${expat}"
-    "--localstatedir=/var" ];
+  configureFlags = [
+    "--with-ssl=${openssl}"
+    "--with-libexpat=${expat}"
+    "--with-libevent=${libevent}"
+    "--localstatedir=/var"
+  ];
 
   meta = {
     description = "Validating, recursive, and caching DNS resolver";
