10ddeea0c658506f6ac91656e2e0fb73fb4c1b77
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-12-24 01:06:49 -0600

    style changes + license + platform for rawdog

--- pkgs/applications/networking/feedreaders/rawdog/default.nix
+++ pkgs/applications/networking/feedreaders/rawdog/default.nix
@@ -1,16 +1,19 @@
 { stdenv, fetchurl, pythonPackages }:
 
 pythonPackages.buildPythonPackage rec {
-  name = "rawdog";
+  name = "rawdog-2.20";
+
   src = fetchurl {
-    url = "http://offog.org/files/rawdog-2.20.tar.gz";
+    url = "http://offog.org/files/${name}.tar.gz";
     sha256 = "0a63b26cc111b0deca441f498177b49be0330760c5c0e24584cdb9ba1e7fd5a6";
   };
 
-   propagatedBuildInputs = with pythonPackages; [ feedparser ];
+  propagatedBuildInputs = with pythonPackages; [ feedparser ];
   
   meta = {
     homepage = "http://offog.org/code/rawdog/";
-    description = "rawdog is an RSS Aggregator Without Delusions Of Grandeur.";
+    description = "an RSS Aggregator Without Delusions Of Grandeur.";
+    license = stdenv.lib.licenses.gpl2;
+    platform = stdenv.lib.platforms.unix;
   };
 }
