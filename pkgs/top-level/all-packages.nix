0771071cf98cd9c98965d905c1b5d66e2c75018a
Eduard Bachmakov <e.bachmakov@gmail.com>
2014-12-27 00:18:18 +0100

    Add photoqt-1.1.0.1
    Also, add myself to maintainers.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10548,6 +10548,8 @@ let
   pdftk = callPackage ../tools/typesetting/pdftk { };
   pdfgrep  = callPackage ../tools/typesetting/pdfgrep { };
 
+  photoqt = callPackage ../applications/graphics/photoqt { };
+
   pianobar = callPackage ../applications/audio/pianobar { };
 
   pianobooster = callPackage ../applications/audio/pianobooster { };
