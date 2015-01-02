a909b4f58ae6dc6fade1659d012e30e604d9e054
William A. Kennington III <william@wkennington.com>
2015-01-02 10:42:11 -0800

    tdb: 1.3.3 -> 1.3.4

--- pkgs/development/libraries/tdb/default.nix
+++ pkgs/development/libraries/tdb/default.nix
@@ -5,11 +5,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "tdb-1.3.3";
+  name = "tdb-1.3.4";
 
   src = fetchurl {
     url = "http://samba.org/ftp/tdb/${name}.tar.gz";
-    sha256 = "03jg7gvyi5ljj93zwvqw1d1p7a9gqy0v4rxwn7ypw4ipxyiavpjl";
+    sha256 = "0a8pa4ar7dxkbsgv1447av2rn35a1m6l1v2s9hgz3ccwni9wv1gm";
   };
 
   buildInputs = [
