f5e874903e0c15137fee255420756ecbef9b022c
Cillian de Róiste <cillian.deroiste@gmail.com>
2015-01-02 23:47:59 +0100

    yoshimi: update from 1.3.0 to 1.3.1

--- pkgs/applications/audio/yoshimi/default.nix
+++ pkgs/applications/audio/yoshimi/default.nix
@@ -6,11 +6,11 @@ assert stdenv ? glibc;
 
 stdenv.mkDerivation  rec {
   name = "yoshimi-${version}";
-  version = "1.3.0";
+  version = "1.3.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/yoshimi/${name}.tar.bz2";
-    sha256 = "1zxblcl6ffwwzxh3d17hw1mp398b73wk5bsagdmx5gph038gdh7y";
+    sha256 = "1nj71xmjgr0pdqwv6h9kigdvljh8hggy6az9rnd7f64r13nmrphl";
   };
 
   buildInputs = [
