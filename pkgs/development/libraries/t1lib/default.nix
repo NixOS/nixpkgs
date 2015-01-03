a2b7bc284195fcb396dd12ed9c9fb3e50b73f4f0
Franz Pletz <fpletz@fnordicwalking.de>
2015-01-03 18:40:37 +0100

    t1lib: Use metalab mirrors (close #5552)
    (cherry picked from commit 261e31c186870f2e47210124f621b7568bc99f93)

--- pkgs/development/libraries/t1lib/default.nix
+++ pkgs/development/libraries/t1lib/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "t1lib-5.1.2";
 
   src = fetchurl {
-    url = "ftp://ftp.nluug.nl/pub/metalab/libs/graphics/t1lib-5.1.2.tar.gz";
+    url = "mirror://metalab/libs/graphics/t1lib-5.1.2.tar.gz";
     sha256 = "0nbvjpnmcznib1nlgg8xckrmsw3haa154byds2h90y2g0nsjh4w2";
   };
 
