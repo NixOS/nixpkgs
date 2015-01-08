92ca6149347b563082d08bee5860927d22cb016c
Marcus Crestani <crestani@informatik.uni-tuebingen.de>
2015-01-08 21:46:05 +0100

    i3: Use xcb-util-cursor-HEAD on OS X.

---
+++ pkgs/development/libraries/xcb-util-cursor/HEAD.nix
@@ -0,0 +1,39 @@
+{ stdenv, fetchgit, bashInteractive, autoconf, automake, libtool, pkgconfig
+, git, xlibs, gnum4, libxcb, gperf }:
+
+stdenv.mkDerivation rec {
+  name = "xcb-util-cursor-HEAD";
+
+  src = fetchgit {
+    url    = http://anongit.freedesktop.org/git/xcb/util-cursor.git;
+    rev    = "f03cc278c6cce0cf721adf9c3764d3c5fba63392";
+    sha256 = "1ljvq1gdc1lc33dwn4pzwppws2zgyqx51y3sd3c8gb7vcg5f27i5";
+  };
+
+  meta = with stdenv.lib; {
+    description = "XCB cursor library (libxcursor port)";
+    homepage    = http://cgit.freedesktop.org/xcb/util-cursor;
+    license     = licenses.mit;
+    maintainer  = with maintainers; [ lovek323 ];
+    platforms   = platforms.linux ++ platforms.darwin;
+  };
+
+  buildInputs = [
+    autoconf
+    automake
+    gnum4
+    gperf
+    libtool
+    libxcb
+    pkgconfig
+    xlibs.utilmacros
+    xlibs.xcbutilimage
+    xlibs.xcbutilrenderutil
+  ];
+
+  configurePhase = ''
+    sed -i '15 i\
+      LT_INIT' configure.ac
+    ${bashInteractive}/bin/bash autogen.sh --prefix="$out"
+  '';
+}
