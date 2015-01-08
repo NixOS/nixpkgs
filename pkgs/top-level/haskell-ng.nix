dbdabe03a78121eaf11393950bb9c07986db0cf8
Peter Simons <simons@cryp.to>
2015-01-08 22:44:08 +0100

    haskell-ng: remove obsolete 7.8.3 package set

--- pkgs/top-level/haskell-ng.nix
+++ pkgs/top-level/haskell-ng.nix
@@ -14,7 +14,6 @@ rec {
     ghc722 = callPackage ../development/compilers/ghc/7.2.2.nix { ghc = compiler.ghc704Binary; gmp = pkgs.gmp.override { withStatic = true; }; };
     ghc742 = callPackage ../development/compilers/ghc/7.4.2.nix { ghc = compiler.ghc704Binary; gmp = pkgs.gmp.override { withStatic = true; }; };
     ghc763 = callPackage ../development/compilers/ghc/7.6.3.nix { ghc = compiler.ghc704Binary; gmp = pkgs.gmp.override { withStatic = true; }; };
-    ghc783 = callPackage ../development/compilers/ghc/7.8.3.nix { ghc = compiler.ghc742Binary; gmp = pkgs.gmp.override { withStatic = true; }; };
     ghc784 = callPackage ../development/compilers/ghc/7.8.4.nix { ghc = compiler.ghc742Binary; gmp = pkgs.gmp.override { withStatic = true; }; };
     ghcHEAD = callPackage ../development/compilers/ghc/head.nix { inherit (packages.ghc784) ghc alex happy; };
     ghc = compiler.ghc784;
