ff5eae075a6a42994b8ae406afdb4cb64146f4b0
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-01-08 23:38:10 +0100

    Add freefall NixOS service module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -144,6 +144,7 @@
   ./services/hardware/acpid.nix
   ./services/hardware/amd-hybrid-graphics.nix
   ./services/hardware/bluetooth.nix
+  ./services/hardware/freefall.nix
   ./services/hardware/nvidia-optimus.nix
   ./services/hardware/pcscd.nix
   ./services/hardware/pommed.nix
