980758bdee156da3a41469a2ca55c4a6b1793022
William A. Kennington III <william@wkennington.com>
2015-01-10 10:48:37 -0800

    kernel: 3.17.7 -> 3.17.8

--- pkgs/os-specific/linux/kernel/linux-3.17.nix
+++ pkgs/os-specific/linux/kernel/linux-3.17.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.17.7";
+  version = "3.17.8";
   extraMeta.branch = "3.17";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "074f56sz22wqnn21gamwf63llhsqc2hgb7dzkbkd5gqjx6pfdj53";
+    sha256 = "0g2q0pnsbsfwzk9rl2ggkfhsmj9bzmy9lccclpd98zfw7kflz8ca";
   };
 
   features.iwlwifi = true;
