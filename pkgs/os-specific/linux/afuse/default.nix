2f1a9ef41f4b9741415bb6329e9669ea19979828
Ryan Mulligan <ryan@ryantm.com>
2015-01-11 13:21:37 -0800

    afuse: change from git to tarball source

--- pkgs/os-specific/linux/afuse/default.nix
+++ pkgs/os-specific/linux/afuse/default.nix
@@ -1,12 +1,11 @@
-{ stdenv, fetchgit, pkgconfig, autoreconfHook, fuse }:
+{ stdenv, fetchurl, pkgconfig, autoreconfHook, fuse }:
 
 stdenv.mkDerivation {
   name = "afuse-0.4.1";
 
-  src = fetchgit {
-    url = git://github.com/pcarrier/afuse.git;
-    rev = "a0892f5506ddcca2031825aff24f1518c8c2f1c8";
-    sha256 = "12071ff5171d4d5ce4d8835385f50e8079b25e885816b8ad6f22eb46c6497b28";
+  src = fetchurl {
+    url = https://github.com/pcarrier/afuse/archive/v0.4.1.tar.gz;
+    sha256 = "1sfhicmxppkvdd4z9klfn63snb71gr9hff6xij1gzk94xg6m0ycc";
   };
 
   buildInputs = [ autoreconfHook pkgconfig fuse ];
