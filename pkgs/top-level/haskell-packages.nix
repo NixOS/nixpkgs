126955d050ebbe48474a19c263761af3199fbe39
Alp Mestanogullari <alpmestan@gmail.com>
2015-01-13 12:04:21 +0100

    add the 'sitemap' haskell package

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -2328,6 +2328,8 @@ self : let callPackage = x : y : modifyPrio (newScope self x y); in
 
   silently = callPackage ../development/libraries/haskell/silently {};
 
+  sitemap = callPackage ../development/libraries/haskell/sitemap {};
+
   sized = callPackage ../development/libraries/haskell/sized {};
 
   sizedTypes = callPackage ../development/libraries/haskell/sized-types {};
