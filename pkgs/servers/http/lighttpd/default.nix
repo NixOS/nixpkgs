d9de59a46b43635aae17cec64e7db694814b8f18
aszlig <aszlig@redmoonstudios.org>
2015-01-13 08:36:34 +0100

    lighttpd: Fix building with magnet support.
    Doesn't build with LUA 5.2, so lets explicitly set LUA 5.1 as a
    dependency.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>
    (cherry picked from commit c5354fafceadbac60c2dcc263fb1d97e550ae7c7)

--- pkgs/servers/http/lighttpd/default.nix
+++ pkgs/servers/http/lighttpd/default.nix
@@ -1,9 +1,9 @@
 { stdenv, fetchurl, pkgconfig, pcre, libxml2, zlib, attr, bzip2, which, file
-, openssl, enableMagnet ? false, lua5 ? null
+, openssl, enableMagnet ? false, lua5_1 ? null
 , enableMysql ? false, mysql ? null
 }:
 
-assert enableMagnet -> lua5 != null;
+assert enableMagnet -> lua5_1 != null;
 assert enableMysql -> mysql != null;
 
 stdenv.mkDerivation rec {
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [ pkgconfig pcre libxml2 zlib attr bzip2 which file openssl ]
-             ++ stdenv.lib.optional enableMagnet lua5
+             ++ stdenv.lib.optional enableMagnet lua5_1
              ++ stdenv.lib.optional enableMysql mysql;
 
   configureFlags = [ "--with-openssl" ]
