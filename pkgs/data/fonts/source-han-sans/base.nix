35810e16479453caf1e857d8dd396a16fc602298
taku0 <mxxouy6x3m_github@tatapa.org>
2015-01-15 10:15:15 +0900

    source-han-sans: fixed dead link

--- pkgs/data/fonts/source-han-sans/base.nix
+++ pkgs/data/fonts/source-han-sans/base.nix
@@ -1,26 +1,27 @@
-{version ? "1.000", prefix, url, sha256, description}:
+{version ? "1.001R", sha256 ? "0cwz3d8jancl0a7vbjxhnh1vgwsjba62lahfjya9yrjkp1ndxlap", region, prefix, description}:
 
-{stdenv, fetchurl, unzip}:
+{stdenv, fetchurl}:
 
 stdenv.mkDerivation rec {
   inherit version;
   name = "${prefix}-${version}";
 
   src = fetchurl {
-    inherit url sha256;
+    url = "https://github.com/adobe-fonts/source-han-sans/archive/${version}.tar.gz";
+    inherit sha256;
   };
 
-  buildInputs = [ unzip ];
+  buildInputs = [ ];
 
   installPhase = ''
     mkdir -p $out/share/fonts/truetype
-    cp $( find . -name '*.otf' ) $out/share/fonts/truetype
+    cp $( find SubsetOTF/${region} -name '*.otf' ) $out/share/fonts/truetype
   '';
 
   meta = {
     inherit description;
 
-    homepage = http://sourceforge.net/adobe/source-han-sans/;
+    homepage = https://github.com/adobe-fonts/source-han-sans;
     license = stdenv.lib.licenses.asl20;
   };
 }
