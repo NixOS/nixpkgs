82aeda6d6b0b95ef2de2dfc14a844427ee6bdff9
koral <koral@mailoo.org>
2015-01-15 21:26:21 +0100

    monad-control: 0.3.3.0 -> 1.0.0.1

--- pkgs/development/libraries/haskell/monad-control/default.nix
+++ pkgs/development/libraries/haskell/monad-control/default.nix
@@ -4,8 +4,8 @@
 
 cabal.mkDerivation (self: {
   pname = "monad-control";
-  version = "0.3.3.0";
-  sha256 = "0vjff64iwnd9vplqfjyylbd900qmsr92h62hnh715wk06yacji7g";
+  version = "1.0.0.1";
+  sha256 = "1332wravhjhcbj0jh3z1hg2dyfxlkqyb3rv6zdkgz49h2pfjlfbn";
   buildDepends = [ transformers transformersBase ];
   meta = {
     homepage = "https://github.com/basvandijk/monad-control";
