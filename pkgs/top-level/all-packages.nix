4fea2833497ade8266c0b69788baf9a304e5ca8e
Sergey Mironov <grrwlf@gmail.com>
2015-01-16 15:04:12 +0300

    sipcmd: add the command-line SIP client

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8178,6 +8178,8 @@ let
 
   shishi = callPackage ../servers/shishi { };
 
+  sipcmd = callPackage ../applications/networking/sipcmd { };
+
   sipwitch = callPackage ../servers/sip/sipwitch { };
 
   spawn_fcgi = callPackage ../servers/http/spawn-fcgi { };
