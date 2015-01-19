3319507b70eec081dbe7395aaf55d40cdf0add31
Thomas Tuegel <ttuegel@gmail.com>
2015-01-19 11:12:04 -0600

    poppler: add Qt 5 version

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6856,6 +6856,7 @@ let
 
   poppler = callPackage ../development/libraries/poppler { lcms = lcms2; };
   popplerQt4 = poppler.poppler_qt4;
+  popplerQt5 = poppler.poppler_qt5;
 
   popt = callPackage ../development/libraries/popt { };
 
