4531b31d755792b7ae8b0c6384baf22ebe8c8fc2
Peter Simons <simons@cryp.to>
2015-01-20 14:54:54 +0100

    haskell-doctest: fix builds with old ghc

--- pkgs/development/haskell-modules/configuration-ghc-7.2.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-7.2.x.nix
@@ -57,4 +57,7 @@ self: super: {
   # These packages need more recent versions of core libraries to compile.
   happy = addBuildTools super.happy [self.containers_0_4_2_1 self.deepseq_1_3_0_1];
 
+  # Setup: Can't find transitive deps for haddock
+  doctest = dontHaddock super.doctest;
+
 }
