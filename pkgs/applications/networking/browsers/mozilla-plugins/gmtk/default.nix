c661352de2932937b12f4e451fd3c09897673b35
Pascal Wittmann <mail@pascal-wittmann.de>
2015-01-20 12:55:40 +0100

    gmtk: update from 1.0.8 to 1.0.9b

--- pkgs/applications/networking/browsers/mozilla-plugins/gmtk/default.nix
+++ pkgs/applications/networking/browsers/mozilla-plugins/gmtk/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, intltool, pkgconfig, gtk, GConf, alsaLib }:
 
 stdenv.mkDerivation rec {
-  name = "gmtk-1.0.8";
+  name = "gmtk-1.0.9b";
 
   src = fetchurl {
     url = "http://gmtk.googlecode.com/files/${name}.tar.gz";
-    sha256 = "034b02nplb2bp01yn4p19345jh3yibhn4lcxznrzcsmsyj2vlzq0";
+    sha256 = "07y5hd94qhvlk9a9vhrpznqaml013j3rq52r3qxmrj74gg4yf4zc";
   };
 
   buildInputs = [ intltool pkgconfig gtk GConf alsaLib ];
