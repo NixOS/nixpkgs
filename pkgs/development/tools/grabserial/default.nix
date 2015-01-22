01fc3e0525c6c6c20cbd0b8f40e9a939505a5e36
Venkateswara Rao Mandela <venkat.mandela@gmail.com>
2015-01-22 22:47:41 +0530

    grabserial: add package
    [Bjørn:
     * buildInputs -> propagatedBuildInputs, so pyserial is found at
       runtime
     * whitespace fix (add missing whitespace inside curly bracket)
    ]

---
+++ pkgs/development/tools/grabserial/default.nix
@@ -0,0 +1,22 @@
+{ stdenv, fetchgit, buildPythonPackage, pythonPackages }:
+
+buildPythonPackage rec {
+
+  name = "grabserial-20141120";
+
+  src = fetchgit {
+    url = https://github.com/tbird20d/grabserial.git;
+    rev  = "8b9c98ea35d382bac2aafc7a8a9c02440369a792";
+    sha256 = "ff27f5e5ab38c8450a4a0291e943e6c5a265e56d29d6a1caa849ae3238d71679";
+  };
+
+  propagatedBuildInputs = [ pythonPackages.pyserial ];
+
+  meta = {
+    description = "Python based serial dump and timing program";
+    homepage = https://github.com/tbird20d/grabserial;
+    license = stdenv.lib.licenses.gpl2;
+    maintainers = with stdenv.lib.maintainers; [ vmandela ];
+    platforms = stdenv.lib.platforms.linux;
+  };
+}
