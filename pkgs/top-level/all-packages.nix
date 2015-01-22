9d71536a7260830bd89a6d9c21e90708de3a7e57
aszlig <aszlig@redmoonstudios.org>
2015-01-22 14:20:14 +0100

    virtualbox: Add an alias for the hardened version.
    This is to ensure that Hydra will build the hardened version of
    VirtualBox and it's also lowPrio to avoid being accidentally installed
    into a user's environment.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>
    (cherry picked from commit 142b12c8640e3ae0ba67b39c7d53924725b99843)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8346,7 +8346,7 @@ let
      for a specific kernel.  This function can then be called for
      whatever kernel you're using. */
 
-  linuxPackagesFor = kernel: self: let callPackage = newScope self; in {
+  linuxPackagesFor = kernel: self: let callPackage = newScope self; in rec {
     inherit kernel;
 
     acpi_call = callPackage ../os-specific/linux/acpi-call {};
@@ -8426,6 +8426,10 @@ let
       enableExtensionPack = config.virtualbox.enableExtensionPack or false;
       pulseSupport = config.pulseaudio or false;
     };
+
+    virtualboxHardened = lowPrio (virtualbox.override {
+      enableHardening = true;
+    });
 
     virtualboxGuestAdditions = callPackage ../applications/virtualization/virtualbox/guest-additions { };
 
