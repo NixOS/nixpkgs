37f1938f5ce8f87560a66ccaf09e376d592879dc
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2015-01-23 19:39:54 +0000

    mpd: update to version 19.8

--- pkgs/servers/mpd/default.nix
+++ pkgs/servers/mpd/default.nix
@@ -27,13 +27,13 @@ let
   opt = stdenv.lib.optional;
   mkFlag = c: f: if c then "--enable-${f}" else "--disable-${f}";
   major = "0.19";
-  minor = "6";
+  minor = "8";
 
 in stdenv.mkDerivation rec {
   name = "mpd-${major}.${minor}";
   src = fetchurl {
     url    = "http://www.musicpd.org/download/mpd/${major}/${name}.tar.gz";
-    sha256 = "023h2d9x4yg1z2glnwf8h2j0p1xhn1hb0wf02mg70j3p0bz63imj";
+    sha256 = "1qifgc20cawhjxj7skn5xg23526i15hmav56wmd7cfxv50kq4j91";
   };
 
   buildInputs = [ pkgconfig glib boost ]
