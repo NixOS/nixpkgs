4fa5d1f626bcac4841c4862ace736d41d5e6cc53
Longrin Wischnewski <robberer@freakmail.de>
2015-01-23 16:15:20 +0100

    openntpd: add extraConfig and extraOptions

--- nixos/modules/services/networking/openntpd.nix
+++ nixos/modules/services/networking/openntpd.nix
@@ -12,6 +12,7 @@ let
 
   cfgFile = pkgs.writeText "openntpd.conf" ''
     ${concatStringsSep "\n" (map (s: "server ${s}") cfg.servers)}
+    ${cfg.extraConfig}
   '';
 in
 {
@@ -24,6 +25,27 @@ in
       default = config.services.ntp.servers;
       type = types.listOf types.str;
       inherit (options.services.ntp.servers) description;
+    };
+
+    extraConfig = mkOption {
+      type = with types; lines;
+      default = "";
+      example = ''
+        listen on 127.0.0.1 
+        listen on ::1 
+      '';
+      description = ''
+        Additional text appended to <filename>openntpd.conf</filename>.
+      '';
+    };
+
+    extraOptions = mkOption {
+      type = with types; string;
+      default = "";
+      example = "-s";
+      description = ''
+        Extra options used when launching openntpd.
+      '';
     };
   };
 
@@ -42,7 +64,7 @@ in
     systemd.services.openntpd = {
       description = "OpenNTP Server";
       wantedBy = [ "multi-user.target" ];
-      serviceConfig.ExecStart = "${package}/sbin/ntpd -d -f ${cfgFile}";
+      serviceConfig.ExecStart = "${package}/sbin/ntpd -d -f ${cfgFile} ${cfg.extraOptions}";
     };
   };
 }
