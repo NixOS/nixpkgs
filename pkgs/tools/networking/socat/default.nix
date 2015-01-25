7a7e59d2a973458c4eab0d1b52590966a478d825
Andrey Arapov <arno@nixaid.com>
2015-01-25 13:48:11 +0100

    socat: Update from 1.7.2.4 to 1.7.3.0, fixes a possible denial of service attack
    socat: Update from 1.7.2.4 to 1.7.3.0, fixes a possible denial of service attack
    (CVE Id pending), improves SSL client security, and provides a couple of bug and
    porting fixes.
    
    Among new features, socat now enables OpenSSL server side use of ECDHE ciphers,
    providing PFS (Perfect Forward Secrecy)
    
    http://www.dest-unreach.org/socat/doc/CHANGES

--- pkgs/tools/networking/socat/default.nix
+++ pkgs/tools/networking/socat/default.nix
@@ -1,14 +1,16 @@
 { stdenv, fetchurl, openssl }:
 
 stdenv.mkDerivation rec {
-  name = "socat-1.7.2.4";
+  name = "socat-1.7.3.0";
 
   src = fetchurl {
     url = "http://www.dest-unreach.org/socat/download/${name}.tar.bz2";
-    sha256 = "028yjka2zr6j1i8pmfmvzqki8ajczdl1hnry1x31xbbg3j83jxsb";
+    sha256 = "011ydc0x8camplf8l6mshs3v5fswarld8v0wf7grz6rjq18fhrq7";
   };
 
   buildInputs = [ openssl ];
+
+  patches = [ ./enable-ecdhe.patch ];
 
   meta = {
     description = "A utility for bidirectional data transfer between two independent data channels";
