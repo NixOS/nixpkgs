d7e38ef19fef9415f0ebdf3aec6498dd367cad23
skityl <mopofhair@gmail.com>
2015-01-27 00:29:52 +1100

    Bumped version of irssi-0.8.16 to irssi-0.8.17
    Signed-off-by: skityl <mopofhair@gmail.com>
    (cherry picked from commit 400adb7ec49d80b6e33d49a7f0fd22117d8ea3e5)

--- pkgs/applications/networking/irc/irssi/default.nix
+++ pkgs/applications/networking/irc/irssi/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, ncurses, glib, openssl, perl, libintlOrEmpty }:
 
 stdenv.mkDerivation rec {
-  name = "irssi-0.8.16";
+  name = "irssi-0.8.17";
   
   src = fetchurl {
     url = "http://irssi.org/files/${name}.tar.bz2";
-    sha256 = "15wzs4h754jzs1l4z7qzsyjllk9wdx3qfb6gymdiykvmlb9gwyiz";
+    sha256 = "01v82q2pfiimx6lh271kdvgp8hl4pahc3srg04fqzxgdsb5015iw";
   };
   
   buildInputs = [ pkgconfig ncurses glib openssl perl libintlOrEmpty ];
