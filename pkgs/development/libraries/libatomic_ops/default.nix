c30b11c776b17922793e39395534deb14d05c686
Johannes Bornhold <johannes@rhodecode.com>
2015-01-26 14:02:59 +0100

    libatomic_ops: Allow on all unix platforms

--- pkgs/development/libraries/libatomic_ops/default.nix
+++ pkgs/development/libraries/libatomic_ops/default.nix
@@ -23,6 +23,6 @@ stdenv.mkDerivation {
     description = ''A library for semi-portable access to hardware-provided atomic memory update operations'';
     license = stdenv.lib.licenses.gpl2Plus ;
     maintainers = [stdenv.lib.maintainers.raskin];
-    platforms = stdenv.lib.platforms.linux;
+    platforms = stdenv.lib.platforms.unix;
   };
 }
