0aad4b7ee418a5a4a6aa0c2da8a590480a63562a
aszlig <aszlig@redmoonstudios.org>
2015-01-28 08:12:51 +0100

    chromium: Update all channels to latest versions.
    Overview of the updated versions:
    
    stable: 40.0.2214.91 -> 40.0.2214.115
    beta:   41.0.2272.16 -> 41.0.2272.64
    dev:    41.0.2272.16 -> 42.0.2305.3
    
    Introduces 42.0.2305.3 as the new dev version, which no longer requires
    our user namespaces sandbox patch. Thanks to everyone participating in
    https://crbug.com/312380 for finally having this upstream.
    
    In the course of supporting the official namespace sandbox (that's what
    the user namespace sandbox is called), a few things needed to be fixed
    for version 42:
    
     * Add an updated nix_plugin_paths.patch, because the old
       one tries to patch the path for libpdf, which is now natively included
       in Chromium.
    
     * Don't copy libpdf.so to libexec path for version 42, it's no longer
       needed as it's completely built-in now.
    
     * Disable SUID sandbox directly in the source instead of going the easy
       route of passing --disable-setuid-sandbox. The reason is that with
       the command line flag a nasty nagbar will appear.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/applications/networking/browsers/chromium/common.nix
+++ pkgs/applications/networking/browsers/chromium/common.nix
@@ -135,11 +135,11 @@ let
         -exec chmod u+w {} +
     '';
 
-    postPatch = ''
+    postPatch = optionalString (versionOlder version "42.0.0.0") ''
       sed -i -e '/base::FilePath exe_dir/,/^ *} *$/c \
         sandbox_binary = base::FilePath(getenv("CHROMIUM_SANDBOX_BINARY_PATH"));
       ' sandbox/linux/suid/client/setuid_sandbox_client.cc
-
+    '' + ''
       sed -i -e '/module_path *=.*libexif.so/ {
         s|= [^;]*|= base::FilePath().AppendASCII("${libexif}/lib/libexif.so")|
       }' chrome/utility/media_galleries/image_metadata_extractor.cc
@@ -166,7 +166,9 @@ let
       use_openssl = useOpenSSL;
       selinux = enableSELinux;
       use_cups = cupsSupport;
+    } // optionalAttrs (versionOlder version "42.0.0.0") {
       linux_sandbox_chrome_path="${libExecPath}/${packageName}";
+    } // {
       werror = "";
       clang = false;
       enable_hidpi = hiDPISupport;
