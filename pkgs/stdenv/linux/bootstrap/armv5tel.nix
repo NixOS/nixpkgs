389d0f668e8722a13f16b20aa2d13941b00e4783
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2015-01-30 23:15:47 +0100

    Bootstrap tools armv5tel
    Conflicts:
    	pkgs/stdenv/linux/default.nix
    	pkgs/stdenv/linux/scripts/unpack-bootstrap-tools-arm.sh

--- pkgs/stdenv/linux/bootstrap/armv5tel.nix
+++ pkgs/stdenv/linux/bootstrap/armv5tel.nix
@@ -1,19 +1,11 @@
-let
-
-  fetch = { file, sha256 }: import <nix/fetchurl.nix> {
-    url = "http://tarballs.nixos.org/stdenv-linux/armv5tel/r18744/${file}";
-    inherit sha256;
-    executable = true;
-  };
-
-in {
+{
   busybox = import <nix/fetchurl.nix> {
-    url = file:///home/viric/busybox
-    sha256 = "80d6e9839d41cd8d77bf6b50a0ce19e112766ff5653a5520a2dfd0e0707f5da0";
+    url = "http://192.168.10.4/~viric/tmp/nix/busybox";
+    sha256 = "1z5zaa7cs70sndfcpabfhlw4ralzcjv1qhii2vi20vng3ldn2bwm";
     executable = true;
   };
 
-  bootstrapTools = {
+  bootstrapTools = import <nix/fetchurl.nix> {
     url = "http://tarballs.nixos.org/stdenv-linux/armv5tel/r18744/bootstrap-tools.cpio.bz2";
     sha256 = "1rn4n5kilqmv62dfjfcscbsm0w329k3gyb2v9155fsi1sl2cfzcb";
   };
