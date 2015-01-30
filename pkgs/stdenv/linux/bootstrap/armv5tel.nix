6944b15d8842d981893d7228cb43548dca8acb2f
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2015-01-30 14:49:32 +0100

    Fix busybox stdenv?
    (cherry picked from commit db8668c0bb0dc8feb4929867adf1b60f7025a925)

--- pkgs/stdenv/linux/bootstrap/armv5tel.nix
+++ pkgs/stdenv/linux/bootstrap/armv5tel.nix
@@ -7,34 +7,10 @@ let
   };
 
 in {
-  sh = fetch {
-    file = "sh";
-    sha256 = "1kv3gc8h209rjc5i0rzzc4pjxq23kzq25iff89in05c9vl20mn4n";
-  };
-
-  bzip2 = fetch {
-    file = "bzip2";
-    sha256 = "090jrj099wl33q0iq092iljilam39pv8acli59m2af0fa9z0d9f0";
-  };
-
-  mkdir = fetch {
-    file = "mkdir";
-    sha256 = "01s8myfvi559dsxvbanxw71vxzjv49k4gi1qh0ak6l0qx0xq602b";
-  };
-
-  cpio = fetch {
-    file = "cpio";
-    sha256 = "07snc8l0mn84w2xrvdmc5yfpmlbrvl2bar8wipbpvm43nzz2qbzs";
-  };
-
-  ln = fetch {
-    file = "ln";
-    sha256 = "0m9fz02bashpfgwfkxmrp4wa8a5r3il52bclvf6z36vsam0vx56d";
-  };
-
-  curl = fetch {
-    file = "curl.bz2";
-    sha256 = "19yqdjqi31zlnqn8ss2ml60iq2a1vrwfw6dmvjqp6qbxmh7yb8n8";
+  busybox = import <nix/fetchurl.nix> {
+    url = file:///home/viric/busybox
+    sha256 = "80d6e9839d41cd8d77bf6b50a0ce19e112766ff5653a5520a2dfd0e0707f5da0";
+    executable = true;
   };
 
   bootstrapTools = {
