6d7e09602f7ece9e6d66aa62a8c53461f9bc81a1
koral <koral@mailoo.org>
2015-01-31 11:09:23 +0100

    youtube-dl: 2015.01.25 -> 2015.01.30.2

--- pkgs/tools/misc/youtube-dl/default.nix
+++ pkgs/tools/misc/youtube-dl/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, python, zip, pandoc }:
 
 let
-  version = "2015.01.25";
+  version = "2015.01.30.2";
 in
 stdenv.mkDerivation rec {
   name = "youtube-dl-${version}";
 
   src = fetchurl {
     url = "http://youtube-dl.org/downloads/${version}/${name}.tar.gz";
-    sha256 = "1rw7nnxc8zss56hfm3mm0z66kqf63cqhv4nzpmnggw56xgv5ld7m";
+    sha256 = "00ymx254qgic9ka7wb41cinv1w0ijq6l0f5k2cfspi958jqql9lm";
   };
 
   buildInputs = [ python ];
