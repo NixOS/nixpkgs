522d059dc92400b61e9947c30ee272e59034ec03
Andrey Arapov <arno@nixaid.com>
2015-01-31 03:20:12 +0100

    libtoxcore, utox, qtox, toxic: Update to the newest Dev version

--- pkgs/applications/networking/instant-messengers/toxic/default.nix
+++ pkgs/applications/networking/instant-messengers/toxic/default.nix
@@ -2,13 +2,13 @@
 , libtoxcore, openal, libvpx, freealut, libconfig, pkgconfig }:
 
 stdenv.mkDerivation rec {
-  name = "toxic-dev-20141130";
+  name = "toxic-dev-20150125";
 
   src = fetchFromGitHub {
     owner = "Tox";
     repo = "toxic";
-    rev = "4acfe84171";
-    sha256 = "1yqglh9fm75zph4fzf3z4gwmamngypwpvb7shpqgakdg8ybq0a8s";
+    rev = "4badc983ea";
+    sha256 = "01zk6316v51f1zvp5ss53ay49h3nnaq5snlk0gxmsrmwg71bsnm6";
   };
 
   makeFlags = [ "-Cbuild" "PREFIX=$(out)" ];
