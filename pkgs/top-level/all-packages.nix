ec0ff0dee4e3514e650491a64c8dbeb63bdfd149
Bjørn Forsman <bjorn.forsman@gmail.com>
2015-02-01 22:52:01 +0100

    arduino: new package
    This commit adds the graphical Arduino IDE, built from the arduino-core
    expression.
    
    Also:
      - Add libusb to RPATH for avrdude. Without this, avrdude is broken.
        Possible TODO: replace bundled avrdude with the one from nixpkgs.
      - Use "cp -r ./build/linux/work/*", so example sketeches, the HTML
        reference documentation and the 'arduino' shell script that starts the
        IDE gets included.
      - Make meta.description slightly more generic, to cover both GUI/core
        variants.
    
    (cherry picked from commit ca982158bf33cef9398f86723d862da0603466da)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -494,9 +494,12 @@ let
 
   arcanist = callPackage ../development/tools/misc/arcanist {};
 
+  arduino = arduino-core.override { withGui = true; };
+
   arduino-core = callPackage ../development/arduino/arduino-core {
     jdk = jdk;
     jre = jdk;
+    withGui = false;
   };
 
   argyllcms = callPackage ../tools/graphics/argyllcms {};
