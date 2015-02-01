12378faab249ed5664e0ef81d3707cfeecfdd4ac
William A. Kennington III <william@wkennington.com>
2015-02-01 15:53:48 -0800

    kernel: 3.19-rc5 -> 3.19-rc6

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.19-rc5";
-  modDirVersion = "3.19.0-rc5";
+  version = "3.19-rc6";
+  modDirVersion = "3.19.0-rc6";
   extraMeta.branch = "3.19";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/testing/linux-${version}.tar.xz";
-    sha256 = "1n1bzdczqi9lqb6sahm1g9f59v1h6vp6r4skyi40dk3v2xacb0nw";
+    sha256 = "04lb9fh85y68zhhyzimvgzjz6qngjszkavklfhd4sly70f8i6bsh";
   };
 
   features.iwlwifi = true;
