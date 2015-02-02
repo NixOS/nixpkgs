a761b33d8c911d8c2c0d89b9068bfbf182163203
Lluís Batlle i Rossell <viric@vicerveza.homeunix.net>
2015-02-02 11:27:06 +0100

    Using fixed bootstraptools.
    Built natively with latest fixes.

--- pkgs/stdenv/linux/bootstrap/armv5tel.nix
+++ pkgs/stdenv/linux/bootstrap/armv5tel.nix
@@ -1,12 +1,12 @@
 {
   busybox = import <nix/fetchurl.nix> {
-    url = "http://192.168.10.4/~viric/tmp/nix/busybox";
-    sha256 = "1z5zaa7cs70sndfcpabfhlw4ralzcjv1qhii2vi20vng3ldn2bwm";
+    url = "http://vicerveza.homeunix.net/~viric/tmp/nix/busybox";
+    sha256 = "1vfadk3d2v0bsvmbaz1pvpn4g1vm7p751hkdxya1lkn5n1a9px5m";
     executable = true;
   };
 
   bootstrapTools = import <nix/fetchurl.nix> {
-    url = "http://tarballs.nixos.org/stdenv-linux/armv5tel/r18744/bootstrap-tools.cpio.bz2";
-    sha256 = "1rn4n5kilqmv62dfjfcscbsm0w329k3gyb2v9155fsi1sl2cfzcb";
+    url = "http://vicerveza.homeunix.net/~viric/tmp/nix/bootstrap-tools.tar.xz";
+    sha256 = "39df65053bab50bc2975060c4da177266e263f30c2afba231a97d23f4c471eb8";
   };
 }
