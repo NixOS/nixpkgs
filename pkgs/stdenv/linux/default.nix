810b416d895477de01c2a984da827a9c98590b35
Ambroz Bizjak <ambrop7@gmail.com>
2015-02-03 20:01:28 +0100

    Consolidate the unpack scripts for the bootstrap tools.

--- pkgs/stdenv/linux/default.nix
+++ pkgs/stdenv/linux/default.nix
@@ -42,10 +42,7 @@ rec {
 
     builder = bootstrapFiles.busybox;
 
-    args = if system == "armv5tel-linux" || system == "armv6l-linux"
-        || system == "armv7l-linux"
-      then [ "ash" "-e" ./scripts/unpack-bootstrap-tools-arm.sh ]
-      else [ "ash" "-e" ./scripts/unpack-bootstrap-tools.sh ];
+    args = [ "ash" "-e" ./scripts/unpack-bootstrap-tools.sh ];
 
     tarball = bootstrapFiles.bootstrapTools;
 
