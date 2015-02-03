04f724873a016895aa5a34b6ebd248b871dd3705
Spencer Whitt <sw@swhitt.me>
2015-02-03 15:19:38 -0500

    libtheora: Fix build on Darwin

--- pkgs/development/libraries/libtheora/default.nix
+++ pkgs/development/libraries/libtheora/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, libogg, libvorbis, tremor, autoconf, automake, libtool}:
+{stdenv, fetchurl, libogg, libvorbis, tremor, autoconf, automake, libtool, pkgconfig}:
 
 stdenv.mkDerivation ({
   name = "libtheora-1.1.1";
@@ -7,7 +7,15 @@ stdenv.mkDerivation ({
     sha256 = "0swiaj8987n995rc7hw0asvpwhhzpjiws8kr3s6r44bqqib2k5a0";
   };
 
+  buildInputs = [pkgconfig];
+
   propagatedBuildInputs = [libogg libvorbis];
+
+  # GCC's -fforce-addr flag is not supported by clang
+  # It's just an optimization, so it's safe to simply remove it
+  postPatch = stdenv.lib.optionalString stdenv.isDarwin ''
+    substituteInPlace configure --replace "-fforce-addr" ""
+  '';
 
   crossAttrs = {
     propagatedBuildInputs = [libogg.crossDrv tremor.crossDrv];
