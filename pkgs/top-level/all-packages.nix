df430ab2ea31052a8b258fe533662e665b82d7fb
Shea Levy <shea@shealevy.com>
2015-02-04 10:00:50 +0000

    Fix imake on darwin.
    Taken from joelteon/nixpkgs@6b3f7e6fb06426e3535ddbcc3fce05c74a1c7588
    
    Original author Mike Sperber <sperber@deinprogramm.de>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8191,7 +8191,7 @@ let
   xorg = recurseIntoAttrs (import ../servers/x11/xorg/default.nix {
     inherit clangStdenv fetchurl fetchgit fetchpatch stdenv pkgconfig intltool freetype fontconfig
       libxslt expat libpng zlib perl mesa_drivers spice_protocol
-      dbus libuuid openssl gperf m4 libevdev
+      dbus libuuid openssl gperf m4 libevdev tradcpp
       autoconf automake libtool xmlto asciidoc flex bison python mtdev pixman;
     mesa = mesa_noglu;
     udev = if stdenv.isLinux then udev else null;
