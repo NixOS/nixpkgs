bc73086149b4cbd3f33b21053fdf1e82441d42fb
Domen Kožar <domen@dev.si>
2015-02-04 13:49:53 +0100

    Revert "arduino: new package"
    This reverts commit ec0ff0dee4e3514e650491a64c8dbeb63bdfd149.
    
    Breaks eval. cc @bjornfor

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -494,12 +494,9 @@ let
 
   arcanist = callPackage ../development/tools/misc/arcanist {};
 
-  arduino = arduino-core.override { withGui = true; };
-
   arduino-core = callPackage ../development/arduino/arduino-core {
     jdk = jdk;
     jre = jdk;
-    withGui = false;
   };
 
   argyllcms = callPackage ../tools/graphics/argyllcms {};
