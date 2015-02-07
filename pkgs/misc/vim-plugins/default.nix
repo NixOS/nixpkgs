a6a9d8137f5e08c33c270a7fb38a0ed55860fedf
Arseniy Seroka <ars.seroka@gmail.com>
2015-02-07 23:15:00 +0300

    vimPlugins.youCompleteMe: fix name
    Using date instead of rev
    See https://github.com/NixOS/nixpkgs/pull/4499#issuecomment-58805580 for more info why.

--- pkgs/misc/vim-plugins/default.nix
+++ pkgs/misc/vim-plugins/default.nix
@@ -528,14 +528,13 @@ rec {
   };
 
   YouCompleteMe = addRtp "${rtpPath}/youcompleteme" (stdenv.mkDerivation rec {
-    rev = "035b6ca862da3bba0ab8aad388a485758311a464";
     src = fetchgit {
-      inherit rev;
+      rev = "035b6ca862da3bba0ab8aad388a485758311a464";
       url = "https://github.com/Valloric/YouCompleteMe.git";
       sha256 = "9cd8fc8bb9b35e7a164d62fa8e3a1bd3be7e18f4d46c78b5827612f32b9541d5";
      };
 
-    name = "vimplugin-youcompleteme-${rev}";
+    name = "vimplugin-youcompleteme-2015-02-05";
 
     buildInputs = [ python cmake llvmPackages.clang ];
 
