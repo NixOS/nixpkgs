fe03dc1a10caa0f21ca912b67a22be032ffbb91a
William A. Kennington III <william@wkennington.com>
2015-02-07 13:38:49 -0800

    spl_git: Update to latest

--- pkgs/os-specific/linux/spl/git.nix
+++ pkgs/os-specific/linux/spl/git.nix
@@ -5,8 +5,8 @@ stdenv.mkDerivation {
 
   src = fetchgit {
     url = git://github.com/zfsonlinux/spl.git;
-    rev = "54cccfc2e30fa84463c056e8ad04b2be9448999e";
-    sha256 = "07nbrq3li51lvncqvj4rzgdw9bnfzr4bc5qqwizrgangd839jlkx";
+    rev = "086476f92005506cb07e75f889e8ddb69026e842";
+    sha256 = "1n3cjdwxccvcns7jydgznkb6jy0p59qx1yg512jjf4m9hf5p2pwm";
   };
 
   patches = [ ./const.patch ./install_prefix-git.patch ];
