8b59b35dd2a31f617db7f58c232ec06d27059db8
Peter Simons <simons@cryp.to>
2015-02-08 13:02:11 +0100

    haskell-rematch-text: fix build by disabling the test suite

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -444,8 +444,9 @@ self: super: {
   # Upstream notified by e-mail.
   OpenGLRaw21 = markBrokenVersion "1.2.0.1" super.OpenGLRaw21;
 
-  # module missing: https://github.com/tcrayford/rematch/issues/5
-  rematch = dontCheck super.rematch; 
+  # Missing module.
+  rematch = dontCheck super.rematch;            # https://github.com/tcrayford/rematch/issues/5
+  rematch-text = dontCheck super.rematch-text;  # https://github.com/tcrayford/rematch/issues/6
 
 } // {
 
