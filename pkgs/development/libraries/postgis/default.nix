926e5aa70a28ab41db8260b33e7dff01671b5f81
Ben Moseley <ben@moseley.name>
2015-02-09 15:57:19 +0000

    Remove old PostGis versions.

--- pkgs/development/libraries/postgis/default.nix
+++ pkgs/development/libraries/postgis/default.nix
@@ -5,22 +5,7 @@ args@{fetchurl, composableDerivation, stdenv, perl, libxml2, postgresql, geos, p
   ### NixOS - usage:
   ==================
 
-    services.posgresql.extraPlugins = [ pkgs.postgis.v_1_5_1 ];
-
-    services.postgresql.extraPlugins = [ (pkgs.postgis.override { postgresql = pkgs.postgresql94; }).v_2_1_4 ];  # BM - 2015/01/01
-
-    or if you want to install 1.5.x and 1.3.x at the same time (which works
-    because the .sql and .so files have different names):
-
-    services.postgis.extraPlugins = [ (pkgs.buildEnv {
-          name = "postgis-plugins";
-          ignoreCollisions = 1; # scripts will collide - but there are aliases with version suffixes
-          paths = [ pkgs.postgis.v_1_3_5 pkgs.postgis.v_1_5_1 ];
-        })];
-
-    By now it is only supported installing one of the 1.3.x verions because
-    their shared libraries don't differ in naming.
-
+    services.postgresql.extraPlugins = [ (pkgs.postgis.override { postgresql = pkgs.postgresql94; }).v_2_1_4 ];
 
 
   ### important Postgis implementation details:
@@ -31,13 +16,12 @@ args@{fetchurl, composableDerivation, stdenv, perl, libxml2, postgresql, geos, p
 
       CREATE FUNCTION [...]
               AS '[..]liblwgeom', 'lwhistogram2d_in' LANGUAGE 'C' IMMUTABLE STRICT; -- WITH (isstrict);
-    
+
    where liblwgeom is the shared library.
    Postgis < 1.5 used absolute paths, in NixOS $libdir is always used.
 
    Thus if you want to use postgresql dumps which were created by non NixOS
    systems you have to adopt the library path.
-
 
 
    ### TODO:
@@ -99,55 +83,6 @@ let
   });
 
 in rec {
-
-  # these builders just add some custom informaton to the receipe above
-
-  v_1_3_5 = pgDerivationBase.merge ( fix: {
-    version = "1.3.5";
-    buildInputs = [ flex ];
-    sha256 = "102d5ybn0db1wrb249dga2v8347vysd4f1brc8zb82d7vdd34wyq";
-    sql_srcs = ["lwpostgis.sql" "spatial_ref_sys.sql"];
-
-    pg_db_postgis_fix_or_load_sql_dump = ./pg_db_postgis_fix_or_load_sql_dump.sh;
-    libName = "liblwgeom";
-    scriptNames = [ "pg_db_postgis_enable" "pg_db_postgis_fix_or_load_sql_dump"]; # helper scripts
-
-    # sql_srcs is defined in postInstall source above
-    # if store path changes sql should not break. So replace absolute path to
-    # shared library by path relatve to $libdir known by Postgres.
-    postInstall = ''
-      sed -i "s@AS '$out/lib/liblwgeom@AS '\$libdir/liblwgeom@" $sql_srcs $out/share/lwpostgis_upgrade.sql
-    '';
-  });
-
-  v_1_3_6 = v_1_3_5.merge ({
-    version = "1.3.6";
-    sha256 = "0i6inyiwc5zgf5a4ssg0y774f8vn45zn5c38ccgnln9r6i54vc6k";
-  });
-
-  v_1_5_1 = pgDerivationBase.merge ( fix : {
-    version = "1.5.1";
-    sha256 = "0nymvqqi6pp4nh4dcshzqm76x4sraf119jp7l27c2q1lygm6p6jr";
-    sql_srcs = ["postgis.sql" "spatial_ref_sys.sql"];
-  });
-
-  v_1_5_8 = pgDerivationBaseNewer.merge ( fix : {
-    version = "1.5.8";
-    sha256 = "016qn2hk3w8rg60rqn91l2nfk9zcsw0pv2s5rb1qhjw15ypgv5j8";
-    sql_srcs = ["postgis.sql" "spatial_ref_sys.sql"];
-  });
-
-  v_2_0_6 = pgDerivationBaseNewer.merge ( fix : {
-    version = "2.0.6";
-    sha256 = "129lyd1crv1bbjadyisjm3wv2vkj1v1nnxyns9z57300ip7wk3s3";
-    sql_srcs = ["postgis.sql" "spatial_ref_sys.sql"];
-    builtInputs = [gdal pkgconfig];
-
-    preConfigure = ''
-      sed -i 's@/usr/bin/file@${file}/bin/file@' configure
-      configureFlags="$configureFlags --with-gdalconfig=${gdal}/bin/gdal-config"
-    '';
-  });
 
   v_2_1_4 = pgDerivationBaseNewer.merge ( fix : {
     version = "2.1.4";
