7d24bcdbcf0c1c018900235ba8e97fe901892156
Herwig Hochleitner <herwig@bendlas.net>
2015-02-10 14:25:23 +0100

    Add gdk_pixbuf, gnome_icon_theme and librsvg to cdemu gui programs

--- pkgs/misc/emulators/cdemu/analyzer.nix
+++ pkgs/misc/emulators/cdemu/analyzer.nix
@@ -1,11 +1,11 @@
-{ callPackage, gtk3, glib, libxml2, gnuplot, makeWrapper, stdenv, gnome3 }:
+{ callPackage, gtk3, glib, libxml2, gnuplot, makeWrapper, stdenv, gnome3, gdk_pixbuf, librsvg }:
 let pkg = import ./base.nix {
   version = "3.0.0";
   pkgName = "image-analyzer";
   pkgSha256 = "1rb3f7c08dxc02zrwrkfvq7qlzlmm0kd2ah1fhxj6ajiyshi8q4v";
 };
 in callPackage pkg {
-  buildInputs = [ glib gtk3 libxml2 gnuplot (callPackage ./libmirage.nix {}) makeWrapper gnome3.gnome_icon_theme_symbolic ];
+  buildInputs = [ glib gtk3 libxml2 gnuplot (callPackage ./libmirage.nix {}) makeWrapper gnome3.gnome_icon_theme_symbolic gnome3.gnome_icon_theme gdk_pixbuf librsvg ];
   drvParams = {
     postFixup = ''
       wrapProgram $out/bin/image-analyzer \
