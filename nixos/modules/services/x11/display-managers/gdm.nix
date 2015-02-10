f7b26bf98ff9fcb65fdb7e609bce4a0a237e64fc
Luca Bruno <lucabru@src.gnome.org>
2015-02-10 16:26:06 +0000

    gnome3: Set XCURSOR_PATH. Closes #6191

--- nixos/modules/services/x11/display-managers/gdm.nix
+++ nixos/modules/services/x11/display-managers/gdm.nix
@@ -55,6 +55,8 @@ in
           GDM_X_SERVER = "${cfg.xserverBin} ${cfg.xserverArgs}";
           GDM_SESSIONS_DIR = "${cfg.session.desktops}";
           XDG_CONFIG_DIRS = "${gnome3.gnome_settings_daemon}/etc/xdg";
+          # Find the mouse
+          XCURSOR_PATH = "~/.icons:${config.system.path}/share/icons";
         };
         execCmd = "exec ${gdm}/sbin/gdm";
       };
