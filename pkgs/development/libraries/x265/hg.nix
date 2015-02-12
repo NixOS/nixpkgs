b60f5a004a9a1a312cbb3600e3d950671688c0d7
codyopel <codyopel@gmail.com>
2015-02-12 17:53:48 -0500

    x265-hg: bump commit

--- pkgs/development/libraries/x265/hg.nix
+++ pkgs/development/libraries/x265/hg.nix
@@ -1,7 +1,7 @@
 { callPackage, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "hg";
-  rev = "5f9f7194267b76f733e9ffb0f9e8b474dfe89a71";
-  sha256 = "056ng8nsadmjf6s7igbgbxmiapjcxpfy6pbayl764xbhpkv4md88";
+  version = "2015-2-11"; # Date of commit used Y-M-D
+  rev = "9ab104096834f51bd799ea1cf1160092f8182944";
+  sha256 = "1j4k6ylglrzng5rz29qx2z06amdrq8wyzvqhm4ivfzvpndfniim6";
 })
