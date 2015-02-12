90ea935252d0aa09095c8fd7a1189d57521e2596
codyopel <codyopel@gmail.com>
2015-02-12 17:53:07 -0500

    x265: 1.4 -> 1.5

--- pkgs/development/libraries/x265/default.nix
+++ pkgs/development/libraries/x265/default.nix
@@ -1,7 +1,7 @@
 { callPackage, ... } @ args:
 
 callPackage ./generic.nix (args // {
-  version = "1.4";
-  rev = "5e604833c5aa605d0b6efbe5234492b5e7d8ac61";
-  sha256 = "1aqksqi1qmjpva5cal6j7h0hzk298wk3nhqv73wnyqdchq2sa8v5";
+  version = "1.5";
+  rev = "9f0324125f53a12f766f6ed6f98f16e2f42337f4";
+  sha256 = "1nyim0l975faj7926s4wba8yvjy4rvx005zb7krv0gb5p84nzgi7";
 })
