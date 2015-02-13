3bc7dfdf22c643b531acfbce4b0ad6b46a5bc796
koral <koral@mailoo.org>
2015-02-13 01:24:05 +0100

    opensmtpd: 5.4.2p1 -> 5.4.4p1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5898,6 +5898,8 @@ let
 
   libarchive = callPackage ../development/libraries/libarchive { };
 
+  libasr = callPackage ../development/libraries/libasr { };
+
   libass = callPackage ../development/libraries/libass { };
 
   libassuan1 = callPackage ../development/libraries/libassuan1 { };
