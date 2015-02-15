3aa2cbea490621d7c742d7b3f7f0e188b6a0cdcb
Bjørn Forsman <bjorn.forsman@gmail.com>
2015-02-15 14:57:16 +0100

    baudline: init at 1.0.8
    Baudline is a scientific signal analysis application.
    
    I was unsure whether the baudline license allowed packaging in nixpkgs,
    so I sent a copy of this nix expression (and some explanation) to their
    support. I got this reply:
    
      From: Erik Olson <erik@sigblips.com>
      Subject: Re: Licensing question
    
      Hello Bjorn,
    
      This sounds fine.  What you suggest doing is very similar to how the
      FreeBSD FreshPorts system deals with baudline.
    
      Erik
    
    So basically, everything is good as long as we only distribute the _expression_
    to install the package. We must not distribute the package (binary).

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10904,6 +10904,10 @@ let
 
   batti = callPackage ../applications/misc/batti { };
 
+  baudline = callPackage ../applications/audio/baudline {
+    jack = jack1;
+  };
+
   cbatticon = callPackage ../applications/misc/cbatticon { };
 
   bazaar = callPackage ../applications/version-management/bazaar { };
