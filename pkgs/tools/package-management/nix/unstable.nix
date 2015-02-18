efba0f9bc79fed45462c215a3fb0882fe7ed04cd
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-02-18 10:39:30 +0100

    nixUnstable: Update
    (cherry picked from commit cbab474ceaded3e8a5fdc359a56e14711ddb7740)

--- pkgs/tools/package-management/nix/unstable.nix
+++ pkgs/tools/package-management/nix/unstable.nix
@@ -1,20 +1,20 @@
-{ stdenv, fetchurl, perl, curl, bzip2, sqlite, openssl ? null
-, pkgconfig, boehmgc, perlPackages
+{ lib, stdenv, fetchurl, perl, curl, bzip2, sqlite, openssl ? null
+, pkgconfig, boehmgc, perlPackages, libsodium
 , storeDir ? "/nix/store"
 , stateDir ? "/nix/var"
 }:
 
 stdenv.mkDerivation rec {
-  name = "nix-1.9pre4028_0d1dafa";
+  name = "nix-1.9pre4045_bd91064";
 
   src = fetchurl {
-    url = "http://hydra.nixos.org/build/19375273/download/4/${name}.tar.xz";
-    sha256 = "592da6102a777c13652f7350f40f5693fa005076e06c7ce6f570a854576d104e";
+    url = "http://hydra.nixos.org/build/19776695/download/4/${name}.tar.xz";
+    sha256 = "3a9d5750fa7cf4958a275516d5b7f3ab0036529a07a4ee16ade89a714fd55f7a";
   };
 
   nativeBuildInputs = [ perl pkgconfig ];
 
-  buildInputs = [ curl openssl sqlite ];
+  buildInputs = [ curl openssl sqlite ] ++ lib.optional stdenv.isLinux libsodium;
 
   propagatedBuildInputs = [ boehmgc ];
 
