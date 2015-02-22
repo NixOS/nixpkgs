e149afe3a1eae95bb776c609e3977fbef39f01a6
Nathaniel Baxter <nathaniel.baxter@gmail.com>
2015-02-22 15:16:39 +1100

    quazip: update from 0.7 to 0.7.1

--- pkgs/development/libraries/quazip/default.nix
+++ pkgs/development/libraries/quazip/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, zip, zlib, qt }:
 
 stdenv.mkDerivation rec {
-  name = "quazip-0.7";
+  name = "quazip-0.7.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/quazip/${name}.tar.gz";
-    sha256 = "8af5e7f9bff98b5a2982800a292eae0176c2b41a98a8deab14f4e1cbe07674a4";
+    sha256 = "1pijy6zn8kdx9m6wrckid24vkgp250hklbpmgrpixiam6l889jbq";
   };
 
   configurePhase = "cd quazip && qmake quazip.pro";
