43f02f27a0a4add09d5ceb468f1f618a43c35dcf
Valérian Galliat <val@codejam.info>
2015-02-22 12:41:33 +0100

    Shared libuv in io.js and Node.js

--- pkgs/development/web/iojs/default.nix
+++ pkgs/development/web/iojs/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, python, utillinux, openssl, http-parser, zlib, nightly ? false }:
+{ stdenv, fetchurl, python, utillinux, openssl, http-parser, zlib, libuv, nightly ? false }:
 
 let
   version = if nightly then "1.2.1-nightly20150213f0296933f8" else "1.2.0";
@@ -19,9 +19,9 @@ in stdenv.mkDerivation {
     sed -e 's|^#!/usr/bin/env python$|#!${python}/bin/python|g' -i configure
   '';
 
-  configureFlags = [ "--shared-openssl" "--shared-http-parser" "--shared-zlib" ];
+  configureFlags = [ "--shared-openssl" "--shared-http-parser" "--shared-zlib" "--shared-libuv" ];
 
-  buildInputs = [ python openssl http-parser zlib ] ++ (optional stdenv.isLinux utillinux);
+  buildInputs = [ python openssl http-parser zlib libuv ] ++ (optional stdenv.isLinux utillinux);
   setupHook = ../nodejs/setup-hook.sh;
 
   meta = {
