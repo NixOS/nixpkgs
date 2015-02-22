9c3da4e723237a5a581abe1559f6f74ac09c1088
Peter Simons <simons@cryp.to>
2015-02-22 13:32:27 +0100

    Cabal: update overrides for version 1.22.1.0

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -7,8 +7,8 @@ self: super: {
   # Some packages need a non-core version of Cabal.
   Cabal_1_18_1_6 = dontCheck super.Cabal_1_18_1_6;
   Cabal_1_20_0_3 = dontCheck super.Cabal_1_20_0_3;
-  Cabal_1_22_0_0 = dontCheck super.Cabal_1_22_0_0;
-  cabal-install = dontCheck (super.cabal-install.override { Cabal = self.Cabal_1_22_0_0; });
+  Cabal_1_22_1_0 = dontCheck super.Cabal_1_22_1_0;
+  cabal-install = dontCheck (super.cabal-install.override { Cabal = self.Cabal_1_22_1_0; });
 
   # Break infinite recursions.
   digest = super.digest.override { inherit (pkgs) zlib; };
