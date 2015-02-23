bc51573f8ad28e52ddc57bd72309840604e77454
Vladimír Čunát <vcunat@gmail.com>
2015-02-23 09:36:33 +0100

    dragonegg-3.5: fix by dropping included patch
    It seems to compile a hello-world fine.

--- pkgs/development/compilers/llvm/3.5/dragonegg.nix
+++ pkgs/development/compilers/llvm/3.5/dragonegg.nix
@@ -5,14 +5,6 @@ stdenv.mkDerivation rec {
 
   src = fetch "dragonegg" "1v1lc9h2nfk3lsk9sx1k4ckwddz813hqjmlp2bx2kwxx9hw364ar";
 
-  patches = [(fetchpatch {
-    url = "https://llvm.org/viewvc/llvm-project/dragonegg/trunk/src/x86/ABIHack.inc"
-      + "?r1=208730&r2=208729&view=patch";
-    sha256 = "1al82gqz90hzjx24p0wls029lw2bgnlgd209kgvxsp82p4z1v1c1";
-    name = "bug-18548.patch";
-  })];
-  patchFlags = "-p2";
-
   # The gcc the plugin will be built for (the same used building dragonegg)
   GCC = "gcc";
 
