c47a3b0663276df524ad8934e5942403115a639c
Bjørn Forsman <bjorn.forsman@gmail.com>
2015-02-23 09:11:08 +0100

    microscheme: update to latest release (0.9.2)
    * Don't "externalize url/rev/sha256 to permit easier override".
      Just override 'src' itself. Then you can get the source from anywhere,
      not just git. I needed to touch this anyway, because I want to use
      fetchzip instead of fetchgit for releases (no need to clone repo).
    * Latest release has "make install" improvements, simplifying our
      install.

--- pkgs/development/compilers/microscheme/default.nix
+++ pkgs/development/compilers/microscheme/default.nix
@@ -1,27 +1,19 @@
-{ stdenv, fetchgit, vim, avrdude, avrgcclibc, makeWrapper }:
+{ stdenv, fetchzip, vim, avrdude, avrgcclibc, makeWrapper }:
 
 stdenv.mkDerivation rec {
   name = "microscheme-${version}";
-  version = "2015-02-04";
+  version = "0.9.2";
 
-  # externalize url/rev/sha256 to permit easier override
-  rev = "2f14781034a67adc081a22728fbf47a632f4484e";
-  sha256 = "15bdlmchzbhxj262r2fj78wm4c4hfrap4kyzv8n5b624svszr0zd";
-  url = https://github.com/ryansuchocki/microscheme.git;
-
-  src = fetchgit {
-    inherit rev;
-    inherit sha256;
-    inherit url;
+  src = fetchzip {
+    name = "${name}-src";
+    url = "https://github.com/ryansuchocki/microscheme/archive/v${version}.tar.gz";
+    sha256 = "0ly1cphvnsip70kng9q0blb07pkyp9allav42sr6ybswqfqg60j9";
   };
 
   buildInputs = [ makeWrapper vim ];
 
   installPhase = ''
-    mkdir -p $out/bin && make install PREFIX=$out
-
-    mkdir -p $out/share/microscheme/
-    cp -r examples/ $out/share/microscheme
+    make install PREFIX=$out
 
     wrapProgram $out/bin/microscheme \
       --prefix PATH : "${avrdude}/bin:${avrgcclibc}/bin"
