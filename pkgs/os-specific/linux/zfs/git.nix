a477e91f47ddd728481818f3ccb824fa16f8b617
Arseniy Seroka <ars.seroka@gmail.com>
2015-02-23 20:30:14 +0300

    zfs-git: fix name (use date)

--- pkgs/os-specific/linux/zfs/git.nix
+++ pkgs/os-specific/linux/zfs/git.nix
@@ -1,15 +1,11 @@
 { callPackage, stdenv, fetchgit, spl_git, ... } @ args:
 
-let
-  gitVersion = "7d2868d5fc88a4c30769b44f56a3a88a4277a9ab";
-in
-
 callPackage ./generic.nix (args // rec {
-  version = stdenv.lib.substring 0 7 gitVersion;
+  version = "2015-02-13";
 
   src = fetchgit {
     url = git://github.com/zfsonlinux/zfs.git;
-    rev = gitVersion;
+    rev = "7d2868d5fc88a4c30769b44f56a3a88a4277a9ab";
     sha256 = "1gf0x8d0zs2h3w910agxm4nb9qr4chs54iiblnqz4k74yyhbmwgg";
   };
 
