1e7ac2090626692d36c9bae7fbc66da59233763e
Nathaniel Baxter <nathaniel.baxter@gmail.com>
2015-02-24 21:50:14 +1100

    aria2: update from 1.18.9 to 1.18.10

--- pkgs/tools/networking/aria2/default.nix
+++ pkgs/tools/networking/aria2/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "aria2-${version}";
-  version = "1.18.9";
+  version = "1.18.10";
 
   src = fetchurl {
     url = "mirror://sourceforge/aria2/stable/${name}/${name}.tar.bz2";
-    sha256 = "1cn4g4mcrnw67y23970a9bngl8nf2x9hh82lc59gz3xyxn8wljz2";
+    sha256 = "1vvc3pv1100xb4293bmgqpxvy3pdvivnz415b9q78n7190kag3a5";
   };
 
   buildInputs = [ pkgconfig openssl libxml2 sqlite zlib ];
