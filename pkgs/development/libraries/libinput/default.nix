51b7fe53faade03620314d6013153e926f37a5f2
William A. Kennington III <william@wkennington.com>
2015-02-24 11:52:19 -0800

    libinput: 0.10 -> 0.11

--- pkgs/development/libraries/libinput/default.nix
+++ pkgs/development/libraries/libinput/default.nix
@@ -15,11 +15,11 @@ in
 
 with stdenv.lib;
 stdenv.mkDerivation rec {
-  name = "libinput-0.10.0";
+  name = "libinput-0.11.0";
 
   src = fetchurl {
     url = "http://www.freedesktop.org/software/libinput/${name}.tar.xz";
-    sha256 = "0h8lbhhxb5020bhdblxp1pkapy4bchjj3l44fxabz9pi1zw03q2c";
+    sha256 = "0hq7plvf9gpscy69pngffrfzqdrcwvpqr0a8fh45xslm5xwxcz4j";
   };
 
   configureFlags = [
