ed75d612d6d6cf1b2e75360eb52ae6aa79b24c69
Austin Seipp <aseipp@pobox.com>
2015-02-25 23:00:52 -0600

    nixpkgs: afl 1.49b -> 1.56b
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/tools/security/afl/default.nix
+++ pkgs/tools/security/afl/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name    = "afl-${version}";
-  version = "1.49b";
+  version = "1.56b";
 
   src = fetchurl {
     url    = "http://lcamtuf.coredump.cx/afl/releases/${name}.tgz";
-    sha256 = "14mbfdg78p80517zirvcsc2l2fbxn7n3j9zyfq8cn9dr2qi9icpq";
+    sha256 = "1nagd9ycj0i1h7kx2pz07fpwy4b528inmc9hj7226qiid0bynsh4";
   };
 
   buildPhase   = "make PREFIX=$out";
