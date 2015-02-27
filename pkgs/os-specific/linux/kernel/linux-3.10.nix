cb24bc18b99cb217ce8b24891c2d44a92c9e69c0
William A. Kennington III <william@wkennington.com>
2015-02-27 14:01:16 -0800

    kernel: 3.10.69 -> 3.10.70

--- pkgs/os-specific/linux/kernel/linux-3.10.nix
+++ pkgs/os-specific/linux/kernel/linux-3.10.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.10.69";
+  version = "3.10.70";
   extraMeta.branch = "3.10";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "14m4npds1f0whsd1nhnpl3bsscck9kg0r8bvrha4bdlhc8fjzrrc";
+    sha256 = "0x7v5gy0wr6g8697slhr57zdpv0i6lwa1yrzzrxizvsgvh4y9mpz";
   };
 
   features.iwlwifi = true;
