18320d3b219a35a2f4c032aa4b16ec89db692901
Joachim Fasting <joachifm@fastmail.fm>
2015-03-02 07:20:45 +0100

    nixos: fix grsec-lock requires

--- nixos/modules/security/grsecurity.nix
+++ nixos/modules/security/grsecurity.nix
@@ -286,7 +286,7 @@ in
 
     systemd.services.grsec-lock = mkIf cfg.config.sysctl {
       description     = "grsecurity sysctl-lock Service";
-      requires        = [ "sysctl.service" ];
+      requires        = [ "systemd-sysctl.service" ];
       wantedBy        = [ "multi-user.target" ];
       serviceConfig.Type = "oneshot";
       serviceConfig.RemainAfterExit = "yes";
