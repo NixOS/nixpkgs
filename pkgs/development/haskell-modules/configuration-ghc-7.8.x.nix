b29d61e0df1dc98480dd204becbe16f99ca161f7
Peter Simons <simons@cryp.to>
2015-03-02 12:10:34 +0100

    haskell-test-sandbox: the build errors have been fixed upstream

--- pkgs/development/haskell-modules/configuration-ghc-7.8.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-7.8.x.nix
@@ -81,14 +81,6 @@ self: super: {
   # Needs mtl 2.2.x due to "plailude".
   clac = dontDistribute super.clac;
 
-  # https://github.com/junjihashimoto/test-sandbox-compose/issues/1
-  hspec-test-sandbox = markBroken super.hspec-test-sandbox;
-  test-framework-sandbox = markBroken super.test-sandbox-quickcheck;
-  test-sandbox = markBroken super.test-sandbox;
-  test-sandbox-compose = markBroken super.test-sandbox-compose;
-  test-sandbox-hunit = markBroken super.test-sandbox-hunit;
-  test-sandbox-quickcheck = markBroken super.test-sandbox-quickcheck;
-
   # These packages need mtl 2.2.x directly or indirectly via dependencies.
   apiary-purescript = markBroken super.apiary-purescript;
   highlighter2 = markBroken super.highlighter2;
