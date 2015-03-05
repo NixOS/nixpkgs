b6a3fd396e318d3f687b719a0e6994c115dcd9b5
Eric Seidel <gridaphobe@gmail.com>
2015-03-05 13:00:50 -0800

    dysnomia: fix syntax error

--- pkgs/tools/package-management/disnix/dysnomia/default.nix
+++ pkgs/tools/package-management/disnix/dysnomia/default.nix
@@ -29,14 +29,14 @@ stdenv.mkDerivation {
   preConfigure = if enableEjabberdDump then "export PATH=$PATH:${ejabberd}/sbin" else "";
   
   configureFlags = [
-     $(if enableApacheWebApplication then "--with-apache" else "--without-apache")
-     $(if enableAxis2WebService then "--with-axis2" else "--without-axis2")
-     $(if enableEjabberdDump then "--with-ejabberd" else "--without-ejabberd")
-     $(if enableMySQLDatabase then "--with-mysql" else "--without-mysql")
-     $(if enablePostgreSQLDatabase then "--with-postgresql" else "--without-postgresql")
-     $(if enableSubversionRepository then "--with-subversion" else "--without-subversion")
-     $(if enableTomcatWebApplication then "--with-tomcat=${catalinaBaseDir}" else "--without-tomcat")
-     $(if enableMongoDatabase then "--with-mongodb" else "--without-mongodb")
+     (if enableApacheWebApplication then "--with-apache" else "--without-apache")
+     (if enableAxis2WebService then "--with-axis2" else "--without-axis2")
+     (if enableEjabberdDump then "--with-ejabberd" else "--without-ejabberd")
+     (if enableMySQLDatabase then "--with-mysql" else "--without-mysql")
+     (if enablePostgreSQLDatabase then "--with-postgresql" else "--without-postgresql")
+     (if enableSubversionRepository then "--with-subversion" else "--without-subversion")
+     (if enableTomcatWebApplication then "--with-tomcat=${catalinaBaseDir}" else "--without-tomcat")
+     (if enableMongoDatabase then "--with-mongodb" else "--without-mongodb")
      "--with-job-template=${jobTemplate}"
    ];
   
