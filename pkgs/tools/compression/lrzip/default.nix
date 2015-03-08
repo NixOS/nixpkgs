eeac4fdf0e07b11c89db5315dcc7aa75402da113
Michael Raskin <7c6f434c@mail.ru>
2015-03-08 21:59:43 +0300

    update zpaq

--- pkgs/tools/compression/lrzip/default.nix
+++ pkgs/tools/compression/lrzip/default.nix
@@ -1,12 +1,12 @@
 {stdenv, fetchurl, zlib, lzo, bzip2, nasm, perl}:
 
 stdenv.mkDerivation rec {
-  version = "0.616";
+  version = "0.620";
   name = "lrzip-${version}";
 
   src = fetchurl {
     url = "http://ck.kolivas.org/apps/lrzip/${name}.tar.bz2";
-    sha256 = "1bimlbsfzjvippbma08ifm1grcy9i7avryrkdvnvrfyqnj6mlbcq";
+    sha256 = "11ilyjrpdg0k29yidbxvz9aq54zvcxd4c308fjy578jgmlw8f2sd";
   };
 
   buildInputs = [ zlib lzo bzip2 nasm perl ];
