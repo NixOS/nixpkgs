cb21db1c77d8a6cee25bea1be6ebc9c84e1b90b1
Domen Kožar <domen@dev.si>
2015-03-10 11:43:53 +0100

    fix eval

--- pkgs/tools/security/pcsctools/default.nix
+++ pkgs/tools/security/pcsctools/default.nix
@@ -19,7 +19,7 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     description = "Tools used to test a PC/SC driver, card or reader";
     homepage = http://ludovic.rousseau.free.fr/softwares/pcsc-tools/;
-    license = licenses.gpl2plus;
+    license = licenses.gpl2Plus;
     maintainers = with maintainers; [ viric ];
     platforms = with platforms; linux;
   };
