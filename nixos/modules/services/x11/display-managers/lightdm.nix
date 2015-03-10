5893d2ecd395596011adc9a116872324888e18c2
William A. Kennington III <william@wkennington.com>
2015-03-10 14:23:43 -0700

    nixos/lightdm: More Cleanups

--- nixos/modules/services/x11/display-managers/lightdm.nix
+++ nixos/modules/services/x11/display-managers/lightdm.nix
@@ -18,6 +18,9 @@ let
       exec ${dmcfg.xserverBin} ${dmcfg.xserverArgs}
     '';
 
+  theme = pkgs.gnome3.gnome_themes_standard;
+  icons = pkgs.gnome3.gnome_icon_theme;
+
   # The default greeter provided with this expression is the GTK greeter.
   # Again, we need a few things in the environment for the greeter to run with
   # fonts/icons.
@@ -30,11 +33,12 @@ let
       makeWrapper ${pkgs.lightdm_gtk_greeter}/sbin/lightdm-gtk-greeter \
         $out/greeter \
         --prefix PATH : "${pkgs.glibc}/bin" \
-        --set GTK_DATA_PREFIX "${pkgs.gnome3.gnome_themes_standard}" \
-        --set GTK_EXE_PREFIX "${pkgs.gnome3.gnome_themes_standard}" \
-        --set GTK_PATH "${pkgs.gnome3.gnome_themes_standard}" \
-        --set XDG_DATA_DIRS "${pkgs.gnome3.gnome_themes_standard}/share:${pkgs.gnome3.gnome_icon_theme}/share" \
-        --set XDG_CONFIG_HOME ${pkgs.gnome3.gnome_themes_standard}/share
+        --set GDK_PIXBUF_MODULE_FILE "$(find ${theme} -name loaders.cache)" \
+        --set GTK_PATH "${theme}:${pkgs.gtk3}" \
+        --set GTK_EXE_PREFIX "${theme}" \
+        --set GTK_DATA_PREFIX "${theme}" \
+        --set XDG_DATA_DIRS "${theme}/share:${icons}/share" \
+        --set XDG_CONFIG_HOME "${theme}/share"
 
       cat - > $out/lightdm-gtk-greeter.desktop << EOF
       [Desktop Entry]
