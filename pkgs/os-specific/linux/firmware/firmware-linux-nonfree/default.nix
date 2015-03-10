73f3432fbbb9c58f63a6b8d5745327c6a34651ea
William A. Kennington III <william@wkennington.com>
2015-03-09 17:01:31 -0700

    firmware-linux-nonfree: 2015-02-27 -> 2015-03-09

--- pkgs/os-specific/linux/firmware/firmware-linux-nonfree/default.nix
+++ pkgs/os-specific/linux/firmware/firmware-linux-nonfree/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchgit }:
 
 stdenv.mkDerivation {
-  name = "firmware-linux-nonfree-2015-02-27";
+  name = "firmware-linux-nonfree-2015-03-09";
 
   src = fetchgit {
     url = "git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
-    rev = "cef33368c4d3425f11306496f0250f8ef1cf3c1f";
-    sha256 = "0az6b7fbhanqdc9v9dl651yqqnfbm1npdibip196vnmd5qlv2iw4";
+    rev = "020e534ec90106d42a890cd9d090b24e3d158c53";
+    sha256 = "101mpps0jcv2dd4jh1w3j4h78d4iv8n8r1cnf4br2vg66zl3zg9v";
   };
 
   preInstall = ''
