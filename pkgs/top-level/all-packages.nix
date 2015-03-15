7e94f7943300cd292d0ad1f45406807b208a0e10
Charles Strahan <charles.c.strahan@gmail.com>
2015-03-15 18:57:44 -0400

    hs-mesos: fix build
    mesos was propagating the wrong protobuf version, and the cabal file
    referenced some impure paths.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4449,7 +4449,7 @@ let
   mesos = callPackage ../applications/networking/cluster/mesos {
     sasl = cyrus_sasl;
     inherit (pythonPackages) python boto setuptools distutils-cfg wrapPython;
-    pythonProtobuf = pythonPackages.protobuf;
+    pythonProtobuf = pythonPackages.protobuf2_5;
   };
 
   mesos-dns = callPackage ../servers/dns/mesos-dns { };
