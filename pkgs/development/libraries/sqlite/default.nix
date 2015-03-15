eee31a2c979b59de6ac966f74ef2530a8f962e65
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2015-03-15 23:03:14 +0100

    Update sqlite to 3.8.8.3

--- pkgs/development/libraries/sqlite/default.nix
+++ pkgs/development/libraries/sqlite/default.nix
@@ -3,11 +3,11 @@
 assert interactive -> readline != null && ncurses != null;
 
 stdenv.mkDerivation {
-  name = "sqlite-3.8.7.4";
+  name = "sqlite-3.8.8.3";
 
   src = fetchurl {
-    url = "http://www.sqlite.org/2014/sqlite-autoconf-3080704.tar.gz";
-    sha1 = "70ca0b8884a6b145b7f777724670566e2b4f3cde";
+    url = "http://sqlite.org/2015/sqlite-autoconf-3080803.tar.gz";
+    sha1 = "2fe3f6226a2a08a2e814b97cd53e36bb3c597112";
   };
 
   buildInputs = lib.optionals interactive [ readline ncurses ];
@@ -20,6 +20,6 @@ stdenv.mkDerivation {
     homepage = http://www.sqlite.org/;
     description = "A self-contained, serverless, zero-configuration, transactional SQL database engine";
     platforms = stdenv.lib.platforms.unix;
-    maintainers = [ stdenv.lib.maintainers.eelco ];
+    maintainers = with stdenv.lib.maintainers; [ eelco np ];
   };
 }
