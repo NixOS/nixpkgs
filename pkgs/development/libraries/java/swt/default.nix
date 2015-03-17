d1cf37eeb76dd1eb2ec731cc09aef3f8ce84f7db
Luis G. Torres <lgtorres42@gmail.com>
2015-03-17 19:22:38 -0400

    Cleaned up swt derivation

--- pkgs/development/libraries/java/swt/default.nix
+++ pkgs/development/libraries/java/swt/default.nix
@@ -7,21 +7,19 @@
 }:
 
 let
-  metadata =
-    if stdenv.isLinux then
-      if stdenv.isx86_64 then
-        { platform = "gtk-linux-x86_64";
-          sha256 = "0hq48zfqx2p0fqr0rlabnz2pdj0874k19918a4dbj0fhzkhrh959"; }
-      else if stdenv.isi686 then
-             { platform = "gtk-linux-x86";
-               sha256 = "10si8kmc7c9qmbpzs76609wkfb784pln3qpmra73gb3fbk7z8caf"; }
-           else { }
-    else if stdenv.isDarwin then
-           if stdenv.isx86_64 then
-             { platform = "cocoa-macosx-x86_64";
-               sha256 = "1565gg63ssrl04fh355vf9mnmq8qwwki3zpc3ybm7bylgkfwc9h4"; }
-           else { }
-         else { };
+  platformMap = {
+    "x86_64-linux" =
+      { platform = "gtk-linux-x86_64";
+        sha256 = "0hq48zfqx2p0fqr0rlabnz2pdj0874k19918a4dbj0fhzkhrh959"; };
+    "i686-linux" =
+      { platform = "gtk-linux-x86";
+        sha256 = "10si8kmc7c9qmbpzs76609wkfb784pln3qpmra73gb3fbk7z8caf"; };
+    "x86_64-darwin" =
+      { platform = "cocoa-macosx-x86_64";
+        sha256 = "1565gg63ssrl04fh355vf9mnmq8qwwki3zpc3ybm7bylgkfwc9h4"; };
+  };
+
+  metadata = assert platformMap ? ${stdenv.system}; platformMap.${stdenv.system};
 
 in stdenv.mkDerivation rec {
   version = "3.7.2";
