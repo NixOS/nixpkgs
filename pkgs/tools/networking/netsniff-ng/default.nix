22625fbf37bd463b32a8b87bec8bbaa9250a5124
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-03-17 21:56:00 +0100

    netsniff-ng 0.5.9-rc4-40-g5107740 -> 0.5.9-rc4-49-g6f54288
    Adds support for IGMP packet generation.

--- pkgs/tools/networking/netsniff-ng/default.nix
+++ pkgs/tools/networking/netsniff-ng/default.nix
@@ -3,14 +3,14 @@
 , pkgconfig, zlib }:
 
 stdenv.mkDerivation rec {
-  version = "0.5.9-rc4-40-g5107740";
+  version = "0.5.9-rc4-49-g6f54288";
   name = "netsniff-ng-${version}";
 
   src = fetchFromGitHub rec { # Upstream recommends and supports git
     repo = "netsniff-ng";
     owner = repo;
-    rev = "5107740603d10feed6104afd75042970cb392843";
-    sha256 = "1z3b7pa5rhz37dhfb1riy1j9lg917bs4z7clqbxm1hzi1x2ln988";
+    rev = "6f542884d002d55d517a50dd9892068e95400b25";
+    sha256 = "0j7rqigfn9zazmzi8w3hapzi8028jr3q27lwyjw7k7lpnayj5iaa";
   };
 
   buildInputs = [ bison flex libcli libnet libnl libnetfilter_conntrack
