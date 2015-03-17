87970786954fcc5a33533199bc661bfe4b95facb
koral <koral@mailoo.org>
2015-03-17 17:18:12 +0000

    git: 2.3.2 -> 2.3.3

--- pkgs/applications/version-management/git-and-tools/git/default.nix
+++ pkgs/applications/version-management/git-and-tools/git/default.nix
@@ -9,7 +9,7 @@
 }:
 
 let
-  version = "2.3.2";
+  version = "2.3.3";
   svn = subversionClient.override { perlBindings = true; };
 in
 
@@ -18,7 +18,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://www.kernel.org/pub/software/scm/git/git-${version}.tar.xz";
-    sha256 = "09gqijsjfnxlbsxbxzlvllg37bfs9f4jwa2plqsanmba09i89sqq";
+    sha256 = "11s6w6dsv9kfgpfa75sas4pi6spw75ph0b0b6b12xq37hl4l8ma7";
   };
 
   patches = [
