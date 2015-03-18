ff0ca7fcedc99aef46f77f23e5f686e9d736b324
Luca Bruno <lethalman88@gmail.com>
2015-03-18 10:11:41 +0100

    p2pvc: remove dot at the end of description

--- pkgs/applications/video/p2pvc/default.nix
+++ pkgs/applications/video/p2pvc/default.nix
@@ -17,7 +17,7 @@ stdenv.mkDerivation {
   };
 
   meta = {
-    description = "A point to point color terminal video chat.";
+    description = "A point to point color terminal video chat";
     homepage = "https://github.com/mofarrell/p2pvc";
     license = stdenv.lib.licenses.mit;
     maintainers = with stdenv.lib.maintainers; [ trino ];
