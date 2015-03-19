fb8308c4aff9ad86de3f2d850468cb5b4eacb43f
Jan Malakhovski <oxij@oxij.org>
2015-03-19 01:41:40 +0000

    redshift: update 1.9.1 -> 1.10-git-20150222

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13056,8 +13056,6 @@ let
   };
 
   redshift = callPackage ../applications/misc/redshift {
-    inherit (xorg) libX11 libXrandr libxcb randrproto libXxf86vm
-      xf86vidmodeproto;
     inherit (gnome) GConf;
     inherit (pythonPackages) pyxdg;
     geoclue = geoclue2;
