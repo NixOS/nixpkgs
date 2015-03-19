da158672bde9f76ce9ab81a7ba4c4b8a0835abbb
Vladimír Čunát <vcunat@gmail.com>
2015-03-19 21:09:20 +0100

    simutrans: update, refactor, improve, add paksets
    Close #7019.
    
    - update all, and add more paksets
    - add config.simutrans.paksets, multiple are possible now
    - fix #6719: missing sounds
    - move user settings from ~/simutrans to ~/.simutrans
    - darwin support is untested (but claimed upstream)
    
    Tested-by: Matthias Beyer <mail@beyermatthias.de>
    (and by the author vcunat)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12798,6 +12798,8 @@ let
   };
 
   simutrans = callPackage ../games/simutrans { };
+  # get binaries without data built by Hydra
+  simutrans_binaries = lowPrio simutrans.binaries;
 
   soi = callPackage ../games/soi {};
 
