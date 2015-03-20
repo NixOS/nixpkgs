16b9fd71f9a2b7bdde243d2b1afad694f3e58559
Peter Simons <simons@cryp.to>
2015-03-20 11:37:06 +0100

    haskell-ansi-wl-pprint: fix the build with ghc-7.10.x

--- pkgs/development/haskell-modules/configuration-ghc-7.10.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-7.10.x.nix
@@ -125,4 +125,10 @@ self: super: {
     sha256 = "1fycvjfr1l9wa03k30bnppl3ns99lffh9kmp9r7sr8b6yiydcajq";
     stripLen = 1;
   });
+
+  # https://github.com/batterseapower/ansi-wl-pprint/issues/13
+  ansi-wl-pprint = appendPatch super.ansi-wl-pprint (pkgs.fetchpatch {
+    url = "https://github.com/hvr/ansi-wl-pprint/commit/7e489ea6b546899074b1cdccf37d2e49ab313098.patch";
+    sha256 = "111aasm6pb55prldzwzgfffd5zz1npl7akdzcwh2hsri865i1806";
+  });
 }
