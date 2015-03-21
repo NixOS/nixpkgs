aa122601c555e413e7024bedf606d7a84987a992
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2015-03-21 22:41:43 +0100

    httping: enable ncurses+openssl+fftw, update to 2.4

--- pkgs/tools/networking/httping/default.nix
+++ pkgs/tools/networking/httping/default.nix
@@ -1,16 +1,23 @@
-{ stdenv, fetchurl, gettext }:
+{ stdenv, fetchurl, gettext, openssl, ncurses, fftw }:
 
 stdenv.mkDerivation rec {
   name = "httping-${version}";
 
-  version = "2.3.4";
+  version = "2.4";
 
   src = fetchurl {
-    url = "http://www.vanheusden.com/httping/httping-2.3.4.tgz";
-    sha256 = "1hkbhdxb0phrvrddx9kcfpqlzm41xv9jvy82nfkqa7bb0v5p2qd7";
+    url = "http://www.vanheusden.com/httping/httping-2.4.tgz";
+    sha256 = "dab59f02b08bfbbc978c005bb16d2db6fe21e1fc841fde96af3d497ddfc82084";
   };
 
-  buildInputs = [ gettext ];
+  buildInputs = [ gettext openssl ncurses fftw ];
+
+  configureFlags = [
+    "--with-tfo"
+    "--with-ncurses"
+    "--with-openssl"
+    "--with-fftw3"
+  ];
 
   makeFlags = [
     "DESTDIR=$(out)"
@@ -20,7 +27,7 @@ stdenv.mkDerivation rec {
   meta = {
     homepage = "http://www.vanheusden.com/httping";
     description = "ping for HTTP requests";
-    maintainers = with stdenv.lib.maintainers; [ rickynils ];
+    maintainers = with stdenv.lib.maintainers; [ rickynils np ];
     platforms = with stdenv.lib.platforms; linux;
   };
 }
