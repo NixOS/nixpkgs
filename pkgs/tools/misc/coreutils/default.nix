68647a4de5afa4a5743e262bf62b0d7048e08a95
Eric Seidel <gridaphobe@gmail.com>
2015-03-21 09:13:06 -0700

    coreutils: don't use bare `sed` on darwin
    it will be BSD sed instead of GNU sed

--- pkgs/tools/misc/coreutils/default.nix
+++ pkgs/tools/misc/coreutils/default.nix
@@ -19,7 +19,7 @@ let
     };
 
     # The test tends to fail on btrfs and maybe other unusual filesystems.
-    postPatch = ''
+    postPatch = stdenv.lib.optionalString (!stdenv.isDarwin) ''
       sed '2i echo Skipping dd sparse test && exit 0' -i ./tests/dd/sparse.sh
     '';
 
