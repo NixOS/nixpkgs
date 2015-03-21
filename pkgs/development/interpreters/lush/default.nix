0d2a00fdeebc82e9b8c0f42df63c63a2e8a13dec
Michael Raskin <7c6f434c@mail.ru>
2015-03-21 21:22:22 +0300

    update lush

--- pkgs/development/interpreters/lush/default.nix
+++ pkgs/development/interpreters/lush/default.nix
@@ -7,7 +7,7 @@ let
     version="2.0.1";
     name="${baseName}-${version}";
     hash="02pkfn3nqdkm9fm44911dbcz0v3r0l53vygj8xigl6id5g3iwi4k";
-    url="mirror://sourceforge/project/lush/lush2/lush-2.0.1.tar.gz";
+    url="http://softlayer-ams.dl.sourceforge.net/project/lush/lush2/lush-2.0.1.tar.gz";
     sha256="02pkfn3nqdkm9fm44911dbcz0v3r0l53vygj8xigl6id5g3iwi4k";
   };
   buildInputs = [
