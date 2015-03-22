786f3216bc132a2e6d0848ff7e9dd18bde481493
William A. Kennington III <william@wkennington.com>
2015-03-22 12:20:53 -0700

    zfs_git: 2015-03-12 -> 2015-03-20

--- pkgs/os-specific/linux/zfs/git.nix
+++ pkgs/os-specific/linux/zfs/git.nix
@@ -1,12 +1,12 @@
 { callPackage, stdenv, fetchgit, spl_git, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "2015-03-12";
+  version = "2015-03-20";
 
   src = fetchgit {
     url = git://github.com/zfsonlinux/zfs.git;
-    rev = "4c7b7eedcde7fababf457ca04445e6d9d1617e29";
-    sha256 = "1v0rb4rfs48c66wihfmigc6cmqk6j5r5xlkv5d03shb9h5ypff84";
+    rev = "bc88866657979c5658441e201e19df365c67ddfe";
+    sha256 = "1d97xw7cak64f0ywwskjssnryljidf4hpngmqv0mmz4lk4hwirm9";
   };
 
   patches = [
