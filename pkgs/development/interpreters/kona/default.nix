52d77f2360140152c3d6a72bdb56ae5016ad95ea
Joachim Fasting <joachifm@fastmail.fm>
2015-03-24 20:16:23 +0100

    kona: reimplement using mkDerivation
    Also set supported platforms to `all`, per upstream, and specify
    license (ISC).

--- pkgs/development/interpreters/kona/default.nix
+++ pkgs/development/interpreters/kona/default.nix
@@ -1,50 +1,22 @@
-x@{builderDefsPackage
-  , fetchgit
-  , ...}:
-builderDefsPackage
-(a :
-let
-  helperArgNames = ["stdenv" "fetchurl" "builderDefsPackage"] ++
-    ["fetchgit"];
+{ stdenv, fetchgit }:
 
-  buildInputs = map (n: builtins.getAttr n x)
-    (builtins.attrNames (builtins.removeAttrs x helperArgNames));
-in
-rec {
-  srcDrv = a.fetchgit {
-    url = "https://github.com/kevinlawler/kona.git";
-    inherit rev;
-    sha256 = "a8189c94fca3e62a740eb6823ab3fc571c0960ff5e7a3eb0353e5cf36ef9fdcb";
-  };
-
-  src = "${srcDrv}/";
-
-  rev = "81e95b395144f4b02fe8782ad87c1f218b511c43";
-  version = "git-${rev}";
+stdenv.mkDerivation rec {
   name = "kona-${version}";
-  inherit buildInputs;
+  src = fetchgit {
+    url = "https://github.com/kevinlawler/kona.git";
+    rev = "81e95b395144f4b02fe8782ad87c1f218b511c43";
+    sha256 = "1jzxz5pg6p1y6nq3wyjyzxh0j72pzjrkm0mn1rs2mrm3zja9q658";
+  };
+  version = "git-${src.rev}";
 
-  /* doConfigure should be removed if not needed */
-  phaseNames = ["prepareOut" "doMakeInstall"];
-  makeFlags = ["PREFIX=\$out"];
+  makeFlags = "PREFIX=$(out)";
+  preInstall = ''mkdir -p "$out/bin"'';
 
-  prepareOut = a.fullDepEntry ''
-    mkdir -p "$out/bin"
-  '' ["minInit" "defEnsureDir"];
-
-  meta = {
+  meta = with stdenv.lib; {
     description = "An interpreter of K, APL-like programming language";
-    maintainers = with a.lib.maintainers;
-    [
-      raskin
-    ];
-    platforms = with a.lib.platforms;
-      linux;
-    license = a.lib.licenses.free;
+    homepage = https://github.com/kevinlawler/kona/;
+    maintainers = with maintainers; [ raskin ];
+    platforms = platforms.all;
+    license = licenses.isc;
   };
-  passthru = {
-    updateInfo = {
-      downloadPage = "https://github.com/kevinlawler/kona";
-    };
-  };
-}) x
+}
