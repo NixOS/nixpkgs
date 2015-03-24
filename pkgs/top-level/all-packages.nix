777415c64842720918fc4a87b863e51afd85beab
Jan Malakhovski <oxij@oxij.org>
2015-03-24 18:53:10 +0000

    tolua++ (toluapp): add version 1.0.92

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4482,6 +4482,10 @@ let
 
   luarocks = luaPackages.luarocks;
 
+  toluapp = callPackage ../development/tools/toluapp {
+    lua = lua5_1; # doesn't work with any other :(
+  };
+
   ### END OF LUA
 
   lush2 = callPackage ../development/interpreters/lush {};
