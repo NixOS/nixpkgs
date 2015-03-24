ad49ea230a6318f4164bc4bd395018acc61cb81a
Thomas Tuegel <ttuegel@gmail.com>
2015-03-24 17:14:32 -0500

    qt54: update to qt-5.4.1

--- pkgs/development/libraries/qt-5/5.4/manifest.sh
+++ pkgs/development/libraries/qt-5/5.4/manifest.sh
@@ -5,7 +5,7 @@ if [ $# -eq 0 ]; then
   # The extra slash at the end of the URL is necessary to stop wget
   # from recursing over the whole server! (No, it's not a bug.)
   $(nix-build ../../../../.. -A autonix.manifest) \
-    http://download.qt.io/official_releases/qt/5.4/5.4.0/submodules/ \
+    http://download.qt.io/official_releases/qt/5.4/5.4.1/submodules/ \
       -A '*.tar.xz'
 
 else
