be7ef555f26ae8d1fc45898f02a7d26b478c9c3c
Joachim Fasting <joachifm@fastmail.fm>
2015-03-24 19:44:00 +0100

    libev: port to mkDerivation

--- pkgs/development/libraries/libev/default.nix
+++ pkgs/development/libraries/libev/default.nix
@@ -1,25 +1,17 @@
-a :  
-let 
-  s = import ./src-for-default.nix;
-  buildInputs = with a; [
-    
-  ];
-in
-rec {
-  src = a.fetchUrlFromSrcInfo s;
+{ stdenv, fetchurl }:
 
-  inherit (s) name;
-  inherit buildInputs;
-  configureFlags = [];
-
-  /* doConfigure should be removed if not needed */
-  phaseNames = ["doConfigure" "doMakeInstall"];
-      
+stdenv.mkDerivation rec {
+  name = "libev-${version}";
+  version="4.15";
+  src = fetchurl {
+    url = "http://dist.schmorp.de/libev/Attic/${name}.tar.gz";
+    sha256 = "1svgc1hq4i5zsw4i02sf7xb4pk2d8kpvc1gdrd856vsmffh47pdj";
+  };
   meta = {
     description = "An event loop library remotely similar to libevent";
     maintainers = [
-      a.lib.maintainers.raskin
+      stdenv.lib.maintainers.raskin
     ];
-    platforms = a.lib.platforms.all;
+    platforms = stdenv.lib.platforms.all;
   };
 }
