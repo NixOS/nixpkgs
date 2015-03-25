164dbfff80e7d314a68b845ca4183dd485a26685
Jascha Geerds <jg@ekby.de>
2015-03-25 13:26:37 +0100

    nixos/tests/printing: Fix CUPS service name (#6767)

--- nixos/tests/printing.nix
+++ nixos/tests/printing.nix
@@ -31,8 +31,8 @@ import ./make-test.nix ({pkgs, ... }: {
       startAll;
 
       # Make sure that cups is up on both sides.
-      $server->waitForUnit("cupsd.service");
-      $client->waitForUnit("cupsd.service");
+      $server->waitForUnit("cups.service");
+      $client->waitForUnit("cups.service");
       $client->succeed("lpstat -r") =~ /scheduler is running/ or die;
       $client->succeed("lpstat -H") =~ "/var/run/cups/cups.sock" or die;
       $client->succeed("curl --fail http://localhost:631/");
