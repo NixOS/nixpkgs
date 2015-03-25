21cd86f2d9449ea2e969900bbc0324bbcad380fa
William A. Kennington III <william@wkennington.com>
2015-03-25 15:20:23 -0700

    makeInitrd: Make prepend optional

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -396,7 +396,7 @@ let
     inherit lib;
   };
 
-  makeInitrd = { contents, compressor ? "gzip -9n", prepend }:
+  makeInitrd = { contents, compressor ? "gzip -9n", prepend ? [ ] }:
     import ../build-support/kernel/make-initrd.nix {
       inherit stdenv perl perlArchiveCpio cpio contents ubootChooser compressor prepend;
     };
