151c42aa98fd26a32e130932725d25c80585ddd9
William A. Kennington III <william@wkennington.com>
2015-03-25 16:02:24 -0700

    libvorbis: 1.3.4 -> 1.3.5

--- pkgs/development/libraries/libvorbis/default.nix
+++ pkgs/development/libraries/libvorbis/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, libogg, pkgconfig }:
 
 stdenv.mkDerivation rec {
-  name = "libvorbis-1.3.4";
+  name = "libvorbis-1.3.5";
 
   src = fetchurl {
     url = "http://downloads.xiph.org/releases/vorbis/${name}.tar.xz";
-    sha256 = "0wpk87jnhngcl3nc5i39flkycx1sjzilx8jjx4zc4p8r55ylj19g";
+    sha256 = "1lg1n3a6r41492r7in0fpvzc7909mc5ir9z0gd3qh2pz4yalmyal";
   };
 
   buildInputs = [ pkgconfig ];
