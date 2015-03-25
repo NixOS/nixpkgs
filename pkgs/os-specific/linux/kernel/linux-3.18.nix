a040e15a48fe5c4e1f8ad7f50cfbc321044857e3
William A. Kennington III <william@wkennington.com>
2015-03-25 11:24:47 -0700

    kernel: 3.18.9 -> 3.18.10

--- pkgs/os-specific/linux/kernel/linux-3.18.nix
+++ pkgs/os-specific/linux/kernel/linux-3.18.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.18.9";
+  version = "3.18.10";
   extraMeta.branch = "3.18";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "00cm64s17d03rlzkgqs0fq91vm95csfa2xw4zhp7y0md9fd73dxi";
+    sha256 = "0kmh0ybjh1l35pm421v2q3z9fyhss85agh1rkmnh9bim2bq1ac6h";
   };
 
   features.iwlwifi = true;
