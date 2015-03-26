63675e8c2775932851cdccf2917c1bc11d157145
William A. Kennington III <william@wkennington.com>
2015-03-26 00:57:34 -0700

    procps: 3.2.8 -> 3.3.10

--- pkgs/os-specific/linux/procps/default.nix
+++ pkgs/os-specific/linux/procps/default.nix
@@ -1,22 +1,15 @@
 { stdenv, fetchurl, ncurses }:
 
 stdenv.mkDerivation {
-  name = "procps-3.2.8";
+  name = "procps-ng-3.3.10";
   
   src = fetchurl {
-    url = http://procps.sourceforge.net/procps-3.2.8.tar.gz;
+    url = "mirror://sourceforge/procps-ng/Production/${name}.tar.xz";
     sha256 = "0d8mki0q4yamnkk4533kx8mc0jd879573srxhg6r2fs3lkc6iv8i";
   };
-  
-  patches =
-    [ ./makefile.patch
-      ./procps-build.patch
-      ./gnumake3.82.patch
-      ./linux-ver-init.patch
-    ];
-    
+
   buildInputs = [ ncurses ];
-  
+
   makeFlags = "DESTDIR=$(out)";
 
   crossAttrs = {
@@ -26,5 +19,7 @@ stdenv.mkDerivation {
   meta = {
     homepage = http://procps.sourceforge.net/;
     description = "Utilities that give information about processes using the /proc filesystem";
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ wkennington ];
   };
 }
