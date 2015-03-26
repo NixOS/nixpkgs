ca8a5a0e597f01ebe78a1280dfb61db00d1ad181
Oliver Matthews <oliver@codersoffortune.net>
2015-03-26 17:58:26 +0100

    Add MediaTomb service

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -191,6 +191,7 @@
   ./services/misc/gitlab.nix
   ./services/misc/gitolite.nix
   ./services/misc/gpsd.nix
+  ./services/misc/mediatomb.nix
   ./services/misc/mesos-master.nix
   ./services/misc/mesos-slave.nix
   ./services/misc/nix-daemon.nix
