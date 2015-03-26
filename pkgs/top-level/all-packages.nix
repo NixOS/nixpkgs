862510079a4ac370d8e7c6015f38a049231fd640
William A. Kennington III <william@wkennington.com>
2015-03-26 13:30:45 -0700

    cups: 1.7.5 -> 2.0.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13983,7 +13983,9 @@ let
 
   beep = callPackage ../misc/beep { };
 
-  cups = callPackage ../misc/cups { libusb = libusb1; };
+  cups = callPackage ../misc/cups {
+    libusb = libusb1;
+  };
 
   cups_filters = callPackage ../misc/cups/filters.nix { };
 
