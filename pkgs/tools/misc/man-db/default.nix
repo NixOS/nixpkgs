2c8cf45772862e7e2d77086bbaaa6ac0183ea0bf
William A. Kennington III <william@wkennington.com>
2015-03-26 12:25:34 -0700

    man-db: 2.6.6 -> 2.7.1

--- pkgs/tools/misc/man-db/default.nix
+++ pkgs/tools/misc/man-db/default.nix
@@ -1,17 +1,30 @@
 { stdenv, fetchurl, pkgconfig, libpipeline, db, groff }:
  
 stdenv.mkDerivation rec {
-  name = "man-db-2.6.6";
+  name = "man-db-2.7.1";
   
   src = fetchurl {
     url = "mirror://savannah/man-db/${name}.tar.xz";
-    sha256 = "1hv6byj6sg6cp3jyf08gbmdm4pwhvd5hzmb94xl0w7prin6hzabx";
+    sha256 = "03ly0hbpgjnag576rgccanaisn7f6422q5qxrj64vyzslc2651y4";
   };
   
   buildInputs = [ pkgconfig libpipeline db groff ];
   
-  configureFlags = ''
-    --disable-setuid
+  configureFlags = [
+    "--disable-setuid"
+    "--sysconfdir=/etc"
+    "--localstatedir=/var"
+    "--with-systemdtmpfilesdir=\${out}/lib/tmpfiles.d"
+  ];
+
+  installFlags = [ "DESTDIR=\${out}" ];
+
+  postInstall = ''
+    mv $out/$out/* $out
+    DIR=$out/$out
+    while rmdir $DIR 2>/dev/null; do
+      DIR="$(dirname "$DIR")"
+    done
   '';
 
   meta = with stdenv.lib; {
