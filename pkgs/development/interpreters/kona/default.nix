933e6cc83244c974403e2bdbee63271a3c9db537
Joachim Fasting <joachifm@fastmail.fm>
2015-03-27 17:58:55 +0100

    kona: bump to 3.21
    Use release tarball rather than GIT checkout. Note: disregard the
    platform and arch embedded in the release tarball name.

--- pkgs/development/interpreters/kona/default.nix
+++ pkgs/development/interpreters/kona/default.nix
@@ -1,13 +1,12 @@
-{ stdenv, fetchgit }:
+{ stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
   name = "kona-${version}";
-  src = fetchgit {
-    url = "https://github.com/kevinlawler/kona.git";
-    rev = "81e95b395144f4b02fe8782ad87c1f218b511c43";
-    sha256 = "1jzxz5pg6p1y6nq3wyjyzxh0j72pzjrkm0mn1rs2mrm3zja9q658";
+  version = "3.21";
+  src = fetchurl {
+    url = "https://github.com/kevinlawler/kona/archive/Win.${version}-64.tar.gz";
+    sha256 = "0c1yf3idqkfq593xgqb25r2ykmfmp83zzh3q7kb8095a069gvri3";
   };
-  version = "git-${src.rev}";
 
   makeFlags = "PREFIX=$(out)";
   preInstall = ''mkdir -p "$out/bin"'';
