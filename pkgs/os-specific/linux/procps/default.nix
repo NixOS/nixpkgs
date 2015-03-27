23c73808ac87bb5701d77ba0aa3717decdeb33ac
William A. Kennington III <william@wkennington.com>
2015-03-27 10:52:14 -0700

    Revert "procps: 3.2.8 -> 3.3.10"
    This reverts commit 63675e8c2775932851cdccf2917c1bc11d157145.
    
    This should be the old version.

--- pkgs/os-specific/linux/procps/default.nix
+++ pkgs/os-specific/linux/procps/default.nix
@@ -1,15 +1,22 @@
 { stdenv, fetchurl, ncurses }:
 
 stdenv.mkDerivation {
-  name = "procps-ng-3.3.10";
+  name = "procps-3.2.8";
   
   src = fetchurl {
-    url = "mirror://sourceforge/procps-ng/Production/${name}.tar.xz";
+    url = http://procps.sourceforge.net/procps-3.2.8.tar.gz;
     sha256 = "0d8mki0q4yamnkk4533kx8mc0jd879573srxhg6r2fs3lkc6iv8i";
   };
-
+  
+  patches =
+    [ ./makefile.patch
+      ./procps-build.patch
+      ./gnumake3.82.patch
+      ./linux-ver-init.patch
+    ];
+    
   buildInputs = [ ncurses ];
-
+  
   makeFlags = "DESTDIR=$(out)";
 
   crossAttrs = {
@@ -19,7 +26,5 @@ stdenv.mkDerivation {
   meta = {
     homepage = http://procps.sourceforge.net/;
     description = "Utilities that give information about processes using the /proc filesystem";
-    platforms = platforms.linux;
-    maintainers = with maintainers; [ wkennington ];
   };
 }
