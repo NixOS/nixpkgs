6644bca846a8b67fe2ba4c3a9de51009e84c37fa
Peter Simons <simons@cryp.to>
2015-03-31 13:53:36 +0200

    idris: fix build by providing appropriate versions of the build inputs
    Resolves https://github.com/NixOS/nixpkgs/issues/7014.

--- pkgs/development/haskell-modules/configuration-ghc-7.8.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-7.8.x.nix
@@ -46,12 +46,14 @@ self: super: {
   # Idris requires mtl 2.2.x.
   idris = overrideCabal (super.idris.overrideScope (self: super: {
     mkDerivation = drv: super.mkDerivation (drv // { doCheck = false; });
+    blaze-markup = self.blaze-markup_0_6_2_0;
+    blaze-html = self.blaze-html_0_7_0_3;
+    haskeline = self.haskeline_0_7_2_1;
+    lens = self.lens_4_7;
+    mtl = super.mtl_2_2_1;
     transformers = super.transformers_0_4_3_0;
     transformers-compat = disableCabalFlag super.transformers-compat "three";
-    haskeline = self.haskeline_0_7_2_1;
-    mtl = super.mtl_2_2_1;
   })) (drv: {
-    jailbreak = true;           # idris is scared of lens 4.7
     patchPhase = "find . -name '*.hs' -exec sed -i -s 's|-Werror||' {} +";
   });                           # warning: "Module ‘Control.Monad.Error’ is deprecated"
 
