0af0b2e067f357c6b6aba19077f64ac31455f839
Nikolay Amiantov <ab@fmap.me>
2015-04-02 14:45:00 +0300

    emacsPackagesNg.evil: update url

--- pkgs/top-level/emacs-packages.nix
+++ pkgs/top-level/emacs-packages.nix
@@ -372,7 +372,7 @@ let self = _self // overrides;
     pname   = "evil";
     version = "20141020";
     src = fetchgit {
-      url = "https://gitorious.org/evil/evil.git";
+      url = "https://github.com/emacsmirror/evil.git";
       rev = "999ec15587f85100311c031aa8efb5d50c35afe4";
       sha256 = "5f67643d19a31172e68f2f195959d33bcd26c2786eb71e67eb27eb52f5bf387a";
     };
