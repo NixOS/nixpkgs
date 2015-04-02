1b338edc80bb1a6eceb4ee59b3a818950ee8fdcf
Austin Seipp <aseipp@pobox.com>
2015-04-02 16:13:03 -0500

    nixpkgs: musl 1.1.7 -> 1.1.8
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/os-specific/linux/musl/default.nix
+++ pkgs/os-specific/linux/musl/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name    = "musl-${version}";
-  version = "1.1.7";
+  version = "1.1.8";
 
   src = fetchurl {
     url    = "http://www.musl-libc.org/releases/${name}.tar.gz";
-    sha256 = "168mz5mwmzr5j6l4jxnwaxw6l89xycql3vfk01jsmy7chziamq6q";
+    sha256 = "04vq4a1hm81kbxfcqa30s6xpzbqf3568gbysfxcmb72v8438b4ps";
   };
 
   enableParallelBuilding = true;
