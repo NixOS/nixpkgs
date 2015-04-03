c76f9542472984e92fd054429316da937fadd85c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-04-03 11:40:06 +0200

    nixUnstable: Update to 1.9pre4087
    (cherry picked from commit f713e7033e963306e62471c5ed5dffb38ca9bed6)

--- pkgs/tools/package-management/nix/unstable.nix
+++ pkgs/tools/package-management/nix/unstable.nix
@@ -5,11 +5,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "nix-1.9pre4083_5114a07";
+  name = "nix-1.9pre4087_afa433e";
 
   src = fetchurl {
-    url = "http://hydra.nixos.org/build/20650421/download/4/${name}.tar.xz";
-    sha256 = "971fdd36bcf39c7e6ce9ef12dbfe09c98d2be3275e482ca2dbacb2e668f0dff9";
+    url = "http://hydra.nixos.org/build/21053128/download/4/${name}.tar.xz";
+    sha256 = "05dda3644ed68364bbff87d0ec662ca18d8542f921683cb2e1f308281d6bf384";
   };
 
   nativeBuildInputs = [ perl pkgconfig ];
