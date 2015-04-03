d2358a6e538e52a34680ff4e67256d4ff2ead88c
Markus Kohlhase <mail@markus-kohlhase.de>
2015-04-03 10:52:40 +0000

    nixpkgs: added old stable nodejs v0.10.38

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1805,6 +1805,7 @@ let
 
   nodejs = callPackage ../development/web/nodejs { libuv = libuvVersions.v1_2_0; };
   nodejs-unstable = callPackage ../development/web/nodejs { libuv = libuvVersions.v1_2_0; unstableVersion = true; };
+  nodejs-0_10 = callPackage ../development/web/nodejs/v0_10.nix { };
 
   nodePackages = callPackage ./node-packages.nix { self = nodePackages; };
 
