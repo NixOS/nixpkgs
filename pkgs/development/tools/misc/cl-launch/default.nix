25317cf30c3be2d57fcc1e606a12567be0df6e94
Michael Raskin <7c6f434c@mail.ru>
2015-04-04 23:54:20 +0300

    Update cl-launch

--- pkgs/development/tools/misc/cl-launch/default.nix
+++ pkgs/development/tools/misc/cl-launch/default.nix
@@ -3,11 +3,11 @@ let
   s = # Generated upstream information
   rec {
     baseName="cl-launch";
-    version="4.1.1";
+    version="4.1.2";
     name="${baseName}-${version}";
-    hash="1nimbv1ms7fcikx8y6dxrzdm63psf4882z5kjr6qdyarqz6gaq20";
-    url="http://common-lisp.net/project/xcvb/cl-launch/cl-launch-4.1.1.tar.gz";
-    sha256="1nimbv1ms7fcikx8y6dxrzdm63psf4882z5kjr6qdyarqz6gaq20";
+    hash="13fgcvg71s1yp3r7jf1cs3kkpfw0pwykgmj7zryh24mw2269rx90";
+    url="http://common-lisp.net/project/xcvb/cl-launch/cl-launch-4.1.2.tar.gz";
+    sha256="13fgcvg71s1yp3r7jf1cs3kkpfw0pwykgmj7zryh24mw2269rx90";
   };
   buildInputs = [
   ];
