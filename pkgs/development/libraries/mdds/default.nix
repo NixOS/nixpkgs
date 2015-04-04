7d80aed6e206ba795a85fc44213425b883c325e6
Michael Raskin <7c6f434c@mail.ru>
2015-04-05 00:49:07 +0300

    Update mdds

--- pkgs/development/libraries/mdds/default.nix
+++ pkgs/development/libraries/mdds/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  version = "0.11.2";
+  version = "0.12.0";
   name = "mdds-${version}";
 
   src = fetchurl {
     url = "http://kohei.us/files/mdds/src/mdds_${version}.tar.bz2";
-    sha256 = "1ax50ahgl549gw76p8kbd5cb0kkihrn59638mppq4raxng40s2nd";
+    sha256 = "10ar7r0gkdl2r7916jlkl5c38cynrh7x9s90a5i8d242r8ixw8ia";
   };
 
   postInstall = ''
