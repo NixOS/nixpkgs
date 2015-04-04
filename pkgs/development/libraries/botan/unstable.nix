e4ce4e9f5b6491551493356fcdb33250edd56293
Michael Raskin <7c6f434c@mail.ru>
2015-04-04 23:54:32 +0300

    Update unstable Botan

--- pkgs/development/libraries/botan/unstable.nix
+++ pkgs/development/libraries/botan/unstable.nix
@@ -2,7 +2,7 @@
 
 callPackage ./generic.nix (args // {
   baseVersion = "1.11";
-  revision = "15";
-  sha256 = "1rkv84v09llbxyvh33szi7zsjm19l02j7h60n9g7jhhc2w667jk0";
+  revision = "16";
+  sha256 = "0z3a7jp10w9ipmbzhc2xazd2savxmns57ca2a8d6vvjahxg4w6m3";
   openssl = null;
 })
