34e0ccd84b0667dd43de043c9d6b1735bee9f658
Andrey Arapov <arno@nixaid.com>
2015-04-05 19:06:36 +0200

    php: Update to 5.4.39

--- pkgs/development/interpreters/php/5.4.nix
+++ pkgs/development/interpreters/php/5.4.nix
@@ -9,7 +9,7 @@ in
 
 composableDerivation.composableDerivation {} ( fixed : let inherit (fixed.fixed) version; in {
 
-  version = "5.4.37";
+  version = "5.4.39";
 
   name = "php-${version}";
 
@@ -253,7 +253,7 @@ composableDerivation.composableDerivation {} ( fixed : let inherit (fixed.fixed)
 
   src = fetchurl {
     url = "http://www.php.net/distributions/php-${version}.tar.bz2";
-    sha256 = "1bd8yciy13v3aw4aiw57zpf8kgxpm6rcv73grp1yj2pbbrkzcyw5";
+    sha256 = "0znpd6pgri5vah4j4wwamhqc60awila43bhh699p973hir9pdsvw";
   };
 
   meta = {
