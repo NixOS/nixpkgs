4cafe9c714f703c39949377d7d0dd1e46e777b8c
Andrey Arapov <arno@nixaid.com>
2015-04-05 23:54:08 +0200

    opendkim and libmilter: hardening with Canary, PIE, Full RELRO and FORTIFY_SOURCE

--- pkgs/development/libraries/opendkim/default.nix
+++ pkgs/development/libraries/opendkim/default.nix
@@ -7,6 +7,12 @@ stdenv.mkDerivation rec {
     sha256 = "01h97h012gcp8rimjbc9mrv4759cnw4flb42ddiady1bmb2p7vy3";
   };
 
+  preConfigure = ''
+    configureFlagsArray=(
+      CFLAGS="-fPIE -fstack-protector-all --param ssp-buffer-size=4 -O2 -D_FORTIFY_SOURCE=2" LDFLAGS="-pie -Wl,-z,relro,-z,now"
+    )
+  '';
+
   configureFlags="--with-openssl=${openssl} --with-milter=${libmilter}";
 
   buildInputs = [openssl libmilter];
