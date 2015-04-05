5a91f9a34db5c4271ac58410672d390c3253c120
AndersonTorres <torres.anderson.85@gmail.com>
2015-04-05 20:58:09 -0300

    Stella: 4.2 -> 4.6

--- pkgs/misc/emulators/stella/default.nix
+++ pkgs/misc/emulators/stella/default.nix
@@ -1,19 +1,20 @@
 { stdenv, fetchurl, pkgconfig, SDL2 }:
 
+with stdenv.lib;
 stdenv.mkDerivation rec {
 
   name = "stella-${version}";
-  version = "4.2";
+  version = "4.6";
 
   src = fetchurl {
     url = "http://downloads.sourceforge.net/project/stella/stella/${version}/${name}-src.tar.gz";
-    sha256 = "0aikd7l5ill0vl5q06dg1wly8v9vav7wd78yjm6rmikd0cd2irzh";
+    sha256 = "03vg8cxr0hn99vrr2dcwhv610xi9vhlw08ypazpm0nny522a9j4d";
   };
 
   buildInputs = with stdenv.lib;
   [ pkgconfig SDL2 ];
 
-  meta = with stdenv.lib; {
+  meta = {
     description = "An open-source Atari 2600 VCS emulator";
     longDescription = ''
     Stella is a multi-platform Atari 2600 VCS emulator released under
