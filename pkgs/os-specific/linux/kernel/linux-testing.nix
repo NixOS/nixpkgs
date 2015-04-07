22bb53dfe2c2eedfc593120f19ce8fa7ba8f11a8
William A. Kennington III <william@wkennington.com>
2015-04-06 18:58:40 -0700

    linux-testing: 4.0-rc6 -> 4.0-rc7

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.0-rc6";
-  modDirVersion = "4.0.0-rc6";
+  version = "4.0-rc7";
+  modDirVersion = "4.0.0-rc7";
   extraMeta.branch = "4.0";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/testing/linux-${version}.tar.xz";
-    sha256 = "1f6xqrc9lqssr2gyzd6d82dk2niikbr1swg68vfc250am0l55vw8";
+    sha256 = "1261p44zmsaq7gf08b8sd9xng2y46d4v7jyfipjlgrrmlkyfgqki";
   };
 
   features.iwlwifi = true;
