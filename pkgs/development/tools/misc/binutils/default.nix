91221f5941a7a23807eaf90d4c5f35f11521a175
William A. Kennington III <william@wkennington.com>
2015-04-08 12:14:08 -0700

    Revert "binutils: 2.23.1 -> 2.25"
    This reverts commit 4adae43646b2e2a7608238b199f280f856f82271.
    
    This breaks the building of samba and likely other binaries as some
    libraries are never linked.

--- pkgs/development/tools/misc/binutils/default.nix
+++ pkgs/development/tools/misc/binutils/default.nix
@@ -4,7 +4,7 @@
 
 assert !stdenv.isDarwin;
 
-let basename = "binutils-2.25"; in
+let basename = "binutils-2.23.1"; in
 
 with { inherit (stdenv.lib) optional optionals optionalString; };
 
@@ -13,7 +13,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnu/binutils/${basename}.tar.bz2";
-    sha256 = "08r9i26b05zcwb9zxb6zllpfdiiicdfsgbpsjlrjmvx3rxjzrpi2";
+    sha256 = "06bs5v5ndb4g5qx96d52lc818gkbskd1m0sz57314v887sqfbcia";
   };
 
   patches = [
@@ -33,7 +33,7 @@ stdenv.mkDerivation rec {
     # Always add PaX flags section to ELF files.
     # This is needed, for instance, so that running "ldd" on a binary that is
     # PaX-marked to disable mprotect doesn't fail with permission denied.
-    ./pt-pax-flags-20141022.patch
+    ./pt-pax-flags-20121023.patch
   ];
 
   nativeBuildInputs = optional gold bison;
