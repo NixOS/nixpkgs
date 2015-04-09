758c8e5c5df835525cb175ade03ee1f9b9a82510
William A. Kennington III <william@wkennington.com>
2015-04-09 13:20:02 -0700

    spl: 0.6.3 -> 0.6.4

--- pkgs/os-specific/linux/spl/git.nix
+++ pkgs/os-specific/linux/spl/git.nix
@@ -9,5 +9,5 @@ callPackage ./generic.nix (args // rec {
     sha256 = "0wq1raz68b9msbn00q1zg89rm5l7l2018k7m31s4b4gj17w38h5b";
   };
 
-  patches = [ ./const.patch ./install_prefix-git.patch ];
+  patches = [ ./const.patch ./install_prefix.patch ];
 })
