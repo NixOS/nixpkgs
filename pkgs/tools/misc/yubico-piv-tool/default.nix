4e51a466bdfa260d15fbaafe52cdccf5280f13d1
Moritz Ulrich <moritz@tarn-vedra.de>
2015-04-09 12:54:32 +0200

    Yubico Suite: Update to latest releases.
    yubico-piv-tool               -> 0.1.6
    yubikey-personalization       -> 1.17.1
    yubikey-personalization-gui-3 -> 3.1.20

--- pkgs/tools/misc/yubico-piv-tool/default.nix
+++ pkgs/tools/misc/yubico-piv-tool/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, openssl, pcsclite }:
 
 stdenv.mkDerivation rec {
-  name = "yubico-piv-tool-0.1.5";
+  name = "yubico-piv-tool-0.1.6";
 
   src = fetchurl {
     url = "https://developers.yubico.com/yubico-piv-tool/Releases/${name}.tar.gz";
-    sha256 = "1zii90f0d1j9cinvxqlzs9h8w7a856ksd8ghgqz12jywmjz0blxq";
+    sha256 = "1fsr7wkbg2ff9igc3d8bfqijdxj88wpfnj7zlidzgzakj4b6bxzg";
   };
 
   buildInputs = [ pkgconfig openssl pcsclite ];
