d923a6eab511c316867d0bc75e9bfd46ae8ccb8f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-04-10 16:11:07 +0200

    Make environment.checkConfigurationOptions an alias
    NixOps uses it, and we can't change it there for
    backward-compatibility reasons.

--- nixos/modules/rename.nix
+++ nixos/modules/rename.nix
@@ -135,7 +135,7 @@ in zipModules ([]
 
 ++ obsolete [ "services" "mysql55" ] [ "services" "mysql" ]
 
-++ obsolete [ "environment" "checkConfigurationOptions" ] [ "_module" "check" ]
+++ alias    [ "environment" "checkConfigurationOptions" ] [ "_module" "check" ]
 
 # XBMC
 ++ obsolete [ "services" "xserver" "windowManager" "xbmc" ] [ "services" "xserver" "desktopManager" "kodi" ]
