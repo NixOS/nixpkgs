73815725a0d494b37aa0eeb2128cdce86792de12
William A. Kennington III <william@wkennington.com>
2015-04-09 18:01:12 -0700

    umurmur: 0.2.15 -> 0.2.16

--- pkgs/applications/networking/umurmur/default.nix
+++ pkgs/applications/networking/umurmur/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "umurmur-${version}";
-  version = "0.2.15";
+  version = "0.2.16";
   
   src = fetchFromGitHub {
     owner = "fatbob313";
     repo = "umurmur";
     rev = version;
-    sha256 = "0q0apnnb3pszhpsbadw52k6mhdc0hk38rk7vnn7dl4fsisfhgmx2";
+    sha256 = "0njvdqvjda13v1a2yyjn47mb0l0cdfb2bfvb5s13wpgwy2xxk0px";
   };
   
   buildInputs = [ autoreconfHook openssl protobufc libconfig ];
