2a10294b74f200125f630c652da84fa228a7f61b
Dmitry Belyaev <be.dmitry@gmail.com>
2015-04-13 15:55:02 +1000

    Update rustcMaster

--- pkgs/development/compilers/rustc/head.nix
+++ pkgs/development/compilers/rustc/head.nix
@@ -2,15 +2,15 @@
 callPackage ./makeRustcDerivation.nix {
   shortVersion = "1.0.0-dev";
   isRelease = false;
-  # src rev for master on 2015/03/09
-  srcRev = "91bdf23f504f79ed59617cde3dfebd3d5e39a476";
-  srcSha = "1s1v1q630d88idz331n4m0c3ninzipmvfzmkvdaqqm28wsn69xx7";
-  snapshotHashLinux686 = "50a47ef247610fb089d2c4f24e4b641eb0ba4afb";
-  snapshotHashLinux64 = "ccb20709b3c984f960ddde996451be8ce2268d7c";
-  snapshotHashDarwin686 = "ad263bdeadcf9bf1889426e0c1391a7cf277364e";
-  snapshotHashDarwin64 = "01c8275828042264206b7acd8e86dc719a2f27aa";
-  snapshotDate = "2015-03-07";
-  snapshotRev = "270a677";
+  # src rev for master on 2015/04/13
+  srcRev = "0cf99c3e06e84d20d68da649c888d63c72f33971";
+  srcSha = "0brnzsbxmidjnmvi36sz582k3kw6wk813y2y837zpmyxg9fjah0l";
+  snapshotHashLinux686 = "1ef82402ed16f5a6d2f87a9a62eaa83170e249ec";
+  snapshotHashLinux64 = "ef2154372e97a3cb687897d027fd51c8f2c5f349";
+  snapshotHashDarwin686 = "0310b1a970f2da7e61770fd14dbbbdca3b518234";
+  snapshotHashDarwin64 = "5f35d9c920b8083a7420ef8cf5b00d5ef3085dfa";
+  snapshotDate = "2015-03-27";
+  snapshotRev = "5520801";
   patches = [
     ./patches/head.patch
   ] ++ stdenv.lib.optional stdenv.needsPax ./patches/grsec.patch;
