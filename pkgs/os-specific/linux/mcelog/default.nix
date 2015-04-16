2123be342d97ad48a1e211f270bc65cf481cee86
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-04-17 00:42:45 +0200

    mcelog 1.0pre-7fa9981 -> 114

--- pkgs/os-specific/linux/mcelog/default.nix
+++ pkgs/os-specific/linux/mcelog/default.nix
@@ -1,37 +1,27 @@
-{ stdenv, fetchgit }:
+{ stdenv, fetchFromGitHub }:
 
-# Shows the machine check exceptions logged by the kernel.
-# E.g. a log is generated when intel processors cpu-throttle.
-
-# The releases of this package are no longer on kernel.org
-# hence we fetch them from github. Apparently, these
-# are also more recent.
-
-let
-
-  rev = "7fa99818367a6d17014b36d6f918ad848cbe7ce2";
-  version = "1.0pre-${stdenv.lib.strings.substring 0 7 rev}"; 
-  sha256 = "15eea3acd76190c7922c71028b31963221a2eefd8afa713879e191a26bc22ae7";
-
-in stdenv.mkDerivation {
-
+let version = "114"; in
+stdenv.mkDerivation {
   name = "mcelog-${version}";
 
-  src = fetchgit {
-    url = "https://github.com/andikleen/mcelog";
-    inherit sha256;
-    inherit rev;
+  src = fetchFromGitHub {
+    sha256 = "1blxz5ilrlh2030gxmfqlhcb53qh2bxp5nxyc97m1z8a52idjh0v";
+    rev = "v${version}";
+    repo = "mcelog";
+    owner = "andikleen";
   };
 
   makeFlags = "prefix=$(out) etcprefix=$(out) DOCDIR=$(out)/share/doc";
 
   preInstall = ''
-    mkdir -p $out/share/doc
+    mkdir -p $out/share/{doc,man/man{5,8}}
   '';
 
-  meta = {
-    description = "Tool to display logged machine check exceptions";
+  meta = with stdenv.lib; {
+    inherit version;
+    description = "Log machine checks (memory, IO, and CPU hardware errors)";
     homepage = http://mcelog.org/;
-    license = stdenv.lib.licenses.gpl2;
+    license = with licenses; gpl2;
+    maintainers = with maintainers; [ nckx ];
   };
 }
