a579886b2b04908bb0bc95b751bf0e7e3ffeb59d
Joachim Fasting <joachifm@fastmail.fm>
2015-04-17 22:11:03 +0200

    vue: reimplement using mkDerivation

--- pkgs/applications/misc/vue/default.nix
+++ pkgs/applications/misc/vue/default.nix
@@ -1,50 +1,27 @@
-x@{builderDefsPackage
-  , jre, unzip
-  , ...}:
-builderDefsPackage
-(a :
-let
-  helperArgNames = ["stdenv" "fetchurl" "builderDefsPackage"] ++
-    [];
+{ stdenv, fetchurl, jre }:
 
-  buildInputs = map (n: builtins.getAttr n x)
-    (builtins.attrNames (builtins.removeAttrs x helperArgNames));
-  sourceInfo = rec {
-    baseName="vue";
-    version="3.2.2";
-    name="${baseName}-${version}";
-    url="releases.atech.tufts.edu/jenkins/job/VUE/64/deployedArtifacts/download/artifact.2";
-    hash="0sb1kgan8fvph2cqfxk3906cwx5wy83zni2vlz4zzi6yg4zvfxld";
-  };
-in
-rec {
-  src = a.fetchurl {
-    url = sourceInfo.url;
-    sha256 = sourceInfo.hash;
+stdenv.mkDerivation rec {
+  name = "vue-${version}";
+  version = "3.2.2";
+  src = fetchurl {
+    url = "releases.atech.tufts.edu/jenkins/job/VUE/64/deployedArtifacts/download/artifact.2";
+    sha256 = "0sb1kgan8fvph2cqfxk3906cwx5wy83zni2vlz4zzi6yg4zvfxld";
   };
 
-  inherit (sourceInfo) name version;
-  inherit buildInputs;
+  phases = "installPhase";
 
-  /* doConfigure should be removed if not needed */
-  phaseNames = ["doDeploy"];
-
-  doDeploy = a.fullDepEntry ''
+  installPhase = ''
     mkdir -p "$out"/{share/vue,bin}
     cp ${src} "$out/share/vue/vue.jar"
-    echo '#!${a.stdenv.shell}' >> "$out/bin/vue"
-    echo '${a.jre}/bin/java -jar "'"$out/share/vue/vue.jar"'" "$@"' >> "$out/bin/vue"
+    echo '#!${stdenv.shell}' >> "$out/bin/vue"
+    echo '${jre}/bin/java -jar "'"$out/share/vue/vue.jar"'" "$@"' >> "$out/bin/vue"
     chmod a+x "$out/bin/vue"
-  '' ["addInputs" "defEnsureDir"];
+  '';
 
   meta = {
     description = "Visual Understanding Environment - mind mapping software";
-    maintainers = with a.lib.maintainers;
-    [
-      raskin
-    ];
-    platforms = with a.lib.platforms;
-      linux;
-    license = a.lib.licenses.free; # Apache License fork, actually
+    maintainers = with stdenv.lib.maintainers; [ raskin ];
+    platforms = with stdenv.lib.platforms; linux;
+    license = stdenv.lib.licenses.free; # Apache License fork, actually
   };
-}) x
+}
