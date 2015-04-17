2f9e95743f4effe047c7ec2458f12ae1b5c2b794
Luca Bruno <lethalman88@gmail.com>
2015-04-17 18:09:03 +0200

    caribou: fix typelib .so path

--- pkgs/desktops/gnome-3/3.16/core/caribou/default.nix
+++ pkgs/desktops/gnome-3/3.16/core/caribou/default.nix
@@ -1,5 +1,5 @@
-{ fetchurl, stdenv, pkgconfig, gnome3, clutter, dbus, pythonPackages, libxml2
-, libxklavier, libXtst, gtk2, intltool, libxslt, at_spi2_core }:
+{ fetchurl, stdenv, pkgconfig, gnome3, clutter, dbus, pythonPackages, libxml2, autoconf
+, libxklavier, libXtst, gtk2, intltool, libxslt, at_spi2_core, automake114x }:
 
 let
   majorVersion = "0.4";
@@ -13,13 +13,14 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = with gnome3;
-    [ glib pkgconfig gtk clutter at_spi2_core dbus pythonPackages.python pythonPackages.pygobject3
-      libxml2 libXtst gtk2 intltool libxslt ];
+    [ glib pkgconfig gtk clutter at_spi2_core dbus pythonPackages.python automake114x
+      pythonPackages.pygobject3 libxml2 libXtst gtk2 intltool libxslt autoconf ];
 
   propagatedBuildInputs = [ gnome3.libgee libxklavier ];
 
   preBuild = ''
     patchShebangs .
+    substituteInPlace libcaribou/Makefile.am --replace "--shared-library=libcaribou.so.0" "--shared-library=$out/lib/libcaribou.so.0"
   '';
 
   meta = with stdenv.lib; {
