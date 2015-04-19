3b8ae5cedce831fd971abd5ac7742bb1a719b5dd
Peter Simons <simons@cryp.to>
2015-04-19 22:12:18 +0200

    darcs: use build from GHC 7.8.4 package set until 7.10.1 compiles this tool

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10262,7 +10262,7 @@ let
 
   d4x = callPackage ../applications/misc/d4x { };
 
-  darcs = haskell-ng.lib.overrideCabal haskellngPackages.darcs (drv: {
+  darcs = haskell-ng.lib.overrideCabal haskell-ng.packages.ghc784.darcs (drv: {
     configureFlags = (stdenv.lib.remove "-flibrary" drv.configureFlags or []) ++ ["-f-library"];
     enableSharedExecutables = false;
     isLibrary = false;
