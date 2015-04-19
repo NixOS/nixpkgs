5c3b8840b33726b880d873e52438da8638ce95c6
Cillian de Róiste <cillian.deroiste@gmail.com>
2015-04-19 15:34:57 +0200

    qtractor: add LV2 plugin support and upgrade 0.6.5 -> 0.6.6
    fixes 7384

--- pkgs/applications/audio/qtractor/default.nix
+++ pkgs/applications/audio/qtractor/default.nix
@@ -1,20 +1,22 @@
 { alsaLib, autoconf, automake, dssi, fetchurl, gtk, jack2
 , ladspaH, ladspaPlugins, liblo, libmad, libsamplerate, libsndfile
-, libtool, libvorbis, pkgconfig, qt4, rubberband, stdenv }:
+, libtool, libvorbis, lilv, lv2, pkgconfig, qt4, rubberband, serd
+, sord, sratom, stdenv, suil }:
 
 stdenv.mkDerivation rec {
-  version = "0.6.5";
+  version = "0.6.6";
   name = "qtractor-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/qtractor/${name}.tar.gz";
-    sha256 = "0znhm0p9azknmhga6m0qp01qaiahlnfzxya1jf9r05jn9hx5lzf0";
+    sha256 = "1n70hs4bx4hq3cp2p35jq5vlcans4fk2c35w72244vlqlajx05c0";
   };
 
   buildInputs =
     [ alsaLib autoconf automake dssi gtk jack2 ladspaH
       ladspaPlugins liblo libmad libsamplerate libsndfile libtool
-      libvorbis pkgconfig qt4 rubberband
+      libvorbis lilv lv2 pkgconfig qt4 rubberband serd sord sratom
+      suil
     ];
 
   meta = with stdenv.lib; {
