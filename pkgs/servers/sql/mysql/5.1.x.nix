37499dcaaf3af10e7d351138a2ecb76e5fbcbdbe
Joachim Schiele <js@lastlog.de>
2015-04-20 15:12:44 +0200

    mysql-5.1.x: add net-tools dependency

--- pkgs/servers/sql/mysql/5.1.x.nix
+++ pkgs/servers/sql/mysql/5.1.x.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, ps, ncurses, zlib, perl, openssl }:
+{ stdenv, fetchurl, ps, ncurses, zlib, perl, openssl, net-tools }:
 
 # Note: zlib is not required; MySQL can use an internal zlib.
 
