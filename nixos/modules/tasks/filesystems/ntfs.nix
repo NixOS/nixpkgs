d4d6b8cc61562e93005c692136a0da8080d27b8b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-04-20 14:24:42 +0200

    Grmbl

--- nixos/modules/tasks/filesystems/ntfs.nix
+++ nixos/modules/tasks/filesystems/ntfs.nix
@@ -2,12 +2,6 @@
 
 with lib;
 
-let
-
-  inInitrd = any (fs: fs == "ntfs") config.boot.initrd.supportedFilesystems;
-
-in
-
 {
   config = mkIf (any (fs: fs == "ntfs" || fs == "ntfs-3g") config.boot.supportedFilesystems) {
 
