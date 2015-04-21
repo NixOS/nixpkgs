6b9d2efcb1915141cdddc816cc235ce8f98ae127
William A. Kennington III <william@wkennington.com>
2015-04-20 17:50:39 -0700

    unbound: 1.5.1 -> 1.5.3

--- pkgs/tools/networking/unbound/default.nix
+++ pkgs/tools/networking/unbound/default.nix
@@ -2,30 +2,24 @@
 
 stdenv.mkDerivation rec {
   name = "unbound-${version}";
-  version = "1.5.1";
+  version = "1.5.3";
 
   src = fetchurl {
     url = "http://unbound.net/downloads/${name}.tar.gz";
-    sha256 = "1v00k4b6m9wk0533s2jpg4rv9lhplh7zdp6vx2yyrmrbzc4jgy0g";
+    sha256 = "1jly2apag4yg649w3flaq73wdrcfyxnhx5py9j73y7adxmswigbn";
   };
 
-  buildInputs = [openssl expat libevent];
-
-  patches = [
-    # This patch fixes unbound 1.5.1 on linux versions previous to 3.15 which
-    # do not implement IP_PMTUDISC_OMIT.
-    #
-    # It should be discarded when this support makes it into a released
-    # version.
-    ./linux-pre-3.15-unbound-1.5.1.patch
-  ];
+  buildInputs = [ openssl expat libevent ];
 
   configureFlags = [
     "--with-ssl=${openssl}"
     "--with-libexpat=${expat}"
     "--with-libevent=${libevent}"
     "--localstatedir=/var"
+    "--sysconfdir=/etc"
   ];
+
+  installFlags = [ "configfile=\${out}/etc/unbound/unbound.conf" ];
 
   meta = {
     description = "Validating, recursive, and caching DNS resolver";
