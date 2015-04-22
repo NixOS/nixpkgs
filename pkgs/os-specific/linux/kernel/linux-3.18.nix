37ab5f0ad0d5576fee14332b39340cfed2d136d8
William A. Kennington III <william@wkennington.com>
2015-04-22 14:49:43 -0700

    kernel: 3.18.11 -> 3.18.12

--- pkgs/os-specific/linux/kernel/linux-3.18.nix
+++ pkgs/os-specific/linux/kernel/linux-3.18.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.18.11";
+  version = "3.18.12";
   extraMeta.branch = "3.18";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "19di7k38adnwimxddd1v6flgdsvxhgf8iswjwfyqi2p2bdcb0p5d";
+    sha256 = "06wfgg00vc5a2vvmg158ipbmigx803hdp3lhf4kv25p4sdmvbsl2";
   };
 
   # FIXME: remove with the next point release.
