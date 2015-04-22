fb235a871aba61cebd098e22ac84b9a9061d69b3
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-04-23 01:48:21 +0200

    mcelog 114 -> 115: major path fixes
    Both $out/bin/mcelog and $out/etc/mcelog/mcelog.conf now point to
    the correct $out/etc/mcelog.

--- pkgs/os-specific/linux/mcelog/default.nix
+++ pkgs/os-specific/linux/mcelog/default.nix
@@ -1,21 +1,24 @@
 { stdenv, fetchFromGitHub }:
 
-let version = "114"; in
+let version = "115"; in
 stdenv.mkDerivation {
   name = "mcelog-${version}";
 
   src = fetchFromGitHub {
-    sha256 = "1blxz5ilrlh2030gxmfqlhcb53qh2bxp5nxyc97m1z8a52idjh0v";
+    sha256 = "13m9y4xfd3klzj2xrwwwwg31pnjfwd0rbrr2845sf557iyqrshki";
     rev = "v${version}";
     repo = "mcelog";
     owner = "andikleen";
   };
 
-  makeFlags = "prefix=$(out) etcprefix=$(out) DOCDIR=$(out)/share/doc";
-
-  preInstall = ''
-    mkdir -p $out/share/{doc,man/man{5,8}}
+  postPatch = ''
+    for i in mcelog.conf paths.h; do
+      substituteInPlace $i --replace /etc $out/etc
+    done
+    touch mcelog.conf.5 # avoid regeneration requiring Python
   '';
+
+  installFlags = "DESTDIR=$(out) prefix= DOCDIR=/share/doc";
 
   meta = with stdenv.lib; {
     inherit version;
