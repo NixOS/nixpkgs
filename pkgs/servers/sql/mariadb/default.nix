cf46c88a8dd2698923df8e05b808338a537b0c26
Vladimír Čunát <vcunat@gmail.com>
2015-04-23 20:12:50 +0200

    mariadb: remove ~250MB of superfluous files
    Picked lines from master, discussion:
    https://github.com/NixOS/nixpkgs/issues/7117#issuecomment-95657579
    
    The output is still ~190 MB, but it's much better.
    On master there's a splitting solution anyway.

--- pkgs/servers/sql/mariadb/default.nix
+++ pkgs/servers/sql/mariadb/default.nix
@@ -28,6 +28,13 @@ stdenv.mkDerivation rec {
   postInstall = ''
     substituteInPlace $out/bin/mysql_install_db \
       --replace basedir=\"\" basedir=\"$out\"
+
+    # Remove superfluous files
+    rm -r $out/mysql-test $out/sql-bench $out/data # Don't need testing data
+    rm $out/share/man/man1/mysql-test-run.pl.1
+    rm $out/bin/rcmysql # Not needed with nixos units
+    rm $out/bin/mysqlbug # Encodes a path to gcc and not really useful
+    find $out/bin -name \*test\* -exec rm {} \;
   '';
 
   passthru.mysqlVersion = "5.5";
