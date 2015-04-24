2c0db9ffbe9738212ff0efd65665416815575e2d
Vladimír Čunát <vcunat@gmail.com>
2015-04-24 09:59:52 +0200

    webkitgtk-2.4: security update, fix and parallel build
    NEWS claims security fixes:
    CVE-2014-1344, CVE-2014-1384, CVE-2014-1385, CVE-2014-1386,
    CVE-2014-1387, CVE-2014-1388, CVE-2014-1389, CVE-2014-1390.
    Otherwise NEWS seems safe, basically only containing bugfixes.
    
    Parallel building should work now, thanks to make update :-)
    tested with 8 threads, once webkitgtk24x and webkitgtk2.

--- pkgs/development/libraries/webkitgtk/2.4.nix
+++ pkgs/development/libraries/webkitgtk/2.4.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, perl, python, ruby, bison, gperf, flex
+{ stdenv, fetchurl, autoreconfHook, perl, python, ruby, bison, gperf, flex
 , pkgconfig, which, gettext, gobjectIntrospection
 , gtk2, gtk3, wayland, libwebp, enchant, sqlite
 , libxml2, libsoup, libsecret, libxslt, harfbuzz
@@ -9,7 +9,7 @@
 
 stdenv.mkDerivation rec {
   name = "webkitgtk-${version}";
-  version = "2.4.6";
+  version = "2.4.8";
 
   meta = with stdenv.lib; {
     description = "Web content rendering engine, GTK+ port";
@@ -21,16 +21,19 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "http://webkitgtk.org/releases/${name}.tar.xz";
-    sha256 = "0mqlq4ivh921k92xjsp5pdvbg9vf75qjliqmx81qwrm2sjl4mvvg";
+    sha256 = "08xxqsxpa63nzgbsz63vrdxdxgpysyiy7jdcjb57k1hprdcibwb8";
   };
 
-  patches = [ ./webcore-svg-libxml-cflags.patch ./webkitgtk-2.4-gmutexlocker.patch ];
+  patches = [ ./webkitgtk-2.4-gmutexlocker.patch ./bug140241.patch ];
 
   CC = "cc";
 
   prePatch = ''
     patchShebangs Tools/gtk
   '';
+
+  # patch *.in between autoreconf and configure
+  postAutoreconf = "patch -p1 < ${./webcore-svg-libxml-cflags.patch}";
 
   configureFlags = with stdenv.lib; [
     "--disable-geolocation"
@@ -43,7 +46,7 @@ stdenv.mkDerivation rec {
   dontAddDisableDepTrack = true;
 
   nativeBuildInputs = [
-    perl python ruby bison gperf flex
+    autoreconfHook/*bug140241.patch*/ perl python ruby bison gperf flex
     pkgconfig which gettext gobjectIntrospection
   ];
 
@@ -58,5 +61,8 @@ stdenv.mkDerivation rec {
     (if withGtk2 then gtk2 else gtk3)
   ];
 
-  #enableParallelBuilding = true; # build problems on Hydra
+  # Probably OK now, see:
+  # https://bugs.webkit.org/show_bug.cgi?id=79498
+  enableParallelBuilding = true;
 }
+
