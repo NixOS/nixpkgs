df000d93e9e67e3f8dc6f4375411a94d2aa9c757
William A. Kennington III <william@wkennington.com>
2015-04-25 18:38:05 -0700

    nettle: 3.1 -> 3.1.1

--- pkgs/development/libraries/nettle/default.nix
+++ pkgs/development/libraries/nettle/default.nix
@@ -1,10 +1,10 @@
 { callPackage, fetchurl, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "3.1";
+  version = "3.1.1";
 
   src = fetchurl {
     url = "mirror://gnu/nettle/nettle-${version}.tar.gz";
-    sha256 = "1ly9kz5fgc8ilykz07crqwgjsfn4p2s6565gj1aq0w4fr179v1gn";
+    sha256 = "0k1x57zviysvi91lkk66cg8v819vywm5g5yqs22wppfqcifx5m2z";
   };
 })
