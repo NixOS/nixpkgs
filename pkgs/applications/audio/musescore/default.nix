fc15ee2b4b78879b6959da2045f64ff47e95a7c2
Cillian de Róiste <cillian.deroiste@gmail.com>
2015-04-27 21:51:21 +0200

    MuseScore: 1.3 -> 2.0.0

--- pkgs/applications/audio/musescore/default.nix
+++ pkgs/applications/audio/musescore/default.nix
@@ -1,33 +1,31 @@
-{ stdenv, fetchurl, makeWrapper, cmake, qt4, pkgconfig, alsaLib, portaudio, jack2, libsndfile}:
+{ stdenv, fetchurl, makeWrapper, cmake, qt5, pkgconfig, alsaLib, portaudio, jack2
+, lame, libsndfile, libvorbis }:
 
 stdenv.mkDerivation rec {
-  name = "musescore-1.3";
+  name = "musescore-${version}";
+  version = "2.0.0";
 
   src = fetchurl {
-    url = "http://ftp.osuosl.org/pub/musescore/releases/MuseScore-1.3/mscore-1.3.tar.bz2";
-    sha256 = "a0b60cc892ac0266c58fc6392be72c0a21c3aa7fd0b6e4f1dddad1c8b36be683";
+    url = "https://github.com/musescore/MuseScore/archive/v${version}.tar.gz";
+    sha256 = "1a4fz9pqwz59brfa7qn61364hyd07lsq3lflkzn1w2q21d7xd20w";
   };
 
-  buildInputs = [ makeWrapper cmake qt4 pkgconfig alsaLib portaudio jack2 libsndfile ];
+  buildInputs = [
+    makeWrapper cmake qt5 pkgconfig alsaLib portaudio jack2 lame libsndfile libvorbis
+  ];
 
-  configurePhase = ''
-    cd mscore;
-    mkdir build;
-    cd build;
-    cmake -DCMAKE_INSTALL_PREFIX=$out -DQT_PLUGINS_DIR=$out/lib/qt4/plugins -DCMAKE_BUILD_TYPE=Release ..'';
-
-  preBuild = ''make lrelease;'';
-
-  postInstall = ''
-    wrapProgram $out/bin/mscore --prefix QT_PLUGIN_PATH : $out/lib/qt4/plugins
+  patchPhase = ''
+    sed s,"/usr/local",$out, -i Makefile
   '';
+
+  preBuild = "make lrelease";
 
   meta = with stdenv.lib; {
     description = "Qt-based score editor";
     homepage = http://musescore.org/;
     license = licenses.gpl2;
     platforms = platforms.linux;
-    maintainers = [ stdenv.lib.maintainers.vandenoever ];
+    maintainers = [ maintainers.vandenoever ];
     repositories.git = https://github.com/musescore/MuseScore;
   };
 }
