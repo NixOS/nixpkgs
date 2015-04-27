a3beaca54f248471ba966dd50ac483712f890ddd
Peter Simons <simons@cryp.to>
2015-04-27 19:44:38 +0200

    haskell-cabal-test-quickcheck: fix build by providing a recent-enough version of QuickCheck

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -155,9 +155,8 @@ self: super: {
     patchPhase = "sed -i -e 's|random.*==.*|random|' -e 's|text.*>=.*,|text,|' -e s'|terminfo == .*|terminfo|' darcs.cabal";
   });
 
-  # The test suite imposes too narrow restrictions on the version of
-  # Cabal that can be used to build this package.
-  cabal-test-quickcheck = dontCheck super.cabal-test-quickcheck;
+  # Needs the latest version of QuickCheck to compile.
+  cabal-test-quickcheck = super.cabal-test-quickcheck.override { QuickCheck = self.QuickCheck_2_8_1; };
 
   # https://github.com/techtangents/ablist/issues/1
   ABList = dontCheck super.ABList;
