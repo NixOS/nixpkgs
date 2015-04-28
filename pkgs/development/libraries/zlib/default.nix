faa9354de70850dd573126ca7658d948c4ccf04a
William A. Kennington III <william@wkennington.com>
2015-04-28 15:35:21 -0700

    zlib: Add platforms

--- pkgs/development/libraries/zlib/default.nix
+++ pkgs/development/libraries/zlib/default.nix
@@ -48,6 +48,7 @@ stdenv.mkDerivation (rec {
   meta = with stdenv.lib; {
     description = "Lossless data-compression library";
     license = licenses.zlib;
+    platforms = platforms.all;
   };
 } // (if stdenv.isDarwin then {
   postInstall = ''
