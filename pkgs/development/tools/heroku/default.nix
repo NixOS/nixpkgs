be71c0871c7edebc72367b1ce1f95ea10af609df
Alexander Flatter <flatter@fastmail.fm>
2015-04-28 17:02:33 +0200

    Add meta information to heroku toolbelt

--- pkgs/development/tools/heroku/default.nix
+++ pkgs/development/tools/heroku/default.nix
@@ -1,8 +1,16 @@
 { stdenv, fetchurl, postgresql, ruby }:
 
+with stdenv.lib;
 stdenv.mkDerivation rec {
   version = "3.30.3";
   name = "heroku-${version}";
+
+  meta = {
+    homepage = "https://toolbelt.heroku.com";
+    description = "Everything you need to get started using Heroku";
+    maintainers = with maintainers; [ aflatter mirdhyn ];
+    license = licenses.mit;
+  };
 
   src = fetchurl {
     url = "https://s3.amazonaws.com/assets.heroku.com/heroku-client/heroku-client-${version}.tgz";
