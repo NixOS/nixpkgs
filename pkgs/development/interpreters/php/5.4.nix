182acabbae644182ee6dafc9779e63abd7991f8c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-04-29 16:09:30 +0200

    Don't use camelCase in file names

--- pkgs/development/interpreters/php/5.4.nix
+++ pkgs/development/interpreters/php/5.4.nix
@@ -1,5 +1,5 @@
 { callPackage, apacheHttpd }:
-callPackage ./makePhpDerivation.nix {
+callPackage ./generic.nix {
   phpVersion = "5.4.40";
   sha = "06m5b3hw5kgwvnarhiylymadj504xalpczagr662vjrwmklgz628";
   apacheHttpd = apacheHttpd;
