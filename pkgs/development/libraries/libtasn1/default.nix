0cce6c3aeac71f16aa0a32f48209c02764c1a595
William A. Kennington III <william@wkennington.com>
2015-04-29 16:00:26 -0700

    libtasn1: 4.4 -> 4.5

--- pkgs/development/libraries/libtasn1/default.nix
+++ pkgs/development/libraries/libtasn1/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, perl, texinfo }:
 
 stdenv.mkDerivation rec {
-  name = "libtasn1-4.4";
+  name = "libtasn1-4.5";
 
   src = fetchurl {
     url = "mirror://gnu/libtasn1/${name}.tar.gz";
-    sha256 = "0p8c5s1gm3z3nn4s9qc6gs18grbk45mx44byqw2l2qzynjqrsd7q";
+    sha256 = "0p8c5s1gm3z3nn4s9qc6gs18grbk45mx44byqw2l2qzynjqrsd7a";
   };
 
   buildInputs = [ perl texinfo ];
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
 
     license = stdenv.lib.licenses.lgpl2Plus;
 
-    maintainers = [ ];
+    maintainers = with maintainers; [ wkennington ];
     platforms = stdenv.lib.platforms.all;
   };
 }
