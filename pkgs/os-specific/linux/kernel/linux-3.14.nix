e9421888766dc92a6de6584625b57c3717ac9a69
William A. Kennington III <william@wkennington.com>
2015-04-29 15:42:01 -0700

    kernel: 3.14.39 -> 3.14.40

--- pkgs/os-specific/linux/kernel/linux-3.14.nix
+++ pkgs/os-specific/linux/kernel/linux-3.14.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.14.39";
+  version = "3.14.40";
   # Remember to update grsecurity!
   extraMeta.branch = "3.14";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0zgfiqlvmprbn55k9ijf6db027mxlcww76y47g4g7vcj5qrpq6rd";
+    sha256 = "1w5j53ny5vahp1ipj16x0ahjb5yj6q859jsdshblymvwrbkcr29a";
   };
 
   features.iwlwifi = true;
