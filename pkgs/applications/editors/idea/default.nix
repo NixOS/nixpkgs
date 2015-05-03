2352fce77694ac1413404b58d06210de1c5c1765
Edward Tjörnhammar <ed@cflags.cc>
2015-05-03 17:01:14 +0200

    idea.android-studio: 1.2.0.8 -> 1.2.0.12

--- pkgs/applications/editors/idea/default.nix
+++ pkgs/applications/editors/idea/default.nix
@@ -212,14 +212,14 @@ in
 
   android-studio = buildAndroidStudio rec {
     name = "android-studio-${version}";
-    version = "1.2.0.8";
-    build = "141.1845774";
+    version = "1.2.0.12";
+    build = "141.1890965";
     description = "Android development environment based on IntelliJ IDEA";
     license = stdenv.lib.licenses.asl20;
     src = fetchurl {
       url = "https://dl.google.com/dl/android/studio/ide-zips/${version}" +
             "/android-studio-ide-${build}-linux.zip";
-      sha256 = "1l201qv1aya1l9jrybgqclv2v2fgzdpcb6qsnxszcq3npplisw9h";
+      sha256 = "01k96rql192ksnprc4yai97fcals7msf06m9bx1q7asn46887h7n";
     };
   };
 
