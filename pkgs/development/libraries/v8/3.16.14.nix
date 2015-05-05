7348b6f88852f2a7de9db326b855e2b0ec601ea3
codyopel <codyopel@gmail.com>
2015-05-05 11:08:33 -0400

    v8: refactor & fix arch flags

--- pkgs/development/libraries/v8/3.16.14.nix
+++ pkgs/development/libraries/v8/3.16.14.nix
@@ -1,9 +1,22 @@
-{ stdenv, lib, fetchurl, gyp, readline, python, which, icu, utillinux}:
+{ stdenv, fetchurl, gyp, icu, python, readline, utillinux, which }:
 
 assert readline != null;
 
 let
-  arch = if stdenv.is64bit then "x64" else "ia32";
+  inherit (stdenv) isArm isDarwin isi686 isLinux isMips isx86_64 system;
+  inherit (stdenv.lib) optional optionalString;
+  # For arches see:
+  # https://github.com/v8/v8-git-mirror/blob/master/build/detect_v8_host_arch.py
+  arch = (
+    if isi686 then "ia32"
+    else if isx86_64 then "x64"
+    else if isArm then "arm"
+    #else if isArm64 then "arm64"
+    #else if isMips then "mipsel" # isMips currently mixes both 32 & 64bit mips
+    #else if isMips64 then "mips64el"
+    #else if isPPC64 then "ppc64" # Nix does not support PPC
+    else throw "v8 does not support the `${system}' platform"
+  );
 in
 
 stdenv.mkDerivation rec {
@@ -11,12 +24,11 @@ stdenv.mkDerivation rec {
   version = "3.16.14";
 
   src = fetchurl {
-    url = "https://commondatastorage.googleapis.com/chromium-browser-official/"
-        + "${name}.tar.bz2";
+    url = "https://commondatastorage.googleapis.com/chromium-browser-official/${name}.tar.bz2";
     sha256 = "073f33zcb7205jp9g5ha5d7j2hfa98zs0jql572nb552z0xw3rkz";
   };
 
-  configurePhase = stdenv.lib.optionalString stdenv.isDarwin ''
+  configurePhase = optionalString isDarwin ''
     ln -s /usr/bin/xcodebuild $TMPDIR
     export PATH=$TMPDIR:$PATH
   '' + ''
@@ -34,8 +46,8 @@ stdenv.mkDerivation rec {
         build/all.gyp
   '';
 
-  nativeBuildInputs = [ which ];
-  buildInputs = [ readline python icu ] ++ lib.optional stdenv.isLinux utillinux;
+  nativeBuildInputs = [ python which ];
+  buildInputs = [ icu readline ] ++ optional isLinux utillinux;
 
   NIX_CFLAGS_COMPILE = "-Wno-error";
 
@@ -49,21 +61,22 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     install -vD out/Release/d8 "$out/bin/d8"
-    ${if stdenv.system == "x86_64-darwin" then ''
-    install -vD out/Release/lib.target/libv8.dylib "$out/lib/libv8.dylib"
-    '' else ''
-    install -vD out/Release/lib.target/libv8.so "$out/lib/libv8.so"
-    ''}
     cp -vr include "$out/"
-  '';
+  '' + (if isDarwin then ''
+      install -vD out/Release/lib.target/libv8.dylib "$out/lib/libv8.dylib"
+    '' else ''
+      install -vD out/Release/lib.target/libv8.so "$out/lib/libv8.so"
+    ''
+  );
 
-  postFixup = if stdenv.isDarwin then ''
+  postFixup = optionalString isDarwin ''
     install_name_tool -change /usr/local/lib/libv8.dylib $out/lib/libv8.dylib $out/bin/d8
     install_name_tool -id $out/lib/libv8.dylib $out/lib/libv8.dylib
-  '' else null;
+  '';
 
   meta = with stdenv.lib; {
     description = "V8 is Google's open source JavaScript engine";
+    homepage = https://developers.google.com/v8/;
     platforms = platforms.linux ++ platforms.darwin;
     license = licenses.bsd3;
   };
--- pkgs/development/libraries/v8/default.nix
+++ pkgs/development/libraries/v8/default.nix
@@ -1,9 +1,22 @@
-{ stdenv, fetchurl, gyp, readline, python, which, icu }:
+{ stdenv, fetchurl, gyp, icu, python, readline, which }:
 
 assert readline != null;
 
 let
-  arch = if stdenv.is64bit then "x64" else "ia32";
+  inherit (stdenv) isArm isDarwin isi686 isMips isx86_64 system;
+  inherit (stdenv.lib) optionalString;
+  # For arches see:
+  # https://github.com/v8/v8-git-mirror/blob/master/build/detect_v8_host_arch.py
+  arch = (
+    if isi686 then "ia32"
+    else if isx86_64 then "x64"
+    else if isArm then "arm"
+    #else if isArm64 then "arm64"
+    #else if isMips then "mipsel" # isMips currently mixes both 32 & 64bit mips
+    #else if isMips64 then "mips64el"
+    #else if isPPC64 then "ppc64" # Nix does not support PPC
+    else throw "v8 does not support the `${system}' platform"
+  );
 in
 
 stdenv.mkDerivation rec {
@@ -11,13 +24,12 @@ stdenv.mkDerivation rec {
   version = "3.30.33.16";
 
   src = fetchurl {
-    url = "https://commondatastorage.googleapis.com/chromium-browser-official/"
-        + "${name}.tar.bz2";
+    url = "https://commondatastorage.googleapis.com/chromium-browser-official/${name}.tar.bz2";
     sha256 = "1azf1b36gqj4z5x0k9wq2dkp99zfyhwb0d6i2cl5fjm3k6js7l45";
   };
 
   patchPhase = ''
-    sed -i 's,#!/usr/bin/env python,#!${python}/bin/python,' build/gyp_v8
+    patchShebangs ./build/gyp_v8
   '';
 
   configurePhase = ''
@@ -34,8 +46,8 @@ stdenv.mkDerivation rec {
         -Dv8_target_arch=${arch}
   '';
 
-  nativeBuildInputs = [ which ];
-  buildInputs = [ readline python icu ];
+  nativeBuildInputs = [ python which ];
+  buildInputs = [ icu readline ];
 
   buildFlags = [
     "LINK=g++"
@@ -48,22 +60,23 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     install -vD out/Release/d8 "$out/bin/d8"
-    ${if stdenv.system == "x86_64-darwin" then ''
-    install -vD out/Release/lib.target/libv8.dylib "$out/lib/libv8.dylib"
-    '' else ''
-    install -vD out/Release/lib.target/libv8.so "$out/lib/libv8.so"
-    ''}
     cp -vr include "$out/"
-  '';
+  '' + (if isDarwin then ''
+      install -vD out/Release/lib.target/libv8.dylib "$out/lib/libv8.dylib"
+    '' else ''
+      install -vD out/Release/lib.target/libv8.so "$out/lib/libv8.so"
+    ''
+  );
 
-  postFixup = if stdenv.isDarwin then ''
+  postFixup = optionalString isDarwin ''
     install_name_tool -change /usr/local/lib/libv8.dylib $out/lib/libv8.dylib -change /usr/lib/libgcc_s.1.dylib ${stdenv.cc.cc}/lib/libgcc_s.1.dylib $out/bin/d8
     install_name_tool -id $out/lib/libv8.dylib -change /usr/lib/libgcc_s.1.dylib ${stdenv.cc.cc}/lib/libgcc_s.1.dylib $out/lib/libv8.dylib
-  '' else null;
+  '';
 
   meta = with stdenv.lib; {
     description = "Google's open source JavaScript engine";
-    platforms = with platforms; linux;
+    homepage = https://developers.google.com/v8/;
+    platforms = platforms.linux ++ platforms.darwin;
     license = licenses.bsd3;
   };
 }
