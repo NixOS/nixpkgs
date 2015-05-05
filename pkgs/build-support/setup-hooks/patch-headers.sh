8edf0029f15cd05ee0e8e06bdbb5a4f805dc8691
William A. Kennington III <william@wkennington.com>
2015-05-04 18:33:29 -0700

    patch-headers: Add hacks to prevent bootstrap tools linking and misc fixes

--- pkgs/build-support/setup-hooks/patch-headers.sh
+++ pkgs/build-support/setup-hooks/patch-headers.sh
@@ -1,7 +1,6 @@
 # This setup hook causes the fixup phase to rewrite all header
 # system includes to be rewritten to absolute paths.  E.g.,
 # #include <stdint.h> will be rewritten to /nix/store/<hash>-some-glibc/include/stdint.h.
-# TODO: Fix absolute paths to headers for cc and libc
 
 fixupOutputHooks+=('if [ -z "$dontPatchHeaders" ]; then patchHeaders "$prefix"; fi')
 
@@ -13,25 +12,41 @@ patchHeaders() {
 
     header "patching header files in $dir"
 
+    # HACK: Fix gcc header linking by using the new cpp binary
+    # This prevents us from linking to bootstrap tools in new stdenvs
+    local cpp=cpp
+    local output
+    for output in $outputs; do
+      if ${!output}/bin/cpp --help >/dev/null 2>&1; then
+        cpp=${!output}/bin/cpp
+        break
+      fi
+    done
+
     # Iterate over every file and replace headers
     rm -f $TMPDIR/patch-headers.awk
+    local file
     for file in $(find $dir/include -type f); do
       # Determine what paths to search for includes
       # CPP is the most reliable way to parse the environment as
       # it gives us system and libc includes as well.
-      local rawCpp="$(cpp -v -M $NIX_CFLAGS_COMPILE $file 2>&1)"
+      local rawCpp="$($cpp -v -M $NIX_CFLAGS_COMPILE $file 2>&1)"
       if [ "$?" -ne "0" ]; then
-        echo "Failed to process: $file"
+        echo "Failed to process: $file" >&2
         continue
       fi
 
       # Parse the cpp output into useful header location lists
-      local systemIncludes="$(echo "$rawCpp" | awk '/End/{f=0};f{print $1};/#include </{f=1};')"
-      local localIncludes="$(echo "$rawCpp" | awk '/#include </{f=0};f{print $1};/#include "/{f=1};')"
-      local tmpComb="$(dirname "$file")"$'\n'"$localIncludes"$'\n'"$systemIncludes"
-      local localAndSystemIncludes="$(echo "$tmpComb" | grep -v '^[ \r]*$')"
+      local cppSystemIncludes="$(echo "$rawCpp" | awk '/End/{f=0};f{print $1};/#include </{f=1};')"
+      local tmpComb="$dir/include"$'\n'"$cppSystemIncludes"
+      local systemIncludes="$(echo "$tmpComb" | grep -v '^[ \r]*$')"
+
+      local cppLocalIncludes="$(echo "$rawCpp" | awk '/#include </{f=0};f{print $1};/#include "/{f=1};')"
+      local tmpComb="$(dirname "$file")"$'\n'"$cppLocalIncludes"$'\n'"$systemIncludes"
+      local localIncludes="$(echo "$tmpComb" | grep -v '^[ \r]*$')"
+
       local headerPath="$(readlink -f "$(dirname "$file")")"
-      local nextIncludes="$(for includeDir in $localAndSystemIncludes; do
+      local nextIncludes="$(for includeDir in $localIncludes; do
         [ "$(readlink -f "$includeDir")" != "$headerPath" ] && echo "$includeDir"; done)"
 
       # Build the script awk uses to fix headers
@@ -44,19 +59,25 @@ patchHeaders() {
       # include_next in this context would effectively exclude any cwd
       local headerTuples
       readarray -t headerTuples <<< \
-        "$(sed -n 's,.*\(#[ ]*include\?\(_next\)[ ]*\(<\|"\)\([^/>][^>]*\)\(>\|"\)\).*,\4 \1,p' $file)"
+        "$(sed -n 's;.*\(#[ ]*include\(_next\)\{0,1\}[ ]*\(<\|"\)\([^/>][^>]*\)\(>\|"\)\).*;\4 \1;p' $file)"
 
       # Determine where each of the includes resides and build an expression to patch it
+      local headerTuple
       for headerTuple in "${headerTuples[@]}"; do
         [ -z "$headerTuple" ] && continue
         local header="$(echo "$headerTuple" | cut -d' ' -f1)"
         local includeStmt="$(echo "$headerTuple" | cut -d' ' -f2-)"
 
+        # HACK: Prevent cyclic references to compiler defined headers
+        # which bring in dependencies like bootstrap-tools.
+        # These headers are guaranteed to exist.
+        grep -q '^\(stddef.h\|stdarg.h\|limits.h\|float.h\)$' <<< "$header" && continue
+
         # Determine directory list based on the type of include
         if echo "$includeStmt" | grep -q '_next'; then
           local includeDirs="$nextIncludes"
         elif echo "$includeStmt" | grep -q '"'; then
-          local includeDirs="$localAndSystemIncludes"
+          local includeDirs="$localIncludes"
         else
           local includeDirs="$systemIncludes"
         fi
@@ -64,9 +85,13 @@ patchHeaders() {
         # Determine which include path holds the header
         # we are trying to include
         local absolute=""
+        local includeDir
         for includeDir in $includeDirs; do
           local tmpName="$(readlink -f "$includeDir/$header")"
-          [ -f "$tmpName" ] && absolute="$tmpName"
+          if [ -f "$tmpName" ]; then
+            absolute="$tmpName"
+            break
+          fi
         done
 
         # Generate the expression used for fixing the header file
