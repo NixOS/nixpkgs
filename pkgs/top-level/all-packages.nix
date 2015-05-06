a7c4aba8968dfa85a147f60398d266441bc2b5ae
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2015-05-06 11:15:13 +0300

    ARM: Assume armv7l-hf-multiplatform instead of beaglebone
    Several places in the tree associate the ARMv7 system
    with the beaglebone platform. Change them to point to
    armv7l-hf-multiplatform as it supports several boards (including the
    beaglebone as well)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -72,7 +72,7 @@ let
       platforms = (import ./platforms.nix);
     in
       if system == "armv6l-linux" then platforms.raspberrypi
-      else if system == "armv7l-linux" then platforms.beaglebone
+      else if system == "armv7l-linux" then platforms.armv7l-hf-multiplatform
       else if system == "armv5tel-linux" then platforms.sheevaplug
       else if system == "mips64el-linux" then platforms.fuloong2f_n32
       else if system == "x86_64-linux" then platforms.pc64
