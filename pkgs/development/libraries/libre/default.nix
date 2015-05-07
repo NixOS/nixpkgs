d524b48d6415eb808d79e0cc86ab04eb61bfe7ac
codyopel <codyopel@gmail.com>
2015-05-07 11:36:31 -0400

    libre: remove static library

--- pkgs/development/libraries/libre/default.nix
+++ pkgs/development/libraries/libre/default.nix
@@ -1,27 +1,33 @@
-{stdenv, fetchurl, zlib, openssl}:
+{ stdenv, fetchurl, openssl, zlib }:
+
 stdenv.mkDerivation rec {
-  version = "0.4.12";
   name = "libre-${version}";
-  src=fetchurl {
+  version = "0.4.12";
+
+  src = fetchurl {
     url = "http://www.creytiv.com/pub/re-${version}.tar.gz";
     sha256 = "1wjdcf5wr50d86rysj5haz53v7d58j7sszpc6k5b4mn1x6604i0d";
   };
-  buildInputs = [zlib openssl];
+
   makeFlags = [
-    "USE_ZLIB=1" "USE_OPENSSL=1" 
-    ''PREFIX=$(out)''
-  ]
-  ++ stdenv.lib.optional (stdenv.cc.cc != null) "SYSROOT_ALT=${stdenv.cc.cc}"
-  ++ stdenv.lib.optional (stdenv.cc.libc != null) "SYSROOT=${stdenv.cc.libc}"
-  ;
-  meta = {
-    homepage = "http://www.creytiv.com/re.html";
-    platforms = with stdenv.lib.platforms; linux;
-    maintainers = with stdenv.lib.maintainers; [raskin];
-    license = with stdenv.lib.licenses; bsd3;
-    inherit version;
-    downloadPage = "http://www.creytiv.com/pub/";
-    updateWalker = true;
-    downloadURLRegexp = "/re-.*[.]tar[.].*";
+    "PREFIX=$(out)"
+    "USE_OPENSSL=1"
+    "USE_ZLIB=1"
+  ] ++ stdenv.lib.optional (stdenv.cc.cc != null) "SYSROOT_ALT=${stdenv.cc.cc}"
+    ++ stdenv.lib.optional (stdenv.cc.libc != null) "SYSROOT=${stdenv.cc.libc}";
+
+  buildInputs = [ openssl zlib ];
+
+  postInstall = ''
+    # Remove static object
+    rm -f $out/lib/libre.a
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Portable generic library for real-time communications";
+    homepage = http://www.creytiv.com/re.html;
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ raskin ];
+    platforms = platforms.linux;
   };
 }
