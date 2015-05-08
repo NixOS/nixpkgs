a7a0fcfbd21d6253ab681c9a1a66f22ca8833e26
rushmorem <rushmore@webenchanter.com>
2015-05-08 12:10:40 +0200

    Add s3ql package
    S3QL is a file system that stores all its data online using storage
    services like Google Storage, Amazon S3, or OpenStack. S3QL effectively
    provides a hard disk of dynamic, infinite capacity that can be accessed
    from any computer with internet access running Linux, FreeBSD or OS-X.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9676,6 +9676,8 @@ let
 
   rtkit = callPackage ../os-specific/linux/rtkit { };
 
+  s3ql = callPackage ../tools/backup/s3ql { };
+
   sassc = callPackage ../development/tools/sassc { };
 
   schedtool = callPackage ../os-specific/linux/schedtool { };
