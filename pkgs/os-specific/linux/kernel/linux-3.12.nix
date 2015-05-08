1998c4967420d25ae605e9502b4c76ad705a4718
William A. Kennington III <william@wkennington.com>
2015-05-07 19:26:48 -0700

    kernel: 3.12.40 -> 3.12.42

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.12.41";
+  version = "3.12.42";
   extraMeta.branch = "3.12";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "17jcfzxwi4lcgypasw6xn9711rbngw7l7k3bxsfwa7844jldxph9";
+    sha256 = "0vp6yllal2gcyng1kklp9n8r18fhcb1m1ssavjbcbfax5chi7w5s";
   };
 
   features.iwlwifi = true;
