7e9d00b05dfad7546eca4c05b50d024e8d589f20
Peter Simons <simons@cryp.to>
2015-05-08 13:48:26 +0200

    libcrafter: fix broken evaluation

--- pkgs/development/libraries/libcrafter/default.nix
+++ pkgs/development/libraries/libcrafter/default.nix
@@ -3,12 +3,12 @@
 stdenv.mkDerivation rec {
   name = "libcrafter-${version}";
   version = "0.3";
-  
+
   src = fetchzip {
     url = "https://github.com/pellegre/libcrafter/archive/version-${version}.zip";
     sha256 = "04lpmshh4wb1dav03p6rnskpd1zmmvhv80xwn8v7l8faps5gvjp4";
   };
-  
+
   preConfigure = "cd libcrafter";
 
   configureScript = "./autogen.sh";
@@ -23,6 +23,6 @@ stdenv.mkDerivation rec {
     homepage = https://github.com/pellegre/libcrafter;
     description = "High level C++ network packet sniffing and crafting library";
     license = stdenv.lib.licenses.bsd3;
-    maintainers = [ maintainers.lethalman ];
+    maintainers = [ stdenv.lib.maintainers.lethalman ];
   };
 }
