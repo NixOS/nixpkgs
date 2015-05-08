a5312e581fc79e31b7b3deed14b08858804d3701
William A. Kennington III <william@wkennington.com>
2015-05-08 05:27:47 -0700

    kernel: Remove obsolete btrfs patch

--- pkgs/os-specific/linux/kernel/linux-3.18.nix
+++ pkgs/os-specific/linux/kernel/linux-3.18.nix
@@ -9,13 +9,6 @@ import ./generic.nix (args // rec {
     sha256 = "043bqjvbamzi201csgnw7hsf8810qm0dn7x9p0kc7s9p9jnyq79n";
   };
 
-  # FIXME: remove with the next point release.
-  kernelPatches = args.kernelPatches ++
-    [ { name = "btrfs-fix-deadlock";
-        patch = ./btrfs-fix-deadlock.patch;
-      }
-    ];
-
   features.iwlwifi = true;
   features.efiBootStub = true;
   features.needsCifsUtils = true;
