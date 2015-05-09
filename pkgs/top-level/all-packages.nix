a5a740a17a4cf554a8eb237a3670f3aef0d3d8f3
Aistis Raulinaitis <sheganinans@gmail.com>
2015-05-08 23:45:07 -0700

    z3 opt

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13768,6 +13768,7 @@ let
   yices = callPackage ../applications/science/logic/yices {};
 
   z3 = callPackage ../applications/science/logic/z3 {};
+  z3_opt = callPackage ../applications/science/logic/z3_opt {};
 
   boolector   = boolector15;
   boolector15 = callPackage ../applications/science/logic/boolector {};
