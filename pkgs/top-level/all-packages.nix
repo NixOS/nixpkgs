6f634e3c3abdffdaac9405c872f683f4fcf465fe
Kranium Gikos Mendoza <kranium@gikos.net>
2015-05-11 01:57:15 +0800

    Add kbdlight package and setuid wrapper
    update nixos/modules/programs/kbdlight.nix to use mkEnableOption

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9155,6 +9155,8 @@ let
 
   kbd = callPackage ../os-specific/linux/kbd { };
 
+  kbdlight = callPackage ../os-specific/linux/kbdlight { };
+
   kmscon = callPackage ../os-specific/linux/kmscon { };
 
   latencytop = callPackage ../os-specific/linux/latencytop { };
