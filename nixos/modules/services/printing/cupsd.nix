19dbe42aeb762ec6ddae9e6af20959424857703a
Jascha Geerds <jg@ekby.de>
2015-05-10 20:47:50 +0200

    Revert "cups: Rename "browsing" config option to "advertise""
    This reverts commit 48e65cec4ecd6bef08233460cf5ffedb023cc7c6.

--- nixos/modules/services/printing/cupsd.nix
+++ nixos/modules/services/printing/cupsd.nix
@@ -80,7 +80,7 @@ in
         '';
       };
 
-      advertise = mkOption {
+      browsing = mkOption {
         type = types.bool;
         default = false;
         description = ''
@@ -285,7 +285,7 @@ in
 
         DefaultShared ${if cfg.defaultShared then "Yes" else "No"}
 
-        Browsing ${if cfg.advertise then "Yes" else "No"}
+        Browsing ${if cfg.browsing then "Yes" else "No"}
 
         WebInterface ${if cfg.webInterface then "Yes" else "No"}
 
