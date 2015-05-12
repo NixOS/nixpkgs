cb9764f847e1a67c69de80d7bb32ac10f1649344
Nikolay Amiantov <ab@fmap.me>
2015-05-12 16:50:14 +0300

    haskell-lambdabot: remove ghc 7.8 patch

--- pkgs/development/haskell-modules/configuration-ghc-7.10.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-7.10.x.nix
@@ -182,9 +182,6 @@ self: super: {
             in addBuildDepends jsaddle' [ self.glib self.gtk3 self.webkitgtk3
                                           self.webkitgtk3-javascriptcore ];
 
-  # Fix evaluation in GHC >=7.8: https://github.com/lambdabot/lambdabot/issues/116
-  lambdabot = appendPatch super.lambdabot ./lambdabot-fix-ghc78.patch;
-
   # https://github.com/haskell-infra/hackage-trustees/issues/24
   brainfuck = appendPatch super.brainfuck ./brainfuck-fix-ghc710.patch;
   unlambda = appendPatch super.unlambda ./unlambda-fix-ghc710.patch;
