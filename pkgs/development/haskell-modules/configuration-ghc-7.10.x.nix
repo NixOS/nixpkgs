367c7779e0be9be9622a655b98a11e9f0931a427
Nikolay Amiantov <ab@fmap.me>
2015-05-12 15:49:03 +0300

    haskell-dependent-sum-template: updated on Hackage

--- pkgs/development/haskell-modules/configuration-ghc-7.10.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-7.10.x.nix
@@ -124,10 +124,6 @@ self: super: {
   unix-time = dontCheck super.unix-time;
 
   # Until the changes have been pushed to Hackage
-  dependent-sum-template = appendPatch super.dependent-sum-template (pkgs.fetchpatch {
-    url = "https://patch-diff.githubusercontent.com/raw/mokus0/dependent-sum-template/pull/4.patch";
-    sha256 = "1yb1ny4ckl4d3sf4xnvpbsa9rw2dficzgipijs5s3729dnsc3rb0";
-  });
   mueval = appendPatch super.mueval (pkgs.fetchpatch {
     url = "https://patch-diff.githubusercontent.com/raw/gwern/mueval/pull/10.patch";
     sha256 = "1gs8p89d1qsrd1qycbhf6kv4qw0sbb8m6dy106dqkmdzcjzcyq74";
