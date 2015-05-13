3df7a9866d413cc2701096b9ff8296289e3b9fb0
Ricardo M. Correia <rcorreia@wizy.org>
2015-05-13 15:15:24 +0200

    rustcMaster: 2015-05-09 -> 2015-05-13

--- pkgs/development/compilers/rustc/head.nix
+++ pkgs/development/compilers/rustc/head.nix
@@ -2,9 +2,9 @@
 callPackage ./makeRustcDerivation.nix {
   shortVersion = "1.0.0-dev";
   isRelease = false;
-  # src rev for master on 2015-05-09
-  srcRev = "95400c51c31877ea4699adc051477edccb5cfbca";
-  srcSha = "1ldgc5nd42pcbr650rpyvpnc8h6bky6gmb116fwy1g6x2ql66djw";
+  # src rev for master on 2015-05-13
+  srcRev = "67433c1a309d3c9457e49f15e80a2d927d165996";
+  srcSha = "0idc3nh0sfjlv7m9710azx7n6awzwj6mhw3aybsb9bbagzy2sdsg";
   snapshotHashLinux686 = "0bc8cffdce611fb71fd7d3d8e7cdbfaf748a4f16";
   snapshotHashLinux64 = "94089740e48167c5975c92c139ae9c286764012f";
   snapshotHashDarwin686 = "54cc35e76497e6e94fddf38d6e40e9d168491ddb";
