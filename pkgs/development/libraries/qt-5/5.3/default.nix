7094ae27da65c8b41843e61a8b7ba5e1fe274ab1
Thomas Tuegel <ttuegel@gmail.com>
2015-05-13 12:49:00 -0500

    qt53: fix qtwebkit build with new glib

--- pkgs/development/libraries/qt-5/5.3/default.nix
+++ pkgs/development/libraries/qt-5/5.3/default.nix
@@ -78,6 +78,7 @@ stdenv.mkDerivation rec {
       (substituteAll { src = ./0010-dlopen-libXcursor.patch; inherit libXcursor; })
       (substituteAll { src = ./0011-dlopen-openssl.patch; inherit openssl; })
       (substituteAll { src = ./0012-dlopen-dbus.patch; dbus_libs = dbus; })
+      ./0013-qtwebkit-glib-2.44.patch
     ];
 
   preConfigure = ''
