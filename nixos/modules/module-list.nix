6a0d21eb86105ac8e631621e9425eabbab3c0a1d
Jussi Maki <jussi.maki@vilea.ch>
2015-05-13 08:31:32 +0000

    VMWare guest support and open-vm-tools package

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -460,5 +460,6 @@
   ./virtualisation/openvswitch.nix
   ./virtualisation/parallels-guest.nix
   ./virtualisation/virtualbox-guest.nix
+  ./virtualisation/vmware-guest.nix
   ./virtualisation/xen-dom0.nix
 ]
