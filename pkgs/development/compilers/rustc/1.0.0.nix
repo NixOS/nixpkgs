af0002d884fa95ea0e5d70f37cfe47cd749b5b4e
Georges Dubus <georges.dubus@compiletoi.net>
2015-05-16 00:54:20 +0200

    rustc: build the stable channel

--- pkgs/development/compilers/rustc/1.0.0.nix
+++ pkgs/development/compilers/rustc/1.0.0.nix
@@ -12,5 +12,5 @@ callPackage ./makeRustcDerivation.nix {
   patches = [
     ./patches/beta.patch
     ] ++ stdenv.lib.optional stdenv.needsPax ./patches/grsec.patch;
-  configureFlags = [ "--release-channel=beta" ];
+  configureFlags = [ "--release-channel=stable" ];
 }
