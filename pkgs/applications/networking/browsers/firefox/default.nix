de0a7421f665c1e2a084b9ee3d313fd4e96225bc
William A. Kennington III <william@wkennington.com>
2015-05-15 15:56:44 -0700

    firefox: 38.0 -> 38.0.1
    (cherry picked from commit 0700e844e29e2a583d66a35971462b30b387fcfc)

--- pkgs/applications/networking/browsers/firefox/default.nix
+++ pkgs/applications/networking/browsers/firefox/default.nix
@@ -15,14 +15,14 @@
 
 assert stdenv.gcc ? libc && stdenv.gcc.libc != null;
 
-let version = "38.0"; in
+let version = "38.0.1"; in
 
 stdenv.mkDerivation rec {
   name = "firefox-${version}";
 
   src = fetchurl {
     url = "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/${version}/source/firefox-${version}.source.tar.bz2";
-    sha1 = "3812a31287d3a8d9ebb1326e131a944fd27f0f2d";
+    sha1 = "20f52c37e099cb2b21f3a76c6e39fe698e1e79e8";
   };
 
   buildInputs =
