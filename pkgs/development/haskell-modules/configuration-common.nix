8b13b29898d8b678a30f80e25de3502ae3b3bc53
Peter Simons <simons@cryp.to>
2015-05-16 14:34:19 +0200

    pandoc: jailbreak to fix build with recent versions of zlib and QuickCheck

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -678,8 +678,11 @@ self: super: {
   # https://github.com/junjihashimoto/test-sandbox-compose/issues/2
   test-sandbox-compose = dontCheck super.test-sandbox-compose;
 
-  # https://github.com/jgm/pandoc/issues/2045
-  pandoc = dontCheck super.pandoc;
+  # https://github.com/jgm/pandoc/issues/2156
+  pandoc = overrideCabal (dontJailbreak super.pandoc) (drv: {
+    doCheck = false;    # https://github.com/jgm/pandoc/issues/2036
+    patchPhase = "sed -i -e 's|zlib .*,|zlib,|' -e 's|QuickCheck .*,|QuickCheck,|' pandoc.cabal";
+  });
 
   # Broken by GLUT update.
   Monadius = markBroken super.Monadius;
