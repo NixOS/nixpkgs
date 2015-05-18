c0e2c4809e49dc3954b7994b1052d626cb9ef0c4
Peter Simons <simons@cryp.to>
2015-05-18 19:26:56 +0200

    cabal-install: fix build for GHC 7.0.x and 7.2.x

--- pkgs/development/haskell-modules/configuration-ghc-7.0.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-7.0.x.nix
@@ -48,7 +48,6 @@ self: super: {
 
   # Newer versions don't compile.
   Cabal_1_18_1_6 = dontJailbreak super.Cabal_1_18_1_6;
-  cabal-install_1_18_1_0 = super.cabal-install_1_18_1_0.override { Cabal = self.Cabal_1_18_1_6; };
   cabal-install = self.cabal-install_1_18_1_0;
 
   # Needs Cabal >= 1.18.x.
