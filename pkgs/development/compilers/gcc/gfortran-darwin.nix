ec2f3eb855ed22df4a56ccded9d285321fd4a69d
Allen Nelson <anelson@narrativescience.com>
2015-05-19 15:59:55 -0500

    added gfortran expression for darwin

---
+++ pkgs/development/compilers/gcc/gfortran-darwin.nix
@@ -0,0 +1,22 @@
+# This is a derivation customized to work on OS X (Darwin).
+{gmp, mpfr, libmpc, fetchurl, stdenv}:
+
+# This package is only intended for OSX.
+assert stdenv.isDarwin;
+
+stdenv.mkDerivation rec {
+  name = "gfortran";
+  version = "5.1.0";
+  buildInputs = [gmp mpfr libmpc];
+  src = fetchurl {
+    url = "https://ftp.gnu.org/gnu/gcc/gcc-${version}/gcc-${version}.tar.bz2";
+    sha256 = "1bd5vj4px3s8nlakbgrh38ynxq4s654m6nxz7lrj03mvkkwgvnmp";
+  };
+  configureFlags = ''
+    --enable-languages=fortran --enable-checking=release --disable-bootstrap
+    --with-gmp=${gmp}
+    --with-mpfr=${mpfr}
+    --with-mpc=${libmpc}
+  '';
+  makeFlags = ["CC=/usr/bin/gcc"];
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3676,7 +3676,8 @@ let
     isl = isl_0_14;
   }));
 
-  gfortran = gfortran48;
+  gfortran = if !stdenv.isDarwin then gfortran48
+             else callPackage ../development/compilers/gcc/gfortran-darwin.nix {};
 
   gfortran48 = wrapCC (gcc48.cc.override {
     name = "gfortran";
