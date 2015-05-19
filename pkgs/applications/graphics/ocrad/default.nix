10e001ab5b8e8d8c4390334b62a874df93280f8d
Pascal Wittmann <mail@pascal-wittmann.de>
2015-05-19 19:39:09 +0200

    ocrad: update from 0.24 to 0.25

--- pkgs/applications/graphics/ocrad/default.nix
+++ pkgs/applications/graphics/ocrad/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, lzip, texinfo }:
 
 stdenv.mkDerivation rec {
-  name = "ocrad-0.24";
+  name = "ocrad-0.25";
 
   src = fetchurl {
     url = "mirror://gnu/ocrad/${name}.tar.lz";
-    sha256 = "0hhlx072d00bi9qia0nj5izsq4qkscpfz2mpbyfc72msl3hfvslv";
+    sha256 = "1m2dblgvvjs48rsglfdwq0ib9zk8h9n34xsh67ibrg0g0ffbw477";
   };
 
   buildInputs = [ lzip texinfo ];
@@ -14,7 +14,6 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Optical character recognition (OCR) program & library";
-
     longDescription =
       '' GNU Ocrad is an OCR (Optical Character Recognition) program based on
          a feature extraction method.  It reads images in pbm (bitmap), pgm
@@ -29,7 +28,6 @@ stdenv.mkDerivation rec {
       '';
 
     license = licenses.gpl3Plus;
-
     maintainers = with maintainers; [ pSub ];
     platforms = platforms.gnu;  # arbitrary choice
   };
