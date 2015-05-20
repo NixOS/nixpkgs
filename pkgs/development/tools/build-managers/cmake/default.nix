845f522b1f3dc0ae99c52ef9fd98fd0fa27cad0b
Rok Garbas <rok@garbas.si>
2015-05-20 14:43:30 +0200

    cygwin: another rebase fix

--- pkgs/development/tools/build-managers/cmake/default.nix
+++ pkgs/development/tools/build-managers/cmake/default.nix
@@ -38,22 +38,15 @@ stdenv.mkDerivation rec {
       url = "http://public.kitware.com/Bug/file_download.php?"
           + "file_id=4981&type=bug";
       sha256 = "16acmdr27adma7gs9rs0dxdiqppm15vl3vv3agy7y8s94wyh4ybv";
-<<<<<<< HEAD
-    });
-
-  buildInputs =
-    [ bzip2 curl expat libarchive xz zlib ]
-    ++ optional (jsoncpp != null) jsoncpp
-=======
-    }) ++
+    }) ++ stdenv.lib.optional stdenv.isCygwin ./2.8.11-cygwin.patch;
     # fix cmake detection of openssl libs
     # see: http://public.kitware.com/Bug/bug_relationship_graph.php?bug_id=15386
     #      and http://www.cmake.org/gitweb?p=cmake.git;a=commitdiff;h=c5d9a8283cfac15b4a5a07f18d5eb10c1f388505#patch1
-    [./cmake_find_openssl_for_openssl-1.0.1m_and_up.patch] ++
-      stdenv.lib.optional stdenv.isCygwin ./2.8.11-cygwin.patch;
+    # [./cmake_find_openssl_for_openssl-1.0.1m_and_up.patch] ++
 
-  buildInputs = optionals (!stdenv.isCygwin) [ curl expat zlib bzip2 libarchive ]
->>>>>>> cygwin cmake
+  buildInputs =
+    (optionals (!stdenv.isCygwin) [ bzip2 curl expat libarchive xz zlib ])
+    ++ optional (jsoncpp != null) jsoncpp
     ++ optional useNcurses ncurses
     ++ optional useQt4 qt4;
 
