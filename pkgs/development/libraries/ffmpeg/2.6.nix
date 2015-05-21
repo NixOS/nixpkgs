61fa78293dece8d75957782f6006f0266a62ed01
codyopel <codyopel@gmail.com>
2015-05-20 20:45:59 -0400

    ffmpeg(-full): 2.6.2 -> 2.6.3

--- pkgs/development/libraries/ffmpeg/2.6.nix
+++ pkgs/development/libraries/ffmpeg/2.6.nix
@@ -1,7 +1,7 @@
 { callPackage, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "${branch}.2";
+  version = "${branch}.3";
   branch = "2.6";
-  sha256 = "1fi93zy98wmls7x3jpr2yvckk2ia6a1yyygwrfaxq95pd6h3m7l8";
+  sha256 = "1yqc3vm1xrwf866q262qd4nr9d6ifp4gg183pjdc4sl9np0rissr";
 })
