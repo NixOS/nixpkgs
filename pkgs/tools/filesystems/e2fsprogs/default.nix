3f4f90b58ba4d09352111c98a3e99019673ff64f
William A. Kennington III <william@wkennington.com>
2015-05-23 16:00:02 -0700

    e2fsprogs: 1.14.12 -> 1.14.13

--- pkgs/tools/filesystems/e2fsprogs/default.nix
+++ pkgs/tools/filesystems/e2fsprogs/default.nix
@@ -1,34 +1,66 @@
-{ stdenv, fetchurl, pkgconfig, libuuid }:
+{ stdenv, fetchurl, pkgconfig
 
+# Optional Dependencies
+, libuuid
+}:
+
+with stdenv;
+let
+  optLibuuid = shouldUsePkg libuuid;
+in
+with stdenv.lib;
 stdenv.mkDerivation rec {
-  name = "e2fsprogs-1.42.12";
+  name = "e2fsprogs-1.42.13";
 
   src = fetchurl {
     url = "mirror://sourceforge/e2fsprogs/${name}.tar.gz";
-    sha256 = "0v0qcfyls0dlrjy8gx9m3s2wbkp5z3lbsr5hb7x8kp8f3bclcy71";
+    sha256 = "1m72lk90b5i3h9qnmss6aygrzyn8x2avy3hyaq2fb0jglkrkz6ar";
   };
 
-  buildInputs = [ pkgconfig libuuid ];
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [ optLibuuid ];
 
-  crossAttrs = {
-    preConfigure = ''
-      export CC=$crossConfig-gcc
-    '';
-  };
-
-  # libuuid, libblkid, uuidd and fsck are in util-linux-ng (the "libuuid" dependency).
-  configureFlags = "--enable-elf-shlibs --disable-libuuid --disable-libblkid --disable-uuidd --disable-fsck --enable-symlink-install";
+  configureFlags = [
+    (mkEnable true                 "symlink-install"           null)
+    (mkEnable true                 "relative-symlinks"         null)
+    (mkEnable true                 "symlink-relative-symlinks" null)
+    (mkEnable false                "compression"               null)
+    (mkEnable true                 "htree"                     null)
+    (mkEnable true                 "elf-shlibs"                null)
+    (mkEnable false                "profile"                   null)
+    (mkEnable false                "gcov"                      null)
+    (mkEnable false                "jbd-debug"                 null)
+    (mkEnable false                "blkid-debug"               null)
+    (mkEnable false                "testio-debug"              null)
+    (mkEnable (optLibuuid == null) "libuuid"                   null)
+    (mkEnable (optLibuuid == null) "libblkid"                  null)
+    (mkEnable true                 "quota"                     null)
+    (mkEnable false                "backtrace"                 null)
+    (mkEnable false                "debugfs"                   null)
+    (mkEnable true                 "imager"                    null)
+    (mkEnable true                 "resizer"                   null)
+    (mkEnable true                 "defrag"                    null)
+    (mkEnable true                 "fsck"                      null)
+    (mkEnable false                "e2initrd-helper"           null)
+    (mkEnable true                 "tls"                       null)
+    (mkEnable false                 "uuidd"                    null)  # Build is broken in 1.42.13
+  ];
 
   enableParallelBuilding = true;
 
-  preInstall = "installFlagsArray=('LN=ln -s')";
+  installFlags = [
+    "LN=ln -s"
+  ];
 
-  postInstall = "make install-libs";
+  postInstall = ''
+    make install-libs
+  '';
 
-  meta = {
+  meta = with stdenv.lib; {
     homepage = http://e2fsprogs.sourceforge.net/;
     description = "Tools for creating and checking ext2/ext3/ext4 filesystems";
-    platforms = stdenv.lib.platforms.linux;
-    maintainers = [ stdenv.lib.maintainers.eelco ];
+    license = licenses.gpl2;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ eelco wkennington ];
   };
 }
