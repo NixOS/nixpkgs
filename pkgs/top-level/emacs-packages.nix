e403495938fecc24491044db81b280e8c09355a3
Jan Malakhovski <oxij@oxij.org>
2015-05-24 14:10:56 +0000

    emacs-packages: cleanup and fix request and request-deferred

--- pkgs/top-level/emacs-packages.nix
+++ pkgs/top-level/emacs-packages.nix
@@ -1093,7 +1093,9 @@ let self = _self // overrides;
       sha256 = "0dja4g43zfjbxqvz2cgivgq5sfm6fz1563qgrp4yxknl7bdggb92";
     };
 
-    meta = with stdenv.lib; {
+    files = [ "request.el" ];
+
+    meta = {
       description = "Easy HTTP request for Emacs Lisp";
       longDescription = ''
         Request.el is a HTTP request library with multiple backends. It supports
@@ -1102,36 +1104,18 @@ let self = _self // overrides;
         Library author can use request.el to avoid imposing external dependencies
         such as curl to users while giving richer experience for users who have curl.
       '';
-      homepage = https://github.com/tkf/emacs-request;
       license = gpl3Plus;
     };
   };
 
   request-deferred = melpaBuild rec {
     pname = "request-deferred";
-    version = "0.2.0";
-
-    src = fetchFromGitHub {
-      owner = "tkf";
-      repo = "emacs-request";
-      rev = "adf7de452f9914406bfb693541f1d280093c4efd";
-      sha256 = "0dja4g43zfjbxqvz2cgivgq5sfm6fz1563qgrp4yxknl7bdggb92";
-    };
-
+    version = request.version;
+    src = request.src;
     packageRequires = [ request deferred ];
-
-    meta = with stdenv.lib; {
-      description = "Easy HTTP request for Emacs Lisp";
-      longDescription = ''
-        Request.el is a HTTP request library with multiple backends. It supports
-        url.el which is shipped with Emacs and curl command line program. User
-        can use curl when s/he has it, as curl is more reliable than url.el.
-        Library author can use request.el to avoid imposing external dependencies
-        such as curl to users while giving richer experience for users who have curl.
-      '';
-      homepage = https://github.com/tkf/emacs-request;
-      license = gpl3Plus;
-    };
+    files = [ "request-deferred.el" ];
+    meta = request.meta
+        // { description = "${request.meta.description} (deferred)"; };
   };
 
   rich-minority = melpaBuild rec {
