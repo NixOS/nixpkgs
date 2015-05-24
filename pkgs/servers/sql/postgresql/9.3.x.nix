adcf1c7a90b48eda3eec707f164c64b38af27e41
William A. Kennington III <william@wkennington.com>
2015-05-23 19:39:21 -0700

    postgresql: Fix sha256 for 9.3.7

--- pkgs/servers/sql/postgresql/9.3.x.nix
+++ pkgs/servers/sql/postgresql/9.3.x.nix
@@ -6,6 +6,6 @@ callPackage ./generic.nix (args // rec {
 
   src = fetchurl {
     url = "mirror://postgresql/source/v${version}/postgresql-${version}.tar.bz2";
-    sha256 = "09iqr9sldiq7jz1rdnywp2wv36lxy5m8kch3vpchd1s4fz75c7aw";
+    sha256 = "0ggz0i91znv053zx9qas7pjf93s5by3dk84z1jxbjkg8yyrnlx4b";
   };
 })
