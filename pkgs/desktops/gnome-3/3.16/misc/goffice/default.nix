2e9bc4aee3e14bdb749925b59f715d620c9ad7cb
Vladimír Čunát <vcunat@gmail.com>
2015-05-24 12:17:43 +0200

    goffice: update
    Some update(s) were lost during GNOME-3.12 drop 25f9555599.
    I hope there weren't much else lost.
    
    @lethalman: do you think it's good to have goffice inside gnome3?
    Nothing from inside refers to it, and it doesn't seem to be follow
    gnome's release cycle. In fact it rather seems tied to gnumeric instead.

--- pkgs/desktops/gnome-3/3.16/misc/goffice/default.nix
+++ pkgs/desktops/gnome-3/3.16/misc/goffice/default.nix
@@ -2,11 +2,11 @@
 , libgsf, libxml2, libxslt, cairo, pango, librsvg, libspectre }:
 
 stdenv.mkDerivation rec {
-  name = "goffice-0.10.12";
+  name = "goffice-0.10.22";
 
   src = fetchurl {
     url = "mirror://gnome/sources/goffice/0.10/${name}.tar.xz";
-    sha256 = "0vh0sdig5n8sxzh4xx82lm8y8d0jcdhc2ipb1kq02qs142zs74ff";
+    sha256 = "0206a87a323b52a874dc54491374245f9e1c5f62e93a2ce4a02fb444a26b0e28";
   };
 
   nativeBuildInputs = [ pkgconfig intltool ];
