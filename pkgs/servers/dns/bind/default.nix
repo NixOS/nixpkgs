9f70b1ab313ecfd6322e488879deb32ab58ccaf3
William A. Kennington III <william@wkennington.com>
2015-05-24 15:01:13 -0700

    bind: Add propagatedBuildInputs

--- pkgs/servers/dns/bind/default.nix
+++ pkgs/servers/dns/bind/default.nix
@@ -55,6 +55,11 @@ stdenv.mkDerivation rec {
     optReadline optLibcap optIdnkit optLibiconv optPostgresql optLibmysql
     optDb optOpenldap
   ];
+  # TODO(wkennington): Remove this hack once we fix header / .la  / .pc files
+  propagatedBuildInputs = [
+    optKerberos optOpenssl optLibxml2 optJson_c
+    optLibseccomp optLibcap
+  ];
 
   configureFlags = [
     (mkOther                          "localstatedir"       "/var")
