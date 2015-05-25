e81c6bd6547b778ffcabb715c516c3f66ef4155a
Michael Alan Dorman <mdorman@ironicdesign.com>
2015-05-25 08:13:21 -0400

    Use fetchzip to retrieve new pasystray.

--- pkgs/tools/audio/pasystray/default.nix
+++ pkgs/tools/audio/pasystray/default.nix
@@ -1,15 +1,15 @@
-{stdenv, fetchurl, unzip, autoconf, automake, makeWrapper, pkgconfig
+{stdenv, fetchzip, autoconf, automake, makeWrapper, pkgconfig
 , gnome3, avahi, gtk3, libnotify, pulseaudio, x11}:
 
 stdenv.mkDerivation rec {
   name = "pasystray-0.5.2";
 
-  src = fetchurl {
+  src = fetchzip {
     url = "https://github.com/christophgysin/pasystray/archive/${name}.zip";
     sha256 = "084jld5zk89h4akll73bwhfav6mpg55zmdd5kvlg396rqi9lqkj4";
   };
 
-  buildInputs = [ unzip autoconf automake makeWrapper pkgconfig 
+  buildInputs = [ autoconf automake makeWrapper pkgconfig 
                   gnome3.defaultIconTheme
                   avahi gtk3 libnotify pulseaudio x11 ];
 
