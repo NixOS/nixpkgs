bd67bfa3065e34a4719ac0da3fd02587aa999b7b
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-05-26 15:12:27 +0200

    neovim: various small improvements
    - Fix package name ("neovim-nightly" -> "neovim")
    - Use fetchFromGitHub, remove unused parameters
    - Improve metadata
    - Maintain

--- pkgs/applications/editors/neovim/default.nix
+++ pkgs/applications/editors/neovim/default.nix
@@ -1,34 +1,37 @@
-{ stdenv, fetchgit, fetchurl, unzip, callPackage, ncurses, gettext, pkgconfig,
-cmake, pkgs, lpeg, lua, luajit, luaMessagePack, luabitop }:
+{ stdenv, fetchFromGitHub, unzip, ncurses, gettext, pkgconfig
+, cmake, pkgs, lpeg, lua, luajit, luaMessagePack, luabitop }:
 
+let version = "2014-11-26"; in
 stdenv.mkDerivation rec {
-  name = "neovim-nightly";
+  name = "neovim-${version}";
 
-  version = "nightly";
-
-  src = fetchgit {
-    url = "https://github.com/neovim/neovim";
+  src = fetchFromGitHub {
+    sha256 = "1bcmv0h3ln736xdv7r7v97vim2yqfdnkvpbckwdxi69p4d6lfms6";
     rev = "68fcd8b696dae33897303c9f8265629a31afbf17";
-    sha256 = "0hxkcy641jpn4qka44gfvhmb6q3dkjx6lvn9748lcl2gx2d36w4i";
+    repo = "neovim";
+    owner = "neovim";
   };
 
   libmsgpack = stdenv.mkDerivation rec {
     version = "0.5.9";
     name = "libmsgpack-${version}";
 
-    src = fetchgit {
+    src = fetchFromGitHub {
+      sha256 = "12np3c2q346963mdgwa61y5dfnb91avq2hy4r6i6bdjwa7w6waq4";
       rev = "ecf4b09acd29746829b6a02939db91dfdec635b4";
-      url = "https://github.com/msgpack/msgpack-c";
-      sha256 = "076ygqgxrc3vk2l20l8x2cgcv05py3am6mjjkknr418pf8yav2ww";
+      repo = "msgpack-c";
+      owner = "msgpack";
     };
 
     buildInputs = [ cmake ];
 
+    enableParallelBuilding = true;
+
     meta = with stdenv.lib; {
       description = "MessagePack implementation for C and C++";
       homepage = http://msgpack.org;
-      maintainers = [ maintainers.manveru ];
       license = licenses.asl20;
+      maintainers = with maintainers; [ manveru nckx ];
       platforms = platforms.all;
     };
   };
@@ -56,10 +59,23 @@ stdenv.mkDerivation rec {
   ];
 
   meta = with stdenv.lib; {
-    description = "Aggressive refactor of Vim";
+    description = "Vim text editor fork focused on extensibility and agility";
+    longDescription = ''
+      Neovim is a project that seeks to aggressively refactor Vim in order to:
+      - Simplify maintenance and encourage contributions
+      - Split the work between multiple developers
+      - Enable the implementation of new/modern user interfaces without any
+        modifications to the core source
+      - Improve extensibility with a new plugin architecture
+    '';
     homepage    = http://www.neovim.org;
-    maintainers = with maintainers; [ manveru ];
+    # "Contributions committed before b17d96 by authors who did not sign the
+    # Contributor License Agreement (CLA) remain under the Vim license.
+    # Contributions committed after b17d96 are licensed under Apache 2.0 unless
+    # those contributions were copied from Vim (identified in the commit logs
+    # by the vim-patch token). See LICENSE for details."
+    license = with licenses; [ asl20 vim ];
+    maintainers = with maintainers; [ manveru nckx ];
     platforms   = platforms.unix;
   };
 }
-
