292abec58e5c194c7f54566f19c1a39d65eeda8c
Bart Brouns <bart@magnetopon.nl>
2015-05-28 00:24:06 +0200

    mod-distortion init at git-2015-05-18

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11749,6 +11749,8 @@ let
 
   moc = callPackage ../applications/audio/moc { };
 
+  mod-distortion = callPackage ../applications/audio/mod-distortion { };
+
   monero = callPackage ../applications/misc/monero { };
 
   monkeysAudio = callPackage ../applications/audio/monkeys-audio { };
