93ec6e3f2440aa7e98c6a9da57297660e0cad4f6
William A. Kennington III <william@wkennington.com>
2015-05-31 02:33:41 -0700

    glibc: Don't force rpath

--- pkgs/development/libraries/glibc/default.nix
+++ pkgs/development/libraries/glibc/default.nix
@@ -36,7 +36,7 @@ in
       if [ -f ${stdenv.cc.cc}/lib/libgcc_s.so.1 ]; then
           mkdir -p $out/lib
           cp ${stdenv.cc.cc}/lib/libgcc_s.so.1 $out/lib/libgcc_s.so.1
-          patchelf --set-rpath $out/lib --force-rpath $out/lib/libgcc_s.so.1
+          patchelf --set-rpath $out/lib $out/lib/libgcc_s.so.1
           # the .so It used to be a symlink, but now it is a script
           cp -a ${stdenv.cc.cc}/lib/libgcc_s.so $out/lib/libgcc_s.so
       fi
