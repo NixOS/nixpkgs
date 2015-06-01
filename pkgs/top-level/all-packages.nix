832aeb66f87fdd2fda341f2c58df570ac05e09c1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-06-01 21:33:51 +0200

    Revert "Fix krb5 reference"
    This reverts commit 9f0da5315b4595c45ace01727808e2ae8750efc0.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8588,7 +8588,7 @@ let
   ### SERVERS
 
   "389-ds-base" = callPackage ../servers/ldap/389 {
-    kerberos = libkrb5;
+    kerberos = krb5;
   };
 
   rdf4store = callPackage ../servers/http/4store { };
