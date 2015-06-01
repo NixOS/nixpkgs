16f87d772f68840b342285f550565e74850ef42d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-06-01 21:37:16 +0200

    Revert "libverto: Add derivation"
    This reverts commit 4389ae5208ec9b7e80b3b0e4ee10e2709333519f.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7213,10 +7213,6 @@ let
 
   libvdpau = callPackage ../development/libraries/libvdpau { };
 
-  libverto = callPackage ../development/libraries/libverto {
-    glib = null; # Don't include fairly heavy dependency
-  };
-
   libvirt = callPackage ../development/libraries/libvirt { };
 
   libvirt-glib = callPackage ../development/libraries/libvirt-glib { };
