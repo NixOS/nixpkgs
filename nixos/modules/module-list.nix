2607aec8f94847ce99be1d2fbd37f33f0cba67fe
Edward Tjörnhammar <ed@cflags.cc>
2015-06-09 00:11:24 +0200

    nixos: added gitit service

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -194,6 +194,7 @@
   ./services/misc/etcd.nix
   ./services/misc/felix.nix
   ./services/misc/folding-at-home.nix
+  ./services/misc/gitit.nix
   ./services/misc/gitlab.nix
   ./services/misc/gitolite.nix
   ./services/misc/gpsd.nix
