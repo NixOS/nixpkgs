ed9dc1fd9e1dec564e74f7b95f87f84613c81040
Edward Tjörnhammar <ed@cflags.cc>
2015-06-09 00:11:24 +0200

    nixos: added gitit service

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -223,6 +223,7 @@
       nix-serve = 199;
       tvheadend = 200;
       uwsgi = 201;
+      gitit = 202;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -424,6 +425,7 @@
       #nix-serve = 199; #unused
       #tvheadend = 200; #unused
       uwsgi = 201;
+      gitit = 202;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
