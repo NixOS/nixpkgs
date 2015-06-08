d8eefa300a6d0e149372e5f3a246b543062bcd48
Edward Tjörnhammar <ed@cflags.cc>
2015-06-09 00:11:24 +0200

    nixos: added gitit service

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -220,6 +220,7 @@
       grafana = 196;
       skydns = 197;
       ripple-rest = 198;
+      gitit = 199;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -418,6 +419,7 @@
       #grafana = 196; #unused
       #skydns = 197; #unused
       #ripple-rest = 198; #unused
+      gitit = 199;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
