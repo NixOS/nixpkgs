12be2af72358b156106d590d5b7c50f9e733a3e3
Kovacsics Robert (NixOS-SSD2) <kovirobi@gmail.com>
2015-06-09 13:37:36 +0100

    txt2tags: init at 2.6
    txt2tags is a KISS markup language
    
    (cherry picked from commit 7234e899131682416d357d531e9397fa3673e108)

---
+++ pkgs/tools/text/txt2tags/default.nix
@@ -0,0 +1,34 @@
+{ stdenv, fetchurl, python }:
+
+stdenv.mkDerivation rec {
+  version = "2.6";
+  name = "txt2tags-${version}";
+
+  dontBuild = true;
+
+  # Python script, needs the interpreter
+  propagatedBuildInputs = [ python ];
+
+  installPhase = ''
+    mkdir -p "$out/bin"
+    mkdir -p "$out/share/doc"
+    mkdir -p "$out/share/man/man1/"
+    sed '1s|/usr/bin/env python|${python}/bin/python|' < txt2tags > "$out/bin/txt2tags"
+    chmod +x "$out/bin/txt2tags"
+    gzip - < doc/manpage.man > "$out/share/man/man1/txt2tags.1.gz"
+    cp doc/userguide.pdf "$out/share/doc"
+    cp -r extras/ samples/ test/ "$out/share"
+  '';
+
+  src = fetchurl {
+    url = "http://txt2tags.googlecode.com/files/${name}.tgz";
+    sha256 = "0p5hql559pk8v5dlzgm75yrcxwvz4z30f1q590yzng0ghvbnf530";
+  };
+
+  meta = {
+    homepage = http://txt2tags.org/;
+    description = "A KISS markup language";
+    license  = stdenv.lib.licenses.gpl2;
+    maintainers = with stdenv.lib.maintainers; [ kovirobi ];
+  };
+}
