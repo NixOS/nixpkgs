d59aee74066239f9ed6ad8b187a73d71224041d9
Siarhei Zirukin <ftrvxmtrx@gmail.com>
2015-06-09 12:57:18 +0200

    efl, elementary: 1.14.0 -> 1.14.1, closes #8242

--- pkgs/desktops/e19/elementary.nix
+++ pkgs/desktops/e19/elementary.nix
@@ -1,10 +1,10 @@
 { stdenv, fetchurl, pkgconfig, e19, libcap, automake114x, autoconf, libdrm, gdbm }:
 stdenv.mkDerivation rec {
   name = "elementary-${version}";
-  version = "1.14.0";
+  version = "1.14.1";
   src = fetchurl {
     url = "http://download.enlightenment.org/rel/libs/elementary/${name}.tar.gz";
-    sha256 = "03h9sv5c3473wxf7hcimdf80d2phxl81yv0kzngx4g1b6cdwl1ma";
+    sha256 = "100bfzw6q57dnzcqg4zm0rqpvkk7zykfklnn6kcymv80j43xg0p1";
   };
   buildInputs = [ pkgconfig e19.efl libdrm gdbm automake114x autoconf ] ++ stdenv.lib.optionals stdenv.isLinux [ libcap ];
   NIX_CFLAGS_COMPILE = [ "-I${libdrm}/include/libdrm" ];
