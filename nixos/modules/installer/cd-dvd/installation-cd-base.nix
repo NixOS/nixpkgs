e5db79a8597873472c49d18061f6fd9e6f3e7437
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-06-10 12:04:26 +0200

    Move stuff to modules/profiles/installation-device.nix

--- nixos/modules/installer/cd-dvd/installation-cd-base.nix
+++ nixos/modules/installer/cd-dvd/installation-cd-base.nix
@@ -7,8 +7,7 @@ with lib;
 
 {
   imports =
-    [ ./channel.nix
-      ./iso-image.nix
+    [ ./iso-image.nix
 
       # Profiles of this basic installation CD.
       ../../profiles/all-hardware.nix
@@ -20,18 +19,6 @@ with lib;
   isoImage.isoName = "${config.isoImage.isoBaseName}-${config.system.nixosVersion}-${pkgs.stdenv.system}.iso";
 
   isoImage.volumeID = substring 0 11 "NIXOS_ISO";
-
-  # Make the installer more likely to succeed in low memory
-  # environments.  The kernel's overcommit heustistics bite us
-  # fairly often, preventing processes such as nix-worker or
-  # download-using-manifests.pl from forking even if there is
-  # plenty of free memory.
-  boot.kernel.sysctl."vm.overcommit_memory" = "1";
-
-  # To speed up installation a little bit, include the complete stdenv
-  # in the Nix store on the CD.  Archive::Cpio is needed for the
-  # initrd builder.  nixos-artwork is needed for the GRUB background.
-  isoImage.storeContents = [ pkgs.stdenv pkgs.busybox pkgs.perlPackages.ArchiveCpio pkgs.nixos-artwork ];
 
   # EFI booting
   isoImage.makeEfiBootable = true;
