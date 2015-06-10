957d9f6d574ac62d698424befb466e9b1376f82c
Peter Simons <simons@cryp.to>
2015-06-10 10:07:46 +0200

    zeromq: update to version 4.1.1
    Version 4.0.6 is "retired" and also failed to compile because its sha256
    hash changed on the upstream site for some reason.

--- pkgs/development/libraries/zeromq/4.x.nix
+++ pkgs/development/libraries/zeromq/4.x.nix
@@ -1,14 +1,14 @@
-{ stdenv, fetchurl, libuuid }:
+{ stdenv, fetchurl, libuuid, pkgconfig, libsodium }:
 
 stdenv.mkDerivation rec {
-  name = "zeromq-4.0.6";
+  name = "zeromq-4.1.1";
 
   src = fetchurl {
     url = "http://download.zeromq.org/${name}.tar.gz";
-    sha256 = "0arl8fy8d03xd5h0mgda1s5bajwg8iyh1kk4hd1420rpcxgkrj91";
+    sha256 = "1nxqvb1n8yy6953nv095riirrpmw4w340qd6sjm4cfdl0rbixmj3";
   };
 
-  buildInputs = [ libuuid ];
+  buildInputs = [ libuuid pkgconfig libsodium ];
 
   meta = with stdenv.lib; {
     branch = "4";
