085a0e8d242765c1ab348f12cd606c281223fc73
Robbin C <robbinch33@gmail.com>
2015-06-10 17:34:51 +0800

    dvd-slideshow: Add package.
    This adds dvd-slideshow, a suite of command line programs that creates a
    slideshow-style video from groups of pictures.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10767,6 +10767,8 @@ let
 
   dvdauthor = callPackage ../applications/video/dvdauthor { };
 
+  dvd-slideshow = callPackage ../applications/video/dvd-slideshow { };
+
   dwb = callPackage ../applications/networking/browsers/dwb { dconf = gnome3.dconf; };
 
   dwbWrapper = wrapFirefox
