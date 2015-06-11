6cb8e04a5c03f26ae6c1c524577ab971b769b708
Nikolay Amiantov <ab@fmap.me>
2015-06-12 02:00:35 +0300

    networkmanager-0.9.8: add package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2333,6 +2333,9 @@ let
 
   netselect = callPackage ../tools/networking/netselect { };
 
+  # stripped down, needed by steam
+  networkmanager098 = callPackage ../tools/networking/network-manager/0.9.8.nix { };
+
   networkmanager = callPackage ../tools/networking/network-manager { };
 
   networkmanager_openvpn = callPackage ../tools/networking/network-manager/openvpn.nix { };
