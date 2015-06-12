248a1b4d51555ce1ca5c3d8d496ec4ee9ef07f54
Luca Bruno <lethalman88@gmail.com>
2015-06-12 11:45:58 +0200

    nixos-artwork: only download the necessary artwork

--- pkgs/data/misc/nixos-artwork/default.nix
+++ pkgs/data/misc/nixos-artwork/default.nix
@@ -1,24 +1,23 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv, fetchurl }:
 
 stdenv.mkDerivation {
   name = "nixos-artwork-2015-02-27";
   # Remember to check the default lightdm wallpaper when updating
 
-  src = fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixos-artwork";
-    rev = "e71b6846023919136795ede22b16d73b2cf1693d";
-    sha256 = "167yvhm2qy7qgyrqqs4hv98mmlarhgxpcsyv0r8a9g3vkblfdczb";
+  GnomeDark = fetchurl {
+    url = https://raw.githubusercontent.com/NixOS/nixos-artwork/7ece5356398db14b5513392be4b31f8aedbb85a2/gnome/Gnome_Dark.png;
+    sha256 = "0c7sl9k4zdjwvdz3nhlm8i4qv4cjr0qagalaa1a438jigixx27l7";
   };
 
+  unpackPhase = "true";
+
   installPhase = ''
-    mkdir -p $out/share/artwork
-    cp -r * $out/share/artwork
-    find $out -name \*.xcf -exec rm {} \;
+    mkdir -p $out/share/artwork/gnome
+    ln -s $GnomeDark $out/share/artwork/gnome/Gnome_Dark.png
   '';
   
   meta = with stdenv.lib; {
-    homepage = "https://github.com/NixOS/nixos-artwork";
+    homepage = https://github.com/NixOS/nixos-artwork;
     platforms = platforms.all;
   };
 }
