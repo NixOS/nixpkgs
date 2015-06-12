e837e025ea3cecab35b38c6f8b3792fbe579dadd
Eduard Bachmakov <e.bachmakov@gmail.com>
2015-06-12 19:51:05 -0400

    mesa: minor update to 10.5.7
    Picked from #8369.

--- pkgs/development/libraries/mesa/default.nix
+++ pkgs/development/libraries/mesa/default.nix
@@ -23,7 +23,7 @@ else
 */
 
 let
-  version = "10.5.6";
+  version = "10.5.7";
   # this is the default search path for DRI drivers
   driverLink = "/run/opengl-driver" + stdenv.lib.optionalString stdenv.isi686 "-32";
 in
@@ -37,7 +37,7 @@ stdenv.mkDerivation {
       "https://launchpad.net/mesa/trunk/${version}/+download/mesa-${version}.tar.xz"
       "ftp://ftp.freedesktop.org/pub/mesa/${version}/mesa-${version}.tar.xz"
     ];
-    sha256 = "66017853bde5f7a6647db3eede30512a091a3491daa1708e0ad8027c328ba595";
+    sha256 = "0648cga781b2pqr8ny59wk7wmyfw8q7g8xnass4q1bv9rn86il04";
   };
 
   prePatch = "patchShebangs .";
