3e61595a3ead55491e0eddbdc16b7c8d09dbe406
Robert Irelan <rirelan@gmail.com>
2015-06-15 08:50:34 +0200

    subsonic: init at 5.2.1
    Add package for the Subsonic personal media streamer server
    (<http://subsonic.org>).

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3002,6 +3002,8 @@ let
 
   su = shadow.su;
 
+  subsonic = callPackage ../servers/misc/subsonic { };
+
   surfraw = callPackage ../tools/networking/surfraw { };
 
   swec = callPackage ../tools/networking/swec {
