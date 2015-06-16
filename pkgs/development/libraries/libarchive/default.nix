d954a695e752caa87cb84445726a4fd7bc4e4939
Vladimír Čunát <vcunat@gmail.com>
2015-06-16 18:34:18 +0200

    libarchive: fix CVE-2015-1197 by upstream patch
    Fixes #6799.
    
    (cherry picked from commit f2c84ffeb28e2681ef00bd339d4b92a681c082a0)

--- pkgs/development/libraries/libarchive/default.nix
+++ pkgs/development/libraries/libarchive/default.nix
@@ -14,6 +14,7 @@ stdenv.mkDerivation rec {
 
   patches = [
     ./CVE-2013-0211.patch # https://github.com/libarchive/libarchive/commit/22531545
+    ./CVE-2015-1197.patch # https://github.com/NixOS/nixpkgs/issues/6799
   ];
 
   buildInputs = [ sharutils libxml2 zlib bzip2 openssl xz ] ++
