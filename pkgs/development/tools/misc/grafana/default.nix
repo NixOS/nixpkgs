b0881eb3a7916d2efb6c1b11b2877f3fa7c13b20
James Baird <james@pancake-labs.com>
2015-06-15 17:16:27 -0700

    Grafana 2 package

--- pkgs/development/tools/misc/grafana/default.nix
+++ pkgs/development/tools/misc/grafana/default.nix
@@ -1,22 +1,19 @@
-{ stdenv, fetchurl, unzip, conf ? null }:
+{ stdenv, fetchurl }:
 
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "grafana-${version}";
-  version = "1.8.0-rc1";
+  version = "2.0.2";
 
   src = fetchurl {
-    url = "http://grafanarel.s3.amazonaws.com/${name}.zip";
-    sha256 = "1wx4zwkpgvb8lxcrkp67zgqd8aqms4bnxzwz3i9190sl55j1yf4i";
+    url = "https://grafanarel.s3.amazonaws.com/builds/${name}.linux-x64.tar.gz";
+    sha1 = "4291aada705bb69e32bd9467fbd6d0d0789e2c59";
   };
-
-  buildInputs = [ unzip ];
 
   phases = ["unpackPhase" "installPhase"];
   installPhase = ''
     mkdir -p $out && cp -R * $out
-    ${optionalString (conf!=null) ''cp ${conf} $out/config.js''}
   '';
 
   meta = {
