e3c98487af1d660baa616ccb7d1956ec0da29219
Daiderd Jordan <daiderd@gmail.com>
2015-06-17 15:22:33 +0200

    wxmac: new package
    - based on the current homebrew package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8227,6 +8227,8 @@ let
     withMesa = lib.elem system lib.platforms.mesaPlatforms;
   };
 
+  wxmac = callPackage ../development/libraries/wxmac { };
+
   wtk = callPackage ../development/libraries/wtk { };
 
   x264 = callPackage ../development/libraries/x264 { };
