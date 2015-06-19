0967c0fcdbe5716207f6c25d1a2b7c5703d83999
William A. Kennington III <william@wkennington.com>
2015-06-18 21:58:45 -0700

    wayland: 1.7.0 -> 1.8.1

--- pkgs/development/libraries/wayland/default.nix
+++ pkgs/development/libraries/wayland/default.nix
@@ -8,11 +8,11 @@ assert expat != null;
 
 stdenv.mkDerivation rec {
   name = "wayland-${version}";
-  version = "1.7.0";
+  version = "1.8.1";
 
   src = fetchurl {
     url = "http://wayland.freedesktop.org/releases/${name}.tar.xz";
-    sha256 = "173w0pqzk2m7hjlg15bymrx7ynxgq1ciadg03hzybxwnvfi4gsmx";
+    sha256 = "1j3gfzn8i0xhk3j34mwb2srrscjxfyi279jhyq80mz943j6r6z7i";
   };
 
   configureFlags = "--with-scanner --disable-documentation";
