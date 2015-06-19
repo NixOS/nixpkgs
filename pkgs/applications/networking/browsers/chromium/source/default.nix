c290595e5b60822d342c5e6354da4dac35a87c5a
aszlig <aszlig@redmoonstudios.org>
2015-06-19 04:34:47 +0200

    chromium: Update all channels to latest versions.
    Overview of the updated versions:
    
    stable: 43.0.2357.124 -> 43.0.2357.125
    beta:   44.0.2403.39  -> 44.0.2403.52  [1]
    dev:    45.0.2421.0   -> 45.0.2431.0   [2]
    
    [1] Beta channel fixes:
    
        I had to fetch a patch from a newer revision of Blink (r195908), see
        https://codereview.chromium.org/1157943002/ for more information
        about the patch.
    
        Here is the failing build log:
    
        https://headcounter.org/hydra/build/704037/nixlog/2
    
        Another fix for Blink, was needed in order to get it to build,
        see: https://codereview.chromium.org/1156113007/
    
        Also I needed to revert https://codereview.chromium.org/1150543002/
        using another patch, because the build won't succeed and bail out
        with WEBRTC_VIDEO_CODEC_FALLBACK_SOFTWARE not being found (it's only
        used once in the source, but isn't defined anywhere). Here are the
        trybot results:
    
        https://chromium-cq-status.appspot.com/patch-status/1150543002/20001
    
    [2] Dev channel fixes:
    
        The "ffmpegsumo" shared library is now no longer built, because it
        is now statically linked against Chromium, for details see:
    
        https://codereview.chromium.org/1141703002
    
    All channels built and tested on my Hydra at:
    
    https://headcounter.org/hydra/eval/178458
    
    Test reports:
    
    x86:    https://headcounter.org/hydra/build/704908/download/1/log.html
    x86_64: https://headcounter.org/hydra/build/704914/download/1/log.html
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/applications/networking/browsers/chromium/source/default.nix
+++ pkgs/applications/networking/browsers/chromium/source/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, python
+{ stdenv, fetchurl, fetchpatch, patchutils, python
 , channel ? "stable"
 , useOpenSSL # XXX
 }:
@@ -19,6 +19,7 @@ let
   ]);
 
   pre44 = versionOlder version "44.0.0.0";
+  is44 = versionOlder version "45.0.0.0" && !pre44;
 
 in stdenv.mkDerivation {
   name = "chromium-source-${version}";
@@ -47,11 +48,39 @@ in stdenv.mkDerivation {
     done
   '';
 
-  patches = if pre44 then [
-    ./nix_plugin_paths_42.patch
-  ] else [
-    ./nix_plugin_paths_44.patch
-  ];
+  patches = let
+    baseURL = "https://codereview.chromium.org/download";
+
+    mkBlinkFix = issue: sha256: fetchpatch {
+      url = "${baseURL}/issue${issue}.diff";
+      inherit sha256;
+      postFetch = ''
+        sed -i -e 's,^\(---\|+++\) *[ab]/,&third_party/WebKit/,' "$out"
+      '';
+    };
+
+    fixes44 = [
+      # WebPluginContainer::setNeedsLayout
+      # https://codereview.chromium.org/1157943002/
+      (mkBlinkFix "1157943002_20001"
+                  "0932yd15zlh2g5a5bbm6qrnfvv22jlfdg8pj0w9z58m5zdzw1p82")
+      # WebRuntimeFeatures::enablePermissionsAPI
+      # https://codereview.chromium.org/1156113007/
+      (mkBlinkFix "1156113007_1"
+                  "1v76brrgdziv1q62ba4bimg0my2dmnkyl68b21nv2vw661v0hzwh")
+      # Revert of https://codereview.chromium.org/1150543002/
+      (fetchpatch {
+        url = "${baseURL}/issue1150543002_1.diff";
+        sha256 = "0x9sya0m1zcb2vcp2vfss88qqdrh6bzcbx2ngfiql7rkbynnpqn6";
+        postFetch = ''
+          ${patchutils}/bin/interdiff "$out" /dev/null > reversed.patch
+          mv reversed.patch "$out"
+        '';
+      })
+    ];
+    pluginPaths = if pre44 then singleton ./nix_plugin_paths_42.patch
+                  else singleton ./nix_plugin_paths_44.patch;
+  in pluginPaths ++ optionals is44 fixes44;
 
   patchPhase = let
     diffmod = sym: "/^${sym} /{s/^${sym} //;${transform ""};s/^/${sym} /}";
