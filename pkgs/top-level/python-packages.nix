f4d876f73c4cc67f03d1e5529c8db4a6087ab7fe
Travis B. Hartwell <nafai@travishartwell.net>
2015-06-20 23:30:09 -0600

    i3-py: init at 0.6.4
    Extract i3-py from i3minator so it can be installed on its own.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3410,6 +3410,26 @@ let
     };
   };
 
+  i3-py = buildPythonPackage rec {
+    version = "0.6.4";
+    name = "i3-py-${version}";
+
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/source/i/i3-py/i3-py-${version}.tar.gz";
+      sha256 = "1sgl438jrb4cdyl7hbc3ymwsf7y3zy09g1gh7ynilxpllp37jc8y";
+    };
+
+    # no tests in tarball
+    doCheck = false;
+
+    meta = {
+      description = "tools for i3 users and developers";
+      homepage =  "https://github.com/ziberna/i3-py";
+      license = licenses.gpl3;
+      platforms = platforms.linux;
+    };
+  };
+
   jsonpatch = buildPythonPackage rec {
     name = "jsonpatch-1.8";
 
