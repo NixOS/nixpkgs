51fa6826559888cfc5dce2a93159b2474bf4eb1d
Jascha Geerds <jg@ekby.de>
2015-06-22 12:28:28 +0200

    hexchat: 2.9.6.1 -> 2.10.2

--- pkgs/applications/networking/irc/hexchat/default.nix
+++ pkgs/applications/networking/irc/hexchat/default.nix
@@ -1,22 +1,22 @@
 { stdenv, fetchurl, pkgconfig, gtk, perl, python, gettext
 , libtool, pciutils, dbus_glib, libcanberra, libproxy
-, libsexy, enchant, libnotify, openssl
+, libsexy, enchant, libnotify, openssl, intltool
 , desktop_file_utils, hicolor_icon_theme
 }:
 
 stdenv.mkDerivation rec {
-  version = "2.9.6.1";
+  version = "2.10.2";
   name = "hexchat-${version}";
 
   src = fetchurl {
     url = "http://dl.hexchat.net/hexchat/${name}.tar.xz";
-    sha256 = "0w34jr1pqril6r011fwxv40m17bnb88q9cv5hf08mv0a9lygyrv2";
+    sha256 = "0b5mw6jxa7c93nbgiwijm7j7klm6nccx6l9zyainyrbnqmjz7sw7";
   };
 
   buildInputs = [
     pkgconfig gtk perl python gettext
     libtool pciutils dbus_glib libcanberra libproxy
-    libsexy libnotify openssl
+    libsexy libnotify openssl intltool
     desktop_file_utils hicolor_icon_theme
   ];
 
