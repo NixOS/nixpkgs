634d9eb4c1a1260d7719b8d38b15613a9dd975cc
William Casarin <bill@casarin.me>
2015-06-22 17:48:41 -0700

    clib: a micro-package manager for C

---
+++ pkgs/tools/package-management/clib/default.nix
@@ -0,0 +1,23 @@
+{ stdenv, fetchzip, curl  }:
+
+stdenv.mkDerivation rec {
+  version = "1.4.2";
+  name = "clib-${version}";
+
+  src = fetchzip {
+    url = "https://github.com/clibs/clib/archive/${version}.zip";
+    sha256 = "0hbi5hf4w0iim96h89j7krxv61x92ffxjbldxp3zk92m5sgpldnm";
+  };
+
+  makeFlags = "PREFIX=$(out)";
+
+  buildInputs = [ curl ];
+
+  meta = with stdenv.lib; {
+    description = "C micro-package manager";
+    homepage = https://github.com/clibs/clib;
+    license = licenses.mit;
+    maintainers = with maintainers; [ jb55 ];
+    platforms = platforms.all;
+  };
+}
