74a07333fd70d9940fab341c33c2444868b235cc
Asko Soukka <asko.soukka@iki.fi>
2015-06-26 12:29:44 +0300

    darwin: gfortran: add needed inputs and other fixes
    Close #10532.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3948,7 +3948,9 @@ let
   }));
 
   gfortran = if !stdenv.isDarwin then gfortran49
-             else callPackage ../development/compilers/gcc/gfortran-darwin.nix {};
+             else callPackage ../development/compilers/gcc/gfortran-darwin.nix {
+    inherit (darwin) Libsystem;
+  };
 
   gfortran48 = wrapCC (gcc48.cc.override {
     name = "gfortran";
