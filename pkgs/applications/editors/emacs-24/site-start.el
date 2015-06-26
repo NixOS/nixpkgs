61ffbf2b14a297989442131b088b3e060fd25ced
Damien Cassou <damien@cassou.me>
2015-06-26 13:39:32 +0200

    emacs: Emacs now can access all NixOS info files
    This patch applies the same trick as in
    <nixpkgs>/nixos/modules/programs/info.nix to get a global `dir` file on
    NixOS.

--- pkgs/applications/editors/emacs-24/site-start.el
+++ pkgs/applications/editors/emacs-24/site-start.el
@@ -11,6 +11,23 @@
                                   (split-string (or (getenv "NIX_PROFILES") ""))))
                  woman-manpath)))
 
+;;; Generate a global 'dir' file out of the paths in INFOPATH. This is
+;;; equivalent to what is done in
+;;; <nixpkgs>/nixos/modules/programs/info.nix.
+(eval-after-load 'info
+  '(progn
+     ;; Emacs relies on the INFOPATH ending in `path-separator' to add
+     ;; `Info-default-directory-list' to the list of info paths.
+     (unless (string-match ":$" (getenv "INFOPATH"))
+       (setenv "INFOPATH" (concat (getenv "INFOPATH") ":")))
+     (info-initialize)
+     (let* ((tmp-directory (make-temp-file "emacs-info-dir" t))
+            (dir-file (expand-file-name "dir" tmp-directory)))
+       (dolist (path Info-directory-list)
+         (dolist (info-file (directory-files path t "\.info$" t))
+           (call-process "install-info" nil nil nil info-file dir-file)))
+       (add-to-list 'Info-directory-list tmp-directory))))
+
 ;; Make tramp work for remote NixOS machines
 ;;; NOTE: You might want to add 
 (eval-after-load 'tramp
