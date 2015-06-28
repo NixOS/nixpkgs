3ea1c5bc0c40342e4270443d6f8808210de4a045
William A. Kennington III <william@wkennington.com>
2015-06-27 19:59:44 -0700

    keepalived: Add libnl support

--- pkgs/tools/networking/keepalived/default.nix
+++ pkgs/tools/networking/keepalived/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, openssl, net_snmp }:
+{ stdenv, fetchurl, openssl, net_snmp, libnl }:
 
 stdenv.mkDerivation rec {
   name = "keepalived-1.2.17";
@@ -8,11 +8,15 @@ stdenv.mkDerivation rec {
     sha256 = "1w7px8phx3pyb3b56m3nz1a9ncx26q34fgy8j4n2dpi284jmqm6z";
   };
 
-  buildInputs = [ openssl net_snmp ];
+  buildInputs = [ openssl net_snmp libnl ];
 
   postPatch = ''
     sed -i 's,$(DESTDIR)/usr/share,$out/share,g' Makefile.in
   '';
+
+  # It doesn't know about the include/libnl<n> directory
+  NIX_CFLAGS_COMPILE="-I${libnl}/include/libnl3";
+  NIX_LDFLAGS="-lnl-3 -lnl-genl-3";
 
   configureFlags = [
     "--sysconfdir=/etc"
