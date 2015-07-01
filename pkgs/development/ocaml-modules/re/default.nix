af7b23649aa67b16603f56f3cb80474e09e44054
Vincent Laporte <Vincent.Laporte@gmail.com>
2015-07-01 20:16:50 +0200

    ocaml-re: 1.4.0 -> 1.4.1

--- pkgs/development/ocaml-modules/re/default.nix
+++ pkgs/development/ocaml-modules/re/default.nix
@@ -1,17 +1,19 @@
-{ stdenv, fetchzip, ocaml, findlib }:
+{ stdenv, fetchzip, ocaml, findlib, ounit }:
 
 stdenv.mkDerivation rec {
-  name = "ocaml-re-1.4.0";
+  name = "ocaml-re-1.4.1";
 
   src = fetchzip {
     url = "https://github.com/ocaml/ocaml-re/archive/${name}.tar.gz";
-    sha256 = "1xcfmhjgd7prqx2g1addlrk84xpfafmqnpjkqrvg7mw0hn1p7v2m";
+    sha256 = "1wmfgazydd20hc796zisqpmsw0sb5lv9g3x77ckmf50v3z8hyhvk";
   };
 
-  buildInputs = [ ocaml findlib ];
+  buildInputs = [ ocaml findlib ounit ];
 
-  configurePhase = "ocaml setup.ml -configure --prefix $out";
+  configurePhase = "ocaml setup.ml -configure --prefix $out --enable-tests";
   buildPhase = "ocaml setup.ml -build";
+  doCheck = true;
+  checkPhase = "ocaml setup.ml -test";
   installPhase = "ocaml setup.ml -install";
 
   createFindlibDestdir = true;
