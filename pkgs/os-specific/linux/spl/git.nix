ca5c1e418716896d5f7251ba3c720076f00ee69a
William A. Kennington III <william@wkennington.com>
2015-07-01 22:49:36 -0700

    spl_git: 2015-06-10 -> 2015-06-29

--- pkgs/os-specific/linux/spl/git.nix
+++ pkgs/os-specific/linux/spl/git.nix
@@ -1,12 +1,12 @@
 { callPackage, fetchgit, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "2015-06-10";
+  version = "2015-06-29";
 
   src = fetchgit {
     url = git://github.com/zfsonlinux/spl.git;
-    rev = "2345368646151718fa59986d9e2d9d38bcdecb2c";
-    sha256 = "08k7ahqgqrf9i118mkfxm01h8s607zp8lyvbvm1crii50dwlvl3g";
+    rev = "77ab5dd33a99bdf7fb062f0ea327582236a225b3";
+    sha256 = "1hbn8hi305cn15nlcm9x99nczjqjkhdc38hzww11xn78py8d90w9";
   };
 
   patches = [ ./const.patch ./install_prefix.patch ];
