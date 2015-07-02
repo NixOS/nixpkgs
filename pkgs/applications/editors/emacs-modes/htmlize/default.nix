8d26a3c1a4f51fb5dc0208cfb7eb1e47b295e4d9
Kevin Quick <kquick@godaddy.com>
2015-07-02 15:58:23 -0700

    htmlize: update source location

--- pkgs/applications/editors/emacs-modes/htmlize/default.nix
+++ pkgs/applications/editors/emacs-modes/htmlize/default.nix
@@ -3,12 +3,17 @@
 stdenv.mkDerivation {
   name = "htmlize-1.47";
 
-  builder = ./builder.sh;
-
   src = fetchurl {
-    url = http://fly.srk.fer.hr/~hniksic/emacs/htmlize.el.cgi;
+    url = https://raw.githubusercontent.com/emacsmirror/htmlize/master/htmlize.el;
     sha256 = "0m7lby95w9sj0xlqv39imlbp80x8ajd295cs6079jyhmryf6mr10";
   };
+
+  unpackPhase = ":;";
+    
+  installPhase = ''
+     mkdir -p $out/share/emacs/site-lisp
+     cp $src $out/share/emacs/site-lisp/htmlize.el
+  '';
 
   meta = {
     description = "Convert buffer text and decorations to HTML";
