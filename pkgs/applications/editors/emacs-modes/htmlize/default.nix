3f580459762abe44c53d1249d1ad75578b0e397b
Kevin Quick <kquick@godaddy.com>
2015-07-03 14:48:47 -0700

    Fix install now that tgz release file is used.

--- pkgs/applications/editors/emacs-modes/htmlize/default.nix
+++ pkgs/applications/editors/emacs-modes/htmlize/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
 
   installPhase = ''
      mkdir -p $out/share/emacs/site-lisp
-     cp $src $out/share/emacs/site-lisp/htmlize.el
+     cp htmlize.el $out/share/emacs/site-lisp/
   '';
 
   meta = {
