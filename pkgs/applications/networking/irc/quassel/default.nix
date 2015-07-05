06f1e2c61e54e5eee3ff7d953cb4bb84a7de30e8
William A. Kennington III <william@wkennington.com>
2015-07-05 16:52:56 -0700

    quassel: Add dependency on qca for crypto

--- pkgs/applications/networking/irc/quassel/default.nix
+++ pkgs/applications/networking/irc/quassel/default.nix
@@ -2,13 +2,12 @@
 , daemon ? false # build Quassel daemon
 , client ? false # build Quassel client
 , withKDE ? stdenv.isLinux # enable KDE integration
-, ssl ? true # enable SSL support
 , previews ? false # enable webpage previews on hovering over URLs
 , tag ? "" # tag added to the package name
 , kdelibs ? null # optional
 , useQt5 ? false
 , phonon_qt5, libdbusmenu_qt5
-, stdenv, fetchurl, cmake, makeWrapper, qt, automoc4, phonon, dconf }:
+, stdenv, fetchurl, cmake, makeWrapper, qt, automoc4, phonon, dconf, qca2, qca-qt5 }:
 
 
 assert stdenv.isLinux;
@@ -33,7 +32,7 @@ in with stdenv; mkDerivation rec {
   enableParallelBuilding = true;
 
   buildInputs = [ cmake makeWrapper ]
-    ++ (if useQt5 then [ qt.base ] else [ qt ])
+    ++ (if useQt5 then [ qt.base qca-qt5 ] else [ qt qca2 ])
     ++ (if useQt5 && (monolithic || daemon) then [ qt.script ] else [])
     ++ (if useQt5 && previews then [ qt.webkit qt.webkitwidgets ] else [])
     ++ lib.optional withKDE kdelibs
