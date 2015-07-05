df4fb819beea9d3cf431ec6f12cbb8ec04c8bd37
Peter Simons <simons@cryp.to>
2015-07-05 20:55:12 +0200

    ghc: add release candidate 2 for compiler version 7.10.2

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -34,6 +34,9 @@ rec {
     ghc7101 = callPackage ../development/compilers/ghc/7.10.1.nix ({ ghc = compiler.ghc784; } // stdenv.lib.optionalAttrs stdenv.isDarwin {
       libiconv = pkgs.darwin.libiconv;
     });
+    ghc7102 = callPackage ../development/compilers/ghc/7.10.2.nix ({ ghc = compiler.ghc784; } // stdenv.lib.optionalAttrs stdenv.isDarwin {
+      libiconv = pkgs.darwin.libiconv;
+    });
     ghcHEAD = callPackage ../development/compilers/ghc/head.nix ({ inherit (packages.ghc784) ghc alex happy; } // stdenv.lib.optionalAttrs stdenv.isDarwin {
       libiconv = pkgs.darwin.libiconv;
     });
@@ -82,6 +85,10 @@ rec {
     };
     ghc7101 = callPackage ../development/haskell-modules {
       ghc = compiler.ghc7101;
+      packageSetConfig = callPackage ../development/haskell-modules/configuration-ghc-7.10.x.nix { };
+    };
+    ghc7102 = callPackage ../development/haskell-modules {
+      ghc = compiler.ghc7102;
       packageSetConfig = callPackage ../development/haskell-modules/configuration-ghc-7.10.x.nix { };
     };
     ghcHEAD = callPackage ../development/haskell-modules {
