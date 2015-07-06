8f17099d6a6f978e4d37082c8b1d3d60808a7777
William A. Kennington III <william@wkennington.com>
2015-07-06 16:41:53 -0700

    bundler: 1.9.2 -> 1.10.5

--- pkgs/development/interpreters/ruby/bundler.nix
+++ pkgs/development/interpreters/ruby/bundler.nix
@@ -1,9 +1,9 @@
 { buildRubyGem, makeWrapper, ruby, coreutils }:
 
 buildRubyGem {
-  name = "bundler-1.9.2";
+  name = "bundler-1.10.5";
   namePrefix = "";
-  sha256 = "0ck9bnqg7miimggj1d6qlabrsa5h9yaw241fqn15cvqh915209zk";
+  sha256 = "1zkxw6699bbmsamrij2lirscbh0j58p1p3bql22jsxvx34j6w5nc";
   dontPatchShebangs = true;
   postInstall = ''
     find $out -type f -perm +0100 | while read f; do
