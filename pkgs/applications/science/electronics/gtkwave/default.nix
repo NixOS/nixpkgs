026f09cea59f4e21e2c99a41e15a8a3bdf8abcb2
Lluís Batlle i Rossell <viric@viric.name>
2015-07-07 12:10:32 +0200

    Updating gtkwave 3.3.64 to 3.3.66

--- pkgs/applications/science/electronics/gtkwave/default.nix
+++ pkgs/applications/science/electronics/gtkwave/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, gtk, gperf, pkgconfig, bzip2, tcl, tk, judy, xz}:
 stdenv.mkDerivation rec {
-  name = "gtkwave-3.3.64";
+  name = "gtkwave-3.3.66";
 
   src = fetchurl {
     url = "mirror://sourceforge/gtkwave/${name}.tar.gz";
-    sha256 = "05bisdh82rsbs0pj0687dzbp0s30p3wzq4gypb9bbjaxwnrmdsfs";
+    sha256 = "17mrmgr6zzl1x1qml0wmjjhcbw7bg3w4mqy60rsy0z6gxd8hikh4";
   };
 
   buildInputs = [ gtk gperf pkgconfig bzip2 tcl tk judy xz ];
