6aa4714dbad2acf3defe340f0c52dd315a95adf7
William A. Kennington III <william@wkennington.com>
2015-07-06 17:37:18 -0700

    coreutils: 8.23 -> 8.24

--- pkgs/tools/misc/coreutils/default.nix
+++ pkgs/tools/misc/coreutils/default.nix
@@ -12,11 +12,11 @@ with { inherit (stdenv.lib) optional optionals optionalString optionalAttrs; };
 
 let
   self = stdenv.mkDerivation rec {
-    name = "coreutils-8.23";
+    name = "coreutils-8.24";
 
     src = fetchurl {
       url = "mirror://gnu/coreutils/${name}.tar.xz";
-      sha256 = "0bdq6yggyl7nkc2pbl6pxhhyx15nyqhz3ds6rfn448n6rxdwlhzc";
+      sha256 = "0w11jw3fb5sslf0f72kxy7llxgk1ia3a6bcw0c9kmvxrlj355mx2";
     };
 
     patches = if stdenv.isCygwin then [ ./coreutils-8.23-4.cygwin.patch ] else null;
