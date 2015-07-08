6bc221f3a64fe8f7217c00c41d2caee4d30b3e6a
Ryan Scheel (Havvy) <ryan.havvy@gmail.com>
2015-07-08 13:04:38 +0200

    Update rustc (v1.0.0 -> v1.1.0)
    Also add myself as a maintainer.

--- pkgs/development/compilers/rustc/1.0.0.nix
+++ pkgs/development/compilers/rustc/1.0.0.nix
@@ -1,14 +1,33 @@
 { stdenv, callPackage }:
 callPackage ./generic.nix {
-  shortVersion = "1.0.0";
+  shortVersion = "1.1.0";
   isRelease = true;
-  srcSha = "14brziw91d3r88fa1kvpvhap5ws4z8h2mas7h6k9lpsc2zl9blak";
-  snapshotHashLinux686 = "1ef82402ed16f5a6d2f87a9a62eaa83170e249ec";
-  snapshotHashLinux64 = "ef2154372e97a3cb687897d027fd51c8f2c5f349";
-  snapshotHashDarwin686 = "0310b1a970f2da7e61770fd14dbbbdca3b518234";
-  snapshotHashDarwin64 = "5f35d9c920b8083a7420ef8cf5b00d5ef3085dfa";
-  snapshotDate = "2015-03-27";
-  snapshotRev = "5520801";
+  srcSha = "0lsfrclj5imxy6129ggya7rb2h04cgqq53f75z2jv40y5xk25sy8";
+
+  /* Rust is bootstrapped from an earlier built version. We need
+  to fetch these earlier versions, which vary per platform.
+  The shapshot info you want can be found at
+
+  https://github.com/rust-lang/rust/blob/{$shortVersion}/src/snapshots.txt
+
+  with the set you want at the top.
+  */
+
+  # linux-i386
+  snapshotHashLinux686 = "0bc8cffdce611fb71fd7d3d8e7cdbfaf748a4f16";
+
+  # linux-x86_64
+  snapshotHashLinux64 = "94089740e48167c5975c92c139ae9c286764012f";
+
+  # macos-i386
+  snapshotHashDarwin686 = "54cc35e76497e6e94fddf38d6e40e9d168491ddb";
+
+  # macos-x86_64
+  snapshotHashDarwin64 = "43a1c1fba0d1dfee4c2ca310d506f8f5f51b3f6f";
+
+  snapshotDate = "2015-04-27";
+  snapshotRev = "857ef6e";
+
   patches = [
     ./patches/beta.patch
     ] ++ stdenv.lib.optional stdenv.needsPax ./patches/grsec.patch;
