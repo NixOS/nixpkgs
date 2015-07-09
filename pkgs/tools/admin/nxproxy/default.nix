98e7674e88162fc0b6517faa962233c8b3ae75e4
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-07-09 02:39:50 +0200

    nxproxy, libxcomp: nxlibs 3.5.0.{31 -> 32}
    Removes the need for patching (for now).

--- pkgs/tools/admin/nxproxy/default.nix
+++ pkgs/tools/admin/nxproxy/default.nix
@@ -1,25 +1,22 @@
 { stdenv, fetchurl, autoreconfHook, libxcomp }:
 
-let version = "3.5.0.31"; in
+let version = "3.5.0.32"; in
 stdenv.mkDerivation {
   name = "nxproxy-${version}";
 
   src = fetchurl {
-    sha256 = "1hi3xrjzr37zs72djw3k7gj6mn2bsihfw1iysl8l0i85jl6sdfkd";
+    sha256 = "02n5bdc1jsq999agb4w6dmdj5l2wlln2lka84qz6rpswwc59zaxm";
     url = "http://code.x2go.org/releases/source/nx-libs/nx-libs-${version}-lite.tar.gz";
   };
 
   meta = with stdenv.lib; {
+    inherit version;
     description = "NX compression proxy";
     homepage = "http://wiki.x2go.org/doku.php/wiki:libs:nx-libs";
     license = licenses.gpl2;
     platforms = with platforms; linux;
     maintainers = with maintainers; [ nckx ];
   };
-
-  patches = [
-    ./0660_nxcomp_fix-negotiation-in-stage-10-error.full+lite.patch
-  ];
 
   buildInputs = [ libxcomp ];
   nativeBuildInputs = [ autoreconfHook ];
