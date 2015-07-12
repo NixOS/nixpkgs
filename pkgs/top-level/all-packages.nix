9b863fac6b812f78754b1fcc89e64e93f59acd42
James Felix Black <james_black@apple.com>
2015-07-12 20:00:43 +0200

    notion: init at 3-2015061300
    The notion (not-ion) window manager [1], plus optional (defaulted to
    true) Xft/pango support (via [2]). Defined a license (notion uses a
    modified LGPL).
    
    As per [3], changes:
    
      1. renamed option from pango -> xft, better to reflect the actual
         nature of the patch;
      2. modified the patch to replace the postPatch changes to system-autodeteck.mk;
      3. commited the patch to the nixpkgs repo;
      4. removed 'fallbackFont' as an option. Use a sensible default, instead.
    
    Many stylistic changes, thanks to @lethalman.
    
    [1] http://notion.sourceforge.net
    [2] http://sourceforge.net/p/notion/feature-requests/47/
    [3] https://github.com/NixOS/nixpkgs/pull/8789

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12112,6 +12112,8 @@ let
 
   mutt-kz = callPackage ../applications/networking/mailreaders/mutt-kz { };
 
+  notion = callPackage ../applications/window-managers/notion { };
+
   openshift = callPackage ../applications/networking/cluster/openshift { };
 
   panamax_api = callPackage ../applications/networking/cluster/panamax/api {
