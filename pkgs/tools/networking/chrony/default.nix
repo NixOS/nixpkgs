1a79058a8185342f1703d1ab579c8a81abfcda22
Franz Pletz <fpletz@fnordicwalking.de>
2015-07-12 05:59:53 +0200

    chrony: Add fpletz to maintainers

--- pkgs/tools/networking/chrony/default.nix
+++ pkgs/tools/networking/chrony/default.nix
@@ -26,7 +26,7 @@ stdenv.mkDerivation rec {
     repository.git = git://git.tuxfamily.org/gitroot/chrony/chrony.git;
     license = licenses.gpl2;
     platforms = with platforms; linux ++ freebsd ++ openbsd;
-    maintainers = [ maintainers.rickynils ];
+    maintainers = with maintainers; [ rickynils fpletz ];
 
     longDescription = ''
       Chronyd is a daemon which runs in background on the system. It obtains measurements via the network of the system clock’s offset relative to time servers on other systems and adjusts the system time accordingly. For isolated systems, the user can periodically enter the correct time by hand (using Chronyc). In either case, Chronyd determines the rate at which the computer gains or loses time, and compensates for this. Chronyd implements the NTP protocol and can act as either a client or a server.
