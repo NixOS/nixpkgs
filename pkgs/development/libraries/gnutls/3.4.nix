08ef62c1009c27db1c6a5750c80c4af01ee791f2
William A. Kennington III <william@wkennington.com>
2015-07-15 12:48:02 -0700

    gnutls: 3.4.2 -> 3.4.3

--- pkgs/development/libraries/gnutls/3.4.nix
+++ pkgs/development/libraries/gnutls/3.4.nix
@@ -1,10 +1,10 @@
 { callPackage, fetchurl, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "3.4.2";
+  version = "3.4.3";
 
   src = fetchurl {
     url = "ftp://ftp.gnutls.org/gcrypt/gnutls/v3.4/gnutls-${version}.tar.lz";
-    sha256 = "1wzasbrs4ncq4yisqyvifl7mzlyyg1pb0idr4fhjmcfpi13sxlaw";
+    sha256 = "1q4adb1xi9pl00iy3cqs4r1qmwllv1g1r44p6xsg6n65dpyf53q2";
   };
 })
