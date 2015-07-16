e9fd07c706723b89b8b454ada6fda2c1b8a621ab
Joachim Fasting <joachifm@fastmail.fm>
2015-07-16 03:45:18 +0200

    dssi: reimplement using mkDerivation

--- pkgs/development/libraries/dssi/default.nix
+++ pkgs/development/libraries/dssi/default.nix
@@ -1,50 +1,29 @@
-x@{builderDefsPackage
-  , ladspaH, libjack2, liblo, alsaLib, qt4, libX11, libsndfile, libSM
-  , libsamplerate, libtool, autoconf, automake, xproto, libICE, pkgconfig
-  , ...}:
-builderDefsPackage
-(a :  
-let 
-  helperArgNames = ["stdenv" "fetchurl" "builderDefsPackage"] ++ 
-    [];
+{ stdenv, fetchurl, ladspaH, libjack2, liblo, alsaLib, qt4, libX11, libsndfile, libSM
+, libsamplerate, libtool, autoconf, automake, xproto, libICE, pkgconfig
+}:
 
-  buildInputs = map (n: builtins.getAttr n x)
-    (builtins.attrNames (builtins.removeAttrs x helperArgNames));
-  sourceInfo = rec {
-    baseName="dssi";
-    version="1.1.1";
-    project="${baseName}";
-    name="${baseName}-${version}";
-    url="mirror://sourceforge/project/${project}/${baseName}/${version}/${name}.tar.gz";
-    hash="0kl1hzhb7cykzkrqcqgq1dk4xcgrcxv0jja251aq4z4l783jpj7j";
-  };
-in
-rec {
-  src = a.fetchurl {
-    url = sourceInfo.url;
-    sha256 = sourceInfo.hash;
+stdenv.mkDerivation rec {
+  name = "dssi-${version}";
+  version = "1.1.1";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/project/dssi/dssi/${version}/${name}.tar.gz";
+    sha256 = "0kl1hzhb7cykzkrqcqgq1dk4xcgrcxv0jja251aq4z4l783jpj7j";
   };
 
-  inherit (sourceInfo) name version;
-  inherit buildInputs;
+  buildInputs =
+    [ ladspaH libjack2 liblo alsaLib qt4 libX11 libsndfile libSM
+      libsamplerate libtool autoconf automake xproto libICE pkgconfig
+    ];
 
-  /* doConfigure should be removed if not needed */
-  phaseNames = ["doConfigure" "doMakeInstall"];
-      
-  meta = {
+  meta = with stdenv.lib; {
     description = "A plugin SDK for virtual instruments";
-    maintainers = with a.lib.maintainers;
+    maintainers = with maintainers;
     [
       raskin
     ];
-    platforms = with a.lib.platforms;
-      linux;
-    license = a.lib.licenses.lgpl21;
+    platforms = platforms.linux;
+    license = licenses.lgpl21;
+    downloadPage = "http://sourceforge.net/projects/dssi/files/dssi/";
   };
-  passthru = {
-    updateInfo = {
-      downloadPage = "http://sourceforge.net/projects/dssi/files/dssi/";
-    };
-  };
-}) x
-
+}
