55072502d2ade748053c45ee896be44f204c401a
Lluís Batlle i Rossell <viric@viric.name>
2015-07-18 00:14:56 +0200

    Updating retroshare 0.6.0 from some svn to rc2

--- pkgs/applications/networking/p2p/retroshare/0.6.nix
+++ pkgs/applications/networking/p2p/retroshare/0.6.nix
@@ -1,16 +1,21 @@
-{ stdenv, fetchsvn, cmake, qt, libupnp, gpgme, gnome3, glib, libssh, pkgconfig, protobuf, bzip2
-, libXScrnSaver, speex, curl, libxml2, libxslt, sqlcipher }:
+{ stdenv, fetchurl, cmake, qt, libupnp, gpgme, gnome3, glib, libssh, pkgconfig, protobuf, bzip2
+, libXScrnSaver, speex, curl, libxml2, libxslt, sqlcipher, libmicrohttpd, opencv
+}:
 
 stdenv.mkDerivation {
-  name = "retroshare-0.6-svn-7445";
+  name = "retroshare-0.6.0-rc2";
 
-  src = fetchsvn {
-    url = svn://svn.code.sf.net/p/retroshare/code/trunk;
-    rev = 7445;
-    sha256 = "1dqh65bn21g7ix752ddrr10kijjdwjgjipgysyxnm90zjmdlx3cc";
+  src = fetchurl {
+    url = "mirror://sourceforge/retroshare/retroshare_0.6.0.RC2~8551_src.tgz";
+    name = "retroshare_0.6.0.RC2-8551_src.tgz";
+    sha256 = "0l705m643w58g8wbppfml7zpflln24c63nhyn146rph5m5nnf813";
   };
 
   NIX_CFLAGS_COMPILE = "-I${glib}/include/glib-2.0 -I${glib}/lib/glib-2.0/include -I${libxml2}/include/libxml2 -I${sqlcipher}/include/sqlcipher";
+
+  postUnpack = ''
+    sourceRoot=`pwd`/retroshare06-0.6.0/src
+  '';
 
   patchPhase = ''
     # Fix build error
@@ -29,7 +34,8 @@ stdenv.mkDerivation {
   #    retroshare-nogui/src/retroshare-nogui.pro
 
   buildInputs = [ speex qt libupnp gpgme gnome3.libgnome_keyring glib libssh pkgconfig
-                  protobuf bzip2 libXScrnSaver curl libxml2 libxslt sqlcipher ];
+                  protobuf bzip2 libXScrnSaver curl libxml2 libxslt sqlcipher
+                  libmicrohttpd opencv ];
 
   configurePhase = ''
     qmake PREFIX=$out DESTDIR=$out RetroShare.pro
