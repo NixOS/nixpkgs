041459a0d2da7fa4dbdd8506812e0a17ed750872
Matthias Beyer <mail@beyermatthias.de>
2015-07-18 18:49:52 +0200

    Use stdenv.shell instead of /bin/sh

--- pkgs/games/gsb/default.nix
+++ pkgs/games/gsb/default.nix
@@ -7,12 +7,12 @@ assert stdenv.system == "i686-linux" || stdenv.system == "x86_64-linux";
 stdenv.mkDerivation rec {
   name = "gsb-1.56.0";
 
-  goBuyItNow = '' 
+  goBuyItNow = ''
     We cannot download the full version automatically, as you require a license.
     Once you bought a license, you need to add your downloaded version to the nix store.
     You can do this by using "nix-prefetch-url file://gsb1324679796.tar.gz" in the
     directory where you saved it.
-  ''; 
+  '';
 
   src = requireFile {
      message = goBuyItNow;
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
   phases = "unpackPhase installPhase";
 
   # XXX: stdenv.lib.makeLibraryPath doesn't pick up /lib64
-  libPath = stdenv.lib.makeLibraryPath [ stdenv.cc.cc stdenv.cc.libc ] 
+  libPath = stdenv.lib.makeLibraryPath [ stdenv.cc.cc stdenv.cc.libc ]
     + ":" + stdenv.lib.makeLibraryPath [ SDL SDL_image libjpeg62 libpng12 mesa ]
     + ":" + stdenv.lib.makeLibraryPath [ curl3 openal libvorbis libogg ]
     + ":" + stdenv.lib.makeLibraryPath [ libX11 libXext libXft fontconfig zlib ]
@@ -47,7 +47,7 @@ stdenv.mkDerivation rec {
     #makeWrapper doesn't do cd. :(
 
     cat > $out/bin/GSB << EOF
-    #!/bin/sh
+    #! ${stdenv.shell}
     cd $out/libexec/positech/GSB
     exec ./GSB.bin.$arch
     EOF
