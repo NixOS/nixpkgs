93a5950b2f64e5e8f8a36d79563aaef37406fa75
Matthias Beyer <mail@beyermatthias.de>
2015-07-18 18:41:21 +0200

    Use stdenv.shell instead of ${bash}/bin/sh

--- pkgs/applications/networking/remote/teamviewer/10.nix
+++ pkgs/applications/networking/remote/teamviewer/10.nix
@@ -30,7 +30,7 @@ stdenv.mkDerivation {
     rm -R $out/share/teamviewer/tv_bin/wine/{bin,lib,share}
 
     cat > $out/bin/teamviewer << EOF
-    #!${bash}/bin/sh
+    #! ${stdenv.shell}
     export LD_LIBRARY_PATH=${toldpath}\''${LD_LIBRARY_PATH:+:\$LD_LIBRARY_PATH}
     export PATH=${topath}\''${PATH:+:\$PATH}
     $out/share/teamviewer/tv_bin/script/teamviewer "\$@"
