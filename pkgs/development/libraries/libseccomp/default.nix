6dac59b09622a10234605cea1841ffb04cc318d8
Austin Seipp <aseipp@pobox.com>
2015-07-20 13:28:49 -0500

    nixpkgs: libseccomp 2.2.1 -> 2.2.3
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/development/libraries/libseccomp/default.nix
+++ pkgs/development/libraries/libseccomp/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name    = "libseccomp-${version}";
-  version = "2.2.1";
+  version = "2.2.3";
 
   src = fetchFromGitHub {
     owner = "seccomp";
     repo = "libseccomp";
     rev = "v${version}";
-    sha256 = "153k3jflcgij19nxghmwlvqlngl84vkld514d31490c6sfkr5fy2";
+    sha256 = "0pl827qjls5b6kjj8qxxdwcn6rviqbm5xjqf0hgx6b04c836mswx";
   };
 
   buildInputs = [ autoreconfHook getopt ];
