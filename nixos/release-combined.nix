606d34e45aeedc843f0ddb375e1fb1aa9be429fd
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-07-22 15:20:40 +0200

    Revert "Revert "Drop tests.installer.grub1 as release critical""
    This reverts commit cd28c5517c224c0c969f6bccb1f85dcc280ed7f2. Still broken:
    
    http://hydra.nixos.org/build/23928152

--- nixos/release-combined.nix
+++ nixos/release-combined.nix
@@ -52,7 +52,6 @@ in rec {
         (all nixos.tests.firefox)
         (all nixos.tests.firewall)
         (all nixos.tests.gnome3)
-        (all nixos.tests.installer.grub1)
         (all nixos.tests.installer.lvm)
         (all nixos.tests.installer.luksroot)
         (all nixos.tests.installer.separateBoot)
