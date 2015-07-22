6f5aa25e89209f29457bc6fd09643bee1370152b
Luca Bruno <lethalman88@gmail.com>
2015-07-22 10:29:34 +0200

    Revert "zfs_git: 2015-06-22 -> 2015-07-01"
    This reverts commit 54022375eb3ae3375385717690ef133c819c9bf1.
    
    Revert "zfs_git: 2015-05-13 -> 2015-06-22"
    
    This reverts commit 59faa759dade5c629409d683e72523548c40f554.
    
    Closes #8941

--- pkgs/os-specific/linux/zfs/git.nix
+++ pkgs/os-specific/linux/zfs/git.nix
@@ -1,12 +1,12 @@
 { callPackage, stdenv, fetchgit, spl_git, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "2015-07-01";
+  version = "2015-05-13";
 
   src = fetchgit {
     url = git://github.com/zfsonlinux/zfs.git;
-    rev = "a7b10a931911d3a98a90965795daad031c6d33a2";
-    sha256 = "0llqbc37f4szx7mwi6j3xmxxz03g3ib7cwypmpcyi0nwkssav5xi";
+    rev = "7fec46b9d8967109ad289d208e8cf36a0c16e40c";
+    sha256 = "0gvzw6vn7wyq2g9psv0fdars7ssidqc5l85x4yym5niccy1xl437";
   };
 
   patches = [ ./nix-build.patch ];
