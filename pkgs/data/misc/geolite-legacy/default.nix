dfeaaa89098bc89f79d1686faf05b1a55d36fa54
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-07-23 04:05:00 +0200

    geolite-legacy: use correct local naming scheme
    Following http://dev.maxmind.com/geoip/legacy/install/city/:
    
      "Then rename the *.dat file to GeoIP.dat, GeoIPRegion.dat, or
       GeoIPCity.dat, for the GeoIP Country, Region or City databases,
       respectively."
    
    Using the default "GeoLiteCity*.dat" name caused ipv6calc to miss it
    (verified using `ipv6calc -vv`).

--- pkgs/data/misc/geolite-legacy/default.nix
+++ pkgs/data/misc/geolite-legacy/default.nix
@@ -1,9 +1,9 @@
 { stdenv, fetchurl }:
 
 let
-  fetchDB = name: sha256: fetchurl {
-    inherit sha256;
-    url = "https://geolite.maxmind.com/download/geoip/database/${name}";
+  fetchDB = src: name: sha256: fetchurl {
+    inherit name sha256;
+    url = "https://geolite.maxmind.com/download/geoip/database/${src}";
   };
 
   # Annoyingly, these files are updated without a change in URL. This means that
@@ -13,17 +13,23 @@ in
 stdenv.mkDerivation {
   name = "geolite-legacy-${version}";
 
-  srcGeoIP = fetchDB "GeoLiteCountry/GeoIP.dat.gz"
+  srcGeoIP = fetchDB
+    "GeoLiteCountry/GeoIP.dat.gz" "GeoIP.dat.gz"
     "0c6jcmlgkybsqiwqwa21igjazf95dj38mn516cqqqfdg7ciaj1d5";
-  srcGeoIPv6 = fetchDB "GeoIPv6.dat.gz"
+  srcGeoIPv6 = fetchDB
+    "GeoIPv6.dat.gz" "GeoIPv6.dat.gz"
     "1vi82p41vas18yp17yk236pn1xamsi9662aav79fa0hm43i3ydx3";
-  srcGeoLiteCity = fetchDB "GeoLiteCity.dat.xz"
+  srcGeoLiteCity = fetchDB
+    "GeoLiteCity.dat.xz" "GeoIPCity.dat.xz"
     "0x5ihg7qikzc195nix9r0izvbdnj4hy4rznvaxk56rf8yqcigdyv";
-  srcGeoLiteCityv6 = fetchDB "GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz"
+  srcGeoLiteCityv6 = fetchDB
+    "GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz" "GeoIPCityv6.dat.gz"
     "0xjzg76vdsayxyy1yyw64w781vad4c9nbhw61slh2qmazdr360g9";
-  srcGeoIPASNum = fetchDB "asnum/GeoIPASNum.dat.gz"
+  srcGeoIPASNum = fetchDB
+    "asnum/GeoIPASNum.dat.gz" "GeoIPASNum.dat.gz"
     "18kxswr0b5klimfpj1zhxipvyvrljvcywic4jc1ggcr44lf4hj9w";
-  srcGeoIPASNumv6 = fetchDB "asnum/GeoIPASNumv6.dat.gz"
+  srcGeoIPASNumv6 = fetchDB
+    "asnum/GeoIPASNumv6.dat.gz" "GeoIPASNumv6.dat.gz"
     "0asnmmirridiy57zm0kccb7g8h7ndliswfv3yfk7zm7dk98njnxs";
 
   meta = with stdenv.lib; {
