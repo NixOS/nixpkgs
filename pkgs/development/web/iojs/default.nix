89cda4c95a2447973ca4d5be251fc7e1d83fa2a9
Ryan Scheel (Havvy) <ryan.havvy@gmail.com>
2015-07-23 06:25:15 +0200

    iojs (2.3.4 -> 2.4.0)

--- pkgs/development/web/iojs/default.nix
+++ pkgs/development/web/iojs/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, python, utillinux, openssl_1_0_2, http-parser, zlib, libuv }:
 
 let
-  version = "2.3.4";
+  version = "2.4.0";
   inherit (stdenv.lib) optional maintainers licenses platforms;
 in stdenv.mkDerivation {
   name = "iojs-${version}";
 
   src = fetchurl {
     url = "https://iojs.org/dist/v${version}/iojs-v${version}.tar.gz";
-    sha256 = "1h9cjrs93c8rdycc0ahhc27wv826211aljvfmxfg8jdmg6nvibhq";
+    sha256 = "0g81bn8q4zgm8skkbxbzwa22dnpbing4b5wjqacvpxq3ygz4c98y";
   };
 
   prePatch = ''
