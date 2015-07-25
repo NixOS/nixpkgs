9bedb70520f87e6b8f2beacbf2a09ac457da56c8
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-07-25 13:34:34 +0200

    man-pages: 4.00 -> 4.01

--- pkgs/data/documentation/man-pages/default.nix
+++ pkgs/data/documentation/man-pages/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl }:
 
-let version = "4.00"; in
+let version = "4.01"; in
 stdenv.mkDerivation rec {
   name = "man-pages-${version}";
 
   src = fetchurl {
     url = "mirror://kernel/linux/docs/man-pages/${name}.tar.xz";
-    sha256 = "18zb1g12s15sanffh0sykmmyx0j176pp7q1xxs0gk0imgvmn8hj4";
+    sha256 = "116jp2rnsdlnb3cwnbfp0g053frcmchndwyrj714swl1lgabb56i";
   };
 
   makeFlags = "MANDIR=$(out)/share/man";
