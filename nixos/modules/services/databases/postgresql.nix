ace332eb36fd537c9a2674d591fd5e723f8fcb62
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-07-27 20:26:19 +0200

    Set a default value for services.postgresql.package
    This is finally possible now that we have ‘system.stateVersion’
    (allowing the default package to change over time).

--- nixos/modules/services/databases/postgresql.nix
+++ nixos/modules/services/databases/postgresql.nix
@@ -154,6 +154,12 @@ in
 
   config = mkIf config.services.postgresql.enable {
 
+    services.postgresql.package =
+      # Note: when changing the default, make it conditional on
+      # ‘system.stateVersion’ to maintain compatibility with existing
+      # systems!
+      mkDefault pkgs.postgresql94;
+
     services.postgresql.authentication = mkAfter
       ''
         # Generated file; do not edit!
