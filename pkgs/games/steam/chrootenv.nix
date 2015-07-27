533657332925dbc84e93b2aec04557be253009fa
Nikolay Amiantov <ab@fmap.me>
2015-07-28 00:31:49 +0300

    steam: libstdc++/mesa incompatibility workaround
    closes #9003

--- pkgs/games/steam/chrootenv.nix
+++ pkgs/games/steam/chrootenv.nix
@@ -70,7 +70,9 @@ buildFHSUserEnv {
 
   profile = ''
     # Ugly workaround for https://github.com/ValveSoftware/steam-for-linux/issues/3504
-    export LD_PRELOAD=/lib32/libpulse.so:/lib64/libpulse.so:/lib32/libasound.so:/lib64/libasound.so
+    export LD_PRELOAD=/lib32/libpulse.so:/lib64/libpulse.so:/lib32/libasound.so:/lib64/libasound.so:$LD_PRELOAD
+    # Another one for https://github.com/ValveSoftware/steam-for-linux/issues/3801
+    export LD_PRELOAD=/lib32/libstdc++.so:/lib64/libstdc++.so:$LD_PRELOAD
   '';
 
   runScript = "steam";
