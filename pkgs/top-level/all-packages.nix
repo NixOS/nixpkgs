6e2103068d6a8de3780b5e9ca275b0d6d548e979
William A. Kennington III <william@wkennington.com>
2015-07-27 13:04:20 -0700

    exempi: Use default boost

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1415,7 +1415,7 @@ let
 
   evtest = callPackage ../applications/misc/evtest { };
 
-  exempi = callPackage ../development/libraries/exempi { boost = boost155; };
+  exempi = callPackage ../development/libraries/exempi { };
 
   execline = callPackage ../tools/misc/execline { };
 
