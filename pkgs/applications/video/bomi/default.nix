a5ef14bb74843a3a089febd38396559a5b720299
Nikolay Amiantov <ab@fmap.me>
2015-07-28 11:16:48 +0300

    bomi: update package

--- pkgs/applications/video/bomi/default.nix
+++ pkgs/applications/video/bomi/default.nix
@@ -26,28 +26,23 @@ assert pulseSupport -> libpulseaudio != null;
 assert cddaSupport -> libcdda != null;
 assert youtubeSupport -> youtube-dl != null;
 
-let
-  waf = fetchurl {
-    url = http://ftp.waf.io/pub/release/waf-1.8.4;
-    sha256 = "1a7skwgpl91adhcwlmdr76xzdpidh91hvcmj34zz6548bpx3a87h";
-  };
-
+let qtPath = makeSearchPath "lib/qt5/qml" [ qt5.declarative qt5.quickcontrols ];
 in
 
 stdenv.mkDerivation rec {
   name = "bomi-${version}";
-  version = "0.9.10";
+  version = "0.9.11";
 
   src = fetchFromGitHub {
     owner = "xylosper";
     repo = "bomi";
     rev = "v${version}";
-    sha256 = "1c7497gks7yxzfy6jx77vn9zs2pdq7y6l9w61miwnkdm91093n17";
+    sha256 = "0a7n46gn3n5098lxxvl3s29s8jlkzss6by9074jx94ncn9cayf2h";
   };
 
   buildInputs = with stdenv.lib;
                 [ libX11 libxcb mesa
-                  qt5.base qt5.x11extras qt5.declarative qt5.quickcontrols
+                  qt5.base qt5.x11extras
                   ffmpeg
                   libchardet
                   mpg123
@@ -72,13 +67,13 @@ stdenv.mkDerivation rec {
   '';
 
   preBuild = ''
-    install -m755 ${waf} src/mpv/waf
     patchShebangs src/mpv/waf
     patchShebangs build-mpv
   '';
 
   postInstall = ''
     wrapProgram $out/bin/bomi \
+      --set QML2_IMPORT_PATH ${qtPath} \
       ${optionalString youtubeSupport "--prefix PATH ':' '${youtube-dl}/bin'"}
   '';
 
