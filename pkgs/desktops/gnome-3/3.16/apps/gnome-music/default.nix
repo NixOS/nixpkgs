7ef2102b672bc4ce299f7c417ef612be3db28ed0
Luca Bruno <lucabru@src.gnome.org>
2015-07-29 11:16:45 +0000

    gnome-music: use wrapGAppsHook

--- pkgs/desktops/gnome-3/3.16/apps/gnome-music/default.nix
+++ pkgs/desktops/gnome-3/3.16/apps/gnome-music/default.nix
@@ -1,5 +1,5 @@
 { stdenv, intltool, fetchurl, gdk_pixbuf, tracker
-, python3, libxml2, python3Packages, libnotify
+, python3, libxml2, python3Packages, libnotify, wrapGAppsHook
 , pkgconfig, gtk3, glib, hicolor_icon_theme, cairo
 , makeWrapper, itstool, gnome3, librsvg, gst_all_1 }:
 
@@ -15,24 +15,16 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ pkgconfig gtk3 glib intltool itstool gnome3.libmediaart
                   gdk_pixbuf gnome3.adwaita-icon-theme librsvg python3
-                  gnome3.grilo libxml2 python3Packages.pygobject3 libnotify
+                  gnome3.grilo gnome3.grilo-plugins libxml2 python3Packages.pygobject3 libnotify
                   python3Packages.pycairo python3Packages.dbus gnome3.totem-pl-parser
                   gst_all_1.gstreamer gst_all_1.gst-plugins-base
                   gst_all_1.gst-plugins-good gst_all_1.gst-plugins-bad
-                  hicolor_icon_theme gnome3.adwaita-icon-theme
+                  hicolor_icon_theme gnome3.adwaita-icon-theme wrapGAppsHook
                   gnome3.gsettings_desktop_schemas makeWrapper tracker ];
 
-  enableParallelBuilding = true;
+  wrapPrefixVariables = [ "PYTHONPATH" ];
 
-  preFixup = ''
-    wrapProgram "$out/bin/gnome-music" \
-      --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE" \
-      --prefix XDG_DATA_DIRS : "${gnome3.gnome_themes_standard}/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH" \
-      --prefix GI_TYPELIB_PATH : "$GI_TYPELIB_PATH" \
-      --prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0" \
-      --prefix GRL_PLUGIN_PATH : "${gnome3.grilo-plugins}/lib/grilo-0.2" \
-      --prefix PYTHONPATH : "$PYTHONPATH"
-  '';
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     homepage = https://wiki.gnome.org/Apps/Music;
