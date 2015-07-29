3932ba7a549284ee2851db120153cf47d35ea9a6
William A. Kennington III <william@wkennington.com>
2015-07-29 10:36:45 -0700

    bind: 9.10.2-P2 -> 9.10.2-P3

--- pkgs/servers/dns/bind/default.nix
+++ pkgs/servers/dns/bind/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, openssl, libtool, perl, libxml2 }:
 
-let version = "9.10.2-P2"; in
+let version = "9.10.2-P3"; in
 
 stdenv.mkDerivation rec {
   name = "bind-${version}";
 
   src = fetchurl {
     url = "http://ftp.isc.org/isc/bind9/${version}/${name}.tar.gz";
-    sha256 = "0pvcnwd4rzfk3l35ys72p14ly9k857wbn1lxzd4ayjk3i2pz1rmi";
+    sha256 = "0jhxbsdb8q5q0vn5aga53ixz2iy9zz8p5vwkzvngymd4vmk9l1vq";
   };
 
   patchPhase = ''
