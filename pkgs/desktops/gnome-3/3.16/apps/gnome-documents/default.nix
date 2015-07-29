95c2935061e6929b84248f7645f97d28c8dd8c9f
Luca Bruno <lucabru@src.gnome.org>
2015-07-29 11:16:55 +0000

    gnome-documents: use wrapGAppsHook

--- pkgs/desktops/gnome-3/3.16/apps/gnome-documents/default.nix
+++ pkgs/desktops/gnome-3/3.16/apps/gnome-documents/default.nix
@@ -3,7 +3,7 @@
 , makeWrapper, itstool, libxslt, webkitgtk
 , gnome3, librsvg, gdk_pixbuf, libsoup, docbook_xsl
 , gobjectIntrospection, json_glib
-, gmp, desktop_file_utils }:
+, gmp, desktop_file_utils, wrapGAppsHook }:
 
 stdenv.mkDerivation rec {
   name = "gnome-documents-${gnome3.version}.0";
@@ -24,21 +24,14 @@ stdenv.mkDerivation rec {
                   libsoup webkitgtk gjs gobjectIntrospection gnome3.rest
                   gnome3.tracker gnome3.libgdata gnome3.gnome_online_accounts
                   gnome3.gnome_desktop gnome3.libzapojit json_glib
-                  hicolor_icon_theme gnome3.adwaita-icon-theme ];
+                  wrapGAppsHook ];
 
   enableParallelBuilding = true;
 
-  preFixup =
-    ''
+  preFixup = ''
     substituteInPlace $out/bin/gnome-documents --replace gapplication "${glib}/bin/gapplication"
 
-    for f in $out/bin/* $out/libexec/*; do
-      wrapProgram "$f" \
-        --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE" \
-        --prefix GI_TYPELIB_PATH : "$GI_TYPELIB_PATH" \
-        --prefix XDG_DATA_DIRS : "${gnome3.gnome_themes_standard}/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH" \
-        --run "if [ -z \"\$XDG_CACHE_DIR\" ]; then XDG_CACHE_DIR=\$HOME/.cache; fi; if [ -w \"\$XDG_CACHE_DIR/..\" ]; then mkdir -p \"\$XDG_CACHE_DIR/gnome-documents\"; fi"
-    done
+    gappsWrapperArgs+=(--run 'if [ -z "$XDG_CACHE_DIR" ]; then XDG_CACHE_DIR=$HOME/.cache; fi; if [ -w "$XDG_CACHE_DIR/.." ]; then mkdir -p "$XDG_CACHE_DIR/gnome-documents"; fi')
   '';
 
   meta = with stdenv.lib; {
