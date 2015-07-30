0743aaf87859756f1d8bbe165b78d09307093fee
Luca Bruno <lethalman88@gmail.com>
2015-07-30 12:01:30 +0200

    libgda: add required openssl dependency, previously implicit

--- pkgs/desktops/gnome-3/3.16/misc/libgda/default.nix
+++ pkgs/desktops/gnome-3/3.16/misc/libgda/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, intltool, itstool, libxml2, gtk3 }:
+{ stdenv, fetchurl, pkgconfig, intltool, itstool, libxml2, gtk3, openssl }:
 
 let
   major = "5.2";
@@ -19,7 +19,7 @@ in stdenv.mkDerivation rec {
 
   enableParallelBuilding = true;
 
-  buildInputs = [ pkgconfig intltool itstool libxml2 gtk3 ];
+  buildInputs = [ pkgconfig intltool itstool libxml2 gtk3 openssl ];
 
   meta = with stdenv.lib; {
     description = "Database access library";
