2c9a97ee14b93e1394f41cd44cde8a5a2c4b0e62
Jascha Geerds <jg@ekby.de>
2015-07-30 18:05:14 +0200

    gnome3: Refactor default screensaver setting

--- nixos/modules/services/x11/desktop-managers/gnome3.nix
+++ nixos/modules/services/x11/desktop-managers/gnome3.nix
@@ -32,11 +32,10 @@ let
      mkdir -p $out/share/nixos-gsettings-schemas/nixos-gsettings-desktop-schemas
      cp -rf ${gnome3.gsettings_desktop_schemas}/share/gsettings-schemas/gsettings-desktop-schemas*/glib-2.0 $out/share/nixos-gsettings-schemas/nixos-gsettings-desktop-schemas/
      chmod -R a+w $out/share/nixos-gsettings-schemas/nixos-gsettings-desktop-schemas
-     cat - > $out/share/nixos-gsettings-schemas/nixos-gsettings-desktop-schemas/glib-2.0/schemas/org.gnome.desktop.background.nixos.gschema.override <<- EOF
+     cat - > $out/share/nixos-gsettings-schemas/nixos-gsettings-desktop-schemas/glib-2.0/schemas/nixos-defaults.gschema.override <<- EOF
        [org.gnome.desktop.background]
        picture-uri='${pkgs.nixos-artwork}/share/artwork/gnome/Gnome_Dark.png'
-     EOF
-     cat - > $out/share/nixos-gsettings-schemas/nixos-gsettings-desktop-schemas/glib-2.0/schemas/org.gnome.desktop.screensaver.nixos.gschema.override <<- EOF
+
        [org.gnome.desktop.screensaver]
        picture-uri='${pkgs.nixos-artwork}/share/artwork/gnome/Gnome_Dark.png'
      EOF
