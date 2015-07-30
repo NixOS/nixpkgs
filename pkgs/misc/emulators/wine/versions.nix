e15a4b0ee24459ef3872a07d37a6f1969068043c
Herwig Hochleitner <herwig@bendlas.net>
2015-07-30 02:53:03 +0200

    wine: 1.7.47 -> 1.7.48

--- pkgs/misc/emulators/wine/versions.nix
+++ pkgs/misc/emulators/wine/versions.nix
@@ -1,7 +1,7 @@
 {
   unstable = {
-    wineVersion = "1.7.47";
-    wineSha256  = "0bmvgcag0kv0671dj6fbfdz86ij91rb2kp2dssqw83a0xidgfx5s";
+    wineVersion = "1.7.48";
+    wineSha256  = "13kcjirif57p8mg4yhzxf0hjpghlwc18iskz66dx94i0dvjmrxg3";
     geckoVersion = "2.36";
     geckoSha256 = "12hjks32yz9jq4w3xhk3y1dy2g3iakqxd7aldrdj51cqiz75g95g";
     gecko64Version = "2.36";
@@ -23,8 +23,8 @@
     #monoSha256 = "00jl24qp7vh3hlqv7wsw1s529lr5p0ybif6s73jy85chqaxj7z1x";
   };
   staging = {
-    version = "1.7.47";
-    sha256 = "0m47v4jbc70b7qxj0lqnc7an1xlcb81b6k7dwzjyk3pv3ixp2snp";
+    version = "1.7.48";
+    sha256 = "06p1h92vaqlzk09aj4na6z7k3a81y9nw19rfg9q2szpcqjdd437w";
   };
   winetricks = {
     version = "20150706";
