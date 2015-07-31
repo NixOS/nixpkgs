cb7c053383ff76a454484992b3a6d6a2f1530c15
Jude Taylor <me@jude.bio>
2015-07-31 16:21:01 -0700

    add fixes to gcc-4.8

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3832,6 +3832,8 @@ let
     libcCross = if crossSystem != null then libcCross else null;
 
     isl = isl_0_14;
+
+    inherit (darwin) CF;
   }));
 
   gcc49 = lowPrio (wrapCC (callPackage ../development/compilers/gcc/4.9 {
