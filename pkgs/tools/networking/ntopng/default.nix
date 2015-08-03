d65a8c81b566ec5192eb379469ed50ff3afd9880
Bjørn Forsman <bjorn.forsman@gmail.com>
2015-08-03 07:36:27 +0200

    ntopng: add extra download URL
    It seems upstream have moved the ntopng-1.2.1.tgz into an "old/"
    subdirectory.
    
    (cherry picked from commit c2aadb38a7b7b29b29135cef80b1c4c8b2b15c89)

--- pkgs/tools/networking/ntopng/default.nix
+++ pkgs/tools/networking/ntopng/default.nix
@@ -29,7 +29,10 @@ stdenv.mkDerivation rec {
   };
 
   src = fetchurl {
-    url = "mirror://sourceforge/project/ntop/ntopng/${name}.tgz";
+    urls = [
+      "mirror://sourceforge/project/ntop/ntopng/old/${name}.tgz"
+      "mirror://sourceforge/project/ntop/ntopng/${name}.tgz"
+    ];
     sha256 = "1db83cd1v4ivl8hxzzdvvdcgk22ji7mwrfnd5nnwll6kb11i364v";
   };
 
