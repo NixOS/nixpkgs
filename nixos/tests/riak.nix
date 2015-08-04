878e69c67c44a521f6ad933bfcfab9e9595243b9
Charles Strahan <charles.c.strahan@gmail.com>
2015-08-03 21:48:29 -0400

    riak2: use sensible default paths
    Make exporting the RIAK_{ETC,LOG,DATA}_DIR variables optional;
    if they're not in the environment, we use the same default values as
    NixOS.

--- nixos/tests/riak.nix
+++ nixos/tests/riak.nix
@@ -16,6 +16,6 @@ import ./make-test.nix {
 
     $master->waitForUnit("riak");
     $master->sleep(20); # Hopefully this is long enough!!
-    $master->succeed("RIAK_DATA_DIR='/var/db/riak' RIAK_LOG_DIR='/var/log/riak' RIAK_ETC_DIR='/etc/riak' riak ping 2>&1");
+    $master->succeed("riak ping 2>&1");
   '';
 }
