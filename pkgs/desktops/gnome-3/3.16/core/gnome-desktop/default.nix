b093790ae29078afe0d8df38f8408eab816f250f
Luca Bruno <lucabru@src.gnome.org>
2015-08-05 22:16:05 +0200

    gnome-desktop: propagate gsettings_desktop_schemas, required by the .pc

--- pkgs/desktops/gnome-3/3.16/core/gnome-desktop/default.nix
+++ pkgs/desktops/gnome-3/3.16/core/gnome-desktop/default.nix
@@ -21,7 +21,9 @@ stdenv.mkDerivation rec {
   buildInputs = [ pkgconfig python libxml2Python libxslt which libX11
                   xkeyboard_config isocodes itstool wayland
                   gtk3 glib intltool gnome_doc_utils libxkbfile
-                  gnome3.gsettings_desktop_schemas gobjectIntrospection ];
+                  gobjectIntrospection ];
+
+  propagatedBuildInputs = [ gnome3.gsettings_desktop_schemas ];
 
   meta = with stdenv.lib; {
     platforms = platforms.linux;
