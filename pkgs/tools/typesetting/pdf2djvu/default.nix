f5c2cd8059961eb2ef8a775184ef92b5cc2ada96
Pascal Wittmann <mail@pascal-wittmann.de>
2015-08-08 00:07:26 +0200

    pdf2djvu: update from 0.7.21 to 0.8

--- pkgs/tools/typesetting/pdf2djvu/default.nix
+++ pkgs/tools/typesetting/pdf2djvu/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, pkgconfig, djvulibre, poppler, fontconfig, libjpeg }:
 
 stdenv.mkDerivation rec {
-  version = "0.7.21";
+  version = "0.8";
   name = "pdf2djvu-${version}";
 
   src = fetchurl {
     url = "https://bitbucket.org/jwilk/pdf2djvu/downloads/${name}.tar.xz";
-    sha256 = "1fc7nrc8z5z66ifjjqbqn3c52hxlzgkgbdrr3cgrwdp27k681m0j";
+    sha256 = "0xdxx5c7hvhzfmi37cf9p17smm2y55wc8z1vsncqqvf1sxqpjmbn";
   };
 
   buildInputs = [ pkgconfig djvulibre poppler fontconfig libjpeg ];
