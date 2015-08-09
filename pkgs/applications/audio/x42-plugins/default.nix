11f638506a1466455232057c74da3dd171b3dd99
Bart Brouns <bart@magnetophon.nl>
2015-08-09 21:23:20 +0200

    x42-plugins : 2014-11-01 -> 2015-07-02

--- pkgs/applications/audio/x42-plugins/default.nix
+++ pkgs/applications/audio/x42-plugins/default.nix
@@ -3,22 +3,17 @@
 , lv2, mesa, gtk2, cairo, pango, fftwFloat, zita-convolver }:
 
 stdenv.mkDerivation rec {
-  version = "2014-11-01";
+  version = "2015-07-02";
   name = "x42-plugins-${version}";
 
   src = fetchurl {
-    url = "http://gareus.org/misc/x42-plugins/x42-plugins-20141101.tar.xz";
-    sha256 = "0pjdhj58hb4n2053v92l7v7097fjm4xzrl8ks4g1hc7miy98ymdk";
+    url = "http://gareus.org/misc/x42-plugins/x42-plugins-20150702.tar.xz";
+    sha256 = "1mq0grabzbl9xsd53v2qajhr8nngk0d4lx9n0n3nwy95y2gmy6sm";
   };
 
   buildInputs = [ mesa_glu ftgl freefont_ttf libjack2 libltc libsndfile libsamplerate lv2 mesa gtk2 cairo pango fftwFloat pkgconfig  zita-convolver];
 
-  makeFlags = [ "PREFIX=$(out)" "FONTFILE=${freefont_ttf}/share/fonts/truetype/FreeSansBold.ttf" ];
-
-  # remove check for zita-convolver in /usr/
-  patchPhase = ''
-    sed -i "38,42d" convoLV2/Makefile
-  '';
+  makeFlags = [ "PREFIX=$(out)" "FONTFILE=${freefont_ttf}/share/fonts/truetype/FreeSansBold.ttf" "LIBZITACONVOLVER=${zita-convolver}/include/zita-convolver.h" ];
 
   meta = with stdenv.lib;
     { description = "Collection of LV2 plugins by Robin Gareus";
