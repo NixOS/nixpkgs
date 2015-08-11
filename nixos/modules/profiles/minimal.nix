54ddd7a3b638b545495b80e277708f3aa4de2017
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-08-11 20:38:25 +0200

    Revert "nixos/ISO profile: fix defaultLocales :-)"
    This reverts commit e2ef5cc163d5585674d37255de2554ce2c47f537. It broke
    the installer tests:
    
    http://hydra.nixos.org/build/24626135

--- nixos/modules/profiles/minimal.nix
+++ nixos/modules/profiles/minimal.nix
@@ -5,7 +5,4 @@
 
 {
   environment.noXlibs = true;
-
-  # This isn't perfect, but let's expect the user specifies an UTF-8 defaultLocale
-  i18n.supportedLocales = [ (config.i18n.defaultLocale + "/UTF-8") ];
 }
