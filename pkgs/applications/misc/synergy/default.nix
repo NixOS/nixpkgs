NixOS/nixpkgs

commit f91dacdd07731e06cec80b727b28b749e64f60e8

synergy: Update from version 1.6.3 to 1.7.4.
Quite a lot of upstream changes happened in between, so here are the
upstream changes:

https://github.com/synergy/synergy/releases/tag/1.7.0
https://github.com/synergy/synergy/releases/tag/v1.7.1-stable
https://github.com/synergy/synergy/releases/tag/v1.7.2-stable
https://github.com/synergy/synergy/releases/tag/v1.7.3-stable
https://github.com/synergy/synergy/releases/tag/v1.7.4-stable

The upstream project has stopped using cryptopp
(synergy/synergy@80343c4) and now uses OpenSSL instead (see issue
synergy/synergy#4313) so we no longer need our patch.

Also, the issue for the 'install' target has been closed as obsolete and
although they had such a target in the meantime, it has been removed
again in synergy/synergy@d1eb7a6, so let's not rely on it anymore and
remove the comment on the closed/obsolete issue.

Forcing support for XRandR in the CMakeLists.txt file isn't needed
anymore, because the issues mentioned in ea4afb7 have been fixed
upstream.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>
Reported-by: devhell <"^"@regexmail.net>

pkgs/applications/misc/synergy/default.nix
@@ -1,38 +1,27 @@
 { stdenv, fetchFromGitHub, cmake, x11, libX11, libXi, libXtst, libXrandr
-, xinput, curl, cryptopp ? null, unzip }:
-
-assert stdenv.isLinux -> cryptopp != null;
+, xinput, curl, openssl, unzip }:
 
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "synergy-${version}";
-  version = "1.6.3";
+  version = "1.7.4";
 
   src = fetchFromGitHub {
     owner = "synergy";
     repo = "synergy";
-    rev = version;
-    sha256 = "0n4zvz669vi2wyn6i6xhxp0j3nvjl4yzm441cqv6hb0d5k26wbcn";
+    rev = "v${version}-stable";
+    sha256 = "0pxj0qpnsaffpaxik8vc5rjfinmx8ab3b2lssrxkfbs7isskvs33";
   };
 
-  patches = optional stdenv.isLinux ./cryptopp.patch;
-
-  postPatch = (if stdenv.isLinux then ''
-    sed -i -e '/HAVE_X11_EXTENSIONS_XRANDR_H/c \
-      set(HAVE_X11_EXTENSIONS_XRANDR_H true)' CMakeLists.txt
-  '' else ''
-    ${unzip}/bin/unzip -d ext/cryptopp562 ext/cryptopp562.zip
-  '') + ''
+  postPatch = ''
     ${unzip}/bin/unzip -d ext/gmock-1.6.0 ext/gmock-1.6.0.zip
     ${unzip}/bin/unzip -d ext/gtest-1.6.0 ext/gtest-1.6.0.zip
   '';
 
-  buildInputs = [ cmake x11 libX11 libXi libXtst libXrandr xinput curl ]
-             ++ optional stdenv.isLinux cryptopp;
-
-  # At this moment make install doesn't work for synergy
-  # http://synergy-foss.org/spit/issues/details/3317/
+  buildInputs = [
+    cmake x11 libX11 libXi libXtst libXrandr xinput curl openssl
+  ];
 
   installPhase = ''
     mkdir -p $out/bin

pkgs/applications/misc/synergy/cryptopp.patch
@@ -1,45 +1,0 @@
-diff -urN synergy-1.4.17-Source/CMakeLists.txt synergy-1.4.17-Source-fix/CMakeLists.txt
---- synergy-1.4.17-Source/CMakeLists.txt	2014-03-14 21:34:19.000000000 +0100
-+++ synergy-1.4.17-Source-fix/CMakeLists.txt	2014-04-11 13:37:18.839338710 +0200
-@@ -145,6 +145,9 @@
- 	check_type_size(long SIZEOF_LONG)
- 	check_type_size(short SIZEOF_SHORT)
- 
-+	# let's just assume cryptopp exists (provided by the Nix expression)
-+	list(APPEND libs cryptopp)
-+
- 	# pthread is used on both Linux and Mac
- 	check_library_exists("pthread" pthread_create "" HAVE_PTHREAD)
- 	if (HAVE_PTHREAD)
-@@ -317,7 +320,6 @@
- endif()
- 
- add_subdirectory(src)
--add_subdirectory(ext)
- 
- if (WIN32)
- 	# TODO:	consider using /analyze to uncover potential bugs in the source code.
-diff -urN synergy-1.4.17-Source/src/lib/io/CryptoMode_cryptopp.h synergy-1.4.17-Source-fix/src/lib/io/CryptoMode_cryptopp.h
---- synergy-1.4.17-Source/src/lib/io/CryptoMode_cryptopp.h	2014-02-28 13:36:45.000000000 +0100
-+++ synergy-1.4.17-Source-fix/src/lib/io/CryptoMode_cryptopp.h	2014-04-11 13:36:01.111985556 +0200
-@@ -25,6 +25,6 @@
- #	pragma GCC system_header
- #endif 
- 
--#include <cryptopp562/gcm.h>
--#include <cryptopp562/modes.h>
--#include <cryptopp562/aes.h>
-+#include <cryptopp/gcm.h>
-+#include <cryptopp/modes.h>
-+#include <cryptopp/aes.h>
-diff -urN synergy-1.4.17-Source/src/lib/io/CryptoStream_cryptopp.h synergy-1.4.17-Source-fix/src/lib/io/CryptoStream_cryptopp.h
---- synergy-1.4.17-Source/src/lib/io/CryptoStream_cryptopp.h	2014-02-28 13:36:45.000000000 +0100
-+++ synergy-1.4.17-Source-fix/src/lib/io/CryptoStream_cryptopp.h	2014-04-11 13:36:07.173013005 +0200
-@@ -25,5 +25,5 @@
- #	pragma GCC system_header
- #endif
- 
--#include <cryptopp562/osrng.h>
--#include <cryptopp562/sha.h>
-+#include <cryptopp/osrng.h>
-+#include <cryptopp/sha.h>

commit 18b2a21583fe7c7025f9caec0b4348e203535a0e

synergy: Fix support for XRandR again.
Regression introduced by f91dacdd07731e06cec80b727b28b749e64f60e8.

Accidentally thought that it's compiling with XRandR support enabled,
because the cmake output said so:

Looking for XRRQueryExtension in Xrandr - found

Unfortunately, despite this message, the relevant part is:

Looking for XRRNotifyEvent - not found

So, ea4afb7 still holds true and I've added a small comment to avoid
this from happening in the future.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

pkgs/applications/misc/synergy/default.nix
@@ -17,6 +17,23 @@ stdenv.mkDerivation rec {
   postPatch = ''
     ${unzip}/bin/unzip -d ext/gmock-1.6.0 ext/gmock-1.6.0.zip
     ${unzip}/bin/unzip -d ext/gtest-1.6.0 ext/gtest-1.6.0.zip
+  ''
+    # We have XRRNotifyEvent (libXrandr), but with the upstream CMakeLists.txt
+    # it's not able to find it (it's trying to search the store path of libX11
+    # instead) and we don't get XRandR support, even though the CMake output
+    # _seems_ to say so:
+    #
+    #   Looking for XRRQueryExtension in Xrandr - found
+    #
+    # The relevant part however is:
+    #
+    #   Looking for XRRNotifyEvent - not found
+    #
+    # So let's force it:
+  + optionalString stdenv.isLinux ''
+    sed -i -e '/HAVE_X11_EXTENSIONS_XRANDR_H/c \
+      set(HAVE_X11_EXTENSIONS_XRANDR_H true)
+    ' CMakeLists.txt
   '';
 
   buildInputs = [