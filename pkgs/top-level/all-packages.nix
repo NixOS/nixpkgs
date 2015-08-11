3607cf316e2712d9bcd2c6d463b5e4fefc99f9a6
Thomas Strobel <ts468@cam.ac.uk>
2015-08-11 12:13:33 +0200

    openpts: init at 0.2.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9026,6 +9026,8 @@ let
 
   nsq = callPackage ../servers/nsq { };
 
+  openpts = callPackage ../servers/openpts { };
+
   openresty = callPackage ../servers/http/openresty { };
 
   opensmtpd = callPackage ../servers/mail/opensmtpd { };
