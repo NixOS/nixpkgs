2f8c6ef1ff24c7660d0fb4e2b952449dfe99ab7a
Thomas Strobel <ts468@cam.ac.uk>
2015-08-13 14:55:54 +0200

    oraclejdk: enable automated download of source packages
    The license of the oraclejdk{7,7psu,8} packages have been changed to the "Oracle
    Binary Code License Agreement for the Java SE Platform Products and JavaFX".
    The packages can now only be installed once the license is accepted, e.g.,
    within the "nixos/modules/misc/licenses.nix" module. In return, the packages are
    downloaded automatically without user intervention.

--- pkgs/development/compilers/oraclejdk/jdk8-linux.nix
+++ pkgs/development/compilers/oraclejdk/jdk8-linux.nix
@@ -1,10 +1,10 @@
 import ./jdk-linux-base.nix {
   productVersion = "8";
   patchVersion = "51";
-  downloadUrl = http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html;
+  url_i686 = http://download.oracle.com/otn-pub/java/jdk/8u51-b16/jdk-8u51-linux-i586.tar.gz;
+  url_x86_64 = http://download.oracle.com/otn-pub/java/jdk/8u51-b16/jdk-8u51-linux-x64.tar.gz;
   sha256_i686 = "0awzgs090n2cj6a5mlla0pgxk0y6aslhm6024pqrnxgai1fkmm1z";
   sha256_x86_64 = "1wggrcr2gjwkv5bawgcw86h6rhyzw0jphxm1sfwcvhjirh99056p";
-  jceName = "jce_policy-8.zip";
-  jceDownloadUrl = http://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html;
+  urlJCE = http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip;
   sha256JCE = "0n8b6b8qmwb14lllk2lk1q1ahd3za9fnjigz5xn65mpg48whl0pk";
 }
