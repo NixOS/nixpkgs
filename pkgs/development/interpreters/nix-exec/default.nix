529596fd316fa6f31a9f39f537939c0bc971543f
Shea Levy <shea@shealevy.com>
2015-08-14 09:49:41 -0400

    Bump nix-exec for new nix

--- pkgs/development/interpreters/nix-exec/default.nix
+++ pkgs/development/interpreters/nix-exec/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, pkgconfig, nix, git }: let
-  version = "4.1.0";
+  version = "4.1.1";
 in stdenv.mkDerivation {
   name = "nix-exec-${version}";
 
   src = fetchurl {
     url = "https://github.com/shlevy/nix-exec/releases/download/v${version}/nix-exec-${version}.tar.xz";
 
-    sha256 = "16hssxv6fwi5a6bz7dlvhjjr3ymiqrvq0xfd38gwhn9qhvynv2ak";
+    sha256 = "1igpxhvj29cgg7mid1ilaq3d1q9sk6nlw8q33ah8y78iy2ah1iv2";
   };
 
   buildInputs = [ pkgconfig nix git ];
