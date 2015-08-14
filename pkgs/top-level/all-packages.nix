a82b3b11aa41d58649876d995d18e68868b5b2b5
Bjørn Forsman <bjorn.forsman@gmail.com>
2015-08-14 10:18:20 +0200

    qt5SDK: build with documentation
    It was disabled due to a build error that is now fixed.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7973,7 +7973,7 @@ let
   qt5SDK = qtcreator.override {
     sdkBuild = true;
     qtLib = qt5Full;
-    withDocumentation = false; # fails to setup some paths
+    withDocumentation = true;
   };
 
   qtcreator = callPackage ../development/qtcreator {
