c40dae086334306dccdf4303e09e1335520305ef
Thomas Tuegel <ttuegel@gmail.com>
2015-08-14 22:28:23 -0500

    ipe: fix homepage

--- pkgs/applications/graphics/ipe/default.nix
+++ pkgs/applications/graphics/ipe/default.nix
@@ -36,7 +36,7 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "An editor for drawing figures";
-    homepage = http://ipe7.sourceforge.net;
+    homepage = http://ipe.otfried.org;
     license = stdenv.lib.licenses.gpl3Plus;
     longDescription = ''
       Ipe is an extensible drawing editor for creating figures in PDF and Postscript format.
