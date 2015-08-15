a1d4e7ff70347814cb48c4adfb91c66ff8e5d341
Pascal Wittmann <mail@pascal-wittmann.de>
2015-08-15 12:33:15 +0200

    u9fs: move from googlecode to bitbucket

--- pkgs/servers/u9fs/default.nix
+++ pkgs/servers/u9fs/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation {
   name = "u9fs-20110513";
   src = fetchhg {
-    url = https://code.google.com/p/u9fs;
+    url = http://bitbucket.org/plan9-from-bell-labs/u9fs;
     rev = "9474edb23b11";
     sha256 = "0irwyk8vnvx0fmz8lmbdb2jrlvas8imr61jr76a1pkwi9wpf2wv6";
   };
@@ -16,7 +16,7 @@ stdenv.mkDerivation {
 
   meta = with stdenv.lib;
     { description = "Serve 9P from Unix";
-      homepage = https://code.google.com/p/u9fs;
+      homepage = http://plan9.bell-labs.com/magic/man2html/4/u9fs;
       license = licenses.free;
       maintainers = [ maintainers.emery ];
       platforms = platforms.unix;
