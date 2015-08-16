0935ef2913542cf843064f6b690a740bcd6db08f
AndersonTorres <torres.anderson.85@gmail.com>
2015-08-15 22:36:44 -0300

    BibleSync: init at 1.1.2
    Biblesync is a dependency of new Xiphos.

---
+++ pkgs/development/libraries/biblesync/default.nix
@@ -0,0 +1,32 @@
+{ stdenv, fetchurl, pkgconfig, cmake, libuuid }:
+
+with stdenv.lib;
+stdenv.mkDerivation rec{
+
+  name = "biblesync-${version}";
+  version = "1.1.2";
+
+  src = fetchurl{
+    url = "http://downloads.sourceforge.net/project/gnomesword/BibleSync/1.1.2/${name}.tar.gz";
+    sha256 = "0190q2da0ppif2242lahl8xfz01n9sijy60aq1a0545qcp0ilvl8";
+  };
+
+  buildInputs = [ pkgconfig cmake libuuid ];
+
+  meta = {
+    homepage = http://www.crosswire.org/wiki/BibleSync;
+    description = "A multicast protocol to Bible software shared conavigation";
+    longDescription = ''
+      BibleSync is a multicast protocol to support Bible software
+      shared co-navigation. It uses LAN multicast in either a
+      personal/small team mutual navigation motif or in a classroom
+      environment where there are Speakers plus the Audience. The
+      library implementing the protocol is a single C++ class
+      providing a complete yet minimal public interface to support
+      mode setting, setup for packet reception, transmit on local
+      navigation, and handling of incoming packets.
+    '';
+    license = licenses.publicDomain;
+    maintainers = [ maintainers.AndersonTorres ]; 
+  };
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8067,6 +8067,8 @@ let
 
   sword = callPackage ../development/libraries/sword { };
 
+  biblesync = callPackage ../development/libraries/biblesync { };
+
   szip = callPackage ../development/libraries/szip { };
 
   t1lib = callPackage ../development/libraries/t1lib { };
