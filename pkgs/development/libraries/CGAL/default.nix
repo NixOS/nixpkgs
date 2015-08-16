844bfc605a35a60ebf701d03005446698aadc7dd
Vladimír Čunát <vcunat@gmail.com>
2015-08-16 18:30:27 +0200

    cgal: fix build by update 4.5 -> 4.6.1
    It was broken by cmake updates (between 3.2.2 and 3.3.1).
    I chose the github tarballs because they have a predictable URL,
    although they're much larger due to not supporting xz.
    
    /cc maintainer @7c6f434c.

--- pkgs/development/libraries/CGAL/default.nix
+++ pkgs/development/libraries/CGAL/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, cmake, boost, gmp, mpfr }:
 
 stdenv.mkDerivation rec {
-  version = "4.5";
+  version = "4.6.1";
 
   name = "cgal-${version}";
 
   src = fetchurl {
-    url = "https://gforge.inria.fr/frs/download.php/34139/CGAL-${version}.tar.xz";
-    sha256 = "00shds5yph4s09lqdrb6n60wnw9kpiwa25ghg9mbsgq3fnr8p7kr";
+    url = "https://github.com/CGAL/releases/archive/releases/CGAL-${version}.tar.gz";
+    sha256 = "09ph5qi7ixbkk3jssq3pdjf2nyw91s73dizi2mkx6brhrzd1zd5y";
   };
 
   buildInputs = [ cmake boost gmp mpfr ];
