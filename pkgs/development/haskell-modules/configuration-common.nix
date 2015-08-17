0eca00a544dbd9fdd79c67ef1f392fe1b4160d64
Peter Simons <simons@cryp.to>
2015-08-17 18:00:19 +0200

    haskell-sets-with was deprecated.

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -922,9 +922,6 @@ self: super: {
   # https://github.com/athanclark/commutative/issues/1
   commutative = dontCheck super.commutative;
 
-  # https://github.com/athanclark/set-with/issues/1
-  set-with = dontCheck super.set-with;
-
   # https://github.com/athanclark/sets/issues/2
   sets = dontCheck super.sets;
 
