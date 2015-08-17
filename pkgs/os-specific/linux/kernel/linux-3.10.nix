109ff7ddee11eb94e8d76ff0f0571d0e12732b2b
William A. Kennington III <william@wkennington.com>
2015-08-17 11:41:19 -0700

    kernel: 3.10.86 -> 3.10.87

--- pkgs/os-specific/linux/kernel/linux-3.10.nix
+++ pkgs/os-specific/linux/kernel/linux-3.10.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.10.86";
+  version = "3.10.87";
   extraMeta.branch = "3.10";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1kax24y6xa49n4wc74qg503pbg0rd2imx5npyfp1wcnc6p68w0mr";
+    sha256 = "01lax9c6j2gw33pr7dla1ly1d89970mkbwh2hnmysgzsyh136rvg";
   };
 
   features.iwlwifi = true;
