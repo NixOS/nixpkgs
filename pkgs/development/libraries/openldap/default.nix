15a6f0ba7b398d13fe42b5cbeabe17851ee10918
William A. Kennington III <william@wkennington.com>
2015-08-18 12:35:51 -0700

    openldap: Fix hash

--- pkgs/development/libraries/openldap/default.nix
+++ pkgs/development/libraries/openldap/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "http://www.openldap.org/software/download/OpenLDAP/openldap-release/${name}.tgz";
-    sha256 = "0qwfpb5ipp2l76v11arghq5mr0sjc6xhjfg8a0kgsaw5qpib1dzft";
+    sha256 = "0qwfpb5ipp2l76v11arghq5mr0sjc6xhjfg8a0kgsaw5qpib1dzf";
   };
 
   outputs = [ "out" "man" ];
