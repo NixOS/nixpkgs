cc0f9acb53f0c1808f23fc7d40628992119315a8
Jascha Geerds <jg@ekby.de>
2015-08-18 14:50:19 +0200

    aria2: Fetch from github instead of sourceforge

--- pkgs/tools/networking/aria2/default.nix
+++ pkgs/tools/networking/aria2/default.nix
@@ -1,20 +1,26 @@
-{ stdenv, fetchurl, pkgconfig, c-ares, openssl, libxml2, sqlite, zlib }:
+{ stdenv, fetchFromGitHub, pkgconfig, autoreconfHook, cppunit, libgcrypt
+, c-ares, openssl, libxml2, sqlite, zlib }:
 
 stdenv.mkDerivation rec {
   name = "aria2-${version}";
   version = "1.19.0";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/aria2/stable/${name}/${name}.tar.xz";
-    sha256 = "0xm4fmap9gp2pz6z01mnnpmazw6pnhzs8qc58181m5ai4gy5ksp2";
+  src = fetchFromGitHub {
+    owner = "tatsuhiro-t";
+    repo = "aria2";
+    rev = "release-${version}";
+    sha256 = "1k4b8jfg4wjsvybb7hysplp6h831allhiqdy9jwsyy0m0zmgk00a";
   };
 
-  buildInputs = [ pkgconfig c-ares openssl libxml2 sqlite zlib ];
+  buildInputs = [
+    pkgconfig autoreconfHook cppunit libgcrypt c-ares openssl libxml2
+    sqlite zlib
+  ];
 
   configureFlags = [ "--with-ca-bundle=/etc/ssl/certs/ca-certificates.crt" ];
 
   meta = with stdenv.lib; {
-    homepage = http://aria2.sourceforge.net/;
+    homepage = https://github.com/tatsuhiro-t/aria2;
     description = "A lightweight, multi-protocol, multi-source, command-line download utility";
     maintainers = [ maintainers.koral ];
     license = licenses.gpl2Plus;
