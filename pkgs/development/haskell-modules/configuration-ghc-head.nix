4cc5be347ad839d7ca6ccf8875c055b9d143f90d
Peter Simons <simons@cryp.to>
2015-08-18 15:03:51 +0200

    haskell-configuration-ghc-head: update obsolete jailbreak-cabal override

--- pkgs/development/haskell-modules/configuration-ghc-head.nix
+++ pkgs/development/haskell-modules/configuration-ghc-head.nix
@@ -34,11 +34,22 @@ self: super: {
   xhtml = null;
 
   # Don't use jailbreak built with Cabal 1.22.x because of https://github.com/peti/jailbreak-cabal/issues/9.
-  jailbreak-cabal = pkgs.haskell.packages.ghc784.jailbreak-cabal;
-
-  # GHC 7.10.x's Haddock binary cannot generate hoogle files.
-  # https://ghc.haskell.org/trac/ghc/ticket/9921
-  mkDerivation = drv: super.mkDerivation (drv // { doHoogle = false; });
+  Cabal_1_23_0_0 = overrideCabal super.Cabal_1_22_4_0 (drv: {
+    version = "1.23.0.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "haskell";
+      repo = "cabal";
+      rev = "fe7b8784ac0a5848974066bdab76ce376ba67277";
+      sha256 = "1d70ryz1l49pkr70g8r9ysqyg1rnx84wwzx8hsg6vwnmg0l5am7s";
+    };
+    jailbreak = false;
+    doHaddock = false;
+    postUnpack = "sourceRoot+=/Cabal";
+  });
+  jailbreak-cabal = overrideCabal super.jailbreak-cabal (drv: {
+    executableHaskellDepends = [ self.Cabal_1_23_0_0 ];
+    preConfigure = "sed -i -e 's/Cabal == 1.20\\.\\*/Cabal >= 1.23/' jailbreak-cabal.cabal";
+  });
 
   # haddock: No input file(s).
   nats = dontHaddock super.nats;
