b284a78bf89936df965e36d425488edf8814c7f1
Jeffrey David Johnson <jefdaj@gmail.com>
2015-08-19 18:38:10 -0700

    add bitcoin-xt as a separate package
    (cherry picked from commit 17c0af24d219f1b0431734d0b086d38200e3da2b)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10774,6 +10774,7 @@ let
     callPackage = newScope { boost = boost157; };
   } );
   bitcoin = altcoins.bitcoin;
+  bitcoin-xt = altcoins.bitcoin-xt;
 
   aumix = callPackage ../applications/audio/aumix {
     gtkGUI = false;
