3f1fa380b2e3b4677f5ddea127ee5375a7aee770
Vladimír Čunát <vcunat@gmail.com>
2015-08-20 15:18:46 +0200

    gdk-pixbuf: fix CVE-2015-4491 by maintenance update

--- pkgs/development/libraries/gdk-pixbuf/default.nix
+++ pkgs/development/libraries/gdk-pixbuf/default.nix
@@ -3,14 +3,14 @@
 
 let
   ver_maj = "2.31";
-  ver_min = "5";
+  ver_min = "6";
 in
 stdenv.mkDerivation rec {
   name = "gdk-pixbuf-${ver_maj}.${ver_min}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gdk-pixbuf/${ver_maj}/${name}.tar.xz";
-    sha256 = "19ppbycbr87rai93vf2ff8k3ksjqq64s8qysq0mfy9fdjw2ffxha";
+    sha256 = "062x2gqd7p6yxhxlib1ha4l3gk9ihcj080hrwwv9vmlmybb064hi";
   };
 
   setupHook = ./setup-hook.sh;
