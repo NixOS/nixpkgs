8b034964d1e8aca285f601680634130fc954070c
Nikolay Amiantov <ab@fmap.me>
2015-08-23 18:12:16 +0300

    steam-runtime: split by architectures

--- pkgs/games/steam/runtime.nix
+++ pkgs/games/steam/runtime.nix
@@ -1,6 +1,10 @@
 { stdenv, fetchurl }:
 
-stdenv.mkDerivation rec {
+let arch = if stdenv.system == "x86_64-linux" then "amd64"
+           else if stdenv.system == "i686-linux" then "i386"
+           else abort "Unsupported platform";
+
+in stdenv.mkDerivation rec {
   name = "steam-runtime-${version}";
   version = "2014-04-15";
 
@@ -13,13 +17,25 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     mkdir -p $out
-    mv * $out/
+    mv ${arch}/* $out/
   '';
+
+  passthru = rec {
+    inherit arch;
+
+    gnuArch = if arch == "amd64" then "x86_64-linux-gnu"
+              else if arch == "i386" then "i386-linux-gnu"
+              else abort "Unsupported architecture";
+
+    libs = [ "lib/${gnuArch}" "lib" "usr/lib/${gnuArch}" "usr/lib" ];
+    bins = [ "bin" "usr/bin" ];
+  };
 
   meta = with stdenv.lib; {
     description = "The official runtime used by Steam";
-    homepage = http://store.steampowered.com/;
-    license = licenses.unfreeRedistributable;
+    homepage = https://github.com/ValveSoftware/steam-runtime;
+    license = licenses.mit;
     maintainers = with maintainers; [ hrdinka ];
+    hydraPlatforms = [];
   };
 }
