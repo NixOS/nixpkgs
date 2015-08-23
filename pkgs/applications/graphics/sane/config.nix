bb81ba7ef662da75566c5642d709c624b245f03c
Nicolas B. Pierron <nicolas.b.pierron@gmail.com>
2015-08-23 13:55:25 +0200

    sane-config: Prevent find warnings by moving maxdepth argument first.

--- pkgs/applications/graphics/sane/config.nix
+++ pkgs/applications/graphics/sane/config.nix
@@ -5,13 +5,13 @@
 with stdenv.lib;
 let installSanePath = path: ''
       if test -e "${path}/lib/sane"; then
-        find "${path}/lib/sane" -not -type d -maxdepth 1 | while read backend; do
+        find "${path}/lib/sane" -maxdepth 1 -not -type d | while read backend; do
           ln -s $backend $out/lib/sane/$(basename $backend)
         done
       fi
 
       if test -e "${path}/etc/sane.d"; then
-        find "${path}/etc/sane.d" -not -type d -maxdepth 1 | while read conf; do
+        find "${path}/etc/sane.d" -maxdepth 1 -not -type d | while read conf; do
           if test $(basename $conf) = "dll.conf"; then
             cat $conf >> $out/etc/sane.d/dll.conf
           else
@@ -21,7 +21,7 @@ let installSanePath = path: ''
       fi
 
       if test -e "${path}/etc/sane.d/dll.d"; then
-        find "${path}/etc/sane.d/dll.d" -not -type d -maxdepth 1 | while read conf; do
+        find "${path}/etc/sane.d/dll.d" -maxdepth 1 -not -type d | while read conf; do
           ln -s $conf $out/etc/sane.d/dll.d/$(basename $conf)
         done
       fi
