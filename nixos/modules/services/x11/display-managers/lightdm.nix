95e11c35698a1645fafdd60496e85de88e79a8fd
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2015-08-23 11:14:06 +0300

    nixos/lightdm: Reference correct output of gdk_pixbuf

--- nixos/modules/services/x11/display-managers/lightdm.nix
+++ nixos/modules/services/x11/display-managers/lightdm.nix
@@ -33,7 +33,7 @@ let
       makeWrapper ${pkgs.lightdm_gtk_greeter}/sbin/lightdm-gtk-greeter \
         $out/greeter \
         --prefix PATH : "${pkgs.glibc.bin}/bin" \
-        --set GDK_PIXBUF_MODULE_FILE "${pkgs.gdk_pixbuf}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache" \
+        --set GDK_PIXBUF_MODULE_FILE "${pkgs.gdk_pixbuf.out}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache" \
         --set GTK_PATH "${theme}:${pkgs.gtk3}" \
         --set GTK_EXE_PREFIX "${theme}" \
         --set GTK_DATA_PREFIX "${theme}" \
