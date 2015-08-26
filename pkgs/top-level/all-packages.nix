4e8279b43b30d119468e8ba76716c1d6bdcf67f5
Jude Taylor <me@jude.bio>
2015-08-25 17:14:02 -0700

    darwin purity: ruby-2.1.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5146,7 +5146,9 @@ let
   ruby_2_1_1 = callPackage ../development/interpreters/ruby/ruby-2.1.1.nix { };
   ruby_2_1_2 = callPackage ../development/interpreters/ruby/ruby-2.1.2.nix { };
   ruby_2_1_3 = callPackage ../development/interpreters/ruby/ruby-2.1.3.nix { };
-  ruby_2_1_6 = callPackage ../development/interpreters/ruby/ruby-2.1.6.nix { };
+  ruby_2_1_6 = callPackage ../development/interpreters/ruby/ruby-2.1.6.nix {
+    inherit (darwin) libobjc libunwind;
+  };
   ruby_2_2_0 = callPackage ../development/interpreters/ruby/ruby-2.2.0.nix {
     inherit (darwin) libobjc libunwind;
   };
