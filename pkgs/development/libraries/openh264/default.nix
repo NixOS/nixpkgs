74ce1df8f54436ffb8bd08adfc4c7d0466a9283c
Thomas Strobel <ts468@cam.ac.uk>
2015-08-26 22:24:05 +0200

    openh264: fix license

--- pkgs/development/libraries/openh264/default.nix
+++ pkgs/development/libraries/openh264/default.nix
@@ -17,6 +17,6 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     description = "A codec library which supports H.264 encoding and decoding";
     homepage = http://www.openh264.org;
-    license = stdenv.licenses.bsd2;
+    license = stdenv.lib.licenses.bsd2;
   };
 }
