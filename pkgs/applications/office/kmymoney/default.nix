e46f2f50fe9f45d088e8c1dc8b3624cd8d31327e
Tobias Blaschke <code@tobiasblaschke.de>
2015-08-26 13:43:40 +0200

    KMyMoney: Update to 4.7.2

--- pkgs/applications/office/kmymoney/default.nix
+++ pkgs/applications/office/kmymoney/default.nix
@@ -3,11 +3,12 @@
 , oxygen_icons, kactivities, aqbanking, perlPackages, gwenhywfar }:
 
 stdenv.mkDerivation rec {
-  name = "kmymoney-4.7.1";
+  name = "kmymoney-4.7.2";
 
   src = fetchurl {
-    url = http://kde.mirrorcatalogs.com/stable/kmymoney/4.7.1/src/kmymoney-4.7.1.tar.xz;
-    sha256 = "7749cbae146eb4adf5c92162c841ae321f971c5720bc32d0227a42a4dd4acfc4"; # v. 4.7.1
+    url = http://kde.mirrorcatalogs.com/stable/kmymoney/4.7.2/src/kmymoney-4.7.2.tar.xz;
+    #sha256 = "7749cbae146eb4adf5c92162c841ae321f971c5720bc32d0227a42a4dd4acfc4"; # v. 4.7.1
+    sha256 = "bfb2c29ff30988f46324c2dae197a06b58d07336a1947adc22bcfed3e554393d"; # v. 4.7.2
   };
 
   buildInputs = [ kdepimlibs perl boost gpgme gmpxx libalkimia libofx libical
