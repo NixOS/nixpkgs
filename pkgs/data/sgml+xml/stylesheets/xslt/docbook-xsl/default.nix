97cd7cfea2d44b97e536b0d36d2ed9d4c15e998e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-08-27 16:56:43 +0200

    docbook-xsl: Remove cruft

--- pkgs/data/sgml+xml/stylesheets/xslt/docbook-xsl/default.nix
+++ pkgs/data/sgml+xml/stylesheets/xslt/docbook-xsl/default.nix
@@ -1,10 +1,24 @@
-{stdenv, fetchurl}:
+{ stdenv, fetchurl }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "docbook-xsl-1.78.1";
-  builder = ./builder.sh;
+
   src = fetchurl {
-    url = mirror://sourceforge/docbook/docbook-xsl-1.78.1.tar.bz2;
+    url = "mirror://sourceforge/docbook/${name}.tar.bz2";
     sha256 = "0rxl013ncmz1n6ymk2idvx3hix9pdabk8xn01cpcv32wmfb753y9";
   };
+
+  buildPhase = "true";
+
+  installPhase =
+    ''
+      dst=$out/share/xml/docbook-xsl
+      mkdir -p $dst
+      rm -rf RELEASE* README* INSTALL TODO NEWS* BUGS install.sh svn* tools log Makefile tests extensions webhelp
+      mv * $dst/
+
+      # Backwards compatibility. Will remove eventually.
+      mkdir -p $out/xml/xsl
+      ln -s $dst $out/xml/xsl/docbook
+    '';
 }
