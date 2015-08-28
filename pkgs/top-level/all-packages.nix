c5e2beab9f6d92a1fc0eb975992a78d9d1c1cba1
William A. Kennington III <william@wkennington.com>
2015-08-28 15:44:48 -0700

    Revert mongodb-tools: use Go 1.4
    Unneeded as it works fine with Go 1.5 now

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -948,7 +948,7 @@ let
 
   mcrypt = callPackage ../tools/misc/mcrypt { };
 
-  mongodb-tools = go14Packages.mongo-tools;
+  mongodb-tools = goPackages.mongo-tools;
 
   mstflint = callPackage ../tools/misc/mstflint { };
 
