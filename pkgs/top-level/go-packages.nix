01a874b3cfd268261f660474377826dfb53a126c
Luca Bruno <lethalman88@gmail.com>
2015-08-28 15:17:14 +0200

    gcloud-golang: mark as broken

--- pkgs/top-level/go-packages.nix
+++ pkgs/top-level/go-packages.nix
@@ -619,6 +619,8 @@ let
     goPackagePath = "google.golang.org/cloud";
     buildInputs = [ net oauth2 protobuf google-api-go-client grpc ];
     excludedPackages = "oauth2";
+
+    meta.broken = true;
   };
 
   gcloud-golang-compute-metadata = buildGoPackage rec {
