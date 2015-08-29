1b89ad283ff9fe85e0bd647cdbe61569cf17ffdd
Benjamin Staffin <benley@gmail.com>
2015-08-29 00:11:59 -0700

    prometheus: 0.14.0 -> 0.15.1

--- pkgs/servers/monitoring/prometheus/default.nix
+++ pkgs/servers/monitoring/prometheus/default.nix
@@ -2,47 +2,41 @@
 
 goPackages.buildGoPackage rec {
   name = "prometheus-${version}";
-  version = "0.14.0";
+  version = "0.15.1";
   goPackagePath = "github.com/prometheus/prometheus";
-  rev = "67e77411ba30b1b0ce0989c85b6684fb3adef430";
+  rev = "64349aade284846cb194be184b1b180fca629a7c";
 
   src = fetchFromGitHub {
     inherit rev;
     owner = "prometheus";
     repo = "prometheus";
-    sha256 = "06xsxigimw5i1fla0k83pf5bpmybskvy50433hs8h876gyvgjxp9";
+    sha256 = "0gljpwnlip1fnmhbc96hji2rc56xncy97qccm7v1z5j1nhc5fam2";
   };
 
-  buildInputs = [
-    goPackages.consul
-    goPackages.dns
-    goPackages.fsnotify.v1
-    goPackages.goleveldb
-    goPackages.logrus
-    goPackages.net
-    goPackages.prometheus.client_golang
-    goPackages.prometheus.log
-    goPackages.yaml-v2
-    vim  # for xxd, used in embed-static.sh
+  buildInputs = with goPackages; [
+    consul
+    dns
+    fsnotify.v1
+    go-zookeeper
+    goleveldb
+    httprouter
+    logrus
+    net
+    prometheus.client_golang
+    prometheus.log
+    yaml-v2
   ];
 
   # Metadata that gets embedded into the binary
-  buildFlagsArray = ''
+  buildFlagsArray = let t = "${goPackagePath}/version"; in
+  ''
     -ldflags=
-        -X main.buildVersion ${version}
-        -X main.buildRevision ${builtins.substring 0 6 rev}
-        -X main.buildBranch master
-        -X main.buildUser nix@nixpkgs
-        -X main.buildDate 20150101-00:00:00
-        -X main.goVersion ${lib.getVersion goPackages.go}
-  '';
-
-  preBuild = ''
-  (
-    cd "go/src/$goPackagePath/web"
-    ${stdenv.shell} ../utility/embed-static.sh static templates \
-      | gofmt > blob/files.go
-  )
+        -X ${t}.Version=${version}
+        -X ${t}.Revision=${builtins.substring 0 6 rev}
+        -X ${t}.Branch=master
+        -X ${t}.BuildUser=nix@nixpkgs
+        -X ${t}.BuildDate=20150101-00:00:00
+        -X ${t}.GoVersion=${lib.getVersion goPackages.go}
   '';
 
   meta = with lib; {
