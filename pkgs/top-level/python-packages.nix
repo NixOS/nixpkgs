88d444b36f6ee2a3e6dd6b8f5d865d4b46c67264
Frederik Rietdijk <fridh@fridh.nl>
2015-08-29 13:02:44 +0200

    python-packages objgraph: init at 2.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8850,6 +8850,27 @@ let
       maintainers = with maintainers; [ phreedom thoughtpolice ];
     };
   });
+  
+  objgraph = buildPythonPackage rec {
+    name = "objgraph-${version}";
+    version = "2.0.1";
+    
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/source/o/objgraph/${name}.tar.gz";
+      sha256 = "841de52715774ec1d0e97d9b4462d6e3e10406155f9b61f54ba7db984c45442a";
+    };
+    
+    # Tests fail with PyPy.
+    disabled = isPyPy;
+    
+    propagatedBuildInputs = with self; [pkgs.graphviz];
+    
+    meta = {
+      description = "Draws Python object reference graphs with graphviz";
+      homepage = http://mg.pov.lt/objgraph/;
+      license = licenses.mit;
+    };
+  };
 
   odo = buildPythonPackage rec {
     name = "odo-${version}";
