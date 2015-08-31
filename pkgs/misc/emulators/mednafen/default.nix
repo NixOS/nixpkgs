9ca277d6e6018d0e7197bbbd3be26c832ea902b7
Cillian de Róiste <deroiste@syslab.com>
2015-08-31 13:49:39 +0200

    mednafen: fix src url (sourceforge -> mednafen.fobby.net)
    Also bump the minor version 0.9.38.5 -> 0.9.38.6
    
    (cherry picked from commit 75f880b1d18667e99b3453385460b0d2bf8fe0e7)

--- pkgs/misc/emulators/mednafen/default.nix
+++ pkgs/misc/emulators/mednafen/default.nix
@@ -7,11 +7,11 @@
 stdenv.mkDerivation rec {
 
   name = "mednafen-${version}";
-  version = "0.9.38.5";
+  version = "0.9.38.6";
 
   src = fetchurl {
-    url = "http://downloads.sourceforge.net/project/mednafen/Mednafen/${version}/${name}.tar.bz2";
-    sha256 = "1s1qwwlhkxr0vhzvlc1m0ib9lj7cws3cldm2mbjz4b421nxfdi8h";
+    url = "http://mednafen.fobby.net/releases/files/${name}.tar.bz2";
+    sha256 = "0ivy0vqy1cjd5namn4bdm9ambay6rdccjl9x5418mjyqdhydlq4l";
   };
 
   buildInputs = with stdenv.lib;
