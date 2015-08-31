0631e2233d43c628ebb414dcc59e77000f2ccb85
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-09-01 00:30:19 +0200

    libtiff: 4.0.4beta -> 4.0.5

--- pkgs/development/libraries/libtiff/default.nix
+++ pkgs/development/libraries/libtiff/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, pkgconfig, zlib, libjpeg, xz }:
 
 let
-  version = "4.0.4beta";
+  version = "4.0.5";
 in
 stdenv.mkDerivation rec {
   name = "libtiff-${version}";
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
       [ "ftp://ftp.remotesensing.org/pub/libtiff/tiff-${version}.tar.gz"
         "http://download.osgeo.org/libtiff/tiff-${version}.tar.gz"
       ];
-    sha256 = "0wj8d1iwk9vnpax2h29xqc2hwknxg3s0ay2d5pxkg59ihbifn6pa";
+    sha256 = "171hgy4mylwmvdm7gp6ffjva81m4j56v3fbqsbfl7avzxn1slpp2";
   };
 
   outputs = [ "out" "doc" "man" ];
