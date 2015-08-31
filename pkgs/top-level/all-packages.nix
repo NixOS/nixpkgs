2cf6f7892d50b29514223c5a54df2cde5db328d0
Peter Jones <pjones@pmade.com>
2015-08-31 14:42:05 -0600

    curaLulzbot: init at 15.02.1-1.03-5064
    (cherry picked from commit 674d0a79924e21e0d04a98eb45c0b4e45f64b0a3)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12814,6 +12814,8 @@ let
 
   cura = callPackage ../applications/misc/cura { };
 
+  curaLulzbot = callPackage ../applications/misc/cura/lulzbot.nix { };
+
   peru = callPackage ../applications/version-management/peru {};
 
   printrun = callPackage ../applications/misc/printrun { };
