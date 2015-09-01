655d1253e7ae72022416cea622079a171b8abcb4
William A. Kennington III <william@wkennington.com>
2015-09-01 00:26:14 -0700

    ceph-dev: 9.0.2 -> 9.0.3

--- pkgs/tools/filesystems/ceph/dev.nix
+++ pkgs/tools/filesystems/ceph/dev.nix
@@ -1,12 +1,12 @@
 { callPackage, fetchgit, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "9.0.2";
+  version = "9.0.3";
 
   src = fetchgit {
     url = "https://github.com/ceph/ceph.git";
     rev = "refs/tags/v${version}";
-    sha256 = "0kydjyvb1566mh33p6dlljfx1r4cfdj8ic4i19h5r9vavkc46nf0";
+    sha256 = "08ccy735srzmi505qlrpqdh5js40mpd5k1vdhnysknra0vqlpmdx";
   };
 
   patches = [ ./fix-pythonpath.patch ];
