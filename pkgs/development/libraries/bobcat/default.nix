7e4675cb00f81f8533a405ab6ff7b8440b8a851f
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-09-01 03:46:39 +0200

    bobcat: 3.25.02 -> 4.00.00

--- pkgs/development/libraries/bobcat/default.nix
+++ pkgs/development/libraries/bobcat/default.nix
@@ -1,26 +1,30 @@
-{ stdenv, fetchurl, icmake, libmilter, libX11, openssl, readline
+{ stdenv, fetchFromGitHub, icmake, libmilter, libX11, openssl, readline
 , utillinux, yodl }:
 
-let version = "3.25.02"; in
+let version = "4.00.00"; in
 stdenv.mkDerivation {
   name = "bobcat-${version}";
 
-  src = fetchurl {
-    sha256 = "0b1370li4q82fqj982vng9cwkf23k2c1df5jsdcgkrk01r53dxry";
-    url = "mirror://debian/pool/main/b/bobcat/bobcat_${version}.orig.tar.gz";
+  src = fetchFromGitHub {
+    sha256 = "0wdb25sgw7i3jk3lbja6b4ipqfg1sncam6adg2bn8l5fcinrpwgs";
+    rev = version;
+    repo = "bobcat";
+    owner = "fbb-git";
   };
 
   meta = with stdenv.lib; {
     inherit version;
     description = "Brokken's Own Base Classes And Templates";
-    downloadPage = http://sourceforge.net/projects/bobcat/files/;
+    homepage = https://fbb-git.github.io/bobcat/;
     license = licenses.gpl3;
-    platforms = with platforms; linux;
+    platforms = platforms.linux;
     maintainers = with maintainers; [ nckx ];
   };
 
   buildInputs = [ libmilter libX11 openssl readline utillinux ];
   nativeBuildInputs = [ icmake yodl ];
+
+  sourceRoot = "bobcat-${version}-src/bobcat";
 
   postPatch = ''
     substituteInPlace INSTALL.im --replace /usr $out
