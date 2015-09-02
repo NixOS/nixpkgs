9a0f8b5a4f27623c81ef87499d6aea541f831079
Vincent Laporte <Vincent.Laporte@gmail.com>
2015-09-02 19:40:38 +0200

    wxPython-3: refactor

--- pkgs/development/python-modules/wxPython/3.0.nix
+++ pkgs/development/python-modules/wxPython/3.0.nix
@@ -1,29 +1,32 @@
-{ stdenv, fetchurl, pkgconfig, wxGTK, pythonPackages, openglSupport ? true, python, isPyPy }:
+{ stdenv, fetchurl, pkgconfig, python, buildPythonPackage, isPy3k, isPyPy, wxGTK, openglSupport ? true, pyopengl }:
 
 assert wxGTK.unicode;
 
-with stdenv.lib;
+buildPythonPackage rec {
 
-let version = "3.0.2.0"; in
+  disabled = isPy3k || isPyPy;
+  doCheck = false;
 
-if isPyPy then throw "wxPython-${version} not supported for interpreter ${python.executable}" else stdenv.mkDerivation {
+  version = "3.0.2.0";
   name = "wxPython-${version}";
-  
-  builder = ./builder3.0.sh;
-  
+
   src = fetchurl {
     url = "mirror://sourceforge/wxpython/wxPython-src-${version}.tar.bz2";
     sha256 = "0qfzx3sqx4mwxv99sfybhsij4b5pc03ricl73h4vhkzazgjjjhfm";
   };
-  
-  buildInputs = [ pkgconfig wxGTK (wxGTK.gtk) pythonPackages.python pythonPackages.wrapPython ]
-                ++ optional openglSupport pythonPackages.pyopengl;
+
+  buildInputs = [ pkgconfig wxGTK (wxGTK.gtk) ]
+                ++ stdenv.lib.optional openglSupport pyopengl;
+
+  preConfigure = "cd wxPython";
+
+  setupPyBuildFlags = [ "WXPORT=gtk2" "NO_HEADERS=1" "BUILD_GLCANVAS=${if openglSupport then "1" else "0"}" "UNICODE=1" ];
+
+  installPhase = ''
+    ${python}/bin/${python.executable} setup.py ${stdenv.lib.concatStringsSep " " setupPyBuildFlags} install --prefix=$out
+  '';
 
   inherit openglSupport;
 
   passthru = { inherit wxGTK openglSupport; };
-  
-  meta = {
-    platforms = stdenv.lib.platforms.all;
-  };
 }
