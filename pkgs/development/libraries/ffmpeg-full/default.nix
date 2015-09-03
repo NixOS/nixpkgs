e2ebe9199104115fd419b9efea6330ef49065f5b
Luca Bruno <lethalman88@gmail.com>
2015-09-03 11:09:55 +0200

    ffmpeg-full: fix src
    (cherry picked from commit e27c796b51c277a4faceeb25604133582c67448d)

--- pkgs/development/libraries/ffmpeg-full/default.nix
+++ pkgs/development/libraries/ffmpeg-full/default.nix
@@ -234,7 +234,7 @@ stdenv.mkDerivation rec {
   version = "2.7.2";
 
   src = fetchurl {
-    url = "https://www.ffmpeg.org/releases/${name}.tar.bz2";
+    url = "https://www.ffmpeg.org/releases/ffmpeg-${version}.tar.bz2";
     sha256 = "1wlygd0jp34dk4qagi4h9psn4yk8zgyj7zy9lrpm5332mm87bsvw";
   };
 
