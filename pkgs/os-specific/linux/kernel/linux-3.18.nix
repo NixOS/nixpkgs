8452d2a316bc0a9b1eba3e13e82458b436348093
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-09-03 16:50:14 +0200

    linux: Update to 3.18.21
    (cherry picked from commit 90dc8da64d26af0c5582f10b044533dbaf78402a)

--- pkgs/os-specific/linux/kernel/linux-3.18.nix
+++ pkgs/os-specific/linux/kernel/linux-3.18.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.18.20";
+  version = "3.18.21";
   extraMeta.branch = "3.18";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1mwm9xgilsqnj95v3jn94dz9a108ggfm4ifb3kxsfsx5rcl5yy8d";
+    sha256 = "0y54kh55grgbyw4k8fa9vx8b426bq9lz12bpvwvzfjs7vimachyw";
   };
 
   features.iwlwifi = true;
