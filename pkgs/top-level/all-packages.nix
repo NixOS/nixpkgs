b4109214af0e93403f0767839b6fcdcb3c6cf19a
William A. Kennington III <william@wkennington.com>
2015-09-02 17:18:40 -0700

    ngrok: Move to go-packages

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2386,7 +2386,7 @@ let
     inherit gnumake3;
   };
 
-  ngrok = callPackage ../tools/misc/ngrok { };
+  ngrok = pkgs.goPackages.ngrok.bin // { outputs = [ "bin" ]; };
 
   noip = callPackage ../tools/networking/noip { };
 
