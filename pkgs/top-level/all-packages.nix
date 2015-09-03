fba289e52c0fdc0891015dc6379db39266e4beff
Nikolay Amiantov <ab@fmap.me>
2015-09-04 01:40:30 +0300

    pcre2: init at 10.20

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7889,6 +7889,10 @@ let
     unicodeSupport = config.pcre.unicode or true;
   };
 
+  pcre2 = callPackage ../development/libraries/pcre2 {
+    unicodeSupport = config.pcre.unicode or true;
+  };
+
   pdf2xml = callPackage ../development/libraries/pdf2xml {} ;
 
   phonon = callPackage ../development/libraries/phonon/qt4 {};
