ed140ff9272885b62dc896ae36e80c251b49917a
Rickard Nilsson <rickynils@gmail.com>
2015-09-04 21:46:10 +0200

    bosun,scollector: Fix NixOS modules to use bin attr of go pkgs

--- nixos/modules/services/monitoring/bosun.nix
+++ nixos/modules/services/monitoring/bosun.nix
@@ -30,7 +30,8 @@ in {
 
       package = mkOption {
         type = types.package;
-        example = literalExample "pkgs.bosun";
+        default = pkgs.bosun.bin;
+        example = literalExample "pkgs.bosun.bin";
         description = ''
           bosun binary to use.
         '';
@@ -95,8 +96,6 @@ in {
 
   config = mkIf cfg.enable {
   
-    services.bosun.package = mkDefault pkgs.bosun; 
-
     systemd.services.bosun = {
       description = "bosun metrics collector (part of Bosun)";
       wantedBy = [ "multi-user.target" ];
