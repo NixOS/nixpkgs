22321f2e58335cd39051c9403975b1015cfa68e0
Bjørn Forsman <bjorn.forsman@gmail.com>
2015-09-04 12:29:22 +0200

    cudatoolkit: don't move $out/include to $out/usr_include
    This effectively reverts 86c283824f76d849acbe6
    ("If cuda headers are presented to nix [...]") and all the following
    workarounds that was added due to that commit.
    
    As far as I can tell[1] this hack isn't needed anymore. And moving
    includes to $out/usr_include causes pain for cudatoolkit users, so
    better get rid of it.
    
    In patches that did more than the $out/usr_include workaround, I only
    changed the line back to $out/include instead of re-generating the
    patches and fully removing the changed line.
    
    [1]: I build tested blender and caffe, and temporarily added
    recurseIntoAttrs to rPackages and haskellPackages so that nox-review
    could get proper coverage. However, many of the packages do not build
    even before this patch. I also built CUDA samples with cudatoolkit7
    that ran fine.

--- pkgs/development/compilers/cudatoolkit/setup-hook.sh
+++
@@ -1,8 +0,0 @@
-addIncludePath () {
-    if test -d "$1/usr_include"
-    then
-        export NIX_CFLAGS_COMPILE="${NIX_CFLAGS_COMPILE} -I$1/usr_include"
-    fi
-}
-
-envHooks=(${envHooks[@]} addIncludePath)
