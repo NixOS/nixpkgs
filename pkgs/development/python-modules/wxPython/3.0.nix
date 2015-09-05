f6a875d5ae12c8ef6587a3a632640225a13aca1b
Vincent Laporte <Vincent.Laporte@gmail.com>
2015-09-05 12:58:00 +0200

    wxPython: refactor to generic

--- pkgs/development/python-modules/wxPython/3.0.nix
+++ pkgs/development/python-modules/wxPython/3.0.nix
@@ -1,32 +1,9 @@
-{ stdenv, fetchurl, pkgconfig, python, buildPythonPackage, isPy3k, isPyPy, wxGTK, openglSupport ? true, pyopengl }:
+{ callPackage, ... } @ args:
 
-assert wxGTK.unicode;
-
-buildPythonPackage rec {
-
-  disabled = isPy3k || isPyPy;
-  doCheck = false;
+callPackage ./generic.nix (args // rec {
 
   version = "3.0.2.0";
-  name = "wxPython-${version}";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/wxpython/wxPython-src-${version}.tar.bz2";
-    sha256 = "0qfzx3sqx4mwxv99sfybhsij4b5pc03ricl73h4vhkzazgjjjhfm";
-  };
+  sha256 = "0qfzx3sqx4mwxv99sfybhsij4b5pc03ricl73h4vhkzazgjjjhfm";
 
-  buildInputs = [ pkgconfig wxGTK (wxGTK.gtk) ]
-                ++ stdenv.lib.optional openglSupport pyopengl;
-
-  preConfigure = "cd wxPython";
-
-  setupPyBuildFlags = [ "WXPORT=gtk2" "NO_HEADERS=1" "BUILD_GLCANVAS=${if openglSupport then "1" else "0"}" "UNICODE=1" ];
-
-  installPhase = ''
-    ${python}/bin/${python.executable} setup.py ${stdenv.lib.concatStringsSep " " setupPyBuildFlags} install --prefix=$out
-  '';
-
-  inherit openglSupport;
-
-  passthru = { inherit wxGTK openglSupport; };
-}
+})
---
+++ pkgs/development/python-modules/wxPython/generic.nix
@@ -0,0 +1,34 @@
+{ stdenv, fetchurl, pkgconfig, python, buildPythonPackage, isPy3k, isPyPy, wxGTK, openglSupport ? true, pyopengl
+, version, sha256, ...
+}:
+
+assert wxGTK.unicode;
+
+buildPythonPackage rec {
+
+  disabled = isPy3k || isPyPy;
+  doCheck = false;
+
+  name = "wxPython-${version}";
+  inherit version;
+
+  src = fetchurl {
+    url = "mirror://sourceforge/wxpython/wxPython-src-${version}.tar.bz2";
+    inherit sha256;
+  };
+
+  buildInputs = [ pkgconfig wxGTK (wxGTK.gtk) ]
+                ++ stdenv.lib.optional openglSupport pyopengl;
+
+  preConfigure = "cd wxPython";
+
+  setupPyBuildFlags = [ "WXPORT=gtk2" "NO_HEADERS=1" "BUILD_GLCANVAS=${if openglSupport then "1" else "0"}" "UNICODE=1" ];
+
+  installPhase = ''
+    ${python}/bin/${python.executable} setup.py ${stdenv.lib.concatStringsSep " " setupPyBuildFlags} install --prefix=$out
+  '';
+
+  inherit openglSupport;
+
+  passthru = { inherit wxGTK openglSupport; };
+}
