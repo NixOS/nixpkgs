59b6c1f51ac464a29f86699c9838a23368cd76db
Pascal Wittmann <mail@pascal-wittmann.de>
2015-09-06 21:16:33 +0200

    tennix: update from 1.0 to 1.1

--- pkgs/games/tennix/default.nix
+++ pkgs/games/tennix/default.nix
@@ -1,23 +1,26 @@
-{stdenv, fetchurl, SDL, SDL_mixer, SDL_image, SDL_ttf} :
+{stdenv, fetchurl, SDL, SDL_mixer, SDL_image, SDL_ttf, SDL_net, python } :
 
 stdenv.mkDerivation rec {
-  name = "tennix-1.0";
+  name = "tennix-1.1";
   src = fetchurl {
     url = "http://icculus.org/tennix/downloads/${name}.tar.gz";
-    sha256 = "18rd7h1j5skpkh037misixw9gigzc7qy13vrnrs21rphcfxzpifn";
+    sha256 = "0np5kw1y7i0z0dsqx4r2nvmq86qj8hv3mmgavm3hxraqnds5z8cm";
   };
 
+  buildInputs = [ python SDL SDL_mixer SDL_image SDL_ttf SDL_net ];
+
+  patches = [ ./fix_FTBFS.patch ];
 
   preConfigure = ''
-    makeFlags="PREFIX=$out USE_PYTHON=0"
+    makeFlags="PREFIX=$out"
     installFlags="PREFIX=$out install"
   '';
 
-  buildInputs = [ SDL SDL_mixer SDL_image SDL_ttf ];
-
-  meta = {
+  meta = with stdenv.lib; {
     homepage = http://icculus.org/tennix/;
-    description = "Tennix 2009 World Tennis Championship Tour";
-    license = stdenv.lib.licenses.gpl2Plus;
+    description = "Classic Championship Tour 2011";
+    license = licenses.gpl2Plus;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ pSub ];
   };
 }
