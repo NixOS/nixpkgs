855716a66b96b5e752eb6a1933a6a5d4328c4d03
Pascal Wittmann <mail@pascal-wittmann.de>
2015-09-08 18:07:12 +0200

    ceptre: 2015-08-15 -> 2015-08-30

--- pkgs/development/interpreters/ceptre/default.nix
+++ pkgs/development/interpreters/ceptre/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchgit, mlton }:
 
 stdenv.mkDerivation rec {
-  name = "ceptre-2015-08-15";
+  name = "ceptre-2015-08-30";
 
   src = fetchgit {
     url = https://github.com/chrisamaphone/interactive-lp;
-    rev = "cffca6943b6c9f47d064e26ddad92c2d8eb6f0f9";
-    sha256 = "0filimwssb656330d9gc8299l44imc5qyaf967p03ypg5260xq0i";
+    rev = "f16ebee257d63396b8456c48698d255c118d7157";
+    sha256 = "0d5s8nzsjl3l7g723588l19j3pyxkdrqnfs9nngv1d9syqyb5395";
   };
 
   nativeBuildInputs = [ mlton ];
