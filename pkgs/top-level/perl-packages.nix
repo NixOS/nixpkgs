67c34d35ec95bb6cb9a5e49767746998e4fa56b0
Pascal Wittmann <mail@pascal-wittmann.de>
2015-09-08 15:35:34 +0200

    IO-Tee: init at 0.64

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -5469,6 +5469,14 @@ let self = _self // overrides; _self = with self; {
     };
   };
 
+  IOTee = buildPerlPackage rec {
+    name = "IO-Tee-0.64";
+    src = fetchurl {
+      url = "mirror://cpan/authors/id/K/KE/KENSHAN/${name}.tar.gz";
+      sha256 = "1mjy6hmwrzipzxcm33qs7ja89ljk6zkk499wclw16lfkqaqpdliy";
+    };
+  };
+
   IOTieCombine = buildPerlPackage {
     name = "IO-TieCombine-1.004";
     src = fetchurl {
