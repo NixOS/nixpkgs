26eaa9309469168bc6be64c638631bde478b2ac1
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-09-10 01:47:09 +0200

    rsstail: 2.0 -> 2015-09-06
    There are no releases, so don't let's pretend.

--- pkgs/applications/networking/feedreaders/rsstail/default.nix
+++ pkgs/applications/networking/feedreaders/rsstail/default.nix
@@ -1,27 +1,14 @@
 { stdenv, fetchFromGitHub, cppcheck, libmrss }:
 
-let version = "2.0"; in
+let version = "2015-09-06"; in
 stdenv.mkDerivation rec {
   name = "rsstail-${version}";
 
   src = fetchFromGitHub {
-    sha256 = "0fbsyl5bdxr2g25ps7kd34sa0mzggklbg4v7qss68gh82zdp16ch";
-    rev = "69dc5e30439b89c037aa49c5af861f28df607c72";
+    sha256 = "1rfzib5fzm0i8wf3njld1lvxgbci0hxxnvp2qx1k4bwpv744xkpx";
+    rev = "16636539e4cc75dafbfa7f05539769be7dad4b66";
     repo = "rsstail";
     owner = "flok99";
-  };
-
-  meta = with stdenv.lib; {
-    inherit version;
-    description = "Monitor RSS feeds for new entries";
-    longDescription = ''
-      RSSTail is more or less an RSS reader: it monitors an RSS feed and if it
-      detects a new entry it'll emit only that new entry.
-    '';
-    homepage = http://www.vanheusden.com/rsstail/;
-    license = licenses.gpl2Plus;
-    platforms = with platforms; linux;
-    maintainers = with maintainers; [ nckx ];
   };
 
   buildInputs = [ libmrss ]
@@ -35,4 +22,17 @@ stdenv.mkDerivation rec {
   enableParallelBuilding = true;
 
   doCheck = true;
+
+  meta = with stdenv.lib; {
+    inherit version;
+    description = "Monitor RSS feeds for new entries";
+    longDescription = ''
+      RSSTail is more or less an RSS reader: it monitors an RSS feed and if it
+      detects a new entry it'll emit only that new entry.
+    '';
+    homepage = http://www.vanheusden.com/rsstail/;
+    license = licenses.gpl2Plus;
+    platforms = with platforms; linux;
+    maintainers = with maintainers; [ nckx ];
+  };
 }
