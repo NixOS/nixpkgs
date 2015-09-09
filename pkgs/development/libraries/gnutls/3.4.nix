afd73615d6bebf20eba64e34f89321400ebf06a0
William A. Kennington III <william@wkennington.com>
2015-09-09 13:05:00 -0700

    gnutls: Fix parallel build issue introduced in 3.4.x

--- pkgs/development/libraries/gnutls/3.4.nix
+++ pkgs/development/libraries/gnutls/3.4.nix
@@ -1,4 +1,4 @@
-{ callPackage, fetchurl, ... } @ args:
+{ callPackage, fetchurl, autoreconfHook, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
   version = "3.4.4";
@@ -7,4 +7,11 @@ callPackage ./generic.nix (args // rec {
     url = "ftp://ftp.gnutls.org/gcrypt/gnutls/v3.4/gnutls-${version}.tar.lz";
     sha256 = "17xazr0fdhlkr13bwiy52xq6z6mssml7q1ydyj8s1hwh68703c75";
   };
+
+  # This fixes some broken parallel dependencies
+  postPatch = ''
+    sed -i 's,^BUILT_SOURCES =,\0 systemkey-args.h,g' src/Makefile.am
+  '';
+
+  nativeBuildInputs = [ autoreconfHook ];
 })
