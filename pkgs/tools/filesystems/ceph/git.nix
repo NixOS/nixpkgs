57d766277a8900f139096f843220a3dfe24a0d21
William A. Kennington III <william@wkennington.com>
2015-09-11 15:06:19 -0700

    ceph-git: 2015-09-04 -> 2015-09-11

--- pkgs/tools/filesystems/ceph/git.nix
+++ pkgs/tools/filesystems/ceph/git.nix
@@ -1,12 +1,12 @@
 { callPackage, fetchgit, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "2015-09-04";
+  version = "2015-09-11";
 
   src = fetchgit {
     url = "git://github.com/ceph/ceph.git";
-    rev = "8c17e0197481205f8726b06d57313ffe08fba2bf";
-    sha256 = "189l412m6x0f0mqzhgzwfa3sgm5xfxzb9synlvbfm3n1fgdhj5iy";
+    rev = "78de6fd61d5c0422f18c2f683b2cc489f3cbb4d3";
+    sha256 = "1kbrsr6vzvprcdq0hg7cgcmvqc61w3i1yzgrmjdmy3jcsmc979xi";
   };
 
   patches = [ ./fix-pythonpath.patch ];
