806f65c5c8d49a98583bd9c879b42131b82de76c
Pascal Wittmann <mail@pascal-wittmann.de>
2015-09-11 18:24:10 +0200

    pinpoint: 0.1.4 -> 0.1.6

--- pkgs/applications/office/pinpoint/default.nix
+++ pkgs/applications/office/pinpoint/default.nix
@@ -3,17 +3,19 @@
 
 stdenv.mkDerivation rec {
   name = "pinpoint-${version}";
-  version = "0.1.4";
+  version = "0.1.6";
   src = fetchurl {
-    url = "http://ftp.gnome.org/pub/GNOME/sources/pinpoint/0.1/${name}.tar.bz2";
-    sha256 = "26df7ba171d13f697c30c272460989b0f1b45e70c797310878a589ed5a6a47de";
+    url = "http://ftp.gnome.org/pub/GNOME/sources/pinpoint/0.1/${name}.tar.xz";
+    sha256 = "0jzkf74w75paflnvsil2y6qsyaqgxf6akz97176xdg6qri4nwal1";
   };
   buildInputs = [ pkgconfig autoconf automake clutter clutter-gst gdk_pixbuf
                   cairo ];
 
-  meta = {
+  meta = with stdenv.lib; {
     homepage = https://wiki.gnome.org/action/show/Apps/Pinpoint;
     description = "A tool for making hackers do excellent presentations";
-    license = stdenv.lib.licenses.lgpl21;
+    license = licenses.lgpl21;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ pSub ];
   };
 }
