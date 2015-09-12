6727f767395b8da7700bc56694a51e13e7f07144
William A. Kennington III <william@wkennington.com>
2015-09-11 17:37:14 -0700

    zfs: Update to 0.6.5

--- pkgs/os-specific/linux/zfs/default.nix
+++ pkgs/os-specific/linux/zfs/default.nix
@@ -1,19 +1,14 @@
 { callPackage, fetchFromGitHub, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "0.6.4.2";
+  version = "0.6.5";
 
   src = fetchFromGitHub {
     owner = "zfsonlinux";
     repo = "zfs";
     rev = "zfs-${version}";
-    sha256 = "192x4z5am5wgrr4hb5skshyr5a6af52xpnk6pni4hs4pxvlpcs37";
+    sha256 = "1jqm2a9mldp4km5m454zszsw6p8hrqd7xrbf52pgp82kf5w3d6wz";
   };
 
-  patches = [
-    ./nix-build.patch
-    ./compat-4.2-1.patch
-    ./compat-4.2-2.patch
-    ./compat-4.2-3.patch
-  ];
+  patches = [ ./nix-build.patch ];
 })
