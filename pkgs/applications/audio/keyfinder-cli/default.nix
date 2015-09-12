a365bbd91057f9d3acdec5989d0b526f1216b6c7
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-09-12 02:34:22 +0200

    keyfinder-cli: 20150201 (sic) -> 2015-09-11

--- pkgs/applications/audio/keyfinder-cli/default.nix
+++ pkgs/applications/audio/keyfinder-cli/default.nix
@@ -1,15 +1,21 @@
-{ stdenv, fetchFromGitHub, libav, libkeyfinder_0_11 }:
+{ stdenv, fetchFromGitHub, libav, libkeyfinder }:
 
-let version = "20150201"; in
+let version = "2015-09-11"; in
 stdenv.mkDerivation rec {
   name = "keyfinder-cli-${version}";
 
   src = fetchFromGitHub {
     repo = "keyfinder-cli";
     owner = "EvanPurkhiser";
-    rev = "e8a20e73f8a465a6c3c9e71dabf4b636244a9b0c";
-    sha256 = "0x198ijr6wgzq24642s4pz5zxn4gvcc7dxmb6d1bfn3dwzi3j8lp";
+    rev = "94df5e6c012a2643a4e5eda49a80cdc57d2e1bb2";
+    sha256 = "04276z5xrpgf1zga565awypy5r7g054fwl4x75l7j308x0xcp12j";
   };
+
+  buildInputs = [ libav libkeyfinder ];
+
+  makeFlagsArray = "PREFIX=$(out)";
+
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     inherit version;
@@ -24,10 +30,4 @@ stdenv.mkDerivation rec {
     platforms = with platforms; linux;
     maintainers = with maintainers; [ nckx ];
   };
-
-  buildInputs = [ libav libkeyfinder_0_11 ];
-
-  makeFlagsArray = "PREFIX=$(out)";
-
-  enableParallelBuilding = true;
 }
