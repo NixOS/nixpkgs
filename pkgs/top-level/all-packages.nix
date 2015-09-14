d749f111ab913e5acd70364a46a54067e2504bcd
Robert Helgesson <robert@rycee.net>
2015-09-14 20:06:17 +0200

    svtplay-dl: 0.10.2015.08.24 -> 0.20.2015.09.13

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3140,7 +3140,7 @@ let
   svnfs = callPackage ../tools/filesystems/svnfs { };
 
   svtplay-dl = callPackage ../tools/misc/svtplay-dl {
-    inherit (pythonPackages) nose mock;
+    inherit (pythonPackages) nose mock requests2;
   };
 
   sysbench = callPackage ../development/tools/misc/sysbench {};
