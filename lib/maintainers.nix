515cd8a93a7ff6fe766eacb450164106b460d948
Samuel Rivas <samuelrivas@gmail.com>
2015-09-14 19:29:35 +0200

    erlang-mode: refactor maintainers

--- lib/maintainers.nix
+++ lib/maintainers.nix
@@ -234,6 +234,7 @@
   rszibele = "Richard Szibele <richard_szibele@hotmail.com>";
   rushmorem = "Rushmore Mushambi <rushmore@webenchanter.com>";
   rycee = "Robert Helgesson <robert@rycee.net>";
+  samuelrivas = "Samuel Rivas <samuelrivas@gmail.com>";
   sander = "Sander van der Burg <s.vanderburg@tudelft.nl>";
   schmitthenner = "Fabian Schmitthenner <development@schmitthenner.eu>";
   schristo = "Scott Christopher <schristopher@konputa.com>";
