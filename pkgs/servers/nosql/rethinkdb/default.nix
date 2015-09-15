bab211893bf3e03abb8843d7478628411acd05af
Austin Seipp <aseipp@pobox.com>
2015-09-15 18:03:30 -0500

    nixpkgs: rethinkdb 2.0.4 -> 2.1.3
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/servers/nosql/rethinkdb/default.nix
+++ pkgs/servers/nosql/rethinkdb/default.nix
@@ -4,11 +4,11 @@
 
 stdenv.mkDerivation rec {
   name = "rethinkdb-${version}";
-  version = "2.0.4";
+  version = "2.1.3";
 
   src = fetchurl {
     url = "http://download.rethinkdb.com/dist/${name}.tgz";
-    sha256 = "19qhia4lfa8a0rzp2v6lnlxp2lf4z4vqhgfxnicfdnx07q4r847i";
+    sha256 = "03w9fq3wcvwy04b3x6zb3hvwar7b9jfbpq77rmxdlgh5w64vvgwd";
   };
 
   preConfigure = ''
@@ -30,14 +30,14 @@ stdenv.mkDerivation rec {
   meta = {
     description = "An open-source distributed database built with love";
     longDescription = ''
-      RethinkDB is built to store JSON documents, and scale to multiple machines with very little
-      effort. It has a pleasant query language that supports really useful queries like table joins
-      and group by, and is easy to setup and learn.
+      RethinkDB is built to store JSON documents, and scale to
+      multiple machines with very little effort. It has a pleasant
+      query language that supports really useful queries like table
+      joins and group by, and is easy to setup and learn.
     '';
-    homepage = http://www.rethinkdb.com;
-    license = stdenv.lib.licenses.agpl3;
-
-    maintainers = [ stdenv.lib.maintainers.bluescreen303 ];
-    platforms = stdenv.lib.platforms.all;
+    homepage    = http://www.rethinkdb.com;
+    license     = stdenv.lib.licenses.agpl3;
+    platforms   = stdenv.lib.platforms.linux;
+    maintainers = with stdenv.lib.maintainers; [ thoughtpolice bluescreen303 ];
   };
 }
