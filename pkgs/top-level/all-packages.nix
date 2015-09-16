e299e8f1eea316789849ad56a146c1c53c39fd32
Andreas Wagner <andreas.wagner@lowfatcomputing.org>
2015-09-16 10:22:41 -0400

    teensy-loader-cli: consistent naming, version bump

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5746,7 +5746,7 @@ let
 
   tcptrack = callPackage ../development/tools/misc/tcptrack { };
 
-  teensy-loader = callPackage ../development/tools/misc/teensy { };
+  teensy-loader-cli = callPackage ../development/tools/misc/teensy-loader-cli { };
 
   texinfo413 = callPackage ../development/tools/misc/texinfo/4.13a.nix { };
   texinfo4 = texinfo413;
