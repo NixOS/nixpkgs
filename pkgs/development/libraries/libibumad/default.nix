05853e1d2a9c4d653b732b75a3df4421e04d5913
William A. Kennington III <william@wkennington.com>
2015-09-17 18:27:06 -0700

    libibumad: 1.3.9 -> 1.3.10.2

--- pkgs/development/libraries/libibumad/default.nix
+++ pkgs/development/libraries/libibumad/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "libibumad-1.3.9";
+  name = "libibumad-1.3.10.2";
 
   src = fetchurl {
     url = "https://www.openfabrics.org/downloads/management/${name}.tar.gz";
-    sha256 = "0j52aiwfgasf7bzx65svd5h2ya7848c5racf191i8irsxa155q74";
+    sha256 = "0bkygb3lbpaj6s4vsyixybrrkcnilbijv4ga5p1xdwyr3gip83sh";
   };
 
   meta = with stdenv.lib; {
