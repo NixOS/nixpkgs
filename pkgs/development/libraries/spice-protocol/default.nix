332826cce4c14b8ca90431239915be4d8a80d43f
William A. Kennington III <william@wkennington.com>
2015-09-18 11:16:44 -0700

    spice-protocol: 0.12.7 -> 0.12.9

--- pkgs/development/libraries/spice-protocol/default.nix
+++ pkgs/development/libraries/spice-protocol/default.nix
@@ -1,12 +1,17 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "spice-protocol-0.12.7";
+  name = "spice-protocol-0.12.9";
 
   src = fetchurl {
     url = "http://www.spice-space.org/download/releases/${name}.tar.bz2";
-    sha256 = "1hhn94bw2l76h09sy05a15bs6zalsijnylyqpwcys5hq6rrwpiln";
+    sha256 = "0xkqycpqpkxjlcg4fk1vyv2vjni60s0fjx5l57918q0mvlsgh319";
   };
+
+  postInstall = ''
+    mkdir -p $out/lib
+    ln -sv ../share/pkgconfig $out/lib/pkgconfig
+  '';
 
   meta = with stdenv.lib; {
     description = "Protocol headers for the SPICE protocol";
