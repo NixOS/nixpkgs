9cc7859b2ea2a9f79a17489ba945c0358296c8ed
Jan Malakhovski <oxij@oxij.org>
2015-09-18 17:50:31 +0000

    nixos: show the manual in system's /share/doc (close #9928)

--- nixos/modules/services/misc/nixos-manual.nix
+++ nixos/modules/services/misc/nixos-manual.nix
@@ -92,7 +92,7 @@ in
 
     system.build.manual = manual;
 
-    environment.systemPackages = [ manual.manpages help ];
+    environment.systemPackages = [ manual.manpages manual.manual help ];
 
     boot.extraTTYs = mkIf cfg.showManual ["tty${cfg.ttyNumber}"];
 
