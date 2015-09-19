d9fd037e889ccfa1b9dd903ea7164fd11d3403f9
William A. Kennington III <william@wkennington.com>
2015-09-19 15:02:59 -0700

    zfs + zfs_git: 0.6.5 -> 0.6.5.1

--- pkgs/os-specific/linux/zfs/default.nix
+++ pkgs/os-specific/linux/zfs/default.nix
@@ -1,17 +1,14 @@
 { callPackage, fetchFromGitHub, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "0.6.5";
+  version = "0.6.5.1";
 
   src = fetchFromGitHub {
     owner = "zfsonlinux";
     repo = "zfs";
     rev = "zfs-${version}";
-    sha256 = "1jqm2a9mldp4km5m454zszsw6p8hrqd7xrbf52pgp82kf5w3d6wz";
+    sha256 = "0lbii5kc3b68zj8mvvznl05czwdkr0ld3a2javbkngfvrcn09rz2";
   };
 
-  patches = [
-    ./nix-build.patch
-    ./0.6.5-fix-corruption.patch
-  ];
+  patches = [ ./nix-build.patch ];
 })
