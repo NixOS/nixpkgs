3d5f95d61bb02b569c7380ccffbe9416225d2931
Ian-Woo Kim <ianwookim@gmail.com>
2015-09-21 17:00:01 +0000

    gradle: reinit at 2.5.
    For some projects (especially using android ndk), we still need to keep 2.5.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5618,6 +5618,8 @@ let
 
   gradle = callPackage ../development/tools/build-managers/gradle { };
 
+  gradle25 = callPackage ../development/tools/build-managers/gradle/2.5.nix { };
+
   gperf = callPackage ../development/tools/misc/gperf { };
 
   grail = callPackage ../development/libraries/grail { };
