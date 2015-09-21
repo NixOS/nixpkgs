b038770579fb0d70525d592fcd52533a05bc329e
William A. Kennington III <william@wkennington.com>
2015-09-21 11:33:30 -0700

    btrfsProgs: 4.2.0 -> 4.2.1

--- pkgs/tools/filesystems/btrfsprogs/default.nix
+++ pkgs/tools/filesystems/btrfsprogs/default.nix
@@ -2,14 +2,14 @@
 , asciidoc, xmlto, docbook_xml_dtd_45, docbook_xsl, libxslt
 }:
 
-let version = "4.2"; in
+let version = "4.2.1"; in
 
 stdenv.mkDerivation rec {
   name = "btrfs-progs-${version}";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v${version}.tar.xz";
-    sha256 = "1glglnglwz1rz6zy54xwdm601n5csbd1g73lx0rz8ay5jhz3q8r5";
+    sha256 = "1nzyyfj84q98n7lf798k6695pbsd02hg2ffd1kgn8g3qcbwqhwar";
   };
 
   buildInputs = [
