9e15068099a5b522660cbee3944a4022a436364a
Robbin C <robbinch33@gmail.com>
2015-09-21 11:56:10 +0800

    ctpp2: init at 2.8.3.
    This adds ctpp2, a high performance templating engine.

---
+++ pkgs/development/libraries/ctpp2/default.nix
@@ -0,0 +1,24 @@
+{ stdenv, fetchurl, cmake, gcc48 }:
+
+stdenv.mkDerivation rec {
+  name = "ctpp2";
+  version = "2.8.3";
+
+  src = fetchurl {
+    url = "http://ctpp.havoc.ru/download/ctpp2-${version}.tar.gz";
+    sha256 = "1z22zfw9lb86z4hcan9hlvji49c9b7vznh7gjm95gnvsh43zsgx8";
+  };
+
+  buildInputs = [ cmake gcc48 ];
+
+  patchPhase = ''
+    # include <unistd.h> to fix undefined getcwd
+    sed -ie 's/<stdlib.h>/<stdlib.h>\n#include <unistd.h>/' src/CTPP2FileSourceLoader.cpp
+  '';
+
+  meta = {
+    description = "A high performance templating engine";
+    homepage = http://ctpp.havoc.ru;
+    maintiainers = with stdenv.lib.maintainers; [ robbinch ];
+  };
+}
