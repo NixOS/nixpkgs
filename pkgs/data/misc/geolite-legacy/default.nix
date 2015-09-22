6f9141cbd33a0b88a674fd58bd4937929766e6c4
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-09-22 03:47:07 +0200

    geolite-legacy: update 2015-09-14 -> 2015-09-22
    (cherry picked from commit ceb94adffeed36708990bfd13a7ef16410881704)

--- pkgs/data/misc/geolite-legacy/default.nix
+++ pkgs/data/misc/geolite-legacy/default.nix
@@ -8,7 +8,7 @@ let
 
   # Annoyingly, these files are updated without a change in URL. This means that
   # builds will start failing every month or so, until the hashes are updated.
-  version = "2015-09-14";
+  version = "2015-09-22";
 in
 stdenv.mkDerivation {
   name = "geolite-legacy-${version}";
@@ -27,10 +27,10 @@ stdenv.mkDerivation {
     "0f3y1cpjfd4q55a2kvhzsklmmp6k19v9vsdsjxr4sapc8f5fgfc9";
   srcGeoIPASNum = fetchDB
     "asnum/GeoIPASNum.dat.gz" "GeoIPASNum.dat.gz"
-    "1f3jrvh2ivwgvx52r2n62dhyr0gb418q3gzra2108ygdxcm0105h";
+    "02vd80bg1hz458i7zfhvr50za1m97l6cqnwsabbby4v09pnlh50v";
   srcGeoIPASNumv6 = fetchDB
     "asnum/GeoIPASNumv6.dat.gz" "GeoIPASNumv6.dat.gz"
-    "16kvbv63l7cn7032b3gvpzjcb5w9fliyrwirn8sxyad8gywkxznw";
+    "1gs79avdx30is31mki82nz66l553ksaiwqcdbrd770ayy85v9pnj";
 
   meta = with stdenv.lib; {
     inherit version;
