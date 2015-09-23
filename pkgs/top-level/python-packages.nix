8ea4686c187278f92f4a3192e683bb882bcbf904
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-09-23 11:58:14 +0200

    pythonPackages.PyXAPI: init at 0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12411,6 +12411,43 @@ let
     };
   });
 
+  PyXAPI = stdenv.mkDerivation rec {
+    name = "PyXAPI-0.1";
+
+    src = pkgs.fetchurl {
+      url = "http://www.pps.univ-paris-diderot.fr/~ylg/PyXAPI/${name}.tar.gz";
+      sha256 = "19lblwfq24bgsgfy7hhqkxdf4bxl40chcxdlpma7a0wfa0ngbn26";
+    };
+
+    buildInputs = [ self.python ];
+
+    installPhase = ''
+      mkdir -p "$out/lib/${python.libPrefix}/site-packages"
+
+      export PYTHONPATH="$out/lib/${python.libPrefix}/site-packages:$PYTHONPATH"
+
+      ${python}/bin/${python.executable} setup.py install \
+        --install-lib=$out/lib/${python.libPrefix}/site-packages \
+        --prefix="$out"
+    '';
+
+    meta = with stdenv.lib; {
+      description = "Python socket module extension & RFC3542 IPv6 Advanced Sockets API";
+      longDescription = ''
+        PyXAPI consists of two modules: `socket_ext' and `rfc3542'.
+        `socket_ext' extends the Python module `socket'. `socket' objects have
+        two new methods: `recvmsg' and `sendmsg'. It defines `ancillary data'
+        objects and some functions related to. `socket_ext' module also provides
+        functions to manage interfaces indexes defined in RFC3494 and not
+        available from standard Python module `socket'.
+        `rfc3542' is a full implementation of RFC3542 (Advanced Sockets
+        Application Program Interface (API) for IPv6).
+      '';
+      homepage = http://www.pps.univ-paris-diderot.fr/~ylg/PyXAPI/;
+      license = licenses.gpl2Plus;
+      maintainers = with maintainers; [ nckx ];
+    };
+  };
 
   pyxattr = buildPythonPackage (rec {
     name = "pyxattr-0.5.1";
