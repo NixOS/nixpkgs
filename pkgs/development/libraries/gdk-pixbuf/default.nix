23f75e9bb05454d0af44fc823a19c57eafd967e3
Vladimír Čunát <vcunat@gmail.com>
2015-09-23 12:15:05 +0200

    gdk-pixbuf: maintenance update 2.31.7 -> 2.32.0

--- pkgs/development/libraries/gdk-pixbuf/default.nix
+++ pkgs/development/libraries/gdk-pixbuf/default.nix
@@ -2,15 +2,15 @@
 , jasper, libintlOrEmpty, gobjectIntrospection, doCheck ? false }:
 
 let
-  ver_maj = "2.31";
-  ver_min = "7";
+  ver_maj = "2.32";
+  ver_min = "0";
 in
 stdenv.mkDerivation rec {
   name = "gdk-pixbuf-${ver_maj}.${ver_min}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gdk-pixbuf/${ver_maj}/${name}.tar.xz";
-    sha256 = "046n9wz16kh3rpzxaiiwphf5bihxgj4g14cjn65wwmw82q4y0dj7";
+    sha256 = "0rqvj5gcs2zfyyg9llm289b0xkj4mrhzxfjpjja0wx1m6vn5axjk";
   };
 
   setupHook = ./setup-hook.sh;
