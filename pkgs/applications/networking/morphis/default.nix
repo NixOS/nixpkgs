f90fcf75f88dc8ea906e1f0a3bd68fbad085cef2
Florian Paul Schmidt <mista.tapas@gmx.net>
2015-09-23 16:08:54 +0200

    add initial morphis package nix

---
+++ pkgs/applications/networking/morphis/default.nix
@@ -0,0 +1,27 @@
+{ lib, pythonPackages, fetchurl }:
+
+pythonPackages.buildPythonPackage rec {
+  name = "morphis-${version}";
+  version = "0.8";
+  namePrefix = "";
+
+  src = fetchurl {
+    url = "https://morph.is/v${version}/morphis-${version}.tar.xz";
+    sha256 = "1wpxh5fhj8nx4yx4cvmc087cnf4iqwxf7zd7rdh2ln3pgxrjfral";
+  };
+
+  buildInputs = [ pythonPackages.pbr ];
+
+  propagatedBuildInputs = with pythonPackages; [
+      pycrypto
+      sqlalchemy
+    ];
+
+  meta = {
+    homepage = http://morph.is;
+    description = "A distributed datastore";
+    maintainers = [ lib.maintainers.madjar ];
+    platforms = lib.platforms.all;
+  };
+}
+
