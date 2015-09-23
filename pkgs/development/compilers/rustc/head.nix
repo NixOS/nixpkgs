de2906e6706c6c342f016906865026cb443d96a1
William A. Kennington III <william@wkennington.com>
2015-09-22 21:18:07 -0700

    rustc: Update both versions of the compiler
      rustc: 1.2.0 -> 1.3.0
      rustcMaster: 2015-09-05 -> 2015-09-21
    
    This also removes the llvm bundling which reduced immediate the closure size
    by ~50MB. It also tries to reduce some of the superfluous dependencies
    to help reduce the number of potential rebuilds (namely removing git).

--- pkgs/development/compilers/rustc/head.nix
+++ pkgs/development/compilers/rustc/head.nix
@@ -1,18 +1,27 @@
 # Please make sure to check if rustfmt still builds when updating nightly
-
 { stdenv, callPackage }:
+
 callPackage ./generic.nix {
-  shortVersion = "2015-09-05";
+  shortVersion = "2015-09-21";
   isRelease = false;
-  # src rev for 2015-09-05's nightly channel
-  srcRev = "779b2a9847319106647dcad12fc6dc472bc0cf4d";
-  srcSha = "0m22lxpcjnwa68bpxhfvp07k52gyds8ykif2pf5r2x22lw28vbg3";
+  srcRev = "547fd5c11e6902e2f9748e7b46893211b36da421";
+  srcSha = "10wklbxz6f4ga9q2rxghih18xdw6xm01bay6c13y72c4bwlnkm2w";
+
+  /* Rust is bootstrapped from an earlier built version. We need
+  to fetch these earlier versions, which vary per platform.
+  The shapshot info you want can be found at
+  https://github.com/rust-lang/rust/blob/{$shortVersion}/src/snapshots.txt
+  with the set you want at the top.
+  */
+
   snapshotHashLinux686 = "e2553bf399cd134a08ef3511a0a6ab0d7a667216";
   snapshotHashLinux64 = "7df8ba9dec63ec77b857066109d4b6250f3d222f";
   snapshotHashDarwin686 = "29750870c82a0347f8b8b735a4e2e0da26f5098d";
   snapshotHashDarwin64 = "c9f2c588238b4c6998190c3abeb33fd6164099a2";
   snapshotDate = "2015-08-11";
   snapshotRev = "1af31d4";
-  patches = stdenv.lib.optional stdenv.needsPax ./patches/grsec.patch;
+
+  patches = [ ./patches/remove-uneeded-git.patch ]
+    ++ stdenv.lib.optional stdenv.needsPax ./patches/grsec-head.patch;
 }
 
