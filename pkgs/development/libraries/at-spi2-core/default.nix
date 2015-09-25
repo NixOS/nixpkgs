8b3e6b5f62f0a6419e782187943d11fdc417409b
Luca Bruno <lethalman88@gmail.com>
2015-09-25 10:31:59 +0200

    at-spi2-core: 2.16.0 -> 2.18.0

--- pkgs/development/libraries/at-spi2-core/default.nix
+++ pkgs/development/libraries/at-spi2-core/default.nix
@@ -2,14 +2,14 @@
 , libX11, xextproto, libSM, libICE, libXtst, libXi, gobjectIntrospection }:
 
 stdenv.mkDerivation rec {
-  versionMajor = "2.16";
+  versionMajor = "2.18";
   versionMinor = "0";
   moduleName   = "at-spi2-core";
   name = "${moduleName}-${versionMajor}.${versionMinor}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${moduleName}/${versionMajor}/${name}.tar.xz";
-    sha256 = "1l3l39mw23zyjlcqidvkyqlr4gwbhplzw2hcv3qvn6p8ikxpf2qw";
+    sha256 = "1aeec77db6eb8087049af39a07f55756c55319f739d2998030fe6f4ced03ca76";
   };
 
   outputs = [ "out" "doc" ];
