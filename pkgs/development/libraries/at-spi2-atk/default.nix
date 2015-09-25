0688a56a49a548960e948a1ce54b80a7ea9c6fef
Luca Bruno <lethalman88@gmail.com>
2015-09-25 10:32:15 +0200

    at-spi2-atk: 2.16.0 -> 2.18.0

--- pkgs/development/libraries/at-spi2-atk/default.nix
+++ pkgs/development/libraries/at-spi2-atk/default.nix
@@ -2,14 +2,14 @@
 , intltool, dbus_glib, at_spi2_core, libSM }:
 
 stdenv.mkDerivation rec {
-  versionMajor = "2.16";
+  versionMajor = "2.18";
   versionMinor = "0";
   moduleName   = "at-spi2-atk";
   name = "${moduleName}-${versionMajor}.${versionMinor}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${moduleName}/${versionMajor}/${name}.tar.xz";
-    sha256 = "1y9gfz1iz3wpja7s000f0bmyyvc6im5fcdl6bxwbz0v3qdgc9vvq";
+    sha256 = "4a6db33453b6efd15fa7d84ef2a3421262a053f57f1df6e7a2536d02bacdf375";
   };
 
   buildInputs = [ python pkgconfig popt atk libX11 libICE xlibs.libXtst libXi
