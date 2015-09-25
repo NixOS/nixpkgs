48ac26f51250c97fda720520d96505afa9dc53c8
Luca Bruno <lethalman88@gmail.com>
2015-09-25 10:33:07 +0200

    gtk+: 3.16.6 -> 3.18.0

--- pkgs/development/libraries/gtk+/3.x.nix
+++ pkgs/development/libraries/gtk+/3.x.nix
@@ -9,8 +9,8 @@ assert xineramaSupport -> xlibs.libXinerama != null;
 assert cupsSupport -> cups != null;
 
 let
-  ver_maj = "3.16";
-  ver_min = "6";
+  ver_maj = "3.18";
+  ver_min = "0";
   version = "${ver_maj}.${ver_min}";
 in
 stdenv.mkDerivation rec {
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/gtk+/${ver_maj}/gtk+-${version}.tar.xz";
-    sha256 = "1gpzlnfrifc17yfk0zki6b2vmsfpf5cmrbh232s6iaan11np44jd";
+    sha256 = "7fb8ae257403317d3852bad28d064d35f67e978b1fed8b71d5997e87204271b9";
   };
 
   nativeBuildInputs = [ pkgconfig gettext gobjectIntrospection perl ];
