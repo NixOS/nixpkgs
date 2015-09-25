d1c21d3529966e469b4a4a1b85f597d8054bca68
Florian Paul Schmidt <mista.tapas@gmx.net>
2015-09-25 11:21:30 +0200

    fix the package a little

--- pkgs/applications/networking/morphis/default.nix
+++ pkgs/applications/networking/morphis/default.nix
@@ -1,32 +1,31 @@
 { lib, fetchFromGitHub, pythonPackages, fetchurl }:
 
 pythonPackages.buildPythonPackage rec {
-  name = "morphis-${version}";
-  version = "0.8";
-  namePrefix = "";
+    name = "morphis-${version}";
+    version = "0.8";
+    namePrefix = "";
 
-  enableParallelBuilding = true;
+    enableParallelBuilding = true;
 
-  src = fetchFromGitHub {
-    owner = "fps";
-    repo = "morphis-mirror";
-    rev = "88259977d63492475cf28f0305ecaa508f4cefaa";
-    sha256 = "15c8wpwz877y5gnqghwiq0cc544mshw8ipwla47i9nk9nmkwq0jr";
-  };
+    src = fetchFromGitHub {
+        owner = "fps";
+        repo = "morphis-mirror";
+        rev = "135ac9d8101d03c83a90e6b635640c3db68190c6";
+        sha256 = "15rhjx3zlxdpyc82gpkbp0dsafcf6p734q61nywj6yx68i8jsnjn";
+    };
 
-  buildInputs = [ pythonPackages.pbr ];
+    buildInputs = [ pythonPackages.pbr ];
 
-  propagatedBuildInputs = with pythonPackages; [
-      pycrypto
-      sqlalchemy9
-      cython
+    propagatedBuildInputs = with pythonPackages; [
+        pycrypto
+        sqlalchemy9
     ];
 
-  meta = {
-    homepage = http://morph.is;
-    description = "A distributed datastore";
-    maintainers = [ "Florian Paul Schmidt <mista.tapas@gmx.net>" ];
-    platforms = lib.platforms.all;
-  };
+    meta = {
+        homepage = http://morph.is;
+        description = "A distributed datastore";
+        maintainers = [ "Florian Paul Schmidt <mista.tapas@gmx.net>" ];
+        platforms = lib.platforms.all;
+    };
 }
 
