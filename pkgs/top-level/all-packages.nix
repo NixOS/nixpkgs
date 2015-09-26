9cf6feceb5599290b21443b8dfb2b5dc497665d8
Adam Russell <adamlr6+pub@gmail.com>
2015-09-26 18:13:06 -0500

    dwarf-fortress: add dfhack

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13713,6 +13713,7 @@ let
     SDL_image = pkgsi686Linux.SDL_image.override {
       libpng = pkgsi686Linux.libpng12;
     };
+    enableDFHack = config.dwarfFortress.enableDFHack or false;
   };
 
   dwarf-therapist = callPackage ../games/dwarf-therapist { };
