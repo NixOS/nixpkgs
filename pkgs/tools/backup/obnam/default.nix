a97cc77aa9141bf45f4ce052144d645c7d93e00e
Maciek Starzyk <mstarzyk@gmail.com>
2015-09-28 22:10:45 +0200

    obnam: 1.16 -> 1.17

--- pkgs/tools/backup/obnam/default.nix
+++ pkgs/tools/backup/obnam/default.nix
@@ -2,13 +2,13 @@
 
 pythonPackages.buildPythonPackage rec {
   name = "obnam-${version}";
-  version = "1.16";
+  version = "1.17";
 
   namePrefix = "";
 
   src = fetchurl rec {
     url = "http://code.liw.fi/debian/pool/main/o/obnam/obnam_${version}.orig.tar.xz";
-    sha256 = "0fv9pxilg0mjs6pw80c3sm23p5smpapyc49zgcm839qarl90fqz5";
+    sha256 = "01h1la07ra9v5zaqbs1qzsri2pdc9qrk2pz8y07msqg850w59k3v";
   };
 
   buildInputs = [ pythonPackages.sphinx attr ];
