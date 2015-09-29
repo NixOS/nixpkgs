d53cb314264083d19cc60c053f2237c9f9ef3870
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-09-29 03:28:54 +0200

    mcelog: 126 -> 127

--- pkgs/os-specific/linux/mcelog/default.nix
+++ pkgs/os-specific/linux/mcelog/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchFromGitHub }:
 
-let version = "126"; in
+let version = "127"; in
 stdenv.mkDerivation {
   name = "mcelog-${version}";
 
   src = fetchFromGitHub {
-    sha256 = "13vf3qrar9j4pp6zpspgsff0kbp7zj21mq33ywqa7ljq4v3szi8x";
+    sha256 = "0ap00f283d1hhv0f6l2fwsbq7sd6b96lf3jwg5cyam03pj2l8qk1";
     rev = "v${version}";
     repo = "mcelog";
     owner = "andikleen";
