9c1fd838a748a62e8b6da85c7fe03dbafab77901
Luca Bruno <lethalman88@gmail.com>
2015-10-02 13:45:59 +0200

    vde2: compile with 64-bit inode support

--- pkgs/tools/networking/vde2/default.nix
+++ pkgs/tools/networking/vde2/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, openssl, libpcap, python }:
+{ stdenv, fetchurl, openssl, libpcap, python, autoreconfHook }:
 
 stdenv.mkDerivation rec {
   name = "vde2-2.3.2";
@@ -8,7 +8,24 @@ stdenv.mkDerivation rec {
     sha256 = "14xga0ib6p1wrv3hkl4sa89yzjxv7f1vfqaxsch87j6scdm59pr2";
   };
 
-  buildInputs = [ openssl libpcap python ];
+  buildInputs = [ openssl libpcap python autoreconfHook ];
+
+  # Fixes:
+  # Cannot resolve ctl dir path '...': Value too large for defined data type
+  # Do not apply to vdetaplib
+
+  postPatch = ''
+    for f in $(find . -name Makefile.am); do
+      if echo $f|grep -q vdetaplib; then
+        continue
+      fi
+
+      sed \
+        -e 's/AM_CPPFLAGS =/AM_CPPFLAGS = -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE/' \
+        -e 's/AM_CFLAGS =/AM_CFLAGS = -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE/' \
+        -i $f
+    done
+  '';
 
   meta = {
     homepage = http://vde.sourceforge.net/;
