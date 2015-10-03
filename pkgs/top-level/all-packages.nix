2323d70d88aab404b9ce00af6c3ab4f5f6da2fa9
Vladimír Čunát <vcunat@gmail.com>
2015-10-03 21:06:37 +0200

    all-packages: revert accidentally committed change

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3842,7 +3842,7 @@ let
     inherit noSysDirs;
 
     # PGO seems to speed up compilation by gcc by ~10%, see #445 discussion
-    profiledCompiler = false; #for now. with stdenv; (!isDarwin && (isi686 || isx86_64));
+    profiledCompiler = with stdenv; (!isDarwin && (isi686 || isx86_64));
 
     # When building `gcc.crossDrv' (a "Canadian cross", with host == target
     # and host != build), `cross' must be null but the cross-libc must still
