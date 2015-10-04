40e4ab37cfd2d80e8ac482a7a2f933f9ea76de4a
Vladimír Čunát <vcunat@gmail.com>
2015-10-04 11:47:01 +0200

    wildmidi: fix build

--- pkgs/development/libraries/wildmidi/default.nix
+++ pkgs/development/libraries/wildmidi/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ cmake ];
 
-  buildInputs = [ alsaLib ];
+  buildInputs = [ alsaLib stdenv.cc.libc/*couldn't find libm*/ ];
 
   preConfigure = ''
     substituteInPlace CMakeLists.txt \
