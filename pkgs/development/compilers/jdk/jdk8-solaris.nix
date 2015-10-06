357c4ded802b2ac4f20d5e1b280bbc91be0deb3c
Danny Wilson <danny@prime.vc>
2015-10-07 00:52:02 +0200

    Update JDK 8 to update 66 build 02

--- pkgs/development/compilers/jdk/jdk8-solaris.nix
+++ pkgs/development/compilers/jdk/jdk8-solaris.nix
@@ -5,14 +5,14 @@
 
 let
 
-  update = "40";
+  update = "66";
 
-  build = "17";
+  build = "02";
 
   # Find new versions at: https://jdk8.java.net/download.html
   x64 = fetchurl {
-    url  = http://www.java.net/download/jdk8u40/archive/b17/binaries/jdk-8u40-ea-bin-b17-solaris-x64-02_dec_2014.tar.gz;
-    md5  = "ee360796cf6562e5d65d5750044d9420";
+    url  = http://www.java.net/download/jdk8u66/archive/b02/binaries/jdk-8u66-ea-bin-b02-solaris-x64-28_jul_2015.tar.gz;
+    md5  = "823e1d184da9021bf1a4704c0b66499b";
   };
 
 in
