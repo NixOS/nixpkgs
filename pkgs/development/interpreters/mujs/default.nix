100904a39ce18e720fb2f630226f27b7922ccc3b
Pascal Wittmann <mail@pascal-wittmann.de>
2015-10-06 16:28:28 +0200

    musj: 2015-09-15 -> 2015-09-29

--- pkgs/development/interpreters/mujs/default.nix
+++ pkgs/development/interpreters/mujs/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchgit, clang }:
 
 stdenv.mkDerivation rec {
-  name = "mujs-2015-09-15";
+  name = "mujs-2015-09-29";
 
   src = fetchgit {
     url = git://git.ghostscript.com/mujs.git;
-    rev  = "17019d29e5494d4b0ae148a3043a940be78e3215";
-    sha256 = "07192f4va733dr3v4ywfaqhz21iyydjwm84ij7zafwjvfi5z2b38";
+    rev  = "08276111f575ac6142e922d62aa264dc1f30b69e";
+    sha256 = "18w7yayrn5p8amack4p23wcz49x9cjh1pmzalrf16fhy3n753hbb";
   };
 
   buildInputs = [ clang ];
