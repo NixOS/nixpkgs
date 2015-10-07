d49a6d47afa54545d2554fa876f3f48a398170ba
Thomas Tuegel <ttuegel@gmail.com>
2015-10-07 06:28:45 -0500

    kdeApps_latest: kdeApps_15_04 -> kdeApps_15_08

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12045,8 +12045,8 @@ let
 
   kdeApps_15_04 = recurseIntoAttrs (callPackage ../applications/kde-apps-15.04 {});
   kdeApps_stable = kdeApps_15_04;
-  kdeApps_latest = kdeApps_15_04;
   kdeApps_15_08 = recurseIntoAttrs (import ../applications/kde-apps-15.08 { inherit pkgs; });
+  kdeApps_latest = kdeApps_15_08;
 
   keepnote = callPackage ../applications/office/keepnote {
     pygtk = pyGtkGlade;
