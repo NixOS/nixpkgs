1c8c5a12787024f8ec2693d5ffb3cdbfc05196f6
Thomas Tuegel <ttuegel@gmail.com>
2015-10-09 18:21:07 -0500

    kf514.extra-cmake-modules: add makeKDEWrapper

--- pkgs/development/libraries/kde-frameworks-5.14/extra-cmake-modules/setup-hook.sh
+++ pkgs/development/libraries/kde-frameworks-5.14/extra-cmake-modules/setup-hook.sh
@@ -1,4 +1,15 @@
+makeKDEWrapper() {
+    KSERVICE_BUILD_KDESYCOCA=${KDESYCOCA:+1}
+    local old="$1"
+    local new="$2"
+    shift
+    shift
+    makeQtWrapper "$old" "$new" ${KDESYCOCA:+--set KDESYCOCA "$KDESYCOCA"} "$@"
+}
+
 wrapKDEProgram() {
     KSERVICE_BUILD_KDESYCOCA=${KDESYCOCA:+1}
-    wrapQtProgram "$1" ${KDESYCOCA:+--set KDESYCOCA "$KDESYCOCA"} "$@"
+    local prog="$1"
+    shift
+    wrapQtProgram "$prog" ${KDESYCOCA:+--set KDESYCOCA "$KDESYCOCA"} "$@"
 }
