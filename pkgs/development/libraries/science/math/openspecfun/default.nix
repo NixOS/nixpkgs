cd2c0e4e86d3eddeb701ee08ea5794517f5ff369
Thomas Tuegel <ttuegel@gmail.com>
2015-10-10 12:55:30 -0500

    openspecfun: init at 0.4

---
+++ pkgs/development/libraries/science/math/openspecfun/default.nix
@@ -0,0 +1,21 @@
+{ stdenv, fetchurl, gfortran }:
+
+stdenv.mkDerivation {
+  name = "openspecfun-0.4";
+  src = fetchurl {
+    url = "https://github.com/JuliaLang/openspecfun/archive/v0.4.tar.gz";
+    sha256 = "0nsa3jjmlhcqkw5ba5ypbn3n0c8b6lc22zzlxnmxkxi9shhdx65z";
+  };
+
+  makeFlags = [ "prefix=$(out)" ];
+
+  nativeBuildInputs = [ gfortran ];
+
+  meta = {
+    description = "A collection of special mathematical functions";
+    homepage = "https://github.com/JuliaLang/openspecfun";
+    license = stdenv.lib.licenses.mit;
+    maintainers = [ stdenv.lib.maintainers.ttuegel ];
+    platforms = stdenv.lib.platforms.all;
+  };
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14271,6 +14271,8 @@ let
 
   openlibm = callPackage ../development/libraries/science/math/openlibm {};
 
+  openspecfun = callPackage ../development/libraries/science/math/openspecfun {};
+
   mathematica = callPackage ../applications/science/math/mathematica { };
   mathematica9 = callPackage ../applications/science/math/mathematica/9.nix { };
 
