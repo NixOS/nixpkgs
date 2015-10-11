a1da594888a9004253ae4669cbd2a10336b188de
Thomas Tuegel <ttuegel@gmail.com>
2015-10-11 10:17:00 -0500

    propagate libkscreen dependencies

--- pkgs/desktops/plasma-5.4/plasma-workspace/default.nix
+++ pkgs/desktops/plasma-5.4/plasma-workspace/default.nix
@@ -19,12 +19,13 @@ plasmaPackage {
   buildInputs = [
     kcmutils kcrash kdbusaddons kdesu kdewebkit kjsembed knewstuff
     knotifyconfig kpackage ktextwidgets kwallet kwayland kxmlrpcclient
-    libdbusmenu libkscreen libSM libXcursor networkmanager-qt pam
-    phonon qtquick1 qtscript qtx11extras solid wayland
+    libdbusmenu libSM libXcursor networkmanager-qt pam phonon qtquick1
+    qtscript qtx11extras solid wayland
   ];
   propagatedBuildInputs = [
     baloo kactivities kdeclarative kdelibs4support kglobalaccel
-    kidletime krunner ktexteditor kwin libksysguard plasma-framework
+    kidletime krunner ktexteditor kwin libkscreen libksysguard
+    plasma-framework
   ];
   patches = [ ./0001-startkde-NixOS-patches.patch ];
 
