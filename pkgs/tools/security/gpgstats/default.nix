bb42213ef2c2670cef85a19329b235e0ddf941b5
Pascal Wittmann <PascalWittmann@gmx.net>
2015-10-11 22:09:23 +0200

    gpgstats: make maintainers a list

--- pkgs/tools/security/gpgstats/default.nix
+++ pkgs/tools/security/gpgstats/default.nix
@@ -23,7 +23,7 @@ stdenv.mkDerivation rec {
     '';
     homepage = http://www.vanheusden.com/gpgstats/;
     license = licenses.gpl2;
-    maintainers = with maintainers; davidak;
+    maintainers = with maintainers; [ davidak ];
     platforms = with platforms; unix;
   };
 }
