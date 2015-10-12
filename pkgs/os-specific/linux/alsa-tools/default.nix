4e96eaee6a1a0b4e37d508893f4ef72a7264c249
Florian Paul Schmidt <mista.tapas@gmx.net>
2015-10-12 22:41:39 +0200

    alsa-tools: add initial default.nix

---
+++ pkgs/os-specific/linux/alsa-tools/default.nix
@@ -0,0 +1,33 @@
+{stdenv, fetchurl, alsaLib, gettext, ncurses, libsamplerate}:
+
+stdenv.mkDerivation rec {
+  name = "alsa-tools-${version}";
+  version = "1.0.29";
+
+  src = fetchurl {
+    urls = [
+      "ftp://ftp.alsa-project.org/pub/tools/${name}.tar.bz2"
+      "http://alsa.cybermirror.org/tools/${name}.tar.bz2"
+    ];
+    sha256 = "16ryhgbapp4pxyvsjc258mcj14wk7x3xs6g9bpnkqj0l7s7haq2i";
+  };
+
+  buildInputs = [ gettext alsaLib ncurses libsamplerate ];
+
+  # configureFlags = "--disable-xmlto --with-udev-rules-dir=$(out)/lib/udev/rules.d";
+
+  # installFlags = "ASOUND_STATE_DIR=$(TMPDIR)/dummy";
+
+  meta = {
+    homepage = http://www.alsa-project.org/;
+    description = "ALSA, the Advanced Linux Sound Architecture utils";
+
+    longDescription = ''
+      The Advanced Linux Sound Architecture (ALSA) provides audio and
+      MIDI functionality to the Linux-based operating system.
+    '';
+
+    platforms = stdenv.lib.platforms.linux;
+    maintainers = [ stdenv.lib.maintainers.AndersonTorres ];
+  };
+}
