30a476c311f88efb570db6d1333b91f6464f206a
Thomas Tuegel <ttuegel@gmail.com>
2015-10-14 06:48:09 -0500

    kconfigwidgets: update patches

--- pkgs/development/libraries/kde-frameworks-5.15/kconfigwidgets/default.nix
+++ pkgs/development/libraries/kde-frameworks-5.15/kconfigwidgets/default.nix
@@ -7,7 +7,7 @@ kdeFramework {
   nativeBuildInputs = [ extra-cmake-modules kdoctools ];
   buildInputs = [ kguiaddons ];
   propagatedBuildInputs = [ kauth kconfig kcodecs ki18n kwidgetsaddons ];
-  patches = [ ./kconfigwidgets-helpclient-follow-symlinks.patch ];
+  patches = [ ./0001-qdiriterator-follow-symlinks.patch ];
   postInstall = ''
     wrapKDEProgram "$out/bin/preparetips5"
   '';
