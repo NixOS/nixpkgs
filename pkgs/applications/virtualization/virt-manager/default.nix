NixOS/nixpkgs

commit 556151911edca15737ca4b7fa6199a2b960f924e

virt-manager: Fixup path to libvirt's cpu_map.xml

pkgs/applications/virtualization/virt-manager/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, pythonPackages, intltool, libxml2Python, curl, python
 , wrapGAppsHook, virtinst, pyGtkGlade, pythonDBus, gnome_python, gtkvnc, vte
 , gtk3, gobjectIntrospection, libvirt-glib, gsettings_desktop_schemas, glib
-, avahi, dconf, spiceSupport ? true, spice_gtk, libosinfo, gnome3
+, avahi, dconf, spiceSupport ? true, spice_gtk, libosinfo, gnome3, system-libvirt
 }:
 
 with stdenv.lib;
@@ -22,7 +22,7 @@ buildPythonPackage rec {
       paste_deploy m2crypto ipy twisted sqlalchemy_migrate
       distutils_extra simplejson readline glance cheetah lockfile httplib2
       urlgrabber virtinst pyGtkGlade pythonDBus gnome_python pygobject3
-      libvirt libxml2Python ipaddr vte libosinfo
+      libxml2Python ipaddr vte libosinfo
     ] ++ optional spiceSupport spice_gtk;
 
   buildInputs =
@@ -39,6 +39,10 @@ buildPythonPackage rec {
       wrapGAppsHook
       dconf
     ];
+
+  patchPhase = ''
+    sed -i 's|/usr/share/libvirt/cpu_map.xml|${system-libvirt}/share/libvirt/cpu_map.xml|g' virtinst/capabilities.py
+  '';
 
   configurePhase = ''
     sed -i 's/from distutils.core/from setuptools/g' setup.py

pkgs/top-level/all-packages.nix
@@ -13086,6 +13086,7 @@ let
     dconf = gnome3.dconf;
     gtkvnc = gtkvnc.override { enableGTK3 = true; };
     spice_gtk = spice_gtk.override { enableGTK3 = true; };
+    system-libvirt = libvirt;
   };
 
   virtinst = callPackage ../applications/virtualization/virtinst {};

commit 326ed47a9777d17fdaa9cd1e6621b73806e72f33

libvirt: Fix typo introduced by 556151911edca15737ca4b7fa6199a2b960f924e

pkgs/applications/virtualization/virt-manager/default.nix
@@ -22,7 +22,7 @@ buildPythonPackage rec {
       paste_deploy m2crypto ipy twisted sqlalchemy_migrate
       distutils_extra simplejson readline glance cheetah lockfile httplib2
       urlgrabber virtinst pyGtkGlade pythonDBus gnome_python pygobject3
-      libxml2Python ipaddr vte libosinfo
+      libvirt libxml2Python ipaddr vte libosinfo
     ] ++ optional spiceSupport spice_gtk;
 
   buildInputs =