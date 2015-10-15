219b9d5086678ac3b5332414f2153601937032f4
Venkateswara Rao Mandela <venkat.mandela@gmail.com>
2015-10-16 04:20:38 +0530

    ruby-zoom: init at 4.1.0
    From the website,
    
    https://gitlab.com/mjwhitta/zoom
    
    Zoom adds some convenience to ag/ack/grep by allowing you to quickly
    open your search results in your editor of choice.
    
    Also available as a ruby gem.
    
    https://rubygems.org/gems/ruby-zoom
    
    In addition to including the ruby-zoom tool, we also added a global
    override for the ruby-terminfo gem to handle the ncurses build time
    dependency.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10330,6 +10330,8 @@ in
 
   seyren = callPackage ../servers/monitoring/seyren { };
 
+  ruby-zoom = callPackage ../tools/text/ruby-zoom { };
+
   sensu = callPackage ../servers/monitoring/sensu {
     ruby = ruby_2_1;
   };
