2e5b6362ae299bf699e5b12f20e8952878b43d75
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-10-15 13:17:37 +0200

    Bump minimum required Nix version
    Issue #10337.

--- default.nix
+++ default.nix
@@ -1,6 +1,8 @@
-if ! builtins ? nixVersion || builtins.compareVersions "1.8" builtins.nixVersion == 1 then
+let requiredVersion = "1.10"; in
 
-  abort "This version of Nixpkgs requires Nix >= 1.8, please upgrade! See https://nixos.org/wiki/How_to_update_when_nix_is_too_old_to_evaluate_nixpkgs"
+if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
+
+  abort "This version of Nixpkgs requires Nix >= ${requiredVersion}, please upgrade! See https://nixos.org/wiki/How_to_update_when_Nix_is_too_old_to_evaluate_Nixpkgs"
 
 else
 
