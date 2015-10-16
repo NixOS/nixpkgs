8f967a3056f58739329e28f3612e695d3938e989
Nikolay Amiantov <ab@fmap.me>
2015-10-16 21:16:13 +0300

    nixos/stage-1: add postEarlyDeviceCommands hook

--- nixos/modules/system/boot/stage-1.nix
+++ nixos/modules/system/boot/stage-1.nix
@@ -203,7 +203,7 @@ let
     inherit (config.boot) resumeDevice devSize runSize;
 
     inherit (config.boot.initrd) checkJournalingFS
-      preLVMCommands postDeviceCommands postMountCommands kernelModules;
+      postEarlyDeviceCommands preLVMCommands postDeviceCommands postMountCommands kernelModules;
 
     resumeDevices = map (sd: if sd ? device then sd.device else "/dev/disk/by-label/${sd.label}")
                     (filter (sd: sd ? label || hasPrefix "/dev/" sd.device) config.swapDevices);
@@ -310,6 +310,14 @@ in
         Shell commands to be executed immediately after stage 1 of the
         boot has loaded kernel modules and created device nodes in
         <filename>/dev</filename>.
+      '';
+    };
+
+    boot.initrd.postEarlyDeviceCommands = mkOption {
+      default = "";
+      type = types.lines;
+      description = ''
+        Shell commands to be executed early after creation of device nodes.
       '';
     };
 
