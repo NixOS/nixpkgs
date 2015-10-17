1dc0ee9ba72a5e1f2fb1df552f252ddecc7bea36
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2015-10-17 23:58:01 +0300

    syslinux: Disable parallel building
    Unfortunately I lost the exact failure, but it was something starting
    with 'No rule to make target: ...', which suggests missing dependencies
    between generated source files.

--- pkgs/os-specific/linux/syslinux/default.nix
+++ pkgs/os-specific/linux/syslinux/default.nix
@@ -13,7 +13,7 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ nasm perl libuuid ];
 
-  enableParallelBuilding = true;
+  enableParallelBuilding = false; # Fails very rarely with 'No rule to make target: ...'
 
   preBuild = ''
     substituteInPlace Makefile --replace /bin/pwd $(type -P pwd)
