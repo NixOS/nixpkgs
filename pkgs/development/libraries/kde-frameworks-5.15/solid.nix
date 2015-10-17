b1569b30153ee38e663293ff49790c863a7b3e4b
Thomas Tuegel <ttuegel@gmail.com>
2015-10-16 19:02:04 -0500

    solid: depend on qtdeclarative

--- pkgs/development/libraries/kde-frameworks-5.15/solid.nix
+++ pkgs/development/libraries/kde-frameworks-5.15/solid.nix
@@ -1,11 +1,13 @@
 { kdeFramework, lib
 , extra-cmake-modules
 , makeKDEWrapper
+, qtdeclarative
 }:
 
 kdeFramework {
   name = "solid";
   nativeBuildInputs = [ extra-cmake-modules makeKDEWrapper ];
+  buildInputs = [ qtdeclarative ];
   postInstall = ''
     wrapKDEProgram "$out/bin/solid-hardware5"
   '';
