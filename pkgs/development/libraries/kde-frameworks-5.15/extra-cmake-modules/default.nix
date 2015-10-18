56fa8ba8ee2a6d5cfd80a181065ae57e51a15b31
Thomas Tuegel <ttuegel@gmail.com>
2015-10-18 11:57:34 -0500

    extra-cmake-modules: propagate MIME type providers

--- pkgs/development/libraries/kde-frameworks-5.15/extra-cmake-modules/default.nix
+++ pkgs/development/libraries/kde-frameworks-5.15/extra-cmake-modules/default.nix
@@ -4,6 +4,8 @@ kdeFramework {
   name = "extra-cmake-modules";
   patches = [ ./0001-extra-cmake-modules-paths.patch ];
 
+  setupHook = ./setup-hook.sh;
+
   # It is OK to propagate these inputs as long as
   # extra-cmake-modules is never a propagated input
   # of some other derivation.
