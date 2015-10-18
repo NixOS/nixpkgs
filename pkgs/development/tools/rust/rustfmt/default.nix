ef8b078ce77aede0014b6c7fe3ee9831274c1185
Ricardo M. Correia <rcorreia@wizy.org>
2015-10-18 03:36:15 +0200

    rustfmt: 2015-09-23 -> 2015-10-17
    Also convert from fetchgit to fetchFromGitHub.

--- pkgs/development/tools/rust/rustfmt/default.nix
+++ pkgs/development/tools/rust/rustfmt/default.nix
@@ -1,18 +1,17 @@
-{ stdenv, fetchgit, rustUnstable, makeWrapper, llvmPackages_37 }:
+{ stdenv, fetchFromGitHub, rustUnstable, makeWrapper }:
 
 with rustUnstable;
 
 buildRustPackage rec {
-  name = "rustfmt-git-2015-09-23";
-  src = fetchgit {
-    url = https://github.com/nrc/rustfmt;
-    rev = "c14cfca0e3de3dfa5fd91d39a85c5b452e7756e3";
-    sha256 = "0q72mfj2ph2n4cd0cs4p2mpyr2ixd6ss607kjlgfinjv6klk1i3b";
+  name = "rustfmt-git-2015-10-17";
+  src = fetchFromGitHub {
+    owner = "nrc";
+    repo = "rustfmt";
+    rev = "36c9a3acf95a036f3f9fa72ff3fe175fba55e20b";
+    sha256 = "1vjnfq3al73qljalr2rvymabcksx6y690gg5r9kgz1lnizlb7yrz";
   };
 
-  buildInputs = [ llvmPackages_37.llvm ];
-
-  depsSha256 = "13i9qaia1wn18lgfl69rrxw7b24bq1bpqhdck2jzxpv3wi2xshlw";
+  depsSha256 = "0vzpq58vfswdwdm2bk44ynk43cmwdxppcbkvpjyfa6sjs2s5x8n9";
 
   meta = with stdenv.lib; {
     description = "A tool for formatting Rust code according to style guidelines";
