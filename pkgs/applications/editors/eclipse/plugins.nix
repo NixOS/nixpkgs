0b70ac102900aee93f131d7475784bb4740aa900
Robert Helgesson <robert@rycee.net>
2015-10-18 18:19:55 +0200

    eclipse-plugin-testng: 6.9.7 -> 6.9.8
    (cherry picked from commit 3a6439f8d51e145cc37a6bd8fe3458be588dafe9)

--- pkgs/applications/editors/eclipse/plugins.nix
+++ pkgs/applications/editors/eclipse/plugins.nix
@@ -335,16 +335,16 @@ rec {
 
   testng = buildEclipsePlugin rec {
     name = "testng-${version}";
-    version = "6.9.7.201510070420";
+    version = "6.9.8.201510130443";
 
     srcFeature = fetchurl {
       url = "http://beust.com/eclipse/features/org.testng.eclipse_${version}.jar";
-      sha256 = "185m6zcz1havhl94qgwms9szcs7vhrrq8rckbrb1vz02208yyhpn";
+      sha256 = "0g0dva1zpqk0rz0vgr025g2cfc47snr857fsqcrssmp9qmy8x0i0";
     };
 
     srcPlugin = fetchurl {
       url = "http://beust.com/eclipse/plugins/org.testng.eclipse_${version}.jar";
-      sha256 = "1rw678cd9nm623dvaddncadaclif62zcb15jl7ryzcmck2y380l5";
+      sha256 = "16mnvqkakixqp3bcnyx6x2iwkhnv3k4q561c97kssz98xsrr8f54";
     };
 
     meta = with stdenv.lib; {
