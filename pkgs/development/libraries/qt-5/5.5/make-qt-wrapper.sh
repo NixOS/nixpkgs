5a2c45427b391a6ac4431db8cb49563d2dc624ca
Thomas Tuegel <ttuegel@gmail.com>
2015-10-18 09:25:50 -0500

    makeQtWrapper: always wrap impurely

--- pkgs/development/libraries/qt-5/5.5/make-qt-wrapper.sh
+++ pkgs/development/libraries/qt-5/5.5/make-qt-wrapper.sh
@@ -8,19 +8,11 @@ addQtDependency() {
 wrapQtProgram() {
     local prog="$1"
     shift
-    if [[ -n "$QT_WRAPPER_IMPURE" ]]; then
-        wrapProgram "$prog" \
-            --prefix QT_PLUGIN_PATH : "$QT_PLUGIN_PATH" \
-            --prefix QML_IMPORT_PATH : "$QML_IMPORT_PATH" \
-            --prefix QML2_IMPORT_PATH : "$QML2_IMPORT_PATH" \
-            "$@"
-    else
-        wrapProgram "$prog" \
-            --set QT_PLUGIN_PATH "$QT_PLUGIN_PATH" \
-            --set QML_IMPORT_PATH "$QML_IMPORT_PATH" \
-            --set QML2_IMPORT_PATH "$QML2_IMPORT_PATH" \
-            "$@"
-    fi
+    wrapProgram "$prog" \
+        --prefix QT_PLUGIN_PATH : "$QT_PLUGIN_PATH" \
+        --prefix QML_IMPORT_PATH : "$QML_IMPORT_PATH" \
+        --prefix QML2_IMPORT_PATH : "$QML2_IMPORT_PATH" \
+        "$@"
 }
 
 makeQtWrapper() {
@@ -28,19 +20,11 @@ makeQtWrapper() {
     local new="$2"
     shift
     shift
-    if [[ -n "$QT_WRAPPER_IMPURE" ]]; then
-        makeWrapper "$old" "$new" \
-            --prefix QT_PLUGIN_PATH : "$QT_PLUGIN_PATH" \
-            --prefix QML_IMPORT_PATH : "$QML_IMPORT_PATH" \
-            --prefix QML2_IMPORT_PATH : "$QML2_IMPORT_PATH" \
-            "$@"
-    else
-        makeWrapper "$old" "$new" \
-            --set QT_PLUGIN_PATH "$QT_PLUGIN_PATH" \
-            --set QML_IMPORT_PATH "$QML_IMPORT_PATH" \
-            --set QML2_IMPORT_PATH "$QML2_IMPORT_PATH" \
-            "$@"
-    fi
+    makeWrapper "$old" "$new" \
+        --prefix QT_PLUGIN_PATH : "$QT_PLUGIN_PATH" \
+        --prefix QML_IMPORT_PATH : "$QML_IMPORT_PATH" \
+        --prefix QML2_IMPORT_PATH : "$QML2_IMPORT_PATH" \
+        "$@"
 }
 
 # cannot use addToSearchPath because these directories may not exist yet
