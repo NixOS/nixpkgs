df22cfdb41acfd7c6af820539d864cc5f284753f
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-10-21 01:43:30 +0200

    gptfdisk 1.0.0 -> 1.0.1
    Changes: http://www.rodsbooks.com/gdisk/revisions.html

--- pkgs/tools/system/gptfdisk/default.nix
+++ pkgs/tools/system/gptfdisk/default.nix
@@ -1,11 +1,14 @@
 { fetchurl, stdenv, libuuid, popt, icu, ncurses }:
 
+let version = "1.0.1"; in
 stdenv.mkDerivation rec {
-  name = "gptfdisk-1.0.0";
+  name = "gptfdisk-${version}";
 
   src = fetchurl {
+    # http://www.rodsbooks.com/gdisk/${name}.tar.gz also works, but the home
+    # page clearly implies a preference for using SourceForge's bandwidth:
     url = "mirror://sourceforge/gptfdisk/${name}.tar.gz";
-    sha256 = "0v0xl0mzwabdf9yisgsvkhpyi48kbik35c6df42gr6d78dkrarjv";
+    sha256 = "1izazbyv5n2d81qdym77i8mg9m870hiydmq4d0s51npx5vp8lk46";
   };
 
   buildInputs = [ libuuid popt icu ncurses ];
@@ -20,13 +23,12 @@ stdenv.mkDerivation rec {
     done
   '';
 
-  meta = {
-    description = "A set of text-mode partitioning tools for Globally Unique Identifier (GUID) Partition Table (GPT) disks";
-
-    license = stdenv.lib.licenses.gpl2;
-
+  meta = with stdenv.lib; {
+    inherit version;
+    description = "Set of text-mode partitioning tools for Globally Unique Identifier (GUID) Partition Table (GPT) disks";
+    license = licenses.gpl2;
     homepage = http://www.rodsbooks.com/gdisk/;
-
-    platforms = stdenv.lib.platforms.linux;
+    maintainers = with maintainers; [ nckx ];
+    platforms = platforms.linux;
   };
 }
