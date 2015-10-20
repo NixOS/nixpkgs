a7f478a3685248ff4238991c8fededa63227d6af
Jude Taylor <me@jude.bio>
2015-10-20 15:53:21 -0700

    darwin: add needed inputs to python-3.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5075,6 +5075,7 @@ let
     self = python33;
   };
   python34 = hiPrio (callPackage ../development/interpreters/python/3.4 {
+    inherit (darwin) CF configd;
     self = python34;
   });
   python35 = hiPrio (callPackage ../development/interpreters/python/3.5 {
