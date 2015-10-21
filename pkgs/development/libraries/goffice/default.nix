f5c0d29c58997fba1b7af00d8fdb04d796c87dab
Vincent Laporte <Vincent.Laporte@gmail.com>
2015-10-21 04:38:13 +0200

    goffice: 0.10.22 -> 0.10.24

--- pkgs/development/libraries/goffice/default.nix
+++ pkgs/development/libraries/goffice/default.nix
@@ -2,11 +2,11 @@
 , libgsf, libxml2, libxslt, cairo, pango, librsvg, libspectre }:
 
 stdenv.mkDerivation rec {
-  name = "goffice-0.10.22";
+  name = "goffice-0.10.24";
 
   src = fetchurl {
     url = "mirror://gnome/sources/goffice/0.10/${name}.tar.xz";
-    sha256 = "0206a87a323b52a874dc54491374245f9e1c5f62e93a2ce4a02fb444a26b0e28";
+    sha256 = "cda70eab0b0b0e29c3bea09849bcfca0c2ccc20038ee69e7e14cde664484af5a";
   };
 
   nativeBuildInputs = [ pkgconfig intltool ];
