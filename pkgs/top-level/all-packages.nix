5e8fe86c5dfb9d75947056c74d94390476a081a3
Brian McKenna <bmckenna@atlassian.com>
2015-10-21 21:31:39 +1100

    pijul: init at 0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12315,6 +12315,10 @@ let
 
   pig = callPackage ../applications/networking/cluster/pig { };
 
+  pijul = callPackage ../applications/version-management/pijul {
+    inherit (ocamlPackages) findlib cryptokit yojson;
+  };
+
   playonlinux = callPackage ../applications/misc/playonlinux { };
 
   shotcut = callPackage ../applications/video/shotcut { mlt = mlt-qt5; };
