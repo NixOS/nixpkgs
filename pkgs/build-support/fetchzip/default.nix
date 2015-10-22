b31e78d41d3a5740648a6b95c9b4fa06bedbd6ee
Kirill Elagin <kirelagin@gmail.com>
2015-10-22 17:48:50 +0300

    fetchzip: respect the `name` argument
    Without this `fetchzip` breaks if `url` doesn’t end with `zip`,
    even if proper `name` was given.

--- pkgs/build-support/fetchzip/default.nix
+++ pkgs/build-support/fetchzip/default.nix
@@ -28,7 +28,7 @@ lib.overrideDerivation (fetchurl ({
       mkdir "$unpackDir"
       cd "$unpackDir"
 
-      renamed="$TMPDIR/${baseNameOf url}"
+      renamed="$TMPDIR/${baseNameOf name}"
       mv "$downloadedFile" "$renamed"
       unpackFile "$renamed"
 
