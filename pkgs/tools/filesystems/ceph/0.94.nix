8379d91e913438006e5020383814dc51e6168400
William A. Kennington III <william@wkennington.com>
2015-10-21 22:12:36 -0700

    ceph: 0.94.3.2 -> 0.94.4

--- pkgs/tools/filesystems/ceph/0.94.nix
+++ pkgs/tools/filesystems/ceph/0.94.nix
@@ -1,12 +1,12 @@
 { callPackage, fetchgit, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "0.94.3.2";
+  version = "0.94.4";
 
   src = fetchgit {
     url = "https://github.com/ceph/ceph.git";
     rev = "refs/tags/v${version}";
-    sha256 = "112yprdy39cmhfgh6pfx34rlw9sp83fgzqixvgpq34akpykhad8c";
+    sha256 = "0jp3c805bix88z3103kbrxv2yndpjcz3j5rp669f7qq46074zw6g";
   };
 
   patches = [ ./fix-pgrefdebugging.patch ];
