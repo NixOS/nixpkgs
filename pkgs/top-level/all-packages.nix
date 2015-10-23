0ebe4b615e47c9739adce956f5e3d202b67caf14
Spencer Janssen <spencerjanssen@gmail.com>
2015-10-23 13:58:42 -0500

    cockatrice: init at 2015-09-24

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13653,6 +13653,8 @@ let
 
   chessdb = callPackage ../games/chessdb { };
 
+  cockatrice = qt5Libs.callPackage ../games/cockatrice {  };
+
   confd = goPackages.confd.bin // { outputs = [ "bin" ]; };
 
   construoBase = lowPrio (callPackage ../games/construo {
