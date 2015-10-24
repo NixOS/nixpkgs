f414f23630168d5e6770e9c8781267a1fee2b95f
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2015-10-24 17:18:18 +0300

    ARM: Don't disable manual in installation images
    Since commits 89e9837 and 5b8dae8 the manual no longer depends on
    evaluation of any packages from nixpkgs, so all errors of the form
    "Package 'foo' is not supported on 'armv7l-linux'" are gone.

--- nixos/modules/installer/cd-dvd/sd-image-armv7l-multiplatform.nix
+++ nixos/modules/installer/cd-dvd/sd-image-armv7l-multiplatform.nix
@@ -26,9 +26,6 @@ in
   boot.kernelPackages = pkgs.linuxPackages_testing;
   boot.kernelParams = ["console=ttyS0,115200n8" "console=ttyAMA0,115200n8" "console=tty0"];
 
-  # FIXME: fix manual evaluation on ARM
-  services.nixosManual.enable = lib.mkOverride 0 false;
-
   # FIXME: this probably should be in installation-device.nix
   users.extraUsers.root.initialHashedPassword = "";
 
