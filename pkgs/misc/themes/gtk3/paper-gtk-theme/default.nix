3113af562785080d89805d67572d482dc14d75c4
Simon Vandel Sillesen <simon.vandel@gmail.com>
2015-10-24 19:01:57 +0200

    Make description conform to nixos standards

--- pkgs/misc/themes/gtk3/paper-gtk-theme/default.nix
+++ pkgs/misc/themes/gtk3/paper-gtk-theme/default.nix
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
   preferLocalBuild = true;
 
   meta = with stdenv.lib; {
-    description = "Paper is a modern desktop theme suite. Its design is mostly flat with a minimal use of shadows for depth.";
+    description = "A modern desktop theme suite featuring a mostly flat with a minimal use of shadows for depth";
     homepage = "http://snwh.org/paper/";
     license = licenses.gpl3;
     maintainers = [ maintainers.simonvandel ];
