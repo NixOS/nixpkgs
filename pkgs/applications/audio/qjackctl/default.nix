0cfd2267b0ef7acab24454b01cf7c5fa6747c74e
Cillian de Róiste <cillian.deroiste@gmail.com>
2015-10-25 18:55:36 +0100

    qjackctl: update from 0.3.12 to 0.4.0

--- pkgs/applications/audio/qjackctl/default.nix
+++ pkgs/applications/audio/qjackctl/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, qt4, alsaLib, libjack2, dbus }:
 
 stdenv.mkDerivation rec {
-  version = "0.3.12";
+  version = "0.4.0";
   name = "qjackctl-${version}";
 
   # some dependencies such as killall have to be installed additionally
 
   src = fetchurl {
     url = "mirror://sourceforge/qjackctl/${name}.tar.gz";
-    sha256 = "14yvnc4k3hwsjflg8b2d04bc63pdl0gyqjc7vl6rdn29nbr23zwc";
+    sha256 = "0nj8c8vy00524hbjqwsqkliblcf9j7h46adk6v5np645pp2iqrav";
   };
 
   buildInputs = [ qt4 alsaLib libjack2 dbus ];
