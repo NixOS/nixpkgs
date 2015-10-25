NixOS/nixpkgs

commit b21fd5d066baadb06ca8e9a2accfcb4e94c60a09

nixos/postgresql: Fix initdb for existing, empty postgres partitions

nixos/modules/services/databases/postgresql.nix
@@ -186,8 +186,9 @@ in
         preStart =
           ''
             # Initialise the database.
-            if ! test -e ${cfg.dataDir}; then
+            if ! test -e ${cfg.dataDir}/PG_VERSION; then
                 mkdir -m 0700 -p ${cfg.dataDir}
+                rm -f ${cfg.dataDir}/*.conf
                 if [ "$(id -u)" = 0 ]; then
                   chown -R postgres ${cfg.dataDir}
                   su -s ${pkgs.stdenv.shell} postgres -c 'initdb -U root'
@@ -195,8 +196,6 @@ in
                   # For non-root operation.
                   initdb
                 fi
-                rm -f ${cfg.dataDir}/*.conf
-                touch "${cfg.dataDir}/.first_startup"
             fi
 
             ln -sfn "${configFile}" "${cfg.dataDir}/postgresql.conf"

commit 60d407b2094cd718b86de1360c9a44d92638d182

nixos/postgresql: Fix execution of initialScript.
Regression introduced by b21fd5d066baadb06ca8e9a2accfcb4e94c60a09.

The initialScript is only executed whenever there is a .first-startup in
the dataDir, so silently dropping the file essentially breaks
initialScript functionality.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

nixos/modules/services/databases/postgresql.nix
@@ -202,6 +202,8 @@ in
                   # For non-root operation.
                   initdb
                 fi
+                # See postStart!
+                touch "${cfg.dataDir}/.first_startup"
             fi
 
             ln -sfn "${configFile}" "${cfg.dataDir}/postgresql.conf"