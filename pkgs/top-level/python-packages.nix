b42223f4dc9f53ab842db13ceecf5eed5bec5109
Frederik Rietdijk <fridh@fridh.nl>
2015-10-25 13:07:19 +0100

    python pkgconfig: init at 1.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10784,6 +10784,37 @@ let
     };
   };
 
+  pkgconfig = buildPythonPackage rec {
+    name = "pkgconfig-${version}";
+    version = "1.1.0";
+
+    # pypy: SyntaxError: __future__ statements must appear at beginning of file
+    disabled = isPyPy;
+
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/source/p/pkgconfig/${name}.tar.gz";
+      sha256 = "709daaf077aa2b33bedac12706373412c3683576a43013bbaa529fc2769d80df";
+    };
+
+    buildInputs = with self; [ nose ];
+
+    propagatedBuildInputs = with self; [pkgs.pkgconfig];
+
+    meta = {
+      description = "Interface Python with pkg-config";
+      homepage = http://github.com/matze/pkgconfig;
+      license = licenses.mit;
+    };
+
+    # nosetests needs to be run explicitly.
+    # Note that the distributed archive does not actually contain any tests.
+    # https://github.com/matze/pkgconfig/issues/9
+    checkPhase = ''
+      nosetests
+    '';
+
+  };
+
   plumbum = buildPythonPackage rec {
     name = "plumbum-1.5.0";
 
