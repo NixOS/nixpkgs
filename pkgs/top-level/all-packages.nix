07849565ed0956acb0040ff39bd670a4025d7114
Jude Taylor <me@jude.bio>
2015-10-28 12:34:57 -0700

    SDL2: add darwin frameworks

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8198,6 +8198,7 @@ let
     alsaSupport = stdenv.isLinux;
     x11Support = !stdenv.isCygwin;
     pulseaudioSupport = false; # better go through ALSA
+    inherit (darwin.apple_sdk.frameworks) AudioUnit Cocoa CoreAudio CoreServices ForceFeedback OpenGL;
   };
 
   SDL2_image = callPackage ../development/libraries/SDL2_image { };
