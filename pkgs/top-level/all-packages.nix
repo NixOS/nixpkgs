3136a17834acb451b44767a180fb5f1d6f640362
Ricardo M. Correia <rcorreia@wizy.org>
2015-10-28 15:20:32 +0100

    rr: 3.0.0 -> 4.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5779,7 +5779,9 @@ let
 
   rman = callPackage ../development/tools/misc/rman { };
 
-  rr = callPackage ../development/tools/analysis/rr { };
+  rr = callPackage ../development/tools/analysis/rr {
+    stdenv = stdenv_32bit;
+  };
 
   saleae-logic = callPackage ../development/tools/misc/saleae-logic { };
 
