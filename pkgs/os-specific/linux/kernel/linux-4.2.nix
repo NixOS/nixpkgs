22e313f37ffe5d9ef90cce272d1ef477276ab2ed
William A. Kennington III <william@wkennington.com>
2015-10-27 23:07:42 -0700

    kernel: 4.2.3 -> 4.2.5
    (cherry picked from commit 221a970e82979a4154f9f369f8684a3fe9e4666b)

--- pkgs/os-specific/linux/kernel/linux-4.2.nix
+++ pkgs/os-specific/linux/kernel/linux-4.2.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.2.3";
+  version = "4.2.5";
   extraMeta.branch = "4.2";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "4ca6c783a0bc87573f5c95e49306cbe5f83dc1cd5afb44ecc9a1917f39e5ad66";
+    sha256 = "11riafz0nljlgx8fpbfwlb5kb07naca2p07vk2wrcwyzil1vnhvw";
   };
 
   features.iwlwifi = true;
