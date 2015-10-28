b5824afa373426cdc1c810c05d682919a5421b8b
William A. Kennington III <william@wkennington.com>
2015-10-27 23:07:33 -0700

    kernel: 4.1.11 -> 4.1.12

--- pkgs/os-specific/linux/kernel/linux-4.1.nix
+++ pkgs/os-specific/linux/kernel/linux-4.1.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.1.11";
+  version = "4.1.12";
   extraMeta.branch = "4.1";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0xip3xj5aga4sij3rrcxv3h7b8qk47iy3x25vak66yjbdai9zbj1";
+    sha256 = "0q9zw731mn043fksl0cmbg8dr16f065378ys5x1h955rn93b4jab";
   };
 
   features.iwlwifi = true;
