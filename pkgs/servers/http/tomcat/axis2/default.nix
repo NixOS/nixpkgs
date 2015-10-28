497817c0d155acd76b6048861b0d0cc7a9beecf0
Sander van der Burg <svanderburg@gmail.com>
2015-10-28 10:24:29 +0000

    axis2: bump to version 1.6.3

--- pkgs/servers/http/tomcat/axis2/default.nix
+++ pkgs/servers/http/tomcat/axis2/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, apacheAnt, jdk, unzip}:
 
 stdenv.mkDerivation {
-  name = "axis2-1.6.2";
+  name = "axis2-1.6.3";
 
   src = fetchurl {
-    url = http://apache.proserve.nl/axis/axis2/java/core/1.6.2/axis2-1.6.2-bin.zip;
-    sha256 = "02i6fv11ksd5ql81i501bcb11ib5gyhq3zxwrz5jm4ic80r097fp";
+    url = http://apache.proserve.nl/axis/axis2/java/core/1.6.3/axis2-1.6.3-bin.zip;
+    sha256 = "0a49m7g1gxb904d0az2kbkab8rg02wm8nzbyipiad9k028masr6r";
   };
 
   buildInputs = [ unzip apacheAnt jdk ];
