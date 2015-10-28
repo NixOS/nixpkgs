46217b9b01eec726bc9361409834c60f464ee7ae
Thomas Tuegel <ttuegel@gmail.com>
2015-10-27 19:40:28 -0500

    makeQtWrapper: wrap XDG directories
    (cherry picked from commit 24babaad9ab3e157d1bbb97531536ab6741511de)

--- pkgs/development/libraries/qt-5/5.5/make-qt-wrapper.sh
+++ pkgs/development/libraries/qt-5/5.5/make-qt-wrapper.sh
@@ -2,6 +2,8 @@ addQtDependency() {
     addToSearchPath QT_PLUGIN_PATH "$1/lib/qt5/plugins"
     addToSearchPath QML_IMPORT_PATH "$1/lib/qt5/imports"
     addToSearchPath QML2_IMPORT_PATH "$1/lib/qt5/qml"
+    addToSearchPath XDG_CONFIG_DIRS "$1/etc/xdg"
+    addToSearchPath XDG_DATA_DIRS "$1/share"
 }
 
 wrapQtProgram() {
@@ -11,6 +13,8 @@ wrapQtProgram() {
         --prefix QT_PLUGIN_PATH : "$QT_PLUGIN_PATH" \
         --prefix QML_IMPORT_PATH : "$QML_IMPORT_PATH" \
         --prefix QML2_IMPORT_PATH : "$QML2_IMPORT_PATH" \
+        --prefix XDG_CONFIG_DIRS : "$XDG_CONFIG_DIRS" \
+        --prefix XDG_DATA_DIRS : "$XDG_DATA_DIRS" \
         "$@"
 }
 
@@ -23,6 +27,8 @@ makeQtWrapper() {
         --prefix QT_PLUGIN_PATH : "$QT_PLUGIN_PATH" \
         --prefix QML_IMPORT_PATH : "$QML_IMPORT_PATH" \
         --prefix QML2_IMPORT_PATH : "$QML2_IMPORT_PATH" \
+        --prefix XDG_CONFIG_DIRS : "$XDG_CONFIG_DIRS" \
+        --prefix XDG_DATA_DIRS : "$XDG_DATA_DIRS" \
         "$@"
 }
 
@@ -30,6 +36,7 @@ makeQtWrapper() {
 export QT_PLUGIN_PATH="$QT_PLUGIN_PATH${QT_PLUGIN_PATH:+:}$out/lib/qt5/plugins"
 export QML_IMPORT_PATH="$QML_IMPORT_PATH${QML_IMPORT_PATH:+:}$out/lib/qt5/imports"
 export QML2_IMPORT_PATH="$QML2_IMPORT_PATH${QML2_IMPORT_PATH:+:}$out/lib/qt5/qml"
+export XDG_CONFIG_DIRS="$XDG_CONFIG_DIRS${XDG_CONFIG_DIRS:+:}$out/etc/xdg"
 export XDG_DATA_DIRS="$XDG_DATA_DIRS${XDG_DATA_DIRS:+:}$out/share"
 
 envHooks+=(addQtDependency)
