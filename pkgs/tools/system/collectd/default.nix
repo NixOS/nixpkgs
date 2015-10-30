7a7e37743bb4945957d4bb0fb4fdfd1258bc1a78
Robbin C <robbinch33@gmail.com>
2015-10-30 10:22:34 +0800

    collectd: 5.4.2 -> 5.5.0

--- pkgs/tools/system/collectd/default.nix
+++ pkgs/tools/system/collectd/default.nix
@@ -1,8 +1,9 @@
-{ stdenv, fetchurl
+{ stdenv, fetchurl, libtool
 # optional:
 , pkgconfig ? null  # most of the extra deps need pkgconfig to be found
 , curl ? null
 , iptables ? null
+, libatasmart ? null
 , libcredis ? null
 , libdbi ? null
 , libgcrypt ? null
@@ -26,18 +27,18 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "collectd-5.4.2";
+  name = "collectd-5.5.0";
 
   src = fetchurl {
     url = "http://collectd.org/files/${name}.tar.bz2";
-    sha256 = "14z3qkqbmfjvqvcb2v17480f7c8j7wa49myk0zlxpd9qq40fk2cp";
+    sha256 = "124yar5jl1mi8c9lzhcqp9ld3q7nzkrql1s5871ivphhbk7q8xl4";
   };
 
   buildInputs = [
-    pkgconfig curl iptables libcredis libdbi libgcrypt libmemcached cyrus_sasl
-    libmodbus libnotify gdk_pixbuf liboping libpcap libsigrok libvirt
-    lm_sensors libxml2 lvm2 mysql.lib postgresql protobufc rabbitmq-c rrdtool
-    varnish yajl
+    pkgconfig curl libtool iptables libcredis libdbi libgcrypt libmemcached
+    cyrus_sasl libatasmart libmodbus libnotify gdk_pixbuf liboping libpcap
+    libsigrok libvirt lm_sensors libxml2 lvm2 mysql.lib postgresql protobufc
+    rabbitmq-c rrdtool varnish yajl
   ];
 
   # for some reason libsigrok isn't auto-detected
