2967b92d60062c0725914e5a84ffa8313694e22a
Stéphane Jourdois <sjourdois@gmail.com>
2015-10-31 22:59:34 +0100

    yara: init at 3.4.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15301,6 +15301,8 @@ let
 
   yandex-disk = callPackage ../tools/filesystems/yandex-disk { };
 
+  yara = callPackage ../tools/security/yara { };
+
   zdfmediathk = callPackage ../applications/video/zdfmediathk { };
 
   zopfli = callPackage ../tools/compression/zopfli { };
