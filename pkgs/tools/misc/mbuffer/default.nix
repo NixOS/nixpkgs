a498bb70590b38247fd595a3dd4ec7c06537458e
Daniel Frank <git@danielfrank.net>
2015-10-31 02:23:54 +0100

    mbuffer: new package

---
+++ pkgs/tools/misc/mbuffer/default.nix
@@ -0,0 +1,18 @@
+{ stdenv, fetchurl } :
+
+stdenv.mkDerivation rec {
+  name = "mbuffer-20151012";
+
+  src = fetchurl {
+    url = "http://www.maier-komor.de/software/mbuffer/mbuffer-20151002.tgz";
+    sha256 = "04pz70jr7fkdyax7b67g9jr0msl6ff2i8s6fl8zginqz5rrxckqk";
+  };
+
+  meta = {
+    homepage = http://www.maier-komor.de/mbuffer.html;
+    description  = "mbuffer is a tool for buffering data streams with a large set of unique features";
+    license = stdenv.lib.licenses.gpl3;
+    maintainers = with stdenv.lib.maintainers; [ tokudan ];
+    platforms = with stdenv.lib.platforms; all;
+  };
+}
