2a561e0b23595eee65bcaf24f192c8f70189419f
Nikolay Amiantov <ab@fmap.me>
2015-11-02 00:47:56 +0300

    pyspf: init at 2.0.12

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6202,6 +6202,24 @@ in modules // {
     propagatedBuildInputs = with self; [ pyramid hawkauthlib tokenlib webtest ];
   };
 
+  pyspf = buildPythonPackage rec {
+    name = "pyspf-${version}";
+    version = "2.0.12";
+
+    src = pkgs.fetchurl {
+      url = "mirror://sourceforge/pymilter/pyspf/${name}/${name}.tar.gz";
+      sha256 = "18j1rmbmhih7q6y12grcj169q7sx1986qn4gmpla9y5gwfh1p8la";
+    };
+
+    meta = {
+      homepage = http://bmsi.com/python/milter.html;
+      description = "Python API for Sendmail Milters (SPF)";
+      maintainers = with maintainers; [ abbradar ];
+      license = licenses.gpl2;
+      platform = platforms.all;
+    };
+  };
+
   radicale = buildPythonPackage rec {
     name = "radicale-${version}";
     namePrefix = "";
