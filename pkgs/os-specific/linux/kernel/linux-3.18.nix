827adff712dde127163c0422a970e29a02d061a4
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-11-04 13:22:22 +0100

    linux: Update to 3.18.24

--- pkgs/os-specific/linux/kernel/linux-3.18.nix
+++ pkgs/os-specific/linux/kernel/linux-3.18.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.18.23";
+  version = "3.18.24";
   extraMeta.branch = "3.18";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1jj3afa9p306j7gv13wsjwy322sdaz6jflhvzp6gwjfylncwyskj";
+    sha256 = "06wcgzhbiddzrabfd3z0zsb9qwa2phva5cq7gg0mzff67a6cym7d";
   };
 
   features.iwlwifi = true;
