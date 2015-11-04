1d1cf4845f1f219f61d439a359f4937df2041f06
aszlig <aszlig@redmoonstudios.org>
2015-11-04 13:59:36 +0100

    python-hetzner: Update to bugfix version 0.7.4.
    Fixes pinned CA root certificate and probing for system CA bundle.
    
    Closes NixOS/nixops#354.
    
    I'm using fetchFromGitHub now because it's use of fetchzip is more
    stable for autogenerated tarballs from GitHub.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7579,12 +7579,13 @@ let
 
   hetzner = buildPythonPackage rec {
     name = "hetzner-${version}";
-    version = "0.7.3";
+    version = "0.7.4";
 
-    src = pkgs.fetchurl {
-      url = "https://github.com/RedMoonStudios/hetzner/archive/"
-          + "v${version}.tar.gz";
-      sha256 = "1a0kcwqd1pj5giwh75m2m3jcnr1kd38v40hh64wgly2zp485nm5m";
+    src = pkgs.fetchFromGitHub {
+      repo = "hetzner";
+      owner = "RedMoonStudios";
+      rev = "v${version}";
+      sha256 = "04dlixczzvpimk48p87ix7j9q54jy46cwn4f05n2dlzsyc5vvxin";
     };
 
     # not there yet, but coming soon.
