53c60a0ce401d281d80b582b8e0c755587ee2033
Vladimír Čunát <vcunat@gmail.com>
2015-11-05 16:56:47 +0100

    live555: fixup after accidental merge, close #10624
    Use a URL that works (checked md5 was the same) but use a stronger hash.

--- pkgs/development/libraries/live555/default.nix
+++ pkgs/development/libraries/live555/default.nix
@@ -1,22 +1,25 @@
 { stdenv, fetchurl }:
 
 # Based on https://projects.archlinux.org/svntogit/packages.git/tree/trunk/PKGBUILD
+let
+  version = "2015.10.12";
+in
+stdenv.mkDerivation {
+  name = "live555-${version}";
 
-stdenv.mkDerivation rec {
-  name = "live555-2015.10.12";
-
-  src = fetchurl {
-    url = "http://www.live555.com/liveMedia/public/live.2015.10.12.tar.gz";
-    md5 = "bcbe18f0b4eb65ee0157333684d6c551";
+  src = fetchurl { # the upstream doesn't provide a stable URL
+    url = "mirror://sourceforge/slackbuildsdirectlinks/live.${version}.tar.gz";
+    sha256 = "05qwws4hda4x3l4ym22k55cc9j07w7g5vj3r18dancgf0fla40j4";
   };
 
-  buildInputs = [];
+  postPatch = "sed 's,/bin/rm,rm,g' -i genMakefiles";
 
   configurePhase = ''
     sed \
       -e 's/$(INCLUDES) -I. -O2 -DSOCKLEN_T/$(INCLUDES) -I. -O2 -I. -fPIC -DRTSPCLIENT_SYNCHRONOUS_INTERFACE=1 -DSOCKLEN_T/g' \
       -i config.linux
-     ./genMakefiles linux
+
+    ./genMakefiles linux
   '';
 
   installPhase = ''
