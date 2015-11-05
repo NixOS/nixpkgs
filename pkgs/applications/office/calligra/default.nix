2ad3cf914063178d3c281140cfe03ff71a8c3193
Nikolay Amiantov <ab@fmap.me>
2015-11-05 23:55:08 +0300

    caligra: add additional paths

--- pkgs/applications/office/calligra/default.nix
+++ pkgs/applications/office/calligra/default.nix
@@ -2,7 +2,7 @@
 , exiv2, boost, sqlite, icu, vc, shared_mime_info, librevenge, libodfgen, libwpg
 , libwpd, poppler_qt4, ilmbase, gsl, qca2, marble, libvisio, libmysql, postgresql
 , freetds, fftw, glew, libkdcraw, pstoedit, opencolorio, kdepimlibs
-, kactivities, okular, git
+, kactivities, okular, git, oxygen_icons, makeWrapper
 # TODO: not found
 #, xbase, openjpeg
 # TODO: package libWPS, Spnav, m2mml, LibEtonyek
@@ -16,17 +16,25 @@ stdenv.mkDerivation rec {
     sha256 = "08a5k8gjmzp9yzq46xy0p1sw7dpvxmxh8zz6dyj8q1dq29719kkc";
   };
 
-  nativeBuildInputs = [ automoc4 cmake perl pkgconfig ];
+  nativeBuildInputs = [ automoc4 cmake perl pkgconfig makeWrapper ];
 
   buildInputs = [
     kdelibs lcms2 libpng eigen
     exiv2 boost sqlite icu vc shared_mime_info librevenge libodfgen libwpg
     libwpd poppler_qt4 ilmbase gsl qca2 marble libvisio libmysql postgresql
-    freetds fftw glew libkdcraw pstoedit opencolorio kdepimlibs
+    freetds fftw glew libkdcraw opencolorio kdepimlibs
     kactivities okular git
   ];
 
   enableParallelBuilding = true;
+
+  postInstall = ''
+    for i in $out/bin/*; do
+      wrapProgram $i \
+        --prefix PATH ':' "${pstoedit}/bin" \
+        --prefix XDG_DATA_DIRS ':' "${oxygen_icons}/share"
+    done
+  '';
 
   meta = {
     description = "A suite of productivity applications";
