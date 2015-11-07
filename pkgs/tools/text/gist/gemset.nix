c0278ded1c656749f34b7641b98fcb47f788e540
zimbatm <zimbatm@zimbatm.com>
2015-11-07 19:36:31 +0000

    gist: 4.3.0 -> 4.4.2

--- pkgs/tools/text/gist/gemset.nix
+++ pkgs/tools/text/gist/gemset.nix
@@ -1,9 +1,9 @@
 {
   "gist" = {
-    version = "4.3.0";
+    version = "4.4.2";
     source = {
       type = "gem";
-      sha256 = "0az6l8nq433sszailr7kglh21l3gkcb11k7ag6668nyxxplm9rp0";
+      sha256 = "0lr4rywpm549llk0ypdpb3sjdpqw9snzwzqc3dggg8qn5wj69k81";
     };
   };
 }
