0ceda119d7f125e879358116c05d291780797ef7
Matthijs Steen <emailmatthijs@gmail.com>
2015-11-08 18:55:47 +0100

    google-chrome: init at 45.0.2454.101-1 (close #10892)
    As suggested the Google Chrome .deb file that is used for Chromium's plugins is reused.
    vcunat removed lots of newlines, as the style was diverging from the
    majority far too much (IHHO).

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11851,6 +11851,8 @@ let
     inherit (gnome) GConf;
   };
 
+  google-chrome = callPackage ../applications/networking/browsers/google-chrome { gconf = gnome.GConf; };
+
   googleearth = callPackage_i686 ../applications/misc/googleearth { };
 
   google_talk_plugin = callPackage ../applications/networking/browsers/mozilla-plugins/google-talk-plugin {
