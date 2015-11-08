699b477997e5b8e07515dd734018b3927b5a28d3
Stéphane Jourdois <sjourdois@gmail.com>
2015-11-08 23:27:52 +0100

    python: et_xmlfile: init at 1.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6532,6 +6532,33 @@ let
     };
   };
 
+  et_xmlfile = buildPythonPackage rec {
+    version = "1.0.1";
+    name = "et_xmlfile-${version}";
+
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/source/e/et_xmlfile/${name}.tar.gz";
+      sha256="0nrkhcb6jdrlb6pwkvd4rycw34y3s931hjf409ij9xkjsli9fkb1";
+    };
+
+    meta = {
+      description = "An implementation of lxml.xmlfile for the standard library";
+      longDescription = ''
+        et_xmlfile is a low memory library for creating large XML files.
+
+        It is based upon the xmlfile module from lxml with the aim of allowing
+        code to be developed that will work with both libraries. It was developed
+        initially for the openpyxl project but is now a standalone module.
+
+        The code was written by Elias Rabel as part of the Python Düsseldorf
+        openpyxl sprint in September 2014.
+      '';
+      homepage = "https://pypi.python.org/pypi/et_xmlfile";
+      license = licenses.mit;
+      maintainers = with maintainers; [ sjourdois ];
+    };
+  };
+
   eventlet = buildPythonPackage rec {
     name = "eventlet-0.17.4";
 
