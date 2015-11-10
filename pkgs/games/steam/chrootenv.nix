6202090f3126257384f19e6a4b380da02e17a9dc
Nikolay Amiantov <ab@fmap.me>
2015-11-10 23:59:57 +0300

    fhs-env: symlink /usr/lib to libs for the main architecture

--- pkgs/games/steam/chrootenv.nix
+++ pkgs/games/steam/chrootenv.nix
@@ -63,11 +63,6 @@ buildFHSUserEnv {
       pkgs.gst_all_1.gst-plugins-ugly # "Audiosurf 2" needs this
     ];
 
-  extraBuildCommandsMulti = ''
-    cd usr/lib
-    ln -sf ../lib64/steam steam
-  '';
-
   profile = ''
     # Ugly workaround for https://github.com/ValveSoftware/steam-for-linux/issues/3504
     export LD_PRELOAD=/lib32/libpulse.so:/lib64/libpulse.so:/lib32/libasound.so:/lib64/libasound.so:$LD_PRELOAD
