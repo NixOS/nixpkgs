50783998a21de80c238e009016ff46f963a14a24
Stéphane Jourdois <sjourdois@gmail.com>
2015-11-11 13:41:15 +0100

    cracklib: 2.9.4 -> 2.9.6

--- pkgs/development/libraries/cracklib/default.nix
+++ pkgs/development/libraries/cracklib/default.nix
@@ -1,17 +1,17 @@
 { stdenv, fetchurl, libintlOrEmpty, zlib, gettext }:
 
 stdenv.mkDerivation rec {
-  name = "cracklib-2.9.4";
+  name = "cracklib-2.9.6";
 
   src = fetchurl {
-    url = "mirror://sourceforge/cracklib/${name}.tar.gz";
-    sha256 = "0n49prh5rffl33bxy8qf46cqm6mswdlqpmm6iqi490w0p6s6da7j";
+    url = "https://github.com/cracklib/cracklib/releases/download/${name}/${name}.tar.gz";
+    sha256 = "0hrkb0prf7n92w6rxgq0ilzkk6rkhpys2cfqkrbzswp27na7dkqp";
   };
 
   buildInputs = [ libintlOrEmpty zlib gettext ];
 
   meta = with stdenv.lib; {
-    homepage    = http://sourceforge.net/projects/cracklib;
+    homepage    = https://github.com/cracklib/cracklib;
     description = "A library for checking the strength of passwords";
     maintainers = with maintainers; [ lovek323 ];
     platforms   = platforms.unix;
