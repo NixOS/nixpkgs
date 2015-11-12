d754c3c4ffee1af26d1f1bbfadf2efbe2ecbb9a8
Bjørn Forsman <bjorn.forsman@gmail.com>
2015-11-12 13:33:41 +0100

    jenkins: 1.636 -> 1.637 (security fixes)
    https://wiki.jenkins-ci.org/display/SECURITY/Jenkins+Security+Advisory+2015-11-11
    (cherry picked from commit 7a1c4ab324f5812eb1fc173dd2540a596b54baae)

--- pkgs/development/tools/continuous-integration/jenkins/default.nix
+++ pkgs/development/tools/continuous-integration/jenkins/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "jenkins-${version}";
-  version = "1.636";
+  version = "1.637";
 
   src = fetchurl {
     url = "http://mirrors.jenkins-ci.org/war/${version}/jenkins.war";
-    sha256 = "1j6wz5npgsgnkg5xyf3ylrziasxwx5plqr72zwx41a83yvncyx3k";
+    sha256 = "12d231gbr56gwnq34h8jzchln01gq3mx37s9f91ri0k6damsaafb";
   };
   meta = with stdenv.lib; {
     description = "An extendable open source continuous integration server";
