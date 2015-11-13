3a4a0f7d03f769f9769cb4dd7ef5907c9f9b0ae6
Pascal Wittmann <mail@pascal-wittmann.de>
2015-11-13 12:04:26 +0100

    calibre: 2.43.0 -> 2.44.0

--- pkgs/applications/misc/calibre/default.nix
+++ pkgs/applications/misc/calibre/default.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://github.com/kovidgoyal/calibre/releases/download/v${meta.version}/${name}.tar.xz";
-    sha256 = "0h8c8w7f63pl1rarr3v6gi82dcsabb3216g7scmjfj50na5cfmhi";
+    sha256 = "0d1sn2wc6h3c5girllsmnqg3jhmkal693ww3j6cj1mz2rraw45xr";
   };
 
   inherit python;
@@ -58,7 +58,7 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with stdenv.lib; {
-    version = "2.43.0";
+    version = "2.44.0";
     description = "Comprehensive e-book software";
     homepage = http://calibre-ebook.com;
     license = licenses.gpl3;
