a2e449e43e82e258b94c723d92a5e9af641967e7
Franz Pletz <fpletz@fnordicwalking.de>
2015-11-14 06:24:15 +0100

    coreutils: Skip some tests (filenames too long)

--- pkgs/tools/misc/coreutils/default.nix
+++ pkgs/tools/misc/coreutils/default.nix
@@ -28,6 +28,8 @@ let
     postPatch = optionalString (!stdenv.isDarwin) ''
       sed '2i echo Skipping dd sparse test && exit 0' -i ./tests/dd/sparse.sh
       sed '2i echo Skipping cp sparse test && exit 0' -i ./tests/cp/sparse.sh
+      sed '2i echo Skipping rm deep-2 test && exit 0' -i ./tests/rm/deep-2.sh
+      sed '2i echo Skipping du long-from-unreadable test && exit 0' -i ./tests/du/long-from-unreadable.sh
     '';
 
     configureFlags = optionalString stdenv.isSunOS "ac_cv_func_inotify_init=no";
