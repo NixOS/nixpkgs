fb683211dcbcc4ad81a575ebfb7bf0b382dcdf46
Russell O'Connor <oconnorr@google.com>
2015-11-16 17:52:12 -0500

    replace-dependency.nix: Remove unnecessary carriage return.

--- pkgs/build-support/replace-dependency.nix
+++ pkgs/build-support/replace-dependency.nix
@@ -80,4 +80,4 @@ let
 
   drvHash = discard (toString drv);
 in assert (stringLength (drvName (toString oldDependency)) == stringLength (drvName (toString newDependency)));
-rewriteMemo.${drvHash} or (warn "replace-dependency.nix: Derivation ${drvHash} does not depend on ${discard (toString oldDependency)}\n" drv)
+rewriteMemo.${drvHash} or (warn "replace-dependency.nix: Derivation ${drvHash} does not depend on ${discard (toString oldDependency)}" drv)
