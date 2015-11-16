17a7ab926b5403a4de953846ba578964b49fedf3
Ryan Scheel (Havvy) <ryan.havvy@gmail.com>
2015-11-16 06:06:52 +0000

    nodejs-argon: init at 2.4.1
    rename nodejs-4_1 -> nodejs-stable
    
    Same with nodejs packages.

---
+++ pkgs/development/web/nodejs/argon.nix
@@ -0,0 +1,62 @@
+# Node.js Argon is v4.2.x and a LTS for Node.js.
+# Support is from October 2015 to April 2018.
+
+{ stdenv, fetchurl, openssl, python, zlib, libuv, utillinux, http-parser
+, pkgconfig, runCommand, which, libtool
+}:
+
+# nodejs can't be built on armv5tel. Armv6 with FPU is minimum.
+# Related post: http://zo0ok.com/techfindings/archives/1820
+assert stdenv.system != "armv5tel-linux";
+
+let
+  version = "4.2.1";
+
+  deps = {
+    inherit openssl zlib libuv;
+    # Note: We use nodejs's internal v8 copy which has its own patches.
+  } // (stdenv.lib.optionalAttrs (!stdenv.isDarwin) {
+    inherit http-parser;
+  });
+
+  sharedConfigureFlags = name: [
+    "--shared-${name}"
+    "--shared-${name}-includes=${builtins.getAttr name deps}/include"
+    "--shared-${name}-libpath=${builtins.getAttr name deps}/lib"
+  ];
+
+  inherit (stdenv.lib) concatMap optional optionals maintainers licenses platforms;
+in stdenv.mkDerivation {
+  name = "nodejs-${version}";
+
+  src = fetchurl {
+    url = "http://nodejs.org/dist/v${version}/node-v${version}.tar.gz";
+    sha256 = "0qr6cxdgkvk3vdj6q5zf0cnxdhrh0h6736m1rl7kinxlqgsbjqc8";
+  };
+
+  configureFlags = concatMap sharedConfigureFlags (builtins.attrNames deps) ++ [ "--without-dtrace" ];
+  dontDisableStatic = true;
+  prePatch = ''
+    patchShebangs .
+    sed -i 's/raise.*No Xcode or CLT version detected.*/version = "7.0.0"/' tools/gyp/pylib/gyp/xcode_emulation.py
+  '';
+
+  patches = stdenv.lib.optionals stdenv.isDarwin [ ./no-xcode.patch ./pkg-libpath.patch ];
+
+  buildInputs = [ python which zlib libuv openssl python ]
+    ++ optionals stdenv.isLinux [ utillinux http-parser ]
+    ++ optionals stdenv.isDarwin [ pkgconfig openssl libtool ];
+  setupHook = ./setup-hook.sh;
+
+  enableParallelBuilding = true;
+
+  passthru.interpreterName = "nodejs";
+
+  meta = {
+    description = "Event-driven I/O framework for the V8 JavaScript engine";
+    homepage = https://nodejs.org/en/blog/release/v4.2.0/;
+    license = licenses.mit;
+    maintainers = [ maintainers.goibhniu maintainers.havvy ];
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2048,7 +2048,13 @@ let
 
   ninka = callPackage ../development/tools/misc/ninka { };
 
-  nodejs-4_1 = callPackage ../development/web/nodejs {
+  nodejs-stable = callPackage ../development/web/nodejs {
+    openssl = openssl_1_0_2;
+    libtool = darwin.cctools;
+  };
+
+  nodejs-argon = callPackage ../development/web/nodejs/argon.nix {
+    openssl = openssl_1_0_2;
     libtool = darwin.cctools;
   };
 
@@ -2060,16 +2066,18 @@ let
   nodejs = if stdenv.system == "armv5tel-linux" then
     nodejs-0_10
   else
-    nodejs-4_1;
+    nodejs-stable;
 
-  nodePackages_4_1 = recurseIntoAttrs (callPackage ./node-packages.nix { self = nodePackages_4_1; nodejs = nodejs-4_1; });
+  nodePackages_stable = recurseIntoAttrs (callPackage ./node-packages.nix { self = nodePackages_stable; nodejs = nodejs-stable; });
+
+  nodePackages_argon = recurseIntoAttrs (callPackage ./node-packages.nix { self = nodePackages_argon; nodejs = nodejs-argon; });
 
   nodePackages_0_10 = callPackage ./node-packages.nix { self = nodePackages_0_10; nodejs = nodejs-0_10; };
 
   nodePackages = if stdenv.system == "armv5tel-linux" then
     nodePackages_0_10
   else
-    nodePackages_4_1;
+    nodePackages_stable;
 
   npm2nix = nodePackages.npm2nix;
 
