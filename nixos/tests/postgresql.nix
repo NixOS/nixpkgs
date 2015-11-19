24cb910fb9d3f3f302cc6fc9b38eda0e22a85d64
Christian Zagrodnick <cz@flyingcircus.io>
2015-11-19 11:41:32 +0100

    Use nixstore for the configuration instead of some echo > filename dance.

--- nixos/tests/postgresql.nix
+++ nixos/tests/postgresql.nix
@@ -10,14 +10,16 @@ import ./make-test.nix ({ pkgs, ...} : {
 
       {
         services.postgresql.enable = true;
-        services.postgresql.initialScript = "/etc/nixos/postgresql-init.sql";
+        services.postgresql.initialScript =  pkgs.writeText "postgresql-init.sql"
+          ''
+          CREATE ROLE postgres WITH superuser login createdb;
+          '';
       };
   };
 
   testScript = ''
     startAll;
 
-    $master->execute("echo 'create role postgres with superuser login createdb;' > /etc/nixos/postgresql-init.sql");
     $master->waitForUnit("postgresql");
     $master->sleep(10); # Hopefully this is long enough!!
     $master->succeed("echo 'select 1' | sudo -u postgres psql");
