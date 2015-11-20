e32defcdac4730c773b1c6dd24bfe560947ca8d0
Brandon Martin <zmbmartin@gmail.com>
2015-11-20 10:14:49 -0700

    elmPackages update from 0.15.1 to 0.16

--- pkgs/development/compilers/elm/packages/elm-repl.nix
+++ pkgs/development/compilers/elm/packages/elm-repl.nix
@@ -1,26 +1,25 @@
 { mkDerivation, base, binary, bytestring, bytestring-trie, cmdargs
 , containers, directory, elm-compiler, elm-package, fetchgit
-, filepath, haskeline, HUnit, mtl, parsec, process, QuickCheck
-, stdenv, test-framework, test-framework-hunit
-, test-framework-quickcheck2
+, filepath, haskeline, HUnit, mtl, parsec, QuickCheck, stdenv
+, test-framework, test-framework-hunit, test-framework-quickcheck2
 }:
 mkDerivation {
   pname = "elm-repl";
-  version = "0.4.2";
+  version = "0.16";
   src = fetchgit {
     url = "https://github.com/elm-lang/elm-repl";
-    sha256 = "a6eadbef7886c4c65243723f101910909bb0d53b2c48454ed7b39cf700f9649c";
-    rev = "0c434fdb24b86a93b06c33c8f26857ce47caf165";
+    sha256 = "36d50cf1f86815900afd4b75da6e5cd15008b2652e97ffed0f321a28e6442874";
+    rev = "265de7283488964f44f0257a8b4a055ad8af984d";
   };
   isLibrary = false;
   isExecutable = true;
-  buildDepends = [
+  executableHaskellDepends = [
     base binary bytestring bytestring-trie cmdargs containers directory
-    elm-compiler elm-package filepath haskeline mtl parsec process
+    elm-compiler elm-package filepath haskeline mtl parsec
   ];
-  testDepends = [
+  testHaskellDepends = [
     base bytestring bytestring-trie cmdargs directory elm-compiler
-    elm-package filepath haskeline HUnit mtl parsec process QuickCheck
+    elm-package filepath haskeline HUnit mtl parsec QuickCheck
     test-framework test-framework-hunit test-framework-quickcheck2
   ];
   jailbreak = true;
