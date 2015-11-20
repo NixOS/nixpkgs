3b73b726d4975d821b90fb759b421a70e388e71d
Charles Strahan <charles.c.strahan@gmail.com>
2015-11-19 19:53:34 -0500

    zkfuse: init

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3652,6 +3652,8 @@ let
 
   zip = callPackage ../tools/archivers/zip { };
 
+  zkfuse = callPackage ../tools/filesystems/zkfuse { };
+
   zpaq = callPackage ../tools/archivers/zpaq { };
   zpaqd = callPackage ../tools/archivers/zpaq/zpaqd.nix { };
 
