d748ac851c164b1c76c8221194e7b6252d8fd557
Vladimír Čunát <vcunat@gmail.com>
2015-11-21 20:34:55 +0100

    putty: security update 0.65 -> 0.66
    It's claimed to fix CVE-2015-5309.

--- pkgs/applications/networking/remote/putty/default.nix
+++ pkgs/applications/networking/remote/putty/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ncurses, gtk, pkgconfig, autoconf, automake, perl, halibut, libtool }:
 
 stdenv.mkDerivation rec {
-  version = "0.65";
+  version = "0.66";
   name = "putty-${version}";
 
   src = fetchurl {
     url = "http://the.earth.li/~sgtatham/putty/latest/${name}.tar.gz";
-    sha256 = "180ccrsyh775hdmxqdnbclfbvsfdp2zk3gsadpa53sj497yw2hym";
+    sha256 = "14r9yfqjs61l82q09m8zifgcxrzvs6dvgx32ndl5i1jldkv14wzy";
   };
 
   preConfigure = ''
