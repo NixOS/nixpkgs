367170236df84f22c54dc46d3fbf369886ed8639
Frederik Rietdijk <fridh@fridh.nl>
2015-11-21 18:23:13 +0100

    Fix licenses

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9232,7 +9232,7 @@ let
     meta = {
       description = "A module for monitoring memory usage of a python program";
       homepage = http://pypi.python.org/pypi/memory_profiler;
-      license = licenses.bsd;
+      license = licenses.bsd3;
     };
   };
 
@@ -10176,7 +10176,7 @@ let
     meta = {
       homepage = http://nipy.org/nipype/;
       description = "Neuroimaging in Python: Pipelines and Interfaces";
-      license = "BSD";
+      license = licenses.bsd3;
     };
   };
 
@@ -13540,7 +13540,7 @@ let
     meta = {
       homepage = http://babel.edgewall.org;
       description = "A collection of tools for internationalizing Python applications";
-      license = "BSD";
+      license = licenses.bsd3;
       maintainers = with maintainers; [ garbas ];
     };
   });
