ce33fd2106bdb017554652a23783f1eff2492bb1
Marius Bakke <m.bakke@warwick.ac.uk>
2015-11-21 12:57:33 +0000

    sphinx_1_2: init at 1.2.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16890,6 +16890,15 @@ let
     };
   });
 
+  sphinx_1_2 = self.sphinx.override rec {
+    name = "sphinx-1.2.3";
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/source/s/sphinx/sphinx-1.2.3.tar.gz";
+      sha256 = "94933b64e2fe0807da0612c574a021c0dac28c7bd3c4a23723ae5a39ea8f3d04";
+    };
+    patches = [];
+    disabled = isPy35;
+  };
 
   sphinx_rtd_theme = buildPythonPackage (rec {
     name = "sphinx_rtd_theme-0.1.8";
