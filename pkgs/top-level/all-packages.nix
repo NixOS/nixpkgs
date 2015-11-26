3cc1f8dd15657c478f2bc145a740942b7aea4ae5
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-11-27 00:10:02 +0100

    hplip 3.15.9 -> 3.15.11
    Keep 3.15.9 available as hplip{,WithPlugin}_3_15_9 in case this
    breaks someone's printer/day job.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15157,6 +15157,10 @@ let
 
   hplipWithPlugin = hplip.override { withPlugin = true; };
 
+  hplip_3_15_9 = callPackage ../misc/drivers/hplip/3.15.9.nix { };
+
+  hplipWithPlugin_3_15_9 = hplip_3_15_9.override { withPlugin = true; };
+
   # using the new configuration style proposal which is unstable
   jack1 = callPackage ../misc/jackaudio/jack1.nix { };
 
