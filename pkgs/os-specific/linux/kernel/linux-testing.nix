03a3a905b9c547f5ac687f341fb8f1ce636f2959
aszlig <aszlig@redmoonstudios.org>
2015-12-01 09:32:49 +0100

    linux-testing: 4.4.0-rc1 -> 4.4.0-rc3
    Upstream changes can be found at:
    
    https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/log/?id=v4.4-rc1&id2=v4.4-rc3
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.4-rc1";
-  modDirVersion = "4.4.0-rc1";
+  version = "4.4-rc3";
+  modDirVersion = "4.4.0-rc3";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/testing/linux-${version}.tar.xz";
-    sha256 = "05zz8vvkd2jm3l19vydz627lmhc6zvhh5v9ij5hrh8m6g3zhyfga";
+    sha256 = "1c45bjclz5y039nqwrfil8yzv108r6vvbjfrq7dpz64iyf7iqnv4";
   };
 
   features.iwlwifi = true;
