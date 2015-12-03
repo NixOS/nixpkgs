474d64eecf8df4d62a1ed02f88c2b612f8e727d8
Evan Danaher <git@edanaher.net>
2015-12-02 23:43:53 -0500

    libstroke: init at version 0.5.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7438,6 +7438,8 @@ let
 
   libstartup_notification = callPackage ../development/libraries/startup-notification { };
 
+  libstroke = callPackage ../development/libraries/libstroke { };
+
   libstrophe = callPackage ../development/libraries/libstrophe { };
 
   libspatialindex = callPackage ../development/libraries/libspatialindex { };
