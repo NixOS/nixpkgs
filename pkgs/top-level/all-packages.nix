7cc6e6718ced96f59e0389b987da860b37fb5c56
Gabriel Ebner <gebner@gebner.org>
2015-12-05 08:43:56 +0100

    simp_le: init at 20151205, fixes #11479

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8352,6 +8352,8 @@ let
 
   simgear = callPackage ../development/libraries/simgear { };
 
+  simp_le = callPackage ../tools/admin/simp_le { };
+
   sfml = callPackage ../development/libraries/sfml { };
 
   signon = callPackage ../development/libraries/signon/old.nix {};
