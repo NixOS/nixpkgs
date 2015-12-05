ce013bf507be3fa89aee67fc9b3d6b308bfe4953
codsl <codsl@riseup.net>
2015-12-05 18:56:33 +0000

    emacs-packages: use-package: 20150317 -> 20151112

--- pkgs/top-level/emacs-packages.nix
+++ pkgs/top-level/emacs-packages.nix
@@ -1886,12 +1886,12 @@ let self = _self // overrides;
 
   use-package = melpaBuild rec {
     pname   = "use-package";
-    version = "20150317";
+    version = "20151112";
     src = fetchFromGitHub {
       owner  = "jwiegley";
       repo   = pname;
-      rev    = "b836266ddfbc835efdb327ecb389ff9e081d7c55";
-      sha256 = "187wnqqm5g43cg8b6a9rbd9ncqad5fhjb96wjszbinjh1mjxyh7i";
+      rev    = "77a77c8b03044f0279e00cadd6a6d1a7ae97b01";
+      sha256 = "14v6wzqn2jhjdbr7nwqilxy9l79m1f2rdrz2c6c6pcla5yjpd1k0";
     };
     packageRequires = [ bind-key diminish ];
     files = [ "use-package.el" ];
