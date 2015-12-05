f76fcaf88c4de785b53b69fd9c80a3e28c8b3261
Jan Malakhovski <oxij@oxij.org>
2015-12-05 13:13:18 +0000

    cmus: 2.6.0 -> 2.7.0, add a maintainer

--- pkgs/applications/audio/cmus/default.nix
+++ pkgs/applications/audio/cmus/default.nix
@@ -1,13 +1,14 @@
-{ stdenv, fetchgit, ncurses, pkgconfig, alsaLib, flac, libmad, ffmpeg, libvorbis, libmpc, mp4v2, libcue, libpulseaudio}:
+{ stdenv, fetchFromGitHub, ncurses, pkgconfig, alsaLib, flac, libmad, ffmpeg, libvorbis, libmpc, mp4v2, libcue, libpulseaudio}:
 
 stdenv.mkDerivation rec {
   name = "cmus-${version}";
-  version = "2.6.0";
+  version = "2.7.0";
 
-  src = fetchgit {
-    url = https://github.com/cmus/cmus.git;
-    rev = "46b71032da827d22d4fae5bf2afcc4c9afef568c";
-    sha256 = "1hkqifll5ryf3ljp3w1dxz1p8m6rk34fpazc6vwavis6ga310hka";
+  src = fetchFromGitHub {
+    owner  = "cmus";
+    repo   = "cmus";
+    rev    = "0306cc74c5073a85cf8619c61da5b94a3f863eaa";
+    sha256 = "18w9mznb843nzkrcqvshfha51mlpdl92zlvb5wfc5dpgrbf37728";
   };
 
   configurePhase = "./configure prefix=$out";
@@ -18,5 +19,6 @@ stdenv.mkDerivation rec {
     description = "Small, fast and powerful console music player for Linux and *BSD";
     homepage = https://cmus.github.io/;
     license = stdenv.lib.licenses.gpl2;
+    maintainers = [ stdenv.lib.maintainers.oxij ];
   };
 }
