9054ddefeb988f28637749a834d884dac2d2f62a
Vladimír Čunát <vcunat@gmail.com>
2015-12-05 13:27:24 +0100

    kde4.plasma-nm: fix build with multiple outputs

--- pkgs/tools/networking/plasma-nm/default.nix
+++ pkgs/tools/networking/plasma-nm/default.nix
@@ -14,7 +14,7 @@ stdenv.mkDerivation {
     sha256 = "0xj14isvjq8ll70b6q66n8adm8ff4j9ng195ndk2gmavjf6bb751";
   };
 
-  NIX_CFLAGS_COMPILE = "-I${glib}/include/glib-2.0 -I${glib}/lib/glib-2.0/include";
+  NIX_CFLAGS_COMPILE = "-I${glib.dev}/include/glib-2.0 -I${glib.out}/lib/glib-2.0/include";
 
   nativeBuildInputs = [ automoc4 cmake gettext perl pkgconfig ];
 
