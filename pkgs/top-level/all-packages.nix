60e2dced1a698e5d918c4b6a91778fdf5cb23c20
Frederik Rietdijk <fridh@fridh.nl>
2015-12-08 15:11:00 +0100

    mirage: use pillow instead of pil

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12371,7 +12371,10 @@ let
 
   mimms = callPackage ../applications/audio/mimms {};
 
-  mirage = callPackage ../applications/graphics/mirage {};
+  mirage = callPackage ../applications/graphics/mirage {
+    inherit (pythonPackages) pygtk;
+    inherit (pythonPackages) pillow;
+  };
 
   mixxx = callPackage ../applications/audio/mixxx {
     inherit (vamp) vampSDK;
