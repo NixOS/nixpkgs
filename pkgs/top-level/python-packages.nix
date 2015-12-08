5a26cfc27f31400f5abf5ba01a904b4d90b9b804
Christoph Hrdinka <c.github@hrdinka.at>
2015-12-08 12:02:51 +0100

    python tilestache: init at 1.50.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17019,6 +17019,27 @@ in modules // {
     };
   };
 
+  tilestache = self.buildPythonPackage rec {
+    name = "tilestache-${version}";
+    version = "1.50.1";
+
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/source/T/TileStache/TileStache-${version}.tar.gz";
+      sha256 = "1z1j35pz77lhhjdn69sq5rmz62b5m444507d8zjnp0in5xqaj6rj";
+    };
+
+    disabled = !isPy27;
+
+    propagatedBuildInputs = with self;
+      [ modestmaps pillow pycairo python-mapnik simplejson werkzeug ];
+
+    meta = {
+      description = "A tile server for rendered geographic data";
+      homepage = http://tilestache.org;
+      license = licenses.bsd3;
+    };
+  };
+
   timelib = buildPythonPackage rec {
     name = "timelib-0.2.4";
 
