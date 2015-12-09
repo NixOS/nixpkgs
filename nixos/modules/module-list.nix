c85ada394f495f3a7277253896ecb584dde8d496
Jakob Gillich <jakob@gillich.me>
2015-12-09 05:46:02 +0100

    rkt: add service

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -507,6 +507,7 @@
   ./virtualisation/amazon-options.nix
   ./virtualisation/openvswitch.nix
   ./virtualisation/parallels-guest.nix
+  ./virtualisation/rkt.nix
   ./virtualisation/virtualbox-guest.nix
   ./virtualisation/virtualbox-host.nix
   ./virtualisation/vmware-guest.nix
