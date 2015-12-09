bcd31d0acba04aad033eea10477aa42446600014
aszlig <aszlig@redmoonstudios.org>
2015-12-09 19:17:30 +0100

    mdadm: 3.3 -> 3.3.4
    Okay, this update is a bit more tricky and the patch I've included is
    not very nice to be honest.
    
    So the patch causes "mdadm --udev-rules" to search for the right store
    path using /proc/self/exec. This has the disadvantage that we could end
    up having /run/current-system/bin/mdadm in the rule file.
    
    But on the other hand, when we're on NixOS, we don't need to use that
    command *at*all*, so we should be safe.
    
    The patch also sets BINDIR to /sbin, which causes the included rule and
    systemd files to not work out of the box on NixOS. But we have a
    substituteInPlace where we do rewrite /sbin/mdadm to the right store
    path in our udev rule generator, so that shouldn't be an issue.
    
    I've tested this using the mdraid installer test and it succeeded.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/os-specific/linux/mdadm/default.nix
+++ pkgs/os-specific/linux/mdadm/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, groff }:
 
 stdenv.mkDerivation rec {
-  name = "mdadm-3.3";
+  name = "mdadm-3.3.4";
 
   # WARNING -- WARNING -- WARNING -- WARNING -- WARNING -- WARNING -- WARNING
   #  Do NOT update this if you're not ABSOLUTELY certain that it will work.
@@ -13,9 +13,11 @@ stdenv.mkDerivation rec {
   #   https://github.com/NixOS/nixpkgs/pull/6006
   # WARNING -- WARNING -- WARNING -- WARNING -- WARNING -- WARNING -- WARNING
   src = fetchurl {
-    url = "mirror://kernel/linux/utils/raid/mdadm/${name}.tar.bz2";
-    sha256 = "0igdqflihiq1dp5qlypzw0xfl44f4n3bckl7r2x2wfgkplcfa1ww";
+    url = "mirror://kernel/linux/utils/raid/mdadm/${name}.tar.xz";
+    sha256 = "0s6a4bq7v7zxiqzv6wn06fv9f6g502dp047lj471jwxq0r9z9rca";
   };
+
+  patches = [ ./no-self-references.patch ];
 
   nativeBuildInputs = [ groff ];
 
@@ -28,7 +30,7 @@ stdenv.mkDerivation rec {
   # /dev/.mdadm/map as a fallback).
   preBuild =
     ''
-      makeFlagsArray=(INSTALL=install BINDIR=$out/sbin MANDIR=$out/share/man RUN_DIR=/dev/.mdadm)
+      makeFlagsArray=(NIXOS=1 INSTALL=install INSTALL_BINDIR=$out/sbin MANDIR=$out/share/man RUN_DIR=/dev/.mdadm)
       if [[ -n "$crossConfig" ]]; then
         makeFlagsArray+=(CROSS_COMPILE=$crossConfig-)
       fi
