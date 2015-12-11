fc6d1471ce114dc91085b68337535ea83ca56682
aszlig <aszlig@redmoonstudios.org>
2015-12-11 11:29:49 +0100

    linux-testing: Revert build fix for -rc3.
    This reverts commit 79bd2b08ee56cc5e900d2f3ad8bd3ea0b5d88415.
    
    The commit was from an upstream commit anyway and has since been applied
    to mainline.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -19,13 +19,4 @@ import ./generic.nix (args // rec {
   # Should the testing kernels ever be built on Hydra?
   extraMeta.hydraPlatforms = [];
 
-  kernelPatches = stdenv.lib.singleton {
-    name = "fix-depmod-cycle";
-    patch = fetchurl {
-      name = "lustre-remove-IOC_LIBCFS_PING_TEST-ioctl.patch";
-      url = "https://lkml.org/lkml/diff/2015/11/6/987/1";
-      sha256 = "0ja9103f4s65fyn5b6z6lggplnm97hhz4rmpfn4m985yqw7zgihd";
-    };
-  };
-
 } // (args.argsOverride or {}))
