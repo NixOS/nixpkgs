e0a7c3a54bda41441ec2298a2e6f5b4653b03ed3
Pascal Wittmann <mail@pascal-wittmann.de>
2015-12-12 11:58:22 +0100

    intel-gpu-tools: 1.12 -> 1.13

--- pkgs/development/tools/misc/intel-gpu-tools/default.nix
+++ pkgs/development/tools/misc/intel-gpu-tools/default.nix
@@ -2,11 +2,11 @@
 , libX11, libXext, libXv, libXrandr, glib, bison, libunwind }:
 
 stdenv.mkDerivation rec {
-  name = "intel-gpu-tools-1.12";
+  name = "intel-gpu-tools-1.13";
 
   src = fetchurl {
     url = "http://xorg.freedesktop.org/archive/individual/app/${name}.tar.bz2";
-    sha256 = "1qwbwgvsqxba0adzlwxqmdw1asykx0pmk9ra0ff0nmjj9apf0gql";
+    sha256 = "0d5ff9l12zw9mdsjwbwn6y9k1gz6xlzsx5k87apz9vq6q625irn6";
   };
 
   buildInputs = [ pkgconfig libdrm libpciaccess cairo dri2proto udev libX11
