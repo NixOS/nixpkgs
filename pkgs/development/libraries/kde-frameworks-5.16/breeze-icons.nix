40242c1bff02556329718ecab9df7f899637d2d0
Thomas Tuegel <ttuegel@gmail.com>
2015-12-12 06:58:51 -0600

    kf516.breeze-icons: propagate qtsvg into user environment

--- pkgs/development/libraries/kde-frameworks-5.16/breeze-icons.nix
+++ pkgs/development/libraries/kde-frameworks-5.16/breeze-icons.nix
@@ -1,8 +1,10 @@
 { kdeFramework
 , extra-cmake-modules
+, qtsvg
 }:
 
 kdeFramework {
   name = "breeze-icons";
   nativeBuildInputs = [ extra-cmake-modules ];
+  propagatedUserEnvPkgs = [ qtsvg ];
 }
