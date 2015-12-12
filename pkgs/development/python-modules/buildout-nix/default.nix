b6776d41dd45c9bc1be80c4a5d99b16ce2fde77d
Asko Soukka <asko.soukka@iki.fi>
2015-12-12 21:32:59 +0200

    zc.buildout-nix: 2.4.3 -> 2.5.0

--- pkgs/development/python-modules/buildout-nix/default.nix
+++ pkgs/development/python-modules/buildout-nix/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, buildPythonPackage }:
 
 buildPythonPackage {
-  name = "zc.buildout-nix-2.4.3";
+  name = "zc.buildout-nix-2.5.0";
 
   src = fetchurl {
-    url = "https://pypi.python.org/packages/source/z/zc.buildout/zc.buildout-2.4.3.tar.gz";
-    sha256 = "db877d791e058a6207ac716e1017296e6862e4f1b5388dd145702eb19d902580";
+    url = "https://pypi.python.org/packages/source/z/zc.buildout/zc.buildout-2.5.0.tar.gz";
+    sha256 = "721bd2231a9f01f2d5c14f3adccb3385f85b093ee05b18d15d0ff2b9f1f1bd02";
   };
 
   patches = [ ./nix.patch ];
