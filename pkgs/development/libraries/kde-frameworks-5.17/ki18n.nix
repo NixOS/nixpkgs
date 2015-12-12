54829da8229e418d955a87ab39104f65d5d99c47
Thomas Tuegel <ttuegel@gmail.com>
2015-12-12 07:15:07 -0600

    kf517.ki18n: add missing qtdeclarative dependency

--- pkgs/development/libraries/kde-frameworks-5.17/ki18n.nix
+++ pkgs/development/libraries/kde-frameworks-5.17/ki18n.nix
@@ -2,13 +2,14 @@
 , extra-cmake-modules
 , gettext
 , python
+, qtdeclarative
 , qtscript
 }:
 
 kdeFramework {
   name = "ki18n";
   nativeBuildInputs = [ extra-cmake-modules ];
-  buildInputs = [ qtscript ];
+  buildInputs = [ qtdeclarative qtscript ];
   propagatedNativeBuildInputs = [ gettext python ];
   meta = {
     maintainers = [ lib.maintainers.ttuegel ];
