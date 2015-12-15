48e7eb65f2ecf12b1399395cd11e248e9c676e8f
Vincent Laporte <Vincent.Laporte@gmail.com>
2015-12-15 22:43:25 +0100

    ocaml-iso8601: init at 0.2.4
    OCaml library for ISO 8601 and RFC 3999 date parsing.
    
    Homepage: http://sagotch.github.io/ISO8601.ml/

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4522,6 +4522,8 @@ let
 
     ipaddr = callPackage ../development/ocaml-modules/ipaddr { };
 
+    iso8601 = callPackage ../development/ocaml-modules/iso8601 { };
+
     javalib = callPackage ../development/ocaml-modules/javalib {
       extlib = ocaml_extlib_maximal;
     };
