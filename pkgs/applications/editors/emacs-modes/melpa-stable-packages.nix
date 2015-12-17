e86f93eada0159d80db7b7ac880b6d94325668f3
Thomas Tuegel <ttuegel@gmail.com>
2015-12-17 07:15:49 -0600

    melpa-stable-packages: add instructions for updating

--- pkgs/applications/editors/emacs-modes/melpa-stable-packages.nix
+++ pkgs/applications/editors/emacs-modes/melpa-stable-packages.nix
@@ -1,3 +1,19 @@
+/*
+
+# Updating
+
+To update the list of packages from MELPA Stable,
+
+1. Clone https://github.com/ttuegel/emacs2nix
+2. Clone https://github.com/milkypostman/melpa
+3. Run `./melpa-stable-packages.sh PATH_TO_MELPA_CLONE` from emacs2nix.
+   Error messages about missing versions are normal; most packages in
+   MELPA do not have a stable version.
+4. Copy the new melpa-stable-packages.json file into Nixpkgs
+5. `git commit -m "melpa-stable-packages $(date -Idate)"`
+
+*/
+
 { lib }:
 
 let
