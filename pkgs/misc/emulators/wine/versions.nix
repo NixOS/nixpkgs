7a0dd2c82c7dc39cdfd507ed78393647313bd62b
Herwig Hochleitner <herwig@bendlas.net>
2015-12-19 23:28:16 +0100

    wine stable: 1.6.2 -> 1.8
    restructure versions file for clarity and brevity

--- pkgs/misc/emulators/wine/versions.nix
+++ pkgs/misc/emulators/wine/versions.nix
@@ -1,26 +1,26 @@
-{
-  unstable = {
-    wineVersion = "1.8-rc4";
-    wineSha256  = "0c4mb49ad11jd880wdl4gs1478q2rkdjw2qxjzskr08bdlcq7h5f";
+rec {
+  stable = {
+    wineVersion = "1.8";
+    wineSha256  = "1x66lzpk7v8qx57clmcq5ag7yh3mqplf1plypwghgchjh70lafzk";
+    ## see http://wiki.winehq.org/Gecko
     geckoVersion = "2.40";
     geckoSha256 = "00nkaxhb9dwvf53ij0q75fb9fh7pf43hmwx6rripcax56msd2a8s";
     gecko64Version = "2.40";
     gecko64Sha256 = "0c4jikfzb4g7fyzp0jcz9fk2rpdl1v8nkif4dxcj28nrwy48kqn3";
+    ## see http://wiki.winehq.org/Mono
     monoVersion = "4.5.6";
     monoSha256 = "09dwfccvfdp3walxzp6qvnyxdj2bbyw9wlh6cxw2sx43gxriys5c";
   };
-  stable = {
-    wineVersion = "1.6.2";
-    wineSha256  = "1gmc0ljgfz3qy50mdxcwwjcr2yrpz54jcs2hdszsrk50wpnrxazh";
-    geckoVersion = "2.21";
-    geckoSha256 = "1n0zccnvchkg0m896sjx5psk4bxw9if32xyxib1rbfdasykay7zh";
-    gecko64Version = "2.21";
-    gecko64Sha256 = "0grc86dkq90i59zw43hakh62ra1ajnk11m64667xjrlzi7f0ndxw";
-    monoVersion = "4.5.6";
-    monoSha256 = "09dwfccvfdp3walxzp6qvnyxdj2bbyw9wlh6cxw2sx43gxriys5c";
+  unstable = {
+    wineVersion = "1.8-rc4"; ## wait for staging 1.8
+    wineSha256  = "0c4mb49ad11jd880wdl4gs1478q2rkdjw2qxjzskr08bdlcq7h5f";
+    inherit (stable)
+      geckoVersion geckoSha256
+      gecko64Version gecko64Sha256
+      monoVersion monoSha256;
   };
   staging = {
-    version = "1.8-rc4";
+    version = unstable.wineVersion;
     sha256 = "11kd4id0zyqqjz1006d27ahmhy3gzai7h0wir9lkxnv3pdld0dxn";
   };
   winetricks = {
