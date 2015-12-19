e8aee16550b66d634ee3fb8c864b2e8aa4a03055
koral <koral@mailoo.org>
2015-12-19 11:05:24 +0000

    ncmpcpp: 0.6.7 -> 0.7

--- pkgs/applications/audio/ncmpcpp/default.nix
+++ pkgs/applications/audio/ncmpcpp/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, boost, mpd_clientlib, ncurses, pkgconfig, readline
-, libiconv
+, libiconv, icu
 , outputsSupport ? false # outputs screen
 , visualizerSupport ? false, fftw ? null # visualizer screen
 , clockSupport ? false # clock screen
@@ -15,11 +15,11 @@ assert taglibSupport -> (taglib != null);
 with stdenv.lib;
 stdenv.mkDerivation rec {
   name = "ncmpcpp-${version}";
-  version = "0.6.7";
+  version = "0.7";
 
   src = fetchurl {
     url = "http://ncmpcpp.rybczak.net/stable/${name}.tar.bz2";
-    sha256 = "0yr1ib14qkgbsv839anpzkfbwkm6gg8wv4bf98ar7q5l2p2pv008";
+    sha256 = "0xzz0g9whqjcjaaqmsw5ph1zvpi2j5v3i5k73g7916rca3q4z4jh";
   };
 
   configureFlags = [ "BOOST_LIB_SUFFIX=" ]
@@ -32,7 +32,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ pkgconfig ];
 
-  buildInputs = [ boost mpd_clientlib ncurses readline libiconv ]
+  buildInputs = [ boost mpd_clientlib ncurses readline libiconv icu ]
     ++ optional curlSupport curl
     ++ optional visualizerSupport fftw
     ++ optional taglibSupport taglib;
