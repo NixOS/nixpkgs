01a0ec7398d06b36ce8de46ed664dcba349541fc
Franz Pletz <fpletz@fnordicwalking.de>
2015-12-22 17:27:35 +0100

    bind: Fix patching Makefile.in
    There is no postPatchPhase.

--- pkgs/servers/dns/bind/default.nix
+++ pkgs/servers/dns/bind/default.nix
@@ -10,11 +10,7 @@ stdenv.mkDerivation rec {
     sha256 = "1w4gp4hdkb452nmz91l413d1rx89isl2l6wv8kpbdd2afpc3phws";
   };
 
-  postPatchPhase = ''
-    sed -i 's/^\t.*run/\t/' Makefile.in
-  '';
-
-  patches = [ ./libressl.patch ];
+  patches = [ ./libressl.patch ./remove-mkdir-var.patch ];
 
   buildInputs = [ openssl libtool perl libxml2 ];
 
