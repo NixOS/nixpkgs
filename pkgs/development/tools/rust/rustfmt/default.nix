37ebb4c2aca8ceb82fa82f48d1d6dc415b3bdc05
Robin Gloster <mail@glob.in>
2015-12-23 22:55:13 +0000

    rustfmt: 2015-12-08 -> 2015-12-23

--- pkgs/development/tools/rust/rustfmt/default.nix
+++ pkgs/development/tools/rust/rustfmt/default.nix
@@ -3,15 +3,15 @@
 with rustPlatform;
 
 buildRustPackage rec {
-  name = "rustfmt-git-2015-12-08";
+  name = "rustfmt-git-2015-12-23";
   src = fetchFromGitHub {
-    owner = "nrc";
+    owner = "rust-lang-nursery";
     repo = "rustfmt";
-    rev = "e94bd34a06d878a41bb8be409f173a8824dda63f";
-    sha256 = "0f0ixbr5nfla0j0b91plmapw75yl3d3lxwvllj2wx4z94nfxanp6";
+    rev = "c0b7de7c521dc65b2ad2b5a3c81fb56030f4af22";
+    sha256 = "1axnp8w26c3dwlx7bby3qi6385n301rlk1ndh5yaz7vkbpn4w9km";
   };
 
-  depsSha256 = "0vsrpw4icn9jf44sqr5749hbazsxp3hqn1g7gr90fvnfvz4s5f07";
+  depsSha256 = "1s2as7qc7jbksc16gj5cxxm52zw8h4nfgka66dmwwjlv9679wj9f";
 
   meta = with stdenv.lib; {
     description = "A tool for formatting Rust code according to style guidelines";
