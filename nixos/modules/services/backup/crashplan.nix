ea01e08e5b590ec5fb819511eea043a42d00306c
Svein Ove Aas <sveina@gmail.com>
2015-12-23 18:10:53 +0100

    crashplan:Bump to 4.5.0, rewrite wrappers, and set maintainer to Baughn
    It appears to work. I needed to manually change the backup location from
    /home/crashplan/backups to /var/lib/crashplan/backupArchives for each
    incoming backup, but that may be because it's an old account; that was
    the previous location of those backups. If you see the behaviour,
    please file a bug.
    
    At any rate, it no longer crashes on start.
    
    To use:
    services.crashplan.enable = true;

--- nixos/modules/services/backup/crashplan.nix
+++ nixos/modules/services/backup/crashplan.nix
@@ -4,6 +4,7 @@ let
   cfg = config.services.crashplan;
   crashplan = pkgs.crashplan;
   varDir = "/var/lib/crashplan";
+  user = "crashplan";
 in
 
 with lib;
@@ -24,38 +25,23 @@ with lib;
   config = mkIf cfg.enable {
     environment.systemPackages = [ crashplan ];
 
+    users.users.${user} = {
+      description = "CrashPlan Backup Engine";
+      isSystemUser = true;
+      home = varDir;
+      createHome = true;
+    };
+
     systemd.services.crashplan = {
       description = "CrashPlan Backup Engine";
 
       wantedBy = [ "multi-user.target" ];
       after    = [ "network.target" ];
 
-      preStart = ''
-        ensureDir() {
-          dir=$1
-          mode=$2
-
-          if ! test -e $dir; then
-            ${pkgs.coreutils}/bin/mkdir -m $mode -p $dir
-          elif [ "$(${pkgs.coreutils}/bin/stat -c %a $dir)" != "$mode" ]; then
-            ${pkgs.coreutils}/bin/chmod $mode $dir
-          fi
-        }
-
-        ensureDir ${crashplan.vardir} 755
-        ensureDir ${crashplan.vardir}/conf 700
-        ensureDir ${crashplan.manifestdir} 700
-        ensureDir ${crashplan.vardir}/cache 700
-        ensureDir ${crashplan.vardir}/backupArchives 700
-        ensureDir ${crashplan.vardir}/log 777
-      '';
-
       serviceConfig = {
-        Type = "forking";
-        EnvironmentFile = "${crashplan}/bin/run.conf";
-        ExecStart = "${crashplan}/bin/CrashPlanEngine start";
-        ExecStop = "${crashplan}/bin/CrashPlanEngine stop";
-        PIDFile = "${crashplan.vardir}/CrashPlanEngine.pid";
+        Type = "idle";
+        User = user;
+        ExecStart = "${crashplan}/bin/crashplan";
         WorkingDirectory = crashplan;
       };
     };
