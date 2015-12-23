38ca880612ff37695659b8b56af70553b2193f5f
Bjørn Forsman <bjorn.forsman@gmail.com>
2015-12-24 00:14:40 +0100

    nixos/cadvisor: rename 'host' to 'listenAddress'
    More descriptive option name.

--- nixos/modules/rename.nix
+++ nixos/modules/rename.nix
@@ -14,6 +14,7 @@ with lib;
     (mkRenamedOptionModule [ "networking" "enableWLAN" ] [ "networking" "wireless" "enable" ])
     (mkRenamedOptionModule [ "networking" "enableRT73Firmware" ] [ "networking" "enableRalinkFirmware" ])
 
+    (mkRenamedOptionModule [ "services" "cadvisor" "host" ] [ "services" "cadvisor" "listenAddress" ])
     (mkRenamedOptionModule [ "services" "dockerRegistry" "host" ] [ "services" "dockerRegistry" "listenAddress" ])
     (mkRenamedOptionModule [ "services" "mpd" "network" "host" ] [ "services" "mpd" "network" "listenAddress" ])
     (mkRenamedOptionModule [ "services" "neo4j" "host" ] [ "services" "neo4j" "listenAddress" ])
