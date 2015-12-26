39c43bc6e22ac739952b5e2e45c1ad6e57ff9e28
Vladimír Čunát <vcunat@gmail.com>
2015-12-26 11:07:29 +0100

    archiveopteryx: improve install paths, parallel build

--- pkgs/servers/mail/archiveopteryx/default.nix
+++ pkgs/servers/mail/archiveopteryx/default.nix
@@ -1,18 +1,24 @@
-{ stdenv, fetchurl, openssl, perl, zlib }:
+{ stdenv, fetchurl, openssl, perl, zlib, jam }:
 stdenv.mkDerivation rec {
   version = "3.2.0";
   name = "archiveopteryx-${version}";
+
   src = fetchurl {
     url = "http://archiveopteryx.org/download/${name}.tar.bz2";
     sha256 = "0i0zg8di8nbh96qnyyr156ikwcsq1w9b2291bazm5whb351flmqx";
   };
+
+  nativeBuildInputs = [ jam ];
   buildInputs = [ openssl perl zlib ];
+
+  preConfigure = ''export PREFIX="$out" '';
+  buildPhase = ''jam "-j$NIX_BUILD_CORES" '';
   installPhase = ''
-    INSTALLROOT=$out make install
-    mkdir $out/bin
-    ln --symbolic $out/usr/local/archiveopteryx/sbin/* $out/bin/
-    ln --symbolic $out/usr/local/archiveopteryx/bin/* $out/bin/
+    jam install
+    mkdir -p "$out/share/doc/archiveopteryx"
+    mv -t "$out/share/doc/archiveopteryx/" "$out"/{bsd.txt,COPYING,README}
   '';
+
   meta = with stdenv.lib; {
     homepage = http://archiveopteryx.org/;
     description = "An advanced PostgreSQL-based IMAP/POP server";
