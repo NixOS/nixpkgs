9ca37a8a4b4e10d36c072cecd8729ba4a1928c26
Peter Simons <simons@cryp.to>
2015-12-26 12:52:47 +0100

    haskell-text: doCheck=false override is now generated by hackage2nix
    This is necessary to break an finite recursion in the test suite dependencies.

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -43,11 +43,6 @@ self: super: {
   options_1_2 = dontCheck super.options_1_2;
   options = dontCheck super.options;
   statistics = dontCheck super.statistics;
-  text_1_1_1_3 = dontCheck super.text_1_1_1_3;
-  text_1_2_0_3 = dontCheck super.text_1_2_0_3;
-  text_1_2_0_4 = dontCheck super.text_1_2_0_4;
-  text_1_2_0_6 = dontCheck super.text_1_2_0_6;
-  text = dontCheck super.text;
   c2hs = if pkgs.stdenv.isDarwin then dontCheck super.c2hs else super.c2hs;
 
   # The package doesn't compile with ruby 1.9, which is our default at the moment.
