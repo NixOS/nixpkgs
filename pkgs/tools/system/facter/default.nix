6c34b9c2adfc25c252444c221429fbb300e59131
Kranium Gikos Mendoza <kranium@gikos.net>
2016-01-01 15:39:03 +0800

    facter: 3.0.2 -> 3.1.3

--- pkgs/tools/system/facter/default.nix
+++ pkgs/tools/system/facter/default.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   name = "facter-${version}";
-  version = "3.0.2";
+  version = "3.1.3";
   src = fetchurl {
     url = "https://downloads.puppetlabs.com/facter/${name}.tar.gz";
-    sha256 = "1myf1r827bh3n0snkzwj2jnc0sax9bq6z1vv9gr90rqr73lixvig";
+    sha256 = "1ngp3xjdh6x1w7lsi4lji2xzqp0x950jngcdlq11lcr0wfnzwyxj";
   };
 
   libyamlcpp_ = libyamlcpp.override { makePIC = true; };
