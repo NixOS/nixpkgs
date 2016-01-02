a5d2d25b367b05e360c62eb557516e3fcbd6a98c
Matthew O'Gorman <mog@rldn.net>
2016-01-02 02:08:18 -0500

    This fixes bug #10825 for hexchat and xchat.  They loads enchant library at runtime so it needs to be in path.

--- pkgs/applications/networking/irc/xchat/default.nix
+++ pkgs/applications/networking/irc/xchat/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, pkgconfig, tcl, gtk}:
+{stdenv, fetchurl, pkgconfig, tcl, gtk, enchant }:
 
 stdenv.mkDerivation {
   name = "xchat-2.8.8";
@@ -6,10 +6,16 @@ stdenv.mkDerivation {
     url = http://www.xchat.org/files/source/2.8/xchat-2.8.8.tar.bz2;
     sha256 = "0d6d69437b5e1e45f3e66270fe369344943de8a1190e498fafa5296315a27db0";
   };
-  buildInputs = [pkgconfig tcl gtk];
+  buildInputs = [pkgconfig tcl gtk enchant ];
   configureFlags = "--disable-nls";
 
   patches = [ ./glib-top-level-header.patch ];
+
+
+ #hexchat and heachat-text loads enchant spell checking library at run time and so it needs to have route to the path
+  patchPhase = ''
+    sed -i "s,libenchant.so.1,${enchant}/lib/libenchant.so.1,g" src/fe-gtk/sexy-spell-entry.c
+  '';
 
   meta = {
     description = "IRC client using GTK";
