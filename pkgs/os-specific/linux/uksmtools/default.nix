5606e8b043ccc976f0f159764a19b1cec2ee5eb6
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2016-01-04 04:06:43 +0100

    uksmtools: add 1-line comment to clarify `fetchgit` use...

--- pkgs/os-specific/linux/uksmtools/default.nix
+++ pkgs/os-specific/linux/uksmtools/default.nix
@@ -4,6 +4,7 @@ let version = "2015-09-25"; in
 stdenv.mkDerivation {
   name = "uksmtools-${version}";
 
+  # This project uses git submodules, which fetchFromGitHub doesn't support:
   src = fetchgit {
     sha256 = "0ngdmici2vgi2z02brzc3f78j1g1y9myzfxn46zlm1skg94fp692";
     rev = "9f59a3a0b494b758aa91d7d8fa04e21b5e6463c0";
