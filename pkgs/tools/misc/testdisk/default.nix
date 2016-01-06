6c377c43a9cea37e783153789e2afcc455e541cc
Jos van den Oever <jos@vandenoever.info>
2016-01-06 12:10:54 +0100

    testdisk 6.14 -> 7.0

--- pkgs/tools/misc/testdisk/default.nix
+++ pkgs/tools/misc/testdisk/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ncurses, libjpeg, e2fsprogs, zlib, openssl, libuuid, ntfs3g }:
 
 stdenv.mkDerivation {
-  name = "testdisk-6.14";
+  name = "testdisk-7.0";
 
   src = fetchurl {
-    url = http://www.cgsecurity.org/testdisk-6.14.tar.bz2;
-    sha256 = "0v1jap83f5h99zv01v3qmqm160d36n4ysi0gyq7xzb3mqgmw75x5";
+    url = http://www.cgsecurity.org/testdisk-7.0.tar.bz2;
+    sha256 = "00bb3b6b22e6aba88580eeb887037aef026968c21a87b5f906c6652cbee3442d";
   };
 
   buildInputs = [ ncurses libjpeg zlib openssl libuuid ]
