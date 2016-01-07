3d3ccd4a1500c9e7acc3e3c377edcc5bbd0618f3
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2016-01-07 12:21:23 +0100

    myguiSvn: remove dead package
    Subversion snapshot from 2011. Only dependent was Rigs of Rods.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7885,8 +7885,6 @@ let
 
   mygui = callPackage ../development/libraries/mygui {};
 
-  myguiSvn = callPackage ../development/libraries/mygui/svn.nix {};
-
   mysocketw = callPackage ../development/libraries/mysocketw { };
 
   mythes = callPackage ../development/libraries/mythes { };
