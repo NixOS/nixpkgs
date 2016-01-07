972c5641e404767df6cc47d290275d9d9b00bcbb
Bjørn Forsman <bjorn.forsman@gmail.com>
2016-01-07 18:37:18 +0100

    thrift: 0.9.2 -> 0.9.3
    Add Twisted as build input so that we can continue to have Python
    support. (./configure disables Python support unless it finds the
    'trial' program, from Twisted.) I don't know whether upstream intended
    that, because it seems perfectly fine to run thrift + Python without
    Twisted. (Only the TTwisted transport uses Twisted...)
    
    Ah, Thrift use Twisted in its unit tests. Even when we pass
    --enable-tests=no to ./configure :-D

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8527,7 +8527,9 @@ let
     python = python2;
   };
 
-  thrift = callPackage ../development/libraries/thrift { };
+  thrift = callPackage ../development/libraries/thrift {
+    inherit (pythonPackages) twisted;
+  };
 
   tidyp = callPackage ../development/libraries/tidyp { };
 
