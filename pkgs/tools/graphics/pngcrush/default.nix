e1873ade382294a4e49acfe68ab811c119f12766
zimbatm <zimbatm@zimbatm.com>
2016-01-09 16:33:54 +0000

    pngcrush: 1.7.85 -> 1.7.92
    also fixes a compilation issue on OSX

--- pkgs/tools/graphics/pngcrush/default.nix
+++ pkgs/tools/graphics/pngcrush/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, libpng }:
 
 stdenv.mkDerivation rec {
-  name = "pngcrush-1.7.85";
+  name = "pngcrush-1.7.92";
 
   src = fetchurl {
     url = "mirror://sourceforge/pmt/${name}-nolib.tar.xz";
-    sha256 = "1hvcync32x2ign694scafkj7xc73gzyy8n2l5z026yxckilyyv19";
+    sha256 = "0dlwbqckv90cpvg8qhkl3nk5yb75ddi61vbpmmp9n0j6qq9lp6y4";
   };
 
   configurePhase = ''
