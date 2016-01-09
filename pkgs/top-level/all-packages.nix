e11004cdf357cd63dd4fe99305a8e990e968855c
Matthew O'Gorman <mog@rldn.net>
2016-01-08 23:00:29 -0500

    zap: init at 2.4.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15765,6 +15765,8 @@ let
 
   yara = callPackage ../tools/security/yara { };
 
+  zap = callPackage ../tools/networking/zap { };
+
   zdfmediathk = callPackage ../applications/video/zdfmediathk { };
 
   zopfli = callPackage ../tools/compression/zopfli { };
