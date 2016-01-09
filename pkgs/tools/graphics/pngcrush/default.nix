93ddfbacd4a25eab23eb5eba073638c6af7657d2
zimbatm <zimbatm@zimbatm.com>
2016-01-09 16:35:27 +0000

    pngcrush: fixes compilation on darwin

--- pkgs/tools/graphics/pngcrush/default.nix
+++ pkgs/tools/graphics/pngcrush/default.nix
@@ -8,6 +8,8 @@ stdenv.mkDerivation rec {
     sha256 = "0dlwbqckv90cpvg8qhkl3nk5yb75ddi61vbpmmp9n0j6qq9lp6y4";
   };
 
+  makeFlags = [ "CC=cc" "LD=cc" ];      # gcc and/or clang compat
+
   configurePhase = ''
     sed -i s,/usr,$out, Makefile
   '';
@@ -18,7 +20,7 @@ stdenv.mkDerivation rec {
     homepage = http://pmt.sourceforge.net/pngcrush;
     description = "A PNG optimizer";
     license = stdenv.lib.licenses.free;
-    platforms = with stdenv.lib.platforms; linux;
+    platforms = with stdenv.lib.platforms; linux ++ darwin;
     maintainers = with stdenv.lib.maintainers; [ the-kenny ];
   };
 }
