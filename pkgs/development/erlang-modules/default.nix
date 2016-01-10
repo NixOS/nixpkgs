b509dadbf157316b88641cd5d64a23ab524f486f
Eric Merritt <eric@merritt.tech>
2016-01-10 15:16:53 -0800

    erlang support: remove broken buildErlang function
    The buildErlang function is broken and and leads Engineers down a wrong
    path. For vanilla erlang that doesn't user rebar3, its better to simply
    use `stdenv.mkDerivation` along with a set setupHook then the existing
    functionality.

--- pkgs/development/erlang-modules/default.nix
+++ pkgs/development/erlang-modules/default.nix
@@ -4,7 +4,6 @@ let
   self = rec {
     hex = import ./hex-packages.nix { callPackage = self.callPackage; };
     callPackage = pkgs.lib.callPackageWith (pkgs // self // hex);
-    buildErlang = callPackage ./build-erlang.nix {};
     buildHex = callPackage ./build-hex.nix {};
   };
 in self // self.hex
