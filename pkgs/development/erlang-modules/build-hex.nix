474e4178914719556b7ae9c3ed7084bb82d31b77
Eric Merritt <eric@merritt.tech>
2016-01-10 14:48:44 -0800

    erlang support: allow nix-shell to work correctly
    This changes build-hex.nix so that the buildHex and related functions
    work correctly in a nix-shell.

--- pkgs/development/erlang-modules/build-hex.nix
+++ pkgs/development/erlang-modules/build-hex.nix
@@ -1,7 +1,9 @@
-{ stdenv, erlang, rebar3, openssl, libyaml, fetchHex, fetchFromGitHub,
+{ stdenv, writeText, erlang, rebar3, openssl, libyaml, fetchHex, fetchFromGitHub,
   rebar3-pc, buildEnv }:
 
-{ name, version, sha256
+{ name, version, sha256 ? false
+, src ? null
+, setupHook ? null
 , hexPkg ? name
 , buildInputs ? [], erlangDeps ? [], pluginDeps ? []
 , postPatch ? ""
@@ -13,64 +15,40 @@ with stdenv.lib;
 
 let
   plugins = pluginDeps ++ (if compilePorts then [rebar3-pc] else []);
-  getDeps = drv: [drv] ++ (map getDeps drv.erlangDeps);
-  recursiveDeps = unique (flatten (map getDeps erlangDeps));
-  recursivePluginsDeps = unique (flatten (map getDeps plugins));
 
-  erlEnv = drv: buildEnv {
-    name = "erlang-env-${drv.name}";
-    paths = [ drv ] ++ recursiveDeps;
-    ignoreCollisions = false;
-    meta = drv.meta;
-  };
 
-  shell = drv: let
-    drvEnv = erlEnv drv;
-  in stdenv.mkDerivation {
-    name = "interactive-shell-${drv.name}";
-    nativeBuildInputs = [ erlang drvEnv ];
-    shellHook = ''
-      export ERL_LIBS="${drvEnv}";
-    '';
-  };
+  shell = drv: stdenv.mkDerivation {
+          name = "interactive-shell-${drv.name}";
+          buildInputs = [ drv ];
+    };
+
   pkg = self: stdenv.mkDerivation (attrs // {
+
     name = "${name}-${version}";
+    inherit version;
 
     buildInputs = buildInputs ++ [ erlang rebar3 openssl libyaml ];
+    propagatedBuildInputs = erlangDeps ++ plugins;
 
-    src = fetchHex {
+    src = if src == null then fetchHex {
       pkg = hexPkg;
       inherit version;
       inherit sha256;
-    };
+    } else src;
+
+    setupHook = if setupHook == null
+    then writeText "setupHook.sh" ''
+       addToSearchPath ERL_LIBS "$1/lib/erlang/lib/"
+    ''
+    else setupHook;
 
     postPatch = ''
       rm -f rebar rebar3
-      if [ -e "src/${name}.app.src" ]; then
-        sed -i -e 's/{ *vsn *,[^}]*}/{vsn, "${version}"}/' "src/${name}.app.src"
-      fi
-
-      ${if compilePorts then ''
-        echo "{plugins, [pc]}." >> rebar.config
-      '' else ''''}
-
-      ${rebar3.setupRegistry}
-
-      ${postPatch}
     '';
 
     configurePhase = ''
       runHook preConfigure
-      ${concatMapStrings (dep: ''
-        header "linking erlang dependency ${dep}"
-        ln -s "${dep}/${dep.name}" "_build/default/lib/${dep.name}"
-        stopNest
-      '') recursiveDeps}
-      ${concatMapStrings (dep: ''
-        header "linking rebar3 plugins ${dep}"
-        ln -s "${dep}/${dep.name}" "_build/default/plugins/${dep.name}"
-        stopNest
-      '') recursivePluginsDeps}
+      rebar3-nix-bootstrap
       runHook postConfigure
     '';
 
@@ -85,11 +63,11 @@ let
 
     installPhase = ''
       runHook preInstall
-      mkdir -p "$out/${name}"
+      mkdir -p "$out/lib/erlang/lib/${name}-${version}"
       for reldir in src ebin priv include; do
         fd="_build/default/lib/${name}/$reldir"
         [ -d "$fd" ] || continue
-        cp -Hrt "$out/${name}" "$fd"
+        cp -Hrt "$out/lib/erlang/lib/${name}-${version}" "$fd"
         success=1
       done
       runHook postInstall
