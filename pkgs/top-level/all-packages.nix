d71fce2b62ff74ac2d557235421c8b141d97efa6
Nikolay Amiantov <ab@fmap.me>
2016-01-10 20:51:11 +0300

    cargoUnstable: init at 2016-01-10

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4881,8 +4881,7 @@ let
   rustPlatform = rustStable;
 
   rustStable = recurseIntoAttrs (makeRustPlatform cargo rustStable);
-  rustUnstable = recurseIntoAttrs (makeRustPlatform
-    (cargo.override { rustPlatform = rustUnstableCargoPlatform; }) rustUnstable);
+  rustUnstable = recurseIntoAttrs (makeRustPlatform cargoUnstable rustUnstable);
 
   # rust platform to build cargo itself (with cargoSnapshot)
   rustCargoPlatform = makeRustPlatform (cargoSnapshot rustc) rustCargoPlatform;
@@ -5539,6 +5538,10 @@ let
   cargo = callPackage ../development/tools/build-managers/cargo {
     # cargo needs to be built with rustCargoPlatform, which uses cargoSnapshot
     rustPlatform = rustCargoPlatform;
+  };
+
+  cargoUnstable = callPackage ../development/tools/build-managers/cargo/head.nix {
+    rustPlatform = rustUnstableCargoPlatform;
   };
 
   cargoSnapshot = rustc:
