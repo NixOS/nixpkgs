49aa382f87c556d9483965303088565b0f54ac76
Thomas Tuegel <ttuegel@gmail.com>
2016-01-11 09:45:50 -0600

    emacsPackagesNg.undo-tree: 0.6.4 -> 0.6.5
    This removes the hand-written expression.

--- pkgs/top-level/emacs-packages.nix
+++ pkgs/top-level/emacs-packages.nix
@@ -1801,20 +1801,6 @@ let
     };
   };
 
-  undo-tree = melpaBuild rec {
-    pname   = "undo-tree";
-    version = "0.6.4";
-    src = fetchgit {
-      url    = "http://www.dr-qubit.org/git/${pname}.git";
-      rev    = "a3e81b682053a81e082139300ef0a913a7a610a2";
-      sha256 = "1qla7njkb7gx5aj87i8x6ni8jfk1k78ivwfiiws3gpbnyiydpx8y";
-    };
-    meta = {
-      description = "A port of Vim's undo tree functionality to Emacs";
-      license = gpl3Plus;
-    };
-  };
-
   use-package = melpaBuild rec {
     pname   = "use-package";
     version = "20151112";
