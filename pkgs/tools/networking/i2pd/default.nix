3a6c354094392c5aed8d785ac1794f57777673c9
Edward Tjörnhammar <ed@cflags.cc>
2016-01-12 21:37:36 +0100

    i2pd: 2.2.0 -> 2.3.0

--- pkgs/tools/networking/i2pd/default.nix
+++ pkgs/tools/networking/i2pd/default.nix
@@ -4,13 +4,13 @@ stdenv.mkDerivation rec {
 
   name = pname + "-" + version;
   pname = "i2pd";
-  version = "2.2.0";
+  version = "2.3.0";
 
   src = fetchFromGitHub {
     owner = "PurpleI2P";
     repo = pname;
     rev = version;
-    sha256 = "0slrfmgrf9b689wpsdpvsnmhbqsygcy558dz259k6xcf50f7lfqh";
+    sha256 = "0gb6bdsyb7m0jkilln9h7z2l8gr8ia10jah17ygc15jzycygijis";
   };
 
   buildInputs = [ boost zlib openssl ];
