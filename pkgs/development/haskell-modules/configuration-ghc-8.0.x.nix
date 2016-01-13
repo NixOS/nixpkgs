88f9fb7c21f8dad560a9576f85dff376201d0e20
Jude Taylor <me@jude.bio>
2016-01-13 14:06:53 -0800

    build jailbreak-cabal on GHC 8.0.x

--- pkgs/development/haskell-modules/configuration-ghc-8.0.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.0.x.nix
@@ -29,4 +29,26 @@ self: super: {
   transformers = null;
   unix = null;
   xhtml = null;
+
+  Cabal_1_23_0_0 = overrideCabal super.Cabal_1_22_4_0 (drv: {
+    version = "1.23.0.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "haskell";
+      repo = "cabal";
+      rev = "18fcd9c1aaeddd9d10a25e44c0e986c9889f06a7";
+      sha256 = "1bakw7h5qadjhqbkmwijg3588mjnpvdhrn8lqg8wq485cfcv6vn3";
+    };
+    jailbreak = false;
+    doHaddock = false;
+    postUnpack = "sourceRoot+=/Cabal";
+    postPatch = ''
+      setupCompileFlags+=" -DMIN_VERSION_binary_0_8_0=1"
+    '';
+  });
+  jailbreak-cabal = super.jailbreak-cabal.override {
+    Cabal = self.Cabal_1_23_0_0;
+    mkDerivation = drv: self.mkDerivation (drv // {
+      preConfigure = "sed -i -e 's/Cabal == 1.20\\.\\*/Cabal >= 1.23/' jailbreak-cabal.cabal";
+    });
+  };
 }
