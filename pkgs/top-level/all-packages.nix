dcd428db08c755d31b3bd5af1785459a7f5a9435
Evgeny Egorochkin <phreedom@yandex.ru>
2016-01-15 03:09:29 +0200

    azure-cli: init at 0.9.13

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -584,6 +584,8 @@ let
 
   awscli = pythonPackages.awscli;
 
+  azure-cli = callPackage ../tools/virtualization/azure-cli { };
+
   ec2_api_tools = callPackage ../tools/virtualization/ec2-api-tools { };
 
   ec2_ami_tools = callPackage ../tools/virtualization/ec2-ami-tools { };
