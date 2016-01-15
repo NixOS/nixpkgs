f4d71737a8af1666f13bc25ab999fc7b792f1876
Nikolay Amiantov <ab@fmap.me>
2016-01-15 14:57:17 +0300

    steam: propagate runtime-wrapped flags for more convenient overrides

--- pkgs/games/steam/default.nix
+++ pkgs/games/steam/default.nix
@@ -1,11 +1,17 @@
-{ pkgs, newScope }:
+{ pkgs, newScope
+, nativeOnly ? false
+, runtimeOnly ? false
+, newStdcpp ? false
+}:
 
 let
   callPackage = newScope self;
 
   self = rec {
     steam-runtime = callPackage ./runtime.nix { };
-    steam-runtime-wrapped = callPackage ./runtime-wrapped.nix { };
+    steam-runtime-wrapped = callPackage ./runtime-wrapped.nix {
+      inherit nativeOnly runtimeOnly newStdcpp;
+    };
     steam = callPackage ./steam.nix { };
     steam-chrootenv = callPackage ./chrootenv.nix { };
     steam-fonts = callPackage ./fonts.nix { };
