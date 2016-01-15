cdb0db3f31973e86652c0dc8b0f2f4ee32c02e8d
Vladimír Čunát <vcunat@gmail.com>
2016-01-15 18:01:28 +0100

    telepathy-qt: fix parallel build by upstream patch

--- pkgs/development/libraries/telepathy/qt/default.nix
+++ pkgs/development/libraries/telepathy/qt/default.nix
@@ -8,12 +8,20 @@ stdenv.mkDerivation rec {
     url = "http://telepathy.freedesktop.org/releases/telepathy-qt/${name}.tar.gz";
     sha256 = "0j7hs055cx5g9chn3b2p0arig70m5g9547qgqvk29kxdyxxxsmqc";
   };
-  patches = [(fetchpatch {
-    name = "gst-1.6.patch";
-    url = "http://cgit.freedesktop.org/telepathy/telepathy-qt/patch"
-      + "/?id=ec4a3d62b68a57254515f01fc5ea3325ffb1dbfb";
-    sha256 = "1rh7n3xyrwpvpa3haqi35qn4mfz4396ha43w4zsqpmcyda9y65v2";
-  })];
+  patches = let
+    mkUrl = hash: "http://cgit.freedesktop.org/telepathy/telepathy-qt/patch/?id=" + hash;
+    in [
+      (fetchpatch {
+        name = "gst-1.6.patch";
+        url = mkUrl "ec4a3d62b68a57254515f01fc5ea3325ffb1dbfb";
+        sha256 = "1rh7n3xyrwpvpa3haqi35qn4mfz4396ha43w4zsqpmcyda9y65v2";
+      })
+      (fetchpatch {
+        name = "parallel-make.patch";
+        url = mkUrl "7389dc990c67d4269f3a79c924c054e87f2e4ac5";
+        sha256 = "0mvdvyy76kpaxacljidf06wd43fr2qripr4mwsakjs3hxb1pkk57";
+      })
+    ];
 
   nativeBuildInputs = [ cmake pkgconfig python ];
   propagatedBuildInputs = [ qtbase dbus_glib telepathy_farstream telepathy_glib pythonDBus ];
@@ -26,7 +34,7 @@ stdenv.mkDerivation rec {
     NIX_CFLAGS_COMPILE+=" `pkg-config --cflags dbus-glib-1`"
   '';
 
-  enableParallelBuilding = false; # missing _gen/future-constants.h
+  enableParallelBuilding = true;
   doCheck = false; # giving up for now
 
   meta = {
