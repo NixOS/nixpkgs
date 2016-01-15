42d4175e4edfc17b75b445bad8d8f8c6822ef36b
Simon Jagoe <simon@simonjagoe.com>
2016-01-15 19:32:23 +0200

    kernel: 4.1.13 -> 4.1.15 (close #12408)

--- pkgs/os-specific/linux/kernel/linux-4.1.nix
+++ pkgs/os-specific/linux/kernel/linux-4.1.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.1.13";
+  version = "4.1.15";
   extraMeta.branch = "4.1";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "04xhkqjpb1rfqkq6hxzfma5cr039p1fad2fgims5ns09nvalq6dr";
+    sha256 = "18sr0dl5ax6pcx6nqp9drb4l6a38g07vxihiqpbwb231jv68h8j7";
   };
 
   features.iwlwifi = true;
