01915ceeb7709ebf3be90b8f704fbe8f04118fe9
Danny Wilson <danny@prime.vc>
2016-01-19 15:59:10 +0100

    glib: fix compilation on SmartOS/Illumos
    Recent illumos includes a linux-incompatible `inotify.h` header, which configure detects: compilation fails.
    Also, a newer `dtrace` on SmartOS fails creating the probes ELF linkable object (with `dtrace -G`). Disable for now.
    
    Remove old configure option `--disable-modular-tests`.

--- pkgs/development/libraries/glib/default.nix
+++ pkgs/development/libraries/glib/default.nix
@@ -68,11 +68,17 @@ stdenv.mkDerivation rec {
 
   configureFlags =
     optional stdenv.isDarwin "--disable-compile-warnings"
-    ++ optional stdenv.isFreeBSD "--with-libiconv=gnu"
-    ++ optional stdenv.isSunOS ["--disable-modular-tests" "--with-libiconv"];
+    ++ optional (stdenv.isFreeBSD || stdenv.isSunOS) "--with-libiconv=gnu"
+    ++ optional stdenv.isSunOS "--disable-dtrace";
 
   NIX_CFLAGS_COMPILE = optionalString stdenv.isDarwin " -lintl"
     + optionalString stdenv.isSunOS " -DBSD_COMP";
+
+  preConfigure = if stdenv.isSunOS then
+    ''
+      sed -i -e 's|inotify.h|foobar-inotify.h|g' configure
+    ''
+    else null;
 
   preBuild = optionalString stdenv.isDarwin
     ''
