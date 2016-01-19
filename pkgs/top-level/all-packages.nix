69ba6c1d30d3b891e4fcda8544ddc5763bf2b6d0
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2016-01-20 00:54:38 +0200

    mmc-utils: init at 2015-11-18, fixes #12489

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10369,6 +10369,8 @@ let
 
   mkinitcpio-nfs-utils = callPackage ../os-specific/linux/mkinitcpio-nfs-utils { };
 
+  mmc-utils = callPackage ../os-specific/linux/mmc-utils { };
+
   module_init_tools = callPackage ../os-specific/linux/module-init-tools { };
 
   aggregateModules = modules:
