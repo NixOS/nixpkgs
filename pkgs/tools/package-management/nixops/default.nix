e775690e5cebb438ecb2522d7b2ba993e19dfd6a
Rob Vermaas <rob.vermaas@gmail.com>
2016-01-19 09:28:52 +0000

    Fix nixops URL

--- pkgs/tools/package-management/nixops/default.nix
+++ pkgs/tools/package-management/nixops/default.nix
@@ -3,7 +3,7 @@
 callPackage ./generic.nix (rec {
   version = "1.3.1";
   src = fetchurl {
-    url = "http://nixos.org/releases/nixops/nixops/nixops-${version}.tar.bz2";
+    url = "http://nixos.org/releases/nixops/nixops-${version}/nixops-${version}.tar.bz2";
     sha256 = "04j8s0gg1aj3wmj1bs7dwscfmlzk2xpwfw9rk4xzxwxw1y0j64nd";
   };
 })
