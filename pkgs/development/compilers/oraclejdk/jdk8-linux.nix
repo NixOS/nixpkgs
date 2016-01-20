b19d702b6575daf686cbec28c3de6f9d24a1cd9e
Rok Garbas <rok@garbas.si>
2016-01-20 16:10:36 +0100

    oraclejdk: 8u65, 8u66 -> 8u71, 8u72, fixes #12498

--- pkgs/development/compilers/oraclejdk/jdk8-linux.nix
+++ pkgs/development/compilers/oraclejdk/jdk8-linux.nix
@@ -1,9 +1,9 @@
 import ./jdk-linux-base.nix {
   productVersion = "8";
-  patchVersion = "72";
+  patchVersion = "71";
   downloadUrl = http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html;
-  sha256_i686 = "023rnlm5v7d9w4d7bgcac8j0r1vqkbv6fl20k8354pzpdjg6liaq";
-  sha256_x86_64 = "167ca39a6y0n8l87kdm5nv2hrp0wf6g4mw1rbychjc04f5igkrs6";
+  sha256_i686 = "11wcizv4gvlffzn2wj34ffwrq21xwh4ikg2vjv63avdfp2hazjqv";
+  sha256_x86_64 = "18jqdrlbv4sdds2hlmp437waq7r9b33f7hdp8kb6l7pkrizr9nwv";
   jceName = "jce_policy-8.zip";
   jceDownloadUrl = http://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html;
   sha256JCE = "0n8b6b8qmwb14lllk2lk1q1ahd3za9fnjigz5xn65mpg48whl0pk";
