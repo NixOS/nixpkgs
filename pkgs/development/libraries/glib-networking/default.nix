68768baf5d358b5ca49d19dde57a761391965db6
Vladimír Čunát <vcunat@gmail.com>
2016-01-21 12:04:16 +0100

    glib-networking: fix certificate problems
    ... by upstream patch. Discussed on
    https://github.com/NixOS/nixpkgs/issues/12518#issuecomment-173531178
    https://bugzilla.redhat.com/show_bug.cgi?id=1284655

--- pkgs/development/libraries/glib-networking/default.nix
+++ pkgs/development/libraries/glib-networking/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, glib, intltool, gnutls, libproxy
+{ stdenv, fetchurl, fetchpatch, pkgconfig, glib, intltool, gnutls, libproxy
 , gsettings_desktop_schemas }:
 
 let
@@ -12,6 +12,12 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/glib-networking/${ver_maj}/${name}.tar.xz";
     sha256 = "8f8a340d3ba99bfdef38b653da929652ea6640e27969d29f7ac51fbbe11a4346";
   };
+  patches = [(fetchpatch {
+    name = "fix-certificates.patch";
+    url = "https://git.gnome.org/browse/glib-networking/patch"
+      + "/?id=44d02b8c57e24f0a796d75b6270ecee265f35f6f";
+    sha256 = "0kfvk4ci786hca794c1c3c8ihh9lgp7qrj887c28ik5ndm4w55i3";
+  })];
 
   configureFlags = "--with-ca-certificates=/etc/ssl/certs/ca-certificates.crt";
 
