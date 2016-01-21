50cc9421a2d251a2997930177762e8000d1cec68
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2016-01-21 22:59:47 +0100

    pythonPackages.ed25519: init at 1.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -25009,4 +25009,21 @@ in modules // {
       maintainer = with maintainers; [ np ];
     };
   };
+
+  ed25519 = buildPythonPackage rec {
+    name = "ed25519-${version}";
+    version = "1.4";
+
+    src = pkgs.fetchurl {
+      url = "http://pypi.python.org/packages/source/e/ed25519/${name}.tar.gz";
+      sha256 = "0ahx1nkxa0xis3cw0h5c4fpgv8mq4znkq7kajly33lc3317bk499";
+    };
+
+    meta = {
+      description = "Ed25519 public-key signatures";
+      homepage = "https://github.com/warner/python-ed25519";
+      license = licenses.mit;
+      maintainers = with maintainers; [ np ];
+    };
+  };
 }
