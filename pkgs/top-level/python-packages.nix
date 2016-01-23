c882af9cf3514157912edd1426bc6fda5f1aadc1
Frederik Rietdijk <fridh@fridh.nl>
2016-01-23 21:22:03 +0100

    pythonPackages.pystache: fix test runner

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16166,6 +16166,18 @@ in modules // {
       sha256 = "f7bbc265fb957b4d6c7c042b336563179444ab313fb93a719759111eabd3b85a";
     };
 
+    LC_ALL = "en_US.UTF-8";
+
+    buildInputs = [ pkgs.glibcLocales ];
+
+    checkPhase = ''
+      ${python.interpreter} -m unittest discover
+    '';
+
+    # SyntaxError Python 3
+    # https://github.com/defunkt/pystache/issues/181
+    disabled = isPy3k;
+
     meta = {
       description = "A framework-agnostic, logic-free templating system inspired by ctemplate and et";
       homepage = https://github.com/defunkt/pystache;
