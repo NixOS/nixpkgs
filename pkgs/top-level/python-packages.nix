3b2e3355db9726a9656761024d3b9c9645640263
Frederik Rietdijk <fridh@fridh.nl>
2016-01-24 09:45:56 +0100

    pythonPackages.traits: disable tests
    Broken test suite.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -20123,6 +20123,18 @@ in modules // {
       md5 = "3ad558eebaedc63c29c80183c0371d2f";
     };
 
+    # Use pytest because its easier to discover tests
+    buildInputs = with self; [ pytest ];
+    checkPhase = ''
+      py.test $out/${python.sitePackages}
+    '';
+
+    # Test suite is broken for 3.x on latest release
+    # https://github.com/enthought/traits/issues/187
+    # https://github.com/enthought/traits/pull/188
+    # Furthermore, some tests fail due to being in a chroot
+    doCheck = false;
+
     propagatedBuildInputs = with self; [ numpy ];
 
     meta = {
