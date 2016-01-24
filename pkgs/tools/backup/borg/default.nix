10229032a9799eeca0068b7bd1e05ab1d127c648
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2016-01-24 04:02:03 +0100

    borg: 0.29.0 -> 0.30.0
    Compatibility notes:
    
    - you may need to use -v (or --info) more often to actually see
      output emitted at INFO log level (because it is suppressed at
      the default WARNING log level). See the general section in the
      usage docs.
    - for borg create, you need --list (additionally to -v) to see
      the long file list (was needed so you can have e.g. --stats
      alone without the long list)
    - see link below about BORG_DELETE_I_KNOW_WHAT_I_AM_DOING
      (was: BORG_CHECK_I_KNOW_WHAT_I_AM_DOING)
    
    More: https://github.com/borgbackup/borg/blob/0.30.0/docs/changes.rst

--- pkgs/tools/backup/borg/default.nix
+++ pkgs/tools/backup/borg/default.nix
@@ -2,12 +2,12 @@
 
 python3Packages.buildPythonPackage rec {
   name = "borgbackup-${version}";
-  version = "0.29.0";
+  version = "0.30.0";
   namePrefix = "";
 
   src = fetchurl {
     url = "https://pypi.python.org/packages/source/b/borgbackup/borgbackup-${version}.tar.gz";
-    sha256 = "1gvx036a7j16hd5rg8cr3ibiig7gwqhmddrilsakcw4wnfimjy5m";
+    sha256 = "0n78c982kdfqbyi9jawcvzgdik4l36c2s7rpzkfr1ka6506k2rx4";
   };
 
   propagatedBuildInputs = with python3Packages;
