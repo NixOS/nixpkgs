ed016c3e1519b0be6871601f6004c4fa00b59478
Christian Theune <ct@flyingcircus.io>
2016-01-25 18:23:06 +0100

    systemd: make ctrl-alt-del target configurable.
    We currently only allow upstream's default of "reboot.target" due to the
    way the symlinks are initialized. I made this configurable similar to the
    default unit.

--- nixos/modules/system/boot/systemd.nix
+++ nixos/modules/system/boot/systemd.nix
@@ -120,7 +120,6 @@ let
       "systemd-poweroff.service"
       "halt.target"
       "systemd-halt.service"
-      "ctrl-alt-del.target"
       "shutdown.target"
       "umount.target"
       "final.target"
@@ -161,7 +160,6 @@ let
       "systemd-localed.service"
       "systemd-hostnamed.service"
     ]
-
     ++ cfg.additionalUpstreamSystemUnits;
 
   upstreamSystemWants =
@@ -475,6 +473,12 @@ in
       default = "multi-user.target";
       type = types.str;
       description = "Default unit started when the system boots.";
+    };
+
+    systemd.ctrl-alt-del = mkOption {
+      default = "reboot.target";
+      type = types.str;
+      description = "Target that should be started when Ctrl-Alt-Delete is pressed.";
     };
 
     systemd.globalEnvironment = mkOption {
