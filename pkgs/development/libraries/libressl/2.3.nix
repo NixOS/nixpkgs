edad608f56ab8cb2d74f2d34e7c0f625e706a5b1
Robin Gloster <mail@glob.in>
2016-01-29 03:41:14 +0000

    libressl_2_3: 2.3.1 -> 2.3.2

--- pkgs/development/libraries/libressl/2.3.nix
+++ pkgs/development/libraries/libressl/2.3.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "libressl-${version}";
-  version = "2.3.1";
+  version = "2.3.2";
 
   src = fetchurl {
     url    = "mirror://openbsd/LibreSSL/${name}.tar.gz";
-    sha256 = "410b58db4ebbcab43c3357612e591094f64fb9339269caa2e68728e36f8d589e";
+    sha256 = "0sm9cjjqvj581sfd4sh0i467sh8p89nq9b8ck2qn3war92p5zx40";
   };
 
   enableParallelBuilding = true;
@@ -15,6 +15,6 @@ stdenv.mkDerivation rec {
     description = "Free TLS/SSL implementation";
     homepage    = "http://www.libressl.org";
     platforms   = platforms.all;
-    maintainers = with maintainers; [ thoughtpolice wkennington fpletz ];
+    maintainers = with maintainers; [ thoughtpolice wkennington fpletz globin ];
   };
 }
