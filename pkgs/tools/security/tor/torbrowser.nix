41204b1dd183b0edbaa2f1045469ce5bd34f753d
Mayeu (Cast) <Mayeu@users.noreply.github.com>
2016-01-29 11:14:44 +0100

    torbrowser: 5.0.7 -> 5.5
    Hash found with nix-prefetch-url

--- pkgs/tools/security/tor/torbrowser.nix
+++ pkgs/tools/security/tor/torbrowser.nix
@@ -16,13 +16,13 @@ let
 
 in stdenv.mkDerivation rec {
   name = "tor-browser-${version}";
-  version = "5.0.7";
+  version = "5.5";
 
   src = fetchurl {
     url = "https://archive.torproject.org/tor-package-archive/torbrowser/${version}/tor-browser-linux${if stdenv.is64bit then "64" else "32"}-${version}_en-US.tar.xz";
     sha256 = if stdenv.is64bit then
-      "0igqh02bcdr0b4m6df46l1l1z38d4nh4pyfn6jqsvahacdl2qbsg" else
-      "1401j340348rqwd5c1m8hnxw7qkwpzqfa64q01cp08lz9cxxml4r";
+      "0glv2zffls1as71idbfg3l34kmsv48f3sk59swl6k8l75nvxlzjk" else
+      "0xbsixxs1hj0ydmazgi796xgvlsvbrkh8vfgaiyqcvgx4vf4ggwf";
   };
 
   desktopItem = makeDesktopItem {
