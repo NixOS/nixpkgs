8491d0d1ca993a186d170ee8a8fffd5bc0d0521c
Tony White <white.tw.tw@gmail.com>
2016-01-31 12:22:20 +0000

    chromium: 47.0.2526.106 - > 48.0.2564.97
    - Fixes CVE-2016-1612 CVE-2016-1613 CVE-2016-1614 CVE-2016-1615
      CVE-2016-1616 CVE-2016-1617 CVE-2016-1618 CVE-2016-1619 CVE-2016-1620.
    - Moves chromium stable and beta channels up one version major.
      vcunat made dev channel stay for now, as it wouldn't download otherwise.
      This is most of PR #12717.

--- pkgs/applications/networking/browsers/chromium/source/sources.nix
+++ pkgs/applications/networking/browsers/chromium/source/sources.nix
@@ -7,15 +7,15 @@
     sha256bin64 = "1l5s504mmr4v5y67fsla9hyrk7dm155yc790ld87s1ya03h455gr";
   };
   beta = {
-    version = "48.0.2564.48";
-    sha256 = "1qasdz9rwdqa2if8y4kkk19fspgjpk3q782c8l1p5n2cm25lkfa9";
-    sha256bin32 = "0pgc3iy4s7whmckc14r0cvgmk7pqgidd2w0xv53bfq2k504b5ykw";
-    sha256bin64 = "1lx6l8s2wdgzldbm8licpf22l6z13mvq05xlbn8ybizhn159nykj";
+    version = "49.0.2623.28";
+    sha256 = "1cpgfcw6kgw70k14wa1m5qzj4rnvkzrw1am30d34516mbwlmmqc8";
+    sha256bin32 = "0nwhjav1x325h1drqknrjl5glivn4fdpahs1nlaqfk8kbjh1q113";
+    sha256bin64 = "0wj6j0v3rngsclzbb8rc8l90wdi98j2sb60r2ypfihaxq2gd138y";
   };
   stable = {
-    version = "47.0.2526.106";
-    sha256 = "1rjqkawj7l127cd9s1wxwn4v7dfbrlahml98sam3fg8a6drm0lv4";
-    sha256bin32 = "14svd5x8lzfyyigfirxx1q9pg2bsnnb0fdwa7vvh1awq3zzfd38n";
-    sha256bin64 = "121289vwaz391b32hp2lihlpaql76y44gdpqkl9a3md0j1zk1nd9";
+    version = "48.0.2564.97";
+    sha256 = "1nsjn8zlqljizy5dmg2rxwkw4k5j4yi15ywxi9ca3acv68dbhd3p";
+    sha256bin32 = "0gdd9krsfsixw5b03msm7nf7zl7xh4chzkh9srb19c1kzygl2j2l";
+    sha256bin64 = "1wmw17gdq1j6vxfdc8s94jc8z30pza8nipk4dh590xnb7faihr52";
   };
 }
