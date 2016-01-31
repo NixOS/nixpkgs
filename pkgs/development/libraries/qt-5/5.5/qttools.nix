2efbf665a381130c2dafee4736224c1b92c32854
Thomas Tuegel <ttuegel@gmail.com>
2016-01-31 09:52:07 -0600

    qt55.qttools: move runtime executables to $out

--- pkgs/development/libraries/qt-5/5.5/qttools.nix
+++ pkgs/development/libraries/qt-5/5.5/qttools.nix
@@ -4,6 +4,9 @@ qtSubmodule {
   name = "qttools";
   qtInputs = [ qtbase ];
   postFixup = ''
+    moveToOutput "bin/qdbus" "$out"
+    moveToOutput "bin/qtpaths" "$out"
+
     fixQtModuleCMakeConfig "Designer"
     fixQtModuleCMakeConfig "Help"
     fixQtModuleCMakeConfig "LinguistTools"
