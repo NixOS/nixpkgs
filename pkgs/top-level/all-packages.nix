5b8e6aaa219390c7e03d984150c7ac3874922fe7
Michiel Leenaars <michiel@staff.isoc.nl>
2016-02-01 17:47:09 +0100

    libcutl: init -> 1.9.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7147,6 +7147,8 @@ let
 
   libcue = callPackage ../development/libraries/libcue { };
 
+  libcutl = callPackage ../development/libraries/libcutl { };
+
   libdaemon = callPackage ../development/libraries/libdaemon { };
 
   libdbi = callPackage ../development/libraries/libdbi { };
