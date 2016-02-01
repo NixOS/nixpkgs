b5b78055431157456703742cfec770f2ff3e6675
Rickard Nilsson <rickynils@gmail.com>
2016-02-01 10:37:41 +0100

    nixos-rebuild: Do not create result symlinks for boot/switch actions (resolves #12665)

--- nixos/modules/installer/tools/nixos-rebuild.sh
+++ nixos/modules/installer/tools/nixos-rebuild.sh
@@ -329,7 +329,7 @@ fi
 if [ -z "$rollback" ]; then
     echo "building the system configuration..." >&2
     if [ "$action" = switch -o "$action" = boot ]; then
-        pathToConfig="$(nixBuild '<nixpkgs/nixos>' -A system "${extraBuildFlags[@]}")"
+        pathToConfig="$(nixBuild '<nixpkgs/nixos>' --no-out-link -A system "${extraBuildFlags[@]}")"
         copyToTarget "$pathToConfig"
         targetHostCmd nix-env -p "$profile" --set "$pathToConfig"
     elif [ "$action" = test -o "$action" = build -o "$action" = dry-build -o "$action" = dry-activate ]; then
