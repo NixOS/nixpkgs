2fbd2995c8fe59707857b0c051df71dc934c380d
Jeffrey David Johnson <jefdaj@gmail.com>
2016-01-31 20:48:28 -0800

    update bitcoin-xt and add bitcoin-classic

--- pkgs/applications/altcoins/bitcoin-classic.nix
+++ pkgs/applications/altcoins/bitcoin-classic.nix
@@ -21,20 +21,19 @@ stdenv.mkDerivation rec {
                      ++ optionals withGui [ "--with-gui=qt4" ];
 
   meta = {
-    description = "We are hard forking bitcoin to a 2 MB blocksize limit. Please join us.";
+    description = "Peer-to-peer electronic cash system (Classic client)";
     longDescription= ''
-      The data show consensus amongst miners for an immediate 2 MB increase, and
-      demand amongst users for 8 MB or more. We are writing the software that miners
-      and users say they want. We will make sure that it solves their needs, help
-      them deploy it, and gracefully upgrade the bitcoin network’s capacity together.
+      Bitcoin is a free open source peer-to-peer electronic cash system that is
+      completely decentralized, without the need for a central server or trusted
+      parties. Users hold the crypto keys to their own money and transact directly
+      with each other, with the help of a P2P network to check for double-spending.
 
       We call our code repository Bitcoin Classic. It starts as a one-feature patch
-      to bitcoin-core that increases the blocksize limit to 2 MB. We will have ports
-      for master and 0.11.2, so that miners and businesses can upgrade to 2 MB blocks
-      from any recent bitcoin software version they run.
-
-      In the future we will continue to release updates that are in line with
-      Satoshi’s whitepaper & vision, and are agreed upon by the community.
+      to bitcoin-core that increases the blocksize limit to 2 MB. We will have
+      ports for master and 0.11.2, so that miners and businesses can upgrade to 2 MB
+      blocks from any recent bitcoin software version they run. In the future we will
+      continue to release updates that are in line with Satoshi’s whitepaper &
+      vision, and are agreed upon by the community.
     '';
     homepage = "https://bitcoinclassic.com/";
     maintainers = with maintainers; [ jefdaj ];
--- pkgs/applications/altcoins/bitcoin-xt.nix
+++ pkgs/applications/altcoins/bitcoin-xt.nix
@@ -23,18 +23,21 @@ stdenv.mkDerivation rec{
   ] ++ optionals withGui [ "--with-gui=qt4" ];
 
   meta = {
-    description = "Peer-to-peer electronic cash system";
+    description = "Peer-to-peer electronic cash system (XT client)";
     longDescription= ''
-       Bitcoin XT is an implementation of a Bitcoin full node, based upon the
-       source code of Bitcoin Core. It is built by taking the latest stable
-       Core release, applying a series of patches, and then doing deterministic
-       builds so anyone can check the downloads correspond to the source code. 
+      Bitcoin is a free open source peer-to-peer electronic cash system that is
+      completely decentralized, without the need for a central server or trusted
+      parties. Users hold the crypto keys to their own money and transact directly
+      with each other, with the help of a P2P network to check for double-spending.
 
-      Maintainer's note: Nix doesn't use the official deterministic builds.
+      Bitcoin XT is an implementation of a Bitcoin full node, based upon the
+      source code of Bitcoin Core. It is built by taking the latest stable
+      Core release, applying a series of patches, and then doing deterministic
+      builds so anyone can check the downloads correspond to the source code. 
     '';
     homepage = "https://bitcoinxt.software/";
     maintainers = with maintainers; [ jefdaj ];
     license = licenses.mit;
     platforms = platforms.unix;
   };
-}
+
