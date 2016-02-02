7e4f0b046ed848860b81e8236753bf146e70e488
Franz Pletz <fpletz@fnordicwalking.de>
2016-02-02 21:05:44 +0100

    darling-dmg: cleanup
    Version was wrong. Bumped to current version 1.0.4.
    
    See #11561 for details.

--- pkgs/tools/filesystems/darling-dmg/default.nix
+++ pkgs/tools/filesystems/darling-dmg/default.nix
@@ -1,16 +1,20 @@
-{ stdenv, fetchFromGitHub, lib, cmake, fuse, zlib, bzip2, openssl, libxml2, icu } :
+{ stdenv, fetchFromGitHub, cmake, fuse, zlib, bzip2, openssl, libxml2, icu } :
 
 stdenv.mkDerivation rec {
   name = "darling-dmg-${version}";
-  version = "1.0";
+  version = "1.0.4";
+
   src = fetchFromGitHub {
     owner = "darlinghq";
     repo = "darling-dmg";
-    rev = "9522e3907b82f6cde141b3e0e0063f09c8710cbf";
-    sha256 = "5d968b0609f9bfbecc26753d21b5182e15183b672967f7dec6038404cd6a6d7f";
+    rev = "v${version}";
+    sha256 = "0x285p16zfnp0p6injw1frc8krif748sfgxhdd7gb75kz0dfbkrk";
   };
+
   buildInputs = [ cmake fuse openssl zlib bzip2 libxml2 icu ];
+
   cmakeConfigureFlagFlags = ["-DCMAKE_BUILD_TYPE=RELEASE"];
+
   meta = {
     homepage = http://www.darlinghq.org/;
     description = "Darling lets you open OS X dmgs on Linux";
