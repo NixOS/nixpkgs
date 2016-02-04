7cc2756f31481a81343eaa62ac39fb30eed67d38
Tim Steinbach <tsteinbach@trustwave.com>
2016-02-04 11:31:12 -0500

    kotlin: 1.0.0-beta-4584 -> 1.0.0-rc-1036

--- pkgs/development/compilers/kotlin/default.nix
+++ pkgs/development/compilers/kotlin/default.nix
@@ -1,15 +1,15 @@
-{ stdenv, fetchurl, makeWrapper, jre, unzip, which }:
+{ stdenv, fetchurl, makeWrapper, jre, unzip }:
 
 stdenv.mkDerivation rec {
-  version = "1.0.0-beta-4584";
+  version = "1.0.0-rc-1036";
   name = "kotlin-${version}";
 
   src = fetchurl {
     url = "https://github.com/JetBrains/kotlin/releases/download/build-${version}/kotlin-compiler-${version}.zip";
-    sha256 = "191698784f4a1f769df14757bad40243293aa6bc3d8e906bfa0c80c3db0a1d2c";
+    sha256 = "8d72ff925678a2172fbbefcb6f0c5a195a300dccf240e5add16c3e8d86e13d67";
   };
 
-  propagatedBuildInputs = [ jre which ] ;
+  propagatedBuildInputs = [ jre ] ;
   buildInputs = [ makeWrapper unzip ] ;
 
   installPhase = ''
