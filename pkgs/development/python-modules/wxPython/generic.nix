e353185cebc483e3f16993a9f5935a6c91977caa
Robin Gloster <mail@glob.in>
2016-02-07 21:17:34 +0000

    wxPython: turn off format hardening

--- pkgs/development/python-modules/wxPython/generic.nix
+++ pkgs/development/python-modules/wxPython/generic.nix
@@ -11,6 +11,10 @@ stdenv.mkDerivation rec {
   disabled = isPy3k || isPyPy;
   doCheck = false;
 
+  sourceRoot = "wxPython-src-${version}/wxPython";
+
+  hardening_format = false;
+
   src = fetchurl {
     url = "mirror://sourceforge/wxpython/wxPython-src-${version}.tar.bz2";
     inherit sha256;
@@ -18,7 +22,6 @@ stdenv.mkDerivation rec {
 
   pythonPath = [ python setuptools ];
   buildInputs = [ python setuptools pkgconfig wxGTK (wxGTK.gtk) wrapPython ]  ++ stdenv.lib.optional openglSupport pyopengl;
-  preConfigure = "cd wxPython";
 
   installPhase = ''
     ${python.interpreter} setup.py install WXPORT=gtk2 NO_HEADERS=1 BUILD_GLCANVAS=${if openglSupport then "1" else "0"} UNICODE=1 --prefix=$out
