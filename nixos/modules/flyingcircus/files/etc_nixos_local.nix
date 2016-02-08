c8b57f81433d2b9cd9ed2e9036651a576db618ec
Christian Zagrodnick <cz@flyingcircus.io>
2016-02-08 12:18:47 +0100

    Add some examples to local.nix

--- nixos/modules/flyingcircus/files/etc_nixos_local.nix
+++ nixos/modules/flyingcircus/files/etc_nixos_local.nix
@@ -1,4 +1,25 @@
 # Add manual, temporary configuration here.
 { ... }:
 {
+
+#    flyingcircus.roles.dovecot.enable = false;
+#    flyingcircus.roles.haproxy.enable = false;
+#    flyingcircus.roles.mysql.enable = false;
+#    flyingcircus.roles.nginx.enable = false;
+#    flyingcircus.roles.postgresql93.enable = false;
+#    flyingcircus.roles.varnish.enable = false;
+#
+#    flyingcircus.compat.gentoo.enable = false;
+#
+#    # Load ENC from file?
+#    fcio.load_enc = false;
+#
+#    # Set additional ENC values here.
+#    fcio.enc = {
+#      parameters = {
+#        directory_secret = "foo!";
+#      };
+#    };
+#
+
 }
