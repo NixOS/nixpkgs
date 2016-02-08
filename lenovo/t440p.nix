ad5412b9f2460ae17e1defbcfb682995f563830f
Nikolay Amiantov <ab@fmap.me>
2016-02-08 15:12:20 +0300

    lenovo/t440p.nix: init

---
+++ lenovo/t440p.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+
+{
+  boot = {
+    extraModprobeConfig = ''
+      options bbswitch use_acpi_to_detect_card_state=1
+    '';
+    kernelModules = [ "kvm-intel" "tpm-rng" ];
+  };
+}
