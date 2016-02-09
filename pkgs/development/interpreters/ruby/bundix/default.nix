6a3d2e8812b6c43bb20b9f9625c524091fd00551
Michael Fellinger <m.fellinger@gmail.com>
2016-02-09 23:47:41 +0100

    bundix: 1.0.4 -> 2.0.4

--- pkgs/development/interpreters/ruby/bundix/default.nix
+++ pkgs/development/interpreters/ruby/bundix/default.nix
@@ -1,20 +1,11 @@
-{ ruby, fetchgit, buildRubyGem, bundler }:
+{ buildRubyGem, makeWrapper, ruby, coreutils, bundler }:
 
-let
-  thor = buildRubyGem {
-    gemName = "thor";
-    version = "0.19.1";
-    type = "gem";
-    sha256 = "08p5gx18yrbnwc6xc0mxvsfaxzgy2y9i78xq7ds0qmdm67q39y4z";
-  };
-
-in buildRubyGem {
+buildRubyGem rec {
+  inherit ruby;
+  buildInputs = [bundler];
+  name = "${gemName}-${version}";
   gemName = "bundix";
-  version = "1.0.4";
-  gemPath = [ thor bundler ];
-  src = fetchgit {
-    url = "https://github.com/cstrahan/bundix.git";
-    rev = "6dcf1f71c61584f5c9b919ee9df7b0c554862076";
-    sha256 = "1w17bvc9srcgr4ry81ispcj35g9kxihbyknmqp8rnd4h5090b7b2";
-  };
+  version = "2.0.4";
+  sha256 = "0i7fdxi6w29yxnblpckczazb79m5x03hja8sfnabndg4yjc868qs";
+  dontPatchShebangs = true;
 }
