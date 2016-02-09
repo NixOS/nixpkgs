877dc973ab76ad73c371a87285fc294a7edb3f41
tg(x) <*@tg-x.net>
2016-02-09 23:41:01 +0100

    wayland window managers: orbment, sway, velox -> latest git

--- pkgs/development/libraries/swc/default.nix
+++ pkgs/development/libraries/swc/default.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation rec {
   name = "swc-${version}";
-  version = "git-2015-09-05";
+  version = "git-2016-02-09";
   repo = "https://github.com/michaelforney/swc";
-  rev = "0dff35ad9b80fc62e6b48417f78c24df6648c9d2";
+  rev = "1da0ef13fddc572accea12439a4471b4d2f64ddd";
 
   src = fetchurl {
     url = "${repo}/archive/${rev}.tar.gz";
-    sha256 = "7af5655b5bb5fe59bb8e6643e35f794419850463b1d7f44f29b45ab6aee01ae9";
+    sha256 = "d1894612d8aa1ce828efb78f1570290f84bba6563e21eb777e08c3c3859b7bbe";
   };
 
   nativeBuildInputs = [ pkgconfig ];
