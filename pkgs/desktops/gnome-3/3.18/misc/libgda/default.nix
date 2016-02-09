9b597ee8a5650fa75818e38860a96b5b2b3ff532
Robin Gloster <mail@glob.in>
2016-02-09 14:09:15 +0000

    gnome3.libgda: turn off format hardening

--- pkgs/desktops/gnome-3/3.18/misc/libgda/default.nix
+++ pkgs/desktops/gnome-3/3.18/misc/libgda/default.nix
@@ -17,6 +17,8 @@ in stdenv.mkDerivation rec {
     "--enable-gi-system-install=no"
   ];
 
+  hardening_format = false;
+
   enableParallelBuilding = true;
 
   buildInputs = [ pkgconfig intltool itstool libxml2 gtk3 openssl ];
