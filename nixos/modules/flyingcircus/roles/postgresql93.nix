b3968a48cebdccb4f90e255e570058d5ceaaad1e
Christian Zagrodnick <cz@flyingcircus.io>
2016-02-10 07:19:57 +0100

    re-indent with 2space (see nix style guide)

--- nixos/modules/flyingcircus/roles/postgresql93.nix
+++ nixos/modules/flyingcircus/roles/postgresql93.nix
@@ -1,28 +1,25 @@
 { config, lib, pkgs, ... }: with lib;
 
-
 {
+  options = {
 
-    options = {
-
-        flyingcircus.roles.postgresql93 = {
-
-            enable = mkOption {
-                type = types.bool;
-                default = false;
-                description = "Enable the Flying Circus PostgreSQL server role.";
-            };
-
-        };
-
+    flyingcircus.roles.postgresql93 = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Enable the Flying Circus PostgreSQL server role.";
+      };
     };
 
-    config = mkIf config.flyingcircus.roles.postgresql93.enable {
+  };
 
-        services.postgresql.enable = true;
-        services.postgresql.package = pkgs.postgresql93;
+  config = mkIf config.flyingcircus.roles.postgresql93.enable {
 
-        services.postgresql.initialScript = ./postgresql-init.sql;
+    services.postgresql.enable = true;
+    services.postgresql.package = pkgs.postgresql93;
 
-    };
+    services.postgresql.initialScript = ./postgresql-init.sql;
+
+  };
+
 }
