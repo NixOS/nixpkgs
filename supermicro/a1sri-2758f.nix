2ead29d3d08ff6300870cc20009f7513fed69ae8
Emery Hemingway <emery@vfemail.net>
2016-02-10 12:24:29 +0100

    supermicro/a1sri-2758f: enable ipmi

--- supermicro/a1sri-2758f.nix
+++ supermicro/a1sri-2758f.nix
@@ -1,8 +1,16 @@
 # http://www.supermicro.com/products/motherboard/Atom/X10/A1SRi-2758F.cfm
+#
+# This board contains a TPM header, but you must supply your own module.
+#
 
-{ ... }:
+{ pkgs, ... }:
 
 {
-  # The Linux NIC driver doesn't properly report link status.
+  environment.systemPackages = [ pkgs.ipmitool ];
+  boot.kernelModules = [ "ipmi_devintf" "ipmi_si" ];
+
+  # The Linux NIC driver seems to have faulty link state reporting
+  # that causes dhcpcd to release every few seconds, which is
+  # more annoying than not releasing when a cable is unplugged.
   networking.dhcpcd.extraConfig = "nolink";
 }
