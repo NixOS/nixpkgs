11b9ed9e6323a82916b95538d40da2858fa99b03
Nikolay Amiantov <ab@fmap.me>
2016-02-10 19:42:31 +0300

    zathura: use mupdf by default

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14041,7 +14041,7 @@ let
   zathuraCollection = recurseIntoAttrs
     (callPackage ../applications/misc/zathura {
         callPackage = newScope pkgs.zathuraCollection;
-        useMupdf = config.zathura.useMupdf or false;
+        useMupdf = config.zathura.useMupdf or true;
       });
 
   zathura = zathuraCollection.zathuraWrapper;
