51269e32054d489e8a4e936e0c1be3662db0ecb1
Christian Zagrodnick <cz@flyingcircus.io>
2016-02-10 10:44:09 +0100

    set wal buffer configuration and compute size by enc memory
    re #20037

--- nixos/modules/flyingcircus/roles/postgresql93.nix
+++ nixos/modules/flyingcircus/roles/postgresql93.nix
@@ -1,5 +1,33 @@
 { config, lib, pkgs, ... }: with lib;
 
+let
+  cfg = config.flyingcircus;
+
+  min = list:
+    builtins.head (builtins.sort builtins.lessThan list);
+  max = list:
+    lib.last (builtins.sort builtins.lessThan list);
+
+
+  # return the "current" system memory in MiB. We currently use the value from
+  # the ENC but should use the actual system memory.
+  current_memory =
+    if lib.hasAttrByPath ["parameters" "memory"] cfg.enc
+    then cfg.enc.parameters.memory
+    else 256;
+
+  shared_memory_max = current_memory / 2 * 1048576;
+
+  shared_buffers =
+    min [
+      (max [16 (current_memory / 4)])
+       (shared_memory_max * 4 / 5)];
+
+  wal_buffers =
+    max [
+      (min [64 (shared_buffers / 32)])
+      1];
+in
 {
   options = {
 
@@ -37,6 +65,27 @@
       %service ALL=(postgres) ALL
     '';
 
+    # System tweaks
+    boot.kernel.sysctl = {
+      "kernel.shmmax" = toString shared_memory_max;
+      "kernel.shmall" = toString (shared_memory_max / 4096);
+    };
+
+
+    # Custom postgresql configuration
+    services.postgresql.extraConfig = ''
+      #------------------------------------------------------------------------------
+      # WRITE AHEAD LOG
+      #------------------------------------------------------------------------------
+      wal_level = hot_standby
+      wal_buffers = ${toString wal_buffers}MB
+      checkpoint_segments = 100
+      checkpoint_completion_target = 0.9
+      archive_mode = on
+      archive_command = '/usr/local/sbin/postgresql-archive-log %p'
+      archive_timeout = 60
+
+    '';
   };
 
 }
