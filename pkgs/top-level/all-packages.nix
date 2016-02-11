8a35315a2000336f0f785022cd56968889d8f1e0
Jakob Gillich <jakob@gillich.me>
2016-02-11 19:49:33 +0100

    gollum: init at 4.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12225,6 +12225,8 @@ let
     inherit (gnome) GConf;
   };
 
+  gollum = callPackage ../applications/misc/gollum { };
+
   google-chrome = callPackage ../applications/networking/browsers/google-chrome { gconf = gnome.GConf; };
 
   googleearth = callPackage_i686 ../applications/misc/googleearth { };
