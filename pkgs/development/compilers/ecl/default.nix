d0c38a0cef9faf2d47492286f1997848a6b9db59
Robin Gloster <mail@glob.in>
2016-02-12 01:01:37 +0000

    ecl: turn off format hardening

--- pkgs/development/compilers/ecl/default.nix
+++ pkgs/development/compilers/ecl/default.nix
@@ -1,47 +1,45 @@
 {stdenv, fetchurl
 , libtool, autoconf, automake
 , gmp, mpfr, libffi
-, noUnicode ? false, 
+, noUnicode ? false,
 }:
+
 let
-  s = # Generated upstream information
-  rec {
-    baseName="ecl";
-    version="16.0.0";
-    name="${baseName}-${version}";
-    hash="0czh78z9i5b7jc241mq1h1gdscvdw5fbhfb0g9sn4rchwk1x8gil";
-    url="https://common-lisp.net/project/ecl/files/ecl-16.0.0.tgz";
-    sha256="0czh78z9i5b7jc241mq1h1gdscvdw5fbhfb0g9sn4rchwk1x8gil";
-  };
-  buildInputs = [
-    libtool autoconf automake
-  ];
-  propagatedBuildInputs = [
-    libffi gmp mpfr
-  ];
+  baseName = "ecl";
+  version = "16.0.0";
 in
 stdenv.mkDerivation {
-  inherit (s) name version;
-  inherit buildInputs propagatedBuildInputs;
+  name = "${baseName}-${version}";
+  inherit version;
+
   src = fetchurl {
-    inherit (s) url sha256;
+    url = "https://common-lisp.net/project/ecl/files/ecl-16.0.0.tgz";
+    sha256 = "0czh78z9i5b7jc241mq1h1gdscvdw5fbhfb0g9sn4rchwk1x8gil";
   };
+
   configureFlags = [
     "--enable-threads"
     "--with-gmp-prefix=${gmp}"
     "--with-libffi-prefix=${libffi}"
-    ]
-    ++
-    (stdenv.lib.optional (! noUnicode)
-      "--enable-unicode")
-    ;
+  ] ++ (stdenv.lib.optional (!noUnicode) "--enable-unicode");
+
+  buildInputs = [
+    libtool autoconf automake
+  ];
+
+  propagatedBuildInputs = [
+    libffi gmp mpfr
+  ];
+
+  hardening_format = false;
+
   postInstall = ''
     sed -e 's/@[-a-zA-Z_]*@//g' -i $out/bin/ecl-config
   '';
+
   meta = {
-    inherit (s) version;
     description = "Lisp implementation aiming to be small, fast and easy to embed";
-    license = stdenv.lib.licenses.mit ;
+    license = stdenv.lib.licenses.mit;
     maintainers = [stdenv.lib.maintainers.raskin];
     platforms = stdenv.lib.platforms.linux;
   };
