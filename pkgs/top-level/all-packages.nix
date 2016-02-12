ff9e73876da8e317e54630fe0be715c9776be21e
= <anothersms@gmail.com>
2016-02-12 20:49:26 +0100

    timemachine: init at 0.3.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3325,6 +3325,8 @@ let
 
   tiled = qt5.callPackage ../applications/editors/tiled { };
 
+  timemachine = callPackage ../applications/audio/timemachine { };
+
   tinc = callPackage ../tools/networking/tinc { };
 
   tinc_pre = callPackage ../tools/networking/tinc/pre.nix { };
