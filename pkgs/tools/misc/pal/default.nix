37cd2e6e21426db31a28c4fe9c15fcfd6a2ff121
Robin Gloster <mail@glob.in>
2016-02-12 13:53:04 +0000

    pal: turn off format hardening

--- pkgs/tools/misc/pal/default.nix
+++ pkgs/tools/misc/pal/default.nix
@@ -12,11 +12,11 @@ stdenv.mkDerivation rec {
     sed -i -e 's,/etc/pal\.conf,'$out/etc/pal.conf, src/input.c
   '';
 
-  preBuild = ''
-    export makeFlags="prefix=$out"
-  '';
+  makeFlags = "prefix=$(out)";
 
   buildInputs = [ glib gettext readline pkgconfig ];
+
+  hardening_format = false;
 
   meta = {
     homepage = http://palcal.sourceforge.net/;
