5e6962ff2eba9142280f9a8be4ca40c6272af584
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-02-12 13:12:10 +0100

    postgresql: Update to 9.5.1, 9.4.6, 9.3.11, 9.2.15, 9.1.20

--- pkgs/servers/sql/postgresql/default.nix
+++ pkgs/servers/sql/postgresql/default.nix
@@ -59,33 +59,33 @@ let
 in {
 
   postgresql91 = common {
-    version = "9.1.19";
+    version = "9.1.20";
     psqlSchema = "9.1";
-    sha256 = "1ihf9h353agsm5p2dr717dvraxvsw6j7chbn3qxdcz8la5s0bmfb";
+    sha256 = "0dr9hz1a0ax30f6jvnv2rck0zzxgk9x7nh4n1xgshrf26i1nq7kd";
   };
 
   postgresql92 = common {
-    version = "9.2.14";
+    version = "9.2.15";
     psqlSchema = "9.2";
-    sha256 = "0bi9zfsfhj84mnaa41ar63j9qgzsnac1wwgjhy2c6j0a68zhphjl";
+    sha256 = "0q1yahkfys78crf59avp02ibd0lp3z7h626xchyfi6cqb03livbw";
   };
 
   postgresql93 = common {
-    version = "9.3.10";
+    version = "9.3.11";
     psqlSchema = "9.3";
-    sha256 = "0c8mailildnqnndwpmnqf8ymxmk1qf5w5dq02hjqmydgfq7lyi75";
+    sha256 = "08ba951nfiy516flaw352shj1zslxg4ryx3w5k0adls1r682l8ix";
   };
 
   postgresql94 = common {
-    version = "9.4.5";
+    version = "9.4.6";
     psqlSchema = "9.4";
-    sha256 = "0faav7k3nlhh1z7j1r3adrhx1fpsji3jixmm2abjm93fdg350z5q";
+    sha256 = "19j0845i195ksg9pvnk3yc2fr62i7ii2bqgbidfjq556056izknb";
   };
 
   postgresql95 = common {
-    version = "9.5.0";
+    version = "9.5.1";
     psqlSchema = "9.5";
-    sha256 = "f1c0d3a1a8aa8c92738cab0153fbfffcc4d4158b3fee84f7aa6bfea8283978bc";
+    sha256 = "1ljvijaja5zy4i5b1450drbj8m3fcm3ly1zzaakp75x30s2rsc3b";
   };
 
 
