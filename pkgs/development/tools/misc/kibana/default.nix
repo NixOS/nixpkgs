4374ffa19adcded710873da4e76109e6e11303ec
Robin Gloster <mail@glob.in>
2016-02-13 14:58:32 +0000

    kibana: 4.1.2 -> 4.4.1

--- pkgs/development/tools/misc/kibana/default.nix
+++ pkgs/development/tools/misc/kibana/default.nix
@@ -4,11 +4,11 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "kibana-${version}";
-  version = "4.1.2";
+  version = "4.4.1";
 
   src = fetchurl {
     url = "http://download.elastic.co/kibana/kibana-snapshot/${name}-snapshot-linux-x86.tar.gz";
-    sha256 = "00ag4wnlw6h2j6zcz0irz6j1s51fr9ix2g1smrhrdw44z5gb6wrh";
+    sha256 = "0kxvrhrkcvx7pcn7myvabhcm4nj8gi86ij4a1xi392lfds2v350z";
   };
 
   buildInputs = [ makeWrapper ];
