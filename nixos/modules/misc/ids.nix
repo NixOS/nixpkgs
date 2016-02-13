7b7cf281d3bb3ddf21aefd8afb6304673de8bcc5
Al Zohali <zohl@fmap.me>
2016-02-13 22:19:31 +0300

    gammu-smsd service: init

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -248,6 +248,7 @@
       matrix-synapse = 224;
       rspamd = 225;
       rmilter = 226;
+      gammu-smsd = 227;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
