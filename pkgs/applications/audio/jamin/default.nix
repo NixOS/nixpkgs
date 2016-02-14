edd63763e158d55caf90dfa4f5caf979cf85e560
Cillian de Róiste <deroiste@syslab.com>
2016-02-15 00:42:16 +0100

    Jamin: adjust to standard code conventions

--- pkgs/applications/audio/jamin/default.nix
+++ pkgs/applications/audio/jamin/default.nix
@@ -1,5 +1,6 @@
 { stdenv, fetchurl, fftwFloat, gtk2, ladspaPlugins, libjack2, liblo, libxml2
-, makeWrapper, pkgconfig, perl, perlXMLParser }:
+, makeWrapper, pkgconfig, perl, perlXMLParser
+}:
 
 stdenv.mkDerivation {
   name = "jamin-0.95.0";
@@ -9,11 +10,13 @@ stdenv.mkDerivation {
     sha256 = "0g5v74cm0q3p3pzl6xmnp4rqayaymfli7c6z8s78h9rgd24fwbvn";
   };
 
-  buildInputs = [ fftwFloat gtk2 ladspaPlugins libjack2 liblo libxml2
-  pkgconfig perl perlXMLParser makeWrapper ];
+  buildInputs = [
+    fftwFloat gtk2 ladspaPlugins libjack2 liblo libxml2 pkgconfig perl
+    perlXMLParser makeWrapper
+  ];
   
   postInstall = ''
-      wrapProgram $out/bin/jamin --set LADSPA_PATH ${ladspaPlugins}/lib/ladspa
+    wrapProgram $out/bin/jamin --set LADSPA_PATH ${ladspaPlugins}/lib/ladspa
   '';
 
   meta = with stdenv.lib; {
