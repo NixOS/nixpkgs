7fc090018d0721def7792416292bb49583b80530
Pascal Wittmann <mail@pascal-wittmann.de>
2016-02-14 14:43:45 +0100

    slock: fix license

--- pkgs/misc/screensavers/slock/default.nix
+++ pkgs/misc/screensavers/slock/default.nix
@@ -7,14 +7,14 @@ stdenv.mkDerivation rec {
   };
   buildInputs = [ xproto libX11 libXext libXrandr ];
   installFlags = "DESTDIR=\${out} PREFIX=";
-  meta = {
+  meta = with stdenv.lib; {
     homepage = http://tools.suckless.org/slock;
     description = "Simple X display locker";
     longDescription = ''
       Simple X display locker. This is the simplest X screen locker.
     '';
-    license = "bsd";
-    maintainers = with stdenv.lib.maintainers; [ astsmtl ];
-    platforms = with stdenv.lib.platforms; linux;
+    license = licenses.mit;
+    maintainers = with maintainers; [ astsmtl ];
+    platforms = platforms.linux;
   };
 }
