ede005ad3fc121042139c7f3a10aca8fb993509c
Marko Poikonen <marko@poikonen.de>
2016-02-16 22:44:23 +0100

    Enabling Media PCI adapters (needed for PCI DVB cards)

--- pkgs/os-specific/linux/kernel/common-config.nix
+++ pkgs/os-specific/linux/kernel/common-config.nix
@@ -478,6 +478,7 @@ with stdenv.lib;
   ''}
   ${optionalString (versionAtLeast version "3.7") ''
     MEDIA_USB_SUPPORT y
+    MEDIA_PCI_SUPPORT y
   ''}
 
   # Our initrd init uses shebang scripts, so can't be modular.
