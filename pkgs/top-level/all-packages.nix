6efcbd89506590a2d16fc3ca8d540ae3e632e219
Svein Ove Aas <svein.ove@aas.no>
2016-02-16 16:37:45 +0000

    mprime: Init at 28.7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2386,6 +2386,8 @@ let
 
   mpage = callPackage ../tools/text/mpage { };
 
+  mprime = callPackage ../tools/misc/mprime { };
+
   mpw = callPackage ../tools/security/mpw { };
 
   mr = callPackage ../applications/version-management/mr { };
