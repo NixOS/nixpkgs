6cdf5fe85fc0e6dd1dc9789993fc59962270374a
Franz Pletz <fpletz@fnordicwalking.de>
2016-02-17 16:30:13 +0100

    linux: 4.1.17 -> 4.1.18

--- pkgs/os-specific/linux/kernel/linux-4.1.nix
+++ pkgs/os-specific/linux/kernel/linux-4.1.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.1.17";
+  version = "4.1.18";
   extraMeta.branch = "4.1";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "084ij19vgm27ljrjabqqmlqn27p168nsm9grhr6rajid4n79h6ab";
+    sha256 = "1n838dg058knmx4n5mfqxh2ai9d3x6w9zs1apkwzm89rpisc1ijb";
   };
 
   kernelPatches = args.kernelPatches;
