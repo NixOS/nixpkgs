392699a82d2c98fe4fa451364ec3b50d22ed5732
Brian McKenna <bmckenna@atlassian.com>
2016-02-18 13:50:26 +1100

    ue4: init at 4.10.2
    This builds Unreal Engine 4 and has a wrapper to start the editor.
    
    Sadly the application requires write access to a lot of paths. I have a
    hack to do a symlink tree under $HOME and it works well, the UE4Editor
    binary just needs to be not a symlink.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14619,6 +14619,8 @@ let
 
   urbanterror = callPackage ../games/urbanterror { };
 
+  ue4 = callPackage ../games/ue4 { };
+
   ue4demos = recurseIntoAttrs (callPackage ../games/ue4demos { });
 
   ut2004demo = callPackage ../games/ut2004demo { };
