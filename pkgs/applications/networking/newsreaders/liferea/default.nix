44510cf929b1a4948ed1000bac0550c36e24191a
zimbatm <zimbatm@zimbatm.com>
2016-02-18 22:35:39 +0000

    liferea: fix download hash
    Fixes #13107

--- pkgs/applications/networking/newsreaders/liferea/default.nix
+++ pkgs/applications/networking/newsreaders/liferea/default.nix
@@ -13,7 +13,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://github.com/lwindolf/${pname}/releases/download/v${version}/${name}.tar.bz2";
-    sha256 = "0wpinar2cxyzq2j7ff7lznc08f0n6qjdm9aavsarbgdpaf2s1xvq";
+    sha256 = "0xyy0qm3h22b69if2hmg36jzvvljxb1w0zy2m2a28kdqfzpa6m8g";
   };
 
   buildInputs = with gst_all_1; [
