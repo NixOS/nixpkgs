c6f143307c10ca4ecf448c523267bc37fa10299e
Nikolay Amiantov <ab@fmap.me>
2016-02-19 17:00:19 +0300

    uwsgi: refactor, throw sensible error if plugin is not found

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3477,6 +3477,8 @@ let
 
   uwsgi = callPackage ../servers/uwsgi {
     plugins = [];
+    withPAM = stdenv.isLinux;
+    withSystemd = stdenv.isLinux;
   };
 
   vacuum = callPackage ../applications/networking/instant-messengers/vacuum {};
