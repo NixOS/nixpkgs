8641b9dec4d6d66c7414f4c64f38e70be89b2af7
Robin Gloster <mail@glob.in>
2016-02-20 22:17:54 +0000

    mjpegtools: turn off format hardening

--- pkgs/tools/video/mjpegtools/default.nix
+++ pkgs/tools/video/mjpegtools/default.nix
@@ -7,9 +7,13 @@
 
 stdenv.mkDerivation rec {
   name = "mjpegtools-2.1.0";
+
   src = fetchurl {
     url = "mirror://sourceforge/mjpeg/${name}.tar.gz";
     sha256 = "01y4xpfdvd4zgv6fmcjny9mr1gbfd4y2i4adp657ydw6fqyi8kw6";
   };
+
   buildInputs = [ gtk libdv libjpeg libpng libX11 pkgconfig SDL SDL_gfx ];
+
+  hardening_format = false;
 }
