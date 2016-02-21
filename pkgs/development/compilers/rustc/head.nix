d6bdc429dafe7c62317ecc9e3888a738f93e6aaf
Graham Christensen <graham@grahamc.com>
2016-02-21 16:50:47 -0600

    Upgrade the nightly rust
     - patch prsec for updated tests
     - remove the cc / linker patch

--- pkgs/development/compilers/rustc/head.nix
+++ pkgs/development/compilers/rustc/head.nix
@@ -1,18 +1,19 @@
+
 { stdenv, callPackage }:
 callPackage ./generic.nix {
-  shortVersion = "2015-08-09";
+  shortVersion = "nightly";
   isRelease = false;
-  # src rev for 2015-08-09's nightly channel
-  srcRev = "a5d33d891";
-  srcSha = "1iivzk9ggjh7y89rbw275apw4rfmzh4jk50kf0milljhvf72660n";
-  snapshotHashLinux686 = "3459275cdf3896f678e225843fa56f0d9fdbabe8";
-  snapshotHashLinux64 = "e451e3bd6e5fcef71e41ae6f3da9fb1cf0e13a0c";
-  snapshotHashDarwin686 = "428944a7984c0988e77909d82ca2ef77d96a1fbd";
-  snapshotHashDarwin64 = "b0515bb7d2892b9a58282fc865fee11a885406d6";
-  snapshotDate = "2015-07-26";
-  snapshotRev = "a5c12f4";
+  srcRev = "98a59cf57e02b6e6a5a3bd74eb47b1422eaacc53";
+  srcSha = "438ffcaed142d8f849106df7083aa590c84a49174f7cd79979d8a8442d6c776a";
+  snapshotHashLinux686 = "a09c4a4036151d0cb28e265101669731600e01f2";
+  snapshotHashLinux64 = "97e2a5eb8904962df8596e95d6e5d9b574d73bf4";
+  snapshotHashDarwin686 = "ca52d2d3ba6497ed007705ee3401cf7efc136ca1";
+  snapshotHashDarwin64 = "3c44ffa18f89567c2b81f8d695e711c86d81ffc7";
+  snapshotDate = "2015-12-18";
+  snapshotRev = "3391630";
   patches = [
     ./patches/head.patch
   ] ++ stdenv.lib.optional stdenv.needsPax ./patches/grsec.patch;
 }
+
 
