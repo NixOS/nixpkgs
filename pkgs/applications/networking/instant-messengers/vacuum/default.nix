95325aa96ff1070292877b3ab5d30f84dea53773
Robin Gloster <mail@glob.in>
2016-02-22 08:57:20 +0000

    vacuum: use mkDerivation and turn off format hardening

--- pkgs/applications/networking/instant-messengers/vacuum/default.nix
+++ pkgs/applications/networking/instant-messengers/vacuum/default.nix
@@ -1,56 +1,31 @@
-x@{builderDefsPackage
-  , qt4, openssl
-  , xproto, libX11, libXScrnSaver, scrnsaverproto
-  , xz
-  , ...}:
-builderDefsPackage
-(a :  
-let 
-  helperArgNames = ["stdenv" "fetchurl" "builderDefsPackage"] ++ 
-    [];
+{ stdenv, fetchurl, qt4, openssl, xproto, libX11
+, libXScrnSaver, scrnsaverproto, xz
+}:
 
-  buildInputs = map (n: builtins.getAttr n x)
-    (builtins.attrNames (builtins.removeAttrs x helperArgNames));
-  sourceInfo = rec {
-    version="1.2.4";
-    baseName="vacuum-im";
-    name="${baseName}-${version}";
+stdenv.mkDerivation rec {
+  name="${baseName}-${version}";
+  baseName = "vacuum-im";
+  version = "1.2.4";
+
+  src = fetchurl {
     url="https://googledrive.com/host/0B7A5K_290X8-d1hjQmJaSGZmTTA/vacuum-1.2.4.tar.gz";
     sha256="10qxpfbbaagqcalhk0nagvi5irbbz5hk31w19lba8hxf6pfylrhf";
   };
-in
-rec {
-  src = a.fetchurl {
-    url = sourceInfo.url;
-    sha256 = sourceInfo.sha256;
-  };
 
-  inherit (sourceInfo) name version;
-  inherit buildInputs;
+  configurePhase = "qmake INSTALL_PREFIX=$out -recursive vacuum.pro";
 
-  /* doConfigure should be removed if not needed */
-  phaseNames = ["addInputs" "doQMake" "doMakeInstall"];
+  hardening_format = false;
 
-  doQMake = a.fullDepEntry (''
-    qmake INSTALL_PREFIX=$out -recursive vacuum.pro
-  '') ["doUnpack" "addInputs"];
-      
-  meta = {
+  buildInputs = [
+    qt4 openssl xproto libX11 libXScrnSaver scrnsaverproto xz
+  ];
+
+  meta = with stdenv.lib; {
     description = "An XMPP client fully composed of plugins";
-    maintainers = with a.lib.maintainers;
-    [
-      raskin
-    ];
-    platforms = with a.lib.platforms;
-      linux;
-    license = with a.lib.licenses;
-      gpl3;
+    maintainers = with maintainers; [ raskin ];
+    platforms = with platforms; linux;
+    license = with licenses; gpl3;
     homepage = "http://code.google.com/p/vacuum-im/";
   };
-  passthru = {
-    updateInfo = {
-      downloadPage = "http://code.google.com/p/vacuum-im/downloads/list?can=2&q=&colspec=Filename";
-    };
-  };
-}) x
+}
 
