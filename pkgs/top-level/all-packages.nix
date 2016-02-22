ac5f358c00ca19c103673aa0f7fd19d82e1543f6
Patrick Callahan <pxcallahan@gmail.com>
2016-02-22 12:53:14 -0700

    genromfs: init at 0.5.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -888,6 +888,8 @@ let
 
   gencfsm = callPackage ../tools/security/gencfsm { };
 
+  genromfs = callPackage ../tools/filesystems/genromfs { };
+
   gist = callPackage ../tools/text/gist { };
 
   gmic = callPackage ../tools/graphics/gmic { };
