759f48ab072426a0145f7a1833285f29882bf80a
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2016-02-23 01:22:32 +0100

    sane-backends-git: 2016-02-19 -> 2016-02-22

--- pkgs/applications/graphics/sane/backends/git.nix
+++ pkgs/applications/graphics/sane/backends/git.nix
@@ -1,10 +1,10 @@
 { callPackage, fetchgit, ... } @ args:
 
 callPackage ./generic.nix (args // {
-  version = "2016-02-19";
+  version = "2016-02-22";
   src = fetchgit {
-    sha256 = "d50971c106c8e0071d71daad7235776ac9a07a5ab0adb1e0eae5536b3021dd5f";
-    rev = "d74d3bcd887d2a3d59ee96e04eb68f15c0a3b882";
+    sha256 = "476abced4a9ccc95eb0796ef1d2e1d9be76c0db14adc248a9feaae3c8a61645e";
+    rev = "a74ebe551daf8750821b1ab57324e54141a84461";
     url = "git://alioth.debian.org/git/sane/sane-backends.git";
   };
 })
