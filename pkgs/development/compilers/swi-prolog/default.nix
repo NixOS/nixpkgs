282d03c4b0b97ba3e4eeb08cbb57aa1375e82607
Robin Gloster <mail@glob.in>
2016-02-24 17:10:58 +0000

    swiProlog: disable format hardening

--- pkgs/development/compilers/swi-prolog/default.nix
+++ pkgs/development/compilers/swi-prolog/default.nix
@@ -17,6 +17,8 @@ stdenv.mkDerivation {
   buildInputs = [ gmp readline openssl libjpeg unixODBC libXinerama
     libXft libXpm libSM libXt zlib freetype pkgconfig fontconfig ];
 
+  hardening_format = false;
+
   configureFlags = "--with-world --enable-gmp --enable-shared";
 
   buildFlags = "world";
