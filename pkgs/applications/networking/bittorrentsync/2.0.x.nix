4da1711988df18a5544a945b2e8aaab4835cfe04
Austin Seipp <aseipp@pobox.com>
2016-02-24 22:28:16 -0600

    nixpkgs: bittorrentSync20 2.3.0 -> 2.3.3
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/applications/networking/bittorrentsync/2.0.x.nix
+++ pkgs/applications/networking/bittorrentsync/2.0.x.nix
@@ -5,15 +5,15 @@ let
     else if stdenv.system == "i686-linux" then "i386"
     else throw "Bittorrent Sync for: ${stdenv.system} not supported!";
 
-  sha256 = if stdenv.system == "x86_64-linux" then "1fzwqi3vjv5shmw02pz2lgdi6b138vj042hn92szc49c9wd1vdgq"
-    else if stdenv.system == "i686-linux" then "1x47jc5i37r0cwrrgi5ylfb14zwj7qyf1xhgl7g724bm1cgdc00n"
+  sha256 = if stdenv.system == "x86_64-linux" then "0l6z2fyd7i3i3cr95gkihbf6fwa7mk1b2m1jpf2nq5ispg0qf74n"
+    else if stdenv.system == "i686-linux" then "06x8f75dh58saqrz2k2xgcilh27v0jmql4k4rs7g361aad9v3pnr"
     else throw "Bittorrent Sync for: ${stdenv.system} not supported!";
 
   libPath = stdenv.lib.makeLibraryPath [ stdenv.cc.libc ];
 in
 stdenv.mkDerivation rec {
   name = "btsync-${version}";
-  version = "2.3.0";
+  version = "2.3.3";
 
   src  = fetchurl {
     url  = "https://download-cdn.getsyncapp.com/${version}/linux-${arch}/BitTorrent-Sync_${arch}.tar.gz";
