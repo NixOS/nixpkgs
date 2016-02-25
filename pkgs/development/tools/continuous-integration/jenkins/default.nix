21917bc8e23f5dd8b237b261523aebd665276926
Allan Espinosa <allan.espinosa@outlook.com>
2016-02-25 02:16:35 -0600

    jenkins: 1.649 -> 1.650

--- pkgs/development/tools/continuous-integration/jenkins/default.nix
+++ pkgs/development/tools/continuous-integration/jenkins/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "jenkins-${version}";
-  version = "1.649";
+  version = "1.650";
 
   src = fetchurl {
     url = "http://mirrors.jenkins-ci.org/war/${version}/jenkins.war";
-    sha256 = "10lvrdd6mfgkz0rdc41hg9fai44vzxy6qlayfvkjg29xgqm0n9ya";
+    sha256 = "0iypkyjcsfj36j683a6yis4q0wil6m8l065fx8v2p7ba4j2ql00n";
   };
   meta = with stdenv.lib; {
     description = "An extendable open source continuous integration server";
