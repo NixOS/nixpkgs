115d76bf47620d00d2df3340f593c77cba8f3c24
Frederik Rietdijk <fridh@fridh.nl>
2016-02-25 22:16:26 +0100

    kde5.kcalcore: init at 16.04

---
+++ pkgs/desktops/kde-5/applications-16.04/kcalcore.nix
@@ -0,0 +1,26 @@
+{ kdeApp
+, lib
+, extra-cmake-modules
+, kdoctools
+, kdelibs4support
+, libical
+}:
+
+kdeApp {
+  name = "kcalcore";
+
+  nativeBuildInputs = [
+    extra-cmake-modules
+    kdoctools
+  ];
+
+  buildInputs = [
+    kdelibs4support
+    libical
+  ];
+
+  meta = {
+    license = with lib.licenses; [ gpl2 ];
+    maintainers = [ lib.maintainers.fridh ];
+  };
+}
