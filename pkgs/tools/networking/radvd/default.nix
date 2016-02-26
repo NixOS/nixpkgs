8b9eccbf2dbc20672a21edccc02abf2a2728ebdd
Robin Gloster <mail@glob.in>
2016-02-26 23:03:00 +0000

    radvd: enable pie hardening

--- pkgs/tools/networking/radvd/default.nix
+++ pkgs/tools/networking/radvd/default.nix
@@ -2,13 +2,15 @@
 
 stdenv.mkDerivation rec {
   name = "radvd-2.11";
-  
+
   src = fetchurl {
     url = "http://www.litech.org/radvd/dist/${name}.tar.xz";
     sha256 = "1k2sbfs4w2lkgz2mh4zh66fgahjrn2hvxcpfc091bykrzj464qq4";
   };
 
   buildInputs = [ pkgconfig libdaemon bison flex check ];
+
+  hardening_pie = true;
 
   meta = with stdenv.lib; {
     homepage = http://www.litech.org/radvd/;
