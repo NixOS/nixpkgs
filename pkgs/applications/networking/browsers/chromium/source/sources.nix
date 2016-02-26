716b79d3a5f3ee438942f59bfd879f46e66204ac
aszlig <aszlig@redmoonstudios.org>
2016-02-26 10:53:52 +0100

    chromium: Provide SHA256s for beta/dev plugins
    As stated in the parent commit, the 32bit Chrome package is not
    available upstream, so let's at least provide the SHA256 hash for the
    64bit package.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/applications/networking/browsers/chromium/source/sources.nix
+++ pkgs/applications/networking/browsers/chromium/source/sources.nix
@@ -3,10 +3,12 @@
   dev = {
     version = "50.0.2657.0";
     sha256 = "1xc66zsjd5snydf60di2k2vzwwcqs1p16yn4jbpj2khm9cmxmkmf";
+    sha256bin64 = "06v7r76pbc979f7ahmln0hmxzd8ipirh4128i0ygidj71xmg51mm";
   };
   beta = {
     version = "49.0.2623.63";
     sha256 = "07i4vqswkijnl7wi6r1a0n1jq54ackm01yf8h3hwcik8q10i1aq5";
+    sha256bin64 = "16pwimg672qaqb89zdvsr8dr7bz50mz7zf6cl0cf45kz7sn8wwlh";
   };
   stable = {
     version = "48.0.2564.116";
