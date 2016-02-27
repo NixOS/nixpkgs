0a9076b5f8f7ddea0a090a09315d7697c6bd9b28
Franz Pletz <fpletz@fnordicwalking.de>
2016-02-27 19:10:06 +0100

    socat: 2.0.0-b8 -> 2.0.0-b9 (CVE-2016-2217)
    https://lwn.net/Vulnerabilities/674840/
    
    http://www.dest-unreach.org/socat/contrib/socat-secadv7.html
    http://www.dest-unreach.org/socat/contrib/socat-secadv8.html

--- pkgs/tools/networking/socat/2.x.nix
+++ pkgs/tools/networking/socat/2.x.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, openssl }:
 
 stdenv.mkDerivation rec {
-  name = "socat-2.0.0-b8";
+  name = "socat-2.0.0-b9";
 
   src = fetchurl {
     url = "http://www.dest-unreach.org/socat/download/${name}.tar.bz2";
-    sha256 = "1slkv1hhcp9a6c88h6yl5cs0z9g60fp2ja6865s6kywqp6fmf168";
+    sha256 = "1ll395xjv4byvv0k2zjbxk8vp3mg3y2w5paa05wv553bqsjv1vs9";
   };
 
   buildInputs = [ openssl ];
