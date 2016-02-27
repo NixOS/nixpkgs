13afef0d8ec6058e8d42f3ea8cb1f0932bef0c2f
Vladimír Čunát <vcunat@gmail.com>
2016-02-27 11:07:04 +0100

    webkitgtk: fix build with glibc-2.23
    ... by a patch accepted upstream.
    2.4 seems to build fine without patching.

--- pkgs/development/libraries/webkitgtk/default.nix
+++ pkgs/development/libraries/webkitgtk/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, perl, python, ruby, bison, gperf, cmake
+{ stdenv, fetchurl, fetchpatch, perl, python, ruby, bison, gperf, cmake
 , pkgconfig, gettext, gobjectIntrospection, libnotify
 , gtk2, gtk3, wayland, libwebp, enchant
 , libxml2, libsoup, libsecret, libxslt, harfbuzz, libpthreadstubs
@@ -28,7 +28,14 @@ stdenv.mkDerivation rec {
     sha256 = "0mghsbfnmmf6nsf7cb3ah76s77aigkzf3k6kw96wgh6all6jdy6v";
   };
 
-  patches = [ ./finding-harfbuzz-icu.patch ];
+  patches = [ ./finding-harfbuzz-icu.patch
+    (fetchpatch {
+      name = "glibc-isnan.patch";
+      url = "http://trac.webkit.org/changeset/194518/trunk/Source/JavaScriptCore"
+        + "/runtime/Options.cpp?format=diff&new=194518";
+      sha256 = "0pzdv1zmlym751n9d310cx3yp752yzsc49cysbvgnrib4dh68nbm";
+    })
+  ];
 
   cmakeFlags = [ "-DPORT=GTK" "-DUSE_LIBHYPHEN=0" ];
 
