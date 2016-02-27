c6b1f48e31724069b5d8057725c21f9f2929fab0
Franz Pletz <fpletz@fnordicwalking.de>
2016-02-27 16:39:25 +0100

    libgcrypt: 1.6.4 -> 1.6.5 (CVE-2015-7511)
    https://www.cs.tau.ac.il/~tromer/ecdh/

--- pkgs/development/libraries/libgcrypt/default.nix
+++ pkgs/development/libraries/libgcrypt/default.nix
@@ -3,11 +3,11 @@
 assert enableCapabilities -> stdenv.isLinux;
 
 stdenv.mkDerivation rec {
-  name = "libgcrypt-1.6.4";
+  name = "libgcrypt-1.6.5";
 
   src = fetchurl {
     url = "mirror://gnupg/libgcrypt/${name}.tar.bz2";
-    sha256 = "09k06gs27gxfha07sa9rpf4xh6mvphj9sky7n09ymx75w9zjrg69";
+    sha256 = "0959mwfzsxhallxdqlw359xg180ll2skxwyy35qawmfl89cbr7pl";
   };
 
   buildInputs =
