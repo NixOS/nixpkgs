afa7d8452903318dd09e742e57899ba745c516cf
Pascal Wittmann <mail@pascal-wittmann.de>
2016-03-01 22:53:49 +0100

    instead: 2.3.0 -> 2.4.0

--- pkgs/games/instead/default.nix
+++ pkgs/games/instead/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, SDL, SDL_ttf, SDL_image, SDL_mixer, pkgconfig, lua, zlib, unzip }:
 
 let
-  version = "2.3.0";
+  version = "2.4.0";
 
   # I took several games at random from http://instead.syscall.ru/games/
   games = [
@@ -33,7 +33,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://sourceforge/project/instead/instead/${version}/instead_${version}.tar.gz";
-    sha256 = "1ldisjkmmcpnmv4vsd25dc1sfiwbr9fcn3hxhl78i4jwlyqgrms8";
+    sha256 = "1xxga3ppgjshxzd0p53vsbaqkpzmjnm4vw0j1v7qbqzjgi3r44ix";
   };
 
   NIX_LDFLAGS = "-llua -lgcc_s";
