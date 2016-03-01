b2197b84c2d140a802cb5b6f009490f76f034010
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-03-01 22:07:54 +0100

    Fix NixOS eval

--- nixos/modules/services/misc/octoprint.nix
+++ nixos/modules/services/misc/octoprint.nix
@@ -62,7 +62,7 @@ in
       };
 
       plugins = mkOption {
-        type = types.functionTo (types.listOf types.package);
+        #type = types.functionTo (types.listOf types.package);
         default = plugins: [];
         defaultText = "plugins: []";
         example = literalExample "plugins: [ m3d-fio ]";
