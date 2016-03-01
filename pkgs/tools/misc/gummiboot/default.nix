2f7e9f26d84b79e9c5a0bd9e7647f10b5d02817e
Robin Gloster <mail@glob.in>
2016-03-01 22:21:21 +0000

    gummiboot: disable stackprotector hardening

--- pkgs/tools/misc/gummiboot/default.nix
+++ pkgs/tools/misc/gummiboot/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ gnu-efi pkgconfig libxslt utillinux ];
 
-  #hardening_all = false;
+  hardening_stackprotector = false;
 
   # Sigh, gummiboot should be able to find this in buildInputs
   configureFlags = [
