9c86acc479d58785790f9292356a8344e3d70869
Marius Bakke <m.bakke@warwick.ac.uk>
2016-03-01 19:49:46 +0000

    zathura-pdf-mupdf: 0.2.8 -> 0.3.0

--- pkgs/applications/misc/zathura/pdf-mupdf/default.nix
+++ pkgs/applications/misc/zathura/pdf-mupdf/default.nix
@@ -1,12 +1,12 @@
 { stdenv, lib, fetchurl, pkgconfig, zathura_core, gtk, girara, mupdf, openssl }:
 
 stdenv.mkDerivation rec {
-  version = "0.2.8";
+  version = "0.3.0";
   name = "zathura-pdf-mupdf-${version}";
 
   src = fetchurl {
     url = "https://pwmt.org/projects/zathura-pdf-mupdf/download/${name}.tar.gz";
-    sha256 = "0439ls8xqnq6hqa53hd0wqxh1qf0xmccfi3pb0m4mlfs5iv952wz";
+    sha256 = "1j3j3wbp49walb19f0966qsnlqbd26wnsjpcxfbf021dav8vk327";
   };
 
   buildInputs = [ pkgconfig zathura_core gtk girara openssl mupdf ];
