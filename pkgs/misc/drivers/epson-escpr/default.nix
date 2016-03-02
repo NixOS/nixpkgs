8ab4500ab44e36e1020f0d2484c137e7efc1ffa0
artuuge <artuuge@gmail.com>
2016-03-02 14:49:23 +0100

    epson-escpr: 1.6.3 -> 1.6.4
    (cherry picked from commit 7ddd6fc1c3672e82bcc2dba8d10cc83565d873c3)

--- pkgs/misc/drivers/epson-escpr/default.nix
+++ pkgs/misc/drivers/epson-escpr/default.nix
@@ -1,15 +1,15 @@
 { stdenv, fetchurl, cups }:
 
 let
-  version = "1.6.3";
+  version = "1.6.4";
 in
   stdenv.mkDerivation {
 
     name = "epson-escpr-${version}";
   
     src = fetchurl {
-      url = "https://download3.ebz.epson.net/dsc/f/03/00/04/33/53/0177a44361d3dfeacf7f15ff4a347cef373688da/epson-inkjet-printer-escpr-1.6.3-1lsb3.2.tar.gz";
-      sha256 = "4988479ce7dd5513bfa1cce4a83f82348572d8d69d6aa3b2c6e154a58a04ad86"; 
+      url = "https://download3.ebz.epson.net/dsc/f/03/00/04/37/97/88177bc0dc7025905eae4a0da1e841408f82e33c/epson-inkjet-printer-escpr-1.6.4-1lsb3.2.tar.gz"; 
+      sha256 = "76c66461a30be82b9cc37d663147a72f488fe060ef54578120602bb87a3f7754"; 
     }; 
 
     patches = [ ./cups-filter-ppd-dirs.patch ]; 
