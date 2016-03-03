e3d9bdf8f8c571510fce1733e8b5b885b1a42eb5
Nikolay Amiantov <ab@fmap.me>
2016-03-03 15:13:26 +0300

    ioquake3: 20151228 -> 2016-02-18

--- pkgs/games/quake3/ioquake/default.nix
+++ pkgs/games/quake3/ioquake/default.nix
@@ -1,14 +1,16 @@
-{ lib, stdenv, fetchgit, xlibsWrapper, SDL2, mesa, openalSoft
+{ lib, stdenv, fetchFromGitHub, xlibsWrapper, SDL2, mesa, openalSoft
 , curl, speex, opusfile, libogg, libopus, libjpeg, mumble, freetype
 }:
 
-stdenv.mkDerivation {
-  name = "ioquake3-git-20151228";
+stdenv.mkDerivation rec {
+  name = "ioquake3-git-${version}";
+  version = "2016-02-18";
 
-  src = fetchgit {
-    url = "https://github.com/ioquake/ioq3";
-    rev = "fe619680f8fa9794906fc82a9c8c6113770696e6";
-    sha256 = "5462441df63eebee6f8ed19a8326de5f874dad31e124d37f73d3bab1cd656a87";
+  src = fetchFromGitHub {
+    owner = "ioquake";
+    repo = "ioq3";
+    rev = "a331637745fb82266f3627fb438f2d58d53e366c";
+    sha256 = "0l9ppv1msd73bhqmdiv5lsvkr5i6khqgi6gi322gbnndr20arn4n";
   };
 
   buildInputs = [ xlibsWrapper SDL2 mesa openalSoft curl speex opusfile libogg libopus libjpeg freetype mumble ];
