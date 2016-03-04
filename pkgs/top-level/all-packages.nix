014a30da93e370b785e4283630a43c3d12e48ee5
Vincent Laporte <Vincent.Laporte@gmail.com>
2016-03-04 07:29:28 +0100

    why3: 0.86.2 -> 0.86.3
    And merge with ocamlPackages.why3
    (the OCaml library would not work correctly if packaged separately).

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5042,10 +5042,6 @@ let
 
     vg = callPackage ../development/ocaml-modules/vg { };
 
-    why3 = callPackage ../development/ocaml-modules/why3 {
-      why3 = pkgs.why3;
-    };
-
     x509 = callPackage ../development/ocaml-modules/x509 { };
 
     xmlm = callPackage ../development/ocaml-modules/xmlm { };
