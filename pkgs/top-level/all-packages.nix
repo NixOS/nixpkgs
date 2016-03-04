22efa703d5d86a12a2e9907b1b8021dcd90f7177
Matthew Bauer <mjbauer95@gmail.com>
2016-03-04 14:09:24 -0600

    fluidsynth: fix build errors in darwin builds
    Build inputs were not being passed in properly.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12081,7 +12081,9 @@ let
 
   fldigi = callPackage ../applications/audio/fldigi { };
 
-  fluidsynth = callPackage ../applications/audio/fluidsynth { };
+  fluidsynth = callPackage ../applications/audio/fluidsynth {
+     inherit (darwin.apple_sdk.frameworks) CoreServices CoreAudio AudioUnit;
+  };
 
   fmit = qt5.callPackage ../applications/audio/fmit { };
 
