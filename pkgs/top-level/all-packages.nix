5325163374b2d7c2fd10800018f8afcec8dd1af6
Franz Pletz <fpletz@fnordicwalking.de>
2016-03-07 22:12:58 +0100

    sslscan: use oldest openssl to support SSLv2/SSLv3 checking

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3692,7 +3692,9 @@ in
 
   sshpass = callPackage ../tools/networking/sshpass { };
 
-  sslscan = callPackage ../tools/security/sslscan { };
+  sslscan = callPackage ../tools/security/sslscan {
+    openssl = openssl_1_0_1;
+  };
 
   sslmate = callPackage ../development/tools/sslmate { };
 
