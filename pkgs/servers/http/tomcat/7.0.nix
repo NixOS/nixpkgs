c686f03305334b247d8fb742fa98ab559f661b6e
Franz Pletz <fpletz@fnordicwalking.de>
2016-03-07 03:05:28 +0100

    tomcat: 6.0.44 -> 6.0.45, 7.0.62 -> 7.0.68, 8.0.23 -> 8.0.32
    Fixes at least CVE-2015-5174, CVE-2015-5345, CVE-2015-5351, CVE-2016-0706,
    CVE-2016-0714, CVE-2016-0763.

--- pkgs/servers/http/tomcat/7.0.nix
+++ pkgs/servers/http/tomcat/7.0.nix
@@ -1,6 +1,6 @@
 import ./recent.nix 
   {
     versionMajor = "7";
-    versionMinor = "0.62";
-    sha256 = "0v8zvyd4h85ynnday58x0ppplw4flxyjsrmrpg78rrv3w49fm1x7";
+    versionMinor = "0.68";
+    sha256 = "1q5qgci5ia25zqa1k1n2xzarsgk1317ya89mfgg0fmi65x1046ic";
   }
