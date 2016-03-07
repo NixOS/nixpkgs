5e204092294e0763288d3c7fc42a29030b448f15
Franz Pletz <fpletz@fnordicwalking.de>
2016-03-07 02:50:51 +0100

    libmatroska: 1.4.1 -> 1.4.4 (security)
    This fixes CVE-2015-8790 & CVE-2015-8791.

--- pkgs/development/libraries/libmatroska/default.nix
+++ pkgs/development/libraries/libmatroska/default.nix
@@ -1,17 +1,16 @@
-{ stdenv, fetchurl, libebml }:
+{ stdenv, fetchurl, pkgconfig, libebml }:
 
 stdenv.mkDerivation rec {
-  name = "libmatroska-1.4.1";
+  name = "libmatroska-1.4.4";
 
   src = fetchurl {
     url = "http://dl.matroska.org/downloads/libmatroska/${name}.tar.bz2";
-    sha256 = "1dzglkl0hpimld1kahkrrp857hw5pg1r7xxbpnx7jmlj7s3j2vq8";
+    sha256 = "1mvb54q3gag9dj0pkwci8w75gp6mm14gi85y0ld3ar1rdngsmvyk";
   };
 
-  configurePhase = "cd make/linux";
-  makeFlags = "prefix=$(out) LIBEBML_INCLUDE_DIR=${libebml}/include LIBEBML_LIB_DIR=${libebml}/lib"
-    + stdenv.lib.optionalString stdenv.isDarwin " CXX=clang++";
-  propagatedBuildInputs = [ libebml ];
+  nativeBuildInputs = [ pkgconfig ];
+
+  buildInputs = [ libebml ];
 
   meta = with stdenv.lib; {
     description = "A library to parse Matroska files";
