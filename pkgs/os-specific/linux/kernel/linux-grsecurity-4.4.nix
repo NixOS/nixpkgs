69dbce32ce00861c7fe953595614b2bc0d08b080
Franz Pletz <fpletz@fnordicwalking.de>
2016-03-08 01:03:47 +0100

    grsecurity: 4.4.2 -> 4.4.4
    See #13505.
    
    (cherry picked from commit 255d710757faf94401a45802e92dffd4339e9c3b)

--- pkgs/os-specific/linux/kernel/linux-grsecurity-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-grsecurity-4.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.4.2";
+  version = "4.4.4";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "09l6y0nb8yv7l16arfwhy4i5h9pkxcbd7hlbw0015n7gm4i2mzc2";
+    sha256 = "0b4190mwmxf329n16yl32my7dfi02pi7qf39a8v61sl9b2gxffad";
   };
 
   kernelPatches = args.kernelPatches;
