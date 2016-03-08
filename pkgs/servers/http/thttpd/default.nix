9321084ec7170816bc667d497c15247648a23e86
Florian Steinel <fsteinel-git@github.com.flonet.net>
2016-03-09 00:57:22 +0100

    thttpd: update from 2.26 to 2.27
    from http://www.acme.com/software/thttpd/#releasenotes
     New in version 2.27:
    -   Stats syslogs changed from LOG_INFO to LOG_NOTICE.
    -   Use memmove() for self-overlapping string copies instead of strcpy().
    -   Couple of subroutine name changes for consistency.

--- pkgs/servers/http/thttpd/default.nix
+++ pkgs/servers/http/thttpd/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "thttpd-${version}";
-  version = "2.26";
+  version = "2.27";
 
   src = fetchurl {
     url = "http://acme.com/software/thttpd/${name}.tar.gz";
-    sha256 = "1idlpnwrd5fpmnfh477h1lzanavx8jxir2d8adax46zy472dg4s6";
+    sha256 = "0ykda5k1zzzag59zbd4bkzj1psavq0xnpy7vpk19rhx7mlvvri5i";
   };
 
   prePatch = ''
