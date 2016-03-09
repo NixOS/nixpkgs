63ffd27a6b0b6665e89c51f08cf06ab3c7bd474b
Lluís Batlle i Rossell <viric@viric.name>
2016-03-09 12:40:17 +0100

    Fixing my-env to get NIX_CFLAGS_COMPILE and so on
    Yes, I still use my-env. I tested that it works on 16.03.

--- pkgs/misc/my-env/default.nix
+++ pkgs/misc/my-env/default.nix
@@ -88,6 +88,7 @@ mkDerivation {
         -e '1i initialPath="${toString initialPath}"' \
         "$setupNew" > "$s"
     cat >> "$out/dev-envs/''${name/env-/}" << EOF
+      defaultNativeBuildInputs="$defaultNativeBuildInputs"
       nativeBuildInputs="$nativeBuildInputs"
       propagatedBuildInputs="$propagatedBuildInputs2"
       # the setup-new script wants to write some data to a temp file.. so just let it do that and tidy up afterwards
