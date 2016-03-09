efad3672d85112908a2d85393c32272ccf8cb0f8
Kamil Chmielewski <kamil.chm@gmail.com>
2016-03-09 08:03:25 +0100

    ntfy: init at 1.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12816,6 +12816,26 @@ in modules // {
     };
   };
 
+  ntfy = buildPythonPackage rec {
+    version = "1.2.0";
+    name = "ntfy-${version}";
+    src = pkgs.fetchFromGitHub {
+      owner = "dschep";
+      repo = "ntfy";
+      rev = "v${version}";
+      sha256 = "0yjxwisxpxy3vpnqk9nw5k3db3xx6wyf6sk1px9m94s30glcq2cc";
+    };
+
+    propagatedBuildInputs = with self; [ appdirs pyyaml requests dbus ];
+
+    meta = {
+      description = "A utility for sending notifications, on demand and when commands finish";
+      homepage = http://ntfy.rtfd.org/;
+      license = licenses.gpl3;
+      maintainers = with maintainers; [ kamilchm ];
+    };
+  };
+
   ntplib = buildPythonPackage rec {
     name = "ntplib-0.3.2";
     src = pkgs.fetchurl {
