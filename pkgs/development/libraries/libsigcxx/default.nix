d9da9ec634372f81c6ffe1586ef7efebbb2adfab
Vladimír Čunát <vcunat@gmail.com>
2016-03-10 11:26:44 +0100

    libsigcxx: make *.pc force C++11
    This fixes compilation problems of some packages against *mm,
    including gtkmm-2 itself.

--- pkgs/development/libraries/libsigcxx/default.nix
+++ pkgs/development/libraries/libsigcxx/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, gnum4 }:
+{ stdenv, fetchurl, fetchpatch, pkgconfig, gnum4 }:
 let
   ver_maj = "2.6"; # odd major numbers are unstable
   ver_min = "2";
@@ -10,6 +10,11 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/libsigc++/${ver_maj}/${name}.tar.xz";
     sha256 = "fdace7134c31de792c17570f9049ca0657909b28c4c70ec4882f91a03de54437";
   };
+  patches = [(fetchpatch {
+    url = "https://anonscm.debian.org/cgit/collab-maint/libsigc++-2.0.git/plain"
+      + "/debian/patches/0002-Enforce-c-11-via-pkg-config.patch?id=d451a4d195b1";
+    sha256 = "19g19473syp2z3kg8vdrli89lm9kcvaqajkqfmdig1vfpkbq0nci";
+  })];
 
   nativeBuildInputs = [ pkgconfig gnum4 ];
 
