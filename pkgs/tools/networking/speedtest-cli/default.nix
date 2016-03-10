83766949c1c6a02bf09660d2da6d38b4b34b6e90
Domen Kožar <domen@dev.si>
2016-03-10 20:42:37 +0000

    speedtest-cli: 0.3.1 -> 0.3.4 (fix runtime)

--- pkgs/tools/networking/speedtest-cli/default.nix
+++ pkgs/tools/networking/speedtest-cli/default.nix
@@ -2,12 +2,11 @@
 
 pythonPackages.buildPythonApplication rec {
   name = "speedtest-cli-${version}";
-  version = "0.3.1";
-  namePrefix = "";
+  version = "0.3.4";
   
   src = fetchurl {
     url = "https://pypi.python.org/packages/source/s/speedtest-cli/speedtest-cli-${version}.tar.gz";
-    sha256 = "0ln2grbskh39ph79lhcim2axm7hp4xhzbrag8xfqbfihq7jdm6ya";
+    sha256 = "19i671cd815fcv0x7h2m0a493slzwkzn7r926g8myx1srkss0q6d";
   };
 
   meta = with stdenv.lib; {
