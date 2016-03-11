c5b31675a85a59b01fbf7716d77eb1da44900284
Lluís Batlle i Rossell <viric@viric.name>
2016-03-11 14:29:56 +0100

    Adding rowhammer test.
    (cherry picked from commit e026b5c243ea39810826e68362718f5d703fb5d0)

---
+++ pkgs/tools/system/rowhammer-test/default.nix
@@ -0,0 +1,27 @@
+{ stdenv, fetchFromGitHub }:
+
+stdenv.mkDerivation {
+  name = "rowhammer-test-20150811";
+
+  src = fetchFromGitHub {
+    owner = "google";
+    repo = "rowhammer-test";
+    rev = "c1d2bd9f629281402c10bb10e52bc1f1faf59cc4"; # 2015-08-11
+    sha256 = "1fbfcnm5gjish47wdvikcsgzlb5vnlfqlzzm6mwiw2j5qkq0914i";
+  };
+
+  buildPhase = "sh -e make.sh";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp rowhammer_test double_sided_rowhammer $out/bin
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Test DRAM for bit flips caused by the rowhammer problem";
+    homepage = https://github.com/google/rowhammer-test;
+    license = licenses.asl20;
+    maintainers = [ maintainers.viric ];
+    platforms = platforms.unix;
+  };
+}
