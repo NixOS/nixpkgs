f3e5ee51bd616a0a1ad17733893ed0bd26ba9504
tg(x) <*@tg-x.net>
2016-03-11 22:36:22 +0100

    pax-utils: https urls

--- pkgs/os-specific/linux/pax-utils/default.nix
+++ pkgs/os-specific/linux/pax-utils/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
   version = "1.1.6";
 
   src = fetchurl {
-    url = "http://dev.gentoo.org/~vapier/dist/${name}.tar.xz";
+    url = "https://dev.gentoo.org/~vapier/dist/${name}.tar.xz";
     sha256 = "04hvsizzspfzfq6hhfif7ya9nwsc0cs6z6n2bq1zfh7agd8nqhzm";
   };
 
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "A suite of tools for PaX/grsecurity";
-    homepage    = "http://dev.gentoo.org/~vapier/dist/";
+    homepage    = "https://dev.gentoo.org/~vapier/dist/";
     license     = licenses.gpl2;
     platforms   = platforms.linux;
     maintainers = with maintainers; [ thoughtpolice ];
