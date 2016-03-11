61556b727a1f340dc01618b6265dc05f7a6d5d4f
Vladimír Čunát <vcunat@gmail.com>
2016-03-11 15:55:10 +0100

    nixos/mantisbt: add a simple service
    It doesn't really deserve a package, as it's just a bunch of PHP scripts
    copied into a folder and we have to copy on reconfiguration anyway.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -220,6 +220,7 @@
   ./services/misc/gitolite.nix
   ./services/misc/gpsd.nix
   ./services/misc/ihaskell.nix
+  ./services/misc/mantisbt.nix
   ./services/misc/mathics.nix
   ./services/misc/matrix-synapse.nix
   ./services/misc/mbpfan.nix
