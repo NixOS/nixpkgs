94a2187cc001ee50d6dfdda4e75095a7e9d6df57
Nikolay Amiantov <ab@fmap.me>
2016-03-12 02:53:55 +0300

    enlightenment.emotion: 1.16.0 -> 1.17.0

--- pkgs/desktops/enlightenment/emotion.nix
+++ pkgs/desktops/enlightenment/emotion.nix
@@ -1,10 +1,10 @@
 { stdenv, fetchurl, pkgconfig, efl, vlc }:
 stdenv.mkDerivation rec {
   name = "emotion_generic_players-${version}";
-  version = "1.16.0";
+  version = "1.17.0";
   src = fetchurl {
     url = "http://download.enlightenment.org/rel/libs/emotion_generic_players/${name}.tar.xz";
-    sha256 = "163ay26c6dx49m1am7vsxxn0gy877zhayxq0yxn9zkbq2srzvjym";
+    sha256 = "03kaql95mk0c5j50v3c5i5lmlr3gz7xlh8p8q87xz8zf9j5h1pp7";
   };
   buildInputs = [ pkgconfig efl vlc ];
   NIX_CFLAGS_COMPILE = [ "-I${efl}/include/eo-1" ];
