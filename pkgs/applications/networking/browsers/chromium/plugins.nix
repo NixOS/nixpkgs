4c0125bc8f7c3dd6a7971d2ae8b1e7c238917e46
Vladimír Čunát <vcunat@gmail.com>
2016-03-11 15:10:30 +0100

    chromium: fixup plugins with multiple outputs
    Chromium+flash seem to work fine now.

--- pkgs/applications/networking/browsers/chromium/plugins.nix
+++ pkgs/applications/networking/browsers/chromium/plugins.nix
@@ -45,6 +45,7 @@ let
 
     phases = [ "unpackPhase" "patchPhase" "installPhase" "checkPhase" ];
     outputs = [ "flash" "widevine" ];
+    out = "flash"; # outputs TODO: is this a hack?
 
     unpackCmd = let
       chan = if source.channel == "dev"    then "chrome-unstable"
@@ -64,7 +65,7 @@ let
     '';
 
     patchPhase = let
-      rpaths = [ stdenv.cc.cc ];
+      rpaths = [ stdenv.cc.cc.lib ];
       mkrpath = p: "${makeSearchPath "lib64" p}:${makeSearchPath "lib" p}";
     in ''
       for sofile in PepperFlash/libpepflashplayer.so \
