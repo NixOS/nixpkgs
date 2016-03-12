0ba77c4cbd41a02345218594583aaea5580f4349
Nikolay Amiantov <ab@fmap.me>
2016-03-12 17:36:25 +0300

    octoprint-plugins.m3d-fio: 0.28.2 -> 0.29

--- pkgs/applications/misc/octoprint/plugins.nix
+++ pkgs/applications/misc/octoprint/plugins.nix
@@ -8,13 +8,13 @@ in {
 
   m3d-fio = buildPlugin rec {
     name = "M3D-Fio-${version}";
-    version = "0.28.2";
+    version = "0.29";
 
     src = fetchFromGitHub {
       owner = "donovan6000";
       repo = "M3D-Fio";
       rev = "V${version}";
-      sha256 = "1fwy6xmbid89rn7w7v779wb34gmfzc1fkggv3im1r7a7jrzph6nx";
+      sha256 = "17jyr7qf9psq3xcckk1zjhaw0h8a0mh3v8lcv9vgqzni27kq9pnb";
     };
 
     patches = [
