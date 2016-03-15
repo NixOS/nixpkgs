0c4bbc8138b930ebc801555da5f98418497790d5
Robin Gloster <mail@glob.in>
2016-03-15 12:55:17 +0000

    virt-manager: remove sqlalchemy from (transitive) dependencies
    Is not used according to source code

--- pkgs/applications/virtualization/virt-manager/default.nix
+++ pkgs/applications/virtualization/virt-manager/default.nix
@@ -19,7 +19,7 @@ buildPythonApplication rec {
 
   propagatedBuildInputs =
     [ eventlet greenlet gflags netaddr carrot routes
-      PasteDeploy m2crypto ipy twisted sqlalchemy_migrate_0_7
+      PasteDeploy m2crypto ipy twisted
       distutils_extra simplejson readline glanceclient cheetah lockfile httplib2
       urlgrabber virtinst pyGtkGlade pythonDBus gnome_python pygobject3
       libvirt libxml2Python ipaddr vte libosinfo gobjectIntrospection gtk3 mox
