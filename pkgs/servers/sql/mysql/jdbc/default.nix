dbf41ebee7e68289b4ca4d8468f1d8345cbe08f7
Graham Christensen <graham@grahamc.com>
2016-03-14 20:12:04 -0500

    mysql_jdbc: 5.1.32 -> 5.1.38

--- pkgs/servers/sql/mysql/jdbc/default.nix
+++ pkgs/servers/sql/mysql/jdbc/default.nix
@@ -1,12 +1,12 @@
 {stdenv, fetchurl, ant, unzip}:
 
 stdenv.mkDerivation {
-  name = "mysql-connector-java-5.1.32";
+  name = "mysql-connector-java-5.1.38";
   builder = ./builder.sh;
 
   src = fetchurl {
-    url = http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.32.zip;
-    sha256 = "11vjwws1pa8fdwn86rrmqdwsq3ld3sh2r0pp4lpr2gxw0w18ykc7";
+    url = http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.38.zip;
+    sha256 = "0b1j2dylnpk6b17gn3168qdrrwq8kdil57nxrd08n1lnkirdsx33";
   };
 
   buildInputs = [ unzip ant ];
