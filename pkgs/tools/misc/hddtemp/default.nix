df0f4baec4cd7811c6ae052fec648c7055903df1
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2016-03-16 01:13:00 +0100

    hddtemp: correct licence: gpl2 -> gpl2Plus

--- pkgs/tools/misc/hddtemp/default.nix
+++ pkgs/tools/misc/hddtemp/default.nix
@@ -23,10 +23,10 @@ stdenv.mkDerivation {
       ./configure --prefix=$out --with-db-path=$out/nix-support/hddtemp.db
     '';
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Tool for displaying hard disk temperature";
     homepage = https://savannah.nongnu.org/projects/hddtemp/;
-    license = stdenv.lib.licenses.gpl2;
-    platforms = stdenv.lib.platforms.linux;
+    license = licenses.gpl2Plus;
+    platforms = platforms.linux;
   };
 }
