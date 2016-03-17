6faa0aea88dc8bf46ebf0e74f91d17d94ad6f7d4
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-03-17 13:05:13 +0100

    linux: 3.18.27 -> 3.18.28
    CVE-2016-2085

--- pkgs/os-specific/linux/kernel/linux-3.18.nix
+++ pkgs/os-specific/linux/kernel/linux-3.18.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.18.27";
+  version = "3.18.28";
   extraMeta.branch = "3.18";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "01lz0c3ns0yp5vnjch1pn10h43g6fr4xw7w3b6kb477083cjr7dc";
+    sha256 = "1vd7xm5wh082bwh93f69r99727y1yd5l6qjdm68rs0j5lzhamdbl";
   };
 
   kernelPatches = args.kernelPatches;
