4b512321de9ef16bcf2301ebc2fae44b644c7b30
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-03-17 13:05:57 +0100

    linux: 4.4.5 -> 4.4.6
    CVE-2016-2143

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.4.5";
+  version = "4.4.6";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1daavrj2msl85aijh1izfm1cwf14c7mi75hldzidr1h2v629l89h";
+    sha256 = "0zapxjnawdn0km6b9pc7399zbjiyb0a28rqmsif3afc9qb2cxg53";
   };
 
   kernelPatches = args.kernelPatches;
