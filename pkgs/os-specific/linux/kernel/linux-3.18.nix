d8a1eaa142c2573753fc69361ec8ae267b47bff5
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-03-17 17:57:17 +0100

    Revert "linux: 3.18.26 -> 3.18.27"
    This reverts commit e5a4d105598dd9a8fa350342e02cbbb75ee00370. Linux
    3.18.27 breaks LUKS:
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1311286
    https://bugzilla.kernel.org/show_bug.cgi?id=112631
    
    http://hydra.nixos.org/build/33388465
    
    machine# Failed to setup dm-crypt key mapping for device /dev/vda3.
    machine# Check that kernel supports aes-xts-plain64 cipher (check syslog for more info).

--- pkgs/os-specific/linux/kernel/linux-3.18.nix
+++ pkgs/os-specific/linux/kernel/linux-3.18.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.18.27";
+  version = "3.18.26";
   extraMeta.branch = "3.18";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "01lz0c3ns0yp5vnjch1pn10h43g6fr4xw7w3b6kb477083cjr7dc";
+    sha256 = "0bhf8x1h5crc9kimprjs7q74p86gsqsdr8nz54nv33c6zmryqsic";
   };
 
   kernelPatches = args.kernelPatches;
