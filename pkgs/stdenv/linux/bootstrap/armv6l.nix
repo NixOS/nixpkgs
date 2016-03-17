cdef1cdd384601ee92c021dad6450733badc9e1f
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2016-03-17 13:51:15 +0200

    ARM stdenv: Update bootstrap tools
    For some reason, the current bootstrap tools fail to build gettext:
    
    init2.c:37: MPFR assertion failed: (64 - 0) == ((64 - 0)/8) * 8 && sizeof(mp_limb_t) == ((64 - 0)/8)
    libxml/xpath.c: In function 'xmlXPathCompPathExpr':
    libxml/xpath.c:10627:1: internal compiler error: Aborted
     xmlXPathCompPathExpr(xmlXPathParserContextPtr ctxt) {
     ^
    Please submit a full bug report,
    with preprocessed source if appropriate.
    See <http://gcc.gnu.org/bugs.html> for instructions.
    make[5]: *** [libxml/libxml_rpl_la-xpath.lo] Error 1
    
    I didn't investigate why this is the case but rebuilding the bootstrap
    tools seems to help.
    
    I used this old-ish WIP branch https://github.com/dezgeg/nixpkgs/commits/arm-bootstrap
    since latest master has even more problems with cross-compiling anything.
    (I will eventually push this stuff and make the ARM bootstraps build on hydra.)

--- pkgs/stdenv/linux/bootstrap/armv6l.nix
+++ pkgs/stdenv/linux/bootstrap/armv6l.nix
@@ -1,12 +1,12 @@
 {
   busybox = import <nix/fetchurl.nix> {
-    url = https://dl.dropboxusercontent.com/s/4705ffxjrxxqnh2/busybox?dl=0;
-    sha256 = "032maafy4akcdgccpxdxrza29pkcpm81g8kh1hv8bj2rvssly3z2";
+    url = http://nixos-arm.dezgeg.me/bootstrap/armv6l/busybox;
+    sha256 = "12hij075qapim3jaqc8rb2rvjdradc4937i9mkfa27b6ly1injs0";
     executable = true;
   };
 
   bootstrapTools = import <nix/fetchurl.nix> {
-    url = https://dl.dropboxusercontent.com/s/pen8ieymeqqdvqn/bootstrap-tools.tar.xz?dl=0;
-    sha256 = "0kjpjwi6qw82ca02ppsih3bnhc3y150q23k9d56xzscs0xf5d0dv";
+    url = http://nixos-arm.dezgeg.me/bootstrap/armv6l/bootstrap-tools.tar.xz;
+    sha256 = "14irgvw2wl2ljqbmdislhw3nakmx6wmlm1xki26rk20q2ciic2il";
   };
 }
