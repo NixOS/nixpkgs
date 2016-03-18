b1179bdbcff0df8f3b8220589d8c8dd72dae8bd5
Nikolay Amiantov <ab@fmap.me>
2016-03-18 23:05:04 +0300

    haskellPackages.Agda: use older cpphs
    Package requires cpphs < 0.20.0.

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -941,9 +941,18 @@ self: super: {
   language-c-quote = super.language-c-quote.override { alex = self.alex_3_1_4; };
 
   # https://github.com/agda/agda/issues/1840
-  Agda_2_4_2_3 = super.Agda_2_4_2_3.override { unordered-containers = self.unordered-containers_0_2_5_1; };
-  Agda_2_4_2_4 = super.Agda_2_4_2_4.override { unordered-containers = self.unordered-containers_0_2_5_1; };
-  Agda = super.Agda.override { unordered-containers = self.unordered-containers_0_2_5_1; };
+  Agda_2_4_2_3 = super.Agda_2_4_2_3.override {
+    unordered-containers = self.unordered-containers_0_2_5_1;
+    cpphs = self.cpphs_1_19_3;
+  };
+  Agda_2_4_2_4 = super.Agda_2_4_2_4.override {
+    unordered-containers = self.unordered-containers_0_2_5_1;
+    cpphs = self.cpphs_1_19_3;
+  };
+  Agda = super.Agda.override {
+    unordered-containers = self.unordered-containers_0_2_5_1;
+    cpphs = self.cpphs_1_19_3;
+  };
 
   # We get lots of strange compiler errors during the test suite run.
   jsaddle = dontCheck super.jsaddle;
