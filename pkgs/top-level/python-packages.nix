729c06910899b59309a620d66aff192cdad8690b
Matthias Beyer <mail@beyermatthias.de>
2016-03-19 21:16:15 +0100

    bugseverywhere: init at 1.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -87,6 +87,8 @@ in modules // {
 
   blivet = callPackage ../development/python-modules/blivet { };
 
+  bugseverywhere = callPackage ../applications/version-management/bugseverywhere {};
+
   dbus = callPackage ../development/python-modules/dbus {
     dbus = pkgs.dbus;
   };
