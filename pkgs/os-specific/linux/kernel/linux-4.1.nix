0717ff9af265f28be0e60a3b88d5d9ffe6c641e1
Tim Steinbach <tim@nequissimus.com>
2016-03-19 18:34:01 +0000

    kernel: 4.1.17 -> 4.1.20

--- pkgs/os-specific/linux/kernel/linux-4.1.nix
+++ pkgs/os-specific/linux/kernel/linux-4.1.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.1.17";
+  version = "4.1.20";
   extraMeta.branch = "4.1";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "084ij19vgm27ljrjabqqmlqn27p168nsm9grhr6rajid4n79h6ab";
+    sha256 = "1dpq8dgj351jzm7n6330a4xriz9dxv7d9wxzj9zn9q7ya22np9gs";
   };
 
   kernelPatches = args.kernelPatches;
