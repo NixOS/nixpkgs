e63847f67267bc88d1129c6bd1784a53d6c92039
Tim Steinbach <tim@nequissimus.com>
2016-03-19 18:32:10 +0000

    kernel: 3.18.26 -> 3.18.29 (close #14057)
    (cherry picked from commit 6476075ccfbe11d4c30b33ec0d76f92d53d91097)

--- pkgs/os-specific/linux/kernel/linux-3.18.nix
+++ pkgs/os-specific/linux/kernel/linux-3.18.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.18.26";
+  version = "3.18.29";
   extraMeta.branch = "3.18";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0bhf8x1h5crc9kimprjs7q74p86gsqsdr8nz54nv33c6zmryqsic";
+    sha256 = "0g8vlhifl31dyghiamykrpgj6n8h5w6gh6n88ir57z6lj188vaj8";
   };
 
   kernelPatches = args.kernelPatches;
