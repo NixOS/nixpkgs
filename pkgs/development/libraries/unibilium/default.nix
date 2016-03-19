07601a08f283c259162d2ce1abe749241e7fbafe
Pascal Wittmann <mail@pascal-wittmann.de>
2016-03-19 12:01:22 +0100

    unibilium: 1.1.2 -> 1.2.0

--- pkgs/development/libraries/unibilium/default.nix
+++ pkgs/development/libraries/unibilium/default.nix
@@ -3,13 +3,13 @@
 stdenv.mkDerivation rec {
   name = "unibilium-${version}";
 
-  version = "1.1.2";
+  version = "1.2.0";
 
   src = fetchFromGitHub {
     owner = "mauke";
     repo = "unibilium";
     rev = "v${version}";
-    sha256 = "143j7qrqjxxmdf3yzhn6av2qwiyjjk4cnskkgz6ir2scjfd5gvja";
+    sha256 = "1qsw19irg70l29j7qv403f32l4rrzn53w7881a6h874j9gisl51s";
   };
 
   makeFlags = [ "PREFIX=$(out)" ]
@@ -20,5 +20,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "A very basic terminfo library";
     license = licenses.lgpl3Plus;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ pSub ];
   };
 }
