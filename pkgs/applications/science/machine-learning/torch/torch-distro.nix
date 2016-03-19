a56b9ab5bebf9534404cd46f6b61f82e17164a0b
Sergey Mironov <grrwlf@gmail.com>
2016-03-19 12:35:40 +0300

    torch: use default luajit

--- pkgs/applications/science/machine-learning/torch/torch-distro.nix
+++ pkgs/applications/science/machine-learning/torch/torch-distro.nix
@@ -1,7 +1,6 @@
 { luarocks, lib , stdenv,  writeText , readline,  makeWrapper,
   less, ncurses, cmake, openblas, coreutils, fetchgit, libuuid, czmq, openssl,
-  gnuplot, fetchurl,
-  src
+  gnuplot, fetchurl, luajit, src
 } :
 
 let
@@ -20,47 +19,6 @@ let
   pkgs_gnuplot = gnuplot;
 
   luapkgs = rec {
-
-
-    luajit =
-      stdenv.mkDerivation rec {
-        name    = "luajit-${version}";
-        version = "2.1.0-beta1";
-        luaversion = "5.1";
-
-        src = fetchurl {
-          url    = "http://luajit.org/download/LuaJIT-${version}.tar.gz";
-          sha256 = "06170d38387c59d1292001a166e7f5524f5c5deafa8705a49a46fa42905668dd";
-        };
-
-        enableParallelBuilding = true;
-
-        patchPhase = ''
-          substituteInPlace Makefile \
-            --replace /usr/local $out
-
-          substituteInPlace src/Makefile --replace gcc cc
-        '' + stdenv.lib.optionalString (stdenv.cc.libc != null)
-        ''
-          substituteInPlace Makefile \
-            --replace ldconfig ${stdenv.cc.libc}/sbin/ldconfig
-        '';
-
-        configurePhase = false;
-        buildFlags     = [ "amalg" ]; # Build highly optimized version
-        installPhase   = ''
-          make install INSTALL_INC=$out/include PREFIX=$out
-          ln -s $out/bin/luajit* $out/bin/luajit
-        '';
-
-        meta = with stdenv.lib; {
-          description = "high-performance JIT compiler for Lua 5.1";
-          homepage    = http://luajit.org;
-          license     = licenses.mit;
-          platforms   = platforms.linux ++ platforms.darwin;
-          maintainers = [ maintainers.thoughtpolice ];
-        };
-      };
 
     luarocks = default_luarocks.override {
       lua = luajit;
