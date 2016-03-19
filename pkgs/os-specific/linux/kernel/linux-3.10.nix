6f5f855a2e6564898c96332db8748258fcf0a4c8
Tim Steinbach <tim@nequissimus.com>
2016-03-19 18:25:24 +0000

    kernel: 3.10.99 -> 3.10.101

--- pkgs/os-specific/linux/kernel/linux-3.10.nix
+++ pkgs/os-specific/linux/kernel/linux-3.10.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.10.99";
+  version = "3.10.101";
   extraMeta.branch = "3.10";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1hq90yn2ry36y317px7f0wy55j70ip3wlxa4qsdl9pzlndadcp24";
+    sha256 = "1g8jx6vla8bjhy3xn0s7r6awinxpfr1w8zqfzjsx88pkqbf8qd9n";
   };
 
   kernelPatches = args.kernelPatches;
