59d187ce3d41942b1e670ed634ffbae3974b3475
Greyson Fischer <greyson@foosoft.us>
2016-03-22 16:05:02 -0400

    slack: init at 2.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12678,6 +12678,8 @@ let
 
   hydrogen = callPackage ../applications/audio/hydrogen { };
 
+  slack = callPackage ../applications/networking/instant-messengers/slack { };
+
   spectrwm = callPackage ../applications/window-managers/spectrwm { };
 
   wlc = callPackage ../development/libraries/wlc { };
