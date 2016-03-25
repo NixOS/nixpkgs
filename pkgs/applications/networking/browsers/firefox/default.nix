8bd86b91d9f09ae58ccd90be3e5147eb9ba7279e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-03-25 15:00:50 +0100

    firefox: 45.0 -> 45.0.1
    (cherry picked from commit 79d6dc91fe7486c03fd3a1e663b1e8ce7266c823)

--- pkgs/applications/networking/browsers/firefox/default.nix
+++ pkgs/applications/networking/browsers/firefox/default.nix
@@ -133,8 +133,8 @@ in {
 
   firefox-unwrapped = common {
     pname = "firefox";
-    version = "45.0";
-    sha256 = "1wbrygxj285vs5qbpv3cq26w36bd533779mgi8d0gpxin44hzarn";
+    version = "45.0.1";
+    sha256 = "1j6raz51zcj2hxk0spk5zq8xzxi5nlxxm60dpnb7cs6dv334m0fi";
   };
 
   firefox-esr-unwrapped = common {
