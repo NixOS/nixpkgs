f9b5ed66d10af4ace464b1031ee3b18bc6c8509d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-03-27 23:31:08 +0200

    Inline qt4-clang
    This prevents a potential name/version conflict in nix-env, and a
    large Hydra build for a proprietary package.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13548,12 +13548,10 @@ in
 
   siproxd = callPackage ../applications/networking/siproxd { };
 
-  # Needed for skype
-  qt4-clang = qt4.override {
-    stdenv = clangStdenv;
-  };
   skype = callPackage_i686 ../applications/networking/instant-messengers/skype {
-    qt4 = pkgsi686Linux.qt4-clang;
+    qt4 = qt4.override {
+      stdenv = clangStdenv;
+    };
   };
 
   skype4pidgin = callPackage ../applications/networking/instant-messengers/pidgin-plugins/skype4pidgin { };
