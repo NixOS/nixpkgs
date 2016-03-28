f12e91f2c59a51a2c8f9e9a872d793f3fa1b2784
aszlig <aszlig@redmoonstudios.org>
2016-03-28 11:31:21 +0200

    chromium: Link using gold linker flags
    I originally wanted to do this a long time (a31301d) but IIRC back then
    it didn't compile. Nowadays with the splitup of the gold linking flags
    and the binutils integration, it's merely just a switch to flip, so
    let's do that.
    
    Only tested it by building against the current Chromium stable version
    on 64bit, because right now builds on Hydra seem to time out (because of
    this?) anyway so we have nothing to lose here.
    
    The linking time was hereby reduced from >30 minutes (I didn't measure
    it exactly but looked half an hour later to the build progress and it
    was *still* linking) to about a few seconds, which I guess is even
    though the measurement is quite bogus a tremendous improvement
    nonetheless.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>
    (cherry picked from commit f9fff51c2af672e71c2f713fb9d8cabcbea8d4e4)

--- pkgs/applications/networking/browsers/chromium/common.nix
+++ pkgs/applications/networking/browsers/chromium/common.nix
@@ -154,8 +154,8 @@ let
     gypFlags = mkGypFlags (gypFlagsUseSystemLibs // {
       linux_use_bundled_binutils = false;
       linux_use_bundled_gold = false;
-      linux_use_gold_binary = false;
-      linux_use_gold_flags = false;
+      linux_use_gold_flags = true;
+
       proprietary_codecs = false;
       use_sysroot = false;
       use_gnome_keyring = gnomeKeyringSupport;
