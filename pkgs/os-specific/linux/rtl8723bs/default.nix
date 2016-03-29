ba3399b92fb7bc1a81c91afeb307ed5ea95b06be
Robin Gloster <mail@glob.in>
2016-03-29 10:25:06 +0000

    linuxPackages.rtl8723bs: disable pic hardening

--- pkgs/os-specific/linux/rtl8723bs/default.nix
+++ pkgs/os-specific/linux/rtl8723bs/default.nix
@@ -5,14 +5,16 @@ let
 in
 stdenv.mkDerivation rec {
   name = "rtl8723bs-${kernel.version}-c517f2b";
-  
+
   src = fetchFromGitHub {
     owner = "hadess";
     repo = "rtl8723bs";
     rev = "c517f2bf8bcc3d57311252ea7cd49ae81466eead";
     sha256 = "0phzrhq85g52pi2b74a9sr9l2x6dzlz714k3pix486w2x5axw4xb";
   };
-  
+
+  hardeningDisable = [ "pic" ];
+
   patchPhase = ''
     substituteInPlace ./Makefile --replace /lib/modules/ "${kernel.dev}/lib/modules/"
     substituteInPlace ./Makefile --replace '$(shell uname -r)' "${kernel.modDirVersion}"
@@ -20,12 +22,12 @@ stdenv.mkDerivation rec {
     substituteInPlace ./Makefile --replace '$(MODDESTDIR)' "$out/lib/modules/${kernel.modDirVersion}/kernel/net/wireless/"
     substituteInPlace ./Makefile --replace '/lib/firmware' "$out/lib/firmware"
   '';
-  
+
   preInstall = ''
     mkdir -p "$out/lib/modules/${kernel.modDirVersion}/kernel/net/wireless/"
     mkdir -p "$out/lib/firmware/rtlwifi"
   '';
-   
+
   meta = {
     description = "Realtek SDIO Wi-Fi driver";
     homepage = "https://github.com/hadess/rtl8723bs";
