3e7e1de483a745769225f16eb53287a981d66ba9
Franz Pletz <fpletz@fnordicwalking.de>
2016-03-29 04:53:53 +0200

    libressl_2_3: 2.3.2 -> 2.3.3

--- pkgs/development/libraries/libressl/2.3.nix
+++ pkgs/development/libraries/libressl/2.3.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "libressl-${version}";
-  version = "2.3.2";
+  version = "2.3.3";
 
   src = fetchurl {
     url    = "mirror://openbsd/LibreSSL/${name}.tar.gz";
-    sha256 = "0sm9cjjqvj581sfd4sh0i467sh8p89nq9b8ck2qn3war92p5zx40";
+    sha256 = "1a8anm8nsfyxds03csk738m2cmzjbsb867my1rz5ij3w31k32wvn";
   };
 
   enableParallelBuilding = true;
