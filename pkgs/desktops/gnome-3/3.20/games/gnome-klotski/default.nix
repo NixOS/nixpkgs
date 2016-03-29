35090a0779feff5b4fe68f44683a9bdbcd26d5ff
Damien Cassou <damien@cassou.me>
2016-03-29 13:37:27 +0200

    gnome-klotski: fix dependencies

--- pkgs/desktops/gnome-3/3.20/games/gnome-klotski/default.nix
+++ pkgs/desktops/gnome-3/3.20/games/gnome-klotski/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, gnome3, gtk3, wrapGAppsHook
-, librsvg, libxml2, intltool, itstool }:
+, librsvg, libxml2, intltool, itstool, libgee, libgames-support }:
 
 stdenv.mkDerivation rec {
   inherit (import ./src.nix fetchurl) name src;
 
   buildInputs = [
-    pkgconfig gtk3 wrapGAppsHook intltool itstool librsvg libxml2
+    pkgconfig gtk3 wrapGAppsHook intltool itstool librsvg libxml2 libgee libgames-support
     gnome3.defaultIconTheme
   ];
 
