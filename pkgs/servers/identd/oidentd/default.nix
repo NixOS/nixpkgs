2fb54132c0e9114cedc9b0c0fddd7cdca95605c4
Teo Klestrup Röijezon <teo@nullable.se>
2016-03-31 00:08:27 +0200

    oidentd: Set C dialect to gnu89 (broken by GCC 5)
    (cherry picked from commit 2d4af4b97908f129602390e6ab9c0c7f2be00a62)

--- pkgs/servers/identd/oidentd/default.nix
+++ pkgs/servers/identd/oidentd/default.nix
@@ -3,6 +3,8 @@
 stdenv.mkDerivation rec {
   name = "oidentd-2.0.8";
 
+  CFLAGS = [ "--std=gnu89" ];
+
   src = fetchurl {
     url = "mirror://sourceforge/ojnk/${name}.tar.gz";
     sha256 = "0vzv2086rrxcaavrm3js7aqvyc0grgaqy78x61d8s7r8hz8vwk55";
