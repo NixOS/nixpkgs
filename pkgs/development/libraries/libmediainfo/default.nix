33f345eed0f4956971bf2f7945155839d1b6222b
devhell <^@regexmail.net>
2016-03-31 23:36:50 +0100

    {lib}mediainfo{-gui}: 0.7.83 -> 0.7.84
    Built and tested locally.
    
    From the changelog:
    ```
    Version 0.7.84, 2016-03-31
    x #I122, MPEG-4/MOV: Crash if mdhd timescale is 0
    x MPEG-4/MOV: Infinite loop if malformed stsc / stsz (detected by
      fuzzing)
    x MPEG-TS: some DVB Subtitles were not detected
    x HLS: better handling of media playlists having EXT-X-BYTERANGE
    ```

--- pkgs/development/libraries/libmediainfo/default.nix
+++ pkgs/development/libraries/libmediainfo/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, autoreconfHook, pkgconfig, libzen, zlib }:
 
 stdenv.mkDerivation rec {
-  version = "0.7.83";
+  version = "0.7.84";
   name = "libmediainfo-${version}";
   src = fetchurl {
     url = "http://mediaarea.net/download/source/libmediainfo/${version}/libmediainfo_${version}.tar.xz";
-    sha256 = "0kl5x07j3jp5mnmhpjvdq0a2nnlgvqnhwar0xalvg3b3msdf8417";
+    sha256 = "0k657ynfxcw6lvakc8plz0pzfixlqbsiih2idimk9k1dd4xzhq3d";
   };
 
   nativeBuildInputs = [ autoreconfHook pkgconfig ];
