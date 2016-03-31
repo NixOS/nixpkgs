52c7f08ee678fd2316bb13d96a1cdce2b805eb19
Nikolay Amiantov <ab@fmap.me>
2016-03-31 12:46:46 +0300

    qt4: build with clang on i686

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8465,6 +8465,9 @@ in
 
     # XXX: mariadb doesn't built on fbsd as of nov 2015
     mysql = if (!stdenv.isFreeBSD) then mysql else null;
+
+    # Workaround for #14234
+    stdenv = if system == "i686-linux" then clangStdenv else stdenv;
   };
 
   qt48Full = appendToName "full" (qt48.override {
@@ -13627,11 +13630,7 @@ in
 
   siproxd = callPackage ../applications/networking/siproxd { };
 
-  skype = callPackage_i686 ../applications/networking/instant-messengers/skype {
-    qt4 = pkgsi686Linux.qt4.override {
-      stdenv = pkgsi686Linux.clangStdenv;
-    };
-  };
+  skype = callPackage_i686 ../applications/networking/instant-messengers/skype { };
 
   skype4pidgin = callPackage ../applications/networking/instant-messengers/pidgin-plugins/skype4pidgin { };
 
