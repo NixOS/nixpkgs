4b8b9d926f33e677f358e508216a45b86df9290f
Christian Zagrodnick <cz@flyingcircus.io>
2016-04-01 20:00:41 +0200

    add mongodb role stub

--- nixos/modules/flyingcircus/roles/default.nix
+++ nixos/modules/flyingcircus/roles/default.nix
@@ -19,6 +19,7 @@ in
      ./dovecot.nix
      ./generic.nix
      ./haproxy.nix
+     ./mongodb.nix
      ./mysql.nix
      ./nginx.nix
      ./postgresql.nix
---
+++ nixos/modules/flyingcircus/roles/mongodb.nix
@@ -0,0 +1,71 @@
+{ config, lib, pkgs, ... }: with lib;
+
+let
+  cfg = config.flyingcircus;
+  fclib = import ../lib;
+
+  listen_addresses = fclib.listenAddresses config "ethsrv";
+
+  local_config_path = /etc/local/mongodb/mongodb.conf;
+
+  local_config =
+    if pathExists local_config_path
+    then builtins.readFile  local_config_path
+    else "";
+
+in
+{
+  options = {
+
+    flyingcircus.roles.mongodb = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Enable the Flying Circus MongoDB role.";
+      };
+    };
+
+  };
+
+  config = mkIf cfg.roles.mongodb.enable {
+
+  services.mongodb.enable = true;
+  services.mongodb.dbpath = "/srv/mongodb";
+  services.mongodb.bind_ip = concatStringsSep "," listen_addresses;
+  services.mongodb.extraConfig =
+    ''
+    ipv6 = true
+    '';
+
+    users.users.mongodb = {
+      shell = "/run/current-system/sw/bin/bash";
+      home = "/srv/mongodb";
+    };
+    system.activationScripts.flyingcircus_mongodb = ''
+      install -d -o ${toString config.ids.uids.mongodb} /srv/mongodb
+      install -d -o ${toString config.ids.uids.mongodb} -g service /etc/local/mongodb
+    '';
+    security.sudo.extraConfig = ''
+      # Service users may switch to the mongodb system user
+      %sudo-srv ALL=(mongodb) ALL
+      %service ALL=(mongodb) ALL
+      %sensuclient ALL=(mongodb) ALL
+    '';
+
+
+    environment.etc."local/mongodb/README.txt".text = ''
+        Put your local mongodb configuration into `mongodb.conf` here.
+        It will be joined with the basic config.
+        '';
+
+
+    # flyingcircus.services.sensu-client.checks = {
+    #   postgresql = {
+    #     notification = "PostgreSQL alive";
+    #     command =  "/var/setuid-wrappers/sudo -u postgres check-postgres-alive.rb -d postgres";
+    #   };
+    # };
+
+  };
+
+}
