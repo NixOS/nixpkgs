76aca1993f0ff5f351a8b5430052894d4d6b5845
Peter Hoeg <peter@speartail.com>
2016-04-02 11:13:22 +0800

    Revert "shinken: init at 2.4.3"
    This reverts commit 52882be0c01e5054391b23e00fc31a8aa142e585.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9165,32 +9165,6 @@ in modules // {
     };
   };
 
-  shinken = buildPythonPackage rec {
-    version = "2.4.3";
-    name = "Shinken-${version}";
-    disabled = isPy3k; # Shinken does not yet support python3
-
-    src = pkgs.fetchurl {
-      url = "https://pypi.python.org/packages/source/S/Shinken/${name}.tar.gz";
-      sha256 = "1hxbnxlk2bacb7p6lim9kqy50581m0d3p3y6b9hw0x800lfk0y6q";
-    };
-
-    dontStrip = true; # no binaries to strip, so the build process generates lots of warnings
-
-    # missing: logging tagging
-
-    propagatedBuildInputs = with self; [
-      arrow bottle cffi cherrypy django paramiko pycurl pymongo pyopenssl
-    ];
-
-    meta = {
-      homepage = http://www.shinken-monitoring.org/;
-      description = "A monitoring framework compatible with Nagios configuration and plugins.";
-      license = licenses.agpl3;
-      maintainers = with maintainers; [ peterhoeg ];
-    };
-  };
-
   flexget = buildPythonPackage rec {
     version = "1.2.337";
     name = "FlexGet-${version}";
