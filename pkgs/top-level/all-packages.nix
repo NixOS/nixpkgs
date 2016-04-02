5e53c63b23195cce876c05644b875e3214161b21
Nicolas B. Pierron <nicolas.b.pierron@gmail.com>
2016-04-02 17:11:59 +0000

    Security: Remove pkgs aliases of clementine.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -989,7 +989,7 @@ in
     gst_plugins = [ gst_plugins_base gst_plugins_good gst_plugins_ugly gst_ffmpeg ];
   };
 
-  clementineFree = clementine.free;
+  clementineFree = self.clementine.free;
 
   ciopfs = callPackage ../tools/filesystems/ciopfs { };
 
