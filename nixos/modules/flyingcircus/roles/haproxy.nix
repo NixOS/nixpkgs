c7fb2dbd09afcdd7661fc76a4627f8a163b90ccd
Christian Zagrodnick <cz@flyingcircus.io>
2016-04-03 10:12:21 +0200

    reindet with 2 spaces

--- nixos/modules/flyingcircus/roles/haproxy.nix
+++ nixos/modules/flyingcircus/roles/haproxy.nix
@@ -1,57 +1,54 @@
 { config, lib, pkgs, ... }: with lib;
 
-
 let
-    nonEmptyString = string: if lib.stringLength string > 0 then true else false;
-    configFromFile = file: default: lib.findFirst (nonEmptyString) default [
-        (if (builtins.pathExists file)
-            then builtins.readFile file
-            else "")
-        ];
+  nonEmptyString = string: if lib.stringLength string > 0 then true else false;
+  configFromFile = file: default: lib.findFirst (nonEmptyString) default [
+    (if (builtins.pathExists file)
+     then builtins.readFile file
+     else "")
+  ];
 
-    haproxyCfg = configFromFile /etc/haproxy/haproxy.cfg
-            ''
-                frontend http
-                    bind 127.0.0.1:8002
-            '';
+  haproxyCfg = configFromFile /etc/haproxy/haproxy.cfg ''
+    frontend http
+    bind 127.0.0.1:8002
+  '';
 
 in
 
 {
 
-    options = {
+  options = {
 
-        flyingcircus.roles.haproxy = {
+    flyingcircus.roles.haproxy = {
 
-            enable = mkOption {
-                type = types.bool;
-                default = false;
-                description = "Enable the Flying Circus haproxy server role.";
-            };
-
-        };
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Enable the Flying Circus haproxy server role.";
+      };
 
     };
 
-    config = mkIf config.flyingcircus.roles.haproxy.enable {
+  };
 
-        services.haproxy.enable = true;
-        services.haproxy.config = haproxyCfg;
+  config = mkIf config.flyingcircus.roles.haproxy.enable {
 
-        jobs.fcio-stubs-haproxy = {
-            description = "Create FC IO stubs haproxy";
-            task = true;
+    services.haproxy.enable = true;
+    services.haproxy.config = haproxyCfg;
 
-            startOn = "started networking";
+    jobs.fcio-stubs-haproxy = {
+      description = "Create FC IO stubs haproxy";
+      task = true;
 
-            script =
-                ''
-                    mkdir -p /etc/haproxy
-                    test -L  /etc/haproxy.cfg || ln -s /etc/haproxy/haproxy.cfg /etc/haproxy.cfg
-                    chown -R vagrant: /etc/haproxy.cfg /etc/haproxy
-                '';
-        };
+      startOn = "started networking";
 
+      script = ''
+        mkdir -p /etc/haproxy
+        test -L  /etc/haproxy.cfg || ln -s /etc/haproxy/haproxy.cfg /etc/haproxy.cfg
+        chown -R vagrant: /etc/haproxy.cfg /etc/haproxy
+      '';
     };
+
+  };
 
 }
