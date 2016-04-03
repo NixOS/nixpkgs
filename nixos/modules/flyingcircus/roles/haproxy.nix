12d40babad952977f275b438da6e7c06ebc1efd0
Christian Zagrodnick <cz@flyingcircus.io>
2016-04-03 11:17:25 +0200

    add sensible example configuration, README, and gennto compat symlinks

--- nixos/modules/flyingcircus/roles/haproxy.nix
+++ nixos/modules/flyingcircus/roles/haproxy.nix
@@ -1,14 +1,17 @@
 { config, lib, pkgs, ... }: with lib;
 
 let
-  nonEmptyString = string: if lib.stringLength string > 0 then true else false;
-  configFromFile = file: default: lib.findFirst (nonEmptyString) default [
-    (if (builtins.pathExists file)
-     then builtins.readFile file
-     else "")
+  cfg = config.flyingcircus;
+
+  nonEmptyString = string: (lib.stringLength string > 0);
+  configFromFile = file: default:
+    lib.findFirst (nonEmptyString) default [
+      (if (builtins.pathExists file)
+       then builtins.readFile file
+       else "")
   ];
 
-  haproxyCfg = configFromFile /etc/haproxy/haproxy.cfg ''
+  haproxyCfg = configFromFile /etc/local/haproxy/haproxy.cfg ''
     frontend http
     bind 127.0.0.1:8002
   '';
@@ -35,20 +38,52 @@ in
 
     services.haproxy.enable = true;
     services.haproxy.config = haproxyCfg;
+    system.activationScripts.haproxy = ''
+      install -d -o ${toString config.ids.uids.haproxy} -g service -m 02775 /etc/local/haproxy
+    '';
 
-    jobs.fcio-stubs-haproxy = {
-      description = "Create FC IO stubs haproxy";
-      task = true;
+    environment.etc = {
+      "local/haproxy/README.txt".text = ''
+        HAProxy is enabled on this machine.
 
-      startOn = "started networking";
-
-      script = ''
-        mkdir -p /etc/haproxy
-        test -L  /etc/haproxy.cfg || ln -s /etc/haproxy/haproxy.cfg /etc/haproxy.cfg
-        chown -R vagrant: /etc/haproxy.cfg /etc/haproxy
+        Put your haproxy configuration here as `haproxy.cfg`. There is also
+        an `example-configuration` here.
       '';
-    };
+      "local/haproxy/example-configuration".text = ''
+      # haproxy configuration example - copy to haproxy.cfg and adapt.
 
+      global
+          daemon
+          chroot /var/empty
+          user haproxy
+          group haproxy
+          maxconn 4096
+
+      defaults
+          mode http
+          log global
+          option httplog
+          option dontlognull
+          option http-server-close
+          timeout connect 5s
+          timeout client 30s    # should be equal to server timeout
+          timeout server 30s    # should be equal to client timeout
+          timeout queue 25s   # discard requests sitting too long in the queue
+
+      listen http-in
+          bind 127.0.0.1:8002
+          bind ::1:8002
+
+      '';
+      "haproxy.cfg" = {
+        source = /etc/local/haproxy/haproxy.cfg;
+        enable = cfg.compat.gentoo.enable;
+      };
+      "haproxy" = {
+        source = /etc/local/haproxy;
+        enable = cfg.compat.gentoo.enable;
+      };
+    };
   };
 
 }
