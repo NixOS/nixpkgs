0d12f543d002d59954f67c32921d590dcda95f78
Nicolas B. Pierron <nicolas.b.pierron@gmail.com>
2016-04-04 20:01:46 +0000

    Security: Remove pkgs aliases of gmp.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6856,7 +6856,7 @@ in
   gmp4 = callPackage ../development/libraries/gmp/4.3.2.nix { }; # required by older GHC versions
   gmp5 = callPackage ../development/libraries/gmp/5.1.x.nix { };
   gmp6 = callPackage ../development/libraries/gmp/6.x.nix { };
-  gmp = gmp5;
+  gmp = self.gmp5;
   gmpxx = appendToName "with-cxx" (gmp.override { cxx = true; });
 
   #GMP ex-satellite, so better keep it near gmp
