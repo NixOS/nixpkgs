ea4fb2c887516552419fa431c988951eed21199a
Matthew O'Gorman <mog@rldn.net>
2016-04-05 22:28:19 -0400

    inspectrum: 20160103 -> 20160403

--- pkgs/applications/misc/inspectrum/default.nix
+++ pkgs/applications/misc/inspectrum/default.nix
@@ -1,17 +1,17 @@
-{ stdenv, fetchFromGitHub, pkgconfig, cmake, fftwFloat, qt5 }:
+{ stdenv, fetchFromGitHub, pkgconfig, cmake, boost, fftwFloat, qt5, gnuradio }:
 
 stdenv.mkDerivation rec {
   name = "inspectrum-${version}";
-  version = "20160103";
+  version = "20160403";
 
   src = fetchFromGitHub {
     owner = "miek";
     repo = "inspectrum";
-    rev = "a60d711b46130d37b7c05074285558cd67a28820";
-    sha256 = "1q7izpyi7c9qszygiaq0zs3swihxlss3n52q7wx2jq97hdi2hmzy";
+    rev = "27381dbb30f59267a429c04d17d792d3953a6b99";
+    sha256 = "0y4j62khq6fcv2qqlqi0kn2ix821m5gcqzg72nhc2zzfb3cdm9nm";
   };
 
-  buildInputs = [ pkgconfig cmake qt5.qtbase fftwFloat ];
+  buildInputs = [ pkgconfig cmake qt5.qtbase fftwFloat boost gnuradio ];
   
   meta = with stdenv.lib; {
     description = "Tool for analysing captured signals from sdr receivers";
