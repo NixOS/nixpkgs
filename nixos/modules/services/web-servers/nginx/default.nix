2f4a4c24bb586cf50a78fb9aa1f2dac314ad843a
Franz Pletz <fpletz@fnordicwalking.de>
2016-04-07 17:27:31 +0200

    nginx: Add default proxy headers for tomcat

--- nixos/modules/services/web-servers/nginx/default.nix
+++ nixos/modules/services/web-servers/nginx/default.nix
@@ -54,6 +54,8 @@ let
       proxy_set_header        X-Real-IP $remote_addr;
       proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
       proxy_set_header        X-Forwarded-Proto $scheme;
+      proxy_set_header        X-Forwarded-Host $host;
+      proxy_set_header        X-Forwarded-Server $host;
       proxy_set_header        Accept-Encoding "";
 
       proxy_redirect          off;
