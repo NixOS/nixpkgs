191af3137f7fbea302d3c9f937b398e974963a70
Silvio Frischknecht <github@lupux.ch>
2016-04-07 17:14:33 +0200

    kdeplasma-addons: fix add dependency

--- pkgs/desktops/kde-5/plasma-5.6/kdeplasma-addons.nix
+++ pkgs/desktops/kde-5/plasma-5.6/kdeplasma-addons.nix
@@ -1,7 +1,7 @@
 { plasmaPackage, extra-cmake-modules, kdoctools, ibus, kconfig
 , kconfigwidgets, kcoreaddons, kcmutils, kdelibs4support, ki18n
 , kio, knewstuff, kross, krunner, kservice, kunitconversion
-, plasma-framework, qtdeclarative, qtx11extras
+, plasma-framework, qtdeclarative, qtx11extras, plasma-workspace
 }:
 
 plasmaPackage {
@@ -12,7 +12,7 @@ plasmaPackage {
   ];
   buildInputs = [
     ibus kconfig kconfigwidgets kcoreaddons kcmutils
-    knewstuff kservice kunitconversion
+    knewstuff kservice kunitconversion plasma-workspace
   ];
   propagatedBuildInputs = [
     kdelibs4support kio kross krunner plasma-framework qtdeclarative
