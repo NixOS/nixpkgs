5cce08c86ef4143f26fdb0bec260e30866e9b5b7
Nikolay Amiantov <ab@fmap.me>
2016-04-08 03:11:40 +0300

    steam: add xdg_utils to chrootenv
    (cherry picked from commit c4bbfdb478fdda41d9e8aa2747440025c29f37a3)

--- pkgs/games/steam/chrootenv.nix
+++ pkgs/games/steam/chrootenv.nix
@@ -22,6 +22,8 @@ buildFHSUserEnv {
       which
       # Needed by gdialog, including in the steam-runtime
       perl
+      # Open URLs
+      xdg_utils
     ]
     ++ lib.optional withJava jdk
     ++ lib.optional withPrimus (primus.override {
