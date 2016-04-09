9db29f44c21bc4fb75b7a445b559d6e27ce1dfbd
Christian Theune <ct@flyingcircus.io>
2016-04-09 13:17:36 +0200

    Revert "Repair check_ping"
    This reverts commit ffabefaa5bfae1bf6305537a8f751c2739520f28.

--- pkgs/servers/monitoring/nagios/plugins/official-2.x.nix
+++ pkgs/servers/monitoring/nagios/plugins/official-2.x.nix
@@ -16,8 +16,8 @@ stdenv.mkDerivation rec {
   # configured on the build machine).
   preConfigure= "
     configureFlagsArray=(
-      --with-ping-command='/var/setuid-wrappers/ping -n -w %d -c %d %s'
-      --with-ping6-command='/var/setuid-wrappers/ping6 -n -w %d -c %d %s'
+      --with-ping-command='/var/setuid-wrappers/ping -n -U -w %d -c %d %s'
+      --with-ping6-command='/var/setuid-wrappers/ping6 -n -U -w %d -c %d %s'
     )
   ";
 
