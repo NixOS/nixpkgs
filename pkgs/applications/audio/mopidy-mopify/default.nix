2933fc44ef402bdd53c99f8dae23c72104cce4a2
Franz Pletz <fpletz@fnordicwalking.de>
2016-04-12 14:05:05 +0200

    mopidy-mopify: 1.5.8 -> 1.5.17

--- pkgs/applications/audio/mopidy-mopify/default.nix
+++ pkgs/applications/audio/mopidy-mopify/default.nix
@@ -3,11 +3,11 @@
 pythonPackages.buildPythonApplication rec {
   name = "mopidy-mopify-${version}";
 
-  version = "1.5.8";
+  version = "1.5.17";
 
   src = fetchurl {
     url = "https://github.com/dirkgroenen/mopidy-mopify/archive/${version}.tar.gz";
-    sha256 = "1gq88i5hbyskwhqf51myndqgmrndkyy6gs022sc387fy3dwxmvn0";
+    sha256 = "1qi7f5i87ygn486gxc84njl22y84xrwabpz58y5a1hw7z1lp7l8s";
   };
 
   propagatedBuildInputs = with pythonPackages; [ mopidy configobj ];
