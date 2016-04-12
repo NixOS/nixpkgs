0bda52462bc1a1ac5d526dbb490a219ea02b79cf
Moritz Ulrich <moritz@tarn-vedra.de>
2016-04-12 22:44:07 +0200

    st: Make patches configurable via config.st.patches.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13722,10 +13722,12 @@ in
 
   st = callPackage ../applications/misc/st {
     conf = config.st.conf or null;
+    patches = config.st.patches or null;
   };
 
   st-wayland = callPackage ../applications/misc/st/wayland.nix {
     conf = config.st.conf or null;
+    patches = config.st.patches or null;
   };
 
   stag = callPackage ../applications/misc/stag {
