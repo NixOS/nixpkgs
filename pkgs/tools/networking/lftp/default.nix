d1998b93d765f1c7584018f43da3fa02d71b3956
Franz Pletz <fpletz@fnordicwalking.de>
2016-04-12 20:29:40 +0200

    lftp: 4.6.4 -> 4.7.1

--- pkgs/tools/networking/lftp/default.nix
+++ pkgs/tools/networking/lftp/default.nix
@@ -1,17 +1,22 @@
-{ stdenv, fetchurl, gnutls, pkgconfig, readline, zlib }:
+{ stdenv, fetchurl, gnutls, pkgconfig, readline, zlib, libidn }:
 
 stdenv.mkDerivation rec {
-  name = "lftp-4.6.4";
+  name = "lftp-4.7.1";
 
   src = fetchurl {
     urls = [
       "http://lftp.yar.ru/ftp/${name}.tar.bz2"
       "http://lftp.yar.ru/ftp/old/${name}.tar.bz2"
       ];
-    sha256 = "0zj0dd6s3nzwdawxjp0xw31ipsa4vzimmg5bzq952q2f29vd0akn";
+    sha256 = "0n4l0n6ra6z5lh6v79hc0r0hhrsq0l6c47ir15vmq80sbgc9mmwv";
   };
 
-  buildInputs = [ gnutls pkgconfig readline zlib ];
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [ gnutls readline zlib libidn ];
+
+  configureFlags = [
+    "--with-readline=${readline}"
+  ];
 
   meta = with stdenv.lib; {
     description = "A file transfer program supporting a number of network protocols";
