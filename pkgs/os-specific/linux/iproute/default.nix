48125520870aa6aa918048f2e902e04f3072cd9d
Franz Pletz <fpletz@fnordicwalking.de>
2016-04-12 15:17:53 +0200

    iproute2: 4.3.0 -> 4.5.0

--- pkgs/os-specific/linux/iproute/default.nix
+++ pkgs/os-specific/linux/iproute/default.nix
@@ -3,11 +3,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "iproute2-4.3.0";
+  name = "iproute2-4.5.0";
 
   src = fetchurl {
     url = "mirror://kernel/linux/utils/net/iproute2/${name}.tar.xz";
-    sha256 = "159988vv3fd78bzhisfl1dl4dd7km3vjzs2d8899a0vcvn412fzh";
+    sha256 = "0jj9phsi8m2sbnz7bbh9cf9vckm67hs62ab5srdwnrg4acpjj59z";
   };
 
   patches = lib.optionals enableFan [
