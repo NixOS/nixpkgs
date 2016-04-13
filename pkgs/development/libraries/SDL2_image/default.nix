5076b8f7b22900b43ddd31894fefd8318e3beefc
Franz Pletz <fpletz@fnordicwalking.de>
2016-04-13 04:24:51 +0200

    SDL2_image: 2.0.0 -> 2.0.1

--- pkgs/development/libraries/SDL2_image/default.nix
+++ pkgs/development/libraries/SDL2_image/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, SDL2, libpng, libjpeg, libtiff, libungif, libXpm, zlib }:
 
 stdenv.mkDerivation rec {
-  name = "SDL2_image-2.0.0";
+  name = "SDL2_image-2.0.1";
 
   src = fetchurl {
     url = "http://www.libsdl.org/projects/SDL_image/release/${name}.tar.gz";
-    sha256 = "0d3jlhkmr0j5a2dd5h6y29jfcsj7mkl16wghm6n3nqqp7g3ib65j";
+    sha256 = "0r3z1l7fdn76qkpy7snpkcjqz8dkv2zp6lsqpq25q4m5xsyaygis";
   };
 
   buildInputs = [SDL2 libpng libjpeg libtiff libungif libXpm zlib];
