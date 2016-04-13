5d997ad90a3a430ff8addd6098bef2e570f3178a
Franz Pletz <fpletz@fnordicwalking.de>
2016-04-13 04:25:07 +0200

    SDL2_mixer: 2.0.0 -> 2.0.1

--- pkgs/development/libraries/SDL2_mixer/default.nix
+++ pkgs/development/libraries/SDL2_mixer/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, SDL2, libogg, libvorbis, enableNativeMidi ? false }:
 
 stdenv.mkDerivation rec {
-  name = "SDL2_mixer-2.0.0";
+  name = "SDL2_mixer-2.0.1";
 
   src = fetchurl {
     url = "http://www.libsdl.org/projects/SDL_mixer/release/${name}.tar.gz";
-    sha256 = "0nvjdxjchrajrn0jag877hdx9zb788hsd315zzg1lyck2wb0xkm8";
+    sha256 = "0pv9jzjpcjlbiaybvwrb4avmv46qk7iqxlnqrd2dfj82c4mgc92s";
   };
 
   propagatedBuildInputs = [SDL2 libogg libvorbis];
