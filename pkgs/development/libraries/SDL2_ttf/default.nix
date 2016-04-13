6f0ad14f2056633917c86bd590466b470f895ee7
Franz Pletz <fpletz@fnordicwalking.de>
2016-04-13 04:25:45 +0200

    SDL2_ttf: 2.0.12 -> 2.0.14

--- pkgs/development/libraries/SDL2_ttf/default.nix
+++ pkgs/development/libraries/SDL2_ttf/default.nix
@@ -1,14 +1,14 @@
-{ stdenv, fetchurl, SDL2, freetype }:
+{ stdenv, fetchurl, SDL2, freetype, mesa }:
 
 stdenv.mkDerivation rec {
-  name = "SDL2_ttf-2.0.12";
+  name = "SDL2_ttf-2.0.14";
 
   src = fetchurl {
     url = "https://www.libsdl.org/projects/SDL_ttf/release/${name}.tar.gz";
-    sha256 = "0vkg6lyj278mdpd52map3rfi65fbq16w67ahmmfcl77a8da60a47";
+    sha256 = "0xljwcpvd2knrjdfag5b257xqayplz55mqlszrqp0kpnphh5xnrl";
   };
 
-  buildInputs = [SDL2 freetype];
+  buildInputs = [ SDL2 freetype mesa ];
 
   postInstall = "ln -s $out/include/SDL2/SDL_ttf.h $out/include/";
 
