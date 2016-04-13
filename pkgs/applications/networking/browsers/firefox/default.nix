18a6403c5964b90abbfe33d18472c2ebed31eb90
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-04-13 12:57:53 +0200

    firefox-esr: 45.0.1esr -> 45.0.2esr
    Also, switch to upstream SHA-512 hashes.

--- pkgs/applications/networking/browsers/firefox/default.nix
+++ pkgs/applications/networking/browsers/firefox/default.nix
@@ -18,14 +18,14 @@ assert stdenv.cc ? libc && stdenv.cc.libc != null;
 
 let
 
-common = { pname, version, sha256 }: stdenv.mkDerivation rec {
+common = { pname, version, sha512 }: stdenv.mkDerivation rec {
   name = "${pname}-unwrapped-${version}";
 
   src = fetchurl {
     url =
       let ext = if lib.versionAtLeast version "41.0" then "xz" else "bz2";
       in "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/${version}/source/firefox-${version}.source.tar.${ext}";
-    inherit sha256;
+    inherit sha512;
   };
 
   buildInputs =
@@ -131,13 +131,13 @@ in {
   firefox-unwrapped = common {
     pname = "firefox";
     version = "45.0.2";
-    sha256 = "0lvs47s0k0lzllygzcqzwgg34hwkm8dfq9i0fn5y9m4h0fn9pc56";
+    sha512 = "8c0b7afb41a1a405fe499299d1a8b1138dac52b9ad67bfc8761b70a26f330581c2aa1d76d67075896ec3a0c3f5367b8b58365ebc8b3a01f801fa37071b3de526";
   };
 
   firefox-esr-unwrapped = common {
     pname = "firefox-esr";
-    version = "45.0.1esr";
-    sha256 = "0rkk3cr3r7v5xzbcqhyx8b633v4a16ika0wk46p0ichh9n5mci0s";
+    version = "45.0.2esr";
+    sha512 = "a1e9e9371ee47181b01252d60166c405a7835063dffb4928dfb8abb9f151edd8db1dd2b59f35d7898f1660a0fcd8e41f91a3e799c25b6dd43b6ab056bc5ad6bf";
   };
 
 }
