c73d14a39fe9ac0067f9031d5d37c08ec4c853bd
Myrl Hex <myrl.0xf@gmail.com>
2016-04-13 22:42:06 +0800

    profont: init

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11408,6 +11408,8 @@ in
 
   powerline-fonts = callPackage ../data/fonts/powerline-fonts { };
 
+  profont = callPackage ../data/fonts/profont { };
+
   proggyfonts = callPackage ../data/fonts/proggyfonts { };
 
   sampradaya = callPackage ../data/fonts/sampradaya { };
