e31983404702ad95ca7cb66d361d232af8a67990
Franz Pletz <fpletz@fnordicwalking.de>
2016-04-13 02:35:30 +0200

    libpwquality: 1.2.3 -> 1.3.0

--- pkgs/development/libraries/libpwquality/default.nix
+++ pkgs/development/libraries/libpwquality/default.nix
@@ -1,11 +1,11 @@
 { stdenv, cracklib, fetchurl, python }:
 
 stdenv.mkDerivation rec {
-  name = "libpwquality-1.2.3";
+  name = "libpwquality-1.3.0";
 
   src = fetchurl {
     url = "https://fedorahosted.org/releases/l/i/libpwquality/${name}.tar.bz2";
-    sha256 = "0sjiabvl5277nfxyy96jdz65a0a3pmkkwrfbziwgik83gg77j75i";
+    sha256 = "0aidriag6h0syfm33nzdfdsqgrnsgihwjv3a5lgkqch3w68fmlkl";
   };
 
   buildInputs = [ cracklib python ];
