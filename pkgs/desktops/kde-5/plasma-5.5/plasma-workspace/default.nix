2780894b1bf8d9541ad024fdbb8694e887ddd649
Thomas Tuegel <ttuegel@gmail.com>
2016-04-14 16:34:45 -0500

    kde5.startkde: init at 5.5.5

--- pkgs/desktops/kde-5/plasma-5.5/plasma-workspace/default.nix
+++ pkgs/desktops/kde-5/plasma-5.5/plasma-workspace/default.nix
@@ -1,14 +1,12 @@
 { plasmaPackage, lib, copyPathsToStore
-, extra-cmake-modules, kdoctools, makeQtWrapper
+, extra-cmake-modules, kdoctools
 , baloo, kactivities, kcmutils, kcrash, kdbusaddons, kdeclarative
 , kdelibs4support, kdesu, kdewebkit, kglobalaccel, kidletime
 , kjsembed, knewstuff, knotifyconfig, kpackage, krunner
 , ktexteditor, ktextwidgets, kwallet, kwayland, kwin, kxmlrpcclient
 , libdbusmenu, libkscreen, libSM, libXcursor, networkmanager-qt
 , pam, phonon, plasma-framework, qtquick1, qtscript, qtx11extras, wayland
-, libksysguard, bash, coreutils, gnused, gnugrep, socat, kconfig
-, kinit, kservice, qttools, dbus_tools, mkfontdir, xmessage
-, xprop, xrdb, xset, xsetroot, solid, qtquickcontrols
+, libksysguard, kconfig, solid, qtquickcontrols
 }:
 
 plasmaPackage {
@@ -17,7 +15,6 @@ plasmaPackage {
   nativeBuildInputs = [
     extra-cmake-modules
     kdoctools
-    makeQtWrapper
   ];
   buildInputs = [
     dbus_tools kcmutils kconfig kcrash kdbusaddons kdesu kdewebkit
@@ -35,32 +32,12 @@ plasmaPackage {
   patches = copyPathsToStore (lib.readPathsFromFile ./. ./series);
 
   postPatch = ''
-    substituteInPlace startkde/startkde.cmake \
-        --subst-var-by bash $(type -P bash) \
-        --subst-var-by sed $(type -P sed) \
-        --subst-var-by grep $(type -P grep) \
-        --subst-var-by socat $(type -P socat) \
-        --subst-var-by kcheckrunning $(type -P kcheckrunning) \
-        --subst-var-by xmessage $(type -P xmessage) \
-        --subst-var-by tr $(type -P tr) \
-        --subst-var-by qtpaths $(type -P qtpaths) \
-        --subst-var-by qdbus $(type -P qdbus) \
-        --subst-var-by dbus-launch $(type -P dbus-launch) \
-        --subst-var-by mkfontdir $(type -P mkfontdir) \
-        --subst-var-by xset $(type -P xset) \
-        --subst-var-by xsetroot $(type -P xsetroot) \
-        --subst-var-by xprop $(type -P xprop) \
-        --subst-var-by start_kdeinit_wrapper "${kinit.out}/lib/libexec/kf5/start_kdeinit_wrapper" \
-        --subst-var-by kwrapper5 $(type -P kwrapper5) \
-        --subst-var-by kdeinit5_shutdown $(type -P kdeinit5_shutdown) \
-        --subst-var-by kbuildsycoca5 $(type -P kbuildsycoca5) \
-        --subst-var-by kreadconfig5 $(type -P kreadconfig5) \
-        --subst-var out
     substituteInPlace startkde/kstartupconfig/kstartupconfig.cpp \
         --replace kdostartupconfig5 $out/bin/kdostartupconfig5
   '';
 
   postInstall = ''
+    rm "$out/bin/startkde"
     rm "$out/bin/startplasmacompositor"
     rm "$out/lib/libexec/startplasma"
     rm -r "$out/share/wayland-sessions"
