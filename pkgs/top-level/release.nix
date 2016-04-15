98a7f819d9ad0f8799b14232c4b979fb604aa53e
obadz <dav-github@odav.org>
2016-04-15 23:22:41 +0100

    qt4: add a toplevel entry for the version skype uses
    Otherwise, it won't get built by hydra
    
    abbradar: use `self` as needed for overrides.

--- pkgs/top-level/release.nix
+++ pkgs/top-level/release.nix
@@ -182,6 +182,7 @@ let
       pythonFull = linux;
       sbcl = linux;
       qt3 = linux;
+      qt4_clang = ["i686-linux"];
       quake3demo = linux;
       reiserfsprogs = linux;
       rubber = allBut cygwin;
