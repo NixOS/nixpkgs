0e5122637b8d5f2b3e6328d9202e26ad26c81931
Aneesh Agrawal <aneeshusa@gmail.com>
2016-04-15 16:18:15 -0400

    system-monitor gnome-shell extension: init

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15175,6 +15175,7 @@ in
   gnome3 = self.gnome3_18 // {
     shellExtensions = {
       impatience = callPackage ../desktops/gnome-3/extensions/impatience.nix {};
+      system-monitor = callPackage ../desktops/gnome-3/extensions/system-monitor.nix {};
       volume-mixer = callPackage ../desktops/gnome-3/extensions/volume-mixer.nix {};
       workspace-grid = callPackage ../desktops/gnome-3/extensions/workspace-grid.nix {};
     };
