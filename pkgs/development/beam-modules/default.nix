f36be6edbfc0bfa8f203db0175b489a4e7553301
Eric Merritt <eric@merritt.tech>
2016-04-15 11:09:52 -0700

    hex-packages: update to the latest version
    hex2nix has changed to accomodate elixir and erlang.mk. This includes
    those changes along with updates from hex itself.

--- pkgs/development/beam-modules/default.nix
+++ pkgs/development/beam-modules/default.nix
@@ -2,7 +2,7 @@
 
 let
   self = rec {
-    hexPackages = import ./hex-packages.nix { stdenv = stdenv; callPackage = self.callPackage; };
+    hexPackages = import ./hex-packages.nix { stdenv = stdenv; callPackage = self.callPackage; pkgs = pkgs; };
     callPackage = pkgs.lib.callPackageWith (pkgs // self // hexPackages);
     buildRebar3 = callPackage ./build-rebar3.nix {};
     buildHex = callPackage ./build-hex.nix {};
