fab311d90c013b9481b518a4def0d30027c2244d
Anthony Cowley <acowley@gmail.com>
2016-04-15 18:07:04 -0400

    vtk: darwin compatibility
    Along with PR #14705, this gets vtk working on darwin.
    
    Fixing the test suite is a matter of supplying the necessary frameworks, and
    plumbing the system's CoreFoundation framework into cmake, but that
    change was deemed too intrusive for nixpkgs, so tests are currently
    disabled on darwin.
    
    Changes to the `postConfigure` phase to make the test suite work are of the form:
    
    ```
    sed -i 's,^\(  TARGET_LINK_LIBRARIES(''${KIT}CxxTests vtkRendering
    vtkIO)\),\1\n  TARGET_LINK_LIBRARIES(''${KIT}CxxTests
    /System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation),'
    ../Graphics/Testing/Cxx/CMakeLists.txt
    ```
    
    A similar change of adding the system `CoreFoundation` to the cmake
    `target_link_libraries` command of anything being built with VTK is also
    necessary.

--- pkgs/development/libraries/vtk/default.nix
+++ pkgs/development/libraries/vtk/default.nix
@@ -1,8 +1,10 @@
 { stdenv, fetchurl, fetchpatch, cmake, mesa, libX11, xproto, libXt
-, qtLib ? null }:
+, qtLib ? null
+# Darwin support
+, Cocoa, CoreServices, DiskArbitration, IOKit, CFNetwork, Security, GLUT
+, ApplicationServices, CoreText, IOSurface, cf-private, ImageIO, xpc, libobjc }:
 
 with stdenv.lib;
-
 let
   os = stdenv.lib.optionalString;
   majorVersion = "5.10";
@@ -20,8 +22,13 @@ stdenv.mkDerivation rec {
   # https://bugzilla.redhat.com/show_bug.cgi?id=1138466
   postPatch = "sed '/^#define GL_GLEXT_LEGACY/d' -i ./Rendering/vtkOpenGL.h";
 
-  buildInputs = [ cmake mesa libX11 xproto libXt ]
-    ++ optional (qtLib != null) qtLib;
+  buildInputs =
+    if !stdenv.isDarwin
+    then [ cmake mesa libX11 xproto libXt ] ++ optional (qtLib != null) qtLib
+    else [ cmake qtLib xpc CoreServices DiskArbitration IOKit
+           CFNetwork Security ApplicationServices CoreText IOSurface ImageIO
+           GLUT ];
+  propagatedBuildInputs = stdenv.lib.optionals stdenv.isDarwin [ Cocoa libobjc ];
 
   # Shared libraries don't work, because of rpath troubles with the current
   # nixpkgs camke approach. It wants to call a binary at build time, just
@@ -29,7 +36,15 @@ stdenv.mkDerivation rec {
   # At least, we use -fPIC for other packages to be able to use this in shared
   # objects.
   cmakeFlags = [ "-DCMAKE_C_FLAGS=-fPIC" "-DCMAKE_CXX_FLAGS=-fPIC" ]
-    ++ optional (qtLib != null) [ "-DVTK_USE_QT:BOOL=ON" ];
+    ++ optional (qtLib != null) [ "-DVTK_USE_QT:BOOL=ON" ]
+    ++ optional stdenv.isDarwin [ "-DBUILD_TESTING:BOOL=OFF" ];
+
+  postConfigure = stdenv.lib.optionalString stdenv.isDarwin ''
+    sed -i 's, -F/Applications/Xcode.app/.*\.sdk/System/Library/Frameworks,,' Rendering/CMakeFiles/vtkRendering.dir/flags.make
+    sed -i 's/#define inline//' ../Utilities/vtktiff/tif_config.h.in
+  '';
+
+  doCheck = !stdenv.isDarwin;
 
   enableParallelBuilding = true;
 
