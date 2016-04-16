49558b04d256c1010c76b3491e84f3c1e1db7a98
Nikolay Amiantov <ab@fmap.me>
2016-04-17 02:45:56 +0300

    liqglviewer: move to qmake4Hook

--- pkgs/development/libraries/libqglviewer/default.nix
+++ pkgs/development/libraries/libqglviewer/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, qt4 }:
+{ stdenv, fetchurl, qt4, qmake4Hook }:
 
 stdenv.mkDerivation rec {
   name = "libqglviewer-2.6.3";
@@ -9,13 +9,11 @@ stdenv.mkDerivation rec {
     sha256 = "00jdkyk4wg1356c3ar6nk3hyp494ya3yvshq9m57kfmqpn3inqdy";
   };
 
-  buildInputs = [ qt4 ];
+  buildInputs = [ qt4 qmake4Hook ];
 
-  buildPhase =
+  postPatch =
     ''
       cd QGLViewer
-      qmake PREFIX=$out
-      make
     '';
 
   meta = with stdenv.lib; {
