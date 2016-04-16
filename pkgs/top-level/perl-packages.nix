2d7a4a76cd676f77ad5ac80ae7e8f6ef3928556a
Robert Helgesson <robert@rycee.net>
2016-04-16 18:32:43 +0200

    perl-Archive-Tar: fix license
    Also minor other cleanups.

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -374,15 +374,15 @@ let self = _self // overrides; _self = with self; {
     };
   };
 
-  ArchiveTar = buildPerlPackage {
+  ArchiveTar = buildPerlPackage rec {
     name = "Archive-Tar-2.04";
     src = fetchurl {
-      url = mirror://cpan/authors/id/B/BI/BINGOS/Archive-Tar-2.04.tar.gz;
+      url = "mirror://cpan/authors/id/B/BI/BINGOS/${name}.tar.gz";
       sha256 = "c3741bba06a468a5a4db6a79d772c55cf2f6673cf33241a6e6a758707a71d293";
     };
     meta = {
       description = "Manipulates TAR archives";
-      license = "perl";
+      license = with stdenv.lib.licenses; [ artistic1 gpl1Plus ];
     };
   };
 
