bfd3d1b56f436e96d51a2278ef0efeeb9006f601
Robert Helgesson <robert@rycee.net>
2016-04-17 15:55:11 +0200

    perl-Mozilla-Ldap: fix build

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -8743,6 +8743,7 @@ let self = _self // overrides; _self = with self; {
     version = "1.5.3";
     USE_OPENLDAP = 1;
     LDAPSDKDIR = pkgs.openldap;
+    LDAPSDKLIBDIR = "${pkgs.openldap.out}/lib";
     src = fetchurl {
       url = "https://ftp.mozilla.org/pub/directory/perldap/releases/${version}/src/perl-mozldap-${version}.tar.gz";
       sha256 = "0s0albdw0zvg3w37s7is7gddr4mqwicjxxsy400n1p96l7ipnw4x";
