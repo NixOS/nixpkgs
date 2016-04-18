cd7a5b44784a75c232f30bfff2fc651b186cee19
Lluís Batlle i Rossell <viric@viric.name>
2016-04-18 11:56:43 +0200

    Removing content-pack download from rigsofrods.
    The content packs from 0.37 don't seem to work. We should get one from 0.4, but
    there are license problems, I think.

--- pkgs/games/rigsofrods/default.nix
+++ pkgs/games/rigsofrods/default.nix
@@ -14,23 +14,7 @@ stdenv.mkDerivation rec {
     sha256 = "0h71nrgq5r5cnh20c7wl8jzyaf50dj1b5jdrwihnklpsfyfvjlw4";
   };
 
-  contentPackSrc = fetchurl {
-    url = mirror://sourceforge/rigsofrods/rigsofrods/0.37/content-pack-0.37.zip;
-    sha256 = "0prvn8lxqazadad4mv0nilax9i4vqb9s7dp7mqzvqc0ycmcnf4ps";
-  };
-
   enableParallelBuilding = true;
-
-  #cmakeFlags = [
-  #  "-DROR_USE_CURL=TRUE"
-  #  "-DROR_USE_MYGUI=TRUE"
-  #  "-DROR_USE_OPNEAL=TRUE"
-  #  "-DROR_USE_CAELUM=TRUE"
-  #  "-DROR_USE_PAGED=TRUE"
-  #  "-DROR_USE_ANGELSCRIPT=TRUE"
-  #  "-DROR_USE_SOCKETW=TRUE"
-  #  "-DCMAKE_BUILD_TYPE=Release"
-  #];
 
   installPhase = ''
     sed -e "s@/usr/local/lib/OGRE@${ogre}/lib/OGRE@" -i ../tools/linux/binaries/plugins.cfg
@@ -38,14 +22,8 @@ stdenv.mkDerivation rec {
     cp -r bin/* $out/share/rigsofrods
     cp ../tools/linux/binaries/plugins.cfg $out/share/rigsofrods
     mkdir -p $out/bin
-    ln -s $out/share/rigsofrods/{RoR,rorconfig} $out/bin
-    cd $out/share/rigsofrods
-    mkdir packs
-    cd packs
-    unzip "${contentPackSrc}"
+    ln -s $out/share/rigsofrods/{RoR,RoRConfig} $out/bin
   '';
-
-  #patches = [ ./paths.patch ];
 
   buildInputs = [ wxGTK30 freeimage cmake zziplib mesa boost pkgconfig
     libuuid openal ogre ois curl gtk mygui unzip angelscript
