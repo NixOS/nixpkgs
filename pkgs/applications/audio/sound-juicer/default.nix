6dd2c84e93ef18ff7c41cd074b08b90efcd49a8a
Franz Pletz <fpletz@fnordicwalking.de>
2016-04-18 22:49:34 +0200

    sound-juicer: Fix merge fail

--- pkgs/applications/audio/sound-juicer/default.nix
+++ pkgs/applications/audio/sound-juicer/default.nix
@@ -18,7 +18,6 @@ in stdenv.mkDerivation rec {
   buildInputs = [ pkgconfig gtk3 intltool itstool libxml2 brasero libcanberra_gtk3
                   gnome3.gsettings_desktop_schemas libmusicbrainz5 libdiscid isocodes
                   gnome3.dconf wrapGAppsHook
-                  makeWrapper gnome3.dconf
                   gst_all_1.gstreamer gst_all_1.gst-plugins-base
                   gst_all_1.gst-plugins-good gst_all_1.gst-plugins-bad
                   gst_all_1.gst-libav
