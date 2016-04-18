815460df40acf183617ddd3baccd07bcababedda
Jos van den Oever <github@vandenoever.info>
2016-04-18 15:19:21 +0000

    davmail: 4.7.1 -> 4.7.2

--- pkgs/applications/networking/davmail/default.nix
+++ pkgs/applications/networking/davmail/default.nix
@@ -1,10 +1,10 @@
 { fetchurl, stdenv, jre, glib, libXtst, gtk, makeWrapper }:
 
 stdenv.mkDerivation rec {
-  name = "davmail-4.7.1";
+  name = "davmail-4.7.2";
   src = fetchurl {
-    url = "mirror://sourceforge/davmail/4.7.1/davmail-linux-x86_64-4.7.1-2416.tgz";
-    sha256 = "c3bf1a3a94f35586a3a8d2d28cdaf8c9514a8cf904a51fd74961e93909c9d2a4";
+    url = "mirror://sourceforge/davmail/4.7.2/davmail-linux-x86_64-4.7.2-2427.tgz";
+    sha256 = "0hanv90klhbsxcizz8xddd4zn1j59bs43j4qllg04r2b1cbpkgh2";
   };
 
   buildInputs = [ makeWrapper ];
