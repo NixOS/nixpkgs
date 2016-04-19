40ebe0bad3cce545f9f5eb41ca6749b3d7a76e02
Thomas Tuegel <ttuegel@gmail.com>
2016-04-18 20:29:19 -0500

    qt55.polkit-qt: use multiple outputs

--- pkgs/development/libraries/polkit-qt-1/default.nix
+++ pkgs/development/libraries/polkit-qt-1/default.nix
@@ -9,6 +9,8 @@ assert (withQt5 -> qtbase != null); assert (!withQt5 -> qt4 != null);
 stdenv.mkDerivation {
   name = "polkit-qt-1-0.112.0";
 
+  outputs = [ "dev" "out" ];
+
   src = fetchurl {
     url = "mirror://kde/stable/apps/KDE4.x/admin/polkit-qt-1-0.112.0.tar.bz2";
     sha256 = "1ip78x20hjqvm08kxhp6gb8hf6k5n6sxyx6kk2yvvq53djzh7yv7";
@@ -17,6 +19,16 @@ stdenv.mkDerivation {
   nativeBuildInputs = [ cmake pkgconfig ] ++ optional (!withQt5) automoc4;
 
   propagatedBuildInputs = [ polkit glib ] ++ [(if withQt5 then qtbase else qt4)];
+
+  preConfigure = ''
+    cmakeFlags+=" -DCMAKE_INSTALL_LIBDIR=''${!outputLib}/lib"
+  '';
+
+  postFixup = ''
+    # Fix library location in CMake module
+    sed -i "$dev/lib/cmake/PolkitQt5-1/PolkitQt5-1Config.cmake" \
+        -e "s,set_and_check.POLKITQT-1_LIB_DIR.*$,set_and_check(POLKITQT-1_LIB_DIR \"''${!outputLib}/lib\"),"
+  '';
 
   meta = {
     description = "A Qt wrapper around PolKit";
