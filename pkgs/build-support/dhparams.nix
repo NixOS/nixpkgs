fcd4cba36032e8c54b947e4f59f278bb4f4fbdef
Tristan Helmich <tristan.helmich@gmail.com>
2016-04-20 18:39:09 +0200

    dhparams: fix multiple output store path

--- pkgs/build-support/dhparams.nix
+++ pkgs/build-support/dhparams.nix
@@ -6,12 +6,12 @@ assert (builtins.elem type [ "dhparam" "moduli" ]);
 
 builtins.getAttr type {
   dhparam = runCommand "dhparams-${name}.pem" {} ''
-    ${openssl}/bin/openssl dhparam -out $out ${toString bits}
+    ${openssl.bin}/bin/openssl dhparam -out $out ${toString bits}
   '';
 
   moduli = runCommand "ssh-moduli-${name}" {} ''
-    ${openssh}/bin/ssh-keygen -G moduli.candidates -b ${toString bits}
-    ${openssh}/bin/ssh-keygen -T $out -f moduli.candidates
+    ${openssh.bin}/bin/ssh-keygen -G moduli.candidates -b ${toString bits}
+    ${openssh.bin}/bin/ssh-keygen -T $out -f moduli.candidates
   '';
 }
 
