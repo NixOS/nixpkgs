901a9814327b5595eee69f11df81506d971b6c99
Tiago de Paula Peixoto <tiago@skewed.de>
2016-04-20 13:25:32 +0200

    graph-tool: Use correct upstream source

--- pkgs/development/python-modules/graph-tool/2.x.x.nix
+++ pkgs/development/python-modules/graph-tool/2.x.x.nix
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
   };
 
   src = fetchurl {
-    url = "https://github.com/count0/graph-tool/archive/release-${version}.tar.gz";
+    url = "https://downloads.skewed.de/graph-tool/graph-tool-${version}.tar.bz2";
     sha256 = "3784d4a15b6b5d0e6dab5e3941d24c1e3fee509f7abf9008f64fef2760bd610d";
   };
 
