55830cfb3305c0b60544304661fcf821f95bfd87
Domen Kožar <domen@dev.si>
2016-04-20 16:11:34 +0100

    add linuxPackages.dpdk: libs and drivers for fast packet processing
    (cherry picked from commit e6899cc80c3ceb7116d21d486b234bd8bd127a08)
    Signed-off-by: Domen Kožar <domen@dev.si>

---
+++ pkgs/os-specific/linux/dpdk/default.nix
@@ -0,0 +1,40 @@
+{ stdenv, lib, kernel, fetchurl }:
+
+assert lib.versionAtLeast kernel.version "3.18";
+
+stdenv.mkDerivation rec {
+  name = "dpdk-${version}-${kernel.version}";
+  version = "16.04";
+
+  src = fetchurl {
+    url = "http://dpdk.org/browse/dpdk/snapshot/dpdk-${version}.tar.gz";
+    sha256 = "0yrz3nnhv65v2jzz726bjswkn8ffqc1sr699qypc9m78qrdljcfn";
+  };
+
+  RTE_KERNELDIR = "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build";
+  RTE_TARGET = "x86_64-native-linuxapp-gcc";
+
+  enableParallelBuilding = true;
+  outputs = [ "out" "examples" ];
+
+  buildPhase = ''
+    make T=x86_64-native-linuxapp-gcc config
+    make T=x86_64-native-linuxapp-gcc install
+    make T=x86_64-native-linuxapp-gcc examples
+  '';
+
+  installPhase = ''
+    cp -pr x86_64-native-linuxapp-gcc/{app,lib,include,kmod} $out/
+
+    mkdir $examples
+    cp -pr examples/* $examples/
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Set of libraries and drivers for fast packet processing";
+    homepage = http://dpdk.org/;
+    license = with licenses; [ lgpl21 gpl2 bsd2 ];
+    platforms = platforms.linux;
+    maintainers = [ maintainers.iElectric ];
+  };
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10550,6 +10550,8 @@ let
 
     cpupower = callPackage ../os-specific/linux/cpupower { };
 
+    dpdk = callPackage ../os-specific/linux/dpdk { };
+
     e1000e = callPackage ../os-specific/linux/e1000e {};
 
     v4l2loopback = callPackage ../os-specific/linux/v4l2loopback { };
