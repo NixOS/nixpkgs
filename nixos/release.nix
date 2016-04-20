eb141309345e0200da8536792cfef41297600819
Octavian Cerna <octavian.cerna@gmail.com>
2016-04-20 17:41:05 +0300

    quagga test: Add test for the quagga service.

--- nixos/release.nix
+++ nixos/release.nix
@@ -279,6 +279,7 @@ in rec {
   tests.printing = callTest tests/printing.nix {};
   tests.proxy = callTest tests/proxy.nix {};
   tests.pumpio = callTest tests/pump.io.nix {};
+  tests.quagga = callTest tests/quagga.nix {};
   tests.quake3 = callTest tests/quake3.nix {};
   tests.runInMachine = callTest tests/run-in-machine.nix {};
   tests.sddm = callTest tests/sddm.nix {};
