047df60ba7a3e81d50e831534f629fc654faa832
Nikolay Amiantov <ab@fmap.me>
2016-04-22 21:02:52 +0300

    speechd: 0.7.1 -> 0.8.3
    Make it actually useful -- support sound output and several voice modules
    
    (cherry picked from commit f3435096e2110ab78e4edeba3bfb0d905d19b2ed)

--- pkgs/development/libraries/speechd/default.nix
+++ pkgs/development/libraries/speechd/default.nix
@@ -1,20 +1,37 @@
-{ fetchurl, stdenv, dotconf, glib, pkgconfig }:
+{ fetchurl, lib, stdenv, intltool, libtool, pkgconfig, glib, dotconf, libsndfile
+, libao, python3Packages
+, withEspeak ? false, espeak
+, withPico ? true, svox
+}:
 
 stdenv.mkDerivation rec {
-  name = "speech-dispatcher-" + version;
-  version = "0.7.1";
+  name = "speech-dispatcher-${version}";
+  version = "0.8.3";
 
   src = fetchurl {
     url = "http://www.freebsoft.org/pub/projects/speechd/${name}.tar.gz";
-    sha256 = "0laag72iw03545zggdzcr860b8q7w1vrjr3csd2ldps7jhlwzad8";
+    sha256 = "0kqy7z4l59n2anc7xn588w4rkacig1hajx8c53qrh90ypar978ln";
   };
 
-  buildInputs = [ dotconf glib pkgconfig ];
+  buildInputs = [ intltool libtool glib dotconf libsndfile libao python3Packages.python ]
+             ++ lib.optional withEspeak espeak
+             ++ lib.optional withPico svox;
+  nativeBuildInputs = [ pkgconfig python3Packages.wrapPython ];
 
-  meta = {
+  pythonPath = with python3Packages; [ pyxdg ];
+
+  postPatch = lib.optionalString withPico ''
+    sed -i 's,/usr/share/pico/lang/,${svox}/share/pico/lang/,g' src/modules/pico.c
+  '';
+
+  postInstall = ''
+    wrapPythonPrograms
+  '';
+
+  meta = with stdenv.lib; {
     description = "Common interface to speech synthesis";
-
     homepage = http://www.freebsoft.org/speechd;
-    license = stdenv.lib.licenses.gpl2Plus;
+    license = licenses.gpl2Plus;
+    platforms = platforms.linux;
   };
 }
