23f3d86a3be8188a7623fe74727e8c842d01e633
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2016-04-23 01:30:53 +0200

    sane-backends-git -> 2016-04-23
    More Pixma fixes.

--- pkgs/applications/graphics/sane/backends/git.nix
+++ pkgs/applications/graphics/sane/backends/git.nix
@@ -1,10 +1,10 @@
 { callPackage, fetchgit, ... } @ args:
 
 callPackage ./generic.nix (args // {
-  version = "2016-04-20";
+  version = "2016-04-23";
   src = fetchgit {
-    sha256 = "f4a20eb41d72ff5961484ef164df44805a987523153107cc6f45acfb3d0208e2";
-    rev = "4b2f171a13248a8e3d79379e368c54fb71ed97e2";
+    sha256 = "11bf60cd5a6b314e855a69a6f57a5ca0db3254527def55662bce25810a2314df";
+    rev = "c8169b1e656f7f95c67946298da5a0e1c143f8e8";
     url = "git://alioth.debian.org/git/sane/sane-backends.git";
   };
 })
