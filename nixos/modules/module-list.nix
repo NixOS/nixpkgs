80e8a10c47c9f28399209547878034f750532df9
Matt McHenry <github@matt.mchenryfamily.org>
2016-04-23 14:49:48 -0400

    nixos/dnscache: add module
    with improvements suggested by Jörg Thalheim <joerg@thalheim.io>
    
    (cherry picked from commit 1b7e5eaa79241080eac2a0b79883a17c9e5f4731)

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -423,6 +423,7 @@
   ./services/networking/ddclient.nix
   ./services/networking/dhcpcd.nix
   ./services/networking/dhcpd.nix
+  ./services/networking/dnscache.nix
   ./services/networking/dnschain.nix
   ./services/networking/dnscrypt-proxy.nix
   ./services/networking/dnscrypt-wrapper.nix
