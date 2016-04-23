a2e40003a19de448bf0988f7c6a429a47b1fad56
Thomas Tuegel <ttuegel@gmail.com>
2016-04-23 07:41:50 -0500

    kde5_latest -> kde5

--- pkgs/desktops/kde-5/plasma-5.5/kgamma5.nix
+++
@@ -1,9 +0,0 @@
-{ plasmaPackage, extra-cmake-modules, kdoctools, kdelibs4support
-, qtx11extras
-}:
-
-plasmaPackage {
-  name = "kgamma5";
-  nativeBuildInputs = [ extra-cmake-modules kdoctools ];
-  propagatedBuildInputs = [ kdelibs4support qtx11extras ];
-}
