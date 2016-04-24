98bef7e0bde3b7b9cc4deff282c8863fc5b504a5
Sven Keidel <svenkeidel@googlemail.com>
2016-04-24 20:42:53 +0200

    brogue: init at 1.7.4
    with fixups by joachifm:
    - Correct license (AGPL-3)
    - Removed redundant build input
    - Cleanup description
    
    Closes #15244

---
+++ pkgs/development/libraries/libtcod/default.nix
@@ -0,0 +1,33 @@
+{ stdenv, fetchFromBitbucket, cmake, SDL, mesa, upx }:
+
+stdenv.mkDerivation rec {
+
+  name = "libtcod-${version}";
+  version = "1.5.1";
+
+  src = fetchFromBitbucket {
+    owner = "libtcod";
+    repo = "libtcod";
+    rev = "1.5.1";
+    sha256 = "1ibsnmnim712npxkqklc5ibnd32hgsx2yzyfzzc5fis5mhinbl63";
+  };
+
+  prePatch = ''
+    sed -i CMakeLists.txt \
+      -e "s,SET(ROOT_DIR.*,SET(ROOT_DIR $out),g" \
+      -e "s,SET(INSTALL_DIR.*,SET(INSTALL_DIR $out),g"
+    echo 'INSTALL(DIRECTORY include DESTINATION .)' >> CMakeLists.txt
+  '';
+
+  cmakeFlags="-DLIBTCOD_SAMPLES=OFF";
+
+  buildInputs = [ cmake SDL mesa upx ];
+
+  meta = {
+    description = "API for roguelike games";
+    homepage = http://roguecentral.org/doryen/libtcod/;
+    license = stdenv.lib.licenses.bsd3;
+    platforms = stdenv.lib.platforms.linux;
+    maintainers = [ stdenv.lib.maintainers.skeidel ];
+  };
+}
