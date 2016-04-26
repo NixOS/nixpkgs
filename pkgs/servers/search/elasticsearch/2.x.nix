ae8a3a14f03cda32340dd55905b4d8c1b18b0446
Philipp Hausmann <phile314@users.noreply.github.com>
2016-04-26 12:49:10 +0200

    elasticsearch: 2.2.0 -> 2.3.1 (#14993)

--- pkgs/servers/search/elasticsearch/2.x.nix
+++ pkgs/servers/search/elasticsearch/2.x.nix
@@ -3,12 +3,12 @@
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
-  version = "2.2.0";
+  version = "2.3.1";
   name = "elasticsearch-${version}";
 
   src = fetchurl {
     url = "https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${version}/${name}.tar.gz";
-    sha256 = "0c2d0mpcr8lpvifvgp2pfj7avdi7fa8a5vib0gqdap7mw60wqw7d";
+    sha256 = "1fqf24bv4jfxai507jh91zm94dp1j8kmygljvpy7414f0drjw2gh";
   };
 
   patches = [ ./es-home-2.x.patch ];
