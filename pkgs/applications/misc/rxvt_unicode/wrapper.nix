c1e9ea6c86343541978593fe1af068d9d62d4e2a
Nikolay Amiantov <ab@fmap.me>
2016-04-26 15:27:05 +0300

    rxvt_unicode-with-plugins: use symlinkJoin

--- pkgs/applications/misc/rxvt_unicode/wrapper.nix
+++ pkgs/applications/misc/rxvt_unicode/wrapper.nix
@@ -1,28 +1,21 @@
-{ stdenv, buildEnv, rxvt_unicode, makeWrapper, plugins }:
+{ stdenv, symlinkJoin, rxvt_unicode, makeWrapper, plugins }:
 
 let
   rxvt = rxvt_unicode.override {
     perlSupport = true;
   };
 
-  drv = buildEnv {
-    name = "${rxvt.name}-with-plugins";
+in symlinkJoin {
+  name = "${rxvt.name}-with-plugins";
 
-    paths = [ rxvt ] ++ plugins;
+  paths = [ rxvt ] ++ plugins;
 
-    postBuild = ''
-      # TODO: This could be avoided if buildEnv could be forced to create all directories
-      if [ -L $out/bin ]; then
-        rm $out/bin
-        mkdir $out/bin
-        for i in ${rxvt}/bin/*; do
-          ln -s $i $out/bin
-        done
-      fi
-      wrapProgram $out/bin/urxvt \
-        --suffix-each URXVT_PERL_LIB ':' "$out/lib/urxvt/perl"
-      wrapProgram $out/bin/urxvtd \
-        --suffix-each URXVT_PERL_LIB ':' "$out/lib/urxvt/perl"
-    '';
-  };
-in stdenv.lib.overrideDerivation drv (x : { buildInputs = x.buildInputs ++ [ makeWrapper ]; })
+  buildInputs = [ makeWrapper ];
+
+  postBuild = ''
+    wrapProgram $out/bin/urxvt \
+      --suffix-each URXVT_PERL_LIB ':' "$out/lib/urxvt/perl"
+    wrapProgram $out/bin/urxvtd \
+      --suffix-each URXVT_PERL_LIB ':' "$out/lib/urxvt/perl"
+  '';
+}
