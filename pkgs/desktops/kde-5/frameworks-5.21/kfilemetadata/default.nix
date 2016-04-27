01e82f9256de42efe1c7e3f454323505444fe73d
Thomas Tuegel <ttuegel@gmail.com>
2016-04-27 08:46:38 -0500

    kde5.kfilemetadata propagate all buildInputs

--- pkgs/desktops/kde-5/frameworks-5.21/kfilemetadata/default.nix
+++ pkgs/desktops/kde-5/frameworks-5.21/kfilemetadata/default.nix
@@ -4,11 +4,11 @@
 
 kdeFramework {
   name = "kfilemetadata";
+  meta = { maintainers = [ lib.maintainers.ttuegel ]; };
   patches = copyPathsToStore (lib.readPathsFromFile ./. ./series);
   nativeBuildInputs = [ extra-cmake-modules ];
-  buildInputs = [ attr ebook_tools exiv2 ffmpeg karchive poppler taglib ];
-  propagatedBuildInputs = [ qtbase ki18n ];
-  meta = {
-    maintainers = [ lib.maintainers.ttuegel ];
-  };
+  buildInputs = [ ];
+  propagatedBuildInputs = [
+    attr ebook_tools exiv2 ffmpeg karchive ki18n poppler qtbase taglib
+  ];
 }
