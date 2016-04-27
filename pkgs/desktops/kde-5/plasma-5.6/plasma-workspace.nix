0e660bf27f204ed82ca9ed66c5c7b711373180e4
Thomas Tuegel <ttuegel@gmail.com>
2016-04-27 06:55:41 -0500

    kde5.plasma-workspace: wrap Qt programs

--- pkgs/desktops/kde-5/plasma-5.6/plasma-workspace.nix
+++ pkgs/desktops/kde-5/plasma-5.6/plasma-workspace.nix
@@ -35,4 +35,21 @@ plasmaPackage {
     rm "$out/lib/libexec/startplasma"
     rm -r "$out/share/wayland-sessions"
   '';
+
+  preFixup = ''
+    wrapQtProgram $out/bin/kcheckrunning
+    wrapQtProgram $out/bin/kcminit
+    wrapQtProgram $out/bin/kcminit_startup
+    wrapQtProgram $out/bin/kdostartupconfig5
+    wrapQtProgram $out/bin/klipper
+    wrapQtProgram $out/bin/krunner
+    wrapQtProgram $out/bin/ksmserver
+    wrapQtProgram $out/bin/ksplashqml
+    wrapQtProgram $out/bin/kstartupconfig5
+    wrapQtProgram $out/bin/kuiserver5
+    wrapQtProgram $out/bin/plasmashell
+    wrapQtProgram $out/bin/plasmawindowed
+    wrapQtProgram $out/bin/systemmonitor
+    wrapQtProgram $out/bin/xembedsniproxy
+  '';
 }
