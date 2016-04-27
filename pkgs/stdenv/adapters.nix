5b7bdb28615cd033d38267328d8d74723570beec
John Ericson <Ericson2314@Yahoo.com>
2016-04-26 20:11:31 -0700

    Remove more dead code: `stdenvDarwinNaked` no longer exists

--- pkgs/stdenv/adapters.nix
+++ pkgs/stdenv/adapters.nix
@@ -238,11 +238,4 @@ rec {
         NIX_CFLAGS_LINK = toString (args.NIX_CFLAGS_LINK or "") + " -fuse-ld=gold";
       });
     };
-
-  dropCxx = drv: drv.override {
-    stdenv = if pkgs.stdenv.isDarwin
-      then pkgs.allStdenvs.stdenvDarwinNaked
-      else pkgs.stdenv;
-  };
-
 }
