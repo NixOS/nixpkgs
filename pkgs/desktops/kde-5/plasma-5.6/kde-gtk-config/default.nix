a43ef07c72e1fa36759fab0b90af7387e811390c
Thomas Tuegel <ttuegel@gmail.com>
2016-04-28 12:50:55 -0500

    kde5.kde-gtk-config: propagate all buildInputs

--- pkgs/desktops/kde-5/plasma-5.6/kde-gtk-config/default.nix
+++ pkgs/desktops/kde-5/plasma-5.6/kde-gtk-config/default.nix
@@ -16,11 +16,10 @@ plasmaPackage {
   name = "kde-gtk-config";
   patches = [ ./0001-follow-symlinks.patch ];
   nativeBuildInputs = [ extra-cmake-modules ];
-  buildInputs = [
-    glib gtk2 gtk3 karchive kcmutils kconfigwidgets kiconthemes
+  propagatedBuildInputs = [
+    ki18n kio glib gtk2 gtk3 karchive kcmutils kconfigwidgets kiconthemes
     knewstuff
   ];
-  propagatedBuildInputs = [ ki18n kio ];
   cmakeFlags = [
     "-DGTK2_GLIBCONFIG_INCLUDE_DIR=${glib.out}/lib/glib-2.0/include"
     "-DGTK2_GDKCONFIG_INCLUDE_DIR=${gtk2.out}/lib/gtk-2.0/include"
