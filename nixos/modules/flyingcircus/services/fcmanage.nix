72a77630361901165072fe4961923b35cd4728e2
Christian Kauhaus <kc@flyingcircus.io>
2016-04-28 10:09:47 +0200

    Source clean up

--- nixos/modules/flyingcircus/services/fcmanage.nix
+++ nixos/modules/flyingcircus/services/fcmanage.nix
@@ -6,7 +6,6 @@
 with lib;
 
 let
-  syscfg = config;
   cfg = config.flyingcircus;
 
 in {
@@ -41,14 +40,13 @@ in {
         restartIfChanged = false;
         unitConfig.X-StopOnRemoval = false;
         serviceConfig.Type = "oneshot";
-
         path = [config.system.build.nixos-rebuild];
 
         # This configuration is stolen from NixOS' own automatic updater.
         environment = config.nix.envVars // {
           inherit (config.environment.sessionVariables) NIX_PATH SSL_CERT_FILE;
           HOME = "/root";
-          PATH = "/run/current-system/sw/bin:/run/current-system/sw/sbin";
+          PATH = "/run/current-system/sw/sbin:/run/current-system/sw/bin";
         };
         script = ''
           ${pkgs.fcmanage}/bin/fc-manage -E ${cfg.enc_path} ${cfg.agent.steps}
@@ -72,7 +70,7 @@ in {
         wantedBy = [ "timers.target" ];
         timerConfig = {
           Unit = "fc-manage.service";
-          # XXX This 10s thing is annoying. There seems to be an issue that
+          # This 15s thing is annoying. There seems to be an issue that
           # networking isn't _really_ up when the timer triggers for the
           # first time even though the 'network-online.target' is waited
           # for.
