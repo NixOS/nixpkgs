f3dc4d9ec32dba8f9694305f00e2e706f66d8085
Josef Kemetmueller <josef.kemetmueller@aon.at>
2016-04-28 20:43:21 +0200

    superlu: init at 5.2.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16382,6 +16382,8 @@ in
   suitesparse_4_4 = callPackage ../development/libraries/science/math/suitesparse {};
   suitesparse = suitesparse_4_4;
 
+  superlu = callPackage ../development/libraries/science/math/superlu {};
+
   ipopt = callPackage ../development/libraries/science/math/ipopt { openblas = openblasCompat; };
 
   gmsh = callPackage ../applications/science/math/gmsh { };
