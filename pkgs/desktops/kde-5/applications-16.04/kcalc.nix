d3b1d5740e9836bbc1a4cb2223f05db9eeeb88cf
Thomas Tuegel <ttuegel@gmail.com>
2016-04-28 13:10:53 -0500

    kde5.kcalc: propagate buildInputs

--- pkgs/desktops/kde-5/applications-16.04/kcalc.nix
+++ pkgs/desktops/kde-5/applications-16.04/kcalc.nix
@@ -13,27 +13,18 @@
 
 kdeApp {
   name = "kcalc";
-
-  nativeBuildInputs = [
-    extra-cmake-modules
-    kdoctools
-  ];
-
-  buildInputs = [
-    gmp
-    kconfig
-    kconfigwidgets
-    kguiaddons
-    kinit
-    knotifications
-  ];
-
-  postInstall = ''
-    wrapQtProgram "$out/bin/kcalc"
-  '';
-
   meta = {
     license = with lib.licenses; [ gpl2 ];
     maintainers = [ lib.maintainers.fridh ];
   };
+  nativeBuildInputs = [
+    extra-cmake-modules
+    kdoctools
+  ];
+  propagatedBuildInputs = [
+    gmp kconfig kconfigwidgets kguiaddons kinit knotifications
+  ];
+  postInstall = ''
+    wrapQtProgram "$out/bin/kcalc"
+  '';
 }
