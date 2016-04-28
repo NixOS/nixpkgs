87b9c4a6b6e0c382f744438c5fa52fbff97b3e54
Charles Strahan <charles@cstrahan.com>
2016-04-27 20:17:01 -0400

    zookeeper: replace tabs with spaces

--- pkgs/servers/zookeeper/default.nix
+++ pkgs/servers/zookeeper/default.nix
@@ -1,36 +1,36 @@
 { stdenv, fetchurl, jre, makeWrapper, bash }:
 
 stdenv.mkDerivation rec {
-	name = "zookeeper-3.4.6";
+  name = "zookeeper-3.4.6";
 
-	src = fetchurl {
-		url = "mirror://apache/zookeeper/${name}/${name}.tar.gz";
-		sha256 = "01b3938547cd620dc4c93efe07c0360411f4a66962a70500b163b59014046994";
-	};
+  src = fetchurl {
+    url = "mirror://apache/zookeeper/${name}/${name}.tar.gz";
+    sha256 = "01b3938547cd620dc4c93efe07c0360411f4a66962a70500b163b59014046994";
+  };
 
-	buildInputs = [ makeWrapper jre ];
+  buildInputs = [ makeWrapper jre ];
 
-	phases = ["unpackPhase" "installPhase"];
+  phases = ["unpackPhase" "installPhase"];
 
-	installPhase = ''
-		mkdir -p $out
-		cp -R conf docs lib ${name}.jar $out
-		mkdir -p $out/bin
-		cp -R bin/{zkCli,zkCleanup,zkEnv}.sh $out/bin
-		for i in $out/bin/{zkCli,zkCleanup}.sh; do
-			wrapProgram $i \
-				--set JAVA_HOME "${jre}" \
-				--prefix PATH : "${bash}/bin"
-		done
-                chmod -x $out/bin/zkEnv.sh
-	'';
+  installPhase = ''
+    mkdir -p $out
+    cp -R conf docs lib ${name}.jar $out
+    mkdir -p $out/bin
+    cp -R bin/{zkCli,zkCleanup,zkEnv}.sh $out/bin
+    for i in $out/bin/{zkCli,zkCleanup}.sh; do
+      wrapProgram $i \
+        --set JAVA_HOME "${jre}" \
+        --prefix PATH : "${bash}/bin"
+    done
+    chmod -x $out/bin/zkEnv.sh
+  '';
 
-	meta = with stdenv.lib; {
-		homepage = "http://zookeeper.apache.org";
-		description = "Apache Zookeeper";
-		license = licenses.asl20;
-		maintainers = [ maintainers.nathan-gs ];	
-		platforms = platforms.unix;	
-	};		
+  meta = with stdenv.lib; {
+    homepage = "http://zookeeper.apache.org";
+    description = "Apache Zookeeper";
+    license = licenses.asl20;
+    maintainers = [ maintainers.nathan-gs ];
+    platforms = platforms.unix;
+  };
 
 }
