747daca4079f2c4150b978ad2972fbec0fa6ab0a
Thomas Tuegel <ttuegel@gmail.com>
2016-04-28 12:59:24 -0500

    kde5.plasma-desktop: propagate all buildInputs

--- pkgs/desktops/kde-5/plasma-5.6/plasma-desktop/default.nix
+++ pkgs/desktops/kde-5/plasma-5.6/plasma-desktop/default.nix
@@ -17,48 +17,14 @@ plasmaPackage rec {
     kdoctools
     makeQtWrapper
   ];
-  buildInputs = [
-    attica
-    boost
-    fontconfig
-    kcmutils
-    kdbusaddons
-    kded
-    kitemmodels
-    knewstuff
-    knotifications
-    knotifyconfig
-    kwallet
-    libcanberra_kde
-    libXcursor
-    libpulseaudio
-    libXft
-    libxkbfile
-    phonon
-    qtsvg
-    xf86inputevdev
-    xf86inputsynaptics
-    xkeyboard_config
-    xinput
-  ];
   propagatedBuildInputs = [
-    baloo
-    kactivities
-    kauth
-    kdeclarative
-    kdelibs4support
-    kemoticons
-    kglobalaccel
-    ki18n
-    kpeople
-    krunner
-    kwin
-    plasma-framework
-    plasma-workspace
-    qtdeclarative
-    qtquick1
-    qtquickcontrols
-    qtx11extras
+    attica boost fontconfig kcmutils kdbusaddons kded kitemmodels knewstuff
+    knotifications knotifyconfig kwallet libcanberra_kde libXcursor
+    libpulseaudio libXft libxkbfile phonon qtsvg xf86inputevdev
+    xf86inputsynaptics xkeyboard_config xinput baloo kactivities kauth
+    kdeclarative kdelibs4support kemoticons kglobalaccel ki18n kpeople krunner
+    kwin plasma-framework plasma-workspace qtdeclarative qtquick1
+    qtquickcontrols qtx11extras
   ];
   patches = [
     ./0001-qt-5.5-QML-import-paths.patch
