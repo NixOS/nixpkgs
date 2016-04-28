e4d3699976d58f454c488e91e2d225a03bdf510d
Thomas Tuegel <ttuegel@gmail.com>
2016-04-28 12:56:40 -0500

    kde5.kwin: propagate all buildInputs

--- pkgs/desktops/kde-5/plasma-5.6/kwin/default.nix
+++ pkgs/desktops/kde-5/plasma-5.6/kwin/default.nix
@@ -15,15 +15,13 @@ plasmaPackage {
     kdoctools
     makeQtWrapper
   ];
-  buildInputs = [
-    epoxy kcompletion kcmutils kconfig kconfigwidgets kcoreaddons
-    kcrash kdecoration kiconthemes kidletime kinit knewstuff knotifications
-    kpackage kservice kwayland kwidgetsaddons kxmlgui libinput libICE
-    libSM qtscript udev wayland xcb-util-cursor
-  ];
   propagatedBuildInputs = [
-    kactivities kdeclarative kglobalaccel ki18n kio kscreenlocker
-    kwindowsystem plasma-framework qtdeclarative qtmultimedia qtx11extras
+    kactivities kdeclarative kglobalaccel ki18n kio kscreenlocker kwindowsystem
+    plasma-framework qtdeclarative qtmultimedia qtx11extras epoxy kcompletion
+    kcmutils kconfig kconfigwidgets kcoreaddons kcrash kdecoration kiconthemes
+    kidletime kinit knewstuff knotifications kpackage kservice kwayland
+    kwidgetsaddons kxmlgui libinput libICE libSM qtscript udev wayland
+    xcb-util-cursor
   ];
   patches = [ ./0001-qdiriterator-follow-symlinks.patch ];
   cmakeFlags = [ "-DCMAKE_SKIP_BUILD_RPATH=OFF" ];
