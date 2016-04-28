b0cfdf77ef2907e2cb72693e702259f96e4ffae9
Thomas Tuegel <ttuegel@gmail.com>
2016-04-28 11:03:19 -0500

    kde5.kwallet: propagate all buildInputs

--- pkgs/desktops/kde-5/frameworks-5.21/kwallet.nix
+++ pkgs/desktops/kde-5/frameworks-5.21/kwallet.nix
@@ -5,17 +5,14 @@
 
 kdeFramework {
   name = "kwallet";
+  meta = { maintainers = [ lib.maintainers.ttuegel ]; };
   nativeBuildInputs = [ extra-cmake-modules kdoctools makeQtWrapper ];
-  buildInputs = [
-    kconfig kconfigwidgets kcoreaddons kdbusaddons kiconthemes
-    knotifications kservice kwidgetsaddons libgcrypt
+  propagatedBuildInputs = [
+    kconfig kconfigwidgets kcoreaddons kdbusaddons ki18n kiconthemes
+    knotifications kservice kwidgetsaddons kwindowsystem libgcrypt
   ];
-  propagatedBuildInputs = [ ki18n kwindowsystem ];
   postInstall = ''
     wrapQtProgram "$out/bin/kwalletd5"
     wrapQtProgram "$out/bin/kwallet-query"
   '';
-  meta = {
-    maintainers = [ lib.maintainers.ttuegel ];
-  };
 }
