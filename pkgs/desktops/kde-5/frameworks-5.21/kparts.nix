6a235b3e7fa4601a862a7ff828e06e85e4335ebb
Thomas Tuegel <ttuegel@gmail.com>
2016-04-28 10:59:22 -0500

    kde5.kparts: propagate all buildInputs

--- pkgs/desktops/kde-5/frameworks-5.21/kparts.nix
+++ pkgs/desktops/kde-5/frameworks-5.21/kparts.nix
@@ -5,13 +5,10 @@
 
 kdeFramework {
   name = "kparts";
+  meta = { maintainers = [ lib.maintainers.ttuegel ]; };
   nativeBuildInputs = [ extra-cmake-modules ];
-  buildInputs = [
-    kconfig kcoreaddons kiconthemes kjobwidgets knotifications
-    kservice kwidgetsaddons
+  propagatedBuildInputs = [
+    kconfig kcoreaddons ki18n kiconthemes kio kjobwidgets knotifications
+    kservice ktextwidgets kwidgetsaddons kxmlgui
   ];
-  propagatedBuildInputs = [ ki18n kio ktextwidgets kxmlgui ];
-  meta = {
-    maintainers = [ lib.maintainers.ttuegel ];
-  };
 }
