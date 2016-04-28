e28e3d576362335b5ac7822478b2d4dc6efc4253
Charles Strahan <charles@cstrahan.com>
2016-04-27 20:19:12 -0400

    zookeeper: include zooInspector.sh
    ZooInspector is a Swing based GUI for interacting with Zookeeper.

--- pkgs/servers/zookeeper/default.nix
+++ pkgs/servers/zookeeper/default.nix
@@ -23,14 +23,28 @@ stdenv.mkDerivation rec {
         --prefix PATH : "${bash}/bin"
     done
     chmod -x $out/bin/zkEnv.sh
+
+    mkdir -p $out/share/zooinspector
+    cp -r contrib/ZooInspector/{${name}-ZooInspector.jar,icons,lib,config} $out/share/zooinspector
+
+    classpath="$out/${name}.jar:$out/share/zooinspector/${name}-ZooInspector.jar"
+    for jar in $out/lib/*.jar $out/share/zooinspector/lib/*.jar; do
+      classpath="$classpath:$jar"
+    done
+
+    cat << EOF > $out/bin/zooInspector.sh
+    #!${stdenv.shell}
+    cd $out/share/zooinspector
+    exec ${jre}/bin/java -cp $classpath org.apache.zookeeper.inspector.ZooInspector
+    EOF
+    chmod +x $out/bin/zooInspector.sh
   '';
 
   meta = with stdenv.lib; {
     homepage = "http://zookeeper.apache.org";
     description = "Apache Zookeeper";
     license = licenses.asl20;
-    maintainers = [ maintainers.nathan-gs ];
+    maintainers = with maintainers; [ nathan-gs cstrahan ];
     platforms = platforms.unix;
   };
-
 }
