3a52c5fa7d059ff75b3f56a817832e84f2c96e04
Nikolay Amiantov <ab@fmap.me>
2016-05-01 12:25:37 +0300

    firefox: fix library path
    Fixes #15126

--- pkgs/applications/networking/browsers/firefox/wrapper.nix
+++ pkgs/applications/networking/browsers/firefox/wrapper.nix
@@ -88,7 +88,7 @@ stdenv.mkDerivation {
     makeWrapper "${browser}/bin/${browserName}" \
         "$out/bin/${browserName}${nameSuffix}" \
         --suffix-each MOZ_PLUGIN_PATH ':' "$plugins" \
-        --suffix-each LD_LIBRARY_PATH ':' "$libs" \
+        --suffix LD_LIBRARY_PATH ':' "$libs" \
         --suffix-each GTK_PATH ':' "$gtk_modules" \
         --suffix-each LD_PRELOAD ':' "$(cat $(filterExisting $(addSuffix /extra-ld-preload $plugins)))" \
         --prefix GST_PLUGIN_SYSTEM_PATH : "$GST_PLUGIN_SYSTEM_PATH" \
@@ -129,7 +129,7 @@ stdenv.mkDerivation {
   # Let each plugin tell us (through its `mozillaPlugin') attribute
   # where to find the plugin in its tree.
   plugins = map (x: x + x.mozillaPlugin) plugins;
-  libs = map (x: x + "/lib") libs ++ map (x: x + "/lib64") libs;
+  libs = lib.makeLibraryPath libs + ":" + lib.makeSearchPathOutputs "lib64" ["lib"] libs;
   gtk_modules = map (x: x + x.gtkModule) gtk_modules;
 
   passthru = { unwrapped = browser; };
