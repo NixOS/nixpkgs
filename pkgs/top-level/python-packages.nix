bc4465e23f1c63e8b1165fb1c3c59699bc62bc34
Tobias Geerinckx-Rice <me@tobias.gr>
2016-05-02 14:06:08 +0200

    pythonPackages.sqlparse: 0.1.16 -> 0.1.18

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -21100,14 +21100,14 @@ in modules // {
 
   sqlparse = buildPythonPackage rec {
     name = "sqlparse-${version}";
-    version = "0.1.16";
+    version = "0.1.18";
 
     # the source wasn't transformed with 2to3 yet
     doCheck = !isPy3k;
 
     src = pkgs.fetchurl {
       url = "mirror://pypi/s/sqlparse/${name}.tar.gz";
-      sha256 = "108gy82x7davjrn3jqn7yv4r5v4jrzp892ysfx8l00abr8v6r337";
+      sha256 = "1kypl9l2nkzy3pmr89mvpfl65xk1m5y4aaghhandcxkgl329dc9r";
     };
 
     meta = {
