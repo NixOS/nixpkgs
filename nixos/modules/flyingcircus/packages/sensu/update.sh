48bfa3838547255406aae57056f8d50d5cd67f58
Christian Zagrodnick <cz@flyingcircus.io>
2016-05-02 19:02:19 +0200

    install nix-prefetch-scripts in update to have nix-prefetch-git available.

--- nixos/modules/flyingcircus/packages/sensu/update.sh
+++ nixos/modules/flyingcircus/packages/sensu/update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -f Gemfile.lock
 rm -rf /tmp/bundix
-nix-shell -p bundler -p openssl --command "bundler package --no-install --path /tmp/bundix/bundle"
-nix-shell -p bundix --command bundix
+nix-shell -p bundler -p openssl -p nix-prefetch-scripts --command "bundler package --no-install --path /tmp/bundix/bundle"
+nix-shell -p bundix  -p nix-prefetch-scripts --command bundix
