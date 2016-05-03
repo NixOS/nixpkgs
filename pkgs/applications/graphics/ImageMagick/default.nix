1b48d611f04299229a82ba4358df38ac5d3a5338
Franz Pletz <fpletz@fnordicwalking.de>
2016-05-04 01:19:27 +0200

    imagemagick: 6.9.3-8 -> 6.9.3-9
    (cherry picked from commit 05eae0242d7483ffe29c006ce6b3b8b238bce284)

--- pkgs/applications/graphics/ImageMagick/default.nix
+++ pkgs/applications/graphics/ImageMagick/default.nix
@@ -13,7 +13,7 @@ in
 
 stdenv.mkDerivation rec {
   name = "imagemagick-${version}";
-  version = "6.9.2-0";
+  version = "6.9.3-9";
 
   src = fetchurl {
     urls = [
@@ -21,7 +21,7 @@ stdenv.mkDerivation rec {
       # the original source above removes tarballs quickly
       "http://distfiles.macports.org/ImageMagick/ImageMagick-${version}.tar.xz"
     ];
-    sha256 = "17ir8bw1j7g7srqmsz3rx780sgnc21zfn0kwyj78iazrywldx8h7";
+    sha256 = "0q19jgn1iv7zqrw8ibxp4z57iihrc9kyb09k2wnspcacs6vrvinf";
   };
 
   outputs = [ "out" "doc" ];
