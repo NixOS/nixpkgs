41b5de4773e0c5e76acd663aa347288e84b52dc0
José Romildo Malaquias <malaquias@gmail.com>
2016-05-03 19:13:09 -0300

    xfce4-whiskermenu-plugin: 1.5.2 -> 1.5.3

--- pkgs/desktops/xfce/panel-plugins/xfce4-whiskermenu-plugin.nix
+++ pkgs/desktops/xfce/panel-plugins/xfce4-whiskermenu-plugin.nix
@@ -4,21 +4,22 @@
 with stdenv.lib;
 stdenv.mkDerivation rec {
   p_name  = "xfce4-whiskermenu-plugin";
-  ver_maj = "1.5";
-  ver_min = "2";
-  rev = "d08418c8d55edfacef190ec14e03e1e9a6988101";
+  version = "1.5.3";
 
-  name = "${p_name}-${ver_maj}.${ver_min}";
+  name = "${p_name}-${version}";
 
   src = fetchFromGitHub {
     owner = "gottcode";
     repo = "xfce4-whiskermenu-plugin";
-    inherit rev;
-    sha256 = "0icphm6bm5p3csh9kwyyvkj2y87shrs12clfifbhv35dm0skb2dx";
+    rev = "v${version}";
+    sha256 = "07gmf9x3pw6xajklj0idahbnv0psnkhiqhb88bmkp344jirsx6ba";
   };
 
-  buildInputs = [ cmake pkgconfig intltool libxfce4util libxfcegui4 xfce4panel
-                  gtk exo garcon ];
+  nativeBuildInputs = [ cmake pkgconfig intltool ];
+
+  buildInputs = [ libxfce4util libxfcegui4 xfce4panel gtk exo garcon ];
+
+  enableParallelBuilding = true;
 
   preFixup = ''
     substituteInPlace $out/bin/xfce4-popup-whiskermenu \
@@ -27,7 +28,8 @@ stdenv.mkDerivation rec {
 
   meta = {
     homepage = "http://goodies.xfce.org/projects/panel-plugins/${p_name}";
-    description = "Whisker Menu is an alternate application launcher for Xfce";
+    description = "Alternate application launcher for Xfce";
+    license = licenses.gpl2Plus;
     platforms = platforms.linux;
     maintainers = [ maintainers.pjbarnoy ];
   };
