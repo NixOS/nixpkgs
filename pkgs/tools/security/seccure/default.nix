ff6a98612cc3f1fe33b01be42efa3d5f4515b149
Tobias Geerinckx-Rice <me@tobias.gr>
2016-05-03 19:13:42 +0200

    seccure: 0.4 -> 0.5

--- pkgs/tools/security/seccure/default.nix
+++ pkgs/tools/security/seccure/default.nix
@@ -1,14 +1,15 @@
-{stdenv, fetchurl, libgcrypt}:
+{ stdenv, fetchurl, libgcrypt }:
 
 stdenv.mkDerivation rec {
-  name = "seccure-0.4";
+  name = "seccure-${version}";
+  version = "0.5";
   
   src = fetchurl {
     url = "http://point-at-infinity.org/seccure/${name}.tar.gz";
-    sha256 = "33d690a7034ee349bce4911a8b7c73e6e3cd13a140f429e9e628d5cd5a3bb955";
+    sha256 = "0nwnk3hfhgvf5xr0xipbh6smfnya22wphc5rj0vgi5d0zr5cwrk5";
   };
 
-  buildInputs = [libgcrypt];
+  buildInputs = [ libgcrypt ];
   
   preConfigure = ''
     sed -e s@/usr/@$out/@g -i Makefile
