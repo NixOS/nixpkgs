0ba62f7150ed1b2756adb52b1bac6d3d7d93831f
rardiol <ricardo.ardissone@gmail.com>
2016-05-03 06:19:19 -0300

    spring, springlobby update (#15182)
    * alure: init at 1.2
    
    * spring: 96.0 -> 101.0
    
    * springlobby: 0.195 -> 0.247

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6416,6 +6416,8 @@ in
 
   afflib = callPackage ../development/libraries/afflib { };
 
+  alure = callPackage ../development/libraries/alure { };
+
   agg = callPackage ../development/libraries/agg { };
 
   allegro = callPackage ../development/libraries/allegro {};
