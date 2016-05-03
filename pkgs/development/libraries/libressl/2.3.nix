853fe4ceeb440d4ec87a51f3b3fc1b8232aa310f
Franz Pletz <fpletz@fnordicwalking.de>
2016-05-03 17:18:04 +0200

    libressl: 2.2.6 -> 2.2.7, 2.3.3 -> 2.3.4
    Fix multiple vulnerabilities in libcrypto relating to ASN.1 and encoding.
    
    http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.2.7-relnotes.txt
    http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.3.4-relnotes.txt
    (cherry picked from commit 6d55b2e9c0a8bd72cb4375c55a5789682cb96b1c)

--- pkgs/development/libraries/libressl/2.3.nix
+++ pkgs/development/libraries/libressl/2.3.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "libressl-${version}";
-  version = "2.3.2";
+  version = "2.3.4";
 
   src = fetchurl {
     url    = "mirror://openbsd/LibreSSL/${name}.tar.gz";
-    sha256 = "0sm9cjjqvj581sfd4sh0i467sh8p89nq9b8ck2qn3war92p5zx40";
+    sha256 = "1ag65pbvdikqj5y1w780jicl3ngi9ld2332ki6794y0gcar3a4bs";
   };
 
   enableParallelBuilding = true;
