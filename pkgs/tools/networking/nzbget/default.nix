36f8b3cad13f7bf61c55a3a09b215e47711d2bd9
Tristan Helmich <tristan.helmich@gmail.com>
2016-05-04 10:58:01 +0200

    nzbget: 16.4 -> 17.0-r1686 and nzbget service

--- pkgs/tools/networking/nzbget/default.nix
+++ pkgs/tools/networking/nzbget/default.nix
@@ -1,17 +1,18 @@
 { stdenv, fetchurl, pkgconfig, libxml2, ncurses, libsigcxx, libpar2
-, gnutls, libgcrypt, zlib }:
+, gnutls, libgcrypt, zlib, openssl }:
 
 stdenv.mkDerivation rec {
   name = "nzbget-${version}";
-  version = "16.4";
+  version = "17.0-r1686";
+  filename = "nzbget-17.0-testing-r1686";
 
   src = fetchurl {
-    url = "http://github.com/nzbget/nzbget/releases/download/v${version}/${name}-src.tar.gz";
-    sha256 = "03sdzxxsjpxp82jpk593xls96yk29989z05j73jah21dbpkkx7lf";
+    url = "http://github.com/nzbget/nzbget/releases/download/v${version}/${filename}-src.tar.gz";
+    sha256 = "0hk0hiccdk3bivdnc2635kqqdwgwf73wvis1wl9k0snds25dwfiw";
   };
 
   buildInputs = [ pkgconfig libxml2 ncurses libsigcxx libpar2 gnutls
-                  libgcrypt zlib ];
+                  libgcrypt zlib openssl ];
 
   enableParallelBuilding = true;
 
