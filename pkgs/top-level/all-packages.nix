4700db89b1672c11c6d4bb0485c118864ac1ec1b
Rahul Gopinath <rahul@gopinath.org>
2016-05-04 23:38:50 -0700

    milu: init at 2016-05-09
    Milu is a mutation testing tool for C and C++ programs
    
    Closes #15341

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2571,6 +2571,8 @@ in
 
   noip = callPackage ../tools/networking/noip { };
 
+  milu = callPackage ../applications/misc/milu { };
+
   mpack = callPackage ../tools/networking/mpack { };
 
   pa_applet = callPackage ../tools/audio/pa-applet { };
