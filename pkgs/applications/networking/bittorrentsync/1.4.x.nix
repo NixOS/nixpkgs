f10759fb194ba4a3d2a5dd421b084c139575379d
Joachim Fasting <joachifm@fastmail.fm>
2016-05-05 18:35:20 +0200

    bittorrentSync14: 1.4.110 -> 1.4.111
    (cherry picked from commit 6d2625a940eeff9a14e8c39d3d799bf69f78b97c)

--- pkgs/applications/networking/bittorrentsync/1.4.x.nix
+++ pkgs/applications/networking/bittorrentsync/1.4.x.nix
@@ -5,15 +5,15 @@ let
     else if stdenv.system == "i686-linux" then "i386"
     else throw "Bittorrent Sync for: ${stdenv.system} not supported!";
 
-  sha256 = if stdenv.system == "x86_64-linux" then "1b9f6qxpvyrzf23l71hw42qyg4i27by3hs91sm34drm24z7m7fpd"
-    else if stdenv.system == "i686-linux" then "0caqwaxd6i8cap35kpzkwy5dknk7iaxf5fbfjy46cbwylgcpsc2x"
+  sha256 = if stdenv.system == "x86_64-linux" then "0bw3ds3ndcnkry5mpv645z2bfi5z387bh0f7b35blxq1yv93r83f"
+    else if stdenv.system == "i686-linux"     then "1qwaj7l7nsd4afx7ksb4b1c22mki9qa40803v9x1a8bhbdfhkczk"
     else throw "Bittorrent Sync for: ${stdenv.system} not supported!";
 
   libPath = stdenv.lib.makeLibraryPath [ stdenv.cc.libc ];
 in
 stdenv.mkDerivation rec {
   name = "btsync-${version}";
-  version = "1.4.110";
+  version = "1.4.111";
 
   src  = fetchurl {
     url  = "http://syncapp.bittorrent.com/${version}/btsync_${arch}-${version}.tar.gz";
