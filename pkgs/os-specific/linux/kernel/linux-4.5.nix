b3b73d8d1f78918b7bc9271f7f2979f1c8e2ad8f
Tim Steinbach <tim.steinbach@exinda.com>
2016-05-06 11:12:04 -0400

    kernel: 4.5.2 -> 4.5.3
    (cherry picked from commit 02d94d335a88bab3e471e64871c2209ffd2db818)

--- pkgs/os-specific/linux/kernel/linux-4.5.nix
+++ pkgs/os-specific/linux/kernel/linux-4.5.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.5.2";
+  version = "4.5.3";
   extraMeta.branch = "4.5";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "17r063zx880ka3ayv9cf1yjfilvxlifhja1rhw5z3w35hgdkj8z3";
+    sha256 = "1z0265cxv63br147vridmcqbz3cx3q3finy40hwljwv1r2lggid4";
   };
 
   kernelPatches = args.kernelPatches;
