a5814c3dc7ef19bf2daa2dab62022bc07db9712b
Tim Steinbach <NeQuissimus@users.noreply.github.com>
2016-05-06 14:25:29 -0400

    kernel: 4.4.8 -> 4.4.9 (#15276)

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.4.8";
+  version = "4.4.9";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "02gf7dkdibhxv8n4qfnyqh645d102g9z7pdid8pcq4jhd99sg9yj";
+    sha256 = "1fgkcl1dgljb4s8ciwc4gpz5g92cf6x31rj7016bd6r0hmvcl1bn";
   };
 
   kernelPatches = args.kernelPatches;
