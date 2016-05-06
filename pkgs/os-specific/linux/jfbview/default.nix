e7cfdd6c142406ec0aebc381d6602e4f9d4c30d2
Tobias Geerinckx-Rice <me@tobias.gr>
2016-05-06 11:56:56 +0200

    jfbview: 0.5.1 -> 0.5.2

--- pkgs/os-specific/linux/jfbview/default.nix
+++ pkgs/os-specific/linux/jfbview/default.nix
@@ -1,22 +1,31 @@
-{ stdenv, fetchFromGitHub, freetype, imlib2, jbig2dec, libjpeg, libX11
-, mujs, mupdf, ncurses, openjpeg, openssl }:
+{ stdenv, fetchFromGitHub
+, freetype, harfbuzz, imlib2, jbig2dec, libjpeg, libX11, mujs, mupdf
+, ncurses, openjpeg, openssl }:
 
 let
   binaries = [ "jfbpdf" "jfbview" "jpdfcat" "jpdfgrep" ];
 in
 stdenv.mkDerivation rec {
   name = "jfbview-${version}";
-  version = "0.5.1";
+  version = "0.5.2";
 
   src = fetchFromGitHub {
-    sha256 = "113bkf49q04k9rjps5l28ychmzsfjajp9cjhr433s9ld0972z01m";
+    sha256 = "1vd2ndl4ar2bzqf0k11qid6gvma59qg62imsa81mgczsqw7kvbx6";
     rev = version;
     repo = "JFBView";
     owner = "jichu4n";
   };
 
-  buildInputs = [ freetype imlib2 jbig2dec libjpeg libX11 mujs mupdf
+  buildInputs = [ freetype harfbuzz imlib2 jbig2dec libjpeg libX11 mujs mupdf
     ncurses openjpeg openssl ];
+
+  configurePhase = ''
+    # Hack. Probing (with `ldconfig -p`) fails with ‘cannot execute binary file’.
+    # Overriding `OPENJP2 = ...` later works, but makes build output misleading:
+    substituteInPlace Makefile --replace "ldconfig -p" "echo libopenjp2"
+
+    make config.mk
+  '';
 
   buildFlags = binaries;
   enableParallelBuilding = true;
