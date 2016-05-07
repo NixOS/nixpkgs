3785515c4b73bb8a3e7607be85f8468a13c509b4
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2016-05-07 23:58:36 +0200

    sane-backends-git -> 2016-05-07
    Color scanning for Samsung models which support JPEG Lossy compression.

--- pkgs/applications/graphics/sane/backends/git.nix
+++ pkgs/applications/graphics/sane/backends/git.nix
@@ -1,10 +1,10 @@
 { callPackage, fetchgit, ... } @ args:
 
 callPackage ./generic.nix (args // {
-  version = "2016-04-23";
+  version = "2016-05-07";
   src = fetchgit {
-    sha256 = "11bf60cd5a6b314e855a69a6f57a5ca0db3254527def55662bce25810a2314df";
-    rev = "c8169b1e656f7f95c67946298da5a0e1c143f8e8";
+    sha256 = "5f8974bc56d5eb1d33fbe53b30e80395b690151a1ea418d8aa44c7e092656896";
+    rev = "926bfade544de4a4fd5f1a8082b85a97e2443770";
     url = "git://alioth.debian.org/git/sane/sane-backends.git";
   };
 })
