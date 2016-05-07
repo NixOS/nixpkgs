440ea1b893e774b04d7012d6d551b3b8e81517d2
Michiel Leenaars <ml.software@leenaa.rs>
2016-05-07 23:06:56 +0200

    p11-kit: 0.23.1 -> 0.23.2

--- pkgs/development/libraries/p11-kit/default.nix
+++ pkgs/development/libraries/p11-kit/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, libiconv, pkgconfig, libffi, libtasn1 }:
 
 stdenv.mkDerivation rec {
-  name = "p11-kit-0.23.1";
+  name = "p11-kit-0.23.2";
 
   src = fetchurl {
     url = "${meta.homepage}releases/${name}.tar.gz";
-    sha256 = "1i3a1wdpagm0p3y1bwaz5x5rjhcpqbcrnhkcp10p259vkxk72wz5";
+    sha256 = "1w7szm190phlkg7qx05ychlj2dbvkgkhx9gw6dx4d5rw62l6wwms";
   };
 
   outputs = [ "dev" "out" "docdev" ];
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
   installFlags = [ "exampledir=\${out}/etc/pkcs11" ];
 
   meta = with stdenv.lib; {
-    homepage = http://p11-glue.freedesktop.org/;
+    homepage = https://p11-glue.freedesktop.org/;
     platforms = platforms.all;
     maintainers = with maintainers; [ urkud wkennington ];
     license = licenses.mit;
