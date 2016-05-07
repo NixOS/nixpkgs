f7c02f867005c4731af51b94ab2b1a64a040e8cf
Nikolay Amiantov <ab@fmap.me>
2016-05-07 14:31:16 +0300

    ejabberd service: add image thumbnailing support

--- nixos/modules/services/networking/ejabberd.nix
+++ nixos/modules/services/networking/ejabberd.nix
@@ -78,6 +78,12 @@ in {
         description = "Configuration dumps that should be loaded on the first startup";
         example = literalExample "[ ./myejabberd.dump ]";
       };
+
+      imagemagick = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Add ImageMagick to server's path; allows for image thumbnailing";
+      };
     };
 
   };
@@ -105,7 +111,7 @@ in {
       description = "ejabberd server";
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
-      path = [ pkgs.findutils pkgs.coreutils ];
+      path = [ pkgs.findutils pkgs.coreutils ] ++ lib.optional cfg.imagemagick pkgs.imagemagick;
 
       serviceConfig = {
         Type = "forking";
