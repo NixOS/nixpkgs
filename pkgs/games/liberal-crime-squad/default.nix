bf3617546d31ae2f11553e31243dc49fc4bfcf87
Ricardo Ardissone <ricardo.ardissone@gmail.com>
2016-05-08 19:26:08 -0300

    liberal-crime-squad: 2016-03-03 -> 2016-05-08

--- pkgs/games/liberal-crime-squad/default.nix
+++ pkgs/games/liberal-crime-squad/default.nix
@@ -1,20 +1,17 @@
-{ fetchFromGitHub, stdenv, ncurses, autoconf, automake , SDL2, SDL2_mixer }:
+{ fetchFromGitHub, stdenv, ncurses, autoreconfHook, SDL2, SDL2_mixer }:
 
 stdenv.mkDerivation rec {
-  version = "2016-03-03";
+  version = "2016-05-08";
   name = "liberal-crime-squad-${version}";
+
   src = fetchFromGitHub {
     owner = "Kamal-Sadek";
     repo = "Liberal-Crime-Squad";
-    rev = "39c5f167e902e33cb37b152215d67f71b9ae8269";
-    sha256 = "18s2w2570fd79y9rb7ikxls1qw29l1lbwk9n2dxxqjslbzrbvv31";
+    rev = "127d712";
+    sha256 = "1n16rmi2gm2mcnpp0ms1whj0nzcbfw52dnd90l52w4d1g4kqf1ck";
   };
 
-  preConfigure = ''
-    ./bootstrap
-  '';
-
-  buildInputs = [ ncurses autoconf automake SDL2 SDL2_mixer ];
+  buildInputs = [ ncurses autoreconfHook SDL2 SDL2_mixer ];
 
   meta = with stdenv.lib; {
     description = "A humorous politically themed ncurses game";
@@ -22,7 +19,8 @@ stdenv.mkDerivation rec {
       Welcome to Liberal Crime Squad! The Conservatives have taken the Executive, Legislative, and Judicial branches of government. Over time, the Liberal laws of this nation will erode and turn the country into a BACKWOODS YET CORPORATE NIGHTMARE. To prevent this from happening, the Liberal Crime Squad was established. The mood of the country is shifting, and we need to turn things around. Go out on the streets and indoctrinate Conservative automatons. That is, let them see their True Liberal Nature. Then arm them and send them forth to Stop Evil.
     '';
     homepage = https://github.com/Kamal-Sadek/Liberal-Crime-Squad;
+    maintainers = [ maintainers.rardiol ];
     license = licenses.gpl2;
-    platforms = platforms.linux;
+    platforms = platforms.all;
   };
 }
