520d3492e919d0495073e513d637edd7bd8412a8
Ricardo Ardissone <ricardo.ardissone@gmail.com>
2016-05-08 11:59:56 -0300

    libvdpau-va-gl: fix build missing libva-x11

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8096,7 +8096,9 @@ in
 
   libvdpau = callPackage ../development/libraries/libvdpau { };
 
-  libvdpau-va-gl = callPackage ../development/libraries/libvdpau-va-gl { };
+  libvdpau-va-gl = callPackage ../development/libraries/libvdpau-va-gl {
+    libva = libva-full; # also wants libva-{x11}
+  };
 
   libvirt = callPackage ../development/libraries/libvirt { };
 
