0adf2b25a12e9867f162fefe1056137d3cfb7859
Domen Kožar <domen@dev.si>
2016-05-08 18:29:40 +0100

    4store: 1.1.5 -> 1.1.6 (convert from builderDefsPackage)

--- pkgs/servers/http/4store/default.nix
+++ pkgs/servers/http/4store/default.nix
@@ -1,48 +1,44 @@
-x@{builderDefsPackage
-  , librdf_raptor, librdf_rasqal,
-  glib, libxml2, pcre, avahi,
-  readline, ncurses, expat,
-  zlib, pkgconfig, which,
-  perl, libuuid, gmp, mpfr
-  , db_dir ? "/var/lib/4store"
-  , ...}:
-builderDefsPackage
-(a :  
-let 
-  s = import ./src-for-default.nix;
-  helperArgNames = ["stdenv" "fetchurl" "builderDefsPackage"] ++ 
-    ["db_dir"];
-  buildInputs = map (n: builtins.getAttr n x)
-    (builtins.attrNames (builtins.removeAttrs x helperArgNames));
-in
-rec {
-  src = a.fetchUrlFromSrcInfo s;
+{ stdenv, fetchFromGitHub, librdf_raptor
+, librdf_rasqal, glib, libxml2, pcre
+, avahi, readline, ncurses, expat, autoreconfHook
+, zlib, pkgconfig, which, perl, libuuid
+, gmp, mpfr
+, db_dir ? "/var/lib/4store" }:
 
-  inherit (s) name;
-  inherit buildInputs;
 
-  /* doConfigure should be removed if not needed */
-  phaseNames = ["doFixConfigure" "doConfigure" "doMakeInstall"
-    "fixInterpreter"];
+stdenv.mkDerivation rec {
+  name = "4store-${version}";
+  version = "1.1.6";
 
-  doFixConfigure = a.fullDepEntry ''
-    sed -e 's@#! */bin/bash@#! ${a.stdenv.shell}@' -i configure
+  src = fetchFromGitHub {
+    owner = "garlik";
+    repo = "4store";
+    rev = "v${version}";
+    sha256 = "1kzdfmwpzy64cgqlkcz5v4klwx99w0jk7afckyf7yqbqb4rydmpk";
+  };
+
+  buildInputs = [ librdf_raptor librdf_rasqal glib libxml2 pcre
+    avahi readline ncurses expat zlib pkgconfig which perl libuuid
+    gmp mpfr autoreconfHook ];
+
+  # needed for ./autogen.sh
+  prePatch = ''
+    echo "${version}" > .version
+  '';
+
+  preConfigure =  ''
+    sed -e 's@#! */bin/bash@#! ${stdenv.shell}@' -i configure
     find . -name Makefile -exec sed -e "s@/usr/local@$out@g" -i '{}' ';'
     
-    sed -e 's@/var/lib/4store@${db_dir}@g' -i src/common/params.h src/utilities/*
+    rm src/utilities/4s-backend 
+    sed -e 's@/var/lib/4store@${db_dir}@g' -i configure.ac src/utilities/*
     sed -e '/FS_STORE_ROOT/d' -i src/utilities/Makefile*
-  '' ["minInit" "doUnpack"];
+  '';
 
-  fixInterpreter = (a.doPatchShebangs "$out/bin");
-      
-  meta = {
+  meta = with stdenv.lib; {
     description = "SparQL query server (RDF storage)";
     homepage = http://4store.org/;
-    maintainers = with a.lib.maintainers;
-    [
-      raskin
-    ];
-    platforms = with a.lib.platforms;
-      linux;
+    maintainers = with maintainers; [ raskin ];
+    platforms = platforms.linux;
   };
-}) x
+}
