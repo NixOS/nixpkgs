f7fcb67109dc57928100502aee9a22f939f0e6ce
Alexander Ried <ried@mytum.de>
2016-05-09 17:28:52 +0200

    letsencrypt: 0.4.0 -> 0.5.0 seems like the dependency on gnureadline is more or less optional for letsencrypt itself but the tests will not pass without it

--- pkgs/tools/admin/letsencrypt/default.nix
+++ pkgs/tools/admin/letsencrypt/default.nix
@@ -2,13 +2,13 @@
 
 pythonPackages.buildPythonApplication rec {
   name = "letsencrypt-${version}";
-  version = "0.4.0";
+  version = "0.5.0";
 
   src = fetchFromGitHub {
-    owner = "letsencrypt";
-    repo = "letsencrypt";
+    owner = "certbot";
+    repo = "certbot";
     rev = "v${version}";
-    sha256 = "0r2wis48w5nailzp2d5brkh2f40al6sbz816xx0akh3ll0rl1hbv";
+    sha256 = "0x098cdyfgqvh7x5d3sz56qjpjyg5b4fl82086sm43d8mbz0h5rm";
   };
 
   propagatedBuildInputs = with pythonPackages; [
@@ -26,7 +26,7 @@ pythonPackages.buildPythonApplication rec {
     zope_component
     zope_interface
   ];
-  buildInputs = with pythonPackages; [ nose dialog ];
+  buildInputs = [ dialog ] ++ (with pythonPackages; [ nose mock gnureadline ]);
 
   patchPhase = ''
     substituteInPlace letsencrypt/notify.py --replace "/usr/sbin/sendmail" "/var/setuid-wrappers/sendmail"
@@ -41,7 +41,7 @@ pythonPackages.buildPythonApplication rec {
   '';
 
   meta = with stdenv.lib; {
-    homepage = https://github.com/letsencrypt/letsencrypt;
+    homepage = src.meta.homepage;
     description = "ACME client that can obtain certs and extensibly update server configurations";
     platforms = platforms.unix;
     maintainers = [ maintainers.iElectric ];
