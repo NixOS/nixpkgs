cb37ab146b6578b540cead3ef1ec282f4de975a7
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-05-09 15:49:44 +0200

    Add mirror://mozilla scheme

--- pkgs/applications/networking/mailreaders/thunderbird/default.nix
+++ pkgs/applications/networking/mailreaders/thunderbird/default.nix
@@ -20,9 +20,8 @@ stdenv.mkDerivation rec {
   name = "thunderbird-${verName}";
 
   src = fetchurl {
-    url = "http://archive.mozilla.org/pub/thunderbird/releases/"
-      + "${verName}/source/thunderbird-${verName}.source.tar.xz";
-    sha256 = "0rynfyxgpvfla17zniaq84slc02kg848qawkjmdbnv74y6bkhs8m";
+    url = "mirror://mozilla/thunderbird/releases/${verName}/source/thunderbird-${verName}.source.tar.xz";
+    sha256 = "1wbkj8a0p62mcbxlq8yyzrx51xi65qm8f2ccqiv5pb6qd51b5d0v";
   };
 
   buildInputs = # from firefox30Pkgs.xulrunner, without gstreamer and libvpx
