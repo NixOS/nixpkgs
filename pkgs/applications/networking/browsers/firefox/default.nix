091ca4e97c4a8435a03592bea3ced99bc7081c15
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-05-10 15:31:52 +0200

    firefox: Revert to SHA-256 hashes
    Using SHA-512 causes problems for users upgrading from NixOS 15.09.
    
    Fixes #15349.

--- pkgs/applications/networking/browsers/firefox/default.nix
+++ pkgs/applications/networking/browsers/firefox/default.nix
@@ -18,14 +18,14 @@ assert stdenv.cc ? libc && stdenv.cc.libc != null;
 
 let
 
-common = { pname, version, sha512 }: stdenv.mkDerivation rec {
+common = { pname, version, sha256 }: stdenv.mkDerivation rec {
   name = "${pname}-unwrapped-${version}";
 
   src = fetchurl {
     url =
       let ext = if lib.versionAtLeast version "41.0" then "xz" else "bz2";
       in "mirror://mozilla/firefox/releases/${version}/source/firefox-${version}.source.tar.${ext}";
-    inherit sha512;
+    inherit sha256;
   };
 
   buildInputs =
@@ -132,13 +132,13 @@ in {
   firefox-unwrapped = common {
     pname = "firefox";
     version = "46.0.1";
-    sha512 = "c58642774f93ceaef4f99bc3fe578db6e4f6de7f1d23080da97b61bc4fc6b516ce99fa04368893c0fa2cb9cd0b36e96955656daa97d0bd0d8f4da6a2d364cb98";
+    sha256 = "1ycb35x40ssmmvgppljdyvf3iax6sbzf2k90yckf9qxp64m2kf4q";
   };
 
   firefox-esr-unwrapped = common {
     pname = "firefox-esr";
     version = "45.1.1esr";
-    sha512 = "ee6bccdf01450c5b371e31c35f5bb084ad49f796fcc9cf3a346646972044ad85ce198cc34b697c32e2d3ad1e25955ef5b91b68790704ecaa4de9d3a412914fc7";
+    sha256 = "0qj5s006m68d1fq2k0759dj3a7m615dz3xqkl7jxzgff3jm3czm2";
   };
 
 }
