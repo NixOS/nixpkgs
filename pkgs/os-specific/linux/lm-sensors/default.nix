311b56b764facd49390e204de23d5bf9e45582f1
Tobias Geerinckx-Rice <me@tobias.gr>
2016-05-10 21:05:31 +0200

    lm_sensors: 3.3.5 -> 3.4.0

--- pkgs/os-specific/linux/lm-sensors/default.nix
+++ pkgs/os-specific/linux/lm-sensors/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, bison, flex, which, perl }:
 
-let version = "3.3.5"; in
+let version = "3.4.0"; in
 
 stdenv.mkDerivation rec {
   name = "lm-sensors-${version}";
   
   src = fetchurl {
     url = "http://dl.lm-sensors.org/lm-sensors/releases/lm_sensors-${version}.tar.bz2";
-    sha256 = "1ksgrynxgrq590nb2fwxrl1gwzisjkqlyg3ljfd1al0ibrk6mbjx";
+    sha256 = "07q6811l4pp0f7pxr8bk3s97ippb84mx5qdg7v92s9hs10b90mz0";
   };
 
   buildInputs = [ bison flex which perl ];
