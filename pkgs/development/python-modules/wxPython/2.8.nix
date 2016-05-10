bfc819a974de2750053f6a9e98aa94fd33597fc5
Frederik Rietdijk <fridh@fridh.nl>
2016-05-10 18:04:58 +0200

    pythonPackages.wxPython: use buildPythonPackage

--- pkgs/development/python-modules/wxPython/2.8.nix
+++ pkgs/development/python-modules/wxPython/2.8.nix
@@ -1,6 +1,36 @@
-{ callPackage, ... } @ args:
+{ lib, buildPythonPackage, fetchurl, pkgconfig, isPy3k, isPyPy, wxGTK, openglSupport ? true, pyopengl
+, libX11, ...
+}:
 
-callPackage ./generic.nix (args // rec {
+assert wxGTK.unicode;
+
+buildPythonPackage rec {
+  name = "wxPython-${version}";
   version = "2.8.12.1";
-  sha256 = "1l1w4i113csv3bd5r8ybyj0qpxdq83lj6jrc5p7cc10mkwyiagqz";
-})
+
+  disabled = isPy3k || isPyPy;
+  doCheck = false;
+
+  src = fetchurl {
+    url = "mirror://sourceforge/wxpython/wxPython-src-${version}.tar.bz2";
+    sha256 = "1l1w4i113csv3bd5r8ybyj0qpxdq83lj6jrc5p7cc10mkwyiagqz";
+  };
+
+  buildInputs = [ pkgconfig wxGTK (wxGTK.gtk) libX11 ]  ++ lib.optional openglSupport pyopengl;
+
+  NIX_LDFLAGS = "-lX11 -lgdk-x11-2.0";
+
+  WXPORT="gtk2";
+  NO_HEADERS=1;
+  BUILD_GLCANVAS=openglSupport;
+  UNICODE=1;
+
+  passthru = { inherit wxGTK openglSupport; };
+
+  # setup.py is in the folder `wxPython`.
+  # A required directory is apparently not being created so we do so manually.
+  preBuild = ''
+    pushd wxPython
+    mkdir -p build-gtk2.unicode/bdist.linux-x86_64/wheel
+  '';
+}
--- pkgs/development/python-modules/wxPython/3.0.nix
+++ pkgs/development/python-modules/wxPython/3.0.nix
@@ -1,9 +1,36 @@
-{ callPackage, ... } @ args:
+{ lib, buildPythonPackage, fetchurl, pkgconfig, isPy3k, isPyPy, wxGTK, openglSupport ? true, pyopengl
+, libX11, ...
+}:
 
-callPackage ./generic.nix (args // rec {
+assert wxGTK.unicode;
 
+buildPythonPackage rec {
+  name = "wxPython-${version}";
   version = "3.0.2.0";
 
-  sha256 = "0qfzx3sqx4mwxv99sfybhsij4b5pc03ricl73h4vhkzazgjjjhfm";
+  disabled = isPy3k || isPyPy;
+  doCheck = false;
 
-})
+  src = fetchurl {
+    url = "mirror://sourceforge/wxpython/wxPython-src-${version}.tar.bz2";
+    sha256 = "0qfzx3sqx4mwxv99sfybhsij4b5pc03ricl73h4vhkzazgjjjhfm";
+  };
+
+  buildInputs = [ pkgconfig wxGTK (wxGTK.gtk) libX11 ]  ++ lib.optional openglSupport pyopengl;
+
+  NIX_LDFLAGS = "-lX11 -lgdk-x11-2.0";
+
+  WXPORT="gtk2";
+  NO_HEADERS=1;
+  BUILD_GLCANVAS=openglSupport;
+  UNICODE=1;
+
+  passthru = { inherit wxGTK openglSupport; };
+
+  # setup.py is in the folder `wxPython`.
+  # A required directory is apparently not being created so we do so manually.
+  preBuild = ''
+    pushd wxPython
+    mkdir -p build/bdist.linux-x86_64/wheel/wx-3.0-gtk2
+  '';
+}
