a447894dc1592410dba7a2eba53b9ec561770d73
Tristan Helmich <fadenb@users.noreply.github.com>
2016-05-11 14:49:18 +0200

    squid: 3.5.17 -> 3.5.19 (#15385)

--- pkgs/servers/squid/default.nix
+++ pkgs/servers/squid/default.nix
@@ -1,10 +1,10 @@
 { fetchurl, stdenv, perl, lib, openldap, pam, db, cyrus_sasl, libcap,
 expat, libxml2, libtool, openssl}:
 stdenv.mkDerivation rec {
-  name = "squid-3.5.17";
+  name = "squid-3.5.19";
   src = fetchurl {
     url = "http://www.squid-cache.org/Versions/v3/3.5/${name}.tar.bz2";
-    sha256 = "1kdq778cm18ak4624gchmbi8avnzyvwgyzjplkd0fkcrgfs44bsf";
+    sha256 = "1iy2r7r12xv0q9414rczbqbbggyyxgdmg21bynpygwkgalaz1dxx";
   };
   buildInputs = [perl openldap pam db cyrus_sasl libcap expat libxml2
     libtool openssl];
