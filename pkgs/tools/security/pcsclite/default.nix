c65e2e18ce16d301bb952734d7e285279b0b243f
Franz Pletz <fpletz@fnordicwalking.de>
2016-05-13 00:11:55 +0200

    pcsclite: 1.8.14 -> 1.8.16

--- pkgs/tools/security/pcsclite/default.nix
+++ pkgs/tools/security/pcsclite/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, udev, dbus_libs, perl, python2 }:
 
 stdenv.mkDerivation rec {
-  name = "pcsclite-1.8.14";
+  name = "pcsclite-1.8.16";
 
   src = fetchurl {
-    url = "https://alioth.debian.org/frs/download.php/file/4138/pcsc-lite-1.8.14.tar.bz2";
-    sha256 = "0kik09dif6hih09vvprd7zvj31lnrclrbrh5y10mlca2c209f7xr";
+    url = "https://alioth.debian.org/frs/download.php/file/4164/pcsc-lite-1.8.16.tar.bz2";
+    sha256 = "12k8q0ckyy1fqcfh7x0b7kfrlfiscrqaqmidcggnzs4pi2iqml77";
   };
 
   configureFlags = [
