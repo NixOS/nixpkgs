5f76761dc244dd078d33d6769e3e8ba01a3691b5
Matthias Beyer <mail@beyermatthias.de>
2016-05-15 13:13:45 +0200

    daemonize: 1.7.6 -> 1.7.7

--- pkgs/tools/system/daemonize/default.nix
+++ pkgs/tools/system/daemonize/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name    = "daemonize-${version}";
-  version = "1.7.6";
+  version = "1.7.7";
 
   src = fetchurl {
     url    = "https://github.com/bmc/daemonize/archive/release-${version}.tar.gz";
-    sha256 = "07yv82mkhc57vnawwldqcy64c5yqyh817gksd3b4rgavbsp1fmwd";
+    sha256 = "01gabcc8m4jkymd31p6v5883ii3g7126cici6rd03maf4jizxjmk";
   };
 
   meta = with stdenv.lib; {
