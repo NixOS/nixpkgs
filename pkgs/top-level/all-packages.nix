c7016de72c0c40f0ae77694c377052e639e2e2a4
Michiel Leenaars <ml.software@leenaa.rs>
2016-05-17 12:12:57 +0200

    mimetic: init at 0.9.8
    Closes #16258

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2429,6 +2429,8 @@ in
 
   mimeo = callPackage ../tools/misc/mimeo { };
 
+  mimetic = callPackage ../development/libraries/mimetic { };
+
   minissdpd = callPackage ../tools/networking/minissdpd { };
 
   miniupnpc = callPackage ../tools/networking/miniupnpc { };
