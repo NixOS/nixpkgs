af364c0f775464f902f47257e673cfebb180540e
Vladimír Čunát <vcunat@gmail.com>
2016-05-17 11:35:49 +0200

    fetchurl mirrors: fix gnupg URLs
    Some mirrors were missing /gcrypt. Now they should be consistent.
    Fixes 15510. Closes 15511.

--- pkgs/build-support/fetchurl/mirrors.nix
+++ pkgs/build-support/fetchurl/mirrors.nix
@@ -54,9 +54,9 @@ rec {
     https://gnupg.org/ftp/gcrypt/
     http://www.ring.gr.jp/pub/net/
     http://gd.tuwien.ac.at/privacy/
-    http://mirrors.dotsrc.org/
-    http://ftp.heanet.ie/mirrors/ftp.gnupg.org/
-    http://www.mirrorservice.org/sites/ftp.gnupg.org/
+    http://mirrors.dotsrc.org/gcrypt/
+    http://ftp.heanet.ie/mirrors/ftp.gnupg.org/gcrypt/
+    http://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/
   ];
 
   # kernel.org's /pub (/pub/{linux,software}) tree.
