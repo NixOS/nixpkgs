22b0cb9a726098c3b034c373297554c7af5b3aac
Mathias Schreck <schreck.mathias@googlemail.com>
2016-05-18 10:31:21 +0200

    nodejs: 6.1.0 -> 6.2.0

--- pkgs/development/web/nodejs/v6.nix
+++ pkgs/development/web/nodejs/v6.nix
@@ -4,9 +4,9 @@
 }@args:
 
 import ./nodejs.nix (args // rec {
-  version = "6.1.0";
+  version = "6.2.0";
   src = fetchurl {
     url = "https://nodejs.org/download/release/v${version}/node-v${version}.tar.gz";
-    sha256 = "1hsbmr3yc6zmyhz058zfxg77dzjhk94g1k0y65p6xq8ihq5yyrwy";
+    sha256 = "1fcldsnkk6px5fms405j9z2yv6mmscin5x7sma8bdavqgn283zgw";
   };
 })
