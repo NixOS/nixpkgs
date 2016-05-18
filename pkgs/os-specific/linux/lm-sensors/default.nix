b71463128c984afd8aa8ad5f8562475f1bbfa16b
Tobias Geerinckx-Rice <me@tobias.gr>
2016-05-18 15:57:09 +0200

    lm_sensors: add Fedora mirror
    Upstream's down, causing build failures on machines not mine.
    Verified against cached upstream tarball in my store.

--- pkgs/os-specific/linux/lm-sensors/default.nix
+++ pkgs/os-specific/linux/lm-sensors/default.nix
@@ -1,12 +1,14 @@
 { stdenv, fetchurl, bison, flex, which, perl }:
 
-let version = "3.4.0"; in
-
 stdenv.mkDerivation rec {
   name = "lm-sensors-${version}";
+  version = "3.4.0"; # don't forget to tweak fedoraproject mirror URL hash
   
   src = fetchurl {
-    url = "http://dl.lm-sensors.org/lm-sensors/releases/lm_sensors-${version}.tar.bz2";
+    urls = [
+      "http://dl.lm-sensors.org/lm-sensors/releases/lm_sensors-${version}.tar.bz2"
+      "http://pkgs.fedoraproject.org/repo/pkgs/lm_sensors/lm_sensors-${version}.tar.bz2/c03675ae9d43d60322110c679416901a/lm_sensors-${version}.tar.bz2"
+    ];
     sha256 = "07q6811l4pp0f7pxr8bk3s97ippb84mx5qdg7v92s9hs10b90mz0";
   };
 
