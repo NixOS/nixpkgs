73fb64a4b5575d829132de347424e4e675895f9a
Franz Pletz <fpletz@fnordicwalking.de>
2016-05-18 04:58:14 +0200

    Revert "linuxPackages: Switch to 4.6 as default kernel"
    This reverts commit 631e8b082a72e09a01488d3e312b5275a62f1ff6.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10861,7 +10861,7 @@ in
   };
 
   # The current default kernel / kernel modules.
-  linuxPackages = self.linuxPackages_4_6;
+  linuxPackages = self.linuxPackages_4_5;
   linux = self.linuxPackages.kernel;
 
   # Update this when adding the newest kernel major version!
