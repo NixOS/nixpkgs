74cba61eb06b3f51ae1bb49893b1288e43f98155
Nikolay Amiantov <ab@fmap.me>
2016-05-19 13:52:52 +0300

    linuxPackages.dpdk: force deterministic arch yet again

--- pkgs/os-specific/linux/dpdk/default.nix
+++ pkgs/os-specific/linux/dpdk/default.nix
@@ -14,8 +14,8 @@ stdenv.mkDerivation rec {
   RTE_KERNELDIR = "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build";
   RTE_TARGET = "x86_64-native-linuxapp-gcc";
 
-  # we need sse3 instructions to build
-  NIX_ENFORCE_NO_NATIVE = "0";
+  # we need ssse3 instructions to build
+  NIX_CFLAGS_COMPILE = [ "-march=core2" ];
 
   enableParallelBuilding = true;
   outputs = [ "out" "examples" ];
