4c646dd6cb608a8e9f7d9f70dddb85fd4b076543
Vladimír Čunát <vcunat@gmail.com>
2016-05-19 22:23:09 +0200

    harfbuzz: 1.1.2 -> 1.2.7
    Updated, after longer time.

--- pkgs/development/libraries/harfbuzz/default.nix
+++ pkgs/development/libraries/harfbuzz/default.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  version = "1.1.2";
+  version = "1.2.7";
   inherit (stdenv.lib) optional optionals optionalString;
 in
 
@@ -14,7 +14,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${version}.tar.bz2";
-    sha256 = "07s6z3hbrb4rdfgzmln169wxz4nm5y7qbr02ik5c7drxpn85fb2a";
+    sha256 = "09lh8x6qj0cd950whgaqqi3c4pqbl6z7aw9ddm73i14bw056185v";
   };
 
   outputs = [ "dev" "out" ];
