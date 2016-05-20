603db7fa6ff7515bc92a40c02725e918f3665040
José Romildo Malaquias <malaquias@gmail.com>
2016-05-20 11:52:31 -0300

    numix-gtk-theme: 2.5.1 -> 2016-05-19

--- pkgs/misc/themes/numix-gtk-theme/default.nix
+++ pkgs/misc/themes/numix-gtk-theme/default.nix
@@ -1,25 +1,31 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchFromGitHub, sass, glib, libxml2, gdk_pixbuf
+, gtk-engine-murrine
+}:
 
 stdenv.mkDerivation rec {
-  version = "2.5.1";
+  version = "2016-05-19";
   name = "numix-gtk-theme-${version}";
-  
-  src = fetchurl {
-    url = "https://github.com/numixproject/Numix/archive/v${version}.tar.gz";
-    sha256 = "0y6c4xr2n9sygxhgviwd97l02n17n53bkpfp62srkm05cq0jy87k";
+
+  src = fetchFromGitHub {
+    repo = "numix-gtk-theme";
+    owner = "numixproject";
+    rev = "266945047ad8c148d36d0d1f00b39730b84482a9";
+    sha256 = "108qjqwn9shqjkbadyw79y1wbq5ndv30x7xw5wjmbcss5jikr3v1";
   };
 
-  dontBuild = true;
+  nativeBuildInputs = [ sass glib libxml2 gdk_pixbuf ];
+
+  buildInputs = [ gtk-engine-murrine ];
 
   installPhase = ''
-    install -dm 755 $out/share/themes/Numix
-    cp -dr --no-preserve='ownership' {LICENSE,CREDITS,index.theme,gtk-2.0,gtk-3.0,metacity-1,openbox-3,unity,xfce-notify-4.0,xfwm4} $out/share/themes/Numix/
+    make install DESTDIR="$out"
   '';
-  
+
   meta = {
-    description = "Numix GTK theme";
+    description = "Modern flat theme with a combination of light and dark elements (GNOME, Unity, Xfce and Openbox)";
     homepage = https://numixproject.org;
-    license = stdenv.lib.licenses.gpl3;
+    license = stdenv.lib.licenses.gpl3Plus;
     platforms = stdenv.lib.platforms.all;
+    maintainers = [ stdenv.lib.maintainers.romildo ];
   };
 }
