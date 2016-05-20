37b5aa040d9a9c96e30b01aabaafc998d80c3948
Eric Sagnes <eric.sagnes@gmail.com>
2016-05-20 22:04:04 +0900

    fcitx-wrapper: remove outdated comments

--- pkgs/tools/inputmethods/fcitx/wrapper.nix
+++ pkgs/tools/inputmethods/fcitx/wrapper.nix
@@ -1,17 +1,5 @@
 { stdenv, symlinkJoin, fcitx, fcitx-configtool, makeWrapper, plugins, kde5 }:
 
-# This is based on the pidgin-with-plugins package.
-# Users should be able to configure what plugins are used
-# by putting the following in their /etc/nixos/configuration.nix:
-# environment.systemPackages = with pkgs; [
-#     (fcitx-with-plugins.override { plugins = [ fcitx-anthy ]; })
-# ]
-# Or, a normal user could use it by putting the following in his
-# ~/.nixpkgs/config.nix:
-# packageOverrides = pkgs: with pkgs; rec {
-#     (fcitx-with-plugins.override { plugins = [ fcitx-anthy ]; })
-# }
-
 symlinkJoin {
   name = "fcitx-with-plugins-${fcitx.version}";
 
@@ -24,4 +12,3 @@ symlinkJoin {
       --set FCITXDIR "$out/"
   '';
 }
-
