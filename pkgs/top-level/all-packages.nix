cc91d802f942b0a132d63ff26b05bc43b768e503
Taylor "Nekroze" Lawson <tlawson@nekroze.com>
2016-05-20 19:52:33 +1000

    Added ldc-calypso package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1322,6 +1322,8 @@ in
 
   dmd = callPackage ../development/compilers/dmd { };
 
+  ldc-calypso = callPackage ../development/compilers/ldc/calypso.nix { };
+
   dmg2img = callPackage ../tools/misc/dmg2img { };
 
   docbook2odf = callPackage ../tools/typesetting/docbook2odf {
