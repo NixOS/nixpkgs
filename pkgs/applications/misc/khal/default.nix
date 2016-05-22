aa0d96134f0f5891906db4cc57a0f1e855cc40f8
Matthias Beyer <mail@beyermatthias.de>
2016-05-22 18:34:14 +0200

    khal: 0.7.0 -> 0.8.2

--- pkgs/applications/misc/khal/default.nix
+++ pkgs/applications/misc/khal/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, pkgs, python3Packages }:
 
 python3Packages.buildPythonApplication rec {
-  version = "0.7.0";
+  version = "0.8.2";
   name = "khal-${version}";
 
   src = fetchurl {
     url = "mirror://pypi/k/khal/khal-${version}.tar.gz";
-    sha256 = "00llxj7cv31mjsx0j6zxmyi9s1q20yvfkn025xcy8cv1ylfwic66";
+    sha256 = "0ihclh3jsxhvq7azgdxbdzwbl7my30cdcg3g5ss5bpm4ivskrzzj";
   };
 
   propagatedBuildInputs = with python3Packages; [
@@ -23,6 +23,7 @@ python3Packages.buildPythonApplication rec {
     tzlocal
     urwid
     pkginfo
+    freezegun
   ];
   buildInputs = with python3Packages; [ setuptools_scm ];
 
