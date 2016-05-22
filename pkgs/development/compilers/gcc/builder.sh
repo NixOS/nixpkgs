NixOS/nixpkgs

commit 90ee00324077acfeec88c94f3f51a5a6ef132c89

gcc: fix mingw build after closure-size merge
This may also (help to) fix also other less usual configurations.

pkgs/development/compilers/gcc/builder.sh
@@ -210,10 +210,14 @@ preInstall() {
 
 
 postInstall() {
+    mkdir -p "$lib" # some configs don't have anything to put into $lib
+
     # Move runtime libraries to $lib.
     moveToOutput "lib/lib*.so*" "$lib"
     moveToOutput "lib/lib*.la"  "$lib"
-    ln -s lib "$lib/lib64" # for *.la
+    if [ -d "$lib/lib" ]; then
+        ln -s lib "$lib/lib64" # for *.la
+    fi
     moveToOutput "share/gcc-*/python" "$lib"
 
     for i in "$lib"/lib/*.{la,py}; do

commit 70cecb9cbdc7684885e348e2e82308f8da373953

gcc: fix #15033: avoid lib64/ symlink to 32-bit lib/
This was caused by me in 90ee003240.

pkgs/development/compilers/gcc/builder.sh
@@ -201,23 +201,20 @@ postConfigure() {
 
 
 preInstall() {
-    # Make ‘lib64’ a symlink to ‘lib’.
+    # Make ‘lib64’ symlinks to ‘lib’.
     if [ -n "$is64bit" -a -z "$enableMultilib" ]; then
-        mkdir -p $out/lib
-        ln -s lib $out/lib64
+        mkdir -p "$out/lib"
+        ln -s lib "$out/lib64"
+        mkdir -p "$lib/lib"
+        ln -s lib "$lib/lib64"
     fi
 }
 
 
 postInstall() {
-    mkdir -p "$lib" # some configs don't have anything to put into $lib
-
     # Move runtime libraries to $lib.
     moveToOutput "lib/lib*.so*" "$lib"
     moveToOutput "lib/lib*.la"  "$lib"
-    if [ -d "$lib/lib" ]; then
-        ln -s lib "$lib/lib64" # for *.la
-    fi
     moveToOutput "share/gcc-*/python" "$lib"
 
     for i in "$lib"/lib/*.{la,py}; do