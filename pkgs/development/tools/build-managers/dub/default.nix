2964212b45fc665dc526ba34471e7c7c78d222e6
Tobias Geerinckx-Rice <me@tobias.gr>
2016-05-23 01:31:58 +0200

    dub: 0.9.24 -> 0.9.25

--- pkgs/development/tools/build-managers/dub/default.nix
+++ pkgs/development/tools/build-managers/dub/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchFromGitHub, curl, dmd, gcc }:
 
-let version = "0.9.24"; in
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "dub-${version}";
+  version = "0.9.25";
 
   src = fetchFromGitHub {
-    sha256 = "1j2cs2mvaj6bjjicabq6lh97nx0v4b2k6pj4cmywki7hf3i1p8yy";
+    sha256 = "0cb4kx72fvk6vfqkk0mrp6fvv512xhw03dq2dn9lng0daydvdcim";
     rev = "v${version}";
     repo = "dub";
     owner = "D-Programming-Language";
@@ -15,16 +15,17 @@ stdenv.mkDerivation {
   propagatedBuildInputs = [ gcc dmd ];
 
   buildPhase = ''
-      # Avoid that the version file is overwritten
-      substituteInPlace build.sh \
-          --replace source/dub/version_.d /dev/null
-      ./build.sh
+    # Avoid that the version file is overwritten
+    substituteInPlace build.sh \
+      --replace source/dub/version_.d /dev/null
+    patchShebangs ./build.sh
+    ./build.sh
   '';
 
   installPhase = ''
-      mkdir $out
-      mkdir $out/bin
-      cp bin/dub $out/bin
+    mkdir $out
+    mkdir $out/bin
+    cp bin/dub $out/bin
   '';
 
   meta = with stdenv.lib; {
