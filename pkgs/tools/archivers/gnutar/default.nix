dc5bbc4700dd95420f87141efcc0e6cb48a710f9
Vladimír Čunát <vcunat@gmail.com>
2016-05-22 18:06:07 +0200

    gnutar: remove patch already applied upstream
    Fallout from #15567. Only Darwin is affected, hashes of the rest remain.

--- pkgs/tools/archivers/gnutar/default.nix
+++ pkgs/tools/archivers/gnutar/default.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
     sha256 = "097hx7sbzp8qirl4m930lw84kn0wmxhmq7v1qpra3mrg0b8cyba0";
   };
 
-  patches = stdenv.lib.optional stdenv.isDarwin ./gnutar-1.28-darwin.patch;
+  patches = [ ]; # FIXME: remove on another stdenv rebuild
 
   # avoid retaining reference to CF during stdenv bootstrap
   configureFlags = stdenv.lib.optionals stdenv.isDarwin [
