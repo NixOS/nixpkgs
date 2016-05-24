823ec00fdf9cbde3da11f411789c20462d9055f5
Tobias Geerinckx-Rice <me@tobias.gr>
2016-05-24 22:25:02 +0200

    libwps: 0.4.2 -> 0.4.3

--- pkgs/development/libraries/libwps/default.nix
+++ pkgs/development/libraries/libwps/default.nix
@@ -1,20 +1,19 @@
 { stdenv, fetchurl, boost, pkgconfig, librevenge, zlib }:
 
-let version = "0.4.2"; in
 stdenv.mkDerivation rec {
   name = "libwps-${version}";
+  version = "0.4.3";
 
   src = fetchurl {
-    url = "mirror://sourceforge/libwps/${name}.tar.gz";
-    sha256 = "0c90i3zafxxsj989bd9bs577blx3mrb90rj52iv6ijc4qivi4wkr";
+    url = "mirror://sourceforge/libwps/${name}.tar.bz2";
+    sha256 = "0v1a0hj96i4jhb5833336s4zcslzb6md5cnmnrvgywx8cmw40c0c";
   };
 
   buildInputs = [ boost pkgconfig librevenge zlib ];
 
   meta = with stdenv.lib; {
-    inherit version;
     homepage = http://libwps.sourceforge.net/;
-    description = "Microsoft Works file word processor format import filter library";
+    description = "Microsoft Works document format import filter library";
     platforms = platforms.linux;
     license = licenses.lgpl21;
   };
