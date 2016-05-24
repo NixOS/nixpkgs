d84741a4bfb6ed2531ec7154479c147d2c9a737c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-05-24 21:07:26 +0200

    X server: Log to the journal instead of /var/log/X.0.log
    This ensures that "journalctl -u display-manager" does what you would
    expect in 2016. However, the main reason is to ensure that our VM
    tests show the output of the X server.
    
    A slight problem is that with KDE user switching, messages from the
    various X servers end up in the same place. However, that's an
    improvement over the previous situation, where the second X server
    would overwrite the /var/log/X.0.log of the first. (This was caused by
    the fact that we were passing a hard-coded value for -logfile.)

--- nixos/modules/services/x11/display-managers/kdm.nix
+++ nixos/modules/services/x11/display-managers/kdm.nix
@@ -139,7 +139,7 @@ in
             mkdir -m 0755 -p /var/lib/kdm
             chown kdm /var/lib/kdm
             ${(optionalString (config.system.boot.loader.id == "grub" && config.system.build.grub != null) "PATH=${config.system.build.grub}/sbin:$PATH ") +
-              "KDEDIRS=/run/current-system/sw exec ${kdebase_workspace}/bin/kdm -config ${kdmrc} -nodaemon"}
+              "KDEDIRS=/run/current-system/sw exec ${kdebase_workspace}/bin/kdm -config ${kdmrc} -nodaemon -logfile /dev/stderr"}
           '';
         logsXsession = true;
       };
