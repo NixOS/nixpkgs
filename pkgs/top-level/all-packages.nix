ae3a35da92f929e57de0c31777f138381b3d2e37
Andrey Trachenko <goodrone@users.noreply.github.com>
2016-05-24 21:18:03 +0000

    simavr: init at 1.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9014,6 +9014,8 @@ in
   silgraphite = callPackage ../development/libraries/silgraphite {};
   graphite2 = callPackage ../development/libraries/silgraphite/graphite2.nix {};
 
+  simavr = callPackage ../development/tools/simavr { };
+
   simgear = callPackage ../development/libraries/simgear { };
 
   simp_le = callPackage ../tools/admin/simp_le { };
