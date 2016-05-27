d231cdf51fd00400f20815d5e2a7c84d1d23f891
Nikolay Amiantov <ab@fmap.me>
2016-05-27 12:10:17 +0300

    rxvt_unicode-with-plugins: don't be paranoid about Perl support
    Fixes #15743

--- pkgs/applications/misc/rxvt_unicode/wrapper.nix
+++ pkgs/applications/misc/rxvt_unicode/wrapper.nix
@@ -1,15 +1,12 @@
 { stdenv, symlinkJoin, rxvt_unicode, makeWrapper, plugins }:
 
 let
-  rxvt = rxvt_unicode.override {
-    perlSupport = true;
-  };
-  rxvt_name = builtins.parseDrvName rxvt.name;
+  rxvt_name = builtins.parseDrvName rxvt_unicode.name;
 
 in symlinkJoin {
   name = "${rxvt_name.name}-with-plugins-${rxvt_name.version}";
 
-  paths = [ rxvt ] ++ plugins;
+  paths = [ rxvt_unicode ] ++ plugins;
 
   buildInputs = [ makeWrapper ];
 
