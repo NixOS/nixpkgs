44ae9a3c0a92b68c3e4b8425784ddc8a777c866d
Eric Litak <elitak@gmail.com>
2016-05-29 09:00:22 -0700

    reorganize crossDrv hooks

--- pkgs/development/libraries/glibc/default.nix
+++ pkgs/development/libraries/glibc/default.nix
@@ -40,7 +40,7 @@ in
     # Building from a proper gcc staying in the path where it was installed,
     # libgcc_s will not be at {gcc}/lib, and gcc's libgcc will be found without
     # any special hack.
-    preInstall = if cross != null then "" else ''
+    preInstall = ''
       if [ -f ${stdenv.cc.cc}/lib/libgcc_s.so.1 ]; then
           mkdir -p $out/lib
           cp ${stdenv.cc.cc}/lib/libgcc_s.so.1 $out/lib/libgcc_s.so.1
@@ -113,6 +113,10 @@ in
 
         dontStrip=1
       '';
+
+      preInstall = null; # clobber the native hook
+
+      separateDebugInfo = false; # this is currently broken for crossDrv
 
       # To avoid a dependency on the build system 'bash'.
       preFixup = ''
