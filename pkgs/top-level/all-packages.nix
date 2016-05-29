e645389834c01696748af90847f2e71be7b781b8
Frederik Rietdijk <freddyrietdijk@fridh.nl>
2016-05-29 23:31:47 +0200

    Neovim: use Python 3.5 again (#15781)
    neovim: use Python 3.5 again

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14457,7 +14457,6 @@ in
 
   neovim = callPackage ../applications/editors/neovim {
     inherit (lua52Packages) lpeg luaMessagePack luabitop;
-    python3Packages = python34Packages;
   };
 
   neovim-qt = callPackage ../applications/editors/neovim/qt.nix {
