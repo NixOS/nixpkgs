8ea1f2d36cef8060e66f2d8a8d367672d2e3c027
Paul Hendry <paul@pshendry.com>
2016-05-30 01:15:58 -0700

    syncthing: 0.12.25 -> 0.13.4 (#15730)
    * Rename v0.12.25 package from 'syncthing' to 'syncthing012'
    * Remove syncthing011

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14229,8 +14229,9 @@ in
 
   symlinks = callPackage ../tools/system/symlinks { };
 
-  syncthing = go15Packages.syncthing.bin // { outputs = [ "bin" ]; };
-  syncthing011 = go15Packages.syncthing011.bin // { outputs = [ "bin" ]; };
+  syncthing = callPackage ../applications/networking/syncthing { };
+
+  syncthing012 = go15Packages.syncthing012.bin // { outputs = [ "bin" ]; };
 
   # linux only by now
   synergy = callPackage ../applications/misc/synergy { };
