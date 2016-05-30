0120d2b898386c95065c7aa590478fedcccc98ed
Florent Becker <Florent.Becker@ens-lyon.org>
2016-05-30 15:11:44 +0200

    ocaml-markup: init at 0.7.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5007,6 +5007,8 @@ in
 
     magick = callPackage ../development/ocaml-modules/magick { };
 
+    markup = callPackage ../development/ocaml-modules/markup { lwt = ocaml_lwt; };
+
     menhir = callPackage ../development/ocaml-modules/menhir { };
 
     merlin = callPackage ../development/tools/ocaml/merlin { };
