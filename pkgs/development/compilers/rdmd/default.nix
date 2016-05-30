353104a486375e927d39b009b9a6f1b2036a2c8e
Matthias Beyer <mail@beyermatthias.de>
2016-05-30 20:45:08 +0200

    rdmd: 2.067.0 -> 2.071.0

--- pkgs/development/compilers/rdmd/default.nix
+++ pkgs/development/compilers/rdmd/default.nix
@@ -1,13 +1,13 @@
 {stdenv, lib, fetchurl, dmd}:
 
 stdenv.mkDerivation {
-  name = "rdmd-2.067.0";
+  name = "rdmd-2.071.0";
 
   buildInputs = [ dmd ];
 
   src = fetchurl {
-    url = "https://github.com/D-Programming-Language/tools/archive/v2.067.0.tar.gz";
-    sha256 = "2702ecda0427c675084d9b688449bc8c8392fd73e30257d79e2488640d5a9982";
+    url = "https://github.com/D-Programming-Language/tools/archive/v2.071.0.tar.gz";
+    sha256 = "0hf4sbpviwjg8n1sh212v42017jxhwghr5dw79rcmqjyp16487z4";
   };
 
   buildPhase = ''
