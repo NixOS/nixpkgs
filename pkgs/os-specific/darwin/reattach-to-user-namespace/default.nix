8e18a2a2d53a478b24fe8e758c846f235028adfe
Tobias Pflug <tobias.pflug@holidaycheck.com>
2016-05-31 19:12:45 +0200

    reattach-to-user-namespace: fix sha

--- pkgs/os-specific/darwin/reattach-to-user-namespace/default.nix
+++ pkgs/os-specific/darwin/reattach-to-user-namespace/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "reattach-to-user-namespace-2.4";
   src = fetchgit {
     url = "https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard.git";
-    sha256 = "1f9q1wxq764zidnx5hbdkbbyxxzfih0l0cjpgr0pxzwbmd2q6cvv";
+    sha256 = "0hrh95di5dvpynq2yfcrgn93l077h28i6msham00byw68cx0dd3z";
     rev = "2765aeab8f337c29e260a912bf4267a2732d8640";
   };
   buildFlags = "ARCHES=x86_64";
