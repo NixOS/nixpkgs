0968641edad9a4655c3edf82d5946af3f4433ea1
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2016-06-01 22:52:34 +0300

    make-bootstrap-tools-cross.nix: Use busybox with musl
    Do this because I cannot figure out how to refer to `glibc.static`
    inside busybox when cross building in a manner that works.

--- pkgs/stdenv/linux/make-bootstrap-tools-cross.nix
+++ pkgs/stdenv/linux/make-bootstrap-tools-cross.nix
@@ -93,8 +93,7 @@ rec {
   })).crossDrv;
 
   busyboxMinimal = (pkgs.busybox.override {
-    # TBD: uClibc is broken.
-    # useUclibc = true;
+    useMusl = true;
     enableStatic = true;
     enableMinimal = true;
     extraConfig = ''
