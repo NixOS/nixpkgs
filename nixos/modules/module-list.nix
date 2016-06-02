ed466b7fef35e7e6ce4eeb80075e9b76151ff029
Tristan Helmich <tristan.helmich@gmail.com>
2016-06-02 21:00:00 +0200

    sonarr service: initial service

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -258,6 +258,7 @@
   ./services/misc/ripple-data-api.nix
   ./services/misc/rogue.nix
   ./services/misc/siproxd.nix
+  ./services/misc/sonarr.nix
   ./services/misc/spice-vdagentd.nix
   ./services/misc/subsonic.nix
   ./services/misc/sundtek.nix
