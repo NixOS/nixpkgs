3eeac052f402cf3b70bf76fd30971ec6f8df4f8e
Franz Pletz <fpletz@fnordicwalking.de>
2016-06-04 17:52:46 +0200

    linux: 3.18.29 -> 3.18.34

--- pkgs/os-specific/linux/kernel/linux-3.18.nix
+++ pkgs/os-specific/linux/kernel/linux-3.18.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.18.29";
+  version = "3.18.34";
   extraMeta.branch = "3.18";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0g8vlhifl31dyghiamykrpgj6n8h5w6gh6n88ir57z6lj188vaj8";
+    sha256 = "0ndsk28r76776wrlscsj7xl8r476ip3g2h1bghar8absm465z5zn";
   };
 
   kernelPatches = args.kernelPatches;
