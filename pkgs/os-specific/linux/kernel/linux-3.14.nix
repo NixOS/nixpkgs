c261538a32ed3b417af950a4a807774675b32b32
Franz Pletz <fpletz@fnordicwalking.de>
2016-06-04 17:50:47 +0200

    linux: 3.14.66 -> 3.14.71

--- pkgs/os-specific/linux/kernel/linux-3.14.nix
+++ pkgs/os-specific/linux/kernel/linux-3.14.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.14.66";
+  version = "3.14.71";
   extraMeta.branch = "3.14";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "08fa2hn8vva0gfnpkdh9fa0lhf5hg5lpgqmf38l5bpaahvwfkg6k";
+    sha256 = "0yvp5rlkiwgwkjggx79j8m4fhy5yr4d3xs5zcwd56dsvhzwzlmf3";
   };
 
   kernelPatches = args.kernelPatches;
