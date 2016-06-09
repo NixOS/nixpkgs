fa853994afb3ea41956ca50ed751599050f58985
koral <koral@mailoo.org>
2016-06-09 21:42:07 +0200

    Add package pythonPackages.pysnmi

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18254,6 +18254,29 @@ in modules // {
     };
   });
 
+  pysmi = buildPythonPackage rec {
+    version = "0.0.7";
+    name = "pysmi-${version}";
+
+    src = pkgs.fetchurl {
+      url = "mirror://pypi/p/pysmi/${name}.tar.gz";
+      sha256 = "05h1lv2a687b9qjc399w6728ildx7majbn338a0c4k3gw6wnv7wr";
+    };
+
+    propagatedBuildInputs = with self; [ ply ];
+
+    # ImportError: No module named pysnmp.smi.builder
+    doCheck = false;
+
+    meta = {
+      homepage = https://sourceforge.net/projects/pysmi/;
+      description = "A pure-Python implementation of SNMP/SMI MIB parsing and conversion library";
+      license = licenses.bsd2;
+      platforms = platforms.all;
+      maintainers = with maintainers; [ koral ];
+    };
+  };
+
   pysnmp = buildPythonPackage rec {
     version = "4.2.5";
     name = "pysnmp-${version}";
