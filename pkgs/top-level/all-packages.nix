e66b51b165c96bf510e805b20e0d095de0036834
Franz Pletz <fpletz@fnordicwalking.de>
2016-06-10 16:14:36 +0200

    Revert "runc: init at baf6536d"
    This reverts commit af6ef4f84fa2f4fdbe7a0192c393b3b54203e9f0.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14008,10 +14008,6 @@ in
 
   rubyripper = callPackage ../applications/audio/rubyripper {};
 
-  runc = callPackage ../applications/virtualization/runc {
-    md2man = goPackages.go-md2man;
-  };
-
   rxvt = callPackage ../applications/misc/rxvt { };
 
   # urxvt
