4fbafb23957c04dded895a17f052428fa21e51e9
Shea Levy <shea@shealevy.com>
2016-06-10 09:30:11 -0400

    linux 4.6.1 -> 4.6.2

--- pkgs/os-specific/linux/kernel/linux-4.6.nix
+++ pkgs/os-specific/linux/kernel/linux-4.6.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.6.1";
+  version = "4.6.2";
   extraMeta.branch = "4.6";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "073vpwidl28ka3y2rd7n6dcckrppi5kalh48vsz980k9z12g9pfm";
+    sha256 = "e158f3c69da87c2ec28d0f194dbe18b05e0d0b9e1142566615cea3390bab1c6a";
   };
 
   kernelPatches = args.kernelPatches;
