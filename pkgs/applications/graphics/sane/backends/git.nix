6558f0add3f15e62f182bf33e1acf0c857d78832
Tobias Geerinckx-Rice <me@tobias.gr>
2016-06-11 21:11:43 +0200

    sane-backends-git: fix corrupted hash

--- pkgs/applications/graphics/sane/backends/git.nix
+++ pkgs/applications/graphics/sane/backends/git.nix
@@ -3,7 +3,7 @@
 callPackage ./generic.nix (args // {
   version = "2016-06-11";
   src = fetchgit {
-    sha256 = "6312c514e3c47f3f7003e3f54750485950720aaa35c8f0e5b7a361bc04e2a795";
+    sha256 = "0jpavig7bg7l72drlwipmsg03j6qdy5aq2r3kj6a2h6ahpnm2549";
     rev = "5ba37467e88ca8052973b37128ce8fd36ad5d61d";
     url = "git://alioth.debian.org/git/sane/sane-backends.git";
   };
