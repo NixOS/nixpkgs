fa5383d1fce4be75955592deb03968056c980388
zimbatm <zimbatm@zimbatm.com>
2016-06-12 14:38:11 +0100

    influxdb: 0.9.4 -> 0.13.0

--- pkgs/servers/nosql/influxdb/default.nix
+++ pkgs/servers/nosql/influxdb/default.nix
@@ -1,26 +1,29 @@
 { lib, buildGoPackage, fetchFromGitHub }:
 
 buildGoPackage rec {
-  name = "influxdb-${rev}";
-  rev = "v0.9.4";
-  goPackagePath = "github.com/influxdb/influxdb";
+  name = "influxdb-${version}";
+  version = "0.13.0";
+
+  goPackagePath = "github.com/influxdata/influxdb";
 
   src = fetchFromGitHub {
-    inherit rev;
-    owner = "influxdb";
+    owner = "influxdata";
     repo = "influxdb";
-    sha256 = "0yarymppnlpf2xab57i8jx595v47s5mdwnf13719mc1fv3q84yqn";
+    rev = "v${version}";
+    sha256 = "0f7af5jb1f65qnslhc7zccml1qvk6xx5naczqfsf4s1zc556fdi4";
   };
 
   excludedPackages = "test";
 
+  # Generated with the `gdm2nix.rb` script and the `Godeps` file from the
+  # influxdb repo root.
   goDeps = ./deps.json;
 
   meta = with lib; {
     description = "An open-source distributed time series database";
     license = licenses.mit;
     homepage = https://influxdb.com/;
-    maintainers = with maintainers; [ offline ];
+    maintainers = with maintainers; [ offline zimbatm ];
     platforms = platforms.linux;
   };
 }
