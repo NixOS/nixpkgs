b6a93998520c034f779c4a7740c561f15737d1f2
Joachim Schiele <js@lastlog.de>
2016-06-14 01:28:14 +0200

    electrong: 0.36.2 to 1.2.2 bump

--- pkgs/development/tools/electron/default.nix
+++ pkgs/development/tools/electron/default.nix
@@ -1,12 +1,12 @@
-{ stdenv, lib, callPackage, fetchurl, unzip, atomEnv }:
+{ stdenv, lib, callPackage, fetchurl, unzip, atomEnv, xorg }:
 
 stdenv.mkDerivation rec {
   name = "electron-${version}";
-  version = "0.36.2";
+  version = "1.2.2";
 
   src = fetchurl {
     url = "https://github.com/atom/electron/releases/download/v${version}/electron-v${version}-linux-x64.zip";
-    sha256 = "01d78j8dfrdygm1r141681b3bfz1f1xqg9vddz7j52z1mlfv9f1d";
+    sha256 = "0jqzs1297f6w7s4j9pd7wyyqbidb0c61yjz47raafslg6nljgp1c";
     name = "${name}.zip";
   };
 
@@ -21,7 +21,7 @@ stdenv.mkDerivation rec {
 
     patchelf \
       --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-      --set-rpath "${atomEnv.libPath}:$out/lib/electron" \
+      --set-rpath "${atomEnv.libPath}:${xorg.libXScrnSaver}/lib:${xorg.libXScrnSaver}/lib64:$out/lib/electron" \
       $out/lib/electron/electron
   '';
 
