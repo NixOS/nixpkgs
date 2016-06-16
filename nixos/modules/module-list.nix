4e50880c828a771ab9ea72739bb70017b724358c
Matthew Bauer <mjbauer95@gmail.com>
2016-06-16 19:18:09 +0000

    packagekit: add latest from hughsie's github repo
    - currently pulled in from Git until the next release of PackageKit
      has Nix support
    - also: add in a service module to start packagekit properly
    - nixos service can be enabled via services.packagekit.enable
    - packagekit requires nixunstable to build properly

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -249,6 +249,7 @@
   ./services/misc/nix-ssh-serve.nix
   ./services/misc/nzbget.nix
   ./services/misc/octoprint.nix
+  ./services/misc/packagekit.nix
   ./services/misc/parsoid.nix
   ./services/misc/phd.nix
   ./services/misc/plex.nix
