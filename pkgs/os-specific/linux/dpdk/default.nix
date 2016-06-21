85ca60250730ee2661dba8db0242a1c323c83315
Ruslan Babayev <ruslan@babayev.com>
2016-06-21 08:16:33 -0700

    dpdk: fix a typo

--- pkgs/os-specific/linux/dpdk/default.nix
+++ pkgs/os-specific/linux/dpdk/default.nix
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
   RTE_KERNELDIR = "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build";
   RTE_TARGET = "x86_64-native-linuxapp-gcc";
 
-  # we need ssse3 instructions to build
+  # we need sse3 instructions to build
   NIX_CFLAGS_COMPILE = [ "-march=core2" ];
 
   enableParallelBuilding = true;
