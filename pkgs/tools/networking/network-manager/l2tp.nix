a8074165eff35115984ef95be06da599e43b32c4
obadz <obadz-git@obadz.com>
2016-06-22 01:53:23 +0100

    network-manager/l2tp: 0.9.8.7 -> 1.2.2

--- pkgs/tools/networking/network-manager/l2tp.nix
+++ pkgs/tools/networking/network-manager/l2tp.nix
@@ -1,20 +1,20 @@
 { stdenv, fetchFromGitHub, automake, autoconf, libtool, intltool, pkgconfig
-, networkmanager, ppp, xl2tpd, strongswan
+, networkmanager, networkmanagerapplet, ppp, xl2tpd, strongswan, libsecret
 , withGnome ? true, gnome3 }:
 
 stdenv.mkDerivation rec {
-  name = "${pname}${if withGnome then "-gnome" else ""}-${version}";
-  pname = "NetworkManager-l2tp";
-  version = "0.9.8.7";
+  name    = "${pname}${if withGnome then "-gnome" else ""}-${version}";
+  pname   = "NetworkManager-l2tp";
+  version = networkmanager.version;
 
   src = fetchFromGitHub {
-    owner  = "seriyps";
-    repo   = "NetworkManager-l2tp";
+    owner  = "nm-l2tp";
+    repo   = "network-manager-l2tp";
     rev    = version;
-    sha256 = "07gl562p3f6l2wn64f3vvz1ygp3hsfhiwh4sn04c3fahfdys69zx";
+    sha256 = "1zqdhm7pzhaq6q8pddj9ki25qs9m6qwqgzc5x07a0qffla2rq5j1";
   };
 
-  buildInputs = [ networkmanager ppp ]
+  buildInputs = [ networkmanager ppp networkmanagerapplet libsecret ]
     ++ stdenv.lib.optionals withGnome [ gnome3.gtk gnome3.libgnome_keyring ];
 
   nativeBuildInputs = [ automake autoconf libtool intltool pkgconfig ];
@@ -25,9 +25,6 @@ stdenv.mkDerivation rec {
     substituteInPlace ./src/nm-l2tp-service.c \
       --replace /sbin/ipsec  ${strongswan}/bin/ipsec \
       --replace /sbin/xl2tpd ${xl2tpd}/bin/xl2tpd
-
-    # Workaround https://github.com/xelerance/xl2tpd/issues/108
-    substituteInPlace ./src/nm-l2tp-service.c --replace 'write_config_option (pppopt_fd, "lock\n");' ""
   '';
 
   preConfigure = "./autogen.sh";
