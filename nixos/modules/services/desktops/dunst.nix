844483e5cfd546e1f6eab237842ad5d9ba7f917e
Matthias Beyer <mail@beyermatthias.de>
2016-06-23 18:22:18 +0200

    Revert "Use a start-script"
    This reverts commit 2c2437bd2432537f3b6da3749e35d0cbb130c326.

--- nixos/modules/services/desktops/dunst.nix
+++ nixos/modules/services/desktops/dunst.nix
@@ -80,12 +80,6 @@ let
     timeout     = ${toString cfg.urgency.critical.timeout}
 '';
 
-    dunstStartScript = builtins.toFile "dunst-start-script" ''
-      #!/usr/bin/env bash
-
-      ${pkgs.dunst}/bin/dunst -config ${dunstConf}
-    '';
-
 in
 with lib;
 {
@@ -435,7 +429,7 @@ with lib;
             description = "Dunst Daemon";
             wantedBy    = [ "multi-user.target" ];
 
-            serviceConfig.ExecStart = "${dunstStartScript}";
+            serviceConfig.ExecStart = "${pkgs.dunst}/bin/dunst -config ${dunstConf} ";
         };
 
     };
