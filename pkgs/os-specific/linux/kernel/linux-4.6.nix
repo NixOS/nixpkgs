0202cf7b2ef75f124fd7a642ca9badf2be2460b5
Tim Steinbach <tim@nequissimus.com>
2016-06-24 22:18:16 +0000

    kernel: 4.6.2 -> 4.6.3
    (cherry picked from commit 125ffff089b6bd360c82cf986d8cc9b17fc2e8ac)

--- pkgs/os-specific/linux/kernel/linux-4.6.nix
+++ pkgs/os-specific/linux/kernel/linux-4.6.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.6.2";
+  version = "4.6.3";
   extraMeta.branch = "4.6";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "e158f3c69da87c2ec28d0f194dbe18b05e0d0b9e1142566615cea3390bab1c6a";
+    sha256 = "0rq9cciabw5gfi2z30fcwh6z4zm2fgp938xmblfw59hyjj98h60k";
   };
 
   kernelPatches = args.kernelPatches;
