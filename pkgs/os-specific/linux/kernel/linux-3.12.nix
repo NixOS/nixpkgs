de0a27422bf4d5380cf43aa01e64eba7f4adf724
Franz Pletz <fpletz@fnordicwalking.de>
2016-06-24 22:33:41 +0200

    linux: 3.12.57 -> 3.12.61
    (cherry picked from commit b7e0b118d9246daab76982e5f7073cea8c010ce0)

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.12.57";
+  version = "3.12.61";
   extraMeta.branch = "3.12";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0qv88rvi0n45z3888w2gis35lxdx34qg2p7c2cac2szbrzv664s8";
+    sha256 = "1q44z8gdbrw76vl5wbm8y2vy119lacxyaf2xi6q9jasvwkjc4h36";
   };
 
   kernelPatches = args.kernelPatches;
