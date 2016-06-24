eb840acd0b7971de20af8827e9db3f5ff3819858
Alexander Ried <ried@mytum.de>
2016-06-24 17:54:57 +0200

    expat: 2.1.1 -> 2.2.0, /cc 16477
    Security patches have been integrated upstream, and
    (vcunat:) and apparently it includes more security fixes.
    
    (cherry picked from commit 964fd18676adf0afe5cabeb25fa693b0f4b08c60)

--- pkgs/development/libraries/expat/default.nix
+++ pkgs/development/libraries/expat/default.nix
@@ -1,16 +1,15 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "expat-2.1.1";
+  name = "expat-2.2.0";
 
   src = fetchurl {
     url = "mirror://sourceforge/expat/${name}.tar.bz2";
-    sha256 = "0ryyjgvy7jq0qb7a9mhc1giy3bzn56aiwrs8dpydqngplbjq9xdg";
+    sha256 = "1zq4lnwjlw8s9mmachwfvfjf2x3lk24jm41746ykhdcvs7r0zrfr";
   };
 
   configureFlags = stdenv.lib.optional stdenv.isFreeBSD "--with-pic";
 
-  patches = [ ./CVE-2015-1283-refix.patch ./CVE-2016-0718-v2-2-1.patch ];
 
   doCheck = true;
 
