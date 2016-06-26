d576729099728b2efa08baee0d3bb0f51c5ac537
Kranium Gikos Mendoza <kranium@gikos.net>
2016-06-26 15:03:58 +0800

    merkaartor: 0.18.1 -> 0.18.2

--- pkgs/applications/misc/merkaartor/default.nix
+++ pkgs/applications/misc/merkaartor/default.nix
@@ -1,15 +1,19 @@
-{ stdenv, fetchurl, qt4, qmake4Hook, boost, proj, gdal_1_11 }:
+{ stdenv, fetchFromGitHub, qt4, qmake4Hook, boost, proj, gdal, sqlite, pkgconfig }:
 
 stdenv.mkDerivation rec {
-  name = "merkaartor-0.18.1";
-  src = fetchurl {
-    url = "http://merkaartor.be/attachments/download/301/merkaartor-0.18.1.tar.bz2";
-    sha256 = "17qk45pmlxqigla1915dvn9pp91y85d2bkcaap4g3m8mk1crcsix";
+  name = "merkaartor-${version}";
+  version = "0.18.2";
+
+  src = fetchFromGitHub {
+    owner = "openstreetmap";
+    repo = "merkaartor";
+    rev = version;
+    sha256 = "1a8kzrc9w0b2a2zgw9dbbi15jy9ynv6nf2sg3k4dbh7f1s2ajx9l";
   };
 
-  buildInputs = [ qt4 boost proj gdal_1_11 ];
+  buildInputs = [ qt4 boost proj gdal sqlite ];
 
-  nativeBuildInputs = [ qmake4Hook ];
+  nativeBuildInputs = [ qmake4Hook pkgconfig ];
 
   meta = {
     description = "An openstreetmap editor";
