d71ef00b9dff04f72de9d0d64e719125d1e3d5c3
Bjørn Forsman <bjorn.forsman@gmail.com>
2016-06-28 18:23:25 +0200

    pythonPackages.lz4: init at 0.8.2
    Needed for xpra (better compression => better performance).

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15569,6 +15569,24 @@ in modules // {
   };
 
 
+  lz4 = buildPythonPackage rec {
+    name = "lz4-0.8.2";
+
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/b5/f0/e1de2bb7feb54011f3c4dcf35b7cca3536e19526764db051b50ea26b58e7/lz4-0.8.2.tar.gz";
+      sha256 = "1irad4sq4hdr30fr53smvv3zzk4rddcf9b4jx19w8s9xsxhr1x3b";
+    };
+
+    buildInputs= with self; [ nose ];
+
+    meta = with stdenv.lib; {
+      description = "Compression library";
+      homepage = https://github.com/python-lz4/python-lz4;
+      license = licenses.bsd3;
+    };
+  };
+
+
   oslo-concurrency = buildPythonPackage rec {
    name = "oslo-concurrency-${version}";
    version = "2.7.0";
