95da3e8dfd326dc0c81b7d952c850b8b04fd6c74
Matthew Bauer <mjbauer95@gmail.com>
2016-06-28 16:57:43 +0000

    travis: use binary tarball directly
    Downloads .bz2 binary from the NixOS.org releases page. Also tries to
    cache tar and nix store. Also some misc. style changes.

--- maintainers/scripts/travis-nox-review-pr.sh
+++ maintainers/scripts/travis-nox-review-pr.sh
@@ -5,9 +5,24 @@ set -e
 # but download-from-binary-cache doesn't respect
 export NIX_CURL_FLAGS=-sS
 
-if [ -d $HOME/.nix-profile ]; then
-    source $HOME/.nix-profile/etc/profile.d/nix.sh
-fi
+##
+## HARDCODED, needs to be updated with Nix
+NIX_VERSION=1.11.2
+case $TRAVIS_OS_NAME in
+    linux)
+        nix=/nix/store/4z8srway6dl128dxzn5r0wwdvglz3m61-nix-1.11.2
+        export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+        ;;
+    osx)
+        nix=/nix/store/g7wllqq33dc8a8dsb6r71wrbd6mz3ykf-nix-1.11.2
+        cacert=/nix/store/brfzgc99w9zyqj68i14w5jhyybg6j1sf-nss-cacert-3.21
+        if [ -d $cacert ]; then
+            export SSL_CERT_FILE=$cacert/etc/ssl/certs/ca-bundle.crt
+        fi
+        ;;
+esac
+##
+##
 
 while test -n "$1"; do
 
@@ -17,38 +32,82 @@ while test -n "$1"; do
     case $1 in
 
         install)
-            echo "=== Installing Nix..."
-
-            curl -sS https://nixos.org/nix/install | sh
 
             # Make sure we can use hydra's binary cache
-            sudo mkdir /etc/nix
-            echo "build-max-jobs = 4" | sudo tee /etc/nix/nix.conf > /dev/null
+            if [ ! -d /etc/nix/ ]; then
+                sudo mkdir -p /etc/nix
+                echo "build-max-jobs = 4" | sudo tee /etc/nix/nix.conf > /dev/null
+            fi
 
-	    # Make sure we can execute within /tmp in Linux
-            if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-                sudo mount -o remount,exec /run
-                sudo mount -o remount,exec /run/user
-                sudo mount > /dev/null
+            if [ ! -d /nix/ ]; then
+                echo "=== Installing Nix..."
+
+                case $TRAVIS_OS_NAME in
+                    linux) system=x86_64-linux ;;
+                    osx) system=x86_64-darwin ;;
+                esac
+
+                unpack=$HOME/nix-$NIX_VERSION-$system
+                if [ ! -d $unpack/store/ ]; then
+                    tarball=$HOME/nix-$NIX_VERSION-$system.tar.bz2
+                    if [ ! -e $tarball ]; then
+                        url="https://nixos.org/releases/nix/nix-$NIX_VERSION/nix-$NIX_VERSION-$system.tar.bz2"
+                        echo "Downloading $url to file in $unpack"
+                        curl -sSL $url -o $tarball
+                    fi
+
+                    echo "Extracting $tarball to Nix store closure in $HOME"
+                    < $tarball bzcat | tar -x -C $HOME
+
+                    echo "Removing $tarball"
+                    rm -f $tarball
+                fi
+
+                sudo mkdir -m 0755 /nix/
+                sudo chown $USER /nix/
+
+                mkdir /nix/store/
+                echo -n "Copying unpacked closure to nix store"
+                for i in $(cd $unpack/store/ > /dev/null && echo *); do
+                    echo -n "."
+                    i_tmp="/nix/store/$i.$$"
+                    if [ -e "$i_tmp" ]; then
+                        rm -rf "$i_tmp"
+                    fi
+                    if ! [ -e "/nix/store/$i" ]; then
+                        cp -Rp "$unpack/store/$i" "$i_tmp"
+                        chmod -R a-w "$i_tmp"
+                        chmod +w "$i_tmp"
+                        mv "$i_tmp" "/nix/store/$i"
+                        chmod -w "/nix/store/$i"
+                    fi
+                done
+                echo
+
+                echo "Initialising the database"
+                $nix/bin/nix-store --init
+
+                echo "Loading the database from the unpacked closure"
+                $nix/bin/nix-store --load-db < $unpack/.reginfo
             fi
             ;;
 
         verify)
             echo "=== Verifying that nixpkgs evaluates..."
 
-            nix-env --file $TRAVIS_BUILD_DIR --query --available --json > /dev/null
+            $nix/bin/nix-env --file $TRAVIS_BUILD_DIR --query --available --json > /dev/null
             ;;
 
         check)
             echo "=== Checking NixOS options"
 
-            nix-build $TRAVIS_BUILD_DIR/nixos/release.nix --attr options --show-trace
+            $nix/bin/nix-build $TRAVIS_BUILD_DIR/nixos/release.nix --attr options --show-trace
             ;;
 
 	tarball)
             echo "=== Checking tarball creation"
 
-            nix-build $TRAVIS_BUILD_DIR/pkgs/top-level/release.nix --attr tarball --show-trace
+            $nix/bin/nix-build $TRAVIS_BUILD_DIR/pkgs/top-level/release.nix --attr tarball --show-trace
             ;;
 
         pr)
@@ -57,7 +116,7 @@ while test -n "$1"; do
             else
                 echo "=== Building pull request #$TRAVIS_PULL_REQUEST"
 
-                nix-shell --packages nox git --run "nox-review pr $TRAVIS_PULL_REQUEST" -I nixpkgs=$TRAVIS_BUILD_DIR
+                $nix/bin/nix-shell --packages nox git nix --run "nox-review pr $TRAVIS_PULL_REQUEST" -I nixpkgs=$TRAVIS_BUILD_DIR
             fi
             ;;
 
