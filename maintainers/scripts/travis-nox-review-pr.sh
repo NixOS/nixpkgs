95b28da35365c51ff1673baca9469b7f74a5bcf0
Matthew Bauer <mjbauer95@gmail.com>
2016-06-30 21:23:55 +0000

    travis: use community supported language
    Switch to the "community supported" Nix version of Travis.

--- maintainers/scripts/travis-nox-review-pr.sh
+++ maintainers/scripts/travis-nox-review-pr.sh
@@ -1,37 +1,12 @@
 #! /usr/bin/env bash
 set -e
 
-# This should make Curl silent
-# but download-from-binary-cache doesn't respect
-export NIX_CURL_FLAGS=-sS
-
-if [ -d $HOME/.nix-profile ]; then
-    source $HOME/.nix-profile/etc/profile.d/nix.sh
-fi
-
 while test -n "$1"; do
 
     # tell Travis to use folding
     echo -en "travis_fold:start:$1\r"
 
     case $1 in
-
-        install)
-            echo "=== Installing Nix..."
-
-            curl -sS https://nixos.org/nix/install | sh
-
-            # Make sure we can use hydra's binary cache
-            sudo mkdir /etc/nix
-            echo "build-max-jobs = 4" | sudo tee /etc/nix/nix.conf > /dev/null
-
-            # Make sure we can execute within /tmp in Linux
-            if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-                sudo mount -o remount,exec /run
-                sudo mount -o remount,exec /run/user
-                sudo mount > /dev/null
-            fi
-            ;;
 
         verify)
             echo "=== Verifying that nixpkgs evaluates..."
