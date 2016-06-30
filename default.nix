3cec655386c15e6346bdc0bfaf991a51f4309f9c
Eric Sagnes <eric.sagnes@gmail.com>
2016-06-30 23:20:48 +0900

    default.nix: change upgrade instructions link

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let requiredVersion = import ./lib/minver.nix; in
 
 if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
 
-  abort "This version of Nixpkgs requires Nix >= ${requiredVersion}, please upgrade! See https://nixos.org/wiki/How_to_update_when_Nix_is_too_old_to_evaluate_Nixpkgs"
+  abort "This version of Nixpkgs requires Nix >= ${requiredVersion}, please upgrade! See https://nixos.org/nixpkgs/manual/#upgrading-nix"
 
 else
 
