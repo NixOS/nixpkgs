d107e2c44fc2da4156ac5521ea4bb46cd633a504
zimbatm <zimbatm@zimbatm.com>
2016-06-30 14:09:08 +0100

    opencl-icd: refactor a bit
    Add missing meta attributes

--- pkgs/development/libraries/opencl-icd/default.nix
+++ pkgs/development/libraries/opencl-icd/default.nix
@@ -1,15 +1,23 @@
-{ stdenv, fetchurl, ruby, opencl-headers }: let
+{ stdenv, fetchurl, ruby, opencl-headers }:
 
+stdenv.mkDerivation rec {
+  name = "opencl-icd-${version}";
   version = "2.2.9";
 
-in stdenv.mkDerivation {
-
-  name = "opencl-icd-${version}";
-  buildInputs = [ ruby opencl-headers ];
-  configureFlags = [ "--enable-official-khronos-headers" ];
   src = fetchurl {
     url = "https://forge.imag.fr/frs/download.php/716/ocl-icd-${version}.tar.gz";
     sha256 = "1rgaixwnxmrq2aq4kcdvs0yx7i6krakarya9vqs7qwsv5hzc32hc";
   };
 
+  buildInputs = [ ruby opencl-headers ];
+
+  configureFlags = [ "--enable-official-khronos-headers" ];
+
+  meta = with stdenv.lib; {
+    description = "This free ICD Loader can load any (free or non free) ICD";
+    homepage = https://forge.imag.fr/projects/ocl-icd/;
+    license = licenses.bsd2;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ zimbatm ];
+  };
 }
