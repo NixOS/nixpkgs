84b41861e4f03ed4f4a9d1581aaa50e8e646eaa3
Bjørn Forsman <bjorn.forsman@gmail.com>
2016-07-01 21:18:25 +0200

    pitivi: 0.95 -> 0.96 (fixes startup error)
    The upgrade of gtk3 to v3.20 broke pitivi, causing it to segfault after
    the user closes the Welcome dialog. This release fixes that.
    
    New dependencies: gst-transcoder and dbus.
    (pitivi imports dbus if it finds the GNOME_DESKTOP_SESSION_ID
    environment variable. Without dbus there will be some ugly errors for
    GNOME desktop users.)

--- pkgs/applications/video/pitivi/default.nix
+++ pkgs/applications/video/pitivi/default.nix
@@ -1,16 +1,33 @@
 { stdenv, fetchurl, pkgconfig, intltool, itstool, makeWrapper
 , python3Packages, gst, gtk3, hicolor_icon_theme
 , gobjectIntrospection, librsvg, gnome3, libnotify
+# for gst-transcoder:
+, which, meson, ninja
 }:
 
 let
-  version = "0.95";
+  version = "0.96";
+
+  # gst-transcoder will eventually be merged with gstreamer (according to
+  # gst-transcoder 1.8.0 release notes). For now the only user is pitivi so we
+  # don't bother exposing the package to all of nixpkgs.
+  gst-transcoder = stdenv.mkDerivation rec {
+    name = "gst-transcoder-1.8.0";
+    src = fetchurl {
+      name = "${name}.tar.gz";
+      url = "https://github.com/pitivi/gst-transcoder/archive/1.8.0.tar.gz";
+      sha256 = "0iggr6idmp7cmfsf6pkhfl3jq1bkga37jl5prbcl1zapkzi26fg6";
+    };
+    buildInputs = [ which meson ninja pkgconfig gobjectIntrospection ]
+      ++ (with gst; [ gstreamer gst-plugins-base ]);
+  };
+
 in stdenv.mkDerivation rec {
   name = "pitivi-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/pitivi/${version}/${name}.tar.xz";
-    sha256 = "04ykw619aikhxk5wj7z44pvwl52053d1kamcxpscw0ixrh5j45az";
+    sha256 = "115d37mvi32yds8gqj2yidkk6pap7szavhjf2hw0388ynydlc2zs";
   };
 
   meta = with stdenv.lib; {
@@ -31,12 +48,14 @@ in stdenv.mkDerivation rec {
     gobjectIntrospection gtk3 librsvg gnome3.gnome_desktop
     gnome3.defaultIconTheme
     gnome3.gsettings_desktop_schemas libnotify
+    gst-transcoder
   ] ++ (with gst; [
     gstreamer gst-editing-services
     gst-plugins-base gst-plugins-good
     gst-plugins-bad gst-plugins-ugly gst-libav gst-validate
   ]) ++ (with python3Packages; [
     python pygobject3 gst-python pyxdg numpy pycairo sqlite3 matplotlib
+    dbus
   ]);
 
   preFixup = ''
