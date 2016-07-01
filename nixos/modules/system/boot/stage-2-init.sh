12a23b3d913573d6d7dbd202e00e4c6a1102d6fc
Parnell Springmeyer <parnell@awakenetworks.com>
2016-06-30 19:22:19 -0500

    boot: create setcap-wrappers dir as a tmpfs

--- nixos/modules/system/boot/stage-2-init.sh
+++ nixos/modules/system/boot/stage-2-init.sh
@@ -141,6 +141,11 @@ if [ -n "@useHostResolvConf@" -a -e /etc/resolv.conf ]; then
     cat /etc/resolv.conf | resolvconf -m 1000 -a host
 fi
 
+# Create /var/permissions-wrappers as a tmpfs.
+rm -rf /var/permissions-wrappers
+mkdir -m 0755 -p /var/permissions-wrappers
+mount -t tmpfs -o "mode=0755" tmpfs /var/permissions-wrappers
+
 # Log the script output to /dev/kmsg or /run/log/stage-2-init.log.
 # Only at this point are all the necessary prerequisites ready for these commands.
 exec {logOutFd}>&1 {logErrFd}>&2
