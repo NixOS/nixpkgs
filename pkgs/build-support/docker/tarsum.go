b9284e445b3d27532c3493eb0fc09822b949e817
Nathan Zadoks <nathan@nathan7.eu>
2016-07-02 02:21:20 -0400

    dockerTools: disable compression in tarsum.go
    Previously, tarsum would compress the (discarded) tarball produced.
    That's a waste of CPU, and a waste of time.

--- pkgs/build-support/docker/tarsum.go
+++ pkgs/build-support/docker/tarsum.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	ts, err := tarsum.NewTarSum(os.Stdin, false, tarsum.Version1)
+	ts, err := tarsum.NewTarSum(os.Stdin, true, tarsum.Version1)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
