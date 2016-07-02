0e5c802120aee2f7c91e4dcac8a8fc32637f2516
Lengyel Balázs <ikervagyok@gmail.com>
2016-07-02 12:32:49 +0200

    [WIP] flashtool: 0.9.14.0 -> 0.9.23.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1529,7 +1529,7 @@ in
 
   flamerobin = callPackage ../applications/misc/flamerobin { };
 
-  flashtool = callPackage_i686 ../development/mobile/flashtool {
+  flashtool = callPackage ../development/mobile/flashtool {
     platformTools = androidenv.platformTools;
   };
 
