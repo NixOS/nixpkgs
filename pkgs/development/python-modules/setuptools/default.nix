6317da40006f6bc2480c6781999c52d88dde2acf
Frederik Rietdijk <fridh@fridh.nl>
2016-07-02 23:43:51 +0200

    pythonPackages.setuptools: fix for use in buildEnv
    pythonPath needs to be set for it to be included in buildEnv.
    This will also fix tox.
    
    (cherry picked from commit def310df459f649a4ce2df8b7ba19aa8e4d5af58)

--- pkgs/development/python-modules/setuptools/default.nix
+++ pkgs/development/python-modules/setuptools/default.nix
@@ -21,6 +21,8 @@ stdenv.mkDerivation rec {
       wrapPythonPrograms
   '';
 
+  pythonPath = [];
+
   meta = with stdenv.lib; {
     description = "Utilities to facilitate the installation of Python packages";
     homepage = http://pypi.python.org/pypi/setuptools;
