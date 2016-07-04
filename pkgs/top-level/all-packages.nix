3f5d7c7e04aba62c14e78133465814925ba4883f
Joachim Fasting <joachifm@fastmail.fm>
2016-07-05 00:51:33 +0200

    geis: refactor & fix build
    - Pass -Wno-error=pedantic to work around ISO C violations
    - Build with X11 unconditionally (also rely on pkgconfig for
      detection, --enable-x11 does not work)
    - Build against evemu

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6290,7 +6290,9 @@ in
 
   m4 = gnum4;
 
-  geis = callPackage ../development/libraries/geis { };
+  geis = callPackage ../development/libraries/geis {
+    inherit (xorg) libX11 libXext libXi libXtst;
+  };
 
   global = callPackage ../development/tools/misc/global { };
 
