01d9081cc348228659410aadb6052df53d10902e
Rahul Gopinath <rahul@gopinath.org>
2016-07-04 19:29:58 -0700

    gnurl: enable additional functionalities

--- pkgs/development/libraries/libgnurl/default.nix
+++ pkgs/development/libraries/libgnurl/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, perl }:
+{ stdenv, fetchurl, perl, zlib, gnutls, gss, openssl, libssh2, libidn, libpsl, openldap }:
 
 stdenv.mkDerivation rec {
   version = "7.48.0";
@@ -10,11 +10,21 @@ stdenv.mkDerivation rec {
     sha256 = "14gch4rdibrc8qs4mijsczxvl45dsclf234g17dk6c8nc2s4bm0a";
   };
 
-  buildInputs = [ perl ];
+  buildInputs = [ perl gnutls gss openssl zlib libidn libssh2 libpsl openldap ];
 
   preConfigure = ''
     sed -e 's|/usr/bin|/no-such-path|g' -i.bak configure
   '';
+
+  configureFlags = [
+    "--with-zlib"
+    "--with-gssapi"
+    "--with-libssh2"
+    "--with-libidn"
+    "--with-libpsl"
+    "--enable-ldap"
+    "--enable-ldaps"
+  ];
 
   meta = with stdenv.lib; {
     description = "A fork of libcurl used by GNUnet";
