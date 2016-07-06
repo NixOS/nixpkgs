e9c2acb852ac9e22c2ae388dce24d8a9a41ccc87
Ram Kromberg <ramkromberg@mail.com>
2016-07-06 19:53:28 +0300

    geany-with-vte: fix desktop shortcut and man pages

--- pkgs/applications/editors/geany/with-vte.nix
+++ pkgs/applications/editors/geany/with-vte.nix
@@ -2,5 +2,7 @@
 let name = builtins.replaceStrings ["geany-"] ["geany-with-vte-"] geany.name;
 in
 runCommand "${name}" { nativeBuildInputs = [ makeWrapper ]; } "
+   mkdir -p $out
+   ln -s ${geany}/share $out
    makeWrapper ${geany}/bin/geany $out/bin/geany --prefix LD_LIBRARY_PATH : ${gnome.vte}/lib
 "
