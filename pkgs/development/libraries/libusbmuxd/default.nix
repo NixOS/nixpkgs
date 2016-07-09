ef6c951d70a5bcd32a18f4f08e6ef5b8cf93cb56
Franz Pletz <fpletz@fnordicwalking.de>
2016-07-10 01:49:22 +0200

    libusbmuxd, libimobiledevice: Fix CVE-2016-5104
    (cherry picked from commit 77f2fff41d636ae9f7c599f55abdf006a2180066)

--- pkgs/development/libraries/libusbmuxd/default.nix
+++ pkgs/development/libraries/libusbmuxd/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, libplist }:
+{ stdenv, fetchurl, fetchpatch, pkgconfig, libplist }:
 
 stdenv.mkDerivation rec {
   name = "libusbmuxd-1.0.10";
@@ -6,6 +6,13 @@ stdenv.mkDerivation rec {
     url = "http://www.libimobiledevice.org/downloads/${name}.tar.bz2";
     sha256 = "1wn9zq2224786mdr12c5hxad643d29wg4z6b7jn888jx4s8i78hs";
   };
+
+  patches = [
+    (fetchpatch { # CVE-2016-5104
+      url = "https://github.com/libimobiledevice/libusbmuxd/commit/4397b3376dc4e4cb1c991d0aed61ce6482614196.patch";
+      sha256 = "0cl3vys7bkwbdzf64d0rz3zlqpfc30w4l7j49ljv01agh42ywhgk";
+    })
+  ];
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ libplist ];
