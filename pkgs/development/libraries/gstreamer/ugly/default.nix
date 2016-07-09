9f629280c631be9a861b71e1a924907756d4ec0a
Vladimír Čunát <vcunat@gmail.com>
2016-07-09 19:19:41 +0200

    gst-*: maintenance 1.8.1 -> 1.8.2
    For now I left *-vaapi out, as the jump would be larger,
    simple update isn't enough, and it's unreferenced in nixpkgs.

--- pkgs/development/libraries/gstreamer/ugly/default.nix
+++ pkgs/development/libraries/gstreamer/ugly/default.nix
@@ -5,7 +5,7 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "gst-plugins-ugly-1.8.1";
+  name = "gst-plugins-ugly-1.8.2";
 
   meta = with stdenv.lib; {
     description = "Gstreamer Ugly Plugins";
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "${meta.homepage}/src/gst-plugins-ugly/${name}.tar.xz";
-    sha256 = "1kj6jijhwdknv362mcnhjm7zbcbhs0i2m3pvsdz7w3g67fd6lrcf";
+    sha256 = "9c5b33a2a98fc1d6d6c99a1b536b1fb2de45f53cc8bf8ab85a8b8141fed1a8ac";
   };
 
   outputs = [ "dev" "out" ];
