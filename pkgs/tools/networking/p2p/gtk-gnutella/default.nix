f854c7d4f4bdbae7ba182955e453070f5343678e
Kranium Gikos Mendoza <kranium@gikos.net>
2016-07-10 10:33:31 +0800

    gtk-gnutella: 1.1.5 -> 1.1.9

--- pkgs/tools/networking/p2p/gtk-gnutella/default.nix
+++ pkgs/tools/networking/p2p/gtk-gnutella/default.nix
@@ -1,30 +1,26 @@
 { stdenv, fetchurl, bison, pkgconfig
-, glib, gtk, libxml2, gettext, zlib }:
+, glib, gtk, libxml2, gettext, zlib, binutils, gnutls }:
 
 let
   name = "gtk-gnutella";
-  version = "1.1.5";
+  version = "1.1.9";
 in
 stdenv.mkDerivation {
   name = "${name}-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/${name}/${name}-${version}.tar.bz2";
-    sha256 = "19d8mmyxrdwdafcjq1hvs9zn40yrcj1127163a2058svi0x08cn3";
+    sha256 = "1zvadgsskmpm82id9mbj24a2lyq38qv768ixv7nmfjl3d4wr2biv";
   };
 
-  nativeBuildInputs = [ bison pkgconfig ];
-  buildInputs = [ glib gtk libxml2 gettext zlib ];
+  nativeBuildInputs = [ bison binutils gettext pkgconfig ];
+  buildInputs = [ glib gnutls gtk libxml2 zlib ];
 
-  NIX_LDFLAGS = "-rpath ${zlib.out}/lib";
-  configureScript = "./Configure";
-  dontAddPrefix = true;
-  configureFlags = "-d -e -D prefix=$out -D gtkversion=2 -D official=true";
+  configureScript = "./build.sh --configure-only";
 
   meta = with stdenv.lib; {
     homepage = http://gtk-gnutella.sourceforge.net/;
     description = "Server/client for Gnutella";
     license = licenses.gpl2;
-    broken = true;
   };
 }
