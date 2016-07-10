ec5ec3141105b6ca26bc32342c2273fc12b0fd52
Ram Kromberg <ramkromberg@mail.com>
2016-07-11 00:20:03 +0300

    pngpp: init at 0.2.9 (#16782)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3075,6 +3075,8 @@ in
     libpng = libpng12;
   };
 
+  pngpp = callPackage ../development/libraries/png++ { };
+
   pngquant = callPackage ../tools/graphics/pngquant { };
 
   podiff = callPackage ../tools/text/podiff { };
