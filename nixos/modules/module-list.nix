d5f0e6d20b34ccec0793ae3be73a5b292e9ce8c6
Damien Cassou <damien@cassou.me>
2016-07-10 19:40:57 +0200

    offlineimap: add nixos module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -366,6 +366,7 @@
   ./services/networking/ntopng.nix
   ./services/networking/ntpd.nix
   ./services/networking/nylon.nix
+  ./services/networking/offlineimap.nix
   ./services/networking/oidentd.nix
   ./services/networking/openfire.nix
   ./services/networking/openntpd.nix
