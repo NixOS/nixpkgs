71b80c7d5cfc634b607b44616ce1c8b6d92b78cd
Evan Danaher <edanaher@edanaher.net>
2016-07-11 11:41:01 -0400

    Use autoreconf, which minor hackery around ChangeLog.

--- pkgs/applications/networking/instant-messengers/pidgin-plugins/pidgin-osd/default.nix
+++ pkgs/applications/networking/instant-messengers/pidgin-plugins/pidgin-osd/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, pidgin, xosd
-, autoconf, automake, libtool } :
+, autoreconfHook } :
 
 stdenv.mkDerivation rec {
   name = "pidgin-osd-0.1.0";
@@ -8,16 +8,19 @@ stdenv.mkDerivation rec {
     sha256 = "11hqfifhxa9gijbnp9kq85k37hvr36spdd79cj9bkkvw4kyrdp3j";
   };
 
-  preConfigure = "./autogen.sh";
-
   makeFlags = "PIDGIN_LIBDIR=$(out)";
+
+  # autoreconf is run such that it *really* wants all the files, and there's no
+  # default ChangeLog.  So make it happy.
+  preAutoreconf = "touch ChangeLog";
 
   postInstall = ''
     mkdir -p $out/lib/pidgin
     ln -s $out/pidgin $out/lib/pidgin
   '';
 
-  buildInputs = [ xosd pidgin autoconf automake libtool ];
+  nativeBuildInputs = [ autoreconfHook ];
+  buildInputs = [ xosd pidgin ];
 
   meta = with stdenv.lib; {
     homepage = https://github.com/mbroemme/pidgin-osd;
