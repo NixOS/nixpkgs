a265ad28425d5ffe925a92c3d7a51d9a0ac5c286
zimbatm <zimbatm@zimbatm.com>
2016-07-11 12:40:58 +0100

    firefox-developer-bin: fix hashes (#16860)
    the nightly tend to get replaced in-place on the FTP and so benefit from
    an update script as well

---
+++ pkgs/applications/networking/browsers/firefox-bin/dev_sources.nix
@@ -0,0 +1,12 @@
+# This file is generated from generate_sources_dev.rb. DO NOT EDIT.
+# Execute the following command to update the file.
+#
+# ruby generate_sources_dev.rb 49.0a2 > dev_sources.nix
+
+{
+  version = "49.0a2";
+  sources = [
+    { locale = "en-US"; arch = "linux-i686"; sha512 = "85c4289e561d2246f96a05e3b8df011337984b9f176670826a705c2cd68a1284056ba507e4b6e4887595bf37f25386d9f7b28a20bc1f125865b9fd7b8be17eaa"; }
+    { locale = "en-US"; arch = "linux-x86_64"; sha512 = "2bf9518dbfbb48348f74929c19d03e8daf51020bf9ba6db577a202b6e98ad7ffb9e9a0b4ca92af010cd3f864ae84940b65438f4230e6de3165f72e4e7280086d"; }
+  ];
+}
