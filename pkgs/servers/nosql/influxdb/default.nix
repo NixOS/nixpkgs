ed36293c18114d9a69fa7690de56aca8b8f2cdc1
Christine Koppelt <cko@users.noreply.github.com>
2016-07-12 00:09:28 +0200

    influxdb: parametrize default.nix to prepare packaging of 1.0

--- pkgs/servers/nosql/influxdb/default.nix
+++ pkgs/servers/nosql/influxdb/default.nix
@@ -1,19 +1,13 @@
-{ lib, buildGoPackage, fetchFromGitHub }:
+{ lib, buildGoPackage, fetchFromGitHub, src, version }:
 
 buildGoPackage rec {
   name = "influxdb-${version}";
-  version = "0.13.0";
 
   goPackagePath = "github.com/influxdata/influxdb";
 
-  src = fetchFromGitHub {
-    owner = "influxdata";
-    repo = "influxdb";
-    rev = "v${version}";
-    sha256 = "0f7af5jb1f65qnslhc7zccml1qvk6xx5naczqfsf4s1zc556fdi4";
-  };
-
   excludedPackages = "test";
+  
+  inherit src;
 
   # Generated with the `gdm2nix.rb` script and the `Godeps` file from the
   # influxdb repo root.
