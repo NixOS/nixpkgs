acfbf1c7d65eb0508a2ce9732add8dc997de442f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-07-12 17:09:49 +0200

    Add AMIs in ap-northeast-2 and ap-south-1
    (cherry picked from commit 94cc18e9aaacf705afc61e8f9836fa8f7a1cdf51)

--- nixos/maintainers/scripts/ec2/create-amis.sh
+++ nixos/maintainers/scripts/ec2/create-amis.sh
@@ -15,7 +15,7 @@ rm -f ec2-amis.nix
 
 types="hvm pv"
 stores="ebs s3"
-regions="eu-west-1 eu-central-1 us-east-1 us-west-1 us-west-2 ap-southeast-1 ap-southeast-2 ap-northeast-1 sa-east-1"
+regions="eu-west-1 eu-central-1 us-east-1 us-west-1 us-west-2 ap-southeast-1 ap-southeast-2 ap-northeast-1 ap-northeast-2 sa-east-1 ap-south-1"
 
 for type in $types; do
     link=$stateDir/$type
