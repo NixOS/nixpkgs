4b8c31d981c0bf35c860bf44df04bcdbb5865685
Luca Bruno <lucabru@src.gnome.org>
2016-07-14 22:05:15 +0100

    gnome3: enable X libinput by default
    See https://bugzilla.gnome.org/show_bug.cgi?id=764257#c12

--- nixos/modules/services/x11/desktop-managers/gnome3.nix
+++ nixos/modules/services/x11/desktop-managers/gnome3.nix
@@ -122,6 +122,7 @@ in {
     services.dbus.packages = mkIf config.services.printing.enable [ pkgs.system-config-printer ];
     services.colord.enable = mkDefault true;
     hardware.bluetooth.enable = mkDefault true;
+    services.xserver.libinput.enable = mkDefault true; # for controlling touchpad settings via gnome control center
 
     fonts.fonts = [ pkgs.dejavu_fonts pkgs.cantarell_fonts ];
 
