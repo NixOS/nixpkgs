72731706d5045ed851118a1a84236d924e2890ad
Franz Pletz <fpletz@fnordicwalking.de>
2016-07-15 15:26:31 +0200

    grafana: 3.0.1 -> 3.1.0

--- nixos/modules/services/monitoring/grafana.nix
+++ nixos/modules/services/monitoring/grafana.nix
@@ -9,6 +9,7 @@ let
 
   envOptions = {
     PATHS_DATA = cfg.dataDir;
+    PATHS_PLUGINS = "${cfg.dataDir}/plugins";
     PATHS_LOGS = "${cfg.dataDir}/log";
 
     SERVER_PROTOCOL = cfg.protocol;
@@ -37,6 +38,8 @@ let
     USERS_AUTO_ASSIGN_ORG_ROLE = cfg.users.autoAssignOrgRole;
 
     AUTH_ANONYMOUS_ENABLED = b2s cfg.auth.anonymous.enable;
+    AUTH_ANONYMOUS_ORG_NAME = cfg.auth.anonymous.org_name;
+    AUTH_ANONYMOUS_ORG_ROLE = cfg.auth.anonymous.org_role;
 
     ANALYTICS_REPORTING_ENABLED = b2s cfg.analytics.reporting.enable;
   } // cfg.extraOptions;
@@ -196,6 +199,17 @@ in {
         default = false;
         type = types.bool;
       };
+      org_name = mkOption {
+        description = "Which organization to allow anonymous access to";
+        default = "Main Org.";
+        type = types.str;
+      };
+      org_role = mkOption {
+        description = "Which role anonymous users have in the organization";
+        default = "Viewer";
+        type = types.str;
+      };
+
     };
 
     analytics.reporting = {
@@ -222,6 +236,8 @@ in {
       "Grafana passwords will be stored as plaintext in the Nix store!"
     ];
 
+    environment.systemPackages = [ cfg.package ];
+
     systemd.services.grafana = {
       description = "Grafana Service Daemon";
       wantedBy = ["multi-user.target"];
@@ -234,6 +250,7 @@ in {
       };
       preStart = ''
         ln -fs ${cfg.package}/share/grafana/conf ${cfg.dataDir}
+        ln -fs ${cfg.package}/share/grafana/vendor ${cfg.dataDir}
       '';
     };
 
