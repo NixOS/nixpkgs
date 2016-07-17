47f59a51e4e1d276f78f79e5b7cd668bc10f050e
Michele Guerini Rocco <rnhmjoj@users.noreply.github.com>
2016-07-17 02:25:38 +0200

    nixos/compton: add user service (#16652)

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -477,6 +477,7 @@
   ./services/web-servers/winstone.nix
   ./services/web-servers/zope2.nix
   ./services/x11/colord.nix
+  ./services/x11/compton.nix
   ./services/x11/unclutter.nix
   ./services/x11/desktop-managers/default.nix
   ./services/x11/display-managers/auto.nix
