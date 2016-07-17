fd95f8f824110d5a757414d457497da675979d4b
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2016-07-17 12:00:17 -0400

    herwig: init a 7.0.3
    thepeg: init at 2.0.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16719,9 +16719,13 @@ in
 
   hepmc = callPackage ../development/libraries/physics/hepmc { };
 
+  herwig = callPackage ../development/libraries/physics/herwig { };
+
   lhapdf = callPackage ../development/libraries/physics/lhapdf { };
 
   rivet = callPackage ../development/libraries/physics/rivet { };
+
+  thepeg = callPackage ../development/libraries/physics/thepeg { };
 
   yoda = callPackage ../development/libraries/physics/yoda { };
 
