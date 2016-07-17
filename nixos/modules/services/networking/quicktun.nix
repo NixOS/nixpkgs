54bf5cc1fa9de7ed6a8e80738cd30a1961b2bdc1
Franz Pletz <fpletz@fnordicwalking.de>
2016-07-17 17:00:18 +0200

    nixos/quicktun: Add support for up script

--- nixos/modules/services/networking/quicktun.nix
+++ nixos/modules/services/networking/quicktun.nix
@@ -76,6 +76,12 @@ with lib;
             default = 5;
             description = "";
           };
+
+          upScript = mkOption {
+            type = types.lines;
+            default = "";
+            description = "";
+          };
         };
       });
     };
@@ -98,6 +104,7 @@ with lib;
             "PRIVATE_KEY" = qtcfg.privateKey;
             "PUBLIC_KEY" = qtcfg.publicKey;
             "TIME_WINDOW" = toString qtcfg.timeWindow;
+            "TUN_UP_SCRIPT" = pkgs.writeScript "quicktun-${name}-up.sh" qtcfg.upScript;
             "SUID" = "nobody";
           };
           serviceConfig = {
