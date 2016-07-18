60d32ff6cca3a19d1fb1565b07d948c3de45779e
Alexey Shmalko <rasen.dubi@gmail.com>
2016-07-19 02:36:33 +0300

    planner: init at 0.14.6 (#17078)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13869,6 +13869,8 @@ in
 
   pijul = callPackage ../applications/version-management/pijul { };
 
+  planner = callPackage ../applications/office/planner { };
+
   playonlinux = callPackage ../applications/misc/playonlinux {
      stdenv = stdenv_32bit;
   };
