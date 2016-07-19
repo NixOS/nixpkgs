47e66f08e12aba48a7bc1e76fd9010bde0371e9e
Frederik Rietdijk <fridh@fridh.nl>
2016-07-19 12:07:47 +0200

    netcdf: remove zlib
    since its propagated now by hdf5

--- pkgs/development/libraries/netcdf/default.nix
+++ pkgs/development/libraries/netcdf/default.nix
@@ -1,6 +1,5 @@
 { stdenv
 , fetchurl
-, zlib
 , hdf5
 , m4
 , curl # for DAP
@@ -16,7 +15,7 @@ in stdenv.mkDerivation rec {
         sha256 = "06ds8zm4qvjlqvv4qb637cqr0xgvbhnghrddisad5vj81s5kvpmx";
     };
 
-    buildInputs = [ hdf5 zlib m4 curl mpi];
+    buildInputs = [ hdf5 m4 curl mpi];
 
     passthru = {
       mpiSupport = mpiSupport;
