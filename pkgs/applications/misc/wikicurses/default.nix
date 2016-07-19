7081bbfc0a8a33a6ac2fcfdf541c43e7cc920b7f
Matthias Beyer <mail@beyermatthias.de>
2016-07-19 08:40:18 +0200

    fixup! wikicurses: init at 1.3

--- pkgs/applications/misc/wikicurses/default.nix
+++ pkgs/applications/misc/wikicurses/default.nix
@@ -14,7 +14,7 @@ pythonPackages.buildPythonApplication rec {
   meta = {
     description = "A simple curses interface for MediaWiki sites such as Wikipedia";
     homepage = "https://github.com/ids1024/wikicurses/";
-    license = stdenv.lib.licenses.mit; # not sure here
+    license = stdenv.lib.licenses.mit;
     platforms = stdenv.lib.platforms.unix;
   };
 
