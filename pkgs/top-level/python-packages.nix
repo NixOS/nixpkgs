848bf3099f969010b8daed1182e69ffbb5009e51
Dan Peebles <pumpkin@me.com>
2016-07-19 09:45:28 -0400

    pythonPackages.moto: init at 0.4.25

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13019,6 +13019,23 @@ in modules // {
     };
   };
 
+  moto = buildPythonPackage rec {
+    version = "0.4.25";
+    name    = "moto-${version}";
+    src = pkgs.fetchurl {
+      url    = "http://pypi.python.org/packages/df/9e/0b22ac0abf61711c86ae75a0548825e19cc123b522ff3508cbc43924969d/moto-0.4.25.tar.gz";
+      sha256 = "1gqm7h6bm5xkspd07bnjwdr2q6cvpxkayx0hkgk8fhkawbg0fqq7";
+    };
+
+    propagatedBuildInputs = with self; [
+      # Main dependencies
+      jinja2 werkzeug flask requests2 six boto httpretty xmltodict
+      # For tests
+      nose sure boto3 freezegun
+    ];
+  };
+
+
   mox = buildPythonPackage rec {
     name = "mox-0.5.3";
 
