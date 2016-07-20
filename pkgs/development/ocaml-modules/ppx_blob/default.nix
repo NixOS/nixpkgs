2df3fde700daf20a00188de616e677bc57c9794f
Ricardo M. Correia <rcorreia@wizy.org>
2016-07-21 01:05:51 +0200

    ocamlPackage.ppx_blob: init at 0.2 (#17129)
    OCaml ppx to include binary data from a file as a string.

---
+++ pkgs/development/ocaml-modules/ppx_blob/default.nix
@@ -0,0 +1,19 @@
+{ stdenv, buildOcaml, fetchurl, ppx_tools }:
+
+buildOcaml rec {
+  name = "ppx_blob";
+  version = "0.2";
+
+  src = fetchurl {
+    url = "https://github.com/johnwhitington/ppx_blob/archive/v${version}.tar.gz";
+    sha256 = "0kvqfm47f4xbgz0cl7ayz29myyb24xskm35svqrgakjq12nkpsss";
+  };
+
+  buildInputs = [ ppx_tools ];
+
+  meta = with stdenv.lib; {
+    homepage = "https://github.com/johnwhitington/ppx_blob";
+    description = "OCaml ppx to include binary data from a file as a string";
+    license = licenses.unlicense;
+  };
+}
