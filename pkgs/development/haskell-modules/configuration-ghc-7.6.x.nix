1545d241b617d4bd2de654f1984abbce48a1d9b1
Peter Simons <simons@cryp.to>
2016-07-20 22:06:01 +0200

    haskell-semigroups: fix build with old compilers

--- pkgs/development/haskell-modules/configuration-ghc-7.6.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-7.6.x.nix
@@ -97,6 +97,6 @@ self: super: {
   conduit = addBuildDepend super.conduit self.void;
 
   # Needs additional inputs on pre 7.10.x compilers.
-  semigroups = addBuildDepends super.semigroups (with self; [nats tagged unordered-containers transformers]);
+  semigroups = addBuildDepends super.semigroups (with self; [bytestring-builder nats tagged unordered-containers transformers]);
 
 }
