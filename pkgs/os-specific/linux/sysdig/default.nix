2a3fe4df43012ddc90b9841701e865441efa8cc6
Markus Hauck <markus1189@gmail.com>
2016-07-20 08:32:14 +0200

    sysdig: 0.10.0 -> 0.11.0

--- pkgs/os-specific/linux/sysdig/default.nix
+++ pkgs/os-specific/linux/sysdig/default.nix
@@ -1,22 +1,30 @@
-{stdenv, fetchurl, cmake, luajit, kernel, zlib, ncurses, perl, jsoncpp, libb64, openssl, curl}:
+{stdenv, fetchurl, fetchFromGitHub, cmake, luajit, kernel, zlib, ncurses, perl, jsoncpp, libb64, openssl, curl}:
 let
   inherit (stdenv.lib) optional optionalString;
   s = rec {
-    baseName="sysdig";
-    version = "0.10.0";
-    name="${baseName}-${version}";
-    url="https://github.com/draios/sysdig/archive/${version}.tar.gz";
-    sha256 = "0hs0r9z9j7padqdcj69bwx52iw6gvdl0w322qwivpv12j3prcpsj";
+    name = "sysdig-${version}";
+    version = "0.11.0";
+    owner = "draios";
+    repo = "sysdig";
+    rev = version;
+    sha256 = "131bafa7jy16r2jwph50j0bxwqdvr319fsfhqkavx6xy18i31q3v";
   };
   buildInputs = [
     cmake zlib luajit ncurses perl jsoncpp libb64 openssl curl
   ];
+  # sysdig-0.11.0 depends on some headers from jq which are not
+  # installed by default.
+  # Relevant sysdig issue: https://github.com/draios/sysdig/issues/626
+  jq-prefix = fetchurl {
+    url="https://github.com/stedolan/jq/releases/download/jq-1.5/jq-1.5.tar.gz";
+    sha256="0g29kyz4ykasdcrb0zmbrp2jqs9kv1wz9swx849i2d1ncknbzln4";
+  };
 in
 stdenv.mkDerivation {
   inherit (s) name version;
   inherit buildInputs;
-  src = fetchurl {
-    inherit (s) url sha256;
+  src = fetchFromGitHub {
+    inherit (s) owner repo rev sha256;
   };
   postPatch = ''
     sed '1i#include <cmath>' -i userspace/libsinsp/{cursesspectro,filterchecks}.cpp
@@ -24,11 +32,17 @@ stdenv.mkDerivation {
 
   cmakeFlags = [
     "-DUSE_BUNDLED_DEPS=OFF"
+    "-DUSE_BUNDLED_JQ=ON"
+    "-DSYSDIG_VERSION=${s.version}"
   ] ++ optional (kernel == null) "-DBUILD_DRIVER=OFF";
   preConfigure = ''
     export INSTALL_MOD_PATH="$out"
   '' + optionalString (kernel != null) ''
     export KERNELDIR="${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+  '';
+  preBuild = ''
+    mkdir -p jq-prefix/src
+    cp ${jq-prefix} jq-prefix/src/jq-1.5.tar.gz
   '';
   postInstall = optionalString (kernel != null) ''
     make install_driver
