781ed961eb47f869220a412c78bc32a163b5b5a5
Proglodyte <proglodyte23@gmail.com>
2016-07-23 13:17:13 -0400

    v8: use 4.5.107 by default
    Default v8 was fixed at 3.30.33, so running nix-build nixpkgs -A v8
    would give us version 3.30.33, whereas nix-env -i v8 would install
    version 4.5.107. Now, both commands build 4.5.107, and there is still an
    option to install version 3.30.33

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6400,9 +6400,9 @@ in
   gob2 = callPackage ../development/tools/misc/gob2 { };
 
   gocd-agent = callPackage ../development/tools/continuous-integration/gocd-agent { };
-  
+
   gocd-server = callPackage ../development/tools/continuous-integration/gocd-server { };
-  
+
   gotty = callPackage ../servers/gotty { };
 
   gradleGen = callPackage ../development/tools/build-managers/gradle { };
@@ -9531,7 +9531,7 @@ in
     inherit (pythonPackages) gyp;
   };
 
-  v8_4_5 = callPackage ../development/libraries/v8/4.5.nix {
+  v8_3_30_33 = callPackage ../development/libraries/v8/3.30.33.nix {
     inherit (pythonPackages) gyp;
   };
 
