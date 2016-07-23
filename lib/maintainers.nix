c26732021982650648c9c38819a149726caf4a12
Proglodyte <proglodyte23@gmail.com>
2016-07-23 03:41:14 -0400

    v8: fix 4.5.107 build
    Similar to #14272, but fixes 4.5 build rather than generic.
    - Ignores errors due to strict-overflow warnings
    - Strips clang-only '-Wno-format-pedantic' flag out since this build
      uses gcc
    
    (cherry picked from commit 16bd51fc0d7ddec1039ea7fe4a3f737f4b06b08b)
    
    Fixes https://hydra.nixos.org/build/37879354/nixlog/1/raw

--- lib/maintainers.nix
+++ lib/maintainers.nix
@@ -267,6 +267,7 @@
   pmiddend = "Philipp Middendorf <pmidden@secure.mailbox.org>";
   prikhi = "Pavan Rikhi <pavan.rikhi@gmail.com>";
   profpatsch = "Profpatsch <mail@profpatsch.de>";
+  proglodyte = "Proglodyte <proglodyte23@gmail.com>";
   psibi = "Sibi <sibi@psibi.in>";
   pSub = "Pascal Wittmann <mail@pascal-wittmann.de>";
   puffnfresh = "Brian McKenna <brian@brianmckenna.org>";
