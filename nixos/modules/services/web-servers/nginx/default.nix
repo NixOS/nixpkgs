8c61b3af0398b1ec93d2abb2b80fa97099155b1c
Tristan Helmich <tristan.helmich@gmail.com>
2016-07-26 18:11:16 +0200

    nginx: fixed duplicate http declaration

--- nixos/modules/services/web-servers/nginx/default.nix
+++ nixos/modules/services/web-servers/nginx/default.nix
@@ -16,6 +16,8 @@ let
     error_log stderr;
     daemon off;
 
+    ${cfg.config}
+
     http {
       include ${cfg.package}/conf/mime.types;
       include ${cfg.package}/conf/fastcgi.conf;
@@ -86,18 +88,10 @@ let
           }
         }
       ''}
-    }
 
-    ${cfg.config}
-
-    # Keep this seperate to allow overriding previous settings
-    ${optionalString (cfg.httpConfig != "") ''
-    http {
-      include ${cfg.package}/conf/mime.types;
-      include ${cfg.package}/conf/fastcgi.conf;
       ${cfg.httpConfig}
     }
-    ''}
+
     ${cfg.appendConfig}
   '';
 
