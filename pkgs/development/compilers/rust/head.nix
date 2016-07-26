0f25add68da6b4e3f61e5f568d7cd6d49479e67a
Moritz Ulrich <moritz@tarn-vedra.de>
2016-07-26 16:20:40 +0200

    rustUnstable.rustc: master-1.11.0 -> master-1.12.0
    Note that tests are failing for this commit.

--- pkgs/development/compilers/rust/head.nix
+++ pkgs/development/compilers/rust/head.nix
@@ -3,13 +3,14 @@
 
 rec {
   rustc = callPackage ./rustc.nix {
-    shortVersion = "master-1.11.0";
+    shortVersion = "master-1.12.0";
     forceBundledLLVM = false;
-    srcRev = "298730e7032cd55809423773da397cd5c7d827d4";
-    srcSha = "0hyz5j1z75sjkgsifzgxviv3b1lhgaz8wqwvmq80xx5vd78yd0c1";
-    patches = [ ./patches/disable-lockfile-check.patch
-                ./patches/use-rustc-1.9.0.patch ]
-      ++ stdenv.lib.optional stdenv.needsPax ./patches/grsec.patch;
+    needsCmake = true;
+    srcRev = "c77f8ce7c3284441a00faed6782d08eb5a78296c";
+    srcSha = "11y24bm2rj7bzsf86iyx3v286ygxprch4c804qbl1w477mkhcac7";
+    patches = [
+      ./patches/disable-lockfile-check.patch
+    ] ++ stdenv.lib.optional stdenv.needsPax ./patches/grsec.patch;
     inherit targets;
     inherit targetPatches;
     inherit targetToolchains;
@@ -17,10 +18,10 @@ rec {
   };
 
   cargo = callPackage ./cargo.nix rec {
-    version = "2016.06.07";
-    srcRev = "3e70312a2a4ebedace131fc63bb8f27463c5db28";
-    srcSha = "0nibzyfjkiqfnq0c00hhqvs856l5qls8wds252p97q5q92yvp40f";
-    depsSha256 = "1xbb33aqnf5yyws6gjys9w8kznbh9rh6hw8mpg1hhq1ahipc2j1f";
+    version = "master-0.13.0";
+    srcRev = "fb2faf29b26da39bd815b470ca73255dbfe30e42";
+    srcSha = "1r7wd3hp85mvmm7ivj01k65qcgb6qk1mys9mp48ww9k5cdniwcaj";
+    depsSha256 = "0lsf99pgl6wnl1lfk0drp5l4agrx7hzgdbps7hy3rprbf41jd6ai";
     inherit rustc; # the rustc that will be wrapped by cargo
     inherit rustPlatform; # used to build cargo
   };
