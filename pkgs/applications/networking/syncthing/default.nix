22bb834b59bd0124cf54f1430b88585d9e797e42
jokogr <ioannis.koutras@gmail.com>
2016-07-27 19:49:43 +0300

    syncthing: 0.14.0 -> 0.14.2 (#17274)
    * Update syncthing from 0.14.0 to 0.14.2
    * Use fetchFromGitHub instead of fetchgit
    * Add joko to package maintainers

--- pkgs/applications/networking/syncthing/default.nix
+++ pkgs/applications/networking/syncthing/default.nix
@@ -1,13 +1,14 @@
-{ stdenv, fetchgit, go }:
+{ stdenv, fetchFromGitHub, go }:
 
 stdenv.mkDerivation rec {
-  version = "0.14.0";
+  version = "0.14.2";
   name = "syncthing-${version}";
 
-  src = fetchgit {
-    url = https://github.com/syncthing/syncthing;
-    rev = "refs/tags/v${version}";
-    sha256 = "15l3q3r6i3q95i474winswx4y149b5ic7xhpnj52s78fxd4va2q2";
+  src = fetchFromGitHub {
+    owner = "syncthing";
+    repo = "syncthing";
+    rev = "v${version}";
+    sha256 = "0is4f1r3im2bbmbca9fafzxffikxaf86vd6f851831fk5wi4pzw9";
   };
 
   buildInputs = [ go ];
@@ -34,7 +35,7 @@ stdenv.mkDerivation rec {
     homepage = https://www.syncthing.net/;
     description = "Open Source Continuous File Synchronization";
     license = stdenv.lib.licenses.mpl20;
-    maintainers = with stdenv.lib.maintainers; [pshendry];
+    maintainers = with stdenv.lib.maintainers; [ pshendry joko ];
     platforms = with stdenv.lib.platforms; linux ++ freebsd ++ openbsd ++ netbsd;
   };
 }
