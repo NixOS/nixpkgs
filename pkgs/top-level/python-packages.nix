0868a918a373fafebdb2f2058d57da8fdeba1512
Frederik Rietdijk <fridh@fridh.nl>
2016-07-29 19:36:28 +0200

    pythonPackages.backports_abc: init at 0.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1638,6 +1638,26 @@ in modules // {
     };
   };
 
+  backports_abc = buildPythonPackage rec {
+    name = "backports_abc-${version}";
+    version = "0.4";
+
+    src = pkgs.fetchurl {
+      url = "mirror://pypi/b/backports_abc/${name}.tar.gz";
+      sha256 = "8b3e4092ba3d541c7a2f9b7d0d9c0275b21c6a01c53a61c731eba6686939d0a5";
+    };
+
+    checkPhase = ''
+      ${python.interpreter} -m unittest discover
+    '';
+
+    meta = {
+      homepage = https://github.com/cython/backports_abc;
+      license = licenses.psfl;
+      description = "A backport of recent additions to the 'collections.abc' module";
+    };
+  };
+
   backports_shutil_get_terminal_size = if !(pythonOlder "3.3") then null else buildPythonPackage rec {
     name = "backports.shutil_get_terminal_size-${version}";
     version = "1.0.0";
