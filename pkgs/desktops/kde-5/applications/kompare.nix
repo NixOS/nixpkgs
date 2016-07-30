4805d008e4333f0e9348ce52a8867352b47a2690
Thomas Tuegel <ttuegel@gmail.com>
2016-07-30 17:35:49 -0500

    kompare: use kdeWrapper

--- pkgs/desktops/kde-5/applications/kompare.nix
+++ pkgs/desktops/kde-5/applications/kompare.nix
@@ -1,19 +1,18 @@
 {
-  kdeApp, lib, ecm, kdoctools, makeQtWrapper,
+  kdeApp, lib, kdeWrapper,
+  ecm, kdoctools,
   kparts, ktexteditor, kwidgetsaddons, libkomparediff2
 }:
 
-kdeApp {
-  name = "kompare";
-  meta = {
-    license = with lib.licenses; [ gpl2 ];
-  };
-
-  nativeBuildInputs = [ ecm kdoctools makeQtWrapper ];
-
-  propagatedBuildInputs = [ kparts ktexteditor kwidgetsaddons libkomparediff2 ];
-
-  postInstall = ''
-    wrapQtProgram "$out/bin/kompare"
-  '';
-}
+let
+  unwrapped =
+    kdeApp {
+      name = "kompare";
+      meta = { license = with lib.licenses; [ gpl2 ]; };
+      nativeBuildInputs = [ ecm kdoctools ];
+      propagatedBuildInputs = [
+        kparts ktexteditor kwidgetsaddons libkomparediff2
+      ];
+    };
+in
+kdeWrapper unwrapped { targets = [ "bin/kompare" ]; }
