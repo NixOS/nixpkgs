a9a4ce112767ed2831c57a44de51d4453d89017e
Mohammed Yaseen Mowzer <yaseen@mowzer.co.za>
2016-07-31 14:23:05 +0200

    geogebra: init at 5.0.264.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16825,6 +16825,8 @@ in
 
   gap = callPackage ../applications/science/math/gap { };
 
+  geogebra = callPackage ../applications/science/math/geogebra {};
+
   maxima = callPackage ../applications/science/math/maxima { };
 
   wxmaxima = callPackage ../applications/science/math/wxmaxima { wxGTK = wxGTK30; };
