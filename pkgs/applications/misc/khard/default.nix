b714fdb5746f57c26a41ced7b8641ac298456fa0
Gabriel Ebner <gebner@gebner.org>
2016-07-31 10:14:13 +0200

    khard: 0.9.0 -> 0.11.1

--- pkgs/applications/misc/khard/default.nix
+++ pkgs/applications/misc/khard/default.nix
@@ -1,22 +1,29 @@
-{ stdenv, fetchurl, pkgs, python2Packages }:
+{ stdenv, fetchurl, glibcLocales, python3Packages }:
 
-python2Packages.buildPythonApplication rec {
-  version = "0.9.0";
+python3Packages.buildPythonApplication rec {
+  version = "0.11.1";
   name = "khard-${version}";
   namePrefix = "";
 
   src = fetchurl {
     url = "https://github.com/scheibler/khard/archive/v${version}.tar.gz";
-    sha256 = "1cj6rlvbk05cfjkl1lnyvq12sb847jjwqy5j8906p2b2x4wq72qi";
+    sha256 = "0055xx9icmsr6l7v0iqrndmygygdpdv10550w6pyrb96svzhry27";
   };
 
-  propagatedBuildInputs = with python2Packages; [
+  # setup.py reads the UTF-8 encoded readme.
+  LC_ALL = "en_US.UTF-8";
+  buildInputs = [ glibcLocales ];
+
+  propagatedBuildInputs = with python3Packages; [
     atomicwrites
     configobj
     vobject
     argparse
     pyyaml
   ];
+
+  # Fails; but there are no tests anyway.
+  doCheck = false;
 
   meta = {
     homepage = https://github.com/scheibler/khard;
