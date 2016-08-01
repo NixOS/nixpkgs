6811c592ab01b961d2c01c5cccbe1e8b359600e5
schneefux <schneefux+commit@schneefux.xyz>
2016-08-01 12:24:03 +0200

    znc: 1.6.3 -> 1.6.4-dev

--- pkgs/applications/networking/znc/default.nix
+++ pkgs/applications/networking/znc/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, openssl, pkgconfig
+{ stdenv, fetchgit, openssl, pkgconfig, autoreconfHook
 , withPerl ? false, perl
 , withPython ? false, python3
 , withTcl ? false, tcl
@@ -7,13 +7,16 @@
 
 with stdenv.lib;
 stdenv.mkDerivation rec {
-  name = "znc-1.6.3";
+  name = "znc-1.6.4-dev";
 
-  src = fetchurl {
-    url = "http://znc.in/releases/${name}.tar.gz";
-    sha256 = "09xqi5fs40x6nj9gq99bnw1a7saq96bvqxknxx0ilq7yfvg4c733";
+  src = fetchgit {
+    url = "https://github.com/znc/znc";
+    rev = "ddec5c27bacb934bf720fd9c0db89cc3fdcb2e29";
+    sha256 = "0bqib1ddn52ppbv008shql0xrg5185ynmabz548i4msydl5990jc";
+    fetchSubmodules = true;
   };
 
+  nativeBuildInputs = [ autoreconfHook ];
   buildInputs = [ openssl pkgconfig ]
     ++ optional withPerl perl
     ++ optional withPython python3
