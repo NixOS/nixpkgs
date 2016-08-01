3bc02f153f9417bc73593652a5ea51c72e1fe88a
obadz <obadz-git@obadz.com>
2016-08-01 11:57:45 +0100

    pokerth: init at 1.1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15836,6 +15836,10 @@ in
 
   pong3d = callPackage ../games/pong3d { };
 
+  pokerth = callPackage ../games/pokerth { };
+
+  pokerth-server = with callPackage ../games/pokerth { }; server;
+
   prboom = callPackage ../games/prboom { };
 
   privateer = callPackage ../games/privateer { };
