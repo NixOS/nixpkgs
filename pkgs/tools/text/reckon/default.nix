4599ac665b3c37ab617b316828c160721fa787a7
Kyle McKean <mckean.kylej@gmail.com>
2016-08-02 19:49:28 -0400

    move to cide like wrapper

--- pkgs/tools/text/reckon/default.nix
+++ pkgs/tools/text/reckon/default.nix
@@ -1,12 +1,26 @@
-{ lib, bundlerEnv, ruby }:
+{ stdenv, lib, bundlerEnv, makeWrapper }:
 
-bundlerEnv {
-  name = "reckon";
-  inherit ruby;
-  gemfile = ./Gemfile;
-  lockfile = ./Gemfile.lock;
-  gemset = ./gemset.nix;
- 
+stdenv.mkDerivation rec {
+  name = "reckon-${version}";
+  version = "0.4.4";
+
+  env = bundlerEnv {
+    name = "${name}-gems";
+
+    gemfile = ./Gemfile;
+    lockfile = ./Gemfile.lock;
+    gemset = ./gemset.nix;
+  };
+
+  phases = [ "installPhase" ];
+
+  buildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    makeWrapper ${env}/bin/reckon $out/bin/cide
+  '';
+
   meta = with lib; {
     description = "Flexibly import bank account CSV files into Ledger for command line accounting";
     license = licenses.mit;
