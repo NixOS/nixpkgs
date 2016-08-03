7ab971a25200041e959ba65eb87528e2b116f8b3
Robin Gloster <mail@glob.in>
2016-08-03 20:32:34 +0000

    scummvm: disable format hardening

--- pkgs/games/scummvm/default.nix
+++ pkgs/games/scummvm/default.nix
@@ -2,13 +2,15 @@
 
 stdenv.mkDerivation rec {
   name = "scummvm-1.8.0";
-  
+
   src = fetchurl {
     url = "mirror://sourceforge/scummvm/${name}.tar.bz2";
     sha256 = "0f3zgvz886lk9ps0v333aq74vx6grlx68hg14gfaxcvj55g73v01";
   };
-  
+
   buildInputs = [ SDL zlib libmpeg2 libmad libogg libvorbis flac alsaLib ];
+
+  hardeningDisable = [ "format" ];
 
   crossAttrs = {
     preConfigure = ''
