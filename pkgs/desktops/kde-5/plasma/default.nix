bed8eb86c611207c877d9ed7e42788e502da81d2
Thomas Tuegel <ttuegel@gmail.com>
2016-08-04 10:00:12 -0500

    kde5.breeze-plymouth: init at 5.7.3

--- pkgs/desktops/kde-5/plasma/default.nix
+++ pkgs/desktops/kde-5/plasma/default.nix
@@ -52,6 +52,7 @@ let
           name = "breeze-${version}";
           paths = map (pkg: pkg.out or pkg) [ breeze-gtk breeze-qt4 breeze-qt5 ];
         };
+    breeze-plymouth = callPackage ./breeze-plymouth {};
     kactivitymanagerd = callPackage ./kactivitymanagerd.nix {};
     kde-cli-tools = callPackage ./kde-cli-tools.nix {};
     kde-gtk-config = callPackage ./kde-gtk-config {};
