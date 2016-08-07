da1bf5fc711c7ce179d3f64cfb9b3abc4adbc42e
Lluís Batlle i Rossell <viric@viric.name>
2016-08-07 14:21:08 +0200

    Making kdenlive find kinit in PATH

--- pkgs/desktops/kde-5/applications-15.12/kdenlive.nix
+++ pkgs/desktops/kde-5/applications-15.12/kdenlive.nix
@@ -10,19 +10,12 @@
 , kiconthemes
 , ki18n
 , kinit
-, kjobwidgets
 , kio
 , kio-extras
-, kparts
-, ktexteditor
 , kwindowsystem
-, kxmlgui
 , kdbusaddons
-, kwallet
 , plasma-framework
-, kitemmodels
 , knotifications
-, threadweaver
 , knewstuff
 , karchive
 , knotifyconfig
@@ -49,16 +42,9 @@ kdeApp {
     kguiaddons
     kiconthemes
     kinit
-    #kjobwidgets
-    #kparts
-    #kxmlgui
     kdbusaddons
-    #kwallet
-    #kitemmodels
     knotifications
-    #threadweaver
     knewstuff
-    #libgit2
     karchive
     knotifyconfig
     kplotting
@@ -73,18 +59,17 @@ kdeApp {
     ki18n
     kio
     kio-extras
-    #ktexteditor
     kwindowsystem
     kfilemetadata
     plasma-framework
     phonon-backend-vlc
   ];
   postInstall = ''
-    wrapQtProgram "$out/bin/kdenlive"
+    wrapQtProgram "$out/bin/kdenlive" \
+        --prefix PATH : "${kinit}/bin"
   '';
   enableParallelBuilding = true;
   meta = {
-    license = with lib.licenses; [ gpl3 lgpl3 lgpl2 ];
-    maintainers = [ lib.maintainers.ttuegel ];
+    license = with lib.licenses; [ gpl2Plus ];
   };
 }
