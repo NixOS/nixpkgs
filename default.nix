230fed52fdc244b575301ef9f74e877c56cc586d
Domen Kožar <domen@dev.si>
2016-08-10 15:38:57 +0200

    Provide correct instructions how to upgrade Nix without nixpkgs.
    fixes #15344

--- default.nix
+++ default.nix
@@ -2,7 +2,23 @@ let requiredVersion = import ./lib/minver.nix; in
 
 if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
 
-  abort "This version of Nixpkgs requires Nix >= ${requiredVersion}, please upgrade! See https://nixos.org/wiki/How_to_update_when_Nix_is_too_old_to_evaluate_Nixpkgs"
+# following derivations are updated based on http://hydra.nixos.org/jobset/nix/maintenance/latest-eval
+
+  abort ''
+  This version of Nixpkgs requires Nix >= ${requiredVersion}, please upgrade:
+
+  On x86_64-linux:
+
+    $ nix-env -i /nix/store/mln2lswvkyx6x7j6pcx80cyf06fsc12m-nix-1.11.2
+
+  On i686-linux:
+
+    $ nix-env -i /nix/store/fpa7k4ra0jcy1jq3cig5738dmmqsqyjc-nix-1.11.2
+
+  On x86_64-darwin:
+
+    $ nix-env -i /nix/store/mcfhmajqw93xs4bgk55822gag11qs7yk-nix-1.11.2
+  ''
 
 else
 
