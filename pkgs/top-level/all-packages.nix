1578322080d3f4466194259788f65c4b951fd456
Katona László <muoniurn@gmail.com>
2016-08-10 12:37:56 +0200

    fehlstart: init at 9f4342d7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12986,6 +12986,8 @@ in
 
   fbreader = callPackage ../applications/misc/fbreader { };
 
+  fehlstart = callPackage ../applications/misc/fehlstart { };
+
   fetchmail = callPackage ../applications/misc/fetchmail { };
 
   flacon = callPackage ../applications/audio/flacon { };
