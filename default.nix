f1fead5d521e1f7bd63115c3b2423c705950d6b9
Domen Kožar <domen@dev.si>
2016-08-10 18:34:53 +0200

    Print Nix install instructions only for current platform

--- default.nix
+++ default.nix
@@ -1,4 +1,22 @@
-let requiredVersion = import ./lib/minver.nix; in
+with builtins;
+
+let
+  requiredVersion = import ./lib/minver.nix;
+  platformPath = {
+    x86_64-linux = "/nix/store/mln2lswvkyx6x7j6pcx80cyf06fsc12m-nix-1.11.2";
+    i686-linux = "/nix/store/fpa7k4ra0jcy1jq3cig5738dmmqsqyjc-nix-1.11.2";
+    x86_64-darwin = "/nix/store/mcfhmajqw93xs4bgk55822gag11qs7yk-nix-1.11.2";
+  };
+  installMsg = if (hasAttr currentSystem platformPath) then ''
+    On your platform ${currentSystem}:
+
+      $ nix-env -i ${getAttr currentSystem platformPath}
+  '' else ''
+    Your platform ${currentSystem} doesn't have a binary cache. Please
+    reinstall Nix following instructions on http://nixos.org/nix/download.html
+  '';
+in
+
 
 if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
 
@@ -7,18 +25,7 @@ if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.
   abort ''
   This version of Nixpkgs requires Nix >= ${requiredVersion}, please upgrade:
 
-  On x86_64-linux:
-
-    $ nix-env -i /nix/store/mln2lswvkyx6x7j6pcx80cyf06fsc12m-nix-1.11.2
-
-  On i686-linux:
-
-    $ nix-env -i /nix/store/fpa7k4ra0jcy1jq3cig5738dmmqsqyjc-nix-1.11.2
-
-  On x86_64-darwin:
-
-    $ nix-env -i /nix/store/mcfhmajqw93xs4bgk55822gag11qs7yk-nix-1.11.2
-  ''
+  ${installMsg}''
 
 else
 
