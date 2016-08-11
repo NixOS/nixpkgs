e26ac7afd49daefc6d0dd560e3c2ccda56233149
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-08-11 15:20:07 +0200

    linux: 4.4.16 -> 4.4.17

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.4.16";
+  version = "4.4.17";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "18v4n7yypl4c8k69zrnf9g09pilh47y0ciy3mwbksz2kmw4yq573";
+    sha256 = "10ags1n345irx1bi3fyal326b3m5myndz19v0klbvxhd3i3m350m";
   };
 
   kernelPatches = args.kernelPatches;
