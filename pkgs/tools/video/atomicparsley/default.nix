0a1cb79908aabb8c9f8a404e8d7a0133a3861592
Matthew Bauer <mjbauer95@gmail.com>
2016-08-10 22:42:51 -0500

    atomicparsley: fix darwin build

--- pkgs/tools/video/atomicparsley/default.nix
+++ pkgs/tools/video/atomicparsley/default.nix
@@ -10,11 +10,19 @@ stdenv.mkDerivation rec {
     sha256 = "de83f219f95e6fe59099b277e3ced86f0430ad9468e845783092821dff15a72e";
   };
 
-  buildInputs = with pkgs; [ unzip ];
+  buildInputs = with pkgs; [ unzip ]
+    ++ stdenv.lib.optional stdenv.isDarwin [ darwin.apple_sdk.frameworks.Cocoa ];
   patches = [ ./casts.patch ];
   setSourceRoot = "sourceRoot=${product}-source-${version}";
   buildPhase = "bash build";
   installPhase = "install -D AtomicParsley $out/bin/AtomicParsley";
+
+  postPatch = ''
+    substituteInPlace build \
+      --replace 'g++' 'c++'
+    substituteInPlace AP_NSImage.mm \
+      --replace '_NSBitmapImageFileType' 'NSBitmapImageFileType'
+  '';
 
   meta = with stdenv.lib; {
     description = ''
