fe02e2197d8e1126e1d120cd022f4cb9a566b3d9
Franz Pletz <fpletz@fnordicwalking.de>
2016-08-11 02:36:29 +0200

    linux_mptcp: 0.90.1 -> 0.91

--- pkgs/os-specific/linux/kernel/linux-mptcp.nix
+++ pkgs/os-specific/linux/kernel/linux-mptcp.nix
@@ -1,18 +1,18 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  mptcpVersion = "0.90.1";
-  modDirVersion = "3.18.25";
+  mptcpVersion = "0.91";
+  modDirVersion = "4.1.26";
   version = "${modDirVersion}-mptcp_v${mptcpVersion}";
 
   extraMeta = {
-    branch = "3.18";
-    maintainers = stdenv.lib.maintainers.layus;
+    branch = "4.1";
+    maintainers = with stdenv.lib.maintainers; [ layus fpletz ];
   };
 
   src = fetchurl {
     url = "https://github.com/multipath-tcp/mptcp/archive/v${mptcpVersion}.tar.gz";
-    sha256 = "088cpxl960xzrsz7x2lkq28ksa4gzjb1hp5yf8hxshihyhdaspwl";
+    sha256 = "0rbvgz89j5wk781y201qdxy2kz4gmlamb72wdbxj8mxv92x56lh3";
   };
 
   extraConfig = ''
