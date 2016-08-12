83a8948da26e4d5afaaca69909b174ff1caa8d70
davidak <git@davidak.de>
2016-08-12 03:58:09 +0200

    caddy: 0.8.3 -> 0.9 (closes #17678)

--- pkgs/servers/caddy/default.nix
+++ pkgs/servers/caddy/default.nix
@@ -2,17 +2,24 @@
 
 buildGoPackage rec {
   name = "caddy-${version}";
-  version = "0.8.3";
-  rev = "e2234497b79603388b58ba226abb157aa4aaf065";
+  version = "0.9";
+  rev = "f28af637327a4f12ae745284c519cfdeca5502ef";
 
   goPackagePath = "github.com/mholt/caddy";
+
+  subPackages = [ "caddy" ];
 
   src = fetchFromGitHub {
     inherit rev;
     owner = "mholt";
     repo = "caddy";
-    sha256 = "1snijkbz02yr7wij7bcmrj4257709sbklb3nhb5qmy95b9ssffm6";
+    sha256 = "1s7z0xbcw516i37pyj1wgxd9diqrifdghf97vs31ilbqs6z0nyls";
   };
+
+  buildFlagsArray = ''
+    -ldflags=
+      -X github.com/mholt/caddy/caddy/caddymain.gitTag=${version}
+  '';
 
   goDeps = ./deps.json;
 }
