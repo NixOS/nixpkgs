1bc03a7b336b5b4bd1a33369b938fb004a26fd37
obadz <obadz-git@obadz.com>
2016-08-12 21:31:06 +0100

    libircclient: init at 1.9

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2390,6 +2390,8 @@ in
 
   libevhtp = callPackage ../development/libraries/libevhtp { };
 
+  libircclient = callPackage ../development/libraries/libircclient { };
+
   liboauth = callPackage ../development/libraries/liboauth { };
 
   libsidplayfp = callPackage ../development/libraries/libsidplayfp { };
