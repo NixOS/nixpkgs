c4acc99549745edd8b42329588a409195aa702be
Matthew Bauer <mjbauer95@gmail.com>
2016-08-11 23:24:44 -0500

    nspr: fix darwin build
    - needs CoreServices

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8891,7 +8891,9 @@ in
 
   non = callPackage ../applications/audio/non { };
 
-  nspr = callPackage ../development/libraries/nspr { };
+  nspr = callPackage ../development/libraries/nspr {
+    inherit (darwin.apple_sdk.frameworks) CoreServices;
+  };
 
   nss = lowPrio (callPackage ../development/libraries/nss { });
   nssTools = nss.tools;
