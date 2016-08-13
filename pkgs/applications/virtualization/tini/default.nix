9a0dfe23d88131cbd0547cd40c7d295da54d78e1
Kranium Gikos Mendoza <kranium@gikos.net>
2016-08-13 14:23:20 +0800

    tini: fix build

--- pkgs/applications/virtualization/tini/default.nix
+++ pkgs/applications/virtualization/tini/default.nix
@@ -7,14 +7,12 @@ stdenv.mkDerivation rec {
     url = "https://github.com/krallin/tini/archive/v0.8.3.tar.gz";
     sha256 ="1w7rj4crrcyv25idmh4asbp2sxzwyihy5mbpw384bzxjzaxn9xpa";
   };
+  patchPhase = "sed -i /tini-static/d CMakeLists.txt";
   NIX_CFLAGS_COMPILE = [
     "-DPR_SET_CHILD_SUBREAPER=36"
     "-DPR_GET_CHILD_SUBREAPER=37"
   ];
   buildInputs = [ cmake ];
-  postInstall = ''
-    rm $out/bin/tini-static
-  '';
   meta = with stdenv.lib; {
     description = "A tiny but valid init for containers";
     homepage = https://github.com/krallin/tini;
