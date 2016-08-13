c60deb0266f904bc26f06db3bd07d379ab3f9a7c
Nikolay Amiantov <ab@fmap.me>
2016-08-13 03:18:13 +0300

    quote homepages for better clickability
    Done while I was traversing packages which I maintain to save extra clicks on
    urxvt (it captures semicolon as a part of URL).

--- pkgs/servers/rpcbind/default.nix
+++ pkgs/servers/rpcbind/default.nix
@@ -26,7 +26,7 @@ in stdenv.mkDerivation rec {
     description = "ONC RPC portmapper";
     license = licenses.bsd3;
     platforms = platforms.unix;
-    homepage = http://sourceforge.net/projects/rpcbind/;
+    homepage = "http://sourceforge.net/projects/rpcbind/";
     maintainers = with maintainers; [ abbradar ];
     longDescription = ''
       Universal addresses to RPC program number mapper.
