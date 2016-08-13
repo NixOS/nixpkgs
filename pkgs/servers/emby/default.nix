fb814e2bd7c968c135861829ea36e56cc972295c
Tristan Helmich <tristan.helmich@gmail.com>
2016-08-13 12:11:17 +0200

    emby: 3.0.6030 -> 3.0.6060

--- pkgs/servers/emby/default.nix
+++ pkgs/servers/emby/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "emby-${version}";
-  version = "3.0.6030";
+  version = "3.0.6060";
 
   src = fetchurl {
     url = "https://github.com/MediaBrowser/Emby/archive/${version}.tar.gz";
-    sha256 = "14fmgb8pwj11n57c1rm002ylwqapdqywbpsv7z6skairbaf6ny09";
+    sha256 = "1s895198x3kiqfznhp56vj67wld9fgh2wd7k8hw69mrasby0kmp3";
   };
 
   propagatedBuildInputs = with pkgs; [
