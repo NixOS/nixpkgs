b940a098ec4f68063a825cf469d757770b7f8f3e
Nikolay Amiantov <ab@fmap.me>
2016-08-13 03:10:46 +0300

    dwarf-fortress-packages.dwarf-fortress-original: 0.42.06 -> 0.43.05

--- pkgs/games/dwarf-fortress/default.nix
+++ pkgs/games/dwarf-fortress/default.nix
@@ -5,7 +5,7 @@ let
   callPackage_i686 = pkgsi686Linux.newScope self;
 
   self = rec {
-    dwarf-fortress-original = callPackage_i686 ./game.nix { };
+    dwarf-fortress-original = callPackage ./game.nix { };
 
     dfhack = callPackage_i686 ./dfhack {
       inherit (pkgsi686Linux.perlPackages) XMLLibXML XMLLibXSLT;
