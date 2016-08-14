9906378e77e6667ea90cc9363b3b9500bf5693b0
Shea Levy <shea@shealevy.com>
2016-08-14 12:33:50 -0400

    long-shebang: 1.0.1 -> 1.1.0
    (cherry picked from commit 7069047f2ed6548910f3f2bff4a23946a363e860)

--- pkgs/misc/long-shebang/default.nix
+++ pkgs/misc/long-shebang/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl }: let
-  version = "1.0.1";
+  version = "1.1.0";
 in stdenv.mkDerivation {
   name = "long-shebang-${version}";
 
   src = fetchurl {
     url = "https://github.com/shlevy/long-shebang/releases/download/v${version}/long-shebang-${version}.tar.xz";
-    sha256 = "0kj299f3a9zawi96fmw1iq6p6yg0pdm1wgmv61iw5w0zn9v4924b";
+    sha256 = "0rlyibf7pczjfsi91nl1n5vri2vqibmvyyy070jaw3wb0wjm565a";
   };
 
   meta = {
