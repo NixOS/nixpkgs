98b5e3a531a5f560314104ceecb730e59fc78a58
Dan Peebles <pumpkin@me.com>
2016-08-14 17:34:55 -0400

    darwin.libpthread: fix messed-up header
    We don't actually need the private headers and the private qos.h was
    overwriting the public one, causing weird issues downstream (especially
    with Swift's CoreFoundation)

--- pkgs/os-specific/darwin/apple-source-releases/libpthread/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/libpthread/default.nix
@@ -9,9 +9,11 @@ appleDerivation {
     mkdir -p $out/include/pthread/
     mkdir -p $out/include/sys/_types
     cp pthread/*.h $out/include/pthread/
-    cp private/*.h $out/include/pthread/
+
+    # This overwrites qos.h, and is probably not necessary, but I'll leave it here for now
+    # cp private/*.h $out/include/pthread/
+
     cp -r sys $out/include
     cp -r sys/_pthread/*.h $out/include/sys/_types/
-
   '';
 }
