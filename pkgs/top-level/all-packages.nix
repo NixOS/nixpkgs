9b4ef5bb54d99b0ca93c9705fb0aee7c2d7b8805
Michal Rus <m@michalrus.com>
2016-08-15 12:17:56 +0200

    awf: init at 1.3.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6113,6 +6113,8 @@ in
 
   astyle = callPackage ../development/tools/misc/astyle { };
 
+  awf = callPackage ../development/tools/misc/awf { };
+
   electron = callPackage ../development/tools/electron { };
 
   autobuild = callPackage ../development/tools/misc/autobuild { };
