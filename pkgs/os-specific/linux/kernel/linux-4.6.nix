df350f56ddd11a00a33883bb719636e61ee340b7
Franz Pletz <fpletz@fnordicwalking.de>
2016-08-17 00:36:14 +0200

    linux: 4.6.6 -> 4.6.7
    Fixes CVE-2016-5696.

--- pkgs/os-specific/linux/kernel/linux-4.6.nix
+++ pkgs/os-specific/linux/kernel/linux-4.6.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.6.6";
+  version = "4.6.7";
   extraMeta.branch = "4.6";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1lx00j0z0rasmc87mcvqd1h6r4znb9c2q22jbs2mrissr5w05vgm";
+    sha256 = "107acpq6jk48ng25jlyv905zslrrv77yxwr46mqbq5lxg8rdg7fh";
   };
 
   kernelPatches = args.kernelPatches;
