74b25f4c694ea3222599582f830f001c1815d4fe
Robert Helgesson <robert@rycee.net>
2016-08-17 22:33:01 +0200

    redstore: 0.5.2 -> 0.5.4

--- pkgs/servers/http/redstore/default.nix
+++ pkgs/servers/http/redstore/default.nix
@@ -2,11 +2,11 @@
   , librdf_rasqal }:
 
 stdenv.mkDerivation rec {
-  name = "redstore-0.5.2";
+  name = "redstore-0.5.4";
 
   src = fetchurl {
     url = "http://www.aelius.com/njh/redstore/${name}.tar.gz";
-    sha256 = "fdbe499a7bbe8c8a756ecb738b83ea375e96af16a1d74245b75600d4d40adb7d";
+    sha256 = "0hc1fjfbfvggl72zqx27v4wy84f5m7bp4dnwd8g41aw8lgynbgaq";
   };
 
   buildInputs = [ gmp pkgconfig redland zlib librdf_raptor2 librdf_rasqal ];
