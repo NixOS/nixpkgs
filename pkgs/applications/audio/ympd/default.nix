6aa729cf52edef0c8579134e92b95f3028b666b1
Franz Pletz <fpletz@fnordicwalking.de>
2016-08-17 13:19:41 +0200

    ympd: remove unncessary preConfigure hook
    Fixes #17788.

--- pkgs/applications/audio/ympd/default.nix
+++ pkgs/applications/audio/ympd/default.nix
@@ -11,20 +11,12 @@ stdenv.mkDerivation rec {
     sha256 = "1nvb19jd556v2h2bi7w4dcl507p3p8xvjkqfzrcsy7ccy3502brq";
   };
 
-  preConfigure = ''
-    export LIBMPDCLIENT_LIBS=${mpd_clientlib}/lib/libmpdclient.${if stdenv.isDarwin then mpd_clientlib.majorVersion + ".dylib" else "so." + mpd_clientlib.majorVersion + ".0." + mpd_clientlib.minorVersion}
-    export LIBMPDCLIENT_CFLAGS=${mpd_clientlib}
-
-    export LIBCLANG_CXXFLAGS="-isystem ${llvmPackages.clang.cc}/include $(llvm-config --cxxflags)"
-    export LIBCLANG_LIBDIR="${llvmPackages.clang.cc}/lib"
-  '';
-
   buildInputs = [ cmake pkgconfig mpd_clientlib openssl ];
 
   meta = {
     homepage = "http://www.ympd.org";
     description = "Standalone MPD Web GUI written in C, utilizing Websockets and Bootstrap/JS";
     maintainers = [ stdenv.lib.maintainers.siddharthist ];
-    platforms = stdenv.lib.platforms.all;
+    platforms = stdenv.lib.platforms.unix;
   };
 }
