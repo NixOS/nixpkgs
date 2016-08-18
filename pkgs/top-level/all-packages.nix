59d1c52f6854b7b00403c503b24b0ff0cff21a5b
William Casarin <bill@casarin.me>
2016-08-18 11:15:16 -0700

    bookerly: Amazon Kindle font, init at 4.21.0.65

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11898,6 +11898,8 @@ in
 
   bgnet = callPackage ../data/documentation/bgnet { };
 
+  bookerly = callPackage ../data/fonts/bookerly { };
+
   cacert = callPackage ../data/misc/cacert { };
 
   caladea = callPackage ../data/fonts/caladea {};
