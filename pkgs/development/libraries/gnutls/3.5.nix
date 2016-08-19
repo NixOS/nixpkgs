c69e2d57639bce0179453908d066573f4004d215
Vladimír Čunát <vcunat@gmail.com>
2016-08-19 16:36:52 +0200

    gnutls35: maintenance 3.5.1 -> 3.5.3

--- pkgs/development/libraries/gnutls/3.5.nix
+++ pkgs/development/libraries/gnutls/3.5.nix
@@ -1,11 +1,11 @@
 { callPackage, fetchurl, autoreconfHook, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "3.5.1";
+  version = "3.5.3";
 
   src = fetchurl {
     url = "ftp://ftp.gnutls.org/gcrypt/gnutls/v3.5/gnutls-${version}.tar.xz";
-    sha256 = "1gdxr1p1sigigwvangqf3v31i8fda07d6ngawykarhr7ls00yjmw";
+    sha256 = "92c4bc999a10a1b95299ebefaeea8333f19d8a98d957a35b5eae74881bdb1fef";
   };
 
   # This fixes some broken parallel dependencies
