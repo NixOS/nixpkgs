23a50ca40776464ea18dc1c7136a37d3e11c5840
Corbin <cds@corbinsimpson.com>
2016-08-18 23:16:37 -0700

    fetchfossil: Don't let Fossil write to $out/.fossil.

--- pkgs/build-support/fetchfossil/builder.sh
+++ pkgs/build-support/fetchfossil/builder.sh
@@ -3,7 +3,8 @@ header "Cloning Fossil $url into $out"
 
 # Fossil, bless its adorable little heart, wants to write global configuration
 # to $HOME/.fossil. AFAICT, there is no way to disable this functionality.
-export HOME=.
+# Instead, we'll let it write to the build directory.
+export HOME=$(pwd)
 
 # We must explicitly set the admin user for the clone to something reasonable.
 fossil clone -A nobody "$url" fossil-clone.fossil
