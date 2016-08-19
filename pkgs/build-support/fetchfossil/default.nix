7880bc0c21498e27523e42f8d3361492dd4fe165
Corbin <cds@corbinsimpson.com>
2016-08-19 07:34:58 -0700

    fetchfossil: Cleanup some copy-and-paste.

--- pkgs/build-support/fetchfossil/default.nix
+++ pkgs/build-support/fetchfossil/default.nix
@@ -2,15 +2,14 @@
 
 {name ? null, url, rev ? null, md5 ? null, sha256 ? null}:
 
-# TODO: statically check if mercurial as the https support if the url starts woth https.
 stdenv.mkDerivation {
   name = "fossil-archive" + (if name != null then "-${name}" else "");
   builder = ./builder.sh;
   buildInputs = [fossil];
 
-  impureEnvVars = [
-    "http_proxy" "https_proxy" "ftp_proxy" "all_proxy" "no_proxy"
-  ];
+  # Envvar docs are hard to find. A link for the future:
+  # https://www.fossil-scm.org/index.html/doc/trunk/www/env-opts.md
+  impureEnvVars = [ "http_proxy" ];
 
   outputHashAlgo = if md5 != null then "md5" else "sha256";
   outputHashMode = "recursive";
