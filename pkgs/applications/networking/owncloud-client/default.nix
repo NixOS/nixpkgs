5120af001f2bb163b58c41c84b11a0c136a207fe
Bjørn Forsman <bjorn.forsman@gmail.com>
2016-08-21 21:53:59 +0200

    owncloud-client: 2.2.1 -> 2.2.3
    Changelog: https://owncloud.org/changelog/desktop/

--- pkgs/applications/networking/owncloud-client/default.nix
+++ pkgs/applications/networking/owncloud-client/default.nix
@@ -3,11 +3,11 @@
 stdenv.mkDerivation rec {
   name = "owncloud-client" + "-" + version;
 
-  version = "2.2.1";
+  version = "2.2.3";
 
   src = fetchurl {
     url = "https://download.owncloud.com/desktop/stable/owncloudclient-${version}.tar.xz";
-    sha256 = "1wis62jk4y4mbr25y39y6af57pi6vp2mbryazmvn6zgnygf69m3h";
+    sha256 = "00bx9wrgvbdhi9vx30qfgkdz0k8nxlj313pac34cchx5xpij3jgq";
   };
 
   buildInputs =
