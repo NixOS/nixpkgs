c2553952195cb390c9ae65e2a31ad02cc82d0f76
Vladimír Čunát <vcunat@gmail.com>
2016-08-22 23:47:57 +0200

    gnome-calculator: work around #17912, in a hacky way

--- pkgs/desktops/gnome-3/3.20/core/gnome-calculator/default.nix
+++ pkgs/desktops/gnome-3/3.20/core/gnome-calculator/default.nix
@@ -1,7 +1,23 @@
 { stdenv, intltool, fetchurl, pkgconfig, libxml2
 , bash, gtk3, glib, wrapGAppsHook
+, autoreconfHook
 , itstool, gnome3, librsvg, gdk_pixbuf, mpfr, gmp, libsoup }:
 
+let
+  # work-around for https://github.com/NixOS/nixpkgs/issues/17912
+  glib-hacked = (glib.overrideDerivation (attrs: {
+    postPatch = attrs.postPatch or "" + ''
+      cat '${rPatch}' | sed '/^---.*gitignore/,/^---/d' | patch -R -p1
+    '';
+    nativeBuildInputs = attrs.nativeBuildInputs ++ [ autoreconfHook ];
+  })).dev; # we need just bin/glib-compile-resources; everything else should be the same
+  rPatch = fetchurl {
+    name = "glib-resources.patch";
+    url = "https://bug673101.bugzilla-attachments.gnome.org/attachment.cgi?"
+      + "id=329105&action=diff&collapsed=&context=patch&format=raw&headers=1";
+    sha256 = "051d7l1wx9sp5wcnv7yk6mbn3akac1m06sp3jl3hqapx74bkmai0";
+  };
+in
 stdenv.mkDerivation rec {
   inherit (import ./src.nix fetchurl) name src;
 
@@ -11,7 +27,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ pkgconfig wrapGAppsHook ];
 
-  buildInputs = [ bash gtk3 glib intltool itstool
+  buildInputs = [ glib-hacked bash gtk3 glib intltool itstool
                   libxml2 gnome3.gtksourceview mpfr gmp
                   gdk_pixbuf gnome3.defaultIconTheme librsvg
                   gnome3.gsettings_desktop_schemas gnome3.dconf libsoup ];
