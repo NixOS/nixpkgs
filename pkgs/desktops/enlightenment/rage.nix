58a52f7e10eeb180971e1d3f33eec0686964b776
José Romildo Malaquias <malaquias@gmail.com>
2016-08-22 19:46:56 -0300

    rage: 0.1.4 -> 0.2.0

--- pkgs/desktops/enlightenment/rage.nix
+++ pkgs/desktops/enlightenment/rage.nix
@@ -1,15 +1,36 @@
-{ stdenv, fetchurl, elementary, efl, automake, autoconf, libtool, pkgconfig, gst_all_1
+{ stdenv, fetchurl, efl, automake, autoconf, libtool, pkgconfig, gst_all_1
 , makeWrapper, lib }:
+
 stdenv.mkDerivation rec {
   name = "rage-${version}";
-  version = "0.1.4";
+  version = "0.2.0";
+  
   src = fetchurl {
     url = "http://download.enlightenment.org/rel/apps/rage/${name}.tar.gz";
-    sha256 = "10j3n8crk16jzqz2hn5djx6vms5f6x83qyiaphhqx94h9dgv2mgg";
+    sha256 = "06fxhznwbd5x341r8ml3cpwmvwn0aq9i1akcgclk4vjdqiyff1d9";
   };
+
+  nativeBuildInputs = [
+    automake autoconf libtool pkgconfig makeWrapper
+  ];
+
+  NIX_CFLAGS_COMPILE = [
+    "-I${efl}/include/ecore-con-1"
+    "-I${efl}/include/ecore-evas-1"
+    "-I${efl}/include/ecore-file-1"
+    "-I${efl}/include/ecore-imf-1"
+    "-I${efl}/include/ecore-input-1"
+    "-I${efl}/include/eet-1"
+    "-I${efl}/include/efreet-1"
+    "-I${efl}/include/eldbus-1"
+    "-I${efl}/include/emile-1"
+    "-I${efl}/include/eo-1"
+    "-I${efl}/include/ethumb-1"
+    "-I${efl}/include/ethumb-client-1"
+  ];
+
   buildInputs = [
-    elementary efl automake autoconf libtool pkgconfig
-    makeWrapper
+    efl
     gst_all_1.gstreamer
     gst_all_1.gst-plugins-base
     gst_all_1.gst-plugins-good
@@ -25,6 +46,7 @@ stdenv.mkDerivation rec {
     wrapProgram $out/bin/rage \
       --prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0"
   '';
+
   meta = {
     description = "Video + Audio player along the lines of mplayer";
     homepage = http://enlightenment.org/;
