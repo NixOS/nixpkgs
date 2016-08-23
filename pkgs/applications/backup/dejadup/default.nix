da55e074c10cb0ba6a0065ba3f45ad3183440ea9
silvester <silvester@gmx.net>
2016-08-24 00:30:02 +0200

    dejadup init:34.2. Some changes concerning vala and Shebangs.

--- pkgs/applications/backup/dejadup/default.nix
+++ pkgs/applications/backup/dejadup/default.nix
@@ -1,6 +1,6 @@
-{ stdenv, fetchurl, cmake, pkgconfig, vala, 
+{ stdenv, fetchurl, cmake, pkgconfig, vala_0_32, 
   gettext, gnome3, libnotify, intltool, itstool, 
-  glib, gtk3, packagekit, libsecret, ... }:
+  glib, gtk3, packagekit, libsecret }:
 
 stdenv.mkDerivation rec {
   name = "dejadup-${version}";
@@ -11,8 +11,16 @@ stdenv.mkDerivation rec {
     sha256 = "ff30732f28233e2422d069f1769a725d4a39c91a279c3bf885c4162fd85818bb";
   };
 
+  # /usr/bin/perl hardcoded.
+  patchPhase = ''
+    patchShebangs .
+  '';
+
+  # couldn't find gio/gdesktopappinfo.h
+  NIX_CFLAGS_COMPILE = "-I${glib.dev}/include/gio-unix-2.0";
+
   doCheck = true;
-  buildInputs = [ cmake pkgconfig vala gettext libnotify 
+  buildInputs = [ cmake pkgconfig vala_0_32 gettext libnotify 
                   gnome3.libpeas intltool itstool glib gtk3 
                   packagekit libsecret ];
 
