ab08440f9c2783d8a6d3630ca159dbcebf64b5b4
obadz <obadz-git@obadz.com>
2016-08-23 14:13:49 +0100

    Revert "Temporarily disable chromium test"
    This reverts commit 9e6eec201b8b56ef493c60af3acb2e8ddd8be258.
    
    Verified locally that cd063d7 fixes the problem.
    cc @edolstra @aszlig

--- nixos/release.nix
+++ nixos/release.nix
@@ -220,7 +220,7 @@ in rec {
   tests.boot = callSubTests tests/boot.nix {};
   tests.boot-stage1 = callTest tests/boot-stage1.nix {};
   tests.cadvisor = hydraJob (import tests/cadvisor.nix { system = "x86_64-linux"; });
-  #tests.chromium = (callSubTests tests/chromium.nix { system = "x86_64-linux"; }).stable;
+  tests.chromium = (callSubTests tests/chromium.nix { system = "x86_64-linux"; }).stable;
   tests.cjdns = callTest tests/cjdns.nix {};
   tests.containers-ipv4 = callTest tests/containers-ipv4.nix {};
   tests.containers-ipv6 = callTest tests/containers-ipv6.nix {};
