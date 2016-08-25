f4bb247bd63762fb431fc8ec60ccf8fc4f63aeed
Thomas Tuegel <ttuegel@gmail.com>
2016-08-25 10:06:23 -0500

    fetch-kde-qt.sh: modularize script allowing multiple versions

---
+++ pkgs/development/libraries/qt-5/5.6/fetch.sh
@@ -0,0 +1,3 @@
+WGET_ARGS=( http://download.qt.io/official_releases/qt/5.6/5.6.1-1/submodules/ \
+            http://download.qt.io/community_releases/5.6/5.6.1/ \
+            -A '*.tar.xz' )
---
+++ pkgs/development/libraries/qt-5/5.7/fetch.sh
@@ -0,0 +1,2 @@
+WGET_ARGS=( http://download.qt.io/official_releases/qt/5.7/5.7.0/submodules/ \
+            -A '*.tar.xz' )
