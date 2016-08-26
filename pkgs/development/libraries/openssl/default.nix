7c96bc4ee68ccd7f4308c0cca13304ab1b49c186
Robin Gloster <mail@glob.in>
2016-08-26 19:31:20 +0000

    openssl: fix merge

--- pkgs/development/libraries/openssl/default.nix
+++ pkgs/development/libraries/openssl/default.nix
@@ -18,7 +18,6 @@ let
 
     patches =
       args.patches
-      ++ [ ./use-etc-ssl-certs.patch ]
       ++ optional (versionOlder version "1.1.0") ./use-etc-ssl-certs.patch
       ++ optional stdenv.isCygwin ./1.0.1-cygwin64.patch
       ++ optional
