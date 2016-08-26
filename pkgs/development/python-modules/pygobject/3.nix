81d95ed12d06e6d4f48022eef1ed6ea0977c95b7
Josef Kemetmueller <josef.kemetmueller@aon.at>
2016-08-26 23:18:32 +0200

    pygobject3: Add missing dependencies (for darwin)
    The packages "which" and "ncurses" are needed for building pygobject3
    (on darwin) during the checkPhase. The ncurses library is necessary only
    because python3.5 is currently built using "-lncurses" and pygobject3
    wants the same libraries that python3.5 was compiled with. (Because it
    uses "python3.5-config --ldflags" during the build)
    
    (cherry picked from commit 717c76716fb839eca7cddf25a391a3a336702a04)

--- pkgs/development/python-modules/pygobject/3.nix
+++ pkgs/development/python-modules/pygobject/3.nix
@@ -1,4 +1,4 @@
-{ lib, fetchurl, mkPythonDerivation, python, pkgconfig, glib, gobjectIntrospection, pycairo, cairo }:
+{ stdenv, fetchurl, mkPythonDerivation, python, pkgconfig, glib, gobjectIntrospection, pycairo, cairo, which, ncurses}:
 
 mkPythonDerivation rec {
   major = "3.20";
@@ -10,12 +10,13 @@ mkPythonDerivation rec {
     sha256 = "0ikzh3l7g1gjh8jj8vg6mdvrb25svp63gxcam4m0i404yh0lgari";
   };
 
-  buildInputs = [ pkgconfig glib gobjectIntrospection ];
+  buildInputs = [ pkgconfig glib gobjectIntrospection ]
+                 ++ stdenv.lib.optionals stdenv.isDarwin [ which ncurses ];
   propagatedBuildInputs = [ pycairo cairo ];
 
   meta = {
     homepage = http://live.gnome.org/PyGObject;
     description = "Python bindings for Glib";
-    platforms = lib.platforms.unix;
+    platforms = stdenv.lib.platforms.unix;
   };
 }
