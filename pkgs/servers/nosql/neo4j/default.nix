f7017a6ede09bb1a6bdfeddad1a24a6e4c1f2a20
Robin Gloster <mail@glob.in>
2016-08-26 13:45:17 +0000

    neo4j: 2.1.3 -> 3.0.4

--- pkgs/servers/nosql/neo4j/default.nix
+++ pkgs/servers/nosql/neo4j/default.nix
@@ -1,14 +1,14 @@
-{ stdenv, fetchurl, makeWrapper, jre, which, gnused }:
+{ stdenv, fetchurl, makeWrapper, jre, which, gnused, gawk, bashCompletion }:
 
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "neo4j-${version}";
-  version = "2.1.3";
+  version = "3.0.4";
 
   src = fetchurl {
     url = "http://dist.neo4j.org/neo4j-community-${version}-unix.tar.gz";
-    sha256 = "0gcyy6ayn8qvxj6za5463lgy320mn4rq7q5qysc26fxjd73drrrk";
+    sha256 = "01g88b9b1510jbks9v4xr4hyw8zy9nivxsl86xi810xi7qb53np1";
   };
 
   buildInputs = [ makeWrapper jre which gnused ];
@@ -22,10 +22,12 @@ stdenv.mkDerivation rec {
     cp -R * "$out/share/neo4j"
 
     mkdir -p "$out/bin"
+    substituteInPlace $out/share/neo4j/bin/neo4j \
+        --replace compgen ${bashCompletion}/share/bash-completion/completions/compgen
     makeWrapper "$out/share/neo4j/bin/neo4j" "$out/bin/neo4j" \
-        --prefix PATH : "${stdenv.lib.makeBinPath [ jre which gnused ]}"
+        --prefix PATH : "${stdenv.lib.makeBinPath [ jre which gnused gawk ]}"
     makeWrapper "$out/share/neo4j/bin/neo4j-shell" "$out/bin/neo4j-shell" \
-        --prefix PATH : "${stdenv.lib.makeBinPath [ jre which gnused ]}"
+        --prefix PATH : "${stdenv.lib.makeBinPath [ jre which gnused gawk ]}"
   '';
 
   meta = with stdenv.lib; {
