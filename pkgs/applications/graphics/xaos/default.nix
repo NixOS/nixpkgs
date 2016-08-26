e0deed0110ce2627d19eae04c16e8341f1d81269
Robin Gloster <mail@glob.in>
2016-08-26 15:19:37 +0000

    ghc: add comment to relocation patch

--- pkgs/applications/graphics/xaos/default.nix
+++ pkgs/applications/graphics/xaos/default.nix
@@ -10,6 +10,8 @@ stdenv.mkDerivation rec {
     sha256 = "15cd1cx1dyygw6g2nhjqq3bsfdj8sj8m4va9n75i0f3ryww3x7wq";
   };
 
+  hardeningDisable = [ "format" ];
+
   buildInputs = [
     aalib gsl libpng libX11 xproto libXext xextproto
     libXt zlib gettext intltool perl
