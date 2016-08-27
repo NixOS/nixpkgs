0782b650590c260a15604bc0ca56c113d53c000f
Nikolay Amiantov <ab@fmap.me>
2016-08-27 19:46:43 +0300

    linuxPackages.displaylink: init at 1.1.62
    (cherry picked from commit fbf6a97b04bba5f36a3caf26f54034324397f15a)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11269,6 +11269,8 @@ in
 
     cpupower = callPackage ../os-specific/linux/cpupower { };
 
+    displaylink = callPackage ../os-specific/linux/displaylink { };
+
     dpdk = callPackage ../os-specific/linux/dpdk { };
 
     pktgen = callPackage ../os-specific/linux/pktgen { };
