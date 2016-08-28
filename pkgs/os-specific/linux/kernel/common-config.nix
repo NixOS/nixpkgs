d02ec1f0da34cbe45c6491b28b20426475b46000
Sophie Taylor <sophie@traumapony.org>
2016-08-28 23:25:55 +1000

    Update common-config.nix

--- pkgs/os-specific/linux/kernel/common-config.nix
+++ pkgs/os-specific/linux/kernel/common-config.nix
@@ -277,7 +277,7 @@ with stdenv.lib;
   RANDOMIZE_BASE? y
   STRICT_DEVMEM y # Filter access to /dev/mem
   SECURITY_SELINUX_BOOTPARAM_VALUE? 0 # Disable SELinux by default
-  DEVKMEM?n # Disable /dev/kmem
+  DEVKMEM? n # Disable /dev/kmem
   ${if versionOlder version "3.14" then ''
     CC_STACKPROTECTOR? y # Detect buffer overflows on the stack
   '' else ''
