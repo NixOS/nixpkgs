aebabb987172f0d14ed7d38b4c9f87f73f097851
Frederik Rietdijk <fridh@fridh.nl>
2016-08-29 12:31:59 +0200

    qarte: create python env

--- pkgs/applications/video/qarte/default.nix
+++ pkgs/applications/video/qarte/default.nix
@@ -1,7 +1,7 @@
-{ stdenv, fetchbzr, python3Packages, rtmpdump, makeWrapper }:
+{ stdenv, fetchbzr, python3, rtmpdump, makeWrapper }:
 
 let
-  inherit (python3Packages) python pyqt5 sip;
+  pythonEnv = python3.withPackages (ps: with ps; [ pyqt5 sip ]);
 in stdenv.mkDerivation {
   name = "qarte-3.2.0";
   src = fetchbzr {
@@ -10,16 +10,14 @@ in stdenv.mkDerivation {
     sha256 = "0dvl38dknmnj2p4yr25p88kw3mh502c6qdp2bd43bhd2sqc3b0v0";
   };
 
-  buildInputs = [ makeWrapper ];
+  buildInputs = [ makeWrapper pythonEnv ];
 
   installPhase = ''
     mkdir -p $out/bin
     mv qarte $out/bin/
     substituteInPlace $out/bin/qarte \
-      --replace '/usr/bin/python3' "${python.interpreter}" \
       --replace '/usr/share' "$out/share"
     wrapProgram $out/bin/qarte \
-      --prefix PYTHONPATH : "${pyqt5}/lib/${python.libPrefix}/site-packages:${sip}/lib/${python.libPrefix}/site-packages" \
       --prefix PATH : "${rtmpdump}/bin"
 
     mkdir -p $out/share/man/man1/
