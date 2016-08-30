4223e83a68e358cda3d375428cf5edad597cdd9d
Tuomas Tynkkynen <tuomas@tuxera.com>
2016-08-30 21:49:34 +0300

    tcltls: Fix reference to openssl dev output

--- pkgs/development/libraries/tcltls/default.nix
+++ pkgs/development/libraries/tcltls/default.nix
@@ -4,24 +4,26 @@ stdenv.mkDerivation rec {
   name = "tcltls-${version}";
   version = "1.6";
 
-  configureFlags = "--with-tcl=" + tcl + "/lib "
-                 + "--with-tclinclude=" + tcl + "/include "
-                 + "--with-ssl-dir=" + openssl;
-
-  preConfigure = ''
-    configureFlags="--exec_prefix=$prefix $configureFlags"
-  '';
-
   src = fetchurl {
     url = "mirror://sourceforge/tls/tls${version}-src.tar.gz";
     sha256 = "adec50143a9ad634a671d24f7c7bbf2455487eb5f12d290f41797c32a98b93f3";
   };
 
+  buildInputs = [ tcl openssl ];
+
+  configureFlags = [
+    "--with-tcl=${tcl}/lib"
+    "--with-tclinclude=${tcl}/include"
+    "--with-ssl-dir=${openssl.dev}"
+  ];
+
+  preConfigure = ''
+    configureFlags="--exec_prefix=$prefix $configureFlags"
+  '';
+
   passthru = {
     libPrefix = "tls${version}";
   };
-
-  buildInputs = [ tcl openssl ];
 
   meta = {
     homepage = "http://tls.sourceforge.net/";
