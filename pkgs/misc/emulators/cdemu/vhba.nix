21282246208b7591d1431beef7fc6c8e7affef0d
Nikolay Amiantov <ab@fmap.me>
2016-08-31 16:00:15 +0300

    linuxPackages.vhba: disable PIC hardening

--- pkgs/misc/emulators/cdemu/vhba.nix
+++ pkgs/misc/emulators/cdemu/vhba.nix
@@ -1,12 +1,22 @@
 { stdenv, fetchurl, kernel }:
-let version = "20140928";
-in stdenv.mkDerivation {
+
+stdenv.mkDerivation rec {
   name = "vhba-${version}";
+  version = "20140928";
+
   src  = fetchurl {
     url = "mirror://sourceforge/cdemu/vhba-module-${version}.tar.bz2";
     sha256 = "18jmpg2kpx87f32b8aprr1pxla9dlhf901rkj1sp3ammf94nxxa5";
   };
-  preBuild = ''
-    makeFlags="KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build INSTALL_MOD_PATH=$out";
-  '';
+
+  makeFlags = [ "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build" "INSTALL_MOD_PATH=$(out)" ];
+
+  hardeningDisable = [ "pic" ];
+
+  meta = with stdenv.lib; {
+    description = "Provides a Virtual (SCSI) HBA";
+    homepage = "http://cdemu.sourceforge.net/about/vhba/";
+    platforms = platforms.linux;
+    licenses = licenses.gpl2Plus;
+  };
 }
