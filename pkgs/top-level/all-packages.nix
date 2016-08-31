bf30d0af9d758dd767758767ac25cba5b40a75de
José Romildo Malaquias <malaquias@gmail.com>
2016-08-31 07:56:52 -0300

    roboto-mono: init at 2016-01-11

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12100,6 +12100,8 @@ in
 
   roboto = callPackage ../data/fonts/roboto { };
 
+  roboto-mono = callPackage ../data/fonts/roboto-mono { };
+
   hasklig = callPackage ../data/fonts/hasklig {};
 
   sound-theme-freedesktop = callPackage ../data/misc/sound-theme-freedesktop { };
