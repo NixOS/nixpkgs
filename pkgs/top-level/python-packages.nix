aeecf5f0d7427449745694c8d85c10f81fe13dc6
Fernando J Pando <fernando.pando@stelligent.com>
2016-08-31 17:54:27 -0400

    txgithub fix

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -26599,8 +26599,14 @@ in modules // {
       sha256 = "16gbizy8vkxasxylwzj4p66yw8979nvzxdj6csidgmng7gi2k8nx";
     };
     propagatedBuildInputs = with self; [ pyopenssl twisted service-identity ];
-    doCheck = !isPy3k;
-    # ERROR: scripts/create_token.py, line 37 except usage.UsageError, errortext:except
+    # fix python3 issues
+    patchPhase = ''
+      sed -i 's/except usage.UsageError, errortext/except usage.UsageError as errortext/' txgithub/scripts/create_token.py
+      sed -i 's/except usage.UsageError, errortext/except usage.UsageError as errortext/' txgithub/scripts/gist.py
+      sed -i 's/print response\[\x27html_url\x27\]/print(response\[\x27html_url\x27\])/' txgithub/scripts/gist.py
+      sed -i '41d' txgithub/scripts/gist.py
+      sed -i '41d' txgithub/scripts/gist.py
+    '';
     meta = {
       description = "GitHub API client implemented using Twisted.";
       homepage    = "https://github.com/tomprince/txgithub";
