b96ba3fc9383c12ad352b509e5229bfbbd01523c
Frederik Rietdijk <fridh@fridh.nl>
2016-09-01 18:11:50 +0200

    pythonPackages.isodate: fix tests

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -21098,6 +21098,13 @@ in modules // {
       sha256 = "42105c41d037246dc1987e36d96f3752ffd5c0c24834dd12e4fdbe1e79544e31";
     };
 
+    # Judging from SyntaxError
+    doCheck = !(isPy3k);
+
+    checkPhase = ''
+      ${python.interpreter} -m unittest discover -s src/isodate/tests
+    '';
+
     meta = {
       description = "ISO 8601 date/time parser";
       homepage = http://cheeseshop.python.org/pypi/isodate;
