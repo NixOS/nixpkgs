81b9043fc4a8e65c908f2418d8714ff1256fb7eb
Eric Sagnes <eric.sagnes@gmail.com>
2016-09-03 17:15:14 +0900

    make-test.nix: remove file

--- nixos/tests/make-test.nix
+++
@@ -1,5 +0,0 @@
-f: { system ? builtins.currentSystem, ... } @ args:
-
-with import ../lib/testing.nix { inherit system; };
-
-makeTest (if builtins.isFunction f then f (args // { inherit pkgs; inherit (pkgs) lib; }) else f)
