e8dea0db30a0fede92ac22733deb33325ee53075
Vladimír Čunát <vcunat@gmail.com>
2016-09-04 11:05:57 +0200

    dejavu_fontsEnv: explain the situation in a comment

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11931,6 +11931,7 @@ in
     inherit (perlPackages) FontTTF;
   });
 
+  # solve collision for nix-env before https://github.com/NixOS/nix/pull/815
   dejavu_fontsEnv = buildEnv {
     name = "${dejavu_fonts.name}";
     paths = [ dejavu_fonts.out ];
