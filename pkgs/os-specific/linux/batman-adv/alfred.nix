c210cff715eaefc28f0885331711bc2c86bba33c
Franz Pletz <fpletz@fnordicwalking.de>
2016-09-05 19:02:57 +0200

    batman-adv: 2016.2 -> 2016.3

--- pkgs/os-specific/linux/batman-adv/alfred.nix
+++ pkgs/os-specific/linux/batman-adv/alfred.nix
@@ -1,18 +1,18 @@
-{ stdenv, fetchurl, pkgconfig, gpsd, libcap }:
+{ stdenv, fetchurl, pkgconfig, gpsd, libcap, libnl }:
 
 let
-  ver = "2016.2";
+  ver = "2016.3";
 in
 stdenv.mkDerivation rec {
   name = "alfred-${ver}";
 
   src = fetchurl {
     url = "http://downloads.open-mesh.org/batman/releases/batman-adv-${ver}/${name}.tar.gz";
-    sha256 = "19025arn926lhn54ss4gmmdss9z4a3yxk4ja5kyv17mi5i4yg7j6";
+    sha256 = "0a92n570hrsh58ivr29c0lkjs7y6zxi1hk0l5mvaqs7k3w7z691l";
   };
 
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ gpsd libcap ];
+  buildInputs = [ gpsd libcap libnl ];
 
   preBuild = ''
     makeFlags="PREFIX=$out PKG_CONFIG=${pkgconfig}/bin/pkg-config"
