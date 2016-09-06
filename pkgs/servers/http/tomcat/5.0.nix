e2d94c1e44e5ca562c1f9376b81a902ab6f6c3c8
Chris Martin <ch.martin@gmail.com>
2016-09-05 20:36:16 -0400

    tomcat 5.0: md5->sha256

--- pkgs/servers/http/tomcat/5.0.nix
+++ pkgs/servers/http/tomcat/5.0.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = https://archive.apache.org/dist/tomcat/tomcat-5/archive/v5.0.27/bin/jakarta-tomcat-5.0.27.tar.gz;
-    md5 = "b802ee042677e284bcf65738c7bdc3b6";
+    sha256 = "0y3xfzb3wch359c6w0a2npxbj3vasrmr5jlvws8m08qn8d5wjgw7";
   };
 
   inherit jdk;
