04fc49a2e1995ae2a10b2f76d55b8acc7cc5eff5
Robert Helgesson <robert@rycee.net>
2016-09-06 11:22:51 +0200

    vp: fix build

--- pkgs/applications/misc/vp/default.nix
+++ pkgs/applications/misc/vp/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, autoconf, automake, SDL, SDL_image }:
+{ stdenv, fetchFromGitHub, autoreconfHook, SDL, SDL_image }:
 
 stdenv.mkDerivation rec {
   name = "vp-${version}";
@@ -11,11 +11,9 @@ stdenv.mkDerivation rec {
     sha256 = "08q6xrxsyj6vj0sz59nix9isqz84gw3x9hym63lz6v8fpacvykdq";
   };
 
-  buildInputs = [ SDL autoconf automake SDL_image ];
+  configureFlags = [ "CFLAGS=-I${SDL_image}/include/SDL" ];
 
-  preConfigure = ''
-    autoreconf -i
-  '';
+  buildInputs = [ SDL autoreconfHook SDL_image ];
 
   meta = with stdenv.lib; {
     homepage = http://brlcad.org/~erik/;
