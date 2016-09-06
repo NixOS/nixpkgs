8a8c1a950162f0451a47912ce124a7a95fd98ada
Michal Rus <m@michalrus.com>
2016-09-06 11:42:10 +0200

    squishyball: init at 19580

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14642,6 +14642,8 @@ in
 
   spideroak = callPackage ../applications/networking/spideroak { };
 
+  squishyball = callPackage ../applications/audio/squishyball { };
+
   ssvnc = callPackage ../applications/networking/remote/ssvnc { };
 
   viber = callPackage ../applications/networking/instant-messengers/viber { };
