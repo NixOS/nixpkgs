f2ddf2a9be551e5c1baa8b653e1677e0f06e812f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-09-06 16:15:22 +0200

    nix: 1.11.3 -> 1.11.4

--- nixos/modules/installer/tools/nix-fallback-paths.nix
+++ nixos/modules/installer/tools/nix-fallback-paths.nix
@@ -1,5 +1,5 @@
 {
-  x86_64-linux = "/nix/store/xryr9g56h8yjddp89d6dw12anyb4ch7c-nix-1.10";
-  i686-linux = "/nix/store/2w92k5wlpspf0q2k9mnf2z42prx3bwmv-nix-1.10";
+  x86_64-linux = "/nix/store/i4mwf2gpvar7dqvlpp5m86llbq3ahbvb-nix-1.11.4";
+  i686-linux = "/nix/store/a3gjrbspb0q4hs3sv5g1y2nza43i8nzv-nix-1.11.4";
+  x86_64-darwin = "/nix/store/7v21yd3qpv0nclcy5cqr5njj9bril12s-nix-1.11.4";
 }
-
