eeebc13e033800bbbe394d358550a4989a2167c6
aszlig <aszlig@redmoonstudios.org>
2016-09-07 11:21:13 +0200

    qtkeychain: 0.4.0 -> 0.7.0
    Upstream changes since version 0.4.0:
    
      * version 0.5.0 (release 2015-05-04):
        - Added support for KWallet5 (KDE5/KF)
      * version 0.6.0 (release 2016-03-18)
        - Added support for the Windows Credential Store
      * version 0.6.1 (release 2016-03-31)
        - Fix KWallet not working (regressions in 0.6.0)
      * version 0.6.2 (release 2016-04-04)
        - KWallet: Fixes a crash when storing passwords, seen on Debian/KDE4
      * version 0.7.0 (release 2016-05-23)
        - Bump SO version due to 0.6 being binary-incompatible to previous
          releases
    
    Tomahawk and owncloud-client depend on this library, both are still
    building fine after this update.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/development/libraries/qtkeychain/default.nix
+++ pkgs/development/libraries/qtkeychain/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "qtkeychain-${version}";
-  version = "0.4.0";
+  version = "0.7.0";
 
   src = fetchFromGitHub {
     owner = "frankosterfeld";
     repo = "qtkeychain";
     rev = "v${version}";
-    sha256 = "10msaylisbwmgpwd59vr4dfgml75kji8mlfwnwq8yp29jikj5amq";
+    sha256 = "04v6ymkw7qd1pf9lwijgqrl89w2hhsnqgz7dm4cdrh8i8dffpn52";
   };
 
   cmakeFlags = [ "-DQT_TRANSLATIONS_DIR=share/qt/translations" ];
