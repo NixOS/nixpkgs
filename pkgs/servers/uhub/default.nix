a84885982944c8e8fda5b251da4e29ccb66db4f8
obadz <obadz-git@obadz.com>
2016-09-07 02:27:39 +0100

    uhub: 0.4.1 -> 0.5.0 (fixes build)
    @ehmry: please have a look so that we can cherry-pick in release-16.09
    and move forward on #18209
    
    (cherry picked from commit 39e197ab1ccad9f1b57c10afe4cfe592a9e40270)
    Signed-off-by: Domen Kožar <domen@dev.si>

--- pkgs/servers/uhub/default.nix
+++ pkgs/servers/uhub/default.nix
@@ -3,13 +3,13 @@
 
 assert tlsSupport -> openssl != null;
 
-let version = "0.4.1"; in
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "uhub-${version}";
+  version = "0.5.0";
 
   src = fetchurl {
     url = "http://www.extatic.org/downloads/uhub/uhub-${version}-src.tar.bz2";
-    sha256 = "1q0n74fb0h5w0k9fhfkznxb4r46qyfb8g2ss3wflivx4l0m1f9x2";
+    sha256 = "1xcqjz20lxikzn96f4f69mqyl9y985h9g0gyc9f7ckj18q22b5j5";
   };
 
   buildInputs = [ cmake sqlite pkgconfig systemd ] ++ stdenv.lib.optional tlsSupport openssl;
@@ -19,14 +19,21 @@ stdenv.mkDerivation {
     "mod_welcome"
     "mod_logging"
     "mod_auth_simple"
-    "mod_auth_sqlite"
     "mod_chat_history"
     "mod_chat_only"
     "mod_topic"
     "mod_no_guest_downloads"
   ];
 
-  patches = [ ./plugin-dir.patch ./systemd.patch ];
+  patches = [
+    ./plugin-dir.patch
+
+    # Fixed compilation on systemd > 210
+    (fetchurl {
+      url = "https://github.com/janvidar/uhub/commit/70f2a43f676cdda5961950a8d9a21e12d34993f8.diff";
+      sha256 = "1jp8fvw6f9jh0sdjml9mahkk6p6b96p6rzg2y601mnnbcdj8y8xp";
+    })
+  ];
 
   cmakeFlags = ''
     -DSYSTEMD_SUPPORT=ON
