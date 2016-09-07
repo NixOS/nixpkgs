84544cd941738c40cc65a4abce616c815b0d6689
mimadrid <mimadrid@ucm.es>
2016-09-07 20:49:12 +0200

    shared-mime-info: 1.5 -> 1.7
    (cherry picked from commit 478c63d469a45b646b7d09a59ec498128050e7db)
    This seems unlikely to introduce problems.

--- pkgs/data/misc/shared-mime-info/default.nix
+++ pkgs/data/misc/shared-mime-info/default.nix
@@ -1,13 +1,13 @@
 {stdenv, fetchurl, pkgconfig, gettext, perl, perlXMLParser, intltool
 , libxml2, glib}:
 
-let version = "1.5"; in
+let version = "1.7"; in
 stdenv.mkDerivation rec {
   name = "shared-mime-info-${version}";
 
   src = fetchurl {
     url = "http://freedesktop.org/~hadess/${name}.tar.xz";
-    sha256 = "1021x95xbkfc5ipx3gi2rdc0y6x2pv36yyzxc5pg6nr6xd02hhfn";
+    sha256 = "0bjd2j1rqrj150mr04j7ib71lfdlgbf235fg8d70g8mszqf7ik7a";
   };
 
   nativeBuildInputs = [
