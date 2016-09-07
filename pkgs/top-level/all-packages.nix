1ebc29d0279f0c6131c802bcbec63388496d7277
Tim Digel <tim.digel@active-group.de>
2016-09-07 08:36:09 +0200

    Changes ec-compiler package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4434,16 +4434,25 @@ in
 
   drumstick = callPackage ../development/libraries/drumstick { };
 
-  ec-compiler = rec {
-    erlang = callPackage ../development/interpreters/erlang/R18.nix { 
-      inherit (darwin.apple_sdk.frameworks) Carbon Cocoa;
-    };
+  #ec-compiler2 = rec {
+  #  erlang = callPackage ../development/interpreters/erlang/R18.nix { 
+  #    inherit (darwin.apple_sdk.frameworks) Carbon Cocoa;
+  #  };
+  #
+  #  elixir = callPackage ../development/interpreters/elixir { };
+  #
+  #  nodejs = callPackage ../development/web/nodejs/v6.nix { 
+  #    libtool = darwin.cctools;
+  #  };
+  #};
 
-    elixir = callPackage ../development/interpreters/elixir { };
-
-    nodejs = callPackage ../development/web/nodejs/v6.nix { 
-      libtool = darwin.cctools;
-    };
+  ec-compiler = with pkgs; buildEnv {
+    name = "ec-compiler";
+    paths = [
+      erlangR18
+      elixir
+      nodejs
+    ];
   };
   
   ecl = callPackage ../development/compilers/ecl { };
