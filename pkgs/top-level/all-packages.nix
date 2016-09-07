d2fc461f6badbfd6cc2b32c42b8e6661c9675afc
Mikael Brockman <mikael@brockman.se>
2016-09-07 17:06:21 +0300

    awslogs: init at 0.7
    fixes #20921

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -475,6 +475,8 @@ in
 
   awscli = pythonPackages.awscli; # Should be moved out of python-packages.nix
 
+  awslogs = callPackage ../tools/admin/awslogs { };
+
   aws_shell = python2Packages.aws_shell; # Should be moved out of python-packages.nix
 
   azure-cli = nodePackages.azure-cli;
