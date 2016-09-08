f0b455101bf979a6f39a0d4f5c3204343411ae99
Franz Pletz <fpletz@fnordicwalking.de>
2016-09-08 12:55:16 +0200

    linuxPackages: default to 4.7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11450,7 +11450,7 @@ in
   };
 
   # The current default kernel / kernel modules.
-  linuxPackages = self.linuxPackages_4_6;
+  linuxPackages = self.linuxPackages_4_7;
   linux = self.linuxPackages.kernel;
 
   # Update this when adding the newest kernel major version!
