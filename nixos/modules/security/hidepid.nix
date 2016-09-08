0dffdaaae52061bf7156be39706c773a37d31b08
Alexander Ried <ried@mytum.de>
2016-09-08 17:05:47 +0200

    security.hidepid: reindent

--- nixos/modules/security/hidepid.nix
+++ nixos/modules/security/hidepid.nix
@@ -3,18 +3,20 @@ with lib;
 
 {
   options = {
-    security.hideProcessInformation = mkEnableOption "" // { description = ''
-      Restrict access to process information to the owning user.  Enabling
-      this option implies, among other things, that command-line arguments
-      remain private.  This option is recommended for most systems, unless
-      there's a legitimate reason for allowing unprivileged users to inspect
-      the process information of other users.
+    security.hideProcessInformation = mkEnableOption "" // {
+      description = ''
+        Restrict access to process information to the owning user.  Enabling
+        this option implies, among other things, that command-line arguments
+        remain private.  This option is recommended for most systems, unless
+        there's a legitimate reason for allowing unprivileged users to inspect
+        the process information of other users.
 
-      Members of the group "proc" are exempt from process information hiding.
-      To allow a service to run without process information hiding, add "proc"
-      to its supplementary groups via
-      <option>systemd.services.&lt;name?&gt;.serviceConfig.SupplementaryGroups</option>.
-    ''; };
+        Members of the group "proc" are exempt from process information hiding.
+        To allow a service to run without process information hiding, add "proc"
+        to its supplementary groups via
+        <option>systemd.services.&lt;name?&gt;.serviceConfig.SupplementaryGroups</option>.
+      '';
+    };
   };
 
   config = mkIf config.security.hideProcessInformation {
