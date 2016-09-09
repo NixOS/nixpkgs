040126092210b5538d9021635a3e0e1507bdc0e0
Théophane Hufschmitt <theophane.hufschmitt@polytechnique.org>
2016-09-09 14:09:57 +0200

    selfoss service: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -471,6 +471,7 @@
   ./services/web-apps/mattermost.nix
   ./services/web-apps/pump.io.nix
   ./services/web-apps/tt-rss.nix
+  ./services/web-apps/selfoss.nix
   ./services/web-servers/apache-httpd/default.nix
   ./services/web-servers/caddy.nix
   ./services/web-servers/fcgiwrap.nix
