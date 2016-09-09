099584a27c48fe9f1d6866da7d884134dac71f00
Joachim Fasting <joachifm@fastmail.fm>
2016-09-09 04:40:14 +0200

    busybox: fix static build
    The static build fails with undefined references to __memcpy_chk when
    the fortify hardening is enabled.

--- pkgs/os-specific/linux/busybox/default.nix
+++ pkgs/os-specific/linux/busybox/default.nix
@@ -33,7 +33,7 @@ stdenv.mkDerivation rec {
     sha256 = "16ii9sqracvh2r1gfzhmlypl269nnbkpvrwa7270k35d3bigk9h5";
   };
 
-  hardeningDisable = [ "format" ];
+  hardeningDisable = [ "format" ] ++ lib.optional enableStatic [ "fortify" ];
 
   patches = [ ./busybox-in-store.patch ];
 
