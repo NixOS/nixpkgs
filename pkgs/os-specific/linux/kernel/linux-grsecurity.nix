91674b75d3b60049dcaa4c59a8aa52b45c3d3131
Joachim Fasting <joachifm@fastmail.fm>
2016-09-10 17:06:42 +0200

    grsecurity: 4.7.2-201608312326 -> 4.7.3-201609072139

--- pkgs/os-specific/linux/kernel/linux-grsecurity.nix
+++ pkgs/os-specific/linux/kernel/linux-grsecurity.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.7.2";
+  version = "4.7.3";
   extraMeta.branch = "4.7";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1234z4wzvrbyzql6cc8i4bq7bbbjgaxhys4fcnqzm14lavk3wjaq";
+    sha256 = "16rr5nj78jh1l46baa0qgzbw44slyqw5ivfn1y7dcxrgb17bvrqc";
   };
 
   kernelPatches = args.kernelPatches;
