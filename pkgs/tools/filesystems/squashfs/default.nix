9ce242b61b91f1ee331758b2d00186b778ff2417
Joachim Fasting <joachifm@fastmail.fm>
2016-09-12 23:11:37 +0200

    squashfsTools: use fetchFromGitHub

--- pkgs/tools/filesystems/squashfs/default.nix
+++ pkgs/tools/filesystems/squashfs/default.nix
@@ -1,10 +1,11 @@
-{ stdenv, fetchgit, zlib, xz }:
+{ stdenv, fetchFromGitHub, zlib, xz }:
 
 stdenv.mkDerivation rec {
   name = "squashfs-4.4dev";
 
-  src = fetchgit {
-    url = https://github.com/plougher/squashfs-tools.git;
+  src = fetchFromGitHub {
+    owner = "plougher";
+    repo = "squashfs-tools";
     sha256 = "059pa2shdysr3zfmwrhq28s12zbi5nyzbpzyaf5lmspgfh1493ks";
     rev = "9c1db6d13a51a2e009f0027ef336ce03624eac0d";
   };
