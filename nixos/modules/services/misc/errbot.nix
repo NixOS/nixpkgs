2faef22cf3072be2b031dd7003067935611d58ae
Robin Gloster <mail@glob.in>
2016-09-12 19:53:15 +0200

    errbot module: multi-instance support

--- nixos/modules/services/misc/errbot.nix
+++ nixos/modules/services/misc/errbot.nix
@@ -2,97 +2,103 @@
 
 with lib;
 
-# FIXME multiple instances
-
 let
   cfg = config.services.errbot;
-  pluginEnv = pkgs.buildEnv {
+  pluginEnv = plugins: pkgs.buildEnv {
     name = "errbot-plugins";
-    paths = cfg.plugins;
+    paths = plugins;
   };
-  configFile = pkgs.writeText "errbot-config.py" ''
+  mkConfigFile = instanceCfg: dataDir: pkgs.writeText "errbot-config.py" ''
     import logging
-    BACKEND = '${cfg.backend}'
-    BOT_DATA_DIR = '${cfg.dataDir}'
-    BOT_EXTRA_PLUGIN_DIR = '${pluginEnv}'
+    BACKEND = '${instanceCfg.backend}'
+    BOT_DATA_DIR = '${dataDir}'
+    BOT_EXTRA_PLUGIN_DIR = '${pluginEnv instanceCfg.plugins}'
 
-    BOT_LOG_LEVEL = logging.${cfg.logLevel}
+    BOT_LOG_LEVEL = logging.${instanceCfg.logLevel}
     BOT_LOG_FILE = False
 
-    BOT_ADMINS = (${concatStringsSep "," (map (name: "'${name}'") cfg.admins)})
+    BOT_ADMINS = (${concatMapStringsSep "," (name: "'${name}'") instanceCfg.admins})
 
-    BOT_IDENTITY = ${cfg.identity}
+    BOT_IDENTITY = ${instanceCfg.identity}
 
-    ${cfg.extraConfig}
+    ${instanceCfg.extraConfig}
   '';
 in {
   options = {
-    services.errbot = {
-      enable = mkEnableOption "errbot";
+    services.errbot.instances = mkOption {
+      default = {};
+      description = "Errbot instance configs";
+      type = types.attrsOf (types.submodule {
+        options = {
+          dataDir = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            description = "Gitlab state directory, logs are stored here.";
+          };
 
-      dataDir = mkOption {
-        type = types.str;
-        default = "/var/lib/errbot/data";
-        description = "Gitlab state directory, logs are stored here.";
-      };
+          plugins = mkOption {
+            type = types.listOf types.path;
+            default = [];
+            description = "Gitlab path for backups.";
+          };
 
-      plugins = mkOption {
-        type = types.nullOr types.path;
-        default = null;
-        description = "Gitlab path for backups.";
-      };
+          logLevel = mkOption {
+            type = types.str;
+            default = "DEBUG";
+            description = "Gitlab database hostname.";
+          };
 
-      logLevel = mkOption {
-        type = types.str;
-        default = "DEBUG";
-        description = "Gitlab database hostname.";
-      };
+          admins = mkOption {
+            type = types.listOf types.str;
+            default = [];
+            description = "Gitlab database user password.";
+          };
 
-      admins = mkOption {
-        type = types.listOf types.str;
-        default = [];
-        description = "Gitlab database user password.";
-      };
+          backend = mkOption {
+            type = types.str;
+            default = "XMPP";
+            description = "Gitlab database name.";
+          };
 
-      backend = mkOption {
-        type = types.str;
-        default = "XMPP";
-        description = "Gitlab database name.";
-      };
+          identity = mkOption {
+            type = types.str;
+            default = "{}";
+            description = "Gitlab database user.";
+          };
 
-      identity = mkOption {
-        type = types.str;
-        default = "{}";
-        description = "Gitlab database user.";
-      };
-
-      extraConfig = mkOption {
-        type = types.lines;
-        default = "";
-        description = "extraConfig";
-      };
+          extraConfig = mkOption {
+            type = types.lines;
+            default = "";
+            description = "extraConfig";
+          };
+        };
+      });
     };
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf (cfg.instances != {}) {
     users.extraUsers.errbot.group = "errbot";
     users.extraGroups.errbot = {};
 
-    systemd.services.errbot = {
+    systemd.services = mapAttrs' (name: instanceCfg: nameValuePair "errbot-${name}" (
+    let
+      dataDir = if !isNull instanceCfg.dataDir then instanceCfg.dataDir else
+        "/var/lib/errbot/${name}";
+    in {
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
       preStart = ''
-        mkdir -p ${cfg.dataDir}
-        chown -R errbot:errbot ${cfg.dataDir}
+        mkdir -p ${dataDir}
+        chown -R errbot:errbot ${dataDir}
       '';
       serviceConfig = {
         Type = "simple";
         User = "errbot";
         Group = "errbot";
         Restart = "on-failure";
-        ExecStart="${pkgs.errbot}/bin/errbot -c ${configFile}";
+        ExecStart="${pkgs.errbot}/bin/errbot -c ${mkConfigFile instanceCfg dataDir}";
         PermissionsStartOnly = true;
       };
-    };
+    })) cfg.instances;
   };
 }
