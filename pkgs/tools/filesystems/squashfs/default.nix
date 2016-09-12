4653160057ceb2a44977c4378b175352409a62a4
Joachim Fasting <joachifm@fastmail.fm>
2016-09-13 01:08:09 +0200

    squashfsTools: optional lz4 support

--- pkgs/tools/filesystems/squashfs/default.nix
+++ pkgs/tools/filesystems/squashfs/default.nix
@@ -1,4 +1,9 @@
-{ stdenv, fetchFromGitHub, zlib, xz }:
+{ stdenv, fetchFromGitHub, zlib, xz
+, lz4 ? null
+, lz4Support ? false
+}:
+
+assert lz4Support -> (lz4 != null);
 
 stdenv.mkDerivation rec {
   name = "squashfs-4.4dev";
@@ -10,13 +15,15 @@ stdenv.mkDerivation rec {
     rev = "9c1db6d13a51a2e009f0027ef336ce03624eac0d";
   };
 
-  buildInputs = [ zlib xz ];
+  buildInputs = [ zlib xz ]
+    ++ stdenv.lib.optional lz4Support lz4;
 
   preBuild = "cd squashfs-tools";
 
   installFlags = "INSTALL_DIR=\${out}/bin";
 
-  makeFlags = "XZ_SUPPORT=1";
+  makeFlags = [ "XZ_SUPPORT=1" ]
+    ++ stdenv.lib.optional lz4Support "LZ4_SUPPORT=1";
 
   meta = {
     homepage = http://squashfs.sourceforge.net/;
