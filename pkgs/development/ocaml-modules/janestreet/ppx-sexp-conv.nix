ff33150bfdfd82984221a9112212df5b77a3940d
Matthew Maurer <matthew.r.maurer@gmail.com>
2016-09-13 18:37:24 -0400

    ppx_sexp_conv: init at 113.33.03

---
+++ pkgs/development/ocaml-modules/janestreet/ppx-sexp-conv.nix
@@ -0,0 +1,14 @@
+{stdenv, buildOcamlJane,
+ ppx_core, ppx_tools, ppx_type_conv, sexplib}:
+
+buildOcamlJane rec {
+  name = "ppx_sexp_conv";
+  hash = "1kgbmlc11w5jhbhmy5n0f734l44zwyry48342dm5qydi9sfzcgq2";
+  propagatedBuildInputs = [ ppx_core ppx_tools ppx_type_conv sexplib];
+
+  meta = with stdenv.lib; {
+    description = "A ppx rewriter that defines an extension node whose value is its source position.";
+    maintainers = [ maintainers.maurer ];
+    license = licenses.asl20;
+  };
+}
