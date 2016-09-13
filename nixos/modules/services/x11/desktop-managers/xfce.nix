aa0fa193734a0bb9c1ee156317008d7409647bdf
Vladimír Čunát <vcunat@gmail.com>
2016-09-13 23:51:25 +0200

    gtk2: move gtk-update-icon-cache to gtk2.out
    ... to be useful for regeneration when building nixos environments.
    Fixes #18536 (hopefully).

--- nixos/modules/services/x11/desktop-managers/xfce.nix
+++ nixos/modules/services/x11/desktop-managers/xfce.nix
@@ -69,7 +69,7 @@ in
     services.xserver.updateDbusEnvironment = true;
 
     environment.systemPackages =
-      [ pkgs.gtk # To get GTK+'s themes.
+      [ pkgs.gtk.out # To get GTK+'s themes and gtk-update-icon-cache
         pkgs.hicolor_icon_theme
         pkgs.tango-icon-theme
         pkgs.shared_mime_info
