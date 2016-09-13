42d08c28821c832e4025b270a14268166c097e64
taku0 <mxxouy6x3m_github@tatapa.org>
2016-09-13 21:12:18 +0900

    flashplayer: 11.2.202.632 -> 11.2.202.635
    (cherry picked from commit 8b6e522bf834354c2b44c84e11f764ab689a3335)

--- pkgs/applications/networking/browsers/mozilla-plugins/flashplayer-11/default.nix
+++ pkgs/applications/networking/browsers/mozilla-plugins/flashplayer-11/default.nix
@@ -70,11 +70,11 @@ let
 in
 stdenv.mkDerivation rec {
   name = "flashplayer-${version}";
-  version = "11.2.202.632";
+  version = "11.2.202.635";
 
   src = fetchurl {
     url = "https://fpdownload.macromedia.com/pub/flashplayer/installers/archive/fp_${version}_archive.zip";
-    sha256 = "0nf2d7jn8g6bp9vilkwwkkh6pm05fg3h73njsn4yvx3285k73lpn";
+    sha256 = "0xlaf6152ksknigrv6fsasscyfnjkxml4nl22apiwzb34nrbzk3m";
   };
 
   nativeBuildInputs = [ unzip ];
