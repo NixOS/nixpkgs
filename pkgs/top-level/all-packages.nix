38c143abe2363aa8770a4879b348f7aeaf3fbca1
Vincent Laporte <Vincent.Laporte@gmail.com>
2016-09-14 19:04:58 +0200

    ocaml-qtest: 2.0.1 -> 2.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5444,9 +5444,7 @@ in
       oasis = ocaml_oasis;
     };
 
-    qtest = callPackage ../development/ocaml-modules/qtest {
-      oasis = ocaml_oasis;
-    };
+    qtest = callPackage ../development/ocaml-modules/qtest { };
 
     re = callPackage ../development/ocaml-modules/re { };
 
