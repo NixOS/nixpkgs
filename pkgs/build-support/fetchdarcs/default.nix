db5c2a0df1c77dc52976de4f0adebbb97bc769f9
rushmorem <rushmore@webenchanter.com>
2016-09-15 01:31:16 +0200

    fetchdarcs: fix CA path

--- pkgs/build-support/fetchdarcs/default.nix
+++ pkgs/build-support/fetchdarcs/default.nix
@@ -1,9 +1,11 @@
-{stdenv, darcs, nix}: {url, rev ? null, context ? null, md5 ? "", sha256 ? ""}:
+{stdenv, darcs, nix, cacert}: {url, rev ? null, context ? null, md5 ? "", sha256 ? ""}:
 
 stdenv.mkDerivation {
   name = "fetchdarcs";
   builder = ./builder.sh;
   buildInputs = [darcs];
+
+  SSL_CERT_FILE="${cacert}/etc/ssl/certs/ca-bundle.crt";
 
   outputHashAlgo = if sha256 == "" then "md5" else "sha256";
   outputHashMode = "recursive";
--- pkgs/development/lisp-modules/lisp-packages.nix
+++ pkgs/development/lisp-modules/lisp-packages.nix
@@ -36,11 +36,11 @@ let lispPackages = rec {
     version = "darcs-2014-11-01";
     description = "Iteration package for Common Lisp";
     deps = [];
-    src = (pkgs.lib.overrideDerivation (pkgs.fetchdarcs {
+    src = pkgs.fetchdarcs {
       url = "https://common-lisp.net/project/iterate/darcs/iterate";
       sha256 = "0gm05s3laiivsqgqjfj1rkz83c2c0jyn4msfgbv6sz42znjpam25";
       context = ./iterate.darcs-context;
-    }) (x: {SSL_CERT_FILE=pkgs.cacert + "/etc/ssl/certs/ca-bundle.crt";}));
+    };
     overrides = x: {
       configurePhase="buildPhase(){ true; }";
     };
@@ -310,10 +310,10 @@ let lispPackages = rec {
     description = "A UTF-8 encoding library";
     deps = [];
     # Source type: darcs
-    src = (pkgs.lib.overrideDerivation (pkgs.fetchdarcs {
+    src = pkgs.fetchdarcs {
       url = ''http://common-lisp.net/project/trivial-utf-8/darcs/trivial-utf-8/'';
       sha256 = "1jz27gz8gvqdmvp3k9bxschs6d5b3qgk94qp2bj6nv1d0jc3m1l1";
-    }) (x: {SSL_CERT_FILE=pkgs.cacert + "/etc/ssl/certs/ca-bundle.crt";}));
+    };
   };
 
   cl-fuse-meta-fs = buildLispPackage rec {
