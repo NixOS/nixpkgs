6a23b16b3eb3638636bb2eebdfe39cf014521c65
Matthew Maurer <matthew.r.maurer@gmail.com>
2016-09-13 21:48:15 -0400

    ocaml-textutils: 112.17.00 -> 112.17.00/113.33.03
    PPX/P4 split

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5408,7 +5408,7 @@ in
       camlp5 = camlp5_transitional;
     };
 
-    textutils = callPackage ../development/ocaml-modules/textutils { };
+    textutils_p4 = callPackage ../development/ocaml-modules/textutils { };
 
     type_conv_108_08_00 = callPackage ../development/ocaml-modules/type_conv/108.08.00.nix { };
     type_conv_109_60_01 = callPackage ../development/ocaml-modules/type_conv/109.60.01.nix { };
@@ -5594,6 +5594,8 @@ in
     core = callPackage ../development/ocaml-modules/janestreet/core.nix {};
 
     re2 = callPackage ../development/ocaml-modules/janestreet/re2.nix {};
+
+    textutils = callPackage ../development/ocaml-modules/janestreet/textutils.nix {};
   };
 
   ocamlPackages = recurseIntoAttrs ocamlPackages_4_01_0;
