ebc5b933bf30d171383ebe32afcbef061733f15d
Matthew Maurer <matthew.r.maurer@gmail.com>
2016-09-14 16:08:02 -0400

    async_unix: 112.24.00 -> 112.24.00/113.33.03
    PPX/P4 split

--- pkgs/development/ocaml-modules/async_extra/default.nix
+++ pkgs/development/ocaml-modules/async_extra/default.nix
@@ -1,4 +1,4 @@
-{stdenv, buildOcaml, fetchurl, async_kernel_p4, async_unix,
+{stdenv, buildOcaml, fetchurl, async_kernel_p4, async_unix_p4,
  bin_prot_p4, core_p4, custom_printf, fieldslib_p4, herelib, pa_ounit,
  pipebang, pa_test, sexplib_p4}:
 
@@ -14,7 +14,7 @@ buildOcaml rec {
   };
 
   buildInputs = [ pa_test pa_ounit ];
-  propagatedBuildInputs = [ async_kernel_p4 async_unix core_p4 bin_prot_p4 custom_printf
+  propagatedBuildInputs = [ async_kernel_p4 async_unix_p4 core_p4 bin_prot_p4 custom_printf
                             fieldslib_p4 herelib pipebang sexplib_p4 ];
 
   meta = with stdenv.lib; {
