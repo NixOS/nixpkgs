46790f360d55bb3940dba47809d0cc6ec917b24b
Franz Pletz <fpletz@fnordicwalking.de>
2016-09-14 10:02:55 +0200

    perlPackages.WWWCurl: fix build due to curl 5.10.3
    The define CURL_STRICTER is only set if CURL_NO_OLDIES is set, but that breaks
    compatibility with this package. Defining CURL_STRICTER is enough to fix the
    build.

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -14167,6 +14167,7 @@ let self = _self // overrides; _self = with self; {
         substituteInPlace Makefile.PL --replace '"cpp"' '"gcc -E"'
         substituteInPlace Makefile.PL --replace '_LASTENTRY\z' '_LASTENTRY\z|CURL_DID_MEMORY_FUNC_TYPEDEFS\z'
       '';
+    NIX_CFLAGS_COMPILE = "-DCURL_STRICTER";
     doCheck = false; # performs network access
   };
 
