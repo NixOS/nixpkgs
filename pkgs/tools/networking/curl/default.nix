ed27ce5a461d8fbda444d0dd1577a571d542913d
Franz Pletz <fpletz@fnordicwalking.de>
2016-09-14 08:25:51 +0200

    curl: 7.50.1 -> 7.50.3, fix CVE-2016-7167
    (cherry picked from commit 4ecd0e74348d25f9942b6eda32ddcd5926b5a379)

--- pkgs/tools/networking/curl/default.nix
+++ pkgs/tools/networking/curl/default.nix
@@ -18,11 +18,11 @@ assert scpSupport -> libssh2 != null;
 assert c-aresSupport -> c-ares != null;
 
 stdenv.mkDerivation rec {
-  name = "curl-7.47.1";
+  name = "curl-7.50.3";
 
   src = fetchurl {
     url = "http://curl.haxx.se/download/${name}.tar.bz2";
-    sha256 = "13z9gba3q2ybp50z0gdkzhwcx9m0i7qkvm278yz4pql2jfml7inx";
+    sha256 = "1v6q83qsrf7dgp3y5fa5vkppgqyy82pnsk8z9b4047b6fvclfwvv";
   };
 
   nativeBuildInputs = [ pkgconfig perl ];
