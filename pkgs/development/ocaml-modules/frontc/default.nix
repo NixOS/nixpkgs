a478af433c44a55f65e0ed56fc19a93f631b817d
Matthew Maurer <matthew.r.maurer@gmail.com>
2016-09-14 21:52:58 -0400

    frontc: trim down unneeded components

--- pkgs/development/ocaml-modules/frontc/default.nix
+++ pkgs/development/ocaml-modules/frontc/default.nix
@@ -1,4 +1,4 @@
-{stdenv, buildOcaml, fetchurl}:
+{lib, buildOcaml, fetchurl}:
 
 buildOcaml rec {
   name = "Frontc";
@@ -9,7 +9,7 @@ buildOcaml rec {
     sha256 = "16dz153s92dgbw1rrfwbhscy73did87kfmjwyh3qpvs748h1sc4g";
   };
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     homepage = https://www.irit.fr/recherches/ARCHI/MARCH/rubrique.php3?id_rubrique=61;
     description = "C Parsing Library";
     license = licenses.lgpl21;
@@ -19,11 +19,6 @@ buildOcaml rec {
   meta_file = fetchurl {
     url = https://raw.githubusercontent.com/ocaml/opam-repository/0f0e610f6499bdf0151e4170411b4f05e4d076d4/packages/FrontC/FrontC.3.4/files/META;
     sha256 = "1flhvwr01crn7d094kby0418s1m4198np85ymjp3b4maz0n7m2mx";
-  };
-
-  opam_patch = fetchurl {
-    url = https://raw.githubusercontent.com/ocaml/opam-repository/0f0e610f6499bdf0151e4170411b4f05e4d076d4/packages/FrontC/FrontC.3.4/files/opam.patch;
-    sha256 = "0xf83ixx0mf3mznwpwp2mjflii0njdzikhhfxpnms7vhnnmlfzy5";
   };
 
   makeFlags = "PREFIX=$(out) OCAML_SITE=$(OCAMLFIND_DESTDIR)";
