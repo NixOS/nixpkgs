164036d728eea2ee487500816ccfc95d3042992b
Fernando J Pando <fernando.pando@stelligent.com>
2016-09-15 19:16:23 -0400

    buildbot: 0.9.0rc2 -> 0.9.0rc3
    Tested on Linux
    Tested Binary Execution

--- pkgs/development/tools/build-managers/buildbot/default.nix
+++ pkgs/development/tools/build-managers/buildbot/default.nix
@@ -7,10 +7,10 @@
 pythonPackages.buildPythonApplication (rec {
   name = "${pname}-${version}";
   pname = "buildbot";
-  version = "0.9.0rc2";
+  version = "0.9.0rc3";
   src = fetchurl {
     url = "mirror://pypi/b/${pname}/${name}.tar.gz";
-    sha256 = "06xzbjzji3by4hldwr850fc989ymsfl2s8nil21klv2g00dgmcmk";
+    sha256 = "18238n9l0pfivb23csy5lnkx91fcm4jgmhxn03f2nfnp00rlppzb";
   };
 
   buildInputs = with pythonPackages; [
@@ -59,15 +59,6 @@ pythonPackages.buildPythonApplication (rec {
   ] ++ plugins;
 
   preInstall = ''
-    # buildbot tries to import 'buildslaves' but does not
-    # include the module in it's package, so get rid of those references
-    sed -i.bak -e '66,$d' buildbot/test/__init__.py
-    sed -i.bak -e '506,$d' buildbot/test/unit/test_worker_base.py
-    sed -i.bak -e '648,$d' buildbot/test/unit/test_worker_ec2.py
-    sed -i.bak -e '289,$d' buildbot/test/unit/test_worker_libvirt.py
-    sed -i.bak -e '190,$d' buildbot/test/unit/test_worker_openstack.py
-    sed -i.bak -e '60,84d' buildbot/test/integration/test_configs.py
-
     # writes out a file that can't be read properly
     sed -i.bak -e '69,84d' buildbot/test/unit/test_www_config.py
   '';
