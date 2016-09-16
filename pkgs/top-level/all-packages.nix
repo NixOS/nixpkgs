d6f20348f3f7f2f10e9b08551c908ac72146bf48
Ram Kromberg <ramkromberg@mail.com>
2016-09-16 17:40:19 +0300

    mlterm: 3.3.8 -> 3.7.2 (#18645)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15603,7 +15603,9 @@ in
 
   xterm = callPackage ../applications/misc/xterm { };
 
-  mlterm = callPackage ../applications/misc/mlterm { };
+  mlterm = callPackage ../applications/misc/mlterm {
+    vte = gnome3.vte_290;
+  };
 
   finalterm = callPackage ../applications/misc/finalterm { };
 
