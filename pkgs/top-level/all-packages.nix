c61d3730b7095e2aab2813e8a29428e13d8f38a2
Vladimír Čunát <vcunat@gmail.com>
2016-09-16 21:43:40 +0200

    ghdl_mcode: get 32-bit version even on 64-bit

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4702,7 +4702,7 @@ in
     profiledCompiler = false;
   });
 
-  ghdl_mcode = callPackage ../development/compilers/ghdl { };
+  ghdl_mcode = callPackage_i686 ../development/compilers/ghdl { };
 
   gcl = callPackage ../development/compilers/gcl {
     gmp = gmp4;
