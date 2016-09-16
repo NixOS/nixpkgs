9715f747088452235149bda1742d5c24012d21b9
Ram Kromberg <ramkromberg@mail.com>
2016-09-16 23:57:59 +0300

    mlterm: fix cairo and multiple other features

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15604,7 +15604,9 @@ in
   xterm = callPackage ../applications/misc/xterm { };
 
   mlterm = callPackage ../applications/misc/mlterm {
-    vte = gnome3.vte_290;
+    vte = gnome3.vte;
+    libssh2 = null;
+    openssl = null;
   };
 
   finalterm = callPackage ../applications/misc/finalterm { };
