ea206c4a98a195285dac88cc7b345dcb12f6ba34
Herwig Hochleitner <herwig@bendlas.net>
2016-09-16 22:09:39 +0200

    systemtap: clean up as per feedback
    - shorter description
    - move systemtap into linuxPackages
      this is only necessary for the wrapper
      systemtap also has a dyninsn backend to instrument userspace apps
      we could experiment with other platforms, using this
    - get rid of stow build dependency
    - update version
    - minor cleanup steps

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11573,6 +11573,8 @@ in
 
     sysdig = callPackage ../os-specific/linux/sysdig {};
 
+    systemtap = callPackage ../development/tools/profiling/systemtap { };
+
     tp_smapi = callPackage ../os-specific/linux/tp_smapi { };
 
     v86d = callPackage ../os-specific/linux/v86d { };
@@ -15129,8 +15131,6 @@ in
 
   # linux only by now
   synergy = callPackage ../applications/misc/synergy { };
-
-  systemtap = callPackage ../development/tools/profiling/systemtap { };
 
   tabbed = callPackage ../applications/window-managers/tabbed {
     # if you prefer a custom config, write the config.h in tabbed.config.h
