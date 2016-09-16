9ffe2e784f81e82df96b8587c063686fe90e2c2f
Domen Kožar <domen@dev.si>
2016-09-16 11:12:19 +0200

    lttng-modules: broken on chromiumos
    (cherry picked from commit 3f4d94a4c2d422836c07ed1206b454cd7b681f01)
    Signed-off-by: Domen Kožar <domen@dev.si>

--- pkgs/os-specific/linux/lttng-modules/default.nix
+++ pkgs/os-specific/linux/lttng-modules/default.nix
@@ -31,7 +31,8 @@ stdenv.mkDerivation rec {
     maintainers = [ maintainers.bjornfor ];
     broken =
       (builtins.compareVersions kernel.version "3.18" == -1) ||
-      (kernel.features.grsecurity or false);
+      (kernel.features.grsecurity or false) ||
+      (kernel.features.chromiumos or false);
   };
 
 }
