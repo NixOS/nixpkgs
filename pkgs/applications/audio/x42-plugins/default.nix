9e7f61c9f19491c84d189053965c71b637f17268
Moritz Ulrich <moritz@tarn-vedra.de>
2016-09-16 10:55:50 +0200

    x42-plugins: Fix unpacking and formatting
    For some reason, adding `xz` to `buildInputs` caused `unpackPhase` to
    fail.

--- pkgs/applications/audio/x42-plugins/default.nix
+++ pkgs/applications/audio/x42-plugins/default.nix
@@ -1,6 +1,6 @@
-{ stdenv, fetchurl, fetchgit, ftgl, freefont_ttf, libjack2, mesa_glu, pkgconfig
-, libltc, libsndfile, libsamplerate, xz
-, lv2, mesa, gtk2, cairo, pango, fftwFloat, zita-convolver }:
+{ stdenv, fetchurl, pkgconfig, fetchgit
+, libltc, libsndfile, libsamplerate, ftgl, freefont_ttf, libjack2
+, mesa_glu, lv2, mesa, gtk2, cairo, pango, fftwFloat, zita-convolver }:
 
 stdenv.mkDerivation rec {
   version = "20160619";
@@ -11,15 +11,22 @@ stdenv.mkDerivation rec {
     sha256 = "1ald0c5xbfkdq6g5xwyy8wmbi636m3k3gqrq16kbh46g0kld1as9";
   };
 
-  buildInputs = [ xz mesa_glu ftgl freefont_ttf libjack2 libltc libsndfile libsamplerate lv2 mesa gtk2 cairo pango fftwFloat pkgconfig  zita-convolver];
+  buildInputs = [
+    mesa_glu ftgl freefont_ttf libjack2 libltc libsndfile libsamplerate
+    lv2 mesa gtk2 cairo pango fftwFloat pkgconfig  zita-convolver
+  ];
 
-  makeFlags = [ "PREFIX=$(out)" "FONTFILE=${freefont_ttf}/share/fonts/truetype/FreeSansBold.ttf" "LIBZITACONVOLVER=${zita-convolver}/include/zita-convolver.h" ];
+  makeFlags = [
+    "PREFIX=$(out)"
+    "FONTFILE=${freefont_ttf}/share/fonts/truetype/FreeSansBold.ttf"
+    "LIBZITACONVOLVER=${zita-convolver}/include/zita-convolver.h"
+  ];
 
-  meta = with stdenv.lib;
-    { description = "Collection of LV2 plugins by Robin Gareus";
-      homepage = https://github.com/x42/x42-plugins;
-      maintainers = with maintainers; [ magnetophon ];
-      license = licenses.gpl2;
-      platforms = platforms.linux;
-    };
+  meta = with stdenv.lib; {
+    description = "Collection of LV2 plugins by Robin Gareus";
+    homepage = https://github.com/x42/x42-plugins;
+    maintainers = with maintainers; [ magnetophon ];
+    license = licenses.gpl2;
+    platforms = platforms.linux;
+  };
 }
