7871ca4024d24eaac35030dd642a69b76bfaf302
Alexander Ried <ried@mytum.de>
2016-09-18 21:32:28 +0200

    gtk+3: 3.20.9 -> 3.22.0

--- pkgs/development/libraries/gtk+/3.x.nix
+++ pkgs/development/libraries/gtk+/3.x.nix
@@ -12,8 +12,8 @@ assert cupsSupport -> cups != null;
 with stdenv.lib;
 
 let
-  ver_maj = "3.20";
-  ver_min = "9";
+  ver_maj = "3.22";
+  ver_min = "0";
   version = "${ver_maj}.${ver_min}";
 in
 stdenv.mkDerivation rec {
@@ -21,7 +21,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/gtk+/${ver_maj}/gtk+-${version}.tar.xz";
-    sha256 = "05xcwvy68p7f4hdhi4bgdm3aycvqqr4pr5kkkr8ba91l5yx0k9l3";
+    sha256 = "0d21qx0j7awm69jlfx7z7vqddbqarylqifl2bfbfrv779dcbpl48";
   };
 
   outputs = [ "out" "dev" ];
