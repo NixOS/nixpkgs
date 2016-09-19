613eb006e2029969cb81e2be78ade1de94b3c5dd
Peter Simons <simons@cryp.to>
2016-09-19 15:41:45 +0200

    nbd: update to version 3.14
    (cherry picked from commit 545040811b1fd20354d8d8e88447774c3fa61237)

--- pkgs/tools/networking/nbd/default.nix
+++ pkgs/tools/networking/nbd/default.nix
@@ -1,12 +1,14 @@
 { stdenv, fetchurl, pkgconfig, glib }:
 
 stdenv.mkDerivation rec {
-  name = "nbd-3.12.1";
+  name = "nbd-3.14";
 
   src = fetchurl {
     url = "mirror://sourceforge/nbd/${name}.tar.xz";
-    sha256 = "1pkkid657zgjymwxv3fm32cxnq9llfz29rl15vp6mn42vnzbj1di";
+    sha256 = "0cc6wznvkgjv0fxsj3diy92qfsjrsw92m7yq13f044qarh726gad";
   };
+
+  patches = [ ./dont-run-make-in-broken-systemd-subdir.patch ];
 
   buildInputs =
     [ pkgconfig glib ]
@@ -17,8 +19,7 @@ stdenv.mkDerivation rec {
     cp README.md "$out/share/doc/${name}/"
   '';
 
-  # The test suite doesn't succeed in chroot builds.
-  doCheck = false;
+  doCheck = true;
 
   # Glib calls `clock_gettime', which is in librt. Linking that library
   # here ensures that a proper rpath is added to the executable so that
