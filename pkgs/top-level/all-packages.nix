b26dff4ea5c7a3a9ee0f885fbc26c5492c6445c3
Octavian Cerna <octavian.cerna@gmail.com>
2016-09-20 22:19:36 +0300

    nvme-cli: init at 0.9

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11602,6 +11602,8 @@ in
 
   numad = callPackage ../os-specific/linux/numad { };
 
+  nvme-cli = callPackage ../os-specific/linux/nvme-cli { };
+
   open-vm-tools = callPackage ../applications/virtualization/open-vm-tools {
     inherit (gnome2) gtk gtkmm;
   };
