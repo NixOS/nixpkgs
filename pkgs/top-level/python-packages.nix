60a9051723dda262afcfc69e2085d2bec091e952
Kirill Boltaev <aske@fmap.me>
2016-09-21 17:30:52 +0300

    pythonPackages.python-Levenshtein: init at 0.12.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12842,6 +12842,26 @@ in modules // {
     ];
   };
 
+  python-Levenshtein = buildPythonPackage rec {
+    name = "python-Levenshtein-${version}";
+    version = "0.12.0";
+
+    src = pkgs.fetchurl {
+      url = "mirror://pypi/p/python-Levenshtein/${name}.tar.gz";
+      sha256 = "1c9ybqcja31nghfcc8xxbbz9h60s9qi12b9hr4jyl69xbvg12fh3";
+    };
+
+    # No tests included in archive
+    doCheck = false;
+
+    meta = {
+      description = "Functions for fast computation of Levenshtein distance and string similarity";
+      homepage    = "https://github.com/ztane/python-Levenshtein";
+      license     = licenses.gpl2;
+      maintainers = with maintainers; [ aske ];
+    };
+  };
+
   libcloud = buildPythonPackage (rec {
     name = "libcloud-0.18.0";
 
