4a2a45ab36ee2c9e052fda1b6dbf36dbedc46e62
rushmorem <rushmore@webenchanter.com>
2016-09-22 00:44:32 +0200

    coredns: init at 001
    (cherry picked from commit b93b37cf0a9c9e4fb8f3bf87013085df832a22bc)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10863,6 +10863,8 @@ in
 
   conntrack_tools = callPackage ../os-specific/linux/conntrack-tools { };
 
+  coredns = callPackage ../servers/dns/coredns { };
+
   cpufrequtils = callPackage ../os-specific/linux/cpufrequtils { };
 
   cryopid = callPackage ../os-specific/linux/cryopid { };
