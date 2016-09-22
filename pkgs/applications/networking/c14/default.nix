4614d056016940d2b090fa6f837bf7d22dbb306e
Alexandre Px <al@px.io>
2016-09-22 22:04:10 +0200

    change date format

--- pkgs/applications/networking/c14/default.nix
+++ pkgs/applications/networking/c14/default.nix
@@ -2,7 +2,7 @@
 
 buildGoPackage rec {
   name = "c14-cli-${version}";
-  version = "20160909-${stdenv.lib.strings.substring 0 7 rev}";
+  version = "09092016-${stdenv.lib.strings.substring 0 7 rev}";
   rev = "e7c7c3cb214fd06df63530a4e861210e7a0a1b6c";
 
   goPackagePath = "github.com/online-net/c14-cli";
