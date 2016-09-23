a3c26fec3221af2e217bd13c82f035c0a8c0f581
Anthony Cowley <acowley@gmail.com>
2016-09-23 00:31:58 -0400

    redprl: init at 2016-09-22

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17471,6 +17471,8 @@ in
 
   putty = callPackage ../applications/networking/remote/putty { };
 
+  redprl = callPackage ../applications/science/logic/redprl { };
+
   retroarchBare = callPackage ../misc/emulators/retroarch { };
 
   retroarch = wrapRetroArch { retroarch = retroarchBare; };
