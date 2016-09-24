31568f1bac939ce88111e7132ed0f5d01f30d98d
Franz Pletz <fpletz@fnordicwalking.de>
2016-09-25 00:07:34 +0200

    linux: 3.18.40 -> 3.18.42
    (cherry picked from commit 17402fc4a3a9a3ef25fc8f8e36a2f1e85c7cac40)

--- pkgs/os-specific/linux/kernel/linux-3.18.nix
+++ pkgs/os-specific/linux/kernel/linux-3.18.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.18.40";
+  version = "3.18.42";
   extraMeta.branch = "3.18";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0x94lp4vkw1472m8sq308r8aw96nirkwppkjvqi2240ayw89d5jb";
+    sha256 = "0ymimvy8kp2a2x0h31m3sipwx1h80576ws04k3cl49wgcgjwpwn4";
   };
 
   kernelPatches = args.kernelPatches;
