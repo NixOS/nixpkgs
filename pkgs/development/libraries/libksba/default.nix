62e8c308d1f245e66e43bc4f952eb07b61f4a834
Franz Pletz <fpletz@fnordicwalking.de>
2016-09-24 19:14:02 +0200

    libksba: 1.3.4 -> 1.3.5

--- pkgs/development/libraries/libksba/default.nix
+++ pkgs/development/libraries/libksba/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, libgpgerror }:
 
 stdenv.mkDerivation rec {
-  name = "libksba-1.3.4";
+  name = "libksba-1.3.5";
 
   src = fetchurl {
     url = "mirror://gnupg/libksba/${name}.tar.bz2";
-    sha256 = "0kxdb02z41cwm1xbwfwj9nbc0dzjhwyq8c475mlhhmpcxcy8ihpn";
+    sha256 = "0h53q4sns1jz1pkmhcz5wp9qrfn9f5g9i3vjv6dafwzzlvblyi21";
   };
 
   outputs = [ "out" "dev" "doc" ];
