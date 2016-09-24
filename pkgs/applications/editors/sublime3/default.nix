f65f6a766808ba7d406497112760a3ee065cb389
zimbatm <zimbatm@zimbatm.com>
2016-09-24 14:52:58 +0100

    sublimetext3: 3124 -> 3126

--- pkgs/applications/editors/sublime3/default.nix
+++ pkgs/applications/editors/sublime3/default.nix
@@ -6,7 +6,7 @@ assert stdenv.system == "i686-linux" || stdenv.system == "x86_64-linux";
 assert gksuSupport -> gksu != null;
 
 let
-  build = "3124";
+  build = "3126";
   libPath = stdenv.lib.makeLibraryPath [glib xorg.libX11 gtk2 cairo pango];
   redirects = [ "/usr/bin/pkexec=${pkexecPath}" ]
     ++ stdenv.lib.optional gksuSupport "/usr/bin/gksudo=${gksu}/bin/gksudo";
@@ -20,13 +20,13 @@ in let
         fetchurl {
           name = "sublimetext-${build}.tar.bz2";
           url = "https://download.sublimetext.com/sublime_text_3_build_${build}_x32.tar.bz2";
-          sha256 = "0ijhxddjpswqr8v1bjv3vqfskdcq4k9m52ykpf7ns880n94gdz95";
+          sha256 = "0acff4wj1s61x3xszdd93lkhaqa26lb7ryqdxnbphxzhf2jfzzwj";
         }
       else
         fetchurl {
           name = "sublimetext-${build}.tar.bz2";
           url = "https://download.sublimetext.com/sublime_text_3_build_${build}_x64.tar.bz2";
-          sha256 = "0ysivcz0fl2drxqlhacnq7jj3hr7cn59vnvjkrrpwsrrc2bc0rl4";
+          sha256 = "0ykj33fq86iv7f9zx76h90pl9y86iri0idhlj09a6prhk8p17nqq";
         };
 
     dontStrip = true;
