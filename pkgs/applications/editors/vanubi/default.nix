c82b9738fbc747d55445b369177042f21918b472
Alexander Ried <ried@mytum.de>
2016-09-25 19:28:05 +0200

    vanubi: use old vala
    this commit should be reverted once vanubi is updated to compile with current vala

--- pkgs/applications/editors/vanubi/default.nix
+++ pkgs/applications/editors/vanubi/default.nix
@@ -1,5 +1,7 @@
 { stdenv, fetchurl, pkgconfig, which, autoconf, automake
-, libtool, gnome3, libwnck3, asciidoc, python3Packages }:
+, libtool, gnome3, libwnck3, asciidoc, python3Packages
+, vala_0_28 # until vanubi is updated, then use vala from gnome package set
+}:
 
 stdenv.mkDerivation rec {
   name = "vanubi-${version}";
@@ -10,9 +12,9 @@ stdenv.mkDerivation rec {
     sha256 = "145zxgaky5bcq5bxm4z7h0pvviq7k1nrgnf40q6nax6ik616ybjq";
   };
 
-  buildInputs = [ pkgconfig which autoconf automake
+  buildInputs = [ pkgconfig which autoconf automake vala_0_28
                   libtool libwnck3 asciidoc python3Packages.pygments
-                ] ++ (with gnome3; [ gtksourceview glib gtk3 vte_290 vala ]);
+                ] ++ (with gnome3; [ gtksourceview glib gtk3 vte_290 ]);
 
   configureScript = "./autogen.sh";
 
