8a02c035a9d5d19a222b734672b8733e7ab655ef
Ram Kromberg <ramkromberg@mail.com>
2016-09-25 22:34:35 +0300

    giflib: 5.1.0 -> 5.1.4 (#18911)
    (cherry picked from commit ff76c7d3caca3cdc059e4066e515ed0e001845f4)

--- pkgs/development/libraries/giflib/5.1.nix
+++ pkgs/development/libraries/giflib/5.1.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl, xmlto, docbook_xml_dtd_412, docbook_xsl, libxml2 }:
 
 stdenv.mkDerivation {
-  name = "giflib-5.1.0";
+  name = "giflib-5.1.4";
   src = fetchurl {
-    url = mirror://sourceforge/giflib/giflib-5.1.0.tar.bz2;
-    sha256 = "06wd32akyawppar9mqdvyhcw47ssdfcj39lryim2w4v83i7nkv2s";
+    url = mirror://sourceforge/giflib/giflib-5.1.4.tar.bz2;
+    sha256 = "1md83dip8rf29y40cm5r7nn19705f54iraz6545zhwa6y8zyq9yz";
   };
 
   buildInputs = [ xmlto docbook_xml_dtd_412 docbook_xsl libxml2 ];
