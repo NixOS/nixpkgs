3edef544e4243088fb9d578d8400507970152eac
Frederik Rietdijk <fridh@fridh.nl>
2016-09-25 23:59:43 +0200

    libgpod: do not use top-level python packages

--- pkgs/development/libraries/libgpod/default.nix
+++ pkgs/development/libraries/libgpod/default.nix
@@ -1,10 +1,12 @@
 {stdenv, lib, fetchurl, gettext, perl, perlXMLParser, intltool, pkgconfig, glib,
   libxml2, sqlite, libusb1, zlib, sg3_utils, gdk_pixbuf, taglib,
-  libimobiledevice, python, pygobject2, mutagen,
+  libimobiledevice, pythonPackages, mutagen,
   monoSupport ? true, mono, gtk-sharp-2_0
 }:
 
-stdenv.mkDerivation rec {
+let
+  inherit (pythonPackages) python pygobject2;
+in stdenv.mkDerivation rec {
   name = "libgpod-0.8.3";
   src = fetchurl {
     url = "mirror://sourceforge/gtkpod/${name}.tar.bz2";
