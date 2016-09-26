NixOS/nixpkgs

commit 12bd2310ece6a4911bea5dab0b39c6c28b826886

getmail: 4.50.0 -> 4.51.0

pkgs/tools/networking/getmail/default.nix
@@ -1,13 +1,13 @@
-{ stdenv, fetchurl, pythonPackages }:
+{ stdenv, fetchurl, buildPythonApplication }:
 
-pythonPackages.buildPythonApplication rec {
-  version = "4.50.0";
+buildPythonApplication rec {
+  version = "4.51.0";
   name = "getmail-${version}";
   namePrefix = "";
 
   src = fetchurl {
     url = "http://pyropus.ca/software/getmail/old-versions/${name}.tar.gz";
-    sha256 = "1hcb5079mkcx3gglfycrhglrgg4jsa499br50yjrh9sal6wpgg7w";
+    sha256 = "1nzdjm62lrg7qbh8xz2irj9wxf2861s3ld6zd5fbcgpjy3v588mw";
   };
 
   doCheck = false;

commit 9e2c48994a06d79ba226a875f72e93449f24a2fe

getmail: pass in pythonPackages again
broken by 12bd2310ece6a4911bea5dab0b39c6c28b826886

pkgs/tools/networking/getmail/default.nix
@@ -1,6 +1,6 @@
-{ stdenv, fetchurl, buildPythonApplication }:
+{ stdenv, fetchurl, pythonPackages }:
 
-buildPythonApplication rec {
+pythonPackages.buildPythonApplication rec {
   version = "4.51.0";
   name = "getmail-${version}";
   namePrefix = "";