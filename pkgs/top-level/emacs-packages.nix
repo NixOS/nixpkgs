4a92bd850bd8d3c438d1f47efa881f0499e2f120
Thomas Tuegel <ttuegel@gmail.com>
2016-09-26 07:39:26 -0500

    emacs-epl: 20140823 -> 0.8

--- pkgs/top-level/emacs-packages.nix
+++ pkgs/top-level/emacs-packages.nix
@@ -213,25 +213,6 @@ let
     };
   };
 
-  epl = melpaBuild rec {
-    pname   = "epl";
-    version = "20140823";
-    src = fetchFromGitHub {
-      owner  = "cask";
-      repo   = pname;
-      rev    = "63c78c08e345455f3d4daa844fdc551a2c18024e";
-      sha256 = "04a2aq8dj2cmy77vw142wcmnjvqdbdsp6z0psrzz2qw0b0am03li";
-    };
-    meta = {
-      description = "Emacs Package Library";
-      longDescription = ''
-        The purpose of this library is to wrap all the quirks and hassle of
-        package.el into a sane API.
-      '';
-      license = gpl3Plus;
-    };
-  };
-
   evil-god-state = melpaBuild rec {
     pname   = "evil-god-state";
     version = "20140830";
