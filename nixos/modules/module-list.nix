2af70511974a79814e88c35d0172be5d502ec0c3
aszlig <aszlig@redmoonstudios.org>
2016-09-26 21:18:06 +0200

    nixos/offlineimap: Move to services/mail
    The services/networking directory is already quite polluted and the
    first point where I was looking for the offlineimap module was in
    services/mail and didn't find it there.
    
    Offlineimap already has IMAP in its name and clearly belongs to the
    "mail" category so let's move it there.
    
    Tested by evaluating a configuration with services.offlineimap enabled.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>
    Cc: @DamienCassou

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -210,6 +210,7 @@
   ./services/mail/freepops.nix
   ./services/mail/mail.nix
   ./services/mail/mlmmj.nix
+  ./services/mail/offlineimap.nix
   ./services/mail/opendkim.nix
   ./services/mail/opensmtpd.nix
   ./services/mail/postfix.nix
@@ -382,7 +383,6 @@
   ./services/networking/ntopng.nix
   ./services/networking/ntpd.nix
   ./services/networking/nylon.nix
-  ./services/networking/offlineimap.nix
   ./services/networking/oidentd.nix
   ./services/networking/openfire.nix
   ./services/networking/openntpd.nix
