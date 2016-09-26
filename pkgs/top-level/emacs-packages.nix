c785a709c5a09e525d24c6da76ae70ed223afa3e
Thomas Tuegel <ttuegel@gmail.com>
2016-09-26 07:44:39 -0500

    emacs-exec-path-from-shell: 20141022 -> 1.11

--- pkgs/top-level/emacs-packages.nix
+++ pkgs/top-level/emacs-packages.nix
@@ -229,21 +229,6 @@ let
     };
   };
 
-  exec-path-from-shell = melpaBuild rec {
-    pname   = "exec-path-from-shell";
-    version = "20141022";
-    src = fetchFromGitHub {
-      owner  = "purcell";
-      repo   = pname;
-      rev    = "e4af0e9b44738e7474c89ed895200b42e6541515";
-      sha256 = "0lxikiqf1jik88lf889q4f4f8kdgg3npciz298x605nhbfd5snbd";
-    };
-    meta = {
-      description = "Slurp environment variables from shell to Emacs";
-      license = gpl3Plus;
-    };
-  };
-
   expand-region = melpaBuild rec {
     pname   = "expand-region";
     version = "20150902";
