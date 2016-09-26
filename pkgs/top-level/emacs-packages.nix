b690d4158049ab85c4d6732b2b4923299ab18aaa
Thomas Tuegel <ttuegel@gmail.com>
2016-09-26 07:37:53 -0500

    emacs-dash-functional: 2.11.0 -> 2.13.0

--- pkgs/top-level/emacs-packages.nix
+++ pkgs/top-level/emacs-packages.nix
@@ -151,23 +151,6 @@ let
     };
   };
 
-  dash-functional = melpaBuild rec {
-    pname = "dash-functional";
-      version = "2.11.0";
-      src = fetchFromGitHub {
-      owner  = "magnars";
-      repo   = "dash.el";
-      rev    = version;
-      sha256 = "02gfrcda7gj3j5yx71dz40xylrafl4pcaj7bgfajqi9by0w2nrnx";
-    };
-    packageRequires = [ dash ];
-    files = [ "dash-functional.el" ];
-    meta = {
-      description = "Collection of useful combinators for Emacs Lisp";
-      license = gpl3Plus;
-    };
-  };
-
   diminish = melpaBuild rec {
     pname   = "diminish";
     version = "0.45";
