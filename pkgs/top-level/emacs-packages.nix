0f312f382576de28a834de048bedbffaf15f5fc3
Thomas Tuegel <ttuegel@gmail.com>
2016-09-26 08:42:05 -0500

    emacs-wgrep: 20141017 -> 2.1.10

--- pkgs/top-level/emacs-packages.nix
+++ pkgs/top-level/emacs-packages.nix
@@ -358,21 +358,6 @@ let
     };
   };
 
-  wgrep = melpaBuild rec {
-    pname   = "wgrep";
-    version = "20141017";
-    src = fetchFromGitHub {
-      owner  = "mhayashi1120";
-      repo   = "Emacs-wgrep";
-      rev    = "7ef26c51feaef8a5ec0929737130ab8ba326983c";
-      sha256 = "075z0glain0dp56d0cp468y5y88wn82ab26aapsrdzq8hmlshwn4";
-    };
-    meta = {
-      description = "Writable grep buffer mode for Emacs";
-      license = gpl3Plus;
-    };
-  };
-
   zenburn-theme = melpaBuild rec {
     pname   = "zenburn-theme";
     version = "2.2";
