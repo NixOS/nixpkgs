36fca1188416b5105ea1e2ae0afec37c31f44580
Thomas Tuegel <ttuegel@gmail.com>
2016-09-26 07:51:14 -0500

    emacs-god-mode: 20140811 -> 20151005.925

--- pkgs/top-level/emacs-packages.nix
+++ pkgs/top-level/emacs-packages.nix
@@ -284,21 +284,6 @@ let
     inherit (external.rtags) meta;
   };
 
-  god-mode = melpaBuild rec {
-    pname   = "god-mode";
-    version = "20140811";
-    src = fetchFromGitHub {
-      owner  = "chrisdone";
-      repo   = pname;
-      rev    = "6b7ae259a58ca1d7776aa4eca9f1092e4c0033e6";
-      sha256 = "1amr98nq82g2d3f3f5wlqm9g38j64avygnsi9rrlbfqz4f71vq7x";
-    };
-    meta = {
-      description = "A global minor mode for entering Emacs commands without modifier keys";
-      license = gpl3Plus;
-    };
-  };
-
   goto-chg = melpaBuild rec {
     pname   = "goto-chg";
     version = "1.6";
