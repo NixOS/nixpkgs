1c569e0cc88e92afd75bc49aa0f33333843ae4f5
Thomas Tuegel <ttuegel@gmail.com>
2016-09-26 07:50:07 -0500

    emacs-gnus: 20140501 -> builtin

--- pkgs/top-level/emacs-packages.nix
+++ pkgs/top-level/emacs-packages.nix
@@ -284,24 +284,6 @@ let
     inherit (external.rtags) meta;
   };
 
-  gnus = melpaBuild rec {
-    pname   = "gnus";
-    version = "20140501";
-    src = fetchgit {
-      url = "http://git.gnus.org/gnus.git";
-      rev = "4228cffcb7afb77cf39678e4a8988a57753502a5";
-      sha256 = "0qd0wpxkz47irxghmdpa524c9626164p8vgqs26wlpbdwyvm64a0";
-    };
-    fileSpecs = [ "lisp/*.el" "texi/*.texi" ];
-    preBuild = ''
-      (cd lisp && make gnus-load.el)
-    '';
-    meta = {
-      description = "News and mail reader for Emacs";
-      license = gpl3Plus;
-    };
-  };
-
   go-mode = melpaBuild rec {
     pname = "go-mode";
     version = "20150817";
