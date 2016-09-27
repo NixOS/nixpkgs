eb8e1d16b52a107b65e0a812043362c3aa338e38
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-09-27 13:34:00 +0200

    google-talk-plugin: Use mesa_noglu instead of mesa
    (cherry picked from commit 62f055af478b97bfb6f981af7e758d149b60f629)

--- pkgs/applications/networking/browsers/mozilla-plugins/google-talk-plugin/default.nix
+++ pkgs/applications/networking/browsers/mozilla-plugins/google-talk-plugin/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, rpm, cpio, mesa, xorg, cairo
+{ stdenv, fetchurl, rpm, cpio, mesa_noglu, xorg, cairo
 , libpng, gtk, glib, gdk_pixbuf, fontconfig, freetype, curl
 , dbus_glib, alsaLib, libpulseaudio, systemd, pango
 }:
@@ -10,7 +10,7 @@ let
   baseURL = "http://dl.google.com/linux/talkplugin/deb/pool/main/g/google-talkplugin";
 
   rpathPlugin = makeLibraryPath
-    [ mesa
+    [ mesa_noglu
       xorg.libXt
       xorg.libX11
       xorg.libXrender
