dd2ac271849ec269780555750545874f3c5cd56d
Matej Cotman <cotman.matej@gmail.com>
2016-09-28 11:30:16 +0200

    amazon image: fetch meta-data everytime in case if something changes

--- nixos/modules/virtualisation/amazon-image.nix
+++ nixos/modules/virtualisation/amazon-image.nix
@@ -67,21 +67,11 @@ let cfg = config.ec2; in
 
         echo "getting EC2 instance metadata..."
 
-        if ! [ -e "$metaDir/ami-manifest-path" ]; then
-          wget -q -O "$metaDir/ami-manifest-path" http://169.254.169.254/1.0/meta-data/ami-manifest-path
-        fi
-
-        if ! [ -e "$metaDir/user-data" ]; then
-          wget -q -O "$metaDir/user-data" http://169.254.169.254/1.0/user-data && chmod 600 "$metaDir/user-data"
-        fi
-
-        if ! [ -e "$metaDir/hostname" ]; then
-          wget -q -O "$metaDir/hostname" http://169.254.169.254/1.0/meta-data/hostname
-        fi
-
-        if ! [ -e "$metaDir/public-keys-0-openssh-key" ]; then
-          wget -q -O "$metaDir/public-keys-0-openssh-key" http://169.254.169.254/1.0/meta-data/public-keys/0/openssh-key
-        fi
+        # fetch metadata everytime!
+        wget -q -O "$metaDir/ami-manifest-path" http://169.254.169.254/1.0/meta-data/ami-manifest-path
+        wget -q -O "$metaDir/user-data" http://169.254.169.254/1.0/user-data && chmod 600 "$metaDir/user-data"
+        wget -q -O "$metaDir/hostname" http://169.254.169.254/1.0/meta-data/hostname
+        wget -q -O "$metaDir/public-keys-0-openssh-key" http://169.254.169.254/1.0/meta-data/public-keys/0/openssh-key
 
         diskNr=0
         diskForUnionfs=
