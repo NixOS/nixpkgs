NixOS/nixpkgs

commit 29caa185a7e4aaa0d621a4f117f3e8f653261fda

lightdm: obbey services.xserver.{window/desktop}Manager.default

nixos/modules/services/x11/display-managers/lightdm.nix
@@ -46,13 +46,15 @@ let
       [Seat:*]
       xserver-command = ${xserverWrapper}
       session-wrapper = ${dmcfg.session.script}
+      ${optionalString (elem defaultSessionName dmcfg.session.names) ''
+        user-session = ${defaultSessionName}
+      ''}
       ${optionalString cfg.greeter.enable ''
         greeter-session = ${cfg.greeter.name}
       ''}
       ${optionalString cfg.autoLogin.enable ''
         autologin-user = ${cfg.autoLogin.user}
         autologin-user-timeout = ${toString cfg.autoLogin.timeout}
-        autologin-session = ${defaultSessionName}
       ''}
       ${cfg.extraSeatDefaults}
     '';

commit 00d1106f5e3e915acc3957d56089f4a805912506

lightdm: Fix regression from 29caa185a7e4aaa0d621a4f117f3e8f653261fda
29caa185a7e4aaa0d621a4f117f3e8f653261fda introduced a bug where when
using i3 and no desktopmanager, it would start xterm and not i3.

Example configuration:

    services.xserver.displayManager.lightdm.enable = true;
    services.xserver.windowManager.i3.enable = true;

when starting up you would get i3 sort of, but xterm would be started
as your user-session.

This was fixable by setting:

    services.xserver.desktopManager.default = "none";

as well, but this was definitely a surprise which broke my system. I
don't think this regression was intentional, so here we are fixing it.

nixos/modules/services/x11/display-managers/lightdm.nix
@@ -63,7 +63,7 @@ let
     let
       dm = xcfg.desktopManager.default;
       wm = xcfg.windowManager.default;
-    in dm + optionalString (wm != "none") (" + " + wm);
+    in dm + optionalString (wm != "" && wm != "none") (" + " + wm);
 in
 {
   # Note: the order in which lightdm greeter modules are imported