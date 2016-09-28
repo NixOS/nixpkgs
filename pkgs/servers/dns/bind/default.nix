e78df0f71641146591fa8801768ea834c407c10e
Franz Pletz <fpletz@fnordicwalking.de>
2016-09-28 03:37:36 +0200

    bind: enable seccomp on linux

--- pkgs/servers/dns/bind/default.nix
+++ pkgs/servers/dns/bind/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, fetchurl, openssl, libtool, perl, libxml2 }:
+{ stdenv, fetchurl, openssl, libtool, perl, libxml2
+, libseccomp ? null }:
 
 let version = "9.10.4-P3"; in
 
@@ -15,7 +16,8 @@ stdenv.mkDerivation rec {
   patches = [ ./dont-keep-configure-flags.patch ./remove-mkdir-var.patch ] ++
     stdenv.lib.optional stdenv.isDarwin ./darwin-openssl-linking-fix.patch;
 
-  buildInputs = [ openssl libtool perl libxml2 ];
+  buildInputs = [ openssl libtool perl libxml2 ] ++
+    stdenv.lib.optional stdenv.isLinux libseccomp;
 
   configureFlags = [
     "--localstatedir=/var"
@@ -31,6 +33,7 @@ stdenv.mkDerivation rec {
     "--without-pkcs11"
     "--without-purify"
     "--without-python"
+    "--enable-seccomp"
   ];
 
   postInstall = ''
