1451c29a74f9c803eae5076b24d42ae703c2cecc
Fatih Altinok <fatih@picussecurity.com>
2016-09-29 08:36:07 +0000

    bump flow version to 0.32

--- pkgs/development/tools/analysis/flow/default.nix
+++ pkgs/development/tools/analysis/flow/default.nix
@@ -3,14 +3,14 @@
 with lib;
 
 stdenv.mkDerivation rec {
-  version = "0.30.0";
+  version = "0.32.0";
   name = "flow-${version}";
 
   src = fetchFromGitHub {
     owner = "facebook";
     repo = "flow";
     rev = "v${version}";
-    sha256 = "1s6l3570r53qjyqs8ghqqgb51rb0skijwjgm6av43xi7b7knkd35";
+    sha256 = "17v2qb9xsjv4lj62x553knnhb7z43y2frzvs0q1hvamw8wyp086h";
   };
 
   installPhase = ''
