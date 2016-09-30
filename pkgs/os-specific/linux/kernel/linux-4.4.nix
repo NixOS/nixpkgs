44557795aed5079d2a21b9aebae3f7f3cb012c93
Eelco Dolstra <edolstra@gmail.com>
2016-09-30 14:41:07 +0200

    linux: 4.4.22 -> 4.4.23
    (cherry picked from commit 613a12a8bde64b5ec26b512f34de7507eba7d861)

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.4.22";
+  version = "4.4.23";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "03qsdnlcycfy8l4q2578qrvpj3xj1gc3aijb46q4ym9vvihmd7b2";
+    sha256 = "1ljvga8c6p3aww3jpi810rvf4m9qr1rjy8ya6s1xq07b08g5hpbk";
   };
 
   kernelPatches = args.kernelPatches;
