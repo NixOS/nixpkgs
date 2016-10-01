82c0aa7d85183da8031d4c0ac7fd562887d03b89
Tuomas Tynkkynen <tuomas@tuxera.com>
2016-10-02 00:11:13 +0300

    openjpeg_2_0: Fix hash

--- pkgs/development/libraries/openjpeg/2.0.nix
+++ pkgs/development/libraries/openjpeg/2.0.nix
@@ -4,5 +4,5 @@ callPackage ./generic.nix (args // rec {
   version = "2.0.1";
   branch = "2";
   revision = "version.2.0.1";
-  sha256 = "1r81hq0hx2papjs3hfmpsl0024f6lblk0bq53dfm2wcpi916q7pw";
+  sha256 = "03d0r8x66cxri9i20nr9gm1jnkp85yyd8mkrbmawv5nvybd0r7wv";
 })
