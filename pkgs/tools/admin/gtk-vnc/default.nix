da658b98ce3454c98ad6e1429c2e78eac07d0a6a
Alexander Ried <ried@mytum.de>
2016-10-01 13:34:44 +0200

    gtk-vnc: 0.5.3 -> 0.6.0
    gtk3 build is now default

--- pkgs/tools/admin/gtk-vnc/default.nix
+++ pkgs/tools/admin/gtk-vnc/default.nix
@@ -1,31 +1,30 @@
 { stdenv, fetchurl, gobjectIntrospection
 , gnutls, cairo, libtool, glib, pkgconfig, libtasn1
 , libffi, cyrus_sasl, intltool, perl, perlPackages, libpulseaudio
-, kbproto, libX11, libXext, xextproto, libgcrypt, gtk3, vala_0_23
+, kbproto, libX11, libXext, xextproto, libgcrypt, gtk3, vala_0_32
 , libogg, libgpgerror, pythonPackages }:
 
 let
   inherit (pythonPackages) pygobject3 python;
 in stdenv.mkDerivation rec {
   name = "gtk-vnc-${version}";
-  version = "0.5.3";
+  version = "0.6.0";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gtk-vnc/0.5/${name}.tar.xz";
-    sha256 = "1bww2ihxb3zzvifdrcsy1lifr664pvikq17hmr1hsm8fyk4ad46l";
+    url = "mirror://gnome/sources/gtk-vnc/0.6/${name}.tar.xz";
+    sha256 = "9559348805e64d130dae569fee466930175dbe150d2649bb868b5c095f130433";
   };
 
   buildInputs = [
     python gnutls cairo libtool pkgconfig glib libffi libgcrypt
     intltool cyrus_sasl libpulseaudio perl perlPackages.TextCSV
     gobjectIntrospection libogg libgpgerror
-    gtk3 vala_0_23 pygobject3 ];
+    gtk3 vala_0_32 pygobject3 ];
 
   NIX_CFLAGS_COMPILE = "-fstack-protector-all";
   configureFlags = [
     "--with-python"
     "--with-examples"
-    "--with-gtk=3.0"
   ];
 
   # Fix broken .la files
