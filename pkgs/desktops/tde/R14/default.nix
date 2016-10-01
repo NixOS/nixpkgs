e4611cae1ac6bc3d5dba126c22e85b2a5f3a3ae9
AndersonTorres <torres.anderson.85@gmail.com>
2016-10-01 12:05:44 -0300

    tqtinterface (tde): init at R14.0.3

--- pkgs/desktops/tde/R14/default.nix
+++ pkgs/desktops/tde/R14/default.nix
@@ -7,5 +7,6 @@ rec {
   tqt3-thread = callPackage ./dependencies/tqt3 { threadSupport = true; };
   tqt3-nothread = callPackage ./dependencies/tqt3 { threadSupport = false; };
   tqt3 = tqt3-thread;
+  tqtinterface = callPackage ./dependencies/tqtinterface { };
 
 }
