2ec9a1a955d49c538206c64424d553da9a379df1
Joachim Fasting <joachifm@fastmail.fm>
2016-10-01 08:47:30 +0200

    grsecurity: 4.7.5-201609261522 -> 4.7.6-201609301918

--- pkgs/os-specific/linux/kernel/linux-grsecurity.nix
+++ pkgs/os-specific/linux/kernel/linux-grsecurity.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.7.5";
+  version = "4.7.6";
   extraMeta.branch = "4.7";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "14nh37v2ankal763zrwd3yrdpbg2ni1yr33kdqjiqrja4vqazc4p";
+    sha256 = "1lidsc85xc025a46pyjimcr1xcwv7mh59jihmnqihfnwqch6albv";
   };
 
   kernelPatches = args.kernelPatches;
--- pkgs/os-specific/linux/kernel/patches.nix
+++ pkgs/os-specific/linux/kernel/patches.nix
@@ -86,9 +86,9 @@ rec {
   };
 
   grsecurity_testing = grsecPatch
-    { kver   = "4.7.5";
-      grrev  = "201609261522";
-      sha256 = "1306lw3byw6cyi0dqh0dcl67l0bi043zmp3582hjpkwj934bahf7";
+    { kver   = "4.7.6";
+      grrev  = "201609301918";
+      sha256 = "153x97vvd3jl2i7fs6d5ilqnmz2v2iyzsf3pg3frrackv6xaisa5";
     };
 
   # This patch relaxes grsec constraints on the location of usermode helpers,
