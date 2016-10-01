a06a36b80b7b9b8ec1b6d9036b5b738a171ce1c2
AndersonTorres <torres.anderson.85@gmail.com>
2016-10-01 12:05:44 -0300

    tqtinterface: init at R14.0.3

---
+++ pkgs/desktops/tde/R14/dependencies/tqtinterface/default.nix
@@ -0,0 +1,36 @@
+{ stdenv, fetchurl, cmake, coreutils, which, libuuid, tqt3, mesa }:
+
+stdenv.mkDerivation rec {
+
+  name = "tqtinterface-${version}";
+  version = "${majorVer}.${minorVer}";
+  majorVer = "R14.0";
+  minorVer = "3";
+
+  src = fetchurl {
+    url = "mirror://tde/${version}/dependencies/${name}.tar.bz2";
+    sha256 = "14kxq7nlkalgmlxpijs78fr0zr2ha9xykzqfbr7lh3zn0r79k5a3";
+  };
+
+  buildInputs = [ cmake coreutils which libuuid ];
+  propagatedBuildInputs = [ tqt3 mesa ];
+  setupHook = ./setup-hook.sh;
+
+  cmakeFlags = [
+    "-DQT_VERSION=3"
+    "-DQT_PREFIX_DIR=${tqt3}"
+    "-DQT_INCLUDE_DIR=${tqt3}/include"
+    "-DMOC_EXECUTABLE=${tqt3}/bin/tqmoc" ];
+
+  preConfigure = ''
+    cd tqtinterface
+  '';
+
+  meta = with stdenv.lib;{
+    description = "A library interface for TQt";
+    homepage = http://www.trinitydesktop.org;
+    license = licenses.gpl2Plus;
+    maintainers = [ maintainers.AndersonTores ];
+    platforms = platforms.linux;
+  };
+}
---
+++ pkgs/desktops/tde/R14/dependencies/tqtinterface/setup-hook.sh
@@ -0,0 +1,5 @@
+
+export TDEDIR=@out@
+export TDEDIRS=@out@
+export XDG_DATA_DIRS=$XDG_DATA_DIRS:$TDEDIR/share
+export XDG_CONFIG_DIRS=$XDG_CONFIG_DIRS:$TDEDIR/etc/xdg
