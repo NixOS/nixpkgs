e19bde5ec744f0ffad6e124d25e995b2fe3a5708
AndersonTorres <torres.anderson.85@gmail.com>
2016-10-02 19:23:10 -0300

    libart-lgpl (tde): init at R14.0.3

--- pkgs/desktops/tde/R14/default.nix
+++ pkgs/desktops/tde/R14/default.nix
@@ -4,9 +4,12 @@ rec {
 
   #### Dependencies
 
+  tqt3 = tqt3-thread;
   tqt3-thread = callPackage ./dependencies/tqt3 { threadSupport = true; };
   tqt3-nothread = callPackage ./dependencies/tqt3 { threadSupport = false; };
-  tqt3 = tqt3-thread;
   tqtinterface = callPackage ./dependencies/tqtinterface { };
   arts = callPackage ./dependencies/arts { };
+  dbus-tqt = callPackage ./dependencies/dbus-tqt { };
+  dbus-1-tqt = callPackage ./dependencies/dbus-1-tqt { };
+  libart-lgpl = callPackage ./dependencies/libart-lgpl { };
 }
---
+++ pkgs/desktops/tde/R14/dependencies/libart-lgpl/default.nix
@@ -0,0 +1,34 @@
+{ stdenv, fetchurl, pkgconfig }:
+
+with stdenv.lib;
+stdenv.mkDerivation rec {
+
+  name = "libart-lgpl-${version}";
+  version = "${majorVer}.${minorVer}";
+  majorVer = "R14.0";
+  minorVer = "3";
+
+  src = fetchurl {
+    url = "mirror://tde/${version}/dependencies/${name}.tar.bz2";
+    sha256 = "0gnzdml05ggc95cv069in72glagmn98bs10lyhmvh8k1snxqdj6h";
+  };
+
+  buildInputs = [ pkgconfig ];
+
+  configureFlags = ''
+    --enable-ansi
+  '';
+
+  preConfigure = ''
+    cd libart-lgpl
+  '';
+
+  meta = with stdenv.lib;{
+    description = "A 2D drawing library";
+    homepage = http://www.trinitydesktop.org;
+    license = licenses.gpl2Plus;
+    maintainers = [ maintainers.AndersonTores ];
+    platforms = platforms.linux;
+  };
+}
+# Warning: autotool build, will be deprecated by cmake in the future
