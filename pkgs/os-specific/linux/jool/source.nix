e165c7230d19ade6b146b822c5445b1de39cd5f5
Franz Pletz <fpletz@fnordicwalking.de>
2016-10-03 17:58:38 +0200

    jool: 3.4.5 -> 3.5.0

--- pkgs/os-specific/linux/jool/source.nix
+++ pkgs/os-specific/linux/jool/source.nix
@@ -1,9 +1,9 @@
 { fetchzip }:
 
 rec {
-  version = "3.4.5";
+  version = "3.5.0";
   src = fetchzip {
     url = "https://github.com/NICMx/releases/raw/master/Jool/Jool-${version}.zip";
-    sha256 = "045j3ax6c5jg8037hhrbgqgznr0a114xrmn03wkasnvsxpsx4hkb";
+    sha256 = "06jp6gpfryn66q0z4w3gwkvfr17bcrjvys597nj49pxfiz4vczb2";
   };
 }
