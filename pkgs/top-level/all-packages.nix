151edd446d6d06e2c0b0dd614ca5d2a4d3eff3fd
Kiba Fox <kiba.fox@foxypossibilities.com>
2016-10-04 17:46:26 -0400

    tinyfugue: init at 50b8

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15667,6 +15667,8 @@ in
 
   tintin = callPackage ../games/tintin { };
 
+  tinyfugue = callPackage ../games/tinyfugue { };
+
   tome4 = callPackage ../games/tome4 { };
 
   trackballs = callPackage ../games/trackballs {
