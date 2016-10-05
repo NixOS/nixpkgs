4e91e8cb3d7947d37738486453c7d7875eaadc80
Alexander Ried <ried@mytum.de>
2016-10-05 10:48:57 +0200

    rtkit: add patch from debian to remove ControlGroup stanza
    fixes log clutter:
    systemd[1]: [/nix/store/....-rtkit-0.11/etc/systemd/system/rtkit-daemon.service:32] Unknown lvalue 'ControlGroup' in section 'Service'

--- pkgs/os-specific/linux/rtkit/default.nix
+++ pkgs/os-specific/linux/rtkit/default.nix
@@ -1,8 +1,8 @@
-{ stdenv, fetchurl, pkgconfig, dbus, libcap }:
+{ stdenv, fetchurl, fetchpatch, pkgconfig, dbus, libcap }:
 
 stdenv.mkDerivation rec {
   name = "rtkit-0.11";
-  
+
   src = fetchurl {
     url = "http://0pointer.de/public/${name}.tar.xz";
     sha256 = "1l5cb1gp6wgpc9vq6sx021qs6zb0nxg3cn1ba00hjhgnrw4931b8";
@@ -10,6 +10,13 @@ stdenv.mkDerivation rec {
 
   configureFlags = [
     "--with-systemdsystemunitdir=$(out)/etc/systemd/system"
+  ];
+
+  patches = [
+    (fetchpatch {
+      url = "https://anonscm.debian.org/cgit/pkg-multimedia/rtkit.git/plain/debian/patches/0002-Drop-Removed-ControlGroup-stanza.patch?id=21f2c6be6985c777cbf113c67043353406744050";
+      sha256 = "0lsxk5nv08i1wjb4xh20i5fcwg3x0qq0k4f8bc0r9cczph2sv7ck";
+    })
   ];
 
   buildInputs = [ pkgconfig dbus libcap ];
