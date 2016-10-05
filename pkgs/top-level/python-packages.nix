1abc6dc1f3e1ca7ed55ec53ce6500fab84c61a0c
Lancelot SIX <lsix+github@lancelotsix.com>
2016-10-05 12:28:17 +0200

    pythonPackages.django_1_10: 1.10.1 -> 1.10.2 (#19193)
    This is a bugfix release.
    
    See https://docs.djangoproject.com/en/1.10/releases/1.10.2/
    (cherry picked from commit 74e52576e0f8f230e2fe79ef7b77eeddcf8a58e8)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9220,12 +9220,12 @@ in modules // {
 
   django_1_10 = buildPythonPackage rec {
     name = "Django-${version}";
-    version = "1.10.1";
+    version = "1.10.2";
     disabled = pythonOlder "2.7";
 
     src = pkgs.fetchurl {
       url = "http://www.djangoproject.com/m/releases/1.10/${name}.tar.gz";
-      sha256 = "1wr438yykg0m5s9xini36hc826di55jm6by8syplczxnbjrcbrnn";
+      sha256 = "1qdwgkwlq5wl0wn247d9gid49xpz4qggk0lcdqxq8d7v1cmg29z1";
     };
 
     patches = [
