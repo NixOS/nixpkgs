4070a67e00177c3e159bb1672cdff380b6778ab8
Robin Gloster <mail@glob.in>
2016-10-08 23:46:19 +0200

    dhparams module: remove

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -95,7 +95,6 @@
   ./security/audit.nix
   ./security/ca.nix
   ./security/chromium-suid-sandbox.nix
-  ./security/dhparams.nix
   ./security/duosec.nix
   ./security/grsecurity.nix
   ./security/hidepid.nix
