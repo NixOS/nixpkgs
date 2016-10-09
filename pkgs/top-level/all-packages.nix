8a59ad167e8d5ceda9730b64f847bca87a58cf39
Franz Pletz <fpletz@fnordicwalking.de>
2016-10-09 12:29:48 +0200

    atlassian-crowd: init at 2.10.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9820,6 +9820,7 @@ in
   archiveopteryx = callPackage ../servers/mail/archiveopteryx/default.nix { };
 
   atlassian-confluence = callPackage ../servers/atlassian/confluence.nix { };
+  atlassian-crowd = callPackage ../servers/atlassian/crowd.nix { };
   atlassian-jira = callPackage ../servers/atlassian/jira.nix { };
 
   cadvisor = callPackage ../servers/monitoring/cadvisor { };
