a0e791a14c924fa387e67f112becc7d09e26e187
Robin Gloster <mail@glob.in>
2016-10-11 15:17:44 +0200

    errbot module: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -236,6 +236,7 @@
   ./services/misc/dysnomia.nix
   ./services/misc/disnix.nix
   ./services/misc/emby.nix
+  ./services/misc/errbot.nix
   ./services/misc/etcd.nix
   ./services/misc/felix.nix
   ./services/misc/folding-at-home.nix
