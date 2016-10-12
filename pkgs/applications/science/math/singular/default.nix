5af1b7ad023ee96e95122ac3f51ddeb36a4c693f
Michael Raskin <7c6f434c@mail.ru>
2016-10-12 17:34:29 +0200

    singular: 3-1-2 -> 3-1-7

--- pkgs/applications/science/math/singular/default.nix
+++ pkgs/applications/science/math/singular/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "singular-${version}";
-  version="3-1-2";
+  version="3-1-7";
 
   src = fetchurl {
-    url = "http://www.mathematik.uni-kl.de/ftp/pub/Math/Singular/SOURCES/${version}/${name}.tar.gz";
-    sha256 = "04f9i1xar0r7qrrbfki1h9rrmx5y2xg4w7rrvlbx05v2dy6s8djv";
+    url = "http://www.mathematik.uni-kl.de/ftp/pub/Math/Singular/SOURCES/${version}/Singular-${version}.tar.gz";
+    sha256 = "1j4mcpnwzdp3h4qspk6ww0m67rmx4s11cy17pvzbpf70lm0jzzh2";
   };
 
   buildInputs = [ gmp bison perl autoconf ncurses readline coreutils ];
@@ -20,7 +20,8 @@ stdenv.mkDerivation rec {
 
   postInstall = ''
     rm -rf "$out/LIB"
-    cp -r Singular/LIB "$out"
+    cp -Tr Singular/LIB "$out/lib"
+    ln -s "$out"/*/include "$out"
     mkdir -p "$out/bin"
     ln -s "$out/"*/Singular "$out/bin"
   '';
@@ -32,10 +33,6 @@ stdenv.mkDerivation rec {
     platforms = platforms.linux;
     license = licenses.gpl3; # Or GPLv2 at your option - but not GPLv4
     homepage = "http://www.singular.uni-kl.de/index.php";
-  };
-  passthru = {
-    updateInfo = {
-      downloadPage = "http://www.mathematik.uni-kl.de/ftp/pub/Math/Singular/SOURCES/";
-    };
+    downloadPage = "http://www.mathematik.uni-kl.de/ftp/pub/Math/Singular/SOURCES/";
   };
 }
