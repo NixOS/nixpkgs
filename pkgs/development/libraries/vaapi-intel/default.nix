6adc67d8d709ca7650a544ab46805ebaf8f340da
Franz Pletz <fpletz@fnordicwalking.de>
2016-10-13 03:12:49 +0200

    vaapiIntel: 1.7.0 -> 1.7.2

--- pkgs/development/libraries/vaapi-intel/default.nix
+++ pkgs/development/libraries/vaapi-intel/default.nix
@@ -3,11 +3,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "libva-intel-driver-1.7.0";
+  name = "libva-intel-driver-1.7.2";
 
   src = fetchurl {
     url = "http://www.freedesktop.org/software/vaapi/releases/libva-intel-driver/${name}.tar.bz2";
-    sha256 = "032w8d0whymi5ac8fk7c5d8nnxxsjgwymw644g7gp959i73xc6cx";
+    sha256 = "1g371q9p31i57fkidjp2akvrbaadpyx3bwmg5kn72sc2mbv7p7h9";
   };
 
   patchPhase = ''
