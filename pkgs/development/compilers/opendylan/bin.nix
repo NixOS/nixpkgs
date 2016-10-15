c4556b01faaafc7698df960875ab8143e479108a
zimbatm <zimbatm@zimbatm.com>
2016-10-15 16:34:04 +0100

    opendylan: use fetchOneOf

--- pkgs/development/compilers/opendylan/bin.nix
+++ pkgs/development/compilers/opendylan/bin.nix
@@ -1,19 +1,20 @@
 # Binaries provided by Open Dylan to be used to bootstrap from source.
 # The binaries can also be used as is.
-{stdenv, fetchurl, patchelf, boehmgc, gnused, gcc, makeWrapper}:
+{stdenv, fetchOneOf, patchelf, boehmgc, gnused, gcc, makeWrapper}:
 
 stdenv.mkDerivation {
   name = "opendylan-2013.2";
 
-  src = if stdenv.system == "x86_64-linux" then fetchurl {
+  src = fetchOneOf stdenv.system {
+    "x86_64-linux" = {
       url = http://opendylan.org/downloads/opendylan/2013.2/opendylan-2013.2-x86_64-linux.tar.bz2;
       sha256 = "035brbw3hm7zrs593q4zc42yglj1gmmkw3b1r7zzlw3ks4i2lg7h";
-    }
-    else if stdenv.system == "i686-linux" then fetchurl {
+    };
+    "i686-linux" = {
       url = http://opendylan.org/downloads/opendylan/2013.2/opendylan-2013.2-x86-linux.tar.bz2;
       sha256 = "0c61ihvblcsjrw6ncr8x8ylhskcrqs8pajs4mg5di36cvqw12nq5";
-    }
-    else throw "platform ${stdenv.system} not supported.";
+    };
+  };
 
   buildInputs = [ patchelf boehmgc gnused makeWrapper ];
 
