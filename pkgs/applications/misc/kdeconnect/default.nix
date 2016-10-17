ec84a18ee06c3beebec7c1e56a0e409d9209f39b
Peter Hoeg <peter@speartail.com>
2016-10-17 18:42:51 +0800

    kdeconnect: let dbus activate via systemd

--- pkgs/applications/misc/kdeconnect/default.nix
+++ pkgs/applications/misc/kdeconnect/default.nix
@@ -12,15 +12,41 @@
 , qca-qt5
 , libfakekey
 , libXtst
+, writeText
 }:
 
-stdenv.mkDerivation rec {
+let
+  busName = "org.kde.kdeconnect";
+  serviceName = "kdeconnectd.service";
+
+  dbus = writeText "dbus-${serviceName}" ''
+    [D-BUS Service]
+    Name=${busName}
+    Exec=@out@/lib/libexec/kdeconnectd
+    SystemdService=dbus-${busName}.service
+  '';
+
+  service = writeText serviceName ''
+    [Unit]
+    Description=KDE Connect daemon
+
+    [Service]
+    Type=dbus
+    BusName=${busName}
+    ExecStart=@out@/lib/libexec/kdeconnectd
+    ExecStop=${kdbusaddons}/bin/kquitapp5 kdeconnectd
+    Restart=on-failure
+    PrivateTmp=true
+    Slice=kde.slice
+  '';
+
+in stdenv.mkDerivation rec {
   name = "kdeconnect-${version}";
-  version = "1.0";
+  version = "1.0.1";
 
   src = fetchurl {
-    url = http://download.kde.org/stable/kdeconnect/1.0/src/kdeconnect-kde-1.0.tar.xz;
-    sha256 = "0pd8qw0w6akc7yzmsr0sjkfj3nw6rgm5xvq41g61ak8pp05syzr0";
+    url = "http://download.kde.org/stable/kdeconnect/${version}/src/kdeconnect-kde-${version}.tar.xz";
+    sha256 = "00p1njkp80lhnpjs82b1xgkzb58xwbgycll1z74hzq32icrwqfsm";
   };
 
   buildInputs = [
@@ -42,6 +68,14 @@ stdenv.mkDerivation rec {
 
   postInstall = ''
     wrapQtProgram "$out/bin/kdeconnect-cli"
+
+    mkdir -p $out/lib/systemd/user $out/share/dbus-1/services
+    sed "s|@out@|$out|g" ${service} > $out/lib/systemd/user/${serviceName}
+    ln -sr $out/lib/systemd/user/${serviceName} $out/lib/systemd/user/dbus-${busName}.service
+
+    rm -f $out/etc/xdg/autostart/${serviceName}
+
+    sed "s|@out@|$out|g" ${dbus} > $out/share/dbus-1/services/${busName}.service
   '';
 
   meta = {
@@ -50,5 +84,4 @@ stdenv.mkDerivation rec {
     maintainers = with lib.maintainers; [ fridh ];
     homepage = https://community.kde.org/KDEConnect;
   };
-
 }
