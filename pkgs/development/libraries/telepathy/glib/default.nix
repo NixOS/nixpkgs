42f0271d3223ceaa545288ee01e022c38699cee3
Frederik Rietdijk <fridh@fridh.nl>
2016-10-18 15:55:15 +0200

    telepathy_glib: fix building with python3

--- pkgs/development/libraries/telepathy/glib/default.nix
+++ pkgs/development/libraries/telepathy/glib/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, dbus_glib, glib, python, pkgconfig, libxslt
-, gobjectIntrospection, valaSupport ? true, vala_0_23 }:
+, gobjectIntrospection, valaSupport ? true, vala_0_23, glibcLocales }:
 
 stdenv.mkDerivation rec {
   name = "telepathy-glib-0.24.1";
@@ -10,10 +10,10 @@ stdenv.mkDerivation rec {
   };
 
   configureFlags = stdenv.lib.optional valaSupport "--enable-vala-bindings";
-
+  LC_ALL = "en_US.UTF-8";
   propagatedBuildInputs = [dbus_glib glib python gobjectIntrospection];
 
-  buildInputs = [pkgconfig libxslt] ++ stdenv.lib.optional valaSupport vala_0_23;
+  buildInputs = [pkgconfig libxslt glibcLocales ] ++ stdenv.lib.optional valaSupport vala_0_23;
 
   preConfigure = ''
     substituteInPlace telepathy-glib/telepathy-glib.pc.in --replace Requires.private Requires
