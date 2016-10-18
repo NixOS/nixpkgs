2892d2e6cf85b264d8e6719adb238f91a8242cf6
Frederik Rietdijk <fridh@fridh.nl>
2016-10-18 15:48:16 +0200

    ardour3: use python2

--- pkgs/applications/audio/ardour/ardour3.nix
+++ pkgs/applications/audio/ardour/ardour3.nix
@@ -3,7 +3,7 @@
 , libgnomecanvas, libgnomecanvasmm, liblo, libmad, libogg, librdf
 , librdf_raptor, librdf_rasqal, libsamplerate, libsigcxx, libsndfile
 , libusb, libuuid, libxml2, libxslt, lilv-svn, lv2, makeWrapper, pango
-, perl, pkgconfig, python, rubberband, serd, sord-svn, sratom, suil, taglib, vampSDK }:
+, perl, pkgconfig, python2, rubberband, serd, sord-svn, sratom, suil, taglib, vampSDK }:
 
 let
 
@@ -43,7 +43,7 @@ stdenv.mkDerivation rec {
       glibmm graphviz gtkmm2 libjack2 libgnomecanvas libgnomecanvasmm liblo
       libmad libogg librdf librdf_raptor librdf_rasqal libsamplerate
       libsigcxx libsndfile libusb libuuid libxml2 libxslt lilv-svn lv2
-      makeWrapper pango perl pkgconfig python rubberband serd sord-svn sratom suil taglib vampSDK
+      makeWrapper pango perl pkgconfig python2 rubberband serd sord-svn sratom suil taglib vampSDK
     ];
 
   patchPhase = ''
@@ -52,12 +52,12 @@ stdenv.mkDerivation rec {
     patchShebangs ./tools/
   '';
 
-  configurePhase = "python waf configure --optimize --docs --with-backends=jack,alsa --prefix=$out";
+  configurePhase = "${python2.interpreter} waf configure --optimize --docs --with-backends=jack,alsa --prefix=$out";
 
-  buildPhase = "python waf";
+  buildPhase = "${python2.interpreter} waf";
 
   installPhase = ''
-    python waf install
+    ${python2.interpreter} waf install
 
     # Install desktop file
     mkdir -p "$out/share/applications"
