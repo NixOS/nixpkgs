59554dd466814b5fbefd5b33699f491f93a38a61
Philipp Steinpass <philipp@xndr.de>
2016-10-18 20:09:39 +0200

    Fixed multiple issues (commit will be squashed).

--- pkgs/tools/admin/dehydrated/default.nix
+++ pkgs/tools/admin/dehydrated/default.nix
@@ -1,26 +1,29 @@
-{ stdenv, bash, curl, openssl, makeWrapper, fetchFromGitHub }:
+{ stdenv, bash, coreutils, curl, diffutils, gawk, gnugrep, gnused, openssl, makeWrapper, fetchFromGitHub }:
+let
+  pkgName = "dehydrated";
+  version = "0.3.1";
+in
 stdenv.mkDerivation rec {
-  name = "dehydrated-0.3.1";
+  name = pkgName + "-" + version;
 
   src = fetchFromGitHub {
     owner = "lukas2511";
     repo = "dehydrated";
-    rev = "ec49a4433b48a7bc8f178d06dad3f55cff24bdf3";
+    rev = "v${version}";
     sha256 = "0prg940ykbsfb4w48bc03j5abycg8v7f9rg9x3kcva37y8ml0jsp";
   };
 
   buildInputs = [ makeWrapper ];
+
   installPhase = ''
     mkdir -p $out/bin
     cp -a dehydrated $out/bin
-    cp -a docs $out/
-    cp -a CHANGELOG $out/
-    wrapProgram "$out/bin/dehydrated" --prefix PATH : "${stdenv.lib.makeBinPath [ curl openssl ]}"
+    wrapProgram "$out/bin/dehydrated" --prefix PATH : "${stdenv.lib.makeBinPath [ openssl coreutils gnused gnugrep diffutils curl gawk ]}"
     '';
 
   meta = with stdenv.lib; {
     inherit (src.meta) homepage;
-    description = "This is a client for signing certificates with an ACME-server (currently only provided by letsencrypt) implemented as a relatively simple bash-script.";
+    description = "Letsencrypt/acme client implemented as a shell-script";
     license = licenses.mit;
     platforms = platforms.all;
   };
