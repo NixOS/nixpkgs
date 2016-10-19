63dc801899df20af143f6a54d2553290983264ff
makefu <makefu@syntax-fehler.de>
2016-10-19 20:26:08 +0200

    devpi-client: 2.3.2 -> 2.7.0
    devpi-common: 2.0.8 -> 3.0.1

--- pkgs/development/tools/devpi-client/default.nix
+++ pkgs/development/tools/devpi-client/default.nix
@@ -2,11 +2,11 @@
 
 pythonPackages.buildPythonApplication rec {
   name = "devpi-client-${version}";
-  version = "2.3.2";
+  version = "2.7.0";
 
   src = fetchurl {
     url = "mirror://pypi/d/devpi-client/${name}.tar.gz";
-    sha256 = "22484d6a1ccc957d3e4f857e428244fb27c042baedd3bf28fe7522cd89d8ff45";
+    sha256 = "0z7vaf0a66n82mz0vx122pbynjvkhp2mjf9lskgyv09y3bxzzpj3";
   };
 
   doCheck = false;
