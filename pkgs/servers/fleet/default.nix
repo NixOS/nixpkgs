cf8362ffeca1fa028cda5e4a4f93915c51d3e14f
Jascha Geerds <jascha@jgeerds.name>
2016-10-19 15:08:14 +0200

    fleet: 0.11.5 -> 0.11.8

--- pkgs/servers/fleet/default.nix
+++ pkgs/servers/fleet/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "fleet-${version}";
-  version = "0.11.5";
+  version = "0.11.8";
 
   src = fetchFromGitHub {
     owner = "coreos";
     repo = "fleet";
     rev = "v${version}";
-    sha256 = "0dc95dpqqc2rclbvgdqjcilrkji7lrpigdrzpwm3nbgz58vkfnz3";
+    sha256 = "13kwaa4hkiic602wnvnk13pflrxqhk2vxwpk1bn52ilwxkjyvkig";
   };
 
   buildInputs = [ go ];
