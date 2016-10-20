fb1114d5515081fb140785f90584caae1ab38298
Matthias Beyer <mail@beyermatthias.de>
2016-10-20 11:48:03 +0200

    fava: Add checkphase override, assets
    As suggested by @Mic92 in
    https://github.com/NixOS/nixpkgs/pull/15785#issuecomment-255036164

--- pkgs/applications/office/fava/default.nix
+++ pkgs/applications/office/fava/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, pkgs, fetchurl, python3Packages }:
+{ stdenv, pkgs, fetchurl, python3Packages, fetchFromGitHub, fetchzip, python3 }:
 
 let
 
@@ -52,15 +52,32 @@ python3Packages.buildPythonApplication rec {
   version = "1.0";
   name = "fava-${version}";
 
-  src = fetchurl {
-    url = "https://github.com/aumayr/fava/releases/download/v${version}/beancount-fava-${version}.tar.gz";
-    sha256 = "1yks1vy4cskv1h4dib0zx2cl8xz4kz4qx9n8v76w3di21w2gmak3";
+  src = fetchFromGitHub {
+    owner = "aumayr";
+    repo = "fava";
+    rev = "v${version}";
+    sha256 = "0dm4x6z80m04r9qa55psvz7f41qnh13hnj2qhvxkrk22yqmkqrka";
   };
 
-  buildInputs = with python3Packages; [ pytest ];
+  assets = fetchzip {
+    url = "https://github.com/aumayr/fava/releases/download/v${version}/beancount-fava-${version}.tar.gz";
+    sha256 = "1vvidwfn5882dslz6qqkkd84m7w52kd34x10qph8yhipyjv1dimc";
+  };
 
-  doCheck = false;
-  checkPhase = "py.test";
+  buildInputs = with python3Packages; [ pytest_30 ];
+
+  checkPhase = ''
+    # pyexcel is optional
+    # the other 2 tests fail due non-unicode locales
+    PATH=$out/bin:$PATH pytest tests \
+      --ignore tests/test_util_excel.py \
+      --ignore tests/test_cli.py \
+      --ignore tests/test_translations.py \
+  '';
+
+  postInstall = ''
+    cp -r $assets/fava/static/gen $out/${python3.sitePackages}/fava/static
+  '';
 
   propagatedBuildInputs = with python3Packages;
     [ flask dateutil pygments wheel markdown2 flaskbabel tornado
