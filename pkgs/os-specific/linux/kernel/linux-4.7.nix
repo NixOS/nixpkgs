adbe0e0a13143ecb7a39b27720c1a65a29e57151
Tim Steinbach <tim@nequissimus.com>
2016-10-22 12:11:09 -0400

    linux: 4.7.9 -> 4.7.10

--- pkgs/os-specific/linux/kernel/linux-4.7.nix
+++ pkgs/os-specific/linux/kernel/linux-4.7.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.7.9";
+  version = "4.7.10";
   extraMeta.branch = "4.7";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "13g2b8ap2xyx0c35ka92j9j3x7mh610ax5fq2xl75787hqmdaf06";
+    sha256 = "1p2r5d0jcrak9gxp0339g9z198x9laf09h08ck4jllhhaajrnicj";
   };
 
   kernelPatches = args.kernelPatches;
