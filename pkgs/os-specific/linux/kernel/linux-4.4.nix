4489454b83ccf519f516dcfcc073a450a4fa8ac7
Tim Steinbach <tim@nequissimus.com>
2016-10-22 12:10:34 -0400

    linux: 4.4.26 -> 4.4.27

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.4.26";
+  version = "4.4.27";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "17h2k7imdv96pp121y4hswzqjx53vd8wx2s628pifk4vfs9kya2q";
+    sha256 = "1zbahhbwxdhl7v0l2ch1ybllywj2df3rmy8w451whr79z7c7shvc";
   };
 
   kernelPatches = args.kernelPatches;
