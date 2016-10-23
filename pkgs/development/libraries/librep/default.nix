1b1c38dd634bb70018d1207e347ca7a8b286d102
AndersonTorres <torres.anderson.85@gmail.com>
2016-10-23 05:20:28 -0200

    Sawfish: 1.11.90 -> 1.12.0

--- pkgs/development/libraries/librep/default.nix
+++ pkgs/development/libraries/librep/default.nix
@@ -7,11 +7,12 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "librep-${version}";
-  version = "0.92.5";
+  version = "0.92.6";
+  sourceName = "librep_${version}";
 
   src = fetchurl {
-    url = "https://github.com/SawfishWM/librep/archive/${name}.tar.gz";
-    sha256 = "1ly425cgs0yi3lb5l84v3bacljw7m2nmzgky3acy1anp709iwi76";
+    url = "http://download.tuxfamily.org/librep/${sourceName}.tar.xz";
+    sha256 = "1k6c0hmyzxh8459r790slh9vv9vwy9d7w3nlmrqypbx9mk855hgy";
   };
 
   buildInputs = [ pkgconfig autoreconfHook readline texinfo ];
@@ -30,9 +31,10 @@ stdenv.mkDerivation rec {
       interpreter, a byte-code compiler, and a virtual
       machine. It can serve as an application extension language
       but is also suitable for standalone scripts.
-    '';
+     '';
     homepage = http://sawfish.wikia.com;
     license = licenses.gpl2;
     maintainers = [ maintainers.AndersonTorres ];
   };
 }
+# TODO: investigate fetchFromGithub
