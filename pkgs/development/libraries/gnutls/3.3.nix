afb55cb674cb843866dfb0078f50cfe0f8f9c734
Tim Steinbach <tim@nequissimus.com>
2016-10-24 22:06:01 -0400

    gnutls: 3.3.24 -> 3.3.25

--- pkgs/development/libraries/gnutls/3.3.nix
+++ pkgs/development/libraries/gnutls/3.3.nix
@@ -1,10 +1,10 @@
 { callPackage, fetchurl, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "3.3.24";
+  version = "3.3.25";
 
   src = fetchurl {
     url = "ftp://ftp.gnutls.org/gcrypt/gnutls/v3.3/gnutls-${version}.tar.xz";
-    sha256 = "5b65fe2a91c8dfa32bedc78acffcb152e5426cd3349e2afc43cccc9bdaf18aa5";
+    sha256 = "0bhzkzpzwg3lhbhpas7m4rcj4mrnyq76zmic9z42wpa68d76r78q";
   };
 })
