0e1066e8e431a3756825cceb94381a0ba05c1a43
Kranium Gikos Mendoza <kranium@gikos.net>
2016-10-25 22:23:19 +0800

    netdata: allow execution without a config file

--- pkgs/tools/system/netdata/default.nix
+++ pkgs/tools/system/netdata/default.nix
@@ -15,6 +15,21 @@ stdenv.mkDerivation rec{
 
   patches = [ ./web_access.patch ];
 
+  # Build will fail trying to create /var/{cache,lib,log}/netdata without this
+  postPatch = ''
+   sed -i '/dist_.*_DATA = \.keep/d' src/Makefile.am
+  '';
+
+  configureFlags = [
+    "--localstatedir=/var"
+  ];
+
+  # App fails on runtime if the default config file is not detected
+  # The upstream installer does prepare an empty file too
+  postInstall = ''
+    touch $out/etc/netdata/netdata.conf
+  '';
+
   meta = with stdenv.lib; {
     description = "Real-time performance monitoring tool";
     homepage = http://netdata.firehol.org;
