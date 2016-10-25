5245e4f1c2128021b4ddff521fa147e7bde290fd
= <anothersms@gmail.com>
2016-10-25 21:20:33 +0200

    pixiewps: init at 1.2.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3225,6 +3225,8 @@ in
 
   pius = callPackage ../tools/security/pius { };
 
+  pixiewps = callPackage ../tools/networking/pixiewps {};
+
   pk2cmd = callPackage ../tools/misc/pk2cmd { };
 
   plantuml = callPackage ../tools/misc/plantuml { };
