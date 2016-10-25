3585e3e9147f16349a8ebeede4c62faac41ca9bb
Frederik Rietdijk <fridh@fridh.nl>
2016-10-25 13:51:20 +0200

    vtk: build Python bindings

--- pkgs/development/libraries/vtk/default.nix
+++ pkgs/development/libraries/vtk/default.nix
@@ -1,5 +1,6 @@
 { stdenv, fetchurl, fetchpatch, cmake, mesa, libX11, xproto, libXt
 , qtLib ? null
+, python, withPython ? true
 # Darwin support
 , Cocoa, CoreServices, DiskArbitration, IOKit, CFNetwork, Security, GLUT, OpenGL
 , ApplicationServices, CoreText, IOSurface, cf-private, ImageIO, xpc, libobjc }:
@@ -20,12 +21,11 @@ stdenv.mkDerivation rec {
     sha256 = "1hrjxkcvs3ap0bdhk90vymz5pgvxmg7q6sz8ab3wsyddbshr1abq";
   };
 
-  buildInputs =
-    if !stdenv.isDarwin
-    then [ cmake mesa libX11 xproto libXt ] ++ optional (qtLib != null) qtLib
-    else [ cmake qtLib xpc CoreServices DiskArbitration IOKit cf-private
-           CFNetwork Security ApplicationServices CoreText IOSurface ImageIO
-           OpenGL GLUT ];
+  buildInputs = [ python cmake  ]
+    ++ optionals (!stdenv.isDarwin) ([ mesa libX11 xproto libXt ] ++ optional (qtLib != null) qtLib)
+    ++ optionals stdenv.isDarwin [ qtLib xpc CoreServices DiskArbitration IOKit cf-private
+      CFNetwork Security ApplicationServices CoreText IOSurface ImageIO
+      OpenGL GLUT ];
   propagatedBuildInputs = stdenv.lib.optionals stdenv.isDarwin [ Cocoa libobjc ];
 
 
@@ -39,11 +39,12 @@ stdenv.mkDerivation rec {
   # At least, we use -fPIC for other packages to be able to use this in shared
   # objects.
   cmakeFlags = [ "-DCMAKE_C_FLAGS=-fPIC" "-DCMAKE_CXX_FLAGS=-fPIC" ]
-    ++ optional (qtLib != null) [ "-DVTK_USE_QT:BOOL=ON" ]
-    ++ optional stdenv.isDarwin [ "-DBUILD_TESTING:BOOL=OFF"
-                                  "-DCMAKE_OSX_SYSROOT="
-                                  "-DCMAKE_OSX_DEPLOYMENT_TARGET="
-                                  "-DOPENGL_INCLUDE_DIR=${OpenGL}/Library/Frameworks" ];
+    ++ optional withPython "-DVTK_PYTHON_VERSION=2"
+    ++ optional (qtLib != null) "-DVTK_USE_QT:BOOL=ON"
+    ++ optionals stdenv.isDarwin [ "-DBUILD_TESTING:BOOL=OFF"
+                                   "-DCMAKE_OSX_SYSROOT="
+                                   "-DCMAKE_OSX_DEPLOYMENT_TARGET="
+                                   "-DOPENGL_INCLUDE_DIR=${OpenGL}/Library/Frameworks" ];
 
   postPatch = stdenv.lib.optionalString stdenv.isDarwin ''
     sed -i 's|COMMAND vtkHashSource|COMMAND "DYLD_LIBRARY_PATH=''${VTK_BINARY_DIR}/lib" ''${VTK_BINARY_DIR}/bin/vtkHashSource-7.0|' ./Parallel/Core/CMakeLists.txt
