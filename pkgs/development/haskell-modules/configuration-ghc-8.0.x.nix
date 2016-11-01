6bbdad7d111a30d28ebed085bbdedafe1201910f
Rickard Nilsson <rickynils@gmail.com>
2016-11-01 11:34:14 +0100

    haskell-xxhash: Use doJailbreak instead of sed

--- pkgs/development/haskell-modules/configuration-ghc-8.0.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.0.x.nix
@@ -60,10 +60,7 @@ self: super: {
     sha256 = "026vv2k3ks73jngwifszv8l59clg88pcdr4mz0wr0gamivkfa1zy";
   });
 
-  xxhash = overrideCabal super.xxhash (drv: {
-    postPatch = (drv.postPatch or "") + ''
-      sed -i -e 's| && <4.7||g' xxhash.cabal
-    '';
-  });
+  # https://github.com/christian-marie/xxhash/issues/3
+  xxhash = doJailbreak super.xxhash;
 
 }
