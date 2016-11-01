ed9f1c575b335e31141601190cf06abb9281c99d
Peter Simons <simons@cryp.to>
2016-11-01 19:01:49 +0100

    git-annex: update version

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -43,7 +43,7 @@ self: super: {
     src = pkgs.fetchFromGitHub {
       owner = "joeyh";
       repo = "git-annex";
-      sha256 = "1nd1q5c4jr9s6xczyv464zq4y10rk8c1av22nfb28abrskxagcjc";
+      sha256 = "1np1v2x5n9dl39cbwlqbjap1j5120q4n8p18cm1884vdxidbkc01";
       rev = drv.version;
     };
   })).overrideScope (self: super: {
