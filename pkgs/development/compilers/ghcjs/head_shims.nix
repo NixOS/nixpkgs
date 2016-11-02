c491309d612acd13f5c149399372f2467705140b
Travis Whitaker <pi.boy.travis@gmail.com>
2016-11-01 19:04:53 -0700

    ghcjs: add ghcsjHEAD, tracking ghc-8.0 branch
    Closes https://github.com/NixOS/nixpkgs/pull/20071.
    Closes https://github.com/NixOS/nixpkgs/issues/19905.

---
+++ pkgs/development/compilers/ghcjs/head_shims.nix
@@ -0,0 +1,7 @@
+{ fetchFromGitHub }:
+fetchFromGitHub {
+  owner = "ghcjs";
+  repo = "shims";
+  rev = "1f555d3ca072c61862cc35f92f5ac05f3b938a37";
+  sha256 = "1pciyrlrp5i9s4s8ai4dvhihcahazva6fg0graxxxkjdvnl789ws";
+}
