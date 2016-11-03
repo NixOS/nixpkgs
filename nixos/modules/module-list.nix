f7c097556ba96375bf69b13ed3b0613e59722011
Matthew Daiter <matthew@nomoko.camera>
2016-11-03 17:49:36 +0100

    stanchion: init at 2.1.1

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -159,6 +159,7 @@
   ./services/databases/postgresql.nix
   ./services/databases/redis.nix
   ./services/databases/riak.nix
+  ./services/databases/stanchion.nix
   ./services/databases/virtuoso.nix
   ./services/desktops/accountsservice.nix
   ./services/desktops/geoclue2.nix
