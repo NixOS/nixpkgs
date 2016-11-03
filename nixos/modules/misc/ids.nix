bfc1ba475ab42cbf5b7ff15b9248afa85a0f2b97
Matthew Daiter <matthew@nomoko.camera>
2016-11-03 18:20:08 +0100

    riak-cs: init at 2.1.1
    riak-cs: added to all-packages
    
    Added Riak CS nix file to pkgs
    
    Added service file for Riak CS
    
    Removed Erlang_basho specific bindings from the Riak CS repo
    
    riak-cs: changed description
    
    riak-cs: added license
    
    riak-cs: added maintainer
    
    riak_cs: chgned indentations
    
    riak-cs: removed overly complex srcs mechanism
    
    riak-cs: added systemd module
    
    riak-cs: changed Erlang module to Basho-specific version
    
    riak-cs: made modular form
    
    riak-cs: Added a default package in service options
    
    riak-cs: Fixed default package in service options
    
    riak-cs: Patched Makefile
    
    riak_cs: added to module-list
    
    riak_cs: changed from string to actual package in modules

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -280,6 +280,7 @@
       leaps = 260;
       ipfs  = 261;
       stanchion = 262;
+      riak-cs = 263;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -530,6 +531,7 @@
       leaps = 260;
       ipfs = 261;
       stanchion = 262;
+      riak-cs = 263;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
