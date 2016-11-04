50a579f1921e580727127cb679c259639a421aab
Tobias Geerinckx-Rice <me@tobias.gr>
2016-11-04 16:24:01 +0100

    dmd: 2.067.1 -> 2.070.2

--- pkgs/development/compilers/dmd/default.nix
+++ pkgs/development/compilers/dmd/default.nix
@@ -1,11 +1,12 @@
 { stdenv, fetchurl, unzip, curl, makeWrapper }:
 
-stdenv.mkDerivation {
-  name = "dmd-2.067.1";
+stdenv.mkDerivation rec {
+  name = "dmd-${version}";
+  version = "2.070.2";
 
   src = fetchurl {
-    url = http://downloads.dlang.org/releases/2015/dmd.2.067.1.zip;
-    sha256 = "0ny99vfllvvgcl79pwisxcdnb3732i827k9zg8c0j4s0n79k5z94";
+    url = "http://downloads.dlang.org/releases/2.x/${version}/dmd.${version}.zip";
+    sha256 = "1pbhxxf41v816j0aky3q2pcd8a6phy3363l7vr5r5pg8ps3gl701";
   };
 
   buildInputs = [ unzip curl makeWrapper ];
@@ -24,7 +25,7 @@ stdenv.mkDerivation {
   # Buid and install are based on http://wiki.dlang.org/Building_DMD
   buildPhase = ''
       cd src/dmd
-      make -f posix.mak INSTALL_DIR=$out
+      make -f posix.mak INSTALL_DIR=$out AUTO_BOOTSTRAP=1
       export DMD=$PWD/dmd
       cd ../druntime
       make -f posix.mak INSTALL_DIR=$out DMD=$DMD
