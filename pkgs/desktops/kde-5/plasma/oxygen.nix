753b0f0828efcf63ca66492482abeece4c40e70b
Thomas Tuegel <ttuegel@gmail.com>
2016-11-05 12:13:54 -0500

    oxygen: depend on kwayland
    (cherry picked from commit 87e75a6f88ea7dcf1277d05e71c759336fbee920)

--- pkgs/desktops/kde-5/plasma/oxygen.nix
+++ pkgs/desktops/kde-5/plasma/oxygen.nix
@@ -1,16 +1,16 @@
-{ plasmaPackage, ecm, ki18n, kcmutils, kconfig
-, kdecoration, kguiaddons, kwidgetsaddons, kservice, kcompletion
-, frameworkintegration, kwindowsystem, makeQtWrapper, qtx11extras
+{
+  plasmaPackage,
+  ecm, makeQtWrapper,
+  frameworkintegration, kcmutils, kcompletion, kconfig, kdecoration, kguiaddons,
+  ki18n, kwidgetsaddons, kservice, kwayland, kwindowsystem, qtx11extras
 }:
 
 plasmaPackage {
   name = "oxygen";
-  nativeBuildInputs = [
-    ecm makeQtWrapper
-  ];
+  nativeBuildInputs = [ ecm makeQtWrapper ];
   propagatedBuildInputs = [
-    kcmutils kconfig kdecoration kguiaddons kwidgetsaddons kservice kcompletion
-    frameworkintegration ki18n kwindowsystem qtx11extras
+    frameworkintegration kcmutils kcompletion kconfig kdecoration kguiaddons
+    ki18n kservice kwayland kwidgetsaddons kwindowsystem qtx11extras
   ];
   postInstall = ''
     wrapQtProgram "$out/bin/oxygen-demo5"
