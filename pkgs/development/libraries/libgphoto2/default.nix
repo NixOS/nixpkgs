f4e27735678f1861ecc6d73407bcd4a46ffb0ce1
Tim Steinbach <tim@nequissimus.com>
2016-11-06 09:05:08 -0500

    libgphoto2: Refactor to use GitHub

--- pkgs/development/libraries/libgphoto2/default.nix
+++ pkgs/development/libraries/libgphoto2/default.nix
@@ -1,21 +1,24 @@
-{ stdenv, fetchurl, pkgconfig, libusb1, libtool, libexif, libjpeg, gettext }:
+{ stdenv, fetchpatch, fetchFromGitHub, pkgconfig, libusb1, libtool, libexif, libjpeg, gettext, autoreconfHook }:
 
 stdenv.mkDerivation rec {
   name = "libgphoto2-${meta.version}";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/gphoto/${name}.tar.bz2";
-    sha256 = "1wjf79ipqwb5phfjjwf15rwgigakylnfqaj4crs5qnds6ba6i1ld";
+  src = fetchFromGitHub {
+    owner = "gphoto";
+    repo = "libgphoto2";
+    rev = "${meta.tag}";
+    sha256 = "17k3jxib2jcr2wk83p34h3lvvjbs2gqhqfcngm8zmlrwb385yalh";
   };
 
-  patches = [(fetchurl {
+  patches = [(fetchpatch {
+    name = "libjpeg_turbo_1.5.0_fix.patch";
     url = "https://anonscm.debian.org/cgit/pkg-phototools/libgphoto2.git/plain"
       + "/debian/patches/libjpeg_turbo_1.5.0_fix.patch?id=8ce79a2a02d";
-    sha256 = "114iyhk6idxz2jhnzpf1glqm6d0x0y8cqfpqxz9i96q9j7x3wwin";
+    sha256 = "1zclgg20nv4krj8gigq3ylirxqiv1v8p59cfji041m156hy80gy2";
   })];
 
-  nativeBuildInputs = [ pkgconfig gettext ];
-  buildInputs = [ libtool libjpeg libusb1 ];
+  nativeBuildInputs = [ pkgconfig gettext autoreconfHook ];
+  buildInputs = [ libtool libjpeg libusb1  ];
 
   # These are mentioned in the Requires line of libgphoto's pkg-config file.
   propagatedBuildInputs = [ libexif ];
@@ -31,6 +34,7 @@ stdenv.mkDerivation rec {
       from digital cameras.
     '';
     version = "2.5.10";
+    tag = "libgphoto2-2_5_10-release";
     # XXX: the homepage claims LGPL, but several src files are lgpl21Plus
     license = stdenv.lib.licenses.lgpl21Plus;
     platforms = with stdenv.lib.platforms; unix;
