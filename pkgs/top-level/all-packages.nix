edc5cdc3c840e73224ee0555558d30189cde8fc7
mingchuan <ming@culpring.com>
2016-11-09 02:29:43 +0800

    opencc: init at 1.0.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2992,6 +2992,8 @@ in
 
   oh-my-zsh = callPackage ../shells/oh-my-zsh { };
 
+  opencc = callPackage ../tools/text/opencc { };
+
   opencryptoki = callPackage ../tools/security/opencryptoki { };
 
   opendbx = callPackage ../development/libraries/opendbx { };
