48dce4ad7be99ccb5e9710aaa19855e85fdd69a3
Eelco Dolstra <edolstra@gmail.com>
2016-11-09 15:28:57 +0100

    libraw: 0.17.1 -> 0.17.2
    Also apply a fix from Debian for building with GCC 6.

--- pkgs/development/libraries/libraw/default.nix
+++ pkgs/development/libraries/libraw/default.nix
@@ -2,12 +2,19 @@
 
 stdenv.mkDerivation rec {
   name = "libraw-${version}";
-  version = "0.17.1";
+  version = "0.17.2";
 
   src = fetchurl {
     url = "http://www.libraw.org/data/LibRaw-${version}.tar.gz";
-    sha256 = "18fygk896gxbx47nh2rn5jp4skisgkl6pdfjqb7h0zn39hd6b6g5";
+    sha256 = "0p6imxpsfn82i0i9w27fnzq6q6gwzvb9f7sygqqakv36fqnc9c4j";
   };
+
+  patches =
+    [ (fetchurl {
+        url = https://anonscm.debian.org/cgit/pkg-phototools/libraw.git/plain/debian/patches/0001-Fix_gcc6_narrowing_conversion.patch?id=d890937aaca6359df45a66b35e547c94ca564823;
+        sha256 = "1lcg5l0wmwiyzhhm67c1c7hy8py6ihxfmicnhrwpi3i6f16vq29w";
+      })
+    ];
 
   outputs = [ "out" "lib" "dev" "doc" ];
 
