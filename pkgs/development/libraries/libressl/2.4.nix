52f1a3789839281ab0f7fad9506f34d6dd379225
Franz Pletz <fpletz@fnordicwalking.de>
2016-11-09 20:02:45 +0100

    libressl_2_4: 2.4.3 -> 2.4.4

--- pkgs/development/libraries/libressl/2.4.nix
+++ pkgs/development/libraries/libressl/2.4.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "libressl-${version}";
-  version = "2.4.3";
+  version = "2.4.4";
 
   src = fetchurl {
     url    = "mirror://openbsd/LibreSSL/${name}.tar.gz";
-    sha256 = "03ln2ac9cv0pbz3109yjk7cr5yxqfk8ld6g61k9sgrs7wbrjcmxx";
+    sha256 = "1ldzxqc0bds9mwnirrckhx42y3k0v5cx997nnbfa2gkk6ilszkvg";
   };
 
   enableParallelBuilding = true;
