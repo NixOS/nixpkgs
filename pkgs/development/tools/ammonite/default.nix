b19edd6d0b96c16954d18fc872b377d9bcda83cc
Tim Steinbach <tim@nequissimus.com>
2016-11-09 19:44:42 -0500

    ammonite-repl: 0.7.8 -> 0.7.9

--- pkgs/development/tools/ammonite/default.nix
+++ pkgs/development/tools/ammonite/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "ammonite-repl-${version}";
-  version = "0.7.8";
+  version = "0.7.9";
 
   src = fetchurl {
     url = "https://github.com/lihaoyi/Ammonite/releases/download/${version}/${version}";
-    sha256 = "0s34p168h5c7ij61rbmaygb95r654yj4j0wh6qya53k4ywl32vkp";
+    sha256 = "1jfb5rmab45j0bizfmjhygsjzijv3zcrygws3cr5awjd6js6jg27";
   };
 
   propagatedBuildInputs = [ jre ] ;
