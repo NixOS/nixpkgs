9f46587841448decbb1ec8e46076e36838f82c93
Matthew Bauer <mjbauer95@gmail.com>
2016-11-09 22:30:32 -0600

    network_cmds: init at 481.20.1
    Fixes #16764

--- pkgs/os-specific/darwin/apple-source-releases/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/default.nix
@@ -35,6 +35,7 @@ let
       libunwind     = "35.3";
       Librpcsvc     = "26";
       developer_cmds= "62";
+      network_cmds  = "481.20.1";
       basic_cmds    = "55";
       adv_cmds      = "163";
     };
@@ -221,6 +222,7 @@ let
     adv_cmds        = applePackage "adv_cmds/xcode.nix" "osx-10.11.6"    "12gbv35i09aij9g90p6b3x2f3ramw43qcb2gjrg8lzkzmwvcyw9q" {};
     basic_cmds      = applePackage "basic_cmds"        "osx-10.11.6"     "0hvab4b1v5q2x134hdkal0rmz5gsdqyki1vb0dbw4py1bqf0yaw9" {};
     developer_cmds  = applePackage "developer_cmds"    "osx-10.11.6"     "1r9c2b6dcl22diqf90x58psvz797d3lxh4r2wppr7lldgbgn24di" {};
+    network_cmds    = applePackage "network_cmds"      "osx-10.11.6"     "0lhi9wz84qr1r2ab3fb4nvmdg9gxn817n5ldg7zw9gnf3wwn42kw" {};
 
     libsecurity_apple_csp      = libsecPackage "libsecurity_apple_csp"      "osx-10.7.5" "1ngyn1ik27n4x981px3kfd1z1n8zx7r5w812b6qfjpy5nw4h746w" {};
     libsecurity_apple_cspdl    = libsecPackage "libsecurity_apple_cspdl"    "osx-10.7.5" "1svqa5fhw7p7njzf8bzg7zgc5776aqjhdbnlhpwmr5hmz5i0x8r7" {};
