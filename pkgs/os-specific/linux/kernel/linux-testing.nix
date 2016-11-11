ad19b9bde532841c727618eeb3d3457fd7b98c6d
Tim Steinbach <tim@nequissimus.com>
2016-11-10 21:08:28 -0500

    linux: 4.9-rc3 -> 4.9-rc4

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.9-rc3";
-  modDirVersion = "4.9.0-rc3";
+  version = "4.9-rc4";
+  modDirVersion = "4.9.0-rc4";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/testing/linux-${version}.tar.xz";
-    sha256 = "16dvjxh1i0s18mzm2bcj1v1drv7n2id39jgy71n7i5pyvzc5ffhj";
+    sha256 = "09w3milw95916g1mc19phw6w51kma7f1i3dfjar2ijnrcri3hvmx";
   };
 
   features.iwlwifi = true;
