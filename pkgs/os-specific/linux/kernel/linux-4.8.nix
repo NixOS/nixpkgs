9ab45d9631cb2119555f747440c70645ce9a8889
Tim Steinbach <NeQuissimus@users.noreply.github.com>
2016-11-10 21:12:06 -0500

    Merge pull request #20322 from NeQuissimus/linux_4_8_7
    linux: 4.8.6 -> 4.8.7
    (cherry picked from commit 933dfca167d7c1bbcb57a11d45fc7c2ea63c897e)

--- pkgs/os-specific/linux/kernel/linux-4.8.nix
+++ pkgs/os-specific/linux/kernel/linux-4.8.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.8.6";
+  version = "4.8.7";
   extraMeta.branch = "4.8";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0szk5m4wj6w0avpri9168acid8apbsjv78wz0k4cymh88804wx3l";
+    sha256 = "03qgqqy1kxdqpb844g2yfyq45xy77110hrz8ddqy0rlkia8kzx8m";
   };
 
   kernelPatches = args.kernelPatches;
