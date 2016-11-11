65f93413708a1aa6b43b573f5d8bebe50fc0f58c
Nikolay Amiantov <ab@fmap.me>
2016-11-11 03:49:02 +0300

    sane service: add saned support

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -84,7 +84,7 @@
       spamd = 56;
       #networkmanager = 57; # unused
       nslcd = 58;
-      #scanner = 59; # unused
+      scanner = 59;
       nginx = 60;
       chrony = 61;
       #systemd-journal = 62; # unused
