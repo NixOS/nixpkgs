49310f947063506a96d3b15da004ca42fb415f3b
Franz Pletz <fpletz@fnordicwalking.de>
2016-11-12 16:25:24 +0100

    neo4j: fix bad merge

--- pkgs/servers/nosql/neo4j/default.nix
+++ pkgs/servers/nosql/neo4j/default.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
     sha256 = "01g88b9b1510jbks9v4xr4hyw8zy9nivxsl86xi810xi7qb53np1";
   };
 
-  buildInputs = [ makeWrapper jre8 which gawk ];
+  buildInputs = [ makeWrapper jre which gawk ];
 
   installPhase = ''
     mkdir -p "$out/share/neo4j"
