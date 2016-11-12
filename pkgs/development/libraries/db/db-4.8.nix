c7d1925b6eb805acbeba9e9324969bf67965d350
Daiderd Jordan <daiderd@gmail.com>
2016-11-13 00:42:17 +0100

    Revert "db: fix build with libc++-3.8"
    This reverts commit 7e933af17344c049cdb13bffeafdb82f0927f3bc.

--- pkgs/development/libraries/db/db-4.8.nix
+++ pkgs/development/libraries/db/db-4.8.nix
@@ -5,23 +5,5 @@ import ./generic.nix (args // rec {
   extraPatches = [ ./clang-4.8.patch ];
   sha256 = "0ampbl2f0hb1nix195kz1syrqqxpmvnvnfvphambj7xjrl3iljg0";
   branch = "4.8";
-  drvArgs.hardeningDisable = [ "format" ];
-
-  # https://community.oracle.com/thread/3952592
-  # this patch renames some sybols that conflict with libc++-3.8
-  # symbols: atomic_compare_exchange, atomic_init, store
-  drvArgs.prePatch = ''
-    substituteInPlace dbinc/db.in \
-      --replace '#define	store' '#define	store_db'
-
-    substituteInPlace dbinc/atomic.h \
-      --replace atomic_compare_exchange atomic_compare_exchange_db \
-      --replace atomic_init atomic_init_db
-    substituteInPlace mp/*.c \
-      --replace atomic_compare_exchange atomic_compare_exchange_db \
-      --replace atomic_init atomic_init_db
-    substituteInPlace mutex/*.c \
-      --replace atomic_compare_exchange atomic_compare_exchange_db \
-      --replace atomic_init atomic_init_db
-  '';
+  drvArgs = { hardeningDisable = [ "format" ]; };
 })
--- pkgs/development/libraries/db/generic.nix
+++ pkgs/development/libraries/db/generic.nix
@@ -20,24 +20,6 @@ stdenv.mkDerivation (rec {
 
   patches = extraPatches;
 
-  # https://community.oracle.com/thread/3952592
-  # this patch renames some sybols that conflict with libc++-3.8
-  # symbols: atomic_compare_exchange, atomic_init, store
-  prePatch = ''
-    substituteInPlace src/dbinc/db.in \
-      --replace '#define	store' '#define	store_db'
-
-    substituteInPlace src/dbinc/atomic.h \
-      --replace atomic_compare_exchange atomic_compare_exchange_db \
-      --replace atomic_init atomic_init_db
-    substituteInPlace src/mp/*.c \
-      --replace atomic_compare_exchange atomic_compare_exchange_db \
-      --replace atomic_init atomic_init_db
-    substituteInPlace src/mutex/*.c \
-      --replace atomic_compare_exchange atomic_compare_exchange_db \
-      --replace atomic_init atomic_init_db
-  '';
-
   configureFlags = [
     (if cxxSupport then "--enable-cxx" else "--disable-cxx")
     (if compat185 then "--enable-compat185" else "--disable-compat185")
