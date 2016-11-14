a6c2b2ef0efb15fc2e60ae8a99c28b1daa003309
Thomas Tuegel <ttuegel@gmail.com>
2016-11-13 20:29:46 -0600

    gtkmm3: multiple outputs

--- pkgs/development/libraries/gtkmm/3.x.nix
+++ pkgs/development/libraries/gtkmm/3.x.nix
@@ -12,6 +12,8 @@ stdenv.mkDerivation rec {
     sha256 = "05da4d4b628fb20c8384630ddf478a3b5562952b2d6181fe28d58f6cbc0514f5";
   };
 
+  outputs = [ "out" "dev" ];
+
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ epoxy ];
 
