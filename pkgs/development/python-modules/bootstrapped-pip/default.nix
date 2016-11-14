d83d2ba4f3b831ded72c7b29a2ddb9b7a528efca
Frederik Rietdijk <fridh@fridh.nl>
2016-11-14 21:58:43 +0100

    pythonPackages.setuptools: 26.1.1 -> 28.8.0

--- pkgs/development/python-modules/bootstrapped-pip/default.nix
+++ pkgs/development/python-modules/bootstrapped-pip/default.nix
@@ -6,8 +6,8 @@ let
     sha256 = "ea8033fc9905804e652f75474d33410a07404c1a78dd3c949a66863bd1050ebd";
   };
   setuptools_source = fetchurl {
-    url = "https://files.pythonhosted.org/packages/3b/c7/e9724e6f81c96248fba5876054418c11d327b3093d075790903cd66fad44/setuptools-26.1.1-py2.py3-none-any.whl";
-    sha256 = "226c9ce65e76c6069e805982b036f36dc4b227b37dd87fc219aef721ec8604ae";
+    url = "https://files.pythonhosted.org/packages/3b/7e/293d19ccd106119e35db4bf3e111b1895098f618b455b758aa636496cf03/setuptools-28.8.0-py2.py3-none-any.whl";
+    sha256 = "2e59d06ac798efce29c567ee0e0687514efca529a665b8f364cf497cd10d21b2";
   };
   argparse_source = fetchurl {
     url = "https://pypi.python.org/packages/2.7/a/argparse/argparse-1.4.0-py2.py3-none-any.whl";
