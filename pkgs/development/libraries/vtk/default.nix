78f9bcddc1d70fe035a44feb1a64579bb9849457
Frederik Rietdijk <fridh@fridh.nl>
2016-11-14 16:33:09 +0100

    vtk: use system libraries

--- pkgs/development/libraries/vtk/default.nix
+++ pkgs/development/libraries/vtk/default.nix
@@ -1,4 +1,5 @@
 { stdenv, fetchurl, fetchpatch, cmake, mesa, libX11, xproto, libXt
+, libjpeg, libtiff, netcdf, netcdfcxx4, netcdffortran, expat, freetype, libxml2, libogg, libtheora, jsoncpp
 , qtLib ? null
 , python, withPython ? true
 # Darwin support
@@ -12,22 +13,37 @@ let
   majorVersion = "7.0";
   minorVersion = "0";
   version = "${majorVersion}.${minorVersion}";
-in
 
-stdenv.mkDerivation rec {
+  # Remove these bundled packages
+  bundled = [
+    "expat"
+    "freetype"
+    "jpeg"
+    "jsoncpp"
+    "tiff"
+    "libxml2"
+    "hdf5"
+    "netcdf"
+    "oggtheora"
+    "png"
+  ];
+
+in stdenv.mkDerivation rec {
   name = "vtk-${os (qtLib != null) "qvtk-"}${version}";
   src = fetchurl {
     url = "${meta.homepage}files/release/${majorVersion}/VTK-${version}.tar.gz";
     sha256 = "1hrjxkcvs3ap0bdhk90vymz5pgvxmg7q6sz8ab3wsyddbshr1abq";
   };
 
-  buildInputs = [ python cmake  ]
+  buildInputs = [ cmake libjpeg libtheora libtiff libxml2 netcdf netcdf.hdf5 expat freetype jsoncpp ]
     ++ optionals (!stdenv.isDarwin) ([ mesa libX11 xproto libXt ] ++ optional (qtLib != null) qtLib)
     ++ optionals stdenv.isDarwin [ qtLib xpc CoreServices DiskArbitration IOKit cf-private
       CFNetwork Security ApplicationServices CoreText IOSurface ImageIO
-      OpenGL GLUT ];
+      OpenGL GLUT ]
+    ++ optionals withPython [
+      python
+    ];
   propagatedBuildInputs = stdenv.lib.optionals stdenv.isDarwin [ Cocoa libobjc ];
-
 
   preBuild = ''
     export LD_LIBRARY_PATH="$(pwd)/lib";
@@ -38,15 +54,35 @@ stdenv.mkDerivation rec {
   # built and requiring one of the shared objects.
   # At least, we use -fPIC for other packages to be able to use this in shared
   # objects.
-  cmakeFlags = [ "-DCMAKE_C_FLAGS=-fPIC" "-DCMAKE_CXX_FLAGS=-fPIC" ]
-    ++ optional withPython "-DVTK_PYTHON_VERSION=2"
-    ++ optional (qtLib != null) "-DVTK_USE_QT:BOOL=ON"
-    ++ optionals stdenv.isDarwin [ "-DBUILD_TESTING:BOOL=OFF"
-                                   "-DCMAKE_OSX_SYSROOT="
-                                   "-DCMAKE_OSX_DEPLOYMENT_TARGET="
-                                   "-DOPENGL_INCLUDE_DIR=${OpenGL}/Library/Frameworks" ];
+  cmakeFlags = [
+    "-DCMAKE_C_FLAGS=-fPIC"
+    "-DCMAKE_CXX_FLAGS=-fPIC"
+    "-DVTK_USE_SYSTEM_LIBRARIES=ON"
+    "-DVTK_USE_SYSTEM_LIBPROJ4=OFF" # We don't have it, and its a mess.
+    "-DNETCDF_CXX_INCLUDE_DIR=${netcdfcxx4}/include"
+    "-DNETCDF_CXX_LIBRARY=${netcdfcxx4}/lib"
+    "-DNETCDF_F90_INCLUDE_DIR=${netcdffortran}/include"
+    "-DNETCDF_F90_LIBRARY=${netcdffortran}/lib"
 
-  postPatch = stdenv.lib.optionalString stdenv.isDarwin ''
+  ] ++ optionals withPython [
+    "-DVTK_PYTHON_VERSION=${substring 0 1 python.majorVersion}"
+    "-DVTK_WRAP_PYTHON=ON"
+  ] ++ optionals (qtLib != null) [
+    "-DVTK_USE_QT:BOOL=ON"
+  ] ++ optionals stdenv.isDarwin [
+    "-DBUILD_TESTING:BOOL=OFF"
+    "-DCMAKE_OSX_SYSROOT="
+    "-DCMAKE_OSX_DEPLOYMENT_TARGET="
+    "-DOPENGL_INCLUDE_DIR=${OpenGL}/Library/Frameworks"
+  ];
+
+  postPatch = ''
+    for x in ${concatStringsSep " " bundled}; do
+      echo "Dropping bundled $x"
+      rm -r ThirdParty/$x/vtk$x
+    done
+    ls -Al ThirdParty/netcdf
+  '' + stdenv.lib.optionalString stdenv.isDarwin ''
     sed -i 's|COMMAND vtkHashSource|COMMAND "DYLD_LIBRARY_PATH=''${VTK_BINARY_DIR}/lib" ''${VTK_BINARY_DIR}/bin/vtkHashSource-7.0|' ./Parallel/Core/CMakeLists.txt
     sed -i 's/fprintf(output, shift)/fprintf(output, "%s", shift)/' ./ThirdParty/libxml2/vtklibxml2/xmlschemas.c
     sed -i 's/fprintf(output, shift)/fprintf(output, "%s", shift)/g' ./ThirdParty/libxml2/vtklibxml2/xpath.c
