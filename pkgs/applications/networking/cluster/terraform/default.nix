215a41355b12d016b0f2553a9560626c79ebb94b
zimbatm <zimbatm@zimbatm.com>
2016-11-14 12:21:10 +0000

    terraform: 0.7.9 -> 0.7.10

--- pkgs/applications/networking/cluster/terraform/default.nix
+++ pkgs/applications/networking/cluster/terraform/default.nix
@@ -2,7 +2,7 @@
 
 buildGoPackage rec {
   name = "terraform-${version}";
-  version = "0.7.9";
+  version = "0.7.10";
   rev = "v${version}";
 
   goPackagePath = "github.com/hashicorp/terraform";
@@ -11,7 +11,7 @@ buildGoPackage rec {
     inherit rev;
     owner = "hashicorp";
     repo = "terraform";
-    sha256 = "0fqd6k88zhhq0ncnpr3y3k55wrxpd75fizs54cjgjd873b90sm8h";
+    sha256 = "187hcnrjdysqzjlkj73fpvhi90zmkfkz1fy7g4fx78i1zxqiabz8";
   };
 
   postInstall = ''
