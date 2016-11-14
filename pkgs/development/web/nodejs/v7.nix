eb5831a28eb11574db1811fffcf34acbb5ea8c03
Christine Koppelt <cko@users.noreply.github.com>
2016-11-14 20:25:28 +0100

    nodejs-7_x: 7.0.0 -> 7.1.0

--- pkgs/development/web/nodejs/v7.nix
+++ pkgs/development/web/nodejs/v7.nix
@@ -10,19 +10,12 @@ let
   baseName = if enableNpm then "nodejs" else "nodejs-slim";
 in
   stdenv.mkDerivation (nodejs // rec {
-    version = "7.0.0";
+    version = "7.1.0";
     name = "${baseName}-${version}";
     src = fetchurl {
       url = "https://nodejs.org/download/release/v${version}/node-v${version}.tar.xz";
-      sha256 = "16l9r91z4dxmgc01fs1y8jdh8xjnmyyrq60isyznnxfnq9v3qv71";
+      sha256 = "10a9rwi9v8ylpxydfh1f59smqbljk5axqwghp1qszqwh40d87bjm";
     };
-
-    patches = nodejs.patches ++ [
-      (fetchpatch {
-        url = "https://github.com/nodejs/node/commit/fc164acbbb700fd50ab9c04b47fc1b2687e9c0f4.patch";
-        sha256 = "1rms3n09622xmddn013yvf5c6p3s8w8s0d2h813zs8c1l15k4k1i";
-      })
-    ];
 
   })
 
