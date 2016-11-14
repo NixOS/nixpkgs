e82c325bff0295ed4bbbd308fd4f99cdfd9ab515
Tobias Geerinckx-Rice <me@tobias.gr>
2016-11-14 05:28:22 +0100

    btrfs-progs: 4.8.2 -> 4.8.3

--- pkgs/tools/filesystems/btrfs-progs/default.nix
+++ pkgs/tools/filesystems/btrfs-progs/default.nix
@@ -2,14 +2,14 @@
 , asciidoc, xmlto, docbook_xml_dtd_45, docbook_xsl, libxslt
 }:
 
-let version = "4.8.2"; in
+let version = "4.8.3"; in
 
 stdenv.mkDerivation rec {
   name = "btrfs-progs-${version}";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v${version}.tar.xz";
-    sha256 = "0pswcfmdnfc586770h74abp67gn2xv8fd46vxlimnmn837sj7h41";
+    sha256 = "1wlflrygnpndppil9g12pk184f75g9qx1lkr0x1gijigglqhr9n1";
   };
 
   buildInputs = [
