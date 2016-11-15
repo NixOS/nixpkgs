5ab9ad1ce274bf7c4db75eb049b567f39750338f
Thomas Tuegel <ttuegel@gmail.com>
2016-11-14 21:07:26 -0600

    startkde: initialize kcminputrc if missing

--- pkgs/desktops/kde-5/plasma/startkde/startkde.sh
+++ pkgs/desktops/kde-5/plasma/startkde/startkde.sh
@@ -7,7 +7,8 @@ export QML_IMPORT_PATH="$QML_IMPORT_PATH${QML_IMPORT_PATH:+:}@QML_IMPORT_PATH@"
 export QML2_IMPORT_PATH="$QML2_IMPORT_PATH${QML2_IMPORT_PATH:+:}@QML2_IMPORT_PATH@"
 
 # Set the default GTK 2 theme
-if [ ! -e $HOME/.gtkrc-2.0 -a -e /run/current-system/sw/share/themes/Breeze/gtk-2.0/gtkrc ]; then
+if ! [ -e $HOME/.gtkrc-2.0 ] \
+     && [ -e /run/current-system/sw/share/themes/Breeze/gtk-2.0/gtkrc ]; then
     cat >$HOME/.gtkrc-2.0 <<EOF
 # Default GTK+ 2 config for NixOS KDE 5
 include "/run/current-system/sw/share/themes/Breeze/gtk-2.0/gtkrc"
@@ -21,7 +22,8 @@ gtk-button-images=1
 EOF
 fi
 
-if [ ! -e $HOME/.config/gtk-3.0/settings.ini -a -e /run/current-system/sw/share/themes/Breeze/gtk-3.0 ]; then
+if ! [ -e $HOME/.config/gtk-3.0/settings.ini ] \
+       && [ -e /run/current-system/sw/share/themes/Breeze/gtk-3.0 ]; then
     cat >$HOME/.config/gtk-3.0/settings.ini <<EOF
 [Settings]
 gtk-theme-name=Breeze
@@ -98,6 +100,14 @@ fi
 # We need to create config folder so we can write startupconfigkeys
 configDir=$(qtpaths --writable-path GenericConfigLocation)
 mkdir -p "$configDir"
+
+if ! [ -e $configDir/kcminputrc ]; then
+    cat >$configDir/kcminputrc <<EOF
+[Mouse]
+cursorTheme=breeze_cursors
+cursorSize=0
+EOF
+fi
 
 THEME=org.kde.breeze
 #This is basically setting defaults so we can use them with kstartupconfig5
