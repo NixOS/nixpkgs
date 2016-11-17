ea292da4c253893dc8b2ac44faeaaa21255ccdbd
Vladimír Čunát <vcunat@gmail.com>
2016-11-17 11:21:18 +0100

    libdrm: 2.4.71 -> 2.4.73
    Also:
    - remove udev remains except explanation;
    - remove maintainers.urkud, as he's been inactive for years.

--- pkgs/development/libraries/libdrm/default.nix
+++ pkgs/development/libraries/libdrm/default.nix
@@ -1,11 +1,11 @@
-{ stdenv, fetchurl, pkgconfig, libpthreadstubs, libpciaccess, udev, valgrind }:
+{ stdenv, fetchurl, pkgconfig, libpthreadstubs, libpciaccess, valgrind }:
 
 stdenv.mkDerivation rec {
-  name = "libdrm-2.4.71";
+  name = "libdrm-2.4.73";
 
   src = fetchurl {
     url = "http://dri.freedesktop.org/libdrm/${name}.tar.bz2";
-    sha256 = "c66287ddeee5f46ea8f8880b94b80acb3bbc33ba6321d17767eef145046df9b8";
+    sha256 = "0kayvwqqrjm45y6dk9q2b4gmjdasci0y28jzv4bq05py8a9d7gwn";
   };
 
   outputs = [ "out" "dev" ];
@@ -13,7 +13,6 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ libpthreadstubs libpciaccess ];
     # libdrm as of 2.4.70 does not actually do anything with udev.
-    #++ stdenv.lib.optional stdenv.isLinux udev;
 
   patches = stdenv.lib.optional stdenv.isDarwin ./libdrm-apple.patch;
 
@@ -21,7 +20,6 @@ stdenv.mkDerivation rec {
     "echo : \\\${ac_cv_func_clock_gettime=\'yes\'} > config.cache";
 
   configureFlags = [ "--enable-freedreno" "--disable-valgrind" ]
-    ++ stdenv.lib.optional stdenv.isLinux "--enable-udev"
     ++ stdenv.lib.optional stdenv.isDarwin "-C";
 
   crossAttrs.configureFlags = configureFlags ++ [ "--disable-intel" ];
@@ -30,7 +28,6 @@ stdenv.mkDerivation rec {
     homepage = http://dri.freedesktop.org/libdrm/;
     description = "Library for accessing the kernel's Direct Rendering Manager";
     license = "bsd";
-    maintainers = [ stdenv.lib.maintainers.urkud ];
     platforms = stdenv.lib.platforms.unix;
   };
 }
