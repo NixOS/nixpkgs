0df6305a9840f90ed53f90d08388db11442df492
Vladimír Čunát <vcunat@gmail.com>
2016-11-19 17:53:28 +0100

    llvmPackages: default to 3.9.x (#20461)
    It seems not to be any mass rebuild at all.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5062,7 +5062,7 @@ in
   llvm_35 = llvmPackages_35.llvm;
   llvm_34 = llvmPackages_34.llvm;
 
-  llvmPackages = recurseIntoAttrs llvmPackages_37;
+  llvmPackages = recurseIntoAttrs llvmPackages_39;
 
   llvmPackagesSelf = llvmPackages_34.override {
     stdenv = libcxxStdenv;
