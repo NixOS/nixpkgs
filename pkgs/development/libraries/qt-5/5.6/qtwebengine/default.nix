7906624425874069f1976a710eadb43147630cda
Jos van den Oever <jos@vandenoever.info>
2016-11-20 13:10:47 +0100

    qtwebengine: hardcode paths
    Hardcode the paths for which the default path resolution does not work in nix.

--- pkgs/development/libraries/qt-5/5.6/qtwebengine/default.nix
+++ pkgs/development/libraries/qt-5/5.6/qtwebengine/default.nix
@@ -30,7 +30,12 @@ qtSubmodule {
       --replace /bin/echo ${coreutils}/bin/echo
     substituteInPlace ./src/3rdparty/chromium/v8/build/standalone.gypi \
       --replace /bin/echo ${coreutils}/bin/echo
-      
+
+    # hardcode paths for which default path resolution does not work in nix
+    sed -i -e 's,\(static QString potentialResourcesPath =\).*,\1 QLatin1String("'$out'/resources");,' src/core/web_engine_library_info.cpp
+    sed -i -e 's,\(static QString processPath\),\1 = QLatin1String("'$out'/libexec/QtWebEngineProcess"),' src/core/web_engine_library_info.cpp
+    sed -i -e 's,\(static QString potentialLocalesPath =\).*,\1 QLatin1String("'$out'/translations/qtwebengine_locales");,' src/core/web_engine_library_info.cpp
+
     configureFlags+="\
         -plugindir $out/lib/qt5/plugins \
         -importdir $out/lib/qt5/imports \
