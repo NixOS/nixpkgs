a2b883a1833a99121adfad0c5a5ac0e740897f87
Franz Pletz <fpletz@fnordicwalking.de>
2016-11-20 04:40:55 +0100

    i18n module: only support en_US.UTF-8 locale
    Reduces closure size by about 100MB.

--- nixos/modules/config/i18n.nix
+++ nixos/modules/config/i18n.nix
@@ -30,7 +30,7 @@ in
 
       supportedLocales = mkOption {
         type = types.listOf types.str;
-        default = ["all"];
+        default = ["en_US.UTF-8/UTF-8"];
         example = ["en_US.UTF-8/UTF-8" "nl_NL.UTF-8/UTF-8" "nl_NL/ISO-8859-1"];
         description = ''
           List of locales that the system should support.  The value
