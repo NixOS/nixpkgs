db66a95e5b704b8c1e9c55f186f1dccbfb6ce580
Franz Pletz <fpletz@fnordicwalking.de>
2016-11-21 22:49:02 +0100

    ntp: 4.2.8p8 -> 4.2.8p9
    Includes fixes for 10 CVEs and contains other fixes.
    
    See http://support.ntp.org/bin/view/Main/SecurityNotice#November_2016_ntp_4_2_8p9_NTP_Se.

--- pkgs/tools/networking/ntp/default.nix
+++ pkgs/tools/networking/ntp/default.nix
@@ -3,16 +3,18 @@
 assert stdenv.isLinux -> libcap != null;
 
 stdenv.mkDerivation rec {
-  name = "ntp-4.2.8p8";
+  name = "ntp-4.2.8p9";
 
   src = fetchurl {
     url = "http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/${name}.tar.gz";
-    sha256 = "1vlpgd0dk2wkpmmf869sfxi8f46sfnmjgk51vl8n6vj5y2sx1cra";
+    sha256 = "0whbyf82lrczbri4adbsa4hg1ppfa6c7qcj7nhjwdfp1g1vjh95p";
   };
 
   configureFlags = [
     "--sysconfdir=/etc"
     "--localstatedir=/var"
+    "--with-openssl-libdir=${openssl.out}/lib"
+    "--with-openssl-incdir=${openssl.dev}/include"
     "--enable-ignore-dns-errors"
   ] ++ stdenv.lib.optional (libcap != null) "--enable-linuxcaps";
 
