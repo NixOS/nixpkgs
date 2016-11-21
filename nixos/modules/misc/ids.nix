fc10bf81a1aed60029de813940341aa6b02fd551
James Cook <jcook@cs.berkeley.edu>
2016-11-20 19:48:28 -0800

    Revert "riak-cs: add to nixpkgs"

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -280,7 +280,6 @@
       leaps = 260;
       ipfs  = 261;
       stanchion = 262;
-      riak-cs = 263;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -531,7 +530,6 @@
       leaps = 260;
       ipfs = 261;
       stanchion = 262;
-      riak-cs = 263;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
