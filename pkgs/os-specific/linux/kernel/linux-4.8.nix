e4a1b76457d6c078cd60f8df84fea89a26dedd7b
Tim Steinbach <tim@nequissimus.com>
2016-11-21 18:07:17 -0500

    linux: 4.8.9 -> 4.8.10

--- pkgs/os-specific/linux/kernel/linux-4.8.nix
+++ pkgs/os-specific/linux/kernel/linux-4.8.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.8.9";
+  version = "4.8.10";
   extraMeta.branch = "4.8";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0z8xdr3gqmi295a76rbzngilf49kxxm6ffhr89lbfxnsffyy6m68";
+    sha256 = "1i3hydxjl3zz4i3v2spnv5y5pidmwgiyc10q6rlwvf0bs8aynh53";
   };
 
   kernelPatches = args.kernelPatches;
