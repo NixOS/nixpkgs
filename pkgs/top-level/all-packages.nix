208fd871492afcdec456326657fbf9376b5c5704
Matthias Beyer <mail@beyermatthias.de>
2016-11-21 15:15:29 +0100

    wordgrinder: init at 0.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17157,6 +17157,8 @@ with pkgs;
 
   wordnet = callPackage ../applications/misc/wordnet { };
 
+  wordgrinder = callPackage ../applications/office/wordgrinder { };
+
   worker = callPackage ../applications/misc/worker { };
 
   workrave = callPackage ../applications/misc/workrave {
