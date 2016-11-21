ab43f932ceaf2fb55351ac8b5c0ba87c1d630f5f
Matthew Daiter <matthew@nomoko.camera>
2016-11-21 12:15:29 +0100

    riak: add defaultText to service
    riak: add default to service

--- nixos/modules/services/databases/riak.nix
+++ nixos/modules/services/databases/riak.nix
@@ -20,6 +20,8 @@ in
 
       package = mkOption {
         type = types.package;
+        default = pkgs.riak;
+        defaultText = "pkgs.riak";
         example = literalExample "pkgs.riak";
         description = ''
           Riak package to use.
