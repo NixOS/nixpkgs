b122d654ccf7bb33c70d5bbb81182628eee8be3b
Franz Pletz <fpletz@fnordicwalking.de>
2016-11-22 21:09:14 +0100

    w3m-nox: use imlib2 without X11 support
    Also, the minimal live CD previously installed both the X11 and
    non-X11 versions (through services.nixosManual) of w3m.

--- nixos/modules/profiles/base.nix
+++ nixos/modules/profiles/base.nix
@@ -7,7 +7,7 @@
   # Include some utilities that are useful for installing or repairing
   # the system.
   environment.systemPackages = [
-    pkgs.w3m # needed for the manual anyway
+    pkgs.w3m-nox # needed for the manual anyway
     pkgs.testdisk # useful for repairing boot problems
     pkgs.mssys # for writing Microsoft boot sectors / MBRs
     pkgs.efibootmgr
