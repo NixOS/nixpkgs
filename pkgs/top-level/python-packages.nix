7a109e63108de4ab59010041f0a8a7f19a14a318
Frederik Rietdijk <fridh@fridh.nl>
2016-11-25 10:01:48 +0100

    pythonPackages.docker: support python3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6214,9 +6214,9 @@ in {
       sha256 = "05f49f6hnl7npmi7kigg0ibqk8s3fhzx1ivvz1kqvlv4ay3paajc";
     };
 
-    buildInputs = [
-      pkgs.glibcLocales
-    ];
+    buildInputs = [ pkgs.glibcLocales ];
+
+    LC_ALL="en_US.UTF-8";
 
     propagatedBuildInputs = with self; [
       six
@@ -6227,10 +6227,8 @@ in {
       docker_pycreds
     ];
 
-    # Version conflict
+    # Flake8 version conflict
     doCheck = false;
-
-    LC_ALL="en_US.UTF-8";
 
     meta = {
       description = "An API client for docker written in Python";
