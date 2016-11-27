f7ad0223ce934e59c41ebdcfb8f09bf599d5321c
Frederik Rietdijk <fridh@fridh.nl>
2016-11-27 09:36:51 +0100

    pythonPackages.ipywidgets: fix tests

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12831,7 +12831,7 @@ in {
       sha256 = "baf6098f054dd5eacc2934b8ea3bef908b81ca8660d839f1f940255a72c660d2";
     };
 
-    buildInputs = with self; [ nose ];
+    buildInputs = with self; [ nose pytest ];
     propagatedBuildInputs = with self; [ipython ipykernel traitlets notebook widgetsnbextension ];
 
     meta = {
