448d605f89fb0b66640c1a660b8f1e2b8d4a9893
Matthew Daiter <matthew@nomoko.camera>
2016-11-28 14:16:37 +0100

    opencv3: adding myself as co-maintainer

--- pkgs/development/libraries/opencv/3.x.nix
+++ pkgs/development/libraries/opencv/3.x.nix
@@ -118,7 +118,7 @@ stdenv.mkDerivation rec {
     description = "Open Computer Vision Library with more than 500 algorithms";
     homepage = http://opencv.org/;
     license = stdenv.lib.licenses.bsd3;
-    maintainers = with stdenv.lib.maintainers; [viric flosse];
+    maintainers = with stdenv.lib.maintainers; [viric flosse mdaiter];
     platforms = with stdenv.lib.platforms; linux;
   };
 }
