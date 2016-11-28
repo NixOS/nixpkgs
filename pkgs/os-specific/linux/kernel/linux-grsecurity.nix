77f365a742d76b2a655b70790688f91b13a3e66a
Joachim Fasting <joachifm@fastmail.fm>
2016-11-28 11:38:10 +0100

    grsecurity: 4.8.10-201611232213 -> 4.8.11-201611271225
    (cherry picked from commit b90ed0cc80293e16a219193f4b2495be14fd563b)

--- pkgs/os-specific/linux/kernel/linux-grsecurity.nix
+++ pkgs/os-specific/linux/kernel/linux-grsecurity.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.8.10";
+  version = "4.8.11";
   extraMeta.branch = "4.8";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1i3hydxjl3zz4i3v2spnv5y5pidmwgiyc10q6rlwvf0bs8aynh53";
+    sha256 = "03w90vfjfcya38mcp1njasa5c67za203sgp9n3w52gms13s443yc";
   };
 
   kernelPatches = args.kernelPatches;
