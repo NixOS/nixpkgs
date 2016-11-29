18a3225dace3cde74d60dc0b8840c07a2ded511a
Tim Steinbach <tim@nequissimus.com>
2016-11-29 17:40:17 -0500

    linux: 3.12.67 -> 3.12.68

--- pkgs/os-specific/linux/kernel/linux-3.12.nix
+++ pkgs/os-specific/linux/kernel/linux-3.12.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.12.67";
+  version = "3.12.68";
   extraMeta.branch = "3.12";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "18laiqnqfabrqc6dw2pkimcg30hs865kgaflflav6zz9lb3ls1d5";
+    sha256 = "0k4kwxmm6vj840k4v6iyswsajaxsb5g9vrc7mzr4grflfbjrgh14";
   };
 
   kernelPatches = args.kernelPatches;
