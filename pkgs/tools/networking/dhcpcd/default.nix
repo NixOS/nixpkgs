445d7d87679a508b527153f1913192ae04e1dc4a
Franz Pletz <fpletz@fnordicwalking.de>
2016-12-02 12:51:58 +0100

    dhcpcd: 6.11.3 -> 6.11.5

--- pkgs/tools/networking/dhcpcd/default.nix
+++ pkgs/tools/networking/dhcpcd/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, udev }:
 
 stdenv.mkDerivation rec {
-  name = "dhcpcd-6.11.3";
+  name = "dhcpcd-6.11.5";
 
   src = fetchurl {
     url = "mirror://roy/dhcpcd/${name}.tar.xz";
-    sha256 = "01lv4a7ls55iv9f9gkh6vswqmbpqkdzj4d8ayq4dcir9vz215gas";
+    sha256 = "17nnhxmbdcc7k2mh6sgvxisqcqbic5540xbig363ds97gvf795kg";
   };
 
   buildInputs = [ pkgconfig udev ];
