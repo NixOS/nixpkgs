fa20c0cb5e17cc23ef0b07a7f271b86dbd83e4ef
Vladimír Čunát <vcunat@gmail.com>
2016-12-03 18:07:04 +0100

    xorg-server variants: eradicate symlink to /var/tmp
    - Fixes #19673; it caused problems in combination with buildEnv.
    - As noted, X falls back to /tmp:
      https://github.com/NixOS/nixpkgs/issues/19673#issuecomment-258871876
    - Removing the directory is still required, as X would attempt to write
      into it if allowed - and probably succeed in case the user set
      nix.readOnlyStore = false; (X runs as root).
    - Archeology link: 9d1569316.
    
    (cherry picked from commit 33abc705b31a294d9e788bda5b0b1708c8eed92d)

--- pkgs/servers/x11/xorg/overrides.nix
+++ pkgs/servers/x11/xorg/overrides.nix
@@ -453,8 +453,7 @@ in
           "--enable-glamor"
         ];
         postInstall = ''
-          rm -fr $out/share/X11/xkb/compiled
-          ln -s /var/tmp $out/share/X11/xkb/compiled
+          rm -fr $out/share/X11/xkb/compiled # otherwise X will try to write in it
           wrapProgram $out/bin/Xephyr \
             --set XKB_BINDIR "${xorg.xkbcomp}/bin" \
             --add-flags "-xkbdir ${xorg.xkeyboardconfig}/share/X11/xkb"
@@ -506,7 +505,6 @@ in
         '';
         postInstall = ''
           rm -fr $out/share/X11/xkb/compiled
-          ln -s /var/tmp $out/share/X11/xkb/compiled
 
           cp -rT ${darwinOtherX}/bin $out/bin
           rm -f $out/bin/X
