863e3ea4a05ff79f7b922eaf6a8f4228d7756da4
Dejan Lukan <dejan@proteansec.com>
2016-12-03 10:40:01 +0100

    gopenvpn: updates according to comments

--- pkgs/desktops/gnome-2/desktop/gopenvpn/default.nix
+++ pkgs/desktops/gnome-2/desktop/gopenvpn/default.nix
@@ -2,9 +2,7 @@
   openssl, cyrus_sasl, gnonlin, sylpheed, gob2, gettext, intltool, libglade, polkit, autoconf, automake, expect,
   openvpn, makeWrapper, pkexecPath ? "/var/setuid-wrappers/pkexec" }:
 
-let
-  redirects = [ "/usr/bin/pkexec=${pkexecPath}" ];
-in stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
   rev = "2b9285f1ea2d11434b8ee8e10d937b2ea5285b63";
   version = "0.8";
   name = "gopenvpn-${version}";
@@ -20,11 +18,22 @@ in stdenv.mkDerivation rec {
   buildInputs = [ glib gtk2 dbus_glib gmime libnotify libgnome_keyring openssl cyrus_sasl gnonlin sylpheed gob2
                   gettext intltool libglade polkit pkgconfig autoconf automake expect openvpn makeWrapper ];
 
+  # disable the format hardening flags in order for the program to compile even though a potential vulnerability
+  # might be present in the form of a format string vulnerability. By failing to add this the program compilation
+  # will end with the following line:
+  #
+  #  > gopenvpn.c:978:10: error: format not a string literal and no format arguments [-Werror=format-security]
+  #
+  hardeningDisable = [ "format" ];
+
   patchPhase = ''
     # configure and remote extra "po/Makefile.in" in AC_CONFIG_FILES
     sed -i -e 's:AC_CONFIG_FILES(\[pixmaps/Makefile po/Makefile.in:AC_CONFIG_FILES(\[pixmaps/Makefile:g' configure.ac
     cat configure.ac | grep CONFIG_FILES
   '';
+
+  # configure with pkexec to be able to run openvpn as privileged user
+  configureFlags = ["--with-pkexec=${pkexecPath}/bin/pkexec"];
 
   preConfigure = ''
     # copy standard gettext infrastructure files into a source package
@@ -33,23 +42,6 @@ in stdenv.mkDerivation rec {
 
     # updating configuration files not required anymore due to autoreconfHook
     autoreconf -vif
-  '';
-
-  preBuild = ''
-    # add no warnings flag to the Makefile, otherwise the following error will occur during built time:
-    #
-    #  > gopenvpn.c:978:10: error: format not a string literal and no format arguments [-Werror=format-security]
-    #
-    # Note that this wasn't solved by adding the "-Wformat-nonliteral -Wno-format-security -Wno-error=format-security"
-    # flags
-    sed -i -e '/^CFLAGS =/ s/$/ -w/' src/Makefile
-  '';
-
-  installPhase = ''
-    make install
-    echo $out
-    find $out
-    wrapProgram $out/bin/gopenvpn --set NIX_REDIRECTS ${builtins.concatStringsSep ":" redirects}
   '';
 
   meta = with stdenv.lib; {
