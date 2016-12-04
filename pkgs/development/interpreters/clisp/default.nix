84d737cfe92967b9759f191d1b7c90ff87bea02b
Vladimír Čunát <vcunat@gmail.com>
2016-12-04 09:58:18 +0100

    clisp: remove Darwin from meta.platforms for now
    It's problematic.  See #20062.

--- pkgs/development/interpreters/clisp/default.nix
+++ pkgs/development/interpreters/clisp/default.nix
@@ -26,7 +26,7 @@ assert x11Support -> (libX11 != null && libXau != null && libXt != null
 stdenv.mkDerivation rec {
   v = "2.49";
   name = "clisp-${v}";
-  
+
   src = fetchurl {
     url = "mirror://gnu/clisp/release/${v}/${name}.tar.bz2";
     sha256 = "8132ff353afaa70e6b19367a25ae3d5a43627279c25647c220641fed00f8e890";
@@ -92,6 +92,7 @@ stdenv.mkDerivation rec {
     description = "ANSI Common Lisp Implementation";
     homepage = http://clisp.cons.org;
     maintainers = with stdenv.lib.maintainers; [raskin tohl];
-    platforms = stdenv.lib.platforms.unix;
+    # problems on Darwin: https://github.com/NixOS/nixpkgs/issues/20062
+    platforms = stdenv.lib.platforms.linux;
   };
 }
