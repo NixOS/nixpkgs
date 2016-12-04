110a0fb028438120447e1b1e6f359ca4ee9b398c
Vincent Laporte <Vincent.Laporte@gmail.com>
2016-12-04 10:17:42 +0000

    ocamlPackages.markup: 0.7.2 -> 0.7.3

--- pkgs/development/ocaml-modules/markup/default.nix
+++ pkgs/development/ocaml-modules/markup/default.nix
@@ -1,13 +1,13 @@
-{ stdenv, fetchurl, ocaml, findlib, ocamlbuild, uutf, lwt }:
+{ stdenv, fetchzip, ocaml, findlib, ocamlbuild, uutf, lwt }:
 
 stdenv.mkDerivation rec {
-  pname = "ocaml-markup";
-  version = "0.7.2";
-  name = "${pname}-${version}";
+  pname = "markup";
+  version = "0.7.3";
+  name = "ocaml${ocaml.version}-${pname}-${version}";
 
-  src = fetchurl {
+  src = fetchzip {
     url = "http://github.com/aantron/markup.ml/archive/${version}.tar.gz";
-    sha256 = "0d3wi22v7h0iqzq8dgl0g4fj2wb67gvmbzdckacifghinrx762k3";
+    sha256 = "03vyv609a60azw8qs7v0kkmy4704hkzw7c3skpzax5krwgwcqfxj";
     };
 
   buildInputs = [ ocaml findlib ocamlbuild ];
