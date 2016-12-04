8e8742eac0e8b1e8af23a0ef678d39bdc94b0527
c74d <8573dd@gmail.com>
2016-12-04 05:33:43 +0000

    perlPackages.ModuleBuildPluggable: init at 0.10
    Add the Perl module `Module::Build::Pluggable`, version 0.10,
    generated by `nix-generate-from-cpan`.
    
    I have tested this change per nixpkgs manual section 11.1 ("Making
    patches").

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -8097,6 +8097,20 @@ let self = _self // overrides; _self = with self; {
     };
   };
 
+  ModuleBuildPluggable = buildPerlModule rec {
+    name = "Module-Build-Pluggable-0.10";
+    src = fetchurl {
+      url = "mirror://cpan/authors/id/T/TO/TOKUHIROM/${name}.tar.gz";
+      sha256 = "e5bb2acb117792c984628812acb0fec376cb970caee8ede57535e04d762b0e40";
+    };
+    propagatedBuildInputs = [ ClassAccessorLite ClassMethodModifiers DataOptList ModuleBuild TestSharedFork ];
+    meta = {
+      homepage = https://github.com/tokuhirom/Module-Build-Pluggable;
+      description = "Module::Build meets plugins";
+      license = with stdenv.lib.licenses; [ artistic1 gpl1Plus ];
+    };
+  };
+
   ModuleBuildTiny = buildPerlModule {
     name = "Module-Build-Tiny-0.039";
     src = fetchurl {
