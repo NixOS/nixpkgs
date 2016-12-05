ee2d5e44c2492472faa5a1692ff5d1629dfc2287
Franz Pletz <fpletz@fnordicwalking.de>
2016-12-06 00:00:24 +0100

    pythonPackages.django_1_6: 1.6.11 -> 1.6.11.5
    Switch to the reviewboard fork of Django 1.6 because it's not maintained
    upstream anymore.
    
    See https://www.reviewboard.org/news/2016/11/01/new-django-1-6-11-5-security-releases/.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9956,14 +9956,14 @@ in {
 
   django_1_6 = buildPythonPackage rec {
     name = "Django-${version}";
-    version = "1.6.11";
+    version = "1.6.11.5";
 
     # Support to python-3.4 and higher was introduced in django_1_7
     disabled = !(isPy26 || isPy27 || isPy33);
 
     src = pkgs.fetchurl {
-      url = "http://www.djangoproject.com/m/releases/1.6/${name}.tar.gz";
-      sha256 = "0misvia78c14y07zs5xsb9lv54q0v217jpaindrmhhw4wiryal3y";
+      url = "https://downloads.reviewboard.org/releases/Django/1.6/Django-${version}.tar.gz";
+      sha256 = "0yj0fw3iql031z8l5ik1fb25sk3l5bw2vc63bbyg5rz2k3znl4il";
     };
 
     # too complicated to setup
