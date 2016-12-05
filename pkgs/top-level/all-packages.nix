179c3010f4f54841844cc1972dd393446512da90
Profpatsch <mail@profpatsch.de>
2016-12-05 13:52:56 +0100

    jid: init at 0.6.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2292,6 +2292,8 @@ in
 
   jhead = callPackage ../tools/graphics/jhead { };
 
+  jid = callPackage ../development/tools/jid { };
+
   jing = self.jing-trang;
   jing-trang = callPackage ../tools/text/xml/jing-trang { };
 
