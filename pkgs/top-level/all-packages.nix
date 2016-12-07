30d4eca088ce837cc1df5237fdcd84dff2218130
Eric Sagnes <eric.sagnes@gmail.com>
2016-12-07 15:01:29 +0900

    styx-themes: init

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14596,6 +14596,8 @@ in
 
   styx = callPackage ../applications/misc/styx { };
 
+  styx-themes = callPackage ../applications/misc/styx/themes.nix { };
+
   tecoc = callPackage ../applications/editors/tecoc { };
 
   viber = callPackage ../applications/networking/instant-messengers/viber { };
