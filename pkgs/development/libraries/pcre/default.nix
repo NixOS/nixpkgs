09dbf259d88066af168ca7cddd7814c8ac2e770d
Sophie Taylor <sophie@traumapony.org>
2016-12-07 16:05:31 +1000

    Update default.nix

--- pkgs/development/libraries/pcre/default.nix
+++ pkgs/development/libraries/pcre/default.nix
@@ -33,8 +33,7 @@ in stdenv.mkDerivation rec {
     # XXX: test failure on Cygwin
     # we are running out of stack on both freeBSDs on Hydra
   preCheck = ''
-    paxmark m $sourceDir/.libs/lt-pcretest
-    paxmark m $sourceDir/.libs/lt-pcre-jit-test
+    paxmark m $sourceDir/.libs/lt-pcre_jit_test
     '';
   postFixup = ''
     moveToOutput bin/pcre-config "$dev"
