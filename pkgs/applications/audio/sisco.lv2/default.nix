22a1e589b8ee3b92b310c367d1d0bd7d26c69c20
Alexander Kahl <ak@sodosopa.io>
2016-12-08 17:40:20 +0100

    Use fetchFromGitHub and adapt accordingly

--- pkgs/applications/audio/sisco.lv2/default.nix
+++ pkgs/applications/audio/sisco.lv2/default.nix
@@ -1,33 +1,35 @@
-{ stdenv, fetchurl, lv2, pkgconfig, mesa, cairo, pango, libjack2 }:
+{ stdenv, fetchFromGitHub, lv2, pkgconfig, mesa, cairo, pango, libjack2 }:
 
 let
-  name = "sisco.lv2-${version}";
+  name = "sisco.lv2-${src.rev}";
   version = "0.7.0";
 
   robtkVersion = "80a2585253a861c81f0bfb7e4579c75f5c73af89";
   robtkName = "robtk-${robtkVersion}";
 
-  src = fetchurl {
-    name = "${name}.tar.gz";
-    url = "https://github.com/x42/sisco.lv2/archive/v${version}.tar.gz";
-    sha256 = "af7caccf2660138ab4da9b33b2f0e545516e6e49369682e9c7a7b791e1fda0b2";
+  src = fetchFromGitHub {
+    owner = "x42";
+    repo = "sisco.lv2";
+    rev = "v${version}";
+    sha256 = "1r6g29yqbdqgkh01x6d3nvmvc58rk2dp94fd0qyyizq37a1qplj1";
   };
 
-  robtkSrc = fetchurl {
-    name = "${robtkName}.tar.gz";
-    url = "https://github.com/x42/robtk/archive/${robtkVersion}.tar.gz";
-    sha256 = "81fc907b97705c218670e1fc2642daca6513d11f3d7554a22f8e144877e5c350";
+  robtkSrc = fetchFromGitHub {
+    owner = "x42";
+    repo = "robtk";
+    rev = robtkVersion;
+    sha256 = "0gk16nrvnrffqqw0yd015kja9wkgbzvb648bl1pagriabhznhfxl";
   };
 in
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   inherit name;
 
   srcs = [ src robtkSrc ];
-  sourceRoot = name;
+  sourceRoot = "${name}-src";
 
   buildInputs = [ pkgconfig lv2 pango cairo libjack2 mesa ];
 
-  postUnpack = "mv ${robtkName}/* ${name}/robtk";
+  postUnpack = "chmod u+w -R ${robtkName}-src; mv ${robtkName}-src/* ${sourceRoot}/robtk";
   sisco_VERSION = version;
   preConfigure = "makeFlagsArray=(PREFIX=$out)";
 
