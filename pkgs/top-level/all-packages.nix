f576c490e369b0cc785e789f15523267d98667d2
Tim Steinbach <tim@nequissimus.com>
2016-12-10 15:18:52 -0500

    linux: 4.4.37 -> 4.4.38

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10997,7 +10997,6 @@ in
     kernelPatches =
       [ kernelPatches.bridge_stp_helper
         kernelPatches.cpu-cgroup-v2."4.4"
-        kernelPatches.packet_fix_race_condition_CVE_2016_8655
       ]
       ++ lib.optionals ((platform.kernelArch or null) == "mips")
       [ kernelPatches.mips_fpureg_emu
