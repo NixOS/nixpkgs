6d1180cf023f309438de77afd38c6de3c3eda3f9
Robert Helgesson <robert@rycee.net>
2016-12-13 23:11:30 +0100

    perl-Sub-Quote: init at 2.003001

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -11832,6 +11832,19 @@ let self = _self // overrides; _self = with self; {
     propagatedBuildInputs = [SubUplevel TestException];
   };
 
+  SubQuote = buildPerlPackage rec {
+    name = "Sub-Quote-2.003001";
+    src = fetchurl {
+      url = "mirror://cpan/authors/id/H/HA/HAARG/${name}.tar.gz";
+      sha256 = "9d471d8e13e7ce4793d5a5ec04a60fface14dd53be78dd94d228871915cfd1f9";
+    };
+    buildInputs = [ TestFatal ];
+    meta = {
+      description = "Efficient generation of subroutines via string eval";
+      license = with stdenv.lib.licenses; [ artistic1 gpl1Plus ];
+    };
+  };
+
   SubUplevel = buildPerlPackage {
     name = "Sub-Uplevel-0.24";
     src = fetchurl {
