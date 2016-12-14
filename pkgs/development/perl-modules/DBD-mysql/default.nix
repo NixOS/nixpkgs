971ae27526f7b955058807ff7fb10b9e97fbae25
Graham Christensen <graham@grahamc.com>
2016-12-14 17:13:18 -0500

    perlPackages.DBDmysql: 4.039 -> 4.041 for CVE-2016-1251

--- pkgs/development/perl-modules/DBD-mysql/default.nix
+++ pkgs/development/perl-modules/DBD-mysql/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, buildPerlPackage, DBI, mysql }:
 
 buildPerlPackage rec {
-  name = "DBD-mysql-4.039";
+  name = "DBD-mysql-4.041";
 
   src = fetchurl {
-    url = "mirror://cpan/authors/id/C/CA/CAPTTOFU/${name}.tar.gz";
-    sha256 = "0k4p3bjdbmxm2amb0qiiwmn8v83zrjkz5qp84xdjrg8k5v9aj0hn";
+    url = "mirror://cpan/authors/id/M/MI/MICHIELB/${name}.tar.gz";
+    sha256 = "0h4h6zwzj8fwh9ljb8svnsa0a3ch4p10hp59kpdibdb4qh8xwxs7";
   };
 
   buildInputs = [ mysql.lib ] ;
