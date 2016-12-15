aad5d1f9a7b933442c646f1f1d69b5d9df0ffbcf
Eelco Dolstra <edolstra@gmail.com>
2016-12-15 13:49:15 +0100

    virtualisation.qemu.program: Remove
    This option is defined in qemu-vm.nix, but that module is not always
    imported.
    
    http://hydra.nixos.org/build/44817443
    (cherry picked from commit 03c55005dfd6fbcd5cf8e00128a3bb6336b3bc0f)

--- nixos/modules/virtualisation/qemu-vm.nix
+++ nixos/modules/virtualisation/qemu-vm.nix
@@ -70,7 +70,7 @@ let
       '')}
 
       # Start QEMU.
-      exec ${cfg.qemu.program} \
+      exec ${pkgs.qemu_test}/bin/qemu-kvm \
           -name ${vmName} \
           -m ${toString config.virtualisation.memorySize} \
           ${optionalString (pkgs.stdenv.system == "x86_64-linux") "-cpu kvm64"} \
@@ -299,14 +299,6 @@ in
       };
 
     virtualisation.qemu = {
-      program = mkOption {
-        type = types.path;
-        default = "${pkgs.qemu_kvm}/bin/qemu-kvm";
-        defaultText = "\${pkgs.qemu_kvm}/bin/qemu-kvm";
-        example = literalExample "\${pkgs.qemu_test}/bin/qemu-kvm";
-        description = "The QEMU variant used to start the VM.";
-      };
-
       options =
         mkOption {
           type = types.listOf types.unspecified;
