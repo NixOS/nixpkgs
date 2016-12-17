9d9bd459970e214472b22bf5e589c2a0a0ec7724
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2016-12-17 00:36:53 -0500

    freebayes: init at 1.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12970,6 +12970,8 @@ in
 
   fossil = callPackage ../applications/version-management/fossil { };
 
+  freebayes = callPackage ../applications/science/biology/freebayes { };
+
   freewheeling = callPackage ../applications/audio/freewheeling { };
 
   fribid = callPackage ../applications/networking/browsers/mozilla-plugins/fribid { };
