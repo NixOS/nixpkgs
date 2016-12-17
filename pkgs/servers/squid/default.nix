4b6796868881db7e3962070c109044982ea1a2ba
Franz Pletz <fpletz@fnordicwalking.de>
2016-12-17 16:12:42 +0100

    squid: 3.5.19 -> 3.5.23
    Fixes CVE-2016-10002 & CVE-2016-10003.

--- pkgs/servers/squid/default.nix
+++ pkgs/servers/squid/default.nix
@@ -1,10 +1,10 @@
 { fetchurl, stdenv, perl, lib, openldap, pam, db, cyrus_sasl, libcap,
 expat, libxml2, libtool, openssl}:
 stdenv.mkDerivation rec {
-  name = "squid-3.5.19";
+  name = "squid-3.5.23";
   src = fetchurl {
-    url = "http://www.squid-cache.org/Versions/v3/3.5/${name}.tar.bz2";
-    sha256 = "1iy2r7r12xv0q9414rczbqbbggyyxgdmg21bynpygwkgalaz1dxx";
+    url = "http://www.squid-cache.org/Versions/v3/3.5/${name}.tar.xz";
+    sha256 = "1nqbljph2mbxjy1jzsis5vplfvvc2y6rdlxy609zx4hyyjchqk7s";
   };
   buildInputs = [perl openldap pam db cyrus_sasl libcap expat libxml2
     libtool openssl];
