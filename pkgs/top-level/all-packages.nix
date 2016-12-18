de1a911ee80599cd148dc8af686689d73c628b94
Balletie <skip_meesie@hotmail.com>
2016-12-18 20:48:41 +0100

    pommed-light: init at 1.50lw

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14347,6 +14347,8 @@ in
 
   pommed = callPackage ../os-specific/linux/pommed {};
 
+  pommed_light = callPackage ../os-specific/linux/pommed-light {};
+
   pond = callPackage ../applications/networking/instant-messengers/pond { };
 
   ponymix = callPackage ../applications/audio/ponymix { };
