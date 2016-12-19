cc30f5dd5e7a277b2a920878a0fe04a6f103b9c2
Aristid Breitkreuz <aristidb@gmail.com>
2016-12-19 16:00:46 +0100

    boost: add 1.62.0 (not default yet)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6729,6 +6729,7 @@ in
   boost155 = callPackage ../development/libraries/boost/1.55.nix { };
   boost159 = callPackage ../development/libraries/boost/1.59.nix { };
   boost160 = callPackage ../development/libraries/boost/1.60.nix { };
+  boost162 = callPackage ../development/libraries/boost/1.62.nix { };
   boost = boost160;
 
   boost_process = callPackage ../development/libraries/boost-process { };
