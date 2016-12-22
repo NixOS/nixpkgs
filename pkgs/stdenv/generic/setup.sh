a75c2e0913460fd77b30620b6cb95dfc1ceeafcc
Joachim Fasting <joachifm@fastmail.fm>
2016-12-22 19:30:05 +0100

    stdenv: scan all outputs for references to build directory

--- pkgs/stdenv/generic/setup.sh
+++ pkgs/stdenv/generic/setup.sh
@@ -794,15 +794,15 @@ installCheckPhase() {
         $makeFlags "${makeFlagsArray[@]}" \
         $installCheckFlags "${installCheckFlagsArray[@]}" ${installCheckTarget:-installcheck}
 
-
     if [ -z "$noErrorBuildTop" -a "$NIX_ENFORCE_PURITY" = "1" -a -n "$NIX_BUILD_TOP" ]; then
-        if grep -qr $NIX_BUILD_TOP $out; then
-            echo "ERROR: The following files include the build path: $NIX_BUILD_TOP"
-            echo "ERROR: Either fix the nix expression or set noErrorBuildTop"
-            grep -lr $NIX_BUILD_TOP $out
-            exit 1
-        fi
-    fi
+        for output in $outputs; do
+            if grep -qr $NIX_BUILD_TOP ${!output} ; then
+                echo "ERROR: found reference to build path $NIX_BUILD_TOP in the following files"
+                grep -lr $NIX_BUILD_TOP ${!output}
+                exit 1
+            fi
+        done
+    done
 
     runHook postInstallCheck
 }
