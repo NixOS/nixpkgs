41bdfa6ce87e5300d625a838d066d083e499f556
Jörg Thalheim <joerg@higgsboson.tk>
2016-12-25 09:19:16 +0100

    Revert "os-prober: init at 1.65"

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2940,8 +2940,6 @@ in
 
   olsrd = callPackage ../tools/networking/olsrd { };
 
-  os-prober = callPackage ../tools/misc/os-prober {};
-
   ossec = callPackage ../tools/security/ossec {};
 
   ostree = callPackage ../tools/misc/ostree { };
