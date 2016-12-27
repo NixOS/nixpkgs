81152a51a4ebbfd4026528137b47530290ea84cf
aszlig <aszlig@redmoonstudios.org>
2016-12-27 20:37:48 +0100

    nixpart: Update to latest master version
    This is mainly for testing purposes during the WIP branch, but it should
    actually do partitioning for some of the NixOS VM tests (particularily
    the .btrfs test).
    
    I've tagged this specifically as unstable-1.0.0 to make sure noone is
    seriously going to use it yet (except for playing around of course).
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/tools/filesystems/nixpart/default.nix
+++ pkgs/tools/filesystems/nixpart/default.nix
@@ -1,21 +1,24 @@
-{ stdenv, fetchurl, python3Packages }:
+{ stdenv, fetchFromGitHub, python3Packages, nix }:
 
 python3Packages.buildPythonApplication rec {
   name = "nixpart-${version}";
-  version = "1.0.0";
+  version = "unstable-1.0.0";
 
-  src = fetchurl {
-    url = "https://github.com/aszlig/nixpart/archive/v${version}.tar.gz";
-    sha256 = "0avwd8p47xy9cydlbjxk8pj8q75zyl68gw2w6fnkk78dcb1a3swp";
+  src = fetchFromGitHub {
+    owner = "aszlig";
+    repo = "nixpart";
+    rev = "e22e1b5ccecf491949addffe9c0555ee55e76008";
+    sha256 = "1j1m7d180jmxngc95z5fghxzbhxw5ysvalnxpa94cmj0s4mp5ljx";
   };
 
+  checkInputs = [ nix ];
   propagatedBuildInputs = [ python3Packages.blivet ];
+  makeWrapperArgs = [ "--set GI_TYPELIB_PATH \"$GI_TYPELIB_PATH\"" ];
 
   meta = {
     description = "NixOS storage manager/partitioner";
     license = stdenv.lib.licenses.gpl2Plus;
     maintainers = [ stdenv.lib.maintainers.aszlig ];
     platforms = stdenv.lib.platforms.linux;
-    broken = true;
   };
 }
