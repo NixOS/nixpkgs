e919a460a769fe6f6aee1405775efb423a478aef
aszlig <aszlig@redmoonstudios.org>
2016-12-27 09:19:12 +0100

    blivet: 0.67 -> 2.1.7
    This is basically a bump from the stone age to the current version, so
    the changelog would be a bit long to summarize here, hence here is the
    URL:
    
    https://github.com/rhinstaller/blivet/blob/blivet-2.1.7/python-blivet.spec#L80-L1278
    
    A few of the direct dependencies of blivet are now direct depencies of
    libblockdev, which inself is a replacement for pyblock in C and uses
    gobject-introspection to resolve the C symbols.
    
    We also patch out hawkey and replace it with a wrapper around nix-store,
    because on NixOS we can't use libsolv to resolve dependencies.
    
    Right now, I'm also disabling the EDD tests, because I think they
    shouldn't work within our VM test environments, but I need to dig a bit
    more into that.
    
    In the end we now still have 12 failing test cases which we need to
    resolve:
    
    https://headcounter.org/hydra/log/1rbpjrrjvc95ldcmmrqbangh5ssizygr-vm-test-run-blivet.drv
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/development/python-modules/blivet/default.nix
+++ pkgs/development/python-modules/blivet/default.nix
@@ -1,38 +1,68 @@
-{ stdenv, fetchFromGitHub, buildPythonPackage, pykickstart, pyparted, pyblock
-, pyudev, six, libselinux, cryptsetup, multipath-tools, lsof, utillinux
+{ stdenv, fetchFromGitHub, buildPythonPackage, python, isPy3k
+, libselinux, libblockdev, libbytesize, pyparted, pyudev
+, coreutils, utillinux, multipath-tools, lsof
 }:
 
-let
-  pyenable = { enablePython = true; };
-  selinuxWithPython = libselinux.override pyenable;
-  cryptsetupWithPython = cryptsetup.override pyenable;
-in buildPythonPackage rec {
+buildPythonPackage rec {
   name = "blivet-${version}";
-  version = "0.67";
+  version = "2.1.7";
 
   src = fetchFromGitHub {
-    owner = "dwlehman";
+    owner = "rhinstaller";
     repo = "blivet";
     rev = name;
-    sha256 = "1gk94ghjrxfqnx53hph1j2s7qcv86fjz48is7l099q9c24rjv8ky";
+    sha256 = "1z2s8dxwk5qpra7a27sqxzfahk89x8hgq2ff5qk6ymb7qzdkc1yj";
   };
 
+  outputs = [ "out" "tests" ];
+
+  # Only works with Python 3!
+  disabled = !isPy3k;
+
+  patches = [ ./no-hawkey.patch ];
+
   postPatch = ''
-    sed -i \
-      -e 's|"multipath"|"${multipath-tools}/sbin/multipath"|' \
-      -e '/^def set_friendly_names/a \    return False' \
-      blivet/devicelibs/mpath.py
-    sed -i -e '/"wipefs"/ {
-      s|wipefs|${utillinux}/sbin/wipefs|
-      s/-f/--force/
-    }' blivet/formats/__init__.py
+    cat > blivet/kickstart_stubs.py <<EOF
+    AUTOPART_TYPE_PLAIN = 0
+    AUTOPART_TYPE_BTRFS = 1
+    AUTOPART_TYPE_LVM = 2
+    AUTOPART_TYPE_LVM_THINP = 3
+
+    CLEARPART_TYPE_LINUX = 0
+    CLEARPART_TYPE_ALL = 1
+    CLEARPART_TYPE_NONE = 2
+    CLEARPART_TYPE_LIST = 3
+    EOF
+
+    # Remove test involving pykickstart
+    rm tests/blivet_test.py
+    # Remove EDD tests, because they don't work within VM tests
+    rm tests/devicelibs_test/edd_test.py
+
+    # Replace imports of pykickstart with the stubs
+    sed -i -e '
+      s/^from pykickstart\.constants import/from blivet.kickstart_stubs import/
+    ' blivet/autopart.py blivet/blivet.py tests/clearpart_test.py
+
+    # patch in paths for multipath-tools
+    sed -i -re '
+      s!(run_program[^"]*")(multipath|kpartx)"!\1${multipath-tools}/bin/\2"!
+    ' blivet/devices/disk.py blivet/devices/dm.py
+
+    # make "kpartx" and "multipath" always available
+    sed -i -re '
+      s/^(KPARTX|MULTIPATH)_APP *=.*/\1_APP = available_resource("\L\1\E")/
+    ' blivet/tasks/availability.py
+
     sed -i -e 's|"lsof"|"${lsof}/bin/lsof"|' blivet/formats/fs.py
-    sed -i -r -e 's|"(u?mount)"|"${utillinux}/bin/\1"|' blivet/util.py
+
+    sed -i -e 's|"wipefs"|"${utillinux}/bin/wipefs"|' \
+      blivet/formats/__init__.py tests/formats_test/methods_test.py
   '';
 
   propagatedBuildInputs = [
-    pykickstart pyparted pyblock pyudev selinuxWithPython cryptsetupWithPython
-    six
+    (libselinux.override { enablePython = true; inherit python; })
+    libblockdev libbytesize pyparted pyudev
   ];
 
   postInstall = ''
