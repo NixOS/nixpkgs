9db36f277dc5741abe78e1ef4b0ba1cbab0be9bb
Bjørn Forsman <bjorn.forsman@gmail.com>
2016-12-27 13:03:08 +0100

    libsigrokdecode: 0.4.0 -> 0.4.1
    (cherry picked from commit cbb29a26a210f801ab5f8df8f8831d75ab2c470b)

--- pkgs/development/tools/libsigrokdecode/default.nix
+++ pkgs/development/tools/libsigrokdecode/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, glib, python3, libsigrok, check }:
 
 stdenv.mkDerivation rec {
-  name = "libsigrokdecode-0.4.0";
+  name = "libsigrokdecode-0.4.1";
 
   src = fetchurl {
     url = "http://sigrok.org/download/source/libsigrokdecode/${name}.tar.gz";
-    sha256 = "0drmxjc2xavccjl2i6vcjipijrn7459nv8cpmm788pi4fcdrszpx";
+    sha256 = "15aabl9p4586v2bkj4bm4xi7l3309r9zb31sw233s5vi170p0pq6";
   };
 
   buildInputs = [ pkgconfig glib python3 libsigrok check ];
