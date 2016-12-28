f3d383553e6ca0e531634a8b86ef54e7d9fddcd2
Vladimír Čunát <vcunat@gmail.com>
2016-12-28 20:16:12 +0100

    gdk-pixbuf: security 2.34.0 -> 2.36.2
    /cc #21457.  The rebuild impact is probably only a few thousand.
    The new utility is put into $out/bin/.
    
    (cherry picked from commit 421a7f37fa62c92921e4d0b8853427317f561733)
    Full bump done, as API+ABI only added new symbols in the meantime.
    https://abi-laboratory.pro/tracker/timeline/gdk-pixbuf/

--- pkgs/development/libraries/gdk-pixbuf/default.nix
+++ pkgs/development/libraries/gdk-pixbuf/default.nix
@@ -2,19 +2,18 @@
 , jasper, libintlOrEmpty, gobjectIntrospection, doCheck ? false }:
 
 let
-  ver_maj = "2.34";
-  ver_min = "0";
+  ver_maj = "2.36";
+  ver_min = "2";
 in
 stdenv.mkDerivation rec {
   name = "gdk-pixbuf-${ver_maj}.${ver_min}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gdk-pixbuf/${ver_maj}/${name}.tar.xz";
-    sha256 = "0yc8indbl3hf18z6x6kjg59xp9sngm1d8vmz4c7bs6g27qw5npnm";
+    sha256 = "3a082ad67d68b55970aed0b2034a06618167be98a42d5c70de736756b45d325d";
   };
 
   outputs = [ "out" "dev" "devdoc" ];
-  outputBin = "dev";
 
   setupHook = ./setup-hook.sh;
 
@@ -27,14 +26,21 @@ stdenv.mkDerivation rec {
 
   propagatedBuildInputs = [ glib libtiff libjpeg libpng jasper ];
 
+  configureFlags = "--with-libjasper --with-x11"
+    + stdenv.lib.optionalString (gobjectIntrospection != null) " --enable-introspection=yes"
+    ;
+
   # on darwin, tests don't link
   preBuild = stdenv.lib.optionalString (stdenv.isDarwin && !doCheck) ''
     substituteInPlace Makefile --replace "docs tests" "docs"
   '';
 
-  configureFlags = "--with-libjasper --with-x11"
-    + stdenv.lib.optionalString (gobjectIntrospection != null) " --enable-introspection=yes"
-    ;
+  postInstall =
+    # All except one utility seem to be only useful during building.
+    ''
+      moveToOutput "bin" "$dev"
+      moveToOutput "bin/gdk-pixbuf-thumbnailer" "$out"
+    '';
 
   # The tests take an excessive amount of time (> 1.5 hours) and memory (> 6 GB).
   inherit (doCheck);
