442623e49918f0c6b5b7ee91e519ab38558ca032
Michael Raskin <7c6f434c@mail.ru>
2016-12-28 15:02:24 +0100

    qemu_28: init at 2.8.0; not updating the main Qemu expression yet because there were some claims about NixOS test fragility

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14481,6 +14481,11 @@ in
     inherit (darwin.stubs) rez setfile;
   };
 
+  qemu_28 = callPackage ../applications/virtualization/qemu/2.8.nix {
+    inherit (darwin.apple_sdk.frameworks) CoreServices Cocoa;
+    inherit (darwin.stubs) rez setfile;
+  };
+
   qgis = callPackage ../applications/gis/qgis {};
 
   qgroundcontrol = qt55.callPackage ../applications/science/robotics/qgroundcontrol { };
