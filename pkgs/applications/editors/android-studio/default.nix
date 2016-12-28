d849f00af52c0cebe8d09e026d9871b9a2a175ca
Hoang Xuan Phu <phuhimself@phunehehe.net>
2016-12-28 20:34:50 +0800

    android-studio: reindent wrapProgram parameters

--- pkgs/applications/editors/android-studio/default.nix
+++ pkgs/applications/editors/android-studio/default.nix
@@ -43,52 +43,56 @@ let
     ];
     installPhase = ''
       cp -r . $out
-      wrapProgram $out/bin/studio.sh --set PATH "${stdenv.lib.makeBinPath [
+      wrapProgram $out/bin/studio.sh \
+        --set PATH "${stdenv.lib.makeBinPath [
 
-        # Checked in studio.sh
-        coreutils
-        findutils
-        gnugrep
-        which
+          # Checked in studio.sh
+          coreutils
+          findutils
+          gnugrep
+          which
 
-        # For Android emulator
-        file
-        glxinfo
-        pciutils
-        setxkbmap
+          # For Android emulator
+          file
+          glxinfo
+          pciutils
+          setxkbmap
 
-        # Used during setup wizard
-        gnutar
-        gzip
+          # Used during setup wizard
+          gnutar
+          gzip
 
-        # Runtime stuff
-        git
+          # Runtime stuff
+          git
 
-      ]}" --prefix LD_LIBRARY_PATH : "${stdenv.lib.makeLibraryPath [
+        ]}" \
+        --prefix LD_LIBRARY_PATH : "${stdenv.lib.makeLibraryPath [
 
-        # Crash at startup without these
-        fontconfig
-        freetype
-        libXext
-        libXi
-        libXrender
-        libXtst
+          # Crash at startup without these
+          fontconfig
+          freetype
+          libXext
+          libXi
+          libXrender
+          libXtst
 
-        # Gradle wants libstdc++.so.6
-        stdenv.cc.cc.lib
-        # mksdcard wants 32 bit libstdc++.so.6
-        pkgsi686Linux.stdenv.cc.cc.lib
+          # Gradle wants libstdc++.so.6
+          stdenv.cc.cc.lib
+          # mksdcard wants 32 bit libstdc++.so.6
+          pkgsi686Linux.stdenv.cc.cc.lib
 
-        # aapt wants libz.so.1
-        zlib
-        pkgsi686Linux.zlib
-        # Support multiple monitors
-        libXrandr
+          # aapt wants libz.so.1
+          zlib
+          pkgsi686Linux.zlib
+          # Support multiple monitors
+          libXrandr
 
-        # For Android emulator
-        libpulseaudio
-        libX11
-      ]}" --set QT_XKB_CONFIG_ROOT "${xkeyboard_config}/share/X11/xkb"
+          # For Android emulator
+          libpulseaudio
+          libX11
+
+        ]}" \
+        --set QT_XKB_CONFIG_ROOT "${xkeyboard_config}/share/X11/xkb"
     '';
     src = fetchurl {
       url = "https://dl.google.com/dl/android/studio/ide-zips/${version}/android-studio-ide-${build}-linux.zip";
