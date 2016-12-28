372fa21422c179c7710b0fd8ae72e96dad9fea73
aszlig <aszlig@redmoonstudios.org>
2016-12-28 15:13:13 +0100

    nixos/tests/storage: Use MiB for test sizes
    The default size unit that's printed by the blivet device tree
    representation is MiB, GiB and so on. This makes it more obvious whether
    the correct size was used for partitioning without the need to convert
    between mebibytes and megabytes.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- nixos/tests/storage.nix
+++ nixos/tests/storage.nix
@@ -125,11 +125,11 @@ in pkgs.lib.mapAttrs mkStorageTest {
         disk.vdb.clear = true;
         disk.vdb.initlabel = true;
 
-        partition.boot.size.mb = 100;
+        partition.boot.size.mib = 100;
         partition.boot.targetDevice = "disk.vdb";
-        partition.swap.size.mb = 500;
+        partition.swap.size.mib = 500;
         partition.swap.targetDevice = "disk.vdb";
-        partition.nix.size.mb = 500;
+        partition.nix.size.mib = 500;
         partition.nix.targetDevice = "disk.vdb";
         partition.root.size = "fill";
         partition.root.targetDevice = "disk.vdb";
@@ -179,7 +179,7 @@ in pkgs.lib.mapAttrs mkStorageTest {
         disk.vdb.clear = true;
         disk.vdb.initlabel = true;
 
-        partition.swap1.size.mb = 500;
+        partition.swap1.size.mib = 500;
         partition.swap1.targetDevice = "disk.vdb";
         partition.btrfs1.size = "fill";
         partition.btrfs1.targetDevice = "disk.vdb";
@@ -187,7 +187,7 @@ in pkgs.lib.mapAttrs mkStorageTest {
         disk.vdc.clear = true;
         disk.vdc.initlabel = true;
 
-        partition.swap2.size.mb = 500;
+        partition.swap2.size.mib = 500;
         partition.swap2.targetDevice = "disk.vdc";
         partition.btrfs2.size = "fill";
         partition.btrfs2.targetDevice = "disk.vdc";
@@ -227,9 +227,9 @@ in pkgs.lib.mapAttrs mkStorageTest {
         disk.vdb.clear = true;
         disk.vdb.initlabel = true;
 
-        partition.swap.size.mb = 500;
+        partition.swap.size.mib = 500;
         partition.swap.targetDevice = "disk.vdb";
-        partition.boot.size.mb = 100;
+        partition.boot.size.mib = 100;
         partition.boot.targetDevice = "disk.vdb";
         partition.root.size = "fill";
         partition.root.targetDevice = "disk.vdb";
@@ -269,9 +269,9 @@ in pkgs.lib.mapAttrs mkStorageTest {
         disk.vdb.clear = true;
         disk.vdb.initlabel = true;
 
-        partition.raid01.size.mb = 200;
+        partition.raid01.size.mib = 200;
         partition.raid01.targetDevice = "disk.vdb";
-        partition.swap1.size.mb = 500;
+        partition.swap1.size.mib = 500;
         partition.swap1.targetDevice = "disk.vdb";
         partition.raid11.size = "fill";
         partition.raid11.targetDevice = "disk.vdb";
@@ -279,9 +279,9 @@ in pkgs.lib.mapAttrs mkStorageTest {
         disk.vdc.clear = true;
         disk.vdc.initlabel = true;
 
-        partition.raid02.size.mb = 200;
+        partition.raid02.size.mib = 200;
         partition.raid02.targetDevice = "disk.vdc";
-        partition.swap2.size.mb = 500;
+        partition.swap2.size.mib = 500;
         partition.swap2.targetDevice = "disk.vdc";
         partition.raid12.size = "fill";
         partition.raid12.targetDevice = "disk.vdc";
@@ -350,10 +350,10 @@ in pkgs.lib.mapAttrs mkStorageTest {
 
         volgroup.nixos.devices = [ "luks.volroot" ];
 
-        logvol.boot.size.mb = 200;
+        logvol.boot.size.mib = 200;
         logvol.boot.group = "volgroup.nixos";
 
-        logvol.swap.size.mb = 500;
+        logvol.swap.size.mib = 500;
         logvol.swap.group = "volgroup.nixos";
 
         logvol.root.size = "fill";
