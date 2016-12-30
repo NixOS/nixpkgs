ebefef6f6d4914325e99527cd7217f4bd75d864c
Peter Hoeg <peter@speartail.com>
2016-12-30 15:27:28 +0800

    nim: wrap gcc

--- pkgs/development/compilers/nim/default.nix
+++ pkgs/development/compilers/nim/default.nix
@@ -1,26 +1,32 @@
-{ stdenv, fetchurl, unzip }:
+{ stdenv, lib, fetchurl, makeWrapper, gcc }:
 
 stdenv.mkDerivation rec {
-  name = "nim-0.15.2";
+  name = "nim-${version}";
+  version = "0.15.2";
 
   src = fetchurl {
     url = "http://nim-lang.org/download/${name}.tar.xz";
     sha256 = "12pyzjx7x4hclzrf3zf6r1qjlp60bzsaqrz0rax2rak2c8qz4pch";
   };
 
+  buildInputs  = [ makeWrapper ];
+
   buildPhase   = "sh build.sh";
+
   installPhase =
     ''
       install -Dt "$out/bin" bin/nim
       substituteInPlace install.sh --replace '$1/nim' "$out"
       sh install.sh $out
+      wrapProgram $out/bin/nim \
+        --suffix PATH : ${lib.makeBinPath [ gcc ]}
     '';
 
   meta = with stdenv.lib;
     { description = "Statically typed, imperative programming language";
       homepage = http://nim-lang.org/;
       license = licenses.mit;
-      maintainers = with maintainers; [ ehmry ];
+      maintainers = with maintainers; [ ehmry peterhoeg ];
       platforms = platforms.linux ++ platforms.darwin; # arbitrary
     };
 }
