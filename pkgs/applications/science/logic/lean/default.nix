68b2d43c883e1f402c856696834691a2419ef063
Gabriel Ebner <gebner@gebner.org>
2016-12-31 13:51:10 +0100

    lean: 2016-12-08 -> 2016-12-30

--- pkgs/applications/science/logic/lean/default.nix
+++ pkgs/applications/science/logic/lean/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "lean-${version}";
-  version = "2016-12-08";
+  version = "2016-12-30";
 
   src = fetchFromGitHub {
     owner  = "leanprover";
     repo   = "lean";
-    rev    = "7b63d6566faaf1dc0f2c8e873c61f51dce9ab618";
-    sha256 = "0xxr7dnh7pmdbpxhl3cq9clwamxjk54zcxplsrz6xirk0qy7ga4l";
+    rev    = "fd4fffea27c442b12a45f664a8680ebb47482ca3";
+    sha256 = "1izbjxbr1nvv5kv2b7qklqjx2b1qmwrxhmvk0f2lrl9pxz9h0bmd";
   };
 
   buildInputs = [ gmp mpfr cmake gperftools ];
