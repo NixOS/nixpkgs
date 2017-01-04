838862cb42e979188d01ddbbf3a78cc11112c728
Franz Pletz <fpletz@fnordicwalking.de>
2017-01-04 09:10:26 +0100

    pythonPackages.urwid: 1.3.0 -> 1.3.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -26195,14 +26195,14 @@ in {
 
 
   urwid = buildPythonPackage (rec {
-    name = "urwid-1.3.0";
+    name = "urwid-1.3.1";
 
     # multiple:  NameError: name 'evl' is not defined
     doCheck = false;
 
     src = pkgs.fetchurl {
       url = "mirror://pypi/u/urwid/${name}.tar.gz";
-      sha256 = "29f04fad3bf0a79c5491f7ebec2d50fa086e9d16359896c9204c6a92bc07aba2";
+      sha256 = "18cnd1wdjcas08x5qwa5ayw6jsfcn33w4d9f7q3s29fy6qzc1kng";
     };
 
     meta = {
