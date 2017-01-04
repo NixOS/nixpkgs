a256cf37d826119df6517b6fd699d5827079892c
Michael Raskin <7c6f434c@mail.ru>
2017-01-04 17:45:34 +0100

    irssi_otr: 1.0.1 -> 1.0.2
    (cherry picked from commit 71c1e2ffbf52ac057d93531df8690d27e750ef42)

--- pkgs/applications/networking/irc/irssi/otr/default.nix
+++ pkgs/applications/networking/irc/irssi/otr/default.nix
@@ -1,34 +1,22 @@
 { stdenv, fetchurl, fetchFromGitHub, libotr, automake, autoconf, libtool, glib, pkgconfig, irssi }:
 
-let
-  versionFix = fetchurl {
-    url = https://patch-diff.githubusercontent.com/raw/cryptodotis/irssi-otr/pull/60.patch;
-    sha256 = "18fk9nbzf3fvhvvvkrxv5l004hhimapqb6ra09m83268kbl4q3jy";
-  };
-in
 with stdenv.lib;
 stdenv.mkDerivation rec {
   name = "irssi-otr-${version}";
-  version = "1.0.1";
+  version = "1.0.2";
 
   src = fetchFromGitHub {
     owner = "cryptodotis";
     repo = "irssi-otr";
-    rev = "4ad3b7b6c85be0154ab3694fe9831796db20c4fe";
-    sha256 = "1hm1whx1wzlx4fh4xf2y68rx9x6whi8bsbrhd6hqjhskg5msssrg";
+    rev = "v${version}";
+    sha256 = "0c5wb2lg9q0i1jdhpyb5vpvxaa2xx00gvp3gdk93ix9v68gq1ppp";
   };
-
-  prePatch = ''
-    sed -i 's,/usr/include/irssi,${irssi}/include/irssi,' src/Makefile.am
-    sed -i "s,/usr/lib/irssi,$out/lib/irssi," configure.ac
-    sed -i "s,/usr/share/irssi,$out/share/irssi," help/Makefile.am
-  '';
-
-  patches = [ versionFix ];
 
   preConfigure = "sh ./bootstrap";
 
   buildInputs = [ libotr automake autoconf libtool glib pkgconfig irssi ];
+
+  NIX_CFLAGS_COMPILE="-I ${irssi}/include/irssi -I ${irssi}/include/irssi/src/core -I ${irssi}/include/irssi/src/";
 
   meta = {
     homepage = https://github.com/cryptodotis/irssi-otr;
