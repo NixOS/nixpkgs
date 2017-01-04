NixOS/nixpkgs

commit 762d847d47d5cd43a809ebc86d3b17555f873aa8

pythonPackages.Wand: 0.3.5 -> 0.4.4

pkgs/top-level/python-packages.nix
@@ -26443,16 +26443,20 @@ EOF
 
   websockets = callPackage ../development/python-modules/websockets { };
 
-  wand = buildPythonPackage rec {
-    name = "Wand-0.3.5";
+  Wand = buildPythonPackage rec {
+    pname = "Wand";
+    version = "0.4.4";
+    name = "${pname}-${version}";
 
     src = pkgs.fetchurl {
-      url = "mirror://pypi/W/Wand/${name}.tar.gz";
-      sha256 = "31e2186ce8d1da0d2ea84d1428fc4d441c2e9d0e25156cc746b35b781026bcff";
+      url = "mirror://pypi/${builtins.substring 0 1 pname}/${pname}/${name}.tar.gz";
+      sha256 = "28e0454c9d16d69c5d5034918d96320d8f9f1377b4fdaf4944eec2f938c74704";
     };
 
     buildInputs = with self; [ pkgs.imagemagick pytest psutil memory_profiler pytest_xdist ];
 
+    # No tests
+    doCheck = false;
     meta = {
       description = "Ctypes-based simple MagickWand API binding for Python";
       homepage = http://wand-py.org/;

commit 4f957a9fdb5a2d33287f3e38cd8a3e6c7f49bed5

pythonPackages.sqlalchemy_imageattach: fix evaluation
Broken since 762d847d47d5cd43a809ebc86d3b17555f873aa8 where the package
name case was changed but not the reverse dependencies.

pkgs/top-level/python-packages.nix
@@ -24753,7 +24753,7 @@ in {
     };
 
     buildInputs = with self; [ pytest webob pkgs.imagemagick nose ];
-    propagatedBuildInputs = with self; [ sqlalchemy8 wand ];
+    propagatedBuildInputs = with self; [ sqlalchemy8 Wand ];
 
     checkPhase = ''
       cd tests