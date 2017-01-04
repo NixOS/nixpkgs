08fd548dfd825a6202595925dd738f0142ddb013
Georges Dubus <georges.dubus@compiletoi.net>
2017-01-04 15:30:26 +0100

    nox: 0.0.4 -> 0.0.5

--- pkgs/tools/package-management/nox/default.nix
+++ pkgs/tools/package-management/nox/default.nix
@@ -2,12 +2,12 @@
 
 pythonPackages.buildPythonApplication rec {
   name = "nox-${version}";
-  version = "0.0.4";
+  version = "0.0.5";
   namePrefix = "";
 
   src = fetchurl {
     url = "mirror://pypi/n/nix-nox/nix-nox-${version}.tar.gz";
-    sha256 = "11f6css8rnh7qz55z7i81cnb5h9ys98fqxq3fps3hsh64zlydj52";
+    sha256 = "1kwrkp7njxn2sqmmzy5d33d07gawbw2ab2bmfjz0y1r23z9iadf2";
   };
 
   buildInputs = [ pythonPackages.pbr git ];
