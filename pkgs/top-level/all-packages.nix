6afd5fbbb7abe40033d9f89ae5f912d45b6e3e85
Benjamin Staffin <benley@gmail.com>
2017-01-04 03:28:14 -0500

    insync: init at 1.3.13.36129

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14892,6 +14892,8 @@ in
 
   dropbox-cli = callPackage ../applications/networking/dropbox-cli { };
 
+  insync = callPackage ../applications/networking/insync { };
+
   lightdm = qt5.callPackage ../applications/display-managers/lightdm {
     qt4 = null;
     withQt5 = false;
