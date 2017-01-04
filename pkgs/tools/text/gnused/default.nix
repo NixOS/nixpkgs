be489cf48ca25a2e8349ff2dcce82a2738e17290
Vladimír Čunát <vcunat@gmail.com>
2017-01-05 00:17:27 +0100

    gnused: 4.2.2 -> 4.3
    Real replacement of #21646.

--- pkgs/tools/text/gnused/default.nix
+++ pkgs/tools/text/gnused/default.nix
@@ -1,14 +1,18 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, perl }:
 
-stdenv.mkDerivation {
-  name = "gnused-4.2.2";
+stdenv.mkDerivation rec {
+  name = "gnused-${version}";
+  version = "4.3";
 
   src = fetchurl {
-    url = mirror://gnu/sed/sed-4.2.2.tar.bz2;
-    sha256 = "f048d1838da284c8bc9753e4506b85a1e0cc1ea8999d36f6995bcb9460cddbd7";
+    url = "mirror://gnu/sed/sed-${version}.tar.xz";
+    sha256 = "1anhdgah8h423hlmn9hwzxzr7hjbqjm6hxq3z1p7p7nf8640vhj7";
   };
 
   outputs = [ "out" "info" ];
+
+  nativeBuildInputs = [ perl ];
+  preConfigure = "patchShebangs ./build-aux/help2man";
 
   meta = {
     homepage = http://www.gnu.org/software/sed/;
