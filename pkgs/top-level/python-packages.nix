969651aacaf79c96527931918dc3c6f6f6ba94ff
Frederik Rietdijk <fridh@fridh.nl>
2017-01-07 17:28:20 +0100

    pythonPackages.pyjwt: use cryptography instead of pycrypto

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -20601,7 +20601,7 @@ in {
     };
 
     buildInputs = with self; [ pytestrunner pytestcov pytest coverage ];
-    propagatedBuildInputs = with self; [ pycrypto ecdsa ];
+    propagatedBuildInputs = with self; [ cryptography ecdsa ];
 
     # We don't need this specific version
     postPatch = ''
