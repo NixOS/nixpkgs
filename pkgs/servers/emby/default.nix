a344cdffb559656923bccfd651748637ff36c07f
Tristan Helmich <tristan.helmich@gmail.com>
2017-01-07 17:41:36 +0100

    emby: 3.0.8500 -> 3.1.2
    Switch back to use pre built binaries as I was unable to build current
    version from source

--- pkgs/servers/emby/default.nix
+++ pkgs/servers/emby/default.nix
@@ -1,15 +1,16 @@
-{ stdenv, fetchurl, pkgs, makeWrapper, mono, ffmpeg, ... }:
+{ stdenv, fetchurl, pkgs, unzip, sqlite, makeWrapper, mono, ffmpeg, ... }:
 
 stdenv.mkDerivation rec {
   name = "emby-${version}";
-  version = "3.0.8500";
+  version = "3.1.2";
 
   src = fetchurl {
-    url = "https://github.com/MediaBrowser/Emby/archive/${version}.tar.gz";
-    sha256 = "0vm2yvwyhswsp31g48qdzm17c4p7c25vyiy1029hgy8nd5qy4shc";
+    url = "https://github.com/MediaBrowser/Emby/releases/download/${version}/Emby.Mono.zip";
+    sha256 = "14v8bnidakcxcric8sqvwd5bzwa0lk0nyiwkx1by2x2ipj0r9rk3";
   };
 
   buildInputs = with pkgs; [
+    unzip
     makeWrapper
   ];
   propagatedBuildInputs = with pkgs; [
@@ -17,15 +18,17 @@ stdenv.mkDerivation rec {
     sqlite
   ];
 
+  # Need to set sourceRoot as unpacker will complain about multiple directory output
+  sourceRoot = ".";
+
   buildPhase = ''
-    xbuild /p:Configuration="Release Mono" /p:Platform="Any CPU" /t:build MediaBrowser.Mono.sln
-    substituteInPlace MediaBrowser.Server.Mono/bin/Release\ Mono/System.Data.SQLite.dll.config --replace libsqlite3.so ${pkgs.sqlite.out}/lib/libsqlite3.so
-    substituteInPlace MediaBrowser.Server.Mono/bin/Release\ Mono/MediaBrowser.Server.Mono.exe.config --replace ProgramData-Server "/var/lib/emby/ProgramData-Server"
+    substituteInPlace System.Data.SQLite.dll.config --replace libsqlite3.so ${sqlite.out}/lib/libsqlite3.so
+    substituteInPlace MediaBrowser.Server.Mono.exe.config --replace ProgramData-Server "/var/lib/emby/ProgramData-Server"
   '';
 
   installPhase = ''
     mkdir -p $out/bin
-    cp -r MediaBrowser.Server.Mono/bin/Release\ Mono/* $out/bin/
+    cp -r * $out/bin
 
     makeWrapper "${mono}/bin/mono" $out/bin/MediaBrowser.Server.Mono \
       --add-flags "$out/bin/MediaBrowser.Server.Mono.exe -ffmpeg ${ffmpeg}/bin/ffmpeg -ffprobe ${ffmpeg}/bin/ffprobe"
