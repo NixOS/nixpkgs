fd92dd06233614a0eaa55c4b36b7500e7475f939
Tim Cuthbertson <tim@gfxmonk.net>
2017-01-07 23:47:28 +1100

    add nix-prefetch-source

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17484,6 +17484,8 @@ in
     nix-prefetch-zip
     nix-prefetch-scripts;
 
+  nix-update-source = callPackage ../tools/package-management/nix-update-source {};
+
   nix-template-rpm = callPackage ../build-support/templaterpm { inherit (pythonPackages) python toposort; };
 
   nix-repl = callPackage ../tools/package-management/nix-repl { };
