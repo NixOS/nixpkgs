390f2491004fb350bc2bc61bc6ac6f044e404314
Tristan Helmich <tristan.helmich@gmail.com>
2017-01-08 16:28:49 +0100

    sonarr: 2.0.0.4427 -> 2.0.0.4472

--- pkgs/servers/sonarr/default.nix
+++ pkgs/servers/sonarr/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "sonarr-${version}";
-  version = "2.0.0.4427";
+  version = "2.0.0.4472";
 
   src = fetchurl {
     url = "http://download.sonarr.tv/v2/master/mono/NzbDrone.master.${version}.mono.tar.gz";
-    sha256 = "066jhvz57hxrcgn902z25a21sj87wiqz224fjzpk1lpxrylv4ac2";
+    sha256 = "0sz03z057pyai151lxsdsgxlv6kvrnd1xxw7i1ss7b79l6xgmpw8";
   };
 
   buildInputs = [
