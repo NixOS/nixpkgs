20999c1a6f5025bf67bf23a0d7323eb4ffc3b9e3
Franz Pletz <fpletz@fnordicwalking.de>
2017-01-10 07:34:39 +0100

    linux: 4.9.1 -> 4.9.2
    (cherry picked from commit 6b01b229c2181ef268081a7fb5d66b44162e008c)

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.9.1";
+  version = "4.9.2";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "06jlfdmfgmhj80q1q9pglkjygjc1s1b5fslhf5bzkicsw4r2369j";
+    sha256 = "0f2p12pkzgrh9k5c7g2wwjnv6gzqha8bgd7b0qgbzq3ss7nrmnld";
   };
 
   kernelPatches = args.kernelPatches;
