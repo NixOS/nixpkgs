cc40fadf62e8843dabb330659941d0d4d522f863
Franz Pletz <fpletz@fnordicwalking.de>
2017-01-11 06:18:29 +0100

    gnutls: 3.4.16 -> 3.4.17

--- pkgs/development/libraries/gnutls/3.4.nix
+++ pkgs/development/libraries/gnutls/3.4.nix
@@ -1,10 +1,10 @@
-{ callPackage, fetchurl, autoreconfHook, ... } @ args:
+{ callPackage, fetchurl, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "3.4.16";
+  version = "3.4.17";
 
   src = fetchurl {
     url = "ftp://ftp.gnutls.org/gcrypt/gnutls/v3.4/gnutls-${version}.tar.xz";
-    sha256 = "09rrjaflgp5p0hi5gqljklz1xpalnvj8bawvjj6baw8768dvp6nr";
+    sha256 = "0bhp8cqrmw15yins65cn0zwbcpj1vmymr4wnbm151sfmf2kfhl4v";
   };
 })
