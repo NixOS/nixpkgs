760edeb2b67a220d521f145a8baa15d240ba91b2
Michael Raskin <7c6f434c@mail.ru>
2017-01-11 14:13:46 +0100

    xfractint: init at 20.04p14

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15651,6 +15651,8 @@ in
 
   xfig = callPackage ../applications/graphics/xfig { };
 
+  xfractint = callPackage ../applications/graphics/xfractint {};
+
   xineUI = callPackage ../applications/video/xine-ui { };
 
   xmind = callPackage ../applications/misc/xmind { };
