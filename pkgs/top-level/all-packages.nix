4eb277c5641813dfbfc5b7a943ab42f985061f8b
Robin Gloster <mail@glob.in>
2017-01-12 11:48:23 +0100

    Revert "gnutls: default to 3.5 branch"
    This reverts commit e198441cdc7a3e5f662bcdb0e50cf0c8ea65ce5b.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7455,7 +7455,7 @@ in
 
   gnu-efi = callPackage ../development/libraries/gnu-efi { };
 
-  gnutls = gnutls35;
+  gnutls = gnutls34;
 
   gnutls33 = callPackage ../development/libraries/gnutls/3.3.nix {
     guileBindings = config.gnutls.guile or false;
