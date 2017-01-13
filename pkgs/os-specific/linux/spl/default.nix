087b19c4de253c83c72279422cbfe28fae1ee1b2
Robin Gloster <mail@glob.in>
2017-01-13 14:52:08 +0100

    linuxPackages.spl: add assert for compatible kernels

--- pkgs/os-specific/linux/spl/default.nix
+++ pkgs/os-specific/linux/spl/default.nix
@@ -10,53 +10,57 @@ let
   buildKernel = any (n: n == configFile) [ "kernel" "all" ];
   buildUser = any (n: n == configFile) [ "user" "all" ];
 
-  common = { version, sha256 } @ args : stdenv.mkDerivation rec {
-    name = "spl-${configFile}-${version}${optionalString buildKernel "-${kernel.version}"}";
+  common = { version, sha256, incompatibleKernelVersion } @ args:
+    if buildKernel &&
+       (incompatibleKernelVersion != null) &&
+       versionAtLeast kernel.version incompatibleKernelVersion then
+      throw "linux v${kernel.version} is not yet supported by zfsonlinux v${version}"
+    else stdenv.mkDerivation rec {
+      name = "spl-${configFile}-${version}${optionalString buildKernel "-${kernel.version}"}";
 
-    src = fetchFromGitHub {
-      owner = "zfsonlinux";
-      repo = "spl";
-      rev = "spl-${version}";
-      inherit sha256;
-    };
+      src = fetchFromGitHub {
+        owner = "zfsonlinux";
+        repo = "spl";
+        rev = "spl-${version}";
+        inherit sha256;
+      };
 
-    patches = [ ./const.patch ./install_prefix.patch ];
+      patches = [ ./const.patch ./install_prefix.patch ];
 
-    nativeBuildInputs = [ autoreconfHook ];
+      nativeBuildInputs = [ autoreconfHook ];
 
-    hardeningDisable = [ "pic" ];
+      hardeningDisable = [ "pic" ];
 
-    preConfigure = ''
-      substituteInPlace ./module/spl/spl-generic.c --replace /usr/bin/hostid hostid
-      substituteInPlace ./module/spl/spl-generic.c --replace "PATH=/sbin:/usr/sbin:/bin:/usr/bin" "PATH=${coreutils}:${gawk}:/bin"
-      substituteInPlace ./module/splat/splat-vnode.c --replace "PATH=/sbin:/usr/sbin:/bin:/usr/bin" "PATH=${coreutils}:/bin"
-      substituteInPlace ./module/splat/splat-linux.c --replace "PATH=/sbin:/usr/sbin:/bin:/usr/bin" "PATH=${coreutils}:/bin"
-    '';
-
-    configureFlags = [
-      "--with-config=${configFile}"
-    ] ++ optionals buildKernel [
-      "--with-linux=${kernel.dev}/lib/modules/${kernel.modDirVersion}/source"
-      "--with-linux-obj=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
-    ];
-
-    enableParallelBuilding = true;
-
-    meta = {
-      description = "Kernel module driver for solaris porting layer (needed by in-kernel zfs)";
-
-      longDescription = ''
-          This kernel module is a porting layer for ZFS to work inside the linux
-          kernel.
+      preConfigure = ''
+        substituteInPlace ./module/spl/spl-generic.c --replace /usr/bin/hostid hostid
+        substituteInPlace ./module/spl/spl-generic.c --replace "PATH=/sbin:/usr/sbin:/bin:/usr/bin" "PATH=${coreutils}:${gawk}:/bin"
+        substituteInPlace ./module/splat/splat-vnode.c --replace "PATH=/sbin:/usr/sbin:/bin:/usr/bin" "PATH=${coreutils}:/bin"
+        substituteInPlace ./module/splat/splat-linux.c --replace "PATH=/sbin:/usr/sbin:/bin:/usr/bin" "PATH=${coreutils}:/bin"
       '';
 
-      homepage = http://zfsonlinux.org/;
-      platforms = platforms.linux;
-      license = licenses.gpl2Plus;
-      maintainers = with maintainers; [ jcumming wizeman wkennington fpletz ];
-    };
-  };
+      configureFlags = [
+        "--with-config=${configFile}"
+      ] ++ optionals buildKernel [
+        "--with-linux=${kernel.dev}/lib/modules/${kernel.modDirVersion}/source"
+        "--with-linux-obj=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+      ];
 
+      enableParallelBuilding = true;
+
+      meta = {
+        description = "Kernel module driver for solaris porting layer (needed by in-kernel zfs)";
+
+        longDescription = ''
+            This kernel module is a porting layer for ZFS to work inside the linux
+            kernel.
+        '';
+
+        homepage = http://zfsonlinux.org/;
+        platforms = platforms.linux;
+        license = licenses.gpl2Plus;
+        maintainers = with maintainers; [ jcumming wizeman wkennington fpletz ];
+      };
+    };
 in
   assert any (n: n == configFile) [ "kernel" "user" "all" ];
   assert buildKernel -> kernel != null;
@@ -64,9 +68,11 @@ in
     splStable = common {
       version = "0.6.5.8";
       sha256 = "000yvaccqlkrq15sdz0734fp3lkmx58182cdcfpm4869i0q7rf0s";
+      incompatibleKernelVersion = "4.9";
     };
     splUnstable = common {
       version = "0.7.0-rc2";
       sha256 = "1y7jlyj8jwgrgnd6hiabms5h9430b6wjbnr3pwb16mv40wns1i65";
+      incompatibleKernelVersion = "4.10";
     };
   }
