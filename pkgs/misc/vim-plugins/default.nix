3ba22934e8f5939ec10723566e8bd5ed24c6a884
Jordan Cran <jordan@instantepiphany.com.au>
2017-01-15 23:23:48 +1100

    vimPlugins: add typescript-vim, tsuquyomi

--- pkgs/misc/vim-plugins/default.nix
+++ pkgs/misc/vim-plugins/default.nix
@@ -750,6 +750,17 @@ rec {
 
   };
 
+  typescript-vim = buildVimPluginFrom2Nix { # created by nix#NixDerivation
+    name = "typescript-vim-2016-08-10";
+    src = fetchgit {
+      url = "git://github.com/leafgarland/typescript-vim";
+      rev = "7e25a901af7cd993498cc9ecfc833ca2ac21db7a";
+      sha256 = "0n5lrn741ar6wkvsi86kf7hgdjdwq34sn3ppzcddhvic5hayrkyk";
+    };
+    dependencies = [];
+
+  };
+
   vim-ipython = buildVimPluginFrom2Nix { # created by nix#NixDerivation
     name = "vim-ipython-2015-06-23";
     src = fetchgit {
@@ -1747,6 +1758,17 @@ rec {
       url = "git://github.com/tomtom/tlib_vim";
       rev = "8c74564396e368788a5cb901b0e8017a3166cee9";
       sha256 = "15bh6q8g7p1giz2lybqhdf7a7xisqwpr751gprqx3vmh97drry5z";
+    };
+    dependencies = [];
+
+  };
+
+  tsuquyomi = buildVimPluginFrom2Nix { # created by nix#NixDerivation
+    name = "tsuquyomi-2017-01-02";
+    src = fetchgit {
+      url = "git://github.com/Quramy/tsuquyomi";
+      rev = "473aa2703950816748329acca56c069df7339c96";
+      sha256 = "0h5gbhs4gsvyjsin2wvdlbrr6ykpcmipmpwpf39595j1dlqnab59";
     };
     dependencies = [];
 
