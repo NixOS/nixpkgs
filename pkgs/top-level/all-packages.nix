3e86a0b496eee935cf67048d7f90e277de0a8c79
Lee Machin <leemachin@users.noreply.github.com>
2017-01-17 10:55:23 +0100

    Add rubocop linter for Ruby (#21934)
    Use gemdir and pname as suggested by @zimbatm
    
    Fix silly typo

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3664,6 +3664,8 @@ in
   rtorrent = callPackage ../tools/networking/p2p/rtorrent { };
 
   rubber = callPackage ../tools/typesetting/rubber { };
+  
+  rubocop = callPackage ../development/tools/rubocop { };
 
   runningx = callPackage ../tools/X11/runningx { };
 
