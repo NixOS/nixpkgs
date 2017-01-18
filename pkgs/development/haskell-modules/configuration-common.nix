1489bb4cb991635a7f998b5f93760f3e34b7bd3c
Peter Simons <simons@cryp.to>
2017-01-18 09:41:37 +0100

    haskell-http-api-data: fix override for latest 0.3.x version

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -1076,7 +1076,7 @@ self: super: {
 
   # http-api-data_0.3.x requires QuickCheck > 2.9, but overriding that version
   # is hard because of transitive dependencies, so we just disable tests.
-  http-api-data_0_3_3 = dontCheck super.http-api-data_0_3_3;
+  http-api-data_0_3_4 = dontCheck super.http-api-data_0_3_4;
 
   # Fix build for latest versions of servant and servant-client.
   servant_0_9_1_1 = super.servant_0_9_1_1.overrideScope (self: super: {
