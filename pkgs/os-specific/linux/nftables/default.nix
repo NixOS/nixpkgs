f09c5c9c453e0c303f1d334fd6419688eaafdce8
Franz Pletz <fpletz@fnordicwalking.de>
2017-01-22 02:21:07 +0100

    nftables: 0.6 -> 0.7, enable xtables support

--- pkgs/os-specific/linux/nftables/default.nix
+++ pkgs/os-specific/linux/nftables/default.nix
@@ -1,22 +1,24 @@
 { stdenv, fetchurl, pkgconfig, docbook2x, docbook_xml_dtd_45
-, flex, bison, libmnl, libnftnl, gmp, readline }:
+, flex, bison, libmnl, libnftnl, gmp, readline, iptables }:
 
 stdenv.mkDerivation rec {
-  name = "nftables-0.6";
+  name = "nftables-0.7";
 
   src = fetchurl {
     url = "http://netfilter.org/projects/nftables/files/${name}.tar.bz2";
-    sha256 = "0bbcrn9nz75daic8bq7rspvcw3ck7l82vqcvkyyg4mhwbxjn5pny";
+    sha256 = "0hzdqigdx4i6jbpxbdyq4zy4p4waqn8l6vvz7685ikh1v0wr4qzy";
   };
 
   configureFlags = [
     "CONFIG_MAN=y"
     "DB2MAN=docbook2man"
+    "--with-xtables"
   ];
 
   XML_CATALOG_FILES = "${docbook_xml_dtd_45}/xml/dtd/docbook/catalog.xml";
 
-  buildInputs = [ pkgconfig docbook2x flex bison libmnl libnftnl gmp readline ];
+  nativeBuildInputs = [ pkgconfig docbook2x flex bison ];
+  buildInputs = [ libmnl libnftnl gmp readline iptables ];
 
   meta = with stdenv.lib; {
     description = "The project that aims to replace the existing {ip,ip6,arp,eb}tables framework";
