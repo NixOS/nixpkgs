210f894c12ed975dba5c8e500237440c8c744326
Franz Pletz <fpletz@fnordicwalking.de>
2017-01-22 02:20:00 +0100

    iptables: split out dev output

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11087,7 +11087,9 @@ in
     inherit (perlPackages) SGMLSpm;
   };
 
-  iptables = callPackage ../os-specific/linux/iptables { };
+  iptables = callPackage ../os-specific/linux/iptables {
+    flex = flex_2_5_35;
+  };
 
   ipset = callPackage ../os-specific/linux/ipset { };
 
