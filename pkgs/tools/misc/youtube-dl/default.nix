6c7228b4030d9b0d61939f8b38a80e560a2b29d7
Franz Pletz <fpletz@fnordicwalking.de>
2017-01-23 14:18:58 +0100

    youtubeDL: 2017.01.18 -> 2017.01.22

--- pkgs/tools/misc/youtube-dl/default.nix
+++ pkgs/tools/misc/youtube-dl/default.nix
@@ -15,11 +15,11 @@ with stdenv.lib;
 buildPythonApplication rec {
 
   name = "youtube-dl-${version}";
-  version = "2017.01.18";
+  version = "2017.01.22";
 
   src = fetchurl {
     url = "https://yt-dl.org/downloads/${version}/${name}.tar.gz";
-    sha256 = "7c16f3ce7cf8a673a4c531e4a1fc10801467a61732cb65430e40b3ab8b2f2d2e";
+    sha256 = "11sqiry50kb1f2v6py1pfb19kd5x04lcd78wx7ss2n6vw6ckm6mw";
   };
 
   buildInputs = [ makeWrapper zip ] ++ optional generateManPage pandoc;
