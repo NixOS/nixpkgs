25d86bdd102f5e4eb7d7047451f4128df539ccad
Kai <kai.harries@gmail.com>
2017-01-24 14:45:01 +0100

    vnstat service: init (#19809)

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -332,6 +332,7 @@
   ./services/monitoring/telegraf.nix
   ./services/monitoring/ups.nix
   ./services/monitoring/uptime.nix
+  ./services/monitoring/vnstat.nix
   ./services/monitoring/zabbix-agent.nix
   ./services/monitoring/zabbix-server.nix
   ./services/network-filesystems/cachefilesd.nix
