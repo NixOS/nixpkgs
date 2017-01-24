e20fc5059d518d9b951d700f6ad7d37585cf1f83
Daiderd Jordan <daiderd@gmail.com>
2017-01-24 22:45:08 +0100

    libcdio: fix darwin build

--- pkgs/development/libraries/libcdio/default.nix
+++ pkgs/development/libraries/libcdio/default.nix
@@ -1,19 +1,20 @@
-{ fetchurl, stdenv, libcddb, pkgconfig, ncurses, help2man }:
+{ stdenv, fetchurl, libcddb, pkgconfig, ncurses, help2man, libiconv }:
 
 stdenv.mkDerivation rec {
   name = "libcdio-0.93";
-  
+
   src = fetchurl {
     url = "mirror://gnu/libcdio/${name}.tar.bz2";
     sha256 = "1a6x2c5bvpnkn7lhmxkjgz4axmh93m1clrlv41s1wzkc48lnc9zq";
   };
 
-  buildInputs = [ libcddb pkgconfig ncurses help2man ];
+  buildInputs = [ libcddb pkgconfig ncurses help2man ]
+    ++ stdenv.lib.optional stdenv.isDarwin libiconv;
 
   # Disabled because one test (check_paranoia.sh) fails.
   #doCheck = true;
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "A library for OS-independent CD-ROM and CD image access";
     longDescription = ''
       GNU libcdio is a library for OS-independent CD-ROM and
@@ -21,8 +22,8 @@ stdenv.mkDerivation rec {
       ISO-9660 filesystems (libiso9660), as well as utility
       programs such as an audio CD player and an extractor.
     '';
-    license = stdenv.lib.licenses.gpl2Plus;
     homepage = http://www.gnu.org/software/libcdio/;
-    platforms = stdenv.lib.platforms.linux;
+    license = licenses.gpl2Plus;
+    platforms = platforms.linux ++ platforms.darwin;
   };
 }
