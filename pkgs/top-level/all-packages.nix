5d5fb4a2fb1fc782f603837a53a0a831e81ad637
Vladimír Čunát <vcunat@gmail.com>
2017-01-25 15:20:18 +0100

    knot-resolver: init at 1.2.0
    Celebrating today's release!

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10328,6 +10328,7 @@ with pkgs;
   jetty = callPackage ../servers/http/jetty { };
 
   knot-dns = callPackage ../servers/dns/knot-dns { };
+  knot-resolver = callPackage ../servers/dns/knot-resolver { };
 
   rdkafka = callPackage ../development/libraries/rdkafka { };
 
