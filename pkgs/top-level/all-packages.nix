74e6aa7de608f1ea62a67dc9dfb7f09db6b64633
Christoph-Simon Senjak <you@example.com>
2017-01-25 21:59:27 +0100

    gbsplay: init at version 2016-12-17

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -862,6 +862,8 @@ with pkgs;
 
   fzy = callPackage ../tools/misc/fzy { };
 
+  gbsplay = callPackage ../applications/audio/gbsplay { };
+
   gdrivefs = python27Packages.gdrivefs;
 
   go-dependency-manager = callPackage ../development/tools/gdm { };
