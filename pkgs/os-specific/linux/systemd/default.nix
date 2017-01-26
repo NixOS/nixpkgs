a38f1911d34f2a72e15d5e98d76bece6cb8042a8
Robin Gloster <mail@glob.in>
2017-01-26 01:52:38 +0100

    systemd: 231 -> 232
    Includes adding some more upstream units and removing obsolete (-.slice) ones.

--- pkgs/os-specific/linux/systemd/default.nix
+++ pkgs/os-specific/linux/systemd/default.nix
@@ -4,29 +4,20 @@
 , kexectools, libmicrohttpd, linuxHeaders ? stdenv.cc.libc.linuxHeaders, libseccomp
 , iptables, gnu-efi
 , autoreconfHook, gettext, docbook_xsl, docbook_xml_dtd_42, docbook_xml_dtd_45
-, enableKDbus ? false
 }:
 
 assert stdenv.isLinux;
 
 stdenv.mkDerivation rec {
-  version = "231";
+  version = "232";
   name = "systemd-${version}";
 
   src = fetchFromGitHub {
-    owner = "NixOS";
+    owner = "mayflower";
     repo = "systemd";
-    rev = "3b11791d323cf2d0e00a156967021e1ae9119de2";
-    sha256 = "1xzldwd6407jdg6z36smd49d961nmqykpay969i4xfdldcgyjdv0";
+    rev = "42a48d16c0ec48330286f8332a4628e1d71c759b";
+    sha256 = "18bcxbba1wk44xhlsna18x3c4b07sq6670wnsy2xy1x2kzfnmhdl";
   };
-
-  patches = [
-    # Fixes tty issues, see #18158. Remove when upgrading to systemd 232.
-    (fetchpatch {
-      url = "https://github.com/systemd/systemd/commit/bd64d82c1c0e3fe2a5f9b3dd9132d62834f50b2d.patch";
-      sha256 = "1gc9fxdlnfmjhbi77xfwcb5mkhryjsdi0rmbh2lq2qq737iyqqwm";
-    })
-  ];
 
   outputs = [ "out" "lib" "man" "dev" ];
 
@@ -53,7 +44,6 @@ stdenv.mkDerivation rec {
       "--with-dbussystemservicedir=$(out)/share/dbus-1/system-services"
       "--with-dbussessionservicedir=$(out)/share/dbus-1/services"
       "--with-tty-gid=3" # tty in NixOS has gid 3
-      "--enable-compat-libs" # get rid of this eventually
       "--disable-tests"
 
       "--enable-lz4"
@@ -80,7 +70,7 @@ stdenv.mkDerivation rec {
       "--with-sysvinit-path="
       "--with-sysvrcnd-path="
       "--with-rc-local-script-path-stop=/etc/halt.local"
-    ] ++ (if enableKDbus then [ "--enable-kdbus" ] else [ "--disable-kdbus" ]);
+    ];
 
   hardeningDisable = [ "stackprotector" ];
 
