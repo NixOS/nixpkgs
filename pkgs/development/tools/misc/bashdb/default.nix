26227434e973481e29d4663f9f2d7ffb5ee74983
Niklas Thörne <notrupertthorne@gmail.com>
2017-01-29 21:01:30 +0100

    Updated after code review comments.

--- pkgs/development/tools/misc/bashdb/default.nix
+++ pkgs/development/tools/misc/bashdb/default.nix
@@ -1,10 +1,15 @@
 { stdenv, fetchurl }:
 
+let
+  basename = "bashdb-4.4-0.92";
+
+in
+
 stdenv.mkDerivation {
-  name = "bashdb-4.4-0.92";
+  name = basename;
 
   src = fetchurl {
-    url =  "mirror://sourceforge/sourceforge/bashdb/bashdb-4.4-0.92.tar.bz2";
+    url =  "mirror://sourceforge/bashdb/${basename}.tar.bz2";
     sha256 = "6a8c2655e04339b954731a0cb0d9910e2878e45b2fc08fe469b93e4f2dbaaf92";
   };
 
