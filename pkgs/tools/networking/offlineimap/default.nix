083cadee45320e740e66cb95a9b5ccf174b2a054
Franz Pletz <fpletz@fnordicwalking.de>
2017-01-29 17:20:22 +0100

    offlineimap: 7.0.12 -> 7.0.13

--- pkgs/tools/networking/offlineimap/default.nix
+++ pkgs/tools/networking/offlineimap/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchFromGitHub, pythonPackages, }:
 
 pythonPackages.buildPythonApplication rec {
-  version = "7.0.12";
+  version = "7.0.13";
   name = "offlineimap-${version}";
   namePrefix = "";
 
@@ -9,7 +9,7 @@ pythonPackages.buildPythonApplication rec {
     owner = "OfflineIMAP";
     repo = "offlineimap";
     rev = "v${version}";
-    sha256 = "1i83p40lxjqnvh88x623iydrwnsxib1k91qbl9myc4hi5i4fnr6x";
+    sha256 = "0108xmp9df6cb1nzw3ym59mir3phgfdgp5d43n44ymsk2cc39xcc";
   };
 
   doCheck = false;
