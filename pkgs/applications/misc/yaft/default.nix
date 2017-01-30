9ece819ef0cf054acaac8437de67f18efe4ca622
Alexey Shmalko <rasen.dubi@gmail.com>
2017-01-30 21:59:36 +0200

    yaft: fix build

--- pkgs/applications/misc/yaft/default.nix
+++ pkgs/applications/misc/yaft/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, pkgconfig, ncurses, readline }:
+{ stdenv, fetchFromGitHub, ncurses }:
 
 stdenv.mkDerivation rec {
   version = "0.2.9";
@@ -11,14 +11,13 @@ stdenv.mkDerivation rec {
     sha256 = "0l1ig8wm545kpn4l7186rymny83jkahnjim290wsl7hsszfq1ckd";
   };
 
-  installPhase = ''
-    mkdir -p $out/{bin,share/man}
-    DESTDIR=$out PREFIX=$out/bin/ MANPREFIX=$out/share/man/
-  '';
+  buildInputs = [ ncurses ];
+
+  installFlags = [ "PREFIX=$(out)" "MANPREFIX=$(out)/share/man" ];
 
   meta = {
-    homepage = "https://github.com/uobikiemukot/yaft";
-    description = "yet another framebuffer terminal";
+    homepage = https://github.com/uobikiemukot/yaft;
+    description = "Yet another framebuffer terminal";
     license = stdenv.lib.licenses.mit;
     maintainers = [ stdenv.lib.maintainers.matthiasbeyer ];
     platforms = with stdenv.lib.platforms; linux;
