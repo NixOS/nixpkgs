6a04086683c0e5d7c4e372c0c62dd32fd7f64eea
Vincent Laporte <Vincent.Laporte@gmail.com>
2017-02-01 07:38:28 +0000

    ocamlPackages.menhir: 20161115 -> 20170101

--- pkgs/development/ocaml-modules/menhir/default.nix
+++ pkgs/development/ocaml-modules/menhir/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ocaml, findlib, ocamlbuild
-, version ? if stdenv.lib.versionAtLeast (stdenv.lib.getVersion ocaml) "4.02" then "20161115" else "20140422"
+, version ? if stdenv.lib.versionAtLeast (stdenv.lib.getVersion ocaml) "4.02" then "20170101" else "20140422"
 }@args:
 
 let
   sha256 =
   if version == "20140422" then "1ki1f2id6a14h9xpv2k8yb6px7dyw8cvwh39csyzj4qpzx7wia0d"
-  else if version == "20161115" then "1j8nmcj2gq6hyyi16z27amiahplgrnk4ppchpm0v4qy80kwkf47k"
+  else if version == "20170101" then "0ika46i9gn3sjvspa62fb5dnr20k783vg5fj30649q0ialv6yscr"
   else throw ("menhir: unknown version " ++ version);
 in
 
