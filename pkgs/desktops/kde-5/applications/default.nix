fd0814761321d7097ff7af3f802cf1498b5a5efb
Peter Hoeg <peter@speartail.com>
2017-02-02 09:26:17 +0800

    okteta: init at 16.12.1 (kf5 version)

--- pkgs/desktops/kde-5/applications/default.nix
+++ pkgs/desktops/kde-5/applications/default.nix
@@ -64,6 +64,7 @@ let
     libkipi = callPackage ./libkipi.nix {};
     libkomparediff2 = callPackage ./libkomparediff2.nix {};
     marble = callPackage ./marble.nix {};
+    okteta = callPackage ./okteta.nix {};
     okular = callPackage ./okular.nix {};
     print-manager = callPackage ./print-manager.nix {};
     spectacle = callPackage ./spectacle.nix {};
