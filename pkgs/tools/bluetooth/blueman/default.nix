5125ecb2c189ca542a37d9066019005c188b74ed
Nikolay Amiantov <ab@fmap.me>
2017-02-05 19:32:36 +0300

    blueman: add librsvg

--- pkgs/tools/bluetooth/blueman/default.nix
+++ pkgs/tools/bluetooth/blueman/default.nix
@@ -1,6 +1,7 @@
 { stdenv, lib, fetchurl, intltool, pkgconfig, pythonPackages, bluez, polkit, gtk3
 , obex_data_server, xdg_utils, libnotify, dconf, gsettings_desktop_schemas, dnsmasq, dhcp
-, hicolor_icon_theme , withPulseAudio ? true, libpulseaudio }:
+, hicolor_icon_theme, librsvg
+, withPulseAudio ? true, libpulseaudio }:
 
 let
   binPath = lib.makeBinPath [ xdg_utils dnsmasq dhcp ];
@@ -16,7 +17,7 @@ in stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ intltool pkgconfig pythonPackages.wrapPython pythonPackages.cython ];
 
-  buildInputs = [ bluez gtk3 pythonPackages.python libnotify dconf
+  buildInputs = [ bluez gtk3 pythonPackages.python libnotify dconf librsvg
                   gsettings_desktop_schemas hicolor_icon_theme ]
                 ++ pythonPath
                 ++ lib.optional withPulseAudio libpulseaudio;
