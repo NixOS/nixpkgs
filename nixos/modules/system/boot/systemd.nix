9d30099b7f6b7f5bef6abd6dc240ada3a666112c
Antoine Eiche <lewo@abesis.fr>
2017-02-06 20:47:25 +0100

    nixos/systemd: set r-x group permissions on /var/log/journal
    This allows services such as systemd-journal-gateway to access the
    systemd journal.
    
    Closes #22288

--- nixos/modules/system/boot/systemd.nix
+++ nixos/modules/system/boot/systemd.nix
@@ -753,7 +753,8 @@ in
 
         # Keep a persistent journal. Note that systemd-tmpfiles will
         # set proper ownership/permissions.
-        mkdir -m 0700 -p /var/log/journal
+        # FIXME: revert to 0700 with systemd v233.
+        mkdir -m 0750 -p /var/log/journal
       '';
 
     users.extraUsers.systemd-network.uid = config.ids.uids.systemd-network;
