a848643709884b63441055842acf4c242f49e61a
Dan Peebles <pumpkin@me.com>
2017-02-08 18:54:08 +0000

    pythonPackages.lmdb: fix on python 3 and add meta

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8175,6 +8175,16 @@ in {
       inherit pname version;
       sha256 = "01nw6r08jkipx6v92kw49z34wmwikrpvc5j9xawdiyg1n2526wrx";
     };
+
+    # Some sort of mysterious failure with lmdb.tool
+    doCheck = !isPy3k;
+
+    meta = {
+      description = "Universal Python binding for the LMDB 'Lightning' Database";
+      homepage = "https://github.com/dw/py-lmdb";
+      license = licenses.openldap;
+      maintainers = with maintainers; [ copumpkin ];
+    };
   };
 
   logilab_astng = buildPythonPackage rec {
