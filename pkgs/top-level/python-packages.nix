4fef9bf8570238394e88f55fe4222e5ca13471b2
Dan Peebles <pumpkin@me.com>
2017-02-08 14:05:13 -0500

    pythonPackages.ansicolors: add meta

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -681,6 +681,13 @@ in {
       inherit version;
       sha256 = "02lmh2fbqcwr98cq13l9ql0fvyad1dcb3ap3c5xq9qwjp45m6r3n";
     };
+
+    meta = {
+      homepage = "https://github.com/verigak/colors/";
+      description = "ANSI colors for Python";
+      license = licenses.isc;
+      maintainers = with maintainers; [ copumpkin ];
+    };
   };
 
   asgiref = buildPythonPackage rec {
