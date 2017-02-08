1684c9a6a09f96af57301a14566e8f7c2020e40d
Renzo Carbonara <gnuk0001@gmail.com>
2017-02-09 00:22:11 +0100

    compilers/ghcjs/base.nix: Take ghcjsNodePkgs as argument

--- pkgs/development/compilers/ghcjs/base.nix
+++ pkgs/development/compilers/ghcjs/base.nix
@@ -39,6 +39,11 @@
 , coreutils
 , libiconv
 
+, ghcjsNodePkgs ? callPackage ../../../top-level/node-packages.nix {
+    generated = ./node-packages-generated.nix;
+    self = ghcjsNodePkgs;
+  }
+
 , version ? "0.2.0"
 , ghcjsSrc ? fetchFromGitHub {
     owner = "ghcjs";
@@ -160,12 +165,7 @@ in mkDerivation (rec {
         --with-gmp-includes ${gmp.dev}/include \
         --with-gmp-libraries ${gmp.out}/lib
   '';
-  passthru = let
-    ghcjsNodePkgs = callPackage ../../../top-level/node-packages.nix {
-      generated = ./node-packages-generated.nix;
-      self = ghcjsNodePkgs;
-    };
-  in {
+  passthru = {
     inherit bootPkgs;
     isCross = true;
     isGhcjs = true;
