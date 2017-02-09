ddfc6a64297b3eaf09a962287c6729b7d1715f38
Franz Pletz <fpletz@fnordicwalking.de>
2017-02-09 16:31:49 +0100

    linux: 3.18.47 -> 3.18.48

--- pkgs/os-specific/linux/kernel/linux-3.18.nix
+++ pkgs/os-specific/linux/kernel/linux-3.18.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.18.47";
+  version = "3.18.48";
   extraMeta.branch = "3.18";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "1d9gcr08i6jlm4h6gxmhkq3hjm2ysd1587wffj10ky7y6428dpdi";
+    sha256 = "0754ymbrdiilav7lv4qr5dsxvgq5gr081md4p266l6nskfknldvq";
   };
 
   kernelPatches = args.kernelPatches;
