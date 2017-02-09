f34d711a9416ced783bf8e92859e80554f422d46
Franz Pletz <fpletz@fnordicwalking.de>
2017-02-09 23:09:07 +0100

    network-interfaces service: add metric option for defaultGateways

--- nixos/modules/tasks/network-interfaces.nix
+++ nixos/modules/tasks/network-interfaces.nix
@@ -130,6 +130,13 @@ let
         description = "The default gateway interface.";
       };
 
+      metric = mkOption {
+        type = types.nullOr types.int;
+        default = null;
+        example = 42;
+        description = "The default gateway metric/preference.";
+      };
+
     };
 
   };
