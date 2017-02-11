32fcd1de59d0cadf8e5555881cb8a960fa9d3983
Tomasz Czyż <tomasz.czyz@gmail.com>
2017-02-11 02:23:15 +0000

    pgjwt: init 0.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2130,6 +2130,8 @@ with pkgs;
 
   pgf_graphics = callPackage ../tools/graphics/pgf { };
 
+  pgjwt = callPackage ../servers/sql/postgresql/pgjwt {};
+
   pigz = callPackage ../tools/compression/pigz { };
 
   pixz = callPackage ../tools/compression/pixz { };
