ccf4d5e3d514eb8706b8aa89a3710c07a21b3483
Frederik Rietdijk <fridh@fridh.nl>
2017-02-12 12:34:11 +0100

    cudatoolkit5: remove this ancient version

--- pkgs/development/compilers/cudatoolkit/default.nix
+++ pkgs/development/compilers/cudatoolkit/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, patchelf, perl, ncurses, expat, python26, python27, zlib
+{ lib, stdenv, fetchurl, patchelf, perl, ncurses, expat, python27, zlib
 , xorg, gtk2, glib, fontconfig, freetype, unixODBC, alsaLib, glibc
 }:
 
@@ -84,13 +84,6 @@ let
     };
 
 in {
-
-  cudatoolkit5 = common {
-    version = "5.5.22";
-    url = http://developer.download.nvidia.com/compute/cuda/5_5/rel/installers/cuda_5.5.22_linux_64.run;
-    sha256 = "b997e1dbe95704e0e806e0cedc5fd370a385351fef565c7bae0917baf3a29aa4";
-    python = python26;
-  };
 
   cudatoolkit6 = common {
     version = "6.0.37";
