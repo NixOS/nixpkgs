c07f1589bf91f35e9e72536e660c0d1ba39285e6
Rob Vermaas <rob.vermaas@gmail.com>
2017-02-12 09:10:35 +0000

    Make azure-mgmt-resource work with requests 2.11.x. We should update the azure python packages, however, that is a much bigger effort. This is needed to make sure nixops keeps working with Azure.
    (cherry picked from commit 81f2bde0a2af99d84666628fae89af648ce2fa3e)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1762,6 +1762,11 @@ in {
       url = mirror://pypi/a/azure-mgmt-resource/azure-mgmt-resource-0.20.1.zip;
       sha256 = "0slh9qfm5nfacrdm3lid0sr8kwqzgxvrwf27laf9v38kylkfqvml";
     };
+    preConfigure = ''
+      # Patch to make this package work on requests >= 2.11.x
+      # CAN BE REMOVED ON NEXT PACKAGE UPDATE
+      sed -i 's|len(request_content)|str(len(request_content))|' azure/mgmt/resource/resourcemanagement.py
+    '';
     postInstall = ''
       echo "__import__('pkg_resources').declare_namespace(__name__)" >> "$out/lib/${python.libPrefix}"/site-packages/azure/__init__.py
       echo "__import__('pkg_resources').declare_namespace(__name__)" >> "$out/lib/${python.libPrefix}"/site-packages/azure/mgmt/__init__.py
