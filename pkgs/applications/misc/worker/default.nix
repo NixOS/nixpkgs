6f2ac3ec4f8eacfb472ca9097916125640e5b1c8
ndowens <ndowens04@gmail.com>
2017-02-12 13:40:16 -0600

    worker: init at 3.8.5
    	modified:   pkgs/top-level/all-packages.nix
    
    	modified:   pkgs/top-level/all-packages.nix

---
+++ pkgs/applications/misc/worker/default.nix
@@ -0,0 +1,19 @@
+{pkgs, stdenv, xorg, fetchurl }:
+
+stdenv.mkDerivation rec {
+  name = "worker";
+  version = "3.8.5";
+
+src = fetchurl {
+  url = "http://www.boomerangsworld.de/cms/worker/downloads/${name}-${version}.tar.gz";
+  sha256 = "1xy02jdf60wg2jycinl6682xg4zvphdj80f8xgs26ip45iqgkmvw";
+};
+
+buildInputs = with pkgs; [ xorg.libX11 ];
+
+meta = with stdenv.lib; {
+  description = "a two-pane file manager with advanced file manipulation features";
+  homepage = "http://www.boomerangsworld.de/cms/worker/index.html";
+  licenses = license.gpl2;
+};
+}
