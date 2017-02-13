836ab672a89c300a1a6f7a55a4f77bd67107dcbf
Jörg Thalheim <Mic92@users.noreply.github.com>
2017-02-13 01:02:09 +0100

    worker: fix indentation

--- pkgs/applications/misc/worker/default.nix
+++ pkgs/applications/misc/worker/default.nix
@@ -4,12 +4,12 @@ stdenv.mkDerivation rec {
   name = "worker";
   version = "3.8.5";
 
- src = fetchurl {
-   url = "http://www.boomerangsworld.de/cms/worker/downloads/${name}-${version}.tar.gz";
-   sha256 = "1xy02jdf60wg2jycinl6682xg4zvphdj80f8xgs26ip45iqgkmvw";
-};
+  src = fetchurl {
+    url = "http://www.boomerangsworld.de/cms/worker/downloads/${name}-${version}.tar.gz";
+    sha256 = "1xy02jdf60wg2jycinl6682xg4zvphdj80f8xgs26ip45iqgkmvw";
+  };
 
-buildInputs = with pkgs; [ xorg.libX11 ];
+  buildInputs = with pkgs; [ xorg.libX11 ];
 
   meta = with stdenv.lib; {
     description = "a two-pane file manager with advanced file manipulation features";
