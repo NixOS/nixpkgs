66eba1f3e0f0bb4c8b424e69c19a704e1b102a7f
Daiderd Jordan <daiderd@gmail.com>
2017-02-13 23:35:09 +0100

    epic5: add configure flags

--- pkgs/applications/networking/irc/epic5/default.nix
+++ pkgs/applications/networking/irc/epic5/default.nix
@@ -1,5 +1,4 @@
-{stdenv, fetchurl, pkgs, openssl
-  , ncurses, libiconv, tcl }:
+{ stdenv, fetchurl, openssl, ncurses, libiconv, tcl }:
 
 stdenv.mkDerivation rec {
   name = "epic5-${version}";
@@ -8,20 +7,21 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "http://ftp.epicsol.org/pub/epic/EPIC5-PRODUCTION/${name}.tar.xz";
     sha256 = "1ap73d5f4vccxjaaq249zh981z85106vvqmxfm4plvy76b40y9jm";
-    };
+  };
 
-   # Darwin needs libiconv, tcl; while Linux build don't
- 
-   buildInputs = [ openssl ncurses ]  
-   ++ stdenv.lib.optionals 
-   stdenv.isDarwin [ libiconv tcl ];
-  
+  # Darwin needs libiconv, tcl; while Linux build don't
+  buildInputs = [ openssl ncurses ]
+    ++ stdenv.lib.optionals stdenv.isDarwin [ libiconv tcl ];
+
+  configureFlags = [ "--disable-debug" "--with-ipv6" ];
+
   postConfigure = ''
-      substituteInPlace bsdinstall \
+    substituteInPlace bsdinstall \
       --replace /bin/cp cp \
       --replace /bin/rm rm \
-      --replace /bin/chmod chmod \
-      '';
+      --replace /bin/chmod chmod
+  '';
+
   meta = with stdenv.lib; {
     homepage = "http://epicsol.org/";
     description = "a IRC client that offers a great ircII interface";
