1b01e8113fc97a69f1baa8189cd5f9103a1f582a
ndowens <ndowens04@gmail.com>
2017-02-12 19:18:18 -0600

    epic5: init at 2.0.1
    worker: fix indentation
    
    Added openssl/ncurses to function; included version in name
    
    Added libiconv for darwin support
    
    Added libiconv for darwin support
    
    Add tcl for darwin support

---
+++ pkgs/applications/networking/irc/epic5/default.nix
@@ -0,0 +1,29 @@
+{stdenv, fetchurl, pkgs, openssl
+  , ncurses, libiconv, tcl }:
+
+stdenv.mkDerivation rec {
+  name = "epic5-${version}";
+  version = "2.0.1";
+
+  src = fetchurl {
+    url = "http://ftp.epicsol.org/pub/epic/EPIC5-PRODUCTION/${name}.tar.xz";
+    sha256 = "1ap73d5f4vccxjaaq249zh981z85106vvqmxfm4plvy76b40y9jm";
+    };
+
+  buildInputs =  [ openssl ncurses libiconv tcl ];
+  postConfigure = ''
+      substituteInPlace bsdinstall \
+      --replace /bin/cp cp \
+      --replace /bin/rm rm \
+      --replace /bin/chmod chmod \
+      '';
+  meta = with stdenv.lib; {
+    homepage = "http://epicsol.org/";
+    description = "a IRC client that offers a great ircII interface";
+    license = licenses.bsd3;
+    maintainers = [ maintainers.ndowens ];
+  };
+}
+
+
+
