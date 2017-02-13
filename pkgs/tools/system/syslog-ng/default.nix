020fb2cce1684e84da2b584374a85e4d5f36fae1
Bas van Dijk <v.dijk.bas@gmail.com>
2017-02-13 17:46:05 +0100

    syslog-ng: 3.6.2 -> 3.9.1
    This should fix: https://github.com/NixOS/nixpkgs/issues/20153

--- pkgs/tools/system/syslog-ng/default.nix
+++ pkgs/tools/system/syslog-ng/default.nix
@@ -1,17 +1,40 @@
-{ stdenv, fetchurl, eventlog, pkgconfig, glib, python, systemd, perl
-, riemann_c_client, protobufc, pcre, yacc }:
+{ stdenv, fetchgit, autoconf, autoconf-archive, automake, libtool, flex, openssl
+, eventlog, pkgconfig, glib, python, systemd, perl
+, riemann_c_client, protobufc, pcre, yacc, which }:
 
 stdenv.mkDerivation rec {
   name = "syslog-ng-${version}";
+  version = "3.9.1";
 
-  version = "3.6.2";
-
-  src = fetchurl {
-    url = "http://www.balabit.com/downloads/files?path=/syslog-ng/sources/${version}/source/syslog-ng_${version}.tar.gz";
-    sha256 = "0qc21mwajk6xrra3gqy2nvaza5gq62psamq4ayphj7lqabdglizg";
+  src = fetchgit {
+    url = "https://github.com/balabit/syslog-ng.git";
+    rev = "59aa4e5d9396d293aae021746214b97d7fe0a8ee"; # tag: syslog-ng-3.9.1
+    sha256 = "15lalqf6dmpm4nr1pp0f2p0a6wbckkrh1k83vhp9ws0by5m8m66r";
   };
 
-  buildInputs = [ eventlog pkgconfig glib python systemd perl riemann_c_client protobufc yacc pcre ];
+  buildInputs = [
+    autoconf
+    autoconf-archive
+    automake
+    libtool
+    which
+    flex
+    openssl
+    eventlog
+    pkgconfig
+    glib
+    python
+    systemd
+    perl
+    riemann_c_client
+    protobufc
+    yacc
+    pcre
+  ];
+
+  preConfigure = ''
+    ./autogen.sh
+  '';
 
   configureFlags = [
     "--enable-dynamic-linking"
