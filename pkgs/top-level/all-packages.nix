538aed6dea201f3f4cee8cbfb4c2b5d075051962
Robin Gloster <mail@glob.in>
2017-02-14 08:59:55 +0100

    twinkle: remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15343,8 +15343,6 @@ with pkgs;
 
   twmn = qt5.callPackage ../applications/misc/twmn { };
 
-  twinkle = callPackage ../applications/networking/instant-messengers/twinkle { };
-
   umurmur = callPackage ../applications/networking/umurmur { };
 
   unigine-valley = callPackage ../applications/graphics/unigine-valley { };
