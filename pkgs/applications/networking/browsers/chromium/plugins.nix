1a77e4141d83b1b0025c226303b77428f6eb74af
Kamil Chmielewski <kamil.chm@gmail.com>
2017-02-15 08:50:15 +0100

    chromium: flashplayer: 24.0.0.194 -> 24.0.0.221
    [Critical security fix] - see #22792

--- pkgs/applications/networking/browsers/chromium/plugins.nix
+++ pkgs/applications/networking/browsers/chromium/plugins.nix
@@ -94,12 +94,12 @@ let
 
   flash = stdenv.mkDerivation rec {
     name = "flashplayer-ppapi-${version}";
-    version = "24.0.0.194";
+    version = "24.0.0.221";
 
     src = fetchzip {
       url = "https://fpdownload.adobe.com/pub/flashplayer/pdc/"
           + "${version}/flash_player_ppapi_linux.x86_64.tar.gz";
-      sha256 = "1l9gz81mwb4p1yj9n8s7hrkxdyw0amcpcc3295dq7zhsr35dm76z";
+      sha256 = "0vqvb098wms9v2r1xm9yq4cpn1h9dr1y7izfy2rwg3y7gr8ycv80";
       stripRoot = false;
     };
 
