db986946ca6c6754aff7ba90299ff18357432abc
Herwig Hochleitner <herwig@bendlas.net>
2017-02-15 04:00:28 +0100

    ja2-stracciatella: init 0.15.1
    This is the open-source version of the venerable Jagged Alliance 2.
    
    Stracciatella means almost vanilla. This version is only changed for
    fixes, stability improvements and to host mods.
    
    The original data files are non-free and have to be provided by the user.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16163,6 +16163,8 @@ with pkgs;
     lua = lua5;
   };
 
+  ja2-stracciatella = callPackage ../games/ja2-stracciatella { };
+
   klavaro = callPackage ../games/klavaro {};
 
   kobodeluxe = callPackage ../games/kobodeluxe { };
