ea309fcf4893d50f8dc1b2db05bac6fadc261444
Franz Pletz <fpletz@fnordicwalking.de>
2017-02-15 01:55:51 +0100

    syslogng_incubator: 141106 -> 0.5.0, mark as broken
    This is the current release but neither this nor the current master
    is compatible with syslog-ng 3.8+. Committed work on update and
    refactor of expression to make it easier in the future to bump.

--- pkgs/tools/system/syslog-ng-incubator/default.nix
+++ pkgs/tools/system/syslog-ng-incubator/default.nix
@@ -1,25 +1,25 @@
 { stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, glib, syslogng
-, eventlog, perl, python, yacc, riemann_c_client, libivykis, protobufc
+, eventlog, perl, python, yacc, protobufc, libivykis
 }:
 
 stdenv.mkDerivation rec {
   name = "syslog-ng-incubator-${version}";
-  version = "141106-54179c5";
+  version = "0.5.0";
 
   src = fetchFromGitHub {
     owner = "balabit";
     repo = "syslog-ng-incubator";
-    rev = "54179c5f733487fe97ee856bc27130d0b09f3d5a";
-    sha256 = "1y099f7pdan1441ycycd67igcwbla2m2cgnxjfvdw76llvi35sam";
+    rev = name;
+    sha256 = "00j123ya0xfj1jicaqnk1liffx07mhhf0r406pabxjjj97gy8nlk";
   };
 
+  nativeBuildInputs = [ pkgconfig autoreconfHook yacc ];
+
   buildInputs = [
-    autoreconfHook pkgconfig glib syslogng eventlog perl python
-    yacc riemann_c_client libivykis protobufc
+    glib syslogng eventlog perl python protobufc libivykis
   ];
 
   configureFlags = [
-    "--without-ivykis"
     "--with-module-dir=$(out)/lib/syslog-ng"
   ];
 
@@ -29,5 +29,6 @@ stdenv.mkDerivation rec {
     license = licenses.gpl2;
     maintainers = [ maintainers.rickynils ];
     platforms = platforms.linux;
+    broken = true; # does not work with our new syslog-ng version yet
   };
 }
