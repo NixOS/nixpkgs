1c366b4e0647abea0385395dcb2981a5b3161cf8
Rob Vermaas <rob.vermaas@gmail.com>
2017-02-16 17:01:56 +0000

    nixops: update to 1.5
    (cherry picked from commit 780a38c611162c34229c9d06b09dd4ec2f8466f4)

--- pkgs/tools/package-management/nixops/unstable.nix
+++ pkgs/tools/package-management/nixops/unstable.nix
@@ -1,10 +1,9 @@
 { callPackage, fetchurl }:
 
 callPackage ./generic.nix (rec {
-  version = "2016-11-23";
+  version = "1.5";
   src = fetchurl {
-    # Hydra doesn't serve production outputs anymore :(
-    url = "https://static.domenkozar.com/nixops-1.5pre0_abcdef.tar.bz2";
-    sha256 = "1a4cyd3zvkdjg9rf9ssr7p4i6r89zr483v5nlr5jzjdjjyi3j2bz";
+    url = "http://nixos.org/releases/nixops/nixops-${version}/nixops-${version}.tar.bz2";
+    sha256 = "0z4pzc55wjab8v4bkrff94f8qp1g9ydgxxpl2dvy5130bg1s52wd";
   };
 })
