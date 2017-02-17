7953cc4863aca8463f4521c62f5dd5609bee626b
Robin Gloster <mail@glob.in>
2017-02-17 13:15:59 +0100

    programs.wireshark: use setcap wrapper

--- nixos/modules/programs/wireshark.nix
+++ nixos/modules/programs/wireshark.nix
@@ -3,27 +3,19 @@
 with lib;
 
 let
-
   cfg = config.programs.wireshark;
   wireshark = cfg.package;
-
-in
-
-{
-
+in {
   options = {
-
     programs.wireshark = {
-
       enable = mkOption {
         type = types.bool;
         default = false;
         description = ''
           Whether to add Wireshark to the global environment and configure a
-          setuid wrapper for 'dumpcap' for users in the 'wireshark' group.
+          setcap wrapper for 'dumpcap'.
         '';
       };
-
       package = mkOption {
         type = types.package;
         default = pkgs.wireshark-cli;
@@ -32,26 +24,15 @@ in
           Which Wireshark package to install in the global environment.
         '';
       };
-
     };
-
   };
 
   config = mkIf cfg.enable {
-
     environment.systemPackages = [ wireshark ];
-    
+
     security.wrappers.dumpcap = {
       source = "${wireshark}/bin/dumpcap";
-      owner = "root";
-      group = "wireshark";
-      setuid = true;
-      setgid = false;
-      permissions = "u+rx,g+x";
+      capabilities = "cap_net_raw+p";
     };
-
-    users.extraGroups.wireshark.gid = config.ids.gids.wireshark;
-
   };
-
 }
