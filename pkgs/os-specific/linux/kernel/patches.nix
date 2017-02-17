ce3ffe7aee1a6e68c7104bebe5095b1794e4b21c
Joachim Fasting <joachifm@fastmail.fm>
2017-02-17 19:09:50 +0100

    linux_4_9: patch for CVE-2017-5986
    Seems fairly low impact[1] but we might as well patch it until a new 4.9
    version is released
    
    [1]: https://bugzilla.redhat.com/show_bug.cgi?id=1420276
    
    (cherry picked from commit e8007c0e8911fbcd99268b0b30d3e9f069f22c20)

--- pkgs/os-specific/linux/kernel/patches.nix
+++ pkgs/os-specific/linux/kernel/patches.nix
@@ -165,4 +165,22 @@ rec {
         sha256 = "19viqjjgq8j8jiz5yhgmzwhqvhwv175q645qdazd1k69d25nv2ki";
       };
     };
+
+  panic_on_icmp6_frag_CVE_2016_9919 = rec
+    { name = "panic_on_icmp6_frag_CVE_2016_9919.patch";
+      patch = fetchpatch {
+        inherit name;
+        url = "https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/patch/?id=79dc7e3f1cd323be4c81aa1a94faa1b3ed987fb2";
+        sha256 = "0mps33r4mnwiy0bmgrzgqkrk59yya17v6kzpv9024g4xlz61rk8p";
+      };
+    };
+
+  sctp_bug_on_CVE_2017_5986 = rec
+    { name = "sctp_BUG_ON_CVE_2017_5986.patch";
+      patch = fetchpatch {
+        inherit name;
+        url = "https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/patch/?id=2dcab598484185dea7ec22219c76dcdd59e3cb90";
+        sha256 = "15np10bfm5yzby9zbkrh23qpm91wnprblsk0xn9yjryypnz8njxh";
+      };
+    };
 }
