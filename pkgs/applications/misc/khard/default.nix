92f66c252033c1a4afd4e78bc00ef8d000819cf4
Matthias Beyer <mail@beyermatthias.de>
2017-02-18 12:12:22 +0100

    khard: 0.11.3 -> 0.11.4

--- pkgs/applications/misc/khard/default.nix
+++ pkgs/applications/misc/khard/default.nix
@@ -1,39 +1,13 @@
 { stdenv, fetchurl, fetchFromGitHub, glibcLocales, python3Packages }:
 
-let
-  vobject = python3Packages.buildPythonPackage rec {
-    version = "0.9.2";
-    name = "vobject-${version}";
-
-    src = fetchFromGitHub {
-      owner = "eventable";
-      repo = "vobject";
-      sha256 = "0zj0wplj8pry98x3g551wdhh12ric7rl6rsd6li23lzdxik82s3g";
-      rev = version;
-    };
-
-    disabled = python3Packages.isPyPy;
-
-    propagatedBuildInputs = with python3Packages; [ dateutil ];
-
-    checkPhase = "${python3Packages.python.interpreter} tests.py";
-
-    meta = with stdenv.lib; {
-      description = "Module for reading vCard and vCalendar files";
-      homepage = http://eventable.github.io/vobject/;
-      license = licenses.asl20;
-      maintainers = [ maintainers.matthiasbeyer ];
-    };
-  };
-in
 python3Packages.buildPythonApplication rec {
-  version = "0.11.3";
+  version = "0.11.4";
   name = "khard-${version}";
   namePrefix = "";
 
   src = fetchurl {
     url = "https://github.com/scheibler/khard/archive/v${version}.tar.gz";
-    sha256 = "0brnwg7f1qnz83q5d6bl2260wykgjhhrpcxxhr2r9gj66q5hdd69";
+    sha256 = "1hngg3z5cdjny7wdf2mf9wv35ffx7ivpq6mx7kgqf40fr5905l0r";
   };
 
   # setup.py reads the UTF-8 encoded readme.
