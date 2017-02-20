38b00a84dd7190ce6f56e4bdc2a4371112fcd188
ndowens <ndowens04@gmail.com>
2017-02-20 17:40:20 -0600

    Swaywm: module

---
+++ nixos/modules/services/x11/window-managers/sway.nix
@@ -0,0 +1,39 @@
+{ config, lib, pkgs, ...}:
+
+with lib;
+
+let
+  cfg = config.services.xserver.windowManager.sway;
+in
+
+{
+  ### Interface
+  options = {
+  services.xserver.windowManager.sway = {
+  enable = mkEnableOption "Sway window manager";
+
+  configFile = mkOption {
+    default = null;
+    type = with types; nullOr path;
+    description = ''
+                  Location of the Sway configuration file.
+		  '';
+      };
+  };
+};
+### Implementation
+
+config = mkIf cfg.enable {
+  services.xserver.windowManager.session = [{
+    name = "sway";
+    start = ''
+            ${pkgs.sway}/bin/sway ${optionalString (cfg.configFile != null)
+	      "-c \"${cfg.configFile}\""
+	      } &
+	      waitPID=$!
+	     '';
+	     }];
+	     environment.systemPackages = [ pkgs.sway ];
+	};
+
+	}
