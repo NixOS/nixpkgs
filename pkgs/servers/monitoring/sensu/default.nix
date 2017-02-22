eae1866c28424be02cb1244a36010d776517f727
Peter Hoeg <peter@speartail.com>
2017-02-22 20:18:05 +0800

    sensu: 0.17.1 -> 0.28.0

--- pkgs/servers/monitoring/sensu/default.nix
+++ pkgs/servers/monitoring/sensu/default.nix
@@ -1,16 +1,17 @@
-  { lib, bundlerEnv, ruby }:
+{ lib, bundlerEnv, ruby }:
 
-  bundlerEnv {
-    name = "sensu-0.17.1";
+bundlerEnv rec {
+  name = "sensu-${version}";
+  version = (import ./gemset.nix).sensu.version;
 
-    inherit ruby;
-    gemdir = ./.;
+  inherit ruby;
+  gemdir = ./.;
 
-    meta = with lib; {
-      description = "A monitoring framework that aims to be simple, malleable, and scalable";
-      homepage    = http://sensuapp.org/;
-      license     = licenses.mit;
-      maintainers = with maintainers; [ theuni ];
-      platforms   = platforms.unix;
-    };
-  }
+  meta = with lib; {
+    description = "A monitoring framework that aims to be simple, malleable, and scalable";
+    homepage    = http://sensuapp.org/;
+    license     = licenses.mit;
+    maintainers = with maintainers; [ theuni peterhoeg ];
+    platforms   = platforms.unix;
+  };
+}
