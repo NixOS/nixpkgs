31dab6ebcc3d523b9576f3c8041c4f8a0949bc11
Gauthier POGAM--LE MONTAGNER <kendos.kenlen@gmail.com>
2017-02-22 16:26:17 +0100

    idea.rubymine; remove old versions

--- pkgs/applications/editors/idea/default.nix
+++ pkgs/applications/editors/idea/default.nix
@@ -230,30 +230,6 @@ in
     wmClass = "jetbrains-rubymine";
   };
 
-  ruby-mine7 = buildRubyMine rec {
-    name = "ruby-mine-${version}";
-    version = "7.1.5";
-    description = "The Most Intelligent Ruby and Rails IDE";
-    license = stdenv.lib.licenses.unfree;
-    src = fetchurl {
-      url = "https://download.jetbrains.com/ruby/RubyMine-${version}.tar.gz";
-      sha256 = "04fcxj1xlap9mxmwf051s926p2darlj5kwl4lms2gy5d8b2lhd5l";
-    };
-    wmClass = "jetbrains-rubymine";
-  };
-
-  ruby-mine8 = buildRubyMine rec {
-    name = "ruby-mine-${version}";
-    version = "8.0.4";
-    description = "The Most Intelligent Ruby and Rails IDE";
-    license = stdenv.lib.licenses.unfree;
-    src = fetchurl {
-      url = "https://download.jetbrains.com/ruby/RubyMine-${version}.tar.gz";
-      sha256 = "0hipxib7377232w1jbf8h98bmh0djkllsrq3lq0w3fdxqglma43a";
-    };
-    wmClass = "jetbrains-rubymine";
-  };
-
   pycharm-community = buildPycharm rec {
     name = "pycharm-community-${version}";
     version = "2016.3.2";
