12b3b8ba61236836773f6f7c733423877dada44e
Jörg Thalheim <Mic92@users.noreply.github.com>
2017-02-22 01:34:09 +0100

    fix buildPythonApplication; right libxml2 output

--- pkgs/applications/science/chemistry/pymol/default.nix
+++ pkgs/applications/science/chemistry/pymol/default.nix
@@ -3,14 +3,14 @@
 let
   version = "1.8.4.0";
 in
-python27Packages.buildPythonPackage {
+python27Packages.buildPythonApplication {
   name = "pymol-${version}";
   src = fetchurl {
     url = "mirror://sourceforge/project/pymol/pymol/1.8/pymol-v1.8.4.0.tar.bz2";
     sha256 = "0yfj8g5yic9zz6f0bw2n8h6ifvgsn8qvhq84alixsi28wzppn55n";
   };
 
-  buildInputs = [ python27Packages.numpy glew freeglut libpng libxml2.dev tk freetype ];
+  buildInputs = [ python27Packages.numpy glew freeglut libpng libxml2 tk freetype ];
   NIX_CFLAGS_COMPILE = "-I ${libxml2.dev}/include/libxml2";
 
   installPhase = ''
