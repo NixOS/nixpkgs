43818b887737bba451f7703ddd206813c0d3cae8
Jörg Thalheim <joerg@higgsboson.tk>
2017-02-23 01:04:26 +0100

    mp4v2: 1.9.1p4 -> 2.0.0

--- pkgs/development/libraries/mp4v2/default.nix
+++ pkgs/development/libraries/mp4v2/default.nix
@@ -1,18 +1,17 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "mp4v2-1.9.1p4";
+  name = "mp4v2-2.0.0";
 
   src = fetchurl {
-    url = "http://mp4v2.googlecode.com/files/${name}.tar.bz2";
-    sha256 = "1d73qbi0faqad3bpmjfr4kk0mfmqpl1f43ysrx4gq9i3mfp1qf2w";
+    url = "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/mp4v2/${name}.tar.bz2";
+    sha256 = "0f438bimimsvxjbdp4vsr8hjw2nwggmhaxgcw07g2z361fkbj683";
   };
 
   # From Handbrake
   # mp4v2 doesn't seem to be actively maintained any more :-/
   patches = [
-    ./A00-nero-vobsub.patch ./A01-divide-zero.patch ./A02-meaningful-4gb-warning.patch
-    ./P00-mingw-dllimport.patch
+    ./A02-meaningful-4gb-warning.patch
   ];
   # `faac' expects `mp4.h'.
   postInstall = "ln -s mp4v2/mp4v2.h $out/include/mp4.h";
