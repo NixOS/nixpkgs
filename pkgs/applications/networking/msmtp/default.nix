1c466059d187b13665d258dbb4daafbcb4d557ec
Peter Hoeg <peter@speartail.com>
2017-02-23 15:57:53 +0800

    msmtp: clean up msmtpq
     - replace relative bin paths with absolute filenames in the nix store
     - make the log and queue paths configurable
     - log to the journal on linux

--- pkgs/applications/networking/msmtp/default.nix
+++ pkgs/applications/networking/msmtp/default.nix
@@ -1,30 +1,49 @@
-{ stdenv, fetchurl, openssl, pkgconfig, gnutls, gsasl, libidn, Security }:
+{ stdenv, lib, fetchurl, autoreconfHook, pkgconfig
+, openssl, netcat, gnutls, gsasl, libidn, Security, systemd }:
 
-stdenv.mkDerivation rec {
-  version = "1.6.6";
+let
+  tester = "n"; # {x| |p|P|n|s}
+  journal = if stdenv.isLinux then "y" else "n";
+
+in stdenv.mkDerivation rec {
   name = "msmtp-${version}";
+  version = "1.6.6";
 
   src = fetchurl {
     url = "mirror://sourceforge/msmtp/${name}.tar.xz";
     sha256 = "0ppvww0sb09bnsrpqnvlrn8vx231r24xn2iiwpy020mxc8gxn5fs";
   };
 
-  buildInputs = [ openssl pkgconfig gnutls gsasl libidn ]
+  patches = [
+    ./paths.patch
+  ];
+
+  buildInputs = [ openssl gnutls gsasl libidn ]
     ++ stdenv.lib.optional stdenv.isDarwin Security;
+  nativeBuildInputs = [ autoreconfHook pkgconfig ];
 
   configureFlags =
     stdenv.lib.optional stdenv.isDarwin [ "--with-macosx-keyring" ];
 
   postInstall = ''
-    cp scripts/msmtpq/msmtp-queue scripts/msmtpq/msmtpq $prefix/bin/
-    chmod +x $prefix/bin/msmtp-queue $prefix/bin/msmtpq
+    substitute scripts/msmtpq/msmtpq $out/bin/msmtpq \
+      --replace @msmtp@      $out/bin/msmtp \
+      --replace @nc@         ${netcat}/bin/nc \
+      --replace @journal@    ${journal} \
+      ${lib.optionalString (journal == "y") "--replace @systemdcat@ ${systemd}/bin/systemd-cat" } \
+      --replace @test@       ${tester}
+
+    substitute scripts/msmtpq/msmtp-queue $out/bin/msmtp-queue \
+      --replace @msmtpq@ $out/bin/msmtpq
+
+    chmod +x $out/bin/*
   '';
 
-  meta = {
-      description = "Simple and easy to use SMTP client with excellent sendmail compatibility";
-      homepage = "http://msmtp.sourceforge.net/";
-      license = stdenv.lib.licenses.gpl3;
-      maintainers = [ stdenv.lib.maintainers.garbas ];
-      platforms = stdenv.lib.platforms.unix;
-    };
+  meta = with stdenv.lib; {
+    description = "Simple and easy to use SMTP client with excellent sendmail compatibility";
+    homepage = "http://msmtp.sourceforge.net/";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ garbas peterhoeg ];
+    platforms = platforms.unix;
+  };
 }
