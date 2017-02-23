2f303bc10e9a499c1f4839a9e0d9c495db804cab
Will Dietz <w@wdtz.org>
2017-02-23 07:46:20 -0600

    R/generate-shell: Use R from current tree, fix missing wget dep.
    Without this change, the 'R' version used might be the wrong
    one which changes the package lists used.
    
    Fixes #19530.

--- pkgs/development/r-modules/generate-shell.nix
+++ pkgs/development/r-modules/generate-shell.nix
@@ -1,10 +1,11 @@
-with import <nixpkgs> {};
+with import ../../.. {};
 
 stdenv.mkDerivation {
   name = "generate-r-packages-shell";
 
   buildCommand = "exit 1";
 
+  buildInputs = [ wget ];
   nativeBuildInputs = [ 
     (rWrapper.override {
       packages = with rPackages; [
