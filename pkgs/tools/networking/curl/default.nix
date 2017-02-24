e8aa813fea2e1ead9917b0bbb2cf86914a8bfc8b
Tim Steinbach <tim@nequissimus.com>
2017-02-24 18:12:34 -0500

    curl: 7.53.0 -> 7.53.1

--- pkgs/tools/networking/curl/default.nix
+++ pkgs/tools/networking/curl/default.nix
@@ -21,11 +21,11 @@ assert scpSupport -> libssh2 != null;
 assert c-aresSupport -> c-ares != null;
 
 stdenv.mkDerivation rec {
-  name = "curl-7.53.0";
+  name = "curl-7.53.1";
 
   src = fetchurl {
     url = "http://curl.haxx.se/download/${name}.tar.bz2";
-    sha256 = "008833dd9w4l2277q9r0bsq1vqmm0fr7qqyzvqlw5d47xy5mld5j";
+    sha256 = "1s1hyndva0yp62xy96pcp4anzrvw6cl0abjajim17sbmdp00fwhw";
   };
 
   patches = [ ];
