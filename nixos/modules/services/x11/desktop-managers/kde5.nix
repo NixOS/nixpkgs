b65cc5c59ed39338d764e00e13a79a953a85004a
Gauthier POGAM--LE MONTAGNER <kendos.kenlen@gmail.com>
2017-02-25 00:35:59 +0100

    kde5: add hack font dependency (fix #22975)

--- nixos/modules/services/x11/desktop-managers/kde5.nix
+++ nixos/modules/services/x11/desktop-managers/kde5.nix
@@ -209,7 +209,10 @@ in
             then { GDK_PIXBUF_MODULE_FILE = "${pkgs.librsvg.out}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"; }
             else { });
 
-      fonts.fonts = [ (kde5.oxygen-fonts or pkgs.noto-fonts) ];
+      fonts.fonts = [
+        (kde5.oxygen-fonts or pkgs.noto-fonts)
+        pkgs.hack-font
+      ];
 
       programs.ssh.askPassword = "${kde5.ksshaskpass.out}/bin/ksshaskpass";
 
