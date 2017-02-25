42353c5ae48f54bbc666565bbc36f210823d8a88
Renaud <c0bw3b@users.noreply.github.com>
2017-02-25 13:01:11 +0100

    Fixed iana-etc source URL and homepage
    The homepage has moved years ago.

--- pkgs/data/misc/iana-etc/default.nix
+++ pkgs/data/misc/iana-etc/default.nix
@@ -4,14 +4,14 @@ stdenv.mkDerivation rec {
   name = "iana-etc-2.30";
 
   src = fetchurl {
-    url = "http://sethwklein.net/projects/iana-etc/downloads/${name}.tar.bz2";
+    url = "http://sethwklein.net/${name}.tar.bz2";
     sha256 = "03gjlg5zlwsdk6qyw3v85l129rna5bpm4m7pzrp864h0n97qg9mr";
   };
 
   preInstall = "installFlags=\"PREFIX=$out\"";
 
   meta = {
-    homepage = http://sethwklein.net/projects/iana-etc/;
+    homepage = http://sethwklein.net/iana-etc;
     description = "IANA protocol and port number assignments (/etc/protocols and /etc/services)";
     platforms = stdenv.lib.platforms.unix;
   };
