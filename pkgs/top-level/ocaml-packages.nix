cf39dba1d8293c9ba005456fb2e35c3e61c7e8f4
Vincent Laporte <Vincent.Laporte@gmail.com>
2017-02-26 14:10:02 +0000

    ocamlPackages.ppx_fail: init at 113.33.00+4.03

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -605,7 +605,10 @@ let
       then callPackage ../development/ocaml-modules/janestreet/ppx_enumerate-113_33_00.nix {}
       else callPackage ../development/ocaml-modules/janestreet/ppx-enumerate.nix {};
 
-    ppx_fail = callPackage ../development/ocaml-modules/janestreet/ppx-fail.nix {};
+    ppx_fail =
+      if lib.versionOlder "4.03" ocaml.version
+      then callPackage ../development/ocaml-modules/janestreet/ppx_fail-113_33_00.nix {}
+      else callPackage ../development/ocaml-modules/janestreet/ppx-fail.nix {};
 
     ppx_fields_conv =
       if lib.versionOlder "4.03" ocaml.version
