fcc6e3a6acfebd8db372695c6f721bf6b07fd4b2
Franz Pletz <fpletz@fnordicwalking.de>
2017-02-26 16:25:09 +0100

    linux: 4.9.12 -> 4.9.13
    (cherry picked from commit 701544d0a785274a6d38ee894b3251b2632c2317)

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.9.12";
+  version = "4.9.13";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0fvq3gb80awkwsy0ir6njri2kvx52knmj86kjlm8lqwqis3m9bgw";
+    sha256 = "0s52hrs3wrsa1chzgcqpxwgb5kjmpfd82i964n9d2bmk9zn4liin";
   };
 
   kernelPatches = args.kernelPatches;
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11265,7 +11265,6 @@ in
         # !!! 4.7 patch doesn't apply, 4.8 patch not up yet, will keep checking
         # kernelPatches.cpu-cgroup-v2."4.7"
         kernelPatches.modinst_arg_list_too_long
-        kernelPatches.DCCP_double_free_vulnerability_CVE-2017-6074
       ]
       ++ lib.optionals ((platform.kernelArch or null) == "mips")
       [ kernelPatches.mips_fpureg_emu
