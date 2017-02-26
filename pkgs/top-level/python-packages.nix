421db29915d0db88144f0de374f7f2c784c49b37
Edward Tjörnhammar <ed@cflags.cc>
2017-02-26 11:03:27 +0100

    pythonPackages.pyext: init at 0.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -20185,6 +20185,8 @@ in {
     };
   };
 
+  pyext = callPackage ../development/python-modules/pyext { };
+
   pyfantom = buildPythonPackage rec {
      name = "pyfantom-${version}";
      version = "unstable-2013-12-18";
