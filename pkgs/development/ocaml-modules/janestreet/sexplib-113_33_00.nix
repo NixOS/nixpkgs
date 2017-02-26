0e06c903b51b9319609786803bbe3877a549149f
Vincent Laporte <Vincent.Laporte@gmail.com>
2017-02-26 12:53:14 +0000

    ocamlPackages.sexplib-113.33.00+4.03: fix version number

--- pkgs/development/ocaml-modules/janestreet/sexplib-113_33_00.nix
+++ pkgs/development/ocaml-modules/janestreet/sexplib-113_33_00.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, ocaml, findlib, ocamlbuild, opam, topkg }:
 
 stdenv.mkDerivation {
-  name = "ocaml${ocaml.version}-sexplib-133.33.00+4.03";
+  name = "ocaml${ocaml.version}-sexplib-113.33.00+4.03";
   src = fetchurl {
     url = http://ocaml.janestreet.com/ocaml-core/113.33/files/sexplib-113.33.00+4.03.tar.gz;
     sha256 = "1dirdrags3z8m80z1vczfnpdfzgcvm2wyy7g61fxdr8h3jgixpl3";
