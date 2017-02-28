a639a2b21481288cd354c5f6af20499509f60663
Thomas Tuegel <ttuegel@mailbox.org>
2017-02-28 07:49:25 -0600

    kwindowsystem: mark broken on Qt 5.5
    (cherry picked from commit cff0e06ed70be6115a98551234e80f2516f20aa1)

--- pkgs/development/libraries/kde-frameworks/kwindowsystem.nix
+++ pkgs/development/libraries/kde-frameworks/kwindowsystem.nix
@@ -1,11 +1,15 @@
-{ kdeFramework, lib
-, extra-cmake-modules
-, qttools, qtx11extras
+{
+  kdeFramework, lib,
+  extra-cmake-modules,
+  qtbase, qttools, qtx11extras
 }:
 
 kdeFramework {
   name = "kwindowsystem";
-  meta = { maintainers = [ lib.maintainers.ttuegel ]; };
+  meta = {
+    maintainers = [ lib.maintainers.ttuegel ];
+    broken = builtins.compareVersions qtbase.version "5.6.0" < 0;
+  };
   nativeBuildInputs = [ extra-cmake-modules qttools ];
   propagatedBuildInputs = [ qtx11extras ];
 }
