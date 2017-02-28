aa51bf9dfc39e2d52afcc51b65fe69520e70ded6
Itai Zukerman <zukerman@math-hat.com>
2017-02-27 23:33:48 -0800

    bazel: replace patch with patchShebangs and substituteInPlace
    (cherry picked from commit 17835f14c52acc0ec80e02eb8379ea81b1c58397)

--- pkgs/development/tools/build-managers/bazel/default.nix
+++ pkgs/development/tools/build-managers/bazel/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, jdk, zip, unzip, which, bash, binutils, perl }:
+{ stdenv, fetchurl, jdk, zip, unzip, which, bash, binutils, coreutils }:
 
 stdenv.mkDerivation rec {
 
@@ -21,24 +21,44 @@ stdenv.mkDerivation rec {
 
   sourceRoot = ".";
 
-  patches = [ ./bin_to_env.patch ];
-
   postPatch = ''
-    patchShebangs ./compile.sh
-    for d in scripts src/java_tools src/test src/tools third_party/ijar/test tools; do
-      patchShebangs $d
+    patchShebangs .
+    for f in \
+      src/main/java/com/google/devtools/build/lib/analysis/CommandHelper.java \
+      src/main/java/com/google/devtools/build/lib/bazel/rules/BazelConfiguration.java \
+      src/test/java/com/google/devtools/build/lib/shell/CommandTest.java \
+      src/test/java/com/google/devtools/build/lib/shell/InterruptibleTest.java \
+      src/test/java/com/google/devtools/build/lib/shell/LoadTest.java \
+      src/test/java/com/google/devtools/build/lib/skylark/SkylarkRuleImplementationFunctionsTest.java \
+      src/test/java/com/google/devtools/build/lib/standalone/StandaloneSpawnStrategyTest.java
+    do
+      substituteInPlace $f \
+        --replace /bin/bash ${bash}/bin/bash \
+        --replace /bin/cat ${coreutils}/bin/cat \
+        --replace /bin/echo ${coreutils}/bin/echo \
+        --replace /bin/false ${coreutils}/bin/false \
+        --replace /bin/pwd ${coreutils}/bin/pwd \
+        --replace /bin/sleep ${coreutils}/bin/sleep \
+        --replace /bin/true ${coreutils}/bin/true
     done
   '';
 
   buildInputs = [
     stdenv.cc
     stdenv.cc.cc.lib
-    bash
     jdk
     zip
     unzip
     which
     binutils
+  ];
+
+  # These must be propagated since the dependency is hidden in a compressed
+  # archive.
+
+  propagatedBuildInputs = [
+    bash
+    coreutils
   ];
 
   # If TMPDIR is in the unpack dir we run afoul of blaze's infinite symlink
@@ -51,6 +71,7 @@ stdenv.mkDerivation rec {
   '';
 
   # Build the CPP and Java examples to verify that Bazel works.
+
   doCheck = true;
   checkPhase = ''
     export TEST_TMPDIR=$(pwd)
