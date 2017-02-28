3acf24e67ec030f2687af9d91fb24141a6bbb541
Thomas Tuegel <ttuegel@mailbox.org>
2017-02-28 07:35:52 -0600

    networkmanager-qt: mark broken on Qt 5.5

--- pkgs/development/libraries/kde-frameworks/networkmanager-qt.nix
+++ pkgs/development/libraries/kde-frameworks/networkmanager-qt.nix
@@ -5,7 +5,10 @@
 
 kdeFramework {
   name = "networkmanager-qt";
-  meta = { maintainers = [ lib.maintainers.ttuegel ]; };
+  meta = {
+    maintainers = [ lib.maintainers.ttuegel ];
+    broken = builtins.compareVersions qtbase.version "5.6.0" < 0;
+  };
   nativeBuildInputs = [ extra-cmake-modules ];
   buildInputs = [ qtbase ];
   propagatedBuildInputs = [ networkmanager ];
