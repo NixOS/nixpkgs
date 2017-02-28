0fa7ce86fa93b6d2a53bb7b90f6f801a5bcb4fb1
Robin Gloster <mail@glob.in>
2017-02-28 15:50:10 +0100

    btanks: fix build
    (cherry picked from commit 768fac6c36c50dc2247767d05af9f5c17220da65)

--- pkgs/games/btanks/default.nix
+++ pkgs/games/btanks/default.nix
@@ -1,4 +1,5 @@
-{stdenv, fetchurl, scons, pkgconfig, SDL, mesa, zlib, smpeg, SDL_image, libvorbis, lua5, zip }:
+{ stdenv, fetchurl, fetchpatch, scons, pkgconfig, SDL, mesa, zlib, smpeg
+, SDL_image, libvorbis, expat, zip, lua5_1 }:
 
 stdenv.mkDerivation rec {
   name = "battle-tanks-0.9.8083";
@@ -8,8 +9,16 @@ stdenv.mkDerivation rec {
     sha256 = "0ha35kxc8xlbg74wsrbapfgxvcrwy6psjkqi7c6adxs55dmcxliz";
   };
 
-  buildInputs = [ scons pkgconfig SDL mesa zlib smpeg SDL_image libvorbis lua5
-    zip ];
+  nativeBuildInputs = [ scons pkgconfig ];
+  buildInputs = [ SDL mesa zlib smpeg SDL_image libvorbis expat zip lua5_1 ];
+
+  NIX_CFLAGS_COMPILE = "-I${SDL_image}/include/SDL";
+
+  patches = [ (fetchpatch {
+    name = "gcc-4.7.patch";
+    url = "https://anonscm.debian.org/viewvc/pkg-games/packages/trunk/btanks/debian/patches/gcc-4.7.patch?revision=13641&view=co&pathrev=15758";
+    sha256 = "1dxlk1xh69gj10sqcsyckiakb8an3h41206wby4z44mpmvxc7pi4";
+  }) ];
 
   buildPhase = ''
     scons prefix=$out
