5461879752e48fe1e3e8d3e8fe4439d972ec1f86
Thomas Tuegel <ttuegel@mailbox.org>
2017-02-28 07:36:54 -0600

    kdnssd: mark broken on Qt 5.5
    (cherry picked from commit e5283531c4b1a0baacf2178b408c774d8ea48536)

--- pkgs/development/libraries/kde-frameworks/kdnssd.nix
+++ pkgs/development/libraries/kde-frameworks/kdnssd.nix
@@ -5,7 +5,10 @@
 
 kdeFramework {
   name = "kdnssd";
-  meta = { maintainers = [ lib.maintainers.ttuegel ]; };
+  meta = {
+    maintainers = [ lib.maintainers.ttuegel ];
+    broken = builtins.compareVersions qtbase.version "5.6.0" < 0;
+  };
   nativeBuildInputs = [ extra-cmake-modules qttools ];
   propagatedBuildInputs = [ avahi ];
   buildInputs = [ qtbase ];
