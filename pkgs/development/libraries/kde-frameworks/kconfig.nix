a67116cf1b44b599fab55018248aa04b597a3959
Thomas Tuegel <ttuegel@mailbox.org>
2017-02-28 07:47:53 -0600

    kconfig: mark broken on Qt 5.5
    (cherry picked from commit a8cc6d909b4e571fe8a5ea2e036ee9d86e8165d4)

--- pkgs/development/libraries/kde-frameworks/kconfig.nix
+++ pkgs/development/libraries/kde-frameworks/kconfig.nix
@@ -2,7 +2,10 @@
 
 kdeFramework {
   name = "kconfig";
-  meta = { maintainers = [ lib.maintainers.ttuegel ]; };
+  meta = {
+    maintainers = [ lib.maintainers.ttuegel ];
+    broken = builtins.compareVersions qtbase.version "5.6.0" < 0;
+  };
   nativeBuildInputs = [ extra-cmake-modules qttools ];
   buildInputs = [ qtbase ];
 }
