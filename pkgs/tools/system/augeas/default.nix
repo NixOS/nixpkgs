d9bc4145212b103275f402b14ce989e42b35d992
ndowens <ndowens04@gmail.com>
2017-03-01 13:38:11 -0600

    augeas: 1.5.0 -> 1.7.0

--- pkgs/tools/system/augeas/default.nix
+++ pkgs/tools/system/augeas/default.nix
@@ -2,20 +2,20 @@
 
 stdenv.mkDerivation rec {
   name = "augeas-${version}";
-  version = "1.5.0";
+  version = "1.7.0";
 
   src = fetchurl {
     url = "http://download.augeas.net/${name}.tar.gz";
-    sha256 = "0gzpafrflkr0incq58vjkabfncrpc97d7mdgglkr57iyzvkbcfr2";
+    sha256 = "0qwpjz23z1x7dkf5k2y9f1cppryzhx4hpxprla6a4yvzs1smacdr";
   };
-
-  buildInputs = [ pkgconfig readline libxml2 ];
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [ readline libxml2 ];
 
   meta = with stdenv.lib; {
     description = "Configuration editing tool";
     license = licenses.lgpl2;
     homepage = http://augeas.net/;
-    maintainers = with maintainers; [offline];
+    maintainers = with maintainers; [ offline ndowens ];
     platforms = platforms.unix;
   };
 }
