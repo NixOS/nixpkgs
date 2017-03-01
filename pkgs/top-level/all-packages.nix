b131ce283083ea6fd0828e6e1b98c2bff75cc65f
Profpatsch <mail@profpatsch.de>
2017-03-01 11:05:23 +0100

    lilyterm-git: init at 2017-01-06
    The latest stable release is quite old (2013-02) and many improvements have been
    made in the meantime.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14181,6 +14181,11 @@ with pkgs;
   lilyterm = callPackage ../applications/misc/lilyterm {
     inherit (gnome2) vte;
     gtk = gtk2;
+    flavour = "stable";
+  };
+
+  lilyterm-git = lilyterm.override {
+    flavour = "git";
   };
 
   lumail = callPackage ../applications/networking/mailreaders/lumail { };
