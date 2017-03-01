ab98b60f80f160c8b12bea7983451651b543eaea
Shea Levy <shea@shealevy.com>
2017-03-01 13:00:57 -0500

    haskell generic-builder: Pass through the list of haskell build inputs.
    Useful for building custom envs.
    
    (cherry picked from commit c153036525575d046fc808a4805d023e3e2e9e75)

--- pkgs/development/haskell-modules/generic-builder.nix
+++ pkgs/development/haskell-modules/generic-builder.nix
@@ -290,7 +290,7 @@ stdenv.mkDerivation ({
 
   passthru = passthru // {
 
-    inherit pname version ghcEnv;
+    inherit pname version ghcEnv haskellBuildInputs;
 
     isHaskellLibrary = hasActiveLibrary;
 
