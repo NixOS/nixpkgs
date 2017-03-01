f769c470cfe8b36c1a09b69b04bf333273e41909
Thomas Tuegel <ttuegel@mailbox.org>
2017-02-28 19:09:56 -0600

    nixos/tests: fix Plasma 5 test
    (cherry picked from commit 0da421ce17a0226e4880c596702adcc58cb19d6c)

--- nixos/release.nix
+++ nixos/release.nix
@@ -255,7 +255,7 @@ in rec {
   tests.influxdb = callTest tests/influxdb.nix {};
   tests.ipv6 = callTest tests/ipv6.nix {};
   tests.jenkins = callTest tests/jenkins.nix {};
-  tests.kde5 = callTest tests/kde5.nix {};
+  tests.plasma5 = callTest tests/plasma5.nix {};
   tests.keymap = callSubTests tests/keymap.nix {};
   tests.initrdNetwork = callTest tests/initrd-network.nix {};
   tests.keystone = callTest tests/keystone.nix {};
