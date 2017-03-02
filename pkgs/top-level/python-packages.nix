0904cf034cda55a423b0dbe9865fde9a813ffe32
Alistair Bill <alistair.bill@gmail.com>
2017-03-02 18:53:22 +0000

    proselint: init at 0.8.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18901,6 +18901,8 @@ in {
      };
   };
 
+  proselint = callPackage ../tools/text/proselint { };
+
   pylibconfig2 = buildPythonPackage rec {
     name = "pylibconfig2-${version}";
     version = "0.2.4";
