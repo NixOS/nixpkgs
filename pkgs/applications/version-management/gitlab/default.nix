9059a326906fb33623cd040600db7ccd4e1128c0
Franz Pletz <fpletz@fnordicwalking.de>
2017-03-02 04:18:19 +0100

    gitlab: 8.16.6 -> 8.17.2

--- pkgs/applications/version-management/gitlab/default.nix
+++ pkgs/applications/version-management/gitlab/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, lib, bundler, fetchFromGitHub, bundlerEnv, libiconv, ruby
-, tzdata, git, nodejs, procps
+{ stdenv, lib, bundler, fetchurl, fetchFromGitHub, bundlerEnv, libiconv, ruby
+, tzdata, git, nodejs, procps, dpkg
 }:
 
 /* When updating the Gemfile add `gem "activerecord-nulldb-adapter"`
@@ -18,19 +18,27 @@ let
     };
   };
 
+  version = "8.17.2";
+
+  gitlabDeb = fetchurl {
+    url = "https://packages.gitlab.com/gitlab/gitlab-ce/packages/debian/jessie/gitlab-ce_${version}-ce.0_amd64.deb/download";
+    sha256 = "134vhqf216blwqvp0lqykyfq9h6zb0q934arq7zr4dyb8w4jhp1l";
+  };
+
 in
 
 stdenv.mkDerivation rec {
   name = "gitlab-${version}";
-  version = "8.16.6";
 
-  buildInputs = [ env ruby bundler tzdata git nodejs procps ];
+  buildInputs = [
+    env ruby bundler tzdata git nodejs procps dpkg
+  ];
 
   src = fetchFromGitHub {
     owner = "gitlabhq";
     repo = "gitlabhq";
     rev = "v${version}";
-    sha256 = "03rzms2frwx4c09l2rig1amlxj965s2iq421i52j8wj2khb7pd7g";
+    sha256 = "1fm46scpmw357c5lmp845sd76pxywj2dx3k46rc0d7df3diqhsil";
   };
 
   patches = [
@@ -64,9 +72,15 @@ stdenv.mkDerivation rec {
 
   buildPhase = ''
     mv config/gitlab.yml.example config/gitlab.yml
-    GITLAB_DATABASE_ADAPTER=nulldb \
-      SKIP_STORAGE_VALIDATION=true \
-      rake assets:precompile RAILS_ENV=production
+
+    dpkg -x ${gitlabDeb} .
+    mv -v opt/gitlab/embedded/service/gitlab-rails/public/assets public
+    rm -rf opt
+
+    export GITLAB_DATABASE_ADAPTER=nulldb
+    export SKIP_STORAGE_VALIDATION=true
+    rake assets:precompile RAILS_ENV=production
+
     mv config/gitlab.yml config/gitlab.yml.example
     rm config/secrets.yml
     mv config config.dist
