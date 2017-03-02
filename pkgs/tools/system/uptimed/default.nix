f8f068af1017a87a183c76f4707c7e9936515367
Jörg Thalheim <Mic92@users.noreply.github.com>
2017-03-02 10:03:00 +0100

    uptimed: autoreconfHook belongs to nativeBuildInputs

--- pkgs/tools/system/uptimed/default.nix
+++ pkgs/tools/system/uptimed/default.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
     owner = "rpodgorny";
   };
 
-  buildInputs = [ autoreconfHook ];
+  nativeBuildInputs = [ autoreconfHook ];
   patches = [ ./no-var-spool-install.patch ];
 
   meta = with stdenv.lib; {
