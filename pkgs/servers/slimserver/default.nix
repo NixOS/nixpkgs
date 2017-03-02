ffa3dbeee1238729af81c4d41fa083007ca39923
Philipp Hausmann <ph_git@314.ch>
2017-03-02 20:38:06 +0100

    init slimserver

---
+++ pkgs/servers/slimserver/default.nix
@@ -0,0 +1,47 @@
+{ stdenv, fetchFromGitHub
+, makeWrapper
+#, sqlite, expat, mp4v2, flac, spidermonkey_1_8_5, taglib, libexif, curl, ffmpeg, file
+, perl, perlPackages }:
+
+stdenv.mkDerivation rec {
+  name = "slimserver-${version}";
+  version = "7.9";
+
+  src = fetchFromGitHub {
+    owner  = "Logitech";
+    repo   = "slimserver";
+    rev    = "095dd886a01e56a1ffe1b2ea31bb290d17c83948";
+    sha256 = "06s945spxh6j4g0l1k6cxpq04011ad4swgqd2in87c86sf6bm445";
+  };
+
+  buildInputs = [
+    makeWrapper
+    perl
+    perlPackages.Log4Perl
+    perlPackages.AudioScan
+    perlPackages.ImageScale
+  ];
+
+  buildPhase = ''
+  '';
+
+  installPhase = ''
+    cp -r . $out
+  '';
+  
+  postFixup = ''
+    wrapProgram $out/slimserver.pl \
+      --set PERL5LIB "${with perlPackages; stdenv.lib.makePerlPath [
+      Log4Perl
+      ]}"
+  '';
+
+  meta = with stdenv.lib; {
+    homepage = https://github.com/Logitech/slimserver;
+    description = "Server for Logitech Squeezebox players. This server is also called Logitech Media Server";
+    # TODO: not all source code is under gpl2!
+    license = licenses.gpl2;
+    maintainers = [ maintainers.phile314 ];
+    platforms = platforms.linux;
+  };
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10896,6 +10896,8 @@ with pkgs;
 
   sipwitch = callPackage ../servers/sip/sipwitch { };
 
+  slimserver = callPackage ../servers/slimserver { };
+
   smcroute = callPackage ../servers/smcroute { };
 
   spawn_fcgi = callPackage ../servers/http/spawn-fcgi { };
