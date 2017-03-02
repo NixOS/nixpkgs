646958098d77b62ccc6056d529d8acba91c28fc0
Graham Christensen <graham@grahamc.com>
2017-03-02 08:25:35 -0500

    kdeFrameworks.kio: patch for insecure URL passing
    (cherry picked from commit 5ce06263a35da4b99589227f3553093d381c24c9)

--- pkgs/development/libraries/kde-frameworks/kio/default.nix
+++ pkgs/development/libraries/kde-frameworks/kio/default.nix
@@ -4,7 +4,7 @@
 , kdbusaddons, kdoctools, ki18n, kiconthemes, kitemviews
 , kjobwidgets, knotifications, kservice, ktextwidgets, kwallet
 , kwidgetsaddons, kwindowsystem, kxmlgui
-, qtscript, qtx11extras, solid
+, qtscript, qtx11extras, solid, fetchpatch
 }:
 
 kdeFramework {
@@ -17,5 +17,12 @@ kdeFramework {
     ktextwidgets kwallet kwidgetsaddons kwindowsystem kxmlgui solid qtscript
     qtx11extras
   ];
-  patches = copyPathsToStore (lib.readPathsFromFile ./. ./series);
+  patches = (copyPathsToStore (lib.readPathsFromFile ./. ./series))
+    ++ [
+      (fetchpatch {
+        name = "SanitizeURLsBeforePassingThemToFindProxyForURL.patch";
+        url = "https://cgit.kde.org/kio.git/patch/?id=f9d0cb47cf94e209f6171ac0e8d774e68156a6e4";
+        sha256 = "1s6rcp8rrlhc6rgy3b303y0qq0s8371n12r5lk9zbkw14wjvbix0";
+      })
+    ];
 }
