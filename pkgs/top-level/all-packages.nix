3f34000358f40c459b533724a0b0240273f682bb
Peter Hoeg <peter@speartail.com>
2017-03-03 16:47:04 +0800

    xca: build against qt 5.6 as 5.7 segfaults

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15966,7 +15966,8 @@ with pkgs;
   };
   xbmc-retroarch-advanced-launchers = kodi-retroarch-advanced-launchers;
 
-  xca = libsForQt5.callPackage ../applications/misc/xca { };
+  # v1.3.2 segfaults with qt 5.7
+  xca = libsForQt56.callPackage ../applications/misc/xca { };
 
   xcalib = callPackage ../tools/X11/xcalib { };
 
