ef8f8c0c1d20901371227354ff2d98e29054339c
Matthias Beyer <mail@beyermatthias.de>
2017-03-03 17:15:24 +0100

    glances: Add optional package injection

--- pkgs/tools/system/glances/default.nix
+++ pkgs/tools/system/glances/default.nix
@@ -1,4 +1,30 @@
-{ pythonPackages }:
+{ lib, pythonPackages
+, withBatinfoPkg ? null
+, withBernhardPkg ? null
+, withBottlePkg ? null
+, withCassandra-driverPkg ? null
+, withCouchdbPkg ? null
+, withDockerPkg ? null
+, withElasticsearchPkg ? null
+, withHddtempPkg ? null
+, withInfluxdbPkg ? null
+, withMatplotlibPkg ? null
+, withNetifacesPkg ? null
+, withNvidia-ml-pyPkg ? null
+, withPikaPkg ? null
+, withPotsdbPkg ? null
+, withPy3sensorsPkg ? null
+, withPy-cpuinfoPkg ? null
+, withPymdstatPkg ? null
+, withPysnmpPkg ? null
+, withPystachePkg ? null
+, withPyzmqPkg ? null
+, withRequestsPkg ? null
+, withScandirPkg ? null
+, withStatsdPkg ? null
+, withWifiPkg ? null
+, withZeroconfPkg ? null
+}:
 
 pythonPackages.buildPythonPackage rec {
   name = "glances-${version}";
@@ -15,7 +41,35 @@ pythonPackages.buildPythonPackage rec {
   doCheck = false;
 
   buildInputs = with pythonPackages; [ unittest2 ];
-  propagatedBuildInputs = with pythonPackages; [ psutil setuptools bottle batinfo pkgs.hddtemp pysnmp ];
+  propagatedBuildInputs = with pythonPackages; [
+    psutil setuptools bottle batinfo pkgs.hddtemp pysnmp
+  ]
+  ++ (if isNull withBatinfoPkg then [] else [withBatinfoPkg] )
+  ++ (if isNull withBernhardPkg then [] else [withBernhardPkg] )
+  ++ (if isNull withBottlePkg then [] else [withBottlePkg] )
+  ++ (if isNull withCassandra-driverPkg then [] else [withCassandr]a)
+  ++ (if isNull withCouchdbPkg then [] else [withCouchdbPkg] )
+  ++ (if isNull withDockerPkg then [] else [withDockerPkg] )
+  ++ (if isNull withElasticsearchPkg then [] else [withElasticsearchPkg] )
+  ++ (if isNull withHddtempPkg then [] else [withHddtempPkg] )
+  ++ (if isNull withInfluxdbPkg then [] else [withInfluxdbPkg] )
+  ++ (if isNull withMatplotlibPkg then [] else [withMatplotlibPkg] )
+  ++ (if isNull withNetifacesPkg then [] else [withNetifacesPkg] )
+  ++ (if isNull withNvidia-ml-pyPkg then [] else [withNvidi]a)
+  ++ (if isNull withPikaPkg then [] else [withPikaPkg] )
+  ++ (if isNull withPotsdbPkg then [] else [withPotsdbPkg] )
+  ++ (if isNull withPy3sensorsPkg then [] else [withPy3sensorsPkg] )
+  ++ (if isNull withPy-cpuinfoPkg then [] else [withP]y)
+  ++ (if isNull withPymdstatPkg then [] else [withPymdstatPkg] )
+  ++ (if isNull withPysnmpPkg then [] else [withPysnmpPkg] )
+  ++ (if isNull withPystachePkg then [] else [withPystachePkg] )
+  ++ (if isNull withPyzmqPkg then [] else [withPyzmqPkg] )
+  ++ (if isNull withRequestsPkg then [] else [withRequestsPkg] )
+  ++ (if isNull withScandirPkg then [] else [withScandirPkg] )
+  ++ (if isNull withStatsdPkg then [] else [withStatsdPkg] )
+  ++ (if isNull withWifiPkg then [] else [withWifiPkg] )
+  ++ (if isNull withZeroconfPkg then [] else [withZeroconfPkg] )
+  ;
 
   preConfigure = ''
     sed -i 's/data_files\.append((conf_path/data_files.append(("etc\/glances"/' setup.py;
