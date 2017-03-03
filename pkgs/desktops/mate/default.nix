8ef7b2149418be4f79dab029b5054f98f3ae434e
Clemens Manert <Clemens@Manert.de>
2017-03-03 03:58:54 +0100

    mate-terminal: Init at 1.17.0

--- pkgs/desktops/mate/default.nix
+++ pkgs/desktops/mate/default.nix
@@ -4,5 +4,6 @@ rec {
   mate-desktop = callPackage ./mate-desktop { };
   mate-icon-theme = callPackage ./mate-icon-theme { };
   mate-icon-theme-faenza = callPackage ./mate-icon-theme-faenza { };
+  mate-terminal = callPackage ./mate-terminal { };
   mate-themes = callPackage ./mate-themes { };
 }
