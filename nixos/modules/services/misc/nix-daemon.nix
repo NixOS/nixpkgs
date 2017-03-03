3070c887987e9c24a72a5b7b18db29ebe1ed6d4c
Eelco Dolstra <edolstra@gmail.com>
2017-03-03 15:42:53 +0100

    Fix incorrect $NIX_BUILD_HOOK on Nix 1.12

--- nixos/modules/services/misc/nix-daemon.nix
+++ nixos/modules/services/misc/nix-daemon.nix
@@ -389,7 +389,11 @@ in
       }
 
       // optionalAttrs cfg.distributedBuilds {
-        NIX_BUILD_HOOK = "${nix}/libexec/nix/build-remote.pl";
+        NIX_BUILD_HOOK =
+          if versionAtLeast (getVersion nix) "1.12pre4997" then
+            "${nix}/libexec/nix/build-remote"
+          else
+            "${nix}/libexec/nix/build-remote.pl";
         NIX_REMOTE_SYSTEMS = "/etc/nix/machines";
         NIX_CURRENT_LOAD = "/run/nix/current-load";
       };
