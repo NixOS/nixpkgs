b79306d29810d0a1ce53ba9bf4c7979ba3ced59e
Renaud <c0bw3b@users.noreply.github.com>
2017-03-04 16:55:47 +0100

    uriparser: 0.8.2 -> 0.8.4
    Minor version bump.
    Fixed the Makefile for the doc : there is no .map files to install in this release since dot outputs images in PNG and SVG

--- pkgs/development/libraries/uriparser/default.nix
+++ pkgs/development/libraries/uriparser/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, cpptest, pkgconfig }:
+{ stdenv, fetchurl, cpptest, pkgconfig, doxygen, graphviz }:
 
 stdenv.mkDerivation rec {
   name = "uriparser-${version}";
@@ -9,9 +9,16 @@ stdenv.mkDerivation rec {
     sha256 = "08vvcmg4mcpi2gyrq043c9mfcy3mbrw6lhp86698hx392fjcsz6f";
   };
   
-  configureFlags = "--disable-doc";
   
-  buildInputs = [ cpptest pkgconfig ];
+  buildInputs = [ cpptest pkgconfig doxygen graphviz ];
+  
+  # There is actually no .map files to install in doc for v0.8.4
+  # (dot outputs only SVG and PNG in this release)
+  preBuild = ''
+    substituteInPlace doc/Makefile.am --replace " html/*.map" ""
+    substituteInPlace doc/Makefile.in --replace " html/*.map" ""
+  '';
+  
   
   meta = with stdenv.lib; {
     homepage = http://uriparser.sourceforge.net/;
