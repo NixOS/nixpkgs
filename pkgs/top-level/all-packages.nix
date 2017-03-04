2c006ca80565a2a58fbf992f6442722baed497cc
David McFarland <corngood@gmail.com>
2017-03-04 16:40:50 -0400

    Revert "openssl: add custom build of 1.0.2 for steam"
    No longer necessary. See https://github.com/NixOS/nixpkgs/pull/23034#issuecomment-291005754
    
    This reverts commit a50784b34e771aa4a6c5f4eeb8c25295c15e4ed4.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9240,7 +9240,6 @@ with pkgs;
   wolfssl = callPackage ../development/libraries/wolfssl { };
 
   openssl = openssl_1_0_2;
-  openssl-steam = openssl_1_0_2-steam;
 
   inherit (callPackages ../development/libraries/openssl {
       fetchurl = fetchurlBoot;
@@ -9250,8 +9249,7 @@ with pkgs;
       };
     })
     openssl_1_0_2
-    openssl_1_1_0
-    openssl_1_0_2-steam;
+    openssl_1_1_0;
 
   openssl-chacha = callPackage ../development/libraries/openssl/chacha.nix {
     cryptodevHeaders = linuxPackages.cryptodev.override {
