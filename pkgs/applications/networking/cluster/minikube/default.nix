8852317358847290060ab746bbc9f876a957c0c1
Peter Hoeg <peter@speartail.com>
2017-03-05 11:23:44 +0800

    minikube: 0.16.0 -> 0.17.1

--- pkgs/applications/networking/cluster/minikube/default.nix
+++ pkgs/applications/networking/cluster/minikube/default.nix
@@ -5,14 +5,14 @@ let
          then "linux-amd64"
          else "darwin-amd64";
   checksum = if stdenv.isLinux
-             then "0njx4vzr0cpr3dba08w0jrlpfb8qrmxq5lqfrk3qrx29x5y6i6hi"
-             else "0i21m1pys6rdxcwsk987l08lhzpcbg4bdrznaam02g6jj6jxvq0x";
+             then "0cdcabsx5l4jbpyj3zzyz5bnzks6wl64bmzdsnk41x92ar5y5yal"
+             else "12f3b7s5lwpvzx4wj6i6h62n4zjshqf206fxxwpwx9kpsdaw6xdi";
 
 # TODO: compile from source
 
 in stdenv.mkDerivation rec {
   pname = "minikube";
-  version = "0.16.0";
+  version = "0.17.1";
   name = "${pname}-${version}";
 
   src = fetchurl {
@@ -20,7 +20,7 @@ in stdenv.mkDerivation rec {
     sha256 = "${checksum}";
   };
 
-  phases = [ "installPhase" ];
+  phases = [ "installPhase" "fixupPhase" ];
 
   buildInputs = [ makeWrapper ];
 
@@ -28,6 +28,10 @@ in stdenv.mkDerivation rec {
 
   installPhase = ''
     install -Dm755 ${src} $out/bin/${pname}
+  '';
+
+  fixupPhase = ''
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$out/bin/minikube"
 
     wrapProgram $out/bin/${pname} \
       --prefix PATH : ${binPath}
