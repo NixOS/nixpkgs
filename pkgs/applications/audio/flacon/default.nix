f1d7e115042a62bfc6f244895520a6647ffc0df5
ndowens <ndowens04@gmail.com>
2017-03-05 09:38:26 -0600

    flacon: 2.0.1 -> 2.1.1
    Added gsettings_desktop_schemas to buildInputs to fix a luanch error

--- pkgs/applications/audio/flacon/default.nix
+++ pkgs/applications/audio/flacon/default.nix
@@ -1,18 +1,22 @@
-{ stdenv, lib, fetchFromGitHub, cmake, qt5, libuchardet, pkgconfig, makeWrapper
-, shntool, flac, opusTools, vorbisTools, mp3gain, lame, wavpack, vorbisgain
+{ stdenv, lib, fetchFromGitHub, cmake, qt5
+, libuchardet, pkgconfig, makeWrapper, shntool
+, flac, opusTools, vorbisTools, mp3gain, lame
+, wavpack, vorbisgain, gsettings_desktop_schemas
 }:
 
 stdenv.mkDerivation rec {
   name = "flacon-${version}";
-  version = "2.0.1";
+  version = "2.1.1";
+
   src = fetchFromGitHub {
     owner = "flacon";
     repo = "flacon";
     rev = "v${version}";
-    sha256 = "0hip411k3arb96rnd22ifs9shlv0xmy96hhx1jcwdk48kw8aa9rw";
+    sha256 = "0jazv3d1xaydp2ws1pd5rmga76z5yk74v3a8yqfc8vbb2z6ahimz";
   };
 
-  buildInputs = [ cmake qt5.qtbase qt5.qttools libuchardet pkgconfig makeWrapper ];
+  nativeBuildInputs = [ cmake pkgconfig makeWrapper ];
+  buildInputs = [ qt5.qtbase qt5.qttools libuchardet gsettings_desktop_schemas ];
 
   postInstall = ''
     wrapProgram $out/bin/flacon \
@@ -20,11 +24,11 @@ stdenv.mkDerivation rec {
       mp3gain lame wavpack vorbisgain ]}"
   '';
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Extracts audio tracks from an audio CD image to separate tracks.";
     homepage = https://flacon.github.io/;
-    license = stdenv.lib.licenses.lgpl21;
-    platforms = stdenv.lib.platforms.linux;
-    maintainers = [ stdenv.lib.maintainers.nico202 ];
+    license = licenses.lgpl21;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ ndowens nico202 ];
   };
 }
