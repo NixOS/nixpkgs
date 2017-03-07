d4d251d1e7dcf97aaf01cc5ab1f433d5764ae9f4
ndowens <ndowens04@gmail.com>
2017-03-06 20:23:23 -0600

    dashpay: 0.12.055 -> 0.12.1.3

--- pkgs/applications/altcoins/dashpay.nix
+++ pkgs/applications/altcoins/dashpay.nix
@@ -1,25 +1,41 @@
-{ fetchzip, stdenv, pkgconfig, autoreconfHook
+{ fetchFromGitHub, stdenv, pkgconfig, autoreconfHook
 , openssl, db48, boost, zlib, miniupnpc
-, qt4, qrencode, glib, protobuf, yasm
-, utillinux }:
+, qrencode, glib, protobuf, yasm, libevent
+, utillinux
+, with_gui ? true, qt4
+, enable_upnp ? false 
+, disable_wallet ? false
+, disable_daemon ? false }:
+
+assert with_gui -> qt4 !=null;
 
 with stdenv.lib;
 stdenv.mkDerivation rec {
+  name = "dashpay-${version}";
+  version = "0.12.1.3";
 
-  name = "dashpay-${meta.version}";
-
-  src = fetchzip {
-    url = "https://github.com/dashpay/dash/archive/v${meta.version}.tar.gz";
-    sha256 = "19bk7cviy3n2dpj4kr3i6i0i3ac2l5ri8ln1a51nd3n90k016wnx";
+  src = fetchFromGitHub {
+    owner = "dashpay";
+    repo= "dash";
+    rev = "v${version}";
+    sha256 = "0h0fxhh30wy5vp06l1mkswhz565qs6j9y0dm84fmn28rdfvhv2aj";
   };
 
-  buildInputs = [ pkgconfig autoreconfHook glib openssl db48 yasm
-                  boost zlib miniupnpc protobuf qt4 qrencode utillinux ];
+  nativeBuildInputs = [ pkgconfig autoreconfHook ];
+  buildInputs = [ glib openssl db48 yasm boost zlib libevent 
+                  miniupnpc protobuf qrencode utillinux ]
+                  ++ optional with_gui qt4
+                  ;
 
-  configureFlags = [ "--with-boost-libdir=${boost.out}/lib" ];
+  configureFlags = [ "--with-boost-libdir=${boost.out}/lib" ]
+                    ++ optional with_gui "--with-gui=qt4"
+                    ++ optional (!with_gui) "--with-gui=no"
+                    ++ optional enable_upnp "--enable-upnp-default"
+                    ++ optional disable_wallet "--disable-wallet"
+                    ++ optional disable_daemon "--disable-wallet"
+                    ;
 
   meta = with stdenv.lib; {
-    version = "0.12.0.55";
     description = "A decentralized key/value registration and transfer system";
     longDescription = ''
       Dash (DASH) is an open sourced, privacy-centric digital currency
