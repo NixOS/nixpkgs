677c9469ba33656f03f486cf5273588a83a62d9d
ndowens <ndowens04@gmail.com>
2017-03-06 20:49:01 -0600

    dogecoin: 1.8.2 -> 1.10.0

--- pkgs/applications/altcoins/dogecoin.nix
+++ pkgs/applications/altcoins/dogecoin.nix
@@ -1,4 +1,4 @@
-{ stdenv , fetchurl
+{ stdenv , fetchFromGitHub
 , pkgconfig, autoreconfHook
 , db5, openssl, boost, zlib, miniupnpc
 , glib, protobuf, utillinux, qt4, qrencode
@@ -8,25 +8,19 @@ with stdenv.lib;
 stdenv.mkDerivation rec {
 
   name = "dogecoin" + (toString (optional (!withGui) "d")) + "-" + version;
-  version = "1.8.2";
+  version = "1.10.0";
 
-  src = fetchurl {
-    url = "https://github.com/dogecoin/dogecoin/archive/v${version}.tar.gz";
-    sha256 = "17jxsxsrsz3qy2hxdpw78vcbnnd0nq614iy42ypzhw4pdpz0s1l7";
+  src = fetchFromGitHub {
+    owner = "dogecoin";
+    repo = "dogecoin";
+    rev = "v${version}";
+    sha256 = "16q3rldj04hkzzjd23h0knszqr5dgixizy4iyc129mz8wa8pbnvy";
   };
 
-  buildInputs = [ autoreconfHook pkgconfig openssl
-                  db5 openssl utillinux protobuf boost zlib miniupnpc ]
+  nativeBuildInputs = [ pkgconfig autoreconfHook ];
+  buildInputs = [ openssl db5 openssl utillinux 
+                  protobuf boost zlib miniupnpc ]
                   ++ optionals withGui [ qt4 qrencode ];
-
-  # BSD DB5 location
-  patchPhase = ''
-    sed -i \
-      -e 's,BDB_CPPFLAGS=$,BDB_CPPFLAGS="-I${db5}/include",g' \
-      -e 's,BDB_LIBS=$,BDB_LIBS="-L${db5}/lib",g' \
-      -e 's,bdbdirlist=$,bdbdirlist="${db5}/include",g' \
-      src/m4/dogecoin_find_bdb51.m4
-  '';
 
   configureFlags = [ "--with-incompatible-bdb"
                      "--with-boost-libdir=${boost.out}/lib" ]
@@ -43,6 +37,6 @@ stdenv.mkDerivation rec {
     homepage = http://www.dogecoin.com/;
     license = licenses.mit;
     maintainers = with maintainers; [ edwtjo offline AndersonTorres ];
-    platforms = with platforms; linux;
+    platforms = platforms.linux;
   };
 }
