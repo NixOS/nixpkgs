7f214e64109438fb8a9bbffd94ca41021d8b8fa7
Justin Bedo <cu@cua0.org>
2017-03-07 15:54:31 +1100

    bedtools: init at 2.26.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16970,6 +16970,8 @@ with pkgs;
     stdenv = overrideCC stdenv gcc49;
   };
 
+  bedtools = callPackage ../applications/science/biology/bedtools/default.nix { };
+
   bcftools = callPackage ../applications/science/biology/bcftools { };
 
   ecopcr = callPackage ../applications/science/biology/ecopcr { };
