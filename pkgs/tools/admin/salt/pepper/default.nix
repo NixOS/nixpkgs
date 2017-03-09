de363e8706e989dd05afe3ee77633e4d7314d507
Pierre Radermecker <pierrer@pi3r.be>
2017-03-09 10:44:39 +0100

    pepper: add 'pierrer' as a maintainer

--- pkgs/tools/admin/salt/pepper/default.nix
+++ pkgs/tools/admin/salt/pepper/default.nix
@@ -10,12 +10,13 @@ python2Packages.buildPythonApplication rec {
     url = "https://github.com/saltstack/pepper/releases/download/${version}/${name}.tar.gz";
     sha256 = "0gf4v5y1kp16i1na4c9qw7cgrpsh21p8ldv9r6b8gdwcxzadxbck";
   };
-  
+
   doCheck = false; # no tests available
 
-  meta = {
+  meta = with lib; {
     description = "A CLI front-end to a running salt-api system";
     homepage = https://github.com/saltstack/pepper;
-    license = lib.licenses.asl20;
+    maintainers = [ maintainers.pierrer ];
+    license = licenses.asl20;
   };
 }
