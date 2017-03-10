19bd746ca390635839a3d1de3b311c418ca01541
Sebastian Hagen <sh_c_git@memespace.net>
2017-03-10 05:14:14 +0000

    hwdata: Use content-addressed source file; previous source tarball changed contents.
    (cherry picked from commit 00f16e3d7c4239bcb4c0b55302e4956adf69d691)

--- pkgs/os-specific/linux/hwdata/default.nix
+++ pkgs/os-specific/linux/hwdata/default.nix
@@ -5,8 +5,8 @@ stdenv.mkDerivation rec {
   version = "0.291";
 
   src = fetchurl {
-    url = "https://git.fedorahosted.org/cgit/hwdata.git/snapshot/hwdata-${version}.tar.xz";
-    sha256 = "121qixrdhdncva1cnj7m7jlqvi1kbj85dpi844jiis3a8hgpzw5a";
+    url = "http://pkgs.fedoraproject.org/repo/pkgs/hwdata/hwdata-0.291.tar.bz2/effe59bf406eb03bb295bd34e0913dd8/hwdata-0.291.tar.bz2";
+    sha256 = "01cq9csryxcrilnqdjd2r8gpaap3mk4968v7y36c7shyyaf9zkym";
   };
 
   preConfigure = "patchShebangs ./configure";
