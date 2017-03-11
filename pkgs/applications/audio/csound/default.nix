dd822be879ebebd40c085d2a793db3ec788afc75
hlolli <hlolli@gmail.com>
2017-03-11 23:40:29 +0100

    bump csound version

--- pkgs/applications/audio/csound/default.nix
+++ pkgs/applications/audio/csound/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, cmake, libsndfile, flex, bison
+{ stdenv, fetchurl, cmake, libsndfile, flex, bison, boost, boost-build
 , alsaLib ? null
 , libpulseaudio ? null
 , tcltk ? null
@@ -12,18 +12,18 @@
 }:
 
 stdenv.mkDerivation {
-  name = "csound-6.04";
+  name = "csound-6.08.1";
 
   enableParallelBuilding = true;
 
   hardeningDisable = [ "format" ];
 
   src = fetchurl {
-    url = mirror://sourceforge/csound/Csound6.04.tar.gz;
-    sha256 = "1030w38lxdwjz1irr32m9cl0paqmgr02lab2m7f7j1yihwxj1w0g";
+    url = https://github.com/csound/csound/archive/6.08.1.tar.gz;
+    sha256 = "153c6c06573dd0c6989f45df1cb32ae48fb2ea942428900c0097ecc1476b82b7";
   };
 
-  buildInputs = [ cmake libsndfile flex bison alsaLib libpulseaudio tcltk ];
+  buildInputs = [ cmake libsndfile flex bison alsaLib libpulseaudio tcltk boost ];
 
   meta = {
     description = "Sound design, audio synthesis, and signal processing system, providing facilities for music composition and performance on all major operating systems and platforms";
