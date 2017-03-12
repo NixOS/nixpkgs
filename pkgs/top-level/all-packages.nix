a92dfe78e95ebe86736c6b02a0cadf939948f18c
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2017-03-12 21:54:14 +0100

    qcachegrind: init at 16.12.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6945,6 +6945,10 @@ with pkgs;
 
   valkyrie = callPackage ../development/tools/analysis/valkyrie { };
 
+  qcachegrind = libsForQt57.callPackage ../development/tools/analysis/qcachegrind {
+    qt = qt57;
+  };
+
   verasco = ocaml-ng.ocamlPackages_4_02.verasco.override {
     coq = coq_8_4;
   };
