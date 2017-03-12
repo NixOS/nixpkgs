7d681d4c8b9a8274e4c71939bd627714c05f85f0
Alastair Pharo <asppsa@gmail.com>
2017-03-12 12:04:31 +1100

    purePackages.octave: 0.7 -> 0.9
    Also fixes a compile error (see #23253)

--- pkgs/development/pure-modules/octave/default.nix
+++ pkgs/development/pure-modules/octave/default.nix
@@ -2,13 +2,15 @@
 
 stdenv.mkDerivation rec {
   baseName = "octave";
-  version = "0.7";
+  version = "0.9";
   name = "pure-${baseName}-${version}";
 
   src = fetchurl {
     url = "https://bitbucket.org/purelang/pure-lang/downloads/${name}.tar.gz";
-    sha256 = "04c1q5cjcyc5sg15ny1hn43rkphja3virw4k110cahc3piwbpsqk";
+    sha256 = "001fc2188bb128d8865fff989e6adb5588645b26e09a9999fc4bdd3c62dd3550";
   };
+
+  CPPFLAGS = "-std=c++11";
 
   buildInputs = [ pkgconfig ];
   propagatedBuildInputs = [ pure octave ];
