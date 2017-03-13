57e768f58e7d2019ea19520226d5fb6e05e98c96
Frederik Rietdijk <fridh@fridh.nl>
2017-03-13 15:30:39 +0100

    hypothesis: 3.6.0 -> 3.6.1

--- pkgs/development/python-modules/hypothesis.nix
+++ pkgs/development/python-modules/hypothesis.nix
@@ -10,14 +10,14 @@ buildPythonPackage rec {
   # If you need these, you can just add them to your environment.
 
   name = "hypothesis-${version}";
-  version = "3.6.0";
+  version = "3.6.1";
 
   # Upstream prefers github tarballs
   src = fetchFromGitHub {
     owner = "HypothesisWorks";
     repo = "hypothesis";
     rev = "${version}";
-    sha256 = "0a3r4c8sr9jn7sv419vdzrzfc9sp7zf105f1lgyiwyzi3cgyvcvg";
+    sha256 = "1zwr9g4h4jizbvm2d7fywdpcxmw8i1m85h8g72kizah07gk12aq1";
   };
 
   buildInputs = stdenv.lib.optionals doCheck [ pytest flake8 flaky ];
