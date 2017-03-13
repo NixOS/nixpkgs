89ea86c0757ec045dc2cb5699e1386d271788996
Vladimír Čunát <vcunat@gmail.com>
2017-03-13 10:34:57 +0100

    gnutls33: bugfix+security 3.3.26 -> 3.3.27
    http://gnutls.org/security.html#GNUTLS-SA-2017-3
    
    This is on 16.09 only, as we have 3.3 branch nowhere else anymore.

--- pkgs/development/libraries/gnutls/3.3.nix
+++ pkgs/development/libraries/gnutls/3.3.nix
@@ -1,10 +1,10 @@
 { callPackage, fetchurl, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "3.3.26";
+  version = "3.3.27";
 
   src = fetchurl {
     url = "ftp://ftp.gnutls.org/gcrypt/gnutls/v3.3/gnutls-${version}.tar.xz";
-    sha256 = "1n90qyz54hhnmf4fmap6zdyv7nihz6mrbqgxhd46h7aqdcmqhzba";
+    sha256 = "0zxiavxpy2k5f8ainwfbp11mvah23nlx2l8d04sc3xcf2mna3zcd";
   };
 })
