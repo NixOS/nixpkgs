bed935bd135215771a11e3b18ff658c5aa6ae521
Orivej Desh <orivej@gmx.fr>
2017-03-13 00:27:41 +0000

    qmetro: init at 0.7.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14998,6 +14998,8 @@ with pkgs;
 
   qjackctl = libsForQt5.callPackage ../applications/audio/qjackctl { };
 
+  qmetro = callPackage ../applications/misc/qmetro { };
+
   qmidinet = callPackage ../applications/audio/qmidinet { };
 
   qmidiroute = callPackage ../applications/audio/qmidiroute { };
