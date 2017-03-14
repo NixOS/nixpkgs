6a6d48be4e80a078e6969e9c565db32aec67dc62
Frederik Rietdijk <fridh@fridh.nl>
2017-03-14 17:07:09 +0100

    pythonPackages.dateparser: mark as broken

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5981,6 +5981,7 @@ in {
       description = "Date parsing library designed to parse dates from HTML pages";
       homepage = http://pypi.python.org/pypi/dateparser;
       license = licenses.bsd3;
+      broken = true;
     };
   };
 
