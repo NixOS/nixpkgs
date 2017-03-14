308c09d41f309e5055e45c66c07454581945d458
Bas van Dijk <v.dijk.bas@gmail.com>
2017-03-14 16:11:51 +0100

    wordpress: security upgrade: 4.7.2 -> 4.7.3 & other improvements (#23837)
    * Moved the wordpress sources derivation to the attribute pkgs.wordpress. This
      makes it easier to override.
    
    * Also introduce the `package` option for the wordpress virtual host config which
      defaults to pkgs.wordpress.
    
    * Also fixed the test in nixos/tests/wordpress.nix.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18122,6 +18122,8 @@ with pkgs;
 
   wmutils-opt = callPackage ../tools/X11/wmutils-opt { };
 
+  wordpress = callPackage ../servers/web-apps/wordpress { };
+
   wraith = callPackage ../applications/networking/irc/wraith { };
 
   wxmupen64plus = callPackage ../misc/emulators/wxmupen64plus { };
