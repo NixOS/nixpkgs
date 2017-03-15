286cc49e6aa36e40d8091d04c7ffac8673ee3e48
Joachim Fasting <joachifm@fastmail.fm>
2017-03-15 20:02:29 +0100

    linux_4_9: 4.9.14 -> 4.9.15
    (cherry picked from commit 12648a455bd9d7755b635f0732ce402590e1bd6f)

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.9.14";
+  version = "4.9.15";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0h5nnf9h00fsd7q66rapvwmfxs4pi0vjljk92ir5vid59hzk8kak";
+    sha256 = "14sfacaqkk9c66zrl8wkmy5331hczgz5j65yms6pnpwwi82sq3c2";
   };
 
   kernelPatches = args.kernelPatches;
