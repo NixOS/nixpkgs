168d9ec6f22f36c7bd87b6352eb8c4f01151992c
Frederik Rietdijk <fridh@fridh.nl>
2017-03-16 18:03:38 +0100

    Python: use setuptools shim instead of our own

--- pkgs/development/interpreters/python/build-python-package-setuptools.nix
+++ pkgs/development/interpreters/python/build-python-package-setuptools.nix
@@ -17,24 +17,16 @@
 , postShellHook ? ""
 , ... } @ attrs:
 
-let
-  # use setuptools shim (so that setuptools is imported before distutils)
-  # pip does the same thing: https://github.com/pypa/pip/pull/3265
-  setuppy = ./run_setup.py;
-
-in attrs // {
-  # we copy nix_run_setup.py over so it's executed relative to the root of the source
-  # many project make that assumption
+attrs // {
   buildPhase = attrs.buildPhase or ''
     runHook preBuild
-    cp ${setuppy} nix_run_setup.py
-    ${python.interpreter} nix_run_setup.py ${lib.optionalString (setupPyBuildFlags != []) ("build_ext " + (lib.concatStringsSep " " setupPyBuildFlags))} bdist_wheel
+    ${python.interpreter} -m setuptools.launch setup.py ${lib.optionalString (setupPyBuildFlags != []) ("build_ext " + (lib.concatStringsSep " " setupPyBuildFlags))} bdist_wheel
     runHook postBuild
   '';
 
   installCheckPhase = attrs.checkPhase or ''
     runHook preCheck
-    ${python.interpreter} nix_run_setup.py test
+    ${python.interpreter} -m setuptools.launch setup.py test
     runHook postCheck
   '';
 
