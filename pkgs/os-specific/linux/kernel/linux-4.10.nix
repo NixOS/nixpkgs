81ad24d4d7ff93d0382f25ff8d963f4627c557fc
Tim Steinbach <tim@nequissimus.com>
2017-03-17 17:19:59 -0400

    linux: 4.10.2 -> 4.10.3

--- pkgs/os-specific/linux/kernel/linux-4.10.nix
+++ pkgs/os-specific/linux/kernel/linux-4.10.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.10.2";
+  version = "4.10.3";
   extraMeta.branch = "4.10";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1m5ahr1m36kdni80xj4imhhw26l8621rsaaa3z4gkjmnq6n0bnxr";
+    sha256 = "1m18hlcimxj42lbg7mc9lh3hmmwpyk6y2c92xf0svmlfpzv6ah8r";
   };
 
   kernelPatches = args.kernelPatches;
