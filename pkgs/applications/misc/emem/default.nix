dbee6b6ef8c13e8eb0793b7f1616859d3683a894
Rommel M. Martinez <ebzzry@gmail.com>
2017-03-18 01:39:03 +0800

    emem: 0.2.41 -> 0.2.42

--- pkgs/applications/misc/emem/default.nix
+++ pkgs/applications/misc/emem/default.nix
@@ -2,17 +2,15 @@
 
 stdenv.mkDerivation rec {
   pname = "emem";
-  version = "0.2.41";
+  version = "0.2.42";
   name = "${pname}-${version}";
 
   inherit jdk;
 
   src = fetchurl {
     url = "https://github.com/ebzzry/${pname}/releases/download/v${version}/${pname}.jar";
-    sha256 = "0pg7m4fyrcbm8d5nj96xvvh1cvml501rw34hniqq384waifr2kqs";
+    sha256 = "0zm3sqgbiaj44nkgkjiifxwwyrcjs42nry6zmbg3dnnpd0v82v8j";
   };
-
-  buildInputs = [ ];
 
   phases = [ "buildPhase" "installPhase" ];
 
