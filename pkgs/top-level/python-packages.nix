7459dbb0991bb63a0c18a148d4f803d68118ff56
Frederik Rietdijk <fridh@fridh.nl>
2017-03-18 11:08:43 +0100

    pythonPackages.multipledispatch: disable tests

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15062,6 +15062,9 @@ in {
       sha256 = "bda6abb8188d9abb429bd17ed15bc7433f77f1b05a78cfff761711ed81daa7a2";
     };
 
+    # No tests in archive
+    doCheck = false;
+
     meta = {
       homepage = http://github.com/mrocklin/multipledispatch/;
       description = "A relatively sane approach to multiple dispatch in Python";
