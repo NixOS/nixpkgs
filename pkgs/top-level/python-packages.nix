8d6fc33bbcee92913f8d4e317e2098c83fa0b3ec
Frederik Rietdijk <fridh@fridh.nl>
2017-03-18 11:07:00 +0100

    pythonPackages.pretend: disable tests

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4950,6 +4950,9 @@ in {
       sha256 = "0r5r7ygz9m6d2bklflbl84cqhjkc2q12xgis8268ygjh30g2q3wk";
     };
 
+    # No tests in archive
+    doCheck = false;
+
     meta = {
       homepage = https://github.com/alex/pretend;
       license = licenses.bsd3;
