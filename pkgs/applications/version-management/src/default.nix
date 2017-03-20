6e587c29dc78bb7944a685cfafbe4b6663a3cf97
Victor Calvert <calvertvl@gmail.com>
2017-03-20 00:27:49 -0400

    src: wrap to properly resolve rcs at runtime

--- pkgs/applications/version-management/src/default.nix
+++ pkgs/applications/version-management/src/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, python, rcs, git }:
+{ stdenv, fetchurl, python, rcs, git, makeWrapper }:
 
 stdenv.mkDerivation rec {
   name = "src-${version}";
@@ -9,13 +9,18 @@ stdenv.mkDerivation rec {
     sha256 = "1m6rjbizx9win3jkciyx176sfy98r5arb1g3l6aqnqam9gpr44zm";
   };
 
-  buildInputs = [ python rcs git ];
+  buildInputs = [ python rcs git makeWrapper ];
 
   preConfigure = ''
     patchShebangs .
   '';
 
   makeFlags = [ "prefix=$(out)" ];
+
+  postInstall = ''
+    wrapProgram $out/bin/src \
+      --suffix PATH ":" "${rcs}/bin"
+  '';
 
   meta = with stdenv.lib; {
     description = "Simple single-file revision control";
