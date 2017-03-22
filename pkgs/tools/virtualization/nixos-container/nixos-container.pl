b1dc7fc253ae6f628842aecc72bbb36bd87ae217
Robin Gloster <mail@glob.in>
2017-03-22 15:11:19 +0100

    nixos-container: check for correct path for chattr
    Could fail on destroy if the container wasn't created correctly

--- pkgs/tools/virtualization/nixos-container/nixos-container.pl
+++ pkgs/tools/virtualization/nixos-container/nixos-container.pl
@@ -251,7 +251,7 @@ if ($action eq "destroy") {
 
     safeRemoveTree($profileDir) if -e $profileDir;
     safeRemoveTree($gcRootsDir) if -e $gcRootsDir;
-    system("chattr", "-i", "$root/var/empty") if -e $root;
+    system("chattr", "-i", "$root/var/empty") if -e "$root/var/empty";
     safeRemoveTree($root) if -e $root;
     unlink($confFile) or die;
 }
