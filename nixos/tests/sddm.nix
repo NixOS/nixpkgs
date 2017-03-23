ae1034ea965f9f86b6cbf1064071923650907618
Thomas Tuegel <ttuegel@mailbox.org>
2017-03-23 18:57:23 -0500

    nixos/sddm: improve test
    Disable SDDM auto-login to improve testing for SDDM themes. See #24246.

--- nixos/tests/sddm.nix
+++ nixos/tests/sddm.nix
@@ -1,26 +1,24 @@
 import ./make-test.nix ({ pkgs, ...} : {
   name = "sddm";
-  meta = with pkgs.stdenv.lib.maintainers; {
-    maintainers = [ ttuegel ];
-  };
 
   machine = { lib, ... }: {
     imports = [ ./common/user-account.nix ];
     services.xserver.enable = true;
-    services.xserver.displayManager.sddm = {
-      enable = true;
-      autoLogin = {
-        enable = true;
-        user = "alice";
-      };
-    };
+    services.xserver.displayManager.sddm.enable = true;
     services.xserver.windowManager.default = "icewm";
     services.xserver.windowManager.icewm.enable = true;
     services.xserver.desktopManager.default = "none";
   };
 
-  testScript = { nodes, ... }: ''
+  enableOCR = true;
+
+  testScript = { nodes, ... }: let
+    user = nodes.machine.config.users.extraUsers.alice;
+  in ''
     startAll;
+    $machine->waitForText(qr/ALICE/);
+    $machine->screenshot("sddm");
+    $machine->sendChars("${user.password}\n");
     $machine->waitForFile("/home/alice/.Xauthority");
     $machine->succeed("xauth merge ~alice/.Xauthority");
     $machine->waitForWindow("^IceWM ");
