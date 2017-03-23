16d2799af22320334332dd8049071d2a62a8711c
ndowens <ndowens04@gmail.com>
2017-03-23 18:04:36 -0500

    Changed fetchgit to fetchFromGitHub

--- pkgs/tools/package-management/cde/default.nix
+++ pkgs/tools/package-management/cde/default.nix
@@ -1,11 +1,14 @@
-{ stdenv, fetchgit }:
+{ stdenv, fetchFromGitHub }:
 
-stdenv.mkDerivation {
-  name = "cde-0.1";
-  src = fetchgit {
-    url = "https://github.com/pgbovine/CDE.git";
-    sha256 = "";
-    rev = "551e54d95eb3f8eefc698891f1b873fc4f02f360";
+stdenv.mkDerivation rec {
+  name = "cde-${version}";
+  version = "0.1";
+
+  src = fetchFromGitHub {
+    owner = "pgbovine";
+    repo = "CDE";
+    sha256 = "0raiz7pczkbnzxpg7g59v7gdp1ipkwgms2vh3431snw1va1gjzmk";
+    rev = "v${version}";
   };
 
   # The build is small, so there should be no problem
@@ -25,7 +28,7 @@ stdenv.mkDerivation {
   '';
 
   meta = with stdenv.lib; {
-    homepage = "https://github.com/pgbovine/CDE";
+    homepage = https://github.com/pgbovine/CDE;
     description = "A packaging tool for building portable packages";
     license = licenses.gpl3;
     maintainers = [ maintainers.rlupton20 ];
