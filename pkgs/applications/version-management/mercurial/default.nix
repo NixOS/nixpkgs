f23f4bf0ebc7008e5ba02a1fbb27dd80c77e714d
Mike Cooper <mythmon@gmail.com>
2017-03-23 10:24:32 -0700

    mercurial, tortoisehg: 3.9.2 -> 4.1.1
    (cherry picked from commit 50bd7a113db244ad8eb45dba38112b53e9ba20c3)

--- pkgs/applications/version-management/mercurial/default.nix
+++ pkgs/applications/version-management/mercurial/default.nix
@@ -4,7 +4,7 @@
 
 let
   # if you bump version, update pkgs.tortoisehg too or ping maintainer
-  version = "3.9.2";
+  version = "4.1.1";
   name = "mercurial-${version}";
   inherit (python2Packages) docutils hg-git dulwich python;
 in python2Packages.buildPythonApplication {
@@ -13,7 +13,7 @@ in python2Packages.buildPythonApplication {
 
   src = fetchurl {
     url = "https://mercurial-scm.org/release/${name}.tar.gz";
-    sha256 = "1kw3cpcjygfapvi5c123limhpbkmg7is2i81pybk1s05gi16l139";
+    sha256 = "17imsf4haqgw364p1z9i416jinmfxfia537b84hcg0rg43hinmv3";
   };
 
   inherit python; # pass it so that the same version can be used in hg2git
