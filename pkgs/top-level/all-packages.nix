21cbf20ad27eba5ab0de1c6ee14e9cdedc83b155
Thomas Tuegel <ttuegel@mailbox.org>
2017-03-24 12:52:20 -0500

    cool-retro-term: pin to Qt 5.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1314,7 +1314,7 @@ with pkgs;
 
   convoy = callPackage ../tools/filesystems/convoy { };
 
-  cool-retro-term = libsForQt55.callPackage ../applications/misc/cool-retro-term { };
+  cool-retro-term = libsForQt56.callPackage ../applications/misc/cool-retro-term { };
 
   coreutils = callPackage ../tools/misc/coreutils {
     aclSupport = stdenv.isLinux;
