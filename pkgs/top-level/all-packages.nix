b4e6351c781127f477f935085e8c50f1447180f7
Thomas Tuegel <ttuegel@mailbox.org>
2017-03-24 12:41:31 -0500

    Remove Qt 5.7
    The community support window for Qt 5.7 has ended. All packages should
    
    - update to Qt 5.8, or
    - pin to Qt 5.6 (the 3-year long-term support release), or
    - for proprietary software, use the vendored libraries.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9460,20 +9460,6 @@ with pkgs;
 
   libsForQt56 = recurseIntoAttrs (lib.makeScope qt56.newScope mkLibsForQt5);
 
-  qt57 = recurseIntoAttrs (import ../development/libraries/qt-5/5.7 {
-    inherit newScope;
-    inherit stdenv fetchurl makeSetupHook makeWrapper;
-    bison = bison2; # error: too few arguments to function 'int yylex(...
-    cups = if stdenv.isLinux then cups else null;
-    harfbuzz = harfbuzz-icu;
-    mesa = mesa_noglu;
-    inherit perl;
-    inherit (gst_all_1) gstreamer gst-plugins-base;
-    inherit (gnome3) gtk3 dconf;
-  });
-
-  libsForQt57 = recurseIntoAttrs (lib.makeScope qt57.newScope mkLibsForQt5);
-
   qt58 = recurseIntoAttrs (import ../development/libraries/qt-5/5.8 {
     inherit newScope;
     inherit stdenv fetchurl makeSetupHook makeWrapper;
