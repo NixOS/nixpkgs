8e9b6e7c441514243c19928c9258f75074d1c04f
Thomas Tuegel <ttuegel@mailbox.org>
2017-03-24 13:07:56 -0500

    goldendict: pin to Qt 5.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13397,7 +13397,7 @@ with pkgs;
 
   gnuradio-osmosdr = callPackage ../applications/misc/gnuradio-osmosdr { };
 
-  goldendict = libsForQt55.callPackage ../applications/misc/goldendict { };
+  goldendict = libsForQt56.callPackage ../applications/misc/goldendict { };
 
   inherit (ocamlPackages) google-drive-ocamlfuse;
 
