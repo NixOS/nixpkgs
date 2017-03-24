317734f99cc095c0158e071ccb3052fa91407723
Peter Hoeg <peter@hoeg.com>
2017-03-24 18:44:45 +0800

    speedcrunch: stick to qt 5.7 as 5.8 is broken

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17613,7 +17613,7 @@ with pkgs;
 
   yacas = callPackage ../applications/science/math/yacas { };
 
-  speedcrunch = libsForQt5.callPackage ../applications/science/math/speedcrunch { };
+  speedcrunch = libsForQt57.callPackage ../applications/science/math/speedcrunch { };
 
   ### SCIENCE / MISC
 
