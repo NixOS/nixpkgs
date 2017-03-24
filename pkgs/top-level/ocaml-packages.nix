d69e9327806496090cb658e377f76b6fdadaaa9c
Vincent Laporte <Vincent.Laporte@gmail.com>
2017-03-24 06:36:41 +0000

    ocamlPackages.ocaml-compiler-libs: init at 0.9.0
    This packages exposes the OCaml compiler libraries repackages
    under the toplevel names Ocaml_common, Ocaml_bytecomp, ...
    
    Homepage: https://github.com/janestreet/ocaml-compiler-libs

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -571,6 +571,8 @@ let
 
     buildOcamlJane = callPackage ../development/ocaml-modules/janestreet/buildOcamlJane.nix {};
 
+    ocaml-compiler-libs = callPackage ../development/ocaml-modules/janestreet/ocaml-compiler-libs.nix {};
+
     ppx_core =
       if lib.versionOlder "4.03" ocaml.version
       then callPackage ../development/ocaml-modules/janestreet/ppx_core-113_33_01.nix {}
