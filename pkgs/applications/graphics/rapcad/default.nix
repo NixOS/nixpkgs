e6dc95697a3cf633e6d700e19470b19e61150715
Thomas Tuegel <ttuegel@mailbox.org>
2017-03-25 09:23:52 -0500

    rapcad: pin to Qt 5.6

--- pkgs/applications/graphics/rapcad/default.nix
+++ pkgs/applications/graphics/rapcad/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, cgal, boost, gmp, mpfr, flex, bison, dxflib, readline
+{ stdenv, fetchFromGitHub, fetchurl, cgal, boost, gmp, mpfr, flex, bison, dxflib, readline
 , qtbase, qmakeHook, mesa_glu
 }:
 
@@ -13,6 +13,14 @@ stdenv.mkDerivation rec {
     sha256 = "0a0sqf6h227zalh0jrz6jpm8iwji7q3i31plqk76i4qm9vsgrhir";
   };
 
+  patches = [
+    (fetchurl {
+      url = "https://github.com/GilesBathgate/RapCAD/commit/278a8d6c7b8fe08f867002528bbab4a6319a7bb6.patch";
+      sha256 = "1vvkyf0wg79zdzs5zlggfrr1lrp1x75dglzl0mspnycwldsdwznj";
+      name = "disable-QVector-qHash.patch";
+    })
+  ];
+
   nativeBuildInputs = [ qmakeHook ];
   buildInputs = [ qtbase cgal boost gmp mpfr flex bison dxflib readline mesa_glu ];
 
@@ -21,6 +29,5 @@ stdenv.mkDerivation rec {
     maintainers = [ maintainers.raskin ];
     platforms = platforms.linux;
     description = ''Constructive solid geometry package'';
-    broken = true; # redefines template instance added in Qt 5.6
   };
 }
