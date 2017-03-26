244c9a9712ced1dd1b582a3e6d7a252d15cdd1d5
Tim Steinbach <tim@nequissimus.com>
2017-03-26 10:05:22 -0400

    linux: 4.10.5 -> 4.10.6
    (cherry picked from commit c0411ea2290f51f8827f0d4413b1605494e8e461)

--- pkgs/os-specific/linux/kernel/linux-4.10.nix
+++ pkgs/os-specific/linux/kernel/linux-4.10.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.10.5";
+  version = "4.10.6";
   extraMeta.branch = "4.10";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "04gwdqsngzddxvw34lcy9r03179l6s25qbxmsv5jz5kbczivzg6x";
+    sha256 = "1vjdd0i4k6m8nx5npwz5p3czg61j93wchw5amggrqfaarwy2j6j5";
   };
 
   kernelPatches = args.kernelPatches;
