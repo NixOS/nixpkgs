9f86136cefbd3e050b96a307346278fe9ad8a5bf
Robin Gloster <mail@glob.in>
2017-03-30 16:03:14 +0200

    rustc: don't build on i686

--- pkgs/development/compilers/rust/rustc.nix
+++ pkgs/development/compilers/rust/rustc.nix
@@ -153,6 +153,6 @@ stdenv.mkDerivation {
     description = "A safe, concurrent, practical language";
     maintainers = with maintainers; [ madjar cstrahan wizeman globin havvy wkennington retrry ];
     license = [ licenses.mit licenses.asl20 ];
-    platforms = platforms.linux ++ platforms.darwin;
+    platforms = subtractLists platforms.i686 (platforms.linux ++ platforms.darwin);
   };
 }
