1e295260e2101ef33e475df28c3dcac3c3e6c950
Maximilian Bosch <maximilian@mbosch.me>
2017-03-30 21:55:05 +0200

    geogebra: 5-0-346-0 -> 5-0-350-0

--- pkgs/applications/science/math/geogebra/default.nix
+++ pkgs/applications/science/math/geogebra/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "geogebra-${version}";
-  version = "5-0-346-0";
+  version = "5-0-350-0";
 
   preferLocalBuild = true;
 
   src = fetchurl {
     url = "http://download.geogebra.org/installers/5.0/GeoGebra-Linux-Portable-${version}.tar.bz2";
-    sha256 = "13yiksdk5qwa7c5zp5l0h3czqq31qlf04ncgrh8mn7z70ng1f3l4";
+    sha256 = "0lr2calglad5d17p5sl1jbjdfsdsmn4dxgy8s89lyh0d4aihy54d";
   };
 
   srcIcon = fetchurl {
