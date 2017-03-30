74c14f8b28a690d3ba281e9008d584ad6e0ea72d
Maximilian Bosch <maximilian@mbosch.me>
2017-03-30 21:04:01 +0200

    lambda-mod-zsh-theme: remove unnecessary `rec` declaration

--- pkgs/shells/lambda-mod-zsh-theme/default.nix
+++ pkgs/shells/lambda-mod-zsh-theme/default.nix
@@ -3,7 +3,7 @@
 let
   pkgName = "lambda-mod-zsh-theme";
   rev = "eceee68cf46bba9f7f42887c2128b48e8861e31b";
-in stdenv.mkDerivation rec {
+in stdenv.mkDerivation {
   name = "${pkgName}-${rev}";
 
   src = fetchgit {
