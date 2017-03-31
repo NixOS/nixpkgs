cb791371c542ff2018916b84dccd43c7c7afd302
Tim Steinbach <tim@nequissimus.com>
2017-03-31 09:19:07 -0400

    linux: 4.4.58 -> 4.4.59

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.4.58";
+  version = "4.4.59";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1kdgd3pbpscpn5fd34m0djw4h69wcab5qnrji63dal2q74fn1pn2";
+    sha256 = "1jlnm6msxdhm7l6all91rd1rkqsqpd0rblbdl68z7nkz9hy55sb4";
   };
 
   kernelPatches = args.kernelPatches;
