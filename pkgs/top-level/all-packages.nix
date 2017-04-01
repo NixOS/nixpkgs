d56d0ab956e70baf7e35514189befbf3e1785d7a
Vasiliy Solovey <miltador@yandex.ua>
2017-04-01 10:21:19 +0300

    idea: rename to jetbrains JetBrains IDEs were put under wrong scope which was confusing. Fixes #24453
    (cherry picked from commit bab0ed75c0207fde57ca80bcf0983ba2e32e1271)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13628,7 +13628,7 @@ with pkgs;
     libart = pkgs.gnome2.libart_lgpl;
   };
 
-  idea = recurseIntoAttrs (callPackages ../applications/editors/idea { androidsdk = androidsdk_extras; });
+  jetbrains = recurseIntoAttrs (callPackages ../applications/editors/jetbrains { androidsdk = androidsdk_extras; });
 
   libquvi = callPackage ../applications/video/quvi/library.nix { };
 
