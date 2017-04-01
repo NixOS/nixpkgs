63402a188837f1a5317afc724ad710d12472f001
Jörg Thalheim <joerg@thalheim.io>
2017-04-01 17:06:19 +0200

    alacritty: init at 2017-07-08
    fixes #21801

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -412,6 +412,10 @@ with pkgs;
 
   albert = libsForQt5.callPackage ../applications/misc/albert {};
 
+  alacritty = callPackage ../applications/misc/alacritty {
+    inherit (xorg) libXcursor libXxf86vm libXi;
+  };
+
   amazon-glacier-cmd-interface = callPackage ../tools/backup/amazon-glacier-cmd-interface { };
 
   ammonite = callPackage ../development/tools/ammonite {};
