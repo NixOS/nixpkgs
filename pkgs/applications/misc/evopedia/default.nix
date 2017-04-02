b320e310d31996134d8b3656f75eba466018c0ae
ndowens <ndowens04@gmail.com>
2017-04-02 12:41:43 -0500

    evopedia: Cosmetic

--- pkgs/applications/misc/evopedia/default.nix
+++ pkgs/applications/misc/evopedia/default.nix
@@ -1,23 +1,24 @@
-{ stdenv, fetchgit, bzip2, qt4, qmake4Hook, libX11 }:
+{ stdenv, fetchFromGitHub, bzip2, qt4, qmake4Hook, libX11 }:
 
 stdenv.mkDerivation rec {
   name = "evopedia-${version}";
   version = "0.4.4";
 
-  src = fetchgit {
-    url = https://github.com/evopedia/evopedia_qt;
-    rev = "refs/tags/v${version}";
+  src = fetchFromGitHub {
+    owner = "evopedia";
+    repo = "evopedia_qt";
+    rev = "v${version}";
     sha256 = "0snp5qiywj306kfaywvkl7j34fivgxcb8dids1lzmbqq5xcpqqvc";
   };
 
   buildInputs = [ bzip2 qt4 libX11 ];
   nativeBuildInputs = [ qmake4Hook ];
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Offline Wikipedia Viewer";
     homepage = http://www.evopedia.info;
-    license = stdenv.lib.licenses.gpl3Plus;
-    maintainers = with stdenv.lib.maintainers; [ qknight ];
-    platforms = with stdenv.lib.platforms; linux;
+    license = licenses.gpl3Plus;
+    maintainers = [ maintainers.qknight ];
+    platforms = platforms.linux;
   };
 }
