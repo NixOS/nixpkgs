53a516b198e1c7815176e0b016e749623ecfcb64
Victor Calvert <calvertvl@gmail.com>
2017-04-02 22:33:23 -0400

    src: 1.12 -> 1.13

--- pkgs/applications/version-management/src/default.nix
+++ pkgs/applications/version-management/src/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "src-${version}";
-  version = "1.12";
+  version = "1.13";
 
   src = fetchurl {
     url = "http://www.catb.org/~esr/src/${name}.tar.gz";
-    sha256 = "1m6rjbizx9win3jkciyx176sfy98r5arb1g3l6aqnqam9gpr44zm";
+    sha256 = "0l13ld8nxm1c720ns22lyx3q1bq2c2zn78vi5w92b7nl6p2nncy8";
   };
 
   buildInputs = [ python rcs git makeWrapper ];
