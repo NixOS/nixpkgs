076ade877ab3fa01226622dafd3a01847921a6d0
AndersonTorres <torres.anderson.85@gmail.com>
2017-04-03 02:39:19 -0300

    avahi-tqt (tde): 14.0.3 -> 14.0.4

--- pkgs/desktops/tde/R14/dependencies/avahi-tqt/default.nix
+++ pkgs/desktops/tde/R14/dependencies/avahi-tqt/default.nix
@@ -1,16 +1,20 @@
 { stdenv, fetchurl, pkgconfig, cmake
 , nssmdns, avahi, tde }:
 
-stdenv.mkDerivation rec{
+let baseName = "avahi-tqt"; in
+with stdenv.lib;
+stdenv.mkDerivation rec {
 
-  name = "avahi-tqt-${version}";
-  version = "${majorVer}.${minorVer}";
-  majorVer = "R14";
-  minorVer = "0.3";
+  name = "${baseName}-${version}";
+  srcName = "${baseName}-R${version}";
+  version = "${majorVer}.${minorVer}.${patchVer}";
+  majorVer = "14";
+  minorVer = "0";
+  patchVer = "4";
 
   src = fetchurl {
-    url = "mirror://tde/${version}/dependencies/${name}.tar.bz2";
-    sha256 = "1g56mcjg6pqhlljxj6m1b0iv7jg7s22fqjl36r0i7bkgdxz8lbwm";
+    url = "mirror://tde/R${version}/dependencies/${srcName}.tar.bz2";
+    sha256 = "179cd6c85v7h7m7dz0rm4n4jmqrqsi2n1ylgaaqps3gr8fm0w2z6";
   };
 
   nativeBuildInputs = [ pkgconfig cmake ];
@@ -18,7 +22,7 @@ stdenv.mkDerivation rec{
   buildInputs = [ nssmdns avahi tde.tqtinterface ];
 
   preConfigure = ''
-    cd avahi-tqt
+    cd ${baseName}
   '';
 
   meta = with stdenv.lib;{
