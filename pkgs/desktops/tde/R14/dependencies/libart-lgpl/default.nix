3802f05a91d28b72caeba2c1d4da751deb148d2c
AndersonTorres <torres.anderson.85@gmail.com>
2017-04-03 02:39:19 -0300

    libart-lgpl (tde): 14.0.3 -> 14.0.4

--- pkgs/desktops/tde/R14/dependencies/libart-lgpl/default.nix
+++ pkgs/desktops/tde/R14/dependencies/libart-lgpl/default.nix
@@ -1,26 +1,29 @@
 { stdenv, fetchurl, pkgconfig }:
 
+let baseName = "libart-lgpl"; in
 with stdenv.lib;
 stdenv.mkDerivation rec {
 
-  name = "libart-lgpl-${version}";
-  version = "${majorVer}.${minorVer}";
-  majorVer = "R14.0";
-  minorVer = "3";
+  name = "${baseName}-${version}";
+  srcName = "${baseName}-R${version}";
+  version = "${majorVer}.${minorVer}.${patchVer}";
+  majorVer = "14";
+  minorVer = "0";
+  patchVer = "4";
 
   src = fetchurl {
-    url = "mirror://tde/${version}/dependencies/${name}.tar.bz2";
-    sha256 = "0gnzdml05ggc95cv069in72glagmn98bs10lyhmvh8k1snxqdj6h";
+    url = "mirror://tde/R${version}/dependencies/${srcName}.tar.bz2";
+    sha256 = "0hz7zr8g5sc06f5ayz91rk4144sdn4m36vrpzv6gl05nq37q27d0";
   };
 
-  buildInputs = [ pkgconfig ];
+  nativeBuildInputs = [ pkgconfig ];
 
   configureFlags = ''
     --enable-ansi
   '';
 
   preConfigure = ''
-    cd libart-lgpl
+    cd ${baseName}
   '';
 
   meta = with stdenv.lib;{
