d86caa3216dce17f54c1505a751cf5b403fd8bcf
John Ericson <Ericson2314@Yahoo.com>
2017-04-03 16:53:22 -0400

    lib: Fix preexisting bugs in old platforms code (now bugs in tests)
    Warning, this changes the compatibility claims of existing packages!

--- lib/tests/systems.nix
+++ lib/tests/systems.nix
@@ -15,9 +15,9 @@ let mseteq = x: y: lib.sort lib.lessThan x == lib.sort lib.lessThan y; in
   all = assertTrue (mseteq all (linux ++ darwin ++ cygwin ++ freebsd ++ openbsd ++ netbsd ++ illumos));
 
   arm = assertTrue (mseteq arm [ "armv5tel-linux" "armv6l-linux" "armv7l-linux" ]);
-  i686 = assertTrue (mseteq i686 [ "i686-linux" "i686-freebsd" "i686-netbsd" "i686-cygwin" ]);
+  i686 = assertTrue (mseteq i686 [ "i686-linux" "i686-freebsd" "i686-netbsd" "i686-openbsd" "i686-cygwin" ]);
   mips = assertTrue (mseteq mips [ "mips64el-linux" ]);
-  x86_64 = assertTrue (mseteq x86_64 [ "x86_64-linux" "x86_64-darwin" "x86_64-freebsd" "x86_64-openbsd" "x86_64-netbsd" "x86_64-cygwin" ]);
+  x86_64 = assertTrue (mseteq x86_64 [ "x86_64-linux" "x86_64-darwin" "x86_64-freebsd" "x86_64-openbsd" "x86_64-netbsd" "x86_64-cygwin" "x86_64-solaris" ]);
 
   cygwin = assertTrue (mseteq cygwin [ "i686-cygwin" "x86_64-cygwin" ]);
   darwin = assertTrue (mseteq darwin [ "x86_64-darwin" ]);
