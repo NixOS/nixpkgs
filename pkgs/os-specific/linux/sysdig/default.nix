699ea78866245b1dd9fcd33cfe01a7811f5f536c
Jörg Thalheim <joerg@thalheim.io>
2017-04-05 23:22:26 +0200

    Revert "sysdig: 1.15.0 -> 1.15.1"
    This reverts commit bb1e6557da293d6a7c9f868cb850afdbe2ba09d6.
    
    segfaults, reason unknown

--- pkgs/os-specific/linux/sysdig/default.nix
+++ pkgs/os-specific/linux/sysdig/default.nix
@@ -3,7 +3,7 @@
 let
   inherit (stdenv.lib) optional optionalString;
   baseName = "sysdig";
-  version = "0.15.1";
+  version = "0.15.0";
 in
 stdenv.mkDerivation rec {
   name = "${baseName}-${version}";
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     name = "${name}.tar.gz";
     url = "https://github.com/draios/sysdig/archive/${version}.tar.gz";
-    sha256 = "0x1ssgrw8n563fk4vb9f2f27yjgv2c05xmpkc8p78l4hv8alwh2b";
+    sha256 = "08spprzgx6ksd7sjp5nk7z5szdlixh2sb0bsb9mfaq4xr12gsjw2";
   };
 
   buildInputs = [
