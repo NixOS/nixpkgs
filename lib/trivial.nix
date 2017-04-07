2d02be089a4a542e03093918213b22a8ebda6c86
Shea Levy <shea@shealevy.com>
2017-04-07 14:03:40 -0400

    lib: Add composeExtensions for composing extension functions
    (cherry picked from commit 0a15af2ed170b30636a264b6b535a9327effddc5)

--- lib/trivial.nix
+++ lib/trivial.nix
@@ -53,6 +53,15 @@ rec {
   # argument, but it's nice this way if several uses of `extends` are cascaded.
   extends = f: rattrs: self: let super = rattrs self; in super // f self super;
 
+  # Compose two extending functions of the type expected by 'extends'
+  # into one where changes made in the first are available in the
+  # 'super' of the second
+  composeExtensions =
+    f: g: self: super:
+      let fApplied = f self super;
+          super' = super // fApplied;
+      in fApplied // g self super';
+
   # Create an overridable, recursive attribute set. For example:
   #
   #     nix-repl> obj = makeExtensible (self: { })
