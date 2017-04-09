f68de22683b8f08f9fe5ba3081de730c6ffeee8a
Nikolay Amiantov <ab@fmap.me>
2017-04-10 02:33:03 +0300

    wrapGAppsHook: add librvsg as a dependency
    User themes may use SVG icons which won't work if the app can't access this
    library. This is quite sure to happen (e.g. Adwaita's icons are vector).

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -325,7 +325,7 @@ with pkgs;
   findXMLCatalogs = makeSetupHook { } ../build-support/setup-hooks/find-xml-catalogs.sh;
 
   wrapGAppsHook = makeSetupHook {
-    deps = [ gnome3.dconf.lib gnome3.gtk makeWrapper ];
+    deps = [ gnome3.dconf.lib gnome3.gtk librsvg makeWrapper ];
   } ../build-support/setup-hooks/wrap-gapps-hook.sh;
 
   separateDebugInfo = makeSetupHook { } ../build-support/setup-hooks/separate-debug-info.sh;
