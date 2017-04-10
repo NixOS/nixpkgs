08abe4fe936f5700900e9e009ead7d703ea76d5f
Michael Raskin <7c6f434c@mail.ru>
2017-04-10 09:57:07 +0200

    lispPackage.iolib: list the hidden systems to make sure bundles exist

--- pkgs/development/lisp-modules/quicklisp-to-nix-overrides.nix
+++ pkgs/development/lisp-modules/quicklisp-to-nix-overrides.nix
@@ -2,7 +2,7 @@
 let
   addDeps = newdeps: x: {deps = x.deps ++ newdeps;};
   addNativeLibs = libs: x: { propagatedBuildInputs = libs; };
-  skipBuildPhase = x: { 
+  skipBuildPhase = x: {
     overrides = y: ((x.overrides y) // { buildPhase = "true"; });
   };
   qlnp = quicklisp-to-nix-packages;
@@ -33,8 +33,13 @@ in
   };
   hunchentoot = addNativeLibs [pkgs.openssl];
   iolib = x: {
-    propagatedBuildInputs = (x.propagatedBuildInputs or []) ++ [pkgs.libfixposix pkgs.gcc];
-    testSystems = (x.testSystems or ["iolib"]) ++ ["iolib/os" "iolib/zstreams"];
+    propagatedBuildInputs = (x.propagatedBuildInputs or []) ++
+     [pkgs.libfixposix pkgs.gcc];
+    testSystems = (x.testSystems or ["iolib"]) ++ [
+      "iolib/os" "iolib/zstreams" "iolib/common-lisp" "iolib/base" "iolib/asdf"
+      "iolib/conf" "iolib/grovel" "iolib/syscalls" "iolib/sockets"
+      "iolib/multiplex" "iolib/streams"
+    ];
   };
   cl-unicode = addDeps (with qlnp; [cl-ppcre flexi-streams]);
   clack =  addDeps (with qlnp;[lack bordeaux-threads prove]);
@@ -51,7 +56,7 @@ in
   wookie = multiOverride [(addDeps (with qlnp; [
       alexandria blackbird cl-async chunga fast-http quri babel cl-ppcre
       cl-fad fast-io vom do-urlencode cl-async-ssl
-    ])) 
+    ]))
     (addNativeLibs (with pkgs; [libuv openssl]))];
   woo = addDeps (with qlnp; [
     cffi lev clack swap-bytes static-vectors fast-http proc-parse quri fast-io
