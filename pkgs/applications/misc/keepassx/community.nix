4a4719c6ec37cf8f36d52c992f247b6e05734fe5
Jon Banafato <jon@jonafato.com>
2017-04-10 20:54:23 -0400

    keepassx-community: 2.1.3 -> 2.1.4
    KeePassXC has a new bugfix release available: https://github.com/keepassxreboot/keepassxc/releases/tag/2.1.4

--- pkgs/applications/misc/keepassx/community.nix
+++ pkgs/applications/misc/keepassx/community.nix
@@ -7,13 +7,13 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "keepassx-community-${version}";
-  version = "2.1.3";
+  version = "2.1.4";
 
   src = fetchFromGitHub {
     owner = "keepassxreboot";
     repo = "keepassxc";
     rev = "${version}";
-    sha256 = "1zamk3dc44fn61b880i3l1r0np2sx2hs05cvcf2x4748r3xicacf";
+    sha256 = "1znnw2xpv58x0rbpmm4y662377mbmcilhf8mhhjsz8vhahms33a8";
   };
 
   cmakeFlags = optional (withKeePassHTTP) [ "-DWITH_XC_HTTP=ON" ];
