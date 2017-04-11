b002d6a8681e4961d9f5c3ee8ca1b06d1a31c450
Tuomas Tynkkynen <tuomas@tuxera.com>
2017-04-11 09:45:50 +0300

    Revert "btrfs-progs: 4.8.2 -> 4.10.2"
    This reverts commit c0cef0425eff1db8ddc07e78ea5aa16eb4a9666b.
    
    The output of the command-line tool has changed somewhat and at least
    nixos-generate-config.pl needs modifications to match. I'm leaving that
    to someone who knows a bit more about btrfs.
    
    Issue #24810.

--- pkgs/tools/filesystems/btrfs-progs/default.nix
+++ pkgs/tools/filesystems/btrfs-progs/default.nix
@@ -2,14 +2,14 @@
 , asciidoc, xmlto, docbook_xml_dtd_45, docbook_xsl, libxslt
 }:
 
-let version = "4.10.2"; in
+let version = "4.8.2"; in
 
 stdenv.mkDerivation rec {
   name = "btrfs-progs-${version}";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v${version}.tar.xz";
-    sha256 = "02p63nz78lrr156cmbb759z76cn95hv6mmz7v592lmiq0dkxy2gd";
+    sha256 = "0pswcfmdnfc586770h74abp67gn2xv8fd46vxlimnmn837sj7h41";
   };
 
   buildInputs = [
