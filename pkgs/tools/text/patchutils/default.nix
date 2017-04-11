1e1b68b4a02f7e671b8e56d841b4c0962e5d6be9
Tuomas Tynkkynen <tuomas@tuxera.com>
2017-04-11 23:13:28 +0300

    patchutils: Drop patch applied upstream

--- pkgs/tools/text/patchutils/default.nix
+++ pkgs/tools/text/patchutils/default.nix
@@ -8,8 +8,6 @@ stdenv.mkDerivation rec {
     sha256 = "0xp8mcfyi5nmb5a2zi5ibmyshxkb1zv1dgmnyn413m7ahgdx8mfg";
   };
 
-  patches = [ ./drop-comments.patch ]; # we would get into a cycle when using fetchpatch on this one
-
   hardeningDisable = [ "format" ];
 
   meta = with stdenv.lib; {
