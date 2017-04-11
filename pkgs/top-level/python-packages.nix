NixOS/nixpkgs

commit 8d18f67a9705617953d43c5a06249f94e2423fbb

awscli: 1.11.45 -> 1.11.75
also update dependency botocore

pkgs/top-level/python-packages.nix
@@ -1625,11 +1625,11 @@ in {
 
   awscli = buildPythonPackage rec {
     name = "awscli-${version}";
-    version = "1.11.45";
+    version = "1.11.75";
       namePrefix = "";
       src = pkgs.fetchurl {
       url = "mirror://pypi/a/awscli/${name}.tar.gz";
-      sha256 = "0sv9dw4zsra8fm7ddbnwhlh80w534z4h8llz2p8asssaaj5nq2ya";
+      sha256 = "0bkjyrgb78f29vvr8j2id0386d30w340wrl7krwiha725c9y3pz1";
     };
 
     # No tests included
@@ -3200,11 +3200,11 @@ in {
   };
 
   botocore = buildPythonPackage rec {
-    version = "1.5.8"; # This version is required by awscli
+    version = "1.5.38"; # This version is required by awscli
     name = "botocore-${version}";
     src = pkgs.fetchurl {
       url = "mirror://pypi/b/botocore/${name}.tar.gz";
-      sha256 = "1qhrq2l9kvhi3gnrgwqhvy42aqbsk93j8mfr4ixqx18yqgbnylvz";
+      sha256 = "04cvsi8g8p3r5vr3vr2nb5ldyrsm0y4c2phimabbpk33wv718qyx";
     };
 
     propagatedBuildInputs =

commit 6047401a71b736bd8f203127bb05ea5592237b82

pythonPackages.boto3: disable tests (fails due to network access)
The test failure started happening with commit
8d18f67a9705617953d43c5a0624 ("awscli: 1.11.45 -> 1.11.75"). That commit
also updates botocore, a dependency of boto3.

This unbreaks 'nixops' (a dependee).

pkgs/top-level/python-packages.nix
@@ -3187,6 +3187,9 @@ in {
       runHook postCheck
     '';
 
+    # Network access
+    doCheck = false;
+
     meta = {
       homepage = https://github.com/boto/boto3;
       license = stdenv.lib.licenses.asl20;