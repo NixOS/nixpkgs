62b1bd180f91ec90e719b9116e73f65e259c27e7
zraexy <zraexy@gmail.com>
2017-04-13 12:08:05 -0800

    pythonPackages.iso-639: init at 0.4.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13273,6 +13273,8 @@ in {
       license = with licenses; [ lgpl21 ];
     };
   };
+  
+  iso-639 = callPackage ../development/python-modules/iso-639 {};
 
   iso8601 = buildPythonPackage rec {
     name = "iso8601-${version}";
