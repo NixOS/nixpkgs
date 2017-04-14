a7ebf59aa7e146791d4a40e5d5e55a721fc8130c
Tuomas Tynkkynen <tuomas@tuxera.com>
2017-04-14 21:41:15 +0300

    stdenv: ARM bootstrap: Update bootstrap tarballs to latest hydra-built ones
    Fixes sandboxed build of glibc. Discussion about a similar failure on aarch64 at:
    https://github.com/NixOS/nixpkgs/commit/8bfa9f528c88243578246798c7c2ddde045e0de0.
    
    Picked from the following cross-trunk evaluation:
    http://hydra.nixos.org/eval/1349278 based on nixpkgs
    commit 1f32d4b4eb50a78613cc9a3866c4d50d03fbe2b0.
    
    armv5tel job: http://hydra.nixos.org/build/51569718
    armv6l job: http://hydra.nixos.org/build/51569717
    armv7l job: http://hydra.nixos.org/build/51569713
    
    (cherry picked from commit 28f87e414186e2f16a094ac354dfe079965e8fe5)

--- pkgs/stdenv/linux/bootstrap-files/armv5tel.nix
+++ pkgs/stdenv/linux/bootstrap-files/armv5tel.nix
@@ -1,12 +1,12 @@
 {
   busybox = import <nix/fetchurl.nix> {
-    url = http://nixos-arm.dezgeg.me/bootstrap-2016-07-20-33a1d8/armv5tel/busybox;
-    sha256 = "03i90dwkly1j2a7i12qixkybjz2b24ixjrl7zsr17s1sv6m27zba";
+    url = http://nixos-arm.dezgeg.me/bootstrap-2017-04-13-1f32d4b4/armv5tel/busybox;
+    sha256 = "00mxas5xg2j9n1g0q0nklr0dy87qqxk0jja5qz1yi7xl7zzsnpnw";
     executable = true;
   };
 
   bootstrapTools = import <nix/fetchurl.nix> {
-    url = http://nixos-arm.dezgeg.me/bootstrap-2016-07-20-33a1d8/armv5tel/bootstrap-tools.tar.xz;
-    sha256 = "1sikiydjlbv8v35fgjvr5swgvj6bc83gmrbjrhpi0hyzyfcinxbn";
+    url = http://nixos-arm.dezgeg.me/bootstrap-2017-04-13-1f32d4b4/armv5tel/bootstrap-tools.tar.xz;
+    sha256 = "0fhiy9l3mbmlhpkby31c2s63bhjiqx25qqr3wdp8cb7fxz8ayx2f";
   };
 }
