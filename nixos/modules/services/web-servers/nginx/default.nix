ee38556ef9e41a93ab0bb22865aefba5da7cd98c
Robin Gloster <mail@glob.in>
2017-04-15 17:53:46 +0200

    nginx module: allow basic configuration of upstreams

--- nixos/modules/services/web-servers/nginx/default.nix
+++ nixos/modules/services/web-servers/nginx/default.nix
@@ -29,6 +29,14 @@ let
     proxy_set_header        Accept-Encoding "";
   '';
 
+  upstreamConfig = toString (flip mapAttrsToList cfg.upstreams (name: upstream: ''
+    upstream ${name} {
+      ${toString (flip mapAttrsToList upstream.servers (name: server: ''
+        server ${name} ${optionalString server.resolve "resolve"} ${optionalString server.backup "backup"};
+      ''))}
+    }
+  ''));
+
   configFile = pkgs.writeText "nginx.conf" ''
     user ${cfg.user} ${cfg.group};
     error_log stderr;
@@ -50,6 +58,7 @@ let
       ${optionalString (cfg.resolver.addresses != []) ''
         resolver ${toString cfg.resolver.addresses} ${optionalString (cfg.resolver.valid != "") "valid=${cfg.resolver.valid}"};
       ''}
+      ${upstreamConfig}
 
       ${optionalString (cfg.recommendedOptimisation) ''
         # optimisation
@@ -418,6 +427,44 @@ in
         };
         description = ''
           Configures name servers used to resolve names of upstream servers into addresses
+        '';
+        default = {};
+      };
+
+      upstreams = mkOption {
+        type = types.attrsOf (types.submodule {
+          options = {
+            servers = mkOption {
+              type = types.attrsOf (types.submodule {
+                options = {
+                  backup = mkOption {
+                    type = types.bool;
+                    default = false;
+                    description = ''
+                      Marks the server as a backup server. It will be passed
+                      requests when the primary servers are unavailable.
+                    '';
+                  };
+                  resolve = mkOption {
+                    type = types.bool;
+                    default = false;
+                    description = ''
+                      Monitors changes of the IP addresses that correspond to a
+                      domain name of the server, and automatically modifies the
+                      upstream configuration without the need of restarting nginx.
+                    '';
+                  };
+                };
+              });
+              description = ''
+                Defines the address and other parameters of the upstream servers.
+              '';
+              default = {};
+            };
+          };
+        });
+        description = ''
+          Defines a group of servers to use as proxy target.
         '';
         default = {};
       };
