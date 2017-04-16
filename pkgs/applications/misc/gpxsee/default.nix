2ec6e82b7fffe354c127f81d7d74e9f8b1dc0c6f
Gabriel Ebner <gebner@gebner.org>
2017-04-16 09:13:32 +0200

    gpxsee: 2.17 -> 4.3

--- pkgs/applications/misc/gpxsee/default.nix
+++ pkgs/applications/misc/gpxsee/default.nix
@@ -1,27 +1,27 @@
-{ stdenv, fetchFromGitHub, qmakeHook }:
+{ stdenv, fetchFromGitHub, qmakeHook, qttools }:
 
 stdenv.mkDerivation rec {
   name = "gpxsee-${version}";
-  version = "2.17";
+  version = "4.3";
 
   src = fetchFromGitHub {
     owner = "tumic0";
     repo = "GPXSee";
     rev = version;
-    sha256 = "1422kgj972ydasqqm0k02qf3v2py7if2ibri7yjg8awqilacy6by";
+    sha256 = "15f686frxlrmdvh5cc837kx62g0ihqj4vb87i8433g7l5vqkv3lf";
   };
 
-  nativeBuildInputs = [ qmakeHook ];
-  
-  patchPhase = ''
-    sed -i '/lang\/gpxsee_cs.qm/d' gpxsee.qrc
+  nativeBuildInputs = [ qmakeHook qttools ];
+
+  preConfigure = ''
+    lrelease lang/*.ts
   '';
 
   preFixup = ''
     mkdir -p $out/bin
     cp GPXSee $out/bin
   '';
-  
+
   meta = with stdenv.lib; {
     homepage = http://tumic.wz.cz/gpxsee;
     description = "GPX viewer and analyzer";
