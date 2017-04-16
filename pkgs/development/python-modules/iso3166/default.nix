a7d2be1471bb31bb047027aa0d3f06370926e0b8
Jörg Thalheim <joerg@thalheim.io>
2017-04-17 00:47:12 +0200

    pythonPackages.iso3166: enable tests
    fixes #24884

--- pkgs/development/python-modules/iso3166/default.nix
+++ pkgs/development/python-modules/iso3166/default.nix
@@ -1,14 +1,23 @@
-{ stdenv, fetchurl, buildPythonPackage }:
- 
+{ stdenv, fetchFromGitHub, buildPythonPackage, pytest }:
+
 buildPythonPackage rec {
   name = "${pname}-${version}";
   pname = "iso3166";
   version = "0.8";
 
-  src = fetchurl {
-    url = "mirror://pypi/i/${pname}/${name}.tar.gz";
-    sha256 = "fbeb17bed90d15b1f6d6794aa2ea458e5e273a1d29b6f4939423c97640e14933";
+  src = fetchFromGitHub {
+    owner = "deactivated";
+    repo = "python-iso3166";
+    # repo has no version tags
+    rev = "f04e499447bbff10af701cf3dd81f6bcdf02f7d7";
+    sha256 = "0zs9za9dr2nl5srxir08yibmp6nffcapmzala0fgh8ny7y6rafrx";
   };
+
+  buildInputs = [ pytest ];
+
+  checkPhase = ''
+    py.test
+  '';
 
   meta = with stdenv.lib; {
     homepage = https://github.com/deactivated/python-iso3166;
