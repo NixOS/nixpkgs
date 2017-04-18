2a17b052669df89dd485e0f543062ee2500f89ec
Mathias Schreck <schreck.mathias@googlemail.com>
2017-04-18 22:43:50 +0200

    nodejs: 7.7.3 -> 7.9.0
    (cherry picked from commit 6cdec331418f7344973d0847d0f004c1a2dc4180)

--- pkgs/development/web/nodejs/v7.nix
+++ pkgs/development/web/nodejs/v7.nix
@@ -10,11 +10,11 @@ let
   baseName = if enableNpm then "nodejs" else "nodejs-slim";
 in
   stdenv.mkDerivation (nodejs // rec {
-    version = "7.7.3";
+    version = "7.9.0";
     name = "${baseName}-${version}";
     src = fetchurl {
       url = "https://nodejs.org/download/release/v${version}/node-v${version}.tar.xz";
-      sha256 = "1pqfrvz06nz88jdp1vsrxfy5z0v8yas1c6pkvl45afvl3zqxlhal";
+      sha256 = "0abaz5z0cv7amd6blm4cm91asj30ydf0lq3j0wdg6aa9i15pcsd5";
     };
 
   })
