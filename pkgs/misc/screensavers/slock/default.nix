224f4da5f9d33fc6471f571308b81054a138f10f
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2017-04-18 19:19:52 +0900

    slock: add configFile support

--- pkgs/misc/screensavers/slock/default.nix
+++ pkgs/misc/screensavers/slock/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, fetchurl, xproto, libX11, libXext, libXrandr }:
-stdenv.mkDerivation rec {
+{ stdenv, fetchurl, xproto, libX11, libXext, libXrandr, conf ? null }:
+with stdenv.lib; stdenv.mkDerivation rec {
   name = "slock-1.4";
   src = fetchurl {
     url = "http://dl.suckless.org/tools/${name}.tar.gz";
@@ -7,6 +7,8 @@ stdenv.mkDerivation rec {
   };
   buildInputs = [ xproto libX11 libXext libXrandr ];
   installFlags = "DESTDIR=\${out} PREFIX=";
+  configFile = optionalString (conf!=null) (writeText "config.def.h" conf);
+  preBuild = optionalString (conf!=null) "cp ${configFile} config.def.h";
   meta = with stdenv.lib; {
     homepage = http://tools.suckless.org/slock;
     description = "Simple X display locker";
