3c213144e6acd708d89159d3b9100972408d5e95
Jörg Thalheim <joerg@thalheim.io>
2017-04-19 19:01:56 +0200

    fetchbzr: prevent log creation in HOME

--- pkgs/build-support/fetchbzr/builder.sh
+++ pkgs/build-support/fetchbzr/builder.sh
@@ -2,7 +2,12 @@ source "$stdenv/setup"
 
 header "exporting \`$url' (revision $rev) into \`$out'"
 
+# avoid creating a log file ~/.bzr.log,
+# because nixbld user have no home
+export BZR_LOG=/dev/null
+
 # Perform a lightweight checkout so that we don't end up importing
+
 # all the repository's history.
 BZR_LOG=/dev/null bzr -Ossl.cert_reqs=none export -r "$rev" --format=dir "$out" "$url"
 
