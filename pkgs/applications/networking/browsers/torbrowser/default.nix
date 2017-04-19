0602b9d1943591d1ee9b056fca7919af8696d599
Joachim Fasting <joachifm@fastmail.fm>
2017-04-20 00:16:19 +0200

    torbrowser: 6.5.1 -> 6.5.2

--- pkgs/applications/networking/browsers/torbrowser/default.nix
+++ pkgs/applications/networking/browsers/torbrowser/default.nix
@@ -81,19 +81,19 @@ let
   fteLibPath = makeLibraryPath [ stdenv.cc.cc gmp ];
 
   # Upstream source
-  version = "6.5.1";
+  version = "6.5.2";
 
   lang = "en-US";
 
   srcs = {
     "x86_64-linux" = fetchurl {
       url = "https://dist.torproject.org/torbrowser/${version}/tor-browser-linux64-${version}_${lang}.tar.xz";
-      sha256 = "1p2bgavvyzahqpjg9vp14c0s50rmha3v1hs1c8zvz6fj8fgrhn0i";
+      sha256 = "0jn98arczlgjigpmql1qg5b7izabv4zy4mji6vvcg3b8g1ma108r";
     };
 
     "i686-linux" = fetchurl {
       url = "https://dist.torproject.org/torbrowser/${version}/tor-browser-linux32-${version}_${lang}.tar.xz";
-      sha256 = "1zfghr01bhpn39wqaw7hyx7yap7xyla4m3mrgz2vi9a5qsyxmbcr";
+      sha256 = "0micxgkbys0py4bj6csbc8xz4gq0x5v2zirgi38krnm5x5riqj3w";
     };
   };
 in
