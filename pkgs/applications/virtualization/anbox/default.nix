71364284702b0e44271308a72f8921416128f08d
Jörg Thalheim <joerg@thalheim.io>
2017-04-20 07:55:10 +0200

    anbox: add kernel version constraint

--- pkgs/applications/virtualization/anbox/default.nix
+++ pkgs/applications/virtualization/anbox/default.nix
@@ -2,6 +2,8 @@
 , lcov, SDL2, mesa_glu, protobuf, lxc, glm, xorg, linuxPackages, pythonPackages, bash, coreutils, python2, SDL2_image
 , makeWrapper, libglvnd, kernel ? null, systemd }:
 
+assert kernel != null -> stdenv.lib.versionAtLeast kernel.version "4.4";
+
 let
   properties-cpp = stdenv.mkDerivation rec {
     name = "properties-cpp-${rev}";
