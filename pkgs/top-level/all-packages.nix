ee7ec319dd26cc0d53cbc8f3e5c64320e49f33da
Eric Sagnes <eric.sagnes@gmail.com>
2017-04-21 17:47:29 +0900

    rainbowstream: add top-level attribute

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3690,6 +3690,8 @@ with pkgs;
 
   radvd = callPackage ../tools/networking/radvd { };
 
+  rainbowstream = pythonPackages.rainbowstream;
+
   rambox = callPackage ../applications/networking/instant-messengers/rambox { };
 
   ranger = callPackage ../applications/misc/ranger { };
