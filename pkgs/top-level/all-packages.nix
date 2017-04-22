eedc6799b6b775ba1c11902fbdf271e96883ff6c
lucas8 <luc.linux@mailoo.org>
2017-04-22 18:40:09 +0200

    cubicle: init at 1.0.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17492,6 +17492,8 @@ with pkgs;
 
   cryptoverif = callPackage ../applications/science/logic/cryptoverif { };
 
+  cubicle = callPackage ../applications/science/logic/cubicle { };
+
   cvc3 = callPackage ../applications/science/logic/cvc3 {
     gmp = lib.overrideDerivation gmp (a: { dontDisableStatic = true; });
   };
