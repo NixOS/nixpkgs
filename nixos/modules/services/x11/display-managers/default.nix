99dfb6dce37edcd1db7cb85c2db97089d9d5f442
Eelco Dolstra <edolstra@gmail.com>
2017-04-22 21:26:36 +0200

    Revert "display-manager: fix argument handling of sddm"
    This reverts commit 40a5498c3f9558b1f033e17ebd65c21c56e4f0ba.
    
    It breaks slim:
    
    Apr 22 21:04:23 hagbard xsession[1037]: /nix/store/bw00yl4yspl3wlyiwk56bi0hljjr00di-xsession: Window manager '/nix/store/bw00yl4yspl3wlyiwk56bi0hljjr00di-xsession 'xfce'' not found.
    Apr 22 21:04:23 hagbard xsession[1037]: /nix/store/bw00yl4yspl3wlyiwk56bi0hljjr00di-xsession: Desktop manager '/nix/store/bw00yl4yspl3wlyiwk56bi0hljjr00di-xsession 'xfce'' not found.
    
    Issue #23264.

--- nixos/modules/services/x11/display-managers/default.nix
+++ nixos/modules/services/x11/display-managers/default.nix
@@ -32,14 +32,8 @@ let
     ''
       #! ${pkgs.bash}/bin/bash
 
-      # SDDM splits "Exec" line in .desktop file by whitespace and pass script path as $1
-      if [[ "$0" = "$1" ]]; then
-        # remove superfluous $1 again
-        shift
-        # join arguments again and evaluate them in a shell context
-        # to interpret shell quoting
-        eval exec "$0" "$@"
-      fi
+      # Handle being called by SDDM.
+      if test "''${1:0:1}" = / ; then eval exec $1 $2 ; fi
 
       ${optionalString cfg.displayManager.logToJournal ''
         if [ -z "$_DID_SYSTEMD_CAT" ]; then
