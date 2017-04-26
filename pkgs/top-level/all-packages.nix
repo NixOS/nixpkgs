be9c0e8b7e093e6b69eb0b3d1aec275ca38dade3
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2017-04-25 23:28:13 -0400

    root: 6.08.02 -> 6.09.02
    This also fixes breakage on darwin due to LLVM4.0 migration.
    
    I had to enable opengl because otherwise macOS build is broken.
    See root commit 23f8871c7 ('Do not include OpenGL directly but use our
    TGLIncludes for that purpose.')
    
    I've enabled xml because TUnfold depends on it.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17789,7 +17789,7 @@ with pkgs;
   megam = callPackage ../applications/science/misc/megam { };
 
   root = callPackage ../applications/science/misc/root {
-    inherit (darwin.apple_sdk.frameworks) Cocoa;
+    inherit (darwin.apple_sdk.frameworks) Cocoa OpenGL;
   };
 
   simgrid = callPackage ../applications/science/misc/simgrid { };
