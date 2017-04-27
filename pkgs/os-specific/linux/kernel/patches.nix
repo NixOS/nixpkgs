7d03a5b5cbc81f9c36f52cb29dcd79f07d98d6b0
Joachim Fasting <joachifm@fastmail.fm>
2017-04-27 20:42:23 +0200

    grsecurity: discontinue support
    Upstream has decided to make -testing patches private, effectively ceasing
    free support for grsecurity/PaX [1].  Consequently, we can no longer
    responsibly support grsecurity on NixOS.
    
    As a first step turn the patch & kernel expressions into eval errors.
    It's unclear whether we might want to prune all grsec/PaX stuff from the
    tree --- the infrastructure might be useful to users wishing to build
    NixOS against grsec patches they've paid for. OTOH, I certainly cannot
    support that use-case so bit-rot is a real concern.
    
    [1]: https://grsecurity.net/passing_the_baton.php

--- pkgs/os-specific/linux/kernel/patches.nix
+++ pkgs/os-specific/linux/kernel/patches.nix
@@ -99,11 +99,7 @@ rec {
     sha256 = "00b1rqgd4yr206dxp4mcymr56ymbjcjfa4m82pxw73khj032qw3j";
   };
 
-  grsecurity_testing = grsecPatch
-    { kver   = "4.9.24";
-      grrev  = "201704220732";
-      sha512 = "0n9v066z3qh296fyvsg1gnygy7jd0cy0pnywxzglh58dnibl28q2ywjnp4ff30andzzq7rvjkk4n151xvs1n04pf2azkgz6igwfisg7";
-    };
+  grsecurity_testing = throw "grsecurity/PaX is unsupported";
 
   # This patch relaxes grsec constraints on the location of usermode helpers,
   # e.g., modprobe, to allow calling into the Nix store.
