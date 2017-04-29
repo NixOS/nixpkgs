f1c7d5a6ba8200d1ac463f1f796f6f359f1423c7
Michael Raskin <7c6f434c@mail.ru>
2017-04-29 16:51:25 +0200

    gimpPlugins.resynthesizer2: fix build

--- pkgs/applications/graphics/gimp/plugins/default.nix
+++ pkgs/applications/graphics/gimp/plugins/default.nix
@@ -125,7 +125,9 @@ rec {
       Filters/Enhance/Smart remove selection
     */
     name = "resynthesizer-2.0.1";
-    buildInputs = [ gimp pkgs.fftw pkgs.autoreconfHook ]
+    buildInputs = [ gimp pkgs.fftw pkgs.autoreconfHook pkgs.pkgconfig pkgs.gtk2
+      pkgs.intltool
+    ]
       ++ gimp.nativeBuildInputs;
     makeFlags = "GIMP_LIBDIR=$out/lib/gimp/2.0/";
     src = fetchFromGitHub {
