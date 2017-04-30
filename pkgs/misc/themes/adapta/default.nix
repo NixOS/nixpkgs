e0d18259939f1fe1a25b08b4922c3f798df53aa5
romildo <malaquias@gmail.com>
2017-04-30 17:23:24 -0300

    adapta-gtk-theme: 3.89.1.66 -> 3.90.0.125

--- pkgs/misc/themes/adapta/default.nix
+++ pkgs/misc/themes/adapta/default.nix
@@ -1,8 +1,8 @@
-{ stdenv, fetchFromGitHub, autoreconfHook, parallel, sassc, inkscape, libxml2, glib, gdk_pixbuf, librsvg, gtk-engine-murrine }:
+{ stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, parallel, sassc, inkscape, libxml2, glib, gdk_pixbuf, librsvg, gtk-engine-murrine }:
 
 stdenv.mkDerivation rec {
   name = "adapta-gtk-theme-${version}";
-  version = "3.89.1.66";
+  version = "3.90.0.125";
 
   meta = with stdenv.lib; {
     description = "An adaptive GTK+ theme based on Material Design";
@@ -16,12 +16,12 @@ stdenv.mkDerivation rec {
     owner = "tista500";
     repo = "Adapta";
     rev = version;
-    sha256 = "08g941xgxg7i8g1srn3zdxz1nxm24bkrg5cx9ipjqk5cwsck7470";
+    sha256 = "0abww5rcbn478w2kdhjlf68bfj8yf8i02nlmrjpp7j1v14r32xr0";
   };
 
   preferLocalBuild = true;
 
-  nativeBuildInputs = [ autoreconfHook parallel sassc inkscape libxml2 glib.dev ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig parallel sassc inkscape libxml2 glib.dev ];
 
   buildInputs = [ gdk_pixbuf librsvg gtk-engine-murrine ];
 
