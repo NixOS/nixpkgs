0af84da84bad7fcba3b514922080626a77280710
Frederik Rietdijk <fridh@fridh.nl>
2017-05-01 09:54:06 +0200

    pythonPackages.colorclass: disable tests

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2466,13 +2466,17 @@ in {
   };
 
   colorclass = buildPythonPackage rec {
+    pname = "colorclass";
     version = "2.2.0";
-    name = "colorclass-${version}";
+    name = "${pname}-${version}";
 
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/c/colorclass/${name}.tar.gz";
+    src = fetchPypi {
+      inherit pname version;
       sha256 = "b05c2a348dfc1aff2d502527d78a5b7b7e2f85da94a96c5081210d8e9ee8e18b";
     };
+
+    # No tests in archive
+    doCheck = false;
 
     meta = {
       homepage = "https://github.com/Robpol86/colorclass";
