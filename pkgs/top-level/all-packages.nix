02a188c9199e6dfdbd92176d090b30365c400c97
Nikolay Amiantov <ab@fmap.me>
2017-05-01 15:22:59 +0300

    linuxPackages.evdi: init at 1.4.1
    (cherry picked from commit 030862f63c8804f3e5aec66e17af2297bfb265f0)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11463,6 +11463,8 @@ with pkgs;
 
     exfat-nofuse = callPackage ../os-specific/linux/exfat { };
 
+    evdi = callPackage ../os-specific/linux/evdi { };
+
     pktgen = callPackage ../os-specific/linux/pktgen { };
 
     odp-dpdk = callPackage ../os-specific/linux/odp-dpdk { };
