cc5f1f54dc32ef6ee6731185a7e91ca716f022f9
José Romildo Malaquias <malaquias@gmail.com>
2017-05-02 06:09:51 -0300

    mkvtoolnix: 9.9.0 -> 11.0.0 (#25349)
    Add dependency on Qt’s multimedia component, required for compilation
    of the GUIs since version 11.0.0.

--- pkgs/applications/video/mkvtoolnix/default.nix
+++ pkgs/applications/video/mkvtoolnix/default.nix
@@ -10,13 +10,13 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "mkvtoolnix-${version}";
-  version = "9.9.0";
+  version = "11.0.0";
 
   src = fetchFromGitHub {
     owner = "mbunkus";
     repo = "mkvtoolnix";
     rev = "release-${version}";
-    sha256 = "1jiz23s52l3gpl84yx4yw3w445jqzcimvnvibvrv3a21k29hyik1";
+    sha256 = "1qqa8ss2mfjzj984l9vc1fnk7czbvhbmmq53m87gnrc65351gkir";
   };
 
   nativeBuildInputs = [ pkgconfig autoconf automake gettext drake ruby docbook_xsl libxslt ];
@@ -26,7 +26,7 @@ stdenv.mkDerivation rec {
     libvorbis flac
   ]
   ++ optional stdenv.isDarwin libiconv
-  ++ optional withGUI qt5.qtbase;
+  ++ optionals withGUI [qt5.qtbase qt5.qtmultimedia];
 
   preConfigure = "./autogen.sh; patchShebangs .";
   buildPhase   = "drake -j $NIX_BUILD_CORES";
