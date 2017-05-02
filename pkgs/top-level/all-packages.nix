1141edcf9bbca2d5990d2f9a0a4f49164d2779a3
ryneeverett <ryneeverett@gmail.com>
2017-05-01 21:45:27 -0400

    lxpanel: init at 0.9.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5912,6 +5912,10 @@ with pkgs;
 
   lxmenu-data = callPackage ../desktops/lxde/core/lxmenu-data.nix { };
 
+  lxpanel = callPackage ../desktops/lxde/core/lxpanel {
+    gtk2 = gtk2-x11;
+  };
+
   kona = callPackage ../development/interpreters/kona {};
 
   lolcode = callPackage ../development/interpreters/lolcode { };
