1401b1320674ea96100155ba570c7e23c8eed054
Maximilian Bosch <maximilian@mbosch.me>
2017-05-04 07:41:57 +0200

    lambda-mod-zsh-theme: 2015-12-15 -> 2017-04-05
    fixes #25492

--- pkgs/shells/lambda-mod-zsh-theme/default.nix
+++ pkgs/shells/lambda-mod-zsh-theme/default.nix
@@ -1,16 +1,13 @@
 { stdenv, fetchFromGitHub }:
 
-let
-  repo = "lambda-mod-zsh-theme";
-  rev = "eceee68cf46bba9f7f42887c2128b48e8861e31b";
-in stdenv.mkDerivation {
-  name = "${repo}-${rev}";
+stdenv.mkDerivation {
+  name = "lambda-mod-zsh-theme-2017-04-05";
 
   src = fetchFromGitHub {
-    inherit rev repo;
-
     owner = "halfo";
-    sha256 = "1410ryc22i20na5ypa1q6f106lkjj8n1qfjmb77q4rspi0ydaiy4";
+    repo = "lambda-mod-zsh-theme";
+    sha256 = "01c77s6fagycin6cpssif56ysbqaa8kiafjn9av12cacakldl84j";
+    rev = "c6445c79cbc73b85cc18871c216fb28ddc8b3d96";
   };
 
   buildPhases = [ "unpackPhase" "installPhase" ];
