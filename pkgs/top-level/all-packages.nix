f7c6266ca6ed9cc1238e73312b2f5b18c121b75f
Profpatsch <mail@profpatsch.de>
2017-05-05 03:11:43 +0200

    fira-code-symbols: init at 20160811

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12559,6 +12559,7 @@ with pkgs;
   fira = callPackage ../data/fonts/fira { };
 
   fira-code = callPackage ../data/fonts/fira-code { };
+  fira-code-symbols = callPackage ../data/fonts/fira-code/symbols.nix { };
 
   fira-mono = callPackage ../data/fonts/fira-mono { };
 
