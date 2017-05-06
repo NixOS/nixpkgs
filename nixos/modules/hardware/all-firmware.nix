b2f36580e0ae6981f9695dedec22d99f8a3d2d64
Profpatsch <mail@profpatsch.de>
2017-05-06 23:39:09 +0200

    modules/hardware/all-firmware: add enableAllUnfreeFirmware
    By introducing `broadcom-bt-firmware`, lots of machines stopped evaluating.
    We need a separate option for those firmwares.

--- nixos/modules/hardware/all-firmware.nix
+++ nixos/modules/hardware/all-firmware.nix
@@ -12,7 +12,14 @@ with lib;
       default = false;
       type = types.bool;
       description = ''
-        Turn on this option if you want to enable all the firmware shipped in linux-firmware.
+        Turn on this option if you want to enable all the firmware shipped in linux-firmware. For unfree firmware, see <literal>hardware.enableAllUnfreeFirmware</literal>.
+      '';
+    };
+    hardware.enableAllUnfreeFirmware = mkOption {
+      default = false;
+      type = types.bool;
+      description = ''
+        Turn on this option if you want to enable all the firmware shipped in linux-firmware that has an unfree license.
       '';
     };
 
@@ -21,14 +28,20 @@ with lib;
 
   ###### implementation
 
-  config = mkIf config.hardware.enableAllFirmware {
-    hardware.firmware = with pkgs; [
-      broadcom-bt-firmware
-      firmwareLinuxNonfree
-      intel2200BGFirmware
-      rtl8723bs-firmware
-      rtl8192su-firmware
-    ];
-  };
+  config = mkMerge [
+    (mkIf config.hardware.enableAllFirmware {
+      hardware.firmware = with pkgs; [
+        firmwareLinuxNonfree
+        intel2200BGFirmware
+        rtl8723bs-firmware
+        rtl8192su-firmware
+      ];
+    })
+    (mkIf config.hardware.enableAllUnfreeFirmware {
+      hardware.firmware = with pkgs; [
+        broadcom-bt-firmware
+      ];
+    })
+  ];
 
 }
