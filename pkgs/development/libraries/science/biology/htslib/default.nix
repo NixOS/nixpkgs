295a23a2db2c6aef712864770f92547c1494e307
mimadrid <mimadrid@ucm.es>
2017-05-07 22:30:41 +0200

    htslib: 1.3.2 -> 1.4

--- pkgs/development/libraries/science/biology/htslib/default.nix
+++ pkgs/development/libraries/science/biology/htslib/default.nix
@@ -1,16 +1,21 @@
-{ stdenv, fetchurl, zlib }:
+{ stdenv, fetchurl, zlib, bzip2, lzma, curl }:
 
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
+  version = "${major}.0";
   pname = "htslib";
-  version = "1.3.2";
+  major = "1.4";
 
   src = fetchurl {
-    url = "https://github.com/samtools/${pname}/releases/download/${version}/${name}.tar.bz2";
-    sha256 = "0iq3blw23s55vkr1z88p9y2dqrb2dybzhl6hz2nlk53ncihrxcdr";
+    url = "https://github.com/samtools/htslib/releases/download/${major}/htslib-${major}.tar.bz2";
+    sha256 = "0l1ki3sqfhawfn7fx9v7i2pm725jki4c5zij9j96xka5zwc8iz2w";
   };
 
-  buildInputs = [ zlib ];
+  buildInputs = [ zlib bzip2 lzma curl ];
+
+  configureFlags = "--enable-libcurl"; # optional but strongly recommended
+
+  installFlags = "prefix=$(out)";
 
   meta = with stdenv.lib; {
     description = "A C library for reading/writing high-throughput sequencing data";
