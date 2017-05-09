da942ea23f28c5a29882a802b728add1b6f309a6
Thomas Tuegel <ttuegel@mailbox.org>
2017-05-09 09:20:39 -0500

    breeze-gtk: bump GTK3 version

--- pkgs/desktops/plasma-5/breeze-gtk.nix
+++ pkgs/desktops/plasma-5/breeze-gtk.nix
@@ -3,6 +3,6 @@
 mkDerivation {
   name = "breeze-gtk";
   nativeBuildInputs = [ extra-cmake-modules ];
-  cmakeFlags = [ "-DWITH_GTK3_VERSION=3.20" ];
+  cmakeFlags = [ "-DWITH_GTK3_VERSION=3.22" ];
   buildInputs = [ qtbase ];
 }
