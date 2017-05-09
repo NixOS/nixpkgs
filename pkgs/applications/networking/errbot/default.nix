3e7aa509dc4b2b5b164b57c074971a3daef523b9
Robin Gloster <mail@glob.in>
2017-05-09 22:08:36 +0200

    errbot: 4.3.3 -> 5.0.1

--- pkgs/applications/networking/errbot/default.nix
+++ pkgs/applications/networking/errbot/default.nix
@@ -1,12 +1,13 @@
-{ stdenv, fetchurl, fetchpatch, pythonPackages }:
+{ stdenv, fetchurl, fetchpatch, pythonPackages, glibcLocales
+, withGraphicalBackend ? false }:
 
 pythonPackages.buildPythonApplication rec {
   name = "errbot-${version}";
-  version = "4.3.3";
+  version = "5.0.1";
 
   src = fetchurl {
     url = "mirror://pypi/e/errbot/${name}.tar.gz";
-    sha256 = "1z6xcm8jx22i56gpcrjym85a82swvaxh09zkryd5pdksi6k41rb4";
+    sha256 = "16kswcf844cxxp7hm7mksmv9w3b501dyn3swm54vq69y99iiynvy";
   };
 
   disabled = !pythonPackages.isPy3k;
@@ -16,11 +17,9 @@ pythonPackages.buildPythonApplication rec {
       url = "https://github.com/mayflower/errbot/commit/d6890c2e02e145b0344a5ea05fd5874218fa40ff.patch";
       sha256 = "0qlninni6rgwk5fvcybpan3zppmchs34p4v9rzwnqqzhn4429mfh";
     })
-    (fetchpatch {
-      url = "https://github.com/mayflower/errbot/commit/3705817fe2dfe957b35c5523bee8d85fa2d5f8f8.patch";
-      sha256 = "1whz72p5hjlwkriiql76vvc3lz1n171p9n0p4mk3mwgapgmwavd8";
-    })
   ];
+
+  LC_ALL = "en_US.utf8";
 
   postPatch = ''
     substituteInPlace setup.py \
@@ -30,12 +29,13 @@ pythonPackages.buildPythonApplication rec {
   # tests folder is not included in release
   doCheck = false;
 
+  buildInputs = [ glibcLocales ];
   propagatedBuildInputs = with pythonPackages; [
     webtest bottle threadpool rocket-errbot requests2 jinja2
     pyopenssl colorlog Yapsy markdown ansi pygments dns pep8
     daemonize pygments-markdown-lexer telegram irc slackclient
     sleekxmpp hypchat pytest
-  ];
+  ] ++ stdenv.lib.optional withGraphicalBackend pyside;
 
   meta = with stdenv.lib; {
     description = "Chatbot designed to be simple to extend with plugins written in Python";
