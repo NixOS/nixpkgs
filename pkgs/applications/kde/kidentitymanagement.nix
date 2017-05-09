e39b5c7f06ea6cda7bf2cdf8a029e7c8c1688d05
Thomas Tuegel <ttuegel@mailbox.org>
2017-05-09 18:33:11 -0500

    kidentitymanagement: init at 17.04.0

---
+++ pkgs/applications/kde/kidentitymanagement.nix
@@ -0,0 +1,20 @@
+{
+  mkDerivation, lib, kdepimTeam,
+  extra-cmake-modules, kdoctools,
+  kcompletion, kcoreaddons, kemoticons, kiconthemes, kio, kpimtextedit,
+  ktextwidgets, kxmlgui
+}:
+
+mkDerivation {
+  name = "kidentitymanagement";
+  meta = {
+    license = with lib.licenses; [ gpl2 lgpl21 fdl12 ];
+    maintainers = kdepimTeam;
+  };
+  nativeBuildInputs = [ extra-cmake-modules kdoctools ];
+  buildInputs = [
+    kcompletion kemoticons kiconthemes kio ktextwidgets kxmlgui
+  ];
+  propagatedBuildInputs = [ kcoreaddons kpimtextedit ];
+  outputs = [ "out" "dev" ];
+}
