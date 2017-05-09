3b69497bb1788d17609b3d7bbe75f4790598c1de
Venkateswara Rao Mandela <venkat.mandela@gmail.com>
2017-05-09 16:34:43 +0530

    git-series: init at 0.9.1
    git series tracks changes to a patch series over time. git series also
    tracks a cover letter for the patch series, formats the series for
    email, and prepares pull requests.
    
    https://github.com/git-series/git-series

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2021,6 +2021,8 @@ with pkgs;
 
   git-lfs = callPackage ../applications/version-management/git-lfs { };
 
+  git-series = callPackage ../development/tools/git-series { };
+
   git-up = callPackage ../applications/version-management/git-up { };
 
   gitfs = callPackage ../tools/filesystems/gitfs { };
