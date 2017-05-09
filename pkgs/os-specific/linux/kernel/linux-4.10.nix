8584a169220a91791158c49e0021ca330092c712
Tim Steinbach <tim@nequissimus.com>
2017-05-09 08:43:19 -0400

    linux: 4.10.14 -> 4.10.15

--- pkgs/os-specific/linux/kernel/linux-4.10.nix
+++ pkgs/os-specific/linux/kernel/linux-4.10.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.10.14";
+  version = "4.10.15";
   extraMeta.branch = "4.10";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1p9k7z91qxp0qqzd0qjfbasxirmhny5s3sasnlizcwsvyb4vicyk";
+    sha256 = "0i943i7gp9fk5ic3dbm9w8ilgpmphav2m3rlj1i3c0r7y0824hfq";
   };
 
   kernelPatches = args.kernelPatches;
