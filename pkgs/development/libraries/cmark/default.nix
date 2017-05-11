e2a807cf128f37701a33b88aa1beccb9055aa652
Jörg Thalheim <joerg@thalheim.io>
2017-05-11 08:35:54 +0100

    cmark: enable tests

--- pkgs/development/libraries/cmark/default.nix
+++ pkgs/development/libraries/cmark/default.nix
@@ -1,20 +1,27 @@
-{ stdenv, fetchurl, cmake }:
+{ stdenv, fetchFromGitHub, cmake }:
 
 stdenv.mkDerivation rec {
   version = "0.27.1";
   name = "cmark-${version}";
 
-  src = fetchurl {
-    url = "https://github.com/jgm/cmark/archive/${version}.tar.gz";
-    sha256 = "1da62ispca9aal2a36gaj87175rv5013pl7x740vk32y6lclr6v6";
+  src = fetchFromGitHub {
+    owner = "jgm";
+    repo = "cmark";
+    rev = version;
+    sha256 = "06miwq3rl2bighkn6iq7bdwzmvcqa53qwpa0pqjqa8yn44j8ijj8";
   };
 
   nativeBuildInputs = [ cmake ];
+  doCheck = true;
+  checkPhase = ''
+    export LD_LIBRARY_PATH=$(readlink -f ./src)
+    CTEST_OUTPUT_ON_FAILURE=1 make test
+  '';
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "CommonMark parsing and rendering library and program in C";
     homepage = https://github.com/jgm/cmark;
-    maintainers = [ stdenv.lib.maintainers.michelk ];
-    platforms = stdenv.lib.platforms.unix;
+    maintainers = [ maintainers.michelk ];
+    platforms = platforms.unix;
   };
 }
