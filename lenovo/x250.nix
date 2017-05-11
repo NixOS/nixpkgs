68e007ae7768711dc2a68f4399a01ec8f5a3ab9a
Jörg Thalheim <joerg@thalheim.io>
2017-05-11 11:38:39 +0100

    x250: enable wheel emulation

--- lenovo/x250.nix
+++ lenovo/x250.nix
@@ -3,4 +3,7 @@
 {
    # modesetting driver leads to freezes with newer kernel at the moment (> 4.4)
    services.xserver.videoDrivers = [ "intel" ];
+
+   services.xserver.libinput.enable = true;
+   hardware.trackpoint.emulateWheel = true;
 }
