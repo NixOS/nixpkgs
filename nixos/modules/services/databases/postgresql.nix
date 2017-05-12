9869cb0f0d43d789f4ba08bfd1eecc4ed273a0ad
Pascal Bach <pascal.bach@nextrem.ch>
2017-05-13 00:24:26 +0200

    postgresql service: make 9.6 the default version for 17.09

--- nixos/modules/services/databases/postgresql.nix
+++ nixos/modules/services/databases/postgresql.nix
@@ -160,7 +160,9 @@ in
       # Note: when changing the default, make it conditional on
       # ‘system.stateVersion’ to maintain compatibility with existing
       # systems!
-      mkDefault (if versionAtLeast config.system.stateVersion "16.03" then pkgs.postgresql95 else pkgs.postgresql94);
+      mkDefault (if versionAtLeast config.system.stateVersion "17.09" then pkgs.postgresql96
+            else if versionAtLeast config.system.stateVersion "16.03" then pkgs.postgresql95
+            else pkgs.postgresql94);
 
     services.postgresql.authentication = mkAfter
       ''
