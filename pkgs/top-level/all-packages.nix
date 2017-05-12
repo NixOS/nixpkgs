b7493e13618e739a386f7a2d2f11db3ed8fb0118
Thomas Tuegel <ttuegel@mailbox.org>
2017-05-12 12:55:10 -0500

    kdiagram: init at 2.6.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9985,6 +9985,8 @@ with pkgs;
       kirigami_1
       kirigami_2;
 
+    kdiagram = callPackage ../development/libraries/kdiagram { };
+
     kirigami = kirigami_1;
 
     libcommuni = callPackage ../development/libraries/libcommuni { };
