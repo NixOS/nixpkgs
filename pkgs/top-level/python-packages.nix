2133d8ffe5c5dc51432395f1c61cf2d9a5530222
Sasha Delly <shura.sdll@gmail.com>
2017-05-14 22:26:02 +0000

    ftfy: init at 4.4.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -31348,6 +31348,8 @@ EOF
 
   cymem = callPackage ../development/python-modules/cymem { };    
 
+  ftfy = callPackage ../development/python-modules/ftfy { };    
+
 });
 
 in fix' (extends overrides packages)
