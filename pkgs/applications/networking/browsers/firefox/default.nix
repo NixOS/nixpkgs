cfaf051d15ad6180740a28312aaa6abff3cb2e44
Vladimír Čunát <vcunat@gmail.com>
2017-05-19 21:05:19 +0200

    firefox: 53.0.2 -> 53.0.3
    (from master commit 0460c77eb21189)

--- pkgs/applications/networking/browsers/firefox/default.nix
+++ pkgs/applications/networking/browsers/firefox/default.nix
@@ -153,8 +153,8 @@ in {
 
   firefox-unwrapped = common {
     pname = "firefox";
-    version = "53.0.2";
-    sha512 = "d56d563db7e1a0faa2de9315b71b479adb9109624d0958224ad2cee331dcbdb4f414380bd91f70ee0b3e543eca812c172eb2d6f6b5620f6dd6f61675f5ac69d8";
+    version = "53.0.3";
+    sha512 = "cef5de1e9d6ddf6509a80cd30169fdce701b2fed022979ba5931ccea7b8f77cb644b01984dae028d350e32321cfe2eefc0236c0731bf5a2be12a994fc3fc1118";
     updateScript = import ./update.nix {
       attrPath = "firefox-unwrapped";
       inherit writeScript lib common-updater-scripts xidel coreutils gnused gnugrep curl;
