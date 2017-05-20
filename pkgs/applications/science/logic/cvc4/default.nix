7bf869a8994c2052319e0a22600d7b60978b01ad
Jörg Thalheim <joerg@thalheim.io>
2017-05-20 12:45:38 +0100

    cvc4: use stable url
    upstream delete their tarballs

--- pkgs/applications/science/logic/cvc4/default.nix
+++ pkgs/applications/science/logic/cvc4/default.nix
@@ -1,16 +1,18 @@
-{ stdenv, fetchurl, cln, gmp, swig, pkgconfig, readline, libantlr3c, boost, jdk, autoreconfHook, python2 }:
+{ stdenv, fetchFromGitHub, cln, gmp, swig, pkgconfig, readline, libantlr3c,
+boost, jdk, autoreconfHook, python2, antlr3_4 }:
 
 stdenv.mkDerivation rec {
-  name = "cvc4-${version}";
-  version = "1.5pre-20170514";
+  name = "cvc4-unstable-${version}";
+  version = "2017-05-18";
 
-
-  src = fetchurl {
-    url = http://cvc4.cs.stanford.edu/downloads/builds/src/unstable/latest-unstable.tar.gz;
-    sha256 = "0d56q7llx78f5mbhx6n4qhbq8c1pjl9a5azx823q7zyj70rmrwlq";
+  src = fetchFromGitHub {
+    owner = "CVC4";
+    repo = "CVC4";
+    rev = "d77107cc56b0a089364c3d1512813701c155ea93";
+    sha256 = "085bjrrm33rl5pwqx13af9sgni9cfbg70wag6lm08jj41ws411xs";
   };
 
-  buildInputs = [ gmp cln pkgconfig readline swig libantlr3c boost jdk autoreconfHook python2 ];
+  buildInputs = [ gmp cln pkgconfig readline swig libantlr3c antlr3_4 boost jdk autoreconfHook python2 ];
   configureFlags = [
     "--enable-language-bindings=c,c++,java"
     "--enable-gpl"
