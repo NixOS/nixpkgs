689f0ce372399a1a29e5fd4505d46ee398a26a23
Jörg Thalheim <joerg@thalheim.io>
2017-05-21 14:38:27 +0100

    enzyme: use fetchPypi

--- pkgs/development/python-modules/enzyme/default.nix
+++ pkgs/development/python-modules/enzyme/default.nix
@@ -1,14 +1,15 @@
-{ stdenv, fetchurl, buildPythonPackage }:
+{ stdenv, fetchPypi, buildPythonPackage }:
 
 buildPythonPackage rec {
-  name = "enzyme-${version}";
+  name = "${pname}-${version}";
+  pname = "enzyme";
   version = "0.4.1";
 
   # Tests rely on files obtained over the network
   doCheck = false;
 
-  src = fetchurl {
-    url = "mirror://pypi/e/enzyme/${name}.tar.gz";
+  src = fetchPypi {
+    inherit pname version;
     sha256 = "1fv2kh2v4lwj0hhrhj9pib1pdjh01yr4xgyljhx11l94gjlpy5pj";
   };
 
