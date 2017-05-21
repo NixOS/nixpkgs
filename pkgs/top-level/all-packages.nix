8f8465113200d6e729ccb33494902fa34194dca6
AndersonTorres <torres.anderson.85@gmail.com>
2017-05-21 18:56:02 -0300

    vms-empire: init at 1.14

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17276,6 +17276,8 @@ with pkgs;
 
   vessel = callPackage_i686 ../games/vessel { };
 
+  vms-empire = callPackage ../games/vms-empire { };
+
   voxelands = callPackage ../games/voxelands {
     libpng = libpng12;
   };
