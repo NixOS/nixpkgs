13965b3274fe7866e5cbc4ed12cbe84251e0c4d8
Jörg Thalheim <joerg@thalheim.io>
2017-05-22 07:05:01 +0100

    hss: use buildRubyGem instead of bundler

--- pkgs/tools/networking/hss/default.nix
+++ pkgs/tools/networking/hss/default.nix
@@ -1,10 +1,26 @@
-{ lib, bundlerEnv, ruby }:
+{ lib, buildRubyGem, ruby, openssh }:
 
-bundlerEnv rec {
+# Example ~/.hss.yml
+#---
+#patterns:
+#  - note: Basic test
+#    example: g -> github
+#    short: '^g$'
+#    long: 'git@github.com'
+
+buildRubyGem rec {
   name = "hss-${version}";
+  inherit ruby;
+  gemName = "hss";
   version = "1.0.1";
+  sha256 = "0hdfpxxqsh6gisn8mm0knsl1aig9fir0h2x9sirk3gr36qbz5xa4";
 
-  gemdir = ./.;
+  postInstall = ''
+   substituteInPlace $GEM_HOME/gems/${gemName}-${version}/bin/hss \
+     --replace \
+       "'ssh'" \
+       "'${openssh}/bin/ssh'"
+  '';
 
   meta = with lib; {
     description = ''
