bb2ac0a0bfa5d9d986bf4ffea1a159566eaff43a
Jörg Thalheim <joerg@thalheim.io>
2017-05-23 21:01:38 +0100

    passopolis: add enablePostgresqlDatabase option

--- nixos/modules/services/misc/passopolis.nix
+++ nixos/modules/services/misc/passopolis.nix
@@ -37,6 +37,13 @@ in {
         description = "Database name";
       };
 
+      enablePostgreSQLDatabase = mkOption {
+        type = types.bool;
+        default = true;
+        description = ''
+          Whether to enable a local postgresql service as database for passopolis
+        '';
+      };
     };
 
   };
@@ -46,30 +53,30 @@ in {
 
   config = mkIf cfg.enable {
 
-    users.extraUsers.passopolis =
-    {
+    users.extraUsers.passopolis = {
       name = cfg.user;
       description = "Passopolis service user";
     };
 
-    systemd.services.passopolis =
-    {
+    services.postgresql.enable = mkDefault true;
+
+    systemd.services.passopolis = {
       description = "Passopolis service";
       after = [ "network.target" "postgresql.service" ];
       wantedBy = [ "multi-user.target" ];
-      path = with pkgs; [
+      path = with pkgs; optionals cfg.enablePostgreSQLDatabase [
         config.services.postgresql.package
       ];
       preStart = ''
       mkdir -p ${cfg.statePath}
       chown ${cfg.user} ${cfg.statePath}
-      if [ "${cfg.databaseHost}" = "127.0.0.1" ]; then
+      ${lib.optionalString cfg.enablePostgreSQLDatabase ''
         if ! test -e "${cfg.statePath}/db-created"; then
           psql postgres -c "CREATE ROLE ${cfg.user} WITH LOGIN NOCREATEDB NOCREATEROLE NOCREATEUSER"
           ${config.services.postgresql.package}/bin/createdb --owner ${cfg.user} ${cfg.databaseName} || true
           touch "${cfg.statePath}/db-created"
         fi
-      fi
+      ''}
       '';
 
       serviceConfig = {
