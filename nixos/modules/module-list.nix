7c3253e519a572f90a907fc56bb6407da004b24c
Dan Peebles <pumpkin@me.com>
2017-05-24 18:56:22 -0400

    Simple proof of concept for how to do other types of services

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -129,6 +129,8 @@
   ./security/rtkit.nix
   ./security/wrappers/default.nix
   ./security/sudo.nix
+  ./service-managers/docker.nix
+  ./service-managers/trivial.nix
   ./services/admin/salt/master.nix
   ./services/admin/salt/minion.nix
   ./services/amqp/activemq/default.nix
