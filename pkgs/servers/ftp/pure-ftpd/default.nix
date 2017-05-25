9145b386b65cc3da758d32b6b4e9d8123177f880
Joachim Fasting <joachifm@fastmail.fm>
2017-05-25 21:10:55 +0200

    pure-ftpd: enable tls support
    For encrypted command & data channels.

--- pkgs/servers/ftp/pure-ftpd/default.nix
+++ pkgs/servers/ftp/pure-ftpd/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, openssl }:
 
 stdenv.mkDerivation rec {
   name = "pure-ftpd-1.0.46";
@@ -7,6 +7,10 @@ stdenv.mkDerivation rec {
     url = "https://download.pureftpd.org/pub/pure-ftpd/releases/${name}.tar.gz";
     sha256 = "0p0arcaz63fbb03fkavbc8z6m1f90p5vbnxb8mqlvpma6mrq0286";
   };
+
+  buildInputs = [ openssl ];
+
+  configureFlags = [ "--with-tls" ];
 
   meta = with stdenv.lib; {
     description = "A free, secure, production-quality and standard-conformant FTP server";
