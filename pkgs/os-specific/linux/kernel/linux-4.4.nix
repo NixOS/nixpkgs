97a3b9b8b8ea610f8cee4d83d2ec289711dc9d0f
Tim Steinbach <tim@nequissimus.com>
2017-05-25 18:21:54 -0400

    linux: 4.4.69 -> 4.4.70
    (cherry picked from commit 8f0ca4f44ae9545c3a1b2a104c6958864834e717)

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.4.69";
+  version = "4.4.70";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1yl4iwmi1rvnp1q74ypzd737r217c6yi48vnl9kxc9zqm98bqyr0";
+    sha256 = "1yid0y4ha7mrn9ns037kjsrgbqffcz2c2p27rgn92jh4m5nb7a60";
   };
 
   kernelPatches = args.kernelPatches;
