b9ae0c7185c500f8f2d6688c96f59777493dc229
Thomas Tuegel <ttuegel@mailbox.org>
2017-05-26 15:47:39 -0500

    kwallet-pam: set path to socat

--- pkgs/desktops/plasma-5/kwallet-pam.nix
+++ pkgs/desktops/plasma-5/kwallet-pam.nix
@@ -1,10 +1,11 @@
-{ mkDerivation, extra-cmake-modules, pam, socat, libgcrypt }:
+{ mkDerivation, lib, extra-cmake-modules, pam, socat, libgcrypt, qtbase, }:
 
 mkDerivation {
   name = "kwallet-pam";
 
   nativeBuildInputs = [ extra-cmake-modules ];
-
-  buildInputs = [ pam socat libgcrypt ];
-
+  buildInputs = [ pam socat libgcrypt qtbase ];
+  postPatch = ''
+    sed -i pam_kwallet_init -e "s|socat|${lib.getBin socat}/bin/socat|"
+  '';
 }
