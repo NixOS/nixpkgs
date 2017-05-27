157b5bd1f0ce284bc258c5442e98f56ff65829da
Joachim Fasting <joachifm@fastmail.fm>
2017-05-28 00:59:35 +0200

    cppcheck: 1.78 -> 1.79
    ChangeLog: https://sourceforge.net/p/cppcheck/news/2017/05/cppcheck-179/

--- pkgs/development/tools/analysis/cppcheck/default.nix
+++ pkgs/development/tools/analysis/cppcheck/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   pname = "cppcheck";
-  version = "1.78";
+  version = "1.79";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/${pname}/${name}.tar.bz2";
-    sha256 = "1cc60y2vjq4g88183jkan91ybzqy0n6p5ncs8z5rh6rjsvvrc9p4";
+    sha256 = "0vgh4zx191d7mcmiilkhcbd9jkspfkppl7l4y5451walvs9vd19b";
   };
 
   buildInputs = [ pcre ];
