eab2823e5ea61686142efde669798d7c4e59e5f8
Thomas Tuegel <ttuegel@mailbox.org>
2017-05-28 08:34:40 -0500

    kdepim-runtime: patch from upstream to fix IMAP resource crash

--- pkgs/applications/kde/kdepim-runtime.nix
+++ pkgs/applications/kde/kdepim-runtime.nix
@@ -1,5 +1,5 @@
 {
-  mkDerivation, lib, kdepimTeam,
+  mkDerivation, lib, kdepimTeam, fetchpatch,
   extra-cmake-modules, kdoctools,
   shared_mime_info,
   akonadi, akonadi-calendar, akonadi-contacts, akonadi-mime, akonadi-notes,
@@ -19,6 +19,13 @@ mkDerivation {
     akonadi akonadi-calendar akonadi-contacts akonadi-mime akonadi-notes
     kalarmcal kcalutils kcontacts kdav kdelibs4support kidentitymanagement kimap
     kmailtransport kmbox kmime knotifications knotifyconfig qtwebengine
+  ];
+  patches = [
+    # Fix crash in IMAP Akonadi resource
+    (fetchpatch {
+      url = "https://cgit.kde.org/kdepim-runtime.git/patch/?id=611510d0a005bc93102aa4b9f1a5b5f9905c4179";
+      sha256 = "1zidfqwzj5waq01iqzgq1imr8aq7a2h5aysygi4ynakwgr4ypxcj";
+    })
   ];
   # Attempts to build some files before dependencies have been generated
   enableParallelBuilding = false;
