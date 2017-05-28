b90d714dbb6f17bfa25beddba2e16b673561c70b
Léo Gaspard <leo@gaspard.io>
2017-05-28 20:05:30 +0200

    dkimproxy: also work without the right PERL5LIB environment variable

--- pkgs/servers/mail/dkimproxy/default.nix
+++ pkgs/servers/mail/dkimproxy/default.nix
@@ -12,6 +12,17 @@ stdenv.mkDerivation rec {
     sha256 = "1gc5c7lg2qrlck7b0lvjfqr824ch6jkrzkpsn0gjvlzg7hfmld75";
   };
 
+  # Idea taken from pkgs/development/perl-modules/generic/builder.sh
+  preFixup = ''
+    perlFlags=
+    for i in $(IFS=:; echo $PERL5LIB); do
+      perlFlags="$perlFlags -I$i"
+    done
+    for f in $(ls $out/bin); do
+      sed -i $out/bin/$f -e "s|#\!\(.*/perl.*\)$|#\! \1 $perlFlags|"
+    done
+  '';
+
   buildInputs = [ perl ];
   propagatedBuildInputs = [ Error MailDKIM MIMEtools NetServer ];
 
