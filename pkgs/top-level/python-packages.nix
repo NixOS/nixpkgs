3f9b6ddbf52386259b09d1697c1637b635463e96
rnhmjoj <micheleguerinirocco@me.com>
2017-05-30 20:02:33 +0200

    pythonPackages.ledgerblue: init at 0.1.13

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -29765,6 +29765,27 @@ EOF
     };
   };
 
+  ledgerblue = buildPythonPackage rec {
+    name = "ledgerblue-${version}";
+    version = "0.1.13";
+
+    src = pkgs.fetchurl {
+      url = "mirror://pypi/l/ledgerblue/${name}.tar.gz";
+      sha256 = "09bsiylvgax6m47w8r0myaf61xj9j0h1spvadx6fx31qy0iqicw0";
+    };
+
+    buildInputs = with self; [
+      hidapi pycrypto pillow protobuf future ecpy
+    ];
+
+    meta = {
+      description = "Python library to communicate with Ledger Blue/Nano S";
+      homepage = "https://github.com/LedgerHQ/blue-loader-python";
+      license = licenses.apache2;
+      maintainers = with maintainers; [ np ];
+    };
+  };
+
   ecpy = buildPythonPackage rec {
     name = "ECPy-${version}";
     version = "0.8.1";
