26b69109be4eb6c57eba174e0b26dc1f94657037
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2017-05-31 10:30:24 +0900

    move fonttools to its own directory

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11417,31 +11417,7 @@ in {
     inherit python;
   };
 
-  fonttools = buildPythonPackage (rec {
-    pname = "fonttools";
-    version = "3.13.0";
-    name = "${pname}-${version}";
-
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "5ec278ff231d0c88afe8266e911ee0f8e66c8501c53f5f144a1a0abbc936c6b8";
-      extension = "zip";
-    };
-
-    buildInputs = with self; [
-      numpy
-    ];
-
-    checkInputs = with self; [
-      pytest
-      pytestrunner
-    ];
-
-    meta = {
-      homepage = "https://github.com/fonttools/fonttools";
-      description = "A library to manipulate font files from Python";
-    };
-  });
+  fonttools = callPackage ../development/python-modules/fonttools { };
 
   foolscap = buildPythonPackage (rec {
     name = "foolscap-${version}";
