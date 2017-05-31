29c4d7f505c39930f0d457c9ba4570d6a768f67c
John Ericson <Ericson2314@Yahoo.com>
2017-05-30 20:08:00 -0400

    podofo: Simplify derivation
    Bunch of junk for linux and cross compilation that's no longer needed

--- pkgs/development/libraries/podofo/default.nix
+++ pkgs/development/libraries/podofo/default.nix
@@ -1,5 +1,6 @@
 { stdenv, fetchurl, cmake, zlib, freetype, libjpeg, libtiff, fontconfig
-, gcc5, openssl, libpng, lua5, pkgconfig, libidn, expat }:
+, openssl, libpng, lua5, pkgconfig, libidn, expat
+}:
 
 stdenv.mkDerivation rec {
   name = "podofo-0.9.5";
@@ -11,17 +12,9 @@ stdenv.mkDerivation rec {
 
   propagatedBuildInputs = [ zlib freetype libjpeg libtiff fontconfig openssl libpng libidn expat ];
 
-  # Does Linux really need gcc5? Darwin doesn't seem to...
-  nativeBuildInputs = [ cmake pkgconfig ] ++ stdenv.lib.optional stdenv.isLinux gcc5;
+  nativeBuildInputs = [ cmake pkgconfig ];
 
-  # Does Linux really need libc here? Darwin doesn't seem to...
-  buildInputs = [ lua5 ] ++ stdenv.lib.optional stdenv.isLinux stdenv.cc.libc;
-
-  crossAttrs = {
-    propagatedBuildInputs = [ zlib.crossDrv freetype.crossDrv libjpeg.crossDrv
-      libtiff.crossDrv fontconfig.crossDrv openssl.crossDrv libpng.crossDrv
-      lua5.crossDrv stdenv.ccCross.libc ];
-  };
+  buildInputs = [ lua5 ];
 
   cmakeFlags = "-DPODOFO_BUILD_SHARED=ON -DPODOFO_BUILD_STATIC=OFF";
 
