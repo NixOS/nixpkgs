f93dfd52594bf53889456f7797dd199bca2292ad
rnhmjoj <micheleguerinirocco@me.com>
2017-05-31 22:57:42 +0200

    pythonPackages.semver: move to pkgs/development/python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -29640,28 +29640,7 @@ EOF
 
   ecpy = callPackage ../development/python-modules/ecpy { };
 
-  trezor = callPackage ../development/python-modules/trezor { };
-
-  semver = buildPythonPackage rec {
-    name = "semver-${version}";
-    version = "2.2.1";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/s/semver/${name}.tar.gz";
-      sha256 = "161gvsfpw0l8lnf1v19rvqc8b9f8n70cc8ppya4l0n6rwc1c1n4m";
-    };
-
-    meta = {
-      description = "Python package to work with Semantic Versioning (http://semver.org/)";
-      homepage = "https://github.com/k-bx/python-semver";
-      license = licenses.bsd3;
-      maintainers = with maintainers; [ np ];
-    };
-  };
-
-  ed25519 = buildPythonPackage rec {
-    name = "ed25519-${version}";
-    version = "1.4";
+  semver = callPackage ../development/python-modules/semver { };
 
     src = pkgs.fetchurl {
       url = "mirror://pypi/e/ed25519/${name}.tar.gz";
