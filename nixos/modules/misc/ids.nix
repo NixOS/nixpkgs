746639b7a5e083a375af9a4e347c4622acf2aed0
Kjartan Ovmilk <y.motara@ru.ac.za>
2017-06-01 16:50:17 +0200

    resilio service: replaces btsync service, which is no longer supported upstream.
    (cherry picked from commit 919b39bb7c70a99b090b8e3debde5addc736c469)

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -289,6 +289,7 @@
       rpc = 271;
       geoip = 272;
       fcron = 273;
+      rslsync = 279;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -547,6 +548,7 @@
       #rpc = 271; # unused
       #geoip = 272; # unused
       fcron = 273;
+      rslsync = 279;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
