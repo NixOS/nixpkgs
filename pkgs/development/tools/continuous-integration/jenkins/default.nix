045515a54a25a30e98b4c1f8f7945473930be2dc
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2017-06-03 19:57:35 +0100

    jenkins: 2.62 -> 2.63

--- pkgs/development/tools/continuous-integration/jenkins/default.nix
+++ pkgs/development/tools/continuous-integration/jenkins/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "jenkins-${version}";
-  version = "2.62";
+  version = "2.63";
 
   src = fetchurl {
     url = "http://mirrors.jenkins-ci.org/war/${version}/jenkins.war";
-    sha256 = "1dqmvbl5sa82zsc03ns5i7ppjmdlg3pyk676xjxyr0p5ccbqlxxh";
+    sha256 = "024bzbca2ikk3904df4f8ri37qjq7nzzmg58jax8cbvcbxcik699";
   };
 
   buildCommand = ''
