c2d1cf5f3645e7a32b9b2c0eaaf2995ca7e34a14
Jörg Thalheim <joerg@thalheim.io>
2017-06-03 12:00:12 +0100

    ipmiview: fix wrapper

--- pkgs/applications/misc/ipmiview/default.nix
+++ pkgs/applications/misc/ipmiview/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec {
    version = "2.12.0";
    buildVersion = "160804";
 
-   src = pkgs.fetchurl {
+   src = fetchurl {
     url = "ftp://ftp.supermicro.com/utility/IPMIView/Linux/IPMIView_${version}_build.${buildVersion}_bundleJRE_Linux_x64.tar.gz";
     sha256 = "787a060413451a4a5993c31805f55a221087b7199bbaf20e9fe1254e2a76db42";
   };
@@ -24,8 +24,9 @@ stdenv.mkDerivation rec {
    installPhase = ''
      mkdir -p $out/bin
      cp -R . $out/
-     echo "$out/jre/bin/java -jar $out/IPMIView20.jar" > $out/bin/IPMIView
-     chmod +x $out/bin/IPMIView
+     makeWrapper $out/jre/bin/java $out/bin/IPMIView \
+       --prefix PATH : "$out/jre/bin" \
+       --add-flags "-jar $out/IPMIView20.jar"
    '';
 
    meta = with stdenv.lib; {
