55dbbfd899c2b286a02099c992b0c8bd13655650
Jaka Hudoklin <jakahudoklin@gmail.com>
2017-06-04 22:41:19 +0200

    kubernetes module: kubelet, add socat to path for kubectl portforward

--- nixos/modules/services/cluster/kubernetes/default.nix
+++ nixos/modules/services/cluster/kubernetes/default.nix
@@ -736,7 +736,7 @@ in {
         description = "Kubernetes Kubelet Service";
         wantedBy = [ "kubernetes.target" ];
         after = [ "network.target" "docker.service" "kube-apiserver.service" ];
-        path = with pkgs; [ gitMinimal openssh docker utillinux iproute ethtool thin-provisioning-tools iptables ] ++ cfg.path;
+        path = with pkgs; [ gitMinimal openssh docker utillinux iproute ethtool thin-provisioning-tools iptables socat ] ++ cfg.path;
         preStart = ''
           docker load < ${infraContainer}
           rm /opt/cni/bin/* || true
