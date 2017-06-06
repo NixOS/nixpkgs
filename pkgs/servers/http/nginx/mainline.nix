b74866b9a7e8c538f5287781fc3adca3e52d1903
Franz Pletz <fpletz@fnordicwalking.de>
2017-06-06 03:39:22 +0200

    nginxMainline: 1.13.0 -> 1.13.1

--- pkgs/servers/http/nginx/mainline.nix
+++ pkgs/servers/http/nginx/mainline.nix
@@ -1,6 +1,6 @@
 { callPackage, ... }@args:
 
 callPackage ./generic.nix (args // {
-  version = "1.13.0";
-  sha256 = "1mq56rl3rq3bhnrqsywxfrwh0y5m0n0q0sck8ca4x18ganv2mxbr";
+  version = "1.13.1";
+  sha256 = "0xk7gcsgwhz047h54adn8crnkrkr7g1z79w8ik34v6k0lrr6r1d5";
 })
