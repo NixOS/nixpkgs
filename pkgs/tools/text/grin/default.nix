02184088468e1d540070bdb91dc7c387cba14a6b
Simon Jagoe <simon@simonjagoe.com>
2017-06-07 14:22:52 +0300

    grin: Use fetchFromGitHub instead of fetchgit

--- pkgs/tools/text/grin/default.nix
+++ pkgs/tools/text/grin/default.nix
@@ -1,11 +1,14 @@
-{ stdenv, fetchgit, python2Packages }:
+{ stdenv, fetchFromGitHub, python2Packages }:
 
 python2Packages.buildPythonApplication rec {
-  name = "grin-1.2.1";
+  program = "grin";
+  version = "1.2.1";
+  name = "${program}-${version}";
   namePrefix = "";
 
-  src = fetchgit {
-    url = "git://github.com/rkern/grin";
+  src = fetchFromGitHub {
+    owner = "rkern";
+    repo = program;
     rev = "8dd4b5309b3bc04fe9d3e71836420f7d8d4a293f";
     sha256 = "0vz2aahwdcy1296g4w3i79dkvmzk9jc2n2zmlcvlg5m3s6h7b6jd";
   };
@@ -13,7 +16,7 @@ python2Packages.buildPythonApplication rec {
   buildInputs = with python2Packages; [ nose ];
 
   meta = {
-    homepage = https://pypi.python.org/pypi/grin;
+    homepage = https://github.com/rkern/grin;
     description = "A grep program configured the way I like it";
     platforms = stdenv.lib.platforms.all;
     maintainers = [ stdenv.lib.maintainers.sjagoe ];
