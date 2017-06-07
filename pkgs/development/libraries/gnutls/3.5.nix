23ca0dddec3e1e915b122c2fc555647611ca3c25
Vladimír Čunát <vcunat@gmail.com>
2017-06-07 08:54:38 +0200

    gnutls: maintenance 3.5.10 -> 3.5.13
    It contains a fix for DOS possibility in servers.
    http://gnutls.org/security.html#GNUTLS-SA-2017-4
    
    (cherry picked from commit 9135c9f73a4c34181d2bba1455d2f432b3c49065)

--- pkgs/development/libraries/gnutls/3.5.nix
+++ pkgs/development/libraries/gnutls/3.5.nix
@@ -1,10 +1,18 @@
 { callPackage, fetchurl, libunistring, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "3.5.10";
+  version = "3.5.13";
 
   src = fetchurl {
     url = "ftp://ftp.gnutls.org/gcrypt/gnutls/v3.5/gnutls-${version}.tar.xz";
-    sha256 = "17apwvdkkazh5w8z8mbanpj2yj8s2002qwy46wz4v3akpa33wi5g";
+    sha256 = "15ihq6p0hnnhs8cnjrkj40dmlcaa1jjg8xg0g2ydbnlqs454ixbr";
   };
+
+  # Skip two tests introduced in 3.5.11.  Probable reasons of failure:
+  #  - pkgconfig: building against the result won't work before installing
+  #  - trust-store: default trust store path (/etc/ssl/...) is missing in sandbox
+  postPatch = ''
+    sed '2iexit 77' -i tests/pkgconfig.sh
+    sed '/^void doit(void)/,$s/{/{ exit(77);/; t' -i tests/trust-store.c
+  '';
 })
