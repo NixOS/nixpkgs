55f0f9e1a4818074c52d6e11e65240fbfdd36890
André-Patrick Bubel <code@andre-bubel.de>
2017-06-08 22:42:58 +0200

    profanity: enable tray icon support
    Profanity can show a tray icon, e.g. showing that messages were received.
    This enables support for the feature.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15482,6 +15482,7 @@ with pkgs;
 
   profanity = callPackage ../applications/networking/instant-messengers/profanity {
     notifySupport   = config.profanity.notifySupport   or true;
+    traySupport     = config.profanity.traySupport     or true;
     autoAwaySupport = config.profanity.autoAwaySupport or true;
   };
 
