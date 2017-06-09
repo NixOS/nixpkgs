97f3009bf86c4db9188e7e6f6443e9ce7bf165b6
Vladimír Čunát <vcunat@gmail.com>
2017-06-09 15:20:41 +0200

    pycairo: fix #26475 - a dead patch download

--- pkgs/development/python-modules/pycairo/default.nix
+++ pkgs/development/python-modules/pycairo/default.nix
@@ -3,7 +3,7 @@
 if (isPyPy) then throw "pycairo not supported for interpreter ${python.executable}" else let
 
   patch_waf = fetchpatch {
-    url = http://www.linuxfromscratch.org/patches/blfs/svn/pycairo-1.10.0-waf_python_3_4-1.patch;
+    url = http://www.linuxfromscratch.org/patches/blfs/8.0/pycairo-1.10.0-waf_python_3_4-1.patch;
     sha256 = "0xfl1i9dips2nykyg91f5h5r3xpk2hp1js1gq5z0hwjr0in55id4";
   };
   patch_waf-py3_5 = ./waf-py3_5.patch;
