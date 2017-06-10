fce7d5638ce0204dd4d15191b28a30871cc2bb13
Alexandre Peyroux <alex@xn--wxa.email>
2017-06-11 00:24:23 +0200

    grammalecte: change order in python-packages.nix

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7126,30 +7126,6 @@ in {
     };
   };
 
-  grammalecte = buildPythonPackage rec {
-    name = "grammalecte";
-    version = "v0.5.17";
-
-    src = pkgs.fetchurl {
-      url = "http://www.dicollecte.org/grammalecte/oxt/Grammalecte-fr-${version}.zip";
-      sha256 = "0ccvj8p8bwvrj8bp370dzjs16pwm755a7364lvk8bp4505n7g0b6";
-    };
-
-    preBuild = "cd ..";
-    postInstall = ''
-    mkdir $out/bin
-    cp $out/cli.py $out/bin/gramalecte
-    chmod a+rx $out/bin/gramalecte
-    '';
-
-   meta = {
-      description = "Grammalecte est un correcteur grammatical open source dédié à la langue française.";
-      homepage = "https://dicollecte.org/grammalecte/";
-      license = licenses.gpl3;
-      maintainers = with maintainers; [ apeyroux ];
-    };
-  };
-  
   grip = buildPythonPackage rec {
     version = "4.3.2";
     name = "grip-${version}";
@@ -11830,6 +11806,31 @@ in {
     };
   };
 
+  grammalecte = buildPythonPackage rec {
+    name = "grammalecte";
+    version = "v0.5.17";
+
+    src = pkgs.fetchurl {
+      url = "http://www.dicollecte.org/grammalecte/oxt/Grammalecte-fr-${version}.zip";
+      sha256 = "0ccvj8p8bwvrj8bp370dzjs16pwm755a7364lvk8bp4505n7g0b6";
+    };
+
+    preBuild = "cd ..";
+    postInstall = ''
+      mkdir $out/bin
+      cp $out/cli.py $out/bin/gramalecte
+      cp $out/server.py $out/bin/gramalected
+      chmod a+rx $out/bin/gramalecte
+      chmod a+rx $out/bin/gramalected
+    '';
+
+   meta = {
+      description = "Grammalecte est un correcteur grammatical open source dédié à la langue française.";
+      homepage = "https://dicollecte.org/grammalecte/";
+      license = licenses.gpl3;
+      maintainers = with maintainers; [ apeyroux ];
+    };
+  };
 
   greenlet = buildPythonPackage rec {
     name = "greenlet-${version}";
