54e060763001e2f8019d54d0cf808772ab806d7d
Tuomas Tynkkynen <tuomas@tuxera.com>
2017-06-11 20:20:13 +0300

    libatomic_ops: Cleanup

--- pkgs/development/libraries/libatomic_ops/default.nix
+++ pkgs/development/libraries/libatomic_ops/default.nix
@@ -1,32 +1,22 @@
 { stdenv, fetchurl, autoconf, automake, libtool }:
-let
-  s = # Generated upstream information
-  rec {
-    baseName="libatomic_ops";
-    version="7.4.4";
-    name="${baseName}-${version}";
-    hash="13vg5fqwil17zpf4hj4h8rh3blzmym693lkdjgvwpgni1mh0l8dz";
-    url="http://www.ivmaisoft.com/_bin/atomic_ops/libatomic_ops-7.4.4.tar.gz";
-    sha256="13vg5fqwil17zpf4hj4h8rh3blzmym693lkdjgvwpgni1mh0l8dz";
-  };
-  
-  buildInputs = stdenv.lib.optionals stdenv.isCygwin [ autoconf automake libtool ];
 
-in stdenv.mkDerivation {
-  inherit (s) name version;
-  inherit buildInputs;
+stdenv.mkDerivation rec {
+  name = "libatomic_ops-${version}";
+  version = "7.4.4";
 
   src = fetchurl {
-    inherit (s) url sha256;
+    url = "http://www.ivmaisoft.com/_bin/atomic_ops/libatomic_ops-${version}.tar.gz";
+    sha256 ="13vg5fqwil17zpf4hj4h8rh3blzmym693lkdjgvwpgni1mh0l8dz";
   };
 
-  preConfigure = if stdenv.isCygwin then ''
-      sed -i -e "/libatomic_ops_gpl_la_SOURCES/a libatomic_ops_gpl_la_LIBADD = libatomic_ops.la" src/Makefile.am
-      ./autogen.sh
-  '' else null;
+  nativeBuildInputs = stdenv.lib.optionals stdenv.isCygwin [ autoconf automake libtool ];
+
+  preConfigure = stdenv.lib.optionalString stdenv.isCygwin ''
+    sed -i -e "/libatomic_ops_gpl_la_SOURCES/a libatomic_ops_gpl_la_LIBADD = libatomic_ops.la" src/Makefile.am
+    ./autogen.sh
+  '';
 
   meta = {
-    inherit (s) version;
     description = ''A library for semi-portable access to hardware-provided atomic memory update operations'';
     license = stdenv.lib.licenses.gpl2Plus ;
     maintainers = [stdenv.lib.maintainers.raskin];
