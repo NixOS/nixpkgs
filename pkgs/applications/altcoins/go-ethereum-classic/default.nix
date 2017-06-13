727ec216dced62f90b4dc3d1cc052a915290904d
Wei Tang <accounts@that.world>
2017-06-14 03:43:33 +0800

    go-ethereum-classic: init at 3.5.0 (#26551)
    thanks

---
+++ pkgs/applications/altcoins/go-ethereum-classic/default.nix
@@ -0,0 +1,24 @@
+{ stdenv, lib, buildGoPackage, fetchgit, fetchhg, fetchbzr, fetchsvn }:
+
+buildGoPackage rec {
+  name = "go-ethereum-classic-${version}";
+  version = "3.5.0";
+  rev = "402c1700fbefb9512e444b32fe12c2d674638ddb";
+
+  goPackagePath = "github.com/ethereumproject/go-ethereum";
+  subPackages = [ "cmd/evm" "cmd/geth" ];
+
+  src = fetchgit {
+    inherit rev;
+    url = "https://github.com/ethereumproject/go-ethereum";
+    sha256 = "15wji12wqcrgsb1glwwz4jv7rsas71bbxh7750iv2phn7jivm0fi";
+  };
+
+  goDeps = ./deps.nix;
+
+  meta = {
+    description = "Golang implementation of Ethereum Classic";
+    homepage = "https://github.com/ethereumproject/go-ethereum";
+    license = with lib.licenses; [ lgpl3 gpl3 ];
+  };
+}
