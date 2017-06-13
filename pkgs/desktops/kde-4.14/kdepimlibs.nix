5ad7d3106e04f961d0c6fdf05b7a9e79c27c9d12
Vladimír Čunát <vcunat@gmail.com>
2017-06-13 14:26:15 +0200

    kde4.kdepimlibs: fixup build by using older cmake
    I have no idea why, but apparently the last cmake update caused:
      kdepimlibs-4.14.3/akonadi/tests/../pastehelper.cpp:343:27:
        fatal error: pastehelper.moc: No such file or directory

--- pkgs/desktops/kde-4.14/kdepimlibs.nix
+++ pkgs/desktops/kde-4.14/kdepimlibs.nix
@@ -1,7 +1,7 @@
 { kde, pkgconfig, boost, cyrus_sasl, gpgme, libical, openldap, prison
 , kdelibs, akonadi, libxslt
 , shared_mime_info, shared_desktop_ontologies, qjson
-, automoc4, cmake, perl
+, automoc4, cmake_2_8, perl
 }:
 
 kde {
@@ -11,7 +11,7 @@ kde {
 
   setOutputFlags = false;
 
-  nativeBuildInputs = [ automoc4 cmake perl pkgconfig ];
+  nativeBuildInputs = [ automoc4 cmake_2_8 perl pkgconfig ];
 
   cmakeFlags = [
     "-DCMAKE_MINIMUM_REQUIRED_VERSION=3.3"
