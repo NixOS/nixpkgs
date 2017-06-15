a7efc9f0cdfec7c9854c4bcbfb848895663c3fe5
Tim Steinbach <tim@nequissimus.com>
2017-06-15 08:53:35 -0400

    linux: 4.9.31 -> 4.9.32

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.9.31";
+  version = "4.9.32";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0mlqr9ngpsg5i2rd2rqmh85y9ga1f3rpfyjv2m82bnhncavdiji8";
+    sha256 = "0r87c65gf9j6shavsr0zviibpbvngccppw5k3wfmmxfjpdsnayf3";
   };
 
   kernelPatches = args.kernelPatches;
