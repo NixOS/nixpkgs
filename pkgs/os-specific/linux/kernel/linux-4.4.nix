07edb44d15ab6daf4af75956694769e108fdb3a0
Tim Steinbach <tim@nequissimus.com>
2017-06-15 08:52:26 -0400

    linux: 4.4.71 -> 4.4.72

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.4.71";
+  version = "4.4.72";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1az64a2qkx4yc0x4snhma9mvgkrgaj66z7pqs1zijcnzsqr5bka4";
+    sha256 = "1s7d6mw4vjsjwdyxbj0p04yq1nsxs3b0b7bi04ilcn2zhldv8cs1";
   };
 
   kernelPatches = args.kernelPatches;
