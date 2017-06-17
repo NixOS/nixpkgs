d1fce4f2c319c9d573242196e26d1ef925069896
Franz Pletz <fpletz@fnordicwalking.de>
2017-06-17 16:47:03 +0200

    linux: 4.9.32 -> 4.9.33
    (cherry picked from commit bbb9182cbc117df5bc7434d1b93d36fd65e5af46)

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.9.32";
+  version = "4.9.33";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0r87c65gf9j6shavsr0zviibpbvngccppw5k3wfmmxfjpdsnayf3";
+    sha256 = "0d2cai3bq1bkmisbsl6l27di765xmzn3lscwray30ldbssijpjpm";
   };
 
   kernelPatches = args.kernelPatches;
