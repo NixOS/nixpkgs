32fd951e26640eb35deb2b4ad0387fe67635adfe
Niklas Hambüchen <mail@nh2.me>
2017-06-18 00:41:48 +0200

    meshlab: 1.3.3 -> 2016.12
    This is based on:
    
    * https://github.com/cnr-isti-vclab/meshlab/pull/68#issuecomment-292308094
    * https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=meshlab

--- pkgs/applications/graphics/meshlab/default.nix
+++ pkgs/applications/graphics/meshlab/default.nix
@@ -1,48 +1,101 @@
-{ stdenv, fetchurl, qt4, bzip2, lib3ds, levmar, muparser, unzip, vcg }:
+{ stdenv, fetchFromGitHub, qt5, bzip2, mesa_glu, lib3ds, levmar, muparser, unzip }:
 
 stdenv.mkDerivation rec {
-  name = "meshlab-1.3.3";
+  version = "2016.12";
+  name = "meshlab-${version}";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/meshlab/meshlab/MeshLab%20v1.3.3/MeshLabSrc_AllInc_v133.tgz";
-    sha256 = "03wqaibfbfag2w1zi1a5z6h546r9d7pg2sjl5pwg24w7yp8rr0n9";
-  };
+  # Should be kept in sync with what the meshlab `version` needs
+  # according to the release notes.
+  vcglibVersion = "1.0.1";
 
-  # I don't know why I need this; without this, the rpath set at the beginning of the
-  # buildPhase gets removed from the 'meshlab' binary
-  dontPatchELF = true;
-
-  patches = [ ./include-unistd.diff ];
+  # Meshlab needs a copy of vcglib *next* to the directory that contains
+  # its own source tree, so we fetch both meshlab and vcglib with `srcs` here.
+  #
+  # Note we use a separate copy of vcglib instead of the one in nixpkgs
+  # because Meshlab typically wants a specific one and we don't want to
+  # constrain the one in nixpkgs to the one that meshlab demands.
+  #
+  # Further, the directory *needs* to be called `vcglib`, because meshlab
+  # has hardcoded paths like `../../../vcglib` in its source.
+  # That's why we rename it in the `buildPhase`.
+  srcs =
+    [
+      (fetchFromGitHub {
+        owner = "cnr-isti-vclab";
+        repo = "meshlab";
+        rev = "v${version}";
+        sha256 = "0srrp7zhi86dsg4zsx1615gr26barz38zdl8s03zq6vm1dgzl3cc";
+      })
+      (fetchFromGitHub {
+        owner = "cnr-isti-vclab";
+        repo = "vcglib";
+        rev = "v${vcglibVersion}";
+        sha256 = "0jh8jc8rn7rci8qr3q03q574fk2hsc3rllysck41j8xkr3rmxz2f";
+      })
+    ];
 
   hardeningDisable = [ "format" ];
 
+  enableParallelBuilding = true;
+
   buildPhase = ''
+    runHook preBuild
+
+    mv vcglib-v${vcglibVersion}-src vcglib
+
+    cd meshlab-v${version}-src
+
     mkdir -p "$out/include"
     export NIX_LDFLAGS="-rpath $out/opt/meshlab $NIX_LDFLAGS"
-    cd meshlab/src
+
+    export QMAKE_FLAGS="-spec linux-g++ CONFIG+=release CONFIG+=qml_release CONFIG+=c++11 QMAKE_CXXFLAGS+=-fPIC QMAKE_CXXFLAGS+=-std=c++11 QMAKE_CXXFLAGS+=-fpermissive QMAKE_CXXFLAGS+=-fopenmp QMAKE_LFLAGS+=-lgomp LIBS+=-L$PWD/lib/linux-g++"
+
+    set -x
+
+    cd src
     pushd external
-    qmake -recursive external.pro
-    make
+
+    qmake -recursive external.pro $QMAKE_FLAGS
+    make ''${enableParallelBuilding:+-j''${NIX_BUILD_CORES} -l''${NIX_BUILD_CORES}}
+
     popd
-    qmake -recursive meshlab_full.pro
-    make
+
+    echo "Fixing io_txt.pro case insensitivity (Unix is case sensitive)"
+    mv plugins_experimental/io_TXT/io_txt.pro plugins_experimental/io_TXT/io_TXT.pro
+
+    qmake -recursive meshlab_full.pro $QMAKE_FLAGS
+    make ''${enableParallelBuilding:+-j''${NIX_BUILD_CORES} -l''${NIX_BUILD_CORES}}
+
+    runHook postBuild
   '';
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/opt/meshlab $out/bin $out/lib
     pushd distrib
     cp -R * $out/opt/meshlab
     popd
     ln -s $out/opt/meshlab/meshlab $out/bin/meshlab
+
+    runHook postInstall
   '';
 
   sourceRoot = ".";
 
-  buildInputs = [ qt4 unzip vcg ];
+  buildInputs = [
+    unzip
+    qt5.qtbase
+    qt5.qttools
+    qt5.qtx11extras
+    qt5.qtscript
+    qt5.qtxmlpatterns
+    mesa_glu
+  ];
 
   meta = {
     description = "System for the processing and editing of unstructured 3D triangular meshes";
-    homepage = http://meshlab.sourceforge.net/;
+    homepage = http://www.meshlab.net/;
     license = stdenv.lib.licenses.gpl2Plus;
     maintainers = with stdenv.lib.maintainers; [viric];
     platforms = with stdenv.lib.platforms; linux;
