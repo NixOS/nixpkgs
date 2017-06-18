6385ef9933c085cb6417948f9fd463a29366c550
Judson <nyarly@gmail.com>
2017-06-17 17:47:06 -0700

    Cockroach v1.0.2

--- pkgs/servers/sql/cockroachdb/default.nix
+++ pkgs/servers/sql/cockroachdb/default.nix
@@ -1,42 +1,49 @@
-{ stdenv, buildGoPackage, fetchurl, cmake, xz, which }:
+{ stdenv, fetchFromGitHub, gcc, go, which, cmake }:
 
-buildGoPackage rec {
+stdenv.mkDerivation rec {
   name = "cockroach-${version}";
   version = "v1.0.2";
 
-  goPackagePath = "github.com/cockroachdb/cockroach";
-
-  src = fetchurl {
-    url = "https://binaries.cockroachdb.com/cockroach-${version}.src.tgz";
-    sha256 = "0xq5lg9a2lxn89lilq3zzcd4kph0a5sga3b5bb9xv6af87igy6zp";
+  src = fetchFromGitHub {
+    name = "cockroach-${version}-src";
+    owner = "cockroachdb";
+    repo = "cockroach";
+    rev = "refs/tags/" + version;
+    sha256 = "1r5dq2q5d1nsvwq7g58ril3976h8p1hclb9sgx02sph9ca6drjak";
+    fetchSubmodules = true;
   };
 
-  buildInputs = [ cmake xz which ];
+  buildRev = "9e3606bd2863ce7a460fd0c842414673d62f0533";
 
-  buildPhase =
-    ''
-      cd $NIX_BUILD_TOP/go/src/${goPackagePath}
-      patchShebangs ./
-      make buildoss
-      cd src/${goPackagePath}
-      for asset in man autocomplete; do
-        ./cockroach gen $asset
-      done
-    '';
+  buildInputs = [ gcc go which cmake ];
 
-  installPhase =
-    ''
-      mkdir -p $bin/{bin,share,etc/bash_completion.d}
-      mv cockroach $bin/bin
-      mv man $bin/share
-      mv cockroach.bash $bin/etc/bash_completion.d
-    '';
+  patches = [./nix.patch];
+
+  dontMakeSourcesWritable = true;
+  sourceRoot = "./src/github.com/cockroachdb/cockroach";
+  postUnpack = ''
+    mkdir -p src/github.com/cockroachdb/
+    chmod -R u+w src
+    chmod -R u+w cockroach-${version}-src
+    mv cockroach-${version}-src src/github.com/cockroachdb/cockroach
+  '';
+
+  buildPhase = ''
+    export GOPATH=$NIX_BUILD_TOP
+    make build
+  '';
+  dontUseCmakeConfigure = true;
+
+  postInstall = ''
+    mkdir $out/bin
+    cp $GOPATH/bin/* $out/bin
+  '';
 
   meta = with stdenv.lib; {
     homepage = https://www.cockroachlabs.com;
     description = "A scalable, survivable, strongly-consistent SQL database";
     license = licenses.asl20;
     platforms = [ "x86_64-linux" ];
-    maintainers = [ maintainers.rushmorem ];
+    maintainers = [ maintainers.rushmorem maintainers.nyarly ];
   };
 }
