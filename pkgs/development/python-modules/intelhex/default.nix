20acfd589937d064807c1d50cf81df6b8c8ecf89
Jörg Thalheim <joerg@thalheim.io>
2017-06-18 22:22:40 +0100

    intelhex: enable tests
    (they seem to work for all somehow)

--- pkgs/development/python-modules/intelhex/default.nix
+++ pkgs/development/python-modules/intelhex/default.nix
@@ -13,13 +13,6 @@ buildPythonPackage rec {
     sha256 = "0k5l1mn3gv1vb0jd24ygxksx8xqr57y1ivgyj37jsrwpzrp167kw";
   };
 
-  # FAIL: test_xrange_longint (intelhex.test.TestXrangeLongInt)
-  # AssertionError: OverflowError not raised
-  #
-  # Harmless test failure:
-  #  https://github.com/bialix/intelhex/issues/14
-  doCheck = false;
-
   meta = {
     homepage = https://github.com/bialix/intelhex;
     description = "Python library for Intel HEX files manipulations";
