fc42ec0a5cf9962a6af9c2da39ff748b30299b67
John Ericson <Ericson2314@Yahoo.com>
2017-06-19 15:30:17 -0400

    mingw-w64: Depend on own headers derivation
    Without this, a `#include <float.h>` resolves incorrectly. Either the
    headers weren't on the include path at all, or they only were for
    local, not system, imports.
    
    What's weird is this used to not be a problem. Not sure what other
    change in e.g. cc-wrapper would affect this.

--- pkgs/os-specific/windows/mingw-w64/default.nix
+++ pkgs/os-specific/windows/mingw-w64/default.nix
@@ -1,6 +1,7 @@
-{ stdenv, callPackage }:
+{ stdenv, callPackage, windows }:
 
 stdenv.mkDerivation {
   inherit (callPackage ./common.nix {}) name src;
+  buildInputs = [ windows.mingw_w64_headers ];
   dontStrip = true;
 }
