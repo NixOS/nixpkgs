9193bcb6b4488a768ca96dc542f16866758a4b74
Peter Hoeg <peter@hoeg.com>
2017-06-19 12:31:42 +0800

    kde-applications: 17.04.1 -> 17.04.2
    use https as the remote server expects it

--- pkgs/applications/kde/default.nix
+++ pkgs/applications/kde/default.nix
@@ -18,7 +18,7 @@ still shows most of the available features is in `./gwenview.nix`.
 # Updates
 
 1. Update the URL in `./fetch.sh`.
-2. Run `./maintainers/scripts/fetch-kde-qt.sh pkgs/desktops/kde-5/applications`
+2. Run `./maintainers/scripts/fetch-kde-qt.sh pkgs/applications/kde`
    from the top of the Nixpkgs tree.
 3. Use `nox-review wip` to check that everything builds.
 4. Commit the changes and open a pull request.
