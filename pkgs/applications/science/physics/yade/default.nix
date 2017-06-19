e6a739a5faf977733edcfabdaea8ac6b80f17c5b
remche <remche@remche.org>
2017-06-19 12:05:58 +0200

    Yade: init at 2017.01a

--- pkgs/applications/science/physics/yade/default.nix
+++ pkgs/applications/science/physics/yade/default.nix
@@ -1,4 +1,6 @@
-with (import <nixpkgs> {});
+{ stdenv, fetchurl, fetchFromGitHub, pkgconfig, cmake, makeWrapper, python27Packages,
+boost, cgal, loki, pythonFull, eigen3_3, bzip2, zlib, openblas, vtk, gmp, gts, metis,
+mpfr, suitesparse, glib, pcre } :
 
 let 
 
@@ -7,10 +9,12 @@ let
     minieigen = pythonPackages.buildPythonPackage rec {
       name = "minieigen";
 
-      src = pkgs.fetchurl {
-        url = "https://github.com/eudoxos/minieigen/archive/master.zip";
-        sha256 = "71d91f716aeb85e6433696fc0754c32d8f4956f3685d12df925c2c9183a2a108";
-      };
+      src = pkgs.fetchFromGitHub {
+        owner = "eudoxos";
+        repo = "minieigen";
+        rev = "7bd0a2e823333477a2172b428a3801d9cae0800f";
+        sha256 = "1jksrbbcxshxx8iqpxkc1y0v091hwji9xvz9w963gjpan4jf61wj";
+        };
 
       buildInputs = [ unzip pythonPackages.boost boost.dev eigen3_3 ];
 
@@ -27,95 +31,94 @@ let
 
 in 
 
- {
 
-    minieigen = minieigen;
+  minieigen = minieigen;
 
-    yade = stdenv.mkDerivation rec {
+  stdenv.mkDerivation rec {
 
-      name = "yade-${version}";
-      version = "2017.01a";
+    name = "yade-${version}";
+    version = "2017.01a";
 
-      meta = {
-        description = "An extensible open-source framework for discrete numerical models, focused on Discrete Element Method";
-        license     = stdenv.lib.licenses.gpl2;
-        homepage    = https://www.yade-dem.org/;
-        platforms   = stdenv.lib.platforms.unix;
-        maintainers = with stdenv.lib.maintainers; [ remche ];
-      };
-
-      nativeBuildInputs = [
-        pkgconfig
-        cmake
-        makeWrapper
-        python2Packages.wrapPython
-        gmp.dev
-        mpfr.dev
-        bzip2.dev
-        zlib.dev 
-        glib.dev
-        pcre.dev
-        boost.dev
-      ];
-
-      buildInputs = [ 
-        boost
-        cgal
-        loki
-        python27Full
-        python27Packages.numpy
-        eigen3_3
-        bzip2
-        zlib
-        openblas
-        vtk
-        gmp
-        gts
-        metis
-        mpfr
-        suitesparse
-        glib
-        pcre
-        minieigen
-      ];
-
-      pythonPath = with pythonPackages; [
-        pygments
-        pexpect
-        decorator
-        numpy
-        ipython
-        ipython_genutils
-        traitlets
-        enum
-        six
-        boost
-        minieigen
-        pillow
-        matplotlib
-      ];
-
-      src = fetchurl
-      {
-        url = "https://launchpad.net/yade/trunk/yade-1.00.0/+download/yade-2017.01a.tar.gz";
-        md5 = "29f83f12f6cdacfd24f1928d90f72906";
-      };
-
-      patches = [ ./cmake.patch ];
-
-      postInstall = ''
-        wrapPythonPrograms
-      '';
-
-      enableParallelBuilding = true;
-
-      system = builtins.currentSystem;
-
-      preConfigure = ''
-        cmakeFlags="-DCMAKE_INSTALL_PREFIX=$out -DENABLE_GUI=OFF -DSUFFIX=-${version}"
-      '';
-
+    meta = {
+      description = "An extensible open-source framework for discrete numerical models, focused on Discrete Element Method";
+      license     = stdenv.lib.licenses.gpl2;
+      homepage    = https://www.yade-dem.org/;
+      platforms   = stdenv.lib.platforms.unix;
+      maintainers = with stdenv.lib.maintainers; [ remche ];
     };
 
- }
+    nativeBuildInputs = [
+      pkgconfig
+      cmake
+      makeWrapper
+      python2Packages.wrapPython
+      gmp.dev
+      mpfr.dev
+      bzip2.dev
+      zlib.dev 
+      glib.dev
+      pcre.dev
+      boost.dev
+    ];
+
+    buildInputs = [ 
+      boost
+      cgal
+      loki
+      python27Full
+      python27Packages.numpy
+      eigen3_3
+      bzip2
+      zlib
+      openblas
+      vtk
+      gmp
+      gts
+      metis
+      mpfr
+      suitesparse
+      glib
+      pcre
+      minieigen
+    ];
+
+    pythonPath = with pythonPackages; [
+      pygments
+      pexpect
+      decorator
+      numpy
+      ipython
+      ipython_genutils
+      traitlets
+      enum
+      six
+      boost
+      minieigen
+      pillow
+      matplotlib
+    ];
+
+    src = fetchurl
+    {
+      url = "https://launchpad.net/yade/trunk/yade-1.00.0/+download/yade-2017.01a.tar.gz";
+      md5 = "29f83f12f6cdacfd24f1928d90f72906";
+    };
+
+    patches = [ ./cmake.patch ];
+
+    postInstall = ''
+      wrapPythonPrograms
+    '';
+
+    enableParallelBuilding = true;
+
+    system = builtins.currentSystem;
+
+    preConfigure = ''
+      cmakeFlags="-DCMAKE_INSTALL_PREFIX=$out -DENABLE_GUI=OFF -DSUFFIX=-${version}"
+    '';
+
+  };
+
+
 
