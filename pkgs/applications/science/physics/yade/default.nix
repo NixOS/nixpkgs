9908a39c283a6acee37ecf09cd6cc605dd626bdd
remche <remche@remche.org>
2017-06-19 12:31:13 +0200

    Yade: init at 2017.01a

--- pkgs/applications/science/physics/yade/default.nix
+++ pkgs/applications/science/physics/yade/default.nix
@@ -112,8 +112,6 @@ in
 
     enableParallelBuilding = true;
 
-    system = builtins.currentSystem;
-
     preConfigure = ''
       cmakeFlags="-DCMAKE_INSTALL_PREFIX=$out -DENABLE_GUI=OFF -DSUFFIX=-${version}"
     '';
