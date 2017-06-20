fcbf28d6b55aef837db6ab885efb4f915bbcf0e5
Franz Pletz <fpletz@fnordicwalking.de>
2017-06-20 02:00:48 +0200

    squid: 3.5.23 -> 3.5.26

--- pkgs/servers/squid/default.nix
+++ pkgs/servers/squid/default.nix
@@ -2,11 +2,11 @@
 , expat, libxml2, openssl }:
 
 stdenv.mkDerivation rec {
-  name = "squid-3.5.23";
+  name = "squid-3.5.26";
 
   src = fetchurl {
     url = "http://www.squid-cache.org/Versions/v3/3.5/${name}.tar.xz";
-    sha256 = "1nqbljph2mbxjy1jzsis5vplfvvc2y6rdlxy609zx4hyyjchqk7s";
+    sha256 = "10m9g7wcik11gv7xqpv9wbkmmfbwxs00s1jm8hgqh63fgp5yx8ds";
   };
 
   buildInputs = [
