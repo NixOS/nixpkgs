3d33e291f9d26b52d5c7992548050c4601a1af12
Thomas Tuegel <ttuegel@mailbox.org>
2017-06-21 08:44:30 -0500

    akonadi-mime: fixup inputs and outputs

--- pkgs/applications/kde/akonadi-mime.nix
+++ pkgs/applications/kde/akonadi-mime.nix
@@ -1,6 +1,6 @@
 {
   mkDerivation, lib, kdepimTeam,
-  extra-cmake-modules,
+  extra-cmake-modules, shared_mime_info,
   akonadi, kdbusaddons, ki18n, kio, kitemmodels, kmime
 }:
 
@@ -10,7 +10,7 @@ mkDerivation {
     license = with lib.licenses; [ gpl2 lgpl21 ];
     maintainers = kdepimTeam;
   };
-  nativeBuildInputs = [ extra-cmake-modules ];
+  nativeBuildInputs = [ extra-cmake-modules shared_mime_info ];
   buildInputs = [ akonadi kdbusaddons ki18n kio kitemmodels kmime ];
   outputs = [ "out" "dev" ];
 }
