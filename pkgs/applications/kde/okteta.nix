46ecfa19085d74cad42b9c0a2a22733cbe497323
Thomas Tuegel <ttuegel@mailbox.org>
2017-06-22 08:30:31 -0500

    okteta: fixup inputs

--- pkgs/applications/kde/okteta.nix
+++ pkgs/applications/kde/okteta.nix
@@ -1,9 +1,7 @@
 {
   mkDerivation, lib,
-  extra-cmake-modules, kdoctools,
-  qtscript, kconfig, kinit, karchive,
-  kcmutils, kconfigwidgets, knewstuff, kparts, qca-qt5,
-  shared_mime_info
+  extra-cmake-modules, kdoctools, shared_mime_info,
+  kconfig, kinit, kcmutils, kconfigwidgets, knewstuff, kparts, qca-qt5, qtscript
 }:
 
 mkDerivation {
@@ -12,10 +10,8 @@ mkDerivation {
     license = with lib.licenses; [ gpl2 ];
     maintainers = with lib.maintainers; [ peterhoeg ];
   };
-  nativeBuildInputs = [ qtscript extra-cmake-modules kdoctools ];
-  buildInputs = [ shared_mime_info ];
-  propagatedBuildInputs = [
-    kconfig kinit kcmutils kconfigwidgets knewstuff kparts qca-qt5
-    karchive
+  nativeBuildInputs = [ extra-cmake-modules kdoctools shared_mime_info ];
+  buildInputs = [
+    kconfig kinit kcmutils kconfigwidgets knewstuff kparts qca-qt5 qtscript
   ];
 }
