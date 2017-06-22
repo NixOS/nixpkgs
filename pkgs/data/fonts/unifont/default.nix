95c98b9bf511d3ff618e4143de6943e2a1784a27
Lancelot SIX <lsix@lancelotsix.com>
2017-06-22 09:03:23 +0200

    unifont: 9.0.06 -> 10.0.01
    See http://lists.gnu.org/archive/html/info-gnu/2017-06/msg00009.html
    for release information

--- pkgs/data/fonts/unifont/default.nix
+++ pkgs/data/fonts/unifont/default.nix
@@ -2,16 +2,16 @@
 
 stdenv.mkDerivation rec {
   name = "unifont-${version}";
-  version = "9.0.06";
+  version = "10.0.01";
 
   ttf = fetchurl {
     url = "mirror://gnu/unifont/${name}/${name}.ttf";
-    sha256 = "0r96giih04din07wlmw8538izwr7dh5v6dyriq13zfn19brgn5z2";
+    sha256 = "0yfz5y4vidb7h6csv6k8h0mx808psdn4vx4842bnyz0fkyhr9h3y";
   };
 
   pcf = fetchurl {
     url = "mirror://gnu/unifont/${name}/${name}.pcf.gz";
-    sha256 = "11ivfzpyz54rbz0cvd437abs6qlv28q0qp37kn27jggxlcpfh8vd";
+    sha256 = "0shlr5804knh14qnv270yzsyfndw6na5ajbx4kvx20gfyxzcsi76";
   };
 
   buildInputs = [ mkfontscale mkfontdir ];
