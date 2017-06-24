241c567a736d9cfb18e748ce5b18586ba71730d3
Volth <volth@webmaster.ms>
2017-06-24 14:02:32 +0000

    use fetchpatch

--- pkgs/tools/misc/rrdtool/default.nix
+++ pkgs/tools/misc/rrdtool/default.nix
@@ -1,15 +1,24 @@
-{ fetchFromGitHub, stdenv, gettext, perl, pkgconfig, libxml2, pango, cairo, groff
+{ fetchFromGitHub, fetchpatch, stdenv, gettext, perl, pkgconfig, libxml2, pango, cairo, groff
 , tcl-8_5, darwin }:
 
 stdenv.mkDerivation rec {
-  name = "rrdtool-1.7.0.2017-06-23";
+  version = "1.7.0";
+  name = "rrdtool-${version}.2017-06-11";
 
   src = fetchFromGitHub {
     owner = "oetiker";
     repo = "rrdtool-1.x";
-    rev = "e0eb6257ad9b150636ef0ccc3797b71e6a3e822a";
-    sha256 = "0szjnvwailsianlhxvvgzr97d78nvkkid3imcfvawy5w4x6bvasv";
+    rev = "v${version}";
+    sha256 = "1p8x717w4cgjcyr5kcd0s6vf1n94arif1w9pkbcl708j7gdi5gx1";
   };
+
+  patches = [
+    # fix regression https://github.com/oetiker/rrdtool-1.x/issues/794
+    (fetchpatch {
+      url = "https://github.com/oetiker/rrdtool-1.x/compare/0f28f99...f1edd12.patch";
+      sha256 = "10g56zy0rdjpv3kvvmf6vvaysmla05wi8byy3l0xrz2x8m02ylqq";
+    })
+  ];
 
   nativeBuildInputs = [ pkgconfig ];
 
