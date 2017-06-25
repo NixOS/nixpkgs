6e8c3fae64745a4d3439d43c3027e970c7b942f1
Linus Heckemann <git@sphalerite.org>
2017-06-25 17:59:23 +0100

    GNOME: 3.22 -> 3.24

--- pkgs/development/libraries/appstream-glib/default.nix
+++ pkgs/development/libraries/appstream-glib/default.nix
@@ -1,23 +1,23 @@
 { stdenv, fetchFromGitHub, pkgconfig, gettext, gtk3, intltool, glib
 , gtk_doc, autoconf, automake, libtool, libarchive, libyaml
 , gobjectIntrospection, sqlite, libsoup, gcab, attr, acl, docbook_xsl
-, libuuid, json_glib, autoconf-archive
+, libuuid, json_glib, autoconf-archive, gperf
 }:
 
 stdenv.mkDerivation rec {
-  name = "appstream-glib-0.6.3";
+  name = "appstream-glib-0.6.13";
 
   src = fetchFromGitHub {
     owner = "hughsie";
     repo = "appstream-glib";
     rev = stdenv.lib.replaceStrings ["." "-"] ["_" "_"] name;
-    sha256 = "12l0vzhi9vpyrnf7vrpq21rb26mb6yskp5zgngdjyjanwhzmc617";
+    sha256 = "1csscszdp1imd0hiib6xx7mfylfl1r64xmma14p5jq1yprprpa9h";
   };
 
   nativeBuildInputs = [ autoconf automake libtool pkgconfig intltool autoconf-archive ];
   buildInputs = [ glib gtk_doc gettext sqlite libsoup
                   gcab attr acl docbook_xsl libuuid json_glib
-                  libarchive libyaml gobjectIntrospection ];
+                  libarchive libyaml gobjectIntrospection gperf ];
   propagatedBuildInputs = [ gtk3 ];
   configureScript = "./autogen.sh";
 
