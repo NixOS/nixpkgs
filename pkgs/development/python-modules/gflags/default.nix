37c9ee94d92ea802dd35ef717c9c550e40ef39d3
Jörg Thalheim <joerg@thalheim.io>
2017-06-26 09:19:44 +0100

    python-gflags: fix tests

--- pkgs/development/python-modules/gflags/default.nix
+++ pkgs/development/python-modules/gflags/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib, buildPythonPackage, fetchPypi, six, pytest }:
 
 buildPythonPackage rec {
   version = "3.1.1";
@@ -9,6 +9,16 @@ buildPythonPackage rec {
     pname = "python-gflags";
     sha256 = "0qvcizlz6r4511kl4jlg6fr34y1ka956dr2jj1q0qcklr94n9zxa";
   };
+
+  buildInputs = [ pytest ];
+
+  propagatedBuildInputs = [ six ];
+
+  checkPhase = ''
+    # clashes with our pythhon wrapper (which is in argv0)
+    # AssertionError: 'gflags._helpers_test' != 'nix_run_setup.py'
+    py.test -k 'not testGetCallingModule'
+  '';
 
   meta = {
     homepage = https://github.com/google/python-gflags;
