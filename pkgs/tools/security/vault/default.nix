ca16df17bd72fe8516a4a2dc6a18d3e9f86f89b0
Volth <volth@webmaster.ms>
2017-06-29 02:02:13 +0000

    vault: build as Makefile project, not as Go package

--- pkgs/tools/security/vault/default.nix
+++ pkgs/tools/security/vault/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildGoPackage, fetchFromGitHub }:
+{ stdenv, fetchFromGitHub, go, gox }:
 
 let
   vaultBashCompletions = fetchFromGitHub {
@@ -7,11 +7,9 @@ let
     rev = "e2f59b64be1fa5430fa05c91b6274284de4ea77c";
     sha256 = "10m75rp3hy71wlmnd88grmpjhqy0pwb9m8wm19l0f463xla54frd";
   };
-in buildGoPackage rec {
+in stdenv.mkDerivation rec {
   name = "vault-${version}";
   version = "0.7.3";
-
-  goPackagePath = "github.com/hashicorp/vault";
 
   src = fetchFromGitHub {
     owner = "hashicorp";
@@ -20,14 +18,21 @@ in buildGoPackage rec {
     sha256 = "15wj1pfgzwzjfrqy7b5bx4y9f0hbpqlfif58l5xamwm88229qk4m";
   };
 
-  buildFlagsArray = ''
-    -ldflags=
-      -X github.com/hashicorp/vault/version.GitCommit=${version}
+  nativeBuildInputs = [ go gox ];
+
+  buildPhase = ''
+    substituteInPlace scripts/build.sh --replace 'git rev-parse HEAD' 'echo ${src.rev}'
+
+    mkdir -p src/github.com/hashicorp
+    ln -s $(pwd) src/github.com/hashicorp/vault
+
+    GOPATH=$(pwd) make
   '';
 
-  postInstall = ''
-    mkdir -p $bin/share/bash-completion/completions/
-    cp ${vaultBashCompletions}/vault-bash-completion.sh $bin/share/bash-completion/completions/vault
+  installPhase = ''
+    mkdir -p $out/bin $out/share/bash-completion/completions
+    cp pkg/*/* $out/bin/
+    cp ${vaultBashCompletions}/vault-bash-completion.sh $out/share/bash-completion/completions/vault
   '';
 
   meta = with stdenv.lib; {
