fd488c4a2ad10dee98e8ce8b86a73184fba80abd
mimadrid <mimadrid@ucm.es>
2017-07-02 10:27:20 +0200

    vifm: 0.8.2 -> 0.9

--- pkgs/applications/misc/vifm/default.nix
+++ pkgs/applications/misc/vifm/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   name = "vifm-${version}";
-  version = "0.8.2";
+  version = "0.9";
 
   src = fetchurl {
-    url = "mirror://sourceforge/project/vifm/vifm/${name}.tar.bz2";
-    sha256 = "07r15kq7kjl3a41sd11ncpsii866xxps4f90zh3lv8jqcrv6silb";
+    url = "https://github.com/vifm/vifm/releases/download/v${version}/vifm-${version}.tar.bz2";
+    sha256 = "1zd72vcgir3g9rhs2iyca13qf5fc0b1f22y20f5gy92c3sfwj45b";
   };
 
   nativeBuildInputs = [ pkgconfig ];
