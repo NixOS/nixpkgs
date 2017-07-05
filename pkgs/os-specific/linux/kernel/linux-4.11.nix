00f0f7e9f60924881b8d2382c96f525df95857c2
Tim Steinbach <tim@nequissimus.com>
2017-07-05 12:49:56 -0400

    linux: 4.11.8 -> 4.11.9

--- pkgs/os-specific/linux/kernel/linux-4.11.nix
+++ pkgs/os-specific/linux/kernel/linux-4.11.nix
@@ -1,12 +1,12 @@
 { stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.11.8";
+  version = "4.11.9";
   extraMeta.branch = "4.11";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1f4x4wym6kr57zhjpmvj9p7z2j43zcvpyvkh2nnhsb4yxg5qszcf";
+    sha256 = "0q60690hmqhz2x3v6qyjq7lhp2j99dcldvd46myc9ggp78d93j1z";
   };
 
   kernelPatches = args.kernelPatches;
