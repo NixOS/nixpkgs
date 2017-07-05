ce09887b3c04a6eacddcaa5cb5a94fb59c479379
Lengyel Balázs <balazs.lengyel@tuwien.ac.at>
2017-07-05 14:19:45 +0200

    fix #23912

--- nixos/modules/system/boot/tmp.nix
+++ nixos/modules/system/boot/tmp.nix
@@ -2,6 +2,14 @@
 
 with lib;
 
+let
+
+  conf = config.boot.tmpOnTmpfs;
+
+  size = if isString conf then "size=${conf}" else "";
+
+in
+
 {
 
   ###### interface
@@ -17,10 +25,11 @@ with lib;
     };
 
     boot.tmpOnTmpfs = mkOption {
-      type = types.bool;
+      type = with types; either bool string;
       default = false;
       description = ''
          Whether to mount a tmpfs on <filename>/tmp</filename> during boot.
+         Setting this to `true` is equivalent to 50% of available memory. For details about valid strings, see `man mount`.
       '';
     };
 
@@ -30,8 +39,13 @@ with lib;
 
   config = {
 
-    systemd.additionalUpstreamSystemUnits = optional config.boot.tmpOnTmpfs "tmp.mount";
+    systemd.mounts = mkIf (conf != false) [{
+      what = "tmpfs";
+      where = "/tmp";
+      type = "tmpfs";
+      options = concatStringsSep "," (lists.filter (x : x != "") [ "mode=1777" "strictatime" "nosuid" "nodev" size ]);
 
+    }];
     systemd.tmpfiles.rules = optional config.boot.cleanTmpDir "D! /tmp 1777 root root";
 
   };
