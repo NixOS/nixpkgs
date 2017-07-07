0fb8456b1316797cc6cd45814b3250e432eeef77
Pascal Bach <pascal.bach@nextrem.ch>
2017-07-08 00:11:44 +0200

    minio service: add additional config options
    Set access and secret key and disable browser.
    Tests extended to do real operations against minio.

--- nixos/tests/minio.nix
+++ nixos/tests/minio.nix
@@ -4,8 +4,15 @@ import ./make-test.nix ({ pkgs, ...} : {
     maintainers = [ bachp ];
   };
 
-  machine = { config, pkgs, ... }: {
-    services.minio.enable = true;
+  nodes = {
+    machine = { config, pkgs, ... }: {
+      services.minio = {
+        enable = true;
+        accessKey = "BKIKJAA5BMMU2RHO6IBB";
+        secretKey = "V7f1CwQqAcwo80UEIJEjc5gVQUSSx5ohQ9GSrr12";
+      };
+      environment.systemPackages = [ pkgs.minio-client ];
+    };
   };
 
   testScript =
@@ -14,6 +21,12 @@ import ./make-test.nix ({ pkgs, ...} : {
       $machine->waitForUnit("minio.service");
       $machine->waitForOpenPort(9000);
       $machine->succeed("curl --fail http://localhost:9000/minio/index.html");
+
+      # Create a test bucket on the server
+      $machine->succeed("mc config host add minio http://localhost:9000 BKIKJAA5BMMU2RHO6IBB V7f1CwQqAcwo80UEIJEjc5gVQUSSx5ohQ9GSrr12 S3v4");
+      $machine->succeed("mc mb minio/test-bucket");
+      $machine->succeed("mc ls minio") =~ /test-bucket/ or die;
       $machine->shutdown;
+
     '';
 })
