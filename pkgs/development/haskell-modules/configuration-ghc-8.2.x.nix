2180d2c1180b04b14877cccad841fdb06941255a
Peter Simons <simons@cryp.to>
2017-07-07 12:24:06 +0200

    haskell-jailbreak-cabal: use stable version from ghc-8.0.x in ghc-8.2.x package set
    The version compiled with Cabal 2.x breaks many packages, like doctest and
    hashable. This needs further investigation.

--- pkgs/development/haskell-modules/configuration-ghc-8.2.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.2.x.nix
@@ -39,8 +39,8 @@ self: super: {
   # cabal-install can use the native Cabal library.
   cabal-install = super.cabal-install.override { Cabal = null; };
 
-  # jailbreak-cabal can use the native Cabal library.
-  jailbreak-cabal = super.jailbreak-cabal.override { Cabal = null; };
+  # jailbreak-cabal doesn't seem to work right with the native Cabal version.
+  jailbreak-cabal = pkgs.haskellPackages.jailbreak-cabal;
 
   # https://github.com/bmillwood/applicative-quoters/issues/6
   applicative-quoters = appendPatch super.applicative-quoters (pkgs.fetchpatch {
