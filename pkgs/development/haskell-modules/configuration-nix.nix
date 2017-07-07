47feb4531341c08502ef4755f8c7cca2ca0206ec
Peter Simons <simons@cryp.to>
2017-07-07 17:32:28 +0200

    haskell-hsyslog: update version override
    And move it from configuration-nix to configuration-common.

--- pkgs/development/haskell-modules/configuration-nix.nix
+++ pkgs/development/haskell-modules/configuration-nix.nix
@@ -470,7 +470,4 @@ self: super: builtins.intersectAttrs super {
   # Needs a newer version of brick than lts-8.x provides.
   hledger-iadd = super.hledger-iadd.override { brick = self.brick_0_19; };
 
-  # Needs a newer version of hsyslog than lts-8.x provides.
-  logging-facade-syslog = super.logging-facade-syslog.override { hsyslog = self.hsyslog_5; };
-
 }
