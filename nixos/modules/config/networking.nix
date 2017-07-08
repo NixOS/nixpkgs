2f9799399255bdacc028092b904f6ea0afc7a09a
Valentin Shirokov <regellosigkeitsaxiom@yandex.ru>
2017-07-09 00:28:05 +0300

    Documentation fixes

--- nixos/modules/config/networking.nix
+++ nixos/modules/config/networking.nix
@@ -34,12 +34,12 @@ in
       default = {};
       example = ''
         {
-          "localhost" = [ "foo.bar" ];
+          "127.0.0.1" = [ "foo.bar.baz" ];
           "192.168.0.2" = [ "fileserver.local" "nameserver.local" ];
         };
       '';
       description = ''
-        Locally defined maps of IP addresses to hostnames'
+        Locally defined maps of hostnames to IP addresses.
       '';
     };
 
