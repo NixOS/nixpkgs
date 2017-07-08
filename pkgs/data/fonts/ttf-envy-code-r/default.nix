5172fd68d3fcdb1f5f39b685d7bd43ba2221e0ec
Vladimír Čunát <vcunat@gmail.com>
2017-07-08 10:18:53 +0200

    ttf-envy-code-r: nitpick changes
    - start version with 0. to get actual version semantics
    - better location for readme.txt
    - widen platforms to unix

--- pkgs/data/fonts/ttf-envy-code-r/default.nix
+++ pkgs/data/fonts/ttf-envy-code-r/default.nix
@@ -1,8 +1,10 @@
 { stdenv, fetchurl, unzip }:
-
-stdenv.mkDerivation rec {
-  name = "ttf-envy-code-r";
+let
+  pname = "ttf-envy-code-r";
   version = "PR7";
+in
+stdenv.mkDerivation rec {
+  name = "${pname}-0.${version}";
 
   src = fetchurl {
     url = "http://download.damieng.com/fonts/original/EnvyCodeR-${version}.zip";
@@ -15,14 +17,14 @@ stdenv.mkDerivation rec {
     for f in *.ttf; do
         install -Dm 644 "$f" "$out/share/fonts/truetype/$f"
     done
-    install -Dm 644 Read\ Me.txt "$out/share/doc/readme.txt"
+    install -Dm 644 Read\ Me.txt "$out/share/doc/${pname}/readme.txt"
   '';
 
   meta = with stdenv.lib; {
     homepage = http://damieng.com/blog/tag/envy-code-r;
     description = "Free scalable coding font by DamienG";
     license = licenses.unfree;
-    platforms = platforms.linux;
+    platforms = platforms.unix;
     maintainers = [ maintainers.lyt ];
   };
 }
