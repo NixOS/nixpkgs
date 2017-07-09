3fc83370e0cdae7cebf73d74d71fa71b68e3d790
hectorj <hector.jusforgues@gmail.com>
2017-07-09 12:44:34 +0200

    zipkin-server: init at 1.28.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11560,6 +11560,8 @@ with pkgs;
   zabbix20 = callPackage ../servers/monitoring/zabbix/2.0.nix { };
   zabbix22 = callPackage ../servers/monitoring/zabbix/2.2.nix { };
 
+  zipkin-server = callPackage ../servers/monitoring/zipkin { };
+
 
   ### OS-SPECIFIC
 
