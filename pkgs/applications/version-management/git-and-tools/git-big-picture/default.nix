4ae629dd9191bd3123e90c31fa28aa52334f3553
Niklas Thörne <notrupertthorne@gmail.com>
2017-07-09 21:21:51 +0200

    git-big-picture: init at 0.9.0
    Updated after review comments.

--- pkgs/applications/version-management/git-and-tools/git-big-picture/default.nix
+++ pkgs/applications/version-management/git-and-tools/git-big-picture/default.nix
@@ -1,23 +1,30 @@
-{ fetchgit, python2Packages, stdenv, git, graphviz }:
+{ fetchFromGitHub, python2Packages, stdenv, git, graphviz }:
 
 python2Packages.buildPythonApplication rec {
-    pname = "git-big-picture";
-    version = "0.9.0";
+  pname = "git-big-picture";
+  version = "0.9.0";
 
-    name = "${pname}-${version}";
+  name = "${pname}-${version}";
 
-    src = fetchgit {
-      url = "https://github.com/esc/${pname}.git";
-      rev = "fbe3b9504e255da859067fd58e90d849d63e5381";
-      sha256 = "1h283gzs4nx8lrarmr454zza52cilmnbdrqn1n33v3cn1rayl3c9";
-    };
+  src = fetchFromGitHub {
+    owner = "esc";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "1h283gzs4nx8lrarmr454zza52cilmnbdrqn1n33v3cn1rayl3c9";
+  };
 
-  propagatedBuildInputs = [ git graphviz ];
+  buildInputs = [ git graphviz ];
+
+  postFixup = ''
+    wrapProgram $out/bin/git-big-picture \
+      --prefix PATH ":" ${ stdenv.lib.makeBinPath buildInputs  }
+    '';
 
   meta = {
     description = "Tool for visualization of Git repositories.";
     homepage = https://github.com/esc/git-big-picture;
     license = stdenv.lib.licenses.gpl3;
     platforms = stdenv.lib.platforms.linux;
+    maintainers = [ stdenv.lib.maintainers.nthorne ];
   };
 }
