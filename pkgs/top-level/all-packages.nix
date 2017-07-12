07e7527fdb14769b6ecb39a787fa2c053895f9f0
Andrew Cann <shum@canndrew.org>
2017-07-12 21:23:40 +0800

    undaemonize: init at 2017-07-11 (#27325)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19127,4 +19127,6 @@ with pkgs;
   linode-cli = callPackage ../tools/virtualization/linode-cli { };
 
   hss = callPackage ../tools/networking/hss {};
+
+  undaemonize = callPackage ../tools/system/undaemonize {};
 }
