45a2534459ea6b2ccc4c015236a0a690304621ea
Tim Steinbach <tim@nequissimus.com>
2017-07-13 08:40:08 -0400

    linux-copperhead: 4.12.e -> 4.12.1.a

--- pkgs/os-specific/linux/kernel/linux-hardened-copperhead.nix
+++ pkgs/os-specific/linux/kernel/linux-hardened-copperhead.nix
@@ -1,9 +1,9 @@
 { stdenv, hostPlatform, fetchFromGitHub, perl, buildLinux, ... } @ args:
 
 let
-  version = "4.12";
-  revision = "e";
-  sha256 = "1zxmfddj3nx8fd4nfxi0sxa3j9byq1dkp05plnknmi45b3ji57zh";
+  version = "4.12.1";
+  revision = "a";
+  sha256 = "0fjw5fmxpvdhfqkr4lcpmqw8xxj92q19ya8q48yhxvv149ahcvhq";
 in
 
 import ./generic.nix (args // {
