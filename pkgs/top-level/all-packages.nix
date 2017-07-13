e10d7655dcc1f4d6ddc97b740ba4de83d345d3bf
Peter Hoeg <peter@hoeg.com>
2017-07-13 13:59:23 +0800

    anydesk: init at 2.9.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1252,6 +1252,8 @@ with pkgs;
 
   ori = callPackage ../tools/backup/ori { };
 
+  anydesk = callPackage ../applications/networking/remote/anydesk { };
+
   atool = callPackage ../tools/archivers/atool { };
 
   bsc = callPackage ../tools/compression/bsc { };
