NixOS/nixpkgs

commit b917a8760ef63d28cb23b76ecfe000e9b0bd1f3c

slurm: 15-08-5-1 -> 17.02.6, slurm-llnl -> slurm

pkgs/top-level/all-packages.nix
@@ -11485,9 +11485,9 @@ with pkgs;
 
   storm = callPackage ../servers/computing/storm { };
 
-  slurm-llnl = callPackage ../servers/computing/slurm { gtk2 = null; };
+  slurm = callPackage ../servers/computing/slurm { gtk2 = null; };
 
-  slurm-llnl-full = appendToName "full" (callPackage ../servers/computing/slurm { });
+  slurm-full = appendToName "full" (callPackage ../servers/computing/slurm { });
 
   systemd-journal2gelf = callPackage ../tools/system/systemd-journal2gelf { };
 

nixos/modules/services/computing/slurm/slurm.nix
@@ -36,9 +36,9 @@ in
 
       package = mkOption {
         type = types.package;
-        default = pkgs.slurm-llnl;
-        defaultText = "pkgs.slurm-llnl";
-        example = literalExample "pkgs.slurm-llnl-full";
+        default = pkgs.slurm;
+        defaultText = "pkgs.slurm";
+        example = literalExample "pkgs.slurm-full";
         description = ''
           The package to use for slurm binaries.
         '';

pkgs/servers/computing/slurm/default.nix
@@ -1,19 +1,24 @@
-{ stdenv, fetchurl, pkgconfig, curl, python, munge, perl, pam, openssl
+{ stdenv, fetchurl, pkgconfig, libtool, curl, python, munge, perl, pam, openssl
 , ncurses, mysql, gtk2, lua, hwloc, numactl
 }:
 
 stdenv.mkDerivation rec {
-  name = "slurm-llnl-${version}";
-  version = "15-08-5-1";
+  name = "slurm-${version}";
+  version = "17.02.6";
 
   src = fetchurl {
-    url = "https://github.com/SchedMD/slurm/archive/slurm-${version}.tar.gz";
-    sha256 = "05si1cn7zivggan25brsqfdw0ilvrlnhj96pwv16dh6vfkggzjr1";
+    url = "https://www.schedmd.com/downloads/latest/slurm-17.02.6.tar.bz2";
+    sha256 = "1sp4xg15jc569r6dh61svgk2fmy3ndcgr5358yryajslf1w14mzh";
   };
 
   outputs = [ "out" "dev" ];
 
-  nativeBuildInputs = [ pkgconfig ];
+  # nixos test fails to start slurmd with 'undefined symbol: slurm_job_preempt_mode'
+  # https://groups.google.com/forum/#!topic/slurm-devel/QHOajQ84_Es
+  # this doesn't fix tests completely at least makes slurmd to launch
+  hardeningDisable = [ "bindnow" ];
+
+  nativeBuildInputs = [ pkgconfig libtool ];
   buildInputs = [
     curl python munge perl pam openssl mysql.lib ncurses gtk2 lua hwloc numactl
   ];
@@ -25,8 +30,8 @@ stdenv.mkDerivation rec {
     ] ++ stdenv.lib.optional (gtk2 == null)  "--disable-gtktest";
 
   preConfigure = ''
-    substituteInPlace ./doc/html/shtml2html.py --replace "/usr/bin/env python" "${python.interpreter}"
-    substituteInPlace ./doc/man/man2html.py --replace "/usr/bin/env python" "${python.interpreter}"
+    patchShebangs ./doc/html/shtml2html.py
+    patchShebangs ./doc/man/man2html.py
   '';
 
   postInstall = ''

commit ba8fe290ecadae3b6abc375377937302cfd67ff2

slurm: add alias attribute for backward compatibility
Fixes: b917a8760 ('slurm: 15-08-5-1 -> 17.02.6, slurm-llnl -> slurm')

pkgs/top-level/all-packages.nix
@@ -11490,8 +11490,10 @@ with pkgs;
   storm = callPackage ../servers/computing/storm { };
 
   slurm = callPackage ../servers/computing/slurm { gtk2 = null; };
+  slurm-llnl = slurm; # renamed July 2017
 
   slurm-full = appendToName "full" (callPackage ../servers/computing/slurm { });
+  slurm-llnl-full = slurm-full; # renamed July 2017
 
   systemd-journal2gelf = callPackage ../tools/system/systemd-journal2gelf { };
 