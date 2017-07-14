3597b5f7fcb31b54131bfbcccf397aa82c69215f
Rob Vermaas <rob.vermaas@gmail.com>
2017-07-14 10:51:37 +0000

    nlopt: build without octave support, as it is broken with octave 4.x+

--- pkgs/development/libraries/nlopt/default.nix
+++ pkgs/development/libraries/nlopt/default.nix
@@ -1,4 +1,4 @@
-{ fetchurl, stdenv, octave ? null }:
+{ fetchurl, stdenv}:
 
 stdenv.mkDerivation rec {
   name = "nlopt-2.4.2";
@@ -8,24 +8,13 @@ stdenv.mkDerivation rec {
     sha256 = "12cfkkhcdf4zmb6h7y6qvvdvqjs2xf9sjpa3rl3bq76px4yn76c0";
   };
 
-  buildInputs = [ octave ];
-
-  configureFlags = "--with-cxx --enable-shared --with-pic --without-guile --without-python
-  --without-matlab " +
-    stdenv.lib.optionalString (octave != null) ("--with-octave " +
-        "M_INSTALL_DIR=$(out)/${octave.sitePath}/m " +
-        "OCT_INSTALL_DIR=$(out)/${octave.sitePath}/oct ");
-
-  preConfigure = ''
-    find octave -name '*.cc' | xargs sed -i 's|Octave_map|octave_map|g'
-  '';
+  configureFlags = "--with-cxx --enable-shared --with-pic --without-guile --without-python --without-matlab ";
 
   meta = {
     homepage = "http://ab-initio.mit.edu/nlopt/";
     description = "Free open-source library for nonlinear optimization";
     license = stdenv.lib.licenses.lgpl21Plus;
     hydraPlatforms = stdenv.lib.platforms.linux;
-    broken = true;              # cannot cope with Octave 4.x
   };
 
 }
