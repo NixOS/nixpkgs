66cb8f484ff32a23b71e3a430697cd876e794e06
adisbladis <adis@blad.is>
2017-07-14 11:44:24 +0800

    pythonPackages.zc_lockfile: 1.0.2 -> 1.2.1

---
+++ pkgs/development/python-modules/zc_lockfile/default.nix
@@ -0,0 +1,19 @@
+{ buildPythonPackage, fetchPypi, stdenv }:
+
+buildPythonPackage rec {
+  pname = "zc.lockfile";
+  version = "1.2.1";
+  name = "${pname}-${version}";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "11db91ada7f22fe8aae268d4bfdeae012c4fe655f66bbb315b00822ec00d043e";
+  };
+
+  meta = with stdenv.lib; {
+    description = "Inter-process locks";
+    homepage =  http://www.python.org/pypi/zc.lockfile;
+    license = licenses.zpt20;
+    maintainers = [ maintainers.goibhniu ];
+  };
+}
