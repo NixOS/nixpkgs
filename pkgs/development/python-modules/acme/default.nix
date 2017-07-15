704658373d5d1d9074023b07d253173127816d58
wisut hantanong <wizzup@gmail.com>
2017-07-15 13:18:20 +0700

    python.pkgs.acme  move to separate expression

---
+++ pkgs/development/python-modules/acme/default.nix
@@ -0,0 +1,21 @@
+{
+stdenv, lib, buildPythonPackage, fetchPypi,
+certbot, nose, cryptography, pyasn1, pyopenssl, pyRFC3339, pytz,
+requests, six, werkzeug, mock, ndg-httpsclient
+}:
+
+buildPythonPackage rec {
+  inherit (certbot) src version;
+
+  pname = "acme";
+  name = "${pname}-${version}";
+
+  propagatedBuildInputs = [
+    cryptography pyasn1 pyopenssl pyRFC3339 pytz requests six werkzeug mock
+    ndg-httpsclient
+  ];
+
+  buildInputs = [ nose ];
+
+  postUnpack = "sourceRoot=\${sourceRoot}/acme";
+}
