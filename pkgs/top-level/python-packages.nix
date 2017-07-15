0fa5f704be458867fa893acc31c4aea3ea0592d3
wisut hantanong <wizzup@gmail.com>
2017-07-15 13:05:39 +0700

    python.pkgs.vega  move to separate expression

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -265,31 +265,8 @@ in {
   aafigure = callPackage ../development/python-modules/aafigure { };
 
   altair = callPackage ../development/python-modules/altair { };
-  vega = buildPythonPackage rec {
-    name = "vega-0.4.4";
 
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/v/vega/${name}.tar.gz";
-      sha256 = "08k92afnk0bivm07h1l5nh26xl2rfp7qn03aq17q1hr3fs5r6cdm";
-    };
-
-    buildInputs = [ self.pytest ];
-    propagatedBuildInputs = with self; [ jupyter_core pandas ];
-
-    meta = {
-      description = " An IPython/Jupyter widget for Vega and Vega-Lite.";
-      longDescription = ''
-        To use this you have to enter a nix-shell with vega. Then run:
-
-        jupyter nbextension install --user --py vega
-        jupyter nbextension enable --user vega
-      '';
-      homepage = https://github.com/vega/ipyvega;
-      license = licenses.bsd3;
-      platforms = platforms.linux;
-      maintainers = with maintainers; [ teh ];
-    };
-  };
+  vega = callPackage ../development/python-modules/vega { };
 
   acme = buildPythonPackage rec {
     inherit (pkgs.certbot) src version;
