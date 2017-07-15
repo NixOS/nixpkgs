a840b2ee6019545e8f8db0924321b9349f304f2b
wisut hantanong <wizzup@gmail.com>
2017-07-15 14:07:43 +0700

    python.pkgs.yamllint  move to separate expression

---
+++ pkgs/development/python-modules/yamllint/default.nix
@@ -0,0 +1,26 @@
+{
+stdenv, lib, buildPythonPackage, fetchPypi,
+nose, pyyaml
+}:
+
+buildPythonPackage rec {
+  pname = "yamllint";
+  version = "0.5.2";
+  name = "${pname}-${version}";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "0brdy1crhfng10hlw0420bv10c2xnjk8ndnhssybkzym47yrzg84";
+  };
+
+  buildInputs = [ nose ];
+
+  propagatedBuildInputs = [  pyyaml ];
+
+  meta = {
+    description = "A linter for YAML files";
+    homepage = "https://github.com/adrienverge/yamllint";
+    license = lib.licenses.gpl3;
+    maintainers = with lib.maintainers; [ mikefaille ];
+  };
+}
