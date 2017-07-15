0f09b057944dd3fdee3422ba46d66d5f28298f3e
Vladimír Čunát <vcunat@gmail.com>
2017-07-15 11:39:46 +0200

    libpng: 1.6.29 -> 1.6.30

--- pkgs/development/libraries/libpng/default.nix
+++ pkgs/development/libraries/libpng/default.nix
@@ -5,13 +5,13 @@
 assert zlib != null;
 
 let
-  version = "1.6.29";
-  patchVersion = "1.6.26";
+  version = "1.6.30";
+  patchVersion = "1.6.30";
   # patchVersion = version;
-  sha256 = "0fgjqp7x6jynacmqh6dj72cn6nnf6yxjfqqqfsxrx0pyx22bcia2";
+  sha256 = "0rin6la7q03vb7wsafhlvzqri1v9ky30g4ljsfcwa37pzwpk6z16";
   patch_src = fetchurl {
     url = "mirror://sourceforge/libpng-apng/libpng-${patchVersion}-apng.patch.gz";
-    sha256 = "0b6p2k4afvhk1svargpllcvhxb4g3p857wkqk85cks0yv42ckph1";
+    sha256 = "06nrcp2n77f563hch8g9gv62jg894mvya6zizj5fsmbqzaqmjqqs";
   };
   whenPatched = stdenv.lib.optionalString apngSupport;
 
