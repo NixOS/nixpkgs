37d87343bff111152aad2a5f486c581939d37a12
Jörg Thalheim <joerg@thalheim.io>
2017-07-18 22:37:56 +0100

    python-gnupg: move out of python-packages

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -515,33 +515,7 @@ in {
     };
   };
 
-  python-gnupg = buildPythonPackage rec {
-    name    = "python-gnupg-${version}";
-    version = "0.4.1";
-
-    src = pkgs.fetchurl {
-      url    = "mirror://pypi/p/python-gnupg/${name}.tar.gz";
-      sha256 = "06hfw9cmiw5306fyisp3kzg1hww260qzip829g7y7pj1mwpb0izg";
-    };
-
-    propagatedBuildInputs = [ pkgs.gnupg1 ];
-
-    # Let's make the library default to our gpg binary
-    patchPhase = ''
-      substituteInPlace gnupg.py \
-        --replace "gpgbinary='gpg'" "gpgbinary='${pkgs.gnupg1}/bin/gpg'"
-      substituteInPlace test_gnupg.py \
-        --replace "gpgbinary=GPGBINARY" "gpgbinary='${pkgs.gnupg1}/bin/gpg'"
-    '';
-
-    meta = {
-      description = "A wrapper for the Gnu Privacy Guard";
-      homepage    = "https://pypi.python.org/pypi/python-gnupg";
-      license     = licenses.bsd3;
-      maintainers = with maintainers; [ copumpkin ];
-      platforms   = platforms.unix;
-    };
-  };
+  python-gnupg = callPackage ../development/python-modules/python-gnupg {};
 
   python-uinput = buildPythonPackage rec {
     name = "python-uinput-${version}";
