26d6eab6550dc6bf1581aa0d11f05203f6181f24
Luke Adams <luke.adams@belljar.io>
2017-07-19 17:41:21 -0500

    prl-tools: remove unnecessary kernel modules

--- nixos/modules/virtualisation/parallels-guest.nix
+++ nixos/modules/virtualisation/parallels-guest.nix
@@ -55,7 +55,7 @@ in
 
     boot.extraModulePackages = [ prl-tools ];
 
-    boot.kernelModules = [ "prl_tg" "prl_eth" "prl_fs" "prl_fs_freeze" "acpi_memhotplug" ];
+    boot.kernelModules = [ "prl_tg" "prl_eth" "prl_fs" "prl_fs_freeze" ];
 
     services.timesyncd.enable = false;
 
