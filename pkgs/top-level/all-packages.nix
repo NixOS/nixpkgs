bbafe64bddac90ac1ca7ee5dcdfb2f90dc21b5e4
Franz Pletz <fpletz@fnordicwalking.de>
2017-07-19 22:35:41 +0200

    hue-cli: init at 0.1.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14608,6 +14608,8 @@ with pkgs;
 
   ht = callPackage ../applications/editors/ht { };
 
+  hue-cli = callPackage ../tools/networking/hue-cli { };
+
   hugin = callPackage ../applications/graphics/hugin { };
 
   hugo = callPackage ../applications/misc/hugo { };
