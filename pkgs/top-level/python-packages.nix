17c8fe21fd3bdda83d8261023d1675a9f0c9cb98
volth <volth@webmaster.ms>
2017-07-20 21:31:04 +0000

    libvirt: 3.1.0 -> 3.5.0 (#25411)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -26839,13 +26839,13 @@ EOF
   };
 
   libvirt = let
-    version = "3.1.0";
+    version = "3.5.0";
   in assert version == pkgs.libvirt.version; pkgs.stdenv.mkDerivation rec {
     name = "libvirt-python-${version}";
 
     src = pkgs.fetchurl {
       url = "http://libvirt.org/sources/python/${name}.tar.gz";
-      sha256 = "06524dhm27fjfnmr5bqdxlmm1g9ixvzaaq572hgyy5dqwfn64spk";
+      sha256 = "06mc0cm4k90z8vxaslk3ifpajg8w8dvm0m2mxwcd6fdzps8fwpsw";
     };
 
     buildInputs = with self; [ python pkgs.pkgconfig pkgs.libvirt lxml ];
