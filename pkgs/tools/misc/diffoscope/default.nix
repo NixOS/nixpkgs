0d61f5f78a338cd3f7928368998e410d25d6802b
Eelco Dolstra <edolstra@gmail.com>
2017-07-21 15:00:50 +0200

    Revert "diffoscope: wrap executable and include tools in PATH"
    This reverts commit 23ad77b9980fdd7f943da38cd745c4ccfdda15a7.
    
    Double wrapper scripts considered harmful.
    
    (cherry picked from commit 3e68facc72db94224cf8955bfe6745537deaf1eb)

--- pkgs/tools/misc/diffoscope/default.nix
+++ pkgs/tools/misc/diffoscope/default.nix
@@ -5,13 +5,7 @@
 , enableBloat ? false
 }:
 
-    # Still missing these tools: enjarify, otool & lipo (maybe OS X only), showttf
-    # Also these libraries: python3-guestfs
-    # FIXME: move xxd into a separate package so we don't have to pull in all of vim.
-let tools = [ acl binutils bzip2 cbfstool cdrkit cpio diffutils e2fsprogs file gettext
-              gzip libcaca poppler_utils sng sqlite squashfsTools unzip vim xz colordiff
-            ] ++ lib.optionals enableBloat [ colord fpc ghc gnupg1 jdk mono pdftk ];
-in python3.pkgs.buildPythonApplication rec {
+python3.pkgs.buildPythonApplication rec {
   pname = "diffoscope";
   name = "${pname}-${version}";
   version = "77";
@@ -32,6 +26,14 @@ in python3.pkgs.buildPythonApplication rec {
     sed -i setup.py -e "/'rpm-python',/d"
   '';
 
+  # Still missing these tools: enjarify, otool & lipo (maybe OS X only), showttf
+  # Also these libraries: python3-guestfs
+  # FIXME: move xxd into a separate package so we don't have to pull in all of vim.
+  buildInputs =
+    map lib.getBin ([ acl binutils bzip2 cbfstool cdrkit cpio diffutils e2fsprogs file gettext
+      gzip libcaca poppler_utils sng sqlite squashfsTools unzip vim xz colordiff
+    ] ++ lib.optionals enableBloat [ colord fpc ghc gnupg1 jdk mono pdftk ]);
+
   pythonPath = with python3.pkgs; [ debian libarchive-c python_magic tlsh rpm ];
 
   doCheck = false; # Calls 'mknod' in squashfs tests, which needs root
@@ -39,7 +41,6 @@ in python3.pkgs.buildPythonApplication rec {
   postInstall = ''
     mkdir -p $out/share/man/man1
     ${docutils}/bin/rst2man.py debian/diffoscope.1.rst $out/share/man/man1/diffoscope.1
-    wrapProgram $out/bin/diffoscope --prefix PATH : ${lib.makeBinPath tools}
   '';
 
   meta = with stdenv.lib; {
