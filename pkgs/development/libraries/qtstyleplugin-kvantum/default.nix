2a84ffd4f99265805a82490df74e0153e9fc6144
bugworm <bugworm@zoho.com>
2017-07-23 21:38:47 +0300

    Changed description

--- pkgs/development/libraries/qtstyleplugin-kvantum/default.nix
+++ pkgs/development/libraries/qtstyleplugin-kvantum/default.nix
@@ -11,8 +11,12 @@ stdenv.mkDerivation rec {
     sha256 = "0ky44s1fgqxraywagx1mv07yz76ppgiz3prq447db78wkwqg2d8p";
   };
 
+<<<<<<< HEAD
   nativeBuildInputs = [ qmake qttools ];
   buildInputs = [ qtbase qtsvg qtx11extras libX11 libXext  ];
+=======
+  nativeBuildInputs = [ qmake qtbase qtsvg qtx11extras libX11 libXext qttools ];
+>>>>>>> 1749de44b63cdf5f015087646b34e8eedf221a24
 
   postUnpack = "sourceRoot=\${sourceRoot}/Kvantum";
 
@@ -22,7 +26,11 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with stdenv.lib; {
+<<<<<<< HEAD
     description = "SVG-based Qt5 theme engine plus a config tool and extra themes";
+=======
+    description = "A Linux SVG-based theme engine for Qt4/Qt5 and KDE";
+>>>>>>> 1749de44b63cdf5f015087646b34e8eedf221a24
     homepage = "https://github.com/tsujan/Kvantum";
     license = licenses.gpl2;
     platforms = platforms.linux;
