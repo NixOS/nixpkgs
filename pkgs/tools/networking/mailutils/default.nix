f1a2b227c966c6e9fac445cde3d6b29e434734bb
Jörg Thalheim <joerg@thalheim.io>
2017-07-23 21:24:02 +0100

    mailutils: fix sandbox download

--- pkgs/tools/networking/mailutils/default.nix
+++ pkgs/tools/networking/mailutils/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchgit, fetchpatch
+{ stdenv, fetchgit, fetchpatch, fetchurl
 , autoconf, automake, bison, dejagnu, flex, gettext, git, libtool, pkgconfig, rsync, wget
 , gdbm, pam, readline, ncurses, gnutls, guile, texinfo, gnum4, sasl, fribidi, nettools
 , gss, mysql }:
@@ -26,6 +26,11 @@ stdenv.mkDerivation rec {
     gss mysql.lib
   ];
 
+  radius_m4 = fetchurl {
+    url = "https://git.savannah.gnu.org/cgit/radius.git/plain/scripts/radius.m4?id=d91dd0a1c54ed1d88500d6a33795a77b927d7134";
+    sha256 = "18h81flgfdhx093p3pmi6s3793k5n8aa3hfl2axckn2xjml2bxfx";
+  };
+
   patches = [
     (fetchpatch {
       url = "http://git.gnu.org.ua/cgit/mailutils.git/patch/?id=afbb33cf9ff750e93a9a4c1f51a3b62d584f056e";
@@ -43,7 +48,8 @@ stdenv.mkDerivation rec {
            -e 's/chmod [24]755/chmod 0755/' \
       */Makefile{.in,.am}
     git add gnulib
-    bash -ex bootstrap
+    install -D ${radius_m4} m4/radius.m4
+    bash -ex bootstrap --skip-po
   '';
 
   configureFlags = [
