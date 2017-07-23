9e90d9e5a30238aedd92f3fcdc54658687cd9879
Robert Helgesson <robert@rycee.net>
2017-07-23 12:47:30 +0200

    gpsbabel: 1.5.3 -> 1.5.4
    Also really use QT5 and switch download location to GitHub.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14528,7 +14528,7 @@ with pkgs;
 
   gosmore = callPackage ../applications/misc/gosmore { };
 
-  gpsbabel = libsForQt5.callPackage ../applications/misc/gpsbabel {
+  gpsbabel = libsForQt56.callPackage ../applications/misc/gpsbabel {
     inherit (darwin) IOKit;
   };
 
