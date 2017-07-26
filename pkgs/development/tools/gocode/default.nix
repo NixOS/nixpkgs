4244a16917ea3392b9360e57292e1fab603ed5ef
Charles Strahan <charles@cstrahan.com>
2017-07-26 02:28:49 -0400

    gocode: update to f1eef9a6, fix stdlib completions

--- pkgs/development/tools/gocode/default.nix
+++ pkgs/development/tools/gocode/default.nix
@@ -2,14 +2,19 @@
 
 buildGoPackage rec {
   name = "gocode-${version}";
-  version = "20170219-${stdenv.lib.strings.substring 0 7 rev}";
-  rev = "f54790e5d4386b60b80d0c6f9e59db345839d7cc";
-  
+  version = "20170530-${stdenv.lib.strings.substring 0 7 rev}";
+  rev = "f1eef9a6ba005abb145d7b58fdd225e83a3c6a05";
+
   goPackagePath = "github.com/nsf/gocode";
+
+  # we must allow references to the original `go` package,
+  # because `gocode` needs to dig into $GOROOT to provide completions for the
+  # standard packages.
+  allowGoReference = true;
 
   src = fetchgit {
     inherit rev;
     url = "https://github.com/nsf/gocode";
-    sha256 = "1x9wdahpdkqwqkipxl5m0sh8d59i389rdvrsyz57slpfd0hapkks";
+    sha256 = "1hkr46ikrprx203i2yr6xds1bzxggblh7bg026m2cda6dxgpnsgw";
   };
 }
