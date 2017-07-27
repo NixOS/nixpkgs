539dcb8e2760ee6cab990ce929a586b050496520
John Ericson <Ericson2314@Yahoo.com>
2017-07-27 14:15:10 -0400

    cc-wrapper-test: Forgot to `extern "C"` now that main is C++

--- pkgs/test/macos-sierra-shared/default.nix
+++ pkgs/test/macos-sierra-shared/default.nix
@@ -32,7 +32,7 @@ let
 
         #include <assert.h>
 
-        ${toString (lib.genList (i: "extern unsigned int asdf_${toString i}(void); ") count)}
+        ${toString (lib.genList (i: "extern \"C\" unsigned int asdf_${toString i}(void); ") count)}
 
         unsigned int (*funs[])(void) = {
           ${toString (lib.genList (i: "asdf_${toString i},") count)}
