a8dd453617fab5fc7c85ddaaac22e70a8cbe3f04
Robin Gloster <mail@glob.in>
2017-07-29 01:17:08 +0200

    tinc_pre: fix merge

--- pkgs/tools/networking/tinc/pre.nix
+++ pkgs/tools/networking/tinc/pre.nix
@@ -15,13 +15,6 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ autoreconfHook texinfo ];
   buildInputs = [ ncurses readline zlib lzo openssl ];
 
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/gsliepen/tinc/commit/323c17e232539f3f06e7cebc664ab48f60127e0e.patch";
-      sha256 = "1flnprxa0gviiih0lbpqa5kj41ndh1sylm5j9ibly7f8m5rg16rs";
-    })
-  ];
-
   prePatch = ''
     substituteInPlace configure.ac --replace UNKNOWN ${version}
   '';
@@ -31,6 +24,10 @@ stdenv.mkDerivation rec {
     (fetchpatch {
       url = https://github.com/gsliepen/tinc/compare/acefa66...e4544db.patch;
       sha256 = "1jz7anqqzk7j96l5ifggc2knp14fmbsjdzfrbncxx0qhb6ihdcvn";
+    })
+    (fetchpatch {
+      url = "https://github.com/gsliepen/tinc/commit/323c17e232539f3f06e7cebc664ab48f60127e0e.patch";
+      sha256 = "1flnprxa0gviiih0lbpqa5kj41ndh1sylm5j9ibly7f8m5rg16rs";
     })
   ];
 
