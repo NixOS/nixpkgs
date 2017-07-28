6d09e0c9bea3944e9c7dfa4601d2454886afb257
wisut hantanong <wizzup@gmail.com>
2017-07-28 14:05:23 +0700

    python.pkgs.apsw: move to separate expression

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -588,25 +588,7 @@ in {
     };
   };
 
-  apsw = buildPythonPackage rec {
-    name = "apsw-3.7.6.2-r1";
-    disabled = isPyPy;
-
-    src = pkgs.fetchurl {
-      url = "http://apsw.googlecode.com/files/${name}.zip";
-      sha256 = "cb121b2bce052609570a2f6def914c0aa526ede07b7096dddb78624d77f013eb";
-    };
-
-    buildInputs = with self; [ pkgs.sqlite ];
-
-    # python: double free or corruption (fasttop): 0x0000000002fd4660 ***
-    doCheck = false;
-
-    meta = {
-      description = "A Python wrapper for the SQLite embedded relational database engine";
-      homepage = http://code.google.com/p/apsw/;
-    };
-  };
+  apsw = callPackage ../development/python-modules/apsw {};
 
   astor = buildPythonPackage rec {
     name = "astor-${version}";
