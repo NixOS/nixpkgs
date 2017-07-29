a444b34b52b13b1658875633f871b96456b1cd71
Robert Helgesson <robert@rycee.net>
2017-07-29 15:04:34 +0200

    facedetect: init at 0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -997,6 +997,8 @@ with pkgs;
 
   f3 = callPackage ../tools/filesystems/f3 { };
 
+  facedetect = callPackage ../tools/graphics/facedetect { };
+
   facter = callPackage ../tools/system/facter {
     boost = boost160;
   };
