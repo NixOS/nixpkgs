da0f822ce8e13f431ce750dc2bd72a352a731f9e
Robin Gloster <mail@glob.in>
2017-07-29 02:12:09 +0200

    kodi: fix wrong mysql include handling

--- pkgs/applications/video/kodi/default.nix
+++ pkgs/applications/video/kodi/default.nix
@@ -101,6 +101,10 @@ in stdenv.mkDerivation rec {
         --replace 'usr/share/zoneinfo' 'etc/zoneinfo'
       substituteInPlace tools/depends/target/ffmpeg/autobuild.sh \
         --replace "/bin/bash" "${bash}/bin/bash -ex"
+      substituteInPlace xbmc/dbwrappers/mysqldataset.h \
+        --replace "mysql/mysql.h" "mysql.h"
+      substituteInPlace xbmc/dbwrappers/mysqldataset.cpp \
+        --replace "mysql/errmsg.h" "errmsg.h"
       cp ${ffmpeg_3_1_6} tools/depends/target/ffmpeg/ffmpeg-3.1.6-${rel}.tar.gz
       ln -s ${libdvdcss.src} tools/depends/target/libdvdcss/libdvdcss-master.tar.gz
       cp ${libdvdnav_src} tools/depends/target/libdvdnav/libdvdnav-master.tar.gz
