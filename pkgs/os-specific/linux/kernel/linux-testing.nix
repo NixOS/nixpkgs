979817d1535bf911d181be6685ad4879e4bdf37f
aszlig <aszlig@redmoonstudios.org>
2017-07-31 08:41:48 +0200

    linux-testing: 4.13-rc2 -> 4.13-rc3
    Tested via building the linux_testing attribute, but didn't test it at
    runtime (yet).
    
    Diffed unpacked tarball against my local git clone and the contents
    match.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -1,13 +1,13 @@
 { stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.13-rc2";
-  modDirVersion = "4.13.0-rc2";
+  version = "4.13-rc3";
+  modDirVersion = "4.13.0-rc3";
   extraMeta.branch = "4.13";
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "1ni0z3v8zkqlmxn4czbw71yaipp6hbyh39vxdzpqy1dqn7zalmif";
+    sha256 = "07cxqf57hgs3wnbvkqixiwhjrwdf433pjwmh0hv1id0bk8wdrjjl";
   };
 
   features.iwlwifi = true;
