a038866c974525ced1853e8668adf4e2711d0599
Franz Pletz <fpletz@fnordicwalking.de>
2017-07-31 23:30:59 +0200

    facter: 3.6.4 -> 3.6.6

--- pkgs/tools/system/facter/default.nix
+++ pkgs/tools/system/facter/default.nix
@@ -2,14 +2,17 @@
 
 stdenv.mkDerivation rec {
   name = "facter-${version}";
-  version = "3.6.4";
+  version = "3.6.6";
 
   src = fetchFromGitHub {
-    sha256 = "177mmg5a4s4q2p76df4z6c51nfnr73qya1pvvj6fcs1gld01xjr6";
+    sha256 = "07jphvwfmvrq28f8k15k16kz090zvb11nn6bd895fz5axag01ins";
     rev = version;
     repo = "facter";
     owner = "puppetlabs";
   };
+
+  CXXFLAGS = "-fpermissive";
+  NIX_CFLAGS_COMPILE = "-Wno-error";
 
   cmakeFlags = [ "-DFACTER_RUBY=${ruby}/lib/libruby.so" ];
 
@@ -17,6 +20,8 @@ stdenv.mkDerivation rec {
   preConfigure = "cmakeFlags+=\" -DRUBY_LIB_INSTALL=$out/lib/ruby\"";
 
   buildInputs = [ boost cmake cpp-hocon curl leatherman libyamlcpp openssl ruby utillinux ];
+
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     homepage = https://github.com/puppetlabs/facter;
