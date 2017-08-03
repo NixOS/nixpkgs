7c17925aaea7821f7cac182fee85bf6b4b52429c
Cillian de Róiste <cillian.deroiste@gmail.com>
2017-08-03 21:27:28 +0200

    key-mon: fix download and homepage URLs

--- pkgs/applications/video/key-mon/default.nix
+++ pkgs/applications/video/key-mon/default.nix
@@ -6,7 +6,7 @@ pythonPackages.buildPythonApplication rec {
   namePrefix = "";
 
   src = fetchurl {
-    url = "http://key-mon.googlecode.com/files/${name}.tar.gz";
+    url = "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/key-mon/${name}.tar.gz";
     sha256 = "1liz0dxcqmchbnl1xhlxkqm3gh76wz9jxdxn9pa7dy77fnrjkl5q";
   };
 
@@ -20,7 +20,7 @@ pythonPackages.buildPythonApplication rec {
   '';
 
   meta = with stdenv.lib; {
-    homepage = http://code.google.com/p/key-mon;
+    homepage = https://code.google.com/archive/p/key-mon;
     description = "Utility to show live keyboard and mouse status for teaching and screencasts";
     license = licenses.asl20;
     maintainers = [ maintainers.goibhniu ];
