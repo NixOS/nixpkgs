2bbfac7b97e9ff72ac0ae7d1ab150802c254c157
Robert Helgesson <robert@rycee.net>
2017-08-06 11:24:12 +0200

    expat: fix build on aarch

--- pkgs/development/libraries/expat/default.nix
+++ pkgs/development/libraries/expat/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchpatch, fetchurl }:
 
 stdenv.mkDerivation rec {
   name = "expat-2.2.3";
@@ -10,6 +10,16 @@ stdenv.mkDerivation rec {
 
   outputs = [ "out" "dev" ]; # TODO: fix referrers
   outputBin = "dev";
+
+  patches = [
+    (fetchpatch {
+      name = "fix-aarch-build.patch";
+      url = "https://github.com/libexpat/libexpat/commit/d98d4399174fd6f71d70e7bd89993a0e7346753d.patch";
+      sha256 = "0z89wb4mzyf7vvl6kbflk5w1z7yc39jwvs3mkznin5agj34x063w";
+      stripLen = 1;
+      excludes = [ "coverage.sh" ];
+    })
+  ];
 
   configureFlags = stdenv.lib.optional stdenv.isFreeBSD "--with-pic";
 
