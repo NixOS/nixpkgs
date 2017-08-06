2227d03d3e9a6bbe6d925b798943ca865deaabc9
evujumenuk <evujumenuk@mailinator.com>
2017-08-06 19:13:39 +0200

    keybase: explain how to find the latest version...
    ...'blessed' by upstream.

--- pkgs/tools/security/keybase/default.nix
+++ pkgs/tools/security/keybase/default.nix
@@ -2,6 +2,9 @@
 
 buildGoPackage rec {
   name = "keybase-${version}";
+  # The latest version 'blessed' by upstream can be extracted from:
+  #     https://aur.archlinux.org/packages/keybase-git/
+  # `version` partially reuses `pkgver`; `nix-prefetch-git` provides the date.
   version = "1.0.27-20170726114412.8169d66";
 
   goPackagePath = "github.com/keybase/client";
@@ -12,6 +15,7 @@ buildGoPackage rec {
   src = fetchFromGitHub {
     owner  = "keybase";
     repo   = "client";
+    # `pkgver` in the PKGBUILD provides the short hash.
     rev    = "8169d666e2a4d920da7025170baccde336fce11d";
     sha256 = "1nnfyvbmn6cy0g4dy0ayg17jblxkyck0097f7bbmwbnrplaj2w6h";
   };
