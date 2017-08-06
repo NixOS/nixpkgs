7eb469246f579399f18a0e1af594a559dc48c4ad
roblabla <robinlambertz+dev@gmail.com>
2017-08-06 16:27:54 +0200

    gitlab: 9.3.4 -> 9.4.3
    (cherry picked from commit 8975d153a0d4385234aafdf67fbfc08a522f74d6)

--- pkgs/applications/version-management/gitlab/default.nix
+++ pkgs/applications/version-management/gitlab/default.nix
@@ -6,8 +6,69 @@
    to allow building the assets without a database */
 
 let
-  env = bundlerEnv {
+  # Taken from yarn2nix
+  buildYarnPackageDeps = {
+    name,
+    packageJson,
+    yarnLock,
+    yarnNix,
+    pkgConfig ? {},
+    yarnFlags ? []
+  }:
+    let
+      offlineCache = (pkgs.callPackage yarnNix {}).offline_cache;
+      extraBuildInputs = (lib.flatten (builtins.map (key:
+        pkgConfig.${key} . buildInputs or []
+      ) (builtins.attrNames pkgConfig)));
+      postInstall = (builtins.map (key:
+        if (pkgConfig.${key} ? postInstall) then
+          ''
+            for f in $(find -L -path '*/node_modules/${key}' -type d); do
+              (cd "$f" && (${pkgConfig.${key}.postInstall}))
+            done
+          ''
+        else
+          ""
+      ) (builtins.attrNames pkgConfig));
+    in
+    stdenv.mkDerivation {
+      name = "${name}-modules";
+
+      phases = ["buildPhase"];
+      buildInputs = [ yarn nodejs ] ++ extraBuildInputs;
+
+      buildPhase = ''
+        # Yarn writes cache directories etc to $HOME.
+        export HOME=`pwd`/yarn_home
+        cp ${packageJson} ./package.json
+        cp ${yarnLock} ./yarn.lock
+        chmod +w ./yarn.lock
+        yarn config --offline set yarn-offline-mirror ${offlineCache}
+        # Do not look up in the registry, but in the offline cache.
+        # TODO: Ask upstream to fix this mess.
+        sed -i -E 's|^(\s*resolved\s*")https?://.*/|\1|' yarn.lock
+        yarn install ${lib.escapeShellArgs yarnFlags}
+        ${lib.concatStringsSep "\n" postInstall}
+        mkdir $out
+        mv node_modules $out/
+        patchShebangs $out
+      '';
+    };
+  node-env = buildYarnPackageDeps {
     name = "gitlab";
+    packageJson = ./package.json;
+    yarnLock = ./yarn.lock;
+    yarnNix = ./yarn.nix;
+    yarnFlags = [
+      "--offline"
+      "--frozen-lockfile"
+      "--ignore-engines"
+      "--ignore-scripts"
+    ];
+    # pkgConfig might need to come from node-packages ?
+  };
+  ruby-env = bundlerEnv {
+    name = "gitlab-env-0.2";
     inherit ruby;
     gemdir = ./.;
     meta = with lib; {
@@ -18,12 +79,7 @@ let
     };
   };
 
-  version = "8.17.8";
-
-  gitlabDeb = fetchurl {
-    url = "https://packages.gitlab.com/gitlab/gitlab-ce/packages/debian/jessie/gitlab-ce_${version}-ce.0_amd64.deb/download";
-    sha256 = "0r4f1illaalhrvc6zgq19g4jcs77jf665l8bgx6pyvw45yn6wv7h";
-  };
+  version = "9.4.3";
 
 in
 
@@ -38,7 +94,7 @@ stdenv.mkDerivation rec {
     owner = "gitlabhq";
     repo = "gitlabhq";
     rev = "v${version}";
-    sha256 = "041ld3na8m42akqpljipid984d0mv0wzk2p1630fncc1w3iaq06d";
+    sha256 = "1r4fvj94l73p3zqlcv80iw4gbsyq26d6x5d47v9zs3pjzkgz0891";
   };
 
   patches = [
@@ -73,13 +129,18 @@ stdenv.mkDerivation rec {
   buildPhase = ''
     mv config/gitlab.yml.example config/gitlab.yml
 
-    dpkg --fsys-tarfile ${gitlabDeb} | tar -x --no-same-permissions --no-same-owner
-    mv -v opt/gitlab/embedded/service/gitlab-rails/public/assets public
-    rm -rf opt
+    # Emulate yarn install --production --pure-lockfile
+    mkdir -p node_modules/
+    ln -s ${node-env}/node_modules/* node_modules/
+    ln -s ${node-env}/node_modules/.bin node_modules/
 
+    # Compile assets. We skip the yarn check because it fails
     export GITLAB_DATABASE_ADAPTER=nulldb
     export SKIP_STORAGE_VALIDATION=true
-    rake assets:precompile RAILS_ENV=production
+    rake rake:assets:precompile RAILS_ENV=production NODE_ENV=production
+    rake webpack:compile RAILS_ENV=production NODE_ENV=production
+    rake gitlab:assets:fix_urls RAILS_ENV=production NODE_ENV=production
+    rake gettext:compile RAILS_ENV=production
 
     mv config/gitlab.yml config/gitlab.yml.example
     rm config/secrets.yml
