260bab6c7f5445f08c1c6200479251b10af7e426
AndersonTorres <torres.anderson.85@gmail.com>
2017-08-07 22:50:31 -0300

    higan: 102 -> 103

--- pkgs/misc/emulators/higan/default.nix
+++ pkgs/misc/emulators/higan/default.nix
@@ -10,12 +10,12 @@ with stdenv.lib;
 stdenv.mkDerivation rec {
 
   name = "higan-${version}";
-  version = "102";
+  version = "103";
   sourceName = "higan_v${version}-source";
 
   src = fetchurl {
     urls = [ "http://download.byuu.org/${sourceName}.7z" ];
-    sha256 = "1wcr2sxk0n4rngnf9g2qcjcv70s8rf5cqj195sav1yjwxkrdrnjj";
+    sha256 = "0xj2k5g1zyl71hk3kwaixk1axbi6s9kqq31c702rl7qkljv6lfp6";
     curlOpts = "--user-agent 'Mozilla/5.0'"; # the good old user-agent trick...
   };
 
@@ -35,6 +35,7 @@ stdenv.mkDerivation rec {
     make compiler=c++ -C higan
   '';
 
+  # Now the cheats file will be distributed separately
   installPhase = ''
     install -dm 755 $out/bin $out/share/applications $out/share/higan $out/share/pixmaps
     install -m 755 icarus/out/icarus $out/bin/
@@ -42,8 +43,6 @@ stdenv.mkDerivation rec {
     install -m 644 higan/data/higan.desktop $out/share/applications/
     install -m 644 higan/data/higan.png $out/share/pixmaps/higan-icon.png
     install -m 644 higan/resource/logo/higan.png $out/share/pixmaps/higan-logo.png
-    cp --recursive --no-dereference --preserve='links' --no-preserve='ownership' \
-      higan/data/cheats.bml  $out/share/higan/
     cp --recursive --no-dereference --preserve='links' --no-preserve='ownership' \
       higan/systems/* $out/share/higan/
   '';
@@ -57,7 +56,6 @@ stdenv.mkDerivation rec {
     cat <<EOF > $out/bin/higan-init.sh
     #!${stdenv.shell}
 
-    cp --update $out/share/higan/cheats.bml \$HOME/.config/
     cp --recursive --update $out/share/higan/*.sys \$HOME/.local/share/higan/
 
     EOF
@@ -68,7 +66,7 @@ stdenv.mkDerivation rec {
   meta = {
     description = "An open-source, cycle-accurate Nintendo multi-system emulator";
     longDescription = ''
-      Higan (formerly bsnes) is a Nintendo multi-system emulator.
+      Higan (formerly bsnes) is a multi-system game console emulator.
       It currently supports the following systems:
         - Nintendo's Famicom, Super Famicom (with subsystems: 
           Super Game Boy, BS-X Satellaview, Sufami Turbo); 
