11861b7c97af2c64761e68763dd3d13a1508fd4e
Lukas Werling <lukas.werling@gmail.com>
2017-08-08 22:43:54 +0200

    pcmanfm: build with gtk3

--- pkgs/applications/misc/pcmanfm/default.nix
+++ pkgs/applications/misc/pcmanfm/default.nix
@@ -1,5 +1,11 @@
-{ stdenv, fetchurl, glib, gtk2, intltool, libfm, libX11, pango, pkgconfig }:
+{ stdenv, fetchurl, glib, intltool, libfm, libX11, pango, pkgconfig
+, withGtk3 ? true, gtk2, gtk3 }:
 
+let
+  libfm' = libfm.override { inherit withGtk3; };
+  gtk = if withGtk3 then gtk3 else gtk2;
+  inherit (stdenv.lib) optional;
+in
 stdenv.mkDerivation rec {
   name = "pcmanfm-1.2.5";
   src = fetchurl {
@@ -7,7 +13,9 @@ stdenv.mkDerivation rec {
     sha256 = "0rxdh0dfzc84l85c54blq42gczygq8adhr3l9hqzy1dp530cm1hc";
   };
 
-  buildInputs = [ glib gtk2 intltool libfm libX11 pango pkgconfig ];
+  buildInputs = [ glib gtk intltool libfm' libX11 pango pkgconfig ];
+
+  configureFlags = optional withGtk3 "--with-gtk=3";
 
   meta = with stdenv.lib; {
     homepage = http://blog.lxde.org/?cat=28/;
