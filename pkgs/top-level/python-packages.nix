6d82bac4243231338ff01c88886d3b012a6b041e
adisbladis <adis@blad.is>
2017-08-09 12:28:02 +0800

    pythonPackages.hypatia: Remove outdated package

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8383,21 +8383,6 @@ in {
     };
   };
 
-  hypatia = buildPythonPackage rec {
-    name = "hypatia-0.3";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/h/hypatia/${name}.tar.gz";
-      sha256 = "fb4d394eeac4b06ff2259cada6174aebbe77edd243ffd1deda320cb327f98bd9";
-    };
-
-    buildInputs = with self; [ zope_interface zodb ];
-
-    meta = {
-      maintainers = with maintainers; [ domenkozar ];
-    };
-  };
-
 
   zope_copy = buildPythonPackage rec {
     name = "zope.copy-4.0.2";
