5db062dc04323698c0421255c050ba4b2f38c5c8
Grant Wu <grantwwu@gmail.com>
2017-08-09 02:08:58 -0400

    \o/ it builds

--- pkgs/tools/networking/p2p/libtorrent/default.nix
+++ pkgs/tools/networking/p2p/libtorrent/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchFromGitHub, pkgconfig
 , libtool, autoconf, automake, cppunit
-, openssl, libsigcxx, zlib, boost }:
+, openssl, libsigcxx, zlib, boost, boost-build }:
 
 stdenv.mkDerivation rec {
   name = "libtorrent-${version}";
@@ -13,9 +13,13 @@ stdenv.mkDerivation rec {
     sha256 = "0h6gfj0swhjzxmpqgdma4i9kr99gsfhb6j3icpfbg09hcza3zhiv";
   };
 
-  buildInputs = [ pkgconfig libtool autoconf automake cppunit openssl libsigcxx zlib boost ];
+  buildInputs = [ pkgconfig libtool autoconf automake cppunit openssl libsigcxx zlib boost boost-build ];
 
-  preConfigure = "./autotool.sh";
+  # preConfigure = "./autotool.sh";
+
+  buildPhase = "bjam release";
+
+  installPhase = "bjam --prefix=$out release install";
 
   meta = with stdenv.lib; {
     homepage = http://www.libtorrent.org/;
