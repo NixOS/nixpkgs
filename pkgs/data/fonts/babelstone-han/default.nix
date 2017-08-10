8be265ef450a2aa5f9a37ce76e8c623de1b9c45e
Volth <volth@webmaster.ms>
2017-08-10 15:24:32 +0000

    babelstone-han: 9.0.2 -> 10.0.0

--- pkgs/data/fonts/babelstone-han/default.nix
+++ pkgs/data/fonts/babelstone-han/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "babelstone-han-${version}";
-  version = "9.0.2";
+  version = "10.0.0";
 
   src = fetchurl {
-    url = "http://www.babelstone.co.uk/Fonts/8672/BabelStoneHan.zip";
-    sha256 = "09zlrp3mqdsbxpq4sssd8gj5isnxfbr56pcdp7mnr27nv4pvp6ha";
+    url = http://www.babelstone.co.uk/Fonts/0816/BabelStoneHan.zip;
+    sha256 = "1vccpyk5fd35fq2mk48cdhzy5q6id1pzlski1knsiqamvyys4ykd";
   };
 
   buildInputs = [ unzip ];
@@ -17,6 +17,10 @@ stdenv.mkDerivation rec {
     mkdir -p $out/share/fonts/truetype
     cp -v *.ttf $out/share/fonts/truetype
   '';
+
+  outputHashAlgo = "sha256";
+  outputHashMode = "recursive";
+  outputHash = "0648hv5c1hq3bq7mlk7bnmflzzqj4wh137bjqyrwj5hy3nqzvl5r";
 
   meta = with stdenv.lib; {
     description = "Unicode CJK font with over 32600 Han characters";
