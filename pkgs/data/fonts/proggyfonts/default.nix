1f263ab20087b38d59b4c3c097379677df0fe437
volth <volth@webmaster.ms>
2017-08-11 02:52:46 +0000

    proggyfonts: dead link fixed
    - [x] dead link fixed
    - [x] make the font derivations fixed-output (https://github.com/NixOS/nixpkgs/issues/27754)

--- pkgs/data/fonts/proggyfonts/default.nix
+++ pkgs/data/fonts/proggyfonts/default.nix
@@ -1,19 +1,15 @@
-{ stdenv, fetchurl, mkfontdir, mkfontscale }:
+{ stdenv, fetchzip, mkfontdir, mkfontscale }:
 
 # adapted from https://aur.archlinux.org/packages/proggyfonts/
 
-stdenv.mkDerivation rec {
+fetchzip {
   name = "proggyfonts-0.1";
 
-  src = fetchurl {
-    url = "http://kaictl.net/software/${name}.tar.gz";
-    sha256 = "1plcm1sjpa3hdqhhin48fq6zmz3ndm4md72916hd8ff0w6596q0n";
-  };
+  url = "http://web.archive.org/web/20150801042353/http://kaictl.net/software/proggyfonts-0.1.tar.gz";
 
-  buildInputs = [ mkfontdir mkfontscale ];
+  postFetch = ''
+      tar -xzf $downloadedFile --strip-components=1
 
-  installPhase =
-    ''
       mkdir -p $out/share/doc/$name $out/share/fonts/misc $out/share/fonts/truetype
 
       cp Licence.txt $out/share/doc/$name/LICENSE
@@ -26,10 +22,12 @@ stdenv.mkDerivation rec {
 
       for f in misc truetype; do
         cd $out/share/fonts/$f
-        mkfontscale
-        mkfontdir
+        ${mkfontscale}/bin/mkfontscale
+        ${mkfontdir}/bin/mkfontdir
       done
-    '';
+  '';
+
+  sha256 = "06jsf3rw6q4l1jrw1161h4vxa1xbvpry5x12d8sh5g7hjk88p77g";
 
   meta = with stdenv.lib; {
     homepage = http://upperbounds.net;
