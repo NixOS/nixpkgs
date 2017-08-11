05b8cae9ec88156f46d9b98424643b6208664222
Robin Gloster <mail@glob.in>
2017-08-11 19:11:48 +0200

    linux: remove unused kernel patches

--- pkgs/os-specific/linux/kernel/patches.nix
+++ pkgs/os-specific/linux/kernel/patches.nix
@@ -21,12 +21,6 @@ in
 
 rec {
 
-  multithreaded_rsapubkey =
-    {
-      name = "multithreaded-rsapubkey-asn1.patch";
-      patch = ./multithreaded-rsapubkey-asn1.patch;
-    };
-
   bridge_stp_helper =
     { name = "bridge-stp-helper";
       patch = ./bridge-stp-helper.patch;
@@ -35,12 +29,6 @@ rec {
   p9_fixes =
     { name = "p9-fixes";
       patch = ./p9-fixes.patch;
-    };
-
-  no_xsave =
-    { name = "no-xsave";
-      patch = ./no-xsave.patch;
-      features.noXsave = true;
     };
 
   mips_fpureg_emu =
@@ -63,22 +51,6 @@ rec {
       patch = ./modinst-arg-list-too-long.patch;
     };
 
-  ubuntu_fan_4_4 =
-    { name = "ubuntu-fan";
-      patch = ./ubuntu-fan-4.4.patch;
-    };
-
-  ubuntu_unprivileged_overlayfs =
-    { name = "ubuntu-unprivileged-overlayfs";
-      patch = ./ubuntu-unprivileged-overlayfs.patch;
-    };
-
-  tuxonice_3_10 = makeTuxonicePatch {
-    version = "2013-11-07";
-    kernelVersion = "3.10.18";
-    sha256 = "00b1rqgd4yr206dxp4mcymr56ymbjcjfa4m82pxw73khj032qw3j";
-  };
-
   grsecurity_testing = throw ''
     Upstream has ceased free support for grsecurity/PaX.
 
@@ -86,11 +58,6 @@ rec {
     and https://grsecurity.net/passing_the_baton_faq.php
     for more information.
   '';
-
-  crc_regression =
-    { name = "crc-backport-regression";
-      patch = ./crc-regression.patch;
-    };
 
   genksyms_fix_segfault =
     { name = "genksyms-fix-segfault";
@@ -107,46 +74,7 @@ rec {
       patch = ./chromiumos-patches/no-link-restrictions.patch;
     };
 
-  chromiumos_mfd_fix_dependency =
-    { name = "mfd_fix_dependency";
-      patch = ./chromiumos-patches/mfd-fix-dependency.patch;
-    };
-
-  hiddev_CVE_2016_5829 =
-    { name = "hiddev_CVE_2016_5829";
-      patch = fetchpatch {
-        url = "https://sources.debian.net/data/main/l/linux/4.6.3-1/debian/patches/bugfix/all/HID-hiddev-validate-num_values-for-HIDIOCGUSAGES-HID.patch";
-        sha256 = "14rm1qr87p7a5prz8g5fwbpxzdp3ighj095x8rvhm8csm20wspyy";
-      };
-    };
-
   cpu-cgroup-v2 = import ./cpu-cgroup-v2-patches;
-
-  lguest_entry-linkage =
-    { name = "lguest-asmlinkage.patch";
-      patch = fetchpatch {
-        url = "https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git"
-            + "/patch/drivers/lguest/x86/core.c?id=cdd77e87eae52";
-        sha256 = "04xlx6al10cw039av6jkby7gx64zayj8m1k9iza40sw0fydcfqhc";
-      };
-    };
-
-  packet_fix_race_condition_CVE_2016_8655 =
-    { name = "packet_fix_race_condition_CVE_2016_8655.patch";
-      patch = fetchpatch {
-        url = "https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/patch/?id=84ac7260236a49c79eede91617700174c2c19b0c";
-        sha256 = "19viqjjgq8j8jiz5yhgmzwhqvhwv175q645qdazd1k69d25nv2ki";
-      };
-    };
-
-  panic_on_icmp6_frag_CVE_2016_9919 = rec
-    { name = "panic_on_icmp6_frag_CVE_2016_9919.patch";
-      patch = fetchpatch {
-        inherit name;
-        url = "https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/patch/?id=79dc7e3f1cd323be4c81aa1a94faa1b3ed987fb2";
-        sha256 = "0mps33r4mnwiy0bmgrzgqkrk59yya17v6kzpv9024g4xlz61rk8p";
-      };
-    };
 
   DCCP_double_free_vulnerability_CVE-2017-6074 = rec
     { name = "DCCP_double_free_vulnerability_CVE-2017-6074.patch";
