bfdd9d2e664a4b28fa7966b39af0de2ea41d8926
Robin Gloster <mail@glob.in>
2017-08-12 20:21:24 +0200

    automake114x: remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6607,8 +6607,6 @@ with pkgs;
 
   automake113x = callPackage ../development/tools/misc/automake/automake-1.13.x.nix { };
 
-  automake114x = callPackage ../development/tools/misc/automake/automake-1.14.x.nix { };
-
   automake115x = callPackage ../development/tools/misc/automake/automake-1.15.x.nix { };
 
   automoc4 = callPackage ../development/tools/misc/automoc4 { };
