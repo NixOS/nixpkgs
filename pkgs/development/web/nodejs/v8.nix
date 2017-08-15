c699694cbf4400480fe4267b7bee33b77747db07
Mathias Schreck <schreck.mathias@googlemail.com>
2017-08-16 00:47:17 +0200

    nodejs: 8.3.0 -> 8.4.0

--- pkgs/development/web/nodejs/v8.nix
+++ pkgs/development/web/nodejs/v8.nix
@@ -10,11 +10,11 @@ let
   baseName = if enableNpm then "nodejs" else "nodejs-slim";
 in
   stdenv.mkDerivation (nodejs // rec {
-    version = "8.3.0";
+    version = "8.4.0";
     name = "${baseName}-${version}";
     src = fetchurl {
       url = "https://nodejs.org/download/release/v${version}/node-v${version}.tar.xz";
-      sha256 = "0lbfp7j73ig0xa3gh8wnl4g3lji7lm34l0ybfys4swl187c3da63";
+      sha256 = "0qgkccsldpdrjxwwq78z94hsqz6qivmi4n2738iiginw06hs4njx";
     };
 
     patches = stdenv.lib.optionals stdenv.isDarwin [ ./no-xcode-v7.patch ];
