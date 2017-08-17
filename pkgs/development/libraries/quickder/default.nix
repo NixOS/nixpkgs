61b750cc7f1538f7dbc5a3febf58f9b1839f62bf
Michiel Leenaars <ml.software@leenaa.rs>
2017-08-17 18:02:30 +0000

    quickder: 1.0-RC2 -> 1.2.2

--- pkgs/development/libraries/quickder/default.nix
+++ pkgs/development/libraries/quickder/default.nix
@@ -1,36 +1,29 @@
-{ stdenv, fetchFromGitHub, fetchurl, hexio, python, which, asn2quickder, bash }:
+{ stdenv, fetchFromGitHub, fetchurl, pythonPackages, which, bash, cmake, git }:
 
 stdenv.mkDerivation rec {
   pname = "quickder";
   name = "${pname}-${version}";
-  version = "1.0-RC2";
+  version = "1.2-2";
 
   src = fetchFromGitHub {
-    sha256 = "1nzk8x6qzpvli8bf74dc2qya63nlppqjrnkaxvjxr2dbqb8qcrqd";
+    sha256 = "0bd8x4acfswpq3hrqmp22mfqq2l60swmybmki69jayzxb5m96p3w";
     rev = "version-${version}";
     owner = "vanrein";
     repo = "quick-der";
   };
 
-  buildInputs = [ which asn2quickder bash ];
+  buildInputs = [ which bash cmake git ];
+  propagatedBuildInputs = with pythonPackages; [ pyparsing asn1ate ];
 
-  patchPhase = ''
-    substituteInPlace Makefile \
-      --replace 'lib tool test rfc' 'lib test rfc'
-    substituteInPlace ./rfc/Makefile \
-      --replace 'ASN2QUICKDER_CMD = ' 'ASN2QUICKDER_CMD = ${asn2quickder}/bin/asn2quickder #'
-    '';
-
-  installFlags = "ASN2QUICKDER_DIR=${asn2quickder}/bin ASN2QUICKDER_CMD=${asn2quickder}/bin/asn2quickder";
-  installPhase = ''
-    mkdir -p $out/lib $out/man
-    make DESTDIR=$out PREFIX=/ all
-    make DESTDIR=$out PREFIX=/ install
+    buildPhase = with pythonPackages; ''
+      mkdir -p "$out/lib/${python.libPrefix}/site-packages"
+      export PYTHONPATH="${asn1ate}/lib/${python.libPrefix}/site-packages:$PYTHONPATH"
+      cmake .
     '';
 
   meta = with stdenv.lib; {
     description = "Quick (and Easy) DER, a Library for parsing ASN.1";
-    homepage = https://github.com/vanrein/quick-der;
+    homepage = "https://github.com/vanrein/quick-der";
     license = licenses.bsd2;
     platforms = platforms.linux;
     maintainers = with maintainers; [ leenaars ];
