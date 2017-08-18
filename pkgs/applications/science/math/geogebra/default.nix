54644b3b7840c1b3f3a571e0cef565b831706721
Maximilian Bosch <maximilian@mbosch.me>
2017-08-18 21:40:58 +0200

    geogebra: 5-0-377-0 -> 5-0-382-0
    (cherry picked from commit 242d950ab3f3d7b59551f9028c3f039283f1ac84)

--- pkgs/applications/science/math/geogebra/default.nix
+++ pkgs/applications/science/math/geogebra/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "geogebra-${version}";
-  version = "5-0-377-0";
+  version = "5-0-382-0";
 
   preferLocalBuild = true;
 
   src = fetchurl {
     url = "http://download.geogebra.org/installers/5.0/GeoGebra-Linux-Portable-${version}.tar.bz2";
-    sha256 = "0rvsjpf7pyz8z5yrqmc5ixzq7mnf1pyp00i914qd6wn5bx0apkxv";
+    sha256 = "0xqln1ssm35q8ry4a0ly8rkgw41brmrhn26l6q6r0qqrnw85cnyv";
   };
 
   srcIcon = fetchurl {
