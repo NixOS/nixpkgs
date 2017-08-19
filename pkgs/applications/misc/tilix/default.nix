d79530dd1b3733af4aa59841ff39734fb8276b86
midchildan <midchildan+git@gmail.com>
2017-08-19 15:58:59 +0900

    tilix: 1.6.1 -> 1.6.4

--- pkgs/applications/misc/tilix/default.nix
+++ pkgs/applications/misc/tilix/default.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation rec {
   name = "tilix-${version}";
-  version = "1.6.1";
+  version = "1.6.4";
 
   src = fetchFromGitHub {
     owner = "gnunn1";
     repo = "tilix";
     rev = "${version}";
-    sha256 = "10nw3q6s941dm44bkfryl1xclr1xy1vjr2n8w7g6kfahpcazf8f8";
+    sha256 = "1vqi68jlbbaky1569kd4lr6p02zsiv7v2rfb8j1pzwj7gydblaac";
   };
 
   nativeBuildInputs = [
