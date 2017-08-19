85da3514b0652e22434a94c1b2130b8ed0a4100b
Marko Poikonen <marko@poikonen.de>
2017-08-19 10:51:46 +0200

    libraw: 0.17.2 -> 0.18.2
    (cherry picked from commit a379ee6863ee42eb66629b1f6d96d13456aa2fa8)

--- pkgs/development/libraries/libraw/default.nix
+++ pkgs/development/libraries/libraw/default.nix
@@ -2,19 +2,12 @@
 
 stdenv.mkDerivation rec {
   name = "libraw-${version}";
-  version = "0.17.2";
+  version = "0.18.2";
 
   src = fetchurl {
     url = "http://www.libraw.org/data/LibRaw-${version}.tar.gz";
-    sha256 = "0p6imxpsfn82i0i9w27fnzq6q6gwzvb9f7sygqqakv36fqnc9c4j";
+    sha256 = "1imby9x88pjx4ad1frdi3bfb8dw90ccyj5pb6w3i6i0iijrnndnf";
   };
-
-  patches =
-    [ (fetchurl {
-        url = https://anonscm.debian.org/cgit/pkg-phototools/libraw.git/plain/debian/patches/0001-Fix_gcc6_narrowing_conversion.patch?id=d890937aaca6359df45a66b35e547c94ca564823;
-        sha256 = "1lcg5l0wmwiyzhhm67c1c7hy8py6ihxfmicnhrwpi3i6f16vq29w";
-      })
-    ];
 
   outputs = [ "out" "lib" "dev" "doc" ];
 
