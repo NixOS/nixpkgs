1360efeac6ee1d2154dfb596e456bfe22c67f3da
John Ericson <Ericson2314@Yahoo.com>
2017-08-22 15:17:12 -0400

    haskell infra: Make sure packages get custom stdenv
    One of the goals of 74f5fe5 was to allow passing in a custom stdenv,
    which would be used for genericBuilder's `mkDerivation` call. That does
    work, but if packages takes `stdenv` as an parameter for any reason,
    they'll get the default one instead. This change remedies it.
    
    (cherry picked from commit 19de1f537e377dbc314b3902e624439204f74f62)

--- pkgs/development/haskell-modules/make-package-set.nix
+++ pkgs/development/haskell-modules/make-package-set.nix
@@ -89,7 +89,7 @@ self: let
       };
     in stdenv.lib.makeOverridable drvScope (auto // manualArgs);
 
-  mkScope = scope: pkgs // pkgs.xorg // pkgs.gnome2 // scope;
+  mkScope = scope: pkgs // pkgs.xorg // pkgs.gnome2 // { inherit stdenv; } // scope;
   defaultScope = mkScope self;
   callPackage = drv: args: callPackageWithScope defaultScope drv args;
 
