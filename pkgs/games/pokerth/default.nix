85bc2bb860073ee569420a99d77c44982febf0ef
Matthew Bauer <mjbauer95@gmail.com>
2017-08-22 12:38:54 -0500

    pokerth: usee https for homepage

--- pkgs/games/pokerth/default.nix
+++ pkgs/games/pokerth/default.nix
@@ -36,7 +36,7 @@ in stdenv.mkDerivation rec {
   '';
 
   meta = with stdenv.lib; {
-    homepage    = http://www.pokerth.net/;
+    homepage    = https://www.pokerth.net;
     description = "Open Source Poker client and server";
     license     = licenses.gpl3;
     maintainers = with maintainers; [ obadz ];
