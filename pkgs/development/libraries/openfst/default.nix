61f2007dabc7d7b8808fd60999da11bd6aa7da78
Divam <dfordivam@gmail.com>
2017-08-22 11:02:19 +0900

    Minor edits

--- pkgs/development/libraries/openfst/default.nix
+++ pkgs/development/libraries/openfst/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
   pname = "openfst";
   version = "1.6.3";
 
-  src =fetchurl {
+  src = fetchurl {
     url = "http://www.openfst.org/twiki/pub/FST/FstDownload/${name}.tar.gz";
     sha256 = "5c28b6ccd017fc6ff94ebd0c73ed8ab37d48f563dab1c603856fb05bc9333d99";
   };
@@ -22,3 +22,4 @@ stdenv.mkDerivation rec {
     platforms = stdenv.lib.platforms.linux;
   };
 }
+
