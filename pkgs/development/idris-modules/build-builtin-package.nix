ca381067e77f01b875cacdee3fe0934c87b89bda
Thomas Tuegel <ttuegel@mailbox.org>
2017-08-22 19:48:56 -0500

    idrisPackages.build-builtin-package: Include version in name

--- pkgs/development/idris-modules/build-builtin-package.nix
+++ pkgs/development/idris-modules/build-builtin-package.nix
@@ -1,8 +1,12 @@
 # Build one of the packages that come with idris
 # name: The name of the package
 # deps: The dependencies of the package
-{ idris, build-idris-package, lib }: name: deps: build-idris-package {
-  inherit name;
+{ idris, build-idris-package, lib }: name: deps:
+let
+  inherit (builtins.parseDrvName idris.name) version;
+in
+build-idris-package {
+  name = "${name}-${version}";
 
   propagatedBuildInputs = deps;
 
