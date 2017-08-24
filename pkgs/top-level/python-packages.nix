4934a4d553843c1fb2bef1fb9075c0e7d86d455c
Alex Brandt <alunduil@alunduil.com>
2017-08-23 21:32:24 -0500

    pythonPackages.parse: init at 1.6.6

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -27574,6 +27574,8 @@ EOF
   thinc = callPackage ../development/python-modules/thinc { };
 
   spacy = callPackage ../development/python-modules/spacy { };
+ 
+  parse = callPackage ../development/python-modules/parse { };
 });
 
 in fix' (extends overrides packages)
