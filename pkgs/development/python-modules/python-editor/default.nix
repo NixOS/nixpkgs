1e4029fa0a36e9948c3df7afd276e9651cea1a85
Frederik Rietdijk <fridh@fridh.nl>
2017-08-25 10:34:58 +0200

    python.pkgs.python-editor: fix by disabling tests

--- pkgs/development/python-modules/python-editor/default.nix
+++ pkgs/development/python-modules/python-editor/default.nix
@@ -10,8 +10,12 @@ buildPythonPackage rec {
     sha256 = "a3c066acee22a1c94f63938341d4fb374e3fdd69366ed6603d7b24bed1efc565";
   };
 
+  # No proper tests
+  doCheck = false;
+
   meta = with stdenv.lib; {
     description = "A library that provides the `editor` module for programmatically";
     homepage = https://github.com/fmoo/python-editor;
+    license = licenses.asl20;
   };
 }
