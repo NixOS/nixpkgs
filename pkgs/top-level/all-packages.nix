9bdf89692cce9c43145a3c7775c9c781e08dc1a2
Roger Qiu <roger.qiu@polyhack.io>
2017-08-26 22:44:39 +1000

    flyway: init at 4.2.0 (#28586)
    * flyway: init at 4.2.0
    
    * flyway: simplify install phase

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7879,6 +7879,8 @@ with pkgs;
   fltk13 = callPackage ../development/libraries/fltk { };
   fltk = self.fltk13;
 
+  flyway = callPackage ../development/tools/flyway { };
+
   fplll = callPackage ../development/libraries/fplll {};
   fplll_20160331 = callPackage ../development/libraries/fplll/20160331.nix {};
 
