0387f84cf37acdf322aa020a6c4aac0eb1c07c28
Joachim Fasting <joachifm@fastmail.fm>
2017-08-26 18:44:06 +0200

    Revert "xsv: 0.12.1 -> 0.12.2"
    This reverts commit edb61d115126ac60fcf7ead646af10cf42d10c17.
    
    The sha256 is malformed.

--- pkgs/tools/text/xsv/default.nix
+++ pkgs/tools/text/xsv/default.nix
@@ -4,16 +4,16 @@ with rustPlatform;
 
 buildRustPackage rec {
   name = "xsv-${version}";
-  version = "0.12.2";
+  version = "0.12.1";
 
   src = fetchFromGitHub {
     owner = "BurntSushi";
     repo = "xsv";
     rev = "${version}";
-    sha256 = "0z1z3b6nzaid510jaikkawvpmv4kjphzz84p0hppq6vcp5jy00s2";
+    sha256 = "0xmjx5grwjrx2zsqmpblid9pqpwkk9pv468ffqlza3w35n9x5dax";
   };
 
-  depsSha256 = "013hy835871zxdnakwsr4bjm4krahlz4aqk5lh0rw78avfla89q9q";
+  depsSha256 = "0gdbxgykhy6wm89mbdvl7ck2v0f66hwlm0m1q7r64bkb7i10fmkd";
 
   meta = with stdenv.lib; {
     description = "A fast CSV toolkit written in Rust";
