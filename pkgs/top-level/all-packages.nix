db59381a5d1781dcb3e72814caff408b194c5aac
Bob van der Linden <bobvanderlinden@gmail.com>
2017-08-27 23:08:18 +0200

    ngrok: 1.7.1 -> 2.2.8, added ngrok1 1.1.7
    ngrok now releases a proprietary version (2.x). 1.1.7 can still be used
    with a self-hosted ngrokd, but it won't work out of the box.
    
    Therefore ngrok 2.2.8 is now the default for the `ngrok` package and
    ngrok 1.1.7 can be installed using the `ngrok1` package.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3418,7 +3418,11 @@ with pkgs;
 
   ngrep = callPackage ../tools/networking/ngrep { };
 
-  ngrok = callPackage ../tools/networking/ngrok { };
+  ngrok = ngrok2;
+
+  ngrok2 = callPackage ../tools/networking/ngrok { };
+
+  ngrok1 = callPackage ../tools/networking/ngrok1 { };
 
   noice = callPackage ../applications/misc/noice { };
 
