1089ccedb7362efebbbbd5e9a76a72e54c03dbc4
Jörg Thalheim <joerg@thalheim.io>
2017-08-27 15:32:28 +0100

    python.pkgs.pytest-warnings: init at 0.3.0

---
+++ pkgs/development/python-modules/pytest-warnings/default.nix
@@ -0,0 +1,20 @@
+{ stdenv, buildPythonPackage, fetchPypi, pytest }:
+
+buildPythonPackage rec {
+  name = "${pname}-${version}";
+  pname = "pytest-warnings";
+  version = "0.3.0";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "18yxh153icmndaw8fkl1va0bk0mwzrbpaa6wxd29w3iwxym5zn2a";
+  };
+
+  propagatedBuildInputs = [ pytest ];
+
+  meta = with stdenv.lib; {
+    description = "Plugin to list Python warnings in pytest report";
+    homepage = github.com/fschulze/pytest-warnings;
+    license = lib.licenses.mit;
+  };
+}
