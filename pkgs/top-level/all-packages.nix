d4d3659e54ff609c8acc1249d8a8d412a47fd30c
Vladimír Čunát <vcunat@gmail.com>
2017-08-28 18:12:40 +0200

    pprof: init at 2017-08-28
    (cherry picked from commit e0010cd1c3ef224730da3df2348d3b2a44c8637e)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6701,6 +6701,8 @@ with pkgs;
 
   postiats-utilities = callPackage ../development/tools/postiats-utilities {};
 
+  pprof = callPackage ../development/tools/profiling/pprof { };
+
   prelink = callPackage ../development/tools/misc/prelink { };
 
   premake3 = callPackage ../development/tools/misc/premake/3.nix { };
