e0f027f73e4a180305f97cebc6db785ad469f0cc
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-08-28 11:17:22 +0200

    ocamlPackages.ppx_deriving: 4.1 -> 4.2
    This fixes compilation of ppx_deriving with OCaml 4.05.

--- pkgs/development/ocaml-modules/ppx_deriving/default.nix
+++ pkgs/development/ocaml-modules/ppx_deriving/default.nix
@@ -1,23 +1,23 @@
 { stdenv, buildOcaml, ocaml, fetchzip
-, cppo, ppx_tools, result, ounit
+, cppo, ppx_tools, ppx_derivers, result, ounit, ocaml-migrate-parsetree
 }:
 
 buildOcaml rec {
   name = "ppx_deriving";
-  version = "4.1";
+  version = "4.2";
 
   minimumSupportedOcamlVersion = "4.02";
 
   src = fetchzip {
     url = "https://github.com/whitequark/${name}/archive/v${version}.tar.gz";
-    sha256 = "0cy9p8d8cbcxvqyyv8fz2z9ypi121zrgaamdlp4ld9f3jnwz7my9";
+    sha256 = "0scsg45wp6xdqj648fz155r4yngyl2xcd3hdszfzqwdpbax33914";
   };
 
   hasSharedObjects = true;
 
   buildInputs = [ cppo ounit ];
   propagatedBuildInputs =
-    [ ppx_tools result ];
+    [ ppx_tools ppx_derivers result ocaml-migrate-parsetree ];
 
   installPhase = "OCAMLPATH=$OCAMLPATH:`ocamlfind printconf destdir` make install";
 
@@ -25,6 +25,5 @@ buildOcaml rec {
     description = "deriving is a library simplifying type-driven code generation on OCaml >=4.02.";
     maintainers = [ maintainers.maurer ];
     license = licenses.mit;
-    broken = versionAtLeast ocaml.version "4.05";
   };
 }
