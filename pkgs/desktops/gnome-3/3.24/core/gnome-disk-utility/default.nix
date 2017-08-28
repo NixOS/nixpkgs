b64f149ea981b2e90b8230d15936283820729333
Jan Tojnar <jtojnar@gmail.com>
2017-08-28 23:27:04 +0200

    gnome-disk-utility: fix missing schemas for gnome-disk-image-mounter
    Continuation of #28053
    
    gnome-disk-image-mounter from gnome-disk-utility was not wrapped, resulting in an
    error due to the inability to find gsettings schemas.
    
    This commit replaces the manual wrapping of gnome-disks binary with wrapGAppsHook
    so that all binaries are wrapped correctly.

--- pkgs/desktops/gnome-3/3.24/core/gnome-disk-utility/default.nix
+++ pkgs/desktops/gnome-3/3.24/core/gnome-disk-utility/default.nix
@@ -1,6 +1,6 @@
 { stdenv, intltool, fetchurl, pkgconfig, udisks2, libsecret, libdvdread
-, bash, gtk3, glib, makeWrapper, cracklib, libnotify
-, itstool, gnome3, librsvg, gdk_pixbuf, libxml2, python
+, bash, gtk3, glib, wrapGAppsHook, cracklib, libnotify
+, itstool, gnome3, gdk_pixbuf, libxml2, python
 , libcanberra_gtk3, libxslt, libtool, docbook_xsl, libpwquality }:
 
 stdenv.mkDerivation rec {
@@ -16,14 +16,8 @@ stdenv.mkDerivation rec {
                   libxslt libtool libsecret libpwquality cracklib
                   libnotify libdvdread libcanberra_gtk3 docbook_xsl
                   gdk_pixbuf gnome3.defaultIconTheme
-                  librsvg udisks2 gnome3.gnome_settings_daemon
-                  gnome3.gsettings_desktop_schemas makeWrapper libxml2 ];
-
-  preFixup = ''
-    wrapProgram "$out/bin/gnome-disks" \
-      --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE" \
-      --prefix XDG_DATA_DIRS : "${gnome3.gnome_themes_standard}/share:$out/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
-  '';
+                  udisks2 gnome3.gnome_settings_daemon
+                  gnome3.gsettings_desktop_schemas wrapGAppsHook libxml2 ];
 
   meta = with stdenv.lib; {
     homepage = https://en.wikipedia.org/wiki/GNOME_Disks;
