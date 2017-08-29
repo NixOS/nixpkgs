1a16760bf03f3160b672b1208172e101a2c6bf7a
Will Dietz <w@wdtz.org>
2017-08-29 09:54:47 -0500

    massif-visualizer: 0.3.90 unstable -> 0.4.0 stable

--- pkgs/development/tools/analysis/massif-visualizer/default.nix
+++ pkgs/development/tools/analysis/massif-visualizer/default.nix
@@ -3,11 +3,11 @@
 
 stdenv.mkDerivation rec {
   name = "massif-visualizer-${version}";
-  version = "0.3.90";
+  version = "0.4.0";
 
   src = fetchurl {
-    url = "mirror://kde/unstable/massif-visualizer/${version}/src/${name}.tar.xz";
-    sha256 = "9940fa90137ca5eef08b9ec220825fadbf03db423a670a2c7fe3edab271d9922";
+    url = "mirror://kde/stable/massif-visualizer/${version}/src/${name}.tar.xz";
+    sha256 = "0yawzp5lw7ryjcpqkhz70q4hc5hwq5imq5lgqnwgss057zcln7z8";
   };
 
   nativeBuildInputs = [ automoc4 cmake gettext perl pkgconfig ];
