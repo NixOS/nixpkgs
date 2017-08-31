83c420f2749774e20c44005415889ed4cfc4b113
betaboon <betaboon@0x80.ninja>
2017-08-31 21:24:35 +0200

    pythonPackages.django-sites: init at 0.9

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8883,6 +8883,8 @@ in {
 
   django-sampledatahelper = callPackage ../development/python-modules/django-sampledatahelper { };
 
+  django-sites = callPackage ../development/python-modules/django-sites { };
+
   django_tagging = callPackage ../development/python-modules/django_tagging { };
 
   django_tagging_0_3 = self.django_tagging.overrideAttrs (attrs: rec {
