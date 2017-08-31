af51aa79d2bb8ebedd9c241e7e3be719e3d944ed
Graham Christensen <graham@grahamc.com>
2017-08-30 20:24:36 -0400

    installer: add a comment hinting about enabling libinput for touchpads

--- nixos/modules/installer/tools/nixos-generate-config.pl
+++ nixos/modules/installer/tools/nixos-generate-config.pl
@@ -605,6 +605,9 @@ $bootLoaderConfig
   # services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
 
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
   # Enable the KDE Desktop Environment.
   # services.xserver.displayManager.sddm.enable = true;
   # services.xserver.desktopManager.plasma5.enable = true;
