bdd6c037c0b64a60d3e5a118f9d73e326cbfd5d2
John Ericson <John.Ericson@Obsidian.Systems>
2017-08-31 15:29:03 -0400

    cc-wrapper: Use separate mangler for "bool" variables
    This avoids any `NIX_FOOBAR=1 1` not triggering conditions.

--- pkgs/build-support/cc-wrapper/utils.sh
+++ pkgs/build-support/cc-wrapper/utils.sh
@@ -1,16 +1,31 @@
 mangleVarList() {
-    declare var="$1"
+    local var="$1"
     shift
-    declare -a role_infixes=("$@")
+    local -a role_infixes=("$@")
 
-    outputVar="${var/+/_@infixSalt@_}"
-    export ${outputVar}+=''
+    local outputVar="${var/+/_@infixSalt@_}"
+    declare -gx ${outputVar}+=''
     # For each role we serve, we accumulate the input parameters into our own
     # cc-wrapper-derivation-specific environment variables.
     for infix in "${role_infixes[@]}"; do
-        inputVar="${var/+/${infix}}"
+        local inputVar="${var/+/${infix}}"
         if [ -v "$inputVar" ]; then
             export ${outputVar}+="${!outputVar:+ }${!inputVar}"
+        fi
+    done
+}
+
+mangleVarBool() {
+    local var="$1"
+    shift
+    local -a role_infixes=("$@")
+
+    local outputVar="${var/+/_@infixSalt@_}"
+    declare -gxi ${outputVar}+=0
+    for infix in "${role_infixes[@]}"; do
+        local inputVar="${var/+/${infix}}"
+        if [ -v "$inputVar" ]; then
+            let "${outputVar} |= ${!inputVar}"
         fi
     done
 }
