60b77f4785f80ab9be48a940e831637272a171c3
betaboon <betaboon@0x80.ninja>
2017-08-31 21:11:11 +0200

    pythonPackages.easy-thumbnails: init at 2.4.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5462,6 +5462,8 @@ in {
     };
   };
 
+  easy-thumbnails = callPackage ../development/python-modules/easy-thumbnails { };
+
   eccodes = if (isPy27) then
       (pkgs.eccodes.overrideAttrs (oldattrs: {
     name = "${python.libPrefix}-" + oldattrs.name;
