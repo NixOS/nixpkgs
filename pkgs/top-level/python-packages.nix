4658e7b1e385e7d23ad719cba1f1f0619bc99ced
betaboon <betaboon@0x80.ninja>
2017-08-31 21:27:20 +0200

    pythonPackages.djmail: init at 1.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9110,6 +9110,8 @@ in {
     };
   };
 
+  djmail = callPackage ../development/python-modules/djmail { };
+
   pillowfight = buildPythonPackage rec {
     name = "pillowfight-${version}";
     version = "0.2";
