42a4aa1e8397acf53cdb71b447f4253b1163f9a1
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2017-08-30 21:02:06 -0400

    dia: undo persistence wrapper
    The wrapper was introduced as workaround for #1691. The original bug was
    present in 0.97.2 but was fixed in 0.97.3.

--- pkgs/applications/graphics/dia/default.nix
+++ pkgs/applications/graphics/dia/default.nix
@@ -11,11 +11,6 @@ stdenv.mkDerivation rec {
     sha256 = "0d3x6w0l6fwd0l8xx06y1h56xf8ss31yzia3a6xr9y28xx44x492";
   };
 
-  correctPersistence = fetchurl {
-    url = https://launchpadlibrarian.net/132677658/persistence;
-    sha256 = "1rv6zv9i03bna4bdp1wzn72lg7kdwi900y1izdq0imibi54nxjsk";
-  };
-
   buildInputs =
     [ gtk2 perlXMLParser libxml2 gettext python libxml2Python docbook5
       libxslt docbook_xsl libart_lgpl
@@ -31,14 +26,6 @@ stdenv.mkDerivation rec {
   # It have no reasons to exist in a redistribuable package
   postInstall = ''
     rm $out/share/icons/hicolor/icon-theme.cache
-
-    cd "$out"/bin/
-    mv dia .dia-wrapped
-    echo '#! ${stdenv.shell}' >> dia
-    echo 'test -f "$HOME/.dia/persistence" || cp ${correctPersistence} "$HOME/.dia/persistence" ' >> dia
-    echo 'chmod u+rw "$HOME/.dia/persistence" ' >> dia
-    echo "\"$out/bin/"'.dia-wrapped" "$@"' >> dia
-    chmod a+x dia
   '';
 
   meta = {
