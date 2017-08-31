ea825f78d17f673094fa7074030020030651d6f8
wisut hantanong <wizzup@gmail.com>
2017-08-31 21:03:02 +0700

    pythonPackages.pyshp : move to separate expression
    (cherry picked from commit 489bfcfa0f80c3bac4877ef0a1fc13d0ca7b20e7)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17922,23 +17922,7 @@ in {
 
   pyrr = callPackage ../development/python-modules/pyrr { };
 
-  pyshp = buildPythonPackage rec {
-    name = "pyshp-${version}";
-    version = "1.2.3";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/p/pyshp/pyshp-${version}.tar.gz";
-      sha256 = "e18cc19659dadc5ddaa891eb780a6958094da0cf105a1efe0f67e75b4fa1cdf9";
-    };
-
-    buildInputs = with self; [ setuptools ];
-
-    meta = {
-      description = "Pure Python read/write support for ESRI Shapefile format";
-      homepage = https://github.com/GeospatialPython/pyshp;
-      license = licenses.mit;
-    };
-  };
+  pyshp = callPackage ../development/python-modules/pyshp { };
 
   pyspread = callPackage ../development/python-modules/pyspread { };
 
