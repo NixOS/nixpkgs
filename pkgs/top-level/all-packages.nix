79bca40c37d3e8de6bd0183a5515a22d403b1c80
Vladimír Čunát <vcunat@gmail.com>
2017-08-31 21:06:13 +0200

    ghostscript: include X by default
    Currently the closure-size difference between `ghostscript` and
    `ghostscriptX` was ~140 vs. ~142 MB, which was wasteful, as the output
    itself is ~40 MB. (x86_64-linux)
    
    Also make ghostscriptX the full derivation, including CUPS support.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18717,12 +18717,13 @@ with pkgs;
 
   gensgs = callPackage_i686 ../misc/emulators/gens-gs { };
 
-  ghostscript = callPackage ../misc/ghostscript {
-    x11Support = false;
+  ghostscript = callPackage ../misc/ghostscript rec {
     cupsSupport = config.ghostscript.cups or (!stdenv.isDarwin);
+    x11Support = cupsSupport; # with CUPS, X11 only adds very little
   };
 
   ghostscriptX = appendToName "with-X" (ghostscript.override {
+    cupsSupport = true;
     x11Support = true;
   });
 
