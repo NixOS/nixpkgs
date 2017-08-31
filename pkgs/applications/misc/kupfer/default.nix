c9b56dd03696913f9655beb77f3aea049c05ba6d
Jörg Thalheim <joerg@thalheim.io>
2017-08-31 18:50:28 +0100

    kupfer: wrap with wrapGAppsHook

--- pkgs/applications/misc/kupfer/default.nix
+++ pkgs/applications/misc/kupfer/default.nix
@@ -1,5 +1,4 @@
-{ lib
-, stdenv
+{ stdenv
 , makeWrapper
 , fetchurl
 , intltool
@@ -9,44 +8,58 @@
 , libwnck3
 , keybinder3
 , hicolor_icon_theme
+, wrapGAppsHook
 }:
 
-let inherit (python3Packages) mkPythonDerivation pygobject3 pyxdg dbus-python docutils;
-in  mkPythonDerivation rec {
+with python3Packages;
+
+buildPythonApplication rec {
   name = "kupfer-${version}";
   version = "319";
-  # Since this is an application, don't prefix it with python3.x-
-  namePrefix = "";
 
   src = fetchurl {
     url = "https://github.com/kupferlauncher/kupfer/releases/download/v${version}/kupfer-v${version}.tar.xz";
     sha256 = "0c9xjx13r8ckfr4az116bhxsd3pk78v04c3lz6lqhraak0rp4d92";
   };
 
-  buildInputs = [ intltool hicolor_icon_theme docutils ];
-  propagatedBuildInputs = [ pygobject3 gtk3 pyxdg dbus dbus-python libwnck3 keybinder3 ];
+  nativeBuildInputs = [ wrapGAppsHook intltool ];
+  buildInputs = [ hicolor_icon_theme docutils libwnck3 keybinder3 ];
+  propagatedBuildInputs = [ pygobject3 gtk3 pyxdg dbus-python ];
 
   configurePhase = ''
-    ./waf configure --prefix=$prefix
+    runHook preConfigure
+    python ./waf configure --prefix=$prefix
+    runHook postConfigure
   '';
 
   buildPhase = ''
-    ./waf
+    runHook preBuild
+    python ./waf
+    runHook postBuild
   '';
 
-  installPhase = ''
-    ./waf install
+  installPhase = let
+    pythonPath = (stdenv.lib.concatMapStringsSep ":"
+      (m: "${m}/lib/${python.libPrefix}/site-packages")
+      propagatedBuildInputs);
+  in ''
+    runHook preInstall
+    python ./waf install
 
-    # TODO: we're capturing unneeded build inputs here...
-    wrapProgram $out/bin/kupfer \
-        --prefix GI_TYPELIB_PATH : "$GI_TYPELIB_PATH" \
-        --prefix PYTHONPATH : "$PYTHONPATH" \
-        --set PYTHONNOUSERSITE 1
+    gappsWrapperArgs+=(
+      "--prefix" "PYTHONPATH" : pythonPath
+      "--set" "PYTHONNOUSERSITE" "1"
+    )
+
+    runHook postInstall
   '';
 
-  meta = with lib; {
+  doCheck = false; # no tests
+
+  meta = with stdenv.lib; {
     description = "A smart, quick launcher";
     homepage    = "https://kupferlauncher.github.io/";
     license     = licenses.gpl3;
+    platforms   = platforms.linux;
   };
 }
