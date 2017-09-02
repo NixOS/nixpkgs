831e310f2407dd432e0aa3a843e0e6ace0db682b
Vladimír Čunát <vcunat@gmail.com>
2017-09-02 20:51:16 +0200

    gkt3: maintenance 3.22.18 -> 3.22.19
    (cherry picked from commit 3980abe191d4b9f8d4d60b101f67f3658f380a56)

--- pkgs/development/libraries/gtk+/3.x.nix
+++ pkgs/development/libraries/gtk+/3.x.nix
@@ -13,7 +13,7 @@ with stdenv.lib;
 
 let
   ver_maj = "3.22";
-  ver_min = "18";
+  ver_min = "19";
   version = "${ver_maj}.${ver_min}";
 in
 stdenv.mkDerivation rec {
@@ -21,7 +21,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/gtk+/${ver_maj}/gtk+-${version}.tar.xz";
-    sha256 = "b64b1c2ec20adf128ac08ee704d1f4e7b0a8d3df097d51f62edb271c7bb1bf69";
+    sha256 = "9883d3ca5244f9b3168ce660e87d52900e141d0f7147dea0640bdd6fe8e5166c";
   };
 
   outputs = [ "out" "dev" ];
