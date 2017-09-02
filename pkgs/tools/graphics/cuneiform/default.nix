4bc42d9c355498fa4340823f52ba28b6948af3c9
Michael Raskin <7c6f434c@mail.ru>
2017-09-02 18:53:06 +0200

    Trying a less disruptive patch for Cuneiform

--- pkgs/tools/graphics/cuneiform/default.nix
+++ pkgs/tools/graphics/cuneiform/default.nix
@@ -1,18 +1,24 @@
-{ stdenv, fetchbzr, cmake, patchelf, imagemagick }:
+{ stdenv, fetchurl, cmake, patchelf, imagemagick }:
 
 stdenv.mkDerivation rec {
   name = "cuneiform-${version}";
   version = "1.1.0";
 
-  src = fetchbzr {
-    url = "lp:~f0ma/cuneiform-linux/devel";
-    rev = "540";
-    sha256 = "0sj7v3plf2rrc2vzxl946h9yfribc0jfn4b3ffppghxk2g6kicsb";
+  src = fetchurl {
+    url = "https://launchpad.net/cuneiform-linux/1.1/1.1/+download/cuneiform-linux-1.1.0.tar.bz2";
+    sha256 = "1bdvppyfx2184zmzcylskd87cxv56d8f32jf7g1qc8779l2hszjp";
   };
 
-  buildInputs = [
-    cmake imagemagick
+  patches = [
+  (fetchurl {
+    url = "https://git.archlinux.org/svntogit/community.git/plain/cuneiform/trunk/build-fix.patch?id=a2ec92f05de006b56d16ac6a6c370d54a554861a";
+    sha256 = "19cmrlx4khn30qqrpyayn7bicg8yi0wpz1x1bvqqrbvr3kwldxyj";
+  })
   ];
+
+  buildInputs = [ imagemagick ];
+
+  nativeBuildInputs = [ cmake ];
 
   meta = {
     description = "Multi-language OCR system";
