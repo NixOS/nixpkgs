9e66acf8d1fbebe7fead5e81cc591873e6a4ac34
Robin Gloster <mail@glob.in>
2017-09-05 00:49:04 +0200

    jenkins: 2.73 -> 2.77
    (cherry picked from commit 8502d94b31fb1aadbd984a7a0e8febbe274e8664)

--- pkgs/development/tools/continuous-integration/jenkins/default.nix
+++ pkgs/development/tools/continuous-integration/jenkins/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "jenkins-${version}";
-  version = "2.73";
+  version = "2.77";
 
   src = fetchurl {
     url = "http://mirrors.jenkins-ci.org/war/${version}/jenkins.war";
-    sha256 = "1q9q2vjza3slmbh6sbxak1y3ryvisphlh92nidsp88rv60zn3nv8";
+    sha256 = "1hmj5f14qpq58018q2jmdd4j36v2idsbb9caiakxfy08gppzhz00";
   };
 
   buildCommand = ''
