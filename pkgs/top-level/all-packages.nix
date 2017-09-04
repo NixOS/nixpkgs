896f0daa1a0ebdd70367aa08a64aeeeb6dea20dd
Samuel Leathers <sam@appliedtrust.com>
2017-09-04 09:14:57 -0400

    libctemplate: remove 2.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8702,8 +8702,6 @@ with pkgs;
 
   libctemplate = callPackage ../development/libraries/libctemplate { };
 
-  libctemplate_2_2 = callPackage ../development/libraries/libctemplate/2.2.nix { };
-
   libcouchbase = callPackage ../development/libraries/libcouchbase { };
 
   libcue = callPackage ../development/libraries/libcue { };
