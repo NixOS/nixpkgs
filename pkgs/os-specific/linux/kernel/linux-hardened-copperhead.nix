967077537b42fffca3a81a3af3a228d1864fd61e
Tim Steinbach <tim@nequissimus.com>
2017-09-04 11:09:29 -0400

    linux-copperhead: 4.12.10.a -> 4.13.a

--- pkgs/os-specific/linux/kernel/linux-hardened-copperhead.nix
+++ pkgs/os-specific/linux/kernel/linux-hardened-copperhead.nix
@@ -1,15 +1,25 @@
 { stdenv, hostPlatform, fetchFromGitHub, perl, buildLinux, ... } @ args:
 
-let
-  version = "4.12.10";
-  revision = "a";
-  sha256 = "00vm7bc4sfj2qj3yar9hy6qf8m2kmkxmxlf8q908jb1m541pfvpn";
-in
+with stdenv.lib;
 
+let
+  version = "4.13";
+  revision = "a";
+  sha256 = "1d118yi40yqzfjxdwl00h7alp1z0qq7rk5q14w3hs281ig773aip";
+
+  # modVersion needs to be x.y.z, will automatically add .0 if needed
+  modVersion = concatStrings (intersperse "." (take 3 (splitString "." "${version}.0")));
+
+  # branchVersion needs to be x.y
+  branchVersion = concatStrings (intersperse "." (take 2 (splitString "." version)));
+
+  modDirVersion = "${modVersion}-hardened";
+in
 import ./generic.nix (args // {
+  inherit modDirVersion;
+
   version = "${version}-${revision}";
-  extraMeta.branch = "4.12";
-  modDirVersion = "${version}-hardened";
+  extraMeta.branch = "${branchVersion}";
 
   src = fetchFromGitHub {
     inherit sha256;
