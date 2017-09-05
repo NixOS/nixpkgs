50bbd596597ad88d2a1d40f0ae87256021d2f2ed
Badi Abdul-Wahid <abdulwahidc@gmail.com>
2017-09-04 23:39:15 -0400

    nixos/unified-remote: create service

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -606,6 +606,7 @@
   ./services/web-apps/tt-rss.nix
   ./services/web-apps/selfoss.nix
   ./services/web-apps/quassel-webserver.nix
+  ./services/web-apps/unified-remote.nix
   ./services/web-servers/apache-httpd/default.nix
   ./services/web-servers/caddy.nix
   ./services/web-servers/fcgiwrap.nix
