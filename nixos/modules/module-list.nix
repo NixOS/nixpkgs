675cdbb03ac40300c5dee10b419b9274eb974d99
geistesk <github.jyf300hj@0x21.biz>
2017-09-08 17:56:57 +0200

    firehol: init at 3.1.4, iprange: init at 1.0.3
    This commit adds the FireHOL-software (v3.1.4) and it's dependency
    iprange (v1.0.3). The FireHOL-package also contains FireQOS, which
    is a human-friendly tc-wrapper. A NixOS-module to interact with
    FireQOS was also provided.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -392,6 +392,7 @@
   ./services/networking/fakeroute.nix
   ./services/networking/ferm.nix
   ./services/networking/firefox/sync-server.nix
+  ./services/networking/fireqos.nix
   ./services/networking/firewall.nix
   ./services/networking/flannel.nix
   ./services/networking/flashpolicyd.nix
