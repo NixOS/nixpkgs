c0e79833597730bde5998124950215e1ac901b3b
Daiderd Jordan <daiderd@gmail.com>
2017-09-09 23:42:16 +0200

    et: fix build with clang

--- pkgs/applications/misc/et/default.nix
+++ pkgs/applications/misc/et/default.nix
@@ -14,6 +14,11 @@ stdenv.mkDerivation rec {
   buildInputs = [ libnotify gdk_pixbuf ];
   nativeBuildInputs = [ pkgconfig ];
 
+  prePatch = ''
+    substituteInPlace Makefile \
+        --replace ' = gcc' ' ?= gcc'
+  '';
+
   installPhase = ''
     mkdir -p $out/bin
     cp et $out/bin
