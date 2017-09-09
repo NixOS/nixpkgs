b3bbfd9fc49822c706b96d259920587b4e33d0d9
zraexy <zraexy@gmail.com>
2017-09-08 23:33:41 -0800

    kreport: init at 3.0.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10127,6 +10127,8 @@ with pkgs;
 
     kproperty = callPackage ../development/libraries/kproperty { };
 
+    kreport = callPackage ../development/libraries/kreport { };
+
     kirigami = kirigami_1;
 
     libcommuni = callPackage ../development/libraries/libcommuni { };
