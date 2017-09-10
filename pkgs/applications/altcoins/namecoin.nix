b277ca17518dfc2159cc3252b86e81cdffb90798
lassulus <lassulus@lassul.us>
2017-09-10 10:28:11 +0200

    namecoin: 0.3.80 -> nc0.13.0rc1

--- pkgs/applications/altcoins/namecoin.nix
+++ pkgs/applications/altcoins/namecoin.nix
@@ -1,20 +1,32 @@
-{ stdenv, db4, boost, openssl, qt4, qmake4Hook, miniupnpc, unzip, namecoind }:
+{ stdenv, fetchFromGitHub, pkgconfig, autoreconfHook, openssl, db4, boost,
+libevent, libtool, libuuid, qt4, protobuf, withGui }:
 
 with stdenv.lib;
+
 stdenv.mkDerivation rec {
+  version = "nc0.13.0rc1";
+  name = "namecoin" + (toString (optional (!withGui) "d")) + "-" + version;
 
-  name = "namecoin-${version}";
-  version = namecoind.version;
-  src = namecoind.src;
+  src = fetchFromGitHub {
+    owner = "namecoin";
+    repo = "namecoin-core";
+    rev = version;
+    sha256 = "17zz0rm3js285w2assxp8blfx830rs0ambcsaqqfli9mnaik3m39";
+  };
 
-  buildInputs = [ db4 boost openssl unzip qt4 qmake4Hook miniupnpc ];
+  nativeBuildInputs = [ pkgconfig autoreconfHook ];
+  buildInputs = [ openssl db4 boost libtool libuuid
+                  protobuf libevent ]
+                  ++ optionals withGui [ qt4 ];
 
-  qmakeFlags = [ "USE_UPNP=-" ];
+  configureFlags = [ "--with-boost-libdir=${boost.out}/lib" ]
+                     ++ optionals withGui [ "--with-gui=qt4" ];
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp namecoin-qt $out/bin
-  '';
-
-  meta = namecoind.meta;
+  meta = {
+    description = "Decentralized open source information registration and transfer system based on the Bitcoin cryptocurrency";
+    homepage = http://namecoin.info;
+    license = licenses.mit;
+    maintainers = with maintainers; [ doublec AndersonTorres ];
+    platforms = platforms.linux;
+  };
 }
