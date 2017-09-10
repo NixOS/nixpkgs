b8a8f6f174518e327aa479a905e7b46458c18e04
Tim Steinbach <tim@nequissimus.com>
2017-09-10 12:11:20 -0400

    linux: 4.9.48 -> 4.9.49
    (cherry picked from commit 2ec7d290cd96d7bfb1ed61a1a8a2d5b0625bf260)

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,12 +1,12 @@
 { stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.9.48";
+  version = "4.9.49";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0y3s5xbh5axdvsx9iwz7sinbm7v6j2dvmj8dz46zj1mziwicagwq";
+    sha256 = "1ywiww2h1gf3ps305irzk7n9xkcgg9bclr2jw6r5cqxmh3qxrv2p";
   };
 
   kernelPatches = args.kernelPatches;
