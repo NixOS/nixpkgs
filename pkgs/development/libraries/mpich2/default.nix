362c84227332dddf536238e75acb7fd1c646714a
timor <timor.dd@googlemail.com>
2017-09-10 13:43:32 +0200

    mpich2: 1.4 -> 3.2

--- pkgs/development/libraries/mpich2/default.nix
+++ pkgs/development/libraries/mpich2/default.nix
@@ -1,33 +1,17 @@
 { stdenv, fetchurl, python, perl, gfortran }:
 
-let version = "1.4"; in
-stdenv.mkDerivation {
-  name = "mpich2-${version}";
+stdenv.mkDerivation  rec {
+  name = "mpich-${version}";
+  version = "3.2";
 
   src = fetchurl {
-    url = "http://www.mcs.anl.gov/research/projects/mpich2/downloads/tarballs/${version}/mpich2-${version}.tar.gz";
+    url = "http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz";
     sha256 = "0bvvk4n9g4rmrncrgs9jnkcfh142i65wli5qp1akn9kwab1q80z6";
   };
 
   configureFlags = "--enable-shared --enable-sharedlib";
 
-  buildInputs = [ python perl gfortran ];
-  propagatedBuildInputs = stdenv.lib.optional (stdenv ? glibc) stdenv.glibc;
-
-  patchPhase =
-    '' for i in $(find -type f -not -name Makefile.\*)
-       do
-         if grep -q /usr/bin/env "$i"
-         then
-             interpreter="$(cat $i | grep /usr/bin/env | sed -'es|^.*/usr/bin/env \([^ ]\+\).*$|\1|g')"
-             echo "file \`$i' -> interpreter \`$interpreter'"
-             path="$(type -P $interpreter)"
-             echo "\`/usr/bin/env $interpreter' -> \`$path' in \`$i'..."
-             sed -i "$i" -e "s|/usr/bin/env $interpreter|$path|g"
-         fi
-       done
-       true
-    '';
+  buildInputs = [ perl gfortran ];
 
   meta = {
     description = "Implementation of the Message Passing Interface (MPI) standard";
