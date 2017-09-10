274333e22299f1bef44b2044b296dc9e07780ce4
timor <timor.dd@googlemail.com>
2017-09-10 18:00:38 +0200

    ParaView: 4.0.1 -> 5.4.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15698,7 +15698,7 @@ with pkgs;
 
   pavucontrol = callPackage ../applications/audio/pavucontrol { };
 
-  paraview = callPackage ../applications/graphics/paraview { };
+  paraview = libsForQt5.callPackage ../applications/graphics/paraview { };
 
   packet = callPackage ../development/tools/packet { };
 
