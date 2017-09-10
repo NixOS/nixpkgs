1b261730f262a974a37126004b2df29e5f7f67cd
dywedir <dywedir@protonmail.ch>
2017-09-10 22:25:26 +0300

    feh: 2.19.3 -> 2.20

--- pkgs/applications/graphics/feh/default.nix
+++ pkgs/applications/graphics/feh/default.nix
@@ -6,11 +6,11 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "feh-${version}";
-  version = "2.19.3";
+  version = "2.20";
 
   src = fetchurl {
-    url = "http://feh.finalrewind.org/${name}.tar.bz2";
-    sha256 = "1l3yvv0l0ggwlfyhk84p2g9mrqvzqrg1fgalf88kzppvb9jppjay";
+    url = "https://feh.finalrewind.org/${name}.tar.bz2";
+    sha256 = "02vhdv16nf4kjna4inpbfy4k3p40bhl7xpc4kh4xvily14146l2b";
   };
 
   outputs = [ "out" "man" "doc" ];
@@ -44,7 +44,7 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "A light-weight image viewer";
-    homepage = https://derf.homelinux.org/projects/feh/;
+    homepage = "https://feh.finalrewind.org/";
     license = licenses.mit;
     maintainers = [ maintainers.viric maintainers.willibutz ];
     platforms = platforms.unix;
