9c723d4b2b757eebf830de8992ac51333f4f89bb
Tim Steinbach <tim@nequissimus.com>
2017-09-10 12:12:27 -0400

    linux: 4.12.11 -> 4.12.12

--- pkgs/os-specific/linux/kernel/linux-4.12.nix
+++ pkgs/os-specific/linux/kernel/linux-4.12.nix
@@ -1,12 +1,12 @@
 { stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.12.11";
+  version = "4.12.12";
   extraMeta.branch = "4.12";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "015kyh0v9lkk4a6sa7ysddi7dm639z5p45v2sh2kswsih4sf7kxf";
+    sha256 = "1d254yxn46ydp0x3s5cpyg4p74zvdwiqfiaaim1m3g6rwjmlkjpa";
   };
 
   kernelPatches = args.kernelPatches;
