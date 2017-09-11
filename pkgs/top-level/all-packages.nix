74ed524310e4a022bb143e72798a4cbf7752bb5e
Orivej Desh <orivej@gmx.fr>
2017-09-11 23:44:33 +0000

    aseprite: 0.9.5 -> 1.1.7
    (cherry picked from commit 05eb7ba6e4105d5efa0501b19b1e51ffb3c2f779)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13440,9 +13440,7 @@ with pkgs;
 
   atom = callPackage ../applications/editors/atom { };
 
-  aseprite = callPackage ../applications/editors/aseprite {
-    giflib = giflib_4_1;
-  };
+  aseprite = callPackage ../applications/editors/aseprite { };
 
   astroid = callPackage ../applications/networking/mailreaders/astroid { };
 
