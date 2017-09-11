ea18f35e768d7e37aa95c99ac3d04084ef2ea5ca
Hamish Hutchings <moredhel@aoeu.me>
2017-09-11 17:54:27 +0100

    Traefik init at 1.3.8
    (cherry picked from commit e0b81b9c3cebf46f6b3f7da1760fd60597f12ff0)

--- lib/maintainers.nix
+++ lib/maintainers.nix
@@ -237,6 +237,7 @@
   guillaumekoenig = "Guillaume Koenig <guillaume.edward.koenig@gmail.com>";
   guyonvarch = "Joris Guyonvarch <joris@guyonvarch.me>";
   hakuch = "Jesse Haber-Kucharsky <hakuch@gmail.com>";
+  hamhut1066 = "Hamish Hutchings <github@hamhut1066.com>";
   havvy = "Ryan Scheel <ryan.havvy@gmail.com>";
   hbunke = "Hendrik Bunke <bunke.hendrik@gmail.com>";
   hce = "Hans-Christian Esperer <hc@hcesperer.org>";
