b5763e0054b484187298fe2561d606a31ae49909
Michael Weiss <dev.primeos@gmail.com>
2017-09-11 15:13:08 +0200

    dynamips: init at 0.2.17
    (cherry picked from commit 1011c765983050a60da3e6fb7738022bb8829016)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13895,6 +13895,8 @@ with pkgs;
     patches = config.dwm.patches or [];
   };
 
+  dynamips = callPackage ../applications/virtualization/dynamips { };
+
   evilwm = callPackage ../applications/window-managers/evilwm {
     patches = config.evilwm.patches or [];
   };
