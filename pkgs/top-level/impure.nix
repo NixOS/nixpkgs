0c368ef02f40fdc01641959be0d6a3ee8c1df5ff
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2017-09-14 01:03:39 +0300

    treewide: Escape backslash in strings properly
    "\." is apparently the same as "." wheras the correct one is "\\."

--- pkgs/top-level/impure.nix
+++ pkgs/top-level/impure.nix
@@ -50,7 +50,7 @@ in
           # it's a directory, so the set of overlays from the directory, ordered lexicographically
           let content = readDir path; in
           map (n: import (path + ("/" + n)))
-            (builtins.filter (n: builtins.match ".*\.nix" n != null || pathExists (path + ("/" + n + "/default.nix")))
+            (builtins.filter (n: builtins.match ".*\\.nix" n != null || pathExists (path + ("/" + n + "/default.nix")))
               (attrNames content))
         else 
           # it's a file, so the result is the contents of the file itself
