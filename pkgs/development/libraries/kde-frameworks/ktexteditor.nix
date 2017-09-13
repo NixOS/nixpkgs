e468ef511945c4db32226e0194d449414db3525a
Thomas Tuegel <ttuegel@secure.mailbox.org>
2017-09-13 11:57:15 -0500

    Revert "kdeFrameworks: 5.37 -> 5.38"

--- pkgs/development/libraries/kde-frameworks/ktexteditor.nix
+++ pkgs/development/libraries/kde-frameworks/ktexteditor.nix
@@ -2,7 +2,7 @@
   mkDerivation, lib, copyPathsToStore,
   extra-cmake-modules, perl,
   karchive, kconfig, kguiaddons, ki18n, kiconthemes, kio, kparts, libgit2,
-  qtscript, qtxmlpatterns, sonnet, syntax-highlighting, qtquickcontrols
+  qtscript, qtxmlpatterns, sonnet, syntax-highlighting
 }:
 
 mkDerivation {
@@ -11,7 +11,7 @@ mkDerivation {
   nativeBuildInputs = [ extra-cmake-modules perl ];
   buildInputs = [
     karchive kconfig kguiaddons ki18n kiconthemes kio libgit2 qtscript
-    qtxmlpatterns sonnet syntax-highlighting qtquickcontrols
+    qtxmlpatterns sonnet syntax-highlighting
   ];
   propagatedBuildInputs = [ kparts ];
 }
