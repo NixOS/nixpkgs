e07535d6f955455e50d131806f95ee750c04889d
Matthias Beyer <mail@beyermatthias.de>
2017-09-14 17:03:27 +0200

    [WIP] gitAndTools.git-dit: 0.1.0 -> 0.3.0

--- pkgs/applications/version-management/git-and-tools/git-dit/default.nix
+++ pkgs/applications/version-management/git-and-tools/git-dit/default.nix
@@ -1,39 +1,22 @@
-{ stdenv, fetchFromGitHub, pandoc }:
+{ stdenv
+, fetchFromGitHub
+, rustPlatform
+}:
 
-stdenv.mkDerivation rec {
+with rustPlatform;
+
+buildRustPackage rec {
   name = "git-dit-${version}";
-  version = "0.1.0";
-
-  buildInputs = [ pandoc ];
+  version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "neithernut";
     repo = "git-dit";
     rev = "v${version}";
-    sha256 = "1rvp2dhnb8yqrracvfpvf8z1vz4fs0rii18hhrskr6n1sfd7x9kd";
+    sha256 = "1xl698ngl0b7hd21917zavxcyba267xcsmri2q5a8mgavyssjazp";
   };
 
-  # the Makefile doesn’t work, we emulate it below
-  dontBuild = true;
-
-  postPatch = ''
-    # resolve binaries to the right path
-    sed -e "s|exec git-dit-|exec $out/bin/git-dit-|" -i git-dit
-
-    # we change every git dit command to the local subcommand path
-    # (git dit foo -> /nix/store/…-git-dit/bin/git-dit-foo)
-    for script in git-dit-*; do
-      sed -e "s|git dit |$out/bin/git-dit-|g" -i "$script"
-    done
-  '';
-
-  installPhase = ''
-    mkdir -p $out/{bin,share/man/man1}
-    # from the Makefile
-    ${stdenv.lib.getBin pandoc}/bin/pandoc -s -t man git-dit.1.md \
-                                           -o $out/share/man/man1/git-dit.1
-    cp git-dit* $out/bin
-  '';
+  depsSha256 = "19lrj4i6vzmf22r6xg99zcwvzjpiar8pqin1m2nvv78xzxx5yvgb";
 
   meta = with stdenv.lib; {
     inherit (src.meta) homepage;
