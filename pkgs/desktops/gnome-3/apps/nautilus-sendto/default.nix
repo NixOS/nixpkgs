64e34730178cc924ead9349c8e43c16acb2b95a3
Jan Tojnar <jtojnar@gmail.com>
2017-09-17 01:35:19 +0200

    nautilus-sendto: 3.8.1 → 3.8.6

--- pkgs/desktops/gnome-3/apps/nautilus-sendto/default.nix
+++ pkgs/desktops/gnome-3/apps/nautilus-sendto/default.nix
@@ -1,18 +1,17 @@
-{ stdenv, fetchurl, glib, pkgconfig, gnome3, intltool
-, gobjectIntrospection, makeWrapper }:
+{ stdenv, fetchurl, meson, ninja, glib, pkgconfig, gnome3, appstream-glib, gettext }:
 
 stdenv.mkDerivation rec {
   name = "nautilus-sendto-${version}";
 
-  version = "3.8.1";
+  version = "3.8.6";
 
   src = fetchurl {
     url = "mirror://gnome/sources/nautilus-sendto/3.8/${name}.tar.xz";
-    sha256 = "03fa46bff271acdbdedab6243b2a84e5ed3daa19c81b69d087b3e852c8fe5dab";
+    sha256 = "164d7c6e8bae29c4579bcc67a7bf50d783662b1545b62f3008e7ea3c0410e04d";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ glib gobjectIntrospection intltool makeWrapper ];
+  nativeBuildInputs = [ meson ninja pkgconfig appstream-glib gettext ];
+  buildInputs = [ glib ];
 
   meta = with stdenv.lib; {
     description = "Integrates Evolution and Pidgin into the Nautilus file manager";
