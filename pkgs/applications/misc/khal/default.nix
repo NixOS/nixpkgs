e85ec8617b7cec4766f22c7d91efce6fa94a61c3
Matthias Beyer <mail@beyermatthias.de>
2017-09-20 13:03:19 +0200

    khal: 0.9.5 -> 0.9.7
    (cherry picked from commit d697403303faccc4349da1a248b2dd9ef16dd023)

--- pkgs/applications/misc/khal/default.nix
+++ pkgs/applications/misc/khal/default.nix
@@ -3,12 +3,12 @@
 with python3Packages;
 
 buildPythonApplication rec {
-  version = "0.9.5";
+  version = "0.9.7";
   name = "khal-${version}";
 
   src = fetchurl {
     url = "mirror://pypi/k/khal/khal-${version}.tar.gz";
-    sha256 = "0fvv0kjym9q8v20zbpr5m8ig65b8hva4p0c935qsdvgdni68jidr";
+    sha256 = "0x1p62ff7ggb172rjr6sbdrjh1gl3ck3bwxsqlsix8i5wycwvnmv";
   };
 
   LC_ALL = "en_US.UTF-8";
