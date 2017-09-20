1787043323491a8fa0d431e405f8127b4e19e2c3
Ruben Maher <r@rkm.id.au>
2017-09-20 10:09:15 +0930

    flow: 0.54.0 -> 0.55.0

--- pkgs/development/tools/analysis/flow/default.nix
+++ pkgs/development/tools/analysis/flow/default.nix
@@ -3,14 +3,14 @@
 with lib;
 
 stdenv.mkDerivation rec {
-  version = "0.54.0";
+  version = "0.55.0";
   name = "flow-${version}";
 
   src = fetchFromGitHub {
     owner = "facebook";
     repo = "flow";
     rev = "v${version}";
-    sha256 = "02z3jrfrr6gn056wplwkhymd1nyncjs67yl3vhv6n27jr3572npn";
+    sha256 = "042q2197m9l7r6j053dd4hqaqv7dsc5wh8h6aifkcmqa4ypp4w5r";
   };
 
   installPhase = ''
