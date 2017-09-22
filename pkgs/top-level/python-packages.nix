053d9a4dabedf429a1133f18b06f9184250087f4
Joerg Thalheim <joerg@thalheim.io>
2017-09-22 10:54:01 +0100

    topydo: use throw instead of trace to not break evaluation
    (cherry picked from commit f2aecac5c74d82e8720b89964c8691473ca578ea)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -26034,7 +26034,7 @@ EOF
     };
   };
 
-  topydo = builtins.trace "python3Packages.topydo was moved to topydo" pkgs.topydo; # 2017-09-22
+  topydo = throw "python3Packages.topydo was moved to topydo"; # 2017-09-22
 
   w3lib = buildPythonPackage rec {
     name = "w3lib-${version}";
