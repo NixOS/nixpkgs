e5a864fa6df12fbf529225326baf96aa8d60ad4f
Thomas Tuegel <ttuegel@mailbox.org>
2017-09-22 06:50:54 -0500

    hplip: remove 3.15.9
    hplip-3.15.9 has not been buildable in some time due to changes in CUPS.
    
    (cherry picked from commit 4c09acad5c1fbae2ebe3ccb3fbf0115d61b3806b)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18782,10 +18782,6 @@ with pkgs;
 
   hplipWithPlugin = hplip.override { withPlugin = true; };
 
-  hplip_3_15_9 = callPackage ../misc/drivers/hplip/3.15.9.nix { };
-
-  hplipWithPlugin_3_15_9 = hplip_3_15_9.override { withPlugin = true; };
-
   epkowa = callPackage ../misc/drivers/epkowa { };
 
   illum = callPackage ../tools/system/illum { };
