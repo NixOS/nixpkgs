06caa2ac3a4682ccbc595b4224bde67094ecfe34
Kranium Gikos Mendoza <kranium@gikos.net>
2017-09-22 21:37:26 +1000

    trackballs: 1.1.4 (broken) -> 1.2.3
    (cherry picked from commit 92ec5cddbce1091d9a457b238b5fd28b56f10876)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17709,10 +17709,7 @@ with pkgs;
 
   tome4 = callPackage ../games/tome4 { };
 
-  trackballs = callPackage ../games/trackballs {
-    debug = false;
-    guile = guile_1_8;
-  };
+  trackballs = callPackage ../games/trackballs { };
 
   tremulous = callPackage ../games/tremulous { };
 
