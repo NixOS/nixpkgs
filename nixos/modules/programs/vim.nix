b6203c03065885b867189e8e14f5ba6acdcc1aa7
Jaka Hudoklin <jakahudoklin@gmail.com>
2017-09-23 20:30:03 +0200

    nixos/vim: add enable option
    Adds `enable` option that installs vim regardless if `defaultEditor`
    option is set.

--- nixos/modules/programs/vim.nix
+++ nixos/modules/programs/vim.nix
@@ -6,31 +6,36 @@ let
   cfg = config.programs.vim;
 in {
   options.programs.vim = {
+    enable = mkOption {
+      type = types.bool;
+      default = cfg.defaultEditor;
+      description = "When enabled, installs vim globally";
+    };
+
     defaultEditor = mkOption {
       type = types.bool;
       default = false;
       description = ''
-        When enabled, installs vim and configures vim to be the default editor
-        using the EDITOR environment variable.
-        '';
+        Configures vim to be the default editor using the EDITOR environment
+        variable and installs vim globally by default.
+      '';
     };
 
     vimrc = mkOption {
       type = types.lines;
       default = "";
-      description = ''
-        Global vimrc configuration
-      '';
+      description = "Global vimrc configuration";
     };
   };
 
-  config = mkIf cfg.defaultEditor {
-    environment.systemPackages = [ pkgs.vim_configurable ];
-    environment.variables = { EDITOR = mkOverride 900 "vim"; };
+  config = mkMerge [{
+    environment.systemPackages = mkIf cfg.enable [ pkgs.vim_configurable ];
 
     environment.etc."vimrc" = {
       enable = cfg.vimrc != "";
       text = cfg.vimrc;
     };
-  };
+  } (mkIf cfg.defaultEditor {
+    environment.variables = { EDITOR = mkOverride 900 "vim"; };
+  })];
 }
