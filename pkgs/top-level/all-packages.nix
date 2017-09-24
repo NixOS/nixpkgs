f79fde1ca4da148fd7a440d376e85b6670d4560d
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2017-09-24 21:04:04 +0200

    routino: init at 3.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4179,6 +4179,8 @@ with pkgs;
 
   rnv = callPackage ../tools/text/xml/rnv { };
 
+  routino = callPackage ../tools/misc/routino { };
+
   rq = callPackage ../development/tools/rq {
     v8 = v8_static;
   };
