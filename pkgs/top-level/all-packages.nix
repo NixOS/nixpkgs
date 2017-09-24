b0f8a85ed9bb5c879432c2edfa71078fc3267533
Michal Rus <m@michalrus.com>
2017-09-24 02:10:30 +0200

    xpad: init at 5.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19288,6 +19288,10 @@ with pkgs;
 
   xosd = callPackage ../misc/xosd { };
 
+  xpad = callPackage ../applications/misc/xpad {
+    inherit (gnome3) gtksourceview;
+  };
+
   xsane = callPackage ../applications/graphics/sane/xsane.nix {
     libpng = libpng12;
   };
