3e1940744a730de238c4f7b862b09b9f2abd358d
Joerg Thalheim <joerg@thalheim.io>
2017-09-24 15:57:49 +0100

    python.pkgs.simanneal: enable tests
    (cherry picked from commit dd9959295205489016e1877f96a0f0c83eef5743)

--- pkgs/development/python-modules/simanneal/default.nix
+++ pkgs/development/python-modules/simanneal/default.nix
@@ -1,19 +1,24 @@
-{ lib, fetchPypi, buildPythonPackage }:
+{ stdenv, fetchFromGitHub, buildPythonPackage, pytest }:
 
 buildPythonPackage rec {
   pname = "simanneal";
   version = "0.4.1";
   name = "${pname}-${version}";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "1ib1hv2adq6x25z13nb4s1asci6dqarpyhpdq144y2rqqff2m01x";
+  src = fetchFromGitHub {
+    owner = "perrygeo";
+    repo = "simanneal";
+    rev = version;
+    sha256 = "12499wvf7ii7cy8z2f1d472p7q9napg1lj0h9xx8l1mbr1hjlp3q";
   };
 
-  meta = {
+  checkInputs = [ pytest ];
+  checkPhase = "pytest tests";
+
+  meta = with stdenv.lib; {
     description = "A python implementation of the simulated annealing optimization technique";
     homepage = https://github.com/perrygeo/simanneal;
-    license = lib.licenses.isc;
-    maintainers = with lib.maintainers; [ veprbl ];
+    license = licenses.isc;
+    maintainers = with maintainers; [ veprbl ];
   };
 }
