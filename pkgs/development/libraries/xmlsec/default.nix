0208f5422971782898bbbfa7647c128ee6547228
Eelco Dolstra <edolstra@gmail.com>
2017-09-25 19:49:21 +0200

    Add more dev outputs
    (cherry picked from commit 8ccce8de891923b40b897c11f9a494c6b3722a58)

--- pkgs/development/libraries/xmlsec/default.nix
+++ pkgs/development/libraries/xmlsec/default.nix
@@ -12,12 +12,20 @@ stdenv.mkDerivation rec {
     sha256 = "17qfw5crkqn4v6xbkjxrjvcccfc00dy053892wrwv54qdk8n7m21";
   };
 
+  outputs = [ "out" "dev" ];
+
   buildInputs = [ makeWrapper libxml2 gnutls libxslt pkgconfig libgcrypt libtool openssl ];
+
   enableParallelBuilding = true;
   doCheck = true;
 
   # otherwise libxmlsec1-gnutls.so won't find libgcrypt.so, after #909
   NIX_LDFLAGS = [ "-lgcrypt" ];
+
+  postInstall = ''
+    moveToOutput "bin/xmlsec1-config" "$dev"
+    moveToOutput "lib/xmlsec1Conf.sh" "$dev"
+  '';
 
   postFixup = ''
     wrapProgram "$out/bin/xmlsec1" --prefix LD_LIBRARY_PATH ":" "$out/lib"
