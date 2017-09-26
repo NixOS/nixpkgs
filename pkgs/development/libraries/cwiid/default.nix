eba5dd84eff82f01a9867614544c7965736f8a3d
Peter Hoeg <peter@hoeg.com>
2017-09-26 23:11:44 +0800

    cwiid: boy scout cleanups

--- pkgs/development/libraries/cwiid/default.nix
+++ pkgs/development/libraries/cwiid/default.nix
@@ -1,12 +1,14 @@
-{ stdenv, autoreconfHook, fetchgit, bison, flex, bluez, pkgconfig, gtk2 }:
+{ stdenv, fetchFromGitHub, autoreconfHook, bison, flex, bluez, pkgconfig, gtk2 }:
 
 stdenv.mkDerivation rec {
-  name = "cwiid-2010-02-21-git";
+  name = "cwiid-${version}-git";
+  version = "2010-02-21";
 
-  src = fetchgit {
-      url = https://github.com/abstrakraft/cwiid;
-      sha256 = "0qdb0x757k76nfj32xc2nrrdqd9jlwgg63vfn02l2iznnzahxp0h";
-      rev = "fadf11e89b579bcc0336a0692ac15c93785f3f82";
+  src = fetchFromGitHub {
+    owner  = "abstrakraft";
+    repo   = "cwiid";
+    rev    = "fadf11e89b579bcc0336a0692ac15c93785f3f82";
+    sha256 = "0qdb0x757k76nfj32xc2nrrdqd9jlwgg63vfn02l2iznnzahxp0h";
   };
 
   hardeningDisable = [ "format" ];
@@ -17,18 +19,20 @@ stdenv.mkDerivation rec {
     sed -i -e '/$(LDCONFIG)/d' common/include/lib.mak.in
   '';
 
-  buildInputs = [ autoreconfHook bison flex bluez pkgconfig gtk2 ];
+  buildInputs = [ bison flex bluez gtk2 ];
+
+  nativeBuildInputs = [ autoreconfHook pkgconfig ];
 
   postInstall = ''
     # Some programs (for example, cabal-install) have problems with the double 0
     sed -i -e "s/0.6.00/0.6.0/" $out/lib/pkgconfig/cwiid.pc
   '';
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Linux Nintendo Wiimote interface";
-    homepage = http://cwiid.org;
-    license = stdenv.lib.licenses.gpl2Plus;
-    maintainers = [ stdenv.lib.maintainers.bennofs ];
-    platforms = stdenv.lib.platforms.linux;
+    homepage    = http://cwiid.org;
+    license     = licenses.gpl2Plus;
+    maintainers = with maintainers; [ bennofs ];
+    platforms   = platforms.linux;
   };
 }
