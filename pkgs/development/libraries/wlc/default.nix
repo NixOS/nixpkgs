1ee3559c4c12e4520593fbeecfb64cefe4dc7acd
Michael Weiss <root@tracer.primeos.eu>
2017-09-26 16:53:42 +0200

    wlc: Refactor the code
    The code was a bit messy (unused parameters, etc.) and caused some
    warnings/errors which could potentially cause some problems.
    
    (cherry picked from commit 4b85b235348698460f6b0f90b312c6c097d247a7)

--- pkgs/development/libraries/wlc/default.nix
+++ pkgs/development/libraries/wlc/default.nix
@@ -1,36 +1,37 @@
-{ lib, stdenv, fetchgit, cmake, pkgconfig
-, glibc, wayland, pixman, libxkbcommon, libinput, libxcb, xcbutilwm, xcbutilimage, mesa, libdrm, udev, systemd, dbus_libs
-, libpthreadstubs, libX11, libXau, libXdmcp, libXext, libXdamage, libxshmfence, libXxf86vm
-, wayland-protocols
+{ stdenv, fetchFromGitHub, cmake, pkgconfig
+, wayland, pixman, libxkbcommon, libinput, libxcb, xcbutilwm, xcbutilimage, mesa
+, libdrm, udev, libX11, libXdamage, systemd, dbus_libs, wayland-protocols
+, libpthreadstubs, libXau, libXdmcp, libXext, libXxf86vm
 }:
 
 stdenv.mkDerivation rec {
   name = "wlc-${version}";
   version = "0.0.9";
 
-  src = fetchgit {
-    url = "https://github.com/Cloudef/wlc";
-    rev = "refs/tags/v${version}";
-    sha256 = "1r6jf64gs7n9a8129wsc0mdwhcv44p8k87kg0714rhx3g2w22asg";
+  src = fetchFromGitHub {
+    owner = "Cloudef";
+    repo = "wlc";
+    rev = "v${version}";
     fetchSubmodules = true;
-   };
+    sha256 = "1r6jf64gs7n9a8129wsc0mdwhcv44p8k87kg0714rhx3g2w22asg";
+  };
 
   nativeBuildInputs = [ cmake pkgconfig ];
 
   buildInputs = [
-    wayland pixman libxkbcommon libinput libxcb xcbutilwm xcbutilimage mesa libdrm udev
-    libX11 libXdamage systemd dbus_libs wayland-protocols
+    wayland pixman libxkbcommon libinput libxcb xcbutilwm xcbutilimage mesa
+    libdrm udev libX11 libXdamage systemd dbus_libs wayland-protocols
+    libpthreadstubs libXau libXdmcp libXext libXxf86vm
   ];
-
 
   doCheck = true;
   checkTarget = "test";
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "A library for making a simple Wayland compositor";
-    homepage    = https://github.com/Cloudef/wlc;
-    license     = lib.licenses.mit;
-    platforms   = lib.platforms.linux;
-    maintainers = with lib.maintainers; [ ];
+    inherit (src.meta) homepage;
+    license     = licenses.mit;
+    platforms   = platforms.linux;
+    maintainers = with maintainers; [ primeos ]; # Trying to keep it up-to-date.
   };
 }
