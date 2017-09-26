e96e0a21c6c083274bf3db650a542aa8e44ecae0
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-09-26 12:28:02 +0000

    libmirage: add to top level
    (cherry picked from commit 8815b819264e17b094767e8dfdc49ccdc92070f7)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9196,6 +9196,8 @@ with pkgs;
 
   libminc = callPackage ../development/libraries/libminc { };
 
+  libmirage = callPackage ../misc/emulators/cdemu/libmirage.nix { }; 
+  
   libmkv = callPackage ../development/libraries/libmkv { };
 
   libmms = callPackage ../development/libraries/libmms { };
