547dba10c01207dc1d9ebfcf53867d5d8ee26a8c
volth <volth@webmaster.ms>
2017-09-27 13:37:22 +0000

    libmicrohttpd: fix darwin build

--- pkgs/development/libraries/libmicrohttpd/default.nix
+++ pkgs/development/libraries/libmicrohttpd/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, libgcrypt, curl, gnutls, pkgconfig }:
+{ stdenv, fetchurl, libgcrypt, curl, gnutls, pkgconfig, libiconv, libintlOrEmpty }:
 
 stdenv.mkDerivation rec {
   name = "libmicrohttpd-0.9.55";
@@ -9,7 +9,8 @@ stdenv.mkDerivation rec {
   };
 
   outputs = [ "out" "dev" "devdoc" "info" ];
-  buildInputs = [ libgcrypt curl gnutls pkgconfig ];
+  buildInputs = [ libgcrypt curl gnutls pkgconfig ]
+    ++ stdenv.lib.optionals stdenv.isDarwin [ libiconv libintlOrEmpty ];
 
   preCheck = ''
     # Since `localhost' can't be resolved in a chroot, work around it.
@@ -32,6 +33,7 @@ stdenv.mkDerivation rec {
     homepage = http://www.gnu.org/software/libmicrohttpd/;
 
     maintainers = with maintainers; [ eelco vrthra fpletz ];
-    platforms = platforms.linux;
+    platforms = platforms.unix;
   };
 }
+
