c35be631b449b1abbdeb7a99053f3b9df9d3e28e
Michael Weiss <dev.primeos@gmail.com>
2017-09-27 20:17:58 +0200

    wlc: Downgrade back to 0.0.9
    Unfortunately wlc 0.0.10 seems to be the cause for segfaults on sway,
    way-cooler and orbment.
    
    This will also build wlc with all optional packages (i.e. zlib,
    valgrind and doxygen).
    
    (cherry picked from commit 2d640b9d6ee45e46ff510744e849175e9cb55d05)

--- pkgs/development/libraries/wlc/default.nix
+++ pkgs/development/libraries/wlc/default.nix
@@ -2,18 +2,19 @@
 , wayland, pixman, libxkbcommon, libinput, libxcb, xcbutilwm, xcbutilimage, mesa
 , libdrm, udev, libX11, libXdamage, systemd, dbus_libs, wayland-protocols
 , libpthreadstubs, libXau, libXdmcp, libXext, libXxf86vm
+, withOptionalPackages ? true, zlib, valgrind, doxygen
 }:
 
 stdenv.mkDerivation rec {
   name = "wlc-${version}";
-  version = "0.0.10";
+  version = "0.0.9"; # 0.0.10 currently causes segfaults
 
   src = fetchFromGitHub {
     owner = "Cloudef";
     repo = "wlc";
     rev = "v${version}";
     fetchSubmodules = true;
-    sha256 = "09kvwhrpgkxlagn9lgqxc80jbg56djn29a6z0n6h0dsm90ysyb2k";
+    sha256 = "1r6jf64gs7n9a8129wsc0mdwhcv44p8k87kg0714rhx3g2w22asg";
   };
 
   nativeBuildInputs = [ cmake pkgconfig ];
@@ -21,8 +22,8 @@ stdenv.mkDerivation rec {
   buildInputs = [
     wayland pixman libxkbcommon libinput libxcb xcbutilwm xcbutilimage mesa
     libdrm udev libX11 libXdamage systemd dbus_libs wayland-protocols
-    libpthreadstubs libXau libXdmcp libXext libXxf86vm
-  ];
+    libpthreadstubs libXau libXdmcp libXext libXxf86vm ]
+    ++ stdenv.lib.optionals withOptionalPackages [ zlib valgrind doxygen ];
 
   doCheck = true;
   checkTarget = "test";
