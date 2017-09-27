14befbf78d34b0d3ea251a2d0bcbc1665c10f2fa
xd1le <elisp.vim@gmail.com>
2017-09-27 23:27:26 +1000

    guile-lint: code formatting nits

--- pkgs/development/tools/guile/guile-lint/default.nix
+++ pkgs/development/tools/guile/guile-lint/default.nix
@@ -1,15 +1,20 @@
-{stdenv, fetchurl, guile}:
+{ stdenv, fetchurl, guile }:
 
-stdenv.mkDerivation rec {
-  name = "guile-lint-14";
+let
+  name = "guile-lint-${version}";
+  version = "14";
+in stdenv.mkDerivation {
+  inherit name;
+
   src = fetchurl {
-    url = "http://download.tuxfamily.org/user42/" + name + ".tar.bz2";
+    url = "http://download.tuxfamily.org/user42/${name}.tar.bz2";
     sha256 = "5bfcf7a623338b2ef81ac097e3e136eaf32856dd0730b7eeaff3161067b5d0be";
   };
 
   buildInputs = [ guile ];
 
   unpackPhase = ''tar xjvf "$src" && sourceRoot="$PWD/${name}"'';
+
   patchPhase = ''
     cat guile-lint.in |						\
     sed 's|^exec guile|exec $\{GUILE:-${guile}/bin/guile}|g' > ,,tmp &&	\
@@ -20,7 +25,7 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "Guile-Lint checks syntax and semantics in a Guile program or module";
-    homepage = http://user42.tuxfamily.org/guile-lint/index.html;
+    homepage = "http://user42.tuxfamily.org/guile-lint/index.html";
     license = "GPL";
   };
 }
