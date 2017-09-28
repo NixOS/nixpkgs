47710757db8d768d883b09e9e20bccf3bc35857b
xd1le <elisp.vim@gmail.com>
2017-09-28 14:42:55 +1000

    guile-sdl2: set the correct upstream license

--- pkgs/development/guile-modules/guile-sdl2/default.nix
+++ pkgs/development/guile-modules/guile-sdl2/default.nix
@@ -36,7 +36,7 @@ in stdenv.mkDerivation {
   meta = with stdenv.lib; {
     description = "Bindings to SDL2 for GNU Guile";
     homepage = "https://dthompson.us/projects/guile-sdl2.html";
-    license = licenses.gpl3;
+    license = licenses.lgpl3Plus;
     maintainers = with maintainers; [ seppeljordan vyp ];
     platforms = platforms.all;
   };
