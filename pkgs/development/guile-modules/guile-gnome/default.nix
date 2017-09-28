b3dab5b088a75f095198d73a1cd2b3acf2a94a36
Robin Gloster <mail@glob.in>
2017-09-28 14:48:19 +0200

    guile-gnome: nitpicks fixed

--- pkgs/development/guile-modules/guile-gnome/default.nix
+++ pkgs/development/guile-modules/guile-gnome/default.nix
@@ -1,7 +1,7 @@
 { fetchurl, stdenv, guile, guile-lib, gwrap
 , pkgconfig, gconf, glib, gnome_vfs, gtk2
 , libglade, libgnome, libgnomecanvas, libgnomeui
-, pango, guile-cairo, autoconf, automake, texinfo
+, pango, guile-cairo, texinfo
 }:
 
 stdenv.mkDerivation rec {
@@ -10,19 +10,19 @@ stdenv.mkDerivation rec {
   version = "2.16.4";
 
   src = fetchurl {
-    url = "http://ftp.gnu.org/pub/gnu/guile-gnome/${pname}/${name}.tar.gz";
+    url = "mirror://gnu/guile-gnome/${pname}/${name}.tar.gz";
     sha256 = "adabd48ed5993d8528fd604e0aa0d96ad81a61d06da6cdd68323572ad6c216c3";
   };
 
   buildInputs = [
-    autoconf automake texinfo guile gwrap pkgconfig gconf glib gnome_vfs gtk2
+    texinfo guile gwrap pkgconfig gconf glib gnome_vfs gtk2
     libglade libgnome libgnomecanvas libgnomeui pango guile-cairo
   ] ++ stdenv.lib.optional doCheck guile-lib;
 
-  preConfigure = "./autogen.sh";
-
   # The test suite tries to open an X display, which fails.
   doCheck = false;
+
+  GUILE_AUTO_COMPILE = 0;
 
   meta = with stdenv.lib; {
     description = "GNOME bindings for GNU Guile";
