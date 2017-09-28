8543e31f66aff5359b0f5c8cd8807580ee2d177b
xd1le <elisp.vim@gmail.com>
2017-09-28 14:54:04 +1000

    guile-sdl2: 0.1.0 -> 0.2.0
    Autoconf, automake, and running `./bootstrap` is no longer required
    because we are now building from the tarball, which includes the
    generated ./configure script.

--- pkgs/development/guile-modules/guile-sdl2/default.nix
+++ pkgs/development/guile-modules/guile-sdl2/default.nix
@@ -1,28 +1,23 @@
-{ stdenv, fetchgit, autoconf, automake, guile, libtool, pkgconfig
+{ stdenv, fetchurl, guile, libtool, pkgconfig
 , SDL2, SDL2_image, SDL2_ttf, SDL2_mixer
 }:
 
 let
   name = "${pname}-${version}";
   pname = "guile-sdl2";
-  version = "0.1.0";
+  version = "0.2.0";
 in stdenv.mkDerivation {
   inherit name;
 
-  src = fetchgit {
-    url = "git://dthompson.us/guile-sdl2.git";
-    rev = "048f80ddb5c6b03b87bba199a99a6f22d911bfff";
-    sha256 = "1v7bc2bsddb46qdzq7cyzlw5i2y175kh66mbzbjky85sjfypb084";
+  src = fetchurl {
+    url = "https://files.dthompson.us/${pname}/${name}.tar.gz";
+    sha256 = "0yq9lsl17cdvj77padvpk3jcw2g6g0pck9jrchc7n2767rrc012b";
   };
 
   buildInputs = [
-    autoconf automake guile libtool pkgconfig
+    guile libtool pkgconfig
     SDL2 SDL2_image SDL2_ttf SDL2_mixer
   ];
-
-  preConfigurePhases = [ "bootstrapPhase" ];
-
-  bootstrapPhase = "./bootstrap";
 
   configureFlags = [
     "--with-libsdl2-prefix=${SDL2}"
