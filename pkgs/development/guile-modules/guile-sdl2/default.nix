1c818af667cc67f01acea09cb1b1a7bc1b8a7691
xd1le <elisp.vim@gmail.com>
2017-09-28 14:41:12 +1000

    guile-sdl2: use the proper homepage URL

--- pkgs/development/guile-modules/guile-sdl2/default.nix
+++ pkgs/development/guile-modules/guile-sdl2/default.nix
@@ -35,7 +35,7 @@ in stdenv.mkDerivation {
 
   meta = with stdenv.lib; {
     description = "Bindings to SDL2 for GNU Guile";
-    homepage = "https://git.dthompson.us/guile-sdl2.git";
+    homepage = "https://dthompson.us/projects/guile-sdl2.html";
     license = licenses.gpl3;
     maintainers = with maintainers; [ seppeljordan vyp ];
     platforms = platforms.all;
