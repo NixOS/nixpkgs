25b17ae1d78e0c2d48ac4a16a70f548661570add
mimadrid <mimadrid@ucm.es>
2017-09-30 19:43:19 +0200

    htslib: 1.5.0 -> 1.6.0

--- pkgs/development/libraries/science/biology/htslib/default.nix
+++ pkgs/development/libraries/science/biology/htslib/default.nix
@@ -1,21 +1,31 @@
-{ stdenv, fetchurl, zlib, bzip2, lzma, curl }:
+{ stdenv, fetchurl, zlib, bzip2, lzma, curl, perl }:
 
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
   version = "${major}.0";
   pname = "htslib";
-  major = "1.5";
+  major = "1.6";
 
   src = fetchurl {
     url = "https://github.com/samtools/htslib/releases/download/${major}/htslib-${major}.tar.bz2";
-    sha256 = "0bcjmnbwp2bib1z1bkrp95w9v2syzdwdfqww10mkb1hxlmg52ax0";
+    sha256 = "1jsca3hg4rbr6iqq6imkj4lsvgl8g9768bcmny3hlff2w25vx24m";
   };
+
+  propagatedNativeBuildInputs = [ perl ];
 
   buildInputs = [ zlib bzip2 lzma curl ];
 
   configureFlags = "--enable-libcurl"; # optional but strongly recommended
 
   installFlags = "prefix=$(out)";
+
+  enableParallelBuilding = true;
+
+  doCheck = true;
+
+  preCheck = ''
+    find test -name "*.pl" -exec sed -ie 's|/usr/bin/\(env[[:space:]]\)\{0,1\}perl|${perl}/bin/perl|' {} +
+  '';
 
   meta = with stdenv.lib; {
     description = "A C library for reading/writing high-throughput sequencing data";
