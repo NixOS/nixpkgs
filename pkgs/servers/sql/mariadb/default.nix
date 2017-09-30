6aab18749ce38e9633d330a1a2693389b7741a3f
Robin Gloster <mail@glob.in>
2017-09-30 19:32:43 +0200

    mariadb: 10.1.26 -> 10.2.6

--- pkgs/servers/sql/mariadb/default.nix
+++ pkgs/servers/sql/mariadb/default.nix
@@ -15,12 +15,11 @@ mariadb = everything // {
 };
 
 common = rec { # attributes common to both builds
-  version = "10.1.28";
+  version = "10.2.6";
 
   src = fetchurl {
-    url    = "https://downloads.mariadb.org/f/mariadb-${version}/source/mariadb-${version}.tar.gz/from/http%3A//ftp.hosteurope.de/mirror/archive.mariadb.org/?serve";
-    sha256 = "1g9b0c04qhgcgw6xw29bvdjjjacr7kn82crc7apvvi10ykzwhb99";
-    name   = "mariadb-${version}.tar.gz";
+    url    = "https://downloads.mariadb.org/interstitial/mariadb-${version}/source/mariadb-${version}.tar.gz";
+    sha256 = "1rd2b1b6s87ymr5qhlggr4q4ljazv82ih0msgrbz1rfn81pcg1f3";
   };
 
   prePatch = ''
@@ -155,9 +154,6 @@ everything = stdenv.mkDerivation (common // {
   postInstall = common.postInstall + ''
     rm -r "$out"/{mysql-test,sql-bench,data} # Don't need testing data
     rm "$out"/share/man/man1/mysql-test-run.pl.1
-
-    # Don't install mysqlbug to prevent a dependency on gcc.
-    rm $out/bin/mysqlbug
   '';
 });
 
