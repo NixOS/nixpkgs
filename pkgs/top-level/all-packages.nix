72c1855a0a53cc4cf3ff718954e265f55050e808
Eric Litak <elitak@gmail.com>
2017-09-29 18:06:10 -0700

    ipfs-migrator: init
    (cherry picked from commit b76f9d7a4487f6762791e4fa7966b44451de4fe3)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2680,6 +2680,7 @@ with pkgs;
   iperf = iperf3;
 
   ipfs = callPackage ../applications/networking/ipfs { };
+  ipfs-migrator = callPackage ../applications/networking/ipfs-migrator { };
 
   ipmitool = callPackage ../tools/system/ipmitool {
     static = false;
