c06e5a8d64c978e7adf66f6a00e573635b6930e2
Vincent Laporte <Vincent.Laporte@gmail.com>
2017-10-05 05:57:03 +0000

    ocamlPackages.react: 1.2.0 -> 1.2.1

--- pkgs/development/ocaml-modules/ocsigen-server/default.nix
+++ pkgs/development/ocaml-modules/ocsigen-server/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, ocaml, findlib, which, ocaml_react, ocaml_ssl,
+{stdenv, fetchurl, ocaml, findlib, which, react, ocaml_ssl,
 lwt, ocamlnet, ocaml_pcre, cryptokit, tyxml, ipaddr, zlib,
 libev, openssl, ocaml_sqlite3, tree, uutf, makeWrapper, camlp4
 , camlzip, pgocaml
@@ -17,7 +17,7 @@ stdenv.mkDerivation {
     sha256 = "1v44qv2ixd7i1qinyhlzzqiffawsdl7xhhh6ysd7lf93kh46d5sy";
   };
 
-  buildInputs = [ocaml which findlib ocaml_react ocaml_ssl lwt
+  buildInputs = [ocaml which findlib react ocaml_ssl lwt
   ocamlnet ocaml_pcre cryptokit tyxml ipaddr zlib libev openssl
   ocaml_sqlite3 tree uutf makeWrapper camlp4 pgocaml camlzip ];
 
