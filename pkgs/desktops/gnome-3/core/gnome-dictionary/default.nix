7bd84953700e9eca107f90370ab437834438ad8d
Jan Tojnar <jtojnar@gmail.com>
2017-10-07 12:51:16 +0200

    gnome3.gnome-dictionary: Rely on XML_CATALOG_FILES variable

--- pkgs/desktops/gnome-3/core/gnome-dictionary/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-dictionary/default.nix
@@ -11,18 +11,8 @@ stdenv.mkDerivation rec {
   propagatedBuildInputs = [ gnome3.defaultIconTheme ];
 
   nativeBuildInputs = [ meson ninja pkgconfig wrapGAppsHook libxml2 gettext itstool
-                        desktop_file_utils appstream-glib libxslt docbook_xsl ];
+                        desktop_file_utils appstream-glib libxslt docbook_xsl docbook_xml_dtd_43];
   buildInputs = [ gtk glib gnome3.gsettings_desktop_schemas ];
-
-  postPatch = ''
-    substituteInPlace data/meson.build --replace \
-      "http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl" \
-      "${docbook_xsl}/xml/xsl/docbook/manpages/docbook.xsl"
-
-    substituteInPlace data/gnome-dictionary.xml --replace \
-      http://www.oasis-open.org/docbook/xml/4.3/docbookx.dtd \
-      ${docbook_xml_dtd_43}/xml/dtd/docbook/docbookx.dtd
-  '';
 
   checkPhase = "meson test";
 
