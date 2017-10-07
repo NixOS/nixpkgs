547fba51407afed4c3885cd935cf23702e386be8
Piotr Bogdan <ppbogdan@gmail.com>
2017-10-07 20:10:32 +0100

    ruby_2_2: 2.2.7 -> 2.2.8
    For multiple CVE's:
    
    - CVE-2017-0898
    - CVE-2017-10784
    - CVE-2017-14033
    - CVE-2017-14064
    
    See https://www.ruby-lang.org/en/news/2017/09/14/ruby-2-2-8-released/

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6700,7 +6700,7 @@ with pkgs;
   inherit (callPackage ../development/interpreters/ruby {})
     ruby_2_0_0
     ruby_2_1_10
-    ruby_2_2_7
+    ruby_2_2_8
     ruby_2_3_5
     ruby_2_4_2;
 
@@ -6708,7 +6708,7 @@ with pkgs;
   ruby = ruby_2_3;
   ruby_2_0 = ruby_2_0_0;
   ruby_2_1 = ruby_2_1_10;
-  ruby_2_2 = ruby_2_2_7;
+  ruby_2_2 = ruby_2_2_8;
   ruby_2_3 = ruby_2_3_5;
   ruby_2_4 = ruby_2_4_2;
 
