680d344185a9a0f2d584fbc85c48cb060782abda
Piotr Bogdan <ppbogdan@gmail.com>
2017-10-08 19:58:11 +0100

    newsbeuter-dev: remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3513,8 +3513,6 @@ with pkgs;
 
   newsbeuter = callPackage ../applications/networking/feedreaders/newsbeuter { };
 
-  newsbeuter-dev = callPackage ../applications/networking/feedreaders/newsbeuter/dev.nix { };
-
   nextcloud = callPackage ../servers/nextcloud { };
 
   nextcloud-client = libsForQt56.callPackage ../applications/networking/nextcloud-client { };
