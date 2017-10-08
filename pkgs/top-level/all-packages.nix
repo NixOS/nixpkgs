977c8e673c4938fff42b24d80f724e67c6202f4e
gnidorah <gnidorah@users.noreply.github.com>
2017-10-08 09:03:54 +0300

    hotpatch: init at 0.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2593,6 +2593,8 @@ with pkgs;
 
   host = bind.host;
 
+  hotpatch = callPackage ../development/libraries/hotpatch { };
+
   hotspot = libsForQt56.callPackage ../development/tools/analysis/hotspot { };
 
   hping = callPackage ../tools/networking/hping { };
