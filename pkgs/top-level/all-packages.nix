ba4cefe4ae70cfb9121281f508ed5d9fa9dbc662
Rostislav Beneš <r.dee.b.b@gmail.com>
2017-10-08 21:42:22 +0200

    aspell: added new patch data-dirs-from-nix-profiles.patch
    Aspell will search for dictionaries in all nix profiles even when used as library.
    Setting data-dir or dict-dir in ASPELL_CONF will disable this behavior.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7769,7 +7769,9 @@ with pkgs;
 
   aspellDicts = recurseIntoAttrs (callPackages ../development/libraries/aspell/dictionaries.nix {});
 
-  aspellWithDicts = callPackage ../development/libraries/aspell/aspell-with-dicts.nix { };
+  aspellWithDicts = callPackage ../development/libraries/aspell/aspell-with-dicts.nix {
+    aspell = aspell.override { searchNixProfiles = false; };
+  };
 
   attica = callPackage ../development/libraries/attica { };
 
