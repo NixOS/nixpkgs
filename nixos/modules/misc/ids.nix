f6858e55c2e9fd4e7dfb127bf3595ab7cfe79d15
Shea Levy <shea@shealevy.com>
2017-10-09 07:44:32 -0400

    Reserve uid/gids for kanboard

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -296,6 +296,7 @@
       clickhouse = 278;
       rslsync = 279;
       minio = 280;
+      kanboard = 281;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -561,6 +562,7 @@
       clickhouse = 278;
       rslsync = 279;
       minio = 280;
+      kanboard = 281;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
