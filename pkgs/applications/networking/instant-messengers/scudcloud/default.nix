6c8d037548be221f873ec58e3221bb2d76c837f9
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-10-10 21:16:42 +0300

    scudcloud: 0.58 -> 0.63

--- pkgs/applications/networking/instant-messengers/scudcloud/default.nix
+++ pkgs/applications/networking/instant-messengers/scudcloud/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, python3Packages }:
 
-let version = "1.58";
+let version = "1.63";
 in python3Packages.buildPythonPackage {
   name = "scudcloud-${version}";
 
   src = fetchurl {
     url = "https://github.com/raelgc/scudcloud/archive/v${version}.tar.gz";
-    sha256 = "1j84qdc2j3dvl1nhrjqm0blc8ww723p9a6hqprkkp8alw77myq1l";
+    sha256 = "08hd6n6vwv88qgx869lpa3i8zbaq2pjqplljvcbxl3sx25rcplg0";
   };
 
   propagatedBuildInputs = with python3Packages; [ pyqt5 dbus-python ];
