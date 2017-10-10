67c3eab34820379beee14dfa784a8230d6fda006
Eelco Dolstra <edolstra@gmail.com>
2017-10-10 12:41:50 +0200

    linux: 4.9.53 -> 4.9.54

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.9.53";
+  version = "4.9.54";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "095k7kpzic0c2vhwnfm5vcp9j60lyf4qyx2pj9vkp68bpcrmm49j";
+    sha256 = "1i8g44d3mq7c24mn6b5w59d2z3fxjzbfn5blc36lzkpvdvdha435";
   };
 } // (args.argsOverride or {}))
