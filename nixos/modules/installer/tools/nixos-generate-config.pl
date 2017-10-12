18e3a5333264afe65358db34239e406c8ba2ae96
Linus Heckemann <git@sphalerite.org>
2017-10-12 09:19:27 +0100

    nixos-generate-config: use mkDefault for governor
    Without mkDefault, this will conflict with the tlp module or user-set
    cpu frequency governors.

--- nixos/modules/installer/tools/nixos-generate-config.pl
+++ nixos/modules/installer/tools/nixos-generate-config.pl
@@ -103,7 +103,7 @@ if (-e "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors") {
 
     foreach $e (@desired_governors) {
         if (index($governors, $e) != -1) {
-            last if (push @attrs, "powerManagement.cpuFreqGovernor = \"$e\";");
+            last if (push @attrs, "powerManagement.cpuFreqGovernor = lib.mkDefault \"$e\";");
         }
     }
 }
