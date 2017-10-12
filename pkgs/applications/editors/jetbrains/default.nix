10f4373da441aea47a2cfe3e1a7168bd25fbd9e9
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-10-12 15:47:13 +0300

    jetbrains.clion: 2017.2.2 -> 2017.2.3
    (cherry picked from commit cbdc72b6f112267662265c83895c5b42c5fa7da7)

--- pkgs/applications/editors/jetbrains/default.nix
+++ pkgs/applications/editors/jetbrains/default.nix
@@ -219,12 +219,12 @@ in
 
   clion = buildClion rec {
     name = "clion-${version}";
-    version = "2017.2.2"; /* updated by script */
+    version = "2017.2.3"; /* updated by script */
     description  = "C/C++ IDE. New. Intelligent. Cross-platform";
     license = stdenv.lib.licenses.unfree;
     src = fetchurl {
       url = "https://download.jetbrains.com/cpp/CLion-${version}.tar.gz";
-      sha256 = "1rnncii7z44186jv9xl3ax179ws9wfwd6yndyf9pvnpcd4jcs6d0"; /* updated by script */
+      sha256 = "02hyndyfcrvfbi4q8vmmj0xh2bggwc2azggm24g3m03iffa7j6fx"; /* updated by script */
     };
     wmClass = "jetbrains-clion";
     update-channel = "CLion_Release"; # channel's id as in http://www.jetbrains.com/updates/updates.xml
