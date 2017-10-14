8c618fce30b958b002c056a73f416b7722b0b6f0
Tim Steinbach <tim@nequissimus.com>
2017-10-14 10:28:27 -0400

    linux: 4.13.6 -> 4.13.7
    CVE-2017-5123

--- pkgs/os-specific/linux/kernel/linux-4.13.nix
+++ pkgs/os-specific/linux/kernel/linux-4.13.nix
@@ -1,11 +1,11 @@
 { stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.13.6";
+  version = "4.13.7";
   extraMeta.branch = "4.13";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0l30vlxmp49mm743cydkvr3wfw4nmh0q71avasnksd8xmv71km27";
+    sha256 = "16vjjl3qw0a8ci6xbnywhb8bpr3ccbs0i6xa54lc094cd5gvx4v3";
   };
 } // (args.argsOverride or {}))
