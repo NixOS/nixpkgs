d433a96fe7123d5ea7df176ef4cb0b5db749cb62
Peter Hoeg <peter@hoeg.com>
2017-10-14 18:13:10 +0800

    docbook: put DTDs in $out/share/xml instead of $out/xml

--- pkgs/data/sgml+xml/schemas/xml-dtd/docbook/builder.sh
+++ pkgs/data/sgml+xml/schemas/xml-dtd/docbook/builder.sh
@@ -1,7 +1,10 @@
 source $stdenv/setup
 
-mkdir -p $out/xml/dtd/docbook
-cd $out/xml/dtd/docbook
+mkdir -p $out/share/xml/dtd/docbook
+cd $out/share/xml/dtd/docbook
+# The symlink is for compatibility and should be removed when derivations refer to
+# share/xml instead of xml
+ln -s $out/share/xml $out/xml
 unpackFile $src
 find . -type f -exec chmod -x {} \;
 eval "$postInstall"
