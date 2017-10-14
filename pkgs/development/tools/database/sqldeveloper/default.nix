1e6662dc6a12c7454a1296af40a88c8dba1b598e
Timo Kaufmann <eisfreak7@gmail.com>
2017-10-14 18:13:25 -0500

    sqldeveloper: Add a detailed requireFile message

--- pkgs/development/tools/database/sqldeveloper/default.nix
+++ pkgs/development/tools/database/sqldeveloper/default.nix
@@ -1,12 +1,29 @@
 { stdenv, makeWrapper, requireFile, unzip, openjdk }:
 
 stdenv.mkDerivation rec {
-  name = "sqldeveloper-17.2.0.188.1159";
+  version = "17.2.0.188.1159";
+  name = "sqldeveloper-${version}";
 
-  src = requireFile {
-    name = "${name}-no-jre.zip";
-    url = "http://www.oracle.com/technetwork/developer-tools/sql-developer/downloads/";
-	/* Actual direct link (would it be allowed to give that link?): http://download.oracle.com/otn/java/sqldeveloper/sqldeveloper-17.2.0.188.1159-no-jre.zip */
+  src = requireFile rec {
+    name = "sqldeveloper-${version}-no-jre.zip";
+    url = "http://www.oracle.com/technetwork/developer-tools/sql-developer/downloads/sqldev-downloads-172-3939055.html";
+    message = ''
+      This Nix expression requires that ${name} already be part of the store. To
+      obtain it you need to
+
+      - navigate to ${url}
+      - accept the license agreement
+      - download the file listed under "Other Platforms"
+      - sign in or create an oracle account if neccessary
+
+      and then add the file to the Nix store using either:
+
+        nix-store --add-fixed sha256 ${name}
+
+      or
+
+        nix-prefetch-url --type sha256 file:///path/to/${name}
+    '';
     sha256 = "03960705fabc7b3fa98d55a31ee6a17f783b8c8b01462613c6b6a699e8cae4d5";
   };
 
