8df1c9ac175d72c0c18ede3d22764ece9dceeeef
Peter Hoeg <peter@hoeg.com>
2017-10-14 14:40:53 +0800

    Revert "firewalld: init at 0.4.4.4"
    This reverts commit 178a96f99be69a173669254295d5a06732e7a906.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12212,8 +12212,6 @@ with pkgs;
 
   firejail = callPackage ../os-specific/linux/firejail {};
 
-  firewalld = callPackage ../os-specific/linux/firewalld {};
-
   fnotifystat = callPackage ../os-specific/linux/fnotifystat { };
 
   forkstat = callPackage ../os-specific/linux/forkstat { };
