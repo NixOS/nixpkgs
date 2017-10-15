98aad83abcb130815da647dbce7e2b98299ce9b7
Stefan Lau <github@stefanlau.com>
2017-10-15 12:28:12 +0200

    selendroid: 0.11.0 -> 0.17.0

--- pkgs/development/tools/selenium/selendroid/default.nix
+++ pkgs/development/tools/selenium/selendroid/default.nix
@@ -4,15 +4,15 @@ with stdenv.lib;
 let
     name = "selendroid-standalone-${version}";
     pluginName = "selendroid-grid-plugin-${version}";
-    version = "0.11.0";
+    version = "0.17.0";
     srcs = {
       jar = fetchurl {
         url = "https://github.com/selendroid/selendroid/releases/download/${version}/${name}-with-dependencies.jar";
-        sha256 = "1p6k974pr2634q1g65wx243cxdqhac63x8w2gsmh6vnni0818clk";
+        sha256 = "10lxdsgp711pv8r6dk2aagnbvnn1b25zfqjvz7plc73zqhx1dxvw";
       };
       gridPlugin = fetchurl {
         url = "https://search.maven.org/remotecontent?filepath=io/selendroid/selendroid-grid-plugin/${version}/${pluginName}.jar";
-        sha256 = "1iazmdv2z0k03fa1xlfipwdf3s9j6404kkpfs5xdyy0513ah02a0";
+        sha256 = "1x6cjmp2hpghbgbf8vss0qaj2n4sfl29wp3bc4k1s3hnnpccvz70";
       };
     };
 in
