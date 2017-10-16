59f51eba29820a1b6294a66c977a49d4a92a55c7
Nikolay Amiantov <ab@fmap.me>
2017-10-16 04:25:18 +0300

    opencv3: use older cudatoolkit

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10014,6 +10014,7 @@ with pkgs;
 
   opencv3 = callPackage ../development/libraries/opencv/3.x.nix {
     enableCuda = config.cudaSupport or false;
+    cudatoolkit = cudatoolkit8;
     inherit (darwin.apple_sdk.frameworks) AVFoundation Cocoa QTKit;
   };
 
