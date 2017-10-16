922bd8182e060e576150b0bc442f116d0a6eca1e
Michael Peyton Jones <me@michaelpj.com>
2017-10-16 15:46:10 -0700

    tzupdate: init at 1.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4635,6 +4635,8 @@ with pkgs;
 
   timetrap = callPackage ../applications/office/timetrap { };
 
+  tzupdate = callPackage ../applications/misc/tzupdate { };
+
   tinc = callPackage ../tools/networking/tinc { };
 
   tie = callPackage ../development/tools/misc/tie { };
