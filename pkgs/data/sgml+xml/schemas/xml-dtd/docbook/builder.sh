c8f65872b847e3ae3c02126d722ac50d80835b19
Peter Hoeg <peter@hoeg.com>
2017-10-16 12:44:17 +0800

    fef

--- pkgs/data/sgml+xml/schemas/xml-dtd/docbook/builder.sh
+++ pkgs/data/sgml+xml/schemas/xml-dtd/docbook/builder.sh
@@ -1,10 +1,8 @@
 source $stdenv/setup
 
-mkdir -p $out/share/xml/dtd/docbook
-cd $out/share/xml/dtd/docbook
-# The symlink is for compatibility and should be removed when derivations refer to
-# share/xml instead of xml
-ln -s $out/share/xml $out/xml
+dir=$out/share/${stdenv.lib.replaceStrings [ "-xml" ] [ "" ] name}/dtd
+mkdir -p $dir
+cd $dir
 unpackFile $src
 find . -type f -exec chmod -x {} \;
 eval "$postInstall"
