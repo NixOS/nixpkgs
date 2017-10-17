0dd2ebc518c36f2dbf163d0d651777743b44b440
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2017-10-17 16:51:51 +0300

    nix-info: Add separate tested variant for Hydra

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19159,6 +19159,7 @@ with pkgs;
   nix-bundle = callPackage ../tools/package-management/nix-bundle { nix = nixUnstable; };
 
   nix-info = callPackage ../tools/nix/info { };
+  nix-info-tested = callPackage ../tools/nix/info { doCheck = true; };
 
   nix-index = callPackage ../tools/package-management/nix-index { };
 
