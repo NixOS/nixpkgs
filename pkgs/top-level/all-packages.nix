85a1c9e8674ac4bdfb43b5e59b097cb91bb022c6
lewo <lewo@abesis.fr>
2017-10-17 10:37:20 +0200

    bgpdump: init at 2017-09-29 (#30461)
    * bgpdump: init at 2017-09-29
    
    * bgpdump: switch to autoreconfHook

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13814,6 +13814,8 @@ with pkgs;
     inherit (gnome2) zenity;
   };
 
+  bgpdump = callPackage ../tools/networking/bgpdump { };
+
   blackbox = callPackage ../applications/version-management/blackbox { };
 
   bleachbit = callPackage ../applications/misc/bleachbit { };
