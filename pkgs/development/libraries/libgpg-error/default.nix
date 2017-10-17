4df330ea6e6a4b5b6882781b1c1b5f87e67d8300
Ben Gamari <ben@smart-cactus.org>
2017-10-17 16:40:51 -0400

    libgpg-error: Enable cross-compilation

--- pkgs/development/libraries/libgpg-error/default.nix
+++ pkgs/development/libraries/libgpg-error/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, gettext }:
+{ stdenv, buildPackages, fetchurl, gettext }:
 
 stdenv.mkDerivation rec {
   name = "libgpg-error-${version}";
@@ -16,6 +16,7 @@ stdenv.mkDerivation rec {
 
   # If architecture-dependent MO files aren't available, they're generated
   # during build, so we need gettext for cross-builds.
+  crossAttrs.nativeBuildInputs = [ gettext buildPackages.stdenv.cc ];
   crossAttrs.buildInputs = [ gettext ];
 
   postConfigure =
@@ -27,7 +28,7 @@ stdenv.mkDerivation rec {
     # Thus, re-run it with Bash.
       "${stdenv.shell} config.status";
 
-  doCheck = true;
+  doCheck = stdenv.hostPlatform == stdenv.buildPlatform;
 
   meta = with stdenv.lib; {
     homepage = https://www.gnupg.org/related_software/libgpg-error/index.html;
@@ -45,4 +46,3 @@ stdenv.mkDerivation rec {
     maintainers = [ maintainers.fuuzetsu maintainers.vrthra ];
   };
 }
-
