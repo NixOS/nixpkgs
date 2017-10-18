67343e2f30f6ca5ae9d93619f9c625a0d09ea3f9
Tim Steinbach <tim@nequissimus.com>
2017-10-18 08:38:39 -0400

    linux: 4.13.7 -> 4.13.8

--- pkgs/os-specific/linux/kernel/linux-4.13.nix
+++ pkgs/os-specific/linux/kernel/linux-4.13.nix
@@ -1,11 +1,11 @@
 { stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.13.7";
+  version = "4.13.8";
   extraMeta.branch = "4.13";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "16vjjl3qw0a8ci6xbnywhb8bpr3ccbs0i6xa54lc094cd5gvx4v3";
+    sha256 = "09zl4gpw9j4xn6p78s6ba6qjjxpsy8whhvn19wnjhr9w4al8rrk4";
   };
 } // (args.argsOverride or {}))
