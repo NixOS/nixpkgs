ebcccaf7544457a2cce2f08a404d6ccb565b7acd
Thomas Tuegel <ttuegel@mailbox.org>
2017-10-19 18:22:03 -0500

    plasma-workspace: enable Wayland session support

--- pkgs/desktops/plasma-5/plasma-workspace/default.nix
+++ pkgs/desktops/plasma-5/plasma-workspace/default.nix
@@ -1,18 +1,22 @@
 {
-  mkDerivation, lib, copyPathsToStore,
+  mkDerivation, lib,
 
   extra-cmake-modules, kdoctools,
 
-  isocodes, libdbusmenu, libSM, libXcursor, libXtst, pam, wayland,
+  coreutils, dbus, gnugrep, gnused, isocodes, libdbusmenu, libSM, libXcursor,
+  libXtst, pam, wayland, xmessage, xprop, xrdb, xsetroot,
 
   baloo, kactivities, kcmutils, kconfig, kcrash, kdbusaddons, kdeclarative,
-  kdelibs4support, kdesu, kglobalaccel, kidletime, kjsembed, knewstuff,
+  kdelibs4support, kdesu, kglobalaccel, kidletime, kinit, kjsembed, knewstuff,
   knotifyconfig, kpackage, krunner, kscreenlocker, ktexteditor, ktextwidgets,
   kwallet, kwayland, kwin, kxmlrpcclient, libkscreen, libksysguard,
   networkmanager-qt, phonon, plasma-framework, prison, solid,
 
-  qtgraphicaleffects, qtquickcontrols, qtquickcontrols2, qtscript, qtx11extras,
+  qtgraphicaleffects, qtquickcontrols, qtquickcontrols2, qtscript, qttools,
+  qtwayland, qtx11extras,
 }:
+
+let inherit (lib) getBin getLib; in
 
 mkDerivation {
   name = "plasma-workspace";
@@ -27,21 +31,47 @@ mkDerivation {
     kwallet kwayland kwin kxmlrpcclient libkscreen libksysguard
     networkmanager-qt phonon plasma-framework prison solid
 
-    qtgraphicaleffects qtquickcontrols qtquickcontrols2 qtscript qtx11extras
+    qtgraphicaleffects qtquickcontrols qtquickcontrols2 qtscript qtwayland qtx11extras
   ];
   outputs = [ "out" "dev" "bin" ];
 
-  patches = copyPathsToStore (lib.readPathsFromFile ./. ./series);
+  cmakeFlags = [
+    "-DNIXPKGS_XMESSAGE=${getBin xmessage}/bin/xmessage"
+    "-DNIXPKGS_MKDIR=${getBin coreutils}/bin/mkdir"
+    "-DNIXPKGS_XRDB=${getBin xrdb}/bin/xrdb"
+    "-DNIXPKGS_QTPATHS=${getBin qttools}/bin/qtpaths"
+    "-DNIXPKGS_XSETROOT=${getBin xsetroot}/bin/xsetroot"
+    "-DNIXPKGS_XPROP=${getBin xprop}/bin/xprop"
+    "-DNIXPKGS_ID=${getBin coreutils}/bin/id"
+    "-DNIXPKGS_DBUS_UPDATE_ACTIVATION_ENVIRONMENT=${getBin dbus}/bin/dbus-update-activation-environment"
+    "-DNIXPKGS_START_KDEINIT_WRAPPER=${getLib kinit}/lib/libexec/kf5/start_kdeinit_wrapper"
+    "-DNIXPKGS_QDBUS=${getBin qttools}/bin/qdbus"
+    "-DNIXPKGS_KWRAPPER5=${getBin kinit}/bin/kwrapper5"
+    "-DNIXPKGS_KREADCONFIG5=${getBin kconfig}/bin/kreadconfig5"
+    "-DNIXPKGS_GREP=${getBin gnugrep}/bin/grep"
+    "-DNIXPKGS_KDEINIT5_SHUTDOWN=${getBin kinit}/bin/kdeinit5_shutdown"
+    "-DNIXPKGS_SED=${getBin gnused}/bin/sed"
+  ];
 
-  postPatch = ''
-    substituteInPlace startkde/kstartupconfig/kstartupconfig.cpp \
-        --replace kdostartupconfig5 ''${!outputBin}/bin/kdostartupconfig5
+  # To regenerate ./plasma-workspace.patch,
+  #
+  # > git clone https://github.com/ttuegel/plasma-workspace
+  # > cd plasma-workspace
+  # > git checkout nixpkgs/$x.$y  # where $x.$y.$z == $version
+  # ... make some commits ...
+  # > git diff v$x.$y.$z
+  #
+  # Add upstream patches to the list below. For new patchs, particularly if not
+  # submitted upstream, please make a pull request and add it to
+  # ./plasma-workspace.patch.
+  patches = [ ./plasma-workspace.patch ];
+
+  preConfigure = ''
+    NIX_CFLAGS_COMPILE+=" -DNIXPKGS_KDOSTARTUPCONFIG5=\"''${!outputBin}/bin/kdostartupconfig5\""
+    cmakeFlags+=" -DNIXPKGS_STARTPLASMA=''${!outputBin}/lib/libexec/startplasma"
   '';
 
   postInstall = ''
-    rm "''${!outputBin}/bin/startkde"
-    rm "''${!outputBin}/bin/startplasmacompositor"
-    rm "''${!outputLib}/lib/libexec/startplasma"
-    rm -r "''${!outputBin}/share/wayland-sessions"
+    moveToOutput lib/libexec/startplasma ''${!outputBin}
   '';
 }
