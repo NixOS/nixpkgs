af91c65777c126d7bcf66c916910605879b6909e
Maxime Dénès <mail@maximedenes.fr>
2017-10-22 16:15:12 +0200

    coqPackages.mathcomp: 1.6.1 -> 1.6.2

--- pkgs/development/coq-modules/mathcomp/default.nix
+++ pkgs/development/coq-modules/mathcomp/default.nix
@@ -2,9 +2,9 @@
 
 let param =
   {
-    version = "1.6.1";
-    url = https://github.com/math-comp/math-comp/archive/mathcomp-1.6.1.tar.gz;
-    sha256 = "1j9ylggjzrxz1i2hdl2yhsvmvy5z6l4rprwx7604401080p5sgjw";
+    version = "1.6.2";
+    url = https://github.com/math-comp/math-comp/archive/mathcomp-1.6.2.tar.gz;
+    sha256 = "0lg5ncr7p4y8qqq6pfw6brqc6a9xzlfa0drprwfdn0rnyaq5nca6";
   }; in
 
 callPackage ./generic.nix {
