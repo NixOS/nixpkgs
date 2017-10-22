4716a077cb0d31b5d6d68e54b31c8ec0824787a3
Erick Gonzalez <erick@codemonkeylabs.de>
2017-10-22 10:25:37 +0200

    strongSwan: Enable EAP Radius plugin for remote authentication of dial in ipsec clients

--- pkgs/tools/networking/strongswan/default.nix
+++ pkgs/tools/networking/strongswan/default.nix
@@ -48,7 +48,7 @@ stdenv.mkDerivation rec {
       "--enable-eap-sim" "--enable-eap-sim-file" "--enable-eap-simaka-pseudonym"
       "--enable-eap-simaka-reauth" "--enable-eap-identity" "--enable-eap-md5"
       "--enable-eap-gtc" "--enable-eap-aka" "--enable-eap-aka-3gpp2"
-      "--enable-eap-mschapv2" "--enable-xauth-eap" "--enable-ext-auth"
+      "--enable-eap-mschapv2" "--enable-eap-radius" "--enable-xauth-eap" "--enable-ext-auth"
       "--enable-forecast" "--enable-connmark" "--enable-acert"
       "--enable-pkcs11" "--enable-eap-sim-pcsc" "--enable-dnscert" "--enable-unbound"
       "--enable-af-alg" "--enable-xauth-pam" "--enable-chapoly" ]
