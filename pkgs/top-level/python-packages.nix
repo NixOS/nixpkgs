817b7bb3491dc0931bd508a57fcca57c67121ac0
Robin Gloster <mail@glob.in>
2017-10-23 13:36:22 +0200

    python.pkgs.nose_warnings_filters: init at 0.1.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13124,6 +13124,8 @@ in {
     buildInputs = with self; [ nose ];
   };
 
+  nose_warnings_filters = callPackage ../development/python-modules/nose_warnings_filters { };
+
   notebook = callPackage ../development/python-modules/notebook { };
 
   notify = pkgs.stdenv.mkDerivation (rec {
