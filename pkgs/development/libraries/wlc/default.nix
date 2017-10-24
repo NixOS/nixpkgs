c7285c33424338f7ea080ad3c4991eecfd46fd58
gnidorah <gnidorah@users.noreply.github.com>
2017-10-24 20:48:43 +0300

    wlc: cleanup

--- pkgs/development/libraries/wlc/default.nix
+++ pkgs/development/libraries/wlc/default.nix
@@ -1,11 +1,17 @@
-{ stdenv, fetchFromGitHub, cmake, pkgconfig
-, wayland, pixman, libxkbcommon, libinput, libxcb, xcbutilwm, xcbutilimage, mesa
-, libdrm, udev, libX11, libXdamage, systemd, dbus_libs, wayland-protocols
-, libpthreadstubs, libXau, libXdmcp, libXext, libXxf86vm
+{ stdenv, fetchFromGitHub, fetchpatch, cmake, pkgconfig
+, wayland, pixman, libxkbcommon, libinput, xcbutilwm, xcbutilimage, mesa_noglu
+, libX11, dbus_libs, wayland-protocols
+, libpthreadstubs, libXdmcp, libXext
 , withOptionalPackages ? true, zlib, valgrind, doxygen
 }:
 
-stdenv.mkDerivation rec {
+let
+  # for 0.0.10
+  xwaylandPatch = fetchpatch {
+    url = "https://github.com/Cloudef/wlc/commit/a130f6006560fb8ac02fb59a90ced1659563f9ca.diff";
+    sha256 = "0kzcbqklcyg8bganm65di8cif6dpc8bkrsvkjia09kr92lymxm2c";
+  };
+in stdenv.mkDerivation rec {
   name = "wlc-${version}";
   version = "0.0.10";
 
@@ -17,16 +23,21 @@ stdenv.mkDerivation rec {
     sha256 = "09kvwhrpgkxlagn9lgqxc80jbg56djn29a6z0n6h0dsm90ysyb2k";
   };
 
+  patches = [
+    xwaylandPatch
+  ];
+
   nativeBuildInputs = [ cmake pkgconfig ];
 
   buildInputs = [
-    wayland pixman libxkbcommon libinput libxcb xcbutilwm xcbutilimage mesa
-    libdrm udev libX11 libXdamage systemd dbus_libs wayland-protocols
-    libpthreadstubs libXau libXdmcp libXext libXxf86vm ]
+    wayland pixman libxkbcommon libinput xcbutilwm xcbutilimage mesa_noglu
+    libX11 dbus_libs wayland-protocols
+    libpthreadstubs libXdmcp libXext ]
     ++ stdenv.lib.optionals withOptionalPackages [ zlib valgrind doxygen ];
 
   doCheck = true;
   checkTarget = "test";
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     description = "A library for making a simple Wayland compositor";
