5ddea062e269c124346c8d2c6cee1eac1aa36bfd
Cray Elliott <MP2E@archlinux.us>
2017-10-23 23:23:23 -0700

    ffmpeg: 3.3.4 -> 3.4
    ffmpeg_3 bumped to 3.4, and 3.3.4 removed from the tree

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8098,14 +8098,14 @@ with pkgs;
   ffmpeg_2_8 = callPackage ../development/libraries/ffmpeg/2.8.nix {
     inherit (darwin.apple_sdk.frameworks) Cocoa;
   };
-  ffmpeg_3_3 = callPackage ../development/libraries/ffmpeg/3.3.nix {
+  ffmpeg_3_4 = callPackage ../development/libraries/ffmpeg/3.4.nix {
     inherit (darwin.apple_sdk.frameworks) Cocoa CoreMedia;
   };
   # Aliases
   ffmpeg_0 = ffmpeg_0_10;
   ffmpeg_1 = ffmpeg_1_2;
   ffmpeg_2 = ffmpeg_2_8;
-  ffmpeg_3 = ffmpeg_3_3;
+  ffmpeg_3 = ffmpeg_3_4;
   ffmpeg = ffmpeg_3;
 
   ffmpeg-full = callPackage ../development/libraries/ffmpeg-full {
