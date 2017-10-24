a4bb25a480fcc0c187a767eebb89a49b6002f306
Franz Pletz <fpletz@fnordicwalking.de>
2017-10-24 13:03:20 +0200

    db{48,53,60,62}: add patch to fix CVE-2017-10140
    (cherry picked from commit 5072f4792e890c809a43763c8e0b283a22ee0c14)

--- pkgs/development/libraries/db/db-5.3.nix
+++ pkgs/development/libraries/db/db-5.3.nix
@@ -3,5 +3,5 @@
 import ./generic.nix (args // rec {
   version = "5.3.28";
   sha256 = "0a1n5hbl7027fbz5lm0vp0zzfp1hmxnz14wx3zl9563h83br5ag0";
-  extraPatches = [ ./clang-5.3.patch ];
+  extraPatches = [ ./clang-5.3.patch ./CVE-2017-10140-cwd-db_config.patch ];
 })
