416a975f74b965425831e829047f45f7e1502a11
Frederik Rietdijk <fridh@fridh.nl>
2017-10-29 13:44:43 +0100

    python.pkgs.tabulate: move expression

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -20714,29 +20714,7 @@ in {
     inherit (pkgs) pkgconfig systemd;
   };
 
-  tabulate = buildPythonPackage rec {
-    version = "0.7.7";
-    pname = "tabulate";
-    name = "${pname}-${version}";
-
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "83a0b8e17c09f012090a50e1e97ae897300a72b35e0c86c0b53d3bd2ae86d8c6";
-    };
-
-    buildInputs = with self; [ nose ];
-
-    # Tests: cannot import common (relative import).
-    doCheck = false;
-
-    meta = {
-      description = "Pretty-print tabular data";
-      homepage = https://bitbucket.org/astanin/python-tabulate;
-      license = licenses.mit;
-      maintainers = with maintainers; [ fridh ];
-    };
-
-  };
+  tabulate = callPackage ../development/python-modules/tabulate { };
 
   targetcli_fb = buildPythonPackage rec {
     version = "2.1.fb33";
