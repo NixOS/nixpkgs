d1a6e26d493403a19d757e9a7d91ca0bcaef441d
Ben Gamari <ben@smart-cactus.org>
2017-10-29 16:46:38 -0400

    libnfsidmap: Fix setting of configureFlags
    The previous construction broke cross-compilation.

--- pkgs/development/libraries/libnfsidmap/default.nix
+++ pkgs/development/libraries/libnfsidmap/default.nix
@@ -2,16 +2,18 @@
 
 stdenv.mkDerivation rec {
   name = "libnfsidmap-0.25";
-  
+
   src = fetchurl {
     url = "http://www.citi.umich.edu/projects/nfsv4/linux/libnfsidmap/${name}.tar.gz";
     sha256 = "1kzgwxzh83qi97rblcm9qj80cdvnv8kml2plz0q103j0hifj8vb5";
   };
 
-  preConfigure =
-    ''
-      configureFlags="--with-pluginpath=$out/lib/libnfsidmap"
-    '';
+  configureFlags = [
+    "--with-pluginpath=$out/lib/libnfsidmap"
+  ] ++ stdenv.lib.optionals (stdenv.hostPlatform != stdenv.buildPlatform) [
+    "ac_cv_func_malloc_0_nonnull=yes"
+    "ac_cv_func_realloc_0_nonnull=yes"
+  ];
 
   meta = {
     homepage = http://www.citi.umich.edu/projects/nfsv4/linux/;
