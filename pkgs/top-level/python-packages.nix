7357b369d51dee7e9eadf60d0da88ffb6f1ed6a4
Bjørn Forsman <bjorn.forsman@gmail.com>
2017-10-29 11:45:46 +0100

    pythonPackages.pyodbc: move to python-modules/
    Easier maintenance when expressions are in separate files.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17039,28 +17039,7 @@ in {
     };
   };
 
-
-  pyodbc = buildPythonPackage rec {
-    pname = "pyodbc";
-    version = "4.0.19";
-    name = "${pname}-${version}";
-    disabled = isPyPy;  # use pypypdbc instead
-
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "05mkaxbi9n02bpr3l0qnyfb3458f35hk71bq8jmadikp3h8al7dg";
-    };
-
-    buildInputs = with self; [ pkgs.libiodbc ];
-
-    meta = {
-      description = "Python ODBC module to connect to almost any database";
-      homepage = "https://github.com/mkleehammer/pyodbc";
-      license = licenses.mit;
-      platforms = platforms.linux;
-      maintainers = with maintainers; [ bjornfor ];
-    };
-  };
+  pyodbc = callPackage ../development/python-modules/pyodbc { };
 
   pyocr = callPackage ../development/python-modules/pyocr { };
 
