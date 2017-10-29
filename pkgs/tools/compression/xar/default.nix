ed4fad293387a0cc6d2e9646e0b703b04ebe67f0
Dan Peebles <pumpkin@me.com>
2017-10-29 16:50:06 +0100

    xar: fix to work on openssl 1.1.x

--- pkgs/tools/compression/xar/default.nix
+++ pkgs/tools/compression/xar/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, libxml2, openssl, zlib, bzip2 }:
+{ stdenv, fetchurl, libxml2, openssl, zlib, bzip2, autoconf }:
 
 stdenv.mkDerivation rec {
   version = "1.6.1";
@@ -9,7 +9,15 @@ stdenv.mkDerivation rec {
     sha256 = "0ghmsbs6xwg1092v7pjcibmk5wkyifwxw6ygp08gfz25d2chhipf";
   };
 
-  buildInputs = [ libxml2 openssl zlib bzip2 ];
+  buildInputs = [ libxml2 openssl zlib bzip2 autoconf ];
+
+  prePatch = ''
+    substituteInPlace configure.ac \
+      --replace 'OpenSSL_add_all_ciphers' 'OPENSSL_init_crypto' \
+      --replace 'openssl/evp.h' 'openssl/crypto.h'
+  '';
+
+  preConfigure = "./autogen.sh";
 
   meta = {
     homepage    = https://mackyle.github.io/xar/;
