09bdae2dd77257c659ebf0595562ea939d73b79e
Jörg Thalheim <joerg@thalheim.io>
2017-11-01 05:14:33 -0500

    pythonPackages.rply: enable tests

--- pkgs/development/python-modules/rply/default.nix
+++ pkgs/development/python-modules/rply/default.nix
@@ -1,16 +1,23 @@
-{ stdenv, fetchurl, buildPythonPackage, appdirs }:
+{ stdenv, pytest, fetchFromGitHub, buildPythonPackage, appdirs }:
 
 buildPythonPackage rec {
   pname = "rply";
   name = "${pname}-${version}";
   version = "0.7.5";
 
-  src = fetchurl {
-    url = "mirror://pypi/r/rply/${name}.tar.gz";
-    sha256 = "0lv428895zxsz43968qx0q9bimwqnfykndz4dpjbq515w2gvzhjh";
+  src = fetchFromGitHub {
+    owner = "alex";
+    repo = "rply";
+    rev = "v${version}";
+    sha256 = "0v05gdy5dval30wvz96lywvz2jyf000dp0pnrd1lwdx3cyywq659";
   };
 
   buildInputs = [ appdirs ];
+
+  checkInputs = [ pytest ];
+  checkPhase = ''
+    HOME=$(mktemp -d) py.test tests
+  '';
 
   meta = with stdenv.lib; {
     description = "A python Lex/Yacc that works with RPython";
