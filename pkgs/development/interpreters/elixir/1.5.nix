83a37126f2b62a78c516591dc4820e7efb7fb24c
Jonathan C. Otsuka <djgoku@gmail.com>
2017-11-02 00:07:39 -0500

    Bump elixir to latest version 1.5.2.

--- pkgs/development/interpreters/elixir/1.5.nix
+++ pkgs/development/interpreters/elixir/1.5.nix
@@ -1,7 +1,9 @@
 { mkDerivation }:
 
+# to generate sha256 use nix-prefetch-git git@github.com:elixir-lang/elixir.git --rev 05418eaa4bf4fa8473900741252d93d76ed3307b
+
 mkDerivation rec {
-  version = "1.5.1";
-  sha256 = "0q0zr3v9cyb7p9aab8v038hnjm84nf9b60kikffp6w9rfqqqf767";
+  version = "1.5.2";
+  sha256 = "0ng7z2gz1c8lkn07fric18b3awcw886s9xb864kmnn2iah5gc65j";
   minimumOTPVersion = "18";
 }
