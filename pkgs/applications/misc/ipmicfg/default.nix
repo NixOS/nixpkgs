da067f40f93693083416d59028e4fa344941e394
Orivej Desh <orivej@gmx.fr>
2017-11-02 23:06:05 +0000

    ipmicfg: fix style

--- pkgs/applications/misc/ipmicfg/default.nix
+++ pkgs/applications/misc/ipmicfg/default.nix
@@ -1,31 +1,33 @@
-{ stdenv, lib, fetchurl, patchelf, unzip }:
-
-assert stdenv.isLinux;
+{ stdenv, fetchzip }:
 
 stdenv.mkDerivation rec {
   name = "ipmicfg-${version}";
   version = "1.27.0";
   buildVersion = "170620";
 
-  src = fetchurl {
+  src = fetchzip {
     url = "ftp://ftp.supermicro.com/utility/IPMICFG/IPMICFG_${version}_build.${buildVersion}.zip";
-    sha256 = "0mlhrxnkwazq5456csfds6w2z5pv2ksnqnxlv4prp07bwi2bccid";
+    sha256 = "0jr2vih4hzymb62mbqyykwcrjhbhazf6wr1g0cq8ji586i3z3vw5";
   };
 
-  buildInputs = [ unzip ];
   installPhase = ''
-    mkdir -p "$out/opt/ipmicfg"
+    mkdir -p "$out/bin" "$out/opt/ipmicfg"
     cp Linux/64bit/* "$out/opt/ipmicfg"
 
-    patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) "$out/opt/ipmicfg/IPMICFG-Linux.x86_64"
-    patchelf --set-rpath "${lib.makeLibraryPath [ stdenv.cc.cc ]}:${stdenv.cc.cc.lib}/lib64" "$out/opt/ipmicfg/IPMICFG-Linux.x86_64"
+    patchelf "$out/opt/ipmicfg/IPMICFG-Linux.x86_64" \
+       --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+       --set-rpath "${stdenv.lib.makeLibraryPath [ stdenv.cc.cc ]}"
 
-    mkdir "$out/bin"
     ln -s "$out/opt/ipmicfg/IPMICFG-Linux.x86_64" "$out/bin/ipmicfg"
   '';
 
+   dontPatchShebangs = true; # There are no scripts and it complains about null bytes.
+
    meta = with stdenv.lib; {
+     description = "Supermicro IPMI configuration tool";
+     homepage = "http://www.supermicro.com/products/nfo/ipmi.cfm";
      license = licenses.unfree;
+     platforms = [ "x86_64-linux" ];
      maintainers = with maintainers; [ sorki ];
    };
 }
