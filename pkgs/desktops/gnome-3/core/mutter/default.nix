3c98114d6ff86b9934ca9d63a010a8b4447c4a63
Jan Tojnar <jtojnar@gmail.com>
2017-11-03 23:30:56 +0100

    gnome3.mutter: cleanup

--- pkgs/desktops/gnome-3/core/mutter/default.nix
+++ pkgs/desktops/gnome-3/core/mutter/default.nix
@@ -9,20 +9,30 @@ stdenv.mkDerivation rec {
   # fatal error: gio/gunixfdlist.h: No such file or directory
   NIX_CFLAGS_COMPILE = "-I${gnome3.glib.dev}/include/gio-unix-2.0 -Wno-error=format -Wno-error=sign-compare";
 
-  configureFlags = "--with-x --disable-static --enable-shape --enable-sm --enable-startup-notification --enable-xsync --enable-verbose-mode --with-libcanberra --with-xwayland-path=${xwayland}/bin/Xwayland";
+  configureFlags = [
+    "--with-x"
+    "--disable-static"
+    "--enable-shape"
+    "--enable-sm"
+    "--enable-startup-notification"
+    "--enable-xsync"
+    "--enable-verbose-mode"
+    "--with-libcanberra"
+    "--with-xwayland-path=${xwayland}/bin/Xwayland"
+  ];
 
   propagatedBuildInputs = [
     # required for pkgconfig to detect mutter-clutter
     libXtst
   ];
 
-  nativeBuildInputs = [ autoreconfHook ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig intltool libtool makeWrapper ];
 
   buildInputs = with gnome3;
-    [ pkgconfig intltool glib gobjectIntrospection gtk gsettings_desktop_schemas upower
+    [ glib gobjectIntrospection gtk gsettings_desktop_schemas upower
       gnome_desktop cairo pango cogl clutter zenity libstartup_notification libcanberra_gtk2
       gnome3.geocode_glib libudev libinput libgudev libwacom
-      libcanberra_gtk3 zenity libtool makeWrapper xkeyboard_config libxkbfile
+      libcanberra_gtk3 zenity xkeyboard_config libxkbfile
       libxkbcommon ];
 
   patches = [
