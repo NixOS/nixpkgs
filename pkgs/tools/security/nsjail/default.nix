9754503c6be6bcaa489658b73dbc4cbad99404d6
Renaud <c0bw3b@users.noreply.github.com>
2017-11-04 20:02:23 +0100

    nsjail: 2.1 -> 2.2
    plus fixed meta.license which should be Apache License 2.0

--- pkgs/tools/security/nsjail/default.nix
+++ pkgs/tools/security/nsjail/default.nix
@@ -3,29 +3,31 @@
 
 stdenv.mkDerivation rec {
   name = "nsjail-${version}";
-  version = "2.1";
+  version = "2.2";
 
   src = fetchFromGitHub {
     owner           = "google";
     repo            = "nsjail";
     rev             = version;
     fetchSubmodules = true;
-    sha256          = "1wkhy86d0vgzngdvv593yhcghjh63chb8s67v891zll6bwgwg5h2";
+    sha256          = "11323j5wd02nm8ibvzbzq7dla70bmcldc71lv5bpk4x7h64ai14v";
   };
 
   nativeBuildInputs = [ autoconf libtool pkgconfig ];
   buildInputs = [ bison flex libnl protobuf protobufc ];
+  enableParallelBuilding = true;
 
   installPhase = ''
-    mkdir -p $out/bin
-    cp nsjail $out/bin
+    mkdir -p $out/bin $out/share/man/man1
+    install nsjail $out/bin/
+    install nsjail.1 $out/share/man/man1/
   '';
 
   meta = with stdenv.lib; {
     description = "A light-weight process isolation tool, making use of Linux namespaces and seccomp-bpf syscall filters";
     homepage    = http://nsjail.com/;
-    license     = licenses.apsl20;
-    maintainers = [ maintainers.bosu ];
+    license     = licenses.asl20;
+    maintainers = with maintainers; [ bosu c0bw3b ];
     platforms   = platforms.linux;
   };
 }
