5fde24c20bf212e3faa80802e2831c19a62abdbc
Frederik Rietdijk <fridh@fridh.nl>
2017-11-05 13:44:16 +0100

    python.pkgs.urwid: fix build

--- pkgs/development/python-modules/urwid/default.nix
+++ pkgs/development/python-modules/urwid/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildPythonPackage, fetchPypi }:
+{ stdenv, buildPythonPackage, fetchPypi, fetchurl }:
 
 buildPythonPackage (rec {
   pname = "urwid";
@@ -9,6 +9,13 @@ buildPythonPackage (rec {
     inherit pname version;
     sha256 = "18cnd1wdjcas08x5qwa5ayw6jsfcn33w4d9f7q3s29fy6qzc1kng";
   };
+
+  patches = [
+    (fetchurl {
+      url = https://github.com/urwid/urwid/commit/4b0ed8b6030450e6d99909a7c683e9642e546387.patch;
+      sha256 = "0ygghd6wzjqij5szg61l1dsk8b0yv8bwyj3bgxxj1lj4m17zsy5q";
+    })
+  ];
 
   meta = with stdenv.lib; {
     description = "A full-featured console (xterm et al.) user interface library";
