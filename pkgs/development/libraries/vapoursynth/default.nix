99cf2b6eec330a00d9cdef9070974a5a50a701e5
rnhmjoj <rnhmjoj@inventati.org>
2017-11-05 19:08:36 +0100

    vapoursynth: R38 -> R39

--- pkgs/development/libraries/vapoursynth/default.nix
+++ pkgs/development/libraries/vapoursynth/default.nix
@@ -1,7 +1,8 @@
-{ stdenv, fetchFromGitHub, pkgconfig, autoreconfHook,
-  zimg, libass, yasm, python3, libiconv, ApplicationServices,
-  ocrSupport ?  false, tesseract,
-  imwriSupport? true,  imagemagick7
+{ stdenv, fetchFromGitHub, pkgconfig, autoreconfHook
+, zimg, libass, python3, libiconv
+, ApplicationServices, nasm
+, ocrSupport ?  false, tesseract
+, imwriSupport? true,  imagemagick7
 }:
 
 assert ocrSupport   -> tesseract != null;
@@ -11,18 +12,18 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "vapoursynth-${version}";
-  version = "R38";
+  version = "R39";
 
   src = fetchFromGitHub {
     owner  = "vapoursynth";
     repo   = "vapoursynth";
     rev    = version;
-    sha256 = "0nabl6949s7awy7rnr4ck52v50xr0hwr280fyzsqixgp8w369jn0";
+    sha256 = "0cw7w8xiwhxhwykydy13m44wm9vn9hrsi30z6017ngga9d84fhqy";
   };
 
-  nativeBuildInputs = [ pkgconfig autoreconfHook ];
+  nativeBuildInputs = [ pkgconfig autoreconfHook nasm ];
   buildInputs = [
-    zimg libass tesseract yasm
+    zimg libass
     (python3.withPackages (ps: with ps; [ sphinx cython ]))
   ] ++ optionals stdenv.isDarwin [ libiconv ApplicationServices ]
     ++ optional ocrSupport   tesseract
