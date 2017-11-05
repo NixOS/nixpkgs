f9031957eda8d880edf17cc9eb8469e47f382bb0
rnhmjoj <rnhmjoj@inventati.org>
2017-11-05 17:32:06 +0100

    pdns-recursor: 4.0.4 -> 4.0.6

--- pkgs/servers/dns/pdns-recursor/default.nix
+++ pkgs/servers/dns/pdns-recursor/default.nix
@@ -11,11 +11,11 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "pdns-recursor-${version}";
-  version = "4.0.4";
+  version = "4.0.6";
 
   src = fetchurl {
     url = "https://downloads.powerdns.com/releases/pdns-recursor-${version}.tar.bz2";
-    sha256 = "0k8y9zxj2lz4rq782vgzr28yd43q0hwlnvszwq0k9l6c967pff13";
+    sha256 = "03fnjiacvhdlkr3a2206mham0p6p24gkawashs5v12r68k32l67j";
   };
 
   nativeBuildInputs = [ pkgconfig ];
