30f6d630f1908d05da1d22aff80c489f3c09652d
Tim Steinbach <tim@nequissimus.com>
2017-11-05 16:47:03 -0500

    linux: 4.14.0-rc7 -> 4.14.0-rc8

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -1,13 +1,13 @@
 { stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.14-rc7";
-  modDirVersion = "4.14.0-rc7";
+  version = "4.14-rc8";
+  modDirVersion = "4.14.0-rc8";
   extraMeta.branch = "4.14";
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "1w7b1sc5dsxcqywsdbwgs92i8jpj7hsnss67yzb58z3bz3hb73m3";
+    sha256 = "0ir2ggflm1xjnn1kvv2c99m5zni5kg0ygzlpm588wnhkzd93nqjh";
   };
 
   # Should the testing kernels ever be built on Hydra?
