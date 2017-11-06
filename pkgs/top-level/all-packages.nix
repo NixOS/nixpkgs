a95389e1a83749f3fe350132aefe0343355bf6b1
Ryan Bernstein <ryanbernstein1@gmail.com>
2017-11-06 18:16:39 -0500

    vcftools: init at 0.1.15

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4835,6 +4835,8 @@ with pkgs;
 
   vboot_reference = callPackage ../tools/system/vboot_reference { };
 
+  vcftools = callPackage ../applications/science/biology/vcftools { };
+
   vcsh = callPackage ../applications/version-management/vcsh {
     inherit (perlPackages) ShellCommand TestMost;
   };
