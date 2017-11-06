2b8d48a9fc82b3e70a4402828cac6603dead937c
Orivej Desh <orivej@gmx.fr>
2017-11-06 23:20:14 +0000

    rednotebook: move to applications/editors

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -22735,28 +22735,7 @@ EOF
     };
   };
 
-
-  redNotebook = buildPythonPackage rec {
-    name = "rednotebook-1.8.1";
-
-    src = pkgs.fetchurl {
-      url = "mirror://sourceforge/rednotebook/${name}.tar.gz";
-      sha256 = "00b7s4xpqpxsbzjvjx9qsx5d84m9pvn383c5di1nsfh35pig0rzn";
-    };
-
-    # no tests available
-    doCheck = false;
-
-    propagatedBuildInputs = with self; [ pygtk pywebkitgtk pyyaml chardet ];
-
-    meta = {
-      homepage = http://rednotebook.sourceforge.net/index.html;
-      description = "A modern journal that includes a calendar navigation, customizable templates, export functionality and word clouds";
-      license = licenses.gpl2;
-      maintainers = with maintainers; [ tstrobel ];
-    };
-  };
-
+  redNotebook = callPackage ../applications/editors/rednotebook { };
 
   uncertainties = callPackage ../development/python-modules/uncertainties { };
 
