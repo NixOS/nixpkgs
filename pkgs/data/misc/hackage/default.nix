a38a3e21291ca8e751959a3f797ed2729bed6029
Will Fancher <elvishjerricco@gmail.com>
2017-11-06 16:26:05 -0500

    Extract files one at a time from all-cabal-hashes

--- pkgs/data/misc/hackage/default.nix
+++ pkgs/data/misc/hackage/default.nix
@@ -1,11 +1,6 @@
-{ fetchFromGitHub }:
+{ fetchurl }:
 
-# Use builtins.fetchTarball "https://github.com/commercialhaskell/all-cabal-hashes/archive/hackage.tar.gz"
-# instead if you want the latest Hackage automatically at the price of frequent re-downloads.
-
-fetchFromGitHub {
-  owner = "commercialhaskell";
-  repo = "all-cabal-hashes";
-  rev = "5e87c40f2cd96bd5dd953758e82f302107c7895e";
-  sha256 = "12rw5fld64s0a2zjsdijfs0dv6vc6z7gcf24h4m2dmymzms4namg";
+fetchurl {
+  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/5e87c40f2cd96bd5dd953758e82f302107c7895e.tar.gz";
+  sha256 = "0hjkddda9mdm21nb9bkhr9n5r9jllisif1qmzha91a9cps5w1mx5";
 }
