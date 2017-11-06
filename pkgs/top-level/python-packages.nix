e4a535afb9e384f836213b8730effb87d3ccfc0a
Orivej Desh <orivej@gmx.fr>
2017-11-06 20:56:48 +0000

    Revert "pythonpackages.redNotebook: Remove outdated package"
    This reverts commit 33a56ef0a68cc06298556d866b24f1f2a8286639.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -22735,6 +22735,29 @@ EOF
     };
   };
 
+
+  redNotebook = buildPythonPackage rec {
+    name = "rednotebook-1.8.1";
+
+    src = pkgs.fetchurl {
+      url = "mirror://sourceforge/rednotebook/${name}.tar.gz";
+      sha256 = "00b7s4xpqpxsbzjvjx9qsx5d84m9pvn383c5di1nsfh35pig0rzn";
+    };
+
+    # no tests available
+    doCheck = false;
+
+    propagatedBuildInputs = with self; [ pygtk pywebkitgtk pyyaml chardet ];
+
+    meta = {
+      homepage = http://rednotebook.sourceforge.net/index.html;
+      description = "A modern journal that includes a calendar navigation, customizable templates, export functionality and word clouds";
+      license = licenses.gpl2;
+      maintainers = with maintainers; [ tstrobel ];
+    };
+  };
+
+
   uncertainties = callPackage ../development/python-modules/uncertainties { };
 
   funcy = buildPythonPackage rec {
