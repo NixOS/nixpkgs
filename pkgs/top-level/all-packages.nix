a38765215963b1fb4c6beb394fffc5c87282a478
Austin Seipp <aseipp@pobox.com>
2017-11-06 23:03:11 -0600

    nixpkgs: remove busted 'tmin' package
    Obsoleted by afl-tmin
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4687,8 +4687,6 @@ with pkgs;
 
   tmuxinator = callPackage ../tools/misc/tmuxinator { };
 
-  tmin = callPackage ../tools/security/tmin { };
-
   tmsu = callPackage ../tools/filesystems/tmsu { };
 
   toilet = callPackage ../tools/misc/toilet { };
