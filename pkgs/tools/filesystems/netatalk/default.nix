997d66c489ca7e252a9e8db2bbbec0fe24cddd2c
kyren <kerriganw@gmail.com>
2017-11-07 17:57:01 -0500

    netatalk: 3.1.7 -> 3.1.11
    Netatalk 3.1.11 is the latest stable release.  There is also a nasty bug
    with netatalk < 3.1.11 (https://sourceforge.net/p/netatalk/bugs/636/)
    that prevents using netatalk shares for time machine backups.

--- pkgs/tools/filesystems/netatalk/default.nix
+++ pkgs/tools/filesystems/netatalk/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, pkgconfig, db, libgcrypt, avahi, libiconv, pam, openssl, acl, ed, glibc }:
 
 stdenv.mkDerivation rec{
-  name = "netatalk-3.1.7";
+  name = "netatalk-3.1.11";
 
   src = fetchurl {
     url = "mirror://sourceforge/netatalk/netatalk/${name}.tar.bz2";
-    sha256 = "0wf09fyqzza024qr1s26z5x7rsvh9zb4pv598gw7gm77wjcr6174";
+    sha256 = "3434472ba96d3bbe3b024274438daad83b784ced720f7662a4c1d0a1078799a6";
   };
 
   nativeBuildInputs = [ pkgconfig ];
@@ -46,3 +46,4 @@ stdenv.mkDerivation rec{
     maintainers = with stdenv.lib.maintainers; [ jcumming ];
   };
 }
+
