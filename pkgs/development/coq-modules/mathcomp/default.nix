198fd526ca69873b9808202e2e8dffa6813823e7
Tim Steinbach <NeQuissimus@users.noreply.github.com>
2017-11-07 17:15:09 +0000

    Revert "coqPackages.{ssreflect,mathcomp}: 1.6.1 -> 1.6.4"

--- pkgs/development/coq-modules/mathcomp/default.nix
+++ pkgs/development/coq-modules/mathcomp/default.nix
@@ -2,9 +2,9 @@
 
 let param =
   {
-    version = "1.6.4";
-    url = https://github.com/math-comp/math-comp/archive/mathcomp-1.6.4.tar.gz;
-    sha256 = "0qmjjb6jsxmmf4gpw10r30rmrvwqgzirvvgyy41mz2vhgwis8wn6";
+    version = "1.6.1";
+    url = https://github.com/math-comp/math-comp/archive/mathcomp-1.6.1.tar.gz;
+    sha256 = "1j9ylggjzrxz1i2hdl2yhsvmvy5z6l4rprwx7604401080p5sgjw";
   }; in
 
 callPackage ./generic.nix {
