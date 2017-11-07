2d3149f4a808c2f90ed1dbf188f7371d193e7964
Vladimír Čunát <vcunat@gmail.com>
2017-11-07 06:33:09 +0100

    icu: only patch-out xlocale if using glibc

--- pkgs/development/libraries/icu/base.nix
+++ pkgs/development/libraries/icu/base.nix
@@ -26,7 +26,9 @@ stdenv.mkDerivation {
   '';
 
   # https://sourceware.org/glibc/wiki/Release/2.26#Removal_of_.27xlocale.h.27
-  postPatch = "substituteInPlace i18n/digitlst.cpp --replace '<xlocale.h>' '<locale.h>'";
+  postPatch = if stdenv ? glibc
+    then "substituteInPlace i18n/digitlst.cpp --replace '<xlocale.h>' '<locale.h>'"
+    else null; # won't find locale_t on darwin
 
   inherit patchFlags patches;
 
