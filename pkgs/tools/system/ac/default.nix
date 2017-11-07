a2ff400f54fb7f5342a4f92a3e4c4099b18e1d4b
Joerg Thalheim <joerg@thalheim.io>
2017-11-07 09:22:14 +0000

    ac: adapt to nixpkgs conventions

--- pkgs/tools/system/ac/default.nix
+++ pkgs/tools/system/ac/default.nix
@@ -1,16 +1,25 @@
-{ stdenv, fetchgit, ats }:
+{ stdenv, fetchFromGitHub, ats }:
 
 stdenv.mkDerivation rec {
   name = "ac-0.1.2";
-  src = fetchgit {
-    url = "https://github.com/vmchale/fastcat";
+
+  src = fetchFromGitHub {
+    owner = "vmchale";
+    repo = "fastcat";
     rev = "5031c47631ea5a31d4eb8a584c8226a698453125";
-    sha256 = "d2757a58a29c721f65a9677f803d5f51aa2b099fd0b2ab61f97a3a65dd01fce8";
+    sha256 = "1s7w07fnafksz5hspcnhkw4jpajibwyq0zv7m5jiywlwl9c7lxfj";
   };
-  outputs = [ "out" ];
+
+  buildInputs = [ ats ];
+
   installPhase = ''
-    PATH=$ats/bin:$PATH
-    atscc src/ac.dats -o $out -O3
+    mkdir -p $out/bin
+    atscc src/ac.dats -o $out/bin/ac -O3
   '';
-  inherit ats;
+
+  meta = with stdenv.lib; {
+    homepage = https://github.com/vmchale/fastcat;
+    license = licenses.bsd3;
+    maintainers = [];
+  };
 }
