2e55ae468662510a4d679f73376a401a2fedff47
Parnell Springmeyer <parnell@digitalmentat.com>
2017-11-06 20:48:22 -0600

    As suggested by @NickHu

--- pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
@@ -1,48 +1,40 @@
-{ stdenv, lib, fetchurl, dpkg, electron, gnome2, atk, cairo, gdk_pixbuf
-, glib, zulu, zulu8, freetype, fontconfig, fontconfig_210, dbus_daemon
-, xlibs, gnome3, glib-tested, nss, nspr, alsaLib, cups, expat, nodejs
-, libudev
+{ stdenv, lib, fetchurl, dpkg, gnome2, atk, cairo, gdk_pixbuf, glib, freetype,
+fontconfig, dbus, libX11, xlibs, libXi, libXcursor, libXdamage, libXrandr,
+libXcomposite, libXext, libXfixes, libXrender, libXtst, libXScrnSaver, nss,
+nspr, alsaLib, cups, expat, udev
 }:
 let
   rpath = lib.makeLibraryPath [
     alsaLib
     atk
     cairo
-    cups.lib
-    dbus_daemon.lib
-    electron
+    cups
+    dbus
     expat
     fontconfig
-    fontconfig_210
     freetype
     gdk_pixbuf
     glib
-    glib-tested
     gnome2.GConf
     gnome2.gtk
     gnome2.pango
-    gnome3.gconf
-    libudev
-    nodejs
+    libX11
+    libXScrnSaver
+    libXcomposite
+    libXcursor
+    libXdamage
+    libXext
+    libXfixes
+    libXi
+    libXrandr
+    libXrender
+    libXtst
     nspr
     nss
     stdenv.cc.cc
-    xlibs.libX11
-    xlibs.libX11
-    xlibs.libXScrnSaver
-    xlibs.libXcomposite
-    xlibs.libXcursor
-    xlibs.libXdamage
-    xlibs.libXext
-    xlibs.libXfixes
-    xlibs.libXi
-    xlibs.libXrandr
-    xlibs.libXrender
-    xlibs.libXtst
+    udev
     xlibs.libxcb
-    zulu
-    zulu8
-  ] + ":${stdenv.cc.cc.lib}/lib64";
+  ];
 
 in
   stdenv.mkDerivation rec {
@@ -59,21 +51,22 @@ in
       else
         throw "Signal for Desktop is not currently supported on ${stdenv.system}";
 
+    phases = [ "unpackPhase" "installPhase" ];
     nativeBuildInputs = [ dpkg ];
-    unpackPhase = "true";
-    buildCommand = ''
+    unpackPhase = "dpkg-deb -x $src .";
+    installPhase = ''
       mkdir -p $out
-      dpkg -x $src $out
+      cp -R opt $out
 
-      mv $out/usr/share $out/share
+      mv ./usr/share $out/share
       mv $out/opt/Signal $out/libexec
-      rmdir $out/usr $out/opt
+      rmdir $out/opt
 
       chmod -R g-w $out
 
       # Patch signal
-      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/libexec/signal-desktop
-      patchelf --set-rpath ${rpath}:$out/libexec $out/libexec/signal-desktop
+      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+               --set-rpath ${rpath}:$out/libexec $out/libexec/signal-desktop
 
       # Symlink to bin
       mkdir -p $out/bin
@@ -85,7 +78,7 @@ in
     '';
 
     meta = {
-      description = "Signal messenger for the desktop.";
+      description = "Signal Private Messenger for the Desktop.";
       homepage = https://signal.org/;
       license = lib.licenses.gpl3;
       platforms = [
