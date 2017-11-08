d341d28401920f26ae0328526e942762da6d250c
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-11-08 23:09:18 +0800

    ihaskell/wrapper.nix: unify ihaskellEnv and ihaskell
    Having both `ihaskell` and `ihaskellEnv` is redundant, and might even
    cause problems if the two differ somehow.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2687,7 +2687,7 @@ with pkgs;
 
 
   ihaskell = callPackage ../development/tools/haskell/ihaskell/wrapper.nix {
-    inherit (haskellPackages) ihaskell ghcWithPackages;
+    inherit (haskellPackages) ghcWithPackages;
 
     jupyter = python3.withPackages (ps: [ ps.jupyter ps.notebook ]);
 
