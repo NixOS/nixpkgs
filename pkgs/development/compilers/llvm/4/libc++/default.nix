44cb95a23460707cb100720c23aae6e497177268
aszlig <aszlig@nix.build>
2017-11-08 22:01:20 +0100

    llvmPackages/libc++: Fix build against glibc 2.26
    This is very similar to what we had in bb0b0822ef39d987952a04ae61fb8071.
    
    The xlocale.h header is no longer existing in glibc version 2.26, so we
    need to avoid including it.
    
    I've tested building against all of the libcxx attributes of LLVM 3.5,
    3.7, 3.8, 3.9, 4 and 5.
    
    All of them succeeded except version 3.5, which failed because of an
    unrelated issue (build of libc++abi has failed, one of its
    dependencies), so I only verified whether the patch applies cleanly.
    
    Signed-off-by: aszlig <aszlig@nix.build>
    Cc: @vcunat

--- pkgs/development/compilers/llvm/4/libc++/default.nix
+++ pkgs/development/compilers/llvm/4/libc++/default.nix
@@ -10,8 +10,12 @@ stdenv.mkDerivation rec {
     export LIBCXXABI_INCLUDE_DIR="$PWD/$(ls -d libcxxabi-${version}*)/include"
   '';
 
-  # https://github.com/llvm-mirror/libcxx/commit/bcc92d75df0274b9593ebd097fcae60494e3bffc
-  patches = [ ./pthread_mach_thread_np.patch ];
+  patches = [
+    # https://github.com/llvm-mirror/libcxx/commit/bcc92d75df0274b9593ebd097fcae60494e3bffc
+    ./pthread_mach_thread_np.patch
+    # glibc 2.26 fix
+    ./xlocale-glibc-2.26.patch
+  ];
 
   prePatch = ''
     substituteInPlace lib/CMakeLists.txt --replace "/usr/lib/libc++" "\''${LIBCXX_LIBCXXABI_LIB_PATH}/libc++"
