NixOS/nixpkgs

commit 4f3d971ef55b82ef349e5fb198196d2028fc0cf5

Atlassian apps: remove misleading JRE switch logic
 Only the Oracle JRE is supported by Atlassian appsAtlassian apps
 (see https://jira.atlassian.com/browse/JRASERVER-46152)
 Plus Atlassian apps are non free so the switch logic always chose
 Oracle JRE anyway.
 Option is kept in case someone want to patch apps to support openjdk.

nixos/modules/services/web-apps/atlassian/confluence.nix
@@ -127,14 +127,10 @@ in
 
 
 
-      jrePackage = let
-        jreSwitch = unfree: free: if config.nixpkgs.config.allowUnfree or false then unfree else free;
-      in mkOption {
+      jrePackage = mkOption {
         type = types.package;
-        default = jreSwitch pkgs.oraclejre8 pkgs.openjdk8.jre;
-        defaultText = jreSwitch "pkgs.oraclejre8" "pkgs.openjdk8.jre";
-        example = literalExample "pkgs.openjdk8.jre";
-        description = "Java Runtime to use for Confluence. Note that Atlassian recommends the Oracle JRE.";
+        default = pkgs.oraclejre8;
+        description = "Note that Atlassian only support the Oracle JRE (JRASERVER-46152).";
       };
     };
   };

nixos/modules/services/web-apps/atlassian/crowd.nix
@@ -93,14 +93,10 @@ in
         };
       };
 
-      jrePackage = let
-        jreSwitch = unfree: free: if config.nixpkgs.config.allowUnfree or false then unfree else free;
-      in mkOption {
+      jrePackage = mkOption {
         type = types.package;
-        default = jreSwitch pkgs.oraclejre8 pkgs.openjdk8.jre;
-        defaultText = jreSwitch "pkgs.oraclejre8" "pkgs.openjdk8.jre";
-        example = literalExample "pkgs.openjdk8.jre";
-        description = "Java Runtime to use for Crowd. Note that Atlassian recommends the Oracle JRE.";
+        default = pkgs.oraclejre8;
+        description = "Note that Atlassian only support the Oracle JRE (JRASERVER-46152).";
       };
     };
   };

nixos/modules/services/web-apps/atlassian/jira.nix
@@ -131,14 +131,10 @@ in
         };
       };
 
-      jrePackage = let
-        jreSwitch = unfree: free: if config.nixpkgs.config.allowUnfree or false then unfree else free;
-      in mkOption {
+      jrePackage = mkOption {
         type = types.package;
-        default = jreSwitch pkgs.oraclejre8 pkgs.openjdk8.jre;
-        defaultText = jreSwitch "pkgs.oraclejre8" "pkgs.openjdk8.jre";
-        example = literalExample "pkgs.openjdk8.jre";
-        description = "Java Runtime to use for JIRA. Note that Atlassian recommends the Oracle JRE.";
+        default = pkgs.oraclejre8;
+        description = "Note that Atlassian only support the Oracle JRE (JRASERVER-46152).";
       };
     };
   };

commit 0e790b9f664136a4a75b78652c9dac834d914c71

nixos/atlassian: Fix eval error in jrePackage
The error got introduced by 4f3d971ef55b82ef349e5fb198196d2028fc0cf5,
which removed the *Text attributes from the option.

This in turn leads to an evaluation error while building the
manual/manpage, because oraclejre8 is marked unfree.

Signed-off-by: aszlig <aszlig@nix.build>
Cc: @jbgi, @orivej, @globin

nixos/modules/services/web-apps/atlassian/confluence.nix
@@ -130,6 +130,7 @@ in
       jrePackage = mkOption {
         type = types.package;
         default = pkgs.oraclejre8;
+        defaultText = "pkgs.oraclejre8";
         description = "Note that Atlassian only support the Oracle JRE (JRASERVER-46152).";
       };
     };

nixos/modules/services/web-apps/atlassian/crowd.nix
@@ -96,6 +96,7 @@ in
       jrePackage = mkOption {
         type = types.package;
         default = pkgs.oraclejre8;
+        defaultText = "pkgs.oraclejre8";
         description = "Note that Atlassian only support the Oracle JRE (JRASERVER-46152).";
       };
     };

nixos/modules/services/web-apps/atlassian/jira.nix
@@ -134,6 +134,7 @@ in
       jrePackage = mkOption {
         type = types.package;
         default = pkgs.oraclejre8;
+        defaultText = "pkgs.oraclejre8";
         description = "Note that Atlassian only support the Oracle JRE (JRASERVER-46152).";
       };
     };