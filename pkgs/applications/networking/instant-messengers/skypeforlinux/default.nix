9b59ab72838e4f8ed40d798f76cae655ac3a9891
Raymond Gauthier <jraygauthier@gmail.com>
2017-11-09 23:12:57 -0500

    skypeforlinux: 5.5.0.1 -> 8.11.0.4
    cherry picked from commits:
    f7ac0760a14999837462e7338ef81e5632c93b2f
    4ce310f95dc74c4209987bcfee127b31bb798fba
    9045920bfaff301557a1a47046001971c374f2b2
    
    because 5.5.0.1 does not connect
    
    Fixes #32977

--- pkgs/applications/networking/instant-messengers/skypeforlinux/default.nix
+++ pkgs/applications/networking/instant-messengers/skypeforlinux/default.nix
@@ -1,10 +1,12 @@
 { stdenv, fetchurl, dpkg, makeWrapper
-, alsaLib, atk, cairo, cups, curl, dbus, expat, fontconfig, freetype, glib, glibc, gnome2, libsecret
-, libnotify, nspr, nss, systemd, xorg, libv4l, libstdcxx5 }:
+, alsaLib, atk, cairo, cups, curl, dbus, expat, fontconfig, freetype, glib, glibc, gnome2
+, libnotify, libpulseaudio, libsecret, libstdcxx5, libv4l, nspr, nss, systemd, xorg }:
 
 let
 
-  version = "5.5.0.1";
+  # Please keep the version x.y.0.z and do not update to x.y.76.z because the
+  # source of the latter disappears much faster.
+  version = "8.11.0.4";
 
   rpath = stdenv.lib.makeLibraryPath [
     alsaLib
@@ -17,9 +19,8 @@ let
     fontconfig
     freetype
     glib
-    libsecret
     glibc
-    libstdcxx5
+    libsecret
 
     gnome2.GConf
     gnome2.gdk_pixbuf
@@ -29,10 +30,12 @@ let
     gnome2.gnome_keyring
 
     libnotify
+    libpulseaudio
     nspr
     nss
     stdenv.cc.cc
     systemd
+    libstdcxx5
     libv4l
 
     xorg.libxkbfile
@@ -54,7 +57,7 @@ let
     if stdenv.system == "x86_64-linux" then
       fetchurl {
         url = "https://repo.skype.com/deb/pool/main/s/skypeforlinux/skypeforlinux_${version}_amd64.deb";
-        sha256 = "1r65qlsjwp0n0fmlhvbp71h03b3x73r26jk9f4q687sjqnzmkvcr";
+        sha256 = "1chwc4rqcwwim03n6nski5dar33bb1gnadbvcjg6gln3xqr0ipib";
       }
     else
       throw "Skype for linux is not supported on ${stdenv.system}";
@@ -82,8 +85,8 @@ in stdenv.mkDerivation {
 
   postFixup = ''
     for file in $(find $out -type f \( -perm /0111 -o -name \*.so\* -or -name \*.node\* \) ); do
-                patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$file" || true
-                patchelf --set-rpath ${rpath}:$out/share/skypeforlinux $file || true
+      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$file" || true
+      patchelf --set-rpath ${rpath}:$out/share/skypeforlinux $file || true
     done
 
     ln -s "$out/share/skypeforlinux/skypeforlinux" "$out/bin/skypeforlinux"
@@ -92,15 +95,13 @@ in stdenv.mkDerivation {
     substituteInPlace $out/share/applications/skypeforlinux.desktop \
       --replace /usr/bin/ $out/bin/ \
       --replace /usr/share/ $out/share/
-
   '';
 
   meta = with stdenv.lib; {
     description = "Linux client for skype";
     homepage = https://www.skype.com;
     license = licenses.unfree;
-    maintainers = with stdenv.lib.maintainers; [ panaeon ];
+    maintainers = with stdenv.lib.maintainers; [ panaeon jraygauthier ];
     platforms = [ "x86_64-linux" ];
   };
 }
-
