2f974a29c62efc5ebc3387a8f7c5e6ae870c56a3
Michael Raskin <7c6f434c@mail.ru>
2017-11-11 14:53:17 +0100

    ipmiutil: 3.0.5 -> 3.0.7

--- pkgs/tools/system/ipmiutil/default.nix
+++ pkgs/tools/system/ipmiutil/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   baseName = "ipmiutil";
-  version = "3.0.5";
+  version = "3.0.7";
   name = "${baseName}-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/project/${baseName}/${name}.tar.gz";
-    sha256 = "0ppfxh04c0aj9dxpkzj3xv1kylc17k1kvqabp8r7h70k6ljzgmxj";
+    sha256 = "0bsl4czbwmz1f42b15y0fabys50bbfll4z73nm9xk161i2njzz6y";
   };
 
   buildInputs = [ openssl ];
