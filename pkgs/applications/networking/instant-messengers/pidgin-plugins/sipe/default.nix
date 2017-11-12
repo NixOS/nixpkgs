dbd333747e53d1ae206c777f705c9168feca5a9d
Jens Binkert <jensbin@protonmail.com>
2017-11-12 09:35:21 +0100

    pidginsipe: 1.22.1 -> 1.23.0

--- pkgs/applications/networking/instant-messengers/pidgin-plugins/sipe/default.nix
+++ pkgs/applications/networking/instant-messengers/pidgin-plugins/sipe/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, pidgin, intltool, libxml2, nss, nspr }:
 
-let version = "1.22.1"; in
+let version = "1.23.0"; in
 
 stdenv.mkDerivation {
   name = "pidgin-sipe-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/sipe/pidgin-sipe-${version}.tar.gz";
-    sha256 = "f6b7b7475e349c0214eb02814b808b04850113a91dd8e8d2c26e7179a3fd1ae8";
+    sha256 = "795811ced33fcc5affae259828f6452bfc0e0b02737ea68483e1bd9ec0459013";
   };
 
   meta = with stdenv.lib; {
