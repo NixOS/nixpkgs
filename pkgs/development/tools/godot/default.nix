c19136c4c9fa1d448038023fdfedd22108a33981
Jason Hilton <jasface901@gmail.com>
2017-11-13 05:32:06 +0000

    godot: 2.1.3 -> 2.1.4
    When I tested this locally I had already merged in
    pull #31587.
    
    fixes #31588 #31587
    
    (cherry picked from commit 61b20e24156e5653df2b5e8d316331cf89cd4ee4)

--- pkgs/development/tools/godot/default.nix
+++ pkgs/development/tools/godot/default.nix
@@ -1,20 +1,21 @@
-{ stdenv, fetchFromGitHub, gcc, scons, pkgconfig, libX11, libXcursor
+{ stdenv, fetchFromGitHub, gcc5, scons, pkgconfig, libX11, libXcursor
 , libXinerama, libXrandr, libXrender, freetype, openssl, alsaLib
 , libpulseaudio, mesa_glu, zlib }:
 
 stdenv.mkDerivation rec {
   name    = "godot-${version}";
-  version = "2.1.3-stable";
+  version = "2.1.4-stable";
 
   src = fetchFromGitHub {
     owner  = "godotengine";
     repo   = "godot";
     rev    = version;
-    sha256 = "04qbab0icpv3ascr4dqgj18sqvw04a1jypcngb0ji8npa8q9wxb2";
+    sha256 = "0d2zczn5k7296sky5gllq55cxd586nx134y2iwjpkqqjr62g0h48";
   };
 
+  nativeBuildInputs = [ pkgconfig ];
   buildInputs = [
-    gcc scons pkgconfig libX11 libXcursor libXinerama libXrandr libXrender
+    gcc5 scons libX11 libXcursor libXinerama libXrandr libXrender
     freetype openssl alsaLib libpulseaudio mesa_glu zlib
   ];
 
