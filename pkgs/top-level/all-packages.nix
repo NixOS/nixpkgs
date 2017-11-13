bfe9c928c18583d3adfb5978a55b48c392649ef5
Tim Steinbach <tim@nequissimus.com>
2017-11-13 08:34:51 -0500

    linux_latest: 4.9 -> 4.14
    4.14 is going to be the next LTS kernel - http://kroah.com/log/blog/2017/10/16/linux-kernel-community-enforcement-statement/

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12715,7 +12715,7 @@ with pkgs;
   });
 
   # The current default kernel / kernel modules.
-  linuxPackages = linuxPackages_4_9;
+  linuxPackages = linuxPackages_4_14;
   linux = linuxPackages.kernel;
 
   # Update this when adding the newest kernel major version!
