e9776f044624dec78a789c3650d5e6b0f7a5b5b3
taku0 <mxxouy6x3m_github@tatapa.org>
2017-11-13 21:12:59 +0900

    firefox: 56.0.2 -> 57.0
    (cherry picked from commit 559433d0db3979f77befe73ad5c65b8500277629)
    Signed-off-by: Domen Kožar <domen@dev.si>

--- pkgs/applications/networking/browsers/firefox/common.nix
+++ pkgs/applications/networking/browsers/firefox/common.nix
@@ -28,11 +28,9 @@
 # Set to `privacySupport` or `false`.
 
 , webrtcSupport ? !privacySupport
-, geolocationSupport ? !privacySupport
-, googleAPISupport ? geolocationSupport
+, googleAPISupport ? !privacySupport
 , crashreporterSupport ? false
 
-, safeBrowsingSupport ? false
 , drmSupport ? false
 
 ## other
@@ -158,10 +156,8 @@ stdenv.mkDerivation (rec {
   ++ flag ffmpegSupport "ffmpeg"
   ++ lib.optional (!ffmpegSupport) "--disable-gstreamer"
   ++ flag webrtcSupport "webrtc"
-  ++ flag geolocationSupport "mozril-geoloc"
   ++ lib.optional googleAPISupport "--with-google-api-keyfile=ga"
   ++ flag crashreporterSupport "crashreporter"
-  ++ flag safeBrowsingSupport "safe-browsing"
   ++ lib.optional drmSupport "--enable-eme=widevine"
 
   ++ (if debugBuild then [ "--enable-debug" "--enable-profiling" ]
