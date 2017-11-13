53730c5a9228be45add3f71482f6827b3d950fa0
Tim Steinbach <tim@nequissimus.com>
2017-11-13 08:34:36 -0500

    linux: Add 4.14

---
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -0,0 +1,18 @@
+{ stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
+
+with stdenv.lib;
+
+import ./generic.nix (args // rec {
+  version = "4.14";
+
+  # modDirVersion needs to be x.y.z, will automatically add .0 if needed
+  modDirVersion = concatStrings (intersperse "." (take 3 (splitString "." "${version}.0")));
+
+  # branchVersion needs to be x.y
+  extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
+
+  src = fetchurl {
+    url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
+    sha256 = "19wn1wswvivhlxxixnqi962jamxy9wpw13g1gj2k18chgr3mj7gq";
+  };
+} // (args.argsOverride or {}))
