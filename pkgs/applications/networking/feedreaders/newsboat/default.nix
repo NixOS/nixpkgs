d03e57d382e2aea9841b56ceaa3b469c689187b5
Thomas Friese <tasmo@tasmo.de>
2017-11-15 23:21:52 +0100

    newsboat: init at r2.10.1
    because [Newsbeuter is not actively maintained](https://github.com/akrennmair/newsbeuter/#abandoned-an-actively-maintained-fork-is-available-in-newsboat-repo) anymore

---
+++ pkgs/applications/networking/feedreaders/newsboat/default.nix
@@ -0,0 +1,39 @@
+{ stdenv, fetchurl, sqlite, curl, pkgconfig, libxml2, stfl, json-c-0-11, ncurses
+, gettext, asciidoc, libiconv, makeWrapper, perl, fetchpatch }:
+
+stdenv.mkDerivation rec {
+  name = "${pname}-${version}";
+  pname = "newsboat";
+  version = "r2.10.1";
+
+  src = fetchurl {
+    url = "https://github.com/newsboat/${pname}/archive/${version}.tar.gz";
+    sha256 = "82d5e3e2a6dab845aac0bf72580f46c2756375d49214905a627284e5bc32e327";
+  };
+
+  buildInputs
+    = [ pkgconfig sqlite curl libxml2 stfl json-c-0-11 ncurses gettext asciidoc perl libiconv ]
+      ++ stdenv.lib.optional stdenv.isDarwin makeWrapper;
+
+  preBuild = ''
+    sed -i -e 107,112d config.sh
+    sed -i "1 s%^.*$%#!${perl}/bin/perl%" txt2h.pl
+    export LDFLAGS=-lncursesw
+  '';
+
+  installFlags = [ "DESTDIR=$(out)" "prefix=" ];
+
+  postInstall = stdenv.lib.optionalString stdenv.isDarwin ''
+    for prog in $out/bin/*; do
+      wrapProgram "$prog" --prefix DYLD_LIBRARY_PATH : "${stfl}/lib"
+    done
+  '';
+
+  meta = {
+    homepage    = https://newsboat.org/;
+    description = "A fork of Newsbeuter, an RSS/Atom feed reader for the text console. The only difference is that Newsboat is actively maintained while Newsbeuter isn't";
+    maintainers = with stdenv.lib.maintainers; [ tasmo ];
+    license     = stdenv.lib.licenses.mit;
+    platforms   = stdenv.lib.platforms.unix;
+  };
+}
