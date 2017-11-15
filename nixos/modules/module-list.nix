adc2cd6f2624732ebab48a66686b1cb119decd89
Parnell Springmeyer <parnell@awakenetworks.com>
2017-11-14 19:14:33 -0600

    nghttpx: Add a new NixOS module for the nghttpx proxy server
    This change also adds a global `uid` and `gid` for a `nghttpx` user
    and group as well as an integration test.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -482,6 +482,7 @@
   ./services/networking/networkmanager.nix
   ./services/networking/nftables.nix
   ./services/networking/ngircd.nix
+  ./services/networking/nghttpx/default.nix
   ./services/networking/nix-serve.nix
   ./services/networking/nntp-proxy.nix
   ./services/networking/nsd.nix
