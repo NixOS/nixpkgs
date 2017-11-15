8b3293d947b5bdf3628835b57403b3649db171a8
Tim Steinbach <tim@nequissimus.com>
2017-11-15 16:43:34 -0500

    linux: 4.4.97 -> 4.4.98

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.4.97";
+  version = "4.4.98";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "03hnm66nv7l1f21z893rghgpgghs1i2pxzhcahpi7d6nsm5mwqgq";
+    sha256 = "1mvk6vw6fjqnl01bx78viydkalgj33v2ynz4gi4yk1d357l54yar";
   };
 } // (args.argsOverride or {}))
