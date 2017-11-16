d7039d5029f248617e5e29c6d981e89e5b1bec78
Daiderd Jordan <daiderd@gmail.com>
2017-11-16 22:57:34 +0100

    libmpack: fix darwin build
    On darwin we need the correct prefix during the buildPhase, otherwise
    the dynamic libraries have an incorrect install_name.

--- pkgs/development/libraries/libmpack/default.nix
+++ pkgs/development/libraries/libmpack/default.nix
@@ -9,11 +9,11 @@ stdenv.mkDerivation rec {
     rev = version;
     sha256 = "0rai5djdkjz7bsn025k5489in7r1amagw1pib0z4qns6b52kiar2";
   };
-  LIBTOOL = "libtool";
-  buildInputs = [ libtool ];
-  preInstall = ''
-    export PREFIX=$out
-  '';
+
+  nativeBuildInputs = [ libtool ];
+
+  makeFlags = [ "LIBTOOL=libtool" "PREFIX=$(out)" ];
+
   meta = with stdenv.lib; {
     description = "Simple implementation of msgpack in C";
     homepage = https://github.com/tarruda/libmpack/;
