26f1ae6ee4a7bfb8d673eef2a4cf6e032471e29c
adisbladis <adis@blad.is>
2017-11-17 14:23:16 +0800

    libmsgpack-1_4: Drop package. Broken with gcc-7 and not depended on.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9265,7 +9265,6 @@ with pkgs;
 
   libmsgpack = callPackage ../development/libraries/libmsgpack { };
   libmsgpack_2_0 = callPackage ../development/libraries/libmsgpack/2.0.nix { };
-  libmsgpack_1_4 = callPackage ../development/libraries/libmsgpack/1.4.nix { };
 
   libmysqlconnectorcpp = callPackage ../development/libraries/libmysqlconnectorcpp {
     mysql = mysql57;
