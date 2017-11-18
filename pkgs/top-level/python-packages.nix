ab447d9d760c0b5cabb2f2f9810bcc7cdcb2caca
André-Patrick Bubel <code@apb.name>
2017-11-18 18:46:31 +0100

    pythonPackages.jsonrpclib-pelix: init at 0.3.1
    This fork of jsonrpclib supports Python 3 and is necessary for electrum
    from version 3.0.0 onwards.
    
    Adding myself - moredread - as maintainer.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5940,6 +5940,8 @@ in {
     };
   };
 
+  jsonrpclib-pelix = callPackage ../development/python-modules/jsonrpclib-pelix {};
+
   jsonwatch = buildPythonPackage rec {
     name = "jsonwatch-0.2.0";
 
