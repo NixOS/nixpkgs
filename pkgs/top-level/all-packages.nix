e59b6afd7c598c464908925ce86b23f4fd13835a
Raymond Gauthier <jraygauthier@gmail.com>
2017-11-19 09:31:32 -0500

    libwebcam: Init at 0.2.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9717,6 +9717,8 @@ with pkgs;
 
   libvorbis = callPackage ../development/libraries/libvorbis { };
 
+  libwebcam = callPackage ../os-specific/linux/libwebcam { };
+
   libwebp = callPackage ../development/libraries/libwebp { };
 
   libwmf = callPackage ../development/libraries/libwmf { };
