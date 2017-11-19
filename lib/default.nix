daf8311ce1741dfc8b97d6d85b1d83e026549d6b
Orivej Desh <orivej@gmx.fr>
2017-11-19 10:13:06 +0000

    Revert "lib: delete reference to lib/sandbox.nix"
    This reverts commit 9ee089ae05d1ca9eed567670895aeed69fbbeaac.
    
    0fe7bde3d88027977a07a24baee98d2ecc07222f is not in master yet and it would break
    master if it were: https://github.com/NixOS/nixpkgs/pull/31803#pullrequestreview-77638434

--- lib/default.nix
+++ lib/default.nix
@@ -41,6 +41,7 @@ let
     generators = callLibs ./generators.nix;
     misc = callLibs ./deprecated.nix;
     # domain-specific
+    sandbox = callLibs ./sandbox.nix;
     fetchers = callLibs ./fetchers.nix;
 
     # Eval-time filesystem handling
