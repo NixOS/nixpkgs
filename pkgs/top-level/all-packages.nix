fd565ced04c14eee3446dcbcf93682d9bf8785ca
Peter Hoeg <peter@hoeg.com>
2017-11-20 07:26:14 +0800

    mime_types: rename to mime-types

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13542,7 +13542,7 @@ with pkgs;
 
   shaderc = callPackage ../development/compilers/shaderc { };
 
-  mime_types = callPackage ../data/misc/mime-types { };
+  mime-types = callPackage ../data/misc/mime-types { };
 
   shared_mime_info = callPackage ../data/misc/shared-mime-info { };
 
