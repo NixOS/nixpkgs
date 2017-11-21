54a4b77caa17113f317c76984434c3545cf7e741
Eelco Dolstra <edolstra@gmail.com>
2017-11-21 18:22:05 +0100

    vm: Create /dev/full
    https://hydra.nixos.org/build/64519371
    (cherry picked from commit 9f74cf3e1242e9e2c16d5b4f7a8c4b535ade38e0)

--- pkgs/build-support/vm/default.nix
+++ pkgs/build-support/vm/default.nix
@@ -64,6 +64,7 @@ rec {
     ''
       mknod -m 666 ${dev}/null    c 1 3
       mknod -m 666 ${dev}/zero    c 1 5
+      mknod -m 666 ${dev}/full    c 1 7
       mknod -m 666 ${dev}/random  c 1 8
       mknod -m 666 ${dev}/urandom c 1 9
       mknod -m 666 ${dev}/tty     c 5 0
