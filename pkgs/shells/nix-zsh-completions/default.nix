4234927101e282752d9292ee0cd02b9b52cc1238
Tor Hedin Brønner <torhedinbronner@gmail.com>
2017-11-21 14:01:35 +0100

    nix-zsh-completions: 0.3.3 -> 0.3.5
    Various improvements including attribute completion when supplying a cached
    url as a repo.
    
    (cherry picked from commit d1635a58bfb503a828105f6e037974afbc11ae1e)

--- pkgs/shells/nix-zsh-completions/default.nix
+++ pkgs/shells/nix-zsh-completions/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchFromGitHub }:
 
 let
-  version = "0.3.3";
+  version = "0.3.5";
 in
 
 stdenv.mkDerivation rec {
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
     owner = "spwhitt";
     repo = "nix-zsh-completions";
     rev = "${version}";
-    sha256 = "0gvqm6grz3s0llz20sa18n5d2anmsrk892lcv7blv8rai151whpw";
+    sha256 = "1fp565qbzbbwj99rq3c28gpq8gcnlxb2glj05382zimas1dfd0y9";
   };
 
   installPhase = ''
