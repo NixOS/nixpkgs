80227d350fcfc565bc56ca5ba5b93d53004e4902
Florian Klink <flokli@flokli.de>
2017-11-22 21:50:43 +0100

    afew: enable tests

--- pkgs/development/python-modules/afew/default.nix
+++ pkgs/development/python-modules/afew/default.nix
@@ -21,8 +21,6 @@ buildPythonPackage rec {
     chardet
   ] ++ stdenv.lib.optional (!isPy3k) subprocess32;
 
-  doCheck = false;
-
   postInstall = ''
     wrapProgram $out/bin/afew \
       --prefix LD_LIBRARY_PATH : ${notmuch}/lib
