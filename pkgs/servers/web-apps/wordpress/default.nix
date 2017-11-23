93a37dcaf8503733c796338e01e1e1b32d64a7cc
Piotr Bogdan <ppbogdan@gmail.com>
2017-11-23 16:45:42 +0000

    wordpress: 4.7.4 -> 4.7.6

--- pkgs/servers/web-apps/wordpress/default.nix
+++ pkgs/servers/web-apps/wordpress/default.nix
@@ -2,8 +2,8 @@
 { fetchFromGitHub, lib } : fetchFromGitHub {
   owner = "WordPress";
   repo = "WordPress";
-  rev = "4.7.4";
-  sha256 = "1ia9d3n085x2r6pvdrv4rk6gdp6xhjhpsq5g7a6448xzv9hf14ri";
+  rev = "4.7.6";
+  sha256 = "0da44nqb6ny0r2z9m93zvigxzjz1k1ggxfc1320yy3f77ini82jr";
   meta = {
     homepage = https://wordpress.org;
     description = "WordPress is open source software you can use to create a beautiful website, blog, or app.";
