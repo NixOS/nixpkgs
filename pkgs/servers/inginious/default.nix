984e50cec5272137b0b304a69a98862af910edff
zimbatm <zimbatm@zimbatm.com>
2017-11-23 15:17:06 +0000

    inginious: 0.3a2.dev0 -> 0.4.0
    This is wholy untested. It's only propery is that it builds and can be
    markes as unbroken, so that the related module can be evaluated.

--- pkgs/servers/inginious/default.nix
+++ pkgs/servers/inginious/default.nix
@@ -1,60 +1,80 @@
 { pkgs, lib, python2Packages }:
 with lib;
+with python2Packages;
 
 let
-  pythonPackages = python2Packages;
+  # Alias back to the original names
+  PyYAML = pyyaml;
+  msgpack-python = msgpack;
+  pytidylib = tidylib;
 
-  docker_1_7_2 = pythonPackages.docker.overrideAttrs (oldAttrs: rec {
-    name = "docker-py-1.7.2";
+  docker-py = docker.overrideAttrs (oldAttrs: rec {
+    pname = "docker-py";
+    version = "1.10.6";
+    name = "${pname}-${version}";
 
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/d/docker-py/${name}.tar.gz";
-      sha256 = "0k6hm3vmqh1d3wr9rryyif5n4rzvcffdlb1k4jvzp7g4996d3ccm";
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "05f49f6hnl7npmi7kigg0ibqk8s3fhzx1ivvz1kqvlv4ay3paajc";
     };
   });
 
-  webpy-custom = pythonPackages.web.override {
-    name = "web.py-INGI";
-    src = pkgs.fetchFromGitHub {
-      owner = "UCL-INGI";
-      repo = "webpy-INGI";
-      # tip of branch "ingi"
-      rev = "f487e78d65d6569eb70003e588d5c6ace54c384f";
-      sha256 = "159vwmb8554xk98rw380p3ah170r6gm861r1nqf2l452vvdfxscd";
+  web-py = web.overrideAttrs (oldAttrs: rec {
+    pname = "web.py";
+    version = "0.40.dev0";
+    name = "${pname}-${version}";
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "18v91c4s683r7a797a8k9p56r1avwplbbcb3l6lc746xgj6zlr6l";
+    };
+  });
+
+  sphinx-rtd-theme = buildPythonPackage rec {
+    pname = "sphinx_rtd_theme";
+    version = "0.2.4";
+    name = "${pname}-${version}";
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "05rlhjzdyapr2w74jjs7mrm8hi69qskcr5vya9f9drpsys7lpxrd";
     };
   };
 
-in pythonPackages.buildPythonApplication rec {
-  version = "0.3a2.dev0";
+in buildPythonApplication rec {
+  version = "0.4.0";
   name = "inginious-${version}";
 
-  disabled = pythonPackages.isPy3k;
+  disabled = isPy3k;
 
-  patchPhase = ''
-    # transient failures
-    substituteInPlace inginious/backend/tests/TestRemoteAgent.py \
-      --replace "test_update_task_directory" "noop"
-  '';
-
-  propagatedBuildInputs = with pythonPackages; [
-    requests
-    cgroup-utils docker_1_7_2 docutils PyLTI mock pygments
-    pymongo pyyaml rpyc sh simpleldap sphinx_rtd_theme tidylib
-    websocket_client watchdog webpy-custom flup
+  propagatedBuildInputs = [
+    docker-py #>=1.9.0
+    docutils #>=0.12
+    pymongo #>=3.2.2
+    PyYAML #>=3.11",
+    web-py #>=0.40.dev0",
+    watchdog #>= 0.8.3",
+    msgpack-python #>= 0.4.7",
+    pyzmq #>= 15.3.0",
+    natsort #>= 5.0.1",
+    psutil #>= 4.4.2"
+    oauth2 #>=1.9.0.post1
+    httplib2 #>=0.9",
+    six #>=1.10.0"
+    # Doc stuff
+    pytidylib #>=0.2.4",
+    sphinx-rtd-theme #>=0.1.8"
+    # ??
+    sh #>=1.11
   ];
 
-  buildInputs = with pythonPackages; [ nose selenium virtual-display ];
+  buildInputs = [ nose selenium virtual-display ];
 
-  /* Hydra fix exists only on github for now.
-  src = pkgs.fetchurl {
-    url = "mirror://pypi/I/INGInious/INGInious-${version}.tar.gz";
-  };
-  */
   src = pkgs.fetchFromGitHub {
     owner = "UCL-INGI";
     repo = "INGInious";
-    rev = "07d111c0a3045c7cc4e464d4adb8aa28b75a6948";
-    sha256 = "0kldbkc9yw1mgg5w5q5v8k2hz089c5c4rvxb5xhbagkzgm2gn230";
+    rev = "v${version}";
+    sha256 = "06p9nzz09n32v94ky4x1v65fafjhn5vvi3y4zak31w0jixmjy205";
   };
 
   # Only patch shebangs in /bin, other scripts are run within docker
@@ -64,8 +84,9 @@ in pythonPackages.buildPythonApplication rec {
     patchShebangs $prefix/bin
   '';
 
+  doCheck = false;
+
   meta = {
-    broken = true;
     description = "An intelligent grader that allows secured and automated testing of code made by students";
     homepage = https://github.com/UCL-INGI/INGInious;
     license = licenses.agpl3;
