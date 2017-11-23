5986bd30d9fd363d7efc43a914eeeb0536e91025
Eshin Kunishima <ek@esh.ink>
2017-11-23 16:21:15 +0900

    inconsolata: 1.010 -> 2.001

--- pkgs/data/fonts/inconsolata/default.nix
+++ pkgs/data/fonts/inconsolata/default.nix
@@ -1,20 +1,28 @@
-{ stdenv, fetchzip }:
+{ stdenv, fetchFromGitHub }:
 
-let
-  version = "1.010";
-in fetchzip {
+stdenv.mkDerivation rec {
   name = "inconsolata-${version}";
+  version = "2.001";
 
-  url = "http://www.levien.com/type/myfonts/Inconsolata.otf";
+  src = fetchFromGitHub {
+    owner = "google";
+    repo = "fonts";
+    rev = "4c3e95c802f8f12b78869ff50d552014de63f9c1";
+    sha256 = "1ndmsf4c0k36dakmps0vr7hhg5ss8m7ywja7v55xdrinvli58v2f";
+  };
 
-  postFetch = "install -Dm644 $downloadedFile $out/share/fonts/opentype/inconsolata.otf";
+  installPhase = ''
+    install -m644 --target $out/share/fonts/truetype/inconsolata -D $src/ofl/inconsolata/*.ttf
+  '';
 
-  sha256 = "1yyf7agabfv0ia57c7in0r33x7c8ay445zf7c3dfc83j6w85g3i7";
+  outputHashAlgo = "sha256";
+  outputHashMode = "recursive";
+  outputHash = "04pdg5lzdid4b0gbzykia2jaiajc6pdbv5r8ml5ya9alyw30xza6";
 
   meta = with stdenv.lib; {
     homepage = http://www.levien.com/type/myfonts/inconsolata.html;
     description = "A monospace font for both screen and print";
-    maintainers = with maintainers; [ raskin rycee ];
+    maintainers = with maintainers; [ mikoim raskin rycee ];
     license = licenses.ofl;
     platforms = platforms.all;
   };
