ab02f5400e02fa6e270ca62bbf1398317cbb1a3d
Florian Klink <flokli@flokli.de>
2017-11-23 23:53:28 +0100

    astral: init at 1.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -165,6 +165,8 @@ in {
 
   asn1crypto = callPackage ../development/python-modules/asn1crypto { };
 
+  astral = callPackage ../development/python-modules/astral { };
+
   astropy = callPackage ../development/python-modules/astropy {  };
 
   augeas = callPackage ../development/python-modules/augeas {
