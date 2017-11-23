c0e77c0e789cee1b218d51cdea7f9a267dbdf5c7
Matthias Beyer <mail@beyermatthias.de>
2017-11-23 12:36:26 +0100

    Remove service options which could be covered by extraConfig

--- nixos/modules/services/networking/zeronet.nix
+++ nixos/modules/services/networking/zeronet.nix
@@ -43,47 +43,8 @@ let
   + (ifNotNull cfg.workers
                "workers = ${cfg.workers}")
 
-  + (ifNotNull cfg.fileserverIp
-               "fileserver_ip = ${cfg.fileserverIp}")
-
-  + (ifNotNull cfg.fileserverPort
-               "fileserver_port = ${cfg.fileserverPort}")
-
-  + (ifNotNull cfg.ipLocal
-               "ip_local = ${cfg.IpLocal}")
-
-  + (ifNotNull cfg.disableUdp
-               "disable_udp = true")
-
-  + (ifNotNull cfg.proxy
-               "proxy = ${cfg.proxy}")
-
   + (ifNotNull cfg.bind
                "bind = ${cfg.bind}")
-
-  + (ifNotNull cfg.ipExternal
-               "ip_external = ${cfg.ipExternal}")
-
-  + (ifNotNull cfg.trackers
-               "trackers = ${cfg.trackers}")
-
-  + (ifNotNull cfg.trackersFile
-               "trackers_file = ${cfg.trackersFile}")
-
-  + (ifNotNull cfg.useOpenssl
-               "use_openssl = ${cfg.useOpenssl}")
-
-  + (ifNotNull cfg.disableDb
-               "disable_db = ${cfg.disableDb}")
-
-  + (ifNotNull cfg.disableEncryption
-               "disable_encryption = ${xdf.disableEncryption}")
-
-  + (ifNotNull cfg.disableSslcompression
-               "disable_sslcompression = ${cfg.disableSslcompression}")
-
-  + (ifNotNull cfg.keepSslCert
-               "keep_ssl_cert = ${cfg.keepSslCert}")
 
   + (ifNotNull cfg.maxFilesOpened
                "max_files_opened = ${cfg.maxFilesOpened}")
@@ -93,51 +54,6 @@ let
 
   + (ifNotNull cfg.useTempfiles
                "use_tempfiles = ${cfg.useTempfiles}")
-
-  + (ifNotNull cfg.streamDownloads
-               "stream_downloads = ${cfg.streamDownloads}")
-
-  + (ifNotNull cfg.msgpackPurepython
-               "msgpack_purepython = ${cfg.msgpackPurepython}")
-
-  + (ifNotNull cfg.fixFloatDecimals
-               "fix_float_decimals = ${cfg.fixFloatDecimals}")
-
-  + (ifNotNull cfg.dbMode
-               "db_mode = ${cfg.dbMode}")
-
-  + (ifNotNull cfg.downloadOptional
-               "download_optional = ${cfg.downloadOptional}")
-
-  + (ifNotNull cfg.coffeescriptCompiler
-               "coffeescript_compiler = ${cfg.coffeescriptCompiler}")
-
-  + (ifNotNull cfg.tor
-               "tor = ${cfg.tor}")
-
-  + (ifNotNull cfg.torController
-               "tor_controller = ${cfg.torController}")
-
-  + (ifNotNull cfg.torProxy
-               "tor_proxy = ${cfg.torProxy}")
-
-  + (ifNotNull cfg.torPassword
-               "tor_password = ${cfg.torPassword}")
-
-  + (ifNotNull cfg.torHsLimit
-               "tor_hs_limit = ${cfg.torHsLimit}")
-
-  + (ifNotNull cfg.bitResolver
-               "bit_resolver = ${cfg.bitResolver}")
-
-  + (ifNotNull cfg.optionalLimit
-               "optional_limit = ${cfg.optionalLimit}")
-
-  + (ifNotNull cfg.pinBigfile
-               "pin_bigfile = ${cfg.pinBigfile}")
-
-  + (ifNotNull cfg.autodownloadBigfileSizeLimit
-               "autodownload_bigfile_size_limit = ${cfg.autodownloadBigfileSizeLimit}")
 
   + cfg.extraConfig;
 
@@ -196,6 +112,7 @@ in
           plain text)
           '';
         };
+
         uiPort = mkOption {
           type = types.nullOr types.str;
           default = null;
@@ -259,98 +176,7 @@ in
           '';
         };
 
-        fileserverIp = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        fileserverPort = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        ipLocal = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        disableUdp = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        proxy = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
         bind = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        ipExternal = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        trackers = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        trackersFile = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        useOpenssl = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        disableDb = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        disableEncryption = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        disableSslcompression = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        keepSslCert = mkOption {
           type = types.nullOr types.str;
           default = null;
           description = ''
@@ -372,111 +198,6 @@ in
         };
 
         useTempfiles = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        streamDownloads = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        msgpackPurepython = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        fixFloatDecimals = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        dbMode = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        downloadOptional = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        coffeescriptCompiler = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        tor = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        torController = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        torProxy = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        torPassword = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        torHsLimit = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        bitResolver = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        optionalLimit = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        pinBigfile = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-          '';
-        };
-
-        autodownloadBigfileSizeLimit = mkOption {
           type = types.nullOr types.str;
           default = null;
           description = ''
