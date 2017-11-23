6a3b92626e43b5ae432eb7f81f76af92b59a2e13
Frederik Rietdijk <fridh@fridh.nl>
2017-11-23 17:04:05 +0100

    python.pkgs.psutil: 4.3.0 -> 5.4.1

--- pkgs/development/python-modules/psutil/default.nix
+++ pkgs/development/python-modules/psutil/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "psutil";
-  version = "4.3.0";
+  version = "5.4.1";
   name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1w4r09fvn6kd80m5mx4ws1wz100brkaq6hzzpwrns8cgjzjpl6c6";
+    sha256 = "42e2de159e3c987435cb3b47d6f37035db190a1499f3af714ba7af5c379b6ba2";
   };
 
   # No tests in archive
