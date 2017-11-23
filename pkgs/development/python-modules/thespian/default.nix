2233ae6a9977f01095ad6944ecc1e0b6319842c4
Kevin Quick <quick@sparq.org>
2017-11-22 21:23:43 -0800

    Removed extraneous comment; updated note reg testing use of network.

--- pkgs/development/python-modules/thespian/default.nix
+++ pkgs/development/python-modules/thespian/default.nix
@@ -9,13 +9,12 @@ buildPythonPackage rec {
     inherit pname version;
     extension = "zip";
     sha256 = "0vvwsh3waxd5ldrayr86kdcshv07bp361fl7p16g9i044vklwly4";
-    # fdbbea4edb6bad0ac0e53fc7bc6970e78e12eef4944aa4146bcdcb573201676c";
   };
 
   # Do not run the test suite: it takes a long type and uses
-  # significant system resources.  Thespian tests are performed via
-  # it's Travis CI configuration and do not need to be duplicated
-  # here.
+  # significant system resources, including requiring localhost
+  # network operations.  Thespian tests are performed via it's Travis
+  # CI configuration and do not need to be duplicated here.
   doCheck = false;
 
   meta = with lib; {
