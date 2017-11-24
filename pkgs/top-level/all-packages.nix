77a3331a10daa985452a65c4313730c743c184ef
Robert Scott <code@humanleg.org.uk>
2017-11-24 12:12:47 +0000

    cloudfoundry-cli: init at 6.32.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7089,6 +7089,8 @@ with pkgs;
 
   "cl-launch" = callPackage ../development/tools/misc/cl-launch {};
 
+  cloudfoundry-cli = callPackage ../development/tools/cloudfoundry-cli { };
+
   coan = callPackage ../development/tools/analysis/coan { };
 
   compile-daemon = callPackage ../development/tools/compile-daemon { };
