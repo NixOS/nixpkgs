cb9bf1c8541f68513eb446c89e763657a6a9a1ef
Kier Davis <kierdavis@gmail.com>
2017-11-24 00:03:22 +0000

    PySolFC: init at 2.0
    PySolFC is a solitaire game written in Python, supporting thousands of
    solitaire variants.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18080,6 +18080,8 @@ with pkgs;
 
   privateer = callPackage ../games/privateer { };
 
+  pysolfc = callPackage ../games/pysolfc { };
+
   qweechat = callPackage ../applications/networking/irc/qweechat { };
 
   qqwing = callPackage ../games/qqwing { };
