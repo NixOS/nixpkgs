f62c87b45fde41e7131819a7dd452c2bb97a149d
aszlig <aszlig@nix.build>
2017-11-24 23:13:41 +0100

    linuxPackages: Revert accidental bump to 4.14
    Regression introduced by e5c24abf3b7f12232b7d2ae08415537d54d75c91.
    
    I've added this for my WIP tests and forgot to reset them out of the
    mentioned commits.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12757,7 +12757,7 @@ with pkgs;
   });
 
   # The current default kernel / kernel modules.
-  linuxPackages = linuxPackages_4_14;
+  linuxPackages = linuxPackages_4_9;
   linux = linuxPackages.kernel;
 
   # Update this when adding the newest kernel major version!
