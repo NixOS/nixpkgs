3914c27b39e2b76012e89332344924410b6b9e32
Thomas Friese <tasmo@tasmo.de>
2017-11-24 11:18:56 +0100

    fixes and merges from @nicknovitsk
    still does not compile man pages

--- pkgs/applications/networking/feedreaders/newsboat/default.nix
+++ pkgs/applications/networking/feedreaders/newsboat/default.nix
@@ -1,21 +1,27 @@
-{ stdenv, fetchurl, sqlite, curl, pkgconfig, libxml2, stfl, json-c-0-11, ncurses
+{ stdenv, fetchzip, sqlite, curl, pkgconfig, libxml2, stfl, json-c-0-11, ncurses
 , gettext, asciidoc, docbook_xml_xslt, libiconv, makeWrapper, perl, fetchpatch }:
 
 stdenv.mkDerivation rec {
-  name = "${pname}-${version}";
-  pname = "newsboat";
-  version = "r2.10.1";
+  name    = "${pname}-${version}";
+  pname   = "newsboat";
+  version = "2.10.1";
 
-  src = fetchurl {
-    url = "https://github.com/newsboat/${pname}/archive/${version}.tar.gz";
-    sha256 = "82d5e3e2a6dab845aac0bf72580f46c2756375d49214905a627284e5bc32e327";
+  src = fetchzip {
+    url = "https://newsboat.org/releases/${version}/${name}.tar.xz";
+    sha256 = "019bm8j9vbpj39vs6xhrj34cd9ipjyqpwkl5psaks2w6g7wzyp1p";
   };
 
-  outputs = [ "out" "doc" ];
-
   buildInputs
-    = [ pkgconfig sqlite curl libxml2 stfl json-c-0-11 ncurses gettext asciidoc docbook_xml_xslt libiconv ]
+    = [ pkgconfig sqlite curl libxml2 stfl json-c-0-11 ncurses gettext
+        asciidoc docbook_xml_xslt libiconv ]
       ++ stdenv.lib.optional stdenv.isDarwin makeWrapper;
+
+  postPatch = ''
+    substituteInPlace config.sh \
+      --replace "ncurses5.4" "ncurses"
+    substituteInPlace ./Makefile \
+      --replace "a2x" "${asciidoc}/bin/a2x --no-xmllint"
+  '';
 
   preBuild = ''
     sed -i -e 107,112d config.sh
@@ -26,16 +32,17 @@ stdenv.mkDerivation rec {
   installFlags = [ "DESTDIR=$(out)" "prefix=" ];
 
   postInstall = stdenv.lib.optionalString stdenv.isDarwin ''
+    cp -r $src/contrib $out
     for prog in $out/bin/*; do
       wrapProgram "$prog" --prefix DYLD_LIBRARY_PATH : "${stfl}/lib"
     done
   '';
 
-  meta = {
+  meta = with stdenv.lib; {
     homepage    = https://newsboat.org/;
-    description = "A fork of Newsbeuter, an RSS/Atom feed reader for the text console. The only difference is that Newsboat is actively maintained while Newsbeuter isn't";
-    maintainers = with stdenv.lib.maintainers; [ tasmo ];
-    license     = stdenv.lib.licenses.mit;
-    platforms   = stdenv.lib.platforms.unix;
+    description = "An open-source RSS/Atom feed reader for the text console";
+    maintainers = with maintainers; [ nicknovitsk itasmo ];
+    license     = licenses.mit;
+    platforms   = platforms.unix;
   };
 }
