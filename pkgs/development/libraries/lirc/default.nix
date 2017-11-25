47b9934a99bb5b539dd87813f2f2c104349652b3
Christian Kögler <ck3d@gmx.de>
2017-11-25 22:23:57 +0100

    lirc: 0.9.4 -> 0.9.4d + more driver
    * added a bunch of optional libraries to get more IR drivers built
    * removed deprecated configure flags
    * unneeded make flags
    * simplified install flags

--- pkgs/development/libraries/lirc/default.nix
+++ pkgs/development/libraries/lirc/default.nix
@@ -1,31 +1,41 @@
-{ stdenv, fetchurl, alsaLib, bash, help2man, pkgconfig, xlibsWrapper, python3, libxslt }:
+{ stdenv, fetchurl, alsaLib, bash, help2man, pkgconfig, xlibsWrapper
+, python3Packages, libxslt, systemd, libusb, libftdi1 }:
 
 stdenv.mkDerivation rec {
-  name = "lirc-0.9.4";
+  name = "lirc-0.9.4d";
 
   src = fetchurl {
     url = "mirror://sourceforge/lirc/${name}.tar.bz2";
-    sha256 = "19c6ldjsdnk1md66q3nb035ja1xj217k8iabhxpsb8rs10a6kwi6";
+    sha256 = "1as19rnaz9vpp58kbk9q2lch51vf2fdi27bl19f8d6s8bg1ii3y6";
   };
 
-  preBuild = "patchShebangs .";
+  postPatch = ''
+    patchShebangs .
+
+    # fix overriding PYTHONPATH
+    sed -i 's,PYTHONPATH=,PYTHONPATH=$(PYTHONPATH):,' \
+      doc/Makefile.in
+  '';
+
+  preConfigure = ''
+    # use empty inc file instead of a from linux kernel generated one
+    touch lib/lirc/input_map.inc
+  '';
 
   nativeBuildInputs = [ pkgconfig help2man ];
 
-  buildInputs = [ alsaLib xlibsWrapper python3 libxslt ];
+  buildInputs = [ alsaLib xlibsWrapper libxslt systemd libusb libftdi1 ]
+  ++ (with python3Packages; [ python pyyaml ]);
 
   configureFlags = [
-    "--with-driver=devinput"
     "--sysconfdir=/etc"
     "--localstatedir=/var"
-    "--enable-sandboxed"
+    "--with-systemdsystemunitdir=$(out)/lib/systemd/system"
   ];
 
-  makeFlags = [ "m4dir=$(out)/m4" ];
-
   installFlags = [
-    "sysconfdir=\${out}/etc"
-    "localstatedir=\${TMPDIR}"
+    "sysconfdir=$out/etc"
+    "localstatedir=$TMPDIR"
   ];
 
   meta = with stdenv.lib; {
