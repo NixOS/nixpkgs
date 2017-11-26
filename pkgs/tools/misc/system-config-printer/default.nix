f83bb7d1eafd6ee0aa0d42c360c1a9026366e264
Frederik Rietdijk <fridh@fridh.nl>
2017-11-26 09:15:39 +0100

    Revert "Merge pull request #32024 from andir/unstable-fix-gnome3-printing"
    This reverts commit 03207c067c6fab03ff59d04c569b6b84f72e40ee, reversing
    changes made to 822342ffdfc01464866aca0451c55b782b2d7b07.
    
    Reverted because the change was not needed anymore:
    https://github.com/NixOS/nixpkgs/pull/32024#issuecomment-346947664

--- pkgs/tools/misc/system-config-printer/default.nix
+++ pkgs/tools/misc/system-config-printer/default.nix
@@ -18,47 +18,49 @@ stdenv.mkDerivation rec {
 
   patches = [ ./detect_serverbindir.patch ];
 
-  buildInputs = [
-    intltool pkgconfig glib udev libusb1 cups xmlto
-    libxml2 docbook_xml_dtd_412 docbook_xsl desktop_file_utils
-
-    libnotify gobjectIntrospection gdk_pixbuf pango atk
-    libgnome_keyring3
-
-    (pythonPackages.python.withPackages (ps: with ps; [
-      pycups pycurl dbus-python pygobject3 requests pycairo pysmbc
-    ]))
-  ];
+  buildInputs =
+    [ intltool pkgconfig glib udev libusb1 cups xmlto
+      libxml2 docbook_xml_dtd_412 docbook_xsl desktop_file_utils
+      pythonPackages.python pythonPackages.wrapPython
+      libnotify gobjectIntrospection gdk_pixbuf pango atk
+      libgnome_keyring3
+    ];
 
   nativeBuildInputs = [ wrapGAppsHook ];
 
-  configureFlags = [
-    "--with-udev-rules"
-    "--with-udevdir=$(out)/etc/udev"
-    "--with-systemdsystemunitdir=$(out)/etc/systemd/system"
-  ];
+  pythonPath = with pythonPackages; requiredPythonModules [ pycups pycurl dbus-python pygobject3 requests pycairo pysmbc ];
+
+  configureFlags =
+    [ "--with-udev-rules"
+      "--with-udevdir=$(out)/etc/udev"
+      "--with-systemdsystemunitdir=$(out)/etc/systemd/system"
+    ];
 
   stripDebugList = [ "bin" "lib" "etc/udev" ];
 
-  postInstall = ''
-    gappsWrapperArgs+=(
-      --prefix PATH : "$program_PATH"
-      --prefix PYTHONPATH : "$out/${pythonPackages.python.sitePackages}"
-      --set CUPS_DATADIR "${cups-filters}/share/cups"
-    )
+  postInstall =
+    ''
+      buildPythonPath "$out $pythonPath"
+      gappsWrapperArgs+=(
+        --prefix PATH : "$program_PATH"
+        --set CUPS_DATADIR "${cups-filters}/share/cups"
+      )
 
-    # The below line will be unneeded when the next upstream release arrives.
-    sed -i -e "s|/usr/local/bin|$out/bin|" \
-      "$out/share/dbus-1/services/org.fedoraproject.Config.Printing.service"
+      find $out/share/system-config-printer -name \*.py -type f -perm -0100 -print0 | while read -d "" f; do
+        patchPythonScript "$f"
+      done
 
-    # Manually expand literal "$(out)", which have failed to expand
-    sed -e "s|ExecStart=\$(out)|ExecStart=$out|" \
-      -i "$out/etc/systemd/system/configure-printer@.service"
-  '';
+      # The below line will be unneeded when the next upstream release arrives.
+      sed -i -e "s|/usr/local/bin|$out/bin|" "$out/share/dbus-1/services/org.fedoraproject.Config.Printing.service"
 
-  meta = with stdenv.lib; {
+      # Manually expand literal "$(out)", which have failed to expand
+      sed -e "s|ExecStart=\$(out)|ExecStart=$out|" \
+          -i "$out/etc/systemd/system/configure-printer@.service"
+    '';
+
+  meta = {
     homepage = http://cyberelk.net/tim/software/system-config-printer/;
-    platforms = platforms.linux;
-    license = licenses.gpl2;
+    platforms = stdenv.lib.platforms.linux;
+    license = stdenv.lib.licenses.gpl2;
   };
 }
