455cef7d5eb59988a545203a804fd7a365acf32c
Eric Litak <elitak@gmail.com>
2017-11-26 20:02:34 -0800

    opentx: init at 2.2.1
    This does not yet cross-compile the firmware images.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16825,6 +16825,8 @@ with pkgs;
 
   openscad = callPackage ../applications/graphics/openscad {};
 
+  opentx = callPackage ../applications/misc/opentx { };
+
   opera = callPackage ../applications/networking/browsers/opera {};
 
   orca = python3Packages.callPackage ../applications/misc/orca {
