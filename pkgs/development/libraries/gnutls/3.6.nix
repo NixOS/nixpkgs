739366580352591a817509ddbeeb0e11bed47d43
Vladimír Čunát <vcunat@gmail.com>
2017-11-28 07:58:08 +0100

    gnutls: fixup download URL after update
    I'm sorry about that; I forgot 3.5 in there.
    Still, I don't know why their ftp won't work well with curl;
    wget is perfectly OK.

--- pkgs/development/libraries/gnutls/3.6.nix
+++ pkgs/development/libraries/gnutls/3.6.nix
@@ -4,7 +4,7 @@ callPackage ./generic.nix (args // rec {
   version = "3.6.1";
 
   src = fetchurl {
-    url = "ftp://ftp.gnutls.org/gcrypt/gnutls/v3.5/gnutls-${version}.tar.xz";
+    url = "http://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-${version}.tar.xz";
     sha256 = "1vdsir53ddxyapnxh5fpnfyij3scx3818iri4hl07g4lk4n0vc90";
   };
 
