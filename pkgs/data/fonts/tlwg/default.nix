e5203208e3a4ef12fe8eaf42749f5dff0a84f86e
Joerg Thalheim <joerg@thalheim.io>
2017-11-28 12:41:20 +0000

    tlwg: don't depend on .git folder
    This is always kind of ugly.

--- pkgs/data/fonts/tlwg/default.nix
+++ pkgs/data/fonts/tlwg/default.nix
@@ -1,26 +1,26 @@
-{ stdenv, fetchgit, autoconf, automake, git, fontforge }:
+{ stdenv, fetchFromGitHub, autoreconfHook, fontforge }:
 
 stdenv.mkDerivation rec {
   name = "tlwg-${version}";
   version = "0.6.4";
 
-  src = fetchgit {
-   url = "https://github.com/tlwg/fonts-tlwg";
-   rev = "v${version}";
-   sha256 = "1agvarmcy467y1nk670j3vffgx7ihld4z1l2fpyhbybzqwdp708k";
-   leaveDotGit = true;
+  src = fetchFromGitHub {
+    owner = "tlwg";
+    repo = "fonts-tlwg";
+    rev = "v${version}";
+    sha256 = "13bx98ygyyizb15ybdv3856lkxhx1fss8f7aiqmp0lk9zgw4mqyk";
   };
 
-  preConfigure = ''
-    ./autogen.sh
-  '';
+  nativeBuildInputs = [ autoreconfHook ];
 
-  buildInputs = [ autoconf automake fontforge git ];
+  buildInputs = [ fontforge ];
+
+  preAutoreconf = "echo ${version} > VERSION";
 
   meta = with stdenv.lib; {
     description = "A collection of Thai scalable fonts available under free licenses";
     homepage = https://linux.thai.net/projects/fonts-tlwg;
-    license = [ licenses.gpl2 licenses.publicDomain licenses.lppl13c licenses.free ];
+    license = with licenses; [ gpl2 publicDomain lppl13c free ];
     platforms = platforms.unix;
     maintainers = [ maintainers.yrashk ];
   };
