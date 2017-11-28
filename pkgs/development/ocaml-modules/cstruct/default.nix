81143b67aafdf90af910c7625942df6a9f276e97
Vincent Laporte <Vincent.Laporte@gmail.com>
2017-11-28 01:11:15 +0000

    ocamlPackages.cstruct: fix evaluation

--- pkgs/development/ocaml-modules/cstruct/default.nix
+++ pkgs/development/ocaml-modules/cstruct/default.nix
@@ -1,28 +1,28 @@
 { stdenv, fetchurl, ocaml, jbuilder, findlib, sexplib, ocplib-endian }:
 
 stdenv.mkDerivation rec {
-	name = "ocaml${ocaml.version}-cstruct-${version}";
-	version = "3.0.2";
-	src = fetchurl {
-		url = "https://github.com/mirage/ocaml-cstruct/releases/download/v${version}/cstruct-${version}.tbz";
-		sha256 = "03caxcyzfjmbnnwa15zy9s1ckkl4sc834d1qkgi4jcs3zqchvd8z";
-	};
+  name = "ocaml${ocaml.version}-cstruct-${version}";
+  version = "3.0.2";
+  src = fetchurl {
+    url = "https://github.com/mirage/ocaml-cstruct/releases/download/v${version}/cstruct-${version}.tbz";
+    sha256 = "03caxcyzfjmbnnwa15zy9s1ckkl4sc834d1qkgi4jcs3zqchvd8z";
+  };
 
-	unpackCmd = "tar -xjf $curSrc";
+  unpackCmd = "tar -xjf $curSrc";
 
-	buildInputs = [ ocaml jbuilder findlib ];
+  buildInputs = [ ocaml jbuilder findlib ];
 
-	propagatedBuildInputs = [ sexplib ocplib-endian ];
+  propagatedBuildInputs = [ sexplib ocplib-endian ];
 
-	buildPhase = "jbuilder build -p cstruct";
+  buildPhase = "jbuilder build -p cstruct";
 
-	inherit (jbuilder) installPhase;
+  inherit (jbuilder) installPhase;
 
-	meta = {
-		description = "Access C-like structures directly from OCaml";
-		license = stdenv.lib.licenses.isc;
-		maintainers = [ stdenv.lib.maintainers.vbgl ];
-		inherit (src.meta) homepage;
-		inherit (ocaml.meta) platforms;
-	};
+  meta = {
+    description = "Access C-like structures directly from OCaml";
+    license = stdenv.lib.licenses.isc;
+    homepage = "https://github.com/mirage/ocaml-cstruct";
+    maintainers = [ stdenv.lib.maintainers.vbgl ];
+    inherit (ocaml.meta) platforms;
+  };
 }
