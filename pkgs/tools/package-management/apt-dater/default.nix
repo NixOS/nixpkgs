248dfe3f5651f19c35ddd12e77dec93df39794e9
Joerg Thalheim <joerg@thalheim.io>
2017-11-29 21:13:39 +0000

    apt-dater: split of native dependencies

--- pkgs/tools/package-management/apt-dater/default.nix
+++ pkgs/tools/package-management/apt-dater/default.nix
@@ -1,4 +1,7 @@
-{ stdenv, automake115x, autoconf, vim, glib, clang, libxml2, fetchurl, openssl, ncurses, pkgconfig, popt, screen, gettext }:
+{ stdenv, fetchurl
+, automake115x, autoconf, pkgconfig, gettext
+, vim, glib, libxml2, openssl, ncurses, popt, screen
+}:
 
 stdenv.mkDerivation rec {
   name = "apt-dater-${version}";
@@ -9,24 +12,36 @@ stdenv.mkDerivation rec {
     sha256 = "891b15e4dd37c7b35540811bbe444e5f2a8d79b1c04644730b99069eabf1e10f";
   };
 
-  buildInputs = [ clang libxml2 pkgconfig ncurses automake115x autoconf vim glib popt screen gettext ];
+  nativeBuildInputs = [
+    pkgconfig automake115x autoconf gettext
+  ];
+
+  buildInputs = [
+    libxml2 ncurses vim glib popt screen
+  ];
 
   configureFlags = [ "--disable-history" ];
 
   prePatch = ''
-    substituteInPlace etc/Makefile.am --replace 02770 0770
+    substituteInPlace etc/Makefile.am \
+      --replace 02770 0770
   '';
 
   postPatch = ''
-    substituteInPlace configure.ac --replace "/usr/bin/screen" "${screen}/bin/screen"
+    substituteInPlace configure.ac \
+      --replace "/usr/bin/screen" "${screen}/bin/screen"
   '';
 
   doCheck = true;
 
   meta = with stdenv.lib; {
     homepage = https://github.com/DE-IBH/apt-dater;
-    description = "terminal-based remote package update manager";
-    longDescription = "apt-dater provides an ncurses frontend for managing package updates on a large number of remote hosts using SSH. It supports Debian-based managed hosts as well as rug (e.g. openSUSE) and yum (e.g. CentOS) based systems.";
+    description = "Terminal-based remote package update manager";
+    longDescription = ''
+      Provides an ncurses frontend for managing package updates on a large
+      number of remote hosts using SSH. It supports Debian-based managed hosts
+      as well as rug (e.g. openSUSE) and yum (e.g. CentOS) based systems.
+    '';
     license = licenses.gpl2;
     maintainers = with maintainers; [ enko ];
   };
