a58b35aa0fa6b08ca5a4c7cdb032e64c46597c4c
Wout Mertens <Wout.Mertens@gmail.com>
2017-11-29 20:41:32 +0100

    pam-sshagent: only allow managed SSH keys
    [Breaking change] Fixes #316

--- nixos/modules/security/pam.nix
+++ nixos/modules/security/pam.nix
@@ -257,7 +257,7 @@ let
           ${optionalString cfg.logFailures
               "auth required pam_tally.so"}
           ${optionalString (config.security.pam.enableSSHAgentAuth && cfg.sshAgentAuth)
-              "auth sufficient ${pkgs.pam_ssh_agent_auth}/libexec/pam_ssh_agent_auth.so file=~/.ssh/authorized_keys:~/.ssh/authorized_keys2:/etc/ssh/authorized_keys.d/%u"}
+              "auth sufficient ${pkgs.pam_ssh_agent_auth}/libexec/pam_ssh_agent_auth.so file=/etc/ssh/authorized_keys.d/%u"}
           ${optionalString cfg.fprintAuth
               "auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so"}
           ${optionalString cfg.u2fAuth
