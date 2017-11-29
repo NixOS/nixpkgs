NixOS/nixpkgs

commit 366f1525ff47bfbee398eeac0cec0edd85e999ac

nano: 2.9.0 -> 2.9.1
See http://lists.gnu.org/archive/html/info-gnu/2017-11/msg00012.html
for release information

pkgs/applications/editors/nano/default.nix
@@ -20,11 +20,11 @@ let
 
 in stdenv.mkDerivation rec {
   name = "nano-${version}";
-  version = "2.9.0";
+  version = "2.9.1";
 
   src = fetchurl {
     url = "mirror://gnu/nano/${name}.tar.xz";
-    sha256 = "17hjgvig59a2ha2b0494bprrci3d33sayjqvxjhsnlzgr8whrlyj";
+    sha256 = "0z5sxji8jh8sh0g3inbzndhsrbm4qyqlvjrxl5wkxbr61lnxa5k3";
   };
 
   nativeBuildInputs = [ texinfo ] ++ optional enableNls gettext;

commit 14ec308514ac1a7498687349134e7c0ef0fbfd53

nano: fix darwin build
Broken by 366f1525ff47bfbee398eeac0cec0edd85e999ac in #32140.

pkgs/applications/editors/nano/default.nix
@@ -27,6 +27,8 @@ in stdenv.mkDerivation rec {
     sha256 = "0z5sxji8jh8sh0g3inbzndhsrbm4qyqlvjrxl5wkxbr61lnxa5k3";
   };
 
+  patches = [ ./nano-2.9.1-darwin.patch ];
+
   nativeBuildInputs = [ texinfo ] ++ optional enableNls gettext;
   buildInputs = [ ncurses ];
 
@@ -41,6 +43,8 @@ in stdenv.mkDerivation rec {
   postInstall = ''
     cp ${nixSyntaxHighlight}/nix.nanorc $out/share/nano/
   '';
+
+  enableParallelBuilding = true;
 
   meta = {
     homepage = https://www.nano-editor.org/;

pkgs/applications/editors/nano/nano-2.9.1-darwin.patch
@@ -1,0 +1,9 @@
+--- a/lib/stat.c
++++ b/lib/stat.c
+@@ -48,4 +48,6 @@ orig_stat (const char *filename, struct stat *buf)
+ #include "sys/stat.h"
+ 
++#include "stat-time.h"
++
+ #include <errno.h>
+ #include <limits.h>