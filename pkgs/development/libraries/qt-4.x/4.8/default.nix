c8d822252b86022a71dcc4f0f48bc869ef446401
Orivej Desh <orivej@gmx.fr>
2017-11-30 09:30:24 +0000

    qt4: delete parallel-build.patch to replace with a generic fix

--- pkgs/development/libraries/qt-4.x/4.8/default.nix
+++ pkgs/development/libraries/qt-4.x/4.8/default.nix
@@ -126,11 +126,6 @@ stdenv.mkDerivation rec {
     sed -i 's/QMAKE_CXX = g++/QMAKE_CXX = clang++/' mkspecs/common/g++-base.conf
   '';
 
-  postConfigure = ''
-    echo "applying patch ${./parallel-build.patch}"
-    patch -p1 < ${./parallel-build.patch}
-  '';
-
   prefixKey = "-prefix ";
 
   configureFlags =
