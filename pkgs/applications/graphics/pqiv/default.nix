1d10e7f1c5e32d3fd933c3cf039fb8803be447b5
Bart Brouns <bart@magnetophon.nl>
2017-12-01 19:48:45 +0100

    pqiv: 2.9 -> 2.10.1

--- pkgs/applications/graphics/pqiv/default.nix
+++ pkgs/applications/graphics/pqiv/default.nix
@@ -1,23 +1,23 @@
-{ stdenv, fetchFromGitHub, getopt, which, pkgconfig, gtk2 } :
+{ stdenv, fetchFromGitHub, getopt, which, pkgconfig, gtk3 } :
 
 stdenv.mkDerivation (rec {
   name = "pqiv-${version}";
-  version = "2.9";
+  version = "2.10.1";
 
   src = fetchFromGitHub {
     owner = "phillipberndt";
     repo = "pqiv";
     rev = version;
-    sha256 = "1xncf6aq52zpxpmz3ikmlkinz7y3nmbpgfxjb7q40sqs00n0mfsd";
+    sha256 = "06blqckj3bpbi2kl5ndv2d10r7nw62r386kfwrkic9amynlv9gki";
   };
 
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ getopt which gtk2 ];
+  buildInputs = [ getopt which gtk3 ];
 
   prePatch = "patchShebangs .";
 
   meta = with stdenv.lib; {
-    description = "Rewrite of qiv (quick image viewer)";
+    description = "Powerful image viewer with minimal UI";
     homepage = http://www.pberndt.com/Programme/Linux/pqiv;
     license = licenses.gpl3;
     maintainers = [ maintainers.ndowens ];
