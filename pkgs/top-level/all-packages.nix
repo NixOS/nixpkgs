777f49bfff76be99eb2fd2ed89d91d315fac2d37
Tim Steinbach <tim@nequissimus.com>
2017-12-01 10:04:09 -0500

    linux: 4.9.65 -> 4.9.66

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12560,7 +12560,6 @@ with pkgs;
   linux_4_9 = callPackage ../os-specific/linux/kernel/linux-4.9.nix {
     kernelPatches =
       [ kernelPatches.bridge_stp_helper
-        kernelPatches.p9_fixes
         kernelPatches.cpu-cgroup-v2."4.9"
         kernelPatches.modinst_arg_list_too_long
       ]
