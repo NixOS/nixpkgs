7a12b3de448576806a56694918ec10802ce186aa
Vincent Laporte <Vincent.Laporte@gmail.com>
2017-12-02 08:41:40 +0000

    coqPackages_8_4.coqExtLib: remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18806,7 +18806,6 @@ with pkgs;
       in
         recurseIntoAttrs contribs;
 
-    coqExtLib = callPackage ../development/coq-modules/coq-ext-lib {};
     coqeal = callPackage ../development/coq-modules/coqeal {};
     coquelicot = callPackage ../development/coq-modules/coquelicot {};
     domains = callPackage ../development/coq-modules/domains {};
