9d18aa073d676f749f862413694dfd38b7eca801
Jan Tojnar <jtojnar@gmail.com>
2017-12-02 03:25:39 +0100

    gnome3.cheese: reorganize build inputs

--- pkgs/desktops/gnome-3/apps/cheese/default.nix
+++ pkgs/desktops/gnome-3/apps/cheese/default.nix
@@ -1,14 +1,16 @@
 { stdenv, intltool, fetchurl, wrapGAppsHook, gnome-video-effects, libcanberra_gtk3
 , pkgconfig, gtk3, glib, clutter_gtk, clutter-gst, udev, gst_all_1, itstool
-, libgudev
+, libgudev, vala, appstream-glib
 , adwaita-icon-theme, librsvg, gdk_pixbuf, gnome3, gnome_desktop, libxml2 }:
 
 stdenv.mkDerivation rec {
   inherit (import ./src.nix fetchurl) name src;
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ gtk3 glib intltool wrapGAppsHook gnome-video-effects itstool
-                  gdk_pixbuf adwaita-icon-theme librsvg udev gst_all_1.gstreamer libxml2
+  nativeBuildInputs = [
+    pkgconfig intltool itstool vala wrapGAppsHook libxml2 appstream-glib
+  ];
+  buildInputs = [ gtk3 glib gnome-video-effects
+                  gdk_pixbuf adwaita-icon-theme librsvg udev gst_all_1.gstreamer
                   gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good gnome_desktop
                   gst_all_1.gst-plugins-bad clutter_gtk clutter-gst
                   libcanberra_gtk3 libgudev ];
