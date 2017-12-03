601c895bc13fd2c60db2f9dab59c741c4693a4c4
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2017-12-03 18:11:24 +0100

    abcMIDI: 2017.06.10	-> 2017.11.27

--- pkgs/tools/audio/abcmidi/default.nix
+++ pkgs/tools/audio/abcmidi/default.nix
@@ -1,15 +1,16 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv, fetchurl, unzip }:
 
 stdenv.mkDerivation rec {
   name = "abcMIDI-${version}";
-  version = "2017.06.10";
+  version = "2017.11.27";
 
-  src = fetchFromGitHub {
-    owner = "leesavide";
-    repo = "abcmidi";
-    rev = name;
-    sha256 = "0y92m3mj63vvy79ksq4z5hgkz6w50drg9a4bmbk6jylny0l0bdpy";
+  # You can find new releases on http://ifdo.ca/~seymour/runabc/top.html
+  src = fetchurl {
+    url = "http://ifdo.ca/~seymour/runabc/${name}.zip";
+    sha256 = "095nnyaqnsr3v7hsswpad9g0hxdnr4s6z8yk1bmr3g1j0cfv1xs9";
   };
+
+  nativeBuildInputs = [ unzip ];
 
   # There is also a file called "makefile" which seems to be preferred by the standard build phase
   makefile = "Makefile";
@@ -17,7 +18,7 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     homepage = http://abc.sourceforge.net/abcMIDI/;
     license = licenses.gpl2Plus;
-    description = "abc <-> MIDI conversion utilities";
+    description = "Utilities for converting between abc and MIDI";
     maintainers = [ maintainers.dotlambda ];
   };
 }
