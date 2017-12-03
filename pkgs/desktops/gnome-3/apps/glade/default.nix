dc206ce033df17fd4ffcbc761d3efcab6491005a
Jan Tojnar <jtojnar@gmail.com>
2017-12-03 01:51:52 +0100

    gnome3.glade: 3.20.0 → 3.20.2

--- pkgs/desktops/gnome-3/apps/glade/default.nix
+++ pkgs/desktops/gnome-3/apps/glade/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, intltool, fetchurl, python, autoreconfHook
+{ stdenv, intltool, fetchurl, python
 , pkgconfig, gtk3, glib, gobjectIntrospection
 , wrapGAppsHook, itstool, libxml2, docbook_xsl
 , gnome3, gdk_pixbuf, libxslt }:
@@ -6,32 +6,14 @@
 stdenv.mkDerivation rec {
   inherit (import ./src.nix fetchurl) name src;
 
-  propagatedUserEnvPkgs = [ gnome3.gnome_themes_standard ];
-
   nativeBuildInputs = [
     pkgconfig intltool itstool wrapGAppsHook docbook_xsl libxslt gobjectIntrospection
-    # reconfiguration
-    autoreconfHook gnome3.gnome_common gnome3.yelp_tools
   ];
   buildInputs = [ gtk3 glib libxml2 python
                   gnome3.gsettings_desktop_schemas
                   gdk_pixbuf gnome3.defaultIconTheme ];
 
   enableParallelBuilding = true;
-
-  patches = [
-    # https://bugzilla.gnome.org/show_bug.cgi?id=782161
-    (fetchurl {
-      url = https://bugzilla.gnome.org/attachment.cgi?id=351054;
-      sha256 = "093wjjj40027pkqqnm14jb2dp2i2m8p1bayqx1lw18pq66c8fahn";
-    })
-  ];
-
-  preFixup = ''
-    wrapProgram "$out/bin/glade" \
-      --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE" \
-      --prefix XDG_DATA_DIRS : "${gnome3.gnome_themes_standard}/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
-  '';
 
   meta = with stdenv.lib; {
     homepage = https://wiki.gnome.org/Apps/Glade;
