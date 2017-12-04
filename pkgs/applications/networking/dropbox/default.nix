86ab2403c90f30c13509dc7bc6477278b87f5be8
Benjamin Staffin <benley@gmail.com>
2017-12-04 18:54:48 -0500

    dropbox: fix version sort order
    This makes the startup wrapper work as intended instead of
    re-downgrading Dropbox after each time it updates itself.
    
    (cherry picked from commit 7a9784c571a89455c88c7d79bab87b6d704944ae)

--- pkgs/applications/networking/dropbox/default.nix
+++ pkgs/applications/networking/dropbox/default.nix
@@ -54,7 +54,7 @@ buildFHSUserEnv {
         do_install=1
     else
         installed_version=$(cat "$HOME/.dropbox-dist/VERSION")
-        latest_version=$(printf "${version}\n$installed_version\n" | sort -V | head -n 1)
+        latest_version=$(printf "${version}\n$installed_version\n" | sort -rV | head -n 1)
         if [ "x$installed_version" != "x$latest_version" ]; then
             do_install=1
         fi
