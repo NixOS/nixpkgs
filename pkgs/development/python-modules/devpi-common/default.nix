8002a3b760fb843b47e062b02802a3fdebaa9412
makefu <github@syntax-fehler.de>
2017-12-04 15:09:50 +0100

    devpi-common: 3.1.0 -> 3.2.0

--- pkgs/development/python-modules/devpi-common/default.nix
+++ pkgs/development/python-modules/devpi-common/default.nix
@@ -2,20 +2,18 @@
 
 with pythonPackages;buildPythonPackage rec {
   pname = "devpi-common";
-  version = "3.1.0";
+  version = "3.2.0";
   name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "d89634a57981ed43cb5dcd25e00c9454ea111189c5ddc08d945b3d5187ada5fd";
+    sha256 = "0rh119iw5hk41gsvbjr0wixvl1i4f0b1vcnw9ym35rmcp517z0wb";
   };
 
   propagatedBuildInputs = [ requests py ];
   checkInputs = [ pytest ];
 
   checkPhase = ''
-    # Don't know why this test is failing!
-    substituteInPlace testing/test_request.py --replace "test_env" "noop_test_env"
     py.test
   '';
 
