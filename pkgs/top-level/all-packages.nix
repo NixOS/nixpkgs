38e2228a3540af86e88858ca465cb07c40f102f6
Patrick Chilton <chpatrick@gmail.com>
2017-12-04 14:59:25 +0100

    meshlab: 1.3.3 -> 2016.12

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15724,7 +15724,7 @@ with pkgs;
 
   merkaartor = libsForQt5.callPackage ../applications/misc/merkaartor { };
 
-  meshlab = callPackage ../applications/graphics/meshlab { };
+  meshlab = libsForQt5.callPackage ../applications/graphics/meshlab { };
 
   metersLv2 = callPackage ../applications/audio/meters_lv2 { };
 
