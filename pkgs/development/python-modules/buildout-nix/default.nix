4d237df79d019bccb3745084b98217ee7821b17d
Asko Soukka <asko.soukka@iki.fi>
2017-12-04 22:13:16 +0200

    pythonPackages.zc_buildout_nix: 2.9.5 -> 2.10.0

--- pkgs/development/python-modules/buildout-nix/default.nix
+++ pkgs/development/python-modules/buildout-nix/default.nix
@@ -2,12 +2,12 @@
 
 buildPythonPackage rec {
   pname = "zc.buildout";
-  version = "2.9.5";
+  version = "2.10.0";
   name = "${pname}-nix-${version}";
 
   src = fetchurl {
     url = "mirror://pypi/${builtins.substring 0 1 pname}/${pname}/${pname}-${version}.tar.gz";
-    sha256 = "dce840ea379e2ff10bae27465778708704e0a4287cde92eb6c7961f5b0277cfa";
+    sha256 = "00wi0f6wpfl2gywr02x2yqvx6i1k0ll5w4lhdl0khijk4g7mk8dq";
   };
 
   patches = [ ./nix.patch ];
