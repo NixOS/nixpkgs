6841064ac538291820a67145810ce4bf139b74bd
aszlig <aszlig@nix.build>
2017-12-04 06:38:19 +0100

    python/hetzner: 0.7.5 -> 0.8.0
    New features:
    
     * Support for retrieving reverse PTRs.
     * Support for subnet-ranges.
     * Add logging (aszlig/hetzner#14).
    
    Fixes:
    
     * Hide internal methods from the public API.
     * Fix Python 3 compatibility.
     * Fix for creating admin accounts with Hetzner's new login site.
     * Fix __repr__/__str__ issue with some exceptions (aszlig/hetzner#23).
     * Fix login for RobotWebInterface
    
    Changes for the hetznerctl utility:
    
     * show: Show subnets
     * show: Show reverse PTRs
     * New 'rdns' subcommand for getting/setting/removing reverse-PTRs.
     * Use 'argparse' instead of 'optparse'.
     * Add command for managing admin accounts.
     * New '--debug' flag for printing debugging information.
    
    This also fixes NixOS/nixops#778.
    
    Tested building against Python 2.7 and Python 3.6.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9406,17 +9406,14 @@ in {
 
   hetzner = buildPythonPackage rec {
     name = "hetzner-${version}";
-    version = "0.7.5";
+    version = "0.8.0";
 
     src = pkgs.fetchFromGitHub {
       repo = "hetzner";
-      owner = "RedMoonStudios";
+      owner = "aszlig";
       rev = "v${version}";
-      sha256 = "1fw7i1z4a39i1ljd9qd4f5p1p3a4257jfglkdpw90xjwl7fdpq42";
+      sha256 = "04q2q2w2qkhfly8rfjg2h5pnh42gs18l6cmipqc37yf7qvkw3nd0";
     };
-
-    # not there yet, but coming soon.
-    doCheck = false;
 
     meta = {
       homepage = "https://github.com/RedMoonStudios/hetzner";
