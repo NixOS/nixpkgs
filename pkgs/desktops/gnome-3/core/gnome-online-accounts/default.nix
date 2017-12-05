5d91b063ff4bdc54362a58cbeee0a15da1f61400
Jan Tojnar <jtojnar@gmail.com>
2017-12-05 22:25:09 +0100

    gnome3.gnome_online_accounts: build devdocs

--- pkgs/desktops/gnome-3/core/gnome-online-accounts/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-online-accounts/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, pkgconfig, vala, glib, libxslt, gtk, wrapGAppsHook
-, webkitgtk, json_glib, rest, libsecret, dbus_glib, gnome_common
+, webkitgtk, json_glib, rest, libsecret, dbus_glib, gnome_common, gtk_doc
 , telepathy_glib, intltool, dbus_libs, icu, glib_networking
 , libsoup, docbook_xsl_ns, docbook_xsl, gnome3
 }:
@@ -9,13 +9,17 @@ stdenv.mkDerivation rec {
 
   NIX_CFLAGS_COMPILE = "-I${dbus_glib.dev}/include/dbus-1.0 -I${dbus_libs.dev}/include/dbus-1.0";
 
-  outputs = [ "out" "man" "dev" ];
+  outputs = [ "out" "man" "dev" "devdoc" ];
+
+  configureFlags = [
+    "--enable-gtk-doc"
+  ];
 
   enableParallelBuilding = true;
 
   nativeBuildInputs = [
     pkgconfig vala gnome_common intltool wrapGAppsHook
-    libxslt docbook_xsl_ns docbook_xsl
+    libxslt docbook_xsl_ns docbook_xsl gtk_doc
   ];
   buildInputs = [
     glib gtk webkitgtk json_glib rest libsecret dbus_glib telepathy_glib glib_networking icu libsoup
