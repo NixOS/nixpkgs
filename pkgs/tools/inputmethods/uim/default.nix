b1186bcb4722fa42af4736fe8fc254948d20d3c9
Jan Malakhovski <oxij@oxij.org>
2017-12-05 11:44:42 +0000

    uim: add even more options

--- pkgs/tools/inputmethods/uim/default.nix
+++ pkgs/tools/inputmethods/uim/default.nix
@@ -8,6 +8,14 @@
 , withQt4 ? withQt, qt4 ? null
 , withKde ? withQt
 , withKde4 ? withKde && withQt4, kdelibs4 ? null, automoc4 ? null
+, withKNotify4 ? withKde4
+, withLibnotify ? !withKNotify4, libnotify ? null
+, withSqlite ? true, sqlite ? null
+, withNetworking ? true, curl ? null, openssl ? null
+, withFFI ? true, libffi ? null
+
+# Things that are clearly an overkill to be enabled by default
+, withMisc ? false, libeb ? null
 }:
 
 with stdenv.lib;
@@ -17,6 +25,12 @@ assert withGtk2 -> gtk2 != null;
 assert withGtk3 -> gtk3 != null;
 assert withQt4 -> qt4 != null;
 assert withKde4 -> withQt4 && kdelibs4 != null && automoc4 != null;
+assert withKNotify4 -> withKde4 && !withLibnotify;
+assert withLibnotify -> !withKNotify4 && libnotify != null;
+assert withSqlite -> sqlite != null;
+assert withNetworking -> curl != null && openssl != null;
+assert withFFI -> libffi != null;
+assert withMisc -> libeb != null;
 
 stdenv.mkDerivation rec {
   version = "1.8.6";
@@ -37,7 +51,14 @@ stdenv.mkDerivation rec {
   ++ optional withQt4 qt4
   ++ optionals withKde4 [
     kdelibs4 automoc4
-  ];
+  ]
+  ++ optional withLibnotify libnotify
+  ++ optional withSqlite sqlite
+  ++ optionals withNetworking [
+    curl openssl
+  ]
+  ++ optional withFFI libffi
+  ++ optional withMisc libeb;
 
   patches = [ ./data-hook.patch ];
 
@@ -54,10 +75,25 @@ stdenv.mkDerivation rec {
   ++ optionals withQt4 [
     "--with-qt4"
     "--with-qt4-immodule"
-  ] ++ optionals withKde4 [
-    "--enable-kde4-applet"
-    "--enable-notify=knotify4"
-  ];
+  ]
+  ++ optional withKde4 "--enable-kde4-applet"
+  ++ optional withKNotify4 "--enable-notify=knotify4"
+  ++ optional withLibnotify "--enable-notify=libnotify"
+  ++ optional withSqlite "--with-sqlite3"
+  ++ optionals withNetworking [
+    "--with-curl"
+    "--with-openssl-dir=${openssl.dev}"
+  ]
+  ++ optional withFFI "--with-ffi"
+  ++ optional withMisc "--with-eb";
+
+  # TODO: things in `./configure --help`, but not in nixpkgs
+  #--with-canna            Use Canna [default=no]
+  #--with-wnn              Build with libwnn [default=no]
+  #--with-mana             Build a plugin for Mana [default=yes]
+  #--with-prime            Build a plugin for PRIME [default=yes]
+  #--with-sj3              Use SJ3 [default=no]
+  #--with-osx-dcs          Build with OS X Dictionary Services [default=no]
 
   dontUseCmakeConfigure = true;
 
