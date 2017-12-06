38925de8400cd0d94d4e225857f362c24f0a7be8
Maik Figura <maik@maikfigura.de>
2017-12-06 20:48:16 +0100

    0.3.1 -> 0.6.0

--- pkgs/development/python-modules/pluggy/default.nix
+++ pkgs/development/python-modules/pluggy/default.nix
@@ -1,12 +1,12 @@
 { stdenv, buildPythonPackage, fetchPypi, pytest, six, doCheck ? true }:
 buildPythonPackage rec {
   pname = "pluggy";
-  version = "0.3.1";
+  version = "0.6.0";
   name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "18qfzfm40bgx672lkg8q9x5hdh76n7vax99aank7vh2nw21wg70m";
+    sha256 = "7f8ae7f5bdf75671a718d2daf0a64b7885f74510bcd98b1a0bb420eb9a9d0cff";
   };
 
   # Circular dependency on pytest
