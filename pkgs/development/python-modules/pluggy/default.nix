50001ccf8128a5179ce61641f10f1f266d81b83b
Maik Figura <maik@maikfigura.de>
2017-12-07 18:44:53 +0100

    Remove name and redundant call doCheck

--- pkgs/development/python-modules/pluggy/default.nix
+++ pkgs/development/python-modules/pluggy/default.nix
@@ -2,18 +2,18 @@
 buildPythonPackage rec {
   pname = "pluggy";
   version = "0.6.0";
-  name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "7f8ae7f5bdf75671a718d2daf0a64b7885f74510bcd98b1a0bb420eb9a9d0cff";
   };
 
-  checkInputs = stdenv.lib.optionals doCheck [ pytest ];
+  checkInputs = [ pytest ];
 
   checkPhase = ''
     pytest testing/
   '';
+
 
   meta = with stdenv.lib; {
     description = "A minimalist production ready plugin system";
