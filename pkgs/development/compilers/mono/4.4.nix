90bcfc78c3fcf1439ac7951861cfa266d1c6da1f
Austin Seipp <aseipp@pobox.com>
2017-12-07 17:29:21 -0600

    nixpkgs: no parallel builds for mono 4.4
    See #32386 -- while Mono in general should build correctly with parallel
    building, it seems the 4.4 branch has broken.
    
    Instead, allow parallel build support to be overridden by individual
    versions, and default to true.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/development/compilers/mono/4.4.nix
+++ pkgs/development/compilers/mono/4.4.nix
@@ -4,4 +4,5 @@ callPackage ./generic.nix (rec {
   inherit Foundation libobjc;
   version = "4.4.2.11";
   sha256 = "0cxnypw1j7s253wr5hy05k42ghgg2s9qibp10kndwnp5bv12q34h";
+  buildParallel = false; # see #32386 -- parallel building broken on 4.4
 })
