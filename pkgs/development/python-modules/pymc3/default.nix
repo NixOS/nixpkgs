300840c56c4814610b54e51483ca895ce7f967fb
Ilya Kolpakov <ilya.kolpakov@gmail.com>
2017-12-07 22:07:34 +0100

    pythonPackages.pymc3: init at 3.2 (w/PR review feedback)

--- pkgs/development/python-modules/pymc3/default.nix
+++ pkgs/development/python-modules/pymc3/default.nix
@@ -1,44 +1,53 @@
 { lib
-, fetchurl
+, fetchPypi
 , buildPythonPackage
 , Theano
-, gfortran
-, openblas
-, numpy
-, scipy
-, matplotlib
-, h5py
 , pandas
 , patsy
-, tqdm
 , joblib
+, tqdm
+, six
+, h5py
+, run-tests ? false # these are quite heavy
+, pytest
+, nose
+, nose-parameterized
+, matplotlib
 }:
 
 buildPythonPackage rec {
   pname = "pymc3";
   version = "3.2";
 
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "0hpzhkpv7sbwkcva7x914yvzcf1d1a952ynbcx6mvlgv5lqghc39";
+  };
+
+  patches = [ ./0001-strip-pytest-cov.patch ];
+
   propagatedBuildInputs = [
     Theano
-    gfortran
-    openblas
-    numpy
-    scipy
-    matplotlib
-    # unzip
-    h5py
     pandas
     patsy
-    tqdm
     joblib
+    tqdm
+    six
+    h5py
   ];
 
-  doCheck = false;
-
-  src = fetchurl {
-    url = "https://github.com/pymc-devs/pymc3/archive/v${version}.zip";
-    sha256 = "091vwngzv7pfwgb1rba52f6rma4lmz1livbf1qgk4lsgp15vw145";
-  };
+  checkInputs = [
+    pytest
+    nose
+    nose-parameterized
+  ];
+  doCheck = run-tests;
+  # If enabled tests are run during the install phase.
+  preInstall = ''
+    echo "Setting HOME for theano as it tries to write in ~/.theano"
+    export HOME=$(mktemp -d)
+  '';
+  postInstall = "rm -rf $HOME";
 
   meta = {
     description = "Bayesian estimation, particularly using Markov chain Monte Carlo (MCMC)";
