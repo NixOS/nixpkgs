414c57ba72bcefa4fd426ce8f6bb0e4346e6ded9
Andreas Rammhold <andreas@rammhold.de>
2017-12-08 10:25:59 +0100

    erlangR20: 20.1 -> 20.1.7 (fixes CVE-2017-1000385)
    (cherry picked from commit 175f2e147f95214731b26733aea20461b12ca3bc)

--- pkgs/development/interpreters/erlang/R20.nix
+++ pkgs/development/interpreters/erlang/R20.nix
@@ -1,8 +1,8 @@
 { mkDerivation, fetchurl }:
 
 mkDerivation rec {
-  version = "20.0";
-  sha256 = "12dbay254ivnakwknjn5h55wndb0a0wqx55p156h8hwjhykj2kn0";
+  version = "20.1.7";
+  sha256 = "0sbxl10d76bm7awxb9s07l9815jiwfg78bps07xj2ircxdr08pls";
 
   prePatch = ''
     substituteInPlace configure.in --replace '`sw_vers -productVersion`' '10.10'
