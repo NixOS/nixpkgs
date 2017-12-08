afd3dbf63672cc4bd63bd2f8c04cc61d87705d19
Yuriy Taraday <yorik.sar@gmail.com>
2017-12-08 07:44:06 +0400

    chromium: 62.0.3202.94 -> 63.0.3239.84
    New stable release with bunch of security fixes and other changes [0]
    
    Also:
    * remove patch for dev already landed upstream
    * remove patches specific to version 62
    * dev is broken again, need to investigate failures
    
    [0] https://chromereleases.googleblog.com/2017/12/stable-channel-update-for-desktop.html
    
    (cherry picked from commit 994a614ca3a82f3bf99d2c6504c94964955ee1ab)
    Signed-off-by: Herwig Hochleitner <herwig@bendlas.net>
    ref #32433

--- pkgs/applications/networking/browsers/chromium/common.nix
+++ pkgs/applications/networking/browsers/chromium/common.nix
@@ -64,9 +64,8 @@ let
     # "libjpeg" # fails with multiple undefined references to chromium_jpeg_*
     # "re2" # fails with linker errors
     # "ffmpeg" # https://crbug.com/731766
-  ] ++ optionals (versionRange "62" "63") [
-    "harfbuzz-ng" # in versions over 63 harfbuzz and freetype are being built together
-                  # so we can't build with one from system and other from source
+    # "harfbuzz-ng" # in versions over 63 harfbuzz and freetype are being built together
+                    # so we can't build with one from system and other from source
   ];
 
   opusWithCustomModes = libopus.override {
@@ -80,9 +79,8 @@ let
     xdg_utils yasm minizip libwebp
     libusb1 re2 zlib
     ffmpeg libxslt libxml2
-  ] ++ optionals (versionRange "62" "63") [
-    harfbuzz-icu # in versions over 63 harfbuzz and freetype are being built together
-                 # so we can't build with one from system and other from source
+    # harfbuzz-icu # in versions over 63 harfbuzz and freetype are being built together
+                   # so we can't build with one from system and other from source
   ];
 
   # build paths and release info
@@ -139,17 +137,9 @@ let
       # https://gitweb.gentoo.org/repo/gentoo.git/plain/www-client/chromium/
       # https://git.archlinux.org/svntogit/packages.git/tree/trunk?h=packages/chromium
       # for updated patches and hints about build flags
-      ++ optionals (versionRange "62" "63") [
-      ./patches/chromium-gn-bootstrap-r17.patch
-      ./patches/chromium-gcc5-r3.patch
-      ./patches/chromium-glibc2.26-r1.patch
-    ]
       ++ optionals (versionRange "63" "64") [
       ./patches/chromium-gcc5-r4.patch
       ./patches/include-math-for-round.patch
-    ]
-      ++ optionals (versionAtLeast version "64") [
-      ./patches/gn_bootstrap_observer.patch
     ]
       ++ optional enableWideVine ./patches/widevine.patch;
 
@@ -269,15 +259,14 @@ let
         ninja -C "${buildPath}"  \
           -j$(( ($NIX_BUILD_CORES+1) / 2 )) -l$(( $NIX_BUILD_CORES+1 )) \
           "${target}"
-      '' + optionalString (target == "mksnapshot" || target == "chrome") ''
-        paxmark m "${buildPath}/${target}"
-      '' + optionalString (versionAtLeast version "63") ''
         (
           source chrome/installer/linux/common/installer.include
           PACKAGE=$packageName
           MENUNAME="Chromium"
           process_template chrome/app/resources/manpage.1.in "${buildPath}/chrome.1"
         )
+      '' + optionalString (target == "mksnapshot" || target == "chrome") ''
+        paxmark m "${buildPath}/${target}"
       '';
       targets = extraAttrs.buildTargets or [];
       commands = map buildCommand targets;
