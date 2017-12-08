c0ab4d5ff03b9d804f00a3d529cf490f7d604263
Orivej Desh <orivej@gmx.fr>
2017-12-08 21:33:53 +0000

    ed: fix cross build

--- pkgs/applications/editors/ed/default.nix
+++ pkgs/applications/editors/ed/default.nix
@@ -2,7 +2,7 @@
 , buildPlatform, hostPlatform
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (rec {
   name = "ed-${version}";
   version = "1.14.2";
 
@@ -36,4 +36,9 @@ stdenv.mkDerivation rec {
     maintainers = [ ];
     platforms = stdenv.lib.platforms.unix;
   };
-}
+} // stdenv.lib.optionalAttrs (hostPlatform != buildPlatform) {
+  # This may be moved above during a stdenv rebuild.
+  preConfigure = ''
+    configureFlagsArray+=("CC=$CC")
+  '';
+})
