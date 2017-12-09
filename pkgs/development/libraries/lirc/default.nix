e369aa52444cbda7ff1e909ce5bb404a2e1871a3
Christian Kögler <ck3d@gmx.de>
2017-12-09 23:35:13 +0100

    lirc: 0.9.4d -> 0.10.0

--- pkgs/development/libraries/lirc/default.nix
+++ pkgs/development/libraries/lirc/default.nix
@@ -2,17 +2,19 @@
 , libxslt, systemd, libusb, libftdi1 }:
 
 stdenv.mkDerivation rec {
-  name = "lirc-0.9.4d";
+  name = "lirc-0.10.0";
 
   src = fetchurl {
     url = "mirror://sourceforge/lirc/${name}.tar.bz2";
-    sha256 = "1as19rnaz9vpp58kbk9q2lch51vf2fdi27bl19f8d6s8bg1ii3y6";
+    sha256 = "0lzmqcw0sc28s19yd4bqvl52p4f77razq50w7z92a4xrn7l2sz75";
   };
 
   postPatch = ''
     patchShebangs .
 
     # fix overriding PYTHONPATH
+    sed -i 's,^PYTHONPATH *= *,PYTHONPATH := $(PYTHONPATH):,' \
+      Makefile.in
     sed -i 's,PYTHONPATH=,PYTHONPATH=$(PYTHONPATH):,' \
       doc/Makefile.in
   '';
@@ -25,12 +27,14 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ pkgconfig help2man ];
 
   buildInputs = [ alsaLib xlibsWrapper libxslt systemd libusb libftdi1 ]
-  ++ (with python3.pkgs; [ python pyyaml ]);
+  ++ (with python3.pkgs; [ python pyyaml setuptools ]);
 
   configureFlags = [
     "--sysconfdir=/etc"
     "--localstatedir=/var"
     "--with-systemdsystemunitdir=$(out)/lib/systemd/system"
+    "--enable-uinput" # explicite activation because build env has no uinput
+    "--enable-devinput" # explicite activation because build env has not /dev/input
   ];
 
   installFlags = [
