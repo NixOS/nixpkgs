286eadc1475d9f30177222e215c12d1463b73d6e
Shea Levy <shea@shealevy.com>
2017-12-09 15:28:39 -0500

    Add gurobipy for python2.7 on Linux

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5634,6 +5634,8 @@ in {
     then callPackage ../development/python-modules/gurobipy/darwin.nix
            { inherit (pkgs.darwin) cctools insert_dylib;
            }
+  else if stdenv.system == "x86_64-linux"
+    then callPackage ../development/python-modules/gurobipy/linux.nix {}
   else throw "gurobipy not yet supported on ${stdenv.system}";
 
   helper = buildPythonPackage rec {
