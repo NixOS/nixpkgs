ae1ba39c7d52253c7936124a5a49924df456338c
Thomas Tuegel <ttuegel@mailbox.org>
2017-12-11 12:20:29 -0600

    taglib: 1.10 -> 1.11.1
    The homepage is also updated to http://taglib.org/.

--- pkgs/development/libraries/taglib/default.nix
+++ pkgs/development/libraries/taglib/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, zlib, cmake}:
 
 stdenv.mkDerivation rec {
-  name = "taglib-1.10";
+  name = "taglib-1.11.1";
 
   src = fetchurl {
-    url = "http://taglib.github.io/releases/${name}.tar.gz";
-    sha256 = "1alv6vp72p0x9i9yscmz2a71anjwqy53y9pbcbqxvc1c0i82vhr4";
+    url = "http://taglib.org/releases/${name}.tar.gz";
+    sha256 = "0ssjcdjv4qf9liph5ry1kngam1y7zp8fzr9xv4wzzrma22kabldn";
   };
 
   cmakeFlags = "-DWITH_ASF=ON -DWITH_MP4=ON";
@@ -14,7 +14,7 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ cmake ];
 
   meta = {
-    homepage = http://developer.kde.org/~wheeler/taglib.html;
+    homepage = http://taglib.org/;
     repositories.git = git://github.com/taglib/taglib.git;
 
     description = "A library for reading and editing the meta-data of several popular audio formats";
