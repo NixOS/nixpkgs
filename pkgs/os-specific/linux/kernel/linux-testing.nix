05f159b624f9013672af17e381fc1bc4edf82c18
aszlig <aszlig@nix.build>
2017-12-11 10:21:45 +0100

    linux-testing: 4.15-rc2 -> 4.15-rc3
    Built successfully on x86_64-linux.
    
    I've verified the contents of the tarball by comparing its contents
    against the tag of my local checkout. Furthermore, I've checked the GPG
    signature of the tag.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -1,13 +1,13 @@
 { stdenv, hostPlatform, fetchurl, perl, buildLinux, libelf, utillinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.15-rc2";
-  modDirVersion = "4.15.0-rc2";
+  version = "4.15-rc3";
+  modDirVersion = "4.15.0-rc3";
   extraMeta.branch = "4.15";
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "1i79gkjipj1q7w0d4zjz2hj43r12jicgznxk0wz0la2d8a4d3lcq";
+    sha256 = "1rbyh5phx6mfr3wrz3q33gj8bgw2r76hvbzhvq1ya7fw54jjnz98";
   };
 
   # Should the testing kernels ever be built on Hydra?
