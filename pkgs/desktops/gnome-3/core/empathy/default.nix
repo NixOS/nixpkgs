cb4e5f94f437bd4666fda32f6f9c20e8b5f44da4
Jan Tojnar <jtojnar@gmail.com>
2017-12-12 20:53:59 +0100

    gnome3.empathy: enable more features

--- pkgs/desktops/gnome-3/core/empathy/default.nix
+++ pkgs/desktops/gnome-3/core/empathy/default.nix
@@ -6,9 +6,8 @@
 , telepathy_logger, libnotify, clutter, libsoup, gnutls
 , evolution_data_server, yelp_xsl
 , libcanberra_gtk3, p11_kit, farstream, libtool, shared_mime_info
-, bash, wrapGAppsHook, itstool, libxml2, libxslt, icu, libgee }:
-
-# TODO: enable more features
+, bash, wrapGAppsHook, itstool, libxml2, libxslt, icu, libgee
+, isocodes, enchant, libchamplain, geoclue2, geocode_glib, cheese, libgudev }:
 
 stdenv.mkDerivation rec {
   inherit (import ./src.nix fetchurl) name src;
@@ -24,7 +23,7 @@ stdenv.mkDerivation rec {
     libxml2 libxslt yelp_xsl
   ];
   buildInputs = [
-    gtk3 glib webkitgtk icu
+    gtk3 glib webkitgtk icu gnome_online_accounts
     telepathy_glib clutter_gtk clutter-gst cogl
     gst_all_1.gstreamer gst_all_1.gst-plugins-base
     gcr libsecret libpulseaudio gdk_pixbuf
@@ -32,6 +31,12 @@ stdenv.mkDerivation rec {
     libcanberra_gtk3 telepathy_farstream farstream
     gnome3.defaultIconTheme gnome3.gsettings_desktop_schemas
     librsvg
+    # Spell-checking
+    enchant isocodes
+    # Display maps, location awareness, geocode support
+    libchamplain geoclue2 geocode_glib
+    # Cheese webcam support, camera monitoring
+    cheese libgudev
   ];
 
   meta = with stdenv.lib; {
