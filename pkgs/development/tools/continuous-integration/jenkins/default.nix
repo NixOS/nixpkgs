a47baa6f1bf73f2b572baffcfeedd3589d6a24c3
James Earl Douglas <james@earldouglas.com>
2017-12-12 08:05:01 -0700

    jenkins: 2.93 -> 2.94
    (cherry picked from commit 8699ccb8ac85cf635f60fa1307da59f3a496ee9a)

--- pkgs/development/tools/continuous-integration/jenkins/default.nix
+++ pkgs/development/tools/continuous-integration/jenkins/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "jenkins-${version}";
-  version = "2.93";
+  version = "2.94";
 
   src = fetchurl {
     url = "http://mirrors.jenkins-ci.org/war/${version}/jenkins.war";
-    sha256 = "1wk62lmll0abbgl3drmrf5kg8hya3glkwx7h0gfhcna3vjb95ll1";
+    sha256 = "1s7kk2h7vabnny0vqsa1qhjn4r1mqk5l34jx4s6z0g7508rb4900";
   };
 
   buildCommand = ''
