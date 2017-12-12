6a6c62954e0ca33a93a67c5fd9811f31a2558ce2
Franz Pletz <fpletz@fnordicwalking.de>
2017-12-12 14:27:49 +0100

    xmr-stak: 2.0.0 -> 2.1.0
    (cherry picked from commit f429d40f4a3fde7b16ec24066eb4535cd2562f65)

--- pkgs/applications/misc/xmr-stak/default.nix
+++ pkgs/applications/misc/xmr-stak/default.nix
@@ -1,28 +1,30 @@
 { stdenv, lib, fetchFromGitHub, cmake, libuv, libmicrohttpd, openssl
 , opencl-headers, ocl-icd, hwloc, cudatoolkit
 , devDonationLevel ? "0.0"
-, cudaSupport ? false  # doesn't work currently
+, cudaSupport ? false
+, openclSupport ? false
 }:
 
 stdenv.mkDerivation rec {
   name = "xmr-stak-${version}";
-  version = "2.0.0";
+  version = "2.1.0";
 
   src = fetchFromGitHub {
     owner = "fireice-uk";
     repo = "xmr-stak";
     rev = "v${version}";
-    sha256 = "1gsp5d2qmc8qwbfm87c2vnak6ks6y9csfjbsi0570pdciapaf8vs";
+    sha256 = "0ijhimsd03v1psj7pyj70z4rrgfvphpf69y7g72p06010xq1agp8";
   };
 
   NIX_CFLAGS_COMPILE = "-O3";
 
-  cmakeFlags = lib.optional (!cudaSupport) "-DCUDA_ENABLE=OFF";
+  cmakeFlags = lib.optional (!cudaSupport) "-DCUDA_ENABLE=OFF"
+    ++ lib.optional (!openclSupport) "-DOpenCL_ENABLE=OFF";
 
   nativeBuildInputs = [ cmake ];
-  buildInputs =
-    [ libmicrohttpd openssl opencl-headers ocl-icd hwloc ]
-    ++ lib.optional cudaSupport cudatoolkit;
+  buildInputs = [ libmicrohttpd openssl hwloc ]
+    ++ lib.optional cudaSupport cudatoolkit
+    ++ lib.optionals openclSupport [ opencl-headers ocl-icd ];
 
   postPatch = ''
     substituteInPlace xmrstak/donate-level.hpp \
