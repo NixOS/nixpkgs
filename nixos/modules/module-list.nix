dbc414a8a52acb71dcb7bcc4d0d36e9490433511
Maximilian Bosch <maximilian@mbosch.me>
2017-12-12 01:14:00 +0100

    yabar: add module
    To make the configuration of `yabar` more pleasant and easier to
    validate, a NixOS module will be quite helpful.
    
    An example config could look like this:
    
    ```
    {
      programs.yabar = {
        enable = true;
        bars.top.indicators.exec = "YA_DATE";
      };
    }
    ```
    
    The module adds a user-controlled systemd service which runs `yabar` after
    starting up X.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -110,6 +110,7 @@
   ./programs/wireshark.nix
   ./programs/xfs_quota.nix
   ./programs/xonsh.nix
+  ./programs/yabar.nix
   ./programs/zsh/oh-my-zsh.nix
   ./programs/zsh/zsh.nix
   ./programs/zsh/zsh-syntax-highlighting.nix
