fde83078469f404e71a00635df841c7c3679bb76
Ilya Kolpakov <ilya.kolpakov@gmail.com>
2017-12-14 15:13:51 +0100

    pythonPackages.inflection: use fetchpypi, run tests (0.3.1)

--- pkgs/development/python-modules/inflection/default.nix
+++ pkgs/development/python-modules/inflection/default.nix
@@ -1,21 +1,23 @@
-{ lib, fetchurl, buildPythonPackage, pytest } :
+{ lib, fetchPypi, buildPythonPackage, pytest } :
 
 buildPythonPackage rec {
-   version = "0.3.1";
-   name = "inflection-${version}";
+  pname = "inflection";
+  version = "0.3.1";
 
-   src = fetchurl {
-     url= "mirror://pypi/i/inflection/${name}.tar.gz";
-     sha256 = "1jhnxgnw8y3mbzjssixh6qkc7a3afc4fygajhqrqalnilyvpzshq";
-   };
+  src = fetchPypi {
+   inherit pname version;
+   sha256 = "1jhnxgnw8y3mbzjssixh6qkc7a3afc4fygajhqrqalnilyvpzshq";
+  };
 
-   propagatedBuildInputs = [ pytest ];
+  checkInputs = [ pytest ];
+  # Suppress overly verbose output if tests run successfully
+  checkPhase = ''pytest >/dev/null || pytest'';
 
-   meta = {
-     homepage = https://github.com/jpvanhal/inflection;
-     description = "A port of Ruby on Rails inflector to Python";
-     maintainers = with lib.maintainers; [ NikolaMandic ilya-kolpakov ];
-     license = lib.licenses.mit;
-   };
+  meta = {
+   homepage = https://github.com/jpvanhal/inflection;
+   description = "A port of Ruby on Rails inflector to Python";
+   maintainers = with lib.maintainers; [ NikolaMandic ilya-kolpakov ];
+   license = lib.licenses.mit;
+  };
 }
 
