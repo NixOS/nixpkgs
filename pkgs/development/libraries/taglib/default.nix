b2973d5ad441388b0f8454ed6ac5479bf9ee3519
Graham Christensen <graham@grahamc.com>
2017-12-14 07:32:00 -0500

    taglib: fixup meta

--- pkgs/development/libraries/taglib/default.nix
+++ pkgs/development/libraries/taglib/default.nix
@@ -17,8 +17,8 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     homepage = http://taglib.org/;
     repositories.git = git://github.com/taglib/taglib.git;
-    shortDescription = "A library for reading and editing audio file metadata.";
-    description = ''
+    description = "A library for reading and editing audio file metadata.";
+    longDescription = ''
       TagLib is a library for reading and editing the meta-data of several
       popular audio formats. Currently it supports both ID3v1 and ID3v2 for MP3
       files, Ogg Vorbis comments and ID3 tags and Vorbis comments in FLAC, MPC,
