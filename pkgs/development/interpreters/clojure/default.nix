56a33199e5e57b3adb15b33f0afc653f2ce13b8a
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-15 18:46:04 +0000

    clojure: build clojure-tools from source

--- pkgs/development/interpreters/clojure/default.nix
+++ pkgs/development/interpreters/clojure/default.nix
@@ -1,51 +1,42 @@
-{ stdenv, fetchurl, jdk, makeWrapper }:
+{ stdenv, fetchFromGitHub, fetchMaven, maven }:
 
-let version = "1.9.0.273"; in
-
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "clojure-${version}";
+  version = "1.9.0.273";
 
-  src = fetchurl {
-    url = "https://download.clojure.org/install/clojure-tools-${version}.tar.gz";
-    sha256 = "0xmrq3xvr002jgq8m1j0y5ld0rcr49608g3gqxgyxzjqswacglb4";
+  src = fetchFromGitHub {
+    owner = "clojure";
+    repo = "brew-install";
+    rev = version;
+    sha256 = "06cfr2jjwvqml19na5220dgyjy4dhkv27l0b2ck02fy5a796vh84";
   };
 
-  buildInputs = [ jdk makeWrapper ];
+  deps = fetchMaven {
+    inherit src;
+    sha256 = "0h80p8jl9w59hc0xrr6lswznh2hi8bjj8h99p2jpjnqfcwdyxrnd";
+  };
+
+  nativeBuildInputs = [ maven ];
+
+  buildPhase = "mvn -o -Dmaven.repo.local=${deps} package";
 
   installPhase = ''
-    pwd
-    ls -la
-    mkdir -p $out/libexec $out/bin
-    cp -f deps.edn example-deps.edn $out
-    cp -f clojure-tools-${version}.jar $out/libexec
-    sed -i -e "s@PREFIX@$out@g" clojure
-    cp -f clj clojure $out/bin
+    local prefix=$out/share/clojure
+    local source=target/classes
+
+    substituteInPlace $source/clojure --replace PREFIX $prefix
+    install -Dt $out/bin $source/{clj,clojure}
+
+    mkdir -p $prefix $prefix/libexec
+    cp target/clojure-tools-${version}.jar $prefix/libexec
+    cp $source/{,example-}deps.edn $prefix
   '';
 
   meta = with stdenv.lib; {
-    description = "A Lisp dialect for the JVM";
-    homepage = https://clojure.org/;
+    description = "The Clojure programming language";
+    homepage = https://clojure.org;
     license = licenses.epl10;
-    longDescription = ''
-      Clojure is a dynamic programming language that targets the Java
-      Virtual Machine. It is designed to be a general-purpose language,
-      combining the approachability and interactive development of a
-      scripting language with an efficient and robust infrastructure for
-      multithreaded programming. Clojure is a compiled language - it
-      compiles directly to JVM bytecode, yet remains completely
-      dynamic. Every feature supported by Clojure is supported at
-      runtime. Clojure provides easy access to the Java frameworks, with
-      optional type hints and type inference, to ensure that calls to Java
-      can avoid reflection.
-
-      Clojure is a dialect of Lisp, and shares with Lisp the code-as-data
-      philosophy and a powerful macro system. Clojure is predominantly a
-      functional programming language, and features a rich set of immutable,
-      persistent data structures. When mutable state is needed, Clojure
-      offers a software transactional memory system and reactive Agent
-      system that ensure clean, correct, multithreaded designs.
-    '';
-    maintainers = with maintainers; [ the-kenny ];
+    maintainers = with maintainers; [ the-kenny yegortimoshenko ];
     platforms = platforms.unix;
   };
 }
