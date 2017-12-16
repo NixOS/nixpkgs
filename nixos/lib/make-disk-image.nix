a7664a656f5c0a0d7335ab917c1c50544699f89f
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2017-12-17 00:26:51 +0200

    make-disk-image.nix: Really fix write permissions in the store
    I think the current one applies the -exec only to those that match
    '-type d'. Let's switch it to something that humans can understand...
    
    (cherry picked from commit 758b4c1ea4db81efcaf4cd8fb5ce04c1b42a991f)
    
    (Yes it should use 'find -print0 | xargs -0' but I'm really afraid of
    screwing up again in the same way. Nix doesn't allow spaces and/or
    newlines in store paths anyway and it has -maxdepth 1 -mindepth 1 so it
    won't fail in practice. If someone can provide a *tested* that doesn't
    suffer from the same problems, feel free to improve.)

--- nixos/lib/make-disk-image.nix
+++ nixos/lib/make-disk-image.nix
@@ -125,7 +125,7 @@ let
     fakeroot nixos-prepare-root $root ${channelSources} ${config.system.build.toplevel} closure
 
     # fakeroot seems to always give the owner write permissions, which we do not want
-    find $root/nix/store -mindepth 1 -maxdepth 1 -type f -o -type d -exec chmod -R a-w '{}' \;
+    find $root/nix/store -mindepth 1 -maxdepth 1 -type f -o -type d | xargs chmod -R a-w
 
     echo "copying staging root to image..."
     cptofs ${pkgs.lib.optionalString partitioned "-P 1"} -t ${fsType} -i $diskImage $root/* /
