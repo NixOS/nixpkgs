ecff1df7f2b84ccde9744c9deb2fbc0fde830fcf
Domen Kožar <domen@dev.si>
2017-12-17 15:59:05 +0000

    atom: depend on libsecret
    To support: $ atom -d
    
    (cherry picked from commit 75417d878164de6d936e38b052834cd5ba57b2c6)
    Signed-off-by: Domen Kožar <domen@dev.si>

--- pkgs/applications/editors/atom/env.nix
+++ pkgs/applications/editors/atom/env.nix
@@ -1,6 +1,6 @@
 { stdenv, lib, zlib, glib, alsaLib, dbus, gtk2, atk, pango, freetype, fontconfig
 , libgnome_keyring3, gdk_pixbuf, gvfs, cairo, cups, expat, libgpgerror, nspr
-, gconf, nss, xorg, libcap, systemd, libnotify
+, gconf, nss, xorg, libcap, systemd, libnotify, libsecret
 }:
 
 let
@@ -10,7 +10,7 @@ let
     xorg.libXrender xorg.libX11 xorg.libXext xorg.libXdamage xorg.libXtst
     xorg.libXcomposite xorg.libXi xorg.libXfixes xorg.libXrandr
     xorg.libXcursor xorg.libxkbfile xorg.libXScrnSaver libcap systemd libnotify
-    xorg.libxcb
+    xorg.libxcb libsecret
   ];
 
   libPathNative = lib.makeLibraryPath packages;
