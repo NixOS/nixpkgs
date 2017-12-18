2e2c2cad6c51588328b72f955d4ef1332623b786
Peter Hoeg <peter@hoeg.com>
2017-12-18 13:35:51 +0800

    uchiwa: 0.26.3 -> 1.1.0

--- pkgs/servers/monitoring/uchiwa/src.nix
+++ pkgs/servers/monitoring/uchiwa/src.nix
@@ -1,4 +1,4 @@
 {
-  version = "0.26.3-1";
-  sha256  = "13gklnrzhcj7s7j1ga5pa7ijwav0w9a96lqh4m7858mmm92v0bsk";
+    version = "1.1.0-1";
+    sha256  = "1ljrqz3ads5nn1alw4pa6lz7mbwhs09w498bpgg2hlcf8vklxg1y";
 }
