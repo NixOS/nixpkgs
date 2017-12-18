e198522137028c2f0fd723b5004a33fdc63ababe
Peter Hoeg <peter@hoeg.com>
2017-12-18 17:56:37 +0800

    scummvm: 1.9.0 -> 2.0.0

--- pkgs/games/scummvm/default.nix
+++ pkgs/games/scummvm/default.nix
@@ -1,19 +1,30 @@
-{ stdenv
-, fetchurl, SDL, zlib, libmpeg2, libmad, libogg, libvorbis, flac, alsaLib, mesa
+{ stdenv, nasm
+, fetchurl, SDL2, SDL2_net, freetype, zlib, libmpeg2, libjpeg, libmad, libogg, libvorbis, flac, alsaLib, mesa
 , hostPlatform
 }:
 
 stdenv.mkDerivation rec {
-  name = "scummvm-1.9.0";
+  name = "scummvm-${version}";
+  version = "2.0.0";
 
   src = fetchurl {
-    url = "http://scummvm.org/frs/scummvm/1.9.0/scummvm-1.9.0.tar.bz2";
-    sha256 = "813d7d8a76e3d05b45001d37451368711dadc32899ecf907df1cc7abfb1754d2";
+    url = "http://scummvm.org/frs/scummvm/${version}/${name}.tar.xz";
+    sha256 = "0q6aiw97wsrf8cjw9vjilzhqqsr2rw2lll99s8i5i9svan6l314p";
   };
-  
-  buildInputs = [ SDL zlib libmpeg2 libmad libogg libvorbis flac alsaLib mesa ];
 
-  hardeningDisable = [ "format" ];
+  nativeBuildInputs = [ nasm ];
+
+  buildInputs = [
+    SDL2 SDL2_net
+    freetype libjpeg libmpeg2 libmad libogg libvorbis flac alsaLib mesa zlib
+  ];
+
+  enableParallelBuilding = true;
+
+  configureFlags = [
+    "--enable-c++11"
+    "--enable-release"
+  ];
 
   crossAttrs = {
     preConfigure = ''
@@ -27,10 +38,11 @@ stdenv.mkDerivation rec {
     '';
   };
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Program to run certain classic graphical point-and-click adventure games (such as Monkey Island)";
     homepage = http://www.scummvm.org/;
-    platforms = stdenv.lib.platforms.linux;
+    license = licenses.gpl2;
+    maintainers = [ peterhoeg ];
+    platforms = platforms.linux;
   };
 }
-
