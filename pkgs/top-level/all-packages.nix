bf75e9b57a53a81f3e3b440d752cdfeb672c04d7
davidak <git@davidak.de>
2017-12-18 01:17:57 +0100

    elementary-gtk-theme: init at 5.1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18545,6 +18545,8 @@ with pkgs;
 
   deepin-gtk-theme = callPackage ../misc/themes/deepin { };
 
+  elementary-gtk-theme = callPackage ../misc/themes/elementary { };
+
   albatross = callPackage ../misc/themes/albatross { };
 
   gtk_engines = callPackage ../misc/themes/gtk2/gtk-engines { };
