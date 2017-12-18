95913d27689c506da5f3716394f1814ba66e9f50
Bas van Dijk <v.dijk.bas@gmail.com>
2017-12-18 19:07:46 +0100

    elk: 5.6.1 -> 5.6.5
    https://www.elastic.co/guide/en/elasticsearch/reference/5.6/release-notes-5.6.0.html
    https://www.elastic.co/guide/en/logstash/5.6/logstash-5-6-5.html
    https://www.elastic.co/guide/en/kibana/5.6/release-notes-5.6.5.html
    https://www.elastic.co/guide/en/beats/libbeat/5.6/release-notes-5.6.5.html

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1948,7 +1948,7 @@ with pkgs;
   evemu = callPackage ../tools/system/evemu { };
 
   # The latest version used by elasticsearch, logstash, kibana and the the beats from elastic.
-  elk5Version = "5.6.1";
+  elk5Version = "5.6.5";
 
   elasticsearch = callPackage ../servers/search/elasticsearch { };
   elasticsearch2 = callPackage ../servers/search/elasticsearch/2.x.nix { };
