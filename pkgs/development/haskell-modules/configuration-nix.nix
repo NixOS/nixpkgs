1c0c6d80e74a00169fd207d329f38832b75197a9
Peter Simons <simons@cryp.to>
2017-12-19 17:20:49 +0100

    haskell-configuration-nix: drop obsolete overrides

--- pkgs/development/haskell-modules/configuration-nix.nix
+++ pkgs/development/haskell-modules/configuration-nix.nix
@@ -501,9 +501,6 @@ self: super: builtins.intersectAttrs super {
   # Without this override, the builds lacks pkg-config.
   opencv-extra = addPkgconfigDepend super.opencv-extra (pkgs.opencv3.override { enableContrib = true; });
 
-  # Written against the 6.X series of megaparsec
-  htoml-megaparsec = super.htoml-megaparsec.override { megaparsec = self.megaparsec_6_3_0; };
-
   # Break cyclic reference that results in an infinite recursion.
   partial-semigroup = dontCheck super.partial-semigroup;
   colour = dontCheck super.colour;
