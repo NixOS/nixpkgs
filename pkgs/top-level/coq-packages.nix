962f440fd9b91f1717e8643e04ef885d0373983d
Vincent Laporte <Vincent.Laporte@gmail.com>
2017-12-19 14:26:11 +0000

    coqPackages.tlc: init at 20171206
    TLC is a general purpose Coq library that provides an alternative to Coq's
    standard library.
    
    Homepage: http://www.chargueraud.org/softs/tlc/

--- pkgs/top-level/coq-packages.nix
+++ pkgs/top-level/coq-packages.nix
@@ -33,6 +33,7 @@ let
       paco = callPackage ../development/coq-modules/paco {};
       QuickChick = callPackage ../development/coq-modules/QuickChick {};
       ssreflect = callPackage ../development/coq-modules/ssreflect { };
+      tlc = callPackage ../development/coq-modules/tlc {};
     };
 
   filterCoqPackages = coq:
