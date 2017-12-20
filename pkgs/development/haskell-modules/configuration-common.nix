e4bb2a784d2c384eaad9747d925c21b3ac2317e3
Peter Simons <simons@cryp.to>
2017-12-20 12:25:06 +0100

    liquidhaskell: drop obsolete override

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -874,9 +874,6 @@ self: super: {
   # https://github.com/danidiaz/tailfile-hinotify/issues/2
   tailfile-hinotify = dontCheck super.tailfile-hinotify;
 
-  # build liquidhaskell with the proper (new) aeson version
-  liquidhaskell = super.liquidhaskell.override { aeson = dontCheck self.aeson_1_2_3_0; };
-
   # Test suite fails: https://github.com/lymar/hastache/issues/46.
   # Don't install internal mkReadme tool.
   hastache = overrideCabal super.hastache (drv: {
