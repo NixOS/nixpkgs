cb7b220e1edf763cb8484117141c5736101f64f9
Eelco Dolstra <edolstra@gmail.com>
2017-12-20 16:35:22 +0100

    create-amis.sh: Ass eu-west-3
    (cherry picked from commit 014800706a6aac94f46b983a77e6bfec1231bb34)

--- nixos/maintainers/scripts/ec2/create-amis.sh
+++ nixos/maintainers/scripts/ec2/create-amis.sh
@@ -18,7 +18,7 @@ rm -f ec2-amis.nix
 
 types="hvm"
 stores="ebs"
-regions="eu-west-1 eu-west-2 eu-central-1 us-east-1 us-east-2 us-west-1 us-west-2 ca-central-1 ap-southeast-1 ap-southeast-2 ap-northeast-1 ap-northeast-2 sa-east-1 ap-south-1"
+regions="eu-west-1 eu-west-2 eu-west-3 eu-central-1 us-east-1 us-east-2 us-west-1 us-west-2 ca-central-1 ap-southeast-1 ap-southeast-2 ap-northeast-1 ap-northeast-2 sa-east-1 ap-south-1"
 
 for type in $types; do
     link=$stateDir/$type
