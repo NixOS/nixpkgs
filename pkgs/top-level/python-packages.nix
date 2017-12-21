2fd52664326b9c453795e1d59bcdebfc9664f6b4
Bas van Dijk <v.dijk.bas@gmail.com>
2017-12-21 22:09:41 +0100

    python.pkgs.elasticsearch-curator: init at 5.4.1
    (cherry picked from commit 85ee9df0e16935b742dfc6361f2461a31bc05ec7)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5151,6 +5151,8 @@ in {
     };
   });
 
+  elasticsearch-curator = callPackage ../development/python-modules/elasticsearch-curator { };
+
   entrypoints = callPackage ../development/python-modules/entrypoints { };
 
   enzyme = callPackage ../development/python-modules/enzyme {};
