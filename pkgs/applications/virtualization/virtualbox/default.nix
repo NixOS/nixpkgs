eb12741c7af5c2bffd9916ec6159297ba7d51aa2
Florian Klink <flokli@flokli.de>
2017-12-22 12:22:01 +0100

    virtualbox: add license

--- pkgs/applications/virtualization/virtualbox/default.nix
+++ pkgs/applications/virtualization/virtualbox/default.nix
@@ -206,6 +206,7 @@ in stdenv.mkDerivation {
 
   meta = {
     description = "PC emulator";
+    license = licenses.gpl2;
     homepage = http://www.virtualbox.org/;
     maintainers = with maintainers; [ flokli sander ];
     platforms = [ "x86_64-linux" "i686-linux" ];
