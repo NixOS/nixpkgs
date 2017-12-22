e4249c52bde81d6048c7e7c8e4bc1c0059748e95
Bert Moens <bert.moens@gmail.com>
2017-12-22 15:35:33 +0100

    rhvoice: init at 2017-09-24

---
+++ pkgs/applications/audio/rhvoice/default.nix
@@ -0,0 +1,62 @@
+{ stdenv, lib, pkgconfig, fetchFromGitHub, scons, python, glibmm, libao
+}:
+
+with lib;
+stdenv.mkDerivation rec {
+  name = "rhvoice";
+  #version = "2017-09-24";
+  version = "0.5";
+
+  src = fetchFromGitHub {
+    owner = "Olga-Yakovleva";
+    repo = "RHVoice";
+    rev = version;
+    sha256 = "1gk5z1v7g7xg1dvc2qfa0ljgma8s96njg2k70rsnr3s1wsrgvqk7";
+  };
+
+  nativeBuildInputs = [
+    scons pkgconfig
+  ];
+
+  buildInputs = [
+    python glibmm libao
+  ];
+
+  # SConstruct patches
+  # 1. Scons creates an independent environment that assumes standard POSIX paths.
+  #    The patch is needed to push the nix environment.
+  #    - PATH
+  #    - PKG_CONFIG_PATH, to find available (sound) libraries
+  #    - RPATH, to link to the newly built libraries
+  # 2. Some data is being zipped. Zip can't handle files from 1970.
+  #
+  postPatch = ''
+    substituteInPlace SConstruct --replace 'env=Environment(**env_args)' 'env=Environment(**env_args)
+        env.PrependENVPath("PATH", os.environ["PATH"])
+        env["ENV"]["PKG_CONFIG_PATH"]=os.environ["PKG_CONFIG_PATH"]
+            '
+    substituteInPlace SConstruct --replace 'else:
+            env["BUILDDIR"]=BUILDDIR
+    ' 'else:
+            env["BUILDDIR"]=BUILDDIR
+            env["RPATH"]="'$out'/lib"
+    '
+    find "./" '!' -newermt '1980-01-01' -exec touch -d '1980-01-02' '{}' '+'
+  '';
+
+  buildPhase = ''
+    scons prefix=$out
+  '';
+
+  installPhase = ''
+    scons install
+  ''; 
+
+  meta = {
+    description = "A free and open source speech synthesizer for Russian language and others";
+    homepage = https://github.com/Olga-Yakovleva/RHVoice/wiki;
+    license = licenses.lgpl3;
+    maintainers = with maintainers; [ berce ];
+    platforms = with platforms; all;
+  };
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15182,6 +15182,8 @@ with pkgs;
 
   quvi_scripts = callPackage ../applications/video/quvi/scripts.nix { };
 
+  rhvoice = callPackage ../applications/audio/rhvoice { };
+
   svox = callPackage ../applications/audio/svox { };
 
   gkrellm = callPackage ../applications/misc/gkrellm {
