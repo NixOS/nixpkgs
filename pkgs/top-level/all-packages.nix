f97c4668abb630875318f057d452f8ebfe52f8e2
Bert Moens <bert.moens@gmail.com>
2017-12-22 15:35:33 +0100

    rhvoice: init at unstable-2018-02-10

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15836,6 +15836,8 @@ with pkgs;
 
   quvi_scripts = callPackage ../applications/video/quvi/scripts.nix { };
 
+  rhvoice = callPackage ../applications/audio/rhvoice { };
+
   svox = callPackage ../applications/audio/svox { };
 
   gkrellm = callPackage ../applications/misc/gkrellm {
