f6526819a03d99854dfed3bddeb28176c81a5859
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-24 13:29:41 +0000

    tests/build-test: rename to eval-test

--- tests/eval-test.sh
+++ tests/eval-test.sh
@@ -4,5 +4,5 @@ cd $(dirname $0)
 
 for profile in $(find .. -name \*.nix); do
     echo $profile >&2
-    nixos-rebuild -I nixos-config=build-test.nix -I nixos-hardware-profile=$profile dry-build
+    nixos-rebuild -I nixos-config=eval-test.nix -I nixos-hardware-profile=$profile dry-build
 done
