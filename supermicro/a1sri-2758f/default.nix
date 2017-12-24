95c8873c41ef57d820b027e16db0709c050579e1
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-24 12:35:46 +0000

    supermicro/a1sri-2758f: fix evaluation, drop old kernel notes

--- supermicro/a1sri-2758f/default.nix
+++ supermicro/a1sri-2758f/default.nix
@@ -1,20 +1,9 @@
 # http://www.supermicro.com/products/motherboard/Atom/X10/A1SRi-2758F.cfm
-#
 # This board contains a TPM header, but you must supply your own module.
-#
 
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
-  imports = [ ../lib/kernel-version.nix ];
-
-  environment.systemPackages = [ pkgs.ipmitool ];
   boot.kernelModules = [ "ipmi_devintf" "ipmi_si" ];
-
-  kernelAtleast = lib.singleton
-    { version = "4.4";
-      msg =
-        "ethernet driver may be buggy on older kernels, "+
-        ''try 'networking.dhcpcd.extraConfig = "nolink";' if you encounter loss of link problems'';
-    };
+  environment.systemPackages = [ pkgs.ipmitool ];
 }
