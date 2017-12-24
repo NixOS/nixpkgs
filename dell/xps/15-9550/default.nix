0344137b57a5429a8ac49557be38a806ed30ba3b
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-24 12:30:41 +0000

    dell/xps/15-9550: fix evaluation, drop old kernel notes

--- dell/xps/15-9550/default.nix
+++ dell/xps/15-9550/default.nix
@@ -1,25 +1,11 @@
-{ config, pkgs, ... }:
+{ lib, ... }:
 
 {
-  imports =
-    [ ../lib/kernel-version.nix
-    ];
+  boot.loader.systemd-boot.enable = lib.mkDefault true;
+  boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
 
-  # Use the systemd-boot efi boot loader. (From default generated configuration.nix)
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Honestly forget if needed or just better for touchpad.
-  # Synaptics also works, but doesn't have working palm and thumb detection.
-  services.xserver.libinput.enable = true;
-
-  # Intel Graphics confirmed not working at 4.1, confirmed working at {4.3, 4.4}
-  kernelAtleast =
-    [ { version = "4.2"; msg = "Intel Graphics confirmed not to work."; }
-      { version = "4.3"; msg = "Intel Graphics untested."; }
-      { version = "4.4"; msg = "Touchpad does not work, though the touchscreen still does"; }
-    ];
-
-  # To just use intel integrated graphics with Intel's open source driver
+  # To just use Intel integrated graphics with Intel's open source driver
   # hardware.nvidiaOptimus.disable = true;
+
+  services.xserver.libinput.enable = lib.mkDefault true;
 }
