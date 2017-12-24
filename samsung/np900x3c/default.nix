f62ead1ce0874c3773fb8d762e5de3b336e8c4ac
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-24 12:34:24 +0000

    samsung/np900x3c: fix evaluation, drop old kernel notes

--- samsung/np900x3c/default.nix
+++ samsung/np900x3c/default.nix
@@ -1,13 +1,5 @@
 { lib, ... }:
-with lib;
+
 {
-  imports = [ ../lib/kernel-version.nix ];
-
-  services.xserver.synaptics.enable = true;
-
-  kernelAtleast = singleton
-    { version = "3.9";
-      msg = "Runtime system freezes can be expected on Linux kernels prior to 3.9, probably  because of bugs in intel video drivers.";
-    };
-
+  services.xserver.synaptics.enable = lib.mkDefault true;
 }
