a8b7b6ffe5813545e289404e504a21e2222f7ab5
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-24 12:13:25 +0000

    apple/macbook-pro/10-1: fix evaluation, drop old kernel notes

--- apple/macbook-pro/10-1/default.nix
+++ apple/macbook-pro/10-1/default.nix
@@ -1,27 +1,16 @@
-{ config, pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
-  imports =
-    [ ../lib/kernel-version.nix
-    ];
+  imports = [ ../. ];
 
-  # Use the systemd-boot efi boot loader. (From default generated configuration.nix)
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # I use this but not sure whether its needed.
-  # Right click does *NOT* work
-  services.xserver.libinput.enable = true;
-
-  kernelAtleast =
-    [ { version = "4.7"; msg = "Broadcom WiFi confirmed not to work."; }
-    ];
-
-  # Couldn't get X to work with nvidia
-  # Also, PTYs don't work after X/nvidia starts
-  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.driSupport32Bit = true;
 
-  # Seems to improve battery life *and* keep the CPU cooler
-  services.mbpfan.enable = true;
+  services.xserver = {
+    libinput.enable = lib.mkDefault true;
+    # TODO: we should not enable unfree drivers
+    videoDrivers = [ "nvidia" ];
+  };
 }
