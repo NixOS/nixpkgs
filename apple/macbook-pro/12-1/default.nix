cbe052c49e80ccb27d2573e9dc9e7bbc6df4a0a4
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-24 12:16:10 +0000

    apple/macbook-pro/12-1: clean up

--- apple/macbook-pro/12-1/default.nix
+++ apple/macbook-pro/12-1/default.nix
@@ -1,17 +1,12 @@
-{ config, lib, pkgs, ... }:
+{ lib, ... }:
+
 {
-  imports =
-    [ <nixpkgs/nixos/modules/hardware/network/broadcom-43xx.nix>
-      <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
-    ];
+  imports = [
+    <nixpkgs/nixos/modules/hardware/network/broadcom-43xx.nix>
+  ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
-
-  # The USB subsystem wakes up the MBP directly after suspend if
-  # we don't disable it.
-  services.udev.extraRules = ''
+  # USB subsystem wakes up MBP right after suspend unless we disable it.
+  services.udev.extraRules = lib.mkDefault ''
     SUBSYSTEM=="pci", KERNEL=="0000:00:14.0", ATTR{power/wakeup}="disabled"
   '';
 }
