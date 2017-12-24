097d47e3f80f3a24462669a05696065128773bdb
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-24 11:50:06 +0000

    c720: drop kernel notes for unsupported kernel versions

--- acer/c720/default.nix
+++ acer/c720/default.nix
@@ -1,10 +1,1 @@
-{ lib, ... }:
-with lib;
-{
-  imports = singleton ../lib/kernerl-version.nix;
-
-  kernelAtleast = singleton
-    { version = "3.18";
-      msg = "Cypress APA touchpad supported added in Linux-3.17-rc1";
-    };
-}
+{}
