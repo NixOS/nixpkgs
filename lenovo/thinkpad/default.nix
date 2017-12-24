2951a4f4b0b33ae266d0cd98f693023c6ed99577
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-24 11:17:39 +0000

    thinkpad: move fprintd comment from X220 to parent profile

--- lenovo/thinkpad/default.nix
+++ lenovo/thinkpad/default.nix
@@ -9,4 +9,7 @@
   hardware.trackpoint.enable = lib.mkDefault true;
   services.tlp.enable = lib.mkDefault true;
   services.xserver.libinput.enable = lib.mkDefault true;
+
+  # Fingerprint reader: login and unlock with fingerprint (if you add one with `fprintd-enroll`)
+  # services.fprintd.enable = true;
 }
--- lenovo/thinkpad/x220/default.nix
+++ lenovo/thinkpad/x220/default.nix
@@ -5,12 +5,5 @@
 
   # hard disk protection if the laptop falls
   services.hdapsd.enable = lib.mkDefault true;
-
-  # fingerprint reader: login and unlock with fingerprint (if you add one with `fprintd-enroll`)
-  #services.fprintd.enable = true;
-  #security.pam.services.login.fprintAuth = true;
-  #security.pam.services.xscreensaver.fprintAuth = true;
-  # similarly for other PAM providers
-
   services.xserver.videoDrivers = [ "intel" ];
 }
