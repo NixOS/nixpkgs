554629a957f703f4d9382f502c86288a56f3a052
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-25 20:29:10 +0000

    thinkpad: do not enable thinkfan by default

--- lenovo/thinkpad/default.nix
+++ lenovo/thinkpad/default.nix
@@ -4,7 +4,6 @@
   imports = [ ../../common/pc/laptop ];
 
   hardware.trackpoint.enable = lib.mkDefault true;
-  services.thinkfan.enable = lib.mkDefault true;
 
   # Fingerprint reader: login and unlock with fingerprint (if you add one with `fprintd-enroll`)
   # services.fprintd.enable = true;
--- lenovo/thinkpad/x250/default.nix
+++ lenovo/thinkpad/x250/default.nix
@@ -1,13 +1,6 @@
-{ config, lib, pkgs, ... }:
-
 {
   imports = [
     ../.
     ../../../common/cpu/intel
   ];
-
-  # Thinkfan is enabled by thinkpad profile.
-  # However it requires to set a different `sensor` for this hardware, since there is no /proc/acpi/ibm/thermal
-  # Regulation also works fine without thinkfan daemon though.
-  services.thinkfan.enable = lib.mkOverride 900 false;
 }
