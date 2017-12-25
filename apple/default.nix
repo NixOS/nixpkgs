e228c7827b5ac2b08f07433aa8fbc8f2c41e85a5
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-25 04:30:32 +0000

    apple: add hid_apple.iso_layout=0 kernel param
    In my experience, this makes keyboard behavior match letters printed
    on it both on EU and US keyboards.

--- apple/default.nix
+++ apple/default.nix
@@ -1,6 +1,10 @@
 { config, lib, ... }:
 
 {
+  boot.kernelParams = [
+    "hid_apple.iso_layout=0"
+  ];
+
   hardware.facetimehd.enable = lib.mkDefault
     (config.nixpkgs.config.allowUnfree or false);
 
