6e6d37f54d2a081ffb63c3b6568d2093d64d6395
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2017-12-25 17:15:49 -0500

    Libsystem: generate symbol lists as intersection of the lists for the supported macOS versions

---
+++ pkgs/os-specific/darwin/apple-source-releases/Libsystem/symbols/gen_common_symbols.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -x
+
+productVersions=( 10.10.5 10.11.6 10.12.6 10.13.2 )
+lists=( system_c_symbols_i386 system_c_symbols_x86_64 system_kernel_symbols_i386 system_kernel_symbols_x86_64 )
+first="${productVersions[0]}"
+unset -v 'productVersions[0]'
+for l in "${lists[@]}"; do
+	cp "$first"/"$l" current
+	for v in "${productVersions[@]}"; do
+		comm -12 current "$v"/"$l" > current_new
+		mv current_new current
+	done
+	mv current "$l"
+done
--- pkgs/os-specific/darwin/apple-source-releases/Libsystem/symbols/gen_symbol_lists.sh
+++ pkgs/os-specific/darwin/apple-source-releases/Libsystem/symbols/gen_symbol_lists.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+set -x
+set -e
+
+productVersion=`sw_vers -productVersion`
+
+mkdir "$productVersion" || true
+pushd "$productVersion" 1>/dev/null 2>/dev/null
+
 nm -jgU -arch x86_64 /usr/lib/system/libsystem_c.dylib > system_c_symbols_x86_64
 nm -jgU -arch i386 /usr/lib/system/libsystem_c.dylib > system_c_symbols_i386
 nm -jgU -arch x86_64 /usr/lib/system/libsystem_kernel.dylib > system_kernel_symbols_x86_64
 nm -jgU -arch i386 /usr/lib/system/libsystem_kernel.dylib > system_kernel_symbols_i386
+
+popd 1>/dev/null 2>/dev/null
