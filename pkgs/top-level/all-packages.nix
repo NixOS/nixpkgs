4102078edd6aff92502a7be6728d19bf37f68cda
Ivan Jager <aij+git@mrph.org>
2017-12-24 20:16:39 -0600

    opensm: init at 3.3.20

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3960,6 +3960,8 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Carbon;
   };
 
+  opensm = callPackage ../tools/networking/opensm { };
+
   openssh =
     callPackage ../tools/networking/openssh {
       hpnSupport = false;
