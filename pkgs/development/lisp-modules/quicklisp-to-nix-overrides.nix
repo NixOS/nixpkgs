a11eb64250d051096f4598663ceaa30c8c063cce
Robin Gloster <mail@glob.in>
2017-12-25 16:57:13 +0100

    mariadb: replace to connector-c

--- pkgs/development/lisp-modules/quicklisp-to-nix-overrides.nix
+++ pkgs/development/lisp-modules/quicklisp-to-nix-overrides.nix
@@ -56,8 +56,8 @@ in
     propagatedBuildInputs = with pkgs; [mysql postgresql sqlite zlib];
     overrides = y: (x.overrides y) // {
       preConfigure = ((x.overrides y).preConfigure or "") + ''
-        export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${pkgs.lib.getDev pkgs.mysql.client}/include/mysql"
-        export NIX_LDFLAGS="$NIX_LDFLAGS -L${pkgs.lib.getLib pkgs.mysql.client}/lib/mysql"
+        export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${pkgs.mysql.connector-c}/include/mariadb"
+        export NIX_LDFLAGS="$NIX_LDFLAGS -L${pkgs.mysql.connector-c}/lib/mariadb"
       '';
     };
   };
