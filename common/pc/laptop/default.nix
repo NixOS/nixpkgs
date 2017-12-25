f9a1ac62972dc755c70a8cf2c8b28989d6832369
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-25 04:46:15 +0000

    common/pc/laptop: disable cpuFreqGovernor

--- common/pc/laptop/default.nix
+++ common/pc/laptop/default.nix
@@ -7,5 +7,10 @@
     "vm.laptop_mode" = lib.mkDefault 5;
   };
 
+  # TODO: fix in NixOS/nixpkgs
+  # Disable governor set in hardware-configuration.nix,
+  # required when services.tlp.enable is true:
+  powerManagement.cpuFreqGovernor = lib.mkForce null;
+
   services.tlp.enable = lib.mkDefault true;
 }
