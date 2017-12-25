2227f2c67a3c3b7b31c9133acf5ddfdf2cd60ba5
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-25 04:49:53 +0000

    apple/macbook-air/4: resolve tearing

--- apple/macbook-air/4/default.nix
+++ apple/macbook-air/4/default.nix
@@ -1,7 +1,13 @@
+{ lib, ... }:
+
 {
   imports = [ ../. ];
 
   boot.kernelParams = [
     "acpi_backlight=vendor"
   ];
+
+  services.xserver.deviceSection = lib.mkDefault ''
+    Option "TearFree" "true"
+  '';
 }
