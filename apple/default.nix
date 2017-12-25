5a1a527f1600d5132d255423cd2238e33c32ff5b
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-25 01:28:02 +0000

    apple: enable facetimehd if allowUnfree = true

--- apple/default.nix
+++ apple/default.nix
@@ -1,5 +1,6 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 {
+  hardware.facetimehd.enable = lib.mkDefault (config.nixpkgs.config.allowUnfree or false);
   services.mbpfan.enable = lib.mkDefault true;
 }
