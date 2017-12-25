571bea15a4fbc8a89990a3dfe0156c21713505ad
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-25 14:39:59 +0000

    CODEWONERS: init

--- lenovo/thinkpad/x250/default.nix
+++ lenovo/thinkpad/x250/default.nix
@@ -10,6 +10,4 @@
   # However it requires to set a different `sensor` for this hardware, since there is no /proc/acpi/ibm/thermal
   # Regulation also works fine without thinkfan daemon though.
   services.thinkfan.enable = lib.mkOverride 900 false;
-
-  # maintainers: Mic92
 }
