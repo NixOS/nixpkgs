4271a210b0ac9fbbea5382ddee1cfa835f3e35b9
Tuomas Tynkkynen <tuomas@tuxera.com>
2017-12-26 19:07:58 +0200

    zathura: No need for manual rst2man path anymore

--- pkgs/applications/misc/zathura/core/default.nix
+++ pkgs/applications/misc/zathura/core/default.nix
@@ -29,7 +29,6 @@ stdenv.mkDerivation rec {
 
   makeFlags = [
     "PREFIX=$(out)"
-    "RSTTOMAN=${docutils}/bin/rst2man.py"
     "VERBOSE=1"
     "TPUT=${ncurses.out}/bin/tput"
     (optionalString synctexSupport "WITH_SYNCTEX=1")
