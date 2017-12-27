091c2b9f04189301f18ff0bc8336541d137f1d61
Daiderd Jordan <daiderd@gmail.com>
2017-12-27 21:26:36 +0100

    cacert: cleanup exporting SSL_CERT_FILE

--- pkgs/build-support/fetchgx/default.nix
+++ pkgs/build-support/fetchgx/default.nix
@@ -6,15 +6,13 @@ stdenv.mkDerivation {
   name = "${name}-gxdeps";
   inherit src;
 
-  buildInputs = [ go gx gx-go ];
+  buildInputs = [ cacert go gx gx-go ];
 
   outputHashAlgo = "sha256";
   outputHashMode = "recursive";
   outputHash = sha256;
 
   phases = [ "unpackPhase" "buildPhase" "installPhase" ];
-
-  NIX_SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
 
   buildPhase = ''
     export GOPATH=$(pwd)/vendor
