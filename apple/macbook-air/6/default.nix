b65efccd54525139df7d6a551e921c5c1992a27f
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-12-27 22:09:57 +0000

    apple/macbook-air/6: add acpi_osi= kernel param

--- apple/macbook-air/6/default.nix
+++ apple/macbook-air/6/default.nix
@@ -6,6 +6,9 @@
   boot = {
     extraModulePackages = with config.boot.kernelPackages; [ mba6x_bl ];
     kernelModules = [ "mba6x_bl" ];
+
+    # Divides power consumption by two.
+    kernelParams = [ "acpi_osi=" ];
   };
 
   services.xserver.deviceSection = lib.mkDefault ''
