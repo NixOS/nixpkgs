89f90180d33584f19d13e10d60911217bc250bfa
John Wiegley <johnw@newartisans.com>
2017-12-28 11:42:58 -0800

    transfig: 3.2.4 -> 3.2.6a; executable is now named "xfig"

--- pkgs/tools/graphics/transfig/default.nix
+++ pkgs/tools/graphics/transfig/default.nix
@@ -1,61 +1,16 @@
-{stdenv, fetchurl, zlib, libjpeg, libpng, imake}:
+{ stdenv, fetchurl, zlib, libjpeg, libpng, xorg, Xaw3d }:
 
 stdenv.mkDerivation rec {
-  name = "transfig-3.2.4";
+  name = "transfig-3.2.6a";
   src = fetchurl {
-    url = ftp://ftp.tex.ac.uk/pub/archive/graphics/transfig/transfig.3.2.4.tar.gz;
-    sha256 = "0429snhp5acbz61pvblwlrwv8nxr6gf12p37f9xxwrkqv4ir7dd4";
+    url = https://sourceforge.net/projects/mcj/files/xfig-3.2.6a.tar.xz;
+    sha256 = "0z1636w27hvgjpq98z40k8h535b4x2xr2whkvr7bibaa89fynym8";
   };
 
-  buildInputs = [zlib libjpeg libpng imake];
-
-  patches = [
-    ./patch-fig2dev-dev-Imakefile
-    ./patch-fig2dev-Imakefile
-    ./patch-transfig-Imakefile
-    ./patch-fig2dev-fig2dev.h
-    ./patch-fig2dev-dev-gensvg.c
-  ];
-
-  patchPhase = ''
-    runHook prePatch
-
-    configureImakefiles() {
-        local sedcmd=$1
-
-        sed "$sedcmd" fig2dev/Imakefile > tmpsed
-        cp tmpsed fig2dev/Imakefile
-
-        sed "$sedcmd" fig2dev/dev/Imakefile > tmpsed
-        cp tmpsed fig2dev/dev/Imakefile
-
-        sed "$sedcmd" transfig/Imakefile > tmpsed
-        cp tmpsed transfig/Imakefile
-    }
-
-    for i in $patches; do
-        header "applying patch $i" 3
-        patch -p0 < $i
-        stopNest
-    done
-
-    configureImakefiles "s:__PREFIX_PNG:${libpng}:"
-    configureImakefiles "s:__PREFIX:$out:"
-
-    runHook postPatch
-  '';
-
-  preBuild = ''
-    xmkmf
-    make Makefiles
-  '';
-
-  makeFlags = [ "CC=cc" ];
-
-  preInstall = ''
-    mkdir -p $out
-    mkdir -p $out/lib
-  '';
+  buildInputs = with xorg;
+    [ zlib libjpeg libpng Xaw3d
+      libX11 libXau libXaw libXext libXft libXmu libXpm libXt
+];
 
   hardeningDisable = [ "format" ];
 
