50a6403b094355bb43d2347dd0c7ea08ee561bb2
hyphon81 <zero812n@gmail.com>
2017-12-28 17:39:11 +0100

    maintainers: Add hyphon81

--- lib/maintainers.nix
+++ lib/maintainers.nix
@@ -282,6 +282,7 @@
   hodapp = "Chris Hodapp <hodapp87@gmail.com>";
   hrdinka = "Christoph Hrdinka <c.nix@hrdinka.at>";
   htr = "Hugo Tavares Reis <hugo@linux.com>";
+  hyphon81 = "Masato Yonekawa <zero812n@gmail.com>";
   iand675 = "Ian Duncan <ian@iankduncan.com>";
   ianwookim = "Ian-Woo Kim <ianwookim@gmail.com>";
   iblech = "Ingo Blechschmidt <iblech@speicherleck.de>";
