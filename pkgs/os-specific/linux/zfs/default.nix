6b74d2ca07dea6923d3543799b57e30c8f318cf8
Franz Pletz <fpletz@fnordicwalking.de>
2017-12-29 22:46:34 +0100

    linuxPackages.zfs: use correct kernel module build flag
    In this expression the boolean flags `buildUser` and `buildKernel` determine
    if either userspace tools or the kernel module is being built.
    
    cc #33166

--- pkgs/os-specific/linux/zfs/default.nix
+++ pkgs/os-specific/linux/zfs/default.nix
@@ -39,7 +39,7 @@ let
       patches = extraPatches;
 
       nativeBuildInputs = [ autoreconfHook nukeReferences ]
-         ++ optional (kernel != null) kernel.moduleBuildDependencies;
+         ++ optional buildUser kernel.moduleBuildDependencies;
       buildInputs =
            optionals buildKernel [ spl ]
         ++ optionals buildUser [ zlib libuuid python attr ]
