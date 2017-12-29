b7fdefc8a4182956ec114eeeaae029418a310c4d
Markus Kowalewski <markus.kowalewski@gmail.com>
2017-12-28 17:51:42 -0800

    beegfs: init at 6.17
    package, kernel module, nixos module, and nixos test

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -404,6 +404,7 @@
   ./services/monitoring/vnstat.nix
   ./services/monitoring/zabbix-agent.nix
   ./services/monitoring/zabbix-server.nix
+  ./services/network-filesystems/beegfs.nix
   ./services/network-filesystems/cachefilesd.nix
   ./services/network-filesystems/davfs2.nix
   ./services/network-filesystems/drbd.nix
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1338,6 +1338,8 @@ with pkgs;
 
   beanstalkd = callPackage ../servers/beanstalkd { };
 
+  beegfs = callPackage ../os-specific/linux/beegfs { };
+
   beets = callPackage ../tools/audio/beets {
     pythonPackages = python2Packages;
   };
@@ -12820,6 +12822,8 @@ with pkgs;
     bcc = callPackage ../os-specific/linux/bcc { };
 
     bbswitch = callPackage ../os-specific/linux/bbswitch {};
+
+    beegfs-module = callPackage ../os-specific/linux/beegfs/kernel-module.nix { };
 
     ati_drivers_x11 = callPackage ../os-specific/linux/ati-drivers { };
 
