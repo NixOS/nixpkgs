495bf9b9c191538e51330dcc51d3f92c93f9ea77
Frederik Rietdijk <fridh@fridh.nl>
2017-12-30 12:23:20 +0100

    python: grpcio: 1.7.3 -> 1.8.2

--- pkgs/development/python-modules/grpcio/default.nix
+++ pkgs/development/python-modules/grpcio/default.nix
@@ -3,11 +3,11 @@
 
 buildPythonPackage rec {
   pname = "grpcio";
-  version = "1.7.3";
+  version = "1.8.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1wkrxj1jmf2dyx207fc9ysyns9h27gls3drgg05mzdckjqr5lnl6";
+    sha256 = "1ea1336f0d1158c4e00e96a94df84b75f6bbff9816abb6cc68cbdc9442a9ac55";
   };
 
   propagatedBuildInputs = [ six protobuf ]
