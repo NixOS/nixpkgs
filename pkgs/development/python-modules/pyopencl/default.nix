0bed3b1deeef57705219bbf7ecadc74ef64bdf85
Frederik Rietdijk <fridh@fridh.nl>
2017-12-31 11:31:47 +0100

    python.pkgs.pyopencl: fix expression

--- pkgs/development/python-modules/pyopencl/default.nix
+++ pkgs/development/python-modules/pyopencl/default.nix
@@ -16,9 +16,9 @@
 buildPythonPackage rec {
   pname = "pyopencl";
   version = "2017.2.2";
-  name = "${pname}-${version}";
 
-  buildInputs = [ pytest opencl-headers ocl-icd ];
+  checkInputs = [ pytest ];
+  buildInputs = [ opencl-headers ocl-icd ];
 
   propagatedBuildInputs = [ numpy cffi pytools decorator appdirs six Mako ];
 
@@ -26,6 +26,11 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "d2f7b04d2e819c6e90d6366b7712a7452a39fba218e51b11b02c85ab07fd2983";
   };
+
+  # py.test is not needed during runtime, so remove it from `install_requires`
+  postPatch = ''
+    substituteInPlace setup.py --replace "pytest>=2" ""
+  '';
 
   # gcc: error: pygpu_language_opencl.cpp: No such file or directory
   doCheck = false;
