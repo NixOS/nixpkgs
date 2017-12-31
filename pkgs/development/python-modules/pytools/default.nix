d33334c02ec1ba3aeb7be02e243124533a0af4a2
Frederik Rietdijk <fridh@fridh.nl>
2017-12-31 11:25:36 +0100

    python.pkgs.pytools: fix expression
    (cherry picked from commit 718356d6987508658ee2f3224abdc075ec6a0014)
    
    python.pkgs.pytools: 2017.4 -> 2017.6
    
    (cherry picked from commit bed35cf5216b277f32f1946d5e4fee424ce98b9c)
    
    pytools fixup

---
+++ pkgs/development/python-modules/pytools/default.nix
@@ -0,0 +1,40 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, decorator
+, appdirs
+, six
+, numpy
+, pytest
+}:
+
+buildPythonPackage rec {
+  pname = "pytools";
+  version = "2017.6";
+  name = "${pname}-${version}";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "80f1bba4469d473c1b3969bc8e188c03bcc94d35807a889ceebbfc78e3208115";
+  };
+
+  checkInputs = [ pytest ];
+
+  propagatedBuildInputs = [
+    decorator
+    appdirs
+    six
+    numpy
+  ];
+
+  checkPhase = ''
+    py.test -k 'not test_persistent_dict'
+  '';
+
+  meta = {
+    homepage = https://github.com/inducer/pytools/;
+    description = "Miscellaneous Python lifesavers.";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ artuuge ];
+  };
+}
