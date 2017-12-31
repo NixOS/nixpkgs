6c86adff20c9c29db24aec4757001cae9fb6f857
Frederik Rietdijk <fridh@fridh.nl>
2017-12-31 11:11:32 +0100

    python.pkgs.pyglet: fix build

--- pkgs/development/python-modules/pyglet/default.nix
+++ pkgs/development/python-modules/pyglet/default.nix
@@ -1,22 +1,23 @@
 { stdenv, buildPythonPackage, fetchPypi
-, mesa, xorg, freetype, fontconfig}:
+, mesa, xorg, freetype, fontconfig, future}:
 
 buildPythonPackage rec {
   version = "1.3.0";
   pname = "pyglet";
-  name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "640a8f8e3d7bf8dbb551fa707f14021f619932990ab1401c48ba9dbcc6c2242c";
   };
 
-  patchPhase = let
+  postPatch = let
     libs = [ mesa xorg.libX11 freetype fontconfig ];
     paths = builtins.concatStringsSep "," (map (l: "\"${l}/lib\"") libs);
   in "sed -i -e 's|directories\.extend.*lib[^]]*|&,${paths}|' pyglet/lib.py";
 
   doCheck = false;
+
+  propagatedBuildInputs = [ future ];
 
   meta = with stdenv.lib; {
     homepage = "http://www.pyglet.org/";
