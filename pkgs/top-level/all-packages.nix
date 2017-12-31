a1a341cb62a770ef6d13c4ec4d987dc269593dfb
Andreas Rammhold <andreas@rammhold.de>
2017-12-31 15:47:36 +0100

    template-glib: init at 3.28.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15061,6 +15061,8 @@ with pkgs;
 
   theano = callPackage ../data/fonts/theano { };
 
+  template-glib = callPackage ../development/libraries/template-glib { };
+
   tempora_lgc = callPackage ../data/fonts/tempora-lgc { };
 
   terminus_font = callPackage ../data/fonts/terminus-font { };
