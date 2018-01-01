00891d44121b6d750225541a4e12b6d10ec60569
Frederik Rietdijk <fridh@fridh.nl>
2018-01-01 10:40:02 +0100

    python: ipywidgets: 7.0.0 -> 7.0.5

--- pkgs/development/python-modules/ipywidgets/default.nix
+++ pkgs/development/python-modules/ipywidgets/default.nix
@@ -14,12 +14,12 @@
 
 buildPythonPackage rec {
   pname = "ipywidgets";
-  version = "7.0.0";
+  version = "7.0.5";
   name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "63e454202f72796044e99846881c33767c47fa050735dc1f927657b9cd2b7fcd";
+    sha256 = "321be3dc48193130ba16e8080172bb5cd052eb65e3ad0ea7b5f80ff73e24bc54";
   };
 
   # Tests are not distributed
