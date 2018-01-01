ebc5ea84d0d8bbcbb57c49a2e59e6b5320e0013a
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-01-01 10:53:57 -0500

    vbindiff: Updates homepage URL.
    This commit automatically fixes the following problem:
    
    > Homepage link "http://www.cjmweb.net/vbindiff/" is a permanent redirect to
    > "https://www.cjmweb.net/vbindiff/" and should be updated
    
    (Commit automatically made using repology-autofixer)

--- pkgs/applications/editors/vbindiff/default.nix
+++ pkgs/applications/editors/vbindiff/default.nix
@@ -7,13 +7,13 @@ stdenv.mkDerivation rec {
   buildInputs = [ ncurses ];
 
   src = fetchurl {
-    url = "http://www.cjmweb.net/vbindiff/${name}.tar.gz";
+    url = "https://www.cjmweb.net/vbindiff/${name}.tar.gz";
     sha256 = "0gcqy4ggp60qc6blq1q1gc90xmhip1m6yvvli4hdqlz9zn3mlpbx";
   };
 
   meta = {
     description = "A terminal visual binary diff viewer";
-    homepage = http://www.cjmweb.net/vbindiff/;
+    homepage = https://www.cjmweb.net/vbindiff/;
     license = stdenv.lib.licenses.gpl2Plus;
     platforms = stdenv.lib.platforms.unix;
   };
