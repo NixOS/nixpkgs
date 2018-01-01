b998bfa8ba92a049a3ac911fb048c230f9c23991
Frederik Rietdijk <fridh@fridh.nl>
2018-01-01 17:00:21 +0100

    python: brotlipy: 0.6.0 -> 0.7.0

--- pkgs/development/python-modules/brotlipy/default.nix
+++ pkgs/development/python-modules/brotlipy/default.nix
@@ -4,18 +4,29 @@
 , cffi
 , enum34
 , construct
+, pytest
+, hypothesis
 }:
 
 buildPythonPackage rec {
   pname = "brotlipy";
-  version = "0.6.0";
+  version = "0.7.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "10s2y19zywfkf3sksrw81czhva759aki0clld2pnnlgf64sz7016";
+    sha256 = "36def0b859beaf21910157b4c33eb3b06d8ce459c942102f16988cca6ea164df";
   };
 
   propagatedBuildInputs = [ cffi enum34 construct ];
+
+  checkInputs = [ pytest hypothesis ];
+
+  checkPhase = ''
+    py.test
+  '';
+
+  # Missing test files
+  doCheck = false;
 
   meta = {
     description = "Python bindings for the reference Brotli encoder/decoder";
