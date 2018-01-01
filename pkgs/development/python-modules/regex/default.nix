f15e854904c6c1f352b54140351bc677422e32a1
Frederik Rietdijk <fridh@fridh.nl>
2018-01-01 11:50:37 +0100

    python: regex: 2016.11.18 -> 2017.12.12

--- pkgs/development/python-modules/regex/default.nix
+++ pkgs/development/python-modules/regex/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "regex";
-  version = "2016.11.18";
+  version = "2017.12.12";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "126ds2b355n3pgl7brshhscpxn14ycs0yznzl8k4akj4sps1i6c6";
+    sha256 = "ee069308c2757e565cc2b6f417ba5288e76cfe4c1764b6826063f4fbd53219d7";
   };
 
   meta = {
