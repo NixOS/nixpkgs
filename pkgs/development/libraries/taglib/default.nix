2e00e2b27667cd2da433996a5c957bb36520aa77
adisbladis <adis@blad.is>
2018-01-02 10:51:03 +0100

    taglib: Fix CVE-2017-12678

--- pkgs/development/libraries/taglib/default.nix
+++ pkgs/development/libraries/taglib/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, zlib, cmake}:
+{stdenv, fetchurl, zlib, cmake, fetchpatch}:
 
 stdenv.mkDerivation rec {
   name = "taglib-1.11.1";
@@ -7,6 +7,15 @@ stdenv.mkDerivation rec {
     url = "http://taglib.org/releases/${name}.tar.gz";
     sha256 = "0ssjcdjv4qf9liph5ry1kngam1y7zp8fzr9xv4wzzrma22kabldn";
   };
+
+  patches = [
+    (fetchpatch {
+      # https://github.com/taglib/taglib/issues/829
+      name = "CVE-2017-12678.patch";
+      url = "https://github.com/taglib/taglib/commit/eb9ded1206f18.patch";
+      sha256 = "1bvpxsvmlpi3by7myzss9kkpdkv405612n8ff68mw1ambj8h1m90";
+    })
+  ];
 
   nativeBuildInputs = [ cmake ];
 
