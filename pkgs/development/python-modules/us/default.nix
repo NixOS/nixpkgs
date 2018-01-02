8de024ef5ecda938aa8b33fba99f4fc8c1641996
Dominic Steinitz <dominic@steinitz.org>
2018-01-02 16:37:40 +0000

    An attempt at packaging the python package 'us'

---
+++ pkgs/development/python-modules/us/default.nix
@@ -0,0 +1,30 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, stdenv
+, jellyfish
+}:
+
+buildPythonPackage rec {
+  pname = "us";
+  version = "1.0.0";
+  name = "${pname}-${version}";
+  doCheck = !stdenv.isDarwin;
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1niglalkp7pinibzbxjdz9mxx9qmwkrh8884dag3kr72cfkrpp09";
+  };
+
+  meta = {
+    description = "A package for easily working with US and state metadata";
+    longDescription = ''
+    all US states and territories, postal abbreviations, Associated Press style
+    abbreviations, FIPS codes, capitals, years of statehood, time zones, phonetic
+    state name lookup, is contiguous or continental, URLs to shapefiles for state,
+    census, congressional districts, counties, and census tracts
+    '';
+    homepage = https://github.com/unitedstates/python-us/;
+    license = lib.licenses.bsd3;
+  };
+}
