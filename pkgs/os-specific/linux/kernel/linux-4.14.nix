dd396efe68b66be8e0b252a875c64f98f4e312cb
Tim Steinbach <tim@nequissimus.com>
2018-01-02 19:31:31 -0500

    linux: 4.14.10 -> 4.14.11
    (cherry picked from commit 1e129a3f9934ae62b77475909f6812f2ac3ab51f)

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 import ./generic.nix (args // rec {
-  version = "4.14.10";
+  version = "4.14.11";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = concatStrings (intersperse "." (take 3 (splitString "." "${version}.0")));
@@ -13,6 +13,6 @@ import ./generic.nix (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "046i3vcnhavblid71hrqdid4aay0rrcpzbiwlkcvs0x09hvz3fl6";
+    sha256 = "02kdp2k27gqqfxjv78wqlvqn1niin72masxs97mlw79za5m9as3p";
   };
 } // (args.argsOverride or {}))
