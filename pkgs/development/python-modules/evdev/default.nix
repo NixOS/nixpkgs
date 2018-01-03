2a859190795ac2e64a51f93354d891977ba8a344
Orivej Desh <orivej@gmx.fr>
2018-01-03 00:31:01 +0000

    python.pkgs.evdev: 0.6.4 -> 0.7.0

--- pkgs/development/python-modules/evdev/default.nix
+++ pkgs/development/python-modules/evdev/default.nix
@@ -1,20 +1,23 @@
-{ lib, buildPythonPackage, isPy34, fetchurl, linuxHeaders }:
+{ lib, buildPythonPackage, isPy34, fetchPypi, linuxHeaders }:
 
 buildPythonPackage rec {
-  version = "0.6.4";
-  name = "evdev-${version}";
-  disabled = isPy34;  # see http://bugs.python.org/issue21121
+  pname = "evdev";
+  version = "0.7.0";
 
-  src = fetchurl {
-    url = "mirror://pypi/e/evdev/${name}.tar.gz";
-    sha256 = "1wkag91s8j0f45jx5n619z354n8pz8in9krn81hp7hlkhi6p8s2j";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "188ahmqnh5y1f46m7pyjdmi9zfxswaggn6xga65za554d72azvap";
   };
 
   buildInputs = [ linuxHeaders ];
 
-  patchPhase = "sed -e 's#/usr/include/linux/#${linuxHeaders}/include/linux/#g' -i setup.py";
+  patchPhase = ''
+    substituteInPlace setup.py --replace /usr/include/linux ${linuxHeaders}/include/linux
+  '';
 
   doCheck = false;
+
+  disabled = isPy34;  # see http://bugs.python.org/issue21121
 
   meta = with lib; {
     description = "Provides bindings to the generic input event interface in Linux";
