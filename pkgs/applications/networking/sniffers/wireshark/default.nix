4447281ef91b6cb7ddabc9f5d9c32c2b408bb073
Andreas Rammhold <andreas@rammhold.de>
2018-01-05 20:08:20 +0100

    wireshark-gtk: explicitly disable the Qt build when building Gtk
    In a recent Cmake(?) or wireshark release the default behaviour did
    change. A failing build log can be seen at hydra [1].
    
    [1] https://hydra.nixos.org/build/67179559/nixlog/1
    
    (cherry picked from commit c77e0539e0bbf051281e13f611705e02b9fb7938)

--- pkgs/applications/networking/sniffers/wireshark/default.nix
+++ pkgs/applications/networking/sniffers/wireshark/default.nix
@@ -23,7 +23,7 @@ in stdenv.mkDerivation {
     sha256 = "0n3g28hrhifnchlz4av0blq4ykm4zaxwwxbzdm9wsba27677b6h4";
   };
 
-  cmakeFlags = optional withGtk "-DBUILD_wireshark_gtk=TRUE";
+  cmakeFlags = optional withGtk "-DBUILD_wireshark_gtk=TRUE -DBUILD_wireshark=OFF";
 
   nativeBuildInputs = [
     bison cmake extra-cmake-modules flex
