19eb5d6c273d0d31ff4e3e6954d8ff2fc6f3c686
Eelco Dolstra <edolstra@gmail.com>
2018-01-05 16:28:08 +0100

    linux: 4.9.74 -> 4.9.75

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.9.74";
+  version = "4.9.75";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1ivhzmsa8n5ns8igryzb9dyaakq2p51j23f6j9kpqyby7842i1y8";
+    sha256 = "1pyan45647wnliwhhp3dlmyvz7ibl1i56qplf3ilfh4dcsvk2v6y";
   };
 } // (args.argsOverride or {}))
