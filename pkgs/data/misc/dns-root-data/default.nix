42121f7c063f317199ac4c39591e5efb66426e2c
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2018-01-05 16:59:21 -0500

    dns-root-data: fix broken download
    This basically backports 9bf32737 ('dns-root-data: use a stable URL that I maintain anyway')
    /cc #31862

--- pkgs/data/misc/dns-root-data/default.nix
+++ pkgs/data/misc/dns-root-data/default.nix
@@ -3,7 +3,13 @@
 let
 
   rootHints = fetchurl {
-    url = "https://www.internic.net/domain/named.root";
+    # Original source https://www.internic.net/domain/named.root
+    # occasionally suffers from pointless hash changes,
+    # and having stable sources for older versions has advantages, too.
+    urls = [
+      "https://gist.githubusercontent.com/veprbl/145bfd5db525d63ac4ad506af2c3ac90/raw/af0e4acaa1d3d93b1e8d60f8f9865b5449cac766/named.root"
+      "https://gateway.ipfs.io/ipfs/QmVPqoxuwHZ4J7ddpT8TV648JCyZ8Kx8tk7u5vm7YCqRPi/named.root"
+      ];
     sha256 = "01n4bqf95kbvig1hahqzmmdkpn4v7mzfc1p944gq922i5j3fjr92";
   };
 
@@ -24,6 +30,6 @@ stdenv.mkDerivation {
 
   meta = with lib; {
     description = "DNS root data including root zone and DNSSEC key";
-    maintainers = with maintainers; [ fpletz ];
+    maintainers = with maintainers; [ fpletz veprbl ];
   };
 }
