eaf661e15fdf826a7362ba7ca121c61871d3e67a
Elis Hirwing <elis@hirwing.se>
2018-01-06 08:21:57 +0100

    php56: 5.6.32 -> 5.6.33

--- pkgs/development/interpreters/php/default.nix
+++ pkgs/development/interpreters/php/default.nix
@@ -338,8 +338,8 @@ let
 
 in {
   php56 = generic {
-    version = "5.6.32";
-    sha256 = "0lfbmdkvijkm6xc4p9sykv66y8xwhws0vsmka8v5cax4bxx4xr1y";
+    version = "5.6.33";
+    sha256 = "1k1ip1slk89hkp57qiqp8k2m5yrg9lx5rja542g87k8xfslrdxh7";
   };
 
   php70 = generic {
