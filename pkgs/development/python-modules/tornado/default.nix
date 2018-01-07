2ed13cb30fa5689273d7ae390f6a641191a99bc5
Frederik Rietdijk <fridh@fridh.nl>
2018-01-07 09:01:34 +0100

    python: tornado: 4.5.2 -> 4.5.3
    (cherry picked from commit deb4d8ec76ce0039641a8ef0fec22919f961f25c)

--- pkgs/development/python-modules/tornado/default.nix
+++ pkgs/development/python-modules/tornado/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "tornado";
-  version = "4.5.2";
+  version = "4.5.3";
   name = "${pname}-${version}";
 
   propagatedBuildInputs = [ backports_abc backports_ssl_match_hostname certifi singledispatch ];
@@ -23,6 +23,6 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1fb8e494cd46c674d86fac5885a3ff87b0e283937a47d74eb3c02a48c9e89ad0";
+    sha256 = "6d14e47eab0e15799cf3cdcc86b0b98279da68522caace2bd7ce644287685f0a";
   };
 }
