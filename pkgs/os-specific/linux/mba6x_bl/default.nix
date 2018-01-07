5a2cfb2f9e900ed675ee8f3630b4cb77898ebf7c
Yegor Timoshenko <yegortimoshenko@gmail.com>
2018-01-07 17:51:23 +0000

    mba6x_bl: fix build
    *grumble*

--- pkgs/os-specific/linux/mba6x_bl/default.nix
+++ pkgs/os-specific/linux/mba6x_bl/default.nix
@@ -12,6 +12,8 @@ stdenv.mkDerivation rec {
 
   enableParallelBuilding = true;
   hardeningDisable = [ "pic" ];
+  
+  nativeBuildInputs = kernel.moduleBuildDependencies;
 
   makeFlags = [
     "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
