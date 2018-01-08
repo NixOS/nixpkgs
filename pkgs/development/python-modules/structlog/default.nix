60c8c1bd2da7f5580cdd50328690d237b8b951c8
Will Dietz <w@wdtz.org>
2018-01-08 09:46:40 -0600

    pythonPackages.structlog: patch to fix tests, build

--- pkgs/development/python-modules/structlog/default.nix
+++ pkgs/development/python-modules/structlog/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, fetchpatch
 , pytest
 , pretend
 , freezegun
@@ -15,6 +16,14 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "6980001045abd235fa12582222627c19b89109e58b85eb77d5a5abc778df6e20";
   };
+
+  patches = [
+    # Fix tests for pytest 3.3
+    (fetchpatch {
+      url = "https://github.com/hynek/structlog/commit/22f0ae50607a0cb024361599f84610ce290deb99.patch";
+      sha256 = "03622i13ammkpyrdk48kimbz94gbkpcmdpy0kj2z09m1kp6q2ljv";
+    })
+  ];
 
   checkInputs = [ pytest pretend freezegun ];
   propagatedBuildInputs = [ simplejson ];
