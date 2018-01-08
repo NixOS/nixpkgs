87e3d8d214b4954b372c2e02e77e39055dba656d
Will Dietz <w@wdtz.org>
2018-01-08 09:11:17 -0600

    httpbin: patch to fix test failures when used as dependency

--- pkgs/development/python-modules/httpbin/default.nix
+++ pkgs/development/python-modules/httpbin/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , buildPythonPackage
 , fetchPypi
+, fetchpatch
 , flask
 , flask_common
 , flask_limiter
@@ -21,6 +22,16 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "0afa0486a76305cac441b5cc80d5d4ccd82b20875da7c5119ecfe616cefef45f";
   };
+
+  patches = [
+    # https://github.com/kennethreitz/httpbin/issues/403
+    # https://github.com/kennethreitz/flask-common/issues/7
+    # https://github.com/evansd/whitenoise/issues/166
+    (fetchpatch {
+      url = "https://github.com/javabrett/httpbin/commit/5735c888e1e51b369fcec41b91670a90535e661e.patch";
+      sha256 = "167h8mscdjagml33dyqk8nziiz3dqbggnkl6agsirk5270nl5f7q";
+    })
+  ];
 
   propagatedBuildInputs = [ brotlipy flask flask_common flask_limiter markupsafe decorator itsdangerous raven six ];
 
