2c332380b565fe1f088a3437e09546186192092e
Jörg Thalheim <joerg@thalheim.io>
2018-01-09 01:10:56 +0100

    librepcb: use fetchFromGitHub; set qmakeFlags

--- pkgs/applications/science/electronics/librepcb/default.nix
+++ pkgs/applications/science/electronics/librepcb/default.nix
@@ -1,29 +1,32 @@
-{ stdenv, fetchgit, qtbase, qttools, qmake, mesa, openssl, zlib }:
+{ stdenv, fetchFromGitHub, qtbase, qttools, qmake, mesa, openssl, zlib }:
 
 stdenv.mkDerivation rec {
-	name = "librepcb-unstable";
-	version = "2017-12-29";
-	src = fetchgit {
-		url = "git://github.com/LibrePCB/LibrePCB";
-		rev = "4efb06fa42755abc5e606da4669cc17e8de2f8c6";
-		sha256 = "0r33fm1djqpy0dzvnf5gv2dfh5nj2acaxb7w4cn8yxdgrazjf7ak";
-	};
+  name = "librepcb-${version}";
+  version = "2017-12-29";
 
-	enableParallelBuilding=true;
+  src = fetchFromGitHub {
+    owner = "LibrePCB";
+    repo = "LibrePCB";
+    fetchSubmodules = true;
+    rev = "4efb06fa42755abc5e606da4669cc17e8de2f8c6";
+    sha256 = "0r33fm1djqpy0dzvnf5gv2dfh5nj2acaxb7w4cn8yxdgrazjf7ak";
+  };
 
-	nativeBuildInputs = [ qmake qttools qtbase ];
+  enableParallelBuilding = true;
 
-	patches = [ ./fix-2017-12.patch ];
+  nativeBuildInputs = [ qmake qttools ];
 
-	#recursive qmake building is required -> therefore this line is required (qmake -r)
-	preBuild = "mkdir build && cd build && qmake -r ../librepcb.pro PREFIX=$out QMAKE_LRELEASE=${stdenv.lib.getDev qttools.dev}/bin/lrelease";
+  buildInputs = [ qtbase ];
 
-	meta = with stdenv.lib; {
-		description = "A free EDA software to develop printed circuit boards";
-		homepage = http://librepcb.org/;
-		maintainers = with maintainers; [ luz ];
-		license = licenses.gpl3;
-		platforms = platforms.linux;
-	};
+  patches = [ ./fix-2017-12.patch ];
+
+  qmakeFlags = ["-r"];
+
+  meta = with stdenv.lib; {
+    description = "A free EDA software to develop printed circuit boards";
+    homepage = http://librepcb.org/;
+    maintainers = with maintainers; [ luz ];
+    license = licenses.gpl3;
+    platforms = platforms.linux;
+  };
 }
-
