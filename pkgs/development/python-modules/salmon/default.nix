7140c4bbf9797f8be27dab9cfe5bad640ca108db
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2018-01-11 00:26:19 +0200

    pythonPackages.salmon: bec795cd -> 3.0.0

--- pkgs/development/python-modules/salmon/default.nix
+++ pkgs/development/python-modules/salmon/default.nix
@@ -3,18 +3,14 @@
 
 buildPythonPackage rec {
   pname = "salmon";
-  # NOTE: The latest release version 2 is over 3 years old. So let's use some
-  # recent commit from master. There will be a new release at some point, then
-  # one can change this to use PyPI. See:
-  # https://github.com/moggers87/salmon/issues/52
-  version = "bec795cdab744be4f103d8e35584dfee622ddab2";
+  version = "3.0.0";
   name = "${pname}-${version}";
 
   src = fetchFromGitHub {
     owner = "moggers87";
     repo = "salmon";
     rev = version;
-    sha256 = "0nx8pyxy7n42nsqqvhd85ycm1i5wlacsi7lwb4rrs9d416bpd300";
+    sha256 = "0ffnvvms9w5b2hkwq0ss018vadg5n7xwhc51dlrfynddhrf9p3m3";
   };
 
   checkInputs = [ nose jinja2 mock ];
