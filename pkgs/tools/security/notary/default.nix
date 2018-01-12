9e27701d64e74912eb57d83da0eee8fc94249d7b
Jörg Thalheim <joerg@thalheim.io>
2018-01-12 19:16:20 +0000

    notary: get rid of .git folder in source
    tested with:
    
      $ notary version

--- pkgs/tools/security/notary/default.nix
+++ pkgs/tools/security/notary/default.nix
@@ -1,44 +1,55 @@
-{ stdenv, fetchgit, buildGoPackage, git, libtool }:
+{ stdenv, fetchFromGitHub, buildGoPackage, git, libtool }:
 
 buildGoPackage rec {
   name = "notary-${version}";
   version = "0.5.1";
   gitcommit = "9211198";
 
-  src = fetchgit {
-    url = "https://github.com/theupdateframework/notary";
-    rev = "refs/tags/v${version}";
-    sha256 = "1iybkwvl7021g7kklqybrip8d7hsvvqggpb19yx9kjjisasi2dw6";
-    leaveDotGit = true;
+  src = fetchFromGitHub {
+    owner = "theupdateframework";
+    repo = "notary";
+    rev = "v${version}";
+    sha256 = "0z9nsb1mrl0q5j02jkyzbc6xqsm83qzacsckypsxcrijhw935rs5";
   };
 
-  buildInputs = [ git libtool ];
+  buildInputs = [ libtool ];
 
   goPackagePath = "github.com/docker/notary";
+
   buildPhase = ''
     cd go/src/github.com/docker/notary
-    make GITCOMMIT=${gitcommit} client
+    make GITCOMMIT=${gitcommit} GITUNTRACKEDCHANGES= client
   '';
 
   installPhase = ''
-    mkdir -p $bin/bin
-    cp bin/notary $bin/bin/notary
+    install -D bin/notary $bin/bin/notary
   '';
 
   meta = with stdenv.lib; {
-      description = " Notary is a project that allows anyone to have trust over arbitrary collections of data";
-      longDescription = ''
-        The Notary project comprises a server and a client for running and interacting with trusted collections. See the service architecture documentation for more information.
+    description = " Notary is a project that allows anyone to have trust over arbitrary collections of data";
+    longDescription = ''
+      The Notary project comprises a server and a client for running and
+      interacting with trusted collections. See the service architecture
+      documentation for more information.
 
-        Notary aims to make the internet more secure by making it easy for people to publish and verify content. We often rely on TLS to secure our communications with a web server which is inherently flawed, as any compromise of the server enables malicious content to be substituted for the legitimate content.
+      Notary aims to make the internet more secure by making it easy for people
+      to publish and verify content. We often rely on TLS to secure our
+      communications with a web server which is inherently flawed, as any
+      compromise of the server enables malicious content to be substituted for
+      the legitimate content.
 
-        With Notary, publishers can sign their content offline using keys kept highly secure. Once the publisher is ready to make the content available, they can push their signed trusted collection to a Notary Server.
+      With Notary, publishers can sign their content offline using keys kept
+      highly secure. Once the publisher is ready to make the content available,
+      they can push their signed trusted collection to a Notary Server.
 
-        Consumers, having acquired the publisher's public key through a secure channel, can then communicate with any notary server or (insecure) mirror, relying only on the publisher's key to determine the validity and integrity of the received content.
-      '';
-      license = licenses.asl20;
-      homepage = https://github.com/theupdateframework/notary;
-      maintainers = with maintainers; [ vdemeester ];
-      platforms = with platforms; unix;
+      Consumers, having acquired the publisher's public key through a secure
+      channel, can then communicate with any notary server or (insecure) mirror,
+      relying only on the publisher's key to determine the validity and
+      integrity of the received content.
+    '';
+    license = licenses.asl20;
+    homepage = https://github.com/theupdateframework/notary;
+    maintainers = with maintainers; [ vdemeester ];
+    platforms = with platforms; unix;
   };
 }
