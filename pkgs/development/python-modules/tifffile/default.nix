f190a734adaacdacbd6084bd3365e9f233ffa46f
Jörg Thalheim <joerg@thalheim.io>
2018-01-13 18:11:32 +0000

    python.pkgs.tifffile: actually run tests

--- pkgs/development/python-modules/tifffile/default.nix
+++ pkgs/development/python-modules/tifffile/default.nix
@@ -1,25 +1,28 @@
-{lib, stdenv, fetchFromGitHub, python, buildPythonPackage, isPyPy, isPy27, numpy, nose,
- enum34, pythonOlder, futures}:
+{ lib, stdenv, fetchPypi, buildPythonPackage, isPy27, pythonOlder
+, numpy, nose, enum34, futures }:
 
 buildPythonPackage rec {
   pname = "tifffile";
-  rev = "v0.13.0";
-  name = "${pname}-${rev}";
+  version = "0.13.0";
 
-  src = fetchFromGitHub {
-    owner = "blink1073";
-    repo = "tifffile";
-    inherit rev;
-    sha256 = "0adm1zf3b4gf8yjriidjnl9abcycqiy5bzannwyb8rcbh3jwdbzv";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1rqx2bar6dcsbmzq68mjh5bsyjzhbkqxi2dsv7j0vdbnjs5dfq9q";
   };
 
-  buildInputs = [ nose ];
-    
-  propagatedBuildInputs = [ numpy ] ++ lib.optional isPy27 futures ++ lib.optional (pythonOlder "3.0") enum34;
+  checkInputs = [ nose ];
+  checkPhase = ''
+    nosetests --exe -v --exclude="test_extension"
+  '';
 
-  meta = {
+  propagatedBuildInputs = [ numpy ]
+    ++ lib.optional isPy27 futures
+    ++ lib.optional (pythonOlder "3.0") enum34;
+
+  meta = with stdenv.lib; {
     description = "Read and write image data from and to TIFF files.";
     homepage = https://github.com/blink1073/tifffile;
-    maintainers = with lib.maintainers; [ lebastr ];
+    maintainers = [ maintainers.lebastr ];
+    license = licenses.bsd2;
   };
 }
