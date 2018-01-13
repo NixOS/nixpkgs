f566e3f987869c2c269986efbe4e3d256c6846cb
Will Dietz <w@wdtz.org>
2018-01-12 19:36:58 -0600

    gcc7: more selftest disabling, whenever building cross

--- pkgs/development/compilers/gcc/7/default.nix
+++ pkgs/development/compilers/gcc/7/default.nix
@@ -67,7 +67,7 @@ let version = "7.3.0";
       [ ]
       ++ optional (targetPlatform != hostPlatform) ../libstdc++-target.patch
       ++ optional noSysDirs ../no-sys-dirs.patch
-      ++ optional (targetPlatform.libc == "musl") (fetchpatch {
+      ++ optional (hostPlatform != buildPlatform) (fetchpatch { # XXX: Refine when this should be applied
         url = "https://git.busybox.net/buildroot/plain/package/gcc/7.1.0/0900-remove-selftests.patch?id=11271540bfe6adafbc133caf6b5b902a816f5f02";
         sha256 = "0mrvxsdwip2p3l17dscpc1x8vhdsciqw1z5q9i6p5g9yg1cqnmgs";
       })
