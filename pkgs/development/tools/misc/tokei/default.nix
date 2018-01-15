c6c3344cc47e215b4edb4d0ed0759889f15a6119
dywedir <dywedir@protonmail.ch>
2018-01-15 12:33:15 +0200

    tokei: 6.1.2 -> 7.0.0

--- pkgs/development/tools/misc/tokei/default.nix
+++ pkgs/development/tools/misc/tokei/default.nix
@@ -2,26 +2,21 @@
 
 rustPlatform.buildRustPackage rec {
   name = "tokei-${version}";
-  version = "6.1.2";
+  version = "7.0.0";
 
   src = fetchFromGitHub {
     owner = "Aaronepower";
     repo = "tokei";
     rev = "v${version}";
-    sha256 = "1bzs3mr6f9bna39b9ddwwq0raas07nbn106mnq3widxg59i0gxhd";
+    sha256 = "1c8m2arhy58ky8pzj0dp3w9gpacia9jwmayi0il640l4fm8nr734";
   };
 
-  cargoSha256 = "0y0rkxhkv31v5sa0425dwskd80i6srwbqhqkrw1g1kbmbs9y0vxz";
-
-  buildPhase = ''
-    # do not pass --frozen since Cargo.lock has the wrong tokei version
-    cargo build --release
-  '';
+  cargoSha256 = "1cl4fjbvrw7zhpb8rxj566ddlxbj9vdsb1cp7mh6llmvaia2vgks";
 
   meta = with stdenv.lib; {
     description = "Count code, quickly";
     homepage = https://github.com/Aaronepower/tokei;
-    license = licenses.mit;
+    license = with licenses; [ asl20 /* or */ mit ];
     maintainers = with maintainers; [ gebner ];
     platforms = platforms.all;
   };
