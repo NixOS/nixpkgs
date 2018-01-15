245a67df8bc4f6e1fa0ab40578c1b9668599a042
Ashley Gillman <ashley.gillman@csiro.au>
2018-01-15 16:53:28 +1000

    dcm2niix: cmake is native, style

--- pkgs/applications/science/biology/dcm2niix/default.nix
+++ pkgs/applications/science/biology/dcm2niix/default.nix
@@ -4,32 +4,30 @@
 , libyamlcpp
 }:
 
-let
-  version = "v1.0.20170130";
-  sha256 = "1f2nzd8flp1rfn725bi64z7aw3ccxyyygzarxijw6pvgl476i532";
-
-in stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
+  version = "1.0.20170130";
   name = "dcm2niix-${version}";
 
   src = fetchFromGitHub {
-    inherit sha256;
     owner = "rordenlab";
     repo = "dcm2niix";
-    rev = version;
+    rev = "v${version}";
+    sha256 = "1f2nzd8flp1rfn725bi64z7aw3ccxyyygzarxijw6pvgl476i532";
   };
 
   enableParallelBuilding = true;
-  buildInputs = [ cmake libyamlcpp];
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ libyamlcpp ];
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "dcm2niix DICOM to NIfTI converter";
     longDescription = ''
       dcm2niix is a designed to convert neuroimaging data from the
       DICOM format to the NIfTI format.
     '';
     homepage = https://www.nitrc.org/projects/dcm2nii;
-    license = stdenv.lib.licenses.bsd3;
-    maintainers = [ stdenv.lib.maintainers.ashgillman ];
-    platforms = stdenv.lib.platforms.linux;
-};
+    license = licenses.bsd3;
+    maintainers = [ maintainers.ashgillman ];
+    platforms = platforms.all;
+  };
 }
