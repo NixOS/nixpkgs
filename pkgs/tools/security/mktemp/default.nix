3785e0ac7e2dfa7e9ff4687ab3b4cb0e76b2c7ed
Will Dietz <w@wdtz.org>
2018-01-14 19:25:25 -0600

    mktemp: fix w/cross

--- pkgs/tools/security/mktemp/default.nix
+++ pkgs/tools/security/mktemp/default.nix
@@ -6,6 +6,11 @@ stdenv.mkDerivation {
   # Have `configure' avoid `/usr/bin/nroff' in non-chroot builds.
   NROFF = "${groff}/bin/nroff";
 
+  # Don't use "install -s"
+  postPatch = ''
+    substituteInPlace Makefile.in --replace " 0555 -s " " 0555 "
+  '';
+
   src = fetchurl {
     url = ftp://ftp.mktemp.org/pub/mktemp/mktemp-1.7.tar.gz;
     sha256 = "0x969152znxxjbj7387xb38waslr4yv6bnj5jmhb4rpqxphvk54f";
