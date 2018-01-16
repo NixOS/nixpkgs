a892bd8ebfe796beb67db4b143d912df394aef80
Florian Jacob <projects+git@florianjacob.de>
2018-01-16 17:02:57 +0100

    piwik 3.2.1 -> matomo 3.3.0 minor release, mainly rename

--- pkgs/servers/web-apps/matomo/default.nix
+++ pkgs/servers/web-apps/matomo/default.nix
@@ -1,12 +1,14 @@
 { stdenv, fetchurl, makeWrapper, php }:
 
 stdenv.mkDerivation rec {
-  name = "piwik-${version}";
-  version = "3.2.1";
+  name = "matomo-${version}";
+  version = "3.3.0";
 
   src = fetchurl {
-    url = "https://builds.piwik.org/${name}.tar.gz";
-    sha512 = "1yisgywz7dm6kygh9mc207xnqpvdxbw4pa2l9gjh495a6979x3chi7z5rf410z4dmrg0kbj8wqm8mmmslfn276xvw37l2d4h73ij1h2";
+    # TODO: As soon as the tarballs are renamed as well on future releases, this should be enabled again
+    # url = "https://builds.matomo.org/${name}.tar.gz";
+    url = "https://builds.matomo.org/piwik-${version}.tar.gz";
+    sha256 = "1ybzj3kk0x29nv8c6xnhir5d9dr0q0fl1vnm4i7zvhml73ryqk0f";
   };
 
   nativeBuildInputs = [ makeWrapper ];
@@ -21,21 +23,22 @@ stdenv.mkDerivation rec {
     cp ${./bootstrap.php} bootstrap.php
   '';
 
+  # TODO: future versions might rename the PIWIK_… variables to MATOMO_…
   # TODO: Move more unnecessary files from share/, especially using PIWIK_INCLUDE_PATH.
-  #       See https://forum.piwik.org/t/bootstrap-php/5926/10 and
-  #       https://github.com/piwik/piwik/issues/11654#issuecomment-297730843
+  #       See https://forum.matomo.org/t/bootstrap-php/5926/10 and
+  #       https://github.com/matomo-org/matomo/issues/11654#issuecomment-297730843
   installPhase = ''
     runHook preInstall
 
     # copy evertything to share/, used as webroot folder, and then remove what's known to be not needed
     mkdir -p $out/share
     cp -ra * $out/share/
-    # tmp/ is created by piwik in PIWIK_USER_PATH
+    # tmp/ is created by matomo in PIWIK_USER_PATH
     rmdir $out/share/tmp
     # config/ needs to be copied to PIWIK_USER_PATH anyway
     mv $out/share/config $out/
 
-    makeWrapper ${php}/bin/php $out/bin/piwik-console \
+    makeWrapper ${php}/bin/php $out/bin/matomo-console \
       --add-flags "$out/share/console"
 
     runHook postInstall
@@ -44,7 +47,7 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     description = "A real-time web analytics application";
     license = licenses.gpl3Plus;
-    homepage = https://piwik.org/;
+    homepage = https://matomo.org/;
     platforms = platforms.all;
     maintainers = [ maintainers.florianjacob ];
   };
