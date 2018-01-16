0f18644d4cc0ea8d7f52e8be717155215fa7ab98
schuppentier <janne.hess@faveve.uni-stuttgart.de>
2018-01-16 10:05:42 +0100

    evdi: 1.4.1+git2017-06-12 -> unstable-2018-01-12 (#33907)
    (cherry picked from commit 6f7c1977005401a6a7a3e6ff00b5cf386f16bd0e)

--- pkgs/os-specific/linux/evdi/default.nix
+++ pkgs/os-specific/linux/evdi/default.nix
@@ -2,14 +2,16 @@
 
 stdenv.mkDerivation rec {
   name = "evdi-${version}";
-  version = "1.4.1+git2017-06-12";
+  version = "unstable-2018-01-12";
 
   src = fetchFromGitHub {
     owner = "DisplayLink";
     repo = "evdi";
-    rev = "ee1c578774e62fe4b08d92750620ed3094642160";
-    sha256 = "1m3wkmw4hjpjax7rvhmpicz09d7vxcxklq797ddjg6ljvf12671b";
+    rev = "e7a08d08e3876efba8007e91df1b296f2447b8de";
+    sha256 = "01z7bx5rgpb5lc4c6dxfiv52ni25564djxmvmgy3d7r1x1mqhxgs";
   };
+
+  nativeBuildInputs = kernel.moduleBuildDependencies;
 
   buildInputs = [ kernel libdrm ];
 
@@ -27,6 +29,6 @@ stdenv.mkDerivation rec {
     platforms = platforms.linux;
     license = licenses.gpl2;
     homepage = http://www.displaylink.com/;
-    broken = versionOlder kernel.version "4.9" || !stdenv.lib.versionOlder kernel.version "4.13";
+    broken = versionOlder kernel.version "4.9";
   };
 }
