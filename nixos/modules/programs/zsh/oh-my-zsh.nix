NixOS/nixpkgs

commit b55d4c0564b0bde4cbc0c01b6ba7a1276382335a

programs.zsh.ohMyZsh: add `cacheDir` option (#33150)
The default cache directory set by oh-my-zsh is $ohMyZsh/cache which
lives in the Nix store in our case. This causes issues with several
completion plugins provided by oh-my-zsh.

nixos/modules/programs/zsh/oh-my-zsh.nix
@@ -48,6 +48,15 @@ in
             Name of the theme to be used by oh-my-zsh.
           '';
         };
+
+        cacheDir = mkOption {
+          default = "$HOME/.cache/oh-my-zsh";
+          type = types.str;
+          description = ''
+            Cache directory to be used by `oh-my-zsh`.
+            Default is /nix/store/<oh-my-zsh>/cache.
+          '';
+        };
       };
     };
 
@@ -73,6 +82,13 @@ in
         ${optionalString (stringLength(cfg.theme) > 0)
           "ZSH_THEME=\"${cfg.theme}\""
         }
+
+        ${optionalString (cfg.cacheDir != null) ''
+          if [[ ! -d "${cfg.cacheDir}" ]]; then
+            mkdir -p "${cfg.cacheDir}"
+          fi
+          ZSH_CACHE_DIR=${cfg.cacheDir}
+        ''}
 
         source $ZSH/oh-my-zsh.sh
       '';

commit 78dd0beb23077a3045545a97ace3ea6ad7dab139

fix manual
broken by: https://github.com/NixOS/nixpkgs/commit/b55d4c0564b0bde4cbc0c01b6ba7a1276382335a

nixos/modules/programs/zsh/oh-my-zsh.nix
@@ -54,7 +54,7 @@ in
           type = types.str;
           description = ''
             Cache directory to be used by `oh-my-zsh`.
-            Default is /nix/store/<oh-my-zsh>/cache.
+            Default is /nix/store/&#60;oh-my-zsh&#62;/cache.
           '';
         };
       };