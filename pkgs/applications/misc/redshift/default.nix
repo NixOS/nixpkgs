fccaa2acab1756b018a4f99b51cd7880e199be37
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-01-16 16:18:06 +0000

    redshift: reintroduce longDescription

--- pkgs/applications/misc/redshift/default.nix
+++ pkgs/applications/misc/redshift/default.nix
@@ -47,6 +47,14 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Screen color temperature manager";
+    longDescription = ''
+      Redshift adjusts the color temperature according to the position
+      of the sun. A different color temperature is set during night and
+      daytime. During twilight and early morning, the color temperature
+      transitions smoothly from night to daytime temperature to allow
+      your eyes to slowly adapt. At night the color temperature should
+      be set to match the lamps in your room.
+    '';
     license = licenses.gpl3Plus;
     homepage = http://jonls.dk/redshift;
     platforms = platforms.linux;
