82cab72dd41cf3e243a76bdbbe2ba9631e9ffa5a
Tuomas Tynkkynen <tuomas@tuxera.com>
2018-01-16 18:28:21 +0200

    release-lib: forAllSupportedSystems -> forTheseSystems
    I'm going to move forAllSystems from nixos/release.nix, and these
    functions sound too similar while doing different things.

--- pkgs/top-level/release-cross.nix
+++ pkgs/top-level/release-cross.nix
@@ -75,7 +75,7 @@ in
         f (["buildPackages"] ++ path) { inherit system crossSystem; }
       );
 
-    testEqual = path: systems: forAllSupportedSystems systems (testEqualOne path);
+    testEqual = path: systems: forTheseSystems systems (testEqualOne path);
 
     mapTestEqual = lib.mapAttrsRecursive testEqual;
 
