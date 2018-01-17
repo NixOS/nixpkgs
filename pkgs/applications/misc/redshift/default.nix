846b5c42f19fe2ea8deb58f072d5c2df56605d19
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-01-17 04:48:16 +0000

    redshift: add myself to maintainers

--- pkgs/applications/misc/redshift/default.nix
+++ pkgs/applications/misc/redshift/default.nix
@@ -58,6 +58,6 @@ stdenv.mkDerivation rec {
     license = licenses.gpl3Plus;
     homepage = http://jonls.dk/redshift;
     platforms = platforms.linux;
-    maintainers = with maintainers; [ mornfall ];
+    maintainers = with maintainers; [ mornfall yegortimoshenko ];
   };
 }
