ca91bc038999cbe99e2e7e3ee72a0735ff98a7ed
Tim Steinbach <tim@nequissimus.com>
2018-01-17 08:55:14 -0500

    linux: 4.9.76 -> 4.9.77
    (cherry picked from commit 7981bb421986feb775819f0e137ffe3250a28b86)

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.9.76";
+  version = "4.9.77";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1pl7x1fnyhvwbdxgh0w5fka9dyysi74n8lj9fkgfmapz5hrr8axq";
+    sha256 = "0p3hnfj0597vznvvjcb4ynciafnmvmnphkk6izcj67kgp4zvqabw";
   };
 } // (args.argsOverride or {}))
