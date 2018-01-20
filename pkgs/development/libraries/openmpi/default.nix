e013f0d81ac7fb9e0b33de1df8bea33e8679ad17
Markus Kowalewski <markus.kowalewski.uu@gmail.com>
2018-01-19 16:16:35 -0800

    openmpi: 1.10.7->3.0.0, add markuskowa as maintainer
    * add license
    * update description
    * add markuskowa as maintainer

--- pkgs/development/libraries/openmpi/default.nix
+++ pkgs/development/libraries/openmpi/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, gfortran, perl, rdma-core
+{ stdenv, fetchurl, gfortran, perl, libnl, rdma-core, zlib
 
 # Enable the Sun Grid Engine bindings
 , enableSGE ? false
@@ -10,41 +10,47 @@
 with stdenv.lib;
 
 let
-  majorVersion = "1.10";
+  majorVersion = "3.0";
+  minorVersion = "0";
 
 in stdenv.mkDerivation rec {
-  name = "openmpi-${majorVersion}.7";
+  name = "openmpi-${majorVersion}.${minorVersion}";
 
   src = fetchurl {
     url = "http://www.open-mpi.org/software/ompi/v${majorVersion}/downloads/${name}.tar.bz2";
-    sha256 = "142s1vny9gllkq336yafxayjgcirj2jv0ddabj879jgya7hyr2d0";
+    sha256 = "1mw2d94k6mp4scg1wnkj50vdh734fy5m2ygyrj65s4mh3prbz6gn";
   };
 
-  buildInputs = [ gfortran ]
-    ++ optional (stdenv.isLinux || stdenv.isFreeBSD) rdma-core;
+  postPatch = ''
+    patchShebangs ./
+  '';
+
+  buildInputs = with stdenv; [ gfortran zlib ]
+    ++ optional isLinux libnl
+    ++ optional (isLinux || isFreeBSD) rdma-core;
 
   nativeBuildInputs = [ perl ];
 
   configureFlags = []
+    ++ optional stdenv.isLinux  "--with-libnl=${libnl.dev}"
     ++ optional enableSGE "--with-sge"
     ++ optional enablePrefix "--enable-mpirun-prefix-by-default"
     ;
 
   enableParallelBuilding = true;
 
-  preBuild = ''
-    patchShebangs ompi/mpi/fortran/base/gen-mpi-sizeof.pl
-  '';
-
   postInstall = ''
-		rm -f $out/lib/*.la
+    rm -f $out/lib/*.la
    '';
+
+  doCheck = true;
 
   meta = {
     homepage = http://www.open-mpi.org/;
-    description = "Open source MPI-2 implementation";
-    longDescription = "The Open MPI Project is an open source MPI-2 implementation that is developed and maintained by a consortium of academic, research, and industry partners. Open MPI is therefore able to combine the expertise, technologies, and resources from all across the High Performance Computing community in order to build the best MPI library available. Open MPI offers advantages for system and software vendors, application developers and computer science researchers.";
-    maintainers = [ ];
+    description = "Open source MPI-3 implementation";
+    longDescription = "The Open MPI Project is an open source MPI-3 implementation that is developed and maintained by a consortium of academic, research, and industry partners. Open MPI is therefore able to combine the expertise, technologies, and resources from all across the High Performance Computing community in order to build the best MPI library available. Open MPI offers advantages for system and software vendors, application developers and computer science researchers.";
+    maintainers = with maintainers; [ markuskowa ];
+    license = licenses.bsd3;
     platforms = platforms.unix;
   };
 }
