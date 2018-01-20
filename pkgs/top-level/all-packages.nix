c6b828b86eef331e26840e11da0114472c020beb
Markus Kowalewski <markus.kowalewski@gmail.com>
2018-01-20 15:09:14 -0800

    rdma-core: init at 16.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4321,6 +4321,8 @@ with pkgs;
 
   rc = callPackage ../shells/rc { };
 
+  rdma-core = callPackage ../os-specific/linux/rdma-core { };
+
   read-edid = callPackage ../os-specific/linux/read-edid { };
 
   redir = callPackage ../tools/networking/redir { };
