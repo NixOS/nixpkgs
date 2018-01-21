9ab8c28e3dffca69cdc5daf1086d3117e50bdf78
dywedir <dywedir@protonmail.ch>
2018-01-21 17:01:58 +0200

    dunst: 1.2.0 -> 1.3.0

--- pkgs/applications/misc/dunst/default.nix
+++ pkgs/applications/misc/dunst/default.nix
@@ -1,34 +1,39 @@
 { stdenv, fetchFromGitHub, fetchpatch
-, pkgconfig, which, perl, gtk2, xrandr
-, cairo, dbus, gdk_pixbuf, glib, libX11, libXScrnSaver
+, pkgconfig, which, perl, libXrandr
+, cairo, dbus, systemd, gdk_pixbuf, glib, libX11, libXScrnSaver
 , libXinerama, libnotify, libxdg_basedir, pango, xproto, librsvg
 }:
 
 stdenv.mkDerivation rec {
   name = "dunst-${version}";
-  version = "1.2.0";
+  version = "1.3.0";
 
   src = fetchFromGitHub {
     owner = "dunst-project";
     repo = "dunst";
     rev = "v${version}";
-    sha256 = "0jncnb4z4hg92ws08bkf52jswsd4vqlzyznwbynhh2jh6q0sl18b";
+    sha256 = "1085v4193yfj8ksngp4mk5n0nwzr3s5y3cs3c74ymaldfl20x91k";
   };
 
-  nativeBuildInputs = [ perl pkgconfig which ];
+  nativeBuildInputs = [ perl pkgconfig which systemd ];
 
   buildInputs = [
     cairo dbus gdk_pixbuf glib libX11 libXScrnSaver
-    libXinerama libnotify libxdg_basedir pango xproto librsvg gtk2 xrandr
+    libXinerama libnotify libxdg_basedir pango xproto librsvg libXrandr
   ];
 
   outputs = [ "out" "man" ];
 
-  makeFlags = [ "PREFIX=$(out)" "VERSION=$(version)" ];
+  makeFlags = [
+    "PREFIX=$(out)"
+    "VERSION=$(version)"
+    "SERVICEDIR_DBUS=$(out)/share/dbus-1/services"
+    "SERVICEDIR_SYSTEMD=$(out)/lib/systemd/user"
+  ];
 
   meta = with stdenv.lib; {
     description = "Lightweight and customizable notification daemon";
-    homepage = http://www.knopwob.org/dunst/;
+    homepage = https://dunst-project.org/;
     license = licenses.bsd3;
     # NOTE: 'unix' or even 'all' COULD work too, I'm not sure
     platforms = platforms.linux;
