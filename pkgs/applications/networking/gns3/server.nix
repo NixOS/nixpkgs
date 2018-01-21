fbe17703ab437cd294d1d97bb3bc5395cea74b64
Michael Weiss <dev.primeos@gmail.com>
2018-01-21 21:15:22 +0100

    gns3Packages.server{Stable,Preview}: Unlock the multidict version

--- pkgs/applications/networking/gns3/server.nix
+++ pkgs/applications/networking/gns3/server.nix
@@ -4,29 +4,6 @@
 
 let
   pythonPackages = python3Packages;
-  # TODO: Not sure if all these overwrites are really required...
-  # Upstream seems to have some reasons (bugs, incompatibilities) though.
-  multidict_3_1_3 =
-    (stdenv.lib.overrideDerivation pythonPackages.multidict (oldAttrs:
-      rec {
-        pname = "multidict";
-        version = "3.1.3";
-        name = "${pname}-${version}";
-        src = pythonPackages.fetchPypi {
-          inherit pname version;
-          sha256 = "04kdxh19m41c6vbshwk8jfbidsfsqn7mn0abvx09nyg78sh80pw7";
-        };
-        doInstallCheck = false;
-      }));
-  yarl = (stdenv.lib.overrideDerivation pythonPackages.yarl
-    (oldAttrs:
-      { propagatedBuildInputs = [ multidict_3_1_3 pythonPackages.idna ]; }));
-  aiohttp = (stdenv.lib.overrideDerivation pythonPackages.aiohttp
-    (oldAttrs:
-      rec {
-        propagatedBuildInputs = [ yarl multidict_3_1_3 ]
-          ++ (with pythonPackages; [ async-timeout chardet ]);
-      }));
   aiohttp-cors = (stdenv.lib.overrideDerivation pythonPackages.aiohttp-cors
     (oldAttrs:
       rec {
@@ -37,7 +14,6 @@ let
           inherit pname version;
           sha256 = "11b51mhr7wjfiikvj3nc5s8c7miin2zdhl3yrzcga4mbpkj892in";
         };
-        propagatedBuildInputs = [ aiohttp ];
       }));
 in pythonPackages.buildPythonPackage rec {
   name = "${pname}-${version}";
@@ -50,15 +26,12 @@ in pythonPackages.buildPythonPackage rec {
     sha256 = sha256Hash;
   };
 
-  propagatedBuildInputs = [ yarl aiohttp aiohttp-cors multidict_3_1_3 ]
+  propagatedBuildInputs = [ aiohttp-cors ]
     ++ (with pythonPackages; [
+      yarl aiohttp multidict
       jinja2 psutil zipstream raven jsonschema typing
       prompt_toolkit
     ]);
-
-  postPatch = ''
-    sed -i 's/yarl>=0.11,<0.12/yarl/g' requirements.txt
-  '';
 
   # Requires network access
   doCheck = false;
