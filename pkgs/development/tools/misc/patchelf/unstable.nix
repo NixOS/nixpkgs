7706f38a6cbf06c7484bb60d31be90618bd9c3e8
Herwig Hochleitner <herwig@bendlas.net>
2018-01-22 00:52:32 +0100

    patchelfUnstable: 0.10-pre-20160920 -> 0.10-pre-20180108

--- pkgs/development/tools/misc/patchelf/unstable.nix
+++ pkgs/development/tools/misc/patchelf/unstable.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchFromGitHub, autoreconfHook }:
 
 stdenv.mkDerivation rec {
-  name = "patchelf-0.10-pre-20160920";
+  name = "patchelf-0.10-pre-20180108";
 
   src = fetchFromGitHub {
     owner = "NixOS";
     repo = "patchelf";
-    rev = "327d80443672c397970738f9e216a7e86cbf3ad7";
-    sha256 = "0nghzywda4jrj70gvn4dnrzasafgdp0basj04wfir1smvsi047zr";
+    rev = "48452cf6b4ccba1c1f47a09f4284a253634ab7d1";
+    sha256 = "07vx75f2r1bh58qj4b6bl27v39srs2rfr1jrif7syspfhkn4qzw4";
   };
 
   setupHook = [ ./setup-hook.sh ];
