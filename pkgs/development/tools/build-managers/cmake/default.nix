ba6e0ae33df36306b065c7876d42ec483edac7cd
Vladimír Čunát <vcunat@gmail.com>
2018-01-21 11:59:12 +0100

    cmake: 3.9.6 -> 3.10.2
    Close #33435 (superseded).

--- pkgs/development/tools/build-managers/cmake/default.nix
+++ pkgs/development/tools/build-managers/cmake/default.nix
@@ -16,8 +16,8 @@ with stdenv.lib;
 
 let
   os = stdenv.lib.optionalString;
-  majorVersion = "3.9";
-  minorVersion = "6";
+  majorVersion = "3.10";
+  minorVersion = "2";
   version = "${majorVersion}.${minorVersion}";
 in
 
@@ -28,8 +28,8 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "${meta.homepage}files/v${majorVersion}/cmake-${version}.tar.gz";
-    # from https://cmake.org/files/v3.9/cmake-3.9.6-SHA-256.txt
-    sha256 = "7410851a783a41b521214ad987bb534a7e4a65e059651a2514e6ebfc8f46b218";
+    # from https://cmake.org/files/v3.10/cmake-3.10.2-SHA-256.txt
+    sha256 = "80d0faad4ab56de07aa21a7fc692c88c4ce6156d42b0579c6962004a70a3218b";
   };
 
   prePatch = optionalString (!useSharedLibraries) ''
