fd828d2d69c2a4c099990870ca157f559ba49afa
Luke Adams <luke.adams@belljar.io>
2018-01-20 20:12:48 -0600

    anbox: finalize android-image urls/shas

--- pkgs/applications/virtualization/anbox/default.nix
+++ pkgs/applications/virtualization/anbox/default.nix
@@ -27,10 +27,26 @@
 
 }:
 let
-  images = {
-    "armv7l-linux" = "https://build.anbox.io/android-images/2017/06/12/android_1_armhf.img";
-    "aarch64-linux" = "https://build.anbox.io/android-images/2017/08/04/android_1_arm64.img";
-    "x86_64-linux" = "https://build.anbox.io/android-images/2017/07/13/android_3_amd64.img";
+  imgroot = "https://build.anbox.io/android-images";
+  android_image = {
+    "armv7l-linux" = {
+      url = "${imgroot}/2017/06/12/android_1_armhf.img";
+      sha256 = "1za4q6vnj8wgphcqpvyq1r8jg6khz7v6b7h6ws1qkd5ljangf1w5";
+    };
+    "aarch64-linux" = {
+      url = "${imgroot}/2017/08/04/android_1_arm64.img";
+      sha256 = "02yvgpx7n0w0ya64y5c7bdxilaiqj9z3s682l5s54vzfnm5a2bg5";
+    };
+    "x86_64-linux" = {
+      url = "${imgroot}/2017/07/13/android_3_amd64.img";
+      sha256 = "1l22pisan3kfq4hwpizxc24f8lr3m5hd6k69nax10rki9ljypji0";
+    };
+  }.${stdenv.system};
+  imgDrv = stdenv.mkDerivation {
+    name = "anbox-image-${stdenv.system}-${version}";
+    src = fetchurl {
+      inherit (android_image) url sha256;
+    };
   };
   version = "2018-01-06";
 in
@@ -46,6 +62,7 @@ stdenv.mkDerivation {
 
   nativeBuildInputs = [ cmake pkgconfig ];
   buildInputs = [
+    imgDrv
     boost dbus dbus.lib dbus-cpp glib glm gmock gtest
     libcap libdrm libproperties-cpp libpthreadstubs libXdamage libXdmcp lxc
     mesa protobuf protobufc
