7c04c9b216dd7f35fe8942f5989696241f50370f
Tuomas Tynkkynen <tuomas@tuxera.com>
2018-01-23 22:17:11 +0200

    kernel: 4.14.14 -> 4.14.15
    (cherry picked from commit 1481937af27c923f729af8ab329d26108e579731)

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,16 +3,15 @@
 with stdenv.lib;
 
 import ./generic.nix (args // rec {
-  version = "4.14.14";
+  version = "4.14.15";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
-  modDirVersion = concatStrings (intersperse "." (take 3 (splitString "." "${version}.0")));
 
   # branchVersion needs to be x.y
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0jh46bfxfiw9kg36r4zvfrqlhnsqw8zikrw0b5am5qasnlp3d5lb";
+    sha256 = "0hk15qslkq15x53zkp70gnhdmjg5j9xigyykmig3g03gqsh97hzz";
   };
 } // (args.argsOverride or {}))
