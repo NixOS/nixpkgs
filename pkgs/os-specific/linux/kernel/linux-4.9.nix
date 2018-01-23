cee2f856766d100ed2c49c2d76dcadf507399b47
Tuomas Tynkkynen <tuomas@tuxera.com>
2018-01-23 22:17:55 +0200

    kernel: 4.9.77 -> 4.9.78
    (cherry picked from commit acf5b8e662f832d2bd6460685a10d9dd5e30c1bd)

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.9.77";
+  version = "4.9.78";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0p3hnfj0597vznvvjcb4ynciafnmvmnphkk6izcj67kgp4zvqabw";
+    sha256 = "1wy02y9nkwsi3bbcg5w4jzxp3f7aalylh1gh79bzi4knysz4zlfj";
   };
 } // (args.argsOverride or {}))
