cd2e740dde9541ad5f1d9efd93bcb5a967379ece
Tuomas Tynkkynen <tuomas@tuxera.com>
2018-01-24 16:43:16 +0200

    nixos/sd-image-aarch64.nix: Set avoid_warnings in RPi config.txt
    Also add some comments on the existing config settings as well.

--- nixos/modules/installer/cd-dvd/sd-image-aarch64.nix
+++ nixos/modules/installer/cd-dvd/sd-image-aarch64.nix
@@ -42,8 +42,17 @@ in
     populateBootCommands = let
       configTxt = pkgs.writeText "config.txt" ''
         kernel=u-boot-rpi3.bin
+
+        # Boot in 64-bit mode.
         arm_control=0x200
+
+        # U-Boot used to need this to work, regardless of whether UART is actually used or not.
+        # TODO: check when/if this can be removed.
         enable_uart=1
+
+        # Prevent the firmware from smashing the framebuffer setup done by the mainline kernel
+        # when attempting to show low-voltage or overtemperature warnings.
+        avoid_warnings=1
       '';
       in ''
         (cd ${pkgs.raspberrypifw}/share/raspberrypi/boot && cp bootcode.bin fixup*.dat start*.elf $NIX_BUILD_TOP/boot/)
