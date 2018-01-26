b22851023a4a83cb27bb5eb170684ccf5d02f5e6
Daiderd Jordan <daiderd@gmail.com>
2018-01-26 23:40:40 +0100

    linux-headers: fix Darwin cross build
    Carefully fake cc-version and cc-fullversion to avoid needing a compiler
    for the kernel itself to build the headers.

--- pkgs/os-specific/linux/kernel-headers/default.nix
+++ pkgs/os-specific/linux/kernel-headers/default.nix
@@ -1,12 +1,12 @@
 { stdenvNoCC, lib, buildPackages
-, hostPlatform
-, fetchurl, perl
+, buildPlatform, hostPlatform
+, fetchurl, fetchpatch, perl
 }:
 
 assert hostPlatform.isLinux;
 
 let
-  common = { version, sha256, patches ? null }: stdenvNoCC.mkDerivation {
+  common = { version, sha256, patches ? [] }: stdenvNoCC.mkDerivation {
     name = "linux-headers-${version}";
 
     src = fetchurl {
@@ -23,16 +23,35 @@ let
 
     extraIncludeDirs = lib.optional hostPlatform.isPowerPC ["ppc"];
 
-    # "patches" array defaults to 'null' to avoid changing hash
-    # and causing mass rebuild
     inherit patches;
 
-    buildPhase = ''
-      make mrproper headers_check SHELL=bash
+    hardeningDisable = lib.optional buildPlatform.isDarwin "format";
+
+    makeFlags = [
+      "SHELL=bash"
+      # Avoid use of runtime build->host compilers for checks
+      "cc-version:=9999"
+      "cc-fullversion:=999999"
+      # `$(..)` expanded by make alone
+      "HOSTCC:=$(BUILD_CC)"
+      "HOSTCXX:=$(BUILD_CXX)"
+    ];
+
+    # Skip clean on darwin, case-sensitivity issues.
+    buildPhase = lib.optionalString (!buildPlatform.isDarwin) ''
+      make mrproper $makeFlags
+    '' + ''
+      make headers_install $makeFlags
+    '';
+
+    # doCheck = !buildPlatform.isDarwin;
+
+    checkPhase = ''
+      make headers_check $makeFlags
     '';
 
     installPhase = ''
-      make INSTALL_HDR_PATH=$out headers_install
+      make headers_install INSTALL_HDR_PATH=$out $makeFlags
 
       # Some builds (e.g. KVM) want a kernel.release.
       mkdir -p $out/include/config
@@ -50,5 +69,13 @@ in {
   linuxHeaders = common {
     version = "4.15";
     sha256 = "0sd7l9n9h7vf9c6gd6ciji28hawda60yj0llh17my06m0s4lf9js";
+    patches = [
+       ./no-relocs.patch # for building x86 kernel headers on non-ELF platforms
+       ./no-dynamic-cc-version-check.patch # so we can use `stdenvNoCC`, see `makeFlags` above
+       #(fetchpatch { # For building on non-ELF platforms
+       #  url = https://github.com/richfelker/musl-cross-make/blame/master/patches/linux-4.4.10/0001-archscripts.diff;
+       #  sha256 = "0mdqa9w1p6cmli6976v4wi0sw9r4p5prkj7lzfd1877wk11c9c73";
+       #})
+    ];
   };
 }
