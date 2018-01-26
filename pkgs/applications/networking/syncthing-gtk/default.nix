33bfe21e6d5594985b60ac93345e52ba3ba32186
Jan Tojnar <jtojnar@gmail.com>
2018-01-26 06:10:54 +0100

    syncthing-gtk: another clean-up

--- pkgs/applications/networking/syncthing-gtk/default.nix
+++ pkgs/applications/networking/syncthing-gtk/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, libnotify, librsvg, psmisc, gtk3, syncthing, wrapGAppsHook, gnome3, python2Packages }:
+{ stdenv, fetchFromGitHub, libnotify, librsvg, psmisc, gtk3, substituteAll, syncthing, wrapGAppsHook, gnome3, python2Packages }:
 
 python2Packages.buildPythonApplication rec {
   version = "0.9.2.7";
@@ -15,9 +15,9 @@ python2Packages.buildPythonApplication rec {
 
   buildInputs = [
     gtk3 (librsvg.override { enableIntrospection = true; })
-    libnotify psmisc
+    libnotify
     # Schemas with proxy configuration
-    syncthing gnome3.gsettings_desktop_schemas
+    gnome3.gsettings_desktop_schemas
   ];
 
   propagatedBuildInputs = with python2Packages; [
@@ -26,23 +26,27 @@ python2Packages.buildPythonApplication rec {
 
   patches = [
     ./disable-syncthing-binary-configuration.patch
+    (substituteAll {
+      src = ./paths.patch;
+      killall = "${psmisc}/bin/killall";
+      syncthing = "${syncthing}/bin/syncthing";
+    })
   ];
 
   postPatch = ''
     substituteInPlace setup.py --replace "version = get_version()" "version = '${version}'"
     substituteInPlace scripts/syncthing-gtk --replace "/usr/share" "$out/share"
     substituteInPlace syncthing_gtk/app.py --replace "/usr/share" "$out/share"
-    substituteInPlace syncthing_gtk/configuration.py --replace "/usr/bin/syncthing" "${syncthing}/bin/syncthing"
     substituteInPlace syncthing_gtk/uisettingsdialog.py --replace "/usr/share" "$out/share"
     substituteInPlace syncthing_gtk/wizard.py --replace "/usr/share" "$out/share"
     substituteInPlace syncthing-gtk.desktop --replace "/usr/bin/syncthing-gtk" "$out/bin/syncthing-gtk"
   '';
 
   meta = with stdenv.lib; {
-    description = " GTK3 & python based GUI for Syncthing ";
+    description = "GTK3 & python based GUI for Syncthing";
     maintainers = with maintainers; [ ];
     platforms = syncthing.meta.platforms;
-    homepage = "https://github.com/syncthing/syncthing-gtk";
+    homepage = https://github.com/syncthing/syncthing-gtk;
     license = licenses.gpl2;
   };
 }
