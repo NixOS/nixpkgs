3979a20d7489efb7f12b2f477e92d4596b9bab31
rnhmjoj <rnhmjoj@inventati.org>
2018-01-26 23:17:38 +0100

    monero: build libmonero-wallet

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16118,7 +16118,7 @@ with pkgs;
 
   mod-distortion = callPackage ../applications/audio/mod-distortion { };
 
-  monero = callPackage ../applications/misc/monero { };
+  monero = callPackage ../applications/altcoins/monero { };
 
   xmr-stak = callPackage ../applications/misc/xmr-stak {
     hwloc = hwloc-nox;
