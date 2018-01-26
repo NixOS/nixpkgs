f8776eabdd07d4a37c9aa4bb817554371385f571
Peter Simons <simons@cryp.to>
2018-01-26 11:04:13 +0100

    cabal2nix: build with latest version of hpack

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -605,7 +605,8 @@ self: super: {
     haskell-src-exts = self.haskell-src-exts_1_20_1;
   };
 
-  # Needs newer version of its dependencies than we have in LTS-10.x.
+  # Need newer versions of their dependencies than the ones we have in LTS-10.x.
+  cabal2nix = super.cabal2nix.override { hpack = self.hpack_0_22_0; };
   hlint = super.hlint.overrideScope (self: super: { haskell-src-exts = self.haskell-src-exts_1_20_1; });
 
   # https://github.com/bos/configurator/issues/22
