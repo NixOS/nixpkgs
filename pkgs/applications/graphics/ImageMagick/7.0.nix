c9f94405ed5cd4381cee34de1a43ccbe1711c958
Franz Pletz <fpletz@fnordicwalking.de>
2018-01-28 17:48:45 +0100

    imagemagick7: 7.0.7-21 -> 7.0.7-22

--- pkgs/applications/graphics/ImageMagick/7.0.nix
+++ pkgs/applications/graphics/ImageMagick/7.0.nix
@@ -14,8 +14,8 @@ let
     else throw "ImageMagick is not supported on this platform.";
 
   cfg = {
-    version = "7.0.7-21";
-    sha256 = "0680dbg77gcyb3g4n22z5mp7c8mg0jpkqb0g4nj7d7r78nl869rv";
+    version = "7.0.7-22";
+    sha256 = "12c48cfhc2a3zvhgxdywrfy8b4m2vx85vn2qj69iyni5x849xpj9";
     patches = [];
   };
 in
