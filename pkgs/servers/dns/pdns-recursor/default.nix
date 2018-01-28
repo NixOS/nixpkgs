6d6aa676e62c86c7933d41eba251398ff6753ff2
rnhmjoj <rnhmjoj@inventati.org>
2018-01-29 00:57:05 +0100

    pdns-recursor: 4.0.8 -> 4.1.1
    (cherry picked from commit 8853d4408b7b0bdd844908d4ac0d5e90070428df)

--- pkgs/servers/dns/pdns-recursor/default.nix
+++ pkgs/servers/dns/pdns-recursor/default.nix
@@ -1,27 +1,25 @@
 { stdenv, fetchurl, pkgconfig, boost
 , openssl, systemd, lua, luajit, protobuf
-, enableLua ? false
 , enableProtoBuf ? false
 }:
-
-assert enableLua      -> lua != null && luajit != null;
 assert enableProtoBuf -> protobuf != null;
 
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "pdns-recursor-${version}";
-  version = "4.0.8";
+  version = "4.1.1";
 
   src = fetchurl {
     url = "https://downloads.powerdns.com/releases/pdns-recursor-${version}.tar.bz2";
-    sha256 = "04v5y6mfdhn8ikigqmm3k5k0zz5l8d3k1a7ih464n1161q7z0vww";
+    sha256 = "0srrw726qpwg69v75dwbxab9hk73x1wia4rcnmf7g5qr2k3h7swg";
   };
 
+  nativeBuildInputs = [ pkgconfig ];
   buildInputs = [
-    boost openssl pkgconfig systemd
-  ] ++ optional enableLua [ lua luajit ]
-    ++ optional enableProtoBuf protobuf;
+    boost openssl systemd
+    lua luajit
+  ] ++ optional enableProtoBuf protobuf;
 
   configureFlags = [
     "--enable-reproducible"
