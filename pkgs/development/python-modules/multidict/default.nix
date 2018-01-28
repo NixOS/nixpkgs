9d8d6df50cedf30c2014d122e346d4083e8c5031
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-01-28 14:08:30 +0100

    python3Packages.multidict: 4.0.0 -> 4.1.0

--- pkgs/development/python-modules/multidict/default.nix
+++ pkgs/development/python-modules/multidict/default.nix
@@ -6,15 +6,13 @@
 , isPy3k
 }:
 
-let
-
-in buildPythonPackage rec {
+buildPythonPackage rec {
   pname = "multidict";
-  version = "4.0.0";
+  version = "4.1.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0y0pg3r9hlknny0zwg906wz81h8in6lgvnpbmzvl911bmnrqc95p";
+    sha256 = "0liazqlyk2nmr82nhiw2z72j7bjqxaisifkj476msw140d4i4i7v";
   };
 
   buildInputs = [ cython ];
@@ -22,9 +20,10 @@ in buildPythonPackage rec {
 
   disabled = !isPy3k;
 
-  meta = {
+  meta = with lib; {
     description = "Multidict implementation";
     homepage = https://github.com/aio-libs/multidict/;
-    license = lib.licenses.asl20;
+    license = licenses.asl20;
+    maintainers = with maintainers; [ dotlambda ];
   };
 }
