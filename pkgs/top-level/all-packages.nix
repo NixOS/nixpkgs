80df6205ede8f996023972334e52f50bd298f7da
Ivan Solyankin <vanzef@gmail.com>
2018-01-30 14:47:16 +0300

    libkkc: init at 0.3.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1661,6 +1661,10 @@ with pkgs;
     inherit (pythonPackages) marisa;
   };
 
+  libkkc = callPackage ../tools/inputmethods/libkkc {
+    inherit (gnome3) libgee;
+  };
+
   ibus = callPackage ../tools/inputmethods/ibus {
     inherit (gnome3) dconf gconf glib;
   };
