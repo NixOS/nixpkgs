b1aeda330f469a0e02f72b6802513884118e1dba
Stefan Frijters <sfrijters@gmail.com>
2018-01-30 08:18:50 +0100

    Disable check

--- pkgs/development/tools/profiling/pyprof2calltree/default.nix
+++ pkgs/development/tools/profiling/pyprof2calltree/default.nix
@@ -9,11 +9,13 @@ buildPythonApplication rec {
     sha256 = "38a0774f7716d5303d4c57ff89ec24258154942666e4404558f6ac10f8bb1e52";
   };
 
-  checkInputs = [ setuptools ];
-  checkPhase = ''
-    ${python.interpreter} -m unittest discover -s test
-  '';
-#  doCheck = false;
+  # Check disabled because of upstream packaging bug
+  # https://github.com/NixOS/nixpkgs/pull/34379#discussion_r164498796
+  # checkInputs = [ setuptools ];
+  # checkPhase = ''
+  #   ${python.interpreter} -m unittest discover -s test
+  # '';
+  doCheck = false;
 
   meta = with lib; {
     description = "Help visualize profiling data from cProfile with kcachegrind and qcachegrind";
