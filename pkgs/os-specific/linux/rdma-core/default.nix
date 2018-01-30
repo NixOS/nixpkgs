c208456d3aaf9b290a477c6ba415ef38556f3443
Jörg Thalheim <joerg@thalheim.io>
2018-01-30 23:01:25 +0000

    rdma-core: set correct RUNDIR and SHAREDSTATEDIR

--- pkgs/os-specific/linux/rdma-core/default.nix
+++ pkgs/os-specific/linux/rdma-core/default.nix
@@ -18,9 +18,13 @@ in stdenv.mkDerivation {
   nativeBuildInputs = [ cmake pkgconfig ];
   buildInputs = [ libnl ethtool nettools libudev python perl ];
 
+  cmakeFlags = [
+    "-DCMAKE_INSTALL_RUNDIR=/run"
+    "-DCMAKE_INSTALL_SHAREDSTATEDIR=/var/lib"
+  ];
+
   postPatch = ''
     substituteInPlace providers/rxe/rxe_cfg.in \
-      --replace '@CMAKE_INSTALL_FULL_SHAREDSTATEDIR@' '/run' \
       --replace ethtool "${ethtool}/bin/ethtool" \
       --replace ifconfig "${nettools}/bin/ifconfig"
   '';
