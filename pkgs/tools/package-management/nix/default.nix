ef351646bad830097a5dde943337432754119f11
Eelco Dolstra <edolstra@gmail.com>
2018-01-31 22:22:47 +0100

    Revert "Restore "nixUnstable: rename to nix-unstable""
    This reverts commit 695027f61c702ea0de6baa3122b282d672fede09.
    
    We really can't have "nix-env -i nix" *not* upgrade nixUnstable to a
    newer nixStable. For instance, it would cause "nix upgrade-nix" to
    produce a user environment with collisions.

--- pkgs/tools/package-management/nix/default.nix
+++ pkgs/tools/package-management/nix/default.nix
@@ -160,7 +160,7 @@ in rec {
   }) // { perl-bindings = nixStable; };
 
   nixUnstable = (lib.lowPrio (common rec {
-    name = "nix-unstable-1.12${suffix}";
+    name = "nix-1.12${suffix}";
     suffix = "pre5873_b76e282d";
     src = fetchFromGitHub {
       owner = "NixOS";
