27f5262cf24cc3b3bd8bc3ff411d1e249ef54978
Tim Steinbach <tim@nequissimus.com>
2018-01-31 09:05:54 -0500

    linux: 4.14.15 -> 4.14.16

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,15 +3,13 @@
 with stdenv.lib;
 
 import ./generic.nix (args // rec {
-  version = "4.14.15";
-
-  # modDirVersion needs to be x.y.z, will automatically add .0 if needed
+  version = "4.14.16";
 
   # branchVersion needs to be x.y
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0hk15qslkq15x53zkp70gnhdmjg5j9xigyykmig3g03gqsh97hzz";
+    sha256 = "095c2cjmjfsgnmml4f3lzc0pbhjy8nv8w07rywgpp5s5494dn2q7";
   };
 } // (args.argsOverride or {}))
