NixOS/nixpkgs

commit 561b9ca8ac37509213369797fc56437cda2961ca

moreutils: 0.61 -> 0.62

pkgs/tools/misc/moreutils/default.nix
@@ -3,12 +3,12 @@
 with stdenv.lib;
 stdenv.mkDerivation rec {
   name = "moreutils-${version}";
-  version = "0.61";
+  version = "0.62";
 
   src = fetchgit {
     url = "git://git.joeyh.name/moreutils";
     rev = "refs/tags/${version}";
-    sha256 = "1qvwlq0a2zs7qkjqc9c842979axkjfdr7nic1gsm4zc6jd72y7pr";
+    sha256 = "0sk7rgqsqbdwr69mh7y4v9lv4v0nfmsrqgvbpy2gvy82snhfzar"2;
   };
 
   preBuild = ''

commit 9b55f7b69c8fe0aa96ed84a8c9b8cfd23abb05bd

moreutils: Fix eval
Was broken in 561b9ca8ac37509213369797fc56437cda2961ca.

pkgs/tools/misc/moreutils/default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
   src = fetchgit {
     url = "git://git.joeyh.name/moreutils";
     rev = "refs/tags/${version}";
-    sha256 = "0sk7rgqsqbdwr69mh7y4v9lv4v0nfmsrqgvbpy2gvy82snhfzar"2;
+    sha256 = "0sk7rgqsqbdwr69mh7y4v9lv4v0nfmsrqgvbpy2gvy82snhfzar2";
   };
 
   preBuild = ''