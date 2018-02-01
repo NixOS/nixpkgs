f55fffc1cf6b811be742bd67800f6d02ef795918
Michael Weiss <dev.primeos@gmail.com>
2018-02-01 10:31:32 +0100

    android-studio-preview: 3.1.0.8 -> 3.1.0.9

--- pkgs/applications/editors/android-studio/default.nix
+++ pkgs/applications/editors/android-studio/default.nix
@@ -27,9 +27,9 @@ in rec {
 
   preview = mkStudio {
     pname = "android-studio-preview";
-    version = "3.1.0.8"; # "Android Studio 3.1 Canary 9"
-    build = "173.4559767";
-    sha256Hash = "0wy3bqd4wvvcwlqcv06mwlqgc119pjpc102ix3yacqvki9qyi1r0";
+    version = "3.1.0.9"; # "Android Studio 3.1 Beta 1"
+    build = "173.4567466";
+    sha256Hash = "01c6a46pk5zbhwk2w038nm68fkx86nafiw1v2i5rdr93mxvx9cag";
 
     meta = stable.meta // {
       description = "The Official IDE for Android (preview version)";
