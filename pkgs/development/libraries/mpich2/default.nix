ff8b077071d50198e82f2a9c11eb6a7aa0df339f
Markus Kowalewski <markus.kowalewski@gmail.com>
2018-02-02 10:35:42 -0800

    mpich2: 3.2 -> 3.2.1

--- pkgs/development/libraries/mpich2/default.nix
+++ pkgs/development/libraries/mpich2/default.nix
@@ -1,17 +1,33 @@
-{ stdenv, fetchurl, python, perl, gfortran }:
+{ stdenv, fetchurl, python, perl, gfortran
+, slurm, openssh, hwloc
+} :
 
 stdenv.mkDerivation  rec {
   name = "mpich-${version}";
-  version = "3.2";
+  version = "3.2.1";
 
   src = fetchurl {
-    url = "http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz";
-    sha256 = "0bvvk4n9g4rmrncrgs9jnkcfh142i65wli5qp1akn9kwab1q80z6";
+    url = "http://www.mpich.org/static/downloads/${version}/mpich-${version}.tar.gz";
+    sha256 = "1w9h4g7d46d9l5jbcyfxpaqzpjrc5hyvr9d0ns7278psxpr3pdax";
   };
 
-  configureFlags = "--enable-shared --enable-sharedlib";
+  configureFlags = [
+    "--enable-shared"
+    "--enable-sharedlib"
+  ];
 
-  buildInputs = [ perl gfortran ];
+  buildInputs = [ perl gfortran slurm openssh hwloc ];
+
+  doCheck = true;
+
+  preFixup = ''
+    # /tmp/nix-build... ends up in the RPATH, fix it manually
+    for entry in $out/bin/mpichversion $out/bin/mpivars; do
+      echo "fix rpath: $entry"
+      patchelf --set-rpath "$out/lib" $entry
+    done
+  '';
+
 
   meta = {
     description = "Implementation of the Message Passing Interface (MPI) standard";
