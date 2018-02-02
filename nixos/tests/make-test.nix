2a09b4275c353bde6ee72d126cd0e99834dcf66e
aszlig <aszlig@nix.build>
2018-02-02 17:44:08 +0100

    nixos/tests/make-test.nix: Fix eval error
    Regression introduced by 943592f69850fd07dd2422da062b1c1ebc45974d.
    
    The lib attribute isn't in scope here, so we need to use pkgs.lib
    instead for isFunction.
    
    Signed-off-by: aszlig <aszlig@nix.build>
    Cc: @shlevy

--- nixos/tests/make-test.nix
+++ nixos/tests/make-test.nix
@@ -2,4 +2,4 @@ f: { system ? builtins.currentSystem, ... } @ args:
 
 with import ../lib/testing.nix { inherit system; };
 
-makeTest (if lib.isFunction f then f (args // { inherit pkgs; inherit (pkgs) lib; }) else f)
+makeTest (if pkgs.lib.isFunction f then f (args // { inherit pkgs; inherit (pkgs) lib; }) else f)
