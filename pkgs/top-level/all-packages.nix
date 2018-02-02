04769e898f5f453e4a59cda3a908b3fc334705fd
Elmar Athmer <elmar@athmer.org>
2018-02-03 00:36:02 +0100

    hcloud: init at 1.3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7603,6 +7603,8 @@ with pkgs;
     guile = guile_2_0;
   };
 
+  hcloud = callPackage ../development/tools/hcloud { };
+
   help2man = callPackage ../development/tools/misc/help2man {
     inherit (perlPackages) LocaleGettext;
   };
