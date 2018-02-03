7afd65aed4e6cec482e0f536e1fdb7a88bfe4646
Frederik Rietdijk <fridh@fridh.nl>
2018-02-03 14:30:17 +0100

    python.pkgs.packet-python: 1.33 -> 1.37.1

--- pkgs/development/python-modules/packet-python/default.nix
+++ pkgs/development/python-modules/packet-python/default.nix
@@ -3,20 +3,28 @@
 , fetchPypi
 , requests
 , python
+, fetchpatch
 }:
 
 buildPythonPackage rec {
   pname = "packet-python";
-  version = "1.33";
+  version = "1.37.1";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0bmvfmvjm8jx0y8sv0jf5mhv0h3v8idx0sc5myxs7ig200584dd3";
+    sha256 = "316941d2473c0f42ac17ac89e9aa63a023bb96f35cf8eafe9e091ea424892778";
   };
   propagatedBuildInputs = [ requests ];
 
   checkPhase = ''
     ${python.interpreter} -m unittest discover -s test
   '';
+
+  patches = [
+    (fetchpatch {
+      url = https://github.com/packethost/packet-python/commit/361ad0c60d0bfce2a992eefd17e917f9dcf36400.patch;
+      sha256 = "1cmzyq0302y4cqmim6arnvn8n620qysq458g2w5aq4zj1vz1q9g1";
+    })
+  ];
 
   # Not all test files are included in archive
   doCheck = false;
