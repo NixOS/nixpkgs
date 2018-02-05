f2f57b0c1a7ed18c357ffdea507bc8a666d53ede
dywedir <dywedir@protonmail.ch>
2018-02-05 16:15:35 +0200

    linux_testing_bcachefs: 4.11.2017.08.23 -> 4.15.2018.02.05

--- pkgs/os-specific/linux/kernel/linux-testing-bcachefs.nix
+++ pkgs/os-specific/linux/kernel/linux-testing-bcachefs.nix
@@ -1,15 +1,15 @@
-{ stdenv, hostPlatform, fetchgit, perl, buildLinux, ... } @ args:
+{ stdenv, buildPackages, hostPlatform, fetchgit, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.11.2017.08.23";
-  modDirVersion = "4.11.0";
+  version = "4.15.2018.02.05";
+  modDirVersion = "4.15.0";
   extraMeta.branch = "master";
   extraMeta.maintainers = [ stdenv.lib.maintainers.davidak ];
 
   src = fetchgit {
     url = "https://evilpiepirate.org/git/bcachefs.git";
-    rev = "fb8082a13d49397346a04ce4d3904569b0287738";
-    sha256 = "18csg2zb4lnhid27h5w95j3g8np29m8y3zfpfgjl1jr2jks64kid";
+    rev = "67109c13b64a806473f6fb126d3ea99c3a79592c";
+    sha256 = "0dpacpnps3gzd4jwppm54gqgq6vw3mq2sgd78qmpg6rr2dw848mb";
   };
 
   extraConfig = ''
@@ -20,4 +20,3 @@ import ./generic.nix (args // rec {
   extraMeta.hydraPlatforms = [];
 
 } // (args.argsOverride or {}))
-
