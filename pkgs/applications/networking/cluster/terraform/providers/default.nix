b2e7bb5ee80303ec6d0671085f5da424014a601f
Raitis <wimuan@gmail.com>
2018-02-06 21:09:22 +0000

    terraform: add the version component to terraform provider paths (#34497)
    Terraform checks the provider versions, but this breaks if the versions
    are not provided, as they can be, if the plugins are provided by nix.

--- pkgs/applications/networking/cluster/terraform/providers/default.nix
+++ pkgs/applications/networking/cluster/terraform/providers/default.nix
@@ -11,6 +11,10 @@ let
         inherit owner repo sha256;
         rev = "v${version}";
       };
+
+      # Terraform allow checking the provider versions, but this breaks
+      # if the versions are not provided via file paths.
+      postBuild = "mv go/bin/${repo}{,_v${version}}";
     };
 
   maybeDrv = name: data:
