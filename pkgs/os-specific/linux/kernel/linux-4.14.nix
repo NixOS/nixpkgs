e90bd5fa0d078e91d29b3a63a660943f3abb1461
Tim Steinbach <tim@nequissimus.com>
2018-02-07 19:36:21 -0500

    linux: 4.14.17 -> 4.14.18
    (cherry picked from commit 0b43d5a9cf99dec2cccfd74c13487ecf0635f731)

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,13 +3,13 @@
 with stdenv.lib;
 
 import ./generic.nix (args // rec {
-  version = "4.14.17";
+  version = "4.14.18";
 
   # branchVersion needs to be x.y
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0jqa86bnnlzv0r0bvzvmbj1c89a5m64zrjfvfrjlwg3vy63r9ii7";
+    sha256 = "0m73kz9jg6mylgql0zzypm76g6x7m3bq7dklivhkm4ldqg0r8sl6";
   };
 } // (args.argsOverride or {}))
