a90f841769b0478afd39025f32e48038b8a165c8
Will Dietz <w@wdtz.org>
2018-02-07 20:53:49 -0600

    heaptrack: Add dep on kdiagram for chart goodness

--- pkgs/development/tools/profiling/heaptrack/default.nix
+++ pkgs/development/tools/profiling/heaptrack/default.nix
@@ -1,7 +1,7 @@
 {
   stdenv, fetchFromGitHub, cmake, extra-cmake-modules,
   zlib, boost, libunwind, elfutils, sparsehash,
-  qtbase, kio, kitemmodels, threadweaver, kconfigwidgets, kcoreaddons,
+  qtbase, kio, kitemmodels, threadweaver, kconfigwidgets, kcoreaddons, kdiagram
 }:
 
 stdenv.mkDerivation rec {
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ cmake extra-cmake-modules ];
   buildInputs = [
     zlib boost libunwind elfutils sparsehash
-    qtbase kio kitemmodels threadweaver kconfigwidgets kcoreaddons
+    qtbase kio kitemmodels threadweaver kconfigwidgets kcoreaddons kdiagram
   ];
 
   meta = with stdenv.lib; {
