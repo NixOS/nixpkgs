0a659c4f75d5e005f8d8fb560903a7ec8ea0564e
Dan Peebles <pumpkin@me.com>
2018-02-08 16:48:35 -0500

    treewide: s/pkgs.fedoraproject.org/src.fedoraproject.org/
    Upstream killed the pkgs server but src continues to serve up the exact
    same content, so we can just point there and all hashes should be unchanged.
    
    (morally a cherry-pick of dfd300c81dfb7292d26cfbce3df9717ea408e410)

--- pkgs/os-specific/linux/hwdata/default.nix
+++ pkgs/os-specific/linux/hwdata/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
   version = "0.300";
 
   src = fetchurl {
-    url = "http://pkgs.fedoraproject.org/repo/pkgs/hwdata/v0.300.tar.gz/sha512/34294fcf65c3cb17c19d625732d1656ec1992dde254a68ee35681ad2f310bc05028a85889efa2c1d1e8a2d10885ccc00185475a00f6f2fb82d07e2349e604a51/v0.300.tar.gz";
+    url = "http://src.fedoraproject.org/repo/pkgs/hwdata/v0.300.tar.gz/sha512/34294fcf65c3cb17c19d625732d1656ec1992dde254a68ee35681ad2f310bc05028a85889efa2c1d1e8a2d10885ccc00185475a00f6f2fb82d07e2349e604a51/v0.300.tar.gz";
     sha256 = "03xlj05qyixhnsybq1qnr7j5q2nvirs4jxpgg4sbw8swsqj3dgqi";
   };
 
