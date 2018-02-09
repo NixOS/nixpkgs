197d9a8d901ee51e64f8f5947a7be9f2f4c30789
Sam Parkinson <sam@sam.today>
2018-02-09 22:23:37 +1100

    granite: refactor package
    * move build time dependencies into nativeBuildInputs
    * convert cmakeFlags to an array
    * use `with` in the meta section
    * add `ninja`

--- pkgs/development/libraries/granite/default.nix
+++ pkgs/development/libraries/granite/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, perl, cmake, vala, pkgconfig, gobjectIntrospection, glib, gtk3, gnome3, gettext }:
+{ stdenv, fetchFromGitHub, perl, cmake, ninja, vala, pkgconfig, gobjectIntrospection, glib, gtk3, gnome3, gettext }:
 
 stdenv.mkDerivation rec {
   name = "granite-${version}";
@@ -11,16 +11,32 @@ stdenv.mkDerivation rec {
     sha256 = "15l8z1jkqhvappnr8jww27lfy3dwqybgsxk5iccyvnvzpjdh2s0h";
   };
 
-  cmakeFlags = "-DINTROSPECTION_GIRDIR=share/gir-1.0/ -DINTROSPECTION_TYPELIBDIR=lib/girepository-1.0";
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [perl cmake vala gobjectIntrospection glib gtk3 gnome3.libgee gettext];
+  cmakeFlags = [
+    "-DINTROSPECTION_GIRDIR=share/gir-1.0/"
+    "-DINTROSPECTION_TYPELIBDIR=lib/girepository-1.0"
+  ];
 
-  meta = {
+  nativeBuildInputs = [
+    vala
+    pkgconfig
+    cmake
+    ninja
+    perl
+    gettext
+    gobjectIntrospection
+  ];
+  buildInputs = [
+    glib
+    gtk3
+    gnome3.libgee
+  ];
+
+  meta = with stdenv.lib; {
     description = "An extension to GTK+ used by elementary OS";
     longDescription = "An extension to GTK+ that provides several useful widgets and classes to ease application development. Designed for elementary OS.";
     homepage = https://github.com/elementary/granite;
-    license = stdenv.lib.licenses.lgpl3;
-    platforms = stdenv.lib.platforms.linux;
-    maintainers = [ stdenv.lib.maintainers.vozz ];
+    license = licenses.lgpl3;
+    platforms = platforms.linux;
+    maintainers = [ maintainers.vozz ];
   };
 }
