ebf394be85b4cd34f68c9906c50e45e21ef88db7
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-02-10 13:30:51 +0800

    racket-minimal: add description

--- pkgs/development/interpreters/racket/minimal.nix
+++ pkgs/development/interpreters/racket/minimal.nix
@@ -7,4 +7,11 @@ racket.overrideAttrs (oldAttrs: rec {
     inherit name;
     sha256 = "0c565jy2y3gjl5lncd5adjsrj8c24p4i062kphv26ni5q1nn5ip5";
   };
+
+  description = "Racket without bundled packages, such as Mr Racket.";
+  longDescription = ''The essential package racket-libs is included,
+    as well as libraries that live in collections. In particular, raco
+    and the pkg library are still bundled.
+  '';
+
 })
