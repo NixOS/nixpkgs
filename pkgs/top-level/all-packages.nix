2564e75a341db90ac1ac3fcef9f1a170b4864812
Andreas Rammhold <andreas@rammhold.de>
2018-02-10 20:06:24 +0100

    syncthing013: removed ancient version
    Removal as announced by Peter Hoege on the ML [1]
    
    [1] https://groups.google.com/forum/#!msg/nix-devel/CR1eldka5SU/eO-xyV4UAgAJ

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17327,8 +17327,6 @@ with pkgs;
 
   syncthing = callPackage ../applications/networking/syncthing { };
 
-  syncthing013 = callPackage ../applications/networking/syncthing013 { };
-
   syncthing-gtk = python2Packages.callPackage ../applications/networking/syncthing-gtk { };
 
   syncthing-inotify = callPackage ../applications/networking/syncthing/inotify.nix { };
