941acb4fe6ac0b1fa8f95d8ce7d9b8fa844dd9ba
Andrew Dunham <andrew@du.nham.ca>
2018-02-11 00:49:46 -0800

    syncthing: Also build and install discovery and relay servers

--- pkgs/applications/networking/syncthing/default.nix
+++ pkgs/applications/networking/syncthing/default.nix
@@ -11,19 +11,27 @@ stdenv.mkDerivation rec {
     sha256 = "1gdkx6lbzmdz2hqc9slbq41rwgkxmdisnj0iywx4mppmc2b4v6wh";
   };
 
+  outputs = [ "out" "server" ];
+
   buildInputs = [ go removeReferencesTo ];
 
   buildPhase = ''
     # Syncthing expects that it is checked out in $GOPATH, if that variable is
     # set.  Since this isn't true when we're fetching source, we can explicitly
     # unset it and force Syncthing to set up a temporary one for us.
-    env GOPATH= go run build.go -no-upgrade -version v${version} build
+    for bin in syncthing stdiscosrv strelaysrv; do
+      env GOPATH= go run build.go -no-upgrade -version v${version} build $bin
+    done
   '';
 
   installPhase = ''
     mkdir -p $out/lib/systemd/{system,user}
+    mkdir -p $server/lib/systemd/system $server/bin
 
     install -Dm755 syncthing $out/bin/syncthing
+    for sbin in stdiscosrv strelaysrv; do
+      install -Dm755 $sbin $server/bin/$sbin
+    done
 
     # This installs man pages in the correct directory according to the suffix
     # on the filename
@@ -46,6 +54,10 @@ stdenv.mkDerivation rec {
     substitute etc/linux-systemd/user/syncthing.service \
                $out/lib/systemd/user/syncthing.service \
                --replace /usr/bin/syncthing $out/bin/syncthing
+
+    substitute cmd/strelaysrv/etc/linux-systemd/strelaysrv.service \
+               $server/lib/systemd/system/strelaysrv.service \
+               --replace /usr/bin/strelaysrv $server/bin/strelaysrv
   '';
 
   preFixup = ''
