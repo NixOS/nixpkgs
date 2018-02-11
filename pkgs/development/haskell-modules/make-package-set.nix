5e4a52cc0ed184dc866917db72a14ef76023e161
Will Fancher <elvishjerricco@gmail.com>
2018-02-11 18:33:20 -0500

    Use static cabal2nix in callCabal2nix
    (cherry picked from commit 627444cfc2354d79ade0a59b55e8182729e51e16)
    
    The use of this function is disallowed in nixpkgs, and purely there for
    the convenience of downstream users. This improves closure size without
    any loss of functionality.

--- pkgs/development/haskell-modules/make-package-set.nix
+++ pkgs/development/haskell-modules/make-package-set.nix
@@ -111,7 +111,7 @@ let
       sha256Arg = if isNull sha256 then "--sha256=" else ''--sha256="${sha256}"'';
     in pkgs.stdenv.mkDerivation {
       name = "cabal2nix-${name}";
-      buildInputs = [ pkgs.haskellPackages.cabal2nix ];
+      nativeBuildInputs = [ pkgs.cabal2nix ];
       preferLocalBuild = true;
       phases = ["installPhase"];
       LANG = "en_US.UTF-8";
