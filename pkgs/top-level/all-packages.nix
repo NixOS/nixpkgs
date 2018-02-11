b5c8eb8280bf38858cdf0f2f94bca19a62e41e51
Andrew Dunham <andrew@du.nham.ca>
2018-02-10 23:35:05 -0800

    massren: init at 1.5.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1245,6 +1245,8 @@ with pkgs;
 
   masscan = callPackage ../tools/security/masscan { };
 
+  massren = callPackage ../tools/misc/massren { };
+
   meritous = callPackage ../games/meritous { };
 
   meson = callPackage ../development/tools/build-managers/meson { };
