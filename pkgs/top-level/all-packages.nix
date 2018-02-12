9f6f25339326a86ae7cea22eeebc0eee4598f58b
Bart Brouns <bart@magnetophon.nl>
2018-02-12 20:54:54 +0100

    faust: 2.5.10 -> 2.5.21

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19822,7 +19822,7 @@ with pkgs;
   faust1 = callPackage ../applications/audio/faust/faust1.nix { };
 
   faust2 = callPackage ../applications/audio/faust/faust2.nix {
-    llvm = llvm_4;
+    llvm = llvm_5;
   };
 
   faust2alqt = callPackage ../applications/audio/faust/faust2alqt.nix { };
