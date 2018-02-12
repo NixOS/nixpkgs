8a0f4f54a03f7a4b422db6025e9d35cd9519d1a9
Profpatsch <mail@profpatsch.de>
2018-02-12 06:12:11 +0100

    jid: remove package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2957,8 +2957,6 @@ with pkgs;
 
   jhead = callPackage ../tools/graphics/jhead { };
 
-  jid = callPackage ../development/tools/jid { };
-
   jing = self.jing-trang;
   jing-trang = callPackage ../tools/text/xml/jing-trang { };
 
