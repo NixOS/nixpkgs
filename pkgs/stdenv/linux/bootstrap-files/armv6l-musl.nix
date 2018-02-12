4619f8e06d33b72a424cdc0a603953d275c62220
Will Dietz <w@wdtz.org>
2018-02-12 17:33:21 -0600

    new musl bootstrap URL's, much smaller.  rebuild all the things.

--- pkgs/stdenv/linux/bootstrap-files/armv6l-musl.nix
+++ pkgs/stdenv/linux/bootstrap-files/armv6l-musl.nix
@@ -1,11 +1,11 @@
 {
   busybox = import <nix/fetchurl.nix> {
-    url = https://wdtz.org/files/m4n9p43b7q8k1wqbpw264947rm70mj1z-stdenv-bootstrap-tools-armv6l-unknown-linux-musleabihf/on-server/busybox;
+    url = https://wdtz.org/files/xmz441m69qrlfdw47l2k10zf87fsya6r-stdenv-bootstrap-tools-armv6l-unknown-linux-musleabihf/on-server/busybox;
     sha256 = "01d0hp1xgrriiy9w0sd9vbqzwxnpwiyah80pi4vrpcmbwji36j1i";
     executable = true;
   };
   bootstrapTools = import <nix/fetchurl.nix> {
-    url = https://wdtz.org/files/m4n9p43b7q8k1wqbpw264947rm70mj1z-stdenv-bootstrap-tools-armv6l-unknown-linux-musleabihf/on-server/bootstrap-tools.tar.xz;
-    sha256 = "1n9wm08fg99wa1q3ngim6n6bg7kxlkzx2v7fqw013rb3d5drjwrq";
+    url = https://wdtz.org/files/xmz441m69qrlfdw47l2k10zf87fsya6r-stdenv-bootstrap-tools-armv6l-unknown-linux-musleabihf/on-server/bootstrap-tools.tar.xz;
+    sha256 = "1r9mz9w8y5jd7gfwfsrvs20qarzxy7bvrp5dlm41hnx6z617if1h";
   };
 }
--- pkgs/stdenv/linux/bootstrap-files/x86_64-musl.nix
+++ pkgs/stdenv/linux/bootstrap-files/x86_64-musl.nix
@@ -3,8 +3,7 @@
 
 {
   bootstrapTools = import <nix/fetchurl.nix> {
-    # XXX: Find a permanent location for this
-    url = https://wdtz.org/files/5zfs7s729n4lrlxmhlnc6qmfrlhahy9s-stdenv-bootstrap-tools-x86_64-unknown-linux-musl/on-server/bootstrap-tools.tar.xz;
-    sha256 = "0lwi08c2v7ip2z9li597ixywix976561hr358z2fbd6sqi943axl";
+    url = https://wdtz.org/files/030q34q7fk6jdfxkgcqp5rzr4yhw3pgx-stdenv-bootstrap-tools-x86_64-unknown-linux-musl/on-server/bootstrap-tools.tar.xz;
+    sha256 = "0ly0wj8wzbikn2j8sn727vikk90bq36drh98qvfx1kkh5k5azm2j";
   };
 }
