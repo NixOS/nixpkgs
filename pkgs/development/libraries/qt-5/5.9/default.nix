3ce29412bd596608fc4fb0109ce53a33976f4222
Vladimír Čunát <vcunat@gmail.com>
2018-02-12 23:00:28 +0100

    qt59.qtbase: fix with openssl-1.1.0 via ported Fedora patch

--- pkgs/development/libraries/qt-5/5.9/default.nix
+++ pkgs/development/libraries/qt-5/5.9/default.nix
@@ -37,7 +37,11 @@ let
   srcs = import ./srcs.nix { inherit fetchurl; inherit mirror; };
 
   patches = {
-    qtbase = [ ./qtbase.patch ./qtbase-fixguicmake.patch ];
+    qtbase = [
+      ./qtbase.patch
+      ./qtbase-fixguicmake.patch
+      ./qtbase-openssl_1_1.patch
+    ];
     qtdeclarative = [ ./qtdeclarative.patch ];
     qtscript = [ ./qtscript.patch ];
     qtserialport = [ ./qtserialport.patch ];
