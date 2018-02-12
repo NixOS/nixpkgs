a4c69744dcfbe8d92da8b6439d36ea1c7c970a55
Will Dietz <w@wdtz.org>
2018-02-12 16:28:20 -0600

    aarch64-musl, armv6l-musl: fetch from wdtz.org instead of storePath

--- pkgs/stdenv/linux/bootstrap-files/aarch64-musl.nix
+++ pkgs/stdenv/linux/bootstrap-files/aarch64-musl.nix
@@ -1,5 +1,11 @@
 {
-  # https://cache.allvm.org
-  busybox = builtins.storePath /nix/store/88snbnw04cldx4c3k9qrd0y2vifld2b2-stdenv-bootstrap-tools-aarch64-unknown-linux-musl/on-server/busybox;
-  bootstrapTools = builtins.storePath /nix/store/88snbnw04cldx4c3k9qrd0y2vifld2b2-stdenv-bootstrap-tools-aarch64-unknown-linux-musl/on-server/bootstrap-tools.tar.xz;
+  busybox = import <nix/fetchurl.nix> {
+    url = https://wdtz.org/files/88snbnw04cldx4c3k9qrd0y2vifld2b2-stdenv-bootstrap-tools-aarch64-unknown-linux-musl/on-server/busybox;
+    sha256 = "01s6bwq84wyrjh3rdsgxni9gkzp7ss8rghg0cmp8zd87l79y8y4g";
+    executable = true;
+  };
+  bootstrapTools = import <nix/fetchurl.nix> {
+    url = https://wdtz.org/files/88snbnw04cldx4c3k9qrd0y2vifld2b2-stdenv-bootstrap-tools-aarch64-unknown-linux-musl/on-server/bootstrap-tools.tar.xz;
+    sha256 = "0n97q17h9i9hyc8c5qklfn6vii1vr87kj4k9acdc52jayv6c3kas";
+  };
 }
--- pkgs/stdenv/linux/bootstrap-files/armv6l-musl.nix
+++ pkgs/stdenv/linux/bootstrap-files/armv6l-musl.nix
@@ -1,5 +1,11 @@
 {
-  # https://cache.allvm.org
-  busybox = builtins.storePath /nix/store/m4n9p43b7q8k1wqbpw264947rm70mj1z-stdenv-bootstrap-tools-armv6l-unknown-linux-musleabihf/on-server/busybox;
-  bootstrapTools = builtins.storePath /nix/store/m4n9p43b7q8k1wqbpw264947rm70mj1z-stdenv-bootstrap-tools-armv6l-unknown-linux-musleabihf/on-server/bootstrap-tools.tar.xz;
+  busybox = import <nix/fetchurl.nix> {
+    url = https://wdtz.org/files/m4n9p43b7q8k1wqbpw264947rm70mj1z-stdenv-bootstrap-tools-armv6l-unknown-linux-musleabihf/on-server/busybox;
+    sha256 = "01d0hp1xgrriiy9w0sd9vbqzwxnpwiyah80pi4vrpcmbwji36j1i";
+    executable = true;
+  };
+  bootstrapTools = import <nix/fetchurl.nix> {
+    url = https://wdtz.org/files/m4n9p43b7q8k1wqbpw264947rm70mj1z-stdenv-bootstrap-tools-armv6l-unknown-linux-musleabihf/on-server/bootstrap-tools.tar.xz;
+    sha256 = "1n9wm08fg99wa1q3ngim6n6bg7kxlkzx2v7fqw013rb3d5drjwrq";
+  };
 }
