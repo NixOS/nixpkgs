469c9ffe4f9138afcdb265c6441b9b94638f3ca7
Tim Steinbach <tim@nequissimus.com>
2018-02-12 07:56:09 -0500

    linux-copperhead: 4.15.2.a -> 4.15.3.a
    (cherry picked from commit 8cd97c86ff466d349ef59c4a34c182f723de6254)

--- pkgs/os-specific/linux/kernel/linux-hardened-copperhead.nix
+++ pkgs/os-specific/linux/kernel/linux-hardened-copperhead.nix
@@ -3,9 +3,9 @@
 with stdenv.lib;
 
 let
-  version = "4.15.2";
+  version = "4.15.3";
   revision = "a";
-  sha256 = "1rmia5k07pfx47qkc8nx3xiap6mxbwlry3jxrx4kwf3hh5cnqnsv";
+  sha256 = "1fxdllg60hwlbmjijcj7w6c3xz0rf9268f12qy45diahmydyccgc";
 
   # modVersion needs to be x.y.z, will automatically add .0 if needed
   modVersion = concatStrings (intersperse "." (take 3 (splitString "." "${version}.0")));
