b2282e8f9bafdd31bdf86ec6978ec723a1c76d4a
Will Dietz <w@wdtz.org>
2018-02-13 15:02:17 -0600

    psol: fixup hash after beta became stable (apparently)

--- pkgs/development/libraries/psol/default.nix
+++ pkgs/development/libraries/psol/default.nix
@@ -1,5 +1,5 @@
 { callPackage }:
 callPackage ./generic.nix {} {
-  version = "1.13.35.2"; # Latest beta, 2018-01-11
-  sha256  = "1z8m0qgvk5gqj37p4kps3y4h7jwaqk0rw0y513nlsb163z4b05f5";
+  version = "1.13.35.2"; # Latest stable, 2018-02-05
+  sha256  = "0xi2srf9gx0x2sz9r45zb35k2n0iv457if1lqzvbanls3f935cmr";
 }
