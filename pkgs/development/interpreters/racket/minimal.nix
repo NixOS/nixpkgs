e87fb710851063b48731b4b8c4db2ae77270cd54
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-02-13 08:05:35 +0800

    racket-minimal: Correct description: Dr. Racket, not Mr.

--- pkgs/development/interpreters/racket/minimal.nix
+++ pkgs/development/interpreters/racket/minimal.nix
@@ -8,7 +8,7 @@ racket.overrideAttrs (oldAttrs: rec {
     sha256 = "0c565jy2y3gjl5lncd5adjsrj8c24p4i062kphv26ni5q1nn5ip5";
   };
 
-  description = "Racket without bundled packages, such as Mr Racket.";
+  description = "Racket without bundled packages, such as Dr. Racket.";
   longDescription = ''The essential package racket-libs is included,
     as well as libraries that live in collections. In particular, raco
     and the pkg library are still bundled.
