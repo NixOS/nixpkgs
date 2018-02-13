acac200b4e0faa077892605cb64ee2ab66aa109e
Alexander Krupenkin <mail@akru.me>
2018-02-13 17:42:26 +0300

    parity: 1.8.7 -> 1.8.9; parity-beta: 1.9.0 -> 1.9.2

--- pkgs/applications/altcoins/parity/beta.nix
+++ pkgs/applications/altcoins/parity/beta.nix
@@ -1,7 +1,7 @@
 let
-  version     = "1.9.0";
-  sha256      = "1m34960yq8s6qjdshlm9brghi9dgfnk18nfk8vzjfpsk7x7hcmli";
-  cargoSha256 = "1vs56v7v02bfqrn9qx1wqbgzvrmaj0awy94w7iyib8zh8xkw8k7i";
+  version     = "1.9.2";
+  sha256      = "0dhjqp23qqypdvnxcqgbc2jrjx0pbnf465f9m99r3y0i37lkdrd5";
+  cargoSha256 = "1vdvqs6ligp5fkw5s7v44vwqwz5dqa0ipilx0piz6swz0drilima";
   patches     = [ ./patches/vendored-sources-1.9.patch ];
 in
   import ./parity.nix { inherit version sha256 cargoSha256 patches; }
--- pkgs/applications/altcoins/parity/default.nix
+++ pkgs/applications/altcoins/parity/default.nix
@@ -1,7 +1,7 @@
 let
-  version     = "1.8.7";
-  sha256      = "1bpkkf9yl2zpj7yzr34hf5kmn8mkj97ngwlmlgzvm38n04fbf05h";
-  cargoSha256 = "1mf2xgj192h0kdb7b4wrh5cj1dgnjkjjvs0hw3bp1b3vc25rfbhz";
+  version     = "1.8.9";
+  sha256      = "0pqr85rlbi001swp16p5rqhmhqn0fbdb6cldg3idzplhnh82hhrp";
+  cargoSha256 = "1d8j9hlnks0yph1l2ny7dw05ha0dg2w22sznms9xwz7rgh44k6c9";
   patches     = [ ./patches/vendored-sources-1.8.patch ];
 in
   import ./parity.nix { inherit version sha256 cargoSha256 patches; }
