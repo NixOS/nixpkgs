a39d3d6635311f6120f49d54acf177dbd685b950
Nikolay Amiantov <ab@fmap.me>
2018-02-14 15:03:59 +0300

    buildFHSEnv: export TZDIR
    This is needed since NixOS keeps tzdata in non-standard /etc/zoneinfo path.

--- pkgs/build-support/build-fhs-userenv/env.nix
+++ pkgs/build-support/build-fhs-userenv/env.nix
@@ -52,6 +52,7 @@ let
     export LOCALE_ARCHIVE='/usr/lib/locale/locale-archive'
     export LD_LIBRARY_PATH='/run/opengl-driver/lib:/run/opengl-driver-32/lib:/usr/lib:/usr/lib32'
     export PATH='/run/wrappers/bin:/usr/bin:/usr/sbin'
+    export TZDIR='/etc/zoneinfo'
 
     # Force compilers and other tools to look in default search paths
     export NIX_CC_WRAPPER_${stdenv.cc.infixSalt}_TARGET_HOST=1
