494a4ef3ce33d7edc1661273e18cee2a2f10b9d2
James Kay <james@hadean.com>
2018-02-14 17:43:33 +0000

    godot_headers: init at 51bca3bf5d917341f3e15076c5a9191f8a5118ae

---
+++ pkgs/development/libraries/godot_headers/default.nix
@@ -0,0 +1,20 @@
+{ stdenv, fetchFromGitHub }:
+stdenv.mkDerivation rec {
+  name = "godot_headers";
+  version = "51bca3bf5d917341f3e15076c5a9191f8a5118ae";
+  src = fetchFromGitHub {
+    owner = "GodotNativeTools";
+    repo = "godot_headers";
+    rev = version;
+    sha256 = "0z562pqm8y8wldmfiya72cvwwpvcfznpl0wypagw50v0f41ilywh";
+  };
+  buildPhase = "true";
+  installPhase = "cp -r . $out";
+  meta = {
+    homepage = "https://github.com/GodotNativeTools/godot_headers/";
+    description = "Headers for the Godot API supplied by the GDNative module";
+    license = stdenv.lib.licenses.mit;
+    platforms = stdenv.lib.platforms.linux;
+    maintainers = [ stdenv.lib.maintainers.twey ];
+  };
+}
