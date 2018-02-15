7b6fb652971b0c3c587d18b873581214fcb8fa62
Nikolay Amiantov <ab@fmap.me>
2018-02-15 22:04:23 +0300

    attr: cleanup style and meta

--- pkgs/development/libraries/attr/default.nix
+++ pkgs/development/libraries/attr/default.nix
@@ -12,13 +12,14 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ gettext ];
 
-  configureFlags = "MAKE=make MSGFMT=msgfmt MSGMERGE=msgmerge XGETTEXT=xgettext ECHO=echo SED=sed AWK=gawk";
+  configureFlags = [ "MAKE=make" "MSGFMT=msgfmt" "MSGMERGE=msgmerge" "XGETTEXT=xgettext" "ECHO=echo" "SED=sed" "AWK=gawk" ];
 
-  installTargets = "install install-lib install-dev";
+  installTargets = [ "install" "install-lib" "install-dev" ];
 
-  meta = {
-    homepage = http://savannah.nongnu.org/projects/attr/;
+  meta = with stdenv.lib; {
+    homepage = "http://savannah.nongnu.org/projects/attr/";
     description = "Library and tools for manipulating extended attributes";
-    platforms = stdenv.lib.platforms.linux;
+    platforms = platforms.linux;
+    license = licenses.gpl2Plus;
   };
 }
