e9ba4342ead9da9aa305a958348c0a8e2a20bfb8
Bignaux Ronan <ronan@aimao.org>
2018-02-15 17:02:26 +0100

    caprice32 : init at 20170210-53de695

---
+++ pkgs/misc/emulators/caprice32/default.nix
@@ -0,0 +1,31 @@
+{ stdenv, fetchFromGitHub, libpng, pkgconfig, SDL, freetype, zlib, mesa }:
+
+stdenv.mkDerivation rec {
+
+  pname = "caprice32";
+  version = "20170210";
+  rev = "53de69543300f81af85df32cbd21bb5c68cab61e";
+  short_rev = "${builtins.substring 0 7 rev}";
+  name = "${pname}-${version}-${short_rev}";
+
+  src = fetchFromGitHub {
+    owner = "ColinPitrat";
+    repo  = "${rev}";
+    rev = "53de69543300f81af85df32cbd21bb5c68cab61e";
+    sha256 = "12yv56blm49qmshpk4mgc802bs51wv2ra87hmcbf2wxma39c45fy";
+  };
+
+  postPatch = "substituteInPlace cap32.cfg --replace /usr/local $out";
+
+  meta = with stdenv.lib; {
+    description = "a complete emulation of CPC464, CPC664 and CPC6128";
+    homepage = https://github.com/ColinPitrat/caprice32 ;
+    license = licenses.gpl2;
+    maintainers = [ maintainers.genesis ];
+    platforms = platforms.linux;
+  };
+
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [ libpng SDL freetype zlib ];
+  makeFlags = [ "GIT_HASH=$(src.rev)" "DESTDIR=$(out)" "prefix=/"];
+}
