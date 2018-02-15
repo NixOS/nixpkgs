e4fa91422fa982524affcffcc705cf8a7f44b6ef
Rodney Lorrimar <dev@rodney.id.au>
2018-02-15 17:17:53 +0000

    buildkite-agent: 2.6.6 -> 2.6.9
    Various upstream bug fixes.
    
    https://github.com/buildkite/agent/releases/tag/v2.6.7
    https://github.com/buildkite/agent/releases/tag/v2.6.8
    https://github.com/buildkite/agent/releases/tag/v2.6.9

--- pkgs/development/tools/continuous-integration/buildkite-agent/default.nix
+++ pkgs/development/tools/continuous-integration/buildkite-agent/default.nix
@@ -1,6 +1,6 @@
 { stdenv, buildGoPackage, fetchFromGitHub, makeWrapper, coreutils, git, openssh, bash, gnused, gnugrep }:
 let
-  version = "2.6.6";
+  version = "2.6.9";
   goPackagePath = "github.com/buildkite/agent";
 in
 buildGoPackage {
@@ -12,7 +12,7 @@ buildGoPackage {
     owner = "buildkite";
     repo = "agent";
     rev = "v${version}";
-    sha256 = "0rpi63mfzlm39517l4xjcka3m4dnfjzwvpi0i1rpf1z2288cnkyx";
+    sha256 = "0rlinj7dcr8vzl1pb15nfny8jkvvj50i8czf4ahv26avnfycm4pz";
   };
 
   nativeBuildInputs = [ makeWrapper ];
@@ -44,6 +44,6 @@ buildGoPackage {
     homepage = https://buildkite.com/docs/agent;
     license = licenses.mit;
     maintainers = with maintainers; [ pawelpacana zimbatm ];
-    platforms = platforms.linux ++ platforms.darwin;
+    platforms = platforms.unix;
   };
 }
