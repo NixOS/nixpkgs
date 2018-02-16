a57a9d65887bd501ea916f7749791b3d62e609da
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-02-17 02:57:01 +0300

    sayonara:  address some review suggestions

--- pkgs/applications/audio/sayonara/default.nix
+++ pkgs/applications/audio/sayonara/default.nix
@@ -11,9 +11,11 @@ stdenv.mkDerivation {
     sha256 = "1fl7zplnrrvbv1xm4g348bpd46jj39jvbm808hyjjq92i64wqg37";
   };
 
-  buildInputs = [ cmake qt5.qtbase qt5.qttools zlib taglib pkgconfig pcre gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good gst_all_1.gst-plugins-ugly ];
-
-  postUnpack = "sourceRoot=\${sourceRoot}/";
+  nativeBuildInputs = [ cmake pkgconfig ];
+  buildInputs = with qt5; with gst_all_1;
+      [ gstreamer gst-plugins-base gst-plugins-good gst-plugins-ugly
+        pcre qtbase qttools taglib zlib
+      ];
 
   # CMake Error at src/GUI/Resources/Icons/cmake_install.cmake:49 (file):
   #   file cannot create directory: /usr/share/icons.  Maybe need administrative
@@ -32,18 +34,6 @@ stdenv.mkDerivation {
   # /tmp/nix-build-sayonara-player-1.0.0-git5-20180115.drv-0/sayonara-player/src/Components/Engine/AbstractPipeline.cpp:28:32: fatal error: gst/app/gstappsink.h: No such file or directory
   #  #include <gst/app/gstappsink.h>
   NIX_CFLAGS_COMPILE = "-I${gst_all_1.gst-plugins-base.dev}/include/gstreamer-1.0";
-
-  configurePhase = ''
-    mkdir build
-    cd build
-    runHook preConfigure
-    cmake .. \
-      -DCMAKE_INSTALL_PREFIX=$out \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DDESKTOPDIR=$out/share/applications \
-      -DICONDIR=$out/share/pixmaps
-    runHook postConfigure
-  '';
 
   meta = with stdenv.lib;
     { description = "Sayonara music player";
