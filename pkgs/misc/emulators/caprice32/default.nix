0fd4f5299a46adb1b51118494c0d4ba94837e0c8
Bignaux Ronan <ronan@aimao.org>
2018-02-16 20:34:55 +0100

    fixes according to PR

--- pkgs/misc/emulators/caprice32/default.nix
+++ pkgs/misc/emulators/caprice32/default.nix
@@ -3,29 +3,29 @@
 stdenv.mkDerivation rec {
 
   pname = "caprice32";
-  version = "20180210";
+  version = "2018-02-10";
   rev = "53de69543300f81af85df32cbd21bb5c68cab61e";
   short_rev = "${builtins.substring 0 7 rev}";
   name = "${pname}-${version}-${short_rev}";
 
   src = fetchFromGitHub {
+    inherit rev;
     owner = "ColinPitrat";
     repo  = "caprice32";
-    rev = "${rev}";
     sha256 = "12yv56blm49qmshpk4mgc802bs51wv2ra87hmcbf2wxma39c45fy";
   };
 
   postPatch = "substituteInPlace cap32.cfg --replace /usr/local $out";
 
   meta = with stdenv.lib; {
-    description = "a complete emulation of CPC464, CPC664 and CPC6128";
+    description = "A complete emulation of CPC464, CPC664 and CPC6128";
     homepage = https://github.com/ColinPitrat/caprice32 ;
     license = licenses.gpl2;
-    maintainers = [ maintainers.genesis ];
+    maintainers = [ maintainers.bignaux ];
     platforms = platforms.linux;
   };
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ libpng SDL freetype zlib ];
-  makeFlags = [ "GIT_HASH=$(src.rev)" "DESTDIR=$(out)" "prefix=/"];
+  makeFlags = [ "GIT_HASH=${src.rev}" "DESTDIR=$(out)" "prefix=/"];
 }
