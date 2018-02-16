a4763e1fd20c906475530cec669d8b37d5c3fec6
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-02-16 12:25:34 +0800

    racket-minimal: Add aarch64-linux to meta.platforms
    Move description and longDescription to meta -- they were accidentally
    in the parent set.

--- pkgs/development/interpreters/racket/minimal.nix
+++ pkgs/development/interpreters/racket/minimal.nix
@@ -8,10 +8,12 @@ racket.overrideAttrs (oldAttrs: rec {
     sha256 = "0c565jy2y3gjl5lncd5adjsrj8c24p4i062kphv26ni5q1nn5ip5";
   };
 
-  description = "Racket without bundled packages, such as Dr. Racket.";
-  longDescription = ''The essential package racket-libs is included,
-    as well as libraries that live in collections. In particular, raco
-    and the pkg library are still bundled.
-  '';
-
+  meta = oldAttrs.meta // {
+    description = "Racket without bundled packages, such as Dr. Racket.";
+    longDescription = ''The essential package racket-libs is included,
+      as well as libraries that live in collections. In particular, raco
+      and the pkg library are still bundled.
+    '';
+    platforms = [ "x86_64-linux" "aarch64-linux" ];
+  };
 })
