a89899ce4e50f2851c7c1c426200e34d2de1192c
Jan Malakhovski <oxij@oxij.org>
2018-02-17 18:44:43 +0000

    fetchurl: cleanup, better errors
    Also fix what seems like bugs in uncommon `stdenv`s.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -195,7 +195,7 @@ with pkgs;
 
   # `fetchurl' downloads a file from the network.
   fetchurl = import ../build-support/fetchurl {
-    inherit stdenvNoCC;
+    inherit lib stdenvNoCC;
     # On darwin, libkrb5 needs bootstrap_cmds which would require
     # converting many packages to fetchurl_boot to avoid evaluation cycles.
     curl = buildPackages.curl.override (lib.optionalAttrs stdenv.isDarwin { gssSupport = false; });
