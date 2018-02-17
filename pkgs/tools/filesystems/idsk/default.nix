5088917402d719156c5bf9ad3770d7222f533da7
Bignaux Ronan <ronan@aimao.org>
2018-02-17 08:20:00 +0100

    fixing tab and maintainers

--- pkgs/tools/filesystems/idsk/default.nix
+++ pkgs/tools/filesystems/idsk/default.nix
@@ -2,29 +2,29 @@
 
 stdenv.mkDerivation rec {
 
-	repo = "idsk";
-	version = "unstable-2018-02-11";
-	rev = "1846729ac3432aa8c2c0525be45cfff8a513e007";
-	name = "${repo}-${version}";
+  repo = "idsk";
+  version = "unstable-2018-02-11";
+  rev = "1846729ac3432aa8c2c0525be45cfff8a513e007";
+  name = "${repo}-${version}";
 
-	meta = with stdenv.lib; {
-		description = "Manipulating CPC dsk images and files";
-		homepage = https://github.com/cpcsdk/idsk ;
-		license = "unknown";
-		maintainers = [ maintainers.bignaux ];
-		platforms = platforms.linux;
-	};
+  meta = with stdenv.lib; {
+    description = "Manipulating CPC dsk images and files";
+    homepage = https://github.com/cpcsdk/idsk ;
+    license = "unknown";
+    maintainers = [ maintainers.genesis ];
+    platforms = platforms.linux;
+  };
 
-	src = fetchFromGitHub {
-		inherit rev repo;
-		owner = "cpcsdk";
-		sha256 = "0d891lvf2nc8bys8kyf69k54rf3jlwqrcczbff8xi0w4wsiy5ckv";
-	};
+  src = fetchFromGitHub {
+    inherit rev repo;
+    owner = "cpcsdk";
+    sha256 = "0d891lvf2nc8bys8kyf69k54rf3jlwqrcczbff8xi0w4wsiy5ckv";
+  };
 
-	nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [ cmake ];
 
-	installPhase = ''
-		mkdir -p $out/bin
-		cp iDSK $out/bin
-		'';
+  installPhase = ''
+    mkdir -p $out/bin
+    cp iDSK $out/bin
+  '';
 }
