fef4c0262198801a2497b1201eb4c87822ab1b9d
volth <volth@webmaster.ms>
2018-02-18 19:01:01 +0000

    babelstone-han: 10.0.0 -> 10.0.2

--- pkgs/data/fonts/babelstone-han/default.nix
+++ pkgs/data/fonts/babelstone-han/default.nix
@@ -1,16 +1,16 @@
 {stdenv, fetchzip}:
 
 let
-  version = "10.0.0";
+  version = "10.0.2";
 in fetchzip {
   name = "babelstone-han-${version}";
 
-  url = http://www.babelstone.co.uk/Fonts/0816/BabelStoneHan.zip;
+  url = http://www.babelstone.co.uk/Fonts/7932/BabelStoneHan.zip;
   postFetch = ''
     mkdir -p $out/share/fonts/truetype
     unzip $downloadedFile '*.ttf' -d $out/share/fonts/truetype
   '';
-  sha256 = "0648hv5c1hq3bq7mlk7bnmflzzqj4wh137bjqyrwj5hy3nqzvl5r";
+  sha256 = "17r5cf028v66yzjf9qbncn4rchv2xxkl2adxr35ppg1l7zssz9v6";
 
   meta = with stdenv.lib; {
     description = "Unicode CJK font with over 32600 Han characters";
