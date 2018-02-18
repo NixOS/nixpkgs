965ddac92fe59b4ed3d39d214732c239b8186019
Ambroz Bizjak <ambrop7@gmail.com>
2018-02-18 14:42:04 +0100

    cups: Add patch for bug where CUPS fails to save files to disk
    Upstream report: https://github.com/apple/cups/issues/5118
    
    In addition to not saving files, this results in spam messages in the log once per second (Expiring subscriptions...)
    which is what led me to discover this bug. The patch was made by hand because the patch from the commit does not apply.

--- pkgs/misc/cups/default.nix
+++ pkgs/misc/cups/default.nix
@@ -25,6 +25,7 @@ stdenv.mkDerivation rec {
       url = "https://git.archlinux.org/svntogit/packages.git/plain/trunk/cups-systemd-socket.patch?h=packages/cups";
       sha256 = "1ddgdlg9s0l2ph6l8lx1m1lx6k50gyxqi3qiwr44ppq1rxs80ny5";
     })
+    ./cups-clean-dirty.patch
   ];
 
   nativeBuildInputs = [ pkgconfig removeReferencesTo ];
