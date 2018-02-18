23e418f495ae87cf46c5819846c392ebfb042739
Vladimír Čunát <vcunat@gmail.com>
2018-02-18 19:56:34 +0100

    libav*: maintenance updates
    Also remove myself from meta.maintainers,
    as I can't really give them too much maintenance.
    
    (cherry picked from commit 655446c7f574e08e92d191f0a80dd952bd33a687)
    I see some security fixes in the ChangeLog.

--- pkgs/development/libraries/libav/default.nix
+++ pkgs/development/libraries/libav/default.nix
@@ -28,9 +28,9 @@ let inherit (stdenv.lib) optional optionals hasPrefix; in
 let
   result = {
     # e.g. https://libav.org/releases/libav-11.11.tar.xz.sha1
-    libav_0_8 = libavFun "0.8.20" "0c7a2417c3a01eb74072691bb93ce802ae1be08f";
-    libav_11  = libavFun "11.11"  "d7444fa4f135bdd7347cc962ab4b5228796b0f23";
-    libav_12  = libavFun "12.1"   "df048240318b888e3938f649422b967f62e43e3c";
+    libav_0_8 = libavFun "0.8.21" "d858f65128dad0bac1a8c3a51e5cbb27a7c79b3f";
+    libav_11  = libavFun "11.12"  "61d5dcab5fde349834af193a572b12a5fd6a4d42";
+    libav_12  = libavFun "12.3"   "386c18c8b857f23dfcf456ce40370716130211d9";
   };
 
   libavFun = version : sha1 : stdenv.mkDerivation rec {
@@ -124,7 +124,6 @@ let
       license = with licenses; if enableUnfree then unfree #ToDo: redistributable or not?
         else if enableGPL then gpl2Plus else lgpl21Plus;
       platforms = with platforms; linux ++ darwin;
-      maintainers = [ maintainers.vcunat ];
     };
   }; # libavFun
 
