f0d2baad08c7585718ec2a300c6a529bba640910
Kosyrev Serge <deepfire@users.noreply.github.com>
2018-02-19 22:16:37 +0300

    sayonara:  init at 1.0.0-git5-20180115 (#35046)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17178,6 +17178,8 @@ with pkgs;
     vte = gnome3.vte;
   };
 
+  sayonara = callPackage ../applications/audio/sayonara { };
+
   sbagen = callPackage ../applications/misc/sbagen { };
 
   scantailor = callPackage ../applications/graphics/scantailor { };
