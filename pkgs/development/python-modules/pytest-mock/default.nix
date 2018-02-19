1977a2d5ee3eb426b0772cef1e9001945f399370
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-02-19 11:42:41 +0100

    pythonPackages.pytest-mock: fix build
    This applies the following patch: https://github.com/pytest-dev/pytest-mock/pull/107

--- pkgs/development/python-modules/pytest-mock/default.nix
+++ pkgs/development/python-modules/pytest-mock/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, fetchPypi, isPy3k, pytest, mock, setuptools_scm }:
+{ lib, buildPythonPackage, fetchPypi, fetchpatch, isPy3k, pytest, mock, setuptools_scm }:
 
 buildPythonPackage rec {
   pname = "pytest-mock";
@@ -7,6 +7,11 @@ buildPythonPackage rec {
   src = fetchPypi {
     inherit pname version;
     sha256 = "8ed6c9ac6b7565b226b4da2da48876c9198d76401ec8d9c5e4c69b45423e33f8";
+  };
+
+  patches = fetchpatch {
+    url = "${meta.homepage}/pull/107.patch";
+    sha256 = "07p7ra6lilfv04wyxc855zmfwxvnpmi9s0v6vh5bx769cj9jwxck";
   };
 
   propagatedBuildInputs = [ pytest ] ++ lib.optional (!isPy3k) mock;
