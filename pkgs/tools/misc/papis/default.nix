43860cc5147c460f5e6baa993c4f7fff05499c09
Nicolò Balzarotti <anothersms@gmail.com>
2018-02-20 23:53:43 +0100

    papis: 0.5.2 -> 0.5.3

--- pkgs/tools/misc/papis/default.nix
+++ pkgs/tools/misc/papis/default.nix
@@ -1,39 +1,43 @@
 { buildPythonApplication, lib, fetchFromGitHub
 , argcomplete, arxiv2bib, beautifulsoup4, bibtexparser
-, configparser, habanero, papis-python-rofi, pylibgen
-, prompt_toolkit, pyparser, python_magic, pyyaml
-, requests, unidecode, urwid, vobject, tkinter
+, configparser, dmenu-python, habanero, papis-python-rofi
+, pylibgen, prompt_toolkit, pyparser, pytest, python_magic
+, pyyaml, requests, unidecode, urwid, vobject, tkinter
 , vim
 }:
 
 buildPythonApplication rec {
   pname = "papis";
-  version = "0.5.2";
+  version = "0.5.3";
 
   # Missing tests on Pypi
   src = fetchFromGitHub {
     owner = "alejandrogallo";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0cw6ajdaknijka3j2bkkkn0bcxqifk825kq0a0rdbbmc6661pgxb";
+    sha256 = "1yc4ilb7bw099pi2vwawyf8mi0n1kp87wgwgwcwc841ibq62q8ic";
   };
 
   postPatch = "sed -i 's/configparser>=3.0.0/# configparser>=3.0.0/' setup.py";
 
   propagatedBuildInputs = [
     argcomplete arxiv2bib beautifulsoup4 bibtexparser
-    configparser habanero papis-python-rofi pylibgen
-    prompt_toolkit pyparser python_magic pyyaml
+    configparser dmenu-python habanero papis-python-rofi
+    pylibgen prompt_toolkit pyparser python_magic pyyaml
     requests unidecode urwid vobject tkinter
     vim
   ];
 
+  checkInputs = [ pytest ];
+
   # Papis tries to create the config folder under $HOME during the tests
-  preCheck = ''
+  checkPhase = ''
     mkdir -p check-phase
     export HOME=$(pwd)/check-phase
-  '';
 
+    make test
+    make test-non-pythonic
+  '';
 
   meta = {
     description = "Powerful command-line document and bibliography manager";
