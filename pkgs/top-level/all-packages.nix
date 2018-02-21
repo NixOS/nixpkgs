4fcadba5eabba61c47bdd2a82dc60cdc4e7d09d7
Vincent Laporte <Vincent.Laporte@gmail.com>
2018-02-21 17:22:00 +0000

    ocamlPackages: default to 4.05

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16425,7 +16425,7 @@ with pkgs;
 
   linuxsampler = callPackage ../applications/audio/linuxsampler { };
 
-  llpp = ocaml-ng.ocamlPackages.callPackage ../applications/misc/llpp { };
+  llpp = ocaml-ng.ocamlPackages_4_04.callPackage ../applications/misc/llpp { };
 
   lmms = libsForQt5.callPackage ../applications/audio/lmms {
     lame = null;
