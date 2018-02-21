9560b320f1084cb472cabe80817dcf5e066845b2
Matthias Beyer <mail@beyermatthias.de>
2018-02-21 15:23:35 +0100

    haxor-news: try to disable tests which seem to depend on network

--- pkgs/applications/misc/haxor-news/default.nix
+++ pkgs/applications/misc/haxor-news/default.nix
@@ -20,10 +20,58 @@ buildPythonApplication rec {
     six
   ];
 
-  checkInputs = [ mock pexpect ];
+  disabledTests = stdenv.lib.concatMapStringsSep " and " (s: "not " + s) [
+    "test_ask"
+    "test_best"
+    "test_hiring"
+    "test_freelance"
+    "test_jobs"
+    "test_new"
+    "test_onion"
+    "test_show"
+    "test_top"
+    "test_user"
+    "test_view"
+    "test_ask"
+    "test_best"
+    "test_format_markdown"
+    "test_headlines_message"
+    "test_hiring_and_freelance"
+    "test_jobs"
+    "test_new"
+    "test_onion"
+    "test_show"
+    "test_top"
+    "test_user"
+    "test_view_setup_query_recent"
+    "test_view_setup_query_unseen"
+    "test_format_comment"
+    "test_format_index_title"
+    "test_format_item"
+    "test_print_comments_unseen"
+    "test_print_comments_unseen_hide_non_matching"
+    "test_print_comments_regex"
+    "test_print_comments_regex_hide_non_matching"
+    "test_print_comments_regex_seen"
+    "test_print_item_not_found"
+    "test_print_items"
+    "test_print_tip_view"
+    "test_match_comment_unseen"
+    "test_match_regex"
+    "test_match_regex_user"
+    "test_match_regex_item"
+    "test_view"
+    "test_view_comments"
+    "test_view_no_url"
+    "test_view_browser_url"
+    "test_view_browser_comments"
+  ];
+
+  checkInputs = [ mock pexpect python.pkgs.pytest ];
 
   checkPhase = ''
-    PATH=$out/bin:$PATH ${python.interpreter} -m unittest discover -s tests -v
+    py.test -k "${disabledTests}"
+    #PATH=$out/bin:$PATH ${python.interpreter} -m unittest discover -s tests -v
   '';
 
   meta = with stdenv.lib; {
