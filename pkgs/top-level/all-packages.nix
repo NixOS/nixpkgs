6d09fbfcf4a721317dd9a042db440887eb58b64b
Francesco Gazzetta <francygazz@gmail.com>
2018-02-22 18:52:54 +0000

    python: isso: init at 0.10.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9429,6 +9429,8 @@ with pkgs;
     stdenv = llvmPackages_4.stdenv;
   };
 
+  isso = callPackage ../servers/isso { };
+
   itk = callPackage ../development/libraries/itk { };
 
   jasper = callPackage ../development/libraries/jasper { };
