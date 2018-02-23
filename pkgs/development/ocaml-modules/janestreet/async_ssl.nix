435713b27cc8b9ca0a477879c841f2f8aceaba65
Vincent Laporte <Vincent.Laporte@gmail.com>
2018-02-23 17:19:25 +0000

    ocamlPackages.ctypes: 0.11.5 -> 0.13.1

--- pkgs/development/ocaml-modules/janestreet/async_ssl.nix
+++ pkgs/development/ocaml-modules/janestreet/async_ssl.nix
@@ -1,10 +1,12 @@
-{stdenv, buildOcamlJane, fetchurl, async, comparelib, core, ctypes, openssl,
- fieldslib, herelib, pipebang, sexplib, ocaml_oasis}:
+{ stdenv, ocaml, buildOcamlJane, fetchurl, async, comparelib, core, ctypes
+, openssl, fieldslib, herelib, pipebang, sexplib, ocaml_oasis, integers
+}:
 
 buildOcamlJane rec {
   name = "async_ssl";
   version = "113.33.07";
   hash = "0bhzpnmlx6dy4fli3i7ipjwqbsdi7fq171jrila5dr3ciy3841xs";
+  postPatch = "export CAML_LD_LIBRARY_PATH=${integers}/lib/ocaml/${ocaml.version}/site-lib/stubslibs:$CAML_LD_LIBRARY_PATH";
   propagatedBuildInputs = [ ctypes async comparelib core fieldslib
                             herelib pipebang sexplib openssl ocaml_oasis ];
   meta = with stdenv.lib; {
