de910a040b64a2e0c808a89e62717bb80c9f0043
Jan Tojnar <jtojnar@gmail.com>
2018-02-23 01:42:35 +0100

    meson: fix rpath fixup
    In common distributions, RPATH is only needed for internal libraries so
    meson removes everything else. With Nix, the locations of libraries
    are not as predictable, therefore we need to keep them in the RPATH. [1]
    
    Previously we have just kept the RPATH produced by the linker, patching
    meson not to remove it. This deprived us of potentially replacing it
    with install_rpath provided by project so we had to re-add it manually,
    and also introduced a vulnerability of keeping build paths in RPATH.
    
    This commit restores the clean-up but modifies it so the items starting
    with /nix/store are retained.
    
    This should be relatively safe since the store is immutable, however,
    there might be some unwanted retainment of build_rpath [2] if it contains
    paths from Nix store.
    
    [1]: https://github.com/NixOS/nixpkgs/issues/31222#issuecomment-365811634
    [2]: http://mesonbuild.com/Release-notes-for-0-42-0.html#added-build_rpath-keyword-argument

--- pkgs/development/tools/build-managers/meson/default.nix
+++ pkgs/development/tools/build-managers/meson/default.nix
@@ -1,4 +1,4 @@
-{ lib, python3Packages, stdenv, targetPlatform, writeTextDir, m4 }: let
+{ lib, python3Packages, stdenv, targetPlatform, writeTextDir, substituteAll }: let
   targetPrefix = lib.optionalString stdenv.isCross
                    (targetPlatform.config + "-");
 in python3Packages.buildPythonApplication rec {
@@ -27,11 +27,17 @@ in python3Packages.buildPythonApplication rec {
     # We patch Meson to add a --fallback-library-path argument with
     # library install_dir to g-ir-scanner.
     ./gir-fallback-path.patch
-  ];
 
-  postPatch = ''
-    sed -i -e 's|e.fix_rpath(install_rpath)||' mesonbuild/scripts/meson_install.py
-  '';
+    # In common distributions, RPATH is only needed for internal libraries so
+    # meson removes everything else. With Nix, the locations of libraries
+    # are not as predictable, therefore we need to keep them in the RPATH.
+    # At the moment we are keeping the paths starting with /nix/store.
+    # https://github.com/NixOS/nixpkgs/issues/31222#issuecomment-365811634
+    (substituteAll {
+      src = ./fix-rpath.patch;
+      inherit (builtins) storeDir;
+    })
+  ];
 
   setupHook = ./setup-hook.sh;
 
