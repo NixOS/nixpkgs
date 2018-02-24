dc01b66eef65b275cf671f46ed9683ac4c341229
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-02-23 20:29:24 -0500

    uboot: add Rock64 support
    The bootloader is currently non-functional because it lacks the ARM Trusted
    Firmware image (bl31.elf).
    
    (cherry picked from commit 6f5a0bded2ae20759003d8be9548b5358c8aefcd)

---
+++ pkgs/misc/uboot/rock64.nix
@@ -0,0 +1,26 @@
+{ buildUBoot, fetchFromGitHub }: buildUBoot rec {
+  name = "uboot-${defconfig}-${version}";
+  version = "2018.01";
+
+  src = fetchFromGitHub {
+    owner = "ayufan-rock64";
+    repo = "linux-u-boot";
+    rev = "19e31fac0dee3c4f6b2ea4371e4321f79db0f495";
+    sha256 = "1vmv7q9yafsc0zivd0qdfmf930dvhzkf4a3j6apxxgx9g10wgwrg";
+  };
+
+  extraMakeFlags = [ "u-boot.itb" "all" ];
+  postBuild = ''
+    ./tools/mkimage -n rk3328 -T rksd -d tpl/u-boot-tpl.bin idbloader.img
+    cat spl/u-boot-spl.bin >> idbloader.img
+    dd if=u-boot.itb of=idbloader.img seek=448 conv=notrunc
+  '';
+
+  defconfig = "rock64-rk3328_defconfig";
+  filesToInstall = [ "spl/u-boot-spl.bin" "tpl/u-boot-tpl.bin" "u-boot.itb" "idbloader.img"];
+
+  extraMeta = {
+    maintainers = [ lib.maintainers.lopsided98 ];
+    platforms = ["aarch64-linux"];
+  };
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13886,6 +13886,8 @@ with pkgs;
 
   ubootGuruplug = callPackage ../misc/uboot/guruplug.nix { };
 
+  ubootRock64 = callPackage ../misc/uboot/rock64.nix { };
+
   uclibc = callPackage ../os-specific/linux/uclibc { };
 
   uclibcCross = lowPrio (callPackage ../os-specific/linux/uclibc {
