b841b09c2ba0b0012c2d038340d2ba76c760dc3b
Vladimír Čunát <vcunat@gmail.com>
2018-02-24 16:39:47 +0100

    Merge #35370: texlive.bin: fix with poppler 0.62
    (cherry picked from commit 63ca2cc78668969452e297012d50e2643fe7fc67)

--- pkgs/tools/typesetting/tex/texlive/bin.nix
+++ pkgs/tools/typesetting/tex/texlive/bin.nix
@@ -164,7 +164,7 @@ inherit (core-big) metafont metapost luatex xetex;
 core-big = stdenv.mkDerivation { #TODO: upmendex
   name = "texlive-core-big.bin-${version}";
 
-  inherit (common) src;
+  inherit (common) src patches;
 
   hardeningDisable = [ "format" ];
 
