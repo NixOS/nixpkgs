09b54dec1d678dc8a75cbc078377abf9c420494f
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-02-24 11:42:09 +0000

    paper-icon-theme: build GTK icon cache

--- pkgs/data/icons/paper-icon-theme/default.nix
+++ pkgs/data/icons/paper-icon-theme/default.nix
@@ -1,18 +1,24 @@
-{ stdenv, fetchFromGitHub, autoreconfHook }:
+{ stdenv, fetchFromGitHub, autoreconfHook, gtk3 }:
 
 stdenv.mkDerivation rec {
-  name = "${package-name}-${version}";
-  package-name = "paper-icon-theme";
+  name = "${pname}-${version}";
+  pname = "paper-icon-theme";
   version = "2017-11-20";
 
   src = fetchFromGitHub {
     owner = "snwh";
-    repo = package-name;
+    repo = pname;
     rev = "af0296ecc872ad723fad7dca6e7e89eb85cbb3a8";
     sha256 = "18a9zl9lbw9gc3zas49w329xrps4slvkp4nv815nlnmimz8dj85m";
   };
 
-  nativeBuildInputs = [ autoreconfHook ];
+  nativeBuildInputs = [ autoreconfHook gtk3 ];
+
+  postFixup = ''
+    for theme in $out/share/icons/*; do
+      gtk-update-icon-cache $theme
+    done
+  '';
 
   meta = with stdenv.lib; {
     description = "Modern icon theme designed around bold colours and simple geometric shapes";
