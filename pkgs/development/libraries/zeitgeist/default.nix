6d1ee453027da82c0380f0c64225bbe5fc9b3fad
Jan Tojnar <jtojnar@gmail.com>
2018-02-25 07:12:49 +0100

    zeitgeist: 1.0 → 1.0.1
    https://cgit.freedesktop.org/zeitgeist/zeitgeist/commit/?id=3cf082f10692ab8fd75a0a59dfa6a97b77a39bd6

--- pkgs/development/libraries/zeitgeist/default.nix
+++ pkgs/development/libraries/zeitgeist/default.nix
@@ -1,37 +1,34 @@
-{ stdenv, fetchgit, pkgconfig, glib, sqlite, gnome3, vala_0_38
-, intltool, libtool, dbus_libs, telepathy-glib
+{ stdenv, fetchgit, pkgconfig, glib, sqlite, vala_0_38
+, autoconf, automake, libtool, gettext, dbus_libs, telepathy-glib
 , gtk3, json-glib, librdf_raptor2, dbus-glib
 , pythonSupport ? true, python2Packages
 }:
 
 stdenv.mkDerivation rec {
-  version = "1.0";
+  version = "1.0.1";
   name = "zeitgeist-${version}";
 
   src = fetchgit {
     url = "git://anongit.freedesktop.org/git/zeitgeist/zeitgeist";
     rev = "v${version}";
-    sha256 = "0vw6m0azycqabbz8f0fd8xsh5kf6j3ig4wpqlhw6sklvf44ii5b8";
+    sha256 = "1lgqcqr5h9ba751b7ajp7h2w1bb5qza2w3k1f95j3ab15p7q0q44";
   };
 
-  configureScript = "./autogen.sh";
+  preConfigure = "NOCONFIGURE=1 ./autogen.sh";
 
   configureFlags = [ "--with-session-bus-services-dir=$(out)/share/dbus-1/services" ];
 
-  nativeBuildInputs = [ pkgconfig gnome3.gnome-common intltool libtool vala_0_38 ];
-  buildInputs = [ glib sqlite dbus_libs telepathy-glib dbus-glib
-                  gtk3 json-glib librdf_raptor2 python2Packages.rdflib ];
+  nativeBuildInputs = [ autoconf automake libtool pkgconfig gettext vala_0_38 python2Packages.python ];
+  buildInputs = [
+    glib sqlite dbus_libs telepathy-glib dbus-glib
+    gtk3 json-glib librdf_raptor2 python2Packages.rdflib
+  ];
 
   prePatch = "patchShebangs .";
 
-  patches = [ ./dbus_glib.patch ];
-
-  patchFlags = [ "-p0" ];
-
   enableParallelBuilding = true;
 
-  postFixup = ''
-  '' + stdenv.lib.optionalString pythonSupport ''
+  postFixup = stdenv.lib.optionalString pythonSupport ''
     moveToOutput lib/${python2Packages.python.libPrefix} "$py"
   '';
 
