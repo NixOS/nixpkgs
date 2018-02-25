be5090fa5279e4ebde857175193ce74e912db183
Sarah Brofeldt <sbrofeldt@gmail.com>
2018-02-25 13:38:22 +0100

    dockerTools: Pin go to 1.9
    This is the Go version that Docker currently uses:
    https://github.com/moby/moby/pull/35739
    
    Fixes #35505

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -121,7 +121,8 @@ with pkgs;
 
   dieHook = makeSetupHook {} ../build-support/setup-hooks/die.sh;
 
-  dockerTools = callPackage ../build-support/docker { };
+  # go 1.9 pin until https://github.com/moby/moby/pull/35739
+  dockerTools = callPackage ../build-support/docker { go = go_1_9; };
 
   docker_compose = pythonPackages.docker_compose;
 
