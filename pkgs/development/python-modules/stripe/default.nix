1dd167efe0d682cf34e10c4a2059420325749ef4
Frederik Rietdijk <fridh@fridh.nl>
2018-02-26 18:47:05 +0100

    python: stripe: 1.77.2 -> 1.79.0

--- pkgs/development/python-modules/stripe/default.nix
+++ pkgs/development/python-modules/stripe/default.nix
@@ -3,7 +3,7 @@
 
 buildPythonPackage rec {
   pname = "stripe";
-  version = "1.77.2";
+  version = "1.79.0";
   name = "${pname}-${version}";
 
   # Tests require network connectivity and there's no easy way to disable
@@ -12,7 +12,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "3bcd55108dd2c0e853a91147ee843bc375f35767e64d0f7680e5bd82ddb7fbf1";
+    sha256 = "490bb7bfc7d224e483d643171fd860f8a1670e31fd9cef8cb8d9a7c19806d450";
   };
 
   buildInputs = [ unittest2 mock ];
