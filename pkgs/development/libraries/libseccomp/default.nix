e2243a5ac95826b5b8c23c77c3562d81e5146b29
Vladimír Čunát <vcunat@gmail.com>
2018-02-26 22:54:48 +0100

    Revert "Revert "Merge staging at '8d490ca9934d0' into master""
    This reverts commit f5ce8f86dfa0a9f9dfe63462161f5ba982e57738.

--- pkgs/development/libraries/libseccomp/default.nix
+++ pkgs/development/libraries/libseccomp/default.nix
@@ -9,6 +9,8 @@ stdenv.mkDerivation rec {
     sha256 = "0mdiyfljrkfl50q1m3ws8yfcyfjwf1zgkvcva8ffcwncji18zhkz";
   };
 
+  outputs = [ "out" "lib" "dev" "man" ];
+
   buildInputs = [ getopt makeWrapper ];
 
   patchPhase = ''
--- pkgs/development/python-modules/daphne/default.nix
+++ pkgs/development/python-modules/daphne/default.nix
@@ -1,18 +1,30 @@
-{ stdenv, buildPythonPackage, fetchPypi,
-  asgiref, autobahn, twisted, hypothesis
+{ stdenv, buildPythonPackage, isPy3k, fetchFromGitHub
+, asgiref, autobahn, twisted, pytestrunner
+, hypothesis, pytest, pytest-asyncio
 }:
 buildPythonPackage rec {
   pname = "daphne";
-  name = "${pname}-${version}";
   version = "2.0.3";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "bb2075ce35ca00f2e5440cc034dfebd5c00d346de62ea45f099db089b868c31f";
+  disabled = !isPy3k;
+
+  src = fetchFromGitHub {
+    owner = "django";
+    repo = pname;
+    rev = version;
+    sha256 = "1rdnzpgyk5cnx4xc3c7k11v2x9xpihgjpq14fib80jfpcqggw687";
   };
 
-  buildInputs = [ hypothesis ];
+  nativeBuildInputs = [ pytestrunner ];
+
   propagatedBuildInputs = [ asgiref autobahn twisted ];
+
+  checkInputs = [ hypothesis pytest pytest-asyncio ];
+
+  checkPhase = ''
+    # Other tests fail, seems to be due to filesystem access
+    py.test -k "test_cli or test_utils"
+  '';
 
   meta = with stdenv.lib; {
     description = "Django ASGI (HTTP/WebSocket) server";
