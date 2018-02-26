cd0d2444c60b5fe33c456d3cf1a4427c948d8f45
Jörg Thalheim <joerg@thalheim.io>
2018-02-26 22:27:41 +0000

    avro-c: nitpicks

--- pkgs/development/libraries/avro-c/default.nix
+++ pkgs/development/libraries/avro-c/default.nix
@@ -1,35 +1,30 @@
 { stdenv, bash, cmake, fetchurl, pkgconfig, jansson, zlib }:
 
-let version = "1.8.2"; in
-
-stdenv.mkDerivation rec {
+let
+  version = "1.8.2";
+in stdenv.mkDerivation rec {
   name = "avro-c-${version}";
 
-  #
   src = fetchurl {
     url = "mirror://apache/avro/avro-${version}/c/avro-c-${version}.tar.gz";
     sha256 = "03pixl345kkpn1jds03rpdcwjabi41rgdzi8f7y93gcg5cmrhfa6";
   };
 
-  patchPhase = ''
-    substituteInPlace version.sh \
-      --replace /bin/bash "$bash/bin/bash"
+  postPatch = ''
+    patchShebangs .
   '';
 
-  buildInputs = [
-    pkgconfig
-    cmake
-    jansson
-    zlib
-  ];
+  nativeBuildInputs = [ pkgconfig cmake ];
+
+  buildInputs = [ jansson zlib ];
 
   enableParallelBuilding = true;
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "A C library which implements parts of the Avro Specification";
     homepage = https://avro.apache.org/;
-    license = stdenv.lib.licenses.asl20;
-    maintainers = with stdenv.lib.maintainers; [ lblasc ];
-    platforms = stdenv.lib.platforms.all;
+    license = licenses.asl20;
+    maintainers = with maintainers; [ lblasc ];
+    platforms = platforms.all;
   };
 }
