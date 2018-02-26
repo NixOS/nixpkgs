31a94650e2a3be50fce643436c8252e7ee396b90
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-02-27 00:41:49 +0100

    pythonPackages.stripe: add simplejson dependency for python 2

--- pkgs/development/python-modules/stripe/default.nix
+++ pkgs/development/python-modules/stripe/default.nix
@@ -1,10 +1,9 @@
-{ stdenv, buildPythonPackage, fetchPypi
-, unittest2, mock, requests }:
+{ lib, buildPythonPackage, fetchPypi, isPy3k
+, unittest2, mock, requests, simplejson }:
 
 buildPythonPackage rec {
   pname = "stripe";
   version = "1.79.0";
-  name = "${pname}-${version}";
 
   # Tests require network connectivity and there's no easy way to disable
   # them. ~ C.
@@ -15,11 +14,11 @@ buildPythonPackage rec {
     sha256 = "490bb7bfc7d224e483d643171fd860f8a1670e31fd9cef8cb8d9a7c19806d450";
   };
 
-  buildInputs = [ unittest2 mock ];
+  checkInputs = [ unittest2 mock ];
 
-  propagatedBuildInputs = [ requests ];
+  propagatedBuildInputs = [ requests ] ++ lib.optional (!isPy3k) simplejson;
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Stripe Python bindings";
     homepage = https://github.com/stripe/stripe-python;
     license = licenses.mit;
