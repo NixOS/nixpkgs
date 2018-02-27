cd323e86025198fd58a0e827f8e3b5b9d8a11dfb
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-02-27 17:40:42 +0100

    python3Packages.netdisco: allow newer zeroconf versions

--- pkgs/development/python-modules/netdisco/default.nix
+++ pkgs/development/python-modules/netdisco/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildPythonPackage, isPy3k, fetchFromGitHub, requests, zeroconf, netifaces, pytest }:
+{ stdenv, buildPythonPackage, isPy3k, fetchFromGitHub, fetchpatch, requests, zeroconf, netifaces, pytest }:
 
 buildPythonPackage rec {
   pname = "netdisco";
@@ -14,6 +14,12 @@ buildPythonPackage rec {
     sha256 = "170s9py8rw07cfgwvv7mf69g8jjg32m2rgw8x3kbvjqlmrdijxmm";
   };
 
+  # Allow newer zeroconf versions
+  patches = fetchpatch {
+    url = "${meta.homepage}/commit/78f83046a2a0d77527274c8be9c3fd75737c19d0.patch";
+    sha256 = "098zkwqg9181vavw97yhi9rsdsf023gnapg4gkr1n0awz3f3l9nm";
+  };
+
   propagatedBuildInputs = [ requests zeroconf netifaces ];
 
   checkInputs = [ pytest ];
@@ -24,7 +30,7 @@ buildPythonPackage rec {
 
   meta = with stdenv.lib; {
     description = "Python library to scan local network for services and devices";
-    homepage = https://github.com/home-assistant/netdisco/;
+    homepage = https://github.com/home-assistant/netdisco;
     license = licenses.asl20;
     platforms = platforms.unix;
     maintainers = with maintainers; [ dotlambda ];
