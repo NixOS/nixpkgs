40ffdbf728019b5ed9e56e3a10fa2ba28c50c37a
Alexander Krupenkin <mail@akru.me>
2018-03-01 01:29:27 +0300

    parity-beta: updated vendored sources

--- pkgs/applications/altcoins/parity/beta.nix
+++ pkgs/applications/altcoins/parity/beta.nix
@@ -1,7 +1,7 @@
 let
   version     = "1.9.3";
   sha256      = "19qyp6kafnnfhdnbq745v8zybnqizjzzc3k4701ly9hf0dvx53ka";
-  cargoSha256 = "1vdvqs6ligp5fkw5s7v44vwqwz5dqa0ipilx0piz6swz0drilima";
+  cargoSha256 = "1f2rq96ci1pm29wlaahp4vq6wmmywq33a7svdi9nw5wqvbr1l1nk";
   patches     = [ ./patches/vendored-sources-1.9.patch ];
 in
   import ./parity.nix { inherit version sha256 cargoSha256 patches; }
