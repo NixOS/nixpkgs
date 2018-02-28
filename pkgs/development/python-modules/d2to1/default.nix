1aa8e259c70133ed7f61067bd50e05a29fc16916
makefu <github@syntax-fehler.de>
2018-02-28 08:28:56 +0100

    pythonPackages.d2to1: 0.2.11 -> 0.2.12

--- pkgs/development/python-modules/d2to1/default.nix
+++ pkgs/development/python-modules/d2to1/default.nix
@@ -1,17 +1,19 @@
 { buildPythonPackage
 , lib
-, fetchPypi
+, fetchFromGitHub
 , nose
 }:
 buildPythonPackage rec {
   pname = "d2to1";
-  version = "0.2.11";
+  version = "0.2.12";
 
   checkInputs = [ nose ];
 
-  src = fetchPypi {
-    inherit version pname;
-    sha256 = "1a5z367b7dpd6dgi0w8pymb68aj2pblk8w04l2c8hibhj8dpl2b4";
+  src = fetchFromGitHub {
+    owner = "embray";
+    repo = pname;
+    rev = version;
+    sha256 = "1q04ab8vjvx7fmq9ckkl8r9hlwwbqiyjbzaa4v1mv5zicfssxwsi";
   };
 
   meta = with lib;{
