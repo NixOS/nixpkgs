516d7a2d616792850a7843c2423794624dd02e0f
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-03-01 12:10:19 +0100

    esptool: 2.1 -> 2.3.1

--- pkgs/tools/misc/esptool/default.nix
+++ pkgs/tools/misc/esptool/default.nix
@@ -2,16 +2,16 @@
 
 python3.pkgs.buildPythonApplication rec {
   name = "esptool-${version}";
-  version = "2.1";
+  version = "2.3.1";
 
   src = fetchFromGitHub {
     owner = "espressif";
     repo = "esptool";
     rev = "v${version}";
-    sha256 = "137p0kcscly95qpjzgx1yxm8k2wf5y9v3srvlhp2ajniirgv8ijv";
+    sha256 = "0gwnl6z5s2ax07l3n38h9hdyz71pn8lzn4fybcwyrii0bj2kapvc";
   };
 
-  buildInputs = with python3.pkgs; [ flake8 flake8-future-import ];
+  checkInputs = with python3.pkgs; [ flake8 flake8-future-import flake8-import-order ];
   propagatedBuildInputs = with python3.pkgs; [ pyserial pyaes ecdsa openssl ];
 
   meta = with stdenv.lib; {
