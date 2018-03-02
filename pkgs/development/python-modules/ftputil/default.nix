83d9c1c76cf057009e050b838105b83b09806c74
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-03-02 03:41:20 +0100

    pythonPackages.ftputil: 3.3 -> 3.4 and fix tests
    (cherry picked from commit 62bb869309e165dadecfbe19a83e8ef7e76e52b7)

--- pkgs/development/python-modules/ftputil/default.nix
+++ pkgs/development/python-modules/ftputil/default.nix
@@ -1,19 +1,27 @@
-{ lib, buildPythonPackage, fetchPypi, isPy3k }:
+{ lib, buildPythonPackage, fetchPypi, pytest }:
 
 buildPythonPackage rec {
-  version = "3.3";
+  version = "3.4";
   pname = "ftputil";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1714w0v6icw2xjx5m54yv2qgkq49qwxwllq4gdb7wkz25iiapr8b";
+    sha256 = "374b01e174079e91babe2a462fbd6f6c00dbfbfa299dec04239ca4229fbf8762";
   };
 
-  disabled = isPy3k;
+  checkInputs = [ pytest ];
+
+  checkPhase = ''
+    touch Makefile
+    # Disable tests that require network access or access /home
+    py.test test \
+      -k "not test_public_servers and not test_real_ftp \
+          and not test_set_parser and not test_repr"
+  '';
 
   meta = with lib; {
     description = "High-level FTP client library (virtual file system and more)";
-    homepage    = https://pypi.python.org/pypi/ftputil;
+    homepage    = http://ftputil.sschwarzer.net/;
     license     = licenses.bsd2; # "Modified BSD license, says pypi"
   };
 }
