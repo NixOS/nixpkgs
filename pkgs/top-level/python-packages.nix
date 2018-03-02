cc2aa6f3589ed6f1e9d134ba9b38622b71103c3f
Andrey Golovizin <ag@sologoc.com>
2018-03-02 16:46:36 +0100

    pythonPackages.pymetar: add myself as maintainer

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12433,6 +12433,7 @@ in {
       description = "A command-line tool to show the weather report by a given station ID";
       homepage = http://www.schwarzvogel.de/software/pymetar.html;
       license = licenses.gpl2;
+      maintainers = with maintainers; [ erosennin ];
     };
   };
 
