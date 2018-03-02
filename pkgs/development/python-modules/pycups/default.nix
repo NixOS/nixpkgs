4d7ed482dcddde49e4f5cbf7b1fe447162ca5a0a
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-03-02 02:38:48 +0100

    pythonPackages.pycups: fix on Darwin
    (cherry picked from commit a5e1dfe3e2836350bcf776c500387cb25fe87e04)

--- pkgs/development/python-modules/pycups/default.nix
+++ pkgs/development/python-modules/pycups/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, fetchurl, cups }:
+{ stdenv, lib, buildPythonPackage, fetchurl, cups, libiconv }:
 
 buildPythonPackage rec {
   pname = "pycups";
@@ -9,7 +9,7 @@ buildPythonPackage rec {
     sha256 = "c381be011889ca6f728598578c89c8ac9f7ab1e95b614474df9f2fa831ae5335";
   };
 
-  buildInputs = [ cups ];
+  buildInputs = [ cups ] ++ lib.optional stdenv.isDarwin libiconv;
 
   # Wants to connect to CUPS
   doCheck = false;
