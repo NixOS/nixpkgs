966223e2aea373e2b1ffda5e7d7ea47f63a7d77f
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-03-02 02:03:28 +0100

    pythonPackages.ldappool: move expression
    (cherry picked from commit f14c9ae8837ce1349b6a08edf8a033ec2f653164)

--- pkgs/development/python-modules/ldappool/default.nix
+++ pkgs/development/python-modules/ldappool/default.nix
@@ -1,9 +1,8 @@
-{ lib, buildPythonPackage, fetchPypi, isPy3k
-, pbr, ldap, fixtures, testresources, testtools }:
+{ lib, buildPythonPackage, fetchPypi, isPy3k }:
 
 buildPythonPackage rec {
   name = "ldappool-${version}";
-  version = "2.2.0";
+  version = "1.0";
 
   src = fetchPypi {
     pname = "ldappool";
@@ -11,15 +10,10 @@ buildPythonPackage rec {
     sha256 = "1akmzf51cjfvmd0nvvm562z1w9vq45zsx6fa72kraqgsgxhnrhqz";
   };
 
-  nativeBuildInputs = [ pbr ];
-
-  propagatedBuildInputs = [ ldap ];
-
-  checkInputs = [ fixtures testresources testtools ];
+  # Judging from SyntaxError
+  disabled = isPy3k;
 
   meta = with lib; {
-    description = "A simple connector pool for python-ldap";
-    homepage = https://git.openstack.org/cgit/openstack/ldappool;
-    license = licenses.mpl20;
+    homepage = "https://github.com/mozilla-services/ldappool";
   };
 }
