c67d4aaf4de503e27bab63f702c5498a1302d467
adisbladis <adis@blad.is>
2018-03-02 10:24:13 +0800

    elixir: 1.6.0 -> 1.6.2

--- pkgs/development/interpreters/elixir/1.6.nix
+++ pkgs/development/interpreters/elixir/1.6.nix
@@ -1,7 +1,7 @@
 { mkDerivation }:
 
 mkDerivation rec {
-  version = "1.6.0";
-  sha256 = "0wfmbrq70n85mx17kl9h2k0xzgnhncz3xygjx9cbvpmiwwdzgrdx";
+  version = "1.6.2";
+  sha256 = "18f5afmvra78y0x73bfnwbddlyqfndyaj1h8n1ybj32w4nvy96y7";
   minimumOTPVersion = "18";
 }
