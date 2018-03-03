dc4060408c940b3568c7a966774709cbad4dd728
Jan Tojnar <jtojnar@gmail.com>
2018-03-03 01:57:27 +0100

    gnome3.libgee: 0.20.0 → 0.20.1

--- pkgs/desktops/gnome-3/core/libgee/default.nix
+++ pkgs/desktops/gnome-3/core/libgee/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, autoconf, vala, pkgconfig, glib, gobjectIntrospection, gnome3 }:
 let
   pname = "libgee";
-  version = "0.20.0";
+  version = "0.20.1";
 in
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${gnome3.versionBranch version}/${name}.tar.xz";
-    sha256 = "1fy24dr8imrjlmsqj1syn0gi139gba6hwk3j5vd6sr3pxniqnc11";
+    sha256 = "0c26x8gi3ivmhlbqcmiag4jwrkvcy28ld24j55nqr3jikb904a5v";
   };
 
   doCheck = true;
@@ -27,6 +27,7 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Utility library providing GObject-based interfaces and classes for commonly used data structures";
+    homepage = https://wiki.gnome.org/Projects/Libgee;
     license = licenses.lgpl21Plus;
     platforms = platforms.unix;
     maintainers = gnome3.maintainers;
