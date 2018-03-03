a5913ce2722b067fdc346a3c06e51ff5aaa1aef9
Frederik Rietdijk <fridh@fridh.nl>
2018-03-03 11:59:25 +0100

    python: you-get: 0.4.1025 -> 0.4.1040

--- pkgs/tools/misc/you-get/default.nix
+++ pkgs/tools/misc/you-get/default.nix
@@ -2,7 +2,7 @@
 
 buildPythonApplication rec {
   pname = "you-get";
-  version = "0.4.1025";
+  version = "0.4.1040";
 
   # Tests aren't packaged, but they all hit the real network so
   # probably aren't suitable for a build environment anyway.
@@ -10,7 +10,7 @@ buildPythonApplication rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1dbalpwhzn39kgswjy84720wfjssa087adavbwig53krsjdvhj6k";
+    sha256 = "fdc9021e8b1cf936aad4bd6c74b80ea8fa3573b807c41242ba781e247f8c8ca8";
   };
 
   meta = with stdenv.lib; {
