3e55c5d1f7311db044ac95c8b5259a560b8d2c66
Graham Christensen <graham@grahamc.com>
2018-03-03 22:34:17 -0500

    Revert "lib/maintainers: rename file to maintainers-list.nix"
    This reverts commit 24684008df7b7c748eeca64de1270a0e6c460a61.

--- lib/default.nix
+++ lib/default.nix
@@ -21,7 +21,7 @@ let
 
     # packaging
     customisation = callLibs ./customisation.nix;
-    maintainers = import ./maintainers-list.nix;
+    maintainers = import ./maintainers.nix;
     meta = callLibs ./meta.nix;
     sources = callLibs ./sources.nix;
 
