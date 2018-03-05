d9f179252d980e60e20bb87be51ca4c9f67ffc39
Vladimír Čunát <vcunat@gmail.com>
2018-03-05 21:16:31 +0100

    WIP:
    waiting for https://github.com/NixOS/nixos-org-configurations/pull/39

--- nixos/modules/misc/version.nix
+++ nixos/modules/misc/version.nix
@@ -70,7 +70,7 @@ in
     defaultChannel = mkOption {
       internal = true;
       type = types.str;
-      default = https://nixos.org/channels/nixos-unstable;
+      default = https://nixos.org/channels/nixos-18.03;
       description = "Default NixOS channel to which the root user is subscribed.";
     };
 
