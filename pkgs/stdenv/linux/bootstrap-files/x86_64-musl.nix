856335396f0a0208474fb033b96e9dfb7d194dab
Will Dietz <w@wdtz.org>
2018-03-05 16:42:14 -0600

    x86_64-musl: use native-built bootstrap package on x86_64
    Built using:
    https://github.com/NixOS/nixpkgs/commits/46f83fa9d1bf00b09b1e9a9aa83f577c246ac674

--- pkgs/stdenv/linux/bootstrap-files/x86_64-musl.nix
+++ pkgs/stdenv/linux/bootstrap-files/x86_64-musl.nix
@@ -5,7 +5,7 @@
     executable = true;
   };
   bootstrapTools = import <nix/fetchurl.nix> {
-    url = https://wdtz.org/files/030q34q7fk6jdfxkgcqp5rzr4yhw3pgx-stdenv-bootstrap-tools-x86_64-unknown-linux-musl/on-server/bootstrap-tools.tar.xz;
-    sha256 = "0ly0wj8wzbikn2j8sn727vikk90bq36drh98qvfx1kkh5k5azm2j";
+    url = https://wdtz.org/files/2m15z3pmg495w52jc8dg2nbxxzmzvb04-stdenv-bootstrap-tools/on-server/bootstrap-tools.tar.xz;
+    sha256 = "1w66l0ra0sfy83hs80w6l0lb015hrhdg3xd89xh4c5kr8bcrjriw";
   };
 }
