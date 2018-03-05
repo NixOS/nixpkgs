49e02c53f04563f2fe2e56a8d1427739caec27ce
Jan Tojnar <jtojnar@gmail.com>
2018-03-05 01:32:07 +0100

    gnome3.libgit2-glib: fix build

--- pkgs/desktops/gnome-3/misc/libgit2-glib/default.nix
+++ pkgs/desktops/gnome-3/misc/libgit2-glib/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, fetchurl, gnome3, libtool, pkgconfig, vala, libssh2
-, gtk-doc, gobjectIntrospection, libgit2, glib }:
+{ stdenv, fetchurl, gnome3, meson, ninja, pkgconfig, vala, libssh2
+, gtk-doc, gobjectIntrospection, libgit2, glib, python3 }:
 
 stdenv.mkDerivation rec {
   name = "libgit2-glib-${version}";
@@ -10,12 +10,19 @@ stdenv.mkDerivation rec {
     sha256 = "0nhyqas110q7ingw97bvyjdb7v4dzch517dq8sn8c33s8910wqcp";
   };
 
+  postPatch = ''
+    for f in meson_vapi_link.py meson_python_compile.py; do
+      chmod +x $f
+      patchShebangs $f
+    done
+  '';
+
   passthru = {
     updateScript = gnome3.updateScript { packageName = "libgit2-glib"; attrPath = "gnome3.libgit2-glib"; };
   };
 
   nativeBuildInputs = [
-    gnome3.gnome-common libtool pkgconfig vala gtk-doc gobjectIntrospection
+    meson ninja pkgconfig vala gtk-doc gobjectIntrospection
   ];
 
   propagatedBuildInputs = [
@@ -23,7 +30,10 @@ stdenv.mkDerivation rec {
     libgit2 glib
   ];
 
-  buildInputs = [ libssh2 ];
+  buildInputs = [
+    libssh2
+    python3.pkgs.pygobject3 # this should really be a propagated input of python output
+  ];
 
   meta = with stdenv.lib; {
     description = "A glib wrapper library around the libgit2 git access library";
