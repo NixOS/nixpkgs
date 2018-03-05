d7c2cffd86978999b14a671b0976306366cb16a8
Will Dietz <w@wdtz.org>
2018-03-05 17:34:45 -0600

    x86_64-musl: regen again w/symlinks preserved

--- pkgs/stdenv/linux/bootstrap-files/x86_64-musl.nix
+++ pkgs/stdenv/linux/bootstrap-files/x86_64-musl.nix
@@ -1,11 +1,11 @@
 {
   busybox = import <nix/fetchurl.nix> {
-    url = https://wdtz.org/files/030q34q7fk6jdfxkgcqp5rzr4yhw3pgx-stdenv-bootstrap-tools-x86_64-unknown-linux-musl/on-server/busybox;
-    sha256 = "16lzrwwvdk6q3g08gs45pldz0rh6xpln2343xr444960h6wqxl5v";
+    url = https://wdtz.org/files/6bh4giyw5rf8mc28621rxipw8d6w6w8d-stdenv-bootstrap-tools/on-server/busybox;
+    sha256 = "0779c2wn00467h76xpqil678gfi1y2p57c7zq2d917jsv2qj5009";
     executable = true;
   };
   bootstrapTools = import <nix/fetchurl.nix> {
-    url = https://wdtz.org/files/2m15z3pmg495w52jc8dg2nbxxzmzvb04-stdenv-bootstrap-tools/on-server/bootstrap-tools.tar.xz;
-    sha256 = "1w66l0ra0sfy83hs80w6l0lb015hrhdg3xd89xh4c5kr8bcrjriw";
+    url = https://wdtz.org/files/6bh4giyw5rf8mc28621rxipw8d6w6w8d-stdenv-bootstrap-tools/on-server/bootstrap-tools.tar.xz;
+    sha256 = "197h8gjw51q3i25myapzgqba2l4h2skzwi3q1iry26mzjjmbcvys";
   };
 }
