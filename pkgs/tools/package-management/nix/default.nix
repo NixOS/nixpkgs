36864d2bf74b50769a35b66d25937f2290742b82
Will Dietz <w@wdtz.org>
2018-03-05 17:23:47 -0600

    nix: fix conditional so brotli is used w/2.0 builds, needed for logs!
    cc https://github.com/NixOS/nix/issues/1951
    
    (cherry picked from commit 7dafa09ed93e8fb932870d839b0668c9ed1c92ee)

--- pkgs/tools/package-management/nix/default.nix
+++ pkgs/tools/package-management/nix/default.nix
@@ -42,7 +42,7 @@ let
 
     buildInputs = [ curl openssl sqlite xz ]
       ++ lib.optional (stdenv.isLinux || stdenv.isDarwin) libsodium
-      ++ lib.optionals fromGit [ brotli ] # Since 1.12
+      ++ lib.optionals is20 [ brotli ] # Since 1.12
       ++ lib.optional stdenv.isLinux libseccomp
       ++ lib.optional ((stdenv.isLinux || stdenv.isDarwin) && is20)
           (aws-sdk-cpp.override {
