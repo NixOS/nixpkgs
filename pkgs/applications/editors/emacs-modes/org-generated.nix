7881ffdb786567504c2a247fdd15d0c12f692a2e
Michael Alan Dorman <mdorman@ironicdesign.com>
2018-03-05 12:44:49 -0500

    org-packages: 2018-04-01

--- pkgs/applications/editors/emacs-modes/org-generated.nix
+++ pkgs/applications/editors/emacs-modes/org-generated.nix
@@ -1,10 +1,10 @@
 { callPackage }: {
     org = callPackage ({ elpaBuild, fetchurl, lib }: elpaBuild {
         pname = "org";
-        version = "20180226";
+        version = "20180402";
         src = fetchurl {
-          url = "https://orgmode.org/elpa/org-20180226.tar";
-          sha256 = "0jqvry6gah1bwnryha4asynj13jyds3qim0xcy7s01rxk99m2ziy";
+          url = "https://orgmode.org/elpa/org-20180402.tar";
+          sha256 = "0gb8hh26jzjqy262ll8jl3ym0cpw6s17id2gizv5qvw18knxs751";
         };
         packageRequires = [];
         meta = {
@@ -14,10 +14,10 @@
       }) {};
     org-plus-contrib = callPackage ({ elpaBuild, fetchurl, lib }: elpaBuild {
         pname = "org-plus-contrib";
-        version = "20180226";
+        version = "20180402";
         src = fetchurl {
-          url = "https://orgmode.org/elpa/org-plus-contrib-20180226.tar";
-          sha256 = "034wp70hcqnpidji5k1k80mj35iyyy098nbvc2sl7i2aca4m03zc";
+          url = "https://orgmode.org/elpa/org-plus-contrib-20180402.tar";
+          sha256 = "09q5nr0ka7z719mi626wj5d51bcvdb08gk4zf94dzpks0gsqiikr";
         };
         packageRequires = [];
         meta = {
