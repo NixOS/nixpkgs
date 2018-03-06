7b987e2e86b1b46a029f5800edd73fab6cef7b89
Michael Raskin <7c6f434c@mail.ru>
2018-03-06 19:56:27 +0000

    Merge pull request #36389 from cpages/sdl2
    Sdl2 and friends update
    
    (cherry picked from commit c8664a2640dea279ee2b623ea46aa3c4178770df)
    Security fixes inside, reportedly.

--- pkgs/development/libraries/SDL2_image/default.nix
+++ pkgs/development/libraries/SDL2_image/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "SDL2_image-${version}";
-  version = "2.0.2";
+  version = "2.0.3";
 
   src = fetchurl {
     url = "http://www.libsdl.org/projects/SDL_image/release/${name}.tar.gz";
-    sha256 = "1s3ciydixrgv34vlf45ak5syq5nlfaqf19wf162lbz4ixxd0gpvj";
+    sha256 = "0s13dmakn21q6yw8avl67d4zkxzl1wap6l5nwf6cvzrmlxfw441m";
   };
 
   buildInputs = [ SDL2 libpng libjpeg libtiff libungif libXpm zlib ]
@@ -19,5 +19,6 @@ stdenv.mkDerivation rec {
     homepage = http://www.libsdl.org/projects/SDL_image/;
     platforms = platforms.unix;
     license = licenses.zlib;
+    maintainers = with maintainers; [ cpages ];
   };
 }
