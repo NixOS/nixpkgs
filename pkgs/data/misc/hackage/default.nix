32471ea14b480a69e59237831fa979d17990a66f
Peter Simons <simons@cryp.to>
2018-03-06 21:24:50 +0100

    hackage: update database snapshot to latest version

--- pkgs/data/misc/hackage/default.nix
+++ pkgs/data/misc/hackage/default.nix
@@ -1,6 +1,6 @@
 { fetchurl }:
 
 fetchurl {
-  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/30eca1c57b9358eff984ab93dbfd213d6b08706b.tar.gz";
-  sha256 = "0kd5y2jlydbr1jv2ckdpsn2ialbxk2aj19c8i0brzz0wwzirg7q5";
+  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/56955295749aea09bd42591cd506c2616d48ebfd.tar.gz";
+  sha256 = "17c8a1wj1jxr14sxvwq0i9kidwycz2hkl1ls51yfz14anylxr84l";
 }
