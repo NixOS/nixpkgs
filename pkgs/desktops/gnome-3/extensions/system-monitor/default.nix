b443d3cb2d676df240dbd685ae3965c1f3112248
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2018-03-07 09:24:31 +0100

    gnomeExtensions.system-monitor: fix this package and upgrade to v33
    (cherry picked from commit 0935cbf2872d2b6fff3b700eeae324ad6e317068)

--- pkgs/desktops/gnome-3/extensions/system-monitor/default.nix
+++ pkgs/desktops/gnome-3/extensions/system-monitor/default.nix
@@ -1,26 +1,33 @@
-{ stdenv, fetchFromGitHub, glib }:
+# This package needs the following configuration in configuration.nix:
+# services.xserver.desktopManager.gnome3.sessionPath = [ pkgs.libgtop pkgs.glib_networking ];
+{ config, stdenv, fetchFromGitHub, glib, glib_networking, libgtop, pkgs }:
 
 stdenv.mkDerivation rec {
   name = "gnome-shell-system-monitor-${version}";
-  version = "8b31f070e9e59109d729661ced313d6a63e31787";
+  version = "v33";
 
   src = fetchFromGitHub {
     owner = "paradoxxxzero";
     repo = "gnome-shell-system-monitor-applet";
     rev = version;
-    sha256 = "0fm5zb6qp53jjy2mnkb8ybxygzjwpb314giiq0ywq87hhrpch8m3";
+    sha256 = "0abqaanl5r26x8f0mm0jgrjsr86hcx7mk75dx5c3zz7csw4nclkk";
   };
 
   buildInputs = [
     glib
+    glib_networking
+    libgtop
   ];
+
+  patches = [ ./remove_nonexisting_mounts.patch ];
 
   buildPhase = ''
     ${glib.dev}/bin/glib-compile-schemas --targetdir=${uuid}/schemas ${uuid}/schemas
   '';
 
   installPhase = ''
-    cp -r ${uuid} $out
+    mkdir -p $out/share/gnome-shell/extensions
+    cp -r ${uuid} $out/share/gnome-shell/extensions
   '';
 
   uuid = "system-monitor@paradoxxx.zero.gmail.com";
@@ -28,7 +35,7 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     description = "Display system informations in gnome shell status bar";
     license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ aneeshusa ];
+    maintainers = with maintainers; [ aneeshusa tiramiseb ];
     homepage = https://github.com/paradoxxxzero/gnome-shell-system-monitor-applet;
   };
 }
