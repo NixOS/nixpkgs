c2cf630c739ccbe5e95ccb7ef8adae5fe995b659
Timo Kaufmann <timokau@zoho.com>
2018-03-07 19:30:54 +0100

    pythonPackages.pkgconfig: 1.1.0 -> 1.3.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12516,14 +12516,14 @@ in {
 
   pkgconfig = buildPythonPackage rec {
     name = "pkgconfig-${version}";
-    version = "1.1.0";
+    version = "1.3.1";
 
     # pypy: SyntaxError: __future__ statements must appear at beginning of file
     disabled = isPyPy;
 
     src = pkgs.fetchurl {
       url = "mirror://pypi/p/pkgconfig/${name}.tar.gz";
-      sha256 = "709daaf077aa2b33bedac12706373412c3683576a43013bbaa529fc2769d80df";
+      sha256 = "107x2wmchlch8saixb488cgjz9n6inl38wi7nxkb942rbaapxiqb";
     };
 
     buildInputs = with self; [ nose ];
