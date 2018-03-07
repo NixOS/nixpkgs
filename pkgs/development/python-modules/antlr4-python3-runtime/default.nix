df4237761505c754e03438a5c61f0f7329ef57f9
Maxime Dénès <mail@maximedenes.fr>
2018-03-07 19:10:17 +0100

    pythonPackages.antlr4-python3-runtime: init at 4.7.1

---
+++ pkgs/development/python-modules/antlr4-python3-runtime/default.nix
@@ -0,0 +1,17 @@
+{buildPythonPackage, isPy3k, pkgs}:
+buildPythonPackage rec {
+  version = "4.7.1";
+  name = "antlr4-python3-runtime-${version}";
+  disabled = !isPy3k;
+
+  src = pkgs.fetchurl {
+    url = "mirror://pypi/a/antlr4-python3-runtime/${name}.tar.gz";
+    sha256 = "1lrzmagawmavyw1n1z0qarvs2jmbnbv0p89dah8g7klj8hnbf9hv";
+  };
+
+  meta = {
+    description = "Runtime for ANTLR";
+    homepage = "http://www.antlr.org/";
+    license = pkgs.lib.licenses.bsd3;
+  };
+}
