e39f948cb0c9c7e6e2f977f61a3bdc7052536140
Alex Brandt <alunduil@alunduil.com>
2018-03-07 11:02:22 -0600

    parse: 1.6.6 -> 1.8.2

--- pkgs/development/python-modules/parse/default.nix
+++ pkgs/development/python-modules/parse/default.nix
@@ -3,21 +3,13 @@
 }:
 buildPythonPackage rec {
   pname = "parse";
-  version = "1.6.6";
+  version = "1.8.2";
   name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "71435aaac494e08cec76de646de2aab8392c114e56fe3f81c565ecc7eb886178";
+    sha256 = "1lj9v1q4imszyhvipb6drsm3xdl35nan011mqxxas1yaypixsj40";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "python-3.5-tests-compat.patch";
-      url = "https://github.com/r1chardj0n3s/parse/pull/34.patch";
-      sha256 = "16iicgkf3lwivmdnp3xkq4n87wjmr3nb77z8mwz67b7by9nnp3jg";
-    })
-  ];
 
   checkPhase = ''
     ${python.interpreter} test_parse.py
