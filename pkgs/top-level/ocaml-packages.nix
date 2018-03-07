bd6d6078c601a5af90dd4ca7a96df2c58bd7bde9
Vincent Laporte <Vincent.Laporte@gmail.com>
2018-03-07 16:52:35 +0000

    wyrd: fix build
    (cherry picked from commit 1d4bbef0e88a066521ec4fef1e64220149fcadca)

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -955,10 +955,6 @@ let
 
     # Apps / from all-packages
 
-    wyrd = callPackage ../tools/misc/wyrd {
-      ncurses = pkgs.ncurses5;
-    };
-
     haxe = callPackage ../development/compilers/haxe { };
 
     ocamlnat = callPackage  ../development/ocaml-modules/ocamlnat { };
