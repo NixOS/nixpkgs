6290ae54127efb90164e1248382a713a37d9f47b
Ryan Mulligan <ryan@ryantm.com>
2018-03-08 13:55:38 -0800

    gperftools: 2.6.1 -> 2.6.3
    Semi-automatic update. These checks were done:
    
    - built on NixOS
    - ran `/nix/store/ba54ymdab4y2jcxwbmn71sm698cy1wik-gperftools-2.6.3/bin/pprof --help` got 0 exit code
    - ran `/nix/store/ba54ymdab4y2jcxwbmn71sm698cy1wik-gperftools-2.6.3/bin/pprof --help` and found version 2.6.3
    - found 2.6.3 with grep in /nix/store/ba54ymdab4y2jcxwbmn71sm698cy1wik-gperftools-2.6.3
    - found 2.6.3 in filename of file in /nix/store/ba54ymdab4y2jcxwbmn71sm698cy1wik-gperftools-2.6.3

--- pkgs/development/libraries/gperftools/default.nix
+++ pkgs/development/libraries/gperftools/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, libunwind }:
 
 stdenv.mkDerivation rec {
-  name = "gperftools-2.6.1";
+  name = "gperftools-2.6.3";
 
   src = fetchurl {
     url = "https://github.com/gperftools/gperftools/releases/download/${name}/${name}.tar.gz";
-    sha256 = "10cxd6s5pkm2d934gh47hrn9xcrw4qlc9yr7s99z4a508bmngd1q";
+    sha256 = "17zfivp6n00rlqbrx6q6h71y2f815nvlzysff1ihgk4mxpv2yjri";
   };
 
   buildInputs = stdenv.lib.optional stdenv.isLinux libunwind;
