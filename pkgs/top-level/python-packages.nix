2963d76ca34c4ccddf81348194ca5335f30787a3
Shea Levy <shea@shealevy.com>
2018-03-08 10:22:37 -0500

    pythonPackages.pyspark: Init at 2.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -20654,6 +20654,8 @@ EOF
 
   spacy_models = callPackage ../development/python-modules/spacy/models.nix { };
 
+  pyspark = callPackage ../development/python-modules/pyspark { };
+
   sseclient = callPackage ../development/python-modules/sseclient { };
 
   textacy = callPackage ../development/python-modules/textacy { };
