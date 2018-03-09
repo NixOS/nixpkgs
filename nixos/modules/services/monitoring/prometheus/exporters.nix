aff77d8e2601cbf49ac37ad60588f5240f76dff5
WilliButz <wbutz@cyberfnord.de>
2018-03-09 21:33:09 +0100

    nixos/prometheus-exporters: rewrite and restructure
    - prometheus exporters are now configured with
      `services.prometheus.exporters.<name>`
    - the exporters are now defined by attribute sets
      from which the options for each exporter are generated
    - most of the exporter definitions are used unchanged,
      except for some changes that should't have any impact
      on the functionality.
    
    (cherry picked from commit f4d03b5c9cf2b694792dec88e3ca805cf7b43e3c)

--- nixos/modules/services/monitoring/prometheus/exporters.nix
+++ nixos/modules/services/monitoring/prometheus/exporters.nix
@@ -25,7 +25,6 @@ let
     minio    = import ./exporters/minio.nix    { inherit config lib pkgs; };
     nginx    = import ./exporters/nginx.nix    { inherit config lib pkgs; };
     node     = import ./exporters/node.nix     { inherit config lib pkgs; };
-    postfix  = import ./exporters/postfix.nix  { inherit config lib pkgs; };
     snmp     = import ./exporters/snmp.nix     { inherit config lib pkgs; };
     unifi    = import ./exporters/unifi.nix    { inherit config lib pkgs; };
     varnish  = import ./exporters/varnish.nix  { inherit config lib pkgs; };
@@ -167,6 +166,4 @@ in
       conf = cfg.${name};
     }) exporterOpts)
   );
-
-  meta.doc = ./exporters.xml;
 }
