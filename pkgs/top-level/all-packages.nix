fc3b588207f646bc9e8fb664dc8ee7f0d69424ed
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-03-09 23:01:31 +0100

    mpv: fix darwin build
    The optional video acceleration api (vaapi) is not supported on darwin.
    
    (cherry picked from commit 38c589b5d0b66fd2d244ab8fa0d9fc135887bde5)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16632,6 +16632,7 @@ with pkgs;
     dvdreadSupport     = !stdenv.isDarwin;
     dvdnavSupport      = !stdenv.isDarwin;
     drmSupport         = !stdenv.isDarwin;
+    vaapiSupport       = !stdenv.isDarwin;
     x11Support         = !stdenv.isDarwin;
     xineramaSupport    = !stdenv.isDarwin;
     xvSupport          = !stdenv.isDarwin;
