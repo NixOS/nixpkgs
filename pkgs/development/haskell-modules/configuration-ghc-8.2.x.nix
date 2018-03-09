d825500f01c313d1c4e90fae7c1ea3e52d9f52d0
Peter Simons <simons@cryp.to>
2018-03-09 15:35:49 +0100

    Cabal-2.2.0.0: fix build with GHC 8.2.2

--- pkgs/development/haskell-modules/configuration-ghc-8.2.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.2.x.nix
@@ -91,23 +91,4 @@ self: super: {
     text = self.text_1_2_3_0;
   });
 
-  # These packages need Cabal 2.2.x, which is not the default.
-  distribution-nixpkgs = super.distribution-nixpkgs.overrideScope (self: super: {
-    Cabal = self.Cabal_2_2_0_0;
-    text = self.text_1_2_3_0;
-  });
-  hackage-db_2_0_1 = super.hackage-db_2_0_1.overrideScope (self: super: {
-    Cabal = self.Cabal_2_2_0_0;
-    text = self.text_1_2_3_0;
-  });
-  cabal2nix = super.cabal2nix.overrideScope (self: super: {
-    Cabal = self.Cabal_2_2_0_0;
-    text = self.text_1_2_3_0;
-  });
-  stylish-cabal = dontHaddock (dontCheck (super.stylish-cabal.overrideScope (self: super: {
-    Cabal = self.Cabal_2_2_0_0;
-    text = self.text_1_2_3_0;
-    haddock-library = dontHaddock (dontCheck self.haddock-library_1_5_0_1);
-  })));
-
 }
