89a3398fe506e726e60cc346cf34bd58c21d735f
Jan Tojnar <jtojnar@gmail.com>
2018-03-10 00:53:54 +0100

    gnome3.gnome-control-center: add gnome-color-manager support

--- pkgs/desktops/gnome-3/core/gnome-control-center/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-control-center/default.nix
@@ -1,26 +1,21 @@
-{ fetchurl, stdenv, pkgconfig, gnome3, ibus, intltool, upower, wrapGAppsHook
+{ fetchurl, stdenv, substituteAll, pkgconfig, gnome3, ibus, intltool, upower, wrapGAppsHook
 , libcanberra-gtk3, accountsservice, libpwquality, libpulseaudio
-, gdk_pixbuf, librsvg, libnotify, libgudev
+, gdk_pixbuf, librsvg, libnotify, libgudev, gnome-color-manager
 , libxml2, polkit, libxslt, libgtop, libsoup, colord, colord-gtk
-, cracklib, libkrb5, networkmanagerapplet, networkmanager
-, libwacom, samba, shared-mime-info, tzdata, libtool
+, cracklib, libkrb5, networkmanagerapplet, networkmanager, glibc
+, libwacom, samba, shared-mime-info, tzdata, libtool, libgnomekbd
 , docbook_xsl, docbook_xsl_ns, modemmanager, clutter, clutter-gtk
 , fontconfig, sound-theme-freedesktop, grilo }:
 
-stdenv.mkDerivation rec {
-  name = "gnome-control-center-${version}";
+let
   version = "3.26.2";
+in stdenv.mkDerivation rec {
+  name = "gnome-control-center-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-control-center/${gnome3.versionBranch version}/${name}.tar.xz";
     sha256 = "07aed27d6317f2cad137daa6d94a37ad02c32b958dcd30c8f07d0319abfb04c5";
   };
-
-  passthru = {
-    updateScript = gnome3.updateScript { packageName = "gnome-control-center"; attrPath = "gnome3.gnome-control-center"; };
-  };
-
-  propagatedUserEnvPkgs = [ gnome3.gnome-themes-standard ];
 
   nativeBuildInputs = [
     pkgconfig intltool wrapGAppsHook libtool libxslt docbook_xsl docbook_xsl_ns
@@ -37,15 +32,13 @@ stdenv.mkDerivation rec {
     networkmanager modemmanager gnome-bluetooth tracker
   ];
 
-  preBuild = ''
-    substituteInPlace panels/datetime/tz.h --replace "/usr/share/zoneinfo/zone.tab" "${tzdata}/share/zoneinfo/zone.tab"
-
-    substituteInPlace panels/region/cc-region-panel.c --replace "gkbd-keyboard-display" "${gnome3.libgnomekbd}/bin/gkbd-keyboard-display"
-
-    # hack to make test-endianess happy
-    mkdir -p $out/share/locale
-    substituteInPlace panels/datetime/test-endianess.c --replace "/usr/share/locale/" "$out/share/locale/"
-  '';
+  patches = [
+    (substituteAll {
+      src = ./paths.patch;
+      gcm = gnome-color-manager;
+      inherit glibc libgnomekbd tzdata;
+    })
+  ];
 
   preFixup = ''
     gappsWrapperArgs+=(
@@ -58,6 +51,13 @@ stdenv.mkDerivation rec {
       substituteInPlace $i --replace "gnome-control-center" "$out/bin/gnome-control-center"
     done
   '';
+
+  passthru = {
+    updateScript = gnome3.updateScript {
+      packageName = "gnome-control-center";
+      attrPath = "gnome3.gnome-control-center";
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Utilities to configure the GNOME desktop";
