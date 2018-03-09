49c9d5ee727c9f03423bc3876826d6c16f51f208
Tim Steinbach <tim@nequissimus.com>
2018-03-09 13:12:48 -0500

    atom: 1.24.0 -> 1.24.1
    (cherry picked from commit 8338d38e20ed0ab961430d8629dfd3a16ad169bd)

--- pkgs/applications/editors/atom/default.nix
+++ pkgs/applications/editors/atom/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "atom-${version}";
-  version = "1.25.1";
+  version = "1.24.1";
 
   src = fetchurl {
     url = "https://github.com/atom/atom/releases/download/v${version}/atom-amd64.deb";
-    sha256 = "0h0kr4w26c6i89rb9y4aw4l8f63al42i2cy3ddk16m1irzij9fk4";
+    sha256 = "0llg2q1hn2wmhi80ddwwb2lir2xwwmrw19i2c1y3qmfb0apynhw3";
     name = "${name}.deb";
   };
 
