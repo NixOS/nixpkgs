f9387d8e332f1203a1e988a68adcadc1e2b87eb3
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-03-11 00:15:41 +0100

    pythonPackages.discid: fix darwin build
    (cherry picked from commit 57d862fecf6f69aa1786fcab202dce798e3b380c)

--- pkgs/development/python-modules/discid/default.nix
+++ pkgs/development/python-modules/discid/default.nix
@@ -10,15 +10,17 @@ buildPythonPackage rec {
     sha256 = "78a3bf6c8377fdbe3d85e914a209ff97aa43e35605779639847b510ced31f7b9";
   };
 
-  patchPhase = ''
-    substituteInPlace discid/libdiscid.py \
-      --replace '_open_library(_LIB_NAME)' "_open_library('${libdiscid}/lib/libdiscid.so.0')"
-  '';
+  patchPhase =
+    let extension = stdenv.hostPlatform.extensions.sharedLibrary; in
+    ''
+      substituteInPlace discid/libdiscid.py \
+        --replace "_open_library(_LIB_NAME)" \
+                  "_open_library('${libdiscid}/lib/libdiscid${extension}')"
+    '';
 
   meta = with stdenv.lib; {
     description = "Python binding of libdiscid";
     homepage    = "https://python-discid.readthedocs.org/";
     license     = licenses.lgpl3Plus;
-    platforms   = platforms.linux;
   };
 }
