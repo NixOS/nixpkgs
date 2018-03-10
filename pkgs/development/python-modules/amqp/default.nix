79f81972c30732e438e9049c1540d8bf7fcbe71b
Elis Hirwing <elis@hirwing.se>
2018-03-10 13:26:50 +0100

    amqp: move to own directory

---
+++ pkgs/development/python-modules/amqp/default.nix
@@ -0,0 +1,22 @@
+{ stdenv, buildPythonPackage, fetchPypi, pythonOlder, pytest, case, vine }:
+
+buildPythonPackage rec {
+  pname = "amqp";
+  version = "2.1.4";
+
+  disabled = pythonOlder "2.6";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1ybywzkd840v1qvb1p2bs08js260vq1jscjg8182hv7bmwacqy0k";
+  };
+
+  buildInputs = [ pytest case ];
+  propagatedBuildInputs = [ vine ];
+
+  meta = with stdenv.lib; {
+    homepage = https://github.com/celery/py-amqp;
+    description = "Python client for the Advanced Message Queuing Procotol (AMQP). This is a fork of amqplib which is maintained by the Celery project";
+    license = licenses.lgpl21;
+  };
+}
