b24eccc23055df3b8225a722f0bf5b55ce5097f6
Johannes Frankenau <johannes@frankenau.net>
2018-03-11 10:40:56 +0100

    urlscan: 0.8.6 -> 0.8.7

--- pkgs/applications/misc/urlscan/default.nix
+++ pkgs/applications/misc/urlscan/default.nix
@@ -1,20 +1,23 @@
 { stdenv, python3Packages, fetchFromGitHub }:
 
 python3Packages.buildPythonApplication rec {
-  name = "urlscan-${version}";
-  version = "0.8.6";
+  pname = "urlscan";
+  version = "0.8.7";
 
   src = fetchFromGitHub {
     owner = "firecat53";
-    repo = "urlscan";
+    repo = pname;
     rev = version;
-    sha256 = "1v26fni64n0lbv37m35plh2bsrvhpb4ibgmg2mv05qfc3df721s5";
+    sha256 = "1jxjcq869jimsq1ihk2fbjhp5lj7yga0hbp0msskxyz92afl1kz8";
   };
 
   propagatedBuildInputs = [ python3Packages.urwid ];
 
+  doCheck = false; # No tests available
+
   meta = with stdenv.lib; {
     description = "Mutt and terminal url selector (similar to urlview)";
+    homepage = https://github.com/firecat53/urlscan;
     license = licenses.gpl2;
     maintainers = with maintainers; [ dpaetzel jfrankenau ];
   };
