5b1cd5d27b1e3e3271a09836e06d9d79d6886978
Johannes Frankenau <johannes@frankenau.net>
2018-03-11 19:05:41 +0100

    f3: 6.0-2016.11.16-unstable -> 7.0

--- pkgs/tools/filesystems/f3/default.nix
+++ pkgs/tools/filesystems/f3/default.nix
@@ -1,27 +1,26 @@
 { stdenv, fetchFromGitHub
 , parted, udev
 }:
-let
-  version = "6.0-2016.11.16-unstable";
-in
+
 stdenv.mkDerivation rec {
-  name = "f3-${version}";
+  name = "${pname}-${version}";
+  pname = "f3";
+  version = "7.0";
 
   enableParallelBuilding = true;
 
   src = fetchFromGitHub {
     owner = "AltraMayor";
-    repo = "f3";
-    rev = "eabf001f69a788e64912bc9e812c118a324077d5";
-    sha256 = "0ypqyqwqiy3ynssdd9gamk1jxywg6avb45ndlzhv3wxh2qcframm";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "195j2zd747ffbsl8p5rf7dyn1j5n05zfqy1s9fm4y6lz8yc1nr17";
   };
 
   buildInputs = [ parted udev ];
 
   patchPhase = "sed -i 's/-oroot -groot//' Makefile";
 
-  buildFlags   = [ "CFLAGS=-fgnu89-inline"  # HACK for weird gcc incompatibility with -O2
-                   "all"                    # f3read, f3write
+  buildFlags   = [ "all"                    # f3read, f3write
                    "extra"                  # f3brew, f3fix, f3probe
                  ];
 
