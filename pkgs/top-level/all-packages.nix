c568f4d21a63be29a8b7e61be094e3cfe2f8e72b
Matthias Beyer <mail@beyermatthias.de>
2018-03-11 16:59:29 +0100

    lumail: Use lua5.1
    This is necessary because the standard library which is distributed with
    lumail (the lumail core configuration so to speak) is written for lua5.1
    apparently.
    
    The website states 5.1 or 5.2 or 5.3, but 5.2 fails because "loadstring"
    was deprecated in lua 5.2.
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>
    (cherry picked from commit 13e95f33db894f7d7a46f1ffe054fe56ba83b030)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16405,7 +16405,9 @@ with pkgs;
 
   looking-glass-client = callPackage ../applications/virtualization/looking-glass-client { };
 
-  lumail = callPackage ../applications/networking/mailreaders/lumail { };
+  lumail = callPackage ../applications/networking/mailreaders/lumail {
+    lua = lua5_1;
+  };
 
   lv2bm = callPackage ../applications/audio/lv2bm { };
 
