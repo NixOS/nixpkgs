5fdfe61b35945b287b084f22076fef34062f17d2
Timo Kaufmann <timokau@zoho.com>
2018-03-11 15:21:01 +0100

    conan: Fix build
    conan has very strict requirements on the versions of its dependencies.
    This patch adds downgraded versinos of node-semver and distro to
    statisfy these requirements.

--- pkgs/development/tools/build-managers/conan/default.nix
+++ pkgs/development/tools/build-managers/conan/default.nix
@@ -1,26 +1,28 @@
 { lib, buildPythonApplication, fetchPypi
 , requests, fasteners, pyyaml, pyjwt, colorama, patch
-, bottle, pluginbase, six, distro, pylint, node-semver
+, bottle, pluginbase, six, distro11, pylint, node-semver2
 , future, pygments, mccabe
+, fetchpatch
 }:
 
 buildPythonApplication rec {
-  version = "0.28.1";
+  version = "1.1.1";
   pname = "conan";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0zf564iqh0099yd779f9fgk21qyp87d7cmgfj34hmncf8y3qh32a";
+    sha256 = "1k1r401bc9fgmhd5n5f29mjcn346r3zdrm7p28nwpr2r2p3fslrl";
   };
 
   propagatedBuildInputs = [
     requests fasteners pyyaml pyjwt colorama patch
-    bottle pluginbase six distro pylint node-semver
+    bottle pluginbase six distro11 pylint node-semver2
     future pygments mccabe
   ];
 
   # enable tests once all of these pythonPackages available:
   # [ nose nose_parameterized mock webtest codecov ]
+  # update 2018-03-11: only nose_parameterized is missing
   doCheck = false;
 
   meta = with lib; {
