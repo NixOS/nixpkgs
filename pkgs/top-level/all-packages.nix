624e8ca4daaf326e0a0154571a1d55147c3f7d9c
Michael Raskin <7c6f434c@mail.ru>
2018-03-11 15:44:20 +0100

    fmbt: init at 0.39

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2298,6 +2298,10 @@ with pkgs;
   flameGraph = flamegraph;
 
   flvtool2 = callPackage ../tools/video/flvtool2 { };
+  
+  fmbt = callPackage ../development/tools/fmbt {
+    python = python2;
+  };
 
   fontforge = lowPrio (callPackage ../tools/misc/fontforge {
     inherit (darwin.apple_sdk.frameworks) Carbon Cocoa;
