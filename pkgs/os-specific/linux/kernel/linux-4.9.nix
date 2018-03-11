b416dd1b997fe00ff143d6f72a724c4c4f76d1d9
Tim Steinbach <tim@nequissimus.com>
2018-03-11 13:45:54 -0400

    linux: 4.9.86 -> 4.9.87

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.92";
+  version = "4.9.87";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0r3fg3za68p6ls40svryqxixmlia2ql3hqnd1kvvrdw6wqgap382";
+    sha256 = "05y9wjmshd3pr3ymfpx80hjv5973i6l3zk1mpww7wnnwd6pzdjbs";
   };
 } // (args.argsOverride or {}))
