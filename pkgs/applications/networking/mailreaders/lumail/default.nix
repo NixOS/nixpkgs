13e95f33db894f7d7a46f1ffe054fe56ba83b030
Matthias Beyer <mail@beyermatthias.de>
2018-03-11 16:59:29 +0100

    lumail: Use lua5.1
    This is necessary because the standard library which is distributed with
    lumail (the lumail core configuration so to speak) is written for lua5.1
    apparently.
    
    The website states 5.1 or 5.2 or 5.3, but 5.2 fails because "loadstring"
    was deprecated in lua 5.2.
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/applications/networking/mailreaders/lumail/default.nix
+++ pkgs/applications/networking/mailreaders/lumail/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, fetchurl, pkgconfig, lua5_2, file, ncurses, gmime, pcre-cpp
-, perl, perlPackages
+{ stdenv, fetchurl, pkgconfig, lua, file, ncurses, gmime, pcre-cpp
+, perl, perlPackages, makeWrapper
 , debugBuild ? false
 , alternativeGlobalConfigFilePath ? null
 }:
@@ -36,7 +36,7 @@ stdenv.mkDerivation {
 
   nativeBuildInputs = [ pkgconfig makeWrapper ];
   buildInputs = [
-    lua5_2 file ncurses gmime pcre-cpp
+    lua file ncurses gmime pcre-cpp
     perl perlPackages.JSON perlPackages.NetIMAPClient
   ];
 
