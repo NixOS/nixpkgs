7d936904360ec8cba4f9a2a3691b0d7ace28e37f
volth <volth@volth.com>
2018-03-12 18:45:57 +0000

    [cpan2nix] perlPackages.NetAddrIP: cleanup

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -10190,6 +10190,7 @@ let self = _self // overrides; _self = with self; {
       license = with stdenv.lib.licenses; [ artistic1 gpl1Plus ];
       maintainers = [ maintainers.rycee ];
     };
+    doCheck = false;
   };
 
   NetAmazonAWSSign = buildPerlPackage {
