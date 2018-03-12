a351eeae13851b605e368a18f81f8c58eedb7a12
volth <volth@volth.com>
2018-03-12 18:49:38 +0000

    [cpan2nix] perlPackages.Redis: cleanup

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -12232,7 +12232,7 @@ let self = _self // overrides; _self = with self; {
     propagatedBuildInputs = [ Readonly ];
   };
 
-  Redis = buildPerlPackage rec {
+  Redis = buildPerlModule rec {
     name = "Redis-1.991";
     src = fetchurl {
       url = "mirror://cpan/authors/id/D/DA/DAMS/${name}.tar.gz";
