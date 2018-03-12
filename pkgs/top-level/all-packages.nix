cd2db6624c8203291c1b2414aab5b1490a62b088
Daiderd Jordan <daiderd@gmail.com>
2018-03-12 21:32:38 +0100

    libcdio: fix darwin build
    /cc ZHF #36454
    
    (cherry picked from commit af041b34a29d2e681af9870ce3c359c88e1dbe03)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9639,9 +9639,7 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Carbon IOKit;
   };
 
-  libcdio-paranoia = callPackage ../development/libraries/libcdio-paranoia {
-    inherit (darwin.apple_sdk.frameworks) DiskArbitration IOKit;
-  };
+  libcdio-paranoia = callPackage ../development/libraries/libcdio-paranoia { };
 
   libcdr = callPackage ../development/libraries/libcdr { lcms = lcms2; };
 
