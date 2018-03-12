03ace8290a8dc93857704341fee20d749a9d4e27
volth <volth@volth.com>
2018-03-12 18:46:47 +0000

    [cpan2nix] perlPackages.TieIxHash: cleanup

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -15323,7 +15323,7 @@ let self = _self // overrides; _self = with self; {
     };
   };
 
-  TieIxHash = buildPerlPackage {
+  TieIxHash = buildPerlModule {
     name = "Tie-IxHash-1.23";
     src = fetchurl {
       url = mirror://cpan/authors/id/C/CH/CHORNY/Tie-IxHash-1.23.tar.gz;
