634cbfa8f2796ad9ddf151ab639e6ca68b80aaa1
Sander van der Burg <svanderburg@gmail.com>
2018-03-12 12:16:17 +0100

    nodejs: patch bundled node-gyp module to not invoke xcodebuild on macOS causing modules with native dependencies to fail

--- pkgs/development/web/nodejs/v8.nix
+++ pkgs/development/web/nodejs/v8.nix
@@ -7,5 +7,5 @@ in
     inherit enableNpm;
     version = "8.9.4";
     sha256 = "0vy8rlg58kg75j4sw3xadmbrwxfa56iaykmjl18g9a8wkjfdxp3c";
-    patches = lib.optionals stdenv.isDarwin [ ./no-xcode-v7.patch ];
+    patches = lib.optionals stdenv.isDarwin [ ./no-xcode-v7.patch ./no-xcodebuild.patch ];
   }
