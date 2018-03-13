2abfbcbfc612557ece7015ce9cd1f76e1c170b0f
Robin Gloster <mail@glob.in>
2018-03-13 17:09:16 +0100

    nodejs: add headers output

--- pkgs/development/web/nodejs/v6.nix
+++ pkgs/development/web/nodejs/v6.nix
@@ -7,5 +7,6 @@ in
     inherit enableNpm;
     version = "6.13.0";
     sha256 = "012dpfqxsrmd3xc4dmq0mik1kab4czf56s8wm2jvm7xjqvi6y5mp";
+    headersSha256 = "0lmmy1gz3xkisp5i3yqxc1z1sj57qp08g6jwcp9if65gr8xjcnqw";
     patches = lib.optionals stdenv.isDarwin [ ./no-xcode.patch ./no-xcodebuild.patch ];
   }
