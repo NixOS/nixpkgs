9caa55276b1d3957a2fdf822368003998ec3bd83
Michael Weiss <dev.primeos@gmail.com>
2018-03-13 02:23:41 +0100

    androidStudioPackages.{dev,canary}: init at 3.2.0.5
    Switch the naming scheme to the channel names.
    
    (cherry picked from commit 74d922638ba2f6a86b6aeb5d6ea15bdf45f5d333)

--- pkgs/applications/editors/android-studio/default.nix
+++ pkgs/applications/editors/android-studio/default.nix
@@ -15,10 +15,10 @@ in rec {
   # linux-bundle
   stable = mkStudio {
     pname = "android-studio";
-    #pname = "android-studio-stable"; # TODO: Rename and provide symlink
-    version = "3.1.0.16"; # "Android Studio 3.1"
-    build = "173.4670197";
-    sha256Hash = "1i0ldyadrcyy5pl9vjpm2k755mf08xi9x5qz8655qsbiajzqf9fy";
+    #pname = "android-studio-stable"; # TODO: Rename
+    version = "3.0.1.0"; # "Android Studio 3.0.1"
+    build = "171.4443003";
+    sha256Hash = "1krahlqr70nq3csqiinq2m4fgs68j11hd9gg2dx2nrpw5zni0wdd";
 
     meta = with stdenv.lib; {
       description = "The Official IDE for Android (stable channel)";
@@ -36,6 +36,7 @@ in rec {
   # linux-beta-bundle
   beta = mkStudio {
     pname = "android-studio-preview";
+    #pname = "android-studio-beta"; # TODO: Rename
     version = "3.1.0.14"; # "Android Studio 3.1 RC 2"
     build = "173.4640767";
     sha256Hash = "00v8qbis4jm31v1g9989f9y15av6p3ywj8mmfxcsc3hjlpzdgid8";
@@ -46,19 +47,25 @@ in rec {
     };
   };
 
-  dev = mkStudio (latestVersion // {
+  dev = mkStudio {
     pname = "android-studio-dev";
+    version = "3.2.0.5"; # "Android Studio 3.2 Canary 6"
+    build = "173.4640885";
+    sha256Hash = "1fbjk1dhvi975dm09s9iz9ja53fjqca07nw5h068gdj3358pj3k8";
 
     meta = beta.meta // {
       description = "The Official IDE for Android (dev channel)";
     };
-  });
+  };
 
-  canary = mkStudio (latestVersion // {
+  canary = mkStudio {
     pname = "android-studio-canary";
+    version = "3.2.0.5"; # "Android Studio 3.2 Canary 6"
+    build = "173.4640885";
+    sha256Hash = "1fbjk1dhvi975dm09s9iz9ja53fjqca07nw5h068gdj3358pj3k8";
 
     meta = beta.meta // {
       description = "The Official IDE for Android (canary channel)";
     };
-  });
+  };
 }
