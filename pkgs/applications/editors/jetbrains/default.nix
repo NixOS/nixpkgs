50126f54fe257017898b1c116d5d17343cddf907
Averell Dalton <averell@rxd4.com>
2018-03-14 08:32:26 +0100

    jetbrains.idea-community: 2017.3.4 -> 2017.3.5
    (cherry picked from commit 09b05ede821b38810d26793ce6b62e087c20b5ba)

--- pkgs/applications/editors/jetbrains/default.nix
+++ pkgs/applications/editors/jetbrains/default.nix
@@ -273,12 +273,12 @@ in
 
   idea-community = buildIdea rec {
     name = "idea-community-${version}";
-    version = "2017.3.4"; /* updated by script */
+    version = "2017.3.5"; /* updated by script */
     description = "Integrated Development Environment (IDE) by Jetbrains, community edition";
     license = stdenv.lib.licenses.asl20;
     src = fetchurl {
       url = "https://download.jetbrains.com/idea/ideaIC-${version}.tar.gz";
-      sha256 = "15qsfirzmmjhwzkhx36zr4n0z5lhs021n2n3wim01g309ymr4gl9"; /* updated by script */
+      sha256 = "124rzc46f4bqyhllwyb26lkavh7zwbj62axrwhif34rayl2rcmpm"; /* updated by script */
     };
     wmClass = "jetbrains-idea-ce";
     update-channel = "IDEA_Release";
