b6e90e68bb4119b6697bf92280eb0131d5abf14a
Adriaan de Groot <groot@kde.org>
2018-03-14 08:51:20 -0400

    Shuffle branding into a subdirectory, add a module

---
+++ pkgs/by-name/ca/calamares-nixos-extensions/src/modules/filekeeper/FileKeeper.cpp
@@ -0,0 +1,60 @@
+/* === This file is part of Calamares - <https://github.com/calamares> ===
+ *
+ *   Copyright 2018, Adriaan de Groot <groot@kde.org>
+ *
+ *   Calamares is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Calamares is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "FileKeeper.h"
+
+#include <QProcess>
+#include <QDateTime>
+#include <QThread>
+
+#include <JobQueue.h>
+#include <GlobalStorage.h>
+
+#include <utils/Logger.h>
+
+FileKeeperJob::FileKeeperJob( QObject* parent )
+    : Calamares::CppJob( parent )
+{
+}
+
+
+FileKeeperJob::~FileKeeperJob()
+{
+}
+
+
+QString
+FileKeeperJob::prettyName() const
+{
+    return tr( "File keeper Job" );
+}
+
+Calamares::JobResult
+FileKeeperJob::exec()
+{
+    return Calamares::JobResult::ok();
+}
+
+
+void
+FileKeeperJob::setConfigurationMap( const QVariantMap& configurationMap )
+{
+    Q_UNUSED( configurationMap );
+}
+
+CALAMARES_PLUGIN_FACTORY_DEFINITION( FileKeeperJobFactory, registerPlugin<FileKeeperJob>(); )
---
+++ pkgs/by-name/ca/calamares-nixos-extensions/src/modules/filekeeper/FileKeeper.h
@@ -0,0 +1,48 @@
+/* === This file is part of Calamares - <https://github.com/calamares> ===
+ *
+ *   Copyright 2018, Adriaan de Groot <groot@kde.org>
+ *
+ *   Calamares is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Calamares is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FILEKEEPER_H
+#define FILEKEEPER_H
+
+#include <QObject>
+#include <QVariantMap>
+
+#include <CppJob.h>
+
+#include <utils/PluginFactory.h>
+
+#include <PluginDllMacro.h>
+
+class PLUGINDLLEXPORT FileKeeperJob : public Calamares::CppJob
+{
+    Q_OBJECT
+
+public:
+    explicit FileKeeperJob( QObject* parent = nullptr );
+    virtual ~FileKeeperJob() override;
+
+    QString prettyName() const override;
+
+    Calamares::JobResult exec() override;
+
+    void setConfigurationMap( const QVariantMap& configurationMap ) override;
+};
+
+CALAMARES_PLUGIN_FACTORY_DECLARATION( FileKeeperJobFactory )
+
+#endif
