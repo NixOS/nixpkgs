3d2db221f71194c37e04e88aa07f6da570f12f25
Jan Tojnar <jtojnar@gmail.com>
2018-03-14 14:58:39 +0100

    gnome3.gspell: fix build

--- pkgs/desktops/gnome-3/misc/gspell/default.nix
+++ pkgs/desktops/gnome-3/misc/gspell/default.nix
@@ -1,22 +1,30 @@
-{ stdenv, fetchurl, pkgconfig, glib, gtk3, enchant, isocodes, vala, gobjectIntrospection, gnome3 }:
+{ stdenv, fetchurl, pkgconfig, libxml2, glib, gtk3, enchant2, isocodes, vala, gobjectIntrospection, gnome3 }:
 
-stdenv.mkDerivation rec {
-  name = "gspell-${version}";
+let
+  pname = "gspell";
   version = "1.8.0";
+in stdenv.mkDerivation rec {
+  name = "${pname}-${version}";
+
+  outputs = [ "out" "dev" ];
+  outputBin = "dev";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gspell/${gnome3.versionBranch version}/${name}.tar.xz";
+    url = "mirror://gnome/sources/${pname}/${gnome3.versionBranch version}/${name}.tar.xz";
     sha256 = "0ickabxngl567lv1jax4fasr5brq29hg04ymaay47pjfp32w4zqv";
   };
 
-  passthru = {
-    updateScript = gnome3.updateScript { packageName = "gspell"; attrPath = "gnome3.gspell"; };
-  };
+  propagatedBuildInputs = [ enchant2 ]; # required for pkgconfig
 
-  propagatedBuildInputs = [ enchant ]; # required for pkgconfig
-
-  nativeBuildInputs = [ pkgconfig vala gobjectIntrospection ];
+  nativeBuildInputs = [ pkgconfig vala gobjectIntrospection libxml2 ];
   buildInputs = [ glib gtk3 isocodes ];
+
+  passthru = {
+    updateScript = gnome3.updateScript {
+      packageName = pname;
+      attrPath = "gnome3.${pname}";
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "A spell-checking library for GTK+ applications";
