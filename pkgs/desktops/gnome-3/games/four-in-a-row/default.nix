e9d597e0dfcc9f2cfa9dffc654902cf89770de30
Jan Tojnar <jtojnar@gmail.com>
2018-03-14 01:41:23 +0100

    gnome3.four-in-a-row: fix build

--- pkgs/desktops/gnome-3/games/four-in-a-row/default.nix
+++ pkgs/desktops/gnome-3/games/four-in-a-row/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, pkgconfig, gnome3, gtk3, wrapGAppsHook
-, intltool, itstool, libcanberra-gtk3, librsvg, libxml2 }:
+, gettext, itstool, libcanberra-gtk3, librsvg, libxml2 }:
 
 stdenv.mkDerivation rec {
   name = "four-in-a-row-${version}";
@@ -10,15 +10,15 @@ stdenv.mkDerivation rec {
     sha256 = "1iszaay2r92swb0q67lmip6r1w3hw2dwmlgnz9v2h6blgdyncs4k";
   };
 
-  passthru = {
-    updateScript = gnome3.updateScript { packageName = "four-in-a-row"; attrPath = "gnome3.four-in-a-row"; };
-  };
+  nativeBuildInputs = [ pkgconfig wrapGAppsHook gettext itstool libxml2 ];
+  buildInputs = [ gtk3 libcanberra-gtk3 librsvg gnome3.defaultIconTheme ];
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [
-    gtk3 wrapGAppsHook intltool itstool libcanberra-gtk3 librsvg
-    libxml2 gnome3.defaultIconTheme
-  ];
+  passthru = {
+    updateScript = gnome3.updateScript {
+      packageName = "four-in-a-row";
+      attrPath = "gnome3.four-in-a-row";
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = https://wiki.gnome.org/Apps/Four-in-a-row;
