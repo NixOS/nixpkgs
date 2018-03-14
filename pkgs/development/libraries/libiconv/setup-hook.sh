c295e7a7f53e336c3654618b836404ba2bfb733b
Matthew Bauer <mjbauer95@gmail.com>
2018-03-14 16:58:57 -0500

    libiconv: fix bad setup-hook

--- pkgs/development/libraries/libiconv/setup-hook.sh
+++ pkgs/development/libraries/libiconv/setup-hook.sh
@@ -1,7 +1,7 @@
-# libintl must be listed in load flags on non-Glibc
+# libiconv must be listed in load flags on non-Glibc
 # it doesn't hurt to have it in Glibc either though
 iconvLdflags() {
-    export NIX_LDFLAGS="$NIX_LDFLAGS -iconv"
+    export NIX_LDFLAGS="$NIX_LDFLAGS -liconv"
 }
 
 addEnvHooks "$hostOffset" iconvLdflags
