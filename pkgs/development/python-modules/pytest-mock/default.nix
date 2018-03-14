4b5b1370a7ff61db4408afdf961e0e4ba1c8aebf
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-03-14 17:33:39 +0100

    pythonPackages.pytest-mock: remove superfluous patch

--- pkgs/development/python-modules/pytest-mock/default.nix
+++ pkgs/development/python-modules/pytest-mock/default.nix
@@ -9,11 +9,6 @@ buildPythonPackage rec {
     sha256 = "0jgr1h1f0m9dl3alxiiw55as28pj2lpihz12gird9z1i3vvdyydq";
   };
 
-  patches = fetchpatch {
-    url = "${meta.homepage}/pull/107.patch";
-    sha256 = "07p7ra6lilfv04wyxc855zmfwxvnpmi9s0v6vh5bx769cj9jwxck";
-  };
-
   propagatedBuildInputs = [ pytest ] ++ lib.optional (!isPy3k) mock;
   nativeBuildInputs = [ setuptools_scm ];
 
