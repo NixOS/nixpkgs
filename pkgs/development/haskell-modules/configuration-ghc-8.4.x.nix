a11eaf1f04dc4cf75cee430e18a934296b6da35c
Peter Simons <simons@cryp.to>
2018-03-15 20:03:46 +0100

    haskell-regex-tdfa: drop obsolete override for ghc-8.4.x

--- pkgs/development/haskell-modules/configuration-ghc-8.4.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.4.x.nix
@@ -149,16 +149,6 @@ self: super: {
   lens = super.lens_4_16;
 
   ## Needs bump to a versioned attribute
-  regex-tdfa = overrideCabal super.regex-tdfa_1_2_3 (drv: {
-    ##     • No instance for (Semigroup (CharMap a))
-    ##         arising from the superclasses of an instance declaration
-    ##     • In the instance declaration for ‘Monoid (CharMap a)’
-    ##
-    ## error: while evaluating ‘overrideCabal’ at nixpkgs://pkgs/development/haskell-modules/lib.nix:37:24, called from /home/deepfire/nixpkgs/pkgs/development/haskell-modules/configuration-ghc-8.4.x.nix:188:16:
-    editedCabalFile = null;
-  });
-
-  ## Needs bump to a versioned attribute
   ## Setup: Encountered missing dependencies:
   ## doctest >=0.11.1 && <0.14
   semigroupoids = super.semigroupoids_5_2_2;
