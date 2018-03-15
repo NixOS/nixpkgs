408f6b2843b44109d09f3ad2787fc49c75d99776
adisbladis <adis@blad.is>
2018-03-16 01:14:12 +0800

    pythonPackages.pycurl: Move to separate file

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12943,34 +12943,7 @@ in {
 
   };
 
-  pycurl = buildPythonPackage (rec {
-    pname = "pycurl";
-    version = "7.43.0.1";
-    disabled = isPyPy; # https://github.com/pycurl/pycurl/issues/208
-
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "1ali1gjs9iliwjra7w0y5hwg79a2fd0f4ydvv6k27sgxpbr1n8s3";
-    };
-
-    buildInputs = with self; [ pkgs.curl pkgs.openssl.out ];
-
-    checkInputs = with self; [ bottle pytest nose flaky ];
-
-    checkPhase = ''
-      py.test -k "not test_ssl_in_static_libs and not ssh_key_cb_test" tests
-    '';
-
-    preConfigure = ''
-      substituteInPlace setup.py --replace '--static-libs' '--libs'
-      export PYCURL_SSL_LIBRARY=openssl
-    '';
-
-    meta = {
-      homepage = http://pycurl.sourceforge.net/;
-      description = "Python wrapper for libcurl";
-    };
-  });
+  pycurl = callPackage ../development/python-modules/pycurl { };
 
   pycurl2 = buildPythonPackage (rec {
     name = "pycurl2-7.20.0";
