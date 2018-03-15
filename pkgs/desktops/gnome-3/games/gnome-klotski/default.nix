21a27267d33b7ef1aa1eefd5d78aca5c8a65cbdb
Jan Tojnar <jtojnar@gmail.com>
2018-03-15 01:06:04 +0100

    gnome3.gnome-klotski: add missing dependencies

--- pkgs/desktops/gnome-3/games/gnome-klotski/default.nix
+++ pkgs/desktops/gnome-3/games/gnome-klotski/default.nix
@@ -1,24 +1,26 @@
-{ stdenv, fetchurl, pkgconfig, gnome3, gtk3, wrapGAppsHook
-, librsvg, libxml2, intltool, itstool, libgee, libgames-support }:
+{ stdenv, fetchurl, pkgconfig, vala, gnome3, gtk3, wrapGAppsHook, appstream-glib, desktop-file-utils
+, glib, librsvg, libxml2, intltool, itstool, libgee, libgnome-games-support }:
 
-stdenv.mkDerivation rec {
-  name = "gnome-klotski-${version}";
+let
+  pname = "gnome-klotski";
   version = "3.22.3";
+in stdenv.mkDerivation rec {
+  name = "${pname}-${version}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gnome-klotski/${gnome3.versionBranch version}/${name}.tar.xz";
+    url = "mirror://gnome/sources/${pname}/${gnome3.versionBranch version}/${name}.tar.xz";
     sha256 = "0prc0s28pdflgzyvk1g0yfx982q2grivmz3858nwpqmbkha81r7f";
   };
 
-  passthru = {
-    updateScript = gnome3.updateScript { packageName = "gnome-klotski"; attrPath = "gnome3.gnome-klotski"; };
-  };
+  nativeBuildInputs = [ pkgconfig vala wrapGAppsHook intltool itstool libxml2 appstream-glib desktop-file-utils ];
+  buildInputs = [ glib gtk3 librsvg libgee libgnome-games-support gnome3.defaultIconTheme ];
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [
-    gtk3 wrapGAppsHook intltool itstool librsvg libxml2 libgee libgames-support
-    gnome3.defaultIconTheme
-  ];
+  passthru = {
+    updateScript = gnome3.updateScript {
+      packageName = "${pname}";
+      attrPath = "gnome3.${pname}";
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = https://wiki.gnome.org/Apps/Klotski;
