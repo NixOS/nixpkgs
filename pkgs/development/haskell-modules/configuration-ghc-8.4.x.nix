54c1ca5fe9c7991dfc3fff0478ebe8acefefb598
Peter Simons <simons@cryp.to>
2018-03-17 21:15:32 +0100

    haskell-xmonad: patch to fix the build with ghc-8.4.x

--- pkgs/development/haskell-modules/configuration-ghc-8.4.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.4.x.nix
@@ -710,4 +710,10 @@ self: super: {
   # https://github.com/pikajude/th-printf/issues/8
   th-printf = doJailbreak super.th-printf;
 
+  # https://github.com/xmonad/xmonad/issues/155
+  xmonad = addBuildDepend (appendPatch super.xmonad (pkgs.fetchpatch
+    { url = https://github.com/xmonad/xmonad/pull/153/commits/c96a59fa0de2f674e60befd0f57e67b93ea7dcf6.patch;
+      sha256 = "1mj3k0w8aqyy71kmc71vzhgxmr4h6i5b3sykwflzays50grjm5jp";
+    })) self.semigroups;
+
 }
