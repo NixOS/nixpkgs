bc9dc2ccf1186f4c626104820379c7d2200f2e52
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-03-17 19:19:32 +0100

    pythonPackages.hvac: 0.2.15 -> 0.5.0

--- pkgs/development/python-modules/hvac/default.nix
+++ pkgs/development/python-modules/hvac/default.nix
@@ -2,14 +2,17 @@
 
 buildPythonPackage rec {
   pname = "hvac";
-  version = "0.2.15";
+  version = "0.5.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0qxa4g1ij1bj27mbp8l54lcr7d5krkb2rayisc6shkpf2b51ip4c";
+    sha256 = "2c9308334301daee3b5c6d56a032ca2c81eeb97d2777b73d795e201e8d037687";
   };
 
   propagatedBuildInputs = [ requests ];
+
+  # Requires running a Vault server
+  doCheck = false;
 
   meta = with lib; {
     description = "HashiCorp Vault API client";
