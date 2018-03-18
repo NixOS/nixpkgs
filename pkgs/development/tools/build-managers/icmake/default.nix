0b0bd0c8cc157356f5b9a61c9f4805872cbc47b7
Pascal Wittmann <mail@pascal-wittmann.de>
2018-03-18 20:43:54 +0100

    icmake: 9.02.06 -> 9.02.07

--- pkgs/development/tools/build-managers/icmake/default.nix
+++ pkgs/development/tools/build-managers/icmake/default.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   name = "icmake-${version}";
-  version = "9.02.06";
+  version = "9.02.07";
 
   src = fetchFromGitHub {
-    sha256 = "1hs7fhqpkhlrjvjhfarf5bmxl8dw3r0immzdib27gwh3sfzgpx0b";
+    sha256 = "1q3rwri5s1sqm4h75bahkjnlym4bk2ygg4fb75yrniwnj8rhdp12";
     rev = version;
     repo = "icmake";
     owner = "fbb-git";
