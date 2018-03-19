dc3b791854589d7c1c6d210f070a1e82315798f6
Andrew Kelley <superjoe30@gmail.com>
2018-03-18 20:13:46 -0400

    zig: 0.1.1 -> 0.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11851,7 +11851,9 @@ with pkgs;
     czmq = czmq3;
   };
 
-  zig = callPackage ../development/compilers/zig {};
+  zig = callPackage ../development/compilers/zig {
+    llvmPackages = llvmPackages_6;
+  };
 
   zimlib = callPackage ../development/libraries/zimlib { };
 
