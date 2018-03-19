63e3251637d44463b750c9d6cd97732258ecd82d
gnidorah <gnidorah@users.noreply.github.com>
2018-03-19 21:42:25 +0300

    cgit: wrap python scripts
    (cherry picked from commit 55f1bbe6234cd10f44e3fd3447c80aaa3fba56ec)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14845,7 +14845,9 @@ with pkgs;
 
   centerim = callPackage ../applications/networking/instant-messengers/centerim { };
 
-  cgit = callPackage ../applications/version-management/git-and-tools/cgit { };
+  cgit = callPackage ../applications/version-management/git-and-tools/cgit {
+    inherit (python3Packages) python wrapPython pygments markdown;
+  };
 
   cgminer = callPackage ../applications/misc/cgminer {
     amdappsdk = amdappsdk28;
