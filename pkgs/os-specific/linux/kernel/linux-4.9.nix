8960960816de07173f2c15eddbe80632648bb291
Tim Steinbach <tim@nequissimus.com>
2018-03-19 08:59:40 -0400

    linux: 4.9.87 -> 4.9.88
    (cherry picked from commit c4963d395d3bc5b29387e38ab1d59e6175c44fc3)

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.87";
+  version = "4.9.88";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "05y9wjmshd3pr3ymfpx80hjv5973i6l3zk1mpww7wnnwd6pzdjbs";
+    sha256 = "094cxc86ajnsai1vwy76mmg7l3b9lvhk6mw6746lsr3fnzv1fkq7";
   };
 } // (args.argsOverride or {}))
