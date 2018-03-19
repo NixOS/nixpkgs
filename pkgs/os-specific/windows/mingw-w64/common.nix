aec57cfabe44acf0b87fa0bf9512da6b808f3c5f
Remy Goldschmidt <taktoa@gmail.com>
2018-03-19 16:45:37 -0400

    mingw-w64: Add enable-idl / enable-secure-api flags

--- pkgs/os-specific/windows/mingw-w64/common.nix
+++ pkgs/os-specific/windows/mingw-w64/common.nix
@@ -8,4 +8,9 @@ rec {
     url = "mirror://sourceforge/mingw-w64/mingw-w64-v${version}.tar.bz2";
     sha256 = "0p01vm5kx1ixc08402z94g1alip4vx66gjpvyi9maqyqn2a76h0c";
   };
+
+  configureFlags = [
+    "--enable-idl"
+    "--enable-secure-api"
+  ];
 }
