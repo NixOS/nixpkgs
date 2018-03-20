9fa2844784c7f76ba1c74896975e0260a95eefa9
Jan Tojnar <jtojnar@gmail.com>
2018-03-20 19:36:40 +0100

    webkitgtk220x: cleanup

--- pkgs/development/libraries/webkitgtk/2.20.nix
+++ pkgs/development/libraries/webkitgtk/2.20.nix
@@ -1,6 +1,6 @@
-{ stdenv, fetchurl, perl, python2, ruby, bison, gperf, cmake
+{ stdenv, fetchurl, perl, python2, ruby, bison, gperf, cmake, ninja
 , pkgconfig, gettext, gobjectIntrospection, libnotify, gnutls, libgcrypt
-, gtk3, wayland, libwebp, enchant, xorg, libxkbcommon, epoxy, at-spi2-core
+, gtk3, wayland, libwebp, enchant2, xorg, libxkbcommon, epoxy, at-spi2-core
 , libxml2, libsoup, libsecret, libxslt, harfbuzz, libpthreadstubs, pcre, nettle, libtasn1, p11-kit
 , libidn, libedit, readline, libGLU_combined, libintlOrEmpty
 , enableGeoLocation ? true, geoclue2, sqlite
@@ -26,29 +26,10 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ ];
   };
 
-  postConfigure = optionalString stdenv.isDarwin ''
-    substituteInPlace Source/WebKit2/CMakeFiles/WebKit2.dir/link.txt \
-        --replace "../../lib/libWTFGTK.a" ""
-    substituteInPlace Source/JavaScriptCore/CMakeFiles/JavaScriptCore.dir/link.txt \
-        --replace "../../lib/libbmalloc.a" ""
-    sed -i "s|[\./]*\.\./lib/lib[^\.]*\.a||g" \
-        Source/JavaScriptCore/CMakeFiles/LLIntOffsetsExtractor.dir/link.txt \
-        Source/JavaScriptCore/shell/CMakeFiles/jsc.dir/link.txt \
-        Source/JavaScriptCore/shell/CMakeFiles/testb3.dir/link.txt \
-        Source/WebKit2/CMakeFiles/DatabaseProcess.dir/link.txt \
-        Source/WebKit2/CMakeFiles/NetworkProcess.dir/link.txt \
-        Source/WebKit2/CMakeFiles/webkit2gtkinjectedbundle.dir/link.txt \
-        Source/WebKit2/CMakeFiles/WebProcess.dir/link.txt
-    substituteInPlace Source/JavaScriptCore/CMakeFiles/JavaScriptCore.dir/link.txt \
-        --replace "../../lib/libWTFGTK.a" "-Wl,-all_load ../../lib/libWTFGTK.a"
-  '';
-
   src = fetchurl {
     url = "http://webkitgtk.org/releases/${name}.tar.xz";
     sha256 = "0g0an3pc2yz13gzpaysfgch2yp510gw1qcpk0xr8m6mx43vl1xjp";
   };
-
-  # see if we can clean this up....
 
   patches = optionals stdenv.isDarwin [
     ## TODO add necessary patches for Darwin
@@ -61,13 +42,13 @@ stdenv.mkDerivation rec {
   cmakeFlags = [
   "-DPORT=GTK"
   "-DUSE_LIBHYPHEN=0"
+  "-DENABLE_INTROSPECTION=ON"
   ]
   ++ optional (!enableGtk2Plugins) "-DENABLE_PLUGIN_PROCESS_GTK2=OFF"
   ++ optional stdenv.isLinux "-DENABLE_GLES2=ON"
   ++ optionals stdenv.isDarwin [
   "-DUSE_SYSTEM_MALLOC=ON"
   "-DUSE_ACCELERATE=0"
-  "-DENABLE_INTROSPECTION=ON"
   "-DENABLE_MINIBROWSER=OFF"
   "-DENABLE_VIDEO=ON"
   "-DENABLE_QUARTZ_TARGET=ON"
@@ -82,12 +63,12 @@ stdenv.mkDerivation rec {
   NIX_CFLAGS_COMPILE = optionalString stdenv.isDarwin " -lintl";
 
   nativeBuildInputs = [
-    cmake perl python2 ruby bison gperf
+    cmake ninja perl python2 ruby bison gperf
     pkgconfig gettext gobjectIntrospection
   ];
 
   buildInputs = libintlOrEmpty ++ [
-    libwebp enchant libnotify gnutls pcre nettle libidn libgcrypt woff2
+    libwebp enchant2 libnotify gnutls pcre nettle libidn libgcrypt woff2
     libxml2 libsecret libxslt harfbuzz libpthreadstubs libtasn1 p11-kit
     sqlite gst-plugins-base gst-plugins-bad libxkbcommon epoxy at-spi2-core
   ] ++ optional enableGeoLocation geoclue2
@@ -99,8 +80,6 @@ stdenv.mkDerivation rec {
   propagatedBuildInputs = [
     libsoup gtk3
   ];
-
-  enableParallelBuilding = true;
 
   outputs = [ "out" "dev" ];
 }
