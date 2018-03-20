a2b768a2965151e7f6c7c386ddfe641dc4088df6
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-03-20 11:04:43 +0100

    pythonPackages.django_pipeline: 1.5.1 -> 1.6.14
    (cherry picked from commit 6e78005ef1de6dca89abc6df4877722cc3ed841a)

--- pkgs/development/python-modules/django-pipeline/default.nix
+++ pkgs/development/python-modules/django-pipeline/default.nix
@@ -1,16 +1,32 @@
-{ lib, buildPythonPackage, fetchPypi
-, django, futures }:
+{ lib, buildPythonPackage, fetchFromGitHub, isPy3k
+, django, futures, mock, jinja2, jsmin, slimit }:
 
 buildPythonPackage rec {
   pname = "django-pipeline";
-  version = "1.5.1";
+  version = "1.6.14";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "1y49fa8jj7x9qjj5wzhns3zxwj0s73sggvkrv660cqw5qb7d8hha";
+  # no tests on PyPI
+  src = fetchFromGitHub {
+    owner = "jazzband";
+    repo = pname;
+    rev = version;
+    sha256 = "1xf732bd17mgha75jfhlnms46ib2pffhpfa0ca7bmng9jhbvsl9j";
   };
 
-  propagatedBuildInputs = [ django futures ];
+  postPatch = ''
+    substituteInPlace tests/tests/test_compiler.py \
+      --replace "/usr/bin/env" ""
+  '';
+
+  propagatedBuildInputs = [ django ] ++ lib.optional (!isPy3k) futures;
+
+  checkInputs = [ jinja2 jsmin slimit ] ++ lib.optional (!isPy3k) mock;
+
+  checkPhase = ''
+    export PYTHONPATH=.:$PYTHONPATH
+    export DJANGO_SETTINGS_MODULE=tests.settings
+    ${django}/bin/django-admin.py test tests
+  '';
 
   meta = with lib; {
     description = "Pipeline is an asset packaging library for Django";
