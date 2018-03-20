9fd7e2eee205b122175d28d4c7de76c2e415287a
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-03-20 12:06:31 +0100

    pythonPackages.stem: run unit tests
    (cherry picked from commit 685dd8d21db16ffb3d8888f4f9cd5e6d5dd3c6c6)

--- pkgs/development/python-modules/stem/default.nix
+++ pkgs/development/python-modules/stem/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib, buildPythonPackage, fetchPypi, python, mock }:
 
 buildPythonPackage rec {
   pname = "stem";
@@ -8,6 +8,18 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "1va9p3ij7lxg6ixfsvaql06dn11l3fgpxmss1dhlvafm7sqizznp";
   };
+
+  postPatch = ''
+    rm test/unit/installation.py
+    sed -i "/test.unit.installation/d" test/settings.cfg
+  '';
+
+  checkInputs = [ mock ];
+
+  checkPhase = ''
+    touch .gitignore
+    ${python.interpreter} run_tests.py -u
+  '';
 
   meta = with lib; {
     description = "Controller library that allows applications to interact with Tor";
