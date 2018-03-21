bdfbe578da8d061f46c6a548502f5bc2fe743ff2
Jan Tojnar <jtojnar@gmail.com>
2018-03-21 11:38:38 +0100

    corebird: fix build

--- pkgs/applications/networking/corebird/default.nix
+++ pkgs/applications/networking/corebird/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, fetchFromGitHub, gtk3, json-glib, sqlite, libsoup, gettext, vala_0_32
-, automake, autoconf, libtool, pkgconfig, gnome3, gst_all_1, wrapGAppsHook
+{ stdenv, fetchFromGitHub, glib, gtk3, json-glib, sqlite, libsoup, gettext, vala_0_40
+, meson, ninja, pkgconfig, gnome3, gst_all_1, wrapGAppsHook, gobjectIntrospection
 , glib-networking }:
 
 stdenv.mkDerivation rec {
@@ -13,20 +13,24 @@ stdenv.mkDerivation rec {
     sha256 = "0qjffsmg1hm64dgsbkfwzbzy9q4xa1q4fh4h8ni8a2b1p3h80x7n";
   };
 
-  preConfigure = ''
-    ./autogen.sh
-  '';
-
-  nativeBuildInputs = [ automake autoconf libtool pkgconfig wrapGAppsHook ];
+  nativeBuildInputs = [
+    meson ninja vala_0_40 pkgconfig wrapGAppsHook
+    gobjectIntrospection # for setup hook
+  ];
 
   buildInputs = [
-    gtk3 json-glib sqlite libsoup gettext vala_0_32 gnome3.dconf gnome3.gspell glib-networking
+    glib gtk3 json-glib sqlite libsoup gettext gnome3.dconf gnome3.gspell glib-networking
   ] ++ (with gst_all_1; [ gstreamer gst-plugins-base gst-plugins-good (gst-plugins-bad.override { gtkSupport = true; }) gst-libav ]);
+
+  postPatch = ''
+    chmod +x data/meson_post_install.py # patchShebangs requires executable file
+    patchShebangs data/meson_post_install.py
+  '';
 
   meta = {
     description = "Native Gtk+ Twitter client for the Linux desktop";
     longDescription = "Corebird is a modern, easy and fun Twitter client.";
-    homepage = http://corebird.baedert.org;
+    homepage = https://corebird.baedert.org/;
     license = stdenv.lib.licenses.gpl3;
     platforms = stdenv.lib.platforms.linux;
     maintainers = [ stdenv.lib.maintainers.jonafato ];
