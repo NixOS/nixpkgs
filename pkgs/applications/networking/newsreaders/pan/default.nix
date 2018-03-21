96f0d4e98cd0be7c05b4b2d0a3a6d8db2c6726c5
Will Dietz <w@wdtz.org>
2018-03-21 07:44:40 -0500

    pan: wrap to provide gpg2, which it crashes without
    Fixes #37556.

--- pkgs/applications/networking/newsreaders/pan/default.nix
+++ pkgs/applications/networking/newsreaders/pan/default.nix
@@ -1,6 +1,7 @@
 { spellChecking ? true
 , stdenv, fetchurl, pkgconfig, gtk2, gtkspell2 ? null
 , perl, pcre, gmime, gettext, intltool, itstool, libxml2, dbus-glib, libnotify
+, makeWrapper, gnupg
 }:
 
 assert spellChecking -> gtkspell2 != null;
@@ -15,9 +16,13 @@ stdenv.mkDerivation {
     sha256 = "0l07y75z8jxhbmfv28slw81gjncs7i89x7fq44zif7xhq5vy7yli";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ gtk2 perl gmime gettext intltool itstool libxml2 dbus-glib libnotify ]
+  nativeBuildInputs = [ pkgconfig makeWrapper ];
+  buildInputs = [ gtk2 perl gmime gettext gnupg intltool itstool libxml2 dbus-glib libnotify ]
     ++ stdenv.lib.optional spellChecking gtkspell2;
+
+  postInstall = ''
+    wrapProgram $out/bin/pan --suffix PATH : ${gnupg}/bin
+  '';
 
   enableParallelBuilding = true;
 
