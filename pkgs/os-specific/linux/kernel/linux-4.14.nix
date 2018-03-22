deb9ff164f1bf451377a3222f03000ea58bbace2
Tim Steinbach <tim@nequissimus.com>
2018-03-21 20:42:33 -0400

    linux: 4.14.28 -> 4.14.29

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,13 +3,13 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.14.28";
+  version = "4.14.29";
 
   # branchVersion needs to be x.y
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0byx2824ml2ck97p66gfipnasbn9zz6rhjps61n6gprg3ac5fd07";
+    sha256 = "06a6q69jwvggns5rf473nfgfwlkfyj8zs6vrjppwf8lrrrq7pxhq";
   };
 } // (args.argsOverride or {}))
