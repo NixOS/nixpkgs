db5350092a1ecfd72e991ce06cc223af481069d2
dbornside <dan.bornside@obsidian.systems>
2018-03-23 13:45:28 -0400

    libesmtp: add metadata

--- pkgs/development/libraries/libesmtp/default.nix
+++ pkgs/development/libraries/libesmtp/default.nix
@@ -8,5 +8,11 @@ stdenv.mkDerivation rec {
     url = "http://brianstafford.info/libesmtp/libesmtp-1.0.6.tar.bz2";
     sha256 = "02zbniyz7qys1jmx3ghx21kxmns1wc3hmv80gp7ag7yra9f1m9nh";
   };
+
+  meta = {
+    homepage = http://brianstafford.info/libesmtp/index.html;
+    description = "A Library for Posting Electronic Mail";
+    licenses = stdenv.licenses.lgpl3Plus;
+  };
 }
 
