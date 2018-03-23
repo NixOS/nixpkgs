1482c3c8d8257d5b33e978c8e1888d69c706130a
John Ericson <John.Ericson@Obsidian.Systems>
2018-03-23 12:07:11 -0400

    gettext: Make setup hook work for cross

--- pkgs/development/libraries/gettext/gettext-setup-hook.sh
+++ pkgs/development/libraries/gettext/gettext-setup-hook.sh
@@ -9,7 +9,18 @@ addEnvHooks "$hostOffset" gettextDataDirsHook
 # libintl must be listed in load flags on non-Glibc
 # it doesn't hurt to have it in Glibc either though
 gettextLdflags() {
-    export NIX_LDFLAGS="$NIX_LDFLAGS -lintl"
+    # The `depHostOffset` describes how the host platform of the dependencies
+    # are slid relative to the depending package. It is brought into scope of
+    # the environment hook defined as the role of the dependency being applied.
+    case $depHostOffset in
+        -1) local role='BUILD_' ;;
+        0)  local role='' ;;
+        1)  local role='TARGET_' ;;
+        *)  echo "cc-wrapper: Error: Cannot be used with $depHostOffset-offset deps" >2;
+            return 1 ;;
+    esac
+
+    export NIX_${role}LDFLAGS+=" -lintl"
 }
 
 if [ ! -z "@gettextNeedsLdflags@" ]; then
