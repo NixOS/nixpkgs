9ec9c5e236bd0b5f649d2c5661df26473d402255
Graham Christensen <graham@grahamc.com>
2018-03-24 11:36:10 -0400

    docs: Build with a makefile
    (cherry picked from commit 7e25ff7106f00bdca9957b1109e2fd9beca86ea1)

--- doc/default.nix
+++ doc/default.nix
@@ -7,81 +7,23 @@ in
 pkgs.stdenv.mkDerivation {
   name = "nixpkgs-manual";
 
-
   buildInputs = with pkgs; [ pandoc libxml2 libxslt zip ];
 
-  xsltFlags = ''
-    --param section.autolabel 1
-    --param section.label.includes.component.label 1
-    --param html.stylesheet 'style.css'
-    --param xref.with.number.and.title 1
-    --param toc.section.depth 3
-    --param admon.style '''
-    --param callout.graphics.extension '.gif'
-  '';
+  src = ./.;
 
+  XSL = "${pkgs.docbook5_xsl}/xml/xsl";
+  RNG = "${pkgs.docbook5}/xml/rng/docbook/docbook.rng";
+  xsltFlags = lib.concatStringsSep " " [
+    "--param section.autolabel 1"
+    "--param section.label.includes.component.label 1"
+    "--param html.stylesheet 'style.css'"
+    "--param xref.with.number.and.title 1"
+    "--param toc.section.depth 3"
+    "--param admon.style ''"
+    "--param callout.graphics.extension '.gif'"
+  ];
 
-  buildCommand = let toDocbook = { useChapters ? false, inputFile, outputFile }:
-    let
-      extraHeader = lib.optionalString (!useChapters)
-        ''xmlns="http://docbook.org/ns/docbook" xmlns:xlink="http://www.w3.org/1999/xlink" '';
-    in ''
-      {
-        pandoc '${inputFile}' -w docbook+smart ${lib.optionalString useChapters "--top-level-division=chapter"} \
-          -f markdown+smart \
-          | sed -e 's|<ulink url=|<link xlink:href=|' \
-              -e 's|</ulink>|</link>|' \
-              -e 's|<sect. id=|<section xml:id=|' \
-              -e 's|</sect[0-9]>|</section>|' \
-              -e '1s| id=| xml:id=|' \
-              -e '1s|\(<[^ ]* \)|\1${extraHeader}|'
-      } > '${outputFile}'
-    '';
-  in
-
-  ''
-    ln -s '${sources}/'*.xml .
-    mkdir ./languages-frameworks
-    cp -s '${sources-langs}'/* ./languages-frameworks
-  ''
-  + toDocbook {
-      inputFile = ./introduction.chapter.md;
-      outputFile = "introduction.chapter.xml";
-      useChapters = true;
-    }
-  + toDocbook {
-      inputFile = ./shell.section.md;
-      outputFile = "shell.section.xml";
-    }
-  + toDocbook {
-      inputFile = ./languages-frameworks/python.section.md;
-      outputFile = "./languages-frameworks/python.section.xml";
-    }
-  + toDocbook {
-      inputFile = ./languages-frameworks/haskell.section.md;
-      outputFile = "./languages-frameworks/haskell.section.xml";
-    }
-  + toDocbook {
-      inputFile = ./languages-frameworks/idris.section.md;
-      outputFile = "languages-frameworks/idris.section.xml";
-    }
-  + toDocbook {
-      inputFile = ./languages-frameworks/node.section.md;
-      outputFile = "languages-frameworks/node.section.xml";
-    }
-  + toDocbook {
-      inputFile = ./languages-frameworks/r.section.md;
-      outputFile = "languages-frameworks/r.section.xml";
-    }
-  + toDocbook {
-      inputFile = ./languages-frameworks/rust.section.md;
-      outputFile = "./languages-frameworks/rust.section.xml";
-    }
-  + toDocbook {
-      inputFile = ./languages-frameworks/vim.section.md;
-      outputFile = "./languages-frameworks/vim.section.xml";
-    }
-  + ''
+  postPatch = ''
     echo ${lib.nixpkgsVersion} > .version
   '';
 
