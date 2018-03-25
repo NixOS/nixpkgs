58d520b82425166c2f8c828a94c836faac997368
Brendan Miller <bhmiller@gmail.com>
2018-03-25 17:12:07 -0400

    pants: 1.3.0 -> 1.4.0
    Tested via running "nix-build -A pkgs.pants"

--- pkgs/development/tools/build-managers/pants/default.nix
+++ pkgs/development/tools/build-managers/pants/default.nix
@@ -4,7 +4,7 @@ with stdenv.lib;
 with pythonPackages;
 
 let
-  version = "1.3.0";
+  version = "1.4.0";
 in buildPythonApplication rec {
   inherit version;
   pname = "pantsbuild.pants";
@@ -12,7 +12,7 @@ in buildPythonApplication rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "18fcf9047l9k006wz21g525p1w5avyjabmabh0giyz22xnm8g5gp";
+    sha256 = "1jd041av1kipg4psbcwk70nwh0bsh5mkbjjcmnw4vrqdd09vg1gz";
   };
 
   prePatch = ''
@@ -24,10 +24,10 @@ in buildPythonApplication rec {
   dontStrip = true;
 
   propagatedBuildInputs = [
-    twitter-common-collections setproctitle setuptools six ansicolors
-    packaging pathspec scandir twitter-common-dirutil psutil requests
-    pystache pex docutils markdown pygments twitter-common-confluence
-    fasteners coverage pywatchman futures cffi
+    twitter-common-collections setproctitle ansicolors packaging pathspec
+    scandir twitter-common-dirutil psutil requests pystache pex docutils
+    markdown pygments twitter-common-confluence fasteners pywatchman
+    futures cffi subprocess32 contextlib2 faulthandler pyopenssl
   ];
 
   meta = {
