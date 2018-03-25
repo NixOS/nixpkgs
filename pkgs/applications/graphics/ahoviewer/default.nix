c276b454769999e94a543df0fc3c53be632394e5
Albert Safin <xzfcpw@gmail.com>
2018-03-25 14:33:41 +0700

    ahoviewer: add useUnrar option

--- pkgs/applications/graphics/ahoviewer/default.nix
+++ pkgs/applications/graphics/ahoviewer/default.nix
@@ -1,6 +1,10 @@
-{ stdenv, pkgs, fetchurl, fetchFromGitHub, pkgconfig, libconfig, 
-  gtkmm2, glibmm, libxml2, libsecret, curl, unrar, libzip,
-  librsvg, gst_all_1, autoreconfHook, makeWrapper }:
+{ stdenv, pkgs, fetchurl, fetchFromGitHub, pkgconfig, libconfig,
+  gtkmm2, glibmm, libxml2, libsecret, curl, libzip,
+  librsvg, gst_all_1, autoreconfHook, makeWrapper,
+  useUnrar ? false, unrar
+}:
+
+assert useUnrar -> unrar != null;
 
 stdenv.mkDerivation rec {
   name = "ahoviewer-${version}";
@@ -13,23 +17,25 @@ stdenv.mkDerivation rec {
     sha256 = "1adzxp30fwh41y339ha8i5qp89zf21dw18vcicqqnzvyxbk5r3ig";
   };
 
-  enableParallelBuilding = true; 
-  
+  enableParallelBuilding = true;
+
   nativeBuildInputs = [ autoreconfHook pkgconfig makeWrapper ];
-  buildInputs = [ glibmm libconfig gtkmm2 glibmm libxml2
-                  libsecret curl unrar libzip librsvg 
-                  gst_all_1.gstreamer
-                  gst_all_1.gst-plugins-good 
-                  gst_all_1.gst-libav
-                  gst_all_1.gst-plugins-base ];
+  buildInputs = [
+    glibmm libconfig gtkmm2 glibmm libxml2
+    libsecret curl libzip librsvg
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-libav
+    gst_all_1.gst-plugins-base
+  ] ++ stdenv.lib.optional useUnrar unrar;
 
   # https://github.com/ahodesuka/ahoviewer/issues/60
   # Already fixed in the master branch
   # TODO: remove this next release
   makeFlags = [ ''LIBS=-lssl -lcrypto'' ];
-  
+
   postPatch = ''patchShebangs version.sh'';
-  
+
   postInstall = ''
     wrapProgram $out/bin/ahoviewer \
     --prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0" \
