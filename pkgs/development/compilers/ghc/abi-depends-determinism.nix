ae174ee217aea691468031aab025f0c6a7fbd111
Shea Levy <shea@shealevy.com>
2018-03-26 10:09:00 -0400

    haskell.compiler.ghc8{22,41}: Fix abi-depends non-determinism.
    See https://github.com/haskell/cabal/issues/4728 for symptoms,
    https://phabricator.haskell.org/D4159 for the base of the fix.
    
    (cherry picked from commit 59a53aada0965bbbfa79349f4d7eed9ff50b4058)

---
+++ pkgs/development/compilers/ghc/abi-depends-determinism.nix
@@ -0,0 +1,10 @@
+# https://phabricator.haskell.org/D4159 to fix non-determinism in
+# cached abi-depends fields in package databases, modified to only
+# contain hunks that exist in distribution tarballs.
+{ fetchpatch, runCommand }: let
+  base = fetchpatch { # Non-determinism in cached abi-depends fields
+    url = https://phabricator-files.haskell.org/file/data/4pqrbo5b62sifktfbrls/PHID-FILE-4g4zjiqlfxmmlaos7lz7/D4159.diff;
+    sha256 = "0b8a08sisf1swmarm6nh9rgw7cpzi2rwdzvrd6ny49c7wk0f7x4b";
+  };
+in runCommand base.name {}
+  "sed -n '/utils\\/ghc-pkg/,$p' ${base} >$out"
