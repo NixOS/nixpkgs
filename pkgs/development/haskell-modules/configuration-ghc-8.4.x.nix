5dd264af4201e06b86f75b9a2fa46ffffa1b2351
Peter Simons <simons@cryp.to>
2018-03-26 11:59:30 +0200

    haskell-getopt-generics: drop obsolete override for GHC 8.4.1

--- pkgs/development/haskell-modules/configuration-ghc-8.4.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.4.x.nix
@@ -628,7 +628,6 @@ self: super: {
 
   # Older versions don't compile.
   brick = self.brick_0_35_1;
-  getopt-generics = self.getopt-generics_0_13_0_2;
   HaTeX = self.HaTeX_3_19_0_0;
   json = self.json_0_9_2;
   matrix = self.matrix_0_3_6_1;
