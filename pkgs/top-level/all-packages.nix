26acdfa35507ff0545f11f50ff262ed885a7da49
Johannes Frankenau <johannes@frankenau.net>
2018-03-26 12:35:23 +0200

    libmysofa: init at 0.6 and enable in ffmpeg-full

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10352,6 +10352,8 @@ with pkgs;
 
   libmypaint = callPackage ../development/libraries/libmypaint { };
 
+  libmysofa = callPackage ../development/libraries/audio/libmysofa { };
+
   libmysqlconnectorcpp = callPackage ../development/libraries/libmysqlconnectorcpp {
     mysql = mysql57;
   };
