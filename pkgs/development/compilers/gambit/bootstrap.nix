0874789dd5659b2048bdb5c054bc976cb00142fb
Mario Rodas <marsam@users.noreply.github.com>
2018-03-26 14:32:50 -0500

    gambit: enable on darwin

--- pkgs/development/compilers/gambit/bootstrap.nix
+++ pkgs/development/compilers/gambit/bootstrap.nix
@@ -35,7 +35,7 @@ stdenv.mkDerivation rec {
     description = "Optimizing Scheme to C compiler, bootstrap step";
     homepage    = "http://gambitscheme.org";
     license     = stdenv.lib.licenses.lgpl2;
-    platforms   = stdenv.lib.platforms.linux;
+    platforms   = stdenv.lib.platforms.unix;
     maintainers = with stdenv.lib.maintainers; [ thoughtpolice raskin fare ];
   };
 }
