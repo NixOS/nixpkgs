0bf74b94ba0b845b57e2e7eaa7395de6929068a0
Sarah Brofeldt <sbrofeldt@gmail.com>
2018-03-27 17:29:27 +0200

    godot: 2.1.4 -> 3.0 + godot_headers
    (cherry picked from commit 048724c4affebacbc4bd40d1a667ba6a7551fdd0)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2551,6 +2551,7 @@ with pkgs;
   gocryptfs = callPackage ../tools/filesystems/gocrypfs { };
 
   godot = callPackage ../development/tools/godot {};
+  godot_headers = callPackage ../development/libraries/godot_headers {};
 
   goklp = callPackage ../tools/networking/goklp {};
 
