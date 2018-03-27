4b0404e7effa6fddb5ba2f246f21e11d4f318e20
Jan Tojnar <jtojnar@gmail.com>
2018-03-27 14:53:42 +0200

    mypaint-brushes: init at 1.3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17079,6 +17079,8 @@ with pkgs;
 
   mypaint = callPackage ../applications/graphics/mypaint { };
 
+  mypaint-brushes = callPackage ../development/libraries/mypaint-brushes { };
+
   mythtv = libsForQt5.callPackage ../applications/video/mythtv { };
 
   micro = callPackage ../applications/editors/micro { };
