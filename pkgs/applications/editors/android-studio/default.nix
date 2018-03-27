aacd7d6d58fd2cc6108e914a87c6aa419c5361dd
Michael Weiss <dev.primeos@gmail.com>
2018-03-27 17:00:59 +0200

    androidStudioPackages.{dev,canary}: 3.2.0.6 -> 3.2.0.7

--- pkgs/applications/editors/android-studio/default.nix
+++ pkgs/applications/editors/android-studio/default.nix
@@ -8,9 +8,9 @@ let
     inherit (gnome2) GConf gnome_vfs;
   };
   latestVersion = {
-    version = "3.2.0.6"; # "Android Studio 3.2 Canary 7"
-    build = "173.4658582";
-    sha256Hash = "0a6ssw5mb9my5kfd5fkfdddkgdi2jdq23jlhd6rrqcs6rhdlpl38";
+    version = "3.2.0.7"; # "Android Studio 3.2 Canary 8"
+    build = "173.4670218";
+    sha256Hash = "0p1lls1pkhji8x0p32clsiq3ng64jhqv2vxkhdkmsbh5p4dc1g21";
   };
 in rec {
   # Old alias
