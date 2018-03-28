3ad3e69907a7bc9b9831d5499f5651d4b2ef711b
Tim Steinbach <tim@nequissimus.com>
2018-03-28 16:55:06 -0400

    linux: 4.14.30 -> 4.14.31
    (cherry picked from commit 6baf64f8f17020a8a7e346d56831f03e3371db39)

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,13 +3,13 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.14.30";
+  version = "4.14.31";
 
   # branchVersion needs to be x.y
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0ib6zqn1psffgffmvqmh9x3wdh15yd8z0gwwkamvgwa8xcpv0nvw";
+    sha256 = "0h30z1dlrr9zdxvqlk5lq5m9db3k6n9ci39mlsjkd5kx4ia8lnyd";
   };
 } // (args.argsOverride or {}))
