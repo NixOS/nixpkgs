231cdaf159bb2e1c19cd36c68850f81b918abf56
Will Dietz <w@wdtz.org>
2018-03-28 11:07:09 -0500

    mesa: 17.3.6 -> 17.3.7

--- pkgs/development/libraries/mesa/default.nix
+++ pkgs/development/libraries/mesa/default.nix
@@ -67,7 +67,7 @@ let
 in
 
 let
-  version = "17.3.6";
+  version = "17.3.7";
   branch  = head (splitString "." version);
 in
 
@@ -81,7 +81,7 @@ let self = stdenv.mkDerivation {
       "ftp://ftp.freedesktop.org/pub/mesa/older-versions/${branch}.x/${version}/mesa-${version}.tar.xz"
       "https://mesa.freedesktop.org/archive/mesa-${version}.tar.xz"
     ];
-    sha256 = "e5915680d44ac9d05defdec529db7459ac9edd441c9845266eff2e2d3e57fbf8";
+    sha256 = "07mcfyksspr4xp7fhhdgjhxm0182jhyav11shpzahrdsix591585";
   };
 
   prePatch = "patchShebangs .";
