53fc399bbb44d39f0b436e0fda419a9f4fb5b828
Jan Tojnar <jtojnar@gmail.com>
2018-03-29 21:21:04 +0200

    pythonPackages.mutagen: 1.36 → 1.40.0

--- pkgs/development/python-modules/mutagen/default.nix
+++ pkgs/development/python-modules/mutagen/default.nix
@@ -1,28 +1,31 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, hypothesis
+, pycodestyle
+, pyflakes
 , pytest
 , pkgs
 }:
 
 buildPythonPackage rec {
   pname = "mutagen";
-  version = "1.36";
+  version = "1.40.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1kabb9b81hgvpd3wcznww549vss12b1xlvpnxg1r6n4c7gikgvnp";
+    sha256 = "0ppfmpf60c78p4yp7in3f8y1l1fd34a38vw9swpg2fl6hz7c58mj";
   };
 
   checkInputs = [
     pkgs.faad2 pkgs.flac pkgs.vorbis-tools pkgs.liboggz
-    pkgs.glibcLocales pytest
+    pkgs.glibcLocales pycodestyle pyflakes pytest hypothesis
   ];
   LC_ALL = "en_US.UTF-8";
 
   meta = with lib; {
     description = "Python multimedia tagging library";
     homepage = https://mutagen.readthedocs.io/;
-    license = licenses.lgpl2;
+    license = licenses.lgpl2Plus;
   };
 }
