c4e848197f3e2c78740f95f1b2e9050be2192001
Jan Tojnar <jtojnar@gmail.com>
2018-03-31 00:24:44 +0200

    libvisio: fix license

--- pkgs/development/libraries/libvisio/default.nix
+++ pkgs/development/libraries/libvisio/default.nix
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     description = "A library providing ability to interpret and import visio diagrams into various applications";
     homepage = https://wiki.documentfoundation.org/DLP/Libraries/libvisio;
-    license = licenses.mpl2;
+    license = licenses.mpl20;
     platforms = platforms.unix;
   };
 }
