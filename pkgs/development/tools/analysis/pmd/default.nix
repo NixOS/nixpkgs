ca3c49c71c0204a0f6d2e4205785eceec3f381d8
R. RyanTM <ryantm+bot@ryantm.com>
2018-03-30 20:21:22 -0700

    pmd: 6.1.0 -> 6.2.0
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/pmd/versions.
    
    These checks were done:
    
    - built on NixOS
    - Warning: no binary found that responded to help or version flags. (This warning appears even if the package isn't expected to have binaries.)
    - found 6.2.0 with grep in /nix/store/flm3k0shklh231zj06ksx2h0zvb1kvp5-pmd-6.2.0
    - found 6.2.0 in filename of file in /nix/store/flm3k0shklh231zj06ksx2h0zvb1kvp5-pmd-6.2.0
    - directory tree listing: https://gist.github.com/99fc1a75766445dbe9a6459e2fe10750

--- pkgs/development/tools/analysis/pmd/default.nix
+++ pkgs/development/tools/analysis/pmd/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "pmd-${version}";
-  version = "6.1.0";
+  version = "6.2.0";
 
   buildInputs = [ unzip ];
 
   src = fetchurl {
     url = "mirror://sourceforge/pmd/pmd-bin-${version}.zip";
-    sha256 = "1d2hbsnyvwvawhfh832yy3w93kr4whrk99zpg4q5ws2b2hhdvzv8";
+    sha256 = "12j6m6lhp4xw27x0x8jcy0vlwbanjwks7w6zl56xihv6r8cm40fz";
   };
 
   installPhase = ''
