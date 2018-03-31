64886dbf8f84f0a08a0f3adbf74c979ad9ff19f4
R. RyanTM <ryantm+bot@ryantm.com>
2018-03-30 20:40:05 -0700

    pipenv: 10.1.2 -> 11.9.0
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/pipenv/versions.
    
    These checks were done:
    
    - built on NixOS
    - ran `/nix/store/rvcwp8484qsyr6v5ywyv6sczwjm38ja8-pipenv-11.9.0/bin/.pipenv-wrapped --help` got 0 exit code
    - ran `/nix/store/rvcwp8484qsyr6v5ywyv6sczwjm38ja8-pipenv-11.9.0/bin/.pipenv-wrapped --version` and found version 11.9.0
    - ran `/nix/store/rvcwp8484qsyr6v5ywyv6sczwjm38ja8-pipenv-11.9.0/bin/pipenv -h` got 0 exit code
    - ran `/nix/store/rvcwp8484qsyr6v5ywyv6sczwjm38ja8-pipenv-11.9.0/bin/pipenv --help` got 0 exit code
    - ran `/nix/store/rvcwp8484qsyr6v5ywyv6sczwjm38ja8-pipenv-11.9.0/bin/pipenv --version` and found version 11.9.0
    - found 11.9.0 with grep in /nix/store/rvcwp8484qsyr6v5ywyv6sczwjm38ja8-pipenv-11.9.0
    - directory tree listing: https://gist.github.com/337ab29e465a94749153801749378df1

--- pkgs/development/tools/pipenv/default.nix
+++ pkgs/development/tools/pipenv/default.nix
@@ -2,11 +2,11 @@
 with python3Packages; buildPythonApplication rec {
     name = "${pname}-${version}";
     pname = "pipenv";
-    version = "10.1.2";
+    version = "11.9.0";
 
     src = fetchPypi {
       inherit pname version;
-      sha256 = "ce6dbb305fb1f262dba0dcb50c06591e4d146f7bfe079cc9f0ce3f89c7516ae9";
+      sha256 = "0s291ayiszqg4ia0fiya2g3ra6m6bf7mrds1c4dscz71azxm4g3v";
     };
 
     LC_ALL = "en_US.UTF-8";
