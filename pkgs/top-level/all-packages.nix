96ccbe16ee45ec8bdc01947eec27a7006deeb630
Daiderd Jordan <daiderd@gmail.com>
2018-03-31 22:56:46 +0200

    gnumake: mark head variant lowPrio
    Fixes #38257
    
    (cherry picked from commit 86705445dc4389a9dcabc4e747eb93f5cd9f3b5d)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7817,8 +7817,8 @@ with pkgs;
   gnumake42 = callPackage ../development/tools/build-managers/gnumake/4.2 { };
   gnumake = if hostPlatform.isRiscV # Technically this check should be for glibc version.
     then gnumake42HEAD
-  else gnumake42;
-  gnumake42HEAD = callPackage ../development/tools/build-managers/gnumake/4.2/head.nix { };
+    else gnumake42;
+  gnumake42HEAD = lowPrio (callPackage ../development/tools/build-managers/gnumake/4.2/head.nix { });
 
   gnustep = recurseIntoAttrs (callPackage ../desktops/gnustep {});
 
