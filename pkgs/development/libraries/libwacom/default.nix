2d57df881fc15d3e4806490af0cce1760d964c5e
R. RyanTM <ryantm+bot@ryantm.com>
2018-03-31 05:02:08 -0700

    libwacom: 0.28 -> 0.29
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/libwacom/versions.
    
    These checks were done:
    
    - built on NixOS
    - ran `/nix/store/pdk4f0vsb3ynh1jdvqfinji6grl47m8m-libwacom-0.29/bin/libwacom-list-local-devices -h` got 0 exit code
    - ran `/nix/store/pdk4f0vsb3ynh1jdvqfinji6grl47m8m-libwacom-0.29/bin/libwacom-list-local-devices --help` got 0 exit code
    - ran `/nix/store/pdk4f0vsb3ynh1jdvqfinji6grl47m8m-libwacom-0.29/bin/libwacom-list-local-devices help` got 0 exit code
    - found 0.29 with grep in /nix/store/pdk4f0vsb3ynh1jdvqfinji6grl47m8m-libwacom-0.29
    - directory tree listing: https://gist.github.com/cc947710a926712edfc2c49d7f55510b

--- pkgs/development/libraries/libwacom/default.nix
+++ pkgs/development/libraries/libwacom/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "libwacom-${version}";
-  version = "0.28";
+  version = "0.29";
 
   src = fetchurl {
     url = "mirror://sourceforge/linuxwacom/libwacom/${name}.tar.bz2";
-    sha256 = "1vv768870597rvwxdb59v6pjn1pxaxg4r6znbb5j3cl828q35mp7";
+    sha256 = "1diklgcjhmvcxi9p1ifp6wcnyr6k7z9jhrlzfhzjqd6zipk01slw";
   };
 
   nativeBuildInputs = [ pkgconfig ];
