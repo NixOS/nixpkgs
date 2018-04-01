4e6b166b50faec4e365789ee05ab95d9b31ce846
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2018-04-01 13:33:55 +0200

    curaByDagoma: init at 1520506579 (#38075)
    Allow NixOS users and Dagoma 3D printer owners to slice 3D images for
    their printer

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17749,6 +17749,8 @@ with pkgs;
 
   curaLulzbot = callPackage ../applications/misc/cura/lulzbot.nix { };
 
+  curaByDagoma = callPackage ../applications/misc/curabydagoma { };
+
   peru = callPackage ../applications/version-management/peru {};
 
   pmidi = callPackage ../applications/audio/pmidi { };
