c1c01c784883cb6bba226becb3d8212e9409076a
Tuomas Tynkkynen <tuomas@tuxera.com>
2018-04-02 23:20:02 +0300

    raspberrypifw: Drop all non-firmware parts
    They are now built from source in raspberrypi-tools.
    
    (cherry picked from commit b59f9497aa96da5cd5f696fd0a4b03da756ca01d)

--- pkgs/os-specific/linux/firmware/raspberrypi/default.nix
+++ pkgs/os-specific/linux/firmware/raspberrypi/default.nix
@@ -11,27 +11,16 @@ stdenv.mkDerivation rec {
     sha256 = "12aisha8rlr28310hakps04z9p45kd2wvks0w1vxw1kwfh1ncy9s";
   };
 
-  dontStrip = true;    # Stripping breaks some of the binaries
-
   installPhase = ''
     mkdir -p $out/share/raspberrypi/boot
     cp -R boot/* $out/share/raspberrypi/boot
-    cp -R hardfp/opt/vc/* $out
-    cp opt/vc/LICENCE $out/share/raspberrypi
-
-    for f in $out/bin/*; do
-      if isELF "$f"; then
-        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$f"
-        patchelf --set-rpath "$out/lib" "$f"
-      fi
-    done
   '';
 
   meta = with stdenv.lib; {
     description = "Firmware for the Raspberry Pi board";
-    homepage = https://github.com/raspberrypi;
-    license = licenses.unfree;
+    homepage = https://github.com/raspberrypi/firmware;
+    license = licenses.unfreeRedistributableFirmware; # See https://github.com/raspberrypi/firmware/blob/master/boot/LICENCE.broadcom
     platforms = [ "armv6l-linux" "armv7l-linux" "aarch64-linux" ];
-    maintainers = with maintainers; [ viric tavyc ];
+    maintainers = with maintainers; [ dezgeg viric tavyc ];
   };
 }
