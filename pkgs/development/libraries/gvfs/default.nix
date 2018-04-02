ebda67e15fb202e50c0d5794d9b7847c8c6830a6
Tor Hedin Brønner <torhedinbronner@gmail.com>
2018-04-02 11:10:40 +0200

    gvfs: fix non-deterministic build failure

--- pkgs/development/libraries/gvfs/default.nix
+++ pkgs/development/libraries/gvfs/default.nix
@@ -5,7 +5,16 @@
 , gnomeSupport ? false, gnome, makeWrapper
 , libimobiledevice, libbluray, libcdio-paranoia, libnfs, openssh
 , libsecret, libgdata
+# Remove when switching back to meson
+, autoreconfHook, lzma, bzip2
 }:
+
+# TODO: switch to meson when upstream fixes a non-deterministic build failure
+# See https://bugzilla.gnome.org/show_bug.cgi?id=794549
+
+# Meson specific things are commented out and annotated, so switching back
+# should simply require deleting autotools specific things and adding back meson
+# flags etc.
 
 let
   pname = "gvfs";
@@ -19,13 +28,15 @@ stdenv.mkDerivation rec {
     sha256 = "1fsn6aa9a68cfbna9s00l1ry4ym1fr7ii2f45hzj2fipxfpqihwy";
   };
 
-  postPatch = ''
-    chmod +x meson_post_install.py # patchShebangs requires executable file
-    patchShebangs meson_post_install.py
-  '';
+  # Uncomment when switching back to meson
+  # postPatch = ''
+  #   chmod +x meson_post_install.py # patchShebangs requires executable file
+  #   patchShebangs meson_post_install.py
+  # '';
 
   nativeBuildInputs = [
-    meson ninja
+    autoreconfHook # Remove when switching to meson
+    # meson ninja
     pkgconfig gettext makeWrapper
     libxml2 libxslt docbook_xsl docbook_xml_dtd_42
   ];
@@ -35,24 +46,30 @@ stdenv.mkDerivation rec {
       libgphoto2 avahi libarchive fuse libcdio
       samba libmtp libcap polkit libimobiledevice libbluray
       libcdio-paranoia libnfs openssh
+      # Remove when switching back to meson
+      lzma bzip2
       # ToDo: a ligther version of libsoup to have FTP/HTTP support?
     ] ++ stdenv.lib.optionals gnomeSupport (with gnome; [
       libsoup gcr
       gnome-online-accounts libsecret libgdata
     ]);
 
-  mesonFlags = [
-    "-Dgio_module_dir=lib/gio/modules"
-    "-Dsystemduserunitdir=lib/systemd/user"
-    "-Ddbus_service_dir=share/dbus-1/services"
-    "-Dtmpfilesdir=no"
-  ] ++ stdenv.lib.optionals (!gnomeSupport) [
-    "-Dgcr=false" "-Dgoa=false" "-Dkeyring=false" "-Dhttp=false"
-    "-Dgoogle=false"
-  ] ++ stdenv.lib.optionals (samba == null) [
-    # Xfce don't want samba
-    "-Dsmb=false"
-  ];
+  # Remove when switching back to meson
+  configureFlags = stdenv.lib.optional (!gnomeSupport) "--disable-gcr";
+
+  # Uncomment when switching back to meson
+  # mesonFlags = [
+  #   "-Dgio_module_dir=lib/gio/modules"
+  #   "-Dsystemduserunitdir=lib/systemd/user"
+  #   "-Ddbus_service_dir=share/dbus-1/services"
+  #   "-Dtmpfilesdir=no"
+  # ] ++ stdenv.lib.optionals (!gnomeSupport) [
+  #   "-Dgcr=false" "-Dgoa=false" "-Dkeyring=false" "-Dhttp=false"
+  #   "-Dgoogle=false"
+  # ] ++ stdenv.lib.optionals (samba == null) [
+  #   # Xfce don't want samba
+  #   "-Dsmb=false"
+  # ];
 
   enableParallelBuilding = true;
 
