f6bfb10c7f53554e3084a400af640748d8140467
Elis Hirwing <elis@hirwing.se>
2018-04-03 14:41:30 +0200

    pythonPackages.docker_pycreds: Move to own file

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2134,27 +2134,7 @@ in {
 
   dockerpty = callPackage ../development/python-modules/dockerpty {};
 
-  docker_pycreds = buildPythonPackage rec {
-    name = "docker-pycreds-${version}";
-    version = "0.2.1";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/d/docker-pycreds/${name}.tar.gz";
-      sha256 = "0j3k5wk3bww5y0f2rvgzsin0q98k0i9j308vpsmxidw0y8n3m0wk";
-    };
-
-    doCheck = false; # require docker-credential-helpers binaries
-
-    propagatedBuildInputs = with self; [
-      six
-    ];
-
-    meta = {
-      description = "Python bindings for the docker credentials store API.";
-      homepage = https://github.com/shin-/dockerpy-creds;
-      license = licenses.asl20;
-    };
-  };
+  docker_pycreds = callPackage ../development/python-modules/docker-pycreds {};
 
   docker_registry_core = buildPythonPackage rec {
     name = "docker-registry-core-2.0.3";
