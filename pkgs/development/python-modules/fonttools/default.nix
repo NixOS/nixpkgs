21d5ac3d9647e4e323f8759b3b30f99f14430a55
Frederik Rietdijk <fridh@fridh.nl>
2018-04-04 20:08:31 +0200

    python: fonttools: 3.24.1 -> 3.25.0

--- pkgs/development/python-modules/fonttools/default.nix
+++ pkgs/development/python-modules/fonttools/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "fonttools";
-  version = "3.24.1";
+  version = "3.25.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "d13e98c9f3b635a5334dab69eb471d7286928ac82db7ca57b5bf4cdf3824789a";
+    sha256 = "c1b7eb0469d4e684bb8995906c327109beac870a33900090d64f85d79d646360";
     extension = "zip";
   };
 
