3a18d042f2b7765ad6c2d9ec7d3478627eed971d
Frederik Rietdijk <fridh@fridh.nl>
2018-04-04 20:04:41 +0200

    python: cookiecutter: 1.4.0 -> 1.6.0

--- pkgs/development/python-modules/cookiecutter/default.nix
+++ pkgs/development/python-modules/cookiecutter/default.nix
@@ -1,23 +1,29 @@
 { stdenv, buildPythonPackage, fetchPypi, isPyPy
-, itsdangerous, pytest, freezegun, docutils, jinja2, future, binaryornot, click
-, whichcraft, poyo, jinja2_time }:
+, pytest, pytestcov, pytest-mock, freezegun
+, jinja2, future, binaryornot, click, whichcraft, poyo, jinja2_time, requests }:
 
 buildPythonPackage rec {
   pname = "cookiecutter";
-  version = "1.4.0";
+  version = "1.6.0";
 
   # not sure why this is broken
   disabled = isPyPy;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1clxnabmc5s4b519r1sxyj1163x833ir8xcypmdfpf6r9kbb35vn";
+    sha256 = "1316a52e1c1f08db0c9efbf7d876dbc01463a74b155a0d83e722be88beda9a3e";
   };
 
-  buildInputs = [ itsdangerous pytest freezegun docutils ];
+  checkInputs = [ pytest pytestcov pytest-mock freezegun ];
   propagatedBuildInputs = [
-    jinja2 future binaryornot click whichcraft poyo jinja2_time
+    jinja2 future binaryornot click whichcraft poyo jinja2_time requests
   ];
+  
+  # requires network access for cloning git repos
+  doCheck = false;
+  checkPhase = ''
+    pytest
+  '';
 
   meta = with stdenv.lib; {
     homepage = https://github.com/audreyr/cookiecutter;
