8d55d88749764463f4916bed99978d91870173ca
Frederik Rietdijk <fridh@fridh.nl>
2018-04-04 20:18:05 +0200

    python: transaction: 2.1.2 -> 2.2.1

--- pkgs/development/python-modules/transaction/default.nix
+++ pkgs/development/python-modules/transaction/default.nix
@@ -9,11 +9,11 @@
 buildPythonPackage rec {
   name = "${pname}-${version}";
   pname = "transaction";
-  version = "2.1.2";
+  version = "2.2.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1mab0r3grmgz9d97y8pynhg0r34v0am35vpxyvh7ff5sgmg3dg5r";
+    sha256 = "f2242070e437e5d555ea3df809cb517860513254c828f33847df1c5e4b776c7a";
   };
 
   propagatedBuildInputs = [ zope_interface mock ];
