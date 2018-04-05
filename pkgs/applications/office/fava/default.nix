cd042ab504fb88b28de99e3097bd9ec034efd967
Matthias Beyer <mail@beyermatthias.de>
2018-04-05 14:07:05 +0200

    fixup! pythonPackages.uritemplate: 0.6 -> 3.0.0

--- pkgs/applications/office/fava/default.nix
+++ pkgs/applications/office/fava/default.nix
@@ -1,27 +1,38 @@
 { stdenv, python3, beancount }:
 
 let
-  inherit (python3.pkgs) buildPythonApplication fetchPypi;
+  python = python3.override {
+    packageOverrides = self: super: {
+      uritemplate = super.uritemplate.overridePythonAttrs (oldAttrs: rec {
+        version = "0.6";
+        src = oldAttrs.src.override {
+          inherit version;
+          sha256 = "1zapwg406vkwsirnzc6mwq9fac4az8brm6d9bp5xpgkyxc5263m3";
+        };
+      });
+    };
+  };
 in
-buildPythonApplication rec {
+
+python.pkgs.buildPythonApplication rec {
   pname = "fava";
   version = "1.7";
 
-  src = fetchPypi {
+  src = python.pkgs.fetchPypi {
     inherit pname version;
     sha256 = "0bxnh977r821b8vy36z81k40pazbxspx4wa9sp4ppanx7cha9sy4";
   };
 
   doCheck = false;
 
-  propagatedBuildInputs = with python3.pkgs;
+  propagatedBuildInputs = with python.pkgs;
     [ flask dateutil pygments wheel markdown2 flaskbabel tornado
       click beancount uritemplate httplib2 Babel ];
 
   meta = {
-    homepage = https://beancount.github.io/fava;
+    homepage    = https://beancount.github.io/fava;
     description = "Web interface for beancount";
-    license = stdenv.lib.licenses.mit;
+    license     = stdenv.lib.licenses.mit;
     maintainers = with stdenv.lib.maintainers; [ matthiasbeyer ];
   };
 }
