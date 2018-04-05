d7324f3d9b1d62eccba748ca934064fef34d27d3
Matthias Beyer <mail@beyermatthias.de>
2018-04-05 12:13:02 +0200

    fava: 1.6 -> 1.7

--- pkgs/applications/office/fava/default.nix
+++ pkgs/applications/office/fava/default.nix
@@ -5,19 +5,19 @@ let
 in
 buildPythonApplication rec {
   pname = "fava";
-  version = "1.6";
+  version = "1.7";
   name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0iif4imx76ra0lsisksrq5vf54wbivnrb3xqz6mkx9lik3pp5sbx";
+    sha256 = "0bxnh977r821b8vy36z81k40pazbxspx4wa9sp4ppanx7cha9sy4";
   };
 
   doCheck = false;
 
   propagatedBuildInputs = with python3.pkgs;
     [ flask dateutil pygments wheel markdown2 flaskbabel tornado
-      click beancount ];
+      click beancount uritemplate httplib2 Babel ];
 
   meta = {
     homepage = https://beancount.github.io/fava;
