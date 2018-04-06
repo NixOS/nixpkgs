4292c633b438c10ad45030fa656b40cf3dfd6e44
R. RyanTM <ryantm+bot@ryantm.com>
2018-04-06 10:18:21 -0700

    libuv: 1.19.2 -> 1.20.0
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/libuv/versions.
    
    These checks were done:
    
    - built on NixOS
    - Warning: no binary found that responded to help or version flags. (This warning appears even if the package isn't expected to have binaries.)
    - found 1.20.0 with grep in /nix/store/r6j9x782s7pl6a5055icynmzk1991imw-libuv-1.20.0
    - directory tree listing: https://gist.github.com/cff77ff2c13f732c2027bb680a833c5e

--- pkgs/development/libraries/libuv/default.nix
+++ pkgs/development/libraries/libuv/default.nix
@@ -2,14 +2,14 @@
 , ApplicationServices, CoreServices }:
 
 stdenv.mkDerivation rec {
-  version = "1.19.2";
+  version = "1.20.0";
   name = "libuv-${version}";
 
   src = fetchFromGitHub {
     owner = "libuv";
     repo = "libuv";
     rev = "v${version}";
-    sha256 = "118r8wigm65107fm7kzfz7gc4awy8xxg0knvwnshx1j40ks08x9z";
+    sha256 = "0bjxny5bjl0qy978i9n2b96vd72h9dd7ghylvhfsi1aqfwc0s1qz";
   };
 
   postPatch = let
