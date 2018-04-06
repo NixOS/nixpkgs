2bf437f0c8fafdbc3b409cf5ee57cffef29054aa
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-04-06 15:06:16 +0200

    python.pkgs.wakeonlan: fix build

--- pkgs/development/python-modules/wakeonlan/default.nix
+++ pkgs/development/python-modules/wakeonlan/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchPypi, buildPythonPackage }:
+{ stdenv, fetchPypi, buildPythonPackage, setuptools_scm, pytest, mock }:
 
 buildPythonPackage rec {
   pname = "wakeonlan";
@@ -8,6 +8,19 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "5e6013a17004809e676c150689abd94bcc0f12a37ad3fbce1f6270968f95ffa9";
   };
+
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace "setuptools-scm ~= 1.15.7" "setuptools-scm"
+  '';
+
+  checkInputs = [ pytest mock ];
+
+  nativeBuildInputs = [ setuptools_scm ];
+
+  checkPhase = ''
+    py.test
+  '';
 
   meta = with stdenv.lib; {
     description = "A small python module for wake on lan";
