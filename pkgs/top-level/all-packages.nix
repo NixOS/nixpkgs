ef5e212d20959d4d883df2ff2a54e5ef4b2512a5
Maximilian Bosch <maximilian@mbosch.me>
2018-04-06 17:16:31 +0200

    clementine: provide free derivation by default
    The `clementine` package is actually released under Apache license, but
    requires the unfree `libspotify` package to build.
    
    Now `nixpkgs.clementine` points to the free derivation and
    `nixpkgs.clementineUnfree` has been introduced for the package bundled
    with spotify support.
    
    Fixes #38315

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1603,7 +1603,7 @@ with pkgs;
       with gst_all_1; [ gst-plugins-base gst-plugins-good gst-plugins-ugly gst-libav ];
   };
 
-  clementineFree = clementine.free;
+  clementineUnfree = clementine.unfree;
 
   ciopfs = callPackage ../tools/filesystems/ciopfs { };
 
