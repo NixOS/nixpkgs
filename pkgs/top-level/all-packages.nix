b88d66ce058b5f2894aa08ea4e97d3f35e134837
Isaac Shapira <fresheyeball@gmail.com>
2018-04-06 01:57:20 -0600

    pywal: init at 2.0.5 (#38443)
    (cherry picked from commit ff5baea979b81cd3324580a6e182a8019337fe7e)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4384,6 +4384,8 @@ with pkgs;
 
   pytrainer = callPackage ../applications/misc/pytrainer { };
 
+  pywal = callPackage ../tools/graphics/pywal {};
+
   remarshal = callPackage ../development/tools/remarshal { };
 
   rtaudio = callPackage ../development/libraries/audio/rtaudio { };
