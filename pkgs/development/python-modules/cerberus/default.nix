e15ba18cee713bf43f56d7f6b1ba4a9bad559669
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-04-06 18:46:49 +0200

    python.pkgs.cerberus: specify checkInputs

--- pkgs/development/python-modules/cerberus/default.nix
+++ pkgs/development/python-modules/cerberus/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildPythonPackage, fetchPypi }:
+{ stdenv, buildPythonPackage, fetchPypi, pytestrunner, pytest }:
 
 buildPythonPackage rec {
   pname = "Cerberus";
@@ -8,6 +8,8 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "a5b39090fde3ec3294c9d7030b8eda935b42222160a66a922e0c8aea34cabfdf";
   };
+
+  checkInputs = [ pytestrunner pytest ];
 
   meta = with stdenv.lib; {
     homepage = http://python-cerberus.org/;
