f0e1b6399b1e22ebf44556fbabf22b6ffbe5e9b6
volth <volth@volth.com>
2018-04-07 07:51:11 +0000

    [cpan2nix] perlPackages.CPANMetaYAML: 0.016 -> 0.018

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -2628,12 +2628,11 @@ let self = _self // overrides; _self = with self; {
   };
 
   CPANMetaYAML = buildPerlPackage rec {
-    name = "CPAN-Meta-YAML-0.016";
+    name = "CPAN-Meta-YAML-0.018";
     src = fetchurl {
       url = "mirror://cpan/authors/id/D/DA/DAGOLDEN/${name}.tar.gz";
-      sha256 = "195v3lrfjzqxfiqi1zp02xmhp6mg9y3p7abmlfk2nj1rb28p0yrl";
+      sha256 = "150jh9l7baddl2587m23qs2l0pb395qsx9bhsgdsnn6y9k4zgjik";
     };
-    buildInputs = [ JSONPP ];
     doCheck = true;
     meta = {
       homepage = https://github.com/dagolden/CPAN-Meta-YAML;
