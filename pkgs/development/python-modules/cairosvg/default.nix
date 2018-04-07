9b3bb26f6009960da14cf55a1c22afbd3a3f531e
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-04-07 13:57:20 +0200

    python.pkgs.cairosvg: 2.1.3 -> 2.1.2
    The tarball for 2.1.3 is strangely called CairoSVG-2.1.3.linux-x86_64.tar.gz.
    Using version 2.1.2 for now.
    See https://github.com/Kozea/CairoSVG/issues/188.

--- pkgs/development/python-modules/cairosvg/default.nix
+++ pkgs/development/python-modules/cairosvg/default.nix
@@ -1,15 +1,21 @@
-{ stdenv, buildPythonPackage, fetchPypi, cairocffi }:
+{ stdenv, buildPythonPackage, fetchPypi, isPy3k
+, cairocffi, cssselect2, defusedxml, pillow, tinycss2
+, pytestrunner, pytestcov, pytest-flake8, pytest-isort }:
 
 buildPythonPackage rec {
-  pname = "cairosvg";
-  version = "2.1.3";
+  pname = "CairoSVG";
+  version = "2.1.2";
+
+  disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0b334f4ec436d78ecbc934c652f961a73a27d770f1133082cac265afab3dd64a";
+    sha256 = "70f32d0a7aec46629975d584f43093f59022a47dde403c52b095dcea10cf5b80";
   };
 
-  propagatedBuildInputs = [ cairocffi ];
+  propagatedBuildInputs = [ cairocffi cssselect2 defusedxml pillow tinycss2 ];
+
+  checkInputs = [ pytestrunner pytestcov pytest-flake8 pytest-isort ];
 
   meta = with stdenv.lib; {
     homepage = https://cairosvg.org;
