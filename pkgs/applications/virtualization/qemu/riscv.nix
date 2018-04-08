4b7f2dd6223eee408181a1136068b2eda129d905
Austin Seipp <aseipp@pobox.com>
2018-04-07 22:16:18 -0500

    qemu-riscv: update to qemu-2.11.92pre60378_f733c7b5f
    This obsoletes two of the included patches, one of them RISC-V specific,
    since they've been picked up by upstream.
    
    This build has been confirmed as being able to build and run an (extremely
    recent) RISC-V Fedora 28 Rawhide image, available from:
    
        https://fedorapeople.org/groups/risc-v/disk-images/
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/applications/virtualization/qemu/riscv.nix
+++ pkgs/applications/virtualization/qemu/riscv.nix
@@ -1,22 +1,25 @@
 { qemu, fetchFromGitHub, lib }: let
   src = fetchFromGitHub {
-    owner = "riscv";
-    repo = "riscv-qemu";
-    rev = "af435b709d4a5de3ec2e59ff4dcd05b0b295a730";
-    sha256 = "1kqcsn8yfdg3zyd991i4v5dxznd1l4a4hjry9304lvsm3sz2wllw";
+    owner  = "riscv";
+    repo   = "riscv-qemu";
+    rev    = "f733c7b5f86147216e14aff90c03ccdd76056bef";
+    sha256 = "1ppr9qqwi7qqh8m6dgk1hrzg8zri240il27l67vfayd8ijagb9zq";
     fetchSubmodules = true;
   };
-  version = "2.11.50";
-  revCount = "58771";
-  shortRev = "af435b709d";
+  version = "2.11.92";
+  revCount = "60378";
+  shortRev = builtins.substring 0 9 src.rev;
   targets = [ "riscv32-linux-user" "riscv32-softmmu"
               "riscv64-linux-user" "riscv64-softmmu"
             ];
 in lib.overrideDerivation qemu (orig: {
   name = "${(builtins.parseDrvName qemu.name).name}-${version}pre${revCount}_${shortRev}";
   inherit src;
-  # https://github.com/riscv/riscv-qemu/pull/109
-  patches = orig.patches ++ [ ./riscv-initrd.patch ];
+  # The pulseaudio and statfs patches are in 2.12.0+, which this is based on
+  patches = [
+    ./force-uid0-on-9p.patch
+    ./no-etc-install.patch
+  ];
   configureFlags = orig.configureFlags ++ [ "--target-list=${lib.concatStringsSep "," targets}" ];
   postInstall = null;
 })
