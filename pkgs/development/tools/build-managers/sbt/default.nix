2ab846211c52e3d7bb26b0888c9777598e921591
Tim Steinbach <tim@nequissimus.com>
2018-04-09 08:56:21 -0400

    sbt: 1.1.2 -> 1.1.3

--- pkgs/development/tools/build-managers/sbt/default.nix
+++ pkgs/development/tools/build-managers/sbt/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   name = "sbt-${version}";
-  version = "1.1.2";
+  version = "1.1.3";
 
   src = fetchurl {
     urls = [
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
       "https://github.com/sbt/sbt/releases/download/v${version}/sbt-${version}.tgz"
       "https://cocl.us/sbt-${version}.tgz"
     ];
-    sha256 = "0w6j9isrcr1iqrwjlpv73l09wyqknd426almnpnzmqd1m10wwxsz";
+    sha256 = "1pqw70qghzp6k0wal9nzqrd6wis0ha23i06s04l6wrf6kkc77ski";
   };
 
   patchPhase = ''
