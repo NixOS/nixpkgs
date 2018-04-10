45f6d9411469cd41631802ca9a9b09b63a4b055b
John Boehr <john@johnboehr.is>
2018-04-09 18:16:33 -0700

    nixos/cockroachdb: create new service
    This also includes a full end-to-end CockroachDB clustering test to
    ensure everything basically works. However, this test is not currently
    enabled by default, though it can be run manually. See the included
    comments in the test for more information.
    
    Closes #51306. Closes #38665.
    
    Co-authored-by: Austin Seipp <aseipp@pobox.com>
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -337,6 +337,7 @@
       alerta = 310;
       minetest = 311;
       rss2email = 312;
+      cockroachdb = 313;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -634,6 +635,7 @@
       alerta = 310;
       minetest = 311;
       rss2email = 312;
+      cockroachdb = 313;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
