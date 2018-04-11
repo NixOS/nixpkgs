23a1b1264ddc6369e385ebdfa612394017780a4e
worldofpeace <worldofpeace@users.noreply.github.com>
2018-04-11 14:01:07 -0400

    add worldofpeace as maintainer

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3971,6 +3971,11 @@
     github = "womfoo";
     name = "Kranium Gikos Mendoza";
   };
+  worldofpeace = {
+    email = "worldofpeace@users.noreply.github.com";
+    github = "worldofpeace";
+    name = "Worldofpeace";
+  };
   wscott = {
     email = "wsc9tt@gmail.com";
     github = "wscott";
