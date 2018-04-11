42588359e286db95d7efa3503000b9d15d172087
Timo Kaufmann <timokau@zoho.com>
2018-04-11 18:59:38 +0200

    pythonPackages.flask-oldsessions: init at 0.10

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5596,6 +5596,8 @@ in {
 
   flask_oauthlib = callPackage ../development/python-modules/flask-oauthlib { };
 
+  flask-oldsessions = callPackage ../development/python-modules/flask-oldsessions { };
+
   flask_principal = callPackage ../development/python-modules/flask-principal { };
 
   flask-pymongo = callPackage ../development/python-modules/Flask-PyMongo { };
