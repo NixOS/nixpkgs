00b81140f717a7b0e8424906867d446e1c69c0ea
Nikolay Amiantov <ab@fmap.me>
2018-04-12 17:19:05 +0300

    saga: use wxGTK_3

--- pkgs/applications/gis/saga/default.nix
+++ pkgs/applications/gis/saga/default.nix
@@ -1,15 +1,16 @@
-{ stdenv, fetchurl, gdal, wxGTK30, proj, libiodbc, lzma, jasper,
+{ stdenv, fetchurl, gdal, wxGTK_3, proj, libiodbc, lzma, jasper,
   libharu, opencv, vigra, postgresql }:
 
 stdenv.mkDerivation rec {
-  name = "saga-6.3.0";
+  name = "saga-${version}";
+  version = "6.3.0";
 
-  buildInputs = [ gdal wxGTK30 proj libharu opencv vigra postgresql libiodbc lzma jasper ];
+  buildInputs = [ gdal wxGTK_3 proj libharu opencv vigra postgresql libiodbc lzma jasper ];
 
   enableParallelBuilding = true;
 
   src = fetchurl {
-    url = "mirror://sourceforge/project/saga-gis/SAGA%20-%206/SAGA%20-%206.3.0/saga-6.3.0.tar.gz";
+    url = "mirror://sourceforge/project/saga-gis/SAGA%20-%206/SAGA%20-%20${version}/${name}.tar.gz";
     sha256 = "0hyjim8fcp3mna1hig22nnn4ki3j6b7096am2amcs99sdr09jjxv";
   };
 
