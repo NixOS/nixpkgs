93dba1ab6b34d61b06f2f8a3c209f1e51320924f
Nikolay Amiantov <ab@fmap.me>
2018-04-12 17:40:34 +0300

    wxGTK30: 3.0.3.1 -> 3.0.4
    Update flags and expose them in passthru.

--- pkgs/development/libraries/wxwidgets/3.0/default.nix
+++ pkgs/development/libraries/wxwidgets/3.0/default.nix
@@ -1,10 +1,10 @@
-{ stdenv, fetchFromGitHub, fetchurl, fetchpatch, pkgconfig
+{ stdenv, fetchurl, fetchpatch, pkgconfig
 , gtk2, gtk3, libXinerama, libSM, libXxf86vm
 , xf86vidmodeproto , gstreamer, gst-plugins-base, GConf, setfile
 , withMesa ? true, libGLU ? null, libGL ? null
-, compat24 ? false, compat26 ? true, unicode ? true
-, withGtk2 ? true
-, withWebKit ? false, webkitgtk24x-gtk2 ? null, webkitgtk ? null
+, compat26 ? true, compat28 ? true, unicode ? true
+, withGtk2 ? false
+, withWebKit ? true, webkitgtk24x-gtk2 ? null, webkitgtk ? null
 , AGL ? null, Carbon ? null, Cocoa ? null, Kernel ? null, QTKit ? null
 }:
 
@@ -14,17 +14,13 @@ assert withWebKit -> (if withGtk2 then webkitgtk24x-gtk2 else webkitgtk) != null
 
 with stdenv.lib;
 
-let
-  version = "3.0.3.1";
-in
-stdenv.mkDerivation {
-  name = "wxwidgets-${version}";
+stdenv.mkDerivation rec {
+  name = "wxWidgets-${version}";
+  version = "3.0.4";
 
-  src = fetchFromGitHub {
-    owner = "wxWidgets";
-    repo = "wxWidgets";
-    rev = "v${version}";
-    sha256 = "1b90in65k1ij6kyk41knxs86i6hx5lkz30gpvzdvh0cbjagv5asq";
+  src = fetchurl {
+    url = "https://github.com/wxWidgets/wxWidgets/releases/download/v${version}/${name}.tar.bz2";
+    sha256 = "1w7pgfqjab7n84lc4aarydl3g55d1hdgl2ilwml766r6inc7y5cn";
   };
 
   buildInputs =
@@ -44,23 +40,19 @@ stdenv.mkDerivation {
           + "fix_assertion_using_hide_in_destroy.diff";
       sha256 = "009y3dav79wiig789vkkc07g1qdqprg1544lih79199kb1h64lvy";
     })
-    # "Add support for WebKit2GTK+ in wxWebView". Will be in 3.0.4
-  ] ++ optional (!withGtk2) (fetchpatch {
-      url = "https://github.com/wxWidgets/wxWidgets/commit/ec6e54bc893fb7516731ca9c71e0d0bbc5ae9ff7.patch";
-      sha256 = "0gxd83xajm7gdv9rdzyvqwa2p5nz29nr23i0zx2dgfpsvz2qjp3q";
-    });
+  ];
 
   configureFlags =
     [ "--disable-precomp-headers" "--enable-mediactrl"
-      (if compat24 then "--enable-compat24" else "--disable-compat24")
-      (if compat26 then "--enable-compat26" else "--disable-compat26") ]
+      (enableFeature compat26 "compat26")
+      (enableFeature compat28 "compat28") ]
     ++ optional unicode "--enable-unicode"
     ++ optional withMesa "--with-opengl"
     ++ optionals stdenv.isDarwin
       # allow building on 64-bit
       [ "--with-cocoa" "--enable-universal-binaries" "--with-macosx-version-min=10.7" ]
     ++ optionals withWebKit
-      ["--enable-webview" "--enable-webview-webkit"];
+      [ "--enable-webview" "--enable-webviewwebkit" ];
 
   SEARCH_LIB = "${libGLU.out}/lib ${libGL.out}/lib ";
 
@@ -82,7 +74,7 @@ stdenv.mkDerivation {
   ";
 
   passthru = {
-    inherit compat24 compat26 unicode;
+    inherit compat26 compat28 unicode withGtk2 withWebKit;
     gtk = if withGtk2 then gtk2 else gtk3;
   };
 
