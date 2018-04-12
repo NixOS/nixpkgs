aa6ee2ad0e63fcfdff7690c17b5e9a1cd7275c40
Nikolay Amiantov <ab@fmap.me>
2018-04-12 17:47:15 +0300

    poedit: use wxGTK_3

--- pkgs/tools/text/poedit/default.nix
+++ pkgs/tools/text/poedit/default.nix
@@ -1,14 +1,15 @@
-{ stdenv, fetchurl, wxGTK29, boost }:
+{ stdenv, fetchurl, wxGTK_3, boost }:
 
 stdenv.mkDerivation rec {
-  name = "poedit-1.5.7";
+  name = "poedit-${version}";
+  version = "1.5.7";
 
   src = fetchurl {
     url = "http://prdownloads.sourceforge.net/poedit/${name}.tar.gz";
     sha256 = "0y0gbkb1jvp61qhh8sh7ar8849mwirizc42pk57zpxy84an5qlr4";
   };
 
-  buildInputs = [ wxGTK29 boost ];
+  buildInputs = [ wxGTK_3 boost ];
 
   meta = with stdenv.lib; {
     description = "Cross-platform gettext catalogs (.po files) editor";
