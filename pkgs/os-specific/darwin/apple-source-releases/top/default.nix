23467e207feb13d2ca783f0d36a21988e037507b
Matthew Bauer <mjbauer95@gmail.com>
2018-04-12 18:42:21 -0500

    top: add meta

--- pkgs/os-specific/darwin/apple-source-releases/top/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/top/default.nix
@@ -1,4 +1,4 @@
-{xcbuild, appleDerivation, apple_sdk, ncurses, libutil-new}:
+{xcbuild, appleDerivation, apple_sdk, ncurses, libutil-new, lib}:
 
 appleDerivation {
   buildInputs = [ xcbuild apple_sdk.frameworks.IOKit ncurses libutil-new ];
@@ -8,4 +8,8 @@ appleDerivation {
     install -D Products/Release/libtop.h $out/include/libtop.h
     install -D Products/Release/top $out/bin/top
   '';
+  meta = {
+    platforms = lib.platforms.darwin;
+    maintainers = with lib.maintainers; [ matthewbauer ];
+  };
 }
