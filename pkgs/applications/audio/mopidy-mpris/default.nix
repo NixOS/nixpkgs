542d433bee13303b917277397e3156fdc9e535f8
Thor Erik Lie <thor@thorerik.com>
2018-04-13 09:50:00 +0200

    Address change requests in PR

--- pkgs/applications/audio/mopidy-mpris/default.nix
+++ pkgs/applications/audio/mopidy-mpris/default.nix
@@ -1,20 +1,25 @@
-{ stdenv, fetchFromGitHub, pythonPackages, mopidy }:
+{ stdenv, pythonPackages, mopidy, gst_all_1, glib-networking, gobjectIntrospection }:
 
 pythonPackages.buildPythonApplication rec {
-  name = "mopidy-mpris-${version}";
-
+  pname = "Mopidy-MPRIS";
   version = "1.4.0";
 
-  src = fetchFromGitHub {
-    owner = "mopidy";
-    repo = "mopidy-mpris";
-    rev = "v${version}";
-    sha256 = "0a8hd7vcvy2whmb9qamafx787rjnvnkz4z36ncnydi0dirbnc3d1";
+  src = pythonPackages.fetchPypi {
+    inherit pname version;
+    sha256 = "0y90k3yzv8lq5g3lxfh4fys7bsmdvqgnwx2kjr00w5j3zrhmfcpa";
   };
+
+  buildInputs = with gst_all_1; [
+    gst-plugins-base gst-plugins-good gst-plugins-ugly gst-plugins-bad
+    glib-networking gobjectIntrospection
+  ];
 
   propagatedBuildInputs = with pythonPackages; [ mopidy ];
 
-  doCheck = false;
+  checkInputs = with pythonPackages; [ pytest mock gst-python pygobject3 pykka tornado requests ];
+  checkPhase = ''
+    py.test
+  '';
 
   meta = with stdenv.lib; {
     description = "Mopidy extension for controlling Mopidy via dbus";
