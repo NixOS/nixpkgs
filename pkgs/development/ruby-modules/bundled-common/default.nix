NixOS/nixpkgs

commit 8686b98612dc41d14df2a1f09a313d9084e5f6b6

ruby-modules: Allow overriding gemset
This allows patching the gemset output by bundix from a default.nix
file, making it easier to perform updates since the bundix update no
longer has to be manually updated.

pkgs/development/ruby-modules/bundled-common/default.nix
@@ -29,7 +29,9 @@ with  import ./functions.nix { inherit lib gemConfig; };
 let
   gemFiles = bundlerFiles args;
 
-  importedGemset = import gemFiles.gemset;
+  importedGemset = if builtins.typeOf gemFiles.gemset == "path"
+    then import gemFiles.gemset
+    else gemFiles.gemset;
 
   filteredGemset = filterGemset { inherit ruby groups; } importedGemset;
 

commit 746f431d9ba16d2af439bc4635b9b983abaf7297

ruby-modules: Import gemset if it's a path OR a string.
This fixes the bug introduced by 8686b98612dc which broke bundlerEnv
exprs when gemdir was a string (thus making gemset a string by
`gemset = gemdir + "/gemset.nix"`) which made it being treated as a
set.

pkgs/development/ruby-modules/bundled-common/default.nix
@@ -29,7 +29,7 @@ with  import ./functions.nix { inherit lib gemConfig; };
 let
   gemFiles = bundlerFiles args;
 
-  importedGemset = if builtins.typeOf gemFiles.gemset == "path"
+  importedGemset = if builtins.typeOf gemFiles.gemset != "set"
     then import gemFiles.gemset
     else gemFiles.gemset;
 