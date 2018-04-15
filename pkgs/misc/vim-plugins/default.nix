1935782c0019128e4b5a8902968760e51795c69c
Daiderd Jordan <daiderd@gmail.com>
2018-04-15 16:08:12 +0200

    vim-plugins: add bats.vim

--- pkgs/misc/vim-plugins/default.nix
+++ pkgs/misc/vim-plugins/default.nix
@@ -54,6 +54,7 @@ rec {
   # aliasess
   airline             = vim-airline;
   alternative         = a-vim; # backwards compat, added 2014-10-21
+  bats                = bats-vim;
   calendar            = calendar-vim;
   coffee-script       = vim-coffee-script;
   coffeeScript        = coffee-script; # backwards compat, added 2014-10-18
@@ -3041,6 +3042,17 @@ rec {
       url = "https://github.com/vim-scripts/argtextobj.vim";
       rev = "f3fbe427f7b4ec436416a5816d714dc917dc530b";
       sha256 = "1l4jh5hdmky1qj5z26jpnk49a6djjcvzyyr6pknrrgb8rzkiln48";
+    };
+    dependencies = [];
+
+  };
+
+  bats-vim = buildVimPluginFrom2Nix { # created by nix#NixDerivation
+    name = "bats-vim-2013-07-03";
+    src = fetchgit {
+      url = "https://github.com/vim-scripts/bats.vim";
+      rev = "3c283f594ff8bc7fb0c25cd07ebef0f17385f94a";
+      sha256 = "06f3hdf7y5gpwmc6inrhk938qmn7cr6mbk00amrnl1qjvk09givx";
     };
     dependencies = [];
 
