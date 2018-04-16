490e6fc9eb051b93482761ab667697972a259b34
Peter Hoeg <peter@hoeg.com>
2018-04-16 17:30:42 +0800

    vlc_npapi: drop it as firefox no longer support NPAPI

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18183,11 +18183,8 @@ with pkgs;
   vkeybd = callPackage ../applications/audio/vkeybd {};
 
   vlc = libsForQt5.vlc;
-  vlc_qt5 = vlc;
 
-  vlc_npapi = callPackage ../applications/video/vlc/plugin.nix {
-    gtk = gtk2;
-  };
+  vlc_qt5 = vlc;
 
   vmpk = callPackage ../applications/audio/vmpk { };
 
