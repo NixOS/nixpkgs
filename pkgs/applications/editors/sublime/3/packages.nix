bd4ad008dd2bfe60f7473cbf652ed3c1b11f4955
Jan Tojnar <jtojnar@gmail.com>
2018-04-16 13:06:28 +0200

    sublime3-dev: 3161 → 3162

--- pkgs/applications/editors/sublime/3/packages.nix
+++ pkgs/applications/editors/sublime/3/packages.nix
@@ -5,14 +5,14 @@ let
 in
   rec {
     sublime3-dev = common {
-      buildVersion = "3161";
-      x32sha256 = "0qrm2qmfsj71lr83c8zas2n3xk8hk9k4w8ygnasjhggmyjm3wy0q";
-      x64sha256 = "0cgadylm68s2jly10r038q1fvmbzmpc2nvqy86vlyq9avgqbm5pc";
+      buildVersion = "3162";
+      x32sha256 = "190il02hqvv64w17w7xc1fz2wkbhk5a5y96jb25dvafmslm46d4i";
+      x64sha256 = "1nsjhjs6zajhx7m3dk7i450krg6pb03zffm1n3m1v0xb9zr37xz3";
     } {};
 
     sublime3 = common {
       buildVersion = "3143";
       x32sha256 = "0dgpx4wij2m77f478p746qadavab172166bghxmj7fb61nvw9v5i";
-      x64sha256 = "06b554d2cvpxc976rvh89ix3kqc7klnngvk070xrs8wbyb221qcw";    
+      x64sha256 = "06b554d2cvpxc976rvh89ix3kqc7klnngvk070xrs8wbyb221qcw";
     } {};
   }
