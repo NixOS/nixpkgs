622745c3bf79054f99eb8b8a835f9a542fbc36b3
Shea Levy <shea@shealevy.com>
2018-04-17 07:23:27 -0400

    nix-plugins: 4.0.3 -> 4.0.4

--- pkgs/development/libraries/nix-plugins/default.nix
+++ pkgs/development/libraries/nix-plugins/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchFromGitHub, nix, cmake, pkgconfig, boost }:
-let version = "4.0.3"; in
+let version = "4.0.4"; in
 stdenv.mkDerivation {
   name = "nix-plugins-${version}";
 
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
     owner = "shlevy";
     repo = "nix-plugins";
     rev = version;
-    sha256 = "0dkrrh94s3gvym7hhdqivxzphsjh0828c0y6w6a51xdpm8rlajzj";
+    sha256 = "02lz62n55pvqin4x44qlxb5knrapyckmj9k8ggk4qxgb36368ifn";
   };
 
   nativeBuildInputs = [ cmake pkgconfig ];
