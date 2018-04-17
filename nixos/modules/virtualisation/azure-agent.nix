1d971b7a9f75333ace1f0e9c792a3f3c23221e8c
Jean-Baptiste Giraudeau <jb.giraudeau@lombardodier.com>
2018-04-17 16:04:04 +0200

    azure-agent: add option to control auto mount
     of resource disk.

--- nixos/modules/virtualisation/azure-agent.nix
+++ nixos/modules/virtualisation/azure-agent.nix
@@ -66,6 +66,10 @@ in
       default = false;
       description = "Whether to enable verbose logging.";
     };
+    mountResourceDisk = mkOption {
+      default = true;
+      description = "Whether the agent should format (ext4) and mount the resource disk to /mnt/resource.";
+    };
   };
 
   ###### implementation
@@ -112,7 +116,7 @@ in
         Provisioning.ExecuteCustomData=n
 
         # Format if unformatted. If 'n', resource disk will not be mounted.
-        ResourceDisk.Format=y
+        ResourceDisk.Format=${if cfg.mountResourceDisk then "y" else "n"}
 
         # File system on the resource disk
         # Typically ext3 or ext4. FreeBSD images should use 'ufs2' here.
