3cf0012da4de787fc9dcadcfba8dd4feb4b867f9
xeji <xeji@cat3.de>
2018-04-19 19:55:36 +0200

    gede: 2.0.4 -> 2.6.1, fix ctags dependency

--- pkgs/development/tools/misc/gede/default.nix
+++ pkgs/development/tools/misc/gede/default.nix
@@ -2,16 +2,16 @@
 
 stdenv.mkDerivation rec {
   name = "gede-${version}";
-  version = "2.0.4";
+  version = "2.6.1";
 
   src = fetchurl {
     url = "http://gede.acidron.com/uploads/source/${name}.tar.xz";
-    sha256 = "0ip86ss35sc330p4aykv5qj74jbdwh38i928w1bxb6g3w0xmfqba";
+    sha256 = "0jallpchl3c3i90hwic4n7n0ggk5wra0fki4by9ag26ln0k42c4r";
   };
 
   nativeBuildInputs = [ makeWrapper python ];
 
-  buildInputs = [ qt4 ];
+  buildInputs = [ qt4 ctags ];
 
   postPatch = ''
     sed -i build.py -e 's,qmake-qt4,qmake,'
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
   buildPhase = ":";
 
   installPhase = ''
-    python build.py install --prefix="$out"
+    python build.py install --verbose --prefix="$out"
     wrapProgram $out/bin/gede \
       --prefix PATH : ${stdenv.lib.makeBinPath [ ctags gdb ]}
   '';
