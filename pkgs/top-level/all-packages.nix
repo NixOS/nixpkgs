737937af342af2aca6997d2781dab069945025ae
Peter Hoeg <peter@hoeg.com>
2018-04-19 18:21:25 +0800

    toxvpn: 2017-06-25 -> 2018-04-17

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5226,7 +5226,7 @@ with pkgs;
 
   torsocks = callPackage ../tools/security/tor/torsocks.nix { };
 
-  toxvpn = callPackage ../tools/networking/toxvpn { };
+  toxvpn = callPackage ../tools/networking/toxvpn { libtoxcore = libtoxcore_0_1; };
 
   tpmmanager = callPackage ../applications/misc/tpmmanager { };
 
