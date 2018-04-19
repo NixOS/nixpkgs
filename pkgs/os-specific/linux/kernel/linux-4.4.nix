9e5e51d07f4d11335d0d55ff847e0300e7a90849
Tim Steinbach <tim@nequissimus.com>
2018-04-19 12:16:13 -0400

    linux: 4.4.127 -> 4.4.128

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.127";
+  version = "4.4.128";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1gscsvijik24p0skhjnhqxlvzj3kfy5cmn3x9wn6ka687hwjb3qa";
+    sha256 = "1c5nlxazn2ijfra1bn3x4fdz3fx02j76hg430jgyij61vndgi5ka";
   };
 } // (args.argsOverride or {}))
