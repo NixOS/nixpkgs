12ce0db1bffb048fbf81a852aed7240aedc31757
Peter Hoeg <peter@hoeg.com>
2018-04-19 16:10:57 +0800

    beep: fix for CVE-2018-0492

--- pkgs/misc/beep/default.nix
+++ pkgs/misc/beep/default.nix
@@ -1,25 +1,33 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchFromGitHub }:
 
 # this package is working only as root
 # in order to work as a non privileged user you would need to suid the bin
 
 stdenv.mkDerivation {
   name = "beep-1.3";
-  src = fetchurl {
-    url = http://www.johnath.com/beep/beep-1.3.tar.gz;
-    sha256 = "0bgch6jq5cahakk3kbr9549iysf2dik09afixxy5brbxk1xfzb2r";
+
+  src = fetchFromGitHub {
+    owner = "johnath";
+    repo = "beep";
+    rev = "0d790fa45777896749a885c3b93b2c1476d59f20";
+    sha256 = "0dxz58an0sz5r82al5sc935y2z2k60rz12ikjvx7sz39rfirgfpc";
   };
 
-  makeFlags = "INSTALL_DIR=\${out}/bin/ MAN_DIR=\${out}/man/man1/";
+  patches = [ ./cve-2018-0492.patch ];
+
+  makeFlags = [
+    "INSTALL_DIR=${placeholder "out"}/bin/"
+    "MAN_DIR=${placeholder "out"}/man/man1/"
+  ];
 
   preInstall = ''
-    mkdir -p $out/bin
-    mkdir -p $out/man/man1
+    mkdir -p $out/{bin,man/man1}
   '';
-  meta = {
+
+  meta = with stdenv.lib; {
     description = "The advanced PC speaker beeper";
     homepage = http://www.johnath.com/beep/;
-    license = stdenv.lib.licenses.gpl2;
-    platforms = stdenv.lib.platforms.linux;
+    license = licenses.gpl2;
+    platforms = platforms.linux;
   };
 }
