5a3a794875e7de0a4a0481b03c203b64ba0b1c22
Peter Hoeg <peter@hoeg.com>
2018-04-19 13:59:52 +0800

    use recursive attribute set

--- pkgs/development/libraries/audio/vamp/default.nix
+++ pkgs/development/libraries/audio/vamp/default.nix
@@ -1,31 +1,30 @@
 # set VAMP_PATH ?
-# plugins availible on sourceforge and http://www.vamp-plugins.org/download.html (various licenses)
+# plugins available on sourceforge and http://www.vamp-plugins.org/download.html (various licenses)
 
 { stdenv, fetchFromGitHub, pkgconfig, libsndfile }:
 
 rec {
 
-  vampSDK = stdenv.mkDerivation {
-    name = "vamp-sdk-2.7.1";
-    # version = "2.7.1";
+  vampSDK = stdenv.mkDerivation rec {
+    name = "vamp-sdk-${version}";
 
     src = fetchFromGitHub {
       owner = "c4dm";
       repo = "vamp-plugin-sdk";
-      rev = "vamp-plugin-sdk-v2.7.1";
+      rev = "vamp-plugin-sdk-v${version}";
       sha256 = "1ifd6l6b89pg83ss4gld5i72fr0cczjnl2by44z5jnndsg3sklw4";
     };
 
-  nativeBuildInputs = [ pkgconfig ];
+    nativeBuildInputs = [ pkgconfig ];
+
     buildInputs = [ libsndfile ];
 
     meta = with stdenv.lib; {
       description = "Audio processing plugin system for plugins that extract descriptive information from audio data";
       homepage = https://sourceforge.net/projects/vamp;
       license = licenses.bsd3;
-      maintainers = [ maintainers.goibhniu maintainers.marcweber ];
+      maintainers = with maintainers; [ goibhniu marcweber ];
       platforms = platforms.linux;
     };
   };
-
 }
