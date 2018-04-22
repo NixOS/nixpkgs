b52f5dba48d39a67112f95a15dd0a786959f0e52
Tim Cuthbertson <tim@gfxmonk.net>
2018-04-22 11:17:59 +1000

    gup: simplify build expression

--- pkgs/development/tools/build-managers/gup/default.nix
+++ pkgs/development/tools/build-managers/gup/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, fetchFromGitHub, lib, python, which }:
-let
+{ stdenv, fetchFromGitHub, lib, python, which, pychecker ? null }:
+stdenv.mkDerivation rec {
   version = "0.7.0";
   src = fetchFromGitHub {
     sha256 = "1pwnmlq2pgkkln9sgz4wlb9dqlqw83bkf105qljnlvggc21zm3pv";
@@ -7,15 +7,19 @@ let
     repo = "gup";
     owner = "timbertson";
   };
-in
-import ./build.nix
-  { inherit stdenv lib python which; }
-  { inherit src version;
-    meta = {
-      inherit (src.meta) homepage;
-      description = "A better make, inspired by djb's redo";
-      license = stdenv.lib.licenses.lgpl2Plus;
-      maintainers = [ stdenv.lib.maintainers.timbertson ];
-      platforms = stdenv.lib.platforms.all;
-    };
-  }
+  name = "gup-${version}";
+  buildInputs = lib.remove null [ python which pychecker ];
+  SKIP_PYCHECKER = pychecker == null;
+  buildPhase = "make python";
+  installPhase = ''
+    mkdir $out
+    cp -r python/bin $out/bin
+  '';
+  meta = {
+    inherit (src.meta) homepage;
+    description = "A better make, inspired by djb's redo";
+    license = stdenv.lib.licenses.lgpl2Plus;
+    maintainers = [ stdenv.lib.maintainers.timbertson ];
+    platforms = stdenv.lib.platforms.all;
+  };
+}
