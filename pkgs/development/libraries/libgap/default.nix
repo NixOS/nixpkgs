68394a737ecfe74846ce53f655fd3381246e85ee
Timo Kaufmann <timokau@zoho.com>
2018-04-22 23:17:00 +0200

    libgap: switch to upstream url

--- pkgs/development/libraries/libgap/default.nix
+++ pkgs/development/libraries/libgap/default.nix
@@ -1,10 +1,17 @@
-{stdenv, fetchurl, gmp}:
+{ stdenv
+, fetchurl
+, gmp
+}:
+# will probably be obsolte (or at leat built from the upstream gap sources) soon (gap 4.9?). See
+# - https://github.com/gap-system/gap/projects/5#card-6239828
+# - https://github.com/markuspf/gap/issues/2
+# - https://trac.sagemath.org/ticket/22626
 stdenv.mkDerivation rec {
   name = "libgap-${version}";
+  # Has to be the same version as "gap"
   version = "4.8.6";
-  # or fetchFromGitHub(owner,repo,rev) or fetchgit(rev)
   src = fetchurl {
-    url = "http://mirrors.mit.edu/sage/spkg/upstream/libgap/libgap-${version}.tar.gz";
+    url = "mirror://sageupstream/libgap/libgap-${version}.tar.gz";
     sha256 = "1h5fx5a55857w583ql7ly2jl49qyx9mvs7j5abys00ra9gzrpn5v";
   };
   buildInputs = [gmp];
