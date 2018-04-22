6e0a68b1e6bd8decfb78ea5cc0b21d9c65fc3058
Matthew Bauer <mjbauer95@gmail.com>
2018-04-22 15:46:51 -0500

    mono: add knownVulnerabilities for 4.0.4
    http://www.mono-project.com/docs/about-mono/vulnerabilities/#string-to-double-parser-bug

--- pkgs/development/compilers/mono/4.0.nix
+++ pkgs/development/compilers/mono/4.0.nix
@@ -3,4 +3,7 @@ callPackage ./generic.nix (rec {
   inherit Foundation libobjc;
   version = "4.0.4.1";
   sha256 = "1ydw9l89apc9p7xr5mdzy0h97g2q6v243g82mxswfc2rrqhfs4gd";
+  meta = {
+    knownVulnerabilities = [ "CVE-2009-0689" ];
+  };
 })
