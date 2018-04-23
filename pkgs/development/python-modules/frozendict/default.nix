8b5d312f277efe6fc37eba567068bdc03254033b
Florian Jacob <projects+git@florianjacob.de>
2018-04-23 15:29:50 +0200

    frozendict: disable checks as there are none

--- pkgs/development/python-modules/frozendict/default.nix
+++ pkgs/development/python-modules/frozendict/default.nix
@@ -9,6 +9,9 @@ buildPythonPackage rec {
     sha256 = "0ibf1wipidz57giy53dh7mh68f2hz38x8f4wdq88mvxj5pr7jhbp";
   };
 
+  # frozendict does not come with tests
+  doCheck = false;
+
   meta = with stdenv.lib; {
     homepage = https://github.com/slezica/python-frozendict;
     description = "An immutable dictionary";
