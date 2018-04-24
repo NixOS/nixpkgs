bdff139b3cba49d914e72a6afd0c49ae25bec0f6
Vincent Laporte <Vincent.Laporte@gmail.com>
2018-04-24 14:21:21 +0000

    coqPackages.mathcomp: 1.6.4 -> 1.7.0

--- pkgs/development/coq-modules/ssreflect/generic.nix
+++ pkgs/development/coq-modules/ssreflect/generic.nix
@@ -17,7 +17,7 @@ stdenv.mkDerivation {
   inherit patches;
 
   preBuild = ''
-    patchShebangs etc/utils/ssrcoqdep
+    patchShebangs etc/utils/ssrcoqdep || true
     cd mathcomp/ssreflect
     export COQBIN=${coq}/bin/
   '';
@@ -43,7 +43,7 @@ stdenv.mkDerivation {
   };
 
   passthru = {
-    compatibleCoqVersions = v: builtins.elem v [ "8.5" "8.6" "8.7" ];
+    compatibleCoqVersions = v: builtins.elem v [ "8.5" "8.6" "8.7" "8.8" ];
   };
 
 }
