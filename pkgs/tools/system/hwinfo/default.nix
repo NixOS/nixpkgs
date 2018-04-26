50a4977252f231d5b8f533ef5d81e9be7c24ded8
R. RyanTM <ryantm+bot@ryantm.com>
2018-04-26 04:43:51 -0700

    hwinfo: 21.52 -> 21.53
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/hwinfo/versions.
    
    These checks were done:
    
    - built on NixOS
    - ran ‘/nix/store/a5f46qz0541j2jbqycbaq1y33nb356pj-hwinfo-21.53/bin/hwinfo -h’ got 0 exit code
    - ran ‘/nix/store/a5f46qz0541j2jbqycbaq1y33nb356pj-hwinfo-21.53/bin/hwinfo --help’ got 0 exit code
    - ran ‘/nix/store/a5f46qz0541j2jbqycbaq1y33nb356pj-hwinfo-21.53/bin/hwinfo --version’ and found version 21.53
    - ran ‘/nix/store/a5f46qz0541j2jbqycbaq1y33nb356pj-hwinfo-21.53/bin/check_hd help’ got 0 exit code
    - ran ‘/nix/store/a5f46qz0541j2jbqycbaq1y33nb356pj-hwinfo-21.53/bin/getsysinfo -h’ got 0 exit code
    - ran ‘/nix/store/a5f46qz0541j2jbqycbaq1y33nb356pj-hwinfo-21.53/bin/getsysinfo --help’ got 0 exit code
    - ran ‘/nix/store/a5f46qz0541j2jbqycbaq1y33nb356pj-hwinfo-21.53/bin/getsysinfo help’ got 0 exit code
    - found 21.53 with grep in /nix/store/a5f46qz0541j2jbqycbaq1y33nb356pj-hwinfo-21.53
    - directory tree listing: https://gist.github.com/c3f6d76aa5a198a4ded2f2598380a0ef

--- pkgs/tools/system/hwinfo/default.nix
+++ pkgs/tools/system/hwinfo/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "hwinfo-${version}";
-  version = "21.52";
+  version = "21.53";
 
   src = fetchFromGitHub {
     owner = "opensuse";
     repo = "hwinfo";
     rev = "${version}";
-    sha256 = "1kva5bhylxbxgp2lv48av8mzcyybigj45rbwl9736l1kiv58i21r";
+    sha256 = "1hrazksr95pxl5p0r0x3cdph1ps98cls2v9avg0qs0qf9y18hady";
   };
 
   patchPhase = ''
