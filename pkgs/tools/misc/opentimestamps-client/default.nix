f05dc3a2bf8d6c4e4f1d63e085195cf063b27113
Erik Arvstedt <erik.arvstedt@gmail.com>
2018-04-27 20:21:50 +0200

    opentimestamps-client: 0.5.1 -> 0.6.0

--- pkgs/tools/misc/opentimestamps-client/default.nix
+++ pkgs/tools/misc/opentimestamps-client/default.nix
@@ -1,19 +1,21 @@
 { lib, buildPythonApplication, fetchFromGitHub, isPy3k
-, opentimestamps, GitPython, pysocks }:
+, opentimestamps, appdirs, GitPython, pysocks }:
 
 buildPythonApplication rec {
-  name = "opentimestamps-client-${version}";
-  version = "0.5.1";
+  pname = "opentimestamps-client";
+  version = "0.6.0";
   disabled = (!isPy3k);
 
+  # We can't use the pypi source because it doesn't include README.md which is
+  # needed in setup.py
   src = fetchFromGitHub {
     owner = "opentimestamps";
     repo = "opentimestamps-client";
-    rev = "opentimestamps-client-v0.5.1";
-    sha256 = "0s549xkb75r5wyvjlfmac8a1df6w0y55l98f492zsihdns1d6rzq";
+    rev = "opentimestamps-client-v${version}";
+    sha256 = "05m8nllqad3k69mvby5q08y22i0wrj84gqifdgcldimrrn1i00xp";
   };
 
-  propagatedBuildInputs = [ opentimestamps GitPython pysocks ];
+  propagatedBuildInputs = [ opentimestamps appdirs GitPython pysocks ];
 
   meta = {
     description = "Command-line tool to create and verify OpenTimestamps proofs";
