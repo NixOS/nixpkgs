abbf9656830f7f77210770a86b6044be4abb89e8
Sander van der Burg <svanderburg@gmail.com>
2018-04-27 16:35:02 +0200

    nodejs: use xcodebuild patch for Node.js 10.x so that modules with native dependencies on darwin will build properly

--- pkgs/development/web/nodejs/v10.nix
+++ pkgs/development/web/nodejs/v10.nix
@@ -7,5 +7,5 @@ in
     inherit enableNpm;
     version = "10.0.0";
     sha256 = "0l5bx2j4f2ij19kx14my7g7k37j3fn9qpjvbisjvhpbm42810fg2";
-    patches = lib.optionals stdenv.isDarwin [ ./no-xcode-v7.patch ];
+    patches = lib.optionals stdenv.isDarwin [ ./no-xcode-v7.patch ./no-xcodebuild.patch ];
   }
