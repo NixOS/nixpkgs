8542bde6fba24d9e5dccf5bb69dcfb1c8b97d2a9
Matthew Justin Bauer <mjbauer95@gmail.com>
2018-04-26 20:47:16 -0500

    inkscape: fix on darwin again
    I have no idea why but this broke Darwin. So added a conditional for the shebang patching.
    
    https://hydra.nixos.org/build/73283875

--- pkgs/applications/graphics/inkscape/default.nix
+++ pkgs/applications/graphics/inkscape/default.nix
@@ -29,10 +29,13 @@ stdenv.mkDerivation rec {
     patchShebangs fix-roff-punct
 
     # XXX: Not needed for CMake:
-    patchShebangs share/palettes
-    patchShebangs share/patterns
-    patchShebangs share/symbols
-    patchShebangs share/templates
+    ${lib.optionalString (!stdenv.isDarwin) ''
+      patchShebangs share/filters
+      patchShebangs share/palettes
+      patchShebangs share/patterns
+      patchShebangs share/symbols
+      patchShebangs share/templates
+    ''}
 
     # Python is used at run-time to execute scripts, e.g., those from
     # the "Effects" menu.
