191f4c210a87b2eddeabd098cd57a85fadfa0ee2
aszlig <aszlig@nix.build>
2018-04-27 17:24:21 +0200

    aqbanking: 5.6.12 -> 5.7.8
    Upstream changes in 5.7.8:
    
      * Update bank information files for German banks because the format of
        ZKA files has changed in the meantime.
    
    Upstream release notes in German:
    
    https://www.aquamaniac.de/sites/download/releasenote.php?package=03&release=217
    
    Upstream changes in 5.7.7:
    
      * Fix of manually adding new HBCI accounts (for example using
        KMyMoney).
      * Fix wrong truncation of account numbers in some turnovers of the
        SWIFT parser.
      * Simplified code for inserting new account data while fetching HBCI
        accounts.
      * Update information for German banks.
    
    Upstream release notes in German:
    
    https://www.aquamaniac.de/sites/download/releasenote.php?package=03&release=216
    
    As with the gwenhywfar update, the updater script (update.sh) actually
    didn't get the right file to download, so I temporarily modified it to
    download file number 2 because file number 1 is the GPG signature. The
    reason why I didn't fix the updater is because the upstream site will
    have different URLs in the next upcoming version, so our updater is
    basically end-of-life for now.
    
    I also changed the package expression to use the version from
    sources.nix instead of a hardcoded value.
    
    Signed-off-by: aszlig <aszlig@nix.build>
    Cc: @goibhniu

--- pkgs/development/libraries/aqbanking/sources.nix
+++ pkgs/development/libraries/aqbanking/sources.nix
@@ -6,7 +6,7 @@
   libchipcard.version = "5.0.4";
   libchipcard.sha256 = "0fj2h39ll4kiv28ch8qgzdbdbnzs8gl812qnm660bw89rynpjnnj";
   libchipcard.releaseId = "200";
-  aqbanking.version = "5.6.12";
-  aqbanking.sha256 = "08jbwmiv6f3v8iqdr44x4szna496fqcjfi6mlx04cnbx91m70lh6";
-  aqbanking.releaseId = "208";
+  aqbanking.version = "5.7.8";
+  aqbanking.sha256 = "0s67mysskbiw1h1p0np4ph4351r7wq3nc873vylam7lsqi66xy0n";
+  aqbanking.releaseId = "217";
 }
