10a6e6a2ee09b832b0ed7e67c2aed82c77501312
Tim Steinbach <tim@nequissimus.com>
2018-04-27 02:04:08 +0000

    linux-copperhead: 4.15.18.a -> 4.14.36.a
    The 4.15.x series is EOL'd, we should instead follow the LTS kernel

--- pkgs/os-specific/linux/kernel/linux-copperhead-hardened.nix
+++ pkgs/os-specific/linux/kernel/linux-copperhead-hardened.nix
@@ -3,9 +3,9 @@
 with stdenv.lib;
 
 let
-  version = "4.15.18";
+  version = "4.14.36";
   revision = "a";
-  sha256 = "1wld16i3r06mqxwwkghsxnnivg1jpc6c15y9iclhdgl4srcs1qp6";
+  sha256 = "052ck8giy5nxy7871crhx6cdkgzzzpffdm8ilq1ys0rd3vldzl9b";
 
   # modVersion needs to be x.y.z, will automatically add .0 if needed
   modVersion = concatStrings (intersperse "." (take 3 (splitString "." "${version}.0")));
