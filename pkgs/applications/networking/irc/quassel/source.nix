afb0745366f7384aed91f1088ea3b045046001f0
Andreas Rammhold <andreas@rammhold.de>
2018-04-28 11:08:42 +0200

    quassel: 0.12.4 -> 0.12.5 (fixes RCE & remote crash)
    It was found that Quassel could be remotely crashed and had an
    unauthenticated RCE vulnerability. The public annoucement can be found
    on the oss-sec archive [1]. The bump to 0.12.5 is supposed fixe both issues.
    
    [1] http://seclists.org/oss-sec/2018/q2/77

--- pkgs/applications/networking/irc/quassel/source.nix
+++ pkgs/applications/networking/irc/quassel/source.nix
@@ -1,9 +1,9 @@
 { fetchurl }:
 
 rec {
-  version = "0.12.4";
+  version = "0.12.5";
   src = fetchurl {
     url = "https://github.com/quassel/quassel/archive/${version}.tar.gz";
-    sha256 = "0q2qlhy1d6glw9pwxgcgwvspd1mkk3yi6m21dx9gnj86bxas2qs2";
+    sha256 = "04f42x87a4wkj3va3wnmj2jl7ikqqa7d7nmypqpqwalzpzk7kxwv";
   };
 }
