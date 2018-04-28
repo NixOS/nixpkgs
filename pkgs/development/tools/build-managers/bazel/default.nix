0c4601ca65d6ae7f59b710f35578e7ee04563944
Mathieu Boespflug <m@tweag.io>
2018-04-28 21:24:37 +0200

    bazel: 0.11.1 -> 0.12.0

--- pkgs/development/tools/build-managers/bazel/default.nix
+++ pkgs/development/tools/build-managers/bazel/default.nix
@@ -6,7 +6,7 @@
 
 stdenv.mkDerivation rec {
 
-  version = "0.11.1";
+  version = "0.12.0";
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/bazelbuild/bazel/";
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://github.com/bazelbuild/bazel/releases/download/${version}/bazel-${version}-dist.zip";
-    sha256 = "e8d762bcc01566fa50952c8028e95cfbe7545a39b8ceb3a0d0d6df33b25b333f";
+    sha256 = "0m3phbmmycc5rskm67i97ffr7j1bm34wmdwdqzylcl0ldp3psgiv";
   };
 
   sourceRoot = ".";
