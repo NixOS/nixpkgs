49e4a597cb2eed29cf8abe6852dc142119cb5d7b
R. RyanTM <ryantm+bot@ryantm.com>
2018-04-30 14:17:03 -0700

    mopidy-iris: 3.17.1 -> 3.17.5
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/Mopidy-Iris/versions.
    
    These checks were done:
    
    - built on NixOS
    - Warning: no binary found that responded to help or version flags. (This warning appears even if the package isn't expected to have binaries.)
    - found 3.17.5 with grep in /nix/store/ih97pncwmlk71wa9cv49aa9hx7mxc156-Mopidy-Iris-3.17.5
    - directory tree listing: https://gist.github.com/0f0187f3e9d60193ebddabe0b082e78f

--- pkgs/applications/audio/mopidy/iris.nix
+++ pkgs/applications/audio/mopidy/iris.nix
@@ -2,11 +2,11 @@
 
 pythonPackages.buildPythonApplication rec {
   pname = "Mopidy-Iris";
-  version = "3.17.1";
+  version = "3.17.5";
 
   src = pythonPackages.fetchPypi {
     inherit pname version;
-    sha256 = "02k1br077v9c5x6nn0391vh28pvn1zjbkjv8h508vy7k6ch2xjyq";
+    sha256 = "011bccvjy1rdrc43576hgfb7md404ziqmkam6na2z6v9km1b9gwr";
   };
 
   propagatedBuildInputs = [
