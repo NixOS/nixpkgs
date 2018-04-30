6a671c9cd8dcd29815a6dff6ac5281bd0a8de1b1
Tim Steinbach <tim@nequissimus.com>
2018-04-29 22:29:10 -0400

    linux: 4.17-rc2 -> 4.17-rc3

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -1,13 +1,13 @@
 { stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, libelf, utillinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.17-rc2";
-  modDirVersion = "4.17.0-rc2";
+  version = "4.17-rc3";
+  modDirVersion = "4.17.0-rc3";
   extraMeta.branch = "4.17";
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "1z4kl25x8m498wicbzhx21kvksp63ab8l2s0nfxf7fwj7dd13cld";
+    sha256 = "1divgjzmpl98b5j416vhkq53li0y9v5vvdwbgwpr2xznspzbkygq";
   };
 
   # Should the testing kernels ever be built on Hydra?
