92e89d2d142b61f4200c1c96ba1937673fd06755
Graham Christensen <graham@grahamc.com>
2018-04-30 20:52:09 -0400

    xmlformat: init at 1.04

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5744,6 +5744,8 @@ with pkgs;
 
   xml2 = callPackage ../tools/text/xml/xml2 { };
 
+  xmlformat = callPackage ../tools/text/xml/xmlformat { };
+
   xmlroff = callPackage ../tools/typesetting/xmlroff { };
 
   xmloscopy = callPackage ../tools/text/xml/xmloscopy { };
