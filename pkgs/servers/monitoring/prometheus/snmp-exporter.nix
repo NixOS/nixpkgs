1e97aa1da758865ead893e853e7ec9ab2eb31085
WilliButz <wbutz@cyberfnord.de>
2018-05-01 14:55:24 +0200

    prometheus-snmp-exporter: 0.3.0 -> 0.9.0

--- pkgs/servers/monitoring/prometheus/snmp-exporter.nix
+++ pkgs/servers/monitoring/prometheus/snmp-exporter.nix
@@ -2,16 +2,15 @@
 
 buildGoPackage rec {
   name = "snmp_exporter-${version}";
-  version = "0.3.0";
-  rev = "v${version}";
+  version = "0.9.0";
 
   goPackagePath = "github.com/prometheus/snmp_exporter";
 
   src = fetchFromGitHub {
-    inherit rev;
     owner = "prometheus";
     repo = "snmp_exporter";
-    sha256 = "1cklsi3cpalmnp0qjkgb7xbgbkr014hk2z54gfynzvzqjmsbxk6a";
+    rev = "v${version}";
+    sha256 = "081ah4zyy53plhm6znwrx55phm2ysxzyx7d4hm8zzrv5r967rgl1";
   };
 
   buildInputs = [ net_snmp ];
@@ -22,7 +21,7 @@ buildGoPackage rec {
     description = "SNMP Exporter for Prometheus";
     homepage = https://github.com/prometheus/snmp_exporter;
     license = licenses.asl20;
-    maintainers = with maintainers; [ oida ];
+    maintainers = with maintainers; [ oida willibutz ];
     platforms = platforms.unix;
   };
 }
