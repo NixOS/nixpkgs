92256a9453bde2ccb34a4d9544148f4fade924a8
Peter Simons <simons@cryp.to>
2018-05-02 13:27:47 +0200

    haskell-tar: drop obsolete override for ghc-8.4.x

--- pkgs/development/haskell-modules/configuration-ghc-8.4.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.4.x.nix
@@ -191,20 +191,6 @@ self: super: {
   });
 
   singletons = super.singletons_2_4_1;
-
-  ## Upstreamed, awaiting a Hackage release
-  tar = overrideCabal super.tar (drv: {
-    ##     • No instance for (Semigroup (Entries e))
-    ##         arising from the superclasses of an instance declaration
-    ##     • In the instance declaration for ‘Monoid (Entries e)’
-    src = pkgs.fetchFromGitHub {
-      owner  = "haskell";
-      repo   = "tar";
-      rev    = "abf2ccb8f7da0514343a0b2624cabebe081bdfa8";
-      sha256 = "0s33lgrr574i1r7zc1jqahnwx3dv47ny30mbx5zfpdzjw0jdl5ny";
-    };
-  });
-
   th-desugar = super.th-desugar_1_8;
 
   ## Upstreamed, awaiting a Hackage release
