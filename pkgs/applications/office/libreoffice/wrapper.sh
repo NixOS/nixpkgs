7bb2300026a81d72f7797e701abb259201c5ff3c
Michael Raskin <7c6f434c@mail.ru>
2018-05-02 22:46:01 +0200

    Fix libreoffice theme

--- pkgs/applications/office/libreoffice/wrapper.sh
+++ pkgs/applications/office/libreoffice/wrapper.sh
@@ -1,6 +1,6 @@
 #!@bash@/bin/bash
 export JAVA_HOME="${JAVA_HOME:-@jdk@}"
-export SAL_USE_VCLPLUGIN="${SAL_USE_VCLPLUGIN:-gen}"
+#export SAL_USE_VCLPLUGIN="${SAL_USE_VCLPLUGIN:-gen}"
 
 if uname | grep Linux > /dev/null && 
        ! ( test -n "$DBUS_SESSION_BUS_ADDRESS" && 
@@ -8,6 +8,7 @@ if uname | grep Linux > /dev/null &&
     dbus_tmp_dir="/run/user/$(id -u)/libreoffice-dbus"
     mkdir "$dbus_tmp_dir"
     dbus_socket_dir="$(mktemp -d -p "$dbus_tmp_dir")"
+<<<<<<< HEAD
     cat "@dbus@/share/dbus-1/system.conf" |
         grep -v '[<]user[>]messagebus' > "$dbus_socket_dir/system.conf"
     if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
@@ -18,6 +19,10 @@ if uname | grep Linux > /dev/null &&
         "@dbus@"/bin/dbus-daemon --nopidfile --nofork --config-file "$dbus_socket_dir/system.conf" --address "unix:path=$dbus_socket_dir/system" >&2 &
         export DBUS_SYSTEM_BUS_ADDRESS="unix:path=$dbus_socket_dir/system"
     fi
+=======
+    "@dbus@"/bin/dbus-daemon --nopidfile --nofork --config-file "@dbus@"/share/dbus-1/session.conf --address "unix:path=$dbus_socket_dir/session"  &> /dev/null &
+    export DBUS_SESSION_BUS_ADDRESS="unix:path=$dbus_socket_dir/session"
+>>>>>>> 1f52bfb67cf... libreoffice: wrapper: do not set SAL_USE_VCLPLUGIN for now
 fi
 
 "@libreoffice@/bin/$(basename "$0")" "$@"
