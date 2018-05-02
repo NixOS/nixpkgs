690c86e84dd969aa633e1f6fa36f3fcf0804a475
Tim Steinbach <tim@nequissimus.com>
2018-05-02 12:24:21 -0400

    Revert "linux: 4.9.97 -> 4.9.98"
    This reverts commit c25aeeddb2f0a8ee06dab42f7c85f1c65986dcb3.

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.98";
+  version = "4.9.97";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1gdpq4w6srz2fpyi8bqpgz0p5wm3mrk7ir967c6f2285mdvcb7r6";
+    sha256 = "08vg8lm03s04cpyicq1jj342c25x3039nnxvcvwr80j18w4biwf4";
   };
 } // (args.argsOverride or {}))
