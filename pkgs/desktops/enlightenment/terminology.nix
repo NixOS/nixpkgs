12b78da1967df6761aeceb346c94ca0931d11014
José Romildo Malaquias <malaquias@gmail.com>
2018-05-02 21:09:14 -0300

    terminology: 1.1.1 -> 1.2.0
    - Update to version 1.2.0
    - Switch build tools from autotools to meson

--- pkgs/desktops/enlightenment/terminology.nix
+++ pkgs/desktops/enlightenment/terminology.nix
@@ -1,15 +1,17 @@
-{ stdenv, fetchurl, pkgconfig, efl, pcre, makeWrapper }:
+{ stdenv, fetchurl, meson, ninja, pkgconfig, efl, pcre, mesa_noglu, makeWrapper }:
 
 stdenv.mkDerivation rec {
   name = "terminology-${version}";
-  version = "1.1.1";
+  version = "1.2.0";
 
   src = fetchurl {
     url = "http://download.enlightenment.org/rel/apps/terminology/${name}.tar.xz";
-    sha256 = "05ncxvzb9rzkyjvd95hzn8lswqdwr8cix6rd54nqn9559jibh4ns";
+    sha256 = "0kw34l5lahn1qaks3ah6x8k41d6hfywpqfak2p7qq1z87zj506mx";
   };
 
   nativeBuildInputs = [
+    meson
+    ninja
     (pkgconfig.override { vanilla = true; })
     makeWrapper
   ];
@@ -17,11 +19,12 @@ stdenv.mkDerivation rec {
   buildInputs = [
     efl
     pcre
+    mesa_noglu
   ];
 
   meta = {
-    description = "The best terminal emulator written with the EFL";
-    homepage = http://enlightenment.org/;
+    description = "Powerful terminal emulator based on EFL";
+    homepage = https://www.enlightenment.org/about-terminology;
     platforms = stdenv.lib.platforms.linux;
     license = stdenv.lib.licenses.bsd2;
     maintainers = with stdenv.lib.maintainers; [ matejc tstrobel ftrvxmtrx ];
