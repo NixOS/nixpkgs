919d824efe142c6e23d7808621f571890b0d0503
José Romildo Malaquias <malaquias@gmail.com>
2018-05-03 15:45:57 -0300

    iconpack-obsidian: init at 4.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14396,6 +14396,8 @@ with pkgs;
 
   ibm-plex = callPackage ../data/fonts/ibm-plex { };
 
+  iconpack-obsidian = callPackage ../data/icons/iconpack-obsidian { };
+
   inconsolata = callPackage ../data/fonts/inconsolata {};
   inconsolata-lgc = callPackage ../data/fonts/inconsolata/lgc.nix {};
 
