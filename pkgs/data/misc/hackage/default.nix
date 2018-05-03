f5f0b6850af4bf075cfcc2683cced9ae1577335d
Peter Simons <simons@cryp.to>
2018-05-03 14:47:03 +0200

    all-cabal-hashes: update data to Hackage at 2018-05-03T12:04:48Z

--- pkgs/data/misc/hackage/default.nix
+++ pkgs/data/misc/hackage/default.nix
@@ -1,6 +1,6 @@
 { fetchurl }:
 
 fetchurl {
-  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/61de1110af16fa0645a8cc8bbaf4a9c6fca9d8e8.tar.gz";
-  sha256 = "0vwfm1imwcffyliaama572ksjgqmwxk48py0wgvgpi2vg08w7a12";
+  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/8e0e92e4c9aea65e8af3aa0c15ee49231dcb0433.tar.gz";
+  sha256 = "0n6sliykb5li2nn7x6gwq5wms9wx90c03498cd9baf4571hb6jwa";
 }
