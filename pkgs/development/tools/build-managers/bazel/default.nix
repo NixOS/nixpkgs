328199b768ed2453ec50b6fde2202a2ce6a30882
Uri Baghin <uri@canva.com>
2018-05-07 14:08:47 +1000

    bazel: 0.12.0 -> 0.13.0

--- pkgs/development/tools/build-managers/bazel/default.nix
+++ pkgs/development/tools/build-managers/bazel/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, lib, fetchurl, jdk, zip, unzip, bash, writeScriptBin, coreutils, makeWrapper, which, python
+{ stdenv, lib, fetchurl, jdk, zip, unzip, bash, writeScriptBin, coreutils, makeWrapper
+, which, perl, python, gnused, gnugrep, findutils, diffutils, gawk
 # Always assume all markers valid (don't redownload dependencies).
 # Also, don't clean up environment variables.
 , enableNixHacks ? false
@@ -6,7 +7,7 @@
 
 stdenv.mkDerivation rec {
 
-  version = "0.12.0";
+  version = "0.13.0";
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/bazelbuild/bazel/";
@@ -20,26 +21,56 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://github.com/bazelbuild/bazel/releases/download/${version}/bazel-${version}-dist.zip";
-    sha256 = "3b3e7dc76d145046fdc78db7cac9a82bc8939d3b291e53a7ce85315feb827754";
+    sha256 = "82e9035084660b9c683187618a29aa896f8b05b5f16ae4be42a80b5e5b6a7690";
   };
 
   sourceRoot = ".";
 
-  patches = lib.optional enableNixHacks ./nix-hacks.patch;
-
-  # Bazel expects several utils to be available in Bash even without PATH. Hence this hack.
-
-  customBash = writeScriptBin "bash" ''
-    #!${stdenv.shell}
-    PATH="$PATH:${lib.makeBinPath [ coreutils ]}" exec ${bash}/bin/bash "$@"
-  '';
+  patches = [ ./full-paths.patch ] ++ lib.optionals (enableNixHacks) [ ./nix-hacks.patch ];
 
   postPatch = ''
-    find src/main/java/com/google/devtools -type f -print0 | while IFS="" read -r -d "" path; do
+    paths=(
+      scripts/packages/bazel.sh
+      src/main/java/com/google/devtools/build/lib/analysis/CommandHelper.java
+      src/main/java/com/google/devtools/build/lib/bazel/rules/BazelConfiguration.java
+      src/main/java/com/google/devtools/build/lib/rules/objc/ObjcRuleClasses.java
+      src/main/java/com/google/devtools/build/lib/util/CommandBuilder.java
+      src/main/java/com/google/devtools/build/lib/bazel/rules/BazelConfiguration.java
+      src/test/java/com/google/devtools/build/lib/bazel/rules/BazelConfigurationTest.java
+      src/test/java/com/google/devtools/build/lib/bazel/rules/genrule/GenRuleConfiguredTargetTest.java
+      src/test/java/com/google/devtools/build/lib/shell/CommandLargeInputsTest.java
+      src/test/java/com/google/devtools/build/lib/shell/CommandTest.java
+      src/test/java/com/google/devtools/build/lib/skylark/SkylarkRuleImplementationFunctionsTest.java
+      src/test/java/com/google/devtools/build/lib/standalone/StandaloneSpawnStrategyTest.java
+      src/test/shell/integration/linux-sandbox_test.sh
+      tools/cpp/osx_cc_wrapper.sh
+      tools/cpp/osx_cc_wrapper.sh.tpl
+      tools/genrule/genrule-setup.sh
+      tools/test/collect_coverage.sh
+      tools/test/test-setup.sh
+    )
+    for path in "''${paths[@]}"; do
       substituteInPlace "$path" \
-        --replace /bin/bash ${customBash}/bin/bash \
-        --replace /usr/bin/env ${coreutils}/bin/env
+        --replace '<~>bash<~>' ${bash} \
+        --replace '<~>coreutils<~>' ${coreutils} \
+        --replace '<~>gnused<~>' ${gnused} \
+        --replace '<~>gnugrep<~>' ${gnugrep} \
+        --replace '<~>perl<~>' ${perl} \
+        --replace '<~>zip<~>' ${zip} \
+        --replace '<~>unzip<~>' ${unzip} \
+        --replace '<~>python<~>' ${python} \
+        --replace '<~>findutils<~>' ${findutils} \
+        --replace '<~>diffutils<~>' ${diffutils} \
+        --replace '<~>gawk<~>' ${gawk}
     done
+    echo "build --copt=\"$(echo $NIX_CFLAGS_COMPILE | sed -e 's/ /" --copt=\"/g')\"" >> .bazelrc
+    echo "build --host_copt=\"$(echo $NIX_CFLAGS_COMPILE | sed -e 's/ /" --host_copt=\"/g')\"" >> .bazelrc
+    echo "build --linkopt=\"-Wl,$(echo $NIX_LDFLAGS | sed -e 's/ /" --linkopt=\"-Wl,/g')\"" >> .bazelrc
+    echo "build --host_linkopt=\"-Wl,$(echo $NIX_LDFLAGS | sed -e 's/ /" --host_linkopt=\"-Wl,/g')\"" >> .bazelrc
+    sed -i -e "362 a --copt=\"$(echo $NIX_CFLAGS_COMPILE | sed -e 's/ /" --copt=\"/g')\" \\\\" scripts/bootstrap/compile.sh
+    sed -i -e "362 a --host_copt=\"$(echo $NIX_CFLAGS_COMPILE | sed -e 's/ /" --host_copt=\"/g')\" \\\\" scripts/bootstrap/compile.sh
+    sed -i -e "362 a --linkopt=\"-Wl,$(echo $NIX_LDFLAGS | sed -e 's/ /" --linkopt=\"-Wl,/g')\" \\\\" scripts/bootstrap/compile.sh
+    sed -i -e "362 a --host_linkopt=\"-Wl,$(echo $NIX_LDFLAGS | sed -e 's/ /" --host_linkopt=\"-Wl,/g')\" \\\\" scripts/bootstrap/compile.sh
     patchShebangs .
   '';
 
@@ -49,11 +80,17 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [
     zip
+    perl
     python
     unzip
     makeWrapper
     which
-    customBash
+    bash
+    coreutils
+    gnused
+    gnugrep
+    diffutils
+    gawk
   ];
 
   # If TMPDIR is in the unpack dir we run afoul of blaze's infinite symlink
@@ -87,12 +124,6 @@ stdenv.mkDerivation rec {
     mkdir -p $out/share/bash-completion/completions $out/share/zsh/site-functions
     mv output/bazel-complete.bash $out/share/bash-completion/completions/
     cp scripts/zsh_completion/_bazel $out/share/zsh/site-functions/
-  '';
-
-  # Save paths to hardcoded dependencies so Nix can detect them.
-  postFixup = ''
-    mkdir -p $out/nix-support
-    echo "${customBash} ${coreutils}" > $out/nix-support/depends
   '';
 
   dontStrip = true;
