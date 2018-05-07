c412fb2c978e538deb8b7a1b8314e4567d5f4508
Timo Kaufmann <timokau@zoho.com>
2018-05-07 23:45:02 +0200

    sympy: backport upstream fixes

--- pkgs/development/python-modules/sympy/default.nix
+++ pkgs/development/python-modules/sympy/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, fetchpatch
 , glibcLocales
 , mpmath
 }:
@@ -24,6 +25,22 @@ buildPythonPackage rec {
   preCheck = ''
     export LANG="en_US.UTF-8"
   '';
+
+  patches = [
+    # see https://trac.sagemath.org/ticket/20204 and https://github.com/sympy/sympy/issues/12825
+    # There is also an upstream patch for this, included in the next release (PR #128826).
+    # However that doesn't quite fix the issue yet. Apparently some changes by sage are required.
+    # TODO re-evaluate the change once a new sympy version is released (open a sage trac ticket about
+    # it).
+    (fetchpatch {
+      url = "https://git.sagemath.org/sage.git/plain/build/pkgs/sympy/patches/03_undeffun_sage.patch?id=07d6c37d18811e2b377a9689790a7c5e24da16ba";
+      sha256 = "1mh2va1rlgizgvx8yzqwgvbf5wvswarn511002b361mc8yy0bnhr";
+    })
+    (fetchpatch {
+      url = "https://github.com/sympy/sympy/pull/13276.patch";
+      sha256 = "1rz74b5c74vwh3pj9axxgh610i02l3555vvsvr4a15ya7siw7zxh";
+    })
+  ];
 
   meta = {
     description = "A Python library for symbolic mathematics";
