480d52ebed1c80e3eff8e8dc6ddb2a402bf3d102
Yurii Rashkovskii <yrashk@gmail.com>
2018-05-07 16:18:22 -0700

    sit: better fix for aarch64 bug

--- pkgs/applications/version-management/sit/default.nix
+++ pkgs/applications/version-management/sit/default.nix
@@ -15,7 +15,7 @@ rustPlatform.buildRustPackage rec {
 
   cargoSha256 = "102haqix13nwcncng1s8qkw68spn6fhh3vysk2nbahw6f78zczqg";
 
-  patches = [ ./aarch64-eexist.patch ];
+  patches = [ ./aarch64-isel.patch ];
 
   meta = with stdenv.lib; {
     description = "Serverless Information Tracker";
