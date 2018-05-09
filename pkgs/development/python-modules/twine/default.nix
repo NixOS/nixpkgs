444d43de3f5719c6d632893fb55179f8a4e756d6
Manuel Mendez <mmendez534@gmail.com>
2018-05-09 00:40:32 -0400

    twine: update 1.9.1 -> 1.11.0

--- pkgs/development/python-modules/twine/default.nix
+++ pkgs/development/python-modules/twine/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "twine";
-  version = "1.9.1";
+  version = "1.11.0";
   name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "caa45b7987fc96321258cd7668e3be2ff34064f5c66d2d975b641adca659c1ab";
+    sha256 = "09cz9v63f8mrs4znbjapjj2z3wdfryq8q364zm0wzjhbzzcs9n9g";
   };
 
   propagatedBuildInputs = [ pkginfo requests requests_toolbelt tqdm pyblake2 ];
