NixOS/nixpkgs

commit f8d047da50dbc7d012a8d322b4d5e5a0b639733c

nixos/release-combined.nix: make tests consistent (#40131)
... with release-small.nix
Tests for small channel should be a subset of tests for full channel.

nixos/release-combined.nix
@@ -58,6 +58,8 @@ in rec {
         nixos.ova.x86_64-linux or []
 
         #(all nixos.tests.containers)
+        (all nixos.tests.containers-imperative)
+        (all nixos.tests.containers-ipv4)
         nixos.tests.chromium.x86_64-linux or []
         (all nixos.tests.firefox)
         (all nixos.tests.firewall)
@@ -98,6 +100,7 @@ in rec {
         (all nixos.tests.misc)
         (all nixos.tests.mutableUsers)
         (all nixos.tests.nat.firewall)
+        (all nixos.tests.nat.firewall-conntrack)
         (all nixos.tests.nat.standalone)
         (all nixos.tests.networking.scripted.loopback)
         (all nixos.tests.networking.scripted.static)
@@ -112,6 +115,7 @@ in rec {
         (all nixos.tests.nfs4)
         (all nixos.tests.openssh)
         (all nixos.tests.php-pcre)
+        (all nixos.tests.predictable-interface-names)
         (all nixos.tests.printing)
         (all nixos.tests.proxy)
         (all nixos.tests.sddm.default)

commit 50efa1979d86a852bbc713f624ca9897fd22e65d

nixos/release-combined.nix: fix eval of tested job
bug introduced by f8d047da50dbc7d012a8d322b4d5e5a0b639733c

nixos/release-combined.nix
@@ -115,7 +115,10 @@ in rec {
         (all nixos.tests.nfs4)
         (all nixos.tests.openssh)
         (all nixos.tests.php-pcre)
-        (all nixos.tests.predictable-interface-names)
+        (all nixos.tests.predictable-interface-names.predictable)
+        (all nixos.tests.predictable-interface-names.unpredictable)
+        (all nixos.tests.predictable-interface-names.predictableNetworkd)
+        (all nixos.tests.predictable-interface-names.unpredictableNetworkd)
         (all nixos.tests.printing)
         (all nixos.tests.proxy)
         (all nixos.tests.sddm.default)