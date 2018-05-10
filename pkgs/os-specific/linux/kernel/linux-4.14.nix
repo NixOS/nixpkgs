6c8aff71c15103ffcd6f60e3933e2be64762b48d
Tim Steinbach <tim@nequissimus.com>
2018-05-10 07:57:41 -0400

    linux: 4.14.39 -> 4.9.40

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,13 +3,13 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.14.39";
+  version = "4.14.40";
 
   # branchVersion needs to be x.y
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1gdpq4w6srz2fpyi8bqpgz0p5wm3mrk7ir967c6f2285mdvcb7r6";
+    sha256 = "03nh71nqgifkamkb0gn12jny5h3lbn5kmpdy0ff886wyrl34sw6l";
   };
 } // (args.argsOverride or {}))
