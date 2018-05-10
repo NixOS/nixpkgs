104c019ade9fd15200de6a8270f3f459ee87af33
Anton-Latukha <anton.latukha@gmail.com>
2018-05-10 11:18:18 +0300

    a52dec: rm old HandBrake patches
    Patches no longer used in HandBrake upstream

--- pkgs/development/libraries/a52dec/default.nix
+++ pkgs/development/libraries/a52dec/default.nix
@@ -2,19 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "a52dec-0.7.4p4";
-  
+
   src = fetchurl {
     url = "${meta.homepage}/files/a52dec-0.7.4.tar.gz";
     sha256 = "0czccp4fcpf2ykp16xcrzdfmnircz1ynhls334q374xknd5747d2";
   };
-
-  # From Handbrake
-  patches = [
-    ./A00-a52-state-t-public.patch
-    ./A01-thread-safe.patch
-    ./A02-imdct-shutup.patch
-    ./A03-automake.patch
-  ];
 
   meta = {
     description = "ATSC A/52 stream decoder";
