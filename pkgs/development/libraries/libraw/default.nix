44b2e4342a888718441a0548e8208251d63bd9ba
R. RyanTM <ryantm+bot@ryantm.com>
2018-05-11 06:14:45 -0700

    libraw: 0.18.10 -> 0.18.11
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/libraw/versions.
    
    These checks were done:
    
    - built on NixOS
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/raw-identify -h’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/raw-identify --help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/raw-identify help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/unprocessed_raw -h’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/unprocessed_raw --help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/unprocessed_raw help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/4channels -h’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/4channels --help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/4channels help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/simple_dcraw -h’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/simple_dcraw --help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/simple_dcraw help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/mem_image -h’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/mem_image --help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/mem_image help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/dcraw_half -h’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/dcraw_half --help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/dcraw_half help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/half_mt -h’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/half_mt --help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/half_mt help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/multirender_test -h’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/multirender_test --help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/multirender_test help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/postprocessing_benchmark -h’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/postprocessing_benchmark help’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/dcraw_emu -h’ got 0 exit code
    - ran ‘/nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11/bin/dcraw_emu help’ got 0 exit code
    - found 0.18.11 with grep in /nix/store/0ghav8lhx7nd7rl2d7rhyiwycmh2n618-libraw-0.18.11
    - directory tree listing: https://gist.github.com/237be4ff577316cefd0205ca6699ea88

--- pkgs/development/libraries/libraw/default.nix
+++ pkgs/development/libraries/libraw/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "libraw-${version}";
-  version = "0.18.10";
+  version = "0.18.11";
 
   src = fetchurl {
     url = "http://www.libraw.org/data/LibRaw-${version}.tar.gz";
-    sha256 = "0klrzg1cn8ksxqbhx52dldi5bbmad190npnhhgkyr2jzpgrbpj88";
+    sha256 = "07a7k83hx7icahh6jaxfbd7pw5jjm5i11xcqjj31b28d1aj29xvw";
   };
 
   outputs = [ "out" "lib" "dev" "doc" ];
