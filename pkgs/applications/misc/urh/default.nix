4feaaf68fbed9d1607d3029958c38bd15544c805
R. RyanTM <ryantm-bot@ryantm.com>
2018-05-12 05:04:41 -0700

    urh: 2.0.2 -> 2.0.4 (#40059)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/urh/versions.
    
    These checks were done:
    
    - built on NixOS
    - ran ‘/nix/store/g2bnnh1shkjhn7jyvjfrjyfv7li4wrld-urh-2.0.4/bin/.urh-wrapped --version’ and found version 2.0.4
    - ran ‘/nix/store/g2bnnh1shkjhn7jyvjfrjyfv7li4wrld-urh-2.0.4/bin/urh --version’ and found version 2.0.4
    - found 2.0.4 with grep in /nix/store/g2bnnh1shkjhn7jyvjfrjyfv7li4wrld-urh-2.0.4
    - directory tree listing: https://gist.github.com/f984d6b54a7cfcf840ab5c9a24c0ac95

--- pkgs/applications/misc/urh/default.nix
+++ pkgs/applications/misc/urh/default.nix
@@ -2,13 +2,13 @@
 
 python3Packages.buildPythonApplication rec {
   name = "urh-${version}";
-  version = "2.0.2";
+  version = "2.0.4";
 
   src = fetchFromGitHub {
     owner = "jopohl";
     repo = "urh";
     rev = "v${version}";
-    sha256 = "1qqb31y65rd85rf3gvxxxy06hm89ary00km1ac84qz5bwm6n5fyb";
+    sha256 = "1b796lfwasp02q1340g43z0gmza5y6jn1ga6nb22vfas0yvqpz6p";
   };
 
   buildInputs = [ hackrf rtl-sdr ];
