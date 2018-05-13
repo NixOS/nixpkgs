187ac3ee0683fd26d24fae7045bf9c5b7efc6c36
Satoshi Shishiku <satoshi.shishiku@tuta.io>
2018-05-13 15:24:20 +0300

    rspamd test: update
    Use unix sockets, don't test IPv6, general cleanup.

--- nixos/tests/rspamd.nix
+++ nixos/tests/rspamd.nix
@@ -4,7 +4,7 @@ with pkgs.lib;
 let
   initMachine = ''
     startAll
-    $machine->waitForUnit("rspamd.service");
+    $machine->waitForUnit("multi-user.target");
     $machine->succeed("id \"rspamd\" >/dev/null");
   '';
   checkSocket = socket: user: group: mode: ''
@@ -13,45 +13,30 @@ let
     $machine->succeed("[[ \"\$(stat -c %G ${socket})\" == \"${group}\" ]]");
     $machine->succeed("[[ \"\$(stat -c %a ${socket})\" == \"${mode}\" ]]");
   '';
-  simple = name: socketActivation: enableIPv6: makeTest {
+  simple = name: socketActivation: makeTest {
     name = "rspamd-${name}";
     machine = {
       services.rspamd = {
         enable = true;
         socketActivation = socketActivation;
       };
-      networking.enableIPv6 = enableIPv6;
     };
     testScript = ''
-      startAll
-      $machine->waitForUnit("multi-user.target");
-      $machine->waitForOpenPort(11334);
-      $machine->waitForUnit("rspamd.service");
-      $machine->succeed("id \"rspamd\" >/dev/null");
-      ${checkSocket "/run/rspamd/rspamd.sock" "rspamd" "rspamd" "660" }
-      sleep 10;
-      $machine->log($machine->succeed("cat /etc/rspamd.conf"));
+      ${initMachine}
       $machine->log($machine->succeed("systemctl cat rspamd.service"));
       ${if socketActivation then ''
         $machine->log($machine->succeed("systemctl cat rspamd-controller-1.socket"));
-        $machine->log($machine->succeed("systemctl cat rspamd-normal-1.socket"));
       '' else ''
         $machine->fail("systemctl cat rspamd-controller-1.socket");
-        $machine->fail("systemctl cat rspamd-normal-1.socket");
+        $machine->waitForFile("/run/rspamd/controller.sock");
       ''}
-      $machine->log($machine->succeed("curl http://localhost:11334/auth"));
-      $machine->log($machine->succeed("curl http://127.0.0.1:11334/auth"));
-      ${optionalString enableIPv6 ''
-        $machine->log($machine->succeed("curl http://[::1]:11334/auth"));
-      ''}
+      $machine->log($machine->succeed("curl --unix-socket /run/rspamd/controller.sock http://localhost/auth"));
     '';
   };
 in
 {
-  simple = simple "simple" false true;
-  ipv4only = simple "ipv4only" false false;
-  simple-socketActivated = simple "simple-socketActivated" true true;
-  ipv4only-socketActivated = simple "ipv4only-socketActivated" true false;
+  simple = simple "simple" false;
+  simple-socketActivated = simple "simple-socketActivated" true;
   deprecated = makeTest {
     name = "rspamd-deprecated";
     machine = {
@@ -67,7 +52,6 @@ in
       $machine->waitForFile("/run/rspamd.sock");
       ${checkSocket "/run/rspamd.sock" "root" "root" "600" }
       ${checkSocket "/run/rspamd-worker.sock" "root" "root" "666" }
-      $machine->log($machine->succeed("cat /etc/rspamd.conf"));
       $machine->fail("systemctl cat rspamd-normal-1.socket");
       $machine->log($machine->succeed("rspamc -h /run/rspamd-worker.sock stat"));
       $machine->log($machine->succeed("curl --unix-socket /run/rspamd-worker.sock http://localhost/ping"));
@@ -100,7 +84,6 @@ in
       $machine->waitForFile("/run/rspamd.sock");
       ${checkSocket "/run/rspamd.sock" "root" "root" "600" }
       ${checkSocket "/run/rspamd-worker.sock" "root" "root" "666" }
-      $machine->log($machine->succeed("cat /etc/rspamd.conf"));
       $machine->fail("systemctl cat rspamd-normal-1.socket");
       $machine->log($machine->succeed("rspamc -h /run/rspamd-worker.sock stat"));
       $machine->log($machine->succeed("curl --unix-socket /run/rspamd-worker.sock http://localhost/ping"));
@@ -131,7 +114,6 @@ in
       $machine->waitForFile("/run/rspamd.sock");
       ${checkSocket "/run/rspamd.sock" "root" "root" "600" }
       ${checkSocket "/run/rspamd-worker.sock" "root" "root" "666" }
-      $machine->log($machine->succeed("cat /etc/rspamd.conf"));
       $machine->log($machine->succeed("systemctl cat rspamd-normal-1.socket"));
       $machine->log($machine->succeed("rspamc -h /run/rspamd-worker.sock stat"));
       $machine->log($machine->succeed("curl --unix-socket /run/rspamd-worker.sock http://localhost/ping"));
