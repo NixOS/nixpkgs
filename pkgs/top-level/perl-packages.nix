3a4eda2e38fd80e6c9bd36606e9da16a47e41922
volth <volth@webmaster.ms>
2018-05-13 20:12:02 +0000

    perlPackages.GD: fix build on staging (#40449)

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -6461,6 +6461,9 @@ let self = _self // overrides; _self = with self; {
     # Patch has been sent upstream.
     patches = [ ../development/perl-modules/gd-options-passthrough-and-fontconfig.patch ];
 
+    # otherwise "cc1: error: -Wformat-security ignored without -Wformat [-Werror=format-security]"
+    NIX_CFLAGS_COMPILE = [ "-Wno-error=format-security" ];
+
     # tests fail
     doCheck = false;
 
