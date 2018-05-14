bdb5cd5b7f3fa7cdfe3d6f4fd4a0ed84010fe1e5
Okina Matara <okinan@chiru.no>
2018-05-13 22:36:53 -0500

    qtchan: init at v0.100

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17551,6 +17551,8 @@ with pkgs;
 
   qtbitcointrader = callPackage ../applications/misc/qtbitcointrader { };
 
+  qtchan = callPackage ../applications/networking/browsers/qtchan { };
+
   qtox = libsForQt5.callPackage ../applications/networking/instant-messengers/qtox { };
 
   qtpass = libsForQt5.callPackage ../applications/misc/qtpass { };
