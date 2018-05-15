6ba1e22e3fc48634bd6beab7af26de5330f7621f
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-05-15 23:25:30 +0300

    qca6174-firmware: pin down commit

--- dell/xps/13-9360/qca6174-firmware.nix
+++ dell/xps/13-9360/qca6174-firmware.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
   target = "QCA6174";
 
   src = fetchurl {
-    url = "https://github.com/kvalo/ath10k-firmware/raw/master/${target}/hw3.0/${branch}/firmware-6.bin_RM.${version}-QCARMSWP-1";
+    url = "https://github.com/kvalo/ath10k-firmware/raw/952afa4949cb34193040cd4e7441e1aee50ac731/${target}/hw3.0/${branch}/firmware-6.bin_RM.${version}-QCARMSWP-1";
     sha256 = "01vvz3qhqw5l3yilcqgk1spk4y9k4qy7na7a57cbl037r231szdh";
   };
 
