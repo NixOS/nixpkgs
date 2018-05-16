2cce00ab4ddff395249ae85051e93a1aec8cd003
Moritz Angermann <moritz.angermann@gmail.com>
2018-05-16 15:56:27 +0800

    cleanup mingw

--- pkgs/os-specific/windows/mingw-w64/headers.nix
+++ pkgs/os-specific/windows/mingw-w64/headers.nix
@@ -1,11 +1,15 @@
-{ stdenvNoCC, callPackage }:
+{ stdenvNoCC, callPackage, hostPlatform, targetPlatform }:
 
 let
   inherit (callPackage ./common.nix {}) name src;
 
 in stdenvNoCC.mkDerivation {
-  name = name + "-headers";
+  name = name + "-headers" + stdenvNoCC.lib.optionalString (targetPlatform != hostPlatform) "-${targetPlatform.config}";
   inherit src;
+
+  # https://sourceforge.net/p/mingw-w64/wiki2/Cross%20Win32%20and%20Win64%20compiler/
+  # specifies that the headers should be in
+  prefix = "\${out}/${targetPlatform.config}";
 
   preConfigure = ''
     cd mingw-w64-headers
