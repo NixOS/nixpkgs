e03acfe161f6c28518211df6a9d1611c84957b8f
R. RyanTM <ryantm-bot@ryantm.com>
2018-05-16 05:47:29 -0700

    timescaledb: 0.9.1 -> 0.9.2 (#40468)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/timescaledb/versions.
    
    These checks were done:
    
    - built on NixOS
    - Warning: no binary found that responded to help or version flags. (This warning appears even if the package isn't expected to have binaries.)
    - found 0.9.2 with grep in /nix/store/m28fll152hk6ik9fiz2jy66qccjxklq3-timescaledb-0.9.2
    - directory tree listing: https://gist.github.com/a922008862652d9f3c6f2e2a678c4c71
    - du listing: https://gist.github.com/8c5a0a1374fbb013168d4197d67ba214

--- pkgs/servers/sql/postgresql/timescaledb/default.nix
+++ pkgs/servers/sql/postgresql/timescaledb/default.nix
@@ -8,7 +8,7 @@
 
 stdenv.mkDerivation rec {
   name = "timescaledb-${version}";
-  version = "0.9.1";
+  version = "0.9.2";
 
   nativeBuildInputs = [ cmake ];
   buildInputs = [ postgresql ];
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
     owner  = "timescale";
     repo   = "timescaledb";
     rev    = "refs/tags/${version}";
-    sha256 = "00k8fk5a1xpv9nxlmafnngk31wh80h6m72vsl1hnyq7nhby7ylic";
+    sha256 = "1zgyd407skqbsw2zj3l9hixwlisnj82yb6hbq5khjg9k0ifvvgyp";
   };
 
   # Fix the install phase which tries to install into the pgsql extension dir,
