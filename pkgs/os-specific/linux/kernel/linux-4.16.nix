f58c4d9a9d56555ffca10844569343361ee48822
Tim Steinbach <tim@nequissimus.com>
2018-05-16 07:24:22 -0400

    linux: 4.16.8 -> 4.16.9

--- pkgs/os-specific/linux/kernel/linux-4.16.nix
+++ pkgs/os-specific/linux/kernel/linux-4.16.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.16.8";
+  version = "4.16.9";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = concatStrings (intersperse "." (take 3 (splitString "." "${version}.0")));
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1z4q7khag48wildvq4hf4vwaipkfbh9yywm2m9zfj43vk1ysvyp4";
+    sha256 = "12lvdnfz06r7pj5f15x39c4glhbp3sv7bdbwj4yimbp6iqwvndv0";
   };
 } // (args.argsOverride or {}))
