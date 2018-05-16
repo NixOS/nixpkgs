12677e712972a620f62d4711597d9b2d59e44fcd
Silvan Mosberger <infinisil@icloud.com>
2018-05-16 22:03:58 +0200

    znc: add config for testing

---
+++ config.nix
@@ -0,0 +1,22 @@
+{ pkgs, options, ... }: {
+  imports = [
+    ./nixos/modules/profiles/minimal.nix
+  ];
+  environment.systemPackages = with pkgs; [
+    vim
+  ];
+  i18n.consoleUseXkbConfig = true;
+  services.xserver.xkbVariant = "dvp";
+  users.users.root.password = "root";
+  services.znc = {
+    enable = true;
+    mutable = false;
+    confOptions.passBlock = ''
+      <Pass password>
+        Method = sha256
+        Hash = e2ce303c7ea75c571d80d8540a8699b46535be6a085be3414947d638e48d9e93
+        Salt = l5Xryew4g*!oa(ECfX2o
+      </Pass>
+    '';
+  };
+}
