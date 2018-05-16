734c5cce54cd54f1e188ef66a43fdf47d49f802e
Aistis Raulinaitis <sheganinans@gmail.com>
2018-05-16 15:17:21 -0700

    Pijul 0.8.0 -> 0.10.0

--- pkgs/applications/version-management/pijul/default.nix
+++ pkgs/applications/version-management/pijul/default.nix
@@ -1,24 +1,25 @@
-{ stdenv, fetchurl, rustPlatform, darwin }:
+{ stdenv, fetchurl, rustPlatform, darwin, libsodium, openssl, pkgconfig }:
 
 with rustPlatform;
 
 buildRustPackage rec {
   name = "pijul-${version}";
-  version = "0.8.0";
+  version = "0.10.0";
 
   src = fetchurl {
     url = "https://pijul.org/releases/${name}.tar.gz";
-    sha256 = "00pi03yp2bgnjpsz2hgaapxfw2i4idbjqc88cagpvn4yr1612wqx";
+    sha256 = "1lkipcp83rfsj9yqddvb46dmqdf2ch9njwvjv8f3g91rmfjcngys";
   };
 
-  sourceRoot = "${name}/pijul";
+  nativeBuildInputs = [ pkgconfig ];
 
-  buildInputs = stdenv.lib.optionals stdenv.isDarwin
-    (with darwin.apple_sdk.frameworks; [ Security ]);
+  buildInputs = [ libsodium openssl ] ++
+    stdenv.lib.optionals stdenv.isDarwin
+      (with darwin.apple_sdk.frameworks; [ Security ]);
 
   doCheck = false;
 
-  cargoSha256 = "1cnr08qbpia3336l37k1jli20d7kwnrw2gys8s9mg271cb4vdx03";
+  cargoSha256 = "1419mlxa4p53hm5qzfd1yi2k0n1bcv8kaslls1nyx661vknhfamw";
 
   meta = with stdenv.lib; {
     description = "A distributed version control system";
