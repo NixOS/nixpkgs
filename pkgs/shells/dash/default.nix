391a2b376d0f0223a743023e618d273de0204d25
R. RyanTM <ryantm+bot@ryantm.com>
2018-05-17 07:52:30 -0700

    dash: 0.5.10 -> 0.5.10.1
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/dash/versions.
    
    These checks were done:
    
    - built on NixOS
    - /nix/store/xdfhswm4pr989ad5582dphj02nlhd09g-dash-0.5.10.1/bin/dash passed the binary check.
    - 1 of 1 passed binary check by having a zero exit code.
    - 0 of 1 passed binary check by having the new version present in output.
    - directory tree listing: https://gist.github.com/c7533f80b4151e29fb67f31acbea016b
    - du listing: https://gist.github.com/455f25b553fefbc281441c80772198d1

--- pkgs/shells/dash/default.nix
+++ pkgs/shells/dash/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "dash-0.5.10";
+  name = "dash-0.5.10.1";
 
   src = fetchurl {
     url = "http://gondor.apana.org.au/~herbert/dash/files/${name}.tar.gz";
-    sha256 = "1arimvc9zcghhb3nin9z3yr5706vxfri4a9r3j9j9d0n676f0w5d";
+    sha256 = "1bl4brz5vy07lrss54glp4vfca3q8d73hyc87sqdk99f76z95b6s";
   };
 
   hardeningDisable = [ "format" ];
