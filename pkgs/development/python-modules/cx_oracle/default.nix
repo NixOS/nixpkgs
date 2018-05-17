639f7952be31f89d22512bf661d03a4fe2b075a2
Florian Klink <flokli@flokli.de>
2018-05-17 18:43:25 +0200

    pythonPackages.cx_oracle: fix dlopen
    previously, $ORACLE_HOME had to be set for each python script using this
    library.
    
    We now patch odpi to load libclntsh.so from oracle-instantclient if
    $ORACLE_HOME was not provided.

--- pkgs/development/python-modules/cx_oracle/default.nix
+++ pkgs/development/python-modules/cx_oracle/default.nix
@@ -13,6 +13,12 @@ buildPythonPackage rec {
     sha256 = "0200j6jh80rpgzxmvgcxmkshaj4zadq32g2i97nlwiq3f7q374l7";
   };
 
+  patches = [ ./0001-odpi-src-dpiOci.c-nixify-libclntsh.so-dlopen.patch ];
+
+  postPatch = ''
+    substituteInPlace odpi/src/dpiOci.c --replace @libclntsh@ ${oracle-instantclient}/lib/libclntsh.so";
+  '';
+
   # Check need an Oracle database to run
   doCheck = false;
 
