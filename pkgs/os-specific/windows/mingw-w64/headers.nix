1353a6eb25688e9b7301bf01ddb0899793797186
Moritz Angermann <moritz.angermann@gmail.com>
2018-05-17 14:06:44 +0800

    make bulding libgcc work.

--- pkgs/os-specific/windows/mingw-w64/headers.nix
+++ pkgs/os-specific/windows/mingw-w64/headers.nix
@@ -7,11 +7,13 @@ in stdenvNoCC.mkDerivation {
   name = name + "-headers" + stdenvNoCC.lib.optionalString (targetPlatform != hostPlatform) "-${targetPlatform.config}";
   inherit src;
 
-  # https://sourceforge.net/p/mingw-w64/wiki2/Cross%20Win32%20and%20Win64%20compiler/
-  # specifies that the headers should be in
-  prefix = "\${out}/${targetPlatform.config}";
-
   preConfigure = ''
     cd mingw-w64-headers
   '';
+
+  configureFlags = [
+    "--enable-idl"
+    "--enable-secure-api"
+    # "--with-default-win32-winnt=0x600"
+  ];
 }
--- pkgs/os-specific/windows/mingw-w64/pthreads.nix
+++ pkgs/os-specific/windows/mingw-w64/pthreads.nix
@@ -1,9 +1,11 @@
-{ stdenv, callPackage }:
+{ stdenv, callPackage, windows }:
 with stdenv.lib;
 let inherit (callPackage ./common.nix {}) name src;
 in stdenv.mkDerivation {
   name = name + "-pthreads";
   inherit src;
+
+  buildInputs = [ windows.mingw_w64_headers windows.mingw_w64 ];
 
   preConfigure = ''
     cd mingw-w64-libraries/winpthreads
