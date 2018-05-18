d46aa9d606199a8acd49248e880783aa07560a06
obadz <obadz-git@obadz.com>
2018-05-18 22:15:07 +0100

    opensmtpd: 6.0.2p1 -> 6.0.3p1

--- pkgs/servers/mail/opensmtpd/default.nix
+++ pkgs/servers/mail/opensmtpd/default.nix
@@ -5,28 +5,27 @@
 # see also https://github.com/OpenSMTPD/OpenSMTPD/issues/678
 , unpriviledged_smtpctl_encrypt ? true
 
-# This enables you to override the '+' character which typically separates the user from the tag in user+tag@domain.tld
+# Deprecated: use the subaddressing-delimiter in the config file going forward
 , tag_char ? null
 }:
 
-stdenv.mkDerivation rec {
+if (tag_char != null)
+then throw "opensmtpd: the tag_char argument is deprecated as it can now be specified at runtime via the 'subaddressing-delimiter' option of the configuration file"
+else stdenv.mkDerivation rec {
   name = "opensmtpd-${version}";
-  version = "6.0.2p1";
+  version = "6.0.3p1";
 
   nativeBuildInputs = [ autoconf automake libtool bison ];
   buildInputs = [ libasr libevent zlib openssl db pam ];
 
   src = fetchurl {
     url = "https://www.opensmtpd.org/archives/${name}.tar.gz";
-    sha256 = "1b4h64w45hpmfq5721smhg4s0shs64gbcjqjpx3fbiw4hz8bdy9a";
+    sha256 = "291881862888655565e8bbe3cfb743310f5dc0edb6fd28a889a9a547ad767a81";
   };
 
   patches = [ ./proc_path.diff ];
 
   postPatch = with builtins; with lib;
-    optionalString (isString tag_char) ''
-      sed -i -e "s,TAG_CHAR.*'+',TAG_CHAR '${tag_char}'," smtpd/smtpd-defines.h
-    '' +
     optionalString unpriviledged_smtpctl_encrypt ''
       substituteInPlace smtpd/smtpctl.c --replace \
         'if (geteuid())' \
