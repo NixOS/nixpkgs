f8ed43e600228ab415866d4938703fa5eaa7b121
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-05-19 17:20:33 +0300

    wireguard: use src from wireguard-tools

--- pkgs/os-specific/linux/wireguard/default.nix
+++ pkgs/os-specific/linux/wireguard/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchzip, kernel }:
+{ stdenv, fetchzip, kernel, wireguard-tools }:
 
 # module requires Linux >= 3.10 https://www.wireguard.io/install/#kernel-requirements
 assert stdenv.lib.versionAtLeast kernel.version "3.10";
@@ -7,10 +7,7 @@ stdenv.mkDerivation rec {
   name = "wireguard-${version}";
   version = "0.0.20180514";
 
-  src = fetchzip {
-    url    = "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-${version}.tar.xz";
-    sha256 = "15z0s1i8qyq1fpw8j6rky53ffrpp3f49zn1022jwdslk4g0ncaaj";
-  };
+  inherit (wireguard-tools) src;
 
   preConfigure = ''
     cd src
