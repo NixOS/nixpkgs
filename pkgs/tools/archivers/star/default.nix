ad8e2684ab007ed65b1af0fc416ea19de4dff89b
wucke13 <wucke13@gmail.com>
2018-05-20 23:29:48 +0200

    Adjusted to rycee's review on pull request 40771, took xejis hint

--- pkgs/tools/archivers/star/default.nix
+++ pkgs/tools/archivers/star/default.nix
@@ -4,31 +4,32 @@ stdenv.mkDerivation rec {
   name = "star-${version}";
   version = "1.5.3";
   src = fetchurl {
-    url = "https://sourceforge.net/projects/s-tar/files/${name}.tar.gz";
-    sha256 = "1yii6p6x38r6w12s5yrdi08aij481jcg3xf8g738nmh0hqypbfl7";
+    url = "mirror://sourceforge/s-tar/${name}.tar.bz2";
+    sha256 = "0nsg3adv8lwqsbizicgmyxx8w26d1f4almprkcb08cd87s1l40q7";
   };
 
   preConfigure = "rm configure";
   preBuild = "sed 's_/bin/__g' -i RULES/*";
-  makeFlags = [ "DESTDIR=$(out)" "INS_BASE=/ install"];
+  makeFlags = [ "GMAKE_NOWARN=true" ];
+  installFlags = [ "DESTDIR=$(out)" "INS_BASE=/" ];
   postInstall = ''
-  find $out/bin -type l -delete
-  rm -r $out/etc $out/include $out/sbin
+    find $out/bin -type l -delete
+    rm -r $out/etc $out/include $out/sbin
   '';
 
   meta = {
-    description = "Star is a very fast tar like tape archiver with  improved functionality.";
+    description = "A very fast tar like tape archiver with improved functionality";
     longDescription = ''
-Star archives and extracts multiple files to and from a single file called a tarfile.
-A tarfile is usually a magnetic tape, but it can be any file.
-In all cases, appearance of a directory name refers to the files and (recursively) sub-directories of that directory.
-Star's actions are controlled by the mandatory command flags from the list below.
-The way star acts may be modified by additional options.
-Note that unpacking tar archives may be a security risk because star may overwrite existing files.
+      Star archives and extracts multiple files to and from a single file called a tarfile.
+      A tarfile is usually a magnetic tape, but it can be any file.
+      In all cases, appearance of a directory name refers to the files and (recursively) sub-directories of that directory.
+      Star's actions are controlled by the mandatory command flags from the list below.
+      The way star acts may be modified by additional options.
+      Note that unpacking tar archives may be a security risk because star may overwrite existing files.
     '';
     homepage = http://cdrtools.sourceforge.net/private/star.html;
     license = stdenv.lib.licenses.cddl;
     maintainers = [ stdenv.lib.maintainers.wucke13 ];
-    platforms = stdenv.lib.platforms.all;
+    platforms = [ "x86_64-linux" ];
   };
 }
