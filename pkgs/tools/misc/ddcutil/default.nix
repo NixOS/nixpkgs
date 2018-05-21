15bcea5f9285125df1863ec3a0714f4fde6f1e2e
R. RyanTM <ryantm+bot@ryantm.com>
2018-05-20 20:32:07 -0700

    ddcutil: 0.8.6 -> 0.9.0
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/ddcutil/versions.
    
    These checks were done:
    
    - built on NixOS
    - /nix/store/xlxjnfwwcwkhzkpg2qwxz5j5gbh4csnk-ddcutil-0.9.0/bin/ddcutil passed the binary check.
    - 1 of 1 passed binary check by having a zero exit code.
    - 0 of 1 passed binary check by having the new version present in output.
    - found 0.9.0 with grep in /nix/store/xlxjnfwwcwkhzkpg2qwxz5j5gbh4csnk-ddcutil-0.9.0
    - directory tree listing: https://gist.github.com/39ac3ce627e1198f84287e5a42257663
    - du listing: https://gist.github.com/b896a17a17dd52041097af423a55aea6

--- pkgs/tools/misc/ddcutil/default.nix
+++ pkgs/tools/misc/ddcutil/default.nix
@@ -3,13 +3,13 @@
 
 stdenv.mkDerivation rec {
   name = "ddcutil-${version}";
-  version = "0.8.6";
+  version = "0.9.0";
 
   src = fetchFromGitHub {
     owner  = "rockowitz";
     repo   = "ddcutil";
     rev    = "v${version}";
-    sha256 = "1c4cl9cac90xf9rap6ss2d4yshcmhdq8pdfjz3g4cns789fs1vcf";
+    sha256 = "1lcn3jbhpcm6ixp24vsfnk1v0qi0fjkkf57f4grs1wg148s3jpvc";
   };
 
   nativeBuildInputs = [ autoreconfHook pkgconfig ];
