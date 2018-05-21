56fad146a12a6f934d1d5ef875eb729be1b19129
Moritz Angermann <moritz.angermann@gmail.com>
2018-05-21 19:26:25 +0800

    ghc: fix dylib load command limits in compiler version 8.x
    See https://phabricator.haskell.org/D4714 for the full details.
    This will be part of ghc 8.6.
    
    Closes https://github.com/NixOS/nixpkgs/pull/40877.
    
    (cherry picked from commit 4b2eceaa3530e8cc1490d771d64492c4599e51f3)
    Signed-off-by: Domen Kožar <domen@dev.si>

--- pkgs/development/compilers/ghc/8.0.2.nix
+++ pkgs/development/compilers/ghc/8.0.2.nix
@@ -84,7 +84,8 @@ stdenv.mkDerivation rec {
       extraPrefix = "libraries/Cabal/";
     })
   ] ++ stdenv.lib.optional stdenv.isLinux ./ghc-no-madv-free.patch
-    ++ stdenv.lib.optional stdenv.isDarwin ./ghc-8.0.2-no-cpp-warnings.patch;
+    ++ stdenv.lib.optional stdenv.isDarwin ./ghc-8.0.2-no-cpp-warnings.patch
+    ++ stdenv.lib.optional stdenv.isDarwin ./backport-dylib-command-size-limit.patch;
 
   # GHC is a bit confused on its cross terminology.
   preConfigure = ''
