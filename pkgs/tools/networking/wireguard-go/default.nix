0f2de9334edb9e6235e51d7a06a1095ef83a1786
Jason A. Donenfeld <Jason@zx2c4.com>
2018-05-22 02:16:33 +0200

    wireguard: bump version and limit platforms

--- pkgs/tools/networking/wireguard-go/default.nix
+++ pkgs/tools/networking/wireguard-go/default.nix
@@ -2,13 +2,13 @@
 
 buildGoPackage rec {
   name = "wireguard-go-${version}";
-  version = "0.0.20180514";
+  version = "0.0.20180519";
 
   goPackagePath = "wireguard-go";
 
   src = fetchzip {
     url = "https://git.zx2c4.com/wireguard-go/snapshot/wireguard-go-${version}.tar.xz";
-    sha256 = "1i1w4vj8w353b92nfhs92k0f7fifrwi067qfmgckdk0kk76nv2id";
+    sha256 = "d2b0f43679b3559952cf8d244d537903d03699ed7c8a2c1e7fc37ee424e30439";
   };
 
   goDeps = ./deps.nix;
@@ -23,6 +23,6 @@ buildGoPackage rec {
     homepage = https://git.zx2c4.com/wireguard-go/about/;
     license = licenses.gpl2;
     maintainers = with maintainers; [ kirelagin ];
-    platforms = with platforms; linux ++ darwin ++ windows;
+    platforms = with platforms; darwin;
   };
 }
