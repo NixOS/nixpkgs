8d5767468b5a59b8ee3150a400e8c154b6706517
Herman Fries <baracoder@googlemail.com>
2018-05-23 01:45:22 +0200

    springlobby: 0.255 -> 0.264 with buildFHSUserEnv
    This allows to drop the dependency on spring, which is
    currently broken and to use spring versions downloaded by
    springlobby instead

--- pkgs/games/spring/springlobby.nix
+++ pkgs/games/spring/springlobby.nix
@@ -1,31 +1,8 @@
 { stdenv, fetchurl, cmake, wxGTK30, openal, pkgconfig, curl, libtorrentRasterbar
-, libpng, libX11, gettext, bash, gawk, boost, libnotify, gtk2, doxygen, spring
-, makeWrapper, glib, minizip, alure, pcre, jsoncpp }:
-
-stdenv.mkDerivation rec {
-  name = "springlobby-${version}";
-  version = "0.255";
-
-  src = fetchurl {
-    url = "http://www.springlobby.info/tarballs/springlobby-${version}.tar.bz2";
-    sha256 = "12iv6h1mz998lzxc2jwkza0m1yvaaq8h05k36i85xyp7g90197jw";
-  };
-
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [
-    cmake wxGTK30 openal curl gettext libtorrentRasterbar pcre jsoncpp
-    boost libpng libX11 libnotify gtk2 doxygen makeWrapper glib minizip alure
-  ];
-
-  patches = [ ./revert_58b423e.patch ]; # Allows springLobby to continue using system installed spring until #707 is fixed
-
-  enableParallelBuilding = true;
-
-  postInstall = ''
-    wrapProgram $out/bin/springlobby \
-      --prefix PATH : "${spring}/bin" \
-      --set SPRING_BUNDLE_DIR "${spring}/lib"
-  '';
+, libpng, libX11, gettext, bash, gawk, boost, libnotify, gtk2, doxygen
+, makeWrapper, glib, minizip, alure, pcre, jsoncpp, buildFHSUserEnv, SDL2, libGLU, curlFull }:
+let
+  version = "0.264";
 
   meta = with stdenv.lib; {
     homepage = http://springlobby.info/;
@@ -35,4 +12,35 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ phreedom qknight domenkozar ];
     platforms = platforms.linux;
   };
-}
+  springlobby = stdenv.mkDerivation rec {
+    name = "springlobby-${version}";
+    inherit version;
+    inherit meta;
+
+    src = fetchurl {
+      url = "http://www.springlobby.info/tarballs/springlobby-${version}.tar.bz2";
+      sha256 = "5d3c98169a4c9106dd6f112823cfa0e44846cedca3920050151472bfb75561c4";
+    };
+
+    nativeBuildInputs = [ pkgconfig ];
+    buildInputs = [
+      cmake wxGTK30 openal curl gettext libtorrentRasterbar pcre jsoncpp
+      boost libpng libX11 libnotify gtk2 doxygen makeWrapper glib minizip alure
+    ];
+
+    patches = [ ./revert_58b423e.patch ]; # Allows springLobby to continue using system installed spring until #707 is fixed
+
+    enableParallelBuilding = true;
+
+    postInstall = ''
+      wrapProgram $out/bin/springlobby
+    '';
+
+  };
+in
+  buildFHSUserEnv {
+    name = "springlobby";
+    targetPkgs = pkgs: [ springlobby SDL2 libGLU openal curlFull ];
+    runScript = "springlobby";
+    inherit meta;
+  }
