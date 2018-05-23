b656e03aae96dc135296a2336ea3bc056e212762
Alvar <geistesk@users.noreply.github.com>
2018-05-24 01:08:37 +0200

    pythonPackages.pynmea2: init at 1.12.0 (#40757)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11262,6 +11262,8 @@ in {
     inherit (pkgs) fetchurl systemd;
   };
 
+  pynmea2 = callPackage ../development/python-modules/pynmea2 {};
+
   pynzb = buildPythonPackage (rec {
     name = "pynzb-0.1.0";
 
