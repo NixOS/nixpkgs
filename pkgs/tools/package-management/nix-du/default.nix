a570aa476999aaf65128dc0f003f2eb34d023b63
Symphorien Gibol <symphorien+git@xlumurb.eu>
2018-05-23 22:46:22 +0200

    nix-du: 0.1.1 -> 0.1.2

--- pkgs/tools/package-management/nix-du/default.nix
+++ pkgs/tools/package-management/nix-du/default.nix
@@ -1,17 +1,17 @@
 { stdenv, fetchFromGitHub, rustPlatform, nix, boost, graphviz }:
 rustPlatform.buildRustPackage rec {
   name = "nix-du-${version}";
-  version = "0.1.1";
+  version = "0.1.2";
 
   src = fetchFromGitHub {
     owner = "symphorien";
     repo = "nix-du";
     rev = "v${version}";
-    sha256 = "0kxacn5qw21pp4zl6wr9wyb2mm2nlnp6mla3m5p9dm7vrm1fd1x9";
+    sha256 = "1y7ifr4c3v1494swh6akvna0d0rxjy9jw3mw2wdd6vj1xphvmimq";
   };
-  cargoSha256 = "04c48lzi7hny3nq4ffdpvsr4dxbi32faka163fp1yc9953zdw9az";
+  cargoSha256 = "0qq7a6ncxnbjvnmly99awqrk9f3z9b55ifil7b0bn5yhk4h9sa6y";
 
-  doCheck = !stdenv.isDarwin;
+  doCheck = true;
   checkInputs = [ graphviz ];
   nativeBuildInputs = [] ++ stdenv.lib.optionals doCheck checkInputs;
 
