04e5cf666511d6d84d20814557a04f2392fa4a83
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-05-24 10:38:06 +0300

    renoise: add platforms, derive src version from proper version string

--- pkgs/applications/audio/renoise/default.nix
+++ pkgs/applications/audio/renoise/default.nix
@@ -6,30 +6,34 @@
 # 2) Override the releasePath attribute to point to the location of the newly downloaded bundle.
 # Note: Renoise creates an individual build for each license which screws somewhat with the
 # use of functions like requireFile as the hash will be different for every user.
-let fileversion = "3_1_0";
+let
+  srcVersion = version: lib.replaceStrings ["."] ["_"] version;
 in
+
 stdenv.mkDerivation rec {
-  name = "renoise-${lib.replaceStrings ["_"] ["."] fileversion}";
-  buildInputs = [ libX11 libXext libXcursor libXrandr alsaLib libjack2 ];
+  name = "renoise-${version}";
+  version = "3.1.0";
 
   src =
     if stdenv.system == "x86_64-linux" then
         if builtins.isNull releasePath then
         fetchurl {
-            url = "http://files.renoise.com/demo/Renoise_${fileversion}_Demo_x86_64.tar.bz2";
-            sha256 = "0pan68fr22xbj7a930y29527vpry3f07q3i9ya4fp6g7aawffsga";
+           url = "http://files.renoise.com/demo/Renoise_${srcVersion version}_Demo_x86_64.tar.bz2";
+           sha256 = "0pan68fr22xbj7a930y29527vpry3f07q3i9ya4fp6g7aawffsga";
         }
         else
         releasePath
     else if stdenv.system == "i686-linux" then
         if builtins.isNull releasePath then
         fetchurl {
-            url = "http://files.renoise.com/demo/Renoise_${fileversion}_Demo_x86.tar.bz2";
-            sha256 = "1lccjj4k8hpqqxxham5v01v2rdwmx3c5kgy1p9lqvzqma88k4769";
+          url = "http://files.renoise.com/demo/Renoise_${srcVersion version}_Demo_x86.tar.bz2";
+          sha256 = "1lccjj4k8hpqqxxham5v01v2rdwmx3c5kgy1p9lqvzqma88k4769";
         }
         else
         releasePath
     else throw "Platform is not supported by Renoise";
+
+  buildInputs = [ libX11 libXext libXcursor libXrandr alsaLib libjack2 ];
 
   installPhase = ''
     cp -r Resources $out
@@ -52,9 +56,11 @@ stdenv.mkDerivation rec {
     patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) --set-rpath $out/lib $out/renoise
   '';
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Modern tracker-based DAW";
     homepage = http://www.renoise.com/;
-    license = stdenv.lib.licenses.unfree;
+    license = licenses.unfree;
+    maintainers = [];
+    platforms = [ "i686-linux" "x86_64-linux" ];
   };
 }
