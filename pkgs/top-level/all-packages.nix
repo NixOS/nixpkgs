8033f52a12980518a416dd5e240cac980d0ddeb9
Gabriel Ebner <gebner@gebner.org>
2018-05-25 10:27:02 +0200

    fstar: 0.9.5.0 -> 0.9.6.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6469,7 +6469,7 @@ with pkgs;
   };
 
   fstar = callPackage ../development/compilers/fstar {
-    ocamlPackages = ocaml-ng.ocamlPackages_4_02;
+    ocamlPackages = ocaml-ng.ocamlPackages_4_06;
   };
 
   dotnetPackages = recurseIntoAttrs (callPackage ./dotnet-packages.nix {});
