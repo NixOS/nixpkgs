30c3487e0451defff5b438b425b16e33d46d98e3
Peter Hoeg <peter@hoeg.com>
2018-05-26 11:11:48 +0800

    ids: assing openvpn

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -315,6 +315,7 @@
       duplicati = 289;
       monetdb = 290;
       restic = 291;
+      openvpn = 292;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -590,6 +591,7 @@
       duplicati = 289;
       monetdb = 290;
       restic = 291;
+      openvpn = 292;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
