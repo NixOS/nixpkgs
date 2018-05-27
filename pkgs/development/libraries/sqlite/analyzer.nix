232bc96376842da70425cb166363856e8823ce48
Will Dietz <w@wdtz.org>
2018-05-27 00:43:26 -0500

    Revert "sqlite{,-analyzer}: use the same src (#40945)"
    This reverts commit e28a586f94e77f22d943288ad84f88be23c587f8.

--- pkgs/development/libraries/sqlite/analyzer.nix
+++ pkgs/development/libraries/sqlite/analyzer.nix
@@ -1,11 +1,23 @@
-{ stdenv, tcl, sqlite }:
+{ stdenv, fetchurl, unzip, sqlite, tcl }:
+
+let
+  archiveVersion = import ./archive-version.nix stdenv.lib;
+in
 
 stdenv.mkDerivation rec {
   name = "sqlite-analyzer-${version}";
-  inherit (sqlite) src version;
+  version = "3.23.1";
 
-  nativeBuildInputs = [ tcl ];
+  src = assert version == sqlite.version; fetchurl {
+    url = "https://sqlite.org/2018/sqlite-src-${archiveVersion version}.zip";
+    sha256 = "1z3xr8d8ds4l8ndkg34cii13d0w790nlxdkrw6virinqi7wmmd1d";
+  };
+
+  nativeBuildInputs = [ unzip ];
+  buildInputs = [ tcl ];
+
   makeFlags = [ "sqlite3_analyzer" ];
+
   installPhase = "install -Dt $out/bin sqlite3_analyzer";
 
   meta = with stdenv.lib; {
