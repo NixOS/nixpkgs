11f418cee130b31d6239606a3d4733a595bf48e0
Frederik Rietdijk <fridh@fridh.nl>
2018-05-28 14:18:50 +0200

    python: phonenumbers: 8.9.5 -> 8.9.6

--- pkgs/development/python-modules/phonenumbers/default.nix
+++ pkgs/development/python-modules/phonenumbers/default.nix
@@ -2,12 +2,12 @@
 
 buildPythonPackage rec {
   pname = "phonenumbers";
-  version = "8.9.5";
+  version = "8.9.6";
   name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "09swdf3f9sdvqyyzkap93012m9fnhamizf42d5kzpdjwikvsfx8x";
+    sha256 = "eec334a7908746b675a0ba296b3a11322ed970614b62f2a6e5f086de4f3e2a84";
   };
 
   meta = {
