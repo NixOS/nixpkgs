518910707f6a0200e801d2c500076d6f4d158d76
Frederik Rietdijk <fridh@fridh.nl>
2018-05-28 14:18:23 +0200

    python: marshmallow: 2.15.1 -> 2.15.3

--- pkgs/development/python-modules/marshmallow/default.nix
+++ pkgs/development/python-modules/marshmallow/default.nix
@@ -5,7 +5,7 @@
 buildPythonPackage rec {
   pname = "marshmallow";
   name = "${pname}-${version}";
-  version = "2.15.1";
+  version = "2.15.3";
 
   meta = {
     homepage = "https://github.com/marshmallow-code/marshmallow";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b73361eab812af97eaf8e8691333a1096787968450051d132c8b9fb90aa1db5a";
+    sha256 = "171f409d48b44786b7df2793cbd7f1a9062f0fe2c14d547da536b5010f671ade";
   };
 
   propagatedBuildInputs = [ dateutil simplejson ];
