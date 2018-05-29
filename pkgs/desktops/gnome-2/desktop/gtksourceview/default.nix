86270fc19e8dce35bbb81507f0b8c9647a82abf2
Jan Tojnar <jtojnar@gmail.com>
2018-05-29 10:11:02 +0200

    gnome2.gtksourceview: fix patch url

--- pkgs/desktops/gnome-2/desktop/gtksourceview/default.nix
+++ pkgs/desktops/gnome-2/desktop/gtksourceview/default.nix
@@ -16,12 +16,12 @@ stdenv.mkDerivation rec {
   patches = optionals stdenv.isDarwin [
     (fetchpatch {
       name = "change-igemacintegration-to-gtkosxapplication.patch";
-      url = "https://git.gnome.org/browse/gtksourceview/patch/?id=e88357c5f210a8796104505c090fb6a04c213902";
+      url = "https://gitlab.gnome.org/GNOME/gtksourceview/commit/e88357c5f210a8796104505c090fb6a04c213902.patch";
       sha256 = "0h5q79q9dqbg46zcyay71xn1pm4aji925gjd5j93v4wqn41wj5m7";
     })
     (fetchpatch {
       name = "update-to-gtk-mac-integration-2.0-api.patch";
-      url = "https://git.gnome.org/browse/gtksourceview/patch/?id=ab46e552e1d0dae73f72adac8d578e40bdadaf95";
+      url = "https://gitlab.gnome.org/GNOME/gtksourceview/commit/ab46e552e1d0dae73f72adac8d578e40bdadaf95.patch";
       sha256 = "0qzrbv4hpa0v8qbmpi2vp575n13lkrvp3cgllwrd2pslw1v9q3aj";
     })
   ];
