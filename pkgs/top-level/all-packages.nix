e2126dfd71a536c0db7c890e94c574afa06f01e7
Shea Levy <shea@shealevy.com>
2018-05-30 16:23:37 -0400

    nix-plugins: 4.0.4 -> 4.0.5.
    Allows building against nix stable.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10850,9 +10850,7 @@ with pkgs;
   };
   libnghttp2 = nghttp2.lib;
 
-  nix-plugins = callPackage ../development/libraries/nix-plugins {
-    nix = nixUnstable;
-  };
+  nix-plugins = callPackage ../development/libraries/nix-plugins {};
 
   nlohmann_json = callPackage ../development/libraries/nlohmann_json { };
 
