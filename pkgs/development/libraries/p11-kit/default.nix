6d2ffac0cb32cd4572910f1054d0cd7d10d9122c
R. RyanTM <ryantm+bot@ryantm.com>
2018-05-30 11:36:19 -0700

    p11-kit: 0.23.10 -> 0.23.11
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/p11-kit/versions.
    
    These checks were done:
    
    - built on NixOS
    
    - 0 of 0 passed binary check by having a zero exit code.
    - 0 of 0 passed binary check by having the new version present in output.
    - found 0.23.11 with grep in /nix/store/9whclrmrld36nawkkdshhj636jlh1cdm-p11-kit-0.23.11
    - directory tree listing: https://gist.github.com/be74c2d2f6d0b1ee2fb5e2b0e30fb945
    - du listing: https://gist.github.com/470d2f00cd777773ea45ea6ea280a07a

--- pkgs/development/libraries/p11-kit/default.nix
+++ pkgs/development/libraries/p11-kit/default.nix
@@ -3,13 +3,13 @@
 
 stdenv.mkDerivation rec {
   name = "p11-kit-${version}";
-  version = "0.23.10";
+  version = "0.23.11";
 
   src = fetchFromGitHub {
     owner = "p11-glue";
     repo = "p11-kit";
     rev = version;
-    sha256 = "0n0wqv028flzvnxllqv8i6x9nv705csl7ddzi0fzvppc9fp2yinp";
+    sha256 = "08jfxml3w8gn85qz19ps1z8yb66k921z0cgzizqn1ipl9pp5pxyd";
   };
 
   outputs = [ "out" "dev"];
