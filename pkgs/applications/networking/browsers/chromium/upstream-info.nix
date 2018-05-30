a3efa24da976a993848059fa9adf5e28fccb0d65
Yuriy Taraday <yorik.sar@gmail.com>
2018-05-31 02:50:27 +0400

    chromium: bump dev version

--- pkgs/applications/networking/browsers/chromium/upstream-info.nix
+++ pkgs/applications/networking/browsers/chromium/upstream-info.nix
@@ -7,8 +7,8 @@
   };
   dev = {
     sha256 = "0hy36r938k6c89alam4a8yy2wynnlcc0zqhhb0jgy3qlnqrksvrc";
-    sha256bin64 = "0y69ykzj1phpvmhd380wijzs0f8s15gmvb88zd4m8fzqi6gg51dj";
-    version = "68.0.3438.3";
+    sha256bin64 = "0liqgz8sip5fz5mxb54zagz3p8s24j96p26i5dql935bic7p3lhw";
+    version = "68.0.3440.7";
   };
   stable = {
     sha256 = "03bpznpnr22lphixvvc1cr0pn4p7fxjlz0wwysjw9j7i5y9n7vnm";
