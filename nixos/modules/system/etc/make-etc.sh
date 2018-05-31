902a8949052378373834842da98c9bcbabd738d2
Thomas Kerber <t.kerber@ed.ac.uk>
2018-05-31 11:46:07 +0100

    Revert "nixos/etc: remove unreachable globbing logic."
    This reverts commit 910bab8add9851ac93ae37421cc1be70b737f27a.

--- nixos/modules/system/etc/make-etc.sh
+++ nixos/modules/system/etc/make-etc.sh
@@ -14,21 +14,33 @@ for ((i = 0; i < ${#targets_[@]}; i++)); do
     source="${sources_[$i]}"
     target="${targets_[$i]}"
 
-    mkdir -p "$out/etc/$(dirname "$target")"
-    if ! [ -e "$out/etc/$target" ]; then
-        ln -s "$source" "$out/etc/$target"
-    else
-        echo "duplicate entry $target -> $source"
-        if test "$(readlink "$out/etc/$target")" != "$source"; then
-            echo "mismatched duplicate entry $(readlink "$out/etc/$target") <-> $source"
-            exit 1
-        fi
-    fi
+    if [[ "$source" =~ '*' ]]; then
 
-    if test "${modes_[$i]}" != symlink; then
-        echo "${modes_[$i]}"  > "$out/etc/$target".mode
-        echo "${users_[$i]}"  > "$out/etc/$target".uid
-        echo "${groups_[$i]}" > "$out/etc/$target".gid
+        # If the source name contains '*', perform globbing.
+        mkdir -p $out/etc/$target
+        for fn in $source; do
+            ln -s "$fn" $out/etc/$target/
+        done
+
+    else
+
+        mkdir -p "$out/etc/$(dirname "$target")"
+        if ! [ -e "$out/etc/$target" ]; then
+            ln -s "$source" "$out/etc/$target"
+        else
+            echo "duplicate entry $target -> $source"
+            if test "$(readlink "$out/etc/$target")" != "$source"; then
+                echo "mismatched duplicate entry $(readlink "$out/etc/$target") <-> $source"
+                exit 1
+            fi
+        fi
+
+        if test "${modes_[$i]}" != symlink; then
+            echo "${modes_[$i]}"  > "$out/etc/$target".mode
+            echo "${users_[$i]}"  > "$out/etc/$target".uid
+            echo "${groups_[$i]}" > "$out/etc/$target".gid
+        fi
+
     fi
 done
 
