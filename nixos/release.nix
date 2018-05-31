86c4a640b4c952388831a516fbebc060b6e7868b
Jan Tojnar <jtojnar@gmail.com>
2018-06-01 00:17:38 +0200

    Revert "nixos: disable gdk-pixbuf test"
    This reverts commit 218ada1ba7ecb0a3b1d9e7aed9147ba35d484faa.

--- nixos/release.nix
+++ nixos/release.nix
@@ -289,7 +289,7 @@ in rec {
   tests.flatpak = callTest tests/flatpak.nix {};
   tests.firewall = callTest tests/firewall.nix {};
   tests.fwupd = callTest tests/fwupd.nix {};
-  #tests.gdk-pixbuf = callTest tests/gdk-pixbuf.nix {};
+  tests.gdk-pixbuf = callTest tests/gdk-pixbuf.nix {};
   #tests.gitlab = callTest tests/gitlab.nix {};
   tests.gitolite = callTest tests/gitolite.nix {};
   tests.gjs = callTest tests/gjs.nix {};
