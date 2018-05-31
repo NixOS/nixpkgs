910bab8add9851ac93ae37421cc1be70b737f27a
Thomas Kerber <t.kerber@ed.ac.uk>
2018-05-31 11:33:22 +0100

    nixos/etc: remove unreachable globbing logic.

--- nixos/modules/system/etc/make-etc.sh
+++ nixos/modules/system/etc/make-etc.sh
@@ -14,33 +14,21 @@ for ((i = 0; i < ${#targets_[@]}; i++)); do
     source="${sources_[$i]}"
     target="${targets_[$i]}"
 
-    if [[ "$source" =~ '*' ]]; then
-
-        # If the source name contains '*', perform globbing.
-        mkdir -p $out/etc/$target
-        for fn in $source; do
-            ln -s "$fn" $out/etc/$target/
-        done
-
+    mkdir -p "$out/etc/$(dirname "$target")"
+    if ! [ -e "$out/etc/$target" ]; then
+        ln -s "$source" "$out/etc/$target"
     else
-
-        mkdir -p "$out/etc/$(dirname "$target")"
-        if ! [ -e "$out/etc/$target" ]; then
-            ln -s "$source" "$out/etc/$target"
-        else
-            echo "duplicate entry $target -> $source"
-            if test "$(readlink "$out/etc/$target")" != "$source"; then
-                echo "mismatched duplicate entry $(readlink "$out/etc/$target") <-> $source"
-                exit 1
-            fi
+        echo "duplicate entry $target -> $source"
+        if test "$(readlink "$out/etc/$target")" != "$source"; then
+            echo "mismatched duplicate entry $(readlink "$out/etc/$target") <-> $source"
+            exit 1
         fi
+    fi
 
-        if test "${modes_[$i]}" != symlink; then
-            echo "${modes_[$i]}"  > "$out/etc/$target".mode
-            echo "${users_[$i]}"  > "$out/etc/$target".uid
-            echo "${groups_[$i]}" > "$out/etc/$target".gid
-        fi
-
+    if test "${modes_[$i]}" != symlink; then
+        echo "${modes_[$i]}"  > "$out/etc/$target".mode
+        echo "${users_[$i]}"  > "$out/etc/$target".uid
+        echo "${groups_[$i]}" > "$out/etc/$target".gid
     fi
 done
 
