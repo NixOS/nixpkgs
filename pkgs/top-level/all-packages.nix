8603f343a326b954f7a61ec8f07322f0f8712cf6
Sebastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2018-05-31 14:00:33 +0200

    gildas: init at 201806_a

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20686,6 +20686,8 @@ with pkgs;
 
   fityk = callPackage ../applications/science/misc/fityk { };
 
+  gildas = callPackage ../applications/science/astronomy/gildas { };
+
   gplates = callPackage ../applications/science/misc/gplates {
     boost = boost160;
     cgal = cgal.override { boost = boost160; };
