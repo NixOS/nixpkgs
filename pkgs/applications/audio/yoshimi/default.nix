e37e19ff6182258a3b9f0ab68604797efd66deff
R. RyanTM <ryantm-bot@ryantm.com>
2018-05-31 01:13:32 -0700

    yoshimi: 1.5.7 -> 1.5.8 (#41221)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/yoshimi/versions.
    
    These checks were done:
    
    - built on NixOS
    - /nix/store/jpvzkcfk0dablksr19lnwi7xajm7d7bh-yoshimi-1.5.8/bin/yoshimi passed the binary check.
    - 1 of 1 passed binary check by having a zero exit code.
    - 0 of 1 passed binary check by having the new version present in output.
    - found 1.5.8 with grep in /nix/store/jpvzkcfk0dablksr19lnwi7xajm7d7bh-yoshimi-1.5.8
    - directory tree listing: https://gist.github.com/53f8899fb2961c61707a03b4bcb225d1
    - du listing: https://gist.github.com/3b61f944f2b81b3f5bc299051a284cec

--- pkgs/applications/audio/yoshimi/default.nix
+++ pkgs/applications/audio/yoshimi/default.nix
@@ -6,11 +6,11 @@ assert stdenv ? glibc;
 
 stdenv.mkDerivation  rec {
   name = "yoshimi-${version}";
-  version = "1.5.7";
+  version = "1.5.8";
 
   src = fetchurl {
     url = "mirror://sourceforge/yoshimi/${name}.tar.bz2";
-    sha256 = "1w916mmi6hh547a7icrgx6qr2kwxlxwlm6ampql427rshcz9r61k";
+    sha256 = "0gwsr5srzy28hwqhfzrc8pswysmyra8kbww3bxfx8bq4mdjifdj6";
   };
 
   buildInputs = [
