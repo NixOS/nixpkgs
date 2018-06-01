397267a078d3b19aa74f2a20794643ddf20df76d
Jörg Thalheim <joerg@thalheim.io>
2018-06-01 10:42:42 +0100

    dep2nix: init at 0.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13869,6 +13869,8 @@ with pkgs;
 
   dep = callPackage ../development/tools/dep { };
 
+  dep2nix = callPackage ../development/tools/dep2nix { };
+
   easyjson = callPackage ../development/tools/easyjson { };
 
   go-bindata = callPackage ../development/tools/go-bindata { };
