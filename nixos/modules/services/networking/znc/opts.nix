db2ad67ab61a4f7721896fd8cfc8ab53e99c6adf
Silvan Mosberger <infinisil@icloud.com>
2018-06-02 16:32:57 +0200

    Move around confOptions

--- nixos/modules/services/networking/znc/opts.nix
+++ nixos/modules/services/networking/znc/opts.nix
@@ -6,6 +6,109 @@ let
 
   cfg = config.services.znc;
 
+
+  conf.options = {
+    modules = mkOption {
+      type = types.listOf types.str;
+      default = [ "webadmin" "adminlog" ];
+      example = [ "partyline" "webadmin" "adminlog" "log" ];
+      description = ''
+        A list of modules to include in the `znc.conf` file.
+      '';
+    };
+
+    userModules = mkOption {
+      type = types.listOf types.str;
+      default = [ "chansaver" "controlpanel" ];
+      example = [ "chansaver" "controlpanel" "fish" "push" ];
+      description = ''
+        A list of user modules to include in the `znc.conf` file.
+      '';
+    };
+
+    userName = mkOption {
+      default = "znc";
+      example = "johntron";
+      type = types.string;
+      description = ''
+        The user name used to log in to the ZNC web admin interface.
+      '';
+    };
+
+    networks = mkOption {
+      default = { };
+      type = with types; attrsOf (submodule networkOpts);
+      description = ''
+        IRC networks to connect the user to.
+      '';
+      example = {
+        "freenode" = {
+          server = "chat.freenode.net";
+          port = 6697;
+          useSSL = true;
+          modules = [ "simple_away" ];
+        };
+      };
+    };
+
+    nick = mkOption {
+      default = "znc-user";
+      example = "john";
+      type = types.string;
+      description = ''
+        The IRC nick.
+      '';
+    };
+
+    passBlock = mkOption {
+      example = literalExample ''
+        # password "nixospass"
+        Pass.password = {
+          Method = "sha256";
+          Hash = "e2ce303c7ea75c571d80d8540a8699b46535be6a085be3414947d638e48d9e93";
+          Salt = "l5Xryew4g*!oa(ECfX2o";
+        };
+
+        # Or as returned by `znc --makepass` also works:
+        <Pass password>
+          Method = sha256
+          Hash = e2ce303c7ea75c571d80d8540a8699b46535be6a085be3414947d638e48d9e93
+          Salt = l5Xryew4g*!oa(ECfX2o
+        </Pass>
+      '';
+      type = types.string;
+      description = ''
+        Generate with `nix-shell -p znc --command "znc --makepass"`.
+        This is the password used to log in to the ZNC web admin interface.
+      '';
+    };
+
+    port = mkOption {
+      default = 5000;
+      example = 5000;
+      type = types.int;
+      description = ''
+        Specifies the port on which to listen.
+      '';
+    };
+
+    useSSL = mkOption {
+      default = true;
+      type = types.bool;
+      description = ''
+        Indicates whether the ZNC server should use SSL when listening on the specified port. A self-signed certificate will be generated.
+      '';
+    };
+
+    extraZncConf = mkOption {
+      default = "";
+      type = types.lines;
+      description = ''
+        Extra config to `znc.conf` file.
+      '';
+    };
+  };
+
   networkOpts.options = {
     server = mkOption {
       type = types.str;
@@ -118,106 +221,11 @@ in
       '';
     };
 
-    confOptions = {
-      modules = mkOption {
-        type = types.listOf types.str;
-        default = [ "webadmin" "adminlog" ];
-        example = [ "partyline" "webadmin" "adminlog" "log" ];
-        description = ''
-          A list of modules to include in the `znc.conf` file.
-        '';
-      };
-
-      userModules = mkOption {
-        type = types.listOf types.str;
-        default = [ "chansaver" "controlpanel" ];
-        example = [ "chansaver" "controlpanel" "fish" "push" ];
-        description = ''
-          A list of user modules to include in the `znc.conf` file.
-        '';
-      };
-
-      userName = mkOption {
-        default = "znc";
-        example = "johntron";
-        type = types.string;
-        description = ''
-          The user name used to log in to the ZNC web admin interface.
-        '';
-      };
-
-      networks = mkOption {
-        default = { };
-        type = with types; attrsOf (submodule networkOpts);
-        description = ''
-          IRC networks to connect the user to.
-        '';
-        example = {
-          "freenode" = {
-            server = "chat.freenode.net";
-            port = 6697;
-            useSSL = true;
-            modules = [ "simple_away" ];
-          };
-        };
-      };
-
-      nick = mkOption {
-        default = "znc-user";
-        example = "john";
-        type = types.string;
-        description = ''
-          The IRC nick.
-        '';
-      };
-
-      passBlock = mkOption {
-        example = literalExample ''
-          # password "nixospass"
-          Pass.password = {
-            Method = "sha256";
-            Hash = "e2ce303c7ea75c571d80d8540a8699b46535be6a085be3414947d638e48d9e93";
-            Salt = "l5Xryew4g*!oa(ECfX2o";
-          };
-
-          # Or as returned by `znc --makepass` also works:
-          <Pass password>
-            Method = sha256
-            Hash = e2ce303c7ea75c571d80d8540a8699b46535be6a085be3414947d638e48d9e93
-            Salt = l5Xryew4g*!oa(ECfX2o
-          </Pass>
-        '';
-        type = types.string;
-        description = ''
-          Generate with `nix-shell -p znc --command "znc --makepass"`.
-          This is the password used to log in to the ZNC web admin interface.
-        '';
-      };
-
-      port = mkOption {
-        default = 5000;
-        example = 5000;
-        type = types.int;
-        description = ''
-          Specifies the port on which to listen.
-        '';
-      };
-
-      useSSL = mkOption {
-        default = true;
-        type = types.bool;
-        description = ''
-          Indicates whether the ZNC server should use SSL when listening on the specified port. A self-signed certificate will be generated.
-        '';
-      };
-
-      extraZncConf = mkOption {
-        default = "";
-        type = types.lines;
-        description = ''
-          Extra config to `znc.conf` file.
-        '';
-      };
+    confOptions = mkOption {
+      type = types.submodule conf;
+      description = ''
+        Legacy options
+      '';
     };
   };
 
