dc5b6b8c46c14ea1bbca6ddaea40aad075bcdce5
volth <volth@volth.com>
2018-06-02 20:33:28 +0000

    [cpan2nix] perlPackages.CPANPerlReleases: 3.50 -> 3.58

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -2887,10 +2887,10 @@ let self = _self // overrides; _self = with self; {
   };
 
   CPANPerlReleases = buildPerlPackage rec {
-    name = "CPAN-Perl-Releases-3.50";
+    name = "CPAN-Perl-Releases-3.58";
     src = fetchurl {
       url = "mirror://cpan/authors/id/B/BI/BINGOS/${name}.tar.gz";
-      sha256 = "1nr739rnrw58p9r9z26pr4sc4s5z7wbhgyva3r2b2spyaylp5qkz";
+      sha256 = "1v56gj1032p5hbmlvg0nakrah7rhwin17ak06fpnyjn5lacyy753";
     };
     meta = {
       homepage = https://github.com/bingos/cpan-perl-releases;
