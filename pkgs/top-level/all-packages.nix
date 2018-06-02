4d88c022d0d8ca8b856733aff9e3b131202dd4ac
Justin Humm <justin.humm@posteo.de>
2018-06-02 10:06:28 +0200

    mapmap: init at 0.6.1 (#41228)
    The Nix package currently doesn't support video textures, just still images.
    When I figure out why video textures don't work I will provide a fix.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16787,6 +16787,8 @@ with pkgs;
 
   makeself = callPackage ../applications/misc/makeself { };
 
+  mapmap = libsForQt5.callPackage ../applications/video/mapmap { };
+
   marathon = callPackage ../applications/networking/cluster/marathon { };
   marathonctl = callPackage ../tools/virtualization/marathonctl { } ;
 
