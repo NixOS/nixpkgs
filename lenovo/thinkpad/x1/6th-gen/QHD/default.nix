32a7893b8c3068a32f42eab45f08faebd3a16359
Alberto Berti <alberto@metapensiero.it>
2018-06-03 01:55:59 +0200

    Set a default to fix GTK/GNOME ui elements appareance

--- lenovo/thinkpad/x1/6th-gen/QHD/default.nix
+++ lenovo/thinkpad/x1/6th-gen/QHD/default.nix
@@ -1,11 +1,18 @@
 # X1 6th generation with a QHD (2560x1440px) display
-{ config,  ... }:
+{ config, lib, ... }:
 
 {
   imports = [
     ../.
   ];
+
   # Fix font sizes in X
   services.xserver.dpi = 210;
   fonts.fontconfig.dpi = 210;
+
+  # Fix sizes of GTK/GNOME ui elements
+  environment.variables = {
+    GDK_SCALE = lib.mkDefault "2";
+    GDK_DPI_SCALE= lib.mkDefault "0.5";
+  };
 }
