ed2497f3a703c0e85fb5561183b13df59b277af8
Anton-Latukha <anton.latukha@gmail.com>
2018-06-03 01:08:29 +0300

    at-spi2-atk: 2.26.1 -> 2.26.2, clean-up

--- pkgs/development/libraries/at-spi2-atk/default.nix
+++ pkgs/development/libraries/at-spi2-atk/default.nix
@@ -1,25 +1,24 @@
-{ stdenv, fetchurl, pkgconfig, meson, ninja, libxml2
-, python, popt, atk, libX11, libICE, xorg, libXi
-, intltool, dbus-glib, at-spi2-core, libSM }:
+{ stdenv, fetchurl, meson, ninja, pkgconfig
+, at-spi2-core, atk, dbus, glib, libxml2 }:
 
 stdenv.mkDerivation rec {
-  versionMajor = "2.26";
-  versionMinor = "1";
-  moduleName   = "at-spi2-atk";
   name = "${moduleName}-${versionMajor}.${versionMinor}";
+  moduleName   = "at-spi2-atk";
+  versionMajor = "2.26";
+  versionMinor = "2";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${moduleName}/${versionMajor}/${name}.tar.xz";
-    sha256 = "0x9vc99ni46fg5dzlx67vbw0zqffr24gz8jvbdxbmzyvc5xw5w5l";
+    sha256 = "0vkan52ab9vrkknnv8y4f1cspk8x7xd10qx92xk9ys71p851z2b1";
   };
 
-  nativeBuildInputs = [ meson ninja pkgconfig intltool ];
-  buildInputs = [ python popt atk libX11 libICE xorg.libXtst libXi
-                  dbus-glib at-spi2-core libSM libxml2 ];
-
-  doCheck = false; # needs dbus daemon
+  nativeBuildInputs = [ meson ninja pkgconfig ];
+  buildInputs = [ at-spi2-core atk dbus glib libxml2 ];
 
   meta = with stdenv.lib; {
+    description = "D-Bus bridge for Assistive Technology Service Provider Interface (AT-SPI) and Accessibility Toolkit (ATK)";
+    homepage = "https://gitlab.gnome.org/GNOME/at-spi2-atk";
     platforms = platforms.unix;
+
   };
 }
