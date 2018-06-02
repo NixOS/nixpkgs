5dd535392df4ed2299ed39c4500bbd432e62cff0
Alberto Berti <alberto@metapensiero.it>
2018-06-03 01:28:40 +0200

    Move battery settings to the configuration for all the 6th gen models

--- lenovo/thinkpad/x1/6th-gen/QHD/default.nix
+++ lenovo/thinkpad/x1/6th-gen/QHD/default.nix
@@ -5,21 +5,6 @@
   imports = [
     ../.
   ];
-  # give tlp more control over battery
-  boot = {
-    extraModulePackages = with config.boot.kernelPackages; [
-      acpi_call
-    ];
-    kernelModules = [
-      "acpi_call"
-    ];
-  };
-
-  # see https://linrunner.de/en/tlp/docs/tlp-faq.html#battery
-  services.tlp.extraConfig = ''
-START_CHARGE_THRESH_BAT0=75
-STOP_CHARGE_THRESH_BAT0=80
-'';
   # fix font sizes in X
   services.xserver.dpi = 210;
   fonts.fontconfig.dpi = 210;
--- lenovo/thinkpad/x1/6th-gen/default.nix
+++ lenovo/thinkpad/x1/6th-gen/default.nix
@@ -2,4 +2,19 @@
   imports = [
     ../.
   ];
+  # Give TLP service more control over battery
+  boot = {
+    extraModulePackages = with config.boot.kernelPackages; [
+      acpi_call
+    ];
+    kernelModules = [
+      "acpi_call"
+    ];
+  };
+
+  # See https://linrunner.de/en/tlp/docs/tlp-faq.html#battery
+  services.tlp.extraConfig = ''
+START_CHARGE_THRESH_BAT0=75
+STOP_CHARGE_THRESH_BAT0=80
+'';
 }
