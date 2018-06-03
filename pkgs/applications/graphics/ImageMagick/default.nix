33a51d78a2c1e6c32f80e0bc321613e9bc78a17f
Ruud van Asseldonk <dev@veniogames.com>
2018-06-03 12:00:27 +0200

    imagemagick: depend on fftw for fft feature

--- pkgs/applications/graphics/ImageMagick/default.nix
+++ pkgs/applications/graphics/ImageMagick/default.nix
@@ -1,6 +1,6 @@
 { lib, stdenv, fetchFromGitHub, fetchpatch, pkgconfig, libtool
 , bzip2, zlib, libX11, libXext, libXt, fontconfig, freetype, ghostscript, libjpeg
-, lcms2, openexr, libpng, librsvg, libtiff, libxml2, openjpeg, libwebp
+, lcms2, openexr, libpng, librsvg, libtiff, libxml2, openjpeg, libwebp, fftw
 , ApplicationServices
 , buildPlatform, hostPlatform
 }:
@@ -73,7 +73,7 @@ stdenv.mkDerivation rec {
     ++ lib.optional stdenv.isDarwin ApplicationServices;
 
   propagatedBuildInputs =
-    [ bzip2 freetype libjpeg lcms2 ]
+    [ bzip2 freetype libjpeg lcms2 fftw ]
     ++ lib.optionals (!hostPlatform.isMinGW)
       [ libX11 libXext libXt libwebp ]
     ;
