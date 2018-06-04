fe727a9b957cbdfe1c381b9ac76ee2b1be6f8efd
Tim Steinbach <tim@nequissimus.com>
2018-06-04 14:12:03 -0400

    linux: Add 4.17
    (cherry picked from commit 39444569e0c6e96bceea74840762202e0e968f32)
    
    all-packages.nix differs in that only 4_17 has been added.

---
+++ pkgs/os-specific/linux/kernel/linux-4.17.nix
@@ -0,0 +1,18 @@
+{ stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, modDirVersionArg ? null, ... } @ args:
+
+with stdenv.lib;
+
+buildLinux (args // rec {
+  version = "4.17";
+
+  # modDirVersion needs to be x.y.z, will automatically add .0 if needed
+  modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
+
+  # branchVersion needs to be x.y
+  extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
+
+  src = fetchurl {
+    url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
+    sha256 = "04yd7hnsdyaq4xmrgg7509qjf09k1dy6k1p8qqfrdspajvc1valz";
+  };
+} // (args.argsOverride or {}))
