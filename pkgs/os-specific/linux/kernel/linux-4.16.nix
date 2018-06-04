c58d0fc3677d455024cd21916f4570c5e2d23727
Tim Steinbach <tim@nequissimus.com>
2018-06-04 10:22:09 -0400

    linux-copperhead: 4.16.12.a -> 4.16.13.a
    (cherry picked from commit b03de18acffdda51cb3a34698d083b3d5ed751f5)

--- pkgs/os-specific/linux/kernel/linux-4.16.nix
+++ pkgs/os-specific/linux/kernel/linux-4.16.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
+{ stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, modDirVersionArg ? null, ... } @ args:
 
 with stdenv.lib;
 
@@ -6,7 +6,7 @@ buildLinux (args // rec {
   version = "4.16.13";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
-  modDirVersion = concatStrings (intersperse "." (take 3 (splitString "." "${version}.0")));
+  modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
 
   # branchVersion needs to be x.y
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
