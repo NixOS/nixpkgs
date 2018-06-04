7cf5a17ee91242d8a369dd6d6ca25c5fa1539aea
Fernando J Pando <fernando.pando@stelligent.com>
2018-06-04 12:52:38 -0400

    opendune: 0.9 version and comments
    - tested on nixos

--- pkgs/games/opendune/default.nix
+++ pkgs/games/opendune/default.nix
@@ -1,5 +1,10 @@
 { stdenv, fetchFromGitHub, SDL, SDL_image, SDL_mixer, binutils }:
 
+# - set the opendune configuration at ~/.config/opendune/opendune.ini:
+#     [opendune]
+#     datadir=/path/to/opendune-data
+# - download dune2 into [datadir] http://www.bestoldgames.net/eng/old-games/dune-2.php
+
 stdenv.mkDerivation rec {
   name = "opendune-${version}";
   version = "0.9";
@@ -7,7 +12,7 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "OpenDUNE";
     repo = "OpenDUNE";
-    rev = "af3f3239eca1073ac9d621286014fba282c1dc5a";
+    rev = version;
     sha256 = "15rvrnszdy3db8s0dmb696l4isb3x2cpj7wcl4j09pdi59pc8p37";
   };
 
@@ -15,10 +20,6 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     install -m 555 -D bin/opendune $out/bin/opendune
-    echo 'set the opendune configuration at ~/.config/opendune/opendune.ini:'
-    echo '[opendune]'
-    echo 'datadir=/path/to/opendune-data'
-    echo 'download dune2 into [datadir] http://www.bestoldgames.net/eng/old-games/dune-2.php'
   '';
 
   meta = with stdenv.lib; {
