dcfaef2b8ee59a7c329ae1d0a8cf8fe2a9556ccc
Michael Weiss <dev.primeos@gmail.com>
2018-06-05 17:16:53 +0200

    tdesktopPackages.preview: 1.3.0 -> 1.3.1

--- pkgs/applications/networking/instant-messengers/telegram/tdesktop/default.nix
+++ pkgs/applications/networking/instant-messengers/telegram/tdesktop/default.nix
@@ -14,7 +14,7 @@ in {
   stable = mkTelegram stableVersion;
   preview = mkTelegram (stableVersion // {
     stable = false;
-    version = "1.3.0";
-    sha256Hash = "1h5zcvd58bjm02b0rfb7fx1nx1gmzdlk1854lm6kg1hd6mqrrb0i";
+    version = "1.3.1";
+    sha256Hash = "084zrdx3w1yq53r53zjbqpz7s8fc3ga26ffwyfxj07ysmyn7ck25";
   });
 }
