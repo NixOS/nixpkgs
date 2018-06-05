706cc4ac68af7a04ec2881f4127b7663f2789755
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-06-05 22:05:11 +0300

    ctmg: review

--- pkgs/tools/security/ctmg/default.nix
+++ pkgs/tools/security/ctmg/default.nix
@@ -1,25 +1,21 @@
-{ stdenv, fetchurl, ...  }:
+{ stdenv, fetchzip }:
 
 stdenv.mkDerivation rec {
-  name    = "ctmg-${version}";
+  name = "ctmg-${version}";
   version = "1.2";
 
-  src = fetchurl {
-    url    = "https://git.zx2c4.com/ctmg/snapshot/ctmg-${version}.tar.xz";
-    sha256 = "1djxrf66abl63cybxfgjshr8avbp6nq158d0jx4mlfa3pdj32kv9";
+  src = fetchzip {
+    url = "https://git.zx2c4.com/ctmg/snapshot/ctmg-${version}.tar.xz";
+    sha256 = "1i4v8sriwjrmj3yizbl1ysckb711yl9qsn9x45jq0ij1apsydhyc";
   };
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp ctmg.sh $out/bin/ctmg
-  '';
+  installPhase = "install -D ctmg.sh $out/bin/ctmg";
 
   meta = with stdenv.lib; {
     description = "An encrypted container manager for Linux using cryptsetup";
-    homepage    = https://git.zx2c4.com/ctmg/about/;
-    license     = licenses.free;
+    homepage = https://git.zx2c4.com/ctmg/about/;
+    license = licenses.isc;
     maintainers = with maintainers; [ mrVanDalo ];
-    platforms   = platforms.linux;
+    platforms = platforms.linux;
   };
 }
-
