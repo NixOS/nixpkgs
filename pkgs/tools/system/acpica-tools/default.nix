f925ff0f35a78a7004bda87f805d205f79a6ce65
R. RyanTM <ryantm+bot@ryantm.com>
2018-06-06 05:36:40 -0700

    acpica-tools: 20180508 -> 20180531
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/acpica-tools/versions.
    
    These checks were done:
    
    - built on NixOS
    - /nix/store/x1sw7fxqaqayhy9hyvh57xc2zjg5d4yw-acpica-tools-20180531/bin/acpibin passed the binary check.
    - /nix/store/x1sw7fxqaqayhy9hyvh57xc2zjg5d4yw-acpica-tools-20180531/bin/acpidump passed the binary check.
    - /nix/store/x1sw7fxqaqayhy9hyvh57xc2zjg5d4yw-acpica-tools-20180531/bin/acpiexec passed the binary check.
    - /nix/store/x1sw7fxqaqayhy9hyvh57xc2zjg5d4yw-acpica-tools-20180531/bin/acpihelp passed the binary check.
    - /nix/store/x1sw7fxqaqayhy9hyvh57xc2zjg5d4yw-acpica-tools-20180531/bin/acpinames passed the binary check.
    - /nix/store/x1sw7fxqaqayhy9hyvh57xc2zjg5d4yw-acpica-tools-20180531/bin/acpixtract passed the binary check.
    - 6 of 6 passed binary check by having a zero exit code.
    - 0 of 6 passed binary check by having the new version present in output.
    - directory tree listing: https://gist.github.com/0bdfbe66fba66901dbdc08214997cf2d
    - du listing: https://gist.github.com/28d13e951134d3565b807bef5d9610ec

--- pkgs/tools/system/acpica-tools/default.nix
+++ pkgs/tools/system/acpica-tools/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "acpica-tools-${version}";
-  version = "20180508";
+  version = "20180531";
 
   src = fetchurl {
     url = "https://acpica.org/sites/acpica/files/acpica-unix-${version}.tar.gz";
-    sha256 = "1n7lqmv77kg28drahvxzybwl9v4hzwi8i7xkpgliclfcp5ff909b";
+    sha256 = "0rbn0anxs6r1ks1lgaxqhiv2kqgh4f1fq5qi2kdv7hir82mdqv4g";
   };
 
   NIX_CFLAGS_COMPILE = "-O3";
