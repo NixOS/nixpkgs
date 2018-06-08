d71ed3fe7b00b67ff5570048bfd6ac774bc2a6d6
R. RyanTM <ryantm+bot@ryantm.com>
2018-06-08 05:22:08 -0700

    itstool: 2.0.2 -> 2.0.4
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/itstool/versions.
    
    These checks were done:
    
    - built on NixOS
    - /nix/store/529hp8wa185yycz3chnlvhzcmrpz56v9-itstool-2.0.4/bin/itstool passed the binary check.
    - 1 of 1 passed binary check by having a zero exit code.
    - 1 of 1 passed binary check by having the new version present in output.
    - found 2.0.4 with grep in /nix/store/529hp8wa185yycz3chnlvhzcmrpz56v9-itstool-2.0.4
    - directory tree listing: https://gist.github.com/914d166f6731c4ee40fe5862db64b679
    - du listing: https://gist.github.com/a04e85b32506657e23cf311ebe3d12cc

--- pkgs/development/tools/misc/itstool/default.nix
+++ pkgs/development/tools/misc/itstool/default.nix
@@ -4,11 +4,11 @@
 stdenv.mkDerivation rec {
   # 2.0.3+ breaks the build of gnome3.gnome-desktop
   # https://github.com/itstool/itstool/issues/17
-  name = "itstool-2.0.2";
+  name = "itstool-2.0.4";
 
   src = fetchurl {
     url = "http://files.itstool.org/itstool/${name}.tar.bz2";
-    sha256 = "bf909fb59b11a646681a8534d5700fec99be83bb2c57badf8c1844512227033a";
+    sha256 = "0q7b4qrc758zfx3adsgvz0r93swdbxjr42w37rahngm33nshihlp";
   };
 
   buildInputs = [ python2 libxml2Python ];
