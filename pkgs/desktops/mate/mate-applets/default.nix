a5950ab0f7c4765e49f55be773ca4a735cc8ad0d
R. RyanTM <ryantm-bot@ryantm.com>
2018-06-08 15:51:23 -0700

    mate.mate-applets: 1.20.1 -> 1.20.2 (#41546)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/mate-applets/versions.
    
    These checks were done:
    
    - built on NixOS
    - Warning: no invocation of /nix/store/ll379k2bj134fdja5l40wl49d1zzyj5v-mate-applets-1.20.2/bin/mate-cpufreq-selector had a zero exit code or showed the expected version
    - Warning: no invocation of /nix/store/ll379k2bj134fdja5l40wl49d1zzyj5v-mate-applets-1.20.2/bin/.mate-cpufreq-selector-wrapped had a zero exit code or showed the expected version
    - 0 of 2 passed binary check by having a zero exit code.
    - 0 of 2 passed binary check by having the new version present in output.
    - found 1.20.2 with grep in /nix/store/ll379k2bj134fdja5l40wl49d1zzyj5v-mate-applets-1.20.2
    - directory tree listing: https://gist.github.com/8917b9a2989aa781cfe8f2a3b86c8821
    - du listing: https://gist.github.com/867b59527b5cda0652968142a6f9ef54

--- pkgs/desktops/mate/mate-applets/default.nix
+++ pkgs/desktops/mate/mate-applets/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "mate-applets-${version}";
-  version = "1.20.1";
+  version = "1.20.2";
 
   src = fetchurl {
     url = "http://pub.mate-desktop.org/releases/${mate.getRelease version}/${name}.tar.xz";
-    sha256 = "1a119g49sr7jrd8i32bw7sn2qlsg3sdiwqdb2v36bm2999j261wc";
+    sha256 = "0c860yxbphpvci281gj78ipw1rvcka4rr0bxwvg0nkpangdhj9ls";
   };
 
   nativeBuildInputs = [
