a444dcad030800e3d05850425bc89457f7bf0e89
Tim Steinbach <tim@nequissimus.com>
2018-06-10 21:00:31 -0400

    linux-copperhead: LTS based on regular 4.14

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -1,9 +1,12 @@
-{ stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
+{ stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, modDirVersionArg ? null, ... } @ args:
 
 with stdenv.lib;
 
 buildLinux (args // rec {
   version = "4.14.48";
+
+  # modDirVersion needs to be x.y.z, will automatically add .0 if needed
+  modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
 
   # branchVersion needs to be x.y
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
