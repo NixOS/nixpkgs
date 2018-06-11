a33b69808d03eb58bc8ce109a87e3f8765337cb7
Augustin Borsu <a.borsu@gmail.com>
2018-06-11 21:31:41 +0200

    nextcloud: 13.03 -> 13.04

--- pkgs/servers/nextcloud/default.nix
+++ pkgs/servers/nextcloud/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name= "nextcloud-${version}";
-  version = "13.0.3";
+  version = "13.0.4";
 
   src = fetchurl {
     url = "https://download.nextcloud.com/server/releases/${name}.tar.bz2";
-    sha256 = "1r4k3vbjxm07mlm430hmp61dx052ikgzw0bqlmg09p8011a6fdhq";
+    sha256 = "18d514145fcddc86f48d0a5fa4a0d4b07617135a1b23107137a6ea3ed519bd54";
   };
 
   installPhase = ''
