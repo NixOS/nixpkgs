774957b72164ca65338eccc7678767a094e07152
Frederik Rietdijk <fridh@fridh.nl>
2018-06-12 18:47:09 +0200

    python: home-assistant-frontend: 20180509.0 -> 20180607.0

--- pkgs/servers/home-assistant/frontend.nix
+++ pkgs/servers/home-assistant/frontend.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "home-assistant-frontend";
-  version = "20180509.0";
+  version = "20180607.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "11d9c4a07565358e6ee001f5c57c8393b4aaadac0d993a0a39a0387a33644fba";
+    sha256 = "22ac0b7615c9c7e6700db250079d8a1041c8d40788375402701adaace8b21889";
   };
 
   propagatedBuildInputs = [ user-agents ];
