d63819f9cfe4ecc0d863aec7708bae1670cb97bd
Frederik Rietdijk <fridh@fridh.nl>
2018-06-12 18:46:59 +0200

    python: decorator: 4.2.1 -> 4.3.0

--- pkgs/development/python-modules/decorator/default.nix
+++ pkgs/development/python-modules/decorator/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "decorator";
-  version = "4.2.1";
+  version = "4.3.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "7d46dd9f3ea1cf5f06ee0e4e1277ae618cf48dfb10ada7c8427cd46c42702a0e";
+    sha256 = "c39efa13fbdeb4506c476c9b3babf6a718da943dab7811c206005a4a956c080c";
   };
 
   meta = {
