4eb3784c5568e1a77fd31d788481d40e2326402d
Frederik Rietdijk <fridh@fridh.nl>
2018-06-12 20:56:11 +0200

    python.pkgs.setuptools: 39.0.1 -> 39.2.0

--- pkgs/development/python-modules/setuptools/default.nix
+++ pkgs/development/python-modules/setuptools/default.nix
@@ -8,13 +8,13 @@
 # Should use buildPythonPackage here somehow
 stdenv.mkDerivation rec {
   pname = "setuptools";
-  version = "39.0.1";
+  version = "39.2.0";
   name = "${python.libPrefix}-${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "bec7badf0f60e7fc8153fac47836edc41b74e5d541d7692e614e635720d6a7c7";
+    sha256 = "f7cddbb5f5c640311eb00eab6e849f7701fa70bf6a183fc8a2c33dd1d1672fb2";
   };
 
   nativeBuildInputs = [ unzip wrapPython ];
@@ -34,7 +34,7 @@ stdenv.mkDerivation rec {
     description = "Utilities to facilitate the installation of Python packages";
     homepage = https://pypi.python.org/pypi/setuptools;
     license = with licenses; [ psfl zpl20 ];
-    platforms = platforms.all;
+    platforms = python.meta.platforms;
     priority = 10;
   };
 }
