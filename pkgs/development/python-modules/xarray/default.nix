d75814852e96ca15b8ef4252a0ef470209dc8253
Frederik Rietdijk <fridh@fridh.nl>
2018-06-12 18:47:08 +0200

    python: xarray: 0.10.4 -> 0.10.7

--- pkgs/development/python-modules/xarray/default.nix
+++ pkgs/development/python-modules/xarray/default.nix
@@ -10,11 +10,11 @@
 
 buildPythonPackage rec {
   pname = "xarray";
-  version = "0.10.4";
+  version = "0.10.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "64c2d07b75b8ea1320cba521818a2f09107f5f46514d50ff5ef1c4d5da90aaf1";
+    sha256 = "d87241580e3eccb961dfc16e804e1b92b3d6a8000ffee82ceb076767934342cc";
   };
 
   checkInputs = [ pytest ];
