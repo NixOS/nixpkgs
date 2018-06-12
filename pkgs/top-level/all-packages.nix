f8cd0cbc9c4e449acc8c3a72643643ce3e288f32
Lorenzo Manacorda <lorenzo@mailbox.org>
2018-06-12 18:54:44 +0200

    asc-key-to-qr-code-gif: init at 20180613

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -680,6 +680,8 @@ with pkgs;
 
   passExtensions = recurseIntoAttrs pass.extensions;
 
+  asc-key-to-qr-code-gif = callPackage ../tools/security/asc-key-to-qr-code-gif { };
+
   gopass = callPackage ../tools/security/gopass { };
 
   browserpass = callPackage ../tools/security/browserpass { };
