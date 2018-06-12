765638200f906820f213f42bd287066e52c96d13
Frederik Rietdijk <fridh@fridh.nl>
2018-06-12 18:47:05 +0200

    python: pyspark: 2.3.0 -> 2.3.1

--- pkgs/development/python-modules/pyspark/default.nix
+++ pkgs/development/python-modules/pyspark/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "pyspark";
-  version = "2.3.0";
+  version = "2.3.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0vlq07yqy6c7ayg401i0qynnliqz405bmw1r8alkck0m1s8kcd8b";
+    sha256 = "52d77a7ef43088b0235742cfcafc83435d0d98c5fdded1d8c600f1887e9e0213";
   };
 
   # pypandoc is broken with pandoc2, so we just lose docs.
