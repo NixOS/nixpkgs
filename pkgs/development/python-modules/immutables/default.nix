4eafcfc161d084b6f88c755f04b3bec04462d94b
Frederik Rietdijk <fridh@fridh.nl>
2018-06-12 18:47:01 +0200

    python: immutables: 0.5 -> 0.6

--- pkgs/development/python-modules/immutables/default.nix
+++ pkgs/development/python-modules/immutables/default.nix
@@ -2,12 +2,12 @@
 
 buildPythonPackage rec {
   pname = "immutables";
-  version = "0.5";
+  version = "0.6";
   disabled = pythonOlder "3.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1hba0vkqanwfnb5b3rs14bs7schsmczhan5nd93c1i6fzi17glap";
+    sha256 = "63023fa0cceedc62e0d1535cd4ca7a1f6df3120a6d8e5c34e89037402a6fd809";
   };
 
   meta = {
