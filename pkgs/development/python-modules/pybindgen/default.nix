b2d62bc3905eb9641a8548192736d394a44f3b3b
Frederik Rietdijk <fridh@fridh.nl>
2018-06-12 18:47:04 +0200

    python: PyBindGen: 0.18.0 -> 0.19.0

--- pkgs/development/python-modules/pybindgen/default.nix
+++ pkgs/development/python-modules/pybindgen/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchPypi, buildPythonPackage, setuptools_scm, pygccxml }:
 buildPythonPackage rec {
   pname = "PyBindGen";
-  version = "0.18.0";
+  version = "0.19.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1sl4jn8rildv6f62cab66w791cixhaaxl7gwg9labs099rl74yl6";
+    sha256 = "23f2b760e352729208cd4fbadbc618bd00f95a0a24db21a4182833afcc3b5208";
   };
 
   buildInputs = [ setuptools_scm ];
