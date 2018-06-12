06ad318f2f805f41edf29d3a105c20748c16f8b8
Frederik Rietdijk <fridh@fridh.nl>
2018-06-12 18:47:08 +0200

    python: yarl: 1.1.1 -> 1.2.5

--- pkgs/development/python-modules/yarl/default.nix
+++ pkgs/development/python-modules/yarl/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "yarl";
-  version = "1.1.1";
+  version = "1.2.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a69dd7e262cdb265ac7d5e929d55f2f3d07baaadd158c8f19caebf8dde08dfe8";
+    sha256 = "dd5da4150a882f5cd26aeec7939f38e4b08b790717b9d696409dba9e18ff3ab6";
   };
 
   checkInputs = [ pytest pytestrunner ];
