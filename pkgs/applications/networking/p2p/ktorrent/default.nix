0b8e92a7fde68a7d1023fa504a6551e0b7046148
Andrey Golovizin <ag@sologoc.com>
2018-06-13 21:16:51 +0200

    ktorrent: fix build with CMake 3.11

--- pkgs/applications/networking/p2p/ktorrent/default.nix
+++ pkgs/applications/networking/p2p/ktorrent/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, cmake
+{ stdenv, fetchurl, fetchpatch, cmake
 , extra-cmake-modules, qtbase, qtscript
 , karchive, kcrash, kdnssd, ki18n, kio, knotifications, knotifyconfig
 , kdoctools, kross, kcmutils, kwindowsystem
@@ -20,6 +20,14 @@ stdenv.mkDerivation rec {
     qtbase qtscript
     karchive kcrash kdnssd ki18n kio knotifications knotifyconfig kross kcmutils kwindowsystem
     libktorrent taglib libgcrypt kplotting
+  ];
+
+  patches = [
+    # Fix build with CMake 3.11
+    (fetchpatch {
+      url = "https://cgit.kde.org/ktorrent.git/patch/?id=672c5076de7e3a526d9bdbb484a69e9386bc49f8";
+      sha256 = "1cn4rnbhadrsxqx50fawpd747azskavbjraygr6s11rh1wbfrxid";
+    })
   ];
 
   enableParallelBuilding = true;
