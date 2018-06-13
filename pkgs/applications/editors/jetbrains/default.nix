3cbb2a4be99d1ed67eed5660cff111dde20654ca
Ioannis Koutras <ioannis.koutras@gmail.com>
2018-06-13 19:12:09 +0300

    jetbrains.phpstorm: 2018.1.4 -> 2018.1.6
    (cherry picked from commit e8d8592937160c85a9a1fdd667ea1e1e0981054c)

--- pkgs/applications/editors/jetbrains/default.nix
+++ pkgs/applications/editors/jetbrains/default.nix
@@ -304,12 +304,12 @@ in
 
   phpstorm = buildPhpStorm rec {
     name = "phpstorm-${version}";
-    version = "2018.1.4"; /* updated by script */
+    version = "2018.1.6"; /* updated by script */
     description = "Professional IDE for Web and PHP developers";
     license = stdenv.lib.licenses.unfree;
     src = fetchurl {
       url = "https://download.jetbrains.com/webide/PhpStorm-${version}.tar.gz";
-      sha256 = "0rrcsn44va942nrznldjkxymir45q8gq1lf3f8vg1w3k87cfk1zp"; /* updated by script */
+      sha256 = "0v07sfg7ywawk6wb52zhr3753hscpxw3m53ppgw4n50dcdwx0kdn"; /* updated by script */
     };
     wmClass = "jetbrains-phpstorm";
     update-channel = "PhpStorm 2018.1";
