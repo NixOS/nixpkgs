0fe24c4586a7cb6af1abb5c177bdab53f0fbddf7
R. RyanTM <ryantm+bot@ryantm.com>
2018-06-14 04:44:33 -0700

    libsixel: 1.7.3 -> 1.8.1
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/libsixel/versions.
    
    These checks were done:
    
    - built on NixOS
    - /nix/store/ik92zw2fv4xpgnyrqj7aqyck2zzvxva5-libsixel-1.8.1/bin/img2sixel passed the binary check.
    - /nix/store/ik92zw2fv4xpgnyrqj7aqyck2zzvxva5-libsixel-1.8.1/bin/sixel2png passed the binary check.
    - /nix/store/ik92zw2fv4xpgnyrqj7aqyck2zzvxva5-libsixel-1.8.1/bin/libsixel-config passed the binary check.
    - 3 of 3 passed binary check by having a zero exit code.
    - 1 of 3 passed binary check by having the new version present in output.
    - found 1.8.1 with grep in /nix/store/ik92zw2fv4xpgnyrqj7aqyck2zzvxva5-libsixel-1.8.1
    - directory tree listing: https://gist.github.com/12d77ba43524f986a70f71b8e6f1714e
    - du listing: https://gist.github.com/9642634786b1c4b8f9c5396f7d8c02cd

--- pkgs/development/libraries/libsixel/default.nix
+++ pkgs/development/libraries/libsixel/default.nix
@@ -1,13 +1,13 @@
 {stdenv, fetchFromGitHub}:
 stdenv.mkDerivation rec {
-  version = "1.7.3";
+  version = "1.8.1";
   name = "libsixel-${version}";
 
   src = fetchFromGitHub {
     repo = "libsixel";
     rev = "v${version}";
     owner = "saitoha";
-    sha256 = "1hzmypzzigmxl07vgc52wp4dgxkhya3gfk4yzaaxc8s630r6ixs8";
+    sha256 = "0cbhvd1yk0q08nxva5bga7bpp8yxjfdfnqicvip4l6k28mzz7pmf";
   };
 
   meta = with stdenv.lib; {
