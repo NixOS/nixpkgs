4b94bd661c63698d52b21b5362a6d9f66e8921ce
R. RyanTM <ryantm+bot@ryantm.com>
2018-06-14 04:57:40 -0700

    jackett: 0.8.1014 -> 0.8.1070
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/jackett/versions.
    
    These checks were done:
    
    - built on NixOS
    - /nix/store/a9gjyqrjq6gf7l9s16pipj77p7vb3qcm-jackett-0.8.1070/bin/Jackett passed the binary check.
    - 1 of 1 passed binary check by having a zero exit code.
    - 0 of 1 passed binary check by having the new version present in output.
    - found 0.8.1070 with grep in /nix/store/a9gjyqrjq6gf7l9s16pipj77p7vb3qcm-jackett-0.8.1070
    - directory tree listing: https://gist.github.com/ee9b1ad710c8226187cbc65aa3ab9042
    - du listing: https://gist.github.com/c374f814801d76ac2507c5cb6fa5d405

--- pkgs/servers/jackett/default.nix
+++ pkgs/servers/jackett/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "jackett-${version}";
-  version = "0.8.1014";
+  version = "0.8.1070";
 
   src = fetchurl {
     url = "https://github.com/Jackett/Jackett/releases/download/v${version}/Jackett.Binaries.Mono.tar.gz";
-    sha256 = "0pvm9q8dpavkgsv180mw4y6555iixjv5s7mnmai9xzjspjpf264k";
+    sha256 = "1xdmf1nvs1y8jhivmc0sw8cip925xrj8hg66z0awcymri6qnp6s9";
   };
 
   buildInputs = [ makeWrapper ];
