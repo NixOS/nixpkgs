f3c913d466f1994192f758002a6bbb796bda05ef
Lancelot SIX <lsix@lancelotsix.com>
2018-06-14 10:36:06 +0200

    libgcrypt: 1.8.2 -> 1.8.3
    See http://lists.gnu.org/archive/html/info-gnu/2018-06/msg00006.html for
    release information
    
    (cherry picked from commit a0b2ce51020b2a8425d962f27f1708d1652c8195)

--- pkgs/development/libraries/libgcrypt/default.nix
+++ pkgs/development/libraries/libgcrypt/default.nix
@@ -6,11 +6,11 @@ assert enableCapabilities -> stdenv.isLinux;
 
 stdenv.mkDerivation rec {
   name = "libgcrypt-${version}";
-  version = "1.8.2";
+  version = "1.8.3";
 
   src = fetchurl {
     url = "mirror://gnupg/libgcrypt/${name}.tar.bz2";
-    sha256 = "01sca9m8hm6b5v8hmqsfdjhyz013869p1f0fxw9ln52qfnp4q1n8";
+    sha256 = "0z5gs1khzyknyfjr19k8gk4q148s6q987ya85cpn0iv70fz91v36";
   };
 
   outputs = [ "out" "dev" "info" ];
