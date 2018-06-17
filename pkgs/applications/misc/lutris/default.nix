a5680c01d703212a78f946d5543a438293d7d918
Okina Matara <okinan@chiru.no>
2018-06-17 15:51:18 -0500

    lutris: Add more dependencies

--- pkgs/applications/misc/lutris/default.nix
+++ pkgs/applications/misc/lutris/default.nix
@@ -3,7 +3,7 @@
 
 let
   qt5Deps = with pkgs; with qt5; [ qtbase qtmultimedia ];
-  gnome2Deps = with pkgs; with gnome2; [ libglade gtkglext ];
+  gnome2Deps = with pkgs; with gnome2; [ libglade gtkglext GConf ];
   gnome3Deps = with pkgs; with gnome3; [ zenity ];
   python2Deps = with pkgs; with python2Packages; [ pygtk ];
 
@@ -13,15 +13,24 @@ let
 
   xorgDeps = with pkgs; with xorg; [
     libX11 libXrender libXrandr libxcb libXmu libpthreadstubs libXext libXdmcp
-    libXxf86vm libXinerama libSM xrandr
+    libXxf86vm libXinerama libSM libXv libXaw libXi libXcursor libXcomposite
+    xrandr xdg_utils
+  ];
+
+  gstDeps = with pkgs; with gst_all_1; [
+    gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly
+    gst-libav
   ];
 
   deps = with pkgs; [
     # Common
-    libGL libGLU_combined libsndfile libtheora libogg libvorbis libopus libGLU
-    libpcap libpulseaudio libao libusb libevdev libudev libgcrypt libxml2 libusb
-    zlib alsaLib glib zziplib keyutils bash cabextract freetype curl unzip dbus
-    coreutils readline SDL SDL2 graphite2 gtk2 gtk3
+    stdenv.cc.cc libGL libGLU_combined libsndfile libtheora libogg libvorbis
+    libopus libGLU libpcap libpulseaudio libao libusb libevdev libudev libgcrypt
+    libxml2 libusb libpng libmpeg2 libv4l libjpeg libxkbcommon libass libcdio
+    libjack2 libsamplerate libzip libmad libaio libcap libtiff libva libgphoto2
+    libxslt libtxc_dxtn libsndfile giflib zlib glib alsaLib zziplib bash dbus
+    keyutils zip cabextract freetype unzip coreutils readline gcc SDL SDL2 curl
+    graphite2 gtk2 gtk3 udev gtksourceview ncurses wayland
 
     # Lutris
     gobjectIntrospection gdk_pixbuf pango openssl sqlite xterm
@@ -32,21 +41,73 @@ let
     # Desmume
     lua agg soundtouch openal desktop-file-utils pangox_compat atk
 
-    # DGen
-    # TODO: libarchive is broken
+    # DGen // TODO: libarchive is broken
 
     # Dolphin
     bluez ffmpeg gettext portaudio wxGTK30 miniupnpc mbedtls lzo sfml gsm
     wavpack gnutls-kdh orc nettle gmp pcre vulkan-loader
 
+    # DOSBox
+    SDL_net SDL_sound
+
+    # Higan // TODO: "higan is not available for the x86_64 architecture"
+
+    # Libretro
+    fluidsynth hidapi mesa_noglu libdrm
+
+    # MAME
+    qt48 fontconfig SDL2_ttf
+
+    # Mednafen
+    freeglut mesa_glu
+
+    # MESS
+    expat
+
+    # Mupen64Plus
+    boost dash
+
+    # Osmose
+    qt4
+
+    # PCSX2 // TODO: "libgobject-2.0.so.0: wrong ELF class: ELFCLASS64"
+
+    # PPSSPP
+    glew snappy
+
+    # Redream // "redream is not available for the x86_64 architecture"
+
+    # ResidualVM
+    flac
+
+    # rpcs3 // TODO: "error while loading shared libraries: libz.so.1..."
+    llvm_4
+
+    # ScummVM
+    nasm sndio
+
+    # Snes9x
+    epoxy minizip
+
+    # Steam
+    steam
+
+    # Vice
+    bison flex
+
     # WINE
-    wine perl which p7zip gnused gnugrep
+    wine perl which p7zip gnused gnugrep psmisc cups lcms2 mpg123 cairo unixODBC
+    samba4 sane-backends openldap opencl-headers ocl-icd
+
+    # ZDOOM
+    soundfont-fluid bzip2 game-music-emu
   ] ++ qt5Deps
     ++ gnome2Deps
     ++ gnome3Deps
     ++ python2Deps
     ++ python3Deps
-    ++ xorgDeps;
+    ++ xorgDeps
+    ++ gstDeps;
 
   lutris = python3Packages.buildPythonApplication rec {
     name = "lutris-${version}";
