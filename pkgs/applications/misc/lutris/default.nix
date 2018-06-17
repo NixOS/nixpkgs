b72a72733930fefc1a4f2193ad5c615b751ce9eb
Okina Matara <okinan@chiru.no>
2018-06-17 12:49:26 -0500

    lutris: Add more dependencies, restructure

--- pkgs/applications/misc/lutris/default.nix
+++ pkgs/applications/misc/lutris/default.nix
@@ -1,28 +1,56 @@
-{ stdenv, pkgs, buildFHSUserEnv, makeDesktopItem, fetchFromGitHub
-, wrapGAppsHook, python3 }:
+{ stdenv, pkgs, buildFHSUserEnv, makeDesktopItem, fetchFromGitHub, fetchpatch
+, wrapGAppsHook, python2Packages, python3Packages }:
 
 let
-  inherit (python3.pkgs) buildPythonApplication evdev pyyaml pyxdg pygobject3
-    dbus-python;
+  qt5Deps = with pkgs; with qt5; [ qtbase qtmultimedia ];
+  gnome2Deps = with pkgs; with gnome2; [ libglade gtkglext ];
+  gnome3Deps = with pkgs; with gnome3; [ zenity ];
+  python2Deps = with pkgs; with python2Packages; [ pygtk ];
 
-  deps = with pkgs; with xorg; [
-    stdenv.cc.cc gtk2 gtk3 gobjectIntrospection gdk_pixbuf cairo glib pango zlib
-    openssl python3 polkit wine desktop-file-utils hicolor-icon-theme cabextract
-    evdev pyxdg pyyaml pygobject3 dbus-python sqlite xrandr xterm libX11 libGL
-    libXrandr libxml2 SDL alsaLib dbus pulseaudio harfbuzz freetype psmisc atk
-    bluez ffmpeg libao libGLU_combined pcre gettext libpthreadstubs libXext
-    libXxf86vm libXinerama libSM readline openal libXdmcp portaudio libusb
-    libevdev curl qt5.qtbase vulkan-loader libpulseaudio graphite2 libsndfile
+  python3Deps = with pkgs; with python3Packages; [
+    evdev pyyaml pyxdg pygobject3 pyqt5 dbus-python
   ];
 
-  lutris = buildPythonApplication rec {
+  xorgDeps = with pkgs; with xorg; [
+    libX11 libXrender libXrandr libxcb libXmu libpthreadstubs libXext libXdmcp
+    libXxf86vm libXinerama libSM xrandr
+  ];
+
+  deps = with pkgs; [
+    # Common
+    libGL libGLU_combined libsndfile libtheora libogg libvorbis libopus libGLU
+    libpcap libpulseaudio libao libusb libevdev libudev libgcrypt libxml2 libusb
+    zlib alsaLib glib zziplib keyutils bash cabextract freetype curl unzip dbus
+    coreutils readline SDL SDL2 graphite2 gtk2 gtk3
+
+    # Lutris
+    gobjectIntrospection gdk_pixbuf pango openssl sqlite xterm
+
+    # Adventure Game Studio
+    allegro dumb
+
+    # Desmume
+    lua agg soundtouch openal desktop-file-utils pangox_compat atk
+
+    # DGen
+    # TODO: libarchive is broken
+
+    # Dolphin
+    bluez ffmpeg gettext portaudio wxGTK30 miniupnpc mbedtls lzo sfml gsm
+    wavpack gnutls-kdh orc nettle gmp pcre vulkan-loader
+
+    # WINE
+    wine perl which p7zip gnused gnugrep
+  ] ++ qt5Deps
+    ++ gnome2Deps
+    ++ gnome3Deps
+    ++ python2Deps
+    ++ python3Deps
+    ++ xorgDeps;
+
+  lutris = python3Packages.buildPythonApplication rec {
     name = "lutris-${version}";
     version = "v0.4.18";
-    enableParallelBuilding = true;
-    nativeBuildInputs = [ wrapGAppsHook ];
-    propagatedBuildInputs = deps;
-    fullPath = stdenv.lib.makeLibraryPath deps;
-    preConfigure = "export HOME=$PWD";
 
     src = fetchFromGitHub {
       owner = "lutris";
@@ -31,12 +59,30 @@ let
       sha256 = "1pgvk3qaaph1dlkrc5cq2jifr3yqlhnqsfa0wkaqzssh9acd5q9b";
     };
 
+    patches = [(fetchpatch {
+      url = "https://github.com/lutris/lutris/commit/403a83e18690511cb723f883254c86081870f050.patch";
+      sha256 = "1zc45jgaqvpqgr6f9mkqzcwx48lffrjkp7zfk0fz4l2a05jim6hm";
+    })];
+
+    enableParallelBuilding = true;
+    nativeBuildInputs = [ wrapGAppsHook ];
+    propagatedBuildInputs = deps;
+    python2Path = python2Deps;
+    fullPath = stdenv.lib.makeLibraryPath deps;
+    preConfigure = "export HOME=$PWD";
+
     makeWrapperArgs = [
       "--prefix LD_LIBRARY_PATH : ${fullPath}:$out/lib"
       "--set GI_TYPELIB_PATH $GI_TYPELIB_PATH"
       "--prefix XDG_DATA_DIRS : $out/share"
       "--suffix XDG_DATA_DIRS : $XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
     ];
+
+    postInstall = ''
+      mkdir -p $out/lib
+      ln -sf ${pkgs.gsm}/lib/libgsm.so $out/lib/libgsm.so.1
+      mv $out/bin/lutris $out/bin/lutris-${version}
+    '';
 
     meta = with stdenv.lib; {
       homepage = "https://lutris.net";
@@ -59,11 +105,12 @@ let
   };
 in buildFHSUserEnv rec {
   name = "lutris";
-  runScript = "lutris";
+  runScript = "lutris-${lutris.version}";
   targetPkgs = pkgs: [ lutris ];
 
   extraInstallCommands = ''
-    mkdir -p $out/share/applications
-    cp ${desktopItem}/share/applications/* $out/share/applications
+    mkdir -p $out/share
+    cp -r ${desktopItem}/share/applications $out/share
+    ln -sf ${lutris}/share/icons $out/share
   '';
 }
