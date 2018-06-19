447ad26b107d4202f3c46e81d2e6828e13ed3f6a
R. RyanTM <ryantm+bot@ryantm.com>
2018-06-18 23:30:22 -0700

    riot-web: 0.15.4 -> 0.15.5
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/riot-web/versions.
    
    These checks were done:
    
    - built on NixOS
    
    - 0 of 0 passed binary check by having a zero exit code.
    - 0 of 0 passed binary check by having the new version present in output.
    - found 0.15.5 with grep in /nix/store/p5phic74phlnz8zyrfdvkq6qyacbjfdh-riot-web-0.15.5
    - directory tree listing: https://gist.github.com/2dfaa31736d9687934c557f92066d007
    - du listing: https://gist.github.com/f769d44c305628e76a52b8d63351b060

--- pkgs/applications/networking/instant-messengers/riot/riot-web.nix
+++ pkgs/applications/networking/instant-messengers/riot/riot-web.nix
@@ -3,11 +3,11 @@
 let configFile = writeText "riot-config.json" conf; in
 stdenv.mkDerivation rec {
   name= "riot-web-${version}";
-  version = "0.15.4";
+  version = "0.15.5";
 
   src = fetchurl {
     url = "https://github.com/vector-im/riot-web/releases/download/v${version}/riot-v${version}.tar.gz";
-    sha256 = "1c0b9717xmzvzzxj006hqxafvcpvnnrsifxjnbxxla58jmnwr9v5";
+    sha256 = "04sij99njkiiwc1q23gwa8z6h4z0riw6yb9z3ds7v2qiyi4sshdz";
   };
 
   installPhase = ''
