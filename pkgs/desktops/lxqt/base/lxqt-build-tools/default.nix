f85d30b3f859415dc8c3abcfc70ff042a46d466f
Jörg Thalheim <joerg@thalheim.io>
2018-06-20 22:49:24 +0100

    lxqt.lxqt-build-tools: 0.4.0 -> 0.5.0

--- pkgs/desktops/lxqt/base/lxqt-build-tools/default.nix
+++ pkgs/desktops/lxqt/base/lxqt-build-tools/default.nix
@@ -1,17 +1,19 @@
-{ stdenv, fetchFromGitHub, cmake, pkgconfig, pcre, qt5 }:
+{ stdenv, fetchFromGitHub, cmake, pkgconfig, pcre, qt5, glib }:
 
 stdenv.mkDerivation rec {
   name = "lxqt-build-tools-${version}";
-  version = "0.4.0";
+  version = "0.5.0";
 
   src = fetchFromGitHub {
     owner = "lxde";
     repo = "lxqt-build-tools";
     rev = version;
-    sha256 = "0i3pzgyd80n73dnqs8f6axinaji7biflgqsi33baxn4r1hy58ym1";
+    sha256 = "0dcwzrijmn4sgivmy2zwz3xa4y69pwhranyw0m90g0pp55di2psz";
   };
 
-  nativeBuildInputs = [ cmake pkgconfig pcre qt5.qtbase ];
+  nativeBuildInputs = [ cmake pkgconfig ];
+
+  buildInputs = [ qt5.qtbase glib pcre ];
 
   preConfigure = ''cmakeFlags+=" -DLXQT_ETC_XDG_DIR=$out/etc/xdg"'';
 
