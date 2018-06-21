80110d0bc420078bc29faecec997215b6890ecb0
Frederik Rietdijk <fridh@fridh.nl>
2018-06-21 07:37:15 +0200

    python: aiohue: 1.5.0 -> 1.6.0

--- pkgs/development/python-modules/aiohue/default.nix
+++ pkgs/development/python-modules/aiohue/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "aiohue";
-  version = "1.5.0";
+  version = "1.6.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "9300ccb1d2d6c193eae81d7bc2ee6a07dda04f1800572adc666a07d39fea4092";
+    sha256 = "87f0f86865e88ea715ab358b1e5f2838b79ee7cdc0bdf762e9ed60aaf4c8bd4a";
   };
 
   propagatedBuildInputs = [ aiohttp ];
