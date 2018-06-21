e3a279785ddcafbb5fcb2aaf23aa91befa2472b6
Frederik Rietdijk <fridh@fridh.nl>
2018-06-21 07:37:19 +0200

    python: pirate-get: 0.3.0 -> 0.3.1

--- pkgs/tools/networking/pirate-get/default.nix
+++ pkgs/tools/networking/pirate-get/default.nix
@@ -5,13 +5,13 @@ with python3Packages;
 buildPythonApplication rec {
   name = "${pname}-${version}";
   pname = "pirate-get";
-  version = "0.3.0";
+  version = "0.3.1";
 
   doCheck = false;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "171dd2d387fd8af726abcf4cac0d806463bedc8e5f892655179fb4b215df47b2";
+    sha256 = "9d7cc4b15dd8c6a82f9e03a666372e38613ccafdc846ad4c1226ba936beea68d";
   };
 
   propagatedBuildInputs = [ colorama veryprettytable beautifulsoup4 ];
