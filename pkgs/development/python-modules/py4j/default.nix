21f4050d67f8b45593c41a1641c8eddd7695794d
Frederik Rietdijk <fridh@fridh.nl>
2018-06-22 11:43:10 +0200

    python.pkgs.py4j: 0.10.6 -> 0.10.7

--- pkgs/development/python-modules/py4j/default.nix
+++ pkgs/development/python-modules/py4j/default.nix
@@ -3,15 +3,16 @@
 buildPythonPackage rec {
   pname = "py4j";
 
-  version = "0.10.6";
+  version = "0.10.7";
 
   src = fetchPypi {
     inherit pname version;
     extension= "zip";
-    sha256 = "10shayghsmcdr03w12a7sdm6vsxpjm8alw3ym3mr1hki45yarryk";
+    sha256 = "721189616b3a7d28212dfb2e7c6a1dd5147b03105f1fc37ff2432acd0e863fa5";
   };
 
-  propagatedBuildInputs = [  ];
+  # No tests in archive
+  doCheck = false;
 
   meta = with stdenv.lib; {
     description = "Py4J enables Python programs running in a Python interpreter to dynamically access Java objects in a Java Virtual Machine. Methods are called as if the Java objects resided in the Python interpreter and Java collections can be accessed through standard Python collection methods. Py4J also enables Java programs to call back Python objects.";
