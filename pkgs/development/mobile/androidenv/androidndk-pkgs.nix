5643d5c97017ada1878e6ee1a6b7677b1dc37cda
Matthew Bauer <mjbauer95@gmail.com>
2018-06-22 09:33:25 -0400

    android: Use NDK 17 for aarch32

--- pkgs/development/mobile/androidenv/androidndk-pkgs.nix
+++ pkgs/development/mobile/androidenv/androidndk-pkgs.nix
@@ -18,12 +18,12 @@ let
     "armv5tel-unknown-linux-androideabi" = {
       arch = "arm";
       triple = "arm-linux-androideabi";
-      gccVer = "4.8";
+      gccVer = "4.9";
     };
     "armv7a-unknown-linux-androideabi" = {
       arch = "arm";
       triple = "arm-linux-androideabi";
-      gccVer = "4.8";
+      gccVer = "4.9";
     };
     "aarch64-unknown-linux-android" = {
       arch = "arm64";
