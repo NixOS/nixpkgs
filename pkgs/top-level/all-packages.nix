c8fd285c8d9f307c02baa0da0c3fab0182bdc6ea
Matthew Bauer <mjbauer95@gmail.com>
2018-06-22 10:21:43 -0400

    android: add ndkVer to resolve ndk ambiguity
    It wasn’t exactly clear which NDK you were using previously. This adds
    an attribute to system that handles what version of the NDK we should
    use when building things.
    
    /cc @Ericson2314

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8695,7 +8695,7 @@ with pkgs;
 
   # TODO(@Ericson2314): Build bionic libc from source
   bionic = assert hostPlatform.useAndroidPrebuilt;
-    androidenv.androidndkPkgs.libraries;
+    androidenv."androidndkPkgs_${hostPlatform.ndkVer}".libraries;
 
   bobcat = callPackage ../development/libraries/bobcat { };
 
