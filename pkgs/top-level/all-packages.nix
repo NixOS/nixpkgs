eadd264712ed2cf53e4c713f144ea09ce64753d4
Leo Colisson <leo.colisson@gmail.com>
2018-06-22 21:24:31 +0200

    imapnotify: inherit inside all-packages

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16561,7 +16561,7 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) ApplicationServices;
   });
 
-  imapnotify = nodePackages.imapnotify;
+  inherit (nodePackages) imapnotify;
   
   # Impressive, formerly known as "KeyJNote".
   impressive = callPackage ../applications/office/impressive { };
