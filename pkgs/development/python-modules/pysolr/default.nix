956b45d8ad27f07ba34c70228beb89adecfb1dca
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-06-22 11:16:38 +0200

    python.pkgs.pysolr: 3.3.3 -> 3.7.0

--- pkgs/development/python-modules/pysolr/default.nix
+++ pkgs/development/python-modules/pysolr/default.nix
@@ -1,17 +1,21 @@
-{ lib, buildPythonPackage, fetchPypi, requests }:
+{ lib, buildPythonPackage, fetchPypi, setuptools_scm, requests, mock }:
 
 buildPythonPackage rec {
   pname = "pysolr";
-  version = "3.3.3";
+  version = "3.7.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1wapg9n7myn7c82r3nzs2gisfzx52nip8w2mrfy0yih1zn02mnd6";
+    sha256 = "b2c5f920d1cabaff8b465447ee152c6985f120b5895f091cf7bf15ff71ade1dc";
   };
+
+  nativeBuildInputs = [ setuptools_scm ];
 
   propagatedBuildInputs = [ requests ];
 
-  doCheck = false; # no tests in PyPI tarball
+  checkInputs = [ mock ];
+
+  doCheck = false; # requires network access
 
   meta = with lib; {
     description = "Lightweight Python wrapper for Apache Solr";
