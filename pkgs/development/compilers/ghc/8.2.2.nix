73c1b19c0d100b43fa4d75eb113b26ce505cfb4f
Will Dietz <w@wdtz.org>
2018-06-23 12:53:45 -0500

    ghc822: useLLVM when building against musl too

--- pkgs/development/compilers/ghc/8.2.2.nix
+++ pkgs/development/compilers/ghc/8.2.2.nix
@@ -8,7 +8,7 @@
 
 , libffi, libiconv ? null, ncurses
 
-, useLLVM ? !targetPlatform.isx86
+, useLLVM ? !targetPlatform.isx86 || targetPlatform.isMusl
 , # LLVM is conceptually a run-time-only depedendency, but for
   # non-x86, we need LLVM to bootstrap later stages, so it becomes a
   # build-time dependency too.
