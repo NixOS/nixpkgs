301fe22be471e36ce689e08e304519febfe6b159
Matthew Bauer <mjbauer95@gmail.com>
2018-06-23 17:47:52 -0400

    agda-stdlib: broken on darwin

--- pkgs/development/libraries/agda/agda-stdlib/default.nix
+++ pkgs/development/libraries/agda/agda-stdlib/default.nix
@@ -23,6 +23,7 @@ agda.mkDerivation (self: rec {
     description = "A standard library for use with the Agda compiler";
     license = stdenv.lib.licenses.mit;
     platforms = stdenv.lib.platforms.unix;
+    broken = stdenv.isDarwin;
     maintainers = with maintainers; [ jwiegley fuuzetsu mudri ];
   };
 })
