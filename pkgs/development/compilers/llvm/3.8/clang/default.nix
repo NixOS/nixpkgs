1b0c73555fd3366ecc0dedf618635d03e15187f4
Will Dietz <w@wdtz.org>
2018-06-23 12:09:57 -0500

    clang_38: musl fixup, cmake should be nativeBuildInput

--- pkgs/development/compilers/llvm/3.8/clang/default.nix
+++ pkgs/development/compilers/llvm/3.8/clang/default.nix
@@ -13,7 +13,8 @@ let
       mv clang-tools-extra-* $sourceRoot/tools/extra
     '';
 
-    buildInputs = [ cmake libxml2 llvm python ];
+    nativeBuildInputs = [ cmake ];
+    buildInputs = [ libxml2 llvm python ];
 
     cmakeFlags = [
       "-DCMAKE_CXX_FLAGS=-std=c++11"
@@ -27,6 +28,8 @@ let
     postPatch = ''
       sed -i -e 's/Args.hasArg(options::OPT_nostdlibinc)/true/' lib/Driver/Tools.cpp
       sed -i -e 's/DriverArgs.hasArg(options::OPT_nostdlibinc)/true/' lib/Driver/ToolChains.cpp
+    '' + stdenv.lib.optionalString stdenv.hostPlatform.isMusl ''
+      sed -i -e 's/lgcc_s/lgcc_eh/' lib/Driver/Tools.cpp
     '';
 
     outputs = [ "out" "lib" "python" ];
