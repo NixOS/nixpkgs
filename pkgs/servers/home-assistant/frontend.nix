bd7b8d5deaeb1d197bab03873d4836a12be2a5b9
Franz Pletz <fpletz@fnordicwalking.de>
2018-06-24 16:55:45 +0200

    python: home-assistant-frontend: 20180607.0 -> 20180622.1

--- pkgs/servers/home-assistant/frontend.nix
+++ pkgs/servers/home-assistant/frontend.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "home-assistant-frontend";
-  version = "20180607.0";
+  version = "20180622.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "22ac0b7615c9c7e6700db250079d8a1041c8d40788375402701adaace8b21889";
+    sha256 = "1kzz1cmnpmsnrbc9amjcg8rb4a544xx2mpq4g23si6rk46b7n0x7";
   };
 
   propagatedBuildInputs = [ user-agents ];
