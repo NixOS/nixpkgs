1e8081bc74c81a373e24c55fc531ec11b125bcd0
R. RyanTM <ryantm-bot@ryantm.com>
2018-06-24 11:15:40 -0700

    debootstrap: 1.0.102 -> 1.0.103 (#42514)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/debootstrap/versions.
    
    These checks were done:
    
    - built on NixOS
    - /nix/store/ysxn2fpiivgri65bcqf9z2ys794vv7xk-debootstrap-1.0.103/bin/debootstrap passed the binary check.
    - 1 of 1 passed binary check by having a zero exit code.
    - 0 of 1 passed binary check by having the new version present in output.
    - found 1.0.103 with grep in /nix/store/ysxn2fpiivgri65bcqf9z2ys794vv7xk-debootstrap-1.0.103
    - directory tree listing: https://gist.github.com/06c1aec1c5c510a9ce4b0895c3a4fea2
    - du listing: https://gist.github.com/81f3607504e359d1e330c44eb817d04b

--- pkgs/tools/misc/debootstrap/default.nix
+++ pkgs/tools/misc/debootstrap/default.nix
@@ -4,13 +4,13 @@
 # There is also cdebootstrap now. Is that easier to maintain?
 stdenv.mkDerivation rec {
   name = "debootstrap-${version}";
-  version = "1.0.102";
+  version = "1.0.103";
 
   src = fetchurl {
     # git clone git://git.debian.org/d-i/debootstrap.git
     # I'd like to use the source. However it's lacking the lanny script ? (still true?)
     url = "mirror://debian/pool/main/d/debootstrap/debootstrap_${version}.tar.gz";
-    sha256 = "1i8fnxyf073dqywcvj6n32k1crzl7bwlmn373n5342pz71rmqrq9";
+    sha256 = "07qqbm50ji1j04hgv1fmy7y1jwl4rjh8q57znh9g3rr4j6fmax7q";
   };
 
   buildInputs = [ dpkg gettext gawk perl ];
