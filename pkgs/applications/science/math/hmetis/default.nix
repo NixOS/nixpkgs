b66bb46bd1d17ec01b21ea9531faf7e052a07f99
System administrator <root@ip-172-31-2-217.us-west-1.compute.internal>
2018-06-25 22:23:21 +0000

    re-do default.nix with hello example

--- pkgs/applications/science/math/hmetis/default.nix
+++ pkgs/applications/science/math/hmetis/default.nix
@@ -1,71 +1,25 @@
-{ stdenv, lib, makeWrapper, fetchurl, unzip, atomEnv, makeDesktopItem, buildFHSUserEnv }:
+{ stdenv, fetchurl }:
 
-let
-  version = "0.10.0";
+stdenv.mkDerivation rec {
   name = "hmetis-${version}";
+  version = "2.10";
 
-  throwSystem = throw "Unsupported system: ${stdenv.system}";
+  src = fetchurl {
+    url = "mirror://gnu/hello/${name}.tar.gz";
+    sha256 = "0ssi1wpaf7plaswqqjwigppsg5fyh99vdlb9kzl7c9lng89ndq1i";
+  };
+
+  doCheck = true;
 
   meta = with stdenv.lib; {
-    description = "Browse and use Ðapps on the Ethereum network";
-    homepage = https://github.com/ethereum/mist;
-    license = licenses.gpl3;
-    maintainers = with maintainers; [];
-    platforms = [ "x86_64-linux" "i686-linux" ];
-  };
-
-  urlVersion = builtins.replaceStrings ["."] ["-"] version;
-
-  desktopItem = makeDesktopItem rec {
-    name = "Hmetis";
-    exec = "hmetis";
-    icon = "hmetis";
-    desktopName = name;
-    genericName = "Mist Browser";
-    categories = "Network;";
-  };
-
-  hmetis = stdenv.lib.appendToName "unwrapped" (stdenv.mkDerivation {
-    inherit name version;
-
-    src = {
-      i686-linux = fetchurl {
-        url = "https://github.com/ethereum/mist/releases/download/v${version}/Mist-linux32-${urlVersion}.zip";
-        sha256 = "01hvxlm9w522pwvsjdy18gsrapkfjr7d1jjl4bqjjysxnjaaj2lk";
-      };
-      x86_64-linux = fetchurl {
-        url = "https://github.com/ethereum/mist/releases/download/v${version}/Mist-linux64-${urlVersion}.zip";
-        sha256 = "01k17j7fdfhxfd26njdsiwap0xnka2536k9ydk32czd8db7ya9zi";
-      };
-    }.${stdenv.system} or throwSystem;
-
-    buildInputs = [ unzip makeWrapper ];
-
-    buildCommand = ''
-      mkdir -p $out/lib/hmetis $out/bin
-      unzip -d $out/lib/hmetis $src
-      ln -s $out/lib/hmetis/hmetis $out/bin
-      fixupPhase
-      mkdir -p $out/share/applications
-      ln -s ${desktopItem}/share/applications/* $out/share/applications
-      patchelf \
-        --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-        --set-rpath "${atomEnv.libPath}:$out/lib/hmetis" \
-        $out/lib/hmetis/hmetis
+    description = "A program that produces a familiar, friendly greeting";
+    longDescription = ''
+      GNU Hello is a program that prints "Hello, world!" when you run it.
+      It is fully customizable.
     '';
-  });
-in
-buildFHSUserEnv {
-  inherit name;
-
-  targetPkgs = pkgs: with pkgs; [
-     hmetis
-  ];
-
-  extraInstallCommands = ''
-    mkdir -p "$out/share/applications"
-    cp "${desktopItem}/share/applications/"* $out/share/applications
-  '';
-
-  runScript = "hmetis";
+    homepage = http://www.gnu.org/software/hello/manual/;
+    license = licenses.gpl3Plus;
+    maintainers = [ maintainers.eelco ];
+    platforms = platforms.all;
+  };
 }
