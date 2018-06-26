48ca5cf05b1018f971192d52492140a582244f3f
Pi3r <pierrer@pi3r.be>
2018-06-26 21:33:14 +0200

    Update configuration-common.nix
    `filecache` test suite won't pass in `osx` :
    see https://hydra.nixos.org/job/nixpkgs/trunk/haskellPackages.filecache.x86_64-darwin
    see https://github.com/bartavelle/filecache/issues/4

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -919,6 +919,7 @@ self: super: {
   # With ghc-8.2.x haddock would time out for unknown reason
   # See https://github.com/haskell/haddock/issues/679
   language-puppet = dontHaddock super.language-puppet;
+  filecache = dontCheck super.filecache;
 
   # Missing FlexibleContexts in testsuite
   # https://github.com/EduardSergeev/monad-memo/pull/4
