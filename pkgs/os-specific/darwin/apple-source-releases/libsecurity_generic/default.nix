fe9f4abec29b5b4bfe1742658f421642b35c3860
Matthew Justin Bauer <mjbauer95@gmail.com>
2018-06-26 13:25:06 -0400

    libsecurity_generic: fix apple_sdk lookup
    apple_sdk is now multiple output.

--- pkgs/os-specific/darwin/apple-source-releases/libsecurity_generic/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/libsecurity_generic/default.nix
@@ -21,7 +21,7 @@ name: version: sha256: args: let
         grep -Rl MacTypes.h . | while read file; do
           substituteInPlace "''$file" --replace \
             '<CoreServices/../Frameworks/CarbonCore.framework/Headers/MacTypes.h>' \
-            '"${pkgs.darwin.apple_sdk.sdk.out}/include/MacTypes.h"'
+            '"${stdenv.lib.getDev pkgs.darwin.apple_sdk.sdk}/include/MacTypes.h"'
         done || true # grep returns 1 if it can't find the string
       '';
       preBuild = ''
