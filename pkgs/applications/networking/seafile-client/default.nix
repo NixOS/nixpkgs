5bce4a3727e46e6a6efb2ce18a34eea6fb8c7c33
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-06-26 23:39:49 +0200

    seafile-client: 6.1.8 -> 6.2.1

--- pkgs/applications/networking/seafile-client/default.nix
+++ pkgs/applications/networking/seafile-client/default.nix
@@ -5,14 +5,14 @@
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
-  version = "6.1.8";
+  version = "6.2.1";
   name = "seafile-client-${version}";
 
   src = fetchFromGitHub {
     owner = "haiwen";
     repo = "seafile-client";
     rev = "v${version}";
-    sha256 = "0gy7jfxr5f8qvbqj80g7fzaw9b3vax750c4z5cr7f43rv99284pc";
+    sha256 = "02nzihs720pmgr5139s7fz88f7gm19dn1x2kqdpi81k7gdwqq0bp";
   };
 
   nativeBuildInputs = [ pkgconfig cmake makeWrapper ];
