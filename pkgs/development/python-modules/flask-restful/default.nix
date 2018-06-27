d44980198e89993db34eacb50aa21f1ce380948c
Marti Serra <marti.serra.coscollano@gmail.com>
2018-06-27 20:55:00 +0200

    pythonPackages.flask-restful: disable broken test

--- pkgs/development/python-modules/flask-restful/default.nix
+++ pkgs/development/python-modules/flask-restful/default.nix
@@ -17,6 +17,10 @@ buildPythonPackage rec {
   patchPhase = if isPy3k then ''
     rm tests/test_crypto.py tests/test_paging.py
     '' else null;
+# Disable test broken with aniso > 3.0.0, can be removed when updating to a flask-restful version with commit 54979f0
+  preCheck = ''
+  substituteInPlace "./tests/test_inputs.py" --replace "test_bad_isointervals" "disabled_bad_isointervals"
+  '';
   buildInputs = [ nose mock blinker ];
   propagatedBuildInputs = [ flask six pytz aniso8601 pycrypto ];
   PYTHON_EGG_CACHE = "`pwd`/.egg-cache";
