f1509d8b11b6be40774d0160f5fbbd918c52fa52
Franz Pletz <fpletz@fnordicwalking.de>
2018-06-29 19:17:54 +0200

    nixos/nextcloud: init
    (cherry picked from commit ebd38185c8f50535b251b487375a671f3943a6be)
    
    Co-authored-by: Franz Pletz <fpletz@fnordicwalking.de>
    Co-authored-by: Robin Gloster <mail@glob.in>
    Co-authored-by: Janne Heß <janne@hess.ooo>
    Co-authored-by: Florian Klink <flokli@flokli.de>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -679,6 +679,7 @@
   ./services/web-apps/atlassian/jira.nix
   ./services/web-apps/frab.nix
   ./services/web-apps/mattermost.nix
+  ./services/web-apps/nextcloud.nix
   ./services/web-apps/nexus.nix
   ./services/web-apps/pgpkeyserver-lite.nix
   ./services/web-apps/matomo.nix
