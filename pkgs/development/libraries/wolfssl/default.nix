062fee525505f74ead337c67820e111f8ec46414
R. RyanTM <ryantm-bot@ryantm.com>
2018-06-29 15:07:05 -0700

    wolfssl: 3.15.0 -> 3.15.3 (#42670)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/wolfssl/versions.
    
    These checks were done:
    
    - built on NixOS
    
    - 0 of 0 passed binary check by having a zero exit code.
    - 0 of 0 passed binary check by having the new version present in output.
    - found 3.15.3 in filename of file in /nix/store/64hjsmi2gzinf6qn0dfs16c3zzi4rfpx-wolfssl-3.15.3
    - directory tree listing: https://gist.github.com/f48303498f02ef2dc587368e1b17a323
    - du listing: https://gist.github.com/db2ab599e28e35132df0568d69c7f4c8

--- pkgs/development/libraries/wolfssl/default.nix
+++ pkgs/development/libraries/wolfssl/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "wolfssl-${version}";
-  version = "3.15.0";
+  version = "3.15.3";
 
   src = fetchFromGitHub {
     owner = "wolfSSL";
     repo = "wolfssl";
     rev = "v${version}-stable";
-    sha256 = "1jqhq0dl2zxks7dm6g3dm1zvrl646ab073kgg8ypv19nkd5cvbdv";
+    sha256 = "00mpq1z8j37a873dbk9knb835m3qlwqnd1rslirqkc44hpz1i64j";
   };
 
   outputs = [ "out" "dev" "doc" "lib" ];
