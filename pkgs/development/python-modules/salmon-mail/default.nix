b1abfb01328c7f9e13bceb1e00d37e5aab143d60
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2018-06-29 08:48:36 +0300

    pythonPackages.salmon-mail: fix tests

--- pkgs/development/python-modules/salmon-mail/default.nix
+++ pkgs/development/python-modules/salmon-mail/default.nix
@@ -14,6 +14,12 @@ buildPythonPackage rec {
   checkInputs = [ nose jinja2 mock ];
   propagatedBuildInputs = [ chardet dnspython lmtpd pythondaemon six ];
 
+  # The tests use salmon executable installed by salmon itself so we need to add
+  # that to PATH
+  checkPhase = ''
+    PATH=$out/bin:$PATH nosetests .
+  '';
+
   meta = with stdenv.lib; {
     homepage = http://salmon-mail.readthedocs.org/;
     description = "Pythonic mail application server";
