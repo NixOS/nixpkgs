3052de3bfd467b52cf203b7b8869e3071a77c45f
R. RyanTM <ryantm-bot@ryantm.com>
2018-06-29 15:03:04 -0700

    poppler_utils: 0.65.0 -> 0.66.0 (#42460)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/poppler-utils/versions.
    
    These checks were done:
    
    - built on NixOS
    - /nix/store/cgsca7brl8gxnlc1xaxdsyizbdlmha2q-poppler-utils-0.66.0/bin/pdftoppm passed the binary check.
    - /nix/store/cgsca7brl8gxnlc1xaxdsyizbdlmha2q-poppler-utils-0.66.0/bin/pdftocairo passed the binary check.
    - Warning: no invocation of /nix/store/cgsca7brl8gxnlc1xaxdsyizbdlmha2q-poppler-utils-0.66.0/bin/pdfdetach had a zero exit code or showed the expected version
    - /nix/store/cgsca7brl8gxnlc1xaxdsyizbdlmha2q-poppler-utils-0.66.0/bin/pdffonts passed the binary check.
    - /nix/store/cgsca7brl8gxnlc1xaxdsyizbdlmha2q-poppler-utils-0.66.0/bin/pdfimages passed the binary check.
    - /nix/store/cgsca7brl8gxnlc1xaxdsyizbdlmha2q-poppler-utils-0.66.0/bin/pdfinfo passed the binary check.
    - /nix/store/cgsca7brl8gxnlc1xaxdsyizbdlmha2q-poppler-utils-0.66.0/bin/pdfsig passed the binary check.
    - /nix/store/cgsca7brl8gxnlc1xaxdsyizbdlmha2q-poppler-utils-0.66.0/bin/pdftops passed the binary check.
    - /nix/store/cgsca7brl8gxnlc1xaxdsyizbdlmha2q-poppler-utils-0.66.0/bin/pdftotext passed the binary check.
    - /nix/store/cgsca7brl8gxnlc1xaxdsyizbdlmha2q-poppler-utils-0.66.0/bin/pdftohtml passed the binary check.
    - /nix/store/cgsca7brl8gxnlc1xaxdsyizbdlmha2q-poppler-utils-0.66.0/bin/pdfseparate passed the binary check.
    - /nix/store/cgsca7brl8gxnlc1xaxdsyizbdlmha2q-poppler-utils-0.66.0/bin/pdfunite passed the binary check.
    - 11 of 12 passed binary check by having a zero exit code.
    - 0 of 12 passed binary check by having the new version present in output.
    - found 0.66.0 with grep in /nix/store/cgsca7brl8gxnlc1xaxdsyizbdlmha2q-poppler-utils-0.66.0
    - directory tree listing: https://gist.github.com/af4034ac452cfd8a440300c9bfc12e28
    - du listing: https://gist.github.com/776d2f71e00585d9b456fd950c335614

--- pkgs/development/libraries/poppler/default.nix
+++ pkgs/development/libraries/poppler/default.nix
@@ -8,7 +8,7 @@
 }:
 
 let # beware: updates often break cups-filters build
-  version = "0.65.0";
+  version = "0.66.0";
   mkFlag = optset: flag: "-DENABLE_${flag}=${if optset then "on" else "off"}";
 in
 stdenv.mkDerivation rec {
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "${meta.homepage}/poppler-${version}.tar.xz";
-    sha256 = "01wgvpi1l7nqkvixnmhmlpld0fjryhl7rlwvba6agz9yz1rwzj49";
+    sha256 = "1rzar5f27xzkjih07yi8kxcinvk4ny4nhimyacpvqx7vmlqn829c";
   };
 
   outputs = [ "out" "dev" ];
