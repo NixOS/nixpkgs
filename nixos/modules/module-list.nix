4a72999c75a7d94ebb1fb9c9dd160541a45b44e5
Michishige Kaito <me@mkaito.com>
2018-06-29 15:36:03 +0100

    oauth2_proxy: add nginx vhost module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -624,6 +624,7 @@
   ./services/security/hologram-agent.nix
   ./services/security/munge.nix
   ./services/security/oauth2_proxy.nix
+  ./services/security/oauth2_proxy_nginx.nix
   ./services/security/physlock.nix
   ./services/security/shibboleth-sp.nix
   ./services/security/sks.nix
