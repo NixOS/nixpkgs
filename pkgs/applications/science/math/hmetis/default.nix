822966644ab16d0f5abfccb60b16b0603613f3af
Michael Gough <innovative.engineer@gmail.com>
2018-07-01 06:08:37 +0000

    move patchelf to patchPhase

--- pkgs/applications/science/math/hmetis/default.nix
+++ pkgs/applications/science/math/hmetis/default.nix
@@ -9,12 +9,21 @@ stdenv.mkDerivation rec {
     sha256 = "e835a098c046e9c26cecb8addfea4d18ff25214e49585ffd87038e72819be7e1";
   };
 
+  binaryFiles = "hmetis khmetis shmetis";
+
+  patchPhase = ''
+    for binaryfile in $binaryFiles; do 
+      patchelf \
+        --set-interpreter ${stdenv.glibc}/lib/ld-linux.so.2 \
+        --set-rpath ${stdenv.glibc}/lib \
+        $binaryfile
+    done
+  '';
+
   installPhase = ''
     mkdir -p $out/bin
-    for binaryfile in hmetis khmetis shmetis; do 
+    for binaryfile in $binaryFiles; do 
       cp $binaryfile $out/bin
-      patchelf --set-interpreter ${stdenv.glibc}/lib/ld-linux.so.2 \
-        --set-rpath ${stdenv.glibc}/lib $out/bin/$binaryfile
     done
   '';
 
