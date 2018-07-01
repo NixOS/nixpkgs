834f1424e66f24b001e1f48debca93e9b04b8801
Jacob Mitchell <jacob.mitchell@iohk.io>
2018-07-01 15:47:09 -0700

    vmTools: update Debian minor version to 8.11

--- pkgs/build-support/vm/default.nix
+++ pkgs/build-support/vm/default.nix
@@ -965,22 +965,22 @@ rec {
     };
 
     debian8i386 = {
-      name = "debian-8.10-jessie-i386";
-      fullName = "Debian 8.10 Jessie (i386)";
+      name = "debian-8.11-jessie-i386";
+      fullName = "Debian 8.11 Jessie (i386)";
       packagesList = fetchurl {
         url = mirror://debian/dists/jessie/main/binary-i386/Packages.xz;
-        sha256 = "1w1gm195dcrndy5486kcv0h9l3br9dqnqyyhmavp4vr5w2zk7amk";
+        sha256 = "0adblarhx50yga900il6m25ng0csa81i3wid1dxxmydbdmri7v7d";
       };
       urlPrefix = mirror://debian;
       packages = commonDebianPackages;
     };
 
     debian8x86_64 = {
-      name = "debian-8.10-jessie-amd64";
-      fullName = "Debian 8.10 Jessie (amd64)";
+      name = "debian-8.11-jessie-amd64";
+      fullName = "Debian 8.11 Jessie (amd64)";
       packagesList = fetchurl {
         url = mirror://debian/dists/jessie/main/binary-amd64/Packages.xz;
-        sha256 = "045700qsrmd3lng2rw8nfs5ci7pf660lwl6alpzkyjikyp6pg7k8";
+        sha256 = "09y1mv4kqllhxpk1ibjsyl5jig5bp0qxw6pp4sn56rglrpygmn5x";
       };
       urlPrefix = mirror://debian;
       packages = commonDebianPackages;
