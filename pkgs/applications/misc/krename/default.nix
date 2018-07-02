8c98ebdf186b7f3be14fe907e5f942c1ae0e6612
Peter Hoeg <peter@hoeg.com>
2018-07-02 13:38:29 +0800

    krename: 20170610 -> 5.0.0

--- pkgs/applications/misc/krename/default.nix
+++ pkgs/applications/misc/krename/default.nix
@@ -1,5 +1,5 @@
 {
-  mkDerivation, fetchFromGitHub, lib,
+  mkDerivation, fetchurl, lib,
   extra-cmake-modules, kdoctools, wrapGAppsHook,
   kconfig, kinit, kjsembed, taglib, exiv2, podofo,
   kcrash
@@ -7,15 +7,14 @@
 
 let
   pname = "krename";
-  version = "20170610";
+  version = "5.0.0";
+
 in mkDerivation rec {
   name = "${pname}-${version}";
 
-  src = fetchFromGitHub {
-    owner  = "KDE";
-    repo   = "krename";
-    rev    = "18000edfec52de0b417d575e14eb078b4bd7b2f3";
-    sha256 = "0hsrlfrbi42jqqnkcz682c6yrfi3xpl299672knj22074wr6sv0j";
+  src = fetchurl {
+    url = "mirror://kde/stable/${pname}/${version}/src/${name}.tar.xz";
+    sha256 = "136j1dkqrhv458rjh5v3vzjhvq6dhz7k79zk6mmx8zvqacc7cq8a";
   };
 
   meta = with lib; {
