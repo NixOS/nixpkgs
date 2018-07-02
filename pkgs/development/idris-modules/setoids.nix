d99bf66282f7c991b7f693fe4743fdd1d97943c4
Silvan Mosberger <infinisil@icloud.com>
2018-07-02 05:22:31 +0200

    idrisPackages.setoids: 2017-03-13 -> 2018-06-18, fix build

--- pkgs/development/idris-modules/setoids.nix
+++ pkgs/development/idris-modules/setoids.nix
@@ -1,16 +1,19 @@
 { build-idris-package
 , fetchFromGitHub
+, contrib
 , lib
 }:
 build-idris-package  {
   name = "setoids";
-  version = "2017-03-13";
+  version = "2018-06-18";
+
+  idrisDeps = [ contrib ];
 
   src = fetchFromGitHub {
     owner = "danilkolikov";
     repo = "setoids";
-    rev = "a50cfc010cb4321cc9b7988c0a4f387d83d34839";
-    sha256 = "0q0h2qj9vylkm16h70l78l2p5xjkx4qmg2a2ixfl8vq8b1zm8gch";
+    rev = "41b4af3b1a537d9471107a639ad77c7abee2de18";
+    sha256 = "0fl1g59s16vnrdnplps5ncv27j7a93nxp9cmqp2iavjxlzlzin1v";
   };
 
   meta = {
