6c6e352b7f461f65c61b5eced1e350823d672a60
Ivan Jager <aij+git@mrph.org>
2018-07-03 12:22:44 -0500

    flow: 0.72.0 -> 0.75.0

--- pkgs/development/tools/analysis/flow/default.nix
+++ pkgs/development/tools/analysis/flow/default.nix
@@ -4,15 +4,22 @@
 with lib;
 
 stdenv.mkDerivation rec {
-  version = "0.72.0";
+  version = "0.75.0";
   name = "flow-${version}";
 
   src = fetchFromGitHub {
     owner = "facebook";
     repo = "flow";
     rev = "v${version}";
-    sha256 = "1lfnl1crwkygzbv3l85n30dqsfz627xrnmn4z3zxh7lazir5h8b8";
+    sha256 = "0xrcjjk16w6anpy58qa4la1jyfjs0xg5xkp58slhai996wqif24k";
   };
+
+  # lwt.log is being split out into a separate package, so this can be
+  # removed once nixpkgs is updated.
+  # See https://github.com/ocsigen/lwt/issues/453#issuecomment-352897664
+  postPatch = ''
+    substituteInPlace Makefile --replace lwt_log lwt.log
+  '';
 
   installPhase = ''
     mkdir -p $out/bin
