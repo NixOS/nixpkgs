a4d56d0635fc372288b405c5d7d501a60d1a2512
Tim Steinbach <tim@nequissimus.com>
2018-07-03 08:35:04 -0400

    linux-hardened: Adjust config for 4.17.4

--- pkgs/os-specific/linux/kernel/hardened-config.nix
+++ pkgs/os-specific/linux/kernel/hardened-config.nix
@@ -28,7 +28,9 @@ ${optionalString (stdenv.platform.kernelArch == "x86_64") ''
   # Reduce attack surface by disabling various emulations
   IA32_EMULATION n
   X86_X32 n
-  MODIFY_LDT_SYSCALL? n
+  ${optionalString (versionOlder version "4.17") ''
+    MODIFY_LDT_SYSCALL? n
+  ''}
 
   VMAP_STACK y # Catch kernel stack overflows
 
