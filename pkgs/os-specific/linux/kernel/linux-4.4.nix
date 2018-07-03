923fd70fe80a88398deee5992784a619e4fcbc8b
Tim Steinbach <tim@nequissimus.com>
2018-07-03 08:33:21 -0400

    linux: 4.4.138 -> 4.4.139
    (cherry picked from commit e7270cbf16caa61b6bb51c9b7e8dc228a6f6b8ab)

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.138";
+  version = "4.4.139";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1vn45hvwk49cfm283yg4j76k7dnn351rg5zy28z3bfp02x7cdyg6";
+    sha256 = "0igdsv9ihblmxfsgj646xac5n2bdawmwsr9hwyz6yjld43a5aq5n";
   };
 } // (args.argsOverride or {}))
