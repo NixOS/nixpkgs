6e98661a8939c090ac49aa1dfc3c9b10d23b8c47
R. RyanTM <ryantm-bot@ryantm.com>
2018-07-03 14:25:20 -0700

    you-get: 0.4.1077 -> 0.4.1099 (#42942)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/you-get/versions.
    
    These checks were done:
    
    - built on NixOS
    - /nix/store/fbylh0d2jn5306sn6grzn15c5cb5wi7b-you-get-0.4.1099/bin/.you-get-wrapped passed the binary check.
    - /nix/store/fbylh0d2jn5306sn6grzn15c5cb5wi7b-you-get-0.4.1099/bin/you-get passed the binary check.
    - 2 of 2 passed binary check by having a zero exit code.
    - 0 of 2 passed binary check by having the new version present in output.
    - found 0.4.1099 with grep in /nix/store/fbylh0d2jn5306sn6grzn15c5cb5wi7b-you-get-0.4.1099
    - directory tree listing: https://gist.github.com/10a8076f36d5cef1644b16df703b91bd
    - du listing: https://gist.github.com/bff762c6ef186599bdcac2f64b255ccf

--- pkgs/tools/misc/you-get/default.nix
+++ pkgs/tools/misc/you-get/default.nix
@@ -2,7 +2,7 @@
 
 buildPythonApplication rec {
   pname = "you-get";
-  version = "0.4.1077";
+  version = "0.4.1099";
 
   # Tests aren't packaged, but they all hit the real network so
   # probably aren't suitable for a build environment anyway.
@@ -10,7 +10,7 @@ buildPythonApplication rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1qf4n1zgbr533a0qbxp8548f25jwqdwfl42gi5dyfhm4f1y1jk1a";
+    sha256 = "1z9zhxv2vgbb20hgjiksyqm2zglwq71azbp27na1779hbqc467ik";
   };
 
   meta = with stdenv.lib; {
