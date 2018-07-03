715f9b84ff5e83d79fbad1b58d02ccde9f82fe4a
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2018-07-03 23:04:01 +0900

    gauche: 0.9.5 -> 0.9.6
    - Support mbedtls
    - UTF-8 encoding is default

--- pkgs/development/interpreters/gauche/default.nix
+++ pkgs/development/interpreters/gauche/default.nix
@@ -1,23 +1,29 @@
-{ stdenv, fetchurl, pkgconfig, texinfo, libiconv, gdbm, openssl, zlib }:
+{ stdenv, fetchurl, pkgconfig, texinfo, libiconv, gdbm, openssl, zlib
+, mbedtls, cacert
+}:
 
 stdenv.mkDerivation rec {
   name = "gauche-${version}";
-  version = "0.9.5";
+  version = "0.9.6";
 
   src = fetchurl {
     url = "mirror://sourceforge/gauche/Gauche-${version}.tgz";
-    sha256 = "0g77nik15whm5frxb7l0pwzd95qlq949dym5pn5p04p17lhm72jc";
+    sha256 = "1bwwwvyxsrp2a4cfib6hn0hcgwzmp2znylm088w09f331miji2fd";
   };
 
   nativeBuildInputs = [ pkgconfig texinfo ];
 
-  buildInputs = [ libiconv gdbm openssl zlib ];
+  buildInputs = [ libiconv gdbm openssl zlib mbedtls cacert ];
+
+  postPatch = ''
+    patchShebangs .
+  '';
 
   configureFlags = [
-    "--enable-multibyte=utf-8"
     "--with-iconv=${libiconv}"
     "--with-dbm=gdbm"
     "--with-zlib=${zlib}"
+    "--with-ca-bundle=$SSL_CERT_FILE"
     # TODO: Enable slib
     #       Current slib in nixpkgs is specialized to Guile
     # "--with-slib=${slibGuile}/lib/slib"
