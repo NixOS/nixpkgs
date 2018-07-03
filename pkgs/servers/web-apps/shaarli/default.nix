a939f97baa7032b03ade33ecc33b2d13804be977
R. RyanTM <ryantm+bot@ryantm.com>
2018-07-03 14:58:19 -0700

    shaarli: 0.9.6 -> 0.9.7
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/shaarli/versions.
    
    These checks were done:
    
    - built on NixOS
    
    - 0 of 0 passed binary check by having a zero exit code.
    - 0 of 0 passed binary check by having the new version present in output.
    - found 0.9.7 with grep in /nix/store/qb0qnz5g71qcigji532rz8fb3fmds607-shaarli-0.9.7
    - directory tree listing: https://gist.github.com/d5b4de9629307cb77b06763e100b9ffd
    - du listing: https://gist.github.com/db083d0d397fa00514a7de0a57879f39

--- pkgs/servers/web-apps/shaarli/default.nix
+++ pkgs/servers/web-apps/shaarli/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "shaarli-${version}";
-  version = "0.9.6";
+  version = "0.9.7";
 
   src = fetchurl {
     url = "https://github.com/shaarli/Shaarli/releases/download/v${version}/shaarli-v${version}-full.tar.gz";
-    sha256 = "0vi59988bjxb1cyifh0fynpxa7l6cl0v57hrxima5c9iid10pw54";
+    sha256 = "191nnk4p6cpbljij1a30mpidqdvcwn1x6ndb4lgkqwbpnh86q57l";
   };
 
   outputs = [ "out" "doc" ];
