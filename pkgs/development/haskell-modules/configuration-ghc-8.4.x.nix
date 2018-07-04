0f2099aa938c5f6b102c908e6b3902945b1e4964
Peter Simons <simons@cryp.to>
2018-07-04 13:37:07 +0200

    haskell-dhall: update override for the new version that's used by ghc 8.4.x

--- pkgs/development/haskell-modules/configuration-ghc-8.4.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.4.x.nix
@@ -395,8 +395,9 @@ self: super: {
   # Older versions don't compile.
   base-compat = self.base-compat_0_10_4;
   brick = self.brick_0_37_1;
-  dhall = self.dhall_1_14_0;
-  dhall_1_13_0 = doJailbreak super.dhall_1_14_0;  # support ansi-terminal 0.8.x
+  dhall = self.dhall_1_15_0;
+  # This is probably obsolete:
+  # dhall_1_15_0 = doJailbreak super.dhall_1_15_0;  # support ansi-terminal 0.8.x
   HaTeX = self.HaTeX_3_19_0_0;
   hpack = self.hpack_0_28_2;
   hspec = dontCheck super.hspec_2_5_4;
