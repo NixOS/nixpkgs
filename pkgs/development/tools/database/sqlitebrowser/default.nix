4f15883ada699e51b35cf9f0cc24373dc52974e3
Maximilian Bosch <maximilian@mbosch.me>
2018-07-04 23:50:56 +0200

    sqlitebrowser: fix build
    This patch slightly cleans up the `sqlitebrowser` derivation and
    switches from a cmake-based build to a qmake build using
    `qmakeConfigurePhase` and the corresponding hook.
    
    QMake is far more suitable for QT-based builds (and suggested e.g. here
    https://github.com/NixOS/nixpkgs/issues/42320#issuecomment-401791602),
    furthermore the CMake build suffers from issues like missing commands
    including `qt5_use_modules`.
    
    Fixes #42320
    See https://hydra.nixos.org/build/76952038

--- pkgs/development/tools/database/sqlitebrowser/default.nix
+++ pkgs/development/tools/database/sqlitebrowser/default.nix
@@ -1,5 +1,6 @@
-{ mkDerivation, lib, fetchFromGitHub, cmake, antlr
-, qtbase, qttools, qscintilla, sqlite }:
+{ mkDerivation, lib, fetchFromGitHub, qmake, antlr, qscintilla
+, qtbase, qttools, sqlite
+}:
 
 mkDerivation rec {
   version = "3.10.1";
@@ -12,11 +13,20 @@ mkDerivation rec {
     sha256 = "1brzam8yv6sbdmbqsp7vglhd6wlx49g2ap8llr271zrkld4k3kar";
   };
 
+  patches = [
+    ./no-custom-qscintilla.patch
+  ];
+
   buildInputs = [ qtbase qscintilla sqlite ];
 
-  nativeBuildInputs = [ cmake antlr qttools ];
+  nativeBuildInputs = [ qmake antlr qttools ];
 
   enableParallelBuilding = true;
+
+  qmakeFlags = [
+    "QMAKE_RELEASE=${qttools.dev}/bin/lrelease"
+    "QMAKE_LUPDATE=${qttools.dev}/bin/lupdate"
+  ];
 
   # We have to patch out Test and PrintSupport to make this work with Qt 5.9
   # It can go when the application supports 5.9
@@ -29,11 +39,15 @@ mkDerivation rec {
       --replace PrintSupport ""
   '';
 
+  preInstall = ''
+    substituteInPlace "src/src.pro" --replace "/usr/local" $out
+  '';
+
   meta = with lib; {
     description = "DB Browser for SQLite";
     homepage = http://sqlitebrowser.org/;
     license = licenses.gpl3;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ ma27 ];
     platforms = platforms.linux; # can only test on linux
   };
 }
