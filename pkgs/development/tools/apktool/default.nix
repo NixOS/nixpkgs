cb6f5bdd4ee56a3b3176fe4968ce8b851a40fd05
Matthew Bauer <mjbauer95@gmail.com>
2018-07-04 23:51:07 -0400

    apktool: use wrapCommand

--- pkgs/development/tools/apktool/default.nix
+++ pkgs/development/tools/apktool/default.nix
@@ -1,32 +1,20 @@
-{ stdenv, fetchurl, makeWrapper, jre, buildTools }:
+{ wrapCommand, fetchurl, jre, buildTools, lib }:
 
-stdenv.mkDerivation rec {
-  name = "apktool-${version}";
-  version = "2.3.3";
-
-  src = fetchurl {
+let
+  jar = fetchurl {
     urls = [
       "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_${version}.jar"
       "https://github.com/iBotPeaches/Apktool/releases/download/v${version}/apktool_${version}.jar"
     ];
     sha256 = "1wjpn1wxg8fid2mch5ili35xqvasa3pk8h1xaiygw5idpxh3cm0f";
   };
-
-  phases = [ "installPhase" ];
-
-  nativeBuildInputs = [ makeWrapper ];
-
-  sourceRoot = ".";
-
-  installPhase = ''
-    install -D ${src} "$out/libexec/apktool/apktool.jar"
-    mkdir -p "$out/bin"
-    makeWrapper "${jre}/bin/java" "$out/bin/apktool" \
-        --add-flags "-jar $out/libexec/apktool/apktool.jar" \
-        --prefix PATH : "${buildTools}/build-tools/25.0.1/"
-  '';
-
-  meta = with stdenv.lib; {
+  version = "2.3.3";
+in wrapCommand "apktool" {
+  inherit version;
+  executable = "${jre}/bin/java";
+  makeWrapperArgs = ["--add-flags -jar" "--add-flags ${jar}"
+                     "--prefix PATH : ${buildTools}/build-tools/25.0.1"];
+  meta = with lib; {
     description = "A tool for reverse engineering Android apk files";
     homepage    = https://ibotpeaches.github.io/Apktool/;
     license     = licenses.asl20;
