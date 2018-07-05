18989b108519c4bbc2ac62aab0589a9765d11fd7
Matthew Bauer <mjbauer95@gmail.com>
2018-07-04 23:56:01 -0400

    emv: use wrapCommand

--- pkgs/tools/misc/emv/default.nix
+++ pkgs/tools/misc/emv/default.nix
@@ -1,26 +1,18 @@
-{ stdenv, fetchurl }:
+{ wrapCommand, lib, bash, fetchurl }:
 
-stdenv.mkDerivation rec {
-  name = "emv-${version}";
-  version = "1.95";
-
+let
   src = fetchurl {
     url = "http://www.i0i0.de/toolchest/emv";
     sha256 = "7e0e12afa45ef5ed8025e5f2c6deea0ff5f512644a721f7b1b95b63406a8f7ce";
   };
-
-  phases = [ "installPhase" ];
-
-  installPhase = ''
-    mkdir -pv $out/bin
-    cp $src $out/bin/emv
-    chmod +x $out/bin/emv
-  '';
-
-  meta = {
+in wrapCommand "emv" {
+  version = "1.95";
+  executable = "${bash}/bin/bash";
+  makeWrapperArgs = [ "--add-flags ${src}" ];
+  meta = with lib; {
     homepage = http://www.i0i0.de/toolchest/emv;
     description = "Editor Move: Rename files with your favourite text editor";
-    license = stdenv.lib.licenses.publicDomain;
-    platforms = stdenv.lib.platforms.unix;
+    license = licenses.publicDomain;
+    platforms = platforms.unix;
   };
 }
