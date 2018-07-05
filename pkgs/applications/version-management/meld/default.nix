8208a79d5ce9e2d740622983c741510a769fbf04
Jan Tojnar <jtojnar@gmail.com>
2018-07-06 00:37:15 +0200

    meld: 3.18.1 → 3.18.2

--- pkgs/applications/version-management/meld/default.nix
+++ pkgs/applications/version-management/meld/default.nix
@@ -5,14 +5,14 @@
 
 let
   pname = "meld";
-  version = "3.18.1";
+  version = "3.18.2";
   inherit (python3Packages) python buildPythonApplication pycairo pygobject3;
 in buildPythonApplication rec {
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${gnome3.versionBranch version}/${name}.tar.xz";
-    sha256 = "0yg8i1ff3rsavsaxbnd6dxmfsiyv49kk8rv5wqnyhf2zmfp8rxis";
+    sha256 = "109px6phfizi2jqrc7d7k7j6nvmanbfp5lykqfrk2sky77sand0r";
   };
 
   buildInputs = [
