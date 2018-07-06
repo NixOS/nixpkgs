fec309aee46c7faa53fd05fcab5e2dcc1bd03f05
Matthew Bauer <mjbauer95@gmail.com>
2018-07-06 11:54:21 -0400

    .dir-locals.el: init
    This adds some initial values for .dir-locals.el. Mainly this is
    useful for using bug-reference-mode.
    
    So if you have bug-reference-mode enabled -
    
    > M-x bug-reference-mode
    
    You will see as clickable text like this:
    
      Fixes #15
    
      (#12)
    
      Closed #1252
    
      issue #1

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (bug-reference-bug-regexp . "\\(\\(?:[Ii]ssue \\|[Ff]ixe[ds] \\|[Rr]esolve[ds]? \\|[Cc]lose[ds]? \\|[Pp]\\(?:ull [Rr]equest\\|[Rr]\\) \\|(\\)#\\([0-9]+\\))?\\)")
+  (bug-reference-url-format . "https://github.com/NixOS/nixpkgs/issues/%s"))
+ (nix-mode
+  (tab-width . 2)))
