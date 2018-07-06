e13f46b3c2fd61e006caae761f4a803afd4cde31
Zer0- <phil.volguine@gmail.com>
2018-07-05 22:16:27 -0400

    MusE: add new package at version 3.0.2 (#42544)
    * muse: init at version 3.0.2
    
    Added MusE; an application for working with midi. Allowing recording
    from midi instruments, playback and editing of midi files.
    
    * muse: remove unnecessary dependencies
    
    -builds and works just fine without listing these
    
    * Use fetchFromGitHub and add sha256

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21152,6 +21152,8 @@ with pkgs;
 
   mupen64plus = callPackage ../misc/emulators/mupen64plus { };
 
+  muse = callPackage ../applications/audio/muse { };
+
   mynewt-newt = callPackage ../tools/package-management/mynewt-newt { };
 
   inherit (callPackages ../tools/package-management/nix {
