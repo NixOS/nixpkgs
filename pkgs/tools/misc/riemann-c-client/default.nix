6519ea5671771a31ef2e4a53d6169053add6ef3d
R. RyanTM <ryantm-bot@ryantm.com>
2018-07-06 11:51:40 -0700

    riemann_c_client: 1.10.1 -> 1.10.2 (#42961)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/riemann-c-client/versions.
    
    These checks were done:
    
    - built on NixOS
    - /nix/store/hxh4dj91473d8wlyr2kaqf5n6hcn8z4b-riemann-c-client-1.10.2/bin/riemann-client passed the binary check.
    - 1 of 1 passed binary check by having a zero exit code.
    - 1 of 1 passed binary check by having the new version present in output.
    - found 1.10.2 with grep in /nix/store/hxh4dj91473d8wlyr2kaqf5n6hcn8z4b-riemann-c-client-1.10.2
    - directory tree listing: https://gist.github.com/64c27a461ed26c33098195c79327e87c
    - du listing: https://gist.github.com/7bdbbd558a2ce067e9e413f0bbba1185

--- pkgs/tools/misc/riemann-c-client/default.nix
+++ pkgs/tools/misc/riemann-c-client/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, file , protobufc }:
 
 stdenv.mkDerivation rec {
-  name = "riemann-c-client-1.10.1";
+  name = "riemann-c-client-1.10.2";
 
   src = fetchFromGitHub {
     owner = "algernon";
     repo = "riemann-c-client";
     rev = "${name}";
-    sha256 = "1pzyngvj9aq1w2185qpg6rxrjn406pnpy40bnh4c21fn4ql5kk9p";
+    sha256 = "185wn6fqgrs16f9c0lkzw14477wmkgandz86h4miw7cgi7ki4l5i";
   };
 
   nativeBuildInputs = [ autoreconfHook pkgconfig ];
