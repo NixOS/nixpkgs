f5cecc1f0993ee4e1b208def5eab5f6daf2dd18d
R. RyanTM <ryantm-bot@ryantm.com>
2018-07-08 15:46:17 -0700

    libivykis: 0.42.1 -> 0.42.2 (#42991)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/libivykis/versions.
    
    These checks were done:
    
    - built on NixOS
    
    - 0 of 0 passed binary check by having a zero exit code.
    - 0 of 0 passed binary check by having the new version present in output.
    - found 0.42.2 with grep in /nix/store/mpzbl2ic44k0zb14i6vh1jnfgjw991fm-libivykis-0.42.2
    - directory tree listing: https://gist.github.com/e262298d600bcbe234280fd968536833
    - du listing: https://gist.github.com/b1a6d5fe802c7441fc1f194f04f6b1b5

--- pkgs/development/libraries/libivykis/default.nix
+++ pkgs/development/libraries/libivykis/default.nix
@@ -3,11 +3,11 @@
 stdenv.mkDerivation rec {
   name = "libivykis-${version}";
 
-  version = "0.42.1";
+  version = "0.42.2";
 
   src = fetchurl {
     url = "mirror://sourceforge/libivykis/${version}/ivykis-${version}.tar.gz";
-    sha256 = "0c90cfpxipw2m8i3ajr7vy7lb8gvcz2kh5n8sd542zphr4na8whq";
+    sha256 = "14vb613j4xas70wr7g5z9c9z871xhayd4zliywwf88myd41jcsw8";
   };
 
   nativeBuildInputs = [ autoreconfHook pkgconfig ];
