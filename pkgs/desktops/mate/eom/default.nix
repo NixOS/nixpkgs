eccf998393166fe927d6baad0cfe92edacaa029f
R. RyanTM <ryantm-bot@ryantm.com>
2018-07-08 14:35:03 -0700

    mate.eom: 1.20.1 -> 1.21.0 (#43028)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/eom/versions.
    
    These checks were done:
    
    - built on NixOS
    - /nix/store/hk60glvzvq8psbzxj585dy6q6dhyl0ig-eom-1.21.0/bin/eom passed the binary check.
    - /nix/store/hk60glvzvq8psbzxj585dy6q6dhyl0ig-eom-1.21.0/bin/.eom-wrapped passed the binary check.
    - 2 of 2 passed binary check by having a zero exit code.
    - 0 of 2 passed binary check by having the new version present in output.
    - found 1.21.0 with grep in /nix/store/hk60glvzvq8psbzxj585dy6q6dhyl0ig-eom-1.21.0
    - directory tree listing: https://gist.github.com/c4f2ea89bae8c82147f2b52c14253958
    - du listing: https://gist.github.com/bd1cfd9e324adb177aa55542d4ac27ac

--- pkgs/desktops/mate/eom/default.nix
+++ pkgs/desktops/mate/eom/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "eom-${version}";
-  version = "1.20.1";
+  version = "1.21.0";
 
   src = fetchurl {
     url = "http://pub.mate-desktop.org/releases/${mate.getRelease version}/${name}.tar.xz";
-    sha256 = "0z9l96j0q637hw2mkcc2w737acl7g2c5brgrlk4h73hjamfmsdrm";
+    sha256 = "165n314hxapq8glzlv7vs5x44z13wjmpysl5p7q3ckrsxy5pczww";
   };
 
   nativeBuildInputs = [
