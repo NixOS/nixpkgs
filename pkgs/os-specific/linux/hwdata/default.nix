e93b7ec9edaba36fa8d46d626cbcde357f58e85a
R. RyanTM <ryantm+bot@ryantm.com>
2018-07-08 00:41:35 -0700

    hwdata: 0.312 -> 0.313
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/hwdata/versions.
    
    <details><summary>Version release notes (from GitHub)</summary>
    </details>
    
    These checks were done:
    
    - built on NixOS
    
    - 0 of 0 passed binary check by having a zero exit code.
    - 0 of 0 passed binary check by having the new version present in output.
    - found 0.313 with grep in /nix/store/q7cj4jh3pf1g9wlhsmz8034mi6ljink0-hwdata-0.313
    - directory tree listing: https://gist.github.com/f132a672f7d5f48add649a0fabcdfd67
    - du listing: https://gist.github.com/750425ee97475d74680486f11bebc6a5

--- pkgs/os-specific/linux/hwdata/default.nix
+++ pkgs/os-specific/linux/hwdata/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "hwdata-${version}";
-  version = "0.312";
+  version = "0.313";
 
   src = fetchurl {
-    url = "https://github.com/vcrhonek/hwdata/archive/v0.312.tar.gz";
-    sha256 = "04dbxfn40b8vyw49qpkslv20akbqm5hwl3cndmqacp6cik1l0gai";
+    url = "https://github.com/vcrhonek/hwdata/archive/v0.313.tar.gz";
+    sha256 = "0x0qk2cim1mv8cl8h8rwqn8mbbs43j04rn06m81b531i182zii17";
   };
 
   preConfigure = "patchShebangs ./configure";
