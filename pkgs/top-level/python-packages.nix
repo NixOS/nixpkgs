eece903626937070aadca7d712274f90f18e0edf
Michiel Leenaars <ml.software@leenaa.rs>
2018-07-08 14:21:54 +0200

    pythonPackages.authres: init at 1.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -195,6 +195,8 @@ in {
     inherit (pkgs) augeas;
   };
 
+  authres = callPackage ../development/python-modules/authres { };
+
   autograd = callPackage ../development/python-modules/autograd { };
 
   automat = callPackage ../development/python-modules/automat { };
