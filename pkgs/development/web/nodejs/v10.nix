09abbfd7fe5987f0b7fa576f4ade5d084fb9a012
Wout Mertens <Wout.Mertens@gmail.com>
2018-07-09 11:47:22 +0200

    NodeJS: 10.5.0 -> 10.6.0

--- pkgs/development/web/nodejs/v10.nix
+++ pkgs/development/web/nodejs/v10.nix
@@ -5,7 +5,7 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "10.5.0";
-    sha256 = "1g1kdcrhahdsrkazfl9wj25abgjvkncgwwcm2ppgj3avfi1wam3v";
+    version = "10.6.0";
+    sha256 = "0sxkhjkcqpyf5gkv1nw93w2hmkglwy2qzfsdy2hgx3z1pvxfn1w0";
     patches = lib.optionals stdenv.isDarwin [ ./no-xcode-v7.patch ./no-xcodebuild.patch ];
   }
