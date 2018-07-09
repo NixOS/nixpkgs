649861a1284a5e7fd78c597ebac62569dd646f68
Pascal Wittmann <mail@pascal-wittmann.de>
2018-07-09 19:31:15 +0200

    calibre: 3.27.0 -> 3.27.1

--- pkgs/applications/misc/calibre/default.nix
+++ pkgs/applications/misc/calibre/default.nix
@@ -5,12 +5,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "3.27.0";
+  version = "3.27.1";
   name = "calibre-${version}";
 
   src = fetchurl {
     url = "https://download.calibre-ebook.com/${version}/${name}.tar.xz";
-    sha256 = "1367fhgqynh6ic318wppv4m7343ghvdgazm4qq2jqli8y1gxdfbz";
+    sha256 = "0hmdlnwrfql2b675xmjf50hnnrrkv5jbky0ssdpyfjfa5vxya2bh";
   };
 
   patches = [
