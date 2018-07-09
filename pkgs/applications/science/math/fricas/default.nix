aa71744f7542ce693269fb77a2b86270cedfb575
R. RyanTM <ryantm-bot@ryantm.com>
2018-07-09 07:02:58 -0700

    fricas: 1.3.3 -> 1.3.4 (#43023)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/fricas/versions.
    
    These checks were done:
    
    - built on NixOS
    - /nix/store/zbmbsjm8mg6i7s6fm3zas1bndg24w91x-fricas-1.3.4/bin/fricas passed the binary check.
    - /nix/store/zbmbsjm8mg6i7s6fm3zas1bndg24w91x-fricas-1.3.4/bin/efricas passed the binary check.
    - 2 of 2 passed binary check by having a zero exit code.
    - 0 of 2 passed binary check by having the new version present in output.
    - found 1.3.4 with grep in /nix/store/zbmbsjm8mg6i7s6fm3zas1bndg24w91x-fricas-1.3.4
    - directory tree listing: https://gist.github.com/75d751d9733f6b0a7b582ab838a71963
    - du listing: https://gist.github.com/4c6fe5f342430a2b36568da3229d3dc4

--- pkgs/applications/science/math/fricas/default.nix
+++ pkgs/applications/science/math/fricas/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, sbcl, libX11, libXpm, libICE, libSM, libXt, libXau, libXdmcp }:
 
 let
-  version = "1.3.3";
+  version = "1.3.4";
   name = "fricas-" + version;
 in
 stdenv.mkDerivation {
@@ -9,7 +9,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url    = "http://sourceforge.net/projects/fricas/files/fricas/${version}/${name}-full.tar.bz2";
-    sha256 = "1avp9mbl5yn192c7kz5c2d18k33hay9lwii363b0v5hj3qgq2hhl";
+    sha256 = "156k9az1623y5808j845c56z2nvvdrm48dzg1v0ivpplyl7vp57x";
   };
 
   buildInputs = [ sbcl libX11 libXpm libICE libSM libXt libXau libXdmcp ];
