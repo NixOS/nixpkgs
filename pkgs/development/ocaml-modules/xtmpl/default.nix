1e9d9b50f804819bd0644612da59289c1287c481
Vincent Laporte <vbgl@users.noreply.github.com>
2018-07-10 23:13:01 +0000

    ocamlPackages.xtmpl: 0.16.0 -> 0.17.0 (#43304)

--- pkgs/development/ocaml-modules/xtmpl/default.nix
+++ pkgs/development/ocaml-modules/xtmpl/default.nix
@@ -1,29 +1,30 @@
-{ stdenv, fetchFromGitHub, ocaml, findlib, uutf, sedlex, ppx_tools, js_of_ocaml
-, js_of_ocaml-camlp4, camlp4, re }:
+{ stdenv, fetchFromGitLab, ocaml, findlib, iri, ppx_tools, js_of_ocaml
+, js_of_ocaml-ppx, re }:
 
 if stdenv.lib.versionOlder ocaml.version "4.03"
 then throw "xtmpl not supported for ocaml ${ocaml.version}"
 else
 stdenv.mkDerivation rec {
-  name = "xtmpl-${version}";
-  version = "0.16.0";
-  src = fetchFromGitHub {
+  name = "ocaml${ocaml.version}-xtmpl-${version}";
+  version = "0.17.0";
+  src = fetchFromGitLab {
+    domain = "framagit.org";
     owner = "zoggy";
     repo = "xtmpl";
     rev = version;
-    sha256 = "1dj5b4b266y4d8q3v1g0xsivz4vkhj0gi0jis37w84xcnlgiik8k";
+    sha256 = "1hq6y4rhz958q40145k4av8hx8jyvspg78xf741samd7vc3jd221";
   };
 
-  buildInputs = [ ocaml findlib ppx_tools js_of_ocaml js_of_ocaml-camlp4 camlp4 ];
-  propagatedBuildInputs = [ sedlex uutf re ];
+  buildInputs = [ ocaml findlib ppx_tools js_of_ocaml js_of_ocaml-ppx ];
+  propagatedBuildInputs = [ iri re ];
 
   createFindlibDestdir = true;
 
   dontStrip = true;
 
   meta = with stdenv.lib; {
-    description = "Xml template library for OCaml";
-    homepage = https://zoggy.github.io/xtmpl/;
+    description = "XML templating library for OCaml";
+    homepage = "https://www.good-eris.net/xtmpl/";
     license = licenses.lgpl3;
     platforms = ocaml.meta.platforms or [];
     maintainers = with maintainers; [ regnat ];
