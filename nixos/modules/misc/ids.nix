38f2a3efbff43975cc2391011ebb912ee89871e1
Okina Matara <okinan@chiru.no>
2018-07-10 18:10:02 -0500

    nixos/hydron: init

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -321,6 +321,7 @@
       hdfs = 295;
       mapred = 296;
       hadoop = 297;
+      hydron = 298;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -602,6 +603,7 @@
       hdfs = 295;
       mapred = 296;
       hadoop = 297;
+      hydron = 298;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
