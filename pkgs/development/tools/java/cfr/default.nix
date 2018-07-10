1f7689f189418c29247a4f362138b767fd67de7c
R. RyanTM <ryantm-bot@ryantm.com>
2018-07-10 16:07:26 -0700

    cfr: 0.130 -> 0.132 (#43215)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/cfr/versions.
    
    These checks were done:
    
    - built on NixOS
    - /nix/store/6yymrnh077nbg02mlpyiplqi0g8h3pwx-cfr-0.132/bin/cfr passed the binary check.
    - 1 of 1 passed binary check by having a zero exit code.
    - 0 of 1 passed binary check by having the new version present in output.
    - found 0.132 with grep in /nix/store/6yymrnh077nbg02mlpyiplqi0g8h3pwx-cfr-0.132
    - directory tree listing: https://gist.github.com/03397074385526106bce1680bfe6b8c9
    - du listing: https://gist.github.com/c5ff1f69de44790159ee04282b1b3102

--- pkgs/development/tools/java/cfr/default.nix
+++ pkgs/development/tools/java/cfr/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "cfr-${version}";
-  version = "0.130";
+  version = "0.132";
 
   src = fetchurl {
     url = "http://www.benf.org/other/cfr/cfr_${version}.jar";
-    sha256 = "0zy63jxl05f0wm9s344y846hzqn0zk34w6qpbf48a98si5ciwsmn";
+    sha256 = "1fpmd5v3x91lw6jc7x997ic2kklj449fndyg14zvgxawhdkic2z1";
   };
 
   buildInputs = [ makeWrapper ];
