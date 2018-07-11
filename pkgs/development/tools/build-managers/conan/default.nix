b4d9f481f526382e4808e1a83255cc7bbebc52a1
R. RyanTM <ryantm-bot@ryantm.com>
2018-07-11 09:34:17 -0700

    conan: 1.5.1 -> 1.5.2 (#43354)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools. This update was made based on information from https://repology.org/metapackage/conan/versions.

--- pkgs/development/tools/build-managers/conan/default.nix
+++ pkgs/development/tools/build-managers/conan/default.nix
@@ -20,12 +20,12 @@ let newPython = python.override {
 };
 
 in newPython.pkgs.buildPythonApplication rec {
-  version = "1.5.1";
+  version = "1.5.2";
   pname = "conan";
 
   src = newPython.pkgs.fetchPypi {
     inherit pname version;
-    sha256 = "1b0iym41fpgv8vwbyys5hrh87pvh3hy2m9c01ybm72z2ki2pwxim";
+    sha256 = "0r5ymq27j60py1fb396zshq7z6adda34a857lwrj3k8hqhs0ihpp";
   };
 
   postPatch = ''
