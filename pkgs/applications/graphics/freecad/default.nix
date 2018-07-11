e791a3c014858dc160fc205d6232a1a2f5c3291f
Will Dietz <w@wdtz.org>
2018-07-10 20:32:15 -0500

    freecad: fix by using python-enabled boost

--- pkgs/applications/graphics/freecad/default.nix
+++ pkgs/applications/graphics/freecad/default.nix
@@ -7,6 +7,11 @@ assert mpi != null;
 
 let
   pythonPackages = python27Packages;
+  boost_py = boost.override {
+    inherit (pythonPackages) python;
+    enablePython = true;
+  };
+
 in stdenv.mkDerivation rec {
   name = "freecad-${version}";
   version = "0.17";
@@ -19,7 +24,7 @@ in stdenv.mkDerivation rec {
   buildInputs = with pythonPackages; [ cmake coin3d xercesc ode eigen qt4 opencascade gts
     zlib  swig gfortran soqt libf2c makeWrapper  mpi vtk hdf5 medfile
   ] ++ (with pythonPackages; [
-    matplotlib pycollada pyside pysideShiboken pysideTools pivy python boost
+    matplotlib pycollada pyside pysideShiboken pysideTools pivy python boost_py
   ]);
 
   patches = [
