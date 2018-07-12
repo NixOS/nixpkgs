7bcdf6520843a85bdabeae1a3d322a3a30c5e00c
Vincent Laporte <Vincent.Laporte@gmail.com>
2018-07-12 21:26:06 +0000

    flow: use lwt_log

--- pkgs/development/tools/analysis/flow/default.nix
+++ pkgs/development/tools/analysis/flow/default.nix
@@ -1,5 +1,4 @@
-{ stdenv, fetchFromGitHub, lib, ocaml, libelf, cf-private, CoreServices,
-  findlib, camlp4, sedlex, ocamlbuild, lwt_ppx, wtf8, dtoa }:
+{ stdenv, fetchFromGitHub, lib, ocamlPackages, libelf, cf-private, CoreServices }:
 
 with lib;
 
@@ -14,21 +13,15 @@ stdenv.mkDerivation rec {
     sha256 = "0r3yl4m7dhm1h4c431zp8hd2gg6k1d9bwd2371xav5q7hviwmjl6";
   };
 
-  # lwt.log is being split out into a separate package, so this can be
-  # removed once nixpkgs is updated.
-  # See https://github.com/ocsigen/lwt/issues/453#issuecomment-352897664
-  postPatch = ''
-    substituteInPlace Makefile --replace lwt_log lwt.log
-  '';
-
   installPhase = ''
     mkdir -p $out/bin
     cp bin/flow $out/bin/
   '';
 
-  buildInputs = [
-    ocaml libelf findlib camlp4 sedlex ocamlbuild lwt_ppx wtf8 dtoa
-  ] ++ optionals stdenv.isDarwin [ cf-private CoreServices ];
+  buildInputs = [ libelf
+  ] ++ (with ocamlPackages; [
+    ocaml findlib camlp4 sedlex ocamlbuild lwt_ppx lwt_log wtf8 dtoa
+  ]) ++ optionals stdenv.isDarwin [ cf-private CoreServices ];
 
   meta = with stdenv.lib; {
     description = "A static type checker for JavaScript";
