bc05053631991b591242c84d4ff9eafa509ad246
Jörg Thalheim <joerg@thalheim.io>
2018-07-12 07:26:56 -0400

    tig: zsh completion should be named _tig

--- pkgs/applications/version-management/git-and-tools/tig/default.nix
+++ pkgs/applications/version-management/git-and-tools/tig/default.nix
@@ -31,10 +31,8 @@ stdenv.mkDerivation rec {
   installPhase = ''
     make install
     make install-doc
-    mkdir -p $out/etc/bash_completion.d/
-    cp contrib/tig-completion.bash $out/etc/bash_completion.d/
-    mkdir -p $out/share/zsh/site-functions
-    cp contrib/tig-completion.zsh $out/share/zsh/site-functions/
+    install -D contrib/tig-completion.bash $out/etc/bash_completion.d/tig-completion.bash
+    install -D contrib/tig-completion.zsh $out/share/zsh/site-functions/_tig
     cp contrib/vim.tigrc $out/etc/
 
     wrapProgram $out/bin/tig \
