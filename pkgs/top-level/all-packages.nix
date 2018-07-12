4d209dcb153a0679a8c38e2c359ec9d8cf9b61f8
Izorkin <Izorkin@gmail.com>
2018-07-12 16:08:13 +0300

    percona-xtrabackup: 2.4.9 -> 2.4.12

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4364,6 +4364,7 @@ with pkgs;
   pepper = callPackage ../tools/admin/salt/pepper { };
 
   percona-xtrabackup = callPackage ../tools/backup/percona-xtrabackup {
+    stdenv = overrideCC stdenv gcc5;
     boost = boost159;
   };
 
