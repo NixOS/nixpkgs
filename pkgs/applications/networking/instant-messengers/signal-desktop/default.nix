f417ec3b81c4abb6afa03153d1ffdd087444f5e5
Michael Weiss <dev.primeos@gmail.com>
2018-07-13 16:14:40 +0200

    signal-desktop: 1.13.0 -> 1.14.0

--- pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
@@ -40,11 +40,11 @@ let
 
 in stdenv.mkDerivation rec {
   name = "signal-desktop-${version}";
-  version = "1.13.0";
+  version = "1.14.0";
 
   src = fetchurl {
     url = "https://updates.signal.org/desktop/apt/pool/main/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-    sha256 = "1iapkkfqssbjcksgic7i8x0cwp6gwcbbrfxlw7vp6k1cfvjwv9pf";
+    sha256 = "1f76dzm9qq12i4s95c51d9s923n69y8cbg8yz79qjpd6k30j8vkq";
   };
 
   phases = [ "unpackPhase" "installPhase" ];
