5f101b0cb77a779e5f5cc2c5b9b2b18590c55915
Jörg Thalheim <joerg@thalheim.io>
2018-07-13 19:48:12 -0400

    iana-etc: 20180405 -> 20180711

--- pkgs/data/misc/iana-etc/default.nix
+++ pkgs/data/misc/iana-etc/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "iana-etc-${version}";
-  version = "20180405";
+  version = "20180711";
 
   src = fetchurl {
     url = "https://github.com/Mic92/iana-etc/releases/download/${version}/iana-etc-${version}.tar.gz";
-    sha256 = "0x6i95arrc4lcq2il3r05hbnd9vsi87z4yc8s3agkvbj74d6hfhj";
+    sha256 = "0xigkz6pcqx55px7fap7j6p3hz27agv056crbl5pgfcdix7y8z26";
   };
 
   installPhase = ''
