d30b429cf8eb4b940ac598119dc716bf95158cfe
volth <volth@volth.com>
2018-07-14 12:37:27 +0000

    xfce4-panel: fix tzdata path

--- pkgs/desktops/xfce4-13/xfce4-panel/default.nix
+++ pkgs/desktops/xfce4-13/xfce4-panel/default.nix
@@ -1,4 +1,4 @@
-{ mkXfceDerivation, exo, garcon, gtk2, gtk3, libxfce4ui, libxfce4util, libwnck3, xfconf }:
+{ mkXfceDerivation, tzdata, exo, garcon, gtk2, gtk3, libxfce4ui, libxfce4util, libwnck3, xfconf }:
 
 mkXfceDerivation rec {
   category = "xfce";
@@ -8,6 +8,12 @@ mkXfceDerivation rec {
   sha256 = "194pihmg7af4x81nia2fy3h7rls306a7c0bqny9ycqikvi6nmdmn";
 
   buildInputs = [ exo garcon gtk2 gtk3 libxfce4ui libxfce4util libwnck3 xfconf ];
+
+  postPatch = ''
+    substituteInPlace plugins/clock/clock.c \
+       --replace "/usr/share/zoneinfo" "${tzdata}/share/zoneinfo" \
+       --replace "if (!g_file_test (filename, G_FILE_TEST_IS_SYMLINK))" ""
+  '';
 
   meta =  {
     description = "Xfce's panel";
