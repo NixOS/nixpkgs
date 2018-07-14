c9c8678ccc57cfdd660eec2c3160d8c1ec156584
R. RyanTM <ryantm-bot@ryantm.com>
2018-07-14 11:41:22 -0700

    mate.mate-calc: 1.20.2 -> 1.21.0 (#43174)
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools.
    
    This update was made based on information from https://repology.org/metapackage/mate-calc/versions.
    
    These checks were done:
    
    - built on NixOS
    - Warning: no invocation of /nix/store/pj0sfjgfdibchfcjxmw8hqqz6b3733vf-mate-calc-1.21.0/bin/mate-calc had a zero exit code or showed the expected version
    - /nix/store/pj0sfjgfdibchfcjxmw8hqqz6b3733vf-mate-calc-1.21.0/bin/mate-calc-cmd passed the binary check.
    - Warning: no invocation of /nix/store/pj0sfjgfdibchfcjxmw8hqqz6b3733vf-mate-calc-1.21.0/bin/.mate-calculator-wrapped had a zero exit code or showed the expected version
    - Warning: no invocation of /nix/store/pj0sfjgfdibchfcjxmw8hqqz6b3733vf-mate-calc-1.21.0/bin/mate-calculator had a zero exit code or showed the expected version
    - Warning: no invocation of /nix/store/pj0sfjgfdibchfcjxmw8hqqz6b3733vf-mate-calc-1.21.0/bin/.mate-calc-wrapped had a zero exit code or showed the expected version
    - /nix/store/pj0sfjgfdibchfcjxmw8hqqz6b3733vf-mate-calc-1.21.0/bin/.mate-calc-cmd-wrapped passed the binary check.
    - 2 of 6 passed binary check by having a zero exit code.
    - 0 of 6 passed binary check by having the new version present in output.
    - found 1.21.0 with grep in /nix/store/pj0sfjgfdibchfcjxmw8hqqz6b3733vf-mate-calc-1.21.0
    - directory tree listing: https://gist.github.com/5dedfa9ca0998bbe4bc5a135a203e38d
    - du listing: https://gist.github.com/aa96b097f6ceb6a2b427396a17d41e73

--- pkgs/desktops/mate/mate-calc/default.nix
+++ pkgs/desktops/mate/mate-calc/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "mate-calc-${version}";
-  version = "1.20.2";
+  version = "1.21.0";
 
   src = fetchurl {
     url = "http://pub.mate-desktop.org/releases/${mate.getRelease version}/${name}.tar.xz";
-    sha256 = "1ghz03j9lfgrjrh8givsw83dpbkw4wlhq4ar3r5g1bf1z636jlx0";
+    sha256 = "07mmc99wwgqbp15zrr6z7iz0frc388z19jwk28ymyzgn6bcc9cn6";
   };
 
   nativeBuildInputs = [
