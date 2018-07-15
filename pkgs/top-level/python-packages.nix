403f09777a031c5c3898dbee7126f0050d03206f
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-07-15 15:29:23 +0200

    python.pkgs.vega_datasets: init at 0.5.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14572,6 +14572,8 @@ in {
     };
   };
 
+  vega_datasets = callPackage ../development/python-modules/vega_datasets { };
+
   virtkey = callPackage ../development/python-modules/virtkey { };
 
   virtual-display = callPackage ../development/python-modules/virtual-display { };
