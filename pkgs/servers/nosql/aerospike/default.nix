059e251cdfc211ae461209f05cda7b4fc889f6af
Uli Baum <xeji@cat3.de>
2018-07-16 08:45:31 +0200

    aerospike: fix ofborg eval
    platforms = [ "x86_64-linux" ] broke ofborg eval of nixos manual
    because the aerospike module references this package.
    Fixed for now by setting platforms to platforms.linux,
    even if aarch64 isn't really supported.

--- pkgs/servers/nosql/aerospike/default.nix
+++ pkgs/servers/nosql/aerospike/default.nix
@@ -30,7 +30,8 @@ stdenv.mkDerivation rec {
     description = "Flash-optimized, in-memory, NoSQL database";
     homepage = http://aerospike.com/;
     license = licenses.agpl3;
-    platforms = [ "x86_64-linux" ];
+    #platforms = [ "x86_64-linux" ];  # breaks eval of nixos manual for aarch64
+    platforms = platforms.linux;
     maintainer = with maintainers; [ kalbasit ];
   };
 }
