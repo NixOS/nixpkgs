6a9cba67b9323baed8617bf29f1f11262a12c095
Tim Steinbach <tim@nequissimus.com>
2018-07-16 09:22:54 -0400

    linux: 4.18-rc4 -> 4.18-rc5

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -1,13 +1,13 @@
 { stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, libelf, utillinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.18-rc4";
-  modDirVersion = "4.18.0-rc4";
+  version = "4.18-rc5";
+  modDirVersion = "4.18.0-rc5";
   extraMeta.branch = "4.18";
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "1nhl82ygz7sm6njnb9qg9k4jp0gr1cv2wxpayrpc4ab21xb6b5mj";
+    sha256 = "1r7ia0dc4p8xvyrl5kx9v7ii1m25ly0hg6xyz3zwhaailg1y4jzk";
   };
 
   # Should the testing kernels ever be built on Hydra?
