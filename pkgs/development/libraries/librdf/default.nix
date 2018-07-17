29b86847eac50691d49cb6b12af4870983659941
Jan Malakhovski <oxij@oxij.org>
2018-07-17 23:11:12 +0000

    librdf: fix tests

--- pkgs/development/libraries/librdf/default.nix
+++ pkgs/development/libraries/librdf/default.nix
@@ -1,5 +1,6 @@
 { stdenv, fetchurl, pkgconfig, autoreconfHook
 , librdf_raptor2, ladspaH, openssl, zlib
+, doCheck ? stdenv.config.doCheckByDefault or false, ladspaPlugins
 }:
 
 stdenv.mkDerivation rec {
@@ -11,13 +12,17 @@ stdenv.mkDerivation rec {
     sha256 = "18p2flb2sv2hq6w2qkd29z9c7knnwqr3f12i2srshlzx6vwkm05s";
   };
 
-  preAutoreconf = "rm m4/*";
-  postPatch = "sed -i -e 's:usr/local:usr:' examples/{instances,remove}_test.c";
+  postPatch = stdenv.lib.optionalString doCheck ''
+    sed -i -e 's:usr/local:${ladspaPlugins}:' examples/{instances,remove}_test.c
+  '';
 
+  preAutoreconf = "rm m4/*";
   nativeBuildInputs = [ autoreconfHook pkgconfig ];
   buildInputs = [ ladspaH openssl zlib ];
 
   propagatedBuildInputs = [ librdf_raptor2 ];
+
+  inherit doCheck;
 
   meta = {
     description = "Lightweight RDF library with special support for LADSPA plugins";
