b8d0cd2e481f50856ceca91e26b8672e747bb749
Jan Malakhovski <oxij@oxij.org>
2018-07-17 23:10:54 +0000

    fribidi: fix tests

--- pkgs/development/libraries/fribidi/default.nix
+++ pkgs/development/libraries/fribidi/default.nix
@@ -5,6 +5,7 @@
 , ninja
 , pkgconfig
 , fixDarwinDylibNames
+, python3
 }:
 
 stdenv.mkDerivation rec {
@@ -12,16 +13,22 @@ stdenv.mkDerivation rec {
   pname = "fribidi";
   version = "1.0.4";
 
+  outputs = [ "out" "devdoc" ];
+
   # NOTE: 2018-06-06 v1.0.4: Only URL tarball has "Have pre-generated man pages: true", which works-around upstream usage of some rare ancient `c2man` fossil application.
   src = fetchurl {
     url = "https://github.com/${pname}/${pname}/releases/download/v${version}/${name}.tar.bz2";
     sha256 = "1gipy8fjyn6i4qrhima02x8xs493d21f22dijp88nk807razxgcl";
   };
 
+  postPatch = ''
+    patchShebangs test
+  '';
+
   nativeBuildInputs = [ meson ninja pkgconfig ];
   buildInputs = stdenv.lib.optional stdenv.isDarwin fixDarwinDylibNames;
 
-  outputs = [ "out" "devdoc" ];
+  checkInptus = [ python3 ];
 
   meta = with stdenv.lib; {
     homepage = https://github.com/fribidi/fribidi;
