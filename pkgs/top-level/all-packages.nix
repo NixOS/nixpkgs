497db661abf9156911c8cbd9e4a64e9dc22d3db9
nek0 <nek0@chelnok.de>
2018-07-17 18:48:46 +0200

    beginning fixin gthings up

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17236,7 +17236,8 @@ with pkgs;
 
   monkeysphere = callPackage ../tools/security/monkeysphere { };
 
-  monodevelop = callPackage ../applications/editors/monodevelop {};
+  monodevelop = callPackage ../applications/editors/monodevelop {
+  };
 
   monotone = callPackage ../applications/version-management/monotone {
     lua = lua5;
