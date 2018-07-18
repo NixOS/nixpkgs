441a92af6137bdea720dc35685eb7a29d98bbd24
Tim Steinbach <tim@nequissimus.com>
2018-07-18 07:15:00 -0400

    linux: 4.17.7 -> 4.17.8
    (cherry picked from commit 8432dec854f7e9f622217671958a92b535dc0d51)

--- pkgs/os-specific/linux/kernel/linux-4.17.nix
+++ pkgs/os-specific/linux/kernel/linux-4.17.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.17.7";
+  version = "4.17.8";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "16bbvxr4ncql7sk505g81c0i3bichqz11dfvgaji57d1yaknx4k4";
+    sha256 = "0hkqypjgvr8lyskwk8z3dac8pyi4wappnk25508vs3fy08365h0k";
   };
 } // (args.argsOverride or {}))
