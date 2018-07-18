68864f12683fe54636a364d30157e67d99c61e12
Will Dietz <w@wdtz.org>
2018-07-18 09:41:21 -0500

    mesa: more include fixes
    mostly from void-linux (thanks!)

--- pkgs/development/libraries/mesa/default.nix
+++ pkgs/development/libraries/mesa/default.nix
@@ -92,7 +92,7 @@ let self = stdenv.mkDerivation {
   patches = [
     ./glx_ro_text_segm.patch # fix for grsecurity/PaX
     ./symlink-drivers.patch
-    ./missing-include.patch # dev_t needs sys/stat.h, fixes build w/musl
+    ./missing-includes.patch # dev_t needs sys/stat.h, time_t needs time.h, etc.-- fixes build w/musl
   ];
 
   outputs = [ "out" "dev" "drivers" "osmesa" ];
