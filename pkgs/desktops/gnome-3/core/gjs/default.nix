618bb001d4ab41bc3ae45a28354e3022f39f670a
Dawid Ciężarkiewicz <dpc@dpc.pw>
2018-07-18 14:04:41 -0700

    gnome3.gjs: 1.52.3 -> 1.53.3
    I was told that the infamous gnome memory leak was finally fixed upstream:
    
    https://bugs.launchpad.net/gnome-shell/+bug/1672297/comments/93

--- pkgs/desktops/gnome-3/core/gjs/default.nix
+++ pkgs/desktops/gnome-3/core/gjs/default.nix
@@ -4,11 +4,11 @@
 
 stdenv.mkDerivation rec {
   name = "gjs-${version}";
-  version = "1.52.3";
+  version = "1.53.3";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gjs/${gnome3.versionBranch version}/${name}.tar.xz";
-    sha256 = "1z4n15wdz6pbqd2hfzrqc8mmprhv50v4jk43p08v0xv07yldh8ff";
+    sha256 = "12abmzjiankvfzqsz8iw4cvm4k36id1i00i644m05nwngpb9fxlk";
   };
 
   passthru = {
