e07ee596d637a25885dc68d9af432c603753fe78
volth <volth@webmaster.ms>
2018-07-18 21:42:28 +0000

    xfconf: 4.13.4 -> 4.13.5 (#43703)

--- pkgs/desktops/xfce4-13/xfconf/default.nix
+++ pkgs/desktops/xfce4-13/xfconf/default.nix
@@ -3,9 +3,9 @@
 mkXfceDerivation rec {
   category = "xfce";
   pname = "xfconf";
-  version = "4.13.4";
+  version = "4.13.5";
 
-  sha256 = "1bm4q06rwlmkmcy6qnwm6l70w6749iqfrmsrgj3y1jb2sacc3pd4";
+  sha256 = "0xpnwb04yw5qdn0bj8b740a7rmiy316vhja5pp8p6sdiqm32yi8a";
 
   buildInputs = [ libxfce4util ];
 }
