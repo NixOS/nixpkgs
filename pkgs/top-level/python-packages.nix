2528487797c85759b4fc3b41dda34847908ef56c
Maximilian Bosch <maximilian@mbosch.me>
2018-07-18 21:57:38 +0200

    pythonPackages.llvmlite: fix build
    `llvmlite` requires LLVM v6 as build input:
    https://github.com/numba/llvmlite/commit/9ae78b184965f76d32b2120c25216cabe23bb3c4
    
    See https://hydra.nixos.org/build/77706684

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7150,7 +7150,7 @@ in {
 
   locustio = callPackage ../development/python-modules/locustio { };
 
-  llvmlite = callPackage ../development/python-modules/llvmlite { llvm = pkgs.llvm; };
+  llvmlite = callPackage ../development/python-modules/llvmlite { llvm = pkgs.llvm_6; };
 
   lockfile = buildPythonPackage rec {
     pname = "lockfile";
