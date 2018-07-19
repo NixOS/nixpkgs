22a35168ada8f5b67549e7bbe63e5c6cb62d6104
John Wiegley <johnw@newartisans.com>
2018-07-19 09:20:34 -0700

    perlPackages.TextBibTeX: Update perl version in an output path

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -16214,7 +16214,7 @@ let self = _self // overrides; _self = with self; {
       install_name_tool -change "$oldPath" "$newPath" "$out/bin/biblex"
       install_name_tool -change "$oldPath" "$newPath" "$out/bin/bibparse"
       install_name_tool -change "$oldPath" "$newPath" "$out/bin/dumpnames"
-      install_name_tool -change "$oldPath" "$newPath" "$out/lib/perl5/site_perl/5.24.3/darwin-2level/auto/Text/BibTeX/BibTeX.bundle"
+      install_name_tool -change "$oldPath" "$newPath" "$out/lib/perl5/site_perl/5.24.4/darwin-2level/auto/Text/BibTeX/BibTeX.bundle"
     '';
     meta = {
       description = "Interface to read and parse BibTeX files";
