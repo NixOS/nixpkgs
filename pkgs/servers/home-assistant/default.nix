c55fa4b9f4e1dc4e1668fde8a8b6ec660ab22d6f
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-07-20 19:45:24 +0200

    home-assistant: 0.73.2 -> 0.74.0

--- pkgs/servers/home-assistant/default.nix
+++ pkgs/servers/home-assistant/default.nix
@@ -1,6 +1,12 @@
 { lib, fetchFromGitHub, python3
+
+# Look up dependencies of specified components in component-packages.nix
 , extraComponents ? []
+
+# Additional packages to add to propagatedBuildInputs
 , extraPackages ? ps: []
+
+# Skip pip install of required packages on startup
 , skipPip ? true }:
 
 let
@@ -68,7 +74,7 @@ let
   extraBuildInputs = extraPackages py.pkgs;
 
   # Don't forget to run parse-requirements.py after updating
-  hassVersion = "0.73.2";
+  hassVersion = "0.74.0";
 
 in with py.pkgs; buildPythonApplication rec {
   pname = "homeassistant";
@@ -83,7 +89,7 @@ in with py.pkgs; buildPythonApplication rec {
     owner = "home-assistant";
     repo = "home-assistant";
     rev = version;
-    sha256 = "1hfxwm49hgikdh827jnlzgfz9h51rjz1gm841f5iz1vwz301l6sk";
+    sha256 = "05njkzh4hwz3vbxq2j98znnk8yzs9zkzcd4d99qwcw2hn9kdc385";
   };
 
   propagatedBuildInputs = [
