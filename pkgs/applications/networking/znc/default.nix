d1f889e99db2e2dc8b2b9458d502fa3ca462ac5a
R. RyanTM <ryantm+bot@ryantm.com>
2018-07-21 08:09:01 -0700

    znc: 1.7.0 -> 1.7.1
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools. This update was made based on information from https://repology.org/metapackage/znc/versions.

--- pkgs/applications/networking/znc/default.nix
+++ pkgs/applications/networking/znc/default.nix
@@ -9,11 +9,11 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "znc-${version}";
-  version = "1.7.0";
+  version = "1.7.1";
 
   src = fetchurl {
     url = "https://znc.in/releases/archive/${name}.tar.gz";
-    sha256 = "0vxra50418bsjfdpf8vl70fijv8syvasjqdxfyjliff6k91k2zn0";
+    sha256 = "1i1r1lh9q2mr1bg520zrvrlwhrhy6wibrin78wjxq1gab1qymks4";
   };
 
   nativeBuildInputs = [ pkgconfig ];
