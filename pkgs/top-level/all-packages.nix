bad08afd72ce756d0c40b3d30329e9b5d47f19c4
Mario Rodas <marsam@users.noreply.github.com>
2018-07-21 18:43:42 -0500

    ia-writer-duospace: init at 20180721

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14739,6 +14739,8 @@ with pkgs;
 
   hanazono = callPackage ../data/fonts/hanazono { };
 
+  ia-writer-duospace = callPackage ../data/fonts/ia-writer-duospace { };
+
   ibm-plex = callPackage ../data/fonts/ibm-plex { };
 
   iconpack-obsidian = callPackage ../data/icons/iconpack-obsidian { };
