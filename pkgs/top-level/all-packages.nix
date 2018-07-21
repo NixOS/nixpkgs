b4e25d1e1eab41487db6d34449f8fe71d42bdd20
Matthew Bauer <matthew.bauer@obsidian.systems>
2018-07-20 22:00:58 -0400

    windows.libgnurx: init

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14548,6 +14548,8 @@ with pkgs;
     };
 
     wxMSW = callPackage ../os-specific/windows/wxMSW-2.8 { };
+
+    libgnurx = callPackage ../os-specific/windows/libgnurx { };
   };
 
   wirelesstools = callPackage ../os-specific/linux/wireless-tools { };
