7ecc21b37e40ddec0480e997e8fd2b2c52189a20
Janne Heß <janne@hess.ooo>
2018-07-22 20:49:28 +0200

    sieve-connect: Init at 0.89

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5065,6 +5065,8 @@ with pkgs;
 
   siege = callPackage ../tools/networking/siege {};
 
+  sieve-connect = callPackage ../applications/networking/sieve-connect {};
+
   sigal = callPackage ../applications/misc/sigal {
     inherit (pythonPackages) buildPythonApplication fetchPypi;
   };
