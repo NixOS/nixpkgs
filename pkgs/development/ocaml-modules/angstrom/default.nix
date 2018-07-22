fda3999e00afa65a710ce15b68e77aae60c9cd0a
Vincent Laporte <Vincent.Laporte@gmail.com>
2018-07-22 06:15:10 +0000

    ocamlPackages.angstrom: 0.8.1 -> 0.10.0

--- pkgs/development/ocaml-modules/angstrom/default.nix
+++ pkgs/development/ocaml-modules/angstrom/default.nix
@@ -1,22 +1,24 @@
-{ stdenv, fetchFromGitHub, ocaml, findlib, jbuilder, alcotest, result }:
+{ stdenv, fetchFromGitHub, ocaml, findlib, jbuilder, alcotest, result
+, bigstringaf
+}:
 
 if !stdenv.lib.versionAtLeast ocaml.version "4.03"
 then throw "angstrom is not available for OCaml ${ocaml.version}"
 else
 
 stdenv.mkDerivation rec {
-  version = "0.8.1";
+  version = "0.10.0";
   name = "ocaml${ocaml.version}-angstrom-${version}";
 
   src = fetchFromGitHub {
     owner  = "inhabitedtype";
     repo   = "angstrom";
     rev    = "${version}";
-    sha256 = "067r3vy5lac1bfx947gy722amna3dbcak54nlh24vx87pmcq31qc";
+    sha256 = "0lh6024yf9ds0nh9i93r9m6p5psi8nvrqxl5x7jwl13zb0r9xfpw";
   };
 
   buildInputs = [ ocaml findlib jbuilder alcotest ];
-  propagatedBuildInputs = [ result ];
+  propagatedBuildInputs = [ bigstringaf result ];
 
   buildPhase = "jbuilder build -p angstrom";
 
