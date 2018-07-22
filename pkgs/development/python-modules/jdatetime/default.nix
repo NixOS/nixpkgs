b98bcdbb17e884f0c10eb628b9f3d508149b3544
Frederik Rietdijk <fridh@fridh.nl>
2018-07-22 12:11:18 +0200

    python: jdatetime: 2.1.0 -> 2.2.0

--- pkgs/development/python-modules/jdatetime/default.nix
+++ pkgs/development/python-modules/jdatetime/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "jdatetime";
-  version = "2.1.0";
+  version = "2.2.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ac5646460defa5bf3d062504d870954c77d6234536365baf52433fb845b620d0";
+    sha256 = "0c5983ef1a36b5d98d2b20c16f1d26a52452043d84f3ff1bf8f37e46bc314874";
   };
 
   propagatedBuildInputs = [ six ];
