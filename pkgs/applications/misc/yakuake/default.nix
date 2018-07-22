b39087fcb65840a23996da9d61e61bd6a51938ba
Pascal Wittmann <mail@pascal-wittmann.de>
2018-07-22 16:51:55 +0200

    yakuake: add license
    see issue #43716

--- pkgs/applications/misc/yakuake/default.nix
+++ pkgs/applications/misc/yakuake/default.nix
@@ -65,5 +65,6 @@ mkDerivation rec {
     homepage = https://yakuake.kde.org;
     description = "Quad-style terminal emulator for KDE";
     maintainers = with lib.maintainers; [ fridh ];
+    licenses = lib.licenses.gpl2;
   };
 }
