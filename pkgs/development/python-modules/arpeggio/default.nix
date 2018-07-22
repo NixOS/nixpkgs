0a07bf6420eaedc51c80ed946e39dec16456242d
Frederik Rietdijk <fridh@fridh.nl>
2018-07-22 12:04:21 +0200

    python: Arpeggio: 1.8.0 -> 1.9.0

--- pkgs/development/python-modules/arpeggio/default.nix
+++ pkgs/development/python-modules/arpeggio/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "Arpeggio";
-  version = "1.8.0";
+  version = "1.9.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0rvgwc2nxqf22fjnggswqw2i3sn1f2hhq043vhjr3af7ldfai3l2";
+    sha256 = "a5258b84f76661d558492fa87e42db634df143685a0e51802d59cae7daad8732";
   };
 
   # Shall not be needed for next release
