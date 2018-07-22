20776ede6551e9b35a553094a62f6338705b7298
Vincent Laporte <Vincent.Laporte@gmail.com>
2018-07-22 06:11:39 +0000

    ocamlPackages.bigstringaf: init at 0.3.0
    Bigstring intrinsics and fast blits based on memcpy/memmove
    
    homepage: https://github.com/inhabitedtype/bigstringaf

---
+++ pkgs/development/ocaml-modules/bigstringaf/default.nix
@@ -0,0 +1,32 @@
+{ stdenv, fetchFromGitHub, ocaml, findlib, jbuilder, alcotest }:
+
+if !stdenv.lib.versionAtLeast ocaml.version "4.03"
+then throw "bigstringaf is not available for OCaml ${ocaml.version}"
+else
+
+stdenv.mkDerivation rec {
+  version = "0.3.0";
+  name = "ocaml${ocaml.version}-bigstringaf-${version}";
+
+  src = fetchFromGitHub {
+    owner = "inhabitedtype";
+    repo = "bigstringaf";
+    rev = version;
+    sha256 = "1yx6hv8rk0ldz1h6kk00rwg8abpfc376z00aifl9f5rn7xavpscs";
+  };
+
+  buildInputs = [ ocaml findlib jbuilder alcotest ];
+
+  doCheck = true;
+  checkPhase = "dune runtest";
+
+  inherit (jbuilder) installPhase;
+
+  meta = {
+    description = "Bigstring intrinsics and fast blits based on memcpy/memmove";
+    license = stdenv.lib.licenses.bsd3;
+    maintainers = [ stdenv.lib.maintainers.vbgl ];
+    inherit (src.meta) homepage;
+    inherit (ocaml.meta) platforms;
+  };
+}
