51c30cd7074e40ab8b6a2150ee2747cba78ecc23
Frederik Rietdijk <fridh@fridh.nl>
2018-07-22 12:05:31 +0200

    python: botocore: 1.10.57 -> 1.10.62

--- pkgs/development/python-modules/botocore/default.nix
+++ pkgs/development/python-modules/botocore/default.nix
@@ -12,11 +12,11 @@
 buildPythonPackage rec {
   name = "${pname}-${version}";
   pname = "botocore";
-  version = "1.10.57";
+  version = "1.10.62";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0mif7c12643hac6zxq89gv0wjf4r3vqlmm01bm68psljaj40jnpi";
+    sha256 = "047d553ec2a4c7f80f9ca02f73c3ab443577bad6bcb079c698fb9dd8cc93c0af";
   };
 
   propagatedBuildInputs = [
