1891611271d1d04dbd78820e962c999543c1f9ec
Frederik Rietdijk <fridh@fridh.nl>
2018-07-22 12:17:18 +0200

    python: pyflakes: 1.6.0 -> 2.0.0

--- pkgs/development/python-modules/pyflakes/default.nix
+++ pkgs/development/python-modules/pyflakes/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "pyflakes";
-  version = "1.6.0";
+  version = "2.0.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "8d616a382f243dbf19b54743f280b80198be0bca3a5396f1d2e1fca6223e8805";
+    sha256 = "9a7662ec724d0120012f6e29d6248ae3727d821bba522a0e6b356eff19126a49";
   };
 
   buildInputs = [ unittest2 ];
