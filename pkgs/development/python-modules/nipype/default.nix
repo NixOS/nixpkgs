4fcf767df61b728a6667cee9bae3ee346b8d87ed
Frederik Rietdijk <fridh@fridh.nl>
2018-07-22 12:14:10 +0200

    python: nipype: 1.0.4 -> 1.1.0

--- pkgs/development/python-modules/nipype/default.nix
+++ pkgs/development/python-modules/nipype/default.nix
@@ -33,11 +33,11 @@ assert !isPy3k -> configparser != null;
 
 buildPythonPackage rec {
   pname = "nipype";
-  version = "1.0.4";
+  version = "1.1.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "4c3c1eb15fc016457525d1f7eb701d1bbe595eb48a036ae8dc2d21b843f9e525";
+    sha256 = "2a5ebbc9244a18e3b2d83a9639da09248e92bc0742b81a86550ef1a18c1fccbc";
   };
 
   # see https://github.com/nipy/nipype/issues/2240
