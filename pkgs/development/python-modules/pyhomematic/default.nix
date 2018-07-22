ea1fa45b408e7644f1a5083c1578e5c0260e20c3
Frederik Rietdijk <fridh@fridh.nl>
2018-07-22 12:17:38 +0200

    python: pyhomematic: 0.1.44 -> 0.1.46

--- pkgs/development/python-modules/pyhomematic/default.nix
+++ pkgs/development/python-modules/pyhomematic/default.nix
@@ -2,13 +2,13 @@
 
 buildPythonPackage rec {
   pname = "pyhomematic";
-  version = "0.1.44";
+  version = "0.1.46";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "09f928d33fefd1ef5d18325877d73d01c6811b6177fc97fc1190c20acc5e12a9";
+    sha256 = "0a0ba8de05fc6d60bbb0beec7e808fb231abcb566c3bc17de532f72b18fe2837";
   };
 
   # PyPI tarball does not include tests/ directory
