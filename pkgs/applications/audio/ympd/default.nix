NixOS/nixpkgs

commit dac43da8da1a842ac8ed82b1e0c705be03334ff0

ympd: add license
see issue #43716

pkgs/applications/audio/ympd/default.nix
@@ -19,5 +19,6 @@ stdenv.mkDerivation rec {
     description = "Standalone MPD Web GUI written in C, utilizing Websockets and Bootstrap/JS";
     maintainers = [ stdenv.lib.maintainers.siddharthist ];
     platforms = stdenv.lib.platforms.unix;
+    license = stdenv.licenses.gpl2;
   };
 }

commit 27af1633a497c49308bd008d510b0388b723656d

fix eval
broken by dac43da8da1a842ac8ed82b1e0c705be03334ff0
cc @pSub

pkgs/applications/audio/ympd/default.nix
@@ -19,6 +19,6 @@ stdenv.mkDerivation rec {
     description = "Standalone MPD Web GUI written in C, utilizing Websockets and Bootstrap/JS";
     maintainers = [ stdenv.lib.maintainers.siddharthist ];
     platforms = stdenv.lib.platforms.unix;
-    license = stdenv.licenses.gpl2;
+    license = stdenv.lib.licenses.gpl2;
   };
 }