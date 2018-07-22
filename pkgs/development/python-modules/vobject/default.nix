f9b5d92fe31132102ceb62b924698e56b4fadee7
Frederik Rietdijk <fridh@fridh.nl>
2018-07-22 12:23:54 +0200

    python: vobject: 0.9.6 -> 0.9.6.1

--- pkgs/development/python-modules/vobject/default.nix
+++ pkgs/development/python-modules/vobject/default.nix
@@ -1,12 +1,12 @@
 { lib, buildPythonPackage, fetchPypi, isPyPy, python, dateutil }:
 
 buildPythonPackage rec {
-  version = "0.9.6";
+  version = "0.9.6.1";
   pname = "vobject";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "cd9ede4363f83c06ba8d8f1541c736efa5c46f9a431430002b2f84f4f4e674d8";
+    sha256 = "96512aec74b90abb71f6b53898dd7fe47300cc940104c4f79148f0671f790101";
   };
 
   disabled = isPyPy;
