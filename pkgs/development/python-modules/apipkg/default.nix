1c28ff05fa34c88d910462645c91bd7ee8408df1
Frederik Rietdijk <fridh@fridh.nl>
2018-07-23 04:22:53 +0200

    python.pkgs.apipkg: fix build

--- pkgs/development/python-modules/apipkg/default.nix
+++ pkgs/development/python-modules/apipkg/default.nix
@@ -1,5 +1,5 @@
 { stdenv, buildPythonPackage, fetchPypi
-, pytest }:
+, pytest, setuptools_scm }:
 
 buildPythonPackage rec {
   pname = "apipkg";
@@ -10,7 +10,8 @@ buildPythonPackage rec {
     sha256 = "37228cda29411948b422fae072f57e31d3396d2ee1c9783775980ee9c9990af6";
   };
 
-  buildInputs = [ pytest ];
+  buildInputs = [ setuptools_scm ];
+  checkInputs = [ pytest ];
 
   checkPhase = ''
     py.test
