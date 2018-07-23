adeef214eff2fb5654ad953cb2ac5ba0fb25cacb
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-07-23 11:54:48 +0200

    python.pkgs.tox: update propagatedBuildInputs

--- pkgs/development/python-modules/tox/default.nix
+++ pkgs/development/python-modules/tox/default.nix
@@ -1,11 +1,12 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, py
-, virtualenv
+, packaging
 , pluggy
-, setuptools_scm
+, py
 , six
+, virtualenv
+, setuptools_scm
 }:
 
 buildPythonPackage rec {
@@ -13,12 +14,18 @@ buildPythonPackage rec {
   version = "3.1.2";
 
   buildInputs = [ setuptools_scm ];
-  propagatedBuildInputs = [ py virtualenv pluggy six ];
+  propagatedBuildInputs = [ packaging pluggy py six virtualenv ];
 
   doCheck = false;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "9f0cbcc36e08c2c4ae90d02d3d1f9a62231f974bcbc1df85e8045946d8261059";
+  };
+
+  meta = with lib; {
+    description = "Virtualenv-based automation of test activities";
+    homepage = https://tox.readthedocs.io/;
+    license = licenses.mit;
   };
 }
