e2c6f6815f53cc74a20d594026aa12028a17e6e2
Fatih Altinok <photonia@gmail.com>
2018-07-23 15:55:11 +0300

    ocamlPackages.ppx_gen_rec: init at 1.0.0

---
+++ pkgs/development/ocaml-modules/ppx_gen_rec/default.nix
@@ -0,0 +1,30 @@
+{ stdenv, fetchurl, ocaml, findlib, jbuilder, ocaml-migrate-parsetree }:
+
+assert stdenv.lib.versionAtLeast (stdenv.lib.getVersion ocaml) "4.01";
+
+stdenv.mkDerivation rec {
+  pname = "ppx_gen_rec";
+  name = "ocaml${ocaml.version}-${pname}-${version}";
+  version = "1.0.0";
+
+  src = fetchurl {
+    url = "https://github.com/flowtype/ocaml-${pname}/releases/download/v${version}/${pname}-${version}.tbz";
+    sha256 = "0qy0wa3rd5yh1612jijadi1yddfslpsmmmf69phi2dhr3vmkhza7";
+  };
+
+  unpackCmd = "tar xjf $src";
+
+  buildInputs = [ ocaml findlib jbuilder ocaml-migrate-parsetree ];
+
+  buildPhase = "jbuilder build -p ppx_gen_rec";
+
+  inherit (jbuilder) installPhase;
+
+  meta = with stdenv.lib; {
+    homepage = https://github.com/flowtype/ocaml-ppx_gen_rec;
+    description = "ocaml preprocessor that generates a recursive module";
+    license = licenses.mit;
+    platforms = ocaml.meta.platforms or [];
+    maintainers = [ maintainers.frontsideair ];
+  };
+}
