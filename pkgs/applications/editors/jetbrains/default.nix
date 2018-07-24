8d621910f5a74d7afd74e504dc98b38d8bd32640
Averell Dalton <averell+nixpkgs@rxd4.com>
2018-07-24 23:37:03 +0200

    jetbrains.datagrip: 2018.1.4 -> 2018.1.5

--- pkgs/applications/editors/jetbrains/default.nix
+++ pkgs/applications/editors/jetbrains/default.nix
@@ -250,12 +250,12 @@ in
 
   datagrip = buildDataGrip rec {
     name = "datagrip-${version}";
-    version = "2018.1.4"; /* updated by script */
+    version = "2018.1.5"; /* updated by script */
     description = "Your Swiss Army Knife for Databases and SQL";
     license = stdenv.lib.licenses.unfree;
     src = fetchurl {
       url = "https://download.jetbrains.com/datagrip/${name}.tar.gz";
-      sha256 = "1m0qckq645jw7cj40m5vfgr212b8hji539skavch2j5s5fbqy3ln"; /* updated by script */
+      sha256 = "0jqkqiml7v1m9hkcjwmx866vrn5myxcg4zqzyghviyx5i7jrvx3c"; /* updated by script */
     };
     wmClass = "jetbrains-datagrip";
     update-channel = "DataGrip 2018.1";
