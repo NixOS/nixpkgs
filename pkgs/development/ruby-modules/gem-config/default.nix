24bd84aeefb445d16956d7fd73efb5e77dd6bea3
Alyssa Ross <hi@alyssa.is>
2018-07-25 17:30:54 +0100

    gem-config: add libxml2-ruby

--- pkgs/development/ruby-modules/gem-config/default.nix
+++ pkgs/development/ruby-modules/gem-config/default.nix
@@ -206,6 +206,13 @@ in
     buildFlags = [ "--with-system-v8=true" ];
   };
 
+  libxml-ruby = attrs: {
+    buildFlags = [
+      "--with-xml2-lib=${libxml2.out}/lib"
+      "--with-xml2-include=${libxml2.dev}/include/libxml2"
+    ];
+  };
+
   msgpack = attrs: {
     buildInputs = [ libmsgpack ];
   };
