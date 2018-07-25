0ae316fb4f0d9731c8df74ff704e065f7868c5e3
Tuomas Tynkkynen <tuomas@tuxera.com>
2018-07-25 23:36:45 +0300

    nixos/rename: Add temporary aliases for the system.nixos.* rename revert
    See the previous commit for motivation.

--- nixos/modules/rename.nix
+++ nixos/modules/rename.nix
@@ -4,6 +4,11 @@ with lib;
 
 {
   imports = [
+    # !!! These were renamed the other way, but got reverted later.
+    # !!! Drop these before 18.09 is released.
+    (mkRenamedOptionModule [ "system" "nixos" "stateVersion" ] [ "system" "stateVersion" ])
+    (mkRenamedOptionModule [ "system" "nixos" "defaultChannel" ] [ "system" "defaultChannel" ])
+
     (mkRenamedOptionModule [ "dysnomia" ] [ "services" "dysnomia" ])
     (mkRenamedOptionModule [ "environment" "x11Packages" ] [ "environment" "systemPackages" ])
     (mkRenamedOptionModule [ "environment" "enableBashCompletion" ] [ "programs" "bash" "enableCompletion" ])
