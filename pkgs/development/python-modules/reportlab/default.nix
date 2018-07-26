69b6a44066b94f41d26fb147755ba6d4ae1eecf0
Frederik Rietdijk <fridh@fridh.nl>
2018-07-26 13:10:08 +0200

    python: reportlab: 3.5.1 -> 3.5.2

--- pkgs/development/python-modules/reportlab/default.nix
+++ pkgs/development/python-modules/reportlab/default.nix
@@ -13,11 +13,11 @@ let
   ft = freetype.overrideAttrs (oldArgs: { dontDisableStatic = true; });
 in buildPythonPackage rec {
   pname = "reportlab";
-  version = "3.5.1";
+  version = "3.5.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "5345494df4f1563fdab5597f7f543eae44c0aeecce05bd4d93c199f34c4b8c0c";
+    sha256 = "08986267eaf25d62c3802512f0a97dc3426d0c82f52c8beb576689582eb85b7f";
   };
 
   checkInputs = [ glibcLocales ];
