4758fe7c564bd5cb55c351930201fcafcca1bd4a
Carlos D <carlos.dagostino@gmail.com>
2018-07-26 12:56:44 +1000

    kustomize: init at 1.0.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8288,6 +8288,8 @@ with pkgs;
 
   kube-aws = callPackage ../development/tools/kube-aws { };
 
+  kustomize = callPackage ../development/tools/kustomize { };
+
   Literate = callPackage ../development/tools/literate-programming/Literate {};
 
   lcov = callPackage ../development/tools/analysis/lcov { };
