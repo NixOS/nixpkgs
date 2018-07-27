fb8a3e8d4af5238b3349c2e7b1e81a17a99ffe7b
Väinö Järvelä <vaino.jarvela@reaktor.com>
2018-07-27 18:03:29 +0300

    nodejs: 10.6.0 -> 10.7.0

--- pkgs/development/web/nodejs/v10.nix
+++ pkgs/development/web/nodejs/v10.nix
@@ -5,7 +5,7 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "10.6.0";
-    sha256 = "13vyzh53j2c4mv3q3yb0hkdpl1mag5705k7kmb8nmcvdhcm78q0r";
+    version = "10.7.0";
+    sha256 = "0qp93ddbnvadimj11wnznwhkq8vq1f7q259iq8siy5b7r936kvil";
     patches = lib.optionals stdenv.isDarwin [ ./no-xcode-v7.patch ./no-xcodebuild.patch ];
   }
