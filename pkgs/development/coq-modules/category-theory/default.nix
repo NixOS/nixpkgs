NixOS/nixpkgs

commit 08bf0b7630afa8656bb36bd61fac8ce9b2aff613

coqPackages.category-theory: Add equations as a buildInput

pkgs/development/coq-modules/category-theory/default.nix
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [ coq.ocaml coq.camlp5 coq.findlib ];
-  propagatedBuildInputs = [ coq ssreflect ];
+  propagatedBuildInputs = [ coq ssreflect equations ];
 
   enableParallelBuilding = false;
 

commit 6d6522d30d1c05126e5f53d9996637475d1ac75d

coqPackages.category-theory: revert "Add equations as a buildInput"
This reverts commit 08bf0b7630afa8656bb36bd61fac8ce9b2aff613, which broke
Nixpkgs evaluation.

Ping @jwiegley.

pkgs/development/coq-modules/category-theory/default.nix
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [ coq.ocaml coq.camlp5 coq.findlib ];
-  propagatedBuildInputs = [ coq ssreflect equations ];
+  propagatedBuildInputs = [ coq ssreflect ];
 
   enableParallelBuilding = false;
 