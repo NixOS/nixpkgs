392f3b8a167dc9c3ebe9251e5f7cea63e0f550b6
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-07-28 12:59:03 +0200

    mustache-go: init at 1.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14833,6 +14833,8 @@ with pkgs;
 
   mustache-spec = callPackage ../data/documentation/mustache-spec { };
 
+  mustache-go = callPackage ../development/tools/mustache-go { };
+
   myrica = callPackage ../data/fonts/myrica { };
 
   nafees = callPackage ../data/fonts/nafees { };
