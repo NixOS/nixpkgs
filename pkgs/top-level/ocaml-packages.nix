433ecae681bc73cdab075f57b8acb9ea74b04f11
Vincent Laporte <vbgl@users.noreply.github.com>
2018-07-29 08:35:35 +0000

    ocamlPackages.labltk: init at 8.06 (#43963)
    LablTk is an OCaml interface to the Tcl/Tk GUI framework.
    
    homepage: http://labltk.forge.ocamlcore.org/

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -370,6 +370,8 @@ let
       gtkmathview = callPackage ../development/libraries/gtkmathview { };
     };
 
+    labltk = callPackage ../development/ocaml-modules/labltk { };
+
     lambdaTerm-1_6 = callPackage ../development/ocaml-modules/lambda-term/1.6.nix { lwt = lwt2; };
     lambdaTerm =
       if lib.versionOlder "4.02" ocaml.version
