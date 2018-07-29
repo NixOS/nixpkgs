068155371556d6f3997944bddaf308edb3e6dc7c
Jamey Sharp <jamey@minilop.net>
2018-07-28 20:48:43 -0700

    gnome-builder: init at 3.28.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2712,6 +2712,8 @@ with pkgs;
     inherit (gnome2) gnome_python gnome_python_desktop;
   };
 
+  gnome-builder = callPackage ../applications/editors/gnome-builder { };
+
   gnokii = callPackage ../tools/misc/gnokii { };
 
   gnuapl = callPackage ../development/interpreters/gnu-apl { };
