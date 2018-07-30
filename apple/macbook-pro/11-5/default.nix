a9f65b889655b0730935d6754fb64926fce42bf4
Rehno Lindeque <errantkid@gmail.com>
2018-07-30 14:04:59 -0400

    apple/macbook-pro/11-5: Remove boot loader
    Since the boot loader is already automatically generated by
    nixos-generate-config, there is no need to include it here.

--- apple/macbook-pro/11-5/default.nix
+++ apple/macbook-pro/11-5/default.nix
@@ -13,9 +13,6 @@ in
     <nixpkgs/nixos/modules/hardware/network/broadcom-43xx.nix>
   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
   # Apparently this is currently only supported by ati_unfree drivers, not ati
   hardware.opengl.driSupport32Bit = false;
 
