329a507768b50e3b54205904b382e2be864cb156
Jan Tojnar <jtojnar@gmail.com>
2018-07-31 23:05:49 +0200

    libdazzle: 3.28.3 → 3.28.5

--- pkgs/development/libraries/libdazzle/default.nix
+++ pkgs/development/libraries/libdazzle/default.nix
@@ -2,7 +2,7 @@
 , gtk-doc, docbook_xsl, dbus, xvfb_run, glib, gtk3, gnome3 }:
 
 let
-  version = "3.28.3";
+  version = "3.28.5";
   pname = "libdazzle";
 in
 stdenv.mkDerivation {
@@ -13,7 +13,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "mirror://gnome/sources/libdazzle/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0q8apibs99yvrwdxlknq4nayss88wsj0rp5fv2x4hh0bgfw8i7w7";
+    sha256 = "08qdwv2flywnh6kibkyv0pnm67pk8xlmjh4yqx6hf13hyhkxkqgg";
   };
 
   nativeBuildInputs = [ ninja meson pkgconfig vala gobjectIntrospection libxml2 gtk-doc docbook_xsl dbus xvfb_run ];
