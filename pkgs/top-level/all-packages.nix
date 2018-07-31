6528a506ffa02ae6faa0922f205c0e625bed3099
Michiel Leenaars <ml.software@leenaa.rs>
2018-08-01 01:15:47 +0200

    inriafonts: init at 1.200

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14776,6 +14776,9 @@ with pkgs;
 
   input-fonts = callPackage ../data/fonts/input-fonts { };
 
+  inriafonts = callPackage ../data/fonts/inriafonts { };
+
+
   iosevka = callPackage ../data/fonts/iosevka {
     nodejs = nodejs-8_x;
   };
