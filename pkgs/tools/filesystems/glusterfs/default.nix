7b5535ac31c68ad56b9c8adcb88f78567e61207c
Pascal Bach <pascal.bach@nextrem.ch>
2018-08-01 00:27:11 +0200

    glusterfs: 3.12.4 -> 3.12.12

--- pkgs/tools/filesystems/glusterfs/default.nix
+++ pkgs/tools/filesystems/glusterfs/default.nix
@@ -15,10 +15,10 @@ let
     #       The command
     #         find /nix/store/...-glusterfs-.../ -name '*.py' -executable
     #       can help with finding new Python scripts.
-    version = "3.12.4";
+    version = "3.12.12";
     name="${baseName}-${version}";
     url="https://github.com/gluster/glusterfs/archive/v${version}.tar.gz";
-    sha256 = "01gsc3dw491ipl47q733iznddxbg42aa749vkyaq6i6w4d7m157f";
+    sha256 = "1q6rcf9y98w3kvgwdlbhl65phkdl0mfil6y7i3gnpf3d21gfb6nw";
   };
   buildInputs = [
     fuse bison flex_2_5_35 openssl ncurses readline
