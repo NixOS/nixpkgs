8b34c32fc81f2f79af62dfa6600ef306622a1be1
Will Dietz <github@wdtz.org>
2018-08-01 15:15:54 -0500

    proselint: 0.9.0 -> 0.10.0 (#44313)

--- pkgs/tools/text/proselint/default.nix
+++ pkgs/tools/text/proselint/default.nix
@@ -2,13 +2,13 @@
 
 buildPythonApplication rec {
   name = "proselint-${version}";
-  version = "0.9.0";
+  version = "0.10.0";
 
   doCheck = false; # fails to pass because it tries to run in home directory
 
   src = fetchurl {
     url = "mirror://pypi/p/proselint/${name}.tar.gz";
-    sha256 = "1fibk24fx00bfn0z4iikcv519cz2nkcil9k187sf3adb2ldzg4ab";
+    sha256 = "0kmr95mf2gij40qy4660ryfanw13vxlhpmivqia1mdbii8iziyhg";
   };
 
   propagatedBuildInputs = [ click future six ];
