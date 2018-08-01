1ac434c8be2cc829dd84f1458edffb0d59036c5e
Matt McHenry <github@matt.mchenryfamily.org>
2018-07-31 23:28:33 -0400

    haskell-language-ecmascript: jailbreak to fix the build

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -1125,4 +1125,7 @@ self: super: {
   # Tests require a browser: https://github.com/ku-fpg/blank-canvas/issues/73
   blank-canvas = dontCheck super.blank-canvas;
   blank-canvas_0_6_2 = dontCheck super.blank-canvas_0_6_2;
+
+  # needed because of testing-feat >=0.4.0.2 && <1.1
+  language-ecmascript = doJailbreak super.language-ecmascript;
 }
