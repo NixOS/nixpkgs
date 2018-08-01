2125f8933b29f788914314f95bedf1f6fd330e8d
markuskowa <markus.kowalewski@gmail.com>
2018-08-01 14:57:51 +0200

    halide: init at 2018_02_15 (#44207)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3004,6 +3004,8 @@ with pkgs;
 
   halibut = callPackage ../tools/typesetting/halibut { };
 
+  halide = callPackage ../development/compilers/halide {};
+
   hardinfo = callPackage ../tools/system/hardinfo { };
 
   hdapsd = callPackage ../os-specific/linux/hdapsd { };
