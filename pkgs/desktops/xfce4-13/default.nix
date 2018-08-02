03ff38856cdd990d0537245df648584609b6161d
volth <volth@webmaster.ms>
2018-08-02 08:35:21 +0000

    xfce4-13.xfce4-session: init at 4.13.0 (#44336)

--- pkgs/desktops/xfce4-13/default.nix
+++ pkgs/desktops/xfce4-13/default.nix
@@ -74,9 +74,12 @@ makeScope newScope (self: with self; {
     inherit (gnome3) libsoup;
   };
 
-  xfce4-taskmanager = callPackage ./xfce4-taskmanager { };
+  xfce4-session = callPackage ./xfce4-session { };
+  xinitrc = "${xfce4-session}/etc/xdg/xfce4/xinitrc";
 
   xfce4-settings = callPackage ./xfce4-settings { };
+
+  xfce4-taskmanager = callPackage ./xfce4-taskmanager { };
 
   xfce4-terminal = callPackage ./xfce4-terminal {
     inherit (gnome3) vte;
