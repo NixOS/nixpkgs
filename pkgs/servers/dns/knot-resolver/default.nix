1895354eedeb996cdd0e665b9b5bc08b7a64a4ac
Vladimír Čunát <vcunat@gmail.com>
2018-08-02 14:00:29 +0200

    knot-resolver: 2.4.0 -> 2.4.1 (security)
    Fixes CVE-2018-10920 and some less important things.
    https://lists.nic.cz/pipermail/knot-resolver-announce/2018/000002.html

--- pkgs/servers/dns/knot-resolver/default.nix
+++ pkgs/servers/dns/knot-resolver/default.nix
@@ -12,11 +12,11 @@ inherit (stdenv.lib) optional concatStringsSep;
 
 unwrapped = stdenv.mkDerivation rec {
   name = "knot-resolver-${version}";
-  version = "2.4.0";
+  version = "2.4.1";
 
   src = fetchurl {
     url = "https://secure.nic.cz/files/knot-resolver/${name}.tar.xz";
-    sha256 = "8c88c73dd50dad6f371bfc170f49cd374022e59f8005ac1fa6cd99764f72b4d1";
+    sha256 = "e8044316cd897ad29b3c5284de06652e1568c4d5861e3147ec2191fbacd8d9ff";
   };
 
   outputs = [ "out" "dev" ];
