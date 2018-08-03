505007471d0ad0d967c76f0f61a21cf7e3fad41b
Johan Thomsen <jth@dbc.dk>
2018-08-03 16:52:15 +0200

    certmgr: init at 1.6.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1696,6 +1696,8 @@ with pkgs;
   ceph = callPackage ../tools/filesystems/ceph { boost = boost165; };
   ceph-dev = ceph;
 
+  certmgr = callPackage ../tools/security/certmgr { };
+
   cfdg = callPackage ../tools/graphics/cfdg {
     ffmpeg = ffmpeg_2;
   };
