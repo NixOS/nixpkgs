355f36c5bc471e6899ba5731fc719685acbfdebb
Janne Heß <janne@hess.ooo>
2018-08-03 23:24:53 +0200

    smarty3: Init at 3.1.32

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5171,6 +5171,8 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) IOKit ApplicationServices;
   };
 
+  smarty3 = callPackage ../development/libraries/smarty3 { };
+
   smbldaptools = callPackage ../tools/networking/smbldaptools {
     inherit (perlPackages) perlldap CryptSmbHash DigestSHA1;
   };
