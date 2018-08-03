fdca8ec48cc5fac7c99846da62b0c2c87d6ecbc4
Tim Steinbach <tim@nequissimus.com>
2018-08-03 08:20:22 -0400

    linux: 4.17.11 -> 4.17.12

--- pkgs/os-specific/linux/kernel/linux-4.17.nix
+++ pkgs/os-specific/linux/kernel/linux-4.17.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.17.11";
+  version = "4.17.12";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "133jyd75a0gq71m783s3ydjkpnzw5xxn4wrk1zal6fr19znq87nv";
+    sha256 = "18xmkj1bmfrmvx6p8cl5l7pyv0zk5y3mxhyfnlzqwkxnvkjf5660";
   };
 } // (args.argsOverride or {}))
