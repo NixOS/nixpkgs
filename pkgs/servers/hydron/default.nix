f90a9c5a9062bfa3a51248a3df903b44bc86c71d
Okina Matara <okinan@chiru.no>
2018-08-03 10:43:00 -0500

    hydron: 2018-07-15 -> 2018-07-30

--- pkgs/servers/hydron/default.nix
+++ pkgs/servers/hydron/default.nix
@@ -3,25 +3,20 @@
 
 buildGoPackage rec {
   name = "hydron-unstable-${version}";
-  version = "2018-07-15";
+  version = "2018-07-30";
   goPackagePath = "github.com/bakape/hydron";
   goDeps = ./deps.nix;
 
   src = fetchFromGitHub {
-    rev = "3906ace0b4cf48ba9acccf372377c7feb0665be4";
     owner = "bakape";
     repo = "hydron";
-    sha256 = "079a88740wxgq73sq8w96zppfng7af76k7h484x3w695qk83j33r";
+    rev = "586af9da1e551b2a7128c154200e2e2e32d1af7e";
+    sha256 = "1lif63kcllsbmv06n3b8ayx89k90lximyp23108blcq456wrf0zi";
   };
 
   enableParallelBuilding = true;
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ ffmpeg-full graphicsmagick quicktemplate go-bindata easyjson ];
-
-  # Temporary workaround for https://github.com/NixOS/nixpkgs/issues/43593
-  preBuild = ''
-    rm go/src/github.com/bakape/hydron/ico.syso
-  '';
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/bakape/hydron";
