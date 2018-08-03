51163cd2c16314c00f1ddc1dbd4cd610b4cde1e6
Yarny0 <41838844+Yarny0@users.noreply.github.com>
2018-08-03 17:35:15 +0200

    k3b: wrapper as separate derivation
    The k3b package uses a wrapper script to inject binaries
    that are called by k3b and do the actual work into PATH.
    Most of those binaries are beyond reproach.
    There are, however, legitimate reasons to replace
    cdrtools with cdrkit, or with nothing at all to encourage
    k3b to use suid wrappers from the surrounding PATH.
    
    To ease the use of such cdrtools replacements,
    this commit moves the wrapper script into its
    own derivation (created with symlinkJoin).
    To use k3b with cdrkit, this line in configuration.nix
    suffices and won't trigger a rebuild of k3b itself:
    > environment.systemPackages = with pkgs; [ k3b.override { cdrtools = cdrkit; } ];
    Using "null" for cdrtools is possible and will skip
    the addition of cdrtools to PATH in the wrapper script.
    
    This commit is motivated by
    https://github.com/NixOS/nixpkgs/issues/19154 .

--- pkgs/applications/kde/k3b.nix
+++ pkgs/applications/kde/k3b.nix
@@ -1,5 +1,5 @@
 { mkDerivation, lib
-, extra-cmake-modules, kdoctools, makeWrapper, shared-mime-info
+, extra-cmake-modules, kdoctools, makeWrapper, shared-mime-info, symlinkJoin
 , qtwebkit
 , libkcddb, karchive, kcmutils, kfilemetadata, knewstuff, knotifyconfig, solid, kxmlgui
 , flac, lame, libmad, libmpcdec, libvorbis
@@ -8,41 +8,51 @@
 , ffmpeg, libmusicbrainz3, normalize, sox, transcode, kinit
 }:
 
-mkDerivation {
-  name = "k3b";
-  meta = with lib; {
-    license = with licenses; [ gpl2Plus ];
-    maintainers = with maintainers; [ sander phreedom ];
-    platforms = platforms.linux;
+let
+
+  unwrapped = mkDerivation {
+    name = "k3b";
+    meta = with lib; {
+      license = with licenses; [ gpl2Plus ];
+      maintainers = with maintainers; [ sander phreedom ];
+      platforms = platforms.linux;
+    };
+    nativeBuildInputs = [ extra-cmake-modules kdoctools ];
+    buildInputs = [
+      # qt
+      qtwebkit
+      # kde
+      libkcddb karchive kcmutils kfilemetadata knewstuff knotifyconfig solid kxmlgui
+      # formats
+      flac lame libmad libmpcdec libvorbis
+      # sound utilities
+      libsamplerate libsndfile taglib
+      # cd/dvd
+      cdparanoia libdvdcss libdvdread
+      # others
+      ffmpeg libmusicbrainz3 shared-mime-info
+    ];
+    propagatedUserEnvPkgs = [ (lib.getBin kinit) ];
   };
-  nativeBuildInputs = [ extra-cmake-modules kdoctools makeWrapper ];
-  buildInputs = [
-    # qt
-    qtwebkit
-    # kde
-    libkcddb karchive kcmutils kfilemetadata knewstuff knotifyconfig solid kxmlgui
-    # formats
-    flac lame libmad libmpcdec libvorbis
-    # sound utilities
-    libsamplerate libsndfile taglib
-    # cd/dvd
-    cdparanoia libdvdcss libdvdread
-    # others
-    ffmpeg libmusicbrainz3 shared-mime-info
+
+  binPath = lib.makeBinPath ( lib.remove null [
+    cdrdao cdrtools dvdplusrwtools libburn normalize sox transcode
+    vcdimager flac
+  ]);
+
+  libraryPath = lib.makeLibraryPath [
+    cdparanoia
   ];
-  propagatedUserEnvPkgs = [ (lib.getBin kinit) ];
-  postFixup =
-    let
-      binPath = lib.makeBinPath [
-        cdrdao cdrtools dvdplusrwtools libburn normalize sox transcode
-        vcdimager flac
-      ];
-      libraryPath = lib.makeLibraryPath [
-        cdparanoia
-      ];
-    in ''
-      wrapProgram "$out/bin/k3b"     \
-        --prefix PATH : "${binPath}" \
-        --prefix LD_LIBRARY_PATH : ${libraryPath}
-    '';
+
+in
+
+symlinkJoin {
+  inherit (unwrapped) name meta;
+  paths = [ unwrapped ];
+  nativeBuildInputs = [ makeWrapper ];
+  postBuild = ''
+    wrapProgram "$out/bin/k3b"     \
+      --prefix PATH : "${binPath}" \
+      --prefix LD_LIBRARY_PATH : ${libraryPath}
+  '';
 }
