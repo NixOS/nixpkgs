84568728927bc38854712116aee6f8bbffa4e521
Vladyslav Mykhailichenko <dywedir@pm.me>
2018-08-05 12:23:19 +0300

    mdbook: 0.1.8 -> 0.2.0

--- pkgs/tools/text/mdbook/default.nix
+++ pkgs/tools/text/mdbook/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   name = "mdbook-${version}";
-  version = "0.1.8";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "rust-lang-nursery";
     repo = "mdBook";
     rev = "v${version}";
-    sha256 = "1xmw4v19ff6mvimwk5l437wslzw5npy60zdb8r4319bjf32pw9pn";
+    sha256 = "0lmklvpjvi21hcx9cnl6nizgnwflf5hs150yhckik0sp990sdvp7";
   };
 
-  cargoSha256 = "0kcc0b2644qbalz7dnqwxsjdmw1h57k0rjrvwqh8apj2sgl64gyv";
+  cargoSha256 = "0n7jcccp56lv5w8x25zij6big5g3z1wxgh06q7b413nnvj9wq47k";
 
   buildInputs = stdenv.lib.optionals stdenv.isDarwin [ CoreServices ];
 
