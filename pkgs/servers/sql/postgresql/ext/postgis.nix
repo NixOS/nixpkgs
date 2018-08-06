0b3d62278105c816c5974edc285f0c53a57cb3dc
Austin Seipp <aseipp@pobox.com>
2018-08-06 13:27:42 -0500

    postgresqlPackages.postgis: enable PostgreSQL 11 JIT support
    This was a bug that was fixed upstream; the applied patch is slightly
    different but with the same result. Should be removed when a new version
    of PostGIS is out.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/servers/sql/postgresql/ext/postgis.nix
+++ pkgs/servers/sql/postgresql/ext/postgis.nix
@@ -71,20 +71,28 @@ in stdenv.mkDerivation rec {
   '';
 
   buildInputs = [ libxml2 postgresql geos proj perl gdal json_c pkgconfig ];
+  dontDisableStatic = true;
 
   sql_comments = "postgis_comments.sql";
-
   sql_srcs = ["postgis.sql" "spatial_ref_sys.sql"];
 
   # postgis config directory assumes /include /lib from the same root for json-c library
   NIX_LDFLAGS = "-L${stdenv.lib.getLib json_c}/lib";
 
-  dontDisableStatic = true;
   preConfigure = ''
     sed -i 's@/usr/bin/file@${file}/bin/file@' configure
     configureFlags="--datadir=$out/share --datarootdir=$out/share --bindir=$out/bin --with-gdalconfig=${gdal}/bin/gdal-config --with-jsondir=${json_c.dev}"
+
     makeFlags="PERL=${perl}/bin/perl datadir=$out/share pkglibdir=$out/lib bindir=$out/bin"
+
+    # fix the build with clang/JIT support
+    # see https://trac.osgeo.org/postgis/ticket/4060 -- can be removed later
+    substituteInPlace ./postgis/Makefile.in \
+      --replace 'PG_CPPFLAGS +=' 'PG_CPPFLAGS += -I../liblwgeom'
+    substituteInPlace ./raster/rt_pg/Makefile.in \
+      --replace 'LIBPGCOMMON_CFLAGS="-I../../libpgcommon"' 'LIBPGCOMMON_CFLAGS=-I ../../liblwgeom -I../../libpgcommon'
   '';
+
   postConfigure = ''
     sed -i "s|@mkdir -p \$(DESTDIR)\$(PGSQL_BINDIR)||g ;
             s|\$(DESTDIR)\$(PGSQL_BINDIR)|$prefix/bin|g
@@ -94,10 +102,6 @@ in stdenv.mkDerivation rec {
             " \
         "raster/scripts/python/Makefile";
   '';
-
-  passthru = {
-    versionCheck = postgresql.hasJitSupport == false;
-  };
 
   meta = with stdenv.lib; {
     description = "Geographic Objects for PostgreSQL";
