6a3a1fa46790fe9b341bf53b35a7ea056763dbb7
Sage Raflik <neonfuz@gmail.com>
2018-08-05 22:53:13 -0700

    crispy-doom: init at 5.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19483,6 +19483,8 @@ with pkgs;
 
   chocolateDoom = callPackage ../games/chocolate-doom { };
 
+  crispyDoom = callPackage ../games/crispy-doom { };
+
   ckan = callPackage ../games/ckan { };
 
   cockatrice = libsForQt5.callPackage ../games/cockatrice {  };
