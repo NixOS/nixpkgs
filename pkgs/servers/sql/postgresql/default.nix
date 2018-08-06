8caf52331f6a3d8c1ddaf2aa7fd0530c6c748e0f
Austin Seipp <aseipp@pobox.com>
2018-08-06 10:19:07 -0500

    postgresql11: remove hard clang dependency for JIT support
    When JIT support was enabled, PGXS embedded a copy of clang-wrapper's
    path into its $out, resulting in a large increase in closure size due
    to dragging the whole compiler along. A similar problem existed
    previously by way of binutils 'ld', which was worked around. Perform
    that same workaround for clang-wrapper, too.
    
    With JIT support enabled, this reduces the closure size of
    postgresql11.out from ~1.4GB to ~410MB.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/servers/sql/postgresql/default.nix
+++ pkgs/servers/sql/postgresql/default.nix
@@ -83,9 +83,6 @@ let
         moveToOutput "lib/*.a" "$out"
         moveToOutput "lib/libecpg*" "$out"
 
-        # Prevent a retained dependency on gcc-wrapper.
-        substituteInPlace "$out/lib/pgxs/src/Makefile.global" --replace ${stdenv.cc}/bin/ld ld
-
         if [ -z "''${dontDisableStatic:-}" ]; then
           # Remove static libraries in case dynamic are available.
           for i in $out/lib/*.a; do
@@ -95,6 +92,12 @@ let
             fi
           done
         fi
+
+        # Prevent a retained dependency on gcc-wrapper.
+        substituteInPlace "$out/lib/pgxs/src/Makefile.global" --replace ${stdenv.cc}/bin/ld ld
+      '' + lib.optionalString jitEnabled ''
+        # In the case of JIT support, prevent a retained dependency on clang-wrapper, too
+        substituteInPlace "$out/lib/pgxs/src/Makefile.global" --replace ${stdenv.cc}/bin/clang clang
       '';
 
     postFixup = lib.optionalString (!stdenv.isDarwin && stdenv.hostPlatform.libc == "glibc")
@@ -105,7 +108,7 @@ let
 
     doInstallCheck = false; # needs a running daemon?
 
-    disallowedReferences = lib.optionals (!jitEnabled) [ stdenv.cc ];
+    disallowedReferences = [ stdenv.cc ];
 
     passthru = {
       # Note: we export 'stdenv', because the chosen stdenv *might* be a llvmPackages-based
