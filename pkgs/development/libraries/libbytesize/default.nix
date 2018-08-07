5ba079ed78f84634f3bf0fe5205e43803ff141c4
Jan Malakhovski <oxij@oxij.org>
2018-08-07 12:56:10 +0000

    libbytesize: fix tests, more or less

--- pkgs/development/libraries/libbytesize/default.nix
+++ pkgs/development/libraries/libbytesize/default.nix
@@ -1,12 +1,11 @@
 { stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, gettext
 , gtk-doc, libxslt, docbook_xml_dtd_43, docbook_xsl
-, python3, pcre, gmp, mpfr
+, python2Packages, python3, pcre, gmp, mpfr
 }:
 
-let
-  version = "1.3";
-in stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
   name = "libbytesize-${version}";
+  version = "1.3";
 
   src = fetchFromGitHub {
     owner = "storaged-project";
@@ -20,6 +19,17 @@ in stdenv.mkDerivation rec {
   nativeBuildInputs = [ autoreconfHook pkgconfig gettext gtk-doc libxslt docbook_xml_dtd_43 docbook_xsl python3 ];
 
   buildInputs = [ pcre gmp mpfr ];
+
+  # canary_tests.sh needs systemd =/ and some python modules not packaged in nixpkgs
+  postPatch = ''
+    sed -i 's/canary_tests.sh//g' tests/Makefile.am
+  '';
+
+  postConfigure = ''
+    patchShebangs tests
+  '';
+
+  checkInputs = with python2Packages; [ python six ];
 
   meta = with stdenv.lib; {
     description = "A tiny library providing a C “class” for working with arbitrary big sizes in bytes";
