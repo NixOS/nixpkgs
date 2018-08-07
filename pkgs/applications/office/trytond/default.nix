b07e617585f15d2f3d8b62c65c2a6259cee17df6
Udo Spallek <udono@virtual-things.biz>
2018-08-07 09:39:34 +0200

    trytond: 4.8.2 -> 4.8.3

--- pkgs/applications/office/trytond/default.nix
+++ pkgs/applications/office/trytond/default.nix
@@ -5,10 +5,10 @@ with stdenv.lib;
 
 python2Packages.buildPythonApplication rec {
   pname = "trytond";
-  version = "4.8.2";
+  version = "4.8.3";
   src = python2Packages.fetchPypi {
     inherit pname version;
-    sha256 = "ec5c419418517bf9f7d2af08b2bfd3bcc7526f6d6f07d122abf1699c728ae2a9";
+    sha256 = "11d01dc984d9274229b5c80f883679d130a55e14e9124b051274fce9aea4ef77";
   };
 
   # Tells the tests which database to use
