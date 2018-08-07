9e75b0132c4704e9b93bfd6fdcc67277db1b2c9e
CrazedProgrammer <crazedprogrammer@gmail.com>
2018-08-07 15:50:11 +0200

    ccemux: init at 1.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21117,6 +21117,8 @@ with pkgs;
 
   calaos_installer = libsForQt5.callPackage ../misc/calaos/installer {};
 
+  ccemux = callPackage ../misc/emulators/ccemux { };
+
   click = callPackage ../applications/networking/cluster/click { };
 
   cups = callPackage ../misc/cups {
