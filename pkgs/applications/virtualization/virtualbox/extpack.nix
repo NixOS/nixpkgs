8ccfb99ac07476a7d73518944c983d0fdc30643f
(cdep)illabout <cdep.illabout@gmail.com>
2018-08-08 23:22:02 +0900

    Add a derivation that downloads the virtualbox extension pack.

---
+++ pkgs/applications/virtualization/virtualbox/extpack.nix
@@ -0,0 +1,25 @@
+{stdenv, fetchurl, lib}:
+
+with lib;
+
+let extpackRev = "123301";
+    version = "5.2.14";
+in
+stdenv.mkDerivation rec {
+  name = "Oracle_VM_VirtualBox_Extension_Pack-${version}-${toString extpackRev}.vbox-extpack";
+  src = fetchurl {
+    url = "http://download.virtualbox.org/virtualbox/${version}/${name}";
+    sha256 = "d90c1b0c89de19010f7c7fe7a675ac744067baf29a9966b034e97b5b2053b37e";
+  };
+
+  installPhase = "cp -r . $out";
+  phases = ["installPhase"];
+
+  meta = {
+    description = "Oracle Extension pack for VirtualBox";
+    license = licenses.virtualbox-puel;
+    homepage = https://www.virtualbox.org/;
+    maintainers = with maintainers; [ flokli sander cdepillabout ];
+    platforms = [ "x86_64-linux" "i686-linux" ];
+  };
+}
