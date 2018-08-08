5d485b34eed287328d8cde5297e92530073325c7
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-08-07 21:35:03 -0400

    pythonPackages.sortedcontainers: 1.5.7 -> 2.0.4
    Moving sortedcontianers from top-level -> python-modules and updating
    to newest sortedcontianers release.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -501,6 +501,8 @@ in {
 
   sip = callPackage ../development/python-modules/sip { };
 
+  sortedcontainers = callPackage ../development/python-modules/sortedcontainers { };
+
   sklearn-deap = callPackage ../development/python-modules/sklearn-deap { };
 
   slackclient = callPackage ../development/python-modules/slackclient { };
@@ -7593,26 +7595,6 @@ in {
   mistune = callPackage ../development/python-modules/mistune { };
 
   brotlipy = callPackage ../development/python-modules/brotlipy { };
-
-  sortedcontainers = buildPythonPackage rec {
-    name = "sortedcontainers-${version}";
-    version = "1.5.7";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/s/sortedcontainers/${name}.tar.gz";
-      sha256 = "1sjh8lccbmvwna91mlhl5m3z4320p07h063b8x8br4p4cll49w0g";
-    };
-
-    # tries to run tests for all python versions and uses virtualenv weirdly
-    doCheck = false;
-    #buildInputs = with self; [ tox nose ];
-
-    meta = {
-      description = "Python Sorted Container Types: SortedList, SortedDict, and SortedSet";
-      homepage = "http://www.grantjenks.com/docs/sortedcontainers/";
-      license = licenses.asl20;
-    };
-  };
 
   sortedcollections = buildPythonPackage rec {
     name = "sortedcollections-${version}";
