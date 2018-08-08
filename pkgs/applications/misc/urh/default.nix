bb19ab7864ee52eb5a6d3965f9e997ed9d2d3196
R. RyanTM <ryantm+bot@ryantm.com>
2018-08-07 21:58:30 -0700

    urh: 2.2.2 -> 2.2.3
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools. This update was made based on information from https://repology.org/metapackage/urh/versions.

--- pkgs/applications/misc/urh/default.nix
+++ pkgs/applications/misc/urh/default.nix
@@ -2,13 +2,13 @@
 
 python3Packages.buildPythonApplication rec {
   name = "urh-${version}";
-  version = "2.2.2";
+  version = "2.2.3";
 
   src = fetchFromGitHub {
     owner = "jopohl";
     repo = "urh";
     rev = "v${version}";
-    sha256 = "0ncvfl5iyngw3lr2g7awpskrrld6y0x7w0xyp827lcr7x73fvqgp";
+    sha256 = "1iq84590cjpf2rlxb60fy4hxi7vir27bbb10axbwrqwnp5cc4bql";
   };
 
   buildInputs = [ hackrf rtl-sdr ];
