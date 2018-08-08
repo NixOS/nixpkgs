05890dda3db11a77c13d4aa88084bc6a6680e772
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-08-08 15:30:26 -0400

    pythonPackages.joblib: 0.12.1 -> 0.12.2
    In order to get all the tests passing the Github repository was
    downloaded instead of from pypi so that the `conftest.py` is
    available.
    
    In addition to updating the version:
    
      - compatible with 2.7, 3+ now
      - all tests are running and passing

--- pkgs/development/python-modules/joblib/default.nix
+++ pkgs/development/python-modules/joblib/default.nix
@@ -1,29 +1,36 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 , sphinx
 , numpydoc
 , pytest
+, python-lz4
 }:
 
 
 buildPythonPackage rec {
   pname = "joblib";
-  version = "0.12.1";
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "68e6128e4734196616a39e2d48830ec7d61551c7f5748849e4c91478d2444524";
+  version = "0.12.4";
+
+  # get full repository inorder to run tests
+  src = fetchFromGitHub {
+    owner = "joblib";
+    repo = pname;
+    rev = version;
+    sha256 = "06zszgp7wpa4jr554wkk6kkigp4k9n5ad5h08i6w9qih963rlimb";
   };
 
   checkInputs = [ sphinx numpydoc pytest ];
+  propagatedBuildInputs = [ python-lz4 ];
 
   checkPhase = ''
-    py.test -k 'not test_disk_used and not test_nested_parallel_warnings' joblib/test
+    py.test joblib
   '';
 
   meta = {
     description = "Lightweight pipelining: using Python functions as pipeline jobs";
     homepage = https://pythonhosted.org/joblib/;
     license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [ costrouc ];
   };
 }
