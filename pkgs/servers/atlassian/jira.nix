9362fb6af4d7082a62c183f7fcd8de4af4a057da
Simon Lackerbauer <simon@lackerbauer.com>
2018-08-08 10:50:32 +0200

    atlassian-jira: 7.11.0 -> 7.11.1
    (cherry picked from commit 093eb27a9e5793343f6506d063debb3d11794ac1)

--- pkgs/servers/atlassian/jira.nix
+++ pkgs/servers/atlassian/jira.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   name = "atlassian-jira-${version}";
-  version = "7.11.0";
+  version = "7.11.1";
 
   src = fetchurl {
     url = "https://downloads.atlassian.com/software/jira/downloads/atlassian-jira-software-${version}.tar.gz";
-    sha256 = "0w2fgs5n2zdvxgcx2rn010nz81z4q3z6cbq9hmpyzxy9ygjby2w4";
+    sha256 = "0zcpxh8713f0v4jmczcvsnqjjvlmrmc0d76mkivn0b294z2s8sp4";
   };
 
   phases = [ "unpackPhase" "buildPhase" "installPhase" "fixupPhase" ];
