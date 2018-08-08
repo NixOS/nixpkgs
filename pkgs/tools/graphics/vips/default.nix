42c56458eb077e7fc999b69928fa7c4e95c821d8
R. RyanTM <ryantm+bot@ryantm.com>
2018-08-08 01:03:06 -0700

    vips: 8.6.4 -> 8.6.5
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools. This update was made based on information from https://repology.org/metapackage/vips/versions.

--- pkgs/tools/graphics/vips/default.nix
+++ pkgs/tools/graphics/vips/default.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   name = "vips-${version}";
-  version = "8.6.4";
+  version = "8.6.5";
 
   src = fetchurl {
     url = "https://github.com/jcupitt/libvips/releases/download/v${version}/${name}.tar.gz";
-    sha256 = "1x4ai997yfl4155r4k3m5fa5hj3030c4abi5g49kfarbr60a0ca6";
+    sha256 = "1nymm4vzscb68aifin9q742ff64b4k4ddppq1060w8hf6h7ay0l7";
   };
 
   buildInputs =
