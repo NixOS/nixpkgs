e665aa3163ae3b9548caccf635ff1ebb0a3fba9d
R. RyanTM <ryantm+bot@ryantm.com>
2018-08-08 17:11:52 -0700

    geos: 3.6.2 -> 3.6.3
    Semi-automatic update generated by https://github.com/ryantm/nixpkgs-update tools. This update was made based on information from https://repology.org/metapackage/geos/versions.

--- pkgs/development/libraries/geos/default.nix
+++ pkgs/development/libraries/geos/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, python }:
 
 stdenv.mkDerivation rec {
-  name = "geos-3.6.2";
+  name = "geos-3.6.3";
 
   src = fetchurl {
     url = "https://download.osgeo.org/geos/${name}.tar.bz2";
-    sha256 = "0ak5szby29l9l0vy43dm5z2g92xzdky20q1gc1kah1fnhkgi6nh4";
+    sha256 = "0jrypv61rbyp7vi9qpnnaiigjj8cgdqvyk8ymik8h1ppcw5am7mb";
   };
 
   enableParallelBuilding = true;
