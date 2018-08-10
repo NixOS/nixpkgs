ae65ddc8de520a0ddf38171cd7b287018ed6c58a
Youfu Zhang <1315097+zhangyoufu@users.noreply.github.com>
2018-08-10 11:52:16 +0800

    add comment for issue #28620

--- nixos/modules/tasks/network-interfaces-scripted.nix
+++ nixos/modules/tasks/network-interfaces-scripted.nix
@@ -473,6 +473,9 @@ let
               # Remove Dead Interfaces
               ip link show "${n}" >/dev/null 2>&1 && ip link delete "${n}"
               ip link add link "${v.interface}" name "${n}" type vlan id "${toString v.id}"
+              # Try to bring up vlan interface
+              # Note: When master interface is down, we cannot bring vlan interfaces up (#28620)
+              #       Vlan interfaces will be automatically bring up when master is up
               ip link set "${n}" up || true
             '';
             postStop = ''
