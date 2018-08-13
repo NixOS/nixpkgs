95ae8956cdc3f96baf1313ffb39e015fa62acfbc
Frederik Rietdijk <fridh@fridh.nl>
2018-08-13 09:34:57 +0200

    python: zodbpickle: 1.0.1 -> 1.0.2

--- pkgs/development/python-modules/zodbpickle/default.nix
+++ pkgs/development/python-modules/zodbpickle/default.nix
@@ -5,12 +5,12 @@
 
 buildPythonPackage rec {
   pname = "zodbpickle";
-  version = "1.0.1";
+  version = "1.0.2";
   disabled = isPyPy; # https://github.com/zopefoundation/zodbpickle/issues/10
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "5fdfa84f05b25511a4e1190ec98728aa487e2eb6db016a951fdbb79bcc7307e0";
+    sha256 = "f26e6eba6550ff1575ef2f2831fc8bc0b465f17f9757d0b6c7db55fab5702061";
   };
 
   # fails..
