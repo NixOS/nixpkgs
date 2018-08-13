f2e647b662ad82e463e6916863f35e3536349114
Frederik Rietdijk <fridh@fridh.nl>
2018-08-13 09:30:11 +0200

    python: pycryptodome: 3.6.4 -> 3.6.5

--- pkgs/development/python-modules/pycryptodome/default.nix
+++ pkgs/development/python-modules/pycryptodome/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchPypi, buildPythonPackage }:
 
 buildPythonPackage rec {
-  version = "3.6.4";
+  version = "3.6.5";
   pname = "pycryptodome";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "9c7790ffd291c81b934fe0ca8155a67235d33f70d4914bbf7467a447d9dbcb09";
+    sha256 = "99d653f3a92f35e3c768a142aa83c8c7b104a787655c51e25dca89ed778960b8";
   };
 
   meta = {
