68dd59345ab29685fad5cfa723242e09ec3620ef
Frederik Rietdijk <fridh@fridh.nl>
2018-08-13 09:31:26 +0200

    python: pytest-asyncio: 0.8.0 -> 0.9.0

--- pkgs/development/python-modules/pytest-asyncio/default.nix
+++ pkgs/development/python-modules/pytest-asyncio/default.nix
@@ -1,13 +1,13 @@
 { stdenv, buildPythonPackage, fetchPypi, pytest, isPy3k }:
 buildPythonPackage rec {
   pname = "pytest-asyncio";
-  version = "0.8.0";
+  version = "0.9.0";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f32804bb58a66e13a3eda11f8942a71b1b6a30466b0d2ffe9214787aab0e172e";
+    sha256 = "fbd92c067c16111174a1286bfb253660f1e564e5146b39eeed1133315cf2c2cf";
   };
 
   buildInputs = [ pytest ];
