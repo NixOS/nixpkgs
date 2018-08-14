NixOS/nixpkgs

commit 5523ec8f3c78704c6e76b7675bfce41d24a3feb1

Make getHaskellBuildInputs / shellFor work with overrideCabal

pkgs/development/haskell-modules/lib.nix
@@ -297,8 +297,11 @@ rec {
   # This is useful to build environments for developing on that
   # package.
   getHaskellBuildInputs = p:
-    (p.override { mkDerivation = extractBuildInputs p.compiler;
-                }).haskellBuildInputs;
+    (overrideCabal p (args: {
+      passthru = (args.passthru or {}) // {
+        _getHaskellBuildInputs = extractBuildInputs p.compiler args;
+      };
+    }))._getHaskellBuildInputs;
 
   # Under normal evaluation, simply return the original package. Under
   # nix-shell evaluation, return a nix-shell optimized environment.

pkgs/development/haskell-modules/make-package-set.nix
@@ -38,7 +38,7 @@ let
   inherit (stdenv) buildPlatform hostPlatform;
 
   inherit (stdenv.lib) fix' extends makeOverridable;
-  inherit (haskellLib) overrideCabal;
+  inherit (haskellLib) overrideCabal getHaskellBuildInputs;
 
   mkDerivationImpl = pkgs.callPackage ./generic-builder.nix {
     inherit stdenv;
@@ -229,7 +229,7 @@ in package-set { inherit pkgs stdenv callPackage; } self // {
     shellFor = { packages, withHoogle ? false, ... } @ args:
       let
         selected = packages self;
-        packageInputs = builtins.map (p: p.override { mkDerivation = haskellLib.extractBuildInputs p.compiler; }) selected;
+        packageInputs = builtins.map getHaskellBuildInputs selected;
         haskellInputs =
           builtins.filter
             (input: pkgs.lib.all (p: input.outPath != p.outPath) selected)

commit 7372d3bf31bf4cc4a767eb52cdd0bbaff1f31058

haskell.lib.getHaskellBuildInputs: Actually get the haskell build inputs.
5523ec8f3c78704c6e76b7675bfce41d24a3feb1 accidentally dropped the attribute lookup.

pkgs/development/haskell-modules/lib.nix
@@ -301,7 +301,7 @@ rec {
   getHaskellBuildInputs = p:
     (overrideCabal p (args: {
       passthru = (args.passthru or {}) // {
-        _getHaskellBuildInputs = extractBuildInputs p.compiler args;
+        _getHaskellBuildInputs = (extractBuildInputs p.compiler args).haskellBuildInputs;
       };
     }))._getHaskellBuildInputs;
 