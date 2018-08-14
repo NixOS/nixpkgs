a0649086b50d26ff82212af5ba45bd85ff12e73c
Tim Steinbach <tim@nequissimus.com>
2018-08-13 20:18:56 -0400

    zfs: unsupported for 4.18

--- pkgs/os-specific/linux/zfs/default.nix
+++ pkgs/os-specific/linux/zfs/default.nix
@@ -157,7 +157,7 @@ in {
   # to be adapted
   zfsStable = common {
     # comment/uncomment if breaking kernel versions are known
-    incompatibleKernelVersion = null;
+    incompatibleKernelVersion = "4.18";
 
     # this package should point to the latest release.
     version = "0.7.9";
@@ -176,7 +176,7 @@ in {
 
   zfsUnstable = common rec {
     # comment/uncomment if breaking kernel versions are known
-    incompatibleKernelVersion = null;
+    incompatibleKernelVersion = "4.18";
 
     # this package should point to a version / git revision compatible with the latest kernel release
     version = "2018-05-22";
