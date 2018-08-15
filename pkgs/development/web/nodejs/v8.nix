b421a7d0a60ca52b47aa25f990c195936da4de2b
Matthew Bauer <mjbauer95@gmail.com>
2018-08-15 17:35:56 -0500

    nodejs: remove ‘no-xcode’ patches
    now provided by xcbuild

--- pkgs/development/web/nodejs/v8.nix
+++ pkgs/development/web/nodejs/v8.nix
@@ -7,5 +7,4 @@ in
     inherit enableNpm;
     version = "8.11.3";
     sha256 = "1q3fc791ng1sgk0i5qnxpxri7235nkjm50zx1z34c759vhgpaz2p";
-    patches = lib.optionals stdenv.isDarwin [ ./no-xcode-v7.patch ./no-xcodebuild.patch ];
   }
