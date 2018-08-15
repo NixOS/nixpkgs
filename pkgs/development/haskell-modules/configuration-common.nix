08f48236d20a61a5ab4da1428d7db19bbdb5832f
Peter Simons <simons@cryp.to>
2018-08-15 09:34:17 +0200

    cabal2nix: update hpack override to the new version

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -1073,16 +1073,15 @@ self: super: {
 
   # cabal2nix requires hpack >= 0.29.6 but the LTS has hpack-0.28.2.
   # Lets remove this once the LTS has upraded to 0.29.6.
-  hpack = super.hpack_0_29_6;
+  hpack = super.hpack_0_29_7;
 
-  cabal2nix =
-    # The test suite does not know how to find the 'cabal2nix' binary.
-    overrideCabal super.cabal2nix (drv: {
-      preCheck = ''
-        export PATH="$PWD/dist/build/cabal2nix:$PATH"
-        export HOME="$TMPDIR/home"
-      '';
-    });
+  # The test suite does not know how to find the 'cabal2nix' binary.
+  cabal2nix = overrideCabal super.cabal2nix (drv: {
+    preCheck = ''
+      export PATH="$PWD/dist/build/cabal2nix:$PATH"
+      export HOME="$TMPDIR/home"
+    '';
+  });
 
   # Break out of "aeson <1.3, temporary <1.3".
   stack = doJailbreak super.stack;
