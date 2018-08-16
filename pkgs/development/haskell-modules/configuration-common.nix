816f4ef18879a60734c3223d1b65abca520d9ee6
Shea Levy <shea@shealevy.com>
2018-08-16 15:16:54 -0400

    haskellPackages.cabal-plan: Remove broken patch.

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -951,12 +951,6 @@ self: super: {
   # https://github.com/yesodweb/Shelly.hs/issues/162
   shelly = dontCheck super.shelly;
 
-  # Support ansi-terminal 0.7.x.
-  cabal-plan = appendPatch super.cabal-plan (pkgs.fetchpatch {
-    url = "https://github.com/haskell-hvr/cabal-plan/pull/16.patch";
-    sha256 = "0i889zs46wn09d7iqdy99201zaqxb175cfs8jz2zi3mv4ywx3a0l";
-  });
-
   # https://github.com/simonmichael/hledger/issues/852
   hledger-lib = appendPatch super.hledger-lib (pkgs.fetchpatch {
     url = "https://github.com/simonmichael/hledger/commit/007b9f8caaf699852511634752a7d7c86f6adc67.patch";
