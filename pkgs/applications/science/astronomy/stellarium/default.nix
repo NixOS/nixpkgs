c274490146253c7c029f1c043a0e8fdd6eba5894
R. RyanTM <ryantm+bot@ryantm.com>
2018-08-16 12:03:22 -0700

    stellarium: 0.18.1 -> 0.18.2
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/stellarium/versions.

--- pkgs/applications/science/astronomy/stellarium/default.nix
+++ pkgs/applications/science/astronomy/stellarium/default.nix
@@ -6,13 +6,13 @@
 
 mkDerivation rec {
   name = "stellarium-${version}";
-  version = "0.18.1";
+  version = "0.18.2";
 
   src = fetchFromGitHub {
     owner = "Stellarium";
     repo = "stellarium";
     rev = "v${version}";
-    sha256 = "0srwi08azzzayf50dr4dr1zcdcc8hwribzv7xvb7hbp6xp51c813";
+    sha256 = "17764i52dmipaz62k5n23fyx07d7cjir0dgav0s5b6sjd8gbjwbf";
   };
 
   nativeBuildInputs = [ cmake perl ];
