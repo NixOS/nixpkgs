c218a9cfe0f02d2b5f983de71334be0faf5be336
Stephen <stephengroat@users.noreply.github.com>
2018-08-16 13:14:36 -0700

    terraform_0_11: 0.11.7 -> 0.11.8 (#45117)

--- pkgs/applications/networking/cluster/terraform/default.nix
+++ pkgs/applications/networking/cluster/terraform/default.nix
@@ -104,8 +104,8 @@ in rec {
   terraform_0_10-full = terraform_0_10.withPlugins lib.attrValues;
 
   terraform_0_11 = pluggable (generic {
-    version = "0.11.7";
-    sha256 = "0q5gl8yn1f8fas1v68lz081k88gbmlk7f2xqlwqmh01qpqjxd42q";
+    version = "0.11.8";
+    sha256 = "1kdmx21l32vj5kvkimkx0s5mxgmgkdwlgbin4f3iqjflzip0cddh";
     patches = [ ./provider-path.patch ];
     passthru = { inherit plugins; };
   });
