928a53cd6c48f2678230b8f171d4775dd8c2f3f7
Herwig Hochleitner <herwig@bendlas.net>
2018-08-16 09:50:06 +0200

    dropbox: update installer base version
    cc @ttuegel

--- pkgs/applications/networking/dropbox/default.nix
+++ pkgs/applications/networking/dropbox/default.nix
@@ -7,7 +7,7 @@ assert lib.elem stdenv.system platforms;
 # Dropbox client to bootstrap installation.
 # The client is self-updating, so the actual version may be newer.
 let
-  version = "52.3.56";
+  version = "55.4.171";
 
   arch = {
     "x86_64-linux" = "x86_64";
