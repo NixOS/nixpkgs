2058c69de08dc2ca49c3ac683268c712c82da971
Daiderd Jordan <daiderd@gmail.com>
2018-08-16 20:48:37 +0200

    grpc: fix darwin build

--- pkgs/development/libraries/grpc/default.nix
+++ pkgs/development/libraries/grpc/default.nix
@@ -9,6 +9,7 @@ stdenv.mkDerivation rec {
   };
   nativeBuildInputs = [ cmake pkgconfig ];
   buildInputs = [ zlib c-ares c-ares.cmake-config openssl protobuf gflags ];
+
   cmakeFlags =
     [ "-DgRPC_ZLIB_PROVIDER=package"
       "-DgRPC_CARES_PROVIDER=package"
@@ -16,11 +17,19 @@ stdenv.mkDerivation rec {
       "-DgRPC_PROTOBUF_PROVIDER=package"
       "-DgRPC_GFLAGS_PROVIDER=package"
     ];
+
+  # CMake creates a build directory by default, this conflicts with the
+  # basel BUILD file on case-insensitive filesystems.
+  preConfigure = ''
+    rm -vf BUILD
+  '';
+
   enableParallelBuilds = true;
 
   meta = with stdenv.lib; {
     description = "The C based gRPC (C++, Python, Ruby, Objective-C, PHP, C#)";
     license = licenses.asl20;
+    maintainers = [ maintainers.lnl7 ];
     homepage = https://grpc.io/;
   };
 }
