c8adb511eb230eaad41c5d2a053ffe4fd8db3555
R. RyanTM <ryantm+bot@ryantm.com>
2018-08-16 22:56:25 -0700

    mcelog: 159 -> 160
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/mcelog/versions.

--- pkgs/os-specific/linux/mcelog/default.nix
+++ pkgs/os-specific/linux/mcelog/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "mcelog-${version}";
-  version = "159";
+  version = "160";
 
   src = fetchFromGitHub {
     owner  = "andikleen";
     repo   = "mcelog";
     rev    = "v${version}";
-    sha256 = "1w8y4igxi48r2d9s6g9fm1bgmsga94gfz6x0xaln6rhvbgi318xg";
+    sha256 = "1m985wvdykl3003967lp1i7707qhwdj3h13cl8g1afjaip9ccd48";
   };
 
   postPatch = ''
