8ac25594e4be7cdfd567f888db097d7af0037775
Susan Potter <me@susanpotter.net>
2018-08-17 12:07:11 -0500

    rubygems: use official HTTPS src location

--- pkgs/development/interpreters/ruby/rubygems-src.nix
+++ pkgs/development/interpreters/ruby/rubygems-src.nix
@@ -3,6 +3,6 @@
 , sha256 ? "1jsmmd31j8j066b83lin4bbqz19jhrirarzb41f3sjhfdjiwkcjc"
 }:
 fetchurl {
-  url = "http://production.cf.rubygems.org/rubygems/rubygems-${version}.tgz";
+  url = "https://rubygems.org/rubygems/rubygems-${version}.tgz";
   sha256 = sha256;
 }
