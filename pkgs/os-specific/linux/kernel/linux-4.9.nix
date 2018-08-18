80448a474723ec8c8a23f43068559f1fbee239ba
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-08-17 20:16:44 -0700

    linux: 4.9.120 -> 4.9.121

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.120";
+  version = "4.9.121";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "14gx6gqahz74vaw8jd0wkxn0w05i7cyfgi24ld2q3p2yhq3gannp";
+    sha256 = "0q8naw5jqpwa2ylwkbw88yz6zcnkr5jma91rr2xnll02mg8ri35s";
   };
 } // (args.argsOverride or {}))
