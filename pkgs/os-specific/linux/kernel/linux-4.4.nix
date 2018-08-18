928bc3f864dc721905de9d3a04814803ea112a6c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-08-17 20:18:03 -0700

    linux: 4.4.148 -> 4.4.149

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.148";
+  version = "4.4.149";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "10yrqizwkawbs332rl3fmr3cpwcl2j0mik4md7isg5xlkc00zc8r";
+    sha256 = "0v0w3f9jyhr34vcjivfwdxiz7gpl51532d7492ppzivmx1shqqf2";
   };
 } // (args.argsOverride or {}))
