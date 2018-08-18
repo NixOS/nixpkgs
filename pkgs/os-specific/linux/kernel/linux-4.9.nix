017bfce67c3f1011848f7d34d880923b06301118
Edmund Wu <fangkazuto@gmail.com>
2018-08-18 08:27:29 -0400

    linux: 4.9.121 -> 4.9.122

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.121";
+  version = "4.9.122";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0q8naw5jqpwa2ylwkbw88yz6zcnkr5jma91rr2xnll02mg8ri35s";
+    sha256 = "0v7qdkdlgpv83v4lzm59jgaxy1l7dzkqjr3fcahqrnrcdf3r0vx4";
   };
 } // (args.argsOverride or {}))
