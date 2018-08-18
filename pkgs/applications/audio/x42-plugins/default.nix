a4e013b02f11cfe097c382ef8cf7e32a1d747761
Bart Brouns <bart@magnetophon.nl>
2018-08-18 19:01:15 +0200

    x42-plugins: 20180803 > 20180812

--- pkgs/applications/audio/x42-plugins/default.nix
+++ pkgs/applications/audio/x42-plugins/default.nix
@@ -3,12 +3,12 @@
 , libGLU, lv2, gtk2, cairo, pango, fftwFloat, zita-convolver }:
 
 stdenv.mkDerivation rec {
-  version = "20180803";
+  version = "20180812";
   name = "x42-plugins-${version}";
 
   src = fetchurl {
     url = "https://gareus.org/misc/x42-plugins/${name}.tar.xz";
-    sha256 = "1v7p6vnkcbzyvmcysabhmn603cndzx9mwzaw5dppy4wd687vhgis";
+    sha256 = "0gzwzxpa2k2w9c6j3pspwi9slfyd57wb192d6yqcg92pfmnxy9dz";
   };
 
   nativeBuildInputs = [ pkgconfig ];
