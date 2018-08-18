e2c053d207f327734f078a3865904990a5e23a5e
Edmund Wu <fangkazuto@gmail.com>
2018-08-18 08:27:12 -0400

    linux: 4.4.149 -> 4.4.150

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.149";
+  version = "4.4.150";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0v0w3f9jyhr34vcjivfwdxiz7gpl51532d7492ppzivmx1shqqf2";
+    sha256 = "1xdfq11pa4ayi89vynbddq5k47f01szc04lbl5aaxpnch982jj8g";
   };
 } // (args.argsOverride or {}))
