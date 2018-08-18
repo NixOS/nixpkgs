6ce5555bb33f85eb150d7a67f32b395d00c9b8ff
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-08-17 20:12:56 -0700

    linux: 4.17.15 -> 4.17.16
    (cherry picked from commit 2366de6c0b1dfc9703846f5f117604df3ffaa380)

--- pkgs/os-specific/linux/kernel/linux-4.17.nix
+++ pkgs/os-specific/linux/kernel/linux-4.17.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.17.15";
+  version = "4.17.16";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0yg0hh1ld3d5cd4ll7f48p769wda2a1ap8fmnnsfsazidka5vf75";
+    sha256 = "1dd1cf28ai9y47dic3y084c5ajwcqijdmx2g9xjb0llbybm3kw1g";
   };
 } // (args.argsOverride or {}))
