de61d0da203889a6ebd07085a406ac8d3e90ee30
Michiel Leenaars <ml.software@leenaa.rs>
2018-08-19 09:33:14 +0200

    ydiff: init at 1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19416,6 +19416,8 @@ with pkgs;
 
   yate = callPackage ../applications/misc/yate { };
 
+  ydiff = callPackage ../development/tools/ydiff { };
+
   yed = callPackage ../applications/graphics/yed {};
 
   inherit (gnome3) yelp;
