NixOS/nixpkgs

commit 59027e28800dd5c2cd1c9d434c1a71d39be19266

Revert "libvirt: fix dlopen("libjansson.so.4")"
This reverts commit d46cfd2ff087816697f46d69f9f795ff878301c5, which
broke libvirt + some dependent packages (notably nixops) on Darwin

pkgs/development/libraries/libvirt/default.nix
@@ -59,10 +59,6 @@ in stdenv.mkDerivation rec {
     substituteInPlace src/lxc/lxc_conf.c \
       --replace 'lxc_path,' '"/run/libvirt/nix-emulators/libvirt_lxc",'
 
-    [ -f ${jansson}/lib/libjansson.so.4 ] || exit 1
-    substituteInPlace src/util/virjsoncompat.c \
-      --replace '"libjansson.so.4"' '"${jansson}/lib/libjansson.so.4"'
-
     patchShebangs . # fixes /usr/bin/python references
   '';
 

commit 929a53f2814982247f34fa296f6c1dd0435caf20

libvirt: fix dlopen("libjansson.so.4")
Commit 59027e2 broke libvirt broke libvirt/nixops on linux.
Use ${targetPlatform.extensions.sharedLibrary} to accomodate both linux and darwin.

pkgs/development/libraries/libvirt/default.nix
@@ -60,7 +60,9 @@ in stdenv.mkDerivation rec {
       --replace 'lxc_path,' '"/run/libvirt/nix-emulators/libvirt_lxc",'
 
     patchShebangs . # fixes /usr/bin/python references
-  '';
+    substituteInPlace src/util/virjsoncompat.c --replace \
+        '"libjansson.so.4"' '"${jansson}/lib/libjansson${stdenv.targetPlatform.extensions.sharedLibrary}"'
+   '';
 
   configureFlags = [
     "--localstatedir=/var"