f45cefe91a87e0c407754e10ece27b61dc43a400
Daniël de Kok <me@danieldk.eu>
2018-08-19 08:26:01 +0200

    resilio-sync: 2.5.12 -> 2.6.0
    Changelog: https://help.resilio.com/hc/en-us/articles/206216855-Sync-2-x-change-log
    (cherry picked from commit 6eb4d6fbd2003e0707ed0fc1ac637a5559742236)

--- pkgs/applications/networking/resilio-sync/default.nix
+++ pkgs/applications/networking/resilio-sync/default.nix
@@ -9,13 +9,13 @@ let
 
 in stdenv.mkDerivation rec {
   name = "resilio-sync-${version}";
-  version = "2.5.12";
+  version = "2.6.0";
 
   src = fetchurl {
     url = "https://download-cdn.resilio.com/${version}/linux-${arch}/resilio-sync_${arch}.tar.gz";
     sha256 = {
-      "x86_64-linux" = "176gf5704wh22kqig8n0gg83048w71grw7h37k2qvxjaf4vypnc7";
-      "i686-linux"   = "15m2s2cqrkpb7ladbwimbs9agqb4ww9jr4gf0ifjwzm2grv5ffnh";
+      "x86_64-linux" = "0041axi9carspkfaxvyirfvsa29zz55al01x90nh93nzxvpvywsz";
+      "i686-linux"   = "1ar36lp4f6a1z9i82g3gpak4q4ny09faqxdd59q1pvfzq25ypdhs";
     }.${stdenv.system};
   };
 
