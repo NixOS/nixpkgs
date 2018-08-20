b712736283a465aec9420534727c71459d6d1839
WilliButz <wbutz@cyberfnord.de>
2018-08-21 01:41:15 +0200

    prometheus-json-exporter: 2016-09-13 -> 2017-10-06

--- pkgs/servers/monitoring/prometheus/json-exporter.nix
+++ pkgs/servers/monitoring/prometheus/json-exporter.nix
@@ -3,23 +3,23 @@
 
 buildGoPackage rec {
   name = "prometheus-json-exporter-${version}";
-  version = "unstable-2016-09-13";
-  rev = "d45e5ebdb08cb734ad7a8683966032af1d91a76c";
+  version = "unstable-2017-10-06";
 
   goPackagePath = "github.com/kawamuray/prometheus-json-exporter";
 
   src = fetchFromGitHub {
-    inherit rev;
     owner = "kawamuray";
     repo = "prometheus-json-exporter";
-    sha256 = "0v3as7gakdqpsir97byknsrqxxxkq66hp23j4cscs45hsdb24pi9";
+    rev = "51e3dc02a30ab818bb73e5c98c3853231c2dbb5f";
+    sha256 = "1v1p4zcqnb3d3rm55r695ydn61h6gz95f55cpa22hzw18dasahdh";
   };
 
   goDeps = ./json-exporter_deps.nix;
 
-  meta = {
+  meta = with lib; {
     description = "A prometheus exporter which scrapes remote JSON by JSONPath";
     homepage = https://github.com/kawamuray/prometheus-json-exporter;
-    license = lib.licenses.asl20;
+    license = licenses.asl20;
+    maintainers = with maintainers; [ willibutz ];
   };
 }
