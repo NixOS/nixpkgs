19db7d77544302d0b807ee63adfd54ba57efe22a
Jona Stubbe <nixos@crystalgamma.de>
2018-08-20 17:27:47 +0200

    lib/systems, stdenv: syntax cleanup

--- lib/systems/platforms.nix
+++ lib/systems/platforms.nix
@@ -474,6 +474,6 @@ rec {
       "armv7l-linux" = armv7l-hf-multiplatform;
       "aarch64-linux" = aarch64-multiplatform;
       "mipsel-linux" = fuloong2f_n32;
-      powerpc64le-linux = powernv;
+      "powerpc64le-linux" = powernv;
     }.${system} or pcBase;
 }
