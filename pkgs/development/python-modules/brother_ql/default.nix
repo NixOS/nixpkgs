87b8a1a48a56e4b2b7c2f283a6ba677659d739eb
Daniel Poelzleithner <git@poelzi.org>
2018-08-21 17:15:12 +0200

    fix long description

--- pkgs/development/python-modules/brother_ql/default.nix
+++ pkgs/development/python-modules/brother_ql/default.nix
@@ -14,9 +14,9 @@ buildPythonPackage rec {
   propagatedBuildInputs = [ future packbits pillow pyusb ];
 
   meta = with stdenv.lib; {
-    description = ''Python package for the raster language protocol of the Brother QL series\
-                   label printers (QL-500, QL-550, QL-570, QL-700, QL-710W, QL-720NW, QL-800,\
-                   QL-820NWB, QL-1050 and more)'';
+    description = "Python package for the raster language protocol of the Brother QL series label printers";
+    long_description = ''Python package for the raster language protocol of the Brother QL series label printers
+     (QL-500, QL-550, QL-570, QL-700, QL-710W, QL-720NW, QL-800, QL-820NWB, QL-1050 and more)'';
     homepage = https://github.com/pklaus/brother_ql;
     license = licenses.gpl3;
     maintainers = with maintainers; [ poelzi ];
