e18d57f0e2e826497b92db6a9e6a29e8bf5012a6
Will Dietz <github@wdtz.org>
2018-08-21 12:05:58 -0500

    iosevka-bin: 1.14.3 -> 2.0.0 (#45407)
    "iosevka" needs some work to update to 2.0.0
    (config file must be used instead of arguments to make)
    but don't know that the two necessarily need to be at the same version.
    
    Also I "think" that using the 'ttc' zip is the equivalent of what
    was previously the "pack" but noting this here since haven't found
    this documented elsewhere and am not entirely sure.

--- pkgs/data/fonts/iosevka/bin.nix
+++ pkgs/data/fonts/iosevka/bin.nix
@@ -1,18 +1,18 @@
 { stdenv, fetchzip }:
 
 let
-  version = "1.14.3";
+  version = "2.0.0";
 in fetchzip rec {
   name = "iosevka-bin-${version}";
 
-  url = "https://github.com/be5invis/Iosevka/releases/download/v${version}/iosevka-pack-${version}.zip";
+  url = "https://github.com/be5invis/Iosevka/releases/download/v${version}/ttc-iosevka-${version}.zip";
 
   postFetch = ''
     mkdir -p $out/share/fonts
     unzip -j $downloadedFile \*.ttc -d $out/share/fonts/iosevka
   '';
 
-  sha256 = "0qc5i6ijr25d2jwi5r4bcvbaw74y1p05a5fvlwss3l9rhmmxsfpl";
+  sha256 = "17ldxs8rn4y5mzpc6h5rms4khk9fp4d1ixz5bh0pglh1kdansz45";
 
   meta = with stdenv.lib; {
     homepage = https://be5invis.github.io/Iosevka/;
