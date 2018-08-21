27f66004194ca05f172c905b4f2d1c7aacf8cb06
R. RyanTM <ryantm-bot@ryantm.com>
2018-08-21 14:43:27 -0700

    nodejs-slim-10_x: 10.7.0 -> 10.9.0 (#45172)
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/nodejs-slim/versions.

--- pkgs/development/web/nodejs/v10.nix
+++ pkgs/development/web/nodejs/v10.nix
@@ -5,6 +5,6 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "10.7.0";
-    sha256 = "0qp93ddbnvadimj11wnznwhkq8vq1f7q259iq8siy5b7r936kvil";
+    version = "10.9.0";
+    sha256 = "00hdachbmcf9pyd1iksprsi5mddwp6z59mb3lr81z8ynfbmzhzni";
   }
