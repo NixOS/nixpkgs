158e3cb0da88e887cb9d980482867fbc3567fef1
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2018-08-22 13:04:04 +0200

    numactl: 2.0.11 -> 2.0.12

--- pkgs/os-specific/linux/numactl/default.nix
+++ pkgs/os-specific/linux/numactl/default.nix
@@ -2,23 +2,18 @@
 
 stdenv.mkDerivation rec {
   name = "numactl-${version}";
-  version = "2.0.11";
+  version = "2.0.12";
 
   src = fetchFromGitHub {
     owner = "numactl";
     repo = "numactl";
     rev = "v${version}";
-    sha256 = "0bcffqawwbyrnza8np0whii25mfd0dria35zal9v3l55xcrya3j9";
+    sha256 = "0crhpxwakp0gvd7wwpbkfd3brnrdf89lkbf03axnbrs0b6kaygg2";
   };
 
   nativeBuildInputs = [ autoreconfHook ];
 
-  patches = [
-    (fetchpatch {
-      url = https://raw.githubusercontent.com/gentoo/gentoo/b64d15e731e3d6a7671f0ec6c34a20203cf2609d/sys-process/numactl/files/numactl-2.0.11-sysmacros.patch;
-      sha256 = "05277kv3x12n2xlh3fgnmxclxfc384mkwb0v9pd91046khj6h843";
-    })
-  ] ++ stdenv.lib.optional stdenv.hostPlatform.isMusl (fetchpatch {
+  patches = stdenv.lib.optional stdenv.hostPlatform.isMusl (fetchpatch {
       url = https://git.alpinelinux.org/cgit/aports/plain/testing/numactl/musl.patch?id=0592b128c71c3e70d493bc7a13caed0d7fae91dd;
       sha256 = "080b0sygmg7104qbbh1amh3b322yyiajwi2d3d0vayffgva0720v";
     });
