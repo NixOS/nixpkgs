d895f47d689f66b2467e5144d3fb1a2294a18f16
Giulio Eulisse <giulio.eulisse@cern.ch>
2018-08-22 17:06:29 +0200

    alibuild: init at 1.5.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22247,4 +22247,8 @@ with pkgs;
   doing = callPackage ../applications/misc/doing  { };
 
   undervolt = callPackage ../os-specific/linux/undervolt { };
+
+  alibuild = callPackage ../development/tools/build-managers/alibuild {
+    python = python27;
+  };
 }
