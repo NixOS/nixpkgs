343c009f3e873dbe13a628c267313d5490f4be06
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-08-22 17:41:08 +0200

    nixos/firejail: add wrappedPackages option
    Using this, not only the wrapped binaries but also all other
    directories from the derivations are put into /run/current-system/sw.
    This is useful if you need desktop entries, icons, etc.

--- nixos/modules/programs/firejail.nix
+++ nixos/modules/programs/firejail.nix
@@ -38,6 +38,25 @@ let
       '') cfg.wrappedBinaries)}
     '';
 
+  wrappedPkgs = map (pkg:
+    pkgs.symlinkJoin {
+      name = "firejail-" + pkg.name;
+      paths = [ pkg ];
+      buildInputs = with pkgs; [ tree ];
+      postBuild = ''
+        for bin in $(find "$out/bin" -type l); do
+        oldbin="$(readlink "$bin")" 
+        rm "$bin"
+        cat <<_EOF >"$bin"
+        #!${pkgs.stdenv.shell} -e
+        /run/wrappers/bin/firejail "$oldbin" "\$@"
+        _EOF
+        chmod 0755 "$bin"
+        done
+      '';
+    }
+  ) cfg.wrappedPackages;
+
 in {
   options.programs.firejail = {
     enable = mkEnableOption (lib.mdDoc "firejail");
@@ -87,6 +106,22 @@ in {
         Wrap the binaries in firejail and place them in the global path.
       '';
     };
+
+    wrappedPackages = mkOption {
+      type = with types; listOf package;
+      default = [ ];
+      example = literalExample ''
+        [ pkgs.mpv ]
+      '';
+      description = ''
+        Put a package into <option>systemPackages</option>,
+        but wrap its binaries with firejail.
+        Compared to <option>wrappedBinaries</option>,
+        this e.g. has the advantage of providing desktop entries and icons.
+        However, you should be careful about using these packages'
+        libraries as they will not be wrapped.
+      '';
+    };
   };
 
   config = mkIf cfg.enable {
@@ -97,7 +132,7 @@ in {
         source = "${lib.getBin pkgs.firejail}/bin/firejail";
       };
 
-    environment.systemPackages = [ pkgs.firejail ] ++ [ wrappedBins ];
+    environment.systemPackages = [ pkgs.firejail ] ++ [ wrappedBins ] ++ wrappedPkgs;
   };
 
   meta.maintainers = with maintainers; [ peterhoeg ];
