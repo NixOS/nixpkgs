c40e9f94556e37d50f702437e369f27ca24c63b9
WilliButz <wbutz@cyberfnord.de>
2018-08-23 00:23:36 +0200

    wt3: 3.3.10 -> 3.3.11

--- pkgs/development/libraries/wt/default.nix
+++ pkgs/development/libraries/wt/default.nix
@@ -43,8 +43,8 @@ let
     };
 in {
   wt3 = generic {
-    version = "3.3.10";
-    sha256 = "1y25mhghgbgjgycpny0x4z95xn98q0wraab1c5gkwnay097bgwdy";
+    version = "3.3.11";
+    sha256 = "1s1bwg3s7brnspr9ya1vg5mr29dbvhf05s606fiv409b7ladqvxq";
   };
 
   wt4 = generic {
