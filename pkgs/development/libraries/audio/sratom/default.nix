510354437b443a0e6526fffc6ca542efda992e8d
Bart Brouns <bart@magnetophon.nl>
2018-08-23 23:25:30 +0200

    sratom: 0.6.0 -> 0.6.2

--- pkgs/development/libraries/audio/sratom/default.nix
+++ pkgs/development/libraries/audio/sratom/default.nix
@@ -1,16 +1,16 @@
-{ stdenv, fetchurl, lv2, pkgconfig, python, serd, sord }:
+{ stdenv, fetchurl, lv2, pkgconfig, python, serd, sord, libsndfile }:
 
 stdenv.mkDerivation rec {
   name = "sratom-${version}";
-  version = "0.6.0";
+  version = "0.6.2";
 
   src = fetchurl {
     url = "https://download.drobilla.net/${name}.tar.bz2";
-    sha256 = "0hrxd9i66s06bpn6i3s9ka95134g3sm8yscmif7qgdzhyjqw42j4";
+    sha256 = "0lz883ravxjf7r9wwbx2gx9m8vhyiavxrl9jdxfppjxnsralll8a";
   };
 
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ lv2 python serd sord ];
+  buildInputs = [ lv2 python serd sord libsndfile ];
 
   configurePhase = "${python.interpreter} waf configure --prefix=$out";
 
