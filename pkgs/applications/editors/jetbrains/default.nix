e1147d90a768f930d4ae40e1ace89b0f35e371e2
Herman Fries <h.fries@nyris.io>
2018-08-23 18:04:26 +0200

    rider: 2018.1.3 -> 2018.2

--- pkgs/applications/editors/jetbrains/default.nix
+++ pkgs/applications/editors/jetbrains/default.nix
@@ -353,15 +353,15 @@ in
 
   rider = buildRider rec {
     name = "rider-${version}";
-    version = "2018.1.3"; /* updated by script */
+    version = "2018.2"; /* updated by script */
     description = "A cross-platform .NET IDE based on the IntelliJ platform and ReSharper";
     license = stdenv.lib.licenses.unfree;
     src = fetchurl {
       url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-      sha256 = "00wskr60mjipyp44wrpc3pn6awc92djn7rwhc08nrar3zv0j4mgg"; /* updated by script */
+      sha256 = "0yigw9g53i6xamwva8vcd38f0aysxqkf77avms4l7l63dkap99nq"; /* updated by script */
     };
     wmClass = "jetbrains-rider";
-    update-channel = "Rider 2018.1.3";
+    update-channel = "Rider 2018.2";
   };
 
   ruby-mine = buildRubyMine rec {
