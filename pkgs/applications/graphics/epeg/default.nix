2025bd892e13107669e5bb90298501155de2f302
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2018-08-23 11:57:07 +0200

    epeg: add license

--- pkgs/applications/graphics/epeg/default.nix
+++ pkgs/applications/graphics/epeg/default.nix
@@ -26,6 +26,9 @@ stdenv.mkDerivation rec {
     homepage = https://github.com/mattes/epeg;
     description = "Insanely fast JPEG/ JPG thumbnail scaling";
     platforms = platforms.linux ++ platforms.darwin;
+    license = {
+      url = "https://github.com/mattes/epeg#license";
+    };
     maintainers = with maintainers; [ nh2 ];
   };
 }
