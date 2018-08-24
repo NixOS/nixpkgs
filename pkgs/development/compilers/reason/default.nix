a1581034bce25f9cb23d88692cb7b79747f7bac7
Jörg Thalheim <joerg@thalheim.io>
2018-08-24 12:24:42 +0100

    reason: fix compilation with newer menhir

--- pkgs/development/compilers/reason/default.nix
+++ pkgs/development/compilers/reason/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, makeWrapper, buildOcaml, fetchFromGitHub,
+{ stdenv, makeWrapper, buildOcaml, fetchFromGitHub, fetchpatch,
   ocaml, opam, jbuilder, menhir, merlin_extend, ppx_tools_versioned, utop }:
 
 buildOcaml rec {
@@ -11,6 +11,13 @@ buildOcaml rec {
     rev = "fefe5e4db3a54a7946c2220ee037dd2f407011c9";
     sha256 = "1x0dbacgq9pa36zgzwrc0gm14wbb6v27y9bf7wcwk55a1ck0am18";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/facebook/reason/commit/e02f9bfb73f9cbbc00f0fa6215b107c6f70a22c2.patch";
+      sha256 = "18slimrsp79fgz2dbfx3sls70ybyj37b99fgykx3chc66i4yd53f";
+    })
+  ];
 
   propagatedBuildInputs = [ menhir merlin_extend ppx_tools_versioned ];
 
