a66cd66f796dfa638b0f22e02cf17092d06df10a
R. RyanTM <ryantm+bot@ryantm.com>
2018-08-24 11:50:10 -0700

    clipgrab: 3.6.8 -> 3.6.9
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/clipgrab/versions.

--- pkgs/applications/video/clipgrab/default.nix
+++ pkgs/applications/video/clipgrab/default.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   name = "clipgrab-${version}";
-  version = "3.6.8";
+  version = "3.6.9";
 
   src = fetchurl {
-    sha256 = "0agp97g79mlqcwfz2xk5rdxw4kx0hm92xikdspbpxlfji1mkh10p";
+    sha256 = "16r0h286vqw1bns29sx5x2919pj3y8gxf1k7dpf9xrz0vm2zrc3v";
     # The .tar.bz2 "Download" link is a binary blob, the source is the .tar.gz!
     url = "https://download.clipgrab.org/${name}.tar.gz";
   };
