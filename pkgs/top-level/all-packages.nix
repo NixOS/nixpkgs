bfb14fbe72037a5abf947649fd6a53ffdeb39e54
Michiel Leenaars <ml.software@leenaa.rs>
2018-08-25 19:40:26 +0200

    thonny: init at 3.0.0b3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18736,6 +18736,8 @@ with pkgs;
 
   nylas-mail-bin = callPackage ../applications/networking/mailreaders/nylas-mail-bin { };
 
+  thonny = callPackage ../applications/editors/thonny { };
+
   thunderbird = callPackage ../applications/networking/mailreaders/thunderbird {
     inherit (gnome2) libIDL;
     libpng = libpng_apng;
