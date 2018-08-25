954eb34a5363877e5efed27ddc2bc748f6265019
Bas van Dijk <v.dijk.bas@gmail.com>
2018-08-26 01:20:15 +0200

    elasticsearch-curator: explain why we disable the test-suite

--- pkgs/development/python-modules/elasticsearch-curator/default.nix
+++ pkgs/development/python-modules/elasticsearch-curator/default.nix
@@ -24,6 +24,7 @@ buildPythonPackage rec {
     sha256 = "e75abeb7f7be939b1c64c071898760dc10ab5f08307c253fc074abf8a41a76f0";
   };
 
+  # The test hangs so we disable it.
   doCheck = false;
 
   propagatedBuildInputs = [
