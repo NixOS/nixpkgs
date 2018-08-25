aedf32c80957655096e0e691f27c441b3e236c9a
Frederik Rietdijk <fridh@fridh.nl>
2018-08-25 07:42:44 +0200

    python: autobahn: 18.7.1 -> 18.8.1

--- pkgs/development/python-modules/autobahn/default.nix
+++ pkgs/development/python-modules/autobahn/default.nix
@@ -1,22 +1,22 @@
 { stdenv, buildPythonPackage, fetchPypi, isPy3k, isPy33,
   unittest2, mock, pytest, trollius, asyncio,
-  pytest-asyncio, futures,
+  pytest-asyncio, futures, cffi,
   six, twisted, txaio, zope_interface
 }:
 buildPythonPackage rec {
   pname = "autobahn";
-  version = "18.7.1";
+  version = "18.8.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "163dd329c2a29f4ef82f7fe85a6f6e654a2aa6837bd83834cf537e4efa9204be";
+    sha256 = "b69858e0be4bff8437b0bd82a0db1cbef7405e16bd9354ba587c043d6d5e1ad9";
   };
 
   # Upstream claim python2 support, but tests require pytest-asyncio which
   # is pythn3 only. Therefore, tests are skipped for python2.
   doCheck = isPy3k;
-  buildInputs = stdenv.lib.optionals isPy3k [ unittest2 mock pytest pytest-asyncio ];
-  propagatedBuildInputs = [ six twisted zope_interface txaio ] ++
+  checkInputs = stdenv.lib.optionals isPy3k [ unittest2 mock pytest pytest-asyncio ];
+  propagatedBuildInputs = [ cffi six twisted zope_interface txaio ] ++
     (stdenv.lib.optional isPy33 asyncio) ++
     (stdenv.lib.optionals (!isPy3k) [ trollius futures ]);
 
@@ -29,6 +29,5 @@ buildPythonPackage rec {
     homepage    = "https://crossbar.io/autobahn";
     license     = licenses.mit;
     maintainers = with maintainers; [ nand0p ];
-    platforms   = platforms.all;
   };
 }
