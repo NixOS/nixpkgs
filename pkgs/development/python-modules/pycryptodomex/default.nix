5544e3bfc12726af2624a44c95317a44ac24c365
Frederik Rietdijk <fridh@fridh.nl>
2018-08-25 07:48:34 +0200

    python: pycryptodomex: 3.6.5 -> 3.6.6

--- pkgs/development/python-modules/pycryptodomex/default.nix
+++ pkgs/development/python-modules/pycryptodomex/default.nix
@@ -2,7 +2,7 @@
 
 buildPythonPackage rec {
   pname = "pycryptodomex";
-  version = "3.6.5";
+  version = "3.6.6";
 
   meta = {
     description = "A self-contained cryptographic library for Python";
@@ -12,6 +12,6 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ff7d28474ef2134fe3027b53fa3cf13ed5f776ce15d6209b0e48fbc7592f9396";
+    sha256 = "d0d448484e161786922b41e112b5d7cf76ef5f0c725ea5107ef866bb14a38b12";
   };
 }
