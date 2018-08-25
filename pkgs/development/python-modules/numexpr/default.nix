99696fb9a0b497506cfbabec0049988ad12216f4
Frederik Rietdijk <fridh@fridh.nl>
2018-08-25 18:41:12 +0200

    python: numexpr: 2.6.7 -> 2.6.8

--- pkgs/development/python-modules/numexpr/default.nix
+++ pkgs/development/python-modules/numexpr/default.nix
@@ -7,11 +7,11 @@
 
 buildPythonPackage rec {
   pname = "numexpr";
-  version = "2.6.7";
+  version = "2.6.8";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "3d007946b6c042fc9feb1db52dd0847d2f9b7f3175ae0591476ea80dcd664771";
+    sha256 = "ee8bc7201aa2f1962c67d27c326a11eef9df887d7b87b1278a1d4e722bf44375";
   };
 
   propagatedBuildInputs = [ numpy ];
@@ -19,20 +19,15 @@ buildPythonPackage rec {
   # Run the test suite.
   # It requires the build path to be in the python search path.
   checkPhase = ''
+    pushd $out
     ${python}/bin/${python.executable} <<EOF
-    import sysconfig
     import sys
-    import os
-    f = "lib.{platform}-{version[0]}.{version[1]}"
-    lib = f.format(platform=sysconfig.get_platform(),
-                   version=sys.version_info)
-    build = os.path.join(os.getcwd(), 'build', lib)
-    sys.path.insert(0, build)
     import numexpr
     r = numexpr.test()
     if not r.wasSuccessful():
         sys.exit(1)
     EOF
+    popd
   '';
 
   meta = {
