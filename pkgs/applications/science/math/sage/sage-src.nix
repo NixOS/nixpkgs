0e4160a3a11d67a66ba9bc556a6d6d4ee3af9c0e
Timo Kaufmann <timokau@zoho.com>
2018-08-26 19:56:08 +0200

    pythonPackages.sympy: 1.1.1 -> 1.2

--- pkgs/applications/science/math/sage/sage-src.nix
+++ pkgs/applications/science/math/sage/sage-src.nix
@@ -136,6 +136,13 @@ stdenv.mkDerivation rec {
       url = "https://git.sagemath.org/sage.git/patch?id2=8.4.beta0&id=8bef4fd2876a61969b516fe4eb3b8ad7cc076c5e";
       sha256 = "00p3hfsfn3w2vxgd9fjd23mz7xfxjfravf8ysjxkyd657jbkpjmk";
     })
+
+    # https://trac.sagemath.org/ticket/26117
+    (fetchpatch {
+      name = "sympy-1.2.patch";
+      url = "https://git.sagemath.org/sage.git/patch?id2=8.4.beta2&id=d94a0a3a3fb4aec05a6f4d95166d90c284f05c36";
+      sha256 = "0an2xl1pp3jg36kgg2m1vb7sns7rprk1h3d0qy1gxwdab6i7qnvi";
+    })
   ];
 
   patches = nixPatches ++ packageUpgradePatches ++ [
