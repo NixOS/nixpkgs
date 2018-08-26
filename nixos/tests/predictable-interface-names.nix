54e95b2574e88c8b4e5a92f019c4db88de5e3b45
Uli Baum <xeji@cat3.de>
2018-08-27 01:33:38 +0200

    nixos/tests/predictableInterfaceNames: test stateVersion 18.03 and 18.09

--- nixos/tests/predictable-interface-names.nix
+++ nixos/tests/predictable-interface-names.nix
@@ -2,11 +2,12 @@
 
 let
   inherit (import ../lib/testing.nix { inherit system; }) makeTest pkgs;
-in pkgs.lib.listToAttrs (pkgs.lib.crossLists (predictable: withNetworkd: {
+in pkgs.lib.listToAttrs (pkgs.lib.crossLists (predictable: withNetworkd: stateV: {
   name = pkgs.lib.optionalString (!predictable) "un" + "predictable"
-       + pkgs.lib.optionalString withNetworkd "Networkd";
+       + pkgs.lib.optionalString withNetworkd "Networkd"
+       + builtins.replaceStrings ["."] [""] stateV;
   value = makeTest {
-    name = "${if predictable then "" else "un"}predictableInterfaceNames${if withNetworkd then "-with-networkd" else ""}";
+    name = "${if predictable then "" else "un"}predictableInterfaceNames${if withNetworkd then "-with-networkd" else ""}-${stateV}";
     meta = {};
 
     machine = { lib, ... }: {
@@ -15,9 +16,8 @@ in pkgs.lib.listToAttrs (pkgs.lib.crossLists (predictable: withNetworkd: {
       networking.dhcpcd.enable = !withNetworkd;
       # So we can see the initrd device names in the log
       # although we don't add an automated test here
-      boot.initrd.network.enable = true;
-      # test new behavior for 18.09
-      system.stateVersion = "18.09";
+      boot.initrd.network.enable = (stateV == "18.09");
+      system.stateVersion = stateV;
     };
 
     testScript = ''
@@ -26,4 +26,4 @@ in pkgs.lib.listToAttrs (pkgs.lib.crossLists (predictable: withNetworkd: {
       $machine->fail("ip link show ${if predictable then "eth0" else "ens3"}");
     '';
   };
-}) [[true false] [true false]])
+}) [[true false] [true false] ["18.03" "18.09"]])
