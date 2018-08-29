256e347dfe2e8ab0070d85320fc1cc7da163eb92
Michael Weiss <dev.primeos@gmail.com>
2018-08-29 17:13:35 +0200

    signal-desktop-beta: remove
    This package was marked as broken since
    9cb0b496737341faf19ea62d81583ece52617a69.
    Reason: The package is outdated and keeping up with the beta releases
    isn't really worth it (there are regular stable releases).

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5167,8 +5167,6 @@ with pkgs;
 
   signal-desktop = callPackage ../applications/networking/instant-messengers/signal-desktop { };
 
-  signal-desktop-beta = callPackage ../applications/networking/instant-messengers/signal-desktop/beta.nix { };
-
   # aka., pgp-tools
   signing-party = callPackage ../tools/security/signing-party { };
 
