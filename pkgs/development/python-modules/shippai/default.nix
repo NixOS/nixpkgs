b07a57bd01cb6be2bf3749a02fecb5411ac0925d
Frederik Rietdijk <fridh@fridh.nl>
2018-08-30 18:33:42 +0200

    Revert "python: shippai: 0.2.3 -> 0.3.1"
    This broke vdirsyncer.
    
    This reverts commit 0bca28c89078ddc25aca0c1f8c50d3bafdd36412.

--- pkgs/development/python-modules/shippai/default.nix
+++ pkgs/development/python-modules/shippai/default.nix
@@ -3,11 +3,11 @@
 buildPythonPackage rec {
   pname = "shippai";
   # Please make sure that vdirsyncer still builds if you update this package.
-  version = "0.3.1";
+  version = "0.2.3";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "9e8d1ca5a742a7ea175cebda3090497d42348517e8d2f05f9854d0f30f1a48ad";
+    sha256 = "1ppwywzg4d12h658682ssmingm6ls6a96p4ak26i2w9d4lf8pfsc";
   };
 
   meta = with stdenv.lib; {
