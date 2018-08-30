efb16b582944c426353e407861d7172f24d965dd
Jan Tojnar <jtojnar@gmail.com>
2018-08-30 20:20:32 +0200

    deadbeefPlugins.infobar: init at 1.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15811,6 +15811,7 @@ with pkgs;
 
   deadbeefPlugins = {
     headerbar-gtk3 = callPackage ../applications/audio/deadbeef/plugins/headerbar-gtk3.nix { };
+    infobar = callPackage ../applications/audio/deadbeef/plugins/infobar.nix { };
     mpris2 = callPackage ../applications/audio/deadbeef/plugins/mpris2.nix { };
     opus = callPackage ../applications/audio/deadbeef/plugins/opus.nix { };
   };
