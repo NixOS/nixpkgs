dce0310237122b71ee79c709c70482a9c98d3d50
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-08-30 18:33:11 +0200

    python.pkgs.sarge: disable tests

--- pkgs/development/python-modules/sarge/default.nix
+++ pkgs/development/python-modules/sarge/default.nix
@@ -9,6 +9,9 @@ buildPythonPackage rec {
     sha256 = "da8cc90883f8e5ab4af0d746438f608662f5f2a35da2e858517927edefa134b0";
   };
 
+  # No tests in PyPI tarball
+  doCheck = false;
+
   meta = with lib; {
     homepage = http://sarge.readthedocs.org/;
     description = "A wrapper for subprocess which provides command pipeline functionality";
