bcccd134716f1abc5457870a866dcf2b1e501ef5
Matthew Pickering <matthewtpickering@gmail.com>
2018-08-30 18:24:31 +0100

    kythe: init at 0.0.28 (#45778)
    Progress towards #27590

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8399,6 +8399,8 @@ with pkgs;
 
   kustomize = callPackage ../development/tools/kustomize { };
 
+  kythe = callPackage ../development/tools/kythe { };
+
   Literate = callPackage ../development/tools/literate-programming/Literate {};
 
   lcov = callPackage ../development/tools/analysis/lcov { };
