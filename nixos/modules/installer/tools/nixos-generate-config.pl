37a45e5a9712727bc8de1b7382d920422306774d
Vladimír Čunát <vcunat@gmail.com>
2018-09-01 20:58:53 +0200

    nixos-generate-config: fix perl quoting after cb1ca42

--- nixos/modules/installer/tools/nixos-generate-config.pl
+++ nixos/modules/installer/tools/nixos-generate-config.pl
@@ -575,7 +575,7 @@ $bootLoaderConfig
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.default = "http://user:password\@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
