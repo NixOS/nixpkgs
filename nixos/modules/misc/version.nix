17827afa4642258904cb6a3cda1d1b750d64c6ca
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-09-02 16:59:10 -0400

    nixos: Sets channel and version for 18.09

--- nixos/modules/misc/version.nix
+++ nixos/modules/misc/version.nix
@@ -68,7 +68,7 @@ in
     defaultChannel = mkOption {
       internal = true;
       type = types.str;
-      default = https://nixos.org/channels/nixos-unstable;
+      default = https://nixos.org/channels/nixos-18.09;
       description = "Default NixOS channel to which the root user is subscribed.";
     };
 
