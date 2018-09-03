e185954cf986b37f4c251f0eef5fb87d65441b43
Vladimír Čunát <vcunat@gmail.com>
2018-09-03 16:53:02 +0200

    treewide: fix various links (homepage, src)
    (cherry picked from commit 9b48a677316ec1e34821e19bf8af68ef0dda6fa3)

--- pkgs/development/libraries/jbig2dec/default.nix
+++ pkgs/development/libraries/jbig2dec/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "jbig2dec-0.14";
 
   src = fetchurl {
-    url = "http://downloads.ghostscript.com/public/jbig2dec/${name}.tar.gz";
+    url = "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs922/${name}.tar.gz";
     sha256 = "0k01hp0q4275fj4rbr1gy64svfraw5w7wvwl08yjhvsnpb1rid11";
   };
 
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
   doCheck = false; # fails 1 of 4 tests
 
   meta = {
-    homepage = https://www.ghostscript.com/jbig2dec.html;
+    homepage = https://www.jbig2dec.com/;
     description = "Decoder implementation of the JBIG2 image compression format";
     license = stdenv.lib.licenses.gpl2Plus;
     platforms = stdenv.lib.platforms.unix;
