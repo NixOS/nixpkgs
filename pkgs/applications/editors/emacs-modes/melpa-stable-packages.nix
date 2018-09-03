9d6ff38f706c29cd376a52323c8f8018ac93aca6
Daniël de Kok <me@github.danieldk.eu>
2018-09-03 23:42:57 +0200

    magit-todos: fix build by providing git as input. (#45999)
    magit-todos requires git during builds. Fixes #45868.

--- pkgs/applications/editors/emacs-modes/melpa-stable-packages.nix
+++ pkgs/applications/editors/emacs-modes/melpa-stable-packages.nix
@@ -151,6 +151,12 @@ self:
             (attrs.nativeBuildInputs or []) ++ [ external.git ];
         });
 
+      magit-todos = super.magit-todos.overrideAttrs (attrs: {
+        # searches for Git at build time
+        nativeBuildInputs =
+          (attrs.nativeBuildInputs or []) ++ [ external.git ];
+      });
+
       # missing OCaml
       merlin = markBroken super.merlin;
 
