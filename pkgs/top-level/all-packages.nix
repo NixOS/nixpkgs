3549879b459cbfbc7f8550e5c712b66ca9e2591d
symphorien <symphorien@users.noreply.github.com>
2018-09-03 21:24:38 +0000

    gede: 2.6.1 -> 2.10.9 (#45995)
    reverting to qt 5.9 fixed the build.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8270,7 +8270,7 @@ with pkgs;
 
   funnelweb = callPackage ../development/tools/literate-programming/funnelweb { };
 
-  gede = libsForQt5.callPackage ../development/tools/misc/gede { };
+  gede = libsForQt59.callPackage ../development/tools/misc/gede { };
 
   gdbgui = callPackage ../development/tools/misc/gdbgui { };
 
