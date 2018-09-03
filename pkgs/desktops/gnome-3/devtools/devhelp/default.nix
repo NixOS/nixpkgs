5043df2499e99d2aa79fb7337b4b7bedbcf34547
Jan Tojnar <jtojnar@gmail.com>
2018-09-03 21:28:16 +0200

    gnome3.devhelp: add python3 for install script
    Meson no longer propagates it so we need to re-add it.

--- pkgs/desktops/gnome-3/devtools/devhelp/default.nix
+++ pkgs/desktops/gnome-3/devtools/devhelp/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, meson, ninja, pkgconfig, gnome3, gtk3, wrapGAppsHook
-, glib, amtk, appstream-glib, gobjectIntrospection
+, glib, amtk, appstream-glib, gobjectIntrospection, python3
 , webkitgtk, gettext, itstool, gsettings-desktop-schemas }:
 
 stdenv.mkDerivation rec {
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
     sha256 = "1rzilsn0v8dj86djankllc5f10d58f6rwg4w1fffh5zly10nlli5";
   };
 
-  nativeBuildInputs = [ meson ninja pkgconfig gettext itstool wrapGAppsHook appstream-glib gobjectIntrospection ];
+  nativeBuildInputs = [ meson ninja pkgconfig gettext itstool wrapGAppsHook appstream-glib gobjectIntrospection python3 ];
   buildInputs = [
     glib gtk3 webkitgtk amtk
     gnome3.defaultIconTheme gsettings-desktop-schemas
