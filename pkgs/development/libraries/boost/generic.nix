NixOS/nixpkgs

commit c70ff28968a5a21013e6c0ddb350a6064f259935

boost: disable python explicitly in b2Args (#45679)
fixes the build of boost_162

(cherry picked from commit 775d1ecf578bed07b22f5780feae60f6272b4f1e)

pkgs/development/libraries/boost/generic.nix
@@ -76,6 +76,7 @@ let
     ++ optional (toolset != null) "toolset=${toolset}"
     ++ optional (mpi != null || stdenv.hostPlatform != stdenv.buildPlatform) "--user-config=user-config.jam"
     ++ optionals (stdenv.hostPlatform.libc == "msvcrt") [
+    ++ optional (!enablePython) "--without-python"
     "threadapi=win32"
   ]);
 

commit 11c2595e40a1d8e8223d3a48acc067d9f48dac45

boost: fix faulty cherry-pick in c70ff28
the change in c70ff28 accidentally ended up in the wrong line

pkgs/development/libraries/boost/generic.nix
@@ -74,9 +74,9 @@ let
   ] ++ optional (link != "static") "runtime-link=${runtime-link}"
     ++ optional (variant == "release") "debug-symbols=off"
     ++ optional (toolset != null) "toolset=${toolset}"
+    ++ optional (!enablePython) "--without-python"
     ++ optional (mpi != null || stdenv.hostPlatform != stdenv.buildPlatform) "--user-config=user-config.jam"
     ++ optionals (stdenv.hostPlatform.libc == "msvcrt") [
-    ++ optional (!enablePython) "--without-python"
     "threadapi=win32"
   ]);
 