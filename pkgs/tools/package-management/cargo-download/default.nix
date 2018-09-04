9f3707d83aad3726e451ee11a18b7af52d3aeceb
Alyssa Ross <hi@alyssa.is>
2018-09-04 10:32:05 +0100

    cargo-download: add Security build input on darwin

--- pkgs/tools/package-management/cargo-download/default.nix
+++ pkgs/tools/package-management/cargo-download/default.nix
@@ -1,5 +1,11 @@
-{ lib, buildPlatform, buildRustCrate, fetchgit }:
+{ stdenv, lib, fetchgit, darwin, buildPlatform
+, buildRustCrate, defaultCrateOverrides }:
 
 ((import ./Cargo.nix { inherit lib buildPlatform buildRustCrate fetchgit; }).cargo_download {}).override {
-
+  crateOverrides = defaultCrateOverrides // {
+    cargo-download = attrs: {
+      buildInputs = lib.optional stdenv.isDarwin
+        darwin.apple_sdk.frameworks.Security;
+    };
+  };
 }
