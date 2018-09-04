dd6d9aecca75d3da81e53657b652f79385965cc0
Daniël de Kok <me@danieldk.eu>
2018-09-04 20:35:05 +0200

    python[23]Packages.mxnet: fix build
    Relax graphviz, numpy, requests dependency version bounds.
    
    (cherry picked from commit e57e92bfa2515b1b7ff5ad2db2e7d503e7ae24fe)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1788,6 +1788,13 @@ in {
 
     doCheck = !isPy3k;
 
+    postPatch = ''
+      substituteInPlace python/setup.py \
+      --replace "graphviz<0.9.0" "graphviz<0.10.0" \
+      --replace "numpy<=1.15.0" "numpy<1.16.0" \
+      --replace "requests<2.19.0" "requests<2.20.0"
+    '';
+
     preConfigure = ''
       cd python
     '';
