f60fea5742e3e379f5060991daae94dafa67ea1f
Markus Kowalewski <markus.kowalewski@gmail.com>
2018-09-04 22:28:32 +0200

    netselect: update homepage + license

--- pkgs/tools/networking/netselect/default.nix
+++ pkgs/tools/networking/netselect/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation {
   name = "netselect-0.3";
-  
+
   src = fetchurl {
     url = http://alumnit.ca/~apenwarr/netselect/netselect-0.3.tar.gz;
     sha256 = "0y69z59vylj9x9nk5jqn6ihx7dkzg09gpv2w1q1rs8fmi4jr90gy";
@@ -15,11 +15,11 @@ stdenv.mkDerivation {
       --replace "-g root" "" \
       --replace "4755"    "0755"
   '';
-  
-  meta = {
-    homepage = http://alumnit.ca/~apenwarr/netselect/;
+
+  meta = with stdenv.lib; {
+    homepage = https://github.com/apenwarr/netselect;
     description = "An ultrafast intelligent parallelizing binary-search implementation of \"ping\"";
-    license = "BSD";
-    platforms = stdenv.lib.platforms.linux;
+    license = licenses.bsd3;
+    platforms = platforms.linux;
   };
 }
