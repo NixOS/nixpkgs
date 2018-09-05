18d419141df4958e8438b7883d6c061609f8170b
Maximilian Bosch <maximilian@mbosch.me>
2018-09-05 17:01:57 +0200

    nixos/weechat: cleanup module, add module documentation
    This adds several improvements the previously introduced
    `services.weechat` module:
    
    * Dropped `services.weechat.init` as the initialization script can now
      be done on package-level since 2af41719bc using the `configure`
      function.
    
    * Added `sessionName` option to explicitly configure a name for the
      `screen` session (by default: weechat-screen).
    
    * Added `binary` option to configure the binary name (e.g.
      `weechat-headless`).
    
    * Added docs regarding `screen` session and `weechat.service`.

--- nixos/modules/services/misc/weechat.nix
+++ nixos/modules/services/misc/weechat.nix
@@ -9,28 +9,33 @@ in
 {
   options.services.weechat = {
     enable = mkEnableOption "weechat";
-    init = mkOption {
-      description = "Weechat commands applied at start, one command per line.";
-      example = ''
-        /set relay.network.password correct-horse-battery-staple
-        /relay add weechat 9001
-      '';
-      type = types.str;
-      default = "";
-    };
     root = mkOption {
       description = "Weechat state directory.";
       type = types.str;
       default = "/var/lib/weechat";
     };
+    sessionName = mkOption {
+      description = "Name of the `screen' session for weechat.";
+      default = "weechat-screen";
+      type = types.str;
+    };
+    binary = mkOption {
+      description = "Binary to execute (by default \${weechat}/bin/weechat).";
+      example = literalExample ''
+        ''${pkgs.weechat}/bin/weechat-headless
+      '';
+      default = "${pkgs.weechat}/bin/weechat";
+    };
   };
 
   config = mkIf cfg.enable {
     users = {
+      groups.weechat = {};
       users.weechat = {
         createHome = true;
         group = "weechat";
         home = cfg.root;
+        isSystemUser = true;
       };
     };
 
@@ -39,29 +44,13 @@ in
       serviceConfig = {
         User = "weechat";
         Group = "weechat";
+        RemainAfterExit = "yes";
       };
-      script = "exec ${pkgs.screen}/bin/screen -D -m ${pkgs.weechat}/bin/weechat";
+      script = "exec ${pkgs.screen}/bin/screen -Dm -S ${cfg.sessionName} ${cfg.binary}";
       wantedBy = [ "multi-user.target" ];
       wants = [ "network.target" ];
     };
-
-    systemd.paths.weechat-fifo = {
-      pathConfig = {
-        PathExists = "${cfg.root}/weechat_fifo";
-        Unit = "weechat-apply-init.service";
-      };
-      wantedBy = [ "multi-user.target" ];
-    };
-
-    systemd.services.weechat-apply-init = let
-      initFile = pkgs.writeText "weechat-init" cfg.init;
-    in {
-      script = "sed 's/^/*/' ${initFile} > ${cfg.root}/weechat_fifo";
-      serviceConfig = {
-        Type = "oneshot";
-        User = "weechat";
-        Group = "weechat";
-      };
-    };
   };
+
+  meta.doc = ./weechat.xml;
 }
