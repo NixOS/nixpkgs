a547a7532d0544ecee356462b1f25515800a2650
Jan Tojnar <jtojnar@gmail.com>
2018-09-05 00:45:34 +0000

    gnome3.gnome-calendar: 3.28.2 → 3.30.0

--- pkgs/desktops/gnome-3/apps/gnome-calendar/default.nix
+++ pkgs/desktops/gnome-3/apps/gnome-calendar/default.nix
@@ -4,13 +4,13 @@
 
 let
   pname = "gnome-calendar";
-  version = "3.28.2";
+  version = "3.30.0";
 in stdenv.mkDerivation rec {
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "0x6wxngf8fkwgbl6x7rzp0srrb43rm55klpb2vfjk2hahpbjvxyw";
+    sha256 = "06kpp6s62sx4cq6w3gxzscibwr2aim7bmgqq0wqhxn9s9mv65c7r";
   };
 
   passthru = {
@@ -27,8 +27,8 @@ in stdenv.mkDerivation rec {
   ];
 
   postPatch = ''
-    chmod +x meson_post_install.py # patchShebangs requires executable file
-    patchShebangs meson_post_install.py
+    chmod +x build-aux/meson/meson_post_install.py # patchShebangs requires executable file
+    patchShebangs build-aux/meson/meson_post_install.py
   '';
 
   meta = with stdenv.lib; {
