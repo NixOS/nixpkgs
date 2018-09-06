a9563a51cac61716fa105f87ef6c22707559a680
Jon Magoon <js.magoon@gmail.com>
2018-09-06 10:25:18 -0600

    arrayfire: fix maintainer and url string

--- pkgs/development/libraries/arrayfire/default.nix
+++ pkgs/development/libraries/arrayfire/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   version = "3.6.1";
   name = "arrayfire-${version}";
   src = fetchurl {
-    url = "http://arrayfire.s3.amazonaws.com/3.6.1/ArrayFire-v3.6.1_Linux_x86_64.sh";
+    url = "http://arrayfire.s3.amazonaws.com/${version}/ArrayFire-v${version}_Linux_x86_64.sh";
     sha256 = "14qsdbq2yz5bmj5zg4y8sb9pybz2h41zsc3fqv9nhmfmllb004gc";
   };
 
@@ -19,7 +19,7 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     description = "Multiplatform library for parallel GPU computation";
     homepage = http://www.arrayfire.com/;
-    maintainers = with maintainers; [ "jmagoon" ];
+    maintainers = with maintainers; [ jmagoon ];
     license = licenses.cc-by-40;
     platforms = platforms.linux;
   };
