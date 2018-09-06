51c5de1b4863a0e338eb1fc4bc7ef3938680ee7f
Claas Augner <github@caugner.de>
2018-09-07 00:09:23 +0200

    nextcloud-client: 2.3.2 -> 2.5.0-rc1

--- pkgs/applications/networking/nextcloud-client/default.nix
+++ pkgs/applications/networking/nextcloud-client/default.nix
@@ -1,36 +1,35 @@
-{ stdenv, fetchgit, cmake, pkgconfig, qtbase, qtwebkit, qtkeychain, qttools, sqlite
+{ stdenv, fetchgit, cmake, libcloudproviders, libzip, openssl, pcre, pkgconfig
+, qtbase, qtdeclarative, qtkeychain, qtsvg, qttools, qtwebengine, sqlite
 , inotify-tools, withGnomeKeyring ? false, makeWrapper, libgnome-keyring }:
 
 stdenv.mkDerivation rec {
   name = "nextcloud-client-${version}";
-  version = "2.3.3";
+  version = "2.5.0-rc1";
 
   src = fetchgit {
-    url = "git://github.com/nextcloud/client_theming.git";
-    rev = "ab40efe1e1475efddd636c09251d8917627261da";
-    sha256 = "19a1kqydgx47sa1a917j46zlbc5g9nynsanasyad9c8sqi0qvyip";
+    url = "https://github.com/nextcloud/desktop.git";
+    rev = "58302e9fc0ed9da5eee5c00140069983edfcb957";
+    sha256 = "01w0yam9jqa0lyadinyxfcx50q1c94dk8a5fff52n0iy1l1px3vw";
     fetchSubmodules = true;
   };
 
-  patches = [ ./find-sql.patch ];
-  patchFlags = "-d client -p1";
-
   nativeBuildInputs = [ pkgconfig cmake ];
 
-  buildInputs = [ qtbase qtwebkit qtkeychain qttools sqlite ]
-    ++ stdenv.lib.optional stdenv.isLinux inotify-tools
-    ++ stdenv.lib.optional withGnomeKeyring makeWrapper;
+  buildInputs = [
+      libcloudproviders libzip makeWrapper openssl pcre qtbase qtdeclarative
+      qtkeychain qtsvg qttools qtwebengine sqlite
+    ]
+    ++ stdenv.lib.optional stdenv.isLinux inotify-tools;
 
   enableParallelBuilding = true;
 
   dontUseCmakeBuildDir = true;
 
-  cmakeDir = "client";
-
   cmakeFlags = [
     "-UCMAKE_INSTALL_LIBDIR"
     "-DCMAKE_BUILD_TYPE=Release"
-    "-DOEM_THEME_DIR=${src}/nextcloudtheme"
+    "-DNO_SHIBBOLETH=1"
+    "-DCMAKE_PREFIX_PATH=${cmake}"
   ] ++ stdenv.lib.optionals stdenv.isLinux [
     "-DINOTIFY_LIBRARY=${inotify-tools}/lib/libinotifytools.so"
     "-DINOTIFY_INCLUDE_DIR=${inotify-tools}/include"
@@ -39,13 +38,15 @@ stdenv.mkDerivation rec {
   postInstall = ''
     sed -i 's/\(Icon.*\)=nextcloud/\1=Nextcloud/g' \
       $out/share/applications/nextcloud.desktop
-  '' + stdenv.lib.optionalString (withGnomeKeyring) ''
+
     wrapProgram "$out/bin/nextcloud" \
-      --prefix LD_LIBRARY_PATH : ${stdenv.lib.makeLibraryPath [ libgnome-keyring ]}
+      ${stdenv.lib.optionalString withGnomeKeyring
+        "--prefix LD_LIBRARY_PATH : ${stdenv.lib.makeLibraryPath [ libgnome-keyring ]}"} \
+      --prefix QT_PLUGIN_PATH : "${qtbase}/${qtbase.qtPluginPrefix}"
   '';
 
   meta = with stdenv.lib; {
-    description = "Nextcloud themed desktop client";
+    description = "Desktop Syncing Client for Nextcloud";
     homepage = https://nextcloud.com;
     license = licenses.gpl2;
     maintainers = with maintainers; [ caugner ];
