fd05b19c0438f5884892cf10e05fd64534434202
Sebastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2018-09-06 12:57:14 +0200

    gildas: 20180801_a -> 20180801_b
      - Update gildas
      - Remove a patch that has been applied upstream

--- pkgs/applications/science/astronomy/gildas/default.nix
+++ pkgs/applications/science/astronomy/gildas/default.nix
@@ -7,8 +7,8 @@ let
 in
 
 stdenv.mkDerivation rec {
-  srcVersion = "aug18a";
-  version = "20180801_a";
+  srcVersion = "aug18b";
+  version = "20180801_b";
   name = "gildas-${version}";
 
   src = fetchurl {
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
     # source code of the previous release to a different directory
     urls = [ "http://www.iram.fr/~gildas/dist/gildas-src-${srcVersion}.tar.gz"
       "http://www.iram.fr/~gildas/dist/archive/gildas/gildas-src-${srcVersion}.tar.gz" ];
-    sha256 = "0mg3wijrj8x1p912vkgrhxbypjx7aj9b1492yxvq2y3fxban6bj1";
+    sha256 = "1468wzjf5p3b9m01jb38ds4nqiqs0j0lxlf6dx4f4zqnpai7jxsk";
   };
 
   enableParallelBuilding = true;
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ gtk2-x11 lesstif cfitsio python27Env ];
 
-  patches = [ ./wrapper.patch ./return-error-code.patch ./clang.patch ./aarch64.patch ./gag-font-bin-rule.patch ];
+  patches = [ ./wrapper.patch ./clang.patch ./aarch64.patch ./gag-font-bin-rule.patch ];
 
   NIX_CFLAGS_COMPILE = stdenv.lib.optionalString stdenv.cc.isClang "-Wno-unused-command-line-argument";
 
