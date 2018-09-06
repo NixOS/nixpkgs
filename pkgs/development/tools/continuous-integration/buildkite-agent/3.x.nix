917d35488e07332f631b96b10cd50e7889b7760d
Yorick van Pelt <yorick@yorickvanpelt.nl>
2018-09-06 14:16:32 +0200

    buildkite-agent3: 3.0.1 -> 3.4.0, fix shell path

--- pkgs/development/tools/continuous-integration/buildkite-agent/3.x.nix
+++ pkgs/development/tools/continuous-integration/buildkite-agent/3.x.nix
@@ -1,12 +1,15 @@
-{ callPackage, fetchFromGitHub, ... } @ args:
+{ bash, callPackage, fetchFromGitHub, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
   src = fetchFromGitHub {
     owner = "buildkite";
     repo = "agent";
     rev = "v${version}";
-    sha256 = "09smyrzp1xkczlmh8vqb7bmjb2b5d6yf9birjgaw36c6m44bpfvs";
+    sha256 = "0g7d8p8dh7a1ip8qxax12q25xrkig59lmq737zdlds2wxadpmk70";
   };
-  version = "3.0.1";
+  version = "3.4.0";
   hasBootstrapScript = false;
+  postPatch = ''
+    substituteInPlace bootstrap/shell/shell.go --replace /bin/bash ${bash}/bin/bash
+  '';
 })
