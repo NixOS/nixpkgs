ab193a69743b550f0cc667626664042d645c436e
Yuriy Taraday <yorik.sar@gmail.com>
2018-09-07 23:34:47 +0400

    chromium: 68.0.3440.75 -> 69.0.3497.81 (18.03 backport)
    Import latest gn from #46328, just from Chromium build, since it will
    trigger huge rebuild otherwise.

---
+++ pkgs/applications/networking/browsers/chromium/gn.nix
@@ -0,0 +1,49 @@
+{ stdenv, lib, fetchgit, fetchzip, fetchpatch, darwin
+, git, ninja, python }:
+
+stdenv.mkDerivation rec {
+  name = "gn-${version}";
+  version = "20180830";
+
+  src = fetchgit {
+    url = "https://gn.googlesource.com/gn";
+    rev = "106b823805adcc043b2bfe5bc21d58f160a28a7b";
+    leaveDotGit = true;  # gen.py uses "git describe" to generate last_commit_position.h
+    deepClone = true;
+    sha256 = "1276jwk3424i78zp21f6hp7idvg6r8qxp8sdfaw8ilkmp7ss19c8";
+  };
+
+  postPatch = ''
+    # FIXME Needed with old Apple SDKs
+    substituteInPlace base/mac/foundation_util.mm \
+      --replace "NSArray<NSString*>*" "NSArray*"
+  '';
+
+  nativeBuildInputs = [ ninja python git ];
+  buildInputs = lib.optionals stdenv.isDarwin (with darwin; with apple_sdk.frameworks; [
+    libobjc
+    cctools
+
+    # frameworks
+    ApplicationServices
+    Foundation
+    AppKit
+  ]);
+
+  buildPhase = ''
+    python build/gen.py --no-sysroot
+    ninja -j $NIX_BUILD_CORES -C out gn
+  '';
+
+  installPhase = ''
+    install -vD out/gn "$out/bin/gn"
+  '';
+
+  meta = with lib; {
+    description = "A meta-build system that generates NinjaBuild files";
+    homepage = https://gn.googlesource.com/gn;
+    license = licenses.bsd3;
+    platforms = platforms.unix;
+    maintainers = with maintainers; [ stesie matthewbauer ];
+  };
+}
