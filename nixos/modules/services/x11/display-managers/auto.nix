1c6f3cbf6ef1c875ce94bf1b18f1a9f5dd10db7a
Jan Malakhovski <oxij@oxij.org>
2018-09-08 21:13:29 +0000

    Revert "nixos/displayManagers/auto: allow root auto-login"
    This reverts commit 5f72169b03756c147fe9c961beeaa5199bc440f1.

--- nixos/modules/services/x11/display-managers/auto.nix
+++ nixos/modules/services/x11/display-managers/auto.nix
@@ -47,20 +47,6 @@ in
       defaultUser = cfg.user;
     };
 
-    # lightdm by default doesn't allow auto login for root, which is
-    # required by some nixos tests. Override it here.
-    security.pam.services.lightdm-autologin.text = lib.mkForce ''
-        auth     requisite pam_nologin.so
-        auth     required  pam_succeed_if.so quiet
-        auth     required  pam_permit.so
-
-        account  include   lightdm
-
-        password include   lightdm
-
-        session  include   lightdm
-    '';
-
   };
 
 }
