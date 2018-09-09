d4e89680d2400a28de8fee772070680458e9e1a9
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-09-09 20:39:15 +0200

    pythonPackages.flask_ldap_login: Fix build

--- pkgs/development/python-modules/flask-ldap-login/default.nix
+++ pkgs/development/python-modules/flask-ldap-login/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildPythonPackage, fetchPypi
+{ stdenv, buildPythonPackage, fetchPypi, fetchpatch
 , flask, flask_wtf, flask_testing, ldap
 , mock, nose }:
 
@@ -10,6 +10,14 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "085rik7q8xrp5g95346p6jcp9m2yr8kamwb2kbiw4q0b0fpnnlgq";
   };
+
+  patches = [
+    # Fix flask_wtf>=0.9.0 incompatibility. See https://github.com/ContinuumIO/flask-ldap-login/issues/41
+    (fetchpatch {
+      url = https://github.com/ContinuumIO/flask-ldap-login/commit/ed08c03c818dc63b97b01e2e7c56862eaa6daa43.patch;
+      sha256 = "19pkhbldk8jq6m10kdylvjf1c8m84fvvj04v5qda4cjyks15aq48";
+    })
+  ];
 
   checkInputs = [ nose mock flask_testing ];
   propagatedBuildInputs = [ flask flask_wtf ldap ];
