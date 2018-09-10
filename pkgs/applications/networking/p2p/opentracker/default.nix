1cf44e6a07b5ba66dcf015bb729d44b6418f0486
Jörg Thalheim <joerg@thalheim.io>
2018-09-10 22:41:23 +0100

    opentracker: use https for downloading

--- pkgs/applications/networking/p2p/opentracker/default.nix
+++ pkgs/applications/networking/p2p/opentracker/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "opentracker-2018-05-26";
 
   src = fetchgit {
-    url = "git://erdgeist.org/opentracker";
+    url = "https://erdgeist.org/gitweb/opentracker";
     rev = "6411f1567f64248b0d145493c2e61004d2822623";
     sha256 = "110nfb6n4clykwdzpk54iccsfjawq0krjfqhg114i1z0ri5dyl8j";
   };
@@ -12,9 +12,9 @@ stdenv.mkDerivation {
   buildInputs = [ libowfat zlib ];
 
   installPhase = ''
-    mkdir -p $out/bin $out/share/doc
-    cp opentracker $out/bin
-    cp opentracker.conf.sample $out/share/doc
+    runHook preInstall
+    install -D opentracker $out/bin/opentracker
+    install -D opentracker.conf.sample $out/share/doc/opentracker.conf.sample
     runHook postInstall
   '';
 
