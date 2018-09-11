5f59ab7d4e07acb33732e540eab6e5315cfce250
Mario Rodas <marsam@users.noreply.github.com>
2018-09-10 22:28:20 -0500

    nodejs-6_x: 6.14.3 -> 6.14.4
    (cherry picked from commit bfdfb73ff154967e80caa98e8717993b037383dd)

--- pkgs/development/web/nodejs/v6.nix
+++ pkgs/development/web/nodejs/v6.nix
@@ -5,7 +5,7 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "6.14.3";
-    sha256 = "1jbrfk875aimm65wni059rrydmhp4z0hrxskq3ci6jvykxr8gwg3";
+    version = "6.14.4";
+    sha256 = "03zc6jhid6jyi871zlcrkjqffmrpxh01z2xfsl3xp2vzg2czqjws";
     patches = lib.optionals stdenv.isDarwin [ ./no-xcode.patch ./no-xcodebuild.patch ];
   }
