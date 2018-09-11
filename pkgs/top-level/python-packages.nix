6764c48edee3454b65b5453071274bda55711f9a
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-09-11 10:34:28 -0400

    pythonPackages.enum34: refactor move to python-modules
    removed testing script becuase it is default behavior.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5017,27 +5017,7 @@ in {
 
   enum-compat = callPackage ../development/python-modules/enum-compat { };
 
-  enum34 = if pythonAtLeast "3.4" then null else buildPythonPackage rec {
-    pname = "enum34";
-    version = "1.1.6";
-    name = "${pname}-${version}";
-
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "8ad8c4783bf61ded74527bffb48ed9b54166685e4230386a9ed9b1279e2df5b1";
-    };
-
-    checkPhase = ''
-      ${python.interpreter} -m unittest discover
-    '';
-
-
-    meta = {
-      homepage = https://pypi.python.org/pypi/enum34;
-      description = "Python 3.4 Enum backported to 3.3, 3.2, 3.1, 2.7, 2.6, 2.5, and 2.4";
-      license = "BSD";
-    };
-  };
+  enum34 = callPackage ../development/python-modules/enum34 { };
 
   epc = buildPythonPackage rec {
     name = "epc-0.0.3";
