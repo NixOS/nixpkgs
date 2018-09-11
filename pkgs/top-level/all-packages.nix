2a686003bad9110382e50de2fb391cb53b997b7d
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-09-10 22:36:11 -0700

    bazel-watcher: init at 4d5928e

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7976,6 +7976,8 @@ with pkgs;
     buildBazelPackage = buildBazelPackage.override { enableNixHacks = false; };
   };
 
+  bazel-watcher = callPackage ../development/tools/bazel-watcher { };
+
   buildBazelPackage = callPackage ../build-support/build-bazel-package { };
 
   bear = callPackage ../development/tools/build-managers/bear { };
