09e3c0536e6280f45c7b2cffb63d2a020f9f17c3
Drakonis <khronosforce@gmail.com>
2018-09-11 11:22:04 -0300

    Update with PR changes

--- pkgs/applications/networking/sync/rclone/browser.nix
+++ pkgs/applications/networking/sync/rclone/browser.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub , rclone, qt5,cmake  }:
+{ stdenv, fetchFromGitHub, qt5,cmake  }:
 
 stdenv.mkDerivation rec {
   name = "rclone-browser-${version}";
@@ -10,11 +10,12 @@ stdenv.mkDerivation rec {
     rev = "${version}";
     sha256 = "1ldradd5c606mfkh46y4mhcvf9kbjhamw0gahksp9w43h5dh3ir7";
   };
+  
   buildInputs = [ qt5.qtbase cmake ];
   meta = with stdenv.lib; {
     description = "Graphical Frontend to Rclone written in Qt";
     homepage = https://github.com/mmozeiko/RcloneBrowser;
     license = licenses.unlicense;
-    platforms = platforms.all;
+    platforms = platforms.linux;
   };
 }
