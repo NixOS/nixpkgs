9901b28c756df657229b31e65de35a36afa360eb
Tor Hedin Brønner <torhedinbronner@gmail.com>
2018-09-12 07:49:25 +0000

    gnome3.gnome-applets: 3.28.0 → 3.30.0

--- pkgs/desktops/gnome-3/misc/gnome-applets/default.nix
+++ pkgs/desktops/gnome-3/misc/gnome-applets/default.nix
@@ -1,7 +1,5 @@
 { stdenv
 , fetchurl
-, fetchpatch
-, autoreconfHook
 , intltool
 , itstool
 , libxml2
@@ -28,36 +26,16 @@
 
 let
   pname = "gnome-applets";
-  version = "3.28.0";
+  version = "3.30.0";
 in stdenv.mkDerivation rec {
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "0wd6pirv57rcxm5d32r1s3ni7sp26gnqd4qhjciw0pn5ak627y5h";
+    sha256 = "1cvl32486kqw301wy40l1a1sdhanra7bx4smq0a3lmnl3x01zg43";
   };
 
-  patches = [
-    # https://github.com/NixOS/nixpkgs/issues/36468
-    # https://gitlab.gnome.org/GNOME/gnome-applets/issues/3
-    (fetchpatch {
-      url = https://gitlab.gnome.org/GNOME/gnome-applets/commit/1ee719581c33d7d640ae9f656e4e9b192bafef78.patch;
-      sha256 = "05wim7d2ii3pxph3n3am76cvnxmkfpggk0cpy8p5xgm3hcibwfrf";
-    })
-    (fetchpatch {
-      url = https://gitlab.gnome.org/GNOME/gnome-applets/commit/1fa778b01f0e6b70678b0e5755ca0ed7a093fa75.patch;
-      sha256 = "0kppqywn0ab18p64ixz0b58cn5bpqf0xy71bycldlc5ybpdx5mq0";
-    })
-
-    # https://gitlab.gnome.org/GNOME/gnome-applets/issues/4
-    (fetchpatch {
-      url = https://gitlab.gnome.org/GNOME/gnome-applets/commit/e14482a90e6113f211e9328d8c39a69bdf5111d8.patch;
-      sha256 = "10ac0kk38hxqh8yvdlriyyv809qrxbpy9ihp01gizhiw7qpz97ff";
-    })
-  ];
-
   nativeBuildInputs = [
-    autoreconfHook
     intltool
     itstool
     pkgconfig
