899b7663b0ce22676884989cefdcff668cd20250
Alexandre Peyroux <alex@xn--wxa.email>
2018-09-12 21:35:16 +0200

    masterpdfeditor: 5.1.12 -> 5.1.36

--- pkgs/applications/misc/masterpdfeditor/default.nix
+++ pkgs/applications/misc/masterpdfeditor/default.nix
@@ -1,21 +1,25 @@
-{ stdenv, fetchurl, sane-backends, qtbase, qtsvg, nss, autoPatchelfHook }:
+{ stdenv, fetchurl, sane-backends, qtbase, qtsvg, nss, autoPatchelfHook, lib, makeWrapper }:
 
 let
-  version = "5.1.12";
+  version = "5.1.36";
 
 in stdenv.mkDerivation {
   name = "masterpdfeditor-${version}";
 
   src = fetchurl {
     url = "https://code-industry.net/public/master-pdf-editor-${version}_qt5.amd64.tar.gz";
-    sha256 = "1i3pdrhnlj06phm36gs42s6b94pigcfb8wa5dhmplxn0dqp434hq";
+    sha256 = "10kydjg9qi6kvzn7ny5nk4xn2fsm47anq3al89hq97ppm9dlvh48";
   };
 
-  nativeBuildInputs = [ autoPatchelfHook ];
+  nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
 
   buildInputs = [ nss qtbase qtsvg sane-backends stdenv.cc.cc ];
 
   dontStrip = true;
+
+  postInstall = ''
+    wrapProgram $out/bin/masterpdfeditor5 --prefix QT_PLUGIN_PATH : ${qtbase}/lib/qt-5.${lib.versions.minor qtbase.version}/plugins
+  '';
 
   installPhase = ''
     runHook preInstall
