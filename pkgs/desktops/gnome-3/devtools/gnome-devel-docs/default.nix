b8ec4a93980f38689487fba701672658aec0caf1
Jan Tojnar <jtojnar@gmail.com>
2018-09-13 00:40:23 +0000

    gnome3.gnome-devel-docs: 3.28.0 → 3.30.1

--- pkgs/desktops/gnome-3/devtools/gnome-devel-docs/default.nix
+++ pkgs/desktops/gnome-3/devtools/gnome-devel-docs/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "gnome-devel-docs-${version}";
-  version = "3.28.0";
+  version = "3.30.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-devel-docs/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "1py0zyfzpaws41p9iw4645ykfnmm408axfghsmq6gnwgp66vl074";
+    sha256 = "1xnvm8cmxyq9w008fhm1m6aj4ckmkfaa9vj2h7rrgc9qzd6lq39j";
   };
 
   passthru = {
