6101c4fda1ee7d71e923d1aca8987bc79d28e34a
Vladyslav Mykhailichenko <dywedir@pm.me>
2018-09-13 16:41:53 +0300

    mako: init at 1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17446,6 +17446,8 @@ with pkgs;
 
   makeself = callPackage ../applications/misc/makeself { };
 
+  mako = callPackage ../applications/misc/mako { };
+
   mapmap = libsForQt5.callPackage ../applications/video/mapmap { };
 
   marathon = callPackage ../applications/networking/cluster/marathon { };
