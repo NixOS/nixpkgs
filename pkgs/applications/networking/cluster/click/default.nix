c96aee40b0bd1d957d43529f9bb567728cf73aa6
Maximilian Bode <maximilian.bode@tngtech.com>
2018-09-15 10:38:15 +0200

    click: 0.3.1 -> 0.3.2
    patch necessary to fix "error: the lock file needs to be updated but --frozen was passed to prevent this"
    
    (cherry picked from commit 95c7d8300f327406d72ba68625fb54a4b99f7b84)

--- pkgs/applications/networking/cluster/click/default.nix
+++ pkgs/applications/networking/cluster/click/default.nix
@@ -4,17 +4,18 @@ with rustPlatform;
 
 buildRustPackage rec {
   name = "click-${version}";
-  version = "0.3.1";
-  rev = "b5dfb4a8f8344330a098cb61523695dfe0fd296a";
+  version = "0.3.2";
 
   src = fetchFromGitHub {
+    rev = "v${version}";
     owner = "databricks";
     repo = "click";
-    sha256 = "0a2hq4hcxkkx7gs5dv7sr3j5jy2dby4r6y090z7zl2xy5wydr7bi";
-    inherit rev;
+    sha256 = "0sbj41kypn637z1w115w2h5v6bxz3y6w5ikgpx3ihsh89lkc19d2";
   };
 
-  cargoSha256 = "03vgbkv9xsnx44vivbbhjgxv9drp0yjnimgy6hwm32x74r00k3hj";
+  cargoSha256 = "05asqp5312a1g26pvf5hgqhc4kj3iw2hdvml2ycvga33sxb7zm7r";
+
+  patches = [ ./fix_cargo_lock_version.patch ];
 
   buildInputs = stdenv.lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
 
