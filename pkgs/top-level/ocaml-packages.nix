886acaff116ab3678d267b904ffbdd763c57efb5
Vincent Laporte <Vincent.Laporte@gmail.com>
2018-09-16 04:37:08 +0000

    ocamlPackages.doc-ock-xml: init at 1.2.1

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -224,6 +224,8 @@ let
 
     doc-ock-html =  callPackage ../development/ocaml-modules/doc-ock-html { };
 
+    doc-ock-xml =  callPackage ../development/ocaml-modules/doc-ock-xml { };
+
     dolmen =  callPackage ../development/ocaml-modules/dolmen { };
 
     dolog = callPackage ../development/ocaml-modules/dolog { };
