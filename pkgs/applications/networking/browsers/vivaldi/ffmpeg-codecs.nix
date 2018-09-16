d078ba9b5d0604d61942ba11f4451f84fae256da
Lukas Werling <lukas.werling@gmail.com>
2018-09-16 20:53:50 +0200

    vivaldi-ffmpeg-codecs: 61.0.3163.91 -> 69.0.3497.73
    (cherry picked from commit 478e1fe36943a90fd08507b1cf6417d347e63b1b)

--- pkgs/applications/networking/browsers/vivaldi/ffmpeg-codecs.nix
+++ pkgs/applications/networking/browsers/vivaldi/ffmpeg-codecs.nix
@@ -1,37 +1,33 @@
 { stdenv, fetchurl
-, dbus-glib, gtk2, gtk3, libexif, libpulseaudio, libXScrnSaver, ninja, nss
-, pciutils, pkgconfig, python2, xdg_utils
+, dbus-glib, gtk3, libexif, libXScrnSaver, ninja, nss
+, pciutils, pkgconfig, python2, xdg_utils, gn
 }:
 
 stdenv.mkDerivation rec {
   name = "${product}-${version}";
   product = "vivaldi-ffmpeg-codecs";
-  version = "61.0.3163.91";
+  version = "69.0.3497.73";
 
   src = fetchurl {
     url = "https://commondatastorage.googleapis.com/chromium-browser-official/chromium-${version}.tar.xz";
-    sha512 = "3f07vwbxllrwy3agqxa6ndcix23vai18i178zscmk0y68flhzffyjdvrwlg7dzlwgiqypj2cyl21qb4qmcay2ilgw9vnr9fql2x0w7p";
+    sha512 = "3qyzxdybiszwy62izr35wffnh1a1plg9y536vrmd4b2xl8p4nz18c7439blr0cdzsr5qplgrdl64446a27mkyhbw8c3iy0gb4zgb5j9";
   };
 
   buildInputs = [ ];
 
   nativeBuildInputs = [
-    dbus-glib gtk2 gtk3 libexif libpulseaudio libXScrnSaver ninja nss pciutils pkgconfig
-    python2 xdg_utils
+    gtk3 libexif libXScrnSaver ninja nss pciutils python2 xdg_utils gn
+    pkgconfig dbus-glib
   ];
 
   patches = [
-    ./chromium-gn-bootstrap-r14.patch
-    ./chromium-gcc-r1.patch
-    ./chromium-gcc5-r1.patch
   ];
 
   configurePhase = ''
     runHook preConfigure
 
-    local args="ffmpeg_branding=\"ChromeOS\" proprietary_codecs=true enable_hevc_demuxing=true use_gconf=false use_gio=false use_gnome_keyring=false use_kerberos=false use_cups=false use_sysroot=false use_gold=false linux_use_bundled_binutils=false fatal_linker_warnings=false treat_warnings_as_errors=false is_clang=false is_component_build=true is_debug=false symbol_level=0"
-    python tools/gn/bootstrap/bootstrap.py -v -s --no-clean --gn-gen-args "$args"
-    out/Release/gn gen out/Release -v --args="$args"
+    local args="ffmpeg_branding=\"ChromeOS\" proprietary_codecs=true enable_hevc_demuxing=true use_gnome_keyring=false use_sysroot=false use_gold=false use_allocator=\"none\" linux_use_bundled_binutils=false fatal_linker_warnings=false treat_warnings_as_errors=false enable_nacl=false enable_nacl_nonsfi=false is_clang=false clang_use_chrome_plugins=false is_component_build=true is_debug=false symbol_level=0 use_custom_libcxx=false use_lld=false use_jumbo_build=false"
+    gn gen out/Release -v --args="$args"
 
     runHook postConfigure
   '';
@@ -53,6 +49,5 @@ stdenv.mkDerivation rec {
     license     = licenses.lgpl21;
     maintainers = with maintainers; [ lluchs ];
     platforms   = [ "x86_64-linux" ];
-    broken = true;
   };
 }
