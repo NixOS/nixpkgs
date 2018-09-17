eb9de57dd7b99d05d21aff5200333399557018c9
Jan Tojnar <jtojnar@gmail.com>
2018-09-17 12:41:24 +0200

    gcolor3: 2.3 → 2.3.1

--- pkgs/applications/graphics/gcolor3/default.nix
+++ pkgs/applications/graphics/gcolor3/default.nix
@@ -1,28 +1,17 @@
-{ stdenv, fetchFromGitHub, fetchpatch, gnome3, meson, ninja, gettext, pkgconfig, libxml2, gtk3, hicolor-icon-theme, wrapGAppsHook }:
+{ stdenv, fetchFromGitLab, gnome3, meson, ninja, gettext, pkgconfig, libxml2, gtk3, hicolor-icon-theme, wrapGAppsHook }:
 
 let
-  version = "2.3";
+  version = "2.3.1";
 in stdenv.mkDerivation {
   name = "gcolor3-${version}";
 
-  src = fetchFromGitHub {
-    owner = "hjdskes";
+  src = fetchFromGitLab {
+    domain = "gitlab.gnome.org";
+    owner = "World";
     repo = "gcolor3";
     rev = "v${version}";
-    sha256 = "186j72kwsqdcakvdik9jl18gz3csdj53j3ylwagr9gfwmy0nmyjb";
+    sha256 = "10cfzlkflwkb7f51rnrxmgxpfryh1qzvqaydj6lffjq9zvnhigg7";
   };
-
-  patches = [
-    # Fix darwin build
-    (fetchpatch {
-      url = https://github.com/Hjdskes/gcolor3/commit/9130ffeff091fbafff6a0c8f06b09f54657d5dfd.patch;
-      sha256 = "1kn5hx536wivafb4awg7lsa8h32njy0lynmn7ci9y78dlp54057r";
-    })
-    (fetchpatch {
-      url = https://github.com/Hjdskes/gcolor3/commit/8d89081a8e13749f5a9051821114bc5fe814eaf3.patch;
-      sha256 = "1ldyr84dl2g6anqkp2mpxsrcr41fcqwi6ck14rfhai7rgrm8yar3";
-    })
-  ];
 
   nativeBuildInputs = [ meson ninja gettext pkgconfig libxml2 wrapGAppsHook ];
 
@@ -35,8 +24,8 @@ in stdenv.mkDerivation {
 
   meta = with stdenv.lib; {
     description = "A simple color chooser written in GTK3";
-    homepage = https://hjdskes.github.io/projects/gcolor3/;
-    license = licenses.gpl2;
+    homepage = https://www.hjdskes.nl/projects/gcolor3/;
+    license = licenses.gpl2Plus;
     maintainers = with maintainers; [ jtojnar ];
     platforms = platforms.unix;
   };
