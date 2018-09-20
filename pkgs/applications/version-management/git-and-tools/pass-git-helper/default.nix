3c08fd09a5174ee75ca9b3c0d19ddc525e705775
Ivan Solyankin <vanzef@gmail.com>
2018-09-20 23:35:25 +0300

    pass-git-helper: init at 0.4

---
+++ pkgs/applications/version-management/git-and-tools/pass-git-helper/default.nix
@@ -0,0 +1,24 @@
+{ stdenv, buildPythonApplication, isPy3k, fetchFromGitHub, pyxdg }:
+
+buildPythonApplication rec {
+  pname   = "pass-git-helper";
+  version = "0.4";
+
+  disabled = !isPy3k;
+
+  src = fetchFromGitHub {
+    owner  = "languitar";
+    repo   = "pass-git-helper";
+    rev    = "${version}";
+    sha256 = "1zccbmq5l6asl9qm1f90vg9467y3spmv3ayrw07qizrj43yfd9ap";
+  };
+
+  propagatedBuildInputs = [ pyxdg ];
+
+  meta = with stdenv.lib; {
+    homepage = "https://github.com/languitar/pass-git-helper";
+    description = "A git credential helper interfacing with pass, the standard unix password manager";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ vanzef ];
+  };
+}
