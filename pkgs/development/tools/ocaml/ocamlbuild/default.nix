d8178831b02e4157f24f578b4ea041d08cedead9
R. RyanTM <ryantm-bot@ryantm.com>
2018-09-20 07:53:11 -0700

    ocamlPackages.ocamlbuild: 0.12.0 -> 0.13.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/ocamlbuild/versions

--- pkgs/development/tools/ocaml/ocamlbuild/default.nix
+++ pkgs/development/tools/ocaml/ocamlbuild/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchFromGitHub, ocaml, findlib }:
 let
-  version = "0.12.0";
+  version = "0.13.0";
 in
 stdenv.mkDerivation {
   name = "ocamlbuild-${version}";
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
     owner = "ocaml";
     repo = "ocamlbuild";
     rev = version;
-    sha256 = "1shyim50ms0816fphc4mk0kldcx3pnba2i6m10q0cbm18m9d7chq";
+    sha256 = "13r9q8c209gkgcmbjhj9z4r5bmi90rxahdsiqm5jx8sr2pia5xbh";
   };
 
   createFindlibDestdir = true;
