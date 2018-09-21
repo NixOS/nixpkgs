da612211895b7d0910a0eeb2b778c0e9e799a6ee
Peter Hoeg <peter@hoeg.com>
2018-09-21 08:51:37 +0800

    scry: init at 0.7.1.20180919

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6418,6 +6418,8 @@ with pkgs;
 
   icr = callPackage ../development/tools/icr {};
 
+  scry = callPackage ../development/tools/scry {};
+
   devpi-client = callPackage ../development/tools/devpi-client {};
 
   devpi-server = callPackage ../development/tools/devpi-server {};
