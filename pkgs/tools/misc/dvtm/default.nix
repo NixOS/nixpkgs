313a01876a559d2c6099a794b76c57084d5d1593
Luke Clifton <Luke.Clifton@cba.com.au>
2018-09-21 15:52:37 +0800

    Patch DVTM to use self-pipe, making it work on more platforms

--- pkgs/tools/misc/dvtm/default.nix
+++ pkgs/tools/misc/dvtm/default.nix
@@ -9,6 +9,17 @@ stdenv.mkDerivation rec {
     sha256 = "0475w514b7i3gxk6khy8pfj2gx9l7lv2pwacmq92zn1abv01a84g";
   };
 
+  patches = [
+    # https://github.com/martanne/dvtm/pull/69
+    # Use self-pipe instead of signal blocking fixes issues on darwin.
+    (fetchurl {
+      url = "https://github.com/martanne/dvtm/commit/1f1ed664d64603f3f1ce1388571227dc723901b2.patch";
+      sha256 = "1cby8x3ckvhzqa8yxlfrwzgm8wk7yz84kr9psdjr7xwpnca1cqrd";
+    })
+  ];
+
+  CFLAGS = stdenv.lib.optionalString stdenv.isDarwin "-D_DARWIN_C_SOURCE";
+
   postPatch = stdenv.lib.optionalString (customConfig != null) ''
     cp ${builtins.toFile "config.h" customConfig} ./config.h
   '';
@@ -28,7 +39,6 @@ stdenv.mkDerivation rec {
     description = "Dynamic virtual terminal manager";
     homepage = http://www.brain-dump.org/projects/dvtm;
     license = licenses.mit;
-    platforms = platforms.linux;
     maintainers = [ maintainers.vrthra ];
   };
 }
