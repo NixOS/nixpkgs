e8c34d612af9537b265f9f4fc331a118c8c42b87
Tor Hedin Brønner <torhedinbronner@gmail.com>
2018-09-22 23:07:02 +0200

    gnucash: don't fail on glib-2.58's new deprecrations

--- pkgs/applications/office/gnucash/default.nix
+++ pkgs/applications/office/gnucash/default.nix
@@ -43,6 +43,10 @@ stdenv.mkDerivation rec {
 
   propagatedUserEnvPkgs = [ dconf ];
 
+  # glib-2.58 deprecrated g_type_class_add_private
+  # Should probably be removed next version bump
+  CXXFLAGS = [ "-Wno-deprecated-declarations" ];
+
   postPatch = ''
     patchShebangs .
   '';
