c73048605ca1d199e82fd6761333bfbf679d0f4f
Alexander Sosedkin <monk@unboiled.info>
2018-09-22 21:20:08 +0700

    pythonPackages.nixpkgs: init at 0.2.2
    Allows to `from nixpkgs import` stuff in interactive Python sessions

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3978,6 +3978,11 @@
     github = "sztupi";
     name = "Attila Sztupak";
   };
+  t184256 = {
+    email = "monk@unboiled.info";
+    github = "t184256";
+    name = "Alexander Sosedkin";
+  };
   tadfisher = {
     email = "tadfisher@gmail.com";
     github = "tadfisher";
