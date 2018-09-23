7e1e77429d50a2a06bb224e5e989a14a49d615de
Matthew Harm Bekkema <mbekkema97@gmail.com>
2018-09-23 16:37:01 +1000

    steam workaround: Move -P argument next to grep
    Makes reading the code easier, as one would know that they are reading a PCRE regex before actually reading it.

--- pkgs/games/steam/chrootenv.nix
+++ pkgs/games/steam/chrootenv.nix
@@ -181,7 +181,7 @@ in buildFHSUserEnv rec {
     # Workaround for issue #44254 (Steam cannot connect to friends network)
     # https://github.com/NixOS/nixpkgs/issues/44254
     test -z ''${TZ+x} &&
-    new_TZ="$(readlink -f /etc/localtime | grep -o '(?<=/zoneinfo/).*$' -P)" &&
+    new_TZ="$(readlink -f /etc/localtime | grep -P -o '(?<=/zoneinfo/).*$')" &&
     export TZ="$new_TZ"
 
     export STEAM_RUNTIME=${if nativeOnly then "0" else "/steamrt"}
