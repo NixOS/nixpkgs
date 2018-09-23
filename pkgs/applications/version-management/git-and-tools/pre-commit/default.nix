47f8d6f5a20de77a0535d18aceb20236f57767c7
Boris Babic <boris.ivan.babic@gmail.com>
2018-09-23 12:43:16 +0200

    gitAndTools.pre-commit: 1.11.0 -> 1.11.1

--- pkgs/applications/version-management/git-and-tools/pre-commit/default.nix
+++ pkgs/applications/version-management/git-and-tools/pre-commit/default.nix
@@ -1,11 +1,11 @@
 { stdenv, python3Packages }:
 with python3Packages; buildPythonApplication rec {
   pname = "pre_commit";
-  version = "1.11.0";
+  version = "1.11.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "03nxkma8qp5j2bg6ailclnyqfhakp8r8d1mn6zcnjw0ac5r9imc8";
+    sha256 = "1kjrq8z78b6aqhyyw07dlwf6cqls88kik6f5l07hs71fj5ddvs9w";
   };
 
   propagatedBuildInputs = [
