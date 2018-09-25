98c02413dbe0e031e4623ecb49ab9673004aeada
Jon Banafato <jon@jonafato.com>
2018-09-24 23:49:26 -0400

    gnomeExtensions.icon-hider: 20 -> 21

--- pkgs/desktops/gnome-3/extensions/icon-hider/default.nix
+++ pkgs/desktops/gnome-3/extensions/icon-hider/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "gnome-shell-extension-icon-hider-${version}";
-  version = "20";
+  version = "21";
 
   src = fetchFromGitHub {
     owner = "ikalnytskyi";
     repo = "gnome-shell-extension-icon-hider";
     rev = "v${version}";
-    sha256 = "0z2rfr0nlp5zhlfxd6y0sxrxf7dj8sknimn6skxlbdm1xawl1fin";
+    sha256 = "0l0jb0ishaq00d4kdfvv5p7pj7b45dz57y3j2ihqr695bzb6b9hr";
   };
 
   uuid = "icon-hider@kalnitsky.org";
