d4ba6c0bc424e632307a421cb77f484dee04450f
Vincent Laporte <Vincent.Laporte@gmail.com>
2018-09-25 13:40:02 +0000

    dune: 1.2.1 -> 1.3.0

--- pkgs/development/tools/ocaml/dune/default.nix
+++ pkgs/development/tools/ocaml/dune/default.nix
@@ -1,26 +1,26 @@
-{ stdenv, fetchFromGitHub, ocamlPackages, opaline }:
+{ stdenv, fetchurl, ocamlPackages, opaline }:
 
 stdenv.mkDerivation rec {
   name = "dune-${version}";
-  version = "1.2.1";
-  src = fetchFromGitHub {
-    owner = "ocaml";
-    repo = "dune";
-    rev = "${version}";
-    sha256 = "14m5zg1b6d0y03c5qsmqmwiwz2h0iys02rlgn6kzm519ck679bkq";
+  version = "1.3.0";
+  src = fetchurl {
+    url = "https://github.com/ocaml/dune/releases/download/${version}/dune-${version}.tbz";
+    sha256 = "15bzakiclipsyjgcfrj83xdpwwww3aaijyw01bn5cx96cmcqyjad";
   };
 
   buildInputs = with ocamlPackages; [ ocaml findlib ];
+
+  buildFlags = "release";
 
   dontAddPrefix = true;
 
   installPhase = "${opaline}/bin/opaline -prefix $out -libdir $OCAMLFIND_DESTDIR";
 
   meta = {
-    inherit (src.meta) homepage;
-    description = "Fast, portable and opinionated build system";
+    homepage = "https://github.com/ocaml/dune";
+    description = "A composable build system";
     maintainers = [ stdenv.lib.maintainers.vbgl ];
-    license = stdenv.lib.licenses.asl20;
+    license = stdenv.lib.licenses.mit;
     inherit (ocamlPackages.ocaml.meta) platforms;
   };
 }
