3a275ac1eb4ea538222e8b282d9804bab6e6e543
Michael Fellinger <michael.fellinger@xing.com>
2018-09-26 13:35:23 +0200

    ejson: init at 1.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1292,6 +1292,8 @@ with pkgs;
 
   et = callPackage ../applications/misc/et {};
 
+  ejson = callPackage ../development/tools/ejson {};
+
   eternal-terminal = callPackage ../tools/networking/eternal-terminal {};
 
   f3 = callPackage ../tools/filesystems/f3 { };
