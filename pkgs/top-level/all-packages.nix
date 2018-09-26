NixOS/nixpkgs

commit c3612b97e625ad0d70c6ba56d191bcb9a403f44c

fetchFromGitLab: add a group optional argument
For example, paperwork is at https://gitlab.gnome.org/World/OpenPaperwork/paperwork
and you can fetch this repo with group = "World", owner =
"OpenPaperwork", and repo = "paperwork".

pkgs/top-level/all-packages.nix
@@ -288,13 +288,13 @@ with pkgs;
 
   # gitlab example
   fetchFromGitLab = {
-    owner, repo, rev, domain ? "gitlab.com", name ? "source",
+    owner, repo, rev, domain ? "gitlab.com", name ? "source", group ? null,
     ... # For hash agility
   }@args: fetchzip ({
     inherit name;
-    url = "https://${domain}/api/v4/projects/${owner}%2F${repo}/repository/archive.tar.gz?sha=${rev}";
-    meta.homepage = "https://${domain}/${owner}/${repo}/";
-  } // removeAttrs args [ "domain" "owner" "repo" "rev" ]) // { inherit rev; };
+    url = "https://${domain}/api/v4/projects/${lib.optionalString (group != null) group+"%2F"}${owner}%2F${repo}/repository/archive.tar.gz?sha=${rev}";
+    meta.homepage = "https://${domain}/${lib.optionalString (group != null) group+"/"}${owner}/${repo}/";
+  } // removeAttrs args [ "domain" "owner" "group" "repo" "rev" ]) // { inherit rev; };
 
   # gitweb example, snapshot support is optional in gitweb
   fetchFromRepoOrCz = {

commit 593062ddc2104cf91b1cf9cc0e4f4b1437f59f6b

fetchFromGitLab: Fix grouping issue for group arg
Issue introduced by c3612b97e625ad0d70c6ba56d191bcb9a403f44c.

Example output before that commit (witch a random package using
fetchFromGitLab):

$ nix-instantiate --eval -A hiawatha.src.urls
[ "https://gitlab.com/api/v4/projects/hsleisink%2Fhiawatha/repository/archive.tar.gz?sha=v10.8.1" ]

After the commit:

$ nix-instantiate --eval -A hiawatha.src.urls
[ "https://gitlab.com/api/v4/projects/%2Fhsleisink%2Fhiawatha/repository/archive.tar.gz?sha=v10.8.1" ]

And with this very commit, which fixes the issue:

$ nix-instantiate --eval -A hiawatha.src.urls
[ "https://gitlab.com/api/v4/projects/hsleisink%2Fhiawatha/repository/archive.tar.gz?sha=v10.8.1" ]

The problem was that it has used optionalString in conjunction with "+",
so the optionalString only applied to the left-hand side of the +
operator.

Thanks to @ryantm for reporting the issue at:

https://github.com/NixOS/nixpkgs/pull/46487/commits/c3612b97e625ad0d70c6ba56d191bcb9a403f44c#r220576764

Signed-off-by: aszlig <aszlig@nix.build>
Reported-by: Ryan Mulligan <ryan@ryantm.com>

pkgs/top-level/all-packages.nix
@@ -292,8 +292,8 @@ with pkgs;
     ... # For hash agility
   }@args: fetchzip ({
     inherit name;
-    url = "https://${domain}/api/v4/projects/${lib.optionalString (group != null) group+"%2F"}${owner}%2F${repo}/repository/archive.tar.gz?sha=${rev}";
-    meta.homepage = "https://${domain}/${lib.optionalString (group != null) group+"/"}${owner}/${repo}/";
+    url = "https://${domain}/api/v4/projects/${lib.optionalString (group != null) "${group}%2F"}${owner}%2F${repo}/repository/archive.tar.gz?sha=${rev}";
+    meta.homepage = "https://${domain}/${lib.optionalString (group != null) "${group}/"}${owner}/${repo}/";
   } // removeAttrs args [ "domain" "owner" "group" "repo" "rev" ]) // { inherit rev; };
 
   # gitweb example, snapshot support is optional in gitweb