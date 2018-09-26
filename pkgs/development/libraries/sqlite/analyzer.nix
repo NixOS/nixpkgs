2e2740165a56497654dfb6a2845efb4fe1e9cb74
Will Dietz <w@wdtz.org>
2018-09-26 10:28:05 -0500

    sqlite: 3.25.1 -> 3.25.2

--- pkgs/development/libraries/sqlite/analyzer.nix
+++ pkgs/development/libraries/sqlite/analyzer.nix
@@ -6,11 +6,11 @@ in
 
 stdenv.mkDerivation rec {
   name = "sqlite-analyzer-${version}";
-  version = "3.25.1";
+  version = "3.25.2";
 
   src = assert version == sqlite.version; fetchurl {
     url = "https://sqlite.org/2018/sqlite-src-${archiveVersion version}.zip";
-    sha256 = "1bkz5n8zshh51brcxvyfysdy6mmx6zy4xhhdnwvsavnynk042bz6";
+    sha256 = "0n7lzp671x1xz6fx138xa1jhi4vfwib3awaxac5skw6fv9q61940";
   };
 
   nativeBuildInputs = [ unzip ];
