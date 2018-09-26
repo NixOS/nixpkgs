3cd93965237d220fd614a646142eadcd5bd6364e
Bernard Fortz <bernard.fortz@gmail.com>
2018-09-26 14:58:28 +0200

    autojump: autoload when programs.autojump.enable is set.
    (cherry picked from commit cd8ffef01b4629980bb77e33a20c24c236ac41be)
    Signed-off-by: Domen Kožar <domen@dev.si>

--- nixos/modules/programs/autojump.nix
+++ nixos/modules/programs/autojump.nix
@@ -3,15 +3,10 @@
 with lib;
 
 let
-
   cfg = config.programs.autojump;
-
+  prg = config.programs;
 in
-
 {
-
-  ###### interface
-
   options = {
     programs.autojump = {
 
@@ -30,5 +25,9 @@ in
   config = mkIf cfg.enable {
     environment.pathsToLink = [ "/share/autojump" ];
     environment.systemPackages = [ pkgs.autojump ];
+
+    programs.bash.interactiveShellInit = "source ${pkgs.autojump}/share/autojump/autojump.bash"; 
+    programs.zsh.interactiveShellInit = mkIf prg.zsh.enable "source ${pkgs.autojump}/share/autojump/autojump.zsh";
+    programs.fish.interactiveShellInit = mkIf prg.fish.enable "source ${pkgs.autojump}/share/autojump/autojump.fish";
   };
 }
