1fd6477b35579ae254999128843e28f848cedaaa
Florian Klink <flokli@flokli.de>
2018-09-28 00:59:36 +0200

    nextcloud: fix sendmail path discovery

--- pkgs/servers/nextcloud/default.nix
+++ pkgs/servers/nextcloud/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, fetchpatch }:
 
 stdenv.mkDerivation rec {
   name = "nextcloud-${version}";
@@ -8,6 +8,12 @@ stdenv.mkDerivation rec {
     url = "https://download.nextcloud.com/server/releases/${name}.tar.bz2";
     sha256 = "14ymc6fr91735yyc2gqh7c89mbbwsgamhhysf6crp9kp27l83z5a";
   };
+
+  patches = [ (fetchpatch {
+    name = "Mailer-discover-sendmail-path-instead-of-hardcoding-.patch";
+    url = https://github.com/nextcloud/server/pull/11404.patch;
+    sha256 = "1h0cqnfwn735vqrm3yh9nh6a7h6srr9h29p13vywd6rqbcndqjjd";
+  }) ];
 
   installPhase = ''
     mkdir -p $out/
