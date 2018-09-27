d152089772b31417cd243c73fb5d5878d7512f7a
Alyssa Ross <hi@alyssa.is>
2018-09-27 18:18:30 +0100

    ranger: syntax highlight previews by default

--- pkgs/applications/misc/ranger/default.nix
+++ pkgs/applications/misc/ranger/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, python3Packages, file, less
+{ stdenv, lib, fetchFromGitHub, python3Packages, file, less, highlight
 , imagePreviewSupport ? true, w3m ? null}:
 
 with stdenv.lib;
@@ -24,6 +24,11 @@ python3Packages.buildPythonApplication rec {
   '';
 
   preConfigure = ''
+    ${lib.optionalString (highlight != null) ''
+      sed -i -e 's|^\s*highlight\b|${highlight}/bin/highlight|' \
+        ranger/data/scope.sh
+    ''}
+
     substituteInPlace ranger/data/scope.sh \
       --replace "/bin/echo" "echo"
 
@@ -46,7 +51,7 @@ python3Packages.buildPythonApplication rec {
       --replace "set preview_images false" "set preview_images true"
   '';
 
-  meta =  with stdenv.lib; {
+  meta =  with lib; {
     description = "File manager with minimalistic curses interface";
     homepage = http://ranger.github.io/;
     license = licenses.gpl3;
