314e48075092b97dce6fa4654aa7562f106835a8
Peter Simons <simons@cryp.to>
2018-09-27 15:15:36 +0200

    all-cabal-hashes: update to Hackage at 2018-09-27T00:15:48Z
    (cherry picked from commit 0af54ac6704a3227cabf93d4a9d86baa1c6ef608)

--- pkgs/data/misc/hackage/default.nix
+++ pkgs/data/misc/hackage/default.nix
@@ -1,6 +1,6 @@
 { fetchurl }:
 
 fetchurl {
-  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/b16aa9c85633ea49e19aee5e1678d082ce953a17.tar.gz";
-  sha256 = "185p6112b35s2vgbswrmy5lwf09q27gzkpcfla6dbddmbk948d38";
+  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/3715ae81be1a8e0c6a1cb5902c054b6cedfb7f41.tar.gz";
+  sha256 = "05bsk99d4k2nq8dfiagpby0cllx19crzn2rh29v26p0irzagrqfb";
 }
