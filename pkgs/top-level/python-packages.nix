2be514d5c389b50aeeb5773ab269863131a294a8
Florian Klink <flokli@flokli.de>
2018-09-29 21:25:08 +0200

    pythonPackages.pystemd: init at 0.5.0 (#47517)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9805,6 +9805,8 @@ in {
 
   python-pushover = callPackage ../development/python-modules/pushover {};
 
+  pystemd = callPackage ../development/python-modules/pystemd { systemd = pkgs.systemd; };
+
   mongodict = buildPythonPackage rec {
     name = "mongodict-${version}";
     version = "0.3.1";
