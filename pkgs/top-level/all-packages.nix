f2d58e5677090fd7c15d56bfed3d8d4bca73e6f8
José Romildo Malaquias <malaquias@gmail.com>
2018-09-28 23:46:08 -0300

    menumaker: init at 0.99.10

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17540,6 +17540,8 @@ with pkgs;
     gconf = pkgs.gnome2.GConf;
   };
 
+  menumaker = callPackage ../applications/misc/menumaker { };
+
   mercurial = callPackage ../applications/version-management/mercurial {
     inherit (darwin.apple_sdk.frameworks) ApplicationServices;
     guiSupport = false; # use mercurialFull to get hgk GUI
