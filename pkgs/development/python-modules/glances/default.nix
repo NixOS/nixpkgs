2df4b3e6eba48ee1a5e2fc7d0d7b7dc1988bb408
Michael Weiss <dev.primeos@gmail.com>
2018-09-29 11:59:48 +0200

    pythonPackages.glances: Enable the tests (skip the failing ones)

--- pkgs/development/python-modules/glances/default.nix
+++ pkgs/development/python-modules/glances/default.nix
@@ -16,8 +16,10 @@ buildPythonPackage rec {
     sha256 = "1jkjblfk4gbr00j7lny7ajiizzqnp0p1yhzfi14074gwk38z0q14";
   };
 
+  patches = lib.optional doCheck ./skip-failing-tests.patch;
+
   # Requires access to /sys/class/power_supply
-  doCheck = false;
+  doCheck = true;
 
   buildInputs = [ unittest2 ];
   propagatedBuildInputs = [ psutil setuptools bottle batinfo pysnmp hddtemp ];
