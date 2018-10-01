89b618771ad4b0cfdb874dee3d51eb267c4257dd
Peter Hoeg <peter@hoeg.com>
2018-10-01 17:01:24 +0800

    emacs: fix egg dependency on git

--- pkgs/applications/editors/emacs-modes/melpa-packages.nix
+++ pkgs/applications/editors/emacs-modes/melpa-packages.nix
@@ -59,8 +59,11 @@ self:
         inherit (self.melpaPackages) easy-kill;
       };
 
-      # missing git
-      egg = markBroken super.egg;
+      egg = super.egg.overrideAttrs (attrs: {
+        # searches for Git at build time
+        nativeBuildInputs =
+          (attrs.nativeBuildInputs or []) ++ [ external.git ];
+      });
 
       # upstream issue: missing file header
       elmine = markBroken super.elmine;
