11ba2f270f2733d933ff5a70ddbc617512cecd1d
Franz Pletz <fpletz@fnordicwalking.de>
2018-10-01 23:34:06 +0200

    nixos/clamav: fix freshclam service if db up to date

--- nixos/modules/services/security/clamav.nix
+++ nixos/modules/services/security/clamav.nix
@@ -137,6 +137,7 @@ in
       serviceConfig = {
         Type = "oneshot";
         ExecStart = "${pkg}/bin/freshclam";
+        SuccessExitStatus = "1"; # if databases are up to date
         PrivateTmp = "yes";
         PrivateDevices = "yes";
       };
