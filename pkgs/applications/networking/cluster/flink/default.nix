9c7d2aa353e27055e00db2c89bbd75dfb041ab16
Maximilian Bode <maximilian.bode@tngtech.com>
2018-10-01 19:51:54 +0200

    flink: 1.6.0 -> 1.6.1

--- pkgs/applications/networking/cluster/flink/default.nix
+++ pkgs/applications/networking/cluster/flink/default.nix
@@ -8,8 +8,8 @@ let
       sha256 = "1fq7pd5qpchkkwhh30h3l9rhf298jfcfv2dc50z39qmwwijdjajk";
     };
     "1.6" = {
-      flinkVersion = "1.6.0";
-      sha256 = "18fnpldzs36qx7myr9rmym9g9p3qkgnd1z3lfkpbaw590ddaqr9i";
+      flinkVersion = "1.6.1";
+      sha256 = "1z4795va15qnnhk2qx3gzimzgfd9nqchfgn759fnqfmcxh6n9vw3";
     };
   };
 in
