5cc251df89d0396202a4bf3fba8a3a133485265c
Will Dietz <w@wdtz.org>
2018-09-30 21:16:56 -0500

    light: user needs to be in the 'video' group

--- nixos/modules/programs/light.nix
+++ nixos/modules/programs/light.nix
@@ -13,7 +13,8 @@ in
         default = false;
         type = types.bool;
         description = ''
-          Whether to install Light backlight control command and udev rules.
+          Whether to install Light backlight control command
+          and udev rules granting access to members of the "video" group.
         '';
       };
     };
