4652b54f380630037a5a32efd448515928024540
Michael Weiss <dev.primeos@gmail.com>
2018-10-01 20:21:45 +0200

    tdesktop: Update the Arch patches

--- pkgs/applications/networking/instant-messengers/telegram/tdesktop/default.nix
+++ pkgs/applications/networking/instant-messengers/telegram/tdesktop/default.nix
@@ -7,8 +7,8 @@ let
     version = "1.4.0";
     sha256Hash = "1zlsvbk9vgsqwplcswh2q0mqjdqf5md1043paab02wy3qg2x37d8";
     # svn log svn://svn.archlinux.org/community/telegram-desktop/trunk
-    archPatchesRevision = "388448";
-    archPatchesHash = "06a1j7fxg55d3wgab9rnwv93dvwdwkx7mvsxaywwwiz7ng20rgs1";
+    archPatchesRevision = "388730";
+    archPatchesHash = "1gvisz36bc6bl4zcpjyyk0a2dl6ixp65an8wgm2lkc9mhkl783q7";
   };
 in {
   stable = mkTelegram stableVersion;
