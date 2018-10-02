82b5887cab2f52aaea4f5f63e05f695af5730769
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-10-02 11:10:58 -0700

    twa: 1.3.1 -> 1.5.1

--- pkgs/tools/networking/twa/default.nix
+++ pkgs/tools/networking/twa/default.nix
@@ -1,19 +1,28 @@
-{ stdenv, fetchFromGitHub, makeWrapper, bash, gawk, curl, netcat, ncurses }:
+{ stdenv
+, bash
+, host
+, curl
+, fetchFromGitHub
+, gawk
+, makeWrapper
+, ncurses
+, netcat
+}:
 
 stdenv.mkDerivation rec {
   name = "twa-${version}";
-  version = "1.3.1";
+  version = "1.5.1";
 
   src = fetchFromGitHub {
     owner = "trailofbits";
     repo = "twa";
     rev = version;
-    sha256 = "16x9nzsrf10waqmjm423vx44820c6mls7gxc8azrdqnz18vdy1h4";
+    sha256 = "14pwiq1kza92w2aq358zh5hrxpxpfhg31am03b56g6vlvqzsvib7";
   };
 
   dontBuild = true;
 
-  buildInputs = [ makeWrapper bash gawk curl netcat ];
+  buildInputs = [ makeWrapper bash gawk curl netcat host.dnsutils ];
 
   installPhase = ''
     install -Dm 0755 twa "$out/bin/twa"
@@ -22,7 +31,7 @@ stdenv.mkDerivation rec {
     install -Dm 0644 README.md "$out/share/doc/twa/README.md"
 
     wrapProgram "$out/bin/twa" \
-      --prefix PATH : ${stdenv.lib.makeBinPath [ curl netcat ncurses ]}
+      --prefix PATH : ${stdenv.lib.makeBinPath [ curl netcat ncurses host.dnsutils ]}
   '';
 
   meta = {
