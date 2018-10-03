6e93cdfe3505f64e591393d115f918b9804348f5
Maximilian Bosch <maximilian@mbosch.me>
2018-10-03 12:31:08 +0200

    nixos/activation: fix systemd-user daemon-reload in auto-upgrade service (#47695)
    The autoupgrade service defined in `system.autoUpgrade`
    (`nixos/modules/installer/tools/auto-upgrade.nix`) doesn't have `su` in
    its path and thus yields a warning during the `daemon-reload`.
    
    Specifying the absolute path fixes the issue.
    
    Fixes #47648
    
    (cherry picked from commit 7297cc550165a7cd10f6a9946c70c3a7198fcc78 and
    resolved conflict)

--- nixos/modules/system/activation/top-level.nix
+++ nixos/modules/system/activation/top-level.nix
@@ -116,6 +116,7 @@ let
       inherit (pkgs) utillinux coreutils;
       systemd = config.systemd.package;
       shell = "${pkgs.bash}/bin/sh";
+      su = "${pkgs.shadow.su}/bin/su";
 
       inherit children;
       kernelParams = config.boot.kernelParams;
