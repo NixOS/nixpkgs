964214b10700fab42bbeac6092d20bb7ad8d4e35
Benjamin Hipple <bhipple@protonmail.com>
2018-10-04 01:17:18 +0000

    cpplint: init at 1.3.0
    Static analyzer for C++ code. Licensed under a very permissive, generic Google license (BSD3 wording).

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8264,6 +8264,8 @@ with pkgs;
 
   cppcheck = callPackage ../development/tools/analysis/cppcheck { };
 
+  cpplint = callPackage ../development/tools/analysis/cpplint { };
+
   cquery = callPackage ../development/tools/misc/cquery {
     llvmPackages = llvmPackages_6;
   };
