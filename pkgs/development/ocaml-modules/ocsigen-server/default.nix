975a5050a33b477e148eb93281a75d546dd378d2
Peter Hoeg <peter@hoeg.com>
2018-10-04 15:12:52 +0800

    tree-wide: /var/run -> /run

--- pkgs/development/ocaml-modules/ocsigen-server/default.nix
+++ pkgs/development/ocaml-modules/ocsigen-server/default.nix
@@ -44,7 +44,7 @@ stdenv.mkDerivation {
 
   postFixup =
   ''
-  rm -rf $out/var/run
+  rm -rf $out/run
   wrapProgram $out/bin/ocsigenserver \
     --prefix CAML_LD_LIBRARY_PATH : "${mkpath ssl "ssl"}:${param.ldpath}:${mkpath ocamlnet "netsys"}:${mkpath ocamlnet "netstring"}:${mkpath ocaml_pcre "pcre"}:${mkpath cryptokit "cryptokit"}:${mkpath ocaml_sqlite3 "sqlite3"}"
   '';
