b4f0a7c2fbb8862857ed6532f4e0fdbec83e41d8
Peter Hoeg <peter@hoeg.com>
2018-10-04 15:12:44 +0800

    tree-wide: /var/run -> /run

--- nixos/modules/programs/xonsh.nix
+++ nixos/modules/programs/xonsh.nix
@@ -50,7 +50,7 @@ in
 
     environment.shells =
       [ "/run/current-system/sw/bin/xonsh"
-        "/var/run/current-system/sw/bin/xonsh"
+        "/run/current-system/sw/bin/xonsh"
         "${pkgs.xonsh}/bin/xonsh"
       ];
 
