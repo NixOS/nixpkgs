0a7e258012b60cbe530a756f09a4f2516786d370
Gabriel Ebner <gebner@gebner.org>
2018-10-04 16:33:00 +0200

    elan: 0.5.0 -> 0.7.1

--- pkgs/applications/science/logic/elan/default.nix
+++ pkgs/applications/science/logic/elan/default.nix
@@ -2,15 +2,15 @@
 
 rustPlatform.buildRustPackage rec {
   name = "elan-${version}";
-  version = "0.5.0";
+  version = "0.7.1";
 
-  cargoSha256 = "01d3s47fjszxx8s5gr3haxq3kz3hswkrkr8x97wx8l4nfhm8ndd2";
+  cargoSha256 = "0vv7kr7rc3lvas7ngp5dp99ajjd5v8k5937ish7zqz1k4970q2f1";
 
   src = fetchFromGitHub {
     owner = "kha";
     repo = "elan";
     rev = "v${version}";
-    sha256 = "13zcqlyz0nwvd574llndrs7kvkznj6njljkq3v5j7kb3vndkj6i9";
+    sha256 = "0x5s1wm78yx5ci63wrmlkzm6k3281p33gn4dzw25k5s4vx0p9n24";
   };
 
   nativeBuildInputs = [ pkgconfig ];
