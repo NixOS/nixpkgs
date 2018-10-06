364d47723692d88e391a9e19679707a1d3a29402
Carlos D'Agostino <m@cdagostino.io>
2018-10-06 17:44:22 +1000

    ocamlPackages.ezxmlm: init at 1.0.2

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -252,6 +252,8 @@ let
 
     ezjsonm = callPackage ../development/ocaml-modules/ezjsonm { };
 
+    ezxmlm = callPackage ../development/ocaml-modules/ezxmlm { };
+
     facile = callPackage ../development/ocaml-modules/facile { };
 
     faillib = callPackage ../development/ocaml-modules/faillib { };
