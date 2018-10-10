42725274ef0855ff28bf01cffc7953326ca58e5c
Tim Steinbach <tim@nequissimus.com>
2018-10-10 19:48:22 -0400

    linux: 4.18.12 -> 4.18.13
    (cherry picked from commit cf440dae56392484f893eb6913b56cb5865976e8)

--- pkgs/os-specific/linux/kernel/linux-4.18.nix
+++ pkgs/os-specific/linux/kernel/linux-4.18.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.18.12";
+  version = "4.18.13";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1icz2nkhkb1xhpmc9gxfhc3ywkni8nywk25ixrmgcxp5rgcmlsl4";
+    sha256 = "0l4n72gajgw2vqghl6ps0yjy1safnypyzkcp841zkkwinp3a31yw";
   };
 } // (args.argsOverride or {}))
