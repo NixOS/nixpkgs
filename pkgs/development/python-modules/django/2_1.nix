50ad0f2cc79a68b2db9d4403eacbd311eedfe38e
Lancelot SIX <lsix@lancelotsix.com>
2018-10-10 14:58:51 +0200

    pythonPackages.django_2_1: 2.1.1 -> 2.1.2
    See https://docs.djangoproject.com/en/2.1/releases/2.1.2/

--- pkgs/development/python-modules/django/2_1.nix
+++ pkgs/development/python-modules/django/2_1.nix
@@ -6,13 +6,13 @@
 
 buildPythonPackage rec {
   pname = "Django";
-  version = "2.1.1";
+  version = "2.1.2";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0b7p6d77xqs2hdq2k1amqgbr3050zn5i8asz5w2lxajqnvb1rk71";
+    sha256 = "0ibbs76pzy8zd47yviljrp1s66fmbf5b62fixayaznj7pdzavg7g";
   };
 
   patches = stdenv.lib.optionals withGdal [
