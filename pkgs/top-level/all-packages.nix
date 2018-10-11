6d6bc0a58c378e37fedad889fa53b486fd6fcba4
Franz Pletz <fpletz@fnordicwalking.de>
2018-10-11 02:07:24 +0200

    equilux-theme: init at 20180927
    (cherry picked from commit 6892cbce0905fed4f67904425478e5fb35d05582)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21451,6 +21451,8 @@ with pkgs;
 
   electricsheep = callPackage ../misc/screensavers/electricsheep { };
 
+  equilux-theme = callPackage ../misc/themes/equilux-theme { };
+
   flam3 = callPackage ../tools/graphics/flam3 { };
 
   glee = callPackage ../tools/graphics/glee { };
