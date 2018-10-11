27298d378f2128450bedbb7cec3d0dc4b967a131
Alexandre Peyroux <alex@xn--wxa.email>
2018-10-11 20:17:37 +0200

    masterpdfeditor: 5.1.36 -> 5.1.60

--- pkgs/applications/misc/masterpdfeditor/default.nix
+++ pkgs/applications/misc/masterpdfeditor/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, sane-backends, qtbase, qtsvg, nss, autoPatchelfHook, lib, makeWrapper }:
 
 let
-  version = "5.1.36";
+  version = "5.1.60";
 
 in stdenv.mkDerivation {
   name = "masterpdfeditor-${version}";
 
   src = fetchurl {
     url = "https://code-industry.net/public/master-pdf-editor-${version}_qt5.amd64.tar.gz";
-    sha256 = "10kydjg9qi6kvzn7ny5nk4xn2fsm47anq3al89hq97ppm9dlvh48";
+    sha256 = "0br5f04klzpbd25igbjjj1dqasmrcrw2zsan5bv0ydnr2lmpb2fz";
   };
 
   nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
