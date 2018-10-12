ea6d6d051d26c952b3587079d8df87e092ab1eaf
Aaron Andersen <aaron@fosslib.net>
2018-10-11 20:53:42 -0400

    added a test to ensure rsyslog is receiving messages when expected

--- nixos/tests/rsyslogd.nix
+++ nixos/tests/rsyslogd.nix
@@ -1,27 +1,37 @@
-import ./make-test.nix ({ pkgs, lib, ... }:
-{
-  name = "rsyslogd";
-  meta.maintainers = [ lib.maintainers.aanderse ];
+{ system ? builtins.currentSystem }:
 
-  nodes = {
-    machine1 =
+with import ../lib/testing.nix { inherit system; };
+{
+  test1 = makeTest {
+    name = "rsyslogd-test1";
+    meta.maintainers = [ lib.maintainers.aanderse ];
+
+    machine =
       { config, pkgs, ... }:
       { services.rsyslogd.enable = true;
         services.journald.forwardToSyslog = false;
       };
 
-    machine2 =
+    # ensure rsyslogd isn't receiving messages from journald if explicitly disabled
+    testScript = ''
+      $machine->waitForUnit("default.target");
+      $machine->fail("test -f /var/log/messages");
+    '';
+  };
+
+  test2 = makeTest {
+    name = "rsyslogd-test2";
+    meta.maintainers = [ lib.maintainers.aanderse ];
+
+    machine =
       { config, pkgs, ... }:
       { services.rsyslogd.enable = true;
       };
+
+    # ensure rsyslogd is receiving messages from journald
+    testScript = ''
+      $machine->waitForUnit("default.target");
+      $machine->succeed("test -f /var/log/messages");
+    '';
   };
-
-  # ensure rsyslogd is receiving messages from journald if and only if setup to do so
-  testScript = ''
-    $machine1->waitForUnit("default.target");
-    $machine1->fail("test -f /var/log/messages");
-
-    $machine2->waitForUnit("default.target");
-    $machine2->succeed("test -f /var/log/messages");
-  '';
-})
+}
