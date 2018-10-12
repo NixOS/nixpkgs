c6407efb84a08e36c745949d49fa227e3dafe280
Renaud <c0bw3b@users.noreply.github.com>
2018-10-12 17:04:55 +0200

    eccodes: src and homepage URLs changed

--- pkgs/development/libraries/eccodes/default.nix
+++ pkgs/development/libraries/eccodes/default.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
   version = "2.9.0";
 
   src = fetchurl {
-    url = "https://software.ecmwf.int/wiki/download/attachments/45757960/eccodes-${version}-Source.tar.gz";
+    url = "https://confluence.ecmwf.int/download/attachments/45757960/eccodes-${version}-Source.tar.gz";
     sha256 = "1mh9zkfb5dj3j8fk3gdhz2bp6z13nik5pmynpf5l6qy3lhgyn17z";
   };
 
@@ -43,7 +43,7 @@ stdenv.mkDerivation rec {
   '';
 
   meta = {
-    homepage = https://software.ecmwf.int/wiki/display/ECC/;
+    homepage = https://confluence.ecmwf.int/display/ECC/;
     license = licenses.asl20;
     maintainers = with maintainers; [ knedlsepp ];
     platforms = platforms.unix;
