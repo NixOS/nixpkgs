3c73476e012ef5716850c96ddfdbcdf1b6d9e6ed
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-10-12 13:39:26 -0400

    lirc: add support for Python bindings

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2927,6 +2927,10 @@ in {
     };
   };
 
+  lirc = disabledIf isPy27 (toPythonModule (pkgs.lirc.override {
+    python3 = python;
+  }));
+
   jupyter = buildPythonPackage rec {
     version = "1.0.0";
     name = "jupyter-${version}";
