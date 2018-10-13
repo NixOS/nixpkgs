e85e0c2cafd5b8fff9e19eb48a92947a30787614
Franz Pletz <fpletz@fnordicwalking.de>
2018-10-13 15:59:20 +0200

    wireguard: 0.0.20180904 -> 0.0.20181007
    backport of 97d2779059869653e2d97ed46da16842046eff81

--- pkgs/os-specific/linux/wireguard/default.nix
+++ pkgs/os-specific/linux/wireguard/default.nix
@@ -5,11 +5,11 @@ assert kernel != null -> stdenv.lib.versionAtLeast kernel.version "3.10";
 
 let
   name = "wireguard-${version}";
-  version = "0.0.20180904";
+  version = "0.0.20181007";
 
   src = fetchurl {
     url    = "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-${version}.tar.xz";
-    sha256 = "1w9qzvrsivc3k18hl1fv513in4gi1m0my26hlb6vfzaak5rav3m3";
+    sha256 = "12dg2jrwp26mv8zzmp4cnqs9n3m55x6dcxkyjjsp2j2r2q90svnj";
   };
 
   meta = with stdenv.lib; {
