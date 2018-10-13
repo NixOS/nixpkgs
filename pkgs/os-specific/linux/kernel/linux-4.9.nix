cb6fbe8a7245bd7efada6cb95a842c441f1083d0
Tim Steinbach <tim@nequissimus.com>
2018-10-13 12:41:07 -0400

    linux: 4.9.132 -> 4.9.133
    (cherry picked from commit b4d4affe41285fb47ed6d9f57967c7998b91439d)

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.132";
+  version = "4.9.133";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1lz53r6p293y5fwx7pz9ymj9sss3wmip8hcc48zwkcwm8phnmrk7";
+    sha256 = "0qv5n8vipkqcd0hpf5l41h023n46rgja39h895phlcxs4p00ywsk";
   };
 } // (args.argsOverride or {}))
