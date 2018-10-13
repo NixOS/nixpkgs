c02fc32e1d3fefb9ccc7e8f3b1cba7d11180851c
Will Dietz <w@wdtz.org>
2018-10-12 21:45:02 -0500

    gpgme: enable tests, they pass! :)

--- pkgs/development/libraries/gpgme/default.nix
+++ pkgs/development/libraries/gpgme/default.nix
@@ -49,7 +49,7 @@ stdenv.mkDerivation rec {
 
   checkInputs = [ which ];
 
-  doCheck = false; # fails 8 out of 26 tests with "GPGME: Decryption failed". Spooky!
+  doCheck = true;
 
   meta = with stdenv.lib; {
     homepage = https://gnupg.org/software/gpgme/index.html;
