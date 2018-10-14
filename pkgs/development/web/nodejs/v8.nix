5a056ea90c85e73dd4d790a3f9a13f2bcd435e3a
adisbladis <adisbladis@gmail.com>
2018-10-14 13:58:02 +0800

    nodejs-8_x: 8.11.4 -> 8.12.0
    (cherry picked from commit 2e94003c9cdb294ab5f24b84139337efeb74a2e0)

--- pkgs/development/web/nodejs/v8.nix
+++ pkgs/development/web/nodejs/v8.nix
@@ -5,6 +5,6 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "8.11.4";
-    sha256 = "02d6a9sq81mbvap6h1ckwrang6wrxbkg0xxzn06wn2vbv7k7vkpv";
+    version = "8.12.0";
+    sha256 = "16j1rrxkhmvpcw689ndw1raql1gz4jqn7n82z55zn63c05cgz7as";
   }
