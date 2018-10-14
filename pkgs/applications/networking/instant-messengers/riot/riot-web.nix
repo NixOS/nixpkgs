34e8009dcb9ca473ce98386697eefd539ac5eb19
R. RyanTM <ryantm-bot@ryantm.com>
2018-10-14 03:46:48 -0700

    riot-web: 0.16.2 -> 0.16.5

--- pkgs/applications/networking/instant-messengers/riot/riot-web.nix
+++ pkgs/applications/networking/instant-messengers/riot/riot-web.nix
@@ -3,11 +3,11 @@
 let configFile = writeText "riot-config.json" conf; in
 stdenv.mkDerivation rec {
   name= "riot-web-${version}";
-  version = "0.16.2";
+  version = "0.16.5";
 
   src = fetchurl {
     url = "https://github.com/vector-im/riot-web/releases/download/v${version}/riot-v${version}.tar.gz";
-    sha256 = "14k8hsz2i1nd126jprvi45spdxawk4c8nb3flkrg7rmjdp5sski2";
+    sha256 = "1b82d82pfv4kjdxghc8y78zwmnc89hi7arvql2bx0zyfhzxj6drl";
   };
 
   installPhase = ''
