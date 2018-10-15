f379425159ee4fd1aa01a6451be9f2f13858bf60
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 15:16:03 -0400

    pythonPackages.mxnet: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1439,32 +1439,7 @@ in {
 
   mpyq = callPackage ../development/python-modules/mpyq { };
 
-  mxnet = buildPythonPackage rec {
-    inherit (pkgs.mxnet) name version src meta;
-
-    buildInputs = [ pkgs.mxnet ];
-    propagatedBuildInputs = with self; [ requests numpy graphviz ];
-
-    LD_LIBRARY_PATH = makeLibraryPath [ pkgs.mxnet ];
-
-    doCheck = !isPy3k;
-
-    postPatch = ''
-      substituteInPlace python/setup.py \
-      --replace "graphviz<0.9.0" "graphviz<0.10.0" \
-      --replace "numpy<=1.15.0" "numpy<1.16.0" \
-      --replace "requests<2.19.0" "requests<2.20.0"
-    '';
-
-    preConfigure = ''
-      cd python
-    '';
-
-    postInstall = ''
-      rm -rf $out/mxnet
-      ln -s ${pkgs.mxnet}/lib/libmxnet.so $out/${python.sitePackages}/mxnet
-    '';
-  };
+  mxnet = callPackage ../development/python-modules/mxnet { };
 
   parsy = callPackage ../development/python-modules/parsy { };
 
