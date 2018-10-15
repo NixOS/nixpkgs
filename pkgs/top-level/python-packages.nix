56739bc25feccb9d83f93d8fa183028032cdef88
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 16:01:07 -0400

    pythonPackages.functools32: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1800,21 +1800,7 @@ in {
 
   fastcache = callPackage ../development/python-modules/fastcache { };
 
-  functools32 = if isPy3k then null else buildPythonPackage rec {
-    name = "functools32-${version}";
-    version = "3.2.3-2";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/f/functools32/functools32-${version}.tar.gz";
-      sha256 = "0v8ya0b58x47wp216n1zamimv4iw57cxz3xxhzix52jkw3xks9gn";
-    };
-
-
-    meta = with stdenv.lib; {
-      description = "This is a backport of the functools standard library module from";
-      homepage = "https://github.com/MiCHiLU/python-functools32";
-    };
-  };
+  functools32 = callPackage ../development/python-modules/functools32 { };
 
   gateone = buildPythonPackage rec {
     name = "gateone-1.2-0d57c3";
