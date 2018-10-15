8843eb63d67090acd5592f6630cf12ff2da3c2e1
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 11:46:43 -0400

    pythonPackages.caldavclientlibrary-asynk: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1065,34 +1065,7 @@ in {
 
   bitmath = callPackage ../development/python-modules/bitmath { };
 
-  caldavclientlibrary-asynk = buildPythonPackage rec {
-    version = "asynkdev";
-    name = "caldavclientlibrary-asynk-${version}";
-
-    src = pkgs.fetchgit {
-      url = "https://github.com/skarra/CalDAVClientLibrary.git";
-      rev = "06699b08190d50cc2636b921a654d67db0a967d1";
-      sha256 = "157q32251ac9x3gdshgrjwsy48nq74vrzviswvph56h9wa8ksnnk";
-    };
-
-    disabled = isPy3k;
-
-    meta = {
-      description = "A Python library and tool for CalDAV";
-
-      longDescription = ''
-        CalDAVCLientLibrary is a Python library and tool for CalDAV.
-
-        This package is the unofficial CalDAVCLientLibrary Python
-        library maintained by the author of Asynk and is needed for
-        that package.
-      '';
-
-      homepage = https://github.com/skarra/CalDAVClientLibrary/tree/asynkdev/;
-      maintainers = with maintainers; [ pjones ];
-      broken = true; # 2018-04-11
-    };
-  };
+  caldavclientlibrary-asynk = callPackage ../development/python-modules/caldavclientlibrary-asynk { };
 
   biopython = callPackage ../development/python-modules/biopython { };
 
