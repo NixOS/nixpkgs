22768e77cb12732e341124535739be829414c5be
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 13:11:39 -0400

    pythonPackages.cntk: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1274,33 +1274,7 @@ in {
 
   CDDB = callPackage ../development/python-modules/cddb { };
 
-  cntk = buildPythonPackage rec {
-    inherit (pkgs.cntk) name version src meta;
-
-    buildInputs = [ pkgs.cntk pkgs.swig pkgs.openmpi ];
-    propagatedBuildInputs = with self; [ numpy scipy enum34 protobuf pip ];
-
-    CNTK_LIB_PATH = "${pkgs.cntk}/lib";
-    CNTK_COMPONENT_VERSION = pkgs.cntk.version;
-
-    postPatch = ''
-      cd bindings/python
-      sed -i 's,"libmpi.so.12","${pkgs.openmpi}/lib/libmpi.so",g' cntk/train/distributed.py
-    '';
-
-    postInstall = ''
-      rm -rf $out/${python.sitePackages}/cntk/libs
-      ln -s ${pkgs.cntk}/lib $out/${python.sitePackages}/cntk/libs
-      # It's not installed for some reason.
-      cp cntk/cntk_py.py $out/${python.sitePackages}/cntk
-    '';
-
-    # Actual tests are broken.
-    checkPhase = ''
-      cd $NIX_BUILD_TOP
-      ${python.interpreter} -c "import cntk"
-    '';
-  };
+  cntk = callPackage ../development/python-modules/cntk { };
 
   celery = callPackage ../development/python-modules/celery { };
 
