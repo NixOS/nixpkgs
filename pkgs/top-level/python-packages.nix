41a4e3bb5f1560e9352b913f9ca4c7cab677ef0b
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 14:28:56 -0400

    pythonPackages.closure-linter: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1312,29 +1312,7 @@ in {
 
   cligj = callPackage ../development/python-modules/cligj { };
 
-  /* There is a project called "closure-linter" on PyPI that is the
-     same as this, but it does not appear to be owned by Google.
-     So we're pulling from Google's GitHub repo instead. */
-  closure-linter = buildPythonPackage rec {
-    name = "closure-linter-${version}";
-    version = "2.3.19";
-
-    /* This project has no Python 3 support, as noted by
-       https://github.com/google/closure-linter/issues/81 */
-    disabled = isPy3k;
-
-    propagatedBuildInputs = with self; [ gflags ];
-    src = pkgs.fetchgit {
-      url = "https://github.com/google/closure-linter";
-      rev = "5c27529075bb88bdc45e73008f496dec8438d658";
-      sha256 = "076c7q7pr7akfvq5y8lxr1ab81wwps07gw00igdkcxnc5k9dzxwc";
-    };
-    meta = {
-      description = "Checks JavaScript files against Google's style guide.";
-      homepage = "https://developers.google.com/closure/utilities/";
-      license = with licenses; [ asl20 ];
-    };
-  };
+  closure-linter = callPackage ../development/python-modules/closure-linter { };
 
   cloudpickle = callPackage ../development/python-modules/cloudpickle { };
 
