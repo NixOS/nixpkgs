e5ca5a81a78e5e0a8d8b0e38ca4390e9ba0f7146
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 16:48:11 -0400

    pythonPackages.lazy-object-proxy: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5674,30 +5674,7 @@ in {
 
   lark-parser = callPackage ../development/python-modules/lark-parser { };
 
-  lazy-object-proxy = buildPythonPackage rec {
-    name = "lazy-object-proxy-${version}";
-    version = "1.2.1";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/l/lazy-object-proxy/${name}.tar.gz";
-      sha256 = "22ed751a2c63c6cf718674fd7461b1dfc45215bab4751ca32b6c9b8cb2734cb3";
-    };
-
-    buildInputs = with self; [ pytest ];
-    checkPhase = ''
-      py.test tests
-    '';
-
-    # Broken tests. Seem to be fixed upstream according to Travis.
-    doCheck = false;
-
-    meta = {
-      description = "A fast and thorough lazy object proxy";
-      homepage = https://github.com/ionelmc/python-lazy-object-proxy;
-      license = with licenses; [ bsd2 ];
-    };
-
-  };
+  lazy-object-proxy = callPackage ../development/python-modules/lazy-object-proxy { };
 
   ldaptor = callPackage ../development/python-modules/ldaptor { };
 
