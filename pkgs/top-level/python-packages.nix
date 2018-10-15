8ea312f7714115d8c0ab18684ed08dc8e53a360b
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 15:31:19 -0400

    pythonPackages.eventlib: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1736,31 +1736,7 @@ in {
 
   eve = callPackage ../development/python-modules/eve {};
 
-  eventlib = buildPythonPackage rec {
-    pname = "python-eventlib";
-    name = "${pname}-${version}";
-    version = "0.2.2";
-
-    # Judging from SyntaxError
-    disabled = isPy3k;
-
-    src = pkgs.fetchdarcs {
-      url = "http://devel.ag-projects.com/repositories/${pname}";
-      rev = "release-${version}";
-      sha256 = "1zxhpq8i4jwsk7wmfncqfm211hqikj3hp38cfv509924bi76wak8";
-    };
-
-    propagatedBuildInputs = with self; [ greenlet ];
-
-    doCheck = false;
-
-    meta = {
-      description = "Eventlib bindings for python";
-      homepage    = "http://ag-projects.com/";
-      license     = licenses.lgpl2;
-      platforms   = platforms.all;
-    };
-  };
+  eventlib = callPackage ../development/python-modules/eventlib { };
 
   events = callPackage ../development/python-modules/events { };
 
