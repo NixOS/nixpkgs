d430e7ece13564115c4cd56ff61831cfc9610d39
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 16:33:23 -0400

    pythonPackages.pythonIRClib: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5595,32 +5595,7 @@ in {
 
   ipdbplugin = callPackage ../development/python-modules/ipdbplugin { };
 
-  pythonIRClib = buildPythonPackage rec {
-    name = "irclib-${version}";
-    version = "0.4.8";
-
-    src = pkgs.fetchurl {
-      url = "mirror://sourceforge/python-irclib/python-irclib-${version}.tar.gz";
-      sha256 = "1x5456y4rbxmnw4yblhb4as5791glcw394bm36px3x6l05j3mvl1";
-    };
-
-    patches = [(pkgs.fetchurl {
-      url = "http://trac.uwc.ac.za/trac/python_tools/browser/xmpp/resources/irc-transport/irclib.py.diff?rev=387&format=raw";
-      name = "irclib.py.diff";
-      sha256 = "5fb8d95d6c95c93eaa400b38447c63e7a176b9502bc49b2f9b788c9905f4ec5e";
-    })];
-
-    patchFlags = "irclib.py";
-
-    propagatedBuildInputs = with self; [ paver ];
-
-    disabled = isPy3k;
-    meta = {
-      description = "Python IRC library";
-      homepage = https://bitbucket.org/jaraco/irc;
-      license = with licenses; [ lgpl21 ];
-    };
-  };
+  pythonIRClib = callPackage ../development/python-modules/pythonirclib { };
 
   iso-639 = callPackage ../development/python-modules/iso-639 {};
 
