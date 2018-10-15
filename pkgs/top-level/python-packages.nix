3c36c02ca58efe53553efb5c6990fe472cf28d37
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 15:34:55 -0400

    pythonPackages.eyeD3: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1740,40 +1740,7 @@ in {
 
   events = callPackage ../development/python-modules/events { };
 
-  eyeD3 = buildPythonPackage rec {
-    version = "0.7.8";
-    name    = "eyeD3-${version}";
-    disabled = isPyPy;
-
-    src = pkgs.fetchurl {
-      url = "http://eyed3.nicfit.net/releases/${name}.tar.gz";
-      sha256 = "1nv7nhfn1d0qm7rgkzksbccgqisng8klf97np0nwaqwd5dbmdf86";
-    };
-
-    buildInputs = with self; [ paver ];
-
-    postInstall = ''
-      for prog in "$out/bin/"*; do
-        wrapProgram "$prog" --prefix PYTHONPATH : "$PYTHONPATH" \
-                            --prefix PATH : ${python}/bin
-      done
-    '';
-
-    meta = {
-      description = "A Python module and command line program for processing ID3 tags";
-      homepage    = http://eyed3.nicfit.net/;
-      license     = licenses.gpl2;
-      maintainers = with maintainers; [ lovek323 ];
-      platforms   = platforms.unix;
-
-      longDescription = ''
-        eyeD3 is a Python module and command line program for processing ID3
-        tags. Information about mp3 files (i.e bit rate, sample frequency, play
-        time, etc.) is also provided. The formats supported are ID3 v1.0/v1.1
-        and v2.3/v2.4.
-      '';
-    };
-  };
+  eyeD3 = callPackage ../development/python-modules/eyed3 { };
 
   execnet = buildPythonPackage rec {
     name = "${pname}-${version}";
