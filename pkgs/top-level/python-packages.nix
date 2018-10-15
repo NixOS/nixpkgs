7fde0dd2ef2a86f2e28074645e6774d0cacff10d
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 15:23:31 -0400

    pythonPackages.etcd: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1730,31 +1730,7 @@ in {
 
   escapism = callPackage ../development/python-modules/escapism { };
 
-  etcd = buildPythonPackage rec {
-    name = "etcd-${version}";
-    version = "2.0.8";
-
-    # PyPI package is incomplete
-    src = pkgs.fetchurl {
-      url = "https://github.com/dsoprea/PythonEtcdClient/archive/${version}.tar.gz";
-      sha256 = "0fi6rxa1yxvz7nwrc7dw6fax3041d6bj3iyhywjgbkg7nadi9i8v";
-    };
-
-    patchPhase = ''
-      sed -i -e '13,14d;37d' setup.py
-    '';
-
-    propagatedBuildInputs = with self; [ simplejson pytz requests ];
-
-    # No proper tests are available
-    doCheck = false;
-
-    meta = {
-      description = "A Python etcd client that just works";
-      homepage = https://github.com/dsoprea/PythonEtcdClient;
-      license = licenses.gpl2;
-    };
-  };
+  etcd = callPackage ../development/python-modules/etcd { };
 
   evdev = callPackage ../development/python-modules/evdev {};
 
