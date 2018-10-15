63a0e8613a507a24694ae23df698c6b5c148484d
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 15:20:29 -0400

    pythonPackages.editorconfig: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1712,30 +1712,7 @@ in {
       pythonPackages = self;
     }));
 
-  EditorConfig = buildPythonPackage rec {
-    name = "EditorConfig-${version}";
-    version = "0.12.0";
-
-    # fetchgit used to ensure test submodule is available
-    src = pkgs.fetchgit {
-      url = "https://github.com/editorconfig/editorconfig-core-py";
-      rev = "refs/tags/v${version}";
-      sha256 = "0svk7id7ncygj2rnxhm7602xizljyidk4xgrl6i0xgq3829cz4bl";
-    };
-
-    buildInputs = [ pkgs.cmake ];
-    checkPhase = ''
-      cmake .
-      # utf_8_char fails with python3
-      ctest -E "utf_8_char" .
-    '';
-
-    meta = {
-      homepage = https://editorconfig.org;
-      description = "EditorConfig File Locator and Interpreter for Python";
-      license = stdenv.lib.licenses.psfl;
-    };
-  };
+  EditorConfig = callPackage ../development/python-modules/editorconfig { };
 
   edward = callPackage ../development/python-modules/edward { };
 
