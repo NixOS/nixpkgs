880a0ef24ab7f373cb81ab75c9f23e2ac016dc5e
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 17:49:46 -0400

    pythonPackages.memcached: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5839,23 +5839,7 @@ in {
 
   meinheld = callPackage ../development/python-modules/meinheld { };
 
-  memcached = buildPythonPackage rec {
-    name = "memcached-1.51";
-
-    src = if isPy3k then pkgs.fetchurl {
-      url = "mirror://pypi/p/python3-memcached/python3-${name}.tar.gz";
-      sha256 = "0na8b369q8fivh3y0nvzbvhh3lgvxiyyv9xp93cnkvwfsr8mkgkw";
-    } else pkgs.fetchurl {
-      url = "http://ftp.tummy.com/pub/python-memcached/old-releases/python-${name}.tar.gz";
-      sha256 = "124s98m6hvxj6x90d7aynsjfz878zli771q96ns767r2mbqn7192";
-    };
-
-    meta = {
-      description = "Python API for communicating with the memcached distributed memory object cache daemon";
-      homepage = http://www.tummy.com/Community/software/python-memcached/;
-    };
-  };
-
+  memcached = callPackage ../development/python-modules/memcached { };
 
   memory_profiler = buildPythonPackage rec {
     name = "memory_profiler-${version}";
