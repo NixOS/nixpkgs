728825729dcb564246f6aad8b01989365a78f8c7
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 23:42:53 -0400

    pythonPackages.jsonpointer: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1926,20 +1926,7 @@ in {
 
   jsonpickle = callPackage ../development/python-modules/jsonpickle { };
 
-  jsonpointer = buildPythonPackage rec {
-    name = "jsonpointer-1.9";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/j/jsonpointer/${name}.tar.gz";
-      sha256 = "39403b47a71aa782de6d80db3b78f8a5f68ad8dfc9e674ca3bb5b32c15ec7308";
-    };
-
-    meta = {
-      description = "Resolve JSON Pointers in Python";
-      homepage = "https://github.com/stefankoegl/python-json-pointer";
-      license = stdenv.lib.licenses.bsd2; # "Modified BSD license, says pypi"
-    };
-  };
+  jsonpointer = callPackage ../development/python-modules/jsonpointer { };
 
   jsonrpclib = buildPythonPackage rec {
     name = "jsonrpclib-${version}";
