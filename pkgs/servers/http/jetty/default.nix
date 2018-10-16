43d79c4b9e00b19c35337dcaa6464a7e3c66315c
R. RyanTM <ryantm-bot@ryantm.com>
2018-10-15 20:47:19 -0700

    jetty: 9.4.8.v20171121 -> 9.4.12.v20180830
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/jetty/versions

--- pkgs/servers/http/jetty/default.nix
+++ pkgs/servers/http/jetty/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "jetty-${version}";
-  version = "9.4.8.v20171121";
+  version = "9.4.12.v20180830";
   src = fetchurl {
     url = "https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-distribution/${version}/jetty-distribution-${version}.tar.gz";
     name = "jetty-distribution-${version}.tar.gz";
-    sha256 = "0bvwi70vdk468yqgvgq99lwrpy2y5znrl0b1cr8j6ygmsgnvvmjh";
+    sha256 = "1z498cxvp1llp4ii8g52rhicypay1v2b6mmdmxrj3d82az6biiq3";
   };
 
   phases = [ "unpackPhase" "installPhase" ];
