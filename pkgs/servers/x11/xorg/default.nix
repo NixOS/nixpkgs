b47e13abec9332e1f0258b000255bb56a88265b9
Lengyel Balazs <ikervagyok@gmail.com>
2018-10-16 14:34:52 +0200

    xorg/libinput: 0.28.0 -> 0.28.1

--- pkgs/servers/x11/xorg/default.nix
+++ pkgs/servers/x11/xorg/default.nix
@@ -1760,11 +1760,11 @@ let
   }) // {inherit inputproto xorgserver xproto ;};
 
   xf86inputlibinput = (mkDerivation "xf86inputlibinput" {
-    name = "xf86-input-libinput-0.28.0";
+    name = "xf86-input-libinput-0.28.1";
     builder = ./builder.sh;
     src = fetchurl {
-      url = mirror://xorg/individual/driver/xf86-input-libinput-0.28.0.tar.bz2;
-      sha256 = "189h8vl0005yizwrs4d0sng6j8lwkd3xi1zwqg8qavn2bw34v691";
+      url = mirror://xorg/individual/driver/xf86-input-libinput-0.28.1.tar.bz2;
+      sha256 = "12yr0yki94j2416bfhmkz5jpacffm27jkra89fl7h03c0y749nls";
     };
     nativeBuildInputs = [ pkgconfig ];
     buildInputs = [ inputproto xorgserver xproto ];
