e0d349a5f105048c3588273d2a5203bf197d164f
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-16 13:52:45 -0400

    pythonPackages.django_silk: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2310,38 +2310,7 @@ in {
 
   django_reversion = callPackage ../development/python-modules/django_reversion { };
 
-  django_silk = buildPythonPackage rec {
-    name = "django-silk-${version}";
-    version = "0.5.6";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/d/django-silk/${name}.tar.gz";
-      sha256 = "845abc688738858ce06e993c4b7dbbcfcecf33029e828f143463ff96f9a78947";
-    };
-
-    doCheck = false;
-
-    buildInputs = [ self.mock ];
-
-    propagatedBuildInputs = with self; [
-      django
-      pygments
-      simplejson
-      dateutil
-      requests
-      sqlparse
-      jinja2
-      autopep8
-      pytz
-      pillow
-    ];
-
-    meta = {
-      description = "Silky smooth profiling for the Django Framework";
-      homepage = https://github.com/mtford90/silk;
-      license = licenses.mit;
-    };
-  };
+  django_silk = callPackage ../development/python-modules/django_silk { };
 
   django_taggit = buildPythonPackage rec {
     name = "django-taggit-${version}";
