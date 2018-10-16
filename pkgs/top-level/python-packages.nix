44d21b7e652febaa10d235c9962ae182d6ac23f2
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-16 14:05:02 -0400

    pythonPackages.pillowfight: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2322,23 +2322,7 @@ in {
 
   djmail = callPackage ../development/python-modules/djmail { };
 
-  pillowfight = buildPythonPackage rec {
-    name = "pillowfight-${version}";
-    version = "0.2";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/p/pillowfight/pillowfight-${version}.tar.gz";
-      sha256 = "1mh1nhcjjgv7x134sv0krri59ng8bp2w6cwsxc698rixba9f3g0m";
-    };
-
-    propagatedBuildInputs = with self; [
-      pillow
-    ];
-    meta = with stdenv.lib; {
-      description = "Pillow Fight";
-      homepage = "https://github.com/beanbaginc/pillowfight";
-    };
-  };
+  pillowfight = callPackage ../development/python-modules/pillowfight { };
 
   kaptan = buildPythonPackage rec {
     name = "kaptan-${version}";
