0c9411544729a2ae80f260df043141c154f85d51
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 23:38:16 -0400

    pythonPackages.gpsoauth: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1838,37 +1838,7 @@ in {
   gpapi = callPackage ../development/python-modules/gpapi { };
   gplaycli = callPackage ../development/python-modules/gplaycli { };
 
-  gpsoauth = buildPythonPackage rec {
-    version = "0.2.0";
-    name = "gpsoauth-${version}";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/g/gpsoauth/${name}.tar.gz";
-      sha256 = "01zxw8rhml8xfwda7ba8983890bzwkfa55ijd6qf8qrdy6ja1ncn";
-    };
-
-    propagatedBuildInputs = with self; [
-      cffi
-      cryptography
-      enum34
-      idna
-      ipaddress
-      ndg-httpsclient
-      pyopenssl
-      pyasn1
-      pycparser
-      pycryptodome
-      requests
-      six
-    ];
-
-    meta = {
-      description = "A python client library for Google Play Services OAuth";
-      homepage = "https://github.com/simon-weber/gpsoauth";
-      license = licenses.mit;
-      maintainers = with maintainers; [ jgillich ];
-    };
-  };
+  gpsoauth = callPackage ../development/python-modules/gpsoauth { };
 
   grip = callPackage ../development/python-modules/grip { };
 
