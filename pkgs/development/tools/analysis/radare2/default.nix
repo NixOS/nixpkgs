6fdc1655649076262a93cddd4cb389457a0287e4
Will Dietz <w@wdtz.org>
2018-10-16 12:35:16 -0500

    r2-for-cutter: revert accidental change to version info

--- pkgs/development/tools/analysis/radare2/default.nix
+++ pkgs/development/tools/analysis/radare2/default.nix
@@ -100,11 +100,11 @@ in {
     cs_sha256 = "1czzqj8zdjgh7h2ixi26ij3mm4bgm4xw2slin6fv73nic8yaw722";
   };
   r2-for-cutter = generic {
-    version_commit = "19720";
+    version_commit = "19349";
     gittap = "2.8.0-189-gf82b28982";
     gittip = "f82b289822825e4c7403734f3b95dfd7f5e4f725";
     rev = "f82b289822825e4c7403734f3b95dfd7f5e4f725";
-    version = "2018-08-13";
+    version = "2018-08-14";
     sha256 = "0zc2a09xmwbxphxd1b0ia0zm8323wfcmxwwx6k239681jj9qwgr1";
     cs_tip = "782ea67e17a391ca0d3faafdc365b335a1a8930a";
     cs_sha256 = "1maww4ir78a193pm3f8lr2kdkizi7rywn68ffa65ipyr7j4pl6i4";
