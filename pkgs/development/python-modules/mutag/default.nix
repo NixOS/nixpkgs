ca6fdfedbac6556631e11ed94d1ebf8fdd34a396
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-16 18:40:34 -0400

    pythonPackages.mutag: refactor move to python-modules

---
+++ pkgs/development/python-modules/mutag/default.nix
@@ -0,0 +1,27 @@
+{ stdenv
+, buildPythonPackage
+, fetchgit
+, isPy3k
+, pyparsing
+}:
+
+buildPythonPackage rec {
+  name = "mutag-0.0.2-2ffa0258ca";
+  disabled = ! isPy3k;
+
+  src = fetchgit {
+    url = "https://github.com/aroig/mutag.git";
+    sha256 = "0axdnwdypfd74a9dnw0g25m16xx1yygyl828xy0kpj8gyqdc6gb1";
+    rev = "2ffa0258cadaf79313241f43bf2c1caaf197d9c2";
+  };
+
+  propagatedBuildInputs = [ pyparsing ];
+
+  meta = with stdenv.lib; {
+    homepage = https://github.com/aroig/mutag;
+    description = "A script to change email tags in a mu indexed maildir";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ ];
+  };
+
+}
