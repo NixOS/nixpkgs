6f4332daa4dc6111317fa9af4b3f2c9ac45785a3
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-16 14:54:07 -0400

    pythonPackages.filebrowser_safe: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2380,39 +2380,7 @@ in {
 
   fdroidserver = callPackage ../development/python-modules/fdroidserver { };
 
-  filebrowser_safe = buildPythonPackage rec {
-    version = "0.3.6";
-    name = "filebrowser_safe-${version}";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/f/filebrowser_safe/${name}.tar.gz";
-      sha256 = "02bn60fdslvng2ckn65fms3hjbzgsa8qa5161a8lr720wbx8gpj2";
-    };
-
-    buildInputs = [ self.django ];
-
-    # There is no test embedded
-    doCheck = false;
-
-    meta = {
-      description = "A snapshot of django-filebrowser for the Mezzanine CMS";
-      longDescription = ''
-        filebrowser_safe was created to provide a snapshot of the FileBrowser
-        asset manager for Django, to be referenced as a dependency for the
-        Mezzanine CMS for Django.
-
-        At the time of filebrowser_safe's creation, FileBrowser was incorrectly
-        packaged on PyPI, and had also dropped compatibility with Django 1.1 -
-        filebrowser_safe was therefore created to address these specific
-        issues.
-      '';
-      homepage = https://github.com/stephenmcd/filebrowser-safe;
-      downloadPage = https://pypi.python.org/pypi/filebrowser_safe/;
-      license = licenses.free;
-      maintainers = with maintainers; [ prikhi ];
-      platforms = platforms.linux;
-    };
-  };
+  filebrowser_safe = callPackage ../development/python-modules/filebrowser_safe { };
 
   pycodestyle = callPackage ../development/python-modules/pycodestyle { };
 
