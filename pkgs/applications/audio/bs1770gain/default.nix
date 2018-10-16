543a943a5a1231add880ab63d93c0151f81eeded
Bart Brouns <bart@magnetophon.nl>
2018-10-16 12:11:50 +0200

    bs1770gain: 0.4.12 -> 0.5.0

--- pkgs/applications/audio/bs1770gain/default.nix
+++ pkgs/applications/audio/bs1770gain/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "bs1770gain-${version}";
-  version = "0.4.12";
+  version = "0.5.0";
 
   src = fetchurl {
     url = "mirror://sourceforge/bs1770gain/${name}.tar.gz";
-    sha256 = "0n9skdap1vnl6w52fx0gsrjlk7w3xgdwi62ycyf96h29rx059z6a";
+    sha256 = "0vd7320k7s2zcn2vganclxbr1vav18ghld27rcwskvcc3dm8prii";
   };
 
   buildInputs = [ ffmpeg sox ];
