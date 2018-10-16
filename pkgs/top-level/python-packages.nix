99a5f22de641a5b83644fe8c48bc0ce6a027948d
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-16 13:49:52 -0400

    pythonPackages.django_reversion: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2308,23 +2308,7 @@ in {
 
   django_redis = callPackage ../development/python-modules/django_redis { };
 
-  django_reversion = buildPythonPackage rec {
-    name = "django-reversion-${version}";
-    version = "1.10.1";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/d/django-reversion/${name}.tar.gz";
-      sha256 = "01iv8w6lmmq98qjhxmnp8ddjxifmhxcmp612ijd91wc8nv8lk12w";
-    };
-
-    propagatedBuildInputs = with self; [ django ];
-
-    meta = {
-      description = "An extension to the Django web framework that provides comprehensive version control facilities";
-      homepage = https://github.com/etianen/django-reversion;
-      license = licenses.bsd3;
-    };
-  };
+  django_reversion = callPackage ../development/python-modules/django_reversion { };
 
   django_silk = buildPythonPackage rec {
     name = "django-silk-${version}";
