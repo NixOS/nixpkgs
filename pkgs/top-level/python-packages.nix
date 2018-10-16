238f179d933c2aa208c412063be7ffff637d5677
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-16 11:34:20 -0400

    pythonPackages.pytun: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2127,28 +2127,7 @@ in {
 
   pytools = callPackage ../development/python-modules/pytools { };
 
-  pytun = buildPythonPackage rec {
-    name = "pytun-${version}";
-    version = "2.2.1";
-    rev = "v${version}";
-
-    src = pkgs.fetchFromGitHub {
-      inherit rev;
-      owner = "montag451";
-      repo = "pytun";
-      sha256 = "1bxk0z0v8m0b01xg94f039j3bsclkshb7girvjqfzk5whbd2nryh";
-    };
-
-    doCheck = false;
-
-    meta = {
-      homepage = https://github.com/montag451/pytun;
-      description = "Linux TUN/TAP wrapper for Python";
-      license = licenses.mit;
-      maintainers = with maintainers; [ montag451 ];
-      platforms = platforms.linux;
-    };
-  };
+  pytun = callPackage ../development/python-modules/pytun { };
 
   python-ctags3 = callPackage ../development/python-modules/python-ctags3 { };
 
