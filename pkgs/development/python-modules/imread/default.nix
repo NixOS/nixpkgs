2ab778df60ebe8cfb8ba1cc717262cd20379ebb7
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-15 23:39:44 -0400

    pythonPackages.imread: refactor move to python-modules

---
+++ pkgs/development/python-modules/imread/default.nix
@@ -0,0 +1,30 @@
+{ stdenv
+, buildPythonPackage
+, fetchurl
+, nose
+, pkgs
+, numpy
+}:
+
+buildPythonPackage rec {
+  pname = "python-imread";
+  version = "0.6";
+
+  src = pkgs.fetchurl {
+    url = "https://github.com/luispedro/imread/archive/release-${version}.tar.gz";
+    sha256 = "0i14bc67200zhzxc41g5dfp2m0pr1zaa2gv59p2va1xw0ji2dc0f";
+  };
+
+  nativeBuildInputs = [ pkgs.pkgconfig ];
+  buildInputs = [ nose pkgs.libjpeg pkgs.libpng pkgs.libtiff pkgs.libwebp ];
+  propagatedBuildInputs = [ numpy ];
+
+  meta = with stdenv.lib; {
+    description = "Python package to load images as numpy arrays";
+    homepage = https://imread.readthedocs.io/en/latest/;
+    maintainers = with maintainers; [ luispedro ];
+    license = licenses.mit;
+    platforms = platforms.linux;
+  };
+
+}
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1892,33 +1892,7 @@ in {
 
   immutables = callPackage ../development/python-modules/immutables {};
 
-  imread = buildPythonPackage rec {
-    name = "python-imread-${version}";
-    version = "0.6";
-
-    src = pkgs.fetchurl {
-      url = "https://github.com/luispedro/imread/archive/release-${version}.tar.gz";
-      sha256 = "0i14bc67200zhzxc41g5dfp2m0pr1zaa2gv59p2va1xw0ji2dc0f";
-    };
-
-    nativeBuildInputs = [ pkgs.pkgconfig ];
-    buildInputs = with self; [
-      nose
-      pkgs.libjpeg
-      pkgs.libpng
-      pkgs.libtiff
-      pkgs.libwebp
-    ];
-    propagatedBuildInputs = with self; [ numpy ];
-
-    meta = with stdenv.lib; {
-      description = "Python package to load images as numpy arrays";
-      homepage = https://imread.readthedocs.io/en/latest/;
-      maintainers = with maintainers; [ luispedro ];
-      license = licenses.mit;
-      platforms = platforms.linux;
-    };
-  };
+  imread = callPackage ../development/python-modules/imread { };
 
   imaplib2 = callPackage ../development/python-modules/imaplib2 { };
 
