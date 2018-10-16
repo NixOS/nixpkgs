c1ee82fdd2e3f8639a57e2d5e9b3b9a2a288ad89
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-16 10:55:33 -0400

    pythonPackages.pypopplar: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2089,37 +2089,7 @@ in {
 
   pyphen = callPackage ../development/python-modules/pyphen {};
 
-  pypoppler = buildPythonPackage rec {
-    name = "pypoppler-${version}";
-    version = "0.12.2";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/p/pypoppler/${name}.tar.gz";
-      sha256 = "47e6ac99e5b114b9abf2d1dd1bca06f22c028d025432512989f659142470810f";
-    };
-
-    NIX_CFLAGS_COMPILE="-I${pkgs.poppler.dev}/include/poppler/";
-    nativeBuildInputs = [ pkgs.pkgconfig ];
-    buildInputs = [ pkgs.poppler.dev ];
-    propagatedBuildInputs = with self; [ pycairo pygobject2 ];
-
-    patches = [
-      ../development/python-modules/pypoppler-0.39.0.patch
-      ../development/python-modules/pypoppler-poppler.c.patch
-    ];
-
-    # Not supported.
-    disabled = isPy3k;
-
-    # No tests in archive
-    doCheck = false;
-
-    meta = {
-      homepage = https://code.launchpad.net/~mriedesel/poppler-python/main;
-      description = "Python bindings for poppler-glib, unofficial branch including bug fixes, and removal of gtk dependencies";
-      license = licenses.gpl2;
-    };
-  };
+  pypoppler = callPackage ../development/python-modules/pypoppler { };
 
   pypillowfight = callPackage ../development/python-modules/pypillowfight { };
 
