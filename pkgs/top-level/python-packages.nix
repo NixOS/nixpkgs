f3b50d95ea0b15a58c8bd246b8c82d703d00c27c
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-16 16:48:57 -0400

    pythonPackages.gyp: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2430,35 +2430,7 @@ in {
 
   gspread = callPackage ../development/python-modules/gspread { };
 
-  gyp = buildPythonPackage rec {
-    name = "gyp-${version}";
-    version = "2015-06-11";
-
-    src = pkgs.fetchgit {
-      url = "https://chromium.googlesource.com/external/gyp.git";
-      rev = "fdc7b812f99e48c00e9a487bd56751bbeae07043";
-      sha256 = "1imgxsl4mr1662vsj2mlnpvvrbz71yk00w8p85vi5bkgmc6awgiz";
-    };
-
-    prePatch = optionals pkgs.stdenv.isDarwin ''
-      sed -i 's/raise.*No Xcode or CLT version detected.*/version = "7.0.0"/' pylib/gyp/xcode_emulation.py
-    '';
-
-    patches = optionals pkgs.stdenv.isDarwin [
-      ../development/python-modules/gyp/no-darwin-cflags.patch
-      ../development/python-modules/gyp/no-xcode.patch
-    ];
-
-    disabled = isPy3k;
-
-    meta = {
-      description = "A tool to generate native build files";
-      homepage = https://chromium.googlesource.com/external/gyp/+/master/README.md;
-      license = licenses.bsd3;
-      maintainers = with maintainers; [ codyopel ];
-      platforms = platforms.all;
-    };
-  };
+  gyp = callPackage ../development/python-modules/gyp { };
 
   guessit = callPackage ../development/python-modules/guessit { };
 
