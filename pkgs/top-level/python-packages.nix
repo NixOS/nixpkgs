7771d40c054e99b8fe67ec6325774e2bdd0ce00c
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-16 16:30:17 -0400

    pythonPackages.genzshcomp: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2374,22 +2374,7 @@ in {
 
   gevent-websocket = callPackage ../development/python-modules/gevent-websocket { };
 
-  genzshcomp = buildPythonPackage {
-    name = "genzshcomp-0.5.1";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/g/genzshcomp/genzshcomp-0.5.1.tar.gz";
-      sha256 = "c77d007cc32cdff836ecf8df6192371767976c108a75b055e057bb6f4a09cd42";
-    };
-
-    buildInputs = with self; [ setuptools ];
-
-    meta = {
-      description = "Automatically generated zsh completion function for Python's option parser modules";
-      license = "BSD";
-    };
-  };
-
+  genzshcomp = callPackage ../development/python-modules/genzshcomp { };
 
   gflags = callPackage ../development/python-modules/gflags { };
 
