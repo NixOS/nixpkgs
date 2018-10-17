1ff8adc940e70470b44a4faa1e1166fd4f804ac3
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-17 14:46:29 -0400

    pythonPackages.objgraph: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2986,26 +2986,7 @@ in {
 
   obfsproxy = callPackage ../development/python-modules/obfsproxy { };
 
-  objgraph = buildPythonPackage rec {
-    name = "objgraph-${version}";
-    version = "2.0.1";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/o/objgraph/${name}.tar.gz";
-      sha256 = "841de52715774ec1d0e97d9b4462d6e3e10406155f9b61f54ba7db984c45442a";
-    };
-
-    # Tests fail with PyPy.
-    disabled = isPyPy;
-
-    propagatedBuildInputs = with self; [pkgs.graphviz];
-
-    meta = {
-      description = "Draws Python object reference graphs with graphviz";
-      homepage = https://mg.pov.lt/objgraph/;
-      license = licenses.mit;
-    };
-  };
+  objgraph = callPackage ../development/python-modules/objgraph { };
 
   odo = callPackage ../development/python-modules/odo { };
 
