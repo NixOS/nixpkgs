eee4d776e9a987b637711fd70252876a674c5667
Andy Chun @noneucat <andy@lolc.at>
2018-10-17 08:26:19 -0700

    Explain why we're not building from source

--- pkgs/applications/misc/polar-bookshelf/default.nix
+++ pkgs/applications/misc/polar-bookshelf/default.nix
@@ -12,6 +12,7 @@ stdenv.mkDerivation rec {
   name = "polar-bookshelf-${version}";
   version = "1.0.2";
 
+  # fetching a .deb because there's no easy way to package this Electron app
   src = fetchurl {
     url = "https://github.com/burtonator/polar-bookshelf/releases/download/${version}/polar-bookshelf-${version}-amd64.deb";
     sha256 = "0fz34qvd7zp5rd5rqljwhq8ya9x29cq5h8axb82z369nkbi1xpbs";
