d9d93f8e14c8459406fbdadb9dad0f5403489256
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-17 14:22:05 -0400

    pythonPackages.ntfy: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2946,25 +2946,7 @@ in {
 
   emoji = callPackage ../development/python-modules/emoji { };
 
-  ntfy = buildPythonPackage rec {
-    version = "1.2.0";
-    name = "ntfy-${version}";
-    src = pkgs.fetchFromGitHub {
-      owner = "dschep";
-      repo = "ntfy";
-      rev = "v${version}";
-      sha256 = "0yjxwisxpxy3vpnqk9nw5k3db3xx6wyf6sk1px9m94s30glcq2cc";
-    };
-
-    propagatedBuildInputs = with self; [ appdirs pyyaml requests dbus-python emoji sleekxmpp mock ];
-
-    meta = {
-      description = "A utility for sending notifications, on demand and when commands finish";
-      homepage = http://ntfy.rtfd.org/;
-      license = licenses.gpl3;
-      maintainers = with maintainers; [ kamilchm ];
-    };
-  };
+  ntfy = callPackage ../development/python-modules/ntfy { };
 
   ntplib = buildPythonPackage rec {
     name = "ntplib-0.3.3";
