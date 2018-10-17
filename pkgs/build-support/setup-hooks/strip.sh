606eebd214c5dfab1a24a059334a25d94d322798
Peter Hoeg <peter@hoeg.com>
2018-10-17 07:30:51 +0000

    strip: strip all in libexec, bin, sbin

--- pkgs/build-support/setup-hooks/strip.sh
+++ pkgs/build-support/setup-hooks/strip.sh
@@ -23,12 +23,12 @@ _doStrip() {
         if [[ "$dontStrip" || "$flag" ]] || ! type -f "$stripCmd" 2>/dev/null
         then continue; fi
 
-        stripDebugList=${stripDebugList:-lib lib32 lib64 libexec bin sbin}
+        stripDebugList=${stripDebugList:-lib lib32 lib64}
         if [ -n "$stripDebugList" ]; then
             stripDirs "$stripCmd" "$stripDebugList" "${stripDebugFlags:--S}"
         fi
 
-        stripAllList=${stripAllList:-}
+        stripAllList=${stripAllList:-libexec bin sbin}
         if [ -n "$stripAllList" ]; then
             stripDirs "$stripCmd" "$stripAllList" "${stripAllFlags:--s}"
         fi
