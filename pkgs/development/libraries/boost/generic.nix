007faf02cadc9c88b1c04bbe1efaa601fa3a061b
Matthew Bauer <mjbauer95@gmail.com>
2018-10-16 22:03:09 -0500

    boost: use correct platform
    "platforms.all" could include any possible os (even a machine with no
    OS at all!). We can’t possible hope to support all of that, so need to
    be more specific.

--- pkgs/development/libraries/boost/generic.nix
+++ pkgs/development/libraries/boost/generic.nix
@@ -109,7 +109,7 @@ stdenv.mkDerivation {
     description = "Collection of C++ libraries";
     license = stdenv.lib.licenses.boost;
 
-    platforms = (if versionOlder version "1.59" then remove "aarch64-linux" else id) platforms.all;
+    platforms = (if versionOlder version "1.59" then remove "aarch64-linux" else id) (platforms.unix ++ platforms.windows);
     maintainers = with maintainers; [ peti wkennington ];
   };
 
