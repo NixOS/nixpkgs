be09209b3ccd351c0411b057d4f8a72346949a65
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-17 14:24:24 -0400

    pythonPackages.ntplib: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2948,21 +2948,7 @@ in {
 
   ntfy = callPackage ../development/python-modules/ntfy { };
 
-  ntplib = buildPythonPackage rec {
-    name = "ntplib-0.3.3";
-    src = pkgs.fetchurl {
-      url = mirror://pypi/n/ntplib/ntplib-0.3.3.tar.gz;
-      sha256 = "c4621b64d50be9461d9bd9a71ba0b4af06fbbf818bbd483752d95c1a4e273ede";
-    };
-
-    # Require networking
-    doCheck = false;
-
-    meta = {
-      description = "Python NTP library";
-      license = licenses.mit;
-    };
-  };
+  ntplib = callPackage ../development/python-modules/ntplib { };
 
   numba = callPackage ../development/python-modules/numba { };
 
