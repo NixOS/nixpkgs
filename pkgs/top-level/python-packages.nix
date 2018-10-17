9b1292552189f42d20435aff0a17e1641b9e1eea
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-17 00:27:15 -0400

    pythonPackages.pygal: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2873,28 +2873,7 @@ in {
 
   pint = callPackage ../development/python-modules/pint { };
 
-  pygal = buildPythonPackage rec {
-    pname = "pygal";
-    version = "2.3.1";
-    name = "${pname}-${version}";
-
-    doCheck = !isPyPy;  # one check fails with pypy
-
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "7ba5a191233d0c2d8bf4b4d26b06e42bd77483a59ba7d3e5b884d81d1a870667";
-    };
-
-    buildInputs = with self; [ flask pyquery pytest ];
-    propagatedBuildInputs = with self; [ cairosvg tinycss cssselect ] ++ optionals (!isPyPy) [ lxml ];
-
-    meta = {
-      description = "Sexy and simple python charting";
-      homepage = http://www.pygal.org;
-      license = licenses.lgpl3;
-      maintainers = with maintainers; [ sjourdois ];
-    };
-  };
+  pygal = callPackage ../development/python-modules/pygal { };
 
   pytaglib = callPackage ../development/python-modules/pytaglib { };
 
