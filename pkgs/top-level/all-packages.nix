9779809e0509f4e31280c986cf8c0dbc6f832737
Milan Svoboda <milan.svoboda@centrum.cz>
2018-10-18 22:42:17 +0200

    wings: fix runtime failure. (#48681)
    Erlang/OTP 18 [erts-7.3.1.4] [source] [64-bit] [async-threads:10] [hipe] [kernel-poll:false]
    
    Eshell V7.3.1.4  (abort with ^G)
    1>
    =ERROR REPORT==== 18-Oct-2018::21:29:53 ===
    beam/beam_load.c(1189): Error loading module sdl:
      mandatory chunk of type 'Atom' not found
    
    =ERROR REPORT==== 18-Oct-2018::21:29:53 ===
    Loading of /nix/store/qalvdrzjqqm3a8nsavjbhfiv1pzhw82k-esdl-1.3.1/lib/erlang/lib/esdl-1.3.1/ebin/sdl.beam failed: badfile
    
    =ERROR REPORT==== 18-Oct-2018::21:29:53 ===
    Error in process <0.35.0> with exit value:
    {undef,[{sdl,init,[1048608],[]},
            {wings_init,init,0,[{file,"wings_init.erl"},{line,28}]},
            {wings,init,1,[{file,"wings.erl"},{line,105}]}]}
    
    Fatal internal error - log written to /home/milan/wings_crash.dump
    
    (cherry picked from commit 43fc3bbb62ec90aec5e7929aba3e11290a520bfe)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19206,6 +19206,7 @@ with pkgs;
   winswitch = callPackage ../tools/X11/winswitch { };
 
   wings = callPackage ../applications/graphics/wings {
+    esdl = esdl.override { erlang = erlangR18; };
     erlang = erlangR18;
   };
 
