c6b09766aa0fe8c0f4c287e3ee207545ca72cb72
Franz Pletz <fpletz@fnordicwalking.de>
2018-10-19 23:26:36 +0200

    minishift: init at 1.25.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17726,6 +17726,8 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) vmnet;
   };
 
+  minishift = callPackage ../applications/networking/cluster/minishift { };
+
   minitube = libsForQt5.callPackage ../applications/video/minitube { };
 
   mimic = callPackage ../applications/audio/mimic {
