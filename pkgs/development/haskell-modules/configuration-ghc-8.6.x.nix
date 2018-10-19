488e41e8df72f9a39d9fd8aec09c550379545377
Peter Simons <simons@cryp.to>
2018-10-19 11:55:03 +0200

    haskell-hledger-ui: jailbreak to fix build with ghc-8.6.1

--- pkgs/development/haskell-modules/configuration-ghc-8.6.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.6.x.nix
@@ -54,6 +54,7 @@ self: super: {
   haddock-library = dontCheck super.haddock-library_1_7_0;
   hledger = doJailbreak super.hledger;
   hledger-lib = doJailbreak super.hledger-lib;
+  hledger-ui = doJailbreak super.hledger-ui;
   hpack = self.hpack_0_31_0;
   hslua = self.hslua_1_0_1;
   hslua-module-text = self.hslua-module-text_0_2_0;
