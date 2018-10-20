50b96cea5f2c905b8d2d993b9d1f090e59ede558
Pierre Bourdon <delroth@gmail.com>
2018-10-20 04:40:11 +0200

    nixos/prometheus: add webExternalUrl option
    Similar to the prometheus.alertmanager.webExternalUrl option, but for
    Prometheus itself.

--- nixos/modules/services/monitoring/prometheus/default.nix
+++ nixos/modules/services/monitoring/prometheus/default.nix
@@ -39,6 +39,7 @@ let
     "-alertmanager.notification-queue-capacity=${toString cfg.alertmanagerNotificationQueueCapacity}"
     "-alertmanager.timeout=${toString cfg.alertmanagerTimeout}s"
     (optionalString (cfg.alertmanagerURL != []) "-alertmanager.url=${concatStringsSep "," cfg.alertmanagerURL}")
+    (optionalString (cfg.webExternalUrl != null) "-web.external-url=${cfg.webExternalUrl}")
   ];
 
   promTypes.globalConfig = types.submodule {
@@ -465,6 +466,16 @@ in {
         default = 10;
         description = ''
           Alert manager HTTP API timeout (in seconds).
+        '';
+      };
+
+      webExternalUrl = mkOption {
+        type = types.nullOr types.str;
+        default = null;
+        example = "https://example.com/";
+        description = ''
+          The URL under which Prometheus is externally reachable (for example,
+          if Prometheus is served via a reverse proxy).
         '';
       };
     };
