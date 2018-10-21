3c1395073364310c9cdf4f5357740e285e47d193
José Romildo Malaquias <malaquias@gmail.com>
2018-10-21 19:48:44 -0300

    go-lib: 1.2.16.1 -> 1.2.16.3

--- pkgs/desktops/deepin/go-lib/default.nix
+++ pkgs/desktops/deepin/go-lib/default.nix
@@ -5,13 +5,13 @@
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
   pname = "go-lib";
-  version = "1.2.16.1";
+  version = "1.2.16.3";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "0nl35dm0bdca38qhnzdpsv6b0vds9ccvm4c86rs42a7c6v655b1q";
+    sha256 = "0dk6k53in3ffwwvkr0sazfk83rf4fyc6rvb6k8fi2n3qj4gp8xd2";
   };
 
   buildInputs = [
