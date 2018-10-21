b872b2a41cb56375ac444725f424da3c86659864
Frederik Rietdijk <fridh@fridh.nl>
2018-10-21 09:30:05 +0200

    python.pkgs.pycodestyle: fix building with python37

--- pkgs/development/python-modules/pycodestyle/default.nix
+++ pkgs/development/python-modules/pycodestyle/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib, buildPythonPackage, fetchPypi, fetchpatch }:
 
 buildPythonPackage rec {
   pname = "pycodestyle";
@@ -8,6 +8,14 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "cbfca99bd594a10f674d0cd97a3d802a1fdef635d4361e1a2658de47ed261e3a";
   };
+
+  patches = [
+    # https://github.com/PyCQA/pycodestyle/pull/801
+    (fetchpatch {
+      url = https://github.com/PyCQA/pycodestyle/commit/397463014fda3cdefe8d6c9d117ae16d878dc494.patch;
+      sha256 = "01zask2y2gim5il9lcmlhr2qaadv9v7kaw1y619l8xbjhpbq2zh8";
+    })
+  ];
 
   meta = with lib; {
     description = "Python style guide checker (formerly called pep8)";
