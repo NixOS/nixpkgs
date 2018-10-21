8e1a4f31ff43599042209d371d72a1a6cba41dc0
Andy Dirnberger <andy@dirnberger.me>
2018-10-20 22:13:41 -0400

    python37: 3.7.0 -> 3.7.1

--- pkgs/development/interpreters/python/cpython/3.7/default.nix
+++ pkgs/development/interpreters/python/cpython/3.7/default.nix
@@ -26,7 +26,7 @@ with stdenv.lib;
 
 let
   majorVersion = "3.7";
-  minorVersion = "0";
+  minorVersion = "1";
   minorVersionSuffix = "";
   version = "${majorVersion}.${minorVersion}${minorVersionSuffix}";
   libPrefix = "python${majorVersion}";
@@ -48,7 +48,7 @@ in stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://www.python.org/ftp/python/${majorVersion}.${minorVersion}/Python-${version}.tar.xz";
-    sha256 = "0j9mic5c9lbd2b20wka7hily7szz740wy9ilfrczxap63rnrk0h3";
+    sha256 = "fa7e2b8e8c9402f192ad56dc4f814089d1c4466c97d780f5e5acc02c04243d6d";
   };
 
   NIX_LDFLAGS = optionalString stdenv.isLinux "-lgcc_s";
