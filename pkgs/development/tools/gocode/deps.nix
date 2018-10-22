fae75e31bf02a547ff8a5154e956b5021f91f396
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-10-22 11:15:34 -0700

    gocode: 2018-07-27 -> 2018-10-22
    github.com/nsf/gocode has also been deprecated in favor of github.com/mdempsky/gocode

---
+++ pkgs/development/tools/gocode/deps.nix
@@ -0,0 +1,11 @@
+[
+  {
+    goPackagePath = "golang.org/x/tools";
+    fetch = {
+      type = "git";
+      url = "https://go.googlesource.com/tools";
+      rev = "6fe81c087942f588f40c3f67b41ce284f2f70eee";
+      sha256 = "04yl7rk2lf94bxz74ja5snh7ava9gcnf2yx6y002pfkk538r6w5d";
+    };
+  }
+]
