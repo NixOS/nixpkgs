02b5b6300236d348c8851449372d299d6c11de09
Patrick Hilhorst <git@hilhorst.be>
2018-10-23 19:18:40 +0200

    sway-beta: split out to separate file

--- pkgs/applications/window-managers/sway/beta.nix
+++ pkgs/applications/window-managers/sway/beta.nix
@@ -1,40 +1,43 @@
 { stdenv, fetchFromGitHub
-, cmake, pkgconfig, asciidoc, libxslt, docbook_xsl
+, meson, ninja
+, pkgconfig, asciidoc, libxslt, docbook_xsl
 , wayland, wlc, libxkbcommon, pcre, json_c, dbus
 , pango, cairo, libinput, libcap, pam, gdk_pixbuf, libpthreadstubs
-, libXdmcp
+, libXdmcp, wlroots, wayland-protocols
 , buildDocs ? true
 }:
 
 stdenv.mkDerivation rec {
-  name = "sway-${version}";
-  version = "0.15.2";
+  name = "${pname}-${version}";
+  pname = "sway-beta";
+  version = "1.0-beta.1";
 
   src = fetchFromGitHub {
     owner = "swaywm";
     repo = "sway";
     rev = version;
-    sha256 = "1p9j5gv85lsgj4z28qja07dqyvqk41w6mlaflvvm9yxafx477g5n";
+    sha256 = "0h9kgrg9mh2acks63z72bw3lwff32pf2nb4i7i5xhd9i6l4gfnqa";
   };
 
   nativeBuildInputs = [
-    cmake pkgconfig
+    pkgconfig meson ninja
   ] ++ stdenv.lib.optional buildDocs [ asciidoc libxslt docbook_xsl ];
+
   buildInputs = [
     wayland wlc libxkbcommon pcre json_c dbus
     pango cairo libinput libcap pam gdk_pixbuf libpthreadstubs
-    libXdmcp
+    libXdmcp wlroots wayland-protocols
   ];
 
   enableParallelBuilding = true;
 
-  cmakeFlags = "-DVERSION=${version} -DLD_LIBRARY_PATH=/run/opengl-driver/lib:/run/opengl-driver-32/lib";
+  mesonFlags = "-Dsway-version=${version}";
 
   meta = with stdenv.lib; {
     description = "i3-compatible window manager for Wayland";
     homepage    = http://swaywm.org;
     license     = licenses.mit;
     platforms   = platforms.linux;
-    maintainers = with maintainers; [ primeos ]; # Trying to keep it up-to-date.
+    maintainers = with maintainers; [ primeos synthetica ]; # Trying to keep it up-to-date.
   };
 }
