ab27bcf01e1686ee449ec63c26595dee289942de
Vincent Demeester <vincent@sbr.pm>
2018-10-23 10:37:56 +0200

    buildah: 1.1 -> 1.4
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkgs/development/tools/buildah/default.nix
+++ pkgs/development/tools/buildah/default.nix
@@ -3,15 +3,15 @@
 , go-md2man }:
 
 let
-  version = "1.1";
+  version = "1.4";
 
   src = fetchFromGitHub {
     rev = "v${version}";
-    owner = "projectatomic";
+    owner = "containers";
     repo = "buildah";
-    sha256 = "0pc7bzcaafrz56glygzhnbilgaz4ca2kmklw8njfgamffbw4d54p";
+    sha256 = "0b9pfi22qcqyp0im8vp02ibrwd49ghgi64d5l98z01cj52n2j2dz";
   };
-  goPackagePath = "github.com/projectatomic/buildah";
+  goPackagePath = "github.com/containers/buildah";
 
 in buildGoPackage rec {
   name = "buildah-${version}";
@@ -45,7 +45,7 @@ in buildGoPackage rec {
 
   meta = {
     description = "A tool which facilitates building OCI images";
-    homepage = https://github.com/projectatomic/buildah;
+    homepage = https://github.com/containers/buildah;
     maintainers = with stdenv.lib.maintainers; [ Profpatsch ];
     license = stdenv.lib.licenses.asl20;
   };
