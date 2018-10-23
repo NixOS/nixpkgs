26ac0b8120a154c9a36fcfc67e9d85fdf7851973
Graham Christensen <graham@grahamc.com>
2018-10-22 20:48:20 -0400

    fixup line lengths, add output diffs

--- doc/functions/library/test.py
+++ doc/functions/library/test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from pprint import pprint
+import pprint
 import xml.etree.ElementTree as ET
 import subprocess
 import json
@@ -10,12 +10,21 @@ import itertools
 import sys
 import shlex
 import argparse
+import difflib
 
-def debug(msg):
-    print(msg, file=sys.stderr)
+def debug(msg, *args, **kwargs):
+    print(msg.format(*args, **kwargs), file=sys.stderr)
+
+
+def diff(a, b):
+    return '\n'.join(
+        difflib.ndiff(pprint.pformat(a).splitlines(),
+                      pprint.pformat(b).splitlines()))
+
 
 def indent(string, by):
     return "\n".join(["{}{}".format(by, line) for line in string.split("\n")])
+
 
 def find_examples(tree):
     ns = {
@@ -30,20 +39,30 @@ def find_examples(tree):
             debug('Skipping <example> with no <title> or xml:id')
             continue
         elif xml_id is None:
-            debug('Skipping <example> with title "{}" because it has no xml:id'.format(title.text))
+            debug(
+                'Skipping <example> with title "{}" because it has no xml:id',
+                title.text
+            )
             continue
 
         code = example.find('.//docbook:programlisting[@role="input"]', ns)
         if code is None:
-            debug('Skipping example {}: no <programlisting role="input">'.format(xml_id))
+            debug(
+                'Skipping example {}: no <programlisting role="input">',
+                xml_id
+            )
             continue
 
         expect = example.find('.//docbook:programlisting[@role="output"]', ns)
         if expect is None:
-            debug('Skipping example {}: no <programlisting role="output">'.format(xml_id))
+            debug(
+                'Skipping example {}: no <programlisting role="output">',
+                xml_id
+            )
             continue
 
-        context = example.find('.//docbook:programlisting[@role="context"]', ns)
+        context = example.find('.//docbook:programlisting[@role="context"]',
+                               ns)
         if context is None:
             context = '{}'
         else:
@@ -56,42 +75,57 @@ def find_examples(tree):
             "expect": expect.text,
         }
 
+
 def print_success(example):
     print("{}: pass".format(example['id']))
 
+
 def print_failure(example, full_expression, result):
-    print("--------  Doc test failure  --------")
-    print("ID: {}".format(example['id']))
-    print("Executing:")
-    print("")
-    print(full_expression)
-    print("")
-    print("Expecting:")
-    print(indent(example['expect'], "  "))
-    print("Found:")
-    print(indent(result['actual'], "  "))
-    print("----")
+    print(f"""
+--------  Doc test failure  --------
+ID: {example['id']}
+Executing:
+
+{full_expression}
+
+Expecting:
+
+{indent(example['expect'], "  ")}
+
+Diff:
+
+{indent(diff(result['actual'], result['expect']), "  ")}
+----
+""")
+
 
 def print_execution_failure(example, full_expression, exception):
-    print("--------  Doc test execution failure  --------")
-    print("ID: {}".format(example['id']))
-    print("Evaluating the Nix expression:")
-    print("")
-    print(full_expression)
-    print("")
-    print("with the following command failed with exit code {}:".format(exception.returncode))
-    print("")
-    print(indent(" ".join([shlex.quote(part) for part in exception.cmd]), "    "))
-    print("")
-    print("and the command printed:")
-    print(indent(exception.output.decode("utf-8"), "  "))
+    print(f"""
+--------  Doc test execution failure  --------
+ID: {example['id']}
+Evaluating the Nix expression:
+
+{full_expression}
+
+with the following command failed with exit code {exception.returncode}
+
+{indent(" ".join([shlex.quote(part) for part in exception.cmd]), "    ")}
+
+and the command printed:
+
+{indent(exception.output.decode("utf-8"), "  ")}
+""")
+
 
 def main():
-    parser = argparse.ArgumentParser(description='Execute doc tests for the Nixpkgs manual..')
-    default_lib_dir = os.path.realpath(os.path.dirname(os.path.dirname(os.path.realpath(__file__)) + "/../../../lib/"))
-    parser.add_argument('--lib-dir', dest='library_dir', type=os.path.realpath,
-                        default=default_lib_dir,
-                        help='Source of the library to test against (default: {})'.format(default_lib_dir))
+    parser = argparse.ArgumentParser(
+        description='Execute doc tests for the Nixpkgs manual..')
+    default_lib_dir = os.path.realpath(os.path.dirname(
+        os.path.dirname(os.path.realpath(__file__)) + "/../../../lib/"))
+    parser.add_argument(
+        '--lib-dir', dest='library_dir', type=os.path.realpath,
+        default=default_lib_dir,
+        help='Library to test against (default: {})'.format(default_lib_dir))
     parser.add_argument('ids', metavar='id', type=str, nargs='*',
                         help='an example ID to test')
 
@@ -133,10 +167,11 @@ def main():
                    indent(example['expect'], "              "))
 
         try:
-            results = subprocess.check_output(["nix-instantiate", "--eval", "--json",
-                                    "--strict", "-E", full_expression],
-                                    stderr=subprocess.STDOUT
-                                    )
+            results = subprocess.check_output(
+                ["nix-instantiate", "--eval", "--json", "--strict",
+                 "-E", full_expression],
+                stderr=subprocess.STDOUT
+            )
         except subprocess.CalledProcessError as e:
             print_execution_failure(example, full_expression, e)
             failed.append(example['id'])
@@ -160,6 +195,7 @@ def main():
             shlex.quote(args.library_dir),
             " ".join([shlex.quote(id) for id in failed])))
         exit(1)
+
 
 if __name__ == "__main__":
     main()
