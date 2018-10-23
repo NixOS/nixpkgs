13649b9eee16174f2e90369cfe68b017ea1d1014
Rob Vermaas <rob.vermaas@gmail.com>
2018-10-23 18:15:05 +0200

    opentracing-cpp: fix version in derivation
    (cherry picked from commit df29219f5118dd1ae1d02108aeea4484e20d67e8)

--- pkgs/development/libraries/opentracing-cpp/default.nix
+++ pkgs/development/libraries/opentracing-cpp/default.nix
@@ -1,10 +1,11 @@
 { stdenv, fetchFromGitHub, cmake }:
-stdenv.mkDerivation {
-  name = "opentracing-cpp";
+stdenv.mkDerivation rec {
+  name = "opentracing-cpp-${version}";
+  version = "1.5.0";
   src = fetchFromGitHub {
     owner = "opentracing";
     repo = "opentracing-cpp";
-    rev = "v1.5.0";
+    rev = "v${version}";
     sha256 = "09hxj59vvz1ncbx4iblgfc3b5i74hvb3vx5245bwwwfkx5cnj1gg";
   };
   buildInputs = [ cmake ];
