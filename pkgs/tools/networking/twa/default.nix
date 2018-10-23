7dd1ea7b56011e595b729c57adc695ea4d1f122a
R. RyanTM <ryantm-bot@ryantm.com>
2018-10-23 10:01:29 -0700

    twa: 1.5.1 -> 1.6.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/twa/versions

--- pkgs/tools/networking/twa/default.nix
+++ pkgs/tools/networking/twa/default.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation rec {
   name = "twa-${version}";
-  version = "1.5.1";
+  version = "1.6.0";
 
   src = fetchFromGitHub {
     owner = "trailofbits";
     repo = "twa";
     rev = version;
-    sha256 = "14pwiq1kza92w2aq358zh5hrxpxpfhg31am03b56g6vlvqzsvib7";
+    sha256 = "19jn4wp4f0vp2bxvrbyhjrwqgbphr5y1l17qawy8vdv3yswbyw46";
   };
 
   dontBuild = true;
