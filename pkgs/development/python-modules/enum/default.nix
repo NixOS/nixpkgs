6615b769961a1ff4413f0d9c64d325216184dec7
R. RyanTM <ryantm-bot@ryantm.com>
2018-10-24 06:16:45 -0700

    python27Packages.enum: 0.4.4 -> 0.4.7
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python2.7-enum/versions

--- pkgs/development/python-modules/enum/default.nix
+++ pkgs/development/python-modules/enum/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "enum";
-  version = "0.4.4";
+  version = "0.4.7";
   disabled = isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "9bdfacf543baf2350df7613eb37f598a802f346985ca0dc1548be6494140fdff";
+    sha256 = "001iq0yqs9f5bslvl793bhkcs71k5km9kv8yrj5h0lfsgrcg6z4c";
   };
 
   doCheck = !isPyPy;
