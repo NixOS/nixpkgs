2e92a416f5aa8a3b26905269aed91add3031e269
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-25 14:29:02 -0400

    pythonPackages.rpmfluff: 0.5.3 -> 0.5.5

--- pkgs/development/python-modules/rpmfluff/default.nix
+++ pkgs/development/python-modules/rpmfluff/default.nix
@@ -1,14 +1,20 @@
-{ stdenv, buildPythonPackage, fetchurl }:
+{ stdenv
+, buildPythonPackage
+, fetchurl
+, glibcLocales
+}:
 
 buildPythonPackage rec {
   pname = "rpmfluff";
-  version = "0.5.3";
-  name  = "${pname}-${version}";
+  version = "0.5.5";
 
   src = fetchurl {
-    url = "https://releases.pagure.org/${pname}/${name}.tar.xz";
-    sha256 = "1i45f012ngpxs83m3dpmaj3hs8z7r9sbf05vnvzgs3hpgsbhxa7r";
+  url = "https://releases.pagure.org/${pname}/${pname}-${version}.tar.xz";
+    sha256 = "0m92ihii8fgdyma9vn3s6fhq0px8n930c27zs554la0mm4548ss3";
   };
+
+  LC_ALL="en_US.utf-8";
+  buildInputs = [ glibcLocales ];
 
   meta = with stdenv.lib; {
     description = "lightweight way of building RPMs, and sabotaging them";
