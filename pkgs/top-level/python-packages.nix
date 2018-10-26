b0bc759a464bd5121d7df8e44498abb3e6f6f304
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-26 09:38:06 -0400

    pythonPackages.mmpython: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3358,25 +3358,7 @@ in {
 
   pyx = callPackage ../development/python-modules/pyx { };
 
-  mmpython = buildPythonPackage rec {
-    version = "0.4.10";
-    name = "mmpython-${version}";
-
-    src = pkgs.fetchurl {
-      url = http://sourceforge.net/projects/mmpython/files/latest/download;
-      sha256 = "1b7qfad3shgakj37gcj1b9h78j1hxlz6wp9k7h76pb4sq4bfyihy";
-      name = "${name}.tar.gz";
-    };
-
-    disabled = isPyPy || isPy3k;
-
-    meta = {
-      description = "Media Meta Data retrieval framework";
-      homepage = https://sourceforge.net/projects/mmpython/;
-      license = licenses.gpl2;
-      maintainers = with maintainers; [ ];
-    };
-  };
+  mmpython = callPackage ../development/python-modules/mmpython { };
 
   kaa-base = buildPythonPackage rec {
     version = "0.99.2dev-384-2b73caca";
