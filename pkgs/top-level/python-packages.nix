bb8ea04dc92d36a8b82b440ea146343ee1a646a8
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-26 11:58:32 -0400

    pythonPackages.rencode: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3512,25 +3512,7 @@ in {
 
   rbtools = callPackage ../development/python-modules/rbtools { };
 
-  rencode = buildPythonPackage rec {
-    name = "rencode-${version}";
-    version = "git20150810";
-    disabled = isPy33;
-
-    src = pkgs.fetchgit {
-      url = https://github.com/aresch/rencode;
-      rev = "b45e04abdca0dea36e383a8199783269f186c99e";
-      sha256 = "b4bd82852d4220e8a9493d3cfaecbc57b1325708a2d48c0f8acf262edb10dc40";
-    };
-
-    buildInputs = with self; [ cython ];
-
-    meta = {
-      homepage = https://github.com/aresch/rencode;
-      description = "Fast (basic) object serialization similar to bencode";
-      license = licenses.gpl3;
-    };
-  };
+  rencode = callPackage ../development/python-modules/rencode { };
 
   reportlab = callPackage ../development/python-modules/reportlab { };
 
