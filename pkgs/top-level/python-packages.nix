d4315f961fdd2f43a0d69aec9408a1af980fd27a
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-26 13:18:31 -0400

    pythonPackages.simplebayes: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3698,34 +3698,7 @@ in {
 
   simanneal = callPackage ../development/python-modules/simanneal { };
 
-  simplebayes = buildPythonPackage rec {
-    name = "simplebayes-${version}";
-    version = "1.5.8";
-
-    # Use GitHub instead of pypi, because it contains tests.
-    src = pkgs.fetchFromGitHub {
-      repo = "simplebayes";
-      owner = "hickeroar";
-      # NOTE: This is actually 1.5.8 but the tag is wrong!
-      rev = "1.5.7";
-      sha256 = "0mp7rvfdmpfxnka4czw3lv5kkh6gdxh6dm4r6hcln1zzfg9lxp4h";
-    };
-
-    checkInputs = [ self.nose self.mock ];
-
-    postPatch = optionalString isPy3k ''
-      sed -i -e 's/open *(\([^)]*\))/open(\1, encoding="utf-8")/' setup.py
-    '';
-
-    checkPhase = "nosetests tests/test.py";
-
-    meta = {
-      description = "Memory-based naive bayesian text classifier";
-      homepage = "https://github.com/hickeroar/simplebayes";
-      license = licenses.mit;
-      platforms = platforms.all;
-    };
-  };
+  simplebayes = callPackage ../development/python-modules/simplebayes { };
 
   simplegeneric = callPackage ../development/python-modules/simplegeneric { };
 
