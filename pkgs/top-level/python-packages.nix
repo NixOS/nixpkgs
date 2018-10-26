7069d5ebe5d59a031d9eff9a47b98fe801a03add
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-26 10:08:15 -0400

    pythonPackages.pyparted: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3386,48 +3386,7 @@ in {
 
   pyparsing = callPackage ../development/python-modules/pyparsing { };
 
-  pyparted = buildPythonPackage rec {
-    name = "pyparted-${version}";
-    version = "3.10.7";
-    disabled = isPyPy;
-
-    src = pkgs.fetchurl {
-      url = "https://github.com/rhinstaller/pyparted/archive/v${version}.tar.gz";
-      sha256 = "0c9ljrdggwawd8wdzqqqzrna9prrlpj6xs59b0vkxzip0jkf652r";
-    };
-
-    postPatch = ''
-      sed -i -e 's|mke2fs|${pkgs.e2fsprogs}/bin/mke2fs|' tests/baseclass.py
-      sed -i -e '
-        s|e\.path\.startswith("/tmp/temp-device-")|"temp-device-" in e.path|
-      ' tests/test__ped_ped.py
-    '' + optionalString stdenv.isi686 ''
-      # remove some integers in this test case which overflow on 32bit systems
-      sed -i -r -e '/class *UnitGetSizeTestCase/,/^$/{/[0-9]{11}/d}' \
-        tests/test__ped_ped.py
-    '';
-
-    preConfigure = ''
-      PATH="${pkgs.parted}/sbin:$PATH"
-    '';
-
-    nativeBuildInputs = [ pkgs.pkgconfig ];
-
-    propagatedBuildInputs = with self; [ pkgs.parted ];
-
-    checkPhase = ''
-      patchShebangs Makefile
-      make test PYTHON=${python.executable}
-    '';
-
-    meta = {
-      homepage = "https://fedorahosted.org/pyparted/";
-      description = "Python interface for libparted";
-      license = licenses.gpl2Plus;
-      platforms = platforms.linux;
-    };
-  };
-
+  pyparted = callPackage ../development/python-modules/pyparted { };
 
   pyptlib = buildPythonPackage (rec {
     name = "pyptlib-${version}";
