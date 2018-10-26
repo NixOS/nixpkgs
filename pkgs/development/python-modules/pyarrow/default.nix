ef35815e9303cb0c36bdc170edfc4bdfaa7d0065
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2018-10-25 22:14:39 -0400

    use pytest

--- pkgs/development/python-modules/pyarrow/default.nix
+++ pkgs/development/python-modules/pyarrow/default.nix
@@ -29,17 +29,6 @@ buildPythonPackage rec {
 
     # fix the hardcoded value
     substituteInPlace cmake_modules/FindParquet.cmake --replace 'set(PARQUET_ABI_VERSION "1.0.0")' 'set(PARQUET_ABI_VERSION "${_parquet-cpp.version}")'
-
-cp pyarrow/__init__.py pyarrow/__init__.py.bak
-cat >pyarrow/__init__.py <<EOF
-import sys
-import os
-os.getcwd()
-print("sys.path.pop(0) {}".format(sys.path.pop(0)))
-print(sys.path)
-from .lib import cpu_count, set_cpu_count
-EOF
-cat pyarrow/__init__.py.bak >> pyarrow/__init__.py
   '';
 
   preCheck = ''
@@ -59,53 +48,18 @@ cat pyarrow/__init__.py.bak >> pyarrow/__init__.py
     # when it is not intended to be imported at all
     rm pyarrow/tests/deserialize_buffer.py
     substituteInPlace pyarrow/tests/test_feather.py --replace "test_deserialize_buffer_in_different_process" "_disabled"
-
-set -x
-env
-pwd
-cat nix_run_setup
-find "$out" -print0 | xargs -0 touch -t 7001010000
-ls -laR "$out"
-echo -------------------------------------
-echo -------------------------------------
-echo -------------------------------------
-echo -------------------------------------
-echo -------------------------------------
-echo -------------------------------------
-echo -------------------------------------
-echo -------------------------------------
-echo -------------------------------------
-echo -------------------------------------
-echo -------------------------------------
-echo -------------------------------------
-echo -------------------------------------
-#rm pyarrow/__init__.py
-find . -print0 | xargs -0 touch -t 7001010000
-ls -laR .
-  '';
-
-  checkPhase = ''
-    runHook preCheck
-cat >test.py <<EOF
-import sys
-print("test.py: sys.path.pop(0) {}".format(sys.path.pop(0)))
-print(1)
-import pyarrow
-print(2)
-#import pyarrow.lib
-#print(3)
-EOF
-sed -i '1iimport sys; sys.path.pop(0)' setup.py
-head setup.py
-    ${python.interpreter} test.py
-    ${python.interpreter} setup.py test
-    runHook postCheck
   '';
 
   ARROW_HOME = _arrow-cpp;
   PARQUET_HOME = _parquet-cpp;
 
   setupPyBuildFlags = ["--with-parquet" ];
+
+  checkPhase = ''
+    mv pyarrow/tests tests
+    rm -rf pyarrow
+    pytest -v
+  '';
 
   meta = with lib; {
     description = "A cross-language development platform for in-memory data";
