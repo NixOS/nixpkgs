d7e98c8ab22b2c9e6ffdc89e135af9a8b84a470e
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-26 10:06:29 -0400

    pythonPackages.pykickstart: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3374,34 +3374,7 @@ in {
 
   pyjwt = callPackage ../development/python-modules/pyjwt { };
 
-  pykickstart = buildPythonPackage rec {
-    name = "pykickstart-${version}";
-    version = "1.99.39";
-    md5_path = "d249f60aa89b1b4facd63f776925116d";
-
-    src = pkgs.fetchurl rec {
-      url = "http://src.fedoraproject.org/repo/pkgs/pykickstart/"
-          + "${name}.tar.gz/${md5_path}/${name}.tar.gz";
-      sha256 = "e0d0f98ac4c5607e6a48d5c1fba2d50cc804de1081043f9da68cbfc69cad957a";
-    };
-
-    postPatch = ''
-      sed -i -e "s/for tst in tstList/for tst in sorted(tstList, \
-                 key=lambda m: m.__name__)/" tests/baseclass.py
-    '';
-
-    propagatedBuildInputs = with self; [ urlgrabber ];
-
-    checkPhase = ''
-      ${python.interpreter} tests/baseclass.py -vv
-    '';
-
-    meta = {
-      homepage = "http://fedoraproject.org/wiki/Pykickstart";
-      description = "Read and write Fedora kickstart files";
-      license = licenses.gpl2Plus;
-    };
-  };
+  pykickstart = callPackage ../development/python-modules/pykickstart { };
 
   pyobjc = if stdenv.isDarwin
     then callPackage ../development/python-modules/pyobjc {}
