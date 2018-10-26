bb37de46d4e1274ac9df4337534dfdf5f3544ea6
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-25 21:27:18 -0400

    pythonPackages.pickleshare: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3141,26 +3141,7 @@ in {
 
   pgspecial = callPackage ../development/python-modules/pgspecial { };
 
-  pickleshare = buildPythonPackage rec {
-    version = "0.7.4";
-    name = "pickleshare-${version}";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/p/pickleshare/${name}.tar.gz";
-      sha256 = "84a9257227dfdd6fe1b4be1319096c20eb85ff1e82c7932f36efccfe1b09737b";
-    };
-
-    propagatedBuildInputs = with self; [pathpy] ++ optional (pythonOlder "3.4") pathlib2;
-
-    # No proper test suite
-    doCheck = false;
-
-    meta = {
-      description = "Tiny 'shelve'-like database with concurrency support";
-      homepage = https://github.com/vivainio/pickleshare;
-      license = licenses.mit;
-    };
-  };
+  pickleshare = callPackage ../development/python-modules/pickleshare { };
 
   piep = buildPythonPackage rec {
     version = "0.8.0";
