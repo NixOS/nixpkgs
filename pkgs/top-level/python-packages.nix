6af8b42e17319bcc436546793fcba20497366e4f
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-26 13:16:32 -0400

    pythonPackages.setuptoolsTrial: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3710,31 +3710,7 @@ in {
 
   setuptoolsDarcs = callPackage ../development/python-modules/setuptoolsdarcs { };
 
-  setuptoolsTrial = buildPythonPackage rec {
-    name = "${pname}-${version}";
-    pname = "setuptools_trial";
-    version = "0.6.0";
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/s/${pname}/${name}.tar.gz";
-      sha256 = "14220f8f761c48ba1e2526f087195077cf54fad7098b382ce220422f0ff59b12";
-    };
-    buildInputs = with self; [ pytest virtualenv pytestrunner pytest-virtualenv ];
-    propagatedBuildInputs = with self; [ twisted pathlib2 ];
-    postPatch = ''
-      sed -i '12,$d' tests/test_main.py
-    '';
-
-    # Couldn't get tests working
-    doCheck = false;
-
-    meta = {
-      description = "Setuptools plugin that makes unit tests execute with trial instead of pyunit.";
-      homepage = "https://github.com/rutsky/setuptools-trial";
-      license = licenses.bsd2;
-      maintainers = with maintainers; [ ryansydnor nand0p ];
-      platforms   = platforms.all;
-    };
-  };
+  setuptoolsTrial = callPackage ../development/python-modules/setuptoolstrial { };
 
   shippai = callPackage ../development/python-modules/shippai {};
 
