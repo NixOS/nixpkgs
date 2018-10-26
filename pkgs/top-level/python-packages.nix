5e9d8c8e136423408a3b52b5eee4db4279628f63
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-26 11:43:34 -0400

    pythonPackages.python-wifi: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3479,25 +3479,7 @@ in {
 
   python-ptrace = callPackage ../development/python-modules/python-ptrace { };
 
-  python-wifi = buildPythonPackage rec {
-    name = "python-wifi-${version}";
-    version = "0.6.1";
-    disabled = !isPy27;
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/p/python-wifi/${name}.tar.bz2";
-      sha256 = "149c3dznb63d82143cz5hqdim0mqjysz6p3yk0zv271vq3xnmzvv";
-    };
-
-    meta = {
-      inherit version;
-      description = "Read & write wireless card capabilities using the Linux Wireless Extensions";
-      homepage = http://pythonwifi.tuxfamily.org/;
-      # From the README: "pythonwifi is licensed under LGPLv2+, however, the
-      # examples (e.g. iwconfig.py and iwlist.py) are licensed under GPLv2+."
-      license = with licenses; [ lgpl2Plus gpl2Plus ];
-    };
-  };
+  python-wifi = callPackage ../development/python-modules/python-wifi { };
 
   python-etcd = buildPythonPackage rec {
     name = "python-etcd-${version}";
