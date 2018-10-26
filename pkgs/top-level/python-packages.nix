6075a7352fbf45c5efeff1d44142ccf49f036847
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-25 22:15:22 -0400

    pythonPackages.pyblock: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3255,35 +3255,7 @@ in {
 
   pybfd = callPackage ../development/python-modules/pybfd { };
 
-  pyblock = stdenv.mkDerivation rec {
-    name = "pyblock-${version}";
-    version = "0.53";
-    md5_path = "f6d33a8362dee358517d0a9e2ebdd044";
-
-    src = pkgs.fetchurl rec {
-      url = "http://src.fedoraproject.org/repo/pkgs/python-pyblock/"
-          + "${name}.tar.bz2/${md5_path}/${name}.tar.bz2";
-      sha256 = "f6cef88969300a6564498557eeea1d8da58acceae238077852ff261a2cb1d815";
-    };
-
-    postPatch = ''
-      sed -i -e 's|/usr/include/python|${python}/include/python|' \
-             -e 's/-Werror *//' -e 's|/usr/|'"$out"'/|' Makefile
-    '';
-
-    buildInputs = with self; [ python pkgs.lvm2 pkgs.dmraid ];
-
-    makeFlags = [
-      "USESELINUX=0"
-      "SITELIB=$(out)/${python.sitePackages}"
-    ];
-
-    meta = {
-      description = "Interface for working with block devices";
-      license = licenses.gpl2Plus;
-      broken = isPy3k; # doesn't build on python 3, 2018-04-11
-    };
-  };
+  pyblock = callPackage ../development/python-modules/pyblock { };
 
   pybcrypt = buildPythonPackage rec {
     pname = "pybcrypt";
