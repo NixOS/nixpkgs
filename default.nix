0869e4f428096e87ca1353da888117e0ea00086b
kraakkop <44498028+kraakkop@users.noreply.github.com>
2018-10-26 14:18:24 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,8 @@
 let requiredVersion = import ./lib/minver.nix; in
 
-if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
+else
+! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 
+if then
 
   abort ''
 
