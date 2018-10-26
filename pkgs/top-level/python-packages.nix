8c8e1d992a12e3dda7200be4115cfcc06e60d45f
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-26 09:49:50 -0400

    pythonPackages.pyinputevent: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3348,23 +3348,7 @@ in {
 
   PyICU = callPackage ../development/python-modules/pyicu { };
 
-  pyinputevent = buildPythonPackage rec {
-    name = "pyinputevent-2016-10-18";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "ntzrmtthihu777";
-      repo = "pyinputevent";
-      rev = "d2075fa5db5d8a402735fe788bb33cf9fe272a5b";
-      sha256 = "0rkis0xp8f9jc00x7jb9kbvhdla24z1vl30djqa6wy6fx0cr6sib";
-    };
-
-    meta = {
-      homepage = "https://github.com/ntzrmtthihu777/pyinputevent";
-      description = "Python interface to the Input Subsystem's input_event and uinput";
-      license = licenses.bsd3;
-      platforms = platforms.linux;
-    };
-  };
+  pyinputevent = callPackage ../development/python-modules/pyinputevent { };
 
   pyinotify = buildPythonPackage rec {
     name = "pyinotify-${version}";
