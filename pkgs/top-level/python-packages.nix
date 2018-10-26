e6ec942902afcadf27a1a504e8c4eb98fdc7ff2b
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-25 21:36:22 -0400

    pythonPackages.pysftp: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3156,29 +3156,7 @@ in {
 
   pymetar = callPackage ../development/python-modules/pymetar { };
 
-  pysftp = buildPythonPackage rec {
-    name = "pysftp-${version}";
-    version = "0.2.9";
-    disabled = isPyPy;
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/p/pysftp/${name}.tar.gz";
-      sha256 = "0jl5qix5cxzrv4lb8rfpjkpcghbkacnxkb006ikn7mkl5s05mxgv";
-    };
-
-    propagatedBuildInputs = with self; [ paramiko ];
-
-    meta = {
-      homepage = https://bitbucket.org/dundeemt/pysftp;
-      description = "A friendly face on SFTP";
-      license = licenses.mit;
-      longDescription = ''
-        A simple interface to SFTP. The module offers high level abstractions
-        and task based routines to handle your SFTP needs. Checkout the Cook
-        Book, in the docs, to see what pysftp can do for you.
-      '';
-    };
-  };
+  pysftp = callPackage ../development/python-modules/pysftp { };
 
   pysoundfile = callPackage ../development/python-modules/pysoundfile { };
 
