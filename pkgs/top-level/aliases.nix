841f3d031bae15d3819d25663304c89777496139
Guillaume Maudoux <guillaume.maudoux@uclouvain.be>
2018-10-27 16:56:25 +0200

    mozart2: init at 2.0.1

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -218,6 +218,8 @@ mapAliases ({
   mobile_broadband_provider_info = mobile-broadband-provider-info; # added 2018-02-25
   module_init_tools = kmod; # added 2016-04-22
   mono-zeroconf = throw "deprecated 2019-09-20: abandoned by upstream.";
+  mozart = mozart2-binary; # added 2019-09-23
+  mozart-binary = mozart2-binary; # added 2019-09-23
   mpich2 = mpich;  # added 2018-08-06
   msf = metasploit; # added 2018-04-25
   libmsgpack = msgpack; # added 2018-08-17
