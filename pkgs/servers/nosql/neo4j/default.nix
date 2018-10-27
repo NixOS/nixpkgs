7c85639ed7be310957c6f4f9c93f0f1f31ff386f
R. RyanTM <ryantm-bot@ryantm.com>
2018-10-26 22:06:29 -0700

    neo4j: 3.4.8 -> 3.4.9
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/neo4j/versions

--- pkgs/servers/nosql/neo4j/default.nix
+++ pkgs/servers/nosql/neo4j/default.nix
@@ -4,11 +4,11 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "neo4j-${version}";
-  version = "3.4.8";
+  version = "3.4.9";
 
   src = fetchurl {
     url = "https://neo4j.com/artifact.php?name=neo4j-community-${version}-unix.tar.gz";
-    sha256 = "1mdxqfy5xzc6944lg87975i9bfpqqx28xl8h70m4wn79x0qgby2v";
+    sha256 = "0kg0dr42qi8cwg3i1hcgczd4psh97s54q5zd8z2wn1fqf4m2h597";
   };
 
   buildInputs = [ makeWrapper jre8 which gawk ];
