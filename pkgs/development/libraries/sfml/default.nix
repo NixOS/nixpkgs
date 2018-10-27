9f0381d4970acd24c22a6fac8d21b4d61345d38c
R. RyanTM <ryantm-bot@ryantm.com>
2018-10-27 05:11:57 -0700

    sfml: 2.5.0 -> 2.5.1 (#48907)
    * sfml: 2.5.0 -> 2.5.1
    
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/sfml/versions
    
    * sfml: cmake is only needed at build time
    
    Moved cmake from buildInputs to nativeBuildInputs
    
    * sfml: use fetchzip instead of fetchurl
    
    Don't use fetchurl for changing GitHub archive tarballs

--- pkgs/development/libraries/sfml/default.nix
+++ pkgs/development/libraries/sfml/default.nix
@@ -1,26 +1,31 @@
-{ stdenv, fetchurl, cmake, libX11, freetype, libjpeg, openal, flac, libvorbis
+{ stdenv, fetchzip, cmake, libX11, freetype, libjpeg, openal, flac, libvorbis
 , glew, libXrandr, libXrender, udev, xcbutilimage
 , IOKit, Foundation, AppKit, OpenAL
 }:
 
 let
-  version = "2.5.0";
+  version = "2.5.1";
 in
 
 stdenv.mkDerivation rec {
   name = "sfml-${version}";
-  src = fetchurl {
+
+  src = fetchzip {
     url = "https://github.com/SFML/SFML/archive/${version}.tar.gz";
-    sha256 = "1x3yvhdrln5b6h4g5r4mds76gq8zsxw6icxqpwqkmxsqcq5yviab";
+    sha256 = "0abr8ri2ssfy9ylpgjrr43m6rhrjy03wbj9bn509zqymifvq5pay";
   };
-  buildInputs = [ cmake libX11 freetype libjpeg openal flac libvorbis glew
+
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ libX11 freetype libjpeg openal flac libvorbis glew
                   libXrandr libXrender xcbutilimage
                 ] ++ stdenv.lib.optional stdenv.isLinux udev
                   ++ stdenv.lib.optionals stdenv.isDarwin [ IOKit Foundation AppKit OpenAL ];
+
   cmakeFlags = [ "-DSFML_INSTALL_PKGCONFIG_FILES=yes"
                  "-DSFML_MISC_INSTALL_PREFIX=share/SFML"
                  "-DSFML_BUILD_FRAMEWORKS=no"
                  "-DSFML_USE_SYSTEM_DEPS=yes" ];
+
   meta = with stdenv.lib; {
     homepage = http://www.sfml-dev.org/;
     description = "Simple and fast multimedia library";
