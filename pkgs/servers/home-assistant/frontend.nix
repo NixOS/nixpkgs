ad8f75387f3b905a8e58299a99af30064194393e
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-10-27 14:20:04 +0200

    home-assistant: 0.80.3 -> 0.81.0

--- pkgs/servers/home-assistant/frontend.nix
+++ pkgs/servers/home-assistant/frontend.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "home-assistant-frontend";
-  version = "20181018.0";
+  version = "20181026.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "83f52421056acda8297f174a7c4e3c540109673c2f2c25720638d171c6bc2653";
+    sha256 = "fa877803ec7201a9b2129e96d6c19822cbcb28fa8ad5d422974bd3c872da1531";
   };
 
   propagatedBuildInputs = [ user-agents ];
