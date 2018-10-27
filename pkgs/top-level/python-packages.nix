17da4404ca0055a266c92168379cbe2c22d7083b
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-27 09:35:09 -0400

    pythonPackages.py3dns: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3798,25 +3798,7 @@ in {
 
   pydns =
     let
-      py3 = buildPythonPackage rec {
-        name = "${pname}-${version}";
-        pname = "py3dns";
-        version = "3.1.1a";
-
-        src = fetchPypi {
-          inherit pname version;
-          sha256 = "0z0qmx9j1ivpgg54gqqmh42ljnzxaychc5inz2gbgv0vls765smz";
-        };
-
-        preConfigure = ''
-          sed -i \
-            -e '/import DNS/d' \
-            -e 's/DNS.__version__/"${version}"/g' \
-            setup.py
-        '';
-
-        doCheck = false;
-      };
+      py3 = callPackage ../development/python-modules/py3dns { };
 
       py2 = buildPythonPackage rec {
         name = "${pname}-${version}";
