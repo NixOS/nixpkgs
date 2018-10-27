f4927a34e764be81fbf76ec54f5d656f3c64dff1
Daiderd Jordan <daiderd@gmail.com>
2018-10-27 18:21:15 +0100

    rust-cbindgen: fix darwin build

--- pkgs/development/tools/rust/cbindgen/default.nix
+++ pkgs/development/tools/rust/cbindgen/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, rustPlatform }:
+{ stdenv, fetchFromGitHub, rustPlatform, Security }:
 
 rustPlatform.buildRustPackage rec {
   name = "rust-cbindgen-${version}";
@@ -12,6 +12,8 @@ rustPlatform.buildRustPackage rec {
   };
 
   cargoSha256 = "1m1chwmfgj74xrmn4gb9yz5kx8c408a1hlqmpcq780kqj0k927i9";
+
+  buildInputs = stdenv.lib.optional stdenv.isDarwin Security;
 
   meta = with stdenv.lib; {
     description = "A project for generating C bindings from Rust code";
