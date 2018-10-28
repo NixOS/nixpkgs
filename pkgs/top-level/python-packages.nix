18c7709d1d0f34151125f8e7c632973c90ee3a0e
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-28 12:15:21 -0400

    pythonPackages.twitter-common-lang: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4019,23 +4019,7 @@ in {
 
   twitter-common-dirutil = callPackage ../development/python-modules/twitter-common-dirutil { };
 
-  twitter-common-lang = buildPythonPackage rec {
-    pname   = "twitter.common.lang";
-    version = "0.3.9";
-    name    = "${pname}-${version}";
-
-    src = self.fetchPypi {
-      inherit pname version;
-      sha256 = "1l8fmnsrx7hgg3ivslg588rnl9n1gfjn2w6224fr8rs7zmkd5lan";
-    };
-
-    meta = {
-      description = "Twitter's 2.x / 3.x compatibility swiss-army knife";
-      homepage    = "https://twitter.github.io/commons/";
-      license     = licenses.asl20;
-      maintainers = with maintainers; [ copumpkin ];
-    };
-  };
+  twitter-common-lang = callPackage ../development/python-modules/twitter-common-lang { };
 
   twitter-common-log = buildPythonPackage rec {
     pname   = "twitter.common.log";
