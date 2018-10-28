c05b3655410bfa1a0d67854ca83dde14970f34dc
Jörg Thalheim <joerg@thalheim.io>
2018-10-28 23:01:17 +0000

    mono44: mark as insecure

--- pkgs/development/compilers/mono/4.4.nix
+++ pkgs/development/compilers/mono/4.4.nix
@@ -5,4 +5,5 @@ callPackage ./generic.nix (rec {
   version = "4.4.2.11";
   sha256 = "0cxnypw1j7s253wr5hy05k42ghgg2s9qibp10kndwnp5bv12q34h";
   enableParallelBuilding = false; # #32386, https://hydra.nixos.org/build/65565737
+  meta.knownVulnerabilities = [ "CVE-2018-1002208" ];
 })
