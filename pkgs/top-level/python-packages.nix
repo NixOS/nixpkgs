c3793aa1b1b3ab8d1a9fdf74c34f659c0c568b65
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-28 11:34:55 -0400

    pythonPackages.tkinter: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3977,26 +3977,7 @@ in {
   # Python package.
   tkinter = let
     py = python.override{x11Support=true;};
-  in buildPythonPackage rec {
-    name = "tkinter-${python.version}";
-    src = py;
-    format = "other";
-
-    disabled = isPyPy;
-
-    installPhase = ''
-      # Move the tkinter module
-      mkdir -p $out/${py.sitePackages}
-      mv lib/${py.libPrefix}/lib-dynload/_tkinter* $out/${py.sitePackages}/
-    '' + stdenv.lib.optionalString (!stdenv.isDarwin) ''
-      # Update the rpath to point to python without x11Support
-      old_rpath=$(patchelf --print-rpath $out/${py.sitePackages}/_tkinter*)
-      new_rpath=$(sed "s#${py}#${python}#g" <<< "$old_rpath" )
-      patchelf --set-rpath $new_rpath $out/${py.sitePackages}/_tkinter*
-    '';
-
-    meta = py.meta;
-  };
+  in callPackage ../development/python-modules/tkinter { py = py; };
 
   tlslite = buildPythonPackage rec {
     name = "tlslite-${version}";
