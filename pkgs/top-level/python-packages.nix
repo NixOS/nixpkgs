6986e9ecacdaf516d02d0791deb86b3db9c0a0a8
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-28 11:36:53 -0400

    pythonPackages.tlslite: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3979,21 +3979,7 @@ in {
     py = python.override{x11Support=true;};
   in callPackage ../development/python-modules/tkinter { py = py; };
 
-  tlslite = buildPythonPackage rec {
-    name = "tlslite-${version}";
-    version = "0.4.8";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/t/tlslite/${name}.tar.gz";
-      sha256 = "1fxx6d3nw5r1hqna1h2jvqhcygn9fyshlm0gh3gp0b1ji824gd6r";
-    };
-
-    meta = {
-      description = "A pure Python implementation of SSL and TLS";
-      homepage = https://pypi.python.org/pypi/tlslite;
-      license = licenses.bsd3;
-    };
-  };
+  tlslite = callPackage ../development/python-modules/tlslite { };
 
   qrcode = buildPythonPackage rec {
     name = "qrcode-${version}";
