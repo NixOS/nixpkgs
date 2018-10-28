413ee24b0855ae85b2ad45b4645f9822e4c34db7
Matthew Bauer <mjbauer95@gmail.com>
2018-10-28 17:15:37 -0500

    nixos/system-path: don’t pull in glibc directly
    These binaries are provided by glibc, but we don’t want to pull them
    directly from glibc in case another libc is being used. Instead, we
    can use some indirect packages that will use whatever libc you are
    linking against:
    
    - getent
    - getconf
    - ldd

--- nixos/modules/config/system-path.nix
+++ nixos/modules/config/system-path.nix
@@ -19,7 +19,9 @@ let
       pkgs.diffutils
       pkgs.findutils
       pkgs.gawk
-      pkgs.glibc # for ldd, getent
+      pkgs.getent
+      pkgs.getconf
+      pkgs.ldd
       pkgs.gnugrep
       pkgs.gnupatch
       pkgs.gnused
