6c10afe1301d8aae442ea431b0c656f3480c13f0
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-29 11:32:23 -0400

    pythonPackages.webhelpers: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4268,25 +4268,7 @@ in {
 
   websocket_client = callPackage ../development/python-modules/websockets_client { };
 
-
-  webhelpers = buildPythonPackage rec {
-    name = "WebHelpers-1.3";
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/W/WebHelpers/${name}.tar.gz";
-      sha256 = "ea86f284e929366b77424ba9a89341f43ae8dee3cbeb8702f73bcf86058aa583";
-    };
-
-    buildInputs = with self; [ routes markupsafe webob nose ];
-
-    # TODO: failing tests https://bitbucket.org/bbangert/webhelpers/pull-request/1/fix-error-on-webob-123/diff
-    doCheck = false;
-
-    meta = {
-      maintainers = with maintainers; [ garbas domenkozar ];
-      platforms = platforms.all;
-    };
-  };
+  webhelpers = callPackage ../development/python-modules/webhelpers { };
 
   whichcraft = callPackage ../development/python-modules/whichcraft { };
 
