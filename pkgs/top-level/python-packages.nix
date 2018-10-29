7735833264fbea3f5b6daafbcafbddea70f5356e
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-29 11:01:14 -0400

    pythonPakcages.tokenlib: refacotr move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4230,17 +4230,7 @@ in {
   tornado = callPackage ../development/python-modules/tornado { };
   tornado_4 = callPackage ../development/python-modules/tornado { version = "4.5.3"; };
 
-  tokenlib = buildPythonPackage rec {
-    name = "tokenlib-${version}";
-    version = "0.3.1";
-    src = pkgs.fetchgit {
-      url = https://github.com/mozilla-services/tokenlib.git;
-      rev = "refs/tags/${version}";
-      sha256 = "0bq6dqyfwh29pg8ngmrm4mx4q27an9lsj0p9l79p9snn4g2rxzc8";
-    };
-
-    propagatedBuildInputs = with self; [ requests webob ];
-  };
+  tokenlib = callPackage ../development/python-modules/tokenlib { };
 
   tunigo = callPackage ../development/python-modules/tunigo { };
 
