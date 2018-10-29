94afc8e39ab76badbb609d2c802327392e4956ce
Will Dietz <w@wdtz.org>
2018-10-29 10:08:58 -0500

    llvmPackages: keep darwin at llvm5
    My previous attempts to bump the Darwin stdenv version didn't
    seem to work, but AFAIK doing so should be straightforward
    for anyone with a Darwin builder and the interest in tackling this.
    
    While not the preferred solution, adding a conditional lets us move on,
    as we missed 6 entirely due to no one getting around to resolving the
    Darwin bootstrap accordingly.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7119,7 +7119,7 @@ with pkgs;
   llvm_37 = llvmPackages_37.llvm;
   llvm_35 = llvmPackages_35.llvm;
 
-  llvmPackages = recurseIntoAttrs llvmPackages_7;
+  llvmPackages = recurseIntoAttrs (if stdenv.isDarwin then llvmPackages_5 else llvmPackages_7);
 
   llvmPackages_35 = callPackage ../development/compilers/llvm/3.5 ({
     isl = isl_0_14;
