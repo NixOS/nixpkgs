d4ff2313e69247afcf078536c65cd5f82751e1b8
Will Dietz <w@wdtz.org>
2018-10-29 10:07:30 -0500

    llvmPackages: 5 -> 7 (bump our default LLVM)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7544,7 +7544,7 @@ in
   llvm_37 = llvmPackages_37.llvm;
   llvm_35 = llvmPackages_35.llvm;
 
-  llvmPackages = recurseIntoAttrs llvmPackages_5;
+  llvmPackages = recurseIntoAttrs llvmPackages_7;
 
   llvmPackages_35 = callPackage ../development/compilers/llvm/3.5 ({
     isl = isl_0_14;
