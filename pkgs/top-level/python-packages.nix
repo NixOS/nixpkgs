e53a0479b51d15c330e31e2960e0749ec74952b5
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-29 12:39:39 -0400

    pythonPackages.svg2tikz: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4361,25 +4361,7 @@ in {
 
   vxi11 = callPackage ../development/python-modules/vxi11 { };
 
-  svg2tikz = self.buildPythonPackage {
-    name = "svg2tikz-1.0.0";
-    disabled = ! isPy27;
-
-    propagatedBuildInputs = with self; [lxml];
-
-    src = pkgs.fetchgit {
-      url = "https://github.com/kjellmf/svg2tikz";
-      sha256 = "429428ec435e53672b85cdfbb89bb8af0ff9f8238f5d05970729e5177d252d5f";
-      rev = "ad36f2c3818da13c4136d70a0fd8153acf8daef4";
-    };
-
-    meta = {
-      homepage = https://github.com/kjellmf/svg2tikz;
-      description = "An SVG to TikZ converter";
-      license = licenses.gpl2Plus;
-      maintainers =  with maintainers; [ gal_bolle ];
-    };
-  };
+  svg2tikz = callPackage ../development/python-modules/svg2tikz { };
 
   WSGIProxy = buildPythonPackage rec {
     name = "WSGIProxy-${version}";
