c2526182c880a6ad969dd231b6802d418ae09b4b
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-10-29 14:51:52 -0400

    pythonPackages.suds: refactor move to python-modules

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4463,27 +4463,7 @@ in {
 
   pafy = callPackage ../development/python-modules/pafy { };
 
-  suds = buildPythonPackage rec {
-    name = "suds-0.4";
-    disabled = isPy3k;
-
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/s/suds/suds-0.4.tar.gz";
-      sha256 = "1w4s9051iv90c0gs73k80c3d51y2wbx1xgfdgg2hk7mv4gjlllnm";
-    };
-
-    patches = [ ../development/python-modules/suds-0.4-CVE-2013-2217.patch ];
-
-    meta = with stdenv.lib; {
-      # Broken for security issues:
-      # - https://github.com/NixOS/nixpkgs/issues/19678
-      # - https://lwn.net/Vulnerabilities/559200/
-      broken = true;
-      description = "Lightweight SOAP client";
-      homepage = https://fedorahosted.org/suds;
-      license = licenses.lgpl3Plus;
-    };
-  };
+  suds = callPackage ../development/python-modules/suds { };
 
   suds-jurko = buildPythonPackage rec {
     name = "suds-jurko-${version}";
