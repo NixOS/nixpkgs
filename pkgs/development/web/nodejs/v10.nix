3751ae66f8a02a99336b795a9f642333a7bfe08d
Mathias Schreck <schreck.mathias@googlemail.com>
2018-10-30 12:02:04 +0100

    nodejs-10_x: 10.12.0 -> 10.13.0

--- pkgs/development/web/nodejs/v10.nix
+++ pkgs/development/web/nodejs/v10.nix
@@ -5,6 +5,6 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "10.12.0";
-    sha256 = "1r0aqcxafha13ks8586x77n77zi88db259cpaix0y1ivdh6qkkfr";
+    version = "10.13.0";
+    sha256 = "0hg6z89lczjs4cc8ljqqdy4h1n5ccwclniyyj2651yr81imck04d";
   }
