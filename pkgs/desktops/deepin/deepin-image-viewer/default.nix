c47f99e4d87d4eb8317296f5405030cb0c874092
José Romildo Malaquias <malaquias@gmail.com>
2018-10-30 21:16:20 -0300

    deepin.deepin-image-viewer: 1.2.23 -> 1.3.1
    - Update to version 1.3.1
    
    - Currently does not build on aarch64-linux. It seems that in the libpng
    package (which is bundled with freeimage) neon is not available on the
    platform, but is detected as being available.

--- pkgs/desktops/deepin/deepin-image-viewer/default.nix
+++ pkgs/desktops/deepin/deepin-image-viewer/default.nix
@@ -6,13 +6,13 @@
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
   pname = "deepin-image-viewer";
-  version = "1.2.23";
+  version = "1.3.1";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "1n1b3j65in6v7q5bxgkiam8qy56kjn9prld3sjrbc2mqzff8sm3q";
+    sha256 = "0dxdvm6hzj6izfxka35za8y7vacd06nksfgzx6xsv7ywzagri4k5";
   };
 
   nativeBuildInputs = [
@@ -48,6 +48,7 @@ stdenv.mkDerivation rec {
     homepage = https://github.com/linuxdeepin/deepin-image-viewer;
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
+    badPlatforms = [ "aarch64-linux" ]; # See https://github.com/NixOS/nixpkgs/pull/46463#issuecomment-420274189
     maintainers = with maintainers; [ romildo ];
   };
 }
