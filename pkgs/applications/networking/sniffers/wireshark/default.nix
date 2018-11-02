5ce0b908620c16e699684a14da1b5cc7a7700dbd
Franz Pletz <fpletz@fnordicwalking.de>
2018-11-02 03:03:15 +0100

    wireshark: 2.4.9 -> 2.4.10
    Security release: https://www.wireshark.org/docs/relnotes/wireshark-2.4.10.html

--- pkgs/applications/networking/sniffers/wireshark/default.nix
+++ pkgs/applications/networking/sniffers/wireshark/default.nix
@@ -12,7 +12,7 @@ assert withQt  -> !withGtk && qt5  != null;
 with stdenv.lib;
 
 let
-  version = "2.4.9";
+  version = "2.4.10";
   variant = if withGtk then "gtk" else if withQt then "qt" else "cli";
 
 in stdenv.mkDerivation {
@@ -20,7 +20,7 @@ in stdenv.mkDerivation {
 
   src = fetchurl {
     url = "http://www.wireshark.org/download/src/all-versions/wireshark-${version}.tar.xz";
-    sha256 = "1qrkqrm96gj3wah42qj62axgdzin8cwiscpcwbq3d596cm3g5i33";
+    sha256 = "0aw8f9h64yv9h1klk37kz5c4s8brnazfv6idsbwpqs5bp8sda4fi";
   };
 
   cmakeFlags = [
