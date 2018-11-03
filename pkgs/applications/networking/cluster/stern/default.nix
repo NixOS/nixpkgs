79924a83133d43358cd97774f2e2d14b933b15e9
Benjamin Staffin <benley@gmail.com>
2018-11-02 23:04:19 -0400

    stern: Include bash & zsh completion support

--- pkgs/applications/networking/cluster/stern/default.nix
+++ pkgs/applications/networking/cluster/stern/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildGoPackage, fetchFromGitHub }:
+{ stdenv, lib, buildGoPackage, fetchFromGitHub }:
 
 buildGoPackage rec {
   name = "stern-${version}";
@@ -15,11 +15,20 @@ buildGoPackage rec {
 
   goDeps = ./deps.nix;
 
-   meta = with lib; {
-      description      = "Multi pod and container log tailing for Kubernetes";
-      homepage         = "https://github.com/wercker/stern";
-      license          = licenses.asl20;
-      maintainers      = with maintainers; [ mbode ];
-      platforms        = platforms.unix;
-    };
+  # Only build shell completion if we're _not_ cross compiling,
+  # because it requires executing the compiled stern binary.
+  postInstall = lib.optionalString (stdenv.buildPlatform == stdenv.hostPlatform) ''
+    mkdir -p $bin/share/bash-completion/completions
+    $bin/bin/stern --completion bash > $bin/share/bash-completion/completions/stern
+    mkdir -p $bin/share/zsh/site-functions
+    $bin/bin/stern --completion zsh > $bin/share/zsh/site-functions/_stern
+  '';
+
+  meta = with lib; {
+    description      = "Multi pod and container log tailing for Kubernetes";
+    homepage         = "https://github.com/wercker/stern";
+    license          = licenses.asl20;
+    maintainers      = with maintainers; [ mbode ];
+    platforms        = platforms.unix;
+  };
 }
