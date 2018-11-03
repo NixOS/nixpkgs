5af9e759834759f5e44290b8719ba60816990fb6
taku0 <mxxouy6x3m_github@tatapa.org>
2018-11-03 21:34:33 +0900

    adoptopenjdk-openj9-bin: 11 -> 11.0.1
    (cherry picked from commit 3cfa035a9957cc0d4169ced909841d7ac3383613)
    Signed-off-by: Domen Kožar <domen@dev.si>

--- pkgs/development/compilers/adoptopenjdk-bin/jdk11-linux.nix
+++ pkgs/development/compilers/adoptopenjdk-bin/jdk11-linux.nix
@@ -1,36 +1,43 @@
 let
-  version = "11";
-  buildNumber = "28";
-  baseUrl = "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-${version}%2B${buildNumber}";
-  makePackage = { packageType, vmType, sha256 }: import ./jdk-linux-base.nix {
+  makePackage = { version, buildNumber, packageType, vmType, sha256 }: import ./jdk-linux-base.nix {
     name = if packageType == "jdk"
       then
         "adoptopenjdk-${vmType}-bin-${version}"
       else
         "adoptopenjdk-${packageType}-${vmType}-bin-${version}";
-    url = "${baseUrl}/OpenJDK${version}-${packageType}_x64_linux_${vmType}_${version}_${buildNumber}.tar.gz";
+
+    url = "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-${version}%2B${buildNumber}/OpenJDK11-${packageType}_x64_linux_${vmType}_${version}_${buildNumber}.tar.gz";
+
     inherit sha256;
   };
 in
 {
   jdk-hotspot = makePackage {
+    version = "11";
+    buildNumber = "28";
     packageType = "jdk";
     vmType = "hotspot";
     sha256 = "e1e18fc9ce2917473da3e0acb5a771bc651f600c0195a3cb40ef6f22f21660af";
   };
   jre-hotspot = makePackage {
+    version = "11";
+    buildNumber = "28";
     packageType = "jre";
     vmType = "hotspot";
     sha256 = "346448142d46c6e51d0fadcaadbcde31251d7678922ec3eb010fcb1b6e17804c";
   };
   jdk-openj9 = makePackage {
+    version = "11.0.1";
+    buildNumber = "13";
     packageType = "jdk";
     vmType = "openj9";
-    sha256 = "fd77f22eb74078bcf974415abd888296284d2ceb81dbaca6ff32f59416ebc57f";
+    sha256 = "765947ab9457a29d2aa9d11460a4849611343c1e0ea3b33b9c08409cd4672251";
   };
   jre-openj9 = makePackage {
+    version = "11.0.1";
+    buildNumber = "13";
     packageType = "jre";
     vmType = "openj9";
-    sha256 = "83a7c95e6b2150a739bdd5e8a6fe0315904fd13d8867c95db67c0318304a2c42";
+    sha256 = "a016413fd8415429b42e543fed7a1bee5010b1dbaf71d29a26e1c699f334c6ff";
   };
 }
