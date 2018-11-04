cd9edde48bf739c4ec29e113a13d84910065ab08
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:05 +0100

    python: manuel: 1.8.0 -> 1.9.0

--- pkgs/development/python-modules/manuel/default.nix
+++ pkgs/development/python-modules/manuel/default.nix
@@ -7,11 +7,11 @@
 
 buildPythonPackage rec {
   pname = "manuel";
-  version = "1.8.0";
+  version = "1.9.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1diyj6a8bvz2cdf9m0g2bbx9z2yjjnn3ylbg1zinpcjj6vldfx59";
+    sha256 = "d324983db5d8e3f36ff20732a723cf0af2e2477d569f871fc649f08b782fa8f1";
   };
 
   propagatedBuildInputs = [ six zope_testing ];
