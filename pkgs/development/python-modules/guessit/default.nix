8c40b80ef46ff2021822cfa6d4326add9ba36da1
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:01 +0100

    python: guessit: 3.0.0 -> 3.0.3

--- pkgs/development/python-modules/guessit/default.nix
+++ pkgs/development/python-modules/guessit/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "guessit";
-  version = "3.0.0";
+  version = "3.0.3";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "bf17e78783cf13bf903750770de4c3bb6c9ca89baafedb1612794660b6ebe32b";
+    sha256 = "590cfaac6adbc65a0297f7b2a44c2accf2cc660eeed6283b43cbad30e65806e0";
   };
 
   # Tests require more packages.
