f7ad1e02f56c213f9cdb65cce8eef1f27773a01a
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:00 +0100

    python: geopy: 1.11.0 -> 1.17.0

--- pkgs/development/python-modules/geopy/default.nix
+++ pkgs/development/python-modules/geopy/default.nix
@@ -9,12 +9,12 @@
 
 buildPythonPackage rec {
   pname = "geopy";
-  version = "1.11.0";
+  version = "1.17.0";
   disabled = !isPy27;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "04j1lxcsfyv03h0n0q7p2ig7a4n13x4x20fzxn8bkazpx6lyal22";
+    sha256 = "1bbea0efc5af91e0a7d4c2b31650f61667bcc1d0d717784d78c03f0ed13bb374";
   };
 
   doCheck = false;  # too much
