d182c76eed57d6c99c9ab44bcb7c52b117c67f47
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:08 +0100

    python: path.py: 11.0.1 -> 11.5.0

--- pkgs/development/python-modules/path.py/default.nix
+++ pkgs/development/python-modules/path.py/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "path.py";
-  version = "11.0.1";
+  version = "11.5.0";
   name = pname + "-" + version;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e7eb9d0ca4110d9b4d7c9baa0696d8c94f837d622409cefc5ec9e7c3d02ea11f";
+    sha256 = "b6687a532a735a2d79a13e92bdb31cb0971abe936ea0fa78bcb47faf4372b3cb";
   };
 
   checkInputs = [ pytest pytestrunner glibcLocales packaging ];
