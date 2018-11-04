389d863b72c9e086f448f2e3d029e50386f4bfdc
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:11 +0100

    python: pycryptopp: 0.6.0.1206569328141510525648634803928199668821045408958 -> 0.7.1.869544967005693312591928092448767568728501330214

--- pkgs/development/python-modules/pycryptopp/default.nix
+++ pkgs/development/python-modules/pycryptopp/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "pycryptopp";
-  version = "0.6.0.1206569328141510525648634803928199668821045408958";
+  version = "0.7.1.869544967005693312591928092448767568728501330214";
   disabled = isPy3k || isPyPy;  # see https://bitbucket.org/pypy/pypy/issue/1190/
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0n90h1yg7bfvlbhnc54xb6dbqm286ykaksyg04kxlhyjgf8mhq8i";
+    sha256 = "08ad57a1a39b7ed23c173692281da0b8d49d98ad3dcc09f8cca6d901e142699f";
   };
 
   # Prefer crypto++ library from the Nix store over the one that's included
