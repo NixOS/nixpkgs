396f1659110e6b5b380752286565351d650f8422
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:05 +0100

    python: librosa: 0.4.3 -> 0.6.2

--- pkgs/development/python-modules/librosa/default.nix
+++ pkgs/development/python-modules/librosa/default.nix
@@ -12,11 +12,11 @@
 
 buildPythonPackage rec {
   pname = "librosa";
-  version = "0.4.3";
+  version = "0.6.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "209626c53556ca3922e52d2fae767bf5b398948c867fcc8898f948695dacb247";
+    sha256 = "2aa868b8aade749b9904eeb7034fcf44115601c367969b6d01f5e1b4b9b6031d";
   };
 
   propagatedBuildInputs = [ joblib matplotlib six scikitlearn decorator audioread resampy ];
