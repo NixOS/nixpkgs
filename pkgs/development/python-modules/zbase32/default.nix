18865d380d8e6fa0c98e07bda5782003c965ae5b
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:22 +0100

    python: zbase32: 1.1.2 -> 1.1.5

--- pkgs/development/python-modules/zbase32/default.nix
+++ pkgs/development/python-modules/zbase32/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "zbase32";
-  version = "1.1.2";
+  version = "1.1.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "2f44b338f750bd37b56e7887591bf2f1965bfa79f163b6afcbccf28da642ec56";
+    sha256 = "9b25c34ba586cbbad4517af516e723599a6f38fc560f4797855a5f3051e6422f";
   };
 
   # Tests require `pyutil' so disable them to avoid circular references.
