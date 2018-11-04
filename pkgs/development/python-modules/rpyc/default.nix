3bee9b1aee56f0a21551d3f5830eae571d6da484
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:16 +0100

    python: rpyc: 3.3.0 -> 4.0.2

--- pkgs/development/python-modules/rpyc/default.nix
+++ pkgs/development/python-modules/rpyc/default.nix
@@ -7,11 +7,11 @@
 
 buildPythonPackage rec {
   pname = "rpyc";
-  version = "3.3.0";
+  version = "4.0.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "43fa845314f0bf442f5f5fab15bb1d1b5fe2011a8fc603f92d8022575cef8b4b";
+    sha256 = "a0d87cbad152f25e3702a03cb7fd67f6b10c87680a60ec3aea8dca5a56307c10";
   };
 
   propagatedBuildInputs = [ nose plumbum ];
