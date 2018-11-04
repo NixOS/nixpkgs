fc5acfaeb2b98e033e9775abb08e8e29ca9dbd4f
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:05 +0100

    python: logutils: 0.3.3 -> 0.3.5

--- pkgs/development/python-modules/logutils/default.nix
+++ pkgs/development/python-modules/logutils/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "logutils";
-  version = "0.3.3";
+  version = "0.3.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "173w55fg3hp5dhx7xvssmgqkcv5fjlaik11w5dah2fxygkjvhhj0";
+    sha256 = "bc058a25d5c209461f134e1f03cab637d66a7a5ccc12e593db56fbb279899a82";
   };
 
   meta = with stdenv.lib; {
