62fc1f4045ba53bd2c424ecd73135ee49d0843e2
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:22 +0100

    python: zope.configuration: 4.0.3 -> 4.3.0

--- pkgs/development/python-modules/zope_configuration/default.nix
+++ pkgs/development/python-modules/zope_configuration/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "zope.configuration";
-  version = "4.0.3";
+  version = "4.3.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1x9dfqypgympnlm25p9m43xh4qv3p7d75vksv9pzqibrb4cggw5n";
+    sha256 = "ddd162b7b9379c0f5cc060cbf2af44133396b7d26eaee9c7cf6e196d87e9aeb3";
   };
 
   propagatedBuildInputs = [ zope_i18nmessageid zope_schema ];
