de6fb294d498f83ea160eb887c34d82dae5c2dfc
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:11 +0100

    python: pydot_ng: 1.0.0 -> 2.0.0

--- pkgs/development/python-modules/pydot_ng/default.nix
+++ pkgs/development/python-modules/pydot_ng/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "pydot_ng";
-  version = "1.0.0";
+  version = "2.0.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0h8k8wlzvnb40v4js7afgfyhp3wasmb1kg4gr6z7ck63iv8fq864";
+    sha256 = "8c8073b97aa7030c28118961e2c6c92f046e4cb57aeba7df87146f7baa6530c5";
   };
 
   buildInputs = [ pytest unittest2 ];
