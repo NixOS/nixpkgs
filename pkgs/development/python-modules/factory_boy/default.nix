5b15b3499cec2d3423e9ee0428d1fa2176d57266
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:34:58 +0100

    python: factory_boy: 2.6.1 -> 2.11.1

--- pkgs/development/python-modules/factory_boy/default.nix
+++ pkgs/development/python-modules/factory_boy/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "factory_boy";
-  version = "2.6.1";
+  version = "2.11.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0a21f8kq917fj8xgmyp6gy8vcrlzzgwn80qas0d76h3vjbdy0bdq";
+    sha256 = "6f25cc4761ac109efd503f096e2ad99421b1159f01a29dbb917359dcd68e08ca";
   };
 
   propagatedBuildInputs = [ fake_factory ];
