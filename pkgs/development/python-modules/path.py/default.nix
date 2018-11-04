590f983ecd8570304829a510502ea807a3f477e1
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:08 +0100

    python: path.py: 11.0.1 -> 11.5.0

--- pkgs/development/python-modules/path.py/default.nix
+++ pkgs/development/python-modules/path.py/default.nix
@@ -6,22 +6,29 @@
 , pytest
 , glibcLocales
 , packaging
+, isPy27
+, backports_os
+, importlib-metadata
 }:
 
 buildPythonPackage rec {
   pname = "path.py";
-  version = "11.0.1";
+  version = "11.5.0";
   name = pname + "-" + version;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e7eb9d0ca4110d9b4d7c9baa0696d8c94f837d622409cefc5ec9e7c3d02ea11f";
+    sha256 = "b6687a532a735a2d79a13e92bdb31cb0971abe936ea0fa78bcb47faf4372b3cb";
   };
 
   checkInputs = [ pytest pytestrunner glibcLocales packaging ];
   buildInputs = [ setuptools_scm ];
+  propagatedBuildInputs = [
+    importlib-metadata
+  ] ++ lib.optional isPy27 backports_os
+  ;
 
-  LC_ALL="en_US.UTF-8";
+  LC_ALL = "en_US.UTF-8";
 
   meta = {
     description = "A module wrapper for os.path";
