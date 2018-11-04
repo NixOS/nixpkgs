b335b334a7740d57357b43de77ba44fbc8dd8dd2
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:34:59 +0100

    python: Fiona: 1.7.13 -> 1.8.0

--- pkgs/development/python-modules/fiona/default.nix
+++ pkgs/development/python-modules/fiona/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "Fiona";
-  version = "1.7.13";
+  version = "1.8.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a156129f0904cb7eb24aa0745b6075da54f2c31db168ed3bcac8a4bd716d77b2";
+    sha256 = "20141a9ece06daa7bb4333fba640c2fe39a49f8aca5492d1da8595d41e91844a";
   };
 
   CXXFLAGS = stdenv.lib.optionalString stdenv.cc.isClang "-std=c++11";
