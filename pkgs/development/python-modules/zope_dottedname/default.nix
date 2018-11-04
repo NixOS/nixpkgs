09f4452a16a6970c41659f6de262c6808b530a53
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:23 +0100

    python: zope.dottedname: 3.4.6 -> 4.3

--- pkgs/development/python-modules/zope_dottedname/default.nix
+++ pkgs/development/python-modules/zope_dottedname/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "zope.dottedname";
-  version = "3.4.6";
+  version = "4.3";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "331d801d98e539fa6c5d50c3835ecc144c429667f483281505de53fc771e6bf5";
+    sha256 = "0cec09844d309550359ac1941abfcd9141e213f67f3c19bb8f90360c40787576";
   };
 
   meta = with stdenv.lib; {
