04a5cec6c77573c7ca3da5a0b0818de7f7853d6c
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:01 +0100

    python: histbook: 1.2.3 -> 1.2.4

--- pkgs/development/python-modules/histbook/default.nix
+++ pkgs/development/python-modules/histbook/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "histbook";
-  version = "1.2.3";
+  version = "1.2.4";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "12d5l4c5pxwac5hzcfif51j87qjljm0w9nd0c8pnhj7q2snap4x4";
+    sha256 = "864b2943005d6be0c889504c6e1090fea2036c9557837e06ca83d06590037c49";
   };
 
   propagatedBuildInputs = [ numpy pandas ];
