3f0dfe7b2019fe293db2c5593f65bea35d124cc8
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:16 +0100

    python: scikit-learn: 0.19.2 -> 0.20.0

--- pkgs/development/python-modules/scikitlearn/default.nix
+++ pkgs/development/python-modules/scikitlearn/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "scikit-learn";
-  version = "0.19.2";
+  version = "0.20.0";
   # UnboundLocalError: local variable 'message' referenced before assignment
   doCheck = false;
   disabled = stdenv.isi686;  # https://github.com/scikit-learn/scikit-learn/issues/5534
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b276739a5f863ccacb61999a3067d0895ee291c95502929b2ae56ea1f882e888";
+    sha256 = "97d1d971f8ec257011e64b7d655df68081dd3097322690afa1a71a1d755f8c18";
   };
 
   buildInputs = [ nose pillow gfortran glibcLocales ];
