761eff933b7a40e671e28e20255dd7a078a93fa6
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:15 +0100

    python: regex: 2018.08.29 -> 2018.11.03

--- pkgs/development/python-modules/regex/default.nix
+++ pkgs/development/python-modules/regex/default.nix
@@ -7,11 +7,11 @@
 
 buildPythonPackage rec {
   pname = "regex";
-  version = "2018.08.29";
+  version = "2018.11.03";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b73cea07117dca888b0c3671770b501bef19aac9c45c8ffdb5bea2cca2377b0a";
+    sha256 = "4a8a645072ff7acc96b1a92c76863e324089cd3fd00bc3e41add4a7016c06761";
   };
 
   postCheck = ''
