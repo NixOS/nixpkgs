cdd4d8b735c8cb48f18990f51afc46ac5be77c7a
Peter Hoeg <peter@hoeg.com>
2018-11-04 19:51:27 +0800

    motion: 4.1.1 -> 4.2

--- pkgs/applications/video/motion/default.nix
+++ pkgs/applications/video/motion/default.nix
@@ -1,23 +1,25 @@
-{ stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, libjpeg, ffmpeg }:
+{ stdenv, fetchFromGitHub, autoreconfHook, pkgconfig
+, ffmpeg, libjpeg, libmicrohttpd }:
 
 stdenv.mkDerivation rec {
   name = "motion-${version}";
-  version = "4.1.1";
+  version = "4.2";
 
   src = fetchFromGitHub {
-    owner = "Motion-Project";
-    repo = "motion";
-    rev = "release-${version}";
-    sha256 = "1prbgl9wb9q7igsb6n11c25m0p0z246fxr1q8n1vcjr4rcb65y38";
+    owner  = "Motion-Project";
+    repo   = "motion";
+    rev    = "release-${version}";
+    sha256 = "0c0q6dl4v561m5y8bp0c0h4p3s52fjgcdnsrrf5ygdi288d3rfxv";
   };
 
   nativeBuildInputs = [ autoreconfHook pkgconfig ];
-  buildInputs = [ libjpeg ffmpeg ];
+
+  buildInputs = [ ffmpeg libjpeg libmicrohttpd ];
 
   meta = with stdenv.lib; {
-    homepage = http://www.lavrsen.dk/foswiki/bin/view/Motion/WebHome;
     description = "Monitors the video signal from cameras";
+    homepage = https://motion-project.github.io/;
     license = licenses.gpl2Plus;
-    maintainers = [ maintainers.puffnfresh ];
+    maintainers = with maintainers; [ puffnfresh ];
   };
 }
