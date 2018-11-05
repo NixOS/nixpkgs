0a74322a5dc52e71e0bdb28c3220d6beb821a6d0
Peter Simons <simons@cryp.to>
2018-11-05 09:47:25 +0100

    haskell-hspec: update overrides for the new version some more

--- pkgs/development/haskell-modules/configuration-ghc-8.6.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.6.x.nix
@@ -59,9 +59,9 @@ self: super: {
   hpack = self.hpack_0_31_1;
   hslua = self.hslua_1_0_1;
   hslua-module-text = self.hslua-module-text_0_2_0;
-  hspec = self.hspec_2_5_9;
-  hspec-core = self.hspec-core_2_5_9;
-  hspec-discover = self.hspec-discover_2_5_9;
+  hspec = self.hspec_2_6_0;
+  hspec-core = self.hspec-core_2_6_0;
+  hspec-discover = self.hspec-discover_2_6_0;
   hspec-megaparsec = doJailbreak super.hspec-megaparsec;  # newer versions need megaparsec 7.x
   hspec-meta = self.hspec-meta_2_5_6;
   JuicyPixels = self.JuicyPixels_3_3_2;
