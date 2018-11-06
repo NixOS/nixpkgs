335941d29f0dd6cd5dfe79296b9efde658a93083
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-11-05 22:12:40 -0800

    gocode: 2018-10-22 -> 2018-11-05

--- pkgs/development/tools/gocode/deps.nix
+++ pkgs/development/tools/gocode/deps.nix
@@ -4,8 +4,8 @@
     fetch = {
       type = "git";
       url = "https://go.googlesource.com/tools";
-      rev = "6fe81c087942f588f40c3f67b41ce284f2f70eee";
-      sha256 = "04yl7rk2lf94bxz74ja5snh7ava9gcnf2yx6y002pfkk538r6w5d";
+      rev = "78dc5bac0cacea7969e98b79c3b86597e0aa4e25";
+      sha256 = "16jg2x1sfm39kz4rchn0gxyq99fnkxw6v51wxriqbs76a2wrznp9";
     };
   }
 ]
