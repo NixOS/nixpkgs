6f0c65ee3f72cc4f1c3195610fc285a2ed441c0b
Will Dietz <w@wdtz.org>
2018-11-05 22:41:29 -0600

    sqlite: 3.25.2 -> 3.25.3
    https://sqlite.org/releaselog/3_25_3.html

--- pkgs/development/libraries/sqlite/default.nix
+++ pkgs/development/libraries/sqlite/default.nix
@@ -10,12 +10,12 @@ in
 
 stdenv.mkDerivation rec {
   name = "sqlite-${version}";
-  version = "3.25.2";
+  version = "3.25.3";
 
   # NB! Make sure to update analyzer.nix src (in the same directory).
   src = fetchurl {
     url = "https://sqlite.org/2018/sqlite-autoconf-${archiveVersion version}.tar.gz";
-    sha256 = "109i1sfryghrdaynkqq9s9aq4347vy653bwkqwx4slix8a2196ns";
+    sha256 = "1pgkja0d13qp5p79ik9kh9lm5y79cwyxwwfc80cr8a1rw5xzksq0";
   };
 
   outputs = [ "bin" "dev" "out" ];
