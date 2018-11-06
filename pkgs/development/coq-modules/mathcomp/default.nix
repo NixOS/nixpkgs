993bd5df2240ef9223dd0b969ccd5f5fc7495526
Jörg Thalheim <joerg@thalheim.io>
2018-11-06 13:45:14 +0000

    coqPackages.mathcomp: use fetchFromGitHub

--- pkgs/development/coq-modules/mathcomp/default.nix
+++ pkgs/development/coq-modules/mathcomp/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, coq, ncurses, which
+{ stdenv, fetchFromGitHub, coq, ncurses, which
 , graphviz, withDoc ? false
 }:
 
@@ -7,12 +7,12 @@ let param =
   if stdenv.lib.versionAtLeast coq.coq-version "8.6" then
   {
     version = "1.7.0";
-    sha256 = "05zgyi4wmasi1rcyn5jq42w0bi9713q9m8dl1fdgl66nmacixh39";
+    sha256 = "0wnhj9nqpx2bw6n1l4i8jgrw3pjajvckvj3lr4vzjb3my2lbxdd1";
   }
   else if stdenv.lib.versionAtLeast coq.coq-version "8.5" then
   {
     version = "1.6.1";
-    sha256 = "1j9ylggjzrxz1i2hdl2yhsvmvy5z6l4rprwx7604401080p5sgjw";
+    sha256 = "1ilw6vm4dlsdv9cd7kmf0vfrh2kkzr45wrqr8m37miy0byzr4p9i";
   }
   else throw "No version of math-comp is available for Coq ${coq.coq-version}";
 
@@ -21,8 +21,10 @@ in
 stdenv.mkDerivation {
   name = "coq${coq.coq-version}-mathcomp-${param.version}";
 
-  src = fetchurl {
-    url = "https://github.com/math-comp/math-comp/archive/mathcomp-${param.version}.tar.gz";
+  src = fetchFromGitHub {
+    owner = "math-comp";
+    repo = "math-comp";
+    rev = "mathcomp-${param.version}";
     inherit (param) sha256;
   };
 
