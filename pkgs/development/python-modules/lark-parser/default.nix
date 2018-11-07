becf8879f4af23db52038e0796a21aab229a723d
Florian Klink <flokli@flokli.de>
2018-11-07 10:56:22 +0100

    lark-parser: 2017-12-18 -> 0.6.5
    This bumps lark-parser to a released version that doesn't loop forever
    and consumes all memory on certain grammars.
    
    erezsh/lark redirects now to lark-parser/lark, so also update homepage.

--- pkgs/development/python-modules/lark-parser/default.nix
+++ pkgs/development/python-modules/lark-parser/default.nix
@@ -5,14 +5,14 @@
 }:
 
 buildPythonPackage rec {
-  pname = "lark-parser"; # PyPI name
-  version = "2017-12-18";
+  pname = "lark-parser";
+  version = "0.6.5";
 
   src = fetchFromGitHub {
-    owner = "erezsh";
+    owner = "lark-parser";
     repo = "lark";
-    rev = "9d6cde9b1ba971f02ea8106fa3b71a934e83d6fa";
-    sha256 = "0nv6nxd8wx9dwhn37m94fkc10gknckrjs1hzajxygla3dpql455j";
+    rev = version;
+    sha256 = "0mf10xm9blqik8mwrpw0r07vqlk2y4r98yqvk1sq849zqlxmqpsr";
   };
 
   checkPhase = ''
@@ -23,7 +23,7 @@ buildPythonPackage rec {
 
   meta = {
     description = "A modern parsing library for Python, implementing Earley & LALR(1) and an easy interface";
-    homepage = https://github.com/erezsh/lark;
+    homepage = https://github.com/lark-parser/lark;
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fridh ];
   };
