9dd7fde27f73b3ff3ecd34a2d0ffedf1aa3a82ce
Robert Hensing <robert@roberthensing.nl>
2018-11-08 00:53:36 +0100

    acsccid: init at 1.1.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9043,6 +9043,8 @@ with pkgs;
 
   acl = callPackage ../development/libraries/acl { };
 
+  acsccid = callPackage ../tools/security/acsccid { };
+
   activemq = callPackage ../development/libraries/apache-activemq { };
 
   adns = callPackage ../development/libraries/adns { };
