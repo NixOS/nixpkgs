0f7f83a25e4916f150f952237a4c88a037a1e88d
wucke13 <wucke13@gmail.com>
2018-11-07 14:24:19 +0100

    betaflight-configurator: init at 10.5.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23006,6 +23006,8 @@ in
 
   apmplanner2 = libsForQt511.callPackage ../applications/science/robotics/apmplanner2 { };
 
+  betaflight-configurator = callPackage ../applications/science/robotics/betaflight-configurator { };
+
   ### MISC
 
   acpilight = callPackage ../misc/acpilight { };
