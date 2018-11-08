c403df65d56be080d8de98d72742d5e015835f49
R. RyanTM <ryantm-bot@ryantm.com>
2018-11-08 10:14:28 -0800

    syncplay: 1.5.5 -> 1.6.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/syncplay/versions

--- pkgs/applications/networking/syncplay/default.nix
+++ pkgs/applications/networking/syncplay/default.nix
@@ -2,13 +2,13 @@
 
 python2Packages.buildPythonApplication rec {
   name = "syncplay-${version}";
-  version = "1.5.5";
+  version = "1.6.0";
 
   format = "other";
 
   src = fetchurl {
-    url = https://github.com/Syncplay/syncplay/archive/v1.5.5.tar.gz;
-    sha256 = "0g12hm84c48fjrmwljl0ii62f55vm6fk2mv8vna7fadabmk6dwhr";
+    url = https://github.com/Syncplay/syncplay/archive/v1.6.0.tar.gz;
+    sha256 = "19x7b694p8b3qp578qk8q4g0pybhfjd4zk8rgrggz40s1yyfnwy5";
   };
 
   propagatedBuildInputs = with python2Packages; [ pyside twisted ];
