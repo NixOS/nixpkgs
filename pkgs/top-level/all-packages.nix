4cd4f7a2c52231856e64ea1c19b2478b57c4c0c6
Renaud <c0bw3b@users.noreply.github.com>
2018-11-08 22:59:17 +0100

    qbs: 1.8 -> 1.12.1
    (#49869)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11849,7 +11849,7 @@ with pkgs;
 
   re2 = callPackage ../development/libraries/re2 { };
 
-  qbs = callPackage ../development/tools/build-managers/qbs { };
+  qbs = libsForQt5.callPackage ../development/tools/build-managers/qbs { };
 
   qca2 = callPackage ../development/libraries/qca2 { qt = qt4; };
   qca2-qt5 = qca2.override { qt = qt5.qtbase; };
