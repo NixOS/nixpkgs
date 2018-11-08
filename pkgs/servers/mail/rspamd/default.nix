b73b02255edde30d9c4f21c2afd05c27ad266074
Alexander V. Nikolaev <avn@avnik.info>
2018-11-08 04:04:08 +0200

    rspamd: enable hyperscan

--- pkgs/servers/mail/rspamd/default.nix
+++ pkgs/servers/mail/rspamd/default.nix
@@ -1,10 +1,13 @@
 { stdenv, lib, fetchFromGitHub, cmake, perl
 , file, glib, libevent, luajit, openssl, pcre, pkgconfig, sqlite, ragel, icu
-, libfann, gd, jemalloc, openblas
+, hyperscan, libfann, gd, jemalloc, openblas
 , withFann ? true
 , withGd ? false
 , withBlas ? true
+, withHyperscan ? stdenv.isx86_64
 }:
+
+assert withHyperscan -> stdenv.isx86_64;
 
 let libmagic = file;  # libmagic provided by file package ATM
 in
@@ -24,6 +27,7 @@ stdenv.mkDerivation rec {
   buildInputs = [ glib libevent libmagic luajit openssl pcre sqlite ragel icu jemalloc ]
     ++ lib.optional withFann libfann
     ++ lib.optional withGd gd
+    ++ lib.optional withHyperscan hyperscan
     ++ lib.optional withBlas openblas;
 
   cmakeFlags = [
@@ -34,6 +38,7 @@ stdenv.mkDerivation rec {
     "-DLOCAL_CONFDIR=/etc/rspamd"
     "-DENABLE_JEMALLOC=ON"
   ] ++ lib.optional withFann "-DENABLE_FANN=ON"
+    ++ lib.optional withHyperscan "-DENABLE_HYPERSCAN=ON"
     ++ lib.optional withGd "-DENABLE_GD=ON";
 
   meta = with stdenv.lib; {
