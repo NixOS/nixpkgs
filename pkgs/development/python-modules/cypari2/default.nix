d5c7b7943052c4bbafa5999273003ddcec697177
R. RyanTM <ryantm-bot@ryantm.com>
2018-11-09 02:55:08 -0800

    python36Packages.cypari2: 1.3.1 -> 2.0.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.6-cypari2/versions

--- pkgs/development/python-modules/cypari2/default.nix
+++ pkgs/development/python-modules/cypari2/default.nix
@@ -12,11 +12,11 @@
 buildPythonPackage rec {
   pname = "cypari2";
   # upgrade may break sage, please test the sage build or ping @timokau on upgrade
-  version = "1.3.1";
+  version = "2.0.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "04f00xp8aaz37v00iqg1mv5wjq00a5qhk8cqa93s13009s9x984r";
+    sha256 = "0yj2wym78g7cl5w6cf2k405zg2116i9h8iqv8fnd7pqphakxh4im";
   };
 
   # This differs slightly from the default python installPhase in that it pip-installs
