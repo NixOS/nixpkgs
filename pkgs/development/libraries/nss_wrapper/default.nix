cbf91ed2c97f009e3906e410db5d72ab81b5bd54
R. RyanTM <ryantm-bot@ryantm.com>
2018-11-09 05:56:52 -0800

    nss_wrapper: 1.1.3 -> 1.1.5
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/nss_wrapper/versions

--- pkgs/development/libraries/nss_wrapper/default.nix
+++ pkgs/development/libraries/nss_wrapper/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, cmake, pkgconfig }:
 
 stdenv.mkDerivation rec {
-  name = "nss_wrapper-1.1.3";
+  name = "nss_wrapper-1.1.5";
 
   src = fetchurl {
     url = "mirror://samba/cwrap/${name}.tar.gz";
-    sha256 = "18rsaw8r8xwn5003arc7xw8iliwbmzxfxgacmp6lhsdwqla4rf69";
+    sha256 = "0srk7hffhkwl811q4pjy0n0gm8rh26akb2bh6avcpbns0zzn9ldn";
   };
 
   nativeBuildInputs = [ pkgconfig ];
