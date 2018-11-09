089e32d7fe11108b5a790873667dfa9074862039
R. RyanTM <ryantm-bot@ryantm.com>
2018-11-09 01:46:03 -0800

    python36Packages.flask-pymongo: 2.1.0 -> 2.2.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.6-flask-pymongo/versions

--- pkgs/development/python-modules/Flask-PyMongo/default.nix
+++ pkgs/development/python-modules/Flask-PyMongo/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "Flask-PyMongo";
-  version = "2.1.0";
+  version = "2.2.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0b99dd99985660ebbc4b34bb44550f88a527cbc573faa01febccce3c4ab28347";
+    sha256 = "0yi1r13p3l1d5dpdfnyp239l6l17nwvyky8y62nmmqxlsp2ja9hi";
   };
 
   checkInputs = [ pytest ];
