2fee653112a04e3396c01bbb3abf01d33eb59f4a
R. RyanTM <ryantm-bot@ryantm.com>
2018-11-10 01:52:19 -0800

    deepin.deepin-icon-theme: 15.12.59 -> 15.12.60
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/deepin-icon-theme/versions

--- pkgs/desktops/deepin/deepin-icon-theme/default.nix
+++ pkgs/desktops/deepin/deepin-icon-theme/default.nix
@@ -3,13 +3,13 @@
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
   pname = "deepin-icon-theme";
-  version = "15.12.59";
+  version = "15.12.60";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "1qkxhqx6a7pahkjhf6m9lm16lw9v9grk0d4j449h9622zwfjkxlq";
+    sha256 = "01iaxi4x4zic67yz9khjwl516wb3rnajcfqrkwaich8209isk6yf";
   };
 
   nativeBuildInputs = [ gtk3 papirus-icon-theme ];
