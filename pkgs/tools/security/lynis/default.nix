eb79c24587c8b401a9aad0d12b23e067d0e02634
Renaud <c0bw3b@users.noreply.github.com>
2018-11-10 14:21:05 +0100

    lynis: remove perl and create plugins subdir

--- pkgs/tools/security/lynis/default.nix
+++ pkgs/tools/security/lynis/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, makeWrapper, fetchFromGitHub, gawk, perl }:
+{ stdenv, makeWrapper, fetchFromGitHub, gawk }:
 
 stdenv.mkDerivation rec {
   pname = "lynis";
@@ -12,18 +12,15 @@ stdenv.mkDerivation rec {
     sha256 = "0rzc0y8lk22bymf56249jzmllki2lh0rz5in4lkrc5fkmp29c2wv";
   };
 
-  nativeBuildInputs = [ makeWrapper perl ];
+  nativeBuildInputs = [ makeWrapper ];
 
   postPatch = ''
     grep -rl '/usr/local/lynis' ./ | xargs sed -i "s@/usr/local/lynis@$out/share/lynis@g"
-    # Don't use predefined binary paths. See https://github.com/CISOfy/lynis/issues/468
-    perl -i -p0e 's/BIN_PATHS="[^"]*"/BIN_PATHS=\$\(echo \$PATH\ | sed "s\/:\/ \/g")/sm;' include/consts
   '';
 
   installPhase = ''
-    mkdir -p $out/share/lynis
+    install -d $out/bin $out/share/lynis/plugins
     cp -r include db default.prf $out/share/lynis/
-    mkdir -p $out/bin
     cp -a lynis $out/bin
     wrapProgram "$out/bin/lynis" --prefix PATH : ${stdenv.lib.makeBinPath [ gawk ]}
   '';
