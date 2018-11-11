e6015447070ee6edb84fa778c336b5c645f9a59d
Frederik Rietdijk <fridh@fridh.nl>
2018-11-11 14:47:25 +0100

    python: httpretty: 0.9.5 -> 0.9.6

--- pkgs/development/python-modules/httpretty/default.nix
+++ pkgs/development/python-modules/httpretty/default.nix
@@ -14,32 +14,17 @@
 
 buildPythonPackage rec {
   pname = "httpretty";
-  version = "0.9.5";
+  version = "0.9.6";
   doCheck = false;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "543fa2bd9c319bfa1e1de9e37d7c9c08fa926a692b65b0be5df4b2f069fd0ad7";
+    sha256 = "01b52d45077e702eda491f4fe75328d3468fd886aed5dcc530003e7b2b5939dc";
   };
 
-  buildInputs = [ tornado requests httplib2 sure nose coverage certifi ];
+  checkInputs = [ tornado requests httplib2 sure nose coverage certifi ];
   propagatedBuildInputs = [ urllib3 ];
 
-  postPatch = ''
-    sed -i -e 's/==.*$//' *requirements.txt
-    # XXX: Drop this after version 0.8.4 is released.
-    patch httpretty/core.py <<DIFF
-    ***************
-    *** 566 ****
-    !                 'content-length': len(self.body)
-    --- 566 ----
-    !                 'content-length': str(len(self.body))
-    DIFF
-
-    # Explicit encoding flag is required with python3, unless locale is set.
-    ${if !isPy3k then "" else
-      "patch -p0 -i ${./setup.py.patch}"}
-  '';
 
   meta = with stdenv.lib; {
     homepage = "https://falcao.it/HTTPretty/";
