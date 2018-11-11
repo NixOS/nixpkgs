a548b6f1317786962878b108ba442a04ac83020f
Frederik Rietdijk <fridh@fridh.nl>
2018-11-11 14:47:27 +0100

    python: ruamel.yaml: 0.15.76 -> 0.15.77

--- pkgs/development/python-modules/ruamel_yaml/default.nix
+++ pkgs/development/python-modules/ruamel_yaml/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "ruamel.yaml";
-  version = "0.15.76";
+  version = "0.15.77";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "9dc2a9869f45ace93bb8ecc83a308498ecf9aabd4e54561280c33d29f1f3546d";
+    sha256 = "aa19db7cc000c863f7f887a9d2268aafe4e04e4767befc63cab58f1de7ec1fd6";
   };
 
   # Tests cannot load the module to test
