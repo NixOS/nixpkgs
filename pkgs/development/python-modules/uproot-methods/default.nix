cad04c42a283eb9562cae35cc6c1a134813382b8
Frederik Rietdijk <fridh@fridh.nl>
2018-11-11 14:47:27 +0100

    python: uproot-methods: 0.2.6 -> 0.2.7

--- pkgs/development/python-modules/uproot-methods/default.nix
+++ pkgs/development/python-modules/uproot-methods/default.nix
@@ -6,12 +6,12 @@
 }:
 
 buildPythonPackage rec {
-  version = "0.2.6";
+  version = "0.2.7";
   pname = "uproot-methods";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1z8gganf8p68z586zzx440dpkar3djdbc4f7670bkriyix0z6lxn";
+    sha256 = "dc687d2144ff7b55e6858b4865beb15ae7a25d274212fb4436ead982993e2f31";
   };
 
   propagatedBuildInputs = [ numpy awkward ];
