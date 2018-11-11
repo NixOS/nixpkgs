31e148a31cb87919800eed23b8492dcaa4cd904e
Frederik Rietdijk <fridh@fridh.nl>
2018-11-11 14:47:26 +0100

    python: pbr: 5.1.0 -> 5.1.1

--- pkgs/development/python-modules/pbr/default.nix
+++ pkgs/development/python-modules/pbr/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "pbr";
-  version = "5.1.0";
+  version = "5.1.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f20ec0abbf132471b68963bb34d9c78e603a5cf9e24473f14358e66551d47475";
+    sha256 = "f59d71442f9ece3dffc17bc36575768e1ee9967756e6b6535f0ee1f0054c3d68";
   };
 
   # circular dependencies with fixtures
