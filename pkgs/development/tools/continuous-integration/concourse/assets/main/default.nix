23bbd12dfa5e20bb6b9b16c00c82c347e1599b25
Ding Xiang Fei <dingxiangfei2009@gmail.com>
2018-11-12 13:04:56 +0800

    add libnss to elm runtime dependencies

--- pkgs/development/tools/continuous-integration/concourse/assets/main/default.nix
+++ pkgs/development/tools/continuous-integration/concourse/assets/main/default.nix
@@ -1,22 +1,25 @@
 {
+  cacert,
   gmp,
+  mkYarnPackage,
   nodePackages,
+  nss,
   patchelf,
   src,
   stdenv,
   zlib,
-  mkYarnPackage,
 }:
 mkYarnPackage {
   name = "concourse-main-assets";
-  nativeBuildInputs = [ nodePackages.yarn patchelf ];
+  nativeBuildInputs = [ nodePackages.yarn patchelf cacert ];
+  buildInputs = [ cacert ];
   inherit src;
   yarnNix = ./yarn.nix;
   pkgConfig = {
     elm = {
       postInstall = ''
         find . -type f -executable -exec patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" {} \;
-        find . -type f -executable -exec patchelf --set-rpath "${gmp}/lib:${zlib}/lib" {} \;
+        find . -type f -executable -exec patchelf --set-rpath "${gmp}/lib:${zlib}/lib:${nss}/lib" {} \;
       '';
     };
   };
