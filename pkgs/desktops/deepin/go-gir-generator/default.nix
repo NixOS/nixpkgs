4da3f08a2c6b6ef3fb4723e81c84c1211c9234bc
Tor Hedin Brønner <torhedinbronner@gmail.com>
2018-11-13 12:20:01 +0100

    deepin.go-gir-generator: fix dde-api with gobject-introspection 1.58+

--- pkgs/desktops/deepin/go-gir-generator/default.nix
+++ pkgs/desktops/deepin/go-gir-generator/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchFromGitHub, pkgconfig, go, gobjectIntrospection,
-  libgudev, deepin }:
+  libgudev, deepin, fetchurl }:
 
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
@@ -12,6 +12,14 @@ stdenv.mkDerivation rec {
     rev = version;
     sha256 = "0grp4ffy3vmlknzmymnxq1spwshff2ylqsw82pj4y2v2fcvnqfvb";
   };
+
+  patches = [
+    # fix: dde-api build error with gobject-introspection 1.58+
+    (fetchurl {
+      url = https://github.com/linuxdeepin/go-gir-generator/commit/a7ab229201e28d1be727f5021b3588fa4a1acf5f.patch;
+      sha256 = "13ywalwkjg8wwvd0pvmc2rv1h38airyvimdn9jfb5wis9xm48401";
+    })
+  ];
 
   nativeBuildInputs = [
     pkgconfig
