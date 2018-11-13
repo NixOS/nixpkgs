ba45d51858b7e88764011eec017782a7b4d7d97e
Florian Jacob <projects+git@florianjacob.de>
2018-11-13 10:15:29 +0100

    nixos/container: Make images systemd-nspawn compatible out of the box by adding /etc/os-release to the image and putting init at the /sbin/init standard location.

--- nixos/modules/profiles/docker-container.nix
+++ nixos/modules/profiles/docker-container.nix
@@ -15,13 +15,22 @@ in {
 
   # Create the tarball
   system.build.tarball = pkgs.callPackage ../../lib/make-system-tarball.nix {
+
     contents = [];
+
     extraArgs = "--owner=0";
 
-    # Add init script to image
     storeContents = [
+      # Add init script to image
       { object = config.system.build.toplevel + "/init";
-        symlink = "/init";
+        # use /sbin/init instead of /init as it's the default location of lxc and systemd-nspawn
+        symlink = "/sbin/init";
+      }
+      # Add /etc/os-release from nix store upfront, so that it is there prior first boot.
+      # for docker, this is only informative and not required, but for lxc, which also
+      # imports this code, it is required when started with systemd-nspawn.
+      { object = config.environment.etc."os-release".source;
+        symlink = "/etc/os-release";
       }
     ] ++ (pkgs2storeContents [ pkgs.stdenv ]);
 
@@ -30,6 +39,9 @@ in {
   };
 
   boot.isContainer = true;
+  # TODO: /sbin/init handling could probably done through this,
+  # but this is not enough for /sbin/init to land in the image
+  # boot.loader.initScript.enable = true;
   boot.postBootCommands =
     ''
       # After booting, register the contents of the Nix store in the Nix
@@ -45,6 +57,6 @@ in {
 
   # Install new init script
   system.activationScripts.installInitScript = ''
-    ln -fs $systemConfig/init /init
+    ln -fs --relative $systemConfig/init /sbin/init
   '';
 }
