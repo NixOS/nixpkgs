NixOS/nixpkgs

commit 209f8b1acd4c855d223d93f02caf3ccf6cec7e32

nixos/release*.nix: Clean nixpkgs sources by default
Currently, when building NixOS from a git clone, Nix has to copy
the entire repo at >1GB into the store by default. That is not
necessary and causes a dumping large path message.
If you need the old behaviour for some reason, you will have to
specify it by passing the path to your repo explicitly as the
nixpkgs argument like this:

  --arg nixpkgs '{outPath = ./.; revCount = 56789; shortRev = "gfedcba"; }'

nixos/release-combined.nix
@@ -2,7 +2,7 @@
 # and nixos-14.04). The channel is updated every time the ‘tested’ job
 # succeeds, and all other jobs have finished (they may fail).
 
-{ nixpkgs ? { outPath = ./..; revCount = 56789; shortRev = "gfedcba"; }
+{ nixpkgs ? { outPath = (import ../lib).cleanSource ./..; revCount = 56789; shortRev = "gfedcba"; }
 , stableBranch ? false
 , supportedSystems ? [ "x86_64-linux" ]
 , limitedSupportedSystems ? [ "i686-linux" ]

nixos/release-small.nix
@@ -2,7 +2,7 @@
 # small subset of Nixpkgs, mostly useful for servers that need fast
 # security updates.
 
-{ nixpkgs ? { outPath = ./..; revCount = 56789; shortRev = "gfedcba"; }
+{ nixpkgs ? { outPath = (import ../lib).cleanSource ./..; revCount = 56789; shortRev = "gfedcba"; }
 , stableBranch ? false
 , supportedSystems ? [ "x86_64-linux" ] # no i686-linux
 }:

nixos/release.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? { outPath = ./..; revCount = 56789; shortRev = "gfedcba"; }
+{ nixpkgs ? { outPath = (import ../lib).cleanSource ./..; revCount = 56789; shortRev = "gfedcba"; }
 , stableBranch ? false
 , supportedSystems ? [ "x86_64-linux" "aarch64-linux" ]
 }:

commit b832c46afb4ac93969a2a494a9f8a72affed1693

release: do not cleanSource when evaluating in restrict-eval
The current behavior, breaking restrict-eval=true, had been introduced
in 209f8b1acd4c855d223d93f02caf3ccf6cec7e32. See also [1].

This change uses a hack to identify whether we're evaluating in
restrict-eval, keeping the optimization when not and reverting to the
old behavior when evaluating in restrict-eval.

This should fix the issues that appeared at [2] and [3], that followed
the change making us actually use this `nixpkgs` argument for tests
since 83b27f60ceff23967e477c90bef8e78cc96d50a2 [4].

CC: @roberth

[1] https://github.com/NixOS/nixpkgs/commit/209f8b1acd4c#commitcomment-28419462

[2] https://github.com/NixOS/nixpkgs/pull/50233#issuecomment-438512807

[3] https://github.com/NixOS/nixpkgs/pull/50186

[4] https://github.com/NixOS/nixpkgs/pull/50233

lib/default.nix
@@ -97,7 +97,7 @@ let
       appendToName mapDerivationAttrset lowPrio lowPrioSet hiPrio
       hiPrioSet;
     inherit (sources) pathType pathIsDirectory cleanSourceFilter
-      cleanSource sourceByRegex sourceFilesBySuffices
+      cleanSource cleanSourceForImport sourceByRegex sourceFilesBySuffices
       commitIdFromGitRepo cleanSourceWith pathHasContext
       canCleanSource;
     inherit (modules) evalModules closeModules unifyModuleSyntax

lib/sources.nix
@@ -32,6 +32,18 @@ rec {
   #          cleanSource ./.
   cleanSource = src: cleanSourceWith { filter = cleanSourceFilter; inherit src; };
 
+  # Cleans the source if not operating in restrict-eval. This will make
+  # it possible to import from the cleaned source.
+  #
+  # Example:
+  #          cleanSourceForImport ./..
+  cleanSourceForImport = src:
+    # This is using a very ugly hack to know whether we're evaluating in restrict-mode.
+    # Please don't do this at home.
+    if builtins.pathExists "/"
+    then cleanSource src
+    else src;
+
   # Like `builtins.filterSource`, except it will compose with itself,
   # allowing you to chain multiple calls together without any
   # intermediate copies being put in the nix store.

nixos/release-combined.nix
@@ -2,7 +2,7 @@
 # and nixos-14.04). The channel is updated every time the ‘tested’ job
 # succeeds, and all other jobs have finished (they may fail).
 
-{ nixpkgs ? { outPath = (import ../lib).cleanSource ./..; revCount = 654321; shortRev = "gfedcba"; }
+{ nixpkgs ? { outPath = (import ../lib).cleanSourceForImport ./..; revCount = 654321; shortRev = "gfedcba"; }
 , stableBranch ? false
 , supportedSystems ? [ "x86_64-linux" ]
 , limitedSupportedSystems ? [ "i686-linux" ]

nixos/release-small.nix
@@ -2,7 +2,7 @@
 # small subset of Nixpkgs, mostly useful for servers that need fast
 # security updates.
 
-{ nixpkgs ? { outPath = (import ../lib).cleanSource ./..; revCount = 654321; shortRev = "gfedcba"; }
+{ nixpkgs ? { outPath = (import ../lib).cleanSourceForImport ./..; revCount = 654321; shortRev = "gfedcba"; }
 , stableBranch ? false
 , supportedSystems ? [ "x86_64-linux" ] # no i686-linux
 }:

nixos/release.nix
@@ -1,6 +1,8 @@
 with import ../lib;
 
-{ nixpkgs ? { outPath = cleanSource ./..; revCount = 654321; shortRev = "gfedcba"; }
+# `revCount` and `shortRev` are placeholders, that should be filled
+# automatically for releases by passing the `nixpkgs` argument
+{ nixpkgs ? { outPath = cleanSourceForImport ./..; revCount = 654321; shortRev = "gfedcba"; }
 , stableBranch ? false
 , supportedSystems ? [ "x86_64-linux" "aarch64-linux" ]
 , configuration ? {}