c831e81a0877955d2f58b4b183d35aa72451bfc6
Franz Pletz <fpletz@fnordicwalking.de>
2018-11-14 04:04:21 +0100

    fcgiwrap: add patch to support changing workdir

--- pkgs/servers/fcgiwrap/default.nix
+++ pkgs/servers/fcgiwrap/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, systemd, fcgi, autoreconfHook, pkgconfig }:
+{ stdenv, fetchurl, fetchpatch, systemd, fcgi, autoreconfHook, pkgconfig }:
 
 stdenv.mkDerivation rec {
   name = "fcgiwrap-${version}";
@@ -8,6 +8,15 @@ stdenv.mkDerivation rec {
     url = "http://github.com/gnosek/fcgiwrap/archive/${version}.tar.gz";
     sha256 = "07y6s4mm86cv7p1ljz94sxnqa89y9amn3vzwsnbq5hrl4vdy0zac";
   };
+
+  patches = [
+    # Support for custom working dir with the FCGI_CHDIR environment variable.
+    # https://github.com/gnosek/fcgiwrap/pull/21
+    (fetchpatch {
+      url = "https://github.com/gnosek/fcgiwrap/commit/caf1f5c7b3e9cf5b777139c0419d47fa933ff510.patch";
+      sha256 = "0npdicjvkhpjvd39parrc3wjwd0871d3vz34nwr7p2mk6x9q5dwi";
+    })
+  ];
 
   NIX_CFLAGS_COMPILE = "-Wno-error=implicit-fallthrough";
   configureFlags = [ "--with-systemd" "--with-systemdsystemunitdir=$(out)/etc/systemd/system" ];
