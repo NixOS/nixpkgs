f0bd0fc0949c9b86e52178ae394e581d713cafe0
Ben Sima <ben@bsima.me>
2018-11-16 10:09:55 -0800

    pythonPackages.py-gfm: Add meta

--- pkgs/development/python-modules/py-gfm/default.nix
+++ pkgs/development/python-modules/py-gfm/default.nix
@@ -18,6 +18,12 @@ buildPythonPackage rec {
   meta = with lib; {
     homepage = https://github.com/zopieux/py-gfm;
     description = "GitHub-Flavored Markdown as an extension to the Python Markdown library.";
+    longDescription = ''
+      This is an implementation of GitHub-Flavored Markdown written as an
+      extension to the Python Markdown library. It aims for maximal
+      compatibility with GitHub's rendering.
+    '';
     license = licenses.bsd3;
+    maintainers = [ maintainers.bsima ];
   };
 }
