07253bb9411856cad252e6aa0be2575d7cf46a12
Ben Sima <ben@bsima.me>
2018-11-16 09:27:59 -0800

    pythonPackages.flexmock: Change 'buildInputs' to 'checkInputs'

--- pkgs/development/python-modules/flexmock/default.nix
+++ pkgs/development/python-modules/flexmock/default.nix
@@ -14,7 +14,7 @@ buildPythonPackage rec {
     sha256 = "fe95c8727f4db73dc8f2f7b4548bffe7992440a965fefd60da291abda5352c2b";
   };
 
-  buildInputs = [ nose pytest ];
+  checkInputs = [ nose pytest ];
 
   meta = with lib; {
     homepage = https://flexmock.readthedocs.io/en/latest/;
