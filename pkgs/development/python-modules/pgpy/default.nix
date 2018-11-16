7b513a4e48df22df38e42ff1cfad0ea304f94b7b
Ben Sima <ben@bsima.me>
2018-11-16 09:34:40 -0800

    pythonPackages.pgpy: Change 'buildInputs' to 'propogatedBuildInputs'

--- pkgs/development/python-modules/pgpy/default.nix
+++ pkgs/development/python-modules/pgpy/default.nix
@@ -16,7 +16,7 @@ buildPythonPackage rec {
     sha256 = "04412dddd6882ac0c5d5daf4326c28d481421851a68e25e7ac8e06cc9dc2b902";
   };
 
-  buildInputs = [ cryptography pyasn1 six singledispatch ];
+  propogatedBuildInputs = [ cryptography pyasn1 six singledispatch ];
 
   meta = with lib; {
     homepage = https://github.com/SecurityInnovation/PGPy;
