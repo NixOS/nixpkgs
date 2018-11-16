fd08b98da90fe1ff064f37f4a54284031a2b7469
Maximilian Bosch <maximilian@mbosch.me>
2018-11-16 21:25:30 +0100

    pythonPackages.fs-s3fs: init at 1.0.0
    `fs` is a python-based file system abstraction layer. The new package
    `fs-s3fs` is an implementation of it which stores files inside an S3
    bucket.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2658,6 +2658,8 @@ in {
 
   fs = callPackage ../development/python-modules/fs { };
 
+  fs-s3fs = callPackage ../development/python-modules/fs-s3fs { };
+
   libcloud = callPackage ../development/python-modules/libcloud { };
 
   libgpuarray = callPackage ../development/python-modules/libgpuarray {
