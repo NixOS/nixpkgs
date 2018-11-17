NixOS/nixpkgs

commit b8e0a873c1493e1f3e76738d285d3ff1213c86c8

python36Packages.musicbrainzngs: 0.5 -> 0.6
Semi-automatic update generated by
https://github.com/ryantm/nixpkgs-update tools. This update was made
based on information from
https://repology.org/metapackage/python3.6-musicbrainzngs/versions

pkgs/development/python-modules/musicbrainzngs/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "musicbrainzngs";
-  version = "0.5";
+  version = "0.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "281388ab750d2996e9feca4580fd4215d616a698e02cd6719cb9b8562945c489";
+    sha256 = "1dddarpjawryll2wss65xq3v9q8ln8dan7984l5dxzqx88d2dvr8";
   };
 
   buildInputs = [ pkgs.glibcLocales ];

commit 1be626afba53190191c24f346096f154d9a37354

pythonPackages.musicbrainzngs: remove breaking tests
Some tests rely on networking, breaking sandboxed builds. Remove them.

Fixes: b8e0a873c149 ("python36Packages.musicbrainzngs: 0.5 -> 0.6")

pkgs/development/python-modules/musicbrainzngs/default.nix
@@ -17,6 +17,11 @@ buildPythonPackage rec {
 
   LC_ALL="en_US.UTF-8";
 
+  preCheck = ''
+    # Remove tests that rely on networking (breaks sandboxed builds)
+    rm test/test_submit.py
+  '';
+
   meta = with stdenv.lib; {
     homepage = http://alastair/python-musicbrainz-ngs;
     description = "Python bindings for musicbrainz NGS webservice";