efa3790521351791992c40adfcc107b3ab11421b
Peter Simons <simons@cryp.to>
2018-11-18 16:21:53 +0100

    haskell-HTF: drop obsolete override for ghc 8.6.x

--- pkgs/development/haskell-modules/configuration-ghc-8.6.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.6.x.nix
@@ -64,7 +64,6 @@ self: super: {
   hspec-discover = self.hspec-discover_2_6_0;
   hspec-megaparsec = doJailbreak super.hspec-megaparsec;  # newer versions need megaparsec 7.x
   hspec-meta = self.hspec-meta_2_5_6;
-  HTF = dontCheck super.HTF_0_13_2_5; # https://github.com/skogsbaer/HTF/issues/74
   JuicyPixels = self.JuicyPixels_3_3_2;
   lens = self.lens_4_17;
   megaparsec = dontCheck (doJailbreak super.megaparsec);
