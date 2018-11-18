60ee032c39b60245f2cfa203b9940e656592eeae
Jörg Thalheim <joerg@thalheim.io>
2018-11-18 11:04:03 +0000

    shc: update homepage

--- pkgs/tools/security/shc/default.nix
+++ pkgs/tools/security/shc/default.nix
@@ -13,7 +13,7 @@ stdenv.mkDerivation rec {
   };
 
   meta = with stdenv.lib; {
-    homepage = http://neurobin.github.io/shc;
+    homepage = https://neurobin.org/projects/softwares/unix/shc/;
     description = "Shell Script Compiler";
     platforms = stdenv.lib.platforms.linux;
     license = licenses.gpl3;
