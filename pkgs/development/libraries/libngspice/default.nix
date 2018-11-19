8183d796fb30337724c88d4c54214395e696cf7c
R. RyanTM <ryantm-bot@ryantm.com>
2018-11-19 00:04:20 -0800

    libngspice: 28 -> 29
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/libngspice/versions

--- pkgs/development/libraries/libngspice/default.nix
+++ pkgs/development/libraries/libngspice/default.nix
@@ -3,11 +3,11 @@
 # Note that this does not provide the ngspice command-line utility. For that see
 # the ngspice derivation.
 stdenv.mkDerivation {
-  name = "libngspice-28";
+  name = "libngspice-29";
 
   src = fetchurl {
-    url = "mirror://sourceforge/ngspice/ngspice-28.tar.gz";
-    sha256 = "0rnz2rdgyav16w7wfn3sfrk2lwvvgz1fh0l9107zkcldijklz04l";
+    url = "mirror://sourceforge/ngspice/ngspice-29.tar.gz";
+    sha256 = "0jjwz73naq7l9yhwdqbpnrfckywp2ffkppivxjv8w92zq7xhyvcd";
   };
 
   nativeBuildInputs = [ flex bison ];
