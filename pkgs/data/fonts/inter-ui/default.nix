40c4584e49b0f539aa7450f4f27950fe7f6dc74b
R. RyanTM <ryantm-bot@ryantm.com>
2018-11-19 12:25:07 -0800

    inter-ui: 2.5 -> 3.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/inter-ui/versions

--- pkgs/data/fonts/inter-ui/default.nix
+++ pkgs/data/fonts/inter-ui/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchzip }:
 
 let
-  version = "2.5";
+  version = "3.0";
 in fetchzip {
   name = "inter-ui-${version}";
 
@@ -12,7 +12,7 @@ in fetchzip {
     unzip -j $downloadedFile \*.otf -d $out/share/fonts/opentype
   '';
 
-  sha256 = "1d88y6c9vbjz5siazhavnpfpazfkvpbcbb4pdycbnj03mmx6y07v";
+  sha256 = "02mscwjh3iwd2nl5vah3qcncwkl4jpmi2xxngyf0bhcsmrixi1c6";
 
   meta = with stdenv.lib; {
     homepage = https://rsms.me/inter/;
