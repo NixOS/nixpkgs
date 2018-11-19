1b8862b93e3b48c2e18aa69f6a2f211466cfefb5
R. RyanTM <ryantm-bot@ryantm.com>
2018-11-19 03:13:56 -0800

    lmodern: 2.004.4 -> 2.004.5
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/lmodern/versions

--- pkgs/data/fonts/lmodern/default.nix
+++ pkgs/data/fonts/lmodern/default.nix
@@ -1,9 +1,9 @@
 { stdenv, fetchzip }:
 
 fetchzip {
-  name = "lmodern-2.004.4";
+  name = "lmodern-2.004.5";
 
-  url = mirror://debian/pool/main/l/lmodern/lmodern_2.004.4.orig.tar.gz;
+  url = mirror://debian/pool/main/l/lmodern/lmodern_2.004.5.orig.tar.gz;
 
   postFetch = ''
     tar xzvf $downloadedFile
@@ -11,13 +11,13 @@ fetchzip {
     mkdir -p $out/texmf-dist/
     mkdir -p $out/share/fonts/
 
-    cp -r lmodern-2.004.4/* $out/texmf-dist/
-    cp -r lmodern-2.004.4/fonts/{opentype,type1} $out/share/fonts/
+    cp -r lmodern-2.004.5/* $out/texmf-dist/
+    cp -r lmodern-2.004.5/fonts/{opentype,type1} $out/share/fonts/
 
     ln -s -r $out/texmf* $out/share/
   '';
 
-  sha256 = "13n7ls8ss4sffd6c1iw2wb5hbq642i0fmivm76mbqwf652l002i5";
+  sha256 = "0f3nszrvs1nqyhsvjgrnnlam72sz741fyggg9261n9slrkql1jja";
 
   meta = {
     description = "Latin Modern font";
