db73ad3ecd2197a6aabd9ba3d56517edf1d7b10b
R. RyanTM <ryantm-bot@ryantm.com>
2018-11-19 04:30:56 -0800

    jackett: 0.10.434 -> 0.10.446
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/jackett/versions
    
    (cherry picked from commit 91796d1020296716920cd66896eee360a9ada56b)

--- pkgs/servers/jackett/default.nix
+++ pkgs/servers/jackett/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "jackett-${version}";
-  version = "0.10.434";
+  version = "0.10.446";
 
   src = fetchurl {
     url = "https://github.com/Jackett/Jackett/releases/download/v${version}/Jackett.Binaries.Mono.tar.gz";
-    sha256 = "1vnkppmv7mw2p9bjcfmfxg66g02dq0020ad4z07gbp4dvixpzsnm";
+    sha256 = "1vmgywklax5br3pynjp5b74l2mkmhk3njiccjrl0l7j8ikyar1fw";
   };
 
   buildInputs = [ makeWrapper ];
