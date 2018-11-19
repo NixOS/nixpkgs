7fa4ff0cfda006ae9f7c64217428b7a7ee6eb6de
Johannes Löthberg <johannes@kyriasis.com>
2018-11-19 22:54:13 +0100

    inter-ui: refactored to make the fetchzip hash match upstream zip
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- pkgs/data/fonts/inter-ui/default.nix
+++ pkgs/data/fonts/inter-ui/default.nix
@@ -1,18 +1,19 @@
 { stdenv, fetchzip }:
 
-let
+stdenv.mkDerivation rec {
   version = "2.5";
-in fetchzip {
   name = "inter-ui-${version}";
 
-  url = "https://github.com/rsms/inter/releases/download/v${version}/Inter-UI-${version}.zip";
+  src = fetchzip {
+    url = "https://github.com/rsms/inter/releases/download/v${version}/Inter-UI-${version}.zip";
+    sha256 = "0m81n89s3miaxfbaa2v2hklffq4kk37kg9rrmb9yjy4zjxqyli1f";
+    stripRoot = false;
+  };
 
-  postFetch = ''
+  installPhase = ''
     mkdir -p $out/share/fonts/opentype
-    unzip -j $downloadedFile \*.otf -d $out/share/fonts/opentype
+    cp 'Inter UI (OTF)'/*.otf $out/share/fonts/opentype
   '';
-
-  sha256 = "1d88y6c9vbjz5siazhavnpfpazfkvpbcbb4pdycbnj03mmx6y07v";
 
   meta = with stdenv.lib; {
     homepage = https://rsms.me/inter/;
@@ -22,4 +23,3 @@ in fetchzip {
     maintainers = with maintainers; [ demize ];
   };
 }
-
