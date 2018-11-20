e7dd7f3c3dd0ac39f4b9e08aa10118256dd8eab3
Frederik Rietdijk <fridh@fridh.nl>
2018-11-20 11:58:37 +0100

    python: WebOb: 1.8.3 -> 1.8.4

--- pkgs/development/python-modules/webob/default.nix
+++ pkgs/development/python-modules/webob/default.nix
@@ -7,11 +7,11 @@
 
 buildPythonPackage rec {
   pname = "WebOb";
-  version = "1.8.3";
+  version = "1.8.4";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b0853dad347ca3777755b6d0659bb45efbeea71f995d8a395291ef6ad5d4f8b2";
+    sha256 = "a48315158db05df0c47fbdd061b57ba0ba85bdd0b6ea9dca87511b4b7c798e99";
   };
 
   propagatedBuildInputs = [ nose pytest ];
