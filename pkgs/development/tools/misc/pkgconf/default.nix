5efb4643e2ea36bf9b49a1848a436428cd568dc2
Francesco Zanini <francesco@zanini.me>
2018-11-20 09:52:13 +0100

    Use distfiles

--- pkgs/development/tools/misc/pkgconf/default.nix
+++ pkgs/development/tools/misc/pkgconf/default.nix
@@ -1,21 +1,12 @@
-{ stdenv, fetchgit, automake, autoconf, libtool }:
+{ stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
   name = "pkgconf-1.5.4";
 
-  src = fetchgit {
-    url = "https://git.dereferenced.org/pkgconf/pkgconf.git";
-    rev = "74133eda31bc1ed5947b4a3a854001e320b6c1fe";
-    sha256 = "159fxbwm5shz8p95jp28wrjvinlhmp42dy60pqg34psjn41wq1q4";
+  src = fetchurl {
+    url = "https://distfiles.dereferenced.org/pkgconf/${name}.tar.xz";
+    sha256 = "0r26qmij9lxpz183na3dxj6lamcma94cjhasy19fya44w2j68n4w";
   };
-
-  buildInputs = [ automake autoconf libtool ];
-
-  preConfigurePhases = ["autogenPhase"];
-
-  autogenPhase = ''
-    ./autogen.sh
-  '';
 
   meta = with stdenv.lib; {
     description = "Package compiler and linker metadata toolkit";
