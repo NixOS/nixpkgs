5887c3d6af9802e99172771909c0a32c25a78af4
Frederik Rietdijk <fridh@fridh.nl>
2018-11-20 11:58:36 +0100

    python: node-semver: 0.5.0 -> 0.5.1

--- pkgs/development/python-modules/node-semver/default.nix
+++ pkgs/development/python-modules/node-semver/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchPypi, buildPythonPackage, pytest }:
 
 buildPythonPackage rec {
-  version = "0.5.0";
+  version = "0.5.1";
   pname = "node-semver";
 
   checkInputs = [ pytest ];
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1717prxvnxhndjyxasr1lcbnfk9i1dy1cnchvw7zk67h7kmw3by7";
+    sha256 = "b87e335179d874a3dd58041198b2715ae70fd20eba81683acde3553c51b28f8e";
   };
 
   meta = with stdenv.lib; {
