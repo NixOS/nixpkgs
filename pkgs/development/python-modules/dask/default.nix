8750843c236e636a97dae035b9c92f6a5a8f5175
Frederik Rietdijk <fridh@fridh.nl>
2018-11-20 11:58:35 +0100

    python: dask: 0.20.1 -> 0.20.2

--- pkgs/development/python-modules/dask/default.nix
+++ pkgs/development/python-modules/dask/default.nix
@@ -12,11 +12,11 @@
 
 buildPythonPackage rec {
   pname = "dask";
-  version = "0.20.1";
+  version = "0.20.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1587202f46ffba3861a18f99195139789b5d810de42bfb940baf75eff0cf769d";
+    sha256 = "d4139a03ab5eb6cedeb06cf1e39af90fb5226ca214d77408def7677d7e6b7af3";
   };
 
   checkInputs = [ pytest ];
