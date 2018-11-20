403f1d98bd8711e0e1076acf9ab4e100548035fd
Frederik Rietdijk <fridh@fridh.nl>
2018-11-20 11:58:36 +0100

    python: openpyxl: 2.5.9 -> 2.5.10

--- pkgs/development/python-modules/openpyxl/default.nix
+++ pkgs/development/python-modules/openpyxl/default.nix
@@ -9,15 +9,21 @@
 
 buildPythonPackage rec {
   pname = "openpyxl";
-  version = "2.5.9";
+  version = "2.5.10";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "022c0f3fa1e873cc0ba20651c54dd5e6276fc4ff150b4060723add4fc448645e";
+    sha256 = "41eb21a5620343d715b38081536c4ed3c37249afb72e569fd2af93852ed4ddde";
   };
 
   checkInputs = [ pytest ];
   propagatedBuildInputs = [ jdcal et_xmlfile lxml ];
+
+  postPatch = ''
+    # LICENSE.rst is missing, and setup.cfg currently doesn't contain anything useful anyway
+    # This should likely be removed in the next update
+    rm setup.cfg
+  '';
 
   # Tests are not included in archive.
   # https://bitbucket.org/openpyxl/openpyxl/issues/610
