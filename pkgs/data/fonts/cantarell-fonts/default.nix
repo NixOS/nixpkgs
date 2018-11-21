3a3afc739f18ea03b8437c0511e799cd489fa6f0
Tor Hedin Brønner <torhedinbronner@gmail.com>
2018-11-21 14:39:51 +0100

    cantarell-fonts: ad-hoc fix for build
    `GETTEXTDATADIRS` isn't populated correctly due to empty buildInputs. Borrow from
    `GETTEXTDATADIRS_FOR_BUILD` until we fix gettext's envHook.
    
    see: https://github.com/NixOS/nixpkgs/issues/50855

--- pkgs/data/fonts/cantarell-fonts/default.nix
+++ pkgs/data/fonts/cantarell-fonts/default.nix
@@ -13,6 +13,12 @@ in stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ meson ninja gettext appstream-glib ];
 
+  # ad-hoc fix for https://github.com/NixOS/nixpkgs/issues/50855
+  # until we fix gettext's envHook
+  preBuild = ''
+    export GETTEXTDATADIRS="$GETTEXTDATADIRS_FOR_BUILD"
+  '';
+
   outputHashAlgo = "sha256";
   outputHashMode = "recursive";
   outputHash = "052nxmhw2j8yvcj90r8xhjf0mzim8h6syip7winxb28vavj6jnba";
