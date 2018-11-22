6f64552851f006b78aaf02a9bc804d4b89ac062a
hark <hark@puscii.nl>
2018-11-22 01:10:28 +0100

    use darcs repo and some other fixes

--- pkgs/tools/audio/jack-tools/default.nix
+++ pkgs/tools/audio/jack-tools/default.nix
@@ -1,33 +1,36 @@
 { stdenv, fetchgit, fetchdarcs, pkgconfig, libjack2, alsaLib, libGL, libGLU
-,freeglut, libsndfile, libsamplerate, liblo, ncurses, libtool, xorg, asciidoc, libxslt, libxml2 }:
+,freeglut, libsndfile, libsamplerate, liblo, ncurses, libtool, xorg, asciidoc, libxslt, libxml2, libpng }:
 
-stdenv.mkDerivation rec {
-  name = "jack-tools-${version}";
-  version = "20131226";
+let
+  
+srcs = {
 
-  src = fetchdarcs {
+  rju = fetchdarcs {
     url = http://rohandrape.net/sw/rju/;
-   sha256 = "1h40hgmw51w2145xfng2dy0r5m5pl6mnl4hhwrajzfxc5g35hs31";
-
+    sha256 = "1h40hgmw51w2145xfng2dy0r5m5pl6mnl4hhwrajzfxc5g35hs31";
   };
 
   c-common = fetchdarcs {
     url = http://rohandrape.net/sw/c-common/;
-sha256 = "1h40hgmw51w2145xfng2dy0r5m5pl6mnl4hhwrajzfxc5g35hs31";
-
-
+    sha256 = "06rjlnhin99hgxh0h560q95szhzbvr7l4mn3x38q9704k7nzj3q4";
   };
-/*
-  src = fetchgit {
-    url    = https://salsa.debian.org/multimedia-team/jack-tools.git;
-    rev    = "6f2a666d6859e7ef764145a73bdd5985ff850173";
-    sha256 = "003pnyqbhncal5azf8qznz34mja4ywbgbmfkdfyizz12j4awscw4";
-  };
-*/
 
-#  nativeBuildInputs = [ pkgconfig ];
+};
+
+
+in stdenv.mkDerivation rec {
+  name = "jack-tools-${version}";
+  version = "unstable-2018-09-2";
+
+  src = srcs.rju;
+
+  postUnpack = ''
+    cp -r ${srcs.c-common} $sourceRoot/cmd/c-common
+    chmod -R +rw $sourceRoot/cmd/c-common
+  '';
  
- buildInputs = [
+ nativeBuildInputs = [
+    pkgconfig
     libjack2
     alsaLib
     libGL
@@ -43,44 +46,48 @@ sha256 = "1h40hgmw51w2145xfng2dy0r5m5pl6mnl4hhwrajzfxc5g35hs31";
     asciidoc
     libxslt
     libxml2 
+    libpng
   ];
 
-#  NIX_CFLAGS_COMPILE = "-fpermissive";
 
-#  configureFlags = [
-#    "--with-faac-prefix=${faac}"
-#    "--with-lame-prefix=${lame.lib}"
-#  ];
-
-/*
   buildPhase = ''
-    make mk-local-c-common
+    cd cmd/c-common
     make
+    cd ../ 
+    make jack-data 
+    make jack-dl 
+    make jack-osc 
+    make jack-play 
+    make jack-play-sc3 
+    make jack-plumbing 
+    make jack-record 
+    make jack-scope 
+    make jack-transport 
+    make jack-udp 
+
   '';
 
   installPhase = ''
     mkdir -p $out/bin
+    cp jack-data $out/bin
     cp jack-dl $out/bin
     cp jack-osc $out/bin
     cp jack-play $out/bin
+    cp jack-play-sc3 $out/bin
     cp jack-plumbing $out/bin
     cp jack-record $out/bin
     cp jack-scope $out/bin
     cp jack-transport $out/bin
     cp jack-udp $out/bin
-    cp jack-dl $out/bin
-
 
   '';
-
- patches = [ ./jack-tools-curses.patch ];
-*/
-#  enableParallelBuilding = true;
 
   meta = {
     homepage = http://rd.slavepianos.org/r/d/darcsweb.cgi?r=rju;
     description = "jackd utilities";
     license = stdenv.lib.licenses.gpl3;
     maintainers = with stdenv.lib.maintainers; [ hark ];
+    platforms = stdenv.lib.platforms.linux;
+
   };
 }
