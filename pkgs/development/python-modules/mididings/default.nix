36d85140813076f35b28e6554022057287a84319
hark <hark@puscii.nl>
2018-11-22 01:48:38 +0100

    some fixes

--- pkgs/development/python-modules/mididings/default.nix
+++ pkgs/development/python-modules/mididings/default.nix
@@ -16,43 +16,36 @@ buildPythonPackage rec {
   pname = "mididings";
   version = "unstable-2015-11-17";
 
-
   patches = [ ./0001-add-boost-suffix.patch ];
 
   LIBRARY_PATH="${(pkgs.boost.override {enablePython = true; inherit python; })}/lib";
   
   nativeBuildInputs = [ 
-	pkgconfig 
-	];
+    pkgconfig 
+  ];
 
   buildInputs = with pkgs; [
-      (boost.override {
-        enablePython = true;
-        inherit python;
-      })
-  	 glib
-         libjack2
-	 alsaLib
-    	pkgconfig 
-    ];
-
-  runtimeDeps = [
-    python36Packages.decorator
+    (boost.override {
+      enablePython = true;
+      inherit python;
+    })
+    glib
+    libjack2
+    alsaLib
   ];
-   propagatedBuildInputs = [ python36Packages.pytest 
- 		   python36Packages.decorator 
-		   python36Packages.pyliblo 
-                   python36Packages.dbus-python
-                   python36Packages.pyinotify 
-                   python36Packages.tkinter  ];
+
+  propagatedBuildInputs = [
+    python36Packages.decorator 
+    python36Packages.pyliblo 
+    python36Packages.dbus-python
+    python36Packages.pyinotify 
+    python36Packages.tkinter  
+  ];
 
 
-   checkInputs = [ python36Packages.pytest 
- 		   python36Packages.decorator 
-		   python36Packages.pyliblo 
-                   python36Packages.dbus-python
-                   python36Packages.pyinotify 
-                   python36Packages.tkinter  ];
+  checkInputs = [ 
+    python36Packages.pytest 
+  ];
 
   src = fetchFromGitHub {
     owner = "dsacre";
@@ -60,14 +53,13 @@ buildPythonPackage rec {
     rev = "bbec99a8c878a2a7029e78e84fc736e4a68ed5a0";
     sha256 = "1pdf5mib87zy7yjh9vpasja419h28wvgq6x5hw2hkm7bg9ds4p2m";
   };
-  #doCheck = false;
-  #checkPhase = ''
-  #  echo check
-  #'';
 
   meta = {
     description = "midi router";
     homepage = https://github.com/dsacre/mididings;
     license = lib.licenses.gpl3;
+    maintainers = with stdenv.lib.maintainers; [ hark ];
+    platforms = stdenv.lib.platforms.linux ++ stdenv.lib.platforms.darwin;
+
   };
 }
