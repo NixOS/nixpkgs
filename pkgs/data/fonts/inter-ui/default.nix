98d569d6c1749c1e6949eb2c4c3a2e58df52cadd
Renaud <c0bw3b@users.noreply.github.com>
2018-11-23 15:21:05 +0100

    inter-ui: correct sha hash

--- pkgs/data/fonts/inter-ui/default.nix
+++ pkgs/data/fonts/inter-ui/default.nix
@@ -12,7 +12,7 @@ in fetchzip {
     unzip -j $downloadedFile \*.otf -d $out/share/fonts/opentype
   '';
 
-  sha256 = "02mscwjh3iwd2nl5vah3qcncwkl4jpmi2xxngyf0bhcsmrixi1c6";
+  sha256 = "16qmb8farkh41i56f0vvbxcg32rbg7my64amwz5y8gyy73i3320q";
 
   meta = with stdenv.lib; {
     homepage = https://rsms.me/inter/;
