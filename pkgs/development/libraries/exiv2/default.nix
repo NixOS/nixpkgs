59437759865029a1d8d383b54ff73ca9732d0397
Vladimír Čunát <vcunat@gmail.com>
2018-11-23 16:14:08 +0100

    exiv2: 0.27-rc1 -> 0.27-rc2

--- pkgs/development/libraries/exiv2/default.nix
+++ pkgs/development/libraries/exiv2/default.nix
@@ -2,13 +2,13 @@
 , cmake }:
 
 stdenv.mkDerivation rec {
-  name = "exiv2-0.27-rc1";
+  name = "exiv2-0.27-rc2";
 
   src = fetchFromGitHub rec {
     owner = "exiv2";
     repo  = "exiv2";
-    rev = "a099f24";
-    sha256 = "1nk5zy7rrf9sv4275gh3pd7k9bxh2h04fg4582d99s4s77gbr4c5";
+    rev = "0.27-RC2";
+    sha256 = "04zcfspdg5ypyky3g8p1mxbfy8i083iqbaihnjk6cqnhf6w6pq6r";
   };
 
   outputs = [ "out" "dev" ];
