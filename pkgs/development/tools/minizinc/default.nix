1040875bf4acd76f5fa36d9566d27695ca96d68d
worldofpeace <worldofpeace@users.noreply.github.com>
2018-11-23 18:51:24 -0500

    minizinc: 2.2.1 -> 2.2.3
    Using a git revision for 2.2.3 because their tags are disappearing.
    
    See:
    - https://github.com/MiniZinc/libminizinc/issues/257
    - https://github.com/MiniZinc/libminizinc/issues/248

--- pkgs/development/tools/minizinc/default.nix
+++ pkgs/development/tools/minizinc/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchFromGitHub, cmake, flex, bison }:
 let
-  version = "2.2.1";
+  version = "2.2.3";
 in
 stdenv.mkDerivation {
   name = "minizinc-${version}";
@@ -8,13 +8,12 @@ stdenv.mkDerivation {
   buildInputs = [ cmake flex bison ];
 
   src = fetchFromGitHub {
-    rev = "${version}";
     owner = "MiniZinc";
     repo = "libminizinc";
-    sha256 = "1i11lan7fqs3lg0s6jfr8sflzwn5nk1ln5j6afjrkrdb08291dr7";
+    rev = "3d66971a0cad6edbe796f4dd940229d38e5bfe3d"; # tags on the repo are disappearing: See https://github.com/MiniZinc/libminizinc/issues/257
+    sha256 = "1q31y9131aj2lsm34srm8i1s0271qcaaknzvym3r8awynm14saq5";
   };
 
-  # meta is all the information about the package..
   meta = with stdenv.lib; {
     homepage = http://www.minizinc.org/;
     description = "MiniZinc is a medium-level constraint modelling language.";
