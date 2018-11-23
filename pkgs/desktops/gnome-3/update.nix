59a94b57f07594f4544896dd90c71a948d1ea089
Jan Tojnar <jtojnar@gmail.com>
2018-11-23 18:03:19 +0100

    update.nix: Run update scripts in parallel
    To make updating large attribute sets faster, the update scripts
    are now run in parallel.
    
    Please note the following changes in semantics:
    
    - The string passed to updateScript needs to be a path to an executable file.
    - The updateScript can also be a list: the tail elements will then be passed
      to the head as command line arguments.

--- pkgs/desktops/gnome-3/update.nix
+++ pkgs/desktops/gnome-3/update.nix
@@ -1,9 +1,10 @@
-{ lib, writeScript, python3, common-updater-scripts, coreutils, gnugrep, gnused }:
+{ stdenv, lib, writeScript, python3, common-updater-scripts, coreutils, gnugrep, gnused }:
 { packageName, attrPath ? packageName, versionPolicy ? "odd-unstable" }:
 
 let
   python = python3.withPackages (p: [ p.requests ]);
 in writeScript "update-${packageName}" ''
+  #!${stdenv.shell}
   set -o errexit
   PATH=${lib.makeBinPath [ common-updater-scripts coreutils gnugrep gnused python ]}
   latest_tag=$(python "${./find-latest-version.py}" "${packageName}" "${versionPolicy}" "stable")
