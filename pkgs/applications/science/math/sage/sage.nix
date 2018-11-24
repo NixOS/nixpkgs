883aa992263ffcb99f73e6a36efbd28711d66416
Timo Kaufmann <timokau@zoho.com>
2018-11-24 22:59:51 +0100

    sage: add short tests

--- pkgs/applications/science/math/sage/sage.nix
+++ pkgs/applications/science/math/sage/sage.nix
@@ -54,6 +54,7 @@ stdenv.mkDerivation rec {
 
   passthru = {
     tests = sage-tests;
+    quicktest = sage-tests.override { longTests = false; timeLimit = 600; }; # as many tests as possible in ~10m
     doc = sagedoc;
     lib = sage-with-env.env.lib;
     kernelspec = jupyter-kernel-definition;
