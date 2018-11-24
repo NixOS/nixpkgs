512ec995afbaedd4a7ecd2ee7233caab5a646c23
Frederik Rietdijk <fridh@fridh.nl>
2018-11-24 14:26:32 +0100

    python.pkgs.rope: disable tests with python 3.7

--- pkgs/development/python-modules/rope/default.nix
+++ pkgs/development/python-modules/rope/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildPythonPackage, fetchPypi }:
+{ stdenv, buildPythonPackage, fetchPypi, isPy37 }:
 
 buildPythonPackage rec {
   pname = "rope";
@@ -8,6 +8,9 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "a108c445e1cd897fe19272ab7877d172e7faf3d4148c80e7d20faba42ea8f7b2";
   };
+
+  # https://github.com/python-rope/rope/issues/247
+  doCheck = !isPy37;
 
   meta = with stdenv.lib; {
     description = "Python refactoring library";
