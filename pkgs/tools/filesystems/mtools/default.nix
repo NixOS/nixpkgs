84f7a5ed07e7faf27fc451473467a1e3fe86f40f
Will Dietz <github@wdtz.org>
2018-11-24 15:34:57 -0600

    mtools: 4.0.20 -> 4.0.21
    * mtools: 4.0.20 -> 4.0.21 (#50993)
    
    https://lists.gnu.org/archive/html/info-mtools/2018-11/msg00009.html
    
    * mtools: homepage is https-capable
    
    (cherry picked from commit 51b526d86e350b4f8086c5f61a2f1bf2ba8b087e)

--- pkgs/tools/filesystems/mtools/default.nix
+++ pkgs/tools/filesystems/mtools/default.nix
@@ -1,17 +1,14 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "mtools-4.0.20";
+  name = "mtools-4.0.21";
 
   src = fetchurl {
     url = "mirror://gnu/mtools/${name}.tar.bz2";
-    sha256 = "1vcahr9s6zv1hnrx2bgjnzcas2y951q90r1jvvv4q9v5kwfd6qb0";
+    sha256 = "1kybydx74qgbwpnjvjn49msf8zipchl43d4cq8zzwcyvfkdzw7h2";
   };
 
-  # Prevents errors such as "mainloop.c:89:15: error: expected ')'"
-  # Upstream issue https://lists.gnu.org/archive/html/info-mtools/2014-02/msg00000.html
-  patches = [ ./fix-dos_to_wchar-declaration.patch ] ++
-    stdenv.lib.optional stdenv.isDarwin ./UNUSED-darwin.patch;
+  patches = stdenv.lib.optional stdenv.isDarwin ./UNUSED-darwin.patch;
 
   # fails to find X on darwin
   configureFlags = stdenv.lib.optional stdenv.isDarwin "--without-x";
@@ -19,7 +16,7 @@ stdenv.mkDerivation rec {
   doCheck = true;
 
   meta = with stdenv.lib; {
-    homepage = http://www.gnu.org/software/mtools/;
+    homepage = https://www.gnu.org/software/mtools/;
     description = "Utilities to access MS-DOS disks";
     platforms = platforms.unix;
     license = licenses.gpl3;
