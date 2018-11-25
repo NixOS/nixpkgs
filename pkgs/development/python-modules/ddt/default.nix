3db122cd9f2f7b27451d2a92ec7e6d2f005b75e3
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-11-25 12:57:31 +0100

    python.pkgs.ddt: fix tests

--- pkgs/development/python-modules/ddt/default.nix
+++ pkgs/development/python-modules/ddt/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , buildPythonPackage
 , fetchPypi
+, nose, six, pyyaml, mock
 }:
 
 buildPythonPackage rec {
@@ -11,6 +12,12 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "898364fc34b27981b925171a0011c174c94633cb678eb1fac05fe7a234c7912c";
   };
+
+  checkInputs = [ nose six pyyaml mock ];
+
+  checkPhase = ''
+    nosetests -s
+  '';
 
   meta = with stdenv.lib; {
     description = "Data-Driven/Decorated Tests, a library to multiply test cases";
