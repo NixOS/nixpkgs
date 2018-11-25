aa358e6ee7faaf4b1b9ca6690b022bdacbb398c4
Bas van Dijk <v.dijk.bas@gmail.com>
2018-11-25 11:22:51 +0100

    opencv3: 3.4.3 -> 3.4.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11656,6 +11656,7 @@ with pkgs;
 
   opencv3 = callPackage ../development/libraries/opencv/3.x.nix {
     enableCuda = config.cudaSupport or false;
+    inherit (darwin) cf-private;
     inherit (darwin.apple_sdk.frameworks) AVFoundation Cocoa QTKit VideoDecodeAcceleration;
   };
 
