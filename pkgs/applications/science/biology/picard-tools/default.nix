5c60e84800bfdec38a339e71cf35aff58654bd61
Renaud <c0bw3b@users.noreply.github.com>
2018-11-27 13:49:16 +0100

    picard-tools: 2.18.16 -> 2.18.17
    Changelog: https://github.com/broadinstitute/picard/releases/tag/2.18.17

--- pkgs/applications/science/biology/picard-tools/default.nix
+++ pkgs/applications/science/biology/picard-tools/default.nix
@@ -2,14 +2,15 @@
 
 stdenv.mkDerivation rec {
   name = "picard-tools-${version}";
-  version = "2.18.16";
+  version = "2.18.17";
 
   src = fetchurl {
     url = "https://github.com/broadinstitute/picard/releases/download/${version}/picard.jar";
-    sha256 = "0xg989d7kdc4g14xr9dm8g8f3lpy8kclm68q06sfxf99mf38ibha";
+    sha256 = "0n8p9yygz1d29ivkykrgm4iid3xx6r9c1y1mz74y73cj75myfmm8";
   };
 
-  buildInputs = [ jre makeWrapper ];
+  nativeBuildInputs = [ makeWrapper ];
+  buildInputs = [ jre ];
 
   phases = [ "installPhase" ];
 
@@ -21,7 +22,7 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with stdenv.lib; {
-    description = "Tools for high-throughput sequencing (HTS) data and formats such as SAM/BAM/CRAM and VCF.";
+    description = "Tools for high-throughput sequencing (HTS) data and formats such as SAM/BAM/CRAM and VCF";
     license = licenses.mit;
     homepage = https://broadinstitute.github.io/picard/;
     maintainers = with maintainers; [ jbedo ];
