cbbd839bc4c651e5c8b64bfe613fa47268c43509
tv <tv@krebsco.de>
2018-11-27 12:18:22 +0100

    nixos: allow overriding lib
    Allows overriding the `lib` argument used in configurations, which in
    turn allows using library functions while working with imports, like
    e.g.:
    
    ```
    { lib, ... }: {
      imports = lib.my-imports-generator;
    }
    ```
    
    The same wouldn't be possible with `pkgs.lib`, as it would cause an
    infinite recursion.

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,4 +1,13 @@
+let
+  looks-like-nixos-lib = { path, prefix }:
+    prefix == "nixos-lib" ||
+    (prefix == "" && builtins.pathExists "${path}/nixos-lib");
+in
+
 { configuration ? import ./lib/from-env.nix "NIXOS_CONFIG" <nixos-config>
+, lib ? if builtins.any looks-like-nixos-lib builtins.nixPath
+          then import <nixos-lib>
+          else import ../lib
 , system ? builtins.currentSystem
 }:
 
@@ -7,12 +16,18 @@ let
   eval = import ./lib/eval-config.nix {
     inherit system;
     modules = [ configuration ];
+    specialArgs = {
+      inherit lib;
+    };
   };
 
   # This is for `nixos-rebuild build-vm'.
   vmConfig = (import ./lib/eval-config.nix {
     inherit system;
     modules = [ configuration ./modules/virtualisation/qemu-vm.nix ];
+    specialArgs = {
+      inherit lib;
+    };
   }).config;
 
   # This is for `nixos-rebuild build-vm-with-bootloader'.
@@ -23,6 +38,9 @@ let
         ./modules/virtualisation/qemu-vm.nix
         { virtualisation.useBootLoader = true; }
       ];
+    specialArgs = {
+      inherit lib;
+    };
   }).config;
 
 in
