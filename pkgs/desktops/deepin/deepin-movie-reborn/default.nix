6c95a3484d2705da8b96dc5b5902212eba6d65c4
José Romildo Malaquias <malaquias@gmail.com>
2018-11-27 15:23:55 -0200

    deepin.deepin-movie-reborn: 3.2.14 -> 3.2.19

--- pkgs/desktops/deepin/deepin-movie-reborn/default.nix
+++ pkgs/desktops/deepin/deepin-movie-reborn/default.nix
@@ -5,13 +5,13 @@
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
   pname = "deepin-movie-reborn";
-  version = "3.2.14";
+  version = "3.2.19";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "1i9sdg2p6qp57rqzrnjbxnqj3mg1qggzyq3yykw271vs8h85a707";
+    sha256 = "1kpfv4bjghhy02z6aydyrv55hsrbj62sbc7b3m7rphdva7h8w62i";
   };
 
   nativeBuildInputs = [
