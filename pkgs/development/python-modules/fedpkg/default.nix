7905c38c74a730951ec0a87ba45780e107f86cc5
Mario Rodas <marsam@users.noreply.github.com>
2018-11-28 07:55:49 -0500

    fedpkg: fix patch

--- pkgs/development/python-modules/fedpkg/default.nix
+++ pkgs/development/python-modules/fedpkg/default.nix
@@ -3,16 +3,14 @@
 buildPythonPackage rec {
   pname = "fedpkg";
   version = "1.29";
-  name  = "${pname}-${version}";
 
   disabled = isPy3k;
 
   src = fetchurl {
-    url = "https://releases.pagure.org/fedpkg/${name}.tar.bz2";
+    url = "https://releases.pagure.org/fedpkg/${pname}-${version}.tar.bz2";
     sha256 = "1cpy5p1rp7w52ighz3ynvhyw04z86y8phq3n8563lj6ayr8pw631";
   };
-
-  #patches = [ ../development/python-modules/fedpkg-buildfix.diff ];
+  patches = [ ./fix-paths.patch ];
   propagatedBuildInputs = [ rpkg offtrac urlgrabber ];
 
   doCheck = false; # requires fedora_cert which isn't used anymore
