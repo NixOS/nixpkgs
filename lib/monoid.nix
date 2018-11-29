e48d89a3f298dcb86db64a70bffe47c11d09bacb
John Ericson <git@JohnEricson.me>
2018-11-28 23:53:15 -0500

    lib: Add monoidal fold

--- lib/monoid.nix
+++ lib/monoid.nix
@@ -14,4 +14,8 @@
     append = lib.trivial.mergeAttrs;
   };
 
+  # Monoidally left-fold a list. The monoid's `append` and `identity` become the
+  # other arguments besides the list to the `foldl`.
+  fold = { identity, append, ... }: lib.foldl append identity;
+
 }
