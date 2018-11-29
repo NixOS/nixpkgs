f0dfdd5b2ee49735ce7fe8d99c8717f19d4fac6d
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-11-29 08:09:00 -0500

    pythonPackages.httpretty: 0.8.10 -> 0.9.6

--- pkgs/development/python-modules/httpretty/default.nix
+++ pkgs/development/python-modules/httpretty/default.nix
@@ -9,22 +9,21 @@
 , coverage
 , certifi
 , urllib3
-, isPy3k
+, rednose
+, nose-randomly
 }:
 
 buildPythonPackage rec {
   pname = "httpretty";
   version = "0.9.6";
-  doCheck = false;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "01b52d45077e702eda491f4fe75328d3468fd886aed5dcc530003e7b2b5939dc";
   };
 
-  checkInputs = [ tornado requests httplib2 sure nose coverage certifi ];
+  checkInputs = [ tornado requests httplib2 sure nose nose-randomly rednose coverage certifi ];
   propagatedBuildInputs = [ urllib3 ];
-
 
   meta = with stdenv.lib; {
     homepage = "https://falcao.it/HTTPretty/";
