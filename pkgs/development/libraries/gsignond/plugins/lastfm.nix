66a8c691344e5aabda2f9d0693e8cf748d49e5fc
worldofpeace <worldofpeace@users.noreply.github.com>
2018-11-29 22:05:09 -0500

    gsignondPlugins.lastfm: correct description

--- pkgs/development/libraries/gsignond/plugins/lastfm.nix
+++ pkgs/development/libraries/gsignond/plugins/lastfm.nix
@@ -29,7 +29,7 @@ stdenv.mkDerivation rec {
   PKG_CONFIG_GSIGNOND_GPLUGINSDIR = "${placeholder "out"}/lib/gsignond/gplugins";
 
   meta = with stdenv.lib; {
-    description = "Plugin for the Accounts-SSO gSignOn daemon handles the Last.FM credentials.";
+    description = "Plugin for the Accounts-SSO gSignOn daemon that handles Last.FM credentials";
     homepage = https://gitlab.com/accounts-sso/gsignond-plugin-lastfm;
     license = licenses.lgpl21Plus;
     maintainers = with maintainers; [ worldofpeace ];
