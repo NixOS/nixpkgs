a196feaa5572bb2f531425467e62142f6aa6bc45
Aneesh Agrawal <aneeshusa@gmail.com>
2018-11-29 23:19:51 -0800

    pgcli: 2.0.0 -> 2.0.1
    Changelog: https://github.com/dbcli/pgcli/blob/master/changelog.rst#201
    Major feature of note is Python 3.7 support.

--- pkgs/development/tools/database/pgcli/default.nix
+++ pkgs/development/tools/database/pgcli/default.nix
@@ -2,11 +2,11 @@
 
 pythonPackages.buildPythonApplication rec {
   pname = "pgcli";
-  version = "2.0.0";
+  version = "2.0.1";
 
   src = pythonPackages.fetchPypi {
     inherit pname version;
-    sha256 = "085fna5nc72nfj1gw0m4ia6wzayinqaffmjy3ajldha1727vqwzi";
+    sha256 = "149naq3gp1n922vag7vixs0hd114bpbmbmv70k4kzc1q7jz748l2";
   };
 
   propagatedBuildInputs = with pythonPackages; [
