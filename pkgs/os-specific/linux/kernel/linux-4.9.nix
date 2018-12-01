2fbf68dc006ca588b5d9371a41fcfd0da45d809c
Tim Steinbach <tim@nequissimus.com>
2018-12-01 11:18:10 -0500

    kernel: 4.9.141 -> 4.9.142
    (cherry picked from commit 4440f74ae57fd83f875f2f340eeed95c015a1b45)

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.141";
+  version = "4.9.142";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "09mc5sxzzxmks20vslimaaaw0aamjcc3lvpyjydmr78s25q5zfsp";
+    sha256 = "01n4v0gpf8xyz8hzc70mhjha7kanz9pv248ypsmab7zpzgd7wjil";
   };
 } // (args.argsOverride or {}))
