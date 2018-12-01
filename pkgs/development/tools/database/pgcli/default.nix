cb261c4bc3ea6d22e09adccd654a02a019674b46
Aneesh Agrawal <aneeshusa@gmail.com>
2018-12-01 13:40:08 -0800

    Use Python 3 for pgcli
    On Python 2 nixpkgs uses the older prompt_toolkit 1.x line
    instead of the latest 2.x line.
    The 1.x line is no longer developed
    and isn't listed as supporting Python 3.6 or above.
    
    Since this is an application, always use Python 3.

--- pkgs/development/tools/database/pgcli/default.nix
+++ pkgs/development/tools/database/pgcli/default.nix
@@ -1,20 +1,20 @@
-{ lib, pythonPackages, fetchFromGitHub }:
+{ lib, python3Packages, fetchFromGitHub }:
 
-pythonPackages.buildPythonApplication rec {
+python3Packages.buildPythonApplication rec {
   pname = "pgcli";
   version = "2.0.1";
 
-  src = pythonPackages.fetchPypi {
+  src = python3Packages.fetchPypi {
     inherit pname version;
     sha256 = "149naq3gp1n922vag7vixs0hd114bpbmbmv70k4kzc1q7jz748l2";
   };
 
-  propagatedBuildInputs = with pythonPackages; [
+  propagatedBuildInputs = with python3Packages; [
     cli-helpers click configobj humanize prompt_toolkit psycopg2
     pygments sqlparse pgspecial setproctitle keyring
   ];
 
-  checkInputs = with pythonPackages; [ pytest mock ];
+  checkInputs = with python3Packages; [ pytest mock ];
 
   checkPhase = ''
     py.test
