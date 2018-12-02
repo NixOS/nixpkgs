9e6817231a7d76d6d2b9fe2d930ce79da354590c
Matthew Bauer <mjbauer95@gmail.com>
2018-12-02 14:26:53 -0600

    ghc: don’t add libiconv automatically
    ghc needs it to fail to correctly detect it for later.

--- pkgs/development/compilers/ghc/8.4.4.nix
+++ pkgs/development/compilers/ghc/8.4.4.nix
@@ -182,6 +182,9 @@ stdenv.mkDerivation (rec {
   # Make sure we never relax`$PATH` and hooks support for compatability.
   strictDeps = true;
 
+  # Don’t add -liconv to LDFLAGS automatically so that GHC will add it itself.
+  dontAddExtraLibs = true;
+
   nativeBuildInputs = [
     perl autoconf automake m4 python3
     ghc bootPkgs.alex bootPkgs.happy bootPkgs.hscolour
