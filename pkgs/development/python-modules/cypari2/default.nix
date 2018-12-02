1c0826b8398a08f7e4c3db36f7dce75fde487d20
Timo Kaufmann <timokau@zoho.com>
2018-12-03 00:05:31 +0100

    Revert "pythonPackages.cypari2: 1.3.1 -> 2.0.1 (#49446)"
    This reverts commit d9b650f4cc5ba78835285963eeef8a7c23b593f8.
    
    The update is causing segfaults. See
    https://trac.sagemath.org/ticket/26442.

--- pkgs/development/python-modules/cypari2/default.nix
+++ pkgs/development/python-modules/cypari2/default.nix
@@ -12,11 +12,11 @@
 buildPythonPackage rec {
   pname = "cypari2";
   # upgrade may break sage, please test the sage build or ping @timokau on upgrade
-  version = "2.0.1";
+  version = "1.3.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "11nqp40nji8bz4zn7j8sbzin93l65kf8s1sc76lvbsbkswaxdyij";
+    sha256 = "04f00xp8aaz37v00iqg1mv5wjq00a5qhk8cqa93s13009s9x984r";
   };
 
   # This differs slightly from the default python installPhase in that it pip-installs
