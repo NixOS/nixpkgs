b3662053b3b05160dcb5759e583c9aedb85722ec
Jörg Thalheim <joerg@thalheim.io>
2018-12-02 11:34:26 +0000

    nixos/nm-applet: make the module smaller
    more readable imho

--- nixos/modules/programs/nm-applet.nix
+++ nixos/modules/programs/nm-applet.nix
@@ -1,43 +1,14 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
-let
-  cfg = config.programs.nm-applet;
-in
-
 {
+  options.programs.nm-applet.enable = lib.mkEnableOption "nm-applet";
 
-  ###### interface
-
-  options = {
-
-    programs.nm-applet = {
-
-      enable = mkOption {
-        type = types.bool;
-        default = false;
-        description = ''
-          Whether to enable nm-applet.
-        '';
-      };
-
-    };
-
-  };
-
-
-  ###### implementation
-
-  config = mkIf cfg.enable {
-
+  config = lib.mkIf config.programs.nm-applet.enable {
     systemd.user.services.nm-applet = {
       description = "Network manager applet";
       wantedBy = [ "graphical-session.target" ];
       partOf = [ "graphical-session.target" ];
       serviceConfig.ExecStart = "${pkgs.networkmanagerapplet}/bin/nm-applet";
     };
-
   };
-
 }
