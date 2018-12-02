a28276bf593a4abd2e8317be7d7ad5a87152b8e5
Henry <cryptix@riseup.net>
2018-12-02 15:05:34 +0100

    systrayhelper: 0.0.4 -> 0.0.5

--- pkgs/tools/misc/systrayhelper/default.nix
+++ pkgs/tools/misc/systrayhelper/default.nix
@@ -1,9 +1,15 @@
-{ stdenv, pkgconfig, libappindicator-gtk3, buildGoPackage, fetchFromGitHub }:
+{ stdenv, pkgconfig, libappindicator-gtk3, buildGoPackage, fetchFromGitHub
+# for tests
+, dbus
+, i3
+, xdotool
+, xvfb_run
+}:
 
 buildGoPackage rec {
   name = "systrayhelper-${version}";
-  version = "0.0.4";
-  rev = "ded1f2ed4d30f6ca2c89a13db0bd3046c6d6d0f9";
+  version = "0.0.5";
+  rev = "48c47f89b1bae8f3a09d449a96f8df2bf0776560";
 
   goPackagePath = "github.com/ssbc/systrayhelper";
 
@@ -11,10 +17,8 @@ buildGoPackage rec {
     rev = "v${version}";
     owner = "ssbc";
     repo = "systrayhelper";
-    sha256 = "1iq643brha5q6w2v1hz5l3d1z0pqzqr43gpwih4cnx3m5br0wg2k";
+    sha256 = "0bn3nf43m89qmh8ds5vmv0phgdz32idz1zisr47jmvqm2ky1a45s";
   };
-
-  goDeps = ./deps.nix;
 
   # re date: https://github.com/NixOS/nixpkgs/pull/45997#issuecomment-418186178
   # > .. keep the derivation deterministic. Otherwise, we would have to rebuild it every time.
@@ -28,6 +32,18 @@ buildGoPackage rec {
 
   nativeBuildInputs = [ pkgconfig libappindicator-gtk3 ];
   buildInputs = [ libappindicator-gtk3 ];
+
+  enableParallelBuilding = true;
+
+  checkInputs = [ xvfb_run dbus i3 xdotool ];
+  doCheck = stdenv.isLinux;
+  # use the override.nix from the source to extract and watch tests
+  checkPhase = ''
+    export TRAY_I3=true
+    xvfb-run -s '-screen 0 800x600x16' dbus-run-session \
+      --config-file=${dbus.daemon}/share/dbus-1/session.conf \
+      go test -timeout 1m -v github.com/ssbc/systrayhelper/test
+  '';
 
   meta = with stdenv.lib; {
     description = "A systray utility written in go, using json over stdio for control and events";
