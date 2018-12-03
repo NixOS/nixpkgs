173852b9362ba4055dc196ec4c66fe493e0489bb
Austin Seipp <aseipp@pobox.com>
2018-12-02 20:14:48 -0600

    nixos/chrony: fix misplaced ConditionCapability= directive
    Signed-off-by: Austin Seipp <aseipp@pobox.com>
    (cherry picked from commit 4594b18070a6dab57a815e8ecc500b8a25c3f5a8)

--- nixos/modules/services/networking/chrony.nix
+++ nixos/modules/services/networking/chrony.nix
@@ -111,6 +111,7 @@ in
           chown chrony:chrony ${stateDir} ${keyFile}
         '';
 
+        unitConfig.ConditionCapability = "CAP_SYS_TIME";
         serviceConfig =
           { Type = "forking";
             ExecStart = "${pkgs.chrony}/bin/chronyd ${chronyFlags}";
@@ -119,8 +120,8 @@ in
             ProtectSystem = "full";
             PrivateTmp = "yes";
 
-            ConditionCapability = "CAP_SYS_TIME";
           };
+
       };
   };
 }
