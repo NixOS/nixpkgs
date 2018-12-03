dc9bb48a736709dba46a9be54cdd826376214cd6
Alexey Shmalko <rasen.dubi@gmail.com>
2018-12-04 01:20:02 +0200

    loop: cleanup

--- pkgs/tools/misc/loop/default.nix
+++ pkgs/tools/misc/loop/default.nix
@@ -1,19 +1,22 @@
 { stdenv, fetchFromGitHub, rustPlatform }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage {
   name = "loop-unstable-2018-10-02";
-  version = "d6ef3c5a0ecd4f533908abee5e481419a1a6eeae";
 
   src = fetchFromGitHub {
     owner = "Miserlou";
     repo  = "Loop";
-    rev   = version;
+    rev   = "d6ef3c5a0ecd4f533908abee5e481419a1a6eeae";
     sha256 = "1fhihm32v77rj6r3scwmnvzsivky50g7a1644qrn8pafpjs4zwx5";
   };
 
   cargoSha256 = "1ccf8dkswwdbwf9diy0l4vc4i2g05ynhi3w1jg3b2ldrvj0j9m9s";
 
-  cargoPatches = [ ./fix_cargo_toml.patch ./fix_cargo_lock.patch ]; # Cargo.lock and Cargo.toml are not aligned
+  cargoPatches = [
+    # Upstream includes mismatched Cargo.lock file.
+    # See https://github.com/Miserlou/Loop/pull/40
+    ./fix_cargo_lock.patch
+  ];
 
   meta = with stdenv.lib; {
     description = "UNIX's missing `loop` command";
