fe939378013848f33c1427cfe845487af8429e3c
rht <rhtbot@protonmail.com>
2018-12-04 19:24:22 +0000

    stdenv: Add a fake date utility together with setup
    Based on https://github.com/NixOS/nixpkgs/pull/2281/commits/c232efa244cb0ca38bba6392a7d5a88b609f9b92.

--- pkgs/stdenv/generic/builder.sh
+++ pkgs/stdenv/generic/builder.sh
@@ -13,6 +13,18 @@ echo "defaultBuildInputs=\"$defaultBuildInputs\"" >> $out/setup
 echo "$preHook" >> $out/setup
 cat "$setup" >> $out/setup
 
+# Setup fake date
+mkdir $out/bin
+real_date=$(type -Pa date | sed 1d)
+if test -n "$real_date"; then
+    cat >$out/bin/date <<EOF
+#!$shell
+exec $real_date -d0 "\$@"
+EOF
+    chmod +x $out/bin/date
+    initialPath="$out $initialPath"
+fi
+
 # Allow the user to install stdenv using nix-env and get the packages
 # in stdenv.
 mkdir $out/nix-support
