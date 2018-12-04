01a7fa88139b5da9d6ad8920486f756176d2b34a
koral <chahine.moreau@gmail.com>
2018-12-05 00:19:33 +0100

    loop: unstable-2018-10-02 -> unstable-2018-12-04

--- pkgs/tools/misc/loop/default.nix
+++ pkgs/tools/misc/loop/default.nix
@@ -1,22 +1,16 @@
 { stdenv, fetchFromGitHub, rustPlatform }:
 
 rustPlatform.buildRustPackage {
-  name = "loop-unstable-2018-10-02";
+  name = "loop-unstable-2018-12-04";
 
   src = fetchFromGitHub {
     owner = "Miserlou";
     repo  = "Loop";
-    rev   = "d6ef3c5a0ecd4f533908abee5e481419a1a6eeae";
-    sha256 = "1fhihm32v77rj6r3scwmnvzsivky50g7a1644qrn8pafpjs4zwx5";
+    rev   = "598ccc8e52bb13b8aff78b61cfe5b10ff576cecf";
+    sha256 = "0f33sc1slg97q1aisdrb465c3p7fgdh2swv8k3yphpnja37f5nl4";
   };
 
   cargoSha256 = "1ccf8dkswwdbwf9diy0l4vc4i2g05ynhi3w1jg3b2ldrvj0j9m9s";
-
-  cargoPatches = [
-    # Upstream includes mismatched Cargo.lock file.
-    # See https://github.com/Miserlou/Loop/pull/40
-    ./fix_cargo_lock.patch
-  ];
 
   meta = with stdenv.lib; {
     description = "UNIX's missing `loop` command";
