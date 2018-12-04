42cdc112373fc07fa5195f7b5ea2f717ddd400f8
leo60228 <iakornfeld@gmail.com>
2018-12-04 11:00:45 -0500

    ntfs-3g: enable extras
    Adds a few extra utilities, most of which are useful.

--- pkgs/tools/filesystems/ntfs-3g/default.nix
+++ pkgs/tools/filesystems/ntfs-3g/default.nix
@@ -38,6 +38,7 @@ stdenv.mkDerivation rec {
     "--enable-posix-acls"
     "--enable-xattr-mappings"
     "--${if crypto then "enable" else "disable"}-crypto"
+    "--enable-extras"
   ];
 
   postInstall =
