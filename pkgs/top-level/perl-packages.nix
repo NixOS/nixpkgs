03d591cdcd5258d13183c3305a10cd96c6151af5
volth <volth@volth.com>
2018-12-05 04:09:45 +0000

    [cpan2nix] perlPackages.HashDiff: cleanup

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -6750,6 +6750,7 @@ let
       license = with stdenv.lib.licenses; [ artistic1 ];
       description = "Return difference between two hashes as a hash";
     };
+    buildInputs = [ TestSimple13 ];
   };
 
   HashFlatten = buildPerlPackage rec {
