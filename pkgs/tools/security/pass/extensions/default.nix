007e177f90408bc0f549731642c6fddf88f399ec
Gürkan Gür <gurkan.gur@innogames.com>
2018-12-06 19:36:01 +0100

    passExtensions.pass-genphrase: init at 0.1

--- pkgs/tools/security/pass/extensions/default.nix
+++ pkgs/tools/security/pass/extensions/default.nix
@@ -12,4 +12,5 @@ with pkgs;
   pass-otp = callPackage ./otp.nix {};
   pass-tomb = callPackage ./tomb.nix {};
   pass-update = callPackage ./update.nix {};
+  pass-genphrase = callPackage ./genphrase.nix {};
 }
