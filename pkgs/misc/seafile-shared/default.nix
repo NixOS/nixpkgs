3df463d351df149eb0657a2f01836dc648e98c45
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-12-07 14:16:26 +0100

    seafile-shared: 6.2.7 -> 6.2.8

--- pkgs/misc/seafile-shared/default.nix
+++ pkgs/misc/seafile-shared/default.nix
@@ -1,14 +1,14 @@
 {stdenv, fetchFromGitHub, which, autoreconfHook, pkgconfig, curl, vala, python, intltool, fuse, ccnet}:
 
 stdenv.mkDerivation rec {
-  version = "6.2.7";
+  version = "6.2.8";
   name = "seafile-shared-${version}";
 
   src = fetchFromGitHub {
     owner = "haiwen";
     repo = "seafile";
     rev = "v${version}";
-    sha256 = "0f8h7x6q830q4pw6f6bbykiyj3lkdlgvjzg2sdaqm4bhj2c4k1n0";
+    sha256 = "1sgrsj60gy4pqxjz1rxn4nyzmc67x3kx16kd2vmwbzr44hasnp46";
   };
 
   nativeBuildInputs = [ pkgconfig which autoreconfHook vala intltool ];
