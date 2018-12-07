06b1017d28ec546bcb673a8e12b2601ec7a55f45
Will Dietz <w@wdtz.org>
2018-12-07 12:06:37 -0600

    mpop: 1.2.6 -> 1.4.0
    Update homepage and license.

--- pkgs/applications/networking/mpop/default.nix
+++ pkgs/applications/networking/mpop/default.nix
@@ -3,24 +3,24 @@
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
-  version = "1.2.6";
+  version = "1.4.0";
   name = "mpop-${version}";
 
   src = fetchurl {
-    url = "mirror://sourceforge/mpop/${name}.tar.xz";
-    sha256 = "0p1ix63jh64dibrlccch8q7gxl9nn18wd2qpyr5z1h4gs2fpmv4z";
+    url = "https://marlam.de/mpop/releases/${name}.tar.xz";
+    sha256 = "14xsvpm5bc1wycisq882gqrnamnyi1q4rlk6anlw8ihzvwgm4h2j";
   };
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ openssl gnutls gsasl libidn ]
     ++ optional stdenv.isDarwin Security;
 
-  configureFlags =
+  configureFlags = [ "--with-tls=openssl" ] ++
     optional stdenv.isDarwin [ "--with-macosx-keyring" ];
 
   meta = {
       description = "POP3 mail retrieval agent";
-      homepage = http://mpop.sourceforge.net/;
+      homepage = https://marlam.de/mpop;
       license = licenses.gpl3Plus;
       platforms = platforms.unix;
     };
