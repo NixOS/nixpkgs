65472e3474159f7d788b356e86a1991d485a4997
Christian Höppner <christian.hoppner@serokell.io>
2018-12-07 14:07:21 +0000

    Bump all-cabal-hashes

--- pkgs/data/misc/hackage/default.nix
+++ pkgs/data/misc/hackage/default.nix
@@ -1,6 +1,6 @@
 { fetchurl }:
 
 fetchurl {
-  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/eacf720a948ab74e63318306d08c34d6fb60c4e8.tar.gz";
-  sha256 = "1cr295g610is8jm6l8z7anj7slfiks0w4x9gh5yzgkfydlgb7r4b";
+  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/a7b2b2b9017132e05ce2d06c93c4656b0809aacd.tar.gz";
+  sha256 = "0y1kf7cq3h9vs8nryaaykx4hndv523srwbzjplvmmmj5i9f26468";
 }
