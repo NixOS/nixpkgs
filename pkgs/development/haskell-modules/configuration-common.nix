6c3afdf05b43434fe6a9dd23acce92d03168b662
Peter Simons <simons@cryp.to>
2018-12-08 15:07:09 +0100

    git-annex: update sha256 hash for new version 7.20181205

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -86,7 +86,7 @@ self: super: {
       name = "git-annex-${super.git-annex.version}-src";
       url = "git://git-annex.branchable.com/";
       rev = "refs/tags/" + super.git-annex.version;
-      sha256 = "0f0pp0d5q4122cjh4j7iasnjh234fmkvlwgb3f49087cg8rr2czh";
+      sha256 = "0iav8l9k7q4n7cs04mqss7z9vxjqz6p4avg30r5wmxxgxfnm626s";
     };
   }).override {
     dbus = if pkgs.stdenv.isLinux then self.dbus else null;
