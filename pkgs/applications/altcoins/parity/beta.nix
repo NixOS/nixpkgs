794462485107c3fbd9a324568280fc20cdf8249e
Alexander Krupenkin <mail@akru.me>
2018-12-08 22:46:32 +0300

    parity-beta: 2.2.1 -> 2.2.4

--- pkgs/applications/altcoins/parity/beta.nix
+++ pkgs/applications/altcoins/parity/beta.nix
@@ -1,9 +1,6 @@
 let
-  version     = "2.2.1";
-  sha256      = "1m65pks2jk83j82f1i901p03qb54xhcp6gfjngcm975187zzvmcq";
-  cargoSha256 = "1mf1jgphwvhlqkvzrgbhnqfyqgf3ljc1l9zckyilzmw5k4lf4g1w";
-  patches     = [
-    ./patches/vendored-sources-2.2.patch
-  ];
+  version     = "2.2.4";
+  sha256      = "12qcfmc56vnay25nlflgwhm3iwlr7hd286wzzanlsalizaj5s5ja";
+  cargoSha256 = "11cwzqd459ld0apl2wnarfc4nb6j9j0dh26y3smvr0zsxvaz1r53";
 in
-  import ./parity.nix { inherit version sha256 cargoSha256 patches; }
+  import ./parity.nix { inherit version sha256 cargoSha256; }
