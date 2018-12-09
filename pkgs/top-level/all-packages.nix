6f5014e0ae8635c73387cc59b0fafa7183a89880
Mathieu Boespflug <m@tweag.io>
2018-12-09 15:53:26 +0100

    bazel: 0.18.0 -> 0.20.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8297,6 +8297,7 @@ in
   bazel = callPackage ../development/tools/build-managers/bazel {
     inherit (darwin) cctools;
     inherit (darwin.apple_sdk.frameworks) CoreFoundation CoreServices Foundation;
+    runJdk = jdk11;
   };
   bazel_jdk11 = callPackage ../development/tools/build-managers/bazel {
     inherit (darwin) cctools;
