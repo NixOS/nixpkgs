4d5a4a64bff00fac7e423ccce8b3574ad8c109c1
José Romildo Malaquias <malaquias@gmail.com>
2018-12-11 11:11:15 -0200

    xcur2png: init at 0.7.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6476,6 +6476,8 @@ in
 
   xclip = callPackage ../tools/misc/xclip { };
 
+  xcur2png = callPackage ../tools/graphics/xcur2png { };
+
   xcwd = callPackage ../tools/X11/xcwd { };
 
   xtitle = callPackage ../tools/misc/xtitle { };
