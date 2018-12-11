NixOS/nixpkgs

commit 0f38d9669ffd74991b65a9c40cdbcf2a98438d1f

python3 is now python37 instead of python36
With Python 3.7 now at 3.7.1, and Python 3.6 at it's final maintenance
mode release, it is time to move on to 3.7 as the default interpreter.

pkgs/top-level/all-packages.nix
@@ -7756,7 +7756,7 @@ with pkgs;
   # When switching these sets, please update docs at ../../doc/languages-frameworks/python.md
   python = python2;
   python2 = python27;
-  python3 = python36;
+  python3 = python37;
   pypy = pypy27;
 
   # Python interpreter that is build with all modules, including tkinter.
@@ -7806,8 +7806,8 @@ with pkgs;
   python27Packages = lib.hiPrioSet (recurseIntoAttrs python27.pkgs);
   python34Packages = python34.pkgs;
   python35Packages = python35.pkgs;
-  python36Packages = recurseIntoAttrs python36.pkgs;
-  python37Packages = python37.pkgs;
+  python36Packages = python36.pkgs;
+  python37Packages = recurseIntoAttrs python37.pkgs;
   pypyPackages = pypy.pkgs;
 
   # Should eventually be moved inside Python interpreters.

nixos/doc/manual/release-notes/rl-1903.xml
@@ -19,6 +19,8 @@
 
   <itemizedlist>
    <listitem>
+    <para>
+    The default Python 3 interpreter is now CPython 3.7 instead of CPython 3.6.
     <para />
    </listitem>
   </itemizedlist>

commit 31298112f3ce56aa4a240ae2c6fd650bec98e47b

caffe2: Use python36Packages explicitly.
Fallout from 0f38d9669ffd74991b65a9c40cdbcf2a98438d1f

pkgs/top-level/all-packages.nix
@@ -21800,9 +21800,9 @@ in
   };
 
   caffe2 = callPackage ../development/libraries/science/math/caffe2 (rec {
-    inherit (python3Packages) python future six numpy pydot;
+    inherit (python36Packages) python future six numpy pydot;
     protobuf = protobuf3_1;
-    python-protobuf = python3Packages.protobuf.override { inherit protobuf; };
+    python-protobuf = python36Packages.protobuf.override { inherit protobuf; };
     # Used only for image loading.
     opencv3 = opencv3WithoutCuda;
   });