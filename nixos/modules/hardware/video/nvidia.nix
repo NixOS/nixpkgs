4f3446f29910d21eb0fb942bd03091b089cdad63
Vladimír Čunát <vcunat@gmail.com>
2018-12-12 00:20:52 +0100

    nixos/nvidia: improve the assertion again
    /cc 13938acd0 #51836.
    
    (cherry picked from commit ad3e9191d16722ea3eec32f4cd689eea730f39f6)

--- nixos/modules/hardware/video/nvidia.nix
+++ nixos/modules/hardware/video/nvidia.nix
@@ -97,7 +97,7 @@ in
   config = mkIf enabled {
     assertions = [
       {
-        assertion = !config.services.xserver.displayManager.gdm.wayland;
+        assertion = with config.services.xserver.displayManager; gdm.enable -> !gdm.wayland;
         message = "NVidia drivers don't support wayland";
       }
       {
