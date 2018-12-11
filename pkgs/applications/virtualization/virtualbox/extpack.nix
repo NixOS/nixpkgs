22acb2e3c304bc9d40c9804d1a2268d7c1290104
Ambroz Bizjak <abizjak.pro@gmail.com>
2018-12-11 20:45:18 +0100

    vboxExtpack: Fix the sha256 to be hex.
    It does not work if the sha256 is not hex, it fails because VBoxExtPackHelperApp requires to be given a hex hash.
    See https://github.com/NixOS/nixpkgs/issues/34846 where the same problem was fixed some time ago.

--- pkgs/applications/virtualization/virtualbox/extpack.nix
+++ pkgs/applications/virtualization/virtualbox/extpack.nix
@@ -7,7 +7,11 @@ in
 fetchurl rec {
   name = "Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack";
   url = "https://download.virtualbox.org/virtualbox/${version}/${name}";
-  sha256 = "1vria59m7xr521hp2sakfihv8282xcfd5hl6dr1gbcjicmk514kp";
+  sha256 =
+    # Manually sha256sum the extensionPack file, must be hex!
+    # Thus do not use `nix-prefetch-url` but instead plain old `sha256sum`.
+    let value = "779250666551b2f5426e86c2d21ceb0209b46174536971611025f753535131ef";
+    in assert (builtins.stringLength value) == 64; value;
 
   meta = {
     description = "Oracle Extension pack for VirtualBox";
