e376aecb1ea57bdeefedeb1a10625c22c464e415
Alexey Shmalko <rasen.dubi@gmail.com>
2018-12-11 17:37:18 +0200

    railcar: add reference to upstream patch

--- pkgs/applications/virtualization/railcar/default.nix
+++ pkgs/applications/virtualization/railcar/default.nix
@@ -10,10 +10,12 @@ rustPlatform.buildRustPackage rec {
     rev = "v${version}";
     sha256 = "09zn160qxd7760ii6rs5nhr00qmaz49x1plclscznxh9hinyjyh9";
   };
-  
+
   cargoSha256 = "16f3ys0zzha8l5jdklmrqivl8hmrb9qgqgzcm3jn06v45hls9lan";
 
   buildInputs = [ libseccomp ];
+
+  # Submitted upstream https://github.com/oracle/railcar/pull/44
   cargoPatches = [ ./cargo-lock.patch ];
 
   meta = with lib; {
