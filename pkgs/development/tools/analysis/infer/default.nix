1c4be1de135d9fe3e1fa65ce38366bed07298fcf
Amar1729 <amar.paul16@gmail.com>
2018-12-11 19:04:01 -0500

    separate infer, infer opam deps, and clang

--- pkgs/development/tools/analysis/infer/default.nix
+++ pkgs/development/tools/analysis/infer/default.nix
@@ -1,5 +1,9 @@
 { stdenv, fetchFromGitHub,
-autoconf, automake, clang_5, cmake, git, gnum4, libtool, ocaml, opam_1_2, openjdk, perl, pkgconfig, python2, rsync, sqlite, which, zlib, }:
+infer-deps, facebook-clang,
+autoconf, automake, cmake, gcc, git, gnum4, ocaml, opam_1_2, openjdk, perl, pkgconfig, python2, sqlite, which, zlib,
+withC ? true,
+withJava ? true
+}:
 
 stdenv.mkDerivation rec {
   pname = "infer";
@@ -9,76 +13,109 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "facebook";
     repo = "infer";
-    #tag = "v0.15.0";
-    rev = "8bda23fadcc51c6ed38a4c3a75be25a266e8f7b4";
+    rev = "v${version}";
     sha256 = "1fzm5s0cwah6jk37rrb9dsrfd61f2ird3av0imspyi4qyp6pyrm6";
+  };
+
+  facebook-clang-plugins = fetchFromGitHub {
+    owner = "facebook";
+    repo = "facebook-clang-plugins";
+    rev = "f31f7c9c28d8fb9b59c0dacc74a24e4bfe90a904";
+    sha256 = "11icpjm73xmyjla9cgg117sn380iibcd3xk743v0z8scwi79aaq8";
   };
 
   case_fail = ./FailingTest.java;
   case_pass = ./PassingTest.java;
 
   dontUseCmakeConfigure = true;
-  doCheck = false;
+  dontBuild = true;
+  doCheck = false; # for now, but want to set to true later
 
-  depsBuildBuild = [ gnum4 git which ];
+  depsBuildBuild = [ git ];
 
-  #nativeBuildInputs = [ ];
+  nativeBuildInputs = [ which ];
+
   buildInputs = [
     autoconf
     automake
-    # clang or gcc on linux for c/c obj analysis
-    #clang_5
-    # on mac for c/c obj analysis
-    cmake
-    # only for java analysis:
-    openjdk
+    facebook-clang
+    gnum4
+    infer-deps
     ocaml
     opam_1_2
     perl
     pkgconfig
     python2
-    # only on mac?
     sqlite
+    # TODO - include ocamlPackages.utop v2.1.0 for infer-repl
     zlib
-  ];
+  ]
+  # infer will need recent gcc or clang to work properly on linux (custom clang depends on libs)
+  #++ stdenv.lib.optionals stdenv.isLinux [ gcc ]
+  ++ stdenv.lib.optionals withC [ cmake facebook-clang ]
+  ++ stdenv.lib.optionals withJava [ openjdk ]
+  ;
 
-  postUnpack = ''
-    # make sure we can git clone stuff (opam pin add -k git)
-    export GIT_SSL_NO_VERIFY=true
-
-    export OPAMROOT=$out/opam
+  postUnpack = "
+    # setup opam stuff
+    export OPAMROOT=${infer-deps}/opam
+    export OPAM_BACKUP=${infer-deps}/opam.bak
     export OCAML_VERSION='4.06.1+flambda'
     export INFER_OPAM_SWITCH=infer-$OCAML_VERSION
 
-    mkdir -p $OPAMROOT
-    opam init --no-setup
-    opam switch set $INFER_OPAM_SWITCH --alias-of $OCAML_VERSION
+    # dumb hack: some opam operations need to write minor build files to opam repo
+    chmod u+w ${infer-deps}
+    # backup stays around if previous builds have failed: make sure to nuke it
+    [[ -d $OPAM_BACKUP ]] && (chmod -R u+w $OPAM_BACKUP && rm -rf $OPAM_BACKUP)
+    cp -r $OPAMROOT $OPAM_BACKUP
+    chmod -R u+w $OPAMROOT
 
     eval $(SHELL=bash opam config env --switch=$INFER_OPAM_SWITCH)
 
-    # install opam deps
-    opam pin add -k git lock 'https://github.com/rgrinberg/opam-lock'
-    opam lock --install < $src/opam.lock
-  '';
+    # link facebook clang plugins and the custom clang itself (bit hacky)
+    chmod u+w $src
+    rm -rf $src/facebook-clang-plugins
+    ln -s ${facebook-clang-plugins} $src/facebook-clang-plugins
+    chmod -R u+w $src/facebook-clang-plugins
 
-  preConfigure = ''
-    ./autogen.sh
-  '';
+    pushd $src/facebook-clang-plugins/clang > /dev/null
+    [[ -h include ]] && rm include
+    [[ -h install ]] && rm install
+    ln -s ${facebook-clang} $src/facebook-clang-plugins/clang/install
+    ln -s ${facebook-clang}/include $src/facebook-clang-plugins/clang/include
+    shasum -a 256 setup.sh src/clang-7.0.tar.xz > installed.version
+    popd > /dev/null
+  ";
 
-  configureFlags = [ "--disable-c-analyzers" ];
+  preConfigure = "./autogen.sh";
 
-  buildPhase = "";
-  installPhase = ''make install'';
+  configureFlags = [ "--with-fcp-clang" ]
+    ++ stdenv.lib.optionals (!withC) [ "--disable-c-analyzers" ]
+    ++ stdenv.lib.optionals (!withJava) [ "--disable-java-analyzers" ]
+  ;
 
-  postInstall = ''
-    #$out/bin/infer --fail-on-issue -- javac ${case_fail}
-    #$out/bin/infer --fail-on-issue -- javac ${case_pass}
-  '';
+  installPhase = "make install";
+
+  postInstall = "
+    # restore original opam state
+    rm -rf $OPAMROOT
+    mv $OPAM_BACKUP $OPAMROOT
+  ";
+
+  #checkPhase = ''
+  #  #$out/bin/infer --fail-on-issue -- javac ${case_fail}
+  #  #$out/bin/infer --fail-on-issue -- javac ${case_pass}
+  #'';
 
   meta = with stdenv.lib; {
-    description = "";
+    description = "A static analyzer for Java, C, C++, and Objective-C";
     longDescription = ''
+        A tool written in OCaml by Facebook for static analysis.
+        See homepage or https://github.com/facebook/infer for more information.
+
+        Note: building java analyzers requires downloading some GPL-licensed components.
     '';
+    homepage = "https://fbinfer.com";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ amar1729 ];
     platforms = platforms.all;
