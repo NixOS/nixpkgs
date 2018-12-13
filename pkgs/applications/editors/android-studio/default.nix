81eed0f30a5303c7b9db7e1e25eddc4cb28a8ca0
Michael Weiss <dev.primeos@gmail.com>
2018-12-13 20:48:52 +0100

    androidStudioPackages.{dev,canary}: 3.4.0.6 -> 3.4.0.7

--- pkgs/applications/editors/android-studio/default.nix
+++ pkgs/applications/editors/android-studio/default.nix
@@ -18,9 +18,9 @@ let
     sha256Hash = "05rjwvcph0wx0p0hai5z6n9lnyhk3i5yvbvhr51jc8s3k3b6jyi5";
   };
   latestVersion = { # canary & dev
-    version = "3.4.0.6"; # "Android Studio 3.4 Canary 7"
-    build = "183.5159543";
-    sha256Hash = "0r685qqx4w1hwbd8jgrh7ks8bw9m7823ffhd3x6pl7j4b9hpc858";
+    version = "3.4.0.7"; # "Android Studio 3.4 Canary 8"
+    build = "183.5173923";
+    sha256Hash = "0bf96c9db15rw1k1znz6yxhbrn9q990zy3pkq0nsirnqfpgllvpi";
   };
 in rec {
   # Old alias
