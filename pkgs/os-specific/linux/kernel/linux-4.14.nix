d3698e03fb7cfebecb92b7c86c335b4cdbcf2ae0
Tim Steinbach <tim@nequissimus.com>
2018-12-13 06:58:07 -0500

    linux: 4.14.87 -> 4.14.88
    (cherry picked from commit f335fa6d74cf6ec7ace4d2fe7ef28e030126064e)

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.14.87";
+  version = "4.14.88";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "09g12a8lkx5xm3c65q6wbzr2asnws956w2d8apj27avxcmylsx1n";
+    sha256 = "0bgm4vr1c4s4k8gyw8i92lxj82nl67jh55q0yg5bdsh8cz3viw5h";
   };
 } // (args.argsOverride or {}))
