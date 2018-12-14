95f11baa21499faf5d6d52a37e91457074887b8e
R. RyanTM <ryantm-bot@ryantm.com>
2018-12-13 18:16:10 -0800

    you-get: 0.4.1167 -> 0.4.1181
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/you-get/versions

--- pkgs/tools/misc/you-get/default.nix
+++ pkgs/tools/misc/you-get/default.nix
@@ -2,7 +2,7 @@
 
 buildPythonApplication rec {
   pname = "you-get";
-  version = "0.4.1167";
+  version = "0.4.1181";
 
   # Tests aren't packaged, but they all hit the real network so
   # probably aren't suitable for a build environment anyway.
@@ -10,7 +10,7 @@ buildPythonApplication rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0lvddm62c0pwx4cksbwh2n0xzz80p26lz1xsvb4z40h7hlwaf41w";
+    sha256 = "1rcy590392aycjazi3z8gf6ll39rxkbgmkgnsbsl6yl5vb3jgk83";
   };
 
   meta = with stdenv.lib; {
