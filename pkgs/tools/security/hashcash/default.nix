ad0d03283bc3ed23bf4cf7421bf746a4358fc7c0
Jim Fowler <fowler@math.osu.edu>
2018-12-14 11:04:17 -0500

    hashcash from http://hashcash.org

---
+++ pkgs/tools/security/hashcash/default.nix
@@ -0,0 +1,29 @@
+{ stdenv, fetchurl, openssl }:
+
+stdenv.mkDerivation rec {
+  name = "hashcash-${version}";
+  version = "1.22";
+
+  buildInputs = [ openssl ]; 
+
+  src = fetchurl {
+      url = "http://www.hashcash.org/source/hashcash-1.22.tgz";
+      sha256 = "15kqaimwb2y8wvzpn73021bvay9mz1gqqfc40gk4hj6f84nz34h1";
+  };
+ 
+  makeFlags = "generic-openssl LIBCRYPTO=-lcrypto";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    mkdir -p $out/share/man/man1  
+    install -m 0755 hashcash $out/bin
+    install -m 0755 sha1 $out/bin
+    install -m 0444 hashcash.1 $out/share/man/man1
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Hashcash is a proof-of-work algorithm, which has been used as a denial-of-service counter measure technique in a number of systems.";
+    homepage = http://hashcash.org;
+    license = licenses.gpl2;
+  };
+}
