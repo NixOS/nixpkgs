1392dc8b3e4e3bca3e69aabf8d2de1c850b15f83
R. RyanTM <ryantm-bot@ryantm.com>
2018-12-14 08:56:48 -0800

    python37Packages.vega_datasets: 0.5.0 -> 0.6.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-vega_datasets/versions

--- pkgs/development/python-modules/vega_datasets/default.nix
+++ pkgs/development/python-modules/vega_datasets/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "vega_datasets";
-  version = "0.5.0";
+  version = "0.6.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1fa672ba89ded093b30c6d59fce10aca3ac7c927df254e588da7b6d14f695181";
+    sha256 = "02zk5d65g58kpmgbbng47fg357yizv25z8nzi95nvw6vp6y2b3d3";
   };
 
   propagatedBuildInputs = [ pandas ];
