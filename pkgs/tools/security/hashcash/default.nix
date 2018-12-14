6206a783d5015463de8e7bc5205bb711d00c49f6
Jörg Thalheim <joerg@thalheim.io>
2018-12-14 18:07:36 +0000

    hashcash: simplify installation

--- pkgs/tools/security/hashcash/default.nix
+++ pkgs/tools/security/hashcash/default.nix
@@ -4,21 +4,18 @@ stdenv.mkDerivation rec {
   name = "hashcash-${version}";
   version = "1.22";
 
-  buildInputs = [ openssl ]; 
+  buildInputs = [ openssl ];
 
   src = fetchurl {
       url = "http://www.hashcash.org/source/hashcash-1.22.tgz";
       sha256 = "15kqaimwb2y8wvzpn73021bvay9mz1gqqfc40gk4hj6f84nz34h1";
   };
- 
+
   makeFlags = "generic-openssl LIBCRYPTO=-lcrypto";
 
   installPhase = ''
-    mkdir -p $out/bin
-    mkdir -p $out/share/man/man1  
-    install -m 0755 hashcash $out/bin
-    install -m 0755 sha1 $out/bin
-    install -m 0444 hashcash.1 $out/share/man/man1
+    install -D -m 0755 --target $out/bin hashcash sha1
+    install -D -m 0444 hashcash.1 $out/share/man/man1/hashcash.1
   '';
 
   meta = with stdenv.lib; {
