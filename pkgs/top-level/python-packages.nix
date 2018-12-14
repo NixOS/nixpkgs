bf40759eacc90874ad2f0eb5ed441ca091dd7ecb
tilpner <till@hoeppner.ws>
2018-12-14 15:47:02 +0100

    pythonPackages.pymdown-extensions: init at 6.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5026,6 +5026,8 @@ in {
 
   aiohue = callPackage ../development/python-modules/aiohue { };
 
+  pymdown-extensions = callPackage ../development/python-modules/pymdown-extensions { };
+
   PyMVGLive = callPackage ../development/python-modules/pymvglive { };
 
   coinmarketcap = callPackage ../development/python-modules/coinmarketcap { };
