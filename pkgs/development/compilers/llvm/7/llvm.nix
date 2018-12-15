adcf4aa5241f8dc37195d9bcbce27a80b913a512
Matthew Bauer <mjbauer95@gmail.com>
2018-12-15 12:14:19 -0600

    lldb: use provided INSTALL_NAME_DIR
    Fixes #47886
    
    We don’t want llvm to set the install name for us. It should be the
    one given to us.

--- pkgs/development/compilers/llvm/7/llvm.nix
+++ pkgs/development/compilers/llvm/7/llvm.nix
@@ -52,7 +52,7 @@ in stdenv.mkDerivation (rec {
 
   postPatch = optionalString stdenv.isDarwin ''
     substituteInPlace cmake/modules/AddLLVM.cmake \
-      --replace 'set(_install_name_dir INSTALL_NAME_DIR "@rpath")' "set(_install_name_dir INSTALL_NAME_DIR "$lib/lib")" \
+      --replace 'set(_install_name_dir INSTALL_NAME_DIR "@rpath")' "set(_install_name_dir)" \
       --replace 'set(_install_rpath "@loader_path/../lib" ''${extra_libdir})' ""
   ''
   # Patch llvm-config to return correct library path based on --link-{shared,static}.
