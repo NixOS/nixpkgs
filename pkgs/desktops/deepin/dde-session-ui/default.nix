1002cbd0677bcdd022923de3055bc8992ca20e39
R. RyanTM <ryantm-bot@ryantm.com>
2018-12-16 00:39:37 -0800

    deepin.dde-session-ui: 4.6.2 -> 4.7.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/dde-session-ui/versions

--- pkgs/desktops/deepin/dde-session-ui/default.nix
+++ pkgs/desktops/deepin/dde-session-ui/default.nix
@@ -6,13 +6,13 @@
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
   pname = "dde-session-ui";
-  version = "4.6.2";
+  version = "4.7.0";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "1fxlrj7vv7nqllwpwc8mxiv9bfqcj9b2qwkpjaq326pfmg5p5lhq";
+    sha256 = "0xnmk88s6hvc9vf1h67bywv5rknp1qglqdl9qbbhzralf11m08il";
   };
 
   nativeBuildInputs = [
