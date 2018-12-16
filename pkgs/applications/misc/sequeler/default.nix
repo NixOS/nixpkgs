6788f036ee1749d40c0471545faa41fd8e3f8cf2
Elis Hirwing <elis@hirwing.se>
2018-12-16 13:36:56 +0100

    sequeler: 0.6.4 -> 0.6.5
    Changelog: https://github.com/Alecaddd/sequeler/releases/tag/v0.6.5

--- pkgs/applications/misc/sequeler/default.nix
+++ pkgs/applications/misc/sequeler/default.nix
@@ -4,7 +4,7 @@
 
 
 let
-  version = "0.6.4";
+  version = "0.6.5";
   sqlGda = libgda.override {
     mysqlSupport = true;
     postgresSupport = true;
@@ -17,7 +17,7 @@ in stdenv.mkDerivation rec {
     owner = "Alecaddd";
     repo = "sequeler";
     rev = "v${version}";
-    sha256 = "1np7554535vwkjlk5kq4fpighfw92l4j35pfn1b2mgnh7xd6zhaj";
+    sha256 = "18d0dwrsn69fx1lwm6ihhk2r4996pxiy4hfv608gc1kl4s4f4sqp";
   };
 
   nativeBuildInputs = [ meson ninja pkgconfig vala gobject-introspection gettext wrapGAppsHook python3 desktop-file-utils ];
