6433f3b13b11c403065b86d43bb1d3ccceba6b71
Franz Pletz <fpletz@fnordicwalking.de>
2018-12-16 17:27:55 +0100

    nixos/tests: add clamav test

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -39,6 +39,7 @@ in
   cfssl = handleTestOn ["x86_64-linux"] ./cfssl.nix {};
   chromium = (handleTestOn ["x86_64-linux"] ./chromium.nix {}).stable or {};
   cjdns = handleTest ./cjdns.nix {};
+  clamav = handleTest ./clamav.nix {};
   cloud-init = handleTest ./cloud-init.nix {};
   codimd = handleTest ./codimd.nix {};
   containers-bridge = handleTest ./containers-bridge.nix {};
