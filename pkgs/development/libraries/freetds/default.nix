58f1f9f0fc56e1013b713db059e071b4cc65ea45
R. RyanTM <ryantm-bot@ryantm.com>
2018-12-15 22:40:54 -0800

    freetds: 1.00.104 -> 1.00.109
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/freetds/versions

--- pkgs/development/libraries/freetds/default.nix
+++ pkgs/development/libraries/freetds/default.nix
@@ -8,11 +8,11 @@ assert odbcSupport -> unixODBC != null;
 
 stdenv.mkDerivation rec {
   name = "freetds-${version}";
-  version = "1.00.104";
+  version = "1.00.109";
 
   src = fetchurl {
     url    = "http://www.freetds.org/files/stable/${name}.tar.bz2";
-    sha256 = "0mlg027mppv2348f4wwdpxpac9baqkdsg7xqx21kyx5dx5kmr71g";
+    sha256 = "0d00ixf78jzkyhccxjsaspz7yvlwk0xvrfcqfca4cwnwvnyb54ry";
   };
 
   buildInputs = [
