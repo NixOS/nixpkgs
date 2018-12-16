1c2328751c36aa97dd95560c62e30b86d9585bd8
R. RyanTM <ryantm-bot@ryantm.com>
2018-12-16 06:18:36 -0800

    deepin.dtkwidget: 2.0.9.9 -> 2.0.9.10
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/dtkwidget/versions

--- pkgs/desktops/deepin/dtkwidget/default.nix
+++ pkgs/desktops/deepin/dtkwidget/default.nix
@@ -5,13 +5,13 @@
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
   pname = "dtkwidget";
-  version = "2.0.9.9";
+  version = "2.0.9.10";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "1h4vm6a4lb6w6nkx2ns7a526mqyi9hqi7j5lqafd7ycwxlrc64nb";
+    sha256 = "0757dzy82bfv97b1gzkwa9zx3jzfbap20v3r1h7lkfcfw95410iw";
   };
 
   nativeBuildInputs = [
