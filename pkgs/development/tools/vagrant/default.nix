245ba973d9bbcf44deac8d6279ed2e2598ea4441
Matthew Harm Bekkema <mbekkema97@gmail.com>
2018-12-17 11:16:20 +1100

    vagrant: Install vagrant-libvirt as system plugin

--- pkgs/development/tools/vagrant/default.nix
+++ pkgs/development/tools/vagrant/default.nix
@@ -62,6 +62,11 @@ in buildRubyGem rec {
 
     mkdir -p "$out/vagrant-plugins/plugins.d"
     echo '{}' > "$out/vagrant-plugins/plugins.json"
+  '' +
+  lib.optionalString withLibvirt ''
+    substitute ${./vagrant-libvirt.json.in} $out/vagrant-plugins/plugins.d/vagrant-libvirt.json \
+      --subst-var-by ruby_version ${ruby.version} \
+      --subst-var-by vagrant_version ${version}
   '';
 
   installCheckPhase = ''
