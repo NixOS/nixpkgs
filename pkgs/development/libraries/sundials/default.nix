e60233db007a81eb846a39ca4a17db00d8d8cc26
R. RyanTM <ryantm-bot@ryantm.com>
2018-12-18 06:46:05 -0800

    sundials: 3.2.1 -> 4.0.0
    * sundials: 3.2.1 -> 4.0.0 (#52022)
    
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/sundials/versions
    
    * sundials: cleanup

--- pkgs/development/libraries/sundials/default.nix
+++ pkgs/development/libraries/sundials/default.nix
@@ -3,23 +3,24 @@
 stdenv.mkDerivation rec {
 
   pname = "sundials";
-  version = "3.2.1";
+  version = "4.0.0";
   name = "${pname}-${version}";
 
   src = fetchurl {
-  url = "https://computation.llnl.gov/projects/${pname}/download/${pname}-${version}.tar.gz";
-  sha256 = "0238r1qnwqz13wcjzfsbcfi8rfnlxcjjmxq2vpf2qf5jgablvna7";
+    url = "https://computation.llnl.gov/projects/${pname}/download/${pname}-${version}.tar.gz";
+    sha256 = "06cspmhx9qn7x722lmy9q2jr80hnnv2h7n54da7y5m951p1xfgcm";
   };
 
   preConfigure = ''
     export cmakeFlags="-DCMAKE_INSTALL_PREFIX=$out -DEXAMPLES_INSTALL_PATH=$out/share/examples $cmakeFlags"
   '';
   
-  buildInputs = [ cmake python ];
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ python ];
 
   meta = with stdenv.lib; {
     description = "Suite of nonlinear differential/algebraic equation solvers";
-    homepage    = https://computation.llnl.gov/casc/sundials/main.html;
+    homepage    = https://computation.llnl.gov/projects/sundials;
     platforms   = platforms.all;
     maintainers = [ maintainers.idontgetoutmuch ];
     license     = licenses.bsd3;
