59f8d67ec7bd70d6565ecbcf561b6eabf6cadda3
worldofpeace <worldofpeace@users.noreply.github.com>
2018-12-18 15:01:49 -0500

    playerctl: enable gtk-doc generation

--- pkgs/tools/audio/playerctl/default.nix
+++ pkgs/tools/audio/playerctl/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, meson, ninja, fetchFromGitHub, glib, pkgconfig, gobject-introspection }:
+{ stdenv, meson, ninja, fetchFromGitHub, glib, pkgconfig, gtk-doc, docbook_xsl, gobject-introspection }:
 
 stdenv.mkDerivation rec {
   name = "playerctl-${version}";
@@ -11,12 +11,8 @@ stdenv.mkDerivation rec {
     sha256 = "0j1fvcc80307ybl1z9l752sr4bcza2fmb8qdivpnm4xmm82faigb";
   };
 
-  nativeBuildInputs = [ meson ninja pkgconfig gobject-introspection ];
+  nativeBuildInputs = [ meson ninja pkgconfig gtk-doc docbook_xsl gobject-introspection ];
   buildInputs = [ glib ];
-
-  # docs somehow crashes the install phase:
-  # https://github.com/acrisci/playerctl/issues/85
-  mesonFlags = [ "-Dgtk-doc=false" ];
 
   meta = with stdenv.lib; {
     description = "Command-line utility and library for controlling media players that implement MPRIS";
