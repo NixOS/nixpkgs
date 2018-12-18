f500a83e93864a5b5641be49959932dd598b1a1d
R. RyanTM <ryantm-bot@ryantm.com>
2018-12-18 15:49:02 -0800

    qtbitcointrader: 1.40.23 -> 1.40.30
    * qtbitcointrader: 1.40.23 -> 1.40.30 (#52077)
    
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/qtbitcointrader/versions
    
    * qtbitcointrader: cleanup
    use fetchzip on GitHub archive tarball

--- pkgs/applications/misc/qtbitcointrader/default.nix
+++ pkgs/applications/misc/qtbitcointrader/default.nix
@@ -1,14 +1,14 @@
-{ stdenv, fetchurl, qt5 }:
+{ stdenv, fetchzip, qt5 }:
 
 let
-  version = "1.40.23";
+  version = "1.40.30";
 in
 stdenv.mkDerivation {
   name = "qtbitcointrader-${version}";
 
-  src = fetchurl {
+  src = fetchzip {
     url = "https://github.com/JulyIGHOR/QtBitcoinTrader/archive/v${version}.tar.gz";
-    sha256 = "11r2jzb09a62hf9fkg6aw8pg2js8c87k6lba9xz2q8n6d6jv44r1";
+    sha256 = "0xbgdmwb8d3lrddcnx2amfsknd3g408f5gy5mdydcm3vqqfi9a0c";
   };
 
   buildInputs = [ qt5.qtbase qt5.qtmultimedia qt5.qtscript ];
@@ -25,11 +25,11 @@ stdenv.mkDerivation {
     runHook postConfigure
   '';
 
-  meta = with stdenv.lib;
-    { description = "Bitcoin trading client";
-      homepage = https://centrabit.com/;
-      license = licenses.lgpl3;
-      platforms = qt5.qtbase.meta.platforms;
-      maintainers = [ maintainers.ehmry ];
-    };
+  meta = with stdenv.lib; {
+    description = "Bitcoin trading client";
+    homepage = https://centrabit.com/;
+    license = licenses.gpl3;
+    platforms = qt5.qtbase.meta.platforms;
+    maintainers = [ maintainers.ehmry ];
+  };
 }
