dd152dbd95cc016face19ae4a9c6e3d7460420fc
Will Dietz <w@wdtz.org>
2018-12-19 13:04:37 -0600

    iasl: 20180313 -> 20181213; match acpica-tools
    Not sure how valueable the separation is,
    but keep it for now and just bump this.
    
    Needs xen fix, coming soon...

--- pkgs/development/compilers/iasl/default.nix
+++ pkgs/development/compilers/iasl/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "iasl-${version}";
-  version = "20180313";
+  version = "20181213";
 
   src = fetchurl {
     url = "https://acpica.org/sites/acpica/files/acpica-unix-${version}.tar.gz";
-    sha256 = "05ab2xfv9wqwbzjaa9xqgrvvan87rxv29hw48h1gcckpc5smp2wm";
+    sha256 = "1vgqlv9pvxc52faxixpgz7hi1awqmj88bw5vqn3bldf6fmkh147w";
   };
 
   NIX_CFLAGS_COMPILE = "-O3";
