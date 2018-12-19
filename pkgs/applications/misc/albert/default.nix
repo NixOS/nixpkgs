4863e9e6fc0588c60371971f9c688e2ba254e757
Patrick Hilhorst <git@hilhorst.be>
2018-12-19 18:57:09 +0100

    albert: 0.14.22 -> 0.15.0

--- pkgs/applications/misc/albert/default.nix
+++ pkgs/applications/misc/albert/default.nix
@@ -1,21 +1,22 @@
 { mkDerivation, lib, fetchFromGitHub, makeWrapper, qtbase,
-  qtdeclarative, qtsvg, qtx11extras, muparser, cmake, python3 }:
+  qtdeclarative, qtsvg, qtx11extras, muparser, cmake, python3,
+  qt5 }:
 
 mkDerivation rec {
   pname = "albert";
-  version = "0.14.22";
+  version = "0.15.0";
 
   src = fetchFromGitHub {
     owner  = "albertlauncher";
     repo   = "albert";
     rev    = "v${version}";
-    sha256 = "0i9kss5szirmd0pzw3cm692kl9rhkan1zfywfqrjdf3i3b6914sg";
+    sha256 = "063z9yq6bsxcsqsw1n93ks5dzhzv6i252mjz1d5mxhxvgmqlfk0v";
     fetchSubmodules = true;
   };
 
   nativeBuildInputs = [ cmake makeWrapper ];
 
-  buildInputs = [ qtbase qtdeclarative qtsvg qtx11extras muparser python3 ];
+  buildInputs = [ qtbase qtdeclarative qtsvg qtx11extras muparser python3 qt5.qtcharts ];
 
   enableParallelBuilding = true;
 
