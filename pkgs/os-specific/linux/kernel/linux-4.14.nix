9caa69fbae07cbf981be2d4cec507a102b408444
Tim Steinbach <tim@nequissimus.com>
2018-12-21 09:10:54 -0500

    linux: 4.14.89 -> 4.14.90
    (cherry picked from commit dea57f15e9559056f96264e97271136f1ba476a6)

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.14.89";
+  version = "4.14.90";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "098b38518va6hhv5114vkyhyr2xzx01wxwib4dm0vpfx8jn1cvnf";
+    sha256 = "1jl6l7zi5dl1ahxj30m4wmnd05s61rxn8yfjkkc4mr45634x07hc";
   };
 } // (args.argsOverride or {}))
