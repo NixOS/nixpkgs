c9677e38be7ec1649324eb66560f3616ea36db0b
Jorge Acereda <jacereda@gmail.com>
2018-12-22 23:51:50 +0100

    sfml: relax Darwin dependencies

--- pkgs/development/libraries/sfml/default.nix
+++ pkgs/development/libraries/sfml/default.nix
@@ -16,10 +16,10 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [ cmake ];
-  buildInputs = [ libX11 freetype libjpeg openal flac libvorbis glew
-                  libXrandr libXrender xcbutilimage
-                ] ++ stdenv.lib.optional stdenv.isLinux udev
-                  ++ stdenv.lib.optionals stdenv.isDarwin [ IOKit Foundation AppKit OpenAL darwin.cf-private ];
+  buildInputs = [ freetype libjpeg openal flac libvorbis glew ]
+    ++ stdenv.lib.optional stdenv.isLinux udev
+    ++ stdenv.lib.optionals (!stdenv.isDarwin) [ libX11 libXrandr libXrender xcbutilimage ]
+    ++ stdenv.lib.optionals stdenv.isDarwin [ IOKit Foundation AppKit OpenAL darwin.cf-private ];
 
   cmakeFlags = [ "-DSFML_INSTALL_PKGCONFIG_FILES=yes"
                  "-DSFML_MISC_INSTALL_PREFIX=share/SFML"
