4861db0cde62a91621a7d12de7e02ca9e6e68e1b
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2018-12-22 17:33:41 -0500

    bzflag: enable on darwin

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20460,7 +20460,9 @@ in
 
   btanks = callPackage ../games/btanks { };
 
-  bzflag = callPackage ../games/bzflag { };
+  bzflag = callPackage ../games/bzflag {
+    inherit (darwin.apple_sdk.frameworks) Carbon CoreServices;
+  };
 
   cataclysm-dda = callPackage ../games/cataclysm-dda {
     inherit (darwin.apple_sdk.frameworks) Cocoa;
