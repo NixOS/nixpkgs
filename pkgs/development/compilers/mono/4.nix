1be47a2c001b16d1333016ad4e0e0504b0b9c7b5
obadz <obadz-git@obadz.com>
2018-12-23 17:59:57 +0000

    mono: renamed generic-cmake.nix -> generic.nix

--- pkgs/development/compilers/mono/4.nix
+++ pkgs/development/compilers/mono/4.nix
@@ -1,6 +1,6 @@
 { callPackage, Foundation, libobjc }:
 
-callPackage ./generic-cmake.nix (rec {
+callPackage ./generic.nix (rec {
   inherit Foundation libobjc;
   version = "4.8.1.0";
   sha256 = "1vyvp2g28ihcgxgxr8nhzyzdmzicsh5djzk8dk1hj5p5f2k3ijqq";
