da01f2a7451072b31daea827bbc1d401c703b70b
Tom Hunger <tehunger@gmail.com>
2018-12-23 19:20:39 +0000

    pyro-ppl: init at 0.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2416,6 +2416,8 @@ in {
     cudaSupport = pkgs.config.cudaSupport or false;
   };
 
+  pyro-ppl = callPackage ../development/python-modules/pyro-ppl {};
+
   opt-einsum = callPackage ../development/python-modules/opt-einsum {};
 
   pytorchWithCuda = self.pytorch.override {
