3b2506b04857a5bfc825a46f5ee15d0132056e65
Pascal Bach <pascal.bach@nextrem.ch>
2018-12-23 21:43:28 +0100

    rmlint: 2.6.1 -> 2.8.0
    The recipe now uses the default scons build phases.

--- pkgs/tools/misc/rmlint/default.nix
+++ pkgs/tools/misc/rmlint/default.nix
@@ -4,23 +4,24 @@
 with stdenv.lib;
 stdenv.mkDerivation rec {
   name = "rmlint-${version}";
-  version = "2.6.1";
+  version = "2.8.0";
 
   src = fetchFromGitHub {
     owner = "sahib";
     repo = "rmlint";
     rev = "v${version}";
-    sha256 = "1j09qk3zypw4my713q9g36kq37ggqd5v9vrs3h821p6p3qmmkdn8";
+    sha256 = "1gc7gbnh0qg1kl151cv1ld87vhpm1v3pnkn7prhscdcc21jrg8nz";
   };
 
-  configurePhase = "scons config";
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ gettext glib json-glib libelf scons sphinx utillinux ];
+  patches = [
+    ./blkid-hack.patch
+  ];
 
-  buildPhase = "scons";
+  nativeBuildInputs = [ pkgconfig sphinx ];
+  buildInputs = [ gettext glib json-glib libelf scons utillinux ];
 
-  installPhase = "scons --prefix=$out install";
+  prefixKey = "--prefix=";
 
   meta = {
     description = "Extremely fast tool to remove duplicates and other lint from your filesystem";
