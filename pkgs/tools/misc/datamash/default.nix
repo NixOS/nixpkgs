81e44b093005fb3f91048f09b425b49b546ccab6
Will Dietz <w@wdtz.org>
2018-12-23 10:52:29 -0600

    datamash: 1.3 -> 1.4
    https://lists.gnu.org/archive/html/info-gnu/2018-12/msg00014.html

--- pkgs/tools/misc/datamash/default.nix
+++ pkgs/tools/misc/datamash/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "datamash-${version}";
-  version = "1.3";
+  version = "1.4";
 
   src = fetchurl {
     url = "mirror://gnu/datamash/${name}.tar.gz";
-    sha256 = "19jpcv4ryvbz0476d6dgpxpbjkpmih4q84wj06nslls338bm5fzf";
+    sha256 = "fa44dd2d5456bcb94ef49dfc6cfe62c83fd53ac435119a85d34e6812f6e6472a";
   };
 
   meta = with stdenv.lib; {
