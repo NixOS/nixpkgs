8aea3e4ca6ae4bf2eef011fdb4622f8dc2ecf1fa
Jan Tojnar <jtojnar@gmail.com>
2018-12-23 02:25:20 +0100

    poppler: 0.67.0 → 0.72.0

--- pkgs/development/libraries/poppler/default.nix
+++ pkgs/development/libraries/poppler/default.nix
@@ -8,7 +8,7 @@
 }:
 
 let # beware: updates often break cups-filters build
-  version = "0.67.0";
+  version = "0.72.0";
   mkFlag = optset: flag: "-DENABLE_${flag}=${if optset then "on" else "off"}";
 in
 stdenv.mkDerivation rec {
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "${meta.homepage}/poppler-${version}.tar.xz";
-    sha256 = "1yb6agmcxf0ixqm65d4aknl0hgmswf94x0k59ic0qqav1wd4yjm3";
+    sha256 = "0lfs1b1jfamxl13zbl5n448dqvl9n8frbv8180y7b7kfyaw7wx61";
   };
 
   outputs = [ "out" "dev" ];
