52cf2dd4a06d88b12705cdc47afd2ecfcc0514eb
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-12-25 14:44:48 -0500

    linuxPackages: 4.14 -> 4.19

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14683,7 +14683,7 @@ in
   });
 
   # The current default kernel / kernel modules.
-  linuxPackages = linuxPackages_4_14;
+  linuxPackages = linuxPackages_4_19;
   linux = linuxPackages.kernel;
 
   # Update this when adding the newest kernel major version!
