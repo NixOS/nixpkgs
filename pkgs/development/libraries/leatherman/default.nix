a14d09f2ca754d03fc079fcce4ba5e0910218874
R. RyanTM <ryantm-bot@ryantm.com>
2018-12-25 10:29:53 -0800

    leatherman: 1.5.3 -> 1.5.4
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/leatherman/versions

--- pkgs/development/libraries/leatherman/default.nix
+++ pkgs/development/libraries/leatherman/default.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   name = "leatherman-${version}";
-  version = "1.5.3";
+  version = "1.5.4";
 
   src = fetchFromGitHub {
-    sha256 = "04b2wii5d0ypar8wrk0msybdq01z1r23xsvnn67bi2mffvczi5l2";
+    sha256 = "08hd6j8w4mgnxj84y26vip1vgrg668jnil5jzq2dk4pfapigfz8l";
     rev = version;
     repo = "leatherman";
     owner = "puppetlabs";
