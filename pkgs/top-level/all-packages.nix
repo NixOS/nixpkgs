855d370a2036aecd2029f21012149772a55fb99c
Andrew Dunham <andrew@du.nham.ca>
2018-12-25 15:13:44 -0500

    gping: init at 1.1
    gping is a utility to display a graph of ping timing in a terminal

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1477,6 +1477,8 @@ in
 
   gosu = callPackage ../tools/misc/gosu { };
 
+  gping = callPackage ../tools/networking/gping { };
+
   greg = callPackage ../applications/audio/greg {
     pythonPackages = python3Packages;
   };
