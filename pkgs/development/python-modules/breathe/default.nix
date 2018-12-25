04b54453417945bd20a57a28aeaa35d28315b8fe
R. RyanTM <ryantm-bot@ryantm.com>
2018-12-24 20:49:48 -0800

    python37Packages.breathe: 4.11.0 -> 4.11.1
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-breathe/versions

--- pkgs/development/python-modules/breathe/default.nix
+++ pkgs/development/python-modules/breathe/default.nix
@@ -1,12 +1,12 @@
 { lib, fetchPypi, buildPythonPackage, docutils, six, sphinx, isPy3k }:
 
 buildPythonPackage rec {
-  version = "4.11.0";
+  version = "4.11.1";
   pname = "breathe";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "05x3qrvsriy0cn0p4bxnzhp27pvxbq2vxlxncr2wqh003gpbp4fa";
+    sha256 = "1mps0cfli6iq2gqsv3d24fs1cp7sq7crd9ji6lw63b9r40998ylv";
   };
 
   propagatedBuildInputs = [ docutils six sphinx ];
