2102ff090d07fc388eccb2be75c3cdf50369c0da
Orivej Desh <orivej@gmx.fr>
2018-12-26 02:41:38 +0000

    pythonPackages.jsonrpclib-pelix: disable tests
    Version 0.3.2 has started to advertise the tests in setup.py, but there are no
    tests in the PyPI distribution.

--- pkgs/development/python-modules/jsonrpclib-pelix/default.nix
+++ pkgs/development/python-modules/jsonrpclib-pelix/default.nix
@@ -12,6 +12,8 @@ buildPythonPackage rec {
     sha256 = "14d288d1b3d3273cf96a729dd21a2470851c4962be8509f3dd62f0137ff90339";
   };
 
+  doCheck = false; # test_suite="tests" in setup.py but no tests in pypi.
+
   meta = with lib; {
     description = "JSON RPC client library - Pelix compatible fork";
     homepage = https://pypi.python.org/pypi/jsonrpclib-pelix/;
