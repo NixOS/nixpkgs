3f93d68ebbf7488edbb578afc905a6fc3df1747e
Frederik Rietdijk <fridh@fridh.nl>
2018-12-26 12:57:19 +0100

    python.pkgs.pywal: hardcode path to feh

--- pkgs/development/python-modules/pywal/default.nix
+++ pkgs/development/python-modules/pywal/default.nix
@@ -9,11 +9,6 @@ python3Packages.buildPythonApplication rec {
     sha256 = "1pj30h19ijwhmbm941yzbkgr19q06dhp9492h9nrqw1wfjfdbdic";
   };
 
-  # necessary for imagemagick to be found during tests
-  buildInputs = [ imagemagick ];
-
-  makeWrapperArgs = [ "--prefix PATH : ${lib.makeBinPath [ imagemagick feh ]}" ];
-
   preCheck = ''
     mkdir tmp
     HOME=$PWD/tmp
@@ -21,10 +16,12 @@ python3Packages.buildPythonApplication rec {
 
   patches = [
     ./convert.patch
+    ./feh.patch
   ];
 
   postPatch = ''
     substituteInPlace pywal/backends/wal.py --subst-var-by convert "${imagemagick}/bin/convert"
+    substituteInPlace pywal/wallpaper.py --subst-var-by feh "${feh}/bin/feh"
   '';
 
   meta = with lib; {
