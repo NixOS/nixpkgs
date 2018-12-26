NixOS/nixpkgs

commit e42495282650010acabaad76e5b7f27de4315665

python: goobook: 1.9 -> 3.1

pkgs/development/python-modules/goobook/default.nix
@@ -13,12 +13,12 @@
 
 buildPythonPackage rec {
   pname = "goobook";
-  version = "1.9";
+  version = "3.1";
   disabled = isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "02xmq8sjavza17av44ks510934wrshxnsm6lvhvazs45s92b671i";
+    sha256 = "139a98d646d5c5963670944d5cfcc1a107677ee11fa98329221bd600457fda6d";
   };
 
   propagatedBuildInputs = [ oauth2client gdata simplejson httplib2 keyring six rsa ];

commit 4f909b59b46e273cfb0682df6131e3e3124e0833

pythonPackages.goobook: fix build
Broken by e42495282650010acabaad76e5b7f27de4315665 in #49741.

pkgs/development/python-modules/goobook/default.nix
@@ -4,6 +4,7 @@
 , isPy3k
 , oauth2client
 , gdata
+, google_api_python_client
 , simplejson
 , httplib2
 , keyring
@@ -21,7 +22,7 @@ buildPythonPackage rec {
     sha256 = "139a98d646d5c5963670944d5cfcc1a107677ee11fa98329221bd600457fda6d";
   };
 
-  propagatedBuildInputs = [ oauth2client gdata simplejson httplib2 keyring six rsa ];
+  propagatedBuildInputs = [ oauth2client gdata google_api_python_client simplejson httplib2 keyring six rsa ];
 
   preConfigure = ''
     sed -i '/distribute/d' setup.py