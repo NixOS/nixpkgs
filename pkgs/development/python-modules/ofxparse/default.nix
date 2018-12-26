NixOS/nixpkgs

commit 0fde2cf4e140d6ca93303bb4622195176ffa94b2

python: ofxparse: 0.14 -> 0.19

pkgs/development/python-modules/ofxparse/default.nix
@@ -7,11 +7,11 @@
 
 buildPythonPackage rec {
   pname = "ofxparse";
-  version = "0.14";
+  version = "0.19";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "d8c486126a94d912442d040121db44fbc4a646ea70fa935df33b5b4dbfbbe42a";
+    sha256 = "d8c81fd5089332106da1a2e8919c412c7c677f08af04d557ca767701a04e0918";
   };
 
   propagatedBuildInputs = [ six beautifulsoup4 ];

commit 669705c6205a7feccf4c3ff52a8e5448571b87d1

pythonPackages.ofxparse: fix build
Broken by 0fde2cf4e140d6ca93303bb4622195176ffa94b2 in #49741.

pkgs/development/python-modules/ofxparse/default.nix
@@ -3,6 +3,7 @@
 , fetchPypi
 , six
 , beautifulsoup4
+, lxml
 }:
 
 buildPythonPackage rec {
@@ -14,7 +15,7 @@ buildPythonPackage rec {
     sha256 = "d8c81fd5089332106da1a2e8919c412c7c677f08af04d557ca767701a04e0918";
   };
 
-  propagatedBuildInputs = [ six beautifulsoup4 ];
+  propagatedBuildInputs = [ six beautifulsoup4 lxml ];
 
   meta = with stdenv.lib; {
     homepage = "http://sites.google.com/site/ofxparse";