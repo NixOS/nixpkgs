NixOS/nixpkgs

commit 8600798172e6a4c526d593ec8da48ce28cf0d15c

python: taskw: 1.0.3 -> 1.2.0

pkgs/development/python-modules/taskw/default.nix
@@ -10,12 +10,12 @@
 }:
 
 buildPythonPackage rec {
-  version = "1.0.3";
+  version = "1.2.0";
   pname = "taskw";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1fa7bv5996ppfbryv02lpnlhk5dra63lhlwrb1i4ifqbziqfqh5n";
+    sha256 = "fadd8afc12df026c3c2d39b633c55d3337f7dca95602fce2239455a048bc85fe";
   };
 
   patches = [ ./use-template-for-taskwarrior-install-path.patch ];

commit 3fd67fbb42d94fefe32af5955a546d146b5f7eb8

pythonPackages.taskw: fix build
Broken by 8600798172e6a4c526d593ec8da48ce28cf0d15c in #49741.

pkgs/development/python-modules/taskw/default.nix
@@ -5,6 +5,7 @@
 , tox
 , six
 , dateutil
+, kitchen
 , pytz
 , pkgs
 }:
@@ -28,7 +29,7 @@ buildPythonPackage rec {
   doCheck = false;
 
   buildInputs = [ nose pkgs.taskwarrior tox ];
-  propagatedBuildInputs = [ six dateutil pytz ];
+  propagatedBuildInputs = [ six dateutil kitchen pytz ];
 
   meta = with stdenv.lib; {
     homepage =  https://github.com/ralphbean/taskw;

pkgs/development/python-modules/taskw/use-template-for-taskwarrior-install-path.patch
@@ -4,45 +4,11 @@ Date:   Sun Nov 16 15:19:38 2014 +0100
 
     Use a template for taskwarrior install path.
 
-diff --git a/taskw/warrior.py b/taskw/warrior.py
-index 21dde88..e0509f2 100644
 --- a/taskw/warrior.py
 +++ b/taskw/warrior.py
-@@ -451,17 +451,17 @@ class TaskWarriorShellout(TaskWarriorBase):
-     def _execute(self, *args):
-         """ Execute a given taskwarrior command with arguments
- 
-         Returns a 2-tuple of stdout and stderr (respectively).
- 
-         """
-         command = (
-             [
+@@ -449 +449 @@ class TaskWarriorShellout(TaskWarriorBase):
 -                'task',
 +                '@@taskwarrior@@/bin/task',
-                 'rc:%s' % self.config_filename,
-             ]
-             + self.get_configuration_override_args()
-             + [six.text_type(arg) for arg in args]
-         )
- 
-         # subprocess is expecting bytestrings only, so nuke unicode if present
-         for i in range(len(command)):
-@@ -525,17 +525,17 @@ class TaskWarriorShellout(TaskWarriorBase):
-         except OSError:
-             # OSError is raised if subprocess.Popen fails to find
-             # the executable.
-             return False
- 
-     @classmethod
-     def get_version(cls):
-         taskwarrior_version = subprocess.Popen(
--            ['task', '--version'],
-+            ['@@taskwarrior@@/bin/task', '--version'],
-             stdout=subprocess.PIPE
-         ).communicate()[0]
-         return LooseVersion(taskwarrior_version.decode())
- 
-     def sync(self, init=False):
-         if self.get_version() < LooseVersion('2.3'):
-             raise UnsupportedVersionException(
-                 "'sync' requires version 2.3 of taskwarrior or later."
+@@ -553 +553 @@ class TaskWarriorShellout(TaskWarriorBase):
+-                ['task', '--version'],
++                ['@@taskwarrior@@/bin/task', '--version'],