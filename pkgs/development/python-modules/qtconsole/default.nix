NixOS/nixpkgs

commit d7ef9a710716e737bd86be1adbefc0418b585d64

python36Packages.qtconsole: 4.4.2 -> 4.4.3
* python36Packages.qtconsole: 4.4.2 -> 4.4.3 (#50590)

Semi-automatic update generated by
https://github.com/ryantm/nixpkgs-update tools. This update was made
based on information from
https://repology.org/metapackage/python3.6-qtconsole/versions

* pythonPackages.qtconsole: linux platforms only

pkgs/development/python-modules/qtconsole/default.nix
@@ -14,11 +14,11 @@
 
 buildPythonPackage rec {
   pname = "qtconsole";
-  version = "4.4.2";
+  version = "4.4.3";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1yihnxya9kll24fp4a929mria930i9r20kx43sjjwh92gcb2k9gs";
+    sha256 = "1b03n1ixzscm0jw97l4dq5iy4fslnqxq5bb8287xb7n2a1gs26xw";
   };
 
   buildInputs = [ nose ] ++ lib.optionals isPy27 [mock];
@@ -31,6 +31,7 @@ buildPythonPackage rec {
     description = "Jupyter Qt console";
     homepage = http://jupyter.org/;
     license = lib.licenses.bsd3;
+    platforms = lib.platforms.linux; # fails on Darwin
     maintainers = with lib.maintainers; [ fridh ];
   };
 }

commit 97ec7a9ab9ec7b86a243d775c8c140db3055c492

pythonPackages.qtconsole: build on darwin
Fixes: d7ef9a71071 ('python36Packages.qtconsole: 4.4.2 -> 4.4.3')

pkgs/development/python-modules/qtconsole/default.nix
@@ -31,7 +31,7 @@ buildPythonPackage rec {
     description = "Jupyter Qt console";
     homepage = http://jupyter.org/;
     license = lib.licenses.bsd3;
-    platforms = lib.platforms.linux; # fails on Darwin
+    platforms = lib.platforms.unix;
     maintainers = with lib.maintainers; [ fridh ];
   };
 }