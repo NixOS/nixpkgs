b86a583592b0fcefd3d6b71670a32ed7fc5a93a2
Anton-Latukha <anton.latukha@gmail.com>
2018-12-28 16:45:00 +0200

    mp4v2: add maintainer
    add

--- pkgs/development/libraries/mp4v2/default.nix
+++ pkgs/development/libraries/mp4v2/default.nix
@@ -32,7 +32,7 @@ stdenv.mkDerivation rec {
       This container format is derived from Apple's QuickTime format.
     '';
     homepage = https://code.google.com/archive/p/mp4v2/;
-    maintainers = [ ];
+    maintainers = [ lib.maintainers.Anton-Latukha ];
     platforms = lib.platforms.unix;
     license = lib.licenses.mpl11;
   };
