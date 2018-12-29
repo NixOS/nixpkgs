406cb1a2c1888d0769138ce306de6b116f3deb0e
CrazedProgrammer <crazedprogrammer@gmail.com>
2018-12-29 15:26:08 +0100

    curlpp: init at 0.8.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9535,6 +9535,8 @@ in
 
   curlcpp = callPackage ../development/libraries/curlcpp { };
 
+  curlpp = callPackage ../development/libraries/curlpp { };
+
   cutee = callPackage ../development/libraries/cutee { };
 
   cutelyst = libsForQt5.callPackage ../development/libraries/cutelyst { };
