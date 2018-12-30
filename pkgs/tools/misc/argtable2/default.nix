dcd020395befa9b11b66c4ebbc026eb9d4b2730d
Robret Soeldner <r.soeldner@gmail.com>
2018-12-30 15:56:19 +0100

    Fix description and url

--- pkgs/tools/misc/argtable2/default.nix
+++ pkgs/tools/misc/argtable2/default.nix
@@ -7,13 +7,13 @@ stdenv.mkDerivation rec {
   version = "2.13";
 
   src = fetchurl {
-    url = http://prdownloads.sourceforge.net/argtable/argtable2-13.tar.gz;
+    url = mirror://sourceforge/argtable/argtable2-13.tar.gz;
     sha256 = "1gyxf4bh9jp5gb3l6g5qy90zzcf3vcpk0irgwbv1lc6mrskyhxwg";
   };
 
   meta = with stdenv.lib; {
     homepage = https://www.argtable.org/;
-    description = "A Cross-Platform, Single-File, ANSI C Command-Line Parsing Library";
+    description = "A Cross-Platform, ANSI C Command-Line Parsing Library";
     license = licenses.bsd3;
     maintainers = with maintainers; [ rsoeldner ];
   };
