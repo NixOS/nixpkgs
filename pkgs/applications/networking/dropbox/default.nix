a3410cf1043585a72e87f3f85a38277c89e11286
Tal Walter <talw@users.noreply.github.com>
2018-12-30 17:58:44 +0900

    dropbox: 55.4.171 -> 63.4.107 (#53062)
    Dropbox version 55.4.171 is too old which results in an error when doing
    the initial account linking.
    
    (cherry picked from commit d5dbfad91add6a0e51fd7619322b5382b8ad7074)
    Reason: To have a usable dropbox package for the next release.

--- pkgs/applications/networking/dropbox/default.nix
+++ pkgs/applications/networking/dropbox/default.nix
@@ -7,7 +7,7 @@ assert lib.elem stdenv.hostPlatform.system platforms;
 # Dropbox client to bootstrap installation.
 # The client is self-updating, so the actual version may be newer.
 let
-  version = "55.4.171";
+  version = "63.4.107";
 
   arch = {
     "x86_64-linux" = "x86_64";
