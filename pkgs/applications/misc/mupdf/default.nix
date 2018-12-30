70fc5edacb59b0d8d2eeed0f89248cb16f69910b
Daniel Fullmer <danielrf12@gmail.com>
2018-12-29 23:12:08 -0500

    mupdf: Fix changed library name

--- pkgs/applications/misc/mupdf/default.nix
+++ pkgs/applications/misc/mupdf/default.nix
@@ -67,7 +67,7 @@ in stdenv.mkDerivation rec {
     Name: mupdf
     Description: Library for rendering PDF documents
     Version: ${version}
-    Libs: -L$out/lib -lmupdf -lmupdfthird
+    Libs: -L$out/lib -lmupdf -lmupdf-third
     Cflags: -I$dev/include
     EOF
 
