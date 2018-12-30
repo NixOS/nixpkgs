8d7b4bd5db50b093c046d9f43c1497acebc6fd93
worldofpeace <worldofpeace@users.noreply.github.com>
2018-12-30 17:42:21 +0700

    Update pkgs/applications/networking/browsers/luakit/default.nix
    Co-Authored-By: emmanuelrosa <emmanuelrosa@users.noreply.github.com>

--- pkgs/applications/networking/browsers/luakit/default.nix
+++ pkgs/applications/networking/browsers/luakit/default.nix
@@ -27,7 +27,7 @@ in stdenv.mkDerivation rec {
     gst_all_1.gstreamer gst_all_1.gst-plugins-base
     gst_all_1.gst-plugins-good gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-ugly
     gst_all_1.gst-libav
-    glib-networking
+    glib-networking # TLS support
   ];
 
   postPatch =
