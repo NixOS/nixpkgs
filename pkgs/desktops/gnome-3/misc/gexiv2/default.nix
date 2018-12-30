f36a2adf824d38b971bfd15bab760ea51e83d8cd
Vladimír Čunát <vcunat@gmail.com>
2018-12-30 10:12:24 +0100

    gnome3.gexiv2: fix build with exiv2-0.27

--- pkgs/desktops/gnome-3/misc/gexiv2/default.nix
+++ pkgs/desktops/gnome-3/misc/gexiv2/default.nix
@@ -1,4 +1,6 @@
-{ stdenv, fetchurl, meson, ninja, pkgconfig, exiv2, glib, gnome3, gobject-introspection, vala }:
+{ stdenv, fetchurl, meson, ninja, pkgconfig, exiv2, glib, gnome3, gobject-introspection, vala
+, fetchpatch
+}:
 
 let
   pname = "gexiv2";
@@ -11,6 +13,14 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
     sha256 = "1vf0zv92p9hybdhn7zx53h3ia53ph97a21xz8rfk877xlr5261l8";
   };
+
+  patches = [
+    (fetchpatch { # should be included in the following release
+      name = "exiv-0.27-includes.diff";
+      url = "https://gitlab.gnome.org/GNOME/gexiv2/commit/d8f96634e1df6.diff";
+      sha256 = "144q23czbmrnfwhyc3s23ri33lf736a1a8mpq1rnmmk4ib1b8qg1";
+    })
+  ];
 
   preConfigure = ''
     patchShebangs .
