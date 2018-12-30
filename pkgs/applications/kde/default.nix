cbf93deee506a4e4fad65b58f4c8fcf0e7eef472
Trolli Schmittlauch <t.schmittlauch+nixos@orlives.de>
2018-12-30 03:12:43 +0100

    kcharselect: initial packaging of v18.12.0
    Packaging `kcharselect` because a KDE tool for inserting special
    characters is very useful.
    
    (cherry picked from commit 25fcb9099d27c5c8561921336d3fa5bd040e9779)

--- pkgs/applications/kde/default.nix
+++ pkgs/applications/kde/default.nix
@@ -93,6 +93,7 @@ let
       kcalc = callPackage ./kcalc.nix {};
       kcalcore = callPackage ./kcalcore.nix {};
       kcalutils = callPackage ./kcalutils.nix {};
+      kcharselect = callPackage ./kcharselect.nix {};
       kcolorchooser = callPackage ./kcolorchooser.nix {};
       kcontacts = callPackage ./kcontacts.nix {};
       kdav = callPackage ./kdav.nix {};
