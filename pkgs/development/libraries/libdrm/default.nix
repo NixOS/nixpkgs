349f63b0b946563997a3a491b74f6e17dde44bd2
Matthew Bauer <mjbauer95@gmail.com>
2018-12-31 13:48:47 -0600

    libdrm: remove darwin patch
    Not needed with clock_gettime available to us.

--- pkgs/development/libraries/libdrm/default.nix
+++ pkgs/development/libraries/libdrm/default.nix
@@ -14,16 +14,11 @@ stdenv.mkDerivation rec {
   buildInputs = [ libpthreadstubs libpciaccess valgrind-light ];
     # libdrm as of 2.4.70 does not actually do anything with udev.
 
-  patches = stdenv.lib.optional stdenv.isDarwin ./libdrm-apple.patch;
-
   postPatch = ''
     for a in */*-symbol-check ; do
       patchShebangs $a
     done
   '';
-
-  preConfigure = stdenv.lib.optionalString stdenv.isDarwin
-    "echo : \\\${ac_cv_func_clock_gettime=\'yes\'} > config.cache";
 
   configureFlags = [ "--enable-install-test-programs" ]
     ++ stdenv.lib.optionals (stdenv.isAarch32 || stdenv.isAarch64)
