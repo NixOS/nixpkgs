aedef387980c10a17b6d610250e713c12524fa01
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2018-12-31 12:54:27 +0100

    Bump teamviewer version to 14

--- pkgs/applications/networking/remote/teamviewer/default.nix
+++ pkgs/applications/networking/remote/teamviewer/default.nix
@@ -1,13 +1,13 @@
-{ stdenv, fetchurl, autoPatchelfHook, makeWrapper, xdg_utils, dbus, qtbase, qtwebkit, qtx11extras, qtquickcontrols, glibc, libXrandr, libX11 }:
+{ stdenv, fetchurl, autoPatchelfHook, makeWrapper, xdg_utils, dbus, qt56, glibc, libXrandr, libX11 }:
 
 
 stdenv.mkDerivation rec {
   name = "teamviewer-${version}";
-  version = "13.1.3026";
+  version = "14.1.3399";
 
   src = fetchurl {
-    url = "https://dl.tvcdn.de/download/linux/version_13x/teamviewer_${version}_amd64.deb";
-    sha256 = "14zaa1xjdfmgbbq40is5mllqcd9zan03sblkzajswd5gps7crsik";
+    url = "https://dl.tvcdn.de/download/linux/version_14x/teamviewer_${version}_amd64.deb";
+    sha256 = "45f341f07240e2f84e054d5a56f818db140d5bb6da1a3abe53230a1d656cd698";
   };
 
   unpackPhase = ''
@@ -16,8 +16,8 @@ stdenv.mkDerivation rec {
   '';
 
   nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
-  buildInputs = [ dbus qtbase qtwebkit qtx11extras libX11 ];
-  propagatedBuildInputs = [ qtquickcontrols ];
+  buildInputs = [ dbus qt56.qtbase qt56.qtwebkit qt56.qtx11extras libX11 ];
+  propagatedBuildInputs = [ qt56.qtquickcontrols ];
 
   installPhase = ''
     mkdir -p $out/share/teamviewer $out/bin $out/share/applications
