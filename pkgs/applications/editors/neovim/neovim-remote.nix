1c78a0313d75ca368c96c0dcdb5dde83ff7d05e1
R. RyanTM <ryantm-bot@ryantm.com>
2019-01-01 10:35:21 -0800

    neovim-remote: 2.1.1 -> 2.1.3 (#52798)
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-neovim-remote/versions

--- pkgs/applications/editors/neovim/neovim-remote.nix
+++ pkgs/applications/editors/neovim/neovim-remote.nix
@@ -4,14 +4,14 @@ with stdenv.lib;
 
 pythonPackages.buildPythonPackage rec {
   pname = "neovim-remote";
-  version = "2.1.1";
+  version = "2.1.3";
   disabled = !pythonPackages.isPy3k;
 
   src = fetchFromGitHub {
     owner = "mhinz";
     repo = "neovim-remote";
     rev = "v${version}";
-    sha256 = "1hkzcc141imjin03wpfykw50k0vs7vj1lr09czb2hsyf937gyjqn";
+    sha256 = "0nx987af29ajlpwnwfc3z8gplxv69gj53s4bzm6pwwsfbhfakdah";
   };
 
   propagatedBuildInputs = with pythonPackages; [ pynvim psutil ];
