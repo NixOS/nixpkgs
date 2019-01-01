eca35b8283326cae1b20c1428527be337795dcca
Will Dietz <w@wdtz.org>
2018-12-31 22:25:39 -0600

    pango: 1.42.4 -> 1.43.0
    * use meson, fix build (required now)
    
    * Add flag to enable doc building to match
      previous behavior and particularly ensure
      output 'devdoc' is created/used :).
    
    * patch from upstream commit to fix linking

--- pkgs/development/libraries/pango/default.nix
+++ pkgs/development/libraries/pango/default.nix
@@ -1,24 +1,25 @@
 { stdenv, fetchurl, pkgconfig, libXft, cairo, harfbuzz
 , libintl, gobject-introspection, darwin, fribidi, gnome3
 , gtk-doc, docbook_xsl, docbook_xml_dtd_43, makeFontsConf, freefont_ttf
+, meson, ninja
 }:
 
 with stdenv.lib;
 
 let
   pname = "pango";
-  version = "1.42.4";
+  version = "1.43.0";
 in stdenv.mkDerivation rec {
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "17bwb7dgbncrfsmchlib03k9n3xaalirb39g3yb43gg8cg6p8aqx";
+    sha256 = "1lnxldmv1a12dq5h0dlq5jyzl4w75k76dp8cn360x2ijlm9w5h6j";
   };
 
   outputs = [ "bin" "dev" "out" "devdoc" ];
 
-  nativeBuildInputs = [ pkgconfig gobject-introspection gtk-doc docbook_xsl docbook_xml_dtd_43 ];
+  nativeBuildInputs = [ pkgconfig gobject-introspection gtk-doc docbook_xsl docbook_xml_dtd_43 meson ninja ];
   buildInputs = optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
     Carbon
     CoreGraphics
@@ -26,7 +27,11 @@ in stdenv.mkDerivation rec {
   ]);
   propagatedBuildInputs = [ cairo harfbuzz libXft libintl fribidi ];
 
+  mesonFlags = [ "-Denable_docs=true" ];
+
   enableParallelBuilding = true;
+
+  patches = [ ./gobject-linking.patch ];
 
   # Fontconfig error: Cannot load default config file
   FONTCONFIG_FILE = makeFontsConf {
