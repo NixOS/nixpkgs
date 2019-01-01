7bf9f243e8f88e67bd75ca9c094509e557389e3d
Olli Helenius <liff@iki.fi>
2019-01-01 12:57:40 +0200

    aspell: add all missing official dictionaries
    From:
    
      https://ftp.gnu.org/gnu/aspell/dict/0index.html
    
    Use a specialized function, buildOfficialDict to specialize builds of
    official aspell dictionaries and add an update script.

--- pkgs/development/libraries/aspell/default.nix
+++ pkgs/development/libraries/aspell/default.nix
@@ -2,6 +2,17 @@
 , searchNixProfiles ? true
 }:
 
+let
+
+  # Source for u-deva.cmap and u-deva.cset: use the Marathi
+  # dictionary like Debian does.
+  devaMapsSource = fetchTarball {
+    url = "ftp://ftp.gnu.org/gnu/aspell/dict/mr/aspell6-mr-0.10-0.tar.bz2";
+    sha256 = "1v8cdl8x2j1d4vbvsq1xrqys69bbccd6mi03fywrhkrrljviyri1";
+  };
+
+in
+
 stdenv.mkDerivation rec {
   name = "aspell-0.60.6.1";
 
@@ -31,6 +42,13 @@ stdenv.mkDerivation rec {
       --enable-pkglibdir=$out/lib/aspell
       --enable-pkgdatadir=$out/lib/aspell
     );
+  '';
+
+  # Include u-deva.cmap and u-deva.cset in the aspell package
+  # to avoid conflict between 'mr' and 'hi' dictionaries as they
+  # both include those files.
+  postInstall = ''
+    cp ${devaMapsSource}/u-deva.{cmap,cset} $out/lib/aspell/
   '';
 
   meta = {
