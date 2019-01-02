6677b40c99908fb5d8e90b0b87fd57bf7a912a01
José Romildo Malaquias <malaquias@gmail.com>
2019-01-02 09:49:41 -0200

    xkbmon: 0.1 -> 0.2

--- pkgs/applications/misc/xkbmon/default.nix
+++ pkgs/applications/misc/xkbmon/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "xkbmon-${version}";
-  version = "0.1";
+  version = "0.2";
 
   src = fetchFromGitHub {
     owner = "xkbmon";
     repo = "xkbmon";
     rev = version;
-    sha256 = "1smyqsd9cpbzqaplm221a8mq0nham6rg6hjsm9g5gph94xmk6d67";
+    sha256 = "1x2xwak0yp0xkl63jzz3k1pf074mh9yxgppwwm96ms3zaslq44yp";
   };
 
   buildInputs = [ libX11 ];
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     homepage = https://github.com/xkbmon/xkbmon;
     description = "Command-line keyboard layout monitor for X11";
-    license = licenses.gpl3;
+    license = licenses.mit;
     platforms = platforms.linux;
     maintainers = [ maintainers.romildo ];
   };
