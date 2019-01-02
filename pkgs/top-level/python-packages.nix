53a0bff01fce3a8ff72e95c4ed0b92581fe2ba5e
Bjørn Forsman <bjorn.forsman@gmail.com>
2019-01-02 08:11:28 +0100

    pythonPackages.blockdiagcontrib-cisco: init at 0.1.8
    blockdiagcontrib-cisco is a noderenderer plugin for blockdiag containing
    Cisco networking symbols.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1129,6 +1129,8 @@ in {
 
   blockdiag = callPackage ../development/python-modules/blockdiag { };
 
+  blockdiagcontrib-cisco = callPackage ../development/python-modules/blockdiagcontrib-cisco { };
+
   bpython = callPackage ../development/python-modules/bpython {};
 
   bsddb3 = callPackage ../development/python-modules/bsddb3 { };
