eb641d59d5444bb2ec1d04101c5bc73fc4ca8c99
R. RyanTM <ryantm-bot@ryantm.com>
2019-01-02 13:05:40 -0800

    python37Packages.ldap3: 2.5.1 -> 2.5.2
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-ldap3/versions

--- pkgs/development/python-modules/ldap3/default.nix
+++ pkgs/development/python-modules/ldap3/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchPypi, buildPythonPackage, gssapi, pyasn1 }:
 
 buildPythonPackage rec {
-  version = "2.5.1";
+  version = "2.5.2";
   pname = "ldap3";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "cc09951809678cfb693a13a6011dd2d48ada60a52bd80cb4bd7dcc55ee7c02fd";
+    sha256 = "063dacy01mphc3n7z2qc2avykjavqm1gllkbvy7xzw5ihlqwhrrz";
   };
 
   buildInputs = [ gssapi ];
