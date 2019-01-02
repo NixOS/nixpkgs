82fd33a16e43969ad89e5755723651658421b097
R. RyanTM <ryantm-bot@ryantm.com>
2019-01-02 11:36:14 -0800

    python37Packages.faker: 0.9.3 -> 1.0.1
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-faker/versions

--- pkgs/development/python-modules/faker/default.nix
+++ pkgs/development/python-modules/faker/default.nix
@@ -8,11 +8,11 @@ assert pythonOlder "3.3" -> ipaddress != null;
 
 buildPythonPackage rec {
   pname = "Faker";
-  version = "0.9.3";
+  version = "1.0.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "8c6df7903c7b4a51f4ac273bc5fec79a249e3220c47b35d1ac1175b41982d772";
+    sha256 = "067mdy9p1vbkypr3vazmrb0sga6maqbk542hr7hmzcb5lp3dr8sj";
   };
 
   buildInputs = [ pytestrunner ];
