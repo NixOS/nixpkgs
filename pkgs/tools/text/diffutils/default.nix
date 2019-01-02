219ca3c558f294f1e782f5aa866c3d5ff06cb488
Will Dietz <w@wdtz.org>
2019-01-01 21:47:10 -0600

    diffutils: 3.6 -> 3.7
    https://savannah.gnu.org/forum/forum.php?forum_id=9341

--- pkgs/tools/text/diffutils/default.nix
+++ pkgs/tools/text/diffutils/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, xz, coreutils ? null }:
 
 stdenv.mkDerivation rec {
-  name = "diffutils-3.6";
+  name = "diffutils-3.7";
 
   src = fetchurl {
     url = "mirror://gnu/diffutils/${name}.tar.xz";
-    sha256 = "1mivg0fy3a6fcn535ln8nkgfj6vxh5hsxxs5h6692wxmsjyyh8fn";
+    sha256 = "09isrg0isjinv8c535nxsi1s86wfdfzml80dbw41dj9x3hiad9xk";
   };
 
   outputs = [ "out" "info" ];
