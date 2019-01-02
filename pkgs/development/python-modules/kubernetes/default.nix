a7c787f60d93b70f5f2bb181b944ef6ad98d49e7
R. RyanTM <ryantm-bot@ryantm.com>
2019-01-02 13:27:37 -0800

    python37Packages.kubernetes: 8.0.0 -> 8.0.1
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-kubernetes/versions

--- pkgs/development/python-modules/kubernetes/default.nix
+++ pkgs/development/python-modules/kubernetes/default.nix
@@ -4,7 +4,7 @@
 
 buildPythonPackage rec {
   pname = "kubernetes";
-  version = "8.0.0";
+  version = "8.0.1";
 
   prePatch = ''
     sed -e 's/sphinx>=1.2.1,!=1.3b1,<1.4 # BSD/sphinx/' -i test-requirements.txt
@@ -23,7 +23,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "54f8e7bb1dd9a55cf416dff76a63c4ae441764280942d9913f2243676f29d02c";
+    sha256 = "0y0aygnd7kpflwdm3zxrmsgws0frk4qwq3lnq92zsiyxcxh8r4i5";
   };
 
   checkInputs = [ isort coverage pytest mock sphinx autopep8 pep8 codecov recommonmark nose ];
