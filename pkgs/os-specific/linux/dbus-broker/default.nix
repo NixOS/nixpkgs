bb9d8c70186ccc2974a6a9e5042e8d75e8a05774
Dominik Xaver Hörl <hoe.dom@gmx.de>
2019-01-03 11:10:42 +0100

    dbus-broker: 13 -> 17

--- pkgs/os-specific/linux/dbus-broker/default.nix
+++ pkgs/os-specific/linux/dbus-broker/default.nix
@@ -1,27 +1,27 @@
 { stdenv, fetchFromGitHub, docutils, meson, ninja, pkgconfig
-, dbus, glib, linuxHeaders, systemd }:
+, dbus, linuxHeaders, systemd }:
 
 stdenv.mkDerivation rec {
   name = "dbus-broker-${version}";
-  version = "13";
+  version = "17";
 
   src = fetchFromGitHub {
     owner  = "bus1";
     repo   = "dbus-broker";
     rev    = "v${version}";
-    sha256 = "1yjkxpnl54pky6ha3y8dsds57lnk10lmriyzpzy0ha2npng2614x";
+    sha256 = "19q5f1c658jdxvr2k2rb2xsnspybqjii4rifnlkzi1qzh0r152md";
     fetchSubmodules = true;
   };
 
   nativeBuildInputs = [ docutils meson ninja pkgconfig ];
 
-  buildInputs = [ dbus glib linuxHeaders systemd ];
+  buildInputs = [ dbus linuxHeaders systemd ];
 
   PKG_CONFIG_SYSTEMD_SYSTEMDSYSTEMUNITDIR = "${placeholder "out"}/lib/systemd/system";
   PKG_CONFIG_SYSTEMD_SYSTEMDUSERUNITDIR = "${placeholder "out"}/lib/systemd/user";
 
   postInstall = ''
-    install -Dm644 ../README $out/share/doc/dbus-broker/README
+    install -Dm644 $src/README.md $out/share/doc/dbus-broker/README
 
     sed -i $out/lib/systemd/{system,user}/dbus-broker.service \
       -e 's,^ExecReload.*busctl,ExecReload=${systemd}/bin/busctl,'
