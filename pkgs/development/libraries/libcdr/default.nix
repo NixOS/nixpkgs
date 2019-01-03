2764297cc5a3a8023bce499d7c37a9dcaffa9d6b
worldofpeace <worldofpeace@users.noreply.github.com>
2019-01-03 17:29:36 -0500

    libcdr: disable werror is default
    So this optional configure flag is uneeded.
    
    See: https://github.com/LibreOffice/libcdr/blob/10211e95bb95d05c2261819c88b7f48d9db20b32/NEWS#L5

--- pkgs/development/libraries/libcdr/default.nix
+++ pkgs/development/libraries/libcdr/default.nix
@@ -18,8 +18,6 @@ stdenv.mkDerivation rec {
     sed -i 's,^CPPFLAGS.*,\0 -DBOOST_ERROR_CODE_HEADER_ONLY -DBOOST_SYSTEM_NO_DEPRECATED,' src/lib/Makefile.in
   '';
 
-  configureFlags = stdenv.lib.optional stdenv.cc.isClang "--disable-werror";
-
   CXXFLAGS="--std=gnu++0x"; # For c++11 constants in lcms2.h
 
   meta = {
