438b6df1b121f3472ab8042944d4d007d43e97c0
Rob Vermaas <rob.vermaas@gmail.com>
2019-01-03 09:50:01 +0100

    julia: update to 1.0.3

--- pkgs/development/compilers/julia/1.0.nix
+++ pkgs/development/compilers/julia/1.0.nix
@@ -1,6 +1,6 @@
 import ./shared.nix {
   majorVersion = "1";
   minorVersion = "0";
-  maintenanceVersion = "1";
-  src_sha256 = "0bqb5c63c7jnb753nplqj5v4k9pvh792k8y4b1n5pq8jiibr86i0";
+  maintenanceVersion = "3";
+  src_sha256 = "0sjyvzdiizibljq0b3b5681mmyyzd5zicvygaavr0k0na9ndzsdz";
 }
