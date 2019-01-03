995c78064ade5153d80169d20903fa7a70714bd2
Rob Vermaas <rob.vermaas@gmail.com>
2019-01-03 19:55:07 +0100

    julia: fix hash for 1.0.3

--- pkgs/development/compilers/julia/1.0.nix
+++ pkgs/development/compilers/julia/1.0.nix
@@ -2,5 +2,5 @@ import ./shared.nix {
   majorVersion = "1";
   minorVersion = "0";
   maintenanceVersion = "3";
-  src_sha256 = "0sjyvzdiizibljq0b3b5681mmyyzd5zicvygaavr0k0na9ndzsdz";
+  src_sha256 = "0666chsc19wx02k5m1yilf6wbc9bw27ay8p1d00jkh8m0jkrpf7l";
 }
