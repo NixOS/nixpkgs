1d41509599707376cf9462ca9a3602012e0337e3
Dmitry Bogatov <KAction@debian.org>
2019-01-03 22:35:21 +0000

    vimPlugins.ansible-vim: init at 2.0

--- pkgs/misc/vim-plugins/generated.nix
+++ pkgs/misc/vim-plugins/generated.nix
@@ -81,6 +81,17 @@ let
     };
   };
 
+  ansible-vim = buildVimPluginFrom2Nix {
+    pname = "ansible-vim";
+    version = "2.0";
+    src = fetchFromGitHub {
+      owner = "pearofducks";
+      repo = "ansible-vim";
+      rev = "01d0e7d4b87936d9cfe5a6038a6c9cec409a3c15";
+      sha256 = "14x7lxwvfrvi27qmffh66fpc3fk21xkzfzps0vx6n3n7h56a7r2g";
+    };
+  };
+
   argtextobj-vim = buildVimPluginFrom2Nix {
     pname = "argtextobj-vim";
     version = "2010-10-18";
