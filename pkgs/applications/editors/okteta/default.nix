181623548ce7d18d73e6d715ec6fcda2367c7b88
R. RyanTM <ryantm-bot@ryantm.com>
2019-01-02 18:33:10 -0800

    kdeApplications.okteta: 0.25.4 -> 0.25.5
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/okteta/versions

--- pkgs/applications/editors/okteta/default.nix
+++ pkgs/applications/editors/okteta/default.nix
@@ -4,11 +4,11 @@
 
 stdenv.mkDerivation rec {
   name = "okteta-${version}";
-  version = "0.25.4";
+  version = "0.25.5";
 
   src = fetchurl {
     url = "mirror://kde/stable/okteta/${version}/src/${name}.tar.xz";
-    sha256 = "0liar1xbns6mr6j320nyxqfii82i4ysp62hf3j6jg1112v874amf";
+    sha256 = "1680hx4n36msz86gyjsdr5v7nf8rpybvzrvfw8y98l95hfq3l6g9";
   };
 
   nativeBuildInputs = [ qtscript extra-cmake-modules kdoctools ];
