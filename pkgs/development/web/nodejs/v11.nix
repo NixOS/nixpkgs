68d8c2e26bc50e2630068f5540c508bb79cc7fd6
Mario Rodas <marsam@users.noreply.github.com>
2019-01-03 03:04:00 -0500

    nodejs-11_x: 11.5.0 -> 11.6.0

--- pkgs/development/web/nodejs/v11.nix
+++ pkgs/development/web/nodejs/v11.nix
@@ -5,6 +5,6 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "11.5.0";
-    sha256 = "07fdpl8wzkcdd8iyaiwf2ah1rgishk2hrl0g73i8aggwplrl69fx";
+    version = "11.6.0";
+    sha256 = "1czrpxmk6calqn0p92rm0bv2vlgbnx6q4z7n2j8r7aw0khwbxwll";
   }
