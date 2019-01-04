eb84501ef97d7ca1ff9ee88bdf3b8529fefbd237
Maximilian Bosch <maximilian@mbosch.me>
2019-01-04 01:15:56 +0100

    python{2,3}Packages.Pweave: fix build
    Version 0.30 was missing the Python dependencies `nbconvert` and `markdown`.
    
    Also Python 2 isn't supported any more since 0.30
    (https://github.com/mpastell/Pweave/blob/master/CHANGELOG.txt) and can
    therefore be dropped.

--- pkgs/development/python-modules/pweave/default.nix
+++ pkgs/development/python-modules/pweave/default.nix
@@ -4,6 +4,9 @@
 , mock
 , matplotlib
 , pkgs
+, nbconvert
+, markdown
+, isPy3k
 }:
 
 buildPythonPackage rec {
@@ -15,8 +18,10 @@ buildPythonPackage rec {
     sha256 = "5e5298d90e06414a01f48e0d6aa4c36a70c5f223d929f2a9c7e2d388451c7357";
   };
 
+  disabled = !isPy3k;
+
   buildInputs = [ mock pkgs.glibcLocales ];
-  propagatedBuildInputs = [ matplotlib ];
+  propagatedBuildInputs = [ matplotlib nbconvert markdown ];
 
   # fails due to trying to run CSS as test
   doCheck = false;
