da00ec4b45f5cfc5b47825818b57db1d1950757d
(cdep)illabout <cdep.illabout@gmail.com>
2019-01-04 16:34:59 +0900

    Add a failing test for mkAliasOptionModule.

--- lib/tests/modules.sh
+++ lib/tests/modules.sh
@@ -149,6 +149,9 @@ checkConfigOutput "1 2 3 4 5 6 7 8 9 10" config.result ./loaOf-with-long-list.ni
 # Check loaOf with many merges of lists.
 checkConfigOutput "1 2 3 4 5 6 7 8 9 10" config.result ./loaOf-with-many-list-merges.nix
 
+# Check mkAliasOptionModuleWithPriority.
+checkConfigOutput "true" config.enable ./alias-with-priority.nix
+
 cat <<EOF
 ====== module tests ======
 $pass Pass
