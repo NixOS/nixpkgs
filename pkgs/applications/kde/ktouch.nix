1d26046b3f1aee8cc564d234c1ab9cc0dae92471
Trolli Schmittlauch <t.schmittlauch+nixos@orlives.de>
2019-01-05 20:22:48 +0100

    ktouch: refining optional dependencies
    It turned out that some dependencies described as optional caused cmake
    to fail if not present. As long as it's not clear whether those are
    needed [1], they are always included into the build.
    
    [1] https://bugs.kde.org/show_bug.cgi?id=402902

--- pkgs/applications/kde/ktouch.nix
+++ pkgs/applications/kde/ktouch.nix
@@ -3,8 +3,8 @@
   extra-cmake-modules, kdoctools,
   kconfig, kconfigwidgets, kcoreaddons, kdeclarative, ki18n,
   kitemviews, kcmutils, kio, knewstuff, ktexteditor, kwidgetsaddons,
-  kwindowsystem, kxmlgui, qtscript, qtdeclarative, kqtquickcharts,
-  withX11 ? true, xorg, qtx11extras}:
+  kwindowsystem, kxmlgui, qtscript, qtdeclarative, kqtquickcharts, qtx11extras,
+  withX11 ? true, xorg }:
 
 
   mkDerivation {
@@ -18,9 +18,10 @@
     buildInputs = [
       kconfig kconfigwidgets kcoreaddons kdeclarative ki18n
       kitemviews kcmutils kio knewstuff ktexteditor kwidgetsaddons
-      kwindowsystem kxmlgui qtscript qtdeclarative kqtquickcharts
+      kwindowsystem kxmlgui qtscript qtdeclarative kqtquickcharts qtx11extras 
+      xorg.libxkbfile
     ]
-    ++ lib.optionals withX11 (with xorg; [ libxcb libxkbfile qtx11extras ]);
-  #propagatedUserEnvPkgs = [ kipi-plugins libkipi (lib.getBin kinit) ];
-  enableParallelBuilding = true;
+    # According to Readme.md, libxkbfile is supposed to be optional as well.
+    # But as CMake currently fails without that lib, we still always include it.
+    ++ lib.optionals withX11 (with xorg; [ libxcb ]);
 }
