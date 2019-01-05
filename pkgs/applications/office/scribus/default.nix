ee4f73c472ae2b40845bf44a2f45ea2a44997d29
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2019-01-05 18:16:06 +0100

    scribus: Add an icon

--- pkgs/applications/office/scribus/default.nix
+++ pkgs/applications/office/scribus/default.nix
@@ -1,8 +1,9 @@
 { stdenv, fetchurl, pkgconfig, freetype, lcms, libtiff, libxml2
 , libart_lgpl, qt4, python2, cups, fontconfig, libjpeg
-, zlib, libpng, xorg, cairo, podofo, aspell, boost, cmake }:
+, zlib, libpng, xorg, cairo, podofo, aspell, boost, cmake, imagemagick }:
 
 let
+  icon = ./icon.svg;
   pythonEnv = python2.withPackages(ps: [ps.tkinter]);
 in stdenv.mkDerivation rec {
   name = "scribus-1.4.7";
@@ -21,7 +22,15 @@ in stdenv.mkDerivation rec {
       boost # for internal 2geom library
       libXaw libXext libX11 libXtst libXi libXinerama
       libpthreadstubs libXau libXdmcp
+      imagemagick # To build the icon
     ];
+
+  postInstall = ''
+    for i in 16 24 48 64 96 128 256 512; do
+      mkdir -p $out/share/icons/hicolor/''${i}x''${i}/apps
+      convert -background none -resize ''${i}x''${i} ${icon} $out/share/icons/hicolor/''${i}x''${i}/apps/scribus.png
+    done
+  '';
 
   meta = {
     maintainers = [ stdenv.lib.maintainers.marcweber ];
