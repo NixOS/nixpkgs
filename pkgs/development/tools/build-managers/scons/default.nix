d376ffe767be6b4d89339687b6c0f856c4b79c19
Michael Weiss <dev.primeos@gmail.com>
2019-01-06 20:11:35 +0100

    scons: Add version 3.0.2
    "SCons release 3.0.2 now available from the download page at
    SourceForge. This release should be used instead of 3.0.1. This release
    fixes several issues. This release supports Python versions 2.7.* as
    well as 3.5+." [0]
    
    Details can be found in the changelog [1].
    
    I'll update the default after running additional tests.
    
    [0]: https://scons.org/scons-302-is-available.html
    [1]: https://raw.githubusercontent.com/SConsProject/scons/rel_3.0.2/src/CHANGES.txt

--- pkgs/development/tools/build-managers/scons/default.nix
+++ pkgs/development/tools/build-managers/scons/default.nix
@@ -15,4 +15,8 @@ in {
     version = "3.0.1";
     sha256 = "0wzid419mlwqw9llrg8gsx4nkzhqy16m4m40r0xnh6cwscw5wir4";
   };
+  scons_3_0_2 = mkScons {
+    version = "3.0.2";
+    sha256 = "00fyvb2rrixj9h6f2sqr6z8q677anc61qcn9ydxgm99f9j7wzbyh";
+  };
 }
