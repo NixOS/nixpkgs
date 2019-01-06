a684ae6e53d3dd7e946c917acbea771a49778b29
Michael Weiss <dev.primeos@gmail.com>
2019-01-06 20:19:54 +0100

    scons: Remove version 3.0.0

--- pkgs/development/tools/build-managers/scons/default.nix
+++ pkgs/development/tools/build-managers/scons/default.nix
@@ -7,10 +7,6 @@ in {
     version = "2.5.1";
     sha256 = "1wji1z9jdkhnmm99apx6fhld9cs52rr56aigniyrcsmlwy52298b";
   };
-  scons_3_0_0 = mkScons {
-    version = "3.0.0";
-    sha256 = "05jjykllk4icnq6gfrkgkbc4ggxm7983q6r33mrhpilqbd02ylqg";
-  };
   scons_3_0_1 = mkScons {
     version = "3.0.1";
     sha256 = "0wzid419mlwqw9llrg8gsx4nkzhqy16m4m40r0xnh6cwscw5wir4";
