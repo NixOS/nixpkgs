2118e66fc310297e9a8ebe0cfb25fe7583206477
Jorge Acereda <jacereda@gmail.com>
2019-01-06 22:56:50 +0100

    futhark: fix darwin build

--- pkgs/development/haskell-modules/configuration-nix.nix
+++ pkgs/development/haskell-modules/configuration-nix.nix
@@ -532,7 +532,7 @@ self: super: builtins.intersectAttrs super {
   # The test-suite requires a running PostgreSQL server.
   Frames-beam = dontCheck super.Frames-beam;
 
-  futhark = with pkgs;
+  futhark = if pkgs.stdenv.isDarwin then super.futhark else with pkgs;
     let path = stdenv.lib.makeBinPath [ gcc ];
     in overrideCabal (addBuildTool super.futhark makeWrapper) (_drv: {
       postInstall = ''
