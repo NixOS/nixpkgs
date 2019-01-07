a4f51746f826349c631910b1a67d7556a9a0bf30
Joachim Fasting <joachifm@fastmail.fm>
2019-01-07 19:16:54 +0100

    Revert "openafs: fix build against linux_hardened"
    This reverts commit 4bb0a719cf8f6b89ec89cd902a511a7e043d9a0d.
    
    Can be reveted once randstruct actually works

--- pkgs/servers/openafs/1.8/module.nix
+++ pkgs/servers/openafs/1.8/module.nix
@@ -18,9 +18,6 @@ in stdenv.mkDerivation rec {
 
   hardeningDisable = [ "pic" ];
 
-  # The RANDSTRUCT gcc plugin rewrites structs using designated initializers
-  NIX_CFLAGS_COMPILE = [ "-Wno-error=designated-init" ];
-
   configureFlags = [
     "--with-linux-kernel-build=${kernelBuildDir}"
     "--sysconfdir=/etc"
