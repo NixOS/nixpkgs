63bedd5bceebcec9aad6bd0e4d1b3046df364bb3
R. RyanTM <ryantm-bot@ryantm.com>
2019-01-07 01:14:18 -0800

    python37Packages.pylast: 2.4.0 -> 3.0.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-pylast/versions

--- pkgs/development/python-modules/pylast/default.nix
+++ pkgs/development/python-modules/pylast/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "pylast";
-  version = "2.4.0";
+  version = "3.0.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "8e883f13b70c3879fc821bbee1accf27ea4e68898f4462cbbe358f615adcbbfb";
+    sha256 = "0mmwx4l2kby4zvf3qvx8gmi6rnlhri70ipz9mvg8pw8z0591q194";
   };
 
   propagatedBuildInputs = [ certifi six ];
