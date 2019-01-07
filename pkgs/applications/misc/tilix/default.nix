a099d3891c992d41224a83a7c96fc2854302aaae
worldofpeace <worldofpeace@users.noreply.github.com>
2019-01-06 20:41:21 -0500

    tilix: 1.8.5 -> 1.8.9
    Also build with meson

--- pkgs/applications/misc/tilix/default.nix
+++ pkgs/applications/misc/tilix/default.nix
@@ -1,33 +1,33 @@
-{ stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, dmd, gnome3, dbus
+{ stdenv, fetchFromGitHub, meson, ninja, pkgconfig, dmd, gnome3, dbus
 , gsettings-desktop-schemas, desktop-file-utils, gettext, gtkd, libsecret
-, perlPackages, wrapGAppsHook, xdg_utils }:
+, appstream-glib, libunwind, perlPackages, wrapGAppsHook, xdg_utils }:
 
 stdenv.mkDerivation rec {
   name = "tilix-${version}";
-  version = "1.8.5";
+  version = "1.8.9";
 
   src = fetchFromGitHub {
     owner = "gnunn1";
     repo = "tilix";
     rev = "${version}";
-    sha256 = "1ixhkssz0xn3x75n2iw6gd3hka6bgmgwfgbvblbjhhx8gcpbw3s7";
+    sha256 = "1l1ib3g01mxiywbwjxc2522qgjy3ymjzy8bxl42k0hprpp95rw9d";
   };
 
   nativeBuildInputs = [
-    autoreconfHook dmd desktop-file-utils perlPackages.Po4a pkgconfig xdg_utils
-    wrapGAppsHook
+    meson ninja dmd desktop-file-utils perlPackages.Po4a pkgconfig xdg_utils
+    appstream-glib wrapGAppsHook
   ];
-  buildInputs = [ gnome3.dconf gettext gsettings-desktop-schemas gtkd dbus libsecret ];
+  buildInputs = [ gnome3.dconf gettext gsettings-desktop-schemas gtkd dbus libsecret libunwind ];
 
-  preBuild = ''
-    makeFlagsArray=(
-      DCFLAGS='-O -inline -release -version=StdLoggerDisableTrace'
-    )
+  # Don't use dub to fetch and build dependencies
+  patches = [ ./no-dub.patch ];
+
+  postPatch = ''
+    chmod +x meson_post_install.py
+    patchShebangs meson_post_install.py
   '';
 
   postInstall = with gnome3; ''
-    ${glib.dev}/bin/glib-compile-schemas $out/share/glib-2.0/schemas
-
     wrapProgram $out/bin/tilix \
       --prefix LD_LIBRARY_PATH ":" "${libsecret}/lib"
   '';
@@ -40,7 +40,7 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with stdenv.lib; {
-    description = "Tiling terminal emulator following the Gnome Human Interface Guidelines.";
+    description = "Tiling terminal emulator following the Gnome Human Interface Guidelines";
     homepage = https://gnunn1.github.io/tilix-web;
     license = licenses.mpl20;
     maintainers = with maintainers; [ midchildan ];
