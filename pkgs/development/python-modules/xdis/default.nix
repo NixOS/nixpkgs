0ca68bddcfce1029b6cde12e6237f610829773a4
R. RyanTM <ryantm-bot@ryantm.com>
2019-01-06 17:51:04 -0800

    python37Packages.xdis: 3.8.8 -> 3.8.9
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-xdis/versions

--- pkgs/development/python-modules/xdis/default.nix
+++ pkgs/development/python-modules/xdis/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "xdis";
-  version = "3.8.8";
+  version = "3.8.9";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "4d212df8a85ab55a35f6ad71b2c29818d903c3e6a95e31eb26d5f3fc66a4e015";
+    sha256 = "1q2dg3hnsmmpjcc7lzjf5nd041mpbwa2bq3dgr4p6wv65vncny9v";
   };
 
   checkInputs = [ pytest ];
