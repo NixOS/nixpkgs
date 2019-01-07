38d9d76d61c55219dbc3f31fb15afa43fe98ebbf
Michael Weiss <dev.primeos@gmail.com>
2019-01-07 15:18:56 +0100

    gringo: Switch to the default SCons version
    Version 3.0.2 works fine and 2.5.1 is therefore no longer required.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1482,7 +1482,7 @@ in
     pythonPackages = python3Packages;
   };
 
-  gringo = callPackage ../tools/misc/gringo { scons = scons_2_5_1; };
+  gringo = callPackage ../tools/misc/gringo { };
 
   grobi = callPackage ../tools/X11/grobi { };
 
