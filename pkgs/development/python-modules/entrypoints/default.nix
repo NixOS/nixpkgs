2cb56757f0667581b11afea8bd12cc046d569551
Timo Kaufmann <timokau@zoho.com>
2019-01-07 20:09:20 +0100

    python.pkgs.entrypoints: 0.2.3 -> 0.3
    The upstream issue requiring one test to be disabled was fixed:
    https://github.com/takluyver/entrypoints/issues/23

--- pkgs/development/python-modules/entrypoints/default.nix
+++ pkgs/development/python-modules/entrypoints/default.nix
@@ -4,31 +4,23 @@
 , configparser
 , pytest
 , isPy3k
-, isPy27
 }:
 
 buildPythonPackage rec {
   pname = "entrypoints";
-  version = "0.2.3";
+  version = "0.3";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "d2d587dde06f99545fb13a383d2cd336a8ff1f359c5839ce3a64c917d10c029f";
+    sha256 = "0lc4si3xb7hza424414rdqdc3vng3kcrph8jbvjqb32spqddf3f7";
   };
 
   checkInputs = [ pytest ];
 
   propagatedBuildInputs = lib.optional (!isPy3k) configparser;
 
-  checkPhase = let
-    # On python2 with pytest 3.9.2 (not with pytest 3.7.4) the test_bad
-    # test fails. It tests that a warning (exectly one) is thrown on a "bad"
-    # path. The pytest upgrade added some warning, resulting in two warnings
-    # being thrown.
-    # upstream: https://github.com/takluyver/entrypoints/issues/23
-    pyTestArgs = if isPy27 then "-k 'not test_bad'" else "";
-  in ''
-    py.test ${pyTestArgs} tests
+  checkPhase = ''
+    py.test tests
   '';
 
   meta = {
