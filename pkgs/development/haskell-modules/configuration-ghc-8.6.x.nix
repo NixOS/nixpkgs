b10f43c1f12ade8ef92c808e596fb7b6ef7dcb2e
Jack Kelly <jack.kelly@data61.csiro.au>
2019-01-08 16:30:17 +1100

    hpc-coveralls: jailbreak for GHC 8.6

--- pkgs/development/haskell-modules/configuration-ghc-8.6.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.6.x.nix
@@ -48,6 +48,7 @@ self: super: {
   cereal = dontCheck super.cereal;
   data-clist = doJailbreak super.data-clist;  # won't cope with QuickCheck 2.12.x
   Diff = dontCheck super.Diff;
+  hpc-coveralls = doJailbreak super.hpc-coveralls; # https://github.com/guillaume-nargeot/hpc-coveralls/issues/82
   http-api-data = doJailbreak super.http-api-data;
   persistent-sqlite = dontCheck super.persistent-sqlite;
   psqueues = dontCheck super.psqueues;    # won't cope with QuickCheck 2.12.x
