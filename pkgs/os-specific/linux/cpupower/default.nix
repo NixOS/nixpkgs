f4a765d4b3fb22da060f28216efc1322b3c8db05
Will Dietz <w@wdtz.org>
2019-01-08 12:22:19 -0600

    cpupower: fix w/5.0-rc1 (completion dir), rework a bit

--- pkgs/os-specific/linux/cpupower/default.nix
+++ pkgs/os-specific/linux/cpupower/default.nix
@@ -1,9 +1,8 @@
 { stdenv, buildPackages, kernel, pciutils, gettext }:
 
 stdenv.mkDerivation {
-  name = "cpupower-${kernel.version}";
-
-  src = kernel.src;
+  pname = "cpupower";
+  inherit (kernel) version src;
 
   nativeBuildInputs = [ gettext ];
   buildInputs = [ pciutils ];
@@ -17,16 +16,18 @@ stdenv.mkDerivation {
 
   makeFlags = [ "CROSS=${stdenv.cc.targetPrefix}" ];
 
-  installFlags = [
-    "bindir=$(out)/bin"
-    "sbindir=$(out)/sbin"
-    "mandir=$(out)/share/man"
-    "includedir=$(out)/include"
-    "libdir=$(out)/lib"
-    "localedir=$(out)/share/locale"
-    "docdir=$(out)/share/doc/cpupower"
-    "confdir=$(out)/etc"
-  ];
+  installFlags = stdenv.lib.mapAttrsToList
+    (n: v: "${n}=${placeholder "out"}/${v}") {
+    bindir = "bin";
+    sbindir = "sbin";
+    mandir = "share/man";
+    includedir = "include";
+    libdir = "lib";
+    localedir = "share/locale";
+    docdir = "share/doc/cpupower";
+    confdir = "etc";
+    bash_completion_dir = "share/bash-completion/completions";
+  };
 
   enableParallelBuilding = true;
 
