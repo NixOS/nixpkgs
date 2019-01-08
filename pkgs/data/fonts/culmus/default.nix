aa330e1803fc34569d9f25241ff18a5a8d5b4528
yochai <yochai@users.noreply.github.com>
2019-01-08 11:49:52 +0200

    culmus: add .otf, .pfa and .afm files

--- pkgs/data/fonts/culmus/default.nix
+++ pkgs/data/fonts/culmus/default.nix
@@ -3,17 +3,24 @@
 let
   version = "0.133";
 in fetchzip {
-  name = "culmus-${version}";
+  name = "culmus-${version}-1";
 
   url = "mirror://sourceforge/culmus/culmus/${version}/culmus-${version}.tar.gz";
 
   postFetch = ''
     tar -xzvf $downloadedFile --strip-components=1
-    mkdir -p $out/share/fonts/truetype
+    mkdir -p $out/share/fonts/{truetype,type1}
+    cp -v *.pfa $out/share/fonts/type1/
+    cp -v *.afm $out/share/fonts/type1/
+    cp -v fonts.scale-type1 $out/share/fonts/type1/fonts.scale
     cp -v *.ttf $out/share/fonts/truetype/
+    cp -v *.otf $out/share/fonts/truetype/
+    cp -v fonts.scale-ttf $out/share/fonts/truetype/fonts.scale
+    mkdir -p $out/etc/fonts/conf.d
+    cp -v culmus.conf $out/etc/fonts/conf.d/39-culmus.conf
   '';
 
-  sha256 = "1jxg2wf4kwasp5cia00nki2lrcdnhsyh4yy7d05l0a9bim5hq2lr";
+  sha256 = "1lvwv15lpk4cqarh2ncl83c43fmqxnzqaqzfm251zkx6svi1l0is";
 
   meta = {
     description = "Culmus Hebrew fonts";
