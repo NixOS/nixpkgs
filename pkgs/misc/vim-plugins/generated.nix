f7113786af544323937c7d7dfe91e0acfa92f410
Timo Kaufmann <timokau@zoho.com>
2019-01-08 17:57:29 +0100

    vimPlugins.neoterm: init at 2018-12-21

--- pkgs/misc/vim-plugins/generated.nix
+++ pkgs/misc/vim-plugins/generated.nix
@@ -1156,6 +1156,17 @@ let
     };
   };
 
+  neoterm = buildVimPluginFrom2Nix {
+    pname = "neoterm";
+    version = "2018-12-21";
+    src = fetchFromGitHub {
+      owner = "kassio";
+      repo = "neoterm";
+      rev = "d74b342a9bc84e4ca3f7e85cd773a08d0aa26fe9";
+      sha256 = "09ywfnm5imx98qx57vg7ih37jixzh2sm6769gvrjz7m6d8d016mn";
+    };
+  };
+
   neovim-sensible = buildVimPluginFrom2Nix {
     pname = "neovim-sensible";
     version = "2017-09-20";
