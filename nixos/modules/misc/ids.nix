9260623a186cd00db2e46962dbb44a956c620709
Peter Hoeg <peter@hoeg.com>
2019-01-09 10:58:32 +0800

    zoneminder: add user for NixOS

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -338,6 +338,7 @@
       minetest = 311;
       rss2email = 312;
       cockroachdb = 313;
+      zoneminder = 314;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -636,6 +637,7 @@
       minetest = 311;
       rss2email = 312;
       cockroachdb = 313;
+      zoneminder = 314;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
