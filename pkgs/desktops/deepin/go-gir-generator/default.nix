d23822de1a295b040b6743150685586566f4cece
José Romildo Malaquias <malaquias@gmail.com>
2019-01-09 00:03:13 -0200

    deepin.go-gir-generator: 1.1.0 -> 1.2.0

--- pkgs/desktops/deepin/go-gir-generator/default.nix
+++ pkgs/desktops/deepin/go-gir-generator/default.nix
@@ -4,22 +4,14 @@
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
   pname = "go-gir-generator";
-  version = "1.1.0";
+  version = "1.2.0";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "0grp4ffy3vmlknzmymnxq1spwshff2ylqsw82pj4y2v2fcvnqfvb";
+    sha256 = "0m6g1raa2r8in4cmnw2psm0yfhnzznlwll824w0pdywx7zlwlll7";
   };
-
-  patches = [
-    # fix: dde-api build error with gobject-introspection 1.58+
-    (fetchurl {
-      url = https://github.com/linuxdeepin/go-gir-generator/commit/a7ab229201e28d1be727f5021b3588fa4a1acf5f.patch;
-      sha256 = "13ywalwkjg8wwvd0pvmc2rv1h38airyvimdn9jfb5wis9xm48401";
-    })
-  ];
 
   nativeBuildInputs = [
     pkgconfig
