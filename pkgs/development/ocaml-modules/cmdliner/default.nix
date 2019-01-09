522148ba4eb3767f1d1beb48a4aba7b6527a147b
Vincent Laporte <Vincent.Laporte@gmail.com>
2019-01-09 15:04:18 +0000

    ocamlPackages.cmdliner: restore version 1.0.2 for OCaml ≤ 4.02

--- pkgs/development/ocaml-modules/cmdliner/default.nix
+++ pkgs/development/ocaml-modules/cmdliner/default.nix
@@ -6,13 +6,23 @@ in
 
 assert stdenv.lib.versionAtLeast ocaml.version "4.01.0";
 
+let param =
+  if stdenv.lib.versionAtLeast ocaml.version "4.03" then {
+    version = "1.0.3";
+    sha256 = "0g3w4hvc1cx9x2yp5aqn6m2rl8lf9x1dn754hfq8m1sc1102lxna";
+  } else {
+    version = "1.0.2";
+    sha256 = "18jqphjiifljlh9jg8zpl6310p3iwyaqphdkmf89acyaix0s4kj1";
+  }
+; in
+
 stdenv.mkDerivation rec {
-  name = "ocaml-${pname}-${version}";
-  version = "1.0.3";
+  name = "ocaml${ocaml.version}-${pname}-${version}";
+  inherit (param) version;
 
   src = fetchurl {
     url = "http://erratique.ch/software/${pname}/releases/${pname}-${version}.tbz";
-    sha256 = "0g3w4hvc1cx9x2yp5aqn6m2rl8lf9x1dn754hfq8m1sc1102lxna";
+    inherit (param) sha256;
   };
 
   nativeBuildInputs = [ ocamlbuild topkg ];
