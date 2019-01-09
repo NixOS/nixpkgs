18f1a270b18299ca4066106ee2bf02167b2f6534
8084 <klntsky@gmail.com>
2019-01-09 23:33:35 +0300

    added: GeoIP

--- pkgs/applications/networking/soulseek/nicotine-plus/default.nix
+++ pkgs/applications/networking/soulseek/nicotine-plus/default.nix
@@ -1,4 +1,14 @@
-{ stdenv, fetchFromGitHub, python27Packages }:
+{ stdenv, fetchFromGitHub, python27Packages, lib
+, geoip ? null
+, geolite-legacy ? null
+
+# Nicotine+ supports a country code blocker. This requires a (GPL'ed) library called GeoIP.
+, enableGeoIP ? true
+}:
+
+assert enableGeoIP -> python27Packages.GeoIP != null;
+assert enableGeoIP -> geoip                  != null;
+assert enableGeoIP -> geolite-legacy         != null;
 
 with stdenv.lib;
 
@@ -12,16 +22,23 @@ python27Packages.buildPythonApplication rec {
     sha256 = "11j2qm67sszfqq730czsr2zmpgkghsb50556ax1vlpm7rw3gm33c";
   };
 
-  # TODO: include GeoIP as an optional dependency
   propagatedBuildInputs = with python27Packages; [
     pygtk
     miniupnpc
     mutagen
     notify
-  ];
+  ] ++ lib.lists.optional enableGeoIP
+     (GeoIP.overrideAttrs (_: {
+       propagatedBuildInputs = lib.lists.singleton
+         (geoip.override  (_: {
+           geoipDatabase = geolite-legacy;
+         }));
+     }));
+
+  postFixup = "mv $out/bin/nicotine $out/bin/nicotine-plus";
 
   meta = {
-    description = "A graphical client for the SoulSeek peer-to-peer system ";
+    description = "A graphical client for the SoulSeek peer-to-peer system";
     homepage = https://www.nicotine-plus.org;
     license = licenses.gpl3;
     maintainers = with maintainers; [ klntsky ];
