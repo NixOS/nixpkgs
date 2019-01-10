08f912e7768e87fbe57328259363f218ff607087
leo60228 <iakornfeld@gmail.com>
2019-01-10 14:07:07 -0500

    funcsigs: fix tests on pypy3

--- pkgs/development/python-modules/funcsigs/default.nix
+++ pkgs/development/python-modules/funcsigs/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildPythonPackage, fetchPypi
+{ stdenv, buildPythonPackage, fetchPypi, fetchpatch
 , unittest2 }:
 
 buildPythonPackage rec {
@@ -9,6 +9,13 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "0l4g5818ffyfmfs1a924811azhjj8ax9xd1cffr1mzd3ycn0zfx7";
   };
+  
+  patches = [
+    (fetchpatch {
+      url = "https://raw.githubusercontent.com/gentoo/gentoo/fd9a5a0a3c15d7400b936ce0f0814fd4078e900c/dev-python/funcsigs/files/funcsigs-1.0.2-fix-pypy3-tests.patch";
+      sha256 = "04pqkgglqxdkvq07mbr21zlijwiw8jarrb10r4d4yxr3swn3zsl6";
+    })
+  ];
 
   buildInputs = [ unittest2 ];
 
