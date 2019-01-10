79229ca8633b58d72e864dd0d0a13937e3bb5a87
Eelco Dolstra <edolstra@gmail.com>
2019-01-10 16:43:43 +0100

    rustfmt: 0.99.5 -> 1.0.0
    Note: there is a 1.0.1, but it doesn't compile with our stable Rust.

--- pkgs/development/tools/rust/rustfmt/default.nix
+++ pkgs/development/tools/rust/rustfmt/default.nix
@@ -2,19 +2,18 @@
 
 rustPlatform.buildRustPackage rec {
   name = "rustfmt-${version}";
-  version = "0.99.5";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "rust-lang";
     repo = "rustfmt";
     rev = "${version}";
-    sha256 = "1gx1bsyb0f94r3f88f1j3b4rcm2x6zppcfab1c5vgpsr2dr6ch28";
+    sha256 = "17ady2zq4jcbgawgpfszrkp6kxabb2f261g82y2az7nfax1h1pwy";
   };
 
-  cargoSha256 = "1rs6jjm75ixxhrf8b3zn991xa5kfayxlf0b70zdx6wd4r6by7w2y";
+  cargoSha256 = "0v8iq50h9368kai3m710br5cxc3p6mpbwz1v6gaf5802n48liqs8";
 
   buildInputs = stdenv.lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Security;
-
 
   # As of 1.0.0 and rustc 1.30 rustfmt requires a nightly compiler
   RUSTC_BOOTSTRAP = 1;
