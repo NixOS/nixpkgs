1fce5f196c89c7510aebad38a3c163d1574baa22
Franz Pletz <fpletz@fnordicwalking.de>
2019-01-11 11:59:17 +0100

    libwhereami: 0.2.0 -> 0.2.2, fix build with gcc8

--- pkgs/development/libraries/libwhereami/default.nix
+++ pkgs/development/libraries/libwhereami/default.nix
@@ -2,17 +2,16 @@
 
 stdenv.mkDerivation rec {
   name = "libwhereami-${version}";
-  version = "0.2.0";
+  version = "0.2.2";
 
   src = fetchFromGitHub {
-    sha256 = "10phq4a11m8ly6b4dc2yg3dnjzg8ad5wnjv0ilvwylnw32800pxr";
+    sha256 = "084n153jaq8fmhjififk0xlx1d1i3lclnw2j3ly8bixvc392vzly";
     rev = version;
     repo = "libwhereami";
     owner = "puppetlabs";
   };
 
-  # post gcc7, upstream bug: https://tickets.puppetlabs.com/browse/FACT-1828
-  NIX_CFLAGS_COMPILE = "-Wno-error=deprecated";
+  NIX_CFLAGS_COMPILE = [ "-Wno-error=catch-value" ];
 
   nativeBuildInputs = [ cmake ];
 
