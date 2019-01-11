15facdae0b14dad89aa1583d217255b684bb9a6a
Franz Pletz <fpletz@fnordicwalking.de>
2019-01-11 11:58:27 +0100

    trinity: 2018-06-08 -> 2018-09-21, fix build with gcc8

--- pkgs/os-specific/linux/trinity/default.nix
+++ pkgs/os-specific/linux/trinity/default.nix
@@ -2,17 +2,17 @@
 
 stdenv.mkDerivation rec {
   name = "trinity-${version}";
-  version = "1.8-git-2018-06-08";
+  version = "1.8-git-2018-09-21";
 
   src = fetchFromGitHub {
     owner = "kernelslacker";
     repo = "trinity";
-    rev = "1b2d43cb383cef86a05acb2df046ce5e9b17a7fe";
-    sha256 = "0dsq10vmd6ii1dnpaqhizk9p8mbd6mwgpmi13b11dxwxpcvbhlar";
+    rev = "9f6f9f916da3b42cef2e7c30101ff4b0397df736";
+    sha256 = "09bjclgr31dhdqhyig84263q9l59x1q3ppa89sxbmg0yn6ixrc9h";
   };
 
   # Fails on 32-bit otherwise
-  NIX_CFLAGS_COMPILE = [
+  NIX_CFLAGS_COMPILE = stdenv.lib.optionals stdenv.targetPlatform.is32bit [
     "-Wno-error=int-to-pointer-cast"
     "-Wno-error=pointer-to-int-cast"
     "-Wno-error=incompatible-pointer-types"
