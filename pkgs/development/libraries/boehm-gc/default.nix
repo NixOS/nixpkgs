NixOS/nixpkgs

commit 587467a18e6ab2730ab49408086e8f4e9c3a334b

github-gentoo-compromized_cannot-download_boehm-gc-7.6.0-sys_select.patch-from-any-mirror - change to gentoo git repository gitweb.gentoo.org

pkgs/development/libraries/boehm-gc/default.nix
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
   '';
 
   patches = [ (fetchpatch {
-    url = "https://raw.githubusercontent.com/gentoo/musl/85b6a600996bdd71162b357e9ba93d8559342432/dev-libs/boehm-gc/files/boehm-gc-7.6.0-sys_select.patch";
+    url = "https://gitweb.gentoo.org/proj/musl.git/plain/dev-libs/boehm-gc/files/boehm-gc-7.6.0-sys_select.patch";
     sha256 = "1gydwlklvci30f5dpp5ccw2p2qpph5y41r55wx9idamjlq66fbb3";
   }) ] ++
     # https://github.com/ivmai/bdwgc/pull/208

commit ac7f4c0478679cc6059bcf7f9bcefe2b5b604064

boehmgc: fix patch url
Fixes: 587467a18e ('github-gentoo-compromized_can...')
Closes: #53809

pkgs/development/libraries/boehm-gc/default.nix
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
   '';
 
   patches = [ (fetchpatch {
-    url = "https://gitweb.gentoo.org/proj/musl.git/plain/dev-libs/boehm-gc/files/boehm-gc-7.6.0-sys_select.patch";
+    url = "https://gitweb.gentoo.org/proj/musl.git/plain/dev-libs/boehm-gc/files/boehm-gc-7.6.0-sys_select.patch?id=85b6a600996bdd71162b357e9ba93d8559342432";
     sha256 = "1gydwlklvci30f5dpp5ccw2p2qpph5y41r55wx9idamjlq66fbb3";
   }) ] ++
     # https://github.com/ivmai/bdwgc/pull/208