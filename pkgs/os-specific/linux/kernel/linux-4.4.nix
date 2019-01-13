08619eabccae83e2075be796b6f58d4ec3327fd7
Tim Steinbach <tim@nequissimus.com>
2019-01-13 15:49:04 -0500

    linux: 4.4.169 -> 4.4.170
    (cherry picked from commit 7f0aba4cbdc441eadaa70b989f14412e4cabd55b)

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.169";
+  version = "4.4.170";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1aah2qmifj15kcck4m6p00zz0d80afs22bg44y3n4l926f0b1w86";
+    sha256 = "04fia71k7hi9kmxmrqsdsi4nl6jw7vn1wkmdyh63hm89yz8dmy64";
   };
 } // (args.argsOverride or {}))
