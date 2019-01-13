ee3949d7207dabcd7ff46725ed8219a7127bbb00
Austin Seipp <aseipp@pobox.com>
2019-01-12 23:46:00 -0600

    nextpnr: use qtbase-5.11 instead of 5.12
    This fixes a bizarre regression in the NextPNR GUI that causes the
    background of the OpenGL floorplanning window to become 'transparent' on
    certain platforms, greatly limiting its utility. However, QT 5.11 seems
    to work just fine here.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7412,7 +7412,11 @@ in
 
   neko = callPackage ../development/compilers/neko { };
 
-  nextpnr = libsForQt5.callPackage ../development/compilers/nextpnr { };
+  nextpnr = libsForQt5.callPackage ../development/compilers/nextpnr {
+    # QT 5.12 has a weird regression involving the floorplanning window having
+    # a 'blank' or 'transparent' background, so fall back to 5.11 for now.
+    qtbase = qt511.qtbase;
+  };
 
   nasm = callPackage ../development/compilers/nasm { };
 
