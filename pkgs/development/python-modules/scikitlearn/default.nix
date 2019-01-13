803c06c012df2db560d700c7b8b011ad793553f0
Averell Dalton <averell+nixpkgs@rxd4.com>
2019-01-13 15:13:10 +0100

    python: scikitlearn: 0.20.0 -> 0.20.2

--- pkgs/development/python-modules/scikitlearn/default.nix
+++ pkgs/development/python-modules/scikitlearn/default.nix
@@ -5,13 +5,13 @@
 
 buildPythonPackage rec {
   pname = "scikit-learn";
-  version = "0.20.0";
+  version = "0.20.2";
   # UnboundLocalError: local variable 'message' referenced before assignment
   disabled = stdenv.isi686;  # https://github.com/scikit-learn/scikit-learn/issues/5534
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "064cbxsis6m7l6pr09ijjwqdv0c0yrfnazabwq8p09gcz1qxklcp";
+    sha256 = "1ri9kx0yrn85h6ivkaja35afbyhimxn8lsairgns2wi5xv3wfnxw";
   };
 
   buildInputs = [ pillow gfortran glibcLocales ];
