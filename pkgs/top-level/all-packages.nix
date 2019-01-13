844331dd18a34f42028fccd78b641dc473ddc713
Andrew Childs <lorne@cons.org.nz>
2019-01-13 21:49:41 +0900

    bitwarden-cli: init at 1.7.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -677,6 +677,8 @@ in
 
   bcachefs-tools = callPackage ../tools/filesystems/bcachefs-tools { };
 
+  bitwarden-cli = callPackage ../tools/security/bitwarden-cli { };
+
   bmap-tools = callPackage ../tools/misc/bmap-tools { };
 
   bonnie = callPackage ../tools/filesystems/bonnie { };
