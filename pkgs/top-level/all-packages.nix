5ed6db229273104fc6114c09ca547a427648897c
Claas Augner <github@caugner.de>
2019-01-14 12:22:42 +0100

    protonvpn-cli: init at 1.1.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18791,6 +18791,8 @@ in
 
   protonmail-bridge = libsForQt5.callPackage ../applications/networking/protonmail-bridge { };
 
+  protonvpn-cli = callPackage ../applications/networking/protonvpn-cli { };
+
   psi = callPackage ../applications/networking/instant-messengers/psi { };
 
   psi-plus = callPackage ../applications/networking/instant-messengers/psi-plus { };
