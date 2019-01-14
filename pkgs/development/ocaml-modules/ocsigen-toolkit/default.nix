5a2f6b08bf79e5e271bb8bd6ac42f940cf0b11f6
Vincent Laporte <Vincent.Laporte@gmail.com>
2019-01-14 22:52:14 +0000

    ocamlPackages.ocsigen-toolkit: 1.1.0 -> 2.0.0

--- pkgs/development/ocaml-modules/ocsigen-toolkit/default.nix
+++ pkgs/development/ocaml-modules/ocsigen-toolkit/default.nix
@@ -1,14 +1,14 @@
-{ stdenv, fetchurl, buildOcaml, ocaml, opaline
+{ stdenv, fetchFromGitHub, ocaml, findlib, opaline
 , calendar, eliom, js_of_ocaml-ppx_deriving_json
 }:
 
-buildOcaml rec
-{
- name = "ocsigen-toolkit";
- version = "1.1.0";
+stdenv.mkDerivation rec {
+ pname = "ocsigen-toolkit";
+ name = "ocaml${ocaml.version}-${pname}-${version}";
+ version = "2.0.0";
 
  propagatedBuildInputs = [ calendar eliom js_of_ocaml-ppx_deriving_json ];
- buildInputs = [ opaline ];
+ buildInputs = [ ocaml findlib opaline ];
 
  installPhase =
   ''
@@ -17,16 +17,21 @@ buildOcaml rec
     opaline -prefix $out
   '';
 
-  src = fetchurl {
-    sha256 = "1i5806gaqqllgsgjz3lf9fwlffqg3vfl49msmhy7xvq2sncbxp8a";
-    url = "https://github.com/ocsigen/${name}/archive/${version}.tar.gz";
+  src = fetchFromGitHub {
+    owner = "ocsigen";
+    repo = pname;
+    rev = version;
+    sha256 = "0gkiqw3xi31l9q9h89fnr5gfmxi9w9lg9rlv16h4ssjgrgq3y5cw";
   };
+
+  createFindlibDestdir = true;
 
   meta = {
     homepage = http://ocsigen.org/ocsigen-toolkit/;
     description = " User interface widgets for Ocsigen applications";
     license = stdenv.lib.licenses.lgpl21;
     maintainers = [ stdenv.lib.maintainers.gal_bolle ];
+    inherit (ocaml.meta) platforms;
   };
 
 
