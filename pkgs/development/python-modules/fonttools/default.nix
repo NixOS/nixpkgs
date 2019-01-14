f2f5cada061e390dfc7decf88a388b816f37b507
Will Dietz <w@wdtz.org>
2019-01-14 15:38:09 -0600

    pythonPackages.fonttools: 3.34.2 -> 3.35.2

--- pkgs/development/python-modules/fonttools/default.nix
+++ pkgs/development/python-modules/fonttools/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "fonttools";
-  version = "3.34.2";
+  version = "3.35.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1ahs82jnc8f7gksh51asg9dcifhslyfdz9dry9sxq424q1p5k9lz";
+    sha256 = "1z7gzkcg6423rdf2z8zb3ryyv00w53ma0894mzzdh2s7vdbc3lyn";
     extension = "zip";
   };
 
