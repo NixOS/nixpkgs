121318a9b47fa6422f0e7e44343a05067c4293c6
Pierre-Etienne Meunier <pierre-etienne.meunier@mu.ie>
2019-01-15 21:36:36 +0000

    toml2nix: init at 0.1.1 (#53883)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5815,6 +5815,8 @@ in
 
   tokei = callPackage ../development/tools/misc/tokei { };
 
+  toml2nix = (callPackage ../tools/toml2nix { }).toml2nix { };
+
   tor = callPackage ../tools/security/tor {
     openssl = openssl_1_1;
     # remove this, when libevent's openssl is upgraded to 1_1_0 or newer.
