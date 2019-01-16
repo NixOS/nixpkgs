90a4773987032a90ae9b7090060ab313eac77b21
Alexander Krupenkin <mail@akru.me>
2019-01-16 12:37:25 +0300

    parity: 2.1.11 -> 2.2.7

--- pkgs/applications/altcoins/parity/default.nix
+++ pkgs/applications/altcoins/parity/default.nix
@@ -1,6 +1,6 @@
 let
-  version     = "2.1.11";
-  sha256      = "0s0vig9pcz9iw774drfanb6hwnx97wm5fgn4hf5pydwb4jws1qrf";
-  cargoSha256 = "1nx6aiq4888d75xfzx9q7ih5jgidjaq1i63bvvgxqyldxq0hjrma";
+  version     = "2.2.7";
+  sha256      = "0bxq4z84vsb8hmbscr41xiw11m9xg6if231v76c2dmkbyqgpqy8p";
+  cargoSha256 = "1izwqg87qxhmmkd49m0k09i7r05sfcb18m5jbpvggjzp57ips09r";
 in
   import ./parity.nix { inherit version sha256 cargoSha256; }
