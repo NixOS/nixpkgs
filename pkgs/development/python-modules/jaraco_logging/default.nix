761042b10eb139e6402a0c4096aff3d946130fba
R. RyanTM <ryantm-bot@ryantm.com>
2019-01-16 14:40:44 -0800

    python37Packages.jaraco_logging: 1.5.2 -> 2.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-jaraco.logging/versions

--- pkgs/development/python-modules/jaraco_logging/default.nix
+++ pkgs/development/python-modules/jaraco_logging/default.nix
@@ -3,10 +3,10 @@
 
 buildPythonPackage rec {
   pname = "jaraco.logging";
-  version = "1.5.2";
+  version = "2.0";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "199pgwx9ziab3gxg6p0c24z8dp3bjpsvvshnmlph9zjsssq0xc93";
+    sha256 = "1lb846j7qs1hgqwkyifv51nhl3f8jimbc4lk8yn9nkaynw0vyzcg";
   };
   doCheck = false;
   buildInputs = [ setuptools_scm ];
