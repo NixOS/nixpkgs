7b8eb4948baf5fd1a4b31db9288e2fd63a6fdfb5
Orivej Desh <orivej@gmx.fr>
2019-01-16 01:21:03 +0000

    libjpeg-turbo: fix CVE-2018-20330 (#53950)
    (cherry picked from commit b0757da432ffc4b1e5b696b51ae81b36feb2d285)

--- pkgs/development/libraries/libjpeg-turbo/default.nix
+++ pkgs/development/libraries/libjpeg-turbo/default.nix
@@ -19,6 +19,11 @@ stdenv.mkDerivation rec {
       url = "https://github.com/libjpeg-turbo/libjpeg-turbo/commit/f8cca819a4fb.diff";
       sha256 = "1kgfag62qmphlrq0mz15g17zw7zrg9nzaz7d2vg50m6m7m5aw4y5";
     })
+    (fetchpatch {
+      name = "CVE-2018-20330.patch";
+      url = "https://github.com/libjpeg-turbo/libjpeg-turbo/commit/3d9c64e9f8aa1ee954d1d0bb3390fc894bb84da3.diff";
+      sha256 = "1jai8izw6xl05ihx24rpc96d1jcr9rp421cb02pbz3v53cxdasji";
+    })
   ];
 
   outputs = [ "bin" "dev" "out" "man" "doc" ];
