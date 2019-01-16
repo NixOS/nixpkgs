80a140c79960a61ea8d060ab4e3f94a2bfe8bdab
Tim Steinbach <tim@nequissimus.com>
2019-01-16 09:51:42 -0500

    awscli: 1.16.72 -> 1.16.89

--- pkgs/tools/admin/awscli/default.nix
+++ pkgs/tools/admin/awscli/default.nix
@@ -19,11 +19,11 @@ let
 
 in py.pkgs.buildPythonApplication rec {
   pname = "awscli";
-  version = "1.16.72"; # N.B: if you change this, change botocore to a matching version too
+  version = "1.16.89"; # N.B: if you change this, change botocore to a matching version too
 
   src = py.pkgs.fetchPypi {
     inherit pname version;
-    sha256 = "1ld4a6yxnh0v96fjjp8wjf7zvx41grl57mqg92p6zbfssr2jbqfv";
+    sha256 = "1i2f8nx8w6150jws0b732pvh8s5r6wq9yvv2m0a2k7cz1ihnzkxd";
   };
 
   # No tests included
