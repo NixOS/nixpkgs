e7f05dff34cdfdd8fab9d0c339c5c7aa594465de
Frederik Rietdijk <freddyrietdijk@fridh.nl>
2019-01-17 16:13:07 +0100

    python: setuptools: 40.6.2 -> 40.6.3

--- pkgs/development/python-modules/setuptools/default.nix
+++ pkgs/development/python-modules/setuptools/default.nix
@@ -8,13 +8,13 @@
 # Should use buildPythonPackage here somehow
 stdenv.mkDerivation rec {
   pname = "setuptools";
-  version = "40.6.2";
+  version = "40.6.3";
   name = "${python.libPrefix}-${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "86bb4d8e1b0fabad1f4642b64c335b673e53e7a381de03c9a89fe678152c4c64";
+    sha256 = "3b474dad69c49f0d2d86696b68105f3a6f195f7ab655af12ef9a9c326d2b08f8";
   };
 
   nativeBuildInputs = [ unzip wrapPython python.pythonForBuild ];
