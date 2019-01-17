b79b18bf750e08db66c2ce0c91c7879925cea1cd
Frederik Rietdijk <freddyrietdijk@fridh.nl>
2019-01-17 16:13:08 +0100

    python: uproot: 3.2.12 -> 3.2.15

--- pkgs/development/python-modules/uproot/default.nix
+++ pkgs/development/python-modules/uproot/default.nix
@@ -14,11 +14,11 @@
 
 buildPythonPackage rec {
   pname = "uproot";
-  version = "3.2.12";
+  version = "3.2.15";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0jxsv0038glxz87skjxr58fafwyqilivkrygpvk4nkp866i5kz2k";
+    sha256 = "a871f57529e3df170aa5556c1353a64077277644baecabb18d042954f2af9030";
   };
 
   buildInputs = [ pytestrunner ];
