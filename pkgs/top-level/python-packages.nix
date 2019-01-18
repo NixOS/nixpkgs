9cc1be5e77c9d5e0b6028369d9beaa013357bcd8
ivegotasthma <ivegotasthma@protonmail.com>
2019-01-18 14:32:23 +0200

    djangorestframework-jwt: init at 1.11.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2261,6 +2261,8 @@ in {
 
   djangorestframework = callPackage ../development/python-modules/djangorestframework { };
 
+  djangorestframework-jwt = callPackage ../development/python-modules/djangorestframework-jwt { };
+
   django-raster = callPackage ../development/python-modules/django-raster { };
 
   django_redis = callPackage ../development/python-modules/django_redis { };
