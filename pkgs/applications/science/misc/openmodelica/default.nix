e378ccf3d16ebdba697c5ca6a7eca5b8db79151a
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2019-01-19 14:38:55 +0200

    openmodelica: upgrade to 1.13.0

--- pkgs/applications/science/misc/openmodelica/default.nix
+++ pkgs/applications/science/misc/openmodelica/default.nix
@@ -1,7 +1,8 @@
-{stdenv, fetchgit, fetchsvn, autoconf, automake, libtool, gfortran, clang, cmake, gnumake,
-hwloc, jre, liblapack, blas, hdf5, expat, ncurses, readline, qt4, webkit, which,
-lp_solve, omniorb, sqlite, libatomic_ops, pkgconfig, file, gettext, flex, bison,
-doxygen, boost, openscenegraph, gnome2, pangox_compat, xorg, git, bash, gtk2, makeWrapper }:
+{stdenv, fetchFromGitHub, fetchgit, fetchsvn, autoconf, automake, libtool,
+gfortran, clang, cmake, gnumake, hwloc, jre, openblas, hdf5, expat, ncurses,
+readline, qt4, webkit, which, lp_solve, omniorb, sqlite, libatomic_ops,
+pkgconfig, file, gettext, flex, bison, doxygen, boost, openscenegraph, gnome2,
+pangox_compat, xorg, git, bash, gtk2, makeWrapper }:
 
 let
 
@@ -9,16 +10,23 @@ let
 
 in
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "openmodelica";
+  version = "1.13.0";
 
-  src = fetchgit (import ./src-main.nix);
+  src = fetchFromGitHub {
+    owner = "OpenModelica";
+    repo = "OpenModelica";
+    rev = "v${version}";
+    fetchSubmodules = true;
+    sha256 = "00vbhvqm79pndx03g1azdsv12yraxnwa5j4sqy5w35bm448ahzjp";
+  };
 
-  buildInputs = [autoconf cmake automake libtool gfortran clang gnumake
-    hwloc jre liblapack blas hdf5 expat ncurses readline qt4 webkit which
-    lp_solve omniorb sqlite libatomic_ops pkgconfig file gettext flex bison
-    doxygen boost openscenegraph gnome2.gtkglext pangox_compat xorg.libXmu
-    git gtk2 makeWrapper];
+  buildInputs = [autoconf cmake automake libtool gfortran clang gnumake hwloc
+    jre openblas hdf5 expat ncurses readline qt4 webkit which lp_solve omniorb
+    sqlite libatomic_ops pkgconfig file gettext flex bison doxygen boost
+    openscenegraph gnome2.gtkglext pangox_compat xorg.libXmu git gtk2
+    makeWrapper];
 
   hardeningDisable = [ "format" ];
 
@@ -40,18 +48,15 @@ stdenv.mkDerivation {
     for e in $(cd $out/bin && ls); do
       wrapProgram $out/bin/$e \
         --prefix PATH : "${gnumake}/bin" \
-        --prefix LIBRARY_PATH : "${stdenv.lib.makeLibraryPath [ liblapack blas ]}"
+        --prefix LIBRARY_PATH : "${stdenv.lib.makeLibraryPath [ openblas ]}"
     done
   '';
 
   meta = with stdenv.lib; {
-    description = "OpenModelica is an open-source Modelica-based modeling and simulation environment";
+    description = "An open-source Modelica-based modeling and simulation environment";
     homepage    = "https://openmodelica.org";
     license     = licenses.gpl3;
     maintainers = with maintainers; [ smironov ];
     platforms   = platforms.linux;
-    broken      = true;
   };
 }
-
-
