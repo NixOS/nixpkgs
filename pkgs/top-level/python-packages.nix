c7677d93008450543ab8639dc08e75084ec9639d
Benjamin Staffin <benley@gmail.com>
2019-01-18 21:01:19 -0500

    pythonPackages.jq: init at 0.1.6 (#54014)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2786,6 +2786,10 @@ in {
     inherit (self) systemd pytest;
   };
 
+  jq = callPackage ../development/python-modules/jq {
+    inherit (pkgs) jq;
+  };
+
   jsondate = callPackage ../development/python-modules/jsondate { };
 
   jsondiff = callPackage ../development/python-modules/jsondiff { };
