c53fe37d0e7f9214353f193d18cffdcebfd9b1ae
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2019-01-20 14:13:39 -0500

    qMasterPassword: fix on darwin, add meta.longDescription

--- pkgs/applications/misc/qMasterPassword/default.nix
+++ pkgs/applications/misc/qMasterPassword/default.nix
@@ -14,7 +14,13 @@ stdenv.mkDerivation rec {
   buildInputs = [ qtbase libX11 libXtst openssl libscrypt ];
   nativeBuildInputs = [ qmake ];
 
-  installPhase = ''
+  # Upstream install is mostly defunct. It hardcodes target.path and doesn't
+  # install anything but the binary.
+  installPhase = if stdenv.isDarwin then ''
+    mkdir -p "$out"/{Applications,bin}
+    mv qMasterPassword.app "$out"/Applications/
+    ln -s ../Applications/qMasterPassword.app/Contents/MacOS/qMasterPassword "$out"/bin/qMasterPassword
+  '' else ''
     mkdir -p $out/bin
     mkdir -p $out/share/{applications,doc/qMasterPassword,icons/qmasterpassword,icons/hicolor/512x512/apps}
     mv qMasterPassword $out/bin
@@ -25,10 +31,18 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with stdenv.lib; {
-      description = "Stateless Master Password Manager";
-      homepage = https://github.com/bkueng/qMasterPassword;
-      license = licenses.gpl3;
-      maintainers = [ maintainers.tadeokondrak ];
-      platforms = platforms.all;
+    description = "Stateless Master Password Manager";
+    longDescription = ''
+      Access all your passwords using only a single master password. But in
+      contrast to other managers it does not store any passwords: Unique
+      passwords are generated from the master password and a site name. This
+      means you automatically get different passwords for each account and
+      there is no password file that can be lost or get stolen. There is also
+      no need to trust any online password service.
+    '';
+    homepage = https://github.com/bkueng/qMasterPassword;
+    license = licenses.gpl3;
+    maintainers = [ maintainers.tadeokondrak ];
+    platforms = platforms.all;
   };
 }
