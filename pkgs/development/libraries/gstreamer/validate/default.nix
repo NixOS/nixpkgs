6fa0062938dc705e2f3da905139a0dbdd316bf8c
Niklas Hambüchen <mail@nh2.me>
2019-01-21 02:48:04 +0100

    gstreamer: 1.14.4 -> 1.15.1, and extended gst-plugins-bad.
    During the 1.14 -> 1.15 upgrade, lots of stuff stopped working because
    gstreamer changed what features are enabled by default and which ones are
    automatically turned on/off via pkgconfig dependency detection.
    
    This resulted in the `gstreamer` ("core" attribute in nixpkgs) package
    to have only 15 of its previous 163 build targets enabled, and downstream
    packages breaking correspondingly.
    
    To ease maintainability and to ensure users will find the expected features
    available (and when not, will see in the nix file why not), we now pass
    the `-Dauto_features=enabled` Meson build flag to all gstreamer builds,
    which sets all `auto` dependencies to `enabled`, and we explicitly disable
    those that we can't build.
    
    This means in particular that `gst-plugins-bad` now has vastly more integrations
    (namely all for which nixpkgs has libraries available).

--- pkgs/development/libraries/gstreamer/validate/default.nix
+++ pkgs/development/libraries/gstreamer/validate/default.nix
@@ -4,7 +4,7 @@
 
 stdenv.mkDerivation rec {
   name = "gst-validate-${version}";
-  version = "1.14.4";
+  version = "1.15.1";
 
   meta = {
     description = "Integration testing infrastructure for the GStreamer framework";
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "${meta.homepage}/src/gst-validate/${name}.tar.xz";
-    sha256 = "1ismv4i7ldi04swq76pcpd5apxqd52yify5hvlyan2yw9flwrp0q";
+    sha256 = "195hwblagfsnq1xn858al3f32jn5nynr4j5x395dpg3vl3c4k5v4";
   };
 
   outputs = [ "out" "dev" ];
@@ -31,4 +31,9 @@ stdenv.mkDerivation rec {
   propagatedBuildInputs = [ gstreamer gst-plugins-base ];
 
   enableParallelBuilding = true;
+
+  mesonFlags = [
+    # Enables all features, so that we know when new dependencies are necessary.
+    "-Dauto_features=enabled"
+  ];
 }
