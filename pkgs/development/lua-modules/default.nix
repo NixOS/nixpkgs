97f5412803f3679edbbe0916a814269b6e50b0c1
Matthieu Coudron <mattator@gmail.com>
2019-01-22 18:44:15 +0900

    some more cleanup

--- pkgs/development/lua-modules/default.nix
+++ pkgs/development/lua-modules/default.nix
@@ -1,7 +1,6 @@
 # inspired by pkgs/development/haskell-modules/default.nix
 { pkgs, stdenv, lib
 , lua
-
 , overrides ? (self: super: {})
 , initialPackages ? import ../../initial-packages.nix
 , nonLuarocksPackages ? import ./non-luarocks-packages.nix
@@ -11,28 +10,13 @@
 let
 
   inherit (lib) extends makeExtensible;
-  # inherit (haskellLib) makePackageSet;
 
-  # luarocks ?
-  # luaPackages = pkgs.callPackage makePackageSet {
-  #   package-set = initialPackages;
-  #   inherit stdenv haskellLib ghc buildHaskellPackages extensible-self all-cabal-hashes;
-  # };
-
-# # Applying this to an attribute set will cause nix-env to look
-  # # inside the set for derivations.
-  # recurseIntoAttrs = attrs: attrs // { recurseForDerivations = true; };
-
-  # recurseIntoAttrs
   initialPackages = (pkgs.callPackage ../../top-level/lua-packages.nix {
     inherit lua;
-    # inherit buildLuaPackage;
   });
 
   commonConfiguration = configurationCommon { inherit pkgs; };
-  # nixConfiguration = configurationNix { inherit pkgs haskellLib; };
 
-  # generatedPackages = pkgs.callPackage ./generated-packages.nix { };
   # when the generation of the file fails, just remove it
   generatedPackages = if (builtins.pathExists ./generated-packages.nix) then
     pkgs.callPackage ./generated-packages.nix { }
@@ -42,7 +26,6 @@ let
     (extends overrides
         (extends commonConfiguration
           (extends generatedPackages
-              # (extends nonLuarocksPackages
               initialPackages
               )
           )
@@ -50,4 +33,3 @@ let
           ;
 in
   extensible-self
-  # initialPackages
