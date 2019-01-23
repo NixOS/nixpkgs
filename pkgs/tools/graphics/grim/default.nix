becca46d4b6f681680cd48c30e0ef1d6dd38ecb1
buffet <niclas@countingsort.com>
2019-01-23 14:36:32 +0100

    grim: init at 1.0

---
+++ pkgs/tools/graphics/grim/default.nix
@@ -0,0 +1,31 @@
+{ stdenv, fetchFromGitHub, cairo, libjpeg, meson, ninja, wayland, pkgconfig, wayland-protocols }:
+
+stdenv.mkDerivation rec {
+  name = "grim-${version}";
+  version = "1.0";
+
+  src = fetchFromGitHub {
+    owner = "emersion";
+    repo = "grim";
+    rev = "v${version}";
+    sha256 = "1mpmxkzssgzqh9z263y8vk40dayw32kah66sb8ja7yw22rm7f4zf";
+  };
+
+  buildInputs = [
+    cairo
+    libjpeg
+    meson
+    ninja
+    pkgconfig
+    wayland
+    wayland-protocols
+  ];
+
+  meta = with stdenv.lib; {
+    description = "Grab images from a Wayland compositor";
+    homepage = https://github.com/emersion/grim;
+    license = licenses.mit;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ buffet ];
+  };
+}
