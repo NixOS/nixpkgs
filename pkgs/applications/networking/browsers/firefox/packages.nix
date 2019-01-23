cb7f7364a4e94834c7217bfd4aade61f93b0d33d
Andreas Rammhold <andreas@rammhold.de>
2019-01-23 01:07:33 +0100

    firefox: 64.0.2 -> 65.0
    There have been some more changes to the source tree which broke the
    buildconfig patch. This commit adds another patch that can be used for
    the future versions. Once all the flavors are based off a new(ish)
    firefox release we can remove the old patch.

--- pkgs/applications/networking/browsers/firefox/packages.nix
+++ pkgs/applications/networking/browsers/firefox/packages.nix
@@ -14,14 +14,14 @@ rec {
 
   firefox = common rec {
     pname = "firefox";
-    ffversion = "64.0.2";
+    ffversion = "65.0";
     src = fetchurl {
       url = "mirror://mozilla/firefox/releases/${ffversion}/source/firefox-${ffversion}.source.tar.xz";
-      sha512 = "2xvzbx20i2qwld04g3wl9j6j8bkcja3i83sf9cpngayllhrjki29020izrwjxrgm0z3isg7zijw656v1v2zzmhlfkpkbk71n2gjj7md";
+      sha512 = "39bx76whgf53rkfqqy8gfhd44wikh89zpnqr930v4grqg3v0pfr8mbvp7xzjjlf5r7bski0wxibn9vyyy273fp99zyj1w2m5ihh9aqh";
     };
 
     patches = nixpkgsPatches ++ [
-      ./no-buildconfig.patch
+      ./no-buildconfig-ffx65.patch
     ];
 
     extraNativeBuildInputs = [ python3 ];
