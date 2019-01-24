e138c1e4e88ffe1058268d5891802447e0b525e0
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2019-01-24 10:09:20 +0100

    python.pkgs.GitPython: use patch to specify path to git

--- pkgs/development/python-modules/GitPython/default.nix
+++ pkgs/development/python-modules/GitPython/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, fetchPypi, git, gitdb2, mock, nose, ddt }:
+{ lib, buildPythonPackage, fetchPypi, substituteAll, git, gitdb2, mock, nose, ddt }:
 
 buildPythonPackage rec {
   version = "2.1.11";
@@ -9,12 +9,15 @@ buildPythonPackage rec {
     sha256 = "8237dc5bfd6f1366abeee5624111b9d6879393d84745a507de0fda86043b65a8";
   };
 
+  patches = [
+    (substituteAll {
+      src = ./hardcode-git-path.patch;
+      inherit git;
+    })
+  ];
+
   checkInputs = [ mock nose ddt ];
   propagatedBuildInputs = [ gitdb2 ];
-
-  postPatch = ''
-    sed -i "s|^refresh()$|refresh(path='${git}/bin/git')|" git/__init__.py
-  '';
 
   # Tests require a git repo
   doCheck = false;
