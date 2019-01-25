db3b270f79dfd4cf30d3ae59cd7174b62a9d06a8
Meghea Iulian <iulian.meghea@gmail.com>
2019-01-26 01:06:26 +0200

    gitkraken: 4.2.0 -> 4.2.1

--- pkgs/applications/version-management/gitkraken/default.nix
+++ pkgs/applications/version-management/gitkraken/default.nix
@@ -12,11 +12,11 @@ let
 in
 stdenv.mkDerivation rec {
   name = "gitkraken-${version}";
-  version = "4.2.0";
+  version = "4.2.1";
 
   src = fetchurl {
     url = "https://release.axocdn.com/linux/GitKraken-v${version}.deb";
-    sha256 = "1r1pamwg830a18928jjfajcq2yiqvadbdvklh89dpdqk2529yha0";
+    sha256 = "07f9h3276bs7m22vwpxrxmlwnq7l5inr2l67nmpiaz1569yabwsg";
   };
 
   libPath = makeLibraryPath [
