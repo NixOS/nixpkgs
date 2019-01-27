38a84d84e16d25c987dfd19b9f01323cc4ce1d63
R. RyanTM <ryantm-bot@ryantm.com>
2019-01-27 03:01:14 -0800

    python37Packages.py3status: 3.15 -> 3.16
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-py3status/versions

--- pkgs/development/python-modules/py3status/default.nix
+++ pkgs/development/python-modules/py3status/default.nix
@@ -19,11 +19,11 @@
 
 buildPythonPackage rec {
   pname = "py3status";
-  version = "3.15";
+  version = "3.16";
   
   src = fetchPypi {
     inherit pname version;
-    sha256 = "78aa7fa0af707641e215ea93bfd4bb5fd47f18a7193d84ed60bb9e6cccb75b7f";
+    sha256 = "1xrfph277bgjln3jbpzpgkhxad04fjvj7s3xfil42q1sxi4s3q3g";
   };
 
   doCheck = false;
