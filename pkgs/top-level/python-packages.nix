1b11d0f80f60f562edadd2c0e28d925c57347b77
Henri Bourcereau <henri.bourcereau@gmail.com>
2019-01-27 11:55:26 +0100

    pythonPackages.pymemoize: init at 1.0.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4488,6 +4488,8 @@ in {
 
   pykickstart = callPackage ../development/python-modules/pykickstart { };
 
+  pymemoize = callPackage ../development/python-modules/pymemoize { };
+
   pyobjc = if stdenv.isDarwin
     then callPackage ../development/python-modules/pyobjc {}
     else throw "pyobjc can only be built on Mac OS";
