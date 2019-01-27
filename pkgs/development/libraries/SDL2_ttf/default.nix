61964bacd915be750f40577ccb1a4fa701e39562
R. RyanTM <ryantm-bot@ryantm.com>
2019-01-26 22:25:26 -0800

    SDL2_ttf: 2.0.14 -> 2.0.15
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/sdl2_ttf/versions

--- pkgs/development/libraries/SDL2_ttf/default.nix
+++ pkgs/development/libraries/SDL2_ttf/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "SDL2_ttf-${version}";
-  version = "2.0.14";
+  version = "2.0.15";
 
   src = fetchurl {
     url = "https://www.libsdl.org/projects/SDL_ttf/release/${name}.tar.gz";
-    sha256 = "0xljwcpvd2knrjdfag5b257xqayplz55mqlszrqp0kpnphh5xnrl";
+    sha256 = "0cyd48dipc0m399qy8s03lci8b0bpiy8xlkvrm2ia7wcv0dfpv59";
   };
 
   configureFlags = stdenv.lib.optional stdenv.isDarwin "--disable-sdltest";
