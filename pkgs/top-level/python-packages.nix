177ed2f520816a1f04d13d85e62805c24a1a4512
Henri Bourcereau <henri.bourcereau@gmail.com>
2019-01-27 11:46:08 +0100

    pythonPackages.django-auth-ldap: init at 1.7.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2907,6 +2907,8 @@ in {
 
   django_appconf = callPackage ../development/python-modules/django_appconf { };
 
+  django-auth-ldap = callPackage ../development/python-modules/django-auth-ldap { };
+
   django_colorful = callPackage ../development/python-modules/django_colorful { };
 
   django-cache-url = callPackage ../development/python-modules/django-cache-url { };
