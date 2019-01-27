d16ccbefad8bf2295758ab5bd62d418c0fdba308
R. RyanTM <ryantm-bot@ryantm.com>
2019-01-26 23:43:39 -0800

    sundials: 4.0.1 -> 4.0.2
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/sundials/versions

--- pkgs/development/libraries/sundials/default.nix
+++ pkgs/development/libraries/sundials/default.nix
@@ -3,12 +3,12 @@
 stdenv.mkDerivation rec {
 
   pname = "sundials";
-  version = "4.0.1";
+  version = "4.0.2";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://computation.llnl.gov/projects/${pname}/download/${pname}-${version}.tar.gz";
-    sha256 = "1m5f2glxmgc6imjr0yqqp448r8q3kvsfp8dxxn83k00fcb40kr19";
+    sha256 = "0xfk0icsi63yi1dby4rn02ppwkzfykciw6q03bk454gdia9xcmk6";
   };
 
   preConfigure = ''
