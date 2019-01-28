f1653f473c8a272d795892a18211afc5b0f80c27
Alec Snyder <linuxbash8@gmail.com>
2019-01-28 14:42:22 -0800

    git-town: init at 7.2.0 fix maintainers section

--- pkgs/tools/misc/git-town/default.nix
+++ pkgs/tools/misc/git-town/default.nix
@@ -15,10 +15,10 @@
 
     buildFlagsArray = ["-ldflags=-X github.com/Originate/git-town/src/cmd.version=v${version} -X github.com/Originate/git-town/src/cmd.buildDate=nix"];
 
-    meta = {
+    meta = with stdenv.lib; {
       description = "Generic, high-level git support for git-flow workflows";
       homepage = http://www.git-town.com/;
-      maintainers = [ stdenv.maintainers.allonsy ];
+      maintainers = with maintainers; [ stdenv.maintainers.allonsy ];
       license = stdenv.lib.licenses.mit;
     };
   }
