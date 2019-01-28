cd1d83f0f8bbd9950c6ee8fc2431f624b5f645f8
Alec Snyder <linuxbash8@gmail.com>
2019-01-28 14:43:06 -0800

    git-town: init at 7.2.0 various cleanup

--- pkgs/tools/misc/git-town/default.nix
+++ pkgs/tools/misc/git-town/default.nix
@@ -13,12 +13,12 @@
       sha256 = "0hr0c6iya34lanfhsg9kj03l4ajalcfxkbn4bgwh0749smhi6mrj";
     };
 
-    buildFlagsArray = ["-ldflags=-X github.com/Originate/git-town/src/cmd.version=v${version} -X github.com/Originate/git-town/src/cmd.buildDate=nix"];
+    buildFlagsArray = [ "-ldflags=-X github.com/Originate/git-town/src/cmd.version=v${version} -X github.com/Originate/git-town/src/cmd.buildDate=nix" ];
 
     meta = with stdenv.lib; {
       description = "Generic, high-level git support for git-flow workflows";
       homepage = http://www.git-town.com/;
-      maintainers = with maintainers; [ stdenv.maintainers.allonsy ];
+      maintainers = with maintainers; [ allonsy ];
       license = stdenv.lib.licenses.mit;
     };
   }
