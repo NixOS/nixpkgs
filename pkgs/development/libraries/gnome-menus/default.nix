e48c7a3a15dd60dc8d346391d1edee863809b8be
R. RyanTM <ryantm-bot@ryantm.com>
2019-01-28 06:22:31 -0800

    gnome-menus: 3.31.3 -> 3.31.4
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/gnome-menus/versions

--- pkgs/development/libraries/gnome-menus/default.nix
+++ pkgs/development/libraries/gnome-menus/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-menus";
-  version = "3.31.3";
+  version = "3.31.4";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "11i5m0w15by1k8d94xla54nr4r8nnb63wk6iq0dzki4cv5d55qgw";
+    sha256 = "1iihxcibjg22jxsw3s1cxzcq0rhn1rdmx4xg7qjqij981afs8dr7";
   };
 
   makeFlags = "INTROSPECTION_GIRDIR=$(out)/share/gir-1.0/ INTROSPECTION_TYPELIBDIR=$(out)/lib/girepository-1.0";
