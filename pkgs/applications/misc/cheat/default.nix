e9b9f056a86d1c7febb80f2b6d097e68327a39f0
Jörg Thalheim <joerg@thalheim.io>
2019-01-29 11:57:36 +0000

    cheat: 2.3.1 -> 2.5.1

--- pkgs/applications/misc/cheat/default.nix
+++ pkgs/applications/misc/cheat/default.nix
@@ -1,30 +1,37 @@
-{ stdenv, python3Packages, fetchFromGitHub }:
+{ stdenv, python3, fetchFromGitHub }:
 
-with python3Packages;
+with python3.pkgs;
 buildPythonApplication rec {
   name = "${pname}-${version}";
   pname = "cheat";
-  version = "2.3.1";
+  version = "2.5.1";
 
-  propagatedBuildInputs = [ docopt pygments ];
+  propagatedBuildInputs = [ docopt pygments termcolor ];
 
   src = fetchFromGitHub {
     owner = "chrisallenlane";
     repo = "cheat";
     rev = version;
-    sha256 = "1dcpjvbv648r8325qjf30m8b4cyrrjbzc2kvh40zy2mbjsa755zr";
+    sha256 = "1i543hvg1yizamfd83bawflfcb500hvc72i59ikck8j1hjk50hsl";
   };
   # no tests available
   doCheck = false;
 
   postInstall = ''
     install -D man1/cheat.1.gz $out/share/man/man1/cheat.1.gz
+    mv $out/${python3.sitePackages}/etc $out/
+    mv $out/${python3.sitePackages}/usr/share/* $out/share/
+    rm -r $out/${python3.sitePackages}/usr
   '';
+
+  makeWrapperArgs = [
+    "--suffix" "CHEAT_PATH" ":" "$out/share/cheat"
+  ];
 
   meta = with stdenv.lib; {
     description = "cheat allows you to create and view interactive cheatsheets on the command-line";
     maintainers = with maintainers; [ mic92 ];
-    license = with licenses; [gpl3 mit];
+    license = with licenses; [ gpl3 mit ];
     homepage = https://github.com/chrisallenlane/cheat;
   };
 }
