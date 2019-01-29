7ed3c32015ce8cfa0fc1258cec647ac9ace62b19
Will Dietz <w@wdtz.org>
2019-01-29 07:07:53 -0600

    xlayoutdisplay: init at 1.0.2
    (promoting from my NUR repo)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23011,6 +23011,8 @@ in
 
   xinput_calibrator = callPackage ../tools/X11/xinput_calibrator { };
 
+  xlayoutdisplay = callPackage ../tools/X11/xlayoutdisplay { };
+
   xlog = callPackage ../applications/misc/xlog { };
 
   xmagnify = callPackage ../tools/X11/xmagnify { };
