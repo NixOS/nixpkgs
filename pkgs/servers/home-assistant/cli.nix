c3b01eed801ff4eb74a8c2637866d16ee26db2c7
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2019-01-29 23:40:11 +0100

    home-assistant-cli: use python36
    dateparser tests fail on python36: https://github.com/NixOS/nixpkgs/issues/52766

--- pkgs/servers/home-assistant/cli.nix
+++ pkgs/servers/home-assistant/cli.nix
@@ -1,10 +1,11 @@
-{ lib, python3, glibcLocales }:
+# dateparser tests fail on pyton37: https://github.com/NixOS/nixpkgs/issues/52766
+{ lib, python36, glibcLocales }:
 
-python3.pkgs.buildPythonApplication rec {
+python36.pkgs.buildPythonApplication rec {
   pname = "homeassistant-cli";
   version = "0.4.2";
 
-  src = python3.pkgs.fetchPypi {
+  src = python36.pkgs.fetchPypi {
     inherit pname version;
     sha256 = "e0b05af9e49baf88a44f1b36c3446a106223016dceefd5f9910e204af5901f44";
   };
@@ -14,11 +15,11 @@ python3.pkgs.buildPythonApplication rec {
     sed -i "s/'\(.*\)\(==\|>=\).*'/'\1'/g" setup.py
   '';
 
-  propagatedBuildInputs = with python3.pkgs; [
+  propagatedBuildInputs = with python36.pkgs; [
     requests pyyaml netdisco click click-log tabulate idna jsonpath_rw jinja2 dateparser
   ];
 
-  checkInputs = with python3.pkgs; [
+  checkInputs = with python36.pkgs; [
     pytest requests-mock glibcLocales
   ];
 
