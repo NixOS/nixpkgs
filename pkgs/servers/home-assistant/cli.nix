cfd556fd4a5baf6bc552cec783843a00ba30f309
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2019-01-29 23:31:10 +0100

    home-assistant-cli: 0.3.0 -> 0.4.2

--- pkgs/servers/home-assistant/cli.nix
+++ pkgs/servers/home-assistant/cli.nix
@@ -1,29 +1,29 @@
-{ lib, python3 }:
+{ lib, python3, glibcLocales }:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "homeassistant-cli";
-  version = "0.3.0";
+  version = "0.4.2";
 
   src = python3.pkgs.fetchPypi {
     inherit pname version;
-    sha256 = "42d7cb008801d7a448b62aed1fc46dd450ee67397bf16faabb02f691417db4b2";
+    sha256 = "e0b05af9e49baf88a44f1b36c3446a106223016dceefd5f9910e204af5901f44";
   };
 
   postPatch = ''
     # Ignore pinned versions
-    sed -i "s/'\(.*\)==.*'/'\1'/g" setup.py
+    sed -i "s/'\(.*\)\(==\|>=\).*'/'\1'/g" setup.py
   '';
 
   propagatedBuildInputs = with python3.pkgs; [
-    requests pyyaml netdisco click click-log tabulate idna jsonpath_rw jinja2
+    requests pyyaml netdisco click click-log tabulate idna jsonpath_rw jinja2 dateparser
   ];
 
   checkInputs = with python3.pkgs; [
-    pytest requests-mock
+    pytest requests-mock glibcLocales
   ];
 
   checkPhase = ''
-    pytest
+    LC_ALL=en_US.UTF-8 pytest
   '';
 
   meta = with lib; {
