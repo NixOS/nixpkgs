d2c9ee8dbbe02c983a2a7ab83cc35b9a11ba3fdf
Tim Steinbach <tim@nequissimus.com>
2019-01-31 07:17:38 -0500

    linux: 4.9.153 -> 4.9.154
    (cherry picked from commit 468b6cea72d40676a7d113008144990569895aea)

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.153";
+  version = "4.9.154";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "06kksywm8yjvmhmwdkqmm6546j5nqprsal3k22p981smqag94rlh";
+    sha256 = "15jnkpf6kg061970cwh2z0l6nscffl63y1d0rq5f2y3gq4d4ycav";
   };
 } // (args.argsOverride or {}))
