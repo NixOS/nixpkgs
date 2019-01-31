5447f7028b3e3e3bd594da7906c47d9388b58aa5
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2019-01-31 19:38:32 +0100

    papis: 0.6 -> 0.7.5
    fixes https://github.com/NixOS/nixpkgs/issues/54821

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15734,7 +15734,7 @@ in
 
   papirus-icon-theme = callPackage ../data/icons/papirus-icon-theme { };
 
-  papis = python3Packages.callPackage ../tools/misc/papis { };
+  papis = callPackage ../tools/misc/papis { };
 
   pecita = callPackage ../data/fonts/pecita {};
 
