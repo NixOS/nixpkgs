77e29cd5fa397c23596a03a34a2c63086b030abb
Herwig Hochleitner <herwig@bendlas.net>
2019-01-31 20:39:03 +0100

    i2p: 0.9.35 -> 0.9.38
    (cherry picked from commit f7165b2ad610a3b19dee81ae8b431873ffd4d702)

--- pkgs/tools/networking/i2p/default.nix
+++ pkgs/tools/networking/i2p/default.nix
@@ -27,10 +27,10 @@ let wrapper = stdenv.mkDerivation rec {
 in
 
 stdenv.mkDerivation rec {
-  name = "i2p-0.9.35";
+  name = "i2p-0.9.38";
   src = fetchurl {
     url = "https://github.com/i2p/i2p.i2p/archive/${name}.tar.gz";
-    sha256 = "02p76vn1777lgv4zs27r6i9s4yk7b2x61b25i8dqmn6j60y3fa4g";
+    sha256 = "0fxn8q6ccpjxr41s97nmjxg7hx12dzwrm5a7gyxgr44r7l77qlv6";
   };
   buildInputs = [ jdk ant gettext which ];
   patches = [ ./i2p.patch ];
