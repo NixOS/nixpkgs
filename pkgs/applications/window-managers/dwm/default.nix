1ad836b326e36700f9e9cefe8f7933ad87d165d6
Jan Malakhovski <oxij@oxij.org>
2019-02-03 15:30:10 +0000

    dwm: cleanup whitespace

--- pkgs/applications/window-managers/dwm/default.nix
+++ pkgs/applications/window-managers/dwm/default.nix
@@ -5,21 +5,21 @@ let
 in
 stdenv.mkDerivation {
   inherit name;
- 
+
   src = fetchurl {
     url = "https://dl.suckless.org/dwm/${name}.tar.gz";
     sha256 = "1zkmwb6df6m254shx06ly90c0q4jl70skk1pvkixpb7hcxhwbxn2";
   };
- 
+
   buildInputs = [ libX11 libXinerama libXft ];
- 
+
   prePatch = ''sed -i "s@/usr/local@$out@" config.mk'';
 
   # Allow users set their own list of patches
   inherit patches;
 
   buildPhase = " make ";
- 
+
   meta = {
     homepage = https://suckless.org/;
     description = "Dynamic window manager for X";
