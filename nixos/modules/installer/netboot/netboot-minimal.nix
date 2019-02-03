399531bdfd86dde7a0f6af831c3d10eca62ee657
lassulus <lassulus@lassul.us>
2019-02-03 14:39:14 +0100

    nixos/netboot: netboot_minimal downloads the iso and extacts it

--- nixos/modules/installer/netboot/netboot-minimal.nix
+++ nixos/modules/installer/netboot/netboot-minimal.nix
@@ -1,10 +1,34 @@
 # This module defines a small netboot environment.
 
-{ ... }:
+{ config, ... }:
 
 {
-  imports =
-    [ ./netboot-base.nix
-      ../../profiles/minimal.nix
-    ];
+  imports = [
+    ../cd-dvd/installation-cd-base.nix
+  ];
+  boot.initrd.network.enable = true;
+  boot.devSize = "600m";
+
+  # TODO add all network modules
+  boot.initrd.kernelModules = [ "e1000" ];
+
+  # needed for dns in initrd
+  boot.initrd.extraUtilsCommands =''
+    cp ${pkgs.stdenv.cc.libc}/lib/libnss_dns.so.* $out/lib/
+  '';
+
+  boot.initrd.network.postCommands = ''
+    iso_link=https://releases.nixos.org$(wget -O- 'https://nixos.org/channels/nixos-${config.system.nixos.release}' | grep -o "/nixos/[^']*minimal[^']*x86_64-linux.iso")
+    wget "$iso_link" -O /dev/root
+  '';
+  boot.initrd.postMountCommands = ''
+    export stage2Init=$(find /mnt-root/nix/store -type f -name init | grep 'nixos-system' | sed 's@^/mnt-root@@')
+  '';
+
+  system.build.netbootIpxeScript = pkgs.writeTextDir "netboot.ipxe" ''
+    #!ipxe
+    kernel ${pkgs.stdenv.hostPlatform.platform.kernelTarget} init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams}
+    initrd initrd
+    boot
+  '';
 }
--- nixos/release.nix
+++ nixos/release.nix
@@ -117,7 +117,7 @@ let
         postBuild = ''
           mkdir -p $out/nix-support
           echo "file ${kernelTarget} ${build.kernel}/${kernelTarget}" >> $out/nix-support/hydra-build-products
-          echo "file initrd ${build.netbootRamdisk}/initrd" >> $out/nix-support/hydra-build-products
+          echo "file initrd ${build.initialRamdisk}/initrd" >> $out/nix-support/hydra-build-products
           echo "file ipxe ${build.netbootIpxeScript}/netboot.ipxe" >> $out/nix-support/hydra-build-products
         '';
         preferLocalBuild = true;
