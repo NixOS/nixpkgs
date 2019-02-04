7a8b65d216d069f8f594151fc47162b179227a86
Sander van der Burg <svanderburg@gmail.com>
2019-02-04 23:07:04 +0100

    titaniumenv: fix broken function header

--- pkgs/development/mobile/titaniumenv/default.nix
+++ pkgs/development/mobile/titaniumenv/default.nix
@@ -1,14 +1,6 @@
-{pkgs, pkgs_i686, tiVersion ? "7.1.0.GA"}:
+{pkgs, pkgs_i686, androidenv, xcodeenv, tiVersion ? "7.1.0.GA"}:
 
 rec {
-  androidenv = import ../../nix-androidenvtests/androidenv {
-    inherit pkgs pkgs_i686;
-  };
-
-  xcodeenv = import ../../nix-xcodeenvtests/xcodeenv {
-    inherit (pkgs) stdenv;
-  };
-
   titaniumsdk = let
     titaniumSdkFile = if tiVersion == "7.1.0.GA" then ./titaniumsdk-7.1.nix
       else if tiVersion == "7.5.1.GA" then ./titaniumsdk-7.5.nix
