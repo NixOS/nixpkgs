d2f21ed3949ecaffe0c0c7d683a7b2f0d55392f9
Benjamin Staffin <benley@gmail.com>
2019-02-04 17:21:42 -0500

    docker-containers: add environment, ports, user, workdir options

--- nixos/modules/virtualisation/docker-containers.nix
+++ nixos/modules/virtualisation/docker-containers.nix
@@ -8,21 +8,58 @@ let
     { name, config, ... }: {
 
       options = {
+
         image = mkOption {
           type = types.str;
           description = "Docker image to run.";
+          example = "library/hello-world";
         };
+
         cmd = mkOption {
           type =  with types; listOf string;
           default = [];
           description = "Commandline arguments to pass to the image's entrypoint.";
         };
+
         entrypoint = mkOption {
-          type = types.nullOr types.string;
+          type = with types; nullOr string;
           description = "Overwrite the default entrypoint of the image.";
           default = null;
         };
+
+        environment = mkOption {
+          type = with types; attrsOf string;
+          default = {};
+          description = "Environment variables to set for this container.";
+          example = {
+            DATABASE_HOST = "db.example.com";
+            DATABASE_PORT = "3306";
+          };
+        };
+
+        ports = mkOption {
+          type = with types; attrsOf string;
+          default = {};
+          description = "Network ports to forward from the host to this container.";
+          example = {
+            "8080" = "9000/tcp";
+          };
+        };
+
+        user = mkOption {
+          type = with types; nullOr string;
+          default = null;
+          description = ''
+            Override the username or UID (and optionally groupname or
+            GID) used in the container.
+          '';
+          example = "nobody:nogroup";
+        };
+
         volumes = mkOption {
+          # Note: these are "src:dst" lists so it's possible for `src`
+          # to refer to a /nix/store path, and for `dst` to include
+          # mount options.
           type = with types; listOf string;
           default = [];
           description = "List of volumes to attach to this container.";
@@ -31,6 +68,14 @@ let
             "/path/on/host:/path/inside/container"
           ];
         };
+
+        workdir = mkOption {
+          type = with types; nullOr string;
+          default = null;
+          description = "Override the default working directory for the container.";
+          example = "/var/lib/hello_world";
+        };
+
         extraDockerOptions = mkOption {
           type = with types; listOf string;
           default = [];
@@ -45,19 +90,22 @@ let
       wantedBy = [ "multi-user.target" ];
       after = [ "docker.service" "docker.socket" ];
       requires = [ "docker.service" "docker.socket" ];
-      script = lib.concatStringsSep " " ([
+      script = concatStringsSep " \\\n  " ([
         "exec ${pkgs.docker}/bin/docker run"
         "--rm"
         "--name=${containerName}"
-      ] ++ lib.optional (! isNull container.entrypoint)
-        "--entrypoint=${lib.escapeShellArg container.entrypoint}"
-        ++ (map (v: "-v ${lib.escapeShellArg v}") container.volumes)
-        ++ [
-          (lib.escapeShellArgs container.extraDockerOptions)
-          container.image
-        ]
+      ] ++ optional (! isNull container.entrypoint)
+        "--entrypoint=${escapeShellArg container.entrypoint}"
+        ++ (mapAttrsToList (k: v: "-e ${escapeShellArg k}=${escapeShellArg v}") container.environment)
+        ++ (mapAttrsToList (k: v: "-p ${escapeShellArg k}:${escapeShellArg v}") container.ports)
+        ++ optional (! isNull container.user) "-u ${escapeShellArg container.user}"
+        ++ (map (v: "-v ${escapeShellArg v}") container.volumes)
+        ++ optional (! isNull container.workdir) "-w ${escapeShellArg container.workdir}"
+        # I know escapeShellArgs exists; this results in prettier output
+        ++ map escapeShellArg container.extraDockerOptions
+        ++ [container.image]
       );
-      scriptArgs = lib.escapeShellArgs container.cmd;
+      scriptArgs = escapeShellArgs container.cmd;
       preStop = "${pkgs.docker}/bin/docker stop ${containerName}";
       reload = "${pkgs.docker}/bin/docker restart ${containerName}";
       serviceConfig = {
@@ -79,7 +127,7 @@ in {
 
   config = {
 
-    systemd.services = lib.mapAttrs' (n: v: lib.nameValuePair "docker-${n}" (mkService n v)) cfg;
+    systemd.services = mapAttrs' (n: v: nameValuePair "docker-${n}" (mkService n v)) cfg;
 
     virtualisation.docker.enable = true;
 
