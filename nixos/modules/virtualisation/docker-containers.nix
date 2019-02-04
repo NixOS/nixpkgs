f4f0bb424c2618532d8115dc77e54ff3ce842e4f
Benjamin Staffin <benley@gmail.com>
2019-02-04 17:44:05 -0500

    docker-containers: log-driver, string->str, line wrapping

--- nixos/modules/virtualisation/docker-containers.nix
+++ nixos/modules/virtualisation/docker-containers.nix
@@ -16,19 +16,19 @@ let
         };
 
         cmd = mkOption {
-          type =  with types; listOf string;
+          type =  with types; listOf str;
           default = [];
           description = "Commandline arguments to pass to the image's entrypoint.";
         };
 
         entrypoint = mkOption {
-          type = with types; nullOr string;
+          type = with types; nullOr str;
           description = "Overwrite the default entrypoint of the image.";
           default = null;
         };
 
         environment = mkOption {
-          type = with types; attrsOf string;
+          type = with types; attrsOf str;
           default = {};
           description = "Environment variables to set for this container.";
           example = {
@@ -37,8 +37,19 @@ let
           };
         };
 
+        log-driver = mkOption {
+          type = types.str;
+          default = "none";
+          description = ''
+            Logging driver for the container.  The default of "none" means that
+            the container's logs will be handled as part of the systemd unit.
+            Setting this to "journald" will result in duplicate logging, but
+            the container's logs will be visible to the `docker logs` command.
+          '';
+        };
+
         ports = mkOption {
-          type = with types; attrsOf string;
+          type = with types; attrsOf str;
           default = {};
           description = "Network ports to forward from the host to this container.";
           example = {
@@ -47,11 +58,11 @@ let
         };
 
         user = mkOption {
-          type = with types; nullOr string;
+          type = with types; nullOr str;
           default = null;
           description = ''
-            Override the username or UID (and optionally groupname or
-            GID) used in the container.
+            Override the username or UID (and optionally groupname or GID) used
+            in the container.
           '';
           example = "nobody:nogroup";
         };
@@ -60,7 +71,7 @@ let
           # Note: these are "src:dst" lists so it's possible for `src`
           # to refer to a /nix/store path, and for `dst` to include
           # mount options.
-          type = with types; listOf string;
+          type = with types; listOf str;
           default = [];
           description = "List of volumes to attach to this container.";
           example = [
@@ -70,14 +81,14 @@ let
         };
 
         workdir = mkOption {
-          type = with types; nullOr string;
+          type = with types; nullOr str;
           default = null;
           description = "Override the default working directory for the container.";
           example = "/var/lib/hello_world";
         };
 
         extraDockerOptions = mkOption {
-          type = with types; listOf string;
+          type = with types; listOf str;
           default = [];
           description = "Extra options for `docker run`.";
           example = ["--network=host"];
@@ -94,6 +105,7 @@ let
         "exec ${pkgs.docker}/bin/docker run"
         "--rm"
         "--name=${containerName}"
+        "--log-driver=${container.log-driver}"
       ] ++ optional (! isNull container.entrypoint)
         "--entrypoint=${escapeShellArg container.entrypoint}"
         ++ (mapAttrsToList (k: v: "-e ${escapeShellArg k}=${escapeShellArg v}") container.environment)
