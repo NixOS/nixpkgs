e6549926a92c2a21b620444bfe4723622cb9ae91
James D. Trotter <james@simula.no>
2019-02-04 08:21:16 +0100

    scotch: Enable building on darwin
    Also, build libesmumps and libptesmumps to later support the mumps
    package.

--- pkgs/applications/science/math/scotch/default.nix
+++ pkgs/applications/science/math/scotch/default.nix
@@ -1,10 +1,11 @@
-{ stdenv, fetchurl, bison, openmpi, flex, zlib}:
+{ stdenv, fetchurl, bison, openmpi, flex, zlib, fixDarwinDylibNames }:
 
 stdenv.mkDerivation rec {
   version = "6.0.4";
   name = "scotch-${version}";
   src_name = "scotch_${version}";
 
+  nativeBuildInputs = stdenv.lib.optional stdenv.isDarwin fixDarwinDylibNames;
   buildInputs = [ bison openmpi flex zlib ];
 
   src = fetchurl {
@@ -15,22 +16,33 @@ stdenv.mkDerivation rec {
   sourceRoot = "${src_name}/src";
 
   preConfigure = ''
-    ln -s Make.inc/Makefile.inc.x86-64_pc_linux2 Makefile.inc
+    substituteInPlace Makefile \
+      --replace '-$(CP) -f ../include/*scotch*.h $(includedir)' '-$(CP) -f ../include/*.h $(includedir)' \
+      --replace '-$(CP) -f ../lib/*scotch*$(LIB) $(libdir)' '-$(CP) -f ../lib/*$(LIB) $(libdir)'
+
+    ln -s Make.inc/Makefile.inc.x86-64_pc_linux2.shlib Makefile.inc
+  '' + stdenv.lib.optionalString stdenv.isDarwin ''
+    substituteInPlace Makefile.inc \
+      --replace gcc mpicc \
+      --replace "-DSCOTCH_PTHREAD " "" \
+      --replace "-DCOMMON_PTHREAD" "-DCOMMON_PTHREAD -DCOMMON_PTHREAD_BARRIER" \
+      --replace "-lrt" "" \
+      --replace "-shared" "-dynamiclib -undefined dynamic_lookup" \
+      --replace ".so" ".dylib"
   '';
 
-  buildFlags = [ "scotch ptscotch" ];
-  installFlags = [ "prefix=\${out}" ];
+  buildFlags = [ "scotch ptscotch esmumps ptesmumps" ];
+  installFlags = [ "prefix=\${out} scotch ptscotch esmumps ptesmumps" ];
 
   meta = {
     description = "Graph and mesh/hypergraph partitioning, graph clustering, and sparse matrix ordering";
     longDescription = ''
-      Scotch is a software package for graph and mesh/hypergraph partitioning, graph clustering, 
+      Scotch is a software package for graph and mesh/hypergraph partitioning, graph clustering,
       and sparse matrix ordering.
     '';
     homepage = http://www.labri.fr/perso/pelegrin/scotch;
     license = stdenv.lib.licenses.cecill-c;
     maintainers = [ stdenv.lib.maintainers.bzizou ];
-    platforms = stdenv.lib.platforms.linux;
+    platforms = stdenv.lib.platforms.linux ++ stdenv.lib.platforms.darwin;
   };
 }
-
