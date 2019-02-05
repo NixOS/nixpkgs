400aa7b86a17a5685913caf3432983964da9aef5
Florian Klink <flokli@flokli.de>
2019-02-05 17:38:34 +0100

    minio: add test to nixos/tests/all-tests.nix

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -129,6 +129,7 @@ in
   matrix-synapse = handleTest ./matrix-synapse.nix {};
   memcached = handleTest ./memcached.nix {};
   mesos = handleTest ./mesos.nix {};
+  minio = handleTest ./minio.nix {};
   misc = handleTest ./misc.nix {};
   mongodb = handleTest ./mongodb.nix {};
   morty = handleTest ./morty.nix {};
