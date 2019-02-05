50087e01c43a8a5d97352a30fbcdfe8531b6c9a6
TG ⊗ Θ <*@tg-x.net>
2019-02-05 18:25:25 +0100

    idrisPackages.tparsec: 2018-11-09 -> 2018-12-21

--- pkgs/development/idris-modules/tparsec.nix
+++ pkgs/development/idris-modules/tparsec.nix
@@ -4,15 +4,15 @@
 }:
 build-idris-package  {
   name = "tparsec";
-  version = "2018-11-09";
+  version = "2018-12-21";
 
   ipkgName = "TParsec";
 
   src = fetchFromGitHub {
     owner = "gallais";
     repo = "idris-tparsec";
-    rev = "fc5bc1e0bf21a53ec854990ed799c4c73e304b06";
-    sha256 = "0ladks6x1qhs884w4rsxnzpq8dpijyqfqbvhk55kq10xh6w1smrz";
+    rev = "6fafcaa894def6f2af86bc799e507013b56e7741";
+    sha256 = "0alnw0hqjs200gvb5f58lb16rna48j1v6wnvq4q7zbw99dcxsxwn";
   };
 
   meta = {
