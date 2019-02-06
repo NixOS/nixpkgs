fb79b89c770af5ff634c67a9ce0204f7ac8faacb
Maximilian Bosch <maximilian@mbosch.me>
2019-02-06 14:58:49 +0100

    python3Packages.python-jenkins: 1.3.0 -> 1.4.0
    Updates to the most recent version of `python-jenkins`. It was
    originally broken during the auto-update in b4588c6a0f2aba6da8cf52c2aef52a4fda6bdb89.
    
    The tests could be run by using `unittest2` and some dependencies for
    the test framwork.

--- pkgs/development/python-modules/python-jenkins/default.nix
+++ pkgs/development/python-modules/python-jenkins/default.nix
@@ -1,4 +1,4 @@
-{ stdenv
+{ lib
 , buildPythonPackage
 , fetchPypi
 , python
@@ -11,28 +11,33 @@
 , testscenarios
 , testrepository
 , kerberos
+, requests
+, unittest2
+, requests-mock
 }:
 
 buildPythonPackage rec {
   pname = "python-jenkins";
-  version = "1.3.0";
+  version = "1.4.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b44b3c8e0dabed371a1a8a301cc8833c635625faf003fd68c176800c71a6597c";
+    sha256 = "1h14hfcwichmppbgxf1k8njw29hchpav1kj574b4lly3j0n2vnag";
   };
 
-  patchPhase = ''
-    sed -i 's@python@${python.interpreter}@' .testr.conf
+  buildInputs = [ mock ];
+  propagatedBuildInputs = [ pbr pyyaml six multi_key_dict requests ];
+
+  checkInputs = [ unittest2 testscenarios requests-mock ];
+  checkPhase = ''
+    unit2
   '';
 
-  buildInputs = [ mock ];
-  propagatedBuildInputs = [ pbr pyyaml six multi_key_dict testtools testscenarios testrepository kerberos ];
-
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Python bindings for the remote Jenkins API";
     homepage = https://pypi.python.org/pypi/python-jenkins;
     license = licenses.bsd3;
+    maintainers = with maintainers; [ ma27 ];
   };
 
 }
