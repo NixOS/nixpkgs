c479113212c6a0b55fd3e3a3d8db9a9c773751a6
R. RyanTM <ryantm-bot@ryantm.com>
2019-02-06 05:17:27 -0800

    cfr: 0.138 -> 0.139
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/cfr/versions

--- pkgs/development/tools/java/cfr/default.nix
+++ pkgs/development/tools/java/cfr/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "cfr-${version}";
-  version = "0.138";
+  version = "0.139";
 
   src = fetchurl {
     url = "http://www.benf.org/other/cfr/cfr_${version}.jar";
-    sha256 = "1v0agc3d26jvgxmskh2pl0sq0nr2czl7g0dckya4l6af4lxp9x7q";
+    sha256 = "0wiag1m0hqk697qhrm4c7srzy18ixkcj5dn911lxhf0nfq5q63nk";
   };
 
   buildInputs = [ makeWrapper ];
