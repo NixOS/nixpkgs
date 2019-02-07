96b66178f1f870aeec5227002cec7084a70730fd
Andrew Dunham <andrew@du.nham.ca>
2019-02-07 05:01:42 -0500

    m2crypto: fix build with libressl (#53537)

--- pkgs/development/python-modules/m2crypto/default.nix
+++ pkgs/development/python-modules/m2crypto/default.nix
@@ -1,4 +1,5 @@
 { stdenv
+, fetchpatch
 , buildPythonPackage
 , fetchPypi
 , swig2
@@ -16,6 +17,14 @@ buildPythonPackage rec {
     sha256 = "a1b2751cdadc6afac3df8a5799676b7b7c67a6ad144bb62d38563062e7cd3fc6";
   };
 
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/void-linux/void-packages/raw/7946d12eb3d815e5ecd4578f1a6133d948694370/srcpkgs/python-M2Crypto/patches/libressl.patch";
+      sha256 = "0z5qnkndg6ma5f5qqrid5m95i9kybsr000v3fdy1ab562kf65a27";
+    })
+  ];
+  patchFlags = "-p0";
+
   buildInputs = [ swig2 openssl ];
 
   propagatedBuildInputs = [ typing ];
@@ -30,6 +39,7 @@ buildPythonPackage rec {
     description = "A Python crypto and SSL toolkit";
     homepage = http://chandlerproject.org/Projects/MeTooCrypto;
     license = licenses.mit;
+    maintainers = with maintainers; [ andrew-d ];
   };
 
 }
