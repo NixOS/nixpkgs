850e93ef0219a45bb101345cf1b3b8bbd6255a9e
Benjamin Hipple <bhipple@protonmail.com>
2019-02-09 14:48:49 -0500

    pythonPackages.testpath: build from src instead of pre-built binary
    In older releases of testpath, pypi only had a pre-compiled binary wheel;
    however, newer releases have a tar.gz src file that we can use to build and test
    this ourselves.

--- pkgs/development/python-modules/testpath/default.nix
+++ pkgs/development/python-modules/testpath/default.nix
@@ -1,17 +1,21 @@
 { stdenv
+, lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
+, pathlib2
 }:
 
 buildPythonPackage rec {
   pname = "testpath";
   version = "0.4.2";
-  format = "wheel";
 
   src = fetchPypi {
-    inherit pname version format;
-    sha256 = "46c89ebb683f473ffe2aab0ed9f12581d4d078308a3cb3765d79c6b2317b0109";
+    inherit pname version;
+    sha256 = "b694b3d9288dbd81685c5d2e7140b81365d46c29f5db4bc659de5aa6b98780f8";
   };
+
+  propagatedBuildInputs = lib.optional (pythonOlder "3.4") pathlib2;
 
   meta = with stdenv.lib; {
     description = "Test utilities for code working with files and commands";
