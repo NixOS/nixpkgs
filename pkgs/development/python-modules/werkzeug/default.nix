15b3cfa5f51da52b6b73c66686cf600d8e4f292e
Corbin <cds@corbinsimpson.com>
2019-02-10 14:44:42 -0800

    pythonModules.werkzeug: Forward-port PyPy fix.
    Included a comment for the upstream commit where this was fixed
    recently:
    https://github.com/pallets/werkzeug/commit/1cfdcf9824cb20e362979e8f7734012926492165

--- pkgs/development/python-modules/werkzeug/default.nix
+++ pkgs/development/python-modules/werkzeug/default.nix
@@ -14,6 +14,13 @@ buildPythonPackage rec {
   propagatedBuildInputs = [ itsdangerous ];
   checkInputs = [ pytest requests glibcLocales hypothesis ];
 
+  # Hi! New version of Werkzeug? Please double-check that this commit is
+  # inclucded, and then remove the following patch.
+  # https://github.com/pallets/werkzeug/commit/1cfdcf9824cb20e362979e8f7734012926492165
+  patchPhase = ''
+    substituteInPlace "tests/test_serving.py" --replace "'python'" "sys.executable"
+  '';
+
   checkPhase = ''
     LC_ALL="en_US.UTF-8" py.test ${stdenv.lib.optionalString stdenv.isDarwin "-k 'not test_get_machine_id'"}
   '';
