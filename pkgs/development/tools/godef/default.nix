9bece0f4719ba4ee5d43d482b3276b1604a4cdeb
Roman Volosatovs <rvolosatovs@riseup.net>
2019-02-12 16:21:30 +0100

    godef: 1.0.0 -> 1.1.1

--- pkgs/development/tools/godef/default.nix
+++ pkgs/development/tools/godef/default.nix
@@ -2,16 +2,16 @@
 
 buildGoPackage rec {
   name = "godef-${version}";
-  version = "1.0.0";
-  rev = "7b4626be9fa8081987905fd4719d2f6628f9d8b5";
+  version = "1.1.1";
+  rev = "v${version}";
 
   goPackagePath = "github.com/rogpeppe/godef";
-  excludedPackages = "go/printer/testdata";
+  subPackages = [ "." ];
 
   src = fetchgit {
     inherit rev;
     url = "https://github.com/rogpeppe/godef";
-    sha256 = "0zhw4ba19hy0kv74c58ax759h8721khmwj04fak2y5800ymsgndg";
+    sha256 = "1bpzqnb9fsk1pjjap3gm94pqch1jz02rfah9hg8iqbfm0dzpy31b";
   };
 
   meta = {
