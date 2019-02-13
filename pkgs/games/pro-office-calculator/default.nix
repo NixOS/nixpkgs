e4239350df61939414e40da3d8044a7221f13d1c
Philipp Middendorf <middendorf@plapadoo.de>
2019-02-13 18:14:19 +0100

    pro-office-calculator: 1.0.6 -> 1.0.13

--- pkgs/games/pro-office-calculator/default.nix
+++ pkgs/games/pro-office-calculator/default.nix
@@ -1,22 +1,16 @@
 { stdenv, fetchFromGitHub, tinyxml-2, cmake, qtbase, qtmultimedia, fetchpatch }:
 stdenv.mkDerivation rec {
-  version = "1.0.6";
+  version = "1.0.13";
   name = "pro-office-calculator-${version}";
 
   src = fetchFromGitHub {
     owner  = "RobJinman";
     repo   = "pro_office_calc";
     rev    = "v${version}";
-    sha256 = "1irgch6cbc2f8il1zh8qf98m43h41hma80dxzz9c7xvbvl99lybd";
+    sha256 = "1v75cysargmp4fk7px5zgib1p6h5ya4w39rndbzk614fcnv0iipd";
   };
 
   buildInputs = [ qtbase qtmultimedia tinyxml-2 ];
-
-  # This fixes a bug resulting in "illegal instruction"
-  patches = [(fetchpatch {
-    url = https://github.com/RobJinman/pro_office_calc/commit/806180d69d4af6b3183873f471c57bfdaf529560.patch;
-    sha256 = "1rcdjy233yf3kv4v18c82jyg08dykj2qspvg08n5b3bir870sbxz";
-  })];
 
   nativeBuildInputs = [ cmake ];
 
