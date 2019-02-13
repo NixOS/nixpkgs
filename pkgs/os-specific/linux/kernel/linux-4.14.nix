01d8894c4d0f7fdd4f3534e6268b8e4c503d7258
Vladimír Čunát <vcunat@gmail.com>
2019-02-13 22:55:36 +0100

    Revert "linux: 4.14.98 -> 4.14.99"
    This reverts commit 0e1eb30a1d92d3ac0573fefd33a8f039b0e8e36b.
    It broke the installer tests, e.g. https://hydra.nixos.org/build/88704861

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.14.99";
+  version = "4.14.98";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "02pbi5jck6fp7y5lihn077i0j3hssxcrbsfbxlyp62xjsnp8rycg";
+    sha256 = "0pqc04ij6qdfhh3rpakas0qc0vqj8mm120z64q9v9vxin5qi20lg";
   };
 } // (args.argsOverride or {}))
