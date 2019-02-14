2eb9ded88659784c1ca451691816f90cc5a194bb
Frederik Rietdijk <fridh@fridh.nl>
2019-02-14 08:37:11 +0100

    python: azure-mgmt-resource: 0.20.1 -> 2.1.0

--- pkgs/development/python-modules/azure-mgmt-resource/default.nix
+++ pkgs/development/python-modules/azure-mgmt-resource/default.nix
@@ -7,13 +7,13 @@
 
 
 buildPythonPackage rec {
-  version = "0.20.1";
+  version = "2.1.0";
   pname = "azure-mgmt-resource";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "0slh9qfm5nfacrdm3lid0sr8kwqzgxvrwf27laf9v38kylkfqvml";
+    sha256 = "aef8573066026db04ed3e7c5e727904e42f6462b6421c2e8a3646e4c4f8128be";
   };
 
   preConfigure = ''
