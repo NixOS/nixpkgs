69807b07cc4557e981a943f78b7a41ac05534736
Frederik Rietdijk <fridh@fridh.nl>
2019-02-14 08:37:29 +0100

    python: restructuredtext_lint: 1.1.3 -> 1.2.2

--- pkgs/development/python-modules/restructuredtext_lint/default.nix
+++ pkgs/development/python-modules/restructuredtext_lint/default.nix
@@ -11,11 +11,11 @@
 
 buildPythonPackage rec {
   pname = "restructuredtext_lint";
-  version = "1.1.3";
+  version = "1.2.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "c48ca9a84c312b262809f041fe47dcfaedc9ee4879b3e1f9532f745c182b4037";
+    sha256 = "82880a8de8a41bfc84f533744091b1ead8e2ab9ad6c0a3f60f4750ef6c802350";
   };
 
   checkInputs = [ nose flake8 pyyaml testtools ];
