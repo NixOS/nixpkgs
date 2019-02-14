f64be2ef8748f086f82611069cacf6a45c589f42
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2019-02-14 14:58:00 +0100

    lektor: 2.3 -> 3.1.3

--- pkgs/development/python-modules/lektor/default.nix
+++ pkgs/development/python-modules/lektor/default.nix
@@ -1,4 +1,6 @@
 { stdenv
+, lib
+, isPy3k
 , buildPythonPackage
 , fetchFromGitHub
 , click
@@ -7,7 +9,9 @@
 , requests
 , mistune
 , inifile
+, enum34
 , Babel
+, functools32
 , jinja2
 , flask
 , pyopenssl
@@ -17,20 +21,20 @@
 
 buildPythonPackage rec {
   pname = "lektor";
-  version = "2.3";
+  version = "3.1.3";
 
   src = fetchFromGitHub {
     owner = "lektor";
     repo = "lektor";
-    rev = "${version}";
-    sha256 = "1n0ylh1sbpvi9li3g6a7j7m28njfibn10y6s2gayjxwm6fpphqxy";
+    rev = version;
+    sha256 = "16qw68rz5q77w84lwyhjpfd3bm4mfrhcjrnxwwnz3vmi610h68hx";
   };
 
   buildInputs = [ pkgs.glibcLocales ];
   propagatedBuildInputs = [
     click watchdog exifread requests mistune inifile Babel jinja2
     flask pyopenssl ndg-httpsclient
-  ];
+  ] ++ lib.optional (!isPy3k) [enum34 functools32];
 
   LC_ALL="en_US.UTF-8";
 
