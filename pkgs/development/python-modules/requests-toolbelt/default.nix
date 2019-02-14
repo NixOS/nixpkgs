b86de1abec8e44772ba9630d87ceb8f9706cc73d
Frederik Rietdijk <fridh@fridh.nl>
2019-02-14 08:37:29 +0100

    python: requests-toolbelt: 0.8.0 -> 0.9.1

--- pkgs/development/python-modules/requests-toolbelt/default.nix
+++ pkgs/development/python-modules/requests-toolbelt/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "requests-toolbelt";
-  version = "0.8.0";
+  version = "0.9.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f6a531936c6fa4c6cfce1b9c10d5c4f498d16528d2a54a22ca00011205a187b5";
+    sha256 = "968089d4584ad4ad7c171454f0a5c6dac23971e9472521ea3b6d49d610aa6fc0";
   };
 
   checkInputs = [ betamax mock pytest ];
