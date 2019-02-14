b93ccb71366c98d14f6c720cdfca6d5c8150118b
Matthieu Coudron <mattator@gmail.com>
2019-02-14 14:32:04 +0900

    lua: add isLua53

--- pkgs/top-level/lua-packages.nix
+++ pkgs/top-level/lua-packages.nix
@@ -87,7 +87,7 @@ with self; {
   inherit toLuaModule lua-setup-hook;
   inherit buildLuarocksPackage buildLuaApplication;
   inherit requiredLuaModules luaOlder luaAtLeast
-    isLua51 isLua52 isLuaJIT lua callPackage;
+    isLua51 isLua52 isLua53 isLuaJIT lua callPackage;
 
   # wraps programs in $out/bin with valid LUA_PATH/LUA_CPATH
   wrapLua = callPackage ../development/interpreters/lua-5/wrap-lua.nix {
