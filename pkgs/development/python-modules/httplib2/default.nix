fa2a085648d8478b816682c0b9c2b78587a5d5e0
Frederik Rietdijk <fridh@fridh.nl>
2019-02-14 08:37:17 +0100

    python: httplib2: 0.11.3 -> 0.12.0

--- pkgs/development/python-modules/httplib2/default.nix
+++ pkgs/development/python-modules/httplib2/default.nix
@@ -2,12 +2,15 @@
 
 buildPythonPackage rec {
   pname = "httplib2";
-  version = "0.11.3";
+  version = "0.12.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1pyb0hmc0j0kcy27yiw38gq9pk7f1fkny5k1vd13cdz6l3csw7g7";
+    sha256 = "f61fb838a94ce3b349aa32c92fd8430f7e3511afdb18bf9640d647e30c90a6d6";
   };
+
+  # Needs setting up
+  doCheck = false;
 
   meta = with lib; {
     homepage = http://code.google.com/p/httplib2;
