e7fa663de6de68a90db2839e07326b5a307b2fbd
Frederik Rietdijk <fridh@fridh.nl>
2019-02-14 08:37:19 +0100

    python: kombu: 4.2.2.post1 -> 4.3.0

--- pkgs/development/python-modules/kombu/default.nix
+++ pkgs/development/python-modules/kombu/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
     pname = "kombu";
-    version = "4.2.2.post1";
+    version = "4.3.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "3c9dca2338c5d893f30c151f5d29bfb81196748ab426d33c362ab51f1e8dbf78";
+    sha256 = "529df9e0ecc0bad9fc2b376c3ce4796c41b482cf697b78b71aea6ebe7ca353c8";
   };
 
   postPatch = ''
