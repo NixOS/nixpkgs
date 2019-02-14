c09d643fb239260b65696e4f923334206cca9c1f
Frederik Rietdijk <fridh@fridh.nl>
2019-02-14 08:37:21 +0100

    python: mpmath: 1.0.0 -> 1.1.0

--- pkgs/development/python-modules/mpmath/default.nix
+++ pkgs/development/python-modules/mpmath/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "mpmath";
-  version = "1.0.0";
+  version = "1.1.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "04d14803b6875fe6d69e6dccea87d5ae5599802e4b1df7997bddd2024001050c";
+    sha256 = "fc17abe05fbab3382b61a123c398508183406fa132e0223874578e20946499f6";
   };
 
   # error: invalid command 'test'
