338efb0a7da183f101a07cd3b827f60cb543f32f
Frederik Rietdijk <fridh@fridh.nl>
2019-02-14 08:37:10 +0100

    python: amqp: 2.3.2 -> 2.4.1

--- pkgs/development/python-modules/amqp/default.nix
+++ pkgs/development/python-modules/amqp/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "amqp";
-  version = "2.3.2";
+  version = "2.4.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "073dd02fdd73041bffc913b767866015147b61f2a9bc104daef172fc1a0066eb";
+    sha256 = "6816eed27521293ee03aa9ace300a07215b11fee4e845588a9b863a7ba30addb";
   };
 
   checkInputs = [ pytest case pytest-sugar ];
