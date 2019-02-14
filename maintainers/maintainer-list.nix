94c4df6f57e8a89b3e2890fb67b4235629b99108
Florian Franzen <Florian.Franzen@gmail.com>
2019-02-14 20:26:05 +0100

    maintainers: add FlorianFranzen (Florian Franzen)

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1535,6 +1535,11 @@
     github = "flokli";
     name = "Florian Klink";
   };
+  FlorianFranzen = {
+    email = "Florian.Franzen@gmail.com";
+    github = "FlorianFranzen";
+    name = "Florian Franzen";
+  };
   florianjacob = {
     email = "projects+nixos@florianjacob.de";
     github = "florianjacob";
