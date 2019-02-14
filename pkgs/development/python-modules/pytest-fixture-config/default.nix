23c34bee69232337d84ee2c9764ec4edb2cf1491
Frederik Rietdijk <fridh@fridh.nl>
2019-02-14 08:37:27 +0100

    python: pytest-fixture-config: 1.3.0 -> 1.4.0

--- pkgs/development/python-modules/pytest-fixture-config/default.nix
+++ pkgs/development/python-modules/pytest-fixture-config/default.nix
@@ -3,11 +3,11 @@
 
 buildPythonPackage rec {
   pname = "pytest-fixture-config";
-  version = "1.3.0";
+  version = "1.4.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1717cd7d2233943cae9af419c6e31dca5e40d5de01ef0bcfd5cd06f37548db08";
+    sha256 = "839d70343c87d6dda5bca88e3ab06e7b2027998dc1ec452c14d50be5725180a3";
   };
 
   nativeBuildInputs = [ setuptools-git ];
