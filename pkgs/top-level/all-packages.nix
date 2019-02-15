6e9074633490c3e8e288d73de1326f332e59344b
Tim Steinbach <tim@nequissimus.com>
2019-02-15 09:51:51 -0500

    linux: 4.14.99 -> 4.14.101
    (cherry picked from commit 7954ec0ffdff541fcc27d89ac9b722d0505931fd)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14104,7 +14104,6 @@ with pkgs;
         # when adding a new linux version
         kernelPatches.cpu-cgroup-v2."4.11"
         kernelPatches.modinst_arg_list_too_long
-        kernelPatches.interpreter-trunc
       ];
   };
 
