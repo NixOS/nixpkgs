cba2549e9867c40bdbf4d65f62306d32d5c8bc17
R. RyanTM <ryantm-bot@ryantm.com>
2019-02-15 14:37:21 -0800

    python37Packages.llfuse: 1.3.5 -> 1.3.6
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-llfuse/versions

--- pkgs/development/python-modules/llfuse/default.nix
+++ pkgs/development/python-modules/llfuse/default.nix
@@ -4,12 +4,12 @@
 
 buildPythonPackage rec {
   pname = "llfuse";
-  version = "1.3.5";
+  version = "1.3.6";
   name = pname + "-" + version;
 
   src = fetchurl {
     url = "mirror://pypi/l/llfuse/${name}.tar.bz2";
-    sha256 = "6e412a3d9be69162d49b8a4d6fb3c343d1c1fba847f4535d229e0ece2548ead8";
+    sha256 = "1j9fzxpgmb4rxxyl9jcf84zvznhgi3hnh4hg5vb0qaslxkvng8ii";
   };
 
   nativeBuildInputs = [ pkgconfig ];
