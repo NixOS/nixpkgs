0e8ab58f459b49e968ecbc312175a062af2776f9
worldofpeace <worldofpeace@users.noreply.github.com>
2019-02-15 18:20:19 -0500

    pythonPackages.llfuse: update homepage

--- pkgs/development/python-modules/llfuse/default.nix
+++ pkgs/development/python-modules/llfuse/default.nix
@@ -24,7 +24,7 @@ buildPythonPackage rec {
 
   meta = with stdenv.lib; {
     description = "Python bindings for the low-level FUSE API";
-    homepage = https://code.google.com/p/python-llfuse/;
+    homepage = https://github.com/python-llfuse/python-llfuse;
     license = licenses.lgpl2Plus;
     platforms = platforms.unix;
     maintainers = with maintainers; [ bjornfor ];
