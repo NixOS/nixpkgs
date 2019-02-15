acaa5c1f1aecfea0bc70fc42bf9950145bd7bfa5
Peter Simons <simons@cryp.to>
2019-02-15 15:03:05 +0100

    stylish-cabal compiles again. Yay!
    Thank you, @pikajude.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7024,7 +7024,7 @@ in
 
   stack = haskell.lib.justStaticExecutables haskellPackages.stack;
   hlint = haskell.lib.justStaticExecutables haskellPackages.hlint;
-  stylish-cabal = haskell.lib.justStaticExecutables haskell.packages.ghc844.stylish-cabal;
+  stylish-cabal = haskell.lib.justStaticExecutables haskellPackages.stylish-cabal;
 
   all-cabal-hashes = callPackage ../data/misc/hackage { };
 
