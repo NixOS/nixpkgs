fc6b829b757773d65404f459de51800b65b40307
Frederik Rietdijk <fridh@fridh.nl>
2019-02-15 10:03:03 +0100

    fixup Revert "python: pylint: 1.9.4 -> 2.2.2"
    This reverts commit b79170cc64e983db939f2c2efc0372ca2b690b07.

--- pkgs/development/python-modules/pylint/1.9.nix
+++ pkgs/development/python-modules/pylint/1.9.nix
@@ -4,11 +4,11 @@
 
 buildPythonPackage rec {
   pname = "pylint";
-  version = "2.2.2";
+  version = "1.9.4";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "689de29ae747642ab230c6d37be2b969bf75663176658851f456619aacf27492";
+    sha256 = "ee1e85575587c5b58ddafa25e1c1b01691ef172e139fc25585e5d3f02451da93";
   };
 
   checkInputs = [ pytest pytestrunner pyenchant ];
