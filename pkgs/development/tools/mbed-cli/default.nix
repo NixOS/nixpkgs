15b1356539cbbd4cdaa513e2529a94cba0b2fc15
Roman Volosatovs <rvolosatovs@riseup.net>
2019-02-16 21:06:03 +0100

    mbed-cli: 1.8.3 -> 1.9.1

--- pkgs/development/tools/mbed-cli/default.nix
+++ pkgs/development/tools/mbed-cli/default.nix
@@ -4,14 +4,14 @@ with python3Packages;
 
 buildPythonApplication rec {
   pname = "mbed-cli";
-  version = "1.8.3";
+  version = "1.9.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "04vn2v0d7y3vmm8cswzvn2z85balgp3095n5flvgf3r60fdlhlmp";
+    sha256 = "1228plh55id03qywsw0ai88ypdpbh9iz18jfcyhn21pci7mj77fv";
   };
 
-  doCheck = false; # no tests available in Pypi
+  doCheck = false; # Tests cannot import mbed.
 
   meta = with lib; {
     homepage = https://github.com/ARMmbed/mbed-cli;
