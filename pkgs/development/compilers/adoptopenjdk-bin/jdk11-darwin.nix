ef02c63ba159b1e7d65db192e4078a951d107107
Vladimír Čunát <vcunat@gmail.com>
2019-02-17 09:39:27 +0100

    Merge #54576: adoptopenjdk-bin: 11.0.1 -> 11.0.2
    Includes security fixes.
    
    (cherry picked from commit 94b518f855f42d4e44507380809cbd94c7a352ea)

--- pkgs/development/compilers/adoptopenjdk-bin/jdk11-darwin.nix
+++ pkgs/development/compilers/adoptopenjdk-bin/jdk11-darwin.nix
@@ -5,6 +5,5 @@ in
   jdk-hotspot = import ./jdk-darwin-base.nix sources.openjdk11.mac.jdk.hotspot;
   jre-hotspot = import ./jdk-darwin-base.nix sources.openjdk11.mac.jre.hotspot;
   jdk-openj9 = import ./jdk-darwin-base.nix sources.openjdk11.mac.jdk.openj9;
-  # openj9 jre builds are currently missing: https://github.com/AdoptOpenJDK/openjdk-build/issues/796
-  #jre-openj9 = import ./jdk-darwin-base.nix sources.openjdk11.mac.jre.openj9;
+  jre-openj9 = import ./jdk-darwin-base.nix sources.openjdk11.mac.jre.openj9;
 }
