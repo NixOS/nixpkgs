94452556cc6acbde329f99b214e0b256cb471792
Frederik Rietdijk <fridh@fridh.nl>
2019-02-17 12:51:12 +0100

    python.pkgs.altair: fix build

--- pkgs/development/python-modules/altair/default.nix
+++ pkgs/development/python-modules/altair/default.nix
@@ -12,16 +12,15 @@ buildPythonPackage rec {
     sha256 = "9f4bc7cd132c0005deb6b36c7041ee213a69bbdfcd8c0b1a9f1ae8c1fba733f6";
   };
 
+  postPatch = ''
+    # Tests require network
+    rm altair/examples/boxplot_max_min.py altair/examples/line_percent.py
+  '';
+
   checkInputs = [ pytest jinja2 sphinx vega_datasets ipython glibcLocales recommonmark ];
 
   propagatedBuildInputs = [ entrypoints jsonschema numpy pandas six toolz ]
     ++ stdenv.lib.optionals (pythonOlder "3.5") [ typing ];
-
-  # hack to prevent typing from being required for python > 3.5
-  postPatch = ''
-    substituteInPlace requirements.txt \
-       --replace "typing" ""
-  '';
 
   checkPhase = ''
     export LANG=en_US.UTF-8
