ba65001b7a79252f0a83e976b332beb7cb8c9fec
Florian Klink <flokli@flokli.de>
2019-02-17 13:42:35 +0100

    gitlab-workhorse: 8.0.1 -> 8.0.2
    (cherry picked from commit 75f66b19dd56650c8def74f8e98d96ef0353f51d)

--- pkgs/applications/version-management/gitlab/gitlab-workhorse/default.nix
+++ pkgs/applications/version-management/gitlab/gitlab-workhorse/default.nix
@@ -3,13 +3,13 @@
 stdenv.mkDerivation rec {
   name = "gitlab-workhorse-${version}";
 
-  version = "8.0.1";
+  version = "8.0.2";
 
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitlab-workhorse";
     rev = "v${version}";
-    sha256 = "1aslcadag1q2rdirf9m0dl5vfaz8v3yy1232mvyjyvy1wb51pf4q";
+    sha256 = "12xwr9yl59i58gnf0yn5yjp7zwz3s46042lk7rihvvzsa0kax690";
   };
 
   buildInputs = [ git go ];
