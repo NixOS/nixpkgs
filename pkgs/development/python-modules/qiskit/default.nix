8d89c741ffeb209a9820cd06bd8318876fe24003
pandaman64 <kointosudesuyo@infoseek.jp>
2019-02-18 10:23:24 +0900

    python: qiskit: 0.6.1 -> 0.7.3

--- pkgs/development/python-modules/qiskit/default.nix
+++ pkgs/development/python-modules/qiskit/default.nix
@@ -2,59 +2,36 @@
 , isPy3k
 , buildPythonPackage
 , fetchPypi
-, numpy
-, scipy
-, sympy
-, matplotlib
-, networkx
-, ply
-, pillow
-, cffi
-, requests
-, requests_ntlm
-, IBMQuantumExperience
-, jsonschema
-, psutil
-, cmake
-, llvmPackages 
+, qiskit-aer
+, qiskit-terra
+, python
 }:
 
 buildPythonPackage rec {
   pname = "qiskit";
-  version = "0.6.1";
+  version = "0.7.3";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "601e8db4db470593b94a32495c6e90e2db11a9382817a34584520573a7b8cc38";
+    sha256 = "63e7a7c3033fe955d715cc825b3fb61d27c25ad66e1761493ca2243b5dbfb4f9";
   };
 
-  buildInputs = [ cmake ]
-    ++ stdenv.lib.optional stdenv.isDarwin llvmPackages.openmp;
-
-  propagatedBuildInputs = [
-    numpy
-    matplotlib
-    networkx
-    ply
-    scipy
-    sympy
-    pillow
-    cffi
-    requests
-    requests_ntlm
-    IBMQuantumExperience
-    jsonschema
-    psutil
+  patches = [
+    # Qiskit overrides the installation to deal with misconfiguration of dependency,
+    # but we don't need it as Nix tracks the right dependency
+    ./no-override.patch
   ];
 
-  # Pypi's tarball doesn't contain tests
-  doCheck = false;
+  propagatedBuildInputs = [
+    qiskit-aer
+    qiskit-terra
+  ];
 
   meta = {
-    description = "Quantum Software Development Kit for writing quantum computing experiments, programs, and applications";
-    homepage    = https://github.com/QISKit/qiskit-terra;
+    description = "Software for developing quantum computing programs";
+    homepage    = https://github.com/Qiskit/qiskit;
     license     = stdenv.lib.licenses.asl20;
     maintainers = with stdenv.lib.maintainers; [
       pandaman
