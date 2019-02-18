c2a1d01a4b4b1a2aa6d6b6b35185ceecfd050d15
Dan Callahan <dan.callahan@gmail.com>
2019-02-18 21:58:30 +0000

    bolt: init at 0.7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14314,6 +14314,8 @@ in
 
   inherit (python3Packages) bedup;
 
+  bolt = callPackage ../os-specific/linux/bolt { };
+
   bridge-utils = callPackage ../os-specific/linux/bridge-utils { };
 
   busybox = callPackage ../os-specific/linux/busybox { };
