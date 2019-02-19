650a16d83fd4e67e70597a8060ef6e281e4895c7
José Romildo Malaquias <malaquias@gmail.com>
2019-02-19 19:15:20 -0300

    vivaldi: 2.3.1440.41-1 -> 2.3.1440.48-1
    (cherry picked from commit a1ba6ab9bdc26fbb28f8054c7b1b1d34ecf6296e)

--- pkgs/applications/networking/browsers/vivaldi/default.nix
+++ pkgs/applications/networking/browsers/vivaldi/default.nix
@@ -13,11 +13,11 @@
 stdenv.mkDerivation rec {
   name = "${product}-${version}";
   product = "vivaldi";
-  version = "2.3.1440.41-1";
+  version = "2.3.1440.48-1";
 
   src = fetchurl {
     url = "https://downloads.vivaldi.com/stable/${product}-stable_${version}_amd64.deb";
-    sha256 = "0wrq7c0sw1b41bshwgzji4pwl0raj0l5h2r7gkcg952rcn0wl9bs";
+    sha256 = "0zc9080np82qqqrfavg78vbjfl7w1b4p7f3s6h5wga9f6w1m078c";
   };
 
   unpackPhase = ''
