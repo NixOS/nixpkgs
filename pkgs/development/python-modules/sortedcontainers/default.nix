55a6f8be1535f187e2fb6fa585ae31c2102c6242
Philipp Middendorf <middendorf@plapadoo.de>
2019-02-19 08:36:22 +0100

    pythonPackages.sortedcontainers: 2.0.5 -> 2.1.0

--- pkgs/development/python-modules/sortedcontainers/default.nix
+++ pkgs/development/python-modules/sortedcontainers/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "sortedcontainers";
-  version = "2.0.5";
+  version = "2.1.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b74f2756fb5e23512572cc76f0fe0832fd86310f77dfee54335a35fb33f6b950";
+    sha256 = "0fm0w5id2yhqld95hg2m636vjgkz377rvgdfqaxc25vbylr9lklp";
   };
 
   # pypi tarball does not come with tests
