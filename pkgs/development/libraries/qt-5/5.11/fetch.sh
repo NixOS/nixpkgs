4b7d9dc86800fa7b861f47ae0e6e3a8f097f9ce7
nyanloutre <paul@nyanlout.re>
2019-02-21 16:27:09 +0100

    fetch-kde-qt.sh: get hashes from metadata

--- pkgs/development/libraries/qt-5/5.11/fetch.sh
+++ pkgs/development/libraries/qt-5/5.11/fetch.sh
@@ -1,2 +1,1 @@
-WGET_ARGS=( http://download.qt.io/official_releases/qt/5.11/5.11.3/submodules/ \
-            -A '*.tar.xz' )
+WGET_ARGS=( http://download.qt.io/official_releases/qt/5.11/5.11.3/submodules/ )
