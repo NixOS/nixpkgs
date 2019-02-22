e1895a52a6c2e69966a7a70e0e31c347355c27c1
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2019-02-22 10:05:40 +0100

    python: readthedocs-sphinx-ext: 0.5.16 -> 0.5.17

--- pkgs/development/python-modules/readthedocs-sphinx-ext/default.nix
+++ pkgs/development/python-modules/readthedocs-sphinx-ext/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "readthedocs-sphinx-ext";
-  version = "0.5.16";
+  version = "0.5.17";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "42b1c63d63dd483a188b541599bd08a540b2d08ec2b166660179618b6ccc3bb0";
+    sha256 = "21097cbb37c9a0590e2cb444b55bd87302fc8f69640fa2d4f2d113e98e9558ff";
   };
 
   propagatedBuildInputs = [ requests ];
@@ -24,13 +24,9 @@ buildPythonPackage rec {
     py.test
   '';
 
-  # https://github.com/rtfd/readthedocs-sphinx-ext/pull/60
-  doCheck = false;
-
   meta = with lib; {
     description = "Sphinx extension for Read the Docs overrides";
     homepage = http://github.com/rtfd/readthedocs-sphinx-ext;
-    # Not sure which one: https://github.com/rtfd/readthedocs-sphinx-ext/issues/59
-    license = licenses.bsdOriginal;
+    license = licenses.mit;
   };
 }
