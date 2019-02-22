3a1f39e1de3d003782cc40c13c29ccc73e23f550
Austin Seipp <aseipp@pobox.com>
2019-02-22 11:19:36 -0600

    kernel: enable pressure stall information on 4.20+
    Closes #56207.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/os-specific/linux/kernel/common-config.nix
+++ pkgs/os-specific/linux/kernel/common-config.nix
@@ -636,6 +636,8 @@ let
       MLX4_EN_VXLAN = whenOlder "4.8" yes;
       MLX5_CORE_EN       = option yes;
 
+      PSI = whenAtLeast "4.20" yes;
+
       MODVERSIONS        = whenOlder "4.9" yes;
       MOUSE_PS2_ELANTECH = yes; # Elantech PS/2 protocol extension
       MTRR_SANITIZER     = yes;
