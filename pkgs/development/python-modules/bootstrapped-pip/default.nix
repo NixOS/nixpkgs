8ce693e81b788508cf9bf3da7cf422704a84869d
Frederik Rietdijk <fridh@fridh.nl>
2019-02-23 15:37:47 +0100

    python.pkgs.bootstrapped-pip: pip 19.0.2 -> 19.0.3, wheel 0.33.0 -> 0.33.1

--- pkgs/development/python-modules/bootstrapped-pip/default.nix
+++ pkgs/development/python-modules/bootstrapped-pip/default.nix
@@ -3,9 +3,9 @@
 let
   wheel_source = fetchPypi {
     pname = "wheel";
-    version = "0.33.0";
+    version = "0.33.1";
     format = "wheel";
-    sha256 = "b79ffea026bc0dbd940868347ae9eee36789b6496b6623bd2dec7c7c540a8f99";
+    sha256 = "8eb4a788b3aec8abf5ff68d4165441bc57420c9f64ca5f471f58c3969fe08668";
   };
   setuptools_source = fetchPypi {
     pname = "setuptools";
@@ -16,13 +16,13 @@ let
 
 in stdenv.mkDerivation rec {
   pname = "pip";
-  version = "19.0.2";
+  version = "19.0.3";
   name = "${python.libPrefix}-bootstrapped-${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
     format = "wheel";
-    sha256 = "6a59f1083a63851aeef60c7d68b119b46af11d9d803ddc1cf927b58edcd0b312";
+    sha256 = "bd812612bbd8ba84159d9ddc0266b7fbce712fc9bc98c82dee5750546ec8ec64";
   };
 
   unpackPhase = ''
