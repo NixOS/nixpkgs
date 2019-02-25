4029ac9b11cb3153db3742fcc3b92155bfac1afe
Vincent Demeester <vincent@sbr.pm>
2019-02-25 16:47:41 +0100

    linuxkit: 2017-07-08 -> v0.6
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkgs/development/tools/misc/linuxkit/default.nix
+++ pkgs/development/tools/misc/linuxkit/default.nix
@@ -1,23 +1,25 @@
 { lib, buildGoPackage, fetchFromGitHub }:
 
 buildGoPackage rec {
-  name = "linuxkit-${version}";
-  version = "2017-07-08";
-  rev = "8ca19a84d5281b1b15c7a48c21e5786943b47f1c";
+  pname = "linuxkit";
+  version = "0.6";
+  rev = "10f07ca1624102de3c2d93da7792be5528c32022";
 
   goPackagePath = "github.com/linuxkit/linuxkit";
 
   src = fetchFromGitHub {
     owner = "linuxkit";
     repo = "linuxkit";
-    inherit rev;
-    sha256 = "150y7hnjhi81iik7np27y5466ldaackq72mpi7vmybbl7vr1wgw4";
+    rev = "v${version}";
+    sha256 = "12nph1sxgp7l2sb3ar7x8a2rrk2bqphca6snwbcqaqln2ixsh78i";
   };
 
   subPackages = [ "src/cmd/linuxkit" ];
 
-  preBuild = ''
-    buildFlagsArray+=("-ldflags" "-X main.GitCommit=${builtins.substring 0 7 rev} -X main.Version=0.0.0")
+  buildFlagsArray = let t = "${goPackagePath}/src/cmd/linuxkit/version"; in ''
+    -ldflags=
+      -s -X ${t}.GitCommit=${builtins.substring 0 7 rev}
+         -X ${t}.Version=${version}
   '';
 
   meta = {
@@ -25,5 +27,6 @@ buildGoPackage rec {
     license = lib.licenses.asl20;
     homepage = https://github.com/linuxkit/linuxkit;
     platforms = lib.platforms.unix;
+    maintainers = with lib.maintainers; [ vdemeester ];
   };
 }
