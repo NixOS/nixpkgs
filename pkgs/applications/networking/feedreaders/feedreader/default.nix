8bcb7879d0e1299726ff5aaf47dd3f62ac7c4850
worldofpeace <worldofpeace@users.noreply.github.com>
2019-02-24 22:49:53 -0500

    feedreader: 2.6.2 -> 2.8.2
    hicolor-icon-theme was added for the setup hook

--- pkgs/applications/networking/feedreaders/feedreader/default.nix
+++ pkgs/applications/networking/feedreaders/feedreader/default.nix
@@ -1,43 +1,33 @@
-{ stdenv, fetchFromGitHub, fetchpatch, meson, ninja, pkgconfig, vala_0_40, gettext, python3
-, appstream-glib, desktop-file-utils, glibcLocales, wrapGAppsHook
-, curl, glib, gnome3, gst_all_1, json-glib, libnotify, libsecret, sqlite, gumbo
+{ stdenv, fetchFromGitHub, meson, ninja, pkgconfig, vala, gettext, python3
+, appstream-glib, desktop-file-utils, wrapGAppsHook, curl, gnome3, gst_all_1
+, json-glib, libnotify, libsecret, sqlite, gumbo, glib, gtk3, libgee, libpeas
+, libsoup, librest, webkitgtk, gsettings-desktop-schemas, gdk_pixbuf, libxml2
+, hicolor-icon-theme
 }:
 
 stdenv.mkDerivation rec {
   pname = "feedreader";
-  version = "2.6.2";
+  version = "2.8.2";
 
   src = fetchFromGitHub {
     owner = "jangernert";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1x5milynfa27zyv2jkzyi7ikkszrvzki1hlzv8c2wvcmw60jqb8n";
+    sha256 = "1qm7scrz8xm68zizcfn13ll4ksdd004fahki7gbwqagsr1fg62y8";
   };
 
-  patches = [
-    # See: https://github.com/jangernert/FeedReader/pull/842
-    (fetchpatch {
-      url = "https://github.com/jangernert/FeedReader/commit/f4ce70932c4ddc91783309708402c7c42d627455.patch";
-      sha256 = "076fpjn973xg2m35lc6z4h7g5x8nb08sghg94glsqa8wh1ig2311";
-    })
-  ];
-
   nativeBuildInputs = [
-    meson ninja pkgconfig vala_0_40 gettext appstream-glib desktop-file-utils
-    python3 glibcLocales wrapGAppsHook
+    meson ninja pkgconfig vala gettext appstream-glib desktop-file-utils
+    libxml2 python3 wrapGAppsHook
   ];
 
   buildInputs = [
-    curl glib json-glib libnotify libsecret sqlite gumbo
-  ] ++ (with gnome3; [
-    gtk libgee libpeas libsoup rest webkitgtk gnome-online-accounts
-    gsettings-desktop-schemas
-  ]) ++ (with gst_all_1; [
+    curl glib json-glib libnotify libsecret sqlite gumbo gtk3
+    libgee libpeas libsoup librest webkitgtk gsettings-desktop-schemas
+    gdk_pixbuf gnome3.gnome-online-accounts hicolor-icon-theme
+  ] ++ (with gst_all_1; [
     gstreamer gst-plugins-base gst-plugins-good
   ]);
-
-  # vcs_tag function fails with UnicodeDecodeError
-  LC_ALL = "en_US.UTF-8";
 
   postPatch = ''
     patchShebangs meson_post_install.py
@@ -47,7 +37,7 @@ stdenv.mkDerivation rec {
     description = "A modern desktop application designed to complement existing web-based RSS accounts";
     homepage = https://jangernert.github.io/FeedReader/;
     license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ edwtjo ];
+    maintainers = with maintainers; [ edwtjo worldofpeace ];
     platforms = platforms.linux;
   };
 }
