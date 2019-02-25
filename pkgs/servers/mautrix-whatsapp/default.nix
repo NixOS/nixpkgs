43c1fced2394a122b75e8d6a3caabf46954fea77
Victor SENE <victor@sene.ovh>
2019-02-25 12:26:21 +0100

    mautrix-whatsapp: 2019-02-20 -> 2019-02-24

--- pkgs/servers/mautrix-whatsapp/default.nix
+++ pkgs/servers/mautrix-whatsapp/default.nix
@@ -2,15 +2,15 @@
 
 buildGoPackage rec {
   name = "mautrix-unstable-${version}";
-  version = "2019-02-20";
+  version = "2019-02-24";
 
   goPackagePath = "maunium.net/go/mautrix-whatsapp";
 
   src = fetchFromGitHub {
     owner = "tulir";
     repo = "mautrix-whatsapp";
-    rev = "c5aac5e770d40fe5d5549a2939b92f0c103d8165";
-    sha256 = "093n8x4c2pdj7idzdnssys7260v5fmay1y6r1l39q16k0qsfsl6r";
+    rev = "485acf6de654b8fb70007876c074fb004eb9717b";
+    sha256 = "1v7h3s8h0aiq6g06h9j1sidw8y5aiw24sgdh9knr1c90pvvc7pmv";
   };
 
   goDeps = ./deps.nix;
