2d0235b0a532e064ac8a8320bd09acef79b06079
Merlin Göttlinger <megoettlinger@gmail.com>
2019-02-26 12:39:44 +0100

    Only works on x86_64 linux

--- pkgs/applications/science/logic/clprover/clprover.nix
+++ pkgs/applications/science/logic/clprover/clprover.nix
@@ -22,5 +22,6 @@ stdenv.mkDerivation rec {
     homepage = http://cgi.csc.liv.ac.uk/~ullrich/CLProver++/;
     license = licenses.gpl3; # Note that while the website states that it is GPLv2 but the file in the zip as well as the comments in the source state it is GPLv3
     maintainers = with maintainers; [ mgttlinger ];
+    platforms = [ "x86_64-linux" ];
   };
 }
