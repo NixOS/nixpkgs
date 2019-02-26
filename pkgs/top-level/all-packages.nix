8e530a43e99122c148f9c3004c1fb4cba2c93baa
Spencer Baugh <sbaugh@catern.com>
2019-02-26 18:45:23 +0000

    xmobar: add to all-packages so it shows up in search
    People are confused when they "nix search" for xmobar (a standalone
    application which just happens to be written in Haskell) and don't
    find it; they assume xmobar is not actually packaged. They have to
    have special Nix knowledge to know that they should look in
    haskellPackages for xmobar.
    
    Let's just put xmobar in all-packages.nix so it shows up in search,
    and people can install it straightforwardly without knowing about
    haskellPackages.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20305,6 +20305,8 @@ in
 
   xkblayout-state = callPackage ../applications/misc/xkblayout-state { };
 
+  xmobar = haskellPackages.xmobar;
+
   xmonad-log = callPackage ../tools/misc/xmonad-log { };
 
   xmonad-with-packages = callPackage ../applications/window-managers/xmonad/wrapper.nix {
