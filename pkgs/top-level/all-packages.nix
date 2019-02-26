91baf80786f4889ed16ac767242b234f1bc3b1cc
Judson <nyarly@gmail.com>
2019-02-26 13:38:51 -0800

    cadre: init at 1.0.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8446,6 +8446,8 @@ in
 
   byacc = callPackage ../development/tools/parsing/byacc { };
 
+  cadre = callPackage ../development/tools/cadre { };
+
   casperjs = callPackage ../development/tools/casperjs {
     inherit (texFunctions) fontsConf;
   };
