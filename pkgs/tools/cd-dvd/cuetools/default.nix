3fb66709ff870986bf64ed7b643c7c9f1bc012e1
worldofpeace <worldofpeace@users.noreply.github.com>
2019-02-26 20:25:50 -0500

    cuetools: use fetchFromGitHub

--- pkgs/tools/cd-dvd/cuetools/default.nix
+++ pkgs/tools/cd-dvd/cuetools/default.nix
@@ -1,14 +1,16 @@
-{ stdenv, fetchurl, autoreconfHook
+{ stdenv, fetchFromGitHub, autoreconfHook
 , bison, flac, flex, id3v2, vorbis-tools
 }:
 
 stdenv.mkDerivation rec {
-  name = "cuetools-${version}";
+  pname = "cuetools";
   version = "1.4.1";
 
-  src = fetchurl {
-    url = "https://github.com/svend/cuetools/archive/${version}.tar.gz";
-    sha256 = "01xi3rvdmil9nawsha04iagjylqr1l9v9vlzk99scs8c207l58i4";
+  src = fetchFromGitHub {
+    owner = "svend";
+    repo = pname;
+    rev = version;
+    sha256 = "02ksv1ahf1v4cr2xbclsfv5x17m9ivzbssb5r8xjm97yh8a7spa3";
   };
 
   nativeBuildInputs = [ autoreconfHook ];
