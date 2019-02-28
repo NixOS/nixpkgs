1c350dc99ea5175f7435fd80af4e347cce22f6fc
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2019-02-28 11:59:17 +0100

    pythonPackages.tensorflow: 1.11 -> 1.13.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5064,8 +5064,8 @@ in {
     else callPackage ../development/python-modules/tensorflow/bin.nix rec {
       cudaSupport = pkgs.config.cudaSupport or false;
       inherit (pkgs.linuxPackages) nvidia_x11;
-      cudatoolkit = pkgs.cudatoolkit_9_0;
-      cudnn = pkgs.cudnn_cudatoolkit_9_0;
+      cudatoolkit = pkgs.cudatoolkit_10_0;
+      cudnn = pkgs.cudnn_cudatoolkit_10_0;
     });
 
   tensorflowWithoutCuda = self.tensorflow.override {
