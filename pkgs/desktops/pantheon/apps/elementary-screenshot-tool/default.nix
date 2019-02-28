96587cccb8aa7fa17b2d54b6ad0deca1f1313ca0
worldofpeace <worldofpeace@users.noreply.github.com>
2019-02-28 18:47:30 -0500

    pantheon.elementary-redacted-script: init at 2016-06-03
    (cherry picked from commit 86f4badc7d69fb1d7efd1b523e5102918ccf0a96)

--- pkgs/desktops/pantheon/apps/elementary-screenshot-tool/default.nix
+++ pkgs/desktops/pantheon/apps/elementary-screenshot-tool/default.nix
@@ -2,12 +2,6 @@
 , ninja, vala, python3, desktop-file-utils, gtk3, granite, libgee
 , libcanberra, gobject-introspection, elementary-icon-theme, wrapGAppsHook }:
 
-let
-
-  redacted-script = callPackage ./redacted-script.nix {};
-
-in
-
 stdenv.mkDerivation rec {
   pname = "screenshot-tool"; # This will be renamed to "screenshot" soon. See -> https://github.com/elementary/screenshot/pull/93
   version = "1.6.2";
@@ -50,11 +44,6 @@ stdenv.mkDerivation rec {
   postPatch = ''
     chmod +x meson/post_install.py
     patchShebangs meson/post_install.py
-  '';
-
-  postInstall = ''
-    mkdir -p $out/share/fonts/truetype
-    cp -rva ${redacted-script}/share/fonts/truetype/redacted-elementary $out/share/fonts/truetype
   '';
 
   meta = with stdenv.lib; {
