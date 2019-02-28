688cdce0f1494b170a7640015cb47f954471f0a5
Matthieu Coudron <mattator@gmail.com>
2019-02-28 15:57:15 +0900

    vimPlugins.vim-markdown-composer: init at 2018-11-05

--- pkgs/misc/vim-plugins/overrides.nix
+++ pkgs/misc/vim-plugins/overrides.nix
@@ -39,6 +39,30 @@ self: super: {
     src = skim.vim;
   };
 
+  vim-markdown-composer = let
+    vim-markdown-composer-bin = rustPlatform.buildRustPackage {
+      name = "vim-markdown-composer-vim";
+      src = super.vim-markdown-composer.src;
+
+      cargoSha256 = "0pjghdk6bfc32v6z6p7nyqmsk8vqzzk3xld6gk8j7m8i19wc0032";
+      buildInputs = stdenv.lib.optionals stdenv.isDarwin [ CoreServices ];
+
+      # FIXME: Use impure version of CoreFoundation because of missing symbols.
+      #   Undefined symbols for architecture x86_64: "_CFURLResourceIsReachable"
+      preConfigure = stdenv.lib.optionalString stdenv.isDarwin ''
+        export NIX_LDFLAGS="-F${CoreFoundation}/Library/Frameworks -framework CoreFoundation $NIX_LDFLAGS"
+      '';
+    };
+  in super.vim-markdown-composer.overrideAttrs(oa: {
+
+    propagatedBuildsInputs = (oa.propagatedBuildsInputs or []) ++ [ vim-markdown-composer-bin ];
+
+    postInstall = ''
+      mkdir -p $out/share/vim-plugins/vim-markdown-composer/target/release/markdown-composer
+      ln -s ${vim-markdown-composer-bin}/bin/markdown-composer $out/share/vim-plugins/vim-markdown-composer/target/release/markdown-composer
+      '';
+  });
+
   LanguageClient-neovim = let
     LanguageClient-neovim-src = fetchurl {
       url = "https://github.com/autozimu/LanguageClient-neovim/archive/0.1.140.tar.gz";
