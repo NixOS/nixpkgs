ef20994fd8f113a66751597e3ca69d5142c38ae3
Averell Dalton <averell+nixpkgs@rxd4.com>
2019-03-01 13:28:19 +0100

    python: tensorflow: remove python3.7 restriction
    (cherry picked from commit 9dd81f9389c886226796613794776b712c1255ad)

--- pkgs/development/python-modules/tensorflow/tf1.13.1-hashes.nix
+++ pkgs/development/python-modules/tensorflow/tf1.13.1-hashes.nix
@@ -11,6 +11,10 @@ linux_py_36_cpu = {
   url = "https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.13.1-cp36-cp36m-linux_x86_64.whl";
   sha256 = "087jwjby3bym09z55cjhc587aasf01y6l009p1q2vcpfq7s7ljmk";
 };
+linux_py_37_cpu = {
+  url = "https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.13.1-cp37-cp37m-linux_x86_64.whl";
+  sha256 = "0as68dp87lh7ffcccb149km6vws15ap04604irxwz35fq9h7grxg";
+};
 linux_py_27_gpu = {
   url = "https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.13.1-cp27-none-linux_x86_64.whl";
   sha256 = "0bf239f2bnsbqs3qh4xdql9pgbsm0zk7j8q1hg0wn0wrq440n0ds";
@@ -22,6 +26,10 @@ linux_py_35_gpu = {
 linux_py_36_gpu = {
   url = "https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.13.1-cp36-cp36m-linux_x86_64.whl";
   sha256 = "1xnbiz36z7nicqrv0cmymfnwb8mdz2hifcv71gh6gnyi1962f2d7";
+};
+linux_py_37_gpu = {
+  url = "https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.13.1-cp37-cp37m-linux_x86_64.whl";
+  sha256 = "10gcrmd9y5a89wpi4rpp9scc9l2krijv8yjp7iphlykmn54ps74k";
 };
 mac_py_2_cpu = {
   url = "https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.13.1-py2-none-any.whl";
