376e915a372e795ec6282c6c4e802624e12c8af9
R. RyanTM <ryantm-bot@ryantm.com>
2019-03-01 15:31:34 -0800

    chirp: 20190201 -> 20190206
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/chirp-daily/versions

--- pkgs/applications/radio/chirp/default.nix
+++ pkgs/applications/radio/chirp/default.nix
@@ -3,11 +3,11 @@
 
 stdenv.mkDerivation rec {
   pname = "chirp-daily";
-  version = "20190201";
+  version = "20190206";
 
   src = fetchurl {
     url = "https://trac.chirp.danplanet.com/chirp_daily/daily-${version}/${pname}-${version}.tar.gz";
-    sha256 = "1ag3qzdq39zhpagviq9gpwk4y3h11z0j40nccsnhlq8h8bxpvwlf";
+    sha256 = "16agynxjl2b6y9ly2fl1imgy51k4fqnzn1w6bah2gnmifssxfny3";
   };
 
   nativeBuildInputs = [ makeWrapper ];
