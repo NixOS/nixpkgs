6ebd6ad7aa13f60f2279fa9e85ea6c694c429f7a
Matti Kariluoma <matti@kariluo.ma>
2019-03-02 10:23:21 -0600

    add matti-kariluoma to maintainers list

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -2865,6 +2865,11 @@
     github = "matthiasbeyer";
     name = "Matthias Beyer";
   };
+  matti-kariluoma = {
+    email = "matti@kariluo.ma";
+    github = "matti-kariluoma";
+    name = "Matti Kariluoma";
+  };
   maurer = {
     email = "matthew.r.maurer+nix@gmail.com";
     github = "maurer";
