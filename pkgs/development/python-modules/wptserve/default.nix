3e603b18a5f7f77f965b4f8532df234aa9f187a6
Michael Raskin <7c6f434c@mail.ru>
2019-03-02 21:33:52 +0100

    pythonPackages.wptserve: fix build by adding a local h2 3.0.1 instance

--- pkgs/development/python-modules/wptserve/default.nix
+++ pkgs/development/python-modules/wptserve/default.nix
@@ -5,6 +5,19 @@
 , isPy3k
 }:
 
+let
+  _h2 = h2;
+in let
+  h2 = _h2.overrideAttrs (x: {
+    version = "3.0.1";
+    src = fetchPypi {
+      pname = "h2";
+      version = "3.0.1";
+      sha256 = "0r3f43r0v7sqgdjjg5ngw0dndk2v6cyd0jncpwya54m37y42z5mj";
+    };
+  });
+in
+
 buildPythonPackage rec {
   pname = "wptserve";
   version = "2.0";
