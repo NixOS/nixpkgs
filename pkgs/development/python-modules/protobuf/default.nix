NixOS/nixpkgs

commit 5c7531872fb55f42b03df9929f60cfb410cf7cc5

python.pkgs.protobuf: fix build

pkgs/development/python-modules/protobuf/default.nix
@@ -13,8 +13,9 @@ buildPythonPackage rec {
     optional stdenv.isDarwin "-I${libcxx}/include/c++/v1"
     ++ optional (versionOlder protobuf.version "2.7.0") "-std=c++98";
 
-  propagatedBuildInputs = [ protobuf google_apputils ];
-  buildInputs = [ google_apputils pyext ];
+  propagatedBuildInputs = [ google_apputils ];
+  nativeBuildInputs = [ google_apputils pyext protobuf ];
+  buildInputs = [ protobuf ];
 
   patches = optional isPy37
     # Python 3.7 compatibility (remove when protobuf 3.7 is released)

commit e53b56a036f3e2ccd515033d09c2307b343290cd

pythonPackages.protobuf: propagate native pkgs.protobuf input
Python packages depending on the protobuf library often use the "protoc"
tool from pkgs.protobuf to codegen at build time.

This fixes a few regressions from 5c7531872fb55f42b03df9929f60cfb410cf7cc5

pkgs/development/python-modules/protobuf/default.nix
@@ -14,7 +14,8 @@ buildPythonPackage rec {
     ++ optional (versionOlder protobuf.version "2.7.0") "-std=c++98";
 
   propagatedBuildInputs = [ google_apputils ];
-  nativeBuildInputs = [ google_apputils pyext protobuf ];
+  propagatedNativeBuildInputs = [ protobuf ];  # For protoc.
+  nativeBuildInputs = [ google_apputils pyext ];
   buildInputs = [ protobuf ];
 
   patches = optional isPy37