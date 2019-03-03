678d98dd1704d0649cf353ebb17bd30e7636d923
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-03 19:13:49 +0100

    gnome3.evolution: 3.30.5 -> 3.32.0

--- pkgs/desktops/gnome-3/apps/evolution/default.nix
+++ pkgs/desktops/gnome-3/apps/evolution/default.nix
@@ -7,13 +7,13 @@
 , libcanberra-gtk3, bogofilter, gst_all_1, procps, p11-kit, openldap }:
 
 let
-  version = "3.30.5";
+  version = "3.32.0";
 in stdenv.mkDerivation rec {
   name = "evolution-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/evolution/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "1hhxj3rh921pp3l3c5k33bdypcas1p66krzs65k1qn82c5fpgl2h";
+    sha256 = "1skwhg3fbyg3acivknnpm7hs5xy0zwlvnyf5cxs59kxh1l41xmnc";
   };
 
   propagatedUserEnvPkgs = [ evolution-data-server ];
