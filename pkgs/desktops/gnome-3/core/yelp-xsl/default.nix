3c271cc2ee250f200a3d01d31c3363fde6258918
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-03 19:33:30 +0100

    gnome3.yelp-xsl: 3.30.1 -> 3.32.0

--- pkgs/desktops/gnome-3/core/yelp-xsl/default.nix
+++ pkgs/desktops/gnome-3/core/yelp-xsl/default.nix
@@ -3,11 +3,11 @@
 
 stdenv.mkDerivation rec {
   name = "yelp-xsl-${version}";
-  version = "3.30.1";
+  version = "3.32.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/yelp-xsl/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "0ffgp3ymcc11r9sdndliwwngljcy1mfqpfxsdfbm8rlcjg2k3vzw";
+    sha256 = "0rmsi2jxa8yl884k5w9i1sjd5ddjql6n58cwi1sq6mw879qny6cv";
   };
 
   passthru = {
