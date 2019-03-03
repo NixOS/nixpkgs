c17bf3ac5205256b24b031f19c1cf2f4717b14ac
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-03 17:33:22 +0100

    glib: 2.58.2 -> 2.60.0
    - The gettext dir has moved for some reason
    - `install_dir : bin` fix is now upstream

--- pkgs/development/libraries/glib/default.nix
+++ pkgs/development/libraries/glib/default.nix
@@ -46,7 +46,7 @@ let
   '';
 
   binPrograms = optional (!stdenv.isDarwin) "gapplication" ++ [ "gdbus" "gio" "gsettings" ];
-  version = "2.58.2";
+  version = "2.60.0";
 in
 
 stdenv.mkDerivation rec {
@@ -54,7 +54,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/glib/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "0jrxfm4gn1qz3y1450z709v74ys2bkjr8yffkgy106kgagb4xcn7";
+    sha256 = "0ls3njqknb345ni5i8hn9nr1n70kn6s8bi0g6kcqj3c4js5mv1i0";
   };
 
   patches = optional stdenv.isDarwin ./darwin-compilation.patch
@@ -66,26 +66,6 @@ stdenv.mkDerivation rec {
       ./schema-override-variable.patch
       # Require substituteInPlace in postPatch
       ./fix-gio-launch-desktop-path.patch
-      # https://gitlab.gnome.org/GNOME/glib/issues/1626
-      # https://gitlab.gnome.org/GNOME/glib/merge_requests/557
-      (fetchpatch {
-        url = https://gitlab.gnome.org/GNOME/glib/commit/85c4031696add9797e2334ced20678edcd96c869.patch;
-        sha256 = "1hmyvhx89wip2a26gk1rvd87k0pjfia51s0ysybjyzf5f1pzw877";
-      })
-      # https://gitlab.gnome.org/GNOME/glib/issues/1645
-      (fetchpatch {
-        url = https://gitlab.gnome.org/GNOME/glib/commit/e695ca9f310c393d8f39694f77471dbcb06daa9e.diff;
-        sha256 = "1jkb2bdnni0xdyn86xrx9z0fdwxrm7y08lagz8x5x01wglkwa26w";
-      })
-      # https://gitlab.gnome.org/GNOME/glib/issues/1643
-      (fetchpatch {
-        url = https://gitlab.gnome.org/GNOME/glib/commit/c792e5adaa8ae3a45e6ff3ff71168ad8d040a0d4.patch;
-        sha256 = "022x70qfn5wlv5gz3nlg0bwiwjxcd7l11j3qvbms2y8d1ffh5rfd";
-      })
-      (fetchpatch {
-        url = https://gitlab.gnome.org/GNOME/glib/commit/30ccbc386026cecac6ef3a77d8fa4f3c24ac68d7.patch;
-        sha256 = "04y3pxgzlx92cppwibx4rlsyvwxb37aq52x2lr6ajfgykv2nzpr3";
-      })
     ];
 
   outputs = [ "bin" "out" "dev" "devdoc" ];
@@ -120,11 +100,10 @@ stdenv.mkDerivation rec {
 
   LC_ALL = "en_US.UTF-8";
 
-  NIX_CFLAGS_COMPILE = optional stdenv.isSunOS "-DBSD_COMP";
+  NIX_CFLAGS_COMPILE = (optional stdenv.isSunOS "-DBSD_COMP")
+    ++ [ "-Wno-error=nonnull" ];
 
   postPatch = ''
-    substituteInPlace meson.build --replace "install_dir : 'bin'," "install_dir : glib_bindir,"
-
     # substitute fix-gio-launch-desktop-path.patch
     substituteInPlace gio/gdesktopappinfo.c --replace "@bindir@" "$out/bin"
 
@@ -154,8 +133,9 @@ stdenv.mkDerivation rec {
 
   '' + ''
     moveToOutput "share/glib-2.0" "$dev"
+    moveToOutput "share/gettext" "$dev"
     substituteInPlace "$dev/bin/gdbus-codegen" --replace "$out" "$dev"
-    sed -i "$dev/bin/glib-gettextize" -e "s|^gettext_dir=.*|gettext_dir=$dev/share/glib-2.0/gettext|"
+    sed -i "$dev/bin/glib-gettextize" -e "s|^gettext_dir=.*|gettext_dir=$dev/share/gettext|"
 
     # This file is *included* in gtk3 and would introduce runtime reference via __FILE__.
     sed '1i#line 1 "${name}/include/glib-2.0/gobject/gobjectnotifyqueue.c"' \
