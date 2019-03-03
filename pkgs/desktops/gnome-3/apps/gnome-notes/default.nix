e1c6d9da2c66b73d3c8732d35a084a2f0771daba
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-03 19:53:52 +0100

    gnome3.gnome-notes: 3.30.3 -> 3.31.92

--- pkgs/desktops/gnome-3/apps/gnome-notes/default.nix
+++ pkgs/desktops/gnome-3/apps/gnome-notes/default.nix
@@ -5,13 +5,13 @@
 , gnome3, libxml2 }:
 
 let
-  version = "3.30.3";
+  version = "3.31.92";
 in stdenv.mkDerivation rec {
   name = "gnome-notes-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/bijiben/${stdenv.lib.versions.majorMinor version}/bijiben-${version}.tar.xz";
-    sha256 = "1mkpi2i9nqpip5l15ihjcscyiri113s0705sjgh6b89164ahyn5k";
+    sha256 = "00xgfsphznisi93bjxxl914wfl3xf14zg0vjnhl93nsqz511byg1";
   };
 
   doCheck = true;
