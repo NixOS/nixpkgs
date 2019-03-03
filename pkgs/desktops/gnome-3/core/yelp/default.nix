68b2f2227861d0c4edd2abe17a0655e97af86bef
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-03 19:33:59 +0100

    yelp: 3.30.0 -> 3.32.0

--- pkgs/desktops/gnome-3/core/yelp/default.nix
+++ pkgs/desktops/gnome-3/core/yelp/default.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   name = "yelp-${version}";
-  version = "3.30.0";
+  version = "3.32.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/yelp/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "060a902j15k76fyhk8xfl38ipvrrcc0qd7nm2mcck4ifb45b0zv4";
+    sha256 = "090klk2mhd87y5w228gd1ia1lvvxaj913lkvxzcb1apz8n0i8mm7";
   };
 
   nativeBuildInputs = [ pkgconfig intltool itstool wrapGAppsHook ];
