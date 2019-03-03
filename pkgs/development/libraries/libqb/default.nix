2c8c1f7f37f2f64bded51070820e73aadd45c88a
Will Dietz <w@wdtz.org>
2019-03-02 18:15:06 -0600

    libqb: 0.17.2 -> 1.0.3
    * build docs
    * multiple outputs
    * upstream patch for build fix
    * triton preFixup

--- pkgs/development/libraries/libqb/default.nix
+++ pkgs/development/libraries/libqb/default.nix
@@ -1,14 +1,30 @@
-{ stdenv, fetchurl, pkgconfig }:
+{ stdenv, fetchurl, fetchpatch, pkgconfig, autoreconfHook, doxygen }:
 
 stdenv.mkDerivation rec{
-  name = "libqb-0.17.2";
+  pname = "libqb";
+  version = "1.0.3";
 
   src = fetchurl {
-    url = "https://fedorahosted.org/releases/q/u/quarterback/${name}.tar.xz";
-    sha256 = "1zpl45p3n6dn1jgbsrrmccrmv2mvp8aqmnl0qxfjf7ymkrj9qhcs";
+    url = "https://github.com/ClusterLabs/libqb/releases/download/v${version}/${pname}-${version}.tar.xz";
+    sha256 = "1xzlx9agx7fir151ni3lwsjp6aicjpqd80xw1fhykgx49dbahax0";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig doxygen ];
+
+  outputs = [ "out" "dev" "doc" "man" ];
+
+  patches = [
+    (fetchurl {
+      url = "https://github.com/ClusterLabs/libqb/commit/6d62b64752c2a94acc3974be4b2528f4d05363cf.patch";
+      sha256 = "178ygfq6v4qla1b39i5q44083d6jbiihcr6b29dir78kysv426bz";
+    })
+  ];
+
+  # Purity fix
+  preFixup = ''
+    grep -q "$TMPDIR" "$out"/lib/libqb.la
+    sed -i "s,\(inherited_linker_flags='\).*,\1'," "$out"/lib/libqb.la
+  '';
 
   meta = with stdenv.lib; {
     homepage = https://github.com/clusterlabs/libqb;
