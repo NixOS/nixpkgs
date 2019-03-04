456f09d76fcae80d4dc75f561981c2c266139241
Peter Simons <simons@cryp.to>
2019-03-04 16:26:46 +0100

    ghc: Drop outdated compiler versions 8.6.1 and 8.6.2. We have 8.6.3.

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -59,18 +59,6 @@ in {
       buildLlvmPackages = buildPackages.llvmPackages_5;
       llvmPackages = pkgs.llvmPackages_5;
     };
-    ghc861 = callPackage ../development/compilers/ghc/8.6.1.nix {
-      bootPkgs = packages.ghc822Binary;
-      inherit (buildPackages.python3Packages) sphinx;
-      buildLlvmPackages = buildPackages.llvmPackages_6;
-      llvmPackages = pkgs.llvmPackages_6;
-    };
-    ghc862 = callPackage ../development/compilers/ghc/8.6.2.nix {
-      bootPkgs = packages.ghc822Binary;
-      inherit (buildPackages.python3Packages) sphinx;
-      buildLlvmPackages = buildPackages.llvmPackages_6;
-      llvmPackages = pkgs.llvmPackages_6;
-    };
     ghc863 = callPackage ../development/compilers/ghc/8.6.3.nix {
       bootPkgs = packages.ghc822Binary;
       inherit (buildPackages.python3Packages) sphinx;
@@ -135,16 +123,6 @@ in {
       buildHaskellPackages = bh.packages.ghc844;
       ghc = bh.compiler.ghc844;
       compilerConfig = callPackage ../development/haskell-modules/configuration-ghc-8.4.x.nix { };
-    };
-    ghc861 = callPackage ../development/haskell-modules {
-      buildHaskellPackages = bh.packages.ghc861;
-      ghc = bh.compiler.ghc861;
-      compilerConfig = callPackage ../development/haskell-modules/configuration-ghc-8.6.x.nix { };
-    };
-    ghc862 = callPackage ../development/haskell-modules {
-      buildHaskellPackages = bh.packages.ghc862;
-      ghc = bh.compiler.ghc862;
-      compilerConfig = callPackage ../development/haskell-modules/configuration-ghc-8.6.x.nix { };
     };
     ghc863 = callPackage ../development/haskell-modules {
       buildHaskellPackages = bh.packages.ghc863;
