8c8c3ab85d12b9e04d22b966f95028ba59867242
Andreas Rammhold <andreas@rammhold.de>
2019-03-05 00:19:37 +0100

    libtorrent: apply openssl 1.1 patch

--- pkgs/tools/networking/p2p/libtorrent/default.nix
+++ pkgs/tools/networking/p2p/libtorrent/default.nix
@@ -1,6 +1,6 @@
 # NOTE: this is rakshava's version of libtorrent, used mainly by rtorrent
 # This is NOT libtorrent-rasterbar, used by Deluge, qbitttorent, and others
-{ stdenv, fetchFromGitHub, pkgconfig
+{ stdenv, fetchFromGitHub, fetchpatch, pkgconfig
 , libtool, autoconf, automake, cppunit
 , openssl, libsigcxx, zlib }:
 
@@ -14,6 +14,14 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     sha256 = "1h5y6ab3gs20yyprdfwcw8fh1c6czs4yrdj0kf54d2vp9qwz685r";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "openssl_1_1.patch";
+      url = https://github.com/rakshasa/libtorrent/commit/7b29b6bd2547e72e22b9b7981df27092842d2a10.patch;
+      sha256 = "0nrh2f9bvi1p0gnsck2gzkgd5dd91yk26gcrhrj2f7wh1d4m0pdd";
+    })
+  ];
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ libtool autoconf automake cppunit openssl libsigcxx zlib ];
