529bffccd86e03836bbb8af69d40551a1b5dc357
Sam Hug <s@m-h.ug>
2019-03-05 12:13:05 -0800

    pythonPackages.ppft: use custom checkPhase

--- pkgs/development/python-modules/ppft/default.nix
+++ pkgs/development/python-modules/ppft/default.nix
@@ -13,6 +13,10 @@ buildPythonPackage rec {
     sha256 = "5537b00afb7b247da0f59cc57ee5680178be61c8b2e21b5a0672b70a3d247791";
   };
 
+  checkPhase = ''
+    python -m ppft.tests
+  '';
+
   propagatedBuildInputs = [ six ];
 
   meta = with stdenv.lib; {
