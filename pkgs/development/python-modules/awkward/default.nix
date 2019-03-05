589d3972aa3bfc7fbd537235a75fe2f619ab9606
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2019-03-05 12:57:28 +0100

    python.pkgs.awkward: fix tests
    pytest.importorskip() doesn't work when we don't use pytest to run tests
    
    (cherry picked from commit 654e6ecc680816149c4928147d15451adbcffe06)

--- pkgs/development/python-modules/awkward/default.nix
+++ pkgs/development/python-modules/awkward/default.nix
@@ -16,12 +16,16 @@ buildPythonPackage rec {
     sha256 = "7016dc02d15b8797b59a461ccc8d218f37c335b97fa6b376638c0edd4ffc9de2";
   };
 
-  buildInputs = [ pytestrunner h5py ];
-  checkInputs = [ pytest ];
+  nativeBuildInputs = [ pytestrunner ];
+  checkInputs = [ pytest h5py ];
   propagatedBuildInputs = [ numpy ];
 
+  checkPhase = ''
+    py.test
+  '';
+
   meta = with lib; {
-    description = "Manipulate jagged, chunky, and/or bitmasked arrays as easily as Numpy.";
+    description = "Manipulate jagged, chunky, and/or bitmasked arrays as easily as Numpy";
     homepage = https://github.com/scikit-hep/awkward-array;
     license = licenses.bsd3;
     maintainers = [ maintainers.costrouc ];
