f271f943e0deb02f9d28c0d6fdd8411360801607
Kjuvi <quentin.vaucher@protonmail.com>
2019-03-05 22:57:51 +0100

    spice-up: 1.7.0 -> 1.8.0

--- pkgs/applications/office/spice-up/default.nix
+++ pkgs/applications/office/spice-up/default.nix
@@ -1,38 +1,35 @@
 { stdenv
 , fetchFromGitHub
-, gettext
-, libxml2
-, pkgconfig
-, gtk3
-, gnome3
-, gobject-introspection
-, json-glib
 , cmake
+, gdk_pixbuf
+, gtk3
+, gettext
 , ninja
+, pantheon
+, pkgconfig
+, json-glib
 , libgudev
 , libevdev
+, libgee
 , libsoup
-, pantheon
 , wrapGAppsHook }:
 
 stdenv.mkDerivation rec {
-  name = "spice-up-${version}";
-  version = "1.7.0";
+  pname = "spice-up";
+  version = "1.8.0";
 
   src = fetchFromGitHub {
     owner = "Philip-Scott";
     repo = "Spice-up";
     rev = version;
-    sha256 = "1qb1hlw7g581dmgg5mh832ixjkcgqm3lqzj6xma2cz8wdncwwjaq";
+    sha256 = "0jbqgf936pqss8ha27fcyjbhvkn4ij96b3d87c6gcx90glmq33zb";
   };
 
-  USER = "nix-build-user";
+  USER = "pbuilder";
 
   nativeBuildInputs = [
     cmake
     gettext
-    gobject-introspection # For setup hook
-    libxml2
     ninja
     pkgconfig
     pantheon.vala
@@ -41,18 +38,19 @@ stdenv.mkDerivation rec {
   buildInputs = [
     pantheon.elementary-icon-theme
     pantheon.granite
-    gnome3.libgee
+    gdk_pixbuf
     gtk3
     json-glib
     libevdev
+    libgee
     libgudev
     libsoup
   ];
 
   meta = with stdenv.lib; {
-    description = "Create simple and beautiful presentations on the Linux desktop";
+    description = "Create simple and beautiful presentations";
     homepage = https://github.com/Philip-Scott/Spice-up;
-    maintainers = with maintainers; [ samdroid-apps ];
+    maintainers = with maintainers; [ samdroid-apps kjuvi ] ++ pantheon.maintainers;
     platforms = platforms.linux;
     # The COPYING file has GPLv3; some files have GPLv2+ and some have GPLv3+
     license = licenses.gpl3Plus;
