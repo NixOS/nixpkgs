b1f767ef90233fb2157113655a1180e090efdb65
zimbatm <zimbatm@zimbatm.com>
2019-03-06 17:56:44 +0000

    pipenv: fix python loading issue (#56495)

--- pkgs/development/tools/pipenv/default.nix
+++ pkgs/development/tools/pipenv/default.nix
@@ -1,24 +1,35 @@
 { stdenv, python3Packages, pew }:
-with python3Packages; buildPythonApplication rec {
-    name = "${pname}-${version}";
-    pname = "pipenv";
-    version = "2018.11.26";
+with python3Packages;
+buildPythonApplication rec {
+  pname = "pipenv";
+  version = "2018.11.26";
 
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "0ip8zsrwmhrankrix0shig9g8q2knmr7b63sh7lqa8a5x03fcwx6";
-    };
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "0ip8zsrwmhrankrix0shig9g8q2knmr7b63sh7lqa8a5x03fcwx6";
+  };
 
-    LC_ALL = "en_US.UTF-8";
+  LC_ALL = "en_US.UTF-8";
 
-    propagatedBuildInputs = [ pew pip requests flake8 parver invoke ];
+  propagatedBuildInputs = [
+    flake8
+    invoke
+    parver
+    pew
+    pip
+    requests
+  ];
 
-    doCheck = false;
+  doCheck = false;
 
-    meta = with stdenv.lib; {
-      description = "Python Development Workflow for Humans";
-      license = licenses.mit;
-      platforms = platforms.all;
-      maintainers = with maintainers; [ berdario ];
-    };
-  }
+  makeWrapperArgs = [
+    "--set PYTHONPATH \"$PYTHONPATH\""
+  ];
+
+  meta = with stdenv.lib; {
+    description = "Python Development Workflow for Humans";
+    license = licenses.mit;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ berdario ];
+  };
+}
