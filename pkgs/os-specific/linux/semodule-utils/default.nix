595c586ac35a059d2db6def90d3500bd57c317eb
Alexander Kahl <ak@sodosopa.io>
2019-03-06 16:01:35 +0100

    semodule-utils: 2.7 -> 2.8

--- pkgs/os-specific/linux/semodule-utils/default.nix
+++ pkgs/os-specific/linux/semodule-utils/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "semodule-utils-${version}";
-  version = "2.7";
+  version = "2.8";
 
   inherit (libsepol) se_release se_url;
 
   src = fetchurl {
     url = "${se_url}/${se_release}/${name}.tar.gz";
-    sha256 = "1fl60x4w8rn5bcwy68sy48aydwsn1a17d48slni4sfx4c8rqpjch";
+    sha256 = "1pya3i6ggpl9hzfjhy74n4c91yqyzr6spkj3n5078qqc0w9rrxa4";
   };
 
   buildInputs = [ libsepol ];
@@ -21,7 +21,6 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     description = "SELinux policy core utilities (packaging additions)";
     license = licenses.gpl2;
-    inherit (libsepol.meta) homepage platforms;
-    maintainers = [ maintainers.e-user ];
+    inherit (libsepol.meta) homepage platforms maintainers;
   };
 }
