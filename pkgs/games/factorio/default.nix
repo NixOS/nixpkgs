9a37532413ee467d74e501a7e24fb4cec13df4f9
Artemis Tosini <me@artem.ist>
2019-03-06 13:39:40 +0000

    factorio: experimental 0.17.5 → 0.17.6

--- pkgs/games/factorio/default.nix
+++ pkgs/games/factorio/default.nix
@@ -53,11 +53,11 @@ let
     x86_64-linux = let bdist = bdistForArch { inUrl = "linux64"; inTar = "x64"; }; in {
       alpha = {
         stable        = bdist { sha256 = "0b4hbpdcrh5hgip9q5dkmw22p66lcdhnr0kmb0w5dw6yi7fnxxh0"; version = "0.16.51"; withAuth = true; };
-        experimental  = bdist { sha256 = "15k0kgayh5j3gd0cq8jc24ykwnay32jd2b5v0nij27jslj0hkdx6"; version = "0.17.5"; withAuth = true; };
+        experimental  = bdist { sha256 = "1a3h24y2s9h8j4vcwzsgzgsgp6xaa522qzrmckfslbjxwqka2sqx"; version = "0.17.6"; withAuth = true; };
       };
       headless = {
         stable        = bdist { sha256 = "0zrnpg2js0ysvx9y50h3gajldk16mv02dvrwnkazh5kzr1d9zc3c"; version = "0.16.51"; };
-        experimental  = bdist { sha256 = "1avwhwc323ga276hhagdy9piy9xlcqf99z8d63pwjmvbshznbl41"; version = "0.17.5"; };
+        experimental  = bdist { sha256 = "0bw12njp2smy2x99s7mrlbafvd8jnmw3a1zm6lkpiy20kn4mmqbc"; version = "0.17.6"; };
       };
       demo = {
         stable        = bdist { sha256 = "0zf61z8937yd8pyrjrqdjgd0rjl7snwrm3xw86vv7s7p835san6a"; version = "0.16.51"; };
