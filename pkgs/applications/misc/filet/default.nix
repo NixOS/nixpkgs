12153dc4cccc83a0b4b88981bdcf7a327c4815a7
Niclas <33751841+buffet@users.noreply.github.com>
2019-03-07 23:34:34 +0100

    filet: 0.1.1 -> 0.1.2 (#57032)

--- pkgs/applications/misc/filet/default.nix
+++ pkgs/applications/misc/filet/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "filet";
-  version = "0.1.1";
+  version = "0.1.2";
 
   src = fetchFromGitHub {
     owner = "buffet";
     repo = "filet";
     rev = version;
-    sha256 = "0mpq0jlm7z853ybmijm2s7vb97afhjj80kgsc65ajb5lg95k6mnm";
+    sha256 = "0fk616rsninl97x4g1f4blidw6drh5dvjy6s41yf6zgragrr2xh5";
   };
 
   makeFlags = [ "PREFIX=$(out)" ];
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
     description = "A fucking fucking fast file fucker (afffff)";
     homepage = https://github.com/buffet/filet;
     license = licenses.mpl20;
-    platforms = platforms.all;
+    platforms = platforms.linux;
     maintainers = with maintainers; [ buffet ];
   };
 }
