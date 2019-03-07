1f8b70e5e5acb9844a66b596b8fd28c5c5136ab4
Frederik Rietdijk <freddyrietdijk@fridh.nl>
2019-03-07 10:04:08 +0100

    ticpp: init at unstable-2019-01-09

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19674,6 +19674,8 @@ in
     inherit (pkgs.gnome2) libgnome libgnomeui;
   };
 
+  ticpp = callPackage ../development/libraries/ticpp { };
+
   tig = gitAndTools.tig;
 
   tilda = callPackage ../applications/misc/tilda {
