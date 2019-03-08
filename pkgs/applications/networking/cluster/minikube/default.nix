4151cc034ef55477d022d1234909f51edba701ca
Vincent Demeester <vincent@sbr.pm>
2019-03-08 22:01:53 +0100

    minikube: 0.30.0 -> 0.35.0 (#56879)
    * minikube: 0.30.0 -> 0.34.1
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>
    
    * minikube: 0.34.1 -> 0.35.0
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>
    (cherry picked from commit 1f037a90f1a6ad306a9942964722c66132d842d6)

--- pkgs/applications/networking/cluster/minikube/default.nix
+++ pkgs/applications/networking/cluster/minikube/default.nix
@@ -14,9 +14,9 @@ let
 in buildGoPackage rec {
   pname   = "minikube";
   name    = "${pname}-${version}";
-  version = "0.30.0";
+  version = "0.35.0";
 
-  kubernetesVersion = "1.11.2";
+  kubernetesVersion = "1.13.4";
 
   goPackagePath = "k8s.io/minikube";
 
@@ -24,7 +24,7 @@ in buildGoPackage rec {
     owner  = "kubernetes";
     repo   = "minikube";
     rev    = "v${version}";
-    sha256 = "02jxwh8qrvjn31rzjwx23908nd1i592drfdykxbc5b6a62fwp02z";
+    sha256 = "1qb342zs50sdfvfb8vbn7vrx11qqjy1hw518c053s7p34ljb24qd";
   };
 
   buildInputs = [ go-bindata makeWrapper gpgme ] ++ stdenv.lib.optional stdenv.hostPlatform.isDarwin vmnet;
@@ -35,7 +35,9 @@ in buildGoPackage rec {
 
     go-bindata -nomemcopy -o pkg/minikube/assets/assets.go -pkg assets deploy/addons/...
 
-    ISO_VERSION=$(grep "^ISO_VERSION" Makefile | sed "s/^.*\s//")
+    VERSION_MAJOR=$(grep "^VERSION_MAJOR" Makefile | sed "s/^.*\s//")
+    VERSION_MINOR=$(grep "^VERSION_MINOR" Makefile | sed "s/^.*\s//")
+    ISO_VERSION=v$VERSION_MAJOR.$VERSION_MINOR.0
     ISO_BUCKET=$(grep "^ISO_BUCKET" Makefile | sed "s/^.*\s//")
     KUBERNETES_VERSION=${kubernetesVersion}
 
@@ -66,7 +68,7 @@ in buildGoPackage rec {
     homepage    = https://github.com/kubernetes/minikube;
     description = "A tool that makes it easy to run Kubernetes locally";
     license     = licenses.asl20;
-    maintainers = with maintainers; [ ebzzry copumpkin ];
+    maintainers = with maintainers; [ ebzzry copumpkin vdemeester ];
     platforms   = with platforms; unix;
   };
 }
