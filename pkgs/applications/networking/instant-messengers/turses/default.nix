f0fb7c64f50b188fd4f84d5427932341a42db4f7
Silvan Mosberger <infinisil@icloud.com>
2019-03-08 22:46:23 +0100

    turses: Fix
    (cherry picked from commit 308dfc17acd7adc85c7c5465aed0461db3cffd73)

--- pkgs/applications/networking/instant-messengers/turses/default.nix
+++ pkgs/applications/networking/instant-messengers/turses/default.nix
@@ -1,10 +1,11 @@
-{ stdenv, python3Packages }:
+{ stdenv, python36Packages }:
 
 with stdenv.lib;
-with python3Packages;
+
+# Needs Python <3.7 for now, see https://github.com/louipc/turses/issues/4
+with python36Packages;
 
 buildPythonPackage rec {
-  name = "${pname}-${version}";
   pname = "turses";
   version = "0.3.1";
 
@@ -13,7 +14,9 @@ buildPythonPackage rec {
     sha256 = "15mkhm3b5ka42h8qph0mhh8izfc1200v7651c62k7ldcs50ib9j6";
   };
 
-  buildInputs = [ mock pytest coverage tox ];
+  disabled = ! python36Packages.pythonOlder "3.7";
+
+  checkInputs = [ mock pytest coverage tox ];
   propagatedBuildInputs = [ urwid tweepy future ];
 
   checkPhase = ''
