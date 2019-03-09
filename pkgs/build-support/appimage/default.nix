fb6bc78a5c98638e24d055a8613d80ec5eb4d910
Timo Kaufmann <timokau@zoho.com>
2019-03-09 22:03:26 +0100

    appimage-run: add missing libs

--- pkgs/build-support/appimage/default.nix
+++ pkgs/build-support/appimage/default.nix
@@ -75,6 +75,8 @@ rec {
       krb5
     ];
 
+    # list of libraries expected in an appimage environment:
+    # https://github.com/AppImage/pkg2appimage/blob/master/excludelist
     multiPkgs = pkgs: with pkgs; [
       desktop-file-utils
       xorg.libXcomposite
@@ -171,6 +173,13 @@ rec {
       xorg.libXft
       libvdpau
       alsaLib
+
+      harfbuzz
+      e2fsprogs
+      libgpgerror
+      keyutils.lib
+      libjack2
+      fribidi
     ];
   };
 }
