16c2c6b66da22bcb9ee8bf1150c7c2b6e29b4162
Maximilian Bosch <maximilian@mbosch.me>
2019-03-09 15:20:48 +0100

    python3Packages.qasm2image: require Python 3.5 at least
    According to `setup.py` in 0.8 older Python wouldn't work.

--- pkgs/development/python-modules/qasm2image/default.nix
+++ pkgs/development/python-modules/qasm2image/default.nix
@@ -8,6 +8,7 @@
 , svgwrite
 , colorama
 , python
+, pythonOlder
 }:
 
 buildPythonPackage rec {
@@ -20,6 +21,8 @@ buildPythonPackage rec {
     rev = "2c01756946ba9782973359dbd7bbf6651af6bee5";
     sha256 = "1bnkzv7wrdvrq71dmsqanb3v2hcsxh5zaglfcxm2d9zzpmvb4a2n";
   };
+
+  disabled = pythonOlder "3.5";
 
   propagatedBuildInputs = [
     cairocffi
