19e075b53b8283b25dbf60d47f1d6bf35f88ccb4
Timo Kaufmann <timokau@zoho.com>
2019-03-09 22:11:47 +0100

    appimage-run: pass arguments to appimage

--- pkgs/tools/package-management/appimage-run/default.nix
+++ pkgs/tools/package-management/appimage-run/default.nix
@@ -13,10 +13,12 @@ in buildFHSUserEnv (fhsArgs // {
     if [ $# -eq 0 ]; then 
       echo "Usage: $0 FILE [OPTION...]"
       echo
+      echo 'Options are passed on to the appimage.'
       echo "If you want to execute a custom command in the appimage's environment, set the APPIMAGE_DEBUG_EXEC environment variable."
       exit 1
     fi
     APPIMAGE="$(realpath "$1")"
+    shift
 
     if [ ! -x "$APPIMAGE" ]; then
       echo "fatal: $APPIMAGE is not executable"
@@ -49,6 +51,6 @@ in buildFHSUserEnv (fhsArgs // {
       exec "$APPIMAGE_DEBUG_EXEC"
     fi
 
-    exec ./AppRun
+    exec ./AppRun "$@"
   '';
 })
