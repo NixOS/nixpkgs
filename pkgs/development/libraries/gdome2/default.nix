NixOS/nixpkgs

commit 57c745f530c9332c8cbd306caa4c5d8733f37c51

gnome2: remove aliases

pkgs/top-level/all-packages.nix
@@ -3175,9 +3175,7 @@ in
 
   gsmartcontrol = callPackage ../tools/misc/gsmartcontrol { };
 
-  gssdp = callPackage ../development/libraries/gssdp {
-    inherit (gnome2) libsoup;
-  };
+  gssdp = callPackage ../development/libraries/gssdp { };
 
   gt5 = callPackage ../tools/system/gt5 { };
 
@@ -3204,9 +3202,7 @@ in
 
   gup = callPackage ../development/tools/build-managers/gup {};
 
-  gupnp = callPackage ../development/libraries/gupnp {
-    inherit (gnome2) libsoup;
-  };
+  gupnp = callPackage ../development/libraries/gupnp { };
 
   gupnp-av = callPackage ../development/libraries/gupnp-av {};
 
@@ -17585,9 +17581,7 @@ in
 
   gspell = callPackage ../development/libraries/gspell { };
 
-  gtk2fontsel = callPackage ../applications/misc/gtk2fontsel {
-    inherit (gnome2) gtk;
-  };
+  gtk2fontsel = callPackage ../applications/misc/gtk2fontsel { };
 
   guake = callPackage ../applications/misc/guake { };
 
@@ -22348,7 +22342,7 @@ in
 
   dpkg = callPackage ../tools/package-management/dpkg { };
 
-  ekiga = newScope pkgs.gnome2 ../applications/networking/instant-messengers/ekiga { };
+  ekiga = callPackage ../applications/networking/instant-messengers/ekiga { };
 
   emulationstation = callPackage ../misc/emulators/emulationstation {
     stdenv = overrideCC stdenv gcc5;

pkgs/applications/misc/gtk2fontsel/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, pkgconfig, gtk }:
+{stdenv, fetchurl, pkgconfig, gtk2 }:
 
 stdenv.mkDerivation rec {
   version = "0.1";
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ stdenv gtk ];
+  buildInputs = [ stdenv gtk2 ];
 
   preferLocalBuild = true;
 

pkgs/applications/networking/instant-messengers/ekiga/default.nix
@@ -1,6 +1,6 @@
 { stdenv, glib, fetchurl, fetchpatch, cyrus_sasl, gettext, openldap, ptlib, opal, libXv, rarian, intltool
 , perlPackages, evolution-data-server, gnome-doc-utils, avahi, autoreconfHook
-, libsigcxx, gtk, dbus-glib, libnotify, libXext, xorgproto, gnome3, boost, libsecret
+, libsigcxx, gtk2, dbus-glib, libnotify, libXext, xorgproto, gnome3, boost, libsecret
 , pkgconfig, libxml2, unixODBC, db, nspr, nss, zlib
 , libXrandr, which, libxslt, libtasn1, gmp, nettle, sqlite, makeWrapper }:
 
@@ -14,7 +14,7 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ cyrus_sasl gettext openldap ptlib opal libXv rarian intltool
                   evolution-data-server gnome-doc-utils avahi
-                  libsigcxx gtk dbus-glib libnotify libXext xorgproto sqlite
+                  libsigcxx gtk2 dbus-glib libnotify libXext xorgproto sqlite
                   gnome3.libsoup glib gnome3.adwaita-icon-theme boost
                   autoreconfHook pkgconfig libxml2 unixODBC db nspr
                   nss zlib libsecret libXrandr which libxslt libtasn1

pkgs/desktops/gnome-2/bindings/gnome-python-desktop/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, gnome_python, librsvg, libwnck, libgtop, pkgconfig, python2, gtk }:
+{ stdenv, fetchurl, gnome_python, librsvg, libwnck, libgtop, pkgconfig, python2, gtk2 }:
 
 let
   inherit (python2.pkgs) python pygtk;
@@ -14,7 +14,7 @@ in stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ gtk librsvg libwnck libgtop python ];
+  buildInputs = [ gtk2 librsvg libwnck libgtop python ];
   propagatedBuildInputs = [ gnome_python pygtk ];
 
   # gnome-python-desktop expects that .pth file is already installed by PyGTK

pkgs/desktops/gnome-2/bindings/gnome-python/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, python2, pkgconfig, libgnome, GConf, glib, gtk, gnome_vfs }:
+{ stdenv, fetchurl, python2, pkgconfig, libgnome, GConf, glib, gtk2, gnome_vfs }:
 
 with stdenv.lib;
 
@@ -14,7 +14,7 @@ in stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ python glib gtk GConf libgnome gnome_vfs ];
+  buildInputs = [ python glib gtk2 GConf libgnome gnome_vfs ];
   propagatedBuildInputs = [ pygobject2 pygtk dbus-python ];
 
   # gnome-python expects that .pth file is already installed by PyGTK in the

pkgs/desktops/gnome-2/bindings/libglademm/default.nix
@@ -1,8 +1,8 @@
-{ stdenv, fetchurl, pkgconfig, intltool, gtkmm, libglade }:
+{ stdenv, fetchurl, pkgconfig, intltool, gtkmm2, libglade }:
 
 stdenv.mkDerivation rec {
   name = "libglademm-2.6.7";
-  
+
   src = fetchurl {
     url = "mirror://gnome/sources/libglademm/2.6/${name}.tar.bz2";
     sha256 = "1hrbg9l5qb7w0xvr7013qamkckyj0fqc426c851l69zpmhakqm1q";
@@ -12,6 +12,6 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ intltool ];
-  
-  propagatedBuildInputs = [ gtkmm libglade ];
+
+  propagatedBuildInputs = [ gtkmm2 libglade ];
 }

pkgs/desktops/gnome-2/default.nix
@@ -1,14 +1,6 @@
-{ stdenv, pkgs, lib }:
+{ config, stdenv, pkgs, lib }:
 
 lib.makeScope pkgs.newScope (self: with self; {
-  # Backward compatibility.
-  gtkdoc = self.gtk-doc;
-  startup_notification = pkgs.libstartup_notification;
-  startupnotification = pkgs.libstartup_notification;
-  gnomedocutils = self.gnome-doc-utils;
-  gnomeicontheme = self.gnome_icon_theme;
-  gnome_common = gnome-common;
-  inherit (pkgs) rarian;
 
 #### PLATFORM
 
@@ -22,9 +14,7 @@ lib.makeScope pkgs.newScope (self: with self; {
 
   libglade = callPackage ./platform/libglade { };
 
-  libgnomeprint = callPackage ./platform/libgnomeprint {
-    bison = pkgs.bison2;
-  };
+  libgnomeprint = callPackage ./platform/libgnomeprint { };
 
   libgnomeprintui = callPackage ./platform/libgnomeprintui { };
 
@@ -46,7 +36,6 @@ lib.makeScope pkgs.newScope (self: with self; {
   gnome_python = callPackage ./bindings/gnome-python { };
 
   gnome_python_desktop = callPackage ./bindings/gnome-python-desktop { };
-  python_rsvg = self.gnome_python_desktop;
 
   gnome_vfs = callPackage ./platform/gnome-vfs { };
 
@@ -61,9 +50,6 @@ lib.makeScope pkgs.newScope (self: with self; {
   gtkhtml = callPackage ./platform/gtkhtml { };
 
   gtkhtml4 = callPackage ./platform/gtkhtml/4.x.nix { };
-
-  # Required for nautilus
-  inherit (pkgs) libunique;
 
   gtkglext = callPackage ./platform/gtkglext { };
 
@@ -84,13 +70,22 @@ lib.makeScope pkgs.newScope (self: with self; {
 
   libglademm = callPackage ./bindings/libglademm { };
 
+} // lib.optionalAttrs (config.allowAliases or true) {
   inherit (pkgs)
     # GTK Libs
     glib glibmm atk atkmm cairo pango pangomm gdk_pixbuf gtkmm2 libcanberra-gtk2
 
     # Included for backwards compatibility
-    libsoup libwnck gtk-doc gnome-doc-utils;
+    libsoup libwnck gtk-doc gnome-doc-utils rarian;
 
   gtk = pkgs.gtk2;
   gtkmm = pkgs.gtkmm2;
+  python_rsvg = self.gnome_python_desktop;
+
+  gtkdoc = pkgs.gtk-doc;
+  startup_notification = pkgs.libstartup_notification;
+  startupnotification = pkgs.libstartup_notification;
+  gnomedocutils = pkgs.gnome-doc-utils;
+  gnomeicontheme = self.gnome_icon_theme;
+  gnome_common = gnome-common;
 })

pkgs/desktops/gnome-2/desktop/gnome-icon-theme/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, intltool, iconnamingutils, gtk }:
+{ stdenv, fetchurl, pkgconfig, intltool, iconnamingutils, gtk2 }:
 
 stdenv.mkDerivation rec {
   name = "gnome-icon-theme-2.91.93";
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
     sha256 = "cc7f15e54e2640697b58c26e74cc3f6ebadeb4ef6622bffe9c1e6874cc3478d6";
   };
 
-  nativeBuildInputs = [ pkgconfig intltool iconnamingutils gtk ];
+  nativeBuildInputs = [ pkgconfig intltool iconnamingutils gtk2 ];
 
   # remove a tree of dirs with no files within
   postInstall = '' rm -r "$out/share/locale" '';

commit 4e2c477cf83d88c3fd438d1fb369101b694142ab

gnome2.gtkdoc -> gtk-doc
fix evaluation when `config.allowAliases` == false
(regression after https://github.com/NixOS/nixpkgs/pull/56791)

pkgs/development/libraries/gdome2/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, pkgconfig, glib, libxml2, gtkdoc}:
+{stdenv, fetchurl, pkgconfig, glib, libxml2, gtk-doc}:
 
 let
   pname = "gdome2";
@@ -16,7 +16,7 @@ stdenv.mkDerivation {
   hardeningDisable = [ "format" ];
 
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ glib libxml2 gtkdoc];
+  buildInputs = [ glib libxml2 gtk-doc ];
   propagatedBuildInputs = [glib libxml2];
   patches = [ ./xml-document.patch ];
 

pkgs/development/libraries/liblangtag/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, fetchFromBitbucket, autoreconfHook, gtkdoc, gettext
+{stdenv, fetchurl, fetchFromBitbucket, autoreconfHook, gtk-doc, gettext
 , pkgconfig, glib, libxml2, gobject-introspection, gnome-common, unzip
 }:
 
@@ -38,7 +38,7 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ gettext glib libxml2 gobject-introspection gnome-common
     unzip ];
-  nativeBuildInputs = [ autoreconfHook gtkdoc gettext pkgconfig ];
+  nativeBuildInputs = [ autoreconfHook gtk-doc gettext pkgconfig ];
 
   meta = {
     inherit version;

pkgs/development/libraries/libunique/3.x.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchurl, pkgconfig
 , dbus, dbus-glib, gtk3, gobject-introspection
-, gtkdoc, docbook_xml_dtd_45, docbook_xsl
+, gtk-doc, docbook_xml_dtd_45, docbook_xsl
 , libxslt, libxml2 }:
 
 with stdenv.lib;
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
   minorVer = "2";
   version = "${majorVer}.${minorVer}";
   name = "libunique3-${version}";
-  srcName = "libunique-${version}";  
+  srcName = "libunique-${version}";
 
   src = fetchurl {
     url = "http://ftp.gnome.org/pub/GNOME/sources/libunique/${majorVer}/${srcName}.tar.xz";
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ dbus dbus-glib gtk3 gobject-introspection gtkdoc docbook_xml_dtd_45 docbook_xsl libxslt libxml2 ];
+  buildInputs = [ dbus dbus-glib gtk3 gobject-introspection gtk-doc docbook_xml_dtd_45 docbook_xsl libxslt libxml2 ];
 
   meta = {
     homepage = https://wiki.gnome.org/Attic/LibUnique;

pkgs/development/tools/misc/intel-gpu-tools/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchurl, pkgconfig, libdrm, libpciaccess, cairo, xorgproto, udev
 , libX11, libXext, libXv, libXrandr, glib, bison, libunwind, python3, kmod
-, procps, utilmacros, gnome2, openssl, peg }:
+, procps, utilmacros, gtk-doc, openssl, peg }:
 
 stdenv.mkDerivation rec {
   name = "intel-gpu-tools-${version}";
@@ -14,7 +14,7 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ pkgconfig utilmacros ];
   buildInputs = [ libdrm libpciaccess cairo xorgproto udev libX11 kmod
     libXext libXv libXrandr glib bison libunwind python3 procps
-    gnome2.gtkdoc openssl peg ];
+    gtk-doc openssl peg ];
 
   preConfigure = ''
     ./autogen.sh

pkgs/tools/security/oath-toolkit/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitLab, pam, xmlsec, autoreconfHook, pkgconfig, libxml2, gtkdoc, perl, gengetopt, bison, help2man }:
+{ stdenv, fetchFromGitLab, pam, xmlsec, autoreconfHook, pkgconfig, libxml2, gtk-doc, perl, gengetopt, bison, help2man }:
 
 let
   securityDependency =
@@ -17,7 +17,7 @@ in stdenv.mkDerivation rec {
 
   buildInputs = [ securityDependency libxml2 perl gengetopt bison ];
 
-  nativeBuildInputs = [ autoreconfHook gtkdoc help2man pkgconfig ];
+  nativeBuildInputs = [ autoreconfHook gtk-doc help2man pkgconfig ];
 
   # man file generation fails when true
   enableParallelBuilding = false;

pkgs/top-level/all-packages.nix
@@ -3989,7 +3989,6 @@ in
   libite = callPackage ../development/libraries/libite { };
 
   liblangtag = callPackage ../development/libraries/liblangtag {
-    inherit (gnome2) gtkdoc;
     inherit (gnome3) gnome-common;
   };
 
@@ -4579,7 +4578,7 @@ in
 
   nzbget = callPackage ../tools/networking/nzbget { };
 
-  oathToolkit = callPackage ../tools/security/oath-toolkit { inherit (gnome2) gtkdoc;  };
+  oathToolkit = callPackage ../tools/security/oath-toolkit { };
 
   obex_data_server = callPackage ../tools/bluetooth/obex-data-server { };
 
@@ -9925,9 +9924,7 @@ in
 
   gdl = callPackage ../development/libraries/gdl { };
 
-  gdome2 = callPackage ../development/libraries/gdome2 {
-    inherit (gnome2) gtkdoc;
-  };
+  gdome2 = callPackage ../development/libraries/gdome2 { };
 
   gdbm = callPackage ../development/libraries/gdbm { };
 
@@ -11478,7 +11475,7 @@ in
   libunibreak = callPackage ../development/libraries/libunibreak { };
 
   libunique = callPackage ../development/libraries/libunique { };
-  libunique3 = callPackage ../development/libraries/libunique/3.x.nix { inherit (gnome2) gtkdoc; };
+  libunique3 = callPackage ../development/libraries/libunique/3.x.nix { };
 
   liburcu = callPackage ../development/libraries/liburcu { };
 