649c1152c88f0fd480ce5a1ed9d44785467511f9
Aaron Andersen <aaron@fosslib.net>
2019-03-09 07:18:23 -0500

    kdevelop, kdev-php, kdev-python: 5.3.1 -> 5.3.2

--- pkgs/applications/editors/kdevelop5/kdev-php.nix
+++ pkgs/applications/editors/kdevelop5/kdev-php.nix
@@ -2,14 +2,14 @@
 
 let
   pname = "kdev-php";
-  version = "5.3.1";
+  version = "5.3.2";
 in
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://github.com/KDE/${pname}/archive/v${version}.tar.gz";
-    sha256 = "1xiz4v6w30dsa7l4nk3jw3hxpkx71b0yaaj2k8s7xzgjif824bgl";
+    sha256 = "0yjn7y7al2xs8g0mrjvcym8gbjy4wmiv7lsljcrasjd7ymag1wgs";
   };
 
   cmakeFlags = [
