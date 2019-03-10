de5fc024460f44f28bdb37debab7872ff1dac1d4
R. RyanTM <ryantm-bot@ryantm.com>
2019-03-10 12:18:56 -0700

    libgweather: 3.28.2 -> 3.28.3
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/libgweather/versions

--- pkgs/development/libraries/libgweather/default.nix
+++ pkgs/development/libraries/libgweather/default.nix
@@ -3,13 +3,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libgweather";
-  version = "3.28.2";
+  version = "3.28.3";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0xfy5ghwvnz2g9074dy6512m4z2pv66pmja14vhi9imgacbfh708";
+    sha256 = "1xz60h59zgyk61y2wwzm0xlqav72i668g7v0x7rh9idz240hxls5";
   };
 
   nativeBuildInputs = [ meson ninja pkgconfig gettext vala gtk-doc docbook_xsl docbook_xml_dtd_43 gobject-introspection python3 ];
