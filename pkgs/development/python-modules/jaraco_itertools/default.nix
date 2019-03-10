105d99ad9324c040bd4265e1d693d63f0d906260
Maximilian Bosch <maximilian@mbosch.me>
2019-03-10 13:12:22 +0100

    pythonPackages.jaraco_itertools: fix build
    The package currently fails because of some flake8 violations. However
    invalid coding style shouldn't break the Nix package.
    
    See also https://hydra.nixos.org/build/90115508
    Addresses #56826
    
    (cherry picked from commit eede376d73716798093930369a3c76cde7af6a28)

--- pkgs/development/python-modules/jaraco_itertools/default.nix
+++ pkgs/development/python-modules/jaraco_itertools/default.nix
@@ -1,5 +1,6 @@
 { lib, buildPythonPackage, fetchPypi, setuptools_scm
-, inflect, more-itertools, six, pytest, pytest-flake8 }:
+, inflect, more-itertools, six, pytest
+}:
 
 buildPythonPackage rec {
   pname = "jaraco.itertools";
@@ -10,9 +11,11 @@ buildPythonPackage rec {
     sha256 = "d1380ed961c9a4724f0bcca85d2bffebaa2507adfde535d5ee717441c9105fae";
   };
 
+  patches = [ ./0001-Don-t-run-flake8-checks-during-the-build.patch ];
+
   buildInputs = [ setuptools_scm ];
   propagatedBuildInputs = [ inflect more-itertools six ];
-  checkInputs = [ pytest pytest-flake8 ];
+  checkInputs = [ pytest ];
 
   checkPhase = ''
     pytest
