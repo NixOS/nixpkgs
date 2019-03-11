5bda56590454bfad07de16346c80f62af7e4a955
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-11 18:47:52 +0100

    gnome3.gnome-tetravex: 3.22.0 -> 3.32.0

--- pkgs/desktops/gnome-3/games/gnome-tetravex/default.nix
+++ pkgs/desktops/gnome-3/games/gnome-tetravex/default.nix
@@ -1,23 +1,33 @@
 { stdenv, fetchurl, pkgconfig, gnome3, gtk3, wrapGAppsHook
-, libxml2, intltool, itstool }:
+, libxml2, gettext, itstool, meson, ninja, python3
+, vala, desktop-file-utils
+}:
 
 stdenv.mkDerivation rec {
   name = "gnome-tetravex-${version}";
-  version = "3.22.0";
+  version = "3.32.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-tetravex/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "0a6d7ff5ffcd6c05454a919d46a2e389d6b5f87bc80e82c52c2f20d9d914e18d";
+    sha256 = "18drxp43j2jnywxl6qa7mn1iv33jxr0dpc1l9xza3lnrb0jp0kjl";
   };
 
   passthru = {
     updateScript = gnome3.updateScript { packageName = "gnome-tetravex"; attrPath = "gnome3.gnome-tetravex"; };
   };
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [
-    gtk3 wrapGAppsHook intltool itstool libxml2 gnome3.adwaita-icon-theme
+  nativeBuildInputs = [
+    wrapGAppsHook itstool libxml2 gnome3.adwaita-icon-theme
+    pkgconfig gettext meson ninja python3 vala desktop-file-utils
   ];
+  buildInputs = [
+    gtk3
+  ];
+
+  postPatch = ''
+    chmod +x build-aux/meson_post_install.py
+    patchShebangs build-aux/meson_post_install.py
+  '';
 
   meta = with stdenv.lib; {
     homepage = https://wiki.gnome.org/Apps/Tetravex;
