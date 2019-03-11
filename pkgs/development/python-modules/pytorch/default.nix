3f290ebb6684467b9aefce15d284d49325b237f7
dnaq <dnaq@users.noreply.github.com>
2019-03-11 20:39:48 +0100

    pytorch: fix CUDA support
    This commit fixes CUDA support when building with `allowUnfree = true` and `cudaSupport = true`. The previous change to pytorch.nix
    built, but at runtime cuda support didn't work.
    
    This is a work in progress, the test-suite still doesn't find CUDA, so no CUDA-tests are made of the compiled package.
    Also the list of packages in `nativeBuildInputs` and `propagatedBuildInputs` are probably wrong, due to a lack of understanding from my side.

--- pkgs/development/python-modules/pytorch/default.nix
+++ pkgs/development/python-modules/pytorch/default.nix
@@ -79,20 +79,19 @@ in buildPythonPackage rec {
 
   nativeBuildInputs = [
      cmake
+     numpy.blas
      utillinux
      which
-  ];
-
-  buildInputs = [
-     numpy.blas
   ] ++ lib.optionals cudaSupport [ cudatoolkit_joined cudnn ]
     ++ lib.optionals stdenv.isLinux [ numactl ];
 
   propagatedBuildInputs = [
     cffi
+    numpy.blas
     numpy
     pyyaml
-  ] ++ lib.optional (pythonOlder "3.5") typing;
+  ] ++ lib.optional (pythonOlder "3.5") typing
+    ++ lib.optionals cudaSupport [ cudatoolkit_joined cudnn ];
 
   checkInputs = [ hypothesis ];
   checkPhase = ''
