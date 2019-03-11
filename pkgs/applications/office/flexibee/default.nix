5b6c56a7ab94beeebd9ffc42f2d2ed97dcb7e121
Petr Kracik <petrkr@petrkr.net>
2019-03-11 10:58:32 +0100

    flexibee: 2019.1.0.1 -> 2019.2.0.2

--- pkgs/applications/office/flexibee/default.nix
+++ pkgs/applications/office/flexibee/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, jre, makeWrapper }:
 
-let basever = "2019.1";
-    version = "${basever}.0.1";in
+let basever = "2019.2";
+    version = "${basever}.0.2";in
 
 stdenv.mkDerivation {
   name = "flexibee-${version}";
 
   src = fetchurl {
     url = "http://download.flexibee.eu/download/${basever}/${version}/flexibee-${version}.tar.gz";
-    sha256 = "9e73b8c670c8e3f9548a7fe7fc7fbb9f4a0205c59564d4de01fd4bf18ef4ed38";
+    sha256 = "13dxv2asmh52dcw704dsz0vgg8kfnzdb5gj0sicdyvjz1df193y1";
   };
 
   nativeBuildInputs = [ makeWrapper ];
