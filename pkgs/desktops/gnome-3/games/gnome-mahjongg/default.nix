01b40d98df592c48533c9826b57e74c75927b0b2
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-11 18:37:48 +0100

    gnome3.gnome-mahjongg: 3.22.0 -> 3.32.0

--- pkgs/desktops/gnome-3/games/gnome-mahjongg/default.nix
+++ pkgs/desktops/gnome-3/games/gnome-mahjongg/default.nix
@@ -1,24 +1,35 @@
 { stdenv, fetchurl, pkgconfig, gnome3, gtk3, wrapGAppsHook
-, librsvg, intltool, itstool, libxml2 }:
+, librsvg, gettext, itstool, libxml2
+, meson, ninja, python3, vala, desktop-file-utils
+}:
 
 stdenv.mkDerivation rec {
   name = "gnome-mahjongg-${version}";
-  version = "3.22.0";
+  version = "3.32.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-mahjongg/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "f5972a14fa4ad04153bd6e68475b85cd79c6b44f6cac1fe1edb64dbad4135218";
+    sha256 = "12kamxnxbh26k4iykhbs873mx25a2wrjnhr013lfkwbyl52kg12j";
   };
 
   passthru = {
     updateScript = gnome3.updateScript { packageName = "gnome-mahjongg"; attrPath = "gnome3.gnome-mahjongg"; };
   };
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [
-    gtk3 wrapGAppsHook librsvg intltool itstool libxml2
-    gnome3.adwaita-icon-theme
+  nativeBuildInputs = [
+    meson ninja vala python3 desktop-file-utils
+    pkgconfig gnome3.adwaita-icon-theme
+    libxml2 itstool gettext wrapGAppsHook
   ];
+  buildInputs = [
+    gtk3 librsvg
+  ];
+
+  postPatch = ''
+    chmod +x data/meson_post_install.py
+    patchShebangs data/meson_post_install.py
+  '';
+
 
   meta = with stdenv.lib; {
     homepage = https://wiki.gnome.org/Apps/Mahjongg;
