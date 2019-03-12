1c4a1e3eba4d7f489c8a0155232cf7784dca65f5
Vaibhav Sagar <vaibhav.sagar@obsidian.systems>
2019-03-12 18:21:47 -0400

    cachix: compose overrides

--- pkgs/development/tools/cachix/default.nix
+++ pkgs/development/tools/cachix/default.nix
@@ -1,8 +1,8 @@
-{ haskellPackages, haskell }:
+{ lib, haskellPackages, haskell }:
 
-(haskellPackages.override {
-  overrides = self: super: {
+(haskellPackages.override (old: {
+  overrides = lib.composeExtensions (old.overrides or (_: _: {})) (self: super: {
     cachix = haskell.lib.justStaticExecutables (super.callPackage ./cachix.nix {});
     cachix-api = super.callPackage ./cachix-api.nix {};
-  };
-}).cachix
+  });
+})).cachix
