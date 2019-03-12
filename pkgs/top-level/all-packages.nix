6b73e0e349f38a34206d9e5e45f7be244ae93509
Yorick van Pelt <yorick@yorickvanpelt.nl>
2019-03-12 15:33:50 +0100

    it87: init at 2018-08-14

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14760,6 +14760,8 @@ in
 
     ixgbevf = callPackage ../os-specific/linux/ixgbevf {};
 
+    it87 = callPackage ../os-specific/linux/it87 {};
+
     ena = callPackage ../os-specific/linux/ena {};
 
     v4l2loopback = callPackage ../os-specific/linux/v4l2loopback { };
