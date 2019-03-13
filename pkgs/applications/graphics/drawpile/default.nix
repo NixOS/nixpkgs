3084be150715a061cfc260077ecd26e5257437a7
Francesco Gazzetta <fgaz@fgaz.me>
2019-03-13 12:13:00 +0100

    drawpile,drawpile-server-headless: add options
    Optional client and server features.
    
    Also add a server-headless variant.

--- pkgs/applications/graphics/drawpile/default.nix
+++ pkgs/applications/graphics/drawpile/default.nix
@@ -1,21 +1,63 @@
 { stdenv
 , fetchurl
 , cmake
-, qtbase
-, qtsvg
-, qtmultimedia
-, qttools
-, kdnssd
-, karchive
-, libsodium
-, libmicrohttpd
-, giflib
-, miniupnpc
 , extra-cmake-modules
+
+# common deps
+, karchive
+
+# client deps
+, qtbase
+, qtmultimedia
+, qtsvg
+, qttools
+
+# optional client deps
+, giflib
+, kdnssd
 , libvpx
+, miniupnpc
+, qtx11extras # kis
+
+# optional server deps
+, libmicrohttpd
+, libsodium
+
+# options
+, buildClient ? true
+, buildServer ? true
+, buildServerGui ? true # if false builds a headless server
+, buildExtraTools ? false
+, enableKisTablet ? false # enable improved graphics tablet support
 }:
 
-stdenv.mkDerivation rec {
+with stdenv.lib;
+
+let
+  commonDeps = [
+    karchive
+  ];
+  clientDeps = [
+    qtbase
+    qtmultimedia
+    qtsvg
+    qttools
+    # optional:
+    giflib # gif animation export support
+    kdnssd # local server discovery with Zeroconf
+    libvpx # WebM video export
+    miniupnpc # automatic port forwarding
+  ];
+  serverDeps = [
+    # optional:
+    libmicrohttpd # HTTP admin api
+    libsodium # ext-auth support
+  ];
+  kisDeps = [
+    qtx11extras
+  ];
+
+in stdenv.mkDerivation rec {
   name = "drawpile-${version}";
   version = "2.1.6";
   src = fetchurl {
@@ -23,26 +65,25 @@ stdenv.mkDerivation rec {
     sha256 = "0vwsdvphigrq1daiazi411qflahlvgx8x8ssp581bng2lbq1vrbd";
   };
   nativeBuildInputs = [
+    cmake
     extra-cmake-modules
   ];
-  buildInputs = [
-    # common deps:
-    cmake
-    qtbase qtsvg qtmultimedia qttools
-    karchive
-    # optional deps:
-    #   server-specific:
-    libsodium # ext-auth support
-    libmicrohttpd # HTTP admin api
-    #   client-specific:
-    giflib # gif animation export support
-    miniupnpc # automatic port forwarding
-    kdnssd # local server discovery with Zeroconf
-    libvpx # WebM video export
-  ];
-  configurePhase = "cmake -DCMAKE_INSTALL_PREFIX=$out .";
+  buildInputs =
+    commonDeps ++
+    optionals buildClient      clientDeps ++
+    optionals buildServer      serverDeps ++
+    optionals enableKisTablet  kisDeps    ;
+  configurePhase = ''
+    cmake . \
+      ${optionalString (!buildClient    )  "-DCLIENT=off"   } \
+      ${optionalString (!buildServer    )  "-DSERVER=off"   } \
+      ${optionalString (!buildServerGui )  "-DSERVERGUI=off"} \
+      ${optionalString ( buildExtraTools)  "-DTOOLS=on"     } \
+      ${optionalString ( enableKisTablet)  "-DKIS_TABLET=on"} \
+      -DCMAKE_INSTALL_PREFIX=$out
+  '';
 
-  meta = with stdenv.lib; {
+  meta = {
     description = "A collaborative drawing program that allows multiple users to sketch on the same canvas simultaneously";
     homepage = https://drawpile.net/;
     downloadPage = https://drawpile.net/download/;
