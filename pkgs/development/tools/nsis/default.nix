ab9f31114b3c1351d6555fffc35bc9c9a10e48de
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-03-14 20:32:53 +0100

    nsis: use sourceforge mirrors

--- pkgs/development/tools/nsis/default.nix
+++ pkgs/development/tools/nsis/default.nix
@@ -6,12 +6,12 @@ stdenv.mkDerivation rec {
 
   src =
     fetchurl {
-      url = "https://vorboss.dl.sourceforge.net/project/nsis/NSIS%203/${version}/nsis-${version}-src.tar.bz2";
+      url = "mirror://sourceforge/project/nsis/NSIS%203/${version}/nsis-${version}-src.tar.bz2";
       sha256 = "1xgllk2mk36ll2509hd31mfq6blgncmdzmwxj3ymrwshdh23d5b0";
     };
   srcWinDistributable =
     fetchzip {
-      url = "https://vorboss.dl.sourceforge.net/project/nsis/NSIS%203/${version}/nsis-${version}.zip";
+      url = "mirror://sourceforge/project/nsis/NSIS%203/${version}/nsis-${version}.zip";
       sha256 = "1g31vz73x4d3cmsw2wfk43qa06bpqp5815fb5qq9vmwms6hym6y2";
     };
 
