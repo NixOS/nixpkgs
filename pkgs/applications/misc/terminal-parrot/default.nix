a7586b3055e7b350ced4f0d1663e52d2e6a01d78
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-03-13 17:49:10 -0700

    terminal-parrot: 1.1.0 -> 1.1.1 using buildGoModule

--- pkgs/applications/misc/terminal-parrot/default.nix
+++ pkgs/applications/misc/terminal-parrot/default.nix
@@ -1,17 +1,17 @@
-{ stdenv, buildGoPackage, fetchFromGitHub }:
+{ stdenv, buildGoModule, fetchFromGitHub }:
 
-buildGoPackage rec {
+buildGoModule rec {
   name = "terminal-parrot-${version}";
-  version = "1.1.0";
-
-  goPackagePath = "github.com/jmhobbs/terminal-parrot";
+  version = "1.1.1";
 
   src = fetchFromGitHub {
     owner = "jmhobbs";
     repo = "terminal-parrot";
     rev = "${version}";
-    sha256 = "1mrxmifsmndf6hdq1956p1gyrrp3abh3rmwjcmxar8x2wqbv748y";
+    sha256 = "1b4vr4s1zpkpf5kc1r2kdlp3hf88qp1f7h05g8kd62zf4sfbj722";
   };
+
+  modSha256 = "0ymqhrkgk94z4f2p3c6v75g2h8qlqzdi7byivqzxzmdczmq9zq2s";
 
   meta = with stdenv.lib; {
     description = "Shows colorful, animated party parrot in your terminial";
