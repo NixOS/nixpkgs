e6b124b3b57eb327f777349086a8a67325f6c949
Tim Steinbach <tim@nequissimus.com>
2019-03-14 09:13:17 -0400

    linux: 5.0.1 -> 5.0.2
    (cherry picked from commit c22922e25148220a423ef628bb564c7cd18d54d2)

--- pkgs/os-specific/linux/kernel/linux-5.0.nix
+++ pkgs/os-specific/linux/kernel/linux-5.0.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.0.1";
+  version = "5.0.2";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0gik1ijakfs2z45h78qv044k0s5m8xks3h5idrqssnyvg53j2ril";
+    sha256 = "16vwvzmphw01gynm4750pqyvk37fkccrlh4h67vfbd14dcxfmgs3";
   };
 } // (args.argsOverride or {}))
