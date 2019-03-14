cfbe152ae87cf6897a774117788b15a135c6d3d0
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-03-13 17:46:32 -0700

    terminal-parrot: indent and use version when fetching dependencies

--- pkgs/applications/misc/terminal-parrot/default.nix
+++ pkgs/applications/misc/terminal-parrot/default.nix
@@ -1,22 +1,23 @@
 { stdenv, buildGoPackage, fetchFromGitHub }:
 
 buildGoPackage rec {
-    name = "terminal-parrot-1.1.0";
-    version = "1.1.0";
-    goPackagePath = "github.com/jmhobbs/terminal-parrot";
+  name = "terminal-parrot-${version}";
+  version = "1.1.0";
 
-    src = fetchFromGitHub {
-        owner = "jmhobbs";
-        repo = "terminal-parrot";
-        rev = "22c9bde916c12d8b13cf80ab252995dbf47837d1";
-        sha256 = "1mrxmifsmndf6hdq1956p1gyrrp3abh3rmwjcmxar8x2wqbv748y";
-    };
+  goPackagePath = "github.com/jmhobbs/terminal-parrot";
 
-    meta = with stdenv.lib; {
-        description = "Shows colorful, animated party parrot in your terminial";
-        homepage = https://github.com/jmhobbs/terminal-parrot;
-        license = licenses.mit;
-        platforms = platforms.all;
-        maintainers = [ maintainers.heel ];
-    };
+  src = fetchFromGitHub {
+    owner = "jmhobbs";
+    repo = "terminal-parrot";
+    rev = "${version}";
+    sha256 = "1mrxmifsmndf6hdq1956p1gyrrp3abh3rmwjcmxar8x2wqbv748y";
+  };
+
+  meta = with stdenv.lib; {
+    description = "Shows colorful, animated party parrot in your terminial";
+    homepage = https://github.com/jmhobbs/terminal-parrot;
+    license = licenses.mit;
+    platforms = platforms.all;
+    maintainers = [ maintainers.heel ];
+  };
 }
