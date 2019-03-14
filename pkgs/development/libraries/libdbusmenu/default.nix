225bf9f38fde9ec3699259ca509218ec1913e9b2
worldofpeace <worldofpeace@users.noreply.github.com>
2019-03-14 00:03:42 -0400

    libdbusmenu: use latest vala, use placeholder

--- pkgs/development/libraries/libdbusmenu/default.nix
+++ pkgs/development/libraries/libdbusmenu/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, lib, file
 , pkgconfig, intltool
 , glib, dbus-glib, json-glib
-, gobject-introspection, vala_0_38, gnome-doc-utils
+, gobject-introspection, vala, gnome-doc-utils
 , gtkVersion ? null, gtk2 ? null, gtk3 ? null }:
 
 with lib;
@@ -18,11 +18,11 @@ stdenv.mkDerivation rec {
     sha256 = "12l7z8dhl917iy9h02sxmpclnhkdjryn08r8i4sr8l3lrlm4mk5r";
   };
 
-  nativeBuildInputs = [ pkgconfig intltool ];
+  nativeBuildInputs = [ vala pkgconfig intltool ];
 
   buildInputs = [
     glib dbus-glib json-glib
-    gobject-introspection vala_0_38 gnome-doc-utils
+    gobject-introspection gnome-doc-utils
   ] ++ optional (gtkVersion != null) (if gtkVersion == "2" then gtk2 else gtk3);
 
   postPatch = ''
@@ -49,9 +49,9 @@ stdenv.mkDerivation rec {
   doCheck = false; # generates shebangs in check phase, too lazy to fix
 
   installFlags = [
-    "sysconfdir=\${out}/etc"
+    "sysconfdir=${placeholder ''out''}/etc"
     "localstatedir=\${TMPDIR}"
-    "typelibdir=\${out}/lib/girepository-1.0"
+    "typelibdir=${placeholder ''out''}/lib/girepository-1.0"
   ];
 
   meta = {
