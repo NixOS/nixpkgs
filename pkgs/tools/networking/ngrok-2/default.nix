92813d5e271165000da330e5ddc24f03a794b926
Nick Novitski <nick@expo.io>
2019-03-15 12:39:27 -0700

    ngrok-2: change to version-stable source urls

--- pkgs/tools/networking/ngrok-2/default.nix
+++ pkgs/tools/networking/ngrok-2/default.nix
@@ -7,13 +7,13 @@ stdenv.mkDerivation rec {
   version = "2.2.8";
 
   src = if stdenv.isLinux && stdenv.isi686 then fetchurl {
-    url = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-i386.tgz";
+    url = "https://bin.equinox.io/a/bAxP1reZDmJ/ngrok-${version}-linux-386.tar.gz";
     sha256 = "0s5ymlaxrvm13q3mlvfirh74sx60qh56c5sgdma2r7q5qlsq41xg";
   } else if stdenv.isLinux && stdenv.isx86_64 then fetchurl {
-    url = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz";
+    url = "https://bin.equinox.io/a/iVLSfdAz1X4/ngrok-${version}-linux-amd64.tar.gz";
     sha256 = "1mn9iwgy6xzrjihikwc2k2j59igqmph0cwx17qp0ziap9lp5xxad";
   } else if stdenv.isDarwin then fetchurl {
-    url = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-386.zip";
+    url = "https://bin.equinox.io/a/dFaKVcgrvJ3/ngrok-${version}-darwin-386.zip";
     sha256 = "0yfd250b55wcpgqd00rqfaa7a82f35fmybb31q5xwdbgc2i47pbh";
   } else throw "platform ${stdenv.hostPlatform.system} not supported!";
 
