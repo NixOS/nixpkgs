9925c288ddc3b8dc43a285e3e6397e1dc560cbea
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2019-03-15 21:32:03 +0100

    python.pkgs.zetup: fix build
    (cherry picked from commit 8eee646d960020d7e73cc10691d746cfab158465)

--- pkgs/development/python-modules/zetup/default.nix
+++ pkgs/development/python-modules/zetup/default.nix
@@ -1,6 +1,6 @@
 { stdenv, buildPythonPackage, fetchPypi
 , setuptools_scm, pathpy, nbconvert
-, pytest }:
+, pytest_3 }:
 
 buildPythonPackage rec {
   pname = "zetup";
@@ -11,11 +11,18 @@ buildPythonPackage rec {
     sha256 = "f1cde33d0ef3bedaf697e432201fa86da698dadd3445c0efd2a114753853c675";
   };
 
+  # Python 3.7 compatibility
+  # See https://github.com/zimmermanncode/zetup/pull/1
+  postPatch = ''
+    substituteInPlace zetup/zetup_config.py \
+      --replace "'3.6']" "'3.6', '3.7']"
+  '';
+
   checkPhase = ''
     py.test test
   '';
 
-  checkInputs = [ pytest pathpy nbconvert ];
+  checkInputs = [ pytest_3 pathpy nbconvert ];
   propagatedBuildInputs = [ setuptools_scm ];
 
   meta = with stdenv.lib; {
