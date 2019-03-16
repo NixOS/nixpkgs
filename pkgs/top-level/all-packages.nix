50ff1e15cf13571980103f2fbc0ad3902fc1f253
gnidorah <gnidorah@users.noreply.github.com>
2019-03-17 00:15:35 +0300

    pakcs: 2.0.2 -> 2.1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4764,9 +4764,7 @@ in
 
   pagmo2 = callPackage ../development/libraries/pagmo2 { };
 
-  pakcs = callPackage ../development/compilers/pakcs {
-    haskellPackages = haskell.packages.ghc844;
-  };
+  pakcs = callPackage ../development/compilers/pakcs { };
 
   pal = callPackage ../tools/misc/pal { };
 
