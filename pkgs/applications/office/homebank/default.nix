27201851d680beacaed61da7ef00bf2ae246578a
Pascal Wittmann <mail@pascal-wittmann.de>
2019-03-16 14:57:52 +0100

    homebank: 5.2.2 -> 5.2.3

--- pkgs/applications/office/homebank/default.nix
+++ pkgs/applications/office/homebank/default.nix
@@ -2,10 +2,10 @@
 , hicolor-icon-theme, libsoup, gnome3 }:
 
 stdenv.mkDerivation rec {
-  name = "homebank-5.2.2";
+  name = "homebank-5.2.3";
   src = fetchurl {
     url = "http://homebank.free.fr/public/${name}.tar.gz";
-    sha256 = "19cm49p2x6nwia2yvwj3fv7jxbhw0vx4bs1zqbfvdr5vzwgj5j5c";
+    sha256 = "1wdg86gwd0rc9m3mni9qlh746kxmjn4mk5q7fplxrcp2ajdcqr3a";
   };
 
   nativeBuildInputs = [ pkgconfig wrapGAppsHook ];
