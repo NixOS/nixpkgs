2864f5a9676f1b6bcaa7b337d5f229cab4210052
Robert Hensing <robert@roberthensing.nl>
2019-03-17 20:56:30 +0100

    vscode-extensions.justusadam.language-haskell: init at 2.5.0

--- pkgs/misc/vscode-extensions/default.nix
+++ pkgs/misc/vscode-extensions/default.nix
@@ -23,6 +23,18 @@ rec {
     };
   };
 
+  justusadam.language-haskell = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "language-haskell";
+      publisher = "justusadam";
+      version = "2.5.0"; # see the note above
+      sha256 = "10jqj8qw5x6da9l8zhjbra3xcbrwb4cpwc3ygsy29mam5pd8g6b3";
+    };
+    meta = {
+      license = stdenv.lib.licenses.bsd3;
+    };
+  };
+
   ms-vscode.cpptools = callPackage ./cpptools {};
 
   ms-python.python = callPackage ./python {};
