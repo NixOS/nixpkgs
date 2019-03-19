fcf74c6dc05d5a41bc04e1ce56072321d715ccfb
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2019-03-19 15:58:12 +0100

    python.pkgs.msrest: 0.6.2 -> 0.6.4

--- pkgs/development/python-modules/msrest/default.nix
+++ pkgs/development/python-modules/msrest/default.nix
@@ -1,25 +1,45 @@
-{ pkgs
+{ lib
 , buildPythonPackage
-, fetchPypi
-, python
-, certifi
+, fetchFromGitHub
+, requests
 , requests_oauthlib
-, typing
 , isodate
+, certifi
+, aiohttp
+, aiodns
+, pytest
+, httpretty
 }:
 
 buildPythonPackage rec {
-  version = "0.6.2";
+  version = "0.6.4";
   pname = "msrest";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "0icklfjaagk0j9iwq897avmqhwwmgs7c5yy5jw3ppdqz6h0sm38v";
+  #src = fetchPypi {
+  #  inherit pname version;
+  #  sha256 = "5dadd54bec98d52cd9f43fb095015c346135b8cafaa35f24c7309cc25d3ad266";
+  #};
+
+  src = fetchFromGitHub {
+    owner = "Azure";
+    repo = "msrest-for-python";
+    rev = "v${version}";
+    sha256 = "0ilrc06qq0dw4qqzq1dq2vs6nymc39h19w52dwcyawwfalalnjzi";
   };
 
-  propagatedBuildInputs = [ certifi requests_oauthlib typing isodate ];
+  propagatedBuildInputs = [
+    requests requests_oauthlib isodate certifi
+    # optional
+    aiohttp aiodns
+  ];
 
-  meta = with pkgs.lib; {
+  checkInputs = [ pytest httpretty ];
+
+  checkPhase = ''
+    pytest tests/
+  '';
+
+  meta = with lib; {
     description = "The runtime library 'msrest' for AutoRest generated Python clients.";
     homepage = "https://azure.microsoft.com/en-us/develop/python/";
     license = licenses.mit;
