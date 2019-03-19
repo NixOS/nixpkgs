428644141f8bebb779a3a2033fe58e0ea6eeca74
Aleksey Kladov <aleksey.kladov@gmail.com>
2019-03-19 08:56:36 +0300

    jumpapp: init at 1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3652,6 +3652,8 @@ in
 
   jsduck = callPackage ../development/tools/jsduck { };
 
+  jumpapp = callPackage ../tools/X11/jumpapp {};
+
   jucipp = callPackage ../applications/editors/jucipp { };
 
   jupp = callPackage ../applications/editors/jupp { };
