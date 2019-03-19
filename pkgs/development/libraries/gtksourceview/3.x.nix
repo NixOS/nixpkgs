20cd8d4b30cbb15dc77a48f575dcc31a4c523749
worldofpeace <worldofpeace@users.noreply.github.com>
2019-03-19 19:39:55 -0400

    gtksourceview3: use latest vala

--- pkgs/development/libraries/gtksourceview/3.x.nix
+++ pkgs/development/libraries/gtksourceview/3.x.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, atk, cairo, glib, gtk3, pango, vala_0_40
+{ stdenv, fetchurl, pkgconfig, atk, cairo, glib, gtk3, pango, vala
 , libxml2, perl, intltool, gettext, gnome3, gobject-introspection, dbus, xvfb_run, shared-mime-info }:
 
 stdenv.mkDerivation rec {
@@ -19,7 +19,7 @@ stdenv.mkDerivation rec {
 
   outputs = [ "out" "dev" ];
 
-  nativeBuildInputs = [ pkgconfig intltool perl gobject-introspection vala_0_40 ];
+  nativeBuildInputs = [ pkgconfig intltool perl gobject-introspection vala ];
 
   checkInputs = [ xvfb_run dbus ];
 
