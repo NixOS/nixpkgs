e48495f132d59e7e59e6ad56b60cef95cebc2d64
aszlig <aszlig@nix.build>
2019-03-19 02:10:51 +0100

    lastwatch: Remove no longer maintained package
    I no longer use nor do I maintain this package upstream and with the
    current version of pylast moving to Python 3, this package is hereby
    obsolete as I'm not willing to port this to Python 3.
    
    Signed-off-by: aszlig <aszlig@nix.build>
    (cherry picked from commit 7c3311d152f156419822068d036f13fb4e323353)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18031,8 +18031,6 @@ in
 
   caps = callPackage ../applications/audio/caps { };
 
-  lastwatch = callPackage ../applications/audio/lastwatch { };
-
   lastfmsubmitd = callPackage ../applications/audio/lastfmsubmitd { };
 
   lbdb = callPackage ../tools/misc/lbdb { abook = null; gnupg = null; goobook = null; khard = null; mu = null; };
