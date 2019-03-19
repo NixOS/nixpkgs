3aa6408a8c2e6c524a1c1744ce1f9b345869f2db
Tim Steinbach <tim@nequissimus.com>
2019-03-19 08:55:47 -0400

    linux: 4.19.29 -> 4.19.30
    (cherry picked from commit b1c72326cebabf55abbce734cab3aea596fc40c3)

--- pkgs/os-specific/linux/kernel/linux-4.19.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.19.29";
+  version = "4.19.30";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1wz77ww1n152qywrq501f215y2ymi0ci3v6h1i4s0kvj7v1ya28d";
+    sha256 = "1hkhxbdxck1hc9hbja5afshmd4afngi4haff57vammrkxfc0jkpy";
   };
 } // (args.argsOverride or {}))
