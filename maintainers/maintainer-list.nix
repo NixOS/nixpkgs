9fd4d6cbcb87c50be5b95062bb5d0bb0665952c7
Peter Ferenczy <ggpeti@gmail.com>
2019-03-20 13:26:45 +0100

    maintainers: add ggpeti

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1770,6 +1770,11 @@
     github = "Gerschtli";
     name = "Tobias Happ";
   };
+  ggpeti = {
+    email = "ggpeti@gmail.com";
+    github = "ggpeti";
+    name = "Peter Ferenczy";
+  };
   gilligan = {
     email = "tobias.pflug@gmail.com";
     github = "gilligan";
