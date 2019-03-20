61e59fbba05a5187ba4ca4a2cc382d719137b228
Michael Weiss <dev.primeos@gmail.com>
2019-03-20 20:57:50 +0100

    tdesktop: 1.6.0 -> 1.6.1
    tdesktopPackages.preview: 1.6.0 -> 1.6.1

--- pkgs/applications/networking/instant-messengers/telegram/tdesktop/default.nix
+++ pkgs/applications/networking/instant-messengers/telegram/tdesktop/default.nix
@@ -4,8 +4,8 @@ let
   mkTelegram = args: qt5.callPackage (import ./generic.nix args) { };
   stableVersion = {
     stable = true;
-    version = "1.6.0";
-    sha256Hash = "0cspz2kbibrb19l0wbad2dbr0c9ibz153vgd4s45byqn3b5v6h6p";
+    version = "1.6.1";
+    sha256Hash = "1gy5al5m1hks0z98cya9kkfinh6k1i8a1d97cy7x6gj0jgmgs88k";
     # svn log svn://svn.archlinux.org/community/telegram-desktop/trunk
     archPatchesRevision = "429149";
     archPatchesHash = "1ylpi9kb6hk27x9wmna4ing8vzn9b7247iya91pyxxrpxrcrhpli";
