45456fecc74f70476c583d4144019a1a9f48267e
Gabriel Ebner <gebner@gebner.org>
2019-03-20 13:33:24 +0100

    elan: 0.5.0 -> 0.7.4
    This fixes a bug with the github integration, the old version no longer
    works due to github changes.
    
    (cherry picked from commit 52c3ee6c4d3fac1ad0a7aff0788abda639d8b3fc)

--- pkgs/applications/science/logic/elan/default.nix
+++ pkgs/applications/science/logic/elan/default.nix
@@ -1,16 +1,16 @@
-{ stdenv, pkgconfig, curl, openssl, zlib, fetchFromGitHub, rustPlatform }:
+{ stdenv, pkgconfig, curl, openssl, zlib, fetchFromGitHub, rustPlatform_1_31 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform_1_31.buildRustPackage rec {
   name = "elan-${version}";
-  version = "0.5.0";
+  version = "0.7.4";
 
-  cargoSha256 = "01d3s47fjszxx8s5gr3haxq3kz3hswkrkr8x97wx8l4nfhm8ndd2";
+  cargoSha256 = "0vv7kr7rc3lvas7ngp5dp99ajjd5v8k5937ish7zqz1k4970q2f1";
 
   src = fetchFromGitHub {
     owner = "kha";
     repo = "elan";
     rev = "v${version}";
-    sha256 = "13zcqlyz0nwvd574llndrs7kvkznj6njljkq3v5j7kb3vndkj6i9";
+    sha256 = "0yf8mfgp4l78f5cjq1imh462axaa24jfhn69cgf7prw4jrmw3bch";
   };
 
   nativeBuildInputs = [ pkgconfig ];
