96eda42857f3ce1f47905e81acbe45ba2145df60
Will Dietz <w@wdtz.org>
2019-03-20 08:36:36 -0500

    libpqxx: 6.3.3 -> 6.4.0
    https://github.com/jtv/libpqxx/releases/tag/6.4.0

--- pkgs/development/libraries/libpqxx/default.nix
+++ pkgs/development/libraries/libpqxx/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libpqxx";
-  version = "6.3.3";
+  version = "6.4.0";
 
   src = fetchFromGitHub {
     owner = "jtv";
     repo = pname;
     rev = version;
-    sha256 = "1gfi4ghnhzlkdza2ifvg6b2rk5qn0swq7ykphwmwalb166vj2wlx";
+    sha256 = "1972c5g34xzn4yzmg1vzrc3wxmlz8s004skiz2dp6qxlla5qjial";
   };
 
   nativeBuildInputs = [ gnused python2 ];
