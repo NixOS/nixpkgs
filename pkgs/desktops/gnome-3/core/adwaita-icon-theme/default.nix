aa606cbd46877dab63b701f87ac82e9d006e1f6c
R. RyanTM <ryantm-bot@ryantm.com>
2019-03-19 20:30:28 -0700

    gnome3.adwaita-icon-theme: 3.30.1 -> 3.32.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/adwaita-icon-theme/versions

--- pkgs/desktops/gnome-3/core/adwaita-icon-theme/default.nix
+++ pkgs/desktops/gnome-3/core/adwaita-icon-theme/default.nix
@@ -3,11 +3,11 @@
 
 stdenv.mkDerivation rec {
   name = "adwaita-icon-theme-${version}";
-  version = "3.30.1";
+  version = "3.32.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/adwaita-icon-theme/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "1kp1lis3dr16jmlgycz1b29jsr6ir8wmqj6laqwlhs663cmjlxbd";
+    sha256 = "11ij35na8nisvxx3qh527iz33h6z2q1a7iinqyp7p65v0zjbd3b9";
   };
 
   # For convenience, we can specify adwaita-icon-theme only in packages
