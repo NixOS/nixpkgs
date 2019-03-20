12e425c17b25b991c60cf190c888bbe2bf632d49
Peter Ferenczy <ggpeti@gmail.com>
2019-03-20 13:27:13 +0100

    musly: init at unstable-2017-04-26

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22577,6 +22577,8 @@ in
 
   muse = callPackage ../applications/audio/muse { };
 
+  musly = callPackage ../applications/audio/musly { };
+
   mynewt-newt = callPackage ../tools/package-management/mynewt-newt { };
 
   inherit (callPackage ../tools/package-management/nix {
