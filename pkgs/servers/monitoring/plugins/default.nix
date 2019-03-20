7304454f86fcffb959f34769c6d5859965b0c81a
Aaron Andersen <aaron@fosslib.net>
2019-03-19 20:55:53 -0400

    monitoring-plugins: add mysql support

--- pkgs/servers/monitoring/plugins/default.nix
+++ pkgs/servers/monitoring/plugins/default.nix
@@ -1,5 +1,6 @@
-{ stdenv, fetchFromGitHub, autoreconfHook
+{ stdenv, fetchFromGitHub, fetchpatch, autoreconfHook
 , coreutils, gnugrep, gnused, lm_sensors, net_snmp, openssh, openssl, perl
+, mysql, zlib
 , runtimeShell }:
 
 with stdenv.lib;
@@ -20,6 +21,14 @@ in stdenv.mkDerivation rec {
     sha256 = "1pw7i6d2cnb5nxi2lbkwps2qzz04j9zd86fzpv9ka896b4aqrwv1";
   };
 
+  patches = [
+    # https://github.com/monitoring-plugins/monitoring-plugins/issues/1508
+    (fetchpatch {
+      url = "https://github.com/monitoring-plugins/monitoring-plugins/commit/ac0437ff896ba9ce2549b2d2ec3de146a886f08a.patch";
+      sha256 = "0jf6fqkyzag66rid92m7asnr2dp8rr8kn4zjvhqg0mqvf8imppky";
+    })
+  ];
+
   # !!! Awful hack. Grrr... this of course only works on NixOS.
   # Anyway the check that configure performs to figure out the ping
   # syntax is totally impure, because it runs an actual ping to
@@ -39,7 +48,7 @@ in stdenv.mkDerivation rec {
   '';
 
   # !!! make openssh a runtime dependency only
-  buildInputs = [ net_snmp openssh openssl perl ];
+  buildInputs = [ mysql net_snmp openssh openssl perl zlib ];
 
   nativeBuildInputs = [ autoreconfHook ];
 
