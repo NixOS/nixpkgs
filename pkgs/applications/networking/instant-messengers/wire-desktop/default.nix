23568a1ed3cebae96e0125951aae525e2546eed7
worldofpeace <worldofpeace@users.noreply.github.com>
2019-03-20 22:24:04 -0400

    wire-desktop: 3.6.2885 -> 3.7.2891
    Wire uses Electron 4.08 and support for i686-linux
    has been dropped in 4.0  [0]
    
    Release Notes:
    https://medium.com/wire-news/linux-3-7-2891-edc6f663c0b2
    
    Changelog:
    https://github.com/wireapp/wire-desktop/compare/linux/3.6.2885...linux/3.7.2891
    
    [0]: https://electronjs.org/blog/linux-32bit-support

--- pkgs/applications/networking/instant-messengers/wire-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/wire-desktop/default.nix
@@ -4,7 +4,9 @@
 , libXrender, libXtst, libXScrnSaver, nss, nspr, alsaLib, cups, expat, udev
 , xdg_utils, hunspell, pulseaudio, pciutils, at-spi2-atk
 }:
+
 let
+
   rpath = stdenv.lib.makeLibraryPath [
     alsaLib
     atk
@@ -43,69 +45,59 @@ let
     xorg.libxcb
   ];
 
-  version = "3.6.2885";
-
-  plat = {
-    "i686-linux" = "i386";
-    "x86_64-linux" = "amd64";
-  }.${stdenv.hostPlatform.system};
-
-  sha256 = {
-    "i686-linux" = "1lj2gjv69z94dj7b4zjhls420fs5zzxkdlwv25p2gp4lkv0v6l98";
-    "x86_64-linux" = "1dl88fpy8v3aprzdp1nnwg08sy7yiljqjnpnl3rw0h5nix6xmv9v";
-  }.${stdenv.hostPlatform.system};
-
 in
-  stdenv.mkDerivation rec {
-    name = "wire-desktop-${version}";
 
-    src = fetchurl {
-      url = "https://wire-app.wire.com/linux/debian/pool/main/Wire-${version}_${plat}.deb";
-      inherit sha256;
-    };
+stdenv.mkDerivation rec {
+  pname = "wire-desktop";
+  version = "3.7.2891";
 
-    desktopItem = makeDesktopItem {
-      name = "wire-desktop";
-      exec = "wire-desktop %U";
-      icon = "wire-desktop";
-      comment = "Secure messenger for everyone";
-      desktopName = "Wire Desktop";
-      genericName = "Secure messenger";
-      categories = "Network;InstantMessaging;Chat;VideoConference";
-    };
+  src = fetchurl {
+    url = "https://wire-app.wire.com/linux/debian/pool/main/Wire-${version}_amd64.deb";
+    sha256 = "0x4nx1rpr9wai0h3n14h1r0f0lj5m29a3pkx1w6mfz5la74iczqc";
+  };
 
-    dontBuild = true;
-    dontPatchELF = true;
-    dontConfigure = true;
+  desktopItem = makeDesktopItem {
+    name = "wire-desktop";
+    exec = "wire-desktop %U";
+    icon = "wire-desktop";
+    comment = "Secure messenger for everyone";
+    desktopName = "Wire Desktop";
+    genericName = "Secure messenger";
+    categories = "Network;InstantMessaging;Chat;VideoConference";
+  };
 
-    nativeBuildInputs = [ dpkg ];
-    unpackPhase = "dpkg-deb -x $src .";
-    installPhase = ''
-      mkdir -p "$out"
-      cp -R "opt" "$out"
-      cp -R "usr/share" "$out/share"
+  dontBuild = true;
+  dontPatchELF = true;
+  dontConfigure = true;
 
-      chmod -R g-w "$out"
+  nativeBuildInputs = [ dpkg ];
+  unpackPhase = "dpkg-deb -x $src .";
+  installPhase = ''
+    mkdir -p "$out"
+    cp -R "opt" "$out"
+    cp -R "usr/share" "$out/share"
 
-      # Patch wire-desktop
-      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-               --set-rpath "${rpath}:$out/opt/Wire" \
-               "$out/opt/Wire/wire-desktop"
+    chmod -R g-w "$out"
 
-      # Symlink to bin
-      mkdir -p "$out/bin"
-      ln -s "$out/opt/Wire/wire-desktop" "$out/bin/wire-desktop"
+    # Patch wire-desktop
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+      --set-rpath "${rpath}:$out/opt/Wire" \
+      "$out/opt/Wire/wire-desktop"
 
-      # Desktop file
-      mkdir -p "$out/share/applications"
-      cp "${desktopItem}/share/applications/"* "$out/share/applications"
-    '';
+    # Symlink to bin
+    mkdir -p "$out/bin"
+    ln -s "$out/opt/Wire/wire-desktop" "$out/bin/wire-desktop"
 
-    meta = with stdenv.lib; {
-      description = "A modern, secure messenger";
-      homepage = https://wire.com/;
-      license = licenses.gpl3;
-      maintainers = with maintainers; [ worldofpeace ];
-      platforms = [ "i686-linux" "x86_64-linux" ];
-    };
-  }
+    # Desktop file
+    mkdir -p "$out/share/applications"
+    cp "${desktopItem}/share/applications/"* "$out/share/applications"
+  '';
+
+  meta = with stdenv.lib; {
+    description = "A modern, secure messenger";
+    homepage = https://wire.com/;
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ worldofpeace ];
+    platforms = [ "x86_64-linux" ];
+  };
+}
