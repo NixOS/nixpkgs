a435269abf157287be753d20d5b62cf619256538
R. RyanTM <ryantm-bot@ryantm.com>
2019-03-21 18:50:10 -0700

    gnuplot: 5.2.5 -> 5.2.6
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/gnuplot/versions

--- pkgs/tools/graphics/gnuplot/default.nix
+++ pkgs/tools/graphics/gnuplot/default.nix
@@ -19,11 +19,11 @@ let
   withX = libX11 != null && !aquaterm && !stdenv.isDarwin;
 in
 stdenv.mkDerivation rec {
-  name = "gnuplot-5.2.5";
+  name = "gnuplot-5.2.6";
 
   src = fetchurl {
     url = "mirror://sourceforge/gnuplot/${name}.tar.gz";
-    sha256 = "1ajw8xcb1kg2vy8n3rhrz71knjr2yivfavv9lqqzvp1dwv6b5783";
+    sha256 = "1vllgap08nhvdmc03idmkdnk9cfl2bp81hps50q1pqrr640qzp9m";
   };
 
   nativeBuildInputs = [ makeWrapper pkgconfig texinfo ] ++ lib.optional withQt qttools;
