afa88c8aa7fe524403f17a4b1c571d50d3313ddd
Elis Hirwing <elis@hirwing.se>
2019-03-24 10:49:23 +0100

    phpPackages.composer: 1.8.0 -> 1.8.4
    Changelogs:
    https://github.com/composer/composer/releases/tag/1.8.1
    https://github.com/composer/composer/releases/tag/1.8.2
    https://github.com/composer/composer/releases/tag/1.8.3
    https://github.com/composer/composer/releases/tag/1.8.4

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -253,17 +253,17 @@ let
   };
 
   composer = pkgs.stdenv.mkDerivation rec {
-    name = "composer-${version}";
-    version = "1.8.0";
+    pname = "composer";
+    version = "1.8.4";
 
     src = pkgs.fetchurl {
       url = "https://getcomposer.org/download/${version}/composer.phar";
-      sha256 = "19pg9ip2mpyf5cyq34fld7qwl77mshqw3c4nif7sxmpnar6sh089";
+      sha256 = "12h5vqwhklxvwrplggzjl21n6kb972pwkj9ivmn2vbxyixn848hp";
     };
 
     unpackPhase = ":";
 
-    buildInputs = [ pkgs.makeWrapper ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
 
     installPhase = ''
       mkdir -p $out/bin
