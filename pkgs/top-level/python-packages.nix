a30bed52d4008e1b3b04d5a37ad3c99600785f8c
Bryan Richter <b@chreekat.net>
2019-03-24 16:37:42 +0200

    pythonPackages.pyhocon: init at 0.3.51 (#57025)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3930,6 +3930,8 @@ in {
 
   purepng = callPackage ../development/python-modules/purepng { };
 
+  pyhocon = callPackage ../development/python-modules/pyhocon { };
+
   pymaging = callPackage ../development/python-modules/pymaging { };
 
   pymaging_png = callPackage ../development/python-modules/pymaging_png { };
