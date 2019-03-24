3b3072e80794a4a321d831c5c41e1257564dc179
worldofpeace <worldofpeace@users.noreply.github.com>
2019-03-24 10:12:00 -0400

    gnome3.gnome-packagekit: add hicolor-icon-theme for setup-hook

--- pkgs/desktops/gnome-3/misc/gnome-packagekit/default.nix
+++ pkgs/desktops/gnome-3/misc/gnome-packagekit/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, pkgconfig, meson, ninja, gettext, gnome3, packagekit, polkit
-, gtk3, systemd, wrapGAppsHook, desktop-file-utils }:
+, gtk3, systemd, wrapGAppsHook, desktop-file-utils, hicolor-icon-theme }:
 
 stdenv.mkDerivation rec {
   name = "gnome-packagekit-${version}";
@@ -10,7 +10,11 @@ stdenv.mkDerivation rec {
     sha256 = "08rhsisdvx7pnx3rrg5v7c09jbw4grglkdj979gwl4a31j24zjsd";
   };
 
-  nativeBuildInputs = [ pkgconfig meson ninja gettext wrapGAppsHook desktop-file-utils ];
+  nativeBuildInputs = [
+    pkgconfig meson ninja gettext wrapGAppsHook desktop-file-utils
+    hicolor-icon-theme # for setup-hook
+  ];
+
   buildInputs = [ gtk3 packagekit systemd polkit ];
 
   postPatch = ''
