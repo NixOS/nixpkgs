36ac87b190a98261fb177a4c73721a7af50d3169
Jon Banafato <jon@jonafato.com>
2019-03-25 00:42:32 -0400

    keepassxc: 2.3.4 -> 2.4.0
    Update keepassxc to latest release. Notable changes:
    
    - Deprecated KeePassHttp support has been removed
    - Database group synchronization implemented via KeeShare
    - Several CLI enhancements
    
    Full changelog available at
    https://github.com/keepassxreboot/keepassxc/releases/tag/2.4.0

--- pkgs/applications/misc/keepassx/community.nix
+++ pkgs/applications/misc/keepassx/community.nix
@@ -1,24 +1,25 @@
 { stdenv, fetchFromGitHub, cmake, makeWrapper, qttools
 
 , curl
+, glibcLocales
+, libXi
+, libXtst
 , libargon2
 , libgcrypt
-, libsodium
-, zlib
-, libmicrohttpd
-, libXtst
-, qtbase
 , libgpgerror
-, glibcLocales
+, libmicrohttpd
+, libsodium
 , libyubikey
-, yubikey-personalization
-, libXi
-, qtx11extras
+, qrencode
+, qtbase
 , qtmacextras
+, qtsvg
+, qtx11extras
+, yubikey-personalization
+, zlib
 
 , withKeePassBrowser ? true
 , withKeePassSSHAgent ? true
-, withKeePassHTTP ? false
 , withKeePassNetworking ? false
 }:
 
@@ -26,13 +27,13 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "keepassxc-${version}";
-  version = "2.3.4";
+  version = "2.4.0";
 
   src = fetchFromGitHub {
     owner = "keepassxreboot";
     repo = "keepassxc";
     rev = "${version}";
-    sha256 = "1gja402dsbws4z8ybnhqbw7rc9svgqnshqjgf7158d6x0ni386m3";
+    sha256 = "1k8s56003gym2dv6c54gxwzs20i7lf6w5g5qnr449jfmf6wvbivr";
   };
 
   NIX_CFLAGS_COMPILE = stdenv.lib.optionalString stdenv.cc.isClang [
@@ -49,8 +50,8 @@ stdenv.mkDerivation rec {
   NIX_LDFLAGS = stdenv.lib.optionalString stdenv.isDarwin "-rpath ${libargon2}/lib";
 
   patches = [
-    ./darwin.patch
-    ./qt511.patch
+    # FIXME: This patch no longer applies and needs to be updated or removed.
+    #./darwin.patch
   ];
 
   cmakeFlags = [
@@ -58,15 +59,18 @@ stdenv.mkDerivation rec {
     "-DWITH_GUI_TESTS=ON"
     "-DWITH_XC_AUTOTYPE=ON"
     "-DWITH_XC_YUBIKEY=ON"
+    # FIXME: WITH_XC_KEESHARE should be configurable, however the build currently
+    # fails without this option. The fix for this issue will be part of the 2.4.1
+    # release. See https://github.com/keepassxreboot/keepassxc/pull/2810.
+    "-DWITH_XC_KEESHARE=ON"
   ]
   ++ (optional withKeePassBrowser "-DWITH_XC_BROWSER=ON")
-  ++ (optional withKeePassHTTP "-DWITH_XC_HTTP=ON")
   ++ (optional withKeePassNetworking "-DWITH_XC_NETWORKING=ON")
   ++ (optional withKeePassSSHAgent "-DWITH_XC_SSHAGENT=ON");
 
   doCheck = true;
   checkPhase = ''
-    export LC_ALL="en_US.UTF-8"
+    export LC_ALL="en_US.UTF-8" QT_PLUGIN_PATH="${qtbase.bin}/${qtbase.qtPluginPrefix}" QT_QPA_PLATFORM=minimal
     make test ARGS+="-E testgui --output-on-failure"
   '';
 
@@ -83,7 +87,9 @@ stdenv.mkDerivation rec {
     libmicrohttpd
     libsodium
     libyubikey
+    qrencode
     qtbase
+    qtsvg
     qtx11extras
     yubikey-personalization
     zlib
@@ -97,7 +103,7 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "Password manager to store your passwords safely and auto-type them into your everyday websites and applications";
-    longDescription = "A community fork of KeePassX, which is itself a port of KeePass Password Safe. The goal is to extend and improve KeePassX with new features and bugfixes to provide a feature-rich, fully cross-platform and modern open-source password manager. Accessible via native cross-platform GUI and via CLI. Includes optional http-interface to allow browser-integration with plugins like PassIFox (https://github.com/pfn/passifox).";
+    longDescription = "A community fork of KeePassX, which is itself a port of KeePass Password Safe. The goal is to extend and improve KeePassX with new features and bugfixes to provide a feature-rich, fully cross-platform and modern open-source password manager. Accessible via native cross-platform GUI, CLI, and browser integration with the KeePassXC Browser Extension (https://github.com/keepassxreboot/keepassxc-browser).";
     homepage = https://keepassxc.org/;
     license = licenses.gpl2;
     maintainers = with maintainers; [ s1lvester jonafato ];
