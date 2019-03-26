29ed7c827f00b26463bdf11c962eb2f73b310627
Pierre Bourdon <delroth@gmail.com>
2019-03-26 02:44:16 +0100

    ghostscript: add patch for CVE-2019-6116
    This is tagged as version 9.26a in the ghostpdl repo, but unfortunately
    there are no tarballs released with that version number so far. We'll
    continue calling this version 9.26 for now for simplicity's sake (and we
    can switch to 9.26a and remove the patch when it's properly released).
    
    Fixes #58262
    Fixes #58089
    
    (cherry picked from commit 91c46d17d5c8b3a69cbae62d91290e53168fe9d2)

--- pkgs/misc/ghostscript/default.nix
+++ pkgs/misc/ghostscript/default.nix
@@ -1,6 +1,6 @@
 { stdenv, lib, fetchurl, pkgconfig, zlib, expat, openssl, autoconf
 , libjpeg, libpng, libtiff, freetype, fontconfig, libpaper, jbig2dec
-, libiconv, ijs
+, libiconv, ijs, fetchpatch
 , x11Support ? false, xlibsWrapper ? null
 , cupsSupport ? false, cups ? null
 }:
@@ -45,6 +45,11 @@ stdenv.mkDerivation rec {
   patches = [
     ./urw-font-files.patch
     ./doc-no-ref.diff
+    (fetchpatch {
+      name = "CVE-2019-6116";
+      url = "http://git.ghostscript.com/?p=ghostpdl.git;a=patch;h=d3537a54740d78c5895ec83694a07b3e4f616f61";
+      sha256 = "1hr8bpi87bbg1kvv28kflmfh1dhzxw66p9q0ddvbrj72qd86p3kx";
+    })
   ];
 
   outputs = [ "out" "man" "doc" ];
