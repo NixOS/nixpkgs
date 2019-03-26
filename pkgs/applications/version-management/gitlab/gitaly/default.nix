e4e7a7ea567efef50c132882c92ae6423a568d97
Florian Klink <flokli@flokli.de>
2019-03-26 20:59:46 +0100

    gitaly: 1.27.0 -> 1.27.1
    (cherry picked from commit e558347243cc41bf762b955ed2f6a72ed1bbc3d7)

--- pkgs/applications/version-management/gitlab/gitaly/default.nix
+++ pkgs/applications/version-management/gitlab/gitaly/default.nix
@@ -7,14 +7,14 @@ let
     gemdir = ./.;
   };
 in buildGoPackage rec {
-  version = "1.27.0";
+  version = "1.27.1";
   name = "gitaly-${version}";
 
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitaly";
     rev = "v${version}";
-    sha256 = "0m96048nxbgdkly5rz9d21yz8dmgv1kqis98is5kdz5b1wmhz18r";
+    sha256 = "0sr1jjw1rvyxrv6vaqvl138m0x2xgjksjdy92ajslrjxrnjlrjvp";
   };
 
   goPackagePath = "gitlab.com/gitlab-org/gitaly";
