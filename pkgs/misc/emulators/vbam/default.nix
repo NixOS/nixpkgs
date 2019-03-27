9e281d5eed9b825663bdc96b018b37f445956257
R. RyanTM <ryantm-bot@ryantm.com>
2019-03-27 07:52:51 -0700

    vbam: 2.1.0 -> 2.1.1 (#57499)
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/visualboyadvance-m/versions

--- pkgs/misc/emulators/vbam/default.nix
+++ pkgs/misc/emulators/vbam/default.nix
@@ -15,12 +15,12 @@
 
 stdenv.mkDerivation rec {
   name = "visualboyadvance-m-${version}";
-  version = "2.1.0";
+  version = "2.1.1";
   src = fetchFromGitHub {
     owner = "visualboyadvance-m";
     repo = "visualboyadvance-m";
     rev = "v${version}";
-    sha256 = "1dppfvy24rgg3h84gv33l1y7zznkv3zxn2hf98w85pca6k1y2afz";
+    sha256 = "03cs7wn01flx925sxhpz1j5sxa6s7wfxq71955kasn7a3xr1kxwn";
   };
 
   buildInputs = [
