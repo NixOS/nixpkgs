e4e52e320b2572e2f82daa33d25f7ce250f51a10
deliciouslytyped <47436522+deliciouslytyped@users.noreply.github.com>
2019-03-27 22:27:15 +0100

    Update eclipse.nix

--- pkgs/tools/security/ghidra/eclipse.nix
+++ pkgs/tools/security/ghidra/eclipse.nix
@@ -3,7 +3,7 @@ ghidraDevZip: {eclipses, fetchzip}:
     name = "GhidraDev";
     version = "2.0.0";
 
-    sourceRoot = "."; #TODO I dont think this is correct but it works?
+    sourceRoot = ".";
 
     src = ghidraDevZip;
     }
