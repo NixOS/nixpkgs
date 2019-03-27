06495335088861930154294cd8a63e0ec89c8564
deliciouslytyped <47436522+deliciouslytyped@users.noreply.github.com>
2019-03-27 22:37:46 +0100

    Update default.nix

--- pkgs/tools/security/ghidra/default.nix
+++ pkgs/tools/security/ghidra/default.nix
@@ -1,9 +1,10 @@
 {pkgs}:
 let
   ghidra = pkgs.callPackage ./ghidra.nix {};
+  ghidraDev = import ./eclipse.nix (ghidra + "/lib/ghidra/Extensions/Eclipse/GhidraDev/GhidraDev-2.0.0.zip");
 in
   ghidra // {
     passthru = {
-      ghidraDev = (import ./eclipse.nix) (ghidra + "/lib/ghidra/Extensions/Eclipse/GhidraDev/GhidraDev-2.0.0.zip");
+      inherit ghidraDev;
       };
     }
