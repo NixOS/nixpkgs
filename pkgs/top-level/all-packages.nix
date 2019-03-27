4ea9d8390b5195dcef5eaf567f73f314952bebc2
Peter Hoeg <peter@hoeg.com>
2019-03-27 15:08:50 +0800

    dupd: init at 1.7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17967,6 +17967,8 @@ in
 
   japa = callPackage ../applications/audio/japa { };
 
+  dupd = callPackage ../tools/misc/dupd { };
+
   jdupes = callPackage ../tools/misc/jdupes { };
 
   jedit = callPackage ../applications/editors/jedit { };
