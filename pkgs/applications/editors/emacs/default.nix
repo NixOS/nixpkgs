NixOS/nixpkgs

commit 5af6e9f75e89dab40b2ac9cb9b0fe92cc66f1311

emacs: link libXcursor when using lucid toolkit

pkgs/applications/editors/emacs/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, fetchurl, ncurses, xlibsWrapper, libXaw, libXpm, Xaw3d
+{ stdenv, lib, fetchurl, ncurses, xlibsWrapper, libXaw, libXpm, Xaw3d, libXcursor
 , pkgconfig, gettext, libXft, dbus, libpng, libjpeg, libungif
 , libtiff, librsvg, gconf, libxml2, imagemagick, gnutls, libselinux
 , alsaLib, cairo, acl, gpm, cf-private, AppKit, GSS, ImageIO, m17n_lib, libotf
@@ -116,6 +116,17 @@ stdenv.mkDerivation rec {
     mkdir -p $out/Applications
     mv nextstep/Emacs.app $out/Applications
   '';
+
+  postFixup =
+    let libPath = lib.makeLibraryPath [
+      libXcursor
+    ];
+    in lib.optionalString (withX && toolkit == "lucid") ''
+      patchelf --set-rpath \
+        "$(patchelf --print-rpath "$out/bin/emacs"):${libPath}" \
+        "$out/bin/emacs"
+      patchelf --add-needed "libXcursor.so.1" "$out/bin/emacs"
+    '';
 
   meta = with stdenv.lib; {
     description = "The extensible, customizable GNU text editor";

commit 26e82e684c7d6c450dd29916d842ef5bd0112969

emacs: only use patchelf on linux
This prevents building Emacs with X on Darwin.

Broken by 5af6e9f75e89dab40b2ac9cb9b0fe92cc66f1311 (#54916)

pkgs/applications/editors/emacs/default.nix
@@ -123,7 +123,7 @@ stdenv.mkDerivation rec {
     let libPath = lib.makeLibraryPath [
       libXcursor
     ];
-    in lib.optionalString (withX && toolkit == "lucid") ''
+    in lib.optionalString (stdenv.isLinux && withX && toolkit == "lucid") ''
       patchelf --set-rpath \
         "$(patchelf --print-rpath "$out/bin/emacs"):${libPath}" \
         "$out/bin/emacs"