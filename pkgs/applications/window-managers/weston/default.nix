3ee28b12e7125e3f556f116a9a1966e61b38d397
Will Dietz <w@wdtz.org>
2019-03-28 18:41:53 -0500

    weston: 5.0.0 -> 6.0.0
    https://lists.freedesktop.org/archives/wayland-devel/2019-March/040356.html
    https://lists.freedesktop.org/archives/wayland-devel/2019-March/040334.html

--- pkgs/applications/window-managers/weston/default.nix
+++ pkgs/applications/window-managers/weston/default.nix
@@ -1,5 +1,6 @@
-{ stdenv, fetchurl, pkgconfig, wayland, libGL, mesa_noglu, libxkbcommon, cairo, libxcb
-, libXcursor, xlibsWrapper, udev, libdrm, mtdev, libjpeg, pam, dbus, libinput
+{ stdenv, fetchurl, meson, ninja, pkgconfig, wayland, libGL, mesa_noglu, libxkbcommon, cairo, libxcb
+, libXcursor, xlibsWrapper, udev, libdrm, mtdev, libjpeg, pam, dbus, libinput, libevdev
+, colord, lcms2
 , pango ? null, libunwind ? null, freerdp ? null, vaapi ? null, libva ? null
 , libwebp ? null, xwayland ? null, wayland-protocols
 # beware of null defaults, as the parameters *are* supplied by callPackage by default
@@ -7,36 +8,40 @@
 
 stdenv.mkDerivation rec {
   name = "weston-${version}";
-  version = "5.0.0";
+  version = "6.0.0";
 
   src = fetchurl {
     url = "https://wayland.freedesktop.org/releases/${name}.tar.xz";
-    sha256 = "1bsc9ry566mpk6fdwkqpvwq2j7m79d9cvh7d3lgf6igsphik98hm";
+    sha256 = "04p6hal5kalmdp5dxwh2h5qhkkb4dvbsk7l091zvvcq70slj6qsl";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
+  nativeBuildInputs = [ meson ninja pkgconfig ];
   buildInputs = [
     wayland libGL mesa_noglu libxkbcommon cairo libxcb libXcursor xlibsWrapper udev libdrm
-    mtdev libjpeg pam dbus libinput pango libunwind freerdp vaapi libva
+    mtdev libjpeg pam dbus libinput libevdev pango libunwind freerdp vaapi libva
     libwebp wayland-protocols
+    colord lcms2
   ];
 
-  configureFlags = [
-    "--enable-x11-compositor"
-    "--enable-drm-compositor"
-    "--enable-wayland-compositor"
-    "--enable-headless-compositor"
-    "--enable-fbdev-compositor"
-    "--enable-screen-sharing"
-    "--enable-clients"
-    "--enable-weston-launch"
-    "--disable-setuid-install" # prevent install target to chown root weston-launch, which fails
-  ] ++ stdenv.lib.optional (freerdp != null) "--enable-rdp-compositor"
-    ++ stdenv.lib.optional (vaapi != null) "--enable-vaapi-recorder"
+  mesonFlags= [
+    "-Dbackend-x11=true"
+    "-Dbackend-drm=true"
+    "-Dbackend-wayland=true"
+    "-Dbackend-headless=true"
+    "-Dbackend-fbdev=true"
+    "-Dbackend-rdp=${if freerdp == null then "true" else "false"}"
+    "-Dscreenshare=true"
+    "-Dweston-launch=true"
+    "-Dremoting=false" # TODO
+    "-Dsimple-dmabuf-drm=auto"
+    "-Dtest-junit-xml=false"
+    #"--enable-clients"
+    #"--disable-setuid-install" # prevent install target to chown root weston-launch, which fails
+  ] ++ stdenv.lib.optional (vaapi != null) "-Dbackend-drm-screencast-vaapi=true"
     ++ stdenv.lib.optionals (xwayland != null) [
-        "--enable-xwayland"
-        "--with-xserver-path=${xwayland.out}/bin/Xwayland"
-      ];
+    "-Dxwayland=true"
+    "-Dxwayland-path=${xwayland.out}/bin/Xwayland"
+  ];
 
   meta = with stdenv.lib; {
     description = "Reference implementation of a Wayland compositor";
