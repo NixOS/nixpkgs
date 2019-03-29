7cbd6d3cb8673669302dcbcb8f5db6ff3689761f
Will Dietz <w@wdtz.org>
2019-03-29 10:13:20 -0500

    libfido2: init at 1.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11243,6 +11243,8 @@ in
 
   libfakekey = callPackage ../development/libraries/libfakekey { };
 
+  libfido2 = callPackage ../development/libraries/libfido2 { };
+
   libfilezilla = callPackage ../development/libraries/libfilezilla { };
 
   libfm = callPackage ../development/libraries/libfm { };
