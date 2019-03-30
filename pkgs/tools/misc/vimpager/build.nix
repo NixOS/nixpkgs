2415f274cb3e9bd4fa1216cd5e36c77513113ad3
Matthias Beyer <mail@beyermatthias.de>
2019-03-30 13:49:53 +0100

    vimpager: Init at 2.06 and master
    Adding a package vimpager-latest because the last release was 2015.
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

---
+++ pkgs/tools/misc/vimpager/build.nix
@@ -0,0 +1,32 @@
+{ stdenv
+, fetchFromGitHub
+, sharutils
+, version
+, sha256
+}:
+
+stdenv.mkDerivation rec {
+  inherit version;
+  name = "vimpager-${version}";
+
+  src = fetchFromGitHub {
+    inherit sha256;
+
+    owner  = "rkitover";
+    repo   = "vimpager";
+    rev    = "${version}";
+  };
+
+  buildInputs = [ sharutils ]; # for uuencode
+
+  makeFlags = [
+    "PREFIX=$(out)"
+  ];
+
+  meta = with stdenv.lib; {
+    description = "Use Vim as PAGER";
+    homepage    = "https://www.vim.org/scripts/script.php?script_id = 1723";
+    license     = with licenses; [ bsd2 mit vim ];
+    platforms   = platforms.unix;
+  };
+}
