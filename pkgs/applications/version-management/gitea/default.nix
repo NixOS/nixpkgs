c94d027510c2c89828445acc8bbf594b6b2b637a
Elis Hirwing <elis@hirwing.se>
2019-03-30 11:23:35 +0100

    gitea: 1.7.3 -> 1.7.5
    Changelogs:
    https://github.com/go-gitea/gitea/releases/tag/v1.7.5
    https://github.com/go-gitea/gitea/releases/tag/v1.7.4

--- pkgs/applications/version-management/gitea/default.nix
+++ pkgs/applications/version-management/gitea/default.nix
@@ -7,14 +7,14 @@
 with stdenv.lib;
 
 buildGoPackage rec {
-  name = "gitea-${version}";
-  version = "1.7.3";
+  pname = "gitea";
+  version = "1.7.5";
 
   src = fetchFromGitHub {
     owner = "go-gitea";
     repo = "gitea";
     rev = "v${version}";
-    sha256 = "0q33xn2l2ii8vd3hxr0f6ipk8mv2ahb3p8fzdzylhgg9w15snvsr";
+    sha256 = "063rfb0jwxfq91cw7gp0achqsgi3dpj1pqfi7k5md67bhp2i2si3";
     # Required to generate the same checksum on MacOS due to unicode encoding differences
     # More information: https://github.com/NixOS/nixpkgs/pull/48128
     extraPostFetch = ''
