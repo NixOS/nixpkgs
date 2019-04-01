0c595c2f5062a525d7b45ece58c675207a0f05aa
Ihar Hrachyshka <ihar@redhat.com>
2019-04-01 09:28:55 -0700

    patchelf: fixed tests for musl
    The recipe is copied from patchelfUnstable.

--- pkgs/development/tools/misc/patchelf/default.nix
+++ pkgs/development/tools/misc/patchelf/default.nix
@@ -1,16 +1,27 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, autoreconfHook }:
 
 stdenv.mkDerivation rec {
-  name = "patchelf-0.10";
+  name = "patchelf-${version}";
+  version = "0.10";
 
   src = fetchurl {
     url = "https://nixos.org/releases/patchelf/${name}/${name}.tar.bz2";
     sha256 = "1wzwvnlyf853hw9zgqq5522bvf8gqadk8icgqa41a5n7593csw7n";
   };
 
+  # Drop test that fails on musl (?)
+  postPatch = stdenv.lib.optionalString stdenv.hostPlatform.isMusl ''
+    substituteInPlace tests/Makefile.am \
+      --replace "set-rpath-library.sh" ""
+  '';
+
   setupHook = [ ./setup-hook.sh ];
 
-  doCheck = false; # fails 8 out of 24 tests, problems when loading libc.so.6
+  nativeBuildInputs = [ autoreconfHook ];
+  buildInputs = [ ];
+
+  # if not Musl, fails 8 out of 25 tests, problems when loading libc.so.6
+  doCheck = stdenv.hostPlatform.isMusl;
 
   meta = with stdenv.lib; {
     homepage = https://nixos.org/patchelf.html;
