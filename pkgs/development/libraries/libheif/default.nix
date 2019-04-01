5af733bbff44ef6d4c50eede0759051ddbe68347
luz.paz <luzpaz@users.noreply.github.com>
2019-04-01 14:43:27 -0400

    libheif: 1.3.2 -> 1.4.0

--- pkgs/development/libraries/libheif/default.nix
+++ pkgs/development/libraries/libheif/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, libde265, x265, libpng, libjpeg }:
 
 stdenv.mkDerivation rec {
-  version = "1.3.2";
+  version = "1.4.0";
   name = "libheif-${version}";
 
   src = fetchFromGitHub {
     owner = "strukturag";
     repo = "libheif";
     rev = "v${version}";
-    sha256 = "0hk8mzig2kp5f94j4jwqxzjrm7ffk16ffvxl92rf0afsh6vgnz7w";
+    sha256 = "977a9831f1d61b5005566945c7e16e31de35a57a8dd6eb715ae0f40a3595cb60";
   };
 
   nativeBuildInputs = [ autoreconfHook pkgconfig ];
