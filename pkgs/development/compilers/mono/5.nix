f46f60ac9b9f1073234de3cc8dc3cb041f55afa6
Jiri Danek <jdanek@redhat.com>
2019-04-02 13:39:21 +0200

    mono: 5.16.0.220 -> 5.18.1.0

--- pkgs/development/compilers/mono/5.nix
+++ pkgs/development/compilers/mono/5.nix
@@ -2,7 +2,7 @@
 
 callPackage ./generic.nix (rec {
   inherit Foundation libobjc;
-  version = "5.16.0.220";
-  sha256 = "1qwdmxssplfdb5rq86f1j8lskvr9dfk5c8hqz9ic09ml69r8c87l";
+  version = "5.18.1.0";
+  sha256 = "1w540f6r84shih0vrxa4h7q1ix1vx6zg0g10vcn95alg9ysqyam9";
   enableParallelBuilding = false;
 })
