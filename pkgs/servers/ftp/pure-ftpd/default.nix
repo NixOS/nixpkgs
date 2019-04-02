18ea86b576335d2821cd57f86df788b765718590
R. RyanTM <ryantm-bot@ryantm.com>
2019-04-02 02:40:00 -0700

    pure-ftpd: 1.0.47 -> 1.0.48
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/pure-ftpd/versions

--- pkgs/servers/ftp/pure-ftpd/default.nix
+++ pkgs/servers/ftp/pure-ftpd/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, openssl }:
 
 stdenv.mkDerivation rec {
-  name = "pure-ftpd-1.0.47";
+  name = "pure-ftpd-1.0.48";
 
   src = fetchurl {
     url = "https://download.pureftpd.org/pub/pure-ftpd/releases/${name}.tar.gz";
-    sha256 = "1b97ixva8m10vln8xrfwwwzi344bkgxqji26d0nrm1yzylbc6h27";
+    sha256 = "1xr1wlf08qaw93irsbdk4kvhqnkvmi6p0jb8kiiz0vr0h92pszxl";
   };
 
   buildInputs = [ openssl ];
