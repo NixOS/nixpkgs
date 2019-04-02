b0f57c66eb32b0239a2c51ef7bdff9cd695e7aba
R. RyanTM <ryantm-bot@ryantm.com>
2019-04-02 06:42:59 -0700

    python27Packages.j2cli: 0.3.5.post1 -> 0.3.6.post1
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python2.7-j2cli/versions

--- pkgs/development/python-modules/j2cli/default.nix
+++ pkgs/development/python-modules/j2cli/default.nix
@@ -9,12 +9,12 @@
 
 buildPythonPackage rec {
   pname = "j2cli";
-  version = "0.3.5.post1";
+  version = "0.3.6.post1";
   disabled = isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "c0439a79308aae320bfd01d82b56893b02fe461195d8b69b438ba9b333075642";
+    sha256 = "1j8s09b75w041b2lawjz341ri997n9fnxbd2ipm9czxj6fhj8hi2";
   };
 
   buildInputs = [ nose ];
