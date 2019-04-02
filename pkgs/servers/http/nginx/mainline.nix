10b394132ef96209e55cae6ba52cb840cf11402c
Franz Pletz <fpletz@fnordicwalking.de>
2019-04-02 09:39:19 +0200

    nginxMainline: 1.15.9 -> 1.15.10
    (cherry picked from commit 4c0d1ae7be66f0c01d864f7239a44e17791a236e)

--- pkgs/servers/http/nginx/mainline.nix
+++ pkgs/servers/http/nginx/mainline.nix
@@ -1,6 +1,6 @@
 { callPackage, ... }@args:
 
 callPackage ./generic.nix (args // {
-  version = "1.15.9";
-  sha256 = "0hxfsz1117r91b9fb5hjddyrf1czvb36lh1z7zalqqdskfcbmkz4";
+  version = "1.15.10";
+  sha256 = "0g3wadbf9r730p0j5c0pnsmbmbrwvvnpyzhgbmsf9g2jplx78rdq";
 })
