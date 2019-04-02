ff36d958785e6dce5a87e3c9f3285c142b6a9138
Franz Pletz <fpletz@fnordicwalking.de>
2019-04-02 12:15:12 +0200

    nixos/quicktun: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -632,6 +632,7 @@
   ./services/networking/prosody.nix
   ./services/networking/quagga.nix
   ./services/networking/quassel.nix
+  ./services/networking/quicktun.nix
   ./services/networking/racoon.nix
   ./services/networking/radicale.nix
   ./services/networking/radvd.nix
