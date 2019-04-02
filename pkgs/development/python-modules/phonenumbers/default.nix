7b688b0fa07bbb8d50517fe5c3f7fa4249e7d439
R. RyanTM <ryantm-bot@ryantm.com>
2019-04-02 09:20:49 -0700

    python37Packages.phonenumbers: 8.10.6 -> 8.10.8
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-phonenumbers/versions

--- pkgs/development/python-modules/phonenumbers/default.nix
+++ pkgs/development/python-modules/phonenumbers/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "phonenumbers";
-  version = "8.10.6";
+  version = "8.10.8";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "2fe47dbf947cc74643ef1a49411466483d1165ced2b62578a14b513dd09642a9";
+    sha256 = "1ka7fnlvmvmw984k89mlkdlwbnnyap186a1yfnykx833bp364yg2";
   };
 
   meta = {
