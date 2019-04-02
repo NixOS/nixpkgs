e96ff107a3cb095b7a2b757c8390a1bdb740d09f
R. RyanTM <ryantm-bot@ryantm.com>
2019-04-02 11:19:45 -0700

    python37Packages.pytest-tornado: 0.5.0 -> 0.6.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-pytest-tornado/versions

--- pkgs/development/python-modules/pytest-tornado/default.nix
+++ pkgs/development/python-modules/pytest-tornado/default.nix
@@ -7,11 +7,11 @@
 
 buildPythonPackage rec {
   pname = "pytest-tornado";
-  version = "0.5.0";
+  version = "0.6.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "214fc59d06fb81696fce3028b56dff522168ac1cfc784cfc0077b7b1e425b4cd";
+    sha256 = "0ndwjsad901km7zw8xxj3igjff651hg1pjcmv5vqx458xhnmbfqw";
   };
 
   # package has no tests
