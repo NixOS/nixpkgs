0bb32d27768867c888076f58261bd4af81f904a6
Tim Steinbach <tim@nequissimus.com>
2019-04-03 19:37:11 -0400

    linux: 5.0.5 -> 5.0.6

--- pkgs/os-specific/linux/kernel/linux-5.0.nix
+++ pkgs/os-specific/linux/kernel/linux-5.0.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.0.5";
+  version = "5.0.6";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1l4l5kqmakqhkc59nha363866d5gkbc94mhnpyh802may9xq1gaz";
+    sha256 = "04zl3xqgr6rlkc44raz3rz74ag7l5j16wkvkdmlzrdq7ia6rljly";
   };
 } // (args.argsOverride or {}))
