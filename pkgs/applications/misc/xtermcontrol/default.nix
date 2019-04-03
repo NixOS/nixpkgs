54cf5ccf47a720df2b5c16313f8f209baa7c3f1e
R. RyanTM <ryantm-bot@ryantm.com>
2019-04-03 04:14:34 -0700

    xtermcontrol: 3.6 -> 3.7
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/xtermcontrol/versions

--- pkgs/applications/misc/xtermcontrol/default.nix
+++ pkgs/applications/misc/xtermcontrol/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  version = "3.6";
+  version = "3.7";
   name = "xtermcontrol-${version}";
 
   src = fetchurl {
     url = "https://thrysoee.dk/xtermcontrol/xtermcontrol-${version}.tar.gz";
-    sha256 = "01bwgxya6qh4br2lx3v98p7j1b99skgr6c1frw5kdkxy57qlpgkz";
+    sha256 = "04m12ddaps5sdbqvkwkp6lh81i8vh5ya5gzcxkrkilsga3m6qff2";
   };
 
   meta = {
