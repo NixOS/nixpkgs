9e696227b8a85ead5d93248a997eb1d7118299f7
Marcial Gaißert <gaisseml@studi.informatik.uni-stuttgart.de>
2019-04-03 22:55:11 +0200

    grisbi: 1.0.4 -> 1.2.1

--- pkgs/applications/office/grisbi/default.nix
+++ pkgs/applications/office/grisbi/default.nix
@@ -3,11 +3,11 @@
 
 stdenv.mkDerivation rec {
   name = "grisbi-${version}";
-  version = "1.0.4";
+  version = "1.2.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/grisbi/${name}.tar.bz2";
-    sha256 = "1rh2iqvi7lpz5l57vn7qk9azil3y1g65mfbi9hhbx956knh9bpf6";
+    sha1 = "1159c5491967fa7afd251783013579ffb45b891b";
   };
 
   nativeBuildInputs = [ pkgconfig wrapGAppsHook ];
