0810f2dd7c310cf1a8b82ce399383a32188628da
R. RyanTM <ryantm-bot@ryantm.com>
2019-04-02 19:40:07 -0700

    snd: 18.8 -> 19.2
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/snd/versions

--- pkgs/applications/audio/snd/default.nix
+++ pkgs/applications/audio/snd/default.nix
@@ -4,11 +4,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "snd-18.8";
+  name = "snd-19.2";
 
   src = fetchurl {
     url = "mirror://sourceforge/snd/${name}.tar.gz";
-    sha256 = "16p6cmxl8y58wa19k1z6i66qsqaz7rld4850b0sprbxjjb6cqhf7";
+    sha256 = "1a6ls2hyvggss12idca22hq5vsq4jw2xkwrx22dx29i9926gdr6h";
   };
 
   nativeBuildInputs = [ pkgconfig ];
