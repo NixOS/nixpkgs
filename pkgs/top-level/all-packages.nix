810df3f55c29e2379218bfaad3b6728096937f9b
eyjhbb@gmail.com <eyjhbb@gmail.com>
2019-04-04 18:05:24 +0200

    curseradio: init at 0.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16768,6 +16768,8 @@ in
 
   cuneiform = callPackage ../tools/graphics/cuneiform {};
 
+  curseradio = callPackage ../applications/audio/curseradio { };
+
   cutecom = libsForQt5.callPackage ../tools/misc/cutecom { };
 
   cutegram =
