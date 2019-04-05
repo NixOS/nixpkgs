dda301df365d36ee11ca434fd47bafea24df1674
Alexandre Peyroux <alex@px.io>
2019-04-05 21:56:45 +0200

    pythonPackages.jsbeautifier: fix build (#58608)
    (cherry picked from commit 946807c888cc5bf9cd13832ae705a3db7113a0e1)

--- pkgs/development/python-modules/jsbeautifier/default.nix
+++ pkgs/development/python-modules/jsbeautifier/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchPypi, buildPythonApplication, EditorConfig, pytest, six }:
+{ lib, fetchPypi, buildPythonApplication, EditorConfig, fetchpatch, pytest, six }:
 
 buildPythonApplication rec {
   pname = "jsbeautifier";
@@ -12,6 +12,15 @@ buildPythonApplication rec {
     inherit pname version;
     sha256 = "7d02baa9b0459bf9c5407c1b99ad5566de04a3b664b18a58ac64f52832034204";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/beautify-web/js-beautify/commit/78e35a11cbb805fc044241d6465800ee2bd57ebc.patch";
+      sha256 = "1ah7nshk96yljy37i20v4fga834dix9cdbhkdc3flfm4904n4523";
+    })
+  ];
+
+  patchFlags = [ "-p2" ];
 
   meta = with lib; {
     homepage    = "http://jsbeautifier.org";
