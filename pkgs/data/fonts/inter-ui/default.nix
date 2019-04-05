dfeaf5e3165ef1dee2af34f2f2fc45b68ee524ee
Will Dietz <w@wdtz.org>
2019-04-05 15:50:50 -0500

    Revert "inter(-ui): 3.3 -> 3.4"
    This reverts commit 4d7a9732336aeffa2772d2840598eb3e8ef599cf.

--- pkgs/data/fonts/inter-ui/default.nix
+++ pkgs/data/fonts/inter-ui/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchzip }:
 
 let
-  version = "3.4";
+  version = "3.3";
 in fetchzip {
   name = "inter-${version}";
 
@@ -12,7 +12,7 @@ in fetchzip {
     unzip -j $downloadedFile \*.otf -d $out/share/fonts/opentype
   '';
 
-  sha256 = "1dl4zc1a5dl9xkg094zmzy6bk7gms8vhsiln58ix7sffzcf3pri8";
+  sha256 = "17fv33ryvbla4f4mfgw7m7gjlwyjlni90a8gpb7jws1qzn0vgazg";
 
   meta = with stdenv.lib; {
     homepage = https://rsms.me/inter/;
