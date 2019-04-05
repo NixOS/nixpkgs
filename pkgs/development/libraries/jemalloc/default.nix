3f0014f4888e97d4003fcd010a51d4934d73c4c1
Will Dietz <w@wdtz.org>
2019-04-05 13:49:27 -0500

    jemalloc: 5.1.0 -> 5.2.0

--- pkgs/development/libraries/jemalloc/default.nix
+++ pkgs/development/libraries/jemalloc/default.nix
@@ -1,4 +1,4 @@
 import ./common.nix {
-  version = "5.1.0";
-  sha256 = "0s3jpcyhzia8d4k0xyc67is78kg416p9yc3c2f9w6fhhqqffd5jk";
+  version = "5.2.0";
+  sha256 = "1d73a5c5qdrwck0fa5pxz0myizaf3s9alsvhiqwrjahdlr29zgkl";
 }
