83f104a73ee4ed2c74d11bab1d3fe36a12742fd7
Will Dietz <w@wdtz.org>
2019-04-06 09:21:31 -0500

    libnotify: 0.7.7 -> 0.7.8
    * cleanup a bit using new helpers
    * move to meson to avoid need for gtk_doc and such
    * NEWS: https://gitlab.gnome.org/GNOME/libnotify/blob/0.7.8/NEWS
    * tag: https://gitlab.gnome.org/GNOME/libnotify/tags/0.7.8

--- pkgs/development/libraries/libnotify/default.nix
+++ pkgs/development/libraries/libnotify/default.nix
@@ -1,21 +1,31 @@
-{ stdenv, fetchurl, pkgconfig, autoreconfHook
-, glib, gdk_pixbuf, gobject-introspection }:
+{ stdenv, fetchurl, meson, ninja, pkgconfig
+, glib, gdk_pixbuf, gobject-introspection, gnome3 }:
 
 stdenv.mkDerivation rec {
-  ver_maj = "0.7";
-  ver_min = "7";
-  name = "libnotify-${ver_maj}.${ver_min}";
+  pname = "libnotify";
+  version = "0.7.8";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/libnotify/${ver_maj}/${name}.tar.xz";
-    sha256 = "017wgq9n00hx39n0hm784zn18hl721hbaijda868cm96bcqwxd4w";
+    url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
+    sha256 = "1371csx0n92g60b5dmai4mmzdnx8081mc3kcgc6a0xipcq5rw839";
   };
 
-  # disable tests as we don't need to depend on gtk+(2/3)
-  configureFlags = [ "--disable-tests" ];
+  mesonFlags = [
+    # disable tests as we don't need to depend on gtk+(2/3)
+    "-Dtests=false"
+    "-Ddocbook_docs=disabled"
+    "-Dgtk_doc=false"
+  ];
 
-  nativeBuildInputs = [ pkgconfig autoreconfHook gobject-introspection ];
+  nativeBuildInputs = [ meson ninja pkgconfig gobject-introspection ];
   buildInputs = [ glib gdk_pixbuf ];
+
+  passthru = {
+    updateScript = gnome3.updateScript {
+      packageName = pname;
+      versionPolicy = "none";
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = https://developer.gnome.org/notification-spec/;
