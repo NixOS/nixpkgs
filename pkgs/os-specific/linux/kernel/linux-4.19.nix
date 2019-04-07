922c07d2d2a857a5cf1685a2178dd3fd122f8b46
Tim Steinbach <tim@nequissimus.com>
2019-04-07 08:34:12 -0400

    linux: Fix update script
    The script was wrecking `modDirVersion`, putting them back and
    ensuring it will not break again.

--- pkgs/os-specific/linux/kernel/linux-4.19.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19.nix
@@ -6,6 +6,7 @@ buildLinux (args // rec {
   version = "4.19.34";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
+  modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
 
   # branchVersion needs to be x.y
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
