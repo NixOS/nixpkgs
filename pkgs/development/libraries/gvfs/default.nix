ef3a463db5d1d261212399e401083a89477e52e4
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-04-08 15:58:09 +0200

    gvfs: 1.38.1 -> 1.38.2
    fix https://nvd.nist.gov/vuln/detail/CVE-2019-3827

--- pkgs/development/libraries/gvfs/default.nix
+++ pkgs/development/libraries/gvfs/default.nix
@@ -9,13 +9,13 @@
 
 let
   pname = "gvfs";
-  version = "1.38.1";
+  version = "1.38.2";
 in stdenv.mkDerivation rec {
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "18311pn5kp9b4kf5prvhcjs0cwf7fm3mqh6s6p42avcr5j26l4zd";
+    sha256 = "0nw6mv5qq0d79d72x28db0vwihv4isny6m8q7vdim11ngk10hgwh";
   };
 
   postPatch = ''
