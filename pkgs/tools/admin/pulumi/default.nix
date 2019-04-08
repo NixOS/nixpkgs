2c88a3fecbf1f0dff5db518b866e406749883055
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2019-04-08 13:29:05 -0400

    pulumi: fix darwin src hash

--- pkgs/tools/admin/pulumi/default.nix
+++ pkgs/tools/admin/pulumi/default.nix
@@ -14,7 +14,7 @@ let
     };
     "x86_64-darwin" = {
       url = "https://get.pulumi.com/releases/sdk/pulumi-v${version}-darwin-x64.tar.gz";
-      sha256 = "1mkz9bkkvpvbpzfnvwpx4892zd05bvjz5rbfwhwzm3wzfcjjs16i";
+      sha256 = "1cn4mbgzq86d69mpp341zxfl8baakz0n5p9yd5chxrjjvxb6i629";
     };
   };
 
