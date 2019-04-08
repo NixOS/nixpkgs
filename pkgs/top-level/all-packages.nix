ae3be1a41a3a79c6b22b35d0f92f53adf35ee0c5
LinArcX <LinArcX@users.noreply.github.com>
2019-04-08 18:00:11 +0430

    ir-standard-fonts: init at unstable-2017-01-21 (#57943)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1586,6 +1586,8 @@ in
 
   ipvsadm = callPackage ../os-specific/linux/ipvsadm { };
 
+  ir-standard-fonts = callPackage ../data/fonts/ir-standard-fonts { };
+
   lynis = callPackage ../tools/security/lynis { };
 
   mathics = pythonPackages.mathics;
