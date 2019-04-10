fcbe865414256c3e03c02926e5592e24c5be801b
Matthew Bauer <mjbauer95@gmail.com>
2019-04-10 01:23:02 -0400

    androidndk: fixup mess
    New android ndk (18) now uses clang. We were going through the wrapper
    that are provided. This lead to surprising errors when building.
    Ideally we could use the llvm linker as well, but this leads to errors
    as many packages don’t support the llvm linker.

--- pkgs/stdenv/cross/default.nix
+++ pkgs/stdenv/cross/default.nix
@@ -53,7 +53,7 @@ in lib.init bootStages ++ [
       cc = if crossSystem.useiOSPrebuilt or false
              then buildPackages.darwin.iosSdkPkgs.clang
            else if crossSystem.useAndroidPrebuilt or false
-             then buildPackages."androidndkPkgs_${crossSystem.ndkVer}".gcc
+             then buildPackages."androidndkPkgs_${crossSystem.ndkVer}".clang
            else buildPackages.gcc;
 
       extraNativeBuildInputs = old.extraNativeBuildInputs
