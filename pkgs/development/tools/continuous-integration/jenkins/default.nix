c2b1f0079fa007be6383cef1da67a67282459a3d
Christopher A. Williamson <home@chrisaw.com>
2019-04-11 20:56:23 +0100

    jenkins: 2.164.1 -> 2.164.2 (#59309)

--- pkgs/development/tools/continuous-integration/jenkins/default.nix
+++ pkgs/development/tools/continuous-integration/jenkins/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "jenkins-${version}";
-  version = "2.164.1";
+  version = "2.164.2";
 
   src = fetchurl {
     url = "http://mirrors.jenkins.io/war-stable/${version}/jenkins.war";
-    sha256 = "02r2kf0v7pbi6xfak5ii7cwi5pq2awq4pcsi6d7k8m7f69b3ym35";
+    sha256 = "1pmya8g3gs5f9ibbgacdh5f6828jcrydw7v7xmg2j86kwc1vclf8";
   };
 
   buildCommand = ''
