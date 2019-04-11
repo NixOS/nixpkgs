549c9b20c059909d5faabb391da488a62288c36c
AmineChikhaoui <amine.chikhaoui91@gmail.com>
2019-04-11 09:56:24 -0400

    pythonPackages.azure-mgmt-storage: remove patch.
    as suggested in the comment.

--- pkgs/development/python-modules/azure-mgmt-storage/default.nix
+++ pkgs/development/python-modules/azure-mgmt-storage/default.nix
@@ -15,12 +15,6 @@ buildPythonPackage rec {
     sha256 = "16iw7hqhq97vlzfwixarfnirc60l5mz951p57brpcwyylphl3yim";
   };
 
-  preConfigure = ''
-    # Patch to make this package work on requests >= 2.11.x
-    # CAN BE REMOVED ON NEXT PACKAGE UPDATE
-    sed -i 's|len(request_content)|str(len(request_content))|' azure/mgmt/storage/storagemanagement.py
-  '';
-
   postInstall = ''
     echo "__import__('pkg_resources').declare_namespace(__name__)" >> "$out/lib/${python.libPrefix}"/site-packages/azure/__init__.py
     echo "__import__('pkg_resources').declare_namespace(__name__)" >> "$out/lib/${python.libPrefix}"/site-packages/azure/mgmt/__init__.py
