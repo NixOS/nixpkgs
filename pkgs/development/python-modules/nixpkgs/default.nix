eca006be052a93643e3ca1b61072b04e73168ec5
Alexander Sosedkin <monk@unboiled.info>
2019-04-11 16:07:07 +0700

    python3Packages.nixpkgs: 0.2.2 -> 0.2.3

--- pkgs/development/python-modules/nixpkgs/default.nix
+++ pkgs/development/python-modules/nixpkgs/default.nix
@@ -9,22 +9,13 @@
 
 buildPythonPackage rec {
   pname = "nixpkgs";
-  version = "0.2.2";
+  version = "0.2.3";
   disabled = ! pythonAtLeast "3.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0gsrd99kkv99jsrh3hckz7ns1zwndi9vvh4465v4gnpz723dd6fj";
+    sha256 = "12ycbv31g4qv14aq6hfb85hhx026lgvzqfsrkpzb64na0c1yjcvn";
   };
-
-  patches = [
-    # Patch should be dropped once https://github.com/t184256/nixpkgs-python-importer/pull/7
-    # is merged and in a release
-    (fetchpatch {
-      url = "https://github.com/adisbladis/nixpkgs-python-importer/commit/749e05f1.patch";
-      sha256 = "1a72phazpqf6vf3hl3m84z9i5n6h1xpa53bqxnpsff6agxxhd21b";
-    })
-  ];
 
   buildInputs = [ pbr ];
   propagatedBuildInputs = [ pythonix ];
