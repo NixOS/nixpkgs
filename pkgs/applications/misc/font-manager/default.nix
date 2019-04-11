b528e0aecfc9307306ecccd7c1b43cc6cf019412
worldofpeace <worldofpeace@users.noreply.github.com>
2019-04-11 01:32:21 -0400

    font-manager: 0.7.4.3 -> 0.7.5, fix build
    Has fixes for type-argument mismatches in lastest vala etc.
    
    Note that there isn't an offical 0.7.5 release yet (not tagged).
    Patching the fixes would have been nicer, but all the changes
    touched the translations.

--- pkgs/applications/misc/font-manager/default.nix
+++ pkgs/applications/misc/font-manager/default.nix
@@ -1,17 +1,17 @@
-{ stdenv, fetchFromGitHub, meson, ninja, gettext, python3,
+{ stdenv, fetchFromGitHub, meson, ninja, gettext, python3, fetchpatch,
   pkgconfig, libxml2, json-glib , sqlite, itstool, librsvg,
   vala, gtk3, gnome3, desktop-file-utils, wrapGAppsHook, gobject-introspection
 }:
 
 stdenv.mkDerivation rec {
   pname = "font-manager";
-  version = "0.7.4.3";
+  version = "0.7.5";
 
   src = fetchFromGitHub {
     owner = "FontManager";
     repo = "master";
-    rev = version;
-    sha256 = "0v6zn25vxsn3ng31zgsgkb2wwrl0kdv4ikw4ij4yqv49aid3qjd5";
+    rev = "cc057f3e93f5b1033b04decee03cdb44177e48b3";
+    sha256 = "1xg80bi2465p5r8zfmb34iga9yqs3is1k4f13hw0ligvhb58gas0";
   };
 
   nativeBuildInputs = [
@@ -38,7 +38,9 @@ stdenv.mkDerivation rec {
     gnome3.adwaita-icon-theme
   ];
 
-  patches = [ ./correct-post-install.patch ];
+  patches = [
+   ./correct-post-install.patch
+  ];
 
   mesonFlags = [
     "-Ddisable_pycompile=true"
