b353f4df1de2eb943b42c2e3a7bfc3a0d7331e7c
Andrey Golovizin <ag@sologoc.com>
2019-04-13 12:00:16 +0200

    gitAndTools.git-remote-hg: refactor
    Use buildPythonApplication, organize build inputs.

--- pkgs/applications/version-management/git-and-tools/git-remote-hg/default.nix
+++ pkgs/applications/version-management/git-and-tools/git-remote-hg/default.nix
@@ -1,11 +1,12 @@
-{ stdenv, fetchFromGitHub, mercurial, makeWrapper,
+{ stdenv, fetchFromGitHub, mercurial, python2Packages,
   asciidoc, xmlto, docbook_xsl, docbook_xml_dtd_45, libxslt, libxml2
 }:
 
-stdenv.mkDerivation rec {
+python2Packages.buildPythonApplication rec {
   version = "1.0.0";
   pname = "git-remote-hg";
 
+  # not using fetchPypi - the PyPI tarball does not include the manpage
   src = fetchFromGitHub {
     owner = "mnauw";
     repo = "git-remote-hg";
@@ -13,17 +14,17 @@ stdenv.mkDerivation rec {
     sha256 = "0anl054zdi5rg5m4bm1n763kbdjkpdws3c89c8w8m5gq1ifsbd4d";
   };
 
-  buildInputs = [ mercurial.python mercurial makeWrapper
-    asciidoc xmlto docbook_xsl docbook_xml_dtd_45 libxslt libxml2
-  ];
+  nativeBuildInputs = [ asciidoc xmlto docbook_xsl docbook_xml_dtd_45 libxslt libxml2 ];
+  propagatedBuildInputs = [ mercurial ];
 
   doCheck = false;
 
-  installFlags = "HOME=\${out} install-doc";
+  patches = [
+    ./patches/no-dynamic-git-version.patch
+  ];
 
   postInstall = ''
-    wrapProgram $out/bin/git-remote-hg \
-      --prefix PYTHONPATH : "$(echo ${mercurial}/lib/python*/site-packages):$(echo ${mercurial.python}/lib/python*/site-packages)${stdenv.lib.concatMapStrings (x: ":$(echo ${x}/lib/python*/site-packages)") mercurial.pythonPackages or []}"
+    make "HOME=${placeholder "out"}" install-doc;
   '';
 
   meta = with stdenv.lib; {
