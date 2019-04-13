9974ad5b89ec37b86dd8e285e412d24f86962774
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-04-13 14:33:54 +0200

    gnome3.file-roller: 3.32.0 -> 3.32.1
    https://gitlab.gnome.org/GNOME/file-roller/blob/master/NEWS

--- pkgs/desktops/gnome-3/apps/file-roller/default.nix
+++ pkgs/desktops/gnome-3/apps/file-roller/default.nix
@@ -3,11 +3,11 @@
 
 stdenv.mkDerivation rec {
   name = "file-roller-${version}";
-  version = "3.32.0";
+  version = "3.32.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/file-roller/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "1id7f6qbgdxfixxyqjypgwy04mf1w1rbczklqmgpqx5ry4kk0a03";
+    sha256 = "0irm72cf8222h93skivn0nn0ckahiiiimy4rb9r3ccjvhi3hiaqw";
   };
 
   LANG = "en_US.UTF-8"; # postinstall.py
