e879991e1b1a3f87f723024eb07e73b1f28c9b87
Markus Kowalewski <markus.kowalewski@gmail.com>
2019-04-14 00:04:06 +0200

    clp: 1.17.0 -> 1.17.11

--- pkgs/applications/science/math/clp/default.nix
+++ pkgs/applications/science/math/clp/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, zlib }:
 
 stdenv.mkDerivation rec {
-  version = "1.17.0";
+  version = "1.17.1";
   name = "clp-${version}";
   src = fetchurl {
     url = "https://www.coin-or.org/download/source/Clp/Clp-${version}.tgz";
-    sha256 = "1p3slyi6vz5b2r7jlp66659kqll5r5hj83y9f0mrla3mh1bxlb79";
+    sha256 = "1wdg820g3iikf9344ijwsc8sy6c0m6im42bzzizm6rlmkvnmxhk9";
   };
 
   propagatedBuildInputs = [ zlib ];
