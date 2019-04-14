984a3567aa11ab8e1e689e3ae4ef999134fd0eef
montag451 <montag451@laposte.net>
2019-04-14 18:43:10 +0200

    Fix build failure on Python versions older the 3.3

--- pkgs/development/python-modules/humanfriendly/default.nix
+++ pkgs/development/python-modules/humanfriendly/default.nix
@@ -1,6 +1,8 @@
 { stdenv
+, pythonOlder
 , buildPythonPackage
 , fetchPypi
+, monotonic
 }:
 
 buildPythonPackage rec {
@@ -11,6 +13,8 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "33ee8ceb63f1db61cce8b5c800c531e1a61023ac5488ccde2ba574a85be00a85";
   };
+
+  propagatedBuildInputs = [] ++ lib.optional (pythonOlder "3.3") monotonic;
 
   # humanfriendly tests depends on coloredlogs which itself depends on
   # humanfriendly. This lead to infinite recursion when trying to
