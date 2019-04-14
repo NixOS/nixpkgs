57354412aad360e9b00c9f8045683d23a97e06e0
R. RyanTM <ryantm-bot@ryantm.com>
2019-04-14 03:15:01 -0700

    catch2: 2.7.0 -> 2.7.1
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/catch2/versions

--- pkgs/development/libraries/catch2/default.nix
+++ pkgs/development/libraries/catch2/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "catch2-${version}";
-  version = "2.7.0";
+  version = "2.7.1";
 
   src = fetchFromGitHub {
     owner = "catchorg";
     repo = "Catch2";
     rev = "v${version}";
-    sha256="05j01v4hmw0vv5vcj11pbngl200b3j2yvawk08fw9a249jzx6v1a";
+    sha256="0716imkrw4f3y7342ch93cg22cv9bbkx50vn3q0bzgazvj7arrkx";
   };
 
   nativeBuildInputs = [ cmake ];
