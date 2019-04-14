0b8d120d84bca97d9dcc3a8b475ff050ab1ca0a0
Mario Rodas <marsam@users.noreply.github.com>
2019-04-14 15:36:39 -0500

    postgresqlPackages.pg_cron: 1.1.3 -> 1.1.4 (#59396)

--- pkgs/servers/sql/postgresql/ext/pg_cron.nix
+++ pkgs/servers/sql/postgresql/ext/pg_cron.nix
@@ -1,19 +1,20 @@
 { stdenv, fetchFromGitHub, postgresql }:
 
 stdenv.mkDerivation rec {
-  name = "pg_cron-${version}";
-  version = "1.1.3";
+  pname = "pg_cron";
+  version = "1.1.4";
 
   buildInputs = [ postgresql ];
 
   src = fetchFromGitHub {
     owner  = "citusdata";
-    repo   = "pg_cron";
+    repo   = pname;
     rev    = "refs/tags/v${version}";
-    sha256 = "0r33b0c9gdx2lfhrp7lpkif3j8qbzxmnywkcs2rcxwf9qzrb4vnj";
+    sha256 = "0wkqgrm3v999hjcc82h24jv1pib6f6bw8jsv83hgk6g3iv6xsjg9";
   };
 
   installPhase = ''
+    mkdir -p $out/bin    # For buildEnv to setup proper symlinks. See #22653
     mkdir -p $out/{lib,share/extension}
 
     cp *.so      $out/lib
@@ -23,9 +24,9 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Run Cron jobs through PostgreSQL";
-    homepage    = https://www.citusdata.com/;
+    homepage    = https://github.com/citusdata/pg_cron;
     maintainers = with maintainers; [ thoughtpolice ];
-    platforms   = platforms.linux;
+    platforms   = postgresql.meta.platforms;
     license     = licenses.postgresql;
   };
 }
