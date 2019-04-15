22157b544bf9ed468f89ab83affc3c7ae86950e6
Daiderd Jordan <daiderd@gmail.com>
2019-04-15 19:56:05 +0200

    libuv: fix darwin sandbox build

--- pkgs/development/libraries/libuv/default.nix
+++ pkgs/development/libraries/libuv/default.nix
@@ -17,11 +17,10 @@ stdenv.mkDerivation rec {
       "spawn_setuid_fails" "spawn_setgid_fails" "fs_chown" # user namespaces
       "getaddrinfo_fail" "getaddrinfo_fail_sync"
       "threadpool_multiple_event_loops" # times out on slow machines
-    ]
-      # Sometimes: timeout (no output), failed uv_listen. Someone
-      # should report these failures to libuv team. There tests should
-      # be much more robust.
-      ++ stdenv.lib.optionals stdenv.isDarwin [
+    ] ++ stdenv.lib.optionals stdenv.isDarwin [
+        # Sometimes: timeout (no output), failed uv_listen. Someone
+        # should report these failures to libuv team. There tests should
+        # be much more robust.
         "process_title" "emfile" "poll_duplex" "poll_unidirectional"
         "ipc_listen_before_write" "ipc_listen_after_write" "ipc_tcp_connection"
         "tcp_alloc_cb_fail" "tcp_ping_pong" "tcp_ref3" "tcp_ref4"
@@ -34,11 +33,16 @@ stdenv.mkDerivation rec {
         "multiple_listen" "delayed_accept"
         "shutdown_close_tcp" "shutdown_eof" "shutdown_twice" "callback_stack"
         "tty_pty" "condvar_5"
-      ] ++ stdenv.lib.optionals stdenv.isAarch32 [
-        # I observe this test failing with some regularity on ARMv7:
-        # https://github.com/libuv/libuv/issues/1871
-        "shutdown_close_pipe"
-      ];
+        # Tests that fail when sandboxing is enabled.
+        "fs_event_close_in_callback" "fs_event_watch_dir"
+        "fs_event_watch_dir_recursive" "fs_event_watch_file"
+        "fs_event_watch_file_current_dir" "fs_event_watch_file_exact_path"
+        "process_priority" "udp_create_early_bad_bind"
+    ] ++ stdenv.lib.optionals stdenv.isAarch32 [
+      # I observe this test failing with some regularity on ARMv7:
+      # https://github.com/libuv/libuv/issues/1871
+      "shutdown_close_pipe"
+    ];
     tdRegexp = lib.concatStringsSep "\\|" toDisable;
     in lib.optionalString doCheck ''
       sed '/${tdRegexp}/d' -i test/test-list.h
@@ -54,6 +58,9 @@ stdenv.mkDerivation rec {
   enableParallelBuilding = true;
 
   doCheck = true;
+
+  # Some of the tests use localhost networking.
+  __darwinAllowLocalNetworking = true;
 
   meta = with lib; {
     description = "A multi-platform support library with a focus on asynchronous I/O";
