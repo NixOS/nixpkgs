59125047c03a7a881811cf4cdbaa3f53b7ce1ec8
pacien <pacien.trangirard@pacien.net>
2019-04-15 14:50:41 +0200

    riot-web: 1.0.6 -> 1.0.7
    (cherry picked from commit f97effd53c9cde62739ed828d88657c1415d2e15)

--- pkgs/applications/networking/instant-messengers/riot/riot-web.nix
+++ pkgs/applications/networking/instant-messengers/riot/riot-web.nix
@@ -3,11 +3,11 @@
 let configFile = writeText "riot-config.json" conf; in
 stdenv.mkDerivation rec {
   name= "riot-web-${version}";
-  version = "1.0.6";
+  version = "1.0.7";
 
   src = fetchurl {
     url = "https://github.com/vector-im/riot-web/releases/download/v${version}/riot-v${version}.tar.gz";
-    sha256 = "09sm1k3iypqn93iazfx10y4bqz06d2h8f0902sgrj5qbnaq4jcxr";
+    sha256 = "1zg9hwvbanmv2yymjcxjzk2bwvv8707i30vrs0gr213iz6i4abg5";
   };
 
   installPhase = ''
