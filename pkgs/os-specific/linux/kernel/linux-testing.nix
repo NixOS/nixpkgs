4907b2165b225213cbf380333f522a6e6bc7a14d
Tim Steinbach <tim@nequissimus.com>
2019-04-15 08:25:56 -0400

    linux: 5.1-rc4 -> 5.1-rc5

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -1,13 +1,13 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, libelf, utillinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "5.1-rc4";
-  modDirVersion = "5.1.0-rc4";
+  version = "5.1-rc5";
+  modDirVersion = "5.1.0-rc5";
   extraMeta.branch = "5.1";
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "1cqr80b3jfr4g48fpni0pj2p5zs9930q6k6m9xjjdnsrhax1isr6";
+    sha256 = "171caaf8zrjx124431a94sv25c0ka6b3ni194l7fpisn4n3x8y47";
   };
 
   # Should the testing kernels ever be built on Hydra?
