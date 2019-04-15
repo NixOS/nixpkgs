8d06aa7fb02ac6bd6dd47ec0f4050e5c09fc852d
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-04-15 17:10:47 -0400

    symengine: init at 0.3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6020,6 +6020,8 @@ in
 
   svtplay-dl = callPackage ../tools/misc/svtplay-dl { };
 
+  symengine = callPackage ../development/libraries/symengine { };
+
   sysbench = callPackage ../development/tools/misc/sysbench {};
 
   system-config-printer = callPackage ../tools/misc/system-config-printer {
