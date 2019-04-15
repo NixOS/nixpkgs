b504e8359b4ea9476471503d3305b79f943d9412
R. RyanTM <ryantm-bot@ryantm.com>
2019-04-14 19:54:51 -0700

    klavaro: 3.04 -> 3.05
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/klavaro/versions

--- pkgs/games/klavaro/default.nix
+++ pkgs/games/klavaro/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "klavaro-${version}";
-  version = "3.04";
+  version = "3.05";
 
   src = fetchurl {
     url = "mirror://sourceforge/klavaro/${name}.tar.bz2";
-    sha256 = "148vznikdn424938fmizgd281ky7311q51mw2zfzrallj87dzbqf";
+    sha256 = "046xc7is1jz5azkwrb7i9wdqkz8hdm68ga02g6whmi53130wd0py";
   };
 
   nativeBuildInputs = [ pkgconfig ];
