23560ea057024811dfeea8be0b7bd9b7a3d63b31
Matthew Bauer <mjbauer95@gmail.com>
2019-04-17 16:41:33 -0400

    systems: fix emulator identity
    Squashed to fix shell quoting, thanks @Ericson2314

--- lib/systems/default.nix
+++ lib/systems/default.nix
@@ -103,7 +103,7 @@ rec {
       in
         if final.parsed.kernel.name == pkgs.stdenv.hostPlatform.parsed.kernel.name &&
            pkgs.stdenv.hostPlatform.isCompatible final
-        then "${pkgs.runtimeShell} -c"
+        then "${pkgs.runtimeShell} -c '\"$@\"' --"
         else if final.isWindows
         then "${wine}/bin/${wine-name}"
         else if final.isLinux && pkgs.stdenv.hostPlatform.isLinux
