5e094b97ebac220033ad4ea1cdaefbeb667090a7
AmineChikhaoui <amine.chikhaoui91@gmail.com>
2019-04-17 10:32:15 -0400

    nixopsUnstable: 1.6.1pre2728_8ed39f9 -> 1.7pre2764_932bf43
    (cherry picked from commit 5b5ca459d2a2adbcfb05e878200c8788a9acc829)

--- pkgs/tools/package-management/nixops/default.nix
+++ pkgs/tools/package-management/nixops/default.nix
@@ -3,7 +3,7 @@
 callPackage ./generic.nix (rec {
   version = "1.7";
   src = fetchurl {
-    url = "https://hydra.nixos.org/build/92372310/download/2/nixops-1.7.tar.bz2";
+    url = "https://nixos.org/releases/nixops/nixops-${version}/nixops-${version}.tar.bz2";
     sha256 = "091c0b5bca57d4aa20be20e826ec161efe3aec9c788fbbcf3806a734a517f0f3";
   };
 })
