db269657f0c243a12627b124ba219f04c8a8732b
Edmund Wu <fangkazuto@gmail.com>
2019-04-17 17:50:11 -0400

    vscode: patch proper executable
    (cherry picked from commit f8eed03044df19457dc7e291e15a00da949741a3)

--- pkgs/applications/editors/vscode/default.nix
+++ pkgs/applications/editors/vscode/default.nix
@@ -110,8 +110,8 @@ in
         cp $out/lib/vscode/resources/app/resources/linux/code.png $out/share/pixmaps/code.png
 
         # Override the previously determined VSCODE_PATH with the one we know to be correct
-        sed -i "/ELECTRON=/iVSCODE_PATH='$out/lib/vscode'" $out/bin/code
-        grep -q "VSCODE_PATH='$out/lib/vscode'" $out/bin/code # check if sed succeeded
+        sed -i "/ELECTRON=/iVSCODE_PATH='$out/lib/vscode'" $out/bin/${executableName}
+        grep -q "VSCODE_PATH='$out/lib/vscode'" $out/bin/${executableName} # check if sed succeeded
       '';
 
     preFixup = lib.optionalString (system == "i686-linux" || system == "x86_64-linux") ''
