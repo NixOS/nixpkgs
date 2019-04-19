7c60ac71cf0712b45ecd322acad9db11e4d56f3c
José Romildo Malaquias <malaquias@gmail.com>
2019-04-19 09:36:22 -0300

    nixos/deepin: add dde-api services and user/group

--- nixos/modules/services/desktops/deepin/deepin.nix
+++ nixos/modules/services/desktops/deepin/deepin.nix
@@ -22,16 +22,27 @@
 
     (lib.mkIf config.services.deepin.core.enable {
       environment.systemPackages = [
+        pkgs.deepin.dde-api
         pkgs.deepin.dde-daemon
       ];
 
       services.dbus.packages = [
+        pkgs.deepin.dde-api
         pkgs.deepin.dde-daemon
       ];
 
       systemd.packages = [
+        pkgs.deepin.dde-api
         pkgs.deepin.dde-daemon
       ];
+
+      users.groups.deepin-sound-player = { };
+
+      users.users.deepin-sound-player = {
+        description = "Deepin sound player";
+        group = "deepin-sound-player";
+        isSystemUser = true;
+      };
 
       users.groups.dde-daemon = { };
 
