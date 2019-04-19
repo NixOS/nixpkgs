e1a2c1572a7e3ea44fab7524d04703adcbf52910
andrewchambers <andrewchamberss@gmail.com>
2019-04-20 08:35:35 +1200

    janet: 0.4.0 -> 0.4.1 (#59799)

--- pkgs/development/interpreters/janet/default.nix
+++ pkgs/development/interpreters/janet/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "janet";
-  version = "0.4.0";
+  version = "0.4.1";
 
   src = fetchFromGitHub {
     owner = "janet-lang";
     repo = "janet";
     rev = "v${version}";
-    sha256 = "1590f1fxb6qfhf1vp2xhbvdn2jfrgipn5572cckk1ma7f13jnkpy";
+    sha256 = "06iq2y7c9i4pcmmgc8x2fklqkj2i3jrvmq694djiiyd4x81kzcj5";
   };
 
   JANET_BUILD=''\"release\"'';
