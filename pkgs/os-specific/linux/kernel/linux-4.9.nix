24867c60e4f8b813bfb7d5a5a20d529ed695cdc2
Tim Steinbach <tim@nequissimus.com>
2019-04-20 08:30:55 -0400

    linux: 4.9.169 -> 4.9.170

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.169";
+  version = "4.9.170";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1c6nz27q0m6nbb7v7kba6zrhzav5bqqllvwzzqf9cmd5cdn66xmp";
+    sha256 = "078k8dz3nmici7rs7x25h4vr1qaa588zwymv4gs722wfzi07p21k";
   };
 } // (args.argsOverride or {}))
