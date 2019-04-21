3027f426b8bd5c17ae5d94aed8c4b49824dffe15
José Romildo Malaquias <malaquias@gmail.com>
2019-04-21 09:02:47 -0300

    mate.mate-power-manager: 1.20.3 -> 1.22.0

--- pkgs/desktops/mate/mate-power-manager/default.nix
+++ pkgs/desktops/mate/mate-power-manager/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "mate-power-manager-${version}";
-  version = "1.20.3";
+  version = "1.22.0";
 
   src = fetchurl {
     url = "http://pub.mate-desktop.org/releases/${mate.getRelease version}/${name}.tar.xz";
-    sha256 = "17x47j5dkxxsq63bv2jwf3xgnddyy2dya4y14ryivq8q3jh5yhr5";
+    sha256 = "03c09h41qfz83wmjfvwzkq4xqc54aswmki4h034qcxbgfnyfmk1i";
   };
 
   buildInputs = [
