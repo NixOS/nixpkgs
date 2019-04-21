dae51d3c98676a540fdc988cf60732e0fdd6b4f1
José Romildo Malaquias <malaquias@gmail.com>
2019-04-21 09:02:49 -0300

    mate.pluma: 1.20.4 -> 1.22.0

--- pkgs/desktops/mate/pluma/default.nix
+++ pkgs/desktops/mate/pluma/default.nix
@@ -1,12 +1,12 @@
-{ stdenv, fetchurl, pkgconfig, intltool, itstool, isocodes, enchant, libxml2, python, gnome3, gtksourceview3, libpeas, mate, wrapGAppsHook }:
+{ stdenv, fetchurl, pkgconfig, intltool, itstool, isocodes, enchant, libxml2, python3, gnome3, gtksourceview3, libpeas, mate, wrapGAppsHook }:
 
 stdenv.mkDerivation rec {
   name = "pluma-${version}";
-  version = "1.20.4";
+  version = "1.22.0";
 
   src = fetchurl {
     url = "http://pub.mate-desktop.org/releases/${mate.getRelease version}/${name}.tar.xz";
-    sha256 = "0qdbm5y6q8lbabd81mg3rnls5bdvbmfii82f6syqw1cw6381mzgz";
+    sha256 = "07rr5asdjr9slmaijp4m8v9vxscihvm36mfrwlp3lv12kry42a05";
   };
 
   nativeBuildInputs = [
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
   buildInputs = [
     enchant
     libxml2
-    python
+    python3
     gtksourceview3
     libpeas
     gnome3.adwaita-icon-theme
