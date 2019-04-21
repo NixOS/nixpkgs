004c320cc739e2d594cd4b4aa69e92fd13d12325
Timo Kaufmann <timokau@zoho.com>
2019-04-21 23:52:14 +0200

    python.pkgs.gmpy2: backport upstream bugfixes

--- pkgs/development/python-modules/gmpy2/default.nix
+++ pkgs/development/python-modules/gmpy2/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , buildPythonPackage
 , fetchFromGitHub
+, fetchpatch
 , isPyPy
 , gmp
 , mpfr
@@ -23,6 +24,17 @@ buildPythonPackage {
     rev = "gmpy2-${version}";
     sha256 = "1wg4w4q2l7n26ksrdh4rwqmifgfm32n7x29cgdvmmbv5lmilb5hz";
   };
+
+  patches = [
+    # Backport of two bugfixes (including a segfault):
+    # https://github.com/aleaxit/gmpy/pull/217
+    # https://github.com/aleaxit/gmpy/pull/218
+    (fetchpatch {
+      name = "bugfixes.patch";
+      url = "https://git.sagemath.org/sage.git/plain/build/pkgs/gmpy2/patches/PR217_PR218_conversion_methods.patch?id=b7fbb9a4dac5d6882f6b83a57447dd79ecafb84c";
+      sha256 = "1x3gwvqac36k4ypclxq37fcvi6p790k4xdpm2bj2b3xsvjb80ycz";
+    })
+  ];
 
   buildInputs = [ gmp mpfr libmpc ];
 
