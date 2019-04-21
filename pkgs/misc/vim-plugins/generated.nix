0a43d78ed99ee457ad96ed0c47581460c4f51a49
Malo Bourgon <mbourgon@gmail.com>
2019-04-21 16:29:04 -0700

    vimPlugins.vim-fish: init at 2017-11-22

--- pkgs/misc/vim-plugins/generated.nix
+++ pkgs/misc/vim-plugins/generated.nix
@@ -2553,6 +2553,17 @@ let
     };
   };
 
+  vim-fish = buildVimPluginFrom2Nix {
+    pname = "vim-fish";
+    version = "2017-11-22";
+    src = fetchFromGitHub {
+      owner = "dag";
+      repo = "vim-fish";
+      rev = "50b95cbbcd09c046121367d49039710e9dc9c15f";
+      sha256 = "1yvjlm90alc4zsdsppkmsja33wsgm2q6kkn9dxn6xqwnq4jw5s7h";
+    };
+  };
+
   vim-flagship = buildVimPluginFrom2Nix {
     pname = "vim-flagship";
     version = "2018-08-15";
