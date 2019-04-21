5c50ce7dfde03aa91c504f4a8a966c392540675b
José Romildo Malaquias <malaquias@gmail.com>
2019-04-21 09:02:48 -0300

    mate.mozo: 1.20.2 -> 1.22.0

--- pkgs/desktops/mate/mozo/default.nix
+++ pkgs/desktops/mate/mozo/default.nix
@@ -1,20 +1,20 @@
-{ stdenv, python, fetchurl, pkgconfig, intltool, mate, gtk3, glib, wrapGAppsHook, gobject-introspection }:
+{ stdenv, python3, fetchurl, pkgconfig, intltool, mate, gtk3, glib, wrapGAppsHook, gobject-introspection }:
 
-python.pkgs.buildPythonApplication rec {
+python3.pkgs.buildPythonApplication rec {
   pname = "mozo";
-  version = "1.20.2";
+  version = "1.22.0";
 
   format = "other";
   doCheck = false;
 
   src = fetchurl {
     url = "http://pub.mate-desktop.org/releases/${mate.getRelease version}/${pname}-${version}.tar.xz";
-    sha256 = "1q4hqhigimxav2a8xxyd53lq8q80szsphcv37y2jhm6g6wvdmvhd";
+    sha256 = "0pmkprby10ys1xas9w1xqdm48l3bl82cysyj39n7br9ddijwb7kz";
   };
 
   nativeBuildInputs = [ pkgconfig intltool gobject-introspection wrapGAppsHook ];
 
-  propagatedBuildInputs =  [ mate.mate-menus python.pkgs.pygobject3 ];
+  propagatedBuildInputs =  [ mate.mate-menus python3.pkgs.pygobject3 ];
 
   buildInputs = [ gtk3 glib ];
 
