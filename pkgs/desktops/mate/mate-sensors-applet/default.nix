65740257d600ecfed3db5b18a50aa666a4e4c477
José Romildo Malaquias <malaquias@gmail.com>
2019-04-21 09:02:47 -0300

    mate.mate-sensors-applet: 1.20.3 -> 1.22.0

--- pkgs/desktops/mate/mate-sensors-applet/default.nix
+++ pkgs/desktops/mate/mate-sensors-applet/default.nix
@@ -1,12 +1,12 @@
-{ stdenv, fetchurl, pkgconfig, intltool, itstool, gtk3, libxml2, libxslt, libatasmart, libnotify, dbus-glib, lm_sensors, mate, hicolor-icon-theme, wrapGAppsHook }:
+{ stdenv, fetchurl, pkgconfig, intltool, itstool, gtk3, libxml2, libxslt, libatasmart, libnotify, lm_sensors, mate, hicolor-icon-theme, wrapGAppsHook }:
 
 stdenv.mkDerivation rec {
   name = "mate-sensors-applet-${version}";
-  version = "1.20.3";
+  version = "1.22.0";
 
   src = fetchurl {
     url = "http://pub.mate-desktop.org/releases/${mate.getRelease version}/${name}.tar.xz";
-    sha256 = "0s98qy3jkri9zh5xqffprqd00cqspaq9av0mcrcakjkl8wyfh2g6";
+    sha256 = "0wf07ppzdg9ql4limnq5vd2c2imyww9dy477hcgxl4vfw45vy19k";
   };
 
   nativeBuildInputs = [
@@ -23,7 +23,6 @@ stdenv.mkDerivation rec {
     libxslt
     libatasmart
     libnotify
-    dbus-glib
     lm_sensors
     mate.mate-panel
     hicolor-icon-theme
