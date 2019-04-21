3e4f14a50b3e475ec00fa121d8884d15edc69de8
Matthew Bauer <mjbauer95@gmail.com>
2019-04-21 16:20:31 -0400

    perl: disable test on TAPParserSourceHandlerpgTAP
    Seem to be broken with newest llvm

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -14483,6 +14483,7 @@ let
       url = "mirror://cpan/authors/id/D/DW/DWHEELER/${name}.tar.gz";
       sha256 = "1655337l1cyd7q62007wrk87q2gbbwfq9xjy1wgx3hyflxpkkvl4";
     };
+    doCheck = !stdenv.isDarwin;
     meta = {
       description = "Stream TAP from pgTAP test scripts";
       license = with stdenv.lib.licenses; [ artistic1 gpl1Plus ];
