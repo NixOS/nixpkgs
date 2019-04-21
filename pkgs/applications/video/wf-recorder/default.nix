1f843c9e371cb3b5c64288d1b245568d7a7ee425
CrazedProgrammer <crazedprogrammer@gmail.com>
2019-04-21 15:34:40 +0200

    wf-recorder: unstable-2019-03-12 -> unstable-2019-04-21

--- pkgs/applications/video/wf-recorder/default.nix
+++ pkgs/applications/video/wf-recorder/default.nix
@@ -1,18 +1,18 @@
-{ stdenv, fetchFromGitHub, meson, ninja, pkgconfig, wayland, wayland-protocols, ffmpeg, x264 }:
+{ stdenv, fetchFromGitHub, meson, ninja, pkgconfig, wayland, wayland-protocols, ffmpeg, x264, libpulseaudio }:
 
 stdenv.mkDerivation rec {
   pname = "wf-recorder";
-  version = "unstable-2019-03-12";
+  version = "unstable-2019-04-21";
 
   src = fetchFromGitHub {
     owner = "ammen99";
     repo = pname;
-    rev = "e6ea77a2569c04975cab8655f5ad4dbcf86df1f5";
-    sha256 = "1jhj5syzy8i8f9b3j4g12jmc5fcsiv4df9hgribdvw61v5pfz9g1";
+    rev = "ddb96690556371007e316577ed1b14f0cb62e13c";
+    sha256 = "04amfd1kyklcj6nmmmf21dz333ykglvhxb3cbzak06v2fxlrp2w3";
   };
 
   nativeBuildInputs = [ meson ninja pkgconfig ];
-  buildInputs = [ wayland wayland-protocols ffmpeg x264 ];
+  buildInputs = [ wayland wayland-protocols ffmpeg x264 libpulseaudio ];
 
   meta = with stdenv.lib; {
     description = "Utility program for screen recording of wlroots-based compositors";
