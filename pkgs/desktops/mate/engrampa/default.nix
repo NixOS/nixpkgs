3777908cd6f0ad214f234222ab45c62c4d8394fe
José Romildo Malaquias <malaquias@gmail.com>
2019-04-21 09:02:45 -0300

    mate.engrampa: 1.20.2 -> 1.22.0

--- pkgs/desktops/mate/engrampa/default.nix
+++ pkgs/desktops/mate/engrampa/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "engrampa-${version}";
-  version = "1.20.2";
+  version = "1.22.0";
 
   src = fetchurl {
     url = "http://pub.mate-desktop.org/releases/${mate.getRelease version}/${name}.tar.xz";
-    sha256 = "0fj957dfagw6p7mq5545h9j2w3hv18yqnkpypnr719r4g13d3f2v";
+    sha256 = "16yjplfl2sqa7n6404hjn0vwkh0xkdch73q7n5czynihmh3azc7p";
   };
 
   nativeBuildInputs = [
