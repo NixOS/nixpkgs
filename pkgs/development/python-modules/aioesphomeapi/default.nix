59b1cdba311f8563bc954d9b8b164d4a54e8456a
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2019-04-22 23:35:37 +0200

    python3.pkgs.aioesphomeapi: 1.8.0 -> 2.0.1
    Pin the protobuf version used by Home Assistant to the version
    required by aioesphomeapi and some components.

--- pkgs/development/python-modules/aioesphomeapi/default.nix
+++ pkgs/development/python-modules/aioesphomeapi/default.nix
@@ -1,12 +1,14 @@
-{ lib, buildPythonPackage, fetchPypi, attrs, protobuf, zeroconf }:
+{ lib, buildPythonPackage, fetchPypi, isPy3k, attrs, protobuf, zeroconf }:
 
 buildPythonPackage rec {
   pname = "aioesphomeapi";
-  version = "1.8.0";
+  version = "2.0.1";
+
+  disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "16ywa7yggmsx8m2r9azdq7w9fxjh736g1vd1aibgh24g7srhwwhj";
+    sha256 = "db09e34dfc148279f303481c7da94b84c9b1442a41794f039c31253e81a58ffb";
   };
 
   propagatedBuildInputs = [ attrs protobuf zeroconf ];
@@ -19,5 +21,9 @@ buildPythonPackage rec {
     homepage = https://github.com/esphome/aioesphomeapi;
     license = licenses.mit;
     maintainers = with maintainers; [ dotlambda ];
+
+    # Home Assistant should pin protobuf to the correct version. Can be tested using
+    #     nix-build -E "with import ./. {}; home-assistant.override { extraPackages = ps: [ ps.aioesphomeapi ]; }"
+    broken = !lib.hasPrefix "3.6.1" protobuf.version;
   };
 }
