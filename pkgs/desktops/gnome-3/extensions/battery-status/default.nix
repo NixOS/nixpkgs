ec6c59bc7768863d36459c317f39a98c06b3a159
Jon Banafato <jon@jonafato.com>
2019-04-21 20:46:29 -0400

    gnomeExtensions.battery-status: mark as not broken
    This extension does not declare support for GNOME 3.32 (or any other
    recent version), but it does work. I've opened an [upstream pull
    request] to address this. Additionally, add `meta.platforms` and update
    the homepage and GitHub owner to reflect the user that `milliburn`
    redirects to.
    
    [upstream pull request]: https://github.com/roberth-k/gnome-shell-extension-battery_status/pull/18

--- pkgs/desktops/gnome-3/extensions/battery-status/default.nix
+++ pkgs/desktops/gnome-3/extensions/battery-status/default.nix
@@ -1,15 +1,19 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv, fetchFromGitHub, gnome3 }:
 
 stdenv.mkDerivation rec {
   name = "gnome-shell-extension-battery-status-${version}";
   version = "6";
 
   src = fetchFromGitHub {
-    owner = "milliburn";
+    owner = "roberth-k";
     repo = "gnome-shell-extension-battery_status";
     rev = "v${version}";
     sha256 = "1w83h863mzffjnmk322xq90qf3y9dzay1w9yw5r0qnbsq1ljl8p4";
   };
+
+  patches = [
+    ./fix-versions.patch
+  ];
 
   uuid = "battery_status@milliburn.github.com";
 
@@ -21,8 +25,8 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     description = "Configurable lightweight battery charge indicator and autohider";
     license = licenses.gpl2;
-    broken = true; # not compatable with latest GNOME
     maintainers = with maintainers; [ jonafato ];
-    homepage = https://github.com/milliburn/gnome-shell-extension-battery_status;
+    platforms = gnome3.gnome-shell.meta.platforms;
+    homepage = https://github.com/roberth-k/gnome-shell-extension-battery_status;
   };
 }
