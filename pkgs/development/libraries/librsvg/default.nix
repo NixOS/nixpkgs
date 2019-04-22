43d76e32323ad35d44e55c724a282c5a7cfd0d7d
Matthew Bauer <mjbauer95@gmail.com>
2019-04-21 22:04:12 -0400

    Revert "librsvg: 2.44.12 -> 2.45.5"
    This reverts commit 43f21c58eafd62d20138f9a6a8286f54d3db8d0b.

--- pkgs/development/libraries/librsvg/default.nix
+++ pkgs/development/libraries/librsvg/default.nix
@@ -5,14 +5,14 @@
 
 let
   pname = "librsvg";
-  version = "2.45.5";
+  version = "2.44.12";
 in
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "001phhq3dd9i6mkbjnmw468vjlqvs1330nwdzldy1rcgc3f74230";
+    sha256 = "1h3qnqhr0l7pd2bxg69ki6ckl4srdwgr471dpp4jq9i4784hp0v6";
   };
 
   outputs = [ "out" "dev" "installedTests" ];
