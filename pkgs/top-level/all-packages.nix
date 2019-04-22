ecb90aedecbd3f802908f2b6742efbc32413fd20
Roosembert Palacios <roosembert.palacios@epfl.ch>
2019-04-22 16:14:12 +0200

    Add package for libguesfs containing libguestfs appliances
    Appliances are necessary when building images using virt-builder.
    (e.g. `virt-builder -o debian-9.img debian-9`)
    
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11248,6 +11248,8 @@ in
 
   libguestfs-appliance = callPackage ../development/libraries/libguestfs/appliance.nix {};
   libguestfs = callPackage ../development/libraries/libguestfs { };
+  libguestfs-with-appliance = libguestfs.override { appliance = libguestfs-appliance; };
+
 
   libhangul = callPackage ../development/libraries/libhangul { };
 
