NixOS/nixpkgs

commit 6b2bd330fa39376fe5143d8095b9a1b925255110

wordgrinder: Fix sha256 hash
The hash used for fetchFromGitHub was on the archive itself, however
fetchFromGitHub is actually a wrapper of fetchzip:

hash mismatch in fixed-output derivation '...-source':
  wanted: sha256:1zqx3p9l22njni44ads3fyw3xh6807wmb5k1x2glg61z81cwc6sf
  got:    sha256:08lnq5wmspfqdjmqm15gizcq0xr7mg4h62qhvwj63v0sd6ks1cal

When using nix-prefetch-url without the --unpack argument, I get:

1zqx3p9l22njni44ads3fyw3xh6807wmb5k1x2glg61z81cwc6sf

And with the --unpack argument:

08lnq5wmspfqdjmqm15gizcq0xr7mg4h62qhvwj63v0sd6ks1cal

Just to be 100% sure the contents are the same, I also diffed the
contents of the former tarball with the outputs of the unpacked dir and
they're the same.

I didn't actually test the package (which doesn't build right now
anyway), but now at least the source checksum is fixed :-)

Signed-off-by: aszlig <aszlig@nix.build>
Cc: @matthiasbeyer

pkgs/applications/office/wordgrinder/default.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
     repo = "wordgrinder";
     owner = "davidgiven";
     rev = "${version}";
-    sha256 = "1zqx3p9l22njni44ads3fyw3xh6807wmb5k1x2glg61z81cwc6sf";
+    sha256 = "08lnq5wmspfqdjmqm15gizcq0xr7mg4h62qhvwj63v0sd6ks1cal";
   };
 
   makeFlags = [

commit 45841f55d0cb5891f3278fba35266ea1eb0e9016

wordgrinder: Fix build
First I tried to fix the build like this:

> This fixes the build of the wordgrinder package by disabling the build
> of "xwordgrinder", the non-curses frontend of wordgrinder.
>
> For re-enabling the build, this patch shall be reverted and then go on
> from there.
>
> I opted for disabling the x frontend of this program because the whole
> purpose of this is to use a curses writing program (at least for me).
> It seems that hacking around the new build system in nixpkgs is a
> non-trivial task and I wanted to get this package out of the door as
> fast as possible because I already delayed the fix by a few days (or is
> it weeks already), so this is the simple fix.
>
> If someone wants to have the X frontend again, I can invest more time at
> some point.

But then the almighty aszlig came around and told me how to do it right. So here
we go, with a patch from me that is acutally from aszlig (see #60122).

This fixes the build.

Suggested-by: @aszlig
Signed-off-by: Matthias Beyer <mail@beyermatthias.de>
Fixes: 6b2bd330fa ("wordgrinder: Fix sha256 hash")
Fixes: 19a3abc1b8 ("wordgrinder: 0.7.1 -> 0.7.2")
CC: @aszlig
CC: @devhell

pkgs/applications/office/wordgrinder/default.nix
@@ -16,7 +16,11 @@ stdenv.mkDerivation rec {
     "PREFIX=$(out)"
     "LUA_INCLUDE=${lua52Packages.lua}/include"
     "LUA_LIB=${lua52Packages.lua}/lib/liblua.so"
-  ] ++ stdenv.lib.optional stdenv.isLinux "XFT_PACKAGE=--libs=\{-lX11 -lXft\}";
+  ];
+
+  preBuild = stdenv.lib.optionalString stdenv.isLinux ''
+    makeFlagsArray+=('XFT_PACKAGE=--cflags={} --libs={-lX11 -lXft}')
+  '';
 
   dontUseNinjaBuild = true;
   dontUseNinjaInstall = true;