9bd8e0de3788f7c333641304b4d91e825e849003
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-04-23 03:29:41 +0200

    egl-wayland: init at 1.1.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11023,6 +11023,8 @@ in
     inherit (darwin.apple_sdk.frameworks) Accelerate CoreGraphics CoreVideo;
   };
 
+  egl-wayland = callPackage ../development/libraries/egl-wayland {};
+
   elastix = callPackage ../development/libraries/science/biology/elastix { };
 
   enchant1 = callPackage ../development/libraries/enchant/1.x.nix { };
