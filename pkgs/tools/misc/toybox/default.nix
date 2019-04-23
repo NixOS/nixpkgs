724e833ea2a9d1b16f7289e1244d0d7ff56d027b
Matthew Bauer <mjbauer95@gmail.com>
2019-04-22 22:36:35 -0400

    treewide: disable -Werror for llvm 7
    Some of these have errors on newest llvm. It’s easiest to just add
    -Wno-error in these cases.

--- pkgs/tools/misc/toybox/default.nix
+++ pkgs/tools/misc/toybox/default.nix
@@ -54,6 +54,8 @@ stdenv.mkDerivation rec {
   checkInputs = [ which ]; # used for tests with checkFlags = [ "DEBUG=true" ];
   checkTarget = "tests";
 
+  NIX_CFLAGS_COMPILE = "-Wno-error";
+
   meta = with stdenv.lib; {
     description = "Lightweight implementation of some Unix command line utilities";
     homepage = https://landley.net/toybox/;
