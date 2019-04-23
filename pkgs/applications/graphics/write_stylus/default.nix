74c1f53745964b15450026f27f593dbfd1129f39
Oyren <m.scheuren@oyra.eu>
2019-04-23 14:35:00 +0200

    write_stylus: removed desktop icons

--- pkgs/applications/graphics/write_stylus/default.nix
+++ pkgs/applications/graphics/write_stylus/default.nix
@@ -7,7 +7,6 @@ stdenv.mkDerivation rec {
     name = "Write";
     exec = "Write";
     comment = "A word processor for handwriting";
-    icon = "write_stylus";
     desktopName = "Write";
     genericName = "Write";
     categories = "Office;Graphics";
@@ -16,13 +15,6 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "http://www.styluslabs.com/write/write${version}.tar.gz";
     sha256 = "1p6glp4vdpwl8hmhypayc4cvs3j9jfmjfhhrgqm2xkgl5bfbv2qd";
-  };
-
-  # taken from: https://www.iconfinder.com/icons/50835/edit_pencil_write_icon
-  # license: Free for commercial use
-  icon = fetchurl {
-    url = "https://oyra.eu/write/icon.tar.gz";
-    sha256 = "1zd98g63apwi17qc1hm1g14maain5d18g4afadxm30qjz2s0mvs8";
   };
 
   sourceRoot = ".";
@@ -34,9 +26,6 @@ stdenv.mkDerivation rec {
     cp -R Write $out/
     # symlink the binary to bin/
     ln -s $out/Write/Write $out/bin/Write
-
-    # untar icons
-    tar -xzf ${icon} *.tar.gz -C $out/
 
     mkdir -p $out/share/applications
     ln -s ${desktopItem}/share/applications/* $out/share/applications/
