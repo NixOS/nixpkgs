3164e991082001488137adedcb863078b302b1be
Mario Rodas <marsam@users.noreply.github.com>
2019-04-24 00:11:00 -0500

    pythonPackages.pyev: drop
    pyev is not available in PyPI anymore

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3801,8 +3801,6 @@ in {
 
   pyenchant = callPackage ../development/python-modules/pyenchant { };
 
-  pyev = callPackage ../development/python-modules/pyev { };
-
   pyexcelerator = callPackage ../development/python-modules/pyexcelerator { };
 
   pyext = callPackage ../development/python-modules/pyext { };
