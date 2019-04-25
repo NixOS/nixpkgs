4d667685c24c3e99cc70156f3172d5ed5f2ceaa6
Linus Heckemann <git@sphalerite.org>
2019-04-26 00:33:40 +0200

    nixos/kexec: don't clobber existing kexec-loaded kernel

--- nixos/modules/system/boot/kexec.nix
+++ nixos/modules/system/boot/kexec.nix
@@ -13,6 +13,9 @@
         path = [ pkgs.kexectools ];
         script =
           ''
+            # Don't load the current system profile if we already have a kernel loaded
+            [[ 1 = "$(</sys/kernel/kexec_loaded)" ]] && exit
+
             p=$(readlink -f /nix/var/nix/profiles/system)
             if ! [ -d $p ]; then exit 1; fi
             exec kexec --load $p/kernel --initrd=$p/initrd --append="$(cat $p/kernel-params) init=$p/init"
