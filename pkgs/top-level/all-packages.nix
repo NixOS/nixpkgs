d287075c22fbcc9e56ae7c9896cd65922936a73a
ajs124 <git@ajs124.de>
2019-04-25 10:07:42 +0200

    evopedia: remove package
    The last upstream update was in 2014. While the software might still work, its usefulness depends on data archives, which are not updated anymore and have not been for several years.
    
    Upstream suggests to use kiwix instead.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17388,8 +17388,6 @@ in
 
   evilvte = callPackage ../applications/misc/evilvte (config.evilvte or {});
 
-  evopedia = callPackage ../applications/misc/evopedia { };
-
   exercism = callPackage ../applications/misc/exercism { };
 
   go-motion = callPackage ../development/tools/go-motion { };
