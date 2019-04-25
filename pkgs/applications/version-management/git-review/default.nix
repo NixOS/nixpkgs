4c7460e20feecbde918476552d297b5d908dcc0a
Renaud <c0bw3b@users.noreply.github.com>
2019-04-25 23:28:24 +0200

    git-review: refactor
    - do not use fetchurl on mutable GitHub archives
    - postPatch is unneeded as argparse has been removed from requirements.txt upstream

--- pkgs/applications/version-management/git-review/default.nix
+++ pkgs/applications/version-management/git-review/default.nix
@@ -1,20 +1,18 @@
-{ stdenv, fetchurl, pythonPackages} :
+{ stdenv, fetchFromGitHub, pythonPackages} :
 
 pythonPackages.buildPythonApplication rec {
-  name = "git-review-${version}";
+  pname = "git-review";
   version = "1.28.0";
 
   # Manually set version because prb wants to get it from the git
   # upstream repository (and we are installing from tarball instead)
   PBR_VERSION = "${version}";
 
-  postPatch = ''
-    sed -i -e '/argparse/d' requirements.txt
-  '';
-
-  src = fetchurl rec {
-    url = "https://github.com/openstack-infra/git-review/archive/${version}.tar.gz";
-    sha256 = "14k003n8n0rb56qqs9a5jl02kg9x9ziapviw1hn8cql5y6mxysv3";
+  src = fetchFromGitHub rec {
+    owner = "openstack-infra";
+    repo = pname;
+    rev = version;
+    sha256 = "1hgw1dkl94m3idv4izc7wf2j7al2c7nnsqywy7g53nzkv9pfv47s";
   };
 
   propagatedBuildInputs = with pythonPackages; [ pbr requests setuptools ];
