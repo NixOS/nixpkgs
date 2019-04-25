361a138f5ad390996f6cda8d9c69e9457b81a4cb
Patrick Hilhorst <git@hilhorst.be>
2019-04-25 14:59:20 +0200

    pandoc-imagine: use full commit hash

--- pkgs/tools/misc/pandoc-imagine/default.nix
+++ pkgs/tools/misc/pandoc-imagine/default.nix
@@ -7,7 +7,7 @@ buildPythonApplication rec {
   src = fetchFromGitHub {
     repo = "imagine";
     owner = "hertogp";
-    rev = "cc9be85";
+    rev = "cc9be85155666c2d12d47a71690ba618cea1fac2";
     sha256 = "0iksh9081g488yfjzd24bz4lm1nrrjamph1vynx3imrcfgyq7nsb";
   };
 
