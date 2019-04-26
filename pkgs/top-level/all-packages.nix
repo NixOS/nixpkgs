111412e1db3a80da37883c4664749875452a536e
Stanislas <angristan@pm.me>
2019-04-26 14:24:34 +0200

    Add minecraft-server_1_14 in all-packages.nix
    (cherry picked from commit df792914a33de1134611410ba69baaff71f46f9a)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20895,9 +20895,10 @@ in
 
   minecraft = callPackage ../games/minecraft { };
 
-  minecraft-server = minecraft-server_1_13_2;
+  minecraft-server = minecraft-server_1_14;
 
   inherit (callPackages ../games/minecraft-server { })
+    minecraft-server_1_14
     minecraft-server_1_13_2
     minecraft-server_1_12_2;
 
