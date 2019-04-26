356010e0a9060d5ed36abee56fd1c2c8c325482b
Stanislas <angristan@pm.me>
2019-04-26 14:01:23 +0200

    Add minecraft-server_1_14
    (cherry picked from commit e6e3673e5b06f4fbb596c365ed9354dfcc64e332)

--- pkgs/games/minecraft-server/default.nix
+++ pkgs/games/minecraft-server/default.nix
@@ -36,6 +36,12 @@ let
     });
 
 in {
+  minecraft-server_1_14 = common {
+    version = "1.14";
+    url    = "https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar";
+    sha256 = "671e3d334dd601c520bf1aeb96e49038145172bef16bc6c418e969fd8bf8ff6c";
+  };
+
   minecraft-server_1_13_2 = common {
     version = "1.13.2";
     url    = "https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar";
