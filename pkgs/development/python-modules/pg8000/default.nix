a416ef58b773b90ae269c2cbed898f757c230c76
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2019-04-27 07:47:38 +0200

    python.pkgs.pg8000: does not support Python 2.7 (#60220)
    * python.pkgs.pg8000: does not support Python 2.7
    
    * python3.pkgs.pg8000: update propagatedBuildInputs

--- pkgs/development/python-modules/pg8000/default.nix
+++ pkgs/development/python-modules/pg8000/default.nix
@@ -1,20 +1,22 @@
 { stdenv
 , buildPythonPackage
 , fetchPypi
-, pytz
-, six
+, isPy3k
+, passlib
 }:
 
 buildPythonPackage rec {
   pname = "pg8000";
   version = "1.13.1";
 
+  disabled = !isPy3k;
+
   src = fetchPypi {
     inherit pname version;
     sha256 = "2208c7aaffe8d61f5c4ccbefeb74ba033003899e64aee37c0eb98aadae8b9c6b";
   };
 
-  propagatedBuildInputs = [ pytz six ];
+  propagatedBuildInputs = [ passlib ];
 
   meta = with stdenv.lib; {
     homepage = https://github.com/tlocke/pg8000;
