3dc4e55ffcad3e966ac5007538b59dac3fc849d5
nyanloutre <paul@nyanlout.re>
2019-04-27 14:54:20 +0200

    wine-mono: 4.8.2 -> 4.8.3

--- pkgs/misc/emulators/wine/sources.nix
+++ pkgs/misc/emulators/wine/sources.nix
@@ -31,9 +31,9 @@ in rec {
 
     ## see http://wiki.winehq.org/Mono
     mono = fetchurl rec {
-      version = "4.8.2";
+      version = "4.8.3";
       url = "http://dl.winehq.org/wine/wine-mono/${version}/wine-mono-${version}.msi";
-      sha256 = "1hvwhqdb11j9yzhhw86fjhj9bawg76zrh0wnx658pn5xghhk2jhy";
+      sha256 = "0xhavcjwwr21am3bxp2cxlvykwasw8y4g8p470j5fg7skc0izynn";
     };
   };
 
