d5e4f8a73723c116c1264445e68da36ddc95e2fc
Michael Raskin <7c6f434c@mail.ru>
2019-04-28 15:25:10 +0200

    Add a changelog link to Hello

--- pkgs/applications/misc/hello/default.nix
+++ pkgs/applications/misc/hello/default.nix
@@ -18,6 +18,7 @@ stdenv.mkDerivation rec {
       It is fully customizable.
     '';
     homepage = https://www.gnu.org/software/hello/manual/;
+    changelog = "http://git.savannah.gnu.org/cgit/hello.git/tree/NEWS?h=v${version}";
     license = licenses.gpl3Plus;
     maintainers = [ maintainers.eelco ];
     platforms = platforms.all;
