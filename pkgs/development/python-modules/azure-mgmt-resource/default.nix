707a567ebdcb0e6585d95bc5cd93d1cd3dd2dfef
Max Wilson <max.wilson@insight.com>
2019-04-29 17:25:41 -0400

    pythonPackages.azure-mgmt-resource: refactor
    Removed doCheck and updated metadata

--- pkgs/development/python-modules/azure-mgmt-resource/default.nix
+++ pkgs/development/python-modules/azure-mgmt-resource/default.nix
@@ -3,6 +3,7 @@
 , fetchPypi
 , python
 , azure-mgmt-common
+, isPy3k
 }:
 
 
@@ -16,17 +17,20 @@ buildPythonPackage rec {
     sha256 = "aef8573066026db04ed3e7c5e727904e42f6462b6421c2e8a3646e4c4f8128be";
   };
 
-  postInstall = ''
+  postInstall = if isPy3k then "" else ''
     echo "__import__('pkg_resources').declare_namespace(__name__)" >> "$out/lib/${python.libPrefix}"/site-packages/azure/__init__.py
     echo "__import__('pkg_resources').declare_namespace(__name__)" >> "$out/lib/${python.libPrefix}"/site-packages/azure/mgmt/__init__.py
   '';
 
   propagatedBuildInputs = [ azure-mgmt-common ];
 
+  # has no tests
+  doCheck = false;
+
   meta = with pkgs.lib; {
     description = "Microsoft Azure SDK for Python";
-    homepage = "https://azure.microsoft.com/en-us/develop/python/";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ olcai ];
+    homepage = https://docs.microsoft.com/en-us/python/api/overview/azure/resources?view=azure-python;
+    license = licenses.mit;
+    maintainers = with maintainers; [ olcai mwilsoninsight ];
   };
 }
