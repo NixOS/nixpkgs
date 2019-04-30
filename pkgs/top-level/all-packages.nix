a670a8dbb1bc045da95673d3916bfac2f361b316
wucke13 <wucke13@gmail.com>
2019-04-30 11:11:59 +0200

    apmplanner-2: 2.0.26 -> 2.0.27-rc1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22833,7 +22833,7 @@ in
 
   ### SCIENCE/ROBOTICS
 
-  apmplanner2 = libsForQt59.callPackage ../applications/science/robotics/apmplanner2 { };
+  apmplanner2 = libsForQt511.callPackage ../applications/science/robotics/apmplanner2 { };
 
   ### MISC
 
