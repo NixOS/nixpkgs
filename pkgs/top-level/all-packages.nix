b775493d239e52269bc8be32644a73ba39952b00
Will Dietz <w@wdtz.org>
2019-04-29 20:04:17 -0500

    obfs4: init at 0.0.10

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4785,6 +4785,8 @@ in
 
   obexd = callPackage ../tools/bluetooth/obexd { };
 
+  obfs4 = callPackage ../tools/networking/obfs4 { };
+
   oci-image-tool = callPackage ../tools/misc/oci-image-tool { };
 
   ocproxy = callPackage ../tools/networking/ocproxy { };
