f1ad53b267902e378db433e7bb6b5aec65bfcf78
worldofpeace <worldofpeace@users.noreply.github.com>
2019-04-30 13:58:40 -0400

    libnotify: fix darwin build
    Patch is upstream MR courtesy of jtojnar.

--- pkgs/development/libraries/libnotify/default.nix
+++ pkgs/development/libraries/libnotify/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, meson, ninja, pkgconfig
+{ stdenv, fetchurl, meson, ninja, pkgconfig, fetchpatch
 , glib, gdk_pixbuf, gobject-introspection, gnome3 }:
 
 stdenv.mkDerivation rec {
@@ -9,6 +9,15 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
     sha256 = "1371csx0n92g60b5dmai4mmzdnx8081mc3kcgc6a0xipcq5rw839";
   };
+
+  patches = [
+    # Fix darwin build
+    # https://gitlab.gnome.org/GNOME/libnotify/merge_requests/9
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/libnotify/commit/55eb69247fe2b479ea43311503042fc03bf4e67d.patch";
+      sha256 = "1hlb5b7c5axiyir1i5j2pi94bm2gyr1ybkp6yaqy7yk6iiqlvv50";
+    })
+  ];
 
   mesonFlags = [
     # disable tests as we don't need to depend on gtk+(2/3)
