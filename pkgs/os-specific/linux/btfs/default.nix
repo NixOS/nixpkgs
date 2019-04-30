0031c72b80ef6d55d4a23a9fa929ed56ed715a8f
R. RyanTM <ryantm-bot@ryantm.com>
2019-04-30 04:52:42 -0700

    btfs: 2.18 -> 2.19
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/btfs/versions

--- pkgs/os-specific/linux/btfs/default.nix
+++ pkgs/os-specific/linux/btfs/default.nix
@@ -3,13 +3,13 @@
 
 stdenv.mkDerivation rec {
   name = "btfs-${version}";
-  version = "2.18";
+  version = "2.19";
 
   src = fetchFromGitHub {
     owner  = "johang";
     repo   = "btfs";
     rev    = "v${version}";
-    sha256 = "1cn21bxx43iqvac6scmwhkw0bql092sl48r6qfidbmhbw30xl5yf";
+    sha256 = "1b58zqha2hpnk4ysp9870wd9pkyy6l106ghp0z0x655q70npj0wn";
   };
 
   nativeBuildInputs = [ pkgconfig ];
