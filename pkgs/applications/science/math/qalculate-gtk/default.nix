1d5a0ffbfc29a7c22c6b2b295c9d6985839309d8
Gabriel Ebner <gebner@gebner.org>
2019-05-01 10:28:04 +0200

    qalculate-gtk: 3.0.0 -> 3.1.0

--- pkgs/applications/science/math/qalculate-gtk/default.nix
+++ pkgs/applications/science/math/qalculate-gtk/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "qalculate-gtk";
-  version = "3.0.0";
+  version = "3.1.0";
 
   src = fetchFromGitHub {
     owner = "qalculate";
     repo = "qalculate-gtk";
     rev = "v${version}";
-    sha256 = "00q6y9dgg9wgpgks79snbipn8alfjajlx02a5hm7wl9a20zd0b81";
+    sha256 = "1ayhzsxf2qgs4j1j0xl942ysfy3zl2z1fp7l2s5nh5awr41mhnqc";
   };
 
   patchPhase = ''
