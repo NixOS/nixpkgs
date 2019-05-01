7909ed1dee47fa154c63937bbf2c0351fe9bbdc8
Timo Kaufmann <timokau@zoho.com>
2019-05-01 13:26:05 +0200

    python.pkgs.pyasn1-modules: fix python2 tests (#60618)
    Broken by the update in e03d55130fbf50563322fe5ad6fe76fe17a508ac.

--- pkgs/development/python-modules/pyasn1-modules/default.nix
+++ pkgs/development/python-modules/pyasn1-modules/default.nix
@@ -1,4 +1,10 @@
-{ stdenv, buildPythonPackage, fetchPypi, pyasn1, isPyPy }:
+{ stdenv
+, buildPythonPackage
+, fetchPypi
+, pyasn1
+, isPyPy
+, pytest
+}:
 
 buildPythonPackage rec {
   pname = "pyasn1-modules";
@@ -10,6 +16,16 @@ buildPythonPackage rec {
   };
 
   propagatedBuildInputs = [ pyasn1 ];
+
+  checkInputs = [
+    pytest
+  ];
+
+  # running tests through setup.py fails only for python2 for some reason:
+  # AttributeError: 'module' object has no attribute 'suitetests'
+  checkPhase = ''
+    py.test
+  '';
 
   meta = with stdenv.lib; {
     description = "A collection of ASN.1-based protocols modules";
