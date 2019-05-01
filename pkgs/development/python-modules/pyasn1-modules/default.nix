NixOS/nixpkgs

commit e03d55130fbf50563322fe5ad6fe76fe17a508ac

python: pyasn1-modules: 0.2.4 -> 0.2.5

pkgs/development/python-modules/pyasn1-modules/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "pyasn1-modules";
-  version = "0.2.4";
+  version = "0.2.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a52090e8c5841ebbf08ae455146792d9ef3e8445b21055d3a3b7ed9c712b7c7c";
+    sha256 = "ef721f68f7951fab9b0404d42590f479e30d9005daccb1699b0a51bb4177db96";
   };
 
   propagatedBuildInputs = [ pyasn1 ];

commit b79ecf4142a6502fc9cbc823dfe66a46f8e2ef96

python.pkgs.pyasn1-modules: fix python2 tests
Broken by the update in e03d55130fbf50563322fe5ad6fe76fe17a508ac.

pkgs/development/python-modules/pyasn1-modules/default.nix
@@ -1,4 +1,10 @@
-{ stdenv, buildPythonPackage, fetchPypi, pyasn1, isPyPy }:
+{ stdenv
+, buildPythonPackage
+, fetchPypi
+, pyasn1
+, isPyPy
+, pytest
+}:
 
 buildPythonPackage rec {
   pname = "pyasn1-modules";
@@ -10,6 +16,16 @@ buildPythonPackage rec {
   };
 
   propagatedBuildInputs = [ pyasn1 ];
+
+  checkInputs = [
+    pytest
+  ];
+
+  # running tests through setup.py fails only for python2 for some reason:
+  # AttributeError: 'module' object has no attribute 'suitetests'
+  checkPhase = ''
+    py.test
+  '';
 
   meta = with stdenv.lib; {
     description = "A collection of ASN.1-based protocols modules";