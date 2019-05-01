e259c9782a1d448acb5a89296debc8e87da50a72
George Lesica <george@lesica.com>
2019-05-01 02:47:35 -0600

    dart: 2.0.0 -> 2.2.0
    New versions of Dart are available that contain substantial
    improvements.

--- pkgs/development/interpreters/dart/default.nix
+++ pkgs/development/interpreters/dart/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, unzip, version ? "2.0.0" }:
+{ stdenv, fetchurl, unzip, version ? "2.2.0" }:
 
 let
 
@@ -40,6 +40,38 @@ let
     "2.0.0-dev.26.0-i686-linux" = fetchurl {
       url = "${dev}/${version}/sdk/dartsdk-linux-ia32-release.zip";
       sha256 = "83ba8b64c76f48d8de4e0eb887e49b7960053f570d27e7ea438cc0bac789955e";
+    };
+    "2.1.0-x86_64-linux" = fetchurl {
+      url = "${stable}/${version}/sdk/dartsdk-linux-x64-release.zip";
+      sha256 = "24673028fbc506dbded6c273b3f5fb8f0f169bd900083d0113ddd5ce0a8adcb6";
+    };
+    "2.1.0-i686-linux" = fetchurl {
+      url = "${stable}/${version}/sdk/dartsdk-linux-ia32-release.zip";
+      sha256 = "db84d4fe7671f61d95797362b63e0588a721d8a3086d77517f4e1619e1a9318c";
+    };
+    "2.1.1-x86_64-linux" = fetchurl {
+      url = "${stable}/${version}/sdk/dartsdk-linux-x64-release.zip";
+      sha256 = "b223f095e2eb836481b6d5041d23a627745f0b45f70f9ce31cc1fbc68e9a9f90";
+    };
+    "2.1.1-i686-linux" = fetchurl {
+      url = "${stable}/${version}/sdk/dartsdk-linux-ia32-release.zip";
+      sha256 = "8c7d359f00f3569dffd9d02fc213cd895a5c3e524d386cf65c89c2373630ca7e";
+    };
+    "2.2.0-x86_64-linux" = fetchurl {
+      url = "${stable}/${version}/sdk/dartsdk-linux-x64-release.zip";
+      sha256 = "89777ceba8227d4dad6081c44bc70d301a259f3c2fdb4c1391961e376ec3af68";
+    };
+    "2.2.0-i686-linux" = fetchurl {
+      url = "${stable}/${version}/sdk/dartsdk-linux-ia32-release.zip";
+      sha256 = "d6d5edab837301bde218c97b074af8390d5dbe00a99961605159fa9e53609b81";
+    };
+    "2.3.0-dev.0.3-x86_64-linux" = fetchurl {
+      url = "${dev}/${version}/sdk/dartsdk-linux-x64-release.zip";
+      sha256 = "5feebfd7ba3274222e22f1d1990883b6c3acaf0e26eae057a4a8667a5c9041cd";
+    };
+    "2.3.0-dev.0.3-i686-linux" = fetchurl {
+      url = "${dev}/${version}/sdk/dartsdk-linux-ia32-release.zip";
+      sha256 = "41cba9b8ef03c7f8d2ec3f54aedf7ac1eb7ad379871fc69d23060e94653d572e";
     };
   };
 
