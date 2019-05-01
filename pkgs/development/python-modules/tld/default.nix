7a22dc48156a8db600240cdfd617f1ca1667a1b6
Florian Klink <flokli@flokli.de>
2019-05-01 23:56:49 +0200

    pythonPackages.tld: enable tests

--- pkgs/development/python-modules/tld/default.nix
+++ pkgs/development/python-modules/tld/default.nix
@@ -1,6 +1,6 @@
-{ stdenv, fetchPypi, buildPythonPackage, six }:
+{ stdenv, fetchPypi, python }:
 
-buildPythonPackage rec {
+python.pkgs.buildPythonPackage rec {
   pname   = "tld";
   version = "0.9.3";
 
@@ -9,8 +9,20 @@ buildPythonPackage rec {
     sha256 = "0i0prgwrmm157h6fa5bx9wm0m70qq2nhzp743374a94p9s766rpp";
   };
 
-  doCheck = false;
-  propagatedBuildInputs = [ six ];
+  propagatedBuildInputs = with python.pkgs; [ six ];
+  checkInputs = with python.pkgs; [ factory_boy faker pytest pytestcov tox ];
+
+  # https://github.com/barseghyanartur/tld/issues/54
+  disabledTests = stdenv.lib.concatMapStringsSep " and " (s: "not " + s) ([
+    "test_1_update_tld_names"
+    "test_1_update_tld_names_command"
+    "test_2_update_tld_names_module"
+  ]);
+
+  checkPhase = ''
+      export PATH="$PATH:$out/bin"
+      py.test -k '${disabledTests}'
+  '';
 
   meta = with stdenv.lib; {
     homepage = https://github.com/barseghyanartur/tld;
