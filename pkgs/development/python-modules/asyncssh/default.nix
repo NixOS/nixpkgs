bb00d468116d13b80be63fb3353c996c8c67aa03
Robert Scott <code@humanleg.org.uk>
2019-05-01 01:11:43 +0100

    pythonPackages.asyncssh: 1.15.1 -> 1.16.1, fixing build with some caveats around ec25519 support
    asyncssh requires openssl 1.1.x for ec25519 support, but the default
    cryptography and pyopenssl packages are built against openssl 1.0.x.
    however, forcing overrides of these using openssl 1.1.x will produce
    package collisions meaning asyncssh may not be installable in the same
    environment as most other default python packages. so rather than do
    that by default, supply an easy route for those who want ec25519
    support

--- pkgs/development/python-modules/asyncssh/default.nix
+++ pkgs/development/python-modules/asyncssh/default.nix
@@ -3,31 +3,42 @@
 , bcrypt, gssapi, libnacl, libsodium, nettle, pyopenssl
 , openssl }:
 
-buildPythonPackage rec {
+let
+  # asyncssh requires openssl 1.1.x for ec25519 support, but the default cryptography and
+  # pyopenssl packages are built against openssl 1.0.x. to enable ec25519 support, supply
+  # this package with a 1.1.x openssl package. note however that this may produce package
+  # collisions with (and not be installable in the same environment as) any of the python
+  # packages that depend on the default cryptography or pyopenssl.
+  cryptography' = (cryptography.override { inherit openssl; });
+  pyopenssl' = (pyopenssl.override { inherit openssl; cryptography = cryptography'; });
+in buildPythonPackage rec {
   pname = "asyncssh";
-  version = "1.15.1";
+  version = "1.16.1";
   disabled = pythonOlder "3.4";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f2065a8b3af0c514c8de264e7b01f08df5213b707bacb7e7c080bd46c3e3bc35";
+    sha256 = "0qia1ay2dhwps5sfh0hif7mrv7yxvykxs9l7cmfp4m6hmqnn3r5r";
   };
 
   propagatedBuildInputs = [
     bcrypt
-    cryptography
+    cryptography'
     gssapi
     libnacl
     libsodium
     nettle
-    pyopenssl
+    pyopenssl'
   ];
 
   checkInputs = [ openssl ];
 
-  # Disables windows specific test (specifically the GSSAPI wrapper for Windows)
+  # Disables windows specific test (specifically the GSSAPI wrapper for Windows) and
+  # prevents sftp test from attempting to set permission bits which we're not allowed
+  # to in the sandbox environment.
   postPatch = ''
     rm ./tests/sspi_stub.py
+    substituteInPlace ./tests/test_sftp.py --replace "0o4321" "0o321"
   '';
 
   meta = with stdenv.lib; {
