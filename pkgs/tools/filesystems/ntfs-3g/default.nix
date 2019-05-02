fb3b220fde4ec582addac9c8a4f370dcd73fa39b
Markus Schmidl <marenz2569@users.noreply.github.com>
2019-05-02 16:30:58 +0200

    ntfs3g: remove obsolete CVE-2017-0358 patch
    * ntfs-3g: remove patch as it been fixed  (#60697)
    
    * ntfs3g: fetchpatch is unused
    
    (cherry picked from commit 3e5a0bea6ef88ebc0deacb6f0ece5a80a21435f1)

--- pkgs/tools/filesystems/ntfs-3g/default.nix
+++ pkgs/tools/filesystems/ntfs-3g/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, fetchpatch, utillinux, libuuid
+{stdenv, fetchurl, utillinux, libuuid
 , crypto ? false, libgcrypt, gnutls, pkgconfig}:
 
 stdenv.mkDerivation rec {
@@ -12,16 +12,9 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = stdenv.lib.optional crypto pkgconfig;
 
   src = fetchurl {
-    url = "http://tuxera.com/opensource/ntfs-3g_ntfsprogs-${version}.tgz";
+    url = "https://tuxera.com/opensource/ntfs-3g_ntfsprogs-${version}.tgz";
     sha256 = "1mb228p80hv97pgk3myyvgp975r9mxq56c6bdn1n24kngcfh4niy";
   };
-
-  patches = [
-    (fetchpatch {
-      url = "https://sources.debian.org/data/main/n/ntfs-3g/1:2016.2.22AR.1+dfsg-1/debian/patches/0003-CVE-2017-0358.patch";
-      sha256 = "0hd05q9q06r18k8pmppvch1sslzqln5fvqj51d5r72g4mnpavpj3";
-    })
-  ];
 
   patchPhase = ''
     substituteInPlace src/Makefile.in --replace /sbin '@sbindir@'
