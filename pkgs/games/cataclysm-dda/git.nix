c2987f1ba88812911204574969785ca964118c5e
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2019-05-03 01:50:40 +0900

    cataclysm-dda{,-git}: refactoring

--- pkgs/games/cataclysm-dda/git.nix
+++ pkgs/games/cataclysm-dda/git.nix
@@ -4,9 +4,9 @@
 }:
 
 let
-  inherit (stdenv.lib) optionals optionalString substring;
-  inherit (callPackage ./common.nix { inherit tiles Cocoa debug; }) common utils;
-  inherit (utils) fetchFromCleverRaven installXDGAppLauncher installMacOSAppLauncher;
+  inherit (stdenv.lib) substring;
+  inherit (callPackage ./common.nix { inherit tiles CoreFoundation Cocoa debug; }) common utils;
+  inherit (utils) fetchFromCleverRaven;
 in
 
 stdenv.mkDerivation (common // rec {
@@ -18,25 +18,11 @@ stdenv.mkDerivation (common // rec {
     sha256 = "18yn0h6b4j9lx67sq1d886la3l6l7bqsnwj6mw2khidssiy18y0n";
   };
 
-  buildInputs = common.buildInputs
-    ++ optionals stdenv.isDarwin [ CoreFoundation ];
-
   patches = [ ./patches/fix_locale_dir_git.patch ];
 
   makeFlags = common.makeFlags ++ [
     "VERSION=git-${version}-${substring 0 8 src.rev}"
   ];
-
-  postInstall = optionalString tiles
-  ( if !stdenv.isDarwin
-    then installXDGAppLauncher
-    else installMacOSAppLauncher
-  );
-
-  # https://hydra.nixos.org/build/65193254
-  # src/weather_data.cpp:203:1: fatal error: opening dependency file obj/tiles/weather_data.d: No such file or directory
-  # make: *** [Makefile:687: obj/tiles/weather_data.o] Error 1
-  enableParallelBuilding = false;
 
   meta = with stdenv.lib.maintainers; common.meta // {
     maintainers = common.meta.maintainers ++ [ rardiol ];
