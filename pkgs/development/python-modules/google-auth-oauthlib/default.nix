d29c13b4de97d7cd19044ab353cd53a78102d9f7
R. RyanTM <ryantm-bot@ryantm.com>
2019-05-01 22:16:27 -0700

    python37Packages.google-auth-oauthlib: 0.2.0 -> 0.3.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-google-auth-oauthlib/versions

--- pkgs/development/python-modules/google-auth-oauthlib/default.nix
+++ pkgs/development/python-modules/google-auth-oauthlib/default.nix
@@ -12,11 +12,11 @@
 
 buildPythonPackage rec {
   pname = "google-auth-oauthlib";
-  version = "0.2.0";
+  version = "0.3.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "226d1d0960f86ba5d9efd426a70b291eaba96f47d071657e0254ea969025728a";
+    sha256 = "03rq2rjac0zh16vsw0q914sp62l9f8fp033wn3191pqd2cchqix0";
   };
 
   checkInputs = [
@@ -28,6 +28,7 @@ buildPythonPackage rec {
   ];
 
   checkPhase = ''
+    rm -fr tests/__pycache__/
     py.test
   '';
 
