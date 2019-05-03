10ffb3864365de5f0287df32c85844a13119057a
José Romildo Malaquias <malaquias@gmail.com>
2019-05-03 08:35:12 -0300

    qogir-theme: 2019-04-07 -> 2019-05-03

--- pkgs/data/themes/qogir/default.nix
+++ pkgs/data/themes/qogir/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "qogir-theme";
-  version = "2019-04-07";
+  version = "2019-05-03";
 
   src = fetchFromGitHub {
     owner = "vinceliuice";
     repo = pname;
     rev = version;
-    sha256 = "0knv35xb4rg4pddxc78hd8frnlm8n0za1yj51ydwskn9b0qqcyhs";
+    sha256 = "031nqr47b3x8ahcym7cfc75y8sy53dcmrrrlywi7m1a10ckfp0pd";
   };
 
   buildInputs = [ gdk_pixbuf librsvg ];
