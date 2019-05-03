8ae2212da3e02eb9e3bf6e71ca1bf0d2d8177f07
Ben Hamlin <protob3n@gmail.com>
2019-05-03 10:06:43 -0700

    evcxr: makeWrapper -> nativeBuildInputs and omit Jupyter kernel
    The Jupyter kernel should be its own package. Omitting it and its dependencies here.

--- pkgs/development/interpreters/evcxr/default.nix
+++ pkgs/development/interpreters/evcxr/default.nix
@@ -1,4 +1,4 @@
-{ cargo, fetchFromGitHub, makeWrapper, pkgconfig, rustPlatform, stdenv, zeromq }:
+{ cargo, fetchFromGitHub, makeWrapper, pkgconfig, rustPlatform, stdenv }:
 
 rustPlatform.buildRustPackage rec {
   name = "evcxr-${version}";
@@ -11,15 +11,18 @@ rustPlatform.buildRustPackage rec {
     sha256 = "1apc93z9vvf6qks5x2pad45rnrj9kjl812rj78w5zmmizccp2fhf";
   };
 
-  cargoSha256 = "1pvw1jfi4p0cla4xxzgxh1jskvbmsyi4l5cimiis7093hyjs0svw";
+  cargoSha256 = "0bd6yna8yyrqjv4w7qr2923nljbjjlbkyad6vjlgfp33yn2pz1iv";
   cargoPatches = [ ./cargo-lock.patch ];
 
-  nativeBuildInputs = [ pkgconfig zeromq ];
-  buildInputs = [ cargo makeWrapper ];
-  postInstall = "wrapProgram $out/bin/evcxr --prefix PATH : ${cargo}/bin";
+  nativeBuildInputs = [ pkgconfig makeWrapper ];
+  buildInputs = [ cargo ];
+  postInstall = ''
+    wrapProgram $out/bin/evcxr --prefix PATH : ${cargo}/bin
+    rm $out/bin/testing_runtime
+  '';
 
   meta = {
-    description = "An evaluation context for Rust, including a CLI REPL and a Jupyter kernel";
+    description = "An evaluation context for Rust";
     homepage = "https://github.com/google/evcxr";
     license = stdenv.lib.licenses.asl20;
     maintainers = [ stdenv.lib.maintainers.protoben ];
