cd1bfc30451b505f07d48c5bfd4c8aa9e5fd7623
Michael Fellinger <michael.fellinger@xing.com>
2019-05-03 18:47:17 +0200

    ppl-address-book: delete

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19348,8 +19348,6 @@ in
 
   posterazor = callPackage ../applications/misc/posterazor { };
 
-  ppl-address-book = callPackage ../applications/office/ppl-address-book { };
-
   pqiv = callPackage ../applications/graphics/pqiv { };
 
   qiv = callPackage ../applications/graphics/qiv { };
