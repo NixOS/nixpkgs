b67e9f3958c4c73b27a1864eea4f47eec733080a
Milan Pässler <me@pbb.lc>
2019-05-04 02:35:55 +0200

    viu: init at 0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6303,6 +6303,8 @@ in
 
   vit = callPackage ../applications/misc/vit { };
 
+  viu = callPackage ../tools/graphics/viu { };
+
   vnc2flv = callPackage ../tools/video/vnc2flv {};
 
   vncrec = callPackage ../tools/video/vncrec { };
