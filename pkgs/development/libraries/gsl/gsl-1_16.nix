091de095c92ab1d2932ec9dd5e810ea2575e2432
volth <volth@volth.com>
2019-05-04 15:31:02 +0000

    -mno-fma is x86_64 only; it hinders aarch64 compilation

--- pkgs/development/libraries/gsl/gsl-1_16.nix
+++ pkgs/development/libraries/gsl/gsl-1_16.nix
@@ -8,7 +8,8 @@ stdenv.mkDerivation rec {
     sha256 = "0lrgipi0z6559jqh82yx8n4xgnxkhzj46v96dl77hahdp58jzg3k";
   };
 
-  NIX_CFLAGS_COMPILE = [ "-mno-fma" ]; # do not let -march=skylake to enable FMA (https://lists.gnu.org/archive/html/bug-gsl/2011-11/msg00019.html)
+  # do not let -march=skylake to enable FMA (https://lists.gnu.org/archive/html/bug-gsl/2011-11/msg00019.html)
+  NIX_CFLAGS_COMPILE = stdenv.lib.optional stdenv.isx86_64 "-mno-fma";
 
   patches = [
     (fetchpatch {
