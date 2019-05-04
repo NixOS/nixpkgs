NixOS/nixpkgs

commit be382109ad01d0aa8660f2b19a20e345c86d246b

gst_all_1.gst-plugins-bad: add more plug-ins

pkgs/development/libraries/gstreamer/bad/default.nix
@@ -1,8 +1,9 @@
 { stdenv, fetchurl, fetchpatch, meson, ninja, gettext
 , pkgconfig, python, gst-plugins-base, orc
 , faacSupport ? false, faac ? null
-, faad2, libass, libkate, libmms
-, libmodplug, mpeg2dec
+, faad2, libass, libkate, libmms, librdf, ladspaH
+, libnice, webrtc-audio-processing, lilv, lv2, serd, sord, sratom
+, libbs2b, libmodplug, mpeg2dec
 , openjpeg, libopus, librsvg
 , wildmidi, fluidsynth, libvdpau, wayland
 , libwebp, xvidcore, gnutls, mjpegtools
@@ -42,6 +43,12 @@ stdenv.mkDerivation rec {
         sha256 = "0hy0rcn35alq65yqwri4fqjz2hf3nyyg5c7rnndk51msmqjxpprk";
     })
     ./fix_pkgconfig_includedir.patch
+    # Enable bs2b compilation
+    # https://bugzilla.gnome.org/show_bug.cgi?id=794346
+    (fetchurl {
+      url = https://bugzilla.gnome.org/attachment.cgi?id=369724;
+      sha256 = "1716mp0h2866ab33w607isvfhv1zwyj71qb4jrkx5v0h276v1pwr";
+    })
   ];
 
   src = fetchurl {
@@ -56,6 +63,10 @@ stdenv.mkDerivation rec {
   buildInputs = [
     gst-plugins-base orc
     faad2 libass libkate libmms
+    libnice webrtc-audio-processing # webrtc
+    libbs2b
+    ladspaH librdf # ladspa plug-in
+    lilv lv2 serd sord sratom # lv2 plug-in
     libmodplug mpeg2dec
     openjpeg libopus librsvg
     fluidsynth libvdpau

commit e795745ff5f3f4958a56e61dee6cafe7bb5a2e4f

gst_all_1.gst-plugins-bad: Fix evaluation on Darwin, fix gstreamer builds.
Commit be382109ad01d0aa8660f2b19a20e345c86d246b broke evaluation
on Darwin because it added these unconditional buildInputs
that don't evaluate on Darwin:

    libnice librdf lilv lv2 serd sord sratom

This commit fixes it, and also fixes recently-added new dependencies
for other packages accordingly.

It further fixes the build of many gstreamer packages on Darwin.

pkgs/development/libraries/gstreamer/bad/default.nix
@@ -10,6 +10,7 @@
 , bluez
 , chromaprint
 , curl
+, darwin
 , directfb
 , fdk_aac
 , flite
@@ -102,29 +103,22 @@ stdenv.mkDerivation rec {
   buildInputs = [
     gst-plugins-base orc
     faad2 libass libkate libmms
-    libnice webrtc-audio-processing # webrtc
+    webrtc-audio-processing # webrtc
     libbs2b
-    ladspaH librdf # ladspa plug-in
-    lilv lv2 serd sord sratom # lv2 plug-in
+    ladspaH # ladspa plug-in
     libmodplug mpeg2dec
     openjpeg libopus librsvg
-    bluez
-    chromaprint
     curl.dev
-    directfb
     fdk_aac
-    flite
     gsm
     libaom
     libdc1394
     libde265
-    libdrm
     libdvdnav
     libdvdread
-    libgudev
-    libofa
     libsndfile
     libusb1
+    mjpegtools
     neon
     openal
     opencv3
@@ -132,9 +126,7 @@ stdenv.mkDerivation rec {
     openh264
     rtmpdump
     pango
-    sbc
     soundtouch
-    spandsp
     srtp
     zbar
     fluidsynth libvdpau
@@ -144,12 +136,39 @@ stdenv.mkDerivation rec {
     srt
   ]
     ++ optional faacSupport faac
+    ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+      # For unknown reasons the order is important, e.g. if
+      # VideoToolbox is last, we get:
+      #     fatal error: 'VideoToolbox/VideoToolbox.h' file not found
+      VideoToolbox
+      AudioToolbox
+      AVFoundation
+      CoreMedia
+      CoreVideo
+      Foundation
+      MediaToolbox
+    ])
+    # These currently do not evaluate on darwin
+    ++ optionals (!stdenv.isDarwin) [
+      bluez
+      chromaprint
+      directfb
+      flite
+      flite
+      libdrm
+      libgudev
+      libnice
+      libofa
+      librdf
+      lilv lv2 serd sord sratom # lv2 plug-in
+      sbc
+      spandsp
+    ]
     ++ optional stdenv.isLinux wayland
     # wildmidi requires apple's OpenAL
     # TODO: package apple's OpenAL, fix wildmidi, include on Darwin
     ++ optional (!stdenv.isDarwin) wildmidi
-    # TODO: mjpegtools uint64_t is not compatible with guint64 on Darwin
-    ++ optional (!stdenv.isDarwin) mjpegtools;
+    ;
 
   mesonFlags = [
     # Enables all features, so that we know when new dependencies are necessary.
@@ -189,7 +208,34 @@ stdenv.mkDerivation rec {
     # see https://github.com/NixOS/nixpkgs/issues/54395
     "-Dnvdec=disabled"
     "-Dnvenc=disabled"
-  ];
+  ]
+  ++ optionals stdenv.isDarwin [
+    "-Dbluez=disabled"
+    "-Dchromaprint=disabled"
+    "-Ddirectfb=disabled"
+    "-Dflite=disabled"
+    "-Dkms=disabled" # renders to libdrm output
+    "-Dofa=disabled"
+    "-Dlv2=disabled"
+    "-Dsbc=disabled"
+    "-Dspandsp=disabled"
+    "-Ddvb=disabled"
+    "-Dfbdev=disabled"
+    "-Duvch264=disabled" # requires gudev
+    "-Dladspa=disabled" # requires lrdf
+    "-Dwebrtc=disabled" # requires libnice, which as of writing doesn't work on Darwin in nixpkgs
+    "-Dwildmidi=disabled" # see dependencies above
+  ]
+  ++ optional (!gst-plugins-base.glEnabled) "-Dgl=disabled"
+  ++ optional (!gst-plugins-base.waylandEnabled) "-Dwayland=disabled"
+  # `applemedia/videotexturecache.h` requires `gst/gl/gl.h`,
+  # but its meson build system does not declare the dependency.
+  ++ optional (!gst-plugins-base.glEnabled) "-Dapplemedia=disabled"
+  ;
+
+  # This package has some `_("string literal")` string formats
+  # that trip up clang with format security enabled.
+  hardeningDisable = [ "format" ];
 
   enableParallelBuilding = true;
 

pkgs/development/libraries/gstreamer/base/default.nix
@@ -4,13 +4,19 @@
 , libintl, libopus
 , isocodes
 , libjpeg
+, libpng
 , libvisual
 , tremor # provides 'virbisidec'
 , gtk-doc, docbook_xsl, docbook_xml_dtd_412
 , enableX11 ? stdenv.isLinux, libXv
 , enableWayland ? stdenv.isLinux, wayland
 , enableAlsa ? stdenv.isLinux, alsaLib
-, enableCocoa ? false, darwin
+# Enabling Cocoa seems to currently not work, giving compile
+# errors. Suspected is that a newer version than clang
+# is needed than 5.0 but it is not clear.
+, enableCocoa ? false
+, darwin
+, enableGl ? (enableX11 || enableWayland || enableCocoa)
 , enableCdparanoia ? (!stdenv.isDarwin), cdparanoia }:
 
 stdenv.mkDerivation rec {
@@ -34,31 +40,19 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [
     pkgconfig python3 gettext gobject-introspection
+    meson
+    ninja
     gtk-doc
     # Without these, enabling the 'gtk_doc' gives us `FAILED: meson-install`
     docbook_xsl docbook_xml_dtd_412
-  ]
-  # Broken meson with Darwin. Should hopefully be fixed soon. Tracking
-  # in https://bugzilla.gnome.org/show_bug.cgi?id=781148.
-  ++ lib.optionals (!stdenv.isDarwin) [ meson ninja ];
+  ];
 
-  # On Darwin, we currently use autoconf, on all other systems Meson
-  # TODO Switch to Meson on Darwin as well
-
-  # TODO How to pass these to Meson?
-  configureFlags = lib.optionals stdenv.isDarwin [
-    "--enable-x11=${if enableX11 then "yes" else "no"}"
-    "--enable-wayland=${if enableWayland then "yes" else "no"}"
-    "--enable-cocoa=${if enableCocoa then "yes" else "no"}"
-  ]
-  # Introspection fails on my MacBook currently
-  ++ lib.optional stdenv.isDarwin "--disable-introspection";
-
-  mesonFlags = lib.optionals (!stdenv.isDarwin) [
+  mesonFlags = [
     # Enables all features, so that we know when new dependencies are necessary.
     "-Dauto_features=enabled"
     "-Dexamples=disabled" # requires many dependencies and probably not useful for our users
     "-Dgl-graphene=disabled" # not packaged in nixpkgs as of writing
+    "-Dgl_platform=[${lib.optionalString (enableX11 || enableWayland || enableCocoa) "auto"}]"
     # See https://github.com/GStreamer/gst-plugins-base/blob/d64a4b7a69c3462851ff4dcfa97cc6f94cd64aef/meson_options.txt#L15 for a list of choices
     "-Dgl_winsys=[${lib.concatStringsSep "," (lib.optional enableX11 "x11" ++ lib.optional enableWayland "wayland" ++ lib.optional enableCocoa "cocoa")}]"
     # We must currently disable gtk_doc API docs generation,
@@ -71,13 +65,24 @@ stdenv.mkDerivation rec {
   ]
   ++ lib.optional (!enableX11) "-Dx11=disabled"
   # TODO How to disable Wayland?
+  ++ lib.optional (!enableGl) "-Dgl=disabled"
   ++ lib.optional (!enableAlsa) "-Dalsa=disabled"
   ++ lib.optional (!enableCdparanoia) "-Dcdparanoia=disabled"
+  ++ lib.optionals stdenv.isDarwin [
+    "-Dlibvisual=disabled"
+  ]
   ;
 
-  buildInputs = [ orc libtheora libintl libopus isocodes libjpeg libvisual tremor ]
+  buildInputs = [ orc libtheora libintl libopus isocodes libjpeg libpng tremor ]
     ++ lib.optional enableAlsa alsaLib
+    ++ lib.optionals (!stdenv.isDarwin) [
+      libvisual
+    ]
     ++ lib.optionals enableX11 [ libXv pango ]
+    ++ lib.optionals stdenv.isDarwin [
+      pango
+      darwin.apple_sdk.frameworks.OpenGL
+    ]
     ++ lib.optional enableWayland wayland
     ++ lib.optional enableCocoa darwin.apple_sdk.frameworks.Cocoa
     ++ lib.optional enableCdparanoia cdparanoia;
@@ -88,6 +93,10 @@ stdenv.mkDerivation rec {
     patchShebangs .
   '';
 
+  # This package has some `_("string literal")` string formats
+  # that trip up clang with format security enabled.
+  hardeningDisable = [ "format" ];
+
   enableParallelBuilding = true;
 
   doCheck = false; # fails, wants DRI access for OpenGL
@@ -95,4 +104,21 @@ stdenv.mkDerivation rec {
   patches = [
     ./fix_pkgconfig_includedir.patch
   ];
+
+  passthru = {
+    # Downstream `gst-*` packages depending on `gst-plugins-base`
+    # have meson build options like 'gl' etc. that depend
+    # on these features being built in `-base`.
+    # If they are not built here, then the downstream builds
+    # will fail, as they, too, use `-Dauto_features=enabled`
+    # which would enable these options unconditionally.
+    # That means we must communicate to these downstream packages
+    # if the `-base` enabled these options or not, so that
+    # the can enable/disable those features accordingly.
+    # The naming `*Enabled` vs `enable*` is intentional to
+    # distinguish inputs from outputs (what is to be built
+    # vs what was built) and to make them easier to search for.
+    glEnabled = enableGl;
+    waylandEnabled = enableWayland;
+  };
 }

pkgs/development/libraries/gstreamer/core/default.nix
@@ -52,6 +52,9 @@ stdenv.mkDerivation rec {
     "-Dauto_features=enabled"
     "-Ddbghelp=disabled" # not needed as we already provide libunwind and libdw, and dbghelp is a fallback to those
     "-Dexamples=disabled" # requires many dependencies and probably not useful for our users
+  ] ++ lib.optionals (stdenv.isDarwin) [
+    "-Dlibunwind=disabled"
+    "-Dlibdw=disabled"
   ];
 
   postInstall = ''

pkgs/development/libraries/gstreamer/good/default.nix
@@ -6,6 +6,9 @@
 , libsoup, libpulseaudio, libintl
 , darwin, lame, mpg123, twolame
 , gtkSupport ? false, gtk3 ? null
+# As of writing, jack2 incurs a Qt dependency (big!) via `ffado`.
+# In the fuure we should probably split `ffado`.
+, enableJack ? false
 , libXdamage
 , libXext
 , libXfixes
@@ -43,7 +46,7 @@ stdenv.mkDerivation rec {
     sha256 = "1zdhif1mhf0ihkjpjyrh65g2iz2cawkjjb3h5w8h9ml06grxwjk5";
   };
 
-  outputs = [ "out" "dev" ];
+  outputs = [ "out" ];
 
   patches = [ ./fix_pkgconfig_includedir.patch ];
 
@@ -65,8 +68,21 @@ stdenv.mkDerivation rec {
     wavpack
   ]
   ++ optional gtkSupport gtk3 # for gtksink
-  ++ optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Cocoa ]
-  ++ optionals stdenv.isLinux [ libv4l libpulseaudio libavc1394 libiec61883 libgudev jack2 ];
+  ++ optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.Cocoa
+    # Needed for NSDefaultRunLoopMode symbols.
+    darwin.cf-private
+  ]
+  ++ optionals stdenv.isLinux [
+    libavc1394
+    libgudev
+    libiec61883
+    libpulseaudio
+    libv4l
+  ]
+  ++ optionals (stdenv.isLinux && enableJack) [
+    jack2
+  ];
 
   mesonFlags = [
     # Enables all features, so that we know when new dependencies are necessary.
@@ -76,10 +92,17 @@ stdenv.mkDerivation rec {
   ]
   ++ optional (!gtkSupport) "-Dgtk3=disabled"
   ++ optionals (!stdenv.isLinux) [
-    "-Djack=disabled" # unclear whether Jack works on Darwin
+    "-Ddv1394=disabled" # Linux only
+    "-Doss4=disabled" # Linux only
+    "-Doss=disabled" # Linux only
+    "-Dpulse=disabled" # TODO check if we can keep this enabled
+    "-Dv4l2-gudev=disabled" # Linux-only
+    "-Dv4l2=disabled" # Linux-only
+    "-Dximagesrc=disabled" # Linux-only
+    "-Dpulse=disabled" # TODO check if we can keep this enabled
   ]
-  ++ optionals (!stdenv.isLinux) [
-    "-Dv4l2-gudev=disabled"
+  ++ optionals (!stdenv.isLinux || !enableJack) [
+    "-Djack=disabled" # unclear whether Jack works on Darwin
   ];
 
   # fails 1 tests with "Unexpected critical/warning: g_object_set_is_valid_property: object class 'GstRtpStorage' has no property named ''"

pkgs/development/libraries/gstreamer/validate/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, gstreamer, gst-plugins-base
+{ stdenv, fetchurl, fetchpatch, pkgconfig, gstreamer, gst-plugins-base
 , python, gobject-introspection, json-glib
 }:
 
@@ -17,6 +17,16 @@ stdenv.mkDerivation rec {
     url = "${meta.homepage}/src/gst-validate/${name}.tar.xz";
     sha256 = "1jfnd0g9hmdbqfxsx96yc9vpf1w6m33hqwrr6lj4i83kl54awcck";
   };
+
+  patches = [
+    # Fixes a duplicate symbol compilation error on Darwin.
+    # TODO Remove when https://gitlab.freedesktop.org/gstreamer/gst-devtools/commit/22e179cbc1acf8e4bcbc8173c833ea5f086fa28c is available in nixpkgs, likely for 1.18.
+    (fetchpatch {
+      url = "https://gitlab.freedesktop.org/gstreamer/gst-devtools/commit/751a6d756c0f7c3a721a235ced74fec17f038185.patch";
+      sha256 = "05pzjn63pcsf3y153r7azy8dbr0cmn10d10r11cdprgw2bv5hlha";
+      stripLen = 1; # gst-validate is a subdir of the `gst-devtools` git repo
+    })
+  ];
 
   outputs = [ "out" "dev" ];
 