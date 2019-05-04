226e6fe54495599bc35220a6be26c0313ea5965c
Renaud <c0bw3b@users.noreply.github.com>
2019-05-04 16:54:16 +0200

    rebar3: don't use fetchurl
    for mutable GitHub archives
    + parse_trans 3.3.0 hash has changed

--- pkgs/development/tools/build-managers/rebar3/default.nix
+++ pkgs/development/tools/build-managers/rebar3/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl,
+{ stdenv, fetchFromGitHub,
   fetchHex, erlang,
   tree, hexRegistrySnapshot }:
 
@@ -65,17 +65,19 @@ let
   parse_trans = fetchHex {
     pkg = "parse_trans";
     version = "3.3.0";
-    sha256 = "17ef63abde837ad30680ea7f857dd9e7ced9476cdd7b0394432af4bfc241b960";
+    sha256 = "0q5r871bzx1a8fa06yyxdi3xkkp7v5yqazzah03d6yl3vsmn7vqp";
   };
 
 in
-stdenv.mkDerivation {
-  name = "rebar3-${version}";
+stdenv.mkDerivation rec {
+  pname = "rebar3";
   inherit version erlang;
 
-  src = fetchurl {
-    url = "https://github.com/rebar/rebar3/archive/${version}.tar.gz";
-    sha256 = "0zl2xr2vb06pm4svm4sg0p7mqas29lpcjcvswrrh2d2zsw5llsv5";
+  src = fetchFromGitHub {
+    owner = "rebar";
+    repo = pname;
+    rev = version;
+    sha256 = "1p34kfkrdmsixg95ad76rifjwfh484vp688lxsjaxg0kf2xjr2d2";
   };
 
   inherit bootstrapper;
@@ -121,7 +123,7 @@ stdenv.mkDerivation {
 
   meta = {
     homepage = https://github.com/rebar/rebar3;
-    description = "rebar 3 is an Erlang build tool that makes it easy to compile and test Erlang applications, port drivers and releases";
+    description = "Erlang build tool that makes it easy to compile and test Erlang applications, port drivers and releases";
 
     longDescription = ''
       rebar is a self-contained Erlang script, so it's easy to distribute or
