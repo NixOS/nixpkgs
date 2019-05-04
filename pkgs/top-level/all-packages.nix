e706a83f0d2d2fbdcde4d22f28d0e8f874ea39f3
Taran Lynn <taranlynn0@gmail.com>
2019-05-04 15:52:01 -0700

    redeclipse: init at 1.6.0.
    See www.redeclipse.net for information.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21837,6 +21837,8 @@ in
 
   racer = callPackage ../games/racer { };
 
+  redeclipse = callPackage ../games/redeclipse { };
+
   residualvm = callPackage ../games/residualvm { };
 
   rftg = callPackage ../games/rftg { };
