b12ea62ec98bc00b5283c5eecc5aba6e5f965444
(cdep)illabout <cdep.illabout@gmail.com>
2019-05-05 20:16:19 +0900

    nixos/systemd-boot: add support for memtest86 EFI app
    This commit adds support for installing the memtest86 EFI app and adding
    a boot entry for it with systemd-boot.

--- nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix
+++ nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix
@@ -25,6 +25,8 @@ let
     inherit (cfg) consoleMode;
 
     inherit (efi) efiSysMountPoint canTouchEfiVariables;
+
+    memtest86 = if cfg.memtest86.enable then pkgs.memtest86-efi else "";
   };
 in {
 
@@ -85,6 +87,19 @@ in {
           </para></listitem>
         </itemizedlist>
       '';
+    };
+
+    memtest86 = {
+      enable = mkOption {
+        default = false;
+        type = types.bool;
+        description = ''
+          Make MemTest86 available from the systemd-boot menu. MemTest86 is a
+          program for testing memory.  MemTest86 is a non-open-source program, so
+          this requires <literal>allowUnfree</literal> to be set to
+          <literal>true</literal>.
+        '';
+      };
     };
   };
 
