4f65d66ffd63dbcbb44b81e859bff8b507016e0e
R. RyanTM <ryantm-bot@ryantm.com>
2019-05-05 16:54:11 -0700

    python37Packages.djangorestframework: 3.9.2 -> 3.9.3
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-djangorestframework/versions
    
    (cherry picked from commit 38af84ad528ca442dc582cef5585690e3d5b22f5)

--- pkgs/development/python-modules/djangorestframework/default.nix
+++ pkgs/development/python-modules/djangorestframework/default.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPythonPackage, fetchPypi, django }:
 buildPythonPackage rec {
-  version = "3.9.2";
+  version = "3.9.3";
   pname = "djangorestframework";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "05sam4z69mypxk8fv415zvs8mp09jqsagmslrbs1qvk51lk6d8pp";
+    sha256 = "1w1rc8cpw89sll5wsg1aj1w3klk91a1bsdz9y4zhg5xrc0qpd118";
   };
 
   # Test settings are missing
