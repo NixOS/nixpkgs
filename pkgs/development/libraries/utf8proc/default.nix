3ca8d0fa1e98691df2f21366adae226fdfe6c6ca
Renaud <c0bw3b@users.noreply.github.com>
2019-05-05 13:15:24 +0200

    utf8proc: use fetchFromGitHub

--- pkgs/development/libraries/utf8proc/default.nix
+++ pkgs/development/libraries/utf8proc/default.nix
@@ -1,19 +1,21 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
-  name = "utf8proc-${version}";
+  pname = "utf8proc";
   version = "2.3.0";
 
-  src = fetchurl {
-    url = "https://github.com/JuliaLang/utf8proc/archive/v${version}.tar.gz";
-    sha256 = "03lk8y9zmqax3yk7d1nmgb2fsk83p9cfj6mb3i49bawvnm4ml9n0";
+  src = fetchFromGitHub {
+    owner = "JuliaStrings";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "1jhjl7nw6262ks5zrk447qmh6z2r5rrnnrm742dk33d7031g3s55";
   };
 
   makeFlags = [ "prefix=$(out)" ];
 
   meta = with stdenv.lib; {
     description = "A clean C library for processing UTF-8 Unicode data";
-    homepage = https://julialang.org/utf8proc;
+    homepage = "https://juliastrings.github.io/utf8proc/";
     license = licenses.mit;
     platforms = platforms.all;
     maintainers = [ maintainers.ftrvxmtrx ];
