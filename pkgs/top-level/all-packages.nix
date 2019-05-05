9168977a98377fa16c8b69e96e6eb8d0d0b6d96d
(cdep)illabout <cdep.illabout@gmail.com>
2019-05-05 15:42:22 +0900

    memtest86-efi: init at 8.0 (backport to 19.03)
    This creates a new package, memtest86-efi.  This is a recent version of
    the unfree version of memtest86.
    
    There appear to be no restrictions on the use or redistribution of this.
    
    Backport from #60967.
    
    (cherry picked from commit d273bf5eb4335e60e7d2c8d63f2325aab4b1041d)
    (cherry picked from commit e01e13a5477b7efb503ac5eb26e3cc2bde1375b9)
    (cherry picked from commit 439a7552bd2e074f97ac574f360e23f1c20ba769)
    (cherry picked from commit 4abafaf3ccdc1b067804ceb8f4e9e26805ee4513)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4239,6 +4239,8 @@ in
 
   memtest86 = callPackage ../tools/misc/memtest86 { };
 
+  memtest86-efi = callPackage ../tools/misc/memtest86-efi { };
+
   memtest86plus = callPackage ../tools/misc/memtest86+ { };
 
   meo = callPackage ../tools/security/meo {
