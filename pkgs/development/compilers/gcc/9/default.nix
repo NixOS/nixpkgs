5964b3a2e21c9d9369b55bbf48dff71ff0671f25
Will Dietz <w@wdtz.org>
2019-05-05 02:23:15 -0500

    gcc9: drop gomp patch (musl)
    https://github.com/NixOS/nixpkgs/pull/60860#issuecomment-489495120

--- pkgs/development/compilers/gcc/9/default.nix
+++ pkgs/development/compilers/gcc/9/default.nix
@@ -49,8 +49,7 @@ let version = "9.1.0";
         sha256 = ""; # TODO: uncomment and check hash when available.
       }) */
       ++ optional langFortran ../gfortran-driving.patch
-      ++ optional (targetPlatform.libc == "musl" && targetPlatform.isPower) ../ppc-musl.patch
-      ++ optional (targetPlatform.libc == "musl") ../libgomp-dont-force-initial-exec.patch;
+      ++ optional (targetPlatform.libc == "musl" && targetPlatform.isPower) ../ppc-musl.patch;
 
     /* Cross-gcc settings (build == host != target) */
     crossMingw = targetPlatform != hostPlatform && targetPlatform.libc == "msvcrt";
