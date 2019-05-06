47c78acd8e3a0530bffb990b88ea4808cf6c0c7b
R. RyanTM <ryantm-bot@ryantm.com>
2019-05-06 05:30:33 -0700

    utf8proc: 2.2.0 -> 2.3.0 (#59724)
    * utf8proc: 2.2.0 -> 2.3.0
    
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/utf8proc/versions
    
    * utf8proc: use fetchFromGitHub

--- pkgs/development/libraries/utf8proc/default.nix
+++ pkgs/development/libraries/utf8proc/default.nix
@@ -1,19 +1,21 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
-  name = "utf8proc-${version}";
-  version = "2.2.0";
+  pname = "utf8proc";
+  version = "2.3.0";
 
-  src = fetchurl {
-    url = "https://github.com/JuliaLang/utf8proc/archive/v${version}.tar.gz";
-    sha256 = "1gsxxp7vk36z1g5mg19kq10j35dks5f9slsab2xfazh5vgdx33rz";
+  src = fetchFromGitHub {
+    owner = "JuliaStrings";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "1jhjl7nw6262ks5zrk447qmh6z2r5rrnnrm742dk33d7031g3s55";
   };
 
   makeFlags = [ "prefix=$(out)" ];
 
   meta = with stdenv.lib; {
     description = "A clean C library for processing UTF-8 Unicode data";
-    homepage = https://julialang.org/utf8proc;
+    homepage = "https://juliastrings.github.io/utf8proc/";
     license = licenses.mit;
     platforms = platforms.all;
     maintainers = [ maintainers.ftrvxmtrx ];
