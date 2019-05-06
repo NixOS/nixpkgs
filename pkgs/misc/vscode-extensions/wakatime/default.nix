11aca36b9eb40fb51f9ae0e7550c22919275cc1b
Edmund Wu <fangkazuto@gmail.com>
2019-05-05 21:45:44 -0400

    vscode-extensions.WakaTime.vscode-wakatime: 2.0.7 -> 2.0.9

--- pkgs/misc/vscode-extensions/wakatime/default.nix
+++ pkgs/misc/vscode-extensions/wakatime/default.nix
@@ -8,8 +8,8 @@ in
     mktplcRef = {
       name = "vscode-wakatime";
       publisher = "WakaTime";
-      version = "2.0.7";
-      sha256 = "049m57s8hcaa3q812331vgpaibkidf3x221hyxpglfk3sarxvqwv";
+      version = "2.0.9";
+      sha256 = "0fbliim80ydq90c606jhl79fdcmmpb9fs402mvy3g9zmjixspnpb";
     };
 
     postPatch = ''
