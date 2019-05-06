233d4bb9a77e5dd64361a952d7afbcdefb69e810
(cdep)illabout <cdep.illabout@gmail.com>
2019-05-06 22:21:33 +0900

    haskell: add a callStack2nix function
    callStack2nix is a function similar to callCabal2nix, but instead of
    just creating a .nix file for a single Haskell package, it creates a
    whole overlay based on an Stackage LTS resolver.
    
    This makes it very easy to build a Stack-based Haskell project with Nix.

--- pkgs/development/haskell-modules/make-package-set.nix
+++ pkgs/development/haskell-modules/make-package-set.nix
@@ -315,6 +315,245 @@ in package-set { inherit pkgs stdenv callPackage; } self // {
           else "${ghcEnv}/lib/${ghcCommand}-${ghc.version}";
       });
 
+    # This uses stack2nix to create a .nix file corresponding to a Haskell
+    # package set for a Stack project.  This .nix file can either be copied
+    # into your project and used as-is, or it can be used directly with the
+    # callStack2nixPkgSet function.
+    #
+    # This function is meant for easily creating a package set for building a
+    # Stack-based project.
+    #
+    # Here's an example of how to use this:
+    #
+    #   haskellPackages.callStack2nix {
+    #     name = "purescript";
+    #     src = fetchFromGitHub {
+    #       owner = "purescript";
+    #       repo = "purescript";
+    #       rev = "v0.12.4";
+    #       sha256 = "0lgfmqrn2j1fipgghxrccd9n5f8sw1ncc7fb25201z6x3klzy7bb";
+    #     };
+    #     sha256 = "1in80b914h46hsscdvkmfz0042181lxy2cj522gzyc36l4qq560w";
+    #     hackageSnapshotTimestamp = "2019-05-06T00:00:00Z";
+    #     compiler = haskell.compiler.ghc864;
+    #   };
+    #
+    # This creates a .nix file containing a Haskell package set with Haskell
+    # package versions determined by the LTS used to build PureScript.  This
+    # example is using LTS-13.12 (which is determined by the stack.yaml file in
+    # the purescript repo).
+    #
+    # See the callStack2nixPkgSet for how to import and build this resulting
+    # package set.
+    #
+    # In general, this function won't be used within nixpkgs, but instead
+    # by end-users who want an easy way to build a stack-based project with nix.
+    #
+    # This produces a fixed-output derivation.  See the comment on the sha256
+    # argument for a little more info about this.  This means that
+    # callStack2nix is suited for building Haskell tools, but not suited for
+    # interactive development on Haskell projects.
+    #
+    # For instance, with the PureScript example above, callStack2nix is
+    # convenient to use if you're working on a frontend project written in
+    # PureScript, and you need the PureScript compiler available in your
+    # environment.  However, callStack2nix should not be used if you want to
+    # hack on the PureScript compiler itself.
+    #
+    # TODO: Currently this doesn't work for stack.yaml files that have extra-deps
+    # that come from places other than Hackage.  This is due to an unfortunate
+    # interaction between stack2nix and cabal2nix.  In theory, this should be able
+    # to be fixed.
+    callStack2nix =
+      { # The source for the Haskell package that contains a stack.yaml file.
+        src
+      , # Hash for the .nix file that is produced by stack2nix.
+        #
+        # It is not possible to know this before running stack2nix.  You are
+        # suggested to first run this with a known incorrect sha256 hash, like
+        # "a24341d51da6db9f9f0edcdefe185dbd7726888ec4e230855fb9871de7c07717".
+        # Nix will complain that the hash is incorrect and will tell you the
+        # correct hash.  Then you should fill in the correct hash and run
+        # the derivation again.
+        #
+        # Since the derivation produced by callStack2nix is a fixed-output
+        # derivation, you must be sure to update the sha256 if you update
+        # src, hackageSnapshotTimestamp, compiler, etc.  If you only update
+        # src (or hackageSnapshotTimestamp, etc) and don't update sha256, nix
+        # won't realize that it should re-run callStack2nix.
+        sha256
+      , # A time stamp specifying a Hackage snapshot version.  In order to
+        # make the output of stack2nix reproducible, stack2nix must always
+        # look at the same state of Hackage.  This is needed because the
+        # packages on Hackage are not actually immutable.
+        #
+        # example: "2019-04-16T00:00:00Z"
+        #
+        # TODO: It should be possible to use stack2nix to figure out what
+        # resolver a package is using (like lts-13.7), then automatically
+        # take the release date of the resolver as the
+        # hackageSnapshotTimestamp.
+        hackageSnapshotTimestamp
+      , # Which GHC to use for stack2nix.  This needs to the GHC version
+        # used the resolver in the stack.yaml file.
+        #
+        # example: haskell.compiler.ghc865
+        compiler
+      , # The name for the package.  This is only used to create the
+        # derivation name for the produced .nix file.  It should either
+        # be a string, or null.
+        #
+        # example: "purescript"
+        name ? null
+      , stack2nix ? buildPackages.haskellPackages.stack2nix_0_2_3
+      , cabal-install ? buildPackages.cabal-install
+      , stdenv ? pkgs.stdenv
+      , cacert ? pkgs.cacert
+      , git ? pkgs.git
+      , iana-etc ? pkgs.iana-etc
+      , libredirect ? pkgs.libredirect
+      , doCheck ? true
+      , doHaddock ? true
+      , doBenchmark ? false
+      }:
+      assert builtins.isString hackageSnapshotTimestamp;
+      stdenv.mkDerivation {
+        name = "stack2nix${if isNull name then "" else "-for-" + name}.nix";
+
+        nativeBuildInputs = [
+          cabal-install
+          cacert
+          compiler
+          git
+          iana-etc
+          libredirect
+          stack2nix
+        ];
+
+        phases = ["installPhase"];
+
+        LANG = "en_US.UTF-8";
+
+        outputHashMode = "flat";
+        outputHashAlgo = "sha256";
+        outputHash = sha256;
+
+        # Certificates need to be overridden for git and Haskell packages.
+        GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+        NIX_SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+        SYSTEM_CERTIFICATE_PATH = "${cacert}/etc/ssl/certs";
+
+        installPhase = ''
+          # Make sure /etc/protocols is available because the libraries stack
+          # depends on use it.
+          export NIX_REDIRECTS=/etc/protocols=${iana-etc}/etc/protocols
+          export LD_PRELOAD=${libredirect}/lib/libredirect.so
+
+          # Set $HOME because stack needs it.
+          export HOME="$TMP"
+
+          # Make a temporary directory.  stack2nix uses stack internally.
+          # stack creates a .stack-work/ directory inside the source code
+          # directory it is trying to build.  Since the source code directory
+          # we are using is in /nix/store and not writable, we copy the
+          # source code to a temporary directory so that stack is able to
+          # create a .stack-work/ directory inside of it.
+          #
+          # Below, the temporary source directory is replaced with the path
+          # to the actual source directory from /nix/store. The name of the
+          # temporary source directory needs to be long enough so it is
+          # reliably unique.
+          temp_src_dir=$(mktemp -d stack2nix-temp.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX)
+          cp --no-target-directory -r ${src} $temp_src_dir
+          chmod -R ugo+rwx $temp_src_dir
+
+          # Make sure .stack-work doesn't already exist in the source directory.
+          rm -rf $temp_src_dir/.stack-work
+
+          stack2nix \
+            ${pkgs.lib.optionalString doCheck "--test"} \
+            ${pkgs.lib.optionalString doHaddock "--haddock"} \
+            ${pkgs.lib.optionalString doBenchmark "--bench"} \
+            -o $out \
+            --hackage-snapshot ${hackageSnapshotTimestamp} \
+            --verbose \
+            --no-ensure-executables \
+            $temp_src_dir
+
+          # stack2nix creates the output nix file with a reference to the
+          # temporary source directory from above.  We need to replace this
+          # with the actual source directory from /nix/store.
+          substituteInPlace $out --replace "$temp_src_dir" "${src}"
+        '';
+      };
+
+    # This uses stack2nix to create a Haskell package set for a Stack project.
+    # This package set contains all the Haskell packages defined in the Stack
+    # project.
+    #
+    # This function is meant for easily building a Stack-based project with
+    # nix.
+    #
+    # Here's an example of how to use this:
+    #
+    #   let
+    #     purescriptPkgSet = haskellPackages.callStack2nixPkgSet {
+    #       name = "purescript";
+    #       src = fetchFromGitHub {
+    #         owner = "purescript";
+    #         repo = "purescript";
+    #         rev = "v0.12.4";
+    #         sha256 = "0lgfmqrn2j1fipgghxrccd9n5f8sw1ncc7fb25201z6x3klzy7bb";
+    #       };
+    #       sha256 = "1in80b914h46hsscdvkmfz0042181lxy2cj522gzyc36l4qq560w";
+    #       hackageSnapshotTimestamp = "2019-05-06T00:00:00Z";
+    #       haskellPackagesCompiler = haskell.packages.ghc864;
+    #     };
+    #   in purescriptPkgSet.purescript
+    #
+    # This is an example of building the PureScript compiler.
+    #
+    # This purescriptPkgSet is a normal Haskell package set (similar to
+    # haskellPackages) that contains all the packages defined in the
+    # stackage resolver from the stack.yaml file in the PureScript repo.
+    # It contains packages like purescript, aeson, protolude, etc.
+    #
+    # If you want to override some of the packages used to build PureScript,
+    # you can do it the same way you'd normally override Haskell packages.
+    #
+    # See the callStack2nix function for some of the limitations of this
+    # approach.
+    callStack2nixPkgSet =
+      { # callStack2nix produces a derivation that takes a nix package set
+        # as an argument.  This pkgs argument is passed to that derivation.
+        # By default, it uses the nixpkgs that contains this function.
+        pkgs ? pkgs
+      , # Which Haskell package set to use for stack2nix.  This needs to the GHC
+        # version used in the resolver in the stack.yaml file.  This is passed to
+        # the derivation created from stack2nix and overridden with the Haskell
+        # package versions from the generated .nix file.
+        #
+        # example: haskell.packages.ghc865
+        haskellPackagesCompiler
+      , # Additional arguments are the same as those for the callStack2nix
+        # function.
+        ...
+      }@args:
+      let
+        callStack2nixArgs =
+          builtins.removeAttrs args ["pkgs" "haskellPackagesCompiler"] // {
+            compiler = haskellPackagesCompiler.ghc;
+          };
+
+        pkgSetNixFileDrv = self.callStack2nix callStack2nixArgs;
+
+        pkgSet = import pkgSetNixFileDrv {
+          inherit pkgs;
+          compiler = haskellPackagesCompiler;
+        };
+
+      in pkgSet;
+
     ghc = ghc // {
       withPackages = self.ghcWithPackages;
       withHoogle = self.ghcWithHoogle;
