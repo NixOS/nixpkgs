addb1b2fc5337caab06e9ca0875c2000da261d73
Robin Gloster <mail@glob.in>
2019-05-08 00:42:47 +0200

    stdenv.mkDerivation: fix configureFlags

--- pkgs/build-support/setup-hooks/multiple-outputs.sh
+++ pkgs/build-support/setup-hooks/multiple-outputs.sh
@@ -70,14 +70,14 @@ _multioutConfig() {
         fi
     fi
 
-    configureFlags="\
-        --bindir=${!outputBin}/bin --sbindir=${!outputBin}/sbin \
-        --includedir=${!outputInclude}/include --oldincludedir=${!outputInclude}/include \
-        --mandir=${!outputMan}/share/man --infodir=${!outputInfo}/share/info \
-        --docdir=${!outputDoc}/share/doc/${shareDocName} \
-        --libdir=${!outputLib}/lib --libexecdir=${!outputLib}/libexec \
-        --localedir=${!outputLib}/share/locale \
-        $configureFlags"
+    configureFlags=(
+        "--bindir=${!outputBin}/bin" "--sbindir=${!outputBin}/sbin"
+        "--includedir=${!outputInclude}/include" "--oldincludedir=${!outputInclude}/include"
+        "--mandir=${!outputMan}/share/man" "--infodir=${!outputInfo}/share/info"
+        "--docdir=${!outputDoc}/share/doc/${shareDocName}"
+        "--libdir=${!outputLib}/lib" "--libexecdir=${!outputLib}/libexec"
+        "--localedir=${!outputLib}/share/locale"
+        ${configureFlags[@]})
 
     installFlags="\
         pkgconfigdir=${!outputDev}/lib/pkgconfig \
