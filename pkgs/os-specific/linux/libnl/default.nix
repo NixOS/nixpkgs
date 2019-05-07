d8a74e9a0c85bf0f9a862084b8603b161fc6461e
Robin Gloster <mail@glob.in>
2019-05-07 02:50:05 +0200

    libnl: disable pythonSupport for cross

--- pkgs/os-specific/linux/libnl/default.nix
+++ pkgs/os-specific/linux/libnl/default.nix
@@ -1,5 +1,5 @@
 { stdenv, file, lib, fetchFromGitHub, fetchpatch, autoreconfHook, bison, flex, pkgconfig
-, pythonSupport ? true, swig ? null, python}:
+, pythonSupport ? stdenv.buildPlatform == stdenv.hostPlatform, swig ? null, python}:
 
 stdenv.mkDerivation rec {
   name = "libnl-${version}";
