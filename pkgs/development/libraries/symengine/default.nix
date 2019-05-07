126b740ca24289cb5e1e8e2e59aa67c54788e3e2
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-05-07 11:27:07 -0400

    symengine: init at 0.3.0
    symengine: init at 0.3.0

--- pkgs/development/libraries/symengine/default.nix
+++ pkgs/development/libraries/symengine/default.nix
@@ -3,26 +3,35 @@
 , cmake
 , gmp
 , flint
+, mpfr
+, libmpc
 }:
 
 stdenv.mkDerivation rec {
   name = "symengine-${version}";
-  version = "0.3.0";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "symengine";
     repo = "symengine";
     rev = "v${version}";
-    sha256 = "1p7hk163rgn1zzvjlq4vskblry3s2rg5bc7xlr08wfqckfr47bqc";
+    sha256 = "1kz893p3pmsw3gfwickk2nliw8p63yp89xriad7kpw4kmhvgr8gb";
   };
 
-  buildInputs = [ cmake gmp flint ];
+  nativeBuildInputs = [ cmake ];
+
+  buildInputs = [ gmp flint mpfr libmpc ];
 
   cmakeFlags = [
     "-DWITH_FLINT=ON"
+    "-DINTEGER_CLASS=flint"
+    "-DWITH_SYMENGINE_THREAD_SAFE=yes"
+    "-DWITH_MPC=yes"
+    "-DBUILD_FOR_DISTRIBUTION=yes"
   ];
 
   doCheck = true;
+
   checkPhase = ''
     ctest
   '';
