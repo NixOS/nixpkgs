05d6273357a659c961046905ae8e6a18618be858
Loïc Fontaine <loicfontaine@fastmail.fm>
2019-05-07 14:11:42 +0200

    rgbds: 0.2.4 -> 0.3.8
    - Switch to the new GitHub organization.
    - Updated to the latest published version.

--- pkgs/development/compilers/rgbds/default.nix
+++ pkgs/development/compilers/rgbds/default.nix
@@ -1,21 +1,35 @@
-{stdenv, fetchFromGitHub, yacc}:
+{stdenv, fetchFromGitHub, bison, flex, pkg-config, libpng}:
+
+# TODO: byacc is the recommended parser generator but due to https://github.com/rednex/rgbds/issues/333
+# it does not work for the moment. We should switch back to byacc as soon as the fix is integrated
+# in a published version.
 
 stdenv.mkDerivation rec {
   name = "rgbds-${version}";
-  version = "0.2.4";
+  version = "0.3.8";
   src = fetchFromGitHub {
-    owner = "bentley";
+    owner = "rednex";
     repo = "rgbds";
     rev = "v${version}";
-    sha256 = "0dwq0p9g1lci8sm12a2rfk0g33z2vr75x78zdf1g84djwbz8ipc6";
+    sha256 = "0db37z886026svhj6qnc3wk56sndbnz1vi41gn2k3bl6ppbnjlpk";
   };
-  nativeBuildInputs = [ yacc ];
+  nativeBuildInputs = [ bison flex pkg-config libpng ];
   installFlags = "PREFIX=\${out}";
 
   meta = with stdenv.lib; {
-    homepage = https://www.anjbe.name/rgbds/;
-    description = "An assembler/linker package that produces Game Boy programs";
-    license = licenses.free;
+    homepage = https://rednex.github.io/rgbds/;
+    description = "A free assembler/linker package for the Game Boy and Game Boy Color";
+    license = licenses.mit;
+    longDescription =
+      ''RGBDS (Rednex Game Boy Development System) is a free assembler/linker package for the Game Boy and Game Boy Color. It consists of:
+
+          - rgbasm (assembler)
+          - rgblink (linker)
+          - rgbfix (checksum/header fixer)
+          - rgbgfx (PNG‐to‐Game Boy graphics converter)
+
+        This is a fork of the original RGBDS which aims to make the programs more like other UNIX tools.
+      '';
     maintainers = with maintainers; [ matthewbauer ];
     platforms = platforms.all;
   };
