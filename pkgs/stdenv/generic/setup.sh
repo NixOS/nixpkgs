8cba3e5a2981d43c57f8f1e40fa335c189dda43f
Robin Gloster <mail@glob.in>
2019-05-09 02:19:20 +0200

    stdenv.mkDerivation: fix prev broken propagatedDeps

--- pkgs/stdenv/generic/setup.sh
+++ pkgs/stdenv/generic/setup.sh
@@ -1138,14 +1138,14 @@ fixupPhase() {
     # Propagate dependencies & setup hook into the development output.
     declare -ra flatVars=(
         # Build
-        ${depsBuildBuildPropagated[@]}
-        ${propagatedNativeBuildInputs[@]}
-        ${depsBuildTargetPropagated[@]}
+        depsBuildBuildPropagated
+        propagatedNativeBuildInputs
+        depsBuildTargetPropagated
         # Host
-        ${depsHostHostPropagated[@]}
-        ${propagatedBuildInputs[@]}
+        depsHostHostPropagated
+        propagatedBuildInputs
         # Target
-        ${depsTargetTargetPropagated[@]}
+        depsTargetTargetPropagated
     )
     declare -ra flatFiles=(
         "${propagatedBuildDepFiles[@]}"
