5cff040755a6bc7f8010e554df63f698ac8e4172
Peter Simons <simons@cryp.to>
2019-05-09 14:44:04 +0000

    haskell-src-exts: apply patches (and update) to fix the build with ghc-8.8.x

--- pkgs/development/haskell-modules/configuration-ghc-8.8.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.8.x.nix
@@ -120,6 +120,10 @@ self: super: {
     url = "https://raw.githubusercontent.com/hvr/head.hackage/master/patches/zlib-0.6.2.patch";
     sha256 = "13fy730z9ihyc9kw3qkh642mi0bdbd7bz01dksj1zz845pr9jjif";
   });
+  haskell-src-exts = appendPatch super.haskell-src-exts_1_21_0 (pkgs.fetchpatch {
+    url = "https://raw.githubusercontent.com/hvr/head.hackage/master/patches/haskell-src-exts-1.21.0.patch";
+    sha256 = "0alb28hcsp774c9s73dgrajcb44vgv1xqfg2n5a9y2bpyngqscs3";
+  });
   optparse-applicative = appendPatch super.optparse-applicative (pkgs.fetchpatch {
     url = "https://raw.githubusercontent.com/hvr/head.hackage/master/patches/optparse-applicative-0.14.3.0.patch";
     sha256 = "068sjj98jqiq3h8h03mg4w2pa11q8lxkx2i4lmxivq77xyhlwq3y";
