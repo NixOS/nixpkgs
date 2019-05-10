f913871a75d94f20b6acf0c4efdcaf26209ad0da
Bas van Dijk <v.dijk.bas@gmail.com>
2019-05-10 22:46:12 +0200

    elk5: 5.6.9 -> 5.6.16
    (cherry picked from commit 8673d82bc18d3c6af28b1e3fe5c109276b5121ed)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2519,7 +2519,7 @@ in
   evemu = callPackage ../tools/system/evemu { };
 
   # The latest version used by elasticsearch, logstash, kibana and the the beats from elastic.
-  elk5Version = "5.6.9";
+  elk5Version = "5.6.16";
   elk6Version = "6.7.1";
   elk7Version = "7.0.1";
 
