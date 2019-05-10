728e17bfd1f20477e5f595130d2da7423a4c9a54
Tom McLaughlin <pyro777@gmail.com>
2019-05-09 20:12:04 -0700

    Be able to generate Python environments with runtime manylinux1 support

--- pkgs/development/interpreters/python/mk-python-derivation.nix
+++ pkgs/development/interpreters/python/mk-python-derivation.nix
@@ -46,6 +46,10 @@
 # Skip wrapping of python programs altogether
 , dontWrapPythonPrograms ? false
 
+# Enable manylinux1 support in generated binaries, by providing the
+# libraries listed in PEP 513 in LD_LIBRARY_PATH
+, manylinux1 ? false
+
 # Remove bytecode from bin folder.
 # When a Python script has the extension `.py`, bytecode is generated
 # Typically, executables in bin have no extension, so no bytecode is generated.
@@ -67,7 +71,7 @@ then throw "${name} not supported for interpreter ${python.executable}"
 else
 
 let self = toPythonModule (python.stdenv.mkDerivation (builtins.removeAttrs attrs [
-    "disabled" "checkInputs" "doCheck" "doInstallCheck" "dontWrapPythonPrograms" "catchConflicts"
+    "disabled" "checkInputs" "doCheck" "doInstallCheck" "dontWrapPythonPrograms" "catchConflicts" "makeWrapperArgs"
   ] // {
 
   name = namePrefix + name;
@@ -91,6 +95,12 @@ let self = toPythonModule (python.stdenv.mkDerivation (builtins.removeAttrs attr
   doCheck = false;
   doInstallCheck = doCheck;
   installCheckInputs = checkInputs;
+
+  makeWrapperArgs = makeWrapperArgs ++ (lib.optionals manylinux1 [
+    "--set"
+    "LD_LIBRARY_PATH"
+    (lib.makeLibraryPath [(lib.callPackage ./manylinux1.nix {}).package])
+  ]);
 
   postFixup = lib.optionalString (!dontWrapPythonPrograms) ''
     wrapPythonPrograms
