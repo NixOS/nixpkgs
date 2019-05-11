033e44204562d0432c52bf0d75602dd9e75a436a
Renaud <c0bw3b@users.noreply.github.com>
2019-05-11 10:55:23 +0200

    libxmlb: restrict to linux platforms
    It fails to build on Darwin

--- pkgs/development/libraries/libxmlb/default.nix
+++ pkgs/development/libraries/libxmlb/default.nix
@@ -33,6 +33,6 @@ stdenv.mkDerivation rec {
     homepage = https://github.com/hughsie/libxmlb;
     license = licenses.lgpl21Plus;
     maintainers = with maintainers; [ jtojnar ];
-    platforms = platforms.unix;
+    platforms = platforms.linux;
   };
 }
