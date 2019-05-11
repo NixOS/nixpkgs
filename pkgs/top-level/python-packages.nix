54f9e90e4f9da32e0b00590de710e7f344a32ec9
Daniël de Kok <me@danieldk.eu>
2019-05-11 15:30:13 +0200

    pythonPackages.blis: init at 0.2.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5429,6 +5429,8 @@ in {
 
   backports_weakref = callPackage ../development/python-modules/backports_weakref { };
 
+  blis = callPackage ../development/python-modules/blis { };
+
   srsly = callPackage ../development/python-modules/srsly { };
 
   thinc = callPackage ../development/python-modules/thinc { };
