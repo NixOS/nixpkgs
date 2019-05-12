7c9de797bc807020c56b02082a709eeb0d818e12
R. RyanTM <ryantm-bot@ryantm.com>
2019-05-12 10:38:04 -0700

    python37Packages.django_guardian: 1.5.0 -> 1.5.1 (#61364)
    * python37Packages.django_guardian: 1.5.0 -> 1.5.1
    
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-django-guardian/versions
    
    * pythonPackages.django-guardian: reenable on Py27

--- pkgs/development/python-modules/django_guardian/default.nix
+++ pkgs/development/python-modules/django_guardian/default.nix
@@ -1,14 +1,14 @@
-{ stdenv, buildPythonPackage, fetchPypi, isPy3k
+{ stdenv, buildPythonPackage, fetchPypi
 , django_environ, mock, django, six
 , pytest, pytestrunner, pytest-django
 }:
 buildPythonPackage rec {
   pname = "django-guardian";
-  version = "1.5.0";
+  version = "1.5.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "9e144bbdfa67f523dc6f70768653a19c0aac29394f947a80dcb8eb7900840637";
+    sha256 = "0fixr2g5amdgqzh0rvfvd7hbxyfd5ra3y3s0fsmp8i1b68p97930";
   };
 
   checkInputs = [ pytest pytestrunner pytest-django django_environ mock ];
@@ -18,6 +18,5 @@ buildPythonPackage rec {
     description = "Per object permissions for Django";
     homepage = https://github.com/django-guardian/django-guardian;
     license = [ licenses.mit licenses.bsd2 ];
-    broken = !isPy3k; # https://github.com/django-guardian/django-guardian/pull/605
   };
 }
