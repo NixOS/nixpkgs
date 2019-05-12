8f29000370b396638a6270993bae0931e4a1e789
Niklas Hambüchen <mail@nh2.me>
2019-05-12 16:28:27 +0200

    pkgsStatic: Add libffi override

--- pkgs/top-level/static.nix
+++ pkgs/top-level/static.nix
@@ -72,6 +72,7 @@ in {
   ncurses = super.ncurses.override {
     enableStatic = true;
   };
+  libffi = super.libffi.overrideAttrs (old: { dontDisableStatic = true; });
   libxml2 = super.libxml2.override {
     enableShared = false;
     enableStatic = true;
