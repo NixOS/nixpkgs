44d32929f6a85202d939c0b4e5aa50e93f8979f8
Jason Felice <jason.m.felice@gmail.com>
2019-05-12 11:34:33 -0400

    kakounePlugins.kak-ansi: init at 0.2.1

--- pkgs/applications/editors/kakoune/plugins/default.nix
+++ pkgs/applications/editors/kakoune/plugins/default.nix
@@ -3,6 +3,7 @@
 {
   inherit parinfer-rust;
 
+  kak-ansi = pkgs.callPackage ./kak-ansi.nix { };
   kak-auto-pairs = pkgs.callPackage ./kak-auto-pairs.nix { };
   kak-buffers = pkgs.callPackage ./kak-buffers.nix { };
   kak-fzf = pkgs.callPackage ./kak-fzf.nix { };
