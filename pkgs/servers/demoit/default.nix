ec9d4497f0b4280a12c9c02338b5be803fff12d6
freezeboy <freezeboy@users.noreply.github.com>
2019-05-12 18:13:43 +0200

    fixing invalid sha256

--- pkgs/servers/demoit/default.nix
+++ pkgs/servers/demoit/default.nix
@@ -12,7 +12,7 @@ buildGoPackage rec {
     owner = "dgageot";
     repo = "demoit";
     rev = "c1d4780620ebf083cb4a81b83c80e7547ff7bc23";
-    sha256 = "03h0ad3v9xfigrppbcxiykzgcrv3dnarm7m1vixi2j4n1340f7q6";
+    sha256 = "0l0pw0kzgnrk6a6f4ls3s82icjp7q9djbaxwfpjswbcfdzrsk4p2";
   };
 
   meta = with stdenv.lib; {
