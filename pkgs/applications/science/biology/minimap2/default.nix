ad5676f8b929d2b3d6ccdc7b761e96d440112c8d
R. RyanTM <ryantm-bot@ryantm.com>
2019-05-11 21:12:42 -0700

    minimap2: 2.16 -> 2.17
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/minimap2/versions

--- pkgs/applications/science/biology/minimap2/default.nix
+++ pkgs/applications/science/biology/minimap2/default.nix
@@ -3,13 +3,13 @@
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
   pname = "minimap2";
-  version = "2.16";
+  version = "2.17";
 
   src = fetchFromGitHub {
     repo = pname;
     owner = "lh3";
     rev = "v${version}";
-    sha256 = "1ggm5psv3gwsz627ik9kl6ry9gzgmfsvya6ni0gv6ahwlrhdim73";
+    sha256 = "0qdwlkib3aa6112372hdgvnvk86hsjjkhjar0p53pq4ajrr2cdlb";
   };
 
   buildInputs = [ zlib ];
