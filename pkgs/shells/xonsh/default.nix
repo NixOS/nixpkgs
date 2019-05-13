b324116100aa8d8c406f2c423ea0e0d9569d1d94
R. RyanTM <ryantm-bot@ryantm.com>
2019-05-13 05:41:00 -0700

    xonsh: 0.8.12 -> 0.9.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/xonsh/versions

--- pkgs/shells/xonsh/default.nix
+++ pkgs/shells/xonsh/default.nix
@@ -2,14 +2,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "xonsh";
-  version = "0.8.12";
+  version = "0.9.0";
 
   # fetch from github because the pypi package ships incomplete tests
   src = fetchFromGitHub {
     owner  = "scopatz";
     repo   = "xonsh";
     rev    = "refs/tags/${version}";
-    sha256 = "1p8slx8nk15vyyzwc9ic50py0amk9p9nggp1973wfas0fxsg1r4f";
+    sha256 = "1833xqw5y65cv3c71skqix1xh3qqgv1kps1500cczpyrc8daka23";
   };
 
   LC_ALL = "en_US.UTF-8";
