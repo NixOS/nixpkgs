4f3012ee355c60265b81a5ec642633fc00971980
midchildan <git@midchildan.org>
2019-05-13 21:18:05 +0900

    nyancat: init at 1.5.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4841,6 +4841,8 @@ in
 
   nwdiag = with python3Packages; toPythonApplication nwdiag;
 
+  nyancat = callPackage ../tools/misc/nyancat { };
+
   nylon = callPackage ../tools/networking/nylon { };
 
   nzbget = callPackage ../tools/networking/nzbget { };
