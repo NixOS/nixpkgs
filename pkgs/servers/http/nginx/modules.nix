872f056bb40155f49a964255b80a235f7c241223
Izorkin <izorkin@elven.pw>
2019-05-13 12:33:20 +0300

    nginxModules.lua: 0.10.14 -> 0.10.15

--- pkgs/servers/http/nginx/modules.nix
+++ pkgs/servers/http/nginx/modules.nix
@@ -132,8 +132,8 @@ in
     src = fetchFromGitHub {
       owner = "openresty";
       repo = "lua-nginx-module";
-      rev = "v0.10.14";
-      sha256 = "1vg6pp9n8z42p6f3jqk15gvdyy7mfvs5ssfbiy83bydjx42fq7g0";
+      rev = "v0.10.15";
+      sha256 = "1j216isp0546hycklbr5wi8mlga5hq170hk7f2sm16sfavlkh5gz";
     };
     inputs = [ pkgs.luajit ];
     preConfigure = ''
