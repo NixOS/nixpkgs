6dc34b838472429d4179d0c6fc298a234b81dce6
Lily Ballard <lily@sb.org>
2019-05-15 14:46:56 -0700

    bat: 0.10.0 -> 0.11.0
    Also add myself as a maintainer, and set platforms to `all`.

--- pkgs/tools/misc/bat/default.nix
+++ pkgs/tools/misc/bat/default.nix
@@ -3,18 +3,18 @@
 }:
 
 rustPlatform.buildRustPackage rec {
-  name    = "bat-${version}";
-  version = "0.10.0";
+  pname   = "bat";
+  version = "0.11.0";
 
   src = fetchFromGitHub {
     owner  = "sharkdp";
-    repo   = "bat";
+    repo   = pname;
     rev    = "v${version}";
-    sha256 = "1q22lbyrwh58vhznpjpkiaa8v4qv6a3a8lrxzaypd8wg78p9dca6";
+    sha256 = "0yyvlplskjvxb2cspqsvfsnahd5m0s83psrp777ng0wc0kr1adbw";
     fetchSubmodules = true;
   };
 
-  cargoSha256 = "0npj2rf4vr45gq3qwqq6kqnv9dh58v5lpx0gsmy2qrq44dxb75rq";
+  cargoSha256 = "078n31c0isvxvna0s1m12xv4bkh15rb2nixfyg4c501mlkalb517";
 
   nativeBuildInputs = [ cmake pkgconfig zlib ];
 
@@ -22,13 +22,14 @@ rustPlatform.buildRustPackage rec {
 
   postInstall = ''
     install -m 444 -Dt $out/share/man/man1 doc/bat.1
+    install -m 444 -Dt $out/share/fish/vendor_completions.d assets/completions/bat.fish
   '';
 
   meta = with stdenv.lib; {
     description = "A cat(1) clone with syntax highlighting and Git integration";
     homepage    = https://github.com/sharkdp/bat;
     license     = with licenses; [ asl20 /* or */ mit ];
-    maintainers = with maintainers; [ dywedir ];
-    platforms   = platforms.linux ++ platforms.darwin;
+    maintainers = with maintainers; [ dywedir lilyball ];
+    platforms   = platforms.all;
   };
 }
