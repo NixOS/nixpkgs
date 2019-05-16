d04b5fc16453dc3106e5aadec216d5a08876b0f4
worldofpeace <worldofpeace@protonmail.ch>
2019-05-16 18:53:45 -0400

    pantheon.elementary-photos: 2.6.3 -> 2.6.4
    Patch in tree is removed because it's included upstream.
    
    https://github.com/elementary/photos/releases/tag/2.6.4
    (cherry picked from commit 632591818a26065c378b3acafb9b275fbe9ce66a)

--- pkgs/desktops/pantheon/apps/elementary-photos/default.nix
+++ pkgs/desktops/pantheon/apps/elementary-photos/default.nix
@@ -5,7 +5,7 @@
 
 stdenv.mkDerivation rec {
   pname = "photos";
-  version = "2.6.3";
+  version = "2.6.4";
 
   name = "elementary-${pname}-${version}";
 
@@ -13,7 +13,7 @@ stdenv.mkDerivation rec {
     owner = "elementary";
     repo = pname;
     rev = version;
-    sha256 = "1s0ww5g26wj0gd1drj8gxs74gvg2c9fdj4ixpifj8jh8yafdmrvg";
+    sha256 = "17r9658s0pqy6s45ysi3915sm8hpvmsp7cw2jahqvjc61r4qpdc1";
   };
 
   passthru = {
@@ -64,12 +64,6 @@ stdenv.mkDerivation rec {
 
   mesonFlags = [
     "-Dplugins=false"
-  ];
-
-  patches = [
-    # https://github.com/elementary/photos/pull/505
-    # Unrelated line got dropped in https://github.com/elementary/photos/pull/498
-    ./fix-missing-line.patch
   ];
 
   postPatch = ''
