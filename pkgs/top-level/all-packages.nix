23cc9ab5b6e5e593841db93fd6709c258d2e622e
zimbatm <zimbatm@zimbatm.com>
2019-05-16 15:01:32 +0200

    Sylk: init at 2.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6023,6 +6023,8 @@ in
 
   systrayhelper = callPackage ../tools/misc/systrayhelper {};
 
+  Sylk = callPackage ../applications/networking/Sylk {};
+
   otter-browser = qt5.callPackage ../applications/networking/browsers/otter {};
 
   privoxy = callPackage ../tools/networking/privoxy {
