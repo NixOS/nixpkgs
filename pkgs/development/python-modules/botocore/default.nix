6efa24ee077ab9fe5cf1d5b3d63b1c3210263e50
Bas van Dijk <v.dijk.bas@gmail.com>
2019-05-17 15:05:13 +0200

    pythonPackages.botocore: 1.12.96 -> 1.12.150

--- pkgs/development/python-modules/botocore/default.nix
+++ pkgs/development/python-modules/botocore/default.nix
@@ -12,11 +12,11 @@
 
 buildPythonPackage rec {
   pname = "botocore";
-  version = "1.12.96"; # N.B: if you change this, change boto3 and awscli to a matching version
+  version = "1.12.150"; # N.B: if you change this, change boto3 and awscli to a matching version
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "55c1594041e6716847d5a8b38181e3cc44e245edbf4598ae2b99e3040073b2cf";
+    sha256 = "1xrbl9dfb65wpz7rq5n4j2qvb591z1ny5d1s6rn813ws96pl8bpq";
   };
 
   propagatedBuildInputs = [
