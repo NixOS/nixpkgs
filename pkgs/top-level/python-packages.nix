8817c50c3c4b28391419f633328a9c591a3034df
Gabriel Ebner <gebner@gebner.org>
2019-05-19 12:11:18 +0200

    pythonPackages.pyspinel: init at 1.0.0a3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -782,6 +782,8 @@ in {
     slurm = pkgs.slurm;
   };
 
+  pyspinel = callPackage ../development/python-modules/pyspinel {};
+
   pyssim = callPackage ../development/python-modules/pyssim { };
 
   pystache = callPackage ../development/python-modules/pystache { };
