54d14bb4cf024989d9cb2a3d1a88596872f81884
Renaud <c0bw3b@users.noreply.github.com>
2019-05-21 14:14:09 +0200

    pwsafe: 1.07 -> 1.08.1 (#61766)
    now ships 'pwsafe-cli' in addition to wx GUI

--- pkgs/applications/misc/pwsafe/default.nix
+++ pkgs/applications/misc/pwsafe/default.nix
@@ -6,17 +6,17 @@
 
 stdenv.mkDerivation rec {
   pname = "pwsafe";
-  version = "1.07";
+  version = "1.08.1";
 
   src = fetchFromGitHub {
-    owner = "${pname}";
-    repo = "${pname}";
+    owner = pname;
+    repo = pname;
     rev = "${version}BETA";
-    sha256 = "0syxmliybgvm9j6d426l7j12ryrl42azy80m66jc56fv9nkqwaya";
+    sha256 = "0x89pn056h8b4yvxbd6l3qwrghslxc7vlxnblmcmsx7xx4i041ng";
   };
 
   nativeBuildInputs = [ 
-  	cmake gettext perl pkgconfig zip
+    cmake gettext perl pkgconfig zip
   ];
   buildInputs = [
     libXext libXi libXt libXtst wxGTK31
@@ -49,11 +49,10 @@ stdenv.mkDerivation rec {
     done
   '';
 
-  installFlags = [ "PREFIX=$(out)" ];
+  installFlags = [ "PREFIX=${placeholder "out"}" ];
 
   meta = with stdenv.lib; {
     description = "A password database utility";
-
     longDescription = ''
       Password Safe is a password database utility. Like many other
       such products, commercial and otherwise, it stores your
@@ -61,8 +60,7 @@ stdenv.mkDerivation rec {
       one password (the "safe combination"), instead of all the
       username/password combinations that you use.
     '';
-
-    homepage = https://pwsafe.org/;
+    homepage = "https://pwsafe.org/";
     maintainers = with maintainers; [ c0bw3b pjones ];
     platforms = platforms.linux;
     license = licenses.artistic2;
