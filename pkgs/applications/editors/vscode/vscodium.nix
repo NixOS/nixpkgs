7e76488bbe7eebf4347ff7765ca530cd5b4425f0
Edmund Wu <fangkazuto@gmail.com>
2019-05-21 16:43:46 -0400

    vscodium: 1.33.1 -> 1.34.0
    (cherry picked from commit d3e5fc1bfc6fc6e6d4c5d22a468df2b211658300)

--- pkgs/applications/editors/vscode/vscodium.nix
+++ pkgs/applications/editors/vscode/vscodium.nix
@@ -12,14 +12,14 @@ let
   archive_fmt = if system == "x86_64-darwin" then "zip" else "tar.gz";
 
   sha256 = {
-    "i686-linux" = "c45be9ff32a5acef91befc85d545402daac5cef627fe8f17fea40ae387b35b22";
-    "x86_64-linux" = "7a87f19bc48cbe017321c72da244d879a7da364b92b3a89b742d437ffcd585d3";
-    "x86_64-darwin" = "e393be5dd82ea1900e3a0597b6cb2e4c924b6acda4297d9d3eff5abcc857f32c";
+    "i686-linux" = "1vr3mblg5223k56yqi9fqwa7yg8qi4r3nkw6ssf87gs8jwfxa47l";
+    "x86_64-linux" = "1vqq365zw44ll22i06bxxviyywv1v2f71c2mricrz3faz25c3lvm";
+    "x86_64-darwin" = "0pa5cz8kq45q375b74kaa8qn1h0r1mp9amh5gsprg3hx89xzvhxi";
   }.${system};
 in
   callPackage ./generic.nix rec {
 
-    version = "1.33.1";
+    version = "1.34.0";
     pname = "vscodium";
 
     executableName = "vscodium";
