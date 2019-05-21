3ac4434816ebb8bc822df735fe7a9cf1597a7fdd
Marek Mahut <marek.mahut@gmail.com>
2019-05-21 15:26:11 +0000

    indent: 2.2.10 -> 2.2.12

--- pkgs/development/tools/misc/indent/default.nix
+++ pkgs/development/tools/misc/indent/default.nix
@@ -1,12 +1,14 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, texinfo }:
 
 stdenv.mkDerivation rec {
-  name = "indent-2.2.10";
+  name = "indent-2.2.12";
 
   src = fetchurl {
     url = "mirror://gnu/indent/${name}.tar.gz";
-    sha256 = "0f9655vqdvfwbxvs1gpa7py8k1z71aqh8hp73f65vazwbfz436wa";
+    sha256 = "12xvcd16cwilzglv9h7sgh4h1qqjd1h8s48ji2dla58m4706hzg7";
   };
+
+  buildInputs = [ texinfo ];
 
   postPatch = stdenv.lib.optionalString stdenv.isDarwin ''
     sed -i 's|#include <malloc.h>|#include <malloc/malloc.h>|' ./man/texinfo2man.c
@@ -15,9 +17,10 @@ stdenv.mkDerivation rec {
   hardeningDisable = [ "format" ];
 
   meta = {
-    homepage = https://www.gnu.org/software/indent/;
+    homepage = "https://www.gnu.org/software/indent/";
     description = "A source code reformatter";
     license = stdenv.lib.licenses.gpl3Plus;
+    maintainers = [ stdenv.lib.maintainers.mmahut ];
     platforms = stdenv.lib.platforms.unix;
   };
 }
