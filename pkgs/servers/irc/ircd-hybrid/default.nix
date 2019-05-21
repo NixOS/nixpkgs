3746cb9fd24399176eab265b0b23f5bab49a1bb2
R. RyanTM <ryantm-bot@ryantm.com>
2019-05-21 16:42:20 -0700

    ircdHybrid: 8.2.24 -> 8.2.25
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/ircd-hybrid/versions

--- pkgs/servers/irc/ircd-hybrid/default.nix
+++ pkgs/servers/irc/ircd-hybrid/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, openssl, zlib }:
 
 stdenv.mkDerivation rec {
-  name = "ircd-hybrid-8.2.24";
+  name = "ircd-hybrid-8.2.25";
 
   src = fetchurl {
     url = "mirror://sourceforge/ircd-hybrid/${name}.tgz";
-    sha256 = "03nmzrhqfsxwry316nm80m9p285v65fz75ns7fg623hcy65jv97a";
+    sha256 = "07f3n3rzpahszm1h9yrd76h5bl9ca2vd0ahx5dflssmykvsbyvp8";
   };
 
   buildInputs = [ openssl zlib ];
