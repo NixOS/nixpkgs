afcbf85852b07c96f99266991faf95e17e1bd304
László Vaskó <vlaci@balabit.hu>
2019-05-21 19:59:12 +0200

    multipath-tools: 0.6.2 -> 0.8.1
    Also fixing library detection in Makefile. In the previous and the current
    version `/usr/include` was hardcoded in Makefile. I have tested that all
    optional features are found during build. Excerpt from build output:
    
        Checking for dm_task_no_flush in /nix/store/d2x72ihg707mdjvbjz3n4s9v02syr86y-lvm2-2.03.01/include/libdevmapper.h ... yes
        Checking for dm_task_set_cookie in /nix/store/d2x72ihg707mdjvbjz3n4s9v02syr86y-lvm2-2.03.01/include/libdevmapper.h ... yes
        Checking for udev_monitor_set_receive_buffer_size in /nix/store/jvm37ymza8h2l74p0j0l3hxwwsnrha1l-systemd-239.20190219-dev/include/libudev.h ... yes
        Checking for dm_task_deferred_remove in /nix/store/d2x72ihg707mdjvbjz3n4s9v02syr86y-lvm2-2.03.01/include/libdevmapper.h ... yes
        Checking if /nix/store/rz560c4bc3czwzzlli1z2az7hxp8axlh-linux-headers-4.19.16/include/linux/nvme_ioctl.h exists ... yes

--- pkgs/os-specific/linux/multipath-tools/default.nix
+++ pkgs/os-specific/linux/multipath-tools/default.nix
@@ -1,21 +1,38 @@
-{ stdenv, fetchurl, lvm2, libaio, gzip, readline, systemd, liburcu }:
+{ stdenv
+, fetchurl
+, substituteAll
+, gzip
+, json_c
+, libaio
+, liburcu
+, linuxHeaders
+, lvm2
+, perl
+, pkg-config
+, readline
+, systemd
+}:
 
 stdenv.mkDerivation rec {
   name = "multipath-tools-${version}";
-  version = "0.6.2";
+  version = "0.8.1";
 
   src = fetchurl {
     name = "${name}.tar.gz";
     url = "https://git.opensvc.com/?p=multipath-tools/.git;a=snapshot;h=${version};sf=tgz";
-    sha256 = "159hxvbk9kh1qay9x04w0gsqzg0hkl5yghfc1wi9kv2n5pcwbkpm";
+    sha256 = "14q620g0wdflzk1pajqv35zkq4j95rhki9h78q1m9kwmghyx8d6c";
   };
 
   patches = [
-    ./fix-paths.patch
+    (substituteAll {
+      src = ./fix-paths.patch;
+      inherit linuxHeaders lvm2;
+      systemd = systemd.dev;
+    })
   ];
 
-  nativeBuildInputs = [ gzip ];
-  buildInputs = [ systemd lvm2 libaio readline liburcu ];
+  nativeBuildInputs = [ gzip perl pkg-config ];
+  buildInputs = [ systemd lvm2 libaio readline liburcu json_c ];
 
   makeFlags = [
     "LIB=lib"
