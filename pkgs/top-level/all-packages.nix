dd3dc7f2b173a096bed11aca0c9da2d66904defa
Renato Alves <alves.rjc@gmail.com>
2019-05-21 19:01:43 +0200

    docear: init at 1.2.0
    Stable URLs are only provided for older versions.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2525,6 +2525,8 @@ in
 
   docbook2mdoc = callPackage ../tools/misc/docbook2mdoc { };
 
+  docear = callPackage ../applications/office/docear { };
+
   dockbarx = callPackage ../applications/misc/dockbarx { };
 
   dog = callPackage ../tools/system/dog { };
