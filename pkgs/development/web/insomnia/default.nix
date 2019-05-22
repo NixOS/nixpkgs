2f76e46c49da19b2e98c7f69f403e38308b3cd3a
Dmitry Moskowski <me@corpix.ru>
2019-05-22 14:19:52 +0000

    insomnia: 6.3.2 -> 6.5.3

--- pkgs/development/web/insomnia/default.nix
+++ pkgs/development/web/insomnia/default.nix
@@ -18,11 +18,11 @@ let
   ];
 in stdenv.mkDerivation rec {
   name = "insomnia-${version}";
-  version = "6.3.2";
+  version = "6.5.3";
 
   src = fetchurl {
     url = "https://github.com/getinsomnia/insomnia/releases/download/v${version}/insomnia_${version}_amd64.deb";
-    sha256 = "15zf5nmsmz3ajb4xmhm3gynn36qp0ark0gah8qd0hqq76n9jmjnp";
+    sha256 = "0km7anw5xpcfr6j7pwqhv26pk7nxv1jywqlz0lpvgj6w85aafcm3";
   };
 
   nativeBuildInputs = [ 
