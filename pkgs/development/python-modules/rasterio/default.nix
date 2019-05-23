1697a9fe3fc9b2b2ff4d515c3e4b5d71d63e8895
Renaud <c0bw3b@users.noreply.github.com>
2019-05-23 14:16:12 +0200

    python.pkgs.rasterio: fix check phase with Py27

--- pkgs/development/python-modules/rasterio/default.nix
+++ pkgs/development/python-modules/rasterio/default.nix
@@ -16,7 +16,7 @@ buildPythonPackage rec {
     sha256 = "1xypc86prwkv6ypy87g6r1p5y6d6c51vfxd3z6hjd1hv2ks7zjs1";
   };
 
-  checkInputs = [ boto3 pytest pytestcov packaging hypothesis ] ++ lib.optional (!isPy3k) [ mock ];
+  checkInputs = [ boto3 pytest pytestcov packaging hypothesis ] ++ lib.optional (!isPy3k) mock;
   nativeBuildInputs = [ cython gdal ];
   propagatedBuildInputs = [ gdal numpy attrs affine cligj click-plugins snuggs ];
 
