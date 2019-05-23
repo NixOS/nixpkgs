d2838d20c6d0769843111984e476b901af918d97
Eric Bailey <eric@ericb.me>
2019-05-23 12:21:12 +0200

    helmfile: 0.40.1 -> 0.64.1
    - Adopt vgo2nix
    - Add me to maintainers

--- pkgs/applications/networking/cluster/helmfile/default.nix
+++ pkgs/applications/networking/cluster/helmfile/default.nix
@@ -1,6 +1,6 @@
 { lib, buildGoPackage, fetchFromGitHub, makeWrapper, kubernetes-helm, ... }:
 
-let version = "0.40.1"; in
+let version = "0.64.1"; in
 
 buildGoPackage {
   name = "helmfile-${version}";
@@ -9,8 +9,10 @@ buildGoPackage {
     owner = "roboll";
     repo = "helmfile";
     rev = "v${version}";
-    sha256 = "02ir10070rpayv9s53anldwjy5ggl268shgf085d188wl6vshaiv";
+    sha256 = "1258c545fv4mcrzaw3z5gxl264fcahigaijgkjd4igh4pl0z0wxk";
   };
+
+  goDeps = ./deps.nix;
 
   goPackagePath = "github.com/roboll/helmfile";
 
@@ -26,12 +28,11 @@ buildGoPackage {
       --prefix PATH : ${lib.makeBinPath [ kubernetes-helm ]}
   '';
 
-
   meta = {
     description = "Deploy Kubernetes Helm charts";
     homepage = https://github.com/roboll/helmfile;
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ pneumaticat ];
+    maintainers = with lib.maintainers; [ pneumaticat yurrriq ];
     platforms = lib.platforms.unix;
   };
 }
