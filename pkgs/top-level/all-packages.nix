07cac4f9bec76aa3f947570a1d9c3077c06aaa6e
Roman Kuznetsov <roman@kuznero.com>
2019-05-24 00:07:21 +0200

    vnote: init at 2.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23756,6 +23756,8 @@ in
 
   ib-controller = callPackage ../applications/office/ib/controller { jdk=oraclejdk8; };
 
+  vnote = libsForQt5.callPackage ../applications/office/vnote { };
+
   ssh-audit = callPackage ../tools/security/ssh-audit { };
 
   thermald = callPackage ../tools/system/thermald { };
