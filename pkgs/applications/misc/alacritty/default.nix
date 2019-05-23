fabd6ac95549431ea2c1880676c010c62c09b940
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-05-23 00:10:10 -0400

    alacritty: build on aarch64-linux

--- pkgs/applications/misc/alacritty/default.nix
+++ pkgs/applications/misc/alacritty/default.nix
@@ -119,6 +119,6 @@ in buildRustPackage rec {
     homepage = https://github.com/jwilm/alacritty;
     license = with licenses; [ asl20 ];
     maintainers = with maintainers; [ mic92 ];
-    platforms = [ "x86_64-linux" "x86_64-darwin" ];
+    platforms = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" ];
   };
 }
