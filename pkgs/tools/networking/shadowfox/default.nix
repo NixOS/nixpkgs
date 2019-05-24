6c73b6dc1f68f9f0f1d4498d5e216af608f0ac8c
Silvan Mosberger <infinisil@icloud.com>
2019-05-24 15:46:00 +0200

    shadowfox: Don't install compiler binary

--- pkgs/tools/networking/shadowfox/default.nix
+++ pkgs/tools/networking/shadowfox/default.nix
@@ -17,6 +17,12 @@ buildGoModule rec {
 
   buildFlags = "--tags release";
 
+  postInstall = ''
+    # Contains compiler package only used by projects CI, and buildGoModule
+    # installs all binaries by default. So we remove this again.
+    rm $out/bin/compiler
+  '';
+
   meta = with stdenv.lib; {
     description = ''
       This project aims at creating a universal dark theme for Firefox while
