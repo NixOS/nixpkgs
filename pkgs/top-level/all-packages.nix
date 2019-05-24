89c34c59c510e8337cb44c9bb129598067d337cd
José Romildo Malaquias <malaquias@gmail.com>
2019-05-24 08:33:26 -0300

    lumina: move to pkgs/desktops/lumina/lumina

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22070,7 +22070,7 @@ in
 
   kakasi = callPackage ../tools/text/kakasi { };
 
-  lumina = recurseIntoAttrs (callPackage ../desktops/lumina { });
+  lumina = libsForQt5.callPackage ../desktops/lumina/lumina { };
 
   lxqt = recurseIntoAttrs (import ../desktops/lxqt {
     inherit pkgs libsForQt5;
