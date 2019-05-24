45f45138c1b6158ea3f04de406303fe9bf809944
Justin Humm <justin.humm@posteo.de>
2019-05-24 15:58:12 +0200

    opensc: add patch for CVE-2019-6502
    Closes #61957

--- pkgs/tools/security/opensc/default.nix
+++ pkgs/tools/security/opensc/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, zlib, readline, openssl
-, libiconv, pcsclite, libassuan, libXt
+, libiconv, pcsclite, libassuan, libXt, fetchpatch
 , docbook_xsl, libxslt, docbook_xml_dtd_412
 , Carbon, PCSC, buildPackages
 , withApplePCSC ? stdenv.isDarwin
@@ -15,6 +15,14 @@ stdenv.mkDerivation rec {
     rev = version;
     sha256 = "10575gb9l38cskq7swyjp0907wlziyxg4ppq33ndz319dsx69d87";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "CVE-2019-6502.patch";
+      url = "https://github.com/OpenSC/OpenSC/commit/0d7967549751b7032f22b437106b41444aff0ba9.patch";
+      sha256 = "1y42lmz8i9w99hgpakdncnv8f94cqjfabz0v4xg6wfz9akl3ff7d";
+    })
+  ];
 
   nativeBuildInputs = [ pkgconfig autoreconfHook ];
   buildInputs = [
