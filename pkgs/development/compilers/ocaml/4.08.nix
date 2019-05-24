c7f090454e6545c429771b0860feb929e406278c
Vincent Laporte <Vincent.Laporte@gmail.com>
2019-05-24 09:06:22 +0200

    ocaml-ng.ocamlPackages_4_08.ocaml: 4.08.0+beta3 -> 4.08.0+rc1

--- pkgs/development/compilers/ocaml/4.08.nix
+++ pkgs/development/compilers/ocaml/4.08.nix
@@ -1,8 +1,8 @@
 import ./generic.nix {
   major_version = "4";
   minor_version = "08";
-  patch_version = "0+beta3";
-  sha256 = "02pk4bxrgqb12hvpbxyqnl4nwr4g2h96wsfzfd1k8vj8h0jmxzc4";
+  patch_version = "0+rc1";
+  sha256 = "014yincnkfg0j2jy0cn30l5hb1y4sf2qf1gy9ix9ghgn32iw5ndk";
 
   # If the executable is stripped it does not work
   dontStrip = true;
