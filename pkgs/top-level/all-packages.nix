fb679236ef1eb7a156cd4ff1ab968f5df0c597e4
Joachim Fasting <joachifm@fastmail.fm>
2019-05-25 15:11:49 +0200

    eudev: use utillinuxMinimal
    Presumably, the target audience for eudev are people who wish to avoid pulling
    in systemd for whatever reason; it makes sense then to make the default build
    not pull in systemd via utillinux.
    
    (cherry picked from commit a37db0289f1401178a16b191c9d4e3302728d3b5)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15463,7 +15463,7 @@ in
     stdenv = crossLibcStdenv;
   };
 
-  eudev = callPackage ../os-specific/linux/eudev {};
+  eudev = callPackage ../os-specific/linux/eudev { utillinux = utillinuxMinimal; };
 
   libudev0-shim = callPackage ../os-specific/linux/libudev0-shim { };
 
