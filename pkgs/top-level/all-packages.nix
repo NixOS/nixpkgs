0d2c40aafdf2a57b5823b4df11b2a6a533f5869c
Free Potion <42352817+freepotion@users.noreply.github.com>
2019-05-25 13:14:43 +0300

    harmonist: init at 0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21457,6 +21457,8 @@ in
 
   gzdoom = callPackage ../games/gzdoom { };
 
+  harmonist = callPackage ../games/harmonist { };
+
   hawkthorne = callPackage ../games/hawkthorne { love = love_0_9; };
 
   hedgewars = callPackage ../games/hedgewars {
