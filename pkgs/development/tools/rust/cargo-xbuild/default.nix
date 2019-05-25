7415e6a9fc002f3bcffe7365943f937bd56b3ab6
xrelkd <46590321+xrelkd@users.noreply.github.com>
2019-05-25 09:49:23 +0800

    cargo-xbuild: 0.5.8 -> 0.5.9

--- pkgs/development/tools/rust/cargo-xbuild/default.nix
+++ pkgs/development/tools/rust/cargo-xbuild/default.nix
@@ -2,21 +2,22 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-xbuild";
-  version = "0.5.8";
+  version = "0.5.9";
 
   src = fetchFromGitHub {
     owner = "rust-osdev";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1ckrvgdjwkxvy3rc6kix9maynn87al0n7lsjshc9mdmkyvvx8h55";
+    sha256 = "0c4gls25vvkh4hw8apic03zb54m1v69n9ycwcp49c73ky8lrn0vj";
   };
 
-  cargoSha256 = "077qiqm470iqcgxqjzbmzxikxd5862vyg788hacli4yzpvyaq9r9";
+  cargoSha256 = "1r9i79lymfwpbcx2lp509v435qpkl9bqly1ya369p41n5yprrcjv";
 
   meta = with stdenv.lib; {
     description = "Automatically cross-compiles the sysroot crates core, compiler_builtins, and alloc";
     homepage = https://github.com/rust-osdev/cargo-xbuild;
     license = with licenses; [ mit asl20 ];
+    maintainers = with maintainers; [ xrelkd ];
     platforms = platforms.all;
   };
 }
