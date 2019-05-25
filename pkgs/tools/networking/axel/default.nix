c14d683b5e9ddf827e192ee05106149dafe08944
Renaud <c0bw3b@users.noreply.github.com>
2019-05-25 17:58:45 +0200

    axel: update meta.homepage
    alioth.debian.org is gone

--- pkgs/tools/networking/axel/default.nix
+++ pkgs/tools/networking/axel/default.nix
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Console downloading program with some features for parallel connections for faster downloading";
-    homepage = http://axel.alioth.debian.org/;
+    homepage = "https://github.com/axel-download-accelerator/axel";
     maintainers = with maintainers; [ pSub ];
     platforms = with platforms; linux;
     license = licenses.gpl2;
