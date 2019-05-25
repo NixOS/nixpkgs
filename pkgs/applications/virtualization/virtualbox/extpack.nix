b2cf23411f401e8173aee37bb394c9e4d28bfa98
Ambroz Bizjak <abizjak.pro@gmail.com>
2019-05-25 18:25:05 +0200

    virtualbox: 6.0.6 -> 6.0.8

--- pkgs/applications/virtualization/virtualbox/extpack.nix
+++ pkgs/applications/virtualization/virtualbox/extpack.nix
@@ -2,7 +2,7 @@
 
 with lib;
 
-let version = "6.0.6";
+let version = "6.0.8";
 in
 fetchurl rec {
   name = "Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack";
@@ -10,7 +10,7 @@ fetchurl rec {
   sha256 =
     # Manually sha256sum the extensionPack file, must be hex!
     # Thus do not use `nix-prefetch-url` but instead plain old `sha256sum`.
-    let value = "794f023a186bd217c29c3d30bd1434b6e9de3b242c7bf740d06d10f2d3d981c6";
+    let value = "6d89127c7f043fa96592da96ca87ac5ee9a7afd347d788380f91b695b67d7954";
     in assert (builtins.stringLength value) == 64; value;
 
   meta = {
