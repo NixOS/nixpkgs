595911c2f158dc412f296450a83b04d184861cbf
Mario Rodas <marsam@users.noreply.github.com>
2019-05-24 20:03:00 -0500

    pythonPackages.chardet: fix pytest 4 support

--- pkgs/development/python-modules/chardet/default.nix
+++ pkgs/development/python-modules/chardet/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, buildPythonPackage, fetchPypi
-, pytest_3, pytestrunner, hypothesis }:
+{ stdenv, buildPythonPackage, fetchPypi, fetchpatch
+, pytest, pytestrunner, hypothesis }:
 
 buildPythonPackage rec {
   pname = "chardet";
@@ -10,7 +10,15 @@ buildPythonPackage rec {
     sha256 = "1bpalpia6r5x1kknbk11p1fzph56fmmnp405ds8icksd3knr5aw4";
   };
 
-  checkInputs = [ pytest_3 pytestrunner hypothesis ];
+  patches = [
+    # Add pytest 4 support. See: https://github.com/chardet/chardet/pull/174
+    (fetchpatch {
+      url = "https://github.com/chardet/chardet/commit/0561ddcedcd12ea1f98b7ddedb93686ed8a5ffa4.patch";
+      sha256 = "1y1xhjf32rdhq9sfz58pghwv794f3w2f2qcn8p6hp4pc8jsdrn2q";
+    })
+  ];
+
+  checkInputs = [ pytest pytestrunner hypothesis ];
 
   meta = with stdenv.lib; {
     homepage = https://github.com/chardet/chardet;
