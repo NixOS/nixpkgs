c892356f00e2d944012b58a6ce65bb8dc87ada25
Vladimír Čunát <v@cunat.cz>
2019-05-26 10:38:16 +0200

    Revert "gtk-doc: fix css file permissions"
    This reverts commit 0e2768050a7fee157556679b31773b75a935d01d.
    Let's revert the gtk-doc update for now; it causes too many problems.

--- pkgs/development/tools/documentation/gtk-doc/default.nix
+++ pkgs/development/tools/documentation/gtk-doc/default.nix
@@ -27,8 +27,6 @@ stdenv.mkDerivation rec {
 
   patches = [
     passthru.respect_xml_catalog_files_var_patch
-    # https://gitlab.gnome.org/GNOME/gtk-doc/issues/84
-    ./0001-highlight-fix-permission-on-file-style.patch
   ];
 
   outputDevdoc = "out";
