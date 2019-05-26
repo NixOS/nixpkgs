863e1bde7c912eab99747222023d5d6c03df8e81
c0bw3b <c0bw3b@users.noreply.github.com>
2019-05-26 17:10:17 +0200

    libtins: 4.0 -> 4.2

--- pkgs/development/libraries/libtins/default.nix
+++ pkgs/development/libraries/libtins/default.nix
@@ -1,14 +1,14 @@
 { boost, cmake, fetchFromGitHub, gtest, libpcap, openssl, stdenv }:
 
 stdenv.mkDerivation rec {
-  name = "libtins-${version}";
-  version = "4.0";
-  
+  pname = "libtins";
+  version = "4.2";
+
   src = fetchFromGitHub {
     owner = "mfontanini";
-    repo = "libtins";
+    repo = pname;
     rev = "v${version}";
-    sha256 = "13sdqad976j7gq2k1il6g51yxwr8rlqdkzf1kj9mzhihjq8541qs";
+    sha256 = "0gv661gdf018zk1sr6fnvcmd5akqjihs4h6zzxv6881v6yhhglrz";
   };
 
   postPatch = ''
@@ -32,14 +32,15 @@ stdenv.mkDerivation rec {
   enableParallelBuilding = true;
   doCheck = true;
   preCheck = ''
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/lib
-    export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$PWD/lib
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD${placeholder "out"}/lib
+    export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$PWD${placeholder "out"}/lib
   '';
   checkTarget = "tests test";
 
   meta = with stdenv.lib; {
     description = "High-level, multiplatform C++ network packet sniffing and crafting library";
-    homepage = https://libtins.github.io/;
+    homepage = "https://libtins.github.io/";
+    changelog = "https://raw.githubusercontent.com/mfontanini/${pname}/v${version}/CHANGES.md";
     license = stdenv.lib.licenses.bsd2;
     maintainers = with maintainers; [ fdns ];
     platforms = stdenv.lib.platforms.unix;
