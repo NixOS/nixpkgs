552803f6f56b503536e77c3bc6bb28ac0375a5f1
Jos van den Oever <jos@vandenoever.info>
2019-05-26 16:43:24 +0200

    qt 5.12.0 -> 5.12.3

--- pkgs/development/libraries/qt-5/5.12/default.nix
+++ pkgs/development/libraries/qt-5/5.12/default.nix
@@ -60,7 +60,6 @@ let
     qtserialport = [ ./qtserialport.patch ];
     qtwebengine = [
       ./qtwebengine-no-build-skip.patch
-      ./qtwebengine-CVE-2019-5786.patch
     ]
       ++ optional stdenv.isDarwin ./qtwebengine-darwin-no-platform-check.patch;
     qtwebkit = [ ./qtwebkit.patch ]
