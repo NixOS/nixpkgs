dd5c0ce7d2537cf1458fe72a89c4bd4538d6d60a
Jos van den Oever <jos@vandenoever.info>
2019-05-26 16:43:24 +0200

    qt 5.12.0 -> 5.12.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12814,7 +12814,7 @@ in
       inherit stdenv fetchurl fetchFromGitHub makeSetupHook makeWrapper;
       bison = bison2; # error: too few arguments to function 'int yylex(...
       inherit cups;
-      harfbuzz = harfbuzzFull;
+      inherit harfbuzz;
       inherit libGL;
       inherit perl;
       inherit gtk3;
