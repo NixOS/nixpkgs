ef7b0adcf7d5103dfd28796ae92d324332ab7c2a
Tim Steinbach <tim@nequissimus.com>
2019-05-28 08:49:13 -0400

    linux: 5.2-rc1 -> 5.2-rc2

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -1,13 +1,13 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, libelf, utillinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "5.2-rc1";
-  modDirVersion = "5.2.0-rc1";
+  version = "5.2-rc2";
+  modDirVersion = "5.2.0-rc2";
   extraMeta.branch = "5.2";
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "169gjmciijgmwwfyz3n7pvf2x18pmfvgdk73s8jlx30ggwzgac1l";
+    sha256 = "0fd6z6zx9a3ax5jyvxm7gmfzain26la5gf18fccxip7bfn72bj4f";
   };
 
   # Should the testing kernels ever be built on Hydra?
