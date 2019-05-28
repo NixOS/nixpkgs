d4e6950ac66227f5a300c80f0d2628dc9f51c2f1
Anthony Cowley <acowley@gmail.com>
2019-05-28 16:12:38 -0400

    Address comments by @aanderse

--- pkgs/applications/graphics/qview/default.nix
+++ pkgs/applications/graphics/qview/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchFromGitHub, qmake}:
 stdenv.mkDerivation rec {
-  name = "qview-${version}";
+  pname = "qview";
   version = "2.0";
   src = fetchFromGitHub {
     owner = "jurplel";
     repo = "qView";
-    rev = "${version}";
+    rev = version;
     sha256 = "1s29hz44rb5dwzq8d4i4bfg77dr0v3ywpvidpa6xzg7hnnv3mhi5";
   };
   nativeBuildInputs = [ qmake ];
@@ -14,7 +14,7 @@ stdenv.mkDerivation rec {
   '';
   meta = with stdenv.lib; {
     description = "Practical and minimal image viewer";
-    homepage = https://interversehq.com/qview/;
+    homepage = "https://interversehq.com/qview/";
     license = licenses.gpl3;
     maintainers = with maintainers; [ acowley ];
     platforms = platforms.all;
