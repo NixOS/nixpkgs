5d9f432a83310600d6a02645ef13eadd43f632a0
Will Dietz <w@wdtz.org>
2019-05-29 02:45:05 -0500

    xarchiver: 0.5.4.12 -> 0.5.4.14
    Changelog seems only at the level of 5.4.x:
    
    https://github.com/ib/xarchiver/blob/0.5.4.14/ChangeLog
    
    Commits from 0.5.4.12 to 0.5.4.14:
    
    https://github.com/ib/xarchiver/compare/0.5.4.12...0.5.4.14

--- pkgs/tools/archivers/xarchiver/default.nix
+++ pkgs/tools/archivers/xarchiver/default.nix
@@ -1,23 +1,15 @@
-{ stdenv, fetchFromGitHub, fetchpatch, gtk3, pkgconfig, intltool, libxslt, hicolor-icon-theme }:
+{ stdenv, fetchFromGitHub, gtk3, pkgconfig, intltool, libxslt, hicolor-icon-theme }:
 
 stdenv.mkDerivation rec {
-  version = "0.5.4.12";
+  version = "0.5.4.14";
   name = "xarchiver-${version}";
 
   src = fetchFromGitHub {
     owner = "ib";
     repo = "xarchiver";
     rev = "${version}";
-    sha256 = "13d8slcx3frz0dhl1w4llj7001n57cjjb8r7dlaw5qacaas3xfwi";
+    sha256 = "1iklwgykgymrwcc5p1cdbh91v0ih1m58s3w9ndl5kyd44bwlb7px";
   };
-
-  patches = [
-    # Fixes darwin build, remove with next update.
-    (fetchpatch {
-      url = https://github.com/ib/xarchiver/commit/8c69d066a827419feafd0bd047d19207ceadc7df.patch;
-      sha256 = "1ch1409hx1ynkm0mz93zy8h7wvcrsn56sz7lczsf6hznc8yzl0qg";
-    })
-  ];
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ gtk3 intltool libxslt hicolor-icon-theme ];
