7503c03c73aa35dd1609271350ea1394618814b8
Mario Rodas <marsam@users.noreply.github.com>
2019-05-30 05:00:00 -0500

    dune: 1.9.2 -> 1.10.0

--- pkgs/development/tools/ocaml/dune/default.nix
+++ pkgs/development/tools/ocaml/dune/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ocaml, findlib, opaline }:
 
 stdenv.mkDerivation rec {
-  name = "dune-${version}";
-  version = "1.9.2";
+  pname = "dune";
+  version = "1.10.0";
   src = fetchurl {
     url = "https://github.com/ocaml/dune/releases/download/${version}/dune-${version}.tbz";
-    sha256 = "0l27d13wh3i1450kgxnhr6r977sgby1dqwsfc8cqd9mqic1mr9f2";
+    sha256 = "15fx9rg16g7ig43rg4sdq0wp0br5h1mjxxgv8b15s317vqlfc5pd";
   };
 
   buildInputs = [ ocaml findlib ];
@@ -21,7 +21,7 @@ stdenv.mkDerivation rec {
   '';
 
   meta = {
-    homepage = https://github.com/ocaml/dune;
+    homepage = "https://dune.build/";
     description = "A composable build system";
     maintainers = [ stdenv.lib.maintainers.vbgl ];
     license = stdenv.lib.licenses.mit;
