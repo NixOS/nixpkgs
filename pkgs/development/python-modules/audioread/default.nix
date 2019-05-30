6b029e62cc4441fd6a191c7cc377f990bfc1dd1e
Frederik Rietdijk <fridh@fridh.nl>
2019-05-30 08:49:48 +0200

    python: audioread: 2.1.6 -> 2.1.7

--- pkgs/development/python-modules/audioread/default.nix
+++ pkgs/development/python-modules/audioread/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "audioread";
-  version = "2.1.6";
+  version = "2.1.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b0b9270c20833a75ce0d167fb2fdad52ddcd8e8f300be8afad3ac9715850bc50";
+    sha256 = "f488f9e6fa1ccb09289e3db194639bc6388168b27ef27b2c62380aa1d35a3abe";
   };
 
   # No tests, need to disable or py3k breaks
