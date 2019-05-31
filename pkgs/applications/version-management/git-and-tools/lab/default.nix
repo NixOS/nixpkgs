ea93de64792d436d587008fe3dad32e91e9c4adf
Mario Rodas <marsam@users.noreply.github.com>
2019-05-31 12:57:17 -0500

    gitAndTools.lab: 0.15.3 -> 0.16.0 (#62301)

--- pkgs/applications/version-management/git-and-tools/lab/default.nix
+++ pkgs/applications/version-management/git-and-tools/lab/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "lab";
-  version = "0.15.3";
+  version = "0.16.0";
 
   src = fetchFromGitHub {
     owner = "zaquestion";
     repo = "lab";
     rev = "v${version}";
-    sha256 = "00c0ljyvcqrzzcsdb3r37lxd4z2wvwp80ggs8jvp7y32p1b61bsg";
+    sha256 = "0f1gi4mlcxjvz2sgh0hzzsqxg5gfvq2ay7xjd0y1kz3pp8kxja7i";
   };
 
   subPackages = [ "." ];
@@ -16,15 +16,16 @@ buildGoModule rec {
   modSha256 = "0bw47dd1b46ywsian2b957a4ipm77ncidipzri9ra39paqlv7abb";
 
   postInstall = ''
-    mkdir -p $out/share/zsh/site-functions
-    LAB_CORE_HOST=a LAB_CORE_USER=b LAB_CORE_TOKEN=c \
+    mkdir -p "$out/share/bash-completion/completions" "$out/share/zsh/site-functions"
+    export LAB_CORE_HOST=a LAB_CORE_USER=b LAB_CORE_TOKEN=c
+    $out/bin/lab completion bash > $out/share/bash-completion/completions/lab
     $out/bin/lab completion zsh > $out/share/zsh/site-functions/_lab
   '';
 
   meta = with stdenv.lib; {
     description = "Lab wraps Git or Hub, making it simple to clone, fork, and interact with repositories on GitLab";
     homepage = https://zaquestion.github.io/lab;
-    license = licenses.unlicense;
+    license = licenses.cc0;
     maintainers = with maintainers; [ marsam dtzWill ];
     platforms = platforms.all;
   };
