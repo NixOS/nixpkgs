2e0bdbe85f55d60761c2a499f870a7f95ef5565f
R. RyanTM <ryantm-bot@ryantm.com>
2019-06-01 06:51:41 -0700

    eternal-terminal: 5.1.9 -> 5.1.10
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/eternal-terminal/versions

--- pkgs/tools/networking/eternal-terminal/default.nix
+++ pkgs/tools/networking/eternal-terminal/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "eternal-terminal-${version}";
-  version = "5.1.9";
+  version = "5.1.10";
 
   src = fetchFromGitHub {
     owner = "MisterTea";
     repo = "EternalTCP";
     rev = "refs/tags/et-v${version}";
-    sha256 = "07ynkcnk3z6wafdlnzdxcd308cw1rzabxyq47ybj79lyji3wsgk7";
+    sha256 = "0jh89229bd9s82h3aj6faaybwr5xvnk8w2kgz47gq263pz021zpl";
   };
 
   nativeBuildInputs = [ cmake ninja ];
