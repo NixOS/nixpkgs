d8a1584c888086a4a9f631c389f6c135da1e2009
R. RyanTM <ryantm-bot@ryantm.com>
2019-05-31 23:32:18 -0700

    babl: 0.1.62 -> 0.1.64
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/babl/versions

--- pkgs/development/libraries/babl/default.nix
+++ pkgs/development/libraries/babl/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "babl-0.1.62";
+  name = "babl-0.1.64";
 
   src = fetchurl {
     url = "https://ftp.gtk.org/pub/babl/0.1/${name}.tar.bz2";
-    sha256 = "047msfzj8v4sfl61a2xhd69r9rh2pjq4lzpk3j10ijyv9qbry9yw";
+    sha256 = "1gsqs5spgla86y9g11riryvw7015asik7y22maainl83nhq4sxxv";
   };
 
   doCheck = true;
