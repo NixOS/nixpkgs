b6dd281f5419b2469df3a6bb9b6e4a518d2db39e
R. RyanTM <ryantm-bot@ryantm.com>
2019-05-31 20:29:11 -0700

    aide: 0.16.1 -> 0.16.2
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/aide/versions

--- pkgs/tools/security/aide/default.nix
+++ pkgs/tools/security/aide/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "aide-${version}";
-  version = "0.16.1";
+  version = "0.16.2";
 
   src = fetchurl {
     url = "https://github.com/aide/aide/releases/download/v${version}/${name}.tar.gz";
-    sha256 = "1dqhc0c24wa4zid06pfy61k357yvzh28ij86bk9jf6hcqzn7qaqg";
+    sha256 = "15xp47sz7kk1ciffw3f5xw2jg2mb2lqrbr3q6p4bkbz5dap9iy8p";
   };
 
   buildInputs = [ flex bison libmhash zlib acl attr libselinux pcre ];
