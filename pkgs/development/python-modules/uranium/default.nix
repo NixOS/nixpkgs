2787b17dbe562387281b1b2e03afbf9c2e358fd0
Gabriel Ebner <gebner@gebner.org>
2019-06-01 11:19:55 +0200

    pythonPackages.uranium: 4.0.0 -> 4.1.0

--- pkgs/development/python-modules/uranium/default.nix
+++ pkgs/development/python-modules/uranium/default.nix
@@ -2,7 +2,7 @@
 , pyqt5, numpy, scipy, shapely, libarcus, doxygen, gettext, pythonOlder }:
 
 buildPythonPackage rec {
-  version = "4.0.0";
+  version = "4.1.0";
   pname = "uranium";
   format = "other";
 
@@ -10,7 +10,7 @@ buildPythonPackage rec {
     owner = "Ultimaker";
     repo = "Uranium";
     rev = version;
-    sha256 = "1dzn064np76q0xpypcwsa7k7arzihg79xw1pgcvdizk9kzc6rw3y";
+    sha256 = "0vjlir9rd3xdlb8icdcr51qjmnh8nny6ajycria9idccriaqficj";
   };
 
   disabled = pythonOlder "3.5.0";
