6767f66303db9bdda1594bffd82b0cae29e8f9ee
Will Dietz <w@wdtz.org>
2019-06-01 08:41:33 -0500

    bro: 2.5.5 -> 2.6.2
    Security fix release mentioned at top of notes, FWIW:
    
    https://raw.githubusercontent.com/zeek/zeek/bb979702cf9a2fa67b8d1a1c7f88d0b56c6af104/NEWS

--- pkgs/applications/networking/ids/bro/default.nix
+++ pkgs/applications/networking/ids/bro/default.nix
@@ -2,15 +2,19 @@
 , geoip, gperftools, python, swig }:
 
 stdenv.mkDerivation rec {
-  name = "bro-2.5.5";
+  name = "bro-2.6.2";
 
   src = fetchurl {
     url = "https://www.bro.org/downloads/${name}.tar.gz";
-    sha256 = "1kvkiq8jjsqryry9jd4vw45pbfb46jly988mq62mv4sd1fqsxwhq";
+    sha256 = "19n0xai1mndx2i28q9cnszam57r6p6zqhprxxfpxh7bv7xpqgxkd";
   };
 
   nativeBuildInputs = [ cmake flex bison file ];
   buildInputs = [ openssl libpcap perl zlib curl geoip gperftools python swig ];
+
+  # Indicate where to install the python bits, since it can't put them in the "usual"
+  # locations as those paths are read-only.
+  cmakeFlags = [ "-DPY_MOD_INSTALL_DIR=${placeholder "out"}/${python.sitePackages}" ];
 
   enableParallelBuilding = true;
 
