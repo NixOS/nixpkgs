6b7bf72a2af8d4de7b9a88b489c4f5501352357f
Nathan Smyth <nathan.smyth@mongodb.com>
2019-06-01 10:11:22 +1000

    fix: Make the 3.4 mongodb high priority

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15609,7 +15609,7 @@ in
   };
   mysql = mariadb; # TODO: move to aliases.nix
 
-  mongodb = mongodb-3_4;
+  mongodb = hiPrio mongodb-3_4;
   
   mongodb-3_4 = callPackage ../servers/nosql/mongodb/v3_4.nix {
     sasl = cyrus_sasl;
