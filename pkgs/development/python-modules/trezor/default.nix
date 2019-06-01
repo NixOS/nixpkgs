2335878b2a2acfff62880f80995f1b9ce5cbfe4f
Marek Mahut <marek.mahut@satoshilabs.com>
2019-06-01 10:24:40 +0200

    python-trezor: 0.11.2 -> 0.11.3

--- pkgs/development/python-modules/trezor/default.nix
+++ pkgs/development/python-modules/trezor/default.nix
@@ -14,13 +14,13 @@
 
 buildPythonPackage rec {
   pname = "trezor";
-  version = "0.11.2";
+  version = "0.11.3";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a6f4b47b37a21247535fc43411cb70a8c61ef0a5a2dfee668bd05611e2741fb8";
+    sha256 = "c79a500e90d003073c8060d319dceb042caaba9472f13990c77ed37d04a82108";
   };
 
   propagatedBuildInputs = [ typing-extensions protobuf hidapi ecdsa mnemonic requests pyblake2 click construct libusb1 rlp ];
@@ -43,6 +43,6 @@ buildPythonPackage rec {
     description = "Python library for communicating with TREZOR Bitcoin Hardware Wallet";
     homepage = https://github.com/trezor/python-trezor;
     license = lib.licenses.gpl3;
-    maintainers = with lib.maintainers; [ np prusnak ];
+    maintainers = with lib.maintainers; [ np prusnak mmahut ];
   };
 }
