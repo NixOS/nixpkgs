a2995b227d815d89713c3a821cf0d00f51de5852
R. RyanTM <ryantm-bot@ryantm.com>
2019-06-01 09:59:56 -0700

    flrig: 1.3.43 -> 1.3.44
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/flrig/versions

--- pkgs/applications/radio/flrig/default.nix
+++ pkgs/applications/radio/flrig/default.nix
@@ -6,12 +6,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "1.3.43";
+  version = "1.3.44";
   pname = "flrig";
 
   src = fetchurl {
     url = "mirror://sourceforge/fldigi/${pname}-${version}.tar.gz";
-    sha256 = "1ir47svjbz7dhgzxvb3xqnkcsnjqb935vhqfddx7yhaawiqrzhk1";
+    sha256 = "0y05qhjmqydh0lripb44jjkcchxzr8bfn1r2k3m48wb4aij607yj";
   };
 
   buildInputs = [
