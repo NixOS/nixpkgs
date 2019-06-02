eef54c2360bddf171b7b898c69a62c71fe757d4b
R. RyanTM <ryantm-bot@ryantm.com>
2019-06-02 16:59:59 -0700

    python37Packages.XlsxWriter: 1.1.7 -> 1.1.8
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-xlsxwriter/versions

--- pkgs/development/python-modules/XlsxWriter/default.nix
+++ pkgs/development/python-modules/XlsxWriter/default.nix
@@ -3,7 +3,7 @@
 buildPythonPackage rec {
 
   pname = "XlsxWriter";
-  version = "1.1.7";
+  version = "1.1.8";
 
   # PyPI release tarball doesn't contain tests so let's use GitHub. See:
   # https://github.com/jmcnamara/XlsxWriter/issues/327
@@ -11,7 +11,7 @@ buildPythonPackage rec {
     owner = "jmcnamara";
     repo = pname;
     rev = "RELEASE_${version}";
-    sha256 = "1zv5222bymfr7046ps5512x1kk7ipcx46jjxylaap1p6llqj7zlj";
+    sha256 = "19qhdcycaiamd3bp8v2z9rpirxsr4c29fgs219k2766fpmfrgx40";
   };
 
   meta = {
