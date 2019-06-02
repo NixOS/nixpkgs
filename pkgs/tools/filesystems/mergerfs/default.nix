822af978ef9cf2487dfd3d1ffd2095624ecaae80
R. RyanTM <ryantm-bot@ryantm.com>
2019-06-01 23:06:20 -0700

    mergerfs: 2.26.1 -> 2.27.1
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/mergerfs/versions

--- pkgs/tools/filesystems/mergerfs/default.nix
+++ pkgs/tools/filesystems/mergerfs/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "mergerfs";
-  version = "2.26.1";
+  version = "2.27.1";
 
   src = fetchFromGitHub {
     owner = "trapexit";
     repo = pname;
     rev = version;
-    sha256 = "16dgm2n36n6k7s1nzprzsv5bdckk0x2s08qpcvc42ny6jbivpjn3";
+    sha256 = "0p8yb9dbbjp388kdi86lg1rg2zdqbjr0q5ka1f04h64s8vmkw41l";
   };
 
   nativeBuildInputs = [
