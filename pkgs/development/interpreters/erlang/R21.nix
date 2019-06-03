975c4bda80b43560d34605c0a12bb09add02b414
Justin Wood <justin.k.wood@protonmail.com>
2019-06-03 12:55:02 -0400

    erlangR21: 21.2.3 -> 21.3.8.2

--- pkgs/development/interpreters/erlang/R21.nix
+++ pkgs/development/interpreters/erlang/R21.nix
@@ -1,8 +1,8 @@
 { mkDerivation }:
 
 mkDerivation rec {
-  version = "21.2.3";
-  sha256 = "1v47c7bddbp31y6f8yzdjyvgcx9sskxql33k7cs0p5fmr05hhxws";
+  version = "21.3.8.2";
+  sha256 = "1jpqvgi4m1mk9xk7m6264dp5b8hk4xipx4jg8vj7bhm0dxvq3mir";
 
   prePatch = ''
     substituteInPlace configure.in --replace '`sw_vers -productVersion`' '10.10'
