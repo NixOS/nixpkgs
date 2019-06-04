ba940e9b1f901003c02c41be6fdbf136d94a377a
Will Dietz <w@wdtz.org>
2019-06-03 21:56:57 -0500

    skype: 8.45.0.41 -> 8.46.0.60
    https://support.skype.com/en/faq/FA34778/release-notes-for-skype-for-windows-mac-linux-and-web

--- pkgs/applications/networking/instant-messengers/skypeforlinux/default.nix
+++ pkgs/applications/networking/instant-messengers/skypeforlinux/default.nix
@@ -7,7 +7,7 @@ let
 
   # Please keep the version x.y.0.z and do not update to x.y.76.z because the
   # source of the latter disappears much faster.
-  version = "8.45.0.41";
+  version = "8.46.0.60";
 
   rpath = stdenv.lib.makeLibraryPath [
     alsaLib
@@ -58,7 +58,7 @@ let
     if stdenv.hostPlatform.system == "x86_64-linux" then
       fetchurl {
         url = "https://repo.skype.com/deb/pool/main/s/skypeforlinux/skypeforlinux_${version}_amd64.deb";
-        sha256 = "1k54gpaphkjv5qhqlwm2vbxbbci7nra4a5l0bhylkkzhsip6cvcm";
+        sha256 = "0v7a28zmgx1mxqgyfrj8byvjs9ibnirqplly8ll221gns8qjvrls";
       }
     else
       throw "Skype for linux is not supported on ${stdenv.hostPlatform.system}";
