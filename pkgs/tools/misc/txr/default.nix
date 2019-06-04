976264d57030c9c86501d959ccc7b37a0bc60632
Will Dietz <w@wdtz.org>
2019-06-04 01:38:47 -0500

    txr: limit to linux for now, dunno why Darwin build is problematic

--- pkgs/tools/misc/txr/default.nix
+++ pkgs/tools/misc/txr/default.nix
@@ -37,5 +37,6 @@ stdenv.mkDerivation rec {
     license = licenses.bsd2;
     homepage = http://nongnu.org/txr;
     maintainers = with stdenv.lib.maintainers; [ dtzWill ];
+    platforms = platforms.linux; # Darwin fails although it should work AFAIK
   };
 }
