184c55c1be9cea45b8d37744ef7047cc0c9cbe32
Renaud <c0bw3b@users.noreply.github.com>
2019-06-04 23:33:11 +0200

    pmd: cleanup

--- pkgs/development/tools/analysis/pmd/default.nix
+++ pkgs/development/tools/analysis/pmd/default.nix
@@ -1,10 +1,10 @@
-{stdenv, fetchurl, unzip}:
+{ stdenv, fetchurl, unzip }:
 
 stdenv.mkDerivation rec {
-  name = "pmd-${version}";
+  pname = "pmd";
   version = "6.15.0";
 
-  buildInputs = [ unzip ];
+  nativeBuildInputs = [ unzip ];
 
   src = fetchurl {
     url = "mirror://sourceforge/pmd/pmd-bin-${version}.zip";
@@ -12,15 +12,17 @@ stdenv.mkDerivation rec {
   };
 
   installPhase = ''
+    runHook preInstall
     mkdir -p $out
-    cp -R * $out
+    cp -R {bin,lib} $out
+    runHook postInstall
   '';
 
   meta = with stdenv.lib; {
     description = "An extensible cross-language static code analyzer";
-    homepage = https://pmd.github.io/;
+    homepage = "https://pmd.github.io/";
+    changelog = "https://pmd.github.io/pmd-${version}/pmd_release_notes.html";
     platforms = platforms.unix;
-    license = with licenses; [ bsdOriginal asl20 ];
+    license = with licenses; [ bsdOriginal asl20 lgpl3Plus ];
   };
 }
-
