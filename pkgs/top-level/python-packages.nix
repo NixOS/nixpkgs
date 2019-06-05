e8b88b2a618b37a7c5f343f3f64dc806cd1c9e63
Jonathan Ringer <jonringer117@gmail.com>
2019-06-05 16:05:51 -0700

    pythonPackages.knack: init at 0.6.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3160,6 +3160,8 @@ in {
 
   kitchen = callPackage ../development/python-modules/kitchen { };
 
+  knack = callPackage ../development/python-modules/knack { };
+
   kubernetes = callPackage ../development/python-modules/kubernetes { };
 
   pylast = callPackage ../development/python-modules/pylast { };
