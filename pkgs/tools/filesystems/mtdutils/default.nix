82edce94d67a7d265a5d748c422ed54be45bd4e3
Will Dietz <w@wdtz.org>
2019-06-04 20:02:01 -0500

    mtdutils: 1.5.2 -> 2.1.0
    http://lists.infradead.org/pipermail/linux-mtd/2019-March/088267.html
    
    And the previous releases, hardcoded version in URL I think caused
    this to be missed by auto-updaters for a while now.
    
    1.5.2, FWIW in 2015:
    
    http://lists.infradead.org/pipermail/linux-mtd/2015-August/060723.html

--- pkgs/tools/filesystems/mtdutils/default.nix
+++ pkgs/tools/filesystems/mtdutils/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, libuuid, lzo, zlib, acl }:
 
 stdenv.mkDerivation rec {
-  name = "mtd-utils-${version}";
-  version = "1.5.2";
+  pname = "mtd-utils";
+  version = "2.1.0";
 
   src = fetchurl {
-    url = ftp://ftp.infradead.org/pub/mtd-utils/mtd-utils-1.5.2.tar.bz2;
-    sha256 = "007lhsd8yb34l899r4m37whhzdw815cz4fnjbpnblfha524p7dax";
+    url = "ftp://ftp.infradead.org/pub/${pname}/${pname}-${version}.tar.bz2";
+    sha256 = "1ljb8nmfhpj0fyiyi9m5biaflvmf3idc0i4fbq9f9blkdnq9bfdl";
   };
 
   patchPhase = ''
