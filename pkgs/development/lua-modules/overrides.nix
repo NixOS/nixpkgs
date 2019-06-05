704358dcb586731b83ee3db4a8def665f9d314d6
Matthieu Coudron <mattator@gmail.com>
2019-06-05 13:21:38 +0900

    luaPackages.luazip: 2007-10-30 -> 1.2.7

--- pkgs/development/lua-modules/overrides.nix
+++ pkgs/development/lua-modules/overrides.nix
@@ -46,6 +46,9 @@ with super;
   lua-iconv = super.lua-iconv.override({
     buildInputs = [ pkgs.libiconv ];
   });
+  luazip = super.luazip.override({
+    buildInputs = [ pkgs.zziplib ];
+  });
   luv = super.luv.overrideAttrs(oa: {
     # Use system libuv instead of building local and statically linking
     # This is a hacky way to specify -DWITH_SHARED_LIBUV=ON which
