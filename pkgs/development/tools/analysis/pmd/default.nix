97b23ec91dea8bfb098b66eb5ba48e325a727fd9
R. RyanTM <ryantm-bot@ryantm.com>
2019-06-04 23:10:59 -0700

    pmd: 6.14.0 -> 6.15.0 (#62503)
    * pmd: 6.14.0 -> 6.15.0
    
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/pmd/versions
    
    * pmd: cleanup

--- pkgs/development/tools/analysis/pmd/default.nix
+++ pkgs/development/tools/analysis/pmd/default.nix
@@ -1,26 +1,28 @@
-{stdenv, fetchurl, unzip}:
+{ stdenv, fetchurl, unzip }:
 
 stdenv.mkDerivation rec {
-  name = "pmd-${version}";
-  version = "6.14.0";
+  pname = "pmd";
+  version = "6.15.0";
 
-  buildInputs = [ unzip ];
+  nativeBuildInputs = [ unzip ];
 
   src = fetchurl {
     url = "mirror://sourceforge/pmd/pmd-bin-${version}.zip";
-    sha256 = "0k40l93fxakms9vm641d4vlb68gfhkblrm24sb7slzvhq2v832dj";
+    sha256 = "0im64lg18bv764i14g3p42dzd7kqq9j5an8dkz1vanypb1jf5j3s";
   };
 
   installPhase = ''
+    runHook preInstall
     mkdir -p $out
-    cp -R * $out
+    cp -R {bin,lib} $out
+    runHook postInstall
   '';
 
   meta = with stdenv.lib; {
     description = "An extensible cross-language static code analyzer";
-    homepage = https://pmd.github.io/;
+    homepage = "https://pmd.github.io/";
+    changelog = "https://pmd.github.io/pmd-${version}/pmd_release_notes.html";
     platforms = platforms.unix;
-    license = with licenses; [ bsdOriginal asl20 ];
+    license = with licenses; [ bsdOriginal asl20 lgpl3Plus ];
   };
 }
-
