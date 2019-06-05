c508e700bd81505c36aa1713c7646d2f24ee7908
Jonathan Ringer <jonringer117@gmail.com>
2019-06-05 14:52:43 -0700

    pythonPackages.azure-cli-telemetry: init at 1.0.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -250,6 +250,8 @@ in {
 
   azure-nspkg = callPackage ../development/python-modules/azure-nspkg { };
 
+  azure-cli-telemetry = callPackage ../development/python-modules/azure-cli-telemetry { };
+
   azure-common = callPackage ../development/python-modules/azure-common { };
 
   azure-cosmos = callPackage ../development/python-modules/azure-cosmos { };
