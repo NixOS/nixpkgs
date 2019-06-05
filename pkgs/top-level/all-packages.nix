8ac6e24571bd7fdb5fd84053d2929ab373a285d6
Silvan Mosberger <infinisil@icloud.com>
2019-06-05 23:30:20 +0200

    haskellPackages: Don't ignore overrides
    This prevented haskell.packages.${compiler} overlays from propagating
    to haskellPackages.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7491,9 +7491,7 @@ in
 
   haskell = callPackage ./haskell-packages.nix { };
 
-  haskellPackages = dontRecurseIntoAttrs (haskell.packages.ghc865.override {
-    overrides = haskell.packageOverrides;
-  });
+  haskellPackages = dontRecurseIntoAttrs haskell.packages.ghc865;
 
   inherit (haskellPackages) ghc;
 
