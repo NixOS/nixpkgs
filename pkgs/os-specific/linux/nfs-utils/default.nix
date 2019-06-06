7e6756af41944569fc2d7903ff122b1d56170e21
Nikolay Amiantov <ab@fmap.me>
2019-06-06 16:00:04 +0300

    nfs-utils: fix format strings in nfsdcld

--- pkgs/os-specific/linux/nfs-utils/default.nix
+++ pkgs/os-specific/linux/nfs-utils/default.nix
@@ -47,7 +47,9 @@ stdenv.mkDerivation rec {
     ]
     ++ lib.optional (stdenv ? glibc) "--with-rpcgen=${stdenv.glibc.bin}/bin/rpcgen";
 
-  patches = lib.optionals stdenv.hostPlatform.isMusl [
+  patches = [
+    ./nfsdcld-sqlite-format.patch
+  ] ++ lib.optionals stdenv.hostPlatform.isMusl [
     (fetchpatch {
       url = "https://raw.githubusercontent.com/alpinelinux/aports/cb880042d48d77af412d4688f24b8310ae44f55f/main/nfs-utils/0011-exportfs-only-do-glibc-specific-hackery-on-glibc.patch";
       sha256 = "0rrddrykz8prk0dcgfvmnz0vxn09dbgq8cb098yjjg19zz6d7vid";
