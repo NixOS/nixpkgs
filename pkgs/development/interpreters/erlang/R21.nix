66e0d30a70354d38007e1bc44dfc1a72292b13e3
Erlend Hamberg <erlend@hamberg.no>
2019-06-06 13:06:11 +0200

    erlangR21: 21.2.3 -> 21.3.7.1

--- pkgs/development/interpreters/erlang/R21.nix
+++ pkgs/development/interpreters/erlang/R21.nix
@@ -1,8 +1,8 @@
 { mkDerivation }:
 
 mkDerivation rec {
-  version = "21.2.3";
-  sha256 = "1v47c7bddbp31y6f8yzdjyvgcx9sskxql33k7cs0p5fmr05hhxws";
+  version = "21.3.7.1";
+  sha256 = "03vbp22vwra3zz76q3sjv23lmic60gi96a7dncry8whbfp4i4j8a";
 
   prePatch = ''
     substituteInPlace configure.in --replace '`sw_vers -productVersion`' '10.10'
