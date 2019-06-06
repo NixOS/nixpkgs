3277a77ebb868107e1d61f626698603113487ed0
Guillaume Maudoux <layus.on@gmail.com>
2019-06-06 11:51:21 +0200

    qtikz: fix wrong sha256

--- pkgs/applications/graphics/ktikz/default.nix
+++ pkgs/applications/graphics/ktikz/default.nix
@@ -32,7 +32,7 @@ stdenv.mkDerivation rec {
   patches = [
     (fetchpatch {
       url = "https://github.com/fhackenberger/ktikz/commit/972685a406517bb85eb561f2c8e26f029eacd7db.patch";
-      sha256 = "16jwsl18marfw5m888vwxdd1h7cqa37rkfqgirzdliacb1cr4f58";
+      sha256 = "13z40rcd4m4n088v7z2ns17lnpn0z3rzp31lsamic3qdcwjwa5k8";
     })
   ];
 
