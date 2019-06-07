ef6aba5258b74c2cc956cfc3dd18048fb2f0a4dc
Fernando Paredes Garcia <fernando@develcuy.com>
2019-06-06 23:47:54 -0500

    #52728 Palemoon 28.5.2 (from binary tarball)

--- pkgs/applications/networking/browsers/palemoon-bin/default.nix
+++ pkgs/applications/networking/browsers/palemoon-bin/default.nix
@@ -12,11 +12,11 @@ in
 
 stdenv.mkDerivation rec {
   name = "palemoon-bin-${version}";
-  version = "28.4.1";
+  version = "28.5.2";
 
   src = fetchurl {
     url = "https://linux.palemoon.org/datastore/release/palemoon-${version}.${arch}.tar.bz2";
-    sha256 = "0yp38qql84j6qv5qcflx6hk0qg838dd3va2gwn02iwhrc042mygw";
+    sha256 = "0r1vl2z4dd47d454ba7k7748x6bir1l23wbivjrbxvmiwar5z1ak";
   };
   outputs = [ "out" "orig" ];
   sourceRoot = ".";
