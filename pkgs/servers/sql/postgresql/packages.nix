ba255d4e8bbc78a55369c2ad024b257a72dda5bb
steve-chavez <stevechavezast@gmail.com>
2019-06-09 14:26:15 -0500

    pg-safeupdate: init at 1.2
    safeupdate is a simple extension to PostgreSQL that raises an error if
    UPDATE and DELETE are executed without specifying conditions.

--- pkgs/servers/sql/postgresql/packages.nix
+++ pkgs/servers/sql/postgresql/packages.nix
@@ -46,4 +46,6 @@ self: super: {
     pgrouting = super.callPackage ./ext/pgrouting.nix { };
 
     pg_partman = super.callPackage ./ext/pg_partman.nix { };
+
+    pg_safeupdate = super.callPackage ./ext/pg_safeupdate.nix { };
 }
