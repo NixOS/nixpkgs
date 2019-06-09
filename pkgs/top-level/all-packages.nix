2a69ddb69b50481fc1fd59b4f417faef58495755
Nikolay Amiantov <ab@fmap.me>
2019-06-09 11:18:59 +0300

    mtprotoproxy: init at 1.0.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14301,6 +14301,8 @@ in
 
   mpdscribble = callPackage ../tools/misc/mpdscribble { };
 
+  mtprotoproxy = python3.pkgs.callPackage ../servers/mtprotoproxy { };
+
   micro-httpd = callPackage ../servers/http/micro-httpd { };
 
   miniHttpd = callPackage ../servers/http/mini-httpd {};
