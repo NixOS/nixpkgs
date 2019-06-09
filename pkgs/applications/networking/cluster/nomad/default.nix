38b42f72c12f4be4f15a248c018b533d7c33834c
Will Dietz <w@wdtz.org>
2019-06-09 15:38:03 -0500

    nomad: 0.8.7 -> 0.9.2
    * https://github.com/hashicorp/nomad/blob/v0.9.2/CHANGELOG.md
      **CVE-2019-12618**
    * https://github.com/hashicorp/nomad/blob/v0.9.1/CHANGELOG.md
    * https://github.com/hashicorp/nomad/blob/v0.9.0/CHANGELOG.md

--- pkgs/applications/networking/cluster/nomad/default.nix
+++ pkgs/applications/networking/cluster/nomad/default.nix
@@ -1,8 +1,8 @@
 { stdenv, buildGoPackage, fetchFromGitHub }:
 
 buildGoPackage rec {
-  name = "nomad-${version}";
-  version = "0.8.7";
+  name = "nomad";
+  version = "0.9.2";
   rev = "v${version}";
 
   goPackagePath = "github.com/hashicorp/nomad";
@@ -12,7 +12,7 @@ buildGoPackage rec {
     owner = "hashicorp";
     repo = "nomad";
     inherit rev;
-    sha256 = "0nkqiqkrccfmn7qkbhd48m9m56ix4xb0a3ar0z0pl4sbm25rlj0b";
+    sha256 = "0nvbyndxv07ql49ddxfar6pp10m17k2xjmbaw7f2czxxcrgx77ry";
   };
 
   meta = with stdenv.lib; {
