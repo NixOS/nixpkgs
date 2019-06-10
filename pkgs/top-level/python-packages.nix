ce0197fafcd3e5e7a7b12abdd012cd9db09bc072
Taito Horiuchi <taito.horiuchi@relexsolutions.com>
2019-06-10 12:17:30 +0300

    add package: robotframework-sshlibrary

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4258,6 +4258,8 @@ in {
 
   robotframework-selenium2library = callPackage ../development/python-modules/robotframework-selenium2library { };
 
+  robotframework-sshlibrary = callPackage ../development/python-modules/robotframework-sshlibrary { };
+
   robotframework-tools = callPackage ../development/python-modules/robotframework-tools { };
 
   robotstatuschecker = callPackage ../development/python-modules/robotstatuschecker { };
