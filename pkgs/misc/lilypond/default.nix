287ab732d941115a86e1fd08db7faf1dda21d0fe
Eric Bailey <eric@ericb.me>
2019-06-10 03:09:41 -0500

    lilypond: mark broken on darwin

--- pkgs/misc/lilypond/default.nix
+++ pkgs/misc/lilypond/default.nix
@@ -61,6 +61,7 @@ stdenv.mkDerivation {
     license = licenses.gpl3;
     maintainers = with maintainers; [ marcweber yurrriq ];
     platforms = platforms.all;
+    broken = stdenv.isDarwin;
   };
 
   patches = [ ./findlib.patch ];
