14ebdbb3d993acd5be94360904670e6b1e790abb
William Casarin <jb55@jb55.com>
2019-06-11 13:35:46 -0700

    bitlebee-discord: add myself as maintainer
    Signed-off-by: William Casarin <jb55@jb55.com>

--- pkgs/applications/networking/instant-messengers/bitlbee-discord/default.nix
+++ pkgs/applications/networking/instant-messengers/bitlbee-discord/default.nix
@@ -26,7 +26,7 @@ stdenv.mkDerivation rec {
 
     homepage = https://github.com/sm00th/bitlbee-discord;
     license = licenses.gpl2Plus;
-    maintainers = [ maintainers.lassulus ];
+    maintainers = with maintainers; [ lassulus jb55 ];
     platforms = stdenv.lib.platforms.linux;
   };
 }
