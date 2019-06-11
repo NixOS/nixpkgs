be8297eee4a8ef344bfef9f5f6b425298b7199b0
Mario Rodas <marsam@users.noreply.github.com>
2019-06-11 16:20:00 -0500

    sops: 3.3.0 -> 3.3.1

--- pkgs/tools/security/sops/default.nix
+++ pkgs/tools/security/sops/default.nix
@@ -2,7 +2,7 @@
 
 buildGoPackage rec {
   pname = "sops";
-  version = "3.3.0";
+  version = "3.3.1";
 
   goPackagePath = "go.mozilla.org/sops";
 
@@ -10,12 +10,13 @@ buildGoPackage rec {
     rev = version;
     owner = "mozilla";
     repo = pname;
-    sha256 = "0h02iy1dfn4874gyj3k07gbw8byb7rngvsi9kjglnad2pkf0pq2d";
+    sha256 = "0jbrz3yz6cj08h8cx6y98m8r0lpclh9367cw5apy6w3v71i3svfi";
   };
 
   meta = with stdenv.lib; {
     inherit (src.meta) homepage;
     description = "Mozilla sops (Secrets OPerationS) is an editor of encrypted files";
+    maintainers = [ maintainers.marsam ];
     license = licenses.mpl20;
   };
 }
