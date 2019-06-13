af37181eedcea4e25402878aea60b7e94ad20baa
Peter Simons <simons@cryp.to>
2019-06-13 22:08:49 +0200

    haskell-ip: use version 1.5.x to fix the build

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -1276,4 +1276,7 @@ self: super: {
   # Test suite won't link for no apparent reason.
   constraints-deriving = dontCheck super.constraints-deriving;
 
+  # The old LTS-13.x version does not compile.
+  ip = self.ip_1_5_0;
+
 } // import ./configuration-tensorflow.nix {inherit pkgs haskellLib;} self super
