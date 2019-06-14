NixOS/nixpkgs

commit 79f7f89442d9132c35b6417187cb1567eca2b910

nixos/cassandra: Use docbook instead of markdown style

nixos/modules/services/databases/cassandra.nix
@@ -229,11 +229,11 @@ in {
       example = literalExample "null";
       description = ''
           Set the interval how often full repairs are run, i.e.
-          `nodetool repair --full` is executed. See
+          <literal>nodetool repair --full</literal> is executed. See
           https://cassandra.apache.org/doc/latest/operating/repair.html
           for more information.
 
-          Set to `null` to disable full repairs.
+          Set to <literal>null</literal> to disable full repairs.
         '';
     };
     fullRepairOptions = mkOption {
@@ -250,11 +250,11 @@ in {
       example = literalExample "null";
       description = ''
           Set the interval how often incremental repairs are run, i.e.
-          `nodetool repair` is executed. See
+          <literal>nodetool repair<literal> is executed. See
           https://cassandra.apache.org/doc/latest/operating/repair.html
           for more information.
 
-          Set to `null` to disable incremental repairs.
+          Set to <literal>null</literal> to disable incremental repairs.
         '';
     };
     incrementalRepairOptions = mkOption {
@@ -342,6 +342,8 @@ in {
       description = ''
         Roles that are allowed to access the JMX (e.g. nodetool)
         BEWARE: The passwords will be stored world readable in the nix-store.
+                It's recommended to use your own protected file using
+                <literal>jmxRolesFile</literal>
 
         Doesn't work in versions older than 3.11 because they don't like that
         it's world readable.
@@ -388,7 +390,8 @@ in {
         { assertion = cfg.remoteJmx -> cfg.jmxRolesFile != null;
           message = ''
             If you want JMX available remotely you need to set a password using
-            `jmxRoles` or `jmxRolesFile` if using Cassandra older than v3.11.
+            <literal>jmxRoles</literal> or <literal>jmxRolesFile</literal> if
+            using Cassandra older than v3.11.
           '';
         }
       ];

commit c26584f1e5bcd2f75fa3d8aec656805d0bab5013

nixos: Fix build of manual
Manual build broken by 79f7f89442d9132c35b6417187cb1567eca2b910, which
is part of pull request #59179 (Fix Cassandra, improve config and
tests).

The issue was just a small error because of an unbalanced <literal/>
tag, so only a "/" was missing :-)

Signed-off-by: aszlig <aszlig@nix.build>
Cc: @aanderse

nixos/modules/services/databases/cassandra.nix
@@ -251,7 +251,7 @@ in {
       example = literalExample "null";
       description = ''
           Set the interval how often incremental repairs are run, i.e.
-          <literal>nodetool repair<literal> is executed. See
+          <literal>nodetool repair</literal> is executed. See
           https://cassandra.apache.org/doc/latest/operating/repair.html
           for more information.
 