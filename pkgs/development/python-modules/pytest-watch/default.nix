9903f218856bcba2d318227ac144b46d940990d1
Daniel Vianna <dmlvianna@gmail.com>
2019-06-14 12:25:49 +1000

    pytest-watch: remove pythonPackages

--- pkgs/development/python-modules/pytest-watch/default.nix
+++ pkgs/development/python-modules/pytest-watch/default.nix
@@ -15,9 +15,10 @@ buildPythonPackage rec {
     sha256 = "06136f03d5b361718b8d0d234042f7b2f203910d8568f63df2f866b547b3d4b9";
   };
 
-  doCheck = true;
-  buildInputs = with pythonPackages; [ pytest ];
-  propagatedBuildInputs = with pythonPackages; [ colorama docopt watchdog ];
+  # No Tests
+  doCheck = false;
+  buildInputs = [ pytest ];
+  propagatedBuildInputs = [ colorama docopt watchdog ];
   
   meta = with lib; {
     homepage = "https://github.com/joeyespo/pytest-watch";
