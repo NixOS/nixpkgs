5d92d16b49ee851dfbed7fb8b47e172ab1104a20
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-06-15 00:00:15 -0400

    sd-image-aarch64: Allows early modesetting for Allwinner boards
    This will reduce the confusion at boot, where the only thing visible is
    the last message from u-boot; where it looks like the board is
    hung, while in reality it's likely resizing partitions.

--- nixos/modules/installer/cd-dvd/sd-image-aarch64.nix
+++ nixos/modules/installer/cd-dvd/sd-image-aarch64.nix
@@ -29,6 +29,8 @@ in
   boot.initrd.availableKernelModules = [
     # Allows early (earlier) modesetting for the Raspberry Pi
     "vc4" "bcm2835_dma" "i2c_bcm2835"
+    # Allows early (earlier) modesetting for Allwinner SoCs
+    "sun4i_drm" "sun8i_drm_hdmi" "sun8i_mixer"
   ];
 
   sdImage = {
