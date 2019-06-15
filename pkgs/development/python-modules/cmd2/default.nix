cd5f46acf342b92733b4dd34002dad5f53881496
Frederik Rietdijk <fridh@fridh.nl>
2019-06-15 09:24:58 +0200

    python: cmd2: 0.9.12 -> 0.9.13

--- pkgs/development/python-modules/cmd2/default.nix
+++ pkgs/development/python-modules/cmd2/default.nix
@@ -6,11 +6,11 @@
 }:
 buildPythonPackage rec {
   pname = "cmd2";
-  version = "0.9.12";
+  version = "0.9.13";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "14pyvihikml1z7q21q9cvdfxvvlf8lhbaasj05hpiq6fjyvd7zsc";
+    sha256 = "b873ade57ebf6c42a9d4e8c705fc2b16777e9d2e53fec5b113914dc65f2eae38";
   };
 
   LC_ALL="en_US.UTF-8";
