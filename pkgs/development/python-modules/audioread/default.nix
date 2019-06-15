bd2188a57dba9ec8c7232acfa3766138c483e5e1
Frederik Rietdijk <fridh@fridh.nl>
2019-06-15 10:15:12 +0200

    pythonPackages.audioread: fix build

--- pkgs/development/python-modules/audioread/default.nix
+++ pkgs/development/python-modules/audioread/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pytestrunner
 }:
 
 buildPythonPackage rec {
@@ -11,6 +12,8 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "073904fabc842881e07bd3e4a5776623535562f70b1655b635d22886168dd168";
   };
+
+  nativeBuildInputs = [ pytestrunner ];
 
   # No tests, need to disable or py3k breaks
   doCheck = false;
