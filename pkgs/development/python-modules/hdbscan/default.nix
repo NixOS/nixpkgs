2be7ccf2a34c68fa515f457fc5c1948ed35c368c
Robert Scott <code@humanleg.org.uk>
2019-06-16 19:10:20 +0100

    pythonPackages.hdbscan: fix build by applying upstream patch
    simple missing import in one of the test modules

--- pkgs/development/python-modules/hdbscan/default.nix
+++ pkgs/development/python-modules/hdbscan/default.nix
@@ -6,6 +6,7 @@
 , scipy
 , scikitlearn
 , fetchPypi
+, fetchpatch
 }:
 
 buildPythonPackage rec {
@@ -16,6 +17,15 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "263e9f34db63eec217d50f2ca3e65049c065775dc4095b5ee817824cd2b5b51b";
   };
+
+  patches = [
+    # Fix Tests. Drop in release >0.8.20
+    (fetchpatch {
+      name = "test-rsl-missing-import.patch";
+      url = https://github.com/scikit-learn-contrib/hdbscan/commit/e40ccef139e56e38adf7bd6912cd63efd97598f9.patch;
+      sha256 = "0cfq4ja7j61h2zwd1jw5gagcz2sg18kjnx29sb0bwa13wfw6fip0";
+    })
+  ];
 
   checkInputs = [ nose ];
 
