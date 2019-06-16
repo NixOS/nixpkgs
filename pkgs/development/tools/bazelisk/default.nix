3d0d64bcbc18e0833bc6b8fcc36dcc188750f305
R. RyanTM <ryantm-bot@ryantm.com>
2019-06-16 07:26:04 -0700

    bazelisk: 0.0.4 -> 0.0.7
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/bazelisk/versions

--- pkgs/development/tools/bazelisk/default.nix
+++ pkgs/development/tools/bazelisk/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "bazelisk";
-  version = "0.0.4";
+  version = "0.0.7";
 
   src = fetchFromGitHub {
     owner = "philwo";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1hi4jmkqy1fjn91q72qlfvm63plz5jqb4hw4c1qv9ddqjgwrmxr3";
+    sha256 = "177x0mal960gs8578h5ir51vgn1bndm9z091110gqxb9xs9jq8pf";
   };
 
   modSha256 = "1f73j6ryidzi3kfy3rhsqx047vzwvzaqcsl7ykhg87rn2l2s7fdl";
