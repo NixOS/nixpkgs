b62fda7acfcc169f31b000c948bebb0db34b5f82
R. RyanTM <ryantm-bot@ryantm.com>
2019-06-16 15:58:46 -0700

    unifont_upper: 12.1.01 -> 12.1.02
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/unifont_upper/versions

--- pkgs/data/fonts/unifont_upper/default.nix
+++ pkgs/data/fonts/unifont_upper/default.nix
@@ -1,7 +1,7 @@
 { lib, fetchzip }:
 
 let
-  version = "12.1.01";
+  version = "12.1.02";
 in fetchzip rec {
   name = "unifont_upper-${version}";
 
@@ -9,7 +9,7 @@ in fetchzip rec {
 
   postFetch = "install -Dm644 $downloadedFile $out/share/fonts/truetype/unifont_upper.ttf";
 
-  sha256 = "11b14ka2w16vssxdhgq7k9bx7xx0sr36hfi2vzyimmaibasi1r74";
+  sha256 = "1bpzsgn64762sjkx4hssbm4qw0c1szwli38pch7r8z8hk4mgcv92";
 
   meta = with lib; {
     description = "Unicode font for glyphs above the Unicode Basic Multilingual Plane";
