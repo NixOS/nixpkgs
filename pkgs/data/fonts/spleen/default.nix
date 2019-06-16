d852492c1882ab3e594415d25acf66c766590f7b
R. RyanTM <ryantm-bot@ryantm.com>
2019-06-16 15:56:10 -0700

    spleen: 1.0.4 -> 1.0.5
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/spleen/versions

--- pkgs/data/fonts/spleen/default.nix
+++ pkgs/data/fonts/spleen/default.nix
@@ -2,7 +2,7 @@
 
 let
   pname = "spleen";
-  version = "1.0.4";
+  version = "1.0.5";
 in fetchurl rec {
   name = "${pname}-${version}";
   url = "https://github.com/fcambus/spleen/releases/download/${version}/spleen-${version}.tar.gz";
@@ -17,7 +17,7 @@ in fetchurl rec {
     install -Dm644 *.bdf -t $d
     install -m644 fonts.alias-spleen $d/fonts.alias
   '';
-  sha256 = "0jab55h08gy7gpyxqfrfj30iinmknpllh3dp5g7ck5q3qfgdzh7m";
+  sha256 = "0144a0lkkl5qx0a8sapymcayj5lp5cs9nfgpbmg3427n41pkqfbb";
 
   meta = with lib; {
     description = "Monospaced bitmap fonts";
