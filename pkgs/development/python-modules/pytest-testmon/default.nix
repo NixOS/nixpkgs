04b7c8f571bd06ba5f5bcf19ffdc1c8bbdd64cf1
Daniel Vianna <dmlvianna@gmail.com>
2019-06-17 08:07:15 +1000

    remove duplicate checkInputs

--- pkgs/development/python-modules/pytest-testmon/default.nix
+++ pkgs/development/python-modules/pytest-testmon/default.nix
@@ -14,7 +14,6 @@ buildPythonPackage rec {
     sha256 = "df00594e55f8f8f826e0e345dc23863ebac066eb749f8229c515a0373669c5bb";
   };
 
-  checkInputs = [ pytest ];
   propagatedBuildInputs = [ coverage ];
   checkInputs = [ pytest ];
   checkPhase = ''
