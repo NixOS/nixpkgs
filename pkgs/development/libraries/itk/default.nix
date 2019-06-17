e7b0c389c25d82229434812de5f502c64409dd63
R. RyanTM <ryantm-bot@ryantm.com>
2019-06-16 20:20:01 -0700

    itk: 4.13.2 -> 5.0.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/itk/versions

--- pkgs/development/libraries/itk/default.nix
+++ pkgs/development/libraries/itk/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, cmake, libX11, libuuid, xz, vtk }:
 
 stdenv.mkDerivation rec {
-  name = "itk-4.13.2";
+  name = "itk-5.0.0";
 
   src = fetchurl {
-    url = mirror://sourceforge/itk/InsightToolkit-4.13.2.tar.xz;
-    sha256 = "19cgfpd63gqrvc3m27m394gy2d7w79g5y6lvznb5qqr49lihbgns";
+    url = mirror://sourceforge/itk/InsightToolkit-5.0.0.tar.xz;
+    sha256 = "0bs63mk4q8jmx38f031jy5w5n9yy5ng9x8ijwinvjyvas8cichqi";
   };
 
   cmakeFlags = [
