eb1a0ff4606b4df02011eac94557aa1699f07d77
Alyssa Ross <hi@alyssa.is>
2019-06-17 19:43:41 +0000

    linux_4_4: 4.4.181 -> 4.4.182
    CVE-2019-11477
    CVE-2019-11478
    CVE-2019-11479
    
    (cherry picked from commit 48108096250ee491f4171d95270ef8aa5d020b84)

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.181";
+  version = "4.4.182";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1lw1qsql9dv8dllz6hglahxdfgzg34rpl9c9gwdrpm4j660nkaxj";
+    sha256 = "09w5v06c2ghai2pyz04kbhgfnqr22v4dkm9npb5kg6ndc67xh2f4";
   };
 } // (args.argsOverride or {}))
