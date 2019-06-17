a38244c3a20dad60cfbb3597428d9229807e7d64
Peter Hoeg <peter@hoeg.com>
2019-06-17 20:41:36 +0800

    pythonPackages.bcrypt: add missing libffi dependency

--- pkgs/development/python-modules/bcrypt/default.nix
+++ pkgs/development/python-modules/bcrypt/default.nix
@@ -1,23 +1,24 @@
-{ stdenv, buildPythonPackage, isPyPy, fetchPypi
+{ lib, buildPythonPackage, isPyPy, fetchPypi
+, libffi
 , cffi, pycparser, mock, pytest, py, six }:
 
-with stdenv.lib;
-
 buildPythonPackage rec {
-  version = "3.1.6";
   pname = "bcrypt";
+  version = "3.1.6";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "44636759d222baa62806bbceb20e96f75a015a6381690d1bc2eda91c01ec02ea";
   };
-  buildInputs = [ pycparser mock pytest py ];
-  propagatedBuildInputs = [ six ] ++ optional (!isPyPy) cffi;
 
-  meta = {
-    maintainers = with maintainers; [ domenkozar ];
+  buildInputs = [ libffi pycparser mock pytest py ];
+
+  propagatedBuildInputs = [ six ] ++ lib.optional (!isPyPy) cffi;
+
+  meta = with lib; {
     description = "Modern password hashing for your software and your servers";
-    license = licenses.asl20;
     homepage = https://github.com/pyca/bcrypt/;
+    license = licenses.asl20;
+    maintainers = with maintainers; [ domenkozar ];
   };
 }
