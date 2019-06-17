9ccdf3c9f546e29df5745950fc1be601214c0320
Alyssa Ross <hi@alyssa.is>
2019-06-17 19:43:44 +0000

    linux_4_9: 4.9.181 -> 4.9.182
    CVE-2019-11477
    CVE-2019-11478
    CVE-2019-11479

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.181";
+  version = "4.9.182";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1vgwfjsn31fy0ikcnpaqbw8w0r0xb25xp3633f0258yb24z25kcg";
+    sha256 = "15cnk5bmd7kd4ggjzrbs9fpc21wliqdrnfnpg623cf0639l14vmi";
   };
 } // (args.argsOverride or {}))
