a6fd6642460cfaae5f42fc65df76755228d43806
José Romildo Malaquias <malaquias@gmail.com>
2019-06-18 11:26:38 -0300

    ocamlPackages.mdx: init at 1.4.0

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -426,6 +426,8 @@ let
 
     markup = callPackage ../development/ocaml-modules/markup { lwt = ocaml_lwt; };
 
+    mdx = callPackage ../development/ocaml-modules/mdx { };
+
     menhir = callPackage ../development/ocaml-modules/menhir { };
 
     merlin = callPackage ../development/tools/ocaml/merlin { };
