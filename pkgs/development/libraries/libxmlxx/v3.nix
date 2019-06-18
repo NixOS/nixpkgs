cf41cb2b4accb45d5e8514b5d97e63945800ed88
Jan Tojnar <jtojnar@gmail.com>
2019-06-18 03:16:05 +0200

    libxmlxx3: split out doc output

--- pkgs/development/libraries/libxmlxx/v3.nix
+++ pkgs/development/libraries/libxmlxx/v3.nix
@@ -9,13 +9,18 @@ stdenv.mkDerivation rec {
     sha256 = "19kik79fmg61nv0by0a5f9wchrcfjwzvih4v2waw01hqflhqvp0r";
   };
 
-  outputs = [ "out" "dev" "devdoc" ];
+  outputs = [ "out" "dev" "doc" "devdoc" ];
 
   nativeBuildInputs = [ pkgconfig perl ];
 
   buildInputs = [ glibmm ];
 
   propagatedBuildInputs = [ libxml2 ];
+
+  postFixup = ''
+    substituteInPlace $dev/lib/pkgconfig/libxml++-3.0.pc \
+      --replace 'docdir=''${datarootdir}' "docdir=$doc/share"
+  '';
 
   meta = with stdenv.lib; {
     homepage = http://libxmlplusplus.sourceforge.net/;
