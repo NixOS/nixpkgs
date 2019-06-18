d2bfd7aa0b21b1cf109e7c408dcb5aded0700b30
worldofpeace <worldofpeace@protonmail.ch>
2019-06-18 19:08:12 -0400

    gvfs: fix CVE-2019-1244{7.8.9}
    CVE-2019-12447:
    daemon/gvfsbackendadmin.c mishandles file ownership because setfsuid is
    not used.
    
    CVE-2019-12448:
    daemon/gvfsbackendadmin.c has race conditions because the admin backend
    doesn't implement query_info_on_read/write.
    
    CVE-2019-12449:
    daemon/gvfsbackendadmin.c mishandles a file's user and group ownership
    during move (and copy with G_FILE_COPY_ALL_METADATA) operations
    from admin:// to file:// URIs, because root privileges are unavailable.
    
    Upstream MR: https://gitlab.gnome.org/GNOME/gvfs/merge_requests/48
    Closes #63067

--- pkgs/development/libraries/gvfs/default.nix
+++ pkgs/development/libraries/gvfs/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, meson, ninja, pkgconfig, gettext, gnome3, dbus
-, glib, libgudev, udisks2, libgcrypt, libcap, polkit
+, glib, libgudev, udisks2, libgcrypt, libcap, polkit, fetchpatch
 , libgphoto2, avahi, libarchive, fuse, libcdio
 , libxml2, libxslt, docbook_xsl, docbook_xml_dtd_42, samba, libmtp
 , gnomeSupport ? false, gnome, gcr, wrapGAppsHook
@@ -17,6 +17,28 @@ in stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
     sha256 = "0nw6mv5qq0d79d72x28db0vwihv4isny6m8q7vdim11ngk10hgwh";
   };
+
+  patches = [
+    # CVE-2019-12448
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/gvfs/commit/a1c2e7ecab0d6457fa2227d92e3569c08516eac5.patch";
+      sha256 = "03fwlpj1vbi80661bbhzv8ddx3czkzv9i1q4h3gqyxi5f1i0xfz4";
+    })
+    # CVE-2019-12447
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/gvfs/commit/0f25dea30d01d920443ab72b0c254560ec40e14c.patch";
+      sha256 = "1p7c48nsx1lkv2qpkyrsm9qfa77xwd28gczwcpv2kbji3ws5qgj5";
+    })
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/gvfs/commit/272e6bdac33309672955e8f8bf1b8f5f1e51fa0a.patch";
+      sha256 = "0zxbhmgqxxw987ag8fh6yjzjn9jl55fqbn814jh9kwrk7x4prx9x";
+    })
+    # CVE-2019-12449
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/gvfs/commit/bed1e9685c9f65f6a3ff3b39dd8547db3e7e77f6.patch";
+      sha256 = "0hfybfaz2gfx3yyw5ymx6q0pqwkx2r1i7gzprfp80bplwslq0d4h";
+    })
+  ];
 
   postPatch = ''
     # patchShebangs requires executable file
