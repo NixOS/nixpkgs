32c0fb0cf7ab592d083b5b1ffaa9e31d1ada8839
BadDecisionsAlex <alex.ameen.tx@gmail.com>
2019-06-19 02:04:20 -0500

    python3Packages.django-filter: init at 2.1.0

---
+++ pkgs/development/python-modules/django-filter/default.nix
@@ -0,0 +1,27 @@
+{ lib
+, fetchPypi
+, django
+, buildPythonPackage
+}:
+buildPythonPackage rec {
+
+  pname = "django-filter";
+  version = "2.1.0";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "3dafb7d2810790498895c22a1f31b2375795910680ac9c1432821cbedb1e176d";
+  };
+
+  doCheck = false;
+  propagatedBuildInputs = [
+    django
+  ];
+
+  meta = with lib; {
+    homepage = "https://github.com/carltongibson/django-filter/tree/master";
+    license = licenses.bsdOriginal;
+    description = "Django-filter is a reusable Django application for allowing users to filter querysets dynamically.";
+  };
+
+}
