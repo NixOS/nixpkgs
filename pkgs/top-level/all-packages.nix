2c6ba83aa0391e71bcda0eb7d8f0b054ff2d060e
Nikolay Korotkiy <sikmir@gmail.com>
2019-06-19 10:19:44 +0300

    qtpbfimageplugin: init at 1.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12987,6 +12987,8 @@ in
 
   qtkeychain = callPackage ../development/libraries/qtkeychain { };
 
+  qtpbfimageplugin = libsForQt5.callPackage ../development/libraries/qtpbfimageplugin { };
+
   qtscriptgenerator = callPackage ../development/libraries/qtscriptgenerator { };
 
   quesoglc = callPackage ../development/libraries/quesoglc { };
