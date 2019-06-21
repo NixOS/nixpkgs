aa7f28ffab2fd48fa6084b79db37461f98faae1e
Jonathan Ringer <jonringer117@gmail.com>
2019-06-21 04:06:37 -0700

    pythonPackages.azure-nspkg: update license

--- pkgs/development/python-modules/azure-nspkg/default.nix
+++ pkgs/development/python-modules/azure-nspkg/default.nix
@@ -19,8 +19,8 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Microsoft Azure SDK for Python";
-    homepage = https://github.com/Azure/azure-sdk-for-python;
+    homepage = "https://github.com/Azure/azure-sdk-for-python";
     license = licenses.mit;
-    maintainers = with maintainers; [ olcai mwilsoninsight ];
+    maintainers = with maintainers; [ olcai jonringer mwilsoninsight ];
   };
 }
