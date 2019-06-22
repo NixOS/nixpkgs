cb75ea64c9bf8726f7b942a32097634120db0e39
José Romildo Malaquias <malaquias@gmail.com>
2019-06-22 09:26:32 -0300

    deepin.dde-daemon: 3.27.1 -> 3.27.2.5

--- pkgs/desktops/deepin/dde-daemon/default.nix
+++ pkgs/desktops/deepin/dde-daemon/default.nix
@@ -9,7 +9,7 @@
 buildGoPackage rec {
   name = "${pname}-${version}";
   pname = "dde-daemon";
-  version = "3.27.1";
+  version = "3.27.2.5";
 
   goPackagePath = "pkg.deepin.io/dde/daemon";
 
@@ -17,7 +17,7 @@ buildGoPackage rec {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "1rbv7fals2bwhalw1hh3swmrdzclqbhny782shnrwqv53235xda3";
+    sha256 = "117yhsb0axwblncyj02fhcyl4b7jl7vvh1bbx08bscih5lnvjihx";
   };
 
   patches = [
