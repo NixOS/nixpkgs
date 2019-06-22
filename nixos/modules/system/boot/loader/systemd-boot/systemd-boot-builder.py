224a6562a4880195afa5c184e755b8ecaba41536
Domen Kožar <domen@dev.si>
2019-06-22 20:10:03 +0200

    Add configurationLimit to systemd-boot to prevent running out of disk space
    Refs #23926

--- nixos/modules/system/boot/loader/systemd-boot/systemd-boot-builder.py
+++ nixos/modules/system/boot/loader/systemd-boot/systemd-boot-builder.py
@@ -133,7 +133,9 @@ def get_generations(profile=None):
         universal_newlines=True)
     gen_lines = gen_list.split('\n')
     gen_lines.pop()
-    return [ (profile, int(line.split()[0])) for line in gen_lines ]
+
+    configurationLimit = @configurationLimit@
+    return [ (profile, int(line.split()[0])) for line in gen_lines ][-configurationLimit:]
 
 def remove_old_entries(gens):
     rex_profile = re.compile("^@efiSysMountPoint@/loader/entries/nixos-(.*)-generation-.*\.conf$")
