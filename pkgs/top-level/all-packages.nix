71b4b7b4c7ccbc6c51a7ace81930715722620ff2
tilpner <till@hoeppner.ws>
2019-06-22 20:41:59 +0200

    linux_4_14: restore __kernel_fpu_{begin,restore}
    Linux commit 12209993e98c5fa1855c467f22a24e3d5b8be205 was backported to
    4.14, so add the reverting patch to restore zfs performance.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15315,6 +15315,7 @@ in
         # when adding a new linux version
         kernelPatches.cpu-cgroup-v2."4.11"
         kernelPatches.modinst_arg_list_too_long
+        kernelPatches.export_kernel_fpu_functions
       ];
   };
 
