7f3eb310c0f682605dc2425496a22e0baac8670f
Vincent Laporte <Vincent.Laporte@gmail.com>
2019-06-24 09:20:14 +0000

    ocamlPackages.elpi: 1.2.0 -> 1.4.0

--- pkgs/development/ocaml-modules/elpi/default.nix
+++ pkgs/development/ocaml-modules/elpi/default.nix
@@ -1,40 +1,27 @@
-{ stdenv, fetchFromGitHub, ocaml, findlib, camlp5
+{ lib, fetchFromGitHub, buildDunePackage, camlp5
 , ppx_tools_versioned, ppx_deriving, re
 }:
 
-if !stdenv.lib.versionAtLeast ocaml.version "4.03"
-then throw "elpi is not available for OCaml ${ocaml.version}"
-else
-
-stdenv.mkDerivation rec {
-  name = "ocaml${ocaml.version}-elpi-${version}";
-  version = "1.2.0";
+buildDunePackage rec {
+  pname = "elpi";
+  version = "1.4.0";
   src = fetchFromGitHub {
     owner = "LPCIC";
-    repo = "elpi";
+    repo = pname;
     rev = "v${version}";
-    sha256 = "1n4jpidx0vk4y66bhd704ajn8n6f1fd5wsi1shj6wijfmjl14h7s";
+    sha256 = "08ddi1gkavy2y8375nvg4sgzqdirw82f086z95axzf2097hm3x1q";
   };
 
-  buildInputs = [ ocaml findlib ppx_tools_versioned ];
+  minimumOCamlVersion = "4.04";
+
+  buildInputs = [ ppx_tools_versioned ];
 
   propagatedBuildInputs = [ camlp5 ppx_deriving re ];
 
-  createFindlibDestdir = true;
-
-  preInstall = "make byte";
-
-  postInstall = ''
-    mkdir -p $out/bin
-    make install-bin BIN=$out/bin
-    make install-bin BYTE=1 BIN=$out/bin
-  '';
-
   meta = {
     description = "Embeddable λProlog Interpreter";
-    license = stdenv.lib.licenses.lgpl21Plus;
-    maintainers = [ stdenv.lib.maintainers.vbgl ];
+    license = lib.licenses.lgpl21Plus;
+    maintainers = [ lib.maintainers.vbgl ];
     inherit (src.meta) homepage;
-    inherit (ocaml.meta) platforms;
   };
 }
