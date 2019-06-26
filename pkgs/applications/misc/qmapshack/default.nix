9a9e1fa462a28141ded55027fbeeb8f2901d6771
Nikolay Korotkiy <sikmir@gmail.com>
2019-06-27 00:21:39 +0300

    qmapshack: 1.12.3 -> 1.13.0

--- pkgs/applications/misc/qmapshack/default.nix
+++ pkgs/applications/misc/qmapshack/default.nix
@@ -1,12 +1,12 @@
-{ stdenv, fetchurl, cmake, qtscript, qtwebengine, gdal, proj, routino, quazip }:
+{ stdenv, fetchurl, fetchpatch, cmake, qtscript, qtwebengine, gdal, proj, routino, quazip }:
 
 stdenv.mkDerivation rec {
   name = "qmapshack-${version}";
-  version = "1.12.3";
+  version = "1.13.0";
 
   src = fetchurl {
     url = "https://bitbucket.org/maproom/qmapshack/downloads/${name}.tar.gz";
-    sha256 = "1yp5gw4q4gwiwr9w4dz19am0bhsla9n2l3bdlk98a7f46kxgnkrx";
+    sha256 = "1cv1f718r8vqyk2l6w3alz2aqjvb4msz8705pm9yr5ndi28qyrba";
   };
 
   nativeBuildInputs = [ cmake ];
@@ -15,11 +15,22 @@ stdenv.mkDerivation rec {
 
   cmakeFlags = [
     "-DROUTINO_XML_PATH=${routino}/share/routino"
-    "-DQUAZIP_INCLUDE_DIR=${quazip}/include/quazip"
+    "-DQUAZIP_INCLUDE_DIR=${quazip}/include/quazip5"
     "-DLIBQUAZIP_LIBRARY=${quazip}/lib/libquazip.so"
   ];
 
   enableParallelBuilding = true;
+
+  patches = [
+    (fetchpatch {
+      url = "https://bitbucket.org/maproom/qmapshack/raw/d0b1b595578a83fda981ccc1ff24166fa636ba1d/FindPROJ4.patch";
+      sha256 = "1nx4ax233bnnj478cmjpm5c1qqmyn1navlihf10q6hhbanay9n99";
+    })
+    (fetchpatch {
+      url = "https://bitbucket.org/maproom/qmapshack/raw/d0b1b595578a83fda981ccc1ff24166fa636ba1d/FindQuaZip5.patch";
+      sha256 = "0z1b2dz2zlz685mxgn8bmh1fyhxpf6dzd6jvkkjyk2kvnrdxv3b9";
+    })
+  ];
 
   meta = with stdenv.lib; {
     homepage = https://bitbucket.org/maproom/qmapshack/wiki/Home;
