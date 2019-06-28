8b599e0bda9c7143bf2b86a444d18ec2029045b0
Taito Horiuchi <taito.horiuchi@relexsolutions.com>
2019-06-28 12:27:43 +0300

    pythonPackages.robotframework: 3.0.4 -> 3.1.2
    pythonPackages.robotframework: source file is zip formated

--- pkgs/development/python-modules/robotframework/default.nix
+++ pkgs/development/python-modules/robotframework/default.nix
@@ -2,11 +2,12 @@
 
 buildPythonPackage rec {
   pname = "robotframework";
-  version = "3.0.4";
+  version = "3.1.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ab94257cbd848dfca7148e092d233a12853cc7e840ce8231af9cbb5e7f51aa47";
+    sha256 = "f10dd7c0c8c7962a4f80dd1e026b5db731b9391bc6e1f9ebb96d685eb1230dbc";
+    extension = "zip";
   };
 
   meta = with stdenv.lib; {
