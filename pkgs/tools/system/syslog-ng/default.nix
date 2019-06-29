72508487c9e1e6ae15c55e19f2e5760f8fb14bb6
Yurii Izorkin <izorkin@elven.pw>
2019-06-29 17:47:21 +0300

    syslog-ng: 3.21.1 -> 3.22.1 (#63902)

--- pkgs/tools/system/syslog-ng/default.nix
+++ pkgs/tools/system/syslog-ng/default.nix
@@ -11,11 +11,11 @@ in
 
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
-  version = "3.21.1";
+  version = "3.22.1";
 
   src = fetchurl {
     url = "https://github.com/balabit/${pname}/releases/download/${name}/${name}.tar.gz";
-    sha256 = "09fdjschp3shy8xp0hh5fh6qv25433zd0biv0igkmkvrmnjks5ld";
+    sha256 = "1j3l9pn3lf9w87vvwbnxk098gprbqzmfpfw1rch46mgsfqvl8mh6";
   };
 
   nativeBuildInputs = [ pkgconfig which ];
