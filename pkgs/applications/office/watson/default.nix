4976d7ec3f1126618f2c312002811157406fc2ee
Nathan Yong <nathyong@users.noreply.github.com>
2019-07-01 13:20:13 +1000

    watson: use pytest for testing

--- pkgs/applications/office/watson/default.nix
+++ pkgs/applications/office/watson/default.nix
@@ -12,10 +12,8 @@ buildPythonApplication rec {
     sha256 = "249313996751f32f38817d424cbf8d74956461df1439f0ee3a962fcc3c77225d";
   };
 
-  # uses tox, test invocation fails
-  doCheck = true;
   checkPhase = ''
-    py.test -vs tests
+    pytest -vs tests
  '';
 
   checkInputs = [ py pytest pytest-datafiles mock pytest-mock pytestrunner ];
