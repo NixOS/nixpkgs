753c65c5584c328e8cdc97d9509b7fc62813325e
R. RyanTM <ryantm-bot@ryantm.com>
2019-07-01 08:09:47 -0700

    git-cola: 3.3 -> 3.4
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/git-cola/versions

--- pkgs/applications/version-management/git-and-tools/git-cola/default.nix
+++ pkgs/applications/version-management/git-and-tools/git-cola/default.nix
@@ -5,13 +5,13 @@ let
 
 in buildPythonApplication rec {
   name = "git-cola-${version}";
-  version = "3.3";
+  version = "3.4";
 
   src = fetchFromGitHub {
     owner = "git-cola";
     repo = "git-cola";
     rev = "v${version}";
-    sha256 = "0gfbzcmaqg6hdy2cfpshgcwh8zgj1ia1vd95i5xdrsvksgb8fq2j";
+    sha256 = "0754d56dprhb1nhb8fwp4my5pyqcgarwzba1l6zx7il87d7vyi5m";
   };
 
   buildInputs = [ git gettext ];
