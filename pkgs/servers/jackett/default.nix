210f3b0d82a06fe1441f7aa95542c457bfda25d7
R. RyanTM <ryantm-bot@ryantm.com>
2019-07-01 12:15:52 -0700

    jackett: 0.11.420 -> 0.11.457
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/jackett/versions

--- pkgs/servers/jackett/default.nix
+++ pkgs/servers/jackett/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "jackett-${version}";
-  version = "0.11.420";
+  version = "0.11.457";
 
   src = fetchurl {
     url = "https://github.com/Jackett/Jackett/releases/download/v${version}/Jackett.Binaries.Mono.tar.gz";
-    sha256 = "12mskqmgvfy6m10x6chxpl30pv1prsq21rxdlmd4pgd6shkfz9gn";
+    sha256 = "139ga5qra6jrn3mkp08a5p495alxq78a8imfj4k8ywv1s05kw7dw";
   };
 
   buildInputs = [ makeWrapper ];
