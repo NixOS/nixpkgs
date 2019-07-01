73392e79aa62e406683d6a732eb4f4101f4732be
Tim Steinbach <tim@nequissimus.com>
2019-07-01 18:29:15 -0400

    linux: 5.2-rc6 -> 5.2-rc7

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.2-rc6";
+  version = "5.2-rc7";
   extraMeta.branch = "5.2";
 
   # modDirVersion needs to be x.y.z, will always add .0
@@ -11,7 +11,7 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "15lwy9596v5sw8c6mhpl9ilfcmm39snvvyajg08ycsg61i2s58v0";
+    sha256 = "1128jb1y4gia5icv614ycp3c5mkvrb2l2wn20765b3si256g68n4";
   };
 
   # Should the testing kernels ever be built on Hydra?
