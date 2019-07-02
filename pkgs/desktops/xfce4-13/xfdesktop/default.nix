ef27e76f63e85e7ef1d56e6e8f9c976a9694691a
volth <volth@webmaster.ms>
2019-07-02 23:59:52 +0000

    xfce4-13: fix xfdesktop-settings crash on launch

--- pkgs/desktops/xfce4-13/xfdesktop/default.nix
+++ pkgs/desktops/xfce4-13/xfdesktop/default.nix
@@ -1,4 +1,4 @@
-{ mkXfceDerivation, exo, gtk3, libxfce4ui, libxfce4util, libwnck3, xfconf }:
+{ mkXfceDerivation, exo, wrapGAppsHook, gtk3, libxfce4ui, libxfce4util, libwnck3, xfconf }:
 
 mkXfceDerivation rec {
   category = "xfce";
@@ -7,6 +7,8 @@ mkXfceDerivation rec {
   rev = "xfce-4.14pre2";
 
   sha256 = "14sfcxbwxhhwn9nmiap46nz6idvw5hwr8wyjqrhq4h79x78g18k4";
+
+  nativeBuildInputs = [ wrapGAppsHook ]; # fix "No GSettings schemas are installed on the system"
 
   buildInputs = [
     exo
