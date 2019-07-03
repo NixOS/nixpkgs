9bd257455e2deb77b9646b71534e819e895e4c72
R. RyanTM <ryantm-bot@ryantm.com>
2019-07-02 22:01:05 -0700

    yubikey-manager-qt: 1.1.1 -> 1.1.2
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/yubikey-manager-qt/versions

--- pkgs/tools/misc/yubikey-manager-qt/default.nix
+++ pkgs/tools/misc/yubikey-manager-qt/default.nix
@@ -27,11 +27,11 @@ let
 
 in stdenv.mkDerivation rec {
   pname = "yubikey-manager-qt";
-  version = "1.1.1";
+  version = "1.1.2";
 
   src = fetchurl {
     url = "https://developers.yubico.com/${pname}/Releases/${pname}-${version}.tar.gz";
-    sha256 = "0hcfl0mh4gqy9c8ghmi58asqr8v009n6li0mgcy3r8fyjr7qryvy";
+    sha256 = "01ax8zjrahs2sjbgsys2ahh57sdcap0ij3y1r1bbvsgzr7xxm2q8";
   };
 
   nativeBuildInputs = [ makeWrapper python3.pkgs.wrapPython qmake ];
