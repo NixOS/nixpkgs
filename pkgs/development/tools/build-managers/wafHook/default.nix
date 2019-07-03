8c4ed06fb562ad59bdfd77f2895c26fb4675c77f
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2019-07-03 10:13:47 -0400

    !fixup
    fix indentation

--- pkgs/development/tools/build-managers/wafHook/default.nix
+++ pkgs/development/tools/build-managers/wafHook/default.nix
@@ -1,9 +1,10 @@
 { lib, stdenv, pkgs, python, makeSetupHook, waf }:
-  makeSetupHook {
-    deps = [ python ];
-    substitutions = {
-      inherit waf;
-      crossFlags = lib.optionalString (stdenv.hostPlatform.system != stdenv.targetPlatform.system)
-        ''--cross-compile "--cross-execute=${stdenv.targetPlatform.emulator pkgs}"'';
-    };
-  } ./setup-hook.sh
+
+makeSetupHook {
+  deps = [ python ];
+  substitutions = {
+    inherit waf;
+    crossFlags = lib.optionalString (stdenv.hostPlatform.system != stdenv.targetPlatform.system)
+      ''--cross-compile "--cross-execute=${stdenv.targetPlatform.emulator pkgs}"'';
+  };
+} ./setup-hook.sh
