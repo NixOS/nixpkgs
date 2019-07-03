4cab72953c6a8823a85302e8f0303477d1b20ae9
Alyssa Ross <hi@alyssa.is>
2019-07-03 16:34:01 +0000

    gnupg: vendor SKS patch
    This quickly became a 404 upstream.
    
    Fixes https://github.com/NixOS/nixpkgs/64256.

--- pkgs/tools/security/gnupg/22.nix
+++ pkgs/tools/security/gnupg/22.nix
@@ -32,10 +32,7 @@ stdenv.mkDerivation rec {
 
   patches = [
     ./fix-libusb-include-path.patch
-    (fetchpatch {
-      url = https://files.gnupg.net/file/data/qmxjhc6kuja3orybj7st/PHID-FILE-vvzlnw36427pdnug2amc/file;
-      sha256 = "13snxkmlgmvn0rgxh5k2sgxkp5mbxqiznzm45sw649nvs3ccghq8";
-    })
+    ./0001-dirmngr-Only-use-SKS-pool-CA-for-SKS-pool.patch
   ];
   postPatch = ''
     sed -i 's,hkps://hkps.pool.sks-keyservers.net,hkps://keys.openpgp.org,g' \
