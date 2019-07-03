44b450592c701bf2a79a2c4bc4d780a334396834
Robert Scott <code@humanleg.org.uk>
2019-07-03 21:27:14 +0100

    rdesktop: 1.8.3 -> 1.8.6
    security release, fixing issue with as-yet-unassigned CVE. debian are using
    DSA-4473-1/DLA-1837-1.
    
    switching to github source because they don't seem to be keeping their
    sourceforge tarballs up to date

--- pkgs/applications/networking/remote/rdesktop/default.nix
+++ pkgs/applications/networking/remote/rdesktop/default.nix
@@ -1,18 +1,20 @@
-{stdenv, fetchurl, openssl, libX11, libgssglue, pkgconfig
+{stdenv, fetchFromGitHub, openssl, libX11, libgssglue, pkgconfig, autoreconfHook
 , enableCredssp ? (!stdenv.isDarwin)
 } :
 
 stdenv.mkDerivation (rec {
   pname = "rdesktop";
-  version = "1.8.3";
+  version = "1.8.6";
   name = "${pname}-${version}";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/${pname}/${name}.tar.gz";
-    sha256 = "1r7c1rjmw2xzq8fw0scyb453gy9z19774z1z8ldmzzsfndb03cl8";
+  src = fetchFromGitHub {
+    owner = pname;
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "02sbhnqbasa54c75c86qw9w9h9sxxbnldj7bjv2gvn18lmq5rm20";
   };
 
-  nativeBuildInputs = [pkgconfig];
+  nativeBuildInputs = [pkgconfig autoreconfHook];
   buildInputs = [openssl libX11]
     ++ stdenv.lib.optional enableCredssp libgssglue;
 
