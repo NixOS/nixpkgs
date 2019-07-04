b3e1e15bd0cc563330d38cd2b34517a1c3c34b73
Jonathan Ringer <jonringer117@gmail.com>
2019-07-03 21:21:25 -0700

    pythonPackages.seekpath: mark as broken

--- pkgs/development/python-modules/seekpath/default.nix
+++ pkgs/development/python-modules/seekpath/default.nix
@@ -7,7 +7,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     inherit pname version;
     sha256 = "b61dadba82acc0838402981b7944155adc092b114ca81f53f61b1d498a512e3a";
-  };  
+  };
 
   LC_ALL = "en_US.utf-8";
 
@@ -17,8 +17,9 @@ buildPythonPackage rec {
 
   meta = with stdenv.lib; {
     description = "A module to obtain and visualize band paths in the Brillouin zone of crystal structures.";
-    homepage = https://github.com/giovannipizzi/seekpath;
+    homepage = "https://github.com/giovannipizzi/seekpath";
     license = licenses.mit;
+    broken = true; # seekpath.test_paths_hpkot.TestPaths3D_HPKOT testMethod=test_oI2Y fails
     maintainers = with maintainers; [ psyanticy ];
   };
 }
