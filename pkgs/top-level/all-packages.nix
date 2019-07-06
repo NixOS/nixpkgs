e39d7fab273e62fbe9b2a506641361b3f056184a
Richard Marko <srk@48.io>
2019-07-06 20:54:16 +0200

    sympa: init at 6.2.52

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15449,6 +15449,8 @@ in
 
   pshs = callPackage ../servers/http/pshs { };
 
+  sympa = callPackage ../servers/mail/sympa { };
+
   system-sendmail = lowPrio (callPackage ../servers/mail/system-sendmail { });
 
   # PulseAudio daemons
