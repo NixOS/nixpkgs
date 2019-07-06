41d303c3425212c23860e4cb9e8c4a07378c7f52
nyanloutre <paul@nyanlout.re>
2019-07-06 21:14:48 +0200

    pythonPackages.telethon: 1.8.0 -> 1.9.0
    (cherry picked from commit 99f0083ea51039917e45dec58f139aea54796317)
    Signed-off-by: Maximilian Bosch <maximilian@mbosch.me>

--- pkgs/development/python-modules/telethon/default.nix
+++ pkgs/development/python-modules/telethon/default.nix
@@ -2,12 +2,12 @@
 
 buildPythonPackage rec {
   pname = "telethon";
-  version = "1.8.0";
+  version = "1.9.0";
 
   src = fetchPypi {
     inherit version;
     pname = "Telethon";
-    sha256 = "6e8f55cbe26a031d427f2c3e23d391b8c2272d1540bcb36fe4df65d928ca2b25";
+    sha256 = "a8797ad5bfee2b350cfc9b73cbb30fc19c8f73c0db42471e0df1371b1a269edc";
   };
 
   propagatedBuildInputs = [
