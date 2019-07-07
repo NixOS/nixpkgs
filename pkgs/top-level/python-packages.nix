34f31030b1f8b32ad3889f7adb0150900174293d
Benjamin Hipple <bhipple@protonmail.com>
2019-07-07 15:09:17 -0400

    pythonPackages.measurement: init at 2.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2454,6 +2454,8 @@ in {
 
   python-mapnik = callPackage ../development/python-modules/python-mapnik { };
 
+  measurement = callPackage ../development/python-modules/measurement {};
+
   midiutil = callPackage ../development/python-modules/midiutil {};
 
   misaka = callPackage ../development/python-modules/misaka {};
