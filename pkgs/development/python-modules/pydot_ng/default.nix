979b29a7d1cdebd4163b3400d525087071ea56d5
Charlie Dyson <charlie@charliedyson.net>
2019-07-08 22:30:37 +0100

    pythonPackages.pydot_ng: fix graphviz path and tests #64461

--- pkgs/development/python-modules/pydot_ng/default.nix
+++ pkgs/development/python-modules/pydot_ng/default.nix
@@ -5,6 +5,7 @@
 , pytest
 , unittest2
 , pkgs
+, mock
 }:
 
 buildPythonPackage rec {
@@ -16,13 +17,18 @@ buildPythonPackage rec {
     sha256 = "8c8073b97aa7030c28118961e2c6c92f046e4cb57aeba7df87146f7baa6530c5";
   };
 
-  buildInputs = [ pytest unittest2 ];
+  patches = [ ./0001-graphviz_path.patch ];
+
+  postPatch =
+    ''
+    substituteInPlace pydot_ng/__init__.py \
+      --replace "NIX_GRAPHVIZ_PATH" "'${pkgs.graphviz}/bin'"
+    '';
+
+  checkInputs = [ pytest unittest2 mock ];
   propagatedBuildInputs = [ pkgs.graphviz pyparsing ];
 
-  checkPhase = ''
-    mkdir test/my_tests
-    py.test test
-  '';
+  checkPhase = "py.test test";
 
   meta = with stdenv.lib; {
     homepage = "https://pypi.python.org/pypi/pydot-ng";
