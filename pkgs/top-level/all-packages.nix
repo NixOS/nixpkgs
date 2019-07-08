0023984140e6ab3be8575b5ac4cfe646249b7af8
Tadeo Kondrak <me@tadeo.ca>
2019-07-08 06:49:41 -0600

    libplacebo: init at 1.1.8.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11955,6 +11955,8 @@ in
 
   libphonenumber = callPackage ../development/libraries/libphonenumber { };
 
+  libplacebo = callPackage ../development/libraries/libplacebo { };
+
   libpng = callPackage ../development/libraries/libpng { };
   libpng_apng = libpng.override { apngSupport = true; };
   libpng12 = callPackage ../development/libraries/libpng/12.nix { };
