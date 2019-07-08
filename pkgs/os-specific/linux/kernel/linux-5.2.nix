1f97fea00e76ab07905dc0a038f4d7361378d4a3
Tim Steinbach <tim@nequissimus.com>
2019-07-08 07:45:27 -0400

    linux: Add 5.2
    Update linuxPackages_latest to 5.2
    
    (cherry picked from commit 3df683c8ca2fce5953749925248da6564e7ea809)

---
+++ pkgs/os-specific/linux/kernel/linux-5.2.nix
@@ -0,0 +1,18 @@
+{ stdenv, buildPackages, fetchurl, perl, buildLinux, modDirVersionArg ? null, ... } @ args:
+
+with stdenv.lib;
+
+buildLinux (args // rec {
+  version = "5.2";
+
+  # modDirVersion needs to be x.y.z, will automatically add .0 if needed
+  modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
+
+  # branchVersion needs to be x.y
+  extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
+
+  src = fetchurl {
+    url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
+    sha256 = "1ry11b5sc20jh7flnp94m20627jzl3l09rzmfjsk3a71fbv6dbal";
+  };
+} // (args.argsOverride or {}))
