ca44d5fa7319376c3a7e94af2c20e98444d674d2
Jamey Sharp <jamey@minilop.net>
2019-07-09 00:21:57 -0700

    nixos/etc: defer setting mode/ownership to end
    If environment.etc is being used to set up /etc/passwd or /etc/group,
    then we can't rely on getpwnam and getgrnam until we've already put all
    the new /etc files in place.
    
    The easiest way to fix that is to make all the symlinks and do all the
    copies first, and then go back to each copied file and fix its ownership
    and permissions.
    
    Since we're deferring ownership changes, we have to also defer
    permissions changes; otherwise an unprivileged service could lose access
    to its config files during this window. Leaving files readable by
    everyone for a bit isn't a security problem because the source of these
    files is already world-readable in the Nix store.

--- nixos/modules/system/etc/setup-etc.pl
+++ nixos/modules/system/etc/setup-etc.pl
@@ -75,15 +75,28 @@ find(\&cleanup, "/etc");
 
 
 # Use /etc/.clean to keep track of copied files.
-my @oldCopied = read_file("/etc/.clean", chomp => 1, err_mode => 'quiet');
+my @oldClean = read_file("/etc/.clean", chomp => 1, err_mode => 'quiet');
 open CLEAN, ">>/etc/.clean";
 
+
+# Leave all copied files owned by root and with their Nix store permissions
+# until all files are in place, in case /etc/passwd or /etc/group are being
+# updated this way. Since we're deferring ownership changes, we have to also
+# defer permissions changes; otherwise an unprivileged service could lose
+# access to its config files during this window. Leaving files readable by
+# everyone for a bit isn't a security problem because the source of these files
+# is already world-readable in the Nix store.
+my %copied;
+sub set_ownership {
+    my $target = shift;
+    $copied{$target} = \@_;
+}
 
 # For every file in the etc tree, create a corresponding symlink in
 # /etc to /etc/static.  The indirection through /etc/static is to make
 # switching to a new configuration somewhat more atomic.
 my %created;
-my @copied;
+my @clean;
 
 sub link {
     my $fn = substr $File::Find::name, length($etc) + 1 or next;
@@ -108,13 +121,10 @@ sub link {
             my $uid = read_file("$_.uid"); chomp $uid;
             my $gid = read_file("$_.gid"); chomp $gid;
             copy "$static/$fn", "$target.tmp" or warn;
-            $uid = getpwnam $uid unless $uid =~ /^\+/;
-            $gid = getgrnam $gid unless $gid =~ /^\+/;
-            chown int($uid), int($gid), "$target.tmp" or warn;
-            chmod oct($mode), "$target.tmp" or warn;
             rename "$target.tmp", $target or warn;
+            set_ownership $target, oct($mode), $uid, $gid;
         }
-        push @copied, $fn;
+        push @clean, $fn;
         print CLEAN "$fn\n";
     } elsif (-l "$_") {
         atomicSymlink "$static/$fn", $target or warn;
@@ -124,9 +134,20 @@ sub link {
 find(\&link, $etc);
 
 
+# Now that all files are in place, it should be safe to call getpwnam and
+# getgrnam.
+foreach my $target (keys %copied) {
+    my ($mode, $uid, $gid) = @{$copied{$target}};
+    $uid = getpwnam $uid unless $uid =~ /^\+/;
+    $gid = getgrnam $gid unless $gid =~ /^\+/;
+    chown int($uid), int($gid), "$target" or warn;
+    chmod $mode, "$target" or warn;
+}
+
+
 # Delete files that were copied in a previous version but not in the
 # current.
-foreach my $fn (@oldCopied) {
+foreach my $fn (@oldClean) {
     if (!defined $created{$fn}) {
         $fn = "/etc/$fn";
         print STDERR "removing obsolete file ‘$fn’...\n";
@@ -137,4 +158,4 @@ foreach my $fn (@oldCopied) {
 
 # Rewrite /etc/.clean.
 close CLEAN;
-write_file("/etc/.clean", map { "$_\n" } @copied);
+write_file("/etc/.clean", map { "$_\n" } @clean);
