fa48ab5556b624aeec9c408d75712fcecff9268c
xrelkd <46590321+xrelkd@users.noreply.github.com>
2019-07-09 20:56:47 +0800

    packr: 2.5.1 -> 2.5.2

--- pkgs/development/libraries/packr/default.nix
+++ pkgs/development/libraries/packr/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "packr";
-  version = "2.5.1";
+  version = "2.5.2";
 
   src = fetchFromGitHub {
     owner = "gobuffalo";
     repo = pname;
     rev = "v${version}";
-    sha256 = "070hpnsr5w1r1cg9wl80cafmhkx4z3s29wq04fa7rk49hmwml4jy";
+    sha256 = "1ciffa5xbd93fylwz93wr4m4fj83dcla55dmdshaqz28rbsapnc1";
   };
 
-  modSha256 = "0xvpk9jjcqac44s4fp0jwpljxvs0ypjwc5qfg0w90s2r7jn50fxn";
+  modSha256 = "086gydrl3i35hawb5m7rsb4a0llcpdpgid1xfw2z9n6jkwkclw4n";
 
   meta = with lib; {
     description = "The simple and easy way to embed static files into Go binaries";
