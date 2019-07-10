8fe76701fd442657df9056fd64807c195cfd7028
Doron Behar <doron.behar@gmail.com>
2019-07-10 16:06:14 +0300

    libsForQt5.qtutilities: init at 5.13.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12955,6 +12955,8 @@ in
       withQt5 = true;
     };
 
+    qtutilities = callPackage ../development/libraries/qtutilities { };
+
     qtinstaller = callPackage ../development/libraries/qtinstaller { };
 
     qtkeychain = callPackage ../development/libraries/qtkeychain {
