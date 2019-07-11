df3ae76dfd9065ce6d7bedc389f33eae768e6197
xrelkd <46590321+xrelkd@users.noreply.github.com>
2019-07-11 16:27:29 +0800

    libfilezilla: 0.16.0 -> 0.17.1

--- pkgs/development/libraries/libfilezilla/default.nix
+++ pkgs/development/libraries/libfilezilla/default.nix
@@ -1,19 +1,26 @@
-{ stdenv, fetchurl, pkgconfig, nettle }:
+{ stdenv
+, fetchurl
+
+, gettext
+, gnutls
+, nettle
+, pkgconfig
+}:
 
 stdenv.mkDerivation rec {
   pname = "libfilezilla";
-  version = "0.16.0";
+  version = "0.17.1";
 
   src = fetchurl {
     url = "https://download.filezilla-project.org/${pname}/${pname}-${version}.tar.bz2";
-    sha256 = "1fd71vmllzvljff5l5ka5wnzbdsxx4i54dpxpklydmbsqpilnv1v";
+    sha256 = "1cnkcl9vif5lz1yx813qrphlpc6gvmzxdmkbd17kh5jqiqdi9vyk";
   };
 
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ nettle ];
+  buildInputs = [ gettext gnutls nettle ];
 
   meta = with stdenv.lib; {
-    homepage = https://lib.filezilla-project.org/;
+    homepage = "https://lib.filezilla-project.org/";
     description = "A modern C++ library, offering some basic functionality to build high-performing, platform-independent programs";
     license = licenses.gpl2Plus;
     maintainers = with maintainers; [ pSub ];
