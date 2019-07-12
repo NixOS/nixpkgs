6d3021f12c087437cc11303291bb54ea05cdbb11
Pradyuman Vig <me@pradyuman.co>
2019-07-11 19:38:25 -0600

    maintainers: add pradyuman

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4080,6 +4080,16 @@
     github = "pradeepchhetri";
     name = "Pradeep Chhetri";
   };
+  pradyuman = {
+    email = "me@pradyuman.co";
+    github = "pradyuman";
+    name = "Pradyuman Vig";
+    keys = [
+      { longkeyid = "rsa4096/4F74D5361C4CA31E";
+        fingerprint = "240B 57DE 4271 2480 7CE3  EAC8 4F74 D536 1C4C A31E";
+      }
+    ];
+  };
   prikhi = {
     email = "pavan.rikhi@gmail.com";
     github = "prikhi";
