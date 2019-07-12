a354cbb7156bb19f4c9da801ec6c90255ff9e0dc
xrelkd <46590321+xrelkd@users.noreply.github.com>
2019-07-13 01:30:33 +0800

    fcitx-qt5: 1.2.1 -> 1.2.3

--- pkgs/tools/inputmethods/fcitx/fcitx-qt5.nix
+++ pkgs/tools/inputmethods/fcitx/fcitx-qt5.nix
@@ -1,24 +1,15 @@
-{ stdenv, fetchurl, cmake, fcitx, pkgconfig, qtbase, extra-cmake-modules
-, fetchpatch
-}:
+{ stdenv, fetchFromGitLab, cmake, fcitx, pkgconfig, qtbase, extra-cmake-modules }:
 
 stdenv.mkDerivation rec {
-  name = "fcitx-qt5-${version}";
-  version = "1.2.1";
+  pname = "fcitx-qt5";
+  version = "1.2.3";
 
-  src = fetchurl {
-    url = "http://download.fcitx-im.org/fcitx-qt5/${name}.tar.xz";
-    sha256 = "0z8ax0dxk88byic41mfaiahjdv1k8ciwn97xfjkkgr4ijgscdr8c";
+  src = fetchFromGitLab {
+    owner = "fcitx";
+    repo = pname;
+    rev = version;
+    sha256 = "0860v3rxsh054wkkbawvyin5mk0flp4cwfcpmcpq147lvdm5lq2i";
   };
-
-  patches = [
-    # Fix build with Qt 5.11
-    # https://github.com/fcitx/fcitx-qt5/issues/34
-    (fetchpatch {
-      url = https://github.com/fcitx/fcitx-qt5/commit/af033e3d5305108eecc568adff7f8b2da5831ed6.diff;
-      sha256 = "14vfz1fw2k362wnqpglw766fg3d3mc8cmfgic2p96yyipjh9xx3b";
-    })
-  ];
 
   nativeBuildInputs = [ cmake extra-cmake-modules pkgconfig ];
 
@@ -32,11 +23,10 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with stdenv.lib; {
-    homepage    = https://github.com/fcitx/fcitx-qt5;
+    homepage    = https://gitlab.com/fcitx/fcitx-qt5;
     description = "Qt5 IM Module for Fcitx";
     license     = licenses.gpl2;
     platforms   = platforms.linux;
     maintainers = with maintainers; [ ericsagnes ];
   };
-
 }
