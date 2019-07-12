2c396d9f0785b062c952db4c93ee521108bcdccf
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-07-12 11:23:56 -0400

    pythonPackages.pysparse: init at 1.1.1-dev

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4274,6 +4274,8 @@ in {
 
   pyspread = callPackage ../development/python-modules/pyspread { };
 
+  pysparse = callPackage ../development/python-modules/pysparse { };
+
   pyupdate = callPackage ../development/python-modules/pyupdate {};
 
   pyx = callPackage ../development/python-modules/pyx { };
