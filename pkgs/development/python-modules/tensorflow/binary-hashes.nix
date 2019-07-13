4172bc0eac2c8c35c59022099b5e55bcea7b039b
Nikolay Amiantov <ab@fmap.me>
2019-07-13 22:37:01 +0300

    python.pkgs.tensorflow: cleanup binary build

--- pkgs/development/python-modules/tensorflow/binary-hashes.nix
+++ pkgs/development/python-modules/tensorflow/binary-hashes.nix
@@ -1,4 +1,5 @@
 {
+version = "1.14.0";
 linux_py_27_cpu = {
   url = "https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.14.0-cp27-none-linux_x86_64.whl";
   sha256 = "0yywdrfk97dh1bxhibspg0raz70fx9lcczj6xlimqy4xb60clx7k";
