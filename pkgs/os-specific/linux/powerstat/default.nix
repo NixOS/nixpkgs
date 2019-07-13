e9c3787793b9ea8f782456016d64f9b1324664f0
worldofpeace <worldofpeace@protonmail.ch>
2019-07-13 12:14:05 -0400

    powerstat: pname

--- pkgs/os-specific/linux/powerstat/default.nix
+++ pkgs/os-specific/linux/powerstat/default.nix
@@ -1,17 +1,21 @@
 { stdenv, lib, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "powerstat-${version}";
+  pname = "powerstat";
   version = "0.02.19";
+  
   src = fetchurl {
-    url = "https://kernel.ubuntu.com/~cking/tarballs/powerstat/powerstat-${version}.tar.gz";
+    url = "https://kernel.ubuntu.com/~cking/tarballs/${pname}/${pname}-${version}.tar.gz";
     sha256 = "0yh6l2mx5gfdgrfx406hxbi03c12cgi29pwlzgdfrpz6zs2icaw5";
   };
+  
   installFlags = [ "DESTDIR=${placeholder "out"}" ];
+  
   postInstall = ''
     mv $out/usr/* $out
     rm -r $out/usr
   '';
+  
   meta = with lib; {
     description = "Laptop power measuring tool";
     homepage = https://kernel.ubuntu.com/~cking/powerstat/;
