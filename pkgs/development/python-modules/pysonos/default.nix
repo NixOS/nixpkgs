67b463c9363c5fb0590ec1a00ff16b04a6bf8f79
Mario Rodas <marsam@users.noreply.github.com>
2019-07-13 04:23:00 -0500

    python3Packages.pysonos: 0.0.21 -> 0.0.22

--- pkgs/development/python-modules/pysonos/default.nix
+++ pkgs/development/python-modules/pysonos/default.nix
@@ -7,27 +7,31 @@
 , ifaddr
 
 # Test dependencies
-, pytest_3, pylint, flake8, graphviz
+, pytest, pylint, flake8, graphviz
 , mock, sphinx, sphinx_rtd_theme
 }:
 
 buildPythonPackage rec {
   pname = "pysonos";
-  version = "0.0.21";
+  version = "0.0.22";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0x2nznjnm721qw9nys5ap3b6hq9s48bsd1yj5xih50pvn0rf0nz2";
+    sha256 = "4a4fe630b97c81261246a448fe9dd2bdfaacd7df4453cf72f020599171416442";
   };
 
   propagatedBuildInputs = [ xmltodict requests ifaddr ];
 
   checkInputs = [
-    pytest_3 pylint flake8 graphviz
+    pytest pylint flake8 graphviz
     mock sphinx sphinx_rtd_theme
   ];
+
+  checkPhase = ''
+    pytest --deselect=tests/test_discovery.py::TestDiscover::test_discover
+  '';
 
   meta = {
     homepage = https://github.com/amelchio/pysonos;
