8f2d6a5709aa89f91dbac5b8a81e1b2457becd06
taku0 <mxxouy6x3m_github@tatapa.org>
2019-07-14 14:52:00 +0900

    rust-cbindgen: 0.8.3 -> 0.8.7

--- pkgs/development/tools/rust/cbindgen/default.nix
+++ pkgs/development/tools/rust/cbindgen/default.nix
@@ -2,18 +2,21 @@
 
 rustPlatform.buildRustPackage rec {
   name = "rust-cbindgen-${version}";
-  version = "0.8.3";
+  version = "0.8.7";
 
   src = fetchFromGitHub {
     owner = "eqrion";
     repo = "cbindgen";
     rev = "v${version}";
-    sha256 = "08zlnk1k1nddjciccfdcplxqngsnz6ml3zxm57mijabzybry8zz1";
+    sha256 = "040rivayr0dgmrhlly5827c850xbr0j5ngiy6rvwyba5j9iv2x0y";
   };
 
   cargoSha256 = "1nig4891p7ii4z4f4j4d4pxx39f501g7yrsygqbpkr1nrgjip547";
 
   buildInputs = stdenv.lib.optional stdenv.isDarwin Security;
+
+  # https://github.com/eqrion/cbindgen/issues/338
+  RUSTC_BOOTSTRAP = 1;
 
   meta = with stdenv.lib; {
     description = "A project for generating C bindings from Rust code";
