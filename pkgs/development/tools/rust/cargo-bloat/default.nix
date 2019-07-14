bf811c4c389cd4077a8a52340af1537048714111
xrelkd <46590321+xrelkd@users.noreply.github.com>
2019-07-14 09:48:42 +0800

    cargo-bloat: 0.7.1 -> 0.7.2

--- pkgs/development/tools/rust/cargo-bloat/default.nix
+++ pkgs/development/tools/rust/cargo-bloat/default.nix
@@ -1,19 +1,19 @@
-{ stdenv, rustPlatform, fetchFromGitHub }:
+{ lib, rustPlatform, fetchFromGitHub }:
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-bloat";
-  version = "0.7.1";
+  version = "0.7.2";
 
   src = fetchFromGitHub {
     owner = "RazrFalcon";
     repo = pname;
     rev = "v${version}";
-    sha256 = "05dk2y223fjaw49sj0pia9ddccs1j0x4fdyz0bnss38dgf1njygx";
+    sha256 = "1g011c98vix0285ws8xby5v50272v15zfyaj948vazsb4zl0gxbm";
   };
 
   cargoSha256 = "0bpy8888zbqy9b8hkbfsdxqcs88dn2r7p3qnhfn7a6ri4bw7ihhw";
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "A tool and Cargo subcommand that helps you find out what takes most of the space in your executable";
     homepage = "https://github.com/RazrFalcon/cargo-bloat";
     license = licenses.mit;
