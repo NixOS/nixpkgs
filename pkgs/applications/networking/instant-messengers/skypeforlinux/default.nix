429adc69dfbc769f94f4485c3642ea0a1f5e9958
R. RyanTM <ryantm-bot@ryantm.com>
2019-07-16 14:17:56 -0700

    skypeforlinux: 8.47.0.59 -> 8.49.0.49
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/skypeforlinux/versions
    
    (cherry picked from commit 2928e4c3fc874e6e146c57e08e1d99bfa0b9f874)

--- pkgs/applications/networking/instant-messengers/skypeforlinux/default.nix
+++ pkgs/applications/networking/instant-messengers/skypeforlinux/default.nix
@@ -7,7 +7,7 @@ let
 
   # Please keep the version x.y.0.z and do not update to x.y.76.z because the
   # source of the latter disappears much faster.
-  version = "8.47.0.59";
+  version = "8.49.0.49";
 
   rpath = stdenv.lib.makeLibraryPath [
     alsaLib
@@ -58,7 +58,7 @@ let
     if stdenv.hostPlatform.system == "x86_64-linux" then
       fetchurl {
         url = "https://repo.skype.com/deb/pool/main/s/skypeforlinux/skypeforlinux_${version}_amd64.deb";
-        sha256 = "0haiccmimbj1nyyyj556b0a181walmxwbbr0m18m2w67wi5z783r";
+        sha256 = "0l5q336kkw9i13076qn7fkknypg7cwjp58qi8xd6h0rwha3kkqa2";
       }
     else
       throw "Skype for linux is not supported on ${stdenv.hostPlatform.system}";
