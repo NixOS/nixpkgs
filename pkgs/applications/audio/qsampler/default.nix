f91d69307c6aac6e6c6dee549317a0c6c9a77252
R. RyanTM <ryantm-bot@ryantm.com>
2019-07-16 10:25:47 -0700

    qsampler: 0.5.5 -> 0.5.6
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/qsampler/versions

--- pkgs/applications/audio/qsampler/default.nix
+++ pkgs/applications/audio/qsampler/default.nix
@@ -3,11 +3,11 @@
 
 stdenv.mkDerivation rec {
   name = "qsampler-${version}";
-  version = "0.5.5";
+  version = "0.5.6";
 
   src = fetchurl {
     url = "mirror://sourceforge/qsampler/${name}.tar.gz";
-    sha256 = "1li2p8zknrdr62wlaassfvgski0rlbr3lvrzywbh32dq8j50w8zf";
+    sha256 = "0lx2mzyajmjckwfvgf8p8bahzpj0n0lflyip41jk32nwd2hzjhbs";
   };
 
   nativeBuildInputs = [ autoconf automake libtool pkgconfig qttools ];
