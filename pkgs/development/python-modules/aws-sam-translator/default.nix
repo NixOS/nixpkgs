541eb4661bfad25ff4772467975bfb0b7de307fb
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-07-17 17:12:14 -0400

    pythonPackages.aws-sam-translator: 1.11.0 -> 1.12.0 fix python-unstable

--- pkgs/development/python-modules/aws-sam-translator/default.nix
+++ pkgs/development/python-modules/aws-sam-translator/default.nix
@@ -10,21 +10,21 @@
 
 buildPythonPackage rec {
   pname = "aws-sam-translator";
-  version = "1.11.0";
+  version = "1.12.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "db872c43bdfbbae9fc8c9201e6a7aeb9a661cda116a94708ab0577b46a38b962";
+    sha256 = "0ic9c2smfwp7sm0ahbzvihq3h1snabigfrc4pqv96v21iw5llv2g";
   };
-
-  # Tests are not included in the PyPI package
-  doCheck = false;
 
   propagatedBuildInputs = [
     boto3
     jsonschema
     six
   ] ++ lib.optionals (pythonOlder "3.4") [ enum34 ];
+
+  # infinite recusion cnf-lint depends on aws-sam-translator
+  doCheck = false;
 
   meta = {
     homepage = https://github.com/awslabs/serverless-application-model;
