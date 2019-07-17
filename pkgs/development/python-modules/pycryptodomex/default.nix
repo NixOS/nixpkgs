55c978c94e1fc419f7a2bbe46b537f8dbe7d38c0
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-07-16 17:48:40 -0700

    pythonPackages.pycryptodomex: 3.7.3 -> 3.8.2

--- pkgs/development/python-modules/pycryptodomex/default.nix
+++ pkgs/development/python-modules/pycryptodomex/default.nix
@@ -2,16 +2,16 @@
 
 buildPythonPackage rec {
   pname = "pycryptodomex";
-  version = "3.7.3";
+  version = "3.8.2";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "01qnl81s8qdmkmwindvcz8zk05bbhk5iin3hppwbbimxdfpia2z5";
+  };
 
   meta = {
     description = "A self-contained cryptographic library for Python";
     homepage = https://www.pycryptodome.org;
     license = lib.licenses.bsd2;
-  };
-
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "b70fe991564e178af02ccf89435a8f9e8d052707a7c4b95bf6027cb785da3175";
   };
 }
