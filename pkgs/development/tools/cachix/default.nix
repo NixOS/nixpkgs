c4b1ceb5a7c9a93062be1dec041370a8eaeb0999
Domen Kožar <domen@dev.si>
2019-07-18 10:41:14 +0200

    cachix: fix #65007

--- pkgs/development/tools/cachix/default.nix
+++ pkgs/development/tools/cachix/default.nix
@@ -1,8 +1,6 @@
 { haskellPackages, haskell }:
 
-haskell.lib.justStaticExecutables (haskellPackages.override {
-  overrides = self: super: {
+haskell.lib.justStaticExecutables (haskellPackages.extend (self: super: {
     cachix = haskell.lib.doDistribute (self.cachix_0_2_1 or self.cachix);
     cachix-api = self.cachix-api_0_2_1 or self.cachix-api;
-  };
-}).cachix
+})).cachix
