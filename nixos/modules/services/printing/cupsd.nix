28040465be4d15bedd2ee9397973e670e2e5ec2c
Matthew Bauer <mjbauer95@gmail.com>
2019-07-18 17:35:13 -0400

    nixos/cupsd: include /run/cups/cups.sock in ListenStreams
    This socket should always be created by systemd.

--- nixos/modules/services/printing/cupsd.nix
+++ nixos/modules/services/printing/cupsd.nix
@@ -304,7 +304,8 @@ in
 
     systemd.sockets.cups = mkIf cfg.startWhenNeeded {
       wantedBy = [ "sockets.target" ];
-      listenStreams = map (x: replaceStrings ["localhost"] ["127.0.0.1"] (removePrefix "*:" x)) cfg.listenAddresses;
+      listenStreams = [ "/run/cups/cups.sock" ]
+        ++ map (x: replaceStrings ["localhost"] ["127.0.0.1"] (removePrefix "*:" x)) cfg.listenAddresses;
     };
 
     systemd.services.cups =
