5498d4aaedb37ab571a0a280c80a6a66564075eb
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-07-18 09:17:06 -0400

    pythonPackages.croniter: init at 0.3.30

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1671,6 +1671,8 @@ in {
 
   crc16 = callPackage ../development/python-modules/crc16 { };
 
+  croniter = callPackage ../development/python-modules/croniter { };
+
   csscompressor = callPackage ../development/python-modules/csscompressor {};
 
   csvs-to-sqlite = callPackage ../development/python-modules/csvs-to-sqlite { };
