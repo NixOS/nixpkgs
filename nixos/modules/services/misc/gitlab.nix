bbd4a0c1001a2cce7457c8ae2a8f8ef905daa424
Johan Thomsen <jth@dbc.dk>
2019-07-19 14:16:15 +0200

    nixos/gitlab: gitlab-workhorse requires exiftool on path to process uploaded images

--- nixos/modules/services/misc/gitlab.nix
+++ nixos/modules/services/misc/gitlab.nix
@@ -585,6 +585,7 @@ in {
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
       path = with pkgs; [
+        exiftool
         gitAndTools.git
         gnutar
         gzip
