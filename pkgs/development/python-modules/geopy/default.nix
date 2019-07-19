6f40b1c93eb2fba0133085bbab9ecdc9f72d2be7
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-07-18 22:23:34 -0700

    pythonPackages.geopy: enable the packge on Python3

--- pkgs/development/python-modules/geopy/default.nix
+++ pkgs/development/python-modules/geopy/default.nix
@@ -1,30 +1,28 @@
 { stdenv
 , buildPythonPackage
 , fetchPypi
+, geographiclib
 , isPy27
 , mock
-, tox
 , pylint
+, pythonOlder
+, tox
 }:
 
 buildPythonPackage rec {
   pname = "geopy";
   version = "1.20.0";
-  disabled = !isPy27;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "9419bc90ee6231590c4ae7acf1cf126cefbd0736942da7a6a1436946e80830e2";
   };
 
-  doCheck = false;  # too much
-
-  buildInputs = [ mock tox pylint ];
+  propagatedBuildInputs = [ geographiclib ];
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/geopy/geopy";
     description = "Python Geocoding Toolbox";
     license = licenses.mit;
   };
-
 }
