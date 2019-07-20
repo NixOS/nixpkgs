909d2c42f2c1215a95175d21b4a717dca357b279
Max Gonzih <gonzih@gmail.com>
2019-07-20 08:05:04 -0400

    slack: Fix dark theme
    Caused by #61328

--- pkgs/applications/networking/instant-messengers/slack/default.nix
+++ pkgs/applications/networking/instant-messengers/slack/default.nix
@@ -94,16 +94,14 @@ in stdenv.mkDerivation {
       --replace /usr/share/ $out/share/
   '' + stdenv.lib.optionalString (theme != null) ''
     cat <<EOF >> $out/lib/slack/resources/app.asar.unpacked/src/static/ssb-interop.js
+    var fs = require('fs');
     document.addEventListener('DOMContentLoaded', function() {
     let tt__customCss = ".menu ul li a:not(.inline_menu_link) {color: #fff !important;}"
-    $.ajax({
-        url: '${theme}/theme.css',
-        success: function(css) {
+      fs.readFile('${theme}/theme.css', 'utf8', function(err, css) {
             \$("<style></style>").appendTo('head').html(css + tt__customCss);
             \$("<style></style>").appendTo('head').html('#reply_container.upload_in_threads .inline_message_input_container {background: padding-box #545454}');
             \$("<style></style>").appendTo('head').html('.p-channel_sidebar {background: #363636 !important}');
             \$("<style></style>").appendTo('head').html('#client_body:not(.onboarding):not(.feature_global_nav_layout):before {background: inherit;}');
-        }
       });
     });
     EOF
