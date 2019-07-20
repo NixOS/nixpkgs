17abf02acdbf1b4c84549cf1e94a09d2b90bbe41
Mario Rodas <marsam@users.noreply.github.com>
2019-07-19 21:29:24 -0500

    dune: 1.10.0 -> 1.11.0

--- pkgs/development/tools/ocaml/dune/default.nix
+++ pkgs/development/tools/ocaml/dune/default.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   pname = "dune";
-  version = "1.10.0";
+  version = "1.11.0";
   src = fetchurl {
-    url = "https://github.com/ocaml/dune/releases/download/${version}/dune-${version}.tbz";
-    sha256 = "15fx9rg16g7ig43rg4sdq0wp0br5h1mjxxgv8b15s317vqlfc5pd";
+    url = "https://github.com/ocaml/dune/releases/download/${version}/dune-build-info-${version}.tbz";
+    sha256 = "11jl2vavypbgvwblch25q10hsd16myik9b3cd4d64zhxk1fzbzdw";
   };
 
   buildInputs = [ ocaml findlib ];
