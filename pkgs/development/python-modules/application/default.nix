eea6d99b33bb8157cbc3cae50a545179bc1325fe
zimbatm <zimbatm@zimbatm.com>
2019-07-20 12:22:17 +0200

    pythonPackages.application: use fetchPypi
    ... and fix the twisted logger

--- pkgs/development/python-modules/application/default.nix
+++ pkgs/development/python-modules/application/default.nix
@@ -1,15 +1,20 @@
-{ lib, buildPythonPackage, fetchdarcs, zope_interface, isPy3k }:
+{ lib, buildPythonPackage, fetchPypi, zope_interface, isPy3k }:
 
 buildPythonPackage rec {
   pname = "python-application";
   version = "2.7.0";
   disabled = isPy3k;
 
-  src = fetchdarcs {
-    url = "http://devel.ag-projects.com/repositories/${pname}";
-    rev = "release-${version}";
-    sha256 = "1xpyk2v3naxkjhpyris58dxg1lxbraxgjd6f7w1sah5j0sk7psla";
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-sf9415QDwbBE5dexSEJmPgraCys1dZV94gf7MG+TbdM=";
   };
+
+  patchPhase = ''
+    sed \
+      -e 's/    import twisted/    #import twisted/g' \
+      -i application/log/extensions/twisted/twisted.py
+  '';
 
   buildInputs = [ zope_interface ];
 
