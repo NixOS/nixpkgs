56146c0cfa2204f2e29b5680a83581ff60871754
Frederik Rietdijk <fridh@fridh.nl>
2019-07-21 08:29:11 +0200

    Revert "spotify: 1.0.96.181.gf6bc1b6b-12 -> 1.1.5.153.gf614956d-16"
    Segmentation fault.
    
    This reverts commit 3670450fc68554373af74e5d72e8edea11d9b519.

--- pkgs/applications/audio/spotify/default.nix
+++ pkgs/applications/audio/spotify/default.nix
@@ -1,7 +1,7 @@
 { fetchurl, stdenv, squashfsTools, xorg, alsaLib, makeWrapper, openssl, freetype
 , glib, pango, cairo, atk, gdk_pixbuf, gtk2, cups, nspr, nss, libpng, libnotify
 , libgcrypt, systemd, fontconfig, dbus, expat, ffmpeg_3, curl, zlib, gnome3
-, at-spi2-atk, at-spi2-core
+, at-spi2-atk
 }:
 
 let
@@ -10,21 +10,20 @@ let
   # If an update breaks things, one of those might have valuable info:
   # https://aur.archlinux.org/packages/spotify/
   # https://community.spotify.com/t5/Desktop-Linux
-  version = "1.1.5.153.gf614956d-16";
+  version = "1.0.96.181.gf6bc1b6b-12";
   # To get the latest stable revision:
   # curl -H 'X-Ubuntu-Series: 16' 'https://api.snapcraft.io/api/v1/snaps/details/spotify?channel=stable' | jq '.download_url,.version,.last_updated'
   # To get general information:
   # curl -H 'Snap-Device-Series: 16' 'https://api.snapcraft.io/v2/snaps/info/spotify' | jq '.'
   # More examples of api usage:
   # https://github.com/canonical-websites/snapcraft.io/blob/master/webapp/publisher/snaps/views.py
-  rev = "35";
+  rev = "30";
 
 
   deps = [
     alsaLib
     atk
     at-spi2-atk
-    at-spi2-core
     cairo
     cups
     curl
@@ -73,7 +72,7 @@ stdenv.mkDerivation {
   # https://community.spotify.com/t5/Desktop-Linux/Redistribute-Spotify-on-Linux-Distributions/td-p/1695334
   src = fetchurl {
     url = "https://api.snapcraft.io/api/v1/snaps/download/pOBIoZ2LrCB3rDohMxoYGnbN14EHOgD7_${rev}.snap";
-    sha512 = "9c2b7c2c6da28fa0c1d75d1ff77aa50604e3d60716f6bfe2b3da452d5d89c9c53c819921a60e1997a8cc42d07d7c15460f707ac5d21180fbb2dcc006e7b0e82b";
+    sha512 = "859730fbc80067f0828f7e13eee9a21b13b749f897a50e17c2da4ee672785cfd79e1af6336e609529d105e040dc40f61b6189524783ac93d49f991c4ea8b3c56";
   };
 
   buildInputs = [ squashfsTools makeWrapper ];
