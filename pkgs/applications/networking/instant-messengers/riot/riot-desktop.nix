a1cb6d601d157f73581ebf6b071f78e01a8b8562
worldofpeace <worldofpeace@protonmail.ch>
2019-07-20 21:29:09 -0400

    riot-desktop: add worldofpeace to maintainers

--- pkgs/applications/networking/instant-messengers/riot/riot-desktop.nix
+++ pkgs/applications/networking/instant-messengers/riot/riot-desktop.nix
@@ -81,7 +81,7 @@ in mkYarnPackage rec {
     description = "A feature-rich client for Matrix.org";
     homepage = https://about.riot.im/;
     license = licenses.asl20;
-    maintainers = with maintainers; [ pacien ];
+    maintainers = with maintainers; [ pacien worldofpeace ];
     inherit (electron.meta) platforms;
   };
 }
