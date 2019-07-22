04c1ca24a97a79f1c737c434bc6d08339814721e
bessonm <besson.mael@gmail.com>
2019-07-22 20:01:30 +0200

    maven: Wrap mvnDebug for convenience

--- pkgs/development/tools/build-managers/apache-maven/builder.sh
+++ pkgs/development/tools/build-managers/apache-maven/builder.sh
@@ -6,6 +6,7 @@ mkdir -p $out/maven
 cp -r $name/* $out/maven
 
 makeWrapper $out/maven/bin/mvn $out/bin/mvn --set JAVA_HOME "$jdk"
+makeWrapper $out/maven/bin/mvnDebug $out/bin/mvnDebug --set JAVA_HOME "$jdk"
 
 # Add the maven-axis and JIRA plugin by default when using maven 1.x
 if [ -e $out/maven/bin/maven ]
