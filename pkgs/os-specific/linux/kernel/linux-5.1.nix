1851d52633aacba98553c2209ff54c03596b0f26
Tim Steinbach <tim@nequissimus.com>
2019-07-21 20:35:10 -0400

    linux: 5.1.18 -> 5.1.19

--- pkgs/os-specific/linux/kernel/linux-5.1.nix
+++ pkgs/os-specific/linux/kernel/linux-5.1.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.1.18";
+  version = "5.1.19";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0spkk3pqp5fhvchgjhcm53zl1qqwsvdy7p4filb1nz4xypfff4v0";
+    sha256 = "1hk8dhp402vr74wq22j66yqr7iik0mcrahpmmq3dcl657vw1n8qf";
   };
 } // (args.argsOverride or {}))
