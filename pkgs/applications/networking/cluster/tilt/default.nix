8d8ed8e6f6107a7508c843661004961e0662422a
xrelkd <46590321+xrelkd@users.noreply.github.com>
2019-07-23 21:08:33 +0800

    tilt: 0.9.2 -> 0.9.4

--- pkgs/applications/networking/cluster/tilt/default.nix
+++ pkgs/applications/networking/cluster/tilt/default.nix
@@ -5,20 +5,20 @@ buildGoPackage rec {
   /* Do not use "dev" as a version. If you do, Tilt will consider itself
      running in development environment and try to serve assets from the
      source tree, which is not there once build completes.  */
-  version = "0.9.2";
+  version = "0.9.4";
 
   src = fetchFromGitHub {
     owner  = "windmilleng";
     repo   = pname;
     rev    = "v${version}";
-    sha256 = "1kwama90bxmnylmj6hipnx5raiqq42dq31lb66xyd89fzbkdnzk4";
+    sha256 = "1n1hys9mwqr4jiwl2z5bi2lgbw4rp800hsavih87xzrda1gzvmad";
   };
 
   goPackagePath = "github.com/windmilleng/tilt";
 
   subPackages = [ "cmd/tilt" ];
 
-  buildFlagsArray = ("-ldflags=-X main.version=${version} -X main.date=2019-07-10");
+  buildFlagsArray = ("-ldflags=-X main.version=${version} -X main.date=2019-07-23");
 
   meta = with stdenv.lib; {
     description = "Local development tool to manage your developer instance when your team deploys to Kubernetes in production";
