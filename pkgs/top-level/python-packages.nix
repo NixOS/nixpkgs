4c85595ada3f4c6df9754c381e702f38c7cbaaf8
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-07-24 14:46:12 -0400

    pythonPackages.simple-salesforce: init at 0.74.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1101,6 +1101,8 @@ in {
 
   simpleeval = callPackage ../development/python-modules/simpleeval { };
 
+  simple-salesforce = callPackage ../development/python-modules/simple-salesforce { };
+
   singledispatch = callPackage ../development/python-modules/singledispatch { };
 
   sip = callPackage ../development/python-modules/sip { };
