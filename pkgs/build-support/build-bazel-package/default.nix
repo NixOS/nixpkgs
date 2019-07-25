a4b84da7e3ff1c4142af1b8b67440c681c47f0f3
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-07-24 22:55:20 -0700

    remove unecessary parens

--- pkgs/build-support/build-bazel-package/default.nix
+++ pkgs/build-support/build-bazel-package/default.nix
@@ -119,7 +119,7 @@ in stdenv.mkDerivation (fBuildAttrs // {
     # wrappers picking them up, pass them in explicitly via `--copt`, `--linkopt`
     # and related flags.
     #
-    '' + (lib.optionalString stdenv.isDarwin ''
+    '' + lib.optionalString stdenv.isDarwin ''
     copts=()
     host_copts=()
     for flag in $NIX_CFLAGS_COMPILE; do
@@ -136,7 +136,7 @@ in stdenv.mkDerivation (fBuildAttrs // {
       linkopts+=( "--linkopt=$flag" )
       host_linkopts+=( "--host_linkopt=$flag" )
     done
-    '') + ''
+    '' + ''
 
     BAZEL_USE_CPP_ONLY_TOOLCHAIN=1 \
     USER=homeless-shelter \
@@ -145,12 +145,12 @@ in stdenv.mkDerivation (fBuildAttrs // {
       --output_user_root="$bazelUserRoot" \
       build \
       -j $NIX_BUILD_CORES \
-      '' + (lib.optionalString stdenv.isDarwin ''
+      '' + lib.optionalString stdenv.isDarwin ''
       "''${copts[@]}" \
       "''${host_copts[@]}" \
       "''${linkopts[@]}" \
       "''${host_linkopts[@]}" \
-      '') + ''
+      '' + ''
       $bazelFlags \
       $bazelTarget
 
