ab362a2bb151b61a129ae18611f14524a471c709
Max Gonzih <gonzih@gmail.com>
2019-07-26 14:33:47 -0400

    k9s: Init at 0.7.13

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18917,6 +18917,8 @@ in
 
   kubectl = callPackage ../applications/networking/cluster/kubectl { };
 
+  k9s = callPackage ../applications/networking/cluster/k9s { };
+
   kubernetes-helm = callPackage ../applications/networking/cluster/helm { };
 
   kubetail = callPackage ../applications/networking/cluster/kubetail { } ;
