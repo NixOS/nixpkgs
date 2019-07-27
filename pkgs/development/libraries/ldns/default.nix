d9c19f4aa69954c43e704e7216c90c5306e58f1b
Will Dietz <w@wdtz.org>
2019-07-26 21:10:17 -0500

    ldns: add myself as maintainer! :)

--- pkgs/development/libraries/ldns/default.nix
+++ pkgs/development/libraries/ldns/default.nix
@@ -45,6 +45,6 @@ stdenv.mkDerivation rec {
     license = licenses.bsd3;
     homepage = http://www.nlnetlabs.nl/projects/ldns/;
     platforms = platforms.unix;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ dtzWill ];
   };
 }
