5f8c6f262a5c3f281c38218e83e50e336657645e
Bart Bakker <bart@thesoftwarecraft.com>
2019-07-28 22:09:34 +0200

    firejail: allow wrapping .desktop applications
    In addition to wrapping binaries it's convenient to wrap the 'Exec' attribute in
    .desktop files with firejail. For example, wrapping the firefox binary with firejail
    removes the firefox.desktop file so you cannot start firefox using an
    application launches like in Gnome.
    
    This patch allows to specify an attribute set of applications whose desktop file
    to wrap. For convenience it allows wrapping their binary through the same config
    element. Since firejail options cannot be part of the desktop file attribute, a
    separate option is provided for those.

--- nixos/modules/programs/firejail.nix
+++ nixos/modules/programs/firejail.nix
@@ -5,6 +5,13 @@ with lib;
 let
   cfg = config.programs.firejail;
 
+  applicationBinaries =
+    let
+      withBinary = _: application: application.binary != null;
+      toBinary = _: application: "${application.jailOptions} ${application.binary}";
+    in
+      mapAttrs toBinary (filterAttrs withBinary cfg.wrappedApplications);
+
   wrappedBins = pkgs.stdenv.mkDerivation rec {
     name = "firejail-wrapped-binaries";
     nativeBuildInputs = with pkgs; [ makeWrapper ];
@@ -16,7 +23,21 @@ let
       /run/wrappers/bin/firejail ${binary} "\$@"
       _EOF
       chmod 0755 $out/bin/${command}
-      '') cfg.wrappedBinaries)}
+      '') (cfg.wrappedBinaries // applicationBinaries))}
+    '';
+  };
+
+  wrappedApps = pkgs.stdenv.mkDerivation rec {
+    name = "firejail-wrapped-applications";
+    nativeBuildInputs = with pkgs; [ makeWrapper ];
+    buildCommand = ''
+      mkdir -p $out/share/applications
+      ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: app: ''
+      cat ${app.desktopFile} \
+          | sed -e 's:^Exec\s*=:Exec=/run/wrappers/bin/firejail ${app.jailOptions}:' \
+          > $out/share/applications/${name}.desktop
+      chmod 0555 $out/share/applications/${name}.desktop
+      '') cfg.wrappedApplications)}
     '';
   };
 
@@ -36,12 +57,38 @@ in {
         not wrapped if they specify the absolute path to the binary.
       '';
     };
+
+    wrappedApplications = mkOption {
+      type = types.attrsOf (types.submodule {
+        options = {
+          binary = mkOption {
+            type = types.nullOr types.string;
+            description = ''
+              If set, wrap the binary in firejail and place it in the global path.
+            '';
+          };
+          desktopFile = mkOption {
+            type = types.string;
+            description = ''
+              Wrap the desktop file 'Exec' in firejail.
+            '';
+          };
+          jailOptions = mkOption {
+            type = types.str;
+            default = "";
+            description = ''
+              Options to pass on to firejail.
+            '';
+          };
+        };
+      });
+    };
   };
 
   config = mkIf cfg.enable {
     security.wrappers.firejail.source = "${lib.getBin pkgs.firejail}/bin/firejail";
 
-    environment.systemPackages = [ wrappedBins ];
+    environment.systemPackages = [ wrappedBins wrappedApps ];
   };
 
   meta.maintainers = with maintainers; [ peterhoeg ];
