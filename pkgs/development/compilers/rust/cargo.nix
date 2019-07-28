b53e237ef679ee8f4dd366750ef01f5ac83de80a
arcnmx <arcnmx@users.noreply.github.com>
2019-07-27 23:35:59 -0700

    cargo: remove darwin hack
    This seems to have recently resolved itself with updates
    and is no longer necessary.

--- pkgs/development/compilers/rust/cargo.nix
+++ pkgs/development/compilers/rust/cargo.nix
@@ -26,13 +26,6 @@ rustPlatform.buildRustPackage rec {
   # fixes: the cargo feature `edition` requires a nightly version of Cargo, but this is the `stable` channel
   RUSTC_BOOTSTRAP = 1;
 
-  # FIXME: Use impure version of CoreFoundation because of missing symbols.
-  # CFURLSetResourcePropertyForKey is defined in the headers but there's no
-  # corresponding implementation in the sources from opensource.apple.com.
-  preConfigure = stdenv.lib.optionalString stdenv.isDarwin ''
-    export NIX_CFLAGS_COMPILE="-F${CoreFoundation}/Library/Frameworks $NIX_CFLAGS_COMPILE"
-  '';
-
   postInstall = ''
     # NOTE: We override the `http.cainfo` option usually specified in
     # `.cargo/config`. This is an issue when users want to specify
