15564fbb4d9e349f75edc1d4791bd7b5f4865973
Frederik Rietdijk <fridh@fridh.nl>
2019-07-28 09:31:48 +0200

    wire-desktop: fix evaluating on other platforms
    to unblock tarball job

--- pkgs/applications/networking/instant-messengers/wire-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/wire-desktop/default.nix
@@ -18,12 +18,12 @@ let
   version = {
     "x86_64-linux" = "3.9.2895";
     "x86_64-darwin" = "3.9.2943";
-  }.${system};
+  }.${system} or "";
 
   sha256 = {
     "x86_64-linux" = "0wrn95m64j4b7ym44h9zawq13kg4m12aixlyyzp56bfyczmjq4a5";
     "x86_64-darwin" = "1y1bzsjmjrj518q29xfx6gg1nhdbaz7y5hzaqrp241az6plp090k";
-  }.${system};
+  }.${system} or "";
 
   meta = with stdenv.lib; {
     description = "A modern, secure messenger for everyone";
