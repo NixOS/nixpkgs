8a3a4ceecac2b76a81922c25b458935c3caa4cbf
Daniel Schaefer <git@danielschaefer.me>
2019-07-29 19:56:30 +0200

    tgt: Install systemd tgtd.service
    Resolves #38012

--- pkgs/tools/networking/tgt/default.nix
+++ pkgs/tools/networking/tgt/default.nix
@@ -38,6 +38,13 @@ stdenv.mkDerivation rec {
       --replace "#!/usr/bin/perl" "#! ${perl}/bin/perl -I${perlPackages.ConfigGeneral}/${perl.libPrefix}"
     wrapProgram $out/sbin/tgt-admin --prefix PATH : \
       ${lib.makeBinPath [ lsof sg3_utils (placeholder "out") ]}
+
+    install -D scripts/tgtd.service $out/etc/systemd/system/tgtd.service
+    substituteInPlace $out/etc/systemd/system/tgtd.service \
+      --replace "/usr/sbin/tgt" "$out/bin/tgt"
+
+    # See https://bugzilla.redhat.com/show_bug.cgi?id=848942
+    sed -i '/ExecStart=/a ExecStartPost=${coreutils}/bin/sleep 5' $out/etc/systemd/system/tgtd.service
   '';
 
   enableParallelBuilding = true;
