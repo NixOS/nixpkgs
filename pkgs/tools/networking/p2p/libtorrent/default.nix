11961d35e719e4caebcfa8e5b2a452e1ae2f5ddc
Robin Gloster <mail@glob.in>
2019-07-29 21:56:30 +0200

    libtorrent: remove released patch

--- pkgs/tools/networking/p2p/libtorrent/default.nix
+++ pkgs/tools/networking/p2p/libtorrent/default.nix
@@ -1,8 +1,8 @@
 # NOTE: this is rakshava's version of libtorrent, used mainly by rtorrent
 # This is NOT libtorrent-rasterbar, used by Deluge, qbitttorent, and others
-{ stdenv, fetchFromGitHub, fetchpatch, pkgconfig
-, libtool, autoconf, automake, cppunit
-, openssl, libsigcxx, zlib }:
+{ stdenv, fetchFromGitHub, pkgconfig, autoreconfHook
+, cppunit, openssl, libsigcxx, zlib
+}:
 
 stdenv.mkDerivation rec {
   pname = "libtorrent";
@@ -15,18 +15,8 @@ stdenv.mkDerivation rec {
     sha256 = "1h5y6ab3gs20yyprdfwcw8fh1c6czs4yrdj0kf54d2vp9qwz685r";
   };
 
-  patches = [
-    (fetchpatch {
-      name = "openssl_1_1.patch";
-      url = https://github.com/rakshasa/libtorrent/commit/7b29b6bd2547e72e22b9b7981df27092842d2a10.patch;
-      sha256 = "0nrh2f9bvi1p0gnsck2gzkgd5dd91yk26gcrhrj2f7wh1d4m0pdd";
-    })
-  ];
-
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ libtool autoconf automake cppunit openssl libsigcxx zlib ];
-
-  preConfigure = "./autogen.sh";
+  nativeBuildInputs = [ pkgconfig autoreconfHook ];
+  buildInputs = [ cppunit openssl libsigcxx zlib ];
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/rakshasa/libtorrent";
