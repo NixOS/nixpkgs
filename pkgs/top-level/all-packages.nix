f76d28d1e59ce65aa8c1034afa6787eed1335b24
Mario Rodas <marsam@users.noreply.github.com>
2019-07-31 04:20:00 -0500

    spotifyd: fix build

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18609,7 +18609,7 @@ in
 
   spotifyd = callPackage ../applications/audio/spotifyd {
     withALSA = stdenv.isLinux;
-    withPulseAudio = config.pulseaudio or true;
+    withPulseAudio = config.pulseaudio or false;
     withPortAudio = stdenv.isDarwin;
   };
 
