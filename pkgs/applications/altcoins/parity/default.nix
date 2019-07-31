80cf9cdf9f1983d4b40b6bbc1fbee7c9075a2f5b
xrelkd <46590321+xrelkd@users.noreply.github.com>
2019-07-31 16:07:21 +0800

    parity: 2.4.9 -> 2.5.5

--- pkgs/applications/altcoins/parity/default.nix
+++ pkgs/applications/altcoins/parity/default.nix
@@ -1,6 +1,6 @@
 let
-  version     = "2.4.9";
-  sha256      = "0pj3yyjzc3fq0r7g8j22anzqxvm377pbcy27np1g7ygkzapzb2v6";
-  cargoSha256 = "1dxn00zxivmgk4a61nxwhjlv7fjsy2ngadyw0br1ssrkgz9k7af2";
+  version     = "2.5.5";
+  sha256      = "193fnrf1pr67wblyxd5gbrg1rgflphnfaxgm3kb4iawjh18br6c6";
+  cargoSha256 = "1w9p43v76igb62mbjk2rl7fynk13l4hpz25jd4f4hk5b2y2wf3r7";
 in
   import ./parity.nix { inherit version sha256 cargoSha256; }
