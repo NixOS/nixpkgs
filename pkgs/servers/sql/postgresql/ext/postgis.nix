19011bad0c512c42f68f1ed589df36ab66cf8ef6
Luke Worth <luke@worth.id.au>
2019-08-01 08:16:59 +1000

    postgis: add explanatory comment

--- pkgs/servers/sql/postgresql/ext/postgis.nix
+++ pkgs/servers/sql/postgresql/ext/postgis.nix
@@ -46,12 +46,17 @@ stdenv.mkDerivation rec {
             " \
         "raster/scripts/python/Makefile";
     mkdir -p $out/bin
+
+    # postgis' build system assumes it is being installed to the same place as postgresql, and looks
+    # for the postgres binary relative to $PREFIX. We gently support this system using an illusion.
     ln -s ${postgresql}/bin/postgres $out/bin/postgres
   '';
 
   # create aliases for all commands adding version information
   postInstall = ''
+    # Teardown the illusory postgres used for building; see postConfigure.
     rm $out/bin/postgres
+
     for prog in $out/bin/*; do # */
       ln -s $prog $prog-${version}
     done
