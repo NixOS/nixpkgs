511c38c9f65b3a68346aadd6ef779ce96d29a214
Matthew Harm Bekkema <id@mbekkema.name>
2019-08-01 22:44:14 +1000

    ghcjs-ng: Enable Hydra builds

--- pkgs/development/compilers/ghcjs-ng/default.nix
+++ pkgs/development/compilers/ghcjs-ng/default.nix
@@ -105,5 +105,4 @@ in stdenv.mkDerivation {
 
     meta.platforms = passthru.bootPkgs.ghc.meta.platforms;
     meta.maintainers = [lib.maintainers.elvishjerricco];
-    meta.hydraPlatforms = [];
   }
