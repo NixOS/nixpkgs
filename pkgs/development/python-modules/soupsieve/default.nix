538fc5e4910b76b7036825e5707ebd161c33b566
Jonathan Ringer <jonringer117@gmail.com>
2019-08-01 15:27:44 -0700

    pythonPackages.soupsieve: 1.7.3 -> 1.9.3

--- pkgs/development/python-modules/soupsieve/default.nix
+++ pkgs/development/python-modules/soupsieve/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "soupsieve";
-  version = "1.7.3";
+  version = "1.9.3";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "87db12ae79194f0ff9808d2b1641c4f031ae39ffa3cab6b907ea7c1e5e5ed445";
+    sha256 = "14hs982wbb0rshnfiwjzda2zv6pcxcdr4bsfxjfpgn5qcqrq8ql6";
   };
 
   checkPhase = ''
