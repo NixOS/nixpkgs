6e880c201f5823213bd481140ab064d3b147a558
Jonathan Ringer <jonringer117@gmail.com>
2019-08-02 15:50:04 -0700

    pythonPackages.nbval: 0.9.1 -> 0.9.2

--- pkgs/development/python-modules/nbval/default.nix
+++ pkgs/development/python-modules/nbval/default.nix
@@ -15,19 +15,20 @@
 
 buildPythonPackage rec {
   pname = "nbval";
-  version = "0.9.1";
+  version = "0.9.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "3f18b87af4e94ccd073263dd58cd3eebabe9f5e4d6ab535b39d3af64811c7eda";
+    sha256 = "0g8xl4158ngyhiynrkk72jpawnk4isznbijz0w085g269fps0vp2";
   };
 
   LC_ALL = "en_US.UTF-8";
 
   buildInputs = [ glibcLocales ];
-  checkInputs = [ matplotlib sympy pytestcov ];
+  checkInputs = [ matplotlib sympy pytestcov pytest ];
   propagatedBuildInputs = [ coverage ipykernel jupyter_client nbformat pytest six ];
 
+  # ignore impure tests
   checkPhase = ''
     pytest tests --current-env --ignore tests/test_timeouts.py
   '';
