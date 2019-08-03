ab9294b6ed6d4c346a0664e4a2d6ba804f3ec051
Jonathan Ringer <jonringer117@gmail.com>
2019-08-03 09:30:03 -0700

    disper: 0.3.1 -> 0.3.1.1

--- pkgs/tools/misc/disper/default.nix
+++ pkgs/tools/misc/disper/default.nix
@@ -1,9 +1,19 @@
-{stdenv, fetchurl, python, xorg, makeWrapper}:
+{ stdenv, fetchFromGitHub, python, xorg, makeWrapper }:
 
 stdenv.mkDerivation rec {
-  name = "disper-0.3.1";
+  pname = "disper";
+  version = "0.3.1.1";
 
-  buildInputs = [python makeWrapper];
+  src = fetchFromGitHub {
+    owner = "apeyser";
+    repo = pname;
+    rev = "${pname}-${version}";
+    sha256 = "1kl4py26n95q0690npy5mc95cv1cyfvh6kxn8rvk62gb8scwg9zn";
+  };
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  buildInputs = [ python ];
 
   preConfigure = ''
     export makeFlags="PREFIX=$out"
@@ -13,11 +23,6 @@ stdenv.mkDerivation rec {
       wrapProgram $out/bin/disper \
         --prefix "LD_LIBRARY_PATH" : "${stdenv.lib.makeLibraryPath [ xorg.libXrandr xorg.libX11 ]}"
   '';
-
-  src = fetchurl {
-    url = http://ppa.launchpad.net/disper-dev/ppa/ubuntu/pool/main/d/disper/disper_0.3.1.tar.gz;
-    sha256 = "1l8brcpfn4iascb454ym0wrv5kqyz4f0h8k6db54nc3zhfwy7vvw";
-  };
 
   meta = {
     description = "On-the-fly display switch utility";
