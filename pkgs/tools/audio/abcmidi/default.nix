49d443efd9f941e3597752ebaef3447311bf6cb4
R. RyanTM <ryantm-bot@ryantm.com>
2019-08-03 14:41:05 -0700

    abcmidi: 2019.06.14 -> 2019.07.12
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/abcmidi/versions

--- pkgs/tools/audio/abcmidi/default.nix
+++ pkgs/tools/audio/abcmidi/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "abcMIDI-${version}";
-  version = "2019.06.14";
+  version = "2019.07.12";
 
   src = fetchzip {
     url = "https://ifdo.ca/~seymour/runabc/${name}.zip";
-    sha256 = "1z503k2j6504h4p205q7wjrvh5x9jhkvsapfz322m3r905l2vc2b";
+    sha256 = "07j9fa2wxl9dnma0lpd06cibrfxzfsxv6mjq06rhr4chwyzhdfw2";
   };
 
   # There is also a file called "makefile" which seems to be preferred by the standard build phase
