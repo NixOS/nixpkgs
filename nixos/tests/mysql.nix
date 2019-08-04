af17a97d3806646ae29a978dc7b5af04d5edfbf7
Aaron Andersen <aaron@fosslib.net>
2019-08-04 10:41:55 -0400

    nixos/mysql: test the mysql package, not just mariadb
    (cherry picked from commit f1faec8249eb74ee7a8a74e52cbffb50386cab25)

--- nixos/tests/mysql.nix
+++ nixos/tests/mysql.nix
@@ -5,20 +5,34 @@ import ./make-test.nix ({ pkgs, ...} : {
   };
 
   nodes = {
-    master =
+    mysql =
       { pkgs, ... }:
 
       {
         services.mysql.enable = true;
         services.mysql.initialDatabases = [ { name = "testdb"; schema = ./testdb.sql; } ];
-        services.mysql.package = pkgs.mysql;
+        services.mysql.package = pkgs.mysql57;
       };
+
+    mariadb =
+      { pkgs, ... }:
+
+      {
+        services.mysql.enable = true;
+        services.mysql.initialDatabases = [ { name = "testdb"; schema = ./testdb.sql; } ];
+        services.mysql.package = pkgs.mariadb;
+      };
+
   };
 
   testScript = ''
     startAll;
 
-    $master->waitForUnit("mysql");
-    $master->succeed("echo 'use testdb; select * from tests' | mysql -u root -N | grep 4");
+    $mysql->waitForUnit("mysql");
+    $mysql->succeed("echo 'use testdb; select * from tests' | mysql -u root -N | grep 4");
+    $mysql->shutdown;
+    $mariadb->waitForUnit("mysql");
+    $mariadb->succeed("echo 'use testdb; select * from tests' | mysql -u root -N | grep 4");
+    $mariadb->shutdown;
   '';
 })
