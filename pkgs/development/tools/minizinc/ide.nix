9f3dd81f86e7363999ef40496ae667fc62838b4b
R. RyanTM <ryantm-bot@ryantm.com>
2019-08-04 16:19:03 -0700

    minizincide: 2.3.0 -> 2.3.1
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/minizinc-ide/versions

--- pkgs/development/tools/minizinc/ide.nix
+++ pkgs/development/tools/minizinc/ide.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchFromGitHub, qtbase, qtwebengine, qtwebkit, qmake, makeWrapper, minizinc }:
 let
-  version = "2.3.0";
+  version = "2.3.1";
 in
 stdenv.mkDerivation {
   name = "minizinc-ide-${version}";
@@ -12,7 +12,7 @@ stdenv.mkDerivation {
     owner = "MiniZinc";
     repo = "MiniZincIDE";
     rev = version;
-    sha256 = "0458k7m97qxxylcl60qvp026kicfc4ilxwlb1p3jvjhi9384r00a";
+    sha256 = "0w9p5j2i7q4khmxyk2lr7a3qb2kd6ff1hfssxhgpm7zgzixm2300";
   };
 
   sourceRoot = "source/MiniZincIDE";
