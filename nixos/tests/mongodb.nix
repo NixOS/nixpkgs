3cf824f55c445f3bb8bfa22681344b079b84aee5
Nathan Smyth <nathan.smyth@mongodb.com>
2019-08-04 16:25:56 +1000

    test: Update MongoDB tests for new pkgs
    Now has tests for 3.4, 3.6, 4.0. Has some duplication, but it appears to
    work on my machine.

--- nixos/tests/mongodb.nix
+++ nixos/tests/mongodb.nix
@@ -12,31 +12,76 @@ in {
   };
 
   nodes = {
-    one =
-      { ... }:
-        {
-          services = {
-           mongodb.enable = true;
-           mongodb.enableAuth = true;
-           mongodb.initialRootPassword = "root";
-           mongodb.initialScript = pkgs.writeText "mongodb_initial.js" ''
-             db = db.getSiblingDB("nixtest");
-             db.createUser({user:"nixtest",pwd:"nixtest",roles:[{role:"readWrite",db:"nixtest"}]});
-           '';
-           mongodb.extraConfig = ''
-             # Allow starting engine with only a small virtual disk
-             storage.journal.enabled: false
-             storage.mmapv1.smallFiles: true
-           '';
-          };
-        };
+    v3_4 = { ... }:
+    {
+      services = {
+        mongodb.enable = true;
+        mongodb.package = pkgs.mongodb-3_4;
+        mongodb.enableAuth = true;
+        mongodb.initialRootPassword = "root";
+        mongodb.initialScript = pkgs.writeText "mongodb_initial.js" ''
+            db = db.getSiblingDB("nixtest");
+            db.createUser({user:"nixtest",pwd:"nixtest",roles:[{role:"readWrite",db:"nixtest"}]});
+        '';
+        mongodb.extraConfig = ''
+            # Allow starting engine with only a small virtual disk
+            storage.journal.enabled: false
+            storage.mmapv1.smallFiles: true
+        '';
+      };
     };
 
+    v3_6 = { ... }:
+    {
+      services = {
+        mongodb.enable = true;
+        mongodb.package = pkgs.mongodb-3_6;
+        mongodb.enableAuth = true;
+        mongodb.initialRootPassword = "root";
+        mongodb.initialScript = pkgs.writeText "mongodb_initial.js" ''
+              db = db.getSiblingDB("nixtest");
+              db.createUser({user:"nixtest",pwd:"nixtest",roles:[{role:"readWrite",db:"nixtest"}]});
+        '';
+        mongodb.extraConfig = ''
+              storage.journal.enabled: false
+        '';
+      };
+    };
+
+    v4_0 = { ... }:
+    {
+      services = {
+        mongodb.enable = true;
+        mongodb.package = pkgs.mongodb-4_0;
+        mongodb.enableAuth = true;
+        mongodb.initialRootPassword = "root";
+        mongodb.initialScript = pkgs.writeText "mongodb_initial.js" ''
+              db = db.getSiblingDB("nixtest");
+              db.createUser({user:"nixtest",pwd:"nixtest",roles:[{role:"readWrite",db:"nixtest"}]});
+        '';
+        mongodb.extraConfig = ''
+              storage.journal.enabled: false
+        '';
+      };
+    };
+
+
+  };
+
   testScript = ''
-    start_all()
-    one.wait_for_unit("mongodb.service")
-    one.succeed(
-        "mongo -u nixtest -p nixtest nixtest ${testQuery} | grep -q hello"
-    )
+         $v3_4->start;
+         $v3_4->waitForUnit("mongodb.service");
+         $v3_4->succeed("mongo -u nixtest -p nixtest nixtest ${testQuery}") =~ /hello/ or die;
+         $v3_4->shutdown;
+
+         $v3_6->start;
+         $v3_6->waitForUnit("mongodb.service");
+         $v3_6->succeed("mongo -u nixtest -p nixtest nixtest ${testQuery}") =~ /hello/ or die;
+         $v3_6->shutdown;
+
+         $v4_0->start;
+         $v4_0->waitForUnit("mongodb.service");
+         $v4_0->succeed("mongo -u nixtest -p nixtest nixtest ${testQuery}") =~ /hello/ or die;
+         $v4_0->shutdown;
   '';
 })
