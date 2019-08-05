40934322f83ee068c3ed444e82e7ecf5d25d6b1a
R. RyanTM <ryantm-bot@ryantm.com>
2019-08-04 18:05:41 -0700

    nagios: 4.4.3 -> 4.4.4
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/nagios/versions

--- pkgs/servers/monitoring/nagios/default.nix
+++ pkgs/servers/monitoring/nagios/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "nagios-${version}";
-  version = "4.4.3";
+  version = "4.4.4";
 
   src = fetchurl {
     url = "mirror://sourceforge/nagios/nagios-4.x/${name}/${name}.tar.gz";
-    sha256 = "0rwzlj6qp8hq931rw6s255b33ggmc2fcxs74g9x2zxwcvklg1a5v";
+    sha256 = "0nkbv8lzpiknddiq0466dlpp3hw8lqmaidk8931hp4664cdsaw57";
   };
 
   patches = [ ./nagios.patch ];
