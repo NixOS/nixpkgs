09781e81c315cc4e9e079c5c96e3d0714fb7bcbd
timor <timor.dd@googlemail.com>
2019-08-05 20:12:36 +0200

    udis86: init at 1.7.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9916,6 +9916,8 @@ in
     inherit (darwin.apple_sdk.frameworks) CoreFoundation;
   };
 
+  udis86 = callPackage  ../development/tools/udis86 { };
+
   uefi-firmware-parser = callPackage ../development/tools/analysis/uefi-firmware-parser { };
 
   uhd = callPackage ../applications/radio/uhd { };
