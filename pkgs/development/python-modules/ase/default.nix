15627bd8e6e062e27714688bf4e7e0c7bdfa7bb6
R. RyanTM <ryantm-bot@ryantm.com>
2019-08-04 23:53:31 -0700

    python37Packages.ase: 3.17.0 -> 3.18.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-ase/versions

--- pkgs/development/python-modules/ase/default.nix
+++ pkgs/development/python-modules/ase/default.nix
@@ -11,11 +11,11 @@
 
 buildPythonPackage rec {
   pname = "ase";
-  version = "3.17.0";
+  version = "3.18.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1d4gxypaahby45zcpl0rffcn2z7n55dg9lcd8sv6jjsmbbf9vr4g";
+    sha256 = "1ycp1yksysiiz902gn762030sfmirxm950pwpw2rcrpjvq95zm1r";
   };
 
   propagatedBuildInputs = [ numpy scipy matplotlib flask pillow psycopg2 ];
