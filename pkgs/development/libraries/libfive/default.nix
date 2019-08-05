7b470c1ccffa58bd9630ef2933427d797ab82342
Chris Hodapp <hodapp87@gmail.com>
2019-08-05 18:10:14 -0400

    libfive: 2018-07-01 -> 2019-08-05

--- pkgs/development/libraries/libfive/default.nix
+++ pkgs/development/libraries/libfive/default.nix
@@ -4,13 +4,13 @@ zlib, libpng, boost, qt5, guile
 
 stdenv.mkDerivation rec {
   name = "libfive-${version}";
-  version = "2018-07-01";
+  version = "2019-08-05";
 
   src = fetchFromGitHub {
     owner  = "libfive";
     repo   = "libfive";
-    rev    = "0f517dde9521d751310a22f85ee69b2c84690267";
-    sha256 = "0bfxysf5f4ripgcv546il8wnw5p0d4s75kdjlwvj32549537hlz0";
+    rev    = "d0e6401";
+    sha256 = "0f8cyc04gjqlzs71cm2l1j6lf4kkksf0sqgz9jfpfsvdvs3q3n6w";
   };
   nativeBuildInputs = [ cmake ninja pkgconfig ];
   buildInputs = [ eigen zlib libpng boost qt5.qtimageformats guile ];
