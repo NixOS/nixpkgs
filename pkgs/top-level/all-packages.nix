49e61479d4a5b7e091f55af9f63ca79ddf9b0227
Peter Hoeg <peter@hoeg.com>
2019-08-05 17:39:40 +0800

    mtr: build a -gui option as well

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4781,6 +4781,8 @@ in
 
   mtr = callPackage ../tools/networking/mtr {};
 
+  mtr-gui = callPackage ../tools/networking/mtr { withGtk = true; };
+
   mtx = callPackage ../tools/backup/mtx {};
 
   mt-st = callPackage ../tools/backup/mt-st {};
