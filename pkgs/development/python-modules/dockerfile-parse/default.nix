91ad920b2ad312e1f20b8b44033ca0800aaeb7c9
R. RyanTM <ryantm-bot@ryantm.com>
2019-08-05 03:07:58 -0700

    python37Packages.dockerfile-parse: 0.0.14 -> 0.0.15
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-dockerfile-parse/versions

--- pkgs/development/python-modules/dockerfile-parse/default.nix
+++ pkgs/development/python-modules/dockerfile-parse/default.nix
@@ -1,12 +1,12 @@
 { stdenv, buildPythonPackage, fetchPypi, six, pytestcov, pytest }:
 
 buildPythonPackage rec {
-  version = "0.0.14";
+  version = "0.0.15";
   pname = "dockerfile-parse";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0b829a9e25ac9af17a0affa41c0fca6541a03b8edb0178f60dc036e2ce59eeb5";
+    sha256 = "1s05s1hc834yk6qxj2yv3fh7grj3q52d6jjy0sv1p05938baprfm";
   };
 
   postPatch = ''
