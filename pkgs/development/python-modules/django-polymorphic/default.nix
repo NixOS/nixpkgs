c9f6f77ccf6b87c50676b25b09e1de1f541a875b
R. RyanTM <ryantm-bot@ryantm.com>
2019-08-05 02:14:38 -0700

    python37Packages.django_polymorphic: 2.0.3 -> 2.1.2
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-django-polymorphic/versions

--- pkgs/development/python-modules/django-polymorphic/default.nix
+++ pkgs/development/python-modules/django-polymorphic/default.nix
@@ -2,14 +2,14 @@
 
 buildPythonPackage rec {
   pname = "django-polymorphic";
-  version = "2.0.3";
+  version = "2.1.2";
 
   # PyPI tarball is missing some test files
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "08qk3rbk0xlphwalkigbhqpmfaqjk1sxmlfh8zy8s8dw7fw1myk4";
+    sha256 = "0zghrq7y7g2ls38cz6y98qj5xwnn992slhb95qyp6l66d420j179";
   };
 
   checkInputs = [ dj-database-url ];
