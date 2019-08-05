4640287f318727f5f864226e5c21d22f453ed554
toon <toon@c3d2.de>
2019-08-05 18:00:24 +0200

    profanity: Add optional omemo support (enabled by default)

--- pkgs/applications/networking/instant-messengers/profanity/default.nix
+++ pkgs/applications/networking/instant-messengers/profanity/default.nix
@@ -8,6 +8,7 @@
 , traySupport ? true,           gnome2 ? null
 , pgpSupport ? true,            gpgme ? null
 , pythonPluginSupport ? true,   python ? null
+, omemoSupport ? true,          libsignal-protocol-c ? null, libgcrypt ? null
 }:
 
 assert autoAwaySupport     -> libXScrnSaver != null && libX11 != null;
@@ -15,6 +16,7 @@ assert notifySupport       -> libnotify != null && gdk-pixbuf != null;
 assert traySupport         -> gnome2 != null;
 assert pgpSupport          -> gpgme != null;
 assert pythonPluginSupport -> python != null;
+assert omemoSupport        -> libsignal-protocol-c != null && libgcrypt != null;
 
 with stdenv.lib;
 
@@ -44,14 +46,16 @@ stdenv.mkDerivation rec {
     ++ optionals notifySupport       [ libnotify gdk-pixbuf ]
     ++ optionals traySupport         [ gnome2.gtk ]
     ++ optionals pgpSupport          [ gpgme ]
-    ++ optionals pythonPluginSupport [ python ];
+    ++ optionals pythonPluginSupport [ python ]
+    ++ optionals omemoSupport        [ libsignal-protocol-c libgcrypt ];
 
   # Enable feature flags, so that build fail if libs are missing
   configureFlags = [ "--enable-c-plugins" "--enable-otr" ]
     ++ optionals notifySupport       [ "--enable-notifications" ]
     ++ optionals traySupport         [ "--enable-icons" ]
     ++ optionals pgpSupport          [ "--enable-pgp" ]
-    ++ optionals pythonPluginSupport [ "--enable-python-plugins" ];
+    ++ optionals pythonPluginSupport [ "--enable-python-plugins" ]
+    ++ optionals omemoSupport        [ "--enable-omemo" ];
 
   preAutoreconf = ''
     mkdir m4
