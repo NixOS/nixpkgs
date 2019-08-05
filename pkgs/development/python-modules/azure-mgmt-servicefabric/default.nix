2b54c29451775a50ad17fb3e2068e43f148c9997
R. RyanTM <ryantm-bot@ryantm.com>
2019-08-05 02:57:52 -0700

    python37Packages.azure-mgmt-servicefabric: 0.2.0 -> 0.3.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-azure-mgmt-servicefabric/versions

--- pkgs/development/python-modules/azure-mgmt-servicefabric/default.nix
+++ pkgs/development/python-modules/azure-mgmt-servicefabric/default.nix
@@ -9,12 +9,12 @@
 
 buildPythonPackage rec {
   pname = "azure-mgmt-servicefabric";
-  version = "0.2.0";
+  version = "0.3.0";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "b2bf2279b8ff8450c35e78e226231655021482fdbda27db09975ebfc983398ad";
+    sha256 = "0cirsp8wnsswba6gbmw4s2ljsjwi3855my063gvi2mqr55spvx2n";
   };
 
   propagatedBuildInputs = [
