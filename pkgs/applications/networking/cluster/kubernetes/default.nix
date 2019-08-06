e6b09af8e2bfb9c4686d3fcc429b960e38052fdd
Johan Thomsen <jth@dbc.dk>
2019-08-06 22:39:07 +0200

    kubernetes: 1.13.7 -> 1.13.9

--- pkgs/applications/networking/cluster/kubernetes/default.nix
+++ pkgs/applications/networking/cluster/kubernetes/default.nix
@@ -15,13 +15,13 @@ with lib;
 
 stdenv.mkDerivation rec {
   name = "kubernetes-${version}";
-  version = "1.13.7";
+  version = "1.13.9";
 
   src = fetchFromGitHub {
     owner = "kubernetes";
     repo = "kubernetes";
     rev = "v${version}";
-    sha256 = "013bbnhxzv41hqnz98k5kyrvaxz39gk9ay7sw996sy3v60vwc3z3";
+    sha256 = "0dnwxd38ixl5czv2g1qix3gfxmyh2ig0zbalgvc3lq9c57wd0yg8";
   };
 
   buildInputs = [ removeReferencesTo makeWrapper which go rsync go-bindata ];
