00a4b9d3390defe455ed311234012c2b89180793
Jonathan Ringer <jonringer117@gmail.com>
2019-08-05 19:06:12 -0700

    pythonPackages.bid-validator: init at 1.2.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1482,6 +1482,8 @@ in {
 
   bidict = callPackage ../development/python-modules/bidict { };
 
+  bids-validator = callPackage ../development/python-modules/bids-validator { };
+
   binwalk = callPackage ../development/python-modules/binwalk { };
 
   binwalk-full = appendToName "full" (self.binwalk.override {
