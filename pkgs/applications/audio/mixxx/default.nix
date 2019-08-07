9e36f272f90eb7b0e423559373138a09fbbeb626
worldofpeace <worldofpeace@protonmail.ch>
2019-08-07 12:52:43 -0400

    mixxx: drop custom wrapper
    This prevented it from being wrapped
    automatically because it wasn't an ELF
    header.

--- pkgs/applications/audio/mixxx/default.nix
+++ pkgs/applications/audio/mixxx/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, mkDerivation, fetchFromGitHub, makeWrapper, chromaprint
+{ stdenv, mkDerivation, fetchFromGitHub, chromaprint
 , fftw, flac, faad2, glibcLocales, mp4v2
 , libid3tag, libmad, libopus, libshout, libsndfile, libusb1, libvorbis
 , libGLU, libxcb, lilv, lv2, opusfile
@@ -17,8 +17,6 @@ mkDerivation rec {
     sha256 = "1q6c2wfpprsx7s7nz1w0mhm2yhikj54jxcv61kwylxx3n5k2na9r";
   };
 
-  nativeBuildInputs = [ makeWrapper ];
-
   buildInputs = [
     chromaprint fftw flac faad2 glibcLocales mp4v2 libid3tag libmad libopus libshout libsndfile
     libusb1 libvorbis libxcb libGLU lilv lv2 opusfile pkgconfig portaudio portmidi protobuf qtbase qtscript qtsvg
@@ -34,10 +32,9 @@ mkDerivation rec {
     "opus=1"
   ];
 
-  fixupPhase = ''
-    wrapProgram $out/bin/mixxx \
-      --set LOCALE_ARCHIVE ${glibcLocales}/lib/locale/locale-archive;
-  '';
+  qtWrapperArgs = [
+    "--set LOCALE_ARCHIVE ${glibcLocales}/lib/locale/locale-archive"
+  ];
 
   meta = with stdenv.lib; {
     homepage = https://mixxx.org;
