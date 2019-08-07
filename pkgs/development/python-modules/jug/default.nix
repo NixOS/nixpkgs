034a97ecc1aaa7f75829253c42d14dd13be9cb2f
Luis Pedro Coelho <luis@luispedro.org>
2019-08-07 11:51:52 +0200

    pythonPackages.jug: 1.6.8 -> 1.6.9
    Removes the need to patch the code for NumPy 1.17.0

--- pkgs/development/python-modules/jug/default.nix
+++ pkgs/development/python-modules/jug/default.nix
@@ -5,7 +5,7 @@
 
 buildPythonPackage rec {
   pname = "Jug";
-  version = "1.6.8";
+  version = "1.6.9";
   buildInputs = [ nose numpy ];
   propagatedBuildInputs = [
     bottle
@@ -16,18 +16,9 @@ buildPythonPackage rec {
     zlib
   ];
 
-  patches = [
-    # Fix numpy usage. Remove with the next release
-    (fetchpatch {
-      url = "https://github.com/luispedro/jug/commit/814405ce1553d3d2e2e96cfbeae05d63dc4f2491.patch";
-      sha256 = "1l8sssp856dmhxbnv3pzxgwgpv6rb884l0in5x7q19czwn5a4vmv";
-      excludes = [ "ChangeLog" ];
-    })
-  ];
-
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0s1l1ln9s6mi2aa132gqr789nnhdpiw057j3sp54v1sbq2cwd42p";
+    sha256 = "0193hp8ap6caw57jdch3vw0hl5m8942lxhjdsfaxk4bfb239l5kz";
   };
 
   meta = with stdenv.lib; {
