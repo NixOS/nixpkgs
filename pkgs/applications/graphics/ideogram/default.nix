c5a6c386832969b4be0083995db832a4151ba09e
worldofpeace <worldofpeace@protonmail.ch>
2019-08-07 12:22:55 -0400

    ideogram: fix cassidyjames/ideogram#26

--- pkgs/applications/graphics/ideogram/default.nix
+++ pkgs/applications/graphics/ideogram/default.nix
@@ -1,5 +1,6 @@
 { stdenv
 , fetchFromGitHub
+, fetchpatch
 , pkgconfig
 , python3
 , glib
@@ -41,6 +42,14 @@ stdenv.mkDerivation rec {
     pantheon.granite
     xorg.libX11
     xorg.libXtst
+  ];
+
+  patches = [
+    # See: https://github.com/cassidyjames/ideogram/issues/26
+    (fetchpatch {
+      url = "https://github.com/cassidyjames/ideogram/commit/65994ee11bd21f8316b057cec01afbf50639a708.patch";
+      sha256 = "12vrvvggpqq53dmhbm7gbbbigncn19m1fjln9wxaady21m0w776c";
+    })
   ];
 
   postPatch = ''
