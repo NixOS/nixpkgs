636cef2f03739b34c8e95a4b53138e6e43937e00
Tim Steinbach <tim@nequissimus.com>
2019-08-07 07:24:50 -0400

    linux: 4.14.136 -> 4.14.137

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.14.136";
+  version = "4.14.137";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0w6z5fhwqgpqnz2js8vj9j5dl6isx8n7rnzrm0vr9r8njaazz396";
+    sha256 = "0a72pab0zxy28i02glnzj6avzcf0a4gxxnadbdd343rh549yky4k";
   };
 } // (args.argsOverride or {}))
