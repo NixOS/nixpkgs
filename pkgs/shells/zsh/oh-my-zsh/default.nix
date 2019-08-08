cacc355f617beb7de6c92f8533fd513c9ee4a257
Tim Steinbach <tim@nequissimus.com>
2019-08-08 09:59:47 -0400

    oh-my-zsh: 2019-08-06 -> 2019-08-07

--- pkgs/shells/zsh/oh-my-zsh/default.nix
+++ pkgs/shells/zsh/oh-my-zsh/default.nix
@@ -4,13 +4,13 @@
 { stdenv, fetchgit }:
 
 stdenv.mkDerivation rec {
-  version = "2019-08-06";
+  version = "2019-08-07";
   name = "oh-my-zsh-${version}";
-  rev = "89366be43fd6ceff52a9da2f2edef654ab619514";
+  rev = "40fafe0f59371d1a9d83b83c614acfd1d740aabb";
 
   src = fetchgit { inherit rev;
     url = "https://github.com/robbyrussell/oh-my-zsh";
-    sha256 = "0adql1x28q7i6cg103jfp7wqajq1smwdgbw1cxrsa7qh8ys7kss0";
+    sha256 = "0vk78dkrgbj51jvbpn337d7dnsb3p7cdj4bk92m6xqby5lmk4q01";
   };
 
   pathsToLink = [ "/share/oh-my-zsh" ];
