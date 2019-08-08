170caa4506fe40ebbbc47be62934161d2d6c0bb7
Manuel Mendez <mmendez534@gmail.com>
2019-08-08 09:03:41 -0400

    flent: 1.2.2 -> 1.3.0

--- pkgs/applications/networking/flent/default.nix
+++ pkgs/applications/networking/flent/default.nix
@@ -1,20 +1,29 @@
-{ stdenv, buildPythonApplication, fetchPypi, matplotlib, procps, pyqt5, qt5
-, sphinx }:
+{ stdenv, buildPythonApplication, fetchPypi, matplotlib, procps, pyqt5, python
+, pythonPackages, qt5, sphinx, xvfb_run }:
 
 buildPythonApplication rec {
   pname = "flent";
-  version = "1.2.2";
+  version = "1.3.0";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0ziblk36rzr99pbi7xzzkci3sr41m0jf72v38ynp63df6szbbfjb";
+    sha256 = "099779i0ghjd9ikq77z6m6scnlmk946lw9issrgz8zm7babiw4d7";
   };
 
   buildInputs = [ sphinx ];
   nativeBuildInputs = [ qt5.wrapQtAppsHook ];
-
-  checkInputs = [ procps ];
-
   propagatedBuildInputs = [ matplotlib procps pyqt5 ];
+  checkInputs = [ procps pythonPackages.mock pyqt5 xvfb_run ];
+
+  checkPhase = ''
+    cat >test-runner <<EOF
+    #!/bin/sh
+
+    ${python.pythonForBuild.interpreter} nix_run_setup test
+    EOF
+    chmod +x test-runner
+    wrapQtApp test-runner --prefix PYTHONPATH : $PYTHONPATH
+    xvfb-run -s '-screen 0 800x600x24' ./test-runner
+  '';
 
   postInstall = ''
     for program in $out/bin/*; do
