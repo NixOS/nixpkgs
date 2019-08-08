30cf263e41b0dfd8597c2f9e36ab143e3f48910e
Jake Waksbaum <jake.waksbaum@gmail.com>
2019-08-07 21:05:53 -0700

    pythonPackages.jsonschema: 2.6.0 -> 3.0.2

--- pkgs/development/python-modules/jsonschema/default.nix
+++ pkgs/development/python-modules/jsonschema/default.nix
@@ -1,25 +1,30 @@
-{ stdenv, buildPythonPackage, fetchPypi, python
-, nose, mock, vcversioner, functools32 }:
+{ stdenv, buildPythonPackage, fetchPypi, python, pythonOlder
+, attrs, pyrsistent, six, functools32, twisted }:
 
 buildPythonPackage rec {
   pname = "jsonschema";
-  version = "2.6.0";
+  version = "3.0.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "00kf3zmpp9ya4sydffpifn0j0mzm342a2vzh82p6r0vh10cg7xbg";
+    sha256 = "8d4a2b7b6c2237e0199c8ea1a6d3e05bf118e289ae2b9d7ba444182a2959560d";
   };
 
-  checkInputs = [ nose mock vcversioner ];
-  propagatedBuildInputs = [ functools32 ];
-
-  postPatch = ''
-    substituteInPlace jsonschema/tests/test_jsonschema_test_suite.py \
-      --replace "python" "${python.pythonForBuild.interpreter}"
-  '';
+  checkInputs = [ twisted ];
+  propagatedBuildInputs =
+    [ attrs pyrsistent six ]
+    ++ stdenv.lib.optional (pythonOlder "3") functools32;
 
   checkPhase = ''
-    nosetests
+    # jsonschema.tests.test_cli fails, and I don't know how to skip tests with
+    # twisted. So we list all the other ones.
+    python -m twisted.trial \
+      jsonschema.tests.test_exceptions \
+      jsonschema.tests.test_format \
+      jsonschema.tests.test_jsonschema_test_suite \
+      jsonschema.tests.test_types \
+      jsonschema.tests.test_validators
+    python -m doctest README.rst
   '';
 
   meta = with stdenv.lib; {
