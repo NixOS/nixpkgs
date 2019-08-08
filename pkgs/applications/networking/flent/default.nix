a88232c31ae733a5bc148175ea20549cc81e23b2
Manuel Mendez <mmendez534@gmail.com>
2019-08-08 08:51:44 -0400

    flent: use fetchPypi

--- pkgs/applications/networking/flent/default.nix
+++ pkgs/applications/networking/flent/default.nix
@@ -1,14 +1,12 @@
-{ stdenv, buildPythonApplication, fetchFromGitHub, matplotlib, procps, pyqt5
+{ stdenv, buildPythonApplication, fetchPypi, matplotlib, procps, pyqt5
 , sphinx }:
 
 buildPythonApplication rec {
   pname = "flent";
   version = "1.2.2";
-  src = fetchFromGitHub {
-    owner = "tohojo";
-    repo = "flent";
-    rev = "v${version}";
-    sha256 = "1llcdakk0nk9xlpjjz7mv4a80yq4sjnbqhaqvyj9m6lbcxgssh2r";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "0ziblk36rzr99pbi7xzzkci3sr41m0jf72v38ynp63df6szbbfjb";
   };
 
   buildInputs = [ sphinx ];
