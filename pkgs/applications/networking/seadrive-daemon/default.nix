dab4d9abb9e718a093de612a5c86cc5c1b4d5a28
Justin Lovinger <git@justinlovinger.com>
2019-08-09 20:04:03 -0400

    seadrive-daemon: include libevent_2_0 dependency
    Instead of pulling from github
    To avoid an additional network operation and to avoid relying on a very
    specific git commit

--- pkgs/applications/networking/seadrive-daemon/default.nix
+++ pkgs/applications/networking/seadrive-daemon/default.nix
@@ -13,10 +13,7 @@
 let
   deblibcurl = callPackage ./lib/deblibcurl {};
   deblibssl = callPackage ./lib/deblibssl {};
-  libevent_2_0 = callPackage (fetchurl {
-    url = "https://github.com/NixOS/nixpkgs/raw/3be19de8e4eba036d1c6ae81295beee1871a7fef~1/pkgs/development/libraries/libevent/default.nix";
-    sha256 = "07x7i3bsm8xngap2baivbza0fnj3jalmfq57avymzfyqy17sb2j4";
-  }) {};
+  libevent_2_0 = callPackage ./lib/libevent_2_0 {};
 in stdenv.mkDerivation rec {
   version = "1.0.6";
   pname = "seadrive-daemon";
