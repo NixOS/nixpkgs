28d637d345dd1e0344d9dd6de7e43e2e24fbf412
Michael Weiss <dev.primeos@gmail.com>
2019-08-10 14:04:40 +0200

    ffmpeg_4: 4.1.4 -> 4.2
    Changelog: https://git.ffmpeg.org/gitweb/ffmpeg.git/blob/refs/heads/release/4.2:/Changelog

--- pkgs/development/libraries/ffmpeg/4.nix
+++ pkgs/development/libraries/ffmpeg/4.nix
@@ -6,12 +6,7 @@
 
 callPackage ./generic.nix (args // rec {
   version = "${branch}";
-  branch = "4.1.4";
-  sha256 = "01w44ygm5bvc243hlhfnvb2lxfb0blz2cxnphxqgw30vj3c1prx7";
-  patches = [(fetchpatch { # remove on update
-    name = "fix-hardcoded-tables.diff";
-    url = "http://git.ffmpeg.org/gitweb/ffmpeg.git/commitdiff_plain/c8232e50074f";
-    sha256 = "0jlksks4fjajby8fjk7rfp414gxfdgd6q9khq26i52xvf4kg2dw6";
-  })];
+  branch = "4.2";
+  sha256 = "1f3glany3p2j832a9wia5vj8ds9xpm0xxlyia91y17hy85gxwsrh";
   darwinFrameworks = [ Cocoa CoreMedia VideoToolbox ];
 })
