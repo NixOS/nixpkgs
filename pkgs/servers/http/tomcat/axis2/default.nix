0e679dd53e1aa60e61dc25db5e0a0ea882bb7416
Marek Mahut <marek.mahut@gmail.com>
2019-08-10 10:24:53 +0200

    axis2: 1.7.8 -> 1.7.9

--- pkgs/servers/http/tomcat/axis2/default.nix
+++ pkgs/servers/http/tomcat/axis2/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "axis2-${version}";
-  version = "1.7.8";
+  version = "1.7.9";
 
   src = fetchurl {
     url = "http://apache.proserve.nl/axis/axis2/java/core/${version}/${name}-bin.zip";
-    sha256 = "0k1ppxb9v6agx4gcfkpgwcri6v29ng3arjyc0b0mxcby6ck85mny";
+    sha256 = "0dh0s9bfh95wmmw8nyf2yw95biq7d9zmrbg8k4vzcyz1if228lac";
   };
 
   buildInputs = [ unzip apacheAnt jdk ];
