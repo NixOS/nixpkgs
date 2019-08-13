bd968b4facdafaa611e1c2ef5b40c57605429e4d
Timothy DeHerrera <tim.deh@pm.me>
2019-08-13 00:07:02 -0600

    vimPlugins.delimitMate: init at 2017-06-18

--- pkgs/misc/vim-plugins/generated.nix
+++ pkgs/misc/vim-plugins/generated.nix
@@ -764,6 +764,17 @@ let
     };
   };
 
+  delimitMate = buildVimPluginFrom2Nix {
+    pname = "delimitMate";
+    version = "2017-06-18";
+    src = fetchFromGitHub {
+      owner = "Raimondi";
+      repo = "delimitMate";
+      rev = "728b57a6564c1d2bdfb9b9e0f2f8c5ba3d7e0c5c";
+      sha256 = "0fskm9gz81dk8arcidrm71mv72a7isng1clssqkqn5wnygbiimsn";
+    };
+  };
+
   denite-extra = buildVimPluginFrom2Nix {
     pname = "denite-extra";
     version = "2019-03-29";
