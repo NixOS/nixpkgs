9825b2ac46b2dc4de7f6c81551c980e3ec76db5c
nyanloutre <paul@nyanlout.re>
2019-08-13 20:24:55 +0200

    kate: add patch "add missing header"

--- pkgs/applications/kde/kate.nix
+++ pkgs/applications/kde/kate.nix
@@ -1,5 +1,5 @@
 {
-  mkDerivation, lib,
+  mkDerivation, lib, fetchpatch,
   extra-cmake-modules, kdoctools,
   kactivities, kconfig, kcrash, kdbusaddons, kguiaddons, kiconthemes, ki18n,
   kinit, kio, kitemmodels, kjobwidgets, knewstuff, knotifications, konsole,
@@ -13,6 +13,15 @@ mkDerivation {
     license = with lib.licenses; [ gpl3 lgpl3 lgpl2 ];
     maintainers = [ lib.maintainers.ttuegel ];
   };
+
+  patches = [
+    # This patch should be backported in 19.04.4 KDE applications
+    (fetchpatch {
+      url = "https://cgit.kde.org/kate.git/patch/?id=76ec8b55a86a29a90125b2ff3f512df007789cb1";
+      sha256 = "1q0bkb6vl4xvh4aba1rlqii4a75pvl7vbcs4plny8lalzslnbzhj";
+    })
+  ];
+
   nativeBuildInputs = [ extra-cmake-modules kdoctools ];
   buildInputs = [ libgit2 ];
   propagatedBuildInputs = [
