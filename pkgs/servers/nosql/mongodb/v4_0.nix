47bd120610e93eee713a036b85508986e80ea9c2
Nathan Smyth <nathan.smyth@mongodb.com>
2019-08-14 22:13:10 +1000

    fix: Add mozjs-45 patch for v4_0

--- pkgs/servers/nosql/mongodb/v4_0.nix
+++ pkgs/servers/nosql/mongodb/v4_0.nix
@@ -8,5 +8,6 @@ in
     sha256 = "0kry8kzzpah0l7j8xa333y1ixwvarc28ip3f6lx5590yy11j8ry2";
     patches = [
       ./forget-build-dependencies.patch
+      ./mozjs-45_fix-3-byte-opcode.patch
     ];
   }
