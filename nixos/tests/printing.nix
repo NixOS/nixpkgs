01cd4663d67b061e9ad580e0e2c689d093b8ae10
Matthew Bauer <mjbauer95@gmail.com>
2019-08-14 11:43:46 -0400

    tests/printing: don’t wait for unit services
    These are now socket activated, we don’t need it to start up front.

--- nixos/tests/printing.nix
+++ nixos/tests/printing.nix
@@ -34,10 +34,6 @@ import ./make-test.nix ({pkgs, ... }: {
     ''
       startAll;
 
-      # Make sure that cups is up on both sides.
-      $server->waitForUnit("cups.service");
-      $client->waitForUnit("cups.service");
-      $client->sleep(10); # wait until cups is fully initialized
       $client->succeed("lpstat -r") =~ /scheduler is running/ or die;
       # check local encrypted connections work without error
       $client->succeed("lpstat -E -r") =~ /scheduler is running/ or die;
