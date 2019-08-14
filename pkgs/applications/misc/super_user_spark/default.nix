e72f59e34b7e3930581bc67ab2cd3be7bf84b455
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-08-14 11:09:30 +0200

    super-user-spark 0.3.2.0-dev -> 0.4.0.1

--- pkgs/applications/misc/super_user_spark/default.nix
+++ pkgs/applications/misc/super_user_spark/default.nix
@@ -1,32 +1,30 @@
-{ mkDerivation, fetchgit, aeson, aeson-pretty, base, bytestring, directory
-, filepath, hspec, hspec-core, HUnit, mtl, optparse-applicative
-, parsec, process, pureMD5, QuickCheck, shelly, stdenv, text
-, transformers, unix
+{ mkDerivation, aeson, aeson-pretty, base, bytestring, directory
+, filepath, genvalidity, genvalidity-hspec, genvalidity-hspec-aeson
+, genvalidity-path, hashable, hspec, hspec-core, mtl
+, optparse-applicative, parsec, path, path-io, process, QuickCheck
+, stdenv, text, transformers, unix, validity, validity-path
 }:
 mkDerivation {
   pname = "super-user-spark";
-  version = "0.3.2.0-dev";
-  src = fetchgit {
-    url = "https://github.com/NorfairKing/super-user-spark";
-    sha256 = "0akyc51bghzkk8j75n0i8v8rrsklidwvljhx3aibxfbkqp33372g";
-    rev = "ab8635682d67842b9e6d909cf3c618014e4157f2";
-  };
+  version = "0.4.0.1";
+  sha256 = "0pxkvc1vjarh4p5rqnai6nlsqxv9as8jvqs2vpywl1525nsfyvy5";
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson aeson-pretty base bytestring directory filepath mtl
-    optparse-applicative parsec process pureMD5 shelly text
-    transformers unix
+    aeson aeson-pretty base bytestring directory filepath hashable mtl
+    optparse-applicative parsec path path-io process text unix validity
+    validity-path
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
-    aeson aeson-pretty base bytestring directory filepath hspec
-    hspec-core HUnit mtl optparse-applicative parsec process pureMD5
-    QuickCheck shelly text transformers unix
+    aeson aeson-pretty base bytestring directory filepath genvalidity
+    genvalidity-hspec genvalidity-hspec-aeson genvalidity-path hashable
+    hspec hspec-core mtl optparse-applicative parsec path path-io
+    process QuickCheck text transformers unix validity validity-path
   ];
-  jailbreak = true;
   description = "Configure your dotfile deployment with a DSL";
   license = stdenv.lib.licenses.mit;
+  jailbreak = true;
   homepage = https://github.com/NorfairKing/super-user-spark;
   maintainers = [ stdenv.lib.maintainers.badi ];
 }
