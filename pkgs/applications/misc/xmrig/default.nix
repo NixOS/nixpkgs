bfad16908b6e852ef25623842b4ef9041d90882c
Artem Vorotnikov <artem@vorotnikov.me>
2019-08-15 00:14:46 +0300

    xmrig{,-proxy}: 2.14.4 -> 3.0.0, add build options customization

--- pkgs/applications/misc/xmrig/default.nix
+++ pkgs/applications/misc/xmrig/default.nix
@@ -1,20 +1,36 @@
-{ stdenv, lib, fetchFromGitHub, cmake, libuv, libmicrohttpd, openssl
+{ stdenv, lib, fetchFromGitHub, cmake, hwloc, libuv, libmicrohttpd, openssl
 , donateLevel ? 0
+, http ? false
+, tls ? true
+, algo ? ["cryptonight-lite" "cryptonight-heavy" "cryptonight-pico" "cryptonight-gpu" "randomx"]
 }:
 
 stdenv.mkDerivation rec {
   name = "xmrig-${version}";
-  version = "2.14.4";
+  version = "3.0.0";
 
   src = fetchFromGitHub {
     owner = "xmrig";
     repo = "xmrig";
     rev = "v${version}";
-    sha256 = "0hmla9ys9gmmcrmkidw05ygiz734rs7skb9w86jnwxi5nlpxqrhj";
+    sha256 = "1m0rsjb7y1j77mzg5cqb3fdvzgvjkrwgmkjn9nv1xl2757z8hcl4";
   };
 
+  cmakeFlags = []
+    ++ lib.optional (!http) "-DWITH_HTTP=OFF"
+    ++ lib.optional (!tls) "-DWITH_TLS=OFF"
+    ++ lib.optional ((lib.count(a: a == "cryptonight-lite")  algo) == 0) "-DWITH_CN_LITE=OFF"
+    ++ lib.optional ((lib.count(a: a == "cryptonight-heavy") algo) == 0) "-DWITH_CN_HEAVY=OFF"
+    ++ lib.optional ((lib.count(a: a == "cryptonight-pico")  algo) == 0) "-DWITH_CN_PICO=OFF"
+    ++ lib.optional ((lib.count(a: a == "cryptonight-gpu")   algo) == 0) "-DWITH_CN_GPU=OFF"
+    ++ lib.optional ((lib.count(a: a == "randomx")           algo) == 0) "-DWITH_RANDOMX=OFF"
+  ;
+
   nativeBuildInputs = [ cmake ];
-  buildInputs = [ libuv libmicrohttpd openssl ];
+  buildInputs = [ hwloc libuv openssl ]
+    ++ lib.optional http libmicrohttpd
+    ++ lib.optional tls openssl
+  ;
 
   postPatch = ''
     substituteInPlace src/donate.h \
--- pkgs/applications/misc/xmrig/proxy.nix
+++ pkgs/applications/misc/xmrig/proxy.nix
@@ -1,20 +1,27 @@
 { stdenv, lib, fetchFromGitHub, cmake, libuv, libmicrohttpd, libuuid, openssl
+, tls ? true
 , donateLevel ? 0
 }:
 
 stdenv.mkDerivation rec {
   name = "xmrig-proxy-${version}";
-  version = "2.14.4";
+  version = "3.0.0";
 
   src = fetchFromGitHub {
     owner = "xmrig";
     repo = "xmrig-proxy";
     rev = "v${version}";
-    sha256 = "11q4w5ncyyf2vgrb64zlr01n3adyaxja2pcqm5id9if0nvwwn4ga";
+    sha256 = "19rv5zmxwr3kmb2m3fas91aq1493cnkhvs88zcflnijr1ra218ks";
   };
 
+  cmakeFlags = []
+    ++ lib.optional (!tls) "-DWITH_TLS=OFF"
+  ;
+
   nativeBuildInputs = [ cmake ];
-  buildInputs = [ libuv libmicrohttpd libuuid openssl ];
+  buildInputs = [ libuv libmicrohttpd libuuid ]
+    ++ lib.optional tls openssl
+  ;
 
   postPatch = ''
     # Set default donation level to 0%. Can be increased at runtime via --donate-level option.
