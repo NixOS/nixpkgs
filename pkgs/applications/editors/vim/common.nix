dff589161b1563491a9869ccfa94073bc2319ead
William Kral <william.kral@gmail.com>
2019-08-14 00:01:25 -0700

    Upgrade vim 8.1.1547 -> 8.1.1845

--- pkgs/applications/editors/vim/common.nix
+++ pkgs/applications/editors/vim/common.nix
@@ -1,12 +1,12 @@
 { lib, fetchFromGitHub }:
 rec {
-  version = "8.1.1547";
+  version = "8.1.1845";
 
   src = fetchFromGitHub {
     owner = "vim";
     repo = "vim";
     rev = "v${version}";
-    sha256 = "01v35wq8wjrbzmdf5r02gp0sfa9yyfswsi3fqzn94cd68qhskkpr";
+    sha256 = "1yaj3lb31qp7bh0lgnqdpln4xbvf7gw7mrgkl6730y65wslfla8l";
   };
 
   enableParallelBuilding = true;
