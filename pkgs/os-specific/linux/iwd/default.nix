09cbcb1f2916d46fbe25914a62e4f7f2d823ea73
Will Dietz <w@wdtz.org>
2019-08-15 18:22:26 -0500

    iwd: revert commit introducing attempt to create /var
    Co-authored-by: worldofpeace <worldofpeace@protonmail.ch>

--- pkgs/os-specific/linux/iwd/default.nix
+++ pkgs/os-specific/linux/iwd/default.nix
@@ -19,6 +19,16 @@ stdenv.mkDerivation rec {
     sha256 = "03ca47d4hn28vkf5fr6ck1gz5py4lm1pw3nw9s1ckw7cqxw961sf";
   };
 
+  patches = [
+    # Undo creating ReadWritePaths as instalation target.
+    (fetchpatch {
+      name = "revert-create-dirs-on-install.patch";
+      url = "https://git.kernel.org/pub/scm/network/wireless/iwd.git/patch/?id=5a96c11664eb553bc28a2142af382b190254edbb";
+      sha256 = "08gkz3ia1l5xsh3pbx4abimgf7m88wygfpfyg77yi6dwavjqm6cx";
+      revert = true;
+    })
+  ];
+
   nativeBuildInputs = [
     autoreconfHook
     pkgconfig
