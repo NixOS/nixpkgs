be4b3e80017ce048c8008d9fae200fc19939399d
Tim Steinbach <tim@nequissimus.com>
2019-08-16 07:37:11 -0400

    linux: 4.19.66 -> 4.19.67
    (cherry picked from commit c768b0f6eaca94879013c8a4b684434eb320184e)

--- pkgs/os-specific/linux/kernel/linux-4.19.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.19.66";
+  version = "4.19.67";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0r6vzarmi77fhivd1n6f667sgcw8zd54ykmhmp6rd52bbkhsp0f9";
+    sha256 = "00m5k0nfcvgff70686rbhn3w8c9wc3jxqvyddw40lylaqdh3s72s";
   };
 } // (args.argsOverride or {}))
