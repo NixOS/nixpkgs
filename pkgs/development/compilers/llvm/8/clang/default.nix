b9f4896573561bbd3fa41de4e26ee1e6970a473d
Craig Hall <ch9871@gmail.com>
2019-08-16 14:57:51 +0100

    clang: use pname

--- pkgs/development/compilers/llvm/8/clang/default.nix
+++ pkgs/development/compilers/llvm/8/clang/default.nix
@@ -6,7 +6,7 @@
 
 let
   self = stdenv.mkDerivation ({
-    name = "clang-${version}";
+    pname = "clang";
     inherit version;
 
     src = fetch "cfe" "0ihnbdl058gvl2wdy45p5am55bq8ifx8m9mhcsgj9ax8yxlzvvvh";
