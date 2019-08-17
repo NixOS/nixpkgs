a3caa6b206a4540379f0665bd7464583bede3593
worldofpeace <worldofpeace@protonmail.ch>
2019-08-17 16:48:36 -0400

    xfce4-14.libxfce4ui: native gobject-introspection

--- pkgs/desktops/xfce4-14/libxfce4ui/default.nix
+++ pkgs/desktops/xfce4-14/libxfce4ui/default.nix
@@ -8,7 +8,8 @@ mkXfceDerivation rec {
 
   sha256 = "0fnncf30s51qhgixn57z4d021pjjhzgsg2x69w4dy68vff2347qy";
 
-  buildInputs =  [ gobject-introspection gtk2 gtk3 libstartup_notification xfconf ];
+  nativeBuildInputs = [ gobject-introspection ];
+  buildInputs =  [ gtk2 gtk3 libstartup_notification xfconf ];
   propagatedBuildInputs = [ libxfce4util libICE libSM ];
 
   meta = with lib; {
