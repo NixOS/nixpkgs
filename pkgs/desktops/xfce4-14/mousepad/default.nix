3d0afe54b92b9ff27b596497734c4c1424f6488d
worldofpeace <worldofpeace@protonmail.ch>
2019-08-16 23:13:20 -0400

    xfce4-14.mousepad: dbus-glib -> GDBus

--- pkgs/desktops/xfce4-14/mousepad/default.nix
+++ pkgs/desktops/xfce4-14/mousepad/default.nix
@@ -1,4 +1,4 @@
-{ mkXfceDerivation, exo, wrapGAppsHook, dbus-glib, gtk3, gtksourceview3 }:
+{ mkXfceDerivation, exo, glib, gtk3, gtksourceview3 }:
 
 mkXfceDerivation rec {
   category = "apps";
@@ -7,6 +7,6 @@ mkXfceDerivation rec {
 
   sha256 = "0pr1w9n0qq2raxhy78982i9g17x0ya02q7vdrn0wb2bpk74hlki5";
 
-  nativeBuildInputs = [ exo wrapGAppsHook ];
-  buildInputs = [ dbus-glib gtk3 gtksourceview3 ];
+  nativeBuildInputs = [ exo ];
+  buildInputs = [ glib gtk3 gtksourceview3 ];
 }
