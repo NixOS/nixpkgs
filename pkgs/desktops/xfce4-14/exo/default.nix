ec4a0f81716b84ca99bac47905a418576159838d
worldofpeace <worldofpeace@protonmail.ch>
2019-08-16 22:59:58 -0400

    xfce4-14.exo: docbook_xsl nativeBuildInputs

--- pkgs/desktops/xfce4-14/exo/default.nix
+++ pkgs/desktops/xfce4-14/exo/default.nix
@@ -8,17 +8,13 @@ mkXfceDerivation rec {
 
   sha256 = "013am7q4pwfncf4hk2a3hv7yx2vxgzb5xm8qsi9mxkj29xdhrvs5";
 
-  nativeBuildInputs = [ libxslt perlPackages.URI ];
+  nativeBuildInputs = [ libxslt perlPackages.URI docbook_xsl ];
   buildInputs = [ gtk3 libxfce4ui libxfce4util ];
 
   postPatch = ''
     substituteInPlace exo-helper/Makefile.am \
       --replace 'exo_helper_2_CFLAGS =' \
                 'exo_helper_2_CFLAGS = $(GIO_UNIX_CFLAGS)'
-
-    substituteInPlace docs/reference/Makefile.am \
-      --replace http://docbook.sourceforge.net/release/xsl/current \
-                ${docbook_xsl}/share/xml/docbook-xsl
   '';
 
   meta = {
