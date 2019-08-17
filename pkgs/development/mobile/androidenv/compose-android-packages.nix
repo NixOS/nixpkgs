07165798859250c32649e133594492f00d3117bd
Erik Arvstedt <erik.arvstedt@gmail.com>
2019-08-17 10:48:05 +0200

    androidenv: Fix deployment of google_apis* system images
    Fixes #56866

--- pkgs/development/mobile/androidenv/compose-android-packages.nix
+++ pkgs/development/mobile/androidenv/compose-android-packages.nix
@@ -121,6 +121,12 @@ rec {
         deployAndroidPackage {
           inherit os;
           package = system-images-packages.${apiVersion}.${type}.${abiVersion};
+          # Patch 'google_apis' system images so they're recognized by the sdk.
+          # Without this, `android list targets` shows 'Tag/ABIs : no ABIs' instead
+          # of 'Tag/ABIs : google_apis*/*' and the emulator fails with an ABI-related error.
+          patchInstructions = stdenv.lib.optionalString (stdenv.lib.hasPrefix "google_apis" type) ''
+            sed -i '/^Addon.Vendor/d' source.properties
+          '';
         }
       ) abiVersions
     ) systemImageTypes
