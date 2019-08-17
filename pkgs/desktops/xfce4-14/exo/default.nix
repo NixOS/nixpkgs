48240a3b51e2268ed6864a0e81d1e4c25e534f49
worldofpeace <worldofpeace@protonmail.ch>
2019-08-16 23:06:19 -0400

    xfce4-14.exo: workaround undeclared gio-unix-2.0
    See https://github.com/NixOS/nixpkgs/issues/36468
    
    Upstream issue: https://bugzilla.xfce.org/show_bug.cgi?id=15825

--- pkgs/desktops/xfce4-14/exo/default.nix
+++ pkgs/desktops/xfce4-14/exo/default.nix
@@ -1,4 +1,4 @@
-{ mkXfceDerivation, docbook_xsl, libxslt, perlPackages, gtk3
+{ mkXfceDerivation, docbook_xsl, glib, libxslt, perlPackages, gtk3
 , libxfce4ui, libxfce4util }:
 
 mkXfceDerivation rec {
@@ -9,13 +9,10 @@ mkXfceDerivation rec {
   sha256 = "013am7q4pwfncf4hk2a3hv7yx2vxgzb5xm8qsi9mxkj29xdhrvs5";
 
   nativeBuildInputs = [ libxslt perlPackages.URI docbook_xsl ];
-  buildInputs = [ gtk3 libxfce4ui libxfce4util ];
+  buildInputs = [ gtk3 glib libxfce4ui libxfce4util ];
 
-  postPatch = ''
-    substituteInPlace exo-helper/Makefile.am \
-      --replace 'exo_helper_2_CFLAGS =' \
-                'exo_helper_2_CFLAGS = $(GIO_UNIX_CFLAGS)'
-  '';
+  # Workaround https://bugzilla.xfce.org/show_bug.cgi?id=15825
+  NIX_CFLAGS_COMPILE = "-I${glib.dev}/include/gio-unix-2.0";
 
   meta = {
     description = "Application library for Xfce";
