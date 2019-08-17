50fbe46ae735e155c4e70dead0ddab7f085fc653
Izorkin <izorkin@elven.pw>
2019-08-17 21:29:50 +0300

    mariadb-galera: 25.3.26 -> 25.3.27

--- pkgs/servers/sql/mariadb/galera/25.nix
+++ pkgs/servers/sql/mariadb/galera/25.nix
@@ -1,6 +1,6 @@
 { callPackage, ... } @ args:
 
 callPackage ./. (args // {
-  version = "25.3.26";
-  sha256 = "0fs0c1px9lknf1a5wwb12z1hj7j7b6hsfjddggikvkdkrnr2xs1f";
+  version = "25.3.27";
+  sha256 = "143kzj0fmak1gdww4qkqmmliw8klxm6mwk5531748swlwm6gqr5q";
 })
