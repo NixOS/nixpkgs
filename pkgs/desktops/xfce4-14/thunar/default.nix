b373ce852b7bbc6c770741060697246a01da6bbf
worldofpeace <worldofpeace@protonmail.ch>
2019-08-17 17:20:04 -0400

    xfce4-14.thunar: gobject-introspection support

--- pkgs/desktops/xfce4-14/thunar/default.nix
+++ pkgs/desktops/xfce4-14/thunar/default.nix
@@ -1,5 +1,5 @@
 { mkXfceDerivation, docbook_xsl, exo, gdk-pixbuf, gtk3, libgudev ? null
-, libnotify ? null, libX11, libxfce4ui, libxfce4util, libxslt, xfconf }:
+, libnotify ? null, libX11, libxfce4ui, libxfce4util, libxslt, xfconf, gobject-introspection }:
 
 mkXfceDerivation rec {
   category = "xfce";
@@ -8,7 +8,7 @@ mkXfceDerivation rec {
 
   sha256 = "01w60csbs2nq1bhb8n1bnmjmx48fm0va3qbnq84z0h2dxpr80b1w";
 
-  nativeBuildInputs = [ libxslt docbook_xsl ];
+  nativeBuildInputs = [ libxslt docbook_xsl gobject-introspection ];
 
   buildInputs = [
     exo
