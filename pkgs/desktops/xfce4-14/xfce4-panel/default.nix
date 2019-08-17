c270552db7950fb8d83d815aa9fb8e6ddd1f8ddf
worldofpeace <worldofpeace@protonmail.ch>
2019-08-17 17:20:24 -0400

    xfce4-14.xfce4-panel: gobject-introspection support

--- pkgs/desktops/xfce4-14/xfce4-panel/default.nix
+++ pkgs/desktops/xfce4-14/xfce4-panel/default.nix
@@ -1,4 +1,4 @@
-{ mkXfceDerivation, tzdata, exo, garcon, gtk2, gtk3, glib, gettext, glib-networking, libxfce4ui, libxfce4util, libwnck3, xfconf }:
+{ mkXfceDerivation, tzdata, exo, garcon, gtk2, gtk3, glib, gettext, glib-networking, libxfce4ui, libxfce4util, libwnck3, xfconf, gobject-introspection }:
 
 mkXfceDerivation rec {
   category = "xfce";
@@ -7,6 +7,7 @@ mkXfceDerivation rec {
 
   sha256 = "1v3f2xjz9gwa8maqqvv9w2dh1cgy03v89a9ny7nrv0cjsxwwrr15";
 
+  nativeBuildInputs = [ gobject-introspection ];
   buildInputs = [ exo garcon gtk2 gtk3 glib glib-networking libxfce4ui libxfce4util libwnck3 xfconf ];
 
   patches = [ ../../xfce/core/xfce4-panel-datadir.patch ];
