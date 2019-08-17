1099bfa7cb287517a16a4f343c7f124d4a927337
worldofpeace <worldofpeace@protonmail.ch>
2019-08-17 00:06:05 -0400

    xfce4-14.thunar: nativeBuildInputs docbook_xsl

--- pkgs/desktops/xfce4-14/thunar/default.nix
+++ pkgs/desktops/xfce4-14/thunar/default.nix
@@ -8,13 +8,7 @@ mkXfceDerivation rec {
 
   sha256 = "01w60csbs2nq1bhb8n1bnmjmx48fm0va3qbnq84z0h2dxpr80b1w";
 
-  postPatch = ''
-    substituteInPlace docs/Makefile.am \
-      --replace http://docbook.sourceforge.net/release/xsl/current \
-                ${docbook_xsl}/share/xml/docbook-xsl
-  '';
-
-  nativeBuildInputs = [ libxslt ];
+  nativeBuildInputs = [ libxslt docbook_xsl ];
 
   buildInputs = [
     exo
