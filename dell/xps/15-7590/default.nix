53ca456f6013f3c6762943a172ee0d4cc656e26f
Tom Bereknyei <tomberek@gmail.com>
2019-08-17 02:08:08 -0400

    Suggested fixes

--- dell/xps/15-7590/default.nix
+++ dell/xps/15-7590/default.nix
@@ -4,10 +4,11 @@
   imports = [
     ../../../common/cpu/intel
     ../../../common/pc/laptop
+    ../../../common/pc/ssd
   ];
 
   # TODO: boot loader
-  boot.loader.systemd-boot.enable = lib.mkDefault true;
+  #boot.loader.systemd-boot.enable = lib.mkDefault true;
   boot.kernelPackages = pkgs.linuxPackages_5_1;
   boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
 
