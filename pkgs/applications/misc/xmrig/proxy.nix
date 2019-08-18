03fbb136f410f642d162d3f7749c35b46b2e13b6
Ahmed Kamal <email.ahmedkamal@googlemail.com>
2019-08-18 16:37:01 +0200

    xmrig-proxy: 2.14.4 -> 3.0.0

--- pkgs/applications/misc/xmrig/proxy.nix
+++ pkgs/applications/misc/xmrig/proxy.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation rec {
   name = "xmrig-proxy-${version}";
-  version = "2.14.4";
+  version = "3.0.0";
 
   src = fetchFromGitHub {
     owner = "xmrig";
     repo = "xmrig-proxy";
     rev = "v${version}";
-    sha256 = "11q4w5ncyyf2vgrb64zlr01n3adyaxja2pcqm5id9if0nvwwn4ga";
+    sha256 = "19rv5zmxwr3kmb2m3fas91aq1493cnkhvs88zcflnijr1ra218ks";
   };
 
   nativeBuildInputs = [ cmake ];
