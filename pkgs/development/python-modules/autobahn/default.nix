78559b5a161b39e29612f086d58017d7b0654f26
Frederik Rietdijk <fridh@fridh.nl>
2019-08-18 09:20:39 +0200

    python: autobahn: 19.1.1 -> 19.8.1

--- pkgs/development/python-modules/autobahn/default.nix
+++ pkgs/development/python-modules/autobahn/default.nix
@@ -4,11 +4,11 @@
 }:
 buildPythonPackage rec {
   pname = "autobahn";
-  version = "19.1.1";
+  version = "19.8.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "aebbadb700c13792a2967c79002855d1153b9ec8f2949d169e908388699596ff";
+    sha256 = "294e7381dd54e73834354832604ae85567caf391c39363fed0ea2bfa86aa4304";
   };
 
   propagatedBuildInputs = [ six txaio twisted zope_interface cffi ] ++
