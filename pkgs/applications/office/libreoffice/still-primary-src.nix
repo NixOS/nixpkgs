2bc8c6705eceb0b4098b0d86ab7cbafc369426d7
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2019-08-18 12:56:29 -0300

    libreoffice-still: 6.1.5.2 -> 6.2.6.2

--- pkgs/applications/office/libreoffice/still-primary-src.nix
+++ pkgs/applications/office/libreoffice/still-primary-src.nix
@@ -2,8 +2,8 @@
 
 rec {
   major = "6";
-  minor = "1";
-  patch = "5";
+  minor = "2";
+  patch = "6";
   tweak = "2";
 
   subdir = "${major}.${minor}.${patch}";
@@ -12,6 +12,6 @@ rec {
 
   src = fetchurl {
     url = "https://download.documentfoundation.org/libreoffice/src/${subdir}/libreoffice-${version}.tar.xz";
-    sha256 = "1wh8qhqkmb89nmfcb0w6iwpdzxwqr7c5kzxgpk4gy60xin6gwjgb";
+    sha256 = "1nzvdb6yq8bpybz7lrppr237cws6dajk3r9hc9qd0zi55kcddjpq";
   };
 }
