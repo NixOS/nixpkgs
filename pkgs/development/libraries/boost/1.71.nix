bf3de125a3f24d34e9414d6dfdba7dfc1174fdca
Will Dietz <w@wdtz.org>
2019-08-19 15:28:12 -0500

    boost171: grab from bintray, not on sf.net ("yet"?)

--- pkgs/development/libraries/boost/1.71.nix
+++ pkgs/development/libraries/boost/1.71.nix
@@ -4,7 +4,8 @@ callPackage ./generic.nix (args // rec {
   version = "1.71.0";
 
   src = fetchurl {
-    url = "mirror://sourceforge/boost/boost_1_71_0.tar.bz2";
+    #url = "mirror://sourceforge/boost/boost_1_71_0.tar.bz2";
+    url = "https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.tar.bz2";
     # SHA256 from http://www.boost.org/users/history/version_1_71_0.html
     sha256 = "d73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee";
   };
