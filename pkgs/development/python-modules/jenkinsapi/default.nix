b33e3ba863ddbb725a97ead69fceb6ece8022b0a
Marek Mahut <marek.mahut@gmail.com>
2019-08-19 17:26:37 +0200

    pythonPackages.jenkinsapi: fixing tests

--- pkgs/development/python-modules/jenkinsapi/default.nix
+++ pkgs/development/python-modules/jenkinsapi/default.nix
@@ -7,6 +7,14 @@
 , mock
 , nose
 , unittest2
+, requests-kerberos
+, codecov
+, tox
+, pylint
+, pycodestyle
+, pytest
+, pytestcov
+, pytest-mock
 }:
 
 buildPythonPackage rec {
@@ -18,8 +26,29 @@ buildPythonPackage rec {
     sha256 = "bf35b208fe05e65508f3b8bbb0f91d164b007632e27ebe5f54041174b681b696";
   };
 
+  patchPhase = ''
+    # Removing unpure tests
+    rm -fr jenkinsapi_tests/systests/conftest.py jenkinsapi_tests/unittests
+    substituteInPlace test-requirements.txt \
+      --replace "pytest==3.7.0" "pytest"
+  '';
+
+
   propagatedBuildInputs = [ pytz requests ];
-  buildInputs = [ coverage mock nose unittest2 ];
+  checkInputs = [
+    requests-kerberos
+    codecov
+    tox
+    pylint
+    pycodestyle
+    coverage
+    mock
+    nose
+    unittest2
+    pytest
+    pytestcov
+    pytest-mock
+  ];
 
   meta = with stdenv.lib; {
     description = "A Python API for accessing resources on a Jenkins continuous-integration server";
