d57a8b360f7b74c71422c19047c77417db445c28
Marek Mahut <marek.mahut@gmail.com>
2019-08-19 17:37:11 +0200

    python3Packages.boltztrap2: moving cmake to nativeBuildInputs
    Cleaning up test dependencies

--- pkgs/development/python-modules/boltztrap2/default.nix
+++ pkgs/development/python-modules/boltztrap2/default.nix
@@ -7,7 +7,6 @@
 , matplotlib
 , ase
 , netcdf4
-, pytest
 , pythonOlder
 , cython
 , cmake
@@ -23,16 +22,11 @@ buildPythonPackage rec {
     sha256 = "81e8a5ef8240c6a2205463fa7bc643b8033125237927f5492dab0b5d1aadb35a";
   };
 
-  buildInputs = [ cython cmake ];
-  checkInputs = [ pytest ];
   propagatedBuildInputs = [ spglib numpy scipy matplotlib ase netcdf4 ];
+  nativeBuildInputs = [ cmake cython ];
 
   # pypi release does no include files for tests
   doCheck = false;
-
-  checkPhase = ''
-    py.test
-  '';
 
   meta = with stdenv.lib; {
     homepage = https://www.boltztrap.org/;
