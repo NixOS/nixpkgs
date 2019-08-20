7ced973114e6a2a7d2d9cf94765b85cf10cba554
Markus Kowalewski <markus.kowalewski@gmail.com>
2019-08-21 00:27:14 +0200

    nixos/orangefs: add test

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -205,6 +205,7 @@ in
   # openstack-image-userdata doesn't work in a sandbox as the simulated openstack instance needs network access
   #openstack-image-userdata = (handleTestOn ["x86_64-linux"] ./openstack-image.nix {}).userdata or {};
   openstack-image-metadata = (handleTestOn ["x86_64-linux"] ./openstack-image.nix {}).metadata or {};
+  orangefs = handleTest ./orangefs.nix {};
   os-prober = handleTestOn ["x86_64-linux"] ./os-prober.nix {};
   osquery = handleTest ./osquery.nix {};
   osrm-backend = handleTest ./osrm-backend.nix {};
