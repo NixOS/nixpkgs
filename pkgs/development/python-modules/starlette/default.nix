f0411f998e9592ccf5c715ec4fcaee7f7446e1b2
R. RyanTM <ryantm-bot@ryantm.com>
2019-08-20 09:13:04 -0700

    python37Packages.starlette: 0.12.4 -> 0.12.7
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-starlette/versions

--- pkgs/development/python-modules/starlette/default.nix
+++ pkgs/development/python-modules/starlette/default.nix
@@ -16,12 +16,12 @@
 
 buildPythonPackage rec {
   pname = "starlette";
-  version = "0.12.4";
+  version = "0.12.7";
   disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1m7qf4g5dn7n36406zbqsag71nmwp2dz91yxpplm7h7wiw2xxw93";
+    sha256 = "0zf7nwma801a9hvwwq4xy3rrkca9vydj30s3bnngmm4dvkk575c4";
   };
 
   propagatedBuildInputs = [
