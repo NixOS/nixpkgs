a3e66b1ee1fdc0db9ef2e71efdd9c63e16b36e78
R. RyanTM <ryantm-bot@ryantm.com>
2019-08-20 08:33:21 -0700

    python37Packages.plotly: 4.0.0 -> 4.1.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-plotly/versions

--- pkgs/development/python-modules/plotly/default.nix
+++ pkgs/development/python-modules/plotly/default.nix
@@ -11,11 +11,11 @@
 
 buildPythonPackage rec {
   pname = "plotly";
-  version = "4.0.0";
+  version = "4.1.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0iw0j2jwlbzknpbdpaqrjjlbycbwqhavp1crblvihf03knn7nkxz";
+    sha256 = "166rscpr9xbs3pwd61mivhlm1gv3chkgpk2cy0fpb86axa7z4cwk";
   };
 
   propagatedBuildInputs = [
