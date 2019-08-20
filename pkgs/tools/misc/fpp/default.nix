d0f27576d8e03ea68dcf3c15955e969d69a3db77
R. RyanTM <ryantm-bot@ryantm.com>
2019-08-19 20:39:52 -0700

    fpp: 0.7.2 -> 0.8.2
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/fpp/versions

--- pkgs/tools/misc/fpp/default.nix
+++ pkgs/tools/misc/fpp/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "fpp-${version}";
-  version = "0.7.2";
+  version = "0.8.2";
 
   src = fetchFromGitHub {
     owner = "facebook";
     repo = "PathPicker";
     rev = version;
-    sha256 = "03n8sc2fvs2vk46jv6qfkjbyqz85yxnphvabji7qnmd3jv631w47";
+    sha256 = "00916xx4scd4xr9zxqkyhilczi27f2qm5y042592wr79ddix4n9v";
   };
 
   postPatch = ''
