1427b963ee7341d3832e6da9fd5a283dceffe851
Doron Behar <doron.behar@gmail.com>
2019-08-20 08:35:37 +0300

    Use the more conventional version scheme and fix it only in src

--- pkgs/applications/networking/feedreaders/castget/default.nix
+++ pkgs/applications/networking/feedreaders/castget/default.nix
@@ -10,12 +10,14 @@
 
 stdenv.mkDerivation rec {
   pname = "castget";
-  version = "1_2_4";
+  version = "1.2.4";
 
   src = fetchFromGitHub {
     owner = "mlj";
     repo = pname;
-    rev = "rel_${version}";
+    # Upstream uses `_` instead of `.` for the version, let's hope it will
+    # change in the next release
+    rev = "rel_${lib.replaceStrings ["."] ["_"] version}";
     sha256 = "1pfrjmsikv35cc0praxgim26zq4r7dfp1pkn6n9fz3fm73gxylyv";
   };
   prePatch = ''
