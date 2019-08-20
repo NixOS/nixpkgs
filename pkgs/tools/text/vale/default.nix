699a3aef6c7aa0d8c7868a975d0d59684b73ae1e
Will Dietz <w@wdtz.org>
2019-08-20 07:54:18 -0500

    vale: 1.4.3 -> 1.7.1, buildGoModule

--- pkgs/tools/text/vale/default.nix
+++ pkgs/tools/text/vale/default.nix
@@ -1,10 +1,8 @@
-{ stdenv, buildGoPackage, fetchFromGitHub }:
+{ stdenv, buildGoModule, fetchFromGitHub }:
 
-buildGoPackage rec {
-  name = "vale-${version}";
-  version = "1.4.3";
-
-  goPackagePath = "github.com/errata-ai/vale";
+buildGoModule rec {
+  pname = "vale";
+  version = "1.7.1";
 
   subPackages = [ "." ];
 
@@ -12,8 +10,10 @@ buildGoPackage rec {
     owner  = "errata-ai";
     repo   = "vale";
     rev    = "v${version}";
-    sha256 = "1dgh2frf577048cacwnrl0xx5hha055z42sqq38lf07ybwyxdxms";
+    sha256 = "1qi3brjppiymk6as0xic2n3bhq8g8qw1z8d9a24w60x9gp52yq5m";
   };
+
+  modSha256 = "0av728w22rrlw2030d09q5dz9ks58p3n4g1hx4xcs0gi33sdsdb8";
 
   meta = with stdenv.lib; {
     homepage = https://errata-ai.github.io/vale/;
