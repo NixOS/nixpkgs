NixOS/nixpkgs

commit 46420bbaa3f8f79ce7b9ee68e98eba1f7bce2db6

treewide: name -> pname (easy cases) (#66585)
treewide replacement of

stdenv.mkDerivation rec {
  name = "*-${version}";
  version = "*";

to pname

pkgs/tools/networking/network-manager/default.nix
@@ -10,7 +10,7 @@ let
   pname = "NetworkManager";
   pythonForDocs = python3.withPackages (pkgs: with pkgs; [ pygobject3 ]);
 in stdenv.mkDerivation rec {
-  name = "network-manager-${version}";
+  pname = "network-manager";
   version = "1.18.2";
 
   src = fetchurl {

nixos/tests/common/letsencrypt/default.nix
@@ -56,11 +56,11 @@
 
 let
   softhsm = pkgs.stdenv.mkDerivation rec {
-    name = "softhsm-${version}";
+    pname = "softhsm";
     version = "1.3.8";
 
     src = pkgs.fetchurl {
-      url = "https://dist.opendnssec.org/source/${name}.tar.gz";
+      url = "https://dist.opendnssec.org/source/${pname}-${version}.tar.gz";
       sha256 = "0flmnpkgp65ym7w3qyg78d3fbmvq3aznmi66rgd420n33shf7aif";
     };
 

pkgs/applications/altcoins/go-ethereum-classic/default.nix
@@ -1,7 +1,7 @@
 { lib, buildGoPackage, fetchgit }:
 
 buildGoPackage rec {
-  name = "go-ethereum-classic-${version}";
+  pname = "go-ethereum-classic";
   version = "4.0.0";
 
   goPackagePath = "github.com/ethereumproject/go-ethereum";

pkgs/applications/altcoins/monero-gui/default.nix
@@ -24,7 +24,7 @@ let
 in
 
 stdenv.mkDerivation rec {
-  name = "monero-gui-${version}";
+  pname = "monero-gui";
   version = "0.14.1.2";
 
   src = fetchFromGitHub {

pkgs/applications/altcoins/monero/default.nix
@@ -11,7 +11,7 @@ assert stdenv.isDarwin -> IOKit != null;
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
-  name    = "monero-${version}";
+  pname = "monero";
   version = "0.14.1.0";
 
   src = fetchgit {

pkgs/applications/altcoins/nano-wallet/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
 
-  name = "nano-wallet-${version}";
+  pname = "nano-wallet";
   version = "18.0";
 
   src = fetchFromGitHub {

pkgs/applications/altcoins/parity-ui/default.nix
@@ -5,13 +5,13 @@ let
 uiEnv = pkgs.callPackage ./env.nix { };
 
 in stdenv.mkDerivation rec {
-  name = "parity-ui-${version}";
+  pname = "parity-ui";
   version = "0.3.4";
 
   src = fetchurl {
     url = "https://github.com/parity-js/shell/releases/download/v${version}/parity-ui_${version}_amd64.deb";
     sha256 = "1xbd00r9ph8w2d6d2c5xg4b5l74ljzs50rpc6kahfznypmh4kr73";
-    name = "${name}.deb";
+    name = "${pname}-${version}.deb";
   };
 
   nativeBuildInputs = [ makeWrapper nodePackages.asar ];

pkgs/applications/audio/AMB-plugins/default.nix
@@ -2,10 +2,10 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "AMB-plugins-${version}";
+  pname = "AMB-plugins";
   version = "0.8.1";
   src = fetchurl {
-    url = "http://kokkinizita.linuxaudio.org/linuxaudio/downloads/${name}.tar.bz2";
+    url = "http://kokkinizita.linuxaudio.org/linuxaudio/downloads/${pname}-${version}.tar.bz2";
     sha256 = "0x4blm4visjqj0ndqr0cg776v3b7lvplpc8cgi9n51llhavn0jpl";
   };
 

commit a7679fa4afc5fc7592e5ccb53a7ec511ba5aa93a

networkmanager: hardcode parts of the source URL
Broke the build as the pname variable was used to create the URL
Introduced by 46420bbaa3f8f79ce7b9ee68e98eba1f7bce2db6

pkgs/tools/networking/network-manager/default.nix
@@ -7,14 +7,13 @@
 , openconnect, curl, meson, ninja, libpsl }:
 
 let
-  pname = "NetworkManager";
   pythonForDocs = python3.withPackages (pkgs: with pkgs; [ pygobject3 ]);
 in stdenv.mkDerivation rec {
   pname = "network-manager";
   version = "1.18.2";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
+    url = "mirror://gnome/sources/NetworkManager/${stdenv.lib.versions.majorMinor version}/NetworkManager-${version}.tar.xz";
     sha256 = "1hx5dx5dgdqh3p8fq7q1pxy2bx2iymc74lj60ycrf7ydfjlprnad";
   };
 