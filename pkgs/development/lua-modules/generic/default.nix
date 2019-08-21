c62337d9c7ff70e9bdfe317388c4fa5d6202142a
Alexei Robyn <shados@shados.net>
2019-08-21 23:03:11 +1000

    lua*Packages: Consolidate separate setup hooks together
    - Lua packages now consistently use LUA_PATH/LUA_CPATH rather than a mix
      of those and NIX_LUA_PATH/NIX_LUA_CPATH
    - Lua libraries are now consistently only added to the search path
    variables if:
        1) The library actually has a corresponding directory to search
        2) The library is not already present in the search path
      This should help prevent the search paths from growing overly large
    - Fixed bugs in some path helpers
    - Changed the affected shell script indentation to 2 spaces; nixpkgs
      shell scripts are inconsistently split between 2 and 4 space
      indentation, but 2 matches better with the Nix expressions, so IMO it
      makes more sense

--- pkgs/development/lua-modules/generic/default.nix
+++ pkgs/development/lua-modules/generic/default.nix
@@ -1,6 +1,6 @@
 { lua, writeText, toLuaModule }:
 
-{ buildInputs ? [], disabled ? false, ... } @ attrs:
+{ disabled ? false, ... } @ attrs:
 
 if disabled then
   throw "${attrs.name} not supported by interpreter lua-${lua.luaversion}"
@@ -18,37 +18,8 @@ else
     //
     {
       name = "lua${lua.luaversion}-" + attrs.name;
-      buildInputs = buildInputs ++ [ lua ];
-
-      setupHook = writeText "setup-hook.sh" ''
-        # check for lua/clua modules and don't add duplicates
-
-        addLuaLibPath() {
-          local package_path="$1/share/lua/${lua.luaversion}"
-          if [[ ! -d $package_path ]]; then return; fi
-          if [[ $LUA_PATH = *"$package_path"* ]]; then return; fi
-
-          if [[ -z $LUA_PATH ]]; then
-            export LUA_PATH="$package_path/?.lua;$package_path/?/init.lua"
-          else
-            export LUA_PATH="$LUA_PATH;$package_path/?.lua;$package_path/?/init.lua"
-          fi
-        }
-
-        addLuaLibCPath() {
-          local package_cpath="$1/lib/lua/${lua.luaversion}"
-          if [[ ! -d $package_cpath ]]; then return; fi
-          if [[ $LUA_CPATH = *"$package_cpath"* ]]; then return; fi
-
-          if [[ -z $LUA_CPATH ]]; then
-            export LUA_CPATH="$package_cpath/?.so"
-          else
-            export LUA_CPATH="$LUA_CPATH;$package_cpath/?.so"
-          fi
-        }
-
-        addEnvHooks "$hostOffset" addLuaLibPath
-        addEnvHooks "$hostOffset" addLuaLibCPath
-      '';
+      propagatedBuildInputs = [
+        lua # propagate it for its setup-hook
+      ];
     }
   ) )
