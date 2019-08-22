bfade23717a963146fe57f715cfbd765b91c324c
Amar1729 <amar.paul16@gmail.com>
2019-08-22 16:59:47 -0400

    android-file-transfer: fix for darwin

--- pkgs/tools/filesystems/android-file-transfer/default.nix
+++ pkgs/tools/filesystems/android-file-transfer/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, mkDerivation, fetchFromGitHub, cmake, fuse, readline, pkgconfig, qtbase }:
+{ stdenv, mkDerivation, fetchFromGitHub, cmake, fuse, readline, pkgconfig, osxfuse, qt5, qtbase }:
 
 mkDerivation rec {
   pname = "android-file-transfer";
@@ -11,14 +11,32 @@ mkDerivation rec {
     sha256 = "1pwayyd5xrmngfrmv2vwr8ns2wi199xkxf7dks8fl9zmlpizg3c3";
   };
 
-  nativeBuildInputs = [ cmake readline pkgconfig ];
-  buildInputs = [ fuse qtbase ];
+  nativeBuildInputs = [ cmake readline pkgconfig ]
+    ++ stdenv.lib.optionals stdenv.isDarwin [ qt5.wrapQtAppsHook ]
+  ;
+  buildInputs = [ qtbase ]
+    ++ stdenv.lib.optionals stdenv.isLinux [ fuse ]
+    ++ stdenv.lib.optionals stdenv.isDarwin [ osxfuse ]
+  ;
+
+  postPatch = stdenv.lib.optionalString stdenv.isDarwin ''
+    sed -i"" -e '1i cmake_policy(SET CMP0025 NEW)' CMakeLists.txt
+  '';
+
+  postInstall = stdenv.lib.optionalString stdenv.isDarwin ''
+    mkdir -p $out/Applications
+    mv $out/android-file-transfer.app $out/Applications
+
+    # Fixes "This application failed to start because it could not find or load the Qt
+    # platform plugin "cocoa"."
+    wrapQtApp $out/Applications/android-file-transfer.app/Contents/MacOS/android-file-transfer
+  '';
 
   meta = with stdenv.lib; {
     description = "Reliable MTP client with minimalistic UI";
     homepage = https://whoozle.github.io/android-file-transfer-linux/;
     license = licenses.lgpl21;
     maintainers = [ maintainers.xaverdh ];
-    platforms = platforms.linux;
+    platforms = platforms.unix;
   };
 }
