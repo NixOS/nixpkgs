8f789cda2861f0bd848deb39049587b5818d962e
Taito Horiuchi <taito.horiuchi@relexsolutions.com>
2019-08-23 15:32:28 +0300

    pythonPackages.robotframework-databaselibrary: init at 1.2.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4691,6 +4691,8 @@ in {
 
   robotframework = callPackage ../development/python-modules/robotframework { };
 
+  robotframework-databaselibrary = callPackage ../development/python-modules/robotframework-databaselibrary { };
+
   robotframework-requests = callPackage ../development/python-modules/robotframework-requests { };
 
   robotframework-ride = callPackage ../development/python-modules/robotframework-ride { };
