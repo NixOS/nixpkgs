877dc46d24ae9c0a6cb3c63d57c7f3005f2e11fb
Jan Tojnar <jtojnar@gmail.com>
2019-08-23 20:30:28 +0200

    ofono-phonesim: init at unstable-2014-04-22

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5128,6 +5128,8 @@ in
 
   offlineimap = callPackage ../tools/networking/offlineimap { };
 
+  ofono-phonesim = libsForQt5.callPackage ../development/tools/ofono-phonesim/default.nix { };
+
   oh-my-zsh = callPackage ../shells/zsh/oh-my-zsh { };
 
   ola = callPackage ../applications/misc/ola { };
