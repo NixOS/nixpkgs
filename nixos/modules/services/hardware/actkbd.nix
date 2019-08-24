25559ac82a06efc420b0d20571b904dddceb2bd4
Timothy DeHerrera <tim.deh@pm.me>
2019-08-23 18:39:27 -0600

    actkbd: add ability to deploy as user service
    Some processes require user level permissions to interact with them,
    E.g. pulseaudio. Allow users to make keybindings for them.
    
    * add new 'userMode' toggle to bindings; default to 'false'
    * add 'ENV{SYSTEMD_USER_WANTS}' to udev rule
    * only launch user service instances if there are relevant bindings
    * add 'start-actkbd' user service to initialize actkbd instances

--- nixos/modules/services/hardware/actkbd.nix
+++ nixos/modules/services/hardware/actkbd.nix
@@ -6,17 +6,46 @@ let
 
   cfg = config.services.actkbd;
 
-  configFile = pkgs.writeText "actkbd.conf" ''
+  isUserBindings = bool:
+    builtins.filter (a: a.userMode == bool) cfg.bindings;
+
+  mkConfigFile = isUser:
+    pkgs.writeText "actkbd.conf" ''
     ${concatMapStringsSep "\n"
       ({ keys, events, attributes, command, ... }:
         ''${concatMapStringsSep "+" toString keys}:${concatStringsSep "," events}:${concatStringsSep "," attributes}:${command}''
       )
-      cfg.bindings}
-    ${cfg.extraConfig}
-  '';
+      ( isUserBindings isUser )}
+      ${cfg.extraConfig}'';
+
+  mkSystemdService = config:
+  {
+    enable = true;
+    restartIfChanged = true;
+    unitConfig = {
+      Description = "actkbd on %I";
+      ConditionPathExists = "%I";
+    };
+    serviceConfig = {
+      Type = "forking";
+      ExecStart = "${pkgs.actkbd}/bin/actkbd -D -c ${config} -d %I";
+    };
+  };
 
   bindingCfg = { ... }: {
     options = {
+
+      userMode = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          whether to deploy this binding as a systemd user service; useful for
+          some programs which require user privileges. This is rarely needed and
+          should only be enabled if it is absolutely required.
+
+          E.g. <option>sound.mediaKeys</option> enables this when <option>hardware.pulseaudio.enable</option> is set.
+          '';
+      };
 
       keys = mkOption {
         type = types.listOf types.int;
@@ -107,21 +136,36 @@ in
     services.udev.packages = lib.singleton (pkgs.writeTextFile {
       name = "actkbd-udev-rules";
       destination = "/etc/udev/rules.d/61-actkbd.rules";
-      text = ''
-        ACTION=="add", SUBSYSTEM=="input", KERNEL=="event[0-9]*", ENV{ID_INPUT_KEY}=="1", TAG+="systemd", ENV{SYSTEMD_WANTS}+="actkbd@$env{DEVNAME}.service"
+      text = let actkbdVar = "actkbd@$env{DEVNAME}.service";
+      in ''
+        ACTION=="add" \
+        , SUBSYSTEM=="input" \
+        , KERNEL=="event[0-9]*" \
+        , ENV{ID_INPUT_KEY}=="1" \
+        , TAG+="systemd" \
+        , ENV{SYSTEMD_WANTS}+="${actkbdVar}" \
+        , ENV{SYSTEMD_USER_WANTS}+="${actkbdVar}"
       '';
     });
 
-    systemd.services."actkbd@" = {
-      enable = true;
-      restartIfChanged = true;
-      unitConfig = {
-        Description = "actkbd on %I";
-        ConditionPathExists = "%I";
-      };
-      serviceConfig = {
-        Type = "forking";
-        ExecStart = "${pkgs.actkbd}/bin/actkbd -D -c ${configFile} -d %I";
+    systemd.services = mkIf ( isUserBindings false != [] )
+    { "actkbd@" = ( mkSystemdService (mkConfigFile false) );
+    };
+
+    systemd.user = mkIf ( isUserBindings true != [] )
+    { services."actkbd@" = mkSystemdService (mkConfigFile true);
+
+      # this is needed because input devices initialize at boot and systemd
+      # only starts device dependant services on their initialization
+      services."start-actkbd" =
+      { enable = true;
+        unitConfig =
+        { Description = "user instances of actkbd";
+          Type = "idle";
+        };
+        script =
+          "${pkgs.systemd}/bin/systemctl --user start 'actkbd@*.service' --all";
+        wantedBy = [ "default.target" ];
       };
     };
 
