7f16f4b71b176c2135c41c4ac28a6259f4bacc86
Ryan Trinkle <ryan.trinkle@gmail.com>
2019-08-23 20:06:05 -0400

    Apply suggestions from code review
    Co-Authored-By: worldofpeace <worldofpeace@protonmail.ch>

--- pkgs/applications/version-management/git-and-tools/git-subrepo/default.nix
+++ pkgs/applications/version-management/git-and-tools/git-subrepo/default.nix
@@ -1,24 +1,29 @@
-{ stdenv, fetchFromGitHub, git, which }:
+{ stdenv, fetchFromGitHub, git, makeWrapper, which }:
 
 stdenv.mkDerivation rec {
-  name = "git-subrepo-${version}";
+  pname = "git-subrepo";
   version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "ingydotnet";
     repo = "git-subrepo";
-    rev = "${version}";
+    rev = version;
     sha256 = "05m2dm9gq2nggwnxxdyq2kjj584sn2lxk66pr1qhjxnk81awj9l7";
   };
 
+  nativeBuildInputs = [
+    makeWrapper
+    which
+  ];
+
   buildInputs = [
-    git which
+    git
   ];
 
   makeFlags = [
-    "PREFIX=$(out)"
-    "INSTALL_LIB=$(out)/bin"
-    "INSTALL_MAN=$(out)/share/man/man1"
+    "PREFIX=${placeholder ''out''}"
+    "INSTALL_LIB=${placeholder ''out''}/bin"
+    "INSTALL_MAN=${placeholder ''out''}/share/man/man1"
   ];
 
   patches = [
@@ -36,6 +41,11 @@ stdenv.mkDerivation rec {
     BASH_COMP_DIR="$out/share/bash-completion/completions"
     mkdir -p "$BASH_COMP_DIR"
     cp share/completion.bash "$BASH_COMP_DIR/git-subrepo"
+  '';
+
+  postFixup = ''
+    wrapProgram $out/bin/git-subrepo \
+      --prefix PATH : "${git}/bin"
   '';
 
   meta = with stdenv.lib; {
