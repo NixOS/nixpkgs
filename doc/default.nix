2cdfc80831236151b1acc0988321a878353fa236
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-08-25 18:26:37 -0400

    docs: The main page is index.html

--- doc/default.nix
+++ doc/default.nix
@@ -21,12 +21,10 @@ in pkgs.stdenv.mkDerivation {
     dest="$out/share/doc/nixpkgs"
     mkdir -p "$(dirname "$dest")"
     mv out/html "$dest"
-    mv "$dest/index.html" "$dest/manual.html"
-
     mv out/epub/manual.epub "$dest/nixpkgs-manual.epub"
 
     mkdir -p $out/nix-support/
-    echo "doc manual $dest manual.html" >> $out/nix-support/hydra-build-products
+    echo "doc manual $dest index.html" >> $out/nix-support/hydra-build-products
     echo "doc manual $dest nixpkgs-manual.epub" >> $out/nix-support/hydra-build-products
   '';
 }
