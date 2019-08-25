440c63a38a5c611806a00bd188b0a7623c6dcc3f
Gabriel Ebner <gebner@gebner.org>
2019-08-25 14:43:51 +0200

    vampire: 4.2.2 -> 4.4

--- pkgs/applications/science/logic/vampire/default.nix
+++ pkgs/applications/science/logic/vampire/default.nix
@@ -1,19 +1,16 @@
-{ stdenv, fetchFromGitHub, fetchpatch, z3, zlib, git }:
+{ stdenv, fetchFromGitHub, fetchpatch, z3, zlib }:
 
 stdenv.mkDerivation rec {
-  version = "4.2.2";
-  name = "vampire-${version}";
+  pname = "vampire";
+  version = "4.4";
 
   src = fetchFromGitHub {
     owner = "vprover";
     repo = "vampire";
     rev = version;
-    sha256 = "03dqjxr3cwz4h6sn9074kc6b6wjz12kpsvsi0mq2w0j5l9f8d80y";
-    #fetchSubmodules = true;
-    #leaveDotGit = true;
+    sha256 = "0v2fdfnk7l5xr5c4y54r25g1nbp4vi85zv29nbklh3r7aws3w9q1";
   };
 
-  nativeBuildInputs = [ git ];
   buildInputs = [ z3 zlib ];
 
   makeFlags = [ "vampire_z3_rel" "CC:=$(CC)" "CXX:=$(CXX)" ];
