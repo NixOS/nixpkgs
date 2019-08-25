c0cd8e33c80b788f016b603756d43632f4e84eae
Marek Mahut <marek.mahut@gmail.com>
2019-08-25 17:07:34 +0200

    jormungandr: installing the bootstrap scripts

--- pkgs/applications/altcoins/jormungandr/default.nix
+++ pkgs/applications/altcoins/jormungandr/default.nix
@@ -24,6 +24,22 @@ rustPlatform.buildRustPackage rec {
   nativeBuildInputs = [ pkgconfig protobuf ];
   buildInputs = [ openssl ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
 
+  patchPhase = ''
+    sed -i "s~SCRIPTPATH=.*~SCRIPTPATH=$out/templates/~g" scripts/bootstrap
+  '';
+
+  installPhase = ''
+    install -d $out/bin $out/templates
+    install -m755 target/*/release/jormungandr $out/bin/
+    install -m755 target/*/release/jcli $out/bin/
+    install -m755 scripts/send-transaction $out/templates
+    install -m755 scripts/jcli-helpers $out/bin/
+    install -m755 scripts/bootstrap $out/bin/jormungandr-bootstrap
+    install -m644 scripts/faucet-send-money.shtempl $out/templates/
+    install -m644 scripts/create-account-and-delegate.shtempl $out/templates/
+    install -m644 scripts/faucet-send-certificate.shtempl $out/templates/
+  '';
+
   PROTOC = "${protobuf}/bin/protoc";
 
   # Disabling integration tests
