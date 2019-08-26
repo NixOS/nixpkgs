336eb202f633dfb9cb46a8b8a6d95a4ea343b3d3
Marek Mahut <marek.mahut@gmail.com>
2019-08-26 10:01:51 +0200

    exiv2: enable tests

--- pkgs/development/libraries/exiv2/default.nix
+++ pkgs/development/libraries/exiv2/default.nix
@@ -53,8 +53,6 @@ stdenv.mkDerivation rec {
     "doc"
   ];
 
-  doCheck = stdenv.isLinux;
-
   # Test setup found by inspecting ${src}/.travis/run.sh; problems without cmake.
   checkTarget = "tests";
 
