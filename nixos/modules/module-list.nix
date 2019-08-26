7f9767954bf77d537f6bd6102c419d716b41be45
geistesk <github.jyf300hj@0x21.biz>
2019-08-26 19:39:36 +0200

    nixos/go-shadowsocks2: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -595,6 +595,7 @@
   ./services/networking/gdomap.nix
   ./services/networking/git-daemon.nix
   ./services/networking/gnunet.nix
+  ./services/networking/go-shadowsocks2.nix
   ./services/networking/gogoclient.nix
   ./services/networking/gvpe.nix
   ./services/networking/hans.nix
