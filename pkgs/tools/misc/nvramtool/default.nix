1747cd2303edea1a4bdcc21dad752b06b6fd8214
Milan Pässler <me@pbb.lc>
2019-08-27 08:47:47 +0200

    nvramtool: 4.8.1 -> 4.10

--- pkgs/tools/misc/nvramtool/default.nix
+++ pkgs/tools/misc/nvramtool/default.nix
@@ -1,13 +1,12 @@
-{ stdenv, fetchgit, iasl, flex, bison }:
+{ stdenv, fetchurl, iasl, flex, bison }:
 
 stdenv.mkDerivation rec {
-  name = "nvramtool-${version}";
-  version = "4.8.1";
+  pname = "nvramtool";
+  version = "4.10";
 
-  src = fetchgit {
-    url = "http://review.coreboot.org/p/coreboot";
-    rev = "refs/tags/${version}";
-    sha256 = "0nrf840jg4fn38zcnz1r10w2yfpvrk1nvsrnbbgdbgkmpjxz0zw9";
+  src = fetchurl {
+    url = "https://coreboot.org/releases/coreboot-${version}.tar.xz";
+    sha256 = "1jsiz17afi2lqg1jv6lsl8s05w7vr7iwgg86y2qp369hcz6kcwfa";
   };
 
   nativeBuildInputs = [ flex bison ];
@@ -16,16 +15,16 @@ stdenv.mkDerivation rec {
   buildPhase = ''
     export LEX=${flex}/bin/flex
     make -C util/nvramtool
-    '';
+  '';
 
   installPhase = ''
     mkdir -p $out/bin
     cp util/nvramtool/nvramtool $out/bin
-    '';
+  '';
 
   meta = with stdenv.lib; {
-    description = "utility for reading/writing coreboot parameters and displaying information from the coreboot table in CMOS/NVRAM";
-    homepage = https://www.coreboot.org/Nvramtool;
+    description = "Read/write coreboot parameters and display information from the coreboot table in CMOS/NVRAM";
+    homepage = "https://www.coreboot.org/Nvramtool";
     license = licenses.gpl2;
     maintainers = [ maintainers.cryptix ];
     platforms = platforms.linux;
