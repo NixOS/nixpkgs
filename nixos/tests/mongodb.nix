95f5165d808b3a00fc67e572b188130149e8ce69
Nathan Smyth <nathan.smyth@mongodb.com>
2019-08-27 17:40:44 +1000

    test: Run all the mongodb tests in one node

--- nixos/tests/mongodb.nix
+++ nixos/tests/mongodb.nix
@@ -1,50 +1,42 @@
 # This test start mongodb, runs a query using mongo shell
 
-import ./make-test-python.nix ({ pkgs, ...} : let
-  testQuery = pkgs.writeScript "nixtest.js" ''
-    db.greetings.insert({ "greeting": "hello" });
-    print(db.greetings.findOne().greeting);
-  '';
+import ./make-test.nix ({ pkgs, ... }:
+  let
+    testQuery = pkgs.writeScript "nixtest.js" ''
+      db.greetings.insert({ "greeting": "hello" });
+      print(db.greetings.findOne().greeting);
+    '';
 
-  mongodbNode = mdbPackage: {...}: {
-    services = {
-      mongodb.enable = true;
-      mongodb.package = mdbPackage;
-      mongodb.enableAuth = true;
-      mongodb.initialRootPassword = "root";
-      mongodb.initialScript = pkgs.writeText "mongodb_initial.js" ''
-            db = db.getSiblingDB("nixtest");
-            db.createUser({user:"nixtest",pwd:"nixtest",roles:[{role:"readWrite",db:"nixtest"}]});
-      '';
-      mongodb.extraConfig = ''
-            # Allow starting engine with only a small virtual disk
-            storage.journal.enabled: false
-            storage.mmapv1.smallFiles: true
-      '';
+    runMongoDBTest = pkg: ''
+      $node->execute("(rm -rf data || true) && mkdir data");
+      $node->execute("${pkg}/bin/mongod --fork --logpath logs --dbpath data");
+      $node->waitForOpenPort(27017);
+
+      $node->succeed("mongo ${testQuery}") =~ /hello/ or die;
+
+      $node->execute("${pkg}/bin/mongod --shutdown --dbpath data");
+      $node->waitForClosedPort(27017);
+    '';
+
+  in {
+    name = "mongodb";
+    meta = with pkgs.stdenv.lib.maintainers; {
+      maintainers = [ bluescreen303 offline cstrahan rvl phile314 ];
     };
-  };
 
-  runTest = node: ''
-         ${node}->start;
-         ${node}->waitForUnit("mongodb.service");
-         ${node}->succeed("mongo -u nixtest -p nixtest nixtest ${testQuery}") =~ /hello/ or die;
-         ${node}->shutdown;
-  '';
+    nodes = {
+      node = {...}: {
+        environment.systemPackages = with pkgs; [
+#          mongodb-3_4
+          mongodb-3_6
+          mongodb-4_0
+        ];
+      };
+    };
 
-
-in {
-  name = "mongodb";
-  meta = with pkgs.stdenv.lib.maintainers; {
-    maintainers = [ bluescreen303 offline cstrahan rvl phile314 ];
-  };
-
-  nodes = {
-    v3_4 = mongodbNode pkgs.mongodb-3_4;
-    v3_6 = mongodbNode pkgs.mongodb-3_6;
-    v4_0 = mongodbNode pkgs.mongodb-4_0;
-  };
-
-  testScript = runTest "$v3_4" +
-    runTest "$v3_6" +
-    runTest "$v4_0";
-})
+    testScript = "$node->start;" 
+#      + runMongoDBTest pkgs.mongodb-3_4
+      + runMongoDBTest pkgs.mongodb-3_6 
+      + runMongoDBTest pkgs.mongodb-4_0
+      + "$node->shutdown;";
+  })
