58f857a9d53a0545572b9d491a64f41f274572a6
Milan Pässler <me@pbb.lc>
2019-08-27 08:46:52 +0200

    seabios: 1.11.0 -> 1.12.1

--- pkgs/applications/virtualization/seabios/default.nix
+++ pkgs/applications/virtualization/seabios/default.nix
@@ -1,13 +1,12 @@
 { stdenv, fetchurl, iasl, python }:
 
 stdenv.mkDerivation rec {
-
-  name = "seabios-${version}";
-  version = "1.11.0";
+  pname = "seabios";
+  version = "1.12.1";
 
   src = fetchurl {
-    url = "http://code.coreboot.org/p/seabios/downloads/get/${name}.tar.gz";
-    sha256 = "1xwvp77djxbxbxg82hzj26pv6zka3556vkdcp09hnfwapcp46av2";
+    url = "https://review.coreboot.org/cgit/seabios.git/snapshot/seabios-rel-${version}.tar.gz";
+    sha256 = "15kdrn0w7wrdrjnjx0g8pw7n00p1ss1r1gcdnq62dvcrzczvpy5m";
   };
 
   buildInputs = [ iasl python ];
@@ -37,7 +36,7 @@ stdenv.mkDerivation rec {
       It can run in an emulator or it can run natively on X86 hardware with the use of coreboot.
       SeaBIOS is the default BIOS for QEMU and KVM.
     '';
-    homepage = http://www.seabios.org;
+    homepage = "https://www.seabios.org";
     license = licenses.lgpl3;
     maintainers = [ maintainers.tstrobel ];
     platforms = [ "i686-linux" "x86_64-linux" ];
