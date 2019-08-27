38294e305101933d7d8ab16af32b1bf945f3b43a
Nikolay Korotkiy <sikmir@gmail.com>
2019-08-27 19:00:53 +0300

    gpxlab: init at 0.7.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19188,6 +19188,8 @@ in
 
   gpsprune = callPackage ../applications/misc/gpsprune { };
 
+  gpxlab = libsForQt5.callPackage ../applications/misc/gpxlab { };
+
   gpxsee = libsForQt5.callPackage ../applications/misc/gpxsee { };
 
   gspell = callPackage ../development/libraries/gspell { };
