16e0743e60874e7218d8e210787ff00444a8dca7
Timothy DeHerrera <tim.deh@pm.me>
2019-08-28 11:30:23 -0600

    kakounePlugins.kak-fzf: add ability to choose between fzf/skim

--- pkgs/applications/editors/kakoune/plugins/kak-fzf.nix
+++ pkgs/applications/editors/kakoune/plugins/kak-fzf.nix
@@ -1,4 +1,7 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv, fetchFromGitHub, fzf }:
+
+assert stdenv.lib.asserts.assertOneOf "fzf" fzf.pname [ "fzf" "skim" ];
+
 stdenv.mkDerivation {
   name = "kak-fzf";
   version = "2019-07-16";
@@ -8,6 +11,17 @@ stdenv.mkDerivation {
     rev = "ede90d3e02bceb714f997adfcbab8260b42e0a19";
     sha256 = "18w90j3fpk2ddn68497s33n66aap8phw5636y1r7pqsa641zdxcv";
   };
+
+  configurePhase = ''
+    if [[ -x "${fzf}/bin/fzf" ]]; then
+      fzfImpl='${fzf}/bin/fzf'
+    else
+      fzfImpl='${fzf}/bin/sk'
+    fi
+
+    substituteInPlace rc/fzf.kak \
+      --replace \'fzf\' \'"$fzfImpl"\'
+  '';
 
   installPhase = ''
     mkdir -p $out/share/kak/autoload/plugins
