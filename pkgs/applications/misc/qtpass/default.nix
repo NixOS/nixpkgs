392763aea636fa9a59889b2d0162d959b11de0da
Chris Rendle-Short <chris@killred.net>
2019-08-28 10:08:28 +1000

    qtpass: fix missing app icon and name when running under Wayland
    Patch has been upstreamed and should be safe to remove in versions >
    1.3.0

--- pkgs/applications/misc/qtpass/default.nix
+++ pkgs/applications/misc/qtpass/default.nix
@@ -1,4 +1,6 @@
-{ stdenv, mkDerivation, fetchFromGitHub, git, gnupg, pass, qtbase, qtsvg, qttools, qmake, makeWrapper }:
+{ stdenv, mkDerivation, fetchFromGitHub, fetchpatch
+, git, gnupg, pass, qtbase, qtsvg, qttools, qmake, makeWrapper
+}:
 
 mkDerivation rec {
   pname = "qtpass";
@@ -14,6 +16,15 @@ mkDerivation rec {
   buildInputs = [ git gnupg pass qtbase qtsvg qttools ];
 
   nativeBuildInputs = [ makeWrapper qmake ];
+
+  # Fix missing app icon on Wayland. Has been upstreamed and should be safe to
+  # remove in versions > 1.3.0
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/IJHack/QtPass/commit/aba8c4180f0ab3d66c44f88b21f137b19d17bde8.patch";
+      sha256 = "009bcq0d75khmaligzd7736xdzy6a8s1m9dgqybn70h801h92fcr";
+    })
+  ];
 
   enableParallelBuilding = true;
 
