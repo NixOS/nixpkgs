1c474c5edcf4038ee57f828dc987c60ee4c488a2
Vaibhav Sagar <vaibhav.sagar@obsidian.systems>
2019-08-29 13:39:16 -0400

    haskell.packages.ghc881.polyparse: fix build

--- pkgs/development/haskell-modules/configuration-ghc-8.8.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.8.x.nix
@@ -153,5 +153,8 @@ self: super: {
     url = "https://raw.githubusercontent.com/hvr/head.hackage/master/patches/lens-4.17.1.patch";
     sha256 = "0w89ipi6dfkx5vlw4a64hh6fd0bm9hg33mwpghliyyxik5jmilv1";
   });
-
+  polyparse = appendPatch (doJailbreak super.polyparse) (pkgs.fetchpatch {
+    url = "https://raw.githubusercontent.com/hvr/head.hackage/master/patches/polyparse-1.12.1.patch";
+    sha256 = "01b2gnsq0x4fd9na8zpk6pajym55mbz64hgzawlwxdw0y6681kr5";
+  });
 }
