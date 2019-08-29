f555f70a976f62b9c08c351a9ba7d2885ebaf039
Will Dietz <w@wdtz.org>
2019-08-29 14:28:01 -0500

    libevdev: pname

--- pkgs/development/libraries/libevdev/default.nix
+++ pkgs/development/libraries/libevdev/default.nix
@@ -1,10 +1,11 @@
 { stdenv, fetchurl, python }:
 
 stdenv.mkDerivation rec {
-  name = "libevdev-1.7.0";
+  pname = "libevdev";
+  version = "1.7.0";
 
   src = fetchurl {
-    url = "https://www.freedesktop.org/software/libevdev/${name}.tar.xz";
+    url = "https://www.freedesktop.org/software/${pname}/${pname}-${version}.tar.xz";
     sha256 = "0sg3lbjn68qaq3yz2k735h29kaf3fmx7b5m1x7rm2fnhn7rf3nqi";
   };
 
