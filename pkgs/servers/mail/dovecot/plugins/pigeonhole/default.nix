ef97ff2a0e32d104769b13c83868630367899bdd
Daniel Frank <git@danielfrank.net>
2019-08-29 16:56:10 +0200

    pigeonhole: remove cve patches, as the latest released version includes them all

--- pkgs/servers/mail/dovecot/plugins/pigeonhole/default.nix
+++ pkgs/servers/mail/dovecot/plugins/pigeonhole/default.nix
@@ -12,16 +12,6 @@ stdenv.mkDerivation rec {
   buildInputs = [ dovecot openssl ];
 
   patches = [
-    (fetchpatch {
-      name = "CVE-2019-11500-1.patch";
-      url = https://github.com/dovecot/pigeonhole/commit/7ce9990a5e6ba59e89b7fe1c07f574279aed922c.patch;
-      sha256 = "07l4m2wkqn910zb8d477q6asryfqzhbhxl4fl0w89s763maiam9v";
-    })
-    (fetchpatch {
-      name = "CVE-2019-11500-2.patch";
-      url = https://github.com/dovecot/pigeonhole/commit/4a299840cdb51f61f8d1ebc0210b19c40dfbc1cc.patch;
-      sha256 = "1p7jl3fcxr63yrgj5m11sbmbfnibrx5v9aifscn1wq858jnn8myf";
-    })
   ];
 
   preConfigure = ''
