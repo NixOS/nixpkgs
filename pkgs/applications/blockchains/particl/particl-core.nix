a54dc85c2e914e0972b7db50e49f8ef394b6447e
Demyan Rogozhin <Demyan.Rogozhin@gmail.com>
2019-08-30 23:05:19 +0200

    particl-core: 0.17.1.2 -> 0.18.1.3

--- pkgs/applications/blockchains/particl/particl-core.nix
+++ pkgs/applications/blockchains/particl/particl-core.nix
@@ -1,51 +1,47 @@
-{ stdenv
-, autoreconfHook
-, boost
-, db48
-, fetchurl
-, libevent
-, miniupnpc
-, openssl
-, pkgconfig
-, zeromq
-, zlib
-, unixtools
-, python3
-}:
+{ stdenv, fetchFromGitHub, autoreconfHook, boost, db48, fetchurl, libevent
+, miniupnpc, openssl, pkgconfig, zeromq, zlib, unixtools, python3 }:
 
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
   pname = "particl-core";
-  version = "0.17.1.2";
 
-  src = fetchurl {
-    url = "https://github.com/particl/particl-core/archive/v${version}.tar.gz";
-    sha256 = "16hcyxwp6yrypwvxz6i2987z3jmpk47xcgnsgh9klih8baqg64p5";
+  version = "0.18.1.3";
+
+  src = fetchFromGitHub {
+    owner = "particl";
+    repo = "particl-core";
+    rev = "v${version}";
+    sha256 = "1djfg5zhf9c0pvfybj56rq3rl9n41b9mmdsq7znrxh2zjkkiqdwy";
   };
 
   nativeBuildInputs = [ pkgconfig autoreconfHook ];
-  buildInputs = [ openssl db48 boost zlib miniupnpc libevent zeromq unixtools.hexdump python3 ];
 
-  configureFlags = [
-    "--disable-bench"
-    "--with-boost-libdir=${boost.out}/lib"
-  ] ++ optionals (!doCheck) [
-    "--enable-tests=no"
-  ];
+  buildInputs =
+    [ openssl db48 boost zlib miniupnpc libevent zeromq unixtools.hexdump ];
+
+  configureFlags = [ "--disable-bench" "--with-boost-libdir=${boost.out}/lib" ]
+    ++ optionals (!doCheck) [ "--enable-tests=no" ];
+
+  checkInputs = [ python3 ];
 
   # Always check during Hydra builds
   doCheck = true;
+
+  checkFlags = [ "LC_ALL=C.UTF-8" ];
+
   preCheck = "patchShebangs test";
+
   enableParallelBuilding = true;
 
   meta = {
-    description = "Privacy-Focused Marketplace & Decentralized Application Platform";
-    longDescription= ''
+    description =
+      "Privacy-Focused Marketplace & Decentralized Application Platform";
+    longDescription = ''
       An open source, decentralized privacy platform built for global person to person eCommerce.
       RPC daemon and CLI client only.
     '';
-    homepage = https://particl.io/;
+    homepage = "https://particl.io/";
     maintainers = with maintainers; [ demyanrogozhin ];
     license = licenses.mit;
     platforms = platforms.unix;
