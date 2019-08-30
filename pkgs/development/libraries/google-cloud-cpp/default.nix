4695468d06a37dce2e6d028c9a70da02ebfd62be
Andreas Rammhold <andreas@rammhold.de>
2019-08-30 15:55:59 +0200

    google-cloud-cpp: 0.11.0 -> 0.12.0

--- pkgs/development/libraries/google-cloud-cpp/default.nix
+++ pkgs/development/libraries/google-cloud-cpp/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, grpc, curl, cmake, pkgconfig, fetchFromGitHub, doxygen, protobuf, crc32c, c-ares, nlohmann_json, fetchurl }:
+{ stdenv, grpc, curl, cmake, pkgconfig, fetchFromGitHub, doxygen, protobuf, crc32c, c-ares, nlohmann_json, fetchurl, runCommand, gnutar }:
 let
   googleapis_rev = "a8ee1416f4c588f2ab92da72e7c1f588c784d3e6";
   googleapis = fetchurl {
@@ -6,15 +6,31 @@ let
     url = "https://github.com/googleapis/googleapis/archive/${googleapis_rev}.tar.gz";
     sha256 = "1kxi27r034p7jfldhvgpbn6rqqqddycnja47m6jyjxj4rcmrp2kb";
   };
+
+  googleapis-cpp-cmakefiles-rev = "v0.1.1";
+  googleapis-cpp-cmakefiles = fetchurl {
+    name = "${googleapis-cpp-cmakefiles-rev}.tar.gz";
+    url = "https://github.com/googleapis/cpp-cmakefiles/archive/${googleapis-cpp-cmakefiles-rev}.tar.gz";
+    sha256 = "e8143e9132a57bd868f3014c0bc382205885c53a2a425b3c2dbb8f9feaa8366c";
+  };
+
+  patched-googleapis-cpp-cmakefiles = runCommand "googleapis-cpp-cmakefiles.tar.gz" {
+    buildInputs = [ gnutar ];
+  } ''
+    tar xf ${googleapis-cpp-cmakefiles}
+    sed -e 's,https://github.com/googleapis/googleapis/archive/${googleapis_rev}.tar.gz,file://${googleapis},' \
+        -i cpp-cmakefiles-*/CMakeLists.txt
+    tar czf $out cpp-cmakefiles*/
+  '';
 in stdenv.mkDerivation rec {
   pname = "google-cloud-cpp";
-  version = "0.11.0";
+  version = "0.12.0";
 
   src = fetchFromGitHub {
     owner = "googleapis";
     repo = "google-cloud-cpp";
     rev = "v${version}";
-    sha256 = "1w942gzyv01ym1cv2a417x92zxra9s2v3xz5crcv84j919f616f8";
+    sha256 = "1r651xy3mc834ia7v8kks9vij02yl8p3w34qwd9qiaggz6ffyj8f";
   };
 
   buildInputs = [ curl grpc protobuf nlohmann_json crc32c c-ares c-ares.cmake-config ];
@@ -28,7 +44,11 @@ in stdenv.mkDerivation rec {
         -e "s,JSON_SHA256 .*,JSON_SHA256 ''${NLOHMANN_SHA256}," \
         -i cmake/DownloadNlohmannJson.cmake
 
-    sed -e 's,https://github.com/googleapis/googleapis/archive/${googleapis_rev}.tar.gz,file://${googleapis},' \
+		GOOGLEAPIS_CPP_CMAKEFILES_SHA256=$(sha256sum ${patched-googleapis-cpp-cmakefiles} | cut -f1 -d' ')
+
+    # just here to make ${googleapis} available during runtime…
+    sed -e 's,https://github.com/googleapis/cpp-cmakefiles/archive/${googleapis-cpp-cmakefiles-rev}.tar.gz,file://${patched-googleapis-cpp-cmakefiles},' \
+        -e "s,${googleapis-cpp-cmakefiles.outputHash},''${GOOGLEAPIS_CPP_CMAKEFILES_SHA256}," \
         -i cmake/external/googleapis.cmake
 
     # Fixup the library path. It would build a path like /build/external//nix/store/…-foo/lib/foo.so for each library instead of /build/external/lib64/foo.so
