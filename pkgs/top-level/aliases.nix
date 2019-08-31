c13c90393437a6d4375de5b23722e6222fe7404d
Izorkin <izorkin@elven.pw>
2019-08-31 14:07:03 +0300

    mariadb-connector-c: remove libmysql alias

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -187,7 +187,6 @@ mapAliases ({
   libintlOrEmpty = stdenv.lib.optional (!stdenv.isLinux || stdenv.hostPlatform.libc != "glibc") gettext; # added 2018-03-14
   libjson_rpc_cpp = libjson-rpc-cpp; # added 2017-02-28
   liblapackWithoutAtlas = liblapack; # added 2018-11-05
-  libmysql = mariadb-connector-c; # added # 2017-12-28, this was a misnomer refering to libmysqlclient
   libqrencode = qrencode;  # added 2019-01-01
   librecad2 = librecad;  # backwards compatibility alias, added 2015-10
   libsysfs = sysfsutils; # added 2018-04-25
