7dc02aa1852db3530ba22113f0383627f8bb32a7
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-09-02 16:44:27 +0000

    gnome3.geary: 3.32.2 -> 3.34.0

--- pkgs/desktops/gnome-3/misc/geary/default.nix
+++ pkgs/desktops/gnome-3/misc/geary/default.nix
@@ -1,28 +1,30 @@
 { stdenv, fetchurl, pkgconfig, gtk3, vala, enchant2, wrapGAppsHook, meson, ninja
 , desktop-file-utils, gnome-online-accounts, gsettings-desktop-schemas, adwaita-icon-theme
-, libnotify, libcanberra-gtk3, libsecret, gmime, isocodes, libxml2, gettext
+, libcanberra-gtk3, libsecret, gmime, isocodes, libxml2, gettext
 , sqlite, gcr, json-glib, itstool, libgee, gnome3, webkitgtk, python3
-, xvfb_run, dbus, shared-mime-info, libunwind, libunity, folks, glib-networking }:
+, xvfb_run, dbus, shared-mime-info, libunwind, libunity, folks, glib-networking
+, gobject-introspection, gspell, appstream-glib, libytnef, libhandy }:
 
 stdenv.mkDerivation rec {
   pname = "geary";
-  version = "3.32.2";
+  version = "3.34.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0sg53zq81v28hdqiy5d048skwfgsa8ck0z7ywsagdh7iaqin68gq";
+    sha256 = "1cxpyjgfrai423vwqc6jv4aqadxkd3ghh2had71drilyywy5bx64";
   };
 
   nativeBuildInputs = [
     desktop-file-utils gettext itstool libxml2 meson ninja
-    pkgconfig vala wrapGAppsHook python3
+    pkgconfig vala wrapGAppsHook python3 appstream-glib
+    gobject-introspection
   ];
 
   buildInputs = [
     adwaita-icon-theme enchant2 gcr gmime gnome-online-accounts
     gsettings-desktop-schemas gtk3 isocodes json-glib libcanberra-gtk3
-    libgee libnotify libsecret sqlite webkitgtk glib-networking
-    libunwind libunity folks
+    libgee libsecret sqlite webkitgtk glib-networking
+    libunwind libunity folks gspell libytnef libhandy
   ];
 
   checkInputs = [ xvfb_run dbus ];
@@ -32,8 +34,8 @@ stdenv.mkDerivation rec {
   ];
 
   postPatch = ''
-    chmod +x build-aux/post_install.py
-    patchShebangs build-aux/post_install.py
+    chmod +x build-aux/post_install.py build-aux/git_version.py
+    patchShebangs build-aux/post_install.py build-aux/git_version.py
 
     chmod +x desktop/geary-attach
   '';
