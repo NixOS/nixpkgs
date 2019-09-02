02bef3c21b6b8aabbb6bfa62225f29eea1156f48
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-09-02 16:45:35 +0000

    gnome3.iagno: 3.32.0 → 3.34.0

--- pkgs/desktops/gnome-3/games/iagno/default.nix
+++ pkgs/desktops/gnome-3/games/iagno/default.nix
@@ -1,22 +1,22 @@
 { stdenv, fetchurl, pkgconfig, gtk3, gnome3, gdk-pixbuf, librsvg, wrapGAppsHook
-, itstool, libcanberra-gtk3, libxml2
+, itstool, gsound, libxml2
 , meson, ninja, python3, vala, desktop-file-utils
 }:
 
 stdenv.mkDerivation rec {
   pname = "iagno";
-  version = "3.32.0";
+  version = "3.34.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/iagno/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "1rcqb4gpam16xw87n4q2akkrg94ksrn16ry21pr6bsd7qs7hw17d";
+    sha256 = "15zan1h63hm9z0bf3y5h7rqd935jjw672qh88waqzwp6mlv1y321";
   };
 
   nativeBuildInputs = [
     meson ninja python3 vala desktop-file-utils
     pkgconfig wrapGAppsHook itstool libxml2
   ];
-  buildInputs = [ gtk3 gnome3.adwaita-icon-theme gdk-pixbuf librsvg libcanberra-gtk3 ];
+  buildInputs = [ gtk3 gnome3.adwaita-icon-theme gdk-pixbuf librsvg gsound ];
 
   enableParallelBuilding = true;
 
