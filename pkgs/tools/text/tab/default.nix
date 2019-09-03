37d038eb2589935a50246cec124206b608c0f418
Maciek Starzyk <mstarzyk@gmail.com>
2019-09-03 22:24:45 +0200

    tab: cleanup

--- pkgs/tools/text/tab/default.nix
+++ pkgs/tools/text/tab/default.nix
@@ -3,24 +3,27 @@
 stdenv.mkDerivation rec {
   version = "7.1";
   pname = "tab";
-  name = "${pname}-${version}";
 
   src = fetchFromBitbucket {
     owner = "tkatchev";
-    repo = "${pname}";
-    rev = "${version}";
+    repo = pname;
+    rev = version;
     sha256 = "049wx24b4v8alhvc05z7snyzaf6fk9w2npc4kklp366yjl1l4qbb";
   };
 
-  buildInputs = [ python2 ];
+  nativeBuildInputs = [ python2 ];
 
   doCheck = true;
 
-  checkPhase = ''make test'';
+  checkTarget = "test";
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/bin
     cp tab $out/bin
+
+    runHook postInstall
   '';
 
   meta = with stdenv.lib; {
