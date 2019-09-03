6e9e9f6864908a9575074daaf5b70b47bad3d5ee
Alyssa Ross <hi@alyssa.is>
2019-09-03 21:16:28 +0000

    sonic-pi: don't use EOL Ruby
    This was added to fix a compiler error in
    4bc516aa540e85ff8e0fa2c88245e4c870b1faf3, where it was noted that it
    still segfaulted after boot. Since it compiles with our default Ruby
    now, I have to assume that the problem the override was added to set
    has since been resolved. I don't know how to actually test, however.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20489,9 +20489,7 @@ in
 
   wavebox = callPackage ../applications/networking/instant-messengers/wavebox { };
 
-  sonic-pi = callPackage ../applications/audio/sonic-pi {
-    ruby = ruby_2_3;
-  };
+  sonic-pi = callPackage ../applications/audio/sonic-pi { };
 
   st = callPackage ../applications/misc/st {
     conf = config.st.conf or null;
