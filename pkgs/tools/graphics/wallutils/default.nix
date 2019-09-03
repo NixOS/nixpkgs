3b7946d0aedd58e2883f5a14f9050f9aa030e90c
Michael Weiss <dev.primeos@gmail.com>
2019-09-03 11:59:43 +0200

    wallutils: 5.8.1 -> 5.8.2

--- pkgs/tools/graphics/wallutils/default.nix
+++ pkgs/tools/graphics/wallutils/default.nix
@@ -1,19 +1,19 @@
 { buildGoModule, fetchFromGitHub, lib
-, wayland, libX11, xbitmaps, libXcursor, libXmu
+, wayland, libX11, xbitmaps, libXcursor, libXmu, libXpm
 }:
 
 buildGoModule rec {
   pname = "wallutils";
-  version = "5.8.1";
+  version = "5.8.2";
 
   src = fetchFromGitHub {
     owner = "xyproto";
     repo = "wallutils";
     rev = version;
-    sha256 = "095pgvk4yp2l6xgl63qp61rr2dij51awndwrs5ha9vpdd1jqgvfi";
+    sha256 = "1ghvcxsy5prj8l38r4lg39imsqbwmvn1zmiv7004j6skmgpaaawh";
   };
 
-  modSha256 = "1kbggry1qrf0nkvysnaky2nl73l5f0bnc4wx0hfr6ifyagfjzy77";
+  modSha256 = "0siw1g3fsk1xjri9k1pb03filax8an5sfza5db52krh80g9xasah";
 
   patches = [ ./lscollection-Add-NixOS-paths-to-DefaultWallpaperDirectories.patch ];
 
@@ -22,7 +22,7 @@ buildGoModule rec {
     sed -iE 's/VersionString = "[0-9].[0-9].[0-9]"/VersionString = "${version}"/' wallutils.go
   '';
 
-  buildInputs = [ wayland libX11 xbitmaps libXcursor libXmu ];
+  buildInputs = [ wayland libX11 xbitmaps libXcursor libXmu libXpm ];
 
   meta = with lib; {
     description = "Utilities for handling monitors, resolutions, and (timed) wallpapers";
