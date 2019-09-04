9bd1ad095ecc45bde85c06413a354b67586333e5
Roland Freikamp <rk@simple-is-better.org>
2019-09-04 19:57:46 +0200

    litmus: init at 0.13

---
+++ pkgs/tools/networking/litmus/default.nix
@@ -0,0 +1,27 @@
+{ stdenv, fetchurl, openssl }:
+
+stdenv.mkDerivation rec {
+  version = "0.13";
+  name = "litmus-${version}";
+
+  src = fetchurl {
+    url = "http://webdav.org/neon/litmus/${name}.tar.gz";
+    sha256 = "09d615958121706444db67e09c40df5f753ccf1fa14846fdeb439298aa9ac3ff";
+  };
+
+  buildInputs = [ openssl ];
+  configureFlags = [ "--with-ssl" ];
+  installFlags = [ "PREFIX=$(out)" ];
+
+  meta = with stdenv.lib; {
+    description = "WebDAV server protocol compliance test suite";
+    long_description = ''
+      litmus is a WebDAV server test suite, which aims to test whether a server
+      is compliant with the WebDAV protocol as specified in RFC2518.
+    '';
+    homepage = "http://webdav.org/neon/litmus/";
+    license = [ lib.licenses.gpl2 lib.licenses.lgpl2 ];
+    platforms = platforms.unix;
+    maintainers = [ maintainers.rkoe ];
+  };
+}
