2817e90de240e888d6690e0a885757f4ff3b84df
Roman Volosatovs <rvolosatovs@riseup.net>
2019-09-04 14:46:45 +0200

    go_1_13: init at 1.13.1
    (cherry picked from commit bd023200a94d6243a59c040dccb1aebd42f74646)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7896,6 +7896,10 @@ in
     inherit (darwin.apple_sdk.frameworks) Security Foundation;
   };
 
+  go_1_13 = callPackage ../development/compilers/go/1.13.nix {
+    inherit (darwin.apple_sdk.frameworks) Security Foundation;
+  };
+
   go = go_1_12;
 
   go-repo-root = callPackage ../development/tools/go-repo-root { };
