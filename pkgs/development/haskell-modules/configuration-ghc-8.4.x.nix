4037a43630da4306be03d31bd86359c8713c02dc
Michael Alan Dorman <mdorman@ironicdesign.com>
2019-09-05 15:20:03 +0300

    jailbreak-cabal: updated package needs updated Cabal library
    Only needs to be overridden for prior GHC releases.

--- pkgs/development/haskell-modules/configuration-ghc-8.4.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.4.x.nix
@@ -42,6 +42,7 @@ self: super: {
 
   # Needs Cabal 3.0.x.
   cabal-install = super.cabal-install.overrideScope (self: super: { Cabal = self.Cabal_3_0_0_0; });
+  jailbreak-cabal = super.jailbreak-cabal.override { Cabal = self.Cabal_3_0_0_0; };
 
   # Restricts aeson to <1.4
   # https://github.com/purescript/purescript/pull/3537
