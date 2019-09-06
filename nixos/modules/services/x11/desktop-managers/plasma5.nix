466f5e534688049be2ed6e75ae5659633016b45e
worldofpeace <worldofpeace@protonmail.ch>
2019-09-06 19:18:52 -0400

    nixos/plasma5: enable libinput

--- nixos/modules/services/x11/desktop-managers/plasma5.nix
+++ nixos/modules/services/x11/desktop-managers/plasma5.nix
@@ -211,6 +211,7 @@ in
       services.udisks2.enable = true;
       services.upower.enable = config.powerManagement.enable;
       services.system-config-printer.enable = (mkIf config.services.printing.enable (mkDefault true));
+      services.xserver.libinput.enable = mkDefault true;
 
       # Extra UDEV rules used by Solid
       services.udev.packages = [
