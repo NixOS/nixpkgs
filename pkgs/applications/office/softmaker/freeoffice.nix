accc62a98c8b1728af748057c8ac97b02edef8b9
Daniël de Kok <me@danieldk.eu>
2019-09-06 16:33:02 +0200

    freeoffice: 966 -> 970
    Changes:
    
    - Adds support for saving documents to the DOC, XLS, and PPT formats.
    - TextMaker adds support for saving to the OpenDocument format.
    - Adds a dark mode.

--- pkgs/applications/office/softmaker/freeoffice.nix
+++ pkgs/applications/office/softmaker/freeoffice.nix
@@ -2,13 +2,13 @@
 
 callPackage ./generic.nix (args // rec {
   pname = "freeoffice";
-  version = "966";
+  version = "970";
   edition = "2018";
   suiteName = "FreeOffice";
 
   src = fetchurl {
     url = "https://www.softmaker.net/down/softmaker-freeoffice-${version}-amd64.tgz";
-    sha256 = "18s92d2pmhmd56sb9c1zirwxzfgpqd8wh11bl0zi6g6skn68zhx4";
+    sha256 = "1maibr4x8mksb32ixvyy2rjn4x9f51191p5fcdj5qwz32pf8h2dr";
   };
 
   archive = "freeoffice${edition}.tar.lzma";
