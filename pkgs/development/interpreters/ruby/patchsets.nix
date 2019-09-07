ca96b55d3a4a6b671ae841f1ad416f80f065bd63
Alyssa Ross <hi@alyssa.is>
2019-09-07 13:02:40 +0000

    ruby_2_6: patch use-after-free
    See https://bugs.ruby-lang.org/issues/16136.
    
    This patch is on Ruby trunk, but hasn't been backported (yet?).

--- pkgs/development/interpreters/ruby/patchsets.nix
+++ pkgs/development/interpreters/ruby/patchsets.nix
@@ -1,4 +1,4 @@
-{ patchSet, useRailsExpress, ops, patchLevel }:
+{ patchSet, useRailsExpress, ops, patchLevel, fetchpatch }:
 
 {
   "2.3.8" = ops useRailsExpress [
@@ -16,7 +16,12 @@
     "${patchSet}/patches/ruby/2.5/head/railsexpress/02-improve-gc-stats.patch"
     "${patchSet}/patches/ruby/2.5/head/railsexpress/03-more-detailed-stacktrace.patch"
   ];
-  "2.6.4" = ops useRailsExpress [
+  "2.6.4" = [
+    (fetchpatch {
+      url = "https://git.ruby-lang.org/ruby.git/patch/?id=ade1283ca276f7d589ffd3539fbc7b9817f682d5";
+      sha256 = "1vgrckmzz0ykyxgzyp8fcifa93xz2hvyfil79bw1gc3xx94wnnxd";
+    })
+  ] ++ ops useRailsExpress [
     "${patchSet}/patches/ruby/2.6/head/railsexpress/01-fix-broken-tests-caused-by-ad.patch"
     "${patchSet}/patches/ruby/2.6/head/railsexpress/02-improve-gc-stats.patch"
     "${patchSet}/patches/ruby/2.6/head/railsexpress/03-more-detailed-stacktrace.patch"
