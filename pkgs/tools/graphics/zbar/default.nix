41fbd5568913fbbdc6e0b608d78bd14f0e4c1150
Jan Tojnar <jtojnar@gmail.com>
2019-09-09 12:07:46 +0200

    zbar: format with nixpkgs-fmt

--- pkgs/tools/graphics/zbar/default.nix
+++ pkgs/tools/graphics/zbar/default.nix
@@ -1,12 +1,26 @@
-{ stdenv, fetchFromGitHub, imagemagickBig, pkgconfig, python2Packages, perl
-, libX11, libv4l, qt5, gtk2, xmlto, docbook_xsl, autoreconfHook, dbus
-, enableVideo ? stdenv.isLinux, enableDbus ? stdenv.isLinux
+{ stdenv
+, lib
+, fetchFromGitHub
+, imagemagickBig
+, pkgconfig
+, python2Packages
+, perl
+, libX11
+, libv4l
+, qt5
+, gtk2
+, xmlto
+, docbook_xsl
+, autoreconfHook
+, dbus
+, enableVideo ? stdenv.isLinux
+, enableDbus ? stdenv.isLinux
 }:
 
-with stdenv.lib;
 let
   inherit (python2Packages) pygtk python;
-in stdenv.mkDerivation rec {
+in
+stdenv.mkDerivation rec {
   pname = "zbar";
   version = "0.23";
 
@@ -17,27 +31,43 @@ in stdenv.mkDerivation rec {
     sha256 = "0hlxakpyjg4q9hp7yp3har1n78341b4knwyll28hn48vykg28pza";
   };
 
-  nativeBuildInputs = [ pkgconfig xmlto autoreconfHook docbook_xsl ];
+  nativeBuildInputs = [
+    pkgconfig
+    xmlto
+    autoreconfHook
+    docbook_xsl
+  ];
 
   buildInputs = [
-    imagemagickBig python pygtk perl libX11
-  ] ++ optional enableDbus dbus
-  ++ optionals enableVideo [
-    libv4l gtk2 qt5.qtbase qt5.qtx11extras
+    imagemagickBig
+    python
+    pygtk
+    perl
+    libX11
+  ] ++ lib.optionals enableDbus [
+    dbus
+  ] ++ lib.optionals enableVideo [
+    libv4l
+    gtk2
+    qt5.qtbase
+    qt5.qtx11extras
   ];
 
   configureFlags = (if enableDbus then [
-    "--with-dbusconfdir=$out/etc/dbus-1/system.d"
-  ] else [ "--without-dbus" ])
-  ++ optionals (!enableVideo) [
-    "--disable-video" "--without-gtk" "--without-qt"
+    "--with-dbusconfdir=${placeholder "out"}/etc/dbus-1/system.d"
+  ] else [
+    "--without-dbus"
+  ]) ++ lib.optionals (!enableVideo) [
+    "--disable-video"
+    "--without-gtk"
+    "--without-qt"
   ];
 
   postInstall = optionalString enableDbus ''
     install -Dm644 dbus/org.linuxtv.Zbar.conf $out/etc/dbus-1/system.d/org.linuxtv.Zbar.conf
   '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Bar code reader";
     longDescription = ''
       ZBar is an open source software suite for reading bar codes from various
