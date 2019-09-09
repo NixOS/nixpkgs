6255a4b8fdf0465161e5bf091bc621eee3f6bfeb
Tim Steinbach <tim@nequissimus.com>
2019-09-09 09:22:27 -0400

    linux: 5.3-rc7 -> 5.3-rc8

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.3-rc7";
+  version = "5.3-rc8";
   extraMeta.branch = "5.3";
 
   # modDirVersion needs to be x.y.z, will always add .0
@@ -11,7 +11,7 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "1cjiy4sd681k6c75kjwldp4h7nqr9if668h3lcg09qyd80cqbywp";
+    sha256 = "01pr8xb9akjzafl8zkpwwkmlsjxghv5bx0larkjqdakjfspqnhzj";
   };
 
   # Should the testing kernels ever be built on Hydra?
