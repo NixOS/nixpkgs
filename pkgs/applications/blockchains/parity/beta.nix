5a1df82bc57a188a24015218bffe42c19ff8e20c
xrelkd <46590321+xrelkd@users.noreply.github.com>
2019-09-09 11:39:14 +0800

    parity-beta: update cargoSha256 hash

--- pkgs/applications/blockchains/parity/beta.nix
+++ pkgs/applications/blockchains/parity/beta.nix
@@ -1,6 +1,6 @@
 let
   version     = "2.6.2";
   sha256      = "1j4249m5k3bi7di0wq6fm64zv3nlpgmg4hr5hnn94fyc09nz9n1r";
-  cargoSha256 = "18zd91n04wck3gd8szj4vxn3jq0bzq0h3rg0wcs6nzacbzhcx2sw";
+  cargoSha256 = "1wr0i54zc3l6n0x6cvlq9zfy3bw9w5fcvdz4vmyym9r1nkvk31s7";
 in
   import ./parity.nix { inherit version sha256 cargoSha256; }
