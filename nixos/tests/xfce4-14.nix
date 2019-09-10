4bacee3cb2f6446f47279ea3038c35cab6219506
worldofpeace <worldofpeace@protonmail.ch>
2019-09-09 22:13:06 -0400

    nixosTests.xfce4-14: fix test by enabling sound
    Same issue as f59b4cb8d545d3bb1bd954f9e3267cb7ebec3557
    
    (cherry picked from commit 17877eaa682407e613429e97cbf28f0297fb2e7d)

--- nixos/tests/xfce4-14.nix
+++ nixos/tests/xfce4-14.nix
@@ -12,6 +12,8 @@ import ./make-test.nix ({ pkgs, ...} : {
       services.xserver.displayManager.auto.user = "alice";
 
       services.xserver.desktopManager.xfce4-14.enable = true;
+
+      hardware.pulseaudio.enable = true; # needed for the factl test, /dev/snd/* exists without them but udev doesn't care then
     };
 
   testScript =
