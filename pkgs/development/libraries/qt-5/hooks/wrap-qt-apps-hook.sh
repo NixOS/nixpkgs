c6d516dfc49c1d84ef03605e0d2b8aa3ea8ae431
Bjørn Forsman <bjorn.forsman@gmail.com>
2019-09-10 08:08:01 +0200

    wrapQtAppsHook: use `patchelf --print-interpreter` instead of `isELFExec`
    Some executables are built as PIEs (e.g. keepassxc) and are technically
    isELFDyn, not isELFExec. Without this change those executables will not
    be wrapped.

--- pkgs/development/libraries/qt-5/hooks/wrap-qt-apps-hook.sh
+++ pkgs/development/libraries/qt-5/hooks/wrap-qt-apps-hook.sh
@@ -82,7 +82,7 @@ wrapQtAppsHook() {
 
         find "$targetDir" -executable -print0 | while IFS= read -r -d '' file
         do
-            isELFExec "$file" || continue
+            patchelf --print-interpreter "$file" >/dev/null 2>&1 || continue
 
             if [ -f "$file" ]
             then
