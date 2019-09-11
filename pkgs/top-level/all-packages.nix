7855afeef7eda3b4d63815b5e927038dc1676e50
Nikolay Korotkiy <sikmir@gmail.com>
2019-09-11 23:20:31 +0300

    mbutil: init at 0.3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5005,6 +5005,8 @@ in
     boost = boost155;
   };
 
+  mbutil = python3Packages.callPackage ../applications/misc/mbutil { };
+
   mc = callPackage ../tools/misc/mc { };
 
   mcabber = callPackage ../applications/networking/instant-messengers/mcabber { };
