5a51a07ab6a6c58b37268366763b98577e9c91bf
eyjhbb@gmail.com <eyjhbb@gmail.com>
2019-09-11 20:07:07 +0200

    modules.unifi added web/gui port to openPorts

--- nixos/modules/services/networking/unifi.nix
+++ nixos/modules/services/networking/unifi.nix
@@ -124,6 +124,7 @@ in
       # https://help.ubnt.com/hc/en-us/articles/218506997
       allowedTCPPorts = [
         8080  # Port for UAP to inform controller.
+        8443  # Port for controller GUI/API as seen in a web browser
         8880  # Port for HTTP portal redirect, if guest portal is enabled.
         8843  # Port for HTTPS portal redirect, ditto.
         6789  # Port for UniFi mobile speed test.
