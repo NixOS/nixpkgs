00609481409f9c6b05c2d27b4f105c5624a366a0
Nils ANDRÉ-CHANG <nils@nilsand.re>
2019-09-12 17:40:22 +0100

    Remove newline in PS1

--- nixos/modules/programs/bash/bash.nix
+++ nixos/modules/programs/bash/bash.nix
@@ -100,9 +100,9 @@ in
             let $UID && PROMPT_COLOR="1;32m"
             if [ -n "$INSIDE_EMACS" -o "$TERM" == "eterm" -o "$TERM" == "eterm-color" ]; then
               # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
-              PS1="\n\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
+              PS1="\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
             else
-              PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
+              PS1="\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
             fi
             if test "$TERM" = "xterm"; then
               PS1="\[\033]2;\h:\u:\w\007\]$PS1"
