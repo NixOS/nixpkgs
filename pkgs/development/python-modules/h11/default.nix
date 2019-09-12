97982cdad8a1140861bba1fed988b91b892eed51
Dima <dgoldin+github@protonmail.ch>
2019-09-12 23:14:01 +0200

    python37Packages.h11: fixing build
    Fixes a broken test-case with more recent pytest versions.
    See https://github.com/python-hyper/h11/issues/86

--- pkgs/development/python-modules/h11/default.nix
+++ pkgs/development/python-modules/h11/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, fetchPypi, pytest }:
+{ lib, buildPythonPackage, fetchPypi, fetchpatch, pytest }:
 
 buildPythonPackage rec {
   pname = "h11";
@@ -10,6 +10,16 @@ buildPythonPackage rec {
   };
 
   checkInputs = [ pytest ];
+
+  patches = [
+    # This fixes a broken test case in 0.9.0 with newer versions of pytest,
+    # when this package version is bumped to >0.9.0 it can likely be dropped.
+    # https://github.com/python-hyper/h11/issues/86
+    ( fetchpatch {
+      url = "https://github.com/python-hyper/h11/commit/cc0c63ed9b67f4ee71f087406008be3443712f6c.patch";
+      sha256 = "05ff4fp89xl62ip16w7f4dcw0grvkwyq5d8wzlknsahbxy5f7a8h";
+    })
+  ];
 
   checkPhase = ''
     py.test
