1c219f178e4792ebe4c885b5be4ba255e3166b88
Aaron Andersen <aaron@fosslib.net>
2019-09-12 12:20:32 -0400

    libzip: 1.3.0 -> 1.5.2

--- pkgs/development/libraries/libzip/default.nix
+++ pkgs/development/libraries/libzip/default.nix
@@ -1,35 +1,26 @@
-{ stdenv, fetchurl, perl, zlib }:
+{ stdenv, fetchurl, cmake, perl, zlib }:
 
 stdenv.mkDerivation rec {
   pname = "libzip";
-  version = "1.3.0";
+  version = "1.5.2";
 
   src = fetchurl {
     url = "https://www.nih.at/libzip/${pname}-${version}.tar.gz";
-    sha256 = "1633dvjc08zwwhzqhnv62rjf1abx8y5njmm8y16ik9iwd07ka6d9";
+    sha256 = "05ay8cbm882br0ir2cmzrvdq8q5mr1bnf53l4305xzigpd54lsdy";
   };
 
   postPatch = ''
-    patchShebangs test-driver
     patchShebangs man/handle_links
   '';
 
   outputs = [ "out" "dev" ];
 
-  nativeBuildInputs = [ perl ];
+  nativeBuildInputs = [ cmake perl ];
   propagatedBuildInputs = [ zlib ];
 
   preCheck = ''
     # regress/runtests is a generated file
     patchShebangs regress
-  '';
-
-  # At least mysqlWorkbench cannot find zipconf.h; I think also openoffice
-  # had this same problem.  This links it somewhere that mysqlworkbench looks.
-  postInstall = ''
-    mkdir -p $dev/lib
-    mv $out/lib/libzip $dev/lib/libzip
-    ( cd $dev/include ; ln -s ../lib/libzip/include/zipconf.h zipconf.h )
   '';
 
   meta = with stdenv.lib; {
