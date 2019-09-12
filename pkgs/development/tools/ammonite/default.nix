dad77d59cb6aaaee503cb3d3469acc6db29dddf4
Tim Steinbach <tim@nequissimus.com>
2019-09-12 08:06:57 -0400

    ammonite: 1.6.9 -> 1.7.1

--- pkgs/development/tools/ammonite/default.nix
+++ pkgs/development/tools/ammonite/default.nix
@@ -5,12 +5,12 @@
 with stdenv.lib;
 stdenv.mkDerivation rec {
   pname = "ammonite";
-  version = "1.6.9";
+  version = "1.7.1";
   scalaVersion = "2.12";
 
   src = fetchurl {
     url = "https://github.com/lihaoyi/Ammonite/releases/download/${version}/${scalaVersion}-${version}";
-    sha256 = "1fi5j0kcndq00x72d8bkx6qiy9nh2i6c6m29gzfqql52qgbq1fd0";
+    sha256 = "0isg9flnzc2ldbx6q0lg0xyg9lnvqkd8qhgfln8p1hbzdq0n6jd0";
   };
 
   propagatedBuildInputs = [ jre ] ;
