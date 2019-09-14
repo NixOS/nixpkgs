90b8ac0ff37205f868bd176583aa8a8a265134ec
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-09-14 02:45:42 +0200

    vte-ng: fix build
    vte now uses meson so don't bother overriding vte.

--- pkgs/development/libraries/vte/ng.nix
+++ pkgs/development/libraries/vte/ng.nix
@@ -1,6 +1,10 @@
-{ vte, fetchFromGitHub, fetchpatch, autoconf, automake, gtk-doc, gettext, libtool, gperf }:
+{ vte, fetchFromGitHub, fetchpatch, autoconf, automake, gtk-doc, gettext, libtool, gperf
+, stdenv, fetchurl, intltool, pkgconfig
+, gnome3, glib, gtk3, ncurses, gobject-introspection, vala, libxml2, gnutls
+, pcre2
+}:
 
-vte.overrideAttrs (oldAttrs: rec {
+stdenv.mkDerivation rec {
   name = "vte-ng-${version}";
   version = "0.54.2.a";
 
@@ -20,8 +24,36 @@ vte.overrideAttrs (oldAttrs: rec {
     })
   ];
 
-  preConfigure = oldAttrs.preConfigure + "; NOCONFIGURE=1 ./autogen.sh";
+  nativeBuildInputs = [
+    gtk-doc autoconf automake gettext libtool gperf
+    gobject-introspection intltool pkgconfig vala gperf libxml2
+  ];
+  buildInputs = [ glib gtk3 ncurses ];
 
-  nativeBuildInputs = oldAttrs.nativeBuildInputs or []
-    ++ [ gtk-doc autoconf automake gettext libtool gperf ];
-})
+  propagatedBuildInputs = [
+    # Required by vte-2.91.pc.
+    gtk3
+    gnutls
+    pcre2
+  ];
+
+  preConfigure = "patchShebangs .; NOCONFIGURE=1 ./autogen.sh";
+
+  configureFlags = [ "--enable-introspection" "--disable-Bsymbolic" ];
+
+  meta = with stdenv.lib; {
+    homepage = https://www.gnome.org/;
+    description = "A library implementing a terminal emulator widget for GTK";
+    longDescription = ''
+      VTE is a library (libvte) implementing a terminal emulator widget for
+      GTK, and a minimal sample application (vte) using that.  Vte is
+      mainly used in gnome-terminal, but can also be used to embed a
+      console/terminal in games, editors, IDEs, etc. VTE supports Unicode and
+      character set conversion, as well as emulating any terminal known to
+      the system's terminfo database.
+    '';
+    license = licenses.lgpl2;
+    maintainers = with maintainers; [ astsmtl antono lethalman ];
+    platforms = platforms.unix;
+  };
+}
