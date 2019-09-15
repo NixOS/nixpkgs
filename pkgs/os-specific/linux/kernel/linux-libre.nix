f2ffac1ee4a0b8b449338bcf957c8fe8fe010555
Dima <43349662+d-goldin@users.noreply.github.com>
2019-09-15 19:42:29 +0200

    linux-libre: fixing build / deblobbing (#68844)
    Build was failing because we were depending on tagged versions of
    the deblobbing scripts. The tags are not updated and thus newer
    changes required won't be reflected unless the tag is re-created, which
    might not be reliably the case.
    
    So bumping revision and switching to use the branches to access the
    deblob scripts.
    
    For context, in our case the missing change is:
    
    --- /nix/store/sfc0rrhj5l44zpqgpsymq5750k5wzg8p-tags-r16790/4.19-gnu/deblob-4.19	1970-01-01 01:00:01.000000000 +0100
    +++ ../deblob-4.19	2019-09-14 14:53:44.637404289 +0200
    @@ -1879,7 +1879,11 @@
    
     announce BRCMFMAC - "Broadcom IEEE802.11n embedded FullMAC WLAN driver"
     reject_firmware drivers/net/wireless/broadcom/brcm80211/brcmfmac/firmware.c
    -reject_firmware drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
    +if grep -q firmware_request_nowarn drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c; then
    +  reject_firmware_nowarn drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
    +else
    +  reject_firmware drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
    +fi
     clean_blob drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.c
     clean_blob drivers/net/wireless/broadcom/brcm80211/brcmfmac/firmware.h
    
    (cherry picked from commit 2a8f7d71ce88983f14936b76a1d18ef432d65883)

--- pkgs/os-specific/linux/kernel/linux-libre.nix
+++ pkgs/os-specific/linux/kernel/linux-libre.nix
@@ -1,8 +1,11 @@
 { stdenv, lib, fetchsvn, linux
 , scripts ? fetchsvn {
-    url = "https://www.fsfla.org/svn/fsfla/software/linux-libre/releases/tags/";
-    rev = "r15295";
-    sha256 = "03kqbjy7w9zg6ry86h9sxa33z0rblznhba109lwmjwy0wx7yk1cs";
+    url = "https://www.fsfla.org/svn/fsfla/software/linux-libre/releases/branches/";
+
+    # Update this if linux_latest-libre fails to build.
+    # $ curl https://www.fsfla.org/svn/fsfla/software/linux-libre/releases/branches/ | grep -Eo 'Revision [0-9]+'
+    rev = "16791";
+    sha256 = "1lpaka4hs7yrpnrzfybd6radjylwvw2p4aly68pypykqs2srvm7j";
   }
 , ...
 }:
@@ -22,7 +25,7 @@ in linux.override {
       name = "${linux.name}-libre-src";
       src = linux.src;
       buildPhase = ''
-        ${scripts}/${majorMinor}-gnu/deblob-${majorMinor} \
+        ${scripts}/${majorMinor}/deblob-${majorMinor} \
             ${major} ${minor} ${patch}
       '';
       checkPhase = ''
