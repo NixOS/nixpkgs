7a7a80bc463f5f1fdea8739fca937630f3486cc4
talyz <kim.lindberger@gmail.com>
2019-09-16 16:22:18 +0200

    nixos/gitlab: Add gnutar and gzip to gitlab-sidekiq's path
    Tar and gzip are needed when importing GitLab project exports.
    
    (cherry picked from commit aceac9d531b72a49546f743ef96ee515770295c9)

--- nixos/modules/services/misc/gitlab.nix
+++ nixos/modules/services/misc/gitlab.nix
@@ -675,6 +675,10 @@ in {
         openssh
         nodejs
         gnupg
+
+        # Needed for GitLab project imports
+        gnutar
+        gzip
       ];
       serviceConfig = {
         Type = "simple";
