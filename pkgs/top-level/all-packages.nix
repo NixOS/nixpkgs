01721f144968b898af74228dd1bcc52462d6e464
Roman Volosatovs <rvolosatovs@riseup.net>
2019-09-16 22:43:58 +0200

    all-packages: use go 1.12 for bazel-watcher

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9283,7 +9283,9 @@ in
 
   bazel-remote = callPackage ../development/tools/build-managers/bazel/bazel-remote { };
 
-  bazel-watcher = callPackage ../development/tools/bazel-watcher { };
+  bazel-watcher = callPackage ../development/tools/bazel-watcher {
+    go = go_1_12;
+  };
 
   bazelisk = callPackage ../development/tools/bazelisk { };
 
