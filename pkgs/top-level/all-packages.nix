72a6ab4d5fb08943720bddffba9ad9a862983e95
Roman Volosatovs <rvolosatovs@riseup.net>
2019-09-16 23:36:02 +0200

    all-packages: use go 1.12 for scaleway-cli

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22523,7 +22523,9 @@ in
 
   sauerbraten = callPackage ../games/sauerbraten {};
 
-  scaleway-cli = callPackage ../tools/admin/scaleway-cli { };
+  scaleway-cli = callPackage ../tools/admin/scaleway-cli {
+    buildGoPackage = buildGo112Package;
+  };
 
   scid = callPackage ../games/scid {
     tcl = tcl-8_5;
