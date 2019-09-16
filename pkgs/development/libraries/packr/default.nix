7f6e616fee2e79526cbe700b3f937c8abe1de2c1
Roman Volosatovs <rvolosatovs@riseup.net>
2019-09-17 00:17:36 +0200

    packr: 2.5.2 -> 2.6.0

--- pkgs/development/libraries/packr/default.nix
+++ pkgs/development/libraries/packr/default.nix
@@ -5,16 +5,18 @@
 
 buildGoModule rec {
   pname = "packr";
-  version = "2.5.2";
+  version = "2.6.0";
 
   src = fetchFromGitHub {
     owner = "gobuffalo";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1ciffa5xbd93fylwz93wr4m4fj83dcla55dmdshaqz28rbsapnc1";
+    sha256 = "11bd0s3hyzvhcg1q0iahv2w9f0w1k57jfxgswhz7dyndxvvr2b8i";
   };
 
-  modSha256 = "086gydrl3i35hawb5m7rsb4a0llcpdpgid1xfw2z9n6jkwkclw4n";
+  subPackages = [ "packr" "v2/packr2" ];
+
+  modSha256 = "0afhkvivma16bi8rz3kwcsz9mhmcn4zm6rrymxkvazx6b844hcdv";
 
   meta = with lib; {
     description = "The simple and easy way to embed static files into Go binaries";
