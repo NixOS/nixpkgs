da04b7e3fdbd1acefba8d24291255d139fc14388
Matthieu Coudron <mattator@gmail.com>
2019-09-18 17:34:02 +0900

    lib.kernel: make public
    Remove the "version" parameter in order to make it more widely
    available.
    Starts making some kernel configuration helpers available.
    The intent is to be able to better build and check the linux kernel
    configuration.
    
    (cherry picked from commit afa0e02d64b15707262f13a348b33dad735bd80f)

--- lib/kernel.nix
+++ lib/kernel.nix
@@ -1,12 +1,7 @@
-{ lib, version }:
+{ lib }:
 
 with lib;
 {
-  # Common patterns/legacy
-  whenAtLeast = ver: mkIf (versionAtLeast version ver);
-  whenOlder   = ver: mkIf (versionOlder version ver);
-  # range is (inclusive, exclusive)
-  whenBetween = verLow: verHigh: mkIf (versionAtLeast version verLow && versionOlder version verHigh);
 
 
   # Keeping these around in case we decide to change this horrible implementation :)
