NixOS/nixpkgs

commit b074a40f74c298d7a6a722733291747bda331618

mdadm: use shared system-sendmail

pkgs/os-specific/linux/mdadm/default.nix
@@ -1,18 +1,5 @@
-{ stdenv, writeScript
-, fetchurl, groff
-}:
+{ stdenv, writeScript, fetchurl, groff, system-sendmail }:
 
-let
-  sendmail-script = writeScript "sendmail-script" ''
-    #!/bin/sh
-
-    if [ -x /run/wrappers/bin/sendmail ]; then
-      /run/wrappers/bin/sendmail "$@"
-    else
-      /run/current-system/sw/bin/sendmail "$@"
-    fi
-  '';
-in
 stdenv.mkDerivation rec {
   name = "mdadm-4.1";
 
@@ -23,7 +10,7 @@ stdenv.mkDerivation rec {
 
   # This is to avoid self-references, which causes the initrd to explode
   # in size and in turn prevents mdraid systems from booting.
-  allowedReferences = [ stdenv.cc.libc.out sendmail-script ];
+  allowedReferences = [ stdenv.cc.libc.out system-sendmail ];
 
   patches = [ ./no-self-references.patch ];
 
@@ -40,7 +27,7 @@ stdenv.mkDerivation rec {
   preConfigure = ''
     sed -e 's@/lib/udev@''${out}/lib/udev@' \
         -e 's@ -Werror @ @' \
-        -e 's@/usr/sbin/sendmail@${sendmail-script}@' -i Makefile
+        -e 's@/usr/sbin/sendmail@${system-sendmail}@' -i Makefile
   '';
 
   meta = with stdenv.lib; {

commit 6b3832a5194feacf4e9708fe94869f3aa7781296

mdadm: fix path to sendmail
Without this, mdadm won't be able to send email notifications:

  $ sudo mdadm --monitor --scan --test
  sh: /nix/store/2v8jn0lxza72grcm6hciak9fpgm7xb3a-system-sendmail-1.0: Is a directory

Fixes: b074a40f74c2 ("mdadm: use shared system-sendmail")

pkgs/os-specific/linux/mdadm/default.nix
@@ -28,7 +28,7 @@ stdenv.mkDerivation rec {
   postPatch = ''
     sed -e 's@/lib/udev@''${out}/lib/udev@' \
         -e 's@ -Werror @ @' \
-        -e 's@/usr/sbin/sendmail@${system-sendmail}@' -i Makefile
+        -e 's@/usr/sbin/sendmail@${system-sendmail}/bin/sendmail@' -i Makefile
     sed -i \
         -e 's@/usr/bin/basename@${coreutils}/bin/basename@g' \
         -e 's@BINDIR/blkid@${utillinux}/bin/blkid@g' \