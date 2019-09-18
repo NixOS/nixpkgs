e9a9b553e7a827ee71b3478280cb0b4731b905e2
Colin L Rice <colin@daedrum.net>
2019-09-17 23:43:25 -0400

    all-packages: use go 1.12 for kubernetes

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19265,7 +19265,9 @@ in
 
   kubeval = callPackage ../applications/networking/cluster/kubeval { };
 
-  kubernetes = callPackage ../applications/networking/cluster/kubernetes { };
+  kubernetes = callPackage ../applications/networking/cluster/kubernetes {
+    go = buildPackages.go_1_12;
+  };
 
   kubectl = callPackage ../applications/networking/cluster/kubectl { };
 
