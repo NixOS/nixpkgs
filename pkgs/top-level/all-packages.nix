4ff0d1e23fe3cd431762a9a3068d964721a23525
worldofpeace <worldofpeace@protonmail.ch>
2019-09-19 00:49:13 -0400

    umockdev: use latest vala

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13931,9 +13931,7 @@ in
 
   uid_wrapper = callPackage ../development/libraries/uid_wrapper { };
 
-  umockdev = callPackage ../development/libraries/umockdev {
-    vala = vala_0_40;
-  };
+  umockdev = callPackage ../development/libraries/umockdev { };
 
   unconvert = callPackage ../development/tools/unconvert { };
 
