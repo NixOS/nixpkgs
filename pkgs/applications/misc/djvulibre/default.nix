ec74a6e47e28949be726c2a2c87ad8960cb78d0e
Anton-Latukha <anton.latukha@gmail.com>
2019-09-20 18:10:32 +0300

    djvulibre: clean-up

--- pkgs/applications/misc/djvulibre/default.nix
+++ pkgs/applications/misc/djvulibre/default.nix
@@ -1,20 +1,32 @@
-{ stdenv, fetchurl, libjpeg, libtiff, librsvg, libiconv }:
+{ stdenv
+, fetchurl
+, libjpeg
+, libtiff
+, librsvg
+, libiconv
+}:
 
 stdenv.mkDerivation rec {
-  name = "djvulibre-3.5.27";
+  pname = "djvulibre";
+  version = "3.5.27";
 
   src = fetchurl {
-    url = "mirror://sourceforge/djvu/${name}.tar.gz";
+    url = "mirror://sourceforge/djvu/${pname}-${version}.tar.gz";
     sha256 = "0psh3zl9dj4n4r3lx25390nx34xz0bg0ql48zdskhq354ljni5p6";
   };
 
   outputs = [ "bin" "dev" "out" ];
 
-  buildInputs = [ libjpeg libtiff librsvg libiconv ];
+  buildInputs = [
+    libjpeg
+    libtiff
+    librsvg
+    libiconv
+  ];
 
   meta = with stdenv.lib; {
     description = "The big set of CLI tools to make/modify/optimize/show/export DJVU files";
-    homepage = http://djvu.sourceforge.net;
+    homepage = "http://djvu.sourceforge.net";
     license = licenses.gpl2;
     maintainers = with maintainers; [ Anton-Latukha ];
     platforms = platforms.all;
