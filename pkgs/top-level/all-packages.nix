b22d36f7683cc318879864f982568d722c80935e
Doron Behar <doron.behar@gmail.com>
2019-09-20 10:55:57 +0300

    onedrive: init at 2.3.9

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12846,6 +12846,8 @@ in
 
   one_gadget = callPackage ../development/tools/misc/one_gadget { };
 
+  onedrive = callPackage ../applications/networking/sync/onedrive { };
+
   oneko = callPackage ../applications/misc/oneko { };
 
   oniguruma = callPackage ../development/libraries/oniguruma { };
