0b88a5c44dfc04bed22e582ba12f44f46234c3af
Tim Steinbach <tim@nequissimus.com>
2019-09-21 20:14:40 -0400

    linux: 4.4.193 -> 4.4.194

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.193";
+  version = "4.4.194";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "13iy0xyrqqagnrk7msp1qfw6xsc0dlc74dpdzki9rfsxcildxz3a";
+    sha256 = "0kvlp2v4nvkilaanhpgwf8dkyfj24msaw0m38rbc4y51y69yhqvz";
   };
 } // (args.argsOverride or {}))
