b397fbc0aa3b9a315e7a1b58ca7f691a24ad2f74
Izorkin <izorkin@elven.pw>
2019-09-22 10:16:59 +0300

    unit: 1.10.0 -> 1.11.0

--- pkgs/servers/http/unit/default.nix
+++ pkgs/servers/http/unit/default.nix
@@ -14,14 +14,14 @@
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
-  version = "1.10.0";
+  version = "1.11.0";
   pname = "unit";
 
   src = fetchFromGitHub {
     owner = "nginx";
     repo = "unit";
     rev = version;
-    sha256 = "00y7hc6bzn38f9mcqxnzddnwwsiba4ss9vwd9vgc95sj0yvv885a";
+    sha256 = "05qplbwki8kcj0xy9d39gf8lhwwg2cx24waxd1q5cb0fq9a5hprm";
   };
 
   nativeBuildInputs = [ which ];
