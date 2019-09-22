dfbbdfd387cbe951342694a8bed2a375a130d7ad
Mario Rodas <marsam@users.noreply.github.com>
2019-09-22 16:20:00 -0500

    xprite-editor: init at 2019-09-22

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7130,6 +7130,10 @@ in
 
   xorriso = callPackage ../tools/cd-dvd/xorriso { };
 
+  xprite-editor = callPackage ../tools/misc/xprite-editor {
+    inherit (darwin.apple_sdk.frameworks) AppKit;
+  };
+
   xpf = callPackage ../tools/text/xml/xpf {
     libxml2 = libxml2Python;
   };
