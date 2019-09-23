13beb8d753a854807b4f823ae37928e4b8cf91f6
Eelco Dolstra <edolstra@gmail.com>
2019-09-23 09:55:57 +0200

    firefox: Use default icu

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18300,7 +18300,6 @@ in
       libpng = libpng_apng;
       python = python2;
       gnused = gnused_422;
-      icu = icu63;
       inherit (darwin.apple_sdk.frameworks) CoreMedia ExceptionHandling
                                             Kerberos AVFoundation MediaToolbox
                                             CoreLocation Foundation AddressBook;
