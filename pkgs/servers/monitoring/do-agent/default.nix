1745ab38a0189299f15d0b31a9d1ddf85d871d50
yvt <i@yvt.jp>
2019-09-23 17:22:22 +0900

    nixos/do-agent: Update description

--- pkgs/servers/monitoring/do-agent/default.nix
+++ pkgs/servers/monitoring/do-agent/default.nix
@@ -19,9 +19,11 @@ buildGoModule rec {
   modSha256 = "0ydjwxdkcz0blpzwapiaq66vh7nrcg4j91z6h7v4ynnw2rgp7pmy";
 
   meta = with lib; {
-    description = ''
-      Collects system metrics from a DigitalOcean Droplet (on which the program
-      runs) and sends them to DigitalOcean
+    description = "DigitalOcean droplet system metrics agent";
+    longDescription = ''
+      do-agent is a program provided by DigitalOcean that collects system
+      metrics from a DigitalOcean Droplet (on which the program runs) and sends
+      them to DigitalOcean to provide resource usage graphs and alerting.
     '';
     homepage = https://github.com/digitalocean/do-agent;
     license = licenses.asl20;
