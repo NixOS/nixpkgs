95b88913205068c1870630c6c54e0e2a8049fcc8
Jonathan Ringer <jonringer117@gmail.com>
2019-09-24 14:50:41 -0700

    dotnet-sdk: 2.2.401 -> 2.2.402

--- pkgs/development/compilers/dotnet/sdk/default.nix
+++ pkgs/development/compilers/dotnet/sdk/default.nix
@@ -12,29 +12,19 @@ let
   rpath = stdenv.lib.makeLibraryPath [ stdenv.cc.cc libunwind libuuid icu openssl zlib curl ];
 in
   stdenv.mkDerivation rec {
-    version = "2.2.401";
-    netCoreVersion = "2.2.6";
+    version = "2.2.402";
+    netCoreVersion = "2.2.7";
     pname = "dotnet-sdk";
 
     src = fetchurl {
       url = "https://dotnetcli.azureedge.net/dotnet/Sdk/${version}/${pname}-${version}-linux-x64.tar.gz";
       # use sha512 from the download page
-      sha512 = "05w3zk7bcd8sv3k4kplf20j906and2006g1fggq7y6kaxrlhdnpd6jhy6idm8v5bz48wfxga5b4yys9qx0fp3p8yl7wi67qljpzrq88";
+      sha512 = "129pnngq2c16cy58zdh47grkf36k9kj39l3zpr8jxapzlin85i3wwwglwv97jp758bd0q5syvprvg84kv7x2difnkikgs2fhzh7v4w1";
     };
 
     sourceRoot = ".";
 
-    buildPhase = ''
-      runHook preBuild
-      patchelf --set-interpreter "${stdenv.cc.bintools.dynamicLinker}" ./dotnet
-      patchelf --set-rpath "${rpath}" ./dotnet
-      find -type f -name "*.so" -exec patchelf --set-rpath '$ORIGIN:${rpath}' {} \;
-      echo -n "dotnet-sdk version: "
-      ./dotnet --version
-      runHook postBuild
-    '';
-
-    dontPatchELF = true;
+    dontBuild = true;
 
     installPhase = ''
       runHook preInstall
@@ -42,6 +32,22 @@ in
       cp -r ./ $out
       ln -s $out/dotnet $out/bin/dotnet
       runHook postInstall
+    '';
+
+    dontPatchelf = true;
+
+    # the dotnet executable cannot be wrapped, must use patchelf.
+    # autoPatchelf isn't able to be used as libicu* and other's aren't
+    # listed under typical binary section
+    postFixup = ''
+      patchelf --set-interpreter "${stdenv.cc.bintools.dynamicLinker}" $out/dotnet
+      patchelf --set-rpath "${rpath}" $out/dotnet
+      find $out -type f -name "*.so" -exec patchelf --set-rpath '$ORIGIN:${rpath}' {} \;
+    '';
+
+    doInstallCheck = true;
+    installCheckPhase = ''
+      $out/bin/dotnet --info
     '';
 
     meta = with stdenv.lib; {
