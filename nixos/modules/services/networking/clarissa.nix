c6afe9e22350d0bab97c2736f6f56e5fef77ef84
Evils <evils.devils@protonmail.com>
2019-09-25 10:26:46 +0200

    clarissa: all options working

--- nixos/modules/services/networking/clarissa.nix
+++ nixos/modules/services/networking/clarissa.nix
@@ -3,126 +3,105 @@
 with lib;
 
 let
-	cfg = config.services.clarissa;
-in
-{
 
+  cfg = config.services.clarissa;
 
-	options.services.clarissa =
-	{
-		enable = mkOption
-		{
-			type = types.bool;
-			default = true;
-			description = "Whether to enable the network census daemon.";
-		};
+in {
 
-		quiet = mkOption
-		{
-			type = types.bool;
-			default = false;
-			description = "Whether to nag an entity hasn't been heard from for a timeout period.";
-		};
+  options.services.clarissa = {
 
-		promiscuous = mkOption
-		{
-			type = types.bool;
-			default = true;
-			description = "Whether to set the used interface to promiscuous mode.";
-		};
+    enable = mkEnableOption "the network census daemon.";
 
-		interface = mkOption
-		{
-			type = types.str;
-			default = "";
-			example = "eth0";
-			description = "Network interface to use instead of the automatically selected one.";
-		};
+    quiet = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Whether to nag an entity hasn't been heard from for a timeout period.";
+    };
 
-		interval = mkOption
-		{
-			type = types.ints.unsigned;
-			default = 1250;
-			description = "How often to nag and cull the list of known entities (in ms).";
-		};
+    promiscuous = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Whether to set the used interface to promiscuous mode.";
+    };
 
-		nags = mkOption
-		{
-			type = types.ints.unsigned;
-			default = 4;
-			description = "How many times to send a frame to a timed out entity.";
-		};
+    interface = mkOption {
+      type = types.str;
+      default = "";
+      example = "eth0";
+      description = "Network interface to use instead of the automatically selected one.";
+    };
 
-		timeout = mkOption
-		{
-			type = types.ints.unsigned;
-			default = 5000;
-			description = "Time in ms to wait before nagging or culling an entry.";
-		};
+    interval = mkOption {
+      type = types.ints.unsigned;
+      default = 1250;
+      description = "How often to nag and cull the list of known entities (in ms).";
+    };
 
-		subnet = mkOption
-		{
-			type = types.str;
-			default = "";
-			example = "192.168.0.0/16";
-			description = "Subnet to filter frames by (in CIDR notation).";
-		};
+    nags = mkOption {
+      type = types.ints.unsigned;
+      default = 4;
+      description = "How many times to send a frame to a timed out entity.";
+    };
 
-		outputFile = mkOption
-		{
-			type = types.str;
-			default = "";
-			example = "/tmp/clar_[dev]_[subnet]-[mask]";
-			description = "Overwrite the default generated filename";
-		};
+    timeout = mkOption {
+      type = types.ints.unsigned;
+      default = 5000;
+      description = "Time in ms to wait before nagging or culling an entry.";
+    };
 
-		outputInterval = mkOption
-		{
-			type = types.ints.unsigned;
-			default = 2500;
-			description = "How often to update the outputFile.";
-		};
+    subnet = mkOption {
+      type = types.str;
+      default = "";
+      example = "192.168.0.0/16";
+      description = "Subnet to filter frames by (in CIDR notation).";
+    };
 
-		extraOptions = mkOption
-		{
-			type = types.str;
-			default = "";
-			example = "-vvv";
-			description = "Additional options passed to the command.";
-		};
-	};
+    outputFile = mkOption {
+      type = types.str;
+      default = "";
+      example = "/tmp/clar_[dev]_[subnet]-[mask]";
+      description = "Overwrite the default generated filename";
+    };
 
-	config = mkIf cfg.enable
-	{
-		systemd.services.clarissa =
-		{
-			description = "the network census daemon";
-			documentation = [ "man:clarissa(8)" "man:clar_count(1)" "https://gitlab.com/evils/clarissa" ];
-			after = [ "network-online.target" ];
-			wants = [ "network-online.target" ];
-			wantedBy = [ "multi-user.target" ];
-			postStop = "rm -f /tmp/clar_*";
-			serviceConfig =
-			{
-				User = "root";
-				Type = "simple";
-				Restart = "always";
-				RestartSec = 1;
-				StartLimitBurst = 10;
-				ExecStart =
-				''
-					${pkgs.clarissa}/bin/clarissa \
-					--promiscuous \
-					--interval ${toString cfg.interval} \
-					--nags ${toString cfg.nags} \
-					--timeout ${toString cfg.timeout} \
-					--output_interval ${toString cfg.outputInterval} \
-					#--subnet ${cfg.subnet} \
-					#--output_file ${cfg.outputFile} \
-					#--interface${cfg.interface} \
-					${cfg.extraOptions}
-				'';
-			};
-		};
-	};
+    outputInterval = mkOption {
+      type = types.ints.unsigned;
+      default = 2500;
+      description = "How often to update the outputFile.";
+    };
+
+    extraOptions = mkOption {
+      type = types.str;
+      default = "";
+      example = "-vvv";
+      description = "Additional options passed to the command.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services.clarissa = {
+      description = "the network census daemon";
+      documentation = [ "https://gitlab.com/evils/clarissa" ];
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+      wantedBy = [ "multi-user.target" ];
+      postStop = "rm -f /tmp/clar_*";
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = 1;
+        StartLimitBurst = 10;
+        ExecStart = "${pkgs.clarissa}/bin/clarissa "
+          + (optionalString cfg.quiet "--quiet ")
+          + (optionalString cfg.promiscuous "--promiscuous ")
+          + "--interval ${toString cfg.interval} "
+          + "--nags ${toString cfg.nags} "
+          + "--timeout ${toString cfg.timeout} "
+          + "--output_interval ${toString cfg.outputInterval} "
+          + (optionalString (cfg.subnet != "") "--subnet ${cfg.subnet} ")
+          + (optionalString (cfg.outputFile != "") "--output_file ${cfg.outputFile} ")
+          + (optionalString (cfg.interface != "") "--interface ${cfg.interface} ")
+          + "${cfg.extraOptions} ";
+      };
+    };
+  };
 }
--- pkgs/tools/misc/clarissa/default.nix
+++ pkgs/tools/misc/clarissa/default.nix
@@ -1,34 +1,33 @@
 { stdenv, fetchFromGitLab, pkgs, libpcap, perl }:
 
-stdenv.mkDerivation rec
-{
-	version = "0.5-nix";
-	name = "clarissa-${version}";
+stdenv.mkDerivation rec {
 
-	src = fetchFromGitLab {
-		owner = "evils";
-		repo = "clarissa";
-		rev = "09362630bd9745d24e774091ef26471c89cc6e1c";
-		sha256 = "1w22vsqbcqw99nj799l251k3g828c1yh8skn1b7vaybv5qd8q8a5";
-	};
+  version = "0.5";
+  name = "clarissa-${version}";
 
-	buildInputs = with pkgs; [ libpcap perl ];
+  src = fetchFromGitLab {
+    owner = "evils";
+    repo = "clarissa";
+    rev = "d98f5fce06cd165699a93c6d3189e49d8d2ecca0";
+    sha256 = "1c74m2w7aicidgaxl6a1i79py4kh15dhqsppkvbxb67sd726xg4i";
+  };
 
-	doCheck = true;
+  buildInputs = with pkgs; [ libpcap perl ];
+
+  doCheck = true;
 
 # set the Makefile paths to the nix store directory we're making
-	installFlags = [ "DESTDIR=$(out)" "SYSDINST=0" ];
+  installFlags = [ "DESTDIR=$(out)" "SYSDINST=0" ];
 
-	meta = with stdenv.lib;
-	{
-		description = "Near real-time network census daemon";
-		longDescription = ''
-			Clarissa is a daemon which keeps track of connected MAC addresses on a network.
-			It can report these with sub-second resolution and can monitor passively.
-			Currently includes a utility to report a count of known and unknown addresses.
-		'';
-		homepage = https://gitlab.com/evils/clarissa/;
-		license = licenses.bsd3;
-		platforms = platforms.linux;
-	};
+  meta = with stdenv.lib; {
+    description = "Near real-time network census daemon";
+    longDescription = ''
+      Clarissa is a daemon which keeps track of connected MAC addresses on a network.
+      It can report these with sub-second resolution and can monitor passively.
+      Currently includes a utility to report a count of known and unknown addresses.
+    '';
+    homepage = https://gitlab.com/evils/clarissa/;
+    license = licenses.bsd3;
+    platforms = platforms.linux;
+  };
 }
