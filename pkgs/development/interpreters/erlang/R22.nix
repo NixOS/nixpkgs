d005fa419eec0640afae4cf6c1333cf41e8754dd
Łukasz Niemier <lukasz@niemier.pl>
2019-09-26 00:24:07 +0200

    feat: update Erlang OTP to 22.1

--- pkgs/development/interpreters/erlang/R22.nix
+++ pkgs/development/interpreters/erlang/R22.nix
@@ -1,8 +1,8 @@
 { mkDerivation }:
 
 mkDerivation {
-  version = "22.0.4";
-  sha256 = "1aqkhd6nwdn4xp5yz02zbymd4x8ij8fjw9ji8kh860n1a513k9ai";
+  version = "22.1";
+  sha256 = "0p0lwajq5skbhrx1nw8ncphj409rl6wghjrgk7d3libz12hnwrpn";
 
   prePatch = ''
     substituteInPlace make/configure.in --replace '`sw_vers -productVersion`' "''${MACOSX_DEPLOYMENT_TARGET:-10.12}"
