13cd9e1bf3fd12458d201f7908898b3ee7c5bb20
László Vaskó <vlaci@balabit.hu>
2019-09-25 09:07:49 +0200

    IPMIView: 2.14.0 --> 2.16.0
    Notes:
     * Previous URL is no longer accesible
     * build has to be adjusted for the updated JRE bundle

--- pkgs/applications/misc/ipmiview/default.nix
+++ pkgs/applications/misc/ipmiview/default.nix
@@ -1,33 +1,37 @@
-{ stdenv, fetchurl, patchelf, makeWrapper, xorg, gcc, gcc-unwrapped }:
+{ stdenv, fetchurl, patchelf, makeWrapper, xorg, fontconfig, freetype, gcc, gcc-unwrapped }:
 
 stdenv.mkDerivation rec {
   pname = "IPMIView";
-  version = "2.14.0";
-  buildVersion = "180213";
+  version = "2.16.0";
+  buildVersion = "190815";
 
   src = fetchurl {
-    url = "ftp://ftp.supermicro.com/utility/IPMIView/Linux/IPMIView_${version}_build.${buildVersion}_bundleJRE_Linux_x64.tar.gz";
-    sha256 = "1wp22wm7smlsb25x0cck4p660cycfczxj381930crd1qrf68mw4h";
+    url = "https://www.supermicro.com/wftp/utility/IPMIView/Linux/IPMIView_${version}_build.${buildVersion}_bundleJRE_Linux_x64.tar.gz";
+    sha256 = "0qw9zfnj0cyvab7ndamlw2y0gpczjhh1jkz8340kl42r2xmhkvpl";
   };
 
   nativeBuildInputs = [ patchelf makeWrapper ];
 
   buildPhase = with xorg; ''
-    patchelf --set-rpath "${stdenv.lib.makeLibraryPath [ libX11 libXext libXrender libXtst libXi ]}" ./jre/lib/amd64/xawt/libmawt.so
+    patchelf --set-rpath "${stdenv.lib.makeLibraryPath [ libX11 libXext libXrender libXtst libXi ]}" ./jre/lib/amd64/libawt_xawt.so
+    patchelf --set-rpath "${stdenv.lib.makeLibraryPath [ freetype ]}" ./jre/lib/amd64/libfontmanager.so
     patchelf --set-rpath "${gcc-unwrapped.lib}/lib" ./libiKVM64.so
-    patchelf --set-rpath "${stdenv.lib.makeLibraryPath [ libXcursor libX11 libXext libXrender libXtst libXi ]}" --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" ./jre/bin/javaws
     patchelf --set-rpath "${gcc.cc}/lib:$out/jre/lib/amd64/jli" --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" ./jre/bin/java
   '';
 
   installPhase = ''
     mkdir -p $out/bin
     cp -R . $out/
+
+    # LD_LIBRARY_PATH: fontconfig is used from java code
     makeWrapper $out/jre/bin/java $out/bin/IPMIView \
+      --set LD_LIBRARY_PATH "${stdenv.lib.makeLibraryPath [ fontconfig ]}" \
       --prefix PATH : "$out/jre/bin" \
-      --add-flags "-jar $out/IPMIView20.jar"
+      --add-flags "-jar $out/IPMIView20.jar" \
   '';
 
   meta = with stdenv.lib; {
     license = licenses.unfree;
+    maintainers = with maintainers; [ vlaci ];
   };
 }
