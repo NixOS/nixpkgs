cc625cfba4359a35c21b3e74ad42391f4d56fc46
Timo Kaufmann <timokau@zoho.com>
2019-09-25 11:29:19 +0200

    fflas-ffpack: add timokau as a maintainer
    I'm interested in this package as a dependency of sage and therefore
    would like ofBorg to notify me on changes.

--- pkgs/development/libraries/fflas-ffpack/default.nix
+++ pkgs/development/libraries/fflas-ffpack/default.nix
@@ -43,12 +43,12 @@ stdenv.mkDerivation rec {
 
   doCheck = true;
 
-  meta = {
+  meta = with stdenv.lib; {
     inherit version;
     description = ''Finite Field Linear Algebra Subroutines'';
-    license = stdenv.lib.licenses.lgpl21Plus;
-    maintainers = [stdenv.lib.maintainers.raskin];
-    platforms = stdenv.lib.platforms.unix;
+    license = licenses.lgpl21Plus;
+    maintainers = with maintainers; [ raskin timokau ];
+    platforms = platforms.unix;
     homepage = https://linbox-team.github.io/fflas-ffpack/;
   };
 }
