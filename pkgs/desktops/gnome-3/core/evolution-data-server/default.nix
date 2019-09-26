93571c1710119418774760d587272debc3e52ad8
R. RyanTM <ryantm-bot@ryantm.com>
2019-09-26 10:37:54 -0700

    evolution-data-server: 3.32.4 -> 3.34.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/evolution-data-server/versions

--- pkgs/desktops/gnome-3/core/evolution-data-server/default.nix
+++ pkgs/desktops/gnome-3/core/evolution-data-server/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "evolution-data-server";
-  version = "3.32.4";
+  version = "3.34.0";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/evolution-data-server/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0zsc9xwy6ixk3x0dx69ax5isrdw8qxjdxg2i5fr95s40nss7rxl3";
+    sha256 = "1kw6wkrr33bxv95z06dr31qvmp7c7x88xkw27k3fq0df4jlb17gv";
   };
 
   patches = [
