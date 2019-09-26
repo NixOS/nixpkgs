329c7f405f8beeff1cf44c7073d1e96e3700217e
R. RyanTM <ryantm-bot@ryantm.com>
2019-09-26 00:32:54 -0700

    bitwarden: 1.15.2 -> 1.16.4
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/bitwarden/versions

--- pkgs/tools/security/bitwarden/default.nix
+++ pkgs/tools/security/bitwarden/default.nix
@@ -16,11 +16,11 @@ let
   pname = "bitwarden";
 
   version = {
-    x86_64-linux = "1.15.2";
+    x86_64-linux = "1.16.4";
   }.${system} or "";
 
   sha256 = {
-    x86_64-linux = "0yz4hkqqwq2zrdjfxk5kybhs90n80k6bkn0625m47b09lwl2di4f";
+    x86_64-linux = "1g9ljxjqs7mx509lkfd7db7xvm9srzypbgv0qfzrr2flqbsfl06m";
   }.${system} or "";
 
   meta = with stdenv.lib; {
