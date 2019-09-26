9fc92766bb418d260ffff4dfa193bb4dfb52e582
R. RyanTM <ryantm-bot@ryantm.com>
2019-09-26 09:58:23 -0700

    gdk-pixbuf: 2.38.1 -> 2.38.2
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/gdk-pixbuf/versions

--- pkgs/development/libraries/gdk-pixbuf/default.nix
+++ pkgs/development/libraries/gdk-pixbuf/default.nix
@@ -4,13 +4,13 @@
 
 let
   pname = "gdk-pixbuf";
-  version = "2.38.1";
+  version = "2.38.2";
 in stdenv.mkDerivation rec {
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "0fmbjgjcyym3qg46f64qgl7icdm4ii77flyc1mhk244rp8vgi7zi";
+    sha256 = "10875lywkabh6nm2rgf0vxqn39h34b72kcbh63fp77fqq0g6bykk";
   };
 
   patches = [
