4d93427421176dc93083ce39720de0da7fa601ec
R. RyanTM <ryantm-bot@ryantm.com>
2019-09-27 16:27:06 -0700

    odamex: 0.7.0 -> 0.8.1
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/odamex/versions

--- pkgs/games/odamex/default.nix
+++ pkgs/games/odamex/default.nix
@@ -1,10 +1,10 @@
 { stdenv, cmake, fetchurl, pkgconfig, SDL, SDL_mixer, SDL_net }:
 
 stdenv.mkDerivation {
-  name = "odamex-0.7.0";
+  name = "odamex-0.8.1";
   src = fetchurl {
-    url = mirror://sourceforge/odamex/odamex-src-0.7.0.tar.bz2;
-    sha256 = "0cb6p58yv55kdyfj7s9n9xcwpvxrj8nyc6brw9jvwlc5n4y3cd5a";
+    url = mirror://sourceforge/odamex/odamex-src-0.8.1.tar.bz2;
+    sha256 = "1dz0lqdx3vb62mylqddcdq3vxsl2mvv0w2xskvwgpg0p04fcic2c";
   };
 
   nativeBuildInputs = [ pkgconfig ];
