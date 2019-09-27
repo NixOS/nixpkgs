4ac8b052a7d6d09711101ef0b308d76621d07f3e
R. RyanTM <ryantm-bot@ryantm.com>
2019-09-27 13:25:41 -0700

    neo4j: 3.5.8 -> 3.5.11
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/neo4j/versions

--- pkgs/servers/nosql/neo4j/default.nix
+++ pkgs/servers/nosql/neo4j/default.nix
@@ -4,11 +4,11 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   pname = "neo4j";
-  version = "3.5.8";
+  version = "3.5.11";
 
   src = fetchurl {
     url = "https://neo4j.com/artifact.php?name=neo4j-community-${version}-unix.tar.gz";
-    sha256 = "0kj92vljxdhk9pf6gr9cvd2a2ilc4myp5djjkrj3gm37f074swgg";
+    sha256 = "0xk37rvb37rd7pwpv4ingmb2v6ad1kqka8ldmd16n89fqfvg5m2d";
   };
 
   buildInputs = [ makeWrapper jre8 which gawk ];
