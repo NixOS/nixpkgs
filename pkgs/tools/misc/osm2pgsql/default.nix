9e431e1f5156062ecb9691487d64d33ddcaee6b2
R. RyanTM <ryantm-bot@ryantm.com>
2019-09-27 16:20:58 -0700

    osm2pgsql: 0.96.0 -> 1.0.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/osm2pgsql/versions

--- pkgs/tools/misc/osm2pgsql/default.nix
+++ pkgs/tools/misc/osm2pgsql/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "osm2pgsql";
-  version = "0.96.0";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "openstreetmap";
     repo = "osm2pgsql";
     rev = version;
-    sha256 = "032cydh8ynaqfhdzmkvgbmqyjql668y6qln1l59l2s3ni9963bbl";
+    sha256 = "1g9qc1z5gzdjd37n586vcmq1qli0lkhbnsrnky0mf22szzv8iwfx";
   };
 
   nativeBuildInputs = [ cmake ];
