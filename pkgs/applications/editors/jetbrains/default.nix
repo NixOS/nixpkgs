6b91398870ce9d55ae6af79d493f8f458307e0f4
Edmund Wu <fangkazuto@gmail.com>
2019-09-27 15:58:04 -0400

    jetbrains.webstorm: 2019.2.1 -> 2019.2.3

--- pkgs/applications/editors/jetbrains/default.nix
+++ pkgs/applications/editors/jetbrains/default.nix
@@ -380,12 +380,12 @@ in
 
   webstorm = buildWebStorm rec {
     name = "webstorm-${version}";
-    version = "2019.2.1"; /* updated by script */
+    version = "2019.2.3"; /* updated by script */
     description = "Professional IDE for Web and JavaScript development";
     license = stdenv.lib.licenses.unfree;
     src = fetchurl {
       url = "https://download.jetbrains.com/webstorm/WebStorm-${version}.tar.gz";
-      sha256 = "1wwy32xq332vwx70v7haa5mvrq1wq8mk5icdpgp5l6pqkzy5xayn"; /* updated by script */
+      sha256 = "0l3c3b1d53b3w90d4g99pjw8vmjj716jjaxsv4hy53shh1ncd55g"; /* updated by script */
     };
     wmClass = "jetbrains-webstorm";
     update-channel = "WebStorm RELEASE";
