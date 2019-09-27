370300758a9d8ee92d5d90659371f75f9448c83d
R. RyanTM <ryantm-bot@ryantm.com>
2019-09-27 13:02:14 -0700

    netcdfcxx4: 4.3.0 -> 4.3.1
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/netcdf-cxx4/versions

--- pkgs/development/libraries/netcdf-cxx4/default.nix
+++ pkgs/development/libraries/netcdf-cxx4/default.nix
@@ -1,11 +1,11 @@
 { lib, stdenv, fetchurl, netcdf, hdf5, curl }:
 stdenv.mkDerivation rec {
   pname = "netcdf-cxx4";
-  version = "4.3.0";
+  version = "4.3.1";
 
   src = fetchurl {
     url = "https://github.com/Unidata/netcdf-cxx4/archive/v${version}.tar.gz";
-    sha256 = "13zi8cbk18gggx1c12a580wdsbl714lw68a1wg7c86x0sybirni5";
+    sha256 = "1p4fjxxbrc0ra8kbs13d33p5zaqa4a6j1wavamr2f73cq0p3vzp3";
   };
 
   buildInputs = [ netcdf hdf5 curl ];
