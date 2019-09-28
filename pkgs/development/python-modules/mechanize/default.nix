91a233570bcfbb57989ff7678d92bba6d69c0efd
R. RyanTM <ryantm-bot@ryantm.com>
2019-09-28 06:49:06 -0700

    python27Packages.mechanize: 0.4.2 -> 0.4.3
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python2.7-mechanize/versions

--- pkgs/development/python-modules/mechanize/default.nix
+++ pkgs/development/python-modules/mechanize/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "mechanize";
-  version = "0.4.2";
+  version = "0.4.3";
   disabled = isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b680ca1b4fabe5ef52024d120f40b8e2ed7d175ed4d67225d2c477dac7c7a58b";
+    sha256 = "1gyxkwjnabqf8xxnkj787xh2dpcnm858g369fnahdcz1wn5hdmyp";
   };
 
   propagatedBuildInputs = [ html5lib ];
