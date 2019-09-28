61801968aa648eed8fafc905bd4a707c416c33a5
xrelkd <46590321+xrelkd@users.noreply.github.com>
2019-09-28 16:12:59 +0800

    parity: 2.5.7 -> 2.5.9

--- pkgs/applications/blockchains/parity/default.nix
+++ pkgs/applications/blockchains/parity/default.nix
@@ -1,6 +1,6 @@
 let
-  version     = "2.5.7";
-  sha256      = "0aprs71cbf98dsvjz0kydngkvdg5x7dijji8j6xadgvsarl1ljnj";
-  cargoSha256 = "11mr5q5aynli9xm4wnxcypl3ij7f4b0p7l557yi9n0cvdraw8ki4";
+  version     = "2.5.9";
+  sha256      = "06gmfw5l8n5i35dimsmj6dn0fxhbp53zjrdvbkff63r5kfqnwnx2";
+  cargoSha256 = "1kdy0bnmyqx4rhpq0a8gliy6mws68n035kfkxrfa6cxr2cn53dyb";
 in
   import ./parity.nix { inherit version sha256 cargoSha256; }
