55d429554576c8a60624dfec177c89d505a0f6d8
R. RyanTM <ryantm-bot@ryantm.com>
2019-09-28 01:35:01 -0700

    python37Packages.azure-mgmt-datamigration: 2.2.0 -> 3.0.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-azure-mgmt-datamigration/versions

--- pkgs/development/python-modules/azure-mgmt-datamigration/default.nix
+++ pkgs/development/python-modules/azure-mgmt-datamigration/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "azure-mgmt-datamigration";
-  version = "2.2.0";
+  version = "3.0.0";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "0bixyya9afas0sv2wji7ivfi64z4dvv8p1gjnppibi5zas1mb4zw";
+    sha256 = "0pc5pf7jpkpdrad5hafh2hki01dpx5773whp6kpxp71gh265mm5n";
   };
 
   propagatedBuildInputs = [
