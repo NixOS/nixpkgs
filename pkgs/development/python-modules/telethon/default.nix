d3f9d94534a3a7539bfe8d822d36d93e370ac37b
R. RyanTM <ryantm-bot@ryantm.com>
2019-09-28 16:03:29 -0700

    python37Packages.telethon: 1.9.0 -> 1.10.4
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-telethon/versions

--- pkgs/development/python-modules/telethon/default.nix
+++ pkgs/development/python-modules/telethon/default.nix
@@ -2,12 +2,12 @@
 
 buildPythonPackage rec {
   pname = "telethon";
-  version = "1.9.0";
+  version = "1.10.4";
 
   src = fetchPypi {
     inherit version;
     pname = "Telethon";
-    sha256 = "a8797ad5bfee2b350cfc9b73cbb30fc19c8f73c0db42471e0df1371b1a269edc";
+    sha256 = "0b0xmy61w7p7f7pdg20nkf7rndfckbf1hba5xpxjzi8h2m144m5r";
   };
 
   propagatedBuildInputs = [
