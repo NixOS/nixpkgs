dc00cee52605712a785e040293994a3f7b5974af
rnhmjoj <rnhmjoj@inventati.org>
2019-09-28 19:59:00 +0200

    x11spice: init at 2019-08-20

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21653,6 +21653,8 @@ in
 
   x11vnc = callPackage ../tools/X11/x11vnc { };
 
+  x11spice = callPackage ../tools/X11/x11spice { };
+
   x2goclient = libsForQt5.callPackage ../applications/networking/remote/x2goclient { };
 
   x2goserver = callPackage ../applications/networking/remote/x2goserver { };
