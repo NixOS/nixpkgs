026a23713ec3abac8edaf160931ce13397dc1bdc
Thomas Tuegel <ttuegel@mailbox.org>
2019-09-28 14:16:13 -0500

    rocs: Remove wrapper

--- pkgs/applications/kde/rocs.nix
+++ pkgs/applications/kde/rocs.nix
@@ -1,5 +1,5 @@
 {
-  stdenv, mkDerivation, makeWrapper, lib,
+  mkDerivation, lib,
   extra-cmake-modules, boost,
   qtbase, qtscript, qtquickcontrols, qtwebkit, qtxmlpatterns, grantlee,
   kdoctools, karchive, kxmlgui, kcrash, kdeclarative, ktexteditor, kguiaddons
@@ -13,9 +13,6 @@ mkDerivation {
     qtbase qtscript qtquickcontrols qtwebkit qtxmlpatterns grantlee
     kxmlgui kcrash kdeclarative karchive ktexteditor kguiaddons
   ];
-  postInstall = ''
-    wrapProgram $out/bin/rocs --prefix QT_PLUGIN_PATH ":" "${qtbase.bin}/${qtbase.qtPluginPrefix}:$out/${qtbase.qtPluginPrefix}"
-  '';
   meta = with lib; {
     homepage = http://www.kde.org;
     license = with licenses; [ gpl2 lgpl21 fdl12 ];
