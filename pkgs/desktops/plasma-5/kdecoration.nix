9a678e664e0b29619c0e2340c8ca04d110653926
Thomas Tuegel <ttuegel@mailbox.org>
2019-09-29 12:27:57 -0500

    kdecoration: Broken with Qt < 5.12.0

--- pkgs/desktops/plasma-5/kdecoration.nix
+++ pkgs/desktops/plasma-5/kdecoration.nix
@@ -2,6 +2,9 @@
 
 mkDerivation {
   name = "kdecoration";
+  meta = {
+    broken = builtins.compareVersions qtbase.version "5.12.0" < 0;
+  };
   nativeBuildInputs = [ extra-cmake-modules ];
   buildInputs = [ qtbase ki18n ];
   outputs = [ "out" "dev" ];
