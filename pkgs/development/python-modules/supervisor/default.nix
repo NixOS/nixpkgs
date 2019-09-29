e1b962d05b3c9ce5cc3d9b673a95a598970a46d2
Jonathan Ringer <jonringer117@gmail.com>
2019-09-29 05:52:08 -0700

    pythonPackages.supervisor: 3.3.5 -> 4.0.4
    (cherry picked from commit f08d4f78e4df759e244b96a944285c100218c9e1)

--- pkgs/development/python-modules/supervisor/default.nix
+++ pkgs/development/python-modules/supervisor/default.nix
@@ -1,27 +1,26 @@
 { lib, buildPythonPackage, isPy3k, fetchPypi
 , mock
 , meld3
+, setuptools
 }:
+
 buildPythonPackage rec {
   pname = "supervisor";
-  version = "3.3.5";
+  version = "4.0.4";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1w3ahridzbc6rxfpbyx8lij6pjlcgf2ymzyg53llkjqxalp6sk8v";
+    sha256 = "02pindhq84hb9a7ykyaqw8i2iqb21h69lpmclyqh7fm1446rji4n";
   };
 
   checkInputs = [ mock ];
 
-  propagatedBuildInputs = [ meld3 ];
+  propagatedBuildInputs = [ meld3 setuptools ];
 
-  # Supervisor requires Python 2.4 or later but does not work on any version of Python 3.  You are using version 3.6.5 (default, Mar 28 2018, 10:24:30)
-  disabled = isPy3k;
-
-  meta = {
+  meta = with lib; {
     description = "A system for controlling process state under UNIX";
     homepage = http://supervisord.org/;
-    license = lib.licenses.free; # http://www.repoze.org/LICENSE.txt
-    maintainers = with lib.maintainers; [ zimbatm ];
+    license = licenses.free; # http://www.repoze.org/LICENSE.txt
+    maintainers = with maintainers; [ zimbatm ];
   };
 }
