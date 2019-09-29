0d956a673d662bba236ee85dd5220c770ad29655
Frederik Rietdijk <fridh@fridh.nl>
2019-09-29 10:46:56 +0200

    python: zerorpc: 0.6.1 -> 0.6.3

--- pkgs/development/python-modules/zerorpc/default.nix
+++ pkgs/development/python-modules/zerorpc/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "zerorpc";
-  version = "0.6.1";
+  version = "0.6.3";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "14d0nmprs0nm17d8kg2f7qalsi8x7c4damsccqgncylj7mpnk9hh";
+    sha256 = "d2ee247a566fc703f29c277d767f6f61f1e12f76d0402faea4bd815f32cbf37f";
   };
 
   propagatedBuildInputs = [ future gevent msgpack pyzmq ];
