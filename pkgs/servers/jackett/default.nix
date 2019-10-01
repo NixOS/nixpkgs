81b5e8e2dae10191bbd882ab2ddb18d78de5d920
nyanloutre <paul@nyanlout.re>
2019-10-01 11:33:57 +0200

    jackett: add ARM support

--- pkgs/servers/jackett/default.nix
+++ pkgs/servers/jackett/default.nix
@@ -1,12 +1,22 @@
 { lib, stdenv, fetchurl, makeWrapper, curl, icu60, openssl, zlib }:
 
-stdenv.mkDerivation rec {
+let
+  arch =
+    with stdenv.hostPlatform;
+    if isx86_64 then "AMDx64"
+    else if isAarch32 then "ARM32"
+    else if isAarch64 then "ARM64"
+    else abort "Unsupported architecture";
+in stdenv.mkDerivation rec {
   pname = "jackett";
   version = "0.11.751";
 
   src = fetchurl {
-    url = "https://github.com/Jackett/Jackett/releases/download/v${version}/Jackett.Binaries.LinuxAMDx64.tar.gz";
-    sha256 = "09y9pck35pj2g89936zallxr3hanmbgp8jc42nj2js68l0z64qz3";
+    url = "https://github.com/Jackett/Jackett/releases/download/v${version}/Jackett.Binaries.Linux${arch}.tar.gz";
+    sha256 = with stdenv.hostPlatform;
+             if isAarch64 then "1qidc8qx4jhxvls304w61pypg74m0vw22bp9hacrdd901an8fjcw"
+             else if isAarch32 then "1sgl3nxbh97xl2781m6yz3pw8j278anxkg5hczp0bk97a10g4cd7"
+             else "09y9pck35pj2g89936zallxr3hanmbgp8jc42nj2js68l0z64qz3";
   };
 
   buildInputs = [ makeWrapper ];
@@ -35,6 +45,6 @@ stdenv.mkDerivation rec {
     homepage = https://github.com/Jackett/Jackett/;
     license = licenses.gpl2;
     maintainers = with maintainers; [ edwtjo nyanloutre ];
-    platforms = platforms.all;
+    platforms = platforms.linux;
   };
 }
