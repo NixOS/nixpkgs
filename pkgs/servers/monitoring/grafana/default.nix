79b99099cf21613e3a5d28cbc8ffe6ff9bac3d49
WilliButz <wbutz@cyberfnord.de>
2019-10-01 15:46:52 +0200

    grafana: 6.3.6 -> 6.4.0

--- pkgs/servers/monitoring/grafana/default.nix
+++ pkgs/servers/monitoring/grafana/default.nix
@@ -2,7 +2,7 @@
 
 buildGoPackage rec {
   pname = "grafana";
-  version = "6.3.6";
+  version = "6.4.0";
 
   goPackagePath = "github.com/grafana/grafana";
 
@@ -12,12 +12,12 @@ buildGoPackage rec {
     rev = "v${version}";
     owner = "grafana";
     repo = "grafana";
-    sha256 = "1dxsd32ny7wcpfyhhdf5m6anzgs0mkyvlrha7ps5pfyiix7brrlk";
+    sha256 = "0rqfgswlq4fs3kb1cdpisvhawxvfirrs5pja6143sm1via3p8y59";
   };
 
   srcStatic = fetchurl {
     url = "https://dl.grafana.com/oss/release/grafana-${version}.linux-amd64.tar.gz";
-    sha256 = "0cz6bg8q8lxxvqh9nb2b64rr5brkmbvpvvg4x7dykc04vcjivf6n";
+    sha256 = "1hyawiq7jgdak93ayzxmksbvkvdmfc26ska0n28f4gqppvrvkcqn";
   };
 
   postPatch = ''
