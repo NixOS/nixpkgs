e1c7d2079336148dd48cdd17cfc7b35e0d1559b3
Trolli Schmittlauch <t.schmittlauch+nixos@orlives.de>
2019-10-01 14:07:57 +0200

    cawbird: init at 1.0.1
    Cawbird is a fork of the discontinued Corebird Twitter client.
    
    Co-Authored-By: Jon <jonringer@users.noreply.github.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1309,6 +1309,8 @@ in
 
   cardpeek = callPackage ../applications/misc/cardpeek { };
 
+  cawbird = callPackage ../applications/networking/cawbird { };
+
   cde = callPackage ../tools/package-management/cde { };
 
   cdemu-daemon = callPackage ../misc/emulators/cdemu/daemon.nix { };
