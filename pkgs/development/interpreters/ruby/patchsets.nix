a1f16b145902a9be334a26f8beda3aa41c757ac1
Mario Rodas <marsam@users.noreply.github.com>
2019-10-02 00:02:00 -0500

    ruby_2_5: 2.5.6 -> 2.5.7
    Changelog: https://www.ruby-lang.org/en/news/2019/10/01/ruby-2-5-7-released/

--- pkgs/development/interpreters/ruby/patchsets.nix
+++ pkgs/development/interpreters/ruby/patchsets.nix
@@ -6,7 +6,7 @@
     "${patchSet}/patches/ruby/2.4/head/railsexpress/02-improve-gc-stats.patch"
     "${patchSet}/patches/ruby/2.4/head/railsexpress/03-display-more-detailed-stack-trace.patch"
   ];
-  "2.5.6" = ops useRailsExpress [
+  "2.5.7" = ops useRailsExpress [
     "${patchSet}/patches/ruby/2.5/head/railsexpress/01-fix-broken-tests-caused-by-ad.patch"
     "${patchSet}/patches/ruby/2.5/head/railsexpress/02-improve-gc-stats.patch"
     "${patchSet}/patches/ruby/2.5/head/railsexpress/03-more-detailed-stacktrace.patch"
