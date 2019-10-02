0dbd38cac0d28c7970439085144d5f469aea2591
Mario Rodas <marsam@users.noreply.github.com>
2019-10-02 04:20:00 -0500

    ruby_2_4: 2.4.5 -> 2.4.9
    Changelog:
    - https://www.ruby-lang.org/en/news/2019/10/01/ruby-2-4-8-released/
    - https://www.ruby-lang.org/en/news/2019/10/02/ruby-2-4-9-released/
    
    (cherry picked from commit 5ed9d8b8aaa1ce53b658704e075387678ba19222)

--- pkgs/development/interpreters/ruby/patchsets.nix
+++ pkgs/development/interpreters/ruby/patchsets.nix
@@ -6,7 +6,7 @@ rec {
     "${patchSet}/patches/ruby/2.3/head/railsexpress/02-improve-gc-stats.patch"
     "${patchSet}/patches/ruby/2.3/head/railsexpress/03-display-more-detailed-stack-trace.patch"
   ];
-  "2.4.5" = ops useRailsExpress [
+  "2.4.9" = ops useRailsExpress [
     "${patchSet}/patches/ruby/2.4/head/railsexpress/01-skip-broken-tests.patch"
     "${patchSet}/patches/ruby/2.4/head/railsexpress/02-improve-gc-stats.patch"
     "${patchSet}/patches/ruby/2.4/head/railsexpress/03-display-more-detailed-stack-trace.patch"
