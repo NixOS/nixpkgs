befd5c954bd14f55f0b4a2216bdf28f9ad50af5b
Mario Rodas <marsam@users.noreply.github.com>
2019-10-02 00:00:00 -0500

    ruby: remove ruby_2_3 patchset
    d3983caf451ae52b2a527e6a9ad1620b950a8276 removed ruby_2_3 from nixpkgs

--- pkgs/development/interpreters/ruby/patchsets.nix
+++ pkgs/development/interpreters/ruby/patchsets.nix
@@ -1,11 +1,6 @@
 { patchSet, useRailsExpress, ops, patchLevel, fetchpatch }:
 
 {
-  "2.3.8" = ops useRailsExpress [
-    "${patchSet}/patches/ruby/2.3/head/railsexpress/01-skip-broken-tests.patch"
-    "${patchSet}/patches/ruby/2.3/head/railsexpress/02-improve-gc-stats.patch"
-    "${patchSet}/patches/ruby/2.3/head/railsexpress/03-display-more-detailed-stack-trace.patch"
-  ];
   "2.4.7" = ops useRailsExpress [
     "${patchSet}/patches/ruby/2.4/head/railsexpress/01-skip-broken-tests.patch"
     "${patchSet}/patches/ruby/2.4/head/railsexpress/02-improve-gc-stats.patch"
