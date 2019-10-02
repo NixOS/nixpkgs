7ed57e7c700d6fbed261a9b612decd00bad3fe01
Mario Rodas <marsam@users.noreply.github.com>
2019-10-02 04:00:00 -0500

    zeek: add tobim as maintainer

--- pkgs/applications/networking/ids/zeek/default.nix
+++ pkgs/applications/networking/ids/zeek/default.nix
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
     description = "Powerful network analysis framework much different from a typical IDS";
     homepage = "https://www.zeek.org";
     license = licenses.bsd3;
-    maintainers = with maintainers; [ pSub marsam ];
+    maintainers = with maintainers; [ pSub marsam tobim ];
     platforms = platforms.unix;
   };
 }
