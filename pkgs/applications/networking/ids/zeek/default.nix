80e262e85ad6dd5d29cb6ca7e99bc980886c8274
Mario Rodas <marsam@users.noreply.github.com>
2019-10-02 03:00:00 -0500

    zeek: add marsam as maintainer

--- pkgs/applications/networking/ids/zeek/default.nix
+++ pkgs/applications/networking/ids/zeek/default.nix
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
     description = "Powerful network analysis framework much different from a typical IDS";
     homepage = "https://www.zeek.org";
     license = licenses.bsd3;
-    maintainers = with maintainers; [ pSub ];
+    maintainers = with maintainers; [ pSub marsam ];
     platforms = platforms.unix;
   };
 }
