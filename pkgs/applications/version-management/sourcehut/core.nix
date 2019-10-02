594378ceeab609270541926860d7805620b39d00
Maximilian Bosch <maximilian@mbosch.me>
2019-10-02 17:13:37 +0200

    sourcehut: mark as broken
    There were several custom python dependencies broken. I decided to
    modify the `sourcehut` expression as it wouldn't even evaluate without
    nodejs-11_x I didn't manage to get it building.

--- pkgs/applications/version-management/sourcehut/core.nix
+++ pkgs/applications/version-management/sourcehut/core.nix
@@ -75,6 +75,7 @@ buildPythonPackage rec {
     homepage = https://git.sr.ht/~sircmpwn/srht;
     description = "Core modules for sr.ht";
     license = licenses.bsd3;
+    broken = true;
     maintainers = with maintainers; [ eadwu ];
   };
 }
