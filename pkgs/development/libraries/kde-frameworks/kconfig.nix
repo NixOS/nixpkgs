a7417ebf45a40ab7bce2d48a7fc0b88136a48900
Thomas Tuegel <ttuegel@mailbox.org>
2019-10-02 13:30:44 -0500

    kconfig: Patch for CVE-2019-14744
    See also: https://phabricator.kde.org/D22979
    See also: https://github.com/NixOS/nixpkgs/issues/70102

--- pkgs/development/libraries/kde-frameworks/kconfig.nix
+++ pkgs/development/libraries/kde-frameworks/kconfig.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, lib, extra-cmake-modules, qtbase, qttools }:
+{ mkDerivation, lib, fetchpatch, extra-cmake-modules, qtbase, qttools }:
 
 mkDerivation {
   name = "kconfig";
@@ -6,6 +6,14 @@ mkDerivation {
     maintainers = [ lib.maintainers.ttuegel ];
     broken = builtins.compareVersions qtbase.version "5.7.0" < 0;
   };
+  patches = [
+    # https://phabricator.kde.org/D22979
+    (fetchpatch {
+      url = "https://cgit.kde.org/kconfig.git/patch/?id=5d3e71b1d2ecd2cb2f910036e614ffdfc895aa22";
+      sha256 = "0vzrwkv3l8dlr786l6h0hqq208i6i1fbnsifi5b36d3732fqbq89";
+      name = "kconfig-D22979.patch";
+    })
+  ];
   nativeBuildInputs = [ extra-cmake-modules ];
   buildInputs = [ qttools ];
   propagatedBuildInputs = [ qtbase ];
