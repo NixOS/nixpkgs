f93ea5abe1d23f6f12661333f510201fb48b533e
Roman Volosatovs <rvolosatovs@riseup.net>
2019-10-05 10:36:53 +0200

    go_1_13: add kalbasit to maintainers

--- pkgs/development/compilers/go/1.13.nix
+++ pkgs/development/compilers/go/1.13.nix
@@ -233,7 +233,7 @@ stdenv.mkDerivation rec {
     homepage = http://golang.org/;
     description = "The Go Programming language";
     license = licenses.bsd3;
-    maintainers = with maintainers; [ cstrahan orivej velovix mic92 rvolosatovs ];
+    maintainers = with maintainers; [ cstrahan orivej velovix mic92 rvolosatovs kalbasit ];
     platforms = platforms.linux ++ platforms.darwin;
   };
 }
