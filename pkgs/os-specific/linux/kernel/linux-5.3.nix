95aa1b3c8b12d6233378044a0bc50ff64ea76c1b
Tim Steinbach <tim@nequissimus.com>
2019-10-05 10:37:52 -0400

    linux: 5.3.2 -> 5.3.4

--- pkgs/os-specific/linux/kernel/linux-5.3.nix
+++ pkgs/os-specific/linux/kernel/linux-5.3.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.3.2";
+  version = "5.3.4";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0szw21mpp94gp3zn2fgllbv6fdjjf20njgrcjay7vjmm7farq7rn";
+    sha256 = "0vi4bgcr921z5l6fbcrcgmhaji5gl2avpmp7njna6v0f7sxism0r";
   };
 } // (args.argsOverride or {}))
