a4f8fbe065b8fd82aea1be23667f1d8bcfedecdf
Will Dietz <w@wdtz.org>
2019-10-04 22:43:56 -0500

    qtpass: 1.3.0 -> 1.3.1
    https://github.com/IJHack/QtPass/releases/tag/v1.3.1
    (cherry picked from commit a009dcdb2732ca914e13f3c9ae48a6e32adc5dce)

--- pkgs/applications/misc/qtpass/default.nix
+++ pkgs/applications/misc/qtpass/default.nix
@@ -1,30 +1,21 @@
-{ stdenv, lib, mkDerivation, fetchFromGitHub, fetchpatch
+{ stdenv, lib, mkDerivation, fetchFromGitHub
 , git, gnupg, pass, qtbase, qtsvg, qttools, qmake
 }:
 
 mkDerivation rec {
   pname = "qtpass";
-  version = "1.3.0";
+  version = "1.3.1";
 
   src = fetchFromGitHub {
     owner  = "IJHack";
     repo   = "QtPass";
     rev    = "v${version}";
-    sha256 = "0v3ca4fdjk6l24vc9wlc0i7r6fdj85kjmnb7jvicd3f8xi9mvhnv";
+    sha256 = "025sdk4fq71jgfs54zj7ssgvlci8vvjkqdckgbwz0nqrynlljy08";
   };
 
   buildInputs = [ git gnupg pass qtbase qtsvg qttools ];
 
   nativeBuildInputs = [ qmake ];
-
-  # Fix missing app icon on Wayland. Has been upstreamed and should be safe to
-  # remove in versions > 1.3.0
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/IJHack/QtPass/commit/aba8c4180f0ab3d66c44f88b21f137b19d17bde8.patch";
-      sha256 = "009bcq0d75khmaligzd7736xdzy6a8s1m9dgqybn70h801h92fcr";
-    })
-  ];
 
   enableParallelBuilding = true;
 
