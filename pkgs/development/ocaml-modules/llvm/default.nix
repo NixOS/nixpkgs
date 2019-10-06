a1d3b6873396f3d618cdd225af069c01aeef3847
Symphorien Gibol <symphorien+git@xlumurb.eu>
2019-10-06 12:00:00 +0000

    ocamlPackages.llvm: propagate needed libs, fix url to patch

--- pkgs/development/ocaml-modules/llvm/default.nix
+++ pkgs/development/ocaml-modules/llvm/default.nix
@@ -8,10 +8,11 @@ stdenv.mkDerivation {
 
   inherit (llvm) src;
 
-  buildInputs = [ python cmake llvm ocaml findlib ctypes ];
+  buildInputs = [ python cmake ocaml findlib ctypes ];
+  propagatedBuildInputs = [ llvm ];
 
   patches = [ (fetchpatch {
-    url = https://raw.githubusercontent.com/ocaml/opam-repository/master/packages/llvm/llvm.3.9/files/cmake.patch;
+    url = https://raw.githubusercontent.com/ocaml/opam-repository/604f1b24af6c426e0ff3a296daae3fff38ff9e95/packages/llvm/llvm.3.9/files/cmake.patch;
     sha256 = "1fcc6ylfiw1npdhx7mrsj7h0dx7cym7i9664kpr76zqazb52ikm9";
   })];
 
@@ -27,6 +28,10 @@ stdenv.mkDerivation {
     mv $out/ocaml $OCAMLFIND_DESTDIR/llvm
     mv $OCAMLFIND_DESTDIR/llvm/META{.llvm,}
   '';
+
+  passthru = {
+    inherit llvm;
+  };
 
   meta = {
     inherit (llvm.meta) license homepage;
