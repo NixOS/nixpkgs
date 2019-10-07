3610e490b34375eb142e1100f7117a2dc1030c9e
Tim Steinbach <tim@nequissimus.com>
2019-10-07 17:54:00 -0400

    linux: 5.2.19 -> 5.2.20

--- pkgs/os-specific/linux/kernel/linux-5.2.nix
+++ pkgs/os-specific/linux/kernel/linux-5.2.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.2.19";
+  version = "5.2.20";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "12mi857lyd5vj8qhj2f505hqnwzsnd829hxd78n9kk88iv1f966y";
+    sha256 = "0sddasxka08c1qjhbd9w4z7rzprfly2i67q4iwppbldngi4y48di";
   };
 } // (args.argsOverride or {}))
