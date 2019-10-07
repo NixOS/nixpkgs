73b61a49d459c3a2039cbb02ff9dd9929738ba2d
Will Dietz <w@wdtz.org>
2019-10-07 14:04:46 -0500

    aspellDicts.en: 2018.04.16-0 -> 2019.10.06-0

--- pkgs/development/libraries/aspell/dictionaries.nix
+++ pkgs/development/libraries/aspell/dictionaries.nix
@@ -298,10 +298,10 @@ in rec {
 
   en = buildOfficialDict {
     language = "en";
-    version = "2018.04.16-0";
+    version = "2019.10.06-0";
     fullName = "English";
     filename = "aspell6";
-    sha256 = "0bxxdzkk9g27plg22y9qzsx9cfjw3aa29w5bmzs561qc9gkp247i";
+    sha256 = "1zai9wrqwgb9z9vfgb22qhrvxvg73jg0ix44j1khm2f6m96lncr4";
   };
 
   eo = buildOfficialDict {
