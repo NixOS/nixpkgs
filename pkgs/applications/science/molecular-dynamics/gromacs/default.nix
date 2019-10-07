ea45dc1053d560d8b3b1e2a386a0a014b4b60b4a
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2019-10-07 13:03:13 +0200

    gromacs: add perl to buildInputs

--- pkgs/applications/science/molecular-dynamics/gromacs/default.nix
+++ pkgs/applications/science/molecular-dynamics/gromacs/default.nix
@@ -1,9 +1,9 @@
-
-{ stdenv, fetchurl, cmake,
-  singlePrec ? true,
-  mpiEnabled ? false,
-  fftw,
-  openmpi
+{ stdenv, fetchurl, cmake
+, singlePrec ? true
+, mpiEnabled ? false
+, fftw
+, openmpi
+, perl
 }:
 
 
@@ -15,13 +15,14 @@ stdenv.mkDerivation {
     sha256 = "0rqqrbjrdhprlw2z6cqid59xwxfdx05ikvywppvdp8f8vzp6chxs";
   };
 
-  buildInputs = [cmake fftw]
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ fftw perl ]
   ++ (stdenv.lib.optionals mpiEnabled [ openmpi ]);
 
   cmakeFlags = ''
     ${if singlePrec then "-DGMX_DOUBLE=OFF" else "-DGMX_DOUBLE=ON -DGMX_DEFAULT_SUFFIX=OFF"}
-    ${if mpiEnabled then "-DGMX_MPI:BOOL=TRUE 
-                          -DGMX_CPU_ACCELERATION:STRING=SSE4.1 
+    ${if mpiEnabled then "-DGMX_MPI:BOOL=TRUE
+                          -DGMX_CPU_ACCELERATION:STRING=SSE4.1
                           -DGMX_OPENMP:BOOL=TRUE
                           -DGMX_THREAD_MPI:BOOL=FALSE"
                      else "-DGMX_MPI:BOOL=FALSE" }
