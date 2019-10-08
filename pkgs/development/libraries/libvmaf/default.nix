ec6246462fd74511f2b7d308ed307b22543ff8f3
Carlos Fernandez Sanz <carlos@ccextractor.org>
2019-10-07 22:09:11 -0700

    Update default.nix
    made changes recommended by @jonringer

--- pkgs/development/libraries/libvmaf/default.nix
+++ pkgs/development/libraries/libvmaf/default.nix
@@ -1,11 +1,13 @@
 { stdenv, fetchurl, autoconf, automake, intltool, libtool, pkgconfig }:
 
 stdenv.mkDerivation rec {
-  name = "libvmaf";
+  pname = "libvmaf";
   version = "1.3.15";
 
-  src = fetchurl {
-    url="https://github.com/Netflix/vmaf/archive/v1.3.15.tar.gz";
+  src = fetchFromGitHub {
+    owner = "netflix";
+    repo = "vmaf";
+    rev = "v${version}";
     sha256="13ajbcidngjvgl0rr7l0mb43h651p5pqj3d1linrfk9c222b9fs3";
   };
 
