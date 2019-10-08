d6cfb60efde74e3a5ffdca5b898efad5557d6ea0
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-10-08 17:53:50 -0400

    ubootRaspberryPi4*: init at 2019.10

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16771,6 +16771,8 @@ in
     ubootRaspberryPi2
     ubootRaspberryPi3_32bit
     ubootRaspberryPi3_64bit
+    ubootRaspberryPi4_32bit
+    ubootRaspberryPi4_64bit
     ubootRaspberryPiZero
     ubootSheevaplug
     ubootSopine
