bffcb980e3a4329b18ee158484a9304062468567
R. RyanTM <ryantm-bot@ryantm.com>
2019-10-08 08:33:20 -0700

    dos2unix: 7.4.0 -> 7.4.1 (#70566)
    * dos2unix: 7.4.0 -> 7.4.1
    
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/dos2unix/versions
    
    * dos2unix: cleanup

--- pkgs/tools/text/dos2unix/default.nix
+++ pkgs/tools/text/dos2unix/default.nix
@@ -2,25 +2,21 @@
 
 stdenv.mkDerivation rec {
   pname = "dos2unix";
-  version = "7.4.0";
+  version = "7.4.1";
 
   src = fetchurl {
     url = "https://waterlan.home.xs4all.nl/dos2unix/${pname}-${version}.tar.gz";
-    sha256 = "12h4c61g376bhq03y5g2xszkrkrj5hwd928rly3xsp6rvfmnbixs";
+    sha256 = "08w6yywzirsxq8bh87jycvvw922ybhc2l426j2iqzliyn1h8mm8w";
   };
 
-  configurePhase = ''
-    substituteInPlace Makefile \
-    --replace /usr $out
-    '';
-
   nativeBuildInputs = [ perl gettext ];
+  makeFlags = [ "prefix=${placeholder "out"}" ];
 
   meta = with stdenv.lib; {
-    homepage = http://waterlan.home.xs4all.nl/dos2unix.html;
-    description = "Tools to transform text files from dos to unix formats and vicervesa";
+    description = "Convert text files with DOS or Mac line breaks to Unix line breaks and vice versa";
+    homepage = "https://waterlan.home.xs4all.nl/dos2unix.html";
+    changelog = "https://sourceforge.net/p/dos2unix/dos2unix/ci/dos2unix-${version}/tree/dos2unix/NEWS.txt?format=raw";
     license = licenses.bsd2;
-    maintainers = with maintainers; [ndowens ];
-
+    maintainers = with maintainers; [ c0bw3b ndowens ];
   };
 }
