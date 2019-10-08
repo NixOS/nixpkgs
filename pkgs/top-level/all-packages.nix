5075996b5921b70a4b509536d40d3a89fb5b7cac
kjuvi <quentin.vaucher@protonmail.com>
2019-10-08 19:05:08 +0200

    envelope: init at 0.0.4
    Co-authored-by: worldofpeace <worldofpeace@protonmail.ch>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18222,6 +18222,8 @@ in
 
   enhanced-ctorrent = callPackage ../applications/networking/enhanced-ctorrent { };
 
+  envelope = callPackage ../applications/office/envelope { };
+
   eolie = callPackage ../applications/networking/browsers/eolie { };
 
   epdfview = callPackage ../applications/misc/epdfview { };
