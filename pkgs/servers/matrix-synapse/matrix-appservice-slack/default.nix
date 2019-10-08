0edddb8e11770e83903bfd9756c80091e75c9200
Christian Kampka <christian@kampka.net>
2019-10-08 15:26:19 +0200

    matrix-appservice-slack: nixpkgs-fmt

--- pkgs/servers/matrix-synapse/matrix-appservice-slack/default.nix
+++ pkgs/servers/matrix-synapse/matrix-appservice-slack/default.nix
@@ -1,24 +1,25 @@
-{ pkgs, nodejs, stdenv, lib, ...}:
+{ pkgs, nodejs, stdenv, lib, ... }:
 
 let
 
-  packageName = with lib; concatStrings (map (entry: (concatStrings (mapAttrsToList (key: value: "${key}-${value}") entry))) (importJSON ./package.json)) ;
+  packageName = with lib; concatStrings (map (entry: (concatStrings (mapAttrsToList (key: value: "${key}-${value}") entry))) (importJSON ./package.json));
 
   nodePackages = import ./node-composition.nix {
     inherit pkgs nodejs;
     inherit (stdenv.hostPlatform) system;
   };
-in nodePackages."${packageName}".override {
+in
+nodePackages."${packageName}".override {
   nativeBuildInputs = [ pkgs.makeWrapper ];
 
   postInstall = ''
-      makeWrapper '${nodejs}/bin/node' "$out/bin/matrix-appservice-slack" \
-      --add-flags "$out/lib/node_modules/matrix-appservice-slack/lib/app.js"
+    makeWrapper '${nodejs}/bin/node' "$out/bin/matrix-appservice-slack" \
+    --add-flags "$out/lib/node_modules/matrix-appservice-slack/lib/app.js"
   '';
 
   meta = with lib; {
     description = "A Matrix <--> Slack bridge";
     maintainers = with maintainers; [ kampka ];
-	license = licenses.asl20;
+    license = licenses.asl20;
   };
 }
