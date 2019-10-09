de513680c50faf05bf1ee585fb5c9128dcf4e67c
Vladimír Čunát <v@cunat.cz>
2019-10-09 14:39:37 +0200

    scylladb: remove the "broken" mark, add "big-parallel"
    The build is really CPU-intensive, and requires around
    2 * --cores GiB RAM.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24494,8 +24494,8 @@ in
   sct = callPackage ../tools/X11/sct {};
 
   scylladb = callPackage ../servers/scylladb {
-   thrift = thrift-0_10;
-   };
+    thrift = thrift-0_10;
+  };
 
   seafile-shared = callPackage ../misc/seafile-shared { };
 
