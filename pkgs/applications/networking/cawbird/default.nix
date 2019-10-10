b4c6764334a95dd58d4be291f3dc955ffa9ae5e8
Trolli Schmittlauch <t.schmittlauch+nixos@orlives.de>
2019-10-10 23:43:11 +0200

    cawbird: 1.0.1 -> 1.0.2
    fixes some TLS connection issues

--- pkgs/applications/networking/cawbird/default.nix
+++ pkgs/applications/networking/cawbird/default.nix
@@ -3,14 +3,14 @@
 , glib-networking, python3, fetchpatch }:
 
 stdenv.mkDerivation rec {
-  version = "1.0.1";
+  version = "1.0.2";
   pname = "cawbird";
 
   src = fetchFromGitHub {
     owner = "IBBoard";
     repo = "cawbird";
     rev = "v${version}";
-    sha256 = "sha256:0bk33fh32nnv6ya6j0ij34abw6a3g6m8fq13303slhhja8xhvmb1";
+    sha256 = "sha256:0b79ngwilicqkgacva93cir4rmk15yzgsih56yb3a4n6bqjispay";
   };
 
   nativeBuildInputs = [
@@ -21,14 +21,6 @@ stdenv.mkDerivation rec {
   buildInputs = [
     glib gtk3 json-glib sqlite libsoup gettext gnome3.dconf gnome3.gspell glib-networking
   ] ++ (with gst_all_1; [ gstreamer gst-plugins-base gst-plugins-bad (gst-plugins-good.override { gtkSupport = true; }) gst-libav ]);
-
-  patches = [
-    # Fix build with vala 0.46
-    (fetchpatch {
-      url = "https://github.com/IBBoard/cawbird/commit/7b88f8856d108b9555ba7b855c7daed7b9e745ca.patch";
-      sha256 = "10kfdy91yas4xyz0hd057q6nsqfrkljcj7pql81xgm43qaff31y0";
-    })
-  ];
 
   postPatch = ''
     chmod +x data/meson_post_install.py # patchShebangs requires executable file
