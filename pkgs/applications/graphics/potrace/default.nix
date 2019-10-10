3f6bbe4ce3d955d4832be486184c7f8a16cd5d12
Renaud <c0bw3b@users.noreply.github.com>
2019-10-10 14:12:48 +0200

    potrace: enable tests
    Upstream provides a test suite with make check

--- pkgs/applications/graphics/potrace/default.nix
+++ pkgs/applications/graphics/potrace/default.nix
@@ -13,6 +13,9 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ zlib ];
 
+  enableParallelBuilding = true;
+  doCheck = true;
+
   meta = with stdenv.lib; {
     homepage = http://potrace.sourceforge.net/;
     description = "A tool for tracing a bitmap, which means, transforming a bitmap into a smooth, scalable image";
