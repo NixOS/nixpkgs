472380979d8d8663ad65ecb339c4f4e993958c5d
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2019-10-11 12:56:36 +0200

    pythonPackages.rcfile: do not run tests

--- pkgs/development/python-modules/rcfile/default.nix
+++ pkgs/development/python-modules/rcfile/default.nix
@@ -6,14 +6,12 @@ buildPythonPackage rec {
   pname = "rcfile";
   version = "0.1.4";
 
-  disabled = pythonOlder "2.6" || (isPy3k && pythonOlder "3.3");
-
   src = fetchPypi {
     inherit pname version;
     sha256 = "1ycbbzx3lshdjjin1lnypknhk7iy2vhr1z7g31y2g9l23dzapg91";
   };
 
-  checkInputs = [ nose ];
+  doCheck = false;
 
   meta = with stdenv.lib; {
     description = "Configuration file loader for Python projects";
