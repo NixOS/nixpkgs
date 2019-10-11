0439282fb8ca88fed2b13966f897607ca19db9cd
Michael Weiss <dev.primeos@gmail.com>
2019-10-11 13:29:36 +0200

    dav1d: 0.4.0 -> 0.5.0
    Release notes: https://code.videolan.org/videolan/dav1d/-/tags/0.5.0
    Changelog: https://code.videolan.org/videolan/dav1d/blob/0.5.0/NEWS

--- pkgs/development/libraries/dav1d/default.nix
+++ pkgs/development/libraries/dav1d/default.nix
@@ -1,30 +1,32 @@
 { stdenv, fetchFromGitLab
 , meson, ninja, nasm, pkgconfig
-, withTools ? false, SDL2
+, withTools ? false # "dav1d" binary
+, withExamples ? false, SDL2 # "dav1dplay" binary
 , useVulkan ? false, libplacebo, vulkan-loader, vulkan-headers
 }:
 
-assert useVulkan -> withTools;
+assert useVulkan -> withExamples;
 
 stdenv.mkDerivation rec {
   pname = "dav1d";
-  version = "0.4.0";
+  version = "0.5.0";
 
   src = fetchFromGitLab {
     domain = "code.videolan.org";
     owner = "videolan";
     repo = pname;
     rev = version;
-    sha256 = "1fbalfzw8j00vwbrh9h8kjdx6h99dr10vmvbpg3rhsspmxq9h66h";
+    sha256 = "0ircy8jf5djai2mdghi4si7i3w8crlfbk8qxjv95fgyf3llz3wv7";
   };
 
   nativeBuildInputs = [ meson ninja nasm pkgconfig ];
   # TODO: doxygen (currently only HTML and not build by default).
-  buildInputs = stdenv.lib.optional withTools SDL2
+  buildInputs = stdenv.lib.optional withExamples SDL2
     ++ stdenv.lib.optionals useVulkan [ libplacebo vulkan-loader vulkan-headers ];
 
   mesonFlags= [
     "-Denable_tools=${stdenv.lib.boolToString withTools}"
+    "-Denable_examples=${stdenv.lib.boolToString withExamples}"
   ];
 
   meta = with stdenv.lib; {
