1b42193d42326dace1b546a5b50e42ca9fd25ee3
Joe Hermaszewski <git@monoid.al>
2019-10-12 18:26:13 +0800

    vimPlugins.neovim-fuzzy: init at 2018-11-15

--- pkgs/misc/vim-plugins/generated.nix
+++ pkgs/misc/vim-plugins/generated.nix
@@ -1827,6 +1827,17 @@ let
     };
   };
 
+  neovim-fuzzy = buildVimPluginFrom2Nix {
+    pname = "neovim-fuzzy";
+    version = "2018-11-15";
+    src = fetchFromGitHub {
+      owner = "cloudhead";
+      repo = "neovim-fuzzy";
+      rev = "c177209678477d091ee4576e231c5b80b44514d0";
+      sha256 = "069phpy1p8dindi6whddsb9x5zyw1adzsnv7br7q955hf6x9bxxj";
+    };
+  };
+
   neovim-sensible = buildVimPluginFrom2Nix {
     pname = "neovim-sensible";
     version = "2017-09-20";
