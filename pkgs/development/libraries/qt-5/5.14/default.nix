bc30286c8034ba0c845b9c7767f3a492d5e2c8d7
Thomas Tuegel <ttuegel@mailbox.org>
2019-10-12 11:17:02 -0500

    qmake: Select debug or release build
    The qmake hook sets its own `CONFIG+=debug` or `CONFIG+=release` depending on
    how `qtbase` was built. We no longer rely on using the custom deriver for this
    feature.

--- pkgs/development/libraries/qt-5/5.14/default.nix
+++ pkgs/development/libraries/qt-5/5.14/default.nix
@@ -182,6 +182,7 @@ let
       qmake = makeSetupHook {
         deps = [ self.qtbase.dev ];
         substitutions = {
+          inherit debug;
           fix_qmake_libtool = ../hooks/fix-qmake-libtool.sh;
         };
       } ../hooks/qmake-hook.sh;
