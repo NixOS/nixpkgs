05ed82e6f43e8d434834114b99256bf323aaf288
WilliButz <wbutz@cyberfnord.de>
2019-10-13 18:46:39 +0200

    prometheus-nextcloud-exporter: use tagged version
    The tag was not pushed when this exporter was added to nixpkgs.
    The source hash remains the same, because the tag refers to the
    same revision.

--- pkgs/servers/monitoring/prometheus/nextcloud-exporter.nix
+++ pkgs/servers/monitoring/prometheus/nextcloud-exporter.nix
@@ -1,13 +1,13 @@
 { lib, fetchFromGitHub, buildGoPackage }:
 
-buildGoPackage {
+buildGoPackage rec {
   pname = "prometheus-nextcloud-exporter";
-  version = "unstable-2019-10-12";
+  version = "0.1.0";
 
   src = fetchFromGitHub {
     owner = "xperimental";
     repo = "nextcloud-exporter";
-    rev = "215c8b6b2daa3125798d883fe222bc419240e7ab";
+    rev = "v${version}";
     sha256 = "1xpc6q6zp92ckkyd24cfl65vyzjv60qwh44ys6mza4k6yrxhacv4";
   };
 
