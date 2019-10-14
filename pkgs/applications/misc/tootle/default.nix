836d2ee1c1a5809dbafc00a5743ef64b3408a43f
worldofpeace <worldofpeace@protonmail.ch>
2019-10-14 16:46:49 -0400

    tootle: fix build with vala 0.46
    See: https://github.com/bleakgrey/tootle/pull/164

--- pkgs/applications/misc/tootle/default.nix
+++ pkgs/applications/misc/tootle/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub
+{ stdenv, fetchFromGitHub, fetchpatch
 , vala, meson, ninja, pkgconfig, python3, libgee, gsettings-desktop-schemas
 , gnome3, pantheon, gobject-introspection, wrapGAppsHook
 , gtk3, json-glib, glib, glib-networking
@@ -29,6 +29,15 @@ in stdenv.mkDerivation {
   buildInputs = [
     gtk3 pantheon.granite json-glib glib glib-networking
     libgee gnome3.libsoup gsettings-desktop-schemas
+  ];
+
+  patches = [
+    # Fix build with Vala 0.46
+    # https://github.com/bleakgrey/tootle/pull/164
+    (fetchpatch {
+      url = "https://github.com/worldofpeace/tootle/commit/0a88bdad6d969ead1e4058b1a19675c9d6857b16.patch";
+      sha256 = "0xyx00pgswnhxxbsxngsm6khvlbfcl6ic5wv5n64x7klk8rzh6cm";
+    })
   ];
 
   postPatch = ''
