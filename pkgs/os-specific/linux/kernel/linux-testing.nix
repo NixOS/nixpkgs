cab3652660c54697f0bd4d67baa050d0019a2e8f
Austin Seipp <aseipp@pobox.com>
2019-10-14 10:32:51 -0500

    linux_testing: 5.4-rc2 -> 5.4-rc3
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.4-rc2";
+  version = "5.4-rc3";
   extraMeta.branch = "5.4";
 
   # modDirVersion needs to be x.y.z, will always add .0
@@ -11,7 +11,7 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "0dnn771wydwc4sngy9a3szvflbw9g97ann4iix4k4bq4gr18jw2c";
+    sha256 = "1hp9b71ip8a9mlgnwhr8x7mhy5qkgz57hd5xqskfx3axbsh2j3f5";
   };
 
   # Should the testing kernels ever be built on Hydra?
