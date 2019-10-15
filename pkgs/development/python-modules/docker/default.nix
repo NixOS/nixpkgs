e995f04ac9afef3613820ef06aac4707ce3eddbf
Jonathan Ringer <jonringer117@gmail.com>
2019-10-14 21:13:49 -0700

    pythonPackages.docker: 4.0.2 -> 4.1.0

--- pkgs/development/python-modules/docker/default.nix
+++ pkgs/development/python-modules/docker/default.nix
@@ -9,19 +9,19 @@
 }:
 
 buildPythonPackage rec {
-  version = "4.0.2";
   pname = "docker";
+  version = "4.1.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0r1i46h8x1vfvadayyvmh5hc6mpzgv3vvp6pv4g1wavamya2wnyc";
+    sha256 = "1hdgics03fz2fbhalzys7a7kjj54jnl5a37h6lzdgym41gkwa1kf";
   };
 
   propagatedBuildInputs = [
-    six
-    requests
-    websocket_client
     paramiko
+    requests
+    six
+    websocket_client
   ] ++ stdenv.lib.optional isPy27 backports_ssl_match_hostname;
 
   checkInputs = [
@@ -36,7 +36,7 @@ buildPythonPackage rec {
 
   meta = with stdenv.lib; {
     description = "An API client for docker written in Python";
-    homepage = https://github.com/docker/docker-py;
+    homepage = "https://github.com/docker/docker-py";
     license = licenses.asl20;
     maintainers = with maintainers; [ jonringer ];
   };
