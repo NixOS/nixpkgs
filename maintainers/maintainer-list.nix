e59b60310a0f2921a2e9fb689e35a7182bd6f25c
arcnmx <arcnmx@users.noreply.github.com>
2019-10-15 10:08:46 -0700

    maintainers: add arcnmx

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -546,6 +546,12 @@
     githubId = 56009;
     name = "Arcadio Rubio García";
   };
+  arcnmx = {
+    email = "arcnmx@users.noreply.github.com";
+    github = "arcnmx";
+    githubId = 13426784;
+    name = "arcnmx";
+  };
   ardumont = {
     email = "eniotna.t@gmail.com";
     github = "ardumont";
