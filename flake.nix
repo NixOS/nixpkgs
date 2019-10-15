7ff2e743e81214665e80dba3462292019999a45a
Eelco Dolstra <edolstra@gmail.com>
2019-10-15 18:17:21 +0200

    flake.nix: Support more systems

--- flake.nix
+++ flake.nix
@@ -5,10 +5,17 @@
 
   outputs = { self }:
     let
+
       jobs = import ./pkgs/top-level/release.nix {
         nixpkgs = self;
       };
+
       lib = import ./lib;
+
+      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" ];
+
+      forAllSystems = f: lib.genAttrs systems (system: f system);
+
     in
     {
       lib = lib // {
@@ -32,7 +39,7 @@
         }).nixos.manual.x86_64-linux;
       };
 
-      legacyPackages = import ./. { system = "x86_64-linux"; };
+      legacyPackages = forAllSystems (system: import ./. { inherit system; });
 
       nixosModules = {
         notDetected = ./nixos/modules/installer/scan/not-detected.nix;
