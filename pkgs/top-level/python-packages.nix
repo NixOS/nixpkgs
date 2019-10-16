6a2a395241ea222bc8617806e9e63c133492317f
David Pätzel <david.paetzel@informatik.uni-augsburg.de>
2019-10-16 19:45:26 +0200

    pythonPackages.mesa: init at 0.8.6

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3798,6 +3798,8 @@ in {
 
   memory_profiler = callPackage ../development/python-modules/memory_profiler { };
 
+  mesa = callPackage ../development/python-modules/mesa { };
+
   metaphone = callPackage ../development/python-modules/metaphone { };
 
   mezzanine = callPackage ../development/python-modules/mezzanine { };
