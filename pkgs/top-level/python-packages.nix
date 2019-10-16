8ca9caac9a7c67a46819641c13f0f2558262a763
geistesk <github.jyf300hj@0x21.biz>
2019-10-16 22:20:44 +0200

    pytmx: init at 3.21.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1086,6 +1086,8 @@ in {
 
   pytest-xprocess = callPackage ../development/python-modules/pytest-xprocess { };
 
+  pytmx = callPackage ../development/python-modules/pytmx { };
+
   python-binance = callPackage ../development/python-modules/python-binance { };
 
   python-dbusmock = callPackage ../development/python-modules/python-dbusmock { };
