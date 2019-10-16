43ba2a06771e1292352b4fe32ccb579c8bc892f9
Frederik Rietdijk <fridh@fridh.nl>
2019-10-16 11:43:08 +0200

    python: holoviews: 1.12.5 -> 1.12.6

--- pkgs/development/python-modules/holoviews/default.nix
+++ pkgs/development/python-modules/holoviews/default.nix
@@ -15,11 +15,11 @@
 
 buildPythonPackage rec {
   pname = "holoviews";
-  version = "1.12.5";
+  version = "1.12.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "15cay2fnhwqr42s4hiq37b8q87sir5k59p14g96mvg5p0gjnhg3w";
+    sha256 = "4f6ad184fb6136e5ee37a74b5276825fc3d5fce5033ff3c8db8831ec11ea2e75";
   };
 
   propagatedBuildInputs = [
