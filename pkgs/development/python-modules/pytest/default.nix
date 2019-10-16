9994aae95ee4a1b630a1d39822749c21aec15634
Jonathan Ringer <jonringer117@gmail.com>
2019-10-16 09:38:41 -0700

    python3Packages.pytest: 5.1.0 -> 5.2.1

--- pkgs/development/python-modules/pytest/default.nix
+++ pkgs/development/python-modules/pytest/default.nix
@@ -3,7 +3,7 @@
 , atomicwrites, mock, writeText, pathlib2, wcwidth, packaging, isPyPy, python
 }:
 buildPythonPackage rec {
-  version = "5.1.0";
+  version = "5.2.1";
   pname = "pytest";
 
   disabled = !isPy3k;
@@ -15,7 +15,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "3805d095f1ea279b9870c3eeae5dddf8a81b10952c8835cd628cf1875b0ef031";
+    sha256 = "185a7k3c4bdmkk7rw2z9hb5i1ib5phkiqc378yrhq7clyhsk8mna";
   };
 
   checkInputs = [ hypothesis mock ];
@@ -26,9 +26,12 @@ buildPythonPackage rec {
   doCheck = !isPyPy; # https://github.com/pytest-dev/pytest/issues/3460
 
   # Ignored file https://github.com/pytest-dev/pytest/pull/5605#issuecomment-522243929
+  # setuptools test attempt to do importlib in non-Nixos compatible way
   checkPhase = ''
     runHook preCheck
-    $out/bin/py.test -x testing/ -k "not test_collect_pyargs_with_testpaths" --ignore=testing/test_junitxml.py
+    $out/bin/py.test -x testing/ -k "not (collect_pyargs_with_testpaths or setuptools)" \
+      --ignore=testing/test_junitxml.py \
+      --ignore=testing/test_assertion.py # requires child plugins
     runHook postCheck
   '';
 
