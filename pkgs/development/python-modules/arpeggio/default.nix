b3e4f01653673582a7a37d950be341cc06a2c6d4
Frederik Rietdijk <fridh@fridh.nl>
2019-10-16 11:43:02 +0200

    python: Arpeggio: 1.9.0 -> 1.9.2

--- pkgs/development/python-modules/arpeggio/default.nix
+++ pkgs/development/python-modules/arpeggio/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "Arpeggio";
-  version = "1.9.0";
+  version = "1.9.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a5258b84f76661d558492fa87e42db634df143685a0e51802d59cae7daad8732";
+    sha256 = "948ce06163a48a72c97f4fe79ad3d1c1330b6fec4f22ece182fb60ef60bd022b";
   };
 
   # Shall not be needed for next release
