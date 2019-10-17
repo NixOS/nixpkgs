d54fda49eb731734f9b2c95885442403da8e0da7
Konrad Borowski <konrad@borowski.pw>
2019-10-17 14:35:16 +0200

    zfsbackup-go: init at unstable-2019-03-05

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7271,6 +7271,8 @@ in
 
   zerofree = callPackage ../tools/filesystems/zerofree { };
 
+  zfsbackup = callPackage ../tools/backup/zfsbackup { };
+
   zfstools = callPackage ../tools/filesystems/zfstools { };
 
   zile = callPackage ../applications/editors/zile { };
