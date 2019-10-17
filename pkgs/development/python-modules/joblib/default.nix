76c2bde6d1b86c62d9e1bd715605b2b32460a014
Frederik Rietdijk <fridh@fridh.nl>
2019-10-17 10:10:33 +0200

    pythonPackages.joblib: 0.13.2 -> 0.14.0

--- pkgs/development/python-modules/joblib/default.nix
+++ pkgs/development/python-modules/joblib/default.nix
@@ -1,7 +1,6 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, fetchpatch
 , stdenv
 , numpydoc
 , pytest
@@ -13,29 +12,12 @@
 
 buildPythonPackage rec {
   pname = "joblib";
-  version = "0.13.2";
+  version = "0.14.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "315d6b19643ec4afd4c41c671f9f2d65ea9d787da093487a81ead7b0bac94524";
+    sha256 = "1zwkl6hgi8wbygcc6ql6yk1if665hwk43sa9shglb2afrfm5gk3g";
   };
-
-  # python-lz4 compatibility
-  # https://github.com/joblib/joblib/pull/847
-  patches = [
-    (fetchpatch {
-      url = https://github.com/joblib/joblib/commit/d3235fd601f40c91e074d48a411d7380329fe155.patch;
-      sha256 = "1hg1vfbba7mfilrpvmd97s68v03vs4bhlp1c1dj9lizi51mj2q2h";
-    })
-    (fetchpatch {
-      url = https://github.com/joblib/joblib/commit/884c92cd2aa5c2c1975ab48786da75556d779833.patch;
-      sha256 = "11kvpkvi428dq13ayy7vfyrib8isvcrdw8cd5hxkp5axr7sl12ba";
-    })
-    (fetchpatch {
-      url = https://github.com/joblib/joblib/commit/f1e177d781cc0d64420ec964a0b17d8268cb42a0.patch;
-      sha256 = "1sq6wcw4bhaq8cqwcd43fdws3467qy342xx3pgv62hp2nn75a21d";
-    })
-  ];
 
   checkInputs = [ sphinx numpydoc pytest ];
   propagatedBuildInputs = [ python-lz4 setuptools ];
