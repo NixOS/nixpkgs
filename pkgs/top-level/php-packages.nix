0d8f2eca320d9a140c4f5c632e98a07a8aef966e
Elis Hirwing <elis@hirwing.se>
2019-10-17 18:45:52 +0200

    phpPackages.phpcbf: 3.5.0 -> 3.5.1
    Changelog: https://github.com/squizlabs/PHP_CodeSniffer/releases/tag/3.5.1

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -338,12 +338,12 @@ let
   };
 
   phpcbf = mkDerivation rec {
-    version = "3.5.0";
+    version = "3.5.1";
     pname = "phpcbf";
 
     src = pkgs.fetchurl {
       url = "https://github.com/squizlabs/PHP_CodeSniffer/releases/download/${version}/phpcbf.phar";
-      sha256 = "15n3r3sc62ar1kq38idw22y7gasvy747bix99zs0l0paapcbxz6n";
+      sha256 = "1b68cmdvg356s2vk5q0jkk8sizza7r7pbcl9v5s0944wi0apsj0r";
     };
 
     phases = [ "installPhase" ];
