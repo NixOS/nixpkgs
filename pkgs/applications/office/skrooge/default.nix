1d5661cd808aaaa3931f3de85633d0a35fedce2a
Flakebi <flakebi@t-online.de>
2019-10-18 01:23:11 +0200

    skrooge: Fix build

--- pkgs/applications/office/skrooge/default.nix
+++ pkgs/applications/office/skrooge/default.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, lib, fetchurl,
+{ mkDerivation, lib, fetchpatch, fetchurl,
   cmake, extra-cmake-modules, qtwebengine, qtscript, grantlee,
   kxmlgui, kwallet, kparts, kdoctools, kjobwidgets, kdesignerplugin,
   kiconthemes, knewstuff, sqlcipher, qca-qt5, kactivities, karchive,
@@ -13,6 +13,14 @@ mkDerivation rec {
     url = "http://download.kde.org/stable/skrooge/${pname}-${version}.tar.xz";
     sha256 = "0rakfngp7j2x7h1isg6lbc5kva6k1kg99dz0zl43dc28s15can1w";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "skrooge-2.20.0-missing-header.patch";
+      url = "https://gitweb.gentoo.org/repo/gentoo.git/plain/app-office/skrooge/files/skrooge-2.20.0-missing-header.patch?id=cb8c91474b0ae2f9e889f89afe2d9114dbd1784f";
+      sha256 = "154zsidx45h6qrcqjh6czjxrcwrcmbyv3yh2k1s40v8pzvjwzrld";
+    })
+  ];
 
   nativeBuildInputs = [
     cmake extra-cmake-modules kdoctools shared-mime-info
@@ -37,6 +45,5 @@ mkDerivation rec {
     license = with licenses; [ gpl3 ];
     maintainers = with maintainers; [ joko ];
     homepage = https://skrooge.org/;
-    broken = true;
   };
 }
