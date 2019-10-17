93b2c9ef59d4f1cef318d785b3ad9360191eb98c
Thorsten Weber <cpt.chaos83@googlemail.com>
2019-10-17 17:45:18 +0200

    aqbanking: 5.7.8 -> 6.1.1

--- pkgs/development/libraries/aqbanking/default.nix
+++ pkgs/development/libraries/aqbanking/default.nix
@@ -1,27 +1,33 @@
 { stdenv, fetchurl, gmp, gwenhywfar, libtool, libxml2, libxslt
-, pkgconfig, gettext, xmlsec, zlib, fetchpatch, autoreconfHook
+, pkgconfig, gettext, xmlsec, zlib
 }:
 
 stdenv.mkDerivation rec {
   pname = "aqbanking";
-  version = "5.7.8";
+  version = "6.1.1";
 
   src = fetchurl {
-    url = https://www.aquamaniac.de/rdm/attachments/download/27/aqbanking-5.7.8.tar.gz;
-    sha256 = "0s67mysskbiw1h1p0np4ph4351r7wq3nc873vylam7lsqi66xy0n";
+    url = https://www.aquamaniac.de/rdm/attachments/download/266/aqbanking-6.1.1.tar.gz;
+    sha256 = "0f33v4ridfn3cd0nj2c183yl8pdb4w2fa30bswrm6mc2sy6a4wpl";
   };
 
-  # patch bug with newer automake
-  # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=906583
-  patches = [
-    (fetchpatch {
-      name = "fix_aqhbci_testlib_dependencies.patch";
-      url = "https://bugs.debian.org/cgi-bin/bugreport.cgi?att=1;bug=906583;filename=fix_aqhbci_testlib_dependencies.patch;msg=10";
-      sha256 = "1vz219cih6mz1s7fhh16snslqlxyn11yjb4q516l52yvc4324yis";
-    })
-  ];
+  /*
+  fix error: 
 
-  postAutoreconf = ''
+  CMake Error at /nix/store/gxpscib9m9f3by77m367fmpiavcai380-gwenhywfar-4.99.22rc6/lib/cmake/gwenhywfar-4.99/gwenhywfar-config.cmake:7 (message):
+  File or directory
+  /nix/store/gxpscib9m9f3by77m367fmpiavcai380-gwenhywfar-4.99.22rc6/include/aqbanking6
+  referenced by variable AQBANKING_INCLUDE_DIRS does not exist !
+  Call Stack (most recent call first):
+  /nix/store/f8wm9s005c4kkc6mhhlb6dv8lqk8vrhf-aqbanking-5.99.40beta/lib/cmake/aqbanking-5.99/aqbanking-config.cmake:24 (set_and_check)
+  CMakeLists.txt:237 (find_package)
+
+  when building kmymoney.
+  i think it's because includedir is overwitten by gwenhywfar's .cmake file.
+  */
+  postPatch = ''
+    sed -i '/^set_and_check(AQBANKING_INCLUDE_DIRS "@aqbanking_headerdir@")/i set_and_check(includedir "@includedir@")' aqbanking-config.cmake.in
+
     sed -i -e '/^aqbanking_plugindir=/ {
       c aqbanking_plugindir="\''${libdir}/gwenhywfar/plugins"
     }' configure
@@ -29,13 +35,11 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ gmp gwenhywfar libtool libxml2 libxslt xmlsec zlib ];
 
-  nativeBuildInputs = [ pkgconfig gettext autoreconfHook ];
-
-  configureFlags = [ "--with-gwen-dir=${gwenhywfar}" ];
+  nativeBuildInputs = [ pkgconfig gettext ];
 
   meta = with stdenv.lib; {
     description = "An interface to banking tasks, file formats and country information";
-    homepage = http://www2.aquamaniac.de/sites/download/packages.php?package=03&showall=1;
+    homepage = https://www.aquamaniac.de/rdm/projects/aqbanking;
     hydraPlatforms = [];
     license = licenses.gpl2Plus;
     maintainers = with maintainers; [ goibhniu ];
