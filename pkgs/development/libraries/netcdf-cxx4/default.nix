56858c2b92bcd038aa5718fa6e0ad72e7c8c9bb9
Renaud <c0bw3b@users.noreply.github.com>
2019-10-18 18:21:45 +0200

    netcdfcxx4: use fetchzip
    Github archives are not stable over time so
    fetchzip should be used instead of fetchurl

--- pkgs/development/libraries/netcdf-cxx4/default.nix
+++ pkgs/development/libraries/netcdf-cxx4/default.nix
@@ -1,11 +1,11 @@
-{ lib, stdenv, fetchurl, netcdf, hdf5, curl }:
+{ lib, stdenv, fetchzip, netcdf, hdf5, curl }:
 stdenv.mkDerivation rec {
   pname = "netcdf-cxx4";
   version = "4.3.1";
 
-  src = fetchurl {
+  src = fetchzip {
     url = "https://github.com/Unidata/netcdf-cxx4/archive/v${version}.tar.gz";
-    sha256 = "1p4fjxxbrc0ra8kbs13d33p5zaqa4a6j1wavamr2f73cq0p3vzp3";
+    sha256 = "05kydd5z9iil5iv4fp7l11cicda5n5lsg5sdmsmc55xpspnsg7hr";
   };
 
   buildInputs = [ netcdf hdf5 curl ];
