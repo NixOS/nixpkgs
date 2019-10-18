76d1658f3f264a654402f2c5bf42945b464f4ab7
Aaron Andersen <aaron@fosslib.net>
2019-10-18 14:31:36 -0400

    zabbix: 4.2.6 -> 4.2.7, 4.0.12 -> 4.0.13

--- pkgs/servers/monitoring/zabbix/versions.nix
+++ pkgs/servers/monitoring/zabbix/versions.nix
@@ -1,12 +1,12 @@
 generic: {
   v42 = generic {
-    version = "4.2.6";
-    sha256 = "0r08v9ka0v8d7w133yr09vaxzl5lhbrsvbx900x15qv8lwliysv4";
+    version = "4.2.7";
+    sha256 = "09znh8x1sass5mw6wjrfmizjbfls8ad2c16y24ldfj40hlfxz6wx";
   };
 
   v40 = generic {
-    version = "4.0.12";
-    sha256 = "1jlbzlz0rzcjixicdcd7xcjhfqwr82hngpazx85f40r9dy574pjw";
+    version = "4.0.13";
+    sha256 = "1hz4sdj4nw8fh124dqd41ag2wv4rcp4snpxgk80nx76rssw6c9ny";
   };
 
   v30 = generic {
