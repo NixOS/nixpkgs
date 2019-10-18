00e7a675f77a11c7a42c7b94fc95feb645f317c3
Edmund Wu <fangkazuto@gmail.com>
2019-10-18 16:49:09 -0400

    linux_testing_bcachefs: 5.2.2019.10.12 -> 5.3.2020.03.25

--- pkgs/os-specific/linux/kernel/linux-testing-bcachefs.nix
+++ pkgs/os-specific/linux/kernel/linux-testing-bcachefs.nix
@@ -1,13 +1,13 @@
 { stdenv, buildPackages, fetchgit, fetchpatch, perl, buildLinux, ... } @ args:
 
 buildLinux (args // {
-  version = "5.2.2019.10.12";
-  modDirVersion = "5.2.0";
+  version = "5.3.2020.03.25";
+  modDirVersion = "5.3.0";
 
   src = fetchgit {
     url = "https://evilpiepirate.org/git/bcachefs.git";
-    rev = "de906c3e2eddad291d46bd0e7c81c68eaadcd08a";
-    sha256 = "1ahabp8pd9slf4lchkbyfkagg9vhic0cw3kwvwryzaxxxjmf2hkk";
+    rev = "96b991466ac851ea3c7adbd2e30184837573e2a0";
+    sha256 = "1x6h2p8j8n727jywyc24k81i169xj645pb5wav3hvn57qja1z3b3";
   };
 
   extraConfig = "BCACHEFS_FS m";
