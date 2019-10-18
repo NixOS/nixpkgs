afebf1ee45eaf3524ff0f6b12fdfb09ecb455f47
tekeri <47343275+tekeri@users.noreply.github.com>
2019-10-18 11:45:45 +0900

    pbis-open: init at 9.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20078,6 +20078,8 @@ in
 
   partio = callPackage ../development/libraries/partio {};
 
+  pbis-open = callPackage ../tools/security/pbis { };
+
   pcmanfm = callPackage ../applications/misc/pcmanfm { };
 
   pcmanfm-qt = lxqt.pcmanfm-qt;
