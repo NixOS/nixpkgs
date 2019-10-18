2dbd1a938ba03aecf20265298872ba8b28622f3d
Renaud <c0bw3b@users.noreply.github.com>
2019-10-18 18:00:38 +0200

    odamex: revamp
    + use pname and version
    + not broken anymore
    + restrict to Linux (never built on Darwin)
    + add wxGTK to build odalaunch client

--- pkgs/games/odamex/default.nix
+++ pkgs/games/odamex/default.nix
@@ -1,23 +1,24 @@
-{ stdenv, cmake, fetchurl, pkgconfig, SDL, SDL_mixer, SDL_net }:
+{ stdenv, cmake, fetchurl, pkgconfig, SDL, SDL_mixer, SDL_net, wxGTK30 }:
 
-stdenv.mkDerivation {
-  name = "odamex-0.8.1";
+stdenv.mkDerivation rec {
+  pname = "odamex";
+  version = "0.8.1";
+
   src = fetchurl {
-    url = mirror://sourceforge/odamex/odamex-src-0.8.1.tar.bz2;
+    url = "mirror://sourceforge/${pname}/${pname}-src-${version}.tar.bz2";
     sha256 = "1dz0lqdx3vb62mylqddcdq3vxsl2mvv0w2xskvwgpg0p04fcic2c";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ cmake SDL SDL_mixer SDL_net ];
+  nativeBuildInputs = [ cmake pkgconfig ];
+  buildInputs = [ SDL SDL_mixer SDL_net wxGTK30 ];
 
   enableParallelBuilding = true;
 
   meta = {
-    homepage = http://odamex.net/;
+    homepage = "http://odamex.net/";
     description = "A client/server port for playing old-school Doom online";
     license = stdenv.lib.licenses.gpl2;
-    platforms = stdenv.lib.platforms.unix;
+    platforms = stdenv.lib.platforms.linux;
     maintainers = with stdenv.lib.maintainers; [ MP2E ];
-    broken = true;
   };
 }
