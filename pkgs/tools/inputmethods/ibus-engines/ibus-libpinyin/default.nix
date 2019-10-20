7b530d01581986291bede8fbaa13b124db700561
Jan Tojnar <jtojnar@gmail.com>
2019-10-20 15:57:03 +0200

    ibus-engines.libpinyin: fix runtime deps
    * We need PyGObject since it is not propagated by ibus anymore.
    * With python.withPackage, we no longer need wrapPython.
    * We need wrapGAppsHook still, to find GSettings schemas.

--- pkgs/tools/inputmethods/ibus-engines/ibus-libpinyin/default.nix
+++ pkgs/tools/inputmethods/ibus-engines/ibus-libpinyin/default.nix
@@ -1,6 +1,16 @@
-{ stdenv, fetchFromGitHub, autoreconfHook
-, intltool, pkgconfig, sqlite, libpinyin, db
-, ibus, glib, gtk3, python3
+{ stdenv
+, fetchFromGitHub
+, autoreconfHook
+, intltool
+, pkgconfig
+, wrapGAppsHook
+, sqlite
+, libpinyin
+, db
+, ibus
+, glib
+, gtk3
+, python3
 }:
 
 stdenv.mkDerivation rec {
@@ -8,26 +18,41 @@ stdenv.mkDerivation rec {
   version = "1.10.0";
 
   src = fetchFromGitHub {
-    owner  = "libpinyin";
-    repo   = "ibus-libpinyin";
-    rev    = version;
+    owner = "libpinyin";
+    repo = "ibus-libpinyin";
+    rev = version;
     sha256 = "0zkzz6ig74nws8phqxbsggnpf5g5f2hxi0mdyn2m3s4nm14q3ma6";
   };
 
-  buildInputs = [ ibus glib sqlite libpinyin python3 gtk3 db ];
-  nativeBuildInputs = [ autoreconfHook intltool pkgconfig python3.pkgs.wrapPython ];
+  nativeBuildInputs = [
+    autoreconfHook
+    intltool
+    pkgconfig
+    wrapGAppsHook
+  ];
+
+  buildInputs = [
+    ibus
+    glib
+    sqlite
+    libpinyin
+    (python3.withPackages (pypkgs: with pypkgs; [
+      pygobject3
+      (toPythonModule ibus)
+    ]))
+    gtk3
+    db
+  ];
 
   postAutoreconf = ''
     intltoolize
   '';
 
-  postFixup = "wrapPythonPrograms";
-
   meta = with stdenv.lib; {
     isIbusEngine = true;
-    description  = "IBus interface to the libpinyin input method";
-    license      = licenses.gpl2;
-    maintainers  = with maintainers; [ ericsagnes ];
-    platforms    = platforms.linux;
+    description = "IBus interface to the libpinyin input method";
+    license = licenses.gpl2;
+    maintainers = with maintainers; [ ericsagnes ];
+    platforms = platforms.linux;
   };
 }
