1fca2ab52dacc18bb0e7a152709924c585760737
Frederik Rietdijk <fridh@fridh.nl>
2019-10-20 17:33:43 +0200

    python: zope.testrunner: 5.0 -> 5.1

--- pkgs/development/python-modules/zope_testrunner/default.nix
+++ pkgs/development/python-modules/zope_testrunner/default.nix
@@ -10,11 +10,11 @@
 
 buildPythonPackage rec {
   pname = "zope.testrunner";
-  version = "5.0";
+  version = "5.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0d8a27e214033b6f1e557556e61bff9568ae95676dfcc8a032483fdeeee792c3";
+    sha256 = "354a65f6c6fe6c0584e2fcf06d7318e90dc7f7de1b7008d8913733e299317870";
   };
 
   propagatedBuildInputs = [ zope_interface zope_exceptions zope_testing six ];
