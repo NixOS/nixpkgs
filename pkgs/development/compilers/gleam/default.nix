f2e615bd350a175ff43bcbbd60227c82ea13550b
Norbert Melzer <nmelzer@nobbz.dev>
2019-10-20 11:24:31 +0200

    gleam: reorganize a bit

--- pkgs/development/compilers/gleam/default.nix
+++ pkgs/development/compilers/gleam/default.nix
@@ -1,15 +1,19 @@
 { pkgs, rustPlatform, fetchFromGitHub, stdenv }:
 
-rustPlatform.buildRustPackage rec {
-  pname = "gleam";
+let
   version = "0.4.1";
 
-  src = "${fetchFromGitHub {
+  github = fetchFromGitHub {
     owner = "lpil";
     repo = "gleam";
     rev = "v${version}";
     sha256 = "0d341aqgvnx5881wbwlb7r5l5ybfw69cq7jv25f1wac2cfhzl4xh";
-  }}/gleam";
+  };
+in rustPlatform.buildRustPackage {
+  pname = "gleam";
+  inherit version;
+
+  src = "${github}/gleam";
 
   cargoSha256 = "06dr5p5qin48d9bjai6l46xg14hvhzlwk7fykbjdv7il9z5lpc8v";
 
