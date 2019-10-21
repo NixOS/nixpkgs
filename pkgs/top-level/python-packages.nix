fd6fd28d1f480779e0df42f625fe04e021de9100
Matt Huszagh <huszaghmatt@gmail.com>
2019-10-20 18:04:20 -0700

    pythonPackages.python-openems: init at unstable-2020-02-15

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4500,6 +4500,8 @@ in {
 
   python-csxcad = callPackage ../development/python-modules/python-csxcad { };
 
+  python-openems = callPackage ../development/python-modules/python-openems { };
+
   pkutils = callPackage ../development/python-modules/pkutils { };
 
   plac = callPackage ../development/python-modules/plac { };
