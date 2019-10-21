fa4ff4d061786aa99b2193a1f5de0bd9f69db5d0
Mario Rodas <marsam@users.noreply.github.com>
2019-10-21 04:20:00 -0500

    nodejs-12_x: 12.10.0 -> 12.13.0
    Changelog: https://github.com/nodejs/node/releases/tag/v12.13.0

--- pkgs/development/web/nodejs/v12.nix
+++ pkgs/development/web/nodejs/v12.nix
@@ -5,8 +5,8 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "12.10.0";
-    sha256 = "1k9hxqs23c3sxpr843ix8nidlca8wn30x0sq998j47wjc1ybh595";
+    version = "12.13.0";
+    sha256 = "1xmy73q3qjmy68glqxmfrk6baqk655py0cic22h1h0v7rx0iaax8";
 
     patches = stdenv.lib.optionals stdenv.isDarwin [ ./disable-libatomic-darwin.patch ];
   }
