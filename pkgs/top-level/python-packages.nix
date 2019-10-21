cc595c2c219d86b8410e39c39d74b39e10423da0
Markus S. Wamser <github-dev@mail2013.wamser.eu>
2019-10-22 00:40:12 +0200

    sphinxcontrib-serializinghtml: init at 1.1.3
    build dep of sphinx 2.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5021,6 +5021,8 @@ in {
 
   sphinxcontrib-qthelp = callPackage ../development/python-modules/sphinxcontrib-qthelp {};
 
+  sphinxcontrib-serializinghtml = callPackage ../development/python-modules/sphinxcontrib-serializinghtml {};
+
   sphinxcontrib-bibtex = callPackage ../development/python-modules/sphinxcontrib-bibtex {};
 
   sphinx-navtree = callPackage ../development/python-modules/sphinx-navtree {};
