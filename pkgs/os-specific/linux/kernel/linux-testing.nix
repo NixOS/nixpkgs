db32ff033369349b55bbcd346139fade76dd99e7
Tim Steinbach <tim@nequissimus.com>
2019-10-21 11:46:17 -0400

    linux: 5.4-rc3 -> 5.4-rc4

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.4-rc3";
+  version = "5.4-rc4";
   extraMeta.branch = "5.4";
 
   # modDirVersion needs to be x.y.z, will always add .0
@@ -11,7 +11,7 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "1hp9b71ip8a9mlgnwhr8x7mhy5qkgz57hd5xqskfx3axbsh2j3f5";
+    sha256 = "0f0qnk9x1drd0vgawfr1nyr77pm8j00fqjij8qs45b69c1l8bx5i";
   };
 
   # Should the testing kernels ever be built on Hydra?
