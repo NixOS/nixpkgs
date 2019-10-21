c1621cb9578a0e4da2d8e6def6012f607a185b57
Brian Hicks <brian@brianthicks.com>
2019-10-21 08:31:55 -0500

    elmPackages.elm: 0.19.0 -> 0.19.1

--- pkgs/development/compilers/elm/update.sh
+++ pkgs/development/compilers/elm/update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -p cabal2nix elm2nix -i bash ../../..
 
-cabal2nix https://github.com/elm/compiler --revision d5cbc41aac23da463236bbc250933d037da4055a > packages/elm.nix
+cabal2nix https://github.com/elm/compiler --revision c9aefb6230f5e0bda03205ab0499f6e4af924495 > packages/elm.nix
 elm2nix snapshot > versions.dat
-pushd "$(nix-build -A elmPackages.elm.src --no-out-link ../../../..)/ui/browser"
+pushd "$(nix-build -A elmPackages.elm.src --no-out-link ../../../..)/reactor"
   elm2nix convert > $OLDPWD/packages/elm-srcs.nix
 popd
