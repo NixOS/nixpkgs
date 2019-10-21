3b2c8246485195f5d2813760b9dba1228b7ad82a
Markus Wamser <wamserma@users.noreply.github.com>
2019-10-22 01:26:50 +0200

    Update pkgs/development/python-modules/sphinxcontrib-applehelp/default.nix
    Co-Authored-By: Jon <jonringer@users.noreply.github.com>

--- pkgs/development/python-modules/sphinxcontrib-applehelp/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-applehelp/default.nix
@@ -13,7 +13,6 @@ buildPythonPackage rec {
     sha256 = "edaa0ab2b2bc74403149cb0209d6775c96de797dfd5b5e2a71981309efab3897";
   };
 
-  propagatedBuildInputs = [  ];
 
   # Check is disabled due to circular dependency of sphinx
   doCheck = false;
