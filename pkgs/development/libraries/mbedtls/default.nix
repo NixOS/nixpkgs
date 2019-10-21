69e6d730583ef097a0827b9263531180940df53e
Franz Pletz <fpletz@fnordicwalking.de>
2019-10-21 12:48:22 +0200

    mbedtls: 2.16.0 -> 2.16.3
    Fixes CVE-2019-16910. Fixes #71452.
    
    (cherry picked from commit 360e57a567a7cb8fbe3d1f41dbb09238eccddefa)

--- pkgs/development/libraries/mbedtls/default.nix
+++ pkgs/development/libraries/mbedtls/default.nix
@@ -12,13 +12,13 @@
 stdenv.mkDerivation rec {
   pname = "mbedtls";
   name = "mbedtls-${version}";
-  version = "2.16.0";
+  version = "2.16.3";
 
   src = fetchFromGitHub {
     owner = "ARMmbed";
     repo = "mbedtls";
     rev = "${pname}-${version}";
-    sha256 = "14gw3rga9qr6j8ssfjy7k4l8spz37gamqxh9qcwas7w48303897l";
+    sha256 = "1mzh92yyz93099a1gb2wvwc76jv12d1k1wg9k3dimbgczxgrkirc";
   };
 
   nativeBuildInputs = [ cmake ninja perl python ];
