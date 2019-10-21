3e75f37c4eaa9db1317db0451b3cf46712e62ada
R. RyanTM <ryantm-bot@ryantm.com>
2019-10-21 12:17:37 -0700

    intel-gmmlib: 19.3.2 -> 19.3.3
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/intel-gmmlib/versions

--- pkgs/development/libraries/intel-gmmlib/default.nix
+++ pkgs/development/libraries/intel-gmmlib/default.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation rec {
   pname = "intel-gmmlib";
-  version = "19.3.2";
+  version = "19.3.3";
 
   src = fetchFromGitHub {
     owner  = "intel";
     repo   = "gmmlib";
     rev    = "${pname}-${version}";
-    sha256 = "04jmzyciswvfb31h9shkja9183bmm1clw0l0sn1flq76888nngma";
+    sha256 = "01nzibd2r82gh7906pds3hsdf1ip4w149k14dvhmndx0qs0ckya1";
   };
 
   nativeBuildInputs = [ cmake ];
