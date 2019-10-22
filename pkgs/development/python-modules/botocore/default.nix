708b97c832583669b9315271331c05e262cfae17
Robin Bate Boerop <robin@engineml.com>
2019-10-22 11:08:22 -0700

    Revert "Update awcli, botocore, boto3"
    This reverts commit a3d0af513e0ba8687630ad999a235b6de994c70d.

--- pkgs/development/python-modules/botocore/default.nix
+++ pkgs/development/python-modules/botocore/default.nix
@@ -12,11 +12,11 @@
 
 buildPythonPackage rec {
   pname = "botocore";
-  version = "1.12.253"; # N.B: if you change this, change boto3 and awscli to a matching version
+  version = "1.12.205"; # N.B: if you change this, change boto3 and awscli to a matching version
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "12y44x5r8w627wqyn3qd4ly2f0idv1k52vwjm6nh4mjp328i5brv";
+    sha256 = "19ls7hdmcaqrrq8przqy05s8chsy8315ic2zg185k6m64pvr0qhd";
   };
 
   outputs = [ "out" "dev" ];
