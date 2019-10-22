dbab6b7994a8ab80b836a631fa1cf6f5b2aa09ff
R. RyanTM <ryantm-bot@ryantm.com>
2019-10-21 23:45:35 -0700

    jmol: 14.29.53 -> 14.29.54
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/jmol/versions

--- pkgs/applications/science/chemistry/jmol/default.nix
+++ pkgs/applications/science/chemistry/jmol/default.nix
@@ -17,14 +17,14 @@ let
   };
 in
 stdenv.mkDerivation rec {
-  version = "14.29.53";
+  version = "14.29.54";
   pname = "jmol";
 
   src = let
     baseVersion = "${lib.versions.major version}.${lib.versions.minor version}";
   in fetchurl {
     url = "mirror://sourceforge/jmol/Jmol/Version%20${baseVersion}/Jmol%20${version}/Jmol-${version}-binary.tar.gz";
-    sha256 = "0wdkiprccgvc5nh6h616l282fbd5mx5z3aiq2y558qa4gn22nqrq";
+    sha256 = "0f4a50rkba8g19d4vm4my0alc10g6p4myn7nqbjlcawx3z8k1dpb";
   };
 
   patchPhase = ''
