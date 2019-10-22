05ebf348bcbf728a5a30b402180a058fa82e41f0
worldofpeace <worldofpeace@protonmail.ch>
2019-10-21 20:30:42 -0400

    deepin.deepin-wm: remove
    Upstream has deprecated this package. And it's currently broken.

--- pkgs/desktops/deepin/default.nix
+++ pkgs/desktops/deepin/default.nix
@@ -38,9 +38,6 @@ let
     };
     deepin-turbo = callPackage ./deepin-turbo { };
     deepin-wallpapers = callPackage ./deepin-wallpapers { };
-    deepin-wm = callPackage ./deepin-wm {
-      vala = pkgs.vala_0_40;
-    };
     dpa-ext-gnomekeyring = callPackage ./dpa-ext-gnomekeyring { };
     dtkcore = callPackage ./dtkcore { };
     dtkwidget = callPackage ./dtkwidget { };
