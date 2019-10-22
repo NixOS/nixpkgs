942eb66bf44c70992e12308de1c5e6bbdef36590
Danielle Lancashire <dani@builds.terrible.systems>
2019-10-22 19:36:12 +0200

    nomad: 0.9.5 -> 0.10.0
    This commit upgrades Nomad from 0.9.5 to 0.10.0 and also pins the Golang
    version to go1.12 as it is the only supported compiler for the current
    release, and due to some dependency issues currently does not support
    go1.13.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5093,7 +5093,9 @@ in
 
   noip = callPackage ../tools/networking/noip { };
 
-  nomad = callPackage ../applications/networking/cluster/nomad { };
+ nomad = callPackage ../applications/networking/cluster/nomad {
+    buildGoPackage = buildGo112Package;
+  };
 
   notable = callPackage ../applications/misc/notable { };
 
