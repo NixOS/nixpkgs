bc78dfe1f8a4c98aef3d25d235db5996915a38d6
R. RyanTM <ryantm-bot@ryantm.com>
2019-10-22 09:46:47 -0700

    mbuffer: 20190725 -> 20191016
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/mbuffer/versions

--- pkgs/tools/misc/mbuffer/default.nix
+++ pkgs/tools/misc/mbuffer/default.nix
@@ -3,12 +3,12 @@
  } :
 
 stdenv.mkDerivation rec {
-  version = "20190725";
+  version = "20191016";
   pname = "mbuffer";
 
   src = fetchurl {
     url = "http://www.maier-komor.de/software/mbuffer/mbuffer-${version}.tgz";
-    sha256 = "1hjhyh3q0q22czq3s2wk5mcky1jrq9xw8fppw6r7cix1riq74m91";
+    sha256 = "05xyvmbs2x5gbj2njgg7hsj3alb5dh96xhab0w0qkhb58x2i1hld";
   };
 
   buildInputs = [ openssl ];
