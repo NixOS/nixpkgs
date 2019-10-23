87ba52a6651fc1a107c9785a9e71812a1d4cb49e
R. RyanTM <ryantm-bot@ryantm.com>
2019-10-22 17:31:21 -0700

    ocamlPackages.lwt4: 4.2.1 -> 4.3.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/ocaml4.07.1-lwt/versions

--- pkgs/development/ocaml-modules/lwt/4.x.nix
+++ pkgs/development/ocaml-modules/lwt/4.x.nix
@@ -7,11 +7,11 @@ let inherit (lib) optional versionAtLeast; in
 
 buildDunePackage rec {
   pname = "lwt";
-  version = "4.2.1";
+  version = "4.3.0";
 
   src = fetchzip {
     url = "https://github.com/ocsigen/${pname}/archive/${version}.tar.gz";
-    sha256 = "1hz24fyhpm7d6603v399pgxvdl236srwagqja41ljvjx83y10ysr";
+    sha256 = "0f7036srqz7zmnz0n164734smgkrqz78r1i35cg30x31kkr3pnn4";
   };
 
   postPatch = ''
