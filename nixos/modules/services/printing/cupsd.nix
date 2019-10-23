fdb9b8ac6e5cbf87b30e41ab75ce77fa062dd768
caadar <v88m@posteo.net>
2019-10-23 19:57:01 +0300

    cups: fix misleading comment

--- nixos/modules/services/printing/cupsd.nix
+++ nixos/modules/services/printing/cupsd.nix
@@ -31,7 +31,7 @@ let
   # part of CUPS itself, e.g. the SMB backend is part of Samba.  Since
   # we can't update ${cups.out}/lib/cups itself, we create a symlink tree
   # here and add the additional programs.  The ServerBin directive in
-  # cupsd.conf tells cupsd to use this tree.
+  # cups-files.conf tells cupsd to use this tree.
   bindir = pkgs.buildEnv {
     name = "cups-progs";
     paths =
