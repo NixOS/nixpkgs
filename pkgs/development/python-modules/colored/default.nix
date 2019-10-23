cdcbf4400ff8ed46f30b4f9a1f5d8238a3ec8e77
R. RyanTM <ryantm-bot@ryantm.com>
2019-10-23 01:51:26 -0700

    python37Packages.colored: 1.3.93 -> 1.4.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-colored/versions

--- pkgs/development/python-modules/colored/default.nix
+++ pkgs/development/python-modules/colored/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "colored";
-  version = "1.3.93";
+  version = "1.4.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0xbhq9nd9xz3b6w0c4q33jfgnv8jid023v2fyhi7hsrz1scym5l2";
+    sha256 = "0wlsg7z406q31r5fpwkqfpyfpigazbmq9qm856wfbn861k2773zf";
   };
 
   # No proper test suite
