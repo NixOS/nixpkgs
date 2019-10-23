9ea43664864997c3c1efda84dbb9f23d8b494068
R. RyanTM <ryantm-bot@ryantm.com>
2019-10-22 20:07:26 -0700

    pgbouncer: 1.11.0 -> 1.12.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/pgbouncer/versions

--- pkgs/servers/sql/pgbouncer/default.nix
+++ pkgs/servers/sql/pgbouncer/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "pgbouncer";
-  version = "1.11.0";
+  version = "1.12.0";
 
   src = fetchurl {
     url = "https://pgbouncer.github.io/downloads/files/${version}/${pname}-${version}.tar.gz";
-    sha256 = "0w3y53kwnkcm9fmf28zbjvqk6ivfic5f2k3nflvca1i8iaj2z044";
+    sha256 = "0gi7ggmyjqd4kxdwm5csmzmjmfrjx7q20dfzk3da1bvc6xj6ag0v";
   };
 
   buildInputs = [ libevent openssl c-ares pkg-config ];
