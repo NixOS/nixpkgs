d414e213f2e0f9d211414964c90a3c5c1116fb6b
Jonathan Ringer <jonringer117@gmail.com>
2019-10-23 00:24:10 -0700

    sshtunnel: init at 0.1.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1301,6 +1301,8 @@ in {
 
   sshpubkeys = callPackage ../development/python-modules/sshpubkeys { };
 
+  sshtunnel = callPackage ../development/python-modules/sshtunnel { };
+
   sslib = callPackage ../development/python-modules/sslib { };
 
   statistics = callPackage ../development/python-modules/statistics { };
