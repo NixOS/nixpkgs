3db040133dec6ed5811043716a4bdedf0a2871c5
R. RyanTM <ryantm-bot@ryantm.com>
2019-10-22 22:17:27 -0700

    python37Packages.azure-mgmt-datafactory: 0.7.0 -> 0.8.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/python3.7-azure-mgmt-datafactory/versions

--- pkgs/development/python-modules/azure-mgmt-datafactory/default.nix
+++ pkgs/development/python-modules/azure-mgmt-datafactory/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "azure-mgmt-datafactory";
-  version = "0.7.0";
+  version = "0.8.0";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "7a50da8415e316bd3be0c90ff7e2bffee2afb959aefea23b5923f22dd7094a37";
+    sha256 = "0rv3443h4f9n88ky0fkfrp6jhf7ck9w3v96q040g3c2vkkywsnwa";
   };
 
   propagatedBuildInputs = [
