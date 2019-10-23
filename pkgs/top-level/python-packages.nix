d0e33f172d3584cdb84d7e362106343f7f7ad33b
Matthieu Coudron <mattator@gmail.com>
2019-10-23 12:58:27 +0900

    pythonPackages.python-doi: init at 0.1.1
    Required by the new papis 0.9

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6229,6 +6229,8 @@ in {
 
   python-docx = callPackage ../development/python-modules/python-docx { };
 
+  python-doi = callPackage ../development/python-modules/python-doi { };
+
   aiohue = callPackage ../development/python-modules/aiohue { };
 
   PyMVGLive = callPackage ../development/python-modules/pymvglive { };
