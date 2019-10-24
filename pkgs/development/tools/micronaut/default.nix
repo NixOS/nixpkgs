f70bd0da4012bd78271678aa50ba8a07c921d5be
Renaud <c0bw3b@users.noreply.github.com>
2019-10-24 15:13:39 +0200

    micronaut: keep description short
    and run pre+post hooks when overriding install phase

--- pkgs/development/tools/micronaut/default.nix
+++ pkgs/development/tools/micronaut/default.nix
@@ -12,25 +12,26 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ makeWrapper installShellFiles ];
 
   installPhase = ''
+    runHook preInstall
     rm bin/mn.bat
     cp -r . $out
     wrapProgram $out/bin/mn \
       --prefix JAVA_HOME : ${jdk} 
     installShellCompletion --bash --name mn.bash bin/mn_completion
+    runHook postInstall
   '';
 
   meta = with stdenv.lib; {
-    description = ''
-      A modern, JVM-based, full-stack framework for building modular, 
-      easily testable microservice and serverless applications.
-    '';
+    description = "Modern, JVM-based, full-stack framework for building microservice applications";
     longDescription = ''
+      Micronaut is a modern, JVM-based, full stack microservices framework
+      designed for building modular, easily testable microservice applications.
       Reflection-based IoC frameworks load and cache reflection data for 
       every single field, method, and constructor in your code, whereas with 
       Micronaut, your application startup time and memory consumption are 
       not bound to the size of your codebase.
     '';
-    homepage = https://micronaut.io/;
+    homepage = "https://micronaut.io/";
     license = licenses.asl20;
     platforms = platforms.all;
     maintainers = with maintainers; [ moaxcp ];
