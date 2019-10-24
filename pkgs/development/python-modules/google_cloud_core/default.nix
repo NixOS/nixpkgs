a7c80591ae44a0f07fdf63f3320a08832cf0bfc9
Frederik Rietdijk <fridh@fridh.nl>
2019-10-24 08:47:32 +0200

    python: google-cloud-core: 0.29.1 -> 1.0.3

--- pkgs/development/python-modules/google_cloud_core/default.nix
+++ pkgs/development/python-modules/google_cloud_core/default.nix
@@ -3,11 +3,11 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-core";
-  version = "0.29.1";
+  version = "1.0.3";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "d85b1aaaf3bad9415ad1d8ee5eadce96d7007a82f13ce0a0629a003a11e83f29";
+    sha256 = "10750207c1a9ad6f6e082d91dbff3920443bdaf1c344a782730489a9efa802f1";
   };
 
   propagatedBuildInputs = [ google_api_core grpcio setuptools ];
