c9a35c5e7061577ce104ad6bef6723412cf53833
Frederik Rietdijk <fridh@fridh.nl>
2019-10-24 08:47:26 +0200

    python: botocore: 1.12.251 -> 1.13.1

--- pkgs/development/python-modules/botocore/default.nix
+++ pkgs/development/python-modules/botocore/default.nix
@@ -12,11 +12,11 @@
 
 buildPythonPackage rec {
   pname = "botocore";
-  version = "1.12.251"; # N.B: if you change this, change boto3 and awscli to a matching version
+  version = "1.13.1"; # N.B: if you change this, change boto3 and awscli to a matching version
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "887a57b3dcfb1d29ae11e504a486cd8dbe1af4f5678980a804dbf02b36d1c4f8";
+    sha256 = "acceec0b79df7e5f8b15eab3033f6a7c7f69d0bc27aa01d65aa5ba4d90742787";
   };
 
   outputs = [ "out" "dev" ];
