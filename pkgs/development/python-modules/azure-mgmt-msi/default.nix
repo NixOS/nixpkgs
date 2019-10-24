5a2a14a4da6baa8edf36cfb813c2091600035164
Jonathan Ringer <jonringer117@gmail.com>
2019-10-24 09:06:26 -0700

    pythonPackages.azure-mgmt-msi: fix python3 namespace

--- pkgs/development/python-modules/azure-mgmt-msi/default.nix
+++ pkgs/development/python-modules/azure-mgmt-msi/default.nix
@@ -1,6 +1,8 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, python
+, isPy3k
 , msrest
 , msrestazure
 , azure-common
@@ -23,6 +25,11 @@ buildPythonPackage rec {
     azure-common
     azure-mgmt-nspkg
   ];
+
+  postInstall = lib.optionalString isPy3k ''
+    rm -f $out/${python.sitePackages}/azure/__init__.py
+    rm -f $out/${python.sitePackages}/azure/mgmt/__init__.py
+  '';
 
   # has no tests
   doCheck = false;
