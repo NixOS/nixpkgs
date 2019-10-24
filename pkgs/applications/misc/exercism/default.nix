f03715ca8673e63ec9d15bd23314e4fa4b4f2284
Norbert Melzer <nmelzer@nobbz.dev>
2019-10-24 21:09:20 +0200

    exercism: 3.0.12 -> 3.0.13

--- pkgs/applications/misc/exercism/default.nix
+++ pkgs/applications/misc/exercism/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "exercism";
-  version = "3.0.12";
+  version = "3.0.13";
 
   src = fetchFromGitHub {
     owner  = "exercism";
     repo   = "cli";
     rev    = "v${version}";
-    sha256 = "1xvxcl7j5izx5lgmjd97zd28lg2sydwgbgn2cnisz5r0d27pj3ra";
+    sha256 = "17gvz9a0sn4p36hf4l77bxhhfipf4x998iay31layqwbnzmb4xy7";
   };
 
   modSha256 = "0pg0hxrr6jjd03wbjn5y65x02md3h352mnm1gr6vyiv7hn4ws14m";
