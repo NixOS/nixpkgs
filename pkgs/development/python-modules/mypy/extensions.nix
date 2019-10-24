e70f0a4cc291df7c1ddf9cf17c5f17cbb92c3e0e
Frederik Rietdijk <fridh@fridh.nl>
2019-10-24 08:47:39 +0200

    python: mypy_extensions: 0.4.2 -> 0.4.3

--- pkgs/development/python-modules/mypy/extensions.nix
+++ pkgs/development/python-modules/mypy/extensions.nix
@@ -2,14 +2,14 @@
 
 buildPythonPackage rec {
   pname = "mypy_extensions";
-  version = "0.4.2";
+  version = "0.4.3";
 
   # Tests not included in pip package.
   doCheck = false;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a161e3b917053de87dbe469987e173e49fb454eca10ef28b48b384538cc11458";
+    sha256 = "2d82818f5bb3e369420cb3c4060a7970edba416647068eb4c5343488a6c604a8";
   };
 
   propagatedBuildInputs = if pythonOlder "3.5" then [ typing ] else [ ];
