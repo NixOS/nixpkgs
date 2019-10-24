3eef2318bb6961491dcf09388770a8f5a665b258
R. RyanTM <ryantm-bot@ryantm.com>
2019-10-23 19:10:28 -0700

    swaks: 20181104.0 -> 20190914.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/swaks/versions

--- pkgs/tools/networking/swaks/default.nix
+++ pkgs/tools/networking/swaks/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "swaks";
-  version = "20181104.0";
+  version = "20190914.0";
 
   src = fetchurl {
     url = "https://www.jetmore.org/john/code/swaks/files/${pname}-${version}.tar.gz";
-    sha256 = "0n1yd27xcyb1ylp5gln3yv5gzi9r377hjy1j32367kgb3247ygq2";
+    sha256 = "12awq5z4sdd54cxprj834zajxhkpy4jwhzf1fhigcx1zbhdaacsp";
   };
 
   buildInputs = [ perl makeWrapper ];
