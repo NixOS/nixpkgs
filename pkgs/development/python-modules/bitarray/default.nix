ea1fc916a0ae9fecda0ca579c9489a166b864853
Jonathan Ringer <jonringer117@gmail.com>
2019-10-26 15:38:03 -0700

    pythonPackages.bitarray: fix build
    remove obsolete patch

--- pkgs/development/python-modules/bitarray/default.nix
+++ pkgs/development/python-modules/bitarray/default.nix
@@ -9,9 +9,6 @@ buildPythonPackage rec {
     sha256 = "3eb500f8b9cde19e14472fcbee0195dbf0fbac006f8406a03f0cfb495dff20a0";
   };
 
-  # Delete once https://github.com/ilanschnell/bitarray/pull/55 is merged
-  patches = [ ./0001-Buffer-Protocol-Py3.patch ];
-
   meta = with lib; {
     description = "Efficient arrays of booleans";
     homepage = https://github.com/ilanschnell/bitarray;
