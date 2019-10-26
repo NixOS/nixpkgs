cc751ef8a88e8af8ee69e19984658552f6e542af
Jonathan Ringer <jonringer117@gmail.com>
2019-10-26 10:44:00 -0700

    pythonPackages.pycurl: disable flakey memory tests
    (cherry picked from commit 95e3e3e0a8486bd63a66cc1c4229a9965a6dfd36)

--- pkgs/development/python-modules/pycurl/default.nix
+++ pkgs/development/python-modules/pycurl/default.nix
@@ -35,13 +35,16 @@ buildPythonPackage rec {
     flaky
   ];
 
+  # skip impure or flakey tests
   checkPhase = ''
     pytest tests -k "not test_ssl_in_static_libs \
                      and not test_keyfunction \
                      and not test_keyfunction_bogus_return \
                      and not test_libcurl_ssl_gnutls \
                      and not test_libcurl_ssl_nss \
-                     and not test_libcurl_ssl_openssl"
+                     and not test_libcurl_ssl_openssl" \
+                 --ignore=tests/getinfo_test.py \
+                 --ignore=tests/memory_mgmt_test.py
   '';
 
   preConfigure = ''
