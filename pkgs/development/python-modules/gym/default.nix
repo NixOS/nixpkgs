4fbf45665f011607405ea5c74b5121d3b9cc4232
Jonathan Ringer <jonringer117@gmail.com>
2019-10-26 17:14:36 -0700

    python3Packages.gym: fix build

--- pkgs/development/python-modules/gym/default.nix
+++ pkgs/development/python-modules/gym/default.nix
@@ -1,6 +1,6 @@
 { lib
 , buildPythonPackage, fetchPypi
-, numpy, requests, six, pyglet, scipy
+, numpy, requests, six, pyglet, scipy, cloudpickle
 }:
 
 buildPythonPackage rec {
@@ -12,8 +12,13 @@ buildPythonPackage rec {
     sha256 = "18381e13bbd1e2f206a1b88a2af4fb87affd7d06ee7955a6e5e6a79478a9adfc";
   };
 
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace "pyglet>=1.2.0,<=1.3.2" "pyglet"
+  '';
+
   propagatedBuildInputs = [
-    numpy requests six pyglet scipy
+    numpy requests six pyglet scipy cloudpickle
   ];
 
   # The test needs MuJoCo that is not free library.
