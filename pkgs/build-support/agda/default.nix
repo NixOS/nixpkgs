ac8eaa85070e736af1548faeeb4b9b6e9976494d
Robin Gloster <mail@glob.in>
2019-10-28 10:17:49 +0100

    treewide: fix *Flags

--- pkgs/build-support/agda/default.nix
+++ pkgs/build-support/agda/default.nix
@@ -47,9 +47,9 @@ let
     includeDirs = self.buildDependsAgdaShareAgda
                   ++ self.sourceDirectories ++ self.topSourceDirectories
                   ++ [ "." ];
-    buildFlags = concatStringsSep " " (map (x: "-i " + x) self.includeDirs);
+    buildFlags = stdenv.lib.concatMap (x: ["-i" x]) self.includeDirs;
 
-    agdaWithArgs = "${Agda}/bin/agda ${self.buildFlags}";
+    agdaWithArgs = "${Agda}/bin/agda ${toString self.buildFlags}";
 
     buildPhase = ''
       runHook preBuild
