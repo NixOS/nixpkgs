785035628301baa00d88b84519f4e4154770ff4f
Frederik Rietdijk <fridh@fridh.nl>
2019-10-29 12:49:41 +0100

    pythonPackages.libxslt: fix reference to output

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3691,7 +3691,7 @@ in {
 
   libxml2 = (toPythonModule (pkgs.libxml2.override{pythonSupport=true; inherit python;})).py;
 
-  libxslt = toPythonModule (pkgs.libxslt.override{pythonSupport=true; inherit python; inherit (self) libxml2;}).py;
+  libxslt = (toPythonModule (pkgs.libxslt.override{pythonSupport=true; inherit python; inherit (self) libxml2;})).py;
 
   limits = callPackage ../development/python-modules/limits { };
 
