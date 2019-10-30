6d08d0eb52e0a5e8a4bfa0c13e3329b425148cf9
José Luis Lafuente <jl@lafuente.me>
2019-10-30 11:16:13 +0100

    k9s: 0.8.4 -> 0.9.3
    Also add missed build flags

--- pkgs/applications/networking/cluster/k9s/default.nix
+++ pkgs/applications/networking/cluster/k9s/default.nix
@@ -2,16 +2,25 @@
 
 buildGoModule rec {
   pname = "k9s";
-  version = "0.8.4";
+  version = "0.9.3";
+  # rev is the release commit, mainly for version command output
+  rev = "1a9a83b34cdd0c9b4e793ed6b4b5c16ea1a949a0";
 
   src = fetchFromGitHub {
     owner  = "derailed";
     repo   = "k9s";
     rev    = version;
-    sha256 = "0wsj6wc2qi5708cg47l2qblq1cg8fcwxdygpkayib9hapx6lc6f8";
+    sha256 = "0k27mfccz563r18zlbaxll305vrmrx19ym6znsikvqxlmhy86g36";
   };
 
-  modSha256 = "1ia9wx6yd9mdr981lcw58xv39iqzz25r03bmn1c6byxmq2xpcjq8";
+  buildFlagsArray = ''
+    -ldflags=
+      -s -w
+      -X github.com/derailed/k9s/cmd.version=${version}
+      -X github.com/derailed/k9s/cmd.commit=${rev}
+  '';
+
+  modSha256 = "09rwbl8zd06ax5hidm5l1schwqvsr5ndlqh09w1rq9fqjijy649y";
 
   meta = with stdenv.lib; {
     description = "Kubernetes CLI To Manage Your Clusters In Style.";
