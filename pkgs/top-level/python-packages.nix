70d788cf186b2bf8c458d58d916c07b7578dd16b
tbenst <lensman@tylerbenster.com>
2019-10-31 13:54:43 -0700

    python3packages.lightparam: init at 0.3.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2707,6 +2707,8 @@ in {
 
   lightning = callPackage ../development/python-modules/lightning { };
 
+  lightparam = callPackage ../development/python-modules/lightparam { };
+
   jupyter = callPackage ../development/python-modules/jupyter { };
 
   jupyter_console = if pythonOlder "3.5" then
