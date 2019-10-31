94ae704df6c5e40a5f80311ef89077109cff426e
Leon Schuermann <leon@is.currently.online>
2019-10-31 20:04:39 +0100

    tpm2-tools: 4.0 -> 4.0.1

--- pkgs/tools/security/tpm2-tools/default.nix
+++ pkgs/tools/security/tpm2-tools/default.nix
@@ -4,11 +4,11 @@
 
 stdenv.mkDerivation rec {
   pname = "tpm2-tools";
-  version = "4.0";
+  version = "4.0.1";
 
   src = fetchurl {
     url = "https://github.com/tpm2-software/${pname}/releases/download/${version}/${pname}-${version}.tar.gz";
-    sha256 = "02p0wj87fnrpsijd2zaqcxqxicqs36q7vakp6y8and920x36jb0y";
+    sha256 = "zOw/ymNwNBoQLFwu8d205c0kK/G7xsUdlp93/HjKZ9E=";
   };
 
   nativeBuildInputs = [ pandoc pkgconfig makeWrapper ];
