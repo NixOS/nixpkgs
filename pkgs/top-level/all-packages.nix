8b41fa2cc093d26ddee8a90164793ab61012f62d
Jan Hrnko <jan.hrnko@satoshilabs.com>
2019-11-01 10:56:59 +0100

    trickster: init at 0.1.10

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16679,6 +16679,8 @@ in
 
   tpacpi-bat = callPackage ../os-specific/linux/tpacpi-bat { };
 
+  trickster = callPackage ../servers/trickster/trickster.nix {};
+
   trinity = callPackage ../os-specific/linux/trinity { };
 
   tunctl = callPackage ../os-specific/linux/tunctl { };
