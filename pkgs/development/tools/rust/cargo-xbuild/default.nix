d4e516cc82af50264529e6f26c263194b4f04960
Maximilian Bosch <maximilian@mbosch.me>
2019-11-01 18:57:41 +0100

    cargo-xbuild: 0.5.15 -> 0.5.18
    https://github.com/rust-osdev/cargo-xbuild/blob/master/CHANGELOG.md#v0518---2019-10-08

--- pkgs/development/tools/rust/cargo-xbuild/default.nix
+++ pkgs/development/tools/rust/cargo-xbuild/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-xbuild";
-  version = "0.5.15";
+  version = "0.5.18";
 
   src = fetchFromGitHub {
     owner = "rust-osdev";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0ck3gwgxbg03z864bhqy8vwcpm7al17fm380zsb6ijb1q2sk2r2n";
+    sha256 = "1hcsdwwl1xc59f1ppwlxj1zyp1md07z70gfvg4zqvafc6dzx708j";
   };
 
-  cargoSha256 = "1r9i79lymfwpbcx2lp509v435qpkl9bqly1ya369p41n5yprrcjv";
+  cargoSha256 = "1pj4x8y5vfpnn8vhxqqm3vicn29870r3jh0b17q3riq4vz1a2afp";
 
   meta = with stdenv.lib; {
     description = "Automatically cross-compiles the sysroot crates core, compiler_builtins, and alloc";
