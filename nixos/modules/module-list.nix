9b0aefac86e743bded74688773e15744f761678c
Jan Hrnko <jan.hrnko@satoshilabs.com>
2019-11-01 10:57:29 +0100

    nixos/trickster: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -713,6 +713,7 @@
   ./services/networking/tinc.nix
   ./services/networking/tinydns.nix
   ./services/networking/tftpd.nix
+  ./services/networking/trickster.nix
   ./services/networking/tox-bootstrapd.nix
   ./services/networking/tox-node.nix
   ./services/networking/toxvpn.nix
