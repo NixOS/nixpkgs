a45f93171b399e12d6d1a7d0f9ee027372b2878c
Franz Pletz <fpletz@fnordicwalking.de>
2019-11-02 18:54:37 +0100

    bctoolbox: fix build with gcc9

--- pkgs/development/libraries/bctoolbox/default.nix
+++ pkgs/development/libraries/bctoolbox/default.nix
@@ -1,9 +1,17 @@
-{stdenv, fetchFromGitHub, cmake, mbedtls, bcunit, srtp}:
+{stdenv, fetchpatch, fetchFromGitHub, cmake, mbedtls, bcunit, srtp}:
 stdenv.mkDerivation rec {
   name = "${baseName}-${version}";
   baseName = "bctoolbox";
   version = "0.6.0";
-  buildInputs = [cmake mbedtls bcunit srtp];
+  nativeBuildInputs = [cmake];
+  buildInputs = [mbedtls bcunit srtp];
+  patches = [
+    (fetchpatch {
+      name = "fix-gcc9.patch";
+      url = "https://github.com/BelledonneCommunications/bctoolbox/commit/2425a224634c7a70ae91f809bbab51771f0a30d7.patch";
+      sha256 = "0ib859mwwlr6jk6qbwp6x25pp8wgv21lh0qgdsx5hyzwdzqx1b3h";
+    })
+  ];
   src = fetchFromGitHub {
     owner = "BelledonneCommunications";
     repo = baseName;
