59a5ba4cbee8d98438fbb4b5f22b0e359e9057b1
Alyssa Ross <hi@alyssa.is>
2019-11-02 14:03:09 +0100

    minijail: android-9.0.0_r3 -> android-10.0.0-r9

--- pkgs/tools/system/minijail/default.nix
+++ pkgs/tools/system/minijail/default.nix
@@ -1,14 +1,13 @@
 { stdenv, fetchgit, libcap }:
 
 stdenv.mkDerivation rec {
-  shortname = "minijail";
-  name = "${shortname}-${version}";
-  version = "android-9.0.0_r3";
+  pname = "minijail";
+  version = "android-10.0.0_r9";
 
   src = fetchgit {
     url = "https://android.googlesource.com/platform/external/minijail";
     rev = version;
-    sha256 = "1g1g52s3q61amcnx8cv1332sbixpck1bmjzgsrjiw5ix7chrzkp2";
+    sha256 = "0gcfsyim1krrddcklydqfxl8mamaxgail2xl5qp9yclq60km8f22";
   };
 
   buildInputs = [ libcap ];
