569611ff22feaea56c463aa5e9438e039145fce3
Marek Mahut <marek.mahut@gmail.com>
2019-11-02 11:00:31 +0100

    Revert "frostwire-bin: 6.7.4 -> 6.8.3"

--- pkgs/applications/networking/p2p/frostwire/frostwire-bin.nix
+++ pkgs/applications/networking/p2p/frostwire/frostwire-bin.nix
@@ -3,12 +3,12 @@
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
-  version = "6.8.3";
+  version = "6.7.4";
   pname = "frostwire";
 
   src = fetchurl {
     url = "https://dl.frostwire.com/frostwire/${version}/frostwire-${version}.noarch.tar.gz";
-    sha256 = "00r7rxzk9a7a1sc0shdx7fq9p9dzg5pwfdf8day0kr1844a5arc2";
+    sha256 = "03vxg0qas4mz5ggrmi396nkz44x1kgq8bfbhbr9mnal9ay9qmi8m";
   };
 
   nativeBuildInputs = [ makeWrapper ];
@@ -18,13 +18,11 @@ stdenv.mkDerivation rec {
     mv $(ls */*.jar) $out/share/java
 
     makeWrapper $out/share/java/frostwire $out/bin/frostwire \
-      --prefix PATH : ${jre}/bin/ \
-      --set JAVA_HOME ${jre.home} \
-      --add-flags '-classpath $CLASSPATH:$out/share/java/*'
+      --prefix PATH : ${jre}/bin/
   '';
 
   meta = with stdenv.lib; {
-    homepage = "https://www.frostwire.com/";
+    homepage = https://www.frostwire.com/;
     description = "BitTorrent Client and Cloud File Downloader";
     license = licenses.gpl2;
     maintainers = with maintainers; [ gavin ];
