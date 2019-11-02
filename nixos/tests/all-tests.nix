52ed193ec7f167579b8acbb27d02283ae9e92eff
Florian Klink <flokli@flokli.de>
2019-11-02 19:55:41 +0100

    nixosTests.systemd-nspawn: add test
    This adds a test downloading an nspawn container via http, and ensures
    sha256sum verification and gpg signature verification work.

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -269,6 +269,7 @@ in
   systemd-confinement = handleTest ./systemd-confinement.nix {};
   systemd-timesyncd = handleTest ./systemd-timesyncd.nix {};
   systemd-networkd-wireguard = handleTest ./systemd-networkd-wireguard.nix {};
+  systemd-nspawn = handleTest ./systemd-nspawn.nix {};
   pdns-recursor = handleTest ./pdns-recursor.nix {};
   taskserver = handleTest ./taskserver.nix {};
   telegraf = handleTest ./telegraf.nix {};
