25eb02ab383d46b35024154cd659675764bf988e
R. RyanTM <ryantm-bot@ryantm.com>
2019-11-01 17:44:50 -0700

    gnu-efi: 3.0.9 -> 3.0.11

--- pkgs/development/libraries/gnu-efi/default.nix
+++ pkgs/development/libraries/gnu-efi/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnu-efi";
-  version = "3.0.9";
+  version = "3.0.11";
 
   src = fetchurl {
     url = "mirror://sourceforge/gnu-efi/${pname}-${version}.tar.bz2";
-    sha256 = "1w3p4aqlc5j93q44la7dc8cr3hky20zvsd0h0k2lyzhwmrzfl5b7";
+    sha256 = "1ffnc4xbzfggs37ymrgfx76j56kk2644c081ivhr2bjkla9ag3gj";
   };
 
   buildInputs = [ pciutils ];
