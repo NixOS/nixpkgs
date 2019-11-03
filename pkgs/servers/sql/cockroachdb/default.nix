55a30b39f5d3966c302f68b1a185839f1ff14c23
Franz Pletz <fpletz@fnordicwalking.de>
2019-11-03 14:09:10 +0100

    cockroachdb: fix build with gcc9

--- pkgs/servers/sql/cockroachdb/default.nix
+++ pkgs/servers/sql/cockroachdb/default.nix
@@ -22,6 +22,8 @@ buildGoPackage rec {
     sha256 = "1pnzzmxxb7qxiiy8qpl2sifk4qrijjbhmzy47bnjj5ssdsjjjcqy";
   };
 
+  NIX_CFLAGS_COMPILE = [ "-Wno-error=deprecated-copy" "-Wno-error=redundant-move" "-Wno-error=pessimizing-move" ];
+
   inherit nativeBuildInputs buildInputs;
 
   buildPhase = ''
