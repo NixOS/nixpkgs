412d0a9f2f3863ad3c305e36953362261d1d9e4c
R. RyanTM <ryantm-bot@ryantm.com>
2019-11-03 01:20:37 -0800

    sysbench: 1.0.17 -> 1.0.18

--- pkgs/development/tools/misc/sysbench/default.nix
+++ pkgs/development/tools/misc/sysbench/default.nix
@@ -2,7 +2,7 @@
 , libaio }:
 
 stdenv.mkDerivation {
-  name = "sysbench-1.0.17";
+  name = "sysbench-1.0.18";
 
   nativeBuildInputs = [ autoreconfHook pkgconfig ];
   buildInputs = [ vim libmysqlclient libaio ];
@@ -10,8 +10,8 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "akopytov";
     repo = "sysbench";
-    rev = "1.0.17";
-    sha256 = "02i9knvp0bjw6ri848xxiy2dbww2xv70nah9yn67a6zgw617hwa6";
+    rev = "1.0.18";
+    sha256 = "1r6lkyfp65xqklj1rdfw551srqqyak144agi8x3wjz3wmsbqls19";
   };
 
   meta = {
