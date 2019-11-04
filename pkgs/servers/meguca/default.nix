72a61efdf8dc7da091aaa1fd9cdb661cae01fe94
Okinan <okinan@chiru.no>
2019-11-03 22:57:29 -0600

    meguca: 2019-03-12 -> 6.5.3

--- pkgs/servers/meguca/default.nix
+++ pkgs/servers/meguca/default.nix
@@ -1,52 +1,39 @@
-{ stdenv, buildGoPackage, fetchFromGitHub, pkgconfig, cmake, ffmpeg-full
-, ghostscript, graphicsmagick, quicktemplate, go-bindata, easyjson
-, nodePackages, emscripten, opencv, statik }:
+{ lib, buildGoModule, fetchFromGitHub, pkgconfig, nodePackages, pth
+, ffmpeg-full, opencv, geoip }:
 
-buildGoPackage {
-  pname = "meguca-unstable";
-  version = "2019-03-12";
-  goPackagePath = "github.com/bakape/meguca";
-  goDeps = ./server_deps.nix;
+buildGoModule rec {
+  pname = "meguca";
+  version = "6.5.3";
 
   src = fetchFromGitHub {
     owner = "bakape";
     repo = "meguca";
-    rev = "21b08de09b38918061c5cd0bbd0dc9bcc1280525";
-    sha256 = "1nb3bf1bscbdma83sp9fbgvmxxlxh21j9h80wakfn85sndcrws5i";
-    fetchSubmodules = true;
+    rev = "v${version}";
+    sha256 = "0fbldmbjg33whgphkpngx19vs6ch0b709li1fhgxzqik0qzy37fs";
   };
 
+  modSha256 = "0ic3nc6pil3rw4spl9va6sykfk71a4y8qa9j6h8ppx8a095rq9w4";
   enableParallelBuilding = true;
-  nativeBuildInputs = [ pkgconfig cmake ];
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [ pth ffmpeg-full opencv geoip ];
 
-  buildInputs = [
-    ffmpeg-full graphicsmagick ghostscript quicktemplate go-bindata
-    easyjson emscripten opencv statik
-  ];
-
-  buildPhase = ''
-    export HOME=`pwd`
-    cd go/src/github.com/bakape/meguca
+  postBuild = ''
     ln -sf ${nodePackages.meguca}/lib/node_modules/meguca/node_modules
     sed -i "/npm install --progress false --depth 0/d" Makefile
-    make -j $NIX_BUILD_CORES generate all
-  '' + stdenv.lib.optionalString (!stdenv.isDarwin) ''
-    make -j $NIX_BUILD_CORES wasm
+    make client
   '';
 
-  installPhase = ''
-    mkdir -p $bin/bin $bin/share/meguca
-    cp meguca $bin/bin
-    cp -r www $bin/share/meguca
+  postInstall = ''
+    mkdir -p $out/share
+    rm -rf www/videos
+    cp -r www $out/share
   '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     homepage = "https://github.com/bakape/meguca";
-    description = "High performance anonymous realtime imageboard";
+    description = "Anonymous realtime imageboard focused on high performance, free speech, and transparent moderation";
     license = licenses.agpl3Plus;
-    maintainers = with maintainers; [ chiiruno ];
     platforms = platforms.all;
-    broken = true; # Broken on Hydra since 2019-04-18:
-    # https://hydra.nixos.org/build/98885902
+    maintainers = with maintainers; [ chiiruno ];
   };
 }
