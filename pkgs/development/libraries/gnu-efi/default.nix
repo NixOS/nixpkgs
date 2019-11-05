901e851fe9f3abbf7c25bc4c38cbc7903856f712
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-11-05 00:43:07 -0500

    gnu-efi: apply patch to fix build on armv6l

--- pkgs/development/libraries/gnu-efi/default.nix
+++ pkgs/development/libraries/gnu-efi/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildPackages, fetchurl, pciutils }:
+{ stdenv, buildPackages, fetchurl, fetchpatch, pciutils }:
 
 with stdenv.lib;
 
@@ -10,6 +10,14 @@ stdenv.mkDerivation rec {
     url = "mirror://sourceforge/gnu-efi/${pname}-${version}.tar.bz2";
     sha256 = "1w3p4aqlc5j93q44la7dc8cr3hky20zvsd0h0k2lyzhwmrzfl5b7";
   };
+
+  patches = [
+    # Fix build on armv6l
+    (fetchpatch {
+      url = "https://sourceforge.net/p/gnu-efi/patches/_discuss/thread/25bb273a18/9c4d/attachment/0001-Fix-ARCH-on-armv6-and-other-32-bit-ARM-platforms.patch";
+      sha256 = "0pj03h20g2bbz6fr753bj1scry6919h57l1h86z3b6q7hqfj0b4r";
+    })
+  ];
 
   buildInputs = [ pciutils ];
 
