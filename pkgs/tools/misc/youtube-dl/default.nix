bc9a9cc03f9b6980849654e613bea3739e5b215d
xrelkd <46590321+xrelkd@users.noreply.github.com>
2019-11-05 08:08:53 +0800

    youtube-dl: 2019.10.29 -> 2019.11.05
    (cherry picked from commit a17cda6ddf06970522dd57a352496c29027c18af)

--- pkgs/tools/misc/youtube-dl/default.nix
+++ pkgs/tools/misc/youtube-dl/default.nix
@@ -18,11 +18,11 @@ buildPythonPackage rec {
   # The websites youtube-dl deals with are a very moving target. That means that
   # downloads break constantly. Because of that, updates should always be backported
   # to the latest stable release.
-  version = "2019.10.29";
+  version = "2019.11.05";
 
   src = fetchurl {
     url = "https://yt-dl.org/downloads/${version}/${pname}-${version}.tar.gz";
-    sha256 = "1lq6ycjbx07831s24yx42q6m6svas4mf02vbszw0965dbbzs7vp4";
+    sha256 = "129461i4103slqj3nq69djnlmgjj3lfgmazn41avc5g967w29b85";
   };
 
   nativeBuildInputs = [ makeWrapper ];
