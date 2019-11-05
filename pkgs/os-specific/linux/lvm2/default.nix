55160f6d401fd4f1412e46c8528648dcd8549602
Kirill Elagin <kirelagin@gmail.com>
2019-11-05 12:14:43 +0300

    Revert "lvm2: enable parallel building"
    This reverts commit 494d2deebfc3bde363ca5fe0bb32ddf81a86e62d.
    
    I am getting
    
    ```
    gcc: error: ../../device_mapper/libdevice-mapper.a: No such file or directory
    ```

--- pkgs/os-specific/linux/lvm2/default.nix
+++ pkgs/os-specific/linux/lvm2/default.nix
@@ -40,7 +40,8 @@ stdenv.mkDerivation {
         --replace /usr/bin/udevadm ${systemd}/bin/udevadm
     '';
 
-  enableParallelBuilding = true;
+  # gcc: error: ../../device_mapper/libdevice-mapper.a: No such file or directory
+  enableParallelBuilding = false;
 
   patches = stdenv.lib.optionals stdenv.hostPlatform.isMusl [
     (fetchpatch {
