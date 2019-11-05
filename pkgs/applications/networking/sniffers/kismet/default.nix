602907127e842ff4edcfc7a19a8dfcb4363a2e12
Jonathan Ringer <jonringer117@gmail.com>
2019-11-05 09:23:06 -0800

    kismet: 2016-07-R1 -> 2019-09-R1

--- pkgs/applications/networking/sniffers/kismet/default.nix
+++ pkgs/applications/networking/sniffers/kismet/default.nix
@@ -1,16 +1,48 @@
-{ stdenv, fetchurl, pkgconfig, libpcap, ncurses, expat, pcre, libnl }:
+{ stdenv, fetchurl
+, pkgconfig
+, libpcap
+, ncurses
+, expat
+, pcre
+, python3
+, libnl
+, zlib
+, libmicrohttpd
+, sqlite
+, protobuf
+, protobufc
+, libusb1
+}:
 
+let
+  py3 = python3.withPackages(ps: with ps; [ setuptools ]);
+in
 stdenv.mkDerivation rec {
   pname = "kismet";
-  version = "2016-07-R1";
+  version = "2019-09-R1";
 
   src = fetchurl {
     url = "https://www.kismetwireless.net/code/${pname}-${version}.tar.xz";
-    sha256 = "0dz28y4ay4lskhl0lawqy2dkcrhgfkbg06v22qxzzw8i6caizcmx";
+    sha256 = "0vffcvyx777kd65acm9hzcxbf689k33ifkszkwgrbaab0kj3mwy3";
   };
 
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ libpcap ncurses expat pcre libnl ];
+
+  buildInputs = [
+    expat
+    libmicrohttpd
+    libnl
+    libpcap
+    libusb1
+    ncurses
+    pcre
+    protobuf
+    protobufc
+    py3
+    sqlite
+    zlib
+  ];
+
   postConfigure = ''
     sed -e 's/-o $(INSTUSR)//' \
         -e 's/-g $(INSTGRP)//' \
