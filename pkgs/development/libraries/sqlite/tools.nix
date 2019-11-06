9aac23db63b3616f8f6f2bdc5ce65f6b57a0a883
Daniel Schaefer <git@danielschaefer.me>
2019-11-06 17:23:45 +0100

    sqlite-analyzer: 3.28.0 -> 3.30.0

--- pkgs/development/libraries/sqlite/tools.nix
+++ pkgs/development/libraries/sqlite/tools.nix
@@ -4,11 +4,11 @@ let
   archiveVersion = import ./archive-version.nix stdenv.lib;
   mkTool = { pname, makeTarget, description, homepage }: stdenv.mkDerivation rec {
     inherit pname;
-    version = "3.28.0";
+    version = "3.30.0";
 
     src = assert version == sqlite.version; fetchurl {
       url = "https://sqlite.org/2019/sqlite-src-${archiveVersion version}.zip";
-      sha256 = "15v57b113bpgcshfsx5jw93szar3da94rr03i053xhl15la7jllh";
+      sha256 = "0d4i87q0f618pmrgax0mr5x7m8bywikrwjvixag3biyhgl5rx7fd";
     };
 
     nativeBuildInputs = [ unzip ];
