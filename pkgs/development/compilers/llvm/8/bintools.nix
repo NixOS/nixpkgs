bf31a9a7ef4b1e5c36eaf5b088d04e2264454c4a
John Ericson <git@JohnEricson.me>
2019-11-06 09:36:12 -0500

    llvm 8 binutils: Add windres wrapper from llvm-mingw

--- pkgs/development/compilers/llvm/8/bintools.nix
+++ pkgs/development/compilers/llvm/8/bintools.nix
@@ -1,6 +1,30 @@
-{ runCommand, stdenv, llvm, lld, version }:
+{ fetchFromGitHub, runCommand, stdenv, llvm, lld, version }:
 
 let
+  # Wrappers that implement some functionality before upstream has it.
+  llvm-mingw = stdenv.mkDerivation rec {
+    name = "llvm-mingw";
+    version = "git-${src.rev}";
+    src = fetchFromGitHub {
+      repo = "llvm-mingw";
+      owner = "mstorsjo";
+      rev = "5d764329d80fe6f0ae3f067d0b606062eb466712";
+      sha256 = "0q3vmjjjrmlpdnqg9b81x4797kvhdbn6yhlambfjnvk4pzi7hglv";
+    };
+    postUnpack = ''
+      sourceRoot=''${sourceRoot}/wrappers;
+    '';
+    buildPhase = ''
+      $CC -iquote . windres-wrapper.c -o llvm-windres
+    '';
+    #buildPhase = ''
+    #  $CC -iquote . windres-wrapper.c -DLLVM_PATH=\"${llvm}/bin/\" -o llvm-windres
+    #'';
+    installPhase = ''
+      mkdir -p "$out/bin"
+      install -m 755 llvm-windres "$out/bin"
+    '';
+  };
   prefix =
     if stdenv.hostPlatform != stdenv.targetPlatform
     then "${stdenv.targetPlatform.config}-"
@@ -10,7 +34,7 @@ in runCommand "llvm-binutils-${version}" { preferLocalBuild = true; } ''
    for prog in ${lld}/bin/*; do
      ln -s $prog $out/bin/${prefix}$(basename $prog)
    done
-   for prog in ${llvm}/bin/*; do
+   for prog in ${llvm}/bin/* ${llvm-mingw}/bin/*; do
      ln -sf $prog $out/bin/${prefix}$(basename $prog)
    done
 
