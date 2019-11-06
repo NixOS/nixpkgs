d7c8ad7d69dbf368047357c61c49274cc72dc25b
worldofpeace <worldofpeace@protonmail.ch>
2019-11-05 22:04:56 -0500

    nixosTests.flatpak: fix eval
    optionalPackages are deprecated.

--- nixos/tests/installed-tests/flatpak.nix
+++ nixos/tests/installed-tests/flatpak.nix
@@ -8,7 +8,7 @@ makeInstalledTest {
     services.xserver.desktopManager.gnome3.enable = true; # TODO: figure out minimal environment where the tests work
     # common/x11.nix enables the auto display manager (lightdm)
     services.xserver.displayManager.gdm.enable = false;
-    environment.gnome3.excludePackages = pkgs.gnome3.optionalPackages;
+    services.gnome3.core-utilities.enable = false;
     services.flatpak.enable = true;
     environment.systemPackages = with pkgs; [ gnupg ostree python2 ];
     virtualisation.memorySize = 2047;
