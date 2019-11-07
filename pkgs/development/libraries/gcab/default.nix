4e96a9d9f7357958479d17922ec95729bf58fd5f
Jan Tojnar <jtojnar@gmail.com>
2019-11-07 13:46:37 +0100

    gcab: clean up
    * format with nixpkgs-fmt
    * remove UTF-8 locale hack, since now we have C.UTF-8
    * add description, fix homepage, correct license
    * split to multiple outputs

--- pkgs/development/libraries/gcab/default.nix
+++ pkgs/development/libraries/gcab/default.nix
@@ -1,21 +1,42 @@
-{ stdenv, fetchurl, gettext, gobject-introspection, pkgconfig
-, meson, ninja, glibcLocales, git, vala, glib, zlib, gnome3
+{ stdenv
+, fetchurl
+, gettext
+, gobject-introspection
+, pkgconfig
+, meson
+, ninja
+, git
+, vala
+, glib
+, zlib
+, gnome3
 }:
 
 stdenv.mkDerivation rec {
   pname = "gcab";
   version = "1.2";
 
-  LC_ALL = "en_US.UTF-8";
+  outputs = [ "bin" "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
     sha256 = "038h5kk41si2hc9d9169rrlvp8xgsxq27kri7hv2vr39gvz9cbas";
   };
 
-  nativeBuildInputs = [ meson ninja glibcLocales git pkgconfig vala gettext gobject-introspection ];
+  nativeBuildInputs = [
+    meson
+    ninja
+    git
+    pkgconfig
+    vala
+    gettext
+    gobject-introspection
+  ];
 
-  buildInputs = [ glib zlib ];
+  buildInputs = [
+    glib
+    zlib
+  ];
 
   mesonFlags = [
     "-Ddocs=false"
@@ -30,9 +51,10 @@ stdenv.mkDerivation rec {
   };
 
   meta = with stdenv.lib; {
+    description = "GObject library to create cabinet files";
+    homepage = "https://gitlab.gnome.org/GNOME/gcab";
+    license = licenses.lgpl21Plus;
+    maintainers = gnome3.maintainers;
     platforms = platforms.linux;
-    license = licenses.lgpl21;
-    homepage = "https://wiki.gnome.org/msitools";
-    maintainers = [ maintainers.lethalman ];
   };
 }
