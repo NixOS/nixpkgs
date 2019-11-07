0f2e99bf3b44a2a297f5f395dd175038022f9345
worldofpeace <worldofpeace@protonmail.ch>
2019-11-06 19:27:10 -0500

    xfce.xfburn: 0.5.5 -> 0.6.1

--- pkgs/desktops/xfce/applications/xfburn/default.nix
+++ pkgs/desktops/xfce/applications/xfburn/default.nix
@@ -1,13 +1,12 @@
-{ mkXfceDerivation, docbook_xsl, exo, gtk2, libburn, libisofs, libxfce4ui, libxslt }:
+{ mkXfceDerivation, docbook_xsl, exo, gtk3, libburn, libisofs, libxfce4ui, libxslt }:
 
 mkXfceDerivation {
   category = "apps";
   pname = "xfburn";
-  version = "0.5.5";
+  version = "0.6.1";
 
-  sha256 = "1lmv48vqrlap1a2ha72g16vqly18zvcwj8y3f3f00l10pmn52bkp";
+  sha256 = "0a1ly79x7j5pgr3vbsabb4i0jd5rryaigj9z8iqzr8p9miypx20v";
 
   nativeBuildInputs = [ libxslt docbook_xsl ];
-  buildInputs = [ exo gtk2 libburn libisofs libxfce4ui ];
-  meta.broken = true;
+  buildInputs = [ exo gtk3 libburn libisofs libxfce4ui ];
 }
