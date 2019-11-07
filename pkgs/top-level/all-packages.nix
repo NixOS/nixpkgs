ccc13c2e53b58018f9f39fb8a060286f1aa2508c
Eric Dallo <ericdallo06@hotmail.com>
2019-11-07 19:21:47 -0300

    srt-vtt: init at 2019-01-03 (#70885)
    * srt-vtt: init at 2019-01-03
    
    * Append 'unstable' to pname
    
    Co-Authored-By: Marek Mahut <marek.mahut@gmail.com>
    
    * Fix version
    
    * Remove which validation
    
    * Add support to linux only
    
    Co-Authored-By: Evils-Devils <30512529+Evils-Devils@users.noreply.github.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6384,6 +6384,8 @@ in
 
   srcml = callPackage ../applications/version-management/srcml { };
 
+  srt-to-vtt-cl = callPackage ../tools/cd-dvd/srt-to-vtt-cl { };
+
   sourcehut = callPackage ../applications/version-management/sourcehut { };
 
   sshfs-fuse = callPackage ../tools/filesystems/sshfs-fuse { };
