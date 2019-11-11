af71b9f205b11d443a1d1754e5db843e5bac0c48
Herman Fries <baracoder@googlemail.com>
2019-11-12 00:45:08 +0100

    dotnetCorePackages.sdk_2_1: init at 2.1.509

--- pkgs/development/compilers/dotnet/default.nix
+++ pkgs/development/compilers/dotnet/default.nix
@@ -11,6 +11,10 @@ let
 in rec {
   combinePackages = attrs: callPackage (import ./combinePackages.nix attrs) {};
 
+  sdk_2_1 = buildNetCoreSdk {
+    version = "2.1.509";
+    sha512 = "4B7DC25841F56AADDD685ADB227B362177268E3D052B4ADDDAC2530889B6506B7B4768B751AE75F131143424D02EEE77B4565DAD6B49048C39D2C47E39412FDF";
+  };
   sdk_2_2 = buildNetCoreSdk {
     version = "2.2.401";
     sha512 = "05w3zk7bcd8sv3k4kplf20j906and2006g1fggq7y6kaxrlhdnpd6jhy6idm8v5bz48wfxga5b4yys9qx0fp3p8yl7wi67qljpzrq88";
