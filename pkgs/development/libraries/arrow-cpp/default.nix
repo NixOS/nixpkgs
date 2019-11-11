NixOS/nixpkgs

commit 8c86e54a25b8d1fbc3097d905702d454768d2550

arrow-cpp: fix/disable some plasma tests on darwin

pkgs/development/libraries/arrow-cpp/default.nix
@@ -80,12 +80,21 @@ in stdenv.mkDerivation rec {
     if doInstallCheck then "${parquet-testing}/data" else null;
   installCheckInputs = [ perl which ];
   installCheckPhase = (stdenv.lib.optionalString stdenv.isDarwin ''
-    for f in release/*test; do
+    for f in release/*test{,s}; do
       install_name_tool -add_rpath "$out"/lib  "$f"
     done
-  '') + ''
-    ctest -L unittest -V
-  '';
+  '')
+  + (let
+    excludedTests = stdenv.lib.optionals stdenv.isDarwin [
+      # Some plasma tests need to be patched to use a shorter AF_UNIX socket
+      # path on Darwin. See https://github.com/NixOS/nix/pull/1085
+      "plasma-external-store-tests"
+      "plasma-client-tests"
+    ];
+  in ''
+    ctest -L unittest -V \
+      --exclude-regex '(${builtins.concatStringsSep "|" excludedTests})'
+  '');
 
   meta = {
     description = "A  cross-language development platform for in-memory data";

commit c57018fb3b51f5ba4e955c6641059ab1b72c49eb

arrow-cpp: fix tests not being run on linux
Fixes: 8c86e54a ('arrow-cpp: fix/disable some plasma tests on darwin')

pkgs/development/libraries/arrow-cpp/default.nix
@@ -93,7 +93,7 @@ in stdenv.mkDerivation rec {
     ];
   in ''
     ctest -L unittest -V \
-      --exclude-regex '(${builtins.concatStringsSep "|" excludedTests})'
+      --exclude-regex '^(${builtins.concatStringsSep "|" excludedTests})$'
   '');
 
   meta = {