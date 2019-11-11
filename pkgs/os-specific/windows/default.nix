0a63190c3115126e6a571b6ca555fa684b8bf4ed
John Ericson <git@JohnEricson.me>
2019-11-10 22:32:31 -0500

    windows top-level: Clean up with makeScope

--- pkgs/os-specific/windows/default.nix
+++ pkgs/os-specific/windows/default.nix
@@ -1,29 +1,27 @@
-{ newScope, crossLibcStdenv }: let
+{ stdenv, newScope, crossLibcStdenv }:
 
-  callPackage = newScope self;
+stdenv.lib.makeScope newScope (self: with self; {
 
-  self = {
-    cygwinSetup = callPackage ./cygwin-setup { };
+  cygwinSetup = callPackage ./cygwin-setup { };
 
-    jom = callPackage ./jom { };
+  jom = callPackage ./jom { };
 
-    w32api = callPackage ./w32api { };
+  w32api = callPackage ./w32api { };
 
-    mingwrt = callPackage ./mingwrt { };
-    mingw_runtime = self.mingwrt;
+  mingwrt = callPackage ./mingwrt { };
+  mingw_runtime = mingwrt;
 
-    mingw_w64 = callPackage ./mingw-w64 {
-      stdenv = crossLibcStdenv;
-    };
-
-    mingw_w64_headers = callPackage ./mingw-w64/headers.nix { };
-
-    mingw_w64_pthreads = callPackage ./mingw-w64/pthreads.nix { };
-
-    pthreads = callPackage ./pthread-w32 { };
-
-    wxMSW = callPackage ./wxMSW-2.8 { };
-
-    libgnurx = callPackage ./libgnurx { };
+  mingw_w64 = callPackage ./mingw-w64 {
+    stdenv = crossLibcStdenv;
   };
-in self
+
+  mingw_w64_headers = callPackage ./mingw-w64/headers.nix { };
+
+  mingw_w64_pthreads = callPackage ./mingw-w64/pthreads.nix { };
+
+  pthreads = callPackage ./pthread-w32 { };
+
+  wxMSW = callPackage ./wxMSW-2.8 { };
+
+  libgnurx = callPackage ./libgnurx { };
+})
