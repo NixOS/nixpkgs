7ea26fa32f6e92581eadabd6c9bcca8f137a4c79
Herman Fries <baracoder@googlemail.com>
2019-11-12 00:27:14 +0100

    dotnetCorePackages.sdk_3_0: init at 3.0.100

--- pkgs/development/compilers/dotnet/default.nix
+++ pkgs/development/compilers/dotnet/default.nix
@@ -19,4 +19,8 @@ in rec {
     version = "2.2.401";
     sha512 = "05w3zk7bcd8sv3k4kplf20j906and2006g1fggq7y6kaxrlhdnpd6jhy6idm8v5bz48wfxga5b4yys9qx0fp3p8yl7wi67qljpzrq88";
   };
+  sdk_3_0 = buildNetCoreSdk {
+    version = "3.0.100";
+    sha512 = "766da31f9a0bcfbf0f12c91ea68354eb509ac2111879d55b656f19299c6ea1c005d31460dac7c2a4ef82b3edfea30232c82ba301fb52c0ff268d3e3a1b73d8f7";
+  };
 }
