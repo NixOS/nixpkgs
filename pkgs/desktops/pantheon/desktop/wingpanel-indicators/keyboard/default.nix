251a5604492e364e90e606acba3bdf696ca5da0d
worldofpeace <worldofpeace@protonmail.ch>
2019-11-10 20:48:12 -0500

    pantheon.wingpanel-indicator-keyboard: 2.1.2 -> 2.2.0
    https://github.com/elementary/wingpanel-indicator-keyboard/releases/tag/2.2.0

--- pkgs/desktops/pantheon/desktop/wingpanel-indicators/keyboard/default.nix
+++ pkgs/desktops/pantheon/desktop/wingpanel-indicators/keyboard/default.nix
@@ -17,13 +17,13 @@
 
 stdenv.mkDerivation rec {
   pname = "wingpanel-indicator-keyboard";
-  version = "2.1.2";
+  version = "2.2.0";
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = pname;
     rev = version;
-    sha256 = "0lrd474m6p8di73hqjilqnnl7qg72ky5narkgcvm4lk8dyi78mz0";
+    sha256 = "0jc12xfaj3micpjssxc7m6hzssvyq26ln5az05x5f1j6v8lccbyn";
   };
 
   passthru = {
