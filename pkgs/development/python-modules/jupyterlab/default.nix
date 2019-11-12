4eed2a56e6a512b184bb614ebfd26658ed11eef4
Frederik Rietdijk <fridh@fridh.nl>
2019-11-12 17:19:57 +0100

    python.pkgs.jupyterlab: 0.35.6 -> 1.2.2

--- pkgs/development/python-modules/jupyterlab/default.nix
+++ pkgs/development/python-modules/jupyterlab/default.nix
@@ -4,31 +4,22 @@
 , jupyterlab_server
 , notebook
 , pythonOlder
-, fetchpatch
 }:
 
 buildPythonPackage rec {
   pname = "jupyterlab";
-  version = "0.35.6";
+  version = "1.2.2";
   disabled = pythonOlder "3.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "2ec845845d51221e39d0d753884a19342c953f39febf3148a68631bf57ecb774";
+    sha256 = "96d3146eb09ffa0a198f31b36706859dadd4a59ed184aa9329a9c12733e6af0b";
   };
 
   propagatedBuildInputs = [ jupyterlab_server notebook ];
 
   makeWrapperArgs = [
     "--set" "JUPYTERLAB_DIR" "$out/share/jupyter/lab"
-  ];
-
-  patches = [
-    (fetchpatch {
-      name = "bump-jupyterlab_server-version";
-      url = https://github.com/jupyterlab/jupyterlab/commit/3b8d451e6f9a4c609e60cde5fbb3cc84aae79951.patch;
-      sha256 = "08vv6rp1k5fbmvj4v9x1d9zb6ymm9pv8ml80j7p45r9fay34rndf";
-    })
   ];
 
   # Depends on npm
