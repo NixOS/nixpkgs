93ee8095b0b50f132160df343a1269c90a0965c1
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2019-11-12 18:46:56 -0500

    snakemake: 5.4.4 -> 5.7.4 (#72929)
     (#72929)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10339,7 +10339,7 @@ in
 
   smc = callPackage ../tools/misc/smc { };
 
-  snakemake = callPackage ../applications/science/misc/snakemake { python = python3Packages; };
+  snakemake = callPackage ../applications/science/misc/snakemake { };
 
   snore = callPackage ../tools/misc/snore { };
 
