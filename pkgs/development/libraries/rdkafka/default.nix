0b76aaf923a87cee87cf2986f757b3745653b075
Renaud <c0bw3b@users.noreply.github.com>
2019-11-13 20:01:45 +0100

    rdkafka: 1.2.1 -> 1.2.2

--- pkgs/development/libraries/rdkafka/default.nix
+++ pkgs/development/libraries/rdkafka/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "rdkafka";
-  version = "1.2.1";
+  version = "1.2.2";
 
   src = fetchFromGitHub {
     owner = "edenhill";
     repo = "librdkafka";
     rev = "v${version}";
-    sha256 = "03dxw94s1lzd91li3xm4jrr9wpqihxjl115mkgi25gwqminrbavs";
+    sha256 = "1daikjr2wcjxcys41hfw3vg2mqk6cy297pfcl05s90wnjvd7fkqk";
   };
 
   nativeBuildInputs = [ pkgconfig ];
