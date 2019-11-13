77b8701c5feef94b5296f3028627d042e59a1894
R. RyanTM <ryantm-bot@ryantm.com>
2019-11-13 14:53:35 -0800

    openapi-generator-cli: 4.1.3 -> 4.2.0
     (#72589)

--- pkgs/tools/networking/openapi-generator-cli/default.nix
+++ pkgs/tools/networking/openapi-generator-cli/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, jre, makeWrapper }:
 
 stdenv.mkDerivation rec {
-  version = "4.1.3";
+  version = "4.2.0";
   pname = "openapi-generator-cli";
 
   jarfilename = "${pname}-${version}.jar";
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "http://central.maven.org/maven2/org/openapitools/${pname}/${version}/${jarfilename}";
-    sha256 = "0bqdjparv12wscq4qhm9xnryyfmdnn3w77hrhnsy8mlvxk2vnk13";
+    sha256 = "00c5y06b8lm6lhnnvl42yndwdgn42bwwfc7p1q7zcsga275b6jpb";
   };
 
   phases = [ "installPhase" ];
