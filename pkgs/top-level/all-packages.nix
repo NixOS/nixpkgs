86c9f703cea79bd7ad7471e0686da67b64a03404
Daniel Schaefer <git@danielschaefer.me>
2019-11-14 22:43:13 +0100

    yq: 2.8.1 -> 2.10.1
    Co-authored-by: Drew Risinger <drewrisinger@users.noreply.github.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11071,7 +11071,9 @@ in
   yodl = callPackage ../development/tools/misc/yodl { };
 
   yq = callPackage ../development/tools/yq {
-    inherit (python3Packages) buildPythonApplication fetchPypi pyyaml xmltodict;
+    inherit (python3Packages)
+    buildPythonApplication fetchPypi pyyaml xmltodict argcomplete pytest
+    coverage flake8 toml;
   };
 
   yq-go = callPackage ../development/tools/yq-go { };
