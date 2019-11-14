a3513b14c93ff00f3a691bb920d0e308bb086726
Jan Tojnar <jtojnar@gmail.com>
2019-11-14 03:42:28 +0100

    kdeApplications.print-manager: disable format-security
    Attempt to unbreak unstable channel:
    
    https://hydra.nixos.org/build/106714604/nixlog/1

--- pkgs/applications/kde/print-manager.nix
+++ pkgs/applications/kde/print-manager.nix
@@ -19,5 +19,9 @@ mkDerivation {
     kconfig kconfigwidgets kdbusaddons kiconthemes kcmutils knotifications
     kwidgetsaddons kitemviews kio kwindowsystem plasma-framework qtdeclarative
   ];
+
+  # /build/print-manager-19.08.2/libkcups/KIppRequest.cpp:192:65: error: format not a string literal and no format arguments [-Werror=format-security]
+  hardeningDisable = [ "format" ];
+
   outputs = [ "out" "dev" ];
 }
