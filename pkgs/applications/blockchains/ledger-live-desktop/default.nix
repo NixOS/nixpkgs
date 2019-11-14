25dbbd2f816d02e478e23302cffe864de0fa945b
nyanloutre <paul@nyanlout.re>
2019-11-15 00:14:31 +0100

    ledger-live-desktop: 1.15.0 -> 1.18.2

--- pkgs/applications/blockchains/ledger-live-desktop/default.nix
+++ pkgs/applications/blockchains/ledger-live-desktop/default.nix
@@ -2,12 +2,12 @@
 
 let
   pname = "ledger-live-desktop";
-  version = "1.15.0";
+  version = "1.18.2";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://github.com/LedgerHQ/${pname}/releases/download/v${version}/${pname}-${version}-linux-x86_64.AppImage";
-    sha256 = "0r7gm7q7gj39v36jd5xz20931za94nf2fpf3clbghkhlbrm0kbnq";
+    sha256 = "1giy8xg1yfv7b7gh98dmfc05wh54xqpd53nanacwcc7lakzizqnn";
   };
 
   appimageContents = appimageTools.extractType2 {
