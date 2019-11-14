07b838f13b5c73373718e935cd168f6649bdd38b
Alexander Bantyev <balsoft75@gmail.com>
2019-11-14 23:28:51 +0300

    ocaml: num: Allow to disable dynamic linking

--- pkgs/development/ocaml-modules/num/default.nix
+++ pkgs/development/ocaml-modules/num/default.nix
@@ -14,12 +14,14 @@ stdenv.mkDerivation rec {
 			url = "https://github.com/ocaml/num/commit/6d4c6d476c061298e6385e8a0864f083194b9307.patch";
 			sha256 = "18zlvb5n327q8y3c52js5dvyy29ssld1l53jqng8m9w1k24ypi0b";
 		})
+    ./enable-static.patch
 	];
 
 	nativeBuildInputs = [ ocaml findlib ];
-  buildInputs = [ findlib ];
+  buildInputs = [ ocaml findlib ];
 
 	createFindlibDestdir = true;
+
 
 	meta = {
 		description = "Legacy Num library for arbitrary-precision integer and rational arithmetic";
