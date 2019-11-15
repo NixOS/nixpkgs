58a4eccf00b68596f9aae3f8e47aaa45641d4dde
tilpner <till@hoeppner.ws>
2019-11-15 15:19:31 +0100

    python3Packages.python-olm: init at (automatic) 3.1.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1149,6 +1149,8 @@ in {
     igraph = pkgs.igraph;
   };
 
+  python-olm = callPackage ../development/python-modules/python-olm { };
+
   python3-openid = callPackage ../development/python-modules/python3-openid { };
 
   python-packer = callPackage ../development/python-modules/python-packer { };
