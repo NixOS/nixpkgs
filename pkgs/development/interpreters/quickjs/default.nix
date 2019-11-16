91182f6e2ea9c49c2a40d6e25b3059202e6a387a
Ivan Kozik <ivan@ludios.org>
2019-11-16 17:10:22 +0000

    quickjs: 2019-07-09 -> 2019-10-27

--- pkgs/development/interpreters/quickjs/default.nix
+++ pkgs/development/interpreters/quickjs/default.nix
@@ -2,24 +2,20 @@
 
 stdenv.mkDerivation rec {
   pname = "quickjs";
-  version = "2019-07-09";
+  version = "2019-10-27";
 
   src = fetchurl {
     url = "https://bellard.org/${pname}/${pname}-${version}.tar.xz";
-    sha256 = "0r6mgh0m85c11y8ah5iphzpw518br0hi36f92mgdg2iivpciq31m";
+    sha256 = "0xm16ja3c0k80jy0xkx0f40r44v2lgx2si4dnaw2w7c5nx7cmkai";
   };
-
-  postPatch = ''
-    sed -e "s/^CONFIG_M32=y/#&/" -i Makefile
-  '';
 
   makeFlags = [ "prefix=${placeholder ''out''}" ];
   enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     description = "A small and embeddable Javascript engine";
-    homepage = https://bellard.org/quickjs/;
-    maintainers = [ maintainers.stesie ];
+    homepage = "https://bellard.org/quickjs/";
+    maintainers = with maintainers; [ stesie ivan ];
     platforms = platforms.linux;
     license = licenses.mit;
   };
