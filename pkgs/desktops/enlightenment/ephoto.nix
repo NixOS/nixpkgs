9367367dfd6e0373c04650e9893deed426d48aa2
c0bw3b <c0bw3b@users.noreply.github.com>
2019-11-16 01:41:23 +0100

    Treewide: fix URL permanent redirects
    Permanent redirects on homepages and/or source URLs
    as reported by Repology

--- pkgs/desktops/enlightenment/ephoto.nix
+++ pkgs/desktops/enlightenment/ephoto.nix
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "Image viewer and editor written using the Enlightenment Foundation Libraries";
-    homepage = http://smhouston.us/ephoto/;
+    homepage = https://smhouston.us/projects/ephoto/;
     license = stdenv.lib.licenses.bsd2;
     platforms = stdenv.lib.platforms.linux;
     maintainers = [ stdenv.lib.maintainers.romildo ];
