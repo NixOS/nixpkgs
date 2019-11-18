1ac7a46b430eb8d45ee3d411d6fbf0db11e3711f
ahiaao <ahiaao@waifu.club>
2019-11-17 19:28:40 -0800

    python37Packages.blist: fix compatibility for python 3.7

--- pkgs/development/python-modules/blist/default.nix
+++ pkgs/development/python-modules/blist/default.nix
@@ -1,5 +1,6 @@
 { stdenv
 , buildPythonPackage
+, fetchpatch
 , fetchPypi
 , isPyPy
 }:
@@ -13,6 +14,14 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "1hqz9pqbwx0czvq9bjdqjqh5bwfksva1is0anfazig81n18c84is";
   };
+
+  patches = [
+    # Fix compatibility for Python 3.7 https://github.com/DanielStutzbach/blist/pull/78
+    (fetchpatch {
+      url = "https://github.com/DanielStutzbach/blist/commit/2dc1ec28ed68611fcec9ac1c68070c782d6b4b4e.patch";
+      sha256 = "0ma0z6ga80w3wzh3sidwd8ckfbgx4j1y7cc29q6j9ddrvxpf276y";
+    })
+  ];
 
   meta = with stdenv.lib; {
     homepage = http://stutzbachenterprises.com/blist/;
