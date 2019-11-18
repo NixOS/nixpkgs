adacd7ad78c3c0044508cea5eab8fda7f48105ee
Mario Rodas <marsam@users.noreply.github.com>
2019-11-18 16:20:00 -0500

    ibm-plex: 3.0.0 -> 4.0.1

--- pkgs/data/fonts/ibm-plex/default.nix
+++ pkgs/data/fonts/ibm-plex/default.nix
@@ -1,7 +1,7 @@
 { lib, fetchzip }:
 
 let
-  version = "3.0.0";
+  version = "4.0.1";
 in fetchzip {
   name = "ibm-plex-${version}";
   url = "https://github.com/IBM/plex/releases/download/v${version}/OpenType.zip";
@@ -9,7 +9,7 @@ in fetchzip {
     mkdir -p $out/share/fonts
     unzip -j $downloadedFile "OpenType/*/*.otf" -d $out/share/fonts/opentype
   '';
-  sha256 = "1vv0lf2fn0y0ln14s4my8x2mykq1lwqpmkjkhs6cm48mzf740nhs";
+  sha256 = "11d5bsv7d5rbp9f1gf1l4za3xj6jlqwnvsl5ipwx4angh1kb7nk8";
 
   meta = with lib; {
     description = "IBM Plex Typeface";
