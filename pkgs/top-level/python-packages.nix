4f971bbccee6b346ccff15637f7a95f5979e2ebb
Frederik Rietdijk <fridh@fridh.nl>
2019-11-19 13:09:58 +0100

    python.pkgs.cerberus11: remove reference after file was moved
    Fixup for 34715da5dc3fcbfe031bc7b5a8480b55ee60c588, fixes eval.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1854,8 +1854,6 @@ in {
 
   cerberus = callPackage ../development/python-modules/cerberus { };
 
-  cerberus11 = callPackage ../development/python-modules/cerberus11 { };
-
   certifi = callPackage ../development/python-modules/certifi { };
 
   characteristic = callPackage ../development/python-modules/characteristic { };
