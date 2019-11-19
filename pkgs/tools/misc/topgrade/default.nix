e4ee075a661f31a1079cea070e92bc299da22595
Oleksii Filonenko <brightone@protonmail.com>
2019-11-19 12:24:47 +0200

    topgrade: 3.3.0 -> 3.4.0

--- pkgs/tools/misc/topgrade/default.nix
+++ pkgs/tools/misc/topgrade/default.nix
@@ -2,22 +2,22 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "topgrade";
-  version = "3.3.0";
+  version = "3.4.0";
 
   src = fetchFromGitHub {
     owner = "r-darwish";
     repo = pname;
     rev = "v${version}";
-    sha256 = "08vhn0w4px0j7ln1zq5zh3nplcv0xdh6z3rb8sczgjkvgi0xi4qn";
+    sha256 = "14p7lpdp85ay5p2r9npm2adp9njcssi47mb1fh2iyn8lp51d22bi";
   };
 
-  cargoSha256 = "0nwhv7j2wbzh6h4jrxsgcyvqi4sj021k74myj5y44asignacvx3i";
+  cargoSha256 = "07h8d8fm20dp9xcz9vic63xnx2rbvanf2ivks1jiv32iy0kgz74p";
 
   meta = with stdenv.lib; {
     description = "Upgrade all the things";
     homepage = "https://github.com/r-darwish/topgrade";
     license = licenses.gpl3;
     platforms = platforms.all;
-    maintainers = [ maintainers.filalex77 ];
+    maintainers = with maintainers; [ filalex77 ];
   };
 }
