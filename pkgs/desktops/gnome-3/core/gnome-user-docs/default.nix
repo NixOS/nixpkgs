f2bc9da81fa622655f7f8d80dbcc598986d14f90
Jan Tojnar <jtojnar@gmail.com>
2019-11-19 14:10:53 +0100

    gnome3.gnome-user-docs: 3.34.0 → 3.34.1
    https://ftp.gnome.org/pub/GNOME/sources/gnome-user-docs/3.34/gnome-user-docs-3.34.1.news

--- pkgs/desktops/gnome-3/core/gnome-user-docs/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-user-docs/default.nix
@@ -1,26 +1,42 @@
-{ stdenv, fetchurl, pkgconfig, gnome3, itstool, libxml2, intltool }:
+{ stdenv
+, fetchurl
+, gettext
+, gnome3
+, itstool
+, libxml2
+, yelp-tools
+}:
 
 stdenv.mkDerivation rec {
   pname = "gnome-user-docs";
-  version = "3.34.0";
+  version = "3.34.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-user-docs/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "1v94mz8zwzx67db2avbi9p2klw36cz45qr6fbskpvygz2pzhg6cg";
+    sha256 = "11m9fv8k2hynrcgah4jvbm6yczg0s1ly302mipysbwpn6gbdkvf2";
   };
+
+  nativeBuildInputs = [
+    gettext
+    itstool
+    libxml2
+    yelp-tools
+  ];
+
+  enableParallelBuilding = true;
 
   passthru = {
-    updateScript = gnome3.updateScript { packageName = "gnome-user-docs"; attrPath = "gnome3.gnome-user-docs"; };
+    updateScript = gnome3.updateScript {
+      packageName = pname;
+      attrPath = "gnome3.${pname}";
+    };
   };
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ gnome3.yelp itstool libxml2 intltool ];
-
   meta = with stdenv.lib; {
-    homepage = https://help.gnome.org/users/gnome-help/;
     description = "User and system administration help for the GNOME desktop";
-    maintainers = gnome3.maintainers;
+    homepage = "https://help.gnome.org/users/gnome-help/";
     license = licenses.cc-by-30;
+    maintainers = gnome3.maintainers;
     platforms = platforms.linux;
   };
 }
