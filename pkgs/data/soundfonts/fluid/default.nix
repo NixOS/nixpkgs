adab6c90bd697c00a9e7aff7c15fd7d118ad7f83
MicrosoftTakeover <49256525+MicrosoftTakeover@users.noreply.github.com>
2019-11-18 16:07:13 -0800

    Update fluid-soundfont
    * Change name & version variables.
    * Switch to Debian's mirror which includes both GM/GS soundfont versions.
    * FluidR3_GM2-2.sf2 renamed to the more standard FluidR3_GM.sf2
    * Update description, add longDescription.

--- pkgs/data/soundfonts/fluid/default.nix
+++ pkgs/data/soundfonts/fluid/default.nix
@@ -1,21 +1,30 @@
 { stdenv, fetchurl }:
 
-stdenv.mkDerivation {
-  name = "Fluid-3";
+stdenv.mkDerivation rec {
+  name = "${pname}-${version}";
+  pname = "fluid-soundfont";
+  version = "3.1";
 
   src = fetchurl {
-    url = "http://www.musescore.org/download/fluid-soundfont.tar.gz";
-    sha256 = "1f96bi0y6rms255yr8dfk436azvwk66c99j6p43iavyq8jg7c5f8";
+    url = "http://http.debian.net/debian/pool/main/f/${pname}/${pname}_${version}.orig.tar.gz";
+    sha256 = "0hq35wa8418qwfk6lvckfq9fcxy51hin7ldx4kdspr3q3jmaq896";
   };
 
   sourceRoot = ".";
 
   installPhase = ''
-    install -Dm644 "FluidR3 GM2-2.SF2" $out/share/soundfonts/FluidR3_GM2-2.sf2
+    install -Dm644 ${name}/FluidR3_GM.sf2 $out/share/soundfonts/FluidR3_GM.sf2
+    install -Dm644 ${name}/FluidR3_GS.sf2 $out/share/soundfonts/FluidR3_GS.sf2
   '';
 
   meta = with stdenv.lib; {
-    description = "Frank Wen's pro-quality GM/GS soundfont";
+    description = "Pro-quality GM/GS soundfont";
+    longDescription = ''
+      FluidR3 is the third release of Frank Wen's pro-quality GM/GS soundfont.
+      The soundfont has lots of excellent samples, including all the GM instruments
+      along side with the GS instruments that are recycled and reprogrammed versions
+      of the GM presets.
+    '';
     homepage = http://www.hammersound.net/;
     license = licenses.mit;
     platforms = platforms.all;
