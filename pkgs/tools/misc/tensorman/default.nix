ee61ee1d48f9351bab104338255eea3906c25ed6
Stefan Rohrbacher <stefan.rohrbacher97@gmail.com>
2019-11-20 20:36:26 +0100

    Update default.nix

--- pkgs/tools/misc/tensorman/default.nix
+++ pkgs/tools/misc/tensorman/default.nix
@@ -4,15 +4,15 @@ rustPlatform.buildRustPackage rec {
   pname = "tensorman-${version}";
   version = "0.1.0";
 
-src = fetchFromGitHub {
-  owner = "pop-os";
-  repo = "tensorman";
-  rev = version;
-  sha256 = "0ywb53snvymmwh10hm6whckz7dwmpqa4rxiggd24y178jdfrm2ns";
-};
+ src = fetchFromGitHub {
+   owner = "pop-os";
+   repo = "tensorman";
+   rev = version;
+   sha256 = "0ywb53snvymmwh10hm6whckz7dwmpqa4rxiggd24y178jdfrm2ns";
+ };
 
-buildInputs = [ pkgconfig openssl ];
-cargoSha256 = "1gh5w6zzrvjk60bqaf355fagijy723rvmqjh4laksd96pmzdfwn9";
+ buildInputs = [ pkgconfig openssl ];
+ cargoSha256 = "1gh5w6zzrvjk60bqaf355fagijy723rvmqjh4laksd96pmzdfwn9";
 
   meta = with stdenv.lib; {
     description = "Utility for easy management of Tensorflow containers";
