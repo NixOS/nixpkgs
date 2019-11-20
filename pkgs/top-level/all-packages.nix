93c5503c2e3fbfba815806afea20828fd6540d54
Alexander Bakker <ab@alexbakker.me>
2019-11-20 14:11:35 +0100

    zenpower: init at 0.1.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16417,6 +16417,8 @@ in
 
     x86_energy_perf_policy = callPackage ../os-specific/linux/x86_energy_perf_policy { };
 
+    zenpower = callPackage ../os-specific/linux/zenpower { };
+
     inherit (callPackage ../os-specific/linux/zfs {
       configFile = "kernel";
       inherit kernel;
