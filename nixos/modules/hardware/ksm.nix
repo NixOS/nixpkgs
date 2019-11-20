3f45518875c4fce419acbd7f7802bc9aa4b04cc5
Kyle Copperfield <kmcopper@danwin1210.me>
2019-11-20 11:46:14 +0000

    nixos/ksm: add configurable zero pages
    adds an option to nix configuration that allows ksm zero pages to be
    toggleable.

--- nixos/modules/hardware/ksm.nix
+++ nixos/modules/hardware/ksm.nix
@@ -20,6 +20,17 @@ in {
         Setting it to <literal>null</literal> uses the kernel's default time.
       '';
     };
+    zero_pages = mkOption {
+      default = null;
+      type = types.nullOr (types.ints.between 0 1);
+      description = ''
+        ksm will merge kernel zero pages with ksm zero pages if this is set to <literal>1</literal>.
+	Only ksm zero pages are merged by default. Kernel zero pages are merged if this is enabled.
+	The default is <literal>0</literal>. This option is defined <literal>null</literal>.
+	Care should be taken enabling this setting as it can degrade performance in many scenarios.
+	Performance can be increased on architectures with colored zero pages.
+      '';
+    };
   };
 
   config = mkIf cfg.enable {
@@ -31,6 +42,8 @@ in {
       script = ''
         echo 1 > /sys/kernel/mm/ksm/run
         ${optionalString (cfg.sleep != null) ''echo ${toString cfg.sleep} > /sys/kernel/mm/ksm/sleep_millisecs''}
+        ${optionalString (cfg.zero_pages != null)
+	''echo ${toString cfg.zero_pages} > /sys/kernel/mm/ksm/use_zero_pages''}
       '';
     };
   };
