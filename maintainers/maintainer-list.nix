3b63361e8a983eaa33a0ef978521c6094d6ecb1c
Stefan Rohrbacher <stefan.rohrbacher97@gmail.com>
2019-11-20 13:38:53 +0100

    Update maintainer-list.nix

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6658,6 +6658,12 @@
     githubId = 42933;
     name = "Andrew Childs";
   };
+  thefenriswolf = {
+    email = "stefan.rohrbacher97@gmail.com";
+    github = "thefenriswolf";
+    githubId = "8547242";
+    name = "Stefan Rohrbacher";
+  };
   thesola10 = {
     email = "thesola10@bobile.fr";
     github = "thesola10";
