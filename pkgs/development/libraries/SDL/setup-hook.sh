44083d99992e08019c112ac9743a901d12f9c8c3
Jan Tojnar <jtojnar@gmail.com>
2019-11-20 03:43:44 +0100

    SDL: fix setup hook
    SDL_LIB_PATH needs to contain -L

--- pkgs/development/libraries/SDL/setup-hook.sh
+++ pkgs/development/libraries/SDL/setup-hook.sh
@@ -3,7 +3,7 @@ addSDLPath () {
     export SDL_PATH="${SDL_PATH-}${SDL_PATH:+ }$1/include/SDL"
   fi
   if [ -e "$1/lib" ]; then
-    export SDL_LIB_PATH="${SDL_LIB_PATH-}${SDL_LIB_PATH:+ }$1/lib"
+    export SDL_LIB_PATH="${SDL_LIB_PATH-}${SDL_LIB_PATH:+ }-L$1/lib"
   fi
 }
 
