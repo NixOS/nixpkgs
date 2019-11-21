NixOS/nixpkgs

commit 770a4c7946a1c55bf6295a34a42e1c4b657d1088

glib: Fix setup hook
It was not robust against multiple matching directories, as the called
function takes exactly 3 arguments.

pkgs/development/libraries/glib/setup-hook.sh
@@ -1,9 +1,11 @@
 
 make_glib_find_gsettings_schemas() {
     # For packages that need gschemas of other packages (e.g. empathy)
-    if [ -d "$1"/share/gsettings-schemas/*/glib-2.0/schemas ]; then
-        addToSearchPath GSETTINGS_SCHEMAS_PATH "$1/share/gsettings-schemas/"*
-    fi
+    for maybe_dir in "$1"/share/gsettings-schemas/*/glib-2.0; do
+        if [[ -d "$maybe_dir/schemas" ]]; then
+            addToSearchPath GSETTINGS_SCHEMAS_PATH "$maybe_dir"
+        fi
+    done
 }
 addEnvHooks "$hostOffset" make_glib_find_gsettings_schemas
 

commit 1c9ba9f6ababe1c1555fb44fb64c56e5e36df80a

glib.setupHook: fix make_glib_find_gsettings_schemas
GSETTINGS_SCHEMAS_PATH needs to contain a list of directories containing glib-2.0/schemas.
770a4c7946a1c55bf6295a34a42e1c4b657d1088 broke that by accidentally adding glib-2.0 to the paths.

pkgs/development/libraries/glib/setup-hook.sh
@@ -1,8 +1,8 @@
 
 make_glib_find_gsettings_schemas() {
     # For packages that need gschemas of other packages (e.g. empathy)
-    for maybe_dir in "$1"/share/gsettings-schemas/*/glib-2.0; do
-        if [[ -d "$maybe_dir/schemas" ]]; then
+    for maybe_dir in "$1"/share/gsettings-schemas/*; do
+        if [[ -d "$maybe_dir/glib-2.0/schemas" ]]; then
             addToSearchPath GSETTINGS_SCHEMAS_PATH "$maybe_dir"
         fi
     done