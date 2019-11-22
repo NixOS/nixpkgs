47aefc88c87f33dda4868723ccffbcbbebcb8ce2
Louis Bettens <louis@bettens.info>
2019-11-22 09:49:23 +0100

    ammonite: fix #68151
    Upstream doesn't include a shebang line but recommends adding one at
    installation time. NixOS users noticed that the command wouldn't exec
    when using certain shells.

--- pkgs/development/tools/ammonite/default.nix
+++ pkgs/development/tools/ammonite/default.nix
@@ -22,6 +22,7 @@ stdenv.mkDerivation rec {
     sed -i '0,/java/{s|java|${jre}/bin/java|}' $out/bin/amm
   '' + optionalString (disableRemoteLogging) ''
     sed -i '0,/ammonite.Main/{s|ammonite.Main|ammonite.Main --no-remote-logging|}' $out/bin/amm
+    sed -i '1i #!/bin/sh' $out/bin/amm
   '';
 
   meta = {
