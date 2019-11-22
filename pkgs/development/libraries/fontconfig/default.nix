87786bc47f70646fefcbb1dbafd7d7326b97fc7b
worldofpeace <worldofpeace@protonmail.ch>
2019-11-22 16:35:27 -0500

    fontconfig: bump configVersion to 2.13

--- pkgs/development/libraries/fontconfig/default.nix
+++ pkgs/development/libraries/fontconfig/default.nix
@@ -23,7 +23,7 @@
 */
 
 let
-  configVersion = "2.11"; # bump whenever fontconfig breaks compatibility with older configurations
+  configVersion = "2.13"; # bump whenever fontconfig breaks compatibility with older configurations
 in
 stdenv.mkDerivation rec {
   pname = "fontconfig";
