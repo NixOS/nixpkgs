f1eeb0c3d1266dec40b4bd8d07a3a293fcb9914e
Izorkin <izorkin@elven.pw>
2019-11-23 17:23:06 +0300

    php: remove unused configureFlags

--- pkgs/development/interpreters/php/default.nix
+++ pkgs/development/interpreters/php/default.nix
@@ -202,9 +202,6 @@ let
 
         export EXTENSION_DIR=$out/lib/php/extensions
 
-        configureFlags+=(--with-config-file-path=$out/etc \
-          --includedir=$dev/include)
-
         ./buildconf --force
       '';
 
