4773059270dc44800fe58c7d86111bf2af2e4f1c
Oleksii Filonenko <brightone@protonmail.com>
2019-11-23 18:46:41 +0200

    sourcetrail: 2019.3.46 -> 2019.4.1
    Sourcetrail is now open source (GPL-3).
    
    - src: fetchurl -> fetchFromGitHub
    - remove installPhase
    - change meta
      - platforms: x86_64-linux -> all
      - license: unfree -> gpl3
      - maintainers: add filalex77

--- pkgs/development/tools/sourcetrail/default.nix
+++ pkgs/development/tools/sourcetrail/default.nix
@@ -1,77 +1,36 @@
-{ stdenv, fetchurl, autoPatchelfHook
-, zlib, expat, dbus, openssl, python3 }:
+{ stdenv, fetchFromGitHub, cmake, imagemagick
+, boost, dbus, expat, git, openssl, python3, qt5, zlib }:
 
 stdenv.mkDerivation rec {
   pname = "sourcetrail";
-  version = "2019.3.46";
+  version = "2019.4.61";
 
-  src = fetchurl {
-    name = "sourtrail.tar.gz";
-    url = "https://www.sourcetrail.com/downloads/${version}/linux/64bit";
-    sha256 = "0dnkxr3fijcsbn6pd20lyxhr9ns6ji1c4dffly9s16yq4x9ad5r4";
+  src = fetchFromGitHub {
+    owner = "CoatiSoftware";
+    repo = pname;
+    rev = version;
+    sha256 = "0srv5nnyhbxssb3ss94i3j0270rw1p0z9w2c8s96n94f5nksri7c";
   };
 
-  nativeBuildInputs = [ autoPatchelfHook ];
-  buildInputs = [ zlib expat dbus stdenv.cc.cc openssl python3 ];
+  patches = [ ./cmake-boost-shared-lib.patch ];
 
-  installPhase = ''
-    runHook preInstall
+  # Qt5_DIR should be specified manually
+  # See https://github.com/CoatiSoftware/Sourcetrail#for-unix
+  cmakeFlags = [ "-DQt5_DIR=${qt5.qtbase.dev}/lib/cmake/Qt5" ];
 
-    mkdir -p $out/bin $out/opt
+  # Should fix "undefined shm_open"
+  # https://stackoverflow.com/questions/9923495/undefined-reference-shm-open-already-add-lrt-flag-here
+  NIX_LDFLAGS = [ "-lrt" ];
 
-    mv -v setup/share $out
-    mv -v data/gui/icon/logo_1024_1024.png $out/share/icons/sourcetrail.png
-    mv -v data/gui/icon/project_256_256.png $out/share/icons/project-sourcetrail.png
-
-    mkdir -p $out/share/sourcetrail/doc
-    mv -v README EULA.txt $out/share/sourcetrail/doc
-    mv -v plugin $out/share/sourcetrail
-
-    cp -rv . $out/opt
-
-    rm $out/opt/lib/libssl.so
-    rm $out/opt/lib/platforms/{libqeglfs.so,libqwebgl.so}
-    ln -s ${openssl}/lib/libssl.so $out/opt/lib/libssl.so
-
-    substituteInPlace \
-      $out/share/applications/sourcetrail.desktop \
-      --replace /usr/bin/ $out/bin/
-
-    cat <<EOF > $out/bin/sourcetrail
-    #! ${stdenv.shell} -e
-
-    # XXX: Sourcetrail somehow copies the initial config files into the home
-    # directory without write permissions. We currently just copy them
-    # ourselves to work around this problem.
-    setup_config() {
-      local src dst
-
-      [ ! -d ~/.config/sourcetrail ] && mkdir -p ~/.config/sourcetrail
-      for src in $out/opt/data/fallback/*; do
-        dst=~/.config/sourcetrail/"\$(basename "\$src")"
-        if [ ! -e "\$dst" ]; then
-          cp -r "\$src" "\$dst"
-        fi
-      done
-
-      chmod -R u+w ~/.config/sourcetrail
-    }
-
-    [ -d "\$HOME" ] && setup_config
-    export PATH="\$PATH:${python3}/bin"
-    exec "$out/opt/Sourcetrail.sh" "\$@"
-    EOF
-
-    chmod +x $out/bin/sourcetrail
-
-    runHook postInstall
-  '';
+  nativeBuildInputs = [ cmake imagemagick ];
+  buildInputs = [ boost dbus expat git openssl python3 zlib ]
+    ++ (with qt5; [ qtbase qtsvg ]);
 
   meta = with stdenv.lib; {
-    homepage = https://www.sourcetrail.com;
+    homepage = "https://www.sourcetrail.com";
     description = "A cross-platform source explorer for C/C++ and Java";
-    platforms = [ "x86_64-linux" ];
-    license = licenses.unfree;
-    maintainers = with maintainers; [ midchildan ];
+    platforms = platforms.all;
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ filalex77 midchildan ];
   };
 }
