ccc12863517b437834d3ef5fb68c724440e672fc
Frederik Rietdijk <fridh@fridh.nl>
2019-11-24 21:23:45 +0100

    Revert "konsole: add missing kglobalaccel"
    This reverts commit 7232ea43b6f1392f0237679862524e78a51b8b84.

--- pkgs/applications/kde/konsole.nix
+++ pkgs/applications/kde/konsole.nix
@@ -4,7 +4,7 @@
   kbookmarks, kcompletion, kconfig, kconfigwidgets, kcoreaddons, kguiaddons,
   ki18n, kiconthemes, kinit, kdelibs4support, kio, knotifications,
   knotifyconfig, kparts, kpty, kservice, ktextwidgets, kwidgetsaddons,
-  kwindowsystem, kxmlgui, qtscript, knewstuff, kglobalaccel
+  kwindowsystem, kxmlgui, qtscript, knewstuff
 }:
 
 mkDerivation {
@@ -18,7 +18,6 @@ mkDerivation {
     kbookmarks kcompletion kconfig kconfigwidgets kcoreaddons kdelibs4support
     kguiaddons ki18n kiconthemes kinit kio knotifications knotifyconfig kparts kpty
     kservice ktextwidgets kwidgetsaddons kwindowsystem kxmlgui qtscript knewstuff
-    kglobalaccel
   ];
 
   propagatedUserEnvPkgs = [ (lib.getBin kinit) ];
