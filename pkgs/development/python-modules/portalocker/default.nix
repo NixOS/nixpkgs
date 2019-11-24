3ba47957883cd00dba8c128cbb70a8a38dcd0e10
Jonathan Ringer <jonringer117@gmail.com>
2019-11-23 20:08:13 -0800

    python3Packages.portalocker: fix build

--- pkgs/development/python-modules/portalocker/default.nix
+++ pkgs/development/python-modules/portalocker/default.nix
@@ -28,13 +28,6 @@ buildPythonPackage rec {
     pytestpep8
   ];
 
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/WoLpH/portalocker/commit/7741925738c7e66ae9c4a0944a04b6a3088037d5.patch";
-      sha256 = "1g95rnfbnagkkk9qfzzd5346dl3clbgjnzr2wk09m0wphds7zd8z";
-    })
-  ];
-
   meta = with lib; {
     description = "A library to provide an easy API to file locking";
     homepage = https://github.com/WoLpH/portalocker;
