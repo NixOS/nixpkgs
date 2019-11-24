0f00c2aee22517634ebec13e9778762ed71ae2b0
worldofpeace <worldofpeace@protonmail.ch>
2019-11-24 06:29:35 -0500

    chessx: use qt5's mkDerivation and latest qt
    Fixes #74020
    
    https://github.com/NixOS/nixpkgs/issues/65399

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22510,7 +22510,7 @@ in
 
   chessdb = callPackage ../games/chessdb { };
 
-  chessx = libsForQt59.callPackage ../games/chessx { };
+  chessx = libsForQt5.callPackage ../games/chessx { };
 
   chiaki = libsForQt5.callPackage ../games/chiaki { };
 
