e80252b9523f715f8aca89fbb2c8481128ea7ff4
Lassi Haasio <ilikeavocadoes@hush.com>
2019-11-25 13:20:00 +0200

    WIP: nixos: update system.stateVersion disclaimer

--- nixos/modules/installer/tools/tools.nix
+++ nixos/modules/installer/tools/tools.nix
@@ -162,7 +162,7 @@ in
         # This value determines the NixOS release with which your system is to be
         # compatible, in order to avoid breaking some software such as database
         # servers. You should change this only after NixOS release notes say you
-        # should.
+        # should. You should never need to change this.
         system.stateVersion = "${config.system.nixos.release}"; # Did you read the comment?
 
       }
