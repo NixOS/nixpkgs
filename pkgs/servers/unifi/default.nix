e35e970cc52d2c8cf841d76ffb13b2f72898a0b6
elseym <elseym@me.com>
2019-11-25 13:21:36 +0100

    unifiStable: 5.12.22 -> 5.12.35

--- pkgs/servers/unifi/default.nix
+++ pkgs/servers/unifi/default.nix
@@ -49,7 +49,7 @@ in {
   };
 
   unifiStable = generic {
-    version = "5.12.22";
-    sha256 = "0jpznm443v6b9gbnw99ighgd2bsknxsnwrklsjmmm6gk7naw27pb";
+    version = "5.12.35";
+    sha256 = "0ln8x7yisanbx1afclhffa0f3fk0sgh7dpj548xyhn5mgpwbj4i2";
   };
 }
