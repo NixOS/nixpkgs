7a49699a30d3e35b84f8f356d12194748fd868ce
Tyler Benster <tbenst@gmail.com>
2019-11-24 18:43:23 -0800

    pythonPackages.gorilla: init at 0.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3777,6 +3777,8 @@ in {
 
   google_resumable_media = callPackage ../development/python-modules/google_resumable_media { };
 
+  gorilla = callPackage ../development/python-modules/gorilla { };
+
   gpgme = toPythonModule (pkgs.gpgme.override {
     pythonSupport = true;
     inherit python;
