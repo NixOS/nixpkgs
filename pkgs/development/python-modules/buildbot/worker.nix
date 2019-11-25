538b4e523aef0aadb42acaae8a129648334ec84d
R. RyanTM <ryantm-bot@ryantm.com>
2019-11-25 08:47:45 -0800

    buildbot-worker: 2.5.0 -> 2.5.1

--- pkgs/development/python-modules/buildbot/worker.nix
+++ pkgs/development/python-modules/buildbot/worker.nix
@@ -3,11 +3,11 @@
 
 buildPythonPackage (rec {
   pname = "buildbot-worker";
-  version = "2.5.0";
+  version = "2.5.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "05c8q6ykharry4lv47imh6agq55fxar8a9ldwx46clb480qwyc43";
+    sha256 = "1kpj85x8xflrccvy840v9bl3q1j63rk9kahj1qirbai1fxwvzbik";
   };
 
   propagatedBuildInputs = [ twisted future ];
