ce2aa10765afce1eeb4a5d186494792a9e956071
talyz <kim.lindberger@gmail.com>
2019-11-26 17:32:01 +0100

    gitlab: 12.4.3 -> 12.5.0

--- pkgs/applications/version-management/gitlab/gitaly/default.nix
+++ pkgs/applications/version-management/gitlab/gitaly/default.nix
@@ -17,14 +17,14 @@ let
       };
   };
 in buildGoPackage rec {
-  version = "1.67.0";
+  version = "1.72.0";
   pname = "gitaly";
 
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitaly";
     rev = "v${version}";
-    sha256 = "1mj2l15hnxwqmyc8xn79d6qpmpqbqw2ishalr8qvn83nzdsbk8l3";
+    sha256 = "1zjxbnl1knhhfa50jl9y5a0l22ylwrlpqsblb1yb78fn4mgyqkk8";
   };
 
   goPackagePath = "gitlab.com/gitlab-org/gitaly";
