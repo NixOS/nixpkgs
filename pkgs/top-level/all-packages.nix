201defc4bf4ef96459878bab4d07ccd6fc017e05
Florian Klink <flokli@flokli.de>
2019-11-26 19:48:04 +0100

    bazel: pass jdk11_headless to --server_javabase (#74279)
    We only refer to openjdk by passing `--server_javabase="${runJdk}"` to
    the `bazel` executable.
    
    By passing jdk11_headless instead of jdk11, we cut bazels runtime
    closure from 1.6 GiB to 1.1 GiB.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9517,7 +9517,7 @@ in
     inherit (darwin.apple_sdk.frameworks) CoreFoundation CoreServices Foundation;
     buildJdk = jdk8;
     buildJdkName = "jdk8";
-    runJdk = jdk11;
+    runJdk = jdk11_headless;
     stdenv = if stdenv.cc.isClang then llvmPackages_6.stdenv else stdenv;
   };
 
