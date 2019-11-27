47f4ddf1acc08ef7d8729e228202e18790512e66
Timo Kaufmann <timokau@zoho.com>
2019-11-28 00:05:49 +0100

    python.pkgs.Cython: 0.29.14 -> 0.29.15

--- pkgs/development/python-modules/Cython/default.nix
+++ pkgs/development/python-modules/Cython/default.nix
@@ -26,11 +26,11 @@ let
 
 in buildPythonPackage rec {
   pname = "Cython";
-  version = "0.29.14";
+  version = "0.29.15";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e4d6bb8703d0319eb04b7319b12ea41580df44fd84d83ccda13ea463c6801414";
+    sha256 = "050lh336791yl76krn44zm2dz00mlhpb26bk9fq9wcfh0f3vpmp4";
   };
 
   nativeBuildInputs = [
