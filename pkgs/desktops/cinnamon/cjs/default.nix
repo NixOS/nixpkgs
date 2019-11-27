1cc7e6eeb0e1ad4e4d492f45a9871a53bc1cf7c1
Maciej Krüger <mkg20001@gmail.com>
2019-11-27 10:23:36 +0100

    feat: upgrade cjs

--- pkgs/desktops/cinnamon/cjs/default.nix
+++ pkgs/desktops/cinnamon/cjs/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cjs";
-  version = "4.2.0";
+  version = "4.4.0";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = "${pname}";
     rev = "${version}";
-    sha256 = "1yvzvfm1z0d8ca9vk9s0gbsir0ln7mcmlbczf0hh8vzpsg7m1zk5";
+    sha256 = "0q5h2pbwysc6hwq5js3lwi6zn7i5qjjy070ynfhfn3z69lw5iz2d";
   };
 
   buildInputs = [ autoconf-archive dbus_glib gobjectIntrospection pkgconfig spidermonkey_52 ];
