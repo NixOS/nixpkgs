11da668a9fe547896f201e7c94ee53f3f26e911e
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2019-11-27 19:01:05 +0200

    pythonPackages.testing-common-database: init at 2.0.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7120,6 +7120,8 @@ in {
 
   textfsm = callPackage ../development/python-modules/textfsm { };
 
+  testing-common-database = callPackage ../development/python-modules/testing-common-database { };
+
   testpath = callPackage ../development/python-modules/testpath { };
 
   testrepository = callPackage ../development/python-modules/testrepository { };
