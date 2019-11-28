eb066d22794e61ae3c073e69fa75bed557400d20
Maciej Krüger <mkg20001@gmail.com>
2019-11-28 22:49:45 +0100

    fix: nemo dbl $out & DESTDIR usage

--- pkgs/desktops/cinnamon/nemo/default.nix
+++ pkgs/desktops/cinnamon/nemo/default.nix
@@ -11,11 +11,7 @@ stdenv.mkDerivation rec {
     sha256 = "0sskq0rssxvna937md446x1489hkhxys1zq03hvl8asjqa259w2q";
   };
 
-  patches = [ ./0001-patch-datadir.patch ];
-
-  preInstallPhases = "exportDestdir";
-
-  exportDestdir = "export DESTDIR=$out";
+  patches = [ ./0001-patch-datadir.patch ./0002-patch-remove-update-mime-db-script.patch ];
 
   NIX_CFLAGS_COMPILE = [ "-I${glib.dev}/include/gio-unix-2.0" ];
 
