3847c08baede1a32c6099907bac7f244fa9cabf9
Maciej Krüger <mkg20001@gmail.com>
2019-11-28 19:15:43 +0100

    fix: finally fix cinnamon control center

--- pkgs/desktops/cinnamon/cinnamon-control-center/default.nix
+++ pkgs/desktops/cinnamon/cinnamon-control-center/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchFromGitHub, pkgconfig, autoreconfHook, glib, gettext, cinnamon-desktop, intltool, libxslt, gtk3, libnotify,
 gnome-menus, libxml2, systemd, upower, cinnamon-settings-daemon, colord, polkit, ibus, libcanberra_gtk3, libpulseaudio, isocodes, kerberos,
-libxkbfile, cinnamon-menus, dbus-glib, libgnomekbd, libxklavier, networkmanager, libwacom, gnome3 }:
+libxkbfile, cinnamon-menus, dbus-glib, libgnomekbd, libxklavier, networkmanager, libwacom, gnome3, libtool, wrapGAppsHook }:
 
 stdenv.mkDerivation rec {
   pname = "cinnamon-control-center";
@@ -14,7 +14,7 @@ stdenv.mkDerivation rec {
   };
 
   # TODO: fix network manager integration
- configureFlags = [ "--enable-systemd" "--disable-update-mimedb" "--disable-networkmanager" "--disable-modemmanager" ];
+ configureFlags = [ "--enable-systemd" "--disable-networkmanager" "--disable-modemmanager" ];
 
  # patches = [ ./region.patch ];
 
@@ -27,7 +27,17 @@ stdenv.mkDerivation rec {
   #  polkit ibus libcanberra_gtk3 libpulseaudio
   #  isocodes kerberos libxkbfile ];
 
-  nativeBuildInputs = [ pkgconfig autoreconfHook gettext /*gnome_common*/ intltool libxslt ];
+  preConfigurePhases = "confFixPhase";
+
+  confFixPhase = ''
+    patchShebangs ./autogen.sh
+    '';
+
+  autoreconfPhase = ''
+    NOCONFIGURE=1 bash ./autogen.sh
+    '';
+
+  nativeBuildInputs = [ pkgconfig autoreconfHook wrapGAppsHook gettext /*gnome_common*/ intltool libxslt libtool ];
 
   meta = {
     homepage = "http://cinnamon.linuxmint.com";
