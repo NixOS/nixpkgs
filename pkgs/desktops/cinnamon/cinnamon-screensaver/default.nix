dfc0221d27a7451bb7fee22dfe7ab08dd813b56b
Maciej Krüger <mkg20001@gmail.com>
2019-11-29 00:05:27 +0100

    refactor: cleanup pkgs/desktops/cinnamon/cinnamon-screensaver/default.nix
    Co-Authored-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/cinnamon/cinnamon-screensaver/default.nix
+++ pkgs/desktops/cinnamon/cinnamon-screensaver/default.nix
@@ -1,5 +1,4 @@
 { stdenv, fetchFromGitHub, pkgconfig, autoreconfHook, glib, gettext, cinnamon-desktop, intltool, libxslt, gtk3, libnotify,
-gnome-menus, libxml2, systemd, upower, cinnamon-settings-daemon, colord, polkit, ibus, libcanberra_gtk3, libpulseaudio, isocodes, kerberos,
 libxkbfile, cinnamon-menus, dbus-glib, libgnomekbd, libxklavier, networkmanager, libwacom, gnome3, libtool, wrapGAppsHook, tzdata, glibc, gobject-introspection, python3, pam }:
 
 stdenv.mkDerivation rec {
