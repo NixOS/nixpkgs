c961094d91422bd34b5f72f7d36d7c59b6b786d7
R. RyanTM <ryantm-bot@ryantm.com>
2019-11-27 16:15:23 -0800

    joker: 0.12.7 -> 0.14.0
    (cherry picked from commit 174190dae1ef010e937043c6126abd56e6b9fe49)

--- pkgs/development/interpreters/joker/default.nix
+++ pkgs/development/interpreters/joker/default.nix
@@ -2,16 +2,20 @@
 
 buildGoModule rec {
   pname = "joker";
-  version = "0.12.7";
+  version = "0.14.0";
 
   src = fetchFromGitHub {
     rev = "v${version}";
     owner = "candid82";
     repo = "joker";
-    sha256 = "0panmhrg1i158xbvqvq3s3217smbj7ynwlaiks18pmss36xx9dk4";
+    sha256 = "1b38alajxs89a9x3f3ldk1nlynp6j90qhl1m2c6561rsm41sqfz0";
   };
 
   modSha256 = "0i16vf7n1xfz5kp9w3fvyc9y9wgz4h396glgpdaznpxjr12rb43j";
+
+  preBuild = ''
+    go generate ./...
+  '';
 
   meta = with stdenv.lib; {
     homepage = https://github.com/candid82/joker;
