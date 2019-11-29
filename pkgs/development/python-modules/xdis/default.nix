170a383516887693eb65eaeb202e2a6e925637cc
ahiaao <ahiaao@waifu.club>
2019-11-29 15:03:57 -0800

    pythonPackages.xdis: Add magics for current python versions

--- pkgs/development/python-modules/xdis/default.nix
+++ pkgs/development/python-modules/xdis/default.nix
@@ -15,6 +15,11 @@ buildPythonPackage rec {
     sha256 = "1h4j8hincf49zyd0rvn4bh0ypj8836y8vz3d496ycb9gjzkr6044";
   };
 
+  patches = [
+    ./python375.patch # Add Python 3.7.5 as a 3.7 release. Fixed upstream in version 4.1.1
+    ./python2717.patch # Add Python 2.7.17 as a 2.7 release. Fixed upstream in version 4.1.3
+  ];
+
   checkInputs = [ pytest ];
   propagatedBuildInputs = [ six click ];
 
