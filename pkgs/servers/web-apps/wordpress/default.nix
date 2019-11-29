7cd3628a1549882c457fb80588253474a60dca93
R. RyanTM <ryantm-bot@ryantm.com>
2019-11-29 14:06:56 -0800

    wordpress: 5.2.4 -> 5.3

--- pkgs/servers/web-apps/wordpress/default.nix
+++ pkgs/servers/web-apps/wordpress/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "wordpress";
-  version = "5.2.4";
+  version = "5.3";
 
   src = fetchurl {
     url = "https://wordpress.org/${pname}-${version}.tar.gz";
-    sha256 = "1vf5220rw37sxvzy6yxn636ip2lx4bkc84z7q8rdwcs2wkv6md1p";
+    sha256 = "1aynx6i5qdlbfq1s9pik6xwk8zxx4rynv63jv52rlyk4gqgr37dj";
   };
 
   installPhase = ''
