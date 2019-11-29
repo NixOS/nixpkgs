20b4e8b2f810a1f5dcc77ee81afd9030949b2c18
Maciej Krüger <mkg20001@gmail.com>
2019-11-29 04:32:32 +0100

    fix: cinnamon-screensaver deps

--- pkgs/desktops/cinnamon/cinnamon-common/default.nix
+++ pkgs/desktops/cinnamon/cinnamon-common/default.nix
@@ -1,4 +1,30 @@
-{ atk, autoreconfHook, cacert, cinnamon-desktop, cinnamon-menus, cjs, dbus-glib, fetchFromGitHub, gdk-pixbuf, glib, gobject-introspection, gtk3, intltool, json-glib, libcroco, libsoup, libstartup_notification, libXtst, muffin, networkmanager, pkgconfig, polkit, stdenv, wrapGAppsHook, libxml2, gnome2, python3 }:
+{ atk,
+  autoreconfHook,
+  cacert,
+  cinnamon-desktop,
+  cinnamon-menus,
+  cjs,
+  dbus-glib,
+  fetchFromGitHub,
+  gdk-pixbuf,
+  glib,
+  gobject-introspection,
+  gtk3,
+  intltool,
+  json-glib,
+  libcroco,
+  libsoup,
+  libstartup_notification,
+  libXtst,
+  muffin,
+  networkmanager,
+  pkgconfig,
+  polkit,
+  stdenv,
+  wrapGAppsHook,
+  libxml2,
+  gnome2,
+  python3 }:
 
 stdenv.mkDerivation rec {
   pname = "cinnamon-common";
@@ -11,8 +37,34 @@ stdenv.mkDerivation rec {
     sha256 = "0sv7nqd1l6c727qj30dcgdkvfh1wxpszpgmbdyh58ilmc8xklnqd";
   };
 
-  buildInputs = [ (python3.withPackages (pp: with pp; [ setproctitle ])) atk cacert cinnamon-desktop cinnamon-menus cjs dbus-glib gdk-pixbuf glib gtk3 json-glib libcroco libsoup libstartup_notification libXtst muffin networkmanager pkgconfig polkit libxml2 ];
-  nativeBuildInputs = [ autoreconfHook wrapGAppsHook intltool gnome2.gtkdoc gobject-introspection ];
+  buildInputs = [ # TODO: review if we really need this all
+    (python3.withPackages (pp: with pp; [ setproctitle pygobject3 ]))
+    atk
+    cacert
+    cinnamon-desktop
+    cinnamon-menus
+    cjs
+    dbus-glib
+    gdk-pixbuf
+    glib
+    gtk3
+    json-glib
+    libcroco
+    libsoup
+    libstartup_notification
+    libXtst muffin
+    networkmanager
+    pkgconfig
+    polkit
+    libxml2
+  ];
+  nativeBuildInputs = [
+    autoreconfHook
+    wrapGAppsHook
+    intltool
+    gnome2.gtkdoc
+    gobject-introspection
+  ];
 
   autoreconfPhase = ''
     GTK_DOC_CHECK=false NOCONFIGURE=1 bash ./autogen.sh
