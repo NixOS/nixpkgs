b105080cbacc194085cab0e8f3efe20abf49d961
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-11-29 00:21:40 -0500

    nixos/device-tree: fix overlays in the Nix store

--- pkgs/os-specific/linux/device-tree/default.nix
+++ pkgs/os-specific/linux/device-tree/default.nix
@@ -5,7 +5,7 @@ with stdenvNoCC.lib; {
     name = "device-tree-overlays";
     nativeBuildInputs = [ dtc findutils ];
     buildCommand = let
-      quotedDtbos = concatMapStringsSep " " (o: "\"${toString o}\"") (toList overlays);
+      quotedDtbos = concatMapStringsSep " " (o: "\"${o}\"") (toList overlays);
     in ''
       for dtb in $(find ${base} -name "*.dtb" ); do
         outDtb=$out/$(realpath --relative-to "${base}" "$dtb")
