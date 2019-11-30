4095c79588953c5e0636ed27e23d3f820fe7c93c
Doron Behar <doron.behar@gmail.com>
2019-11-30 11:41:51 +0200

    gotify-server: 2.0.11 -> 2.0.12

--- pkgs/servers/gotify/ui.nix
+++ pkgs/servers/gotify/ui.nix
@@ -8,13 +8,13 @@ yarn2nix-moretea.mkYarnPackage rec {
   packageJSON = ./package.json;
   yarnNix = ./yarndeps.nix;
 
-  version = "2.0.11";
+  version = "2.0.12";
 
   src_all = fetchFromGitHub {
     owner = "gotify";
     repo = "server";
     rev = "v${version}";
-    sha256 = "0zrylyaxy1cks1wlzyf0di8in2braj4pfriyqa24vipwrlnhvgs6";
+    sha256 = "0pkws83ymmlxcdxadb1w6rmibw84vzhx9xrhxc6b1rjncb80l0kk";
   };
   src = "${src_all}/ui";
 
