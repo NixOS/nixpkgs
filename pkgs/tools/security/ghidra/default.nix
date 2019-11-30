8404d980213a89f6750cacea2b445f021785165b
Latebricole <32686187+Latebricole@users.noreply.github.com>
2019-11-30 22:13:02 +0000

    ghidra: Added desktop file
    Added a desktop file for Ghidra
    
    Ghidra ships its icons as a .ico file, which isn't support by freedesktop. This means that to have icons, we need to extract the pngs from ghidra.ico, then copy them to the relevant folders.
    This adds a requirement on a library to extract them, and also requires them to be copied over, which isn't ideal.
    
    
    Potentially icotool from icoutils could be

--- pkgs/tools/security/ghidra/default.nix
+++ pkgs/tools/security/ghidra/default.nix
@@ -1,8 +1,18 @@
 { stdenv, fetchurl, unzip, lib, makeWrapper, autoPatchelfHook
-, openjdk11, pam
+, openjdk11, pam, makeDesktopItem, imagemagick
 }: let
 
   pkg_path = "$out/lib/ghidra";
+
+  desktopItem = makeDesktopItem {
+    name = "ghidra";
+    exec = "ghidra";
+    icon = "ghidra";
+    desktopName = "Ghidra";
+    genericName = "Ghidra Software Reverse Engineering Suite";
+    categories = "Development;";
+  };
+
 
 in stdenv.mkDerivation {
 
@@ -22,13 +32,29 @@ in stdenv.mkDerivation {
   buildInputs = [
     stdenv.cc.cc.lib
     pam
+    imagemagick
   ];
 
   dontStrip = true;
 
   installPhase = ''
     mkdir -p "${pkg_path}"
+    mkdir -p "${pkg_path}" "$out/share/applications"
     cp -a * "${pkg_path}"
+    ln -s ${desktopItem}/share/applications/* $out/share/applications
+    
+    convert "${pkg_path}/support/ghidra.ico" ghidra.png
+    rm ghidra-3.png
+    for size in 16 24 32 48 64 128 256; do
+      mkdir -p $out/share/icons/hicolor/"$size"x"$size"/apps
+    done;
+    mv ghidra-0.png "$out/share/icons/hicolor/16x16/apps/ghidra.png"
+    mv ghidra-1.png "$out/share/icons/hicolor/24x24/apps/ghidra.png"
+    mv ghidra-2.png "$out/share/icons/hicolor/32x32/apps/ghidra.png"
+    mv ghidra-4.png "$out/share/icons/hicolor/48x48/apps/ghidra.png"
+    mv ghidra-5.png "$out/share/icons/hicolor/64x64/apps/ghidra.png"
+    mv ghidra-6.png "$out/share/icons/hicolor/128x128/apps/ghidra.png"
+    mv ghidra-7.png "$out/share/icons/hicolor/256x256/apps/ghidra.png"
   '';
 
   postFixup = ''
