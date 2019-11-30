f49002cd26fc582bde059a59ad07e7743ad2f946
nyanloutre <paul@nyanlout.re>
2019-11-30 13:20:38 +0100

    libappindicator-gtk2: add missing automake step

--- pkgs/development/libraries/libappindicator/default.nix
+++ pkgs/development/libraries/libappindicator/default.nix
@@ -1,7 +1,7 @@
 # TODO: Resolve the issues with the Mono bindings.
 
 { stdenv, fetchurl, fetchpatch, lib, file
-, pkgconfig, autoconf
+, pkgconfig, autoreconfHook
 , glib, dbus-glib, gtkVersion ? "3"
 , gtk2 ? null, libindicator-gtk2 ? null, libdbusmenu-gtk2 ? null
 , gtk3 ? null, libindicator-gtk3 ? null, libdbusmenu-gtk3 ? null
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
     sha256 = "17xlqd60v0zllrxp8bgq3k5a1jkj0svkqn8rzllcyjh8k0gpr46m";
   };
 
-  nativeBuildInputs = [ pkgconfig autoconf vala gobject-introspection ];
+  nativeBuildInputs = [ pkgconfig autoreconfHook vala gobject-introspection ];
 
   propagatedBuildInputs =
     if gtkVersion == "2"
@@ -47,7 +47,6 @@ stdenv.mkDerivation rec {
   ];
 
   postPatch = ''
-    autoconf
     for f in {configure,ltmain.sh,m4/libtool.m4}; do
       substituteInPlace $f \
         --replace /usr/bin/file ${file}/bin/file
