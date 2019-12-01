503b5c970539cdd25a212ff2f639209f6e5e9fc3
Aaron Andersen <aaron@fosslib.net>
2019-12-01 11:31:16 -0500

    zabbix: drop 4.2.x series

--- pkgs/servers/monitoring/zabbix/versions.nix
+++ pkgs/servers/monitoring/zabbix/versions.nix
@@ -4,11 +4,6 @@ generic: {
     sha256 = "1rpkgimi9kv9zrcis1zwbrny7yzralick206n71i8071hf95jvwx";
   };
 
-  v42 = generic {
-    version = "4.2.7";
-    sha256 = "09znh8x1sass5mw6wjrfmizjbfls8ad2c16y24ldfj40hlfxz6wx";
-  };
-
   v40 = generic {
     version = "4.0.15";
     sha256 = "19q8h79h7lnsq6gbd0awrlw59scg92x0a1375kd2av6bjpx8r0y9";
