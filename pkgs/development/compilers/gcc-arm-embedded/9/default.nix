fcba6a0619bafaaa15fef5c138c2f6c54a20ad6f
Pavol Rusnak <pavol@rusnak.io>
2019-12-01 18:42:16 +0100

    gcc-arm-embedded: unify gcc-arm-embedded/{6,7,8,9}/default.nix

--- pkgs/development/compilers/gcc-arm-embedded/9/default.nix
+++ pkgs/development/compilers/gcc-arm-embedded/9/default.nix
@@ -9,23 +9,20 @@ stdenv.mkDerivation rec {
   version = "9-2019-q4-major";
   subdir = "9-2019q4/RC2.1";
 
-  src =
-    if stdenv.system == "x86_64-linux" then
-      fetchurl {
-        url = "https://developer.arm.com/-/media/Files/downloads/gnu-rm/${subdir}/gcc-arm-none-eabi-${version}-x86_64-linux.tar.bz2";
-        sha256="bcd840f839d5bf49279638e9f67890b2ef3a7c9c7a9b25271e83ec4ff41d177a";
-      }
-    else if stdenv.system == "aarch64-linux" then
-      fetchurl {
-        url = "https://developer.arm.com/-/media/Files/downloads/gnu-rm/${subdir}/gcc-arm-none-eabi-${version}-aarch64-linux.tar.bz2";
-        sha256="1f5b9309006737950b2218250e6bb392e2d68d4f1a764fe66be96e2a78888d83";
-      }
-    else if stdenv.system == "x86_64-darwin" then
-      fetchurl {
-        url = "https://developer.arm.com/-/media/Files/downloads/gnu-rm/${subdir}/gcc-arm-none-eabi-${version}-mac.tar.bz2";
-        sha256="1249f860d4155d9c3ba8f30c19e7a88c5047923cea17e0d08e633f12408f01f0";
-      }
-    else throw "unsupported platform";
+  suffix = {
+    aarch64-linux = "aarch64-linux";
+    x86_64-darwin = "mac";
+    x86_64-linux  = "x86_64-linux";
+  }.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+
+  src = fetchurl {
+    url = "https://developer.arm.com/-/media/Files/downloads/gnu-rm/${subdir}/gcc-arm-none-eabi-${version}-${suffix}.tar.bz2";
+    sha256 = {
+      aarch64-linux = "1f5b9309006737950b2218250e6bb392e2d68d4f1a764fe66be96e2a78888d83";
+      x86_64-darwin = "1249f860d4155d9c3ba8f30c19e7a88c5047923cea17e0d08e633f12408f01f0";
+      x86_64-linux  = "bcd840f839d5bf49279638e9f67890b2ef3a7c9c7a9b25271e83ec4ff41d177a";
+    }.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+  };
 
   phases = [ "unpackPhase" "installPhase" "fixupPhase" ];
 
