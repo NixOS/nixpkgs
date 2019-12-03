efe7ed31b5745e5b7ee665d1e3f5a9f2784670d0
mtrsk <marcos.schonfinkel@protonmail.com>
2019-12-02 21:26:30 -0300

    python3Packages.responses: 0.10.6 -> 0.10.7

--- pkgs/development/python-modules/responses/default.nix
+++ pkgs/development/python-modules/responses/default.nix
@@ -1,16 +1,33 @@
-{ buildPythonPackage, fetchPypi
-, cookies, mock, requests, six }:
+{ buildPythonPackage
+, fetchPypi
+, lib
+, cookies
+, mock
+, requests
+, six
+, flake8
+, pytest
+, pytestcov
+, pytest-localserver
+}:
 
 buildPythonPackage rec {
   pname = "responses";
-  version = "0.10.6";
+  version = "0.10.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "502d9c0c8008439cfcdef7e251f507fcfdd503b56e8c0c87c3c3e3393953f790";
+    sha256 = "0fv28b89bhpba3kyfhay8284dw0yapafs178qxmi1ilzl53fbm26";
   };
 
+  nativeBuildInputs = [ flake8 pytest pytestcov pytest-localserver ];
   propagatedBuildInputs = [ cookies mock requests six ];
 
-  doCheck = false;
+  meta = with lib; {
+    description = "A utility library for mocking out the requests Python library";
+    homepage = "https://github.com/getsentry/responses";
+    license = licenses.apsl20;
+    maintainers = with maintainers; [ mtrsk ];
+    platforms = platforms.linux;
+  };
 }
