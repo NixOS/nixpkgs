34b7bb566c712338b99edc780f4270d94c9f6c1b
davidak <git@davidak.de>
2019-12-03 15:12:00 +0100

    python: pytest: 5.2.4 -> 5.3.1

--- pkgs/development/python-modules/pytest/default.nix
+++ pkgs/development/python-modules/pytest/default.nix
@@ -3,7 +3,7 @@
 , atomicwrites, mock, writeText, pathlib2, wcwidth, packaging, isPyPy, python
 }:
 buildPythonPackage rec {
-  version = "5.2.4";
+  version = "5.3.1";
   pname = "pytest";
 
   disabled = !isPy3k;
@@ -15,7 +15,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ff0090819f669aaa0284d0f4aad1a6d9d67a6efdc6dd4eb4ac56b704f890a0d6";
+    sha256 = "f67403f33b2b1d25a6756184077394167fe5e2f9d8bdaab30707d19ccec35427";
   };
 
   checkInputs = [ hypothesis mock ];
