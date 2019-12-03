c4537f867a64157220ec2e838a6296d5215cde1e
Will Dietz <w@wdtz.org>
2019-12-03 08:27:22 -0600

    gnutls: 3.6.9 -> 3.6.11 (PR #75025)
    (cherry picked from commit c555684c6d55ae8082d298553471022b15be8dcc)
    I do believe that likelihood of improvements is much larger than
    likelihood of breakage.

--- pkgs/development/libraries/gnutls/default.nix
+++ pkgs/development/libraries/gnutls/default.nix
@@ -8,7 +8,7 @@
 
 assert guileBindings -> guile != null;
 let
-  version = "3.6.9";
+  version = "3.6.11.1";
 
   # XXX: Gnulib's `test-select' fails on FreeBSD:
   # http://hydra.nixos.org/build/2962084/nixlog/1/raw .
@@ -24,7 +24,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "mirror://gnupg/gnutls/v3.6/gnutls-${version}.tar.xz";
-    sha256 = "1jqz5s3lv8sa53348cfi9nr5pw5l55n8m40b8msdvv0pb2jzqca3";
+    sha256 = "1y1wadpsrj5ai603xv5bgssl9v0pb1si2hg14zqdnmcsvgri5fpv";
   };
 
   outputs = [ "bin" "dev" "out" "man" "devdoc" ];
