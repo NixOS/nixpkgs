6c1a498cfe2489b2dcee7dfdb4566bade690a354
Oleksii Filonenko <brightone@protonmail.com>
2019-12-03 19:58:20 +0200

    bitwarden_rs: 1.9.1 -> 1.13.0
    Co-authored-by: msteen <msteen@users.noreply.github.com>
    Co-authored-by: Daniel Løvbrøtte Olsen <daniel.olsen99+GitHub@gmail.com>

--- pkgs/tools/security/bitwarden_rs/default.nix
+++ pkgs/tools/security/bitwarden_rs/default.nix
@@ -2,30 +2,29 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "bitwarden_rs";
-  version = "1.9.1";
+  version = "1.13.0";
 
   src = fetchFromGitHub {
     owner = "dani-garcia";
     repo = pname;
     rev = version;
-    sha256 = "0jfb4b2lp2v01aw615lx0qj1qh73hyrbjn9kva7zqp74wcfw12gp";
+    sha256 = "1qhfm58x08q6sv9r5nqs0qryggcyrasjlxw3h1wb3j6aq474p6jf";
   };
-
-  cargoPatches = [
-    # type annotations required: cannot resolve `std::string::String: std::convert::AsRef<_>`
-    ./cargo-lock-lettre.patch
-  ];
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ openssl ] ++ stdenv.lib.optionals stdenv.isDarwin [ Security CoreServices ];
 
   RUSTC_BOOTSTRAP = 1;
 
-  cargoSha256 = "0p39gqrqdmgqhngp1qyh6jl0sp0ifj5n3bxfqafjbspb4zph3ls4";
+  cargoSha256 = "03szl83r9mjn08mqkwah8fmzpszci8ay7ki69yx1rgv3zx7npnxk";
+  cargoBuildFlags = [ "--features sqlite" ];
+
+  # FIXME: The rust check phase seems broken due to passing the flags after --.
+  doCheck = false;
 
   meta = with stdenv.lib; {
-    description = "An unofficial lightweight implementation of the Bitwarden server API using Rust and SQLite";
-    homepage = https://github.com/dani-garcia/bitwarden_rs;
+    description = "Unofficial Bitwarden compatible server written in Rust";
+    homepage = "https://github.com/dani-garcia/bitwarden_rs";
     license = licenses.gpl3;
     maintainers = with maintainers; [ msteen ];
     platforms = platforms.all;
