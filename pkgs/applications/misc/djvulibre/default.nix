3b4f667fbc7354cdd134680ab3b089aed2680052
Dima <dgoldin+github@protonmail.ch>
2019-12-03 02:05:49 +0100

    [19.03] djvulibre: patching multiple CVEs
    Addresses:
    
    https://github.com/NixOS/nixpkgs/issues/73624
    https://github.com/NixOS/nixpkgs/issues/70086
    (cherry picked from commit be7e51a083f3cc5560b3683d915248c32b317acc)

--- pkgs/applications/misc/djvulibre/default.nix
+++ pkgs/applications/misc/djvulibre/default.nix
@@ -12,6 +12,17 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ libjpeg libtiff librsvg libiconv ];
 
+  patches = [
+    ./CVE-2019-18804.patch
+    # This one is needed to make the following
+    # two CVE patches apply cleanly
+    ./fix_hongfuzz_crash.patch
+    ./CVE-2019-15142.patch
+    ./CVE-2019-15143.patch
+    ./CVE-2019-15144.patch
+    ./CVE-2019-15145.patch
+  ];
+
   meta = with stdenv.lib; {
     description = "A library and viewer for the DJVU file format for scanned images";
     homepage = http://djvu.sourceforge.net;
