499369a5d765b394686446ea7767b8a9e69f872e
oxalica <oxalicc@pm.me>
2019-12-04 20:14:34 +0800

    cargo-edit: 0.3.3 -> 0.4.1

--- pkgs/tools/package-management/cargo-edit/default.nix
+++ pkgs/tools/package-management/cargo-edit/default.nix
@@ -4,20 +4,30 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-edit";
-  version = "0.3.3";
+  version = "0.4.1";
 
   src = fetchFromGitHub {
     owner = "killercup";
     repo = pname;
     rev = "v${version}";
-    sha256 = "05b64bm9441crw74xlywjg2y3psljk2kf9xsrixaqwbnnahi0mm5";
+    sha256 = "087l8qdwfnnklw6zyjwflxh7hyhh4r7wala36cc4lrj7lag2xm9n";
   };
 
-  cargoSha256 = "1hjjw3i35vqr6nxsv2m3izq4x8c2a6wvl5c2kjlpg6shy9j2mjaa";
+  cargoSha256 = "1ih1p9jdwr1ymq2p6ipz6rybi17f3qn65kn4bqkgzx36afvpnd5l";
 
   nativeBuildInputs = lib.optional (!stdenv.isDarwin) pkgconfig;
   buildInputs = lib.optional (!stdenv.isDarwin) openssl;
   propagatedBuildInputs = lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Security;
+
+  patches = [
+    # https://github.com/killercup/cargo-edit/pull/362
+    ./no_upgrade_index_in_tests.patch
+  ];
+
+  # The default `/build` will fail the test (seems) due to permission problem.
+  preCheck = ''
+    export TMPDIR="/tmp"
+  '';
 
   meta = with lib; {
     description = "A utility for managing cargo dependencies from the command line";
