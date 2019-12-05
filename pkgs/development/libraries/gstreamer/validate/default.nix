c9e2a3114d1c21c6f61c60e0b720c9214c6b1bca
Will Dietz <w@wdtz.org>
2019-12-05 11:24:24 -0600

    gst_all_1.gst-validate: 1.16.1 -> 1.16.2

--- pkgs/development/libraries/gstreamer/validate/default.nix
+++ pkgs/development/libraries/gstreamer/validate/default.nix
@@ -10,11 +10,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gst-validate";
-  version = "1.16.1";
+  version = "1.16.2";
 
   src = fetchurl {
     url = "${meta.homepage}/src/${pname}/${pname}-${version}.tar.xz";
-    sha256 = "1698arvmpb0cvyi8ll1brvs51vs7i3f3fw19iswh8xhj5adrn1vz";
+    sha256 = "1jpfrzg3yc6kp66bgq3jy14xsj3x71mk2zh0k16yf0326awwqqa8";
   };
 
   outputs = [ "out" "dev" ];
