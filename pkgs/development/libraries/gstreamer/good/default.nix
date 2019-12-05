26393df73804dd9413ca9172545418a2429dda62
Will Dietz <w@wdtz.org>
2019-12-05 11:24:11 -0600

    gst_all_1.gst-plugins-good: 1.16.1 -> 1.16.2

--- pkgs/development/libraries/gstreamer/good/default.nix
+++ pkgs/development/libraries/gstreamer/good/default.nix
@@ -48,13 +48,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "gst-plugins-good";
-  version = "1.16.1";
+  version = "1.16.2";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "${meta.homepage}/src/${pname}/${pname}-${version}.tar.xz";
-    sha256 = "07wgz9anf4ram2snp8n1wv6l0q3pd00iaw8bvw3wgklg05lvxflz";
+    sha256 = "068k3cbv1yf3gbllfdzqsg263kzwh21y8dpwr0wvgh15vapkpfs0";
   };
 
   patches = [ ./fix_pkgconfig_includedir.patch ];
