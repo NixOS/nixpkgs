c28f6fce9948e8d6927fa274803e3887d77fbf46
Oleksii Filonenko <brightone@protonmail.com>
2019-12-05 12:56:33 +0200

    oq: init at 1.0.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5420,6 +5420,8 @@ in
     stdenv = clangStdenv;
   };
 
+  oq = callPackage ../development/tools/oq { };
+
   out-of-tree = callPackage ../development/tools/out-of-tree { };
 
   oppai-ng = callPackage ../tools/misc/oppai-ng { };
