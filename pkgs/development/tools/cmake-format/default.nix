b64a640edf6b989436ae061cfbc544b191c355d5
Tobias Mayer <tobim@fastmail.fm>
2019-12-07 22:41:56 +0100

    cmake-format: 0.6.0 -> 0.6.2

--- pkgs/development/tools/cmake-format/default.nix
+++ pkgs/development/tools/cmake-format/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonApplication rec {
   pname = "cmake-format";
-  version = "0.6.0";
+  version = "0.6.2";
 
   src = fetchPypi {
     inherit version;
     pname = "cmake_format";
-    sha256 = "0sip832bxsvnm7fhqhx49d53g2s7swdk3fhyhlglm2shgj89b5zw";
+    sha256 = "0nqwr7rvqkniqa53vddikncqsvm1r90p768rsginflw5rv33hi2j";
   };
 
   propagatedBuildInputs = [ autopep8 flake8 jinja2 pylint pyyaml ];
