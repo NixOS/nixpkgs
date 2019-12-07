50cc8f9737524770cc0f2b191463691e47aa08d1
Izorkin <izorkin@elven.pw>
2019-12-07 12:44:13 +0300

    phpPackages.phpstan: 0.11.19 -> 0.12.0

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -408,12 +408,12 @@ let
   };
 
   phpstan = mkDerivation rec {
-    version = "0.11.19";
+    version = "0.12.0";
     pname = "phpstan";
 
     src = pkgs.fetchurl {
       url = "https://github.com/phpstan/phpstan/releases/download/${version}/phpstan.phar";
-      sha256 = "0b04d2x07vipx1850v3d2hga3s6ssv7g21x58dhcjrg35i1bvq71";
+      sha256 = "0029727360x0qf6acw4y9yczj59y477i0hx278pb64dvy63fzkmk";
     };
 
     phases = [ "installPhase" ];
