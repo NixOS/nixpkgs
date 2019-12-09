f8e0e907f5190a0d8dc5e64dea36bd2bbafee19e
worldofpeace <worldofpeace@protonmail.ch>
2019-12-08 19:43:41 -0500

    gsignondPlugins: use recurseIntoAttrs
    I believe this is why these packages won't appear
    in nix search.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14677,7 +14677,7 @@ in
     plugins = [];
   };
 
-  gsignondPlugins = {
+  gsignondPlugins = recurseIntoAttrs {
     sasl = callPackage ../development/libraries/gsignond/plugins/sasl.nix { };
     oauth = callPackage ../development/libraries/gsignond/plugins/oauth.nix { };
     lastfm = callPackage ../development/libraries/gsignond/plugins/lastfm.nix { };
