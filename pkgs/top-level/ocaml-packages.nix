583b483ebb45f1a8be23e7766c7c14115856d853
Vincent Laporte <Vincent.Laporte@gmail.com>
2019-12-09 10:34:05 +0000

    ocamlPackages.irmin-watcher: init at 0.4.1

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -386,6 +386,8 @@ let
 
     irmin-test = callPackage ../development/ocaml-modules/irmin/test.nix { };
 
+    irmin-watcher = callPackage ../development/ocaml-modules/irmin-watcher { };
+
     jingoo = callPackage ../development/ocaml-modules/jingoo {
       pcre = ocaml_pcre;
     };
