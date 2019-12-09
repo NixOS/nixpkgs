cf0419e7d5c774ea1d94261174e14eac2e810338
Timo Kaufmann <timokau@zoho.com>
2019-12-09 08:20:03 +0100

    fplll: add changelog

--- pkgs/development/libraries/fplll/default.nix
+++ pkgs/development/libraries/fplll/default.nix
@@ -29,6 +29,15 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = ''Lattice algorithms using floating-point arithmetic'';
+    changelog = [
+      # Some release notes are added to the github tags, though they are not
+      # always complete.
+      "https://github.com/fplll/fplll/releases/tag/${version}"
+      # Releases are announced on this mailing list. Unfortunately it is not
+      # possible to generate a direct link to the most recent announcement, but
+      # this search should find it.
+      "https://groups.google.com/forum/#!searchin/fplll-devel/FPLLL$20${version}"
+    ];
     license = licenses.lgpl21Plus;
     maintainers = with maintainers; [raskin];
     platforms = platforms.unix;
