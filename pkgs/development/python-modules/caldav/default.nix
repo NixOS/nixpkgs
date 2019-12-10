d9724d74082878ef73021b3095897428fd1ecbcb
Frederik Rietdijk <fridh@fridh.nl>
2019-12-10 19:07:38 +0100

    python: caldav: 0.6.1 -> 0.6.2

--- pkgs/development/python-modules/caldav/default.nix
+++ pkgs/development/python-modules/caldav/default.nix
@@ -3,13 +3,13 @@
 
 buildPythonPackage rec {
   pname = "caldav";
-  version = "0.6.1";
+  version = "0.6.2";
 
   propagatedBuildInputs = [ tzlocal requests vobject lxml nose ];
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "eddb7f4e6a3eb5f02eaa2227817a53ac4372d4c4d51876536f4c6f00282f569e";
+    sha256 = "80c33b143539da3a471148ac89512f67d9df3a5286fae5a023e2ad3923246c0d";
   };
 
   meta = with lib; {
