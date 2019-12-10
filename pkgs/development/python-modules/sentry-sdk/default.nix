6084c5eb3256968910b49e5961394d8cdf3f485d
Frederik Rietdijk <fridh@fridh.nl>
2019-12-10 19:07:42 +0100

    python: sentry-sdk: 0.13.2 -> 0.13.5

--- pkgs/development/python-modules/sentry-sdk/default.nix
+++ pkgs/development/python-modules/sentry-sdk/default.nix
@@ -20,11 +20,11 @@
 
 buildPythonPackage rec {
   pname = "sentry-sdk";
-  version = "0.13.2";
+  version = "0.13.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ff1fa7fb85703ae9414c8b427ee73f8363232767c9cd19158f08f6e4f0b58fc7";
+    sha256 = "c6b919623e488134a728f16326c6f0bcdab7e3f59e7f4c472a90eea4d6d8fe82";
   };
 
   checkInputs = [ django flask tornado bottle rq falcon ]
