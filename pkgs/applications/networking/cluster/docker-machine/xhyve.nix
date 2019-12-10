5c01936d7d2b6904476c79342261cbb4ef62db11
Michael Roitzsch <reactorcontrol@icloud.com>
2019-12-10 11:55:52 +0100

    docker-machine-xhyve: update repository location
    The zchee repository now redirects to machine-drivers.

--- pkgs/applications/networking/cluster/docker-machine/xhyve.nix
+++ pkgs/applications/networking/cluster/docker-machine/xhyve.nix
@@ -9,7 +9,7 @@ buildGoPackage rec {
 
   src = fetchFromGitHub {
     rev    = "v${version}";
-    owner  = "zchee";
+    owner  = "machine-drivers";
     repo   = "docker-machine-driver-xhyve";
     sha256 = "0rj6pyqp4yv4j28bglqjs95rip5i77vv8mrkmqv1rxrsl3i8aqqy";
   };
@@ -18,7 +18,7 @@ buildGoPackage rec {
   buildInputs = [ Hypervisor vmnet ];
 
   meta = with stdenv.lib; {
-    homepage = https://github.com/zchee/docker-machine-driver-xhyve;
+    homepage = https://github.com/machine-drivers/docker-machine-driver-xhyve;
     description = "Xhyve driver for docker-machine.";
     license = licenses.bsd3;
     maintainers = with maintainers; [ periklis ];
