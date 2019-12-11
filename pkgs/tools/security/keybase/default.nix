f88f4612551c74972c41734e357c5bacdd783157
Oleksii Filonenko <brightone@protonmail.com>
2019-12-11 23:54:17 +0200

    keybase,keybase-gui: add filalex77 to maintainers

--- pkgs/tools/security/keybase/default.nix
+++ pkgs/tools/security/keybase/default.nix
@@ -35,7 +35,7 @@ buildGoPackage rec {
     homepage = https://www.keybase.io/;
     description = "The Keybase official command-line utility and service.";
     platforms = platforms.linux ++ platforms.darwin;
-    maintainers = with maintainers; [ carlsverre np rvolosatovs ];
+    maintainers = with maintainers; [ carlsverre np rvolosatovs filalex77 ];
     license = licenses.bsd3;
   };
 }
