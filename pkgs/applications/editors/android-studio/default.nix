b916b9e8842904150aea388bdd85d2004f8f2004
Michael Weiss <dev.primeos@gmail.com>
2019-12-14 20:48:27 +0100

    androidStudioPackages.{dev,canary}: 4.0.0.5 -> 4.0.0.6

--- pkgs/applications/editors/android-studio/default.nix
+++ pkgs/applications/editors/android-studio/default.nix
@@ -18,9 +18,9 @@ let
     sha256Hash = "0qlrdf7a6f5585mrni1aa2chic4n7b9c8lgrj8br6q929hc2f5d9";
   };
   latestVersion = { # canary & dev
-    version = "4.0.0.5"; # "Android Studio 4.0 Canary 5"
-    build = "193.6039983";
-    sha256Hash = "19pidwl46z7alc0d7awhvi4aq1r87f99wh5yfi94s1zd2azm9f9z";
+    version = "4.0.0.6"; # "Android Studio 4.0 Canary 6"
+    build = "193.6052267";
+    sha256Hash = "1naxyfnrj7milqha7xbwbcvyi81a7fqb7jsm03hhq5xs2sw55m1c";
   };
 in {
   # Attributes are named by their corresponding release channels
