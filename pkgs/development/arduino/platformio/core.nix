2f525d624c4cedbaf917fc22f273c622d766651f
Justin Charette <charetjc@gmail.com>
2019-12-15 15:11:05 -0500

    platformio: 4.0.3 -> 4.1.0
    - Install marshmallow 2.20.5 with internal package.  PlatformIO is
      working on upgrading to marshmallow 3.x.  Nixpkgs only supplies
      marshmallow 3.2.2.
    - Ignore tests/commands/test_check.py because it depends on internet
      access.
    - Add missing python dependencies for PlatformIO.
    - Fix schema tests by letting Nix download the SPDX license file.
    - Update patch for python search path.
    
    Signed-off-by: Justin Charette <charetjc@gmail.com>

--- pkgs/development/arduino/platformio/core.nix
+++ pkgs/development/arduino/platformio/core.nix
@@ -1,8 +1,10 @@
 { stdenv, lib, buildPythonApplication, fetchFromGitHub
 , bottle, click, colorama
 , lockfile, pyserial, requests
-, pytest, semantic-version, tox, tabulate
-, git
+, pytest, semantic-version, tox, tabulate, pyelftools
+, jsondiff, git
+# Marshmallow deps
+, buildPythonPackage, fetchPypi, dateutil, simplejson
 }:
 
 let
@@ -39,26 +41,57 @@ let
     "test_pkgmanifest.py::test_packages"
   ]) ++ (map (e: "--ignore=tests/${e}") [
     "commands/test_boards.py"
+    "commands/test_check.py"
     "commands/test_platform.py"
     "commands/test_update.py"
     "test_maintenance.py"
     "test_ino2cpp.py"
   ]));
 
+  # Waiting for marshmallow 3.0 support - https://github.com/platformio/platformio-core/pull/3296
+  marshmallow = buildPythonPackage rec {
+    pname = "marshmallow";
+    version = "2.20.5";
+
+    meta = {
+      homepage = "https://github.com/marshmallow-code/marshmallow";
+      description = ''
+        A lightweight library for converting complex objects to and from
+        simple Python datatypes.
+      '';
+      license = lib.licenses.mit;
+    };
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "19yb2936ay2fc9aby4lyzscipf9gd9lk0zwjy7wm3b5j84pqj87f";
+    };
+
+    propagatedBuildInputs = [ dateutil simplejson ];
+
+    doCheck = false;
+  };
+
+  spdx = builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/spdx/license-list-data/v3.6/json/licenses.json";
+    sha256 = "1knwbfhd1gxr0b9cbxizajvnhr67yfmaq6ds3ajvsjmyqp7bk0cl";
+  };
+
 in buildPythonApplication rec {
   pname = "platformio";
-  version = "4.0.3";
+  version = "4.1.0";
 
   # pypi tarballs don't contain tests - https://github.com/platformio/platformio-core/issues/1964
   src = fetchFromGitHub {
     owner = "platformio";
     repo = "platformio-core";
     rev = "v${version}";
-    sha256 = "1naaa53cc7n7zyqggqjvvgkcq8cyzngdf904y9ag0x1vvb70f8j9";
+    sha256 = "10v9jw1zjfqr3wl6kills3cfp0ky7xbm1gc3z0n57wrqbc6cmz95";
   };
 
   propagatedBuildInputs =  [
-    bottle click colorama git lockfile
+    bottle click colorama git jsondiff
+    lockfile marshmallow pyelftools
     pyserial requests semantic-version
     tabulate
   ];
@@ -70,12 +103,16 @@ in buildPythonApplication rec {
   checkPhase = ''
     runHook preCheck
 
+    export SPDX_LICENSE_FILE="${spdx}"
     py.test -v tests ${args}
 
     runHook postCheck
   '';
 
-  patches = [ ./fix-searchpath.patch ];
+  patches = [
+    ./fix-schema-tests.patch
+    ./fix-searchpath.patch
+  ];
 
   meta = with stdenv.lib; {
     broken = stdenv.isAarch64;
