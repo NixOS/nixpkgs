3b3d392034e5490561ed5b5ce64a8e414b452dac
Jonathan Ringer <jonringer117@gmail.com>
2019-12-15 18:56:06 -0800

    python3Packages.mypy: 0.740 -> 0.750
    Also refactored expression to follow ordering of most other python packages

--- pkgs/development/python-modules/mypy/default.nix
+++ pkgs/development/python-modules/mypy/default.nix
@@ -5,19 +5,26 @@
 
 buildPythonPackage rec {
   pname = "mypy";
-  version = "0.740";
+  version = "0.750";
+  disabled = !isPy3k;
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "0k0l74g3jcq7ppzn234sffsaacn6qaq242famckk0cviwgld1jvf";
+  };
+
+  propagatedBuildInputs = [ typed-ast psutil mypy-extensions typing-extensions ];
 
   # Tests not included in pip package.
   doCheck = false;
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "48c8bc99380575deb39f5d3400ebb6a8a1cb5cc669bbba4d3bb30f904e0a0e7d";
-  };
-
-  disabled = !isPy3k;
-
-  propagatedBuildInputs = [ typed-ast psutil mypy-extensions typing-extensions ];
+  pythonImportsCheck = [
+    "mypy"
+    "mypy.types"
+    "mypy.api"
+    "mypy.fastparse"
+    "mypy.report"
+  ];
 
   meta = with stdenv.lib; {
     description = "Optional static typing for Python";
