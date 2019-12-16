5a62aac6ebf3d7cf4711eaa378d6de4dab240519
Silvan Mosberger <contact@infinisil.com>
2019-12-16 16:20:55 +0100

    invidious: 0.19.1 -> 0.20.1

--- pkgs/servers/invidious/shards.nix
+++ pkgs/servers/invidious/shards.nix
@@ -2,8 +2,8 @@
   db = {
     owner = "crystal-lang";
     repo = "crystal-db";
-    rev = "dcd0af5ce81b5a43305e02846106633373f65004";
-    sha256 = "1gf6ylzcb8ywpy6m7w7d49xnvn6q636xb86f3vh292s1kml3vm99";
+    rev = "v0.7.0";
+    sha256 = "0is08qrz5wc09j052jsj67zwmg362xv4k2wl353hn7lzim3s517j";
   };
   exception_page = {
     owner = "crystal-loot";
@@ -14,8 +14,8 @@
   kemal = {
     owner = "kemalcr";
     repo = "kemal";
-    rev = "v0.26.0";
-    sha256 = "0ja5q87djz43fdwc6wgvghgd0qly9pliwjcw4gidb8vwvjmn8hsx";
+    rev = "v0.26.1";
+    sha256 = "169pwkjmk7x6j8i0rf5rpyk1y0hl7jaf9h6yrq4ha2ag9yq9i8fr";
   };
   kilt = {
     owner = "jeromegn";
@@ -23,11 +23,29 @@
     rev = "v0.4.0";
     sha256 = "1w9ib6j4xhwxdxx58nzc06gw7ci7vga03vgj1z1bnklamb0b0l0k";
   };
+  lsquic = {
+    owner = "omarroth";
+    repo = "lsquic.cr";
+    rev = "v0.1.8";
+    sha256 = "1i999mxc8wvdmqdn8lz8gfz8i3mkcl1j869zy2gwrssl4fmjf70b";
+  };
   pg = {
     owner = "will";
     repo = "crystal-pg";
-    rev = "v0.18.1";
-    sha256 = "0q2ckgwmyh03cd13lw0p2h7cyqdrggml39f3hw0hx2h4a7wjl9mg";
+    rev = "v0.19.0";
+    sha256 = "19w7saa30761jxflr7r3h93al7iidlp7mw39myd0kv65bwc4gv0v";
+  };
+  pool = {
+    owner = "ysbaddaden";
+    repo = "pool";
+    rev = "v0.2.3";
+    sha256 = "00wxadhl6v82yyjl28kzmr9kv1yq6msv9dqhbsqdrdq0gg2hnvrs";
+  };
+  protodec = {
+    owner = "omarroth";
+    repo = "protodec";
+    rev = "v0.1.2";
+    sha256 = "1z7651ymjbvxrc6bgpxjbslm0agzd0pwvx1x8rbic2qdmzhfs21r";
   };
   radix = {
     owner = "luislavena";
@@ -38,7 +56,7 @@
   sqlite3 = {
     owner = "crystal-lang";
     repo = "crystal-sqlite3";
-    rev = "v0.13.0";
-    sha256 = "0myd9vvz40wnwl5ij1n1sva63nnmdbfv6xka5fb4y7v77l512y7c";
+    rev = "v0.14.0";
+    sha256 = "0hmfya6zj2nhyqk910hj250p5dhiw52s7pm3ykk9vlbfs4wj74mi";
   };
 }
