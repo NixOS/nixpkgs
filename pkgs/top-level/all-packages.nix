2ada3b5d3e4d776fc1e5dcdc1c34a4b7bd884614
Justin Bedo <cu@cua0.org>
2019-12-17 14:26:28 +1100

    samblaster: init 0.1.24

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24919,6 +24919,8 @@ in
     mpi = true;
   });
 
+  samblaster = callPackage ../applications/science/biology/samblaster { };
+
   samtools = callPackage ../applications/science/biology/samtools { };
   samtools_0_1_19 = callPackage ../applications/science/biology/samtools/samtools_0_1_19.nix {
     stdenv = gccStdenv;
