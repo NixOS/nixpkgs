84e0fbfcac1876000b959773dc69a3e83d5c441d
Oleksii Filonenko <brightone@protonmail.com>
2019-12-17 14:32:59 +0200

    tuir: init at 1.28.03
    Co-authored-by: Nikolay Korotkiy <sikmir@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6823,6 +6823,8 @@ in
 
   ttylog = callPackage ../tools/misc/ttylog { };
 
+  tuir = callPackage ../applications/misc/tuir { };
+
   turses = callPackage ../applications/networking/instant-messengers/turses { };
 
   oysttyer = callPackage ../applications/networking/instant-messengers/oysttyer { };
