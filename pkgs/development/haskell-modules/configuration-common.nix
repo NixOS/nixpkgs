963e138ecd506db5bd735e4c472740839b1a4820
Manuel Bärenz <programming@manuelbaerenz.de>
2019-12-18 14:02:36 +0100

    WIP: Fix hie-bios test suite

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -1327,4 +1327,9 @@ self: super: {
   haskell-lsp_0_18_0_0 = super.haskell-lsp_0_18_0_0.override { haskell-lsp-types = self.haskell-lsp-types_0_18_0_0; };
   lsp-test_0_8_2_0 = (dontCheck super.lsp-test_0_8_2_0).override { haskell-lsp = self.haskell-lsp_0_18_0_0; };
 
+  # test suite needs cabal and stack executables
+  hie-bios = addTestDepends super.hie-bios [
+    pkgs.stack
+    pkgs.cabal-install
+  ];
 } // import ./configuration-tensorflow.nix {inherit pkgs haskellLib;} self super
