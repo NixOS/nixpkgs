583b1a3b89604e2c6c5a97d5f8947b51e4634060
Pamplemousse <xav.maso@gmail.com>
2019-12-18 15:16:49 -0700

    gobuster: init at 3.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24533,6 +24533,8 @@ in
     binutils-arm-embedded = pkgsCross.arm-embedded.buildPackages.binutils;
   };
 
+  gobuster = callPackage ../tools/security/gobuster { };
+
   guetzli = callPackage ../applications/graphics/guetzli { };
 
   gummi = callPackage ../applications/misc/gummi { };
