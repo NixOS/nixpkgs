e095d22de7ec992a3b6024e6299dd4ebe0051ec1
Mario Rodas <marsam@users.noreply.github.com>
2019-12-18 14:00:00 -0500

    nodejs-13_x: 13.3.0 -> 13.5.0
    Changelog:
    - https://github.com/nodejs/node/releases/tag/v13.4.0
    - https://github.com/nodejs/node/releases/tag/v13.5.0

--- pkgs/development/web/nodejs/v13.nix
+++ pkgs/development/web/nodejs/v13.nix
@@ -5,8 +5,8 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "13.3.0";
-    sha256 = "0j36jf0ybq470w91kzg28bcmxlml7ccl4swwklb6x1ibkz697np7";
+    version = "13.5.0";
+    sha256 = "1ng959fm8ls222mmn2vpkw4n4jba02qigpxc8p85jxfj36dsq4ak";
 
     patches = stdenv.lib.optionals stdenv.isDarwin [ ./disable-libatomic-darwin.patch ];
   }
