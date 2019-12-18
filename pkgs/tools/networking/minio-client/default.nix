f100260efb1dc5a1801c80678e74b2722e662f68
Oleksii Filonenko <brightone@protonmail.com>
2019-12-18 18:07:51 +0200

    WIP: minio-client: 2019-10-12 -> 2019-12-17

--- pkgs/tools/networking/minio-client/default.nix
+++ pkgs/tools/networking/minio-client/default.nix
@@ -1,24 +1,26 @@
-{ stdenv, buildGoPackage, fetchFromGitHub }:
+{ stdenv, buildGoModule, fetchFromGitHub }:
 
-buildGoPackage rec {
+buildGoModule rec {
   pname = "minio-client";
-  version = "2019-01-30T19-57-22Z";
+  version = "2019-12-17T23-26-28Z";
 
   src = fetchFromGitHub {
     owner = "minio";
     repo = "mc";
     rev = "RELEASE.${version}";
-    sha256 = "1w0ig0daf0zxpkz449xq2hm7ajhzn8hlnnmpac6ip82qy53xnbm4";
+    sha256 = "1ck32p9k8fm1jnwb3zhq7h02s8wfzq46fhfgi8qmlmxfx02g1azw";
   };
 
   goPackagePath = "github.com/minio/mc";
+
+  modSha256 = "0c3rx2wxszhhw68hnmvq7702bzrwris60gf450gmyyiyflq6a7ab";
 
   preBuild = ''
     buildFlagsArray+=("-ldflags=-X github.com/minio/mc/cmd.Version=${version}")
   '';
 
   meta = with stdenv.lib; {
-    homepage = https://github.com/minio/mc;
+    homepage = "https://github.com/minio/mc";
     description = "A replacement for ls, cp, mkdir, diff and rsync commands for filesystems and object storage";
     maintainers = with maintainers; [ eelco bachp ];
     platforms = platforms.unix;
