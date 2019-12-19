661ac37284c465fd29cb388bf5ceea41614804dc
Frederik Rietdijk <fridh@fridh.nl>
2019-12-19 20:31:09 +0100

    python: distributed: 2.6.0 -> 2.9.0

--- pkgs/development/python-modules/distributed/default.nix
+++ pkgs/development/python-modules/distributed/default.nix
@@ -28,12 +28,12 @@
 
 buildPythonPackage rec {
   pname = "distributed";
-  version = "2.6.0";
+  version = "2.9.0";
 
   # get full repository need conftest.py to run tests
   src = fetchPypi {
     inherit pname version;
-    sha256 = "30b0ca195ace1e39bdd278bf1ad257f7674b3e2b8e7a2a37ce7e2ade4aecccf3";
+    sha256 = "b66a15d6c02c28dd07e815d09cd77dab8484004533d38895714a5f10c7e7e325";
   };
 
   checkInputs = [ pytest pytest-repeat pytest-faulthandler pytest-timeout mock joblib ];
