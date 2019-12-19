20ab906528d4483533ed48feaa9d34f6c7effd56
Frederik Rietdijk <fridh@fridh.nl>
2019-12-19 20:31:22 +0100

    python: responses: 0.10.7 -> 0.10.8

--- pkgs/development/python-modules/responses/default.nix
+++ pkgs/development/python-modules/responses/default.nix
@@ -3,11 +3,11 @@
 
 buildPythonPackage rec {
   pname = "responses";
-  version = "0.10.7";
+  version = "0.10.8";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "46d4e546a19fc6106bc7e804edd4551ef04690405e41e7e750ebc295d042623b";
+    sha256 = "caa5f7afd14666f970ea54a4125a639f6491218b45a013c6dc2544f0473ae2b8";
   };
 
   propagatedBuildInputs = [ cookies mock requests six ];
