71fc8af9e9f5566c9d35991dea0259d212f0779c
David Wood <david@davidtw.co>
2019-12-19 12:14:02 +0000

    mdcat: 0.13.0 -> 0.14.0
    Signed-off-by: David Wood <david.wood@codeplay.com>

--- pkgs/tools/text/mdcat/default.nix
+++ pkgs/tools/text/mdcat/default.nix
@@ -2,24 +2,26 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "mdcat";
-  version = "0.13.0";
+  version = "0.14.0";
 
   src = fetchFromGitHub {
     owner = "lunaryorn";
     repo = pname;
     rev = "mdcat-${version}";
-    sha256 = "0xlcpyfmil7sszv4008v4ipqswz49as4nzac0kzmzsb86np191q0";
+    sha256 = "1q8h6pc1i89j1zl4s234inl9v95vsdrry1fzlis89sl2mnbv8ywy";
   };
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ (stdenv.lib.optional stdenv.isDarwin Security) openssl ];
 
-  cargoSha256 = "16q17gm59lpjqa18q289cjmjlf2jicag12jz529x5kh11x6bjl8v";
+  cargoSha256 = "1hxsfls6fpllc9yg5ib3qz6pa62j1y1va8a6356j6812csk4ifnn";
 
   checkInputs = [ ansi2html ];
   checkPhase = ''
-    # Skip tests that use the network.
-    cargo test -- --skip terminal::iterm2
+    # Skip tests that use the network and that include files.
+    cargo test -- --skip terminal::iterm2 --skip terminal::resources::tests::remote \
+      --skip magic::tests::detect_mimetype_of_svg_image \
+      --skip magic::tests::detect_mimetype_of_png_image
   '';
 
   meta = with stdenv.lib; {
