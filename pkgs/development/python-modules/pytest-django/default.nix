c3a57ecb9df742e1383f9d59a1357898ed447680
Frederik Rietdijk <fridh@fridh.nl>
2019-12-19 20:31:20 +0100

    python: pytest-django: 3.6.0 -> 3.7.0

--- pkgs/development/python-modules/pytest-django/default.nix
+++ pkgs/development/python-modules/pytest-django/default.nix
@@ -10,11 +10,11 @@
 }:
 buildPythonPackage rec {
   pname = "pytest-django";
-  version = "3.6.0";
+  version = "3.7.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b6c900461a6a7c450dcf11736cabc289a90f5d6f28ef74c46e32e86ffd16a4bd";
+    sha256 = "17592f06d51c2ef4b7a0fb24aa32c8b6998506a03c8439606cb96db160106659";
   };
 
   nativeBuildInputs = [ pytest setuptools_scm ];
