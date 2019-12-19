7f3271a8164d7d7d7321919c7e9f23e2b321c234
Martin Potier <martin.potier@marvid.fr>
2019-12-19 10:19:17 +0200

    saml2aws: 2.15.0 -> 2.19.0
    Current version is broken, at least for use with Okta and AWS. New
    version fixes this problem.
    
    Packaging has changed a bit too, deps.nix was removed, and the top-level
    `buildGoModule` is used insead, since the upstream project now exposes a
    go.mod file.

--- pkgs/tools/security/saml2aws/default.nix
+++ pkgs/tools/security/saml2aws/default.nix
@@ -1,27 +1,27 @@
-{ stdenv, buildGoPackage, fetchFromGitHub }:
+{ stdenv, buildGoModule, fetchFromGitHub }:
 
-buildGoPackage rec {
-  name = "saml2aws-${version}";
+buildGoModule rec {
   pname = "saml2aws";
-  version = "2.15.0";
+  version = "2.19.0";
 
-  goPackagePath = "github.com/versent/saml2aws";
-  goDeps = ./deps.nix;
+  src = fetchFromGitHub {
+    owner = "Versent";
+    repo = "saml2aws";
+    rev = "v${version}";
+    sha256 = "1b34v52nvn5mgkxj90lmw0hhpqb35gi3dn47x3v7255890wkzryz";
+  };
+
+  modSha256 = "1ndr8npjjaxg97azshpca7mclxhbqm49wzswmv571y5x519bb7q8";
+
+  subPackages = [ "." "cmd/saml2aws" ];
 
   buildFlagsArray = ''
     -ldflags=-X main.Version=${version}
   '';
 
-  src = fetchFromGitHub {
-    rev = "v${version}";
-    owner = "Versent";
-    repo = "saml2aws";
-    sha256 = "0pn4zdzisgan7vvgi7hp8716wsb2x33gq55c7fw1aa2qwy0bq3gp";
-  };
-
   meta = with stdenv.lib; {
     description = "CLI tool which enables you to login and retrieve AWS temporary credentials using a SAML IDP";
-    homepage    = https://github.com/Versent/saml2aws;
+    homepage    = "https://github.com/Versent/saml2aws";
     license     = licenses.mit;
     platforms   = stdenv.lib.platforms.unix;
     maintainers = [ stdenv.lib.maintainers.pmyjavec ];
