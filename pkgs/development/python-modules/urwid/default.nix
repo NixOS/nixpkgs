d7bd4e4506f9ff1dadcca714fa57ab22543bbc1e
Jonathan Ringer <jonringer117@gmail.com>
2019-12-20 08:29:35 -0800

    pythonPackages.urwid: enable glibclocales for tests

--- pkgs/development/python-modules/urwid/default.nix
+++ pkgs/development/python-modules/urwid/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildPythonPackage, fetchPypi }:
+{ stdenv, buildPythonPackage, fetchPypi, glibcLocales }:
 
 buildPythonPackage rec {
   pname = "urwid";
@@ -8,6 +8,10 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "0896f36060beb6bf3801cb554303fef336a79661401797551ba106d23ab4cd86";
   };
+
+  # tests need to be able to set locale
+  LC_ALL = "en_US.UTF-8";
+  checkInputs = [ glibcLocales ];
 
   meta = with stdenv.lib; {
     description = "A full-featured console (xterm et al.) user interface library";
