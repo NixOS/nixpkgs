37f8757985753b44a7564c725e2142637424923a
Merlin Göttlinger <megoettlinger@gmail.com>
2019-12-20 11:18:21 +0100

    Forgot one reference

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -1343,7 +1343,7 @@ self: super: {
   spacecookie = super.spacecookie.override { systemd = self.systemd_2_2_0; };
 
   # ghcide needs the latest versions of haskell-lsp.
-  ghcide = super.ghcide.override { haskell-lsp = self.haskell-lsp_0_18_0_0; lsp-test = self.lsp-test_0_8_2_0; };
+  ghcide = super.ghcide.override { haskell-lsp = self.haskell-lsp_0_19_0_0; lsp-test = self.lsp-test_0_9_0_0; };
   haskell-lsp_0_19_0_0 = super.haskell-lsp_0_19_0_0.override { haskell-lsp-types = self.haskell-lsp-types_0_19_0_0; };
   lsp-test_0_9_0_0 = (dontCheck super.lsp-test_0_9_0_0).override { haskell-lsp = self.haskell-lsp_0_19_0_0; };
 
