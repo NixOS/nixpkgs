38675a6135bf875d2a1a57af1ea6531261a9d981
Markus Kowalewski <markus.kowalewski@gmail.com>
2019-12-21 14:31:21 +0100

    php: 7.3.11 -> 7.3.13

--- pkgs/development/interpreters/php/default.nix
+++ pkgs/development/interpreters/php/default.nix
@@ -262,8 +262,8 @@ in {
   };
 
   php73 = generic {
-    version = "7.3.11";
-    sha256 = "1rxm256vhnvyabfwmyv51sqrkjlid1g8lczcy4skc2f72d5zzlcj";
+    version = "7.3.13";
+    sha256 = "1g376b9caw21mw8738z354llbzb3shzgc60m7naw7wql5038jysw";
 
     # https://bugs.php.net/bug.php?id=76826
     extraPatches = optional stdenv.isDarwin ./php73-darwin-isfinite.patch;
