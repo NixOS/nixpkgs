82edff82077449b2e361c770215216e282519326
Jonathan Ringer <jonringer117@gmail.com>
2019-12-21 14:34:51 -0800

    python3Packages.pytest-testmon: only run tests which don't import unittest_mixins

--- pkgs/development/python-modules/pytest-testmon/default.nix
+++ pkgs/development/python-modules/pytest-testmon/default.nix
@@ -14,14 +14,15 @@ buildPythonPackage rec {
     sha256 = "b823b03faf5778d1e15fb9f52e104df4da9c1021daeb313b339fccbbfb8dbd5f";
   };
 
-  buildInputs = [ pytest ];
-
   propagatedBuildInputs = [ coverage ];
 
   checkInputs = [ pytest ];
 
+  # avoid tests which try to import unittest_mixins
+  # unittest_mixins doesn't seem to be very active
   checkPhase = ''
-    pytest --deselect=test/test_testmon.py::TestmonDeselect::test_dependent_testmodule
+    cd test
+    pytest test_{core,process_code,pytest_assumptions}.py
   '';
 
   meta = with lib; {
