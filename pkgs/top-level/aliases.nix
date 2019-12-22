0529b27d180bc3a1666fa0e7fe236f18d2f147ba
José Romildo Malaquias <malaquias@gmail.com>
2019-12-22 16:03:12 -0300

    catfish: move to pkgs/desktops/xfce/applications
    Since release 1.4.6 catfish is an Xfce Project. See https://bluesabre.org/2018/07/15/catfish-1-4-6-released-now-an-xfce-project
    
    Makes an alias for catfish.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -61,6 +61,7 @@ mapAliases ({
   buildPerlPackage = perlPackages.buildPerlPackage; # added 2018-10-12
   bundler_HEAD = bundler; # added 2015-11-15
   cantarell_fonts = cantarell-fonts; # added 2018-03-03
+  catfish = xfce.catfish; # added 2019-12-22
   checkbashism = checkbashisms; # added 2016-08-16
   cide = throw "deprecated in 2019-09-11: abandoned by upstream";
   cinepaint = throw "cinepaint has been removed from nixpkgs, as it was unmaintained"; # added 2019-12-10
