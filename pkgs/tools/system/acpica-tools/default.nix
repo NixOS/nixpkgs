d1e1b8465f2fd4011efbb06985a457ebd53cd3f2
R. RyanTM <ryantm-bot@ryantm.com>
2019-12-22 17:36:39 -0800

    acpica-tools: 20191018 -> 20191213

--- pkgs/tools/system/acpica-tools/default.nix
+++ pkgs/tools/system/acpica-tools/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "acpica-tools";
-  version = "20191018";
+  version = "20191213";
 
   src = fetchurl {
     url = "https://acpica.org/sites/acpica/files/acpica-unix-${version}.tar.gz";
-    sha256 = "0pz95fb1zvsj9238bg7a4vxl1svn5mnjg10sn5qvgr008q0v9782";
+    sha256 = "1ip684is3dplf7snkn024vv6bg3dv5msx8v7pz6x9lrnk3gk0j9h";
   };
 
   NIX_CFLAGS_COMPILE = "-O3";
