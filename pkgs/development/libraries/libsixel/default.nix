36b5802a8beb975be4d39afcd760b24722db502d
R. RyanTM <ryantm-bot@ryantm.com>
2019-12-23 15:37:30 -0800

    libsixel: 1.8.2 -> 1.8.4

--- pkgs/development/libraries/libsixel/default.nix
+++ pkgs/development/libraries/libsixel/default.nix
@@ -1,13 +1,13 @@
 {stdenv, fetchFromGitHub}:
 stdenv.mkDerivation rec {
-  version = "1.8.2";
+  version = "1.8.4";
   pname = "libsixel";
 
   src = fetchFromGitHub {
     repo = "libsixel";
     rev = "v${version}";
     owner = "saitoha";
-    sha256 = "1jn5z2ylccjkp9i12n5x53x2zzhhsgmgs6xxi7aja6qimfw90h1n";
+    sha256 = "1zckahfl0j7k68jf87iwdc4yx7fkfhxwa7lrf22dnz36d2iq785v";
   };
 
   meta = with stdenv.lib; {
