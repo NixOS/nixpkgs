5bd1df5c6074c9d3e4d4136dea853b35f8390596
R. RyanTM <ryantm-bot@ryantm.com>
2019-12-23 07:04:31 -0800

    jython: 2.7.1 -> 2.7.2b2

--- pkgs/development/interpreters/jython/default.nix
+++ pkgs/development/interpreters/jython/default.nix
@@ -3,11 +3,11 @@
 stdenv.mkDerivation rec {
   pname = "jython";
 
-  version = "2.7.1";
+  version = "2.7.2b2";
 
   src = fetchurl {
     url = "http://search.maven.org/remotecontent?filepath=org/python/jython-standalone/${version}/jython-standalone-${version}.jar";
-    sha256 = "0jwc4ly75cna78blnisv4q8nfcn5s0g4wk7jf4d16j0rfcd0shf4";
+    sha256 = "0mmrrydr94q2siwjynkw1gw677navmcvjvbi1jpdbp6idfx0jh6b";
   };
 
   buildInputs = [ makeWrapper ];
