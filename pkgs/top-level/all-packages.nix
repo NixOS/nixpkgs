88ec0a05e1bbce17940dd5ced8b45c9559f0a519
Jan Tojnar <jtojnar@gmail.com>
2019-12-24 16:01:01 +0100

    pangox_compat: drop

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11671,8 +11671,6 @@ in
     inherit (darwin.apple_sdk.frameworks) ApplicationServices;
   };
 
-  pangox_compat = callPackage ../development/libraries/pangox-compat { };
-
   gdata-sharp = callPackage ../development/libraries/gdata-sharp { };
 
   gdk-pixbuf = callPackage ../development/libraries/gdk-pixbuf { };
