ec1dc42420b1640007a2a877c122cbbb23226a45
Jonathan Ringer <jonringer117@gmail.com>
2019-12-24 02:07:26 -0800

    python35Packages.ipython: python35 not supported

--- pkgs/development/python-modules/ipython/default.nix
+++ pkgs/development/python-modules/ipython/default.nix
@@ -23,7 +23,7 @@
 buildPythonPackage rec {
   pname = "ipython";
   version = "7.10.2";
-  disabled = pythonOlder "3.5";
+  disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
