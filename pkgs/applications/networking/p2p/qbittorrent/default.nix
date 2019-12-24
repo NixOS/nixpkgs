ace1ae9d11ec1eb0f543f1162accc5a6fb4b7a00
R. RyanTM <ryantm-bot@ryantm.com>
2019-12-24 03:27:14 -0800

    qbittorrent: 4.2.0 -> 4.2.1

--- pkgs/applications/networking/p2p/qbittorrent/default.nix
+++ pkgs/applications/networking/p2p/qbittorrent/default.nix
@@ -10,13 +10,13 @@ with lib;
 
 mkDerivation rec {
   pname = "qbittorrent";
-  version = "4.2.0";
+  version = "4.2.1";
 
   src = fetchFromGitHub {
     owner = "qbittorrent";
     repo = "qbittorrent";
     rev = "release-${version}";
-    sha256 = "17vm6aa2k8k1q14z9r2r06c794bcr4m0l0fdsn08wid6mj1zjsbx";
+    sha256 = "0bz4l7awkx4qf3gh9c8gj8fab989439zj8qy4x9r36wxdjg5cxil";
   };
 
   # NOTE: 2018-05-31: CMake is working but it is not officially supported
