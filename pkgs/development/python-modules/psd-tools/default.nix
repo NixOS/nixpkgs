53ec7111d8ddd1c15766b70eb0d9464f51c34388
Jonathan Ringer <jonringer117@gmail.com>
2019-12-24 01:46:14 -0800

    pythonPackages.psd-tools: mark broken
    missing packbits from nixpkgs

--- pkgs/development/python-modules/psd-tools/default.nix
+++ pkgs/development/python-modules/psd-tools/default.nix
@@ -1,21 +1,27 @@
-{ lib, buildPythonPackage, fetchPypi,
-  docopt, pillow
+{ lib, buildPythonPackage, fetchPypi, isPy27
+, docopt
+, pillow
+, enum34
 }:
 
 buildPythonPackage rec {
   pname = "psd-tools";
   version = "1.8.34";
 
-  meta = {
-    description = "Python package for reading Adobe Photoshop PSD files";
-    homepage = https://github.com/kmike/psd-tools;
-    license = lib.licenses.mit;
-  };
-
   src = fetchPypi {
     inherit pname version;
     sha256 = "db471c867399f7a9cc31f9e83b1e6734411c44b26dcecaaefe04c1141f606707";
   };
 
-  propagatedBuildInputs = [ docopt pillow ];
+  propagatedBuildInputs = [
+    docopt
+    pillow
+  ] ++ lib.optionals isPy27 [ enum34 ];
+
+  meta = {
+    description = "Python package for reading Adobe Photoshop PSD files";
+    homepage = https://github.com/kmike/psd-tools;
+    license = lib.licenses.mit;
+    broken = true; # missing packbits from nixpkgs
+  };
 }
