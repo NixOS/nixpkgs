69ed29f5f41107aca5fb674218a4f75619c22207
Markus Kowalewski <markus.kowalewski@gmail.com>
2019-12-25 16:40:06 +0100

    mediawiki: 1.33.1 -> 1.33.2

--- pkgs/servers/web-apps/mediawiki/default.nix
+++ pkgs/servers/web-apps/mediawiki/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "mediawiki";
-  version = "1.33.1";
+  version = "1.33.2";
 
   src = with stdenv.lib; fetchurl {
     url = "https://releases.wikimedia.org/mediawiki/${versions.majorMinor version}/${pname}-${version}.tar.gz";
-    sha256 = "19x10vsgg2fhcpmfvdswwilqwa65byrsmzvhk2v8bsxzhp6s95vx";
+    sha256 = "01n2lqafyvp0ybv4bc3wfk7fb90qmhvpafd9yg6wh1i2j9p8iqqm";
   };
 
   prePatch = ''
