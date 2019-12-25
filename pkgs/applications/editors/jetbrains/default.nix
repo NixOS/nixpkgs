b2d769dc8adb9cd39805b56f098b7a7404130c11
kolaente <k@knt.li>
2019-12-25 11:11:54 +0100

    Jetbrains IDEA Ultimate 2019.3 -> 2019.3.1

--- pkgs/applications/editors/jetbrains/default.nix
+++ pkgs/applications/editors/jetbrains/default.nix
@@ -302,12 +302,12 @@ in
 
   idea-ultimate = buildIdea rec {
     name = "idea-ultimate-${version}";
-    version = "2019.3"; /* updated by script */
+    version = "2019.3.1"; /* updated by script */
     description = "Integrated Development Environment (IDE) by Jetbrains, requires paid license";
     license = stdenv.lib.licenses.unfree;
     src = fetchurl {
       url = "https://download.jetbrains.com/idea/ideaIU-${version}-no-jbr.tar.gz";
-      sha256 = "14x7wn732i95k6rrq3035wjgv7zx5pgjzrd37b0siwvnhbxxhhn6"; /* updated by script */
+      sha256 = "0rb726nh2c7zxnpjcf6fyrpl29y9wgr6qhpb6hjxia2gzxab9jz0"; /* updated by script */
     };
     wmClass = "jetbrains-idea";
     update-channel = "IntelliJ IDEA RELEASE";
