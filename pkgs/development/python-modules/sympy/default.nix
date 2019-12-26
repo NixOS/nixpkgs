8ac53ed0b17aaa55c37adf51814e262e8ce83d00
Timo Kaufmann <timokau@zoho.com>
2019-12-26 11:46:42 +0100

    python.pkgs.sympy: 1.4 -> 1.5
    Sage needs some doctest changes. The patch was included upstream.
    Modelled after https://trac.sagemath.org/ticket/28911.

--- pkgs/development/python-modules/sympy/default.nix
+++ pkgs/development/python-modules/sympy/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "sympy";
-  version = "1.4"; # Upgrades may break sage. Please test or ping @timokau.
+  version = "1.5"; # Upgrades may break sage. Please test or ping @timokau.
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1q937csy8rd18pk2fz1ssj7jyj7l3rjx4nzbiz5vcymfhrb1x8bi";
+    sha256 = "1s4w7q8gndim2ky825a84jmwcf7ryfn10wm8yxz9dw5z2307smii";
   };
 
   checkInputs = [ glibcLocales ];
@@ -21,14 +21,6 @@ buildPythonPackage rec {
 
   # tests take ~1h
   doCheck = false;
-
-  patches = [
-    # to be fixed by https://github.com/sympy/sympy/pull/13476
-    (fetchpatch {
-      url = "https://git.sagemath.org/sage.git/plain/build/pkgs/sympy/patches/03_undeffun_sage.patch?id=3277ba76d0ba7174608a31a0c6623e9210c63e3d";
-      sha256 = "0xcp1qafvqnivvvi0byh51mbgqicjhmswwvqvamjz9rxfzm5f7d7";
-    })
-  ];
 
   preCheck = ''
     export LANG="en_US.UTF-8"
