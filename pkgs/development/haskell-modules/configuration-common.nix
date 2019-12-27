4d403a3a51663f30efd478c8f35f3c5b49c6bc37
Peter Simons <simons@cryp.to>
2019-12-27 20:21:09 +0100

    haskell-ghcide: drop broken overrides
    We made an effort to support ghcide in Nixpkgs, but the complexity of the
    problem is a bit too high, IMHO. We need to keep older versions of several
    packages around in order to satisfy the build requirements, and some of those
    older packages don't even build themselves (like hie-bios). We had ghcide
    working at some point, but then it was broken again right away after a couple
    of days. I fear that we'll run into that issue again and again with a setup of
    that complexity.
    
    Instead, I'd propose that we work with upstream to fix their build, i.e. let's
    make sure that the proper ghcide build works with recent versions of its build
    inputs.
    
    Closes https://github.com/NixOS/nixpkgs/pull/75449.
    Closes https://github.com/NixOS/nixpkgs/pull/76103.

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -1336,11 +1336,6 @@ self: super: {
   # needs newer version of the systemd package
   spacecookie = super.spacecookie.override { systemd = self.systemd_2_2_0; };
 
-  # ghcide needs the latest versions of haskell-lsp.
-  ghcide = super.ghcide.override { haskell-lsp = self.haskell-lsp_0_18_0_0; lsp-test = self.lsp-test_0_9_0_0; };
-  haskell-lsp_0_18_0_0 = super.haskell-lsp_0_18_0_0.override { haskell-lsp-types = self.haskell-lsp-types_0_18_0_0; };
-  lsp-test_0_9_0_0 = (dontCheck super.lsp-test_0_9_0_0).override { haskell-lsp = self.haskell-lsp_0_18_0_0; };
-
   # 2019-12-19 - glirc wants regex-tdfa >=1.3 which results in errors with regex-base which errors more
   # hoping to make a proper derivation with plugins enabled and more reliable building -- kiwi
   glirc = doJailbreak super.glirc;
