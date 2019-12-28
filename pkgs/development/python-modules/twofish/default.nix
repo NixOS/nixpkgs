61bd1cee00851dfe60361954c85c7c0f424b58da
Kevin Amado <kamadorueda@gmail.com>
2019-12-28 03:57:38 -0500

    pythonPackages.twofish: init at 0.3.0

---
+++ pkgs/development/python-modules/twofish/default.nix
@@ -0,0 +1,32 @@
+{ buildPythonPackage
+, fetchPypi
+, lib
+
+# Python Dependencies
+, pythonPackages
+}:
+
+buildPythonPackage rec {
+  pname = "twofish";
+  version = "0.3.0";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1yihp2n42amrxw0wk9f66llpb3w5kwhgkcdg9krkzcik1nsqp7dh";
+  };
+
+  propagatedBuildInputs = with pythonPackages; [
+    javaobj-py3
+  ];
+
+  doCheck = false;
+
+  meta = {
+    description = "Bindings for the Twofish implementation by Niels Ferguson";
+    homepage = "https://github.com/keybase/python-twofish";
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [
+      kamadorueda
+    ];
+  };
+}
