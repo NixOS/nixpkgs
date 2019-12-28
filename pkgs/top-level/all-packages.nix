650a6354bb48532b08af1854be6f4f803dfd736e
Mikołaj Gałkowski <mikolaj.galkowski@gmail.com>
2019-12-28 19:16:18 +0100

    unityhub: init at 2.2.2 (#65391)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25267,6 +25267,8 @@ in
     inherit (gnome2) GConf;
   };
 
+  unityhub = callPackage ../development/tools/unityhub { };
+
   urbit = callPackage ../misc/urbit { };
 
   utf8proc = callPackage ../development/libraries/utf8proc { };
