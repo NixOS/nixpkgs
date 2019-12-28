e2c8033df4bbdfcb628690e5651e4dc368a7e5e5
Oren Rozen <countoren@gmail.com>
2019-12-28 16:40:13 -0500

    vsode-utils: extracting attributes to limit input range

--- pkgs/misc/vscode-extensions/mktplcExtRefToFetchArgs.nix
+++ pkgs/misc/vscode-extensions/mktplcExtRefToFetchArgs.nix
@@ -1,8 +1,8 @@
-ext: 
+{ publisher, name, version, sha256 ? "" }:
 {
-  url = "https://${ext.publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/${ext.publisher}/extension/${ext.name}/${ext.version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage";
-  sha256 = ext.sha256;
+  url = "https://${publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/${publisher}/extension/${name}/${version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage";
+  sha256 = sha256;
   # The `*.vsix` file is in the end a simple zip file. Change the extension
   # so that existing `unzip` hooks takes care of the unpacking.
-  name = "${ext.publisher}-${ext.name}.zip";
+  name = "${publisher}-${name}.zip";
 }
