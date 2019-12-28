3d6b2cb6367a3548a87435b5616179314cd7b956
Kevin Amado <kamadorueda@gmail.com>
2019-12-28 04:12:01 -0500

    pythonPackages.mixpanel: 4.0.2 -> 4.5.0
    - It now works on python2 and python3

--- pkgs/development/python-modules/mixpanel/default.nix
+++ pkgs/development/python-modules/mixpanel/default.nix
@@ -1,30 +1,30 @@
 { stdenv
 , buildPythonPackage
-, fetchzip
-, pytest
-, mock
+, fetchPypi
+
+# Python Dependencies
 , six
-, isPy3k
 }:
 
 buildPythonPackage rec {
-  version = "4.0.2";
   pname = "mixpanel";
-  disabled = isPy3k;
+  version = "4.5.0";
 
-  src = fetchzip {
-    url = "https://github.com/mixpanel/mixpanel-python/archive/${version}.zip";
-    sha256 = "0yq1bcsjzsz7yz4rp69izsdn47rvkld4wki2xmapp8gg2s9i8709";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "0p446i5y70mm23qlqjnhayksxpwymji334brcv3vw3rrr6xmwfdb";
   };
 
-  checkInputs = [ pytest mock ];
   propagatedBuildInputs = [ six ];
-  checkPhase = "py.test tests.py";
+  doCheck = false;
 
   meta = with stdenv.lib; {
     homepage = https://github.com/mixpanel/mixpanel-python;
     description = ''This is the official Mixpanel Python library'';
     license = licenses.asl20;
+    maintainers = with maintainers; [
+      kamadorueda
+    ];
   };
 
 }
