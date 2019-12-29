f6e327b50c964d22d0f2321b4d2e95d2d28df3cf
Frederik Rietdijk <fridh@fridh.nl>
2019-12-29 10:28:08 +0100

    python: phonopy: 2.4.1.post5 -> 2.4.2

--- pkgs/development/python-modules/phonopy/default.nix
+++ pkgs/development/python-modules/phonopy/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "phonopy";
-  version = "2.4.1.post5";
+  version = "2.4.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "8a393ed79a600fe6371677890198a2d05ad27c37935aa83977f5244dfe3eac96";
+    sha256 = "6e6ce41ce8a51723b94d974adfee032cddce5b9300984dd23b59e101ed0a2861";
   };
 
   propagatedBuildInputs = [ numpy pyyaml matplotlib h5py ];
