16a6c127324c470592585e8f95746106f433e0e4
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-12-29 13:06:34 -0500

    python3Packages.hstspreload: init at 2019.12.25

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2616,6 +2616,8 @@ in {
 
   hsaudiotag3k = callPackage ../development/python-modules/hsaudiotag3k { };
 
+  hstspreload = callPackage ../development/python-modules/hstspreload { };
+
   htmlmin = callPackage ../development/python-modules/htmlmin {};
 
   httpauth = callPackage ../development/python-modules/httpauth { };
