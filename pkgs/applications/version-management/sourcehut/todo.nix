0282a909f74ccc7abb9ff7c07a8836d6ab47b8bb
Edmund Wu <fangkazuto@gmail.com>
2019-12-29 19:41:15 -0500

    todosrht: 0.51.13 -> 0.55.3

--- pkgs/applications/version-management/sourcehut/todo.nix
+++ pkgs/applications/version-management/sourcehut/todo.nix
@@ -5,12 +5,12 @@
 
 buildPythonPackage rec {
   pname = "todosrht";
-  version = "0.51.13";
+  version = "0.55.3";
 
   src = fetchgit {
     url = "https://git.sr.ht/~sircmpwn/todo.sr.ht";
     rev = version;
-    sha256 = "19gywq5j7wlpk7j2whm2ivz0z0i3j50n7k7bx29pghndl7l43c18";
+    sha256 = "1j82yxdnag0q6rp0rpiq3ccn5maa1k58j2f1ilcsxf1gr13pycf5";
   };
 
   patches = [
