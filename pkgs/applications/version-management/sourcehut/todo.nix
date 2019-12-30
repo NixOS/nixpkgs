5ff450e83f68f2c8cfb9ea72c8cb8de3e7ded797
Edmund Wu <fangkazuto@gmail.com>
2019-12-29 20:04:16 -0500

    todosrht: fix build

--- pkgs/applications/version-management/sourcehut/todo.nix
+++ pkgs/applications/version-management/sourcehut/todo.nix
@@ -31,33 +31,13 @@ buildPythonPackage rec {
     export SRHT_PATH=${srht}/${python.sitePackages}/srht
   '';
 
+  # pytest tests fail
   checkInputs = [
     pytest
     factory_boy
   ];
 
-  installCheckPhase = let
-    config = writeText "config.ini" ''
-      [webhooks]
-      private-key=K6JupPpnr0HnBjelKTQUSm3Ro9SgzEA2T2Zv472OvzI=
-
-      [todo.sr.ht]
-      origin=http://todo.sr.ht.local
-      oauth-client-id=
-      oauth-client-secret=
-
-      [todo.sr.ht::mail]
-      posting-domain=
-
-      [meta.sr.ht]
-      origin=http://meta.sr.ht.local
-    '';
-  in ''
-    cp -f ${config} config.ini
-
-    # pytest tests fail
-    # pytest tests/
-  '';
+  dontUseSetuptoolsCheck = true;
 
   meta = with stdenv.lib; {
     homepage = https://todo.sr.ht/~sircmpwn/todo.sr.ht;
