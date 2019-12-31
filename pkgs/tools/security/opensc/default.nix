bda2cfe8c07025b4bba8f8ccafa23ff33301dec0
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2019-12-30 20:50:56 -0500

    opensc: fix CVE-2019-19479, CVE-2019-19480

--- pkgs/tools/security/opensc/default.nix
+++ pkgs/tools/security/opensc/default.nix
@@ -32,6 +32,26 @@ stdenv.mkDerivation rec {
       url = "https://github.com/OpenSC/OpenSC/commit/a3fc7693f3a035a8a7921cffb98432944bb42740.patch";
       sha256 = "1qr9n8cbarrdn4kr5z0ys7flq50hfmcbm8584mhw7r39p08qwmvq";
     })
+    (fetchpatch {
+      name = "CVE-2019-19479.patch";
+      url = "https://github.com/OpenSC/OpenSC/commit/c3f23b836e5a1766c36617fe1da30d22f7b63de2.patch";
+      sha256 = "0xj6c6wycwynxrn3f8kziwz99z8rjwkrsnk8f0ffx7fj4d97krfw";
+    })
+    # needed for CVE-2019-19480.patch
+    (fetchpatch {
+      name = "opensc-memory-leak-fix.patch";
+      url = "https://github.com/OpenSC/OpenSC/commit/630d6adf32cecaab0ee184618f56497bd50400fb.patch";
+      sha256 = "0cb6fx9pjf7z9zdi2kk1b3zz537xb7dclaqn8z0gy48xg59biwy0";
+    })
+    (fetchpatch {
+      name = "CVE-2019-19480.patch";
+      url = "https://github.com/OpenSC/OpenSC/commit/6ce6152284c47ba9b1d4fe8ff9d2e6a3f5ee02c7.patch";
+      sha256 = "0xm6gl11b4nr436a8al0k8myq6i2xpc9mc24nwf4fhwj3wbzp3dq";
+    })
+
+    # CVE-2019-19481 is on applicable to 0.19
+    # cac1 support was only added after it:
+    # https://github.com/OpenSC/OpenSC/commit/e2b1fb81e0e1339eebaa36fb90635e03f69d4da3#diff-6d5e82180f0ee8125f09f8bedb5cd1cb
   ];
 
   nativeBuildInputs = [ pkgconfig autoreconfHook ];
