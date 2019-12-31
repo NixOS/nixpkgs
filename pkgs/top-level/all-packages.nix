7efeca1721994573434eda026eabd57435092c74
K. Bruneau <kira.bruneau@gmail.com>
2019-12-31 17:28:49 -0500

    texlab: 1.7.0 -> 1.9.0 (#76730)
    This commit also removes texlab-citeproc since it was replaced with
    citeproc-rs and is no longer available.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10585,7 +10585,6 @@ in
 
   texlab = callPackage ../development/tools/misc/texlab {
     inherit (darwin.apple_sdk.frameworks) Security;
-    inherit (nodePackages) texlab-citeproc-build-deps;
   };
 
   tflint = callPackage ../development/tools/analysis/tflint { };
