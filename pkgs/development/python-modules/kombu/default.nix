020d878a30ec33c1ded1f6fcbe95f7b5a840bf42
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-01-01 12:48:17 -0500

    pythonPackages.kombu: refactor format and add fakeredis for complete tests

--- pkgs/development/python-modules/kombu/default.nix
+++ pkgs/development/python-modules/kombu/default.nix
@@ -1,39 +1,47 @@
-{ lib, buildPythonPackage, fetchPypi
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
 , amqp
 , case
 , Pyro4
-, pytest
 , pytz
 , sqlalchemy
+, pytest
+, pytest-sugar
+, fakeredis
 }:
 
 buildPythonPackage rec {
   pname = "kombu";
   version = "4.6.7";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "67b32ccb6fea030f8799f8fd50dd08e03a4b99464ebc4952d71d8747b1a52ad1";
+  src = fetchFromGitHub {
+    owner = "celery";
+    repo = "kombu";
+    rev = version;
+    sha256 = "1ra9dr590bpjjbj22rx4shwaclcyr261lzbw06axzdgnr1cqcbmx";
   };
 
-  postPatch = ''
-    substituteInPlace requirements/test.txt \
-      --replace "pytest-sugar" ""
-    substituteInPlace requirements/default.txt \
-      --replace "amqp==2.5.1" "amqp~=2.5"
-  '';
+  propagatedBuildInputs = [
+    amqp
+  ];
 
-  propagatedBuildInputs = [ amqp ];
+  checkInputs = [
+    pytest
+    case
+    pytz
+    Pyro4
+    sqlalchemy
+    fakeredis
+  ];
 
-  checkInputs = [ pytest case pytz Pyro4 sqlalchemy ];
-  # test_redis requires fakeredis, which isn't trivial to package
   checkPhase = ''
-    pytest --ignore t/unit/transport/test_redis.py
+    pytest
   '';
 
   meta = with lib; {
     description = "Messaging library for Python";
-    homepage    = https://github.com/celery/kombu;
-    license     = licenses.bsd3;
+    homepage = https://github.com/celery/kombu;
+    license = licenses.bsd3;
   };
 }
