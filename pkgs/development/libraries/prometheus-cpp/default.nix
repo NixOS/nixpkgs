207a2d78435566fb4a724626f3609c82e15898e1
Franz Pletz <fpletz@fnordicwalking.de>
2020-01-01 20:45:11 +0100

    prometheus-cpp: build shared libraries

--- pkgs/development/libraries/prometheus-cpp/default.nix
+++ pkgs/development/libraries/prometheus-cpp/default.nix
@@ -28,6 +28,7 @@ stdenv.mkDerivation rec {
     "-DUSE_THIRDPARTY_LIBRARIES=OFF"
     "-DCIVETWEB_INCLUDE_DIR=${civetweb.dev}/include"
     "-DCIVETWEB_CXX_LIBRARY=${civetweb}/lib/libcivetweb${stdenv.targetPlatform.extensions.sharedLibrary}"
+    "-DBUILD_SHARED_LIBS=ON"
   ];
 
   NIX_LDFLAGS = [ "-ldl" ];
