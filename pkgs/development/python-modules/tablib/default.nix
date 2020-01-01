83c05917f07fa42e6a1e5e1c88c0c16dc686828f
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-01-01 14:18:25 -0500

    pythonPackages.tablib: 0.12.1 -> 0.14.0 enable tests remove patches

--- pkgs/development/python-modules/tablib/default.nix
+++ pkgs/development/python-modules/tablib/default.nix
@@ -1,27 +1,52 @@
-{ buildPythonPackage, stdenv, fetchPypi, pytest, unicodecsv, pandas
-, xlwt, openpyxl, pyyaml, xlrd, odfpy, fetchpatch
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, pytest
+, pytestcov
+, unicodecsv
+, pandas
+, xlwt
+, openpyxl
+, pyyaml
+, xlrd
+, odfpy
+, markuppy
 }:
 
 buildPythonPackage rec {
   pname = "tablib";
-  version = "0.12.1";
+  version = "0.14.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "11wxchj0qz77dn79yiq30k4b4gsm429f4bizk4lm4rb63nk51kxq";
+  src = fetchFromGitHub {
+    owner = "jazzband";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "1i50nbr7wxk1m6nh8bx0icci2hr9xc9sqi2ypj3ykkvl66sii2z7";
   };
 
-  checkInputs = [ pytest unicodecsv pandas ];
-  propagatedBuildInputs = [ xlwt openpyxl pyyaml xlrd odfpy ];
-
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/kennethreitz/tablib/commit/0e51a2d0944022af186d2dcd34c0ab3c47141ba5.patch";
-      sha256 = "0lbbl871zdn5vpgqyjkil0c2ap3b5hz19rmihhyvrx7m4mlh1aij";
-    })
+  checkInputs = [
+    pytest
+    pytestcov
+    unicodecsv
+    pandas
   ];
 
-  meta = with stdenv.lib; {
+  propagatedBuildInputs = [
+    xlwt
+    openpyxl
+    pyyaml
+    xlrd
+    odfpy
+    markuppy
+  ];
+
+  SETUPTOOLS_SCM_PRETEND_VERSION = version;
+
+  checkPhase = ''
+    pytest
+  '';
+
+  meta = with lib; {
     description = "Tablib: format-agnostic tabular dataset library";
     homepage = http://python-tablib.org;
     license = licenses.mit;
