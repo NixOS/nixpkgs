41d1ac24ba2874f213b331bb60673b3f26c47e47
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-01-01 10:22:15 -0500

    pythonPackages.JayDeBeApi: refactor formatting and metadata changes

--- pkgs/development/python-modules/JayDeBeApi/default.nix
+++ pkgs/development/python-modules/JayDeBeApi/default.nix
@@ -1,16 +1,25 @@
-{ lib, buildPythonPackage, fetchPypi, JPype1 }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, JPype1
+}:
 
 buildPythonPackage rec {
   pname = "JayDeBeApi";
   version = "1.1.1";
+
   src = fetchPypi {
     inherit pname version;
     sha256 = "0a189xs9zw81jvwwglvf2qyqnk6ra0biljssx9n4ffayqn9glbds";
   };
-  propagatedBuildInputs = [ JPype1 ];
-  meta = {
-    homepage = "https://github.com/baztian/jaydebeapi";
-    license = lib.licenses.lgpl2;
-    description = "Use JDBC database drivers from Python 2/3 or Jython with a DB-API.";
+
+  propagatedBuildInputs = [
+    JPype1
+  ];
+
+  meta = with lib; {
+    homepage = https://github.com/baztian/jaydebeapi;
+    license = licenses.lgpl2;
+    description = "Use JDBC database drivers from Python 2/3 or Jython with a DB-API";
   };
 }
