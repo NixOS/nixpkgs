362bb16b407d3743029762c30fb7db8fe1a1ef65
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-01-01 14:15:59 -0500

    pythonPackages.openpyxl: 3.0.2 -> 3.0.1 [1] and enable tests use bitbucket src
    [1] https://bitbucket.org/openpyxl/openpyxl/issues/1373/broken-writer-with-lxml-defusedxml

--- pkgs/development/python-modules/openpyxl/default.nix
+++ pkgs/development/python-modules/openpyxl/default.nix
@@ -1,38 +1,43 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromBitbucket
 , isPy27
 , pytest
 , jdcal
 , et_xmlfile
 , lxml
+, pillow
 }:
 
 buildPythonPackage rec {
   pname = "openpyxl";
-  version = "3.0.2";
+  version = "3.0.1";
   disabled = isPy27; # 2.6.4 was final python2 release
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "eb68c08a72ac6d9812df181e88ebece3a68436364353eb6eda3bed863e3d73a6";
+  src = fetchFromBitbucket {
+    owner = "openpyxl";
+    repo = pname;
+    rev = version;
+    sha256 = "1svmnzby6sfdv1qkb1va8z8krysrcdzfjibnyzvl8jgpydcvi8v5";
   };
 
-  checkInputs = [ pytest ];
-  propagatedBuildInputs = [ jdcal et_xmlfile lxml ];
+  checkInputs = [
+    pytest
+    pillow
+  ];
 
-  postPatch = ''
-    # LICENSE.rst is missing, and setup.cfg currently doesn't contain anything useful anyway
-    # This should likely be removed in the next update
-    rm setup.cfg
+  propagatedBuildInputs = [
+    jdcal
+    et_xmlfile
+    lxml
+  ];
+
+  checkPhase = ''
+    pytest openpyxl
   '';
 
-  # Tests are not included in archive.
-  # https://bitbucket.org/openpyxl/openpyxl/issues/610
-  doCheck = false;
-
   meta = {
-    description = "A Python library to read/write Excel 2007 xlsx/xlsm files";
+    description = "A Python library to read/write Excel 2010 xlsx/xlsm/xltx/xltm files";
     homepage = https://openpyxl.readthedocs.org;
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ lihop sjourdois ];
