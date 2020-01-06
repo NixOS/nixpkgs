d706d39f586bfe09355d4b5182105d54bbf8ac73
R. RyanTM <ryantm-bot@ryantm.com>
2020-01-06 00:57:30 -0800

    davfs2: 1.5.5 -> 1.5.6

--- pkgs/tools/filesystems/davfs2/default.nix
+++ pkgs/tools/filesystems/davfs2/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, neon, zlib }:
 
 stdenv.mkDerivation rec {
-  name = "davfs2-1.5.5";
+  name = "davfs2-1.5.6";
 
   src = fetchurl {
     url = "mirror://savannah/davfs2/${name}.tar.gz";
-    sha256 = "0bxd62268pix7w1lg7f9y94v34f4l45fdf6clyarj43qmljnlz2q";
+    sha256 = "00fqadhmhi2bmdar5a48nicmjcagnmaj9wgsvjr6cffmrz6pcx21";
   };
 
   buildInputs = [ neon zlib ];
