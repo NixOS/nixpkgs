a7e2742b50b6be3cbad84ba82b14ed477c574104
Maximilian Bosch <maximilian@mbosch.me>
2020-01-06 21:33:20 +0100

    wasm-bindgen-cli: 0.2.56 -> 0.2.57
    https://github.com/rustwasm/wasm-bindgen/releases/tag/0.2.57

--- pkgs/development/tools/wasm-bindgen-cli/default.nix
+++ pkgs/development/tools/wasm-bindgen-cli/default.nix
@@ -2,19 +2,19 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "wasm-bindgen-cli";
-  version = "0.2.56";
+  version = "0.2.57";
 
   src = fetchFromGitHub {
     owner = "rustwasm";
     repo = "wasm-bindgen";
     rev = version;
-    sha256 = "0fmwcv192zyplxp26r9amb4zf7qf8150rd6k39sdi60lxp1dx76p";
+    sha256 = "0vh0h4vjpxzbpi4asan41yjy5a0gcvj1qzyaz81bh1ssfb1immi8";
   };
 
   buildInputs = [ openssl ] ++ lib.optionals stdenv.isDarwin [ Security curl ];
   nativeBuildInputs = [ pkgconfig ];
 
-  cargoSha256 = "0xn6sa1gc62gqf8zfn9b4zs2g58diixflxdvflygk5y55bp3rjcy";
+  cargoSha256 = "006wij92x0h8fazkgic3b0kampvb24sflc45qlfiz51ygjcgigma";
   cargoPatches = [ ./0001-Add-cargo.lock.patch ];
   cargoBuildFlags = [ "-p" pname ];
 
