14bbfd8548d28528db2f89823024ef85011eb2ba
davidak <git@davidak.de>
2020-01-06 05:52:37 +0100

    anarchism: init at 15.3-1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17163,6 +17163,8 @@ in
 
   amiri = callPackage ../data/fonts/amiri { };
 
+  anarchism = callPackage ../data/documentation/anarchism { };
+
   andagii = callPackage ../data/fonts/andagii { };
 
   andika = callPackage ../data/fonts/andika { };
