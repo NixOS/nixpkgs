06f9d0b944f6969a1fe2ff3a9b72c38373ddd970
R. RyanTM <ryantm-bot@ryantm.com>
2020-01-06 02:48:31 -0800

    firejail: 0.9.60 -> 0.9.62

--- pkgs/os-specific/linux/firejail/default.nix
+++ pkgs/os-specific/linux/firejail/default.nix
@@ -3,10 +3,10 @@ let
   s = # Generated upstream information
   rec {
     baseName="firejail";
-    version="0.9.60";
+    version="0.9.62";
     name="${baseName}-${version}";
     url="mirror://sourceforge/firejail/firejail/firejail-${version}.tar.xz";
-    sha256="1mw2s040wbxaj4lqfm6033wxrxlnix40mqc2bsfran5sj2gxd3sh";
+    sha256="1q2silgy882fl61p5qa9f9jqkxcqnwa71jig3c729iahx4f0hs05";
   };
   buildInputs = [
     which
