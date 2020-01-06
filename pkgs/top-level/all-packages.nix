80f043b738f20e30d87ac7438a9e2d6c5e734d04
Will Dietz <w@wdtz.org>
2020-01-06 15:37:40 -0600

    samurai: init at 1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10476,6 +10476,8 @@ in
 
   rr = callPackage ../development/tools/analysis/rr { };
 
+  samurai = callPackage ../development/tools/build-managers/samurai { };
+
   saleae-logic = callPackage ../development/tools/misc/saleae-logic { };
 
   sauce-connect = callPackage ../development/tools/sauce-connect { };
