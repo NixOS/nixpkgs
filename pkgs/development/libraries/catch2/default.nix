9d5dbf4bc1289a17a57f8925d548b98861312260
R. RyanTM <ryantm-bot@ryantm.com>
2020-01-06 00:42:19 -0800

    catch2: 2.11.0 -> 2.11.1

--- pkgs/development/libraries/catch2/default.nix
+++ pkgs/development/libraries/catch2/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "catch2";
-  version = "2.11.0";
+  version = "2.11.1";
 
   src = fetchFromGitHub {
     owner = "catchorg";
     repo = "Catch2";
     rev = "v${version}";
-    sha256="1b07drrclvxj17s67ivypr9vr27rg0m36jqnrbci6f4wsp1b0gbl";
+    sha256="1jijj72cas6k5hckkxqppbv1inlwbca37p0ixzs3vzm2jhnd7z31";
   };
 
   nativeBuildInputs = [ cmake ];
