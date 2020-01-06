96d1740d6b274201caeaad7f8d082156153b83be
R. RyanTM <ryantm-bot@ryantm.com>
2020-01-06 08:22:36 -0800

    jgmenu: 3.5 -> 4.0

--- pkgs/applications/misc/jgmenu/default.nix
+++ pkgs/applications/misc/jgmenu/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "jgmenu";
-  version = "3.5";
+  version = "4.0";
 
   src = fetchFromGitHub {
     owner = "johanmalm";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0q0m3sskgmjv28gzvjkphgg3yhwzc9w9fj9i342pibb50impjazy";
+    sha256 = "1s9291y38k4adc2wqj7plfhj431nf36zs262jm6mmb2fs910ncgv";
   };
 
   nativeBuildInputs = [
