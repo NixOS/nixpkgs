1fcb085aace1dcfcaf6ccdbaac9633dcf8748385
R. RyanTM <ryantm-bot@ryantm.com>
2020-01-06 03:06:23 -0800

    faudio: 19.12 -> 20.01

--- pkgs/development/libraries/faudio/default.nix
+++ pkgs/development/libraries/faudio/default.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation rec {
 	pname = "faudio";
-  version = "19.12";
+  version = "20.01";
 
   src = fetchFromGitHub {
     owner = "FNA-XNA";
     repo = "FAudio";
     rev = version;
-    sha256 = "0y8dc7lnhh69wcwqma9spyxcahfhbfyg92h35sqkin6qfh5mngxr";
+    sha256 = "0gqwma3r216xgymjnagm6ndxfvdigzl46nlny4z085sgvydx3n8m";
   };
 
 	nativeBuildInputs = [cmake];
