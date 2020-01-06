e4e051052d269495b1336ddb17e07c4861673b4e
Andrew J. Hesford <ajh@sideband.org>
2020-01-05 21:18:50 -0500

    pam_gnupg: init at fbd75b7 nixos/pam: add support for new pam_gnupg package in pam configs

---
+++ pkgs/os-specific/linux/pam_gnupg/default.nix
@@ -0,0 +1,28 @@
+{ stdenv, fetchgit, autoreconfHook, gnupg, pam } :
+
+stdenv.mkDerivation rec {
+  name = "pam_gnupg-fbd75b7";
+  src = fetchgit {
+    url = https://github.com/cruegge/pam-gnupg;
+    rev = "fbd75b720877e4cf94e852ce7e2b811feb330bb5";
+    sha256 = "0kqn6xb85jfmhvvbd2lasnci46p2pcwy0wq233za9h7xwfr49f7d";
+  };
+
+  nativeBuildInputs = [ autoreconfHook ];
+  buildInputs = [ gnupg pam ];
+
+  preAutoreconf = ''
+    mkdir m4
+  '';
+
+  configurePhase = ''
+    ./configure --prefix=$out --with-moduledir=$out/lib/security
+  '';
+
+  meta = with stdenv.lib; {
+    description = "A PAM plugin to preset GPG passphrases on login";
+    homepage = "https://github.com/cruegge/pam-gnupg/";
+    license = licenses.gpl3;
+    platforms = platforms.linux;
+  };
+}
