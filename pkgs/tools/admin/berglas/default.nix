0320725e862fbd95c414e95fb48b2a7e91734d61
Simonas Kazlauskas <git@kazlauskas.me>
2020-01-07 14:20:10 +0200

    berglas: 0.2.1 -> 0.5.0

--- pkgs/tools/admin/berglas/default.nix
+++ pkgs/tools/admin/berglas/default.nix
@@ -3,16 +3,16 @@
 buildGoModule rec {
 
   name = "berglas-${version}";
-  version = "0.2.1";
+  version = "0.5.0";
 
   src = fetchFromGitHub {
     owner = "GoogleCloudPlatform";
     repo = "berglas";
-    rev = "v0.2.1";
-    sha256 = "1m34rxiynmgsris1avjn7am50b8sds77515zlnna9qvsrywbzljc";
+    rev = "v0.5.0";
+    sha256 = "1y5w2czipwj069w4zxnyb9xqv5mx0yjjramykf3vm3q478bk3rm7";
   };
 
-  modSha256 = "0lfcrsb4r5hxxd652cxff23fnbrphp3lgwp5anpaddzcjcd2qyj8";
+  modSha256 = "0y4ajii3pv25s4gjazf6fl0b9wax17cmwhbmiybqhp61annca7kr";
 
   meta = with stdenv.lib; {
     description = "A tool for managing secrets on Google Cloud";
