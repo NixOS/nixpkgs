229a1e3795514ce42f4cbc2cf48f3caa8d76061d
R. RyanTM <ryantm-bot@ryantm.com>
2020-01-07 01:44:14 -0800

    python27Packages.pyspf: 2.0.13 -> 2.0.14

--- pkgs/development/python-modules/pyspf/default.nix
+++ pkgs/development/python-modules/pyspf/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "pyspf";
-  version = "2.0.13"; # version 2.0.13 should not be used, see #72791
+  version = "2.0.14"; # version 2.0.14 should not be used, see #72791
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "62dc1be39519e343202d480da7ef93d834b5a50cd4f217bef9335ed15292929b";
+    sha256 = "11ki6ky5fk6kzh8ykd1xy1ryv1hn4l303bxnmwx1g450pl0yz9sp";
   };
 
   propagatedBuildInputs = [ pydns ];
