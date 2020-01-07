c44331dd5d5d54db20b941bb8c04dcf8f143648b
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2020-01-07 20:52:24 +0100

    nixosTests.initdb: port to python

--- nixos/tests/initdb.nix
+++ nixos/tests/initdb.nix
@@ -1,26 +1,19 @@
-let
-  pkgs = import <nixpkgs> { };
-in
-with import <nixpkgs/nixos/lib/testing.nix> { inherit pkgs; system = builtins.currentSystem; };
-with pkgs.lib;
+import ./make-test-python.nix ({ pkgs, latestKernel ? false, ... }: {
+  name = "pg-initdb";
 
-makeTest {
-    name = "pg-initdb";
+  machine = {...}:
+    {
+      documentation.enable = false;
+      services.postgresql.enable = true;
+      services.postgresql.package = pkgs.postgresql_9_6;
+      environment.pathsToLink = [
+        "/share/postgresql"
+      ];
+    };
 
-    machine = {...}:
-      {
-        documentation.enable = false;
-        services.postgresql.enable = true;
-        services.postgresql.package = pkgs.postgresql_9_6;
-        environment.pathsToLink = [
-          "/share/postgresql"
-        ];
-      };
-
-    testScript = ''
-      $machine->start;
-      $machine->succeed("sudo -u postgres initdb -D /tmp/testpostgres2");
-      $machine->shutdown;
-    '';
-
-  }
+  testScript = ''
+    machine.start()
+    machine.succeed("sudo -u postgres initdb -D /tmp/testpostgres2")
+    machine.shutdown()
+  '';
+})
