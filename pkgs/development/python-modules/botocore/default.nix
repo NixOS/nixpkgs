ab1707eb2ca021d5f99af475742ef57798477e66
Tim Steinbach <tim@nequissimus.com>
2020-01-07 14:38:04 -0500

    botocore: 1.13.2 -> 1.13.47

--- pkgs/development/python-modules/botocore/default.nix
+++ pkgs/development/python-modules/botocore/default.nix
@@ -12,11 +12,11 @@
 
 buildPythonPackage rec {
   pname = "botocore";
-  version = "1.13.2"; # N.B: if you change this, change boto3 and awscli to a matching version
+  version = "1.13.47"; # N.B: if you change this, change boto3 and awscli to a matching version
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "8223485841ef4731a5d4943a733295ba69d0005c4ae64c468308cc07f6960d39";
+    sha256 = "0whjfwd8d3l67b8hbnf666pcn4n8fdz3qajjwl68jn4mxk2pq655";
   };
 
   outputs = [ "out" "dev" ];
