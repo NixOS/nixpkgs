9404009d0c8f01eb89b0d38c4767f6252f6f3749
worldofpeace <worldofpeace@protonmail.ch>
2020-01-08 09:47:09 -0500

    release: drop nox for nixpkgs-review
    The comment in this file about why nox is here
    was "Needed by travis-ci to test PRs", and we
    for sure don't use travis-ci anymore.
    Instead we're making nixpkgs-review a deliverable
    because it's included in the PR template as a tool
    to use. We also swapped out nox for nixpkgs-review
    in the PR template, so I believe this makes sense
    here because it was changed.

--- pkgs/top-level/release.nix
+++ pkgs/top-level/release.nix
@@ -46,7 +46,7 @@ let
               jobs.go.x86_64-darwin
               jobs.mariadb.x86_64-darwin
               jobs.nix.x86_64-darwin
-              jobs.nox.x86_64-darwin
+              jobs.nixpkgs-review.x86_64-darwin
               jobs.nix-info.x86_64-darwin
               jobs.nix-info-tested.x86_64-darwin
               jobs.openssh.x86_64-darwin
@@ -97,8 +97,8 @@ let
               jobs.pandoc.x86_64-linux
               jobs.python.x86_64-linux
               jobs.python3.x86_64-linux
-              # Needed by travis-ci to test PRs
-              jobs.nox.x86_64-linux
+              # Needed by contributors to test PRs (by inclusion of the PR template)
+              jobs.nixpkgs-review.x86_64-linux
               # Needed for support
               jobs.nix-info.x86_64-linux
               jobs.nix-info-tested.x86_64-linux
@@ -134,7 +134,7 @@ let
               jobs.stdenv.x86_64-darwin
               jobs.python.x86_64-darwin
               jobs.python3.x86_64-darwin
-              jobs.nox.x86_64-darwin
+              jobs.nixpkgs-review.x86_64-darwin
               jobs.nix-info.x86_64-darwin
               jobs.nix-info-tested.x86_64-darwin
               jobs.git.x86_64-darwin
