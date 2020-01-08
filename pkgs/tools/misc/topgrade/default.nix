cbdffb082a379737b5e73677b15eeb31a00807f6
Oleksii Filonenko <brightone@protonmail.com>
2020-01-08 10:50:17 +0200

    topgrade: 3.7.0 -> 3.7.1

--- pkgs/tools/misc/topgrade/default.nix
+++ pkgs/tools/misc/topgrade/default.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "topgrade";
-  version = "3.7.0";
+  version = "3.7.1";
 
   src = fetchFromGitHub {
     owner = "r-darwish";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1lj4hwinw2q9hwcxjn052y3rrpw1ri90agx3m3j0dy741hv9grmm";
+    sha256 = "0vs0gnfs9swlmzsj7m3b88xfzcfy7n68bgm4i94csc3qsbip6m0j";
   };
 
   cargoSha256 = "1y85hl7xl60vsj3ivm6pyd6bvk39wqg25bqxfx00r9myha94iqmd";
