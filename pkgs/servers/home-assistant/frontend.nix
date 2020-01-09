f74fc27ace4eb6d7ce04960c11876337f0858e01
Jörg Thalheim <joerg@thalheim.io>
2020-01-09 10:43:43 +0000

    home-assistant-frontend: 20200108.0 -> 20191204.1

--- pkgs/servers/home-assistant/frontend.nix
+++ pkgs/servers/home-assistant/frontend.nix
@@ -4,11 +4,11 @@ buildPythonPackage rec {
   # the frontend version corresponding to a specific home-assistant version can be found here
   # https://github.com/home-assistant/home-assistant/blob/master/homeassistant/components/frontend/manifest.json
   pname = "home-assistant-frontend";
-  version = "20191204.1";
+  version = "20200108.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1agbc8laql9vb9f8qnha2zwah967avfyna0ckdyvvdd6sjwkc0a2";
+    sha256 = "1h6fgkx8fffzs829893gjbh0wbjgxjzz2ca64v8r5sb938bfayg8";
   };
 
   # no Python tests implemented
