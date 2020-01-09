e126dfaa3a062dfeec49a6c22968ce79079485ba
Matthieu Coudron <mattator@gmail.com>
2020-01-09 15:45:28 +0100

    isync: enable manpage and reformat

--- pkgs/tools/networking/isync/default.nix
+++ pkgs/tools/networking/isync/default.nix
@@ -1,14 +1,24 @@
-{ fetchurl, stdenv, openssl, pkgconfig, db, zlib, cyrus_sasl, perl }:
+{ fetchurl
+, stdenv
+, openssl
+, autoreconfHook
+, pkgconfig
+, db
+, zlib
+, cyrus_sasl
+, perl
+}:
 
 stdenv.mkDerivation rec {
-  name = "isync-1.3.1";
+  pname = "isync";
+  version = "1.3.1";
 
   src = fetchurl {
-    url = "mirror://sourceforge/isync/${name}.tar.gz";
+    url = "mirror://sourceforge/isync/${pname}-${version}.tar.gz";
     sha256 = "1sphd30jplii58y2zmw365bckm6pszmapcy905zhjll1sm1ldjv8";
   };
 
-  nativeBuildInputs = [ pkgconfig perl ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig perl ];
   buildInputs = [ openssl db cyrus_sasl zlib ];
 
   meta = with stdenv.lib; {
