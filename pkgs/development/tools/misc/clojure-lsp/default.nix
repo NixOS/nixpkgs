42a23966ed3c3d482e997adfbf7c13958b2a22cf
Eric Dallo <ericdallo06@hotmail.com>
2020-01-09 19:23:28 -0300

    clojure-lsp: 20200106T233511 -> 20200109T185134

--- pkgs/development/tools/misc/clojure-lsp/default.nix
+++ pkgs/development/tools/misc/clojure-lsp/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "clojure-lsp";
-  version = "20200106T233511";
+  version = "20200109T185134";
 
   src = fetchurl {
     url = "https://github.com/snoe/clojure-lsp/releases/download/release-${version}/${pname}";
-    sha256 = "0z550c15sywbaxbfi1nxx19whfysq4whl4na4fjihnin8ab5sh2x";
+    sha256 = "11fzyf2qzqmxhdyssm59cqkkcjh8hw1i859abc1i2zali3fd7w68";
   };
 
   dontUnpack = true;
