216dea5b9d03db2be2ee9f6631735e676aafb3d1
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2020-01-10 16:05:13 +0700

    qutebrowser: 1.8.3 -> 1.9.0
    Closes #77360

--- pkgs/applications/networking/browsers/qutebrowser/default.nix
+++ pkgs/applications/networking/browsers/qutebrowser/default.nix
@@ -21,12 +21,12 @@ let
 
 in mkDerivationWith python3Packages.buildPythonApplication rec {
   pname = "qutebrowser";
-  version = "1.8.3";
+  version = "1.9.0";
 
   # the release tarballs are different from the git checkout!
   src = fetchurl {
     url = "https://github.com/qutebrowser/qutebrowser/releases/download/v${version}/${pname}-${version}.tar.gz";
-    sha256 = "055zmzk3q0m3hx1742nfy2mdawfllrkvijnbzp1hiv01dj1bxaf8";
+    sha256 = "1y0yq1qfr6g1s7kf3w2crd0b025dv2dfknhlz3v0001ns3rgwj17";
   };
 
   # Needs tox
