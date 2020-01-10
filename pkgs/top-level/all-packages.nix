a8d1f3233bbd0862a643cc4806f8f9cc4edcbc04
Robert Helgesson <robert@rycee.net>
2020-01-10 15:44:09 +0100

    jwt-cli: init at 2.5.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1862,6 +1862,8 @@ in
 
   jotta-cli = callPackage ../applications/misc/jotta-cli { };
 
+  jwt-cli = callPackage ../tools/security/jwt-cli { };
+
   kapacitor = callPackage ../servers/monitoring/kapacitor { };
 
   kisslicer = callPackage ../tools/misc/kisslicer { };
