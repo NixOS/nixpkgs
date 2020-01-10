aa5ec18c331ceb305eb9108c13929b403eeb995c
Milan Pässler <mil@nyantec.com>
2020-01-10 05:25:59 +0100

    ma1sd: 2.1.1 -> 2.2.2, use gradle2nix

--- pkgs/servers/ma1sd/default.nix
+++ pkgs/servers/ma1sd/default.nix
@@ -1,57 +1,25 @@
-{ stdenv, fetchFromGitHub, jre, git, gradle_5, perl, makeWrapper }:
+{ stdenv, fetchFromGitHub, buildGradle, jre, makeWrapper }:
 
-let
-  name = "ma1sd-${version}";
-  version = "2.1.1";
-  rev = "a112a5e57cb38ad282939d2dcb9c1476e038af39";
+buildGradle rec {
+  pname = "ma1sd";
+  version = "2.2.2";
+  rev = "15db563e8dc7b692adf4d1c0cab9bfc3746b396e";
 
   src = fetchFromGitHub {
     inherit rev;
     owner = "ma1uta";
     repo = "ma1sd";
-    sha256 = "1qibn6m6mvxwnbiypxlgkaqg6in358vkf0q47410rv1dx1gjcnv5";
+    sha256 = "15g4pjp51y5ykkzwjzpfjbdm76h64sdc3xs6mhifymap3x34k3nr";
   };
 
-
-  deps = stdenv.mkDerivation {
-    name = "${name}-deps";
-    inherit src;
-    nativeBuildInputs = [ gradle_5 perl git ];
-
-    buildPhase = ''
-      export MA1SD_BUILD_VERSION=${rev}
-      export GRADLE_USER_HOME=$(mktemp -d);
-      gradle --no-daemon build -x test
-    '';
-
-    # perl code mavenizes pathes (com.squareup.okio/okio/1.13.0/a9283170b7305c8d92d25aff02a6ab7e45d06cbe/okio-1.13.0.jar -> com/squareup/okio/okio/1.13.0/okio-1.13.0.jar)
-    installPhase = ''
-      find $GRADLE_USER_HOME/caches/modules-2 -type f -regex '.*\.\(jar\|pom\)' \
-        | perl -pe 's#(.*/([^/]+)/([^/]+)/([^/]+)/[0-9a-f]{30,40}/([^/\s]+))$# ($x = $2) =~ tr|\.|/|; "install -Dm444 $1 \$out/$x/$3/$4/$5" #e' \
-        | sh
-    '';
-
-    dontStrip = true;
-
-    outputHashAlgo = "sha256";
-    outputHashMode = "recursive";
-    outputHash = "1w9cxq0rlzyh7bzqr3v3vn2cjhpn7hhc5lk9qzwj7sdj4jn2qxq6";
-  };
-
-in
-stdenv.mkDerivation {
-  inherit name src version;
-  nativeBuildInputs = [ gradle_5 perl makeWrapper ];
+  nativeBuildInputs = [ makeWrapper ];
   buildInputs = [ jre ];
+  envSpec = ./gradle-env.json;
+  gradleFlags = [ "shadowJar" ];
 
-  patches = [ ./0001-gradle.patch ];
-
-  buildPhase = ''
+  preBuild = ''
     export MA1SD_BUILD_VERSION=${rev}
     export GRADLE_USER_HOME=$(mktemp -d)
-
-    sed -ie "s#REPLACE#mavenLocal(); maven { url '${deps}' }#g" build.gradle
-    gradle --offline --no-daemon build -x test
   '';
 
   installPhase = ''
@@ -61,10 +29,9 @@ stdenv.mkDerivation {
 
   meta = with stdenv.lib; {
     description = "a federated matrix identity server; fork of mxisd";
-    homepage = https://github.com/ma1uta/ma1sd;
+    homepage = "https://github.com/ma1uta/ma1sd";
     license = licenses.agpl3;
     maintainers = with maintainers; [ mguentner ];
     platforms = platforms.all;
   };
-
 }
---
+++ pkgs/servers/ma1sd/update.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p common-updater-scripts jq curl gradle2nix
+
+OWNER=ma1uta
+REPO=ma1sd
+ATTR=ma1sd
+
+TAG=$(curl -s https://api.github.com/repos/$OWNER/$REPO/releases/latest | jq -r .tag_name)
+REV=$(curl -s https://api.github.com/repos/$OWNER/$REPO/git/ref/tags/$TAG | jq -r .object.sha)
+update-source-version $ATTR $REV --version-key=rev
+update-source-version $ATTR $(echo $TAG) --ignore-same-hash
+
+PKG_FILE=$(nix-instantiate --eval --strict -A $ATTR.meta.position | sed -re 's/^"(.*):[0-9]+"$/\1/')
+PKG_DIR=$(dirname $PKG_FILE)
+
+TMPDIR=$(mktemp -d)
+trap "rm -rf $TMPDIR;" EXIT
+cd $TMPDIR
+
+git clone https://github.com/$OWNER/$REPO
+cd $REPO
+git checkout $REV
+
+gradle2nix
+cp gradle-env.json $PKG_DIR/
