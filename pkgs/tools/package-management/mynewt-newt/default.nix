407f23673ff151e6baf10b32ec275f11877ac1e8
Tom Hall <tahall256@protonmail.ch>
2020-01-11 13:48:36 +0000

    mynewt-newt: 1.3.0 -> 1.7.0, use buildGoModule

--- pkgs/tools/package-management/mynewt-newt/default.nix
+++ pkgs/tools/package-management/mynewt-newt/default.nix
@@ -1,18 +1,17 @@
-{ stdenv, buildGoPackage, fetchFromGitHub }:
+{ stdenv, buildGoModule, fetchFromGitHub }:
 
-buildGoPackage rec {
+buildGoModule rec {
   pname = "mynewt-newt";
-  version = "1.3.0";
-
-  goPackagePath = "mynewt.apache.org/newt";
-  goDeps = ./deps.nix;
+  version = "1.7.0";
 
   src = fetchFromGitHub {
     owner = "apache";
-    repo = "incubator-mynewt-newt";
+    repo = "mynewt-newt";
     rev = "mynewt_${builtins.replaceStrings ["."] ["_"] version}_tag";
-    sha256 = "0ia6q1wf3ki2yw8ngw5gnbdrb7268qwi078j05f8gs1sppb3g563";
+    sha256 = "0rwn4ghh7kal8csxlh0w1p29b5m1nam9lkrxla5wdfhnzbsg8hfa";
   };
+
+  modSha256 = "068r8wa2pgd68jv50x0l1w8n96f97b3mgv7z6f85280ahgywaasq";
 
   meta = with stdenv.lib; {
     homepage = https://mynewt.apache.org/;
