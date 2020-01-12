eb0c48b1c20205f981c70462464dd11bd0668ac2
Tom Hall <tahall256@protonmail.ch>
2020-01-12 12:40:00 +0000

    mynewt-newt: patch to fix failure running upgrade on mcuboot

--- pkgs/tools/package-management/mynewt-newt/default.nix
+++ pkgs/tools/package-management/mynewt-newt/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildGoModule, fetchFromGitHub }:
+{ stdenv, buildGoModule, fetchFromGitHub, fetchpatch }:
 
 buildGoModule rec {
   pname = "mynewt-newt";
@@ -10,6 +10,17 @@ buildGoModule rec {
     rev = "mynewt_${builtins.replaceStrings ["."] ["_"] version}_tag";
     sha256 = "0rwn4ghh7kal8csxlh0w1p29b5m1nam9lkrxla5wdfhnzbsg8hfa";
   };
+
+  patches = [
+    (fetchpatch {
+      url = https://github.com/apache/mynewt-newt/commit/6a51e35565323ebe8feb8d1aa6e00960b6ce662e.patch;
+      sha256 = "186yha60jzcjq8r04w12rqqh3cin2w974l77hz2ixhmjzyr56wqv";
+    })
+    (fetchpatch {
+      url = https://github.com/apache/mynewt-newt/commit/7d4ef3fe65a9a83cc58e7bd973654ad235cc68bc.patch;
+      sha256 = "01scmq58bfr4c9icqzm79q7a55izflsb3mlx9xn0dv92m3mbprx7";
+    })
+  ];
 
   modSha256 = "068r8wa2pgd68jv50x0l1w8n96f97b3mgv7z6f85280ahgywaasq";
 
