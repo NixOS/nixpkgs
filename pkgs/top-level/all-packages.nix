36f084b2bbcbfba0da7605d80a731111b36084fc
Francesco Gazzetta <fgaz@fgaz.me>
2020-01-13 10:01:29 +0000

    mindustry: init at 102 (and updates) (#72306)
    * mindustry: init at 99
    
    * mindustry: 99 -> 100
    
    * mindustry: 100 -> 101
    
    * mindustry: 101 -> 101.1
    
    * mindustry: 101.1 -> 102

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23023,6 +23023,8 @@ in
 
   megaglest = callPackage ../games/megaglest {};
 
+  mindustry = callPackage ../games/mindustry { };
+
   minecraft = callPackage ../games/minecraft { };
 
   minecraft-server = callPackage ../games/minecraft-server { };
