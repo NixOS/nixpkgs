ff135a3243d81cecd7b818829cc7151ee9186fb3
Luka Blaskovic <lblasc@tvbeat.com>
2020-01-14 22:43:23 +0000

    oathToolkit: fix build with glibc>=2.28

--- pkgs/tools/security/oath-toolkit/default.nix
+++ pkgs/tools/security/oath-toolkit/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitLab, pam, xmlsec, autoreconfHook, pkgconfig, libxml2, gtk-doc, perl, gengetopt, bison, help2man }:
+{ stdenv, fetchFromGitLab, fetchpatch, pam, xmlsec, autoreconfHook, pkgconfig, libxml2, gtk-doc, perl, gengetopt, bison, help2man }:
 
 let
   securityDependency =
@@ -14,6 +14,15 @@ in stdenv.mkDerivation {
     rev = "0dffdec9c5af5c89a5af43add29d8275eefe7414";
     sha256 = "0n2sl444723f1k0sjmc0mzdwslx51yxac39c2cx2bl3ykacgfv74";
   };
+
+  patches = [
+    # fix for glibc>=2.28
+    (fetchpatch {
+      name   = "new_glibc_check.patch";
+      url    = "https://sources.debian.org/data/main/o/oath-toolkit/2.6.1-1.3/debian/patches/new-glibc-check.patch";
+      sha256 = "0h75xyy3xsl485v7w27yqkks6z9sgsjmrv6wiswy15fdj5wyciv3";
+    })
+  ];
 
   buildInputs = [ securityDependency libxml2 perl gengetopt bison ];
 
