ca17f0c86c1bee59477617cd9343885d19e3628e
ivar <ivar.scholten@protonmail.com>
2020-01-14 20:43:22 +0100

    Switch to autoPatchelfHook as asked

--- pkgs/misc/drivers/xp-pen-g430/default.nix
+++ pkgs/misc/drivers/xp-pen-g430/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchzip, patchelf, libusb1, libX11, libXtst, qt5, libglvnd }:
+{ stdenv, fetchzip, tar, autoPatchelfHook, libusb1, libX11, libXtst, qt5, libglvnd }:
 stdenv.mkDerivation rec {
   name = "xp-pen-G430";
   version = "20190820";
@@ -8,29 +8,27 @@ stdenv.mkDerivation rec {
     sha256 = "091kfqxxj90pdmwncgfl8ldi70pdhwryh3cls30654983m8cgnby";
   };
 
-  dontConfigure = true;  
-  dontBuild = true;
+  nativeBuildInputs = [
+    tar
+    autoPatchelfHook
+  ];
 
-  installPhase = ''
+  buildInputs = [
+    libusb1
+    libX11
+    libXtst
+    qt5.qtbase
+    libglvnd
+    stdenv.cc.cc.lib
+  ];
+
+  unpackPhase = ''
     tar -xzf Linux_Pentablet_V1.3.0.0.tar.gz
-    mkdir -p $out/bin
-    cp -r Linux_Pentablet_V1.3.0.0/Pentablet_Driver Linux_Pentablet_V1.3.0.0/config.xml $out/bin
   '';
 
-  preFixup = let
-    libPath = stdenv.lib.makeLibraryPath [
-      libusb1
-      libX11
-      libXtst
-      qt5.qtbase
-      libglvnd
-      stdenv.cc.cc.lib
-      ];
-  in ''
-    patchelf \
-      --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-      --set-rpath "${libPath}" \
-      $out/bin/Pentablet_Driver
+  installPhase = ''
+    mkdir -p $out/bin
+    cp Linux_Pentablet_V1.3.0.0/Pentablet_Driver Linux_Pentablet_V1.3.0.0/config.xml $out/bin
   '';
 
   meta = with stdenv.lib; {
