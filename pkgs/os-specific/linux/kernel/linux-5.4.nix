fb06094ae36f9efd63453f7e90ff67ef7243f4bf
Tim Steinbach <tim@nequissimus.com>
2020-01-15 09:12:21 -0500

    linux: 5.4.11 -> 5.4.12

--- pkgs/os-specific/linux/kernel/linux-5.4.nix
+++ pkgs/os-specific/linux/kernel/linux-5.4.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.4.11";
+  version = "5.4.12";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0b6pamnhyzf4n6sl8lxcnllrn41xmbldipfca23j1n71spjkdgb2";
+    sha256 = "1yyh934ifzwgqlpd8wy50z9d68hla5arvy50pi6c499dsnicghyr";
   };
 } // (args.argsOverride or {}))
