0285a20b48e2f72a5592371009a08993d040037b
Will Dietz <w@wdtz.org>
2020-01-15 09:49:56 -0600

    editline: patch for improved handling of multiline input
    Minor but observable in `nix repl` when history contains
    expression that wraps and one that doesn't and using up/down
    arrows to toggle between (for example).

--- pkgs/development/libraries/editline/default.nix
+++ pkgs/development/libraries/editline/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, autoreconfHook }:
+{ stdenv, fetchFromGitHub, autoreconfHook, fetchpatch }:
 
 stdenv.mkDerivation rec {
   pname = "editline";
@@ -9,6 +9,14 @@ stdenv.mkDerivation rec {
     rev = version;
     sha256 = "0vjm42y6zjmi6hdcng0l7wkksw7s50agbmk5dxsc3292q8mvq8v6";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "fix-for-multiline-as-one-line.patch";
+      url = "https://github.com/troglobit/editline/commit/ceee039cfc819c8e09eebbfca192091b0cf8df75.patch";
+      sha256 = "149fmfva05ghzwkd0bq1sahdbkys3qyyky28ssqb5jq7q9hw3ddm";
+    })
+  ];
 
   nativeBuildInputs = [ autoreconfHook ];
 
