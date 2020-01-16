575f9552cc898c7586f61a9d239eebde21f7cbc7
Niklas Hambüchen <mail@nh2.me>
2020-01-16 02:52:21 +0100

    libsixel: Enable tests.
    The 1.8.2 -> 1.8.4 upgrade actually made the tests compile.

--- pkgs/development/libraries/libsixel/default.nix
+++ pkgs/development/libraries/libsixel/default.nix
@@ -10,6 +10,12 @@ stdenv.mkDerivation rec {
     sha256 = "1zckahfl0j7k68jf87iwdc4yx7fkfhxwa7lrf22dnz36d2iq785v";
   };
 
+  configureFlags = [
+    "--enable-tests"
+  ];
+
+  doCheck = true;
+
   meta = with stdenv.lib; {
     description = "The SIXEL library for console graphics, and converter programs";
     homepage = http://saitoha.github.com/libsixel;
