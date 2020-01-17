354bcfeab01b283c2fe75d20ca0a584d82030d4d
R. RyanTM <ryantm-bot@ryantm.com>
2020-01-17 12:08:42 +0000

    video2midi: 0.3.9.6 -> 0.4.0.1

--- pkgs/tools/audio/video2midi/default.nix
+++ pkgs/tools/audio/video2midi/default.nix
@@ -8,7 +8,7 @@ let
   });
 in pythonPackages.buildPythonApplication rec {
   pname = "video2midi";
-  version = "0.3.9.6";
+  version = "0.4.0.1";
 
   format = "other";
 
@@ -16,7 +16,7 @@ in pythonPackages.buildPythonApplication rec {
     owner = "svsdval";
     repo = pname;
     rev = version;
-    sha256 = "0x9b8hwl325gd6v9i60yh95gbn49nydpwyfqs92mbq8vvvq7x8fk";
+    sha256 = "1869w60kprjdikqk4iwx058zri2jg4aznzlg668w9myka4mp01r9";
   };
 
   propagatedBuildInputs = with pythonPackages; [ opencv3_ midiutil pygame pyopengl ];
