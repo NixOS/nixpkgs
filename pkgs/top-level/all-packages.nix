d68ae2cf0e14da1433597f68857a837e80a9e031
kjuvi <quentin.vaucher@protonmail.com>
2020-01-19 12:30:45 +0100

    graphui: init at 1.1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19333,6 +19333,8 @@ in
 
   graphicsmagick-imagemagick-compat = callPackage ../applications/graphics/graphicsmagick/compat.nix { };
 
+  graphui = callPackage ../applications/graphics/graphui/default.nix { };
+
   grisbi = callPackage ../applications/office/grisbi { gtk = gtk3; };
 
   gtkpod = callPackage ../applications/audio/gtkpod { };
