b46edad31540a379ac19931a9a5fe249081da275
Will Dietz <w@wdtz.org>
2020-01-19 12:54:04 -0600

    librepo: 1.9.2 -> 1.11.2
    Expects zchunk by default now, added.
    
    cc #78040
    (this PR builds on that one)

--- pkgs/tools/package-management/librepo/default.nix
+++ pkgs/tools/package-management/librepo/default.nix
@@ -1,21 +1,21 @@
-{ stdenv, fetchFromGitHub, cmake, python, pkgconfig, libxml2, glib, openssl, curl, check, gpgme }:
+{ stdenv, fetchFromGitHub, cmake, python, pkgconfig, libxml2, glib, openssl, curl, check, gpgme, zchunk }:
 
 stdenv.mkDerivation rec {
-  version = "1.9.2";
+  version = "1.11.2";
   pname = "librepo";
 
   src = fetchFromGitHub {
     owner  = "rpm-software-management";
     repo   = "librepo";
     rev    = version;
-    sha256 = "0xa9ng9mhpianhjy2a0jnj8ha1zckk2sz91y910daggm1qcv5asx";
+    sha256 = "0f04qky61dlh5h71xdmpngpy98cmlsfyp2pkyj5sbkplvrmh1wzw";
   };
 
   nativeBuildInputs = [ cmake pkgconfig ];
 
   cmakeFlags = ["-DPYTHON_DESIRED=${stdenv.lib.substring 0 1 python.pythonVersion}" ];
 
-  buildInputs = [ python libxml2 glib openssl curl check gpgme ];
+  buildInputs = [ python libxml2 glib openssl curl check gpgme zchunk ];
 
   # librepo/fastestmirror.h includes curl/curl.h, and pkg-config specfile refers to others in here
   propagatedBuildInputs = [ curl gpgme libxml2 ];
