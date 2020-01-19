64e92b596ca494bbb12101c8f0e0cf239548de42
nyanloutre <paul@nyanlout.re>
2020-01-19 22:30:54 +0100

    dotnet-sdk_2: add alias

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -217,6 +217,8 @@ in
 
   dotnet-sdk = dotnetCorePackages.sdk_2_1;
 
+  dotnet-sdk_2 = dotnetCorePackages.sdk_2_1;
+
   dotnet-netcore = dotnetCorePackages.netcore_2_1;
 
   dotnet-aspnetcore = dotnetCorePackages.aspnetcore_2_1;
