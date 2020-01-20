57092f7ef17adfca9817f0ad9d713e1c5114ddff
Márton Boros <martonboros@gmail.com>
2020-01-20 07:51:57 +0000

    fluxus: init at 0.19

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18919,6 +18919,8 @@ in
 
   flrig = callPackage ../applications/radio/flrig { };
 
+  fluxus = callPackage ../applications/graphics/fluxus { };
+
   flwrap = callPackage ../applications/radio/flwrap { };
 
   fluidsynth = callPackage ../applications/audio/fluidsynth {
