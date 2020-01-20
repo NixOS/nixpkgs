becf4017c5656c3cab531063b47586c5501e00fd
Maciej Krüger <mkg20001@gmail.com>
2020-01-20 02:56:38 +0100

    cinnamon.nemo: init at 4.4.1

--- pkgs/desktops/cinnamon/default.nix
+++ pkgs/desktops/cinnamon/default.nix
@@ -3,5 +3,6 @@
 lib.makeScope pkgs.newScope (self: with self; {
   cinnamon-desktop = callPackage ./cinnamon-desktop { };
   cjs = callPackage ./cjs { };
+  nemo = callPackage ./nemo { };
   xapps = callPackage ./xapps { };
 })
