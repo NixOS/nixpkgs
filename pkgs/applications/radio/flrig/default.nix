2415a9990f658d4b9c9c59be8b3acea865e3c476
R. RyanTM <ryantm-bot@ryantm.com>
2020-01-20 07:11:10 +0000

    flrig: 1.3.48 -> 1.3.49

--- pkgs/applications/radio/flrig/default.nix
+++ pkgs/applications/radio/flrig/default.nix
@@ -6,12 +6,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "1.3.48";
+  version = "1.3.49";
   pname = "flrig";
 
   src = fetchurl {
     url = "mirror://sourceforge/fldigi/${pname}-${version}.tar.gz";
-    sha256 = "0f46rmrr8aphj8fsbfa1bywihigzfzyxq9zg66d1laa7d3jsfs9q";
+    sha256 = "0icfniiw2l7d7gyxrzy87n3ragzvf0ykvq7c10ppf111wl5in295";
   };
 
   buildInputs = [
