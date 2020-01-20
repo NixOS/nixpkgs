1170b0ecb02b50172efafc26b8ff607a695645f8
Maciej Krüger <mkg20001@gmail.com>
2020-01-20 19:15:48 +0100

    cinnamon.cinnamon-translations: init at 4.4.0

--- pkgs/desktops/cinnamon/default.nix
+++ pkgs/desktops/cinnamon/default.nix
@@ -3,6 +3,7 @@
 lib.makeScope pkgs.newScope (self: with self; {
   cinnamon-desktop = callPackage ./cinnamon-desktop { };
   cinnamon-menus = callPackage ./cinnamon-menus { };
+  cinnamon-translations = callPackage ./cinnamon-translations { };
   cjs = callPackage ./cjs { };
   xapps = callPackage ./xapps { };
 })
