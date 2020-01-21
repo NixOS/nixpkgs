1102fc870d46fdf9a96c6f2e2a0cbdc8156f05e8
Lancelot SIX <lsix@lancelotsix.com>
2020-01-21 14:00:54 +0100

    pythonPackages.django_2_1: drop
    Django-2.1 support ended on December 2, 2019, so drop it in nixpkgs as
    well.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3187,10 +3187,6 @@ in {
     gdal = self.gdal;
   };
 
-  django_2_1 = callPackage ../development/python-modules/django/2_1.nix {
-    gdal = self.gdal;
-  };
-
   django_2_2 = callPackage ../development/python-modules/django/2_2.nix { };
 
   django_1_8 = callPackage ../development/python-modules/django/1_8.nix { };
