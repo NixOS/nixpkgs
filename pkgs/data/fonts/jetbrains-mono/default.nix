1c45292ea39770f83ede7bad03d680ef4a1e244b
Mario Rodas <marsam@users.noreply.github.com>
2020-01-21 07:25:00 -0500

    jetbrains-mono: 1.0.0 -> 1.0.1
    Changelog: https://github.com/JetBrains/JetBrainsMono/releases/tag/1.0.1

--- pkgs/data/fonts/jetbrains-mono/default.nix
+++ pkgs/data/fonts/jetbrains-mono/default.nix
@@ -1,18 +1,20 @@
 { lib, fetchzip }:
 
 let
-  version = "1.0.0";
+  version = "1.0.1";
 in
 fetchzip rec {
   name = "JetBrainsMono-${version}";
 
-  url = "https://download.jetbrains.com/fonts/JetBrainsMono-${version}.zip";
+  url = "https://github.com/JetBrains/JetBrainsMono/releases/download/${version}/JetBrainsMono-${version}.zip";
 
-  sha256 = "0mwqi66d56v4ml1w7wjsiidrh153jvh0czafyic47rkvmxhnrrhv";
+  sha256 = "15a8fwyg8ns6krq6nsvgn41iaqbd3lgm3cmv7w370gr6brbn6lxq";
 
   postFetch = ''
     unzip $downloadedFile
-    install -m444 -Dt $out/share/fonts/truetype *.ttf
+    install -m444 -Dt $out/share/fonts/truetype ttf/*.ttf
+    install -m444 -Dt $out/share/fonts/woff/ web/woff/*.woff
+    install -m444 -Dt $out/share/fonts/woff2/ web/woff2/*.woff2
   '';
 
   meta = with lib; {
