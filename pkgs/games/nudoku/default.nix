52332244bd9626e36018b5c58b8df6d6b5111c43
Will Dietz <w@wdtz.org>
2020-01-21 10:17:50 -0600

    nudoku: fix w/musl, disable nls
    (cherry picked from commit 027b93ae5f1efe713dc213d56f0f2e4d9858db54)

--- pkgs/games/nudoku/default.nix
+++ pkgs/games/nudoku/default.nix
@@ -14,6 +14,8 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ autoreconfHook pkgconfig gettext ];
   buildInputs = [ ncurses ];
 
+  configureFlags = stdenv.lib.optional stdenv.hostPlatform.isMusl "--disable-nls";
+
   meta = with stdenv.lib; {
     description = "An ncurses based sudoku game";
     homepage = "http://jubalh.github.io/nudoku/";
