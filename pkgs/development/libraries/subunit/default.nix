5693611d596581baf0e4aced6a8c226b91089d3f
Drew Risinger <drewrisinger@users.noreply.github.com>
2020-01-21 19:10:52 -0500

    libraries.subunit: 1.1.0 -> 1.3.0
    Bump subunit, and change source to the main GitHub page.
    Pulling source from launchpad doesn't seem to work for 1.3.0.

--- pkgs/development/libraries/subunit/default.nix
+++ pkgs/development/libraries/subunit/default.nix
@@ -1,17 +1,19 @@
-{ stdenv, fetchurl, pkgconfig, check, cppunit, perl, pythonPackages }:
+{ stdenv, fetchFromGitHub, pkg-config, autoreconfHook, check, cppunit, perl, pythonPackages }:
 
 # NOTE: for subunit python library see pkgs/top-level/python-packages.nix
 
 stdenv.mkDerivation rec {
   pname = "subunit";
-  version = "1.1.0";
+  version = "1.3.0";
 
-  src = fetchurl {
-    url = "https://launchpad.net/subunit/trunk/${version}/+download/${pname}-${version}.tar.gz";
-    sha256 = "0lcah7p66c05p7xpw6ns1is0i02lh0nq8gq51mv4wyvbr6zaasa8";
+  src = fetchFromGitHub {
+    owner = "testing-cabal";
+    repo = pname;
+    rev = version;
+    sha256 = "1hk5v8gg5azwrc079xqigj0sp6qm47vzg1qwjbvb8dbjwlg75rvz";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
+  nativeBuildInputs = [ autoreconfHook pkg-config ];
   buildInputs = [ check cppunit perl pythonPackages.wrapPython ];
 
   propagatedBuildInputs = with pythonPackages; [ testtools testscenarios ];
@@ -20,8 +22,10 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "A streaming protocol for test results";
-    homepage = https://launchpad.net/subunit;
+    homepage = "https://launchpad.net/subunit";
+    downloadPage = "https://github.com/testing-cabal/subunit";
     license = licenses.asl20;
     platforms = platforms.all;
+    maintainers = with maintainers; [ drewrisinger ];
   };
 }
