546fe02875de012f3a0adae465c4e8da6dad6c9f
zimbatm <zimbatm@zimbatm.com>
2020-01-22 09:49:54 +0000

    h: init at 1.0.0 (#78017)
    Co-Authored-By: Jon <jonringer@users.noreply.github.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1593,6 +1593,8 @@ in
 
   direnv = callPackage ../tools/misc/direnv { };
 
+  h = callPackage ../tools/misc/h { };
+
   discount = callPackage ../tools/text/discount { };
 
   diskscan = callPackage ../tools/misc/diskscan { };
