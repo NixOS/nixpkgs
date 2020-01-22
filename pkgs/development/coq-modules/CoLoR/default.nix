4339f5803d9e23280108046565483a35e8588442
Jarrod Pas <jarrod@jarrodpas.com>
2020-01-22 01:40:33 -0600

    coqPackages.CoLoR 1.4.0 -> 1.6.0
    The package is now developed on GitHub, so fetchurl was replace with
    fetchFromGithub.

--- pkgs/development/coq-modules/CoLoR/default.nix
+++ pkgs/development/coq-modules/CoLoR/default.nix
@@ -1,11 +1,13 @@
-{ stdenv, fetchurl, coq, bignums }:
+{ stdenv, fetchFromGitHub, coq, bignums }:
 
 stdenv.mkDerivation {
-  name = "coq${coq.coq-version}-CoLoR-1.4.0";
+  name = "coq${coq.coq-version}-CoLoR-1.6.0";
 
-  src = fetchurl {
-    url = https://gforge.inria.fr/frs/download.php/file/37205/color.1.4.0.tar.gz;
-    sha256 = "1jsp9adsh7w59y41ihbwchryjhjpajgs9bhf8rnb4b3hzccqxgag";
+  src = fetchFromGitHub {
+    owner = "fblanqui";
+    repo = "color";
+    rev = "328aa06270584b578edc0d2925e773cced4f14c8";
+    sha256 = "07sy9kw1qlynsqy251adgi8b3hghrc9xxl2rid6c82mxfsp329sd";
   };
 
   buildInputs = [ coq bignums ];
@@ -15,14 +17,14 @@ stdenv.mkDerivation {
     make -f Makefile.coq COQLIB=$out/lib/coq/${coq.coq-version}/ install
   '';
 
-  meta = with stdenv.lib; {
+  meta = with stdenv.lib ; {
     homepage = http://color.inria.fr/;
     description = "CoLoR is a library of formal mathematical definitions and proofs of theorems on rewriting theory and termination whose correctness has been mechanically checked by the Coq proof assistant.";
-    maintainers = with maintainers; [ jwiegley ];
+    maintainers = with maintainers; [ jpas jwiegley ];
     platforms = coq.meta.platforms;
   };
 
   passthru = {
-    compatibleCoqVersions = v: builtins.elem v [ "8.6" "8.7" ];
+    compatibleCoqVersions = v: builtins.elem v [ "8.6" "8.7" "8.8" "8.9" ];
   };
 }
