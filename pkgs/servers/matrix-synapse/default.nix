b11d3c1d046bb69122a15e3ea1e8051c2903fb97
Maximilian Bosch <maximilian@mbosch.me>
2020-01-23 14:32:22 +0100

    matrix-synapse: 1.8.0 -> 1.9.0
    https://github.com/matrix-org/synapse/releases/tag/v1.9.0
    (cherry picked from commit 6930a1602c861216f99a37d17b7368dffa5420cc)

--- pkgs/servers/matrix-synapse/default.nix
+++ pkgs/servers/matrix-synapse/default.nix
@@ -23,11 +23,11 @@ let
 
 in buildPythonApplication rec {
   pname = "matrix-synapse";
-  version = "1.8.0";
+  version = "1.9.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0dkx53hxafqjs05g240zna2v3l3ndpa4y4z5x85v8dvv5zkibdxz";
+    sha256 = "0ghmxzk2njid7d7ccggda8a9zx4dg1r59n5ymp8f2n9kxz7z2rj2";
   };
 
   patches = [
