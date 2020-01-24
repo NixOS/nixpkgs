53616e8c4ee6a2fe3bb4a2c83de10e7ee28fe021
Tim Steinbach <tim@nequissimus.com>
2020-01-24 11:16:52 -0500

    linux: 4.4.210 -> 4.4.211

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.210";
+  version = "4.4.211";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1pg754s3138d2lq5y2zd1z7dagdy8pl4ifmp0754sa1rkjd3h0ns";
+    sha256 = "1f6qz4bvjn18cfcg3wwfsl75aw2kxwn28r228kdic9aibhy6rpvp";
   };
 } // (args.argsOverride or {}))
