90931eaa398c0a78c612749ee975dc283dd60760
Maciej Krüger <mkg20001@gmail.com>
2020-01-24 23:04:47 +0100

    cinnamon.cinnamon-session: init at 4.4.0

--- pkgs/desktops/cinnamon/default.nix
+++ pkgs/desktops/cinnamon/default.nix
@@ -4,6 +4,7 @@ lib.makeScope pkgs.newScope (self: with self; {
   cinnamon-desktop = callPackage ./cinnamon-desktop { };
   cinnamon-menus = callPackage ./cinnamon-menus { };
   cinnamon-translations = callPackage ./cinnamon-translations { };
+  cinnamon-session = callPackage ./cinnamon-session { };
   cinnamon-settings-daemon = callPackage ./cinnamon-settings-daemon { };
   cjs = callPackage ./cjs { };
   nemo = callPackage ./nemo { };
