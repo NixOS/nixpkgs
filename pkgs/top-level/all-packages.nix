28239c7ca556c922cb3a20ce28d299950b455ef4
Gabor Greif <ggreif@gmail.com>
2020-01-25 00:17:00 +0100

    llvmPackages_10: init at 10.0.0
    Backport of llvmPackages_10 since Chromium 83 will depend on it.
    See https://github.com/NixOS/nixpkgs/pull/83350#issuecomment-605994185
    
    llvmPackages_10: copy llvmPackages_9
    
    * starting with rc2
    * make `lldb` compilable again on Darwin
    * separate out manpage creation for `lldb` into a new derivation
    * minor tweaks to the patching of sources,
      some of which are backportable to earlier versions
    
    (cherry picked from commit f111c6f9cebacc0201f59c25d6be0683014a3935)
    
    llvmPackages_10: rc2 -> rc3
    
    http://lists.llvm.org/pipermail/llvm-dev/2020-March/139729.html
    
    Additionally cherry-picked 3 commits from `llvm-project/master`:
    - llvm/llvm-project@d21664c
    - llvm/llvm-project@3a0f6e6
    - llvm/llvm-project@87dac7d
    
    such that clang can automatically pick up the polly plugin from the
    `llvm-polly` build.
    
    (cherry picked from commit 3a84353edb56a6167fb687f2fdc32cab48a65079)
    
    llvmPackages_10: rc3 -> rc4
    
    Only needed to update hashes and the version.
    Updated comment for extension handling patch
    
    (cherry picked from commit 0ec3f4e26b6bcc9513202f9a479c897fece9b2bb)
    
    llvmPackages_10: removed extra polly-build
    
    There is no good reason to have separate builds for polly and no-polly
    versions. wwwwwThe reason for the two versions was (as far as I can
    tell) to defer rebuilds (see ed60483257b62c32b5b58c6e91c9c8cd586f77d0).
    Polly is now enabled by default.
    
    (cherry picked from commit e9aa8770ea8bd872da0d04cb98a7856ba1fc5412)
    
    llvmPackges_10: rc4 -> rc5
    
    updated versino and hashes for new rc
    
    (cherry picked from commit cdee144dfc0dc3a507a9a84b46bf1626761bb9b1)
    
    llvmPackages_10: rc5 -> release
    
    updated hashes and url
    updated comment regarding version/release_version to make it clearer
    
    (cherry picked from commit 4665b2a9a2ce06a2f1551fa75ce1202b45b72b14)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7848,6 +7848,7 @@ in
   llvm-polly = llvmPackages_latest.llvm-polly;
   clang-polly = llvmPackages_latest.clang.override { cc = llvmPackages_latest.clang-polly-unwrapped; };
 
+  clang_10 = llvmPackages_10.clang;
   clang_9  = llvmPackages_9.clang;
   clang_8  = llvmPackages_8.clang;
   clang_7  = llvmPackages_7.clang;
@@ -8545,6 +8546,7 @@ in
   lld_7 = llvmPackages_7.lld;
   lld_8 = llvmPackages_8.lld;
   lld_9 = llvmPackages_9.lld;
+  lld_10 = llvmPackages_10.lld;
 
   lldb = llvmPackages.lldb;
   lldb_5 = llvmPackages_5.lldb;
@@ -8552,10 +8554,12 @@ in
   lldb_7 = llvmPackages_7.lldb;
   lldb_8 = llvmPackages_8.lldb;
   lldb_9 = llvmPackages_9.lldb;
+  lldb_10 = llvmPackages_10.lldb;
 
   llvm = llvmPackages.llvm;
   llvm-manpages = llvmPackages.llvm-manpages;
 
+  llvm_10 = llvmPackages_10.llvm;
   llvm_9  = llvmPackages_9.llvm;
   llvm_8  = llvmPackages_8.llvm;
   llvm_7  = llvmPackages_7.llvm;
@@ -8606,7 +8610,15 @@ in
     stdenv = gcc6Stdenv; # with gcc-7: undefined reference to `__divmoddi4'
   });
 
-  llvmPackages_latest = llvmPackages_9;
+  llvmPackages_10 = callPackage ../development/compilers/llvm/10 ({
+    inherit (stdenvAdapters) overrideCC;
+    buildLlvmTools = buildPackages.llvmPackages_10.tools;
+    targetLlvmLibraries = targetPackages.llvmPackages_10.libraries;
+  } // stdenv.lib.optionalAttrs (stdenv.hostPlatform.isi686 && buildPackages.stdenv.cc.isGNU) {
+    stdenv = gcc7Stdenv;
+  });
+
+  llvmPackages_latest = llvmPackages_9; # llvmPackages_10: when out of RC
 
   lorri = callPackage ../tools/misc/lorri {
     inherit (darwin.apple_sdk.frameworks) CoreServices Security;
