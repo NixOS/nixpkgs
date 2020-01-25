36cea1ca64c84e82ea209a780fb1a7a5f9f25bd9
Drew Risinger <drewrisinger@users.noreply.github.com>
2020-01-25 18:02:57 -0500

    pythonPackages.ecos: init at 2.0.7.post1
    Embedded cone solver interface for optimization problems in Python.
    Dependency of qiskit-ignis.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3431,6 +3431,8 @@ in {
 
   ecdsa = callPackage ../development/python-modules/ecdsa { };
 
+  ecos = callPackage ../development/python-modules/ecos { };
+
   effect = callPackage ../development/python-modules/effect {};
 
   enum = callPackage ../development/python-modules/enum { };
