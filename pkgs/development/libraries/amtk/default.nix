8f5401811bf17faea3e923083d99eb0b6f8ad973
R. RyanTM <ryantm-bot@ryantm.com>
2020-01-26 16:24:02 +0000

    amtk: 5.0.1 -> 5.0.2

--- pkgs/development/libraries/amtk/default.nix
+++ pkgs/development/libraries/amtk/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, gtk3
 , pkgconfig, gnome3, dbus, xvfb_run }:
 let
-  version = "5.0.1";
+  version = "5.0.2";
   pname = "amtk";
 in stdenv.mkDerivation {
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "09yy95w1s83c43mh9vha1jbb780yighf5pd2j0ygjmc68sjg871d";
+    sha256 = "11jgz2i9wjzv4alrxl1qyxiapb52w7vs5ygfgsw0qgdap8gqkk3i";
   };
 
   nativeBuildInputs = [
