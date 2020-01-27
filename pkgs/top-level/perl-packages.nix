f0f38c30cfb4b716b17d32738158a4e3495d60dd
Alyssa Ross <hi@alyssa.is>
2020-01-27 00:31:32 +0000

    perlPackages.Plack: run some more tests

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -14480,6 +14480,10 @@ let
     };
     buildInputs = [ AuthenSimplePasswd CGIEmulatePSGI FileShareDirInstall HTTPRequestAsCGI HTTPServerSimplePSGI IOHandleUtil LWP LWPProtocolhttp10 LogDispatchArray MIMETypes TestMockTimeHiRes TestRequires TestSharedFork TestTCP ];
     propagatedBuildInputs = [ ApacheLogFormatCompiler CookieBaker DevelStackTraceAsHTML FileShareDir FilesysNotifySimple HTTPEntityParser HTTPHeadersFast HTTPMessage TryTiny ];
+    checkInputs = [ CGICompile FCGI FCGIProcManager LogLog4perl ModuleRefresh ];
+    postPatch = ''
+      patchShebangs t/Plack-Middleware/cgi-bin/cgi_dir.cgi
+    '';
     meta = {
       homepage = https://github.com/plack/Plack;
       description = "Perl Superglue for Web frameworks and Web Servers (PSGI toolkit)";
