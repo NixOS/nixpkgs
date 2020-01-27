NixOS/nixpkgs

commit 23d5d9bbe9aab982d17ad92c0d9da291d01f60c7

nixos/alertmanager: add environmentFile, envsubst for secrets

nixos/modules/services/monitoring/prometheus/alertmanager.nix
@@ -18,7 +18,7 @@ let
     in checkedConfig yml;
 
   cmdlineArgs = cfg.extraFlags ++ [
-    "--config.file ${alertmanagerYml}"
+    "--config.file /tmp/alert-manager-substituted.yaml"
     "--web.listen-address ${cfg.listenAddress}:${toString cfg.port}"
     "--log.level ${cfg.logLevel}"
     ] ++ (optional (cfg.webExternalUrl != null)
@@ -127,6 +127,16 @@ in {
           Extra commandline options when launching the Alertmanager.
         '';
       };
+
+      environmentFile = mkOption {
+        type = types.nullOr types.path;
+        default = null;
+        example = "/root/alertmanager.env";
+        description = ''
+          File to load as environment file. Useful to insert secrets
+          into the configuration (via envsubst).
+        '';
+      };
     };
   };
 
@@ -144,9 +154,14 @@ in {
       systemd.services.alertmanager = {
         wantedBy = [ "multi-user.target" ];
         after    = [ "network.target" ];
+        preStart = ''
+           ${lib.getBin pkgs.envsubst}/bin/envsubst -o /tmp/alert-manager-substituted.yaml" \
+                                                    -i ${alertmanagerYml}"
+        '';
         serviceConfig = {
           Restart  = "always";
-          DynamicUser = true;
+          DynamicUser = true; # implies PrivateTmp
+          EnvironmentFile = lib.mkIf (cfg.environmentFile != null) cfg.environmentFile;
           WorkingDirectory = "/tmp";
           ExecStart = "${cfg.package}/bin/alertmanager" +
             optionalString (length cmdlineArgs != 0) (" \\\n  " +

commit 9414f03d97c6f6ce57dae792a8e25d4931594acf

nixos/alertmanager: Fix startup
Fixed a problem with quotation marks introduced in commit
23d5d9bbe9aab982d17ad92c0d9da291d01f60c7 merged with #78358.

nixos/modules/services/monitoring/prometheus/alertmanager.nix
@@ -157,8 +157,8 @@ in {
         wantedBy = [ "multi-user.target" ];
         after    = [ "network.target" ];
         preStart = ''
-           ${lib.getBin pkgs.envsubst}/bin/envsubst -o /tmp/alert-manager-substituted.yaml" \
-                                                    -i ${alertmanagerYml}"
+           ${lib.getBin pkgs.envsubst}/bin/envsubst -o "/tmp/alert-manager-substituted.yaml" \
+                                                    -i "${alertmanagerYml}"
         '';
         serviceConfig = {
           Restart  = "always";