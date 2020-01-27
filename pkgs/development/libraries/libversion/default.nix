449c1f88b46df1ae5942da9efcb70c46e0845f71
Patrick Hilhorst <git@hilhorst.be>
2020-01-27 14:30:04 +0100

    libversion: 2.9.0 -> 3.0.1

--- pkgs/development/libraries/libversion/default.nix
+++ pkgs/development/libraries/libversion/default.nix
@@ -2,17 +2,20 @@
 
 stdenv.mkDerivation rec {
   pname = "libversion";
-  version = "2.9.0";
+  version = "3.0.1";
 
   src = fetchFromGitHub {
     owner = "repology";
     repo = "libversion";
     rev = version;
-    sha256 = "0h0yfcgxll09dckzjb1im3yf54cjkpsflr7r4kwz1jcr3fxq41fz";
+    sha256 = "13x5djdpv6aryxsbw6a3b6vwzi9f4aa3gn9dqb7axzppggayawyk";
   };
 
   nativeBuildInputs = [ cmake ];
 
+  preCheck = ''
+    export LD_LIBRARY_PATH=/build/source/build/libversion/:$LD_LIBRARY_PATH
+  '';
   doCheck = true;
   checkTarget = "test";
 
