86d286081a9ab27eb599a488e5bff01cfed918c3
misuzu <bakalolka@gmail.com>
2020-01-28 23:15:53 +0200

    nixosTests.docker-containers: port to python

--- nixos/tests/docker-containers.nix
+++ nixos/tests/docker-containers.nix
@@ -1,8 +1,6 @@
 # Test Docker containers as systemd units
 
-import ./make-test.nix ({ pkgs, lib, ... }:
-
-{
+import ./make-test-python.nix ({ pkgs, lib, ... }: {
   name = "docker-containers";
   meta = {
     maintainers = with lib.maintainers; [ benley mkaito ];
@@ -11,8 +9,6 @@ import ./make-test.nix ({ pkgs, lib, ... }:
   nodes = {
     docker = { pkgs, ... }:
       {
-        virtualisation.docker.enable = true;
-
         docker-containers.nginx = {
           image = "nginx-container";
           imageFile = pkgs.dockerTools.examples.nginx;
@@ -22,9 +18,8 @@ import ./make-test.nix ({ pkgs, lib, ... }:
   };
 
   testScript = ''
-    startAll;
-    $docker->waitForUnit("docker-nginx.service");
-    $docker->waitForOpenPort(8181);
-    $docker->waitUntilSucceeds("curl http://localhost:8181|grep Hello");
+    docker.wait_for_unit("docker-nginx.service")
+    docker.wait_for_open_port(8181)
+    docker.wait_until_succeeds("curl http://localhost:8181|grep Hello")
   '';
 })
