db29857eb391ed002046090851a44c452b80bdbd
Matthew Bauer <mjbauer95@gmail.com>
2020-01-28 11:35:19 -0500

    Re-Revert "clang_7,clang_8,clang_9: fix compilation of HIP-code"
    This reverts commit f14a4d60b74625547ba7fc0572e42e9b61c0e54f, and thus
    applying 558af6fbc7c15f472658339668113f4d5c6440e0.

--- pkgs/development/compilers/llvm/9/clang/default.nix
+++ pkgs/development/compilers/llvm/9/clang/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetch, cmake, libxml2, llvm, version, clang-tools-extra_src, python3
+{ stdenv, fetch, cmake, libxml2, llvm, version, clang-tools-extra_src, python3, lld
 , fixDarwinDylibNames
 , enableManpages ? false
 , enablePolly ? false # TODO: get this info from llvm (passthru?)
@@ -22,7 +22,7 @@ let
     nativeBuildInputs = [ cmake python3 ]
       ++ stdenv.lib.optional enableManpages python3.pkgs.sphinx;
 
-    buildInputs = [ libxml2 llvm ]
+    buildInputs = [ libxml2 llvm lld ]
       ++ stdenv.lib.optional stdenv.isDarwin fixDarwinDylibNames;
 
     cmakeFlags = [
@@ -43,6 +43,8 @@ let
       ./purity.patch
       # https://reviews.llvm.org/D51899
       ./compiler-rt-baremetal.patch
+      # make clang -xhip use $PATH to find executables
+      ./HIP-use-PATH-9.patch
     ];
 
     postPatch = ''
