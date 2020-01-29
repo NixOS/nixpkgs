a67e5e2c4cd628083c7c0648d79463760944858d
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-01-30 00:47:22 +0100

    upgrade super-user-spark

--- pkgs/applications/misc/super_user_spark/default.nix
+++ pkgs/applications/misc/super_user_spark/default.nix
@@ -1,11 +1,12 @@
-{ mkDerivation, fetchgit, aeson, aeson-pretty, base, bytestring, directory
-, filepath, hspec, hspec-core, HUnit, mtl, optparse-applicative
-, parsec, process, pureMD5, QuickCheck, shelly, stdenv, text
-, transformers, unix
+{ mkDerivation, aeson, aeson-pretty, base, bytestring, directory
+, filepath, fetchgit, genvalidity, genvalidity-hspec, genvalidity-hspec-aeson
+, genvalidity-path, hashable, hspec, hspec-core, mtl
+, optparse-applicative, parsec, path, path-io, process, QuickCheck
+, stdenv, text, transformers, unix, validity, validity-path
 }:
 mkDerivation {
   pname = "super-user-spark";
-  version = "0.3.2.0-dev";
+  version = "0.4.0.4";
   src = fetchgit {
     url = "https://github.com/NorfairKing/super-user-spark";
     sha256 = "0akyc51bghzkk8j75n0i8v8rrsklidwvljhx3aibxfbkqp33372g";
@@ -14,19 +15,20 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson aeson-pretty base bytestring directory filepath mtl
-    optparse-applicative parsec process pureMD5 shelly text
-    transformers unix
+    aeson aeson-pretty base bytestring directory filepath hashable mtl
+    optparse-applicative parsec path path-io process text unix validity
+    validity-path
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
-    aeson aeson-pretty base bytestring directory filepath hspec
-    hspec-core HUnit mtl optparse-applicative parsec process pureMD5
-    QuickCheck shelly text transformers unix
+    aeson aeson-pretty base bytestring directory filepath genvalidity
+    genvalidity-hspec genvalidity-hspec-aeson genvalidity-path hashable
+    hspec hspec-core mtl optparse-applicative parsec path path-io
+    process QuickCheck text transformers unix validity validity-path
   ];
-  jailbreak = true;
+  homepage = "https://github.com/NorfairKing/super-user-spark";
   description = "Configure your dotfile deployment with a DSL";
   license = stdenv.lib.licenses.mit;
-  homepage = https://github.com/NorfairKing/super-user-spark;
   maintainers = [ stdenv.lib.maintainers.badi ];
 }
+
