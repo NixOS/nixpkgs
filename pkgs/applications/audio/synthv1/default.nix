dffa3e9f9abf67aaae65aeab3824413ab08321ec
R. RyanTM <ryantm-bot@ryantm.com>
2020-01-30 14:03:55 +0000

    synthv1: 0.9.11 -> 0.9.12

--- pkgs/applications/audio/synthv1/default.nix
+++ pkgs/applications/audio/synthv1/default.nix
@@ -2,11 +2,11 @@
 
 mkDerivation rec {
   pname = "synthv1";
-  version = "0.9.11";
+  version = "0.9.12";
 
   src = fetchurl {
     url = "mirror://sourceforge/synthv1/${pname}-${version}.tar.gz";
-    sha256 = "116k2vca9dygvsd684wvxm61p0l1xrrgdph4qrrprlsr6vj0llgm";
+    sha256 = "1amxrl1cqwgncw5437r572frgf6xhss3cfpbgh178i8phlq1q731";
   };
 
   buildInputs = [ qtbase qttools libjack2 alsaLib liblo lv2 ];
