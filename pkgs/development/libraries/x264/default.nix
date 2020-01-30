7c75c128863e8fb680923580b96b6a5c9f757910
worldofpeace <worldofpeace@protonmail.ch>
2020-01-30 06:52:21 -0500

    x264: fix on ARM

--- pkgs/development/libraries/x264/default.nix
+++ pkgs/development/libraries/x264/default.nix
@@ -24,6 +24,8 @@ stdenv.mkDerivation rec {
   preConfigure = lib.optionalString (stdenv.buildPlatform.isx86_64 || stdenv.hostPlatform.isi686) ''
     # `AS' is set to the binutils assembler, but we need nasm
     unset AS
+  '' + lib.optionalString (stdenv.hostPlatform.isAarch64 || stdenv.hostPlatform.isAarch32) ''
+    export AS=$CC
   '';
 
   configureFlags = [ "--enable-shared" ]
