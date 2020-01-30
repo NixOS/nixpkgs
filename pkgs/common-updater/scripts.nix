65543d103193c46abdb190808b671fa2c66dde42
Jan Tojnar <jtojnar@gmail.com>
2020-01-30 21:25:01 +0100

    common-updater-scripts: clean up
    Fix issues reported by shellcheck and few other style issues.
    
    Though we need to ignore $systemArg complaints because Nix does not support passing --system as a single argument.

--- pkgs/common-updater/scripts.nix
+++ pkgs/common-updater/scripts.nix
@@ -1,4 +1,4 @@
-{ stdenv, makeWrapper, coreutils, gawk, gnused, gnugrep, diffutils, nix }:
+{ stdenv, makeWrapper, coreutils, gnused, gnugrep, diffutils, nix }:
 
 stdenv.mkDerivation {
   name = "common-updater-scripts";
@@ -12,7 +12,7 @@ stdenv.mkDerivation {
     cp ${./scripts}/* $out/bin
 
     for f in $out/bin/*; do
-      wrapProgram $f --prefix PATH : ${stdenv.lib.makeBinPath [ coreutils gawk gnused gnugrep nix diffutils ]}
+      wrapProgram $f --prefix PATH : ${stdenv.lib.makeBinPath [ coreutils gnused gnugrep nix diffutils ]}
     done
   '';
 }
