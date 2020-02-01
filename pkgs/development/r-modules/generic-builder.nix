610d6ff8f8bca0e2076cc9a9f3182793a10a815e
Thibault Gagnaux <thibault@gagnaux.ch>
2020-02-01 12:40:30 +0100

    Revert "r-modules/generic-builder: Add missing iconv library to fix build of r-modules on darwin"
    This reverts commit c58985b98cf00d1ebd2a0a642a1c4e5e7c6ca7d9.

--- pkgs/development/r-modules/generic-builder.nix
+++ pkgs/development/r-modules/generic-builder.nix
@@ -1,11 +1,11 @@
-{ stdenv, R, libcxx, xvfb_run, utillinux, Cocoa, Foundation, gettext, gfortran, libiconv }:
+{ stdenv, R, libcxx, xvfb_run, utillinux, Cocoa, Foundation, gettext, gfortran }:
 
 { name, buildInputs ? [], requireX ? false, ... } @ attrs:
 
 stdenv.mkDerivation ({
   buildInputs = buildInputs ++ [R gettext] ++
                 stdenv.lib.optionals requireX [utillinux xvfb_run] ++
-                stdenv.lib.optionals stdenv.isDarwin [Cocoa Foundation gfortran libiconv];
+                stdenv.lib.optionals stdenv.isDarwin [Cocoa Foundation gfortran];
 
   NIX_CFLAGS_COMPILE =
     stdenv.lib.optionalString stdenv.isDarwin "-I${libcxx}/include/c++/v1";
