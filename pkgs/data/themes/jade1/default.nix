28d22d67ffe73452dcbcd1ecfcd1b9ec79e455ef
José Romildo Malaquias <malaquias@gmail.com>
2020-02-03 07:21:07 -0300

    theme-jade1: 1.5 -> 1.6

--- pkgs/data/themes/jade1/default.nix
+++ pkgs/data/themes/jade1/default.nix
@@ -2,25 +2,25 @@
 
 stdenv.mkDerivation rec {
   pname = "theme-jade1";
-  version = "1.5";
+  version = "1.6";
 
   src = fetchFromGitHub {
     owner = "madmaxms";
     repo = "theme-jade-1";
     rev = "v${version}";
-    sha256 = "1m3150iyk8421mkwj4x2pv29wjzqdcnvvnp3bsg11k5kszsm27a8";
+    sha256 = "1lnajrsikw6dljf6dvgmj8aqwywmgdp34h3xsc0xiyq07arhp606";
   };
 
   propagatedUserEnvPkgs = [ gtk-engine-murrine ];
 
   installPhase = ''
     mkdir -p $out/share/themes
-    cp -a Jade-1 $out/share/themes
+    cp -a Jade* $out/share/themes
   '';
 
   meta = with stdenv.lib; {
     description = "Fork of the original Linux Mint theme with dark menus, more intensive green and some other modifications";
-    homepage = https://github.com/madmaxms/theme-jade-1;
+    homepage = "https://github.com/madmaxms/theme-jade-1";
     license = with licenses; [ gpl3 ];
     platforms = platforms.linux;
     maintainers = [ maintainers.romildo ];
