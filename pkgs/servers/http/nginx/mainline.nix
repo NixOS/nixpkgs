6d046e1079601c38540d5eb67ce913632898605b
Emily <vcs@emily.moe>
2020-02-04 22:06:02 +0000

    openresty: rebase on top of nginx package
    The primary motivation of this change was to allow third-party modules
    to be used with OpenResty, but it also results in a significant
    reduction of code duplication.

--- pkgs/servers/http/nginx/mainline.nix
+++ pkgs/servers/http/nginx/mainline.nix
@@ -1,6 +1,6 @@
 { callPackage, ... }@args:
 
-callPackage ./generic.nix (args // {
+callPackage ./generic.nix args {
   version = "1.17.8";
   sha256 = "0nwn4md8sxhks2j77qq1nvk5pfz3yykfhh2b507b6l2idp7kxllp";
-})
+}
