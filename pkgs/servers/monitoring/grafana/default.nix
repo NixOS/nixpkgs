5e0b85b91244831467b6b337ac57144a7e5f70b9
Maximilian Bosch <maximilian@mbosch.me>
2020-02-06 22:17:37 +0100

    grafana: add myself as maintainer

--- pkgs/servers/monitoring/grafana/default.nix
+++ pkgs/servers/monitoring/grafana/default.nix
@@ -38,7 +38,7 @@ buildGoPackage rec {
     description = "Gorgeous metric viz, dashboards & editors for Graphite, InfluxDB & OpenTSDB";
     license = licenses.asl20;
     homepage = "https://grafana.com";
-    maintainers = with maintainers; [ offline fpletz willibutz globin ];
+    maintainers = with maintainers; [ offline fpletz willibutz globin ma27 ];
     platforms = platforms.linux;
   };
 }
