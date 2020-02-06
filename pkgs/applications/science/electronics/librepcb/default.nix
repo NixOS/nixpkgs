2e3c162df5315f7a9e19d3984a752bbb356e93d8
Hernawan Faïz Abdillah <3030950+Abdillah@users.noreply.github.com>
2020-02-06 08:44:16 +0700

    librepcb: Fix platform plugin xcb not found
    (cherry picked from commit 2d4ee4017680db7052596d65e5d4bca0079430e7)

--- pkgs/applications/science/electronics/librepcb/default.nix
+++ pkgs/applications/science/electronics/librepcb/default.nix
@@ -1,6 +1,6 @@
-{ stdenv, fetchFromGitHub, qtbase, qttools, qmake }:
+{ lib, mkDerivation, fetchFromGitHub, qtbase, qttools, qmake }:
 
-stdenv.mkDerivation {
+mkDerivation {
   pname = "librepcb";
   version = "0.1.0";
 
@@ -25,7 +25,7 @@ stdenv.mkDerivation {
       cp share/librepcb/fontobene/newstroke.bene $out/share/librepcb/fontobene/
     '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "A free EDA software to develop printed circuit boards";
     homepage = https://librepcb.org/;
     maintainers = with maintainers; [ luz ];
