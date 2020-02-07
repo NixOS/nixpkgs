39f0e3b32b5b4aa5ec463c21fa3488f677fac18e
Ben Darwin <bcdarwin@gmail.com>
2020-02-07 11:57:12 -0500

    minc_tools: enable building conversion tools

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23884,7 +23884,9 @@ in
 
   messer-slim = callPackage ../applications/science/biology/messer-slim { };
 
-  minc_tools = callPackage ../applications/science/biology/minc-tools { };
+  minc_tools = callPackage ../applications/science/biology/minc-tools {
+    inherit (perlPackages) perl TextFormat;
+  };
 
   minc_widgets = callPackage ../applications/science/biology/minc-widgets { };
 
