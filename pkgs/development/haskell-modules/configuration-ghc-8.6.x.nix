abd59264472101ca31a06bfde08a9b5166dc37ab
Peter Simons <simons@cryp.to>
2020-02-07 20:55:41 +0100

    haskell-policeman: don't build this package with ghc-8.6.x

--- pkgs/development/haskell-modules/configuration-ghc-8.6.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.6.x.nix
@@ -84,4 +84,7 @@ self: super: {
   # cabal2spec needs a recent version of Cabal
   cabal2spec = super.cabal2spec.overrideScope (self: super: { Cabal = self.Cabal_3_0_0_0; });
 
+  # Builds only with ghc-8.8.x and beyond.
+  policeman = markBroken super.policeman;
+
 }
