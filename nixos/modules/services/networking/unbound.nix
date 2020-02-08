30b127110d593df09f43e869c6d7d352258b66c4
kraem <ebrin.ronnie@protonmail.com>
2020-02-08 20:17:23 +0100

    nixos/unbound: add enableRemoteAccess option
    Option that sets remote-control setting to true in unbound.conf which
    in turn enables the new wrapping of unbound-control to access the server locally.
    Also includes options 'remoteAccessInterfaces' and 'remoteAccessPort'
    for remote access.

--- nixos/modules/services/networking/unbound.nix
+++ nixos/modules/services/networking/unbound.nix
@@ -14,6 +14,18 @@ let
 
   isLocalAddress = x: substring 0 3 x == "::1" || substring 0 9 x == "127.0.0.1";
 
+  remote =
+    optionalString (cfg.enableRemoteAccess) ''
+    remote-control:
+      control-enable: "yes"
+      server-key-file: ${stateDir}/unbound_server.key
+      server-cert-file: ${stateDir}/unbound_server.pem
+      control-key-file: ${stateDir}/unbound_control.key
+      control-cert-file: ${stateDir}/unbound_control.pem
+      control-port: ${toString cfg.remoteAccessPort}
+      ${concatMapStringsSep "\n  " (x: "control-interface: ${x}") cfg.remoteAccessInterfaces}
+    '';
+
   forward =
     optionalString (any isLocalAddress cfg.forwardAddresses) ''
       do-not-query-localhost: no
@@ -29,6 +41,22 @@ let
   trustAnchor = optionalString cfg.enableRootTrustAnchor
     "auto-trust-anchor-file: ${rootTrustAnchorFile}";
 
+  unboundWrapped = pkgs.stdenv.mkDerivation {
+      name = "unbound-wrapped";
+
+      buildInputs = [ pkgs.makeWrapper pkgs.unbound ];
+
+      phases = [ "installPhase" ];
+
+      installPhase = ''
+        mkdir -p "$out/bin"
+        makeWrapper ${pkgs.unbound}/bin/unbound-control $out/bin/unbound-control \
+          --add-flags "-c ${stateDir}/unbound.conf"
+        makeWrapper ${pkgs.unbound}/bin/unbound-checkconf $out/bin/unbound-checkconf \
+          --add-flags "${stateDir}/unbound.conf"
+      '';
+    };
+
   confFile = pkgs.writeText "unbound.conf" ''
     server:
       directory: "${stateDir}"
@@ -40,6 +68,7 @@ let
       ${trustAnchor}
     ${cfg.extraConfig}
     ${forward}
+    ${remote}
   '';
 
 in
@@ -94,6 +123,37 @@ in
         '';
       };
 
+      enableRemoteAccess = mkOption {
+        default = false;
+        type = types.bool;
+        description = ''
+          Sets the remote-control option to 'yes' in the config and generates keys + certificates under ${stateDir}
+          with <literal>unbound-control-setup</literal>.
+          Sets the default addresses to <literal>127.0.0.1</literal> <literal>::1</literal> and port <literal>8953</literal>.
+          Interfaces the server listens to can be set with <literal>remoteAccessInterfaces</literal>.
+          The port can be set with <literal>remoteAccessPort</literal>.
+          NOTE: This option doesn't open any ports.
+        '';
+      };
+
+      remoteAccessInterfaces = mkOption {
+        default = [ "127.0.0.1" ] ++ optional config.networking.enableIPv6 "::1";
+        type = types.listOf types.str;
+        description = ''
+          What addresses/interfaces are allowed to remote-control the unbound server.
+          Requires <literal>enableRemoteAccess</literal> set to true.
+        '';
+      };
+
+      remoteAccessPort = mkOption {
+        default = 8953;
+        type = types.port;
+        description = ''
+          What port the unbound server will listen on for remote-control.
+          Requires <literal>enableRemoteAccess</literal> set to true.
+        '';
+      };
+
     };
   };
 
@@ -101,7 +161,7 @@ in
 
   config = mkIf cfg.enable {
 
-    environment.systemPackages = [ cfg.package ];
+    environment.systemPackages = [ cfg.package (hiPrio unboundWrapped) ];
 
     users.users.unbound = {
       description = "unbound daemon user";
@@ -117,6 +177,8 @@ in
       wants = [ "nss-lookup.target" ];
       wantedBy = [ "multi-user.target" ];
 
+      path = mkIf cfg.enableRemoteAccess [ pkgs.openssl ];
+
       preStart = ''
         mkdir -m 0755 -p ${stateDir}/dev/
         cp ${confFile} ${stateDir}/unbound.conf
@@ -126,11 +188,12 @@ in
         ''}
         touch ${stateDir}/dev/random
         ${pkgs.utillinux}/bin/mount --bind -n /dev/urandom ${stateDir}/dev/random
+        ${optionalString cfg.enableRemoteAccess "${pkgs.unbound}/bin/unbound-control-setup -d ${stateDir}"}
       '';
 
       serviceConfig = {
         ExecStart = "${cfg.package}/bin/unbound -d -c ${stateDir}/unbound.conf";
-        ExecStopPost="${pkgs.utillinux}/bin/umount ${stateDir}/dev/random";
+        ExecStopPost = "${pkgs.utillinux}/bin/umount ${stateDir}/dev/random";
 
         ProtectSystem = true;
         ProtectHome = true;
