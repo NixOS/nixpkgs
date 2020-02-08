0e651ce7fa02d8a8cffdf7a73a8bbf2e0f0f1414
Jan Tojnar <jtojnar@gmail.com>
2020-02-08 02:33:05 +0100

    librepo: Clean up

--- pkgs/tools/package-management/librepo/default.nix
+++ pkgs/tools/package-management/librepo/default.nix
@@ -1,30 +1,58 @@
-{ stdenv, fetchFromGitHub, cmake, python, pkgconfig, libxml2, glib, openssl, curl, check, gpgme }:
+{ stdenv
+, fetchFromGitHub
+, cmake
+, python
+, pkgconfig
+, libxml2
+, glib
+, openssl
+, curl
+, check
+, gpgme
+}:
 
 stdenv.mkDerivation rec {
   version = "1.9.2";
   pname = "librepo";
 
   src = fetchFromGitHub {
-    owner  = "rpm-software-management";
-    repo   = "librepo";
-    rev    = version;
+    owner = "rpm-software-management";
+    repo = "librepo";
+    rev = version;
     sha256 = "0xa9ng9mhpianhjy2a0jnj8ha1zckk2sz91y910daggm1qcv5asx";
   };
 
-  nativeBuildInputs = [ cmake pkgconfig ];
+  nativeBuildInputs = [
+    cmake
+    pkgconfig
+  ];
 
-  cmakeFlags = ["-DPYTHON_DESIRED=${stdenv.lib.substring 0 1 python.pythonVersion}" ];
-
-  buildInputs = [ python libxml2 glib openssl curl check gpgme ];
+  buildInputs = [
+    python
+    libxml2
+    glib
+    openssl
+    curl
+    check
+    gpgme
+  ];
 
   # librepo/fastestmirror.h includes curl/curl.h, and pkg-config specfile refers to others in here
-  propagatedBuildInputs = [ curl gpgme libxml2 ];
+  propagatedBuildInputs = [
+    curl
+    gpgme
+    libxml2
+  ];
+
+  cmakeFlags = [
+    "-DPYTHON_DESIRED=${stdenv.lib.substring 0 1 python.pythonVersion}"
+  ];
 
   meta = with stdenv.lib; {
     description = "Library providing C and Python (libcURL like) API for downloading linux repository metadata and packages";
-    homepage    = https://rpm-software-management.github.io/librepo/;
-    license     = licenses.lgpl2Plus;
-    platforms   = platforms.linux;
+    homepage = "https://rpm-software-management.github.io/librepo/";
+    license = licenses.lgpl2Plus;
+    platforms = platforms.linux;
     maintainers = with maintainers; [ copumpkin ];
   };
 }
