6587634ad83156202c4d8911e6bc4b8e28047157
Jörg Thalheim <joerg@thalheim.io>
2020-02-09 08:40:38 +0000

    xps/15-7590: don't enable unfree firmware by default
    Also don't set nixpkgs.pkgs. Instead just add an overlay.

--- dell/xps/15-7590/default.nix
+++ dell/xps/15-7590/default.nix
@@ -18,22 +18,19 @@
 
   # The 48.ucode causes the Killer wifi card to crash.
   # The iwlfwifi-cc-a0-46.ucode works perfectly
-  nixpkgs.pkgs = import <nixpkgs> {
-    config.allowUnfree = true;
-    overlays = [
-      (self: super: {
-        firmwareLinuxNonfree = super.firmwareLinuxNonfree.overrideAttrs (old: {
-          src = super.fetchgit{
-            url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
-            rev = "bf13a71b18af229b4c900b321ef1f8443028ded8";
-            sha256 = "1dcaqdqyffxiadx420pg20157wqidz0c0ca5mrgyfxgrbh6a4mdj";
-          };
-          postInstall = ''
-            rm $out/lib/firmware/iwlwifi-cc-a0-48.ucode
-          '';
-          outputHash = "0dq48i1cr8f0qx3nyq50l9w9915vhgpwmwiw3b4yhisbc3afyay4";
-        });
-      })
-    ];
-  };
+  nixpkgs.overlays = [
+    (self: super: {
+      firmwareLinuxNonfree = super.firmwareLinuxNonfree.overrideAttrs (old: {
+        src = super.fetchgit{
+          url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
+          rev = "bf13a71b18af229b4c900b321ef1f8443028ded8";
+          sha256 = "1dcaqdqyffxiadx420pg20157wqidz0c0ca5mrgyfxgrbh6a4mdj";
+        };
+        postInstall = ''
+          rm $out/lib/firmware/iwlwifi-cc-a0-48.ucode
+        '';
+        outputHash = "0dq48i1cr8f0qx3nyq50l9w9915vhgpwmwiw3b4yhisbc3afyay4";
+      });
+    })
+  ];
 }
