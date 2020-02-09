4657f896999ec4d22304f67cc0aa970c45adb5e9
R. RyanTM <ryantm-bot@ryantm.com>
2020-02-09 06:45:50 +0000

    python27Packages.awkward: 0.12.19 -> 0.12.20

--- pkgs/development/python-modules/awkward/default.nix
+++ pkgs/development/python-modules/awkward/default.nix
@@ -11,11 +11,11 @@
 
 buildPythonPackage rec {
   pname = "awkward";
-  version = "0.12.19";
+  version = "0.12.20";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1s729a8205jzg7pfw8xgmi850x03p9nw8c6a602f5bnmhha96h45";
+    sha256 = "13494pnzz68qfnx17975h4c5l15idgg7wxl9r86q7jp5s1pphvb3";
   };
 
   nativeBuildInputs = [ pytestrunner ];
