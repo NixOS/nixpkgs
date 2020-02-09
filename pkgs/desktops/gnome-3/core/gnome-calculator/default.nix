ad30622ae60b320360a39b8adbbd3a1f25183b3f
Tor Hedin Brønner <torhedinbronner@gmail.com>
2020-02-09 01:35:03 +0100

    gnome3.gnome-calculator: 3.34.1 -> 3.36.0

--- pkgs/desktops/gnome-3/core/gnome-calculator/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-calculator/default.nix
@@ -4,11 +4,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-calculator";
-  version = "3.34.1";
+  version = "3.36.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-calculator/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0lbh87255zzggqzai6543qg920y52bl4vs5m5h5087ghzg14hlsd";
+    sha256 = "1cqd4b25qp1i0p04m669jssg1l5sdapc1mniv9jssvw5r7wk1s52";
   };
 
   nativeBuildInputs = [
