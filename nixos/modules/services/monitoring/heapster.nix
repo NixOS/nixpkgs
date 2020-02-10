NixOS/nixpkgs

commit 1d61efb7f177f7b70c467ab4940fde0a3481d4dc

treewide: use attrs instead of list for types.loaOf options

nixos/modules/services/monitoring/heapster.nix
@@ -49,8 +49,7 @@ in {
       };
     };
 
-    users.users = singleton {
-      name = "heapster";
+    users.users.heapsterrs = {
       uid = config.ids.uids.heapster;
       description = "Heapster user";
     };

commit 17c72ef75f64628f57667a7fd032e42510d2beea

nixos/heapster: fix username
Broken in 1d61efb7f177f7b70c467ab4940fde0a3481d4dc.

nixos/modules/services/monitoring/heapster.nix
@@ -49,7 +49,7 @@ in {
       };
     };
 
-    users.users.heapsterrs = {
+    users.users.heapster = {
       uid = config.ids.uids.heapster;
       description = "Heapster user";
     };