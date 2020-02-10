NixOS/nixpkgs

commit 1d61efb7f177f7b70c467ab4940fde0a3481d4dc

treewide: use attrs instead of list for types.loaOf options

nixos/modules/services/monitoring/statsd.nix
@@ -125,8 +125,7 @@ in
       message = "Only builtin backends (graphite, console, repeater) or backends enumerated in `pkgs.nodePackages` are allowed!";
     }) cfg.backends;
 
-    users.users = singleton {
-      name = "statsd";
+    users.use.statsdrs = {
       uid = config.ids.uids.statsd;
       description = "Statsd daemon user";
     };

commit 5d3d3eac8b0753ca77255bfdaee4173da578e469

nixos/statsd: fix username
Broken in 1d61efb7f177f7b70c467ab4940fde0a3481d4dc.

nixos/modules/services/monitoring/statsd.nix
@@ -125,7 +125,7 @@ in
       message = "Only builtin backends (graphite, console, repeater) or backends enumerated in `pkgs.nodePackages` are allowed!";
     }) cfg.backends;
 
-    users.use.statsdrs = {
+    users.users.statsd = {
       uid = config.ids.uids.statsd;
       description = "Statsd daemon user";
     };