259d76d54119e03805b2225664e5fc77b6458cd8
Jonathan Ringer <jonringer117@gmail.com>
2020-02-09 18:02:58 -0800

    azure-cli: 2.0.80 -> 2.0.81

--- pkgs/tools/admin/azure-cli/default.nix
+++ pkgs/tools/admin/azure-cli/default.nix
@@ -1,12 +1,12 @@
 { stdenv, lib, python, fetchFromGitHub, installShellFiles }:
 
 let
-  version = "2.0.80";
+  version = "2.0.81";
   src = fetchFromGitHub {
     owner = "Azure";
     repo = "azure-cli";
     rev = "azure-cli-${version}";
-    sha256 = "05j74cfxjpi3w79w0i5av3h2m81bavbsc581vvh773ixivndds1k";
+    sha256 = "1fpdc59qqkx9s5aip62jzcky92fhlbvc97hi1x047sldh9ibp02n";
   };
 
   # put packages that needs to be overriden in the py package scope
