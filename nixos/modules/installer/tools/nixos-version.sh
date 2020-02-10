fb05afd78dc63fa93626ee51330c69559754b44d
Eelco Dolstra <edolstra@gmail.com>
2020-02-10 16:32:59 +0100

    Doh

--- nixos/modules/installer/tools/nixos-version.sh
+++ nixos/modules/installer/tools/nixos-version.sh
@@ -6,7 +6,7 @@ case "$1" in
     exit 1
     ;;
   --hash|--revision)
-    if ! [[ @revision@ =~ /[0-9a-f]+/ ]]; then
+    if ! [[ @revision@ =~ ^[0-9a-f]+$ ]]; then
       echo "$0: Nixpkgs commit hash is unknown"
       exit 1
     fi
