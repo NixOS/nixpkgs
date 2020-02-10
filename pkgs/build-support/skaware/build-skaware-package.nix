16cc7a36ba88f1b4c3980d3bbb507047dc1bd821
Profpatsch <mail@profpatsch.de>
2020-02-10 13:51:50 +0100

    skawarePackages: support static builds via pkgsStatic
    Most of the skaware packages already build just fine with pkgsStatic,
    however the wrapper scripts for execline and stdnotify-wrapper needed
    the `-lskarlib` argument to go at the end.
    
    `utmps` and `nsss` still fail with this error:
    
    ```
    exec ./tools/install.sh -D -m 600 utmps-utmpd /bin/utmps-utmpd
    /build/utmps-0.0.3.1/tools/install.sh: line 48: can't create /bin/utmps-utmpd.tmp.479: Permission denied
    make: *** [Makefile:121: /bin/utmps-utmpd] Error 1
    ```

--- pkgs/build-support/skaware/build-skaware-package.nix
+++ pkgs/build-support/skaware/build-skaware-package.nix
@@ -65,6 +65,9 @@ in stdenv.mkDerivation {
 
   configureFlags = configureFlags ++ [
     "--enable-absolute-paths"
+    # We assume every nix-based cross target has urandom.
+    # This might not hold for e.g. BSD.
+    "--with-sysdep-devurandom=yes"
     (if stdenv.isDarwin
       then "--disable-shared"
       else "--enable-shared")
