b3a20c4febe4c6565b5340bee9cfdace294a3aa0
Doron Behar <doron.behar@gmail.com>
2020-02-11 22:49:09 +0200

    octave: remove old fix for build issue

--- pkgs/development/interpreters/octave/default.nix
+++ pkgs/development/interpreters/octave/default.nix
@@ -89,9 +89,6 @@ stdenv.mkDerivation rec {
 
   enableParallelBuilding = true;
 
-  # See https://savannah.gnu.org/bugs/?50339
-  F77_INTEGER_8_FLAG = if openblas.blas64 then "-fdefault-integer-8" else "";
-
   configureFlags = [
     "--enable-readline"
     "--with-blas=openblas"
