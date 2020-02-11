b67006fae1e12e9bd3ae43259ef575a434ff8fd9
Jeff Labonte <grimsleepless@protonmail.com>
2020-02-10 21:53:02 -0500

    pythonPackages.digi-xbee: init at 1.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6950,8 +6950,6 @@ in {
 
   importlib-resources = callPackage ../development/python-modules/importlib-resources {};
 
-  srp = callPackage ../development/python-modules/srp { };
-
   srptools = callPackage ../development/python-modules/srptools { };
 
   srp = callPackage ../development/python-modules/srp { };
