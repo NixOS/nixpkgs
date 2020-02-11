9b2a26d9a8c85f9dddfba7de0a51716c15d746bd
Mario Rodas <marsam@users.noreply.github.com>
2020-02-11 13:40:00 -0500

    nodejs-12_x: 12.15.0 -> 12.16.0
    Changelog: https://github.com/nodejs/node/releases/tag/v12.16.0

--- pkgs/development/web/nodejs/v12.nix
+++ pkgs/development/web/nodejs/v12.nix
@@ -1,12 +1,10 @@
-{ stdenv, callPackage, openssl, icu, enableNpm ? true }:
+{ callPackage, openssl, icu, enableNpm ? true }:
 
 let
   buildNodejs = callPackage ./nodejs.nix { inherit openssl icu; };
 in
   buildNodejs {
     inherit enableNpm;
-    version = "12.15.0";
-    sha256 = "06x8sma4rxbw0mxj0l4438lfpv9abxmnw2ibamq1acng1jl4zyyj";
-
-    patches = stdenv.lib.optionals stdenv.isDarwin [ ./disable-libatomic-darwin.patch ];
+    version = "12.16.0";
+    sha256 = "09grij355z210mkzkzarb6gwz8b02lnaxzdll1249kiz8wvhdjdq";
   }
