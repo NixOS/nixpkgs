26f0535237b1a7ba7eb3917f48f01282950063c0
danbst <abcz2.uprola@gmail.com>
2020-02-13 12:16:28 +0200

    zoom-us: fix launch
    Probably due to glibc update, ZoomLauncher became broken when v4l is present in
    LD_PRELOAD path. It can be fixed by a) removing ZoomLauncher from startup chain,
    so `zoom` is started directly or b) removing v4l from LD_PRELOAD.
    
    The reason v4l was added before was because my video was rotated upside down without it.
    Seem like nowadays this is fixed by Zoom itself, so I'm disabling it.
    
    Fixes https://github.com/NixOS/nixpkgs/issues/79954
    
    Co-authored-by: @mmlb

--- pkgs/applications/networking/instant-messengers/zoom-us/default.nix
+++ pkgs/applications/networking/instant-messengers/zoom-us/default.nix
@@ -5,7 +5,8 @@
 , qtimageformats, qtlocation, qtquickcontrols, qtquickcontrols2, qtscript, qtsvg
 , qttools, qtwayland, qtwebchannel, qtwebengine
 # Runtime
-, coreutils, libjpeg_turbo, pciutils, procps, utillinux, libv4l
+, coreutils, libjpeg_turbo, pciutils, procps, utillinux
+, enableV4l ? false, libv4l
 , pulseaudioSupport ? true, libpulseaudio ? null
 }:
 
@@ -101,11 +102,10 @@ in mkDerivation {
 
   qtWrapperArgs = [
     ''--prefix PATH : ${makeBinPath [ coreutils glib.dev pciutils procps qttools.dev utillinux ]}''
-    ''--prefix LD_PRELOAD : ${libv4l}/lib/libv4l/v4l2convert.so''
     # --run "cd ${placeholder "out"}/share/zoom-us"
     # ^^ unfortunately, breaks run arg into multiple array elements, due to
     # some bad array propagation. We'll do that in bash below
-  ];
+  ] ++ stdenv.lib.optional enableV4l ''--prefix LD_PRELOAD : ${libv4l}/lib/libv4l/v4l2convert.so'';
 
   postFixup = ''
     # Zoom expects "zopen" executable (needed for web login) to be present in CWD. Or does it expect
