381e86a7827a4f1c631e76ee5b37d1d4bfd9e480
Jonathan Ringer <jonringer117@gmail.com>
2020-02-13 15:33:27 -0800

    dwarf-fortress.dfhack: add 0.47.02-alpha0 release

--- pkgs/games/dwarf-fortress/dfhack/default.nix
+++ pkgs/games/dwarf-fortress/dfhack/default.nix
@@ -46,6 +46,12 @@ let
       xmlRev = "23500e4e9bd1885365d0a2ef1746c321c1dd5094";
       prerelease = false;
     };
+    "0.47.02" = {
+      dfHackRelease = "0.47.02-alpha0";
+      sha256 = "19lgykgqm0si9vd9hx4zw8b5m9188gg8r1a6h25np2m2ziqwbjj9";
+      xmlRev = "23500e4e9bd1885365d0a2ef1746c321c1dd509a";
+      prerelease = true;
+    };
   };
 
   release = if hasAttr dfVersion dfhack-releases
