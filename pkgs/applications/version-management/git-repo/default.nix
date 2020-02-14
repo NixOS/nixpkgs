c1741fc12a3329cf4c4509f22c60dfd4d54a192f
Michael Weiss <dev.primeos@gmail.com>
2020-02-14 01:05:01 +0100

    gitRepo: 2.1.1 -> 2.3

--- pkgs/applications/version-management/git-repo/default.nix
+++ pkgs/applications/version-management/git-repo/default.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation rec {
   pname = "git-repo";
-  version = "2.1.1";
+  version = "2.3";
 
   src = fetchFromGitHub {
     owner = "android";
     repo = "tools_repo";
     rev = "v${version}";
-    sha256 = "0p09yak0vrdg8apk76kbx5gy7z57mzis9702rbw8mfx9p0ag6fy7";
+    sha256 = "0jrll0mjfwakyjvlhbxwsdi32jhgss9mwz8c8h24n1qbqqxysrk4";
   };
 
   patches = [ ./import-ssl-module.patch ];
