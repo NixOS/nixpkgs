c5c9ef110c8fbc63ff49f187bcef7a3b7c69b1b4
worldofpeace <worldofpeace@protonmail.ch>
2020-02-14 14:56:27 -0500

    xfce4-14.xfce4-pulseaudio-plugin: 0.4.1 -> 0.4.2, fix volume
    A port of 8e5ed7cfbbb1dcd9a1bb0a9fa54d57cef95287e5.

--- pkgs/desktops/xfce4-14/xfce4-pulseaudio-plugin/default.nix
+++ pkgs/desktops/xfce4-14/xfce4-pulseaudio-plugin/default.nix
@@ -1,19 +1,47 @@
-{ mkXfceDerivation, automakeAddFlags, dbus-glib, dbus, gtk3, libpulseaudio
-, libnotify, libxfce4ui, libxfce4util, xfce4-panel, xfconf }:
+{ mkXfceDerivation
+, automakeAddFlags
+, dbus-glib
+, dbus
+, gtk3
+, libpulseaudio
+, libnotify
+, libxfce4ui
+, libxfce4util
+, xfce4-panel
+, xfconf
+, keybinder3
+, glib
+}:
 
 mkXfceDerivation {
   category = "panel-plugins";
   pname = "xfce4-pulseaudio-plugin";
-  version = "0.4.1";
-  sha256 = "1c8krpg3l6ki00ldd9hifc4bddysdm0w3x5w43fkr31j0zrscvfp";
+  version = "0.4.2";
+  sha256 = "1s996mcniskq42vv7cb9i165pmrfp9c95p5f9rx14hqq8in9mvc5";
 
-  nativeBuildInputs = [ automakeAddFlags ];
+  nativeBuildInputs = [
+    automakeAddFlags
+  ];
 
-  NIX_CFLAGS_COMPILE = [ "-I${dbus-glib.dev}/include/dbus-1.0" "-I${dbus.dev}/include/dbus-1.0" ];
+  NIX_CFLAGS_COMPILE = "-I${dbus-glib.dev}/include/dbus-1.0 -I${dbus.dev}/include/dbus-1.0";
 
   postPatch = ''
     substituteInPlace configure.ac.in --replace gio-2.0 gio-unix-2.0
   '';
 
-  buildInputs = [ gtk3 libnotify libpulseaudio libxfce4ui libxfce4util xfce4-panel xfconf ];
+  buildInputs = [
+    glib
+    gtk3
+    keybinder3
+    libnotify
+    libpulseaudio
+    libxfce4ui
+    libxfce4util
+    xfce4-panel
+    xfconf
+  ];
+
+  meta = {
+    description = "Adjust the audio volume of the PulseAudio sound system";
+  };
 }
