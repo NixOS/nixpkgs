c359c6959a114500fedc0749d280f7f971954fe6
gtgteq <59947264+gtgteq@users.noreply.github.com>
2020-02-16 06:55:35 +0900

    nixos/postgresql: Change local auth method from ident to peer (#80179)

--- nixos/modules/services/databases/postgresql.nix
+++ nixos/modules/services/databases/postgresql.nix
@@ -251,7 +251,7 @@ in
     services.postgresql.authentication = mkAfter
       ''
         # Generated file; do not edit!
-        local all all              ident
+        local all all              peer
         host  all all 127.0.0.1/32 md5
         host  all all ::1/128      md5
       '';
