deb4c21865f47027baa3856f2cacee4d0c3ee9a3
skykanin <3789764+skykanin@users.noreply.github.com>
2020-02-15 19:19:49 +0100

    maintainers: add skykanin

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6648,6 +6648,12 @@
     email = "jot.skrzyp@gmail.com";
     name = "Jakub Skrzypnik";
   };
+  skykanin = {
+    email = "skykanin@users.noreply.github.com";
+    github = "skykanin";
+    githubId = 3789764;
+    name = "skykanin";
+  };
   sleexyz = {
     email = "freshdried@gmail.com";
     github = "sleexyz";
@@ -8103,3 +8109,5 @@
     name = "Asad Saeeduddin";
   };
 }
+
+  
