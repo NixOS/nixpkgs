70f097196e11e9ed8364a616e07d11caf76b3fad
Tim Steinbach <tim@nequissimus.com>
2020-02-15 15:11:00 -0500

    linux: 4.4.213 -> 4.4.214
    (cherry picked from commit e2315d6a7efd4ee757771df8edb5e7302484536d)

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.213";
+  version = "4.4.214";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1cmwn9zvz14jqjy6qkszglhs2p5h6yh82b2269cbzvibg8y3rxq0";
+    sha256 = "0v575wl85fg9c3ksdj570hxjcl9p1dxwzag3fm0qcrq75kp6bamn";
   };
 } // (args.argsOverride or {}))
