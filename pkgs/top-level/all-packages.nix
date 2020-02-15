5c489217b76204bee960048fe9c23b07c21cac1f
gnidorah <gnidorah@users.noreply.github.com>
2020-02-15 09:06:43 +0300

    openjk: 2019-10-25 -> 2019-11-29

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23207,7 +23207,10 @@ in
 
   openclonk = callPackage ../games/openclonk { };
 
-  openjk = callPackage ../games/openjk { };
+  openjk = callPackage ../games/openjk {
+    # segfaults when built with gcc9
+    stdenv = gcc8Stdenv;
+  };
 
   openmw = libsForQt5.callPackage ../games/openmw { };
 
