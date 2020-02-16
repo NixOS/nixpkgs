cc5ba544dad6177c8daa1ecbb5184879d03e518f
koral <chahine.moreau@gmail.com>
2020-02-16 14:40:48 +0100

    mps-youtube: 0.2.8 -> unstable-2020-01-28
    Also move derivation from python-packages to all-packages,
    as this is a standalone application.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6383,8 +6383,6 @@ in {
 
   maildir-deduplicate = callPackage ../development/python-modules/maildir-deduplicate { };
 
-  mps-youtube = callPackage ../development/python-modules/mps-youtube { };
-
   d2to1 = callPackage ../development/python-modules/d2to1 { };
 
   ovh = callPackage ../development/python-modules/ovh { };
