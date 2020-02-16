eb1ffda13c1ddd6a3766461a95eda1c6949aa57b
Jos van den Oever <jos@vandenoever.info>
2020-02-16 23:47:44 +0100

    libfishsound at 1.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12438,6 +12438,8 @@ in
 
   libfilezilla = callPackage ../development/libraries/libfilezilla { };
 
+  libfishsound = callPackage ../development/libraries/libfishsound { };
+
   libfm = callPackage ../development/libraries/libfm { };
   libfm-extra = libfm.override {
     extraOnly = true;
