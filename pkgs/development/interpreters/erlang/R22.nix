126af3108fcd34c5ab6e09f07e60da3b12d572e3
Erlend Hamberg <erlend@hamberg.no>
2020-02-16 09:36:45 +0100

    erlangR22: 22.1.7 -> 22.2.6

--- pkgs/development/interpreters/erlang/R22.nix
+++ pkgs/development/interpreters/erlang/R22.nix
@@ -3,8 +3,8 @@
 # How to obtain `sha256`:
 # nix-prefetch-url --unpack https://github.com/erlang/otp/archive/OTP-${version}.tar.gz
 mkDerivation {
-  version = "22.1.7";
-  sha256 = "18aqy2s8nqd82v4lzzxknrwjva8mv1y2hvai9cakz5nkyd3vwq62";
+  version = "22.2.6";
+  sha256 = "0vvia0xl2j841zny90f3jyjhgvkv7cnrxwj553m16vxz7rvfsfh5";
 
   prePatch = ''
     substituteInPlace make/configure.in --replace '`sw_vers -productVersion`' "''${MACOSX_DEPLOYMENT_TARGET:-10.12}"
