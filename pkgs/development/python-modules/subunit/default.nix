8afa7fc855a2184c6937afa3468866bd64e0ac34
Drew Risinger <drewrisinger@users.noreply.github.com>
2020-02-18 09:34:57 -0500

    pythonPackages.subunit: (WIP) actually run tests

--- pkgs/development/python-modules/subunit/default.nix
+++ pkgs/development/python-modules/subunit/default.nix
@@ -1,7 +1,11 @@
 { buildPythonPackage
 , pkgs
-, python
 , testtools
+  # Check Inputs
+, fixtures
+, hypothesis
+, pytest
+, python
 , testscenarios
 }:
 
@@ -17,10 +21,14 @@ buildPythonPackage {
     sed -i 's/version=VERSION/version="${pkgs.subunit.version}"/' setup.py
   '';
 
-  checkInputs = [ python testscenarios ];
+  checkInputs = [ fixtures hypothesis pytest python testscenarios ];
   pythonImportsCheck = [ "subunit" ];
   checkPhase = ''
-    ${python.interpreter} all_tests.py
+    # Not very necessary. This test isn't actually currently run
+    substituteInPlace python/iso8601/test_iso8601.py --replace "import iso8601" "import subunit.iso8601 as iso8601"
+
+    ${python.interpreter} -m unittest all_tests.py # theoretically this should run all tests, but doesn't work. /bin/sh can't source paths
+    pytest --pyargs subunit.tests # This has ~30 fails, most down to some classes missing "option" attribute.
   '';
 
   meta = pkgs.subunit.meta;
