1c4c53c48ef4a062011815602d465b65771a3dcc
Robert Schütz <dev@schuetz-co.de>
2020-02-18 16:02:35 +0100

    rclone-browser: 1.2 -> 1.8.0
    https://github.com/kapitainsky/RcloneBrowser/releases/tag/1.8.0

--- pkgs/applications/networking/sync/rclone/browser.nix
+++ pkgs/applications/networking/sync/rclone/browser.nix
@@ -1,17 +1,17 @@
-{ stdenv, fetchFromGitHub, cmake, qtbase }:
+{ stdenv, fetchFromGitHub, cmake, wrapQtAppsHook, qtbase }:
 
 stdenv.mkDerivation rec {
   pname = "rclone-browser";
-  version = "1.2";
+  version = "1.8.0";
 
   src = fetchFromGitHub {
-    owner = "mmozeiko";
+    owner = "kapitainsky";
     repo = "RcloneBrowser";
     rev = version;
-    sha256 = "1ldradd5c606mfkh46y4mhcvf9kbjhamw0gahksp9w43h5dh3ir7";
+    sha256 = "14ckkdypkfyiqpnz0y2b73wh1py554iyc3gnymj4smy0kg70ai33";
   };
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [ cmake wrapQtAppsHook ];
 
   buildInputs = [ qtbase ];
 
