495300fe0b607dd6980ec909e7feadc8c816f981
Renato Alves <alves.rjc@gmail.com>
2020-02-18 12:25:59 +0100

    pythonPackages.googletrans: init at 2.4.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2735,6 +2735,8 @@ in {
 
   google-pasta = callPackage ../development/python-modules/google-pasta { };
 
+  googletrans = callPackage ../development/python-modules/googletrans { };
+
   gpapi = callPackage ../development/python-modules/gpapi { };
   gplaycli = callPackage ../development/python-modules/gplaycli { };
 
