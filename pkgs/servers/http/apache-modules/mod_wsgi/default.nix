56debabe34166f38d23b563c1c0467813e31ad81
R. RyanTM <ryantm-bot@ryantm.com>
2020-02-19 08:54:38 +0000

    mod_wsgi: 4.7.0 -> 4.7.1

--- pkgs/servers/http/apache-modules/mod_wsgi/default.nix
+++ pkgs/servers/http/apache-modules/mod_wsgi/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "mod_wsgi";
-  version = "4.7.0";
+  version = "4.7.1";
 
   src = fetchurl {
     url = "https://github.com/GrahamDumpleton/mod_wsgi/archive/${version}.tar.gz";
-    sha256 = "079f4py20jd6n3d7djak5l9j8p6hfq96lf577iir6qpfsk2p0k3n";
+    sha256 = "0dbxhrp3x689ccrhvm2lw2icmmj8i4p86z2lq3xn1zlsf43fax16";
   };
 
   buildInputs = [ apacheHttpd python ncurses ];
