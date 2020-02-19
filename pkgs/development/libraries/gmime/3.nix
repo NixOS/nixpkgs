14e9fb41ec16a25c706a15d9cba620091572d36d
R. RyanTM <ryantm-bot@ryantm.com>
2020-02-19 23:12:04 +0000

    gmime3: 3.2.5 -> 3.2.6

--- pkgs/development/libraries/gmime/3.nix
+++ pkgs/development/libraries/gmime/3.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, pkgconfig, glib, zlib, gnupg, gpgme, libidn2, libunistring, gobject-introspection }:
 
 stdenv.mkDerivation rec {
-  version = "3.2.5";
+  version = "3.2.6";
   pname = "gmime";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gmime/3.2/${pname}-${version}.tar.xz";
-    sha256 = "0ndsg1z1kq4w4caascydvialpyn4rfbjdn7xclzbzhw53x85cxgv";
+    sha256 = "05s7qjrxbj010q016pmdqdq73gz8vl4hv29kwaign0j8gi61kzxb";
   };
 
   outputs = [ "out" "dev" ];
