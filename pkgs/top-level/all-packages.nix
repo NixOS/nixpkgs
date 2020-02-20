78d5a13cf8fc6f19cfb7ea8f39e397ddbd031568
Lengyel Balazs <ikervagyok@gmail.com>
2020-02-20 13:59:21 +0100

    ucx: init at 1.7.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6007,6 +6007,8 @@ in
 
   openmpi = callPackage ../development/libraries/openmpi { };
 
+  ucx = callPackage ../development/libraries/ucx {};
+
   openmodelica = callPackage ../applications/science/misc/openmodelica { };
 
   qarte = libsForQt5.callPackage ../applications/video/qarte { };
