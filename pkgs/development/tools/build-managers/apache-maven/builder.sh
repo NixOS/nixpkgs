0141cfefbc49b475cdff57b1e8d9390f24acd309
Timo Kaufmann <timokau@zoho.com>
2020-02-21 14:26:33 +0100

    maven: make jdk overridable
    This makes it possible to use maven with different (newer) jdks.
    
    Fixes #75630

--- pkgs/development/tools/build-managers/apache-maven/builder.sh
+++ pkgs/development/tools/build-managers/apache-maven/builder.sh
@@ -5,7 +5,7 @@ unpackPhase
 mkdir -p $out/maven
 cp -r $name/* $out/maven
 
-makeWrapper $out/maven/bin/mvn $out/bin/mvn --set JAVA_HOME "$jdk"
+makeWrapper $out/maven/bin/mvn $out/bin/mvn --set-default JAVA_HOME "$jdk"
 
 # Add the maven-axis and JIRA plugin by default when using maven 1.x
 if [ -e $out/maven/bin/maven ]
