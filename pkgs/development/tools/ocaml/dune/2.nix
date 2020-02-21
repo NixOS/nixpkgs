dc774d1356f654f7861d9e511b6bb8dc7ff29206
Mario Rodas <marsam@users.noreply.github.com>
2020-02-21 04:20:00 -0500

    dune_2: 2.3.0 -> 2.3.1
    Changelog: https://github.com/ocaml/dune/releases/tag/2.3.1

--- pkgs/development/tools/ocaml/dune/2.nix
+++ pkgs/development/tools/ocaml/dune/2.nix
@@ -6,11 +6,11 @@ else
 
 stdenv.mkDerivation rec {
   pname = "dune";
-  version = "2.3.0";
+  version = "2.3.1";
 
   src = fetchurl {
     url = "https://github.com/ocaml/dune/releases/download/${version}/dune-${version}.tbz";
-    sha256 = "1zswdp2gran8djk718q5g3ldbvw0qp34j9jj1n7m1xp870g3590l";
+    sha256 = "166a6lncx0nlmn0kxgx9qkn1dnbphm30vy1wncxcpsqspxhkqfra";
   };
 
   buildInputs = [ ocaml findlib ];
