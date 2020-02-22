ec7b4dd5e846662052f6844b4d92a2be766bb172
Mario Rodas <marsam@users.noreply.github.com>
2020-02-22 04:22:00 -0500

    pythonPackages.lazr-restfulclient: init at 0.14.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -821,6 +821,8 @@ in {
 
   lazr_delegates = callPackage ../development/python-modules/lazr/delegates.nix { };
 
+  lazr-restfulclient = callPackage ../development/python-modules/lazr-restfulclient { };
+
   lazr-uri = callPackage ../development/python-modules/lazr-uri { };
 
   libmr = callPackage ../development/python-modules/libmr { };
