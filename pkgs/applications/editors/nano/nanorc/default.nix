be7973cf63dc2417fcf3e3c417ef06ea2cd82069
R. RyanTM <ryantm-bot@ryantm.com>
2020-02-22 14:07:01 +0000

    nanorc: 2018-09-05 -> 2020.1.25

--- pkgs/applications/editors/nano/nanorc/default.nix
+++ pkgs/applications/editors/nano/nanorc/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation {
   pname = "nanorc";
-  version = "2018-09-05";
+  version = "2020.1.25";
 
   src = fetchFromGitHub {
     owner = "scopatz";
@@ -21,7 +21,7 @@ stdenv.mkDerivation {
 
   meta = {
     description = "Improved Nano Syntax Highlighting Files";
-    homepage = https://github.com/scopatz/nanorc;
+    homepage = "https://github.com/scopatz/nanorc";
     license = stdenv.lib.licenses.gpl3;
     maintainers = with stdenv.lib.maintainers; [ nequissimus ];
     platforms = stdenv.lib.platforms.all;
