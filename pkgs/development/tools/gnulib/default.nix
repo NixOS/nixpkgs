dd5b624a39fbab6328ec9c7723dae64d728f46db
ahiaao <ahiaao@waifu.club>
2020-02-21 23:26:00 -0800

    gnulib: 20190811 -> 20200222
    This version includes updated files for using gettext version 0.20
    instead of 0.19

--- pkgs/development/tools/gnulib/default.nix
+++ pkgs/development/tools/gnulib/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation {
   pname = "gnulib";
-  version = "20190811";
+  version = "20200222";
 
   src = fetchgit {
     url = https://git.savannah.gnu.org/r/gnulib.git;
-    rev = "6430babe47ece6953cf18ef07c1d8642c8588e89";
-    sha256 = "14kgykbjly03dlb25sllcfcrpk7zkypa449gr3zbqv4rhpmnzizg";
+    rev = "d571f0dca66b9347251d78b4c9085fefb78eddb3";
+    sha256 = "06mn7cwhq3k1jg49gqnv1sf248mji05h0fjhnrzrfhp9n7p26axp";
   };
 
   dontFixup = true;
