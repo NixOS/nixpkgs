2bdd1f3a8d615cb706d923f2b8a8db1ac2b9278c
Benjamin Koch <bbbsnowball@gmail.com>
2020-02-23 00:09:35 +0000

    nixos/nextcloud: avoid loading imagick extension more than once
    This avoids the following error message:
      Module 'imagick' already loaded at Unknown#0
    
    (cherry picked from commit db32158bbddc218426bd4e0698af82a033ab3852)

--- nixos/modules/services/web-apps/nextcloud.nix
+++ nixos/modules/services/web-apps/nextcloud.nix
@@ -471,7 +471,7 @@ in {
         pools.nextcloud = {
           user = "nextcloud";
           group = "nginx";
-          phpOptions = phpOptionsExtensions + phpOptionsStr;
+          phpOptions = phpOptionsStr;
           phpPackage = phpPackage;
           phpEnv = {
             NEXTCLOUD_CONFIG_DIR = "${cfg.home}/config";
