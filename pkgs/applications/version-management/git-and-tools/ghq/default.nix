b974f0595f318efbb03f6d9451f3a4edd2271a63
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2020-02-23 20:19:24 +0900

    ghq: 0.12.6 -> 1.1.0

--- pkgs/applications/version-management/git-and-tools/ghq/default.nix
+++ pkgs/applications/version-management/git-and-tools/ghq/default.nix
@@ -1,17 +1,17 @@
-{ stdenv, buildGoModule, fetchFromGitHub, fetchpatch }:
+{ stdenv, buildGoModule, fetchFromGitHub }:
 
 buildGoModule rec {
   pname = "ghq";
-  version = "0.12.6";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
-    owner = "motemen";
+    owner = "x-motemen";
     repo = "ghq";
     rev = "v${version}";
-    sha256 = "14rm7fvphr7r9x0ys10vhzjwhfhhscgr574n1i1z4lzw551lrnp4";
+    sha256 = "1i0q9lxdxbyj0l0510cbkwkbycawrx8cxlbdrhb2p2fnk0vqnyiv";
   };
 
-  modSha256 = "1y2v8ir7kc2avgri06nagfyaxqr3xrg4g5pxl9rwzq9dyzm6ci5z";
+  modSha256 = "0hlbhky3c6zva9khn73n6xgq57k5p8anskxy3g2m0wzhr72cyc41";
 
   buildFlagsArray = ''
     -ldflags=
@@ -19,20 +19,13 @@ buildGoModule rec {
   '';
 
   postInstall = ''
-    install -m 444 -D ${src}/zsh/_ghq $out/share/zsh/site-functions/_ghq
+    install -m 444 -D ${src}/misc/zsh/_ghq $out/share/zsh/site-functions/_ghq
+    install -m 444 -D ${src}/misc/bash/_ghq $out/share/bash-completion/completions/_ghq
   '';
-
-  patches = [
-    (fetchpatch {
-      # remove once the commit lands in a release.
-      url = "https://github.com/motemen/ghq/commit/38ac89e60e60182b5870108f9753c9fe8d00e4a6.patch";
-      sha256 = "1z8yvzmka3sh44my6jnwc39p8zs7mczxgvwc9z0pkqk4vgvaj8gj";
-    })
-  ];
 
   meta = {
     description = "Remote repository management made easy";
-    homepage = https://github.com/motemen/ghq;
+    homepage = https://github.com/x-motemen/ghq;
     maintainers = with stdenv.lib.maintainers; [ sigma ];
     license = stdenv.lib.licenses.mit;
   };
