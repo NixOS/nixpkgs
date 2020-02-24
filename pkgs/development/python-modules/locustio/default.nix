ab3a898744630cd86bcdf7cab18228bc209e0ce0
Jonathan Ringer <jonringer117@gmail.com>
2020-02-24 00:14:17 -0800

    pythonPackages.locustio: 0.9.0 -> 0.14.4
    disable python3.8 version, as tests hang indefinitely

--- pkgs/development/python-modules/locustio/default.nix
+++ pkgs/development/python-modules/locustio/default.nix
@@ -1,17 +1,18 @@
-{ buildPythonPackage
-, fetchFromGitHub
-, mock
-, unittest2
-, msgpack
-, requests
+{ buildPythonPackage, fetchFromGitHub, isPy38
 , flask
 , gevent
+, mock
+, msgpack
 , pyzmq
+, requests
+, unittest2
 }:
 
 buildPythonPackage rec {
   pname = "locustio";
-  version = "0.9.0";
+  version = "0.14.4";
+  # tests hang on python38
+  disabled = isPy38;
 
   src = fetchFromGitHub {
     owner = "locustio";
@@ -21,10 +22,14 @@ buildPythonPackage rec {
   };
 
   propagatedBuildInputs = [ msgpack requests flask gevent pyzmq ];
-  buildInputs = [ mock unittest2 ];
+  checkInputs = [ mock unittest2 ];
+  # remove file which attempts to do GET request
+  preCheck = ''
+    rm locust/test/test_stats.py
+  '';
 
   meta = {
-    homepage = https://locust.io/;
+    homepage = "https://locust.io/";
     description = "A load testing tool";
   };
 }
