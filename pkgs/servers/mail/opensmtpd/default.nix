521c6765215271f439b9232a5ee354d7c55b79f1
Andreas Rammhold <andreas@rammhold.de>
2020-02-24 20:50:51 +0100

    opensmtpd: 6.6.3p1 -> 6.6.4p1
    Release notes aren't available at this time [1] it is likely to be
    related to a recent mail to oss-security (either [2] or [3]).
    
    [1] https://www.mail-archive.com/misc@opensmtpd.org/msg04888.html
    [2] https://www.openwall.com/lists/oss-security/2020/02/24/5
    [3] https://www.openwall.com/lists/oss-security/2020/02/24/4
    
    (cherry picked from commit 09725e5f9e896befcb7a1e292eae99c85a7a13b2)

--- pkgs/servers/mail/opensmtpd/default.nix
+++ pkgs/servers/mail/opensmtpd/default.nix
@@ -4,14 +4,14 @@
 
 stdenv.mkDerivation rec {
   pname = "opensmtpd";
-  version = "6.6.3p1";
+  version = "6.6.4p1";
 
   nativeBuildInputs = [ autoconf automake libtool bison ];
   buildInputs = [ libasr libevent zlib libressl db pam ];
 
   src = fetchurl {
     url = "https://www.opensmtpd.org/archives/${pname}-${version}.tar.gz";
-    sha256 = "1dqysjlyl0x3qzdzc9sjrla0063vpmlyq735lzf88p7wgzmw1xwy";
+    sha256 = "1kyph9ycq0j21dl9n1sq5fns9p4gckdi0fmnf8awrcwrdcm9dyg2";
   };
 
   patches = [
