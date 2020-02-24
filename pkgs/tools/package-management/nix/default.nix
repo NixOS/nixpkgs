9dc6361b4d1a3f3dbf484226eba1b1a44ad44dda
Eelco Dolstra <edolstra@gmail.com>
2020-02-24 10:52:45 +0100

    nixFlakes: 2.4pre20200207_d2032ed -> 2.4pre20200220_4a4521f
    (cherry picked from commit 39892985b00987c9bce89310b6d6b5d02d04daa5)

--- pkgs/tools/package-management/nix/default.nix
+++ pkgs/tools/package-management/nix/default.nix
@@ -197,10 +197,10 @@ in rec {
 
   nixFlakes = lib.lowPrio (callPackage common rec {
     name = "nix-2.4${suffix}";
-    suffix = "pre20200207_d2032ed";
+    suffix = "pre20200220_4a4521f";
     src = fetchurl {
-      url = "https://hydra.nixos.org/build/111815420/download/3/nix-2.4${suffix}.tar.xz";
-      sha256 = "e72a20efeee4ccc704cca3a06de9185fb8742bc7ef1a62af5896ec0f379b9ceb";
+      url = "https://hydra.nixos.org/build/113373394/download/3/nix-2.4${suffix}.tar.xz";
+      sha256 = "31fe87c40f40a590bc8f575283725d5f04ecb9aebb6b404f679d77438d75265d";
     };
 
     inherit storeDir stateDir confDir boehmgc;
