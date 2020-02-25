62d454c104a089e831da1e9cba057dbfbc2dd5ea
Shea Levy <shea@shealevy.com>
2020-02-24 19:05:42 -0500

    nix-plugins: Build against default nix.
    (cherry picked from commit 403280a516297486368583561cfaa5276f7b4668)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13428,9 +13428,7 @@ in
   nghttp2 = callPackage ../development/libraries/nghttp2 { };
   libnghttp2 = nghttp2.lib;
 
-  nix-plugins = callPackage ../development/libraries/nix-plugins {
-    nix = nixUnstable;
-  };
+  nix-plugins = callPackage ../development/libraries/nix-plugins {};
 
   nika-fonts = callPackage ../data/fonts/nika-fonts { };
 
