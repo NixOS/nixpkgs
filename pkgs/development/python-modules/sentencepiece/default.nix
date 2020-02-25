8cf1cdd2485dd3889f897f8cd7e680dd7313d593
Daniël de Kok <me@danieldk.eu>
2020-02-25 16:06:24 +0100

    sentencepiece: split into multiple outputs, optional gperftools
    I am using sentencepiece in a downstream application where I want to
    minimize the resulting closures. This commit makes changes to make
    sentencepiece a leaner dependency:
    
    - Split the outputs, so that the binaries/headers do not end up in the
      transitive closure in a library dependency.
    
    - Add the `withGPerfTools` option, which is enabled by default, to
      make it possible to disable the gperftools dependency. According to
      the sentencepiece README, this dependency gives a 10-40% performance
      improvement. But in many cases this is overshadowed by the neural
      networks that use piece identifiers as input anyway.

--- pkgs/development/python-modules/sentencepiece/default.nix
+++ pkgs/development/python-modules/sentencepiece/default.nix
@@ -6,10 +6,13 @@
 
 buildPythonPackage rec {
   pname = "sentencepiece";
-  inherit (sentencepiece) version src meta;
+  inherit (sentencepiece) version src;
 
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ sentencepiece ];
+  buildInputs = [ sentencepiece.dev ];
 
   sourceRoot = "source/python";
+
+  # sentencepiece installs 'bin' output.
+  meta = builtins.removeAttrs sentencepiece.meta [ "outputsToInstall" ];
 }
