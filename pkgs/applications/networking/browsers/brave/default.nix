a9b1721b2ce39fc8765c202e6757e622eec15d2a
Jeff Labonte <grimsleepless@protonmail.com>
2020-02-25 20:03:02 -0500

    brave: 1.3.118 -> 1.4.95
    Update brave from 0.69.128 to 1.4.95
    
    (cherry picked from commit fa166b77d19ec78d4cd01b9696a47f1118ff74f4)
    Reason: Browser should be kept up-to-date for security reasons.

--- pkgs/applications/networking/browsers/brave/default.nix
+++ pkgs/applications/networking/browsers/brave/default.nix
@@ -82,11 +82,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "brave";
-  version = "0.69.128";
+  version = "1.4.95";
 
   src = fetchurl {
     url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
-    sha256 = "1w5p2hbn14k239fbqrbxkw9h3p8wm7cdyjcyvrsss57fj00j8s4r";
+    sha256 = "1a7rk4r7phlf1y3ap3942z5sfvb6i4qglvq06qqhz49wq1wbgvq1";
   };
 
   dontConfigure = true;
