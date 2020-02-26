c094972a458179e4d752b6c12f45a635232f66d3
Jörg Thalheim <joerg@thalheim.io>
2020-02-26 15:51:51 +0000

    hass-nabucasa: 0.29 -> 0.31

--- pkgs/development/python-modules/hass-nabucasa/default.nix
+++ pkgs/development/python-modules/hass-nabucasa/default.nix
@@ -2,13 +2,13 @@
 
 buildPythonPackage rec {
   pname = "hass-nabucasa";
-  version = "0.29";
+  version = "0.31";
 
   src = fetchFromGitHub {
     owner = "nabucasa";
     repo = pname;
     rev = version;
-    sha256 = "182nh5i3hlj0kqkbynk69md0ddq83w02l8lz4m03d8xbjixzi1k1";
+    sha256 = "0hxdvdj41gq5ryafjhrcgf6y8l33lyf45a1vgwwbk0q29sir9bnr";
   };
 
   # upstreamed in https://github.com/NabuCasa/hass-nabucasa/pull/119
