7c618c70b263d58e57dad0d20a9acd7f72d7b016
Jeff Labonte <grimsleepless@protonmail.com>
2020-02-25 19:56:15 -0500

    brave: 1.3.118 -> 1.4.95

--- pkgs/applications/networking/browsers/brave/default.nix
+++ pkgs/applications/networking/browsers/brave/default.nix
@@ -82,11 +82,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "brave";
-  version = "1.1.23";
+  version = "1.4.95";
 
   src = fetchurl {
     url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
-    sha256 = "1kb40h5d76k6p338h75p8lxs0cb88jaasss0cmb7bfc7zykfqmd3";
+    sha256 = "1a7rk4r7phlf1y3ap3942z5sfvb6i4qglvq06qqhz49wq1wbgvq1";
   };
 
   dontConfigure = true;
