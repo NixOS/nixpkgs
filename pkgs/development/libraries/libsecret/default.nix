NixOS/nixpkgs

commit c6a1e23eb7337ced431644d218e4f684ddde3842

libsecret: 0.20.0 -> 0.20.1

pkgs/development/libraries/libsecret/default.nix
@@ -4,11 +4,11 @@
 
 stdenv.mkDerivation rec {
   pname = "libsecret";
-  version = "0.20.0";
+  version = "0.20.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0hxfpm8f4rlx685igd4bv89wb80v2952h373g3w6l42kniq7667i";
+    sha256 = "0ir4ynpf8b64xss1azvsi5x6697lik7hkf3z0xxa2qv2xja3xxsp";
   };
 
   patches = [

commit 35f00b429b54b01a753a16339305d14a3d9d087e

libsecret: remove obsolete patch
remove patch that was merged into the 0.20.1 patch.

regression introduced in c6a1e23eb7337ced431644d218e4f684ddde3842

pkgs/development/libraries/libsecret/default.nix
@@ -11,14 +11,6 @@ stdenv.mkDerivation rec {
     sha256 = "0ir4ynpf8b64xss1azvsi5x6697lik7hkf3z0xxa2qv2xja3xxsp";
   };
 
-  patches = [
-    (fetchpatch {
-      name = "rename-internal-functions-to-avoid-conflicts-and-fix-build.patch";
-      url = "https://gitlab.gnome.org/GNOME/libsecret/commit/cf21ad50b62f7c8e4b22ef374f0a73290a99bdb8.patch";
-      sha256 = "1n9nyzq5qrvw7s6sj5gzj33ia3rrx719jpys1cfhfbayg2sxyd4n";
-    })
-  ];
-
   postPatch = ''
     patchShebangs .
   '';