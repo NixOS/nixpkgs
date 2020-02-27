6a6bf70e48680d23786e338806f940838e742394
Vincent Breitmoser <look@my.amazin.horse>
2020-02-27 16:19:40 +0100

    sphinxsearch: add workaround for darwin build failures
    upstream bug: see http://sphinxsearch.com/bugs/view.php?id=2578
    fix ported from Homebrew: https://github.com/Homebrew/homebrew-core/pull/8619

--- pkgs/servers/search/sphinxsearch/default.nix
+++ pkgs/servers/search/sphinxsearch/default.nix
@@ -31,6 +31,14 @@ stdenv.mkDerivation rec {
     expat
   ];
 
+  CXXFLAGS = with stdenv.lib; concatStringsSep " " (optionals stdenv.isDarwin [
+    # see upstream bug: http://sphinxsearch.com/bugs/view.php?id=2578
+    # workaround for "error: invalid suffix on literal
+    "-Wno-reserved-user-defined-literal"
+    # workaround for "error: non-constant-expression cannot be narrowed from type 'long' to 'int'"
+    "-Wno-c++11-narrowing"
+  ]);
+
   meta = {
     description = "An open source full text search server";
     homepage    = http://sphinxsearch.com;
