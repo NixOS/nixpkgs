5b28292c807659f0b6642b8475c1116d34e676a4
Jaka Hudoklin <jaka@x-truder.net>
2020-02-28 12:52:27 +0700

    modules/hyperv-guest: improvements
    - option to enable xrdp for enchanched session
    - option to enable x11
    - add hyperv_fb driver for xserver
    - add missing sd_mod and sr_mod kernel modules
    - load xrdp pulseaudio module on enchanched session mode

--- nixos/modules/services/networking/xrdp.nix
+++ nixos/modules/services/networking/xrdp.nix
@@ -73,7 +73,7 @@ in {
       };
 
       port = mkOption {
-        type = types.int;
+        type = types.either types.int types.str;
         default = 3389;
         description = ''
           Specifies on which port the xrdp daemon listens.
