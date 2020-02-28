4f50bee7c09aa5514f5df9d33dcc65511f7b3905
Michael Weiss <dev.primeos@gmail.com>
2020-02-28 20:55:43 +0100

    androidStudioPackages.{dev,canary}: 4.0.0.10 -> 4.1.0.1

--- pkgs/applications/editors/android-studio/default.nix
+++ pkgs/applications/editors/android-studio/default.nix
@@ -12,11 +12,15 @@ let
     build = "192.6200805";
     sha256Hash = "1rf79fh6fbaxsj26q9bgl4vvmakv4wc0amjz026cm89hcwwzrb4d";
   };
-  betaVersion = latestVersion;
-  latestVersion = { # canary & dev
+  betaVersion = {
     version = "4.0.0.10"; # "Android Studio 4.0 Beta 1"
     build = "193.6220182";
     sha256Hash = "0ibp54wcss4ihm454hbavv1bhar6cd4alp5b0z248ryjr5w9mixf";
+  };
+  latestVersion = { # canary & dev
+    version = "4.1.0.1"; # "Android Studio 4.1 Canary 1"
+    build = "193.6224510";
+    sha256Hash = "0misff7xx8jcg4zr5ahc8qdwvlkx605il0shzd9i1cm9v1br3sqx";
   };
 in {
   # Attributes are named by their corresponding release channels
