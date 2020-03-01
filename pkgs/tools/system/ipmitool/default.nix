a5e230448b05d41c473908d11d6c33f2529455df
worldofpeace <worldofpeace@protonmail.ch>
2020-03-01 08:18:36 -0500

    ipmitool: fix compile on darwin
    (cherry picked from commit 82217553b0e5926bb8f178a31b5511dc0fe895f1)

--- pkgs/tools/system/ipmitool/default.nix
+++ pkgs/tools/system/ipmitool/default.nix
@@ -17,6 +17,11 @@ stdenv.mkDerivation {
       url = "https://sources.debian.org/data/main/i/ipmitool/1.8.18-6/debian/patches/0120-openssl1.1.patch";
       sha256 = "1xvsjxb782lzy72bnqqnsk3r5h4zl3na95s4pqn2qg7cic2mnbfk";
     })
+    # Fix build on non-linux systems
+    (fetchpatch {
+      url = "https://github.com/ipmitool/ipmitool/commit/5db314f694f75c575cd7c9ffe9ee57aaf3a88866.patch";
+      sha256 = "01niwrgajhrdhl441gzmw6v1r1yc3i8kn98db4b6smfn5fwdp1pa";
+    })
   ];
 
   buildInputs = [ openssl ];
@@ -39,7 +44,7 @@ stdenv.mkDerivation {
     description = ''Command-line interface to IPMI-enabled devices'';
     license = licenses.bsd3;
     homepage = https://sourceforge.net/projects/ipmitool/;
-    platforms = platforms.linux;
+    platforms = platforms.unix;
     maintainers = with maintainers; [ fpletz ];
   };
 }
