cb424d6a689c2f2628c70e4cc58b7e3e71797c9f
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-01 13:37:00 +0000

    libamqpcpp: 4.1.5 -> 4.1.6

--- pkgs/development/libraries/libamqpcpp/default.nix
+++ pkgs/development/libraries/libamqpcpp/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libamqpcpp";
-  version = "4.1.5";
+  version = "4.1.6";
 
   src = fetchFromGitHub {
     owner = "CopernicaMarketingSoftware";
     repo = "AMQP-CPP";
     rev = "v${version}";
-    sha256 = "1jyvpa68wymnn8wnh8i59pa9kdbixv86mh8q338agnwkmzvlgm58";
+    sha256 = "0cw1apj4qxfxpp0gz4my4656d5ijjj6s2y4rjahhj67h10qj4bd2";
   };
 
   buildInputs = [ openssl ];
@@ -21,7 +21,7 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Library for communicating with a RabbitMQ server";
-    homepage = https://github.com/CopernicaMarketingSoftware/AMQP-CPP;
+    homepage = "https://github.com/CopernicaMarketingSoftware/AMQP-CPP";
     license = licenses.asl20;
     maintainers = [ maintainers.mjp ];
     platforms = platforms.all;
