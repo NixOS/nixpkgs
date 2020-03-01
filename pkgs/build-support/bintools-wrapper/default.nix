b62e87a456aa0862d1bb302535730884ebc67b66
Ashish SHUKLA <ashish.is@lostca.se>
2020-03-02 00:52:16 +0530

    stdenv: Improve/fix FreeBSD support
    Able to bootstrap stdenv on FreeBSD by compiling various dependencies
    using built-in FreeBSD tools so mostly works now

--- pkgs/build-support/bintools-wrapper/default.nix
+++ pkgs/build-support/bintools-wrapper/default.nix
@@ -62,6 +62,7 @@ let
     else if targetPlatform.system == "powerpc-linux"  then "${libc_lib}/lib/ld.so.1"
     else if targetPlatform.isMips                     then "${libc_lib}/lib/ld.so.1"
     else if targetPlatform.isDarwin                   then "/usr/lib/dyld"
+    else if targetPlatform.isFreeBSD                  then "/libexec/ld-elf.so.1"
     else if stdenv.lib.hasSuffix "pc-gnu" targetPlatform.config then "ld.so.1"
     else null;
 
