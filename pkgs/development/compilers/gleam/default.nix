d6d9c4bd36704ae8c46b203e83758f0469f1c8b9
Norbert Melzer <timmelzer@gmail.com>
2020-03-02 11:06:12 +0100

    gleam: 0.6.0 -> 0.7.0

--- pkgs/development/compilers/gleam/default.nix
+++ pkgs/development/compilers/gleam/default.nix
@@ -1,19 +1,19 @@
-{ stdenv, rustPlatform, fetchFromGitHub, Security }:
+{pkgs,  stdenv, rustPlatform, fetchFromGitHub, Security }:
 
 rustPlatform.buildRustPackage rec {
   pname = "gleam";
-  version = "0.6.0";
+  version = "0.7.0";
 
   src = fetchFromGitHub {
     owner = "gleam-lang";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1fvy2j6pw1rwm0rg7555q3qg2069cx2b9lk0nsyc3jxsqp9hbn6i";
+    sha256 = "1l9fdghjgyfc0dxqy3jj0hy7v325ypfj5qd1s4ns0kvy4zn0dzp7";
   };
 
   buildInputs = stdenv.lib.optionals stdenv.isDarwin [ Security ];
 
-  cargoSha256 = "1vhra9v4jsy9hyhjrmxjy6rnraxz13h1b7l51xvbai5wxsxm7z8m";
+  cargoSha256 = "0xsna9g5cinkwa3x571rmakc2i68ggwg151rfq8c07wwgp536129";
 
   meta = with stdenv.lib; {
     description = "A statically typed language for the Erlang VM";
