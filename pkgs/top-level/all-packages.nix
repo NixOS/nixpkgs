2338b394da051e335b820eeceea6d555fdd93caf
Tor Hedin Brønner <torhedinbronner@gmail.com>
2020-03-02 14:38:49 +0100

    pipewire: Add back 0.2.7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9629,6 +9629,7 @@ in
   pipenv = callPackage ../development/tools/pipenv {};
 
   pipewire = callPackage ../development/libraries/pipewire {};
+  pipewire_0_2 = callPackage ../development/libraries/pipewire/2.nix {};
 
   pyradio = callPackage ../applications/radio/pyradio {};
 
