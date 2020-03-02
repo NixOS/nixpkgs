7546b5cab7fe908ee10b17c85126b8a8faefa887
talyz <kim.lindberger@gmail.com>
2020-03-02 21:21:35 +0100

    gitaly: 1.83.0 -> 12.8.1

--- pkgs/applications/version-management/gitlab/gitaly/default.nix
+++ pkgs/applications/version-management/gitlab/gitaly/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, fetchFromGitLab, buildGoPackage, ruby, bundlerEnv, pkgconfig, libgit2 }:
+{ stdenv, fetchFromGitLab, fetchFromGitHub, buildGoPackage, ruby,
+  bundlerEnv, pkgconfig, libgit2 }:
 
 let
   rubyEnv = bundlerEnv rec {
@@ -17,14 +18,14 @@ let
       };
   };
 in buildGoPackage rec {
-  version = "1.83.0";
+  version = "12.8.1";
   pname = "gitaly";
 
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitaly";
     rev = "v${version}";
-    sha256 = "1vwa38mhnxyncrrvp45d8s6fg94xaq8c71d7qh9ip77db0ak45kh";
+    sha256 = "0sjkh0j36dpakqmq7l5gd1ydmx1kxgij53bjvvn37r19liqdijnx";
   };
 
   # Fix a check which assumes that hook files are writeable by their
