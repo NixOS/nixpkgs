79076235ebf1a14b3435bc4d02e5df995a75c375
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-02 23:31:15 +0000

    python27Packages.authlib: 0.13 -> 0.14.3
    Rationale for backport: only used by `matrix-synapse` atm which requires
    0.14 to work.
    
    (cherry picked from commit c37b4466c0fca907ba2d62a2e7c85402aa8f4800)
    (cherry picked from commit 7c6a40c812e54ce83601ab1eb278c7e9ced22c81)

--- pkgs/development/python-modules/authlib/default.nix
+++ pkgs/development/python-modules/authlib/default.nix
@@ -8,14 +8,14 @@
 }:
 
 buildPythonPackage rec {
-  version = "0.13";
+  version = "0.14.3";
   pname = "authlib";
 
   src = fetchFromGitHub {
     owner = "lepture";
     repo = "authlib";
     rev = "v${version}";
-    sha256 = "1nv0jbsaqr9qjn7nnl55s42iyx655k7fsj8hs69652lqnfn5y3d5";
+    sha256 = "0ph97j94i40jj7nc5ya8pfq0ccx023zbqpcs5hrxmib53g64k5xy";
   };
 
   propagatedBuildInputs = [ cryptography requests ];
@@ -27,7 +27,7 @@ buildPythonPackage rec {
   '';
 
   meta = with stdenv.lib; {
-    homepage = https://github.com/lepture/authlib;
+    homepage = "https://github.com/lepture/authlib";
     description = "The ultimate Python library in building OAuth and OpenID Connect servers. JWS,JWE,JWK,JWA,JWT included.";
     maintainers = with maintainers; [ flokli ];
     license = licenses.bsd3;
