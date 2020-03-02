eea5f48dd5a0bae431e4daec21e1ef51d769fb7d
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-02 12:47:10 +0000

    marvin: 20.4.0 -> 20.5.0

--- pkgs/applications/science/chemistry/marvin/default.nix
+++ pkgs/applications/science/chemistry/marvin/default.nix
@@ -4,12 +4,12 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   pname = "marvin";
-  version = "20.4.0";
+  version = "20.5.0";
 
   src = fetchurl {
     name = "marvin-${version}.deb";
     url = "http://dl.chemaxon.com/marvin/${version}/marvin_linux_${versions.majorMinor version}.deb";
-    sha256 = "12kygxq24in7hbp7shkx1baqig8rwmzvv0d3kc3ld9sj9hb0a2n1";
+    sha256 = "0hv4jwjb2ihr2h7099ymd70qypci4spxxijwqkn5xcaf5vczzgh3";
   };
 
   nativeBuildInputs = [ dpkg makeWrapper ];
@@ -40,7 +40,7 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "A chemical modelling, analysis and structure drawing program";
-    homepage = https://chemaxon.com/products/marvin;
+    homepage = "https://chemaxon.com/products/marvin";
     maintainers = with maintainers; [ fusion809 ];
     license = licenses.unfree;
     platforms = platforms.linux;
