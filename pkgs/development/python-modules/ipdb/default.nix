6e686dd6817b3f79517616a780b03d83e64dc47d
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-03 07:23:48 +0000

    python27Packages.ipdb: 0.12.3 -> 0.13.0

--- pkgs/development/python-modules/ipdb/default.nix
+++ pkgs/development/python-modules/ipdb/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "ipdb";
-  version = "0.12.3";
+  version = "0.13.0";
   disabled = isPyPy;  # setupterm: could not find terminfo database
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1zbj7xjhkr44grfyv1hb7ff5n1218f9jjabgzica29vh7c74m6jx";
+    sha256 = "0nbs9m2pqg4j10m7c31vyb8h7wy29d9s8kiv0k2igbr821k1y3xr";
   };
 
   propagatedBuildInputs = [ ipython ];
@@ -22,7 +22,7 @@ buildPythonPackage rec {
   '';
 
   meta = with stdenv.lib; {
-    homepage = https://github.com/gotcha/ipdb;
+    homepage = "https://github.com/gotcha/ipdb";
     description = "IPython-enabled pdb";
     license = licenses.bsd0;
     maintainers = [ maintainers.costrouc ];
