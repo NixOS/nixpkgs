eebd6fe75a2306469ed5554975b11246f3ce3e56
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2020-03-03 10:54:23 +0100

    octopus: 9.1 -> 9.2

--- pkgs/applications/science/chemistry/octopus/default.nix
+++ pkgs/applications/science/chemistry/octopus/default.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation rec {
   pname = "octopus";
-  version = "9.1";
+  version = "9.2";
 
   src = fetchFromGitLab {
     owner = "octopus-code";
     repo = "octopus";
     rev = version;
-    sha256 = "1l5fqgllk7rij16q7a3la7qq6isy8a5n37vk400qcscw1v32s90h";
+    sha256 = "083z51sjv70asr04rv53wb9gf4396nblq1zl22qw7jdr28hji4is";
   };
 
   nativeBuildInputs = [ perl procps autoreconfHook ];
