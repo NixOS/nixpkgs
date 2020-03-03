a0fcb7140f2d558ea59e7c1c7e9801062377b5d5
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-03 04:42:49 +0000

    python27Packages.apptools: 4.4.0 -> 4.5.0

--- pkgs/development/python-modules/apptools/default.nix
+++ pkgs/development/python-modules/apptools/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "apptools";
-  version = "4.4.0";
+  version = "4.5.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1dw6vvq7lqkj7mgn3s7r5hs937kl4mj5g7jf2qgvhdld9lsc5xbk";
+    sha256 = "10h52ibhr2aw076pivqxiajr9rpcr1mancg6xlpxzckcm3if02i6";
   };
 
   propagatedBuildInputs = [ traits traitsui configobj ];
@@ -24,7 +24,7 @@ buildPythonPackage rec {
 
   meta = with stdenv.lib; {
     description = "Set of packages that Enthought has found useful in creating a number of applications.";
-    homepage = https://github.com/enthought/apptools;
+    homepage = "https://github.com/enthought/apptools";
     maintainers = with stdenv.lib.maintainers; [ knedlsepp ];
     license = licenses.bsdOriginal;
   };
