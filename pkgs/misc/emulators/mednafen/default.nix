3867188af72d2317c900d75642b0d4ffe0d25b2c
AndersonTorres <torres.anderson.85@protonmail.com>
2020-03-03 09:14:07 -0300

    mednafen: 0.9.48 -> 1.22.2

--- pkgs/misc/emulators/mednafen/default.nix
+++ pkgs/misc/emulators/mednafen/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, pkgconfig, freeglut, libGLU, libGL, libcdio, libjack2
-, libsamplerate, libsndfile, libX11, SDL, SDL_net, zlib }:
+, libsamplerate, libsndfile, libX11, SDL2, SDL2_net, zlib }:
 
 stdenv.mkDerivation rec {
   pname = "mednafen";
-  version = "0.9.48";
+  version = "1.22.2";
 
   src = fetchurl {
     url = "https://mednafen.github.io/releases/files/${pname}-${version}.tar.xz";
-    sha256 = "00i12mywhp43274aq466fwavglk5b7d8z8bfdna12ra9iy1hrk6k";
+    sha256 = "159gvzrf4as1fp74czzc14vamhd6s3hlnvwglfgdd5j6d6n37m7s";
   };
 
   nativeBuildInputs = [ pkgconfig ];
@@ -20,8 +20,8 @@ stdenv.mkDerivation rec {
     libsamplerate
     libsndfile
     libX11
-    SDL
-    SDL_net
+    SDL2
+    SDL2_net
     zlib
   ];
 
@@ -34,7 +34,38 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "A portable, CLI-driven, SDL+OpenGL-based, multi-system emulator";
-    homepage = https://mednafen.github.io/;
+    longDescription = ''
+      Mednafen is a portable, utilizing OpenGL and SDL,
+      argument(command-line)-driven multi-system emulator. Mednafen has the
+      ability to remap hotkey functions and virtual system inputs to a keyboard,
+      a joystick, or both simultaneously. Save states are supported, as is
+      real-time game rewinding. Screen snapshots may be taken, in the PNG file
+      format, at the press of a button. Mednafen can record audiovisual movies
+      in the QuickTime file format, with several different lossless codecs
+      supported.
+
+      The following systems are supported (refer to the emulation module
+      documentation for more details):
+
+      - Apple II/II+
+      - Atari Lynx
+      - Neo Geo Pocket (Color)
+      - WonderSwan
+      - GameBoy (Color)
+      - GameBoy Advance
+      - Nintendo Entertainment System
+      - Super Nintendo Entertainment System/Super Famicom
+      - Virtual Boy
+      - PC Engine/TurboGrafx 16 (CD)
+      - SuperGrafx
+      - PC-FX
+      - Sega Game Gear
+      - Sega Genesis/Megadrive
+      - Sega Master System
+      - Sega Saturn (experimental, x86_64 only)
+      - Sony PlayStation
+    '';
+    homepage = "https://mednafen.github.io/";
     license = licenses.gpl2;
     maintainers = with maintainers; [ AndersonTorres ];
     platforms = platforms.linux;
