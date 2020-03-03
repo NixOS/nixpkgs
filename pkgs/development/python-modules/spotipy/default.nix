6212e0aa152ac8483170964eba07cbaa26d526cd
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-03 13:09:48 +0000

    python27Packages.spotipy: 2.7.1 -> 2.9.0

--- pkgs/development/python-modules/spotipy/default.nix
+++ pkgs/development/python-modules/spotipy/default.nix
@@ -2,17 +2,17 @@
 
 buildPythonPackage rec {
   pname = "spotipy";
-  version = "2.7.1";
+  version = "2.9.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1i4gpmvjk608fxz1kwfb3dnmm4dydr0bir0zw9k2nng7n8b6knvr";
+    sha256 = "163z3j0sd9a7cc9pv9hcrh230gisvvi2fxabh1f6nzhfr8avrncr";
   };
 
   propagatedBuildInputs = [ requests ];
 
   meta = with stdenv.lib; {
-    homepage = https://spotipy.readthedocs.org/;
+    homepage = "https://spotipy.readthedocs.org/";
     description = "A light weight Python library for the Spotify Web API";
     license = licenses.mit;
     maintainers = [ maintainers.rvolosatovs ];
