2410dbb3c742a396e7eb2bfb8c83dd8a0fa5da06
Michiel Leenaars <ml.software@leenaa.rs>
2020-03-03 10:56:00 +0100

    Unbound: 1.9.5 -> 1.10.0

--- pkgs/tools/networking/unbound/default.nix
+++ pkgs/tools/networking/unbound/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "unbound";
-  version = "1.9.5";
+  version = "1.10.0";
 
   src = fetchurl {
     url = "https://unbound.net/downloads/${pname}-${version}.tar.gz";
-    sha256 = "0myv8l886gmlh9nh4j3q5549idxnl51hf9cw20yxfqbwd47l13ca";
+    sha256 = "0mg9divpysr42sp0m693a70693dp8025v6c9dv1yabr4g1jlhbqm";
   };
 
   # https://github.com/NLnetLabs/unbound/pull/90
