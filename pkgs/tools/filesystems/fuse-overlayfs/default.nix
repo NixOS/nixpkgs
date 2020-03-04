4f2e31a1318ddb08a6f1d788e5e2460ced26b118
zowoq <59103226+zowoq@users.noreply.github.com>
2020-03-04 18:56:31 +1000

    fuse-overlayfs: 0.7.6 -> 0.7.7
    https://github.com/containers/fuse-overlayfs/releases/tag/v0.7.7

--- pkgs/tools/filesystems/fuse-overlayfs/default.nix
+++ pkgs/tools/filesystems/fuse-overlayfs/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "fuse-overlayfs";
-  version = "0.7.6";
+  version = "0.7.7";
 
   src = fetchFromGitHub {
     owner = "containers";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1sgl6npbhg49aqlxmm3bnk3cmi9xpg8i5m4hickqfk1ni1z070ij";
+    sha256 = "0fifqa4f79lhklajsf1aiipc4cyykscqlns9pzbcq5hdphi9fbsb";
   };
 
   nativeBuildInputs = [ autoreconfHook pkgconfig ];
