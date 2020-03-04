3e833b26a9c5540c543afd916a8a3e204ccb3def
Emery Hemingway <ehmry@posteo.net>
2020-03-04 13:56:37 +0100

    aespipe: install the included bz2aespipe wrapper

--- pkgs/tools/security/aespipe/default.nix
+++ pkgs/tools/security/aespipe/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, sharutils, makeWrapper }:
 
 stdenv.mkDerivation rec {
   pname = "aespipe";
@@ -9,11 +9,19 @@ stdenv.mkDerivation rec {
     sha256 = "15pg9j27mjzl78mpzkdqd84kdafj0g6j72f8wgjrpp2qkxjy2ddi";
   };
 
+  nativeBuildInputs = [ makeWrapper ];
+
   configureFlags = [ "--enable-padlock" "--enable-intelaes" ];
+
+  postInstall = ''
+    cp bz2aespipe $out/bin
+    wrapProgram $out/bin/bz2aespipe \
+     --prefix PATH : $out/bin:${stdenv.lib.makeBinPath [ sharutils ]}
+  '';
 
   meta = with stdenv.lib; {
     description = "AES encrypting or decrypting pipe";
-    homepage = http://loop-aes.sourceforge.net/aespipe.README;
+    homepage = "http://loop-aes.sourceforge.net/aespipe.README";
     license = licenses.gpl2;
     maintainers = [ maintainers.goibhniu ];
     platforms = platforms.linux;
