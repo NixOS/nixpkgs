2113013ca6a4ce9e49e2bc44530652a7a2c5e1aa
Mario Rodas <marsam@users.noreply.github.com>
2020-03-04 13:00:00 -0500

    nodejs-13_x: 13.9.0 -> 13.10.0
    Changelog: https://github.com/nodejs/node/releases/tag/v13.10.0

--- pkgs/development/web/nodejs/v13.nix
+++ pkgs/development/web/nodejs/v13.nix
@@ -5,6 +5,6 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "13.9.0";
-    sha256 = "0z68hj3z2y8aj4bc14h77mj5l99jb4ljjc10gp0dpg8s4g1x5xzw";
+    version = "13.10.0";
+    sha256 = "11m8sisi3dmr70fpnb7xi6nljil3rm36liz0wfzd7kgxmv6p9mhj";
   }
