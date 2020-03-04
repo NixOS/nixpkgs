61b61ed08765eb6ef8e64281ff38d13345a55687
Ben Darwin <bcdarwin@gmail.com>
2020-03-04 17:01:18 -0500

    python3Packages.batchgenerators: init at 0.19.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1803,6 +1803,8 @@ in {
 
   base58 = callPackage ../development/python-modules/base58 {};
 
+  batchgenerators = callPackage ../development/python-modules/batchgenerators { };
+
   batinfo = callPackage ../development/python-modules/batinfo {};
 
   bcdoc = callPackage ../development/python-modules/bcdoc {};
