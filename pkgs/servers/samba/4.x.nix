ae29c52db5a883136a2d5ff63c5861ce637d8aa8
Pascal Bach <pascal.bach@nextrem.ch>
2020-03-05 23:55:34 +0100

    samba: 4.11.5 -> 4.12.0
    - add support for liburing on linux
    - remove backported patch
    - move native build dependencies to nativeBuildInputs

--- pkgs/servers/samba/4.x.nix
+++ pkgs/servers/samba/4.x.nix
@@ -1,8 +1,8 @@
-{ lib, stdenv, fetchurl, fetchpatch, python, pkgconfig, perl, libxslt, docbook_xsl, rpcgen
+{ lib, stdenv, fetchurl, python, pkgconfig, perl, libxslt, docbook_xsl, rpcgen
 , fixDarwinDylibNames
 , docbook_xml_dtd_42, readline
 , popt, iniparser, libbsd, libarchive, libiconv, gettext
-, krb5Full, zlib, openldap, cups, pam, avahi, acl, libaio, fam, libceph, glusterfs
+, krb5Full, zlib, openldap, cups, pam, avahi, acl, libaio, liburing, fam, libceph, glusterfs
 , gnutls, ncurses, libunwind, systemd, jansson, lmdb, gpgme, libuuid
 
 , enableLDAP ? false
@@ -20,11 +20,11 @@ with lib;
 
 stdenv.mkDerivation rec {
   pname = "samba";
-  version = "4.11.5";
+  version = "4.12.0";
 
   src = fetchurl {
     url = "mirror://samba/pub/samba/stable/${pname}-${version}.tar.gz";
-    sha256 = "0gyr773dl0krcra6pvyp8i9adj3r16ihrrm2b71c0974cbzrkqpk";
+    sha256 = "1zk5jqnkifkfi6ssn02bh2ih7vyw2nsr0angsd6kyg3xaq5bgh3f";
   };
 
   outputs = [ "out" "dev" "man" ];
@@ -34,20 +34,15 @@ stdenv.mkDerivation rec {
     ./patch-source3__libads__kerberos_keytab.c.patch
     ./4.x-no-persistent-install-dynconfig.patch
     ./4.x-fix-makeflags-parsing.patch
-    (fetchpatch {
-      name = "test-oLschema2ldif-fmemopen.patch";
-      url = "https://gitlab.com/samba-team/samba/commit/5e517e57c9d4d35e1042a49d3592652b05f0c45b.patch";
-      sha256 = "1bbldf794svsdvcbp649imghmj0jck7545d3k9xs953qkkgwkbxi";
-    })
   ];
 
-  nativeBuildInputs = optionals stdenv.isDarwin [ rpcgen fixDarwinDylibNames ];
+  nativeBuildInputs = [ pkgconfig perl perl.pkgs.ParseYapp libxslt docbook_xsl docbook_xml_dtd_42 ]
+  ++ optionals stdenv.isDarwin [ rpcgen fixDarwinDylibNames ];
 
   buildInputs = [
-    python pkgconfig perl libxslt docbook_xsl docbook_xml_dtd_42 /*
-    docbook_xml_dtd_45 */ readline popt iniparser jansson
+    python readline popt iniparser jansson
     libbsd libarchive zlib fam libiconv gettext libunwind krb5Full gnutls
-  ] ++ optionals stdenv.isLinux [ libaio systemd ]
+  ] ++ optionals stdenv.isLinux [ libaio liburing systemd ]
     ++ optional enableLDAP openldap
     ++ optional (enablePrinting && stdenv.isLinux) cups
     ++ optional enableMDNS avahi
