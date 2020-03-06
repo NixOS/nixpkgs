ac561f5b5d987aaaf30ac5744209cbd3a9d56ae5
Peter Simons <simons@cryp.to>
2020-03-06 13:48:20 +0100

    spago: disable Hydra build while its marked broken

--- pkgs/development/tools/purescript/spago/spago.nix
+++ pkgs/development/tools/purescript/spago/spago.nix
@@ -45,4 +45,5 @@ mkDerivation {
   homepage = "https://github.com/purescript/spago#readme";
   license = stdenv.lib.licenses.bsd3;
   broken = true;   # Build is broken in lts-15.x.
+  hydraPlatforms = stdenv.lib.platforms.none;
 }
