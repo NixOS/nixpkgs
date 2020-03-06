9336b15a88c01892278bd179907a08a3d11c21d6
Izorkin <izorkin@elven.pw>
2020-03-06 12:16:33 +0300

    libvirt: 5.4.0 -> 5.9.0

--- pkgs/development/python-modules/libvirt/5.9.0.nix
+++ pkgs/development/python-modules/libvirt/5.9.0.nix
@@ -2,12 +2,12 @@
 
 buildPythonPackage rec {
   pname = "libvirt";
-  version = "5.4.0";
+  version = "5.9.0";
 
   src = fetchgit {
     url = git://libvirt.org/libvirt-python.git;
     rev = "v${version}";
-    sha256 = "0ja35z90i3m7vsjfpzfm7awkmja3h0150376i5pzmf2q8vp61fi5";
+    sha256 = "0qvr0s7yasswy1s5cvkm91iifk33pb8s7nbb38zznc46706b358r";
   };
 
   nativeBuildInputs = [ pkgconfig ];
