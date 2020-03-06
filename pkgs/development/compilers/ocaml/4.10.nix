14aacd5e72c24caf040e2eb7f7697b75ae988778
Vincent Laporte <Vincent.Laporte@gmail.com>
2020-03-06 21:39:06 +0100

    ocaml-ng.ocamlPackages_4_10.ocaml: 4.10.0+rc1 → 4.10.0+rc2
    (cherry picked from commit ca6391a7c4abb4497aed1eeb5c0afbe32a822dd4)

--- pkgs/development/compilers/ocaml/4.10.nix
+++ pkgs/development/compilers/ocaml/4.10.nix
@@ -1,6 +1,6 @@
 import ./generic.nix {
   major_version = "4";
   minor_version = "10";
-  patch_version = "0+rc1";
-  sha256 = "1nzmn9wszixsyzz4bhpwqw8dx0m1iy83xmanp4g9f5dfywgcss2c";
+  patch_version = "0+rc2";
+  sha256 = "1iv8x9xr4k2s1x1p4rj4fqdh2iwzhgi56lyshvh6gg224i14rkbz";
 }
