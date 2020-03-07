d95b166336edf6bea4626d9bf0c700edc76a5091
be7a <bela.stoyan@gmail.com>
2020-03-07 18:07:03 +0100

    Fix GSettings schema bug

--- pkgs/applications/blockchains/sia-ui/default.nix
+++ pkgs/applications/blockchains/sia-ui/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, makeDesktopItem, appimageTools }:
+{ stdenv, fetchurl, makeDesktopItem, appimageTools, gtk3 }:
 
 let
   pname = "sia-ui";
@@ -10,9 +10,14 @@ let
     sha256 = "3ab7905f9075dd6a80953d1427bbc99ce1fc48f60d135bcfb136dcd267b11671";
   };
 
+  xdg_dirs = builtins.concatStringsSep ":" [
+    "${gtk3}/share/gsettings-schemas/${gtk3.name}"
+  ];
+
   appimageContents = appimageTools.extractType2 {
     inherit name src;
   };
+
 in appimageTools.wrapType2 rec {
   inherit name src;
 
@@ -22,6 +27,10 @@ in appimageTools.wrapType2 rec {
     install -m 444 -D ${appimageContents}/sia-ui.png $out/share/icons/hicolor/512x512/apps/sia-ui.png
     substituteInPlace $out/share/applications/sia-ui.desktop \
       --replace 'Exec=AppRun' 'Exec=${pname}'
+  '';
+
+   profile = ''
+    export XDG_DATA_DIRS="${xdg_dirs}''${XDG_DATA_DIRS:+:"''$XDG_DATA_DIRS"}"
   '';
 
   meta = with stdenv.lib; {
