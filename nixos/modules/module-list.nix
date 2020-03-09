acba458b7ed7f64336081dd2a29856be9cb8de00
Matt Snider <matt.snider@protonmail.com>
2020-03-09 22:08:44 +0100

    nixos/ankisyncd: init at 2.1.0

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -405,6 +405,7 @@
   ./services/mail/sympa.nix
   ./services/mail/nullmailer.nix
   ./services/misc/airsonic.nix
+  ./services/misc/ankisyncd.nix
   ./services/misc/apache-kafka.nix
   ./services/misc/autofs.nix
   ./services/misc/autorandr.nix
