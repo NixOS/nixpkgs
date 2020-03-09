f7618a27fccfcf692ee173c25b6c2b3df43c35b5
Bignaux Ronan <ronan@aimao.org>
2020-03-09 22:05:04 +0100

    pysolfc: 2.6.4 -> 2.8.0

--- pkgs/games/pysolfc/default.nix
+++ pkgs/games/pysolfc/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchzip, buildPythonApplication, python3Packages
-  , desktop-file-utils, freecell-solver }:
+, freecell-solver }:
 
 buildPythonApplication rec {
   pname = "PySolFC";
-  version = "2.6.4";
+  version = "2.8.0";
 
   src = fetchzip {
     url = "https://versaweb.dl.sourceforge.net/project/pysolfc/PySolFC/PySolFC-${version}/PySolFC-${version}.tar.xz";
-    sha256 = "1bd84law5b1yga3pryggdvlfvm0l62gci2q8y3q79cysdk3z4w3z";
+    sha256 = "01j7lyp7hbybmvph2ww67a6jp455a5ln7pzbs9d1762r323yz5sy";
   };
 
   cardsets = fetchzip {
@@ -15,29 +15,22 @@ buildPythonApplication rec {
     sha256 = "0h0fibjv47j8lkc1bwnlbbvrx2nr3l2hzv717kcgagwhc7v2mrqh";
   };
 
+  postPatch = ''
+    sed -i s:/usr/share/PySolFC:$out/share/PySolFC: pysollib/settings.py
+  '';
+
+  dontUseSetuptoolsCheck = true;
+
   propagatedBuildInputs = with python3Packages; [
-    tkinter six random2
-    # optional :
+    attrs configobj six random2 pysol_cards pycotap tkinter
+    #  optional :
     pygame freecell-solver pillow
   ];
-
-  patches = [
-    ./pysolfc-datadir.patch
-  ];
-
-  nativeBuildInputs = [ desktop-file-utils ];
-  postPatch = ''
-    desktop-file-edit --set-key Icon --set-value ${placeholder "out"}/share/icons/pysol01.png data/pysol.desktop
-    desktop-file-edit --set-key Comment --set-value "${meta.description}" data/pysol.desktop
-  '';
 
   postInstall = ''
     mkdir $out/share/PySolFC/cardsets
     cp -r $cardsets/* $out/share/PySolFC/cardsets
   '';
-
-  # No tests in archive
-  doCheck = false;
 
   meta = with stdenv.lib; {
     description = "A collection of more than 1000 solitaire card games";
