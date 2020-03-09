27a0a1376b4f79e65f220f584d648e55e002acfe
Benjamin Hipple <bhipple@protonmail.com>
2020-03-09 00:34:39 -0400

    btrfs-dedupe: remove broken and abandoned package
    This package was last released in 2017, and no longer compiles with the latest
    Rust compiler. It has just 1 commit from someone other than the original author
    and appears to be a dead project.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1293,8 +1293,6 @@ in
 
   btrfs-progs = callPackage ../tools/filesystems/btrfs-progs { };
 
-  btrfs-dedupe = callPackage ../tools/filesystems/btrfs-dedupe {};
-
   btrbk = callPackage ../tools/backup/btrbk {
     asciidoc = asciidoc-full;
   };
