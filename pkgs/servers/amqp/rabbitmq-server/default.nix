cf2bba74ee3a356f9fdc1da9f88b567a6c58f540
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-10 11:13:39 +0000

    rabbitmq-server: 3.8.2 -> 3.8.3

--- pkgs/servers/amqp/rabbitmq-server/default.nix
+++ pkgs/servers/amqp/rabbitmq-server/default.nix
@@ -7,12 +7,12 @@
 stdenv.mkDerivation rec {
   pname = "rabbitmq-server";
 
-  version = "3.8.2";
+  version = "3.8.3";
 
   # when updating, consider bumping elixir version in all-packages.nix
   src = fetchurl {
     url = "https://github.com/rabbitmq/rabbitmq-server/releases/download/v${version}/${pname}-${version}.tar.xz";
-    sha256 = "17gixahxass9n4d697my8sq4an51rw3cicb36fqvl8fbhnwjjrwc";
+    sha256 = "1fhs3g2pgrq2xi4hnlc437hkv3261l4i134m6mxid00sf1c89p5f";
   };
 
   buildInputs =
@@ -59,7 +59,7 @@ stdenv.mkDerivation rec {
   '';
 
   meta = {
-    homepage = https://www.rabbitmq.com/;
+    homepage = "https://www.rabbitmq.com/";
     description = "An implementation of the AMQP messaging protocol";
     license = stdenv.lib.licenses.mpl11;
     platforms = stdenv.lib.platforms.unix;
