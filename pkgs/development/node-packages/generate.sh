d8e7444f3b47bdd5a01c9cc33f95ef46fed145ad
Jörg Thalheim <joerg@thalheim.io>
2020-03-10 08:16:10 +0000

    nodePackages: fix update.sh
    Previous version was total broken since paths were computed wrong.
    Since we rely on relatives paths we cannot run node2nix outside of nixpkgs.

--- pkgs/development/node-packages/generate.sh
+++ pkgs/development/node-packages/generate.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env nix-shell
-#! nix-shell shell-generate.nix -i bash
+#! nix-shell -p bash -p nodePackages.node2nix -i bash
 set -eu -o pipefail
 
-cd "$NODE_NIXPKGS_PATH/pkgs/development/node-packages"
-rm -f ./node-env.nix
+rm -f node-env.nix
 for version in 10 12 13; do
-    tmpdir=$(mktemp -d)
-    node2nix --nodejs-$version -i node-packages-v$version.json -o $tmpdir/node-packages-v$version.nix -c $tmpdir/composition-v$version.nix
-    if [ $? -eq 0 ]; then
-        mv $tmpdir/node-packages-v$version.nix .
-        mv $tmpdir/composition-v$version.nix .
-    fi
+  node2nix \
+    "--nodejs-$version" \
+    -i "node-packages-v${version}.json" \
+    -o "node-packages-v${version}.nix" \
+    -c "composition-v${version}.nix"
 done
-cd -
