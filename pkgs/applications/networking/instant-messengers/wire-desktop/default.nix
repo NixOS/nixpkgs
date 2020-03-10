7a443c40b74199a001327a26285945249526739a
Marek Fajkus <marek.faj@gmail.com>
2020-03-10 12:17:16 +0100

    wire-desktop (linux): 3.12.2916 -> 3.14.2921

--- pkgs/applications/networking/instant-messengers/wire-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/wire-desktop/default.nix
@@ -21,12 +21,12 @@ let
 
   version = {
     x86_64-darwin = "3.12.3490";
-    x86_64-linux = "3.12.2916";
+    x86_64-linux = "3.14.2921";
   }.${system} or throwSystem;
 
   sha256 = {
     x86_64-darwin = "0xvhx3r99fl1v1cdqj6sk46kzxv9qi7j754amkhv7knrpmgyp55z";
-    x86_64-linux = "0xx03cpy6kapbjpygwranxjg1a0p8s1xq3cpapvi55rnkbk0qvjw";
+    x86_64-linux = "1dygf8lhwp40rhhd94gaky2vl0b96yz18hv2b7krkz2l3qf7w3cl";
   }.${system} or throwSystem;
 
   meta = with stdenv.lib; {
