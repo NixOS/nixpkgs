063795f3f553f5718c037f6eada2c5a7e3c5a584
Thomaz Leite <thomaz@actor.com.br>
2020-03-11 09:03:28 -0300

    maintainers: add thmzlt

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7253,6 +7253,12 @@
     githubId = 844343;
     name = "Thiago K. Okada";
   };
+  thmzlt = {
+    email = "git@thomazleite.com";
+    github = "thmzlt";
+    githubId = 7709;
+    name = "Thomaz Leite";
+  };
   ThomasMader = {
     email = "thomas.mader@gmail.com";
     github = "ThomasMader";
