46e7580f247341130432bf710b43e938b01db0c8
Aaron Andersen <aaron@fosslib.net>
2020-03-11 07:50:05 -0400

    tomcat9: 9.0.21 -> 9.0.31

--- pkgs/servers/http/tomcat/default.nix
+++ pkgs/servers/http/tomcat/default.nix
@@ -44,7 +44,7 @@ in {
 
   tomcat9 = common {
     versionMajor = "9";
-    versionMinor = "0.21";
-    sha256 = "0nsylbqvky4pf3wpsx3a29b85lvwk91ay37mljk9636qffjj1vjh";
+    versionMinor = "0.31";
+    sha256 = "0hybcy280qhhp9if58xw0rmyyqz1m1bzby7qnwz3y2wc1y4is48v";
   };
 }
