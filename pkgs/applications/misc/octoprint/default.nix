c6014c11ac05cd514dbb112057aff75f17e39b1b
Alex Whitt <alex.joseph.whitt@gmail.com>
2020-03-10 20:08:12 -0400

    Attempt at moving to latest Octoprint with Python 3

--- pkgs/applications/misc/octoprint/default.nix
+++ pkgs/applications/misc/octoprint/default.nix
@@ -1,5 +1,4 @@
-{ stdenv, lib, fetchFromGitHub, python2 }:
-
+{ stdenv, lib, fetchFromGitHub, python3 }:
 let
   mkOverride = attrname: version: sha256:
     self: super: {
@@ -17,6 +16,9 @@ let
       (mkOverride "tornado"     "4.5.3"  "02jzd23l4r6fswmwxaica9ldlyc2p6q8dk6dyff7j58fmdzf853d")
       (mkOverride "psutil"      "5.6.7"  "ffad8eb2ac614518bbe3c0b8eb9dffdb3a8d2e3a7d5da51c5b974fb723a5c5aa")
 
+      # Octoprint holds back jinja2 to 2.8.1 due to breaking changes.
+      # This old version does not have updated test config for pytest 4,
+      # and pypi tarball doesn't contain tests dir anyways.
       (pself: psuper: {
         sentry-sdk = psuper.sentry-sdk.overridePythonAttrs (oldAttrs: rec {
           version = "0.13.2";
@@ -26,12 +28,7 @@ let
           };
           checkInputs = with pself; [flask bottle falcon];
         });
-      })
 
-      # Octoprint holds back jinja2 to 2.8.1 due to breaking changes.
-      # This old version does not have updated test config for pytest 4,
-      # and pypi tarball doesn't contain tests dir anyways.
-      (pself: psuper: {
         jinja2 = psuper.jinja2.overridePythonAttrs (oldAttrs: rec {
           version = "2.8.1";
           src = oldAttrs.src.override {
@@ -44,70 +41,37 @@ let
     ]);
   };
 
-  ignoreVersionConstraints = [
-    "Click"
-    "Flask-Assets"
-    "Flask-Babel"
-    "Flask-Principal"
-    "emoji"
-    "flask"
-    "future"
-    "futures"
-    "monotonic"
-    "markdown"
-    "pkginfo"
-    "psutil"
-    "pyserial"
-    "requests"
-    "rsa"
-    "sarge"
-    "scandir"
-    "semantic_version"
-    "watchdog"
-    "websocket-client"
-    "wrapt"
-    "sentry-sdk"
-    "werkzeug" # 0.16 just deprecates some stuff
-  ];
-
-in py.pkgs.buildPythonApplication rec {
+in
+py.pkgs.buildPythonApplication rec {
   pname = "OctoPrint";
-  version = "1.3.12";
+  version = "1.4.0";
 
   src = fetchFromGitHub {
     owner  = "foosel";
     repo   = "OctoPrint";
     rev    = version;
-    sha256 = "1lmqssgwjyhknjf3x58g7cr0fqz7fs5a3rl07r69wfpch63ranyd";
+    sha256 = "1zla1ayr62lkvkr828dh3y287rzj3rv1hpij9kws44ynn4i582ga";
   };
 
   propagatedBuildInputs = with py.pkgs; [
-    awesome-slugify flask_assets rsa requests pkginfo watchdog
-    semantic-version flask_principal werkzeug flaskbabel tornado
-    psutil pyserial flask_login netaddr markdown sockjs-tornado
+    awesome-slugify flask flask_assets rsa requests pkginfo watchdog
+    semantic-version werkzeug flaskbabel tornado
+    psutil pyserial flask_login netaddr markdown
     pylru pyyaml sarge feedparser netifaces click websocket_client
-    scandir chainmap future futures wrapt monotonic emoji
-    frozendict cachelib sentry-sdk typing filetype
+    scandir chainmap future wrapt monotonic emoji jinja2
+    frozendict cachelib sentry-sdk filetype markupsafe
   ] ++ lib.optionals stdenv.isDarwin [ py.pkgs.appdirs ];
 
   checkInputs = with py.pkgs; [ nose mock ddt ];
 
-  postPatch = ''
-    sed -r -i \
-      ${lib.concatStringsSep "\n" (map (e:
-        ''-e 's@${e}[<>=]+.*@${e}",@g' \''
-      ) ignoreVersionConstraints)}
-      setup.py
-  '';
-
   checkPhase = ''
-    HOME=$(mktemp -d) nosetests ${lib.optionalString stdenv.isDarwin "--exclude=test_set_external_modification"}
+    HOME=$(mktemp -d) nosetests --exclude-test=util.test_pip.test_check_setup ${lib.optionalString stdenv.isDarwin "--exclude=test_set_external_modification"}
   '';
 
   meta = with stdenv.lib; {
     homepage = https://octoprint.org/;
     description = "The snappy web interface for your 3D printer";
     license = licenses.agpl3;
-    maintainers = with maintainers; [ abbradar gebner ];
+    maintainers = with maintainers; [ abbradar gebner WhittlesJr ];
   };
 }
