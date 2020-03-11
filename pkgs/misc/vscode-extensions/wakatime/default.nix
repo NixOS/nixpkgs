aeecaa285bd576dfd0964fb7c23dc94f5d6fc53f
Edmund Wu <fangkazuto@gmail.com>
2020-03-10 22:21:12 -0400

    vscode-extensions.WakaTime.vscode-wakatime: prefer local wakatime

--- pkgs/misc/vscode-extensions/wakatime/default.nix
+++ pkgs/misc/vscode-extensions/wakatime/default.nix
@@ -1,4 +1,4 @@
-{ stdenv
+{ lib
 , wakatime, vscode-utils }:
 
 let
@@ -12,12 +12,18 @@ in
       sha256 = "0bwxz8dg00k8frnvkvcngll5yaf9k7z13dg309vmw8xbdgkiyid4";
     };
 
-    postPatch = ''
-      mkdir -p wakatime-master
-      cp -rt wakatime-master --no-preserve=all ${wakatime}/lib/python*/site-packages/wakatime
+    postUnpack = ''
+      mkdir -p $sourceRoot/wakatime-master
+      cp -rt $sourceRoot/wakatime-master --no-preserve=all ${wakatime}/lib/python*/site-packages/wakatime
     '';
 
-    meta = with stdenv.lib; {
+    # Should check on every version bump
+    # https://github.com/wakatime/vscode-wakatime/blob/8c5065e6c6309fb0dc0dc94d4829d738058002ea/src/dependencies.ts#L30
+    postPatch = ''
+      sed -i "s@\(this.standalone\)\(?this.checkAndInstallStandaloneCli(e)\)@(!this.isCliInstalled()\&\&\1)\2@" dist/extension.js
+    '';
+
+    meta = with lib; {
       description = ''
         Visual Studio Code plugin for automatic time tracking and metrics generated
         from your programming activity
