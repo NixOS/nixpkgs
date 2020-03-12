406a7146a4a9531d2127865225a2bbf65bd8f2c3
mlatus <wqseleven@gmail.com>
2020-03-12 21:46:32 +0800

    wpsoffice: 11.1.0.9080 -> 11.1.0.9126

--- pkgs/applications/office/wpsoffice/default.nix
+++ pkgs/applications/office/wpsoffice/default.nix
@@ -1,22 +1,44 @@
-{ stdenv, fetchurl
-, libX11, glib, xorg, fontconfig, freetype
-, zlib, libpng12, libICE, libXrender, cups
-, alsaLib, atk, cairo, dbus, expat
-, gdk-pixbuf, gtk2-x11, lzma, pango, zotero
-, sqlite, libuuid, qt5, dpkg }:
-
-stdenv.mkDerivation rec{
+# Build tools
+{ stdenv, fetchurl, dpkg,
+# Dependencies
+  alsaLib, atk, cairo, dbus,
+  expat, fontconfig, freetype, gdk-pixbuf,
+  gtk2-x11, glib, primusLib, xorg,
+  libtool, lzma, pango, nspr, qt5, qt4,
+  nss, sqlite, libuuid, zlib, cups, file,
+  wrapGAppsHook
+}:
+let
+  deps = [
+    alsaLib atk cairo dbus.lib
+    expat fontconfig.lib
+    freetype gdk-pixbuf gtk2-x11 glib
+    primusLib xorg.libICE libtool.lib
+    lzma pango nspr qt5.qtbase qt4
+    nss xorg.libSM sqlite libuuid
+    xorg.libX11 xorg.libxcb
+    xorg.libXcomposite xorg.libXcursor
+    xorg.libXdamage xorg.libXfixes
+    xorg.libXi xorg.libXrandr
+    xorg.libXrender xorg.libXScrnSaver
+    xorg.libXtst xorg.libXext zlib cups
+  ];
+  libPath = stdenv.lib.makeLibraryPath deps;
+in
+stdenv.mkDerivation rec {
   pname = "wpsoffice";
-  version = "11.1.0.9080";
+  version = "11.1.0.9126";
 
   src = fetchurl {
-    url = "http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/9080/wps-office_11.1.0.9080.XA_amd64.deb";
-    sha256 = "1731e9aea22ef4e558ad66b1373d863452b4f570aecf09d448ae28a821333454";
+    url = "http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/9126/wps-office_11.1.0.9126.XA_amd64.deb";
+    sha256 = "10d5sgpl1i70rj2596i6865hj0xdlzwdrwiplz41zys6l4zbmfp7";
   };
   unpackCmd = "dpkg -x $src .";
   sourceRoot = ".";
 
-  nativeBuildInputs = [ qt5.wrapQtAppsHook dpkg ];
+  nativeBuildInputs = [ wrapGAppsHook qt5.wrapQtAppsHook dpkg ];
+  dontWrapQtApps = true;
+  dontWrapGApps = true;
 
   meta = {
     description = "Office program originally named Kingsoft Office";
@@ -26,43 +48,6 @@ stdenv.mkDerivation rec{
     license = stdenv.lib.licenses.unfreeRedistributable;
     maintainers = [ stdenv.lib.maintainers.mlatus ];
   };
-
-  libPath = with xorg; stdenv.lib.makeLibraryPath [
-    libX11
-    libpng12
-    glib
-    libSM
-    libXext
-    fontconfig
-    zlib
-    freetype
-    libICE
-    cups
-    libXrender
-    libxcb
-
-    alsaLib
-    atk
-    cairo
-    dbus.daemon.lib
-    expat
-    fontconfig.lib
-    gdk-pixbuf
-    gtk2-x11
-    lzma
-    pango
-    zotero
-    sqlite
-    libuuid
-    libXcomposite
-    libXcursor
-    libXdamage
-    libXfixes
-    libXi
-    libXrandr
-    libXScrnSaver
-    libXtst
-  ];
 
   dontPatchELF = true;
 
@@ -77,18 +62,31 @@ stdenv.mkDerivation rec{
     cp -r usr/* $out
     # Avoid forbidden reference error due use of patchelf
     rm -r *
-    for i in wps wpp et wpspdf; do
-      patchelf \
-        --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-        --force-rpath --set-rpath "$(patchelf --print-rpath $prefix/office6/$i):${stdenv.cc.cc.lib}/lib64:${libPath}" \
-        $prefix/office6/$i
+    bins=$(find $prefix/office6 -maxdepth 1 -executable ! -type d|grep -v '.*\.so\.*')
+    for i in $bins; do
+      origin_rpath=$(patchelf --print-rpath $i)
+      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $i || true
+      patchelf --force-rpath --set-rpath "$origin_rpath''${origin_rpath:+:}${stdenv.cc.cc.lib}/lib64:${libPath}" $i || true
+    done
+
+    for i in et wpp wps wpspdf;do
       substituteInPlace $out/bin/$i \
         --replace /opt/kingsoft/wps-office $prefix
     done
+
     for i in $out/share/applications/*;do
       substituteInPlace $i \
         --replace /usr/bin $out/bin \
         --replace /opt/kingsoft/wps-office $prefix
+    done
+  '';
+
+  postFixup = ''
+    bins=$(find $prefix/office6 -maxdepth 1 -executable ! -type d|grep -v '.*\.so\.*')
+    for i in $bins;do
+      echo Wrapping $i
+      wrapQtApp $i
+      wrapGApp $i
     done
   '';
 }
