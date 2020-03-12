c714fb81d53c4e37891a70bca71bbd1a97031119
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-12 18:19:48 +0000

    mtprotoproxy: 1.0.9 -> 1.1.0

--- pkgs/servers/mtprotoproxy/default.nix
+++ pkgs/servers/mtprotoproxy/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "mtprotoproxy";
-  version = "1.0.9";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "alexbers";
     repo = "mtprotoproxy";
     rev = "v${version}";
-    sha256 = "16f9hzh4h41qb5962lxx04653ncar83znh872g2qh564b6f922z2";
+    sha256 = "15svvramxzl8q8xzs8g62gg8czdn46fjy6jhs5hqf5p83ycxsygz";
   };
 
   nativeBuildInputs = [ wrapPython ];
