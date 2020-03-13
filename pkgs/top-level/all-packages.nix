9bcd5eaa0d0d78fc8e2dd6e5c67c285956004c70
Peter Simons <simons@cryp.to>
2020-03-13 13:07:10 +0100

    haskellPackages: update default compiler from GHC version 8.8.2 to 8.8.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8308,7 +8308,7 @@ in
 
   # Please update doc/languages-frameworks/haskell.section.md, “Our
   # current default compiler is”, if you bump this:
-  haskellPackages = dontRecurseIntoAttrs haskell.packages.ghc882;
+  haskellPackages = dontRecurseIntoAttrs haskell.packages.ghc883;
 
   inherit (haskellPackages) ghc;
 
