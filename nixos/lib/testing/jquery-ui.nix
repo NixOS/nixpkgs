0316d36bc3195d3ab2a0e72b20453cba8d9ee010
David Anderson <dave@natulte.net>
2020-03-13 02:22:11 -0700

    jquery-ui: patch CVE-2016-7103.
    Fixes #57149.

--- nixos/lib/testing/jquery-ui.nix
+++ nixos/lib/testing/jquery-ui.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, unzip }:
+{ stdenv, fetchurl, fetchpatch, unzip }:
 
 stdenv.mkDerivation rec {
   name = "jquery-ui-1.11.4";
@@ -7,6 +7,14 @@ stdenv.mkDerivation rec {
     url = "https://jqueryui.com/resources/download/${name}.zip";
     sha256 = "0ciyaj1acg08g8hpzqx6whayq206fvf4whksz2pjgxlv207lqgjh";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://patch-diff.githubusercontent.com/raw/jquery/jquery-ui/pull/1622.patch";
+      sha256 = "0ip563kz6lhwiims5djrxq3mvb7jx9yzkpsqxxhbi9n6qzz7y2az";
+      name = "CVE-2016-7103";
+    })
+  ];
 
   buildInputs = [ unzip ];
 
