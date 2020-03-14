264bdef6741e96bc77d45311cb3947b8c3af2a83
Evan Stoll <evanjsx@gmail.com>
2020-03-14 15:30:25 -0400

    polychromatic: init at unstable-2020-03-10
    use dev-1.0.0 branch

---
+++ pkgs/applications/misc/polychromatic/default.nix
@@ -0,0 +1,88 @@
+{ stdenv
+, fetchFromGitHub
+, bash
+, cairo
+, glib
+, gnome3
+, gobject-introspection
+, gstreamer
+, gtk3
+, libcanberra-gtk3
+, pkgconfig
+, wrapGAppsHook
+, setproctitle
+, distro
+, pygobject3
+, buildPythonApplication
+, openrazer
+, openrazer-daemon
+, requests
+, hicolor-icon-theme
+, gdk-pixbuf
+, imagemagick
+, desktop-file-utils
+, webkit
+, ninja
+, meson
+, sass
+, sassc
+, ibus
+}:
+
+  buildPythonApplication rec {
+    name = "polychromatic-${version}";
+    version = "unstable-2020-03-10";
+
+    format = "other";
+
+    src = fetchFromGitHub {
+      owner = "polychromatic";
+      repo = "polychromatic";
+      rev = "5ca66b65c6d739e20c973b76a74054476e9c8cce";
+      sha256 = "1xvdpf7h148jgf0q7bh6fhzgvwxdfx2kav7w3kj1x9sxpkza72a1";
+    };
+
+    pathsToLink = [ "/bin" "/etc" "/lib" "/share" ];
+
+    buildInputs = [
+      cairo
+      hicolor-icon-theme
+    ];
+
+    propagatedBuildInputs = [
+      setproctitle
+      openrazer
+      openrazer-daemon
+      requests
+      ibus
+    ];
+
+    nativePropagatedBuildInputs = [
+      gobject-introspection
+      gtk3
+      gdk-pixbuf
+      imagemagick
+    ];
+
+    nativeBuildInputs = [
+      desktop-file-utils
+      gobject-introspection
+      wrapGAppsHook
+      webkit
+      gnome3.librsvg
+      sass
+      sassc
+      ninja
+      meson
+    ];
+
+  postInstall = ''
+    patchShebangsAuto $out/lib/python3.7/site-packages/polychromatic
+    install -Dm755 $src/polychromatic-cli $out/bin
+    for file in $(find $out/lib/python3.7/site-packages/polychromatic -type f \( -name \*.py\* \) ); do
+      substituteInPlace $file --replace /usr/share $out/share
+      substituteInPlace $file --replace /usr/bin $out/bin
+      substituteInPlace $file --replace /usr/lib $out/lib
+    done
+  '';
+}
