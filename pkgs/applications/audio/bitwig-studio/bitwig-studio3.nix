1b534db1f18a8a05b5098ef28e7bdab4ceff2500
Ingolf Wagner <contact@ingolf-wagner.de>
2020-03-15 14:19:30 +0800

    bitwig-studio3: 3.1.2 -> 3.1.3

--- pkgs/applications/audio/bitwig-studio/bitwig-studio3.nix
+++ pkgs/applications/audio/bitwig-studio/bitwig-studio3.nix
@@ -1,17 +1,16 @@
-{ fetchurl, bitwig-studio1, pulseaudio, xorg }:
+{ stdenv, fetchurl, bitwig-studio2, xorg, ... }:
 
-bitwig-studio1.overrideAttrs (oldAttrs: rec {
+bitwig-studio2.overrideAttrs (oldAttrs: rec {
   name = "bitwig-studio-${version}";
-  version = "3.1.2";
+  version = "3.1.3";
 
   src = fetchurl {
-    url = "https://downloads.bitwig.com/stable/${version}/bitwig-studio-${version}.deb";
-    sha256 = "07djn52lz43ls6fa4k1ncz3m1nc5zv2j93hwyavnr66r0hlqy7l9";
+    url =
+      "https://downloads.bitwig.com/stable/${version}/bitwig-studio-${version}.deb";
+    sha256 = "11z5flmp55ywgxyccj3pzhijhaggi42i2pvacg88kcpj0cin57vl";
   };
 
-  buildInputs = oldAttrs.buildInputs ++ [ xorg.libXtst ];
-
-  runtimeDependencies = [ pulseaudio ];
+  buildInputs = bitwig-studio2.buildInputs ++ [ xorg.libXtst ];
 
   installPhase = ''
     ${oldAttrs.installPhase}
@@ -20,4 +19,5 @@ bitwig-studio1.overrideAttrs (oldAttrs: rec {
     rm -f $out/libexec/lib/jre
     cp -r opt/bitwig-studio/lib/jre $out/libexec/lib
   '';
+
 })
