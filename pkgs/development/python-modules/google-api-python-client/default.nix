7a1c9d8eb77c6304b3c8d787492fd24b8a8b6a25
Michael Weiss <dev.primeos@gmail.com>
2020-03-15 11:29:08 +0100

    python3Packages.google_api_python_client: 1.7.12 -> 1.8.0
    This version doesn't depend on
    https://github.com/NixOS/nixpkgs/pull/82355 anymore.

--- pkgs/development/python-modules/google-api-python-client/default.nix
+++ pkgs/development/python-modules/google-api-python-client/default.nix
@@ -1,19 +1,23 @@
 { lib, buildPythonPackage, fetchPypi
-, httplib2, google_auth, google-auth-httplib2, six, uritemplate, oauth2client }:
+, google_auth, google-auth-httplib2, google_api_core
+, httplib2, six, uritemplate, oauth2client }:
 
 buildPythonPackage rec {
   pname = "google-api-python-client";
-  version = "1.7.12";
+  version = "1.8.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "02blmm32xfwjjcxds6ym9nnz29wzyiahd64shrsrkrdm6aqagmlz";
+    sha256 = "003rgr15r9j080f3n5y2x6ymxsfv652m3r7j83p7sbrd9shl4nqg";
   };
 
   # No tests included in archive
   doCheck = false;
 
-  propagatedBuildInputs = [ httplib2 google_auth google-auth-httplib2 six uritemplate oauth2client ];
+  propagatedBuildInputs = [
+    google_auth google-auth-httplib2 google_api_core
+    httplib2 six uritemplate oauth2client
+  ];
 
   meta = with lib; {
     description = "The official Python client library for Google's discovery based APIs";
