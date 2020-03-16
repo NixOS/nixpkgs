e931de58a2f724c3632b7c8a656fb47489ecc963
Silvan Mosberger <contact@infinisil.com>
2020-03-16 21:10:05 +0100

    lib/modules: Fix type checks not being done before merging
    Co-Authored-By: Robert Hensing <robert@roberthensing.nl>

--- lib/tests/modules.sh
+++ lib/tests/modules.sh
@@ -198,6 +198,10 @@ checkConfigOutput "empty" config.value.foo ./declare-lazyAttrsOf.nix ./attrsOf-c
 checkConfigError 'The option path .* is an attribute set of options, but it is defined to not be an attribute set in' \
   config.value ./declare-option-set.nix ./define-value-int-zero.nix
 
+# Even with multiple assignments, a type error should be thrown if any of them aren't valid
+checkConfigError 'The option value .* in .* is not of type .*' \
+  config.value ./declare-int-unsigned-value.nix ./define-value-list.nix ./define-value-int-positive.nix
+
 cat <<EOF
 ====== module tests ======
 $pass Pass
