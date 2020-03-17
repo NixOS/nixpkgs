4f12275a06f6374bb05c1b14d1b8dd66141c7708
oxalica <oxalicc@pm.me>
2020-03-17 17:08:02 +0800

    rust-analyzer: 2020-03-09 -> 2020-03-16 vscode-extensions.rust-analyzer: 2020-03-09 -> 2020-03-16

--- pkgs/development/tools/rust/rust-analyzer/default.nix
+++ pkgs/development/tools/rust/rust-analyzer/default.nix
@@ -2,10 +2,10 @@
 
 {
   rust-analyzer-unwrapped = callPackage ./generic.nix rec {
-    rev = "2020-03-09";
+    rev = "2020-03-16";
     version = "unstable-${rev}";
-    sha256 = "1m97sinfyg43p3crhbjrsgf64bn5pyj7m96ikvznps80w8dnsv5n";
-    cargoSha256 = "0rxmyv8va4za0aghwk9765qn4xwd03nnry83mrkjidw0ripka5sf";
+    sha256 = "0h1dpf9jcdf15qvqmq10giiqmcwdnhw3r8jr26jyh8sk0331i3am";
+    cargoSha256 = "1a6gyankb6jxrz8k8cwpdiaq9m5m4ca7rfcpdm0a2qz7z9nm7k34";
   };
 
   rust-analyzer = callPackage ./wrapper.nix {} {
