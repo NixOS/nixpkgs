8d521654316a077a96e16d33f6d19b94a26d3832
Taito Horiuchi <taito.horiuchi@relexsolutions.com>
2020-03-17 20:57:25 +0200

    build-support/fetchgitlab: enable to pass private_token as an option

--- pkgs/build-support/fetchgitlab/default.nix
+++ pkgs/build-support/fetchgitlab/default.nix
@@ -1,7 +1,7 @@
 { fetchzip, lib }:
 
 # gitlab example
-{ owner, repo, rev, domain ? "gitlab.com", name ? "source", group ? null
+{ owner, repo, rev, domain ? "gitlab.com", name ? "source", group ? null, token ? null
 , ... # For hash agility
 } @ args:
 
@@ -12,10 +12,11 @@ let
     ((optional (group != null) group) ++ [ owner repo ]);
 
   escapedSlug = replaceStrings ["." "/"] ["%2E" "%2F"] slug;
+  baseURL = "https://${domain}/api/v4/projects/${escapedSlug}/repository/archive.tar.gz?sha=${rev}";
+  url = if token == null then "${baseURL}" else "${baseURL}&private_token=${token}";
 in
 
 fetchzip ({
-  inherit name;
-  url = "https://${domain}/api/v4/projects/${escapedSlug}/repository/archive.tar.gz?sha=${rev}";
+  inherit name url;
   meta.homepage = "https://${domain}/${slug}/";
-} // removeAttrs args [ "domain" "owner" "group" "repo" "rev" ]) // { inherit rev; }
+} // removeAttrs args [ "domain" "owner" "group" "repo" "rev" "token" ]) // { inherit rev; }
