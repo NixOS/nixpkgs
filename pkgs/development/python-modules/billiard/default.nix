779d4e65b38d2a3bad6252798b177c1f7ec0fe05
Jonathan Ringer <jonringer117@gmail.com>
2020-03-18 12:46:57 -0700

    pythonPackages.billiards: 3.6.1.0 -> 3.6.3.0

--- pkgs/development/python-modules/billiard/default.nix
+++ pkgs/development/python-modules/billiard/default.nix
@@ -1,16 +1,19 @@
-{ stdenv, buildPythonPackage, fetchPypi, isPyPy, pytest_4, case, psutil }:
+{ stdenv, buildPythonPackage, fetchPypi, isPyPy, pytest, case, psutil }:
 
 buildPythonPackage rec {
   pname = "billiard";
-  version = "3.6.1.0";
+  version = "3.6.3.0";
   disabled = isPyPy;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b8809c74f648dfe69b973c8e660bcec00603758c9db8ba89d7719f88d5f01f26";
+    sha256 = "0spssl3byzqsplra166d59jx8iqfxyzvcbx7vybkmwr5ck72a5yr";
   };
 
-  checkInputs = [ pytest_4 case psutil ];
+  checkInputs = [ pytest case psutil ];
+  checkPhase = ''
+    pytest
+  '';
 
   meta = with stdenv.lib; {
     homepage = https://github.com/celery/billiard;
