b6b89f1f0a806d1e34cc7eb5b870a930c9431e42
Daniel Șerbănescu <daniel@serbanescu.dk>
2020-03-18 08:12:58 +0100

    kodi 18.5 -> 18.6.

--- pkgs/applications/video/kodi/default.nix
+++ pkgs/applications/video/kodi/default.nix
@@ -43,15 +43,15 @@ assert vdpauSupport -> libvdpau != null;
 assert useWayland -> wayland != null && wayland-protocols != null && waylandpp != null && libxkbcommon != null;
 
 let
-  kodiReleaseDate = "20191116";
-  kodiVersion = "18.5";
+  kodiReleaseDate = "20200301";
+  kodiVersion = "18.6";
   rel = "Leia";
 
   kodi_src = fetchFromGitHub {
     owner  = "xbmc";
     repo   = "xbmc";
     rev    = "${kodiVersion}-${rel}";
-    sha256 = "0pcrraj1ddzrd296br10yjnaxgb3iym74xzixcakaqhhp00f5hf6";
+    sha256 = "0rwymipn5hljy5xrslzmrljmj6f9wb191wi7gjw20wl6sv44d0bk";
   };
 
   cmakeProto = fetchurl {
@@ -193,15 +193,6 @@ in stdenv.mkDerivation {
       pkgconfig gnumake
       autoconf automake libtool # still needed for some components. Check if that is the case with 19.0
     ] ++ lib.optionals useWayland [ wayland-protocols ];
-
-    patches = [
-      # Adds missing cassert includes, fixing builds. This will be unnecessary
-      # after 18.6 is released (which will contain this patch)
-      (fetchpatch {
-        url = "https://github.com/xbmc/xbmc/commit/d5947e6733fd564edb68df91fd6d389d9fb82319.patch";
-        sha256 = "1shlbsbfba3074wdyhl42vgin6jfzl7sy3zsvxaxkpx8g7my9jn2";
-      })
-    ];
 
     cmakeFlags = [
       "-Dlibdvdcss_URL=${libdvdcss.src}"
