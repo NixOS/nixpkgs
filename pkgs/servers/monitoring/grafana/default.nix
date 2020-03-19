8b42fbc4c0811a8fa090a7d3f531641510bc1c51
Sergey Lukjanov <me@slukjanov.name>
2020-03-19 11:32:30 -0700

    grafana: 6.6.2 -> 6.7.0
    (cherry picked from commit 913e6b5c7b9538a770b98fa867feb9e5b22c6f68)

--- pkgs/servers/monitoring/grafana/default.nix
+++ pkgs/servers/monitoring/grafana/default.nix
@@ -2,7 +2,7 @@
 
 buildGoPackage rec {
   pname = "grafana";
-  version = "6.6.2";
+  version = "6.7.0";
 
   goPackagePath = "github.com/grafana/grafana";
 
@@ -12,12 +12,12 @@ buildGoPackage rec {
     rev = "v${version}";
     owner = "grafana";
     repo = "grafana";
-    sha256 = "0zbc9jcr3w7rwsv96csqaifn5d0b435wyrrajr5wzsmhljygvrcy";
+    sha256 = "013586kaiyrs5b1mxf9vlcfh7va8md5amnh2jj26jph8ns6m0f87";
   };
 
   srcStatic = fetchurl {
     url = "https://dl.grafana.com/oss/release/grafana-${version}.linux-amd64.tar.gz";
-    sha256 = "1plijm7cy92k79ypcnxjmdf2vhlxa4dzwjyl9lkf2npm7kswswsl";
+    sha256 = "05g6lsl0vmc4q60dkm1404dl0k3wrlf6yy2l2cnaydl6aqz1kh8d";
   };
 
   postPatch = ''
