20b5cf2aa22b8427e4d9af87d70bdcdd6a613cf3
José Romildo Malaquias <malaquias@gmail.com>
2020-03-20 09:46:44 -0300

    mate.mate-power-manager: use mate-panel explicitly instead of mate.mate-panel

--- pkgs/desktops/mate/mate-power-manager/default.nix
+++ pkgs/desktops/mate/mate-power-manager/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, gettext, glib, itstool, libxml2, mate, libnotify, libcanberra-gtk3, dbus-glib, upower, gnome3, gtk3, libtool, polkit, wrapGAppsHook }:
+{ stdenv, fetchurl, pkgconfig, gettext, glib, itstool, libxml2, mate-panel, libnotify, libcanberra-gtk3, dbus-glib, upower, gnome3, gtk3, libtool, polkit, wrapGAppsHook }:
 
 stdenv.mkDerivation rec {
   pname = "mate-power-manager";
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
      dbus-glib
      upower
      polkit
-     mate.mate-panel
+     mate-panel
   ];
 
   configureFlags = [ "--enable-applets" ];
