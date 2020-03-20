210f7d7322f429b74afad29626f7f2fc52b52772
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-20 04:06:23 +0000

    python37Packages.nest-asyncio: 1.2.3 -> 1.3.0

--- pkgs/development/python-modules/nest-asyncio/default.nix
+++ pkgs/development/python-modules/nest-asyncio/default.nix
@@ -5,16 +5,16 @@
 }:
 
 buildPythonPackage rec {
-  version = "1.2.3";
+  version = "1.3.0";
   pname = "nest_asyncio";
   disabled = !(pythonAtLeast "3.5");
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0fznrg32rk6fmvpfdxwwhadh526gdjivmdifg2hiciil2gr8n1s3";
+    sha256 = "1cbd885n3sf4qg1dv3mk1ggr5ssk48yzrzssznr92dh53g04ly7g";
   };
 
-  # tests not packaged with source dist as of 1.2.3/1.2.3, and
+  # tests not packaged with source dist as of 1.3.0/1.3.0, and
   # can't check tests out of GitHub easily without specific commit IDs (no tagged releases)
   doCheck = false;
   pythonImportsCheck = [ "nest_asyncio" ];
