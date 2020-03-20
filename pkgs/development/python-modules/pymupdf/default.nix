d79b76c8e31ed9dec10627a2c704727caf195183
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-20 01:06:12 +0000

    python27Packages.pymupdf: 1.16.11 -> 1.16.12

--- pkgs/development/python-modules/pymupdf/default.nix
+++ pkgs/development/python-modules/pymupdf/default.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPythonPackage, fetchPypi, mupdf, swig }:
 buildPythonPackage rec {
   pname = "PyMuPDF";
-  version = "1.16.11";
+  version = "1.16.12";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "006m31qvvdwbpmxnhj1gs7rpd1jb214mf2hacqmisryx8dnb2jm7";
+    sha256 = "0mywnhn8ylm9xy23yfgnxiq4akk1rq3n7bl1m7pw6imsgbdhzrwg";
   };
 
   patchPhase = ''
