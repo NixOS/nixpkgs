fb7d3348ab3f2987e167c7f49769080e67deec7b
Izorkin <izorkin@elven.pw>
2020-03-20 12:01:22 +0300

    ioping: add support for netdata

--- pkgs/tools/system/ioping/default.nix
+++ pkgs/tools/system/ioping/default.nix
@@ -1,4 +1,8 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv, fetchFromGitHub
+, withNetdata ? false
+}:
+
+with stdenv.lib;
 
 stdenv.mkDerivation rec {
   pname = "ioping";
@@ -11,9 +15,15 @@ stdenv.mkDerivation rec {
     sha256 = "10bv36bqga8sdifxzywzzpjil7vmy62psirz7jbvlsq1bw71aiid";
   };
 
+  patches = optional withNetdata [
+    # Add support for netdata
+    # https://github.com/koct9i/ioping/pull/41
+    ./ioping-netdata.patch
+  ];
+
   makeFlags = [ "PREFIX=$(out)" ];
 
-  meta = with stdenv.lib; {
+  meta = {
     description = "Disk I/O latency measuring tool";
     maintainers = with maintainers; [ raskin ];
     platforms = platforms.unix;
