33999ee76f7a8d9c3f714e2357a0570c95a81e99
Manfred Endres <manfred.endres@tslarusso.de>
2020-03-20 11:18:44 +0100

    unityhub: refactor
    Add darwin support for unityhub package.

--- pkgs/development/tools/unityhub/default.nix
+++ pkgs/development/tools/unityhub/default.nix
@@ -1,24 +1,17 @@
-{ stdenv, fetchurl, appimageTools, gsettings-desktop-schemas, gtk3 }:
+{ stdenv, fetchurl, appimageTools, gsettings-desktop-schemas, gtk3, undmg }:
 
-appimageTools.wrapType2 rec {
+let
+  inherit (stdenv.hostPlatform) system;
+
+  throwSystem = throw "Unsupported system: ${system}";
+
   name = "unityhub";
+  appName = "Unity Hub";
 
-  extraPkgs = (pkgs: with pkgs; with xorg; [ gtk2 gdk_pixbuf glib libGL libGLU nss nspr
-    alsaLib cups gnome2.GConf libcap fontconfig freetype pango
-    cairo dbus dbus-glib libdbusmenu libdbusmenu-gtk2 expat zlib libpng12 udev tbb
-    libpqxx gtk3 libsecret lsb-release openssl nodejs ncurses5
-
-    libX11 libXcursor libXdamage libXfixes libXrender libXi
-    libXcomposite libXext libXrandr libXtst libSM libICE libxcb ]);
-
-  profile = ''
-    export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS
-  '';
-
-  src = fetchurl {
-    url = "https://public-cdn.cloud.unity3d.com/hub/prod/UnityHub.AppImage";
-    sha256 = "1rx7ih94ig3pd1yx1d3fpx7zpixq3j5birkpnzkh778qqsdrg0nf";
-  };
+  sha256 = {
+    x86_64-darwin = "0vra8hllfzh5zpxshwxhqmyb7bm07yzn2gcicqggq5yav758z3wb";
+    x86_64-linux = "1rx7ih94ig3pd1yx1d3fpx7zpixq3j5birkpnzkh778qqsdrg0nf";
+  }.${system} or throwSystem;
 
   meta = with stdenv.lib; {
     homepage = https://unity3d.com/;
@@ -28,7 +21,48 @@ appimageTools.wrapType2 rec {
       and interactive experiences.
     '';
     license = licenses.unfree;
-    platforms = [ "x86_64-linux" ];
+    platforms = [ "x86_64-darwin" "x86_64-linux" ];
     maintainers = with maintainers; [ tesq0 ];
   };
-}
+
+  linux = appimageTools.wrapType2 rec {
+    inherit name meta;
+
+    extraPkgs = (pkgs: with pkgs; with xorg; [ gtk2 gdk_pixbuf glib libGL libGLU nss nspr
+      alsaLib cups gnome2.GConf libcap fontconfig freetype pango
+      cairo dbus dbus-glib libdbusmenu libdbusmenu-gtk2 expat zlib libpng12 udev tbb
+      libpqxx gtk3 libsecret lsb-release openssl nodejs ncurses5
+
+      libX11 libXcursor libXdamage libXfixes libXrender libXi
+      libXcomposite libXext libXrandr libXtst libSM libICE libxcb ]);
+
+    profile = ''
+      export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS
+    '';
+
+    src = fetchurl {
+      url = "https://public-cdn.cloud.unity3d.com/hub/prod/UnityHub.AppImage";
+      inherit sha256;
+    };
+  };
+
+  darwin = stdenv.mkDerivation {
+    inherit name meta;
+
+    src = fetchurl {
+      url = "https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.dmg";
+      inherit sha256;
+    };
+
+    buildInputs = [ undmg ];
+    installPhase = ''
+      mkdir -p "$out/Applications/Unity/Hub/${appName}.app"
+      cp -R . "$out/Applications/Unity/Hub/${appName}.app"
+      chmod a+x "$out/Applications/Unity/Hub/${appName}.app/Contents/MacOS/${appName}"
+    '';
+  };
+
+in if stdenv.isDarwin
+  then darwin
+  else linux
+
