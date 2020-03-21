4b9795ad6fcf94da678d3767b1b70624972f50c6
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-21 17:56:28 +0000

    gnome3.gnome-getting-started-docs: 3.34.1 -> 3.36.0

--- pkgs/desktops/gnome-3/apps/gnome-getting-started-docs/default.nix
+++ pkgs/desktops/gnome-3/apps/gnome-getting-started-docs/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-getting-started-docs";
-  version = "3.34.1";
+  version = "3.36.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-getting-started-docs/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "00in6yc02pdalyx8g0fncmch0l7nr819587ngjm83ara9qa8z6fa";
+    sha256 = "14g0ywv3dmfz8y9dpgx8s8zak52i1w8pqjf6ffsfsk27l8gs6i6i";
   };
 
   passthru = {
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
   buildInputs = [ intltool itstool libxml2 ];
 
   meta = with stdenv.lib; {
-    homepage = https://live.gnome.org/DocumentationProject;
+    homepage = "https://live.gnome.org/DocumentationProject";
     description = "Help a new user get started in GNOME";
     maintainers = gnome3.maintainers;
     license = licenses.cc-by-sa-30;
