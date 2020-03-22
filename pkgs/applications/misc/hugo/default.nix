0dbf73c022ba73f9978af17af2e0ff4af662cf3f
Maximilian Bosch <maximilian@mbosch.me>
2020-03-22 22:39:44 +0100

    hugo: 0.68.0 -> 0.68.1
    https://github.com/gohugoio/hugo/releases/tag/v0.68.1

--- pkgs/applications/misc/hugo/default.nix
+++ pkgs/applications/misc/hugo/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "hugo";
-  version = "0.68.0";
+  version = "0.68.1";
 
   src = fetchFromGitHub {
     owner = "gohugoio";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1jdk4mwflxm7n076x4gbqb362z4qriszq8fkc0sjsvd05z613ry6";
+    sha256 = "1h7zymvxk71jq51az4qnldk54jl9sd4zwkn5r5323xzjffwzny82";
   };
 
   modSha256 = "04vzm65kbj9905z4cf5yh6yc6g3b0pd5vc00lrxw84pwgqgc0ykb";
