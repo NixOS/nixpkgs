61a2a6c41161af15ee44a5cb244dbf1d2ffe60d9
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2020-03-22 10:58:49 +0200

    fixup! filesender: init at 2.15

--- pkgs/servers/web-apps/filesender/default.nix
+++ pkgs/servers/web-apps/filesender/default.nix
@@ -11,6 +11,14 @@ stdenv.mkDerivation rec {
     sha256 = "0b53rv5i44cqc94im15ampigj9s5p0i3yzq7gqxkxfw6d5l688bp";
   };
 
+  patches = [
+    # Filesender searches for the configuration in the same directory tree as
+    # the other source code. This patch makes it possible to define a different
+    # location for the configuration directory so the NixOS service can write
+    # the configuration without modifying the entire package.
+    ./config_dir.patch
+  ];
+
   installPhase = ''
     mkdir -p $out/
     cp -R . $out/
