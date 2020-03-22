382e751a68b177f9dd3aedfdbf8cae348b48052b
Florian Klink <flokli@flokli.de>
2020-03-22 01:27:58 +0100

    drush: still needs php 7.3
    If invoked with php 7.4 on a up2date Drupal 7 installation, drush
    complains with a deprecation warning, that swallows all legit output:
    
    > drush uli
    Deprecated function: Array and string offset access syntax with curly braces is   [error]
    deprecated in include_once() (line 20 of
    /var/www/…/htdocs/includes/file.phar.inc).
    
    Use php7.3 when invoking drush.

--- pkgs/development/tools/misc/drush/default.nix
+++ pkgs/development/tools/misc/drush/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, php, which, makeWrapper, bash, coreutils, ncurses }:
+{ stdenv, fetchurl, php73, which, makeWrapper, bash, coreutils, ncurses }:
 
 stdenv.mkDerivation rec {
   name = "drush-6.1.0";
@@ -32,7 +32,7 @@ stdenv.mkDerivation rec {
     mkdir -p "$out"
     cp -r . "$out/src"
     mkdir "$out/bin"
-    wrapProgram "$out/src/drush" --prefix PATH : "${stdenv.lib.makeBinPath [ which php bash coreutils ncurses ]}"
+    wrapProgram "$out/src/drush" --prefix PATH : "${stdenv.lib.makeBinPath [ which php73 bash coreutils ncurses ]}"
     ln -s "$out/src/drush" "$out/bin/drush"
   '';
 }
