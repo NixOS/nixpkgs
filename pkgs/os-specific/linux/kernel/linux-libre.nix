f12b9d4e35824160976469bc5e7d5d0ba7ea680d
Tim Steinbach <tim@nequissimus.com>
2020-03-22 12:05:45 -0400

    linux_latest-libre: 17322 -> 17387
    (cherry picked from commit f0d17c2a17e4ef37ba4e42255987c148168421be)

--- pkgs/os-specific/linux/kernel/linux-libre.nix
+++ pkgs/os-specific/linux/kernel/linux-libre.nix
@@ -1,8 +1,8 @@
 { stdenv, lib, fetchsvn, linux
 , scripts ? fetchsvn {
     url = "https://www.fsfla.org/svn/fsfla/software/linux-libre/releases/branches/";
-    rev = "17322";
-    sha256 = "1hhi1gsfr08zj9d8mglbfk5wicfy1gqrh68vg90hxglp61dsx97x";
+    rev = "17387";
+    sha256 = "1a62n326xsbb8r8y610d4k698n3la22rnhlxa56c2zki2q5697h3";
   }
 , ...
 }:
