d508a2f366b2776a47333b35d034724b5de450da
Izorkin <izorkin@elven.pw>
2020-03-23 12:23:50 +0300

    nixos/netdata: fix permissions for perf.plugin

--- nixos/modules/services/monitoring/netdata.nix
+++ nixos/modules/services/monitoring/netdata.nix
@@ -9,6 +9,7 @@ let
     mkdir -p $out/libexec/netdata/plugins.d
     ln -s /run/wrappers/bin/apps.plugin $out/libexec/netdata/plugins.d/apps.plugin
     ln -s /run/wrappers/bin/freeipmi.plugin $out/libexec/netdata/plugins.d/freeipmi.plugin
+    ln -s /run/wrappers/bin/perf.plugin $out/libexec/netdata/plugins.d/perf.plugin
     ln -s /run/wrappers/bin/slabinfo.plugin $out/libexec/netdata/plugins.d/slabinfo.plugin
   '';
 
@@ -180,6 +181,14 @@ in {
       owner = cfg.user;
       group = cfg.group;
       permissions = "u+rx,g+rx,o-rwx";
+    };
+
+    security.wrappers."perf.plugin" = {
+      source = "${cfg.package}/libexec/netdata/plugins.d/perf.plugin.org";
+      capabilities = "cap_sys_admin+ep";
+      owner = cfg.user;
+      group = cfg.group;
+      permissions = "u+rx,g+rx,o-rx";
     };
 
     security.wrappers."slabinfo.plugin" = {
