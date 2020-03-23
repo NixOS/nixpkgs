210bbf7940a3b7a8625a59d9c2f2da92b4939167
Timo Kaufmann <timokau@zoho.com>
2020-03-23 14:07:08 +0100

    newsboat: add changelog

--- pkgs/applications/networking/feedreaders/newsboat/default.nix
+++ pkgs/applications/networking/feedreaders/newsboat/default.nix
@@ -51,6 +51,7 @@ rustPlatform.buildRustPackage rec {
 
   meta = with stdenv.lib; {
     homepage    = https://newsboat.org/;
+    changelog   = "https://github.com/newsboat/newsboat/blob/${src.rev}/CHANGELOG.md";
     description = "A fork of Newsbeuter, an RSS/Atom feed reader for the text console";
     maintainers = with maintainers; [ dotlambda nicknovitski ];
     license     = licenses.mit;
