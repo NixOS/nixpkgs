968b6ad01775f1bdf57388747789895c691a7049
Elyhaka <57923898+Elyhaka@users.noreply.github.com>
2020-03-24 13:06:45 +0100

    wofi: fixes allow_images=true crash

--- pkgs/applications/misc/wofi/default.nix
+++ pkgs/applications/misc/wofi/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, fetchhg, pkg-config, meson, ninja, wayland, gtk3 }:
+{ stdenv, lib, fetchhg, fetchpatch, pkg-config, meson, ninja, wayland, gtk3, wrapGAppsHook }:
 
 stdenv.mkDerivation rec {
   pname = "wofi";
@@ -10,8 +10,18 @@ stdenv.mkDerivation rec {
     sha256 = "086j5wshawjbwdmmmldivfagc2rr7g5a2gk11l0snqqslm294xsn";
   };
 
-  nativeBuildInputs = [ pkg-config meson ninja ];
+  nativeBuildInputs = [ pkg-config meson ninja wrapGAppsHook ];
   buildInputs = [ wayland gtk3 ];
+
+  # Fixes icon bug on NixOS.
+  # Will need to be removed on next release
+  # see https://todo.sr.ht/~scoopta/wofi/54
+  patches = [
+    (fetchpatch {
+      url = "https://paste.sr.ht/blob/1cbddafac3806afb203940c029e78ce8390d8f49";
+      sha256 = "18960y9ajilrwwl6mjnrh6wj0sm4ivczmacck36p2dj9xd0n8vkh";
+    })
+  ];
 
   meta = with lib; {
     description = "A launcher/menu program for wlroots based wayland compositors such as sway";
