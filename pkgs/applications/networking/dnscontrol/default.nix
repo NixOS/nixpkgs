6ca41603980f153990112cec5bd7599dadf67feb
Marek Mahut <marek.mahut@gmail.com>
2020-03-24 13:46:57 +0100

    dnscontrol: remove unnecessary binries

--- pkgs/applications/networking/dnscontrol/default.nix
+++ pkgs/applications/networking/dnscontrol/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchFromGitHub, buildGoPackage}:
-  
+
 buildGoPackage rec {
   pname = "dnscontrol";
   version = "3.0.0";
 
   goPackagePath = "github.com/StackExchange/dnscontrol";
 
-   goDeps = ./deps.nix;
+  goDeps = ./deps.nix;
 
   src = fetchFromGitHub {
     owner = "StackExchange";
@@ -14,6 +14,10 @@ buildGoPackage rec {
     rev = "v${version}";
     sha256 = "1j8i4k7bqkqmi6dmc9fxfab49a7qigig72rlbga902lw336p6cc7";
   };
+
+  postInstall = ''
+    rm $bin/bin/{build,convertzone,generate,validate}
+  '';
 
   meta = with stdenv.lib; {
     description = "Synchronize your DNS to multiple providers from a simple DSL";
