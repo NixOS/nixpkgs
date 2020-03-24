95e15a991ac93dafc9b4ac620086013c815bdcbd
Rakesh Gupta <rakesh.gupta@formbay.com.au>
2020-03-24 09:41:58 +0530

    pythonPackages.variants: init at 0.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5839,6 +5839,8 @@ in {
 
   user-agents = callPackage ../development/python-modules/user-agents { };
 
+  variants = callPackage ../development/python-modules/variants { };
+
   verboselogs = callPackage ../development/python-modules/verboselogs { };
 
   vega_datasets = callPackage ../development/python-modules/vega_datasets { };
