b29d48acfb9d1b489996726694030c5e3fc540c1
Evils <evils.devils@protonmail.com>
2020-03-24 23:45:20 +0100

    nixos/tuptime: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -545,6 +545,7 @@
   ./services/monitoring/teamviewer.nix
   ./services/monitoring/telegraf.nix
   ./services/monitoring/thanos.nix
+  ./services/monitoring/tuptime.nix
   ./services/monitoring/ups.nix
   ./services/monitoring/uptime.nix
   ./services/monitoring/vnstat.nix
