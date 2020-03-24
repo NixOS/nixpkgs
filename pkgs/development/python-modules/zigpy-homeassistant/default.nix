3aa3a84d8ac0de311e99a26b1b27d07ed5232a94
Elis Hirwing <elis@hirwing.se>
2020-03-24 21:48:28 +0100

    python3Packages.zigpy-homeassistant: 0.11.0 -> 0.19.0

--- pkgs/development/python-modules/zigpy-homeassistant/default.nix
+++ pkgs/development/python-modules/zigpy-homeassistant/default.nix
@@ -1,18 +1,21 @@
 { stdenv, buildPythonPackage, fetchPypi
-, aiohttp, crccheck, pycryptodome, pycrypto
+, aiohttp, crccheck, pycryptodome, pycrypto, voluptuous
 , pytest, pytest-asyncio, asynctest }:
 
-buildPythonPackage rec {
+let
+  version = "0.19.0";
   pname = "zigpy-homeassistant";
-  version = "0.11.0";
+
+in buildPythonPackage {
+  inherit pname version;
 
   nativeBuildInputs = [ pytest pytest-asyncio asynctest ];
   buildInputs = [ aiohttp pycryptodome ];
-  propagatedBuildInputs = [ crccheck pycrypto ];
+  propagatedBuildInputs = [ crccheck pycrypto voluptuous ];
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "021wg9yhz8dsif60r8s5621mf63bsayjjb2bimhq0am03ql0fysl";
+    sha256 = "1p0an7i78gn9h38y1pkafvnf0axj8a1ih734m90p2sxqzxxgz73p";
   };
 
   meta = with stdenv.lib; {
