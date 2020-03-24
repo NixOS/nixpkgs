bfc482ea38ba2e8df8113e391cb9d17e41457d13
Colin L Rice <colin@daedrum.net>
2020-03-23 21:24:07 -0400

    Update lnd to build with go1.12
    Updated the version
    Forced only building subpackages with main to prevent panics over
    multiple modules in one repo

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23092,9 +23092,7 @@ in
   };
   litecoind = litecoin.override { withGui = false; };
 
-  lnd = callPackage ../applications/blockchains/lnd.nix {
-    buildGoModule = buildGo112Module;
-  };
+  lnd = callPackage ../applications/blockchains/lnd.nix { };
 
   monero = callPackage ../applications/blockchains/monero {
     inherit (darwin.apple_sdk.frameworks) CoreData IOKit PCSC;
