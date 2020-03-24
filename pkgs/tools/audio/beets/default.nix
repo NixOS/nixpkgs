befbb792b9d14879f560d5fa26daf7714a3987bd
Doron Behar <doron.behar@gmail.com>
2020-03-25 00:21:57 +0200

    pythonPackages.beets: add unconditionally deps needed for checks

--- pkgs/tools/audio/beets/default.nix
+++ pkgs/tools/audio/beets/default.nix
@@ -172,6 +172,13 @@ in pythonPackages.buildPythonApplication rec {
     nose
     rarfile
     responses
+    # Although considered as plugin dependencies, they are needed for the
+    # tests, for disabling them via an override makes the build fail. see:
+    # https://github.com/beetbox/beets/blob/v1.4.9/setup.py
+    pylast
+    mpd2
+    discogs_client
+    pyxdg
   ];
 
   patches = [
