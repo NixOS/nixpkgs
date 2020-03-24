b79a474044233fca2178e97aeb22bafb4b56ef61
Maximilian Bosch <maximilian@mbosch.me>
2020-03-24 13:35:19 +0100

    prometheus-wireguard-exporter: 3.2.2 -> 3.2.4
    https://github.com/MindFlavor/prometheus_wireguard_exporter/releases/tag/3.2.3
    https://github.com/MindFlavor/prometheus_wireguard_exporter/releases/tag/3.2.4

--- pkgs/servers/monitoring/prometheus/wireguard-exporter.nix
+++ pkgs/servers/monitoring/prometheus/wireguard-exporter.nix
@@ -2,24 +2,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "wireguard-exporter";
-  version = "3.2.2";
+  version = "3.2.4";
 
   src = fetchFromGitHub {
     owner = "MindFlavor";
     repo = "prometheus_wireguard_exporter";
     rev = version;
-    sha256 = "18khym7ygj29w98zf6i1l5c2pz84zla2z34l5jnh595xvwfl94pc";
+    sha256 = "15his6mv3vmzfg972fb8m01h2m3jxmaqz3zw2krfr136mvg2rvjw";
   };
 
-  cargoSha256 = "0m7xa610k260gxn2xg6bc2y6fww0p72mvvik7278j2d15044c4yl";
+  cargoSha256 = "0ajkpshjv0im6falgjrsc2jdbvm2rhibl4v8rcmb2fg3kx7xc8vf";
 
   buildInputs = lib.optional stdenv.isDarwin Security;
-
-  # Commonly used hack in nixpkgs to allow unstable features on a stable rustc. This is needed
-  # since `prometheus_exporter_base` uses `#!feature[]` to enable async which
-  # is actually not needed as `async` is part of rustc 1.39.0-stable. This can be removed
-  # as soon as https://github.com/MindFlavor/prometheus_exporter_base/pull/15 is merged.
-  RUSTC_BOOTSTRAP = 1;
 
   meta = with lib; {
     description = "A Prometheus exporter for WireGuard, written in Rust.";
