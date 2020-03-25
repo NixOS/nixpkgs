368cc58db3a931eb3333ce7739ac1aaedec33c8a
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-25 09:33:38 +0000

    python37Packages.PyChromecast: 4.1.1 -> 4.2.0

--- pkgs/development/python-modules/pychromecast/default.nix
+++ pkgs/development/python-modules/pychromecast/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "PyChromecast";
-  version = "4.1.1";
+  version = "4.2.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "13cxyxs0cl2cwbx1fkw6b26hrzpllx1wfq2q58fx298yjn3niiby";
+    sha256 = "05h7ncymyh1715v2jkj312qza5qlqrz6yfpgyv4kdf1m07zh7pxh";
   };
 
   disabled = !isPy3k;
