f39c3bc8bce3a932b0aa64f905fb80d5c41d10b1
Tim Steinbach <tim@nequissimus.com>
2020-03-25 13:06:02 -0400

    zoom-us: 3.5.372466.0322 -> 3.5.374815.0324
    (cherry picked from commit f47e15a63c28fb964975186bc60cdd5869a1479b)

--- pkgs/applications/networking/instant-messengers/zoom-us/default.nix
+++ pkgs/applications/networking/instant-messengers/zoom-us/default.nix
@@ -15,11 +15,11 @@ assert pulseaudioSupport -> libpulseaudio != null;
 let
   inherit (stdenv.lib) concatStringsSep makeBinPath optional;
 
-  version = "3.5.372466.0322";
+  version = "3.5.374815.0324";
   srcs = {
     x86_64-linux = fetchurl {
       url = "https://zoom.us/client/${version}/zoom_x86_64.tar.xz";
-      sha256 = "0yaf5rmxkzwxs27gqxd13i9c72rysmymh99kg70i1giigv93fbr0";
+      sha256 = "1pnr00jwjjp23bbqdg5sv2s2rnciz57g1453sgi13kzpppydhzi5";
     };
   };
 
