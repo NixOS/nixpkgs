f4a9fe36fa6c5b26cde72154b6cf2df1e262562e
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-26 10:48:43 +0000

    libpoly: 0.1.7 -> 0.1.8

--- pkgs/applications/science/logic/poly/default.nix
+++ pkgs/applications/science/logic/poly/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libpoly";
-  version = "0.1.7";
+  version = "0.1.8";
 
   src = fetchFromGitHub {
     owner = "SRI-CSL";
     repo = "libpoly";
     rev = "v${version}";
-    sha256 = "0i5ar4lhs88glk0rvkmag656ii434i6i1q5dspx6d0kyg78fii64";
+    sha256 = "19ddzrir20571zqg720ajqpl59lhpc6c18bp763r6rw68d9zbjch";
   };
 
   nativeBuildInputs = [ cmake ];
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
   buildInputs = [ gmp python ];
 
   meta = with stdenv.lib; {
-    homepage = https://github.com/SRI-CSL/libpoly;
+    homepage = "https://github.com/SRI-CSL/libpoly";
     description = "C library for manipulating polynomials";
     license = licenses.lgpl3;
     platforms = platforms.all;
