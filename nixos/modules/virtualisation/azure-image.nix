7cf76d9245546f00dfff0e76f35672e8421df633
Cole Mickens <cole.mickens@gmail.com>
2020-03-26 06:11:54 +0000

    azure: init nixos/maintainers/scripts/azure-new

--- nixos/modules/virtualisation/azure-image.nix
+++ nixos/modules/virtualisation/azure-image.nix
@@ -21,6 +21,7 @@ in
       name = "azure-image";
       postVM = ''
         ${pkgs.vmTools.qemu}/bin/qemu-img convert -f raw -o subformat=fixed,force_size -O vpc $diskImage $out/disk.vhd
+        rm $diskImage
       '';
       configFile = ./azure-config-user.nix;
       format = "raw";
