NixOS/nixpkgs

commit 153b0db9675da00bbd72c01ad0c2ddc9d49cedee

perlPackages.CPAN: apply patch to fix changed YAML module default
ZHF: #80379
https://hydra.nixos.org/build/112819370
https://hydra.nixos.org/build/112832567

A default has changed in YAML, breaking the latest release of CPAN. This commit
applies a A patch from the PR fixing the problem.

https://github.com/andk/cpanpm/pull/133

pkgs/top-level/perl-packages.nix
@@ -3279,6 +3279,13 @@ let
       url = mirror://cpan/authors/id/A/AN/ANDK/CPAN-2.27.tar.gz;
       sha256 = "b4b1471a2881e2d616f59e723879b4110ae485b79d5962f115119c28cf69e07f";
     };
+    patches = [
+      (fetchpatch {
+        url = "https://patch-diff.githubusercontent.com/raw/andk/cpanpm/pull/133.diff";
+        name = "patch-YAML-modules-default-for-LoadBlessed-was-changed-to-false";
+        sha256 = "0i8648cwshzzd0b34gyfn68s1vs85d8336ggk2kl99awah1ydsfr";
+      })
+    ];
     propagatedBuildInputs = [ ArchiveZip CPANChecksums CPANPerlReleases Expect FileHomeDir LWP LogLog4perl ModuleBuild TermReadKey YAML YAMLLibYAML YAMLSyck ];
     meta = {
       description = "Query, download and build perl modules from CPAN sites";

commit fa7372330537a35ba44adfe98d1ebb134c1de56b

perlPackages.CPAN: fix patch download
Fixes: 153b0db9 ('perlPackages.CPAN: apply patch to fix changed YAML module default')
Closes: #81480

pkgs/top-level/perl-packages.nix
@@ -3344,9 +3344,9 @@ let
     };
     patches = [
       (fetchpatch {
-        url = "https://patch-diff.githubusercontent.com/raw/andk/cpanpm/pull/133.diff";
+        url = "https://github.com/andk/cpanpm/commit/10da44f1757aff6971e3bc4ed38ab115e738c740.diff";
         name = "patch-YAML-modules-default-for-LoadBlessed-was-changed-to-false";
-        sha256 = "0i8648cwshzzd0b34gyfn68s1vs85d8336ggk2kl99awah1ydsfr";
+        sha256 = "0sr2nxkr1cwavpvpxsqcsryfd5fjv4fkxfihd03jzavv5awj79hp";
       })
     ];
     propagatedBuildInputs = [ ArchiveZip CPANChecksums CPANPerlReleases Expect FileHomeDir LWP LogLog4perl ModuleBuild TermReadKey YAML YAMLLibYAML YAMLSyck ];