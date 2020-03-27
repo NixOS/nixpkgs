bf96f9122def9a2d7ede713b845183482f211e49
Minijackson <minijackson@riseup.net>
2020-03-27 15:50:31 +0100

    vimPlugins.gruvbox-community: 2019-05-31 -> 2020-02-24, switch to autogenerated

--- pkgs/misc/vim-plugins/overrides.nix
+++ pkgs/misc/vim-plugins/overrides.nix
@@ -397,18 +397,6 @@ self: super: {
     dependencies = with super; [ webapi-vim ];
   });
 
-  gruvbox-community = buildVimPluginFrom2Nix {
-    pname = "gruvbox-community";
-    version = "2019-05-31";
-    src = fetchFromGitHub {
-      owner = "gruvbox-community";
-      repo = "gruvbox";
-      rev = "e122091dad968a5524f3e8136615a479c7b6f247";
-      sha256 = "1hncjyfi1gbw62b2pngy5qxyzibrhbyzgfmm9a58sdh1272l8ls8";
-    };
-    meta.maintainers = with stdenv.lib.maintainers; [ minijackson ];
-  };
-
   meson = buildVimPluginFrom2Nix {
     inherit (meson) pname version src;
     preInstall = "cd data/syntax-highlighting/vim";
