a9edd591d4ec12e8d6f784a150c1fcbe9605c236
Gerd Flaig <gefla@pond.sub.org>
2020-03-29 17:56:32 +0200

    Add libGL dependency
    https://github.com/NixOS/nixpkgs/issues/80535

--- pkgs/applications/video/lightworks/default.nix
+++ pkgs/applications/video/lightworks/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, dpkg, makeWrapper, buildFHSUserEnv
-, gtk3, gdk-pixbuf, cairo, libjpeg_original, glib, gnome2, libGLU
+, gtk3, gdk-pixbuf, cairo, libjpeg_original, glib, gnome2, libGLU, libGL
 , nvidia_cg_toolkit, zlib, openssl, portaudio
 }:
 let
@@ -11,6 +11,7 @@ let
     libjpeg_original
     glib
     gnome2.pango
+    libGL
     libGLU
     nvidia_cg_toolkit
     zlib
