4c931a821703b5fbfdbca931dbaa28390efc7070
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2020-03-28 21:48:56 -0400

    higan: fix darwin build

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26305,6 +26305,7 @@ in
 
   higan = callPackage ../misc/emulators/higan {
     inherit (gnome2) gtksourceview;
+    inherit (darwin.apple_sdk.frameworks) Carbon Cocoa OpenGL OpenAL;
   };
 
   bullet = callPackage ../development/libraries/bullet {
