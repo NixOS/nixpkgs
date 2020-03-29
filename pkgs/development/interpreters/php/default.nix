716fd0e2f576c3eeea2a231a259ff67bad3f6594
talyz <kim.lindberger@gmail.com>
2020-03-29 12:01:38 +0200

    php.buildEnv: Put the extraConfig snippet after extensions
    Make sure all the extensions are loaded first, then read the custom
    config where possible.

--- pkgs/development/interpreters/php/default.nix
+++ pkgs/development/interpreters/php/default.nix
@@ -178,9 +178,9 @@ let
 
           extNames = map getExtName extList;
           extraInit = writeText "custom-php.ini" ''
-            ${extraConfig}
             ${lib.concatStringsSep "\n"
               (lib.textClosureList extensionTexts extNames)}
+            ${extraConfig}
           '';
         in
           symlinkJoin {
