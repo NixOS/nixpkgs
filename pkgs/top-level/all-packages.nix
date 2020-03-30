1c951b1484ebf0efc2024dc5fad252bcefe5369b
Daniel Fullmer <danielrf12@gmail.com>
2020-03-29 20:59:49 -0400

    hcxdumptool: init at 6.0.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4039,6 +4039,8 @@ in
 
   hcxtools = callPackage ../tools/security/hcxtools { };
 
+  hcxdumptool = callPackage ../tools/security/hcxdumptool { };
+
   hdapsd = callPackage ../os-specific/linux/hdapsd { };
 
   hdaps-gl = callPackage ../tools/misc/hdaps-gl { };
