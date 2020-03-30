fc1034a1fab64bb386806901565e6dd2f2683f0f
Elis Hirwing <elis@hirwing.se>
2020-03-30 20:42:16 +0200

    nixos/nextcloud: upgrade to php74

--- nixos/modules/services/web-apps/nextcloud.nix
+++ nixos/modules/services/web-apps/nextcloud.nix
@@ -6,12 +6,12 @@ let
   cfg = config.services.nextcloud;
   fpm = config.services.phpfpm.pools.nextcloud;
 
-  phpPackage = pkgs.php73.buildEnv {
+  phpPackage = pkgs.php74.buildEnv {
     exts = pp: with pp.exts; [
       bcmath calendar curl exif ftp filter gd gettext gmp intl json ldap
       mysqlnd opcache openssl pcntl pdo pdo_mysql pdo_odbc pdo_pgsql
       pdo_sqlite pgsql readline session soap sodium sqlite3 zip zlib mbstring
-      posix hash ctype dom simplexml xmlreader xmlwriter pp.apcu
+      posix ctype dom simplexml xmlreader xmlwriter pp.apcu
       pp.redis pp.memcached pp.imagick
     ];
     extraConfig = phpOptionsStr;
