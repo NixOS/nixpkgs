9304c984e291da1e2d2213446f7793ecf895d443
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-30 01:56:54 +0000

    libsignal-protocol-c: 2.3.2 -> 2.3.3

--- pkgs/development/libraries/libsignal-protocol-c/default.nix
+++ pkgs/development/libraries/libsignal-protocol-c/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libsignal-protocol-c";
-  version = "2.3.2";
+  version = "2.3.3";
 
   src = fetchFromGitHub {
     owner = "signalapp";
     repo = "libsignal-protocol-c";
     rev = "v${version}";
-    sha256 = "1qj2w4csy6j9jg1jy66n1qwysx7hgjywk4n35hlqcnh1kpa14k3p";
+    sha256 = "0z5p03vk15i6h870azfjgyfgxhv31q2vq6rfhnybrnkxq2wqzwhk";
   };
 
   nativeBuildInputs = [ cmake ];
@@ -21,7 +21,7 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Signal Protocol C Library";
-    homepage = https://github.com/signalapp/libsignal-protocol-c;
+    homepage = "https://github.com/signalapp/libsignal-protocol-c";
     license = licenses.gpl3;
     platforms = platforms.all;
     maintainers = with maintainers; [ orivej ];
