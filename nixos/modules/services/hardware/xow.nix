081ed8f0126178a0d2c624fd4bd946704db7fc6a
Jan Solanti <jhs@psonet.com>
2020-03-30 04:05:52 +0300

    xow: 0.2 -> 0.4

--- nixos/modules/services/hardware/xow.nix
+++ nixos/modules/services/hardware/xow.nix
@@ -4,27 +4,14 @@ let
   cfg = config.services.hardware.xow;
 in {
   options.services.hardware.xow = {
-    enable = lib.mkEnableOption "Whether to enable xow or not.";
+    enable = lib.mkEnableOption "xow as a systemd service";
   };
 
   config = lib.mkIf cfg.enable {
     hardware.uinput.enable = true;
 
-    users.users.xow = {
-      group = "uinput";
-      isSystemUser = true;
-    };
+    systemd.packages = [ pkgs.xow ];
 
-    systemd.services.xow = {
-      wantedBy = [ "multi-user.target" ];
-      description = "Xbox One Wireless Dongle Driver";
-      after = [ "systemd-udev-settle.service" ];
-      serviceConfig = {
-        ExecStart = ''
-          ${pkgs.xow}/bin/xow
-        '';
-        User = "xow";
-      };
-    };
+    services.udev.packages = [ pkgs.xow ];
   };
 }
--- pkgs/misc/drivers/xow/default.nix
+++ pkgs/misc/drivers/xow/default.nix
@@ -1,32 +1,33 @@
-{ stdenv, fetchFromGitHub, libusb, gitMinimal }:
+{ stdenv, fetchFromGitHub, libusb }:
 
 stdenv.mkDerivation rec {
   pname = "xow";
-  version = "0.2";
+  version = "0.4";
 
   src = fetchFromGitHub {
     owner = "medusalix";
     repo = "xow";
     rev = "v${version}";
-    sha256 = "03ajal91xi52svzy621aa4jcdf0vj4pqd52kljam0wryrlmcpbr3";
+    sha256 = "1xkwcx2gqip9v2h3zjmrn7sgcck3midl5alhsmr3zivgdipamynv";
   };
 
-  makeFlags = [ "BUILD=RELEASE" "VERSION=${version}" ];
+  makeFlags = [
+    "BUILD=RELEASE"
+    "VERSION=${version}"
+    "BINDIR=${placeholder ''out''}/bin"
+    "UDEVDIR=${placeholder ''out''}/lib/udev/rules.d"
+    "MODLDIR=${placeholder ''out''}/lib/modules-load.d"
+    "MODPDIR=${placeholder ''out''}/lib/modprobe.d"
+    "SYSDDIR=${placeholder ''out''}/lib/systemd/system"
+  ];
   enableParallelBuilding = true;
   buildInputs = [ libusb ];
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    cp xow $out/bin
-    runHook postInstall
-  '';
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/medusalix/xow";
     description = "Linux driver for the Xbox One wireless dongle";
     license = licenses.gpl2Plus;
-    maintainers = [ maintainers.pmiddend ];
+    maintainers = [ maintainers.jansol ];
     platforms = platforms.linux;
   };
 }
