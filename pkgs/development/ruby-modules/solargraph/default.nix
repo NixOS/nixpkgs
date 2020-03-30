a1cb3084a1e19aa64d438c1e2696228c87724c76
Burke Libbey <burke@libbey.me>
2020-03-30 15:02:52 -0400

    solargraph: Document the original bundle, not the bundlerApp's:
    bundlerApp creates a bundle, but when running solargraph as a
    bundlerApp, you would ~always want to interact with the original bundle,
    not the one containing solargraph and its dependencies.

--- pkgs/development/ruby-modules/solargraph/default.nix
+++ pkgs/development/ruby-modules/solargraph/default.nix
@@ -1,9 +1,18 @@
-{ lib, bundlerApp, bundlerUpdateScript }:
+{ lib, defaultGemConfig, bundlerApp, bundlerUpdateScript }:
 
 bundlerApp {
   pname = "solargraph";
   exes = ["solargraph"  "solargraph-runtime"];
   gemdir = ./.;
+
+  # Solargraph tries to analyze the active bundle, but bundlerApp creates a bundle.
+  # This patch hacks it to look at the original bundle instead.
+  gemConfig = defaultGemConfig // {
+    solargraph = attrs: {
+      patches = [ ./0001-Analyze-original-bundle-rather-than-solargraph-s.patch ];
+      dontBuild = false;
+    };
+  };
 
   passthru.updateScript = bundlerUpdateScript "solargraph";
 
