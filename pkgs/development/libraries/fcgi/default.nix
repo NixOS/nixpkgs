35523fbb0d8a1df4a6e2cd31a97a5f8063b1901d
Jörg Thalheim <joerg@thalheim.io>
2020-03-31 08:35:52 +0100

    fcgi: 2.4.0 -> 2.4.2

--- pkgs/development/libraries/fcgi/default.nix
+++ pkgs/development/libraries/fcgi/default.nix
@@ -1,24 +1,17 @@
-{ stdenv, fetchurl, fetchpatch }:
+{ stdenv, fetchFromGitHub, fetchpatch, autoreconfHook }:
 
 stdenv.mkDerivation rec {
   pname = "fcgi";
-  version = "2.4.0";
+  version = "2.4.2";
 
-  src = fetchurl {
-    url = "https://launchpad.net/debian/+archive/primary/+files/libfcgi_${version}.orig.tar.gz";
-    #    url = "http://www.fastcgi.com/dist/${name}.tar.gz";
-    sha256 = "1f857wnl1d6jfrgfgfpz3zdaj8fch3vr13mnpcpvy8bang34bz36";
+  src = fetchFromGitHub {
+    owner = "FastCGI-Archives";
+    repo = "fcgi2";
+    rev = version;
+    sha256 = "1jhz6jfwv5kawa8kajvg18nfwc1b30f38zc0lggszd1vcmrwqkz1";
   };
 
-  patches = [
-    ./gcc-4.4.diff
-    (fetchpatch {
-      # Fix a stack-smashing bug:
-      # xhttps://bugs.debian.org/cgi-bin/bugreport.cgi?bug=681591
-      url = "https://bugs.launchpad.net/ubuntu/+source/libfcgi/+bug/933417/+attachment/2745025/+files/poll.patch";
-      sha256 = "0v3gw0smjvrxh1bv3zx9xp633gbv5dd5bcn3ipj6ckqjyv4i6i7m";
-    })
-  ];
+  nativeBuildInputs = [ autoreconfHook ];
 
   postInstall = "ln -s . $out/include/fastcgi";
 
