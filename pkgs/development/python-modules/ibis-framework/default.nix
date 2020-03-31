9de2c465b1b1c8cbbe8b9448db6ddfb1fa68bfa5
Jonathan Ringer <jonringer117@gmail.com>
2020-03-30 19:21:12 -0700

    python3Packages.ibis: 1.2.0 -> 1.3.0, fix tests

--- pkgs/development/python-modules/ibis-framework/default.nix
+++ pkgs/development/python-modules/ibis-framework/default.nix
@@ -1,29 +1,26 @@
-{ lib
-, buildPythonPackage
-, fetchPypi
+{ lib, buildPythonPackage, fetchPypi, isPy27, pythonAtLeast
+, graphviz
 , multipledispatch
 , numpy
 , pandas
+, pyarrow
+, pytest
 , pytz
 , regex
-, toolz
-, isPy27
-, pytest
-, sqlalchemy
 , requests
+, sqlalchemy
 , tables
-, pyarrow
-, graphviz
+, toolz
 }:
 
 buildPythonPackage rec {
   pname = "ibis-framework";
-  version = "1.2.0";
-  disabled = isPy27;
+  version = "1.3.0";
+  disabled = isPy27 || pythonAtLeast "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "3a0b79dae6924be0a79669c881a9a1d4817997ad2f81a0f3b1cd03d70aebb071";
+    sha256 = "1my94a11jzg1hv6ln8wxklbqrg6z5l2l77vr89aq0829yyxacmv7";
   };
 
   propagatedBuildInputs = [
@@ -44,13 +41,16 @@ buildPythonPackage rec {
     pytest
   ];
 
+  # ignore tests which require test dataset, or frameworks not available
   checkPhase = ''
-    pytest ibis
+    pytest ibis \
+      --ignore=ibis/tests/all \
+      --ignore=ibis/{sql,spark}
   '';
 
   meta = with lib; {
     description = "Productivity-centric Python Big Data Framework";
-    homepage = https://github.com/ibis-project/ibis;
+    homepage = "https://github.com/ibis-project/ibis";
     license = licenses.asl20;
     maintainers = [ maintainers.costrouc ];
   };
