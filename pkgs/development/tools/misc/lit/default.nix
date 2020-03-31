fe1929affe341f695c1e0700a424f59def0cf387
Jonathan Ringer <jonringer117@gmail.com>
2020-03-31 10:34:04 -0700

    lit: passthru python interpreter

--- pkgs/development/tools/misc/lit/default.nix
+++ pkgs/development/tools/misc/lit/default.nix
@@ -9,6 +9,10 @@ python3.pkgs.buildPythonApplication rec {
     sha256 = "0vkqv0ijjkfg70j26cxqz75bpn2p78all5j5cw2gfcrn4c5aldf0";
   };
 
+  passthru = {
+    python = python3;
+  };
+
   # Non-standard test suite. Needs custom checkPhase.
   doCheck = false;
 
