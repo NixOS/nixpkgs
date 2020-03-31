ec7bc3258648988afc00ab6ec4a3a6dcc4383ce4
svantepolk <svantepolk@protonmail.com>
2020-03-31 19:11:58 +1000

    raylib: Init at 2.6.0

---
+++ pkgs/development/libraries/libraylib/default.nix
@@ -0,0 +1,36 @@
+with import <nixpkgs> { };
+let
+  version = "2.6.0";
+  name = "raylib-${version}";
+  hash = "494e95eaf8daf3f086116dfd6a5fd2e9a2166fc744eabf4f3067bf887d4fb5ef";
+in
+stdenv.mkDerivation {
+  inherit name;
+
+  meta = {
+    description = "A simple and easy-to-use library to enjoy videogames programming";
+    repo = "raylib";
+    owner = "raysan5";
+    license = stdenv.lib.licenses.zlib;
+    maintainers = [ stdenv.lib.mantainers.svantepolk ];
+    rev = version;
+    sha256 = hash;
+  };
+
+  src = fetchurl {
+    url = "https://github.com/raysan5/raylib/archive/2.6.0.tar.gz";
+    sha256 = hash;
+  };
+
+  buildInputs = [
+    libGL
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXinerama
+    xlibs.libXi
+    xlibs.libXext
+  ];
+
+  nativeBuildInputs = [ cmake ];
+}
