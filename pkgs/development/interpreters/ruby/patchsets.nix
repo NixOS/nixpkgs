a7f1e9f1efbebd657a27d12ecd605a35ac4088d4
Mario Rodas <marsam@users.noreply.github.com>
2020-03-31 04:23:00 -0500

    ruby_2_7: 2.7.0 -> 2.7.1
    Changelog: https://www.ruby-lang.org/en/news/2020/03/31/ruby-2-7-1-released/

--- pkgs/development/interpreters/ruby/patchsets.nix
+++ pkgs/development/interpreters/ruby/patchsets.nix
@@ -16,6 +16,9 @@
     "${patchSet}/patches/ruby/2.6/head/railsexpress/02-improve-gc-stats.patch"
     "${patchSet}/patches/ruby/2.6/head/railsexpress/03-more-detailed-stacktrace.patch"
   ];
-  "2.7.0" = ops useRailsExpress [ # no patches yet (2019-12-25)
+  "2.7.1" = ops useRailsExpress [
+    "${patchSet}/patches/ruby/2.7/head/railsexpress/01-fix-broken-tests-caused-by-ad.patch"
+    "${patchSet}/patches/ruby/2.7/head/railsexpress/02-improve-gc-stats.patch"
+    "${patchSet}/patches/ruby/2.7/head/railsexpress/03-more-detailed-stacktrace.patch"
   ];
 }
