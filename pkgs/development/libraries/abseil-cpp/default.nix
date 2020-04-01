40f57fc01fee626c33e8090c240c00b3fb443c36
Mario Rodas <marsam@users.noreply.github.com>
2020-04-01 04:20:00 -0500

    abseil-cpp: 20191119 -> 20200225.1

--- pkgs/development/libraries/abseil-cpp/default.nix
+++ pkgs/development/libraries/abseil-cpp/default.nix
@@ -2,23 +2,22 @@
 
 stdenv.mkDerivation rec {
   pname = "abseil-cpp";
-  date = "20191119";
-  rev = "8ba96a8244bbe334d09542e92d566673a65c1f78";
-  version = "${date}-${rev}";
+  version = "20200225.1";
 
   src = fetchFromGitHub {
     owner = "abseil";
     repo = "abseil-cpp";
-    rev = rev;
-    sha256 = "089bvlspgdgi40fham20qy1m97gr1jh5k5czz49dincpd18j6inb";
+    rev = version;
+    sha256 = "035bffayslawc19q2gmlkr6n6r7k7mvriaq7352rv6gyzaplr98w";
   };
 
   nativeBuildInputs = [ cmake ];
 
   meta = with stdenv.lib; {
     description = "An open-source collection of C++ code designed to augment the C++ standard library";
-    homepage = https://abseil.io/;
+    homepage = "https://abseil.io/";
     license = licenses.asl20;
+    platforms = platforms.all;
     maintainers = [ maintainers.andersk ];
   };
 }
