d4f2f039c1566ef9ebc640bce0c760cc089c75fb
R. RyanTM <ryantm-bot@ryantm.com>
2020-04-01 09:52:22 +0000

    clojure-lsp: 20200305T151710 -> 20200314T202821

--- pkgs/development/tools/misc/clojure-lsp/default.nix
+++ pkgs/development/tools/misc/clojure-lsp/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "clojure-lsp";
-  version = "20200305T151710";
+  version = "20200314T202821";
 
   src = fetchurl {
     url = "https://github.com/snoe/clojure-lsp/releases/download/release-${version}/${pname}";
-    sha256 = "0zi05skp36azv0b6spxdscal32cj5rc6g0d0gzfnsaavvxd128lg";
+    sha256 = "1phfy4cw8pdx2ylfqc4gkniawlz5qn1bih64cx0hsc5i8n0v6qjz";
   };
 
   dontUnpack = true;
