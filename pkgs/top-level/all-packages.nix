d54afe5fe7b550a8b10842c1952a63839b77878e
Tony Olagbaiye <me@fron.io>
2020-04-01 17:18:11 +0100

    guile-ssh: init at 0.12.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9723,6 +9723,8 @@ in
 
   guile-sqlite3 = callPackage ../development/guile-modules/guile-sqlite3 { };
 
+  guile-ssh = callPackage ../development/guile-modules/guile-ssh { };
+
   guile-xcb = callPackage ../development/guile-modules/guile-xcb {
     guile = guile_2_0;
   };
