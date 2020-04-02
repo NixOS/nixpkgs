2676f8c1752a34b497322d00eb79ccece6077266
Jonathan Ringer <jonringer117@gmail.com>
2020-04-01 23:43:26 -0700

    git-town: 7.2.0 -> 7.3.0

--- pkgs/tools/misc/git-town/default.nix
+++ pkgs/tools/misc/git-town/default.nix
@@ -1,25 +1,25 @@
-{ stdenv, buildGoPackage, fetchFromGitHub }: 
+{ stdenv, buildGoPackage, fetchFromGitHub }:
 
-  buildGoPackage rec {
-    pname = "git-town";
-    version = "7.2.0";
+buildGoPackage rec {
+  pname = "git-town";
+  version = "7.3.0";
 
-    goPackagePath = "github.com/Originate/git-town";
+  goPackagePath = "github.com/Originate/git-town";
 
-    src = fetchFromGitHub {
-      owner = "Originate";
-      repo = "git-town";
-      rev = "v${version}";
-      sha256 = "0hr0c6iya34lanfhsg9kj03l4ajalcfxkbn4bgwh0749smhi6mrj";
-    };
+  src = fetchFromGitHub {
+    owner = "Originate";
+    repo = "git-town";
+    rev = "v${version}";
+    sha256 = "166g9i79hqga8k5wvs0b84q6rqniizzsd39v37s9w16axgdrm6nb";
+  };
 
-    buildFlagsArray = [ "-ldflags=-X github.com/Originate/git-town/src/cmd.version=v${version} -X github.com/Originate/git-town/src/cmd.buildDate=nix" ];
+  buildFlagsArray = [ "-ldflags=-X github.com/Originate/git-town/src/cmd.version=v${version} -X github.com/Originate/git-town/src/cmd.buildDate=nix" ];
 
-    meta = with stdenv.lib; {
-      description = "Generic, high-level git support for git-flow workflows";
-      homepage = http://www.git-town.com/;
-      maintainers = [ maintainers.allonsy ];
-      license = licenses.mit;
-    };
-  }
+  meta = with stdenv.lib; {
+    description = "Generic, high-level git support for git-flow workflows";
+    homepage = "http://www.git-town.com/";
+    maintainers = [ maintainers.allonsy ];
+    license = licenses.mit;
+  };
+}
 
