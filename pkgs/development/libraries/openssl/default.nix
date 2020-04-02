0e5ef8c4709e44dfdf22f742c1ebf92d2420e1cf
Vladimír Čunát <v@cunat.cz>
2020-04-02 11:30:46 +0200

    openssl: 1.1.1d -> 1.1.1f
    https://github.com/pyca/pyopenssl/issues/899#issuecomment-607709065
    The tests in python3.pkgs.pyopenssl succeed! (re-checked on 20.03)
    Fixing this problem we experienced is listed as the only major change:
    https://www.openssl.org/news/openssl-1.1.1-notes.html
    
    (cherry picked from commit 6c8692feb4f0a37a88a1498ca4ad068c6e46ebcb)

--- pkgs/development/libraries/openssl/default.nix
+++ pkgs/development/libraries/openssl/default.nix
@@ -152,17 +152,14 @@ in {
   };
 
   openssl_1_1 = common {
-    version = "1.1.1d";
-    sha256 = "1whinyw402z3b9xlb3qaxv4b9sk4w1bgh9k0y8df1z4x3yy92fhy";
+    version = "1.1.1f";
+    sha256 = "186c6bfe6ecfba7a5b48c47f8a1673d0f3b0e5ba2e25602dd23b629975da3f35";
     patches = [
       ./1.1/nix-ssl-cert-file.patch
 
       (if stdenv.hostPlatform.isDarwin
        then ./1.1/use-etc-ssl-certs-darwin.patch
        else ./1.1/use-etc-ssl-certs.patch)
-
-      ./1.1/cve-2019-1551.patch
-      ./1.1/cve-2019-1551-improve.patch
     ];
     withDocs = true;
   };
