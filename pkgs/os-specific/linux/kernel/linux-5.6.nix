4fbd9e3ab8b9ff17108371b71db20644747536c6
Tim Steinbach <tim@nequissimus.com>
2020-04-02 10:03:15 -0400

    linux: 5.6.1 -> 5.6.2

--- pkgs/os-specific/linux/kernel/linux-5.6.nix
+++ pkgs/os-specific/linux/kernel/linux-5.6.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.6.1";
+  version = "5.6.2";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0c0m9wbzbnhnfsvw2ki7wikvz8fzj94rlbfr76qa8shgdm2gvn26";
+    sha256 = "1fdmcx5fk9wq9yx6vvnw76nvdysbvm83cik1dj1d67lw6bc92k9d";
   };
 } // (args.argsOverride or {}))
