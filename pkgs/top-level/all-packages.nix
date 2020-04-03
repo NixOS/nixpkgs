72fb30d5ccce0c3bc525d35e8b33aee6f3286780
Oleksii Filonenko <brightone@protonmail.com>
2020-04-03 11:55:56 +0300

    kondo: init at 0.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20239,6 +20239,8 @@ in
 
   kodestudio = callPackage ../applications/editors/kodestudio { };
 
+  kondo = callPackage ../applications/misc/kondo { };
+
   konversation = libsForQt5.callPackage ../applications/networking/irc/konversation { };
 
   kotatogram-desktop = qt5.callPackage ../applications/networking/instant-messengers/telegram/kotatogram-desktop { };
