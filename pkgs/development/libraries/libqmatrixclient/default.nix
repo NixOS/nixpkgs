72976bb4e4138b10e52948248189982f1293d9e6
Emery Hemingway <ehmry@posteo.net>
2020-04-03 12:50:52 +0530

    libqmatrixclient_0_5: 0.5.2 -> 0.5.3.2

--- pkgs/development/libraries/libqmatrixclient/default.nix
+++ pkgs/development/libraries/libqmatrixclient/default.nix
@@ -32,7 +32,7 @@ let
 
 in rec {
   libqmatrixclient_0_4 = generic "0.4.2.1" "056hvp2m74wx72yd8vai18siddj9l8bhrvrkc4ia4cwjsqw02kid" "v";
-  libqmatrixclient_0_5 = generic "0.5.2"   "1bhlqfs7251fss4icx794ka614npr6zyrpp4qwc4q5408ykfm7lr" "";
+  libqmatrixclient_0_5 = generic "0.5.3.2" "0gkwr3yw6k2m0j8cc085b5p2q788rf5nhp1p5hc5d55pc7mci2qs" "";
 
   libqmatrixclient = libqmatrixclient_0_4;
 }
