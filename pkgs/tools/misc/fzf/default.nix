3f9c5e9fb44b5bb8607b0780b2f2105e27e354a2
zowoq <59103226+zowoq@users.noreply.github.com>
2020-04-03 18:50:25 +1000

    fzf: 0.21.0-1 -> 0.21.1
    https://github.com/junegunn/fzf/blob/master/CHANGELOG.md#0211
    (cherry picked from commit cc21aa99a4e166b6ddd82da7bfcca0f662a1d07c)

--- pkgs/tools/misc/fzf/default.nix
+++ pkgs/tools/misc/fzf/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "fzf";
-  version = "0.21.0-1";
+  version = "0.21.1";
 
   src = fetchFromGitHub {
     owner = "junegunn";
     repo = pname;
     rev = version;
-    sha256 = "1d4bwcmjirwkkv0m01sx9rxp01iik57iy54zxhdkkz842pxlr2xv";
+    sha256 = "0piz1dzczcw1nsff775zicvpm6iy0iw0v0ba7rj7i0xqv9ni1prw";
   };
 
   modSha256 = "16bb0a9z49jqhh9lmq8rvl7x9vh79mi4ygkb9sm04g41g5z6ag1s";
