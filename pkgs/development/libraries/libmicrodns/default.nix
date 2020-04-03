18f3661f929809bb1071bf4ad03add5e73f60e08
Martin Milata <martin@martinmilata.cz>
2020-04-03 19:22:19 +0200

    libmicrodns: 0.1.0 -> 0.1.2
    - CVE-2020-6071
    - CVE-2020-6072
    - CVE-2020-6073
    - CVE-2020-6077
    - CVE-2020-6078
    - CVE-2020-6079
    
    Buildsystem switched to Meson+Ninja.

--- pkgs/development/libraries/libmicrodns/default.nix
+++ pkgs/development/libraries/libmicrodns/default.nix
@@ -1,28 +1,30 @@
 { stdenv
 , fetchFromGitHub
-, autoreconfHook
+, meson
+, ninja
 , pkgconfig
 }:
 
 stdenv.mkDerivation rec {
-  version = "0.1.0";
+  version = "0.1.2";
   pname = "libmicrodns";
 
   src = fetchFromGitHub {
     owner = "videolabs";
     repo = pname;
     rev = version;
-    sha256 = "1pmf461zn35spbpbls1ih68ki7f8g8xjwhzr2csy63nnyq2k9jji";
+    sha256 = "1yb0j0acidp494d28wqhzhrfski2qjb2a5mp5bq5qrpcg38zyz6i";
   };
 
   nativeBuildInputs = [
-    autoreconfHook
+    meson
+    ninja
     pkgconfig
   ];
 
   meta = with stdenv.lib; {
     description = "Minimal mDNS resolver library, used by VLC";
-    homepage = https://github.com/videolabs/libmicrodns;
+    homepage = "https://github.com/videolabs/libmicrodns";
     license = licenses.lgpl21;
     platforms = platforms.linux;
     maintainers = [ maintainers.shazow ];
