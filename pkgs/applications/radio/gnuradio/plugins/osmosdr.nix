fcee9a05702d4de75137042f26f5d4fe1c98c6bc
Doron Behar <doron.behar@gmail.com>
2020-04-04 23:26:38 +0300

    gnuradio.plugins.osmosdr: cleanup deps and add feature arguments

--- pkgs/applications/radio/gnuradio/plugins/osmosdr.nix
+++ pkgs/applications/radio/gnuradio/plugins/osmosdr.nix
@@ -6,13 +6,32 @@
 , pythonSupport ? true
 , python
 , swig
-, airspy
 , gnuradio
-, hackrf
-, libbladeRF
-, rtl-sdr
-, soapysdr-with-plugins
-, uhd
+# TODO: not avalable because https://cgit.osmocom.org/osmo-sdr/ is not packaged
+, enableOsmoSDR ? false
+, enableFcd ? true
+# TODO: not available because https://github.com/dl1ksv/gr-fcdproplus is not packaged
+, enableFcdPp ? false
+# IQ file source & sink support
+, enableFile ? true
+, enableRtlsdr ? true
+, rtl-sdr ? null
+, enableUhd ? true
+, uhd ? null
+# TODO: not available because https://cgit.osmocom.org/libmirisdr/ is not packaged
+, enableMiri ? false
+, enableHackrf ? true
+, hackrf ? null
+, enableBladerf ? true
+, libbladeRF ? null
+, enableRfspace ? true
+, enableAirspy ? true
+, airspy ? null
+, enableSoapy ? true
+, soapysdr-with-plugins ? null
+, enableRedpitaya ? true
+# TODO: not available (probably) because https://github.com/myriadrf/libfreesrp is not packaged
+, enableFreesrp ? false
 }:
 
 assert pythonSupport -> python != null && swig != null;
@@ -24,6 +43,8 @@ let
   else # if gnuradio.branch == "3.8" then
    "1rdx7fa0xiq0qmgrrbby7z1bblmqhl9qh9jqpsznzxkx91f17ypd"
   ;
+  inherit (stdenv.lib) optionals;
+  onOffBool = b: if b then "ON" else "OFF";
 in
 
 stdenv.mkDerivation rec {
@@ -36,28 +57,44 @@ stdenv.mkDerivation rec {
     sha256 = src_hash;
   };
 
+  propagatedBuildInputs = optionals (pythonSupport) [
+    (python.withPackages (ps: with ps; [
+      cheetah
+    ]))
+  ];
+
   nativeBuildInputs = [
     pkgconfig
-  ];
-  buildInputs = [
     cmake
-    makeWrapper
-    boost
-    airspy
-    gnuradio
-    hackrf
-    libbladeRF
-    rtl-sdr
-    uhd
   ]
-    ++ stdenv.lib.optionals stdenv.isLinux [
-      soapysdr-with-plugins
-    ]
-    ++ stdenv.lib.optionals pythonSupport [
-      python
-      swig
-    ]
+    ++ optionals (pythonSupport) [ swig ]
   ;
+  buildInputs = [
+    boost
+    gnuradio
+  ]
+    ++ optionals (enableRtlsdr) [ rtl-sdr ]
+    ++ optionals (enableUhd) [ uhd ]
+    ++ optionals (enableHackrf) [ hackrf ]
+    ++ optionals (enableAirspy) [ airspy ]
+    ++ optionals (enableSoapy) [ soapysdr-with-plugins ]
+    ++ optionals (enableBladerf) [ libbladeRF ]
+  ;
+  cmakeFlags = [
+    "-DENABLE_OSMO_SDR=${onOffBool enableOsmoSDR}"
+    "-DENABLE_FCD_PP=${onOffBool enableFcdPp}"
+    "-DENABLE_FCD=${onOffBool enableFcd}"
+    "-DENABLE_FILE=${onOffBool enableFile}"
+    "-DENABLE_RTLSDR=${onOffBool enableRtlsdr}"
+    "-DENABLE_UHD=${onOffBool enableUhd}"
+    "-DENABLE_MIRI=${onOffBool enableMiri}"
+    "-DENABLE_HACKRF=${onOffBool enableHackrf}"
+    "-DENABLE_RFSPACE=${onOffBool enableRfspace}"
+    "-DENABLE_AIRSPY=${onOffBool enableAirspy}"
+    "-DENABLE_SOAPY=${onOffBool enableSoapy}"
+    "-DENABLE_REDPITAYA=${onOffBool enableRedpitaya}"
+    "-DENABLE_FREESRP=${onOffBool enableFreesrp}"
+  ];
 
   enableParallelBuilding = true;
 
