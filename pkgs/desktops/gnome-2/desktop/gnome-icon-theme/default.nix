a0aca4e0c3e2ac669bf23975bca7bbe8bab55e94
José Romildo Malaquias <malaquias@gmail.com>
2020-04-03 22:09:52 -0300

    gnome2.gnome_icon_theme: 2.91.93 -> 3.12.0

--- pkgs/desktops/gnome-2/desktop/gnome-icon-theme/default.nix
+++ pkgs/desktops/gnome-2/desktop/gnome-icon-theme/default.nix
@@ -1,12 +1,11 @@
 { stdenv, fetchurl, pkgconfig, intltool, iconnamingutils, gtk2 }:
 
 stdenv.mkDerivation rec {
-  name = "gnome-icon-theme-2.91.93";
+  name = "gnome-icon-theme-3.12.0";
 
   src = fetchurl {
-    #url = "mirror://gnome/sources/gnome-icon-theme/3.4/${name}.tar.xz";
-    url = "mirror://gnome/sources/gnome-icon-theme/2.91/${name}.tar.bz2";
-    sha256 = "cc7f15e54e2640697b58c26e74cc3f6ebadeb4ef6622bffe9c1e6874cc3478d6";
+    url = "mirror://gnome/sources/gnome-icon-theme/3.12/${name}.tar.xz";
+    sha256 = "0fjh9qmmgj34zlgxb09231ld7khys562qxbpsjlaplq2j85p57im";
   };
 
   nativeBuildInputs = [ pkgconfig intltool iconnamingutils gtk2 ];
