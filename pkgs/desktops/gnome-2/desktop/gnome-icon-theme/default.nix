6e337f2f9e6b5b1c2649896085989585dae78c30
José Romildo Malaquias <malaquias@gmail.com>
2020-04-03 22:12:12 -0300

    gnome2.gnome_icon_theme: use pname and version

--- pkgs/desktops/gnome-2/desktop/gnome-icon-theme/default.nix
+++ pkgs/desktops/gnome-2/desktop/gnome-icon-theme/default.nix
@@ -1,10 +1,11 @@
 { stdenv, fetchurl, pkgconfig, intltool, iconnamingutils, gtk2 }:
 
 stdenv.mkDerivation rec {
-  name = "gnome-icon-theme-3.12.0";
+  pname = "gnome-icon-theme";
+  version = "3.12.0";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gnome-icon-theme/3.12/${name}.tar.xz";
+    url = "mirror://gnome/sources/gnome-icon-theme/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
     sha256 = "0fjh9qmmgj34zlgxb09231ld7khys562qxbpsjlaplq2j85p57im";
   };
 
