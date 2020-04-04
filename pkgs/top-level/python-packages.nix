c40b68bf136ffbe3319a2d0548387707492bf39c
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-04-04 11:33:35 -0400

    python3Packages.pulumi: init at 1.14.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1058,6 +1058,8 @@ in {
 
   babelgladeextractor = callPackage ../development/python-modules/babelgladeextractor { };
 
+  pulumi = callPackage ../development/python-modules/pulumi { };
+
   backcall = callPackage ../development/python-modules/backcall { };
 
   backoff = callPackage ../development/python-modules/backoff { };
