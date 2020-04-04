cbfdbbfdf84cc614d20b1cbd5026e2ff8973a867
Linus Heckemann <git@sphalerite.org>
2020-04-04 15:46:08 +0200

    perl-modules: fix building

--- pkgs/development/perl-modules/generic/default.nix
+++ pkgs/development/perl-modules/generic/default.nix
@@ -19,17 +19,19 @@ toPerlModule(stdenv.mkDerivation (
 
     checkTarget = "test";
 
-    # Prevent CPAN downloads.
-    PERL_AUTOINSTALL = "--skipdeps";
+    env = {
+      # Prevent CPAN downloads.
+      PERL_AUTOINSTALL = "--skipdeps";
 
-    # From http://wiki.cpantesters.org/wiki/CPANAuthorNotes: "allows
-    # authors to skip certain tests (or include certain tests) when
-    # the results are not being monitored by a human being."
-    AUTOMATED_TESTING = true;
+      # From http://wiki.cpantesters.org/wiki/CPANAuthorNotes: "allows
+      # authors to skip certain tests (or include certain tests) when
+      # the results are not being monitored by a human being."
+      AUTOMATED_TESTING = true;
 
-    # current directory (".") is removed from @INC in Perl 5.26 but many old libs rely on it
-    # https://metacpan.org/pod/release/XSAWYERX/perl-5.26.0/pod/perldelta.pod#Removal-of-the-current-directory-%28%22.%22%29-from-@INC
-    PERL_USE_UNSAFE_INC = "1";
+      # current directory (".") is removed from @INC in Perl 5.26 but many old libs rely on it
+      # https://metacpan.org/pod/release/XSAWYERX/perl-5.26.0/pod/perldelta.pod#Removal-of-the-current-directory-%28%22.%22%29-from-@INC
+      PERL_USE_UNSAFE_INC = "1";
+    };
 
     meta.homepage = "https://metacpan.org/release/${lib.getName attrs}"; # TODO: phase-out `attrs.name`
     meta.platforms = perl.meta.platforms;
