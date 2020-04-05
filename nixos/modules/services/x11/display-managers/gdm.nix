f74f2f354866c828248a419ef9a2cbddc793b7f9
Jan Tojnar <jtojnar@gmail.com>
2020-04-05 16:30:17 +0200

    nixos/gdm: fix startup
    In https://github.com/NixOS/nixpkgs/commit/7f838b4ddeadffbbe5863a6ca27ad776064a2f4a, we dropped systemd-udev-settle.service from display-manager.service's wants.
    Unfortunately, we are doing something wrong since without it both Xorg and Wayland fail to start:
    
    	Failed to open gpu '/dev/dri/card0': GDBus.Error:org.freedesktop.DBus.Error.AccessDenied: Operation not permitted
    
    Until we sort this out, let's add systemd-udev-settle.service to GDM to unblock the channels.

--- nixos/modules/services/x11/display-managers/gdm.nix
+++ nixos/modules/services/x11/display-managers/gdm.nix
@@ -188,6 +188,9 @@ in
       "systemd-machined.service"
       # setSessionScript wants AccountsService
       "accounts-daemon.service"
+      # Failed to open gpu '/dev/dri/card0': GDBus.Error:org.freedesktop.DBus.Error.AccessDenied: Operation not permitted
+      # https://github.com/NixOS/nixpkgs/pull/25311#issuecomment-609417621
+      "systemd-udev-settle.service"
     ];
 
     systemd.services.display-manager.after = [
@@ -197,6 +200,7 @@ in
       "getty@tty${gdm.initialVT}.service"
       "plymouth-quit.service"
       "plymouth-start.service"
+      "systemd-udev-settle.service"
     ];
     systemd.services.display-manager.conflicts = [
        "getty@tty${gdm.initialVT}.service"
