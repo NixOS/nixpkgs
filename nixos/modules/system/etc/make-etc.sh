59d6f2a1f860f6cf710b3829cf1b58130c244f99
Linus Heckemann <git@sphalerite.org>
2020-04-05 17:09:42 +0200

    nixos/etc: fix building etc with structuredAttrs
    Yay, no more should-use-XML todo!

--- nixos/modules/system/etc/make-etc.sh
+++ nixos/modules/system/etc/make-etc.sh
@@ -1,32 +1,22 @@
-source $stdenv/setup
+mkdir -p "$out/etc"
 
-mkdir -p $out/etc
-
-set -f
-sources_=($sources)
-targets_=($targets)
-modes_=($modes)
-users_=($users)
-groups_=($groups)
-set +f
-
-for ((i = 0; i < ${#targets_[@]}; i++)); do
-    source="${sources_[$i]}"
-    target="${targets_[$i]}"
+for ((i = 0; i < ${#targets[@]}; i++)); do
+    source="${sources[$i]}"
+    target="${targets[$i]}"
 
     if [[ "$source" =~ '*' ]]; then
 
         # If the source name contains '*', perform globbing.
-        mkdir -p $out/etc/$target
+        mkdir -p "$out/etc/$target"
         for fn in $source; do
-            ln -s "$fn" $out/etc/$target/
+            ln -s "$fn" "$out/etc/$target/"
         done
 
     else
-        
-        mkdir -p $out/etc/$(dirname $target)
-        if ! [ -e $out/etc/$target ]; then
-            ln -s $source $out/etc/$target
+
+        mkdir -p "$out/etc/$(dirname "$target")"
+        if ! [ -e "$out/etc/$target" ]; then
+            ln -s "$source" "$out/etc/$target"
         else
             echo "duplicate entry $target -> $source"
             if test "$(readlink $out/etc/$target)" != "$source"; then
@@ -34,13 +24,12 @@ for ((i = 0; i < ${#targets_[@]}; i++)); do
                 exit 1
             fi
         fi
-        
-        if test "${modes_[$i]}" != symlink; then
-            echo "${modes_[$i]}"  > $out/etc/$target.mode
-            echo "${users_[$i]}"  > $out/etc/$target.uid
-            echo "${groups_[$i]}" > $out/etc/$target.gid
+
+        if test "${modes[$i]}" != symlink; then
+            echo "${modes[$i]}"  > "$out/etc/$target.mode"
+            echo "${users[$i]}"  > "$out/etc/$target.uid"
+            echo "${groups[$i]}" > "$out/etc/$target.gid"
         fi
-        
     fi
 done
 
