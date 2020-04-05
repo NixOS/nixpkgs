71db83c38eb7486f915a32ba1d9adef20c3712d0
Doron Behar <doron.behar@gmail.com>
2020-04-05 21:34:10 +0300

    gnuradio.plugins.limesdr: use appropriate src for gnuradio version

--- pkgs/applications/radio/gnuradio/plugins/limesdr.nix
+++ pkgs/applications/radio/gnuradio/plugins/limesdr.nix
@@ -13,7 +13,12 @@
 assert pythonSupport -> python != null && swig != null;
 
 let
-  version = "2.0.0";
+  version = if gnuradio.branch == "3.7" then "2.0.0" else "3.0.1";
+  src_hash = if gnuradio.branch == "3.7" then
+   "0ldqvfwl0gil89l9s31fjf9d7ki0dk572i8vna336igfaz348ypq"
+  else # if gnuradio.branch == "3.8" then
+   "1m9kdxvcnw8j7lm3dysccrc90l23kgv2fnawbf4bxjil6pqkxyvx"
+  ;
 
 in stdenv.mkDerivation {
   pname = "gr-limesdr";
@@ -23,7 +28,7 @@ in stdenv.mkDerivation {
     owner = "myriadrf";
     repo = "gr-limesdr";
     rev = "v${version}";
-    sha256 = "0ldqvfwl0gil89l9s31fjf9d7ki0dk572i8vna336igfaz348ypq";
+    sha256 = src_hash;
   };
 
   nativeBuildInputs = [
