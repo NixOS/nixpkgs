1e25c474d9e257a758a42934feec748a377b48e7
AndersonTorres <torres.anderson.85@protonmail.com>
2020-04-05 11:34:22 -0300

    [WIP] {help wanted} twolame: 2017-09-27 -> 0.4.0

--- pkgs/development/libraries/twolame/default.nix
+++ pkgs/development/libraries/twolame/default.nix
@@ -1,25 +1,27 @@
 { stdenv, fetchFromGitHub
-, autoreconfHook, pkgconfig
-, libsndfile }:
+, autoreconfHook
+, pkgconfig
+, libsndfile, xmlto, asciidoc }:
 
-stdenv.mkDerivation {
+with stdenv.lib;
+stdenv.mkDerivation rec {
 
   pname = "twolame";
-  version = "2017-09-27";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "njh";
     repo = "twolame";
-    rev = "977c8ac55d8ca6d5f35d1d413a119dac2b3b0333";
-    sha256 = "1rq3yc8ygzdqid9zk6pixmm4w9sk2vrlx217lhn5bjaglv7iyf7x";
+    rev = version;
+    sha256 = "0ckwdx5kmgmh1jq2wl6c9d57pf6z6p2wjdy6fw01a8f3clg21a8g";
   };
 
-  nativeBuildInputs = [ autoreconfHook pkgconfig ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig xmlto asciidoc ];
   buildInputs = [ libsndfile ];
 
   doCheck = false; # fails with "../build-scripts/test-driver: line 107: -Mstrict: command not found"
 
-  meta = with stdenv.lib;{
+  meta = {
     description = "A MP2 encoder";
     longDescription = ''
       TwoLAME is an optimised MPEG Audio Layer 2 (MP2) encoder based on
