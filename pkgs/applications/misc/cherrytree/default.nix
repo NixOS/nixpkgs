5d08950e5e619d940c2c624dd58c2633903cf927
AndersonTorres <torres.anderson.85@protonmail.com>
2020-04-05 14:33:23 -0300

    cherrytree: 0.39.0 -> 0.39.2

--- pkgs/applications/misc/cherrytree/default.nix
+++ pkgs/applications/misc/cherrytree/default.nix
@@ -1,12 +1,15 @@
-{ lib, fetchurl, pythonPackages, gettext }:
+{ lib, fetchFromGitHub, pythonPackages, gettext }:
 
 pythonPackages.buildPythonApplication rec {
   pname = "cherrytree";
-  version = "0.39.1";
+  version = "0.39.2";
 
-  src = fetchurl {
-    url = "https://www.giuspen.com/software/${pname}-${version}.tar.xz";
-    sha256 = "0qhycblnixvbybzr8psgmgcpfs6jc9m0p2h9lmd5zmiaggqlcsv7";
+  src = fetchFromGitHub {
+    owner = "giuspen";
+    repo = "cherrytree";
+    rev = version;
+    sha256 = "1l6wh24bhp4yhmsfmc0r4n2n10nlilkv4cmv5sfl80i250fiw7xa";
+
   };
 
   nativeBuildInputs = [ gettext ];
@@ -20,17 +23,16 @@ pythonPackages.buildPythonApplication rec {
   meta = with lib; {
     description = "An hierarchical note taking application";
     longDescription = ''
-      Cherrytree is an hierarchical note taking application,
-      featuring rich text, syntax highlighting and powerful search
-      capabilities. It organizes all information in units called
-      "nodes", as in a tree, and can be very useful to store any piece
-      of information, from tables and links to pictures and even entire
-      documents. All those little bits of information you have scattered
-      around your hard drive can be conveniently placed into a
-      Cherrytree document where you can easily find it.
+      Cherrytree is an hierarchical note taking application, featuring rich
+      text, syntax highlighting and powerful search capabilities. It organizes
+      all information in units called "nodes", as in a tree, and can be very
+      useful to store any piece of information, from tables and links to
+      pictures and even entire documents. All those little bits of information
+      you have scattered around your hard drive can be conveniently placed into
+      a Cherrytree document where you can easily find it.
     '';
     homepage = "http://www.giuspen.com/cherrytree";
     license = licenses.gpl3;
-    maintainers = with maintainers; [ AndersonTorres ];
+    maintainers = with maintainers; [ ];
   };
 }
