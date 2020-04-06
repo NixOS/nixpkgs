30b05e1a0429451eaf3e56179c76812c9a5d0757
toonn <toonn@toonn.io>
2020-04-06 18:25:29 +0200

    wire-desktop: mac 3.15.3621 -> 3.16.3630
    (cherry picked from commit 39c5e1c7233a6845b1da321aa7c0270e33b87435)

--- pkgs/applications/networking/instant-messengers/wire-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/wire-desktop/default.nix
@@ -18,13 +18,13 @@ let
   pname = "wire-desktop";
 
   version = {
-    x86_64-linux = "3.11.2912";
-    x86_64-darwin = "3.10.3215";
+    x86_64-darwin = "3.16.3630";
+    x86_64-linux = "3.16.2923";
   }.${system} or throwSystem;
 
   sha256 = {
-    x86_64-linux = "1d2wa13d750dd2vslnvzf0ibwjmf5s299pxq0rs2x98y2sabw3sl";
-    x86_64-darwin = "0ygm3fgy9k1dp2kjfwsrrwq1i88wgxc6k8y80yz61ivdawgph9wa";
+    x86_64-darwin = "1lnjn45bhd36n9xgx6xx9cggwivvkr2s6r4zai2dwg0aac1bywr5";
+    x86_64-linux = "0c8jmlsg2gnxsvly04xj1al80nj52rg4czfdha58sg14x14lyspz";
   }.${system} or throwSystem;
 
   meta = with stdenv.lib; {
@@ -110,7 +110,7 @@ let
 
     src = fetchurl {
       url = "https://github.com/wireapp/wire-desktop/releases/download/"
-        + "macos%2F${version}/Wire.pkg";
+          + "macos%2F${version}/Wire.pkg";
       inherit sha256;
     };
 
