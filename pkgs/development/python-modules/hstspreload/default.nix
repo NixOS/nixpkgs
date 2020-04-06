c05bdeb23b6121f92d83cbeb88ed66f0a6dd5d6f
R. RyanTM <ryantm-bot@ryantm.com>
2020-04-06 17:22:09 +0000

    python37Packages.hstspreload: 2020.2.29 -> 2020.3.31

--- pkgs/development/python-modules/hstspreload/default.nix
+++ pkgs/development/python-modules/hstspreload/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "hstspreload";
-  version = "2020.2.29";
+  version = "2020.3.31";
   disabled = isPy27;
 
   src = fetchFromGitHub {
     owner = "sethmlarson";
     repo = pname;
     rev = version;
-    sha256 = "1s6f9sdr5l9dqri92s8qr7r1nyvai3vnpcaw06293kc8dribi0m2";
+    sha256 = "12hncxzawvdsrssl350xxn1byfm1firgd3ziqfll4xhhw403jaa9";
   };
 
   # tests require network connection
