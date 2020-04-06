741a3269e257f4a9d74f5e8798f102c720bba79c
Seabass-Chan <54417615+privateseabass@users.noreply.github.com>
2020-04-05 22:42:19 -0700

    protonmail-bridge: 1.2.3-1 -> 1.2.6-1

--- pkgs/applications/networking/protonmail-bridge/default.nix
+++ pkgs/applications/networking/protonmail-bridge/default.nix
@@ -2,7 +2,7 @@
 , libsecret, libGL, libpulseaudio, glib, wrapQtAppsHook, mkDerivation }:
 
 let
-  version = "1.2.3-1";
+  version = "1.2.6-1";
 
   description = ''
     An application that runs on your computer in the background and seamlessly encrypts
@@ -16,7 +16,7 @@ in mkDerivation {
 
   src = fetchurl {
     url = "https://protonmail.com/download/protonmail-bridge_${version}_amd64.deb";
-    sha256 = "032ggk9fvd19fbsqkzwzwh0hpyg8gpkrin71di7zsx6ias5innw1";
+    sha256 = "34fdc917fb2c6f06ec6c6a71041f31ad3599b4ee5556e09ad919c36d71eac247";
   };
 
   sourceRoot = ".";
