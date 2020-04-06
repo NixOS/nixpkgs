f9a65afdef3a821f186c864b20756204fea0d1ff
Matthias Beyer <mail@beyermatthias.de>
2020-04-06 18:16:00 +0200

    peertube: update users.{users, groups} to follow recent standards
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>
    Suggested-by: Immae <contact@mail.immae.eu>

--- nixos/modules/services/web-apps/peertube/default.nix
+++ nixos/modules/services/web-apps/peertube/default.nix
@@ -63,19 +63,20 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    users.users = lib.optionalAttrs (cfg.user == name) (lib.singleton {
-      inherit name;
-      inherit uid;
-      group = cfg.group;
-      description = "Peertube user";
-      home = cfg.dataDir;
-      useDefaultShell = true;
-    });
-
-    users.groups = lib.optionalAttrs (cfg.group == name) (lib.singleton {
-      inherit name;
-      inherit gid;
-    });
+    users.users = lib.optionalAttrs (cfg.user == name) {
+      "${name}" = {
+        inherit uid;
+        group = cfg.group;
+        description = "Peertube user";
+        home = cfg.dataDir;
+        useDefaultShell = true;
+      };
+    };
+    users.groups = lib.optionalAttrs (cfg.group == name) {
+      "${name}" = {
+        inherit gid;
+      };
+    };
 
     systemd.services.peertube = {
       description = "Peertube";
