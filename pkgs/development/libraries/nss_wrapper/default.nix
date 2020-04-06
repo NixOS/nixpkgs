b60dc46142fc56f26640e523b8eccdb33f6a7638
R. RyanTM <ryantm-bot@ryantm.com>
2020-04-06 06:04:08 +0000

    nss_wrapper: 1.1.10 -> 1.1.11

--- pkgs/development/libraries/nss_wrapper/default.nix
+++ pkgs/development/libraries/nss_wrapper/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, cmake, pkgconfig }:
 
 stdenv.mkDerivation rec {
-  name = "nss_wrapper-1.1.10";
+  name = "nss_wrapper-1.1.11";
 
   src = fetchurl {
     url = "mirror://samba/cwrap/${name}.tar.gz";
-    sha256 = "1fifl3allz4hwl331j6fwacc4z2fqwyxdnnkadv518ir8nza3xl8";
+    sha256 = "1q5l6w69yc71ly8gcbnkrcbnq6b64cbiiv99m0z5vn5lgwp36igv";
   };
 
   nativeBuildInputs = [ pkgconfig ];
