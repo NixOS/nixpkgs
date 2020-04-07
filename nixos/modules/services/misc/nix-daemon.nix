e6a15db534d18d0eefb5916ee87b4951d51af2cf
Jörg Thalheim <joerg@thalheim.io>
2020-04-07 08:21:02 +0100

    nixos: default nix.maxJobs to auto
    Instead of making the configuration less portable by hard coding the number of
    jobs equal to the cores we can also let nix set the same number at runtime.

--- nixos/modules/services/misc/nix-daemon.nix
+++ nixos/modules/services/misc/nix-daemon.nix
@@ -92,13 +92,14 @@ in
 
       maxJobs = mkOption {
         type = types.either types.int (types.enum ["auto"]);
-        default = 1;
+        default = "auto";
         example = 64;
         description = ''
-          This option defines the maximum number of jobs that Nix will try
-          to build in parallel.  The default is 1.  You should generally
-          set it to the total number of logical cores in your system (e.g., 16
-          for two CPUs with 4 cores each and hyper-threading).
+          This option defines the maximum number of jobs that Nix will try to
+          build in parallel. The default is auto, which means it will use all
+          available logical cores. It is recommend to set it to the total
+          number of logical cores in your system (e.g., 16 for two CPUs with 4
+          cores each and hyper-threading).
         '';
       };
 
