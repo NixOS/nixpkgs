3c700b8aa669376d4fdf9567e452a1898fdcd3a6
Maximilian Bosch <maximilian@mbosch.me>
2020-04-07 13:19:02 +0200

    cargo-make: 0.30.2 -> 0.30.4
    https://github.com/sagiegurari/cargo-make/releases/tag/0.30.3
    https://github.com/sagiegurari/cargo-make/releases/tag/0.30.4
    (cherry picked from commit 564af64783c62ebeb9fcdc6aeaa90c914df0a3d5)

--- pkgs/development/tools/rust/cargo-make/default.nix
+++ pkgs/development/tools/rust/cargo-make/default.nix
@@ -2,7 +2,7 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-make";
-  version = "0.30.2";
+  version = "0.30.4";
 
   src =
     let
@@ -10,7 +10,7 @@ rustPlatform.buildRustPackage rec {
         owner = "sagiegurari";
         repo = pname;
         rev = version;
-        sha256 = "1p7j80vqlrrprrlnaysng542kl3q87r6kv9mxzdb8605g1ma8wd6";
+        sha256 = "14sl7kcr1n3xb912vd3445pwf3v1kp74lgxlwqybnwiyh1dxihvg";
       };
     in
     runCommand "source" {} ''
