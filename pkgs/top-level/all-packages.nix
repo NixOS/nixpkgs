25c3d0da53ffae6201e6333f763074d02b2d00e4
Yves Fischer <yvesf+git@xapek.org>
2020-04-07 23:46:50 +0200

    qcad: init at 3.24.2.1 (#81209)
    Co-authored-by: zimbatm <zimbatm@zimbatm.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21339,6 +21339,8 @@ in
     guiSupport = false;
   };
 
+  qcad = libsForQt5.callPackage ../applications/misc/qcad { };
+
   qcomicbook = libsForQt5.callPackage ../applications/graphics/qcomicbook { };
 
   eiskaltdcpp = callPackage ../applications/networking/p2p/eiskaltdcpp {
