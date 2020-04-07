c06918fb2fe696f6c8d3b4858b5e009eecdda8aa
Jan Tojnar <jtojnar@gmail.com>
2020-04-07 06:01:05 +0200

    dia: format with nixpkgs-fmt
    and reorder the attributes

--- pkgs/applications/graphics/dia/default.nix
+++ pkgs/applications/graphics/dia/default.nix
@@ -1,7 +1,24 @@
-{ stdenv, fetchgit, autoconf, automake, libtool, gtk2, pkgconfig, perlPackages,
-libxml2, gettext, python, libxml2Python, docbook5, docbook_xsl,
-libxslt, intltool, libart_lgpl, withGNOME ? false, libgnomeui,
-gtk-mac-integration-gtk2 }:
+{ stdenv
+, fetchgit
+, autoconf
+, automake
+, libtool
+, gtk2
+, pkgconfig
+, perlPackages
+, libxml2
+, gettext
+, python
+, libxml2Python
+, docbook5
+, docbook_xsl
+, libxslt
+, intltool
+, libart_lgpl
+, withGNOME ? false
+, libgnomeui
+, gtk-mac-integration-gtk2
+}:
 
 stdenv.mkDerivation {
   pname = "dia";
@@ -13,19 +30,40 @@ stdenv.mkDerivation {
     sha256 = "1fyxfrzdcs6blxhkw3bcgkksaf3byrsj4cbyrqgb4869k3ynap96";
   };
 
-  buildInputs =
-    [ gtk2 libxml2 gettext python libxml2Python docbook5
-      libxslt docbook_xsl libart_lgpl ]
-      ++ stdenv.lib.optional withGNOME libgnomeui
-      ++ stdenv.lib.optional stdenv.isDarwin gtk-mac-integration-gtk2;
+  nativeBuildInputs = [
+    autoconf
+    automake
+    libtool
+    pkgconfig
+    intltool
+  ] ++ (with perlPackages; [
+    perl
+    XMLParser
+  ]);
 
-  nativeBuildInputs = [ autoconf automake libtool pkgconfig intltool ]
-    ++ (with perlPackages; [ perl XMLParser ]);
+  buildInputs = [
+    gtk2
+    libxml2
+    gettext
+    python
+    libxml2Python
+    docbook5
+    libxslt
+    docbook_xsl
+    libart_lgpl
+  ] ++ stdenv.lib.optionals withGNOME [
+    libgnomeui
+  ] ++ stdenv.lib.optionals stdenv.isDarwin [
+    gtk-mac-integration-gtk2
+  ];
+
+  configureFlags = stdenv.lib.optionals withGNOME [
+    "--enable-gnome"
+  ];
 
   preConfigure = ''
     NOCONFIGURE=1 ./autogen.sh # autoreconfHook is not enough
   '';
-  configureFlags = stdenv.lib.optional withGNOME "--enable-gnome";
 
   hardeningDisable = [ "format" ];
 
