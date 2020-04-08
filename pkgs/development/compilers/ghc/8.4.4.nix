e8e92a84691772f64c1e057269e656dfb4eada97
Adam Sandberg Ericsson <adam@sandbergericsson.se>
2020-04-08 19:03:44 +0100

    ghc: don't use ld.gold with musl libc (fixes #84670)
    ld.gold doesn't play well with musl as is documented in #49071 and
    https://sourceware.org/bugzilla/show_bug.cgi?id=23856
    
    (cherry picked from commit 4675649d9cd06d13f8f127855dda4993881dd81b)

--- pkgs/development/compilers/ghc/8.4.4.nix
+++ pkgs/development/compilers/ghc/8.4.4.nix
@@ -79,7 +79,7 @@ let
 
   targetCC = builtins.head toolsForTarget;
 
-  useLdGold = targetPlatform.isLinux && !(targetPlatform.useLLVM or false);
+  useLdGold = targetPlatform.isLinux && !(targetPlatform.useLLVM or false) && !targetPlatform.isMusl;
 
 in
 stdenv.mkDerivation (rec {
