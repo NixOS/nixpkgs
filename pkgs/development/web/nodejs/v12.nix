20f7559f9a57f0ce68177a9bbc152a4e0f1b6119
Mario Rodas <marsam@users.noreply.github.com>
2020-04-08 14:42:09 -0500

    nodejs-12_x: 12.16.1 -> 12.16.2
    Changelog: https://github.com/nodejs/node/releases/tag/v12.16.2

--- pkgs/development/web/nodejs/v12.nix
+++ pkgs/development/web/nodejs/v12.nix
@@ -5,6 +5,6 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "12.16.1";
-    sha256 = "0ba1dla31z6i31z3723l74nky1v04irwbl3iaqmi0iicl1dq958a";
+    version = "12.16.2";
+    sha256 = "0y5yd6h13fr34byi7h5xdjaivgcxiz0ykcmpk9nm5ra01b54fp2m";
   }
