44e56c2503d0bfa60e200f9a0dfc1c12d0219b28
misuzu <bakalolka@gmail.com>
2020-04-08 18:44:50 +0300

    kmon: 1.0.0 -> 1.0.1

--- pkgs/tools/system/kmon/default.nix
+++ pkgs/tools/system/kmon/default.nix
@@ -2,20 +2,24 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "kmon";
-  version = "1.0.0";
+  version = "1.0.1";
 
   src = fetchFromGitHub {
     owner = "orhun";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1zsgyigr6k0zqhhv0icpvvfij7r7fj0l2f77i5zh1vyb89pni50h";
+    sha256 = "0kmkcs13cccmjjfbl25bs3m54zcjcs38fiv84q0vy09a4dnx3gn8";
   };
 
-  cargoSha256 = "15lj6ryll24xdmv8gil04prhcri2d3b1rxqjrwx70blfrlvvhxa8";
+  cargoSha256 = "0l1yq9k6lyk0ww1nzk93axylgrwipkmmqh9r6fq4a31wjlblrkkb";
 
   nativeBuildInputs = [ python3 ];
 
   buildInputs = [ libxcb ];
+
+  postInstall = ''
+    install -D man/kmon.8 -t $out/share/man/man8/
+  '';
 
   meta = with stdenv.lib; {
     description = "Linux Kernel Manager and Activity Monitor";
