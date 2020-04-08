692a70ee8c048a4e1b13df4393a289f53ac513dd
Greg Price <gnprice@gmail.com>
2020-04-07 20:51:27 -0700

    stdenv: handle forceShare under structured attrs
    This one has very few references in the tree -- just here,
    the manual, and a handful of expressions setting it.

--- pkgs/build-support/setup-hooks/move-docs.sh
+++ pkgs/build-support/setup-hooks/move-docs.sh
@@ -5,10 +5,17 @@
 preFixupHooks+=(_moveToShare)
 
 _moveToShare() {
-    forceShare=${forceShare:=man doc info}
+    if [ -n "$__structuredAttrs" ]; then
+        if [ -z "${forceShare-}" ]; then
+            forceShare=( man doc info )
+        fi
+    else
+        forceShare=( ${forceShare:-man doc info} )
+    fi
+
     if [ -z "$forceShare" -o -z "$out" ]; then return; fi
 
-    for d in $forceShare; do
+    for d in "${forceShare[@]}"; do
         if [ -d "$out/$d" ]; then
             if [ -d "$out/share/$d" ]; then
                 echo "both $d/ and share/$d/ exist!"
@@ -20,4 +27,3 @@ _moveToShare() {
         fi
     done
 }
-
