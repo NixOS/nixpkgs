32e50391008f58bc3594e26bce46f2260cbb8897
Vincent Laporte <Vincent.Laporte@gmail.com>
2020-04-09 18:58:36 +0200

    ocamlPackages.fmt: 0.8.5 → 0.8.6

--- pkgs/development/ocaml-modules/fmt/default.nix
+++ pkgs/development/ocaml-modules/fmt/default.nix
@@ -1,16 +1,21 @@
-{ stdenv, fetchurl, ocaml, findlib, ocamlbuild, topkg, cmdliner, result, uchar }:
+{ stdenv, fetchurl, ocaml, findlib, ocamlbuild, topkg, cmdliner, seq, stdlib-shims }:
 
-stdenv.mkDerivation {
-  name = "ocaml${ocaml.version}-fmt-0.8.5";
+if !stdenv.lib.versionAtLeast ocaml.version "4.03"
+then throw "fmt is not available for OCaml ${ocaml.version}"
+else
+
+stdenv.mkDerivation rec {
+  version = "0.8.6";
+  pname = "ocaml${ocaml.version}-fmt";
 
   src = fetchurl {
-    url = "https://erratique.ch/software/fmt/releases/fmt-0.8.5.tbz";
-    sha256 = "1zj9azcxcn6skmb69ykgmi9z8c50yskwg03wqgh87lypgjdcz060";
+    url = "https://erratique.ch/software/fmt/releases/fmt-${version}.tbz";
+    sha256 = "1jlw5izgvqw1adzqi87rp0383j0vj52wmacy3rqw87vxkf7a3xin";
   };
 
   nativeBuildInputs = [ ocaml findlib ocamlbuild ];
   buildInputs = [ findlib topkg cmdliner ];
-  propagatedBuildInputs = [ result uchar ];
+  propagatedBuildInputs = [ seq stdlib-shims ];
 
   inherit (topkg) buildPhase installPhase;
 
