e67d840931d235e882f8c35d1615885f99be8911
Ben Darwin <bcdarwin@gmail.com>
2020-04-09 12:22:12 -0400

    python2Packages.apptools: 4.4.0 -> 4.5.0

--- pkgs/development/python-modules/apptools/default.nix
+++ pkgs/development/python-modules/apptools/default.nix
@@ -1,18 +1,18 @@
-{ stdenv, fetchPypi, buildPythonPackage
-, traits, traitsui, configobj
+{ lib, fetchPypi, buildPythonPackage
+, configobj, six, traitsui
 , nose, tables, pandas
 }:
 
 buildPythonPackage rec {
   pname = "apptools";
-  version = "4.4.0";
+  version = "4.5.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1dw6vvq7lqkj7mgn3s7r5hs937kl4mj5g7jf2qgvhdld9lsc5xbk";
+    sha256 = "10h52ibhr2aw076pivqxiajr9rpcr1mancg6xlpxzckcm3if02i6";
   };
 
-  propagatedBuildInputs = [ traits traitsui configobj ];
+  propagatedBuildInputs = [ configobj six traitsui ];
 
   checkInputs = [
     nose
@@ -21,11 +21,12 @@ buildPythonPackage rec {
   ];
 
   doCheck = true;
+  checkPhase = ''HOME=$TMP nosetests'';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Set of packages that Enthought has found useful in creating a number of applications.";
-    homepage = https://github.com/enthought/apptools;
-    maintainers = with stdenv.lib.maintainers; [ knedlsepp ];
+    homepage = "https://github.com/enthought/apptools";
+    maintainers = with maintainers; [ knedlsepp ];
     license = licenses.bsdOriginal;
   };
 }
