323830282748c1fb31647fc615e67e926a044d23
Jonathan Ringer <jonringer117@gmail.com>
2020-04-09 10:39:15 -0700

    libpoly: fix src

--- pkgs/applications/science/logic/poly/default.nix
+++ pkgs/applications/science/logic/poly/default.nix
@@ -7,8 +7,9 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "SRI-CSL";
     repo = "libpoly";
-    rev = "v${version}";
-    sha256 = "19ddzrir20571zqg720ajqpl59lhpc6c18bp763r6rw68d9zbjch";
+    # they've pushed to the release branch, use explicit tag
+    rev = "refs/tags/v${version}";
+    sha256 = "1n3gijksnl2ybznq4lkwm2428f82423sxq18gnb2g1kiwqlzdaa3";
   };
 
   nativeBuildInputs = [ cmake ];
