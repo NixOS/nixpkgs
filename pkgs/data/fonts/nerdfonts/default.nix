6ae7abcc1a75b8be2675bb8c251ebc16c90ee882
Doron Behar <doron.behar@gmail.com>
2020-04-10 01:22:24 +0300

    nerdfonts: meta: fix descriptions

--- pkgs/data/fonts/nerdfonts/default.nix
+++ pkgs/data/fonts/nerdfonts/default.nix
@@ -21,13 +21,14 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with stdenv.lib; {
-    description = ''
+    description = "Iconic font aggregator, collection, & patcher. 3,600+ icons, 50+ patched fonts";
+    longDescription = ''
       Nerd Fonts is a project that attempts to patch as many developer targeted
       and/or used fonts as possible. The patch is to specifically add a high
       number of additional glyphs from popular 'iconic fonts' such as Font
       Awesome, Devicons, Octicons, and others.
     '';
-    homepage = https://github.com/ryanoasis/nerd-fonts;
+    homepage = "https://nerdfonts.com/";
     license = licenses.mit;
     maintainers = with maintainers; [ doronbehar ];
     hydraPlatforms = []; # 'Output limit exceeded' on Hydra
