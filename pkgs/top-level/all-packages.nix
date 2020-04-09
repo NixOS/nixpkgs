e8f668185e472cc1264f338eb3dddefa19e06523
Jonathan Ringer <jonringer117@gmail.com>
2020-04-09 10:25:18 -0700

    leo-editor: add qt support

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20286,7 +20286,7 @@ in
 
   legit = gitAndTools.legit;
 
-  leo-editor = callPackage ../applications/editors/leo-editor { };
+  leo-editor = libsForQt5.callPackage ../applications/editors/leo-editor { };
 
   libowfat = callPackage ../development/libraries/libowfat { };
 
