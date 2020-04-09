cdda61ab6938f0a5aa422a34930989e6697214ae
R. RyanTM <ryantm-bot@ryantm.com>
2020-04-09 15:42:03 +0000

    dit: 0.5 -> 0.6

--- pkgs/applications/editors/dit/default.nix
+++ pkgs/applications/editors/dit/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "dit";
-  version = "0.5";
+  version = "0.6";
 
   src = fetchurl {
     url = "https://hisham.hm/dit/releases/${version}/${pname}-${version}.tar.gz";
-    sha256 = "05vhr1gl3bb5fg49v84xhmjaqdjw6djampvylw10ydvbpnpvjvjc";
+    sha256 = "0ryvm54xxkg2gcgz4r8zdxrl6j2h8mgg9nfqmdmdr31qkcj8wjsq";
   };
 
   buildInputs = [ ncurses lua ]
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "A console text editor for Unix that you already know how to use";
-    homepage = https://hisham.hm/dit/;
+    homepage = "https://hisham.hm/dit/";
     license = licenses.gpl2;
     platforms = with platforms; linux;
     maintainers = with maintainers; [ davidak ];
