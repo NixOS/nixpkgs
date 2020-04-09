fe72fe28d7503ad8888ef418f36090a3c8bfdd17
R. RyanTM <ryantm-bot@ryantm.com>
2020-04-09 10:03:01 +0000

    yafaray-core: 3.3.0 -> 3.4.1

--- pkgs/tools/graphics/yafaray-core/default.nix
+++ pkgs/tools/graphics/yafaray-core/default.nix
@@ -7,13 +7,13 @@
 stdenv.mkDerivation rec {
 
     pname = "yafaray-core";
-    version = "3.3.0";
+    version = "3.4.1";
 
     src = fetchFromGitHub {
       owner  = "YafaRay";
       repo   = "Core";
       rev    = "v${version}";
-      sha256 = "04p3nlg1rv617qf8v1nzjl6f0w43rvi8w9j6l6ck4bvl77v6cjp6";
+      sha256 = "0ipkrgn5siad6y8naw1pgxkxas70l09g97vabyfbx1k67xrr4ldp";
     };
 
     preConfigure = ''
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
 
     meta = with stdenv.lib; {
       description = "A free, open source raytracer";
-      homepage = http://www.yafaray.org;
+      homepage = "http://www.yafaray.org";
       maintainers = with maintainers; [ hodapp ];
       license = licenses.lgpl21;
       platforms = platforms.linux;
