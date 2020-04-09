2d3371713897cb124abf351f9aed2377a4b2fadf
Jesse Wattenbarger <jwatt@broken.watch>
2020-04-08 21:56:26 -0400

    dasm: init at 2.20.13
    Co-authored-by: Daniel Schaefer <git@danielschaefer.me>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8212,6 +8212,8 @@ in
 
   scry = callPackage ../development/tools/scry {};
 
+  dasm = callPackage ../development/compilers/dasm/default.nix { };
+
   dbmate = callPackage ../development/tools/database/dbmate { };
 
   devpi-client = python3Packages.callPackage ../development/tools/devpi-client {};
