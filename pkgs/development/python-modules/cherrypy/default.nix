a27547e6af9da350ad7307b360418e27580a748e
Milan Pässler <mil@nyantec.com>
2020-04-09 23:59:33 +0200

    python3Packages.cherrypy: 18.3.0 -> 18.5.0
    The test `cherrypy/test/test_tools.py::ToolTests::testCombinedTools` is
    failing since the python was updated to 3.7.7 and is now deselected.

--- pkgs/development/python-modules/cherrypy/default.nix
+++ pkgs/development/python-modules/cherrypy/default.nix
@@ -1,36 +1,25 @@
 { stdenv, buildPythonPackage, fetchPypi, isPy3k
 , setuptools_scm
 , cheroot, portend, more-itertools, zc_lockfile, routes
-, objgraph, pytest, pytestcov, pathpy, requests_toolbelt, pytest-services
+, objgraph, pytest, pytestcov, pathpy, requests_toolbelt, pytest-services, jaraco_collections
 , fetchpatch
 }:
 
 buildPythonPackage rec {
   pname = "cherrypy";
-  version = "18.3.0";
+  version = "18.5.0";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     pname = "CherryPy";
     inherit version;
-    sha256 = "0q6cs4vrv0rwim4byxfizrlp4h6hmwg3n4baz0ga66vvgiz6hgk8";
+    sha256 = "0ayj2s4ldps7shxs7rwr6wiz666dhlilwkmx8lhi2sf470fgdck3";
   };
-
-  # Remove patches once 88d2163 and 713f672
-  # become part of a release - they're currently only present in master.
-  # ref: https://github.com/cherrypy/cherrypy/pull/1820
-  patches = [
-    (fetchpatch {
-      name = "test_HTTP11_Timeout.patch";
-      url = "https://github.com/cherrypy/cherrypy/commit/88d21630f68090c56d07000cabb6df4f1b612a71.patch";
-      sha256 = "1i6a3qs3ijyd9rgsxb8axigkzdlmr5sl3ljif9rvn0d90211bzwh";
-    })
-  ];
 
   propagatedBuildInputs = [
     # required
-    cheroot portend more-itertools zc_lockfile
+    cheroot portend more-itertools zc_lockfile jaraco_collections
     # optional
     routes
   ];
@@ -44,7 +33,9 @@ buildPythonPackage rec {
   # Disable doctest plugin because times out
   checkPhase = ''
     substituteInPlace pytest.ini --replace "--doctest-modules" ""
-    pytest --deselect=cherrypy/test/test_static.py::StaticTest::test_null_bytes ${stdenv.lib.optionalString stdenv.isDarwin "--deselect=cherrypy/test/test_bus.py::BusMethodTests::test_block"}
+    pytest ${stdenv.lib.optionalString stdenv.isDarwin "--deselect=cherrypy/test/test_bus.py::BusMethodTests::test_block"} \
+      --deselect=cherrypy/test/test_static.py::StaticTest::test_null_bytes \
+      --deselect=cherrypy/test/test_tools.py::ToolTests::testCombinedTools
   '';
 
   meta = with stdenv.lib; {
