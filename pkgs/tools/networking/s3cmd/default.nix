9d9ea65c5dc86be093eb0ad0fbbc900ef4bd8c5f
R. RyanTM <ryantm-bot@ryantm.com>
2020-04-09 08:03:25 +0000

    s3cmd: 2.0.2 -> 2.1.0

--- pkgs/tools/networking/s3cmd/default.nix
+++ pkgs/tools/networking/s3cmd/default.nix
@@ -2,19 +2,19 @@
 
 python2Packages.buildPythonApplication rec {
   pname = "s3cmd";
-  version = "2.0.2";
+  version = "2.1.0";
   
   src = fetchFromGitHub {
     owner  = "s3tools";
     repo   = "s3cmd";
     rev    = "v${version}";
-    sha256 = "0ninw830309cxga99gjnfghpkywf9kd6yz4wqsq85zni1dv39cdk";
+    sha256 = "0p6mbgai7f0c12pkw4s7d649gj1f8hywj60pscxvj9jsna3iifhs";
   };
 
   propagatedBuildInputs = with python2Packages; [ python_magic dateutil ];
 
   meta = with stdenv.lib; {
-    homepage = http://s3tools.org/;
+    homepage = "http://s3tools.org/";
     description = "A command-line tool to manipulate Amazon S3 buckets";
     license = licenses.gpl2;
     maintainers = [ maintainers.spwhitt ];
