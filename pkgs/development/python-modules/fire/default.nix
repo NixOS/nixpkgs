b71386f9082b0feb42701d2c15c681f633d89c6b
R. RyanTM <ryantm-bot@ryantm.com>
2020-04-10 16:15:38 +0000

    python27Packages.fire: 0.3.0 -> 0.3.1

--- pkgs/development/python-modules/fire/default.nix
+++ pkgs/development/python-modules/fire/default.nix
@@ -3,13 +3,13 @@
 
 buildPythonPackage rec {
   pname = "fire";
-  version = "0.3.0";
+  version = "0.3.1";
 
   src = fetchFromGitHub {
     owner = "google";
     repo = "python-fire";
     rev = "v${version}";
-    sha256 = "1jwf1qv9xmr1c8nyvwqn02329vzyh9py4slx13n55ms8h1n998nr";
+    sha256 = "0s5r6l39ck2scks54hmwwdf4lcihqqnqzjfx9lz2b67vxkajpwmc";
   };
 
   propagatedBuildInputs = [ six termcolor ] ++ stdenv.lib.optional isPy27 enum34;
