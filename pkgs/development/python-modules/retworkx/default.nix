8555157a97f1049788ec2fc582a48c83d9240e89
Drew Risinger <drewrisinger@users.noreply.github.com>
2020-04-10 18:02:36 -0400

    python3Packages.retworkx: convert to PyPi

--- pkgs/development/python-modules/retworkx/default.nix
+++ pkgs/development/python-modules/retworkx/default.nix
@@ -2,49 +2,49 @@
 , lib
 , rustPlatform
 , buildPythonPackage
-, fetchFromGitHub
-# , fetchPypi
+# , fetchFromGitHub
+, fetchPypi
 # , setuptools-rust
   # Check inputs
 , pytestCheckHook
 }:
 
-let
-  pn = "retworkx";
-  v = "0.3.3";
-  src-raw = fetchFromGitHub {
-    owner = "Qiskit";
-    repo = "retworkx";
-    rev = v;
-    sha256 = "160w5vkzrl5rzcrdwhjq820i5lmc527m6hg0kxx0k6n2bz9qn26g";
-  };
+# let
+#   pn = "retworkx";
+#   v = "0.3.3";
+#   src-raw = fetchFromGitHub {
+#     owner = "Qiskit";
+#     repo = "retworkx";
+#     rev = v;
+#     sha256 = "160w5vkzrl5rzcrdwhjq820i5lmc527m6hg0kxx0k6n2bz9qn26g";
+#   };
 
-  retworkx-rs = rustPlatform.buildRustPackage {
-    pname = pn;
-    version = v;
+#   retworkx-rs = rustPlatform.buildRustPackage {
+#     pname = pn;
+#     version = v;
 
-    src = src-raw;
-    cargoPatches = [ ./cargo-lock.patch ];
-    cargoSha256 = "1hkidvyyw2p75x1xk98fl023sm1fpjihssqsg839bi5m09hj0sd4";
-  };
+#     src = src-raw;
+#     cargoPatches = [ ./cargo-lock.patch ];
+#     cargoSha256 = "1hkidvyyw2p75x1xk98fl023sm1fpjihssqsg839bi5m09hj0sd4";
+#   };
 
-in
+# in
 buildPythonPackage rec {
   pname = "retworkx";
   version = "0.3.3";
-  # format = "wheel";
+  format = "wheel";
 
-  src = src-raw;
-  # src = fetchPypi {
-  #   inherit pname version format;
-  #   python = "cp37";
-  #   abi = "cp37m";
-  #   platform = "manylinux2010_x86_64";
-  #   sha256 = "1gbz7sh9i4h41xs9c40lixfdigmvfykkgxgzwsrs8v0smx20dczy";
-  # };
+  # src = src-raw;
+  src = fetchPypi {
+    inherit pname version format;
+    python = "cp37";
+    # abi = "cp37m";
+    platform = "manylinux2010_x86_64";
+    sha256 = "1gbz7sh9i4h41xs9c40lixfdigmvfykkgxgzwsrs8v0smx20dczy";
+  };
 
   buildInputs = [
-    retworkx-rs
+    # retworkx-rs
   ];
 
   propagatedBuildInputs = [
@@ -53,8 +53,13 @@ buildPythonPackage rec {
 
   pythonImportsCheck = [ "retworkx" ];
 
-  checkInputs = [ pytestCheckHook ];
-  dontUseSetuptoolsCheck = true;
+  # checkInputs = [ pytestCheckHook ];
+  # dontUseSetuptoolsCheck = true;
+
+  checkPhase = ''
+    python -c "import retworkx; retworkx.PyDAG()"
+    echo "Retworkx basic usage check succeeded"
+  '';
 
   meta = with lib; {
     description = "A python graph library implemented in Rust.";
