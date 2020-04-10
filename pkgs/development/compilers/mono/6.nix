de3380c7877dd81ea030d8c7c6c587af448d95d6
Damien Cassou <damien@cassou.me>
2020-04-10 07:21:44 +0200

    mono6: 6.0.0.313 -> 6.8.0.105

--- pkgs/development/compilers/mono/6.nix
+++ pkgs/development/compilers/mono/6.nix
@@ -2,8 +2,8 @@
 
 callPackage ./generic.nix ({
   inherit Foundation libobjc;
-  version = "6.0.0.313";
+  version = "6.8.0.105";
   srcArchiveSuffix = "tar.xz";
-  sha256 = "0l0cd6q5xh1vdm6zr78rkfqdsmrgzanjgpxvgig0pyd3glfyjim9";
+  sha256 = "0y11c7w6r96laqckfxnk1ya42hx2c1nfqvdgbpmsk1iw9k29k1sp";
   enableParallelBuilding = true;
 })
