d773e6728a87c37817503ea62317bf11b7f3cb01
R. RyanTM <ryantm-bot@ryantm.com>
2020-04-11 04:28:07 +0000

    qpdf: 9.1.1 -> 10.0.0

--- pkgs/development/libraries/qpdf/default.nix
+++ pkgs/development/libraries/qpdf/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchurl, libjpeg, zlib, perl }:
 
-let version = "9.1.1";
+let version = "10.0.0";
 in
 stdenv.mkDerivation rec {
   pname = "qpdf";
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://sourceforge/qpdf/qpdf/${version}/${pname}-${version}.tar.gz";
-    sha256 = "0dj27wb9xg6pg95phbflfvy9rwxn1gh3kc4n175g0pf41r0zrim2";
+    sha256 = "1m2si325rpkwczc7gn5p3a9d3bvmxxb635jsmiphhbrnyhwbdq5x";
   };
 
   nativeBuildInputs = [ perl ];
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
   enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
-    homepage = http://qpdf.sourceforge.net/;
+    homepage = "http://qpdf.sourceforge.net/";
     description = "A C++ library and set of programs that inspect and manipulate the structure of PDF files";
     license = licenses.asl20; # as of 7.0.0, people may stay at artistic2
     maintainers = with maintainers; [ abbradar ];
