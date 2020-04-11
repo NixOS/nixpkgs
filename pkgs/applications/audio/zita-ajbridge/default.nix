c17a59170040d4975b609c2a3d236de173575186
R. RyanTM <ryantm-bot@ryantm.com>
2020-04-11 17:40:10 +0000

    zita-ajbridge: 0.8.2 -> 0.8.4

--- pkgs/applications/audio/zita-ajbridge/default.nix
+++ pkgs/applications/audio/zita-ajbridge/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, alsaLib, libjack2, zita-alsa-pcmi, zita-resampler }:
 
 stdenv.mkDerivation rec {
-  name = "zita-ajbridge-0.8.2";
+  name = "zita-ajbridge-0.8.4";
 
   src = fetchurl {
     url = "https://kokkinizita.linuxaudio.org/linuxaudio/downloads/${name}.tar.bz2";
-    sha256 = "1gvk6g6w9rsiib89l0i9myl2cxxfzmcpbg9wdypq6b27l9s5k64j";
+    sha256 = "0g5v0l0zmqh049mhv62n8s5bpm0yrlby7mkxxhs5qwadp8v4w9mw";
   };
 
   buildInputs = [ alsaLib libjack2 zita-alsa-pcmi zita-resampler ];
@@ -23,7 +23,7 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Connect additional ALSA devices to JACK";
-    homepage = http://kokkinizita.linuxaudio.org/linuxaudio/index.html;
+    homepage = "http://kokkinizita.linuxaudio.org/linuxaudio/index.html";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ orivej ];
     platforms = platforms.linux;
