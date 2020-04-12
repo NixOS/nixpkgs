26702dfb585cac4fe3816d301364e8c8a8b855c6
Maximilian Bosch <maximilian@mbosch.me>
2020-04-13 01:21:32 +0200

    wdisplays: 2020-01-12 -> 2020-03-15
    https://github.com/cyclopsian/wdisplays/compare/ba331ca...0faafdc
    (cherry picked from commit 6b5186c2cabbb4aae9b933fda631f43de1a10225)

--- pkgs/tools/graphics/wdisplays/default.nix
+++ pkgs/tools/graphics/wdisplays/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchFromGitHub, meson, ninja, pkgconfig, gtk3, epoxy, wayland }:
 stdenv.mkDerivation {
   pname = "wdisplays-unstable";
-  version = "2020-01-12";
+  version = "2020-03-15";
 
   nativeBuildInputs = [ meson ninja pkgconfig ];
 
@@ -10,8 +10,8 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "cyclopsian";
     repo = "wdisplays";
-    rev = "ba331cab535318888a562f5a2731d2523b310dac";
-    sha256 = "0fk3l78hirxdi74iqmq6mxi9daqnxdkbb5a2wfshmr11ic9xixpm";
+    rev = "0faafdc04d7dd47d3a4e385f348cb9d267f2e60d";
+    sha256 = "1y3bzh4mi6d67n6v0i8j5snpaikpbyr89acayr4m6bx85qnrq4g2";
   };
 
   meta = let inherit (stdenv) lib; in {
