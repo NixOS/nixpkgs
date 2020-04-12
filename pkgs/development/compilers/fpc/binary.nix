5c1086ff557cea3b2ea31d05c060c88e654d82f5
Robin Gloster <mail@glob.in>
2020-04-12 20:16:31 +0200

    fpc: fix build

--- pkgs/development/compilers/fpc/binary.nix
+++ pkgs/development/compilers/fpc/binary.nix
@@ -16,9 +16,22 @@ stdenv.mkDerivation {
       }
     else throw "Not supported on ${stdenv.hostPlatform.system}.";
 
-  builder = ./binary-builder.sh;
+  buildPhase = ''
+    tarballdir=$(pwd)
+    for i in *.tar; do tar xvf $i; done
+  '';
+
+  installPhase = ''
+    mkdir $out
+    cd $out
+    for i in $tarballdir/*.gz; do tar xvf $i; done
+    echo 'Creating ppc* symlink..'
+    for i in $out/lib/fpc/*/ppc*; do
+      ln -fs $i $out/bin/$(basename $i)
+    done
+  '';
 
   meta = {
     description = "Free Pascal Compiler from a binary distribution";
   };
-} 
+}
