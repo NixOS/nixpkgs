07a2315f63a03cead0065c8ff3f400f451110a72
Mario Rodas <marsam@users.noreply.github.com>
2020-04-12 04:20:00 -0500

    victoriametrics: 1.34.5 -> 1.34.7

--- pkgs/servers/nosql/victoriametrics/default.nix
+++ pkgs/servers/nosql/victoriametrics/default.nix
@@ -1,17 +1,18 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{ lib, buildGoPackage, fetchFromGitHub }:
 
-buildGoModule rec {
+buildGoPackage rec {
   pname = "VictoriaMetrics";
-  version = "1.34.5";
+  version = "1.34.7";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "0isifyvbrv0f6l32dycka67cpnizwh2c9ns2q8pz6r2myzbdsf3s";
+    sha256 = "0k4l3nq1d6f5qjx8svgga0ygv9mmhykvs3n3xr824ih6d0vrkzbg";
   };
 
-  modSha256 = "0fr5yah4qicqjfgina1ghflaz8dlzsqk3rrpsvg5l68jb6l7nxkb";
+  goPackagePath = "github.com/VictoriaMetrics/VictoriaMetrics";
+
   meta = with lib; {
     homepage = "https://victoriametrics.com/";
     description = "fast, cost-effective and scalable time series database, long-term remote storage for Prometheus";
