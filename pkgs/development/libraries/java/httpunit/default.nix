7ca4297b3ebcafc1ae6bb7de142440e3370876f3
Robin Gloster <mail@glob.in>
2020-04-12 20:20:37 +0200

    httpunit: fix build

--- pkgs/development/libraries/java/httpunit/default.nix
+++ pkgs/development/libraries/java/httpunit/default.nix
@@ -1,15 +1,19 @@
-{stdenv, fetchurl, unzip} :
+{ stdenv, fetchurl, unzip }:
 
 stdenv.mkDerivation {
   name = "httpunit-1.7";
-  builder = ./builder.sh;
 
   src = fetchurl {
     url = mirror://sourceforge/httpunit/httpunit-1.7.zip;
     sha256 = "09gnayqgizd8cjqayvdpkxrc69ipyxawc96aznfrgdhdiwv8l5zf";
   };
 
-  inherit unzip;
+  nativeBuildInputs = [ unzip ];
+
+  installPhase = ''
+    mkdir $out
+    cp -rv * $out/
+  '';
 
   meta = with stdenv.lib; {
     homepage = http://httpunit.sourceforge.net;
