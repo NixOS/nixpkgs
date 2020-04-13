2a463129e0bf1ad3a084ca6c92a18a4728991b82
Jan Tojnar <jtojnar@gmail.com>
2020-04-14 01:03:59 +0200

    inkscape: 0.92.4 → 0.92.5
    https://gitlab.com/inkscape/inkscape/-/tags/INKSCAPE_0_92_5
    (cherry picked from commit a48d86e8513c9a0d8830696649036dbe3023ee21)

--- pkgs/applications/graphics/inkscape/default.nix
+++ pkgs/applications/graphics/inkscape/default.nix
@@ -30,12 +30,12 @@
 , poppler
 , popt
 , potrace
-, python2
+, python3
 , wrapGAppsHook
 , zlib
 }:
 let
-  python2Env = python2.withPackages
+  python3Env = python3.withPackages
     (ps: with ps; [
       numpy
       lxml
@@ -44,30 +44,12 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "inkscape";
-  version = "0.92.4";
+  version = "0.92.5";
 
   src = fetchurl {
     url = "https://media.inkscape.org/dl/resources/file/${pname}-${version}.tar.bz2";
-    sha256 = "0pjinhjibfsz1aywdpgpj3k23xrsszpj4a1ya5562dkv2yl2vv2p";
+    sha256 = "ge5/aeK9ZKlzQ9g5Wkp6eQWyG4YVZu1eXZF5F41Rmgs=";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "inkscape-poppler_0_76_compat.patch";
-      url = "https://gitlab.com/inkscape/inkscape/commit/e831b034746f8dc3c3c1b88372751f6dcb974831.diff";
-      sha256 = "096rdyi6ppjq1h9jwwsm9hb99nggfrfinik8rm23jkn4h2zl01zf";
-    })
-    (fetchpatch {
-      name = "inkscape-poppler_0_82_compat.patch";
-      url = "https://gitlab.com/inkscape/inkscape/commit/835b6bb62be565efab986d5a3f30a672ad56c7eb.patch";
-      sha256 = "02c6sxi2w52b885vr3pgani6kvxp9gdqqk2jgiykkdzv70hhrnm7";
-    })
-    (fetchpatch {
-      name = "inkscape-poppler_0_83_compat.patch";
-      url = "https://gitlab.com/inkscape/inkscape/commit/b5360a807b12d4e8318475ffd0464b84882788b5.patch";
-      sha256 = "1p44rr2q2i3zkd1y1j7xgdcbgx8yvlq6hq92im8s0bkjby6p5cpz";
-    })
-  ];
 
   # Inkscape hits the ARGMAX when linking on macOS. It appears to be
   # CMake’s ARGMAX check doesn’t offer enough padding for NIX_LDFLAGS.
@@ -82,14 +64,14 @@ stdenv.mkDerivation rec {
     # Python is used at run-time to execute scripts, e.g., those from
     # the "Effects" menu.
     substituteInPlace src/extension/implementation/script.cpp \
-      --replace '"python-interpreter", "python"' '"python-interpreter", "${python2Env}/bin/python"'
+      --replace '"python-interpreter", "python"' '"python-interpreter", "${python3Env}/bin/python"'
   '';
 
   nativeBuildInputs = [
     pkg-config
     cmake
     makeWrapper
-    python2Env
+    python3Env
     wrapGAppsHook
   ] ++ (with perlPackages; [
     perl
@@ -121,7 +103,7 @@ stdenv.mkDerivation rec {
     poppler
     popt
     potrace
-    python2Env
+    python3Env
     zlib
   ] ++ stdenv.lib.optionals (!stdenv.isDarwin) [
     gtkspell2
