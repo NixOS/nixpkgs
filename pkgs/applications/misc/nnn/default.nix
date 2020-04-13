d933524a2a6ba9da5f3bdff3905d4fc86b203b54
Nikolay Korotkiy <sikmir@gmail.com>
2020-04-13 13:23:53 +0300

    nnn: 3.0 -> 3.1

--- pkgs/applications/misc/nnn/default.nix
+++ pkgs/applications/misc/nnn/default.nix
@@ -4,13 +4,13 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   pname = "nnn";
-  version = "3.0";
+  version = "3.1";
 
   src = fetchFromGitHub {
     owner = "jarun";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0kmfg61v3xnf8m4m9nna28sr7hdwqbxwivc7j91zhfj2wpdswywp";
+    sha256 = "0wvn3jbxjcpdg9jzxkhx5dlc0zx2idky6mb75fpha8ww1jg7qf1g";
   };
 
   configFile = optionalString (conf != null) (builtins.toFile "nnn.h" conf);
