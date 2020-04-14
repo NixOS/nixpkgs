202254d278a56362ef9cd5e4095a6833cc129f8e
Maximilian Bosch <maximilian@mbosch.me>
2020-04-15 00:26:32 +0200

    nixos/systemd-nspawn: Allow to modify .nspawn-units with a derivation
    Sometimes it's needed to build a configuration within a `nix-build` for
    systemd units. While this is fairly easy for .service-units (where you
    can easily define overrides), it's not possible for `systemd-nspawn(1)`.
    
    This is mostly a hack to get dedicated bind-mounts of store paths from
    `pkgs.closureInfo` into the configuration without IFD.
    
    In the long term we either want to fix this in systemd or find a more
    suited solution though.

--- nixos/modules/system/boot/systemd-nspawn.nix
+++ nixos/modules/system/boot/systemd-nspawn.nix
@@ -79,9 +79,39 @@ let
           <manvolnum>5</manvolnum></citerefentry> for details.
         '';
       };
+
+      extraDrvConfig = mkOption {
+        type = types.nullOr types.package;
+        default = null;
+        description = ''
+          Extra config for an nspawn-unit that is generated via `nix-build`.
+          This is necessary since nspawn doesn't support overrides in
+          <literal>/etc/systemd/nspawn</literal> natively and sometimes a derivation
+          is needed for configs (e.g. to determine all needed store-paths to bind-mount
+          into a machine).
+        '';
+      };
     };
 
   };
+
+  makeUnit' = name: def:
+    if def.extraDrvConfig == null || !def.enable then makeUnit name def
+    else pkgs.runCommand "nspawn-${mkPathSafeName name}-custom"
+      { preferLocalBuild = true;
+        allowSubstitutes = false;
+      } (let
+        name' = shellEscape name;
+      in ''
+        if [ ! -f "${def.extraDrvConfig}" ]; then
+          echo "systemd.nspawn.${name}.extraDrvConfig is not a file!"
+          exit 1
+        fi
+
+        mkdir -p $out
+        cat ${makeUnit name def}/${name'} > $out/${name'}
+        cat ${def.extraDrvConfig} >> $out/${name'}
+      '');
 
   instanceToUnit = name: def:
     let base = {
@@ -96,7 +126,7 @@ let
         ${attrsToSection def.networkConfig}
       '';
     } // def;
-    in base // { unit = makeUnit name base; };
+    in base // { unit = makeUnit' name base; };
 
 in {
 
