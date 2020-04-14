07f553711de3710e8b2a01f6bb4982fdf94413fc
Jaka Hudoklin <jaka@x-truder.net>
2020-04-14 12:41:57 +0700

    chromium: build chromiumOzone

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18734,6 +18734,8 @@ in
 
   chromiumDev = lowPrio (chromium.override { channel = "dev"; });
 
+  chromiumOzone = lowPrio (chromium.override { useOzone = true; });
+
   chuck = callPackage ../applications/audio/chuck {
     inherit (darwin.apple_sdk.frameworks) AppKit Carbon CoreAudio CoreMIDI CoreServices Kernel;
   };
