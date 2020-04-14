0ef7d22b6cd714af9af507ba9e4eab410fd93930
Damien Cassou <damien@cassou.me>
2020-04-14 22:45:54 +0200

    lenovo/thinkpad/t460s: Remove problematic options
    See there links for details:
    
    https://github.com/NixOS/nixos-hardware/pull/134#pullrequestreview-336181593
    https://github.com/NixOS/nixos-hardware/pull/134#discussion_r361146814

--- lenovo/thinkpad/t460s/default.nix
+++ lenovo/thinkpad/t460s/default.nix
@@ -5,8 +5,4 @@
     ../../../common/cpu/intel
     ../.
   ];
-
-  # TODO: boot loader
-  boot.loader.systemd-boot.enable = lib.mkDefault true;
-  boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
 }
