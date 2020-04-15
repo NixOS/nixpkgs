9ed793229ca8a988f49b2001b13d33f0edc0790d
Graham Christensen <graham@grahamc.com>
2020-04-15 08:20:47 -0400

    teams/maintainers list: show instructions for validating the contents

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -45,7 +45,11 @@
 
     More fields may be added in the future, however, in order to comply with GDPR this file should stay as minimal as possible.
 
-    Please keep the list alphabetically sorted.
+    When editing this file:
+     * keep the list alphabetically sorted
+     * test the validity of the format with:
+         nix-build lib/tests/maintainers.nix
+
     See `./scripts/check-maintainer-github-handles.sh` for an example on how to work with this data.
 */
 {
