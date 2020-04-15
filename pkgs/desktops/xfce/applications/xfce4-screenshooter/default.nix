39343ed9bc9c8ee231975eb7417638ee832acdf7
José Romildo Malaquias <malaquias@gmail.com>
2020-04-15 14:50:52 -0300

    xfce.xfce4-screenshooter: 1.9.5 -> 1.9.7

--- pkgs/desktops/xfce/applications/xfce4-screenshooter/default.nix
+++ pkgs/desktops/xfce/applications/xfce4-screenshooter/default.nix
@@ -3,10 +3,10 @@
 mkXfceDerivation {
   category = "apps";
   pname = "xfce4-screenshooter";
-  version = "1.9.5";
+  version = "1.9.7";
   odd-unstable = false;
 
-  sha256 = "1h14sywvk9l06p3z1cpb79911j8w2wqbk03ldknjkia2rfymjk06";
+  sha256 = "14vbd7iigaw57hl47rnixk873c20q5clqynzkm9zzpqc568dxixd";
 
   buildInputs = [ exo gtk3 libsoup libxfce4ui libxfce4util xfce4-panel glib-networking ];
 
