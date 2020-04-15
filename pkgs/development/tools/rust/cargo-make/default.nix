8c3b765bc6328d3c6053c392375fcca39ac1f23d
Maximilian Bosch <maximilian@mbosch.me>
2020-04-15 22:00:35 +0200

    cargo-make: 0.30.4 -> 0.30.5
    https://github.com/sagiegurari/cargo-make/releases/tag/0.30.5
    (cherry picked from commit b7c3c3f5cc23b57a818e1b9a77b2855d1657c194)

--- pkgs/development/tools/rust/cargo-make/default.nix
+++ pkgs/development/tools/rust/cargo-make/default.nix
@@ -2,7 +2,7 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-make";
-  version = "0.30.4";
+  version = "0.30.5";
 
   src =
     let
@@ -10,7 +10,7 @@ rustPlatform.buildRustPackage rec {
         owner = "sagiegurari";
         repo = pname;
         rev = version;
-        sha256 = "14sl7kcr1n3xb912vd3445pwf3v1kp74lgxlwqybnwiyh1dxihvg";
+        sha256 = "0p6rzkrwyfcrg4qrlb67rf0wb12kqldl1xg0rfnwc23y17fbwx49";
       };
     in
     runCommand "source" {} ''
@@ -24,7 +24,7 @@ rustPlatform.buildRustPackage rec {
   buildInputs = [ openssl ]
     ++ stdenv.lib.optionals stdenv.isDarwin [ Security ];
 
-  cargoSha256 = "0adilcl76mh520zk40vhzi3y6riai73282wzhvjcfaasbcwlr738";
+  cargoSha256 = "0l3k4k63zx4gbdp5bnamrnhib2pw6z0d1ff06n8v2z7rcrawhlyx";
 
   # Some tests fail because they need network access.
   # However, Travis ensures a proper build.
