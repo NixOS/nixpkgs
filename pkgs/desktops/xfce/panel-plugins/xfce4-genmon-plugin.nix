0c6b8040147806f33ff3855e1b6977e886b4688c
José Romildo Malaquias <malaquias@gmail.com>
2020-04-15 14:50:51 -0300

    xfce.xfce4-genmon-plugin: 3.4.0 -> 4.0.2

--- pkgs/desktops/xfce/panel-plugins/xfce4-genmon-plugin.nix
+++ pkgs/desktops/xfce/panel-plugins/xfce4-genmon-plugin.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, intltool, libxfce4util, xfce4-panel, libxfce4ui, xfconf, gtk2, xfce }:
+{ stdenv, fetchurl, pkgconfig, intltool, libxfce4util, xfce4-panel, libxfce4ui, gtk3, xfce }:
 
 let
   category = "panel-plugins";
@@ -6,15 +6,24 @@ in
 
 stdenv.mkDerivation rec {
   pname  = "xfce4-genmon-plugin";
-  version = "3.4.0";
+  version = "4.0.2";
 
   src = fetchurl {
     url = "mirror://xfce/src/${category}/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.bz2";
-    sha256 = "11q3g6lmgz3d5lyh6614mxkd9cblfdyf9jgki7f26mn895xk79dh";
+    sha256 = "1ai3pwgv61nv7i2dyrvncnc63r8kdjbkp40vp51vzak1dx924v15";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ intltool libxfce4util libxfce4ui xfce4-panel xfconf gtk2 ];
+  nativeBuildInputs = [
+    pkgconfig
+    intltool
+  ];
+
+  buildInputs = [
+    libxfce4util
+    libxfce4ui
+    xfce4-panel
+    gtk3
+  ];
   
   passthru.updateScript = xfce.updateScript {
     inherit pname version;
@@ -23,10 +32,10 @@ stdenv.mkDerivation rec {
   };
 
   meta = with stdenv.lib; {
-    homepage = "https://goodies.xfce.org/projects/panel-plugins/${pname}";
-    description = "Cyclically spawns a command and captures its output";
+    homepage = "https://docs.xfce.org/panel-plugins/xfce4-genmon-plugin";
+    description = "Generic monitor plugin for the Xfce panel";
+    license = licenses.gpl2Plus;
     platforms = platforms.linux;
     maintainers = [ maintainers.AndersonTorres ];
-    broken = true;
   };
 }
