cef0895d24ac8e2c44706342293e076bb0bad750
José Romildo Malaquias <malaquias@gmail.com>
2020-04-15 12:58:47 -0300

    xfce.xfce4-netload-plugin: odd minor version number is not unstable

--- pkgs/desktops/xfce/panel-plugins/xfce4-netload-plugin/default.nix
+++ pkgs/desktops/xfce/panel-plugins/xfce4-netload-plugin/default.nix
@@ -5,6 +5,7 @@ mkXfceDerivation {
   pname = "xfce4-netload-plugin";
   version = "1.3.1";
   rev-prefix = "version-";
+  odd-unstable = false;
   sha256 = "0nm8advafw4jpc9p1qszyfqa56194sz51z216rdh4c6ilcrrpy1h";
 
   buildInputs = [ gtk3 libxfce4ui libxfce4util xfce4-panel xfconf ];
