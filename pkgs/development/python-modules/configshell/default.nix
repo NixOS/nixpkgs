027b17cb7934154e6019129d3a45e2131f15ec2e
ajs124 <git@ajs124.de>
2020-04-15 19:09:00 +0200

    pythonPackages.configshell: 1.1.27 -> 1.1.28

--- pkgs/development/python-modules/configshell/default.nix
+++ pkgs/development/python-modules/configshell/default.nix
@@ -2,13 +2,13 @@
 
 buildPythonPackage rec {
   pname = "configshell";
-  version = "1.1.27";
+  version = "1.1.28";
 
   src = fetchFromGitHub {
     owner = "open-iscsi";
-    repo ="${pname}-fb";
+    repo = "${pname}-fb";
     rev = "v${version}";
-    sha256 = "1nldzq3097xqgzd8qxv36ydvx6vj2crwanihz53k46is0myrwcnn";
+    sha256 = "1ym2hkvmmacgy21wnjwzyrcxyl3sx4bcx4hc51vf4lzcnj589l68";
   };
 
   propagatedBuildInputs = [ pyparsing six urwid ];
