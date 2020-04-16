1f6d95badfa535abc94dcce693b9819be529d9fc
Edmund Wu <fangkazuto@gmail.com>
2020-04-16 16:25:18 -0400

    metasrht: 0.41.10 -> 0.42.13

--- pkgs/applications/version-management/sourcehut/meta.nix
+++ pkgs/applications/version-management/sourcehut/meta.nix
@@ -5,12 +5,12 @@
 
 buildPythonPackage rec {
   pname = "metasrht";
-  version = "0.41.10";
+  version = "0.42.13";
 
   src = fetchgit {
     url = "https://git.sr.ht/~sircmpwn/meta.sr.ht";
     rev = version;
-    sha256 = "1srzrajgwq85kjryxykj708m2c98r6a84x4k4a5grwznqw3mwm6p";
+    sha256 = "p7WgnfOsX09YxJJclHwdIky/jYkTOxibbYmXwcmE2S4=";
   };
 
   nativeBuildInputs = srht.nativeBuildInputs;
