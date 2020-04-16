ed20edf012bfda57d9cb70463b6ba8a422148b55
Ismaël Bouya <ismael.bouya@normalesup.org>
2020-04-16 12:32:47 +0200

    nixos/acme: Fix postRun in acme certificate being ran at every run
    (cherry picked from commit 8e88b8dce2826d4b3a55c9ac182574a69caf26a2)

--- nixos/modules/security/acme.nix
+++ nixos/modules/security/acme.nix
@@ -325,7 +325,9 @@ in
 
                           # Test that existing cert is older than new cert
                           KEY=${spath}/certificates/${keyName}.key
+                          KEY_CHANGED=no
                           if [ -e $KEY -a $KEY -nt key.pem ]; then
+                            KEY_CHANGED=yes
                             cp -p ${spath}/certificates/${keyName}.key key.pem
                             cp -p ${spath}/certificates/${keyName}.crt fullchain.pem
                             cp -p ${spath}/certificates/${keyName}.issuer.crt chain.pem
@@ -336,7 +338,10 @@ in
                           chmod ${fileMode} *.pem
                           chown '${data.user}:${data.group}' *.pem
 
-                          ${data.postRun}
+                          if [ "$KEY_CHANGED" = "yes" ]; then
+                            : # noop in case postRun is empty
+                            ${data.postRun}
+                          fi
                         '';
                       in
                         "+${script}";
