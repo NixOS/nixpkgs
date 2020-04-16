cae8d97c232fc12698445b8432673f9e51e21e91
Jan Tojnar <jtojnar@gmail.com>
2020-04-16 11:29:27 +0200

    dconf: fix build

--- pkgs/development/libraries/dconf/default.nix
+++ pkgs/development/libraries/dconf/default.nix
@@ -1,5 +1,6 @@
 { stdenv
 , fetchurl
+, fetchpatch
 , meson
 , ninja
 , python3
@@ -26,6 +27,15 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
     sha256 = "0bfs069pjv6lhp7xrzmrhz3876ay2ryqxzc6mlva1hhz34ibprlz";
   };
+
+  patches = [
+    # Fix bash-completion installation
+    # https://gitlab.gnome.org/GNOME/dconf/merge_requests/58
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/dconf/commit/b3c9423c6151f3c28e526083ea2f04987a780fdf.patch";
+      sha256 = "zrIPKmgEpa1iIGUKv03+z+GNwJwgdf2hDATgP3i8qk0=";
+    })
+  ];
 
   nativeBuildInputs = [
     meson
