9a4ee0c6331cc7d865bfc598f8bcf1eb31899ab1
Jan Tojnar <jtojnar@gmail.com>
2020-04-17 01:22:16 +0200

    scribusUnstable: Fix build with Poppler 0.86

--- pkgs/applications/office/scribus/unstable.nix
+++ pkgs/applications/office/scribus/unstable.nix
@@ -44,28 +44,40 @@ mkDerivation rec {
   };
 
   patches = [
+    # Poppler patches from
+    # https://github.com/scribusproject/scribus/commits/master/scribus/plugins/import/pdf
+
     # fix build with Poppler 0.82
-    (
-      fetchpatch {
-        url = "https://github.com/scribusproject/scribus/commit/6db15ec1af791377b28981601f8c296006de3c6f.patch";
-        sha256 = "1y6g3avmsmiyaj8xry1syaz8sfznsavh6l2rp13pj2bwsxfcf939";
-      }
-    )
+    (fetchpatch {
+      url = "https://github.com/scribusproject/scribus/commit/6db15ec1af791377b28981601f8c296006de3c6f.patch";
+      sha256 = "1y6g3avmsmiyaj8xry1syaz8sfznsavh6l2rp13pj2bwsxfcf939";
+    })
     # fix build with Poppler 0.83
-    (
-      fetchpatch {
-        url = "https://github.com/scribusproject/scribus/commit/b51c2bab4d57d685f96d427d6816bdd4ecfb4674.patch";
-        sha256 = "031yy9ylzksczfnpcc4glfccz025sn47zg6fqqzjnqqrc16bgdlx";
-      }
-    )
+    (fetchpatch {
+      url = "https://github.com/scribusproject/scribus/commit/b51c2bab4d57d685f96d427d6816bdd4ecfb4674.patch";
+      sha256 = "031yy9ylzksczfnpcc4glfccz025sn47zg6fqqzjnqqrc16bgdlx";
+    })
     # fix build with Poppler 0.84
     # TODO: Remove patches with scribus version > 1.5.5 as it should be fixed upstream in next version
-    (
-      fetchpatch {
-        url = "https://github.com/scribusproject/scribus/commit/3742559924136c2471ab15081c5b600dd5feaeb0.patch";
-        sha256 = "1d72h7jbajy9w83bnxmhn1ca947hpfxnfbmq30g5ljlj824c7y9y";
-      }
-    )
+    (fetchpatch {
+      url = "https://github.com/scribusproject/scribus/commit/3742559924136c2471ab15081c5b600dd5feaeb0.patch";
+      sha256 = "1d72h7jbajy9w83bnxmhn1ca947hpfxnfbmq30g5ljlj824c7y9y";
+    })
+    # Formating changes needed for the Poppler 0.86 patch to apply
+    (fetchpatch {
+      url = "https://github.com/scribusproject/scribus/commit/58613b5ce44335f202a55ab15ed303d97fe274cb.patch";
+      sha256 = "qwmVAZVYCijLNMVGwnOLJE8223vcRwPWfEvNKiDjw5o=";
+    })
+    (fetchpatch {
+      url = "https://github.com/scribusproject/scribus/commit/24aba508aac3f672f5f8cd629744a3b71e58ec37.patch";
+      sha256 = "sha256-OY+EIiGBTg2aIAmZOnkI8DPZVZYqFZAKnD7ychge1Dw=";
+      includes = [ "scribus/plugins/import/pdf/*" ];
+    })
+    # fix build with Poppler 0.86
+    (fetchpatch {
+      url = "https://github.com/scribusproject/scribus/commit/67f8771aaff2f55d61b8246f420e762f4b526944.patch";
+      sha256 = "51z+BYKhbH8a9dFph8X60NGpiogSb+5tOhW2d+m/X9M=";
+    })
   ];
 
   enableParallelBuilding = true;
