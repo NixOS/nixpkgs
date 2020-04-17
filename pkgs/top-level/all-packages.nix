4fdc45554381748e364d891bc6bca6217be6d152
Anders Kaseorg <andersk@mit.edu>
2020-04-17 12:00:48 -0700

    mesa-demos: init at 8.4.0 (#74310)
    * mesa-demos: init at 8.4.0
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>
    
    * Update pkgs/tools/graphics/mesa-demos/default.nix
    
    Co-Authored-By: Doron Behar <doron.behar@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4666,6 +4666,8 @@ in
 
   memtester = callPackage ../tools/system/memtester { };
 
+  mesa-demos = callPackage ../tools/graphics/mesa-demos { };
+
   mhonarc = perlPackages.MHonArc;
 
   minergate = callPackage ../applications/misc/minergate { };
