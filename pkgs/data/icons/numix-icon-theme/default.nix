bc895be32cdd0d21fc6de0fce51aa2c68bb55fc5
José Romildo Malaquias <malaquias@gmail.com>
2020-04-17 20:53:38 -0300

    numix-icon-theme: add missing parent icon themes

--- pkgs/data/icons/numix-icon-theme/default.nix
+++ pkgs/data/icons/numix-icon-theme/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, gtk3, hicolor-icon-theme }:
+{ stdenv, fetchFromGitHub, gtk3, gnome-icon-theme, hicolor-icon-theme }:
 
 stdenv.mkDerivation rec {
   pname = "numix-icon-theme";
@@ -13,18 +13,14 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ gtk3 ];
 
-  propagatedBuildInputs = [
-    hicolor-icon-theme
-  ];
+  propagatedBuildInputs = [ gnome-icon-theme hicolor-icon-theme ];
 
   dontDropIconThemeCache = true;
 
   installPhase = ''
     mkdir -p $out/share/icons
     cp -a Numix{,-Light} $out/share/icons/
-  '';
 
-  postFixup = ''
     for theme in $out/share/icons/*; do
       gtk-update-icon-cache $theme
     done
