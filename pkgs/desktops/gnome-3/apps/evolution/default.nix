04ce99995e9b28e6bcdbc7facaefd98be6535bbc
Robin Gloster <mail@glob.in>
2020-04-17 21:51:06 +0200

    evolution: fix build

--- pkgs/desktops/gnome-3/apps/evolution/default.nix
+++ pkgs/desktops/gnome-3/apps/evolution/default.nix
@@ -122,7 +122,7 @@ stdenv.mkDerivation rec {
     };
   };
 
-  PKG_CONFIG_LIBEDATASERVERUI_1_2_UIMODULEDIR = "${placeholder "out"}/lib/evolution-data-server/ui-modules";
+  env.PKG_CONFIG_LIBEDATASERVERUI_1_2_UIMODULEDIR = "${placeholder "out"}/lib/evolution-data-server/ui-modules";
 
   meta = with stdenv.lib; {
     homepage = "https://wiki.gnome.org/Apps/Evolution";
