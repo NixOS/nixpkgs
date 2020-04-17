58362a4b1bb5adf99383de7dc630e878e145e9a7
José Romildo Malaquias <malaquias@gmail.com>
2020-04-17 20:56:28 -0300

    flat-remix-icon-theme: add missing parent icon themes

--- pkgs/data/icons/flat-remix-icon-theme/default.nix
+++ pkgs/data/icons/flat-remix-icon-theme/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub , gtk3 }:
+{ stdenv, fetchFromGitHub , gtk3, breeze-icons, pantheon, gnome-icon-theme, hicolor-icon-theme }:
 
 stdenv.mkDerivation rec  {
   pname = "flat-remix-icon-theme";
@@ -11,14 +11,23 @@ stdenv.mkDerivation rec  {
     sha256 = "14n5wydhd5ifmsbj770s2qg2ksd3xa3m61qxydid6jq39k0lxbd8";
   };
 
-  nativeBuildInputs = [ gtk3 ];
+  nativeBuildInputs = [
+    gtk3
+  ];
+
+  propagatedBuildInputs = [
+    breeze-icons
+    pantheon.elementary-icon-theme
+    gnome-icon-theme
+    hicolor-icon-theme
+  ];
+
+  dontDropIconThemeCache = true;
 
   installPhase = ''
     mkdir -p $out/share/icons
     mv Flat-Remix* $out/share/icons/
-  '';
 
-  postFixup = ''
     for theme in $out/share/icons/*; do
       gtk-update-icon-cache $theme
     done
