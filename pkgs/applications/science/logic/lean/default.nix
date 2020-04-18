f37c9226fc41d204bf4cf5fe3787ef6d18b00213
Mario Rodas <marsam@users.noreply.github.com>
2020-04-18 04:20:00 -0500

    lean: 3.8.0 -> 3.9.0

--- pkgs/applications/science/logic/lean/default.nix
+++ pkgs/applications/science/logic/lean/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "lean";
-  version = "3.8.0";
+  version = "3.9.0";
 
   src = fetchFromGitHub {
     owner  = "leanprover-community";
     repo   = "lean";
     rev    = "v${version}";
-    sha256 = "0frs2vhxlzdliyydb462c1886dn585zd7yp7hdxzsri2v6gdh89g";
+    sha256 = "0sklw211cmqp4y0c6nycfavpdxdpfwhq9a4sbwdlqd5agyf85h1j";
   };
 
   nativeBuildInputs = [ cmake ];
