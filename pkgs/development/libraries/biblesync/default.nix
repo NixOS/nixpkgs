fadcfc3ea4252e2b9ad98e865d41038319ece14a
Pavol Rusnak <pavol@rusnak.io>
2020-04-18 13:43:47 +0200

    treewide: per RFC45, remove more unquoted URLs

--- pkgs/development/libraries/biblesync/default.nix
+++ pkgs/development/libraries/biblesync/default.nix
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
   buildInputs = [ libuuid ];
 
   meta = with stdenv.lib; {
-    homepage = http://www.crosswire.org/wiki/BibleSync;
+    homepage = "http://www.crosswire.org/wiki/BibleSync";
     description = "A multicast protocol to Bible software shared conavigation";
     longDescription = ''
       BibleSync is a multicast protocol to support Bible software shared
