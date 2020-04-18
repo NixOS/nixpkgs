7bac5db6f7c56eec663b74c590544268c8cb563c
Markus Wamser <wamserma@users.noreply.github.com>
2020-04-18 03:14:15 +0200

    maintainers: add wamserma
    Signed-off-by: Markus S. Wamser <github-dev@mail2013.wamser.eu>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8121,6 +8121,12 @@
     githubId = 3889405;
     name = "vyp";
   };
+  wamserma = {
+    name = "Markus S. Wamser";
+    email = "github-dev@mail2013.wamser.eu";
+    github = "wamserma";
+    githubId = 60148;
+  };
   waynr = {
     name = "Wayne Warren";
     email = "wayne.warren.s@gmail.com";
