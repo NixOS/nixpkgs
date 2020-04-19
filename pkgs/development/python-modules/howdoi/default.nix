0e48a68559dabdee75788323bd77ab8b579d6205
Sirio Balmelli <sirio@b-ad.ch>
2020-04-19 21:39:37 +0200

    howdoi: fix Darwin build dependencies
    Signed-off-by: Sirio Balmelli <sirio@b-ad.ch>

--- pkgs/development/python-modules/howdoi/default.nix
+++ pkgs/development/python-modules/howdoi/default.nix
@@ -1,10 +1,13 @@
 { lib
+, stdenv
 , buildPythonPackage
 , fetchPypi
 , six
 , requests-cache
 , pygments
 , pyquery
+, cachelib
+, appdirs
 }:
 
 buildPythonPackage rec {
@@ -16,7 +19,8 @@ buildPythonPackage rec {
     sha256 = "3b322668606d29d8a841c3b28c0574851f512b55c33a7ceb982b6a98d82fa3e3";
   };
 
-  propagatedBuildInputs = [ six requests-cache pygments pyquery ];
+  propagatedBuildInputs = [ six requests-cache pygments pyquery ]
+    ++ lib.optionals stdenv.isDarwin [ cachelib appdirs ];
 
   preCheck = ''
     export HOME=$(mktemp -d)
