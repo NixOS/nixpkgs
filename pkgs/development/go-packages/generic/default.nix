c1facd193e8031a7a86ae1bf93318f99df65b392
zowoq <59103226+zowoq@users.noreply.github.com>
2020-04-19 16:11:10 +1000

    buildGoPackage: remove inactive maintainer

--- pkgs/development/go-packages/generic/default.nix
+++ pkgs/development/go-packages/generic/default.nix
@@ -242,11 +242,7 @@ let
       # Add default meta information
       homepage = "https://${goPackagePath}";
       platforms = go.meta.platforms or lib.platforms.all;
-    } // meta // {
-      # add an extra maintainer to every package
-      maintainers = (meta.maintainers or []) ++
-                    [ lib.maintainers.lethalman ];
-    };
+    } // meta;
   });
 in if disabled then
   throw "${package.name} not supported for go ${go.meta.branch}"
