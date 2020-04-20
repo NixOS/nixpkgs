8c0c24bf93b675c058d9c720d6c7665ba4deb90d
misuzu <bakalolka@gmail.com>
2020-04-20 18:33:33 +0300

    nix-simple-deploy: init at 0.1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25703,6 +25703,8 @@ in
 
   nix-serve = callPackage ../tools/package-management/nix-serve { };
 
+  nix-simple-deploy = callPackage ../tools/package-management/nix-simple-deploy { };
+
   nixfmt = haskell.lib.justStaticExecutables haskellPackages.nixfmt;
 
   nixpkgs-fmt = callPackage ../tools/nix/nixpkgs-fmt { };
