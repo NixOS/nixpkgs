0d1194704badfe656de207cd287bb3b802593ef5
Michael Weiss <dev.primeos@gmail.com>
2020-04-21 19:23:08 +0200

    python3Packages.google_api_python_client: 1.8.0 -> 1.8.1
    This contains a fix for the goobook build which would break due to this
    update.

--- pkgs/development/python-modules/google-api-python-client/default.nix
+++ pkgs/development/python-modules/google-api-python-client/default.nix
@@ -1,15 +1,23 @@
-{ lib, buildPythonPackage, fetchPypi
+{ lib, buildPythonPackage, fetchPypi, fetchpatch
 , google_auth, google-auth-httplib2, google_api_core
 , httplib2, six, uritemplate, oauth2client }:
 
 buildPythonPackage rec {
   pname = "google-api-python-client";
-  version = "1.8.0";
+  version = "1.8.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "003rgr15r9j080f3n5y2x6ymxsfv652m3r7j83p7sbrd9shl4nqg";
+    sha256 = "14d7719sxl7bqpw3k4hhfwd0l0v98lnpi7qlhdaf8jxw21ivzmpz";
   };
+
+  patches = [
+    # To fix a regression/bug in 1.8.1:
+    (fetchpatch {
+      url = "https://github.com/googleapis/google-api-python-client/commit/1d8ec6874e1c6081893de7cd7cbc86d1f6580320.patch";
+      sha256 = "1nr24jzvbkzaigv9c935fkpzfa36hj6k7yx5bdwxqfhpa3p9i8n9";
+    })
+  ];
 
   # No tests included in archive
   doCheck = false;
