22be0dac7b18128f6ab68c5973c011d970f3d946
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-04-22 11:53:25 +0200

    babeld: 1.9.1 → 1.9.2
    Dear all,
    
    Babeld-1.9.2 is available from
    
      https://www.irif.fr/~jch/software/files/babeld-1.9.2.tar.gz
      https://www.irif.fr/~jch/software/files/babeld-1.9.2.tar.gz.asc
    
    For more information about the Babel routing protocol, please see
    
      https://www.irif.fr/~jch/software/babel/
    
    This is a bug fix release.  It fixes two bugs where IPv4 prefixes could be
    represented incorrectly, with a range of confusing symptoms ; many thanks
    to Faban Bläse for diagnosing the issue.  In addition, it fixes incorrect
    parsing of unknown address encodings, thanks to Théo Bastian for the fix.
    
    21 April 2020: babeld-1.9.2
    
      * Fixed two issues that could cause IPv4 routes to be represented
        incorrectly, with a range of confusing symptoms.  Thanks to
        Fabian Bläse.
      * Fixed incorrect parsing of TLVs with an unknown Address Encoding.
        Thanks to Théophile Bastian.
      * Fixed access to mis-aligned data structure.  Thanks to Antonin Décimo.
    
    -- Juliusz Chroboczek
    
    _______________________________________________
    Babel-users mailing list
    Babel-users@alioth-lists.debian.net
    https://alioth-lists.debian.net/cgi-bin/mailman/listinfo/babel-users
    
    (cherry picked from commit 34c230ad12ef4542c088964e2a450fba6a87ad65)

--- pkgs/tools/networking/babeld/default.nix
+++ pkgs/tools/networking/babeld/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "babeld";
-  version = "1.9.1";
+  version = "1.9.2";
 
   src = fetchurl {
     url = "http://www.pps.univ-paris-diderot.fr/~jch/software/files/${pname}-${version}.tar.gz";
-    sha256 = "1d503igqv9s5pgrhvxp1czjy2xfsjhagyyh2iny7g4cjvl0kq6qy";
+    sha256 = "01vzhrspnm4sy9ggaz9n3bfl5hy3qlynr218j3mdcddzm3h00kqm";
   };
 
   preBuild = ''
