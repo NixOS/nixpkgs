2319f0c168aa13a2be6f5b7876ed85b4f7d3792a
Michael Weiss <dev.primeos@gmail.com>
2020-04-22 13:13:51 +0200

    python3Packages.google_api_python_client: 1.8.1 -> 1.8.2

--- pkgs/development/python-modules/google-api-python-client/default.nix
+++ pkgs/development/python-modules/google-api-python-client/default.nix
@@ -1,23 +1,15 @@
-{ lib, buildPythonPackage, fetchPypi, fetchpatch
+{ lib, buildPythonPackage, fetchPypi
 , google_auth, google-auth-httplib2, google_api_core
 , httplib2, six, uritemplate, oauth2client }:
 
 buildPythonPackage rec {
   pname = "google-api-python-client";
-  version = "1.8.1";
+  version = "1.8.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "14d7719sxl7bqpw3k4hhfwd0l0v98lnpi7qlhdaf8jxw21ivzmpz";
+    sha256 = "09g7v6yrf8p0kgmjnzy9d0fx9p1zngk2pmprf0bx19j1zc9jqj5z";
   };
-
-  patches = [
-    # To fix a regression/bug in 1.8.1:
-    (fetchpatch {
-      url = "https://github.com/googleapis/google-api-python-client/commit/1d8ec6874e1c6081893de7cd7cbc86d1f6580320.patch";
-      sha256 = "1nr24jzvbkzaigv9c935fkpzfa36hj6k7yx5bdwxqfhpa3p9i8n9";
-    })
-  ];
 
   # No tests included in archive
   doCheck = false;
