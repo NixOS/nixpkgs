NixOS/nixpkgs

commit c6be4c19578ecd8ac8211b753640f3693d00c8d9

treewide: mark broken packages for 20.03

pkgs/development/python-modules/pyside2-tools/default.nix
@@ -31,5 +31,6 @@ stdenv.mkDerivation {
     license = licenses.gpl2;
     homepage = "https://wiki.qt.io/Qt_for_Python";
     maintainers = with maintainers; [ gebner ];
+    broken = true;
   };
 }

pkgs/applications/misc/safeeyes/default.nix
@@ -66,5 +66,6 @@ in buildPythonApplication rec {
     license = lib.licenses.gpl3;
     maintainers = with lib.maintainers; [ srghma ];
     platforms = lib.platforms.all;
+    broken = true;
   };
 }

pkgs/applications/misc/syncthingtray/default.nix
@@ -54,5 +54,6 @@ mkDerivation rec {
     license = licenses.gpl2;
     maintainers = with maintainers; [ doronbehar ];
     platforms = platforms.linux;
+    broken = true;
   };
 }

pkgs/applications/misc/topydo/default.nix
@@ -30,5 +30,6 @@ buildPythonApplication rec {
     description = "A cli todo application compatible with the todo.txt format";
     homepage = "https://github.com/bram85/topydo";
     license = licenses.gpl3;
+    broken = true;
   };
 }

pkgs/applications/networking/instant-messengers/salut-a-toi/default.nix
@@ -56,5 +56,6 @@ in
       platforms = platforms.linux;
       maintainers = [ maintainers.raskin ];
       license = licenses.gpl3Plus;
+      broken = true;
     };
   }

pkgs/applications/networking/p2p/stig/default.nix
@@ -52,5 +52,6 @@ python3.pkgs.buildPythonApplication rec {
     homepage = "https://github.com/rndusr/stig";
     license = licenses.gpl3;
     maintainers = with maintainers; [ doronbehar ];
+    broken = true;
   };
 }

pkgs/applications/networking/super-productivity/default.nix
@@ -102,5 +102,6 @@ in stdenv.mkDerivation {
     license = licenses.mit;
     platforms = [ "x86_64-linux" ];
     maintainers = with maintainers; [ offline ];
+    broken = true;
   };
 }

pkgs/applications/office/paperwork/default.nix
@@ -77,5 +77,6 @@ python3Packages.buildPythonApplication rec {
     license = lib.licenses.gpl3Plus;
     maintainers = [ lib.maintainers.aszlig ];
     platforms = lib.platforms.linux;
+    broken = true;
   };
 }

commit 150b510420e7df626beb71c97e48096d8ae9817c

pythonPackages.pyside2-tools: unmark as broken
It builds fine.

Fixes: c6be4c1957 ("treewide: mark broken packages for 20.03")

pkgs/development/python-modules/pyside2-tools/default.nix
@@ -31,6 +31,5 @@ stdenv.mkDerivation {
     license = licenses.gpl2;
     homepage = "https://wiki.qt.io/Qt_for_Python";
     maintainers = with maintainers; [ gebner ];
-    broken = true;
   };
 }