bbe7b86eea5cbdf119fcf5ffd82b7ad78c1f60c4
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2020-04-23 19:22:58 +0900

    cataclysmDDA: launch the wrapped program

--- pkgs/games/cataclysm-dda/wrapper.nix
+++ pkgs/games/cataclysm-dda/wrapper.nix
@@ -28,5 +28,20 @@ else symlinkJoin {
         wrapProgram $out/bin/cataclysm-tiles \
             --add-flags "--datadir $out/share/cataclysm-dda/"
     fi
+
+    # Launch the wrapped program
+    replaceProgram() {
+        cp "$1" "''${1}.bk"
+        unlink "$1"
+        mv "''${1}.bk" "$1"
+        sed -i "$1" -e "s,/nix/store/.\+\(/bin/cataclysm-tiles\),$out\1,"
+    }
+    for script in "$out/share/applications/cataclysm-dda.desktop" \
+                  "$out/Applications/Cataclysm.app/Contents/MacOS/Cataclysm.sh"
+    do
+        if [ -e "$script" ]; then
+            replaceProgram "$script"
+        fi
+    done
   '';
 }
