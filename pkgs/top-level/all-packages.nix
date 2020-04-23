019dc065ee5874eee1ef8f23f5236022843fd8c8
Jörg Thalheim <joerg@thalheim.io>
2020-04-23 08:11:06 +0100

    gometalinter: remove
    gometalinter development has stopped. golangci-lint is the replacement

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8599,8 +8599,6 @@ in
 
   gogetdoc = callPackage ../development/tools/gogetdoc { };
 
-  gometalinter = callPackage ../development/tools/gometalinter { };
-
   gox = callPackage ../development/tools/gox { };
 
   gprolog = callPackage ../development/compilers/gprolog { };
