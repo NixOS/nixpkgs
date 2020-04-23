854804bf77724e6d893b8d3007645123538db2c9
Alyssa Ross <hi@alyssa.is>
2020-04-23 16:02:44 +0000

    rustc: 1.42.0 -> 1.43.0
    Kept 1.42 around for Thunderbird.
    
    i686-apple-darwin is no longer supported upstream.  We could still
    support building it, for this one release, since we have the binary
    for the previous release, (or bootstrap it for future releases from
    Rust 1.42,) but since this release is the one that drops support, I
    think it makes sense to do it now.  (And probably nobody is using it
    anyway.)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9017,10 +9017,14 @@ in
   rust_1_42 = callPackage ../development/compilers/rust/1_42.nix {
     inherit (darwin.apple_sdk.frameworks) CoreFoundation Security;
   };
-  rust = rust_1_42;
+  rust_1_43 = callPackage ../development/compilers/rust/1_43.nix {
+    inherit (darwin.apple_sdk.frameworks) CoreFoundation Security;
+  };
+  rust = rust_1_43;
 
   rustPackages_1_42 = rust_1_42.packages.stable;
-  rustPackages = rustPackages_1_42;
+  rustPackages_1_43 = rust_1_43.packages.stable;
+  rustPackages = rustPackages_1_43;
 
   inherit (rustPackages) cargo clippy rustc rustPlatform;
   inherit (rust) makeRustPlatform;
@@ -22145,6 +22149,7 @@ in
 
   thunderbird = callPackage ../applications/networking/mailreaders/thunderbird {
     inherit (gnome2) libIDL;
+    inherit (rustPackages_1_42) rustc;
     libpng = libpng_apng;
     gtk3Support = true;
   };
