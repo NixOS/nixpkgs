1ae963172557035eb07fd27440ea0869770efdb7
Sascha Grunert <sgrunert@suse.com>
2020-04-23 16:36:53 +0200

    cri-o: 1.17.3 -> 1.18.0
    - Make buildtags being able to set via the BUILDTAGS environment
    - Switch to go 1.14
    
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23306,9 +23306,7 @@ in
 
   crispyDoom = callPackage ../games/crispy-doom { };
 
-  cri-o = callPackage ../applications/virtualization/cri-o {
-    buildGoPackage = buildGo112Package;
-  };
+  cri-o = callPackage ../applications/virtualization/cri-o { };
 
   ckan = callPackage ../games/ckan { };
 
