df1fd7d2a4ef9d054f128eedf4d58b123e4fb2d0
adisbladis <adisbladis@gmail.com>
2020-04-24 11:19:48 +0100

    emacsPackages.magit-patch-changelog: Fix build

--- pkgs/applications/editors/emacs-modes/melpa-packages.nix
+++ pkgs/applications/editors/emacs-modes/melpa-packages.nix
@@ -218,6 +218,12 @@ env NIXPKGS_ALLOW_BROKEN=1 nix-instantiate --show-trace ../../../../ -A emacsPac
             (attrs.nativeBuildInputs or []) ++ [ external.git ];
         });
 
+        magit-patch-changelog = super.magit-patch-changelog.overrideAttrs (attrs: {
+          # searches for Git at build time
+          nativeBuildInputs =
+            (attrs.nativeBuildInputs or []) ++ [ external.git ];
+        });
+
         magit-annex = super.magit-annex.overrideAttrs (attrs: {
           # searches for Git at build time
           nativeBuildInputs =
