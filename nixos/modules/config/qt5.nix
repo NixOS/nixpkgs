39a274614c0511930c33ca917b5604120a4a4d7b
gnidorah <gnidorah@users.noreply.github.com>
2020-04-24 16:29:40 +0300

    nixos/qt5: refactor to support more platform themes

--- nixos/modules/config/qt5.nix
+++ nixos/modules/config/qt5.nix
@@ -6,13 +6,63 @@ let
 
   cfg = config.qt5;
 
-  isQGnome = cfg.platformTheme == "gnome" && builtins.elem cfg.style ["adwaita" "adwaita-dark"];
-  isQtStyle = cfg.platformTheme == "gtk2" && !(builtins.elem cfg.style ["adwaita" "adwaita-dark"]);
+  themeType = types.submodule {
+    options = {
+      package = mkOption {
+        internal = true;
+        type = types.nullOr types.package;
+        default = null;
+      };
+      name = mkOption {
+        internal = true;
+        type = types.str;
+      };
+    };
+  };
 
-  packages = if isQGnome then [ pkgs.qgnomeplatform pkgs.adwaita-qt ]
-    else if isQtStyle then [ pkgs.libsForQt5.qtstyleplugins ]
-    else throw "`qt5.platformTheme` ${cfg.platformTheme} and `qt5.style` ${cfg.style} are not compatible.";
+  optionalPackage = opt:
+    optional (opt != null && opt.package != null) opt.package;
 
+  platforms = {
+    gtk2 = rec {
+      description = ''
+        <varlistentry>
+          <term><literal>gtk2</literal></term>
+          <listitem><para>Use GTK2 theme with
+            <link xlink:href="https://github.com/qt/qtstyleplugins">qtstyleplugins</link>
+          </para></listitem>
+        </varlistentry>
+      '';
+      styles = [ "cleanlooks" "gtk2" "cde" "motif" "plastique" ];
+
+      assertions = [
+        {
+          assertion = cfg.style != null && any (name: name == cfg.style.name) styles;
+          message = "`qt5.style.name` is not one of [ ${toString styles} ].";
+        }
+      ];
+      environment.variables.QT_QPA_PLATFORMTHEME = "gtk2";
+      environment.variables.QT_STYLE_OVERRIDE = cfg.style.name;
+      environment.systemPackages = [ pkgs.libsForQt5.qtstyleplugins ];
+    };
+    qgnomeplatform = {
+      description = ''
+        <varlistentry>
+          <term><literal>qgnomeplatform</literal></term>
+          <listitem><para>Use GNOME theme with
+            <link xlink:href="https://github.com/FedoraQt/QGnomePlatform">qgnomeplatform</link>
+          </para></listitem>
+        </varlistentry>
+      '';
+
+      environment.variables.QT_QPA_PLATFORMTHEME = "qgnomeplatform";
+      # TODO: make this optional once https://github.com/NixOS/nixpkgs/issues/54150 is fixed
+      # qgnomeplatform reads theme and other settings from dconf db
+      environment.variables.QT_STYLE_OVERRIDE = cfg.style.name;
+      environment.variables.XDG_DATA_DIRS = [ "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}" ];
+      environment.systemPackages = [ pkgs.qgnomeplatform ];
+    };
+  };
 in
 
 {
@@ -23,82 +73,42 @@ in
       enable = mkEnableOption "Qt5 theming configuration";
 
       platformTheme = mkOption {
-        type = types.enum [
-          "gtk2"
-          "gnome"
-        ];
-        example = "gnome";
-        relatedPackages = [
-          "qgnomeplatform"
-          ["libsForQt5" "qtstyleplugins"]
-        ];
+        type = types.enum (attrNames platforms);
+        example = head (attrNames platforms);
         description = ''
           Selects the platform theme to use for Qt5 applications.</para>
           <para>The options are
           <variablelist>
-            <varlistentry>
-              <term><literal>gtk</literal></term>
-              <listitem><para>Use GTK theme with
-                <link xlink:href="https://github.com/qt/qtstyleplugins">qtstyleplugins</link>
-              </para></listitem>
-            </varlistentry>
-            <varlistentry>
-              <term><literal>gnome</literal></term>
-              <listitem><para>Use GNOME theme with
-                <link xlink:href="https://github.com/FedoraQt/QGnomePlatform">qgnomeplatform</link>
-              </para></listitem>
-            </varlistentry>
+            ${concatStrings (mapAttrsToList (name: value: value.description) platforms)}
           </variablelist>
         '';
       };
 
       style = mkOption {
-        type = types.enum [
-          "adwaita"
-          "adwaita-dark"
-          "cleanlooks"
-          "gtk2"
-          "motif"
-          "plastique"
-        ];
-        example = "adwaita";
-        relatedPackages = [
-          "adwaita-qt"
-          ["libsForQt5" "qtstyleplugins"]
-        ];
-        description = ''
-          Selects the style to use for Qt5 applications.</para>
-          <para>The options are
-          <variablelist>
-            <varlistentry>
-              <term><literal>adwaita</literal></term>
-              <term><literal>adwaita-dark</literal></term>
-              <listitem><para>Use Adwaita Qt style with
-                <link xlink:href="https://github.com/FedoraQt/adwaita-qt">adwaita</link>
-              </para></listitem>
-            </varlistentry>
-            <varlistentry>
-              <term><literal>cleanlooks</literal></term>
-              <term><literal>gtk2</literal></term>
-              <term><literal>motif</literal></term>
-              <term><literal>plastique</literal></term>
-              <listitem><para>Use styles from
-                <link xlink:href="https://github.com/qt/qtstyleplugins">qtstyleplugins</link>
-              </para></listitem>
-            </varlistentry>
-          </variablelist>
+        type = types.nullOr themeType;
+        default = null;
+        example = literalExample ''
+          {
+            name = "Breeze";
+            package = pkgs.breeze-qt5;
+          };
         '';
+        description = "The Qt style to use.";
       };
+
     };
   };
 
   config = mkIf cfg.enable {
 
-    environment.variables.QT_QPA_PLATFORMTHEME = cfg.platformTheme;
+    assertions = attrByPath [ cfg.platformTheme "assertions" ] [] platforms;
 
-    environment.variables.QT_STYLE_OVERRIDE = cfg.style;
+    environment.variables = attrByPath [ cfg.platformTheme "environment" "variables" ] {} platforms;
 
-    environment.systemPackages = packages;
+    environment.systemPackages = attrByPath [ cfg.platformTheme "environment" "systemPackages" ] [] platforms
+      ++ optionalPackage cfg.style;
 
   };
+
+  meta.maintainers = with maintainers; [ worldofpeace gnidorah ];
 }
