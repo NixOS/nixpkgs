bd30b6e47945b7b5c2e9d000dedbba313187b2cf
adisbladis <adisbladis@gmail.com>
2020-04-24 11:19:10 +0100

    emacsPackages.magit-lfs: Fix build

--- pkgs/applications/editors/emacs-modes/melpa-packages.nix
+++ pkgs/applications/editors/emacs-modes/melpa-packages.nix
@@ -200,6 +200,12 @@ env NIXPKGS_ALLOW_BROKEN=1 nix-instantiate --show-trace ../../../../ -A emacsPac
             (attrs.nativeBuildInputs or []) ++ [ external.git ];
         });
 
+        magit-lfs = super.magit-lfs.overrideAttrs (attrs: {
+          # searches for Git at build time
+          nativeBuildInputs =
+            (attrs.nativeBuildInputs or []) ++ [ external.git ];
+        });
+
         magit-annex = super.magit-annex.overrideAttrs (attrs: {
           # searches for Git at build time
           nativeBuildInputs =
