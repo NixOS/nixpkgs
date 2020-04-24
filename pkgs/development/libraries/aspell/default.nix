295475a378a58d4ac344944b470eab2d3f97866e
Kevin Stock <kevin@kevinstock.org>
2020-04-24 10:39:12 -0400

    aspell, tla: use HTTPS instead of FTP
    ftp.gnu.org intends to disable the FTP protocol for downloads on this
    server, and strongly recommends using https instead.

--- pkgs/development/libraries/aspell/default.nix
+++ pkgs/development/libraries/aspell/default.nix
@@ -8,7 +8,7 @@ let
   # dictionary like Debian does.
   devaMapsSource = fetchzip {
     name = "aspell-u-deva";
-    url = "ftp://ftp.gnu.org/gnu/aspell/dict/mr/aspell6-mr-0.10-0.tar.bz2";
+    url = "https://ftp.gnu.org/gnu/aspell/dict/mr/aspell6-mr-0.10-0.tar.bz2";
     sha256 = "1v8cdl8x2j1d4vbvsq1xrqys69bbccd6mi03fywrhkrrljviyri1";
   };
 
