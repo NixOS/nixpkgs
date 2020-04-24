43818efd94526c0ca928362410a436648fc355cf
adisbladis <adisbladis@gmail.com>
2020-04-24 11:19:35 +0100

    emacsPackages.magit-p4: Fix build

--- pkgs/applications/editors/emacs-modes/melpa-packages.nix
+++ pkgs/applications/editors/emacs-modes/melpa-packages.nix
@@ -212,6 +212,12 @@ env NIXPKGS_ALLOW_BROKEN=1 nix-instantiate --show-trace ../../../../ -A emacsPac
             (attrs.nativeBuildInputs or []) ++ [ external.git ];
         });
 
+        magit-p4 = super.magit-p4.overrideAttrs (attrs: {
+          # searches for Git at build time
+          nativeBuildInputs =
+            (attrs.nativeBuildInputs or []) ++ [ external.git ];
+        });
+
         magit-annex = super.magit-annex.overrideAttrs (attrs: {
           # searches for Git at build time
           nativeBuildInputs =
