8e8dd6527020abbb3cf45a5c107020c7906b4209
Austin Seipp <aseipp@pobox.com>
2020-04-24 10:57:17 -0500

    linux_testing: 5.6-rc7 -> 5.7-rc2
    Signed-off-by: Austin Seipp <aseipp@pobox.com>
    (cherry picked from commit d40391145108d3db0f06ddfc806e5de25547c032)

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,15 +3,15 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.6-rc7";
-  extraMeta.branch = "5.6";
+  version = "5.7-rc2";
+  extraMeta.branch = "5.7";
 
   # modDirVersion needs to be x.y.z, will always add .0
   modDirVersion = if (modDirVersionArg == null) then builtins.replaceStrings ["-"] [".0-"] version else modDirVersionArg;
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "0wv3ipfm970y2pyadwn5g7hd9bj117qk8jl8sdhrasbsy1p8936i";
+    sha256 = "06h7lrif84gyzblrdgg4cirsr2v69sdbymwkk89nr3gc87hnjlvs";
   };
 
   # Should the testing kernels ever be built on Hydra?
