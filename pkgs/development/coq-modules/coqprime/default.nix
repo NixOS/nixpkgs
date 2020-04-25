597fa6151661ecc6ec7d30f7385f8f2be28787c8
Cyril Cohen <cohen@crans.org>
2020-04-25 23:06:42 +0200

    Updating coqPackages coqPackages_8_10 coqPackages_8_11
    - fixed bignum version
    - fixed coq-bits version
    - fixed coqprime version
    - fixed mathcomp and mathcomp extra packages
      (reworked building scheme and removed unused ssreflect directory)
    - giving the user access to function filterCoqPackages, because overrideScope' does not re-apply it.

--- pkgs/development/coq-modules/coqprime/default.nix
+++ pkgs/development/coq-modules/coqprime/default.nix
@@ -1,22 +1,26 @@
-{ stdenv, fetchFromGitHub, coq, bignums }:
+{ stdenv, which, fetchFromGitHub, coq, bignums }:
 
-let params =
-  let v_8_8 = {
-      version = "8.8";
-      sha256 = "075yjczk79pf1hd3lgdjiz84ilkzfxjh18lgzrhhqp7d3kz5lxp5";
-    };
-  in
-  {
-    "8.7" = {
-      version = "8.7.2";
-      sha256 = "15zlcrx06qqxjy3nhh22wzy0rb4npc8l4nx2bbsfsvrisbq1qb7k";
-    };
-    "8.8" = v_8_8;
-    "8.9" = v_8_8;
-    "8.10" = v_8_8;
-    };
-  param = params.${coq.coq-version}
-; in
+let
+  params =
+    let v_8_8 = {
+          version = "8.8";
+          sha256 = "075yjczk79pf1hd3lgdjiz84ilkzfxjh18lgzrhhqp7d3kz5lxp5";
+        };
+    in
+      {
+        "8.7" = {
+          version = "8.7.2";
+          sha256 = "15zlcrx06qqxjy3nhh22wzy0rb4npc8l4nx2bbsfsvrisbq1qb7k";
+        };
+        "8.8" = v_8_8;
+        "8.9" = v_8_8;
+        "8.10" = {
+          version = "8.10";
+          sha256 = "0r9gnh5a5ykiiz5h1i8xnzgiydpwc4z9qhndxyya85xq0f910qaz";
+        };
+      };
+  param = params.${coq.coq-version};
+in
 
 stdenv.mkDerivation rec {
 
@@ -30,7 +34,7 @@ stdenv.mkDerivation rec {
     inherit (param) sha256;
   };
 
-  buildInputs = [ coq ];
+  buildInputs = [ which coq ];
 
   propagatedBuildInputs = [ bignums ];
 
