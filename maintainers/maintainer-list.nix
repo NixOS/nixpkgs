32341633786403a48138df03f6b8ebc2938366c0
Wulfsta <wulfstawulfsta@gmail.com>
2020-04-25 16:07:24 -0400

    maintainers: add wulfsta

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8269,6 +8269,12 @@
     githubId = 483465;
     name = "Mateusz Wykurz";
   };
+  wulfsta = {
+    email = "wulfstawulfsta@gmail.com";
+    github = "Wulfsta";
+    githubId = 13378502;
+    name = "Wulfsta";
+  };
   wyvie = {
     email = "elijahrum@gmail.com";
     github = "wyvie";
