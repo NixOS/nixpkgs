c27bd9af4e642173401b50047a4eaf1beaa1bda9
Lily Ballard <lily@sb.org>
2020-04-24 20:23:59 -0700

    bat-extras: init at 20200408

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1170,6 +1170,8 @@ in
     inherit (darwin.apple_sdk.frameworks) Security;
   };
 
+  bat-extras = recurseIntoAttrs (callPackages ../tools/misc/bat-extras { });
+
   bc = callPackage ../tools/misc/bc { };
 
   bdf2psf = callPackage ../tools/misc/bdf2psf { };
