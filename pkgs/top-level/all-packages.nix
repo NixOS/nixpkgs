a416707246077ae84b5597fe94fcfa403903faa0
Doron Behar <doron.behar@gmail.com>
2020-04-25 04:03:21 +0300

    mailnag: 1.3.0 -> 2.0.0
    Notable changes:
    - Switch to Python3 by default.
    - Format inputs and arguments.
    - Use wrapGAppsHook.
    - Use libsecret (replaces libgnome-keyring) by default
    - Add @doronbehar as maintainer.
    - Remove overridden phases.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5047,7 +5047,7 @@ in
   mailhog = callPackage ../servers/mail/mailhog {};
 
   mailnag = callPackage ../applications/networking/mailreaders/mailnag {
-    pythonPackages = python2Packages;
+    pythonPackages = python3Packages;
   };
 
   mailsend = callPackage ../tools/networking/mailsend { };
