b24d2a90dd639bcd3083fe75fd675020884589c8
Peter Simons <simons@cryp.to>
2020-04-25 18:00:09 +0200

    configuration-ghc-8.10.x.nix: cosmetic

--- pkgs/development/haskell-modules/configuration-ghc-8.10.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.10.x.nix
@@ -64,9 +64,9 @@ self: super: {
   zlib = doJailbreak super.zlib;
 
   # Use the latest version to fix the build.
+  ghc-lib-parser-ex = self.ghc-lib-parser-ex_8_10_0_4;
   optics-core = self.optics-core_0_3;
   repline = self.repline_0_3_0_0;
-  ghc-lib-parser-ex = self.ghc-lib-parser-ex_8_10_0_4;
   th-desugar = self.th-desugar_1_11;
 
   # `ghc-lib-parser-ex` (see conditionals in its `.cabal` file) does not need
