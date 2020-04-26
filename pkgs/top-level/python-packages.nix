d0269a3b97efa21334c1b4baccad2621bdd5f271
Renato Florentino Garcia <fgarcia.renato@gmail.com>
2020-04-26 00:37:15 -0300

    pythonPackages.executing: init at 0.4.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3738,6 +3738,8 @@ in {
 
   eventlet = callPackage ../development/python-modules/eventlet { };
 
+  executing = callPackage ../development/python-modules/executing { };
+
   exifread = callPackage ../development/python-modules/exifread { };
 
   fastimport = callPackage ../development/python-modules/fastimport { };
