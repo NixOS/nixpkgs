c463b54051f2bdb8c4d90451c0f8886180c9c684
Vincent Bernat <vincent@bernat.ch>
2020-04-26 20:34:29 +0200

    bgpq4: init at 0.0.6
    This is a fork of bgpq3. It is still unclear if it will superseed
    bgpq3. It is mostly compatible with additional features, like support
    for Mikrotik devices. It also defaults to NTT IRR server instead of
    RADB.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19551,6 +19551,8 @@ in
 
   bgpq3 = callPackage ../tools/networking/bgpq3 { };
 
+  bgpq4 = callPackage ../tools/networking/bgpq4 { };
+
   blackbox = callPackage ../applications/version-management/blackbox { };
 
   bleachbit = callPackage ../applications/misc/bleachbit { };
