4cc520ba0fcb460e42a04802982be1b8d3312773
Jörg Thalheim <joerg@thalheim.io>
2020-04-27 10:11:22 +0100

    kops: remove old versions

--- pkgs/applications/networking/cluster/kops/default.nix
+++ pkgs/applications/networking/cluster/kops/default.nix
@@ -51,21 +51,6 @@ in rec {
 
   mkKops = generic;
 
-  kops_1_12 = mkKops {
-    version = "1.12.3";
-    sha256 = "0rpbaz54l5v1z7ab5kpxcb4jyakkl5ysgz1sxajqmw2d6dvf7xly";
-  };
-
-  kops_1_13 = mkKops {
-    version = "1.13.2";
-    sha256 = "0lkkg34vn020r62ga8vg5d3a8jwvq00xlv3p1s01nkz33f6salng";
-  };
-
-  kops_1_14 = mkKops {
-    version = "1.14.1";
-    sha256 = "0ikd8qwrjh8s1sc95g18sm0q6p33swz2m1rjd8zw34mb2w9jv76n";
-  };
-
   kops_1_15 = mkKops {
     version = "1.15.2";
     sha256 = "1sjfd7pfi81ccq1dkgkh9xx6y94bqzlp727pvyf7l01x3d14z2b3";
