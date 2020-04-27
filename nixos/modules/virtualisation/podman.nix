3c83386c6be2a01ff850d10e70aa8ce7fb2e9a93
zowoq <59103226+zowoq@users.noreply.github.com>
2020-04-27 14:08:19 +1000

    nixos/podman: add assertion for dockerCompat

--- nixos/modules/virtualisation/podman.nix
+++ nixos/modules/virtualisation/podman.nix
@@ -100,6 +100,11 @@ in
     # Enable common /etc/containers configuration
     virtualisation.containers.enable = true;
 
+    assertions = [{
+      assertion = cfg.dockerCompat -> !config.virtualisation.docker.enable;
+      message = "Option dockerCompat conflicts with docker";
+    }];
+
   };
 
 }
