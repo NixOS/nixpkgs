ebd199184d7273753e9ee51553927cd22a9ffd61
Richard Marko <srk@48.io>
2020-04-28 18:32:22 +0200

    nixos/proxychains: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -143,6 +143,7 @@
   ./programs/npm.nix
   ./programs/oblogout.nix
   ./programs/plotinus.nix
+  ./programs/proxychains.nix
   ./programs/qt5ct.nix
   ./programs/screen.nix
   ./programs/sedutil.nix
