ae0685129bb8e37da6306f08549cf43b8c02e767
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2020-04-28 16:42:26 -0400

    sherpa: 2.2.8 -> 2.2.9

--- pkgs/applications/science/physics/sherpa/default.nix
+++ pkgs/applications/science/physics/sherpa/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "sherpa";
-  version = "2.2.8";
+  version = "2.2.9";
 
   src = fetchurl {
     url = "https://www.hepforge.org/archive/sherpa/SHERPA-MC-${version}.tar.gz";
-    sha256 = "1al1imdrknvbcy8k113xysc14lln4msbv281bf0kx7p73wz59mv3";
+    sha256 = "1z7vws97k6zfzyqx0dkv2kq8d83dibi73i5jiqk5a22yplp6bnjh";
   };
 
   buildInputs = [ gfortran sqlite lhapdf rivet ];
@@ -21,13 +21,11 @@ stdenv.mkDerivation rec {
     "--enable-rivet=${rivet}"
   ];
 
-  CXXFLAGS = "-std=c++11"; # needed for rivet on OSX
-
-  meta = {
+  meta = with stdenv.lib; {
     description = "Simulation of High-Energy Reactions of PArticles in lepton-lepton, lepton-photon, photon-photon, lepton-hadron and hadron-hadron collisions";
-    license     = stdenv.lib.licenses.gpl2;
-    homepage    = "https://gitlab.com/sherpa-team/sherpa";
-    platforms   = stdenv.lib.platforms.unix;
-    maintainers = with stdenv.lib.maintainers; [ veprbl ];
+    license = licenses.gpl2;
+    homepage = "https://gitlab.com/sherpa-team/sherpa";
+    platforms = platforms.unix;
+    maintainers = with maintainers; [ veprbl ];
   };
 }
