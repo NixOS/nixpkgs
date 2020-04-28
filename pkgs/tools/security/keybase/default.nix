d26aa07c891bec97c429c98d42bafad222d3798c
Aldwin Vlasblom <aldwin@avaq.it>
2020-04-28 10:41:58 +0200

    keybase,kbfs,keybase-gui: 5.3.1 -> 5.4.2
    (cherry picked from commit e1820dc344d36c27b3b8a81fbd3274d50041cb66)

--- pkgs/tools/security/keybase/default.nix
+++ pkgs/tools/security/keybase/default.nix
@@ -6,7 +6,7 @@
 
 buildGoPackage rec {
   pname = "keybase";
-  version = "5.3.1";
+  version = "5.4.2";
 
   goPackagePath = "github.com/keybase/client";
   subPackages = [ "go/kbnm" "go/keybase" ];
@@ -17,7 +17,7 @@ buildGoPackage rec {
     owner = "keybase";
     repo = "client";
     rev = "v${version}";
-    sha256 = "1a1h2c8jr4r20w4gyvyrpsslmh69bl8syl3jbr0fcr2kka7vqnzg";
+    sha256 = "08lw5aw962f75xi42bwbgba94hiql2n2jnsxrkx84czi0ijs1wlr";
   };
 
   patches = [
