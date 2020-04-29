445d554b9bfb8b4b5a12cc498602c7048481e0af
Nathan Fish <lordcirth@gmail.com>
2020-04-29 16:57:30 +0000

    ipfs: 0.4.23 -> 0.5.0

--- pkgs/applications/networking/ipfs/default.nix
+++ pkgs/applications/networking/ipfs/default.nix
@@ -2,21 +2,26 @@
 
 buildGoModule rec {
   pname = "ipfs";
-  version = "0.4.23";
+  version = "0.5.0";
   rev = "v${version}";
+
+  buildPhase = ''
+    sed -i 'd#\$(d)/preload.sh > $@#' plugin/loader/Rules.mk
+    make install
+  '';
 
   src = fetchFromGitHub {
     owner = "ipfs";
     repo = "go-ipfs";
     inherit rev;
-    sha256 = "19m1bhqf1jghdv2ngdnjdk1kvjcxbkgm1ccdkmkabv4ii43h8jwm";
+    sha256 = "0dbyvs49wyqj46c8hvz0fr4vpgfrdj1h8blniwzjf0jabgvw8nik";
   };
 
   postPatch = ''
     rm -rf test/dependencies
   '';
 
-  modSha256 = "12m4ind1s8zaa6kssblc28z2cafy20w2jp80kzif39hg5ar9bijm";
+  modSha256 = "00xgsvpl47miy6paxl8yn6p76h6ssccackh50z0l4r5s7wcc25q8";
 
   meta = with stdenv.lib; {
     description = "A global, versioned, peer-to-peer filesystem";
