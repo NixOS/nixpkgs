NixOS/nixpkgs

commit 1c8aba83340be3713f68410d1d273a8b15e7e007

treewide: use blas and lapack
This makes packages use lapack and blas, which can wrap different
BLAS/LAPACK implementations.

treewide: cleanup from blas/lapack changes

A few issues in the original treewide:

- can’t assume blas64 is a bool
- unused commented code

pkgs/development/ocaml-modules/owl/default.nix
@@ -6,10 +6,13 @@
 , eigen
 , stdio
 , stdlib-shims
-, openblasCompat
+, openblas, blas, lapack
 , owl-base
 , npy
 }:
+
+assert (!blas.is64bit) && (!lapack.is64bit);
+assert blas.implementation == "openblas" && lapack.implementation == "openblas";
 
 buildDunePackage rec {
   pname = "owl";
@@ -19,7 +22,7 @@ buildDunePackage rec {
   checkInputs = [ alcotest ];
   buildInputs = [ dune-configurator ];
   propagatedBuildInputs = [
-    eigen stdio stdlib-shims openblasCompat owl-base npy
+    eigen stdio stdlib-shims openblas owl-base npy
   ];
 
   doCheck = !stdenv.isDarwin;  # https://github.com/owlbarn/owl/issues/462

pkgs/applications/radio/gnss-sdr/default.nix
@@ -12,7 +12,7 @@
 , pythonPackages
 , uhd
 , log4cpp
-, openblas
+, blas, lapack
 , matio
 , pugixml
 , protobuf
@@ -46,7 +46,7 @@ stdenv.mkDerivation rec {
     # nothing to be gained by leaving it out.
     uhd
     log4cpp
-    openblas
+    blas lapack
     matio
     pugixml
     protobuf
@@ -63,8 +63,8 @@ stdenv.mkDerivation rec {
     # armadillo is built using both, so skip checking for them.
     "-DBLAS=YES"
     "-DLAPACK=YES"
-    "-DBLAS_LIBRARIES=-lopenblas"
-    "-DLAPACK_LIBRARIES=-lopenblas"
+    "-DBLAS_LIBRARIES=-lblas"
+    "-DLAPACK_LIBRARIES=-llapack"
 
     # Similarly, it doesn't actually use gfortran despite checking for
     # its presence.

pkgs/applications/science/biology/plink-ng/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, zlib, openblas, darwin}:
+{ stdenv, fetchFromGitHub, zlib, blas, lapack, darwin}:
 
 stdenv.mkDerivation rec {
   pname = "plink-ng";
@@ -11,13 +11,19 @@ stdenv.mkDerivation rec {
     sha256 = "1zhffjbwpd50dxywccbnv1rxy9njwz73l4awc5j7i28rgj3davcq";
   };
 
-  buildInputs = [ zlib ] ++ (if stdenv.isDarwin then [ darwin.apple_sdk.frameworks.Accelerate ] else [ openblas ]) ;
+  buildInputs = [ zlib ] ++ (if stdenv.isDarwin then [ darwin.apple_sdk.frameworks.Accelerate ] else [ blas lapack ]) ;
 
-  buildPhase = ''
+  preBuild = ''
     sed -i 's|zlib-1.2.8/zlib.h|zlib.h|g' *.c *.h
     ${if stdenv.cc.isClang then "sed -i 's|g++|clang++|g' Makefile.std" else ""}
-    make ZLIB=-lz ${if stdenv.isDarwin then "" else "BLASFLAGS=-lopenblas"} -f Makefile.std
+
+    makeFlagsArray+=(
+      ZLIB=-lz
+      BLASFLAGS="-lblas -lcblas -llapack"
+    );
   '';
+
+  makefile = "Makefile.std";
 
   installPhase = ''
     mkdir -p $out/bin
@@ -31,4 +37,3 @@ stdenv.mkDerivation rec {
     platforms = stdenv.lib.platforms.linux;
   };
 }
-

pkgs/applications/science/chemistry/octopus/default.nix
@@ -1,6 +1,8 @@
 { stdenv, fetchFromGitLab, symlinkJoin, gfortran, perl, procps
-, libyaml, libxc, fftw, openblas, gsl, netcdf, arpack, autoreconfHook
+, libyaml, libxc, fftw, blas, lapack, gsl, netcdf, arpack, autoreconfHook
 }:
+
+assert (!blas.is64bit) && (!lapack.is64bit);
 
 stdenv.mkDerivation rec {
   pname = "octopus";
@@ -14,12 +16,12 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [ perl procps autoreconfHook ];
-  buildInputs = [ libyaml gfortran libxc openblas gsl fftw netcdf arpack ];
+  buildInputs = [ libyaml gfortran libxc blas lapack gsl fftw netcdf arpack ];
 
   configureFlags = [
     "--with-yaml-prefix=${libyaml}"
-    "--with-blas=-lopenblas"
-    "--with-lapack=-lopenblas"
+    "--with-blas=-lblas"
+    "--with-lapack=-llapack"
     "--with-fftw-prefix=${fftw.dev}"
     "--with-gsl-prefix=${gsl}"
     "--with-libxc-prefix=${libxc}"

pkgs/applications/science/chemistry/openmolcas/default.nix
@@ -1,8 +1,10 @@
 { stdenv, fetchFromGitLab, cmake, gfortran, perl
-, openblas, hdf5-cpp, python3, texlive
+, openblas, blas, lapack, hdf5-cpp, python3, texlive
 , armadillo, openmpi, globalarrays, openssh
 , makeWrapper, fetchpatch
 } :
+
+assert blas.implementation == "openblas" && lapack.implementation == "openblas";
 
 let
   version = "19.11";

pkgs/applications/science/chemistry/quantum-espresso/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl
-, gfortran, fftw, openblas
+, gfortran, fftw, blas, lapack
 , mpi ? null
 }:
 
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
     patchShebangs configure
   '';
 
-  buildInputs = [ fftw openblas gfortran ]
+  buildInputs = [ fftw blas lapack gfortran ]
     ++ (stdenv.lib.optionals (mpi != null) [ mpi ]);
 
 configureFlags = if (mpi != null) then [ "LD=${mpi}/bin/mpif90" ] else [ "LD=${gfortran}/bin/gfortran" ];

pkgs/applications/science/chemistry/siesta/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl
-, gfortran, openblas
+, gfortran, blas, lapack
 , mpi ? null, scalapack
 }:
 
@@ -16,7 +16,7 @@ stdenv.mkDerivation {
     inherit mpi;
   };
 
-  buildInputs = [ openblas gfortran ]
+  buildInputs = [ blas lapack gfortran ]
     ++ (stdenv.lib.optionals (mpi != null) [ mpi scalapack ]);
 
   enableParallelBuilding = true;
@@ -33,11 +33,11 @@ stdenv.mkDerivation {
     makeFlagsArray=(
         CC="mpicc" FC="mpifort"
         FPPFLAGS="-DMPI" MPI_INTERFACE="libmpi_f90.a" MPI_INCLUDE="."
-        COMP_LIBS="" LIBS="-lopenblas -lscalapack"
+        COMP_LIBS="" LIBS="-lblas -llapack -lscalapack"
     );
   '' else ''
     makeFlagsArray=(
-      COMP_LIBS="" LIBS="-lopenblas"
+      COMP_LIBS="" LIBS="-lblas -llapack"
     );
   '';
 

pkgs/applications/science/machine-learning/shogun/default.nix
@@ -2,7 +2,7 @@
 # data, compression
 , bzip2, curl, hdf5, json_c, lzma, lzo, protobuf, snappy
 # maths
-, openblasCompat, eigen, nlopt, lp_solve, colpack, liblapack, glpk
+, blas, lapack, eigen, nlopt, lp_solve, colpack, glpk
 # libraries
 , libarchive, libxml2
 # extra support
@@ -12,6 +12,8 @@
 
 assert pythonSupport -> pythonPackages != null;
 assert opencvSupport -> opencv != null;
+
+assert (!blas.is64bit) && (!lapack.is64bit);
 
 let
   pname = "shogun";
@@ -64,8 +66,8 @@ stdenv.mkDerivation rec {
   CCACHE_DIR=".ccache";
 
   buildInputs = with lib; [
-      openblasCompat bzip2 cmake colpack curl ctags eigen hdf5 json_c lp_solve lzma lzo
-      protobuf nlopt snappy swig (libarchive.dev) libxml2 liblapack glpk
+      blas lapack bzip2 cmake colpack curl ctags eigen hdf5 json_c lp_solve lzma lzo
+      protobuf nlopt snappy swig (libarchive.dev) libxml2 lapack glpk
     ]
     ++ optionals (pythonSupport) (with pythonPackages; [ python ply numpy ])
     ++ optional  (opencvSupport) opencv;

commit 8f74348be2087dca400d7a879e9330e914601764

ocamlPackages.owl: unbreak by reversing previous edits
 - package was broken by 1c8aba83 (#83888)
 - fixes #85915

pkgs/development/ocaml-modules/owl/default.nix
@@ -6,13 +6,11 @@
 , eigen
 , stdio
 , stdlib-shims
-, openblas, blas, lapack
+, openblasCompat
 , owl-base
 , npy
 }:
 
-assert (!blas.isILP64) && (!lapack.isILP64);
-assert blas.implementation == "openblas" && lapack.implementation == "openblas";
 
 buildDunePackage rec {
   pname = "owl";
@@ -22,7 +20,7 @@ buildDunePackage rec {
   checkInputs = [ alcotest ];
   buildInputs = [ dune-configurator ];
   propagatedBuildInputs = [
-    eigen stdio stdlib-shims openblas owl-base npy
+    eigen stdio stdlib-shims openblasCompat owl-base npy
   ];
 
   doCheck = !stdenv.isDarwin;  # https://github.com/owlbarn/owl/issues/462