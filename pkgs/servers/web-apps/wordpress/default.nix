1329878ccc54b80860939da4d83eb42ce5064c55
schneefux <schneefux+commit@schneefux.xyz>
2020-04-30 08:39:18 +0200

    wordpress: Patch loader for $WORDPRESS_CONFIG

--- pkgs/servers/web-apps/wordpress/default.nix
+++ pkgs/servers/web-apps/wordpress/default.nix
@@ -9,6 +9,10 @@ stdenv.mkDerivation rec {
     sha256 = "10zjgbr96ri87r5v7860vg5ndbnjfhjhily4m9nyl60f1lbjdhrr";
   };
 
+  # exposes $WORDPRESS_CONFIG variable to configure wp-config.php path
+  # upstream issue: https://core.trac.wordpress.org/ticket/41710
+  patches = [ ./wordpress-wp-config.patch ];
+
   installPhase = ''
     mkdir -p $out/share/wordpress
     cp -r . $out/share/wordpress
