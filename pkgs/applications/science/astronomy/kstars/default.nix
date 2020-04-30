ad9496fb40d7e201495fd8ad1ac80135dbe8ad74
tim put <timput@gmail.com>
2020-04-30 07:58:06 -0600

    Update pkgs/applications/science/astronomy/kstars/default.nix
    Co-authored-by: Manuel Bärenz <programming@manuelbaerenz.de>

--- pkgs/applications/science/astronomy/kstars/default.nix
+++ pkgs/applications/science/astronomy/kstars/default.nix
@@ -36,7 +36,11 @@ mkDerivation {
   meta = with lib; {
     description = "Virtual planetarium astronomy software";
     homepage = "https://kde.org/applications/education/org.kde.kstars";
-    longDescription = "It provides an accurate graphical simulation of the night sky, from any location on Earth, at any date and time. The display includes up to 100 million stars, 13,000 deep-sky objects,all 8 planets, the Sun and Moon, and thousands of comets, asteroids, supernovae, and satellites. For students and teachers, it supports adjustable simulation speeds in order to view phenomena that happen over long timescales, the KStars Astrocalculator to predict conjunctions, and many common astronomical calculations";
+    longDescription = ''
+      It provides an accurate graphical simulation of the night sky, from any location on Earth, at any date and time.
+      The display includes up to 100 million stars, 13.000 deep-sky objects, all 8 planets, the Sun and Moon, and thousands of comets, asteroids, supernovae, and satellites.
+      For students and teachers, it supports adjustable simulation speeds in order to view phenomena that happen over long timescales, the KStars Astrocalculator to predict conjunctions, and many common astronomical calculations.
+    '';
     license = licenses.gpl2;
     platforms = platforms.linux;
     maintainers = with maintainers; [ timput ];
