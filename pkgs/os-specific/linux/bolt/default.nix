ca28500d27a59752fa0f811df97afa92a43b17ce
Robin Gloster <mail@glob.in>
2020-04-30 17:30:34 +0200

    bolt: fix build

--- pkgs/os-specific/linux/bolt/default.nix
+++ pkgs/os-specific/linux/bolt/default.nix
@@ -48,8 +48,8 @@ stdenv.mkDerivation rec {
     "-Dlocalstatedir=/var"
   ];
 
-  PKG_CONFIG_SYSTEMD_SYSTEMDSYSTEMUNITDIR = "${placeholder "out"}/lib/systemd/system";
-  PKG_CONFIG_UDEV_UDEVDIR = "${placeholder "out"}/lib/udev";
+  env.PKG_CONFIG_SYSTEMD_SYSTEMDSYSTEMUNITDIR = "${placeholder "out"}/lib/systemd/system";
+  env.PKG_CONFIG_UDEV_UDEVDIR = "${placeholder "out"}/lib/udev";
 
   meta = with stdenv.lib; {
     description = "Thunderbolt 3 device management daemon";
