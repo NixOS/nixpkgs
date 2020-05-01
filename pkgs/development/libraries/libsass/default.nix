5c10ded39a76c6b897b7c5e8b2dded282ec56154
Julien Moutinho <julm+nixpkgs@sourcephile.fr>
2020-05-01 04:40:51 +0200

    libsass: update to 3.6.3 and fix unpacking

--- pkgs/development/libraries/libsass/default.nix
+++ pkgs/development/libraries/libsass/default.nix
@@ -9,10 +9,15 @@ stdenv.mkDerivation rec {
     repo = pname;
     rev = version;
     sha256 = "1q6lvd8sj5k5an32qir918pa5khhcb8h08dzrg1bcxmw7a23j514";
-    # Remove unicode file names which leads to different checksums on HFS+
+    # Exclude unicode file names which leads to different checksums on HFS+
     # vs. other filesystems because of unicode normalisation.
-    extraPostFetch = ''
-      rm -r $out/test/e2e/unicode-pwd
+    # Or leads to a collision when moving the unpacked source
+    # from a tmpfs $TMPDIR to a ZFS Nix store using normalization=formD.
+    postFetch = ''
+      tar -C "$out" \
+       --strip-components=1 \
+       --exclude=test/e2e/unicode-pwd \
+       -xf "$downloadedFile"
     '';
   };
 
