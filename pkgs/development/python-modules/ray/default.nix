6554c386bbfdd50b75cfd37aadacb14a31a2eeb1
Michael Lingelbach <m.j.lbach@gmail.com>
2020-05-01 02:39:31 -0700

    nixpkgs format

--- pkgs/development/python-modules/ray/default.nix
+++ pkgs/development/python-modules/ray/default.nix
@@ -30,13 +30,13 @@ buildPythonPackage {
 
       rpath = stdenv.lib.makeLibraryPath (libpaths);
     in
-    lib.optionalString stdenv.isLinux ''
-      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" ./_build/pip_packages/lib/python3.7/site-packages/ray/core/src/ray/thirdparty/redis/src/redis-server
-      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" ./_build/pip_packages/lib/python3.7/site-packages/ray/core/src/ray/gcs/gcs_server
-      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" ./_build/pip_packages/lib/python3.7/site-packages/ray/core/src/ray/raylet/raylet
-      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" ./_build/pip_packages/lib/python3.7/site-packages/ray/core/src/ray/raylet/raylet_monitor
-      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" ./_build/pip_packages/lib/python3.7/site-packages/ray/core/src/plasma/plasma_store_server
-    '';
+      lib.optionalString stdenv.isLinux ''
+        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" ./_build/pip_packages/lib/python3.7/site-packages/ray/core/src/ray/thirdparty/redis/src/redis-server
+        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" ./_build/pip_packages/lib/python3.7/site-packages/ray/core/src/ray/gcs/gcs_server
+        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" ./_build/pip_packages/lib/python3.7/site-packages/ray/core/src/ray/raylet/raylet
+        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" ./_build/pip_packages/lib/python3.7/site-packages/ray/core/src/ray/raylet/raylet_monitor
+        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" ./_build/pip_packages/lib/python3.7/site-packages/ray/core/src/plasma/plasma_store_server
+      '';
 
 
   pythonImportsCheck = [
@@ -45,7 +45,7 @@ buildPythonPackage {
 
   meta = with stdenv.lib; {
     description = "A system for parallel and distributed Python that unifies "
-                 "the ML ecosystem.";
+      "the ML ecosystem.";
     homepage = https://ray.io;
     license = licenses.asl20;
     maintainers = with maintainers; [ mjlbach ];
