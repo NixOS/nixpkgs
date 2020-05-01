0682131f1467b32c6deb66f85762ba4a5ab130bf
Florian Klink <flokli@flokli.de>
2020-05-01 17:25:10 +0200

    Revert "Revert "nixos/systemd.nix: don’t require online for multi-user.target""
    This reverts commit 15d761a525a025de0680b62e8ab79a9d183f313d.
    This undoes the revert of 764c8203b833176d546395a5c1adf193a9ca73f8,
    fixing the VM tests afterwards.

--- nixos/modules/system/boot/systemd.nix
+++ nixos/modules/system/boot/systemd.nix
@@ -1033,7 +1033,6 @@ in
     systemd.services.systemd-journald.stopIfChanged = false;
     systemd.targets.local-fs.unitConfig.X-StopOnReconfiguration = true;
     systemd.targets.remote-fs.unitConfig.X-StopOnReconfiguration = true;
-    systemd.targets.network-online.wantedBy = [ "multi-user.target" ];
     systemd.services.systemd-binfmt.wants = [ "proc-sys-fs-binfmt_misc.mount" ];
 
     # Don't bother with certain units in containers.
