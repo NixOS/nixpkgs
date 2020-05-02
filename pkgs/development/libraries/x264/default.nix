d26cb1f116cd89f776470459a1f3c395f7c0afb8
Richard Marko <srk@48.io>
2020-05-02 13:34:43 +0200

    x264: unset AS for aarch32 as well

--- pkgs/development/libraries/x264/default.nix
+++ pkgs/development/libraries/x264/default.nix
@@ -21,7 +21,7 @@ stdenv.mkDerivation rec {
 
   outputs = [ "out" "lib" "dev" ];
 
-  preConfigure = lib.optionalString (stdenv.buildPlatform.isx86_64 || stdenv.hostPlatform.isi686) ''
+  preConfigure = lib.optionalString (stdenv.buildPlatform.isx86_64 || stdenv.hostPlatform.isi686 || stdenv.hostPlatform.isAarch32) ''
     # `AS' is set to the binutils assembler, but we need nasm
     unset AS
   '' + lib.optionalString (stdenv.hostPlatform.isAarch64 || stdenv.hostPlatform.isAarch32) ''
