84bed67dd5388bbf1e05d298c1621bba57ca5748
Michael Lingelbach <m.j.lbach@gmail.com>
2020-05-01 18:50:38 -0700

    Add dependencies

--- pkgs/development/python-modules/ray/default.nix
+++ pkgs/development/python-modules/ray/default.nix
@@ -10,36 +10,24 @@
 }:
 
 
-buildPythonPackage {
+buildPythonPackage rec {
   pname = "ray";
   version = "0.8.4";
+  format = "wheel";
 
   src = fetchPypi {
-    inherit pname version;
+    inherit pname version wheel;
     sha256 = "1pzgj85c6g8vr3dq215cd1y2pn8pxc6wa7mjd9m0zrglr1qwwhdz";
+    python = "cp37";
+    platform = "manylinux1_x86_64";
+
   };
 
-  postFixup =
-      lib.optionalString stdenv.isLinux ''
-        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" ./_build/pip_packages/lib/python3.7/site-packages/ray/core/src/ray/thirdparty/redis/src/redis-server
-        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/${python.sitePackages}/ray/core/src/ray/gcs/gcs_server
-        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/${python.sitePackages}/ray/core/src/ray/raylet/raylet
-        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/${python.sitePackages}/ray/core/src/ray/raylet/raylet_monitor
-        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/${python.sitePackages}/ray/core/src/plasma/plasma_store_server
-      '';
-
-
-  pythonImportsCheck = [
-    "ray"
-  ];
-
   meta = with stdenv.lib; {
-    description = "A system for parallel and distributed Python that unifies "
-      "the ML ecosystem.";
+    description = "A system for parallel and distributed Python that unifies the ML ecosystem.";
     homepage = https://ray.io;
     license = licenses.asl20;
     maintainers = with maintainers; [ mjlbach ];
     platforms = [ "x86_64-linux" ];
-    broken = stdenv.isDarwin;
   };
 }
