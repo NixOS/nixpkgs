3188961af1fdfda368bff36691bc0ae1c1610a99
Gabriel Ebner <gebner@gebner.org>
2020-05-03 11:48:20 +0200

    Update pkgs/top-level/all-packages.nix

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21060,7 +21060,7 @@ in
 
   octoprint = callPackage ../applications/misc/octoprint { };
 
-  octoprint-plugins = throw ''octoprint-plugins are now part of the octoprint package set.'';
+  octoprint-plugins = throw ''octoprint-plugins are now part of the octoprint.python.pkgs package set.'';
 
   ocrad = callPackage ../applications/graphics/ocrad { };
 
