38aadfbbad493465323cf9295b87dc6084c973ad
Matt Huszagh <huszaghmatt@gmail.com>
2020-05-04 10:40:35 -0700

    vhd2vl: init at unstable-2018-09-01

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7242,6 +7242,8 @@ in
 
   verilog = callPackage ../applications/science/electronics/verilog {};
 
+  vhd2vl = callPackage ../applications/science/electronics/vhd2vl { };
+
   video2midi = callPackage ../tools/audio/video2midi {
     pythonPackages = python3Packages;
   };
