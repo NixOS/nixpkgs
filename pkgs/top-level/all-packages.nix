NixOS/nixpkgs

commit 4458697264cf0a56e63ed48c9d00dd982545b068

boost17x: boost171 -> boost172
Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

pkgs/top-level/all-packages.nix
@@ -11177,7 +11177,7 @@ in
   boost170 = callPackage ../development/libraries/boost/1.70.nix { };
   boost171 = callPackage ../development/libraries/boost/1.71.nix { };
   boost172 = callPackage ../development/libraries/boost/1.72.nix { };
-  boost17x = boost171;
+  boost17x = boost172;
   boost = boost16x;
 
   boost_process = callPackage ../development/libraries/boost-process { };

commit cadabbcf1447b539082bc7e8fae487011053c3b6

rippled: fix build after boost 1.72 upgrade
This was broken in #84182; pinning back to 1.71 fixes the build.

pkgs/top-level/all-packages.nix
@@ -16030,8 +16030,9 @@ in
     libtool = darwin.cctools;
   };
 
+  # Fails to compile with boost >= 1.72
   rippled = callPackage ../servers/rippled {
-    boost = boost17x;
+    boost = boost171;
   };
 
   rippled-validator-keys-tool = callPackage ../servers/rippled/validator-keys-tool.nix {