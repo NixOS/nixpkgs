5e3857f902dc20585ad8dd3c81e552fe882310c9
R. RyanTM <ryantm-bot@ryantm.com>
2020-05-04 13:14:04 +0000

    abcmidi: 2020.03.25 -> 2020.05.01

--- pkgs/tools/audio/abcmidi/default.nix
+++ pkgs/tools/audio/abcmidi/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "abcMIDI";
-  version = "2020.03.25";
+  version = "2020.05.01";
 
   src = fetchzip {
     url = "https://ifdo.ca/~seymour/runabc/${pname}-${version}.zip";
-    sha256 = "0rzif8idsja8ryhx0y4zdk8lhn36h10ahfjqa8bmif0rdbyab0kv";
+    sha256 = "0rv8hii3a15x2fz5py03hj4ixb4b8qkhp9hdn8amprjnhizk0dj8";
   };
 
   # There is also a file called "makefile" which seems to be preferred by the standard build phase
