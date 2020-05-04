c683beeaa19a9ae4b6176881cbb228b19b14ec0e
R. RyanTM <ryantm-bot@ryantm.com>
2020-05-04 03:48:21 +0000

    particl-core: 0.17.1.2 -> 0.19.1.1

--- pkgs/applications/blockchains/particl/particl-core.nix
+++ pkgs/applications/blockchains/particl/particl-core.nix
@@ -17,11 +17,11 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   pname = "particl-core";
-  version = "0.17.1.2";
+  version = "0.19.1.1";
 
   src = fetchurl {
     url = "https://github.com/particl/particl-core/archive/v${version}.tar.gz";
-    sha256 = "16hcyxwp6yrypwvxz6i2987z3jmpk47xcgnsgh9klih8baqg64p5";
+    sha256 = "11y5q2srkh6r2samppjb5mg6hl79y16j2lj1r23p0968vb9c45kl";
   };
 
   nativeBuildInputs = [ pkgconfig autoreconfHook ];
