ae19e155a5bfae537d8c1e4ca4bd4de82ae3c42a
Roman Volosatovs <rvolosatovs@riseup.net>
2020-05-05 20:28:40 +0200

    gopass: Add myself to maintainers

--- pkgs/tools/security/gopass/default.nix
+++ pkgs/tools/security/gopass/default.nix
@@ -48,7 +48,7 @@ buildGoModule rec {
     description     = "The slightly more awesome Standard Unix Password Manager for Teams. Written in Go.";
     homepage        = "https://www.gopass.pw/";
     license         = licenses.mit;
-    maintainers     = with maintainers; [ andir ];
+    maintainers     = with maintainers; [ andir rvolosatovs ];
     platforms       = platforms.unix;
 
     longDescription = ''
