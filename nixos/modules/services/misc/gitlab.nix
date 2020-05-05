47c8e52a220e8a84f67042e58e0578e0b6548a91
Milan Pässler <mil@nyantec.com>
2020-05-05 15:29:33 +0200

    nixos/gitlab: use new structure.sql
    According to https://gitlab.com/gitlab-org/gitlab/-/issues/211487

--- nixos/modules/services/misc/gitlab.nix
+++ nixos/modules/services/misc/gitlab.nix
@@ -129,7 +129,7 @@ let
     HOME = "${cfg.statePath}/home";
     UNICORN_PATH = "${cfg.statePath}/";
     GITLAB_PATH = "${cfg.packages.gitlab}/share/gitlab/";
-    SCHEMA = "${cfg.statePath}/db/schema.rb";
+    SCHEMA = "${cfg.statePath}/db/structure.sql";
     GITLAB_UPLOADS_PATH = "${cfg.statePath}/uploads";
     GITLAB_LOG_PATH = "${cfg.statePath}/log";
     GITLAB_REDIS_CONFIG_FILE = pkgs.writeText "redis.yml" (builtins.toJSON redisConfig);
