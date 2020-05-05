b9787479e2972110bce73f99a1a620f710ea4476
Maximilian Bosch <maximilian@mbosch.me>
2020-05-05 14:27:31 +0200

    riot-web: 1.5.15 -> 1.6.0
    https://github.com/vector-im/riot-web/releases/tag/v1.6.0

--- pkgs/applications/networking/instant-messengers/riot/riot-web.nix
+++ pkgs/applications/networking/instant-messengers/riot/riot-web.nix
@@ -12,11 +12,11 @@ let
 
 in stdenv.mkDerivation rec {
   pname = "riot-web";
-  version = "1.5.15";
+  version = "1.6.0";
 
   src = fetchurl {
     url = "https://github.com/vector-im/riot-web/releases/download/v${version}/riot-v${version}.tar.gz";
-    sha256 = "09a9l1l65m2c4ldycjhnqn0mmblm3j65vc9rcjfkdxqbd6bh86h7";
+    sha256 = "1mm4xk69ya1k3gz6jjhc4njx7b3rp157jmrqsxr5i382zs035ff7";
   };
 
   installPhase = ''
