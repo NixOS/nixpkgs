6c03b055ec503a1af75427ed75265ef9c1c15403
Roman Firment <romanfirment@gmail.com>
2020-05-05 13:08:37 +0200

    gnomeExtensions.window-is-ready-remover unstable-2020-03-25 -> 1.02
    Also add license.

--- pkgs/desktops/gnome-3/extensions/window-is-ready-remover/default.nix
+++ pkgs/desktops/gnome-3/extensions/window-is-ready-remover/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-shell-extension-window-is-ready-remover";
-  version = "unstable-2020-03-25";
+  version = "1.02";
 
   src = fetchFromGitHub {
     owner = "nunofarruca";
     repo = "WindowIsReady_Remover";
-    rev = "a9f9b3a060a6ba8eec71332f39dc2569b6e93761";
-    sha256 = "0l6cg9kz2plbvsqhgwfajknzw9yv3mg9gxdbsk147gbh2arnp6v3";
+    rev = "v${version}";
+    sha256 = "1xaf95gn0if44avvkjxyf8fl29y28idn9shnrks0m9k67jcwv8ns";
   };
 
   uuid = "windowIsReady_Remover@nunofarruca@gmail.com";
@@ -21,6 +21,6 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     description = "GNOME Shell extension removing window is ready notification";
     homepage = "https://github.com/nunofarruca/WindowIsReady_Remover";
-    license = licenses.unfree;
+    license = licenses.asl20;
   };
 }
