3b5a1e459168572f304ac236940365cc63e0588a
0x4A6F <0x4A6F@users.noreply.github.com>
2020-05-05 17:52:52 +0200

    xandikos: 0.1.0 -> 0.2.1

--- pkgs/servers/xandikos/default.nix
+++ pkgs/servers/xandikos/default.nix
@@ -5,20 +5,23 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "xandikos";
-  version = "0.1.0";
+  version = "0.2.1";
 
   src = fetchFromGitHub {
     owner = "jelmer";
     repo = "xandikos";
     rev = "v${version}";
-    sha256 = "12r8fciid2qpqf054584ywwh49yddyhhpkpcm6jihzyr5y2r4kn1";
+    sha256 = "0bcihkfi75wg0s2an2hysrcrg6pbqqclia53l0vhkzg9b5b8cga1";
   };
 
   propagatedBuildInputs = with python3Packages; [
+    aiohttp
     dulwich
     defusedxml
     icalendar
     jinja2
+    multidict
+    prometheus_client
   ];
 
   meta = with stdenv.lib; {
