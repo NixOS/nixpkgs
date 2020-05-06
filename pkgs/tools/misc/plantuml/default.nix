b18bc8c150a74f813d7542e4a2d435e0e33096af
R. RyanTM <ryantm-bot@ryantm.com>
2020-05-06 10:54:01 +0000

    plantuml: 1.2020.5 -> 1.2020.8

--- pkgs/tools/misc/plantuml/default.nix
+++ pkgs/tools/misc/plantuml/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, makeWrapper, jre, graphviz }:
 
 stdenv.mkDerivation rec {
-  version = "1.2020.5";
+  version = "1.2020.8";
   pname = "plantuml";
 
   src = fetchurl {
     url = "mirror://sourceforge/project/plantuml/${version}/plantuml.${version}.jar";
-    sha256 = "1cn1bjah9qvhh3hixa92v804crkgz69mg30k8229a551x3alykhd";
+    sha256 = "0xkv8d31dc0dchr40zzgmjw2wyh4i5vxwdk3fhqpw0pk2frxwc1w";
   };
 
   nativeBuildInputs = [ makeWrapper ];
