216ae12ceda0fc5848498e888067b19ef18207b7
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2020-05-06 12:49:12 +0900

    themix-theme-oomox: init at 1.12

--- pkgs/applications/misc/themix/default.nix
+++ pkgs/applications/misc/themix/default.nix
@@ -5,10 +5,12 @@ rec {
     unwrapped = callPackage ./gui/unwrapped.nix {};
     plugins = with themixPlugins; [
       import-images
+      theme-oomox
     ];
   };
 
   themixPlugins = {
     import-images = callPackage ./import-images {};
+    theme-oomox = callPackage ./theme-oomox {};
   };
 }
---
+++ pkgs/applications/misc/themix/theme-oomox/default.nix
@@ -0,0 +1,59 @@
+{ stdenv, fetchFromGitHub, python3, bc, sassc, glib, libxml2, gdk-pixbuf
+, gtk-engine-murrine
+}:
+
+stdenv.mkDerivation rec {
+  pname = "themix-theme-oomox";
+  version = "1.12";
+
+  src = fetchFromGitHub {
+    owner = "themix-project";
+    repo = "oomox-gtk-theme";
+    rev = version;
+    sha256 = "015yj9hl283dsgphkva441r1fr580wiyssm4s2x4xfjprqksxg8w";
+  };
+
+  patches = [
+    # themix-gui generates customized theme by `cp -r` theme skeleton to
+    # working directory and modifying it. As the skeleton is in nix store and
+    # not writable, the copied one is not modifiable.
+    ./writable.patch
+  ];
+
+  postPatch = ''
+    patchShebangs .
+
+    for bin in packaging/bin/*; do
+        sed -i "$bin" -e "s@\(/opt/oomox/\)@$out\1@"
+    done
+  '';
+
+  nativeBuildInputs = [ python3 ];
+
+  propagatedBuildInputs = [ bc sassc glib libxml2 gdk-pixbuf ];
+
+  propagatedUserEnvPkgs = [ gtk-engine-murrine ];
+
+  buildPhase = ''
+    runHook preBuild
+    python -O -m compileall .
+    runHook postBuild
+  '';
+
+  # No tests
+  doCheck = false; 
+
+  installFlags = [ "-f Makefile_oomox_plugin" "DESTDIR=$(out)" "PREFIX=" ];
+
+  postInstall = ''
+    cp -r __pycache__ "$out/opt/oomox/plugins/theme_oomox"/
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Oomox theme plugin for Themix GUI designer";
+    inherit (src.meta) homepage;
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ mnacamura ];
+    platforms = platforms.linux;
+  };
+}
