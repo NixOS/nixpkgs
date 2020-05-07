fc9ca5294431a243bbdf7316f0651fe39c800ac6
Martin Baillie <martin@baillie.email>
2020-05-07 16:13:47 +1000

    ssm-session-manager-plugin: init at 1.1.61.0
    Signed-off-by: Martin Baillie <martin@baillie.email>
    (cherry picked from commit 9566c742e27abd4ad61a4eeb32d14ba356ab3832)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9484,6 +9484,7 @@ in
   spidermonkey = spidermonkey_38;
 
   ssm-agent = callPackage ../applications/networking/cluster/ssm-agent { };
+  ssm-session-manager-plugin = callPackage ../applications/networking/cluster/ssm-session-manager-plugin { };
 
   supercollider = libsForQt5.callPackage ../development/interpreters/supercollider {
     fftw = fftwSinglePrec;
