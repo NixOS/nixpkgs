c54d37598a27941621228c7b4bb1eebb2dfaafdd
D. Guthrie <dguthrie@posteo.net>
2020-05-07 17:37:53 +0100

    Set upstream to https://github.com/ibara/mg
    As per reasonable suggestion, instead of extensively patching the
    hboetes version of mg to work on MacOS using bits from ibara's mg,
    just set upstream to ibara's mg.
    
    This also removes the dependency on libbsd, although I'm not sure
    if this is a good or bad thing.

--- pkgs/applications/editors/mg/default.nix
+++ pkgs/applications/editors/mg/default.nix
@@ -1,14 +1,14 @@
-{ stdenv, fetchFromGitHub, pkgconfig, ncurses, buildPackages, libbsd }:
+{ stdenv, fetchFromGitHub, pkgconfig, ncurses, buildPackages }:
 
 stdenv.mkDerivation rec {
   pname = "mg";
-  version = "20200215";
+  version = "6.7";
 
   src = fetchFromGitHub {
-    owner = "hboetes";
+    owner = "ibara";
     repo = "mg";
-    rev = "20200215";
-    sha256 = "1rss7d43hbq43n63gxfvx4b2vh2km58cchwzdf2ssqhaz3qj40m6";
+    rev = "mg-6.7";
+    sha256 = "15adwibq6xrfxbrxzk765g9250iyfn4wbcxd7kcsabiwn6apm0ai";
   };
 
   enableParallelBuilding = true;
@@ -21,13 +21,11 @@ stdenv.mkDerivation rec {
   '';
   nativeBuildInputs = [ pkgconfig ];
 
-  patches = ./darwin_no_libbsd.patch;
-
-  buildInputs = [ ncurses ] ++ stdenv.lib.optional (!stdenv.isDarwin) libbsd;
+  buildInputs = [ ncurses ];
 
   meta = with stdenv.lib; {
     description = "Micro GNU/emacs, a portable version of the mg maintained by the OpenBSD team";
-    homepage = "https://homepage.boetes.org/software/mg";
+    homepage = "https://man.openbsd.org/OpenBSD-current/man1/mg.1";
     license = licenses.publicDomain;
     platforms = platforms.all;
   };
