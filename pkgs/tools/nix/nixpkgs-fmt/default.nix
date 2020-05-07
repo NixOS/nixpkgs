6fc0c1dee13a49c45e6f3d5d4ca1553cbce79ba4
Andika Demas Riyandi <andika.riyan@gmail.com>
2020-05-07 16:28:04 +0700

    nixpkgs-fmt: 0.8.0 -> 0.9.0 (#87175)

--- pkgs/tools/nix/nixpkgs-fmt/default.nix
+++ pkgs/tools/nix/nixpkgs-fmt/default.nix
@@ -1,25 +1,16 @@
 { lib, rustPlatform, fetchFromGitHub, fetchpatch }:
 rustPlatform.buildRustPackage rec {
   pname = "nixpkgs-fmt";
-  version = "0.8.0";
+  version = "0.9.0";
 
   src = fetchFromGitHub {
     owner = "nix-community";
     repo = pname;
     rev = "v${version}";
-    sha256 = "09lhi8aidw9qf94n07mgs2nfac32a96wkx50glj35dhn06iwzwqr";
+    sha256 = "1kkw87c63nx5pqsxcwn6iw27k02j9ls21zyhb5dvf0zaqd9sz7ad";
   };
-
-  patches = [
-    # Fixes debug output on stdout mangling some files. Fixed in next release.
-    # https://github.com/nix-community/nixpkgs-fmt/issues/201
-    (fetchpatch {
-      url = "https://github.com/nix-community/nixpkgs-fmt/commit/648f46e1d5507592b246311618f467da282bf1b5.patch";
-      sha256 = "18n4criyl0lydxmjqfkcmwx0pniyqzfgfxcjwz6czqwmx5y7b4rb";
-    })
-  ];
-
-  cargoSha256 = "15m40d9354412h51zn806pxsqjai48xiw8chf8slbi0cjxd268j9";
+  
+  cargoSha256 = "1wybvm9qckx9cd656gx9zrbszmaj66ihh2kk6qqdb6maixcq5k0x";
 
   meta = with lib; {
     description = "Nix code formatter for nixpkgs";
