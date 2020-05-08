7edd5a82e8d20c281fe46a784f0809de0686f7e1
Oleksii Filonenko <brightone@protonmail.com>
2020-05-08 07:50:24 +0000

    gleam: 0.8.0-rc1 -> 0.8.0

--- pkgs/development/compilers/gleam/default.nix
+++ pkgs/development/compilers/gleam/default.nix
@@ -2,18 +2,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "gleam";
-  version = "0.8.0-rc1";
+  version = "0.8.0";
 
   src = fetchFromGitHub {
     owner = "gleam-lang";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1asn9k4s9m9fr4cx8f1z2npkby7vjdsypn4q6xfy3bk2pgn306bl";
+    sha256 = "05n8a7b63i9946fvbxphfh8n6ib20qnbv44ghlzcjwca9qvlx7i7";
   };
 
   buildInputs = stdenv.lib.optionals stdenv.isDarwin [ Security ];
 
-  cargoSha256 = "1jxbq32s68zvanmwvq5wm46bkigziqgl920cf2xg5hcv4l25f03n";
+  cargoSha256 = "1ga5dinkmibkg53bfr6vc0i4bbgyn2y3d5qjf5bd965c1v7ba1jl";
 
   meta = with stdenv.lib; {
     description = "A statically typed language for the Erlang VM";
