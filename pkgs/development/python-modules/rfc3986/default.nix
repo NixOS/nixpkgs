0e06e849dd735a09a09f4250bb595f386e475bb7
R. RyanTM <ryantm-bot@ryantm.com>
2020-05-09 21:03:02 +0000

    python27Packages.rfc3986: 1.3.2 -> 1.4.0

--- pkgs/development/python-modules/rfc3986/default.nix
+++ pkgs/development/python-modules/rfc3986/default.nix
@@ -3,11 +3,11 @@
 
 buildPythonPackage rec {
   pname = "rfc3986";
-  version = "1.3.2";
+  version = "1.4.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0344d0bd428126ce554e7ca2b61787b6a28d2bbd19fc70ed2dd85efe31176405";
+    sha256 = "17dvx15m3r49bmif5zlli8kzjd6bys6psixzbp14sd5367d9h8qi";
   };
 
   checkInputs = [ pytest ];
