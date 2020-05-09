bbe90801d42a0031a493052aa087d9f877def5f3
Frederik Rietdijk <fridh@fridh.nl>
2020-05-09 12:01:49 +0200

    python3Packages.google-cloud-firestore: 1.6.0 -> 1.6.2

--- pkgs/development/python-modules/google_cloud_firestore/default.nix
+++ pkgs/development/python-modules/google_cloud_firestore/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-firestore";
-  version = "1.6.0";
+  version = "1.6.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "7fec7b523ab5e1f87721ca61181114818579bb4d17de768a3993811c9d2aacfe";
+    sha256 = "5ad4835c3a0f6350bcbbc42fd70e90f7568fca289fdb5e851888df394c4ebf80";
   };
 
   checkInputs = [ pytest ];
