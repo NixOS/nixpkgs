2a8fb4c39c4f0fe6eec9dde167e1a6eb16da3af9
R. RyanTM <ryantm-bot@ryantm.com>
2020-05-10 19:58:03 +0000

    ttygif: 1.4.0 -> 1.5.0

--- pkgs/tools/misc/ttygif/default.nix
+++ pkgs/tools/misc/ttygif/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "ttygif";
-  version = "1.4.0";
+  version = "1.5.0";
 
   src = fetchFromGitHub {
     owner = "icholy";
     repo = pname;
     rev = version;
-    sha256 = "18l26iacpfn4xqqv1ai6ncabn83mqv98c48gl265gfld66y7zbzn";
+    sha256 = "1w9c3h6hik2gglwsw8ww63piy66i4zqr3273wh5rc9r2awiwh643";
   };
 
   makeFlags = [ "PREFIX=${placeholder "out"}" ];
