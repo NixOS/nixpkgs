151b2bc40751253e6b025fb71b6e218c7aaffd5f
Jonathan Ringer <jonringer117@gmail.com>
2020-05-10 11:36:58 -0700

    python3Packages.arrow: fix dependencies

--- pkgs/development/python-modules/arrow/default.nix
+++ pkgs/development/python-modules/arrow/default.nix
@@ -1,6 +1,6 @@
-{ stdenv, buildPythonPackage, fetchPypi
+{ stdenv, lib, buildPythonPackage, fetchPypi, isPy27
 , nose, chai, simplejson, backports_functools_lru_cache
-, dateutil, pytz, mock, dateparser
+, python-dateutil, pytz, pytest-mock, sphinx, dateparser, pytestcov, pytest
 }:
 
 buildPythonPackage rec {
@@ -12,19 +12,26 @@ buildPythonPackage rec {
     sha256 = "eb5d339f00072cc297d7de252a2e75f272085d1231a3723f1026d1fa91367118";
   };
 
+  propagatedBuildInputs = [ python-dateutil ]
+    ++ lib.optionals isPy27 [ backports_functools_lru_cache ];
+
+  checkInputs = [
+    dateparser
+    pytest
+    pytestcov
+    pytest-mock
+    pytz
+    simplejson
+    sphinx
+  ];
+
   checkPhase = ''
-    nosetests --cover-package=arrow
+    pytest
   '';
 
-  checkInputs = [ nose chai simplejson pytz ];
-  propagatedBuildInputs = [ dateutil backports_functools_lru_cache mock dateparser];
-
-  postPatch = ''
-    substituteInPlace setup.py --replace "==1.2.1" ""
-  '';
-
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Python library for date manipulation";
+    homepage = "https://github.com/crsmithdev/arrow";
     license = licenses.asl20;
     maintainers = with maintainers; [ thoughtpolice ];
   };
