9e7fbc6f2c302f36ed43a43776da2a00179471e4
zowoq <59103226+zowoq@users.noreply.github.com>
2020-05-12 03:58:44 +1000

    nixos/cri-o, cri-o: add wrapper

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23438,7 +23438,8 @@ in
 
   crispyDoom = callPackage ../games/crispy-doom { };
 
-  cri-o = callPackage ../applications/virtualization/cri-o { };
+  cri-o = callPackage ../applications/virtualization/cri-o/wrapper.nix { };
+  cri-o-unwrapped = callPackage ../applications/virtualization/cri-o { };
 
   ckan = callPackage ../games/ckan { };
 
