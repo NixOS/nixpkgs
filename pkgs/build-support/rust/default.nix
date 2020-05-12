6b23cfe6894ab86ddf9b62944f39d616f993ff55
Maximilian Bosch <maximilian@mbosch.me>
2020-05-13 01:28:24 +0200

    rustPlatform: add `buildAndTestSubdir`-argument
    There are several tarballs (such as the `rust-lang/rust`-source) with a
    `Cargo.toml` at root and several sub-packages (with their own Cargo.toml)
    without using workspaces[1].
    
    In such a case it's needed to move into a subdir to only build the
    specified sub-package (e.g. `rustfmt` or `rsl`), however the artifacts
    are at `/target` in the root-dir of the build environment. This breaks
    the build since `buildRustPackage` searches for executables in `target`
    (which is at the build-env's root) at the end of the `buildPhase`.
    
    With the optional `buildAndTestSubdir`-argument, the builder moves into
    the specified subdir using `pushd`/`popd` during `buildPhase` and
    `checkPhase`.
    
    Also moved the logic to find executables and libs to the end of the `buildPhase`
    from a custom `postBuild`-hook to fix packages with custom `build`/`install`-procedures
    such as `uutils-coreutils`.
    
    [1] https://doc.rust-lang.org/book/ch14-03-cargo-workspaces.html

--- pkgs/build-support/rust/default.nix
+++ pkgs/build-support/rust/default.nix
@@ -29,6 +29,12 @@
 , target ? null
 , cargoVendorDir ? null
 , checkType ? buildType
+
+# Needed to `pushd`/`popd` into a subdir of a tarball if this subdir
+# contains a Cargo.toml, but isn't part of a workspace (which is e.g. the
+# case for `rustfmt`/etc from the `rust-sources).
+# Otherwise, everything from the tarball would've been built/tested.
+, buildAndTestSubdir ? null
 , ... } @ args:
 
 assert cargoVendorDir == null -> cargoSha256 != "unset";
@@ -162,6 +168,7 @@ stdenv.mkDerivation (args // {
   '';
 
   buildPhase = with builtins; args.buildPhase or ''
+    ${stdenv.lib.optionalString (buildAndTestSubdir != null) "pushd ${buildAndTestSubdir}"}
     runHook preBuild
 
     (
@@ -178,9 +185,8 @@ stdenv.mkDerivation (args // {
     )
 
     runHook postBuild
-  '';
 
-  postBuild = args.postBuild or "" + ''
+    ${stdenv.lib.optionalString (buildAndTestSubdir != null) "popd"}
 
     # This needs to be done after postBuild: packages like `cargo` do a pushd/popd in
     # the pre/postBuild-hooks that need to be taken into account before gathering
@@ -194,10 +200,12 @@ stdenv.mkDerivation (args // {
   checkPhase = args.checkPhase or (let
     argstr = "${stdenv.lib.optionalString (checkType == "release") "--release"} --target ${rustTarget} --frozen";
   in ''
+    ${stdenv.lib.optionalString (buildAndTestSubdir != null) "pushd ${buildAndTestSubdir}"}
     runHook preCheck
     echo "Running cargo test ${argstr} -- ''${checkFlags} ''${checkFlagsArray+''${checkFlagsArray[@]}}"
     cargo test ${argstr} -- ''${checkFlags} ''${checkFlagsArray+"''${checkFlagsArray[@]}"}
     runHook postCheck
+    ${stdenv.lib.optionalString (buildAndTestSubdir != null) "popd"}
   '');
 
   doCheck = args.doCheck or true;
