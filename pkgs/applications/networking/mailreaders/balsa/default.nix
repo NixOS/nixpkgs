717656fd03ef561a9d6384c3702a16e4707547c1
José Romildo Malaquias <malaquias@gmail.com>
2020-05-13 18:13:16 -0300

    balsa: format with nixpkgs-fmt

--- pkgs/applications/networking/mailreaders/balsa/default.nix
+++ pkgs/applications/networking/mailreaders/balsa/default.nix
@@ -1,7 +1,22 @@
-{ stdenv, fetchurl, pkgconfig, intltool, glib, gtk3, gmime, gnutls,
-  webkitgtk, libesmtp, openssl, libnotify, gtkspell3, gpgme,
-  libcanberra-gtk3, libsecret, gtksourceview, gobject-introspection,
-  wrapGAppsHook
+{ stdenv
+, fetchurl
+, pkgconfig
+, intltool
+, glib
+, gtk3
+, gmime
+, gnutls
+, webkitgtk
+, libesmtp
+, openssl
+, libnotify
+, gtkspell3
+, gpgme
+, libcanberra-gtk3
+, libsecret
+, gtksourceview
+, gobject-introspection
+, wrapGAppsHook
 }:
 
 stdenv.mkDerivation rec {
