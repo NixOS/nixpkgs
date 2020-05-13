38e88e7f696728af050b1cd50c85f97bc61ad631
Aaron Andersen <aaron@fosslib.net>
2020-05-12 21:11:17 -0400

    zabbix: update source url
    (cherry picked from commit c85f02ca4ea64f5147692b91f1dc2711715ecf95)

--- pkgs/servers/monitoring/zabbix/agent.nix
+++ pkgs/servers/monitoring/zabbix/agent.nix
@@ -6,7 +6,7 @@ import ./versions.nix ({ version, sha256 }:
     inherit version;
 
     src = fetchurl {
-      url = "mirror://sourceforge/zabbix/ZABBIX%20Latest%20Stable/${version}/zabbix-${version}.tar.gz";
+      url = "https://cdn.zabbix.com/zabbix/sources/stable/${stdenv.lib.versions.majorMinor version}/zabbix-${version}.tar.gz";
       inherit sha256;
     };
 
