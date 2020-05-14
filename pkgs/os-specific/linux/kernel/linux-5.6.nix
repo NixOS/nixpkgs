1dff0476d9757e89bba5a50a8d8f154a94fd089b
Tim Steinbach <tim@nequissimus.com>
2020-05-14 09:19:09 -0400

    linux: 5.6.12 -> 5.6.13
    (cherry picked from commit 7ef8639163a6e27ed3c218c2a5db6da75dba88a6)

--- pkgs/os-specific/linux/kernel/linux-5.6.nix
+++ pkgs/os-specific/linux/kernel/linux-5.6.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.6.12";
+  version = "5.6.13";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0892ar2irfhd612sb8jpx85w0wwh4n76jgsv8wb92fp6mim37sns";
+    sha256 = "11zriz0jwqizv0pq0laql0svsnspdfnxqykq70v22x39iyfdf9gi";
   };
 } // (args.argsOverride or {}))
