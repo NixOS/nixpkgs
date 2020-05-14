5150040540cfed604d063b7c3419d9835e17ebc1
Frederik Rietdijk <fridh@fridh.nl>
2020-05-14 13:35:11 +0200

    python38: 3.8.2 -> 3.8.3
    (cherry picked from commit 0367fa630d59673624d6742e2758b87eb12fff4c)

--- pkgs/development/interpreters/python/default.nix
+++ pkgs/development/interpreters/python/default.nix
@@ -105,10 +105,10 @@ in {
     sourceVersion = {
       major = "3";
       minor = "8";
-      patch = "2";
+      patch = "3";
       suffix = "";
     };
-    sha256 = "1ps5v323cp5czfshqjmbsqw7nvrdpcbk06f62jbzaqik4gfffii6";
+    sha256 = "0r2qg4pdvv52ld5dd95fl6lzzsxxxhbsxmymwcphh6624g3mxayz";
     inherit (darwin) configd;
     inherit passthruFun;
   };
