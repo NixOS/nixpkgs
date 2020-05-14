8d41f9f281a8317faa45c4270e8d48bf657cc1a0
zowoq <59103226+zowoq@users.noreply.github.com>
2020-05-15 09:54:39 +1000

    go: 1.14.2 -> 1.14.3
    https://golang.org/doc/devel/release.html#go1.14.minor

--- pkgs/development/compilers/go/1.14.nix
+++ pkgs/development/compilers/go/1.14.nix
@@ -30,11 +30,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "go";
-  version = "1.14.2";
+  version = "1.14.3";
 
   src = fetchurl {
     url = "https://dl.google.com/go/go${version}.src.tar.gz";
-    sha256 = "0z3zxsnhmsxplnwfw1l9gr6jgglwp50sr3p5njknv9i6jzk89plq";
+    sha256 = "0mmgf74snprdiajgh99jjliwjl5im71qcgm5qrxpnyfisiw3f0lk";
   };
 
   # perl is used for testing go vet
