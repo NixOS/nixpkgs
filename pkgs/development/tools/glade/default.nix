fe7e564467acbc1e8b3009fab41c62c4de9f8a04
Jan Tojnar <jtojnar@gmail.com>
2020-05-15 17:30:40 +0200

    glade: format with nixpkgs-fmt
    also switch away from dependency aliases and reorder the attributes a bit

--- pkgs/development/tools/glade/default.nix
+++ pkgs/development/tools/glade/default.nix
@@ -1,7 +1,20 @@
-{ stdenv, intltool, fetchurl, python3
-, pkgconfig, gtk3, glib, gobject-introspection
-, wrapGAppsHook, itstool, libxml2, docbook_xsl
-, gnome3, gdk-pixbuf, libxslt, gsettings-desktop-schemas }:
+{ stdenv
+, intltool
+, fetchurl
+, python3
+, pkg-config
+, gtk3
+, glib
+, gobject-introspection
+, wrapGAppsHook
+, itstool
+, libxml2
+, docbook-xsl-nons
+, gnome3
+, gdk-pixbuf
+, libxslt
+, gsettings-desktop-schemas
+}:
 
 stdenv.mkDerivation rec {
   pname = "glade";
@@ -12,20 +25,35 @@ stdenv.mkDerivation rec {
     sha256 = "08bayb1rrpblxf6jhhbw2n3c425w170is4l94pampldl4kmsdvzd";
   };
 
-  passthru = {
-    updateScript = gnome3.updateScript { packageName = "glade"; };
-  };
-
   nativeBuildInputs = [
-    pkgconfig intltool itstool wrapGAppsHook docbook_xsl libxslt libxml2 gobject-introspection
+    pkg-config
+    intltool
+    itstool
+    wrapGAppsHook
+    docbook-xsl-nons
+    libxslt
+    libxml2
+    gobject-introspection
   ];
+
   buildInputs = [
-    gtk3 glib libxml2 python3 python3.pkgs.pygobject3
+    gtk3
+    glib
+    libxml2
+    python3
+    python3.pkgs.pygobject3
     gsettings-desktop-schemas
-    gdk-pixbuf gnome3.adwaita-icon-theme
+    gdk-pixbuf
+    gnome3.adwaita-icon-theme
   ];
 
   enableParallelBuilding = true;
+
+  passthru = {
+    updateScript = gnome3.updateScript {
+      packageName = pname;
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = "https://wiki.gnome.org/Apps/Glade";
