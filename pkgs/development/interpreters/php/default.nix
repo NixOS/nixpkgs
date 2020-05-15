3c7d09cf45c290c0d045f15538323200b63cd253
Elis Hirwing <elis@hirwing.se>
2020-05-15 22:13:35 +0200

    php72: 7.2.29 -> 7.2.30
    Changelog: https://www.php.net/ChangeLog-7.php#7.2.30

--- pkgs/development/interpreters/php/default.nix
+++ pkgs/development/interpreters/php/default.nix
@@ -256,8 +256,8 @@ let
         };
 
   php72base = callPackage generic (_args // {
-    version = "7.2.29";
-    sha256 = "08xry2fgqgg8s0ym1hh11wkbr36av3zq1bn4krbciw1b7x8gb8ga";
+    version = "7.2.30";
+    sha256 = "1gqjly3nmb5z4pl5gbjm4p9bva535z7k2clk1bpqvzg8xffmrky4";
 
     # https://bugs.php.net/bug.php?id=76826
     extraPatches = lib.optional stdenv.isDarwin ./php72-darwin-isfinite.patch;
