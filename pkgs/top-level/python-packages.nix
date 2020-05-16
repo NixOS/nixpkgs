b19523ba2366d90e2556d22f1145af12f1a90fa5
Renato Florentino Garcia <fgarcia.renato@gmail.com>
2020-05-15 23:40:07 -0300

    pythonPackages.icecream: init at 2.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4213,6 +4213,8 @@ in {
 
   icalendar = callPackage ../development/python-modules/icalendar { };
 
+  icecream = callPackage ../development/python-modules/icecream { };
+
   ics = callPackage ../development/python-modules/ics { };
 
   ifaddr = callPackage ../development/python-modules/ifaddr { };
