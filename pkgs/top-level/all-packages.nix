59e1c160302c940cfbace34e71b373586221b6de
dawidsowa <dawid_sowa@posteo.net>
2020-05-16 23:23:35 +0200

    mpvScripts.autoload: init at 2019-02-15

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20836,6 +20836,7 @@ in
   mpv-with-scripts = callPackage ../applications/video/mpv/wrapper.nix { };
 
   mpvScripts = recurseIntoAttrs {
+    autoload = callPackage ../applications/video/mpv/scripts/autoload.nix {};
     convert = callPackage ../applications/video/mpv/scripts/convert.nix {};
     mpris = callPackage ../applications/video/mpv/scripts/mpris.nix {};
   };
