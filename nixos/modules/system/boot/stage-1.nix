c7efbe10a64ebafeb6e4bb28f1e04633b39fac6d
Puck Meerburg <puck@puck.moe>
2020-05-17 17:23:51 +0000

    nixos/stage-1: Add helper function for password requests
    When using Plymouth, it's possible to have a graphical interface for
    requesting passwords and showing boot info. To accomodate this, we add
    a hook that allows other stage-1 code to seamlessly use this when
    available.

--- nixos/modules/system/boot/stage-1.nix
+++ nixos/modules/system/boot/stage-1.nix
@@ -280,7 +280,7 @@ let
     inherit (config.system.build) earlyMountScript;
 
     inherit (config.boot.initrd) checkJournalingFS
-      preLVMCommands preDeviceCommands postDeviceCommands postMountCommands preFailCommands kernelModules;
+      preLVMCommands preLogCommands preDeviceCommands postDeviceCommands postMountCommands preFailCommands kernelModules;
 
     resumeDevices = map (sd: if sd ? device then sd.device else "/dev/disk/by-label/${sd.label}")
                     (filter (sd: hasPrefix "/dev/" sd.device && !sd.randomEncryption.enable
@@ -435,6 +435,14 @@ in
       type = types.lines;
       description = ''
         Shell commands to be executed immediately before LVM discovery.
+      '';
+    };
+
+    boot.initrd.preLogCommands = mkOption {
+      default = "";
+      type = types.lines;
+      description = ''
+        Shell commands to be executed immediately before setting up logging.
       '';
     };
 
