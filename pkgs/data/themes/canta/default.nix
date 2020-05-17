fb07bcf08b326338faa545a1042e63ffc5434a96
José Romildo Malaquias <malaquias@gmail.com>
2020-05-17 11:08:57 -0300

    canta-theme: install the icon theme

--- pkgs/data/themes/canta/default.nix
+++ pkgs/data/themes/canta/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, gdk-pixbuf, librsvg, gtk-engine-murrine }:
+{ stdenv, fetchFromGitHub, gdk-pixbuf, librsvg, gtk-engine-murrine, gtk3, gnome3, gnome-icon-theme, numix-icon-theme-circle, hicolor-icon-theme }:
 
 stdenv.mkDerivation rec {
   pname = "canta-theme";
@@ -11,16 +11,25 @@ stdenv.mkDerivation rec {
     sha256 = "0b9ffkw611xxb2wh43sjqla195jp0ygxph5a8dvifkxdw6nxc2y0";
   };
 
+  nativeBuildInputs = [ gtk3 ];
+
   buildInputs = [ gdk-pixbuf librsvg ];
 
+  propagatedBuildInputs = [ gnome3.adwaita-icon-theme gnome-icon-theme numix-icon-theme-circle hicolor-icon-theme ];
+
   propagatedUserEnvPkgs = [ gtk-engine-murrine ];
+
+  dontDropIconThemeCache = true;
 
   installPhase = ''
     patchShebangs .
     mkdir -p $out/share/themes
-    name= ./install.sh -d $out/share/themes
-    install -D -t $out/share/backgrounds wallpaper/canta-wallpaper.svg
+    name= ./install.sh --dest $out/share/themes
     rm $out/share/themes/*/{AUTHORS,COPYING}
+    install -D -t $out/share/backgrounds wallpaper/canta-wallpaper.svg
+    mkdir -p $out/share/icons
+    cp -a icons/Canta $out/share/icons
+    gtk-update-icon-cache $out/share/icons/Canta
   '';
 
   meta = with stdenv.lib; {
