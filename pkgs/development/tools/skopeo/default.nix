2ee9aac39bcc99cfeee1b35c25af5c4704ee61c8
zowoq <59103226+zowoq@users.noreply.github.com>
2020-05-19 08:16:45 +1000

    skopeo: 0.2.0 -> 1.0.0
    https://github.com/containers/skopeo/releases/tag/v1.0.0

--- pkgs/development/tools/skopeo/default.nix
+++ pkgs/development/tools/skopeo/default.nix
@@ -15,13 +15,13 @@
 
 buildGoModule rec {
   pname = "skopeo";
-  version = "0.2.0";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     rev = "v${version}";
     owner = "containers";
     repo = "skopeo";
-    sha256 = "09zqzrw6f1s6kaknnj3hra3xz4nq6y86vmw5vk8p4f6g7cwakg1x";
+    sha256 = "1zg0agf8x7fa8zdzfzgncm64j363lmxrqjhdzsx6mlig87k17p05";
   };
 
   outputs = [ "out" "man" ];
