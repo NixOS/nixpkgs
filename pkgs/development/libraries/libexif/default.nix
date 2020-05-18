a5cfb36bce01c6a75aaa005926d59141b045bb16
Martin Milata <martin@martinmilata.cz>
2020-05-19 00:24:55 +0200

    libexif: 0.6.21 -> 0.6.22
    Changes: https://github.com/libexif/libexif/releases/tag/libexif-0_6_22-release
    
    Fixes:
    - https://nvd.nist.gov/vuln/detail/CVE-2020-0093
    - https://nvd.nist.gov/vuln/detail/CVE-2020-12767
    - https://nvd.nist.gov/vuln/detail/CVE-2020-13112
    - https://nvd.nist.gov/vuln/detail/CVE-2020-13113
    - https://nvd.nist.gov/vuln/detail/CVE-2020-13114

--- pkgs/development/libraries/libexif/default.nix
+++ pkgs/development/libraries/libexif/default.nix
@@ -1,36 +1,15 @@
-{ stdenv, fetchurl, fetchpatch, gettext }:
+{ stdenv, fetchurl, gettext }:
 
 stdenv.mkDerivation rec {
-  name = "libexif-0.6.21";
+  pname = "libexif";
+  version = "0.6.22";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/libexif/${name}.tar.bz2";
-    sha256 = "06nlsibr3ylfwp28w8f5466l6drgrnydgxrm4jmxzrmk5svaxk8n";
+  src = let
+    underscoreVersion = builtins.replaceStrings [ "." ] [ "_" ] version;
+  in fetchurl {
+    url = "https://github.com/${pname}/${pname}/releases/download/${pname}-${underscoreVersion}-release/${pname}-${version}.tar.xz";
+    sha256 = "0mhcad5zab7fsn120rd585h8ncwkq904nzzrq8vcd72hzk4g2j2h";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "CVE-2017-7544.patch";
-      url = "https://github.com/libexif/libexif/commit/c39acd1692023b26290778a02a9232c873f9d71a.patch";
-      sha256 = "0xgx6ly2i4q05shb61mfx6njwf1yp347jkznm0ka4m85i41xm6sd";
-    })
-    (fetchpatch {
-      name = "CVE-2018-20030-1.patch";
-      url = "https://github.com/libexif/libexif/commit/5d28011c40ec86cf52cffad541093d37c263898a.patch";
-      sha256 = "1wv8s962wmbn2m2xypgirf12g6msrbplpsmd5bh86irfwhkcppj3";
-    })
-    (fetchpatch {
-      name = "CVE-2018-20030-2.patch";
-      url = "https://github.com/libexif/libexif/commit/6aa11df549114ebda520dde4cdaea2f9357b2c89.patch";
-      sha256 = "01aqvz63glwq6wg0wr7ykqqghb4abgq77ghvhizbzadg1k4h7drx";
-      excludes = [ "NEWS" ];
-    })
-    (fetchpatch {
-      name = "CVE-2019-9278.patch";
-      url = "https://github.com/libexif/libexif/commit/75aa73267fdb1e0ebfbc00369e7312bac43d0566.patch";
-      sha256 = "10ikg33mips5zq9as7l9xqnyzbg1wwr4sw17517nzf4hafjpasrj";
-    })
-  ];
 
   buildInputs = [ gettext ];
 
