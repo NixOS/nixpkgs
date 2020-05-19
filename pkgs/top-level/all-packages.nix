131a28e9f2e5f51e37a60af25379cc236f6b71c7
Elyhaka <57923898+Elyhaka@users.noreply.github.com>
2020-05-19 14:03:31 +0200

    fprintd: 0.9.0 -> 1.90.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3512,9 +3512,6 @@ in
   fprot = callPackage ../tools/security/fprot { };
 
   fprintd = callPackage ../tools/security/fprintd { };
-  fprintd-thinkpad = fprintd.override {
-    thinkpad = true;
-  };
 
   franz = callPackage ../applications/networking/instant-messengers/franz { };
 
