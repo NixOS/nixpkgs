c77c661d6e49985699ddd4ba443309eb4fbdf0ab
R. RyanTM <ryantm-bot@ryantm.com>
2020-05-19 05:48:48 +0000

    fluent-bit: 1.4.3 -> 1.4.4

--- pkgs/tools/misc/fluent-bit/default.nix
+++ pkgs/tools/misc/fluent-bit/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "fluent-bit";
-  version = "1.4.3";
+  version = "1.4.4";
 
   src = fetchFromGitHub {
     owner = "fluent";
     repo = "fluent-bit";
     rev = "v${version}";
-    sha256 = "1x5izsm0b3514i2vl1b3x40chjlzidr5zssn5cpgrw9a8w4jpv6h";
+    sha256 = "1p6ay2frrxi18965r9c83crlljx92zbdvzkwl4bl9qkb0lpml3yl";
   };
 
   nativeBuildInputs = [ cmake flex bison ];
