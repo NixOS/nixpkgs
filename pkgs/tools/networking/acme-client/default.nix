d4c80a478b9b8d3303f39fb788f382dfaa823bdc
Ruud van Asseldonk <dev@veniogames.com>
2020-05-19 23:01:00 +0200

    acme-client: 0.2.5 -> 1.0.0
    The upstream version fails to compile due to a missing limits.h include.
    I added a patch to fix that.
    
    I opened a pull request to upstream it too, but the project has moved
    from GitHub onto sr.ht and now asks me to send a patch to the mailing
    list. My default email client is not really suitable for that, and
    getting git-send-email set up will take some work, so in the meantime
    it is easier to just patch it here.

--- pkgs/tools/networking/acme-client/default.nix
+++ pkgs/tools/networking/acme-client/default.nix
@@ -12,19 +12,21 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   pname = "acme-client";
-  version = "0.2.5";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "graywolf";
     repo = "acme-client-portable";
     rev = "v${version}";
-    sha256 = "1d9yk87nj5gizkq26m4wqfh4xhlrn5xlfj7mfgvrpsdiwibqxrrw";
+    sha256 = "1p6jbxg00ing9v3jnpvq234w5r2gf8b04k9qm06mn336lcd2lgpl";
   };
 
   nativeBuildInputs = [ autoreconfHook bison pkgconfig ];
   buildInputs = [ libbsd libressl ] ++ optional stdenv.isDarwin apple_sdk.sdk;
 
   makeFlags = [ "PREFIX=${placeholder "out"}" ];
+
+  patches = [ ./limits.h.patch ];
 
   meta = {
     homepage = "https://github.com/graywolf/acme-client-portable";
