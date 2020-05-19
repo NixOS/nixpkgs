58ba20dd90fbd48b12f66a4863f11da71870cfa8
R. RyanTM <ryantm-bot@ryantm.com>
2020-05-19 09:15:47 +0000

    kubie: 0.8.4 -> 0.9.1

--- pkgs/development/tools/kubie/default.nix
+++ pkgs/development/tools/kubie/default.nix
@@ -4,16 +4,16 @@ with rustPlatform;
 
 buildRustPackage rec {
   pname = "kubie";
-  version = "0.8.4";
+  version = "0.9.1";
 
   src = fetchFromGitHub {
     rev = "v${version}";
     owner = "sbstp";
     repo = "kubie";
-    sha256 = "1f82xlhhxbjadjw609kr1kdm4n69c9mqjia4b3k505wjh7cc55n0";
+    sha256 = "0q1dxry10iaf7zx6vyr0da4ihqx7l8dlyhlqm8qqfz913h2wam8c";
   };
 
-  cargoSha256 = "0mish7wqwq5ynl98n6swdn5i6mg62aih5rfykbl3wx39b468n481";
+  cargoSha256 = "13zs2xz3s4732zxsimg7b22d9707ln4gpscznxi13cjkf5as9gbz";
 
   installPhase = ''
     mkdir -p $out/share/bash-completion/completions
