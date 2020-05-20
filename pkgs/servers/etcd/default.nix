9d92aa38d4088c992948c331c3795fbb1814ce82
zowoq <59103226+zowoq@users.noreply.github.com>
2020-05-20 13:27:36 +1000

    etcd: add maintainer

--- pkgs/servers/etcd/default.nix
+++ pkgs/servers/etcd/default.nix
@@ -33,7 +33,7 @@ buildGoPackage rec {
     description = "Distributed reliable key-value store for the most critical data of a distributed system";
     license = licenses.asl20;
     homepage = "https://etcd.io/";
-    maintainers = with maintainers; [ offline ];
+    maintainers = with maintainers; [ offline zowoq ];
     platforms = platforms.unix;
   };
 }
