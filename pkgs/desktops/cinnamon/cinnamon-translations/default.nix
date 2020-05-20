fcb65fc1900a0491519209ab60fe45601dc6ecd1
R. RyanTM <ryantm-bot@ryantm.com>
2020-05-20 09:34:25 +0000

    cinnamon.cinnamon-translations: 4.4.2 -> 4.6.0

--- pkgs/desktops/cinnamon/cinnamon-translations/default.nix
+++ pkgs/desktops/cinnamon/cinnamon-translations/default.nix
@@ -5,13 +5,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cinnamon-translations";
-  version = "4.4.2";
+  version = "4.6.0";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    sha256 = "1n1nkapcgxmbv0l8hrx5cf588pi4ifx12xbz46lq4p1ijrlfivba";
+    sha256 = "04rlgcbz02n9kj1w12xa56b7f4x10y6g91hsl70zmag568mdclzz";
   };
 
   nativeBuildInputs = [
