55eaa559223da5b6cfd5bd6cd0e51b667d6865c9
Jörg Thalheim <joerg@thalheim.io>
2020-05-20 16:39:39 +0100

    nixos/redis: set TimeoutStartSec/TimeoutStopSec

--- nixos/modules/services/databases/redis.nix
+++ nixos/modules/services/databases/redis.nix
@@ -242,6 +242,8 @@ in
         ExecStart = "${cfg.package}/bin/redis-server /run/redis/redis.conf";
         RuntimeDirectory = "redis";
         StateDirectory = "redis";
+        TimeoutStartSec = "infinity";
+        TimeoutStopSec = "infinity";
         Type = "notify";
         User = "redis";
         Group = "redis";
