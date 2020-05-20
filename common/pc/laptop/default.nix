90868c67e85fcf2efb3f6075203021f0f9bd97f5
Erik Arvstedt <erik.arvstedt@gmail.com>
2020-05-20 16:29:50 +0200

    pc/laptop: remove redundant cpuFreqGovernor setting
    This setting has been part of NixOS since version 16.09

--- common/pc/laptop/default.nix
+++ common/pc/laptop/default.nix
@@ -3,11 +3,5 @@
 {
   imports = [ ../. ];
 
-  # TODO: fix in NixOS/nixpkgs
-  # Disable governor set in hardware-configuration.nix,
-  # required when services.tlp.enable is true:
-  powerManagement.cpuFreqGovernor =
-    lib.mkIf config.services.tlp.enable (lib.mkForce null);
-
   services.tlp.enable = lib.mkDefault true;
 }
