087f2c766cc3c111b5974b6f6a7374e4e64c783a
pablo1107 <dealberapablo07@gmail.com>
2020-05-21 17:41:08 -0300

    ledger2beancount: init at 2.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21995,6 +21995,8 @@ in
 
   ledger-web = callPackage ../applications/office/ledger-web { };
 
+  ledger2beancount = callPackage ../tools/text/ledger2beancount { };
+
   lightburn = libsForQt5.callPackage ../applications/graphics/lightburn { };
 
   lighthouse = callPackage ../applications/misc/lighthouse { };
