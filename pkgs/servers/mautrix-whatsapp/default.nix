d91569ab1ae27392b955d187e98fc0e522906526
Maximilian Bosch <maximilian@mbosch.me>
2020-05-22 11:32:03 +0200

    mautrix-whatsapp: 2020-04-21-1 -> 2020-05-21
    (cherry picked from commit 5cdc8f4818b8ce758d7d5c8b18b87994183c3e1a)

--- pkgs/servers/mautrix-whatsapp/default.nix
+++ pkgs/servers/mautrix-whatsapp/default.nix
@@ -1,17 +1,19 @@
-{ stdenv, buildGoModule, fetchFromGitHub }:
+{ stdenv, buildGoModule, fetchFromGitHub, olm }:
 
 buildGoModule {
   pname = "mautrix-whatsapp-unstable";
-  version = "2020-04-21-1";
+  version = "2020-05-21";
 
   src = fetchFromGitHub {
     owner = "tulir";
     repo = "mautrix-whatsapp";
-    rev = "e0aea74abf090bc9dc499332b28bf03640c162f8";
-    sha256 = "1gayjyh0x0axc1xak38zkdhvx6fy8pwlniqsirqy2mwcgkkll9i5";
+    rev = "b4949eec5982643502bb9787cf5e2872a78807c1";
+    sha256 = "1hjqxqfza6r7fsxr4fgwhfdwjzligxk416692xi4pavd5krfxxmd";
   };
 
-  modSha256 = "1pddabyyz6q1snx9j7yv7dchasqa1y8nbpb5zrwmrpnwpns8kxl7";
+  modSha256 = "1gf92a54l8a51q2yfr7baikbpwici2q29v0g5pdyjnin8dn08vws";
+
+  buildInputs = [ olm ];
 
   meta = with stdenv.lib; {
     homepage = https://github.com/tulir/mautrix-whatsapp;
