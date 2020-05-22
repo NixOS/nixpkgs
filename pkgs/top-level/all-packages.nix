ab190020439c8ffdb80eedb8b62365ece62b7cd2
Scott Hamilton <sgn.hamilton@protonmail.com>
2020-05-22 18:06:50 +0200

    keysmith: Init at v0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4849,6 +4849,8 @@ in
 
   kea = callPackage ../tools/networking/kea { };
 
+  keysmith = libsForQt5.callPackage ../tools/security/keysmith { };
+
   ispell = callPackage ../tools/text/ispell {};
 
   jumanpp = callPackage ../tools/text/jumanpp {};
