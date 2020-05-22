0c70afa6d1626c9a958cf09a9d27ee39217bc51c
adisbladis <adisbladis@gmail.com>
2020-05-22 06:31:07 +0100

    podman-compose: Don't wrap podman
    Podman is now a wrapped with it's container runtimes in the podman
    closure.
    
    This means that the podman passed to podman-compose is not necessarily
    the one that the user wants to use as it lacks user configured
    container runtimes.
    
    This change aims to make package composition more intuitive by
    defering the installation of podman to the user of podman-compose.

--- pkgs/applications/virtualization/podman-compose/default.nix
+++ pkgs/applications/virtualization/podman-compose/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonApplication, fetchPypi, podman, pyyaml }:
+{ lib, buildPythonApplication, fetchPypi, pyyaml }:
 
 buildPythonApplication rec {
   version = "0.1.5";
@@ -9,7 +9,7 @@ buildPythonApplication rec {
     sha256 = "1sgbc889zq127qhxa9frhswa1mid19fs5qnyzfihx648y5i968pv";
   };
 
-  propagatedBuildInputs = [ pyyaml podman ];
+  propagatedBuildInputs = [ pyyaml ];
 
   meta = {
     description = "An implementation of docker-compose with podman backend";
