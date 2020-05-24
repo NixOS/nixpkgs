0476b979c44edb39815c3e45cdf10f9e4426fc07
Markus Kowalewski <markus.kowalewski@gmail.com>
2020-05-24 15:09:15 +0200

    omnisharp-roslyn: add meta.homepage

--- pkgs/development/tools/omnisharp-roslyn/default.nix
+++ pkgs/development/tools/omnisharp-roslyn/default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
 
   pname = "omnisharp-roslyn";
   version = "1.35.1";
-  
+
   src = fetchurl {
     url = "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v${version}/omnisharp-mono.tar.gz";
     sha256 = "0gx87qc9r3lhqn6q95y74z67sjcxnazkkdi9zswmaqyvjn8x7vf4";
@@ -33,6 +33,7 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "OmniSharp based on roslyn workspaces";
+    homepage = "https://github.com/OmniSharp/omnisharp-roslyn";
     platforms = platforms.linux;
     license = licenses.mit;
     maintainers = with maintainers; [ tesq0 ];
