d4d36c37c7ba945597f3ad25a278e304108bab9d
Ruud van Asseldonk <dev@veniogames.com>
2020-05-24 23:00:17 +0200

    acme-client: 1.0.0 -> 1.0.1
    I submitted my patch upstream, it was merged, and version 1.0.1 which
    includes it has been released. So the patch here is no longer necessary.
    
    Also, the maintainer added a Nixpkgs-based build environment to the
    upstream CI setup, so in the future the upstream version will likely
    not need any patching.

--- pkgs/tools/networking/acme-client/default.nix
+++ pkgs/tools/networking/acme-client/default.nix
@@ -12,21 +12,19 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   pname = "acme-client";
-  version = "1.0.0";
+  version = "1.0.1";
 
   src = fetchFromGitHub {
     owner = "graywolf";
     repo = "acme-client-portable";
     rev = "v${version}";
-    sha256 = "1p6jbxg00ing9v3jnpvq234w5r2gf8b04k9qm06mn336lcd2lgpl";
+    sha256 = "0ds7lxn08yiq7hap1xh014smjhd4gf9lv9ypfrf1ahqna3s2w7k8";
   };
 
   nativeBuildInputs = [ autoreconfHook bison pkgconfig ];
   buildInputs = [ libbsd libressl ] ++ optional stdenv.isDarwin apple_sdk.sdk;
 
   makeFlags = [ "PREFIX=${placeholder "out"}" ];
-
-  patches = [ ./limits.h.patch ];
 
   meta = {
     homepage = "https://github.com/graywolf/acme-client-portable";
