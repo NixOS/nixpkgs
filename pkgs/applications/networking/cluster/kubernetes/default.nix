2bed6d533b4c82a042cc7b9799339b48804b0dcf
Sascha Grunert <sgrunert@suse.com>
2020-05-25 15:46:18 +0200

    kubernetes: 1.18.1 -> 1.18.3
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- pkgs/applications/networking/cluster/kubernetes/default.nix
+++ pkgs/applications/networking/cluster/kubernetes/default.nix
@@ -15,13 +15,13 @@ with lib;
 
 stdenv.mkDerivation rec {
   pname = "kubernetes";
-  version = "1.18.1";
+  version = "1.18.3";
 
   src = fetchFromGitHub {
     owner = "kubernetes";
     repo = "kubernetes";
     rev = "v${version}";
-    sha256 = "1z109h0jqqfxvgyhmxsiggf6fb9kdrwvmv9wdi1n58n4yk65hvl6";
+    sha256 = "01r6yym74x8ysvs4i23082nm8maisg6ipy9h41y4h8glvnpha0h8";
   };
 
   nativeBuildInputs = [ removeReferencesTo makeWrapper which go rsync go-bindata ];
