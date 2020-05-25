20f453d20f581fd72fc655a590c04488f3ae52b8
José Romildo Malaquias <malaquias@gmail.com>
2020-05-25 16:43:36 -0300

    enlightenment.efl: 1.24.1 -> 1.24.2

--- pkgs/desktops/enlightenment/efl.nix
+++ pkgs/desktops/enlightenment/efl.nix
@@ -53,11 +53,11 @@
 
 stdenv.mkDerivation rec {
   pname = "efl";
-  version = "1.24.1";
+  version = "1.24.2";
 
   src = fetchurl {
     url = "http://download.enlightenment.org/rel/libs/${pname}/${pname}-${version}.tar.xz";
-    sha256 = "1xsbz5kl74cgzyzwmjy3p50m0iigvi53lklkp92v49k4j99zpak7";
+    sha256 = "0w3srvigg4kfi7xq76c7y4hnq5yr2gxrrsvlyj1g2wc1igz1vyg1";
   };
 
   nativeBuildInputs = [
