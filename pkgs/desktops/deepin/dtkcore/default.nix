aa88848d4e2f61a0a28714124d22a6a731f2fc4b
José Romildo Malaquias <malaquias@gmail.com>
2020-05-26 20:59:16 -0300

    deepin.dtkcore: 2.1.1 -> 5.2.2.3

--- pkgs/desktops/deepin/dtkcore/default.nix
+++ pkgs/desktops/deepin/dtkcore/default.nix
@@ -1,55 +1,78 @@
 { stdenv
 , mkDerivation
 , fetchFromGitHub
-, pkgconfig
+, pkg-config
 , qmake
+, glib
+, deepin-desktop-base
 , gsettings-qt
+, lshw
 , pythonPackages
 , deepin
 }:
 
 mkDerivation rec {
   pname = "dtkcore";
-  version = "2.1.1";
+  version = "5.2.2.3";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "0xdh6mmrv8yr6mjmlwj0fv037parkkwfwlaibcbrskwxqp9iri1y";
+    sha256 = "1w5zclf6vqninwhsmir27aaasrqqhp0gnpq3ipm61b5makflg0k2";
   };
 
   nativeBuildInputs = [
-    pkgconfig
+    pkg-config
     qmake
     pythonPackages.wrapPython
+    glib
     deepin.setupHook
   ];
 
   buildInputs = [
+    deepin-desktop-base
     gsettings-qt
+    lshw
   ];
 
   postPatch = ''
     searchHardCodedPaths  # debugging
 
+    fixPath ${deepin-desktop-base} /etc/deepin-version src/dsysinfo.cpp
+
+    substituteInPlace src/dsysinfo.cpp --replace \"lshw\" \"${lshw}/bin/lshw\"
+
     # Fix shebang
-    sed -i tools/script/dtk-translate.py -e "s,#!env,#!/usr/bin/env,"
+    substituteInPlace tools/script/dtk-translate.py --replace "#!env" "#!/usr/bin/env"
+
+    # Fix paths for other dtk modules (which depends on dtkcore)
+    substituteInPlace dtk_build_config.prf \
+      --replace \
+        'LIB_INSTALL_DIR=$''${QT.dtkcore.libs}' \
+        'LIB_INSTALL_DIR=$$PREFIX/lib' \
+      --replace \
+        'INCLUDE_INSTALL_DIR = $''${QT.dtkcore.includes}/../$$DMODULE_NAME' \
+        'INCLUDE_INSTALL_DIR = $$PREFIX/include/$$LIB_VERSION_NAME/$$DMODULE_NAME'
   '';
 
   qmakeFlags = [
     "DTK_VERSION=${version}"
-    "LIB_INSTALL_DIR=${placeholder "out"}/lib"
     "MKSPECS_INSTALL_DIR=${placeholder "out"}/mkspecs"
+    "QMAKE_PKGCONFIG_PREFIX=${placeholder "out"}"
   ];
 
-  postFixup = ''
-    chmod +x $out/lib/libdtk-${version}/DCore/bin/*.py
-    wrapPythonProgramsIn "$out/lib/libdtk-${version}/DCore/bin" "$out $pythonPath"
-    searchHardCodedPaths $out  # debugging
+  preFixup = ''
+    glib-compile-schemas ${glib.makeSchemaPath "$out" "${pname}-${version}"}
   '';
 
-  enableParallelBuilding = true;
+  postFixup = ''
+    chmod +x $out/lib/libdtk-*/DCore/bin/*.py
+    wrapPythonProgramsIn $out/lib/libdtk-*/DCore/bin "$out $pythonPath"
+    wrapQtApp $out/lib/libdtk-*/DCore/bin/deepin-os-release
+    wrapQtApp $out/lib/libdtk-*/DCore/bin/dtk-settings
+    searchHardCodedPaths $out  # debugging
+  '';
 
   passthru.updateScript = deepin.updateScript { inherit pname version src; };
 
