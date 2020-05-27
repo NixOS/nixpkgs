fd7ddb0d3a20600f5101929bfb6cff5637d0f8e4
José Romildo Malaquias <malaquias@gmail.com>
2020-05-27 13:23:08 -0300

    deepin.dde-polkit-agent: 5.0.0 -> 5.2.0.7

--- pkgs/desktops/deepin/dde-polkit-agent/default.nix
+++ pkgs/desktops/deepin/dde-polkit-agent/default.nix
@@ -1,29 +1,31 @@
 { stdenv
 , mkDerivation
 , fetchFromGitHub
-, pkgconfig
+, pkg-config
 , qmake
-, qttools
-, polkit-qt
-, dtkcore
-, dtkwidget
 , dde-qt-dbus-factory
+, dtkwidget
+, gsettings-qt
+, polkit-qt
+, qtmultimedia
+, qttools
+, qtx11extras
 , deepin
 }:
 
 mkDerivation rec {
   pname = "dde-polkit-agent";
-  version = "5.0.0";
+  version = "5.2.0.7";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "00p8syx6rfwhq7wdsk37hm9mvwd0kwj9h0s39hii892h1psd84q9";
+    sha256 = "076laqrnwr87dp459xl5ji51hisv1g3j4gsvx8bd31048rx3nfbn";
   };
 
   nativeBuildInputs = [
-    pkgconfig
+    pkg-config
     qmake
     qttools
     deepin.setupHook
@@ -31,14 +33,19 @@ mkDerivation rec {
 
   buildInputs = [
     dde-qt-dbus-factory
-    dtkcore
     dtkwidget
+    gsettings-qt
     polkit-qt
+    qtmultimedia
+    qtx11extras
   ];
 
   postPatch = ''
     searchHardCodedPaths
     patchShebangs translate_generation.sh
+
+    # https://github.com/linuxdeepin/developer-center/issues/1721
+    substituteInPlace policykitlistener.cpp --replace 'bool is_deepin = true' 'bool is_deepin = false'
 
     fixPath $out /usr dde-polkit-agent.pro polkit-dde-authentication-agent-1.desktop
     fixPath /run/current-system/sw /usr/lib/polkit-1-dde/plugins pluginmanager.cpp
