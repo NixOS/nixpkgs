5840c749e039c838595f7cf42d157e7cf4788c14
MetaDark <kira.bruneau@gmail.com>
2020-05-27 19:21:14 -0400

    pythonPackages.pytest-datadir: init at 1.3.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2482,6 +2482,8 @@ in {
 
   pytest-cram = callPackage ../development/python-modules/pytest-cram { };
 
+  pytest-datadir = callPackage ../development/python-modules/pytest-datadir { };
+
   pytest-datafiles = callPackage ../development/python-modules/pytest-datafiles { };
 
   pytest-dependency = callPackage ../development/python-modules/pytest-dependency { };
