deed24b16602ce7a678dbc8516c78bff6ea7b106
davidak <git@davidak.de>
2020-05-28 00:50:30 +0200

    klavaro: 3.08 -> 3.09

--- pkgs/games/klavaro/default.nix
+++ pkgs/games/klavaro/default.nix
@@ -2,12 +2,15 @@
 
 stdenv.mkDerivation rec {
   pname = "klavaro";
-  version = "3.08";
+  version = "3.09";
 
   src = fetchurl {
     url = "mirror://sourceforge/klavaro/${pname}-${version}.tar.bz2";
-    sha256 = "0qmvr6d8wshwp0xvk5wbig4vlzxzcxrakhyhd32v8v3s18nhqsrc";
+    sha256 = "12gml7h45b1w9s318h0d5wxw92h7pgajn2kh57j0ak9saq0yb0wr";
   };
+
+  # Fix for https://sourceforge.net/p/klavaro/bugs/56/
+  patches = [ ./patch1 ];
 
   nativeBuildInputs = [ intltool makeWrapper pkgconfig ];
   buildInputs = [ curl gtk3 ];
@@ -25,6 +28,6 @@ stdenv.mkDerivation rec {
     homepage = "http://klavaro.sourceforge.net/";
     license = stdenv.lib.licenses.gpl3Plus;
     platforms = stdenv.lib.platforms.linux;
-    maintainers = [stdenv.lib.maintainers.mimame];
+    maintainers = [ stdenv.lib.maintainers.mimame ];
   };
 }
