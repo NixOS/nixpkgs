d4471a1b89232323627ed11f5c86bcd351d07841
José Romildo Malaquias <malaquias@gmail.com>
2020-05-27 13:25:01 -0300

    deepin.qt5integration: 5.0.0 -> 5.1.0.4

--- pkgs/desktops/deepin/qt5integration/default.nix
+++ pkgs/desktops/deepin/qt5integration/default.nix
@@ -1,7 +1,7 @@
 { stdenv
 , mkDerivation
 , fetchFromGitHub
-, pkgconfig
+, pkg-config
 , qmake
 , mtdev
 , lxqt
@@ -11,28 +11,31 @@
 , qt5platform-plugins
 , qtstyleplugins
 , dtkcore
+, dtkgui
 , dtkwidget
 , deepin
 }:
 
 mkDerivation rec {
   pname = "qt5integration";
-  version = "5.0.0";
+  version = "5.1.0.4";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "140wb3vcm2ji8jhqdxv8f4shiknia1zk8fssqlp09kzc1cmb4ncy";
+    sha256 = "0ir3dkf1xr92pnmcnxbp43s87wp9fnf6ia7xifls3xqf5msz0mmv";
   };
 
   nativeBuildInputs = [
-    pkgconfig
+    pkg-config
     qmake
+    deepin.setupHook
   ];
 
   buildInputs = [
     dtkcore
+    dtkgui
     dtkwidget
     qt5platform-plugins
     mtdev
@@ -44,14 +47,20 @@ mkDerivation rec {
   ];
 
   postPatch = ''
-    sed -i dstyleplugin/dstyleplugin.pro \
-           platformthemeplugin/qt5deepintheme-plugin.pro \
-           iconengineplugins/svgiconengine/svgiconengine.pro \
-           imageformatplugins/svg/svg.pro \
-      -e "s,\$\$\[QT_INSTALL_PLUGINS\],$out/$qtPluginPrefix,"
+    searchHardCodedPaths # debugging
+
+    # # TODO: use path from dde-file-manager package (which may cause mutual dependences)
+    # substituteInPlace platformthemeplugin/qdeepinfiledialoghelper.cpp \
+    #   --replace /usr/bin/dde-file-manager dde-file-manager
+
+    for f in $(grep -lr QT_INSTALL_PLUGINS); do
+      substituteInPlace "$f" --replace '$$[QT_INSTALL_PLUGINS]' "$out/$qtPluginPrefix"
+    done
   '';
 
-  enableParallelBuilding = true;
+  postFixup = ''
+    searchHardCodedPaths $out # debugging
+  '' ;
 
   passthru.updateScript = deepin.updateScript { inherit pname version src; };
 
