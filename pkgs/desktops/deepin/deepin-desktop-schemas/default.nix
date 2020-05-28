0f901bc4fefa463f8eea79d5b78e6be0682a4810
José Romildo Malaquias <malaquias@gmail.com>
2020-05-27 23:54:09 -0300

    deepin.deepin-desktop-schemas: 3.13.9 -> 5.8.0.5

--- pkgs/desktops/deepin/deepin-desktop-schemas/default.nix
+++ pkgs/desktops/deepin/deepin-desktop-schemas/default.nix
@@ -1,6 +1,8 @@
 { stdenv
 , fetchFromGitHub
 , python3
+, go
+, go-lib
 , dconf
 , glib
 , deepin-gtk-theme
@@ -12,17 +14,19 @@
 
 stdenv.mkDerivation rec {
   pname = "deepin-desktop-schemas";
-  version = "3.13.9";
+  version = "5.8.0.5";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "1c69j6s7561zb1hrd1j3ihji1nvpgfzfgnp6svsv8jd8dg8vs8l1";
+    sha256 = "120fs6ca7znq79gjifa54j119b9bzlpann61mk2bxjl7bbcjw1na";
   };
 
   nativeBuildInputs = [
     python3
+    go
+    go-lib
     glib.dev
     deepin.setupHook
   ];
@@ -40,30 +44,41 @@ stdenv.mkDerivation rec {
 
     # fix default background url
     sed -i -e 's,/usr/share/backgrounds/default_background.jpg,/usr/share/backgrounds/deepin/desktop.jpg,' \
-      overrides/common/com.deepin.wrap.gnome.desktop.override
+      overrides/common/com.deepin.wrap.gnome.desktop.override \
+      schemas/com.deepin.dde.appearance.gschema.xml
 
     fixPath ${deepin-wallpapers} /usr/share/backgrounds \
-      overrides/common/com.deepin.wrap.gnome.desktop.override
-
-    fixPath ${deepin-wallpapers} /usr/share/wallpapers/deepin \
+      overrides/common/com.deepin.wrap.gnome.desktop.override \
       schemas/com.deepin.dde.appearance.gschema.xml
+
+    substituteInPlace schemas/wrap/com.deepin.wrap.gnome.desktop.app-folders.gschema.xml \
+      --replace /usr/share/desktop-directories \
+                /run/current-system/sw/share/desktop-directories
 
     # still hardcoded paths:
     #   /etc/gnome-settings-daemon/xrandr/monitors.xml                                ? gnome3.gnome-settings-daemon
+    #   /opt/google/chrome/chrome
     #   /usr/share/backgrounds/gnome/adwaita-lock.jpg                                 ? gnome3.gnome-backgrounds
     #   /usr/share/backgrounds/gnome/adwaita-timed.xml                                gnome3.gnome-backgrounds
-    #   /usr/share/desktop-directories
   '';
 
   makeFlags = [
     "PREFIX=${placeholder "out"}"
   ];
 
+  preBuild = ''
+    export GOPATH="$GOPATH:${go-lib}/share/go"
+    export GOCACHE=$TMPDIR/go-cache
+  '';
+
   doCheck = true;
   checkTarget = "test";
 
   postInstall = ''
     glib-compile-schemas --strict $out/share/glib-2.0/schemas
+  '';
+
+  postFixup = ''
     searchHardCodedPaths $out
   '';
 
