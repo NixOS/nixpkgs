b542984371123c51cfa9905ecf8c24578da26f85
Markus Müller <markus.mueller.1.g@googlemail.com>
2020-05-28 10:41:26 +0200

    updated my previous extension of the netcdf4 expression #67338. Now also the tests run except one that is skipped by the patch. The behavior of the tests is puzzeling to me in two ways: 1. If I drop into nix-shell (even  with --pure ) and call genericBuild    all tests run but if I use nix-build the test fails.    (If you look at the test you see that it tries to load a remote url.    I have no clue why this works in the shell and not in nix-build)
    2. A similar thing happens with py.test:
       It works in the nix-shell but fails with nix-build but here the test
       suite does not even start.
       I used unittest instead which leads to only one test failing (which
       is now skipped by the patch)

--- pkgs/development/python-modules/netcdf4/default.nix
+++ pkgs/development/python-modules/netcdf4/default.nix
@@ -1,36 +1,27 @@
-<<<<<<< HEAD
-{ stdenv, lib, buildPythonPackage, fetchPypi, isPyPy, pytest
+{ stdenv, lib, buildPythonPackage, fetchPypi, isPyPy
 , numpy, zlib, netcdf, hdf5, curl, libjpeg, cython, cftime
-}:
-buildPythonPackage rec {
-  pname = "netCDF4";
-  version = "1.5.3";
-=======
-{ stdenv, buildPythonPackage, fetchPypi, isPyPy, pytest
-, numpy, zlib, netcdf, hdf5, curl, libjpeg, cython, cftime 
 ,mpi4py ? null, openssh ? null}:
-assert (mpi4py!= null) -> 
+assert (mpi4py != null) -> 
   openssh != null
   && hdf5.mpi == mpi4py.mpi 
   && netcdf.mpi == mpi4py.mpi;
->>>>>>> netcdf4:init at 1.5.1.2
 
 let 
-  mpiSupport = (mpi4py!=null);
+  mpiSupport = (mpi4py != null);
   mpi= if mpiSupport then mpi4py.mpi else null;
 in
   buildPythonPackage rec {
     pname = "netCDF4";
-    version = "1.5.1.2";
+    version = "1.5.3";
   
     disabled = isPyPy;
     
     src = fetchPypi {
       inherit pname version;
-      sha256 = "161pqb7xc9nj0dlnp6ply8c6zv68y1frq619xqfrpmc9s1932jzk";
+      sha256 = "2a3ca855848f4bbf07fac366da77a681fcead18c0a8813d91d46302f562dc3be";
     };
   
-    checkInputs = [ pytest ];
+    checkInputs = [ netcdf numpy ] ++(if mpiSupport then [ mpi4py openssh mpi] else []);
   
     buildInputs = [
       cython 
@@ -45,67 +36,23 @@ in
       hdf5
       curl
       libjpeg
-    ] ++ (if mpiSupport then [mpi4py openssh] else []);
+    ] ++ (if mpiSupport then [mpi4py mpi openssh] else []);
 
-<<<<<<< HEAD
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "2a3ca855848f4bbf07fac366da77a681fcead18c0a8813d91d46302f562dc3be";
-  };
-
-  checkInputs = [ pytest ];
-
-  buildInputs = [
-    cython
-  ];
-
-  propagatedBuildInputs = [
-    cftime
-    numpy
-    zlib
-    netcdf
-    hdf5
-    curl
-    libjpeg
-  ];
-
-  checkPhase = ''
-    py.test test/tst_*.py
-  '';
-
-  # Tests need fixing.
-  doCheck = false;
-
-  # Variables used to configure the build process
-  USE_NCCONFIG="0";
-  HDF5_DIR = lib.getDev hdf5;
-  NETCDF4_DIR=netcdf;
-  CURL_DIR=curl.dev;
-  JPEG_DIR=libjpeg.dev;
-
-  meta = with stdenv.lib; {
-    description = "Interface to netCDF library (versions 3 and 4)";
-    homepage = "https://pypi.python.org/pypi/netCDF4";
-    license = licenses.free;  # Mix of license (all MIT* like)
-  };
-}
-=======
-    patches=[./parallel4Detection.patch];
+    patches=[./skipDapTest.patch];
   
     preBuild= if mpiSupport then ''
       makeFlagsArray+=( CC=mpicc CXX=mpicxx )
     '' else "";
 
-    checkPhase = ''
-      py.test test/tst_*.py
+    checkPhase= ''
+      cd test
+      python -m unittest tst_*.py
     '';
   
-    # Tests need fixing.
-    doCheck = false;
   
     # Variables used to configure the build process
     USE_NCCONFIG="0";
-    HDF5_DIR="${hdf5}";
+    HDF5_DIR= lib.getDev hdf5;
     NETCDF4_DIR="${netcdf}";
     CURL_DIR="${curl.dev}";
     JPEG_DIR="${libjpeg.dev}";
@@ -118,9 +65,8 @@ in
       inherit mpi;
     }; 
     meta = with stdenv.lib; {
-      description = "Interface to netCDF library (versions 3 and 4) with support for parallel IO";
+      description = "Interface to netCDF library (versions 3 and 4)";
       homepage = https://pypi.python.org/pypi/netCDF4;
       license = licenses.free;  # Mix of license (all MIT* like)
     };
   }
->>>>>>> netcdf4:init at 1.5.1.2
