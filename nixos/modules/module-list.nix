ab327b27a1d42c8f2fbfe3e174891e695050d738
Notkea <pacien@users.noreply.github.com>
2020-06-01 10:45:04 +0200

    nixos/mautrix-telegram: add module (#63589)

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -464,6 +464,7 @@
   ./services/misc/mathics.nix
   ./services/misc/matrix-appservice-discord.nix
   ./services/misc/matrix-synapse.nix
+  ./services/misc/mautrix-telegram.nix
   ./services/misc/mbpfan.nix
   ./services/misc/mediatomb.nix
   ./services/misc/mesos-master.nix
