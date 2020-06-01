e8aed4a84d4937481cb1e674dad5b7dc270e6b4a
Wesley Nelson <wgn@wesnel.dev>
2020-05-31 22:55:36 -0500

    maintainers: update wesnel

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8133,9 +8133,13 @@
   };
   wesnel = {
     name = "Wesley Nelson";
-    email = "wesley@sent.as";
+    email = "wgn@wesnel.dev";
     github = "wesnel";
     githubId = 43357387;
+    keys = [{
+      longkeyid = "rsa4096/0xA776D2AD099E8BC0";
+      fingerprint = "F844 80B2 0CA9 D6CC C7F5  2479 A776 D2AD 099E 8BC0";
+    }];
   };
   WhittlesJr = {
     email = "alex.joseph.whitt@gmail.com";
