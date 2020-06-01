b96e5f5d50bd0f9d2d82d42732c4e07cba7c0fc9
Milan Pässler <mil@nyantec.com>
2020-06-01 15:45:59 +0200

    minecraft-server: allow update script to be called from nixpkgs root

--- pkgs/games/minecraft-server/update.sh
+++ pkgs/games/minecraft-server/update.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p curl jq common-updater-scripts
 
+BASEDIR="$(dirname "$0")"
+
 # get current release version
 versions=$(curl -s 'https://launchermeta.mojang.com/mc/game/version_manifest.json')
 version=$(echo $versions | jq .latest.release)
@@ -14,6 +16,6 @@ url=$(echo $versions | jq .url)
 echo $version: $url:$sha1
 
 # change default.nix
-sed -i "s/version = \"[0-9.]*\";/version = ${version};/g" default.nix
-sed -i "s+url = \"[a-zA-Z0-9/:.]*/server.jar\";+url = $url;+g" default.nix
-sed -i "s/sha1 = \"[a-zA-Z0-9]*\";/sha1 = ${sha1};/g" default.nix
+sed -i "s/version = \"[0-9.]*\";/version = ${version};/g" "$BASEDIR/default.nix"
+sed -i "s+url = \"[a-zA-Z0-9/:.]*/server.jar\";+url = $url;+g" "$BASEDIR/default.nix"
+sed -i "s/sha1 = \"[a-zA-Z0-9]*\";/sha1 = ${sha1};/g" "$BASEDIR/default.nix"
