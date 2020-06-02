a8848248d23a99434f11862a36e333125d69bf50
José Romildo Malaquias <malaquias@gmail.com>
2020-06-02 10:36:50 -0300

    ubuntu-themes: init at 19.04

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18495,6 +18495,8 @@ in
 
   tzdata = callPackage ../data/misc/tzdata { };
 
+  ubuntu-themes = callPackage ../data/themes/ubuntu-themes { };
+
   ubuntu_font_family = callPackage ../data/fonts/ubuntu-font-family { };
 
   ucs-fonts = callPackage ../data/fonts/ucs-fonts
