762568e3e279bdee28d2a1b6a78d6852bdd788a9
Justin Gerhardt <justin@gerhardt.link>
2020-06-01 22:42:20 -0400

    zfsbackup: unstable-2019-03-05 -> unstable 2020-05-25

--- pkgs/tools/backup/zfsbackup/default.nix
+++ pkgs/tools/backup/zfsbackup/default.nix
@@ -2,8 +2,8 @@
 
 buildGoPackage rec {
   pname = "zfsbackup";
-  version = "unstable-2019-03-05";
-  rev = "78fea6e99f0a5a4c8513d3a3d1d45fb6750cfddf";
+  version = "unstable-2020-05-25";
+  rev = "7793275d92bade8b3d088b990b4c01cf0b98869c";
 
   goPackagePath = "github.com/someone1/zfsbackup-go";
 
