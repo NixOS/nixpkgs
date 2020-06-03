21c43f6e7530563534b34bce0eb723c9cbcca8b0
Jonathan Ringer <jonringer117@gmail.com>
2020-06-02 17:50:33 -0700

    python3Packages.azure-servicebus: add imports check

--- pkgs/development/python-modules/azure-servicebus/default.nix
+++ pkgs/development/python-modules/azure-servicebus/default.nix
@@ -29,6 +29,9 @@ buildPythonPackage rec {
   # has no tests
   doCheck = false;
 
+  # python2 will fail due to pep 420
+  pythonImportsCheck = lib.optionals isPy3k [ "azure.servicebus" ];
+
   meta = with lib; {
     description = "This is the Microsoft Azure Service Bus Client Library";
     homepage = "https://github.com/Azure/azure-sdk-for-python";
