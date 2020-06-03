5bd1e52504cbf00b5fe4cf6602f5c43032af2c06
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-06-03 21:14:53 +0200

    steck: init at 0.6.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6917,6 +6917,8 @@ in
 
   stdman = callPackage ../data/documentation/stdman { };
 
+  steck = callPackage ../servers/pinnwand/steck.nix { };
+
   stenc = callPackage ../tools/backup/stenc { };
 
   stm32loader = with python3Packages; toPythonApplication stm32loader;
