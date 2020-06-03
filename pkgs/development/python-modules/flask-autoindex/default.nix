NixOS/nixpkgs

commit ec12a5fd1e9379b359871efadeb245e8734f59a8

python3Packages.Flask-AutoIndex: 0.6.2 -> 0.6.6

pkgs/development/python-modules/flask-autoindex/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "Flask-AutoIndex";
-  version = "0.6.2";
+  version = "0.6.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "af2cdb34eefe6edbf43ce19200880829e8c2df3598000e75dc63c9b7e3478706";
+    sha256 = "ea319f7ccadf68ddf98d940002066278c779323644f9944b300066d50e2effc7";
   };
 
   propagatedBuildInputs = [

commit caf2c1bb07e32ec1cd7914ed27f992589c8b6da6

python2.pkgs.flask-autoindex: fix build
pathlib is now required, which is part of the standard library in python
3.4+ but not in python 2.7. Broken by
ec12a5fd1e9379b359871efadeb245e8734f59a8

pkgs/development/python-modules/flask-autoindex/default.nix
@@ -1,9 +1,11 @@
-{ stdenv
+{ lib
 , buildPythonPackage
+, pythonOlder
 , fetchPypi
 , flask
 , flask-silk
 , future
+, pathlib
 }:
 
 buildPythonPackage rec {
@@ -19,9 +21,11 @@ buildPythonPackage rec {
     flask
     flask-silk
     future
+  ] ++ lib.optionals (pythonOlder "3.4") [
+    pathlib
   ];
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "The mod_autoindex for Flask";
     longDescription = ''
       Flask-AutoIndex generates an index page for your Flask application automatically.