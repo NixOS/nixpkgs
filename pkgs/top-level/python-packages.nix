3c6637c433dae03e5d3c9920f62cd3dfd8ec61a9
Ben Darwin <bcdarwin@gmail.com>
2020-06-03 15:32:08 -0400

    python3Packages.ci-py: init at 1.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1989,6 +1989,8 @@ in {
 
   ci-info = callPackage ../development/python-modules/ci-info { };
 
+  ci-py = callPackage ../development/python-modules/ci-py { };
+
   cli-helpers = callPackage ../development/python-modules/cli-helpers {};
 
   cmarkgfm = callPackage ../development/python-modules/cmarkgfm { };
