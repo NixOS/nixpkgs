0b1d2e3c975c9eae6ba05eeb5f07a4ff043e0c9e
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2020-06-05 01:24:53 +0300

    maintainers: add yusdacra

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9138,6 +9138,16 @@
       fingerprint = "85F8 E850 F8F2 F823 F934  535B EC50 6589 9AEA AF4C";
     }];
   };
+  yusdacra = {
+    email = "y.bera003.06@protonmail.com";
+    github = "yusdacra";
+    githubId = 19897088;
+    name = "Yusuf Bera Ertan";
+    keys = [{
+      longkeyid = "rsa2048/0x61807181F60EFCB2";
+      fingerprint = "9270 66BD 8125 A45B 4AC4 0326 6180 7181 F60E FCB2";
+    }];
+  };
   yvesf = {
     email = "yvesf+nix@xapek.org";
     github = "yvesf";
