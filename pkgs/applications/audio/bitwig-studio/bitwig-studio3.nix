176df77dab0eb8bc9e0d838422c1477e78d98bd1
Ingolf Wagner <contact@ingolf-wagner.de>
2020-06-05 22:54:24 +0200

    bitwig-studio3: 3.1.3 -> 3.2

--- pkgs/applications/audio/bitwig-studio/bitwig-studio3.nix
+++ pkgs/applications/audio/bitwig-studio/bitwig-studio3.nix
@@ -2,11 +2,11 @@
 
 bitwig-studio1.overrideAttrs (oldAttrs: rec {
   name = "bitwig-studio-${version}";
-  version = "3.1.3";
+  version = "3.2";
 
   src = fetchurl {
     url = "https://downloads.bitwig.com/stable/${version}/bitwig-studio-${version}.deb";
-    sha256 = "11z5flmp55ywgxyccj3pzhijhaggi42i2pvacg88kcpj0cin57vl";
+    sha256 = "1gvj5bdavmy8486rm2hin65b37irncn7n0n1wk1fyivc3vz7mwsh";
   };
 
   buildInputs = oldAttrs.buildInputs ++ [ xorg.libXtst ];
