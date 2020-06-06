f800711aaa7bd6a2df4197c7d88abf19b90e6212
Martin Milata <martin@martinmilata.cz>
2020-06-06 17:45:57 +0200

    lnd: build with same feature tags as upstream
    See: https://github.com/lightningnetwork/lnd/blob/v0.10.3-beta/make/release_flags.mk#L36
    
    Also pass RawTags and GoVersion to build so that it
    shows in e.g. "lncli version".

--- pkgs/applications/blockchains/lnd.nix
+++ pkgs/applications/blockchains/lnd.nix
@@ -1,4 +1,6 @@
-{ buildGoModule, fetchFromGitHub, lib }:
+{ buildGoModule, fetchFromGitHub, lib
+, tags ? [ "autopilotrpc" "signrpc" "walletrpc" "chainrpc" "invoicesrpc" "watchtowerrpc" ]
+}:
 
 buildGoModule rec {
   pname = "lnd";
@@ -14,6 +16,18 @@ buildGoModule rec {
   vendorSha256 = "0a4bk2qry0isnrvl0adwikqn6imxwzlaq5j3nglb5rmwwq2cdz0r";
 
   subPackages = ["cmd/lncli" "cmd/lnd"];
+
+  preBuild = let
+    buildVars = {
+      RawTags = lib.concatStringsSep "," tags;
+      GoVersion = "$(go version | egrep -o 'go[0-9]+[.][^ ]*')";
+    };
+    buildVarsFlags = lib.concatStringsSep " " (lib.mapAttrsToList (k: v: "-X github.com/lightningnetwork/lnd/build.${k}=${v}") buildVars);
+  in
+  lib.optionalString (tags != []) ''
+    buildFlagsArray+=("-tags=${lib.concatStringsSep " " tags}")
+    buildFlagsArray+=("-ldflags=${buildVarsFlags}")
+  '';
 
   meta = with lib; {
     description = "Lightning Network Daemon";
