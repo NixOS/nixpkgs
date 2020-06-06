340d03cd7540472ab8568f8afdf11a751b93056d
Frederik Rietdijk <fridh@fridh.nl>
2020-06-06 08:47:21 +0200

    python: parsedatetime: 2.5 -> 2.6

--- pkgs/development/python-modules/parsedatetime/default.nix
+++ pkgs/development/python-modules/parsedatetime/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "parsedatetime";
-  version = "2.5";
+  version = "2.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "d2e9ddb1e463de871d32088a3f3cea3dc8282b1b2800e081bd0ef86900451667";
+    sha256 = "4cb368fbb18a0b7231f4d76119165451c8d2e35951455dfee97c62a87b04d455";
   };
 
   buildInputs = [ pytest pytestrunner ];
