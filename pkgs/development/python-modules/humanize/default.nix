97650b51b1a608ff4b1d28ef470d676644f49e2e
Frederik Rietdijk <fridh@fridh.nl>
2020-06-06 08:47:13 +0200

    python: humanize: 0.5.1 -> 2.4.0

--- pkgs/development/python-modules/humanize/default.nix
+++ pkgs/development/python-modules/humanize/default.nix
@@ -5,12 +5,12 @@
 }:
 
 buildPythonPackage rec {
-  version = "0.5.1";
+  version = "2.4.0";
   pname = "humanize";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a43f57115831ac7c70de098e6ac46ac13be00d69abbf60bdcac251344785bb19";
+    sha256 = "42ae7d54b398c01bd100847f6cb0fc9e381c21be8ad3f8e2929135e48dbff026";
   };
 
   buildInputs = [ mock ];
