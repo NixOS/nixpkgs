cf79718fc03967b12bb5f982b79200fa2e67fadc
R. RyanTM <ryantm-bot@ryantm.com>
2020-06-06 07:40:30 +0000

    flow: 0.125.1 -> 0.126.1

--- pkgs/development/tools/analysis/flow/default.nix
+++ pkgs/development/tools/analysis/flow/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "flow";
-  version = "0.125.1";
+  version = "0.126.1";
 
   src = fetchFromGitHub {
     owner  = "facebook";
     repo   = "flow";
     rev    = "refs/tags/v${version}";
-    sha256 = "0c6ai2fcssys8xd445xxc0zdn3jz6kcy85mac1p89mnlqd1r1xk4";
+    sha256 = "1acmhyhbl4s2c907nj8jwgg9fjjrfr306vagwkcx2lza4clb2aan";
   };
 
   installPhase = ''
