3625da8994a8bf3f8ed61a7a0384d7d3f1a2d76e
Frederik Rietdijk <fridh@fridh.nl>
2020-06-06 08:47:07 +0200

    python: Flask-Compress: 1.4.0 -> 1.5.0

--- pkgs/development/python-modules/flask-compress/default.nix
+++ pkgs/development/python-modules/flask-compress/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchPypi, buildPythonPackage, flask }:
 
 buildPythonPackage rec {
-  version = "1.4.0";
+  version = "1.5.0";
   pname = "Flask-Compress";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1cxdbdiyxkspg7vkchfmaqr7c6q79gwvakna3fjcc6nivps971j6";
+    sha256 = "f367b2b46003dd62be34f7fb1379938032656dca56377a9bc90e7188e4289a7c";
   };
 
   propagatedBuildInputs = [ flask ];
