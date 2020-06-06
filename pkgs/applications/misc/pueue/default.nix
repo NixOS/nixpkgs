232651e3b66762e97bdf3173fcbcba7f0d4d8ad7
R. RyanTM <ryantm-bot@ryantm.com>
2020-06-06 16:47:27 +0000

    pueue: 0.5.0 -> 0.5.1

--- pkgs/applications/misc/pueue/default.nix
+++ pkgs/applications/misc/pueue/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "pueue";
-  version = "0.5.0";
+  version = "0.5.1";
 
   src = fetchFromGitHub {
     owner = "Nukesor";
     repo = pname;
     rev = "v${version}";
-    sha256 = "17v760mh5k5vb1h3xvaph5rfc5wdl6q42isil2k9n6y8bxr3yw84";
+    sha256 = "1n85b41kvx7x7qdizdnq1p0zrkap4gkwnnvhh4pz6j9njxj8d9ir";
   };
 
-  cargoSha256 = "1m659i3v3b5hfn5vb5126izcy690bkmlyihz2w90h2d02ig7170p";
+  cargoSha256 = "0hkkz74hllc5dzmgls6bgdxsdr871df2fn51sa3shv68ah0avxff";
 
   nativeBuildInputs = [ installShellFiles ];
 
