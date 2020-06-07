cc28f9cea3b24784a1d83dbbb5005cab74ee0329
Maximilian Bosch <maximilian@mbosch.me>
2020-06-07 16:08:39 +0200

    i3status-rust: 0.13.1 -> 0.14.0
    https://github.com/greshake/i3status-rust/releases/tag/v0.14.0
    (cherry picked from commit 8454ef7b296608f42a2447e6cc6db8c7dd542116)

--- pkgs/applications/window-managers/i3/status-rust.nix
+++ pkgs/applications/window-managers/i3/status-rust.nix
@@ -1,17 +1,25 @@
-{ stdenv, rustPlatform, fetchFromGitHub, pkgconfig, dbus, libpulseaudio }:
+{ stdenv, rustPlatform, fetchFromGitHub, pkgconfig, dbus, libpulseaudio, fetchpatch }:
 
 rustPlatform.buildRustPackage rec {
   pname = "i3status-rust";
-  version = "0.13.1";
+  version = "0.14.0";
 
   src = fetchFromGitHub {
     owner = "greshake";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0va6ny1v7lk30hhx4i5qyk9fwg3apy2nmh6kbmxhcf0rs5449ikg";
+    sha256 = "0d2xigm932x6pc9z24g5cg8xq2crd9n3wq1bwi96h35w799lagjg";
   };
 
-  cargoSha256 = "099hn0pm9ppply3m3dwns3f5p43rdag2d3niaj8jyc1mnavviwjv";
+  cargoPatches = [
+    # https://github.com/greshake/i3status-rust/pull/732/ (Update Cargo.lock)
+    (fetchpatch {
+      url = "https://github.com/greshake/i3status-rust/commit/7762a5c7ad668272fb8bb8409f12242094b032b8.patch";
+      sha256 = "097f6w91cn53cj1g3bbdqm9jjib5fkb3id91jqvq88h43x14b8zb";
+    })
+  ];
+
+  cargoSha256 = "098d08likpnycfb32s83vp0bphxf27k1gfcc26721fc9c8ah0lah";
 
   nativeBuildInputs = [ pkgconfig ];
 
