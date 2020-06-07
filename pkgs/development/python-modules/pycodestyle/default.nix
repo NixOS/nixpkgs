5c08320af6ed0f19b42fe970ac7d5fede1233891
Frederik Rietdijk <fridh@fridh.nl>
2020-06-07 09:33:46 +0200

    python.pkgs.pycodestyle: fixup tests

--- pkgs/development/python-modules/pycodestyle/default.nix
+++ pkgs/development/python-modules/pycodestyle/default.nix
@@ -1,6 +1,7 @@
 { buildPythonPackage
 , fetchPypi
 , lib
+, python
 }:
 
 buildPythonPackage rec {
@@ -12,12 +13,14 @@ buildPythonPackage rec {
     sha256 = "c58a7d2815e0e8d7972bf1803331fb0152f867bd89adf8a01dfd55085434192e";
   };
 
+  dontUseSetuptoolsCheck = true;
+
   # https://github.com/PyCQA/pycodestyle/blob/2.5.0/tox.ini#L14
   checkPhase = ''
-    python pycodestyle.py --max-doc-length=72 --testsuite testsuite
-    python pycodestyle.py --statistics pycodestyle.py
-    python pycodestyle.py --max-doc-length=72 --doctest
-    python setup.py test
+    ${python.interpreter} pycodestyle.py --max-doc-length=72 --testsuite testsuite
+    ${python.interpreter} pycodestyle.py --statistics pycodestyle.py
+    ${python.interpreter} pycodestyle.py --max-doc-length=72 --doctest
+    ${python.interpreter} -m unittest discover testsuite -vv
   '';
 
   meta = with lib; {
