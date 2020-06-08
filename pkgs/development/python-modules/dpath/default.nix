4e299b06f77077d9848378b736aaf44198438534
Jonathan Ringer <jonringer117@gmail.com>
2020-06-08 12:03:19 -0700

    python3Packages.dpath: fix tests

--- pkgs/development/python-modules/dpath/default.nix
+++ pkgs/development/python-modules/dpath/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchPypi, buildPythonPackage
-, mock, nose
+, mock, pytestCheckHook, nose, hypothesis
 }:
 
 buildPythonPackage rec {
@@ -11,10 +11,8 @@ buildPythonPackage rec {
     sha256 = "bea06b5f4ff620a28dfc9848cf4d6b2bfeed34238edeb8ebe815c433b54eb1fa";
   };
 
-  checkInputs = [ mock nose ];
-  checkPhase = ''
-    nosetests
-  '';
+  # use pytest as nosetests hangs
+  checkInputs = [ mock nose pytestCheckHook hypothesis ];
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/akesterson/dpath-python";
