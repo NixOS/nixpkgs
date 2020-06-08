86a9689852f6e867d364e62f98b2ce0c50621c39
Michael Weiss <dev.primeos@gmail.com>
2020-06-08 17:01:58 +0200

    linux_5_7: init at 5.7.1
    Changes:
    - Copied linux-5.7.nix from linux-5.6.nix
    - Add linux_5_7 and linuxPackages_5_7
    - Update linux_latest to 5.7
    
    Note:
    The kernel patch 'kernelPatches.export_kernel_fpu_functions."5.3"' is
    still applied as I copied the list from linux_5_7 (vs. linux_testing).
    This patch is probably still required for the ZFS performance.
    
    (cherry picked from commit 19b2efbc3921c967d8260bf246391a93aed30874)

---
+++ pkgs/os-specific/linux/kernel/linux-5.7.nix
@@ -0,0 +1,18 @@
+{ stdenv, buildPackages, fetchurl, perl, buildLinux, modDirVersionArg ? null, ... } @ args:
+
+with stdenv.lib;
+
+buildLinux (args // rec {
+  version = "5.7.1";
+
+  # modDirVersion needs to be x.y.z, will automatically add .0 if needed
+  modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
+
+  # branchVersion needs to be x.y
+  extraMeta.branch = versions.majorMinor version;
+
+  src = fetchurl {
+    url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
+    sha256 = "1vcxrrb2i4366iciw0mfahwbdrzmhrrsr7gi4vdkzznfv2niils0";
+  };
+} // (args.argsOverride or {}))
