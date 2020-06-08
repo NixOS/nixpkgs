4942204374b181c0b203c8a16fd617973bdac6f1
Jonathan Ringer <jonringer117@gmail.com>
2020-06-08 11:53:41 -0700

    python3Packages.simpy: fix

--- pkgs/development/python-modules/simpy/default.nix
+++ pkgs/development/python-modules/simpy/default.nix
@@ -1,4 +1,4 @@
-{ buildPythonPackage, fetchPypi, lib }:
+{ buildPythonPackage, fetchPypi, lib, setuptools_scm, pytestCheckHook }:
 
 buildPythonPackage rec {
   pname = "simpy";
@@ -8,6 +8,9 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "b36542e2faab612f861c5ef4da17220ac1553f5892b3583c67281dbe4faad404";
   };
+
+  nativeBuildInputs = [ setuptools_scm ];
+  checkInputs = [ pytestCheckHook ];
 
   meta = with lib; {
     homepage = "https://simpy.readthedocs.io/en/latest/";
