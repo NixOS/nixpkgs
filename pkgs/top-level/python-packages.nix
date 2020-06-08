5664b31437c3fc43611cac2c3eb01a36282cce4a
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-06-08 03:39:06 +0200

    python3Packages.clldutils: init at 3.5.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1186,6 +1186,8 @@ in {
 
   clize = callPackage ../development/python-modules/clize { };
 
+  clldutils = callPackage ../development/python-modules/clldutils { };
+
   closure-linter = callPackage ../development/python-modules/closure-linter { };
 
   cloudflare = callPackage ../development/python-modules/cloudflare { };
