a4386dbd7e7512a542c496788636308dff5e2783
Jonathan Ringer <jonringer117@gmail.com>
2020-06-08 11:58:33 -0700

    python3Packages.ujson: add missing setuptools_scm dependency

--- pkgs/development/python-modules/ujson/default.nix
+++ pkgs/development/python-modules/ujson/default.nix
@@ -2,6 +2,7 @@
 , buildPythonPackage
 , fetchPypi
 , isPyPy
+, setuptools_scm
 }:
 
 buildPythonPackage rec {
@@ -13,6 +14,8 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "e0199849d61cc6418f94d52a314c6a27524d65e82174d2a043fb718f73d1520d";
   };
+
+  nativeBuildInputs = [ setuptools_scm ];
 
   meta = with stdenv.lib; {
     homepage = "https://pypi.python.org/pypi/ujson";
