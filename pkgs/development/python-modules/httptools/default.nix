3e597721fd809f6d6e90d69d665e3138064311e6
Jonathan Ringer <jonringer117@gmail.com>
2020-06-08 11:49:31 -0700

    python38Packages.httptools: disable tests, no longer on pypi

--- pkgs/development/python-modules/httptools/default.nix
+++ pkgs/development/python-modules/httptools/default.nix
@@ -12,6 +12,11 @@ buildPythonPackage rec {
     sha256 = "41b573cf33f64a8f8f3400d0a7faf48e1888582b6f6e02b82b9bd4f0bf7497ce";
   };
 
+  # tests are not included in pypi tarball
+  doCheck = false;
+
+  pythonImportsCheck = [ "httptools" ];
+
   meta = with lib; {
     description = "A collection of framework independent HTTP protocol utils";
     homepage = "https://github.com/MagicStack/httptools";
