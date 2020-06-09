69c46438cf75f8578264385b1da7348ff6a9de07
Pasquale <p3dimaria@hotmail.it>
2020-06-09 17:52:44 +0200

    nixos/thefuck: run thefuck on system build only

--- nixos/modules/programs/thefuck.nix
+++ nixos/modules/programs/thefuck.nix
@@ -6,8 +6,8 @@ let
   prg = config.programs;
   cfg = prg.thefuck;
 
-  initScript = ''
-    eval $(${pkgs.thefuck}/bin/thefuck --alias ${cfg.alias})
+  initScript = pkgs.runCommand "fuck.sh"{}''
+    ${pkgs.thefuck}/bin/thefuck --alias ${cfg.alias} > $out
   '';
 in
   {
@@ -30,10 +30,10 @@ in
     config = mkIf cfg.enable {
       environment.systemPackages = with pkgs; [ thefuck ];
 
-      programs.bash.interactiveShellInit = initScript;
-      programs.zsh.interactiveShellInit = mkIf prg.zsh.enable initScript;
-      programs.fish.interactiveShellInit = mkIf prg.fish.enable ''
-        ${pkgs.thefuck}/bin/thefuck --alias | source
-      '';
+      environment.shellFiles =  [ {
+        bash_interactiveShellInit = initScript;
+        fish_interactiveShellInit = initScript;
+        zsh_interactiveShellInit = initScript;
+      } ];
     };
   }
