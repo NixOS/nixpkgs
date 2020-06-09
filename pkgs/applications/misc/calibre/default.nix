55a0edb3543e2744553d1f10870bca281951e15e
R. RyanTM <ryantm-bot@ryantm.com>
2020-06-09 09:44:38 +0000

    calibre: 4.17.0 -> 4.18.0

--- pkgs/applications/misc/calibre/default.nix
+++ pkgs/applications/misc/calibre/default.nix
@@ -30,11 +30,11 @@ let
 in
 mkDerivation rec {
   pname = "calibre";
-  version = "4.17.0";
+  version = "4.18.0";
 
   src = fetchurl {
     url = "https://download.calibre-ebook.com/${version}/${pname}-${version}.tar.xz";
-    sha256 = "1qgzx2q8a5g102z311ibz4aw1ipn2j1lbndgadig7xvy6zdhknma";
+    sha256 = "0w9pcfvskjh4v00vjw3i6hzrafy863pgsmmqdx4lffip3p856brw";
   };
 
   patches = [
