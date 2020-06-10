70036d8b2bd7402d5b6f018173fc928d2092b2d6
Dominik Michael Rauh <dmrauh@posteo.de>
2020-06-10 19:34:38 +0200

    gretl: init at 2020b

---
+++ pkgs/applications/science/math/gretl/default.nix
@@ -0,0 +1,44 @@
+{ stdenv, fetchurl, curl, fftw, gmp, gnuplot, gtk3, gtksourceview3, json-glib
+, lapack, libxml2, mpfr, openblas, pkg-config, readline }:
+
+stdenv.mkDerivation rec {
+  pname = "gretl";
+  version = "2020b";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/gretl/${pname}-${version}.tar.xz";
+    sha256 = "0mpb8gc0mcfql8lzwknpkf1sg7mj9ikzd8r1x5xniabd9mmdhplm";
+  };
+
+  buildInputs = [
+    curl
+    fftw
+    gmp
+    gnuplot
+    gtk3
+    gtksourceview3
+    json-glib
+    lapack
+    libxml2
+    mpfr
+    openblas
+    readline
+  ];
+
+  nativeBuildInputs = [ pkg-config ];
+
+  enableParallelBuilding = true;
+
+  meta = with stdenv.lib; {
+    description = "A software package for econometric analysis";
+    longDescription = ''
+      gretl is a cross-platform software package for econometric analysis,
+      written in the C programming language.
+    '';
+    homepage = "http://gretl.sourceforge.net";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ dmrauh ];
+    platforms = with platforms; all;
+  };
+}
+
