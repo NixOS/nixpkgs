852b7951f2ab463a3a1e52d195f638c6f04955c3
Yurii Rashkovskii <yrashk@gmail.com>
2020-06-10 06:29:04 -0700

    lattice-diamond: 3.10 -> 3.11sp2

--- pkgs/development/tools/lattice-diamond/default.nix
+++ pkgs/development/tools/lattice-diamond/default.nix
@@ -1,21 +1,35 @@
 { stdenv, rpmextract, patchelf, makeWrapper, file, requireFile, glib, zlib, 
-    freetype, fontconfig, xorg }:
+    freetype, fontconfig, xorg, libusb-compat-0_1, tcsh, coreutils, libuuid }:
 
 stdenv.mkDerivation {
-  name = "diamond-3.10";
+  name = "diamond-3.11-sp2";
 
-  nativeBuildInputs = [ rpmextract patchelf makeWrapper file ];
+  nativeBuildInputs = [ rpmextract patchelf makeWrapper file tcsh ];
 
   src = requireFile {
-    name = "diamond_3_10-base_x64-111-2-x86_64-linux.rpm";
-    url = "http://www.latticesemi.com/view_document?document_id=52180";
-    sha256 = "ec0b370cf8bd55831eeed7c5eadcabacbd6e63ac657c20209d672119a07a5c0f";
+    name = "diamond_3_11-base_x64-396-4-x86_64-linux.rpm";
+    url = "https://www.latticesemi.com/view_document?document_id=52662";
+    sha256 = "1pik6w1qig28ri5klvy1if5gryin21iscz21vj3j7hfhbz4m8mka";
+  };
+
+  sp1 = requireFile {
+    name = "diamond_3_11-sp1_x64-441-0-x86_64-linux.rpm";
+    url = "https://www.latticesemi.com/view_document?document_id=52742";
+    sha256 = "1basdw6xiz8lc1a1d1q5dvnpc5586p2mjhravyvchx9nnsqmcbhb";
+  };
+
+  sp2 = requireFile {
+    name = "diamond_3_11-sp2_x64-446-3-x86_64-linux.rpm";
+    url = "https://www.latticesemi.com/view_document?document_id=52844";
+    sha256 = "1bqymza3kl1r2n0gippcd4zpcflcn6cpk2kdzliqkisrn8s45r9p";
   };
 
   buildCommand = ''
-    origprefix=usr/local/diamond/3.10_x64
-    prefix=diamond
-    
+    origprefix=usr/local/diamond/3.11_x64
+    prefix=diamond/3.11_x64
+
+    pushd $(pwd)
+
     echo "Unpacking $src..."
     rpmextract $src
     
@@ -42,7 +56,20 @@ stdenv.mkDerivation {
         tar xf $(basename $tb)
         rm $(basename $tb)
     done
-    
+
+    popd
+
+    for sp in $sp1 $sp2 ; do
+        echo "Unpacking $sp..."
+        rpmextract $sp
+        substituteInPlace $origprefix/sp/cp_pack --replace "/bin/csh" "${tcsh}/bin/tcsh"
+        substituteInPlace $origprefix/sp/cp_pack --replace rpm echo
+        substituteInPlace $origprefix/sp/cp_pack --replace '"$temp" == "$temp1"' '"1" == "0"'
+        mv $origprefix/* $out/$prefix
+        $out/$prefix/sp/cp_pack 3.11 $out $out
+        rm -rf $out/$prefix/sp
+    done
+
     # Patch shebangs in start scripts .
     cd $out/$prefix/bin/lin64
     for tool in \
@@ -64,9 +91,11 @@ stdenv.mkDerivation {
         echo "Patching script $prefix/bin/lin64/$tool..."
         patchShebangs $tool
     done
-    
+
+    substituteInPlace $out/$prefix/synpbase/bin/config/platform_set --replace "/usr/bin/id" "${coreutils}/bin/id"
+
     # Patch executable ELFs.
-    for path in bin/lin64 ispfpga/bin/lin64; do
+    for path in bin/lin64 ispfpga/bin/lin64 synpbase/linux_a_64/mbin; do
         cd $out/$prefix/$path
         for f in *; do
             if ! file $f | grep -q "ELF 64-bit LSB executable" ; then
@@ -85,6 +114,7 @@ stdenv.mkDerivation {
     
     # Remove 32-bit libz.
     rm $out/$prefix/bin/lin64/libz.{so,so.1}
+    rm $out/$prefix/bin/lin64/libstdc++.so.{6,6.0.18}
     
     # Make wrappers (should these target more than the 'diamond' tool?).
     # The purpose of these is just to call the target program using its
@@ -97,7 +127,8 @@ stdenv.mkDerivation {
 
   libPath = stdenv.lib.makeLibraryPath [
     glib zlib freetype fontconfig
-    xorg.libSM xorg.libICE xorg.libXrender xorg.libXext xorg.libX11 xorg.libXt
+    xorg.libSM xorg.libICE xorg.libXrender xorg.libXext xorg.libX11 xorg.libXt xorg.libXScrnSaver
+    libusb-compat-0_1 stdenv.cc.cc.lib libuuid
   ];
 
   meta = {
