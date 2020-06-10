54189324fc98b88f296ebfc6a184cc7ef7857e08
Jörg Thalheim <joerg@thalheim.io>
2020-06-10 07:49:56 +0100

    python3.pkgs.zimports: add missing setuptools

--- pkgs/development/python-modules/zimports/default.nix
+++ pkgs/development/python-modules/zimports/default.nix
@@ -5,6 +5,7 @@
 , flake8-import-order
 , pyflakes
 , mock
+, setuptools
 }:
 
 buildPythonPackage rec {
@@ -23,11 +24,20 @@ buildPythonPackage rec {
   propagatedBuildInputs = [
     pyflakes
     flake8-import-order
+    setuptools
   ];
 
   checkInputs = [
     mock
   ];
+
+  doInstallCheck = true;
+
+  installCheckPhase = ''
+    runHook preInstallCheck
+    PYTHONPATH= $out/bin/zimports --help >/dev/null
+    runHook postInstallCheck
+  '';
 
   meta = with lib; {
     description = "Python import rewriter";
