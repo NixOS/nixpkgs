61ec84f7f97f63778091faf08ae853ae30dd465b
Maximilian Bosch <maximilian@mbosch.me>
2020-06-10 22:36:15 +0200

    python3Packages.mautrix: 0.5.0 -> 0.5.4

--- pkgs/development/python-modules/mautrix/default.nix
+++ pkgs/development/python-modules/mautrix/default.nix
@@ -4,11 +4,11 @@
 
 buildPythonPackage rec {
   pname = "mautrix";
-  version = "0.5.0";
+  version = "0.5.4";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0hcm2hwryfr6js33zcl2k95wbjrgcj89pi90lka0hjw9vs9bmdz6";
+    sha256 = "0csvk3y0y2r9gnfqj91fiqprgp8dxiv4n80b6myraab5s7zn1mvv";
   };
 
   propagatedBuildInputs = [
