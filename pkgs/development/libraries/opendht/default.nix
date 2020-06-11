9317014be174796c41e8bc827bbfa2e6666b5fed
R. RyanTM <ryantm-bot@ryantm.com>
2020-06-11 00:25:45 +0000

    opendht: 2.1.1 -> 2.1.3

--- pkgs/development/libraries/opendht/default.nix
+++ pkgs/development/libraries/opendht/default.nix
@@ -5,13 +5,13 @@
 
 stdenv.mkDerivation rec {
   pname = "opendht";
-  version = "2.1.1";
+  version = "2.1.3";
 
   src = fetchFromGitHub {
     owner = "savoirfairelinux";
     repo = "opendht";
     rev = version;
-    sha256 = "10sbiwjljxi0a1q3xakmf6v02x3yf38ljvjpql70q4rqggqj9zhh";
+    sha256 = "0qz9mdkanz4vd38bdpwvcwvbpzf19i6019rmqw3k1vh5mh22ljhr";
   };
 
   nativeBuildInputs =
