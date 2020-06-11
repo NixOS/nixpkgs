6594184e1e326ffcacbb47e8c12eb8e765826365
Frederik Rietdijk <fridh@fridh.nl>
2020-06-11 14:13:08 +0200

    python3.pkgs.pytest-pylint: fix build

--- pkgs/development/python-modules/pytest-pylint/default.nix
+++ pkgs/development/python-modules/pytest-pylint/default.nix
@@ -6,6 +6,7 @@
 , pylint
 , six
 , pytestrunner
+, toml
 }:
 
 buildPythonPackage rec {
@@ -18,12 +19,13 @@ buildPythonPackage rec {
     sha256 = "b0c177d63f6e3f5b82fa2720a6570dd2ecff1616c26ed6d02d0cbf75fd98ddf9";
   };
 
-  buildInputs = [ pytestrunner ];
+  nativeBuildInputs = [ pytestrunner ];
 
   propagatedBuildInputs = [
     pytest
     pylint
     six
+    toml
   ];
 
   # tests not included with release
