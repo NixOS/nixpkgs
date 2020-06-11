418b67bad858dc064567cb783b8dcc89301e1c44
Frederik Rietdijk <fridh@fridh.nl>
2020-06-11 11:48:09 +0200

    python.pkgs.lazy: init at 1.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3069,6 +3069,8 @@ in {
 
   latexcodec = callPackage ../development/python-modules/latexcodec {};
 
+  lazy = callPackage ../development/python-modules/lazy { };
+
   libmodulemd = pipe pkgs.libmodulemd [
     toPythonModule
 
