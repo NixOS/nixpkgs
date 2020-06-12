e618b22356875a5798a80490b9bdc290d8cdb9e2
Peter Simons <simons@cryp.to>
2020-06-12 21:50:53 +0200

    all-cabal-hashes: update to Hackage at 2020-06-12T00:09:36Z

--- pkgs/data/misc/hackage/default.nix
+++ pkgs/data/misc/hackage/default.nix
@@ -1,6 +1,6 @@
 { fetchurl }:
 
 fetchurl {
-  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/e8df5568f80e6230e29c2381e842db35fe11cd71.tar.gz";
-  sha256 = "1fz4iax88pmlqpb4zp3l6mb6bmkzzha0q6mm3xasabh5yl83dbhy";
+  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/73b209cba2ed84fa95c0fdde9616b4c5f983855f.tar.gz";
+  sha256 = "0sm3kqd3blvk18frjfx1nkrbfpds780qv63rkcl5fp5f1z02lhv3";
 }
