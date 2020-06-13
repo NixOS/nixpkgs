NixOS/nixpkgs

commit 7f76daacdb0190ce91c95111cc3eba49cea41fd7

zstd: get rid of static libs if enableShared

pkgs/tools/compression/zstd/default.nix
@@ -30,6 +30,7 @@ stdenv.mkDerivation rec {
 
   cmakeFlags = [
     "-DZSTD_BUILD_SHARED:BOOL=${if enableShared then "ON" else "OFF"}"
+    # They require STATIC for bin/zstd and tests.
     "-DZSTD_LEGACY_SUPPORT:BOOL=${if legacySupport then "ON" else "OFF"}"
     "-DZSTD_BUILD_TESTS:BOOL=ON"
   ];
@@ -54,6 +55,8 @@ stdenv.mkDerivation rec {
     substituteInPlace ../programs/zstdless \
       --replace "zstdcat" "$bin/bin/zstdcat"
   '';
+  # Don't duplicate the library code in runtime closures.
+  postInstall = stdenv.lib.optionalString enableShared ''rm "$out"/lib/libzstd.a'';
 
   outputs = [ "bin" "dev" "man" "out" ];
 

commit 9d1db4bf7df68262b344a31e285789e33427c0b8

zstd: install static libraries to dev
Achieved by patching cmake "install()" directive to ensure that
$dev/lib/cmake is generated with correct paths.

If we don't want to build static libraries we should be setting
ZSTD_BUILD_STATIC to OFF instead.

Fixes: 7f76daac ('zstd: get rid of static libs if enableShared')

pkgs/tools/compression/zstd/default.nix
@@ -28,6 +28,12 @@ stdenv.mkDerivation rec {
     # work fine, and I'm not sure how to write the condition.
     ++ stdenv.lib.optional stdenv.hostPlatform.isWindows ./mcfgthreads-no-pthread.patch;
 
+  postPatch = ''
+    substituteInPlace build/cmake/lib/CMakeLists.txt \
+      --replace 'ARCHIVE DESTINATION "''${CMAKE_INSTALL_LIBDIR}"' \
+                "ARCHIVE DESTINATION \"''${!outputDev}/lib\""
+  '';
+
   cmakeFlags = [
     "-DZSTD_BUILD_SHARED:BOOL=${if enableShared then "ON" else "OFF"}"
     # They require STATIC for bin/zstd and tests.
@@ -56,8 +62,6 @@ stdenv.mkDerivation rec {
     substituteInPlace ../programs/zstdless \
       --replace "zstdcat" "$bin/bin/zstdcat"
   '';
-  # Don't duplicate the library code in runtime closures.
-  postInstall = stdenv.lib.optionalString enableShared ''rm "$out"/lib/libzstd.a'';
 
   outputs = [ "bin" "dev" "man" "out" ];
 