1c61d8707ac8ecf76a656255ee5bb6df978894dd
Benjamin Asbach <asbachb@users.noreply.github.com>
2020-06-13 11:52:46 +0000

    openjdk: added myself as maintainer

--- pkgs/development/compilers/openjdk/11.nix
+++ pkgs/development/compilers/openjdk/11.nix
@@ -135,7 +135,7 @@ let
       homepage = "http://openjdk.java.net/";
       license = licenses.gpl2;
       description = "The open-source Java Development Kit";
-      maintainers = with maintainers; [ edwtjo ];
+      maintainers = with maintainers; [ edwtjo asbachb ];
       platforms = [ "i686-linux" "x86_64-linux" "aarch64-linux" "armv7l-linux" "armv6l-linux" ];
     };
 
