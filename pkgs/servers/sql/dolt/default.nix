14a2d4a3f1d373ef602333452b93a0d8fd4f43b9
Nikolay Korotkiy <sikmir@gmail.com>
2020-06-14 15:24:16 +0300

    dolt: 0.16.3 -> 0.17.2

--- pkgs/servers/sql/dolt/default.nix
+++ pkgs/servers/sql/dolt/default.nix
@@ -1,25 +1,25 @@
 { stdenv, fetchFromGitHub, lib, buildGoModule }:
 
 buildGoModule rec {
-    pname = "dolt";
-    version = "0.16.3";
+  pname = "dolt";
+  version = "0.17.2";
 
-    src = fetchFromGitHub {
-        owner = "liquidata-inc";
-        repo = "dolt";
-        rev = "v${version}";
-        sha256 = "141wv5av7hms4wa3s4md7mnb77bbyn3854d7gj7fy6f6jvzghdny";
-    };
+  src = fetchFromGitHub {
+    owner = "liquidata-inc";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "1fdl18lmdg4wcbf017p963rbq9masm0942vhxam9hxvsvpx08i6y";
+  };
 
-    modRoot = "./go";
-    subPackages = [ "cmd/dolt" "cmd/git-dolt" "cmd/git-dolt-smudge" ];
-  vendorSha256 = "1kjh252p91yxq5mi0igamkwlsr50lij24wsp2ilgz6ksv3ijzfr1";
+  modRoot = "./go";
+  subPackages = [ "cmd/dolt" "cmd/git-dolt" "cmd/git-dolt-smudge" ];
+  vendorSha256 = "1nsfkxq064lhy0kibf7p2x6zbbk2pb0hbph41bwlmbij204wrawh";
 
-    meta = with lib; {
-        description = "Relational database with version control and CLI a-la Git.";
-        homepage = "https://github.com/liquidata-inc/dolt";
-        license = licenses.asl20;
-        maintainers = with maintainers; [ danbst ];
-        platforms = platforms.linux ++ platforms.darwin;
-    };
+  meta = with lib; {
+    description = "Relational database with version control and CLI a-la Git.";
+    homepage = "https://github.com/liquidata-inc/dolt";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ danbst ];
+    platforms = with platforms; linux ++ darwin;
+  };
 }
