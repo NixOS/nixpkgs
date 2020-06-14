37bb17cdd498e85ac76bb9e946a351de1204d9bb
Daniel Fullmer <danielrf12@gmail.com>
2020-06-14 17:25:42 -0400

    OVMF: add symlinks for Fedora dir layout

--- pkgs/applications/virtualization/OVMF/default.nix
+++ pkgs/applications/virtualization/OVMF/default.nix
@@ -38,13 +38,17 @@ edk2.mkDerivation projectDscPath {
 
   postFixup = if stdenv.isAarch64 then ''
     mkdir -vp $fd/FV
+    mkdir -vp $fd/AAVMF
     mv -v $out/FV/QEMU_{EFI,VARS}.fd $fd/FV
 
     # Use Debian dir layout: https://salsa.debian.org/qemu-team/edk2/blob/debian/debian/rules
-    # Note that Fedora dir layout is different: https://src.fedoraproject.org/cgit/rpms/edk2.git/tree/edk2.spec
     dd of=$fd/FV/AAVMF_CODE.fd  if=/dev/zero bs=1M    count=64
     dd of=$fd/FV/AAVMF_CODE.fd  if=$fd/FV/QEMU_EFI.fd conv=notrunc
     dd of=$fd/FV/AAVMF_VARS.fd  if=/dev/zero bs=1M    count=64
+
+    # Also add symlinks for Fedora dir layout: https://src.fedoraproject.org/cgit/rpms/edk2.git/tree/edk2.spec
+    ln -s $fd/FV/AAVMF_CODE.fd $fd/AAVMF/QEMU_EFI-pflash.raw
+    ln -s $fd/FV/AAVMF_VARS.fd $fd/AAVMF/vars-template-pflash.raw
   '' else ''
     mkdir -vp $fd/FV
     mv -v $out/FV/OVMF{,_CODE,_VARS}.fd $fd/FV
