b52d80d198af31bf0810e24621b43603027a194b
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2020-06-14 13:13:55 +0200

    gosec: 1.2.0 -> 2.3.0

--- pkgs/development/tools/gosec/default.nix
+++ pkgs/development/tools/gosec/default.nix
@@ -1,29 +1,28 @@
-{ buildGoPackage
-, lib
-, fetchFromGitHub
-}:
+{ stdenv, buildGoModule, fetchFromGitHub }:
 
-buildGoPackage rec {
+buildGoModule rec {
   pname = "gosec";
-  version = "1.2.0";
+  version = "2.3.0";
 
   goPackagePath = "github.com/securego/gosec";
-  excludedPackages = ''cmd/tlsconfig'';
+
+  subPackages = [ "cmd/gosec" ];
 
   src = fetchFromGitHub {
     owner = "securego";
-    repo = "gosec";
-    rev = version;
-    sha256 = "1420yl4cjp4v4xv0l0wbahgl6bjhz77lx5va9hqa6abddmqvx1hg";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "0z782rr4z0nhlj6cmjd17pbi65zabpmb83mv4y93wi4qa7kkpm2g";
   };
 
-  goDeps = ./deps.nix;
+  vendorSha256 = "0zrmhqcid8xr6i1xxg3s8sll8a667w2vmn5asdw0b43k6k3h941p";
 
-  meta = with lib; {
-    description = "Golang security checker";
+  meta = with stdenv.lib; {
     homepage = "https://github.com/securego/gosec";
-    license = licenses.asl20 ;
-    maintainers = with maintainers; [ kalbasit ];
+    description = "Golang security checker";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ kalbasit nilp0inter ];
     platforms = platforms.linux ++ platforms.darwin;
   };
 }
+
