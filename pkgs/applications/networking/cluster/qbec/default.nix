7304a4ae4ba63220d2974530e37e339c0f00bdbe
Greg Roodt <greg@canva.com>
2020-06-15 20:51:21 +1000

    qbec: add ldflags

--- pkgs/applications/networking/cluster/qbec/default.nix
+++ pkgs/applications/networking/cluster/qbec/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{ lib, go, buildGoModule, fetchFromGitHub }:
 
 buildGoModule rec {
   pname = "qbec";
@@ -12,6 +12,14 @@ buildGoModule rec {
   };
 
   vendorSha256 = "15hbjghi2ifylg7nr85qlk0alsy97h9zj6hf5w84m76dla2bcjf3";
+
+  buildFlagsArray = ''
+    -ldflags=
+      -s -w
+      -X github.com/splunk/qbec/internal/commands.version=${version}
+      -X github.com/splunk/qbec/internal/commands.commit=${src.rev}
+      -X github.com/splunk/qbec/internal/commands.goVersion=${lib.getVersion go}
+  '';
 
   meta = with lib; {
     description = "Configure kubernetes objects on multiple clusters using jsonnet https://qbec.io";
