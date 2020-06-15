f9e5ca8e58d6cddb182cb155be079829898d1a2d
zowoq <59103226+zowoq@users.noreply.github.com>
2020-06-16 08:18:35 +1000

    ffmpeg: 4.2.3 -> 4.3

--- pkgs/development/libraries/ffmpeg/4.nix
+++ pkgs/development/libraries/ffmpeg/4.nix
@@ -5,8 +5,8 @@
 }@args:
 
 callPackage ./generic.nix (rec {
-  version = "4.2.3";
-  branch = "4.2";
-  sha256 = "0pkrariwjv25k7inwshch7b5820ly3hsp991amyb60rkqc8v4zi1";
+  version = "4.3";
+  branch = "4.3";
+  sha256 = "1qnnhd2b0g5sg72pclxs3i8sxzz0raky69k7w9cmpba9zh973s57";
   darwinFrameworks = [ Cocoa CoreMedia VideoToolbox ];
 } // args)
