9e96c714b1d9c7f90b79e6398ef873b84ce1cdb9
Will Dietz <w@wdtz.org>
2020-06-16 11:23:02 -0500

    cascadia-code: 1911.21 -> 2005.15
    Now with OTF variants as well, all released in single zip.
    
    https://github.com/microsoft/cascadia-code/releases/tag/v2004.30
    https://github.com/microsoft/cascadia-code/releases/tag/v2005.15

--- pkgs/data/fonts/cascadia-code/default.nix
+++ pkgs/data/fonts/cascadia-code/default.nix
@@ -1,42 +1,19 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchzip }:
 
 stdenv.mkDerivation rec {
   pname = "cascadia-code";
-  version = "1911.21";
+  version = "2005.15";
 
-  srcs = [
-    (fetchurl {
-      url = "https://github.com/microsoft/cascadia-code/releases/download/v${version}/Cascadia.ttf";
-      sha256 = "1m5ymbngjg3n1g3p6vhcq7d825bwwln9afih651ar3jn7j9njnyg";
-     })
-    (fetchurl {
-      url = "https://github.com/microsoft/cascadia-code/releases/download/v${version}/CascadiaMono.ttf";
-      sha256 = "0vkhm6rhspzd1iayxrzaag099wsc94azfqa3ips7f4x9s8fmbp80";
-    })
-    (fetchurl {
-      url = "https://github.com/microsoft/cascadia-code/releases/download/v${version}/CascadiaMonoPL.ttf";
-      sha256 = "0xxqd8m2ydn97jngp1a3ik1mzpjbm65pfq02a82gfbbvajq5d673";
-    })
-    (fetchurl {
-      url = "https://github.com/microsoft/cascadia-code/releases/download/v${version}/CascadiaPL.ttf";
-      sha256 = "1s83c9flvifd05nbhnk8knwnik7p621sr7i94smknigc7d72wqav";
-    })
-  ];
-
-  unpackCmd = ''
-    ttfName=$(basename $(stripHash $curSrc))
-    cp $curSrc ./$ttfName
-  '';
-
-  sourceRoot = ".";
+  src = fetchzip {
+    url = "https://github.com/microsoft/cascadia-code/releases/download/v${version}/CascadiaCode_${version}.zip";
+    sha256 = "0wm8lqhgkz691w6ai6r45c7199p7bpr00rm8nz4ynafrb15fgm6v";
+    stripRoot = false;
+  };
 
   installPhase = ''
-    install -Dm444 -t $out/share/fonts/truetype *.ttf
+    install -Dm444 -t $out/share/fonts/truetype ttf/*.ttf
+    install -Dm444 -t $out/share/fonts/opentype otf/*.otf
   '';
-
-  outputHashAlgo = "sha256";
-  outputHashMode = "recursive";
-  outputHash = "1gkjs7qa409r4ykdy4ik8i0c3z49hzpklw6kyijhhifhyyyzhz4h";
 
   meta = with stdenv.lib; {
     description = "Monospaced font that includes programming ligatures and is designed to enhance the modern look and feel of the Windows Terminal";
