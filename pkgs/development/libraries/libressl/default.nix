7903e4b4e04258eb224d1c3b78e5ae0d8e971354
Ruud van Asseldonk <dev@veniogames.com>
2020-06-16 21:01:49 +0200

    libressl_3_1: 3.1.2 -> 3.1.3

--- pkgs/development/libraries/libressl/default.nix
+++ pkgs/development/libraries/libressl/default.nix
@@ -69,7 +69,7 @@ in {
   };
 
   libressl_3_1 = generic {
-    version = "3.1.2";
-    sha256 = "14nqg34yc9bm64hz96hhlvm00gwn2acjs0hcwhs9l50plrz2z2pq";
+    version = "3.1.3";
+    sha256 = "184znscbkww65aavy2p4v4xncalp1ni19c2w5yvfq4pnmhb06sy7";
   };
 }
