cd31f0d503e6b3c95267a22cfdcead0e3ad0f54e
Illia Shestakov <illia@ishestakov.com>
2020-06-16 22:51:17 +0300

    utahfs: init at 1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3207,6 +3207,8 @@ in
 
   vorta = python3Packages.callPackage ../applications/backup/vorta { };
 
+  utahfs = callPackage ../applications/networking/utahfs { };
+
   wallutils = callPackage ../tools/graphics/wallutils { };
 
   wev = callPackage ../tools/misc/wev { };
