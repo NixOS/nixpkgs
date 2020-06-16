c2eb932e31a6821a20791aa750dc7e67cb0d6113
Dominik Michael Rauh <dmrauh@posteo.de>
2020-06-16 12:59:36 +0200

    simpy: add missing dependencies

--- pkgs/development/python-modules/simpy/default.nix
+++ pkgs/development/python-modules/simpy/default.nix
@@ -1,4 +1,5 @@
-{ buildPythonPackage, fetchPypi, lib }:
+{ buildPythonPackage, fetchPypi, lib, pytestCheckHook, setuptools
+, setuptools_scm }:
 
 buildPythonPackage rec {
   pname = "simpy";
@@ -9,10 +10,16 @@ buildPythonPackage rec {
     sha256 = "dd8c16ca3cff1574c99fe9f5ea4019c631c327f2bdc842e8b1a5c55f5e3e9d27";
   };
 
+  nativeBuildInputs = [ setuptools_scm setuptools ];
+
+  propagatedBuildInputs = [ setuptools ];
+
+  checkInputs = [ pytestCheckHook ];
+
   meta = with lib; {
     homepage = "https://simpy.readthedocs.io/en/latest/";
     description = "A process-based discrete-event simulation framework based on standard Python.";
     license = [ licenses.mit ];
-    maintainers = with maintainers; [ shlevy ];
+    maintainers = with maintainers; [ dmrauh shlevy ];
   };
 }
