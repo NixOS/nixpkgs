6460602eec5ced5b9720f4d9fdb0dd717b89b4fd
Atemu <atemu.main@gmail.com>
2020-06-16 19:25:07 +0200

    gns3-gui: Add the missing qt5Full runtime dependency
    qt5Full may not be installed on users' systems and the gns3-gui depends
    on it explicitly.
    Note: This also fixes e.g. "nix-shell -p gns3-gui --pure" (at the cost
    of an increased closure size).
    
    (cherry picked from commit 0eaec4dee272d9dc67cee9599a41fcac1039845a)
    Reason: This is a fix for possible runtime crashes.

--- pkgs/applications/networking/gns3/gui.nix
+++ pkgs/applications/networking/gns3/gui.nix
@@ -1,9 +1,8 @@
 { stable, branch, version, sha256Hash, mkOverride }:
 
-{ lib, stdenv, python3, fetchFromGitHub }:
+{ lib, stdenv, python3, pkgs, fetchFromGitHub }:
 
 let
-  # TODO: This package requires qt5Full to launch
   defaultOverrides = [
     (mkOverride "psutil" "5.6.3"
       "1wv31zly44qj0rp2acg58xbnc7bf6ffyadasq093l455q30qafl6")
@@ -29,6 +28,7 @@ in python.pkgs.buildPythonPackage rec {
     raven psutil jsonschema # tox for check
     # Runtime dependencies
     sip (pyqt5.override { withWebSockets = true; }) distro setuptools
+    pkgs.qt5Full
   ];
 
   doCheck = false; # Failing
