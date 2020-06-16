5ae3e2749486a2c260fae88b0c06580b5b028958
Jörg Thalheim <joerg@thalheim.io>
2020-06-17 00:15:29 +0100

    python3.pkgs.httpbin: 0.6.2 -> 0.7.0

--- pkgs/development/python-modules/httpbin/default.nix
+++ pkgs/development/python-modules/httpbin/default.nix
@@ -3,7 +3,6 @@
 , fetchPypi
 , fetchpatch
 , flask
-, flask-common
 , flask-limiter
 , markupsafe
 , decorator
@@ -15,24 +14,14 @@
 
 buildPythonPackage rec {
   pname = "httpbin";
-  version = "0.6.2";
+  version = "0.7.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0afa0486a76305cac441b5cc80d5d4ccd82b20875da7c5119ecfe616cefef45f";
+    sha256 = "1yldvf3585zcwj4vxvfm4yr9wwlz3pa2mx2pazqz8x8mr687gcyb";
   };
 
-  patches = [
-    # https://github.com/kennethreitz/httpbin/issues/403
-    # https://github.com/kennethreitz/flask-common/issues/7
-    # https://github.com/evansd/whitenoise/issues/166
-    (fetchpatch {
-      url = "https://github.com/javabrett/httpbin/commit/5735c888e1e51b369fcec41b91670a90535e661e.patch";
-      sha256 = "167h8mscdjagml33dyqk8nziiz3dqbggnkl6agsirk5270nl5f7q";
-    })
-  ];
-
-  propagatedBuildInputs = [ brotlipy flask flask-common flask-limiter markupsafe decorator itsdangerous raven six ];
+  propagatedBuildInputs = [ brotlipy flask flask-limiter markupsafe decorator itsdangerous raven six ];
 
   # No tests
   doCheck = false;
