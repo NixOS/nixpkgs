c3371da0e723c373879ae9be0d98e7815ab85707
Vincent Laporte <Vincent.Laporte@gmail.com>
2020-06-16 08:15:23 +0200

    coqPackages.simple-io: 1.2.0 → 1.3.0

--- pkgs/development/coq-modules/simple-io/default.nix
+++ pkgs/development/coq-modules/simple-io/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchFromGitHub, coq, coq-ext-lib }:
 
 stdenv.mkDerivation rec {
-  version = "1.2.0";
+  version = "1.3.0";
   name = "coq${coq.coq-version}-simple-io-${version}";
   src = fetchFromGitHub {
     owner = "Lysxia";
     repo = "coq-simple-io";
     rev = version;
-    sha256 = "1im1vwp7l7ha8swnhgbih0qjg187n8yx14i003nf6yy7p0ryxc9m";
+    sha256 = "1yp7ca36jyl9kz35ghxig45x6cd0bny2bpmy058359p94wc617ax";
   };
 
   buildInputs = [ coq ] ++ (with coq.ocamlPackages; [ ocaml ocamlbuild ]);
@@ -28,7 +28,7 @@ stdenv.mkDerivation rec {
   };
 
   passthru = {
-    compatibleCoqVersions = v: builtins.elem v [ "8.7" "8.8" "8.9" "8.10" ];
+    compatibleCoqVersions = v: builtins.elem v [ "8.7" "8.8" "8.9" "8.10" "8.11" ];
   };
 
 }
