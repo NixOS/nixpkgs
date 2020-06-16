fd474c0e0c39e5750ffb011751331d1e110f632f
Yannick Markus <ym@ymarkus.dev>
2020-06-16 08:49:27 +0200

    birdtray: fix qttranslations path birdtray crashes not finding the translations without this fix

--- pkgs/applications/misc/birdtray/default.nix
+++ pkgs/applications/misc/birdtray/default.nix
@@ -7,6 +7,7 @@
   , qtbase
   , qttools
   , qtx11extras
+  , qttranslations
 }:
 
 mkDerivation rec {
@@ -20,10 +21,20 @@ mkDerivation rec {
     sha256 = "15l8drdmamq1dpqpj0h9ajj2r5vcs23cx421drvhfgs6bqlzd1hl";
   };
 
+  patches = [
+    # See https://github.com/NixOS/nixpkgs/issues/86054
+    ./fix-qttranslations-path.diff
+  ];
+
   nativeBuildInputs = [ cmake pkgconfig ];
   buildInputs = [
     qtbase qttools qtx11extras
   ];
+
+  postPatch = ''
+    substituteInPlace src/birdtrayapp.cpp \
+      --subst-var-by qttranslations ${qttranslations}
+  '';
 
   meta = with lib; {
     description = "Mail system tray notification icon for Thunderbird";
