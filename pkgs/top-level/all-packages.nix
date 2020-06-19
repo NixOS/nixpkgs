5a345d60d37dd81fceb6153e615da78548f6bcac
Sebastien Bariteau <bariteau@tehama.io>
2020-06-19 07:00:04 -0400

    the-way: init at 0.3.2
    * the-way: init at 0.3.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3147,6 +3147,8 @@ in
 
   teamocil = callPackage ../tools/misc/teamocil { };
 
+  the-way = callPackage ../development/tools/the-way { };
+
   tsm-client = callPackage ../tools/backup/tsm-client { jdk8 = null; };
   tsm-client-withGui = callPackage ../tools/backup/tsm-client { };
 
