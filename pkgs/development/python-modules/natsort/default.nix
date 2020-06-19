9c650efbb0b8f143c4d8140f78cfac81c7e0051a
Frederik Rietdijk <fridh@fridh.nl>
2020-06-19 11:20:35 +0200

    python2.pkgs.natsort: disable

--- pkgs/development/python-modules/natsort/default.nix
+++ pkgs/development/python-modules/natsort/default.nix
@@ -29,6 +29,9 @@ buildPythonPackage rec {
     sha256 = "a633464dc3a22b305df0f27abcb3e83515898aa1fd0ed2f9726c3571a27258cf";
   };
 
+  # Does not support Python 2
+  disabled = !isPy3k;
+
   # testing based on project's tox.ini
   # natsort_keygen has pytest mock issues
   checkPhase = ''
