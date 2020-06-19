d3125b012f222e7593411315b619b0cd5dae6868
Michael Weiss <dev.primeos@gmail.com>
2020-06-20 00:06:26 +0200

    python3Packages.pycryptodome: Fix the cross compilation

--- pkgs/development/python-modules/pycryptodome/default.nix
+++ pkgs/development/python-modules/pycryptodome/default.nix
@@ -9,6 +9,10 @@ buildPythonPackage rec {
     sha256 = "f1add21b6d179179b3c177c33d18a2186a09cc0d3af41ff5ed3f377360b869f2";
   };
 
+  preBuild = ''
+    export LDSHARED="$CC -shared" # Fix cross compilation, defaults to gcc instead of $CC
+  '';
+
   meta = {
     homepage = "https://www.pycryptodome.org/";
     description = "Python Cryptography Toolkit";
