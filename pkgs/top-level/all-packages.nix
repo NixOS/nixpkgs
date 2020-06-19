fcbd2e68355bf28ca9c0ae69af7e75fde1ae8ae7
Sebastien Bariteau <bariteau@tehama.io>
2020-06-19 04:19:34 -0400

    tellico: init at 3.3.0
    * tellico: init at 3.3.0
    
    * fix pversion vs version

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26300,6 +26300,8 @@ in
 
   prow = callPackage ../applications/networking/cluster/prow { };
 
+  tellico = libsForQt5.callPackage ../applications/misc/tellico { };
+
   termpdfpy = python3Packages.callPackage ../applications/misc/termpdf.py {};
 
   inherit (callPackage ../applications/networking/cluster/terraform { })
