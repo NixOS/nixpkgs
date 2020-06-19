35b514443bed56465975aa12e6c7aa2b9b887859
Artur Cygan <arczicygan@gmail.com>
2020-06-19 13:46:44 +0200

    unicorn: 1.0.1 -> 1.0.2-rc4

--- pkgs/development/libraries/unicorn-emu/default.nix
+++ pkgs/development/libraries/unicorn-emu/default.nix
@@ -1,19 +1,19 @@
-{ stdenv, fetchurl, pkgconfig, python }:
+{ stdenv, fetchurl, pkgconfig }:
 
 stdenv.mkDerivation rec {
   pname = "unicorn-emulator";
-  version = "1.0.1";
+  version = "1.0.2-rc4";
 
   src = fetchurl {
     url    = "https://github.com/unicorn-engine/unicorn/archive/${version}.tar.gz";
-    sha256 = "0z01apwmvhvdldm372ww9pjfn45awkw3m90c0h4v0nj0ihmlysis";
+    sha256 = "05w43jq3r97l3c8ggc745ai8m5l93p1b6q6cfp1zwzz6hl5kifiv";
   };
 
   configurePhase = '' patchShebangs make.sh '';
   buildPhase = '' ./make.sh '' + stdenv.lib.optionalString stdenv.isDarwin "macos-universal-no";
   installPhase = '' env PREFIX=$out ./make.sh install '';
 
-  nativeBuildInputs = [ pkgconfig python ];
+  nativeBuildInputs = [ pkgconfig ];
   enableParallelBuilding = true;
 
   meta = {
