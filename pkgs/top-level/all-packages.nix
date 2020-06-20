5924c80b7221a570037cd36ce1951cbb26517c20
Jan Tojnar <jtojnar@gmail.com>
2020-06-20 22:35:04 +0200

    thunderbird-bin: remove GConf dependency
    It is only optionally dlopened by crash reporter to try to get system proxy settings
    but no system stores proxy in GConf these days so it is completely useless.
    
    https://github.com/mozilla/gecko-dev/blob/97c590ed5532a7b382b5b8500c109de8f1d831e3/toolkit/crashreporter/client/crashreporter_gtk_common.cpp#L121-L122

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22484,7 +22484,6 @@ in
   thunderbolt = callPackage ../os-specific/linux/thunderbolt {};
 
   thunderbird-bin = callPackage ../applications/networking/mailreaders/thunderbird-bin {
-    gconf = pkgs.gnome2.GConf;
     inherit (pkgs.gnome2) libgnome libgnomeui;
   };
 
