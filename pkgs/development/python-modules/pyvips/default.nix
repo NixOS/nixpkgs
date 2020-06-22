6e4d4918608b1310318ab4294e03acf3e56a2f4c
ccellado <annplague@gmail.com>
2020-06-22 10:36:51 +0300

    pyvips: some changes and checking

--- pkgs/development/python-modules/pyvips/default.nix
+++ pkgs/development/python-modules/pyvips/default.nix
@@ -1,17 +1,5 @@
-{ buildPythonPackage
-, fetchPypi
-, python
-, pytestrunner
-, gcc
-, glib
-, python3Packages
-, pkgconfig
-, stdenv
-, cython
-, libffi
-, vips
-,
-}:
+{ buildPythonPackage, fetchPypi, python, pytestrunner, gcc, glib
+, python3Packages, pkgconfig, stdenv, cython, libffi, vips, }:
 
 buildPythonPackage rec {
 
@@ -21,39 +9,37 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-      sha256 = "1kknp9nx0vj9bj2vp0rxr177d3aahvjwj2q59pb76wvw2xnm542y";
+    sha256 = "1kknp9nx0vj9bj2vp0rxr177d3aahvjwj2q59pb76wvw2xnm542y";
   };
+
+  outputs = [ "out" "dev" ];
 
   checkInputs = [ pytestrunner pkgconfig ];
 
+  nativeBuildInputs = [ vips.dev libffi gcc glib.dev glib ];
+
   buildInputs = [
-                  vips
-                  vips.dev
-                  libffi
-                  gcc
-                  glib
-                  python3Packages.pytestrunner
-                  python3Packages.pkgconfig
-                  python3Packages.cffi
-                ];
+    python3Packages.pytestrunner
+    python3Packages.pkgconfig
+    python3Packages.cffi
+    vips.dev
+    glib.dev
+  ];
 
-  propagatedBuildInputs = [
-                ];
-
-  # Tests cannot import pyfftw. pyfftw works fine though.
+  propagatedBuildInputs = [ ];
 
   doCheck = false;
 
-  postConfigure = ''
-    export LDFLAGS="-L${vips.dev}/lib"
-    export CFLAGS="-I${vips.dev}/include"
-  '';
+  pythonImportsCheck = [ "pyvips" ];
+
+  postConfigure =
+    "\n    export LD_LIBRARY_PATH=${glib.out}/lib:${vips.out}/lib\n  ";
 
   meta = {
     description = "A python wrapper for libvips";
     homepage = "https://github.com/libvips/pyvips";
     license = stdenv.lib.licenses.bsd3;
-    maintainers = with stdenv.lib.maintainers; [ ccellado ];
+    #maintainers = with stdenv.lib.maintainers; [ ccellado ];
     platforms = stdenv.lib.platforms.unix;
   };
 }
