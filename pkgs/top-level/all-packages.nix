8415766d6fbfded5928d07f106b135a32518010f
rnhmjoj <rnhmjoj@inventati.org>
2020-06-22 15:39:01 +0200

    zeromq3: remove
    This zeromq version is really old and is no longer needed
    by any package in Nixpkgs.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15322,7 +15322,6 @@ in
 
   zlog = callPackage ../development/libraries/zlog { };
 
-  zeromq3 = callPackage ../development/libraries/zeromq/3.x.nix {};
   zeromq4 = callPackage ../development/libraries/zeromq/4.x.nix {};
   zeromq = zeromq4;
 
