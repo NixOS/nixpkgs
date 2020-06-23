39da4240609ee0d8ea533f142ae4c7e25df95980
Maximilian Bosch <maximilian@mbosch.me>
2020-06-23 17:09:52 +0200

    riot-desktop: 1.6.5 -> 1.6.6
    https://github.com/vector-im/riot-desktop/releases/tag/v1.6.6
    (cherry picked from commit ea13f90b5fd4de6a4e7b72c9c7f8970e7373613f)

--- pkgs/applications/networking/instant-messengers/riot/riot-desktop.nix
+++ pkgs/applications/networking/instant-messengers/riot/riot-desktop.nix
@@ -8,12 +8,12 @@
 
 let
   executableName = "riot-desktop";
-  version = "1.6.5";
+  version = "1.6.6";
   src = fetchFromGitHub {
     owner = "vector-im";
     repo = "riot-desktop";
     rev = "v${version}";
-    sha256 = "1snmfn98z63a8ahh5c7y7h00i8qsdq6wsnidmjjrkzcz3mchfq60";
+    sha256 = "0yrybi6cv3lj0fq4ab4rzjm9l4flfw97hkwzlp7zzixb3z9h09h8";
   };
   electron = electron_7;
 
