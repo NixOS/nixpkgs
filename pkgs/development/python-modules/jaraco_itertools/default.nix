b6f95732fc014e7e4aaf84257775dd70cd8bc9be
Jonathan Ringer <jonringer117@gmail.com>
2020-06-22 18:08:34 -0700

    python3Packages.jaraco_itertools: fix tests

--- pkgs/development/python-modules/jaraco_itertools/default.nix
+++ pkgs/development/python-modules/jaraco_itertools/default.nix
@@ -11,15 +11,17 @@ buildPythonPackage rec {
     sha256 = "6447d567f57efe5efea386265c7864652e9530830a1b80f43e60b4f222b9ab84";
   };
 
-  patches = [ ./0001-Don-t-run-flake8-checks-during-the-build.patch ];
-
-  buildInputs = [ setuptools_scm ];
+  nativeBuildInputs = [ setuptools_scm ];
   propagatedBuildInputs = [ inflect more-itertools six ];
   checkInputs = [ pytest ];
 
+  # tests no longer available through pypi
+  doCheck = false;
   checkPhase = ''
     pytest
   '';
+
+  pythonImportsCheck = [ "jaraco.itertools" ];
 
   meta = with lib; {
     description = "Tools for working with iterables";
