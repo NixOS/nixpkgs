66d0b2a0d05dc526e1d6d30d7289282e63af0ceb
Daniël de Kok <me@danieldk.eu>
2020-06-23 10:12:54 +0200

    python3Packages.tokenizers: init at 0.8.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1502,6 +1502,9 @@ in {
     inherit (pkgs) sentencepiece pkgconfig;
   };
 
+  tokenizers = disabledIf (!isPy3k)
+    (toPythonModule (callPackage ../development/python-modules/tokenizers { }));
+
   transformers = callPackage ../development/python-modules/transformers { };
 
   transforms3d = callPackage ../development/python-modules/transforms3d { };
