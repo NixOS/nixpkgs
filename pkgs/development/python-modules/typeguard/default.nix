NixOS/nixpkgs

commit 9993c38682d2889b784c845145d7b00c1fb126ec

python: typeguard: 2.7.1 -> 2.8.0

pkgs/development/python-modules/typeguard/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "typeguard";
-  version = "2.7.1";
+  version = "2.8.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "2d545c71e9439c21bcd7c28f5f55b3606e6106f7031ab58375656a1aed483ef2";
+    sha256 = "e718f493d805d596cba238a61aa83b874530a333783ca9d597fe5bf27143f042";
   };
 
   buildInputs = [ setuptools_scm ];

commit 40fb898673b427fbfc2c4a0ebeb821d5aa3531be

typeguard: fix tests
Broken by 9993c38682d2889b784c845145d7b00c1fb126ec

    Traceback:
    tests/test_typeguard_py36.py:7: in <module>
        from typing_extensions import Literal
    E   ModuleNotFoundError: No module named 'typing_extensions'

Resolved by adding typing-extensions to checkInputs

pkgs/development/python-modules/typeguard/default.nix
@@ -4,6 +4,7 @@
 , stdenv
 , setuptools_scm
 , pytest
+, typing-extensions
 , glibcLocales
 }:
 
@@ -25,7 +26,7 @@ buildPythonPackage rec {
     substituteInPlace setup.cfg --replace " --cov" ""
   '';
 
-  checkInputs = [ pytest ];
+  checkInputs = [ pytest typing-extensions ];
 
   checkPhase = ''
     py.test .