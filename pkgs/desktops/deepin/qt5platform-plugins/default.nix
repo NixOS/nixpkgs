053c04d80f9e4fdb5d841b290437caacfe1626f0
José Romildo Malaquias <malaquias@gmail.com>
2020-06-24 23:00:02 -0300

    deepin.qt5platform-plugins: 5.0.11 -> 5.0.15

--- pkgs/desktops/deepin/qt5platform-plugins/default.nix
+++ pkgs/desktops/deepin/qt5platform-plugins/default.nix
@@ -1,26 +1,28 @@
 { stdenv
 , mkDerivation
 , fetchFromGitHub
-, pkgconfig
+, fetchpatch
+, pkg-config
 , qmake
-, qtx11extras
-, libSM
-, mtdev
 , cairo
 , deepin
+, libSM
+, mtdev
 , qtbase
+, qtwayland
+, qtx11extras
 }:
 
 mkDerivation rec {
   pname = "qt5platform-plugins";
-  version = "5.0.11";
+  version = "5.0.15";
 
   srcs = [
     (fetchFromGitHub {
       owner = "linuxdeepin";
       repo = pname;
       rev = version;
-      sha256 = "14xkr3p49716jc9v7ksj6jgcmfa65qicqrmablizfi71srg3z2pr";
+      sha256 = "1hmg86rfc8g4q1sdljd0wh66w9a2j9x3rvq0bzsk8xvv9im8kgmp";
     })
     qtbase.src
   ];
@@ -28,28 +30,38 @@ mkDerivation rec {
   sourceRoot = "source";
 
   nativeBuildInputs = [
-    pkgconfig
+    pkg-config
     qmake
+    deepin.setupHook
   ];
 
   buildInputs = [
-    qtx11extras
+    cairo
     libSM
     mtdev
-    cairo
     qtbase
+    qtwayland
+    qtx11extras
   ];
 
   postPatch = ''
-    # The Qt5 platforms plugin is vendored in the package, however what's there is not always up-to-date with what's in nixpkgs.
-    # We simply copy the headers from qtbase's source tarball.
-    mkdir -p platformplugin/libqt5xcbqpa-dev/${qtbase.version}
-    cp -r ../qtbase-everywhere-src-${qtbase.version}/src/plugins/platforms/xcb/*.h platformplugin/libqt5xcbqpa-dev/${qtbase.version}/
+    searchHardCodedPaths  # debuging
+
+    # The Qt5 platforms plugin is vendored in the package, however what is there is not always up-to-date with what is in nixpkgs.
+    # We simply link the headers from qtbase's source tarball.
+    ln -sr ../qtbase-everywhere-src-${qtbase.version}/src/plugins/platforms/xcb xcb/libqt5xcbqpa-dev/${qtbase.version}
+
+    # Disable wayland for now: https://github.com/linuxdeepin/qt5platform-plugins/issues/47
+    sed -i '/wayland/d' qt5platform-plugins.pro
   '';
 
   qmakeFlags = [
     "INSTALL_PATH=${placeholder "out"}/${qtbase.qtPluginPrefix}/platforms"
   ];
+
+  postFixup = ''
+    searchHardCodedPaths -a $out  # debuging
+  '';
 
   passthru.updateScript = deepin.updateScript { inherit pname version; src = (builtins.head srcs); };
 
