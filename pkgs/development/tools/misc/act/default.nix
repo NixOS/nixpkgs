0c339237b8b043daa40095eadb43b79d4b1cb2f1
R. RyanTM <ryantm-bot@ryantm.com>
2020-06-26 01:35:02 +0000

    act: 0.2.9 -> 0.2.10

--- pkgs/development/tools/misc/act/default.nix
+++ pkgs/development/tools/misc/act/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "act";
-  version = "0.2.9";
+  version = "0.2.10";
 
   src = fetchFromGitHub {
     owner = "nektos";
     repo = pname;
     rev = "v${version}";
-    sha256 = "17w7pqpn9pkzc85lrsmyhxy6rip47dxw1hkz4ml3y5n68nysfpb9";
+    sha256 = "0xmrb8wbxkb52l2c7fxxy5wa9lsl591fl65zicv0nrbil36q4wfd";
   };
 
   vendorSha256 = "0qf26g0a2j1mbzlc7xjackww22w9bl1x0iw3q1x6kq7fp8xiwhdn";
