10d6387543d1ea442504e670a4440436d4d0020a
Mario Rodas <marsam@users.noreply.github.com>
2020-06-26 04:20:00 -0500

    starship: add marsam to maintainers

--- pkgs/tools/misc/starship/default.nix
+++ pkgs/tools/misc/starship/default.nix
@@ -36,7 +36,7 @@ rustPlatform.buildRustPackage rec {
     description = "A minimal, blazing fast, and extremely customizable prompt for any shell";
     homepage = "https://starship.rs";
     license = licenses.isc;
-    maintainers = with maintainers; [ bbigras davidtwco filalex77 Frostman ];
+    maintainers = with maintainers; [ bbigras davidtwco filalex77 Frostman marsam ];
     platforms = platforms.all;
   };
 }
