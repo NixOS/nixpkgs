b26f5395ef7ca1890a23e5fb8a4053db8de5ea98
Rohit Goswami <rgoswami@ieee.org>
2020-06-26 13:47:03 +0000

    d-seams: init at 1.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25131,6 +25131,8 @@ in
     eigen = eigen2;
   };
 
+  d-seams = callPackage ../applications/science/chemistry/d-seams {};
+
   gwyddion = callPackage ../applications/science/chemistry/gwyddion {};
 
   jmol = callPackage ../applications/science/chemistry/jmol { };
