467fdc71e52b492703d8304de8062e310249bf6a
Tim Steinbach <tim@nequissimus.com>
2020-06-26 11:08:51 -0400

    linux: 5.8-rc1 -> 5.8-rc2

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,15 +3,15 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.8-rc1";
-  extraMeta.branch = "5.1";
+  version = "5.8-rc2";
+  extraMeta.branch = "5.8";
 
   # modDirVersion needs to be x.y.z, will always add .0
   modDirVersion = if (modDirVersionArg == null) then builtins.replaceStrings ["-"] [".0-"] version else modDirVersionArg;
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "1gb7g2vrgg0zz281lv1ir1r0535spc40j65p0azmdxlk24fkfxfc";
+    sha256 = "03y4p5abmcjm3w5m7p868bqzzrrqz461na9h46f3f99g95z66v09";
   };
 
   # Should the testing kernels ever be built on Hydra?
