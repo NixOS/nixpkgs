b7571c9058aebf248ada699f495c4e1a9ab6e0a7
Michel Weitbrecht <slothofanarchy1@gmail.com>
2020-06-28 01:49:46 +0200

    batsignal: Init at 1.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18944,6 +18944,8 @@ in
 
   batik = callPackage ../applications/graphics/batik { };
 
+  batsignal = callPackage ../applications/misc/batsignal { };
+
   baudline = callPackage ../applications/audio/baudline { };
 
   bb =  callPackage ../applications/misc/bb { };
