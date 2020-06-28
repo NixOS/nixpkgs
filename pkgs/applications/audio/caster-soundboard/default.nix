16cb4fe39ad2bd22f32ede817874fee1d21fc86b
IQubic <avi.caspe@gmail.com>
2020-06-27 20:09:40 -0700

    Apply suggestions from code review

--- pkgs/applications/audio/caster-soundboard/default.nix
+++ pkgs/applications/audio/caster-soundboard/default.nix
@@ -2,14 +2,16 @@
 , qmake, qtmultimedia, gst_all_1 }:
 
 mkDerivation {
-  name = "caster-soundboard";
+  pname = "caster-soundboard";
   version = "unstable-2019-9-28";
-  src = fetchFromGitHub {
+
+   src = fetchFromGitHub {
     owner = "JupiterBroadcasting";
     repo = "CasterSoundboard";
     rev = "1a84315b3a3d4bdca1a4b784bd19460637f44438";
     sha256 = "1npjcxw304hv8ihwv13cslbn2rcd6iqnknyqz94rdi496d1kfwv1";
   };
+
   sourceRoot = "source/CasterSoundboard";
 
   buildInputs = [
@@ -27,11 +29,11 @@ mkDerivation {
     ''--prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0"''
   ];
 
-  meta = {
-    description = "A soundboard for hot-keying and playing back sounds.";
+  meta = with stdenv.lib; {
+    description = "A soundboard for hot-keying and playing back sounds";
     homepage = "https://github.com/JupiterBroadcasting/CasterSoundboard";
-    maintainers = [ stdenv.lib.maintainers.iqubic ];
-    license = stdenv.lib.licenses.lgpl3;
-    platforms = stdenv.lib.platforms.linux;
+    maintainers = [ maintainers.iqubic ];
+    license = licenses.lgpl3;
+    platforms = platforms.linux;
   };
 }
