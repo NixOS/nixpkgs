08ab3757159c524d6a6b4fb2c110e161a29bbfeb
Matías Lang <matiasl@infobytesec.com>
2020-06-28 00:28:30 -0300

    pythonPackages.webargs: init at 8.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8982,6 +8982,8 @@ in {
 
   webhelpers = callPackage ../development/python-modules/webhelpers { };
 
+  webargs = callPackage ../development/python-modules/webargs { };
+
   webob = callPackage ../development/python-modules/webob { };
 
   weboob = callPackage ../development/python-modules/weboob { };
