354150ca2923fcc416618b0d157056e47888c467
cidkid <cidkidnix@protonmail.com>
2020-06-29 01:46:12 -0500

    linux_testing_bcachefs: 5.3 -> 5.7

--- pkgs/os-specific/linux/kernel/linux-testing-bcachefs.nix
+++ pkgs/os-specific/linux/kernel/linux-testing-bcachefs.nix
@@ -1,13 +1,14 @@
-{ stdenv, buildPackages, fetchgit, fetchpatch, perl, buildLinux, ... } @ args:
+{ stdenv, buildPackages, fetchFromGitHub, fetchpatch, perl, buildLinux, ... } @ args:
 
 buildLinux (args // {
-  version = "5.3.2020.04.04";
-  modDirVersion = "5.3.0";
+  version = "5.7.2020.04.04";
+  modDirVersion = "5.7.0";
 
-  src = fetchgit {
-    url = "https://evilpiepirate.org/git/bcachefs.git";
-    rev = "a27d7265e75f6d65c2b972ce4ac27abfc153c230";
-    sha256 = "0wnjl4xs7073d5ipcsplv5qpcxb7zpfqd5gqvh3mhqc5j3qn816x";
+  src = fetchFromGitHub {
+    owner = "koverstreet";
+    repo = "bcachefs";
+    rev = "b1708f0191fcad1b7afa47dd6a7c6b1104c4639d";
+    sha256 = "1fnyyl41fy96pn9hh65qp53jmdvygbjl2qwcvbslb5565mpm5875";
   };
 
   extraConfig = "BCACHEFS_FS m";
