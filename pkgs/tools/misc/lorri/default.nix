44ec3b5d73e7d646559f9e1e248b002c49a1a1b4
Profpatsch <mail@profpatsch.de>
2020-06-30 02:09:13 +0200

    lorri: 1.0 -> 1.1

--- pkgs/tools/misc/lorri/default.nix
+++ pkgs/tools/misc/lorri/default.nix
@@ -4,7 +4,7 @@
 , rustPlatform
   # Updater script
 , runtimeShell
-, writeScript
+, writers
   # Tests
 , nixosTests
   # Apple dependencies
@@ -14,7 +14,7 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "lorri";
-  version = "1.0";
+  version = "1.1";
 
   meta = with stdenv.lib; {
     description = "Your project's nix-env";
@@ -27,11 +27,12 @@ rustPlatform.buildRustPackage rec {
     owner = "target";
     repo = pname;
     # Run `eval $(nix-build -A lorri.updater)` after updating the revision!
-    rev = "88c680c9abf0f04f2e294436d20073ccf26f0781";
-    sha256 = "1415mhdr0pwvshs04clfz1ys76r5qf9jz8jchm63l6llaj6m7mrv";
+    # ALSO don’t forget to update the cargoSha256!
+    rev = "93d93013217cd9aa09d2bd316d6c3abf827a6601";
+    sha256 = "0wbkx8hmikngfp6fp0y65yla22f3k0jszq8a6pas80q0b33llwm5";
   };
 
-  cargoSha256 = "1iwd0cad8dp8q5xz2mm7zn1wphr5brkw937dfygc88afj6bv3d68";
+  cargoSha256 = "1a3n1ylyp63x6v7b07nnqpfxjzmsgwmgraza23lx8z4gh167gv46";
   doCheck = false;
 
   BUILD_REV_COUNT = src.revCount or 1;
@@ -42,8 +43,7 @@ rustPlatform.buildRustPackage rec {
     stdenv.lib.optionals stdenv.isDarwin [ CoreServices Security ];
 
   passthru = {
-    updater = with builtins; writeScript "copy-runtime-nix.sh" ''
-      #!${runtimeShell}
+    updater = writers.writeBash "copy-runtime-nix.sh" ''
       set -euo pipefail
       cp ${src}/nix/runtime.nix ${toString ./runtime.nix}
       cp ${src}/nix/runtime-closure.nix.template ${toString ./runtime-closure.nix.template}
