7db2ad2a6f4ba4a5e4a98335151ee9e764b05850
R. RyanTM <ryantm-bot@ryantm.com>
2020-06-30 10:09:41 +0000

    opencl-headers: 2020.03.13 -> 2020.06.16

--- pkgs/development/libraries/opencl-headers/default.nix
+++ pkgs/development/libraries/opencl-headers/default.nix
@@ -3,13 +3,13 @@
 
 stdenv.mkDerivation rec {
   name = "opencl-headers-${version}";
-  version = "2020.03.13";
+  version = "2020.06.16";
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "OpenCL-Headers";
     rev = "v${version}";
-    sha256 = "1d9ibiwicaj17757h9yyjc9i2hny8d8npn4spbjscins8972z3hw";
+    sha256 = "0viiwhfqccw90r3mr45ab3wyhabpdrihplj5842brn5ny0ayh73z";
   };
 
   installPhase = ''
