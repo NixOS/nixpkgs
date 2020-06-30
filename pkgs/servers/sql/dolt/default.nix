c1ab60fc63be85759167a60edccf98bd2c3d518e
R. RyanTM <ryantm-bot@ryantm.com>
2020-06-30 04:26:36 +0000

    dolt: 0.16.3 -> 0.18.0

--- pkgs/servers/sql/dolt/default.nix
+++ pkgs/servers/sql/dolt/default.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
     pname = "dolt";
-    version = "0.16.3";
+    version = "0.18.0";
 
     src = fetchFromGitHub {
         owner = "liquidata-inc";
         repo = "dolt";
         rev = "v${version}";
-        sha256 = "141wv5av7hms4wa3s4md7mnb77bbyn3854d7gj7fy6f6jvzghdny";
+        sha256 = "12kvjq3z9bggfmgci3wql4v8sh491dwp6qr768k8kwncqmp3jjvx";
     };
 
     modRoot = "./go";
     subPackages = [ "cmd/dolt" "cmd/git-dolt" "cmd/git-dolt-smudge" ];
-  vendorSha256 = "1kjh252p91yxq5mi0igamkwlsr50lij24wsp2ilgz6ksv3ijzfr1";
+  vendorSha256 = "1lscjp4ih5kxrbqch9164bqrp7lbsir6vg4zcczhvhh0phbcbqjs";
 
     meta = with lib; {
         description = "Relational database with version control and CLI a-la Git.";
