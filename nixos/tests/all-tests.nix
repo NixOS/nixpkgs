f1cf202dbbe3de6ee8f4fd8b40a5c61654aaa19d
zowoq <59103226+zowoq@users.noreply.github.com>
2020-07-03 00:17:15 +1000

    nixos/podman: restrict test to x86_64-linux

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -266,7 +266,7 @@ in
   php = handleTest ./php {};
   plasma5 = handleTest ./plasma5.nix {};
   plotinus = handleTest ./plotinus.nix {};
-  podman = handleTest ./podman.nix {};
+  podman = handleTestOn ["x86_64-linux"] ./podman.nix {};
   postgis = handleTest ./postgis.nix {};
   postgresql = handleTest ./postgresql.nix {};
   postgresql-wal-receiver = handleTest ./postgresql-wal-receiver.nix {};
