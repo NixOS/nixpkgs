561a12e64f83c422dedbe4921424dcd655df89a0
Jaka Hudoklin <jaka@x-truder.net>
2020-07-02 13:52:02 +0700

    wolframscript: init at 12.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25193,6 +25193,8 @@ in
   mathematica10 = callPackage ../applications/science/math/mathematica/10.nix { };
   mathematica11 = callPackage ../applications/science/math/mathematica/11.nix { };
 
+  wolframscript = callPackage ../applications/science/math/wolframscript { };
+
   metis = callPackage ../development/libraries/science/math/metis {};
 
   nauty = callPackage ../applications/science/math/nauty {};
