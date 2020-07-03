2f8baeb5880866f54774e113a2950386acf03cae
R. RyanTM <ryantm-bot@ryantm.com>
2020-07-03 09:40:19 +0000

    spleen: 1.7.0 -> 1.8.0

--- pkgs/data/fonts/spleen/default.nix
+++ pkgs/data/fonts/spleen/default.nix
@@ -2,7 +2,7 @@
 
 let
   pname = "spleen";
-  version = "1.7.0";
+  version = "1.8.0";
 in fetchurl {
   name = "${pname}-${version}";
   url = "https://github.com/fcambus/spleen/releases/download/${version}/spleen-${version}.tar.gz";
@@ -19,7 +19,7 @@ in fetchurl {
     # create fonts.dir so NixOS xorg module adds to fp
     ${mkfontscale}/bin/mkfontdir "$d"
   '';
-  sha256 = "17dn6spfr8wv63sy009djb4q12q635m13wsyirzn074qabhr9ggg";
+  sha256 = "0vaawnav4k2gpwsgxn5rmkd8wyxnwsrbd0ksp749sb7rwfd3nphc";
 
   meta = with lib; {
     description = "Monospaced bitmap fonts";
