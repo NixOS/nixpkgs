72293ae7691c05e376f69364855982e9f6579a85
Tamer Abdulradi <tamer@abdulradi.com>
2020-07-04 15:52:05 +0100

    Rename coursier to launcher to cs

--- pkgs/development/tools/coursier/default.nix
+++ pkgs/development/tools/coursier/default.nix
@@ -18,9 +18,9 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ makeWrapper ];
 
   buildCommand = ''
-    install -Dm555 $src $out/bin/coursier
-    patchShebangs $out/bin/coursier
-    wrapProgram $out/bin/coursier --prefix PATH ":" ${jre}/bin
+    install -Dm555 $src $out/bin/cs
+    patchShebangs $out/bin/cs
+    wrapProgram $out/bin/cs --prefix PATH ":" ${jre}/bin
 
     # copy zsh completion
     install -Dm755 ${zshCompletion version} $out/share/zsh/site-functions/_coursier
