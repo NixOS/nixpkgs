92b7ce99f76f55d832e6eebc6ae561a781892226
Vladimír Čunát <v@cunat.cz>
2020-07-04 17:31:18 +0200

    wxGTK3*: switch to gstreamer 1.x
    Configure log still prints `checking for GST... yes`
    and the new GST libs appear in RPATH.
    wxGTK28 does't seem able to configure with non-legacy GST,
    but it's almost unused... so I didn't touch it.

--- pkgs/development/libraries/wxwidgets/3.1/default.nix
+++ pkgs/development/libraries/wxwidgets/3.1/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchFromGitHub, fetchurl, pkgconfig
 , libXinerama, libSM, libXxf86vm
 , gtk2, GConf ? null, gtk3
-, xorgproto, gstreamer, gst-plugins-base, setfile
+, xorgproto, gst_all_1, setfile
 , libGLSupported ? stdenv.lib.elem stdenv.hostPlatform.system stdenv.lib.platforms.mesaPlatforms
 , withMesa ? libGLSupported, libGLU ? null, libGL ? null
 , compat28 ? false, compat30 ? true, unicode ? true
@@ -29,7 +29,7 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [
-    libXinerama libSM libXxf86vm xorgproto gstreamer gst-plugins-base
+    libXinerama libSM libXxf86vm xorgproto gst_all_1.gstreamer gst_all_1.gst-plugins-base
   ] ++ optionals withGtk2 [ gtk2 GConf ]
     ++ optional (!withGtk2) gtk3
     ++ optional withMesa libGLU
