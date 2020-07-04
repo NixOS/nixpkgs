519151fda01073faec295d527d2bb18994d41701
Maximilian Bosch <maximilian@mbosch.me>
2020-07-04 18:32:33 +0200

    mautrix-whatsapp: 0.1.1 -> 0.1.2
    https://github.com/tulir/mautrix-whatsapp/releases/tag/v0.1.2
    (cherry picked from commit 8842a387efbe41d51be90430815b982d6ceb1455)

--- pkgs/servers/mautrix-whatsapp/default.nix
+++ pkgs/servers/mautrix-whatsapp/default.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "mautrix-whatsapp";
-  version = "0.1.1";
+  version = "0.1.2";
 
   src = fetchFromGitHub {
     owner = "tulir";
     repo = "mautrix-whatsapp";
     rev = "v${version}";
-    sha256 = "0cjgyn311zvpdsagyndkw89bvdrcli5kqznss8dsh05wrllxp3x4";
+    sha256 = "1df6m1jjv8la6qcz3c5a6pw0jk81f1khx3d9q17d6v3ihrki8krk";
   };
 
   buildInputs = [ olm ];
 
-  modSha256 = "0jv0xxkz75ny0v74cyhnn57w3qj6gsc8filinnfclnyq8fh0c8wq";
+  modSha256 = "1m4aa6bzsn9wa577abnsnkj5qzkk42z54k17xvybc47a9inc5gp8";
 
   meta = with stdenv.lib; {
     homepage = https://github.com/tulir/mautrix-whatsapp;
