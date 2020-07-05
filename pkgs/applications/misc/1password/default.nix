fb391e5dc562d5fea510d7b5be59422e600d1d3e
Ivar <ivar.scholten@protonmail.com>
2020-07-05 22:34:56 +0200

    _1password: 1.1.1 -> 1.2.0

--- pkgs/applications/misc/1password/default.nix
+++ pkgs/applications/misc/1password/default.nix
@@ -1,8 +1,8 @@
-{ stdenv, fetchzip, autoPatchelfHook, fetchurl, xar, cpio }:
+{ stdenv, fetchzip, fetchurl, autoPatchelfHook, xar, cpio, installShellFiles }:
 
 stdenv.mkDerivation rec {
   pname = "1password";
-  version = "1.1.1";
+  version = "1.2.0";
   src =
     if stdenv.isLinux then fetchzip {
       url = {
@@ -10,14 +10,17 @@ stdenv.mkDerivation rec {
         "x86_64-linux" = "https://cache.agilebits.com/dist/1P/op/pkg/v${version}/op_linux_amd64_v${version}.zip";
       }.${stdenv.hostPlatform.system};
       sha256 = {
-        "i686-linux" = "1andl3ripkcg4jhwdkd4b39c9aaxqpx9wzq21pysn6rlyy4hfcb0";
-        "x86_64-linux" = "0qj5v8psqyp0sra0pvzkwjpm28kx3bgg36y37wklb6zl2ngpxm5g";
+        "i686-linux" = "1fpmglqj7qqaa7k79ngrf16qq6rv4m8b57hzq87fyj22myaxydjy";
+        "x86_64-linux" = "0c0zj3bv0rgdqaanm6gk7fsy5ckj5h8zc5jckrv1nsfqr7lpbydd";
       }.${stdenv.hostPlatform.system};
       stripRoot = false;
     } else fetchurl {
       url = "https://cache.agilebits.com/dist/1P/op/pkg/v${version}/op_darwin_amd64_v${version}.pkg";
-      sha256 = "16inwxkrky4xwlr7vara1l8kapdgjg3kfq1l94i5855782hn4ppm";
+      sha256 = "0sc5fi55gcbjmj5cp9clilkdz7c6nzqyyh1fficfafh8il4qvki6";
     };
+
+  nativeBuildInputs = [ installShellFiles ]
+    ++ stdenv.lib.optionals stdenv.isLinux [ autoPatchelfHook ];
 
   buildInputs = stdenv.lib.optionals stdenv.isDarwin [ xar cpio ];
 
@@ -26,13 +29,14 @@ stdenv.mkDerivation rec {
     zcat Payload | cpio -i
   '';
 
-  installPhase = ''
-    install -D op $out/bin/op
-  '';
-
   dontStrip = stdenv.isDarwin;
 
-  nativeBuildInputs = stdenv.lib.optionals stdenv.isLinux [ autoPatchelfHook ];
+  installPhase = ''
+    install -D op $out/bin/op
+    $out/bin/op completion bash > op.bash
+    $out/bin/op completion zsh > op.zsh
+    installShellCompletion op.{bash,zsh}
+  '';
 
   doInstallCheck = true;
 
@@ -41,11 +45,11 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with stdenv.lib; {
-    description  = "1Password command-line tool";
-    homepage     = "https://support.1password.com/command-line/";
+    description = "1Password command-line tool";
+    homepage = "https://support.1password.com/command-line/";
     downloadPage = "https://app-updates.agilebits.com/product_history/CLI";
-    maintainers  = with maintainers; [ joelburget marsam ];
-    license      = licenses.unfree;
-    platforms    = [ "i686-linux" "x86_64-linux" "x86_64-darwin" ];
+    maintainers = with maintainers; [ joelburget marsam ivar ];
+    license = licenses.unfree;
+    platforms = [ "i686-linux" "x86_64-linux" "x86_64-darwin" ];
   };
 }
