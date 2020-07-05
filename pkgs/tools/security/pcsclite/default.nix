903f5dd519d15b8fb02de13842907c9faa3d21ab
Jan Tojnar <jtojnar@gmail.com>
2020-07-06 01:38:03 +0200

    Revert "pcsclite: Explicitly set ipcdir"
    This reverts commit bc877d8bfcfe8c1b82687b725c823a0154c7d572.
    
    This is no longer necessary, since 1.9.0 finally uses /run by default.
    
    https://salsa.debian.org/rousseau/PCSC/commit/562ef23bc7eab3d5cc49c38f7ac0c6341ade1130

--- pkgs/tools/security/pcsclite/default.nix
+++ pkgs/tools/security/pcsclite/default.nix
@@ -18,7 +18,6 @@ stdenv.mkDerivation rec {
     # The OS should care on preparing the drivers into this location
     "--enable-usbdropdir=/var/lib/pcsc/drivers"
     "--enable-confdir=/etc"
-    "--enable-ipcdir=/run/pcscd"
   ] ++ stdenv.lib.optional stdenv.isLinux
          "--with-systemdsystemunitdir=\${out}/etc/systemd/system"
     ++ stdenv.lib.optional (!stdenv.isLinux)
