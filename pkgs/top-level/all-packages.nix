35c588addba0aab570819d6c47056334414a13fb
Piotr Bogdan <ppbogdan@gmail.com>
2020-07-05 15:32:56 +0100

    feeds: init at 0.16.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3857,6 +3857,8 @@ in
 
   feedreader = callPackage ../applications/networking/feedreaders/feedreader {};
 
+  feeds = callPackage ../applications/networking/feedreaders/feeds {};
+
   fend = callPackage ../tools/misc/fend { };
 
   ferm = callPackage ../tools/networking/ferm { };
