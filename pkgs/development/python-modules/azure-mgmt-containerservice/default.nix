90f7bcdfdf97c8e9fb905de4f9827ebd5157ae4d
R. RyanTM <ryantm-bot@ryantm.com>
2020-07-06 03:30:24 +0000

    python27Packages.azure-mgmt-containerservice: 9.0.1 -> 9.1.0

--- pkgs/development/python-modules/azure-mgmt-containerservice/default.nix
+++ pkgs/development/python-modules/azure-mgmt-containerservice/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "azure-mgmt-containerservice";
-  version = "9.0.1";
+  version = "9.1.0";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "7e4459679bdba4aa67a4b5848e63d94e965a304a7418ef7607eb7a9ce295d886";
+    sha256 = "0xdm4kkqmfsj7vkq3b9inqwx6nif5kmbd09wl40ckkjndvs1sclz";
   };
 
   propagatedBuildInputs = [
