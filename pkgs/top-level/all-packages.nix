cb2ab83d3ea6a98a705f09b8e9bfa49c2438d007
Biser Milanov <bmilanov11@gmail.com>
2020-07-06 20:41:53 +0300

    nxpmicro-mfgtools: init at 1.3.191
    Add nxpmicro-mfgtools to nixpkgs
    
    From the project's homepage [1]:
    
        Freescale/NXP I.MX Chip image deploy tools.
    
    The project's only binary and library at the moment is UUU. NXP
    might add more in the future, so making the package name:
    
        nxpmicro-mfgtools
    
    instead of:
    
        nxp-uuu
    
    or something similar.
    
    [1]: https://github.com/NXPmicro/mfgtools

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5590,6 +5590,8 @@ in
 
   nwdiag = with python3Packages; toPythonApplication nwdiag;
 
+  nxpmicro-mfgtools = callPackage ../development/tools/misc/nxpmicro-mfgtools { };
+
   nyancat = callPackage ../tools/misc/nyancat { };
 
   nylon = callPackage ../tools/networking/nylon { };
