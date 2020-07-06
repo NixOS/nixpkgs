d370ead49608ffcdc81f4593e1802fa2a59eebc1
David Wood <david@davidtw.co>
2020-07-06 12:36:18 +0100

    franz: 5.4.1 -> 5.5.0

--- pkgs/applications/networking/instant-messengers/franz/default.nix
+++ pkgs/applications/networking/instant-messengers/franz/default.nix
@@ -3,10 +3,10 @@
 mkFranzDerivation rec {
   pname = "franz";
   name = "Franz";
-  version = "5.4.1";
+  version = "5.5.0";
   src = fetchurl {
     url = "https://github.com/meetfranz/franz/releases/download/v${version}/franz_${version}_amd64.deb";
-    sha256 = "1g1z5zjm9l081hpqslfc4h7pqh4k76ccmlz71r21204wy630mw6h";
+    sha256 = "0kgfjai0pz0gpbxsmn3hbha7zr2kax0s1j3ygcsy4kzghla990wm";
   };
   meta = with stdenv.lib; {
     description = "A free messaging app that combines chat & messaging services into one application";
