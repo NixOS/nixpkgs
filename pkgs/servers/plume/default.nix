85a70c138d34193290a536fd3032a49dc8a17059
Matthias Beyer <mail@beyermatthias.de>
2020-07-06 21:02:28 +0200

    plume: 0.4.0-alpha-4 -> 0.5.0
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/servers/plume/default.nix
+++ pkgs/servers/plume/default.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "plume";
-  version = "0.4.0-alpha-4";
+  version = "0.5.0";
 
   src = fetchFromGitHub {
     owner = "plume-org";
     repo = "plume";
     rev = version;
-    sha256 = "0ccf7ihpjg261r0znwaasrzakw3jwh5kw202px241bd2vqlf9cv5";
+    sha256 = "0bhl7r8ri0wd8q7vblxlfn3lj7v78qcw2cn9nwad78jsa8y3xyl5";
   };
 
-  cargoSha256 = "1xdnmcvga048av7xfvvgd646q958kl1cw2a7i4m85gjvqq2mp96g";
+  cargoSha256 = "1cihgxsgchx8ihlmnzf62jd8wy4sf9vfmvijxz7fgh8wagnx7h6q";
 
   meta = with stdenv.lib; {
     description = "Serverless Information Tracker";
