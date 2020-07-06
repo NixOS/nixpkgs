f03e06c99faa0375685da63e98d23641070255cc
R. RyanTM <ryantm-bot@ryantm.com>
2020-07-06 06:01:42 +0000

    python27Packages.azure-mgmt-compute: 12.0.0 -> 12.1.0

--- pkgs/development/python-modules/azure-mgmt-compute/default.nix
+++ pkgs/development/python-modules/azure-mgmt-compute/default.nix
@@ -7,13 +7,13 @@
 }:
 
 buildPythonPackage rec {
-  version = "12.0.0";
+  version = "12.1.0";
   pname = "azure-mgmt-compute";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "0vzq93g2fpnij4rykkk3391xq2knhlbz87vhim4zvj8s45sx6z8q";
+    sha256 = "177825xhr5m0h94jl0dg8zglysx3hq252pwqx23bjjsqlipnwhal";
   };
 
   propagatedBuildInputs = [
