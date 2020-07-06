c93f5194c8bde97f61f5c68a7cc6a6ea6e4c39f7
R. RyanTM <ryantm-bot@ryantm.com>
2020-07-06 11:57:32 +0000

    python37Packages.jupyterlab: 2.1.4 -> 2.1.5

--- pkgs/development/python-modules/jupyterlab/default.nix
+++ pkgs/development/python-modules/jupyterlab/default.nix
@@ -8,12 +8,12 @@
 
 buildPythonPackage rec {
   pname = "jupyterlab";
-  version = "2.1.4";
+  version = "2.1.5";
   disabled = pythonOlder "3.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "7b5bd4a05330a01c8522ee7f1cda5cb2e0d96412d9e1e879a19b3afb63d4ac69";
+    sha256 = "162jn51cg36fsn4l2zhnb5n4nbkhm9wlv974ggcnmdij3i4r4yya";
   };
 
   propagatedBuildInputs = [ jupyterlab_server notebook ];
