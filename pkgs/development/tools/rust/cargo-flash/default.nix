943b7425848c9f5e0f39ccf814076f7e63dcfcd7
wucke13 <wucke13@gmail.com>
2020-07-07 09:26:10 +0200

    cargo-flash: 0.6.0 -> 0.8.0

--- pkgs/development/tools/rust/cargo-flash/default.nix
+++ pkgs/development/tools/rust/cargo-flash/default.nix
@@ -2,35 +2,23 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-flash";
-  version = "0.6.0";
+  version = "0.8.0";
 
   src = fetchFromGitHub {
     owner = "probe-rs";
     repo = pname;
     rev = "v" + version; 
-    sha256 = "01zyxbykzbqfy6w0cmhhq10l1qqf4aqxvh4jngj1lqba5r9d7jsc";
+    sha256 = "1bcpv1r4pdpp22w7za7kdy7jl487x3nlwxiz6sqq3iq6wq3j9zj0";
   };
 
-  # Update this regurlarly! It may very well happen that cargo-flash is not
-  # updated, but new features are released to probe-rs. In this case generating
-  # cargo-lock.patch is sufficient to ensure that our cargo-flash pkg follows
-  # probe-rs. To update the cargo-lock.patch follow these steps:
-  #
-  # $ cd /tmp 
-  # $ git clone --branch v0.6.0 https://github.com/probe-rs/cargo-flash.git 
-  # $ cd cargo-flash
-  # $ cargo-update
-  # $ git diff Cargo.lock > cargo-lock.patch
-
-  cargoPatches = [ ./cargo-lock.patch ];
-  cargoSha256 = "1ynlvs4x01jyycj4acawlvwwg5w88zn4d19ril2c4p3z8igarb2c";
+  cargoSha256 = "1pf117fgw9x9diksqv58cw7i0kzmp25yj73y5ll69sk46b6z4j90";
 
   nativeBuildInputs = [ pkg-config rustfmt ];
   buildInputs = [ libusb1.dev ];
 
   meta = with stdenv.lib; {
     description = "A cargo subcommand to flash ELF binaries onto ARM chips";
-    homepage = "https://github.com/probe-rs/" + pname;
+    homepage = "https://github.com/probe-rs/cargo-flash";
     license = with licenses; [ asl20 mit ];
     maintainers = [ maintainers.wucke13 ];
     platforms = platforms.all;
