420918e8e19b6afd2b68948169b4f6a7764398ec
worldofpeace <worldofpeace@protonmail.ch>
2020-07-07 12:09:14 -0400

    pantheon.elementary-dock: 2020-02-28 -> 2020-06-11

--- pkgs/desktops/pantheon/apps/elementary-dock/default.nix
+++ pkgs/desktops/pantheon/apps/elementary-dock/default.nix
@@ -27,7 +27,7 @@
 
 stdenv.mkDerivation rec {
   pname = "elementary-dock";
-  version = "unstable-2020-02-28";
+  version = "unstable-2020-06-11";
 
   outputs = [ "out" "dev" ];
 
@@ -36,8 +36,8 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "elementary";
     repo = repoName;
-    rev = "ac87d9063dc9c81d90f42f3002ad9c5b49460a82";
-    sha256 = "0lhjzd370fza488dav8n155ss486wqv6y7ldkahwg0c3zvlsvha7";
+    rev = "0a389ee58939d8c91c340df4e5340fc4b23d0b80";
+    sha256 = "01vinik73s0vmk56samgf49zr2bl4wjv44x15sz2cmh744llckja";
   };
 
   nativeBuildInputs = [
