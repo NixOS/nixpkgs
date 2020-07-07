d5a4a823039a85633a54384f60f30061c53a981a
worldofpeace <worldofpeace@protonmail.ch>
2020-07-07 10:54:24 -0400

    pantheon.pantheon-agent-polkit: 1.0.2 -> 1.0.3
    https://github.com/elementary/pantheon-agent-polkit/releases/tag/1.0.3
    (cherry picked from commit 3a23ca569f2eb6ec73c02e893242349dd8bcaa16)

--- pkgs/desktops/pantheon/services/pantheon-agent-polkit/default.nix
+++ pkgs/desktops/pantheon/services/pantheon-agent-polkit/default.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation rec {
   pname = "pantheon-agent-polkit";
-  version = "1.0.2";
+  version = "1.0.3";
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = pname;
     rev = version;
-    sha256 = "1gkispg1wr3fmwhbaaw722hc7cfqvj18gwb1nvv7rz3ghk9ih4jy";
+    sha256 = "1kd6spwfwy5r2mrf7xh5l2wrazqia8vr4j3g27s97vn7fcg4pgb0";
   };
 
   passthru = {
