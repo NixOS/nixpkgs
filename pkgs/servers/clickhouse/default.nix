2761377682bdd30de0ceb94407b62e51f1368a4d
Raghav Sood <r@raghavsood.com>
2020-07-08 08:19:43 +0000

    clickhouse: update website

--- pkgs/servers/clickhouse/default.nix
+++ pkgs/servers/clickhouse/default.nix
@@ -60,7 +60,7 @@ stdenv.mkDerivation rec {
   hardeningDisable = [ "format" ];
 
   meta = with stdenv.lib; {
-    homepage = "https://clickhouse.yandex/";
+    homepage = "https://clickhouse.tech/";
     description = "Column-oriented database management system";
     license = licenses.asl20;
     maintainers = with maintainers; [ orivej ];
