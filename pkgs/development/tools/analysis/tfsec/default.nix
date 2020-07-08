aa5ee82db228c07e4fab6dec2ea26cd00ee2f7a7
Mario Rodas <marsam@users.noreply.github.com>
2020-07-08 18:55:18 -0500

    tfsec: add ldflags

--- pkgs/development/tools/analysis/tfsec/default.nix
+++ pkgs/development/tools/analysis/tfsec/default.nix
@@ -13,6 +13,8 @@ buildGoPackage rec {
 
   goPackagePath = "github.com/liamg/tfsec";
 
+  buildFlagsArray = [ "-ldflags=-s -w -X ${goPackagePath}/version.Version=${version}" ];
+
   meta = with lib; {
     homepage = "https://github.com/liamg/tfsec";
     description = "Static analysis powered security scanner for your terraform code";
