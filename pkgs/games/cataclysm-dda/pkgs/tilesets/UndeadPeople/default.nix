cf0e2d51ac42452c55e288efc846e51eeae22323
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2020-07-08 10:17:23 +0900

    cataclysmDDA: update mods

--- pkgs/games/cataclysm-dda/pkgs/tilesets/UndeadPeople/default.nix
+++ pkgs/games/cataclysm-dda/pkgs/tilesets/UndeadPeople/default.nix
@@ -2,13 +2,13 @@
 
 buildTileSet {
   modName = "UndeadPeople";
-  version = "2020-04-07";
+  version = "2020-07-08";
 
   src = fetchFromGitHub {
     owner = "SomeDeadGuy";
     repo = "UndeadPeopleTileset";
-    rev = "6686230b35b712612b3c7573349740b6f7968ff9";
-    sha256 = "02higdazd7v2gci5lr8r7fhc6yr2az6gbvksgz0lrgh1wg8xgqgs";
+    rev = "f7f13b850fafe2261deee051f45d9c611a661534";
+    sha256 = "0r06srjr7rq51jk9yfyxz80nfgb98mkn86cbcjfxpibgbqvcp0zm";
   };
 
   modRoot = "MSX++UnDeadPeopleEdition";
