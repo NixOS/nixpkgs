f076af87a156f72af6755a041cc69656b204edc6
R. RyanTM <ryantm-bot@ryantm.com>
2020-07-10 15:07:15 +0000

    python27Packages.pytest-isort: 1.0.0 -> 1.1.0

--- pkgs/development/python-modules/pytest-isort/default.nix
+++ pkgs/development/python-modules/pytest-isort/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "pytest-isort";
-  version = "1.0.0";
+  version = "1.1.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "758156cb4dc1db72adc1b7e253011f5eea117fab32af03cedb4cbfc6058b5f8f";
+    sha256 = "01j0sx8yxd7sbmvwky68mvnwrxxs5bjkvi61043jzff1ga92kg9h";
   };
 
   propagatedBuildInputs = [ isort ];
