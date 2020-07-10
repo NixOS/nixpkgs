f655ea378abeefd01b4466c23cbc5f5cb26122bf
Daniël de Kok <me@danieldk.eu>
2020-07-10 20:34:25 +0200

    rocclr: init at 3.5.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9235,6 +9235,10 @@ in
 
   rgbds = callPackage ../development/compilers/rgbds { };
 
+  rocclr = callPackage ../development/libraries/rocclr {
+    inherit (llvmPackages_rocm) clang;
+  };
+
   rocm-cmake = callPackage ../development/tools/build-managers/rocm-cmake { };
 
   rocm-device-libs = callPackage ../development/libraries/rocm-device-libs {
