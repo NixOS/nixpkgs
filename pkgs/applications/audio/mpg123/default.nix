5865de8f416b1508b011583f99b5ede8345b0ea1
R. RyanTM <ryantm-bot@ryantm.com>
2020-07-10 01:20:39 +0000

    mpg123: 1.26.1 -> 1.26.2

--- pkgs/applications/audio/mpg123/default.nix
+++ pkgs/applications/audio/mpg123/default.nix
@@ -3,11 +3,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "mpg123-1.26.1";
+  name = "mpg123-1.26.2";
 
   src = fetchurl {
     url = "mirror://sourceforge/mpg123/${name}.tar.bz2";
-    sha256 = "0cp01wdy77ggzqzzasxd5jd9iypcly5m4c89idc9mpgknyd65mkl";
+    sha256 = "1wrgds46wj6xsnqa6bi8kkh3wd29i2nxclbps34w5kjglrzbzxq0";
   };
 
   buildInputs = stdenv.lib.optional (!stdenv.isDarwin) alsaLib;
