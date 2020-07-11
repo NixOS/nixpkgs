bf6559d0084fa5defbb79636cc01d79b877a15a8
freezeboy <freezeboy@users.noreply.github.com>
2020-07-11 16:42:25 +0200

    fixup meta record

--- pkgs/applications/kde/granatier.nix
+++ pkgs/applications/kde/granatier.nix
@@ -5,7 +5,13 @@
 
 mkDerivation {
   name = "granatier";
-  meta = with lib; { maintainers = with maintainers; [ freezeboy ]; };
+  meta = with lib; {
+    homepage = "https://kde.org/applications/en/games/org.kde.granatier";
+    description = "Granatier is a clone of the classic Bomberman game";
+    maintainers = with maintainers; [ freezeboy ];
+    license = licenses.gpl2Plus;
+    platforms = platforms.linux;
+  };
   nativeBuildInputs = [
     extra-cmake-modules
   ];
