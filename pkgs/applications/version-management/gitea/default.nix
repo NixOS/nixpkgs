3970e7a1c07c0c17e5e6b1ab93b27d7f9e9570af
Maximilian Bosch <maximilian@mbosch.me>
2020-07-11 20:55:55 +0200

    gitea: 1.12.1 -> 1.12.2
    https://github.com/go-gitea/gitea/releases/tag/v1.12.2

--- pkgs/applications/version-management/gitea/default.nix
+++ pkgs/applications/version-management/gitea/default.nix
@@ -8,11 +8,11 @@ with stdenv.lib;
 
 buildGoPackage rec {
   pname = "gitea";
-  version = "1.12.1";
+  version = "1.12.2";
 
   src = fetchurl {
     url = "https://github.com/go-gitea/gitea/releases/download/v${version}/gitea-src-${version}.tar.gz";
-    sha256 = "0n92msf5pbgb5q6pa2p0nj9lnzs4y0qis62c5mp4hp8rc1j22wlb";
+    sha256 = "12zzb1c4cl07ccxaravkmia8vdyw5ffxrlx9v95ampvv6a0swpb9";
   };
 
   unpackPhase = ''
