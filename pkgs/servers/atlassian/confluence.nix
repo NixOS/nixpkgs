7b4e73294f36ab896ee0397e0e13c4b0d90596ba
Simon Lackerbauer <simon@lackerbauer.com>
2020-07-11 20:37:25 +0200

    atlassian-confluence: 7.5.1 -> 7.6.0 (#92923)
    * confluence: 7.5.1 -> 7.5.2
    
    * atlassian-confluence: 7.5.2 -> 7.6.0

--- pkgs/servers/atlassian/confluence.nix
+++ pkgs/servers/atlassian/confluence.nix
@@ -8,11 +8,11 @@ assert withMysql -> (mysql_jdbc != null);
 
 stdenvNoCC.mkDerivation rec {
   pname = "atlassian-confluence";
-  version = "7.5.1";
+  version = "7.6.0";
 
   src = fetchurl {
     url = "https://product-downloads.atlassian.com/software/confluence/downloads/${pname}-${version}.tar.gz";
-    sha256 = "0lxvff0sn1kxsm599lq72hw11qnwjn2da3mz1h8mqz0rn2adhg07";
+    sha256 = "1s69b19kz8z8dbac3dsj9yvkvynlygzgnlpm72fbnqg6knp95fyz";
   };
 
   buildPhase = ''
