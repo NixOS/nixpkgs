8b66a6f863729a9223f31f20adc340113b4ec14a
Peter Sanford <psanford@sanford.io>
2020-07-12 16:29:47 -0700

    wormhole-william: init at v1.0.4
    wormhole-william is an end-to-end encrypted file transfer tool.
    It is compatible with Magic Wormhole.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7544,6 +7544,8 @@ in
     inherit (xorg) libxkbfile;
   };
 
+  wormhole-william = callPackage ../tools/networking/wormhole-william { };
+
   wpscan = callPackage ../tools/security/wpscan { };
 
   wsmancli = callPackage ../tools/system/wsmancli {};
