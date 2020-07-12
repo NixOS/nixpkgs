4cc8813cc3a34437f31138b0aec1585d06c46eb8
freezeboy <freezeboy@users.noreply.github.com>
2020-07-12 11:01:01 +0200

    commitizen: init at 4.2.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1622,6 +1622,8 @@ in
 
   colpack = callPackage ../applications/science/math/colpack { };
 
+  commitizen = callPackage ../applications/version-management/commitizen {};
+
   compactor = callPackage ../applications/networking/compactor { };
 
   consul = callPackage ../servers/consul { };
