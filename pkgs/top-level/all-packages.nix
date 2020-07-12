554c1c4c24a2d95939697a35158d59b3c2004326
freezeboy <freezeboy@users.noreply.github.com>
2020-07-12 10:22:25 +0200

    newman: init at 5.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14217,6 +14217,8 @@ in
 
   nettle = callPackage ../development/libraries/nettle { };
 
+  newman = callPackage ../development/web/newman {};
+
   newt = callPackage ../development/libraries/newt { };
 
   nghttp2 = callPackage ../development/libraries/nghttp2 { };
