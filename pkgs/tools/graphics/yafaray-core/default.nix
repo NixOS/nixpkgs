f4a2b8a26253931d9b7540c1c32a1c55937cb512
R. RyanTM <ryantm-bot@ryantm.com>
2020-07-12 15:22:35 +0000

    yafaray-core: 3.4.4 -> 3.5.0

--- pkgs/tools/graphics/yafaray-core/default.nix
+++ pkgs/tools/graphics/yafaray-core/default.nix
@@ -7,13 +7,13 @@
 stdenv.mkDerivation rec {
 
     pname = "yafaray-core";
-    version = "3.4.4";
+    version = "3.5.0";
 
     src = fetchFromGitHub {
       owner  = "YafaRay";
       repo   = "Core";
       rev    = "v${version}";
-      sha256 = "140vnaihz09rdvp0mqgrs26b3a2fv5wm49ar6817p4qklfx71l0g";
+      sha256 = "05r08ynm6p9jq9l3v1v3lqkrfx3bm6zlqcxm1yk7mdv1zv2yxikd";
     };
 
     preConfigure = ''
