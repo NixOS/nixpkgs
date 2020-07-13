0d2112dae7380f881f23e9ad12ca563918f4a830
R. RyanTM <ryantm-bot@ryantm.com>
2020-07-13 15:36:18 +0000

    libfilezilla: 0.22.0 -> 0.23.0

--- pkgs/development/libraries/libfilezilla/default.nix
+++ pkgs/development/libraries/libfilezilla/default.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation rec {
   pname = "libfilezilla";
-  version = "0.22.0";
+  version = "0.23.0";
 
   src = fetchurl {
     url = "https://download.filezilla-project.org/${pname}/${pname}-${version}.tar.bz2";
-    sha256 = "0ylgxw1lxdqvayy5285mlfrkr9cvsgasy2zci6g6mv9rng261xn5";
+    sha256 = "0lk84aw5ylrhpy26djdw3byhjbn9qrzx5k98r0i4nwfizckw3smd";
   };
 
   nativeBuildInputs = [ pkgconfig ];
