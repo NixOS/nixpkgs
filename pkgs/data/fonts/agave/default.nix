fe9baaa69d9ae12357a172d5759ecf0c398715af
Will Dietz <w@wdtz.org>
2020-07-13 15:24:05 -0500

    agave: 16 -> 17
    https://github.com/agarick/agave/releases/tag/v17

--- pkgs/data/fonts/agave/default.nix
+++ pkgs/data/fonts/agave/default.nix
@@ -2,7 +2,7 @@
 
 let
   pname = "agave";
-  version = "16";
+  version = "17";
 in fetchurl {
   name = "${pname}-${version}";
   url = "https://github.com/agarick/agave/releases/download/v${version}/Agave-Regular.ttf";
@@ -13,7 +13,7 @@ in fetchurl {
     install -D $downloadedFile $out/share/fonts/truetype/Agave-Regular.ttf
   '';
 
-  sha256 = "108jvcijnx06v1jzhnb28ql9nvmwqd83309834wcd4aii6bgf9ka";
+  sha256 = "1h61j1nbiwip71gxmz8my6xwy0j8ahf2p39hmrl1rxpsqvsj6dlz";
 
   meta = with lib; {
     description = "truetype monospaced typeface designed for X environments";
