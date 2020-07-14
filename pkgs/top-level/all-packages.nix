032f4db70117d282917415770ff463ebd2587dd8
Jaka Hudoklin <jaka@x-truder.net>
2020-07-14 22:40:36 +0200

    protonvpn-cli: replace old protonvpn-cli with protonvpn-cli-ng

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22254,8 +22254,6 @@ in
 
   protonvpn-cli = callPackage ../applications/networking/protonvpn-cli { };
 
-  protonvpn-cli-ng = callPackage ../applications/networking/protonvpn-cli-ng { };
-
   ps2client = callPackage ../applications/networking/ps2client { };
 
   psi = libsForQt5.callPackage ../applications/networking/instant-messengers/psi { };
