8aa47c15ccf06c36f791a2e13ea9ea791884c458
Arnout Engelen <arnout@bzzt.net>
2020-07-14 08:23:21 +0200

    pythonPackages.enaml: init at 0.11.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2482,6 +2482,8 @@ in {
 
   envs = callPackage ../development/python-modules/envs { };
 
+  enaml = callPackage ../development/python-modules/enaml { };
+
   etelemetry = callPackage ../development/python-modules/etelemetry { };
 
   eth-hash = callPackage ../development/python-modules/eth-hash { };
