02188cbd7445614e0456b00b906e7fc0d1b87a50
claudiiii <claudi+github@grimm.link>
2020-07-15 22:39:34 +0200

    element-desktop, element-web: init at 1.7.0
    (cherry picked from commit a705201e1d771444667c59c8cc2357c4afb88e21)

--- pkgs/applications/networking/instant-messengers/element/element-web.nix
+++ pkgs/applications/networking/instant-messengers/element/element-web.nix
@@ -1,22 +1,22 @@
 { lib, stdenv, fetchurl, writeText, jq, conf ? {} }:
 
 # Note for maintainers:
-# Versions of `riot-web` and `riot-desktop` should be kept in sync.
+# Versions of `element-web` and `element-desktop` should be kept in sync.
 
 let
   noPhoningHome = {
     disable_guests = true; # disable automatic guest account registration at matrix.org
     piwik = false; # disable analytics
   };
-  configOverrides = writeText "riot-config-overrides.json" (builtins.toJSON (noPhoningHome // conf));
+  configOverrides = writeText "element-config-overrides.json" (builtins.toJSON (noPhoningHome // conf));
 
 in stdenv.mkDerivation rec {
-  pname = "riot-web";
-  version = "1.6.7";
+  pname = "element-web";
+  version = "1.7.0";
 
   src = fetchurl {
     url = "https://github.com/vector-im/riot-web/releases/download/v${version}/riot-v${version}.tar.gz";
-    sha256 = "14g5rbvcw6p0q50slbf0c7s39i54ggv2cjc5iv93yvfrwn3z0f8p";
+    sha256 = "033793zx3c95z5ikq70kxajwlbg2b8785wrgjr1zyha3pjq79bm6";
   };
 
   installPhase = ''
@@ -31,8 +31,8 @@ in stdenv.mkDerivation rec {
 
   meta = {
     description = "A glossy Matrix collaboration client for the web";
-    homepage = http://riot.im/;
-    maintainers = with stdenv.lib.maintainers; [ bachp pacien ];
+    homepage = "https://element.io/";
+    maintainers = with stdenv.lib.maintainers; [ pacien worldofpeace ma27 ];
     license = stdenv.lib.licenses.asl20;
     platforms = stdenv.lib.platforms.all;
     hydraPlatforms = [];
