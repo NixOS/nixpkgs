dae5c55cbc28957caa28b0159501aedeb4bdde10
José Romildo Malaquias <malaquias@gmail.com>
2020-07-16 18:26:40 -0300

    kde-frameworks.kdesu: search the daemon first in /run/wrappers/bin
    If looking first in libexec, the eventually wrapped one in
    /run/wrappers/bin can not be found.
    
    This allows wrapping the daemon so that it can be run with sgid
    privileges.

--- pkgs/development/libraries/kde-frameworks/kdesu.nix
+++ pkgs/development/libraries/kde-frameworks/kdesu.nix
@@ -11,4 +11,5 @@ mkDerivation {
   buildInputs = [ kcoreaddons ki18n kpty kservice qtbase ];
   propagatedBuildInputs = [ kpty ];
   outputs = [ "out" "dev" ];
+  patches = [ ./kdesu-search-for-wrapped-daemon-first.patch ];
 }
