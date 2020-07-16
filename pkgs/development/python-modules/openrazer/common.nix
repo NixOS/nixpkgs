8796d0be6de607f83f96beb289a1d8ec183c1c75
Evan Stoll <evanjsx@gmail.com>
2020-07-16 10:53:00 -0400

    openrazer: 2.7.0 -> 2.8.0

--- pkgs/development/python-modules/openrazer/common.nix
+++ pkgs/development/python-modules/openrazer/common.nix
@@ -1,12 +1,12 @@
 { stdenv
 , fetchFromGitHub
 }: rec {
-  version = "2.7.0";
+  version = "2.8.0";
   src = fetchFromGitHub {
     owner = "openrazer";
     repo = "openrazer";
     rev = "v${version}";
-    sha256 = "013r9q4xg2xjmyxybx07zsl2b5lm9vw843anx22ygpvxz1qgz9hp";
+    sha256 = "0mwg6b2y3wfpvgxb9lznwblb3bnrayn858nc4fbbg76zdp5bk5ky";
   };
   meta = with stdenv.lib; {
     homepage = "https://openrazer.github.io/";
