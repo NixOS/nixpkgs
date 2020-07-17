c9ec0f91afb492bff66fda968ac4605b92f74c69
Derpford <10187719+Derpford@users.noreply.github.com>
2020-07-16 23:44:21 -0500

    fluidsynth -> fluidsynth_1

--- pkgs/games/gzdoom/default.nix
+++ pkgs/games/gzdoom/default.nix
@@ -73,7 +73,7 @@ let
     '';
     
     postInstall = ''
-      patchelf --set-rpath "${SDL2}/lib:${openal}/lib:${fluidsynth}/lib:${bzip2}/lib:${zlib}/lib:${libjpeg}/lib:${glibc}/lib:${gcc}/lib:${mpg123}/lib" $out/lib/gzdoom/gzdoom
+      patchelf --set-rpath "${SDL2}/lib:${openal}/lib:${fluidsynth_1}/lib:${bzip2}/lib:${zlib}/lib:${libjpeg}/lib:${glibc}/lib:${gcc}/lib:${mpg123}/lib" $out/lib/gzdoom/gzdoom
     '';
 
     meta = with stdenv.lib; {
