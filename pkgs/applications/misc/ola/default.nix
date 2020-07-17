611183af0190601e8541a18d367454b097b4f29e
Alyssa Ross <hi@alyssa.is>
2020-07-17 15:25:02 +0000

    ola: enable parallel building
    Works fine for me at -j48.

--- pkgs/applications/misc/ola/default.nix
+++ pkgs/applications/misc/ola/default.nix
@@ -34,6 +34,8 @@ stdenv.mkDerivation rec {
 
   configureFlags = [ "--enable-python-libs" ];
 
+  enableParallelBuilding = true;
+
   meta = with stdenv.lib; {
     description = "A framework for controlling entertainment lighting equipment.";
     homepage = "https://www.openlighting.org/ola/";
