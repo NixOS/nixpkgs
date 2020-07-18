16d8632355b2ab03b60789e18f4e539f68d4a8a7
pacien <pacien.trangirard@pacien.net>
2020-07-18 12:22:34 +0200

    beamerpresenter: 0.1.1 -> 0.1.2

--- pkgs/applications/office/beamerpresenter/default.nix
+++ pkgs/applications/office/beamerpresenter/default.nix
@@ -1,15 +1,15 @@
-{ stdenv, mkDerivation, fetchFromGitHub, makeDesktopItem, installShellFiles,
+{ stdenv, mkDerivation, fetchFromGitHub, installShellFiles,
   qmake, qtbase, poppler, qtmultimedia }:
 
 mkDerivation rec {
   pname = "beamerpresenter";
-  version = "0.1.1";
+  version = "0.1.2";
 
   src = fetchFromGitHub {
     owner = "stiglers-eponym";
     repo = "BeamerPresenter";
     rev = "v${version}";
-    sha256 = "0j7wx3qqwhda33ig2464bi0j0a473y5p7ndy5f7f8x9cqdal1d01";
+    sha256 = "12xngnhwa3haf0pdxczgvhq1j20zbsr30y2bfn9qwmlhbwklhkj2";
   };
 
   nativeBuildInputs = [ qmake installShellFiles ];
@@ -26,22 +26,9 @@ mkDerivation rec {
   installPhase = ''
     install -m755 beamerpresenter -Dt $out/bin/
     install -m644 src/icons/beamerpresenter.svg -Dt $out/share/icons/hicolor/scalable/apps/
-    install -m644 $desktopItem/share/applications/*.desktop -Dt $out/share/applications/
+    install -m644 share/applications/beamerpresenter.desktop -Dt $out/share/applications/
     installManPage man/*.{1,5}
   '';
-
-  # TODO: replace with upstream's .desktop file once available.
-  # https://github.com/stiglers-eponym/BeamerPresenter/pull/4
-  desktopItem = makeDesktopItem {
-    name = pname;
-    desktopName = "BeamerPresenter";
-    genericName = "Beamer presentation viewer";
-    comment = "Simple dual screen pdf presentation software";
-    icon = "beamerpresenter";
-    categories = "Office;";
-    exec = "beamerpresenter %F";
-    mimeType = "application/pdf;application/x-pdf;";
-  };
 
   meta = with stdenv.lib; {
     description = "Simple dual screen pdf presentation software";
