55c5c222c2e92bd6e40f0e31288844b7d45028db
Thomas Friese <tasmo@tasmo.de>
2020-07-18 22:13:42 +0200

    bitwig-studio3: 3.2.2 -> 3.2.6

--- pkgs/applications/audio/bitwig-studio/bitwig-studio3.nix
+++ pkgs/applications/audio/bitwig-studio/bitwig-studio3.nix
@@ -2,11 +2,11 @@
 
 bitwig-studio1.overrideAttrs (oldAttrs: rec {
   name = "bitwig-studio-${version}";
-  version = "3.2.4";
+  version = "3.2.6";
 
   src = fetchurl {
     url = "https://downloads.bitwig.com/stable/${version}/bitwig-studio-${version}.deb";
-    sha256 = "11ilcsah79p8v9bpz2r6mk3c6glrwm3idx8n2k4cwvldsg2yvifl";
+    sha256 = "00hrbgnjns3s8lbjbabwwqvbwz4dlrg33cs3d1qlpzgi3y72h3nn";
   };
 
   buildInputs = oldAttrs.buildInputs ++ [ xorg.libXtst ];
