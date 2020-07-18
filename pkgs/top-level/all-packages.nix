d85f50b71fa58651bbc7baef3fcc29a4a271eef3
Janne Heß <janne@hess.ooo>
2020-07-18 17:46:13 +0200

    nixos/gitlab: Support pages
    Fixes #84525

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16295,6 +16295,8 @@ in
 
   gatling = callPackage ../servers/http/gatling { };
 
+  gitlab-pages = callPackage ../servers/http/gitlab-pages { };
+
   glabels = callPackage ../applications/graphics/glabels { };
 
   nats-server = callPackage ../servers/nats-server { };
