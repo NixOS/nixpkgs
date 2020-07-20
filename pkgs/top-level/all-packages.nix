1de96eba5a3ec4e54a07b232fcf0b993ec8fce9a
volth <volth@volth.com>
2020-07-20 04:55:05 +0000

    oraclejdk11: init at 11.0.8, oraclejdk14: init at 14.0.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8967,6 +8967,10 @@ in
         inherit installjdk pluginSupport;
       });
 
+  oraclejdk11 = callPackage ../development/compilers/oraclejdk/jdk11-linux.nix { };
+
+  oraclejdk14 = callPackage ../development/compilers/oraclejdk/jdk14-linux.nix { };
+
   jasmin = callPackage ../development/compilers/jasmin { };
 
   java-service-wrapper = callPackage ../tools/system/java-service-wrapper { };
