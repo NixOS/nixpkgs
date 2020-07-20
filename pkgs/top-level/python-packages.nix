0bd7760b9b2b734b52553e8e1318bd246f59c4e5
laikq <55911173+laikq@users.noreply.github.com>
2020-07-20 18:06:16 +0200

    python: rtmixer: init at 0.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3500,6 +3500,8 @@ in {
 
   mailman-web = callPackage ../servers/mail/mailman/web.nix { };
 
+  rtmixer = callPackage ../development/python-modules/rtmixer { };
+
   mail-parser = callPackage ../development/python-modules/mail-parser { };
 
   Mako = callPackage ../development/python-modules/Mako { };
