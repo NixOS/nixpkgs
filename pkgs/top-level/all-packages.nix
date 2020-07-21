a9582cd0c4911c1d526a5b4cf17e0224ec56839b
Peter Simons <simons@cryp.to>
2020-07-21 14:15:58 +0200

    haskellPackages: bump default compiler to ghc-8.8.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8717,7 +8717,7 @@ in
 
   # Please update doc/languages-frameworks/haskell.section.md, “Our
   # current default compiler is”, if you bump this:
-  haskellPackages = dontRecurseIntoAttrs haskell.packages.ghc883;
+  haskellPackages = dontRecurseIntoAttrs haskell.packages.ghc884;
 
   inherit (haskellPackages) ghc;
 
