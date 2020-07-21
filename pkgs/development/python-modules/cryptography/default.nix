434a0111f6a7618a7024b637c5496ea261b23016
Michael Weiss <dev.primeos@gmail.com>
2020-07-21 02:01:15 +0200

    python3Packages.cryptography: 2.9.2 -> 3.0
    Backwards incompatible changes:
    - Removed support for passing an Extension instance to
      from_issuer_subject_key_identifier(), as per our deprecation policy.
    - Support for LibreSSL 2.7.x, 2.8.x, and 2.9.0 has been removed (2.9.1+
      is still supported).
    - Dropped support for macOS 10.9, macOS users must upgrade to 10.10 or
      newer.
    - RSA generate_private_key() no longer accepts public_exponent values
      except 65537 and 3 (the latter for legacy purposes).
    - X.509 certificate parsing now enforces that the version field contains
      a valid value, rather than deferring this check until version is
      accessed.
    
    Deprecations:
    - Deprecated support for Python 2. At the time there is no time table
      for actually dropping support, however we strongly encourage all users
      to upgrade their Python, as Python 2 no longer receives support from
      the Python core team.

--- pkgs/development/python-modules/cryptography/default.nix
+++ pkgs/development/python-modules/cryptography/default.nix
@@ -22,11 +22,11 @@
 
 buildPythonPackage rec {
   pname = "cryptography";
-  version = "2.9.2"; # Also update the hash in vectors.nix
+  version = "3.0"; # Also update the hash in vectors.nix
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a0c30272fb4ddda5f5ffc1089d7405b7a71b0b0f51993cb4e5dbb4590b2fc229";
+    sha256 = "0lr06a9317n2iwfqwz9mpalqm99acqwk1478arvyj1jj0ay4v4lf";
   };
 
   outputs = [ "out" "dev" ];
