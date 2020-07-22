3b586eb5e83252530d71e80a962d950b134b49b4
Benjamin Andresen <b@lambda.icu>
2020-07-22 22:45:44 +0200

    maintainer-list: change bandresen -> bennyandresen

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -859,12 +859,6 @@
     githubId = 18467667;
     name = "Alexander Bantyev";
   };
-  bandresen = {
-    email = "bandresen@gmail.com";
-    github = "bandresen";
-    githubId = 80325;
-    name = "Benjamin Andresen";
-  };
   baracoder = {
     email = "baracoder@googlemail.com";
     github = "baracoder";
@@ -948,6 +942,12 @@
     github = "bennofs";
     githubId = 3192959;
     name = "Benno Fünfstück";
+  };
+  bennyandresen = {
+    email = "b@lambda.icu";
+    github = "bennyandresen";
+    githubId = 80325;
+    name = "Benjamin Andresen";
   };
   benpye = {
     email = "ben@curlybracket.co.uk";
