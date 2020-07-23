71532e92dbce6fcc1ca87e48e5a9b402030b8908
Mario Rodas <marsam@users.noreply.github.com>
2020-07-22 19:55:28 -0500

    nodejs-14_x: 14.5.0 -> 14.6.0
    https://github.com/nodejs/node/releases/tag/v14.6.0

--- pkgs/development/web/nodejs/v14.nix
+++ pkgs/development/web/nodejs/v14.nix
@@ -1,14 +1,14 @@
-{ callPackage, openssl, icu66, python3, enableNpm ? true }:
+{ callPackage, openssl, icu67, python3, enableNpm ? true }:
 
 let
   buildNodejs = callPackage ./nodejs.nix {
     inherit openssl;
-    icu = icu66;
+    icu = icu67;
     python = python3;
   };
 in
   buildNodejs {
     inherit enableNpm;
-    version = "14.5.0";
-    sha256 = "1d6w7ycdiqbkip7m6m8xly31qgx7ywakzvrnqdq8ini5sricjlgb";
+    version = "14.6.0";
+    sha256 = "153a07ffrmvwbsc78wrc0xnwymmzrhva0kn6mgnfi3086v3h1wss";
   }
