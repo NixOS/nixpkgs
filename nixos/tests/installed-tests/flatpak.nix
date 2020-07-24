379038b4dc818c89c931a4ce6cfc14c56fc811ec
Jan Tojnar <jtojnar@gmail.com>
2020-07-24 21:02:02 +0200

    nixosTests.flatpak: clean up
    GNOME is not necessary. Portals probably are not either,
    but the NixOS module requires them.
    
    Not sure why it did not work without GNOME before.

--- nixos/tests/installed-tests/flatpak.nix
+++ nixos/tests/installed-tests/flatpak.nix
@@ -5,10 +5,7 @@ makeInstalledTest {
   withX11 = true;
 
   testConfig = {
-    services.xserver.desktopManager.gnome3.enable = true; # TODO: figure out minimal environment where the tests work
-    # common/x11.nix enables the auto display manager (lightdm)
-    services.xserver.displayManager.gdm.enable = false;
-    services.gnome3.core-utilities.enable = false;
+    xdg.portal.enable = true;
     services.flatpak.enable = true;
     environment.systemPackages = with pkgs; [ gnupg ostree python3 ];
     virtualisation.memorySize = 2047;
