2138371c00d3852be7795b0b489b7524002fc9b6
Daniël de Kok <me@danieldk.eu>
2020-07-24 11:24:53 +0200

    Revert "nomad: 0.11.3 -> 0.12.1"
    This reverts commit 36b2073298fb4f16fd969821ef34468f8de78edd.

--- pkgs/applications/networking/cluster/nomad/default.nix
+++ pkgs/applications/networking/cluster/nomad/default.nix
@@ -2,7 +2,7 @@
 
 buildGoPackage rec {
   pname = "nomad";
-  version = "0.12.1";
+  version = "0.11.3";
   rev = "v${version}";
 
   goPackagePath = "github.com/hashicorp/nomad";
@@ -12,7 +12,7 @@ buildGoPackage rec {
     owner = "hashicorp";
     repo = pname;
     inherit rev;
-    sha256 = "05q8k2kpbknjwxw893v7p50hgrdljfgx80h8bhnny8q7vrshn51s";
+    sha256 = "1p7g7x2gl77h1w7aip3xji3s530fj46gspargz4j3i6h4wkyvafb";
   };
 
   # ui:
