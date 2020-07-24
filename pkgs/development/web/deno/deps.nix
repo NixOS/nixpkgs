3c45c69f49d14e6945b695204f9c03dd7ea29ea9
06kellyjac <dev@j-k.io>
2020-07-24 16:10:55 +0000

    deno: 1.2.0 -> 1.2.1

--- pkgs/development/web/deno/deps.nix
+++ pkgs/development/web/deno/deps.nix
@@ -2,11 +2,11 @@
 {}:
 rec {
   rustyV8Lib = {
-    version = "0.6.0";
+    version = "0.7.0";
     sha256s = {
-      x86_64-linux = "1mpgddlcsfc5jpkqpwpbld9d2pr3dflkbd1w9ryai0aymv8r2rmb";
-      aarch64-linux = "0qqr0i6z39fb298azp2rm0ns0bw3il3xj5zy58cn96h624rxkn67";
-      x86_64-darwin = "0pj7127cvz01kq6vs91sdxkwkbjx7j3cv7lwvyilifbl01j3vwip";
+      x86_64-linux = "0ggbbppb65pdj92qbqw8sy8w84ir42fnhyk599pihyx1mqqih606";
+      aarch64-linux = "1n30xi4fac77cvd5qfr3mkgvipp9pq0mw24jbxbhlcl0n8mvpxjv";
+      x86_64-darwin = "1kdcapzj9my3ippqlkgdgp2dnj4klyl1wz1jwwps3b00l593nmfh";
     };
   };
 }
