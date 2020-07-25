9ab83c6eed9d1c6840940f5918713383346706a3
Sebastien Bariteau <numkem@numkem.org>
2020-07-24 21:16:21 -0400

    ugrep: init at 2.4.1 (#93296)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7343,6 +7343,8 @@ in
   uget = callPackage ../tools/networking/uget { };
 
   uget-integrator = callPackage ../tools/networking/uget-integrator { };
+  
+  ugrep = callPackage ../tools/text/ugrep { };
 
   uif2iso = callPackage ../tools/cd-dvd/uif2iso { };
 
