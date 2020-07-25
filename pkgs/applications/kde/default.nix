1683ad9b368d64793c33f8c6ebaa02e11c6697fe
freezeboy <freezeboy@users.noreply.github.com>
2020-07-26 01:12:33 +0200

    katomic: init at 20.04.3

--- pkgs/applications/kde/default.nix
+++ pkgs/applications/kde/default.nix
@@ -94,6 +94,7 @@ let
       kalarmcal = callPackage ./kalarmcal.nix {};
       kapptemplate = callPackage ./kapptemplate.nix { };
       kate = callPackage ./kate.nix {};
+      katomic = callPackage ./katomic.nix {};
       kbounce = callPackage ./kbounce.nix {};
       kbreakout = callPackage ./kbreakout.nix {};
       kcachegrind = callPackage ./kcachegrind.nix {};
