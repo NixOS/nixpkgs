d7fc4fe2815b193a28951463839afb09b1b7126b
buffet <niclas@countingsort.com>
2020-07-26 17:57:24 +0000

    kak-auto-pairs: 2019-07-27 -> 2020-07-14

--- pkgs/applications/editors/kakoune/plugins/kak-auto-pairs.nix
+++ pkgs/applications/editors/kakoune/plugins/kak-auto-pairs.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchFromGitHub }:
 stdenv.mkDerivation {
   name = "kak-auto-pairs";
-  version = "2019-07-27";
+  version = "2020-07-14";
   src = fetchFromGitHub {
     owner = "alexherbo2";
     repo = "auto-pairs.kak";
-    rev = "886449b1a04d43e5deb2f0ef4b1aead6084c7a5f";
-    sha256 = "0knfhdvslzw1f1r1k16733yhkczrg3yijjz6n2qwira84iv3239j";
+    rev = "5b4b3b723c34c8b7f40cee60868204974349bf9f";
+    sha256 = "1wgrv03f1lkzflbbaz8n23glij5rvfxf8pcqysd668mbx1hcrk9i";
   };
 
   installPhase = ''
