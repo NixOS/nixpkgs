a6f47d9bdc3f8b2c50aeeb01108b81ae169c5735
Andrew Childs <lorne@cons.org.nz>
2020-07-26 17:27:15 +0900

    libtapi: fix build, install phase and install name
    installTarget looks like a typo for installTargets. This causes a lot
    of llvm and clang to be built and installed.
    
    Clang is not intended to be an external dependency. The source bundle
    includes llvm and clang. Adding include paths and building clangBasic
    first is sufficient to use the internal clang components.

--- pkgs/os-specific/darwin/libtapi/default.nix
+++ pkgs/os-specific/darwin/libtapi/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, cmake, python3, clang_6 }:
+{ lib, stdenv, fetchFromGitHub, cmake, python3 }:
 
 stdenv.mkDerivation {
   name = "libtapi-1000.10.8";
@@ -9,16 +9,25 @@ stdenv.mkDerivation {
     sha256 = "1zb10p6xkls8x7wsdwgy9c0v16z97rfkgidii9ffq5rfczgvrhjh";
   };
 
-  nativeBuildInputs = [ cmake python3 ];
-  buildInputs = [ clang_6.cc ];
+  sourceRoot = "source/src/llvm";
 
-  preConfigure = ''
-    cd src/llvm
-  '';
+  nativeBuildInputs = [ cmake python3 ];
 
   cmakeFlags = [ "-DLLVM_INCLUDE_TESTS=OFF" ];
 
-  buildFlags = [ "libtapi" ];
+  # fixes: fatal error: 'clang/Basic/Diagnostic.h' file not found
+  # adapted from upstream
+  # https://github.com/tpoechtrager/apple-libtapi/blob/3cb307764cc5f1856c8a23bbdf3eb49dfc6bea48/build.sh#L58-L60
+  preConfigure = ''
+    INCLUDE_FIX="-I $PWD/projects/clang/include"
+    INCLUDE_FIX+=" -I $PWD/build/projects/clang/include"
+
+    cmakeFlagsArray+=(-DCMAKE_CXX_FLAGS="$INCLUDE_FIX")
+  '';
+
+  buildFlags = [ "clangBasic" "libtapi" ];
+
+  installTargets = [ "install-libtapi" "install-tapi-headers" ];
 
   installTargets = [ "install-libtapi" "install-tapi-headers"];
 
@@ -26,5 +35,4 @@ stdenv.mkDerivation {
     license = licenses.apsl20;
     maintainers = with maintainers; [ matthewbauer ];
   };
-
 }
