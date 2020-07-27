98f04be570ca4a818a541995bbf1cb302815e9f0
zowoq <59103226+zowoq@users.noreply.github.com>
2020-07-27 20:46:33 +1000

    hugo: use modvendorCopy

--- pkgs/applications/misc/hugo/default.nix
+++ pkgs/applications/misc/hugo/default.nix
@@ -13,21 +13,8 @@ buildGoModule rec {
     sha256 = "0qhv8kdv5k1xfk6106lxvsz7f92k7w6wk05ngz7qxbkb6zkcnshw";
   };
 
-  golibsass = fetchFromGitHub {
-    owner = "bep";
-    repo = "golibsass";
-    rev = "8a04397f0baba474190a9f58019ff499ec43057a";
-    sha256 = "0xk3m2ynbydzx87dz573ihwc4ryq0r545vz937szz175ivgfrhh3";
-  };
-
-  overrideModAttrs = (_: {
-      postBuild = ''
-      rm -rf vendor/github.com/bep/golibsass/
-      cp -r --reflink=auto ${golibsass} vendor/github.com/bep/golibsass
-      '';
-    });
-
-  vendorSha256 = "07dkmrldsxw59v6r4avj1gr4hsaxybhb14qv61hc777qix2kq9v1";
+  modvendorCopy = true;
+  vendorSha256 = "0ypgjn9lkdj48s3cbmaimh0l7md18kkxn6j4zrl5riah9a0bmghg";
 
   buildFlags = [ "-tags" "extended" ];
 
