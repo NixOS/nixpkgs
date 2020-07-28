f268706b20968bd2c3a4e8e345f2922a9d682f37
Noah Hendrickson <nhendrickson@backtrace.io>
2020-07-27 23:58:17 -0400

    Revamped teleport module. Can now set individual teleport services in nix expression lang.

--- nixos/modules/services/networking/teleport.nix
+++ nixos/modules/services/networking/teleport.nix
@@ -11,13 +11,15 @@ let
       echo '${builtins.toJSON x}' | ${pkgs.jq}/bin/jq . > $out
     ";
   # This becomes the main config file
-  # To be used in the future when I add seperate option sections for each part of teleport
-  # Rather then writing a single yaml file directly into your nix file.
   telConfig = {
-    global = cfg.globalConfig;
-    rule_files = cfg.ruleFiles ++ [
-      (pkgs.writeText "teleport.rules" (concatStringsSep "\n" cfg.rules))
-    ];
+    teleport = {
+      nodename = cfg.nodename;
+      auth_token = cfg.auth_token;
+      auth_servers = cfg.auth_servers;
+      auth_service = cfg.auth_service;
+      ssh_service = cfg.ssh_service;
+      proxy_service = cfg.proxy_service;
+    };
   };
 
   generatedTeleportYml = writePrettyJSON "teleport.yaml" telConfig;
@@ -28,6 +30,67 @@ let
     else generatedTeleportYml;
 
   cmdlineArgs = "-c ${teleportYml} --pid-file=/run/teleport.pid";
+
+  telTypes.auth_service = types.submodule {
+    options = {
+      enabled = mkEnableOption {
+        type = types.bool;
+        default = false;
+        description = "
+          Enable the Teleport Cluster SSH daemon.
+        ";
+      };
+      cluster_name = mkOption {
+        type = types.str;
+      };
+      listen_addr = mkOption {
+        type = types.str;
+      };
+      public_addr = mkOption {
+        type = types.str;
+      };
+      tokens = mkOption {
+        type = types.listOf types.str;
+      };
+    };
+  };
+  telTypes.ssh_service = types.submodule {
+    options = {
+      enabled = mkEnableOption {
+        type = types.bool;
+        default = false;
+        description = "
+          Enable the Teleport Cluster SSH daemon.
+        ";
+      };
+    };
+  };
+  telTypes.proxy_service = types.submodule {
+    options = {
+      enabled = mkEnableOption {
+        type = types.bool;
+        default = false;
+        description = "
+          Enable the Teleport Cluster SSH daemon.
+        ";
+      };
+      listen_addr = mkOption {
+        type = types.str;
+      };
+      web_listen_addr = mkOption {
+        type = types.str;
+      };
+      tunnel_listen_addr = mkOption {
+        type = types.str;
+      };
+      https_key_file = mkOption {
+        type = types.str;
+      };
+      https_cert_file = mkOption {
+        type = types.str;
+      };
+    };
+  };
 
 in
 
@@ -56,6 +119,48 @@ in
           teleport.yml.
         ";
       };
+      nodename = mkOption {
+        type = types.str;
+        default = "";
+        description = ''
+          The name of your teleport node.
+          This is what you `tsh ssh` to.
+        '';
+      };
+      auth_token = mkOption {
+        type = types.str;
+        default = types.null;
+        description = ''
+          The auth token your teleport node authenticates with to your teleport auth nodes.
+        '';
+      };
+      auth_servers = mkOption {
+        type = types.listOf types.str;
+        description = ''
+          The set of Auth servers your Teleport node authenticates against.
+        '';
+      };
+      auth_service = mkOption {
+        type = telTypes.auth_service;
+        default = {};
+        description = ''
+          Enable the Teleport SSH Auth service.
+        '';
+      };
+      ssh_service = mkOption {
+        type = telTypes.ssh_service;
+        default = {};
+        description = ''
+          Enable the Teleport SSH service.
+        '';
+      };
+      proxy_service = mkOption {
+        type = telTypes.proxy_service;
+        default = {};
+        description = ''
+          Enable the Teleport SSH Proxy service.
+        '';
+      };
     };
   };
 
@@ -69,7 +174,7 @@ in
           ExecReload = "/run/current-system/sw/bin/kill -HUP $MAINPID";
           PIDFile = "run/teleport.pid";
           Restart = "on-failure";
-          WorkingDirectory = ${cfg.dataDir};
+          WorkingDirectory = cfg.dataDir;
         };
       };
    };
