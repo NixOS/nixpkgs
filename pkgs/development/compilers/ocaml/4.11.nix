04f9cd0b0c118367a0c0ac54301402c7b9985732
Vincent Laporte <Vincent.Laporte@gmail.com>
2020-07-28 07:30:07 +0200

    ocaml-ng.ocamlPackages_4_11.ocaml: 4.11.0+β2 → 4.11.0+β3

--- pkgs/development/compilers/ocaml/4.11.nix
+++ pkgs/development/compilers/ocaml/4.11.nix
@@ -1,6 +1,6 @@
 import ./generic.nix {
   major_version = "4";
   minor_version = "11";
-  patch_version = "0+beta2";
-  sha256 = "1n39qfwvpwlhp9m73zi9hrk9ny9g6spy9apc36n2ydpf5dn7pg75";
+  patch_version = "0+beta3";
+  sha256 = "18lpgirxil00pgy805cyi97v6ycmg93sdvbkc60i35ili030v1f7";
 }
