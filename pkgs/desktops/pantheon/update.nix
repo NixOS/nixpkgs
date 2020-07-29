1526f4534d360eb654e012fe2225235ad42b804e
worldofpeace <worldofpeace@protonmail.ch>
2020-07-29 14:06:17 -0400

    pantheon: delete stray file

--- pkgs/desktops/pantheon/update.nix
+++
@@ -1,8 +0,0 @@
-{ nix-update }:
-
-{ attrPath }:
-
-
-# We're using nix-update https://github.com/Mic92/nix-update which can technically
-# update way more than just pantheon.
-[ "${nix-update}/bin/nix-update" attrPath ]
