7e9fd138fe7c552e943cf13af19bbd9a81712f46
Daniel Olsen <daniel.olsen99@gmail.com>
2020-07-29 11:30:33 +0200

    Guess Ill wait for fallback options now

--- nixos/modules/services/misc/matrix-corporal.nix
+++ nixos/modules/services/misc/matrix-corporal.nix
@@ -18,9 +18,10 @@ in
     settings = lib.mkOption {
       default = {};
       type = lib.types.submodule {
+        config._module.fallbackType = with lib.types; attrsOf (oneOf [ str int bool ]);
+
         options.Matrix.HomeserverDomainName = lib.mkOption {
           type = lib.types.str;
-          default = "localhost";
           description = ''
             The base domain name of your Matrix homeserver.
             This is what user identifiers contain (@user:example.com), and not necessarily the domain name where the Matrix homeserver is hosted (could actually be matrix.example.com)
@@ -34,15 +35,6 @@ in
             This would normally be a local address, as it's convenient to run matrix-corporal on the same machine as Matrix Synapse.
           '';
         };
-        options.Matrix.TimeoutMilliseconds = lib.mkOption {
-          type = lib.types.ints.unsigned;
-          default = 45000;
-          description = ''
-            how long (in milliseconds) HTTP requests (from matrix-corporal to Matrix Synapse) are allowed to take before being timed out.
-            Since clients often use long-polling for /sync (usually with a 30-second limit), setting this to a value of more than 30000 is recommended.
-          '';
-        };
-
         options.Reconciliation.UserId = lib.mkOption {
           type = lib.types.str;
           default = "@matrix-corporal:" + cfg.settings.Matrix.HomeserverDomainName;
@@ -50,13 +42,6 @@ in
             A full Matrix user id of the system (needs to have admin privileges), which will be used to perform reconciliation.
             This user account, with its admin privileges, will be used to find what users are available on the server, what their current state is, etc.
             This user account will also invite and kick users out of communities and rooms, so you need to make sure this user is joined to, and has the appropriate privileges, in all rooms and communities that you would like to manage.
-          '';
-        };
-        options.Reconciliation.RetryIntervalMilliseconds = lib.mkOption {
-          type = lib.types.ints.unsigned;
-          default = 30000;
-          description = ''
-            How long (in milliseconds) to wait before retrying reconciliation, in case the previous reconciliation attempt failed (due to Matrix Synapse being down, etc.).
           '';
         };
         options.HttpGateway.ListenAddress = lib.mkOption {
@@ -68,25 +53,10 @@ in
             If you're running this inside a container, use something like 0.0.0.0:41080.
           '';
         };
-        options.HttpGateway.TimeoutMilliseconds = lib.mkOption {
-          type = lib.types.addCheck lib.types.int (x: x >= cfg.settings.Matrix.TimeoutMilliseconds);
-          default = cfg.settings.Matrix.TimeoutMilliseconds;
-          description = ''
-            How long (in milliseconds) HTTP requests are allowed to take before being timed out.
-            Since clients often use long-polling for /sync (usually with a 30-second limit), setting this to a value of more than 30000 is recommended.
-            For this same reason, making this value larger than Matrix.TimeoutMilliseconds is required.
-          '';
-        };
-
         options.HttpApi.Enabled = lib.mkOption {
           type = lib.types.bool;
           default = true;
           description = "Whether the HTTP API is enabled or not";
-        };
-        options.HttpApi.TimeoutMilliseconds = lib.mkOption {
-          type = lib.types.ints.unsigned;
-          default = 10000; # Seems good?
-          description = "How long (in milliseconds) HTTP requests are allowed to take before being timed out.";
         };
         options.HttpApi.ListenAddress = lib.mkOption {
           type = lib.types.str;
@@ -96,49 +66,25 @@ in
             It's most likely a local one, as there's usually a reverse proxy (like nginx) capturing all traffic first and forwarding it here later on. If you're running this inside a container, use something like 0.0.0.0:41081.
           '';
         };
-
-        options.PolicyProvider = {
-          Type = lib.mkOption {
-            type = lib.types.strMatching "static_file|http|last_seen_store_policy";
-            default = "static_file";
+        options.PolicyProvider = lib.mkOption {
+          type = lib.types.attrs;
+          default = {
+            Type = "static_file";
+            Path = "/etc/matrix-corporal/policy.json";
           };
-          Path = lib.mkOption {
-            type = lib.types.path;
-            default = if cfg.settings.PolicyProvider.Type == "static_file" then "/etc/matrix-corporal/policy.json" else {};
-            description = "Path to policy file";
-          };
-          Uri = lib.mkOption {
-            type = lib.types.nullOr lib.types.str;
-            default = if cfg.settings.PolicyProvider.Type == "http" then "https://localhost/policy.json" else null;
-            description = "The URL from which matrix-corporal will fetch the policy (a GET request is made).";
-          };
-          CachePath = lib.mkOption {
-            type = lib.types.nullOr lib.types.path;
-            default = "/var/matrix-corporal/last-policy.json";
-            description = "A path to a local file, where matrix-corporal will store the last-fetched policy. It's important to store it locally to prevent downtime in case the policy provider is temporarily unavailable for some reason. Can be set to null to disable caching (not recommended).";
-          };
-          ReloadIntervalSeconds = lib.mkOption {
-            type = lib.types.nullOr lib.types.ints.positive;
-            default = 1800;
-            description = "An interval duration at which the policy provider will re-fetch the policy from the given URL. Can be set to 0 or null to disable reloading.";
-          };
-          TimeoutMilliseconds = lib.mkOption {
-            type = lib.types.nullOr lib.types.ints.positive;
-            default = 30000;
-            description = "how long (in milliseconds) HTTP requests (from matrix-corporal to the policy-serving Uri) are allowed to take before being timed out. Can be set to null to allow for unlimited waits (not recommended).";
-          };
-
-        };
-
-        options.Misc.Debug = lib.mkOption {
-          type = lib.types.bool;
-          default = true;
-          description = "Whether to enable debug mode or not (enable for more verbose logs)";
+          description = "The policy provider, see <link xlink:href="https://github.com/devture/matrix-corporal/blob/e8250a98292ae250726ce5edd75bc6ece4eb5115/docs/policy-providers.md"/> for supported values";
         };
       };
     };
   };
   config = lib.mkIf cfg.enable {
+    services.matrix-corporal.settings = {
+      Matrix.TimeoutMilliseconds = lib.mkDefault 45000;
+      Reconciliation.RetryIntervalMilliseconds = lib.mkDefault 30000;
+      HttpGateway.TimeoutMilliseconds = lib.mkDefault cfg.settings.Matrix.TimeoutMilliseconds;
+      HttpApi.TimeoutMilliseconds = lib.mkDefault 10000;
+    };
+
     systemd.services.matrix-corporal = {
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
