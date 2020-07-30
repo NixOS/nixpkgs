90386534e3d46044d727538c5699efe8c3b69804
Luke Gorrie <lukego@gmail.com>
2020-07-30 16:10:28 +0200

    Update pkgs/misc/spacenav/spnavcfg.nix
    Co-authored-by: Pavol Rusnak <pavol@rusnak.io>

--- pkgs/misc/spacenav/spnavcfg.nix
+++ pkgs/misc/spacenav/spnavcfg.nix
@@ -3,14 +3,14 @@
 stdenv.mkDerivation {
   pname = "spnavcfg";
   version = "0.3";
-  src = fetchurl {
-    url = "https://github.com/FreeSpacenav/spnavcfg/releases/download/spnavcfg-0.3/spnavcfg-0.3.tar.gz";
-    sha256 = "1msbxb6i01irrc5b5dvbxirwzf415y9cbdh8pz046hmx9s6hp5ac";
-  };
+  src = fetchFromGitHub {
+    owner = "FreeSpacenav";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "xxx";
   buildInputs = [ xorg.libX11 pkg-config gtk2 ];
   preConfigure = ''
     substituteInPlace Makefile.in --replace 4775 0775
   '';
 }
-
 
