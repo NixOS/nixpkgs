1b2b6b3c2a55ae96bd39817ab240fa3f5d58d290
Jonathan Ringer <jonringer117@gmail.com>
2020-07-30 15:34:58 -0700

    steam-chroot: add cacert

--- pkgs/games/steam/chrootenv.nix
+++ pkgs/games/steam/chrootenv.nix
@@ -19,6 +19,7 @@ let
       # Needed for operating system detection until
       # https://github.com/ValveSoftware/steam-for-linux/issues/5909 is resolved
       lsb-release
+      cacert
       # Errors in output without those
       pciutils
       python2
