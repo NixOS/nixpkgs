6b4c9d57c2c5c6f9a5003a27846dc14b8ae58a64
Luke Gorrie <lukego@gmail.com>
2020-07-30 16:10:44 +0200

    Update pkgs/misc/spacenav/libspnav.nix
    Co-authored-by: Pavol Rusnak <pavol@rusnak.io>

--- pkgs/misc/spacenav/libspnav.nix
+++ pkgs/misc/spacenav/libspnav.nix
@@ -3,11 +3,12 @@
 stdenv.mkDerivation {
   pname = "libspnav";
   version = "0.2.3";
-  src = fetchurl {
-    url = "https://github.com/FreeSpacenav/libspnav/releases/download/libspnav-0.2.3/libspnav-0.2.3.tar.gz";
-    sha256 = "14qzbzpfdb0dfscj4n0g8h8n71fcmh0ix2c7nhldlpbagyxxgr3s";
+  src = fetchFromGitHub {
+    owner = "FreeSpacenav";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "xxx";
   };
   buildInputs = [ xorg.libX11 ];
 }
-
 
