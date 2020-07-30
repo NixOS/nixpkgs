5b42b766658ecd0d70706fb8dfbe1d65f487bc9b
Luke Gorrie <lukego@gmail.com>
2020-07-30 16:10:36 +0200

    Update pkgs/misc/spacenav/spacenavd.nix
    Co-authored-by: Pavol Rusnak <pavol@rusnak.io>

--- pkgs/misc/spacenav/spacenavd.nix
+++ pkgs/misc/spacenav/spacenavd.nix
@@ -3,11 +3,11 @@
 stdenv.mkDerivation {
   pname = "spacenavd";
   version = "0.7.1";
-  src = fetchurl {
-    url = "https://github.com/FreeSpacenav/spacenavd/releases/download/v0.7.1/spacenavd-0.7.1.tar.gz";
-    sha256 = "0awxa90rqmpvwa04fy1wz0hq0cq4c579h5hgmjdwgavxhjdy2rlj";
-  };
+  src = fetchFromGitHub {
+    owner = "FreeSpacenav";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "xxx";
   buildInputs = [ xorg.libX11 ];
 }
-
 
