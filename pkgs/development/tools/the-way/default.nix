d99c48fc41a712e6a3dae8e975c2d5eb1c84313b
Mario Rodas <marsam@users.noreply.github.com>
2020-07-31 08:00:00 -0500

    the-way: fix build on darwin

--- pkgs/development/tools/the-way/default.nix
+++ pkgs/development/tools/the-way/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, rustPlatform }:
+{ stdenv, fetchFromGitHub, rustPlatform, AppKit, Security }:
 
 rustPlatform.buildRustPackage rec {
   pname = "the-way";
@@ -10,6 +10,8 @@ rustPlatform.buildRustPackage rec {
     rev = "v${version}";
     sha256 = "0h33jsai8gvfp0js06qa8cqpzfbjkd001kfj6p24d08ds2i00asx";
   };
+
+  buildInputs = stdenv.lib.optionals stdenv.isDarwin  [ AppKit Security ];
 
   cargoSha256 = "1r0mv1q1bz67zbxnd5qmji4svcbln8h5h0gysfddpn4dy9424fp3";
   #checkFlags = "--test-threads=1";
