6b5ce39d6326d021538c9ebabc2b871789e6b9c8
R. RyanTM <ryantm-bot@ryantm.com>
2020-07-31 19:43:52 +0000

    ckbcomp: 1.195 -> 1.196

--- pkgs/tools/X11/ckbcomp/default.nix
+++ pkgs/tools/X11/ckbcomp/default.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "ckbcomp";
-  version = "1.195";
+  version = "1.196";
 
   src = fetchFromGitLab {
     domain = "salsa.debian.org";
     owner = "installer-team";
     repo = "console-setup";
     rev = version;
-    sha256 = "0d0xagigs5k3appzz5lzxdjncvnkgz4amz6ks7imiq8kkjixvmfy";
+    sha256 = "0c79rycgpna8910as6blw3z3sajzzakz4qlvr6js2yr8zq2d0ylg";
   };
 
   buildInputs = [ perl ];
