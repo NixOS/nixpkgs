0f2bbb850dfc89018e4c4d51d2aea1efea268844
José Romildo Malaquias <malaquias@gmail.com>
2020-07-31 09:32:01 -0300

    enlightenment.econnman: move to pkgs/desktops/enlightenment/econnman/

--- pkgs/desktops/enlightenment/default.nix
+++ pkgs/desktops/enlightenment/default.nix
@@ -7,7 +7,7 @@
   enlightenment = callPackage ./enlightenment { };
 
   #### APPLICATIONS
-  econnman = callPackage ./econnman.nix { };
+  econnman = callPackage ./econnman { };
   terminology = callPackage ./terminology.nix { };
   rage = callPackage ./rage.nix { };
   ephoto = callPackage ./ephoto.nix { };
