872ffacd61ee0aea2613360ca7d6faa9a2776826
IvarWithoutBones <ivar.scholten@protonmail.com>
2020-07-31 13:34:41 +0200

    ryujinx: init at 1.0.5160

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2231,6 +2231,8 @@ in
 
   remarkable-mouse = python3Packages.callPackage ../applications/misc/remarkable/remarkable-mouse { };
 
+  ryujinx = callPackage ../misc/emulators/ryujinx { };
+
   scour = with python3Packages; toPythonApplication scour;
 
   s2png = callPackage ../tools/graphics/s2png { };
