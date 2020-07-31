43b6833451eeee62d6b056913a9dfbcfb0152dfa
R. RyanTM <ryantm-bot@ryantm.com>
2020-07-31 11:38:47 +0000

    the-way: 0.4.0 -> 0.6.1

--- pkgs/development/tools/the-way/default.nix
+++ pkgs/development/tools/the-way/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "the-way";
-  version = "0.4.0";
+  version = "0.6.1";
 
   src = fetchFromGitHub {
     owner = "out-of-cheese-error";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0q7yg90yxnpaafg6sg7mqkh86qkn43kxy73p9nqkkgrikdnrjh5a";
+    sha256 = "0h33jsai8gvfp0js06qa8cqpzfbjkd001kfj6p24d08ds2i00asx";
   };
 
-  cargoSha256 = "1a747bmc6s007ram0w4xf1y2nb3pphvqnlx59098lr3v7gllp7x3";
+  cargoSha256 = "1r0mv1q1bz67zbxnd5qmji4svcbln8h5h0gysfddpn4dy9424fp3";
   #checkFlags = "--test-threads=1";
   doCheck = false;
 
