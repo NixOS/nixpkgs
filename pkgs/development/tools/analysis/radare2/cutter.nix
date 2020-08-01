18188bacd69907933bbf02ecd1f935b8c84313b8
Jörg Thalheim <joerg@thalheim.io>
2020-08-01 09:56:54 +0100

    radare2-cutter: 1.10.3 -> 1.11.0
    (cherry picked from commit fca28bc4964d18919967d6a89a3d3a1d4821c4b2)

--- pkgs/development/tools/analysis/radare2/cutter.nix
+++ pkgs/development/tools/analysis/radare2/cutter.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation rec {
   pname = "radare2-cutter";
-  version = "1.10.3";
+  version = "1.11.0";
 
   src = fetchFromGitHub {
     owner = "radareorg";
     repo = "cutter";
     rev = "v${version}";
-    sha256 = "0qj8jyij02nif4jpirl09ygwnv8a9zi3vkb5sf5s8mg7qwlpnvyk";
+    sha256 = "1xvdap7hpkjz6rg0ngnql1p18p93b8w9gv130g818nwcjsh9i2y5";
   };
 
   postUnpack = "export sourceRoot=$sourceRoot/src";
