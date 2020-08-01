f34f25957957ef329e87c8effad6dd07c445daf1
Pavol Rusnak <pavol@rusnak.io>
2020-08-01 14:03:05 +0200

    gomp: init at 1.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17015,6 +17015,8 @@ in
 
   gmailctl = callPackage ../applications/networking/gmailctl {};
 
+  gomp = callPackage ../applications/version-management/gomp { };
+
   gpm = callPackage ../servers/gpm {
     ncurses = null;  # Keep curses disabled for lack of value
   };
