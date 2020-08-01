7a2b93df9fde2ffc9219e2c25b7be5ea5cce23f3
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-08-01 17:04:18 -0400

    environment.noXlibs: disable X11 support in cairo

--- nixos/modules/config/no-x-libs.nix
+++ nixos/modules/config/no-x-libs.nix
@@ -27,6 +27,7 @@ with lib;
     fonts.fontconfig.enable = false;
 
     nixpkgs.overlays = singleton (const (super: {
+      cairo = super.cairo.override { x11Support = false; };
       dbus = super.dbus.override { x11Support = false; };
       networkmanager-fortisslvpn = super.networkmanager-fortisslvpn.override { withGnome = false; };
       networkmanager-l2tp = super.networkmanager-l2tp.override { withGnome = false; };
