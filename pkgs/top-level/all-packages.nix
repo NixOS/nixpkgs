72ff02c99b7f281cbed490593244ed725c7c256d
Bruno Bigras <bigras.bruno@gmail.com>
2020-08-01 13:19:33 -0400

    kopia: init at 0.5.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18492,6 +18492,8 @@ in
 
   kochi-substitute-naga10 = callPackage ../data/fonts/kochi-substitute-naga10 {};
 
+  kopia = callPackage ../tools/backup/kopia { };
+
   lato = callPackage ../data/fonts/lato {};
 
   league-of-moveable-type = callPackage ../data/fonts/league-of-moveable-type {};
