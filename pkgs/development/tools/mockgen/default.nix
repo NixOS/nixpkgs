c542d21686f1ff8d779e32cac08c46f02f240c5a
R. RyanTM <ryantm-bot@ryantm.com>
2020-08-02 07:10:58 +0000

    mockgen: 1.4.3 -> 1.4.4

--- pkgs/development/tools/mockgen/default.nix
+++ pkgs/development/tools/mockgen/default.nix
@@ -1,14 +1,14 @@
 { buildGoModule, lib, fetchFromGitHub }:
 buildGoModule rec {
   pname = "mockgen";
-  version = "1.4.3";
+  version = "1.4.4";
   src = fetchFromGitHub {
     owner = "golang";
     repo = "mock";
     rev = "v${version}";
-    sha256 = "1p37xnja1dgq5ykx24n7wincwz2gahjh71b95p8vpw7ss2g8j8wx";
+    sha256 = "1lj0dvd6div4jaq1s0afpwqaq9ah8cxhkq93wii2ably1xmp2l0a";
   };
-  vendorSha256 = "1kpiij3pimwv3gn28rbrdvlw9q5c76lzw6zpa12q6pgck76acdw4";
+  vendorSha256 = "1md4cg1zzhc276sc7i2v0xvg5pf6gzy0n9ga2g1lx3d572igq1wy";
   subPackages = [ "mockgen" ];
 
   meta = with lib; {
