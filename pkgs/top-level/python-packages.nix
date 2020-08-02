c104506df6972813046d5707d09cca8dd4705c3e
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-08-02 03:30:27 +0200

    python3Packages.pyHS100: init at 0.3.5.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2572,6 +2572,8 @@ in {
 
   pyhcl = callPackage ../development/python-modules/pyhcl { };
 
+  pyhs100 = callPackage ../development/python-modules/pyhs100 { };
+
   pytest = if isPy3k then self.pytest_5 else self.pytest_4;
 
   pytest_5 = callPackage ../development/python-modules/pytest {
