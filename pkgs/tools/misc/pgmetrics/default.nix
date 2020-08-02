dbb4daf59cd732f904f8526a5a4657a6d23de819
R. RyanTM <ryantm-bot@ryantm.com>
2020-08-02 09:57:27 +0000

    pgmetrics: 1.9.2 -> 1.9.3

--- pkgs/tools/misc/pgmetrics/default.nix
+++ pkgs/tools/misc/pgmetrics/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "pgmetrics";
-  version = "1.9.2";
+  version = "1.9.3";
 
   src = fetchFromGitHub {
     owner  = "rapidloop";
     repo   = pname;
     rev    = "v${version}";
-    sha256 = "1prhzmxrvypqdbxjr9c3207w1c88z3kwsrr2rcbh1y7fx5rrspxv";
+    sha256 = "1g0kdvc93ij155r3g7cd9f5p1x33vdi9p40403waanq5wiavjnzq";
   };
 
   vendorSha256 = "16x33fmh4q993rw0jr65337yimska4fwgyyw3kmq84q0x28a3zg5";
