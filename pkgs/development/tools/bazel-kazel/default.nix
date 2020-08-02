5da5e21ccc8226d39b5c351e237825fb2418be19
R. RyanTM <ryantm-bot@ryantm.com>
2020-08-01 18:46:30 -0700

    bazel-kazel: 0.0.7 -> 0.0.10 (#94470)

--- pkgs/development/tools/bazel-kazel/default.nix
+++ pkgs/development/tools/bazel-kazel/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "bazel-kazel";
-  version = "0.0.7";
+  version = "0.0.10";
 
   src = fetchFromGitHub {
     owner = "kubernetes";
     repo = "repo-infra";
     rev = "v${version}";
-    sha256 = "0g4lgz5xgmxabaa8pygmdnrnslfrcb84vgr6bninl9v5zz4wajbm";
+    sha256 = "1l3dz77h58v1sr7k8cabq5bbdif5w96zdcapax69cv1frr9jbrcb";
   };
 
-  vendorSha256 = "1lizvg9r7cck1cgk20np2syv4ljbc0zj1kydiiajf7y5x7d3lwha";
+  vendorSha256 = "1pzkjh4n9ai8yqi98bkdhicjdr2l8j3fckl5n90c2gdcwqyxvgkf";
 
   subPackages = [ "cmd/kazel" ];
 
