e570f6cab43fdafc4c374d664b3b20618da28fc1
Noah Hendrickson <nhendrickson@backtrace.io>
2020-08-01 23:13:54 -0400

    Fully revamped teleport module. Just needs examples and descriptions.

--- nixos/modules/services/networking/teleport.nix
+++ nixos/modules/services/networking/teleport.nix
@@ -7,9 +7,8 @@ let
 
   # Pretty-print JSON to a file
   writePrettyJSON = name: x:
-    pkgs.runCommand name { } "
-      echo '${builtins.toJSON x}' | ${pkgs.jq}/bin/jq . > $out
-    ";
+    pkgs.runCommand name { }
+    "\n      echo '${builtins.toJSON x}' | ${pkgs.jq}/bin/jq . > $out\n    ";
   # This becomes the main config file
   telConfig = {
     teleport = {
@@ -24,158 +23,177 @@ let
 
   generatedTeleportYml = writePrettyJSON "teleport.yaml" telConfig;
 
-  teleportYml =
-    if cfg.configText != null then
-      pkgs.writeText "teleport.yml" cfg.configText
-    else generatedTeleportYml;
+  teleportYml = if cfg.configText != null then
+    pkgs.writeText "teleport.yml" cfg.configText
+  else
+    generatedTeleportYml;
 
   cmdlineArgs = "-c ${teleportYml} --pid-file=/run/teleport.pid";
 
-  telTypes.auth_service = types.submodule {
-    options = {
-      enabled = mkEnableOption {
+  telTypes.auth_service = {
+    enabled = mkOption {
+      type = types.str;
+      default = "off";
+    };
+    cluster_name = mkOption {
+      type = types.str;
+      default = "";
+    };
+    authentication = {
+      type = mkOption {
+        type = types.str;
+        default = "local";
+      };
+      second_factor = mkOption {
+        type = types.str;
+        default = "otp";
+      };
+    };
+    listen_addr = mkOption {
+      type = types.str;
+      default = "";
+    };
+    public_addr = mkOption {
+      type = types.str;
+      default = "";
+    };
+    tokens = mkOption {
+      type = types.listOf types.str;
+      default = [ "" ];
+    };
+    session_recording = mkOption {
+      type = types.str;
+      default = "node";
+    };
+    client_idle_timeout = mkOption {
+      type = types.str;
+      default = "1hr";
+    };
+    disconnect_expired_cert = mkOption {
+      type = types.str;
+      default = "yes";
+    };
+    keep_alive_interval = mkOption {
+      type = types.int;
+      default = 15;
+    };
+    keep_alive_count_max = mkOption {
+      type = types.int;
+      default = 3;
+    };
+  };
+  telTypes.ssh_service = {
+    enabled = mkOption {
+      type = types.str;
+      default = "on";
+    };
+    listen_addr = mkOption {
+      type = types.str;
+      default = "0.0.0.0:3022";
+    };
+    public_addr = mkOption {
+      type = types.str;
+      default = "";
+    };
+    permit_user_env = mkOption {
+      type = types.bool;
+      default = false;
+    };
+    pam = {
+      enabled = mkOption {
         type = types.bool;
         default = false;
-        description = "
-          Enable the Teleport Cluster SSH daemon.
-        ";
       };
-      cluster_name = mkOption {
+      service_name = mkOption {
         type = types.str;
-      };
-      listen_addr = mkOption {
-        type = types.str;
-      };
-      public_addr = mkOption {
-        type = types.str;
-      };
-      tokens = mkOption {
-        type = types.listOf types.str;
+        default = "sshd";
       };
     };
   };
-  telTypes.ssh_service = types.submodule {
-    options = {
-      enabled = mkEnableOption {
-        type = types.bool;
-        default = false;
-        description = "
-          Enable the Teleport Cluster SSH daemon.
-        ";
-      };
+  telTypes.proxy_service = {
+    enabled = mkOption {
+      type = types.str;
+      default = "off";
+    };
+    listen_addr = mkOption {
+      type = types.str;
+      default = "";
+    };
+    web_listen_addr = mkOption {
+      type = types.str;
+      default = "";
+    };
+    tunnel_listen_addr = mkOption {
+      type = types.str;
+      default = "";
+    };
+    https_key_file = mkOption {
+      type = types.str;
+      default = "";
+    };
+    https_cert_file = mkOption {
+      type = types.str;
+      default = "";
     };
   };
-  telTypes.proxy_service = types.submodule {
-    options = {
-      enabled = mkEnableOption {
-        type = types.bool;
-        default = false;
-        description = "
-          Enable the Teleport Cluster SSH daemon.
-        ";
-      };
-      listen_addr = mkOption {
-        type = types.str;
-      };
-      web_listen_addr = mkOption {
-        type = types.str;
-      };
-      tunnel_listen_addr = mkOption {
-        type = types.str;
-      };
-      https_key_file = mkOption {
-        type = types.str;
-      };
-      https_cert_file = mkOption {
-        type = types.str;
-      };
-    };
-  };
-
-in
-
-{
+  
+in {
   options = {
     services.teleport = {
-      enable = mkEnableOption {
-        type = types.bool;
-        default = false;
-        description = "
-          Enable the Teleport Cluster SSH daemon.
-        ";
-      };
+      enable = mkEnableOption "Teleport Cluster";
       dataDir = mkOption {
         type = types.path;
-        default = "/var/lib/teleport/";
-        description = "
-          Directory to store Teleport Service data.
-        ";
+        default = "/var/lib/teleport";
+        description = "Directory to store Teleport Service data";
       };
       configText = mkOption {
         type = types.nullOr types.lines;
         default = null;
-        description = "
-          If non-null, this option defines the text that is written to
-          teleport.yml.
-        ";
+        description = "If non-null, this option defines the text that is written to teleport.yml";
       };
       nodename = mkOption {
         type = types.str;
         default = "";
-        description = ''
-          The name of your teleport node.
-          This is what you `tsh ssh` to.
-        '';
       };
       auth_token = mkOption {
         type = types.str;
-        default = types.null;
-        description = ''
-          The auth token your teleport node authenticates with to your teleport auth nodes.
-        '';
+        default = "";
       };
       auth_servers = mkOption {
         type = types.listOf types.str;
-        description = ''
-          The set of Auth servers your Teleport node authenticates against.
-        '';
+        default = [ "" ];
+        description = "";
       };
-      auth_service = mkOption {
-        type = telTypes.auth_service;
-        default = {};
-        description = ''
-          Enable the Teleport SSH Auth service.
-        '';
+      advertise_ip = mkOption {
+        type = types.str;
+        default = "";
       };
-      ssh_service = mkOption {
-        type = telTypes.ssh_service;
-        default = {};
-        description = ''
-          Enable the Teleport SSH service.
-        '';
+      connection_limits = {
+        max_connection = mkOption {
+          type = types.int;
+          default = 1000;
+        };
+        max_users = mkOption {
+          type = types.int;
+          default = 250;
+        };
       };
-      proxy_service = mkOption {
-        type = telTypes.proxy_service;
-        default = {};
-        description = ''
-          Enable the Teleport SSH Proxy service.
-        '';
+      inherit (telTypes) ssh_service auth_service proxy_service;
+    };
+  };
+  
+  config = mkIf cfg.enable {
+    systemd.services.teleport = {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.teleport}/bin/teleport start ${cmdlineArgs}";
+        ExecReload = "/run/current-system/sw/bin/kill -HUP $MAINPID";
+        PIDFile = "teleport.pid";
+        Restart = "on-failure";
+        WorkingDirectory = cfg.dataDir;
       };
     };
   };
-
-    config = mkIf cfg.enable {
-      systemd.services.teleport = {
-        wantedBy = [ "multi-user.target" ];
-        after    = [ "network.target" ];
-        serviceConfig = {
-          Type = "simple";
-          ExecStart = "${pkgs.teleport}/bin/teleport start ${cmdlineArgs}";
-          ExecReload = "/run/current-system/sw/bin/kill -HUP $MAINPID";
-          PIDFile = "run/teleport.pid";
-          Restart = "on-failure";
-          WorkingDirectory = cfg.dataDir;
-        };
-      };
-   };
 }
