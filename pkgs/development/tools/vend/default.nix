34ae13430a8f45c7e07c4b0311e97ae0d9cfada2
Colin L Rice <colin@daedrum.net>
2020-08-02 17:58:51 -0400

    vend: Use nix-community fork of vend.
    Note that pkgs.govendor is currently taken by a package which is
    orphaned by upstream. It's mildly confusing to have vend point to
    nix-community/govendor, but I felt govendor was a better name for the
    nix tool since it's easy to understand what's it's for, compared to vend
    which won't really make sense to anyone who doesn't package go.

--- pkgs/development/tools/vend/default.nix
+++ pkgs/development/tools/vend/default.nix
@@ -2,22 +2,20 @@
 
 buildGoModule {
   pname = "vend";
-  version = "unstable-2020-06-04";
-
-  patches = [./remove_tidy.patch];
+  version = "unstable-2020-08-02";
 
   # A permanent fork from master is maintained to avoid non deterministic go tidy
   src = fetchFromGitHub {
-    owner = "c00w";
-    repo = "vend";
-    rev = "24fdebfdb2c3cc0516321a9cf33a3fd81c209c04";
-    sha256 = "112p9dz9by2h2m3jha2bv1bvzn2a86bpg1wphgmf9gksjpwy835l";
+    owner = "nix-community";
+    repo = "govendor";
+    rev = "f01e881a2959c32f9ac3217c32518a856aaea92e";
+    sha256 = "1qwi8wjryv93lmcis5vv5bikghj8qs5j46jh9fkv4fxazv0lxrb0";
   };
 
   vendorSha256 = null;
 
   meta = with stdenv.lib; {
-    homepage = "https://github.com/c00w/vend";
+    homepage = "https://github.com/nix-community/govendor";
     description = "A utility which vendors go code including c dependencies";
     maintainers = with maintainers; [ c00w ];
     license = licenses.mit;
