87b83648519ba11d5aa4a375756ec56996440820
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-08-03 03:54:40 +0200

    zigbee2mqtt: add passthru test

--- pkgs/servers/zigbee2mqtt/default.nix
+++ pkgs/servers/zigbee2mqtt/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, stdenv, system, dataDir ? "/opt/zigbee2mqtt/data" }:
+{ pkgs, stdenv, system, dataDir ? "/opt/zigbee2mqtt/data", nixosTests }:
 let
   package = (import ./node.nix { inherit pkgs system; }).package;
 in
@@ -22,6 +22,8 @@ package.override rec {
     rev = version;
     sha256 = "0yv51rds28az5pqzgkprhrzwmky29l1mvqb73l7dbs8qlx8x1x52";
   };
+
+  passthru.tests.zigbee2mqtt = nixosTests.zigbee2mqtt;
 
   meta = with pkgs.stdenv.lib; {
     description = "Zigbee to MQTT bridge using zigbee-shepherd";
