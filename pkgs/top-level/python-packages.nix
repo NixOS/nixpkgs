7b04c0ff13ccad71c04927d6dfc3f90d20b53a20
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-08-04 21:00:01 -0400

    pythonPackages.dask-gateway-server: init at 0.8.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2741,6 +2741,10 @@ in {
 
   dask-gateway = callPackage ../development/python-modules/dask-gateway { };
 
+  dask-gateway-server = callPackage ../development/python-modules/dask-gateway-server {
+    inherit (pkgs) go;
+  };
+
   dask-glm = callPackage ../development/python-modules/dask-glm { };
 
   dask-image = callPackage ../development/python-modules/dask-image { };
