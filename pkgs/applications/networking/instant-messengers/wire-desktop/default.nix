9e056fef50b4d9dc5cb397b03251a534373e96e6
toonn <toonn@toonn.io>
2020-08-06 00:49:33 +0200

    wire-desktop: linux 3.18.2925 -> 3.19.2928
    (cherry picked from commit cc8d2ba291448139b21ccf39bfc4c070276a2800)

--- pkgs/applications/networking/instant-messengers/wire-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/wire-desktop/default.nix
@@ -19,12 +19,12 @@ let
 
   version = {
     x86_64-darwin = "3.18.3728";
-    x86_64-linux = "3.18.2925";
+    x86_64-linux = "3.19.2928";
   }.${system} or throwSystem;
 
   sha256 = {
     x86_64-darwin = "0xi3k95yw59xsfavncc1giwxlwjz26z34qm3i604ksjjalvpzy5l";
-    x86_64-linux = "01g39hncj1w0zyfrzv4mgv8npwvx0g0lw99azyai99877b30bm8j";
+    x86_64-linux = "038h1j59qfj5ckaxk823qpfflxay47v0jrqc0hmrdmnd2y59dpx7";
   }.${system} or throwSystem;
 
   meta = with stdenv.lib; {
