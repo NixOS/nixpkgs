2f9d71906197789b75e28e5350ad4d62a4026344
Florian Klink <flokli@flokli.de>
2020-08-06 12:16:56 +0200

    nixos/systemd: remove mymachines nss module from passwd: and group: lines
    From the systemd release notes:
    
    nss-mymachines lost support for resolution of users and groups, and now
    only does resolution of hostnames. This functionality is now provided by
    nss-systemd. Thus, the 'mymachines' entry should be removed from the
    'passwd:' and 'group:' lines in /etc/nsswitch.conf (and 'systemd' added
    if it is not already there).

--- nixos/modules/system/boot/systemd.nix
+++ nixos/modules/system/boot/systemd.nix
@@ -907,11 +907,9 @@ in
       )
       ]);
       passwd = (mkMerge [
-        [ "mymachines" ]
         (mkAfter [ "systemd" ])
       ]);
       group = (mkMerge [
-        [ "mymachines" ]
         (mkAfter [ "systemd" ])
       ]);
     };
