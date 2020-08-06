0b8db6f7171e09857338acd3fec24e4996d5e86c
zowoq <59103226+zowoq@users.noreply.github.com>
2020-08-07 04:16:42 +1000

    go: 1.14.6 -> 1.14.7
    (cherry picked from commit 792f562ad546735c0b4ccea6b06813da03eb9c8e)

--- pkgs/development/compilers/go/1.14.nix
+++ pkgs/development/compilers/go/1.14.nix
@@ -31,11 +31,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "go";
-  version = "1.14.6";
+  version = "1.14.7";
 
   src = fetchurl {
     url = "https://dl.google.com/go/go${version}.src.tar.gz";
-    sha256 = "02acr52bxfjlr3m11576gdwn8hjv1pr2pffcis913m0m31w9vz3k";
+    sha256 = "1qrhdjdzi1knchk1wmlaqgkqhxkq2niw14b931rhqrk36m1r4hq6";
   };
 
   # perl is used for testing go vet
