39102883b7ab85766e42157b6e6ed75284f093bc
Daniel Kuehn <daniel@kuehn.se>
2020-08-07 03:24:03 +0200

    nixos/packetbeat: Add basic module for packetbeat
    Basic module to enable configuring and enabling packetbeat to send
    packet data to elasticsearch. Modeled after the journalbeat service.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -387,6 +387,7 @@
   ./services/logging/logrotate.nix
   ./services/logging/logstash.nix
   ./services/logging/rsyslogd.nix
+  ./services/logging/packetbeat.nix
   ./services/logging/syslog-ng.nix
   ./services/logging/syslogd.nix
   ./services/mail/clamsmtp.nix
