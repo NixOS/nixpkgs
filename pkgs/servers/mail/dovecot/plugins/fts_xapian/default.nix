feb597957b9edc5198f71771c04efc1941b34414
Symphorien Gibol <symphorien+git@xlumurb.eu>
2020-08-08 12:00:00 +0000

    dovecot_fts_xapian: 1.3.1 -> 1.3.3

--- pkgs/servers/mail/dovecot/plugins/fts_xapian/default.nix
+++ pkgs/servers/mail/dovecot/plugins/fts_xapian/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchFromGitHub, autoconf, automake, pkg-config, dovecot, libtool, xapian, icu64, sqlite }:
 stdenv.mkDerivation {
   pname = "fts-xapian";
-  version = "1.3.1";
+  version = "1.3.3";
 
   src = fetchFromGitHub {
     owner = "grosjo";
     repo = "fts-xapian";
-    rev = "1.3.1";
-    sha256 = "10yl5fyfbx2ijqckx13vbmzj9mpm5pkh8qzichbdgplrzm738q43";
+    rev = "1.3.3";
+    sha256 = "0saa7c8cc5rv7n6qqjf5bqjkngj7cr2vzqlzcah1cpwmpc89zk5k";
   };
 
   buildInputs = [ dovecot xapian icu64 sqlite ];
