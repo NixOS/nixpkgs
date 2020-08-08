127c8de5210342fd1f24c6a4a7c997af71c6f3cd
Tim Steinbach <tim@nequissimus.com>
2020-08-08 10:02:53 -0400

    linux: 4.19.137 -> 4.19.138

--- pkgs/os-specific/linux/kernel/linux-4.19.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.19.137";
+  version = "4.19.138";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0nbc930k6vn715k8dcnnv8pp1mnk76iagakvy1ky5przx7gkdy0q";
+    sha256 = "00jy3lxzx95smgc0mq7741byaj17jxrcqc2vnxlp4lkcbz82fp6i";
   };
 } // (args.argsOverride or {}))
