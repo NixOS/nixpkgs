0772bb3588178c16dcef187205969050f1266cbe
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-08-08 12:30:03 -0400

    iruby: init at 0.3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4383,6 +4383,8 @@ in
     packages = config.ihaskell.packages or (self: []);
   };
 
+  iruby = callPackage ../applications/editors/jupyter-kernels/iruby { };
+
   imapproxy = callPackage ../tools/networking/imapproxy {
     openssl = openssl_1_0_2;
   };
