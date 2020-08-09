a49a59bc6cb16e10fca6253c9a5ff2be1f8a1f76
Daniël de Kok <me@danieldk.eu>
2020-08-09 08:24:53 +0200

    amd-libflame: init at 2.2
    libflame is a protable library for dense matrix computations,
    providing a complete LAPACK implementation. The AMD fork of libflame
    is optimized for AMD CPUs.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25307,6 +25307,8 @@ in
 
   amd-blis = callPackage ../development/libraries/science/math/amd-blis { };
 
+  amd-libflame = callPackage ../development/libraries/science/math/amd-libflame { };
+
   arpack = callPackage ../development/libraries/science/math/arpack { };
 
   blas = callPackage ../build-support/alternatives/blas { };
