ef062c12973861c2339a669a96701fd1376cb8b8
Stefan Frijters <sfrijters@gmail.com>
2020-08-09 11:41:19 +0200

    urserver: init at 3.6.0.745

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16778,6 +16778,8 @@ in
     unifiStable;
   unifi = unifiStable;
 
+  urserver = callPackage ../servers/urserver { };
+
   victoriametrics = callPackage ../servers/nosql/victoriametrics { };
 
   virtlyst = libsForQt5.callPackage ../servers/web-apps/virtlyst { };
