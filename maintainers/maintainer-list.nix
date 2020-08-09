04989d7f8b6b8d0dc987bc8422d9e4f366a9c916
Gabriel Volpe <volpegabriel@gmail.com>
2020-08-09 17:48:22 +0200

    maintainers: add gvolpe

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3218,6 +3218,12 @@
     githubId = 6768842;
     name = "Joris Guyonvarch";
   };
+  gvolpe = {
+    email = "volpegabriel@gmail.com";
+    github = "gvolpe";
+    githubId = 443978;
+    name = "Gabriel Volpe";
+  };
   hakuch = {
     email = "hakuch@gmail.com";
     github = "hakuch";
