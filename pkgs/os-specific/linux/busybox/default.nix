ea783ce72f7f0e49df4a0813f1d54cc2ce67d7f8
Tethys Svensson <freaken@freaken.dk>
2020-08-09 15:05:34 +0200

    busybox: Pull in upstream patch for CVE-2018-1000500
    (cherry picked from commit 87af0f9871a22743f8466d6658481f1beb7378f4)

--- pkgs/os-specific/linux/busybox/default.nix
+++ pkgs/os-specific/linux/busybox/default.nix
@@ -34,6 +34,9 @@ let
 in
 
 stdenv.mkDerivation rec {
+  # TODO: When bumping this version, please validate whether the wget patch is present upstream
+  # and remove the patch if it is. The patch should be present upstream for all versions 1.32.0+.
+  # See NixOs/nixpkgs#94722 for context.
   name = "busybox-1.31.1";
 
   # Note to whoever is updating busybox: please verify that:
@@ -49,6 +52,7 @@ stdenv.mkDerivation rec {
 
   patches = [
     ./busybox-in-store.patch
+    ./0001-wget-implement-TLS-verification-with-ENABLE_FEATURE_.patch
   ] ++ stdenv.lib.optional (stdenv.hostPlatform != stdenv.buildPlatform) ./clang-cross.patch;
 
   postPatch = "patchShebangs .";
