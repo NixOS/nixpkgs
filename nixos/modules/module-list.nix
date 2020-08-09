05f74fb5d57ba4fa54dee319a7d6f05b65362ba3
Stefan Frijters <sfrijters@gmail.com>
2020-08-09 11:42:55 +0200

    nixos/urserver: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -926,6 +926,7 @@
   ./services/x11/gdk-pixbuf.nix
   ./services/x11/imwheel.nix
   ./services/x11/redshift.nix
+  ./services/x11/urserver.nix
   ./services/x11/urxvtd.nix
   ./services/x11/window-managers/awesome.nix
   ./services/x11/window-managers/default.nix
