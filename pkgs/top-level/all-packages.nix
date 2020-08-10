7c61ce0e34d5394cd27d2e80edcb88abf531abbf
Orivej Desh <orivej@gmx.fr>
2020-08-10 12:37:47 +0000

    libxml2Python: fix propagated inputs
    Partially reverts 6a359b2333d626ab7053605dcc5b56b9668665f7
    
    Fixes build of gnome-doc-utils and others in
    https://hydra.nixos.org/eval/1604922#tabs-now-fail

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13938,7 +13938,9 @@ in
     python = python3;
   };
 
-  libxml2Python = pkgs.buildEnv { # slightly hacky
+  libxml2Python = let
+    libxml2 = python2Packages.libxml2;
+  in pkgs.buildEnv { # slightly hacky
     name = "libxml2+py-${res.libxml2.version}";
     paths = with libxml2; [ dev bin py ];
     inherit (libxml2) passthru;
