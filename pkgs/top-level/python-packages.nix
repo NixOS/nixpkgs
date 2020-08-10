886a9bced5a4017b1f22ce8b02930f6abe5126ea
Drew Risinger <drewrisinger@users.noreply.github.com>
2020-08-10 18:40:57 -0400

    python3Packages.yfinance: init at 0.1.54

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7734,6 +7734,8 @@ in {
 
   yattag = callPackage ../development/python-modules/yattag { };
 
+  yfinance = callPackage ../development/python-modules/yfinance { };
+
   xenomapper = disabledIf (!isPy3k) (callPackage ../applications/science/biology/xenomapper { });
 
   z3 = (toPythonModule (pkgs.z3.override {
