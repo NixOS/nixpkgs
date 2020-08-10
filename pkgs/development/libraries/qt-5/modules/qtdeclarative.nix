5530043208f0bef7aa4963f14cf599b74e9fd7e8
Thomas Tuegel <ttuegel@mailbox.org>
2020-08-10 10:18:51 -0500

    qtdeclarative: Patch for scrollbar regression

--- pkgs/development/libraries/qt-5/modules/qtdeclarative.nix
+++ pkgs/development/libraries/qt-5/modules/qtdeclarative.nix
@@ -1,4 +1,4 @@
-{ qtModule, lib, python3, qtbase, qtsvg }:
+{ qtModule, lib, fetchpatch, python3, qtbase, qtsvg }:
 
 with lib;
 
@@ -23,4 +23,12 @@ qtModule {
     "bin/qmlscene"
     "bin/qmltestrunner"
   ];
+  patches =
+    # https://mail.kde.org/pipermail/kde-distro-packagers/2020-June/000419.html
+    lib.optional (lib.versionAtLeast qtbase.version "5.14.2")
+      (fetchpatch {
+        url = "https://codereview.qt-project.org/gitweb?p=qt/qtdeclarative.git;a=patch;h=3e47ac319b0f53c43cc02a8356c2dec4f0daeef4";
+        sha256 = "0wvncg7047q73nm0svc6kb14sigwk7sc53r4778kn033aj0qqszj";
+        name = "qtdeclarative-QQuickItemView-fix-max-extent.patch";
+      });
 }
