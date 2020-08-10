9946596cc18122ddf5dfb338eac29aa30354b71d
R. RyanTM <ryantm-bot@ryantm.com>
2020-08-10 12:44:36 +0000

    hcxdumptool: 6.1.0 -> 6.1.1

--- pkgs/tools/security/hcxdumptool/default.nix
+++ pkgs/tools/security/hcxdumptool/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "hcxdumptool";
-  version = "6.1.0";
+  version = "6.1.1";
 
   src = fetchFromGitHub {
     owner = "ZerBea";
     repo = "hcxdumptool";
     rev = version;
-    sha256 = "10dhv8lgsciw6w9r328k50pv1ldldf9wikplgh0dq98djlf0l961";
+    sha256 = "0v6dq6x2mrmavazknmhb08ks53773sll367anfrrramild8350bh";
   };
 
   buildInputs = [ openssl ];
