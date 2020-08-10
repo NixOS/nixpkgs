ac103f8c52d1099c0d5e01e81e3c11c358963f67
Yannick Markus <ym@ymarkus.dev>
2020-08-10 09:50:54 +0200

    zfs-prune-snapshots: init at 1.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27302,6 +27302,8 @@ in
 
   kube3d =  callPackage ../applications/networking/cluster/kube3d {};
 
+  zfs-prune-snapshots = callPackage ../tools/backup/zfs-prune-snapshots {};
+
   zfs-replicate = python3Packages.callPackage ../tools/backup/zfs-replicate { };
 
   runwayml = callPackage ../applications/graphics/runwayml {};
