4e320f0a2d55708bd49b03d6e7f91caf3515e61c
R. RyanTM <ryantm-bot@ryantm.com>
2020-08-10 01:20:31 +0000

    asls: 0.4.0 -> 0.4.2

--- pkgs/development/tools/misc/asls/default.nix
+++ pkgs/development/tools/misc/asls/default.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   pname = "asls";
-  version = "0.4.0";
+  version = "0.4.2";
 
   src = fetchurl {
     url = "https://github.com/saulecabrera/asls/releases/download/v${version}/bin.tar.gz";
-    sha256 = "0zy89fvdhk2bj41fzx349gi8237ww96s21hlg6blqmfhvfxsnszg";
+    sha256 = "14dcms0xl6dncwf16vixvf7rq7g15iwq8h4vja0dsiisyfm08aks";
   };
 
   buildInputs = [ erlangR22 ];
