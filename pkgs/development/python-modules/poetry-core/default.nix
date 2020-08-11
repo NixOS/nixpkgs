7e97a3812f75df21ba6f4d533ed9a952147348bd
Jonathan Ringer <jonringer117@gmail.com>
2020-08-11 12:00:24 -0700

    python3Packages.poetry-core: build from source

--- pkgs/development/python-modules/poetry-core/default.nix
+++ pkgs/development/python-modules/poetry-core/default.nix
@@ -1,22 +1,63 @@
-{ lib
-, buildPythonPackage
-, fetchPypi
+{ lib, buildPythonPackage, fetchFromGitHub, pythonOlder, isPy27
+, importlib-metadata
+, intreehooks
+, isort
+, pathlib2
+, pep517
+, pytest-mock
+, pytestCheckHook
+, tomlkit
+, typing
+, virtualenv
 }:
 
 buildPythonPackage rec {
   pname = "poetry-core";
   version = "1.0.0a9";
+  format = "pyproject";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "f08e9829fd06609ca5615faa91739b589eb71e025a6aaf7ddffb698676eb7c8c";
+  src = fetchFromGitHub {
+    owner = "python-poetry";
+    repo = pname;
+    rev = version;
+    sha256 = "1ln47x1bc1yvhdfwfnkqx4d2j7988a59v8vmcriw14whfgzfki75";
   };
 
-  doCheck = false; # No tests in sdist.
+  # avoid mass-rebuild of python packages
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace "^1.7.0" "^1.6.0"
+  '';
 
-  meta = {
+  nativeBuildInputs = [
+    intreehooks
+  ];
+
+  propagatedBuildInputs = lib.optionals (pythonOlder "3.8") [
+    importlib-metadata
+  ] ++ lib.optionals isPy27 [
+    pathlib2
+    typing
+  ];
+
+  checkInputs = [
+    isort
+    pep517
+    pytest-mock
+    pytestCheckHook
+    tomlkit
+    virtualenv
+  ];
+
+  # requires git history to work correctly
+  disabledTests = [ "default_with_excluded_data" ];
+
+  pythonImportsCheck = [ "poetry.core" ];
+
+  meta = with lib; {
     description = "Core utilities for Poetry";
-    homepage = "https://github.com/python-poetry/core";
-    license = lib.licenses.mit;
+    homepage = "https://github.com/python-poetry/poetry-core/";
+    license = licenses.mit;
+    maintainers = with maintainers; [ jonringer ];
   };
 }
