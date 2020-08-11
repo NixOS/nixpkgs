423561ddbd518cac69d225af5a90aafae8f06216
R. RyanTM <ryantm-bot@ryantm.com>
2020-08-11 08:16:58 +0000

    navidrome: 0.27.0 -> 0.29.0

--- pkgs/servers/misc/navidrome/default.nix
+++ pkgs/servers/misc/navidrome/default.nix
@@ -4,11 +4,11 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   pname = "navidrome";
-  version = "0.27.0";
+  version = "0.29.0";
 
   src = fetchurl {
     url = "https://github.com/deluan/navidrome/releases/download/v${version}/navidrome_${version}_Linux_x86_64.tar.gz";
-    sha256 = "0givv23dx6hwzg0axwifrha17qafs19ag34vjz29xrj3smsl8zh3";
+    sha256 = "0dpv68wvrslgfgh18mb8ficji6k1i9jiid9bfw786andf4rwghyc";
   };
 
   nativeBuildInputs = [ makeWrapper ];
