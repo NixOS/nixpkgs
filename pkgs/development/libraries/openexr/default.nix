dec3cdd480909b2a3dab63f242601a0ee8dd62ac
Jan Tojnar <jtojnar@gmail.com>
2020-08-13 01:38:37 +0200

    openexr: 2.5.2 → 2.5.3
    https://github.com/AcademySoftwareFoundation/openexr/releases/tag/v2.5.3
    
    * Includes the musl patch.
    * Also clean up a bit while at it:
        * libtool not necessary since CMake port
        * enableParallelBuilding is on by default with cmake

--- pkgs/development/libraries/openexr/default.nix
+++ pkgs/development/libraries/openexr/default.nix
@@ -6,12 +6,11 @@
 , ilmbase
 , fetchpatch
 , cmake
-, libtool
 }:
 
 stdenv.mkDerivation rec {
   pname = "openexr";
-  version = "2.5.2";
+  version = "2.5.3";
 
   outputs = [ "bin" "dev" "out" "doc" ];
 
@@ -19,7 +18,7 @@ stdenv.mkDerivation rec {
     owner = "AcademySoftwareFoundation";
     repo = "openexr";
     rev = "v${version}";
-    sha256 = "dtVoXA3JdmNs1iqu7cZlAdxt/CAgL5lSbOwu0SheyO0=";
+    sha256 = "xyYdRrwAYdnRZmErIK0tZspguqtrXvixO5+6nMDoOh8=";
   };
 
   patches = [
@@ -28,23 +27,10 @@ stdenv.mkDerivation rec {
       url = "https://github.com/AcademySoftwareFoundation/openexr/commit/6442fb71a86c09fb0a8118b6dbd93bcec4883a3c.patch";
       sha256 = "bwD5WTKPT4DjOJDnPXIvT5hJJkH0b71Vo7qupWO9nPA=";
     })
-  ] ++ lib.optionals (stdenv.hostPlatform.isLinux && stdenv.hostPlatform.libc != "glibc") [
-    # Fix ilmbase/openexr using glibc-only fpstate.
-    # Found via https://git.alpinelinux.org/aports/tree/community/openexr/10-musl-_fpstate.patch?id=80d9611b7b8e406a554c6f511137e03ff26acbae,
-    # TODO Remove when https://github.com/AcademySoftwareFoundation/openexr/pull/798 is merged and available.
-    (fetchpatch {
-      name = "ilmbase-musl-_fpstate.patch.patch";
-      url = "https://raw.githubusercontent.com/void-linux/void-packages/80bbc168faa25448bd3399f4df331b836e74b85c/srcpkgs/ilmbase/patches/musl-_fpstate.patch";
-      sha256 = "1bmyg4qfbz2p5iflrakbj8jzs85s1cf4cpfyclycnnqqi45j8m8d";
-      # The patch's files are written as `IlmBase/...`, this turns it into
-      # `a/IlmBase/...`, so that the `patch -p1` that `patches` does works.
-      extraPrefix = ""; # Changing this requires changing the `sha256` (fixed-output)!
-    })
   ];
-  nativeBuildInputs = [ cmake libtool ];
-  propagatedBuildInputs = [ ilmbase zlib ];
 
-  enableParallelBuilding = true;
+  nativeBuildInputs = [ cmake ];
+  propagatedBuildInputs = [ ilmbase zlib ];
 
   meta = with stdenv.lib; {
     description = "A high dynamic-range (HDR) image file format";
