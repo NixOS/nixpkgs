dc5db88f23da5278dde5031bf274fb8eaadb96a9
syberant <sybrand@neuralcoding.com>
2020-08-14 12:53:15 +0200

    maintainers: add syberant

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7928,6 +7928,12 @@
     githubId = 332289;
     name = "Rafał Łasocha";
   };
+  syberant = {
+    email = "sybrand@neuralcoding.com";
+    github = "syberant";
+    githubId = 20063502;
+    name = "Sybrand Aarnoutse";
+  };
   symphorien = {
     email = "symphorien_nixpkgs@xlumurb.eu";
     github = "symphorien";
