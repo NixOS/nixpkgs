dbe0fcec16c3e8c41250093f078608b7dde9280d
Utku Demir <me@utdemir.com>
2020-08-14 21:06:00 +1200

    dockerTools.streamLayeredImage: Store the customisation layer as a tarball
    This fixes as issue described here[1], where permissions set by 'extraCommands'
    were ignored by Nix.
    
    [1] https://github.com/NixOS/nixpkgs/pull/91084#issuecomment-669834938

--- nixos/tests/docker-tools.nix
+++ nixos/tests/docker-tools.nix
@@ -219,17 +219,10 @@ import ./make-test-python.nix ({ pkgs, ... }: {
         )
 
     with subtest("Ensure correct behavior when no store is needed"):
-        # This check tests two requirements simultaneously
-        #  1. buildLayeredImage can build images that don't need a store.
-        #  2. Layers of symlinks are eliminated by the customization layer.
-        #
+        # This check tests that buildLayeredImage can build images that don't need a store.
         docker.succeed(
             "docker load --input='${pkgs.dockerTools.examples.no-store-paths}'"
         )
-
-        # Busybox will not recognize argv[0] and print an error message with argv[0],
-        # but it confirms that the custom-true symlink is present.
-        docker.succeed("docker run --rm no-store-paths custom-true |& grep custom-true")
 
         # This check may be loosened to allow an *empty* store rather than *no* store.
         docker.succeed("docker run --rm no-store-paths ls /")
