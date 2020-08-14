7fd1779318832f1293ee8f94a3dbf03cd8497464
R. RyanTM <ryantm-bot@ryantm.com>
2020-08-14 03:23:18 +0000

    python27Packages.hg-evolve: 10.0.0 -> 10.0.1

--- pkgs/development/python-modules/hg-evolve/default.nix
+++ pkgs/development/python-modules/hg-evolve/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "hg-evolve";
-  version = "10.0.0";
+  version = "10.0.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "03kn1c62y6rb851wjhsaxkrwq223hkc4ij59i85999byyb2hyqad";
+    sha256 = "9468b3e2721744b077413c3d4a6b321b61370d4c87b90afa40dc2b48ad877d4b";
   };
 
   doCheck = false;
