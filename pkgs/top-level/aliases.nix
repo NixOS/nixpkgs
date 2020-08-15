6afd02dab54b8f584a9b71d22e4b18c9324eed55
Florian Klink <flokli@flokli.de>
2020-08-15 16:29:21 +0200

    mesos: remove package
    It has been broken for >9 months.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -297,6 +297,7 @@ mapAliases ({
   # floating point textures patents are expired,
   # so package reduced to alias
   mesa_drivers = mesa.drivers;
+  mesos = throw "mesos has been removed from nixpkgs, as it's unmaintained"; # added 2020-08-15
   midoriWrapper = midori; # added 2015-01
   mist = throw "mist has been removed as the upstream project has been abandoned, see https://github.com/ethereum/mist#mist-browser-deprecated"; # added 2020-08-15
   mlt-qt5 = libsForQt5.mlt;  # added 2015-12-19
