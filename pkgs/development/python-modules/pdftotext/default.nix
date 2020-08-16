62b4f1743a80ed953b68a141210fc6d28e72e89f
Frederik Rietdijk <fridh@fridh.nl>
2020-08-16 19:31:09 +0200

    python: pdftotext: 2.1.4 -> 2.1.5

--- pkgs/development/python-modules/pdftotext/default.nix
+++ pkgs/development/python-modules/pdftotext/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "pdftotext";
-  version = "2.1.4";
+  version = "2.1.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "00xf3jmpb4m3q758wvk4khpcmiys4gmd88vvrz6i7yw1jl268y6k";
+    sha256 = "98aeb8b07a4127e1a30223bd933ef080bbd29aa88f801717ca6c5618380b8aa6";
   };
 
   buildInputs = [ poppler ];
