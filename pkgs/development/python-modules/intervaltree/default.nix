29badb0218247beaa100b12fa85e533b4fff7971
Frederik Rietdijk <fridh@fridh.nl>
2020-08-16 19:31:04 +0200

    python: intervaltree: 3.0.2 -> 3.1.0

--- pkgs/development/python-modules/intervaltree/default.nix
+++ pkgs/development/python-modules/intervaltree/default.nix
@@ -2,12 +2,12 @@
 , python, pytest, sortedcontainers }:
 
 buildPythonPackage rec {
-  version = "3.0.2";
+  version = "3.1.0";
   pname = "intervaltree";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0wz234g6irlm4hivs2qzmnywk0ss06ckagwh15nflkyb3p462kyb";
+    sha256 = "902b1b88936918f9b2a19e0e5eb7ccb430ae45cde4f39ea4b36932920d33952d";
   };
 
   buildInputs = [ pytest ];
