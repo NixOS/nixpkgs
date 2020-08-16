5d2ea4f19c23a2b0428bdbb94e86b11e9c51a92b
Orivej Desh <orivej@gmx.fr>
2020-08-16 09:30:11 +0000

    mamba: 1.1 -> 1.3

--- pkgs/applications/audio/mamba/default.nix
+++ pkgs/applications/audio/mamba/default.nix
@@ -1,45 +1,40 @@
 { stdenv
 , fetchFromGitHub
-, xorg
-, cairo
-, libjack2
-, pcre
-, libXdmcp
-, libsigcxx
-, liblo
 , pkgconfig
+, cairo
+, libX11
+, libjack2
+, liblo
+, libsigcxx
+, libsmf
 }:
 
 stdenv.mkDerivation rec {
   pname = "mamba";
-  version = "1.1";
+  version = "1.3";
 
   src = fetchFromGitHub {
     owner = "brummer10";
     repo = "Mamba";
     rev = "v${version}";
-    sha256 = "1rjy7l9sx8xvk64s29g0jrrirm5h2qg0pdvvz4i1kkbs7nasnjm5";
+    sha256 = "1wa3f9c4l239mpxa7nxx8hajy4icn40vpvaxq5l1qzskl74w072d";
     fetchSubmodules = true;
   };
 
+  patches = [ ./fix-build.patch ];
+
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [
-    xorg.libX11
-    xorg.libpthreadstubs
-    cairo
-    libjack2
-    pcre
-    libXdmcp
-    libsigcxx
-    liblo
-  ];
+  buildInputs = [ cairo libX11 libjack2 liblo libsigcxx libsmf ];
 
   makeFlags = [ "PREFIX=$(out)" ];
+
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/brummer10/Mamba";
     description = "Virtual MIDI keyboard for Jack Audio Connection Kit";
-    maintainers = [ maintainers.magnetophon ];
     license = licenses.bsd0;
+    maintainers = with maintainers; [ magnetophon orivej ];
+    platforms = platforms.linux;
   };
 }
