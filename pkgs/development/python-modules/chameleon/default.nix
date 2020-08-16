c217b81726332039f5cd3930f9f8eb21f504dc9d
Frederik Rietdijk <fridh@fridh.nl>
2020-08-16 19:30:55 +0200

    python: Chameleon: 3.7.4 -> 3.8.1

--- pkgs/development/python-modules/chameleon/default.nix
+++ pkgs/development/python-modules/chameleon/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "Chameleon";
-  version = "3.7.4";
+  version = "3.8.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "37c01c9af720bc80a0097ebc07eb41e802917ad0006c9a77dc158479c087664b";
+    sha256 = "adf9609a2fa4ad20deb390605495f9a5d617b737bfbd86e51a49bbac2acaf316";
   };
 
   meta = with stdenv.lib; {
