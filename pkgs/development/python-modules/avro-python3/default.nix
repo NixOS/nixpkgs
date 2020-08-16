d43a594e2b829e026682312d9e375557ac4e14c6
Frederik Rietdijk <fridh@fridh.nl>
2020-08-16 19:30:50 +0200

    python: avro-python3: 1.9.2.1 -> 1.10.0

--- pkgs/development/python-modules/avro-python3/default.nix
+++ pkgs/development/python-modules/avro-python3/default.nix
@@ -2,12 +2,12 @@
 
 buildPythonPackage rec {
   pname = "avro-python3";
-  version = "1.9.2.1";
+  version = "1.10.0";
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ca1e77a3da5ac98e8833588f71fb2e170b38e34787ee0e04920de0e9470b7d32";
+    sha256 = "a455c215540b1fceb1823e2a918e94959b54cb363307c97869aa46b5b55bde05";
   };
 
   doCheck = false;        # No such file or directory: './run_tests.py
