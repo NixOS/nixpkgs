0d138c6e4bc2b2148cd3d1cada8e60f63dc32a84
Frederik Rietdijk <fridh@fridh.nl>
2020-08-16 19:31:08 +0200

    python: numba: 0.50.1 -> 0.51.0

--- pkgs/development/python-modules/numba/default.nix
+++ pkgs/development/python-modules/numba/default.nix
@@ -13,14 +13,14 @@
 }:
 
 buildPythonPackage rec {
-  version = "0.50.1";
+  version = "0.51.0";
   pname = "numba";
   # uses f-strings
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "89e81b51b880f9b18c82b7095beaccc6856fcf84ba29c4f0ced42e4e5748a3a7";
+    sha256 = "da57ef00bc814bf54446fb3f8c0374557a7476e40279ceabefd9f12b05cc3c0c";
   };
 
   NIX_CFLAGS_COMPILE = stdenv.lib.optionalString stdenv.isDarwin "-I${libcxx}/include/c++/v1";
