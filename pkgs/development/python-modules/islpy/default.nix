8abd10f4da13159bd0f024e1bdb868f79f5b5e0b
Frederik Rietdijk <fridh@fridh.nl>
2020-08-16 19:31:04 +0200

    python: islpy: 2019.1.2 -> 2020.2

--- pkgs/development/python-modules/islpy/default.nix
+++ pkgs/development/python-modules/islpy/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "islpy";
-  version = "2019.1.2";
+  version = "2020.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "834b6b946f33d578d5c6b2f863dd93f7ecc4c0a2bf73407c96ef9f95b6b71bbf";
+    sha256 = "ee797e1284bffe897568f9cc1f063f1a6fac8d7b87596308b7467e9b870a90ef";
   };
 
   postConfigure = ''
