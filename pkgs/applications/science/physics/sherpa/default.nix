81cf1908827b3921931bee827825ec1175daa406
Maximilian Bosch <maximilian@mbosch.me>
2020-08-16 15:12:48 +0200

    sherpa: fix build w/glibc-2.32

--- pkgs/applications/science/physics/sherpa/default.nix
+++ pkgs/applications/science/physics/sherpa/default.nix
@@ -9,6 +9,10 @@ stdenv.mkDerivation rec {
     sha256 = "1iwa17s8ipj6a2b8zss5csb1k5y9s5js38syvq932rxcinbyjsl4";
   };
 
+  postPatch = ''
+    sed -ie '/sys\/sysctl.h/d' ATOOLS/Org/Run_Parameter.C
+  '';
+
   buildInputs = [ gfortran sqlite lhapdf rivet ];
 
   enableParallelBuilding = true;
