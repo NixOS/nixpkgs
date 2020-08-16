8fb288515fed46e510cb9b07f3c684223f6fbc10
syberant <sybrand@neuralcoding.com>
2020-08-16 08:26:59 +0200

    gitoxide: init at 0.3.0
    Fixes: https://github.com/NixOS/nixpkgs/issues/95317

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20387,6 +20387,8 @@ in
 
   gitolite = callPackage ../applications/version-management/gitolite { };
 
+  gitoxide = callPackage ../applications/version-management/gitoxide { };
+
   inherit (gnome3) gitg;
 
   gmrun = callPackage ../applications/misc/gmrun {};
