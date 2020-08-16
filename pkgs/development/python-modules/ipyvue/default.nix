fb65cd11424b321a3cae9b7d1a4f2aa15d525b24
Frederik Rietdijk <fridh@fridh.nl>
2020-08-16 19:31:04 +0200

    python: ipyvue: 1.3.4 -> 1.4.0

--- pkgs/development/python-modules/ipyvue/default.nix
+++ pkgs/development/python-modules/ipyvue/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage rec {
   pname = "ipyvue";
-  version = "1.3.4";
+  version = "1.4.0";
 
   disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "009815638f33e67f0fd9cc4fa2087b2faa438e32703877ca14ecf9826508b724";
+    sha256 = "3253727e8e12e3c157550e3e8d6986edf61ad0d68299992fbf3fa6acec41a12e";
   };
 
   propagatedBuildInputs = [ ipywidgets ];
