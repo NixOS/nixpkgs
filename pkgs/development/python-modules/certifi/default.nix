614019e0954a8d180b350474a18f7f1b5ee642c9
Frederik Rietdijk <fridh@fridh.nl>
2020-08-16 19:30:54 +0200

    python: certifi: 2020.4.5.2 -> 2020.6.20

--- pkgs/development/python-modules/certifi/default.nix
+++ pkgs/development/python-modules/certifi/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "certifi";
-  version = "2020.4.5.2";
+  version = "2020.6.20";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "5ad7e9a056d25ffa5082862e36f119f7f7cec6457fa07ee2f8c339814b80c9b1";
+    sha256 = "5930595817496dd21bb8dc35dad090f1c2cd0adfaf21204bf6732ca5d8ee34d3";
   };
 
   pythonImportsCheck = [ "certifi" ];
