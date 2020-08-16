f2e65cad1eacf2ca49d351bc600efca2701f7112
Frederik Rietdijk <fridh@fridh.nl>
2020-08-16 19:31:17 +0200

    python: titlecase: 0.12.0 -> 1.1.1

--- pkgs/development/python-modules/titlecase/default.nix
+++ pkgs/development/python-modules/titlecase/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "titlecase";
-  version = "0.12.0";
+  version = "1.1.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0486i99wf8ssa7sgn81fn6fv6i4rhhq6n751bc740b3hzfbpmpl4";
+    sha256 = "16e279edf085293bc9c44a68ce959c7d6cd5c653e6b5669a3a3640015cb63eb6";
   };
 
   checkInputs = [ nose ];
