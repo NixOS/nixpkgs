7a1569c5bf59faaaf82b7a6c22b6be6b6f0071d4
R. RyanTM <ryantm-bot@ryantm.com>
2020-08-16 08:32:31 +0000

    python27Packages.bitarray: 1.5.0 -> 1.5.1

--- pkgs/development/python-modules/bitarray/default.nix
+++ pkgs/development/python-modules/bitarray/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "bitarray";
-  version = "1.5.0";
+  version = "1.5.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e1823b33d2caa7fc54ab5507eff316e74abfdc30434db8f3be908ab52a330021";
+    sha256 = "45bba08bc142781ec7e18a847735219390808f9b6279c356252edddaee1f5fcd";
   };
 
   meta = with lib; {
