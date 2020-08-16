fbdc7496e413b4f32320bb70eae59759d48f6731
Frederik Rietdijk <fridh@fridh.nl>
2020-08-16 19:31:20 +0200

    python: web.py: 0.51 -> 0.61

--- pkgs/development/python-modules/web/default.nix
+++ pkgs/development/python-modules/web/default.nix
@@ -5,13 +5,13 @@
 }:
 
 buildPythonPackage rec {
-  version = "0.51";
+  version = "0.61";
   pname = "web.py";
   disabled = isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b50343941360984d37270186453bb897d13630028a739394fedf38f9cde2fd07";
+    sha256 = "c7a9081aeb086cd3e703c7553a47ee75188d1d325f25eec7654d9bb00b5eccbb";
   };
 
   meta = with stdenv.lib; {
