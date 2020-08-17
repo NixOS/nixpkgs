9dac262767885b16294df9f35496b343b3ef9b9e
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-08-17 12:54:04 +0200

    homeassistant: 0.114.0 -> 0.114.2

--- pkgs/servers/home-assistant/default.nix
+++ pkgs/servers/home-assistant/default.nix
@@ -72,7 +72,7 @@ let
   extraBuildInputs = extraPackages py.pkgs;
 
   # Don't forget to run parse-requirements.py after updating
-  hassVersion = "0.114.0";
+  hassVersion = "0.114.2";
 
 in with py.pkgs; buildPythonApplication rec {
   pname = "homeassistant";
@@ -91,7 +91,7 @@ in with py.pkgs; buildPythonApplication rec {
     owner = "home-assistant";
     repo = "core";
     rev = version;
-    sha256 = "0g7jwhdvdcam7gvrj72aknrsvdwm5i5hs93nngqm26m1g4sng0ma";
+    sha256 = "0llyf3icdgb9mh7x02309m35hxhinzsbd6i31mmb9fjfzp0d27q9";
   };
 
   propagatedBuildInputs = [
