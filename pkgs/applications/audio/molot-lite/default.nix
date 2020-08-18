69f2cfd3cf9f73125e91e40d55e0e7371aa9cd85
Orivej Desh <orivej@gmx.fr>
2020-08-18 20:30:59 +0000

    molot-lite: simplify definition

--- pkgs/applications/audio/molot-lite/default.nix
+++ pkgs/applications/audio/molot-lite/default.nix
@@ -1,14 +1,37 @@
-{ stdenvNoCC, molot-mono-lite, molot-stereo-lite }:
-with stdenvNoCC.lib;
+{ stdenv, fetchurl, unzip, lv2 }:
 
-
-stdenvNoCC.mkDerivation {
+stdenv.mkDerivation {
   pname = "molot-lite";
-  version = molot-mono-lite.version;
+  version = "unstable-2014-04-23";
 
-  buildCommand = ''
-    mkdir -p $out/lib/lv2/
-    ln -s ${makeLibraryPath [molot-mono-lite]}/lv2/Molot_Mono_Lite.lv2 $out/lib/lv2
-    ln -s ${makeLibraryPath [molot-stereo-lite]}/lv2/Molot_Stereo_Lite.lv2 $out/lib/lv2
+  src = fetchurl {
+    # fetchzip does not accept urls that do not end with .zip.
+    url = "https://sourceforge.net/p/molot/code/ci/c4eddc426f8d5821e8ebcf1d67265365e4c8c52a/tree/molot_src.zip?format=raw";
+    sha256 = "1c47dwfgrmn9459px8s5zikcqyr0777v226qzcxlr6azlcjwr51b";
+  };
+
+  nativeBuildInputs = [ unzip ];
+  buildInputs = [ lv2 ];
+
+  unpackPhase = ''
+    unzip $src
   '';
+
+  buildPhase = ''
+    make -C Molot_Mono_Lite
+    make -C Molot_Stereo_Lite
+  '';
+
+  installPhase = ''
+    make install INSTALL_DIR=$out/lib/lv2 -C Molot_Mono_Lite
+    make install INSTALL_DIR=$out/lib/lv2 -C Molot_Stereo_Lite
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Stereo and mono audio signal dynamic range compressor in LV2 format";
+    homepage = "https://sourceforge.net/projects/molot/";
+    license = licenses.gpl3Plus;
+    maintainers = [ maintainers.magnetophon ];
+    platforms = platforms.linux;
+  };
 }
