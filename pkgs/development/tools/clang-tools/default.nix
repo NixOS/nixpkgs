d3b46332818c18b91374356c78feffd4a2bb08fa
Jörg Thalheim <joerg@thalheim.io>
2020-08-18 06:52:43 +0100

    clang-tools: get libc/c++ stdlib headers from $NIX_CC
    This way it should respect the current environment instead
    of hard-coding what was used at build-time.
    Also only include c++ specific headers in CPLUS_INCLUDE_PATH
    as CPATH is respected for c++ as well according to clang(1).

--- pkgs/development/tools/clang-tools/default.nix
+++ pkgs/development/tools/clang-tools/default.nix
@@ -13,9 +13,6 @@ in stdenv.mkDerivation {
     runHook preInstall
 
     mkdir -p $out/bin
-    export libc_includes="${stdenv.lib.getDev stdenv.cc.libc}/include"
-    export libcpp_includes="${llvmPackages.libcxx}/include/c++/v1"
-
     export clang=${clang}
     substituteAll ${./wrapper} $out/bin/clangd
     chmod +x $out/bin/clangd
