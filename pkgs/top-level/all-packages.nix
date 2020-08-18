a62d208d73c0c34174e150ff477e5da7ae6796bb
elkowar <5300871+elkowar@users.noreply.github.com>
2020-08-18 22:46:57 +0200

    fet-sh: init at 1.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -274,6 +274,8 @@ in
 
   etBook = callPackage ../data/fonts/et-book { };
 
+  fet-sh = callPackage ../tools/misc/fet-sh { };
+
   fetchbower = callPackage ../build-support/fetchbower {
     inherit (nodePackages) bower2nix;
   };
