4b9a2e13a82ea293e0d1530e8d32ca07074996dc
Jörg Thalheim <joerg@thalheim.io>
2020-08-18 09:33:18 +0100

    cloud-utils: split of smaller .guest output

--- pkgs/tools/misc/cloud-utils/default.nix
+++ pkgs/tools/misc/cloud-utils/default.nix
@@ -1,9 +1,18 @@
 { stdenv, fetchurl, makeWrapper
 , gawk, gnused, utillinux, file
 , wget, python3, qemu-utils, euca2ools
-, e2fsprogs, cdrkit }:
+, e2fsprogs, cdrkit
+, gptfdisk }:
 
-stdenv.mkDerivation rec {
+let
+  # according to https://packages.debian.org/sid/cloud-image-utils + https://packages.debian.org/sid/admin/cloud-guest-utils
+  guestDeps = [
+    e2fsprogs gptfdisk gawk gnused utillinux
+  ];
+  binDeps = guestDeps ++ [
+    wget file qemu-utils cdrkit
+  ];
+in stdenv.mkDerivation rec {
   # NOTICE: if you bump this, make sure to run
   # $ nix-build nixos/release-combined.nix -A nixos.tests.ec2-nixops
   # growpart is needed in initrd in nixos/system/boot/grow-partition.nix
@@ -17,14 +26,23 @@ stdenv.mkDerivation rec {
   buildInputs = [ python3 ];
   installFlags = [ "LIBDIR=$(out)/lib" "BINDIR=$(out)/bin" "MANDIR=$(out)/man/man1" "DOCDIR=$(out)/doc" ];
 
-  # according to https://packages.ubuntu.com/source/zesty/cloud-utils
-  binDeps = [
-    wget e2fsprogs file gnused gawk utillinux qemu-utils euca2ools cdrkit
-  ];
+  # $guest output contains all executables needed for cloud-init and $out the rest + $guest
+  # This is similar to debian's package split into cloud-image-utils and cloud-guest-utils
+  # The reason is to reduce the closure size
+  outputs = [ "out" "guest"];
 
   postFixup = ''
+    moveToOutput bin/ec2metadata $guest
+    moveToOutput bin/growpart $guest
+    moveToOutput bin/vcs-run $guest
+
     for i in $out/bin/*; do
       wrapProgram $i --prefix PATH : "${stdenv.lib.makeBinPath binDeps}:$out/bin"
+    done
+
+    for i in $guest/bin/*; do
+      wrapProgram $i --prefix PATH : "${stdenv.lib.makeBinPath guestDeps}:$guest/bin"
+      ln -s $i $out/bin
     done
   '';
 
