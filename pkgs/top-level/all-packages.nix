a19d6323c0a23135e175a01b8185c36b830a40e9
Yuki Okushi <huyuumi.dev@gmail.com>
2020-08-20 00:18:52 +0900

    cargo-tree: remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9450,7 +9450,6 @@ in
     inherit (darwin.apple_sdk.frameworks) Security;
   };
   cargo-tarpaulin = callPackage ../development/tools/analysis/cargo-tarpaulin { };
-  cargo-tree = callPackage ../tools/package-management/cargo-tree { };
   cargo-update = callPackage ../tools/package-management/cargo-update {
     inherit (darwin.apple_sdk.frameworks) Security;
   };
