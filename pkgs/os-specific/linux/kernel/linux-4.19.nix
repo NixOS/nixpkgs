3bf0c20b36dad537a70e069790d28c45b6e6e5bd
Tim Steinbach <tim@nequissimus.com>
2020-08-19 08:21:28 -0400

    linux: 4.19.139 -> 4.19.140

--- pkgs/os-specific/linux/kernel/linux-4.19.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.19.139";
+  version = "4.19.140";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "01anspwh3vskmcqf4xclx0jx6h0h33zfgmmvrzxq17wlzqhvyklw";
+    sha256 = "1a4fy48jb16qdx165n48qch6nikfvmxqwys7mw8fynq0pykzj3ya";
   };
 } // (args.argsOverride or {}))
