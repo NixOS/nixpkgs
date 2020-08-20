570fe856ffde2d8b877d67a384a31b341910ae7b
Mario Rodas <marsam@users.noreply.github.com>
2020-08-19 23:20:00 -0500

    protobuf3_13: init at 3.13.0
    https://github.com/protocolbuffers/protobuf/releases/tag/v3.13.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14546,6 +14546,7 @@ in
 
   protobuf = protobuf3_12;
 
+  protobuf3_13 = callPackage ../development/libraries/protobuf/3.13.nix { };
   protobuf3_12 = callPackage ../development/libraries/protobuf/3.12.nix { };
   protobuf3_11 = callPackage ../development/libraries/protobuf/3.11.nix { };
   protobuf3_10 = callPackage ../development/libraries/protobuf/3.10.nix { };
