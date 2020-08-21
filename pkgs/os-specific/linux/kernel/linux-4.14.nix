414c235681062cc8da201bea32b264d1d370095c
Tim Steinbach <tim@nequissimus.com>
2020-08-21 15:25:31 -0400

    linux: 4.14.193 -> 4.14.194

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.14.193";
+  version = "4.14.194";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "00wziff12xphafyspb02rnjim5a15zywnhdk70ks9q9h8hfv83qb";
+    sha256 = "1q7ssi2790bqjn8s8ra5ihma70hmxykahink7iq5h78738id191y";
   };
 } // (args.argsOverride or {}))
