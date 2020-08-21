0dfd4afe3cff3610ddefc2ed8e8bd14d634704c4
zimbatm <zimbatm@zimbatm.com>
2020-08-21 14:26:28 +0200

    pythonPackages.avro: 1.9.2 -> 1.10.0 (#95790)
    pythonPackages.avro: 1.9.2 -> 1.10.0

--- pkgs/development/python-modules/avro/default.nix
+++ pkgs/development/python-modules/avro/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildPythonPackage, fetchPypi, isPy3k, pycodestyle, isort }:
+{ stdenv, buildPythonPackage, isPy3k, fetchPypi, pycodestyle, isort }:
 
 buildPythonPackage rec {
   pname = "avro";
@@ -10,11 +10,22 @@ buildPythonPackage rec {
     sha256 = "bbf9f89fd20b4cf3156f10ec9fbce83579ece3e0403546c305957f9dac0d2f03";
   };
 
+  patchPhase = ''
+    # this test requires network access
+    sed -i 's/test_server_with_path/noop/' avro/test/test_ipc.py
+  '' + (stdenv.lib.optionalString isPy3k ''
+    # these files require twisted, which is not python3 compatible
+    rm avro/txipc.py
+    rm avro/test/txsample*
+  '');
+
   nativeBuildInputs = [ pycodestyle ];
   propagatedBuildInputs = [ isort ];
 
   meta = with stdenv.lib; {
     description = "A serialization and RPC framework";
     homepage = "https://pypi.python.org/pypi/avro/";
+    license = licenses.asl20;
+    maintainers = [ maintainers.zimbatm ];
   };
 }
