269c2ade9737b1c4e0306a2b6f0c513f392c8472
Piotr Bogdan <ppbogdan@gmail.com>
2020-08-21 17:21:47 +0100

    gnome3.gnome-contacts: 3.36.2 -> 3.38

--- pkgs/desktops/gnome-3/core/gnome-contacts/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-contacts/default.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-contacts";
-  version = "3.36.2";
+  version = "3.38";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-contacts/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "048l07biy8xrfidfyzrjd5lrnfzqhb767ih2gl7w6c4mmhj4g2dy";
+    sha256 = "f8LuaJwfOcLmN163YLlOFlHOSoKK1AggbToD4TPaGa8=";
   };
 
   propagatedUserEnvPkgs = [ evolution-data-server ];
