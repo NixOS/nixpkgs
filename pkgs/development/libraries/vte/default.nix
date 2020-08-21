6a6bb1aa4f35f35fb990357b678b1983d183ac6a
Piotr Bogdan <ppbogdan@gmail.com>
2020-08-21 19:37:26 +0100

    vte: 0.61.90 -> 0.62.0

--- pkgs/development/libraries/vte/default.nix
+++ pkgs/development/libraries/vte/default.nix
@@ -24,13 +24,13 @@
 
 stdenv.mkDerivation rec {
   pname = "vte";
-  version = "0.60.2";
+  version = "0.62.0";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "19ccbw0yca78h5qcnm8claj4fg1pj68nj1fsjqqfpzhj7w72i81m";
+    sha256 = "05mhvcwmini97x1hrjvx1xyqp6bqyglsh6j7yrrwq23zsip28bad";
   };
 
   passthru = {
