fceb87e537f0295a635221a88a07cceeb0005f72
Robert Scott <code@humanleg.org.uk>
2020-08-21 19:14:10 +0100

    openexr: 2.4.1 -> 2.4.2

--- pkgs/development/libraries/openexr/default.nix
+++ pkgs/development/libraries/openexr/default.nix
@@ -10,13 +10,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "openexr";
-  version = "2.4.1";
+  version = "2.4.2";
 
   src = fetchFromGitHub {
     owner = "AcademySoftwareFoundation";
     repo = "openexr";
     rev = "v${version}";
-    sha256 = "020gyl8zv83ag6gbcchmqiyx9rh2jca7j8n52zx1gk4rck7kwc01";
+    sha256 = "04ga72h8fha4z31vmwyb21i7xzncn13fzr54b73rcfkb4b017s4v";
   };
 
   outputs = [ "bin" "dev" "out" "doc" ];
