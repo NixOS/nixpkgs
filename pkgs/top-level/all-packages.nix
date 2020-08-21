faa23a0f321c784aff662cb21691689b82dba255
Daniël de Kok <me@danieldk.eu>
2020-08-21 14:48:40 +0200

    clpeak: init at 1.1.0
    A tool which profiles OpenCL devices to find their peak capacities.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26205,6 +26205,8 @@ in
 
   clinfo = callPackage ../tools/system/clinfo { };
 
+  clpeak = callPackage ../tools/misc/clpeak { };
+
   cups = callPackage ../misc/cups { };
 
   cups-filters = callPackage ../misc/cups/filters.nix { };
