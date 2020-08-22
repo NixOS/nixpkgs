ead09d7e6ebb2b135a78fe1dfb4e3ef72cc99255
Louis Bettens <louis@bettens.info>
2020-08-22 22:38:10 +0200

    xpra: force XPRA_COMMAND

--- pkgs/tools/X11/xpra/default.nix
+++ pkgs/tools/X11/xpra/default.nix
@@ -94,6 +94,7 @@ in buildPythonApplication rec {
     makeWrapperArgs+=(
       "''${gappsWrapperArgs[@]}"
       --set XPRA_INSTALL_PREFIX "$out"
+      --set XPRA_COMMAND "$out/bin/xpra"
       --prefix LD_LIBRARY_PATH : ${libfakeXinerama}/lib
       --prefix PATH : ${stdenv.lib.makeBinPath [ getopt xorgserver xauth which utillinux pulseaudio ]}
     )
