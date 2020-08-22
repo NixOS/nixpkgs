0fe76b231ba2061befdc09797859eec6c55762a3
Mario Rodas <marsam@users.noreply.github.com>
2020-08-21 19:54:49 -0500

    recursive: 1.054 -> 1.057
    https://github.com/arrowtype/recursive/releases/tag/1.057

--- pkgs/data/fonts/recursive/default.nix
+++ pkgs/data/fonts/recursive/default.nix
@@ -1,21 +1,21 @@
 { lib, fetchzip }:
 
 let
-  version = "1.054";
+  version = "1.057";
 in
 fetchzip {
   name = "recursive-${version}";
 
-  url = "https://github.com/arrowtype/recursive/releases/download/${version}/ArrowType-Recursive-${version}.zip";
+  url = "https://github.com/arrowtype/recursive/releases/download/${version}/Recursive-${version}.zip";
 
   postFetch = ''
     mkdir -p $out/share/fonts/
-    unzip -j $downloadedFile \*.otf -x __MACOSX/\* -d $out/share/fonts/opentype
-    unzip -j $downloadedFile \*.ttf -x __MACOSX/\* -d $out/share/fonts/truetype
-    unzip -j $downloadedFile \*.woff2 -x __MACOSX/\* -d $out/share/fonts/woff2
+    unzip -j $downloadedFile \*.otf -d $out/share/fonts/opentype
+    unzip -j $downloadedFile \*.ttf -d $out/share/fonts/truetype
+    unzip -j $downloadedFile \*.woff2 -d $out/share/fonts/woff2
   '';
 
-  sha256 = "12ld0w7x5lyvymrnqzfj74a3m6knv7i1795bvnpyljmxxkacscnl";
+  sha256 = "0qsqldnhgl93h1izminjajjymvyxsw91ngdbxlhgbqvadg77325y";
 
   meta = with lib; {
     homepage = "https://recursive.design/";
