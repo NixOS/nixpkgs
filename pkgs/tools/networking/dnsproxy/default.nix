460ad27a4d8ec6cbde294286ee113b2f1711808c
R. RyanTM <ryantm-bot@ryantm.com>
2020-08-22 05:41:01 +0000

    dnsproxy: 0.29.0 -> 0.31.0

--- pkgs/tools/networking/dnsproxy/default.nix
+++ pkgs/tools/networking/dnsproxy/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "dnsproxy";
-  version = "0.29.0";
+  version = "0.31.0";
 
   src = fetchFromGitHub {
     owner = "AdguardTeam";
     repo = pname;
     rev = "v${version}";
-    sha256 = "164l97x1g20a61jkb2dwwmf63md3np9x2m59dri3qf22k4rl4l0d";
+    sha256 = "1jyik1022iv8nqjfrv3lkvcpr8zsaxfx8hi7yagklbs6vzlg80jg";
   };
 
   vendorSha256 = null;
