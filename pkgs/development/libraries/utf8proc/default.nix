d9aed71a388775a62bc37414bd5a5fcaa8a5751d
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-08-23 14:27:55 +0200

    utf8proc: enable tests
    For the current version normtest and graphemetest are disabled. This
    will probably change in the future. Those tests are problematic because
    they depend on unicode data files which are downloaded by cmake at build
    time:
    
        file(MAKE_DIRECTORY data)
        set(UNICODE_VERSION 13.0.0)
        file(DOWNLOAD https://www.unicode.org/Public/${UNICODE_VERSION}/ucd/NormalizationTest.txt data/NormalizationTest.txt SHOW_PROGRESS)
        file(DOWNLOAD https://www.unicode.org/Public/${UNICODE_VERSION}/ucd/auxiliary/GraphemeBreakTest.txt data/GraphemeBreakTest.txt SHOW_PROGRESS)
    
    For the next update, we'll probably need to patch out this section of
    CMakeLists.txt and download the files in question with fetchurl
    ourselves.

--- pkgs/development/libraries/utf8proc/default.nix
+++ pkgs/development/libraries/utf8proc/default.nix
@@ -15,6 +15,8 @@ stdenv.mkDerivation rec {
 
   cmakeFlags = [
     "-DBUILD_SHARED_LIBS=ON"
+    "-DUTF8PROC_ENABLE_TESTING=ON"
+    "-DCMAKE_SKIP_BUILD_RPATH=OFF"
   ];
 
   # the pkg-config file is not created in the cmake installation
@@ -22,6 +24,8 @@ stdenv.mkDerivation rec {
   # see https://github.com/JuliaStrings/utf8proc/issues/198
   preConfigure = "make libutf8proc.pc prefix=$out";
   postInstall = "install -Dm644 ../libutf8proc.pc -t $out/lib/pkgconfig/";
+
+  doCheck = true;
 
   meta = with stdenv.lib; {
     description = "A clean C library for processing UTF-8 Unicode data";
