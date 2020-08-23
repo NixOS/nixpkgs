bcbe1166a54869e8294e5ddbdd7d0edc883dea63
Sascha Grunert <sgrunert@suse.com>
2020-08-23 16:08:10 +0200

    cri-o: add NixOS test via critest
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -71,6 +71,7 @@ in
   convos = handleTest ./convos.nix {};
   corerad = handleTest ./corerad.nix {};
   couchdb = handleTest ./couchdb.nix {};
+  cri-o = handleTestOn ["x86_64-linux"] ./cri-o.nix {};
   deluge = handleTest ./deluge.nix {};
   dhparams = handleTest ./dhparams.nix {};
   dnscrypt-proxy2 = handleTestOn ["x86_64-linux"] ./dnscrypt-proxy2.nix {};
