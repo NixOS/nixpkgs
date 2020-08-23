e33614a7e2938cc40402ead7567c4eeb550f7496
Ben Darwin <bcdarwin@gmail.com>
2020-08-23 16:27:38 -0400

    python3Packages.test-tube: init at 0.7.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6737,6 +6737,8 @@ in {
 
   terminaltables = callPackage ../development/python-modules/terminaltables { };
 
+  test-tube = callPackage ../development/python-modules/test-tube { };
+
   testpath = callPackage ../development/python-modules/testpath { };
 
   testrepository = callPackage ../development/python-modules/testrepository { };
