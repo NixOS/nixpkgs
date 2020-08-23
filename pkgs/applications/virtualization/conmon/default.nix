bb464f7063d162e1d2bd2a92c61744c0a6d4024a
zowoq <59103226+zowoq@users.noreply.github.com>
2020-08-24 08:42:49 +1000

    conmon: add cri-o to passthru.tests

--- pkgs/applications/virtualization/conmon/default.nix
+++ pkgs/applications/virtualization/conmon/default.nix
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
 
   installFlags = [ "PREFIX=$(out)" ];
 
-  passthru.tests = { inherit (nixosTests) podman; };
+  passthru.tests = { inherit (nixosTests) cri-o podman; };
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/containers/conmon";
