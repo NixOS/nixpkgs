520e10f6a50c59e2cc05f3233bf98bbbca140541
Francois-Rene Rideau <fare@tunes.org>
2020-08-24 00:05:00 -0400

    gerbilPackages-unstable.gerbil-utils: 2020-08-02 -> 2020-10-18

--- pkgs/development/compilers/gerbil/gerbil-utils.nix
+++ pkgs/development/compilers/gerbil/gerbil-utils.nix
@@ -2,8 +2,8 @@
 
 gerbil-support.gerbilPackage {
   pname = "gerbil-utils";
-  version = "unstable-2020-08-02";
-  git-version = "0.2-21-g7e7d053";
+  version = "unstable-2020-10-18";
+  git-version = "0.2-36-g8b481b7";
   gerbil-package = "clan";
   gerbil = gerbil-unstable;
   gambit-params = gambit-support.unstable-params;
@@ -12,8 +12,8 @@ gerbil-support.gerbilPackage {
   src = fetchFromGitHub {
     owner = "fare";
     repo = "gerbil-utils";
-    rev = "7e7d053ec5e78cc58d38cb03baf554d83b31b0c6";
-    sha256 = "078vqdcddfavqq0d9pw430iz1562cgx1ck3fw6dpwxjkyc6m4bms";
+    rev = "8b481b787e13e07e14d0718d670aab016131a090";
+    sha256 = "0br8k5b2wcv4wcp65r2bfhji3af2qgqjspf41syqslq9awx47f3m";
   };
   meta = {
     description = "Gerbil Clan: Community curated Collection of Common Utilities";
