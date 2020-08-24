b8b50f7ff6a95470ab19080f6d4e2a5722c16d48
Mario Rodas <marsam@users.noreply.github.com>
2020-08-24 04:20:00 +0000

    vid-stab: 0.98b -> 1.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15521,7 +15521,9 @@ in
 
   vcg = callPackage ../development/libraries/vcg { };
 
-  vid-stab = callPackage ../development/libraries/vid-stab { };
+  vid-stab = callPackage ../development/libraries/vid-stab {
+    inherit (llvmPackages) openmp;
+  };
 
   vigra = callPackage ../development/libraries/vigra { };
 
