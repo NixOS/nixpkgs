3e889c1580e84d270a23d509f9d346d5fbdbdbfd
José Romildo Malaquias <malaquias@gmail.com>
2020-08-24 08:38:15 -0300

    marwaita: 7.4.2 -> 7.4.3

--- pkgs/data/themes/marwaita/default.nix
+++ pkgs/data/themes/marwaita/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "marwaita";
-  version = "7.4.2";
+  version = "7.4.3";
 
   src = fetchFromGitHub {
     owner = "darkomarko42";
     repo = pname;
     rev = version;
-    sha256 = "0kq7d8nqp8m0kbh2k9s0yybfdkyfkhbkjsv22lplnzh1p84pnlx7";
+    sha256 = "1g8xyv0najy4lpwa9xplx3ylxvn86dyi58j7qanc6r9yddy85ln9";
   };
 
   buildInputs = [
@@ -33,7 +33,6 @@ stdenv.mkDerivation rec {
     runHook preInstall
     mkdir -p $out/share/themes
     cp -a Marwaita* $out/share/themes
-    rm $out/share/themes/*/COPYING
     runHook postInstall
   '';
 
