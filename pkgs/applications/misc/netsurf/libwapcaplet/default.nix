0a22ca9820ed5886bb70045bdc29c10215d61576
Maxim Krivchikov <maxim.krivchikov@gmail.com>
2020-08-24 11:12:31 +0300

    netsurf.libwapcaplet: 0.4.2 -> 0.4.3

--- pkgs/applications/misc/netsurf/libwapcaplet/default.nix
+++ pkgs/applications/misc/netsurf/libwapcaplet/default.nix
@@ -6,11 +6,11 @@ stdenv.mkDerivation rec {
 
   name = "netsurf-${libname}-${version}";
   libname = "libwapcaplet";
-  version = "0.4.2";
+  version = "0.4.3";
 
   src = fetchurl {
     url = "http://download.netsurf-browser.org/libs/releases/${libname}-${version}-src.tar.gz";
-    sha256 = "1fjwzbn7j8bi1b9bvwxsy3i2cr6byq2s2d29866801pjnf528g86";
+    sha256 = "0p0c2q9lsj4vs97aa7vjllfhw33zv3dpysdkjblzhib6dpfs2alv";
   };
 
   buildInputs = [ buildsystem ];
@@ -19,8 +19,6 @@ stdenv.mkDerivation rec {
     "PREFIX=$(out)"
     "NSSHARED=${buildsystem}/share/netsurf-buildsystem"
   ];
-
-  NIX_CFLAGS_COMPILE = "-Wno-error=cast-function-type";
 
   meta = with stdenv.lib; {
     homepage = "http://www.netsurf-browser.org/";
