9d016e63937b71935de0d23c1ad537c53b450365
Piotr Bogdan <ppbogdan@gmail.com>
2020-08-25 08:30:47 +0100

    gnome3.gnome-mahjongg: 3.36.2 -> 3.38.1
    Changelogs:
    - https://ftp.gnome.org/pub/GNOME/sources/gnome-mahjongg/3.38/gnome-mahjongg-3.38.0.news
    - https://ftp.gnome.org/pub/GNOME/sources/gnome-mahjongg/3.38/gnome-mahjongg-3.38.1.news
    
    Signed-off-by: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome-3/games/gnome-mahjongg/default.nix
+++ pkgs/desktops/gnome-3/games/gnome-mahjongg/default.nix
@@ -1,41 +1,45 @@
 { stdenv, fetchurl, pkgconfig, gnome3, gtk3, wrapGAppsHook
 , librsvg, gettext, itstool, libxml2
-, meson, ninja, python3, vala, desktop-file-utils
+, meson, ninja, glib, vala, desktop-file-utils
 }:
 
 stdenv.mkDerivation rec {
   pname = "gnome-mahjongg";
-  version = "3.36.2";
+  version = "3.38.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-mahjongg/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "15xfp2acqdnn0pcwg5d77dpv758jjyclwb042wm12gg07rbg3s6j";
-  };
-
-  passthru = {
-    updateScript = gnome3.updateScript { packageName = "gnome-mahjongg"; attrPath = "gnome3.gnome-mahjongg"; };
+    sha256 = "N0LcYxD8M/NewYfwJpnfIDzVb27pS0Hz7vJdrinutkc=";
   };
 
   nativeBuildInputs = [
-    meson ninja vala python3 desktop-file-utils
+    meson ninja vala desktop-file-utils
     pkgconfig gnome3.adwaita-icon-theme
     libxml2 itstool gettext wrapGAppsHook
+    glib # for glib-compile-schemas
   ];
   buildInputs = [
-    gtk3 librsvg
+    glib
+    gtk3
+    librsvg
   ];
 
-  postPatch = ''
-    chmod +x data/meson_post_install.py
-    patchShebangs data/meson_post_install.py
-  '';
+  mesonFlags = [
+    "-Dcompile-schemas=enabled"
+  ];
 
+  passthru = {
+    updateScript = gnome3.updateScript {
+      packageName = pname;
+      attrPath = "gnome3.${pname}";
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = "https://wiki.gnome.org/Apps/Mahjongg";
     description = "Disassemble a pile of tiles by removing matching pairs";
     maintainers = teams.gnome.members;
-    license = licenses.gpl2;
+    license = licenses.gpl3Plus;
     platforms = platforms.linux;
   };
 }
