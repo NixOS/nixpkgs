c63bd6a6709f7cc190f07c2fc2a48555a69ee74f
R. RyanTM <ryantm-bot@ryantm.com>
2020-08-25 01:38:30 +0000

    avfs: 1.1.2 -> 1.1.3

--- pkgs/tools/filesystems/avfs/default.nix
+++ pkgs/tools/filesystems/avfs/default.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   pname = "avfs";
-  version = "1.1.2";
+  version = "1.1.3";
   src = fetchurl {
     url = "mirror://sourceforge/avf/${version}/${pname}-${version}.tar.bz2";
-    sha256 = "035b6y49nzgswf5n70aph8pm48sbv9nqwlnp3wwbq892c39kk4xn";
+    sha256 = "1psh8k7g7rb0gn7aygbjv86kxyi9xq07barxksa99nnmq3lc2kjg";
   };
 
   nativeBuildInputs = [ pkgconfig ];
