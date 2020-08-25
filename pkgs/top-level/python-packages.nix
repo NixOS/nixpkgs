8a59aaef0316d7132a161d0a195a90994a5f7c0c
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-08-25 17:26:26 +0200

    pythonPackages.fontmath: init at 0.6.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4140,6 +4140,8 @@ in {
 
   fontpens = callPackage ../development/python-modules/fontpens { };
 
+  fontmath = callPackage ../development/python-modules/fontmath { };
+
   fonttools = callPackage ../development/python-modules/fonttools { };
 
   foolscap = callPackage ../development/python-modules/foolscap { };
