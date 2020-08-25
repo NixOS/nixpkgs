f5a35eaea43205481dd89ce94d15c298b90fbe78
Sascha Grunert <sgrunert@suse.com>
2020-08-25 12:40:33 +0200

    ginkgo: init at 1.14.0
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17998,6 +17998,8 @@ in
 
   iferr = callPackage ../development/tools/iferr { };
 
+  ginkgo = callPackage ../development/tools/ginkgo { };
+
   go-bindata = callPackage ../development/tools/go-bindata { };
 
   go-bindata-assetfs = callPackage ../development/tools/go-bindata-assetfs { };
