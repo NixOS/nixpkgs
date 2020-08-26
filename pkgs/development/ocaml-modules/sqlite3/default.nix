accca0f2f6ad2721fe6d49ca68e4e7488476b238
R. RyanTM <ryantm-bot@ryantm.com>
2020-08-26 22:33:12 +0000

    ocamlPackages.ocaml_sqlite3: 5.0.1 -> 5.0.2

--- pkgs/development/ocaml-modules/sqlite3/default.nix
+++ pkgs/development/ocaml-modules/sqlite3/default.nix
@@ -2,12 +2,12 @@
 
 buildDunePackage rec {
   pname = "sqlite3";
-  version = "5.0.1";
+  version = "5.0.2";
   minimumOCamlVersion = "4.05";
 
   src = fetchurl {
     url = "https://github.com/mmottl/sqlite3-ocaml/releases/download/${version}/sqlite3-${version}.tbz";
-    sha256 = "0iymkszrs6qwak0vadfzc8yd8jfwn06zl08ggb4jr2mgk2c8mmsn";
+    sha256 = "0sba74n0jvzxibrclhbpqscil36yfw7i9jj9q562yhza6rax9p82";
   };
 
   nativeBuildInputs = [ pkgconfig ];
