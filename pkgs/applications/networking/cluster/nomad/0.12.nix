47198a5e5877c0356df25fbcc4096674e88aa7be
Maxine E. Aubrey <maxeaubrey@gmail.com>
2020-08-27 00:04:46 +0200

    nomad: 0.12.2 -> 0.12.3
    (cherry picked from commit 168a85b122b2c425099ceeef89516f18a7845918)

--- pkgs/applications/networking/cluster/nomad/0.12.nix
+++ pkgs/applications/networking/cluster/nomad/0.12.nix
@@ -1,6 +1,6 @@
 { callPackage }:
 
 callPackage ./generic.nix {
-  version = "0.12.2";
-  sha256 = "1gc286ag6plk5kxw7jzr32cp3n5rwydj1z7rds1rfd0fyq7an404";
+  version = "0.12.3";
+  sha256 = "100ynhc4nm4mmjxx1jhq2kjbqshxvi5x8y482520j8gsyn40g6zc";
 }
