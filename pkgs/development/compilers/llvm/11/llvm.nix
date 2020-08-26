16ea2d296fc606df71c3b39b5b6aa8da47150132
Michael Weiss <dev.primeos@gmail.com>
2020-08-26 14:30:46 +0200

    llvm_11: Enable build-id when linking libLLVM shared libs
    This is used by mesa.drivers (still on LLVM 9) as a cache key. I've
    ported that change to LLVM 11 to test it and so that it doesn't get lost
    in future versions. Credit for the change goes to David McFarland.
    See #93946 for details.
    
    Co-Authored-By: David McFarland <corngood@gmail.com>

--- pkgs/development/compilers/llvm/11/llvm.nix
+++ pkgs/development/compilers/llvm/11/llvm.nix
@@ -95,6 +95,9 @@ in stdenv.mkDerivation (rec {
     ln -sv $PWD/lib $out
   '';
 
+  # E.g. mesa.drivers use the build-id as a cache key (see #93946):
+  LDFLAGS = optionalString enableSharedLibraries "-Wl,--build-id=sha1";
+
   cmakeFlags = with stdenv; [
     "-DCMAKE_BUILD_TYPE=${if debugVersion then "Debug" else "Release"}"
     "-DLLVM_INSTALL_UTILS=ON"  # Needed by rustc
