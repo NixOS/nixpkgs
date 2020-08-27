d32c43763939a777bfcde097a283fb0e5b8c1c34
Jonathan Ringer <jonringer117@gmail.com>
2020-08-27 08:45:27 -0700

    catt: fix pychromecast version

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19719,7 +19719,7 @@ in
 
   catimg = callPackage ../tools/misc/catimg { };
 
-  catt = python3Packages.callPackage ../applications/video/catt { };
+  catt = callPackage ../applications/video/catt { };
 
   cava = callPackage ../applications/audio/cava { };
 
