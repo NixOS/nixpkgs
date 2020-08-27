f19e15d2468e17370e3686f8b56fe38b903bfe81
georgewhewell <georgerw@gmail.com>
2020-08-27 19:52:38 +0100

    spotipy: fix build

--- pkgs/development/python-modules/spotipy/default.nix
+++ pkgs/development/python-modules/spotipy/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildPythonPackage, fetchPypi, requests }:
+{ stdenv, buildPythonPackage, fetchPypi, requests, six, mock }:
 
 buildPythonPackage rec {
   pname = "spotipy";
@@ -9,7 +9,15 @@ buildPythonPackage rec {
     sha256 = "d1a85b8831ed60d2290ab9bbba289c548fb9680889757252c02dab2f2327d865";
   };
 
-  propagatedBuildInputs = [ requests ];
+  propagatedBuildInputs = [ requests six ];
+  checkInputs = [ mock ];
+
+  preConfigure = ''
+    substituteInPlace setup.py \
+      --replace "mock==2.0.0" "mock"
+  '';
+
+  pythonImportsCheck = [ "spotipy" ];
 
   meta = with stdenv.lib; {
     homepage = "https://spotipy.readthedocs.org/";
