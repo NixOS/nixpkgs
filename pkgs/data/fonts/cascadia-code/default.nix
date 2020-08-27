a6b064e6492eb471a5a5cecc0852bb57748e1b6e
Mario Rodas <marsam@users.noreply.github.com>
2020-08-27 04:20:00 +0000

    cascadia-code: 2007.01 -> 2008.25
    https://github.com/microsoft/cascadia-code/releases/tag/v2008.25

--- pkgs/data/fonts/cascadia-code/default.nix
+++ pkgs/data/fonts/cascadia-code/default.nix
@@ -1,13 +1,13 @@
 { lib, fetchzip }:
 let
-  version = "2007.01";
+  version = "2008.25";
 in
 fetchzip {
   name = "cascadia-code-${version}";
 
   url = "https://github.com/microsoft/cascadia-code/releases/download/v${version}/CascadiaCode-${version}.zip";
 
-  sha256 = "173dpr0k4y5b02ps9426pyaazl2pxj1kw5l5jrikbi6zjv4590gb";
+  sha256 = "182ssznm6f5fhykmqqvimq7ihmxkc64gh76faqxg1ihdyzqgi2y6";
 
   postFetch = ''
     mkdir -p $out/share/fonts/
