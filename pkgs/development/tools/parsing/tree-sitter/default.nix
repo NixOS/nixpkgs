238263972960e6e0b04fc81645013c580f0ffd0f
Gabriel Ebner <gebner@gebner.org>
2020-08-28 21:00:19 +0200

    tree-sitter: fix build

--- pkgs/development/tools/parsing/tree-sitter/default.nix
+++ pkgs/development/tools/parsing/tree-sitter/default.nix
@@ -55,6 +55,10 @@ in rustPlatform.buildRustPackage {
     # needed for the tests
     rm -rf test/fixtures/grammars
     ln -s ${grammars} test/fixtures/grammars
+
+    # These functions do not appear in the source code
+    sed -i /_ts_query_context/d lib/binding_web/exports.json
+    sed -i /___assert_fail/d lib/binding_web/exports.json
   '';
 
   # Compile web assembly with emscripten. The --debug flag prevents us from
