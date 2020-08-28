f816402fd0b1266d0bf3e7fc028b60da87f7ff5a
Alexandre Macabies <web+oss@zopieux.com>
2020-08-29 01:28:51 +0200

    rtl-sdr: use hardened udev rules
    rtl-sdr latest release 0.6.0 is from 2018. Six months ago master received
    a patch[1] to use improved udev rules that chown to plugdev instead of
    making the devices world-read-writable.
    
    This change backports [1] as an upstream patch.
    
    [1] https://osmocom.org/projects/rtl-sdr/repository/revisions/b2814731563be4d5a0a68554ece6454a2c63af12

--- pkgs/applications/radio/rtl-sdr/default.nix
+++ pkgs/applications/radio/rtl-sdr/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchgit, cmake, pkgconfig, libusb1 }:
+{ stdenv, fetchgit, fetchpatch, cmake, pkgconfig, libusb1 }:
 
 stdenv.mkDerivation rec {
   pname = "rtl-sdr";
@@ -9,6 +9,12 @@ stdenv.mkDerivation rec {
     rev = "refs/tags/${version}";
     sha256 = "0lmvsnb4xw4hmz6zs0z5ilsah5hjz29g1s0050n59fllskqr3b8k";
   };
+
+  patches = [ (fetchpatch {
+    name = "hardened-udev-rules.patch";
+    url = "https://osmocom.org/projects/rtl-sdr/repository/revisions/b2814731563be4d5a0a68554ece6454a2c63af12/diff?format=diff";
+    sha256 = "0ns740s2rys4glq4la4bh0sxfv1mn61yfjns2yllhx70rsb2fqrn";
+  }) ];
 
   nativeBuildInputs = [ pkgconfig cmake ];
   buildInputs = [ libusb1 ];
