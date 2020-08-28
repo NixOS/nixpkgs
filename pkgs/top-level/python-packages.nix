77984779cbe5ab19ff7b2df71abea5f7c8ab8070
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-08-28 23:39:15 +0200

    pythonPackages.samsungctl: init at 0.7.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3702,6 +3702,8 @@ in {
 
   samplerate = callPackage ../development/python-modules/samplerate { };
 
+  samsungctl = callPackage ../development/python-modules/samsungctl { };
+
   screeninfo = callPackage ../development/python-modules/screeninfo { };
 
   ssdeep = callPackage ../development/python-modules/ssdeep { };
