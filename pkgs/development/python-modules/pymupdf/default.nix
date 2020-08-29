1ee7c949530a605160485a73208e67790bf15d2b
Frederik Rietdijk <fridh@fridh.nl>
2020-08-29 13:41:50 +0200

    python3Packages.PyMuPDF: 1.17.5 -> 1.17.6

--- pkgs/development/python-modules/pymupdf/default.nix
+++ pkgs/development/python-modules/pymupdf/default.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPythonPackage, fetchPypi, mupdf, swig }:
 buildPythonPackage rec {
   pname = "PyMuPDF";
-  version = "1.17.5";
+  version = "1.17.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ddec02c4dd1c2e0ead4d61cd97b52e725d643602f1f7832b5016190f5c653add";
+    sha256 = "5eef4ff61698016a296932858625a5a0039b923ba692043e2c98ca02966f1da2";
   };
 
   patchPhase = ''
