f2b928dfb740a40681f27717ae7883d6b1685a56
Gabriel Ebner <gebner@gebner.org>
2020-08-29 11:27:35 +0200

    binaryen: 95 -> 96

--- pkgs/development/compilers/binaryen/default.nix
+++ pkgs/development/compilers/binaryen/default.nix
@@ -1,15 +1,23 @@
-{ stdenv, cmake, python3, fetchFromGitHub }:
+{ stdenv, cmake, python3, fetchFromGitHub, fetchpatch, emscripten }:
 
 stdenv.mkDerivation rec {
   pname = "binaryen";
-  version = "95";
+  version = "96";
 
   src = fetchFromGitHub {
     owner = "WebAssembly";
     repo = "binaryen";
     rev = "version_${version}";
-    sha256 = "1w4js9bm5qv5aws8bzz4f0n3ni2l7h4fidkq9v5bldf0zxncy8m3";
+    sha256 = "1mqpb6yy87aifpbcy0lczi3bp6kddrwi6d0g6lrhjrdxx2kvbdag";
   };
+
+  patches = [
+    # Adds --minimize-wasm-changes option required by emscripten 2.0.1
+    (fetchpatch {
+      url = "https://patch-diff.githubusercontent.com/raw/WebAssembly/binaryen/pull/3044.patch";
+      sha256 = "1hdbc9h9zhh2d3bl4sqv6v9psfmny715612bwpjdln0ibdvc129s";
+    })
+  ];
 
   nativeBuildInputs = [ cmake python3 ];
 
@@ -19,5 +27,9 @@ stdenv.mkDerivation rec {
     platforms = platforms.all;
     maintainers = with maintainers; [ asppsa ];
     license = licenses.asl20;
+  };
+
+  passthru.tests = {
+    inherit emscripten;
   };
 }
