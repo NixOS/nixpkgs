832df8fcdac3917428109d43dda992de72fe20d9
Jack Kelly <jack@jackkelly.name>
2020-08-29 14:58:34 +1000

    ec2-metadata-fetcher: use IMDSv2, fetch public-ipv4 as well

--- nixos/modules/virtualisation/ec2-metadata-fetcher.nix
+++ nixos/modules/virtualisation/ec2-metadata-fetcher.nix
@@ -1,23 +1,32 @@
 { targetRoot, wgetExtraOptions }:
 ''
+  imds=http://169.254.169.254/2019-10-01
   metaDir=${targetRoot}etc/ec2-metadata
   mkdir -m 0755 -p "$metaDir"
 
   echo "getting EC2 instance metadata..."
 
+  # Construct the PUT request by hand, because wget is busybox wget and can't.
+  TOKEN=$(echo -ne 'PUT /latest/api/token HTTP/1.1\r\nHost: 169.254.169.254\r\nX-aws-ec2-metadata-token-ttl-seconds: 60\r\n\r\n' | nc 169.254.169.254 80 | tail -n 1)
+  wgetCmd="wget ${wgetExtraOptions} --header 'X-aws-ec2-metadata-token: $TOKEN'"
+
   if ! [ -e "$metaDir/ami-manifest-path" ]; then
-    wget ${wgetExtraOptions} -O "$metaDir/ami-manifest-path" http://169.254.169.254/1.0/meta-data/ami-manifest-path
+    $wgetCmd -O "$metaDir/ami-manifest-path" $imds/meta-data/ami-manifest-path
   fi
 
   if ! [ -e "$metaDir/user-data" ]; then
-    wget ${wgetExtraOptions} -O "$metaDir/user-data" http://169.254.169.254/1.0/user-data && chmod 600 "$metaDir/user-data"
+    $wgetCmd -O "$metaDir/user-data" $imds/user-data && chmod 600 "$metaDir/user-data"
   fi
 
   if ! [ -e "$metaDir/hostname" ]; then
-    wget ${wgetExtraOptions} -O "$metaDir/hostname" http://169.254.169.254/1.0/meta-data/hostname
+    $wgetCmd -O "$metaDir/hostname" $imds/meta-data/hostname
+  fi
+
+  if ! [ -e "$metaDir/public-ipv4" ]; then
+    $wgetCmd -O "$metaDir/public-ipv4" $imds/meta-data/public-ipv4
   fi
 
   if ! [ -e "$metaDir/public-keys-0-openssh-key" ]; then
-    wget ${wgetExtraOptions} -O "$metaDir/public-keys-0-openssh-key" http://169.254.169.254/1.0/meta-data/public-keys/0/openssh-key
+    $wgetCmd -O "$metaDir/public-keys-0-openssh-key" $imds/meta-data/public-keys/0/openssh-key
   fi
 ''
