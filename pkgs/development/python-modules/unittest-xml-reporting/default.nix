5bc40850276ee4fd1b837f7c6bd5e52b913cd3b9
Frederik Rietdijk <fridh@fridh.nl>
2020-08-29 13:41:52 +0200

    python3Packages.unittest-xml-reporting: 3.0.3 -> 3.0.4

--- pkgs/development/python-modules/unittest-xml-reporting/default.nix
+++ pkgs/development/python-modules/unittest-xml-reporting/default.nix
@@ -2,7 +2,7 @@
 
 buildPythonPackage rec {
   pname = "unittest-xml-reporting";
-  version = "3.0.3";
+  version = "3.0.4";
   disabled = isPy27;
 
   propagatedBuildInputs = [six];
@@ -12,7 +12,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "89ad3376cc63dc0f7227c1e39d03d5f6a20807fef989c57d8c623446b5f79575";
+    sha256 = "984cebba69e889401bfe3adb9088ca376b3a1f923f0590d005126c1bffd1a695";
   };
   meta = with lib; {
     homepage = "https://github.com/xmlrunner/unittest-xml-reporting/tree/master/";
