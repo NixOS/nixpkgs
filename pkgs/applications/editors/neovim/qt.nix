79b4e5b11fee99013e7d64daa6f156c85b79917a
Kevin Cox <kevincox@kevincox.ca>
2020-08-30 13:54:09 -0400

    Update pkgs/applications/editors/neovim/qt.nix
    Co-authored-by: Jon <jonringer@users.noreply.github.com>

--- pkgs/applications/editors/neovim/qt.nix
+++ pkgs/applications/editors/neovim/qt.nix
@@ -38,7 +38,7 @@ let
 
     meta = with stdenv.lib; {
       description = "Neovim client library and GUI, in Qt5";
-      homepage = https://github.com/equalsraf/neovim-qt;
+      homepage = "https://github.com/equalsraf/neovim-qt";
       license     = licenses.isc;
       maintainers = with maintainers; [ peterhoeg ];
       inherit (neovim.meta) platforms;
