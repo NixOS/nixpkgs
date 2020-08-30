ff7f281b4e093915997fac92dda6e4bf7fc84e56
Jappie Klooster <jappieklooster@hotmail.com>
2020-08-30 16:54:42 +0200

    cut-the-crap: Depend on c2hs
    This makes the 1.4.x builds less broken.
    I made a 1.4.1 release that also includes the header
    files now.
    
    To debug further I need the hackage be regenerated.
    
    The root cause of this breakage is the added dependency of pocketsphinx.
    cabal2nix seems to be able to figure that part out,
    but not something like c2hs, and I just didn't know header
    files wouldn't be included in the sdist. (I though specifying an
    include dir was enough).

--- pkgs/development/haskell-modules/configuration-nix.nix
+++ pkgs/development/haskell-modules/configuration-nix.nix
@@ -722,6 +722,7 @@ self: super: builtins.intersectAttrs super {
   cut-the-crap =
     let path = pkgs.stdenv.lib.makeBinPath [ pkgs.ffmpeg_3 ];
     in overrideCabal (addBuildTool super.cut-the-crap pkgs.makeWrapper) (_drv: {
+      libraryToolDepends = [ self.c2hs ];
       postInstall = ''
         wrapProgram $out/bin/cut-the-crap \
           --prefix PATH : "${path}"
