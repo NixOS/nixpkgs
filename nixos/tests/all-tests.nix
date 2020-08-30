de8f8ed0763a304a3081327dd0dec7e7fc55be0f
Georg Haas <hax404foogit@hax404.de>
2020-08-30 18:31:54 +0200

    nixos/tests/robustirc-bridge: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -298,6 +298,7 @@ in
   redis = handleTest ./redis.nix {};
   redmine = handleTest ./redmine.nix {};
   restic = handleTest ./restic.nix {};
+  robustirc-bridge = handleTest ./robustirc-bridge.nix {};
   roundcube = handleTest ./roundcube.nix {};
   rspamd = handleTest ./rspamd.nix {};
   rss2email = handleTest ./rss2email.nix {};
