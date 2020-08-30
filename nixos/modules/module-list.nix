fe228cb25b88e2525510d8841d3588a4c662551a
Jamie McClymont <jamie@kwiius.com>
2020-08-30 19:40:31 +1200

    nixos/matrix-appservice-irc: init
    Co-authored-by: Brian McKenna <brian@brianmckenna.org>
    Co-authored-by: piegames <git@piegames.de>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -490,6 +490,7 @@
   ./services/misc/lidarr.nix
   ./services/misc/mame.nix
   ./services/misc/matrix-appservice-discord.nix
+  ./services/misc/matrix-appservice-irc.nix
   ./services/misc/matrix-synapse.nix
   ./services/misc/mautrix-telegram.nix
   ./services/misc/mbpfan.nix
