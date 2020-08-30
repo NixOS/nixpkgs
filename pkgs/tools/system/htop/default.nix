b3ffab69b043322bdfd255b2dd911ea19e393ff0
Izorkin <izorkin@elven.pw>
2020-08-30 12:03:01 +0300

    htop: disable use glyphs for checkmarks

--- pkgs/tools/system/htop/default.nix
+++ pkgs/tools/system/htop/default.nix
@@ -1,5 +1,7 @@
-{ lib, fetchFromGitHub, stdenv, autoreconfHook, ncurses,
-IOKit, python3 }:
+{ lib, fetchFromGitHub, stdenv, autoreconfHook
+, ncurses, IOKit, python3
+, fetchpatch
+}:
 
 stdenv.mkDerivation rec {
   pname = "htop";
@@ -12,10 +14,19 @@ stdenv.mkDerivation rec {
     sha256 = "096gdnpaszs5rfp7qj8npi2jkvdqpp8mznn89f97ykrg6pgagwq4";
   };
 
+  patches = [
+    # Never use glyphs for checkmarks. Issue - https://github.com/htop-dev/htop/issues/29
+    # Remove with the next release.
+    (fetchpatch {
+      url = "https://github.com/htop-dev/htop/commit/96074058278829facb86f6f4de099d56a00a0c0e.patch";
+      sha256 = "1rnfvjfsvfgr1s7kzr1hk6nwik6shcq4mg6dlbgdq0f2fz0cnazk";
+    })
+  ];
+
   nativeBuildInputs = [ autoreconfHook python3 ];
-  buildInputs =
-    [ ncurses ] ++
-    lib.optionals stdenv.isDarwin [ IOKit ];
+
+  buildInputs = [ ncurses
+  ] ++ lib.optionals stdenv.isDarwin [ IOKit ];
 
   meta = with stdenv.lib; {
     description = "An interactive process viewer for Linux";
