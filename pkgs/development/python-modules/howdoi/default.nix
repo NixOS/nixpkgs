4f16ce1e8e55998de662ca7fd3e408c2d185788a
Robert Scott <code@humanleg.org.uk>
2020-08-31 15:05:07 +0100

    pythonPackages.howdoi: 2.0.3 -> 2.0.4, fix build
    had to disable the tests (hopefully temporarily) because of missing
    page_cache directory in release tarball.
    
    removed no-longer-needed requests-cache dependency

--- pkgs/development/python-modules/howdoi/default.nix
+++ pkgs/development/python-modules/howdoi/default.nix
@@ -2,11 +2,11 @@
 , buildPythonPackage
 , fetchPypi
 , six
-, requests-cache
 , pygments
 , pyquery
 , cachelib
 , appdirs
+, keep
 }:
 
 buildPythonPackage rec {
@@ -18,11 +18,21 @@ buildPythonPackage rec {
     sha256 = "8e4d048ae7ca6182d648f62a66d07360cca2504fe46649c32748b6ef2735f7f4";
   };
 
-  propagatedBuildInputs = [ six requests-cache pygments pyquery cachelib appdirs ];
+  postPatch = ''
+    substituteInPlace setup.py --replace 'cachelib==0.1' 'cachelib'
+  '';
 
+  propagatedBuildInputs = [ six pygments pyquery cachelib appdirs keep ];
+
+  # author hasn't included page_cache directory (which allows tests to run without
+  # external requests) in pypi tarball. github repo doesn't have release revisions
+  # clearly tagged. re-enable tests when either is sorted.
+  doCheck = false;
   preCheck = ''
+    mv howdoi _howdoi
     export HOME=$(mktemp -d)
   '';
+  pythonImportsCheck = [ "howdoi" ];
 
   meta = with lib; {
     description = "Instant coding answers via the command line";
