5033c2a304673ae396b14be2aa714a96d75f3ce3
Amit Aryeh Levy <amit@amitlevy.com>
2020-08-31 12:42:01 -0700

    zoom-us: 5.1.422789.0705 -> 5.2.446620.0816
    (cherry picked from commit 71ac9f191a9bfdf249421c9dafe1f4dbc6449e83)

--- pkgs/applications/networking/instant-messengers/zoom-us/default.nix
+++ pkgs/applications/networking/instant-messengers/zoom-us/default.nix
@@ -15,11 +15,11 @@ assert pulseaudioSupport -> libpulseaudio != null;
 let
   inherit (stdenv.lib) concatStringsSep makeBinPath optional;
 
-  version = "5.1.422789.0705";
+  version = "5.2.446620.0816";
   srcs = {
     x86_64-linux = fetchurl {
       url = "https://zoom.us/client/${version}/zoom_x86_64.tar.xz";
-      sha256 = "1sc454xadxsbxxyb68qi7ac20yq0vymzzw1i07z19c9idfpjy75f";
+      sha256 = "0d7x4ji5l0rpanh0md75lywa8l9v6pd8bba5dba2xbfp7x9c3c4q";
     };
   };
 
