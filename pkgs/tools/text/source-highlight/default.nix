cbfc33a9cec2cfdcb1c1e2394272cbdb9988f85a
Christian Kampka <christian@kampka.net>
2020-08-31 13:52:09 +0200

    source-highlight: Skip building docs when cross-compiling
    source-highlight uses it's own binary to generate documentation.
    During cross-compilation, that binary was built for the target
    platform architecture, so it can't run on the build host.

--- pkgs/tools/text/source-highlight/default.nix
+++ pkgs/tools/text/source-highlight/default.nix
@@ -12,6 +12,14 @@ stdenv.mkDerivation {
     sha256 = "148w47k3zswbxvhg83z38ifi85f9dqcpg7icvvw1cm6bg21x4zrs";
   };
 
+  # source-highlight uses it's own binary to generate documentation.
+  # During cross-compilation, that binary was built for the target
+  # platform architecture, so it can't run on the build host.
+  patchPhase = stdenv.lib.optionalString (stdenv.hostPlatform != stdenv.buildPlatform) ''
+    substituteInPlace Makefile.in --replace "src doc tests" "src tests"
+  '';
+
+  strictDeps = true;
   buildInputs = [ boost ];
 
   configureFlags = [ "--with-boost=${boost.out}" ];
