89e76e8bdc15d059b1833ca9f0dec2736f21158e
Thomas Tuegel <ttuegel@mailbox.org>
2020-09-01 08:48:03 -0500

    powerdevil: update patches

--- pkgs/desktops/plasma-5/powerdevil.nix
+++ pkgs/desktops/plasma-5/powerdevil.nix
@@ -21,16 +21,16 @@ mkDerivation {
   patches = [
     # This fixes an issue where 'DDCA_Feature_List*' cannot be converted to
     # 'DDCA_Feature_List'.
-    # This can be dropped with the next release.
     # https://bugs.kde.org/show_bug.cgi?id=423605
     (fetchpatch {
       url = "https://invent.kde.org/plasma/powerdevil/-/commit/fcb26be2fb279e6ad3b7b814d26a5921d16201eb.patch";
       sha256 = "0gdyaa0nd1c1d6x2h0m933lascm8zm5sikd99wxmkf7hhaby6k2s";
     })
-    # This is a backport of
-    # https://invent.kde.org/plasma/powerdevil/-/commit/c7590f9065ec9547b7fabad77a548bbc0c693113.patch,
-    # which doesn't apply cleanly to 5.17.5.  It should make it into 5.20, so
-    # this patch can be removed when we upgrade to 5.20.
-    ./patches/0001-Add-a-logging-category-config-file.patch
+
+    # Reduce log message spam by setting the default log level to Warning.
+    (fetchpatch {
+      url = "https://invent.kde.org/plasma/powerdevil/-/commit/c7590f9065ec9547b7fabad77a548bbc0c693113.patch";
+      sha256 = "0vj70mhx6qhvbh4vn9qk9ir5w4s2m76hw2lsxmh3ibgsydz4yilz";
+    })
   ];
 }
