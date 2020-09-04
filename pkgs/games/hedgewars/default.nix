NixOS/nixpkgs

commit c33386195407e13d588ed4e13925c78d524ba86f

hedgewars: disable broken build
(cherry picked from commit 7a1432f0f1210bf4437cf24201de552e35b9b298)

pkgs/games/hedgewars/default.nix
@@ -5,7 +5,7 @@
 
 let
   ghc = ghcWithPackages (pkgs: with pkgs; [
-          network vector utf8-string bytestring-show random hslogger
+          network vector utf8-string /* broken: bytestring-show */ random hslogger
           SHA entropy pkgs.zlib sandi regex-tdfa
         ]);
 in
@@ -74,5 +74,7 @@ stdenv.mkDerivation rec {
        all movement on the battlefield has ceased).'';
     maintainers = with maintainers; [ kragniz fpletz ];
     platforms = ghc.meta.platforms;
+    hydraPlatforms = [];
+    broken = true;  # depends on broken Haskell package bytestring-show
   };
 }

commit 8003bb3cda6c1129a705706fe00e7d150bdaa233

hedgewars: enable build on hydra
Was disabled when the package was marked as broken in
c33386195407e13d588ed4e13925c78d524ba86f. It has since been un-broken,
but the hydraPlatforms exception was left presumably by mistake.

pkgs/games/hedgewars/default.nix
@@ -103,6 +103,5 @@ mkDerivation rec {
        all movement on the battlefield has ceased).'';
     maintainers = with maintainers; [ kragniz fpletz ];
     inherit (ghc.meta) platforms;
-    hydraPlatforms = [];
   };
 }