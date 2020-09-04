60788be0ad95baf740014d41860ddbf5a9367126
José Romildo Malaquias <malaquias@gmail.com>
2020-09-04 13:55:48 -0300

    theme-obsidian2: include theme variants

--- pkgs/data/themes/obsidian2/default.nix
+++ pkgs/data/themes/obsidian2/default.nix
@@ -1,22 +1,22 @@
-{ stdenv, fetchFromGitHub, gtk-engine-murrine }:
+{ stdenv, fetchurl, gtk-engine-murrine }:
 
 stdenv.mkDerivation rec {
   pname = "theme-obsidian2";
   version = "2.14";
 
-  src = fetchFromGitHub {
-    owner = "madmaxms";
-    repo = "theme-obsidian-2";
-    rev = "v${version}";
-    sha256 = "0i5lrj45zswd3rmclvywpk7x30bvib5zldhmfl38gbj477ag8ng8";
+  src = fetchurl {
+    url = "https://github.com/madmaxms/theme-obsidian-2/releases/download/v${version}/obsidian-2-theme.tar.xz";
+    sha256 = "0q713s6fwdvbiirzkm91y9xdpc7x7ay432km0fx90vn4s24p718y";
   };
+
+  sourceRoot = ".";
 
   propagatedUserEnvPkgs = [ gtk-engine-murrine ];
 
   installPhase = ''
     runHook preInstall
     mkdir -p $out/share/themes
-    cp -a Obsidian-2 $out/share/themes
+    cp -a Obsidian-2* $out/share/themes
     runHook postInstall
   '';
 
