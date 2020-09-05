fc1218d98ee7d5a17ab36970130569fb1c4b5c73
Florian Klink <flokli@flokli.de>
2020-09-05 16:07:14 +0200

    nixos/testing: remove remaining coverage-data logic
    This isn't used anymore as per
    https://github.com/NixOS/nixpkgs/pull/72354#discussion_r451031449.

--- nixos/modules/testing/test-instrumentation.nix
+++ nixos/modules/testing/test-instrumentation.nix
@@ -74,14 +74,7 @@ with import ../../lib/qemu-flags.nix { inherit pkgs; };
         # OOM killer randomly get rid of processes, since this leads
         # to failures that are hard to diagnose.
         echo 2 > /proc/sys/vm/panic_on_oom
-
-        # Coverage data is written into /tmp/coverage-data.
-        mkdir -p /tmp/xchg/coverage-data
       '';
-
-    # If the kernel has been built with coverage instrumentation, make
-    # it available under /proc/gcov.
-    boot.kernelModules = [ "gcov-proc" ];
 
     # Panic if an error occurs in stage 1 (rather than waiting for
     # user intervention).
@@ -110,8 +103,6 @@ with import ../../lib/qemu-flags.nix { inherit pkgs; };
     # Prevent tests from accessing the Internet.
     networking.defaultGateway = mkOverride 150 "";
     networking.nameservers = mkOverride 150 [ ];
-
-    systemd.globalEnvironment.GCOV_PREFIX = "/tmp/xchg/coverage-data";
 
     system.requiredKernelConfig = with config.lib.kernelConfig; [
       (isYes "SERIAL_8250_CONSOLE")
