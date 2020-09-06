f0ee5e7d0dd3973b7d42af3da54dda9cbff791d2
James ‘Twey’ Kay <twey@twey.co.uk>
2020-09-06 04:38:29 +0100

    mailpile module: allow multiple instances

--- nixos/modules/services/mail/mailpile.nix
+++ nixos/modules/services/mail/mailpile.nix
@@ -3,70 +3,121 @@
 with lib;
 
 let
-
   cfg = config.services.mailpile;
+  instance = name: { localPort, package, user, group, vhost, location, ... }: let
+    location' = if builtins.substring 0 1 location == "/"
+                then location
+                else "/${location}";
+    address = "http://localhost:${builtins.toString localPort}${location'}";
+    user-name = if user == null then "mailpile-${name}" else user;
+  in {
+    inherit package;
 
-  hostname = cfg.hostname;
-  port = cfg.port;
-
-in
-
-{
-
-  ###### interface
-
-  options = {
-
-    services.mailpile = {
-      enable = mkEnableOption "Mailpile the mail client";
-
-      hostname = mkOption {
-        default = "localhost";
-        description = "Listen to this hostname or ip.";
-      };
-      port = mkOption {
-        default = "33411";
-        description = "Listen on this port.";
+    users = {
+      ${if user != null then null else user-name} = {
+        isSystemUser = true;
+        description = "Mailpile user for ${name}";
+        createHome = true;
+        home = "/var/lib/mailpile/${name}";
       };
     };
 
+    services."mailpile-${name}" = {
+      description = "Mailpile server for ${name}";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        User = user-name;
+        Group = group;
+        ExecStart = ''${package}/bin/mailpile --www "${address}" --wait'';
+        # mixed - first send SIGINT to main process,
+        # then after 2min send SIGKILL to whole group if necessary
+        KillMode = "mixed";
+        KillSignal = "SIGINT";  # like Ctrl+C - safe mailpile shutdown
+        TimeoutSec = 20;  # wait 20s until SIGKILL
+      };
+    };
+
+    vhosts.${vhost}.locations.${location'}.proxyPass = address;
   };
+in {
+  disabledModules = [ "services/networking/mailpile.nix" ];
 
+  options.services.mailpile = mkOption {
+    default = { };
 
-  ###### implementation
+    description = "Named instances of Mailpile to run";
 
-  config = mkIf config.services.mailpile.enable {
-
-    users.users.mailpile =
-      { uid = config.ids.uids.mailpile;
-        description = "Mailpile user";
-        createHome = true;
-        home = "/var/lib/mailpile";
-      };
-
-    users.groups.mailpile =
-      { gid = config.ids.gids.mailpile;
-      };
-
-    systemd.services.mailpile =
-      {
-        description = "Mailpile server.";
-        after = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          User = "mailpile";
-          ExecStart = "${pkgs.mailpile}/bin/mailpile --www ${hostname}:${port} --wait";
-          # mixed - first send SIGINT to main process,
-          # then after 2min send SIGKILL to whole group if neccessary
-          KillMode = "mixed";
-          KillSignal = "SIGINT";  # like Ctrl+C - safe mailpile shutdown
-          TimeoutSec = 120;  # wait 2min untill SIGKILL
+    type = types.attrsOf (types.submodule {
+      options = {
+        localPort = mkOption {
+          default = 33144;
+          type = types.port;
+          description = ''
+            Local port to listen on.
+          '';
         };
-        environment.MAILPILE_HOME = "/var/lib/mailpile/.local/share/Mailpile";
+
+        package = mkOption {
+          default = pkgs.mailpile;
+          defaultText = "pkgs.mailpile";
+          type = types.package;
+          description = ''
+            Mailpile package to use.
+          '';
+        };
+
+        user = mkOption {
+          default = null;
+          type = types.nullOr types.str;
+          description = ''
+            Name of the user to run as.  Pass `null` to create a new
+            dedicated user named `mailpile-<instance name>` with home
+            under `/var/lib/mailpile`.
+          '';
+        };
+
+        group = mkOption {
+          default = "nogroup";
+          type = types.str;
+          description = ''
+            Name of the group to run as.
+          '';
+        };
+
+        vhost = mkOption {
+          default = null;
+          type = types.nullOr types.str;
+          description = ''
+            Name of the nginx virtual host on which to expose Mailpile.
+            Pass `null` to disable nginx integration.
+          '';
+        };
+
+        location = mkOption {
+          default = "/";
+          type = types.str;
+          description = ''
+            HTTP location at which Mailpile will be exposed.
+          '';
+        };
       };
+    });
 
-    environment.systemPackages = [ pkgs.mailpile ];
-
+    example = {
+      my-app = { localPort = 33144; };
+    };
   };
 
+  config = let
+    c = mapAttrsToList instance cfg;
+    vhosts = catAttrs "vhosts" c;
+  in mkIf (cfg != { }) {
+    systemd.tmpfiles.rules = [ "d /var/lib/mailpile 0755 root root - -" ];
+    environment.systemPackages = catAttrs "package" c;
+    users.users = mkMerge (catAttrs "users" c);
+    systemd.services = mkMerge (catAttrs "services" c);
+    services.nginx.enable = builtins.length vhosts > 0;
+    services.nginx.virtualHosts = mkMerge vhosts;
+  };
 }
