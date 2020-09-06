b55db43c5493f4c4e841dd1f74476709db7bf2f1
Lucas Savva <lucas@m1cr0man.com>
2020-09-06 16:53:05 +0100

    django-picklefield: Source from Github, fix test and build

--- pkgs/development/python-modules/django-picklefield/default.nix
+++ pkgs/development/python-modules/django-picklefield/default.nix
@@ -1,15 +1,24 @@
-{ lib, buildPythonPackage, fetchPypi, django }:
+{ lib, buildPythonPackage, fetchFromGitHub, django, python }:
 
 buildPythonPackage rec {
   pname = "django-picklefield";
   version = "3.0.1";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "15ccba592ca953b9edf9532e64640329cd47b136b7f8f10f2939caa5f9ce4287";
+  src = fetchFromGitHub {
+    owner = "gintas";
+    repo = "django-picklefield";
+    rev = "v${version}";
+    sha256 = "0ni7bc86k0ra4pc8zv451pzlpkhs1nyil1sq9jdb4m2mib87b5fk";
   };
 
   propagatedBuildInputs = [ django ];
+
+  dontUseSetuptoolsCheck = true;
+
+  # Taken from project's tox.ini
+  checkPhase = ''
+    ${python.interpreter} -m django test -v3 --settings=tests.settings
+  '';
 
   meta = {
     description = "A pickled object field for Django";
