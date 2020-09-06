dbbe502dfb782bafb922a79df89a20ccc5040bcf
Atemu <atemu.main@gmail.com>
2020-09-06 13:30:38 +0200

    linux_5_8: 5.8.6 -> 5.8.7

--- pkgs/os-specific/linux/kernel/linux-5.8.nix
+++ pkgs/os-specific/linux/kernel/linux-5.8.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.8.6";
+  version = "5.8.7";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "180bka8a0f2ykaifgb323pzgh0n909mlrsk08l08zmifggnh19cc";
+    sha256 = "1zhpzlhl2ykna2nc70m72wlgyv1pkvkpfssb4k8p5pwlkh1ga2vv";
   };
 } // (args.argsOverride or {}))
