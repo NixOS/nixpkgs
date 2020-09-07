9ac6a4c08aaa349fbf43b61a045be3a6076818c7
Edmund Wu <fangkazuto@gmail.com>
2020-09-06 21:48:32 -0400

    vscode-extensions.ms-python.python: 2020.8.103604 -> 2020.9.111407

--- pkgs/misc/vscode-extensions/python/default.nix
+++ pkgs/misc/vscode-extensions/python/default.nix
@@ -41,13 +41,13 @@ in vscode-utils.buildVscodeMarketplaceExtension rec {
   mktplcRef = {
     name = "python";
     publisher = "ms-python";
-    version = "2020.8.103604";
+    version = "2020.9.111407";
   };
 
   vsix = fetchurl {
     name = "${mktplcRef.publisher}-${mktplcRef.name}.zip";
     url = "https://github.com/microsoft/vscode-python/releases/download/${mktplcRef.version}/ms-python-release.vsix";
-    sha256 = "1gncnhmwjipaf7hzpimwzqdzyqyy4sznkq4qr2mn1ndjl6s1hh58";
+    sha256 = "0li68k0saj3v8w44jdmrkacf4jib4hkw0fca2rgv9cf18zswir1f";
   };
 
   buildInputs = [
