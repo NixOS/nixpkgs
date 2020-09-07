f59f61817cf1ea7c2b5acb416d6e769d09977278
makefu <github@syntax-fehler.de>
2020-09-07 08:56:02 +0200

    hovercraft: remove broken meta tag

--- pkgs/applications/misc/hovercraft/default.nix
+++ pkgs/applications/misc/hovercraft/default.nix
@@ -31,6 +31,5 @@ buildPythonApplication rec {
     homepage = https://github.com/regebro/hovercraft;
     license = licenses.mit;
     maintainers = with maintainers; [ goibhniu makefu ];
-    broken = true;
   };
 }
