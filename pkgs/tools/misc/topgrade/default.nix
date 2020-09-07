abd400550dc5efbd369971f4c8569388b4c7a9fe
R. RyanTM <ryantm-bot@ryantm.com>
2020-09-07 04:44:53 +0000

    topgrade: 5.5.0 -> 5.7.0

--- pkgs/tools/misc/topgrade/default.nix
+++ pkgs/tools/misc/topgrade/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "topgrade";
-  version = "5.5.0";
+  version = "5.7.0";
 
   src = fetchFromGitHub {
     owner = "r-darwish";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1adx029cq30g0qnrvdq2di8bpadzdxrpbsqchxfsda8zg6cprh1j";
+    sha256 = "0nxqi2rykfxhvn8jzprklsc47iilxp1pmm2f17ikfyf5dgi69whb";
   };
 
-  cargoSha256 = "0jpjn6sb8bkwnq7np487hb8bkm6rv84mihmqwy3ymgdzlqcng6sk";
+  cargoSha256 = "05afmz2n006331hc8yi2mq9kj574xi1iq6gr983jj75ix7n40rgg";
 
   buildInputs = lib.optional stdenv.isDarwin Foundation;
 
