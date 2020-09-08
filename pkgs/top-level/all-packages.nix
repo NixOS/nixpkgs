37a2b71b52ebcf7662322c88f4b3e25ef9235779
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2020-09-07 21:55:45 -0500

    sddm: use qt512

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23079,7 +23079,7 @@ in
 
   robustirc-bridge = callPackage ../servers/irc/robustirc-bridge { };
 
-  sddm = libsForQt5.callPackage ../applications/display-managers/sddm { };
+  sddm = libsForQt512.callPackage ../applications/display-managers/sddm { };
 
   skrooge = libsForQt514.callPackage ../applications/office/skrooge {};
 
