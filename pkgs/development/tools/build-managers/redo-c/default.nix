da382f565704b32baf271340cda7a1c11525625a
Christian Kögler <christian.koegler@esg.de>
2020-09-08 21:49:57 +0200

    redo-c: Add patch which fixes implicit job lease detection

--- pkgs/development/tools/build-managers/redo-c/default.nix
+++ pkgs/development/tools/build-managers/redo-c/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv, fetchFromGitHub, fetchpatch }:
 stdenv.mkDerivation rec {
   pname = "redo-c";
   version = "0.2";
@@ -9,6 +9,14 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     sha256 = "11wc2sgw1ssdm83cjdc6ndnp1bv5mzhbw7njw47mk7ri1ic1x51b";
   };
+
+  patches = [
+    # Fix non implicit job lease detection. Remove with the next release.
+    (fetchpatch {
+      url = "https://github.com/leahneukirchen/redo-c/commit/3a3f0056f357b4fa2b9f725725806d370395ff39.patch";
+      sha256 = "039hnlwr9gmngsfjvc5fz3zclq32656j8g0njsvl9r3bfxq3ydj5";
+    })
+  ];
 
   postPatch = ''
     cp '${./Makefile}' Makefile
