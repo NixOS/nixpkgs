36e42116249d8e30a45eebba345b7caa486980c7
Tom Hall <tahall256@protonmail.ch>
2020-09-08 20:35:24 +0100

    keysmith: replace hardcoded qt prefixes

--- pkgs/tools/security/keysmith/default.nix
+++ pkgs/tools/security/keysmith/default.nix
@@ -30,8 +30,8 @@ mkDerivation rec {
   postInstall = ''
     mv $out/bin/org.kde.keysmith $out/bin/.org.kde.keysmith-wrapped
     makeWrapper $out/bin/.org.kde.keysmith-wrapped $out/bin/org.kde.keysmith \
-      --set QML2_IMPORT_PATH "${lib.getLib kirigami2}/lib/qt-5.12.7/qml:${lib.getBin qtquickcontrols2}/lib/qt-5.12.7/qml:${lib.getBin qtdeclarative}/lib/qt-5.12.7/qml:${qtgraphicaleffects}/lib/qt-5.12.7/qml" \
-      --set QT_PLUGIN_PATH "${lib.getBin qtbase}/lib/qt-5.12.7/plugins"
+      --set QML2_IMPORT_PATH "${lib.getLib kirigami2}/${qtbase.qtQmlPrefix}:${lib.getBin qtquickcontrols2}/${qtbase.qtQmlPrefix}:${lib.getBin qtdeclarative}/${qtbase.qtQmlPrefix}:${qtgraphicaleffects}/${qtbase.qtQmlPrefix}" \
+      --set QT_PLUGIN_PATH "${lib.getBin qtbase}/${qtbase.qtPluginPrefix}"
     ln -s $out/bin/org.kde.keysmith $out/bin/keysmith
   '';
 
