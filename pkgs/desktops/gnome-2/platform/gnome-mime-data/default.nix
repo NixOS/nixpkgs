9e043f4944e0c84c2457b4463ef11e473ad49649
Christian Kampka <christian@kampka.net>
2020-09-08 11:23:47 +0200

    gnome-2.gnome_mime_data: Move intltool to nativeBuildInputs

--- pkgs/desktops/gnome-2/platform/gnome-mime-data/default.nix
+++ pkgs/desktops/gnome-2/platform/gnome-mime-data/default.nix
@@ -6,5 +6,5 @@ stdenv.mkDerivation {
     url = "mirror://gnome/sources/gnome-mime-data/2.18/gnome-mime-data-2.18.0.tar.bz2";
     sha256 = "1mvg8glb2a40yilmyabmb7fkbzlqd3i3d31kbkabqnq86xdnn69p";
   };
-  buildInputs = [ intltool ];
+  nativeBuildInputs = [ intltool ];
 }
