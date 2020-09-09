2a3ecde9711d02ef81a9224b009b78cf1b966c39
Gabriel Ebner <gebner@gebner.org>
2020-09-09 20:57:32 +0200

    ncftp: do not override $TMPDIR

--- pkgs/tools/networking/ncftp/default.nix
+++ pkgs/tools/networking/ncftp/default.nix
@@ -14,6 +14,8 @@ stdenv.mkDerivation rec {
   enableParallelBuilding = true;
 
   preConfigure = ''
+    find -name Makefile.in | xargs sed -i '/^TMPDIR=/d'
+
     find . -name '*.sh' -or -name '*.in' -or -name '*.c' -or -name configure | xargs sed -i \
       -e 's@/bin/ls@${coreutils}/bin/ls@g' \
       -e 's@/bin/rm@${coreutils}/bin/rm@g'
