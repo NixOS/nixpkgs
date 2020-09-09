836323032dca5799c8a093fe8e813f098d1bb0f6
Tim Steinbach <tim@nequissimus.com>
2020-09-09 12:23:59 -0400

    linux: 5.9-rc3 -> 5.9-rc4
    (cherry picked from commit ff268eb32530f490baadc5da87353777d8e3e4ce)

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.9-rc3";
+  version = "5.9-rc4";
   extraMeta.branch = "5.9";
 
   # modDirVersion needs to be x.y.z, will always add .0
@@ -11,7 +11,7 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "1byckdxy0y5i3lgw3f3n7b4r3v1xb4g1z3ffiq3hp4ga4nf5xzw3";
+    sha256 = "080qd0dmxs0zmkpcx62vnf719wgkk64a3012r4vsaiymzhddxy8k";
   };
 
   # Should the testing kernels ever be built on Hydra?
