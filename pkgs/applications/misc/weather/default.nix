2d0f4462b3519ce722faed2762bffceebc8e3c32
Matthias Beyer <mail@beyermatthias.de>
2020-09-10 11:01:02 +0200

    weather: 2.4 -> 2.4.1
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/applications/misc/weather/default.nix
+++ pkgs/applications/misc/weather/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, pythonPackages }:
 
 stdenv.mkDerivation rec {
-    version = "2.4";
+    version = "2.4.1";
     pname = "weather";
 
     src = fetchurl {
         url = "http://fungi.yuggoth.org/weather/src/${pname}-${version}.tar.xz";
-        sha256 = "084f0am0s1h6y71wgja9acaaxp0mq6k74b6ad4b5wpk2znwv0rzz";
+        sha256 = "0nf680dl7a2vlgavdhj6ljq8a7lkhvr6zghkpzad53vmilxsndys";
     };
 
     nativeBuildInputs = [ pythonPackages.wrapPython ];
