2f88ac4ba9982297dd9d8ff8c3076ad9cbeefd03
Tim Steinbach <tim@nequissimus.com>
2020-09-10 09:23:23 -0400

    linux: 5.8.7 -> 5.8.8

--- pkgs/os-specific/linux/kernel/linux-5.8.nix
+++ pkgs/os-specific/linux/kernel/linux-5.8.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.8.7";
+  version = "5.8.8";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1zhpzlhl2ykna2nc70m72wlgyv1pkvkpfssb4k8p5pwlkh1ga2vv";
+    sha256 = "0xm901zvvrwsb9k88la6pb65nybi43bygiyz1z68njwsx6ripxik";
   };
 } // (args.argsOverride or {}))
