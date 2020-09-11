7754b0c6430b8e638097a6a7f50ae7a7099fbc6e
freezeboy <freezeboy@users.noreply.github.com>
2020-09-11 14:08:39 +0200

    python3Packages.fluent-logger: relax version for msgpack
    Also add imports tests as we can't do more using the pip release

--- pkgs/development/python-modules/fluent-logger/default.nix
+++ pkgs/development/python-modules/fluent-logger/default.nix
@@ -9,10 +9,21 @@ buildPythonPackage rec {
     sha256 = "a7d47eae4d2a11c8cb0df10ae3d034d95b0b8cef9d060e59e7519ad1f82ffa73";
   };
 
+  prePatch = ''
+    substituteInPlace setup.py \
+      --replace "msgpack<1.0.0" "msgpack"
+  '';
+
   propagatedBuildInputs = [ msgpack ];
 
   # Tests fail because absent in package
   doCheck = false;
+  pythonImportsCheck = [
+    "fluent"
+    "fluent.event"
+    "fluent.handler"
+    "fluent.sender"
+  ];
 
   meta = with lib; {
     description = "A structured logger for Fluentd (Python)";
