2f8166ee19dda06a5b50fdcf75c2b7aff2b542d5
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-09-11 02:33:11 +0200

    pythonPackages.supervisor: drop glibc-2.31 patch
    A fix was integrated into the 4.2.1 release.

--- pkgs/development/python-modules/supervisor/default.nix
+++ pkgs/development/python-modules/supervisor/default.nix
@@ -14,13 +14,6 @@ buildPythonPackage rec {
     sha256 = "c479c875853e9c013d1fa73e529fd2165ff1ecaecc7e82810ba57e7362ae984d";
   };
 
-  patches = [
-    # SOMAXCONN limit of glibc-2.31 has been increased from 128 to 4096:
-    # * https://sourceware.org/git/?p=glibc.git;a=commit;h=96958e2700f5b4f4d1183a0606b2b9848a53ea44
-    # * https://github.com/Supervisor/supervisor/issues/1346
-    ./glibc-2.31.patch
-  ];
-
   # wants to write to /tmp/foo which is likely already owned by another
   # nixbld user on hydra
   doCheck = !stdenv.isDarwin;
