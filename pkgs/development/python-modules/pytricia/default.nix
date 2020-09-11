d686ba754268c715f57bc340e798a63daab8b0da
Mark K Gardner <mkg@vt.edu>
2020-09-11 14:08:17 -0400

    pytricia: unstable-2019-01-16 -> 1.0.2

--- pkgs/development/python-modules/pytricia/default.nix
+++ pkgs/development/python-modules/pytricia/default.nix
@@ -1,21 +1,19 @@
-{ stdenv
+{ lib
 , buildPythonPackage
-, fetchFromGitHub
+, fetchPypi
 }:
 
 buildPythonPackage rec {
   pname = "pytricia";
-  version = "unstable-2019-01-16";
+  version = "1.0.2";
 
-  src = fetchFromGitHub {
-    owner = "jsommers";
-    repo = pname;
-    rev = "4ba88f68c3125f789ca8cd1cfae156e1464bde87";
-    sha256 = "0qp5774xkm700g35k5c76pck8pdzqlyzbaqgrz76a1yh67s2ri8h";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "05cvq7r3mv7xq72ziwr3vl8s4a6246vdhmhs39jvg8amrvyhlv3z";
   };
 
-  meta = with stdenv.lib; {
-    description = "A library for fast IP address lookup in Python";
+  meta = with lib; {
+    description = "An efficient IP address storage and lookup module for Python";
     homepage = "https://github.com/jsommers/pytricia";
     license = with licenses; [ lgpl3Plus ];
     maintainers = with maintainers; [ mkg ];
