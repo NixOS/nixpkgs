92a2366ec2266e0eb8d3466b1ea7f6b5ea0f880c
Peter Simons <simons@cryp.to>
2020-09-11 20:19:21 +0200

    dhall-nix: update override for the new dhall version

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -923,7 +923,7 @@ self: super: {
   dhall-json = generateOptparseApplicativeCompletions ["dhall-to-json" "dhall-to-yaml"] super.dhall-json;
   dhall-nix = generateOptparseApplicativeCompletion "dhall-to-nix" (
     super.dhall-nix.overrideScope (self: super: {
-      dhall = super.dhall_1_34_0;
+      dhall = super.dhall_1_35_0;
       repline = self.repline_0_4_0_0;
       haskeline = self.haskeline_0_8_1_0;
     }));
