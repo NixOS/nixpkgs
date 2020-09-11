677def70a35e85ec72c48aecbc2f76e80b320cda
freezeboy <freezeboy@users.noreply.github.com>
2020-09-11 12:18:58 +0200

    lp_solve: fix buildCommand some binaries moved

--- pkgs/applications/science/math/lp_solve/default.nix
+++ pkgs/applications/science/math/lp_solve/default.nix
@@ -23,13 +23,13 @@ stdenv.mkDerivation rec {
     cd lp_solve*/lpsolve55
     bash ccc
     mkdir -pv $out/lib
-    cp -v bin/*/* $out/lib
+    find bin -type f -exec cp -v "{}" $out/lib \;
     )
     (
     cd lp_solve*/lp_solve
     bash ccc
     mkdir -pv $out/bin
-    cp -v bin/*/* $out/bin
+    find bin -type f -exec cp -v "{}" $out/bin \;
     )
     (
     mkdir -pv $out/include
