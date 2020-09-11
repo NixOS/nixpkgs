3f84e96d25346699a7f9f0b0de063b31e6051989
Robert Schütz <dev@schuetz-co.de>
2020-09-12 00:52:12 +0200

    python.pkgs.zulip: init at 0.7.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7710,6 +7710,8 @@ in {
 
   zstd = callPackage ../development/python-modules/zstd { inherit (pkgs) zstd pkgconfig; };
 
+  zulip = callPackage ../development/python-modules/zulip { };
+
   zxcvbn = callPackage ../development/python-modules/zxcvbn { };
 
 });
