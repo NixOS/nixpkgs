c4b869efc5e2e82a794d72d26d2fb35f03ece6e9
AndersonTorres <torres.anderson.85@protonmail.com>
2020-09-11 14:41:55 -0300

    openmsx: 0.15.0 -> 16.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27664,7 +27664,7 @@ in
   snes9x-gtk = callPackage ../misc/emulators/snes9x-gtk { };
 
   openmsx = callPackage ../misc/emulators/openmsx {
-    python = python27;
+    python = python3;
   };
 
   higan = callPackage ../misc/emulators/higan {
