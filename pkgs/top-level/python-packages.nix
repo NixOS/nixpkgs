a92a3a3e043dbcd9991e777931e30e3864a69784
Robert Scott <code@humanleg.org.uk>
2020-09-12 14:30:53 +0100

    pythonPackages.korean-lunar-calendar: init at 0.2.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3190,6 +3190,8 @@ in {
 
   konfig = callPackage ../development/python-modules/konfig { };
 
+  korean-lunar-calendar = callPackage ../development/python-modules/korean-lunar-calendar { };
+
   kubernetes = callPackage ../development/python-modules/kubernetes { };
 
   labelbox = callPackage ../development/python-modules/labelbox { };
