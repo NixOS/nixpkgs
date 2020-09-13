ef7a680719592b06ffb8134ac0f571686f7f46c9
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-09-13 15:10:41 +0200

    python3Packages.brother: 0.1.14 -> 0.1.17

--- pkgs/development/python-modules/brother/default.nix
+++ pkgs/development/python-modules/brother/default.nix
@@ -4,14 +4,14 @@
 
 buildPythonPackage rec {
   pname = "brother";
-  version = "0.1.14";
-  disabled = pythonOlder "3.6";
+  version = "0.1.17";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "bieniu";
     repo = pname;
     rev = version;
-    sha256 = "11pkr30bxrzgbz6bi42dyhav6qhr7rz9fb6a13297g7wa77jn4r4";
+    sha256 = "03gjcpbq8rwnjzplgwhwr8wb7a1zh940dr6iwnq9srklqzzj691m";
   };
 
   propagatedBuildInputs = [
