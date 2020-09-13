95cd8c8d7be15606a0ec418dc313726ff170d9ee
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-09-12 22:30:41 -0400

    ubootRaspberryPi4*: init at 2020.10

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19398,6 +19398,8 @@ in
     ubootRaspberryPi2
     ubootRaspberryPi3_32bit
     ubootRaspberryPi3_64bit
+    ubootRaspberryPi4_32bit
+    ubootRaspberryPi4_64bit
     ubootRaspberryPiZero
     ubootRock64
     ubootRockPi4
