da6e88c281dc22fc346ac82c03c7296b4509a174
Robert Scott <code@humanleg.org.uk>
2020-09-13 12:44:47 +0100

    pythonPackages.httpx: fix tests
    test suite contained a literal timestamp which expired, fixed upstream

--- pkgs/development/python-modules/httpx/default.nix
+++ pkgs/development/python-modules/httpx/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
+, fetchpatch
 , certifi
 , chardet
 , h11
@@ -30,6 +31,14 @@ buildPythonPackage rec {
     rev = version;
     sha256 = "08b6k5g8car3bic90aw4ysb2zvsa5nm8qk3hk4dgamllnnxzl5br";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "fix-cookie-test-timestamp.patch";
+      url = "https://github.com/encode/httpx/pull/1270.patch";
+      sha256 = "1hgrynac6226sgnyzmsr1nr15rn49gbfmk4c2kx3dwkbh6vr7jpd";
+    })
+  ];
 
   propagatedBuildInputs = [
     certifi
