259a2eb4c158ded1b2db7f65995c2e60e0265615
Tethys Svensson <freaken@freaken.dk>
2020-09-13 18:45:23 +0200

    zathura: symlinkJoin all of plugins (#96630)
    * zathura: symlinkJoin all of plugins
    
    * zathura: Add TethysSvensson as a maintainer
    
    Co-authored-by: Matthieu Coudron <mattator@gmail.com>

--- pkgs/applications/misc/zathura/wrapper.nix
+++ pkgs/applications/misc/zathura/wrapper.nix
@@ -1,21 +1,16 @@
 { symlinkJoin, lib, makeWrapper, zathura_core, file, plugins ? [] }:
-
-let
-  pluginsPath = lib.makeSearchPath "lib/zathura" plugins;
-
-in symlinkJoin {
+symlinkJoin {
   name = "zathura-with-plugins-${zathura_core.version}";
 
-  paths = with zathura_core; [ man dev out ];
+  paths = with zathura_core; [ man dev out ] ++ plugins;
 
-  inherit plugins;
 
   buildInputs = [ makeWrapper ];
 
   postBuild = ''
     makeWrapper ${zathura_core.bin}/bin/zathura $out/bin/zathura \
       --prefix PATH ":" "${lib.makeBinPath [ file ]}" \
-      --add-flags --plugins-dir=${pluginsPath}
+      --add-flags --plugins-dir="$out/lib/zathura"
   '';
 
   meta = with lib; {
@@ -29,6 +24,6 @@ in symlinkJoin {
     '';
     license = licenses.zlib;
     platforms = platforms.unix;
-    maintainers = with maintainers; [ smironov globin ];
+    maintainers = with maintainers; [ smironov globin TethysSvensson ];
   };
 }
