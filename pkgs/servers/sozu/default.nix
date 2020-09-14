7cc977e1823b58283bcf0980ca4a46eb5d4e453b
happysalada <raphael@megzari.com>
2020-09-14 13:50:20 +0900

    sozu: 0.11.46 -> 0.11.50; fix Darwin build

--- pkgs/servers/sozu/default.nix
+++ pkgs/servers/sozu/default.nix
@@ -1,17 +1,19 @@
-{ stdenv, rustPlatform, fetchFromGitHub }:
+{ stdenv, rustPlatform, fetchFromGitHub, Security }:
 
 rustPlatform.buildRustPackage rec {
     pname = "sozu";
-    version = "0.11.46";
+    version = "0.11.50";
 
     src = fetchFromGitHub {
         owner = "sozu-proxy";
         repo = pname;
         rev = version;
-        sha256 = "0anng5qvdx9plxs9qqr5wmjjz0gx5113jq28xwbxaaklvd4ni7cm";
+        sha256 = "1srg2b8vwc4vp07kg4fizqj1rbm9hvf6hj1mjdh6yvb9cpbw3jz7";
     };
 
-    cargoSha256 = "19c2s9h4jk9pff72wdqw384mvrf40d8x4sx7qysnpb4hayq2ijh3";
+    cargoSha256 = "5WOigCiQZQ5DaTd15vV8pUh8Xl3UIe9yLG1ptUtY+iA=";
+
+    buildInputs = stdenv.lib.optionals stdenv.isDarwin [ Security ];
 
     meta = with stdenv.lib; {
         description = "Open Source HTTP Reverse Proxy built in Rust for Immutable Infrastructures";
