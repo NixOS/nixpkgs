4c230a64b4f97d96ac6a7cfea1dd2afb29c3965c
Matt Votava <mvnetbiz@gmail.com>
2020-09-14 12:16:49 -0700

    pythonPackages.bcrypt: use 3.1.x for python older than 3.6

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -778,7 +778,10 @@ in {
 
   bcdoc = callPackage ../development/python-modules/bcdoc { };
 
-  bcrypt = callPackage ../development/python-modules/bcrypt { };
+  bcrypt = if pythonOlder "3.6" then
+    callPackage ../development/python-modules/bcrypt/3_1.nix { }
+  else
+    callPackage ../development/python-modules/bcrypt { };
 
   beaker = callPackage ../development/python-modules/beaker { };
 
