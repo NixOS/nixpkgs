31a0f7be66aa51bc36a0669014df3482c21a4c57
ryneeverett <ryneeverett@gmail.com>
2020-09-14 21:34:01 +0000

    chromium-extensions.decentraleyes: init at 2.0.14

--- pkgs/misc/chromium-extensions/default.nix
+++ pkgs/misc/chromium-extensions/default.nix
@@ -2,6 +2,8 @@
 let
   inherit (import ./build-chromium-extension.nix { inherit stdenv pkgs; }) buildChromiumExtension;
 in {
+  decentraleyes = pkgs.callPackage ./extensions/decentraleyes { inherit buildChromiumExtension; };
+
   https-everywhere = pkgs.callPackage ./extensions/https-everywhere { inherit buildChromiumExtension; };
 
   ublock = pkgs.callPackage ./extensions/ublock { inherit buildChromiumExtension; };
