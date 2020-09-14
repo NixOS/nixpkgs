7699e31abb3a2334b7dae8931575899a4d0f66d1
Tim Steinbach <tim@nequissimus.com>
2020-09-14 08:28:10 -0400

    oh-my-zsh: 2020-09-09 -> 2020-09-12

--- pkgs/shells/zsh/oh-my-zsh/default.nix
+++ pkgs/shells/zsh/oh-my-zsh/default.nix
@@ -4,15 +4,15 @@
 { stdenv, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
-  version = "2020-09-09";
+  version = "2020-09-12";
   pname = "oh-my-zsh";
-  rev = "3667f94538c24ebaa9abd46e797a2610f3b67c5e";
+  rev = "8a93c89bda7e244ed5d32d5b01bd5f72ab695fbf";
 
   src = fetchFromGitHub {
     inherit rev;
     owner = "ohmyzsh";
     repo = "ohmyzsh";
-    sha256 = "1pa4240rhxj5gi5m63xi5wm8b91pzpzsmqp72mqd9b394bgsd3wy";
+    sha256 = "0azm4xp5y6w41jbl4342rmqc5hdrg0rz7pmwlilps44k8xdvml7p";
   };
 
   installPhase = ''
