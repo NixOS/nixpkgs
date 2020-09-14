2e03ee8391254caa44e4440e32096c6f4b1522c6
Tim Steinbach <tim@nequissimus.com>
2020-09-14 09:10:36 -0400

    awscli: 1.18.128 -> 1.18.137

--- pkgs/tools/admin/awscli/default.nix
+++ pkgs/tools/admin/awscli/default.nix
@@ -19,11 +19,11 @@ let
 
 in with py.pkgs; buildPythonApplication rec {
   pname = "awscli";
-  version = "1.18.128"; # N.B: if you change this, change botocore to a matching version too
+  version = "1.18.137"; # N.B: if you change this, change botocore to a matching version too
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "c146c1634edbedc32564c536afcaaaeedc3ecc5aa9db3cf64fe5870c1c1a2a32";
+    sha256 = "0g745lvmi30di3bbpbca2bkqqzk7g6l3ssmbpi8pvgy0wrfhij69";
   };
 
   postPatch = ''
