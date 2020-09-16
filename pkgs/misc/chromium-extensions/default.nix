ae13a1e9ca52fe122bb301362828369a9ec076e4
Daniel Fullmer <danielrf12@gmail.com>
2020-09-15 21:15:25 -0700

    chromium-extensions.bypass-paywalls: init at 1.7.5

--- pkgs/misc/chromium-extensions/default.nix
+++ pkgs/misc/chromium-extensions/default.nix
@@ -2,6 +2,8 @@
 let
   inherit (import ./build-chromium-extension.nix { inherit stdenv pkgs; }) buildChromiumExtension;
 in {
+  bypass-paywalls = pkgs.callPackage ./extensions/bypass-paywalls { inherit buildChromiumExtension; };
+
   decentraleyes = pkgs.callPackage ./extensions/decentraleyes { inherit buildChromiumExtension; };
 
   https-everywhere = pkgs.callPackage ./extensions/https-everywhere { inherit buildChromiumExtension; };
