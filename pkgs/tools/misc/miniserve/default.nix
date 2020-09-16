673e5788b5f672b7711db3420d7d1212b821ac4c
zowoq <59103226+zowoq@users.noreply.github.com>
2020-09-17 06:30:21 +1000

    miniserve: 0.8.0 -> 0.9.0
    https://github.com/svenstaro/miniserve/releases/tag/v0.9.0

--- pkgs/tools/misc/miniserve/default.nix
+++ pkgs/tools/misc/miniserve/default.nix
@@ -3,27 +3,26 @@
 , fetchFromGitHub
 , pkg-config
 , zlib
-, openssl
 , Security
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "miniserve";
-  version = "0.8.0";
+  version = "0.9.0";
 
   src = fetchFromGitHub {
     owner = "svenstaro";
     repo = "miniserve";
     rev = "v${version}";
-    sha256 = "1h4872jb0xz8yzs02q8wfvqrp20y7kdva5ka6bh6nq4jrnnky8zb";
+    sha256 = "1abmg2zk1qipqdl1yfj8ibm1w8n7fazxqccsg1gq4xzlhhfp3m2l";
   };
 
-  cargoSha256 = "1vq1rrav9r9z4y0v7hpn0fcq64slq4zrm2pybmnmb7h9nfxxyr6k";
+  cargoSha256 = "0l750067x8k92ngg32bb8mnbq09aj65sdnpzdhij9n1mh90rkck9";
 
   RUSTC_BOOTSTRAP = 1;
 
   nativeBuildInputs = [ pkg-config zlib ];
-  buildInputs = if stdenv.isDarwin then [ Security ] else [ openssl ];
+  buildInputs = stdenv.lib.optionals stdenv.isDarwin [ Security ];
 
   meta = with stdenv.lib; {
     description = "For when you really just want to serve some files over HTTP right now!";
@@ -31,5 +30,6 @@ rustPlatform.buildRustPackage rec {
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ nequissimus zowoq ];
     platforms = platforms.unix;
+    broken = stdenv.isDarwin; # https://github.com/NixOS/nixpkgs/pull/98181
   };
 }
