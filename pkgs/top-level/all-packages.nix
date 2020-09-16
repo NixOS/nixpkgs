d219ce3a8236146625c7b6ec284c90bf19cfb39d
ajs124 <git@ajs124.de>
2020-09-16 21:06:57 +0200

    sky: drop

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22177,10 +22177,6 @@ in
     };
   };
 
-  sky = callPackage ../applications/networking/instant-messengers/sky {
-    qt5 = qt514;
-  };
-
   smplayer = libsForQt5.callPackage ../applications/video/smplayer { };
 
   smtube = libsForQt514.callPackage ../applications/video/smtube {};
