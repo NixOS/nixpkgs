4336f9f7bbea908eff7eb44dc95a90f5b1023969
Manuel Bärenz <programming@manuelbaerenz.de>
2020-09-16 11:59:30 +0200

    tests/agda: Fix comment

--- nixos/tests/agda.nix
+++ nixos/tests/agda.nix
@@ -31,7 +31,7 @@ in
     machine.succeed('echo "import IO" > TestIO.agda')
     machine.succeed("agda -l standard-library -i . TestIO.agda")
 
-    # # Hello world
+    # Hello world
     machine.succeed(
         "cp ${hello-world} HelloWorld.agda"
     )
