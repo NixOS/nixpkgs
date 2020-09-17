a04432b01dedfceb901a374e2eb97b1c3dbf27ad
gnidorah <gnidorah@users.noreply.github.com>
2020-09-17 16:38:07 +0300

    phodav: 2.4 -> 2.5

--- pkgs/tools/networking/phodav/default.nix
+++ pkgs/tools/networking/phodav/default.nix
@@ -2,14 +2,14 @@
 , pkgconfig, libsoup, meson, ninja }:
 
 let
-  version = "2.4";
+  version = "2.5";
 in stdenv.mkDerivation rec {
   pname = "phodav";
   inherit version;
 
   src = fetchurl {
     url = "http://ftp.gnome.org/pub/GNOME/sources/phodav/${version}/${pname}-${version}.tar.xz";
-    sha256 = "1hxq8c5qfah3w7mxcyy3yhzdgswplll31a69p5mqdl04bsvw5pbx";
+    sha256 = "045rdzf8isqmzix12lkz6z073b5qvcqq6ad028advm5gf36skw3i";
   };
 
   mesonFlags = [
