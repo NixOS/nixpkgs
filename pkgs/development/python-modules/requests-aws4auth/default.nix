b620f88aa361a8f6f474f04a71faeb6486d2e991
Maxine E. Aubrey <maxeaubrey@gmail.com>
2020-09-17 02:30:23 +0200

    requests-aws4auth: unbreak on python 3.x, cleanup
    postPatch was attemptting to sed in a file that no longer exists
    tests are still broken on python 3.x
    
    additionally misc cleanup and corrected homepage to be current

--- pkgs/development/python-modules/requests-aws4auth/default.nix
+++ pkgs/development/python-modules/requests-aws4auth/default.nix
@@ -1,5 +1,9 @@
-{ lib, buildPythonPackage, fetchPypi, isPy3k, requests }:
-with lib;
+{ stdenv
+, buildPythonPackage
+, fetchPypi
+, isPy3k
+, requests }:
+
 buildPythonPackage rec {
   pname = "requests-aws4auth";
   version = "1.0";
@@ -9,20 +13,15 @@ buildPythonPackage rec {
     sha256 = "2950f6ff686b5a452a269076d990e4821d959b61cfac319c3d3c6daaa5db55ce";
   };
 
-  postPatch = optionalString isPy3k ''
-    sed "s/path_encoding_style/'path_encoding_style'/" \
-      -i requests_aws4auth/service_parameters.py
-  '';
-
   propagatedBuildInputs = [ requests ];
 
   # The test fail on Python >= 3 because of module import errors.
   doCheck = !isPy3k;
 
-  meta = {
-    description = "Amazon Web Services version 4 authentication for the Python Requests library.";
-    homepage = "https://github.com/sam-washington/requests-aws4auth";
+  meta = with stdenv.lib; {
+    description = "Amazon Web Services version 4 authentication for the Python Requests library";
+    homepage = "https://github.com/tedder/requests-aws4auth";
     license = licenses.mit;
-    maintainers = [ maintainers.basvandijk ];
+    maintainers = with maintainers; [ basvandijk ];
   };
 }
