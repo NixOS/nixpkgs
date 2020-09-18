ae10b6e64e90e9fd089d9deccf47d128fdca7fbd
OPNA2608 <christoph.neidahl@gmail.com>
2020-09-18 06:38:26 +0200

    lyrebird: init at 1.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21572,6 +21572,8 @@ in
 
   lynx = callPackage ../applications/networking/browsers/lynx { };
 
+  lyrebird = callPackage ../applications/audio/lyrebird { };
+
   lyx = libsForQt5.callPackage ../applications/misc/lyx { };
 
   mac = callPackage ../development/libraries/mac { };
