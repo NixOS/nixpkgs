27000908ee03df4ddc55b22f1fb7fa93865a941e
Jakub Kądziołka <kuba@kadziolka.net>
2020-09-18 14:53:58 +0200

    sameboy: init at 0.13.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22972,6 +22972,8 @@ in
 
   sakura = callPackage ../applications/misc/sakura { };
 
+  sameboy = callPackage ../misc/emulators/sameboy { };
+
   sayonara = libsForQt514.callPackage ../applications/audio/sayonara { };
 
   sbagen = callPackage ../applications/misc/sbagen { };
