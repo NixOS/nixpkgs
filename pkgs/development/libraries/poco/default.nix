8f41eda98b64b9cc46da12ce52bac9cc28d91dc4
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-09-18 15:55:44 -0400

    poco: add patch to use GNUInstallDirs

--- pkgs/development/libraries/poco/default.nix
+++ pkgs/development/libraries/poco/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, cmake, pkg-config, zlib, pcre, expat, sqlite, openssl, unixODBC, libmysqlclient }:
+{ lib, stdenv, fetchurl, fetchpatch, cmake, pkg-config, zlib, pcre, expat, sqlite, openssl, unixODBC, libmysqlclient }:
 
 stdenv.mkDerivation rec {
   pname = "poco";
@@ -9,6 +9,25 @@ stdenv.mkDerivation rec {
     url = "https://pocoproject.org/releases/${pname}-${version}/${pname}-${version}-all.tar.gz";
     sha256 = "1jilzh0h6ik5lr167nax7q6nrpzxl99p11pkl202ig06pgh32nbz";
   };
+
+  patches = [
+    # Use GNUInstallDirs (https://github.com/pocoproject/poco/pull/3105)
+    (fetchpatch {
+      name = "use-gnuinstalldirs.patch";
+      url = "https://github.com/pocoproject/poco/commit/9e8f84dff4575f01be02e0b07364efd1561ce66c.patch";
+      sha256 = "1bj4i93gxr7pwx33bfyhg20ad4ak1rbxkrlpsgzk7rm6mh0mld26";
+      # Files not included in release tarball
+      excludes = [
+        "Encodings/Compiler/CMakeLists.txt"
+        "PocoDoc/CMakeLists.txt"
+        "NetSSL_Win/CMakeLists.txt"
+        "PDF/CMakeLists.txt"
+        "SevenZip/CMakeLists.txt"
+        "ApacheConnector/CMakeLists.txt"
+        "CppParser/CMakeLists.txt"
+      ];
+    })
+  ];
 
   nativeBuildInputs = [ cmake pkg-config ];
 
