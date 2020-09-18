c2bc969732a1c1158b7f5a5c04a68979a2b56aa2
Charlotte Van Petegem <charlotte@vanpetegem.me>
2020-09-18 11:28:31 +0200

    maintainers: add charvp

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1445,6 +1445,12 @@
     githubId = 89596;
     name = "Florian Friesdorf";
   };
+  charvp = {
+    email = "nixpkgs@cvpetegem.be";
+    github = "charvp";
+    githubId = 42220376;
+    name = "Charlotte Van Petegem";
+  };
   chattered = {
     email = "me@philscotted.com";
     name = "Phil Scott";
