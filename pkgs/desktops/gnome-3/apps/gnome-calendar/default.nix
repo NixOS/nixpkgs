0be93b1febf871cc6de4c3b4f9ef47974dd5d388
Piotr Bogdan <ppbogdan@gmail.com>
2020-09-18 16:04:20 +0100

    gnome3.gnome-calendar: 3.36.2 -> 3.38.0
    * reformat
    
    https://gitlab.gnome.org/GNOME/gnome-calendar/-/blob/3.38.0/NEWS
    
    Co-authored-by: WORLDofPEACE <worldofpeace@protonmail.ch>

--- pkgs/desktops/gnome-3/apps/gnome-calendar/default.nix
+++ pkgs/desktops/gnome-3/apps/gnome-calendar/default.nix
@@ -1,17 +1,44 @@
-{ stdenv, fetchurl, meson, ninja, pkgconfig, wrapGAppsHook, libdazzle, libgweather, geoclue2, geocode-glib, python3
-, gettext, libxml2, gnome3, gtk3, evolution-data-server, libsoup
-, glib, gnome-online-accounts, gsettings-desktop-schemas, libhandy }:
+{ stdenv
+, fetchurl
+, fetchpatch
+, meson
+, ninja
+, pkgconfig
+, wrapGAppsHook
+, libdazzle
+, libgweather
+, geoclue2
+, geocode-glib
+, python3
+, gettext
+, libxml2
+, gnome3
+, gtk3
+, evolution-data-server
+, libsoup
+, glib
+, gnome-online-accounts
+, gsettings-desktop-schemas
+, libhandy
+, adwaita-icon-theme
+}:
 
-let
+stdenv.mkDerivation rec {
   pname = "gnome-calendar";
-  version = "3.36.2";
-in stdenv.mkDerivation rec {
-  name = "${pname}-${version}";
+  version = "3.38.0";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "07sc1kn65dzxsxpv0vl5dj1a5awljjsfl9jldrg0hnjmq12m7c6h";
+    url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
+    sha256 = "0cs7ggj88n8sira5vzsijmzl3fmflic48lbis24r1d9blx944s63";
   };
+
+  patches = [
+    # Port to libhandy-1
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/gnome-calendar/-/commit/8be361b6ce8f0f8053e1609decbdbdc164ec8448.patch";
+      sha256 = "Ue0pWwcbYyCZPHPPoR0dXW5n948/AZ3wVDMTIZDOnyE=";
+    })
+  ];
 
   passthru = {
     updateScript = gnome3.updateScript {
@@ -20,10 +47,29 @@ in stdenv.mkDerivation rec {
     };
   };
 
-  nativeBuildInputs = [ meson ninja pkgconfig gettext libxml2 wrapGAppsHook python3 ];
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkgconfig
+    gettext
+    libxml2
+    wrapGAppsHook
+    python3
+  ];
+
   buildInputs = [
-    gtk3 evolution-data-server libsoup glib gnome-online-accounts libdazzle libgweather geoclue2 geocode-glib
-    gsettings-desktop-schemas gnome3.adwaita-icon-theme libhandy
+    gtk3
+    evolution-data-server
+    libsoup
+    glib
+    gnome-online-accounts
+    libdazzle
+    libgweather
+    geoclue2
+    geocode-glib
+    gsettings-desktop-schemas
+    adwaita-icon-theme
+    libhandy
   ];
 
   postPatch = ''
