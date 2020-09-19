0ca7bf8c8c40c426482e82625d19ac469ddc8fa0
Gabriel Ebner <gebner@gebner.org>
2020-09-19 14:20:46 +0200

    octave: use jdk8

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10080,15 +10080,18 @@ in
 
   octave = callPackage ../development/interpreters/octave {
     python = python3;
+    jdk = jdk8; # TODO: remove override https://github.com/NixOS/nixpkgs/pull/89731
   };
   octave-jit = callPackage ../development/interpreters/octave {
     python = python3;
     enableJIT = true;
+    jdk = jdk8; # TODO: remove override https://github.com/NixOS/nixpkgs/pull/89731
   };
   octaveFull = (lowPrio (libsForQt512.callPackage ../development/interpreters/octave {
     python = python3;
     enableQt = true;
     overridePlatforms = ["x86_64-linux" "x86_64-darwin"];
+    jdk = jdk8; # TODO: remove override https://github.com/NixOS/nixpkgs/pull/89731
   }));
 
   ocropus = callPackage ../applications/misc/ocropus { };
