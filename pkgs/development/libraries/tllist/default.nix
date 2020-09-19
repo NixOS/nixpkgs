634bddf5ee33d43e1d61fb2fe9127d24622a5c12
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-09-19 12:33:09 +0200

    tllist: 1.0.2 → 1.0.4

--- pkgs/development/libraries/tllist/default.nix
+++ pkgs/development/libraries/tllist/default.nix
@@ -2,23 +2,26 @@
 
 stdenv.mkDerivation rec {
   pname = "tllist";
-  version = "1.0.2";
+  version = "1.0.4";
 
   src = fetchgit {
     url = "https://codeberg.org/dnkl/tllist.git";
     rev = "${version}";
-    sha256 = "095wly66z9n2r6h318rackgl4g1w9l1vj96367ngcw7rpva9yppl";
+    sha256 = "1a26vwb7ll6mv3h8rbafsdx4vic1f286hiqn8s359sw8b7yjkvzs";
   };
 
   nativeBuildInputs = [
     meson ninja
   ];
 
+  doCheck = true;
+
   meta = with lib; {
     homepage = "https://codeberg.org/dnkl/tllist";
     description = "C header file only implementation of a typed linked list";
     maintainers = with maintainers; [ fionera ];
     license = licenses.mit;
-    platforms = with platforms; linux;
+    platforms = with platforms; unix;
+    changelog = "https://codeberg.org/dnkl/tllist/releases/tag/${version}";
   };
 }
