ff3baa2a56061d09e2eeeffa84fe18997b96f298
Robert Scott <code@humanleg.org.uk>
2020-09-19 19:21:33 +0100

    xmlbird: fix build with python 3.8
    also force use of gccStdenv to fix darwin build
    
    (cherry picked from commit 781b870c9167cc01c8783471f059b92238b751cb)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2537,7 +2537,7 @@ in
   biblatex-check = callPackage ../tools/typesetting/biblatex-check { };
 
   birdfont = callPackage ../tools/misc/birdfont { };
-  xmlbird = callPackage ../tools/misc/birdfont/xmlbird.nix { };
+  xmlbird = callPackage ../tools/misc/birdfont/xmlbird.nix { stdenv = gccStdenv; };
 
   blastem = callPackage ../misc/emulators/blastem {
     inherit (python27Packages) pillow;
