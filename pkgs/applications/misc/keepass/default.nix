dd88db826753e9b1ac97698a78cebfd6c959c915
joncojonathan <joncojonathan@gmail.com>
2020-09-19 10:13:56 +0100

    keepass: remove joncojonathan as maintainer

--- pkgs/applications/misc/keepass/default.nix
+++ pkgs/applications/misc/keepass/default.nix
@@ -87,7 +87,7 @@ with builtins; buildDotnetPackage rec {
 
   dynlibPath = stdenv.lib.makeLibraryPath [ gtk2 ];
 
-  postInstall = 
+  postInstall =
   let
     extractFDeskIcons = ./extractWinRscIconsToStdFreeDesktopDir.sh;
   in
@@ -111,7 +111,7 @@ with builtins; buildDotnetPackage rec {
   meta = {
     description = "GUI password manager with strong cryptography";
     homepage = "http://www.keepass.info/";
-    maintainers = with stdenv.lib.maintainers; [ amorsillo obadz joncojonathan jraygauthier ];
+    maintainers = with stdenv.lib.maintainers; [ amorsillo obadz jraygauthier ];
     platforms = with stdenv.lib.platforms; all;
     license = stdenv.lib.licenses.gpl2;
   };
