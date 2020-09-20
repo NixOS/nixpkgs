c87b4b2b0a0503f3ae2db6c808c4a7dba3058375
Doron Behar <doron.behar@gmail.com>
2020-09-20 17:24:43 +0300

    gusb: 0.3.3 -> 0.3.5

--- pkgs/development/libraries/gusb/default.nix
+++ pkgs/development/libraries/gusb/default.nix
@@ -1,20 +1,26 @@
 { stdenv, fetchurl, meson, ninja, pkgconfig, gettext, gobject-introspection
-, gtk-doc, docbook_xsl, docbook_xml_dtd_412, docbook_xml_dtd_44
+, gtk-doc, docbook_xsl, docbook_xml_dtd_412, docbook_xml_dtd_44, python3
 , glib, systemd, libusb1, vala, hwdata
 }:
+
+let
+  pythonEnv = python3.withPackages(ps: with ps; [
+    setuptools
+  ]);
+in
 stdenv.mkDerivation rec {
   pname = "gusb";
-  version = "0.3.3";
+  version = "0.3.5";
 
   outputs = [ "bin" "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "https://people.freedesktop.org/~hughsient/releases/libgusb-${version}.tar.xz";
-    sha256 = "14pbd0812151ga7jrpzi88fcrwkckx6m07ay84l7dzkxbdc44fgr";
+    sha256 = "1pv5ivbwxb9anq2j34i68r8fgs8nwsi4hmss7h9v1i3wk7300ajv";
   };
 
   nativeBuildInputs = [
-    meson ninja pkgconfig gettext
+    meson ninja pkgconfig gettext pythonEnv
     gtk-doc docbook_xsl docbook_xml_dtd_412 docbook_xml_dtd_44
     gobject-introspection vala
   ];
