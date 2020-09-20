7898b87fd3e13165896008eb1ceb9af91f24504d
Doron Behar <doron.behar@gmail.com>
2020-09-20 20:56:43 +0300

    libsoup: 2.70.0 -> 2.72.0
    It adds support for profiling using Sysprof but we do not support Sysprof on Darwin
    so we need to disable it there. Otherwise it breaks eval of Darwin package set
    through “sysprof ← libsoup ← inkscape” chain.

--- pkgs/development/libraries/libsoup/default.nix
+++ pkgs/development/libraries/libsoup/default.nix
@@ -1,14 +1,14 @@
-{ stdenv, fetchurl, glib, libxml2, meson, ninja, pkgconfig, gnome3
+{ stdenv, lib, fetchurl, glib, libxml2, meson, ninja, pkgconfig, gnome3, sysprof
 , gnomeSupport ? true, sqlite, glib-networking, gobject-introspection, vala
 , libpsl, python3, brotli }:
 
 stdenv.mkDerivation rec {
   pname = "libsoup";
-  version = "2.70.0";
+  version = "2.72.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0hjk9lgppc5435my0lyywbpmj7ib5vvcylwfin8ki97g9bvj1c2l";
+    sha256 = "11skbyw2pw32178q3h8pi7xqa41b2x4k6q4k9f75zxmh8s23y30p";
   };
 
   postPatch = ''
@@ -17,7 +17,14 @@ stdenv.mkDerivation rec {
 
   outputs = [ "out" "dev" ];
 
-  buildInputs = [ python3 sqlite libpsl brotli ];
+  buildInputs = [
+    python3
+    sqlite
+    libpsl
+    brotli
+  ] ++ lib.optionals stdenv.isLinux [
+    sysprof
+  ];
   nativeBuildInputs = [ meson ninja pkgconfig gobject-introspection vala glib ];
   propagatedBuildInputs = [ glib libxml2 ];
 
@@ -27,6 +34,8 @@ stdenv.mkDerivation rec {
     "-Dvapi=enabled"
     "-Dgnome=${if gnomeSupport then "true" else "false"}"
     "-Dntlm=disabled"
+  ] ++ lib.optionals (!stdenv.isLinux) [
+    "-Dsysprof=disabled"
   ];
 
   doCheck = false; # ERROR:../tests/socket-test.c:37:do_unconnected_socket_test: assertion failed (res == SOUP_STATUS_OK): (2 == 200)
@@ -41,7 +50,7 @@ stdenv.mkDerivation rec {
   meta = {
     description = "HTTP client/server library for GNOME";
     homepage = "https://wiki.gnome.org/Projects/libsoup";
-    license = stdenv.lib.licenses.gpl2;
+    license = stdenv.lib.licenses.lgpl2Plus;
     inherit (glib.meta) maintainers platforms;
   };
 }
