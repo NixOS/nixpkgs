22d7ac76e437eddb5f1560e02e99696993adf122
Austin Butler <austinabutler@gmail.com>
2020-09-20 18:36:59 -0700

    pythonPackages.mohawk: init at 1.1.0
    (cherry picked from commit 8be7347e2b7c3f0dd54413b613d782bfcb3741b6)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3705,6 +3705,8 @@ in {
 
   modestmaps = callPackage ../development/python-modules/modestmaps { };
 
+  mohawk = callPackage ../development/python-modules/mohawk { };
+
   moinmoin = callPackage ../development/python-modules/moinmoin
     { }; # Needed here because moinmoin is loaded as a Python library.
 
