44dc1ff0752d188726fb5015683e0356d8a3d80a
WORLDofPEACE <worldofpeace@protonmail.ch>
2020-09-20 20:53:32 -0400

    tracker_2: init at 2.3.4
    The following apps still need the old tracker:
    - photos
    - games
    - books
    - documents

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3357,6 +3357,8 @@ in
 
   tracker = callPackage ../development/libraries/tracker { };
 
+  tracker_2 = callPackage ../development/libraries/tracker/2.x { };
+
   tracker-miners = callPackage ../development/libraries/tracker-miners { };
 
   tracy = callPackage ../development/tools/tracy {
