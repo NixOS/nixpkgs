2ced276567a6bb40f5de0daa9804373070b7829d
Arnout Engelen <arnout@bzzt.net>
2020-09-22 14:26:58 +0200

    gem: 2020-03-26 -> 2020-09-22
    removing the need for a patch, as that PR has now been merged.

--- pkgs/applications/audio/pd-plugins/gem/default.nix
+++ pkgs/applications/audio/pd-plugins/gem/default.nix
@@ -15,26 +15,14 @@
 
 stdenv.mkDerivation rec {
   pname = "gem-unstable";
-  # The patch below applies to the latest release (v0.94), but then the build
-  # fails. I didn't track down what changed between that version and the
-  # current master that fixes the build on Nix
-  version = "2020-03-26";
+  version = "2020-09-22";
 
   src = fetchFromGitHub {
     owner = "umlaeute";
     repo = "Gem";
-    rev = "f38748d71bfca00e4d2b2f31d6c4e3759c03d599";
-    sha256 = "0bkky5fk0a836bapslrgzil272iq9y704y7hw254cfq5ffjd4qjy";
+    rev = "2edfde4f0587e72ef325e7f53681936dcc19655b";
+    sha256 = "0k5sq128wxi2qhaidspkw310pdgysxs47agv09pkjgvch2n4d5dq";
   };
-
-  patches = [
-    # Update autoconf OpenGL/GLU/GLUT detection scripts
-    # https://github.com/umlaeute/Gem/pull/251
-    (fetchpatch {
-      url = "https://github.com/umlaeute/Gem/commit/343a486c2b5c3427696f77aeabdff440e6590fc7.diff";
-      sha256 = "0gkzxv80rgg8lgp9av5qp6xng3ldhnbjz9d6r7ym784fw8yx41yj";
-    })
-  ];
 
   nativeBuildInputs = [
     autoreconfHook
