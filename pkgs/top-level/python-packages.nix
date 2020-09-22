e588d640718be8df8e9970ae1e502d77da3f1eb1
Evils <evils.devils@protonmail.com>
2020-09-22 19:17:42 +0200

    libsmu: init at 1.0.4
    with optional python module (pysmu)
      disabled because it fails to build due to a change since 1.0.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9533,6 +9533,11 @@ self: super: with self; {
 
   pyinfra = callPackage ../development/python-modules/pyinfra { };
 
+  pysmu = toPythonModule (pkgs.libsmu.override {
+    pythonSupport = true;
+    inherit python;
+  });
+
   pytibber = callPackage ../development/python-modules/pytibber { };
 
   pytile = callPackage ../development/python-modules/pytile { };
