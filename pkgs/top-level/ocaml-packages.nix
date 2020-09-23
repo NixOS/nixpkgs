891d861b11a304519f87430b6123585d810af15f
Vincent Laporte <Vincent.Laporte@gmail.com>
2020-09-23 08:02:11 +0200

    ocamlPackages.irmin_1: remove at 1.4.0

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -352,8 +352,6 @@ let
 
     ipaddr-sexp = callPackage ../development/ocaml-modules/ipaddr/sexp.nix { };
 
-    irmin_1 = callPackage ../development/ocaml-modules/irmin/1.4.nix { };
-
     iso8601 = callPackage ../development/ocaml-modules/iso8601 { };
 
     iter = callPackage ../development/ocaml-modules/iter { };
