36ad05e8d297beb6ace05c7beb7df3e003829425
Matt Wittmann <mcwitt@gmail.com>
2020-09-22 17:08:22 -0700

    vscode-extensions.haskell.haskell: init at 1.1.0 (#98251)

--- pkgs/misc/vscode-extensions/default.nix
+++ pkgs/misc/vscode-extensions/default.nix
@@ -60,6 +60,18 @@ in
     };
   };
 
+  haskell.haskell = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "haskell";
+      publisher = "haskell";
+      version = "1.1.0";
+      sha256 = "1wg06lyk0qn9jd6gi007sg7v0z9z8gwq7x2449d4ihs9n3w5l0gb";
+    };
+    meta = with stdenv.lib; {
+      license = licenses.mit;
+    };
+  };
+
   james-yu.latex-workshop = buildVscodeMarketplaceExtension {
     mktplcRef = {
       name = "latex-workshop";
