f43c02c7ff649a0594937b5b99c4276d9d89b61f
Shea Levy <shea@shealevy.com>
2020-09-22 20:14:14 -0400

    system76-firmware: Init at 1.0.17

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18276,6 +18276,8 @@ in
 
   nvme-cli = callPackage ../os-specific/linux/nvme-cli { };
 
+  system76-firmware = callPackage ../os-specific/linux/firmware/system76-firmware { };
+
   open-vm-tools = callPackage ../applications/virtualization/open-vm-tools { };
   open-vm-tools-headless = open-vm-tools.override { withX = false; };
 
