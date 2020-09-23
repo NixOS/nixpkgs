25155517b1238b925dd9207fd400681d0e8afb00
R. RyanTM <ryantm-bot@ryantm.com>
2020-09-23 09:49:17 +0000

    go-jira: 1.0.23 -> 1.0.24

--- pkgs/applications/misc/go-jira/default.nix
+++ pkgs/applications/misc/go-jira/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "go-jira";
-  version = "1.0.23";
+  version = "1.0.24";
 
   src = fetchFromGitHub {
     rev = "v${version}";
     owner = "go-jira";
     repo = "jira";
-    sha256 = "0qk5ifjxkqisvgv066rw8xj2zszc9mhc0by4338xjd7ng10jkk7b";
+    sha256 = "1qpimh39hsg75mb32hlvxmd7jj5b0cmhdkqz3dizfcnidllr3grd";
   };
 
   vendorSha256 = "18jwxnkv94lsxfv57ga519knxm077cc8chp5c992ipk58a04nv18";
