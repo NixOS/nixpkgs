19dbdc89fa8ee3577b73d59b6c01bf5e4f5f31e8
Johannes Schleifenbaum <johannes@js-webcoding.de>
2020-09-24 11:32:58 +0200

    dbeaver: 7.2.0 -> 7.2.1

--- pkgs/applications/misc/dbeaver/default.nix
+++ pkgs/applications/misc/dbeaver/default.nix
@@ -7,7 +7,7 @@
 
 stdenv.mkDerivation rec {
   pname = "dbeaver-ce";
-  version = "7.2.0";
+  version = "7.2.1";
 
   desktopItem = makeDesktopItem {
     name = "dbeaver";
@@ -30,7 +30,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://dbeaver.io/files/${version}/dbeaver-ce-${version}-linux.gtk.x86_64.tar.gz";
-    sha256 = "0zpxsdzhn5fsrlq04v5kvkrgf4dsj5zmpypj9awsd2mjcbp6yxd7";
+    sha256 = "0vdwbgff55aq5l67fczdvw7kn7kf0dsz183x5lz2v1sb2blsjvjj";
   };
 
   installPhase = ''
