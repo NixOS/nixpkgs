73133a6af02a65974f06a936615953784a20aecd
Andrew Fontaine <andrew@afontaine.ca>
2020-09-24 16:27:10 -0400

    plexamp: 3.1.1 -> 3.2.0
    Adds remote control support for other plex players, as well as other
    changes.
    
    Release notes: https://forums.plex.tv/t/plexamp-release-notes/221280/16

--- pkgs/applications/audio/plexamp/default.nix
+++ pkgs/applications/audio/plexamp/default.nix
@@ -2,12 +2,12 @@
 
 let
   pname = "plexamp";
-  version = "3.1.1";
+  version = "3.2.0";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-${version}.AppImage";
-    sha256 = "11hgcysa1x9yqvha6ri4vl1zk8gf8vhcpnh3j38wg9ncd7nz5k2v";
+    sha256 = "R1BhobnwoU7oJ7bNes8kH2neXqHlMPbRCNjcHyzUPqo=";
     name="${pname}-${version}.AppImage";
   };
 
