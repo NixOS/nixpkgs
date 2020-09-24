0cd4c321d42c232c891b403c640759c0b666da8b
R. RyanTM <ryantm-bot@ryantm.com>
2020-09-24 18:53:07 +0000

    bcftools: 1.10.2 -> 1.11

--- pkgs/applications/science/biology/bcftools/default.nix
+++ pkgs/applications/science/biology/bcftools/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "bcftools";
-  version = "1.10.2";
+  version = "1.11";
 
   src = fetchurl {
     url = "https://github.com/samtools/bcftools/releases/download/${version}/${pname}-${version}.tar.bz2";
-    sha256 = "0b2f6lqhxdlrvfjqxv7a4nzqj68c1j4avn16iqxwwm80kn302wzm";
+    sha256 = "0r508mp15pqzf8r1269kb4v5naw9zsvbwd3cz8s1yj7carsf9viw";
   };
 
   buildInputs = [ htslib zlib bzip2 lzma curl perl python ];
