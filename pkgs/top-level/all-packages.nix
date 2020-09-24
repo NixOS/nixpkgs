897d92c8546f6d01fbfc74e573a26f134c392cc6
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2020-09-24 03:46:12 +0300

    neovim-qt: 0.2.15 -> 0.2.16.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23820,7 +23820,7 @@ in
 
   neovim = wrapNeovim neovim-unwrapped { };
 
-  neovim-qt = libsForQt514.callPackage ../applications/editors/neovim/qt.nix { };
+  neovim-qt = libsForQt5.callPackage ../applications/editors/neovim/qt.nix { };
 
   olifant = callPackage ../applications/misc/olifant { };
 
