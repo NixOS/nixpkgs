50847e0b174a879ca09dbf31bb8cc4729b82327c
Astro <astro@spaceboyz.net>
2020-09-24 19:24:09 +0200

    wargus: init at 2.4.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27429,6 +27429,8 @@ in
     libpng = libpng12;
   };
 
+  wargus = callPackage ../games/wargus { };
+
   warmux = callPackage ../games/warmux { };
 
   warsow-engine = callPackage ../games/warsow/engine.nix { };
