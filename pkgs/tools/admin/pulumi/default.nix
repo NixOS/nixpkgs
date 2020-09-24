3c721398f3a22ecaa0fc9fcfff3e0a88e9974d1e
Geoffrey Huntley <ghuntley@ghuntley.com>
2020-09-24 14:02:12 +0000

    pulumi: 2.6.1 -> 2.10.2

--- pkgs/tools/admin/pulumi/default.nix
+++ pkgs/tools/admin/pulumi/default.nix
@@ -29,6 +29,7 @@ in stdenv.mkDerivation {
     license = with licenses; [ asl20 ];
     platforms = builtins.attrNames data.pulumiPkgs;
     maintainers = with maintainers; [
+      ghuntley
       peterromfeldhk
       jlesquembre
     ];
