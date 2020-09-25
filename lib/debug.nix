580ca00137239772d4ece04dbd9b9db6b6bc3a47
Silvan Mosberger <contact@infinisil.com>
2020-09-25 16:52:19 +0200

    lib/debug: Switch traceSeqN to new toPretty features

--- lib/debug.nix
+++ lib/debug.nix
@@ -93,19 +93,12 @@ rec {
        traceSeqN 2 { a.b.c = 3; } null
        trace: { a = { b = {…}; }; }
        => null
-   */
-  traceSeqN = depth: x: y: with lib;
-    let snip = v: if      isList  v then noQuotes "[…]" v
-                  else if isAttrs v then noQuotes "{…}" v
-                  else v;
-        noQuotes = str: v: { __pretty = const str; val = v; };
-        modify = n: fn: v: if (n == 0) then fn v
-                      else if isList  v then map (modify (n - 1) fn) v
-                      else if isAttrs v then mapAttrs
-                        (const (modify (n - 1) fn)) v
-                      else v;
-    in trace (generators.toPretty { allowPrettyValues = true; }
-               (modify depth snip x)) y;
+  */
+  traceSeqN = depth: x: y: lib.generators.toPretty {
+    nextState = builtins.trace;
+    initialState = y;
+    recursionLimit = depth;
+  } x;
 
   /* A combination of `traceVal` and `traceSeq` that applies a
      provided function to the value to be traced after `deepSeq`ing
