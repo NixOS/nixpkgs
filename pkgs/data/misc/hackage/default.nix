fe170f2ae670df74c1d35856097dd04969876361
Peter Simons <simons@cryp.to>
2020-09-25 21:10:19 +0200

    all-cabal-hashes: update to Hackage at 2020-09-25T08:31:15Z

--- pkgs/data/misc/hackage/default.nix
+++ pkgs/data/misc/hackage/default.nix
@@ -1,6 +1,6 @@
 { fetchurl }:
 
 fetchurl {
-  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/edb0920a8dbbd592d67a781d0b905728515ab623.tar.gz";
-  sha256 = "08yvpwzw7c3xw3w970ysykj44vglqfiq057kx0axk81s68v83rcy";
+  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/513c2f50baa5338bbe40a93cb328e1fe973df09c.tar.gz";
+  sha256 = "1j5lch66x82ysqfq97jq5sm0f58cdm0vjvy4wpkcm1akgnkkycq3";
 }
