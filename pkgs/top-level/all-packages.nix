8a3e3d1546c5c110fed789cbeb40fbd14fca864a
ajs124 <git@ajs124.de>
2020-09-25 18:15:58 +0200

    thc-ipv6: init at 3.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7353,6 +7353,8 @@ in
 
   thc-hydra = callPackage ../tools/security/thc-hydra { };
 
+  thc-ipv6 = callPackage ../tools/security/thc-ipv6 { };
+
   theharvester = callPackage ../tools/security/theharvester { };
 
   inherit (nodePackages) thelounge;
