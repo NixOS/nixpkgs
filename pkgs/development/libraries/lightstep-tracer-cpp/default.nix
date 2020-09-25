65d2cff4c34eab122c94b4de231329d947e96287
R. RyanTM <ryantm-bot@ryantm.com>
2020-09-25 21:13:51 +0000

    lightstep-tracer-cpp: 0.13.0 -> 0.14.0

--- pkgs/development/libraries/lightstep-tracer-cpp/default.nix
+++ pkgs/development/libraries/lightstep-tracer-cpp/default.nix
@@ -9,13 +9,13 @@ assert enableGrpc -> c-ares != null;
 
 stdenv.mkDerivation rec {
   pname = "lightstep-tracer-cpp";
-  version = "0.13.0";
+  version = "0.14.0";
 
   src = fetchFromGitHub {
     owner = "lightstep";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0d7z9isf0n8s63mvp3j75949w0yv7jsva29i62lq8yxbym688fxb";
+    sha256 = "1xr11dm94qpbx3nxb7si7zy7hzg2akj01pyxkd8pzzbvmkmic16j";
   };
 
   nativeBuildInputs = [
