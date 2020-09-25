9e8f8c2cf8d8c4aa0e30ec8598aca3a3acf9dd3b
Doron Behar <doron.behar@gmail.com>
2020-09-25 18:32:20 +0300

    all-packages.nix: Fix a mis-indented `};`
    And thereby fix a confusion for parenthesis based Vim folding.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22385,7 +22385,7 @@ in
 
   imapfilter = callPackage ../applications/networking/mailreaders/imapfilter.nix {
     lua = lua5;
- };
+  };
 
   maxlib = callPackage ../applications/audio/pd-plugins/maxlib { };
 
