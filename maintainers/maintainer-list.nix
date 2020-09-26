40efae06a7f90fb418b2b8d88518507213c72435
Hugo Lageneste <hugo.lageneste@visium.ch>
2020-09-26 18:18:22 +0200

    maintainers: add hugolgst

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3497,6 +3497,12 @@
     githubId = 39689;
     name = "Hugo Tavares Reis";
   };
+  hugolgst = {
+    email = "hugo.lageneste@pm.me";
+    github = "hugolgst";
+    githubId = 15371828;
+    name = "Hugo Lageneste";
+  };
   hyphon81 = {
     email = "zero812n@gmail.com";
     github = "hyphon81";
