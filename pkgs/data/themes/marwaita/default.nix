f277e9a90afa307d9629ebf23e30d768340c63f6
José Romildo Malaquias <malaquias@gmail.com>
2020-09-26 09:24:43 -0300

    marwaita: 7.6.1 -> 7.7

--- pkgs/data/themes/marwaita/default.nix
+++ pkgs/data/themes/marwaita/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "marwaita";
-  version = "7.6.1";
+  version = "7.7";
 
   src = fetchFromGitHub {
     owner = "darkomarko42";
     repo = pname;
     rev = version;
-    sha256 = "1n7flwrngwh6gmh72j40apf8qk52162m93hsfhgxzrivkhg37zi0";
+    sha256 = "09r9ggngskazddhcm9c2n0cjc4qs9215vyri4i02cbp1jl82kwvg";
   };
 
   buildInputs = [
