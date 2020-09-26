812490a33afc4358fd9cbdaee30aee8da394cbb8
Ingolf Wagner <contact@ingolf-wagner.de>
2020-09-26 20:15:57 +0200

    terranix: init at 2.3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27622,6 +27622,8 @@ in
 
   terragrunt = callPackage ../applications/networking/cluster/terragrunt {};
 
+  terranix = callPackage ../applications/networking/cluster/terranix {};
+
   tilt = callPackage ../applications/networking/cluster/tilt {};
 
   timeular = callPackage ../applications/office/timeular {};
