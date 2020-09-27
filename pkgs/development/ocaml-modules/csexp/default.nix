917036878d96948b59cf7c46503c15ff34393a0d
Hongchang Wu <wuhc85@gmail.com>
2020-09-27 11:33:13 -0400

    ocamlPackages.csexp: fix minimum OCaml version

--- pkgs/development/ocaml-modules/csexp/default.nix
+++ pkgs/development/ocaml-modules/csexp/default.nix
@@ -1,19 +1,17 @@
-{ lib, fetchurl, buildDunePackage }:
+{ lib, fetchurl, buildDunePackage, result }:
 
 buildDunePackage rec {
   pname = "csexp";
   version = "1.3.2";
 
-  minimumOCamlVersion = "4.08";
+  minimumOCamlVersion = "4.02.3";
 
   src = fetchurl {
     url = "https://github.com/ocaml-dune/csexp/releases/download/${version}/csexp-${version}.tbz";
     sha256 = "0jhwrxfjb0x31xj4g4b89fzw34sq19j0rq2hs2zyh1vz4xxl47zj";
   };
 
-  postPatch = ''
-    substituteInPlace src/csexp.ml --replace Result.result Result.t
-  '';
+  propagatedBuildInputs = [ result ];
 
   meta = with lib; {
     homepage = "https://github.com/ocaml-dune/csexp";
