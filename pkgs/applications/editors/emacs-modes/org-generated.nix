5c915aa5f808af413a711fb4b7535b0f94555ece
Jappie Klooster <jappieklooster@hotmail.com>
2020-09-27 16:11:28 +0200

    emacsPackages.org-packages: 2020-09-27

--- pkgs/applications/editors/emacs-modes/org-generated.nix
+++ pkgs/applications/editors/emacs-modes/org-generated.nix
@@ -4,10 +4,10 @@
       elpaBuild {
         pname = "org";
         ename = "org";
-        version = "20200817";
+        version = "20200921";
         src = fetchurl {
-          url = "https://orgmode.org/elpa/org-20200817.tar";
-          sha256 = "159hch9zls3apxq11c5rjpmci1avyl7q3cgsrqxwgnzy8c61104d";
+          url = "https://orgmode.org/elpa/org-20200921.tar";
+          sha256 = "0qwa375j11d2rx6z18sr6s4qja96k78rv9r2y04ksn103gr9dd4l";
         };
         packageRequires = [];
         meta = {
@@ -19,10 +19,10 @@
       elpaBuild {
         pname = "org-plus-contrib";
         ename = "org-plus-contrib";
-        version = "20200817";
+        version = "20200921";
         src = fetchurl {
-          url = "https://orgmode.org/elpa/org-plus-contrib-20200817.tar";
-          sha256 = "0n3fhcxjsk2w78p7djna4nlppa7ypjxzpq3r5dmzc8jpl71mipba";
+          url = "https://orgmode.org/elpa/org-plus-contrib-20200921.tar";
+          sha256 = "1chxm7w8ls2qmrzvv80m3pmi5x5gybppfzpvgxlankz5rj34vgd3";
         };
         packageRequires = [];
         meta = {
