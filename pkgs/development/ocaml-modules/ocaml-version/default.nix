a0dde021d6a97ecc6313e665c0b8c385967a70c0
Vincent Laporte <Vincent.Laporte@gmail.com>
2020-09-27 17:12:13 +0200

    ocamlPackages.ocaml-version: 2.3.0 → 3.0.0

--- pkgs/development/ocaml-modules/ocaml-version/default.nix
+++ pkgs/development/ocaml-modules/ocaml-version/default.nix
@@ -1,16 +1,16 @@
-{ lib, fetchurl, buildDunePackage, result }:
+{ lib, fetchurl, buildDunePackage }:
 
 buildDunePackage rec {
 
   pname = "ocaml-version";
-  version = "2.3.0";
+  version = "3.0.0";
+
+  minimumOCamlVersion = "4.07";
 
   src = fetchurl {
     url = "https://github.com/ocurrent/ocaml-version/releases/download/v${version}/ocaml-version-v${version}.tbz";
-    sha256 = "0c711lifl35xila9k0rvhijy9zm3shd37q3jgw7xf01hn1swg0hn";
+    sha256 = "15vk8sh50p3f2mbv8z7mqnx76cffri36f2krp25zkkwix8jg7ci4";
   };
-
-  propagatedBuildInputs = [ result ];
 
   meta = {
     description = "Manipulate, parse and generate OCaml compiler version strings";
