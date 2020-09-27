61616535b9462902d2a6f19a0899f3509b7acbf8
Peter Simons <simons@cryp.to>
2020-09-27 17:05:35 +0200

    mlt: add libexif, gtk2, fftw, libebur128, opencv4, and SDL2 dependencies
    This enables more optional features. I'm not sure what the difference is
    between mlt and mlt-qt5. I suppose both derivations should be kept in sync?

--- pkgs/development/libraries/mlt/default.nix
+++ pkgs/development/libraries/mlt/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchFromGitHub, makeWrapper
-, SDL, ffmpeg, frei0r, libjack2, libdv, libsamplerate
-, libvorbis, libxml2, movit, pkgconfig, sox
-, gtk2, genericUpdater, common-updater-scripts
+, SDL, ffmpeg, frei0r, libjack2, libdv, libsamplerate, libexif
+, libvorbis, libxml2, movit, pkgconfig, sox, fftw, opencv4, SDL2
+, gtk2, genericUpdater, common-updater-scripts, libebur128
 }:
 
 stdenv.mkDerivation rec {
@@ -17,8 +17,8 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     SDL ffmpeg frei0r libjack2 libdv libsamplerate libvorbis libxml2
-    makeWrapper movit pkgconfig sox
-    gtk2
+    makeWrapper movit pkgconfig sox libexif gtk2 fftw libebur128
+    opencv4 SDL2
   ];
 
   # Mostly taken from:
