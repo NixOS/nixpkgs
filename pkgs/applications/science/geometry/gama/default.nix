b17a97e68fc43e4c6c16c1807e8966d0dca03f46
Lancelot SIX <lsix@lancelotsix.com>
2020-09-27 13:24:57 +0100

    gama: 2.09 -> 2.10
    See https://lists.gnu.org/archive/html/info-gnu/2020-09/msg00008.html
    for release announcement.

--- pkgs/applications/science/geometry/gama/default.nix
+++ pkgs/applications/science/geometry/gama/default.nix
@@ -1,18 +1,18 @@
-{ stdenv, fetchurl, lib, expat, octave, libxml2, texinfo }:
+{ stdenv, fetchurl, lib, expat, octave, libxml2, texinfo, zip }:
 stdenv.mkDerivation rec {
   pname = "gama";
-  version = "2.09";
+  version = "2.10";
 
   src = fetchurl {
     url = "mirror://gnu/${pname}/${pname}-${version}.tar.gz";
-    sha256 = "0c1b28frl6109arj09v4zr1xs859krn8871mkvis517g5pb55dc9";
+    sha256 = "14im3ahh849rildvs4qsk009jywf9w84vcmh7w44ajmlwcw7xiys";
   };
 
   buildInputs = [ expat ];
 
   nativeBuildInputs = [ texinfo ];
 
-  checkInputs = [ octave libxml2 ];
+  checkInputs = [ octave libxml2 zip ];
   doCheck = true;
 
   meta = with lib ; {
