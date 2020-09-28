10232e4d865d9a2b212836b99f24a796a3adf6f4
AndersonTorres <torres.anderson.85@protonmail.com>
2020-09-28 03:10:13 -0300

    Netsurf libnslog: 0.1.2 -> 0.1.3

--- pkgs/applications/networking/browsers/netsurf/libnslog.nix
+++ pkgs/applications/networking/browsers/netsurf/libnslog.nix
@@ -3,20 +3,17 @@
 }:
 
 stdenv.mkDerivation rec {
-
-  name = "netsurf-${libname}-${version}";
+  pname = "netsurf-${libname}";
   libname = "libnslog";
-  version = "0.1.2";
+  version = "0.1.3";
 
   src = fetchurl {
     url = "http://download.netsurf-browser.org/libs/releases/${libname}-${version}-src.tar.gz";
-    sha256 = "1ggs6xvxp8fbg5w8pifalipm458ygr9ab6j2yvj8fnnmxwvdh4jd";
+    sha256 = "sha256-/JjcqdfvpnCWRwpdlsAjFG4lv97AjA23RmHHtNsEU9A=";
   };
 
   nativeBuildInputs = [ pkgconfig bison flex ];
-  buildInputs = [
-    buildsystem
-  ];
+  buildInputs = [ buildsystem ];
 
   makeFlags = [
     "PREFIX=$(out)"
@@ -24,10 +21,10 @@ stdenv.mkDerivation rec {
   ];
 
   meta = with stdenv.lib; {
-    homepage = "http://www.netsurf-browser.org/";
+    homepage = "https://www.netsurf-browser.org/";
     description = "NetSurf Parametric Logging Library";
-    license = licenses.mit;
-    maintainers = [ maintainers.samueldr ];
+    license = licenses.isc;
+    maintainers = [ maintainers.samueldr maintainers.AndersonTorres ];
     platforms = platforms.linux;
   };
 }
