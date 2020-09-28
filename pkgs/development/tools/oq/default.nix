892712e26052114ec46b786ce69f405d44d20ce2
Robert Scott <code@humanleg.org.uk>
2020-09-28 22:50:30 +0100

    oq: fix build
    this patch is actually included in the already-released oq 1.1.2, but we
    can't upgrade to that because it requires crystal 0.35, which we don't
    seem to have yet
    
    enable for darwin

--- pkgs/development/tools/oq/default.nix
+++ pkgs/development/tools/oq/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchFromGitHub, crystal, jq, libxml2, makeWrapper }:
+{ lib, fetchFromGitHub, crystal, jq, libxml2, makeWrapper, fetchpatch }:
 
 crystal.buildCrystalPackage rec {
   pname = "oq";
@@ -10,6 +10,15 @@ crystal.buildCrystalPackage rec {
     rev = "v${version}";
     sha256 = "1zg4kxpfi3sap4cwp42zg46j5dv0nf926qdqm7k22ncm6jdrgpgw";
   };
+
+  patches = [
+    (fetchpatch {
+        # remove once we have upgraded to oq 1.1.2+
+        name = "yaml-test-leniency.patch";
+        url = "https://github.com/Blacksmoke16/oq/commit/93ed2fe50c9ce3fd8d35427e007790ddaaafce60.patch";
+        sha256 = "1iyz0c0w0ykz268bkrlqwvh1jnnrja0mqip6y89sbpa14lp0l37n";
+    })
+  ];
 
   nativeBuildInputs = [ makeWrapper ];
   buildInputs = [ jq libxml2 ];
@@ -32,6 +41,6 @@ crystal.buildCrystalPackage rec {
     homepage = "https://blacksmoke16.github.io/oq/";
     license = licenses.mit;
     maintainers = with maintainers; [ filalex77 ];
-    platforms = platforms.linux;
+    platforms = platforms.unix;
   };
 }
