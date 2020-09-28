2a19824ee1d9a393231a136809ea5e241305ceb6
AndersonTorres <torres.anderson.85@protonmail.com>
2020-09-28 03:48:46 -0300

    Netsurf libnsfb: 0.2.1 -> 0.2.2

--- pkgs/applications/networking/browsers/netsurf/default.nix
+++ pkgs/applications/networking/browsers/netsurf/default.nix
@@ -1,11 +1,27 @@
 { lib, pkgs }:
 
 lib.makeScope pkgs.newScope (self: with self; {
+  # ui can be: gtk2, gtk3, sixel, framebuffer. Note that console display (sixel)
+  # requires a terminal that supports `sixel` capabilities, such as mlterm
+  # or xterm -ti 340
+  ui = "gtk3";
+  uilib =
+    if ui == "gtk2" ||
+       ui == "gtk3" ||
+       ui == "framebuffer" then ui
+    else if ui == "sixel" then "framebuffer"
+    else null; # Never will happen
+  SDL =
+    if ui == "sixel" then pkgs.SDL_sixel
+    else if ui == "framebuffer" then pkgs.SDL
+    else null;
+
   buildsystem    = callPackage ./buildsystem.nix { };
   libcss         = callPackage ./libcss.nix { };
   libdom         = callPackage ./libdom.nix { };
   libhubbub      = callPackage ./libhubbub.nix { };
   libnsbmp       = callPackage ./libnsbmp.nix { };
+  libnsfb        = callPackage ./libnsfb.nix { };
   libnsgif       = callPackage ./libnsgif.nix { };
   libnslog       = callPackage ./libnslog.nix { };
   libnspsl       = callPackage ./libnspsl.nix { };
