3bbc72ba00f987ade07ef55271c4934e857fe364
Tim Steinbach <tim@nequissimus.com>
2020-09-28 13:01:54 -0400

    linux: 5.8.11 -> 5.8.12

--- pkgs/os-specific/linux/kernel/linux-5.8.nix
+++ pkgs/os-specific/linux/kernel/linux-5.8.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.8.11";
+  version = "5.8.12";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0jy0yrrixzql9dlk9305w98pja2pm6ijrdbai326595pnk740n9y";
+    sha256 = "0lla7zqan5cv5mp591f4ii3blijk3j0mm48y6hcz3irzwsjmmzq2";
   };
 } // (args.argsOverride or {}))
