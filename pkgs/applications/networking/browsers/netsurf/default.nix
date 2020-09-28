b06a3b1fcbe65f2ede386524d1db7d9b48117f11
AndersonTorres <torres.anderson.85@protonmail.com>
2020-09-28 03:02:31 -0300

    Netsurf libnsbmp: 0.1.6 -> 0.1.6

--- pkgs/applications/networking/browsers/netsurf/default.nix
+++ pkgs/applications/networking/browsers/netsurf/default.nix
@@ -2,5 +2,6 @@
 
 lib.makeScope pkgs.newScope (self: with self; {
   buildsystem    = callPackage ./buildsystem.nix { };
+  libnsbmp       = callPackage ./libnsbmp.nix { };
   libparserutils = callPackage ./libparserutils.nix { };
 })
