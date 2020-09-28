d1e5d5012fdad05935662ef47865ad956db4ed53
AndersonTorres <torres.anderson.85@protonmail.com>
2020-09-28 03:10:13 -0300

    Netsurf libnslog: 0.1.2 -> 0.1.3

--- pkgs/applications/networking/browsers/netsurf/default.nix
+++ pkgs/applications/networking/browsers/netsurf/default.nix
@@ -4,5 +4,6 @@ lib.makeScope pkgs.newScope (self: with self; {
   buildsystem    = callPackage ./buildsystem.nix { };
   libnsbmp       = callPackage ./libnsbmp.nix { };
   libnsgif       = callPackage ./libnsgif.nix { };
+  libnslog       = callPackage ./libnslog.nix { };
   libparserutils = callPackage ./libparserutils.nix { };
 })
