285e4cffdc0cb1cca67404ab2b35f6931a9627f5
Philip Nelson <me@pnelson.ca>
2020-09-28 08:30:56 +0800

    nextdns: 1.8.3 -> 1.8.6

--- pkgs/applications/networking/nextdns/default.nix
+++ pkgs/applications/networking/nextdns/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "nextdns";
-  version = "1.8.3";
+  version = "1.8.6";
 
   src = fetchFromGitHub {
     owner = "nextdns";
     repo = "nextdns";
     rev = "v${version}";
-    sha256 = "1bl6ky258hnai4v7d0gskn6nf5sdsc3xlwl959iwzqhpp0j3q9xg";
+    sha256 = "0n71wy0x4r46kjhq013d3rry55lr253ilm8c676w4z0fkw7dwk49";
   };
 
   vendorSha256 = "09whpzsn16znyrknfm5zlhla253r69j6d751czza4c83m4r36swj";
