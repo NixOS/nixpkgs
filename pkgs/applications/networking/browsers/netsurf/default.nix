ed204dc61bf393683c15286c312e4b0ec1ef770a
AndersonTorres <torres.anderson.85@protonmail.com>
2020-09-28 03:43:38 -0300

    Netsurf libdom: 0.4.0 -> 0.4.1

--- pkgs/applications/networking/browsers/netsurf/default.nix
+++ pkgs/applications/networking/browsers/netsurf/default.nix
@@ -3,6 +3,7 @@
 lib.makeScope pkgs.newScope (self: with self; {
   buildsystem    = callPackage ./buildsystem.nix { };
   libcss         = callPackage ./libcss.nix { };
+  libdom         = callPackage ./libdom.nix { };
   libhubbub      = callPackage ./libhubbub.nix { };
   libnsbmp       = callPackage ./libnsbmp.nix { };
   libnsgif       = callPackage ./libnsgif.nix { };
