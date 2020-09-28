c3c216b83398b51489553b30b36ee9dbafd18889
Jonathan Ringer <jonringer117@gmail.com>
2020-09-28 10:14:40 -0700

    python3Packages.azure-synapse-spark: 0.2.0 -> 0.3.0

--- pkgs/development/python-modules/azure-synapse-spark/default.nix
+++ pkgs/development/python-modules/azure-synapse-spark/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "azure-synapse-spark";
-  version = "0.2.0";
+  version = "0.3.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1qijqp6llshqas422lnqvpv45iv99n7f13v86znql40y3jp5n3ir";
+    sha256 = "fcfe559e30a2e159f07e371af28d18dc635dc55174fb3457be7210ce8e7e7559";
     extension = "zip";
   };
 
