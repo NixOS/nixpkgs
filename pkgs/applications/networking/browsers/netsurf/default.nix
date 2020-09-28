5c893edde22b1789e7d1b325b0d9ce10639846a5
AndersonTorres <torres.anderson.85@protonmail.com>
2020-09-28 03:05:18 -0300

    Netsurf libnsgif: 0.2.1 -> 0.2.1

--- pkgs/applications/networking/browsers/netsurf/default.nix
+++ pkgs/applications/networking/browsers/netsurf/default.nix
@@ -3,5 +3,6 @@
 lib.makeScope pkgs.newScope (self: with self; {
   buildsystem    = callPackage ./buildsystem.nix { };
   libnsbmp       = callPackage ./libnsbmp.nix { };
+  libnsgif       = callPackage ./libnsgif.nix { };
   libparserutils = callPackage ./libparserutils.nix { };
 })
