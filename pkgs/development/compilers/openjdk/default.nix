64a39be321ec5180a1dbd6e73349c55cd6fc641e
Sarunas Valaskevicius <rakatan@gmail.com>
2020-09-29 17:40:48 +0100

    openjdk14: add symlink for src.zip

--- pkgs/development/compilers/openjdk/default.nix
+++ pkgs/development/compilers/openjdk/default.nix
@@ -91,6 +91,7 @@ let
       mkdir -p $out/share
       ln -s $out/lib/openjdk/include $out/include
       ln -s $out/lib/openjdk/man $out/share/man
+      ln -s $out/lib/openjdk/lib/src.zip $out/lib/src.zip
 
       # jni.h expects jni_md.h to be in the header search path.
       ln -s $out/include/linux/*_md.h $out/include/
