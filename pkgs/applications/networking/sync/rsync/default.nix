79c01c835e4884dc6ffe0997a7c8738895a15cb5
Andrew Dunham <andrew@du.nham.ca>
2020-09-28 21:44:42 -0700

    rsync: 3.1.3 -> 3.2.3
    Add zstd, lz4, openssl and xxHash as optional dependencies, to support
    more compression formats.

--- pkgs/applications/networking/sync/rsync/default.nix
+++ pkgs/applications/networking/sync/rsync/default.nix
@@ -1,9 +1,17 @@
 { stdenv, fetchurl, perl, libiconv, zlib, popt
 , enableACLs ? !(stdenv.isDarwin || stdenv.isSunOS || stdenv.isFreeBSD), acl ? null
+, enableLZ4 ? true, lz4 ? null
+, enableOpenSSL ? true, openssl ? null
+, enableXXHash ? true, xxHash ? null
+, enableZstd ? true, zstd ? null
 , enableCopyDevicesPatch ? false
 }:
 
 assert enableACLs -> acl != null;
+assert enableLZ4 -> lz4 != null;
+assert enableOpenSSL -> openssl != null;
+assert enableXXHash -> xxHash != null;
+assert enableZstd -> zstd != null;
 
 let
   base = import ./base.nix { inherit stdenv fetchurl; };
@@ -18,7 +26,12 @@ stdenv.mkDerivation rec {
   srcs = [mainSrc] ++ stdenv.lib.optional enableCopyDevicesPatch patchesSrc;
   patches = stdenv.lib.optional enableCopyDevicesPatch "./patches/copy-devices.diff";
 
-  buildInputs = [libiconv zlib popt] ++ stdenv.lib.optional enableACLs acl;
+  buildInputs = [libiconv zlib popt]
+                ++ stdenv.lib.optional enableACLs acl
+                ++ stdenv.lib.optional enableZstd zstd
+                ++ stdenv.lib.optional enableLZ4 lz4
+                ++ stdenv.lib.optional enableOpenSSL openssl
+                ++ stdenv.lib.optional enableXXHash xxHash;
   nativeBuildInputs = [perl];
 
   configureFlags = ["--with-nobody-group=nogroup"];
