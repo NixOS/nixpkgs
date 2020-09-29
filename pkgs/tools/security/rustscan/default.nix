928b1bbbfcb4ae1e405daf1137f3248fa9c07be5
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-09-29 12:07:10 +0200

    rustscan: 1.9.0 -> 1.10.0

--- pkgs/tools/security/rustscan/default.nix
+++ pkgs/tools/security/rustscan/default.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "rustscan";
-  version = "1.9.0";
+  version = "1.10.0";
 
   src = fetchFromGitHub {
     owner = "RustScan";
     repo = pname;
     rev = "${version}";
-    sha256 = "0hj7k3yrd5vwxarn1kmxlhj5xlcg71w0vrzgnyrzbcppfxrbrj1m";
+    sha256 = "1x6vi3d250jhy75ybp8gm6cq9ncv0jig5c1v12r26raflhkh7fk3";
   };
 
-  cargoSha256 = "0cknnhzkk7jyi436c4hxif455xxdwg9pc4kw1q1wmgnqdw340xin";
+  cargoSha256 = "0q4p4pa1lh8kw5gmiim0zmmvs3l1kl319a3ji7gmj2fisha4319k";
 
   postPatch = ''
     substituteInPlace src/main.rs \
