9bc12bd2b5b47ab59df40e776e313548084b0487
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-09-29 15:02:37 +0200

    ocamlPackages.angstrom: 0.14.1 → 0.15.0

--- pkgs/development/ocaml-modules/angstrom/default.nix
+++ pkgs/development/ocaml-modules/angstrom/default.nix
@@ -1,8 +1,8 @@
-{ lib, fetchFromGitHub, buildDunePackage, ocaml, alcotest, result, bigstringaf }:
+{ lib, fetchFromGitHub, buildDunePackage, ocaml, alcotest, result, bigstringaf, ppx_let }:
 
 buildDunePackage rec {
   pname = "angstrom";
-  version = "0.14.1";
+  version = "0.15.0";
 
   minimumOCamlVersion = "4.04";
 
@@ -10,10 +10,10 @@ buildDunePackage rec {
     owner  = "inhabitedtype";
     repo   = pname;
     rev    = version;
-    sha256 = "1l69y0qspgi7kgrphyh7718hjb2sml1a9lljkp65bkqmmmi6ybly";
+    sha256 = "1hmrkdcdlkwy7rxhngf3cv3sa61cznnd9p5lmqhx20664gx2ibrh";
   };
 
-  checkInputs = [ alcotest ];
+  checkInputs = [ alcotest ppx_let ];
   propagatedBuildInputs = [ bigstringaf result ];
   doCheck = lib.versionAtLeast ocaml.version "4.05";
 
