b76670f139e140afe0f49016e0beb85b086add48
Sandro <sandro.jaeckel@gmail.com>
2020-09-29 15:28:12 +0200

    duf: 0.3.0 -> 0.3.1 (#99092)

--- pkgs/tools/misc/duf/default.nix
+++ pkgs/tools/misc/duf/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "duf";
-  version = "0.3.0";
+  version = "0.3.1";
 
   src = fetchFromGitHub {
     owner = "muesli";
     repo = "duf";
     rev = "v${version}";
-    sha256 = "08vmhcvqn9g86iqwk42bj0i09lmchhdgha1xaj1jw1ci4k7s9vrf";
+    sha256 = "1y27cc818099drccqg57nq4kcgl2zmg6akxv2k1c8rkz932q718f";
   };
 
   vendorSha256 = "1jmj46yami37r2wmiprpwyljcmj7dir9mcccx5is1jbiai6sx79i";
