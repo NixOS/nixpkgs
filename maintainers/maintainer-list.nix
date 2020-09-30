7f795ebfcc69ff2c1132a282040679e52107f9e0
Joe Lancaster <joe.a.lancas@gmail.com>
2020-09-30 17:47:07 +0100

    maintainers: add joelancaster

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4035,6 +4035,12 @@
     githubId = 60272884;
     name = "Jonathan Jeppener-Haltenhoff";
   };
+  joelancaster = {
+    email = "joe.a.lancas@gmail.com";
+    github = "joelancaster";
+    githubId = 16760945;
+    name = "Joe Lancaster";
+  };
   joelburget = {
     email = "joelburget@gmail.com";
     github = "joelburget";
