93d2df071290241802b2844053c0250a02dc19e2
ajs124 <git@ajs124.de>
2020-09-30 20:18:47 +0200

    nginxMainline: 1.19.2 -> 1.19.3

--- pkgs/servers/http/nginx/mainline.nix
+++ pkgs/servers/http/nginx/mainline.nix
@@ -1,6 +1,6 @@
 { callPackage, ... }@args:
 
 callPackage ./generic.nix args {
-  version = "1.19.2";
-  sha256 = "0wr4ss4gld7x717m4j3a6l6f7ijblrrd55y563lkwhvr7sqpn7vw";
+  version = "1.19.3";
+  sha256 = "1w4dkq7bl5gyix3x0ap3d9lndh7zyvc3mscl693d4ybql57vgrci";
 }
