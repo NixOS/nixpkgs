42798d48c99dfbf3c888be9d5a0cc2d550475bb4
Konrad Borowski <konrad@borowski.pw>
2020-10-01 12:26:56 +0200

    htop: 3.0.1 -> 3.0.2

--- pkgs/tools/system/htop/default.nix
+++ pkgs/tools/system/htop/default.nix
@@ -1,27 +1,27 @@
 { lib, fetchFromGitHub, stdenv, autoreconfHook
-, ncurses, IOKit, python3
+, ncurses, IOKit
 }:
 
 stdenv.mkDerivation rec {
   pname = "htop";
-  version = "3.0.1";
+  version = "3.0.2";
 
   src = fetchFromGitHub {
     owner = "htop-dev";
     repo = pname;
     rev = version;
-    sha256 = "0kjlphdvwwbj91kk91s4ksc954d3c2bznddzx2223jmb1bn9rcsa";
+    sha256 = "1qmqhbnc5yw4brd24yrp85k09770c1c00nl03mkv5pdz2bvqivk7";
   };
 
-  nativeBuildInputs = [ autoreconfHook python3 ];
+  nativeBuildInputs = [ autoreconfHook ];
 
   buildInputs = [ ncurses
   ] ++ lib.optionals stdenv.isDarwin [ IOKit ];
 
   meta = with stdenv.lib; {
     description = "An interactive process viewer for Linux";
-    homepage = "https://hisham.hm/htop/";
-    license = licenses.gpl2Plus;
+    homepage = "https://htop.dev";
+    license = licenses.gpl2Only;
     platforms = with platforms; linux ++ freebsd ++ openbsd ++ darwin;
     maintainers = with maintainers; [ rob relrod ];
   };
