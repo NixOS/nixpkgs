22a2ddce2600e3acdf59c09dcea912fda7a95970
Ben Siraphob <bensiraphob@gmail.com>
2020-10-02 02:19:01 +0700

    mozwire: 0.5.1 -> 0.5.2

--- pkgs/tools/networking/mozwire/default.nix
+++ pkgs/tools/networking/mozwire/default.nix
@@ -2,18 +2,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "MozWire";
-  version = "0.5.1";
+  version = "0.5.2";
 
   src = fetchFromGitHub {
     owner = "NilsIrl";
     repo = pname;
     rev = "v${version}";
-    sha256 = "07icgswmfvrvlm3mkm78pbbk6m2hb73j7ffj7r77whzb11v027v1";
+    sha256 = "0r3x8y1qxxkkac9lqgd0s339zbrm9zmir1f6qs0r5f0bw3ngzqc4";
   };
 
   buildInputs = stdenv.lib.optionals stdenv.isDarwin [ Security ];
-  
-  cargoSha256 = "10lhz7bdlfqj7wgsqnsxdfskms33pvj176fhf4kwci7nb8vgai4b";
+
+  cargoSha256 = "0hyps2wi4wmpllfams3p59jpccwa0ylgzxq7qdn6g6zvf0cajpc0";
 
   meta = with stdenv.lib; {
     description = "MozillaVPN configuration manager giving Linux, macOS users (among others), access to MozillaVPN";
