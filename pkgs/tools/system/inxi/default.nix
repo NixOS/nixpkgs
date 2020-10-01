e34abda24bb517ee6e4431927a9203b439a69ff5
Michael Weiss <dev.primeos@gmail.com>
2020-10-01 17:30:54 +0200

    inxi: 3.1.06-1 -> 3.1.07-1

--- pkgs/tools/system/inxi/default.nix
+++ pkgs/tools/system/inxi/default.nix
@@ -22,13 +22,13 @@ let
     ++ recommendedDisplayInformationPrograms;
 in stdenv.mkDerivation rec {
   pname = "inxi";
-  version = "3.1.06-1";
+  version = "3.1.07-1";
 
   src = fetchFromGitHub {
     owner = "smxi";
     repo = "inxi";
     rev = version;
-    sha256 = "11z90x7rwzm7krkcnmcs9f41i1d284vrj0aqk2xnvl3p79vx25f7";
+    sha256 = "0hs4m2vmfc6srscaz72r6zpkn6n7msgzlps376ks38gj1l103xfn";
   };
 
   buildInputs = [ perl makeWrapper ];
