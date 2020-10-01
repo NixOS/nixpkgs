bb105958b1444621862da9a253b81be51c6677be
elkowar <5300871+elkowar@users.noreply.github.com>
2020-10-01 20:04:06 +0200

    pipr: 0.0.12 -> 0.0.15

--- pkgs/applications/misc/pipr/default.nix
+++ pkgs/applications/misc/pipr/default.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "pipr";
-  version = "0.0.12";
+  version = "0.0.15";
 
   src = fetchFromGitHub {
     owner = "ElKowar";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0l7yvpc59mbzh87lngj6pj8w586fwa07829l5x9mmxnkf6srapmc";
+    sha256 = "1pbj198nqi27kavz9bm31a3h7h70by6l00046x09yf9n8qjpp01w";
   };
 
-  cargoSha256 = "1xzc1x5mjvj2jgdhwmjbdbqi8d7ln1ss7akn72d96kmy1wsj1qsa";
+  cargoSha256 = "1dcrafpf252dpjvimaibb93082nymb26wwbvr34zd6j7z285vach";
 
   nativeBuildInputs = [ makeWrapper ];
   postFixup = ''
