1edaaeabce34423e0111dda3a70f60b079b82346
José Romildo Malaquias <malaquias@gmail.com>
2020-10-01 13:13:00 -0300

    theme-jade1: 1.8 -> 1.9
    - Update to version 1.9
    - Include all the color variants
    - Use .tar.xz upstream source, as it includes all color variants,
      otherwise available only as git branches
    - Run preInstall and postInstall hooks
    - Change license to gpl3only

--- pkgs/data/themes/jade1/default.nix
+++ pkgs/data/themes/jade1/default.nix
@@ -1,27 +1,29 @@
-{ stdenv, fetchFromGitHub, gtk-engine-murrine }:
+{ stdenv, fetchurl, gtk-engine-murrine }:
 
 stdenv.mkDerivation rec {
   pname = "theme-jade1";
-  version = "1.8";
+  version = "1.9";
 
-  src = fetchFromGitHub {
-    owner = "madmaxms";
-    repo = "theme-jade-1";
-    rev = "v${version}";
-    sha256 = "1nvn2ghkdhilrsjpvl7r92aldvbs0nx0xc82jwrfaahi87dgfs8x";
+  src = fetchurl {
+    url = "https://github.com/madmaxms/theme-jade-1/releases/download/v${version}/jade-1-theme.tar.xz";
+    sha256 = "11fzd44ysy76iwyiwkshpf0vf6m3i3hcxyyihl0lg68nb3cv0g9y";
   };
+
+  sourceRoot = ".";
 
   propagatedUserEnvPkgs = [ gtk-engine-murrine ];
 
   installPhase = ''
+    runHook preInstall
     mkdir -p $out/share/themes
     cp -a Jade* $out/share/themes
+    runHook postInstall
   '';
 
   meta = with stdenv.lib; {
     description = "Based on Linux Mint theme with dark menus and more intensive green";
     homepage = "https://github.com/madmaxms/theme-jade-1";
-    license = with licenses; [ gpl3 ];
+    license = with licenses; [ gpl3Only ];
     platforms = platforms.linux;
     maintainers = [ maintainers.romildo ];
   };
