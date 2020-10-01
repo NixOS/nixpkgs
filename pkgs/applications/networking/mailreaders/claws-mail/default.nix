458d7160321abde8b1e7613f97239e220580ad33
Alvar Penning <post@0x21.biz>
2020-10-01 23:05:04 +0200

    claws-mail: 3.17.6 -> 3.17.7

--- pkgs/applications/networking/mailreaders/claws-mail/default.nix
+++ pkgs/applications/networking/mailreaders/claws-mail/default.nix
@@ -1,4 +1,4 @@
-{ config, fetchurl, fetchpatch, stdenv, wrapGAppsHook, autoreconfHook
+{ config, fetchurl, stdenv, wrapGAppsHook, autoreconfHook
 , curl, dbus, dbus-glib, enchant, gtk2, gnutls, gnupg, gpgme
 , libarchive, libcanberra-gtk2, libetpan, libnotify, libsoup, libxml2, networkmanager
 , openldap, perl, pkgconfig, poppler, python, shared-mime-info
@@ -30,25 +30,17 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   pname = "claws-mail";
-  version = "3.17.6";
+  version = "3.17.7";
 
   src = fetchurl {
-    url = "http://www.claws-mail.org/download.php?file=releases/claws-mail-${version}.tar.xz";
-    sha256 = "1s05qw0r0gqwvvkxvrrwbjkbi61dvilixiwrpgcq21qc9csc9r0m";
+    url = "https://www.claws-mail.org/download.php?file=releases/claws-mail-${version}.tar.xz";
+    sha256 = "1j6x09621wng0lavh53nwzh9vqjzpspl8kh5azh7kbihpi4ldfb0";
   };
 
   outputs = [ "out" "dev" ];
 
   patches = [
     ./mime.patch
-
-    # Backports a mitigation to the "mailto vulnerability". This patch is
-    # included in the next release and must therefore be removed.
-    (fetchpatch {
-      name = "fix-4373-4374.patch";
-      url = "https://git.claws-mail.org/?p=claws.git;a=patch;h=4c9d15b4b37cdc57edfa16df550a0a881a156153";
-      sha256 = "0sp0vxr6pk2fv5ydpcakb50rmn2w2nma98apgfsgcgan82qmwk7n";
-    })
   ];
 
   preConfigure = ''
