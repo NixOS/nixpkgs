f403758faccf9d2a4072073cca7fae79055c536d
Matthias Beyer <mail@beyermatthias.de>
2020-10-01 08:25:34 +0200

    sigal: 2.1 -> 2.1.1
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/applications/misc/sigal/default.nix
+++ pkgs/applications/misc/sigal/default.nix
@@ -1,12 +1,12 @@
 { lib, python3Packages, pythonAtLeast, ffmpeg }:
 
 python3Packages.buildPythonApplication rec {
-  version = "2.1";
+  version = "2.1.1";
   pname   = "sigal";
 
   src = python3Packages.fetchPypi {
     inherit version pname;
-    sha256 = "1ddjijpdg0hk8xzqsgnxicivscyh9lz9cxikkxi8yr9hw2wgwjnm";
+    sha256 = "0l07p457svznirz7qllgyl3qbhiisv7klhz7cbdw6417hxf9bih8";
   };
 
   disabled = python3Packages.pythonAtLeast "3.6.0";
