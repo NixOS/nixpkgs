de727c294c46f198cbf35b18278a1840eaa3b2f6
Tim Steinbach <NeQuissimus@users.noreply.github.com>
2020-10-01 09:01:36 -0400

    Merge pull request #99269 from NeQuissimus/linux_4_4_238
    linux: 4.4.237 -> 4.4.238
    (cherry picked from commit ea11b55f44273f2630a8436e30f97983b328c74a)

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.237";
+  version = "4.4.238";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1q6hwjwvlsikgr8b04l7v2jia2wyqxgbli6i7y20aq49h13ap2qk";
+    sha256 = "0r1kb7p0zf0nkavvf9nr9hs7bdjym43cqv87hkp7vrqpbh1i8y06";
   };
 } // (args.argsOverride or {}))
