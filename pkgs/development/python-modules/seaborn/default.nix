4d2c619ae06b726f5e9a5ab3f98f613296a01c8c
Mario Rodas <marsam@users.noreply.github.com>
2020-10-02 04:20:00 +0000

    pythonPackages.seaborn: update homepage

--- pkgs/development/python-modules/seaborn/default.nix
+++ pkgs/development/python-modules/seaborn/default.nix
@@ -29,7 +29,7 @@ buildPythonPackage rec {
 
   meta = {
     description = "Statisitical data visualization";
-    homepage = "http://stanford.edu/~mwaskom/software/seaborn/";
+    homepage = "https://seaborn.pydata.org/";
     license = with lib.licenses; [ bsd3 ];
     maintainers = with lib.maintainers; [ fridh ];
   };
