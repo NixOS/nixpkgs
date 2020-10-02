960c3fcda22e7ea1f51cf9f27c5e19c0082a10f5
Elis Hirwing <elis@hirwing.se>
2020-10-02 12:58:58 +0200

    php74: 7.4.8 -> 7.4.11
    https://www.php.net/ChangeLog-7.php#7.4.9
    https://www.php.net/ChangeLog-7.php#7.4.10
    https://www.php.net/ChangeLog-7.php#7.4.11
    (cherry picked from commit a792db658f6cfbd2748d8e5ab90f873140a14f2f)
    (cherry picked from commit 07c6f1f8ea66e8ddb1f8c53c0791b05d5f8407ab)

--- pkgs/development/interpreters/php/default.nix
+++ pkgs/development/interpreters/php/default.nix
@@ -279,7 +279,7 @@ in {
   };
 
   php74 = generic {
-    version = "7.4.8";
-    sha256 = "0ql01sfg8l7y2bfwmnjxnfw9irpibnz57ssck24b00y00nkd6j3a";
+    version = "7.4.11";
+    sha256 = "1idq2sk3x6msy8l2g42jv3y87h1fgb1aybxw7wpjkliv4iaz422l";
   };
 }
