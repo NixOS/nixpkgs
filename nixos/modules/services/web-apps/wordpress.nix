dc359e33468ce1bc9d619735b2edfd49143ab6ff
mohe2015 <Moritz.Hedtke@t-online.de>
2020-10-02 14:16:36 +0200

    nixos/wordpress: Remove theme version from directory name to aid theme upgrading

--- nixos/modules/services/web-apps/wordpress.nix
+++ nixos/modules/services/web-apps/wordpress.nix
@@ -32,8 +32,8 @@ let
       # Since hard linking directories is not allowed, copying is the next best thing.
 
       # copy additional plugin(s) and theme(s)
-      ${concatMapStringsSep "\n" (theme: "cp -r ${theme} $out/share/wordpress/wp-content/themes/${theme.name}") cfg.themes}
-      ${concatMapStringsSep "\n" (plugin: "cp -r ${plugin} $out/share/wordpress/wp-content/plugins/${plugin.name}") cfg.plugins}
+      ${concatMapStringsSep "\n" (theme: "cp -r ${theme} $out/share/wordpress/wp-content/themes/${if lib.versionAtLeast config.system.stateVersion "21.11" then theme.pname else theme.name}") cfg.themes}
+      ${concatMapStringsSep "\n" (plugin: "cp -r ${plugin} $out/share/wordpress/wp-content/plugins/${if lib.versionAtLeast config.system.stateVersion "21.11" then plugin.pname else plugin.name}") cfg.plugins}
     '';
   };
 
