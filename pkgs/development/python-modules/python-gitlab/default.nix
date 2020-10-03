e7f0fc422c0dd4c1791612510e7fb2e068ea577d
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-03 06:56:54 +0000

    python37Packages.python-gitlab: 2.4.0 -> 2.5.0

--- pkgs/development/python-modules/python-gitlab/default.nix
+++ pkgs/development/python-modules/python-gitlab/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "python-gitlab";
-  version = "2.4.0";
+  version = "2.5.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e240b5c371d9e98c46c980d878c3f03cd83f3da6cda01d533db27fa3e0dd474f";
+    sha256 = "68b42aafd4b620ab2534ff78a52584c7f799e4e55d5ac297eab4263066e6f74b";
   };
 
   propagatedBuildInputs = [ requests ];
