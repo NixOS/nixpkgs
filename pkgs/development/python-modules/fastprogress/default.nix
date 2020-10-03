f2e68f59f2f8fcc9bfd2e29cf199cfaf1f104b63
Robert Scott <code@humanleg.org.uk>
2020-10-03 23:31:43 +0100

    pythonPackages.fastprogress: init at 1.0.0

---
+++ pkgs/development/python-modules/fastprogress/default.nix
@@ -0,0 +1,32 @@
+{ stdenv
+, buildPythonPackage
+, fetchPypi
+, numpy
+, pytest
+, pythonOlder
+}:
+
+buildPythonPackage rec {
+  pname = "fastprogress";
+  version = "1.0.0";
+  disabled = pythonOlder "3.6";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1zhv37q6jkqd1pfhlkd4yzrc3dg83vyksgzf32mjlhd5sb0qmql9";
+  };
+
+  propagatedBuildInputs = [ numpy ];
+
+  # no real tests
+  doCheck = false;
+  pythonImportsCheck = [ "fastprogress" ];
+
+  meta = with stdenv.lib; {
+    homepage = "https://github.com/fastai/fastprogress";
+    description = "Simple and flexible progress bar for Jupyter Notebook and console";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ ris ];
+  };
+
+}
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2004,6 +2004,8 @@ in {
 
   fastpbkdf2 = callPackage ../development/python-modules/fastpbkdf2 { };
 
+  fastprogress = callPackage ../development/python-modules/fastprogress { };
+
   fastrlock = callPackage ../development/python-modules/fastrlock { };
 
   fasttext = callPackage ../development/python-modules/fasttext { };
