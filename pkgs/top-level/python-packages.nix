ad11124ad18fb8898fc42bffc1f02c4a6438b925
Jamie Magee <jamie.magee@gmail.com>
2020-10-03 16:11:13 +0200

    pythonPackages.pyads: init at 3.2.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4728,6 +4728,8 @@ in {
 
   pyacoustid = callPackage ../development/python-modules/pyacoustid { };
 
+  pyads = callPackage ../development/python-modules/pyads { };
+
   pyaes = callPackage ../development/python-modules/pyaes { };
 
   pyairvisual = callPackage ../development/python-modules/pyairvisual { };
