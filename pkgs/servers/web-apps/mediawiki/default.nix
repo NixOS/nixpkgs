8b3f68397e983e00ad02af705d7bda467a6c9572
Solène Rapenne <solene@perso.pw>
2020-10-03 18:29:20 +0200

    mediawiki: 1.34.4 -> 1.35.0
    (cherry picked from commit 807bb3901288c065e806fd91ef8dc60c7540b36d)

--- pkgs/servers/web-apps/mediawiki/default.nix
+++ pkgs/servers/web-apps/mediawiki/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "mediawiki";
-  version = "1.34.4";
+  version = "1.35.0";
 
   src = with stdenv.lib; fetchurl {
     url = "https://releases.wikimedia.org/mediawiki/${versions.majorMinor version}/${pname}-${version}.tar.gz";
-    sha256 = "09sj7xsfsnki8pw1hx5dwikwqgr9pcwhfpklsszsdywdj35wqs9b";
+    sha256 = "1m1f7yaabmfpp3ma394j9nqhx9xv47vpbc48qrbknlq3xhfvqzrs";
   };
 
   prePatch = ''
