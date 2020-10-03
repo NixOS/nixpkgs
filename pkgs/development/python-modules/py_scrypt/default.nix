acbeae428641b45568d9d4653b3330c90a47fe3c
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-03 09:54:26 +0000

    python27Packages.py_scrypt: 0.8.15 -> 0.8.17

--- pkgs/development/python-modules/py_scrypt/default.nix
+++ pkgs/development/python-modules/py_scrypt/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "scrypt";
-  version = "0.8.15";
+  version = "0.8.17";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0643fwj8vl96bsl30jx091zicmwyi0gglza66xqhqizqyqjq0ag6";
+    sha256 = "25b5075f2238be93af1cd574540a5ea01b8547f9b678aa72d22fce22577475ec";
   };
 
   buildInputs = [ openssl ];
