a054b43e8d4b048d7c124265e542d060de63f3ad
Félix Baylac-Jacqué <felix@alternativebit.fr>
2020-10-04 19:47:08 +0200

    plasma-workspace: add systementry sessionmanagement patch
    Systemd v246 hides the interface used by KDE to fetch the user session
    details. We're applying this commit from 5.19.90 upgrading the
    systementry applet to use the new sessionmanagement API.
    
    Because of this API breakage, the "Switch User" item was absent from
    the KDE application laucher.
    
    See the https://github.com/NixOS/nixpkgs/issues/98141 issue for the
    full story.

--- pkgs/desktops/plasma-5/plasma-workspace/default.nix
+++ pkgs/desktops/plasma-5/plasma-workspace/default.nix
@@ -1,5 +1,5 @@
 {
-  mkDerivation, lib,
+  mkDerivation, lib, fetchpatch,
 
   extra-cmake-modules, kdoctools,
 
@@ -46,6 +46,18 @@ mkDerivation {
   patches = [
     ./0001-startkde.patch
     ./0002-absolute-wallpaper-install-dir.patch
+    ( # Systemd v246 hides the interface used by KDE to fetch the
+      # user session details.
+      # We're applying this commit from 5.19.90 upgrading the
+      # systementry applet to use the new sessionmanagement API.
+      # See https://github.com/NixOS/nixpkgs/issues/98141 for more
+      # details.
+      # /!\ Remove this patch for version >= v5.19.90
+      fetchpatch {
+      name = "0003-port-systementry-to-sessionmanagement-api.patch";
+      url = "https://invent.kde.org/plasma/plasma-workspace/-/commit/05414ed58d43d87d907326636faac53ae2e7bd60.patch";
+      sha256 = "16izfcwxjkdn99j777ywjkzl01iyl542h4hpsbpkckccj7hz8sin";
+    })
   ];
 
   postPatch = ''
