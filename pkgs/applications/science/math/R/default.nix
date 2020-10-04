8fb83ec8bc1ba619b3e86837db9ceb7aac9d79ac
Dominic Steinitz <dominic@steinitz.org>
2020-10-04 15:22:42 +0100

    Fix inline-r problem

--- pkgs/applications/science/math/R/default.nix
+++ pkgs/applications/science/math/R/default.nix
@@ -33,6 +33,8 @@ stdenv.mkDerivation rec {
   ];
 
   prePatch = stdenv.lib.optionalString stdenv.isDarwin ''
+    substituteInPlace configure --replace "-install_name libRblas.dylib" "-install_name $out/lib/R/lib/libRblas.dylib"
+    substituteInPlace configure --replace "-install_name libRlapack.dylib" "-install_name $out/lib/R/lib/libRlapack.dylib"
     substituteInPlace configure --replace "-install_name libR.dylib" "-install_name $out/lib/R/lib/libR.dylib"
   '';
 
