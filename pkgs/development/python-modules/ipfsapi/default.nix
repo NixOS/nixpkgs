b90ab189b8b09bcf7c517df1939103f3bd6a75c5
Jonathan Ringer <jonringer117@gmail.com>
2020-10-05 15:48:22 -0700

    python3Packages.ipfsapi: 0.4.2.post1 -> 0.6.1

--- pkgs/development/python-modules/ipfsapi/default.nix
+++ pkgs/development/python-modules/ipfsapi/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage {
   pname = "ipfsapi";
-  version = "0.4.2.post1";
+  version = "0.6.1";
   disabled = isPy27;
 
   src = fetchFromGitHub {
     owner = "ipfs";
     repo = "py-ipfs-api";
-    rev = "0c485544a114f580c65e2ffbb5782efbf7fd9f61";
-    sha256 = "1v7f77cv95yv0v80gisdh71mj7jcq41xcfip6bqm57zfdbsa0xpn";
+    rev = version;
+    sha256 = "0asxl1wjp1g91an3s44rvma1xq2yxcdn94ridpjaazl6mghli9y2";
   };
 
   propagatedBuildInputs = [ six requests ];
