12e6f1ee892a3f5d87d2fd586410d6a17483ab4e
Jonathan Ringer <jonringer117@gmail.com>
2020-10-05 15:48:25 -0700

    python3Packages.kaggle: 1.5.6 -> 1.5.8

--- pkgs/development/python-modules/kaggle/default.nix
+++ pkgs/development/python-modules/kaggle/default.nix
@@ -12,11 +12,11 @@
 
 buildPythonPackage rec {
   pname = "kaggle";
-  version = "1.5.6";
+  version = "1.5.8";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0f5qrkgklcpgbwncrif7aw4f86dychqplh7k3f4rljwnr9yhjb1w";
+    sha256 = "6ab1d12520baa2ab91d42f81c4cb72bd3521a21e138a8d77a150b6691aa99efb";
   };
 
   # The version bounds in the setup.py file are unnecessarily restrictive.
