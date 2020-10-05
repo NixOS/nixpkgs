d2af188154164c68acd8caa6ad7572390ecc74e4
Jonathan Ringer <jonringer117@gmail.com>
2020-10-05 15:48:46 -0700

    python3Packages.robotframework-sshlibrary: 3.4.0 -> 3.5.1

--- pkgs/development/python-modules/robotframework-sshlibrary/default.nix
+++ pkgs/development/python-modules/robotframework-sshlibrary/default.nix
@@ -7,12 +7,12 @@
 }:
 
 buildPythonPackage rec {
-  version = "3.4.0";
+  version = "3.5.1";
   pname = "robotframework-sshlibrary";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "21fa0183776e6061366f517765db479aaa634b707f3dd9d90ef6972adae6a755";
+    sha256 = "843af44722d1d4e39f616e5441d6f49e82202cd1a8cc9d16637a6475a8ecb023";
   };
 
   # unit tests are impure
