0c831178e9a65de51059276cd4f1c9d604d25631
Jonathan Ringer <jonringer117@gmail.com>
2020-10-05 15:48:20 -0700

    python3Packages.greenlet: 0.4.16 -> 0.4.17

--- pkgs/development/python-modules/greenlet/default.nix
+++ pkgs/development/python-modules/greenlet/default.nix
@@ -8,12 +8,12 @@
 
 buildPythonPackage rec {
   pname = "greenlet";
-  version = "0.4.16";
+  version = "0.4.17";
   disabled = isPyPy;  # builtin for pypy
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "6e06eac722676797e8fce4adb8ad3dc57a1bb3adfb0dd3fdf8306c055a38456c";
+    sha256 = "41d8835c69a78de718e466dd0e6bfd4b46125f21a67c3ff6d76d8d8059868d6b";
   };
 
   propagatedBuildInputs = [ six ];
