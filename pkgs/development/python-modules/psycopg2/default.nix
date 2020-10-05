d26442479dd8fe6623959fee34eb037b5629223a
Jonathan Ringer <jonringer117@gmail.com>
2020-10-05 15:48:35 -0700

    python3Packages.psycopg2: 2.8.5 -> 2.8.6

--- pkgs/development/python-modules/psycopg2/default.nix
+++ pkgs/development/python-modules/psycopg2/default.nix
@@ -2,13 +2,13 @@
 
 buildPythonPackage rec {
   pname = "psycopg2";
-  version = "2.8.5";
+  version = "2.8.6";
 
   disabled = isPyPy;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f7d46240f7a1ae1dd95aab38bd74f7428d46531f69219954266d669da60c0818";
+    sha256 = "fb23f6c71107c37fd667cb4ea363ddeb936b348bbd6449278eb92c189699f543";
   };
 
   buildInputs = lib.optional stdenv.isDarwin openssl;
