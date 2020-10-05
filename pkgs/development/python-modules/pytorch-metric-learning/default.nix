9f56d0d6c354df190f4072f929438f372362c0c8
Jonathan Ringer <jonringer117@gmail.com>
2020-10-05 15:48:44 -0700

    python3Packages.pytorch-metric-learning: 0.9.81 -> 0.9.92

--- pkgs/development/python-modules/pytorch-metric-learning/default.nix
+++ pkgs/development/python-modules/pytorch-metric-learning/default.nix
@@ -11,15 +11,15 @@
 
 buildPythonPackage rec {
   pname   = "pytorch-metric-learning";
-  version = "0.9.81";
+  version = "0.9.92";
 
   disabled = isPy27;
 
   src = fetchFromGitHub {
     owner = "KevinMusgrave";
     repo = pname;
-    rev = "cb23328aba64f7f4658374cc2920ef5d56cda5c8";  # no version tag
-    sha256 = "0c2dyi4qi7clln43481xq66f6r4fadrz84jphjc5phz97bp33ds8";
+    rev = "v${version}";  # no version tag
+    sha256 = "085zjbwyl603y863vi9klxa5c29pva8jnmd0wnravajvdl7i22ys";
   };
 
   propagatedBuildInputs = [
