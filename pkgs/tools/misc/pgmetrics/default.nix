62c4da2ea03e0e4dbd710e5bfb3b2afe587d17fb
Mario Rodas <marsam@users.noreply.github.com>
2020-10-06 04:20:00 +0000

    pgmetrics: 1.9.3 -> 1.10.0
    https://github.com/rapidloop/pgmetrics/releases/tag/v1.10.0

--- pkgs/tools/misc/pgmetrics/default.nix
+++ pkgs/tools/misc/pgmetrics/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "pgmetrics";
-  version = "1.9.3";
+  version = "1.10.0";
 
   src = fetchFromGitHub {
     owner  = "rapidloop";
     repo   = pname;
     rev    = "v${version}";
-    sha256 = "1g0kdvc93ij155r3g7cd9f5p1x33vdi9p40403waanq5wiavjnzq";
+    sha256 = "1256mcac75nlr1c7pzsgqmjq026m6qxh9dlldndvl1s08bk7pxr9";
   };
 
   vendorSha256 = "16x33fmh4q993rw0jr65337yimska4fwgyyw3kmq84q0x28a3zg5";
