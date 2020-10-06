9b9bcdbbc1c56bee96aa56b628a2c83b028166b6
oxalica <oxalicc@pm.me>
2020-10-06 16:32:23 +0800

    rust-analyzer: 2020-09-28 -> 2020-10-05

--- pkgs/development/tools/rust/rust-analyzer/default.nix
+++ pkgs/development/tools/rust/rust-analyzer/default.nix
@@ -2,10 +2,10 @@
 
 {
   rust-analyzer-unwrapped = callPackage ./generic.nix rec {
-    rev = "2020-09-28";
+    rev = "2020-10-05";
     version = "unstable-${rev}";
-    sha256 = "1ynwrkdy78105xbrcynk0qimk90y849hn54sw8q3akdlyfx1kdrs";
-    cargoSha256 = "0iy7h3q7dp2nbfzzg7yc8zbkb5npghz8l15d83xd8w8q39i3qff5";
+    sha256 = "1vj5xwqif2ipzlb8ngpq3ymgqvv65d0700ihq7hx81k0i2m2awa6";
+    cargoSha256 = "14n7nk64qq27a7ygqm0bly2bby3bmsav6nvsap3bkbkppyr5gyrg";
   };
 
   rust-analyzer = callPackage ./wrapper.nix {} {
