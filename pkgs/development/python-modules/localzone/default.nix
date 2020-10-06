95e51e9039beb463a182cb766a078a79777f5623
Jonathan Ringer <jonringer117@gmail.com>
2020-10-06 11:49:27 -0700

    python3Packages.localzone: 0.9.6 -> 0.9.7

--- pkgs/development/python-modules/localzone/default.nix
+++ pkgs/development/python-modules/localzone/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage rec {
   pname = "localzone";
-  version = "0.9.6";
+  version = "0.9.7";
 
   src = fetchFromGitHub {
     owner = "ags-slc";
     repo = pname;
     rev = "v${version}";
-    sha256 = "154l7qglsm4jrhqddvlas8cgl9qm2z4dzihv05jmsyqjikcmfwk8";
+    sha256 = "1vzn1vm3zf86l7qncbmghjrwyvla9dc2v8abn8jajbl47gm7r5f7";
   };
 
   propagatedBuildInputs = [ dnspython sphinx ];
@@ -30,6 +30,5 @@ buildPythonPackage rec {
     homepage = "https://localzone.iomaestro.com";
     license = licenses.bsd3;
     maintainers = with maintainers; [ flyfloh ];
-    broken = true; # incompatible with dnspython>=2.0.0
   };
 }
