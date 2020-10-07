9b1a86f4d84b246c9a3862b49410c12aca92f1f3
Ben Siraphob <bensiraphob@gmail.com>
2020-10-07 19:41:09 +0700

    spasm-ng: init at unstable-2020-08-03

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9789,6 +9789,8 @@ in
 
   souffle = callPackage ../development/compilers/souffle { };
 
+  spasm-ng = callPackage ../development/compilers/spasm-ng { };
+
   spirv-llvm-translator = callPackage ../development/compilers/spirv-llvm-translator { };
 
   sqldeveloper = callPackage ../development/tools/database/sqldeveloper {
