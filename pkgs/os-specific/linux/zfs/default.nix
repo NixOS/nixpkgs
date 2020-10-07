51618b2e10025a14ab5c7af6d383cf92ba5188b5
Henri Menke <henri@henrimenke.de>
2020-10-07 19:25:08 +0200

    zfs: 0.8.4 -> 0.8.5

--- pkgs/os-specific/linux/zfs/default.nix
+++ pkgs/os-specific/linux/zfs/default.nix
@@ -153,9 +153,6 @@ let
         substituteInPlace $i --replace "zfs-import-cache.service" "zfs-import.target"
         done
 
-        # Fix pkgconfig.
-        ln -s ../share/pkgconfig $out/lib/pkgconfig
-
         # Remove tests because they add a runtime dependency on gcc
         rm -rf $out/share/zfs/zfs-tests
 
@@ -195,10 +192,9 @@ in {
     # incompatibleKernelVersion = "4.20";
 
     # this package should point to the latest release.
-    version = "0.8.4";
+    version = "0.8.5";
 
-    sha256 = "1hl4n900d24gl4vd65qdzq4m62b7bpvckldazcbd1xqcn8xhi6wp";
-    extraPatches = [ ./BACKPORT-Linux-5.8-compat-__vmalloc.patch ];
+    sha256 = "0vhd3zs2i83pd59nk0llml4vyk4fc178j6nhg00p6k3f6r0l655b";
   };
 
   zfsUnstable = common {
