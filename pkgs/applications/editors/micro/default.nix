f3142e81244e26e2e604e2a327a0fef1bc296b69
Mario Rodas <marsam@users.noreply.github.com>
2020-10-07 04:20:00 +0000

    micro: 2.0.7 -> 2.0.8

--- pkgs/applications/editors/micro/default.nix
+++ pkgs/applications/editors/micro/default.nix
@@ -1,40 +1,33 @@
-{ stdenv, buildGoPackage, fetchFromGitHub, installShellFiles }:
+{ lib, buildGoModule, fetchFromGitHub, installShellFiles }:
 
-buildGoPackage rec {
+buildGoModule rec {
   pname = "micro";
-  version = "2.0.7";
-
-  goPackagePath = "github.com/zyedidia/micro";
+  version = "2.0.8";
 
   src = fetchFromGitHub {
     owner = "zyedidia";
     repo = pname;
     rev = "v${version}";
-    sha256 = "07ck1a9arklic3p0z50wcg608cvpba1kljvlfb4fljr6jhv5cmkb";
-    fetchSubmodules = true;
+    sha256 = "1b51fvc9hrjfl8acr3yybp66xfll7d43412qwi76wxwarn06gkci";
   };
 
   nativeBuildInputs = [ installShellFiles ];
 
   subPackages = [ "cmd/micro" ];
 
-  buildFlagsArray = let t = "${goPackagePath}/internal/util"; in ''
-    -ldflags=
-      -X ${t}.Version=${version}
-      -X ${t}.CommitHash=${src.rev}
-  '';
+  vendorSha256 = "19iqvl63g9y6gkzfmv87rrgj4c4y6ngh467ss94rzrhaybj2b2d8";
 
-  goDeps = ./deps.nix;
+  buildFlagsArray = [ "-ldflags=-s -w -X github.com/zyedidia/micro/v2/internal/util.Version=${version} -X github.com/zyedidia/micro/v2/internal/util.CommitHash=${src.rev}" ];
 
   postInstall = ''
-    installManPage $src/assets/packaging/micro.1
+    installManPage assets/packaging/micro.1
+    install -Dt $out/share/applications assets/packaging/micro.desktop
   '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     homepage = "https://micro-editor.github.io";
     description = "Modern and intuitive terminal-based text editor";
     license = licenses.mit;
     maintainers = with maintainers; [ dtzWill ];
   };
 }
-
