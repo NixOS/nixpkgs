bfbc18ebeb81464da4d8c026b43d84ba51cc9013
Doron Behar <doron.behar@gmail.com>
2020-10-07 18:14:02 +0300

    pythonPackages.pyqtwebengine: Apply qt5.14 patch only if needed
    Since we now use qt5.15 in all python modules (defaulting to the same
    qt5 used in all-packages.nix), That patch is not required, but it will
    be needed if qt5.14 is used if pythonInterpreters is overridden, which
    is likely to happen like it happend to qutebrowser for instance. See:
    https://github.com/NixOS/nixpkgs/pull/99956/commits/2667af4062ef0710b486fbae3108e0141aecb0fc

--- pkgs/development/python-modules/pyqtwebengine/default.nix
+++ pkgs/development/python-modules/pyqtwebengine/default.nix
@@ -7,6 +7,10 @@ let
 
   inherit (pythonPackages) buildPythonPackage python isPy3k pyqt5 enum34;
   inherit (pyqt5) sip;
+  # source: https://www.riverbankcomputing.com/pipermail/pyqt/2020-June/042985.html
+  patches = lib.optional (lib.hasPrefix "5.14" pyqt5.version)
+    [ ./fix-build-with-qt-514.patch ]
+  ;
 
 in buildPythonPackage rec {
   pname = "PyQtWebEngine";
@@ -18,10 +22,7 @@ in buildPythonPackage rec {
     sha256 = "0xdzhl07x3mzfnr5cf4d640168vxi7fyl0fz1pvpbgs0irl14237";
   };
 
-  patches = [
-    # source: https://www.riverbankcomputing.com/pipermail/pyqt/2020-June/042985.html
-    ./fix-build-with-qt-514.patch
-  ];
+  inherit patches;
 
   outputs = [ "out" "dev" ];
 
