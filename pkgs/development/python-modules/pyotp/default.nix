5ed3b0798af1c9e919fa0e7b0a402632ebd2dcc0
IvarWithoutBones <ivar.scholten@protonmail.com>
2020-10-08 11:54:23 +0200

    pythonPackages.pyotp: fix tests
    (cherry picked from commit ae23d0bf634d460be9e75702f369a08c1c2e64ba)

--- pkgs/development/python-modules/pyotp/default.nix
+++ pkgs/development/python-modules/pyotp/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib, buildPythonPackage, fetchPypi, isPy27, urllib3 }:
 
 buildPythonPackage rec {
   pname = "pyotp";
@@ -8,6 +8,10 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "01eceab573181188fe038d001e42777884a7f5367203080ef5bda0e30fe82d28";
   };
+
+  doCheck = (!isPy27); # Tests import urllib3 in a way python27 does not support
+
+  checkInputs = [ urllib3 ];
 
   meta = with lib; {
     description = "Python One Time Password Library";
