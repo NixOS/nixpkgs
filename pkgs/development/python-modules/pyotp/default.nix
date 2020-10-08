e844be7341411f2372e11c1e7092857fa93ab8fb
IvarWithoutBones <ivar.scholten@protonmail.com>
2020-10-08 11:54:23 +0200

    pythonPackages.pyotp: disable on Python27

--- pkgs/development/python-modules/pyotp/default.nix
+++ pkgs/development/python-modules/pyotp/default.nix
@@ -1,13 +1,16 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib, buildPythonPackage, fetchPypi, isPy27 }:
 
 buildPythonPackage rec {
   pname = "pyotp";
   version = "2.4.0";
+  disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "01eceab573181188fe038d001e42777884a7f5367203080ef5bda0e30fe82d28";
   };
+
+  pythonImportsCheck = [ "pyotp" ];
 
   meta = with lib; {
     description = "Python One Time Password Library";
