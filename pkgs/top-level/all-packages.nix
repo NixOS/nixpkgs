7d53fdc91bc4cbd3c69d591b97cc1e2ff538cea6
Doron Behar <doron.behar@gmail.com>
2020-10-08 21:05:53 +0300

    kdoctools, kapidox: Remove out of top-level scope
    They do depend on qt5, and putting them in top level scope could cause
    qt5 incompatibilities errors.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10842,7 +10842,7 @@ in
   };
 
   # Does not actually depend on Qt 5
-  inherit (kdeFrameworks) extra-cmake-modules kapidox kdoctools;
+  inherit (kdeFrameworks) extra-cmake-modules;
 
   coccinelle = callPackage ../development/tools/misc/coccinelle {
     ocamlPackages = ocaml-ng.ocamlPackages_4_05;
@@ -15052,7 +15052,7 @@ in
       kwidgetsaddons kwindowsystem kxmlgui kxmlrpcclient modemmanager-qt
       networkmanager-qt plasma-framework prison qqc2-desktop-style solid sonnet
       syntax-highlighting syndication threadweaver kirigami2 kholidays kpurpose
-      kcontacts kquickcharts;
+      kcontacts kquickcharts kdoctools kapidox;
 
     ### KDE PLASMA 5
 
