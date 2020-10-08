f041a6d0778e5f424f4c637802abafcacf66f88a
IvarWithoutBones <ivar.scholten@protonmail.com>
2020-10-08 11:54:23 +0200

    pythonPackages.pyotp: disable on Python27
    (cherry picked from commit 7a348085020e35f7f8bbe7f7fad59081f2aad025)

--- pkgs/development/python-modules/pyotp/default.nix
+++ pkgs/development/python-modules/pyotp/default.nix
@@ -1,17 +1,16 @@
-{ lib, buildPythonPackage, fetchPypi, isPy27, urllib3 }:
+{ lib, buildPythonPackage, fetchPypi, isPy27 }:
 
 buildPythonPackage rec {
   pname = "pyotp";
   version = "2.4.0";
+  disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "01eceab573181188fe038d001e42777884a7f5367203080ef5bda0e30fe82d28";
   };
 
-  doCheck = (!isPy27); # Tests import urllib3 in a way python27 does not support
-
-  checkInputs = [ urllib3 ];
+  pythonImportsCheck = [ "pyotp" ];
 
   meta = with lib; {
     description = "Python One Time Password Library";
