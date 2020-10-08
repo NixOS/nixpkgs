8b6687d2d4ad296942c644b47600ca5596277342
Florian Ströger <florian@florianstroeger.com>
2020-10-08 08:39:58 +0200

    helmsman: 3.4.4 -> 3.4.5

--- pkgs/applications/networking/cluster/helmsman/default.nix
+++ pkgs/applications/networking/cluster/helmsman/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "helmsman";
-  version = "3.4.4";
+  version = "3.4.5";
 
   src = fetchFromGitHub {
     owner = "Praqma";
     repo = "helmsman";
     rev = "v${version}";
-    sha256 = "01vjghak2szif0p82kall5jw7mbfh4fg7fcjkblmic7l0vlqhfac";
+    sha256 = "0hw2j8bfrlc73q5sxcsj3r2vcsl93cxszfky0svafzgr0a9rnn6z";
   };
 
   vendorSha256 = "05vnysr5r3hbayss1pyifgp989kjw81h95iack8ady62k6ys5njl";
