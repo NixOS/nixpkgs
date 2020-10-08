179986f378ba6d145fe3bd4c1c011bab1537760b
Doron Behar <doron.behar@gmail.com>
2020-10-08 19:49:07 +0300

    webmacs: Use qt5.15, as pyqt5 in it (#99956)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24055,7 +24055,7 @@ in
 
   webcamoid = libsForQt514.callPackage ../applications/video/webcamoid { };
 
-  webmacs = libsForQt514.callPackage ../applications/networking/browsers/webmacs {};
+  webmacs = libsForQt5.callPackage ../applications/networking/browsers/webmacs {};
 
   webtorrent_desktop = callPackage ../applications/video/webtorrent_desktop {};
 
