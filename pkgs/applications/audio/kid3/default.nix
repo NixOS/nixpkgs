a25350b6736902ded44b9ebe4270e1be88b4a008
AndersonTorres <torres.anderson.85@protonmail.com>
2020-10-10 01:57:56 -0300

    kid3: 3.8.3 -> 3.8.4

--- pkgs/applications/audio/kid3/default.nix
+++ pkgs/applications/audio/kid3/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl
-, pkgconfig, cmake, python, ffmpeg_3, phonon, automoc4
+, pkgconfig, cmake, python3, ffmpeg_3, phonon, automoc4
 , chromaprint, docbook_xml_dtd_45, docbook_xsl, libxslt
 , id3lib, taglib, mp4v2, flac, libogg, libvorbis
 , zlib, readline , qtbase, qttools, qtmultimedia, qtquickcontrols
@@ -7,18 +7,17 @@
 }:
 
 stdenv.mkDerivation rec {
-
   pname = "kid3";
-  version = "3.8.3";
+  version = "3.8.4";
 
   src = fetchurl {
     url = "mirror://sourceforge/project/kid3/kid3/${version}/${pname}-${version}.tar.gz";
-    sha256 = "0i0c4bmsm36jj1v535kil47ig0ig70ykrzcw2f56spr25xns06ka";
+    sha256 = "sha256-WYuEOqMu2VMOv6mkVCRXnmInFER/DWfPNqYuaTJ3vAc=";
   };
 
   nativeBuildInputs = [ wrapQtAppsHook ];
-  buildInputs = with stdenv.lib;
-  [ pkgconfig cmake python ffmpeg_3 phonon automoc4
+  buildInputs = [
+    pkgconfig cmake python3 ffmpeg_3 phonon automoc4
     chromaprint docbook_xml_dtd_45 docbook_xsl libxslt
     id3lib taglib mp4v2 flac libogg libvorbis zlib readline
     qtbase qttools qtmultimedia qtquickcontrols ];
@@ -35,34 +34,33 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     description = "A simple and powerful audio tag editor";
     longDescription = ''
-      If you want to easily tag multiple MP3, Ogg/Vorbis, FLAC, MPC,
-      MP4/AAC, MP2, Opus, Speex, TrueAudio, WavPack, WMA, WAV and AIFF
-      files (e.g. full albums) without typing the same information
-      again and again and have control over both ID3v1 and ID3v2 tags,
-      then Kid3 is the program you are looking for.
+      If you want to easily tag multiple MP3, Ogg/Vorbis, FLAC, MPC, MP4/AAC,
+      MP2, Opus, Speex, TrueAudio, WavPack, WMA, WAV and AIFF files (e.g. full
+      albums) without typing the same information again and again and have
+      control over both ID3v1 and ID3v2 tags, then Kid3 is the program you are
+      looking for.
 
       With Kid3 you can:
       - Edit ID3v1.1 tags;
       - Edit all ID3v2.3 and ID3v2.4 frames;
       - Convert between ID3v1.1, ID3v2.3 and ID3v2.4 tags
-      - Edit tags in MP3, Ogg/Vorbis, FLAC, MPC, MP4/AAC, MP2, Opus,
-        Speex, TrueAudio, WavPack, WMA, WAV, AIFF files and tracker
-        modules (MOD, S3M, IT, XM);
-      -  Edit tags of multiple files, e.g. the artist, album, year and
-         genre of all files of an album typically have the same values
-         and can be set together;
+      - Edit tags in MP3, Ogg/Vorbis, FLAC, MPC, MP4/AAC, MP2, Opus, Speex,
+        TrueAudio, WavPack, WMA, WAV, AIFF files and tracker modules (MOD, S3M,
+        IT, XM);
+      - Edit tags of multiple files, e.g. the artist, album, year and genre of
+        all files of an album typically have the same values and can be set
+        together;
       - Generate tags from filenames;
       - Generate tags from the contents of tag fields;
       - Generate filenames from tags;
       - Rename and create directories from tags;
       - Generate playlist files;
       - Automatically convert upper and lower case and replace strings;
-      - Import from gnudb.org, TrackType.org, MusicBrainz, Discogs,
-        Amazon and other sources of album data;
-      - Export tags as CSV, HTML, playlists, Kover XML and in other
-        formats;
-      - Edit synchronized lyrics and event timing codes, import and
-        export LRC files
+      - Import from gnudb.org, TrackType.org, MusicBrainz, Discogs, Amazon and
+        other sources of album data;
+      - Export tags as CSV, HTML, playlists, Kover XML and in other formats;
+      - Edit synchronized lyrics and event timing codes, import and export
+        LRC files.
     '';
     homepage = "http://kid3.sourceforge.net/";
     license = licenses.lgpl2Plus;
