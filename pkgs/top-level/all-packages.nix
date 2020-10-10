2336543338d1df3861b1d6b06364ae12b0fcf0e3
Сухарик <65870+suhr@users.noreply.github.com>
2020-10-10 10:38:22 +0300

    julia-mono: init at 0.018

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19087,6 +19087,8 @@ in
 
   junicode = callPackage ../data/fonts/junicode { };
 
+  julia-mono = callPackage ../data/fonts/julia-mono { };
+
   kanji-stroke-order-font = callPackage ../data/fonts/kanji-stroke-order-font {};
 
   kawkab-mono-font = callPackage ../data/fonts/kawkab-mono {};
