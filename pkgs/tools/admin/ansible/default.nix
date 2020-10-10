3530dc205edb91ea71ce0d012f13c7e43d47d6df
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-10-10 15:58:10 +0200

    ansible: default to ansible_2_10

--- pkgs/tools/admin/ansible/default.nix
+++ pkgs/tools/admin/ansible/default.nix
@@ -5,22 +5,19 @@ rec {
 
   # The python module stays at v2.9.x until the related package set has caught up. Therefore v2.10 gets an override
   # for now.
-  ansible_2_10 = python3Packages.toPythonApplication (python3Packages.ansible.overridePythonAttrs (old: rec {
-    pname = "ansible";
-    version = "2.10.0";
+  ansible_2_10 = python3Packages.toPythonApplication python3Packages.ansible-base;
 
-    # TODO: migrate to fetchurl, when release becomes available on releases.ansible.com
-    src = fetchFromGitHub {
-      owner = pname;
-      repo = pname;
-      rev = "v${version}";
-      sha256 = "0k9rs5ajx0chaq0xr1cj4x7fr5n8kd4y856miss6k01iv2m7yx42";
+  ansible_2_9 = python3Packages.toPythonApplication (python3Packages.ansible-base.overridePythonAttrs (old: rec {
+    pname = "ansible";
+    version = "2.9.14";
+
+    src = fetchurl {
+      url = "https://releases.ansible.com/ansible/${pname}-${version}.tar.gz";
+      sha256 = "1dbgzj5m6wx0jdjqbsz8n58jirlbjylfy94izngdvjgh10z1irzg";
     };
   }));
 
-  ansible_2_9 = python3Packages.toPythonApplication python3Packages.ansible;
-
-  ansible_2_8 = python3Packages.toPythonApplication (python3Packages.ansible.overridePythonAttrs (old: rec {
+  ansible_2_8 = python3Packages.toPythonApplication (python3Packages.ansible-base.overridePythonAttrs (old: rec {
     pname = "ansible";
     version = "2.8.14";
 
