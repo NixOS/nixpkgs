6f14ff8cd49f0c9d8719c1698cb718d366b290ec
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-10-10 15:57:12 +0200

    python3Packages.ansible: init at 2.10.0
    Since 2.10 this package holds the collections. The engine has moved to
    the ansible-base package.

--- pkgs/development/python-modules/ansible/default.nix
+++ pkgs/development/python-modules/ansible/default.nix
@@ -1,57 +1,26 @@
 { lib
-, fetchFromGitHub
 , buildPythonPackage
-, pycrypto
-, paramiko
-, jinja2
-, pyyaml
-, httplib2
-, six
-, netaddr
-, dnspython
-, jmespath
-, dopy
-, ncclient
-, windowsSupport ? false
-, pywinrm
+, fetchPypi
+, ansible-base
 }:
 
 buildPythonPackage rec {
   pname = "ansible";
-  version = "2.9.12";
+  version = "2.10.0";
 
-  src = fetchFromGitHub {
-    owner = "ansible";
-    repo = "ansible";
-    rev = "v${version}";
-    sha256 = "0c794k0cyl54807sh9in0l942ah6g6wlz5kf3qvy5lhd581zlgyb";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "0ink65zi7ghxy14rxwc2g7ck7flj799zx769aihf811zmiiddb9s";
   };
 
-  prePatch = ''
-    # ansible-connection is wrapped, so make sure it's not passed
-    # through the python interpreter.
-    sed -i "s/\[python, /[/" lib/ansible/executor/task_executor.py
-  '';
-
-  postInstall = ''
-    for m in docs/man/man1/*; do
-      install -vD $m -t $out/share/man/man1
-    done
-  '';
-
   propagatedBuildInputs = [
-    pycrypto paramiko jinja2 pyyaml httplib2
-    six netaddr dnspython jmespath dopy ncclient
-  ] ++ lib.optional windowsSupport pywinrm;
-
-  # dificult to test
-  doCheck = false;
+    ansible-base
+  ];
 
   meta = with lib; {
-    homepage = "http://www.ansible.com";
-    description = "Radically simple IT automation";
-    license = [ licenses.gpl3 ] ;
-    maintainers = with maintainers; [ joamaki costrouc hexa ];
-    platforms = platforms.linux ++ platforms.darwin;
+    description = "Ansible collections";
+    homepage = "https://pypi.org/project/ansible/";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ hexa ];
   };
 }
