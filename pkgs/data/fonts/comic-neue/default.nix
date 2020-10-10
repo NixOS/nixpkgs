114ab2551f4613d0914bbe957fcec8a6f47efbc7
AndersonTorres <torres.anderson.85@protonmail.com>
2020-10-10 01:57:12 -0300

    comic-neue: 2.5 -> 2.51

--- pkgs/data/fonts/comic-neue/default.nix
+++ pkgs/data/fonts/comic-neue/default.nix
@@ -2,33 +2,26 @@
 
 stdenv.mkDerivation rec {
   pname = "comic-neue";
-  version = "2.5";
+  version = "2.51";
 
   src = fetchzip {
     url = "http://comicneue.com/${pname}-${version}.zip";
-    sha256 = "1kc0yyha6cc584vcl9z1cq1z6prgkxk93g75mr8gapfdrj25dp3q";
+    sha256 = "sha256-DjRZtFnJOtZnxhfpgU5ihZFAonRK608/BQztCAExIU0=";
     stripRoot = false; # because it comes with a __MACOSX directory
   };
 
   phases = [ "unpackPhase" "installPhase" ];
 
   installPhase = ''
-    mkdir -pv $out/share/{doc/${pname}-${version},fonts/{opentype,truetype,EOT,WOFF,WOFF2}}
+    mkdir -pv $out/share/{doc/${pname}-${version},fonts/{opentype,truetype,WOFF,WOFF2}}
     cp -v ${pname}-${version}/{FONTLOG,OFL-FAQ,OFL}.txt $out/share/doc/
+    cp -v ${pname}-${version}/Booklet-ComicNeue.pdf $out/share/doc/
     cp -v ${pname}-${version}/OTF/ComicNeue-Angular/*.otf $out/share/fonts/opentype
     cp -v ${pname}-${version}/OTF/ComicNeue/*.otf $out/share/fonts/opentype
     cp -v ${pname}-${version}/TTF/ComicNeue-Angular/*.ttf $out/share/fonts/truetype
     cp -v ${pname}-${version}/TTF/ComicNeue/*.ttf $out/share/fonts/truetype
-    cp -v ${pname}-${version}/WebFonts/eot/ComicNeue-Angular/*.eot $out/share/fonts/EOT
-    cp -v ${pname}-${version}/WebFonts/eot/ComicNeue/*.eot $out/share/fonts/EOT
-    cp -v ${pname}-${version}/WebFonts/woff/ComicNeue-Angular/*.woff $out/share/fonts/WOFF
-    cp -v ${pname}-${version}/WebFonts/woff/ComicNeue/*.woff $out/share/fonts/WOFF
-    cp -v ${pname}-${version}/WebFonts/woff2/ComicNeue/*.woff2 $out/share/fonts/WOFF2
-
-    # Quick fix for conflicting names in upstream
-    for i in ${pname}-${version}/WebFonts/woff2/ComicNeue-Angular/*.woff2; do
-      cp -v $i $out/share/fonts/WOFF2/`basename $i|sed -e 's|ComicNeue|ComicNeue-Angular|'`
-    done
+    cp -v ${pname}-${version}/WebFonts/*.woff $out/share/fonts/WOFF
+    cp -v ${pname}-${version}/WebFonts/*.woff2 $out/share/fonts/WOFF2
   '';
 
   meta = with stdenv.lib; {
