a73c1259d13b42bf3410cce5b6d7270d4cbd011f
Florian Klink <flokli@flokli.de>
2020-10-10 16:43:12 +0200

    golden-cheetah: fix build with bison 3.7
    Currently, GoldenCheetah fails to build with Bison >=3.7 (which we use
    for some time).
    
    This applies the commit from https://github.com/GoldenCheetah/GoldenCheetah/pull/3590.
    
    Upstream issue: https://github.com/GoldenCheetah/GoldenCheetah/issues/3586

--- pkgs/applications/misc/golden-cheetah/default.nix
+++ pkgs/applications/misc/golden-cheetah/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, mkDerivation
+{ stdenv, fetchFromGitHub, fetchpatch, mkDerivation
 , qtbase, qtsvg, qtserialport, qtwebengine, qtmultimedia, qttools
 , qtconnectivity, qtcharts, libusb-compat-0_1
 , yacc, flex, zlib, qmake, makeDesktopItem, makeWrapper
@@ -30,6 +30,15 @@ in mkDerivation rec {
     qtconnectivity qtcharts libusb-compat-0_1
   ];
   nativeBuildInputs = [ flex makeWrapper qmake yacc ];
+
+  patches = [
+    # allow building with bison 3.7
+    # PR at https://github.com/GoldenCheetah/GoldenCheetah/pull/3590
+    (fetchpatch {
+      url = "https://github.com/GoldenCheetah/GoldenCheetah/commit/e1f42f8b3340eb4695ad73be764332e75b7bce90.patch";
+      sha256 = "1h0y9vfji5jngqcpzxna5nnawxs77i1lrj44w8a72j0ah0sznivb";
+    })
+  ];
 
   NIX_LDFLAGS = "-lz";
 
