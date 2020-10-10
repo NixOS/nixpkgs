139f44fcd22e19aa7fe12774c238a4a36b4eb10a
Michael Weiss <dev.primeos@gmail.com>
2020-10-10 15:27:47 +0200

    doc: Document a workaround for using an FQDN as hostname
    Since #76542 this workaround is required to use a FQDN as hostname. See
    #94011 and #94022 for the related discussion. Due to some
    potential/unresolved issues (legacy software, backward compatibility,
    etc.) we're documenting this workaround [0].
    
    [0]: https://github.com/NixOS/nixpkgs/issues/94011#issuecomment-705952300

--- nixos/modules/tasks/network-interfaces.nix
+++ nixos/modules/tasks/network-interfaces.nix
@@ -391,6 +391,10 @@ in
         end with a letter or digit, and have as interior characters only
         letters, digits, and hyphen. The maximum length is 63 characters.
         Additionally it is recommended to only use lower-case characters.
+        If (e.g. for legacy reasons) a FQDN is required as the Linux kernel
+        network node hostname (uname --nodename) the option
+        boot.kernel.sysctl."kernel.hostname" can be used as a workaround (but
+        the 64 character limit still applies).
       '';
     };
 
