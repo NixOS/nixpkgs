ba173b438a21105a85fbf3f46e6ce67ec4ad6b4d
Elis Hirwing <elis@hirwing.se>
2020-10-11 18:00:01 +0200

    phpExtensions.imagick: Move to separate directory

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -76,18 +76,7 @@ in
 
     igbinary = callPackage ../development/php-packages/igbinary { };
 
-    imagick = buildPecl {
-      version = "3.4.4";
-      pname = "imagick";
-
-      sha256 = "0xvhaqny1v796ywx83w7jyjyd0nrxkxf34w9zi8qc8aw8qbammcd";
-
-      configureFlags = [ "--with-imagick=${pkgs.imagemagick.dev}" ];
-      nativeBuildInputs = [ pkgs.pkgconfig ];
-      buildInputs = [ pcre' ];
-
-      meta.maintainers = lib.teams.php.members;
-    };
+    imagick = callPackage ../development/php-packages/imagick { };
 
     mailparse = buildPecl {
       version = "3.0.3";
