4f94ba58d37095991cbe0db127af2633c12e94b3
Elis Hirwing <elis@hirwing.se>
2020-10-11 16:41:30 +0200

    phpPackages.php-parallel-lint: Move to separate directory

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -41,42 +41,7 @@ in
 
     php-cs-fixer = callPackage ../development/php-packages/php-cs-fixer { };
 
-    php-parallel-lint = mkDerivation rec {
-      version = "1.0.0";
-      pname = "php-parallel-lint";
-
-      src = pkgs.fetchFromGitHub {
-        owner = "JakubOnderka";
-        repo = "PHP-Parallel-Lint";
-        rev = "v${version}";
-        sha256 = "16nv8yyk2z3l213dg067l6di4pigg5rd8yswr5xgd18jwbys2vnw";
-      };
-
-      buildInputs = [
-        pkgs.makeWrapper
-        php.packages.composer
-        php.packages.box
-      ];
-
-      buildPhase = ''
-        composer dump-autoload
-        box build
-      '';
-
-      installPhase = ''
-        mkdir -p $out/bin
-        install -D parallel-lint.phar $out/libexec/php-parallel-lint/php-parallel-lint.phar
-        makeWrapper ${php}/bin/php $out/bin/php-parallel-lint \
-          --add-flags "$out/libexec/php-parallel-lint/php-parallel-lint.phar"
-      '';
-
-      meta = with pkgs.lib; {
-        description = "This tool check syntax of PHP files faster than serial check with fancier output";
-        license = licenses.bsd2;
-        homepage = "https://github.com/JakubOnderka/PHP-Parallel-Lint";
-        maintainers = with maintainers; [ jtojnar ] ++ teams.php.members;
-      };
-    };
+    php-parallel-lint = callPackage ../development/php-packages/php-parallel-lint { };
 
     phpcbf = mkDerivation rec {
       version = "3.5.5";
