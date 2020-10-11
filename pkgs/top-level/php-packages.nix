794af83ca7b62b5034e9c44ab22b6fc0f73b4cdd
Elis Hirwing <elis@hirwing.se>
2020-10-11 16:38:09 +0200

    phpPackages.composer2: Move to separate directory

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -37,34 +37,7 @@ in
 
     composer = callPackage ../development/php-packages/composer { };
 
-    composer2 = mkDerivation rec {
-      version = "2.0.0-RC1";
-      pname = "composer";
-
-      src = pkgs.fetchurl {
-        url = "https://getcomposer.org/download/${version}/composer.phar";
-        sha256 = "0wzr360gaa59cbjpa3vw9yrpc55a4fmdv68q0rn7vj0mjnz60fhd";
-      };
-
-      dontUnpack = true;
-
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-
-      installPhase = ''
-        mkdir -p $out/bin
-        install -D $src $out/libexec/composer/composer.phar
-        makeWrapper ${php}/bin/php $out/bin/composer \
-          --add-flags "$out/libexec/composer/composer.phar" \
-          --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.unzip ]}
-      '';
-
-      meta = with pkgs.lib; {
-        description = "Dependency Manager for PHP";
-        license = licenses.mit;
-        homepage = "https://getcomposer.org/";
-        maintainers = with maintainers; [ offline ] ++ teams.php.members;
-      };
-    };
+    composer2 = callPackage ../development/php-packages/composer/2.0.nix { };
 
     php-cs-fixer = mkDerivation rec {
       version = "2.16.4";
