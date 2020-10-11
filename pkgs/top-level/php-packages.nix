0fc9ac20fe78b0ab6529db83861fb55979e0cb84
Elis Hirwing <elis@hirwing.se>
2020-10-11 17:32:19 +0200

    phpExtensions.apcu: Move to separate directory

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -62,21 +62,7 @@ in
   # or php.withExtensions to extend the functionality of the PHP
   # interpreter.
   extensions = {
-    apcu = buildPecl {
-      version = "5.1.18";
-      pname = "apcu";
-
-      sha256 = "0ayykd4hfvdzk7qnr5k6yq5scwf6rb2i05xscfv76q5dmkkynvfl";
-
-      buildInputs = [ pcre' ];
-      doCheck = true;
-      checkTarget = "test";
-      checkFlagsArray = ["REPORT_EXIT_STATUS=1" "NO_INTERACTION=1"];
-      makeFlags = [ "phpincludedir=$(dev)/include" ];
-      outputs = [ "out" "dev" ];
-
-      meta.maintainers = lib.teams.php.members;
-    };
+    apcu = callPackage ../development/php-packages/apcu { };
 
     apcu_bc = buildPecl {
       version = "1.0.5";
