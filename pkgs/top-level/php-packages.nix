dacfd9d40cb2b84e545f1790937f1a894b420687
Elis Hirwing <elis@hirwing.se>
2020-10-11 16:46:49 +0200

    phpPackages.psysh: Move to separate directory and be version specific

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -53,32 +53,7 @@ in
 
     psalm = callPackage ../development/php-packages/psalm { };
 
-    psysh = mkDerivation rec {
-      version = "0.10.3";
-      pname = "psysh";
-
-      src = pkgs.fetchurl {
-        url = "https://github.com/bobthecow/psysh/releases/download/v${version}/psysh-v${version}.tar.gz";
-        sha256 = "0glply451fy0g7zbasyp350qvmk2aglrlcrcdd7w0igylgwfkg71";
-      };
-
-      phases = [ "installPhase" ];
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-
-      installPhase = ''
-        mkdir -p $out/bin
-        tar -xzf $src -C $out/bin
-        chmod +x $out/bin/psysh
-        wrapProgram $out/bin/psysh
-      '';
-
-      meta = with pkgs.lib; {
-        description = "PsySH is a runtime developer console, interactive debugger and REPL for PHP.";
-        license = licenses.mit;
-        homepage = "https://psysh.org/";
-        maintainers = with maintainers; [ caugner ] ++ teams.php.members;
-      };
-    };
+    psysh = callPackage ../development/php-packages/psysh { };
   };
 
 
