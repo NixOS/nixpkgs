58c1014f5ad90c1834d77fd154b842dda1c81572
Elis Hirwing <elis@hirwing.se>
2020-10-11 19:36:24 +0200

    phpExtensions.pinba: Move to separate directory

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -124,32 +124,7 @@ in
 
     php_excel = callPackage ../development/php-packages/php_excel { };
 
-    pinba = let
-      version = if isPhp73 then "1.1.2-dev" else "1.1.1";
-      src = pkgs.fetchFromGitHub ({
-        owner = "tony2001";
-        repo = "pinba_extension";
-      } // (if (isPhp73) then {
-        rev = "edbc313f1b4fb8407bf7d5acf63fbb0359c7fb2e";
-        sha256 = "02sljqm6griw8ccqavl23f7w1hp2zflcv24lpf00k6pyrn9cwx80";
-      } else {
-        rev = "RELEASE_1_1_1";
-        sha256 = "1kdp7vav0y315695vhm3xifgsh6h6y6pny70xw3iai461n58khj5";
-      }));
-    in buildPecl {
-      pname = "pinba";
-      inherit version src;
-
-      meta = with pkgs.lib; {
-        description = "PHP extension for Pinba";
-        longDescription = ''
-          Pinba is a MySQL storage engine that acts as a realtime monitoring and
-          statistics server for PHP using MySQL as a read-only interface.
-        '';
-        homepage = "http://pinba.org/";
-        maintainers = teams.php.members;
-      };
-    };
+    pinba = callPackage ../development/php-packages/pinba { };
 
     protobuf = buildPecl {
       version = "3.11.2";
