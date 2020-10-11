6e1cfa6a4f93a7bcf00ff05f36b69d30d1919a4c
Elis Hirwing <elis@hirwing.se>
2020-10-11 16:39:33 +0200

    phpPackages.php-cs-fixer: Move to separate directory

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -39,32 +39,7 @@ in
 
     composer2 = callPackage ../development/php-packages/composer/2.0.nix { };
 
-    php-cs-fixer = mkDerivation rec {
-      version = "2.16.4";
-      pname = "php-cs-fixer";
-
-      src = pkgs.fetchurl {
-        url = "https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v${version}/php-cs-fixer.phar";
-        sha256 = "05rdvypxc86hjs8b7id2csa7g1rf7dk2swzfvd5768abdgfasvr8";
-      };
-
-      phases = [ "installPhase" ];
-      buildInputs = [ pkgs.makeWrapper ];
-
-      installPhase = ''
-        mkdir -p $out/bin
-        install -D $src $out/libexec/php-cs-fixer/php-cs-fixer.phar
-        makeWrapper ${php}/bin/php $out/bin/php-cs-fixer \
-          --add-flags "$out/libexec/php-cs-fixer/php-cs-fixer.phar"
-      '';
-
-      meta = with pkgs.lib; {
-        description = "A tool to automatically fix PHP coding standards issues";
-        license = licenses.mit;
-        homepage = "http://cs.sensiolabs.org/";
-        maintainers = with maintainers; [ jtojnar ] ++ teams.php.members;
-      };
-    };
+    php-cs-fixer = callPackage ../development/php-packages/php-cs-fixer { };
 
     php-parallel-lint = mkDerivation rec {
       version = "1.0.0";
