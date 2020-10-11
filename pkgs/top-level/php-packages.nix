d5ea4254a4230391ccf30f862fda2dd1f7000a63
Elis Hirwing <elis@hirwing.se>
2020-10-11 19:32:05 +0200

    phpExtensions.php_excel: Move to separate directory

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -122,22 +122,7 @@ in
 
     pdo_sqlsrv = callPackage ../development/php-packages/pdo_sqlsrv { };
 
-    php_excel = buildPecl rec {
-      version = "1.0.2";
-      pname = "php_excel";
-      phpVersion = "php7";
-
-      buildInputs = [ pkgs.libxl ];
-
-      src = pkgs.fetchurl {
-        url = "https://github.com/iliaal/php_excel/releases/download/Excel-1.0.2-PHP7/excel-${version}-${phpVersion}.tgz";
-        sha256 = "0dpvih9gpiyh1ml22zi7hi6kslkilzby00z1p8x248idylldzs2n";
-      };
-
-      configureFlags = [ "--with-excel" "--with-libxl-incdir=${pkgs.libxl}/include_c" "--with-libxl-libdir=${pkgs.libxl}/lib" ];
-
-      meta.maintainers = lib.teams.php.members;
-    };
+    php_excel = callPackage ../development/php-packages/php_excel { };
 
     pinba = let
       version = if isPhp73 then "1.1.2-dev" else "1.1.1";
