97af8715afc95dd1bc8f499e870cc34a9110db77
J. Neto <xablau@tuta.io>
2020-10-11 14:28:57 -0300

    textadept: 10.2 -> 10.8

--- pkgs/applications/editors/textadept/default.nix
+++ pkgs/applications/editors/textadept/default.nix
@@ -1,27 +1,36 @@
-{ lib, stdenv, fetchhg, fetchurl, gtk2, glib, pkgconfig, unzip, ncurses, zip }:
+{ lib, stdenv, fetchhg, fetchurl, gtk2, glib, pkgconfig, unzip, ncurses, zip, writeTextFile }:
+
+let
+  # work around trying to download stuff in `make deps`
+  fakeWget = writeTextFile {
+    name = "fake-wget";
+    destination = "/bin/wget";
+    executable = true;
+    text = ''
+      #!${stdenv.shell}
+      true
+    '';
+  };
+in
 stdenv.mkDerivation rec {
-  version = "10.2";
+  version = "10.8";
   pname = "textadept";
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [
-    gtk2 ncurses glib unzip zip
+    gtk2 ncurses glib unzip zip fakeWget
   ];
 
   src = fetchhg {
     url = "http://foicica.com/hg/textadept";
     rev = "textadept_${version}";
-    sha256 = "0fai8xqddkkprmbf0cf8wwgv7ccfdb1iyim30nppm2m16whkc8fl";
+    sha256 = "sha256-dEZSx2tuHTWYhk9q5iGlrWTAvDvKaM8HaHwXcFcv33s=";
   };
 
   preConfigure =
     lib.concatStringsSep "\n" (lib.mapAttrsToList (name: params:
       "ln -s ${fetchurl params} $PWD/src/${name}"
     ) (import ./deps.nix)) + ''
-
-    # work around trying to download stuff in `make deps`
-    function wget() { true; }
-    export -f wget
 
     cd src
     make deps
@@ -31,12 +40,17 @@ stdenv.mkDerivation rec {
     make curses
   '';
 
+  preInstall = ''
+    mkdir -p $out/share/applications
+    mkdir -p $out/share/pixmaps
+  '';
+
   postInstall = ''
     make curses install PREFIX=$out MAKECMDGOALS=curses
   '';
 
   makeFlags = [
-    "PREFIX=$(out)"
+    "PREFIX=$(out) PIXMAPS_DIR=$(out)/share/pixmaps"
   ];
 
   meta = with stdenv.lib; {
