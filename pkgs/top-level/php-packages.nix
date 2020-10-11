bf5ca6bbf382d2dbb7f98ae5ba4238e5e5c884d9
Elis Hirwing <elis@hirwing.se>
2020-10-11 17:33:23 +0200

    phpExtensions.apcu_bc: Move to separate directory

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -64,24 +64,7 @@ in
   extensions = {
     apcu = callPackage ../development/php-packages/apcu { };
 
-    apcu_bc = buildPecl {
-      version = "1.0.5";
-      pname = "apcu_bc";
-
-      sha256 = "0ma00syhk2ps9k9p02jz7rii6x3i2p986il23703zz5npd6y9n20";
-
-      peclDeps = [ php.extensions.apcu ];
-
-      buildInputs = [
-        pcre'
-      ];
-
-      postInstall = ''
-        mv $out/lib/php/extensions/apc.so $out/lib/php/extensions/apcu_bc.so
-      '';
-
-      meta.maintainers = lib.teams.php.members;
-    };
+    apcu_bc = callPackage ../development/php-packages/apcu_bc { };
 
     ast = buildPecl {
       version = "1.0.5";
