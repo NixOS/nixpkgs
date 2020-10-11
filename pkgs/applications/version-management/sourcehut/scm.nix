ef813a7afba3ff1a9bcdacaf5904c69d49ba33c0
Raito Bezarius <masterancpp@gmail.com>
2020-10-11 15:43:48 +0200

    scmsrht: 0.22.3 -> 0.22.4

--- pkgs/applications/version-management/sourcehut/scm.nix
+++ pkgs/applications/version-management/sourcehut/scm.nix
@@ -4,12 +4,12 @@
 
 buildPythonPackage rec {
   pname = "scmsrht";
-  version = "0.22.3";
+  version = "0.22.4";
 
   src = fetchgit {
     url = "https://git.sr.ht/~sircmpwn/scm.sr.ht";
     rev = version;
-    sha256 = "MwgWDYGqOuci55gUFAiKnUfUWgLvQVP5omAMNv/2gPY=";
+    sha256 = "1djyrwa44wml9lj3njy6qbxyc3g1msswbh0y9jyjzxh2c02bl3jn";
   };
 
   nativeBuildInputs = srht.nativeBuildInputs;
