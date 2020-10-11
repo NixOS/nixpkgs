77224fd8e7bea8598acfc758eec7756975380d45
Elis Hirwing <elis@hirwing.se>
2020-10-11 19:10:24 +0200

    phpExtensions.memcached: Move to separate directory

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -82,32 +82,7 @@ in
 
     maxminddb = callPackage ../development/php-packages/maxminddb { };
 
-    memcached = buildPecl rec {
-      version = "3.1.5";
-      pname = "memcached";
-
-      src = fetchgit {
-        url = "https://github.com/php-memcached-dev/php-memcached";
-        rev = "v${version}";
-        sha256 = "01mbh2m3kfbdvih3c8g3g9h4vdd80r0i9g2z8b3lx3mi8mmcj380";
-      };
-
-      internalDeps = [
-        php.extensions.session
-      ] ++ lib.optionals (lib.versionOlder php.version "7.4") [
-        php.extensions.hash
-      ];
-
-      configureFlags = [
-        "--with-zlib-dir=${pkgs.zlib.dev}"
-        "--with-libmemcached-dir=${pkgs.libmemcached}"
-      ];
-
-      nativeBuildInputs = [ pkgs.pkgconfig ];
-      buildInputs = with pkgs; [ cyrus_sasl zlib ];
-
-      meta.maintainers = lib.teams.php.members;
-    };
+    memcached = callPackage ../development/php-packages/memcached { };
 
     mongodb = buildPecl {
       pname = "mongodb";
