f9741399878fc49d2fe8e3d21de3d157b89b489d
Elis Hirwing <elis@hirwing.se>
2020-10-11 19:14:22 +0200

    phpExtensions.mongodb: Move to separate directory

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -84,24 +84,7 @@ in
 
     memcached = callPackage ../development/php-packages/memcached { };
 
-    mongodb = buildPecl {
-      pname = "mongodb";
-      version = "1.6.1";
-
-      sha256 = "1j1w4n33347j9kwvxwsrix3gvjbiqcn1s5v59pp64s536cci8q0m";
-
-      nativeBuildInputs = [ pkgs.pkgconfig ];
-      buildInputs = with pkgs; [
-        cyrus_sasl
-        icu64
-        openssl
-        snappy
-        zlib
-        pcre'
-      ] ++ lib.optional (pkgs.stdenv.isDarwin) pkgs.darwin.apple_sdk.frameworks.Security;
-
-      meta.maintainers = lib.teams.php.members;
-    };
+    mongodb = callPackage ../development/php-packages/mongodb { };
 
     oci8 = buildPecl {
       version = "2.2.0";
