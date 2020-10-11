df08b0fc00f38c95f1bce7cfa06299f948fecd0e
Elis Hirwing <elis@hirwing.se>
2020-10-11 19:05:50 +0200

    phpExtensions.maxminddb: Move to separate directory

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -80,26 +80,7 @@ in
 
     mailparse = callPackage ../development/php-packages/mailparse { };
 
-    maxminddb = buildPecl rec {
-      pname = "maxminddb";
-      version = "1.7.0";
-
-      src = pkgs.fetchFromGitHub {
-        owner = "maxmind";
-        repo = "MaxMind-DB-Reader-php";
-        rev = "v${version}";
-        sha256 = "16msc9s15y43lxw89kj51aldlkd57dc8gms199i51jc984b68ljc";
-      };
-
-      buildInputs = [ pkgs.libmaxminddb ];
-      sourceRoot = "source/ext";
-
-      meta = with pkgs.lib; {
-        description = "C extension that is a drop-in replacement for MaxMind\\Db\\Reader";
-        license = with licenses; [ asl20 ];
-        maintainers = with maintainers; [ ajs124 das_j ] ++ teams.php.members;
-      };
-    };
+    maxminddb = callPackage ../development/php-packages/maxminddb { };
 
     memcached = buildPecl rec {
       version = "3.1.5";
