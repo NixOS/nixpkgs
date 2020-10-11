1f00056a360dd87983bd27d5e91779ebcb15bd6c
Elis Hirwing <elis@hirwing.se>
2020-10-11 19:16:59 +0200

    phpExtensions.oci8: Move to separate directory

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -86,20 +86,7 @@ in
 
     mongodb = callPackage ../development/php-packages/mongodb { };
 
-    oci8 = buildPecl {
-      version = "2.2.0";
-      pname = "oci8";
-
-      sha256 = "0jhivxj1nkkza4h23z33y7xhffii60d7dr51h1czjk10qywl7pyd";
-      buildInputs = [ pkgs.oracle-instantclient ];
-      configureFlags = [ "--with-oci8=shared,instantclient,${pkgs.oracle-instantclient.lib}/lib" ];
-
-      postPatch = ''
-        sed -i -e 's|OCISDKMANINC=`.*$|OCISDKMANINC="${pkgs.oracle-instantclient.dev}/include"|' config.m4
-      '';
-
-      meta.maintainers = lib.teams.php.members;
-    };
+    oci8 = callPackage ../development/php-packages/oci8 { };
 
     pcov = buildPecl {
       version = "1.0.6";
