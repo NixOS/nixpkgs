583bee76855bcccad2f98855c660b2fd3a13a54c
Elis Hirwing <elis@hirwing.se>
2020-10-11 19:00:38 +0200

    phpExtensions.mailparse: Move to separate directory

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -78,18 +78,7 @@ in
 
     imagick = callPackage ../development/php-packages/imagick { };
 
-    mailparse = buildPecl {
-      version = "3.0.3";
-      pname = "mailparse";
-      sha256 = "00nk14jbdbln93mx3ag691avc11ff94hkadrcv5pn51c6ihsxbmz";
-
-      internalDeps = [ php.extensions.mbstring ];
-      postConfigure = ''
-        echo "#define HAVE_MBSTRING 1" >> config.h
-      '';
-
-      meta.maintainers = lib.teams.php.members;
-    };
+    mailparse = callPackage ../development/php-packages/mailparse { };
 
     maxminddb = buildPecl rec {
       pname = "maxminddb";
