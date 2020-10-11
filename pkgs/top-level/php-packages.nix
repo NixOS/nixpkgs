a38bb40902b6f313c36a303324703a06e6e86558
Elis Hirwing <elis@hirwing.se>
2020-10-11 16:46:30 +0200

    phpPackages.psalm: Move to separate directory

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -51,32 +51,7 @@ in
 
     phpstan = callPackage ../development/php-packages/phpstan { };
 
-    psalm = mkDerivation rec {
-      version = "3.11.2";
-      pname = "psalm";
-
-      src = pkgs.fetchurl {
-        url = "https://github.com/vimeo/psalm/releases/download/${version}/psalm.phar";
-        sha256 = "1ani0907whqy2ycr01sjlvrmwps4dg5igim8z1qyv8grhwvw6gb0";
-      };
-
-      phases = [ "installPhase" ];
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-
-      installPhase = ''
-        mkdir -p $out/bin
-        install -D $src $out/libexec/psalm/psalm.phar
-        makeWrapper ${php}/bin/php $out/bin/psalm \
-          --add-flags "$out/libexec/psalm/psalm.phar"
-      '';
-
-      meta = with pkgs.lib; {
-        description = "A static analysis tool for finding errors in PHP applications";
-        license = licenses.mit;
-        homepage = "https://github.com/vimeo/psalm";
-        maintainers = teams.php.members;
-      };
-    };
+    psalm = callPackage ../development/php-packages/psalm { };
 
     psysh = mkDerivation rec {
       version = "0.10.3";
