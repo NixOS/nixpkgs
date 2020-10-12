e67d5b72efd6972464f65d83f3747ded2e1240ad
Maximilian Bosch <maximilian@mbosch.me>
2020-10-12 16:41:11 +0200

    element-desktop: 1.7.8 -> 1.7.9
    https://github.com/vector-im/element-desktop/releases/tag/v1.7.9

--- pkgs/applications/networking/instant-messengers/element/element-desktop.nix
+++ pkgs/applications/networking/instant-messengers/element/element-desktop.nix
@@ -8,12 +8,12 @@
 
 let
   executableName = "element-desktop";
-  version = "1.7.8";
+  version = "1.7.9";
   src = fetchFromGitHub {
     owner = "vector-im";
     repo = "riot-desktop";
     rev = "v${version}";
-    sha256 = "04pfx8knixwlz6s7yxcbff6hpyq75hd4wyj9npil6pcmkxglns5l";
+    sha256 = "0hk9y6zzabfdi35fqzl9987d98a6k74fi0mzmhhvhvvpj8i1ldrn";
   };
   electron = electron_9;
 
