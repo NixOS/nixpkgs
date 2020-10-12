1942dda09bbafafeb9014e70e837eb31178a75a5
Vincent Laporte <Vincent.Laporte@gmail.com>
2020-10-12 13:30:57 +0200

    ocamlPackages.ppx_sqlexpr: use ppxlib instead of the deprecated ppx_core

--- pkgs/development/ocaml-modules/sqlexpr/ppx.nix
+++ pkgs/development/ocaml-modules/sqlexpr/ppx.nix
@@ -1,11 +1,15 @@
 { buildDunePackage, sqlexpr, ounit
-, ppx_core, ppx_tools_versioned, re, lwt_ppx
+, ppxlib, ppx_tools_versioned, re, lwt_ppx
 }:
 
 buildDunePackage {
   pname = "ppx_sqlexpr";
   inherit (sqlexpr) version src meta;
 
-  buildInputs = [ sqlexpr ounit ppx_core ppx_tools_versioned re lwt_ppx ];
+  postPatch = ''
+    substituteInPlace src/ppx/jbuild --replace ppx_core ppxlib
+  '';
+
+  buildInputs = [ sqlexpr ounit ppxlib ppx_tools_versioned re lwt_ppx ];
   doCheck = true;
 }
