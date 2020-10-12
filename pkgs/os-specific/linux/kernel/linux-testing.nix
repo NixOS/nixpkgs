bd0df8ec2101f28555a4327ba9e175f6bc4ef9ae
Sridhar Ratnakumar <srid@srid.ca>
2020-10-12 01:18:43 -0400

    linux: 5.9-rc8 -> 5.9
    https://lwn.net/Articles/833996/

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.9-rc8";
+  version = "5.9";
   extraMeta.branch = "5.9";
 
   # modDirVersion needs to be x.y.z, will always add .0
@@ -11,7 +11,7 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "0yhjanmrg0cjmdn7yq8nx0h7q3aq9cimqhn9k4nis1a976p8wpgw";
+    sha256 = "0z7mmbwghhd93rs7b51lw71zbny109bmvc0f6r475yjl2sc5g2r3";
   };
 
   # Should the testing kernels ever be built on Hydra?
