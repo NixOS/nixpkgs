3f3491d64d24e87534a11d0f78e2ffea13150749
Aaron Janse <aaron@ajanse.me>
2020-10-12 18:41:13 -0700

    fix some whitespace

--- pkgs/build-support/rust/default.nix
+++ pkgs/build-support/rust/default.nix
@@ -75,7 +75,7 @@ let
     else ''
       cargoDepsCopy="$sourceRoot/${cargoVendorDir}"
     '';
-  
+
   targetIsJSON = stdenv.lib.hasSuffix ".json" target;
 
   # see https://github.com/rust-lang/cargo/blob/964a16a28e234a3d397b2a7031d4ab4a428b1391/src/cargo/core/compiler/compile_kind.rs#L151-L168
