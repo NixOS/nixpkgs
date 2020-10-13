1467fe6fcc2390928979fe6a1398855b121c14eb
Nicolas Martin <penguwin@penguwin.eu>
2020-10-13 02:22:05 +0200

    duf: 0.3.1 -> 0.4.0

--- pkgs/tools/misc/duf/default.nix
+++ pkgs/tools/misc/duf/default.nix
@@ -2,22 +2,26 @@
 
 buildGoModule rec {
   pname = "duf";
-  version = "0.3.1";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "muesli";
     repo = "duf";
     rev = "v${version}";
-    sha256 = "1y27cc818099drccqg57nq4kcgl2zmg6akxv2k1c8rkz932q718f";
+    sha256 = "093kir1zwpkl0jic4p1f032ir5k7kra94di2indzn0fa4j4r7a0i";
   };
 
-  vendorSha256 = "1jmj46yami37r2wmiprpwyljcmj7dir9mcccx5is1jbiai6sx79i";
+  dontStrip = true;
+
+  vendorSha256 = "1jqilfsirj7bkhzywimzf98w2b4s777phb06nsw6lr3bi6nnwzr1";
+
+  buildFlagsArray = [ "-ldflags=" "-X=main.Version=${version}" ];
 
   meta = with lib; {
     homepage = "https://github.com/muesli/duf/";
     description = "Disk Usage/Free Utility";
     license = licenses.mit;
     platforms = platforms.linux;
-    maintainers = with maintainers; [ petabyteboy ];
+    maintainers = with maintainers; [ petabyteboy penguwin ];
   };
 }
