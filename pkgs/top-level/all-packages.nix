87056f5fa18af3253c739ce89f64a72c3a982832
AndersonTorres <torres.anderson.85@protonmail.com>
2020-10-13 21:33:37 -0300

    unqlite: init at 1.1.9

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15548,6 +15548,8 @@ in
 
   sqlite = lowPrio (callPackage ../development/libraries/sqlite { });
 
+  unqlite = lowPrio (callPackage ../development/libraries/unqlite { });
+
   inherit (callPackage ../development/libraries/sqlite/tools.nix {
     inherit (darwin.apple_sdk.frameworks) Foundation;
   }) sqlite-analyzer sqldiff;
