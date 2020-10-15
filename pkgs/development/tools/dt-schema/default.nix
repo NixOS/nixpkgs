0b58c6c4c4c6e45b33f0b92c2b691370b9594eb7
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-15 07:51:57 +0000

    dt-schema: 2020.6 -> 2020.8.1

--- pkgs/development/tools/dt-schema/default.nix
+++ pkgs/development/tools/dt-schema/default.nix
@@ -11,11 +11,11 @@
 
 buildPythonPackage rec {
   pname = "dtschema";
-  version = "2020.6";
+  version = "2020.8.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1zdm0zbn1dfk02yqghfvd0nb26hmzivb6mln6bvxjfdcv6n7pdqf";
+    sha256 = "5c98202abb4977aac6a2995a7f4ed2f7e51739db6fd72861d29681f865c27c1b";
   };
 
   nativeBuildInputs = [ setuptools_scm git ];
