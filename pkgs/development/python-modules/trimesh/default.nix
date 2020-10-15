8d11ae53c2cf9bf66d3be3df7c9f699ce0220954
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-15 08:48:54 +0000

    python37Packages.trimesh: 3.8.10 -> 3.8.11

--- pkgs/development/python-modules/trimesh/default.nix
+++ pkgs/development/python-modules/trimesh/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "trimesh";
-  version = "3.8.10";
+  version = "3.8.11";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "24fb0f7f8b8745e181b215a294eb1b22acd3e739d757892394d4c32f74e5c069";
+    sha256 = "790b4804227825a985189e0788cf57dbfcf1ee39f30f767176722ad572da27de";
   };
 
   propagatedBuildInputs = [ numpy ];
