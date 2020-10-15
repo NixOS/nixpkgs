e7a0ecfdc67aebd63d659b82203d1cc22f5f1723
Jonathan Ringer <jonringer117@gmail.com>
2020-10-15 14:33:57 -0700

    python3Packages.pynacl: 1.3.0 -> 1.4.0

--- pkgs/development/python-modules/pynacl/default.nix
+++ pkgs/development/python-modules/pynacl/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "pynacl";
-  version = "1.3.0";
+  version = "1.4.0";
 
   src = fetchPypi {
     inherit version;
     pname = "PyNaCl";
-    sha256 = "0c6100edd16fefd1557da078c7a31e7b7d7a52ce39fdca2bec29d4f7b6e7600c";
+    sha256 = "01b56hxrbif3hx8l6rwz5kljrgvlbj7shmmd2rjh0hn7974a5sal";
   };
 
   checkInputs = [ pytest hypothesis_4 ];
@@ -24,19 +24,9 @@ buildPythonPackage rec {
 
   SODIUM_INSTALL = "system";
 
-  # fixed in next release 1.3.0+
-  # https://github.com/pyca/pynacl/pull/480
-  postPatch = ''
-    substituteInPlace tests/test_bindings.py \
-      --replace "average_size=128," ""
-  '';
-
   checkPhase = ''
     py.test
   '';
-
-  # https://github.com/pyca/pynacl/issues/550
-  PYTEST_ADDOPTS = "-k 'not test_wrong_types'";
 
   meta = with stdenv.lib; {
     maintainers = with maintainers; [ va1entin ];
