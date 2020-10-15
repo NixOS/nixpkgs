846266d9af5fa0435693f30bca36f80ad1f6d8ad
AndersonTorres <torres.anderson.85@protonmail.com>
2020-10-14 22:05:19 -0300

    corectrl: init at 1.1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20126,6 +20126,8 @@ in
 
   copyq = libsForQt514.callPackage ../applications/misc/copyq { };
 
+  corectrl = libsForQt5.callPackage ../applications/misc/corectrl { };
+
   coriander = callPackage ../applications/video/coriander {
     inherit (gnome2) libgnomeui GConf;
   };
