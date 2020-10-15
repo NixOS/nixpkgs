0d73a98650af5507530c1cfd7791fc9181ce3288
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-15 21:32:51 +0000

    python37Packages.pyshp: 2.1.0 -> 2.1.2

--- pkgs/development/python-modules/pyshp/default.nix
+++ pkgs/development/python-modules/pyshp/default.nix
@@ -2,12 +2,12 @@
 , setuptools }:
 
 buildPythonPackage rec {
-  version = "2.1.0";
+  version = "2.1.2";
   pname = "pyshp";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1h75a5fisqqj48m6wq7jhdxv6arjg3mvnr5q404pvfbjscj7yp76";
+    sha256 = "a0aa668cd0fc09b873f10facfe96971c0496b7fe4f795684d96cc7306ac5841c";
   };
 
   buildInputs = [ setuptools ];
