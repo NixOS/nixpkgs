1dc012e0ea6c6e78ed8dd737f982080bb9472e24
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-10-15 12:00:25 +0200

    pythonPackages.afdko: 3.5.0 → 3.5.1

--- pkgs/development/python-modules/afdko/default.nix
+++ pkgs/development/python-modules/afdko/default.nix
@@ -1,21 +1,31 @@
-{ stdenv, buildPythonPackage, fetchPypi, pythonOlder, python
+{ stdenv, buildPythonPackage, fetchPypi, fetchpatch, pythonOlder, python
 , fonttools, defcon, lxml, fs, unicodedata2, zopfli, brotlipy, fontpens
 , brotli, fontmath, mutatormath, booleanoperations
-, ufoprocessor, ufonormalizer, psautohint
+, ufoprocessor, ufonormalizer, psautohint, tqdm
 , setuptools_scm
 , pytest
 }:
 
 buildPythonPackage rec {
   pname = "afdko";
-  version = "3.5.0";
+  version = "3.5.1";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0wid4l70bxm297xgayyrgw5glhp6n92gh4sz1nd4rncgf1ziz8ck";
+    sha256 = "1qg7dgl81yq0sp50pkhgvmf8az1svx20zmpkfa68ka9d0ssh1wjw";
   };
+
+  # Skip date-dependent test. See
+  # https://github.com/adobe-type-tools/afdko/pull/1232
+  # https://github.com/NixOS/nixpkgs/pull/98158#issuecomment-704321117
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/adobe-type-tools/afdko/commit/2c36ad10f9d964759f643e8ed7b0972a27aa26bd.patch";
+      sha256 = "0p6a485mmzrbfldfbhgfghsypfiad3cabcw7qlw2rh993ivpnibf";
+    })
+  ];
 
   nativeBuildInputs = [ setuptools_scm ];
 
@@ -35,6 +45,7 @@ buildPythonPackage rec {
     ufoprocessor
     ufonormalizer
     psautohint
+    tqdm
   ];
 
   # tests are broken on non x86_64
