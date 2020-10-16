97642e84ede80e1a508f3f20adb44b0d5c4e796e
Mario Rodas <marsam@users.noreply.github.com>
2020-10-15 19:50:00 -0500

    nodejs-14_x: 14.13.1 -> 14.14.0
    https://github.com/nodejs/node/releases/tag/v14.14.0

--- pkgs/development/web/nodejs/v14.nix
+++ pkgs/development/web/nodejs/v14.nix
@@ -8,6 +8,6 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "14.13.1";
-    sha256 = "1xhmdhzsk3pfl4k8l0ipd9a1v5z807sl65mwkw51y7lc44gbsqb0";
+    version = "14.14.0";
+    sha256 = "1rphkl3lqg0rzgg0r58bfv1hjw3rg96qvqcilk2927kbpps3bs84";
   }
