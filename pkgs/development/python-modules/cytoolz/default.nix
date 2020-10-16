ee8db0e84fb1c7c8d77f5bd12e30bf94187e192a
Jonathan Ringer <jonringer117@gmail.com>
2020-10-15 23:54:52 -0700

    python2Packages.cytoolz: disable
    no longer compatibled with python2
    
    ```
        File "cytoolz/itertoolz.pyx", line 15, in init cytoolz.itertoolz
          from itertools import chain, islice, zip_longest
      ImportError: cannot import name zip_longest
    ```

--- pkgs/development/python-modules/cytoolz/default.nix
+++ pkgs/development/python-modules/cytoolz/default.nix
@@ -6,19 +6,18 @@
 , toolz
 , python
 , fetchpatch
+, isPy27
 }:
 
 buildPythonPackage rec {
   pname = "cytoolz";
   version = "0.11.0";
+  disabled = isPy27 || isPyPy;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "c64f3590c3eb40e1548f0d3c6b2ccde70493d0b8dc6cc7f9f3fec0bb3dcd4222";
   };
-
-  # Extension types
-  disabled = isPyPy;
 
   checkInputs = [ nose ];
   propagatedBuildInputs = [ toolz ];
