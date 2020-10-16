667f164f0881e213032e0e4a00ccb4a77cedf4d0
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-16 17:29:55 +0000

    python37Packages.azure-mgmt-sql: 0.21.0 -> 0.22.0

--- pkgs/development/python-modules/azure-mgmt-sql/default.nix
+++ pkgs/development/python-modules/azure-mgmt-sql/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "azure-mgmt-sql";
-  version = "0.21.0";
+  version = "0.22.0";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "7bf7a41d2e42532a00b5f41d3fc0512244451a35f038ac0a25c96ef2c5c04300";
+    sha256 = "01c82a1559f49e5f51021b8fbd35e738e08cbede1f39cf00da8085c853575f39";
   };
 
   propagatedBuildInputs = [
