17861b0d2233ffca727f1bf3017d0cd737aaa24b
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-16 12:58:35 +0000

    python37Packages.kubernetes: 11.0.0 -> 12.0.0

--- pkgs/development/python-modules/kubernetes/default.nix
+++ pkgs/development/python-modules/kubernetes/default.nix
@@ -4,7 +4,7 @@
 
 buildPythonPackage rec {
   pname = "kubernetes";
-  version = "11.0.0";
+  version = "12.0.0";
 
   prePatch = ''
     sed -e 's/sphinx>=1.2.1,!=1.3b1,<1.4 # BSD/sphinx/' -i test-requirements.txt
@@ -24,7 +24,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1a2472f8b01bc6aa87e3a34781f859bded5a5c8ff791a53d889a8bd6cc550430";
+    sha256 = "72f095a1cd593401ff26b3b8d71749340394ca6d8413770ea28ce18efd5bcf4c";
   };
 
   checkInputs = [ isort coverage pytest mock sphinx autopep8 pep8 codecov recommonmark nose ];
