0c323896620511ffcb415f3beab8c05257dc3321
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-16 05:57:43 +0000

    python37Packages.numcodecs: 0.6.4 -> 0.7.2

--- pkgs/development/python-modules/numcodecs/default.nix
+++ pkgs/development/python-modules/numcodecs/default.nix
@@ -12,11 +12,11 @@
 
 buildPythonPackage rec {
   pname = "numcodecs";
-  version = "0.6.4";
+  version = "0.7.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ef4843d5db4d074e607e9b85156835c10d006afc10e175bda62ff5412fca6e4d";
+    sha256 = "4a038064d5604e6181a64db668d7b700d9ae87e4041984c04cbf0042469664b0";
   };
 
   nativeBuildInputs = [
