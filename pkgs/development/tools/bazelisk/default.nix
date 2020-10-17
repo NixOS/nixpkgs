7218bd1506066a13559b7d67aa546e740ac6e69d
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-17 17:41:33 +0000

    bazelisk: 1.7.1 -> 1.7.2

--- pkgs/development/tools/bazelisk/default.nix
+++ pkgs/development/tools/bazelisk/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "bazelisk";
-  version = "1.7.1";
+  version = "1.7.2";
 
   src = fetchFromGitHub {
     owner = "bazelbuild";
     repo = pname;
     rev = "v${version}";
-    sha256 = "18akakh9bnpn0sngxar9f0r9hhx7dkd8y6q4j16x2d193gcw53c7";
+    sha256 = "0psqhv2cm2xwjyivaza2s6x780q6yjn1nsjdy538zjky22dazqq4";
   };
 
   vendorSha256 = "116wy1a7gmi2w8why9hszhcybfvpwp4iq62vshb25cdcma6q4mjh";
