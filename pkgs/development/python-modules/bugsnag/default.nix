264f308aa6a0e0176c731ad2bc4b428463ddc430
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-17 01:49:53 +0000

    python37Packages.bugsnag: 3.7.1 -> 4.0.1

--- pkgs/development/python-modules/bugsnag/default.nix
+++ pkgs/development/python-modules/bugsnag/default.nix
@@ -7,11 +7,11 @@
 
 buildPythonPackage rec {
   pname = "bugsnag";
-  version = "3.7.1";
+  version = "4.0.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "32966bfe625ec6fc0dbc9d86d79a18f31b22b2fdec3ca070eeb3495304f7e18d";
+    sha256 = "01c2186f6c2a6f801b66d8fc73b8986bd2d4931a6ab40b720e5fd0b66757facc";
   };
 
   propagatedBuildInputs = [ six webob ];
