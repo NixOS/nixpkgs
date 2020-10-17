5d1b7e310ecdffe9f599a12e0b4f9808c3525406
zowoq <59103226+zowoq@users.noreply.github.com>
2020-10-18 08:25:36 +1000

    python3Packages.aubio: fix eval

--- pkgs/development/python-modules/aubio/default.nix
+++ pkgs/development/python-modules/aubio/default.nix
@@ -29,7 +29,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "a library for audio and music analysis";
     homepage = "https://aubio.org";
-    licenses = license.gpl3;
+    licenses = licenses.gpl3;
     maintainers = with maintainers; [ hexa ];
   };
 }
