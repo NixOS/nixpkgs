8c6b2f5b5336aae3f4d51f80cc88fd1558fffda6
Ryan Burns <rtburns@protonmail.com>
2020-10-17 14:22:46 -0700

    cairo: fix build on darwin

--- pkgs/development/libraries/cairo/default.nix
+++ pkgs/development/libraries/cairo/default.nix
@@ -35,6 +35,9 @@ in stdenv.mkDerivation rec {
       url    = "https://gitlab.freedesktop.org/cairo/cairo/commit/6edf572ebb27b00d3c371ba5ae267e39d27d5b6d.patch";
       sha256 = "112hgrrsmcwxh1r52brhi5lksq4pvrz4xhkzcf2iqp55jl2pb7n1";
     })
+  ] ++ optionals stdenv.hostPlatform.isDarwin [
+    # Workaround https://gitlab.freedesktop.org/cairo/cairo/-/issues/121
+    ./skip-configure-stderr-check.patch
   ];
 
   outputs = [ "out" "dev" "devdoc" ];
