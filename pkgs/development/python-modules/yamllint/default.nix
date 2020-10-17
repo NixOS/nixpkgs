3a97a73454cde7e615dd63658f1595ba0a815657
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-17 04:46:53 +0000

    python37Packages.yamllint: 1.24.2 -> 1.25.0

--- pkgs/development/python-modules/yamllint/default.nix
+++ pkgs/development/python-modules/yamllint/default.nix
@@ -3,11 +3,11 @@
 
 buildPythonPackage rec {
   pname = "yamllint";
-  version = "1.24.2";
+  version = "1.25.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "07xn11i0c7x72xjxkkzrq9zxl40vfdr41mfvhlayrk6dpbk8vdj0";
+    sha256 = "b1549cbe5b47b6ba67bdeea31720f5c51431a4d0c076c1557952d841f7223519";
   };
 
   checkInputs = [ nose ];
