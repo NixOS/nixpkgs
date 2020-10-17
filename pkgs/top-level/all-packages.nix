1010c72b02aad687f0a51f1f184b3a8f6468fdde
marius david <mariusdavid@laposte.net>
2020-10-17 10:50:51 +0200

    portmod: init at 2.0_beta9

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25345,6 +25345,8 @@ in
 
   openmw-tes3mp = libsForQt5.callPackage ../games/openmw/tes3mp.nix { };
 
+  portmod = callPackage ../games/portmod { };
+
   tr-patcher = callPackage ../games/tr-patcher { };
 
   tes3cmd = callPackage ../games/tes3cmd { };
