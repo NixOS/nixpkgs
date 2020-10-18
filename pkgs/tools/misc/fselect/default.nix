01e30308fc242d1805af571a8846996b7f654d8e
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-18 08:18:49 +0000

    fselect: 0.6.10 -> 0.7.1

--- pkgs/tools/misc/fselect/default.nix
+++ pkgs/tools/misc/fselect/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "fselect";
-  version = "0.6.10";
+  version = "0.7.1";
 
   src = fetchFromGitHub {
     owner = "jhspetersson";
     repo = "fselect";
     rev = version;
-    sha256 = "17dz0qj2981plvzp72yisyrjnyz1sy3pqyvhx76ws2754vjgq4ra";
+    sha256 = "1q7y5agsi6wjb1dnyvdhm4qmdhpv30cx5a8m1blks8is9z2bblz0";
   };
 
-  cargoSha256 = "19b05gx717xjg4arn4zgrqh7ckzgzx0ygg9gkfzsg7phz7f01626";
+  cargoSha256 = "0r2zj0dvf6h4ph3b75z2rdlqwzkdjrjj2iad4dbf9nsr63giwd9n";
 
   nativeBuildInputs = [ installShellFiles ];
 
