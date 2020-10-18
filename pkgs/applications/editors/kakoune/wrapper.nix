8270da38a4d99ad757d1f70e9278f07fa3750f88
Daniel Gorin <jcpetruzza@gmail.com>
2020-10-18 16:19:09 +0100

    kakoune: Fix :doc when using plugins
    So, kakoune takes the documentation shown for `:doc` from
    `$KAKOUNE_RUNTIME/share/kak/doc`. Unfortunately, it seems
    that it will ignore files that are symlinks. This is arguably
    a bug in kakoune, we workaround it for now by copying the
    content of the docfiles.

--- pkgs/applications/editors/kakoune/wrapper.nix
+++ pkgs/applications/editors/kakoune/wrapper.nix
@@ -18,6 +18,13 @@ in
       # unless explicitly overriden with KAKOUNE_RUNTIME
       rm "$out/bin/kak"
       makeWrapper "${kakoune}/bin/kak" "$out/bin/kak" --set KAKOUNE_RUNTIME "$out/share/kak"
+
+      # currently kakoune ignores doc files if they are symlinks, so workaround by
+      # copying doc files over, so they become regular files...
+      mkdir "$out/DELETE_ME"
+      mv "$out/share/kak/doc" "$out/DELETE_ME"
+      cp -r --dereference "$out/DELETE_ME/doc" "$out/share/kak"
+      rm -Rf "$out/DELETE_ME"
     '';
 
     meta = kakoune.meta // { priority = (kakoune.meta.priority or 0) - 1; };
