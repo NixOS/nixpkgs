878edb26e3ad3fe3cb7c36960a08d4118daf01a1
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-18 02:09:19 +0000

    doctl: 1.46.0 -> 1.48.1

--- pkgs/development/tools/doctl/default.nix
+++ pkgs/development/tools/doctl/default.nix
@@ -2,7 +2,7 @@
 
 buildGoModule rec {
   pname = "doctl";
-  version = "1.46.0";
+  version = "1.48.1";
 
   vendorSha256 = null;
 
@@ -32,7 +32,7 @@ buildGoModule rec {
     owner = "digitalocean";
     repo = "doctl";
     rev = "v${version}";
-    sha256 = "1f9gw1qjannswx1vy64a5a2cfr8azsci241pk0xhrhk6aqpjzx1n";
+    sha256 = "1bykvwv1zhiki7c86hycwck78s8gqw4dzd7smfp4iz609wyk1wqb";
   };
 
   meta = with lib; {
