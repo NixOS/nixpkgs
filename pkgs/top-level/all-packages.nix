b58e90bd25ca5f0a30713d89260bc89b8453e66f
Gabriel Ebner <gebner@gebner.org>
2020-10-18 15:12:12 +0200

    freecadStable: remove
    It depends on the insecure qtwebkit package.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20863,11 +20863,6 @@ in
   freecad = libsForQt5.callPackage ../applications/graphics/freecad {
     mpi = openmpi;
   };
-  freecadStable = libsForQt5.callPackage ../applications/graphics/freecad/stable.nix {
-    mpi = openmpi;
-    opencascade-occt = opencascade-occt730;
-    python3Packages = python37Packages;
-  };
 
   freemind = callPackage ../applications/misc/freemind {
     jdk = jdk8; # TODO: remove override https://github.com/NixOS/nixpkgs/pull/89731
