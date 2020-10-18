1d65c5366544db3581c1d4799a308eb03c9d8430
Ivan Tham <pickfire@riseup.net>
2020-10-18 17:41:39 +0800

    gtk+: refactor name
    Use newer pname + version instead of name, I will be using this
    when packaging lxde.

--- pkgs/development/libraries/gtk/2.x.nix
+++ pkgs/development/libraries/gtk/2.x.nix
@@ -13,10 +13,11 @@ assert cupsSupport -> cups != null;
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
-  name = "gtk+-2.24.32";
+  pname = "gtk+";
+  version = "2.24.32";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gtk+/2.24/${name}.tar.xz";
+    url = "mirror://gnome/sources/gtk+/2.24/${pname}-${version}.tar.xz";
     sha256 = "b6c8a93ddda5eabe3bfee1eb39636c9a03d2a56c7b62828b359bf197943c582e";
   };
 
