583f986c8dd5e27eb3aa523dfac443c3082da119
Jack Kelly <jack@jackkelly.name>
2020-10-20 09:25:07 +1000

    python3Packages.aws-lambda-builders: 0.9.0 -> 1.1.0

--- pkgs/development/python-modules/aws-lambda-builders/default.nix
+++ pkgs/development/python-modules/aws-lambda-builders/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "aws-lambda-builders";
-  version = "0.9.0";
+  version = "1.1.0";
 
   # No tests available in PyPI tarball
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = "aws-lambda-builders";
     rev = "v${version}";
-    sha256 = "0cgb0hwf4xg5dmm32wwlxqy7a77jw6gpnj7v8rq5948hsy2sfrcp";
+    sha256 = "1r4939m5k1nj6l9bv972z4fkmkl0z5f5r29bq7588yk113kkqr0c";
   };
 
   # Package is not compatible with Python 3.5
