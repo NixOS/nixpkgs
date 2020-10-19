a2937f322b9fadae149cf44b2641afe706e209df
Jonathan Ringer <jonringer117@gmail.com>
2020-10-18 18:44:32 -0700

    konquest: use qtquickcontrols2

--- pkgs/applications/kde/konquest.nix
+++ pkgs/applications/kde/konquest.nix
@@ -4,13 +4,13 @@
 , kdoctools
 , kdelibs4support
 , libkdegames
-, qtquickcontrols
+, qtquickcontrols2
 }:
 
 mkDerivation {
   name = "konquest";
   nativeBuildInputs = [ extra-cmake-modules kdoctools ];
-  buildInputs = [ kdelibs4support libkdegames qtquickcontrols ];
+  buildInputs = [ kdelibs4support libkdegames qtquickcontrols2 ];
   meta = {
     license = with lib.licenses; [ gpl2 ];
     maintainers = with lib.maintainers; [ lheckemann ];
