1305cc40feda4de62ed8c3af0abc7c1f19af662b
Joseph Long <me@joseph-long.com>
2020-10-19 20:31:06 -0700

    vagrant-guest: use security.sudo.wheelNeedsPassword instead of configFile

--- nixos/modules/virtualisation/vagrant-guest.nix
+++ nixos/modules/virtualisation/vagrant-guest.nix
@@ -31,13 +31,5 @@
     ];
   };
 
-  security.sudo.configFile = ''
-      Defaults:root,%wheel env_keep+=LOCALE_ARCHIVE
-      Defaults:root,%wheel env_keep+=NIX_PATH
-      Defaults:root,%wheel env_keep+=TERMINFO_DIRS
-      Defaults env_keep+=SSH_AUTH_SOCK
-      Defaults lecture = never
-      root   ALL=(ALL) SETENV: ALL
-      %wheel ALL=(ALL) NOPASSWD: ALL, SETENV: ALL
-    '';
+  security.sudo.wheelNeedsPassword = false;
 }
