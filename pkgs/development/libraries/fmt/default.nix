67f70f9bcfce7f5435f43201254469b5246139c2
Samuel Gräfenstein <git@samuelgrf.com>
2020-10-21 13:38:53 +0200

    fmt: 6.2.1 -> 7.0.3

--- pkgs/development/libraries/fmt/default.nix
+++ pkgs/development/libraries/fmt/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "fmt";
-  version = "6.2.1";
+  version = "7.0.3";
 
   outputs = [ "out" "dev" ];
 
@@ -10,31 +10,8 @@ stdenv.mkDerivation rec {
     owner = "fmtlib";
     repo = "fmt";
     rev = version;
-    sha256 = "1i6nfxazq4d05r3sxyc3ziwkqq7s8rdbv9p16afv66aqmsbqqqic";
+    sha256 = "17q2fdzakk5p0s3fx3724gs5k2b5ylp8f1d6j2m3wgvlfldx9k9a";
   };
-
-  patches = [
-    # Fix BC break breaking Kodi
-    # https://github.com/xbmc/xbmc/issues/17629
-    # https://github.com/fmtlib/fmt/issues/1620
-    (fetchpatch {
-      url = "https://github.com/fmtlib/fmt/commit/7d01859ef16e6b65bc023ad8bebfedecb088bf81.patch";
-      sha256 = "0v8hm5958ih1bmnjr16fsbcmdnq4ykyf6b0hg6dxd5hxd126vnxx";
-    })
-
-    # Fix paths in pkg-config file
-    # https://github.com/fmtlib/fmt/pull/1657
-    (fetchpatch {
-      url = "https://github.com/fmtlib/fmt/commit/78f041ab5b40a1145ba686aeb8013e8788b08cd2.patch";
-      sha256 = "1hqp96zl9l3qyvsm7pxl6ah8c26z035q2mz2pqhqa0wvzd1klcc6";
-    })
-
-    # Fix cmake config paths.
-    (fetchpatch {
-      url = "https://github.com/fmtlib/fmt/pull/1702.patch";
-      sha256 = "18cadqi7nac37ymaz3ykxjqs46rvki396g6qkqwp4k00cmic23y3";
-    })
-  ];
 
   nativeBuildInputs = [ cmake ];
 
