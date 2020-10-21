5681936aa96f57f769f43c3bd2ded64113870e6f
Matt Wittmann <mcwitt@gmail.com>
2020-10-20 19:49:24 -0700

    vscode-extensions.dhall.vscode-dhall-lsp-server: init at 0.0.4

--- pkgs/misc/vscode-extensions/default.nix
+++ pkgs/misc/vscode-extensions/default.nix
@@ -58,6 +58,16 @@ in
     meta = { license = stdenv.lib.licenses.mit; };
   };
 
+  dhall.vscode-dhall-lsp-server = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "vscode-dhall-lsp-server";
+      publisher = "dhall";
+      version = "0.0.4";
+      sha256 = "1zin7s827bpf9yvzpxpr5n6mv0b5rhh3civsqzmj52mdq365d2js";
+    };
+    meta = { license = stdenv.lib.licenses.mit; };
+  };
+
   formulahendry.auto-close-tag = buildVscodeMarketplaceExtension {
     mktplcRef = {
       name = "auto-close-tag";
