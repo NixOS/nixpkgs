f4ed4a324d09d524ccf1e96c701bec64b493307a
Nick Braga <nick.braga@bookbub.com>
2020-10-21 12:28:06 -0400

    ssm-session-manager-plugin: add support for x86_64-darwin

--- pkgs/applications/networking/cluster/ssm-session-manager-plugin/default.nix
+++ pkgs/applications/networking/cluster/ssm-session-manager-plugin/default.nix
@@ -1,29 +1,47 @@
-{ stdenv, lib, fetchurl, autoPatchelfHook, dpkg, awscli }:
+{ stdenv, lib, fetchurl, fetchzip, autoPatchelfHook, dpkg, awscli }:
 stdenv.mkDerivation rec {
   pname = "ssm-session-manager-plugin";
   version = "1.1.61.0";
 
-  src = fetchurl {
-    url =
-      "https://s3.amazonaws.com/session-manager-downloads/plugin/${version}/ubuntu_64bit/session-manager-plugin.deb";
-    sha256 = "0z59irrpwhjjhn379454xyraqs590hij2n6n6k25w5hh8ak7imfl";
-  };
+  base_url = "https://s3.amazonaws.com/session-manager-downloads/plugin";
 
-  nativeBuildInputs = [ autoPatchelfHook dpkg ];
+  src = if stdenv.isDarwin then
+    fetchzip rec {
+      name = "sessionmanager-bundle.zip";
+      url = "${base_url}/${version}/mac/${name}";
+      sha256 = "0i5kc9wkn3dv4jbkbmjqf4w581i2n2sfqi42gm3vq28i7dwf0axz";
+    }
+  else
+    fetchurl {
+      url = "${base_url}/${version}/ubuntu_64bit/session-manager-plugin.deb";
+      sha256 = "0z59irrpwhjjhn379454xyraqs590hij2n6n6k25w5hh8ak7imfl";
+    };
+
+  nativeBuildInputs = stdenv.lib.optionals stdenv.hostPlatform.isLinux [
+    autoPatchelfHook
+    dpkg
+  ];
 
   buildInputs = [ awscli ];
 
-  unpackPhase = "dpkg-deb -x $src .";
+  unpackPhase = stdenv.lib.optionalString stdenv.hostPlatform.isLinux ''
+    dpkg-deb -x $src .
+  '';
 
-  installPhase =
-    "install -m755 -D usr/local/sessionmanagerplugin/bin/session-manager-plugin $out/bin/session-manager-plugin";
+  installPhase = if stdenv.isDarwin then ''
+    ./install -i $out -b $out/bin/${pname}
+    addToSearchPath _PATH $out/bin
+  '' else ''
+    install -m755 -D usr/local/sessionmanagerplugin/bin/session-manager-plugin $out/bin/session-manager-plugin
+  '';
 
   meta = with lib; {
     homepage =
       "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html";
     description = "Amazon SSM Session Manager Plugin";
-    platforms = [ "x86_64-linux" ];
+    platforms = [ "x86_64-linux" "x86_64-darwin" ];
     license = licenses.unfree;
+    changelog = "https://docs.aws.amazon.com/systems-manager/${version}/userguide/session-manager-working-with-install-plugin.html#plugin-version-history";
     maintainers = with maintainers; [ mbaillie ];
   };
 }
