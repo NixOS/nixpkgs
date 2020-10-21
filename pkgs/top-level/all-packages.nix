2a54c4f95058222998705cb96bef5e0db9d6170b
Francesco Zanini <francesco@zanini.me>
2020-10-21 14:58:52 +0200

    nanovna-saver: init at 0.3.7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14603,6 +14603,8 @@ in
 
   nanomsg = callPackage ../development/libraries/nanomsg { };
 
+  nanovna-saver = libsForQt5.callPackage ../applications/science/electronics/nanovna-saver { };
+
   ndpi = callPackage ../development/libraries/ndpi { };
 
   nifticlib = callPackage ../development/libraries/science/biology/nifticlib { };
