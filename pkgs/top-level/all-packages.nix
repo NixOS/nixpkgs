fa08ce096ef911f18b7a222f7e2389ecad1bc392
Eelco Dolstra <edolstra@gmail.com>
2020-10-21 20:21:20 +0200

    kdeconnect: Use Qt 5.12
    Fixes #99951.
    
      $ kdeconnect-app
      Cannot mix incompatible Qt library (5.12.7) with this library (5.15.0)
      Aborted (core dumped)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21596,7 +21596,7 @@ in
 
   k4dirstat = libsForQt5.callPackage ../applications/misc/k4dirstat { };
 
-  kdeconnect = libsForQt5.callPackage ../applications/misc/kdeconnect { };
+  kdeconnect = libsForQt512.callPackage ../applications/misc/kdeconnect { };
 
   inherit (kdeFrameworks) kdesu;
 
