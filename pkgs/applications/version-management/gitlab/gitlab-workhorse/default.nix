223860569a89dbc1fd560ff56cd1c404205eac14
Milan Pässler <milan@petabyte.dev>
2020-10-22 13:04:51 +0200

    gitlab: 13.4.3 -> 13.5.1

--- pkgs/applications/version-management/gitlab/gitlab-workhorse/default.nix
+++ pkgs/applications/version-management/gitlab/gitlab-workhorse/default.nix
@@ -3,29 +3,19 @@
 buildGoPackage rec {
   pname = "gitlab-workhorse";
 
-  version = "8.46.0";
+  version = "8.51.0";
 
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitlab-workhorse";
     rev = "v${version}";
-    sha256 = "1qhaln1k6vcjzphmxdqhwz1cwdx77gclyq0b2ibzgy7xg9l91n99";
+    sha256 = "012xbnd4lqfv478bbd2r9jhnb2f162gsa5y77z3by0kqqpsglgl5";
   };
 
   goPackagePath = "gitlab.com/gitlab-org/gitlab-workhorse";
   goDeps = ./deps.nix;
   buildInputs = [ git ];
   buildFlagsArray = "-ldflags=-X main.Version=${version}";
-
-  # gitlab-workhorse depends on an older version of labkit which
-  # contains old, vendored versions of some packages; gitlab-workhorse
-  # also explicitly depends on newer versions of these libraries,
-  # but buildGoPackage exposes the vendored versions instead,
-  # leading to compilation errors. Since the vendored libraries
-  # aren't used here anyway, we'll just remove them.
-  postConfigure = ''
-    rm -r "$NIX_BUILD_TOP/go/src/gitlab.com/gitlab-org/labkit/vendor"
-  '';
 
   meta = with stdenv.lib; {
     homepage = "http://www.gitlab.com/";
