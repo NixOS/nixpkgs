383bd9f62f5631e5aae4153b69fa99f98db58815
Jonathan Strickland <djanatyn@gmail.com>
2020-10-22 19:07:01 -0400

    zalgo: init at unstable-2020-08-26
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>
    Co-authored-by: Nikolay Korotkiy <sikmir@gmail.com>
    Co-authored-by: Ivar <41924494+IvarWithoutBones@users.noreply.github.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28342,6 +28342,8 @@ in
 
   navidrome = callPackage ../servers/misc/navidrome {};
 
+  zalgo = callPackage ../tools/misc/zalgo { };
+
   zettlr = callPackage ../applications/misc/zettlr { };
 
   unifi-poller = callPackage ../servers/monitoring/unifi-poller {};
