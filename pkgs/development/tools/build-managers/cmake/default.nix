0eb5f7d59afbd30f4b74da07727374f3eaa852ab
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-22 10:36:13 +0000

    cmake: 3.18.2 -> 3.18.4

--- pkgs/development/tools/build-managers/cmake/default.nix
+++ pkgs/development/tools/build-managers/cmake/default.nix
@@ -19,12 +19,12 @@ stdenv.mkDerivation rec {
           + lib.optionalString useNcurses "-cursesUI"
           + lib.optionalString withQt5 "-qt5UI"
           + lib.optionalString useQt4 "-qt4UI";
-  version = "3.18.2";
+  version = "3.18.4";
 
   src = fetchurl {
     url = "${meta.homepage}files/v${lib.versions.majorMinor version}/cmake-${version}.tar.gz";
     # compare with https://cmake.org/files/v${lib.versions.majorMinor version}/cmake-${version}-SHA-256.txt
-    sha256 = "0zhxsnxm5d8wdarz2gs3r41r1dfrnh35ki75fa684gaxfzy40kjx";
+    sha256 = "0xsg5lw3i4bw610q987cwz3iz06x3lrbbaa2mnzyr4kaiqsn2z2r";
   };
 
   patches = [
