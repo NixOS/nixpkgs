e833e5be2f7a181397c16d2fc85ad3737f67f572
David Birks <david@birks.dev>
2020-10-22 00:24:25 -0400

    lens: init at 3.6.7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21780,6 +21780,8 @@ in
 
   legit = gitAndTools.legit;
 
+  lens = callPackage ../applications/networking/cluster/lens { };
+
   leo-editor = libsForQt5.callPackage ../applications/editors/leo-editor { };
 
   libowfat = callPackage ../development/libraries/libowfat { };
