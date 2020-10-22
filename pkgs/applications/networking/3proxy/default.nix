8a2b08dec571b9b19f1f00da020ba1b33dcaaa99
misuzu <bakalolka@gmail.com>
2020-10-22 22:24:46 +0300

    _3proxy: 0.8.13 -> 0.9.3

--- pkgs/applications/networking/3proxy/default.nix
+++ pkgs/applications/networking/3proxy/default.nix
@@ -1,21 +1,27 @@
-{ stdenv, fetchFromGitHub, coreutils }:
+{ stdenv, fetchFromGitHub, coreutils, nixosTests }:
 
 stdenv.mkDerivation rec {
   pname = "3proxy";
-  version = "0.8.13";
+  version = "0.9.3";
+
   src = fetchFromGitHub {
     owner = "z3APA3A";
     repo = pname;
     rev = version;
-    sha256 = "1k5rqldiyakhwhplazlhswkgy3psdkpxhn85605ncwaqx49qy8vk";
+    sha256 = "0vx6cf3jdlg1h8755ryvfzqh9ld3r5i62ygw9vc6clzl5k1jkapm";
   };
-  makeFlags = [
-    "INSTALL=${coreutils}/bin/install"
-    "prefix=$(out)"
-  ];
+
   preConfigure = ''
     ln -s Makefile.Linux Makefile
   '';
+
+  installPhase = ''
+    mkdir -p $out/libexec
+    make INSTALL=${coreutils}/bin/install CHROOTDIR=$out prefix=$out man_prefix=$out install-bin install-man
+  '';
+
+  passthru.tests._3proxy = nixosTests._3proxy;
+
   meta = with stdenv.lib; {
     description = "Tiny free proxy server";
     homepage = "https://github.com/z3APA3A/3proxy";
