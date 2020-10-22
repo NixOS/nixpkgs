1b2d1e3ed66e0136a6de989092aacc61c06fc453
Michael Weiss <dev.primeos@gmail.com>
2020-10-22 12:50:08 +0200

    signal-desktop: 1.37.1 -> 1.37.2

--- pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
@@ -25,7 +25,7 @@ let
       else "");
 in stdenv.mkDerivation rec {
   pname = "signal-desktop";
-  version = "1.37.1"; # Please backport all updates to the stable channel.
+  version = "1.37.2"; # Please backport all updates to the stable channel.
   # All releases have a limited lifetime and "expire" 90 days after the release.
   # When releases "expire" the application becomes unusable until an update is
   # applied. The expiration date for the current release can be extracted with:
@@ -35,7 +35,7 @@ in stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://updates.signal.org/desktop/apt/pool/main/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-    sha256 = "0zj068wkgb0k7iq4ld0lb06vk2zlsvv5pf0csr4zkzkq0hgzx33s";
+    sha256 = "0n4g2fh1ibw6fl5vbnnchx2vafasqrq8rl9w0ch7lai665zkxsk3";
   };
 
   nativeBuildInputs = [
