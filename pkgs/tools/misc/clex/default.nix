538d99b79a74f11c166e765440d0c3c254010cf9
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-22 08:23:45 +0000

    clex: 4.6.patch9 -> 4.6.patch10

--- pkgs/tools/misc/clex/default.nix
+++ pkgs/tools/misc/clex/default.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   pname = "clex";
-  version = "4.6.patch9";
+  version = "4.6.patch10";
 
   src = fetchurl {
-    sha256 = "1qj5yp8k90wag5sb3zrm2pn90qqx3zbrgf2gqpqpdqmlgffnv1jc";
+    sha256 = "03niihqk57px7rm2c84qira5jm5vw8lj5s58dximk0w5gsis4fhw";
     url = "${meta.homepage}/download/${pname}-${version}.tar.gz";
   };
 
