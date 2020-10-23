08deae645f5e23bee88b8d618512f78a0f12b455
Jan Tojnar <jtojnar@gmail.com>
2020-10-23 19:28:05 +0200

    gnome3.gnome-online-miners: clean up
    * format
    * do not use aliases
    * correct license

--- pkgs/desktops/gnome-3/core/gnome-online-miners/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-online-miners/default.nix
@@ -1,6 +1,25 @@
-{ stdenv, fetchurl, pkgconfig, glib, gnome3, libxml2
-, libgdata, grilo, libzapojit, grilo-plugins, gnome-online-accounts, libmediaart
-, tracker_2, gfbgraph, librest, libsoup, json-glib, gmp, openssl, dleyna-server, wrapGAppsHook }:
+{ stdenv
+, fetchurl
+, pkg-config
+, glib
+, gnome3
+, libxml2
+, libgdata
+, grilo
+, libzapojit
+, grilo-plugins
+, gnome-online-accounts
+, libmediaart
+, tracker_2
+, gfbgraph
+, librest
+, libsoup
+, json-glib
+, gmp
+, openssl
+, dleyna-server
+, wrapGAppsHook
+}:
 
 stdenv.mkDerivation rec {
   pname = "gnome-online-miners";
@@ -11,12 +30,27 @@ stdenv.mkDerivation rec {
     sha256 = "1n2jz9i8a42zwxx5h8j2gdy6q1vyydh4vl00r0al7w8jzdh24p44";
   };
 
-  nativeBuildInputs = [ pkgconfig wrapGAppsHook ];
+  nativeBuildInputs = [
+    pkg-config
+    wrapGAppsHook
+  ];
+
   buildInputs = [
-    glib libgdata libxml2 libsoup gmp openssl
-    grilo libzapojit grilo-plugins
-    gnome-online-accounts libmediaart
-    tracker_2 gfbgraph json-glib librest
+    glib
+    libgdata
+    libxml2
+    libsoup
+    gmp
+    openssl
+    grilo
+    libzapojit
+    grilo-plugins
+    gnome-online-accounts
+    libmediaart
+    tracker_2
+    gfbgraph
+    json-glib
+    librest
     dleyna-server
   ];
 
@@ -33,7 +67,7 @@ stdenv.mkDerivation rec {
     homepage = "https://wiki.gnome.org/Projects/GnomeOnlineMiners";
     description = "A set of crawlers that go through your online content and index them locally in Tracker";
     maintainers = teams.gnome.members;
-    license = licenses.gpl2;
+    license = licenses.gpl2Plus;
     platforms = platforms.linux;
   };
 }
