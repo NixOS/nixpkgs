147fae2bd6cb546fe068408479d2713f77021ca8
Alireza Meskin <alireza.meskin@gmail.com>
2020-10-23 15:47:11 +0200

    zktree:: init at 0.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28814,4 +28814,6 @@ in
   psftools = callPackage ../os-specific/linux/psftools {};
 
   lc3tools = callPackage ../development/tools/lc3tools {};
+
+  zktree = callPackage ../applications/misc/zktree {};
 }
