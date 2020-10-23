f06ef735a334a0254f6d887bb46ae7b8cf94851a
Scott Dier <scott@dier.name>
2020-10-23 12:26:31 +0200

    igmpproxy: init at version 0.2.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4569,6 +4569,8 @@ in
       irods
       irods-icommands;
 
+  igmpproxy = callPackage ../tools/networking/igmpproxy { };
+
   ihaskell = callPackage ../development/tools/haskell/ihaskell/wrapper.nix {
     inherit (haskellPackages) ghcWithPackages;
 
