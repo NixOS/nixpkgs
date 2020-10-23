e52aaf2d43d80dcbadcaae651661351d62660864
David Birks <david@birks.dev>
2020-10-23 18:47:45 -0400

    material-shell: 7 -> 8 (#101427)
    And removed the patch that is now included in release 8.

--- pkgs/desktops/gnome-3/extensions/material-shell/default.nix
+++ pkgs/desktops/gnome-3/extensions/material-shell/default.nix
@@ -1,24 +1,15 @@
-{ stdenv, lib, fetchFromGitHub, gnome3, fetchpatch }:
+{ stdenv, lib, fetchFromGitHub, gnome3 }:
 
 stdenv.mkDerivation rec {
   pname = "gnome-shell-extension-material-shell";
-  version = "7";
+  version = "8";
 
   src = fetchFromGitHub {
     owner = "material-shell";
     repo = "material-shell";
     rev = version;
-    sha256 = "076cv1l5qr5x71przjwvbzx0m91n4z0byc2gc3r48l8vsr2d0hwf";
+    sha256 = "08zc6xl2b7k7l5l6afr40ii3gnxxbysan3cqv2s9g614rbsmc62r";
   };
-
-  patches = [
-    # Fix for https://github.com/material-shell/material-shell/issues/284
-    # (Remove this patch when updating to version >= 8)
-    (fetchpatch {
-      url = "https://github.com/material-shell/material-shell/commit/fc27489a1ec503a4a5c7cb2f4e1eefa84a7ea2f1.patch";
-      sha256 = "0x2skg955c4jqgwbkfhk7plm8bh1qnk66cdds796bzkp3hb5syw8";
-    })
-  ];
 
   # This package has a Makefile, but it's used for building a zip for
   # publication to extensions.gnome.org. Disable the build phase so
