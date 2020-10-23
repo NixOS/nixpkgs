2f19a83ca71b1fe0d0296fc2966503e13c66bdf7
Marcos Benevides <marcos.schonfinkel@protonmail.com>
2020-10-23 20:37:11 -0300

    pythonPackages.imutils: refactor

--- pkgs/development/python-modules/imutils/default.nix
+++ pkgs/development/python-modules/imutils/default.nix
@@ -2,6 +2,8 @@
 , buildPythonPackage
 , fetchPypi
 , opencv3
+, opencv4
+, withOpenCV3 ? false
 }:
 
 buildPythonPackage rec {
@@ -13,15 +15,17 @@ buildPythonPackage rec {
     sha256 = "857af6169d90e4a0a814130b9b107f5d611150ce440107e1c1233521c6fb1e2b";
   };
 
-  propagatedBuildInputs = [ opencv3 ];
+  propagatedBuildInputs = [ (if withOpenCV3 then opencv3 else opencv4) ];
 
   # no tests
   doCheck = false;
+
+  pythonImportsCheck = [ "imutils" ];
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/jrosebr1/imutils";
     description = "A series of convenience functions to make basic image processing functions";
     license = licenses.mit;
-    maintainers = [ maintainers.costrouc ];
+    maintainers = with maintainers; [ costrouc mtrsk ];
   };
 }
