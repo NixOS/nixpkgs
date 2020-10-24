f6bfc2c38be89d26f2aa60823656355049ffe621
Luflosi <luflosi@luflosi.de>
2020-10-24 12:29:52 +0200

    wordgrinder: 0.7.2 -> 0.8
    https://github.com/davidgiven/wordgrinder/releases/tag/0.8

--- pkgs/applications/office/wordgrinder/default.nix
+++ pkgs/applications/office/wordgrinder/default.nix
@@ -3,13 +3,13 @@
 
 stdenv.mkDerivation rec {
   pname = "wordgrinder";
-  version = "0.7.2";
+  version = "0.8";
 
   src = fetchFromGitHub {
     repo = "wordgrinder";
     owner = "davidgiven";
     rev = version;
-    sha256 = "08lnq5wmspfqdjmqm15gizcq0xr7mg4h62qhvwj63v0sd6ks1cal";
+    sha256 = "124d1bnn2aqs6ik8pdazzni6a0583prz9lfdjrbwyb97ipqga9pm";
   };
 
   makeFlags = [
@@ -25,6 +25,7 @@ stdenv.mkDerivation rec {
 
   dontUseNinjaBuild = true;
   dontUseNinjaInstall = true;
+  dontConfigure = true;
 
   nativeBuildInputs = [
     pkgconfig
@@ -33,11 +34,12 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
-    libXft
     lua52Packages.lua
     ncurses
     readline
     zlib
+  ] ++ stdenv.lib.optionals stdenv.isLinux [
+    libXft
   ];
 
   # To be able to find <Xft.h>
