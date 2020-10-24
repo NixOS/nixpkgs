fdd6de0ab1de379444de95c791610c0a074afc9b
zowoq <59103226+zowoq@users.noreply.github.com>
2020-10-25 08:19:54 +1000

    libslirp: fix darwin build

--- pkgs/development/libraries/libslirp/default.nix
+++ pkgs/development/libraries/libslirp/default.nix
@@ -4,6 +4,7 @@
 , ninja
 , pkg-config
 , glib
+, fetchpatch
 }:
 
 stdenv.mkDerivation rec {
@@ -18,6 +19,14 @@ stdenv.mkDerivation rec {
     sha256 = "0pzgjj2x2vrjshrzrl2x39xp5lgwg4b4y9vs8xvadh1ycl10v3fv";
   };
 
+  patches = [
+    # fix meson.build on darwin
+    (fetchpatch {
+      url = "https://gitlab.freedesktop.org/slirp/libslirp/-/commit/9f82a47b81f2864422b82c1e40e51a2ed9c6ac32.patch";
+      sha256 = "13mfhfi6scchy9l9ri4cvvic1i0jh0mwcv0pkvrf667wzllsjxll";
+    })
+  ];
+
   nativeBuildInputs = [ meson ninja pkg-config ];
 
   buildInputs = [ glib ];
@@ -31,6 +40,6 @@ stdenv.mkDerivation rec {
     homepage = "https://gitlab.freedesktop.org/slirp/libslirp";
     license = licenses.bsd3;
     maintainers = with maintainers; [ orivej ];
-    platforms = platforms.linux;
+    platforms = platforms.unix;
   };
 }
