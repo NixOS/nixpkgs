4eb2d13a644db7c4e6bf54b72ccf735a25d59b72
Jan Tojnar <jtojnar@gmail.com>
2020-10-24 02:57:11 +0000

    gnome3.gdm: 3.38.0 → 3.38.1

--- pkgs/desktops/gnome-3/core/gdm/default.nix
+++ pkgs/desktops/gnome-3/core/gdm/default.nix
@@ -47,13 +47,13 @@ in
 
 stdenv.mkDerivation rec {
   pname = "gdm";
-  version = "3.38.0";
+  version = "3.38.1";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gdm/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "1fimhklb204rflz8k345756jikgbw8113hms3zlcwk6975f43m26";
+    sha256 = "0wkzy5mrxq963fm4pmn6bhml73zmc8bq61frm77a175046c590q8";
   };
 
   mesonFlags = [
@@ -122,13 +122,6 @@ stdenv.mkDerivation rec {
     # Set up the environment properly when launching sessions
     # https://github.com/NixOS/nixpkgs/issues/48255
     ./reset-environment.patch
-
-    # Fix runtime patch location.
-    # https://gitlab.gnome.org/GNOME/gdm/-/merge_requests/114
-    (fetchpatch {
-      url = "https://gitlab.gnome.org/GNOME/gdm/-/commit/9d841d681f3d1c58e3df51a98421257f774cd185.patch";
-      sha256 = "0lf5kpz9ghylqlbybc0mpfsvr4i29z1ag8wf6j1918hjrfcipnxj";
-    })
   ];
 
   postPatch = ''
