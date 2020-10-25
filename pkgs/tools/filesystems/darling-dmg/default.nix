3d38fae90df5d2a4e203096fb55e86f63cc34fbf
Luflosi <luflosi@luflosi.de>
2020-10-25 10:02:27 +0100

    darling-dmg: 1.0.4+git20180914 -> 1.0.4+git20200427

--- pkgs/tools/filesystems/darling-dmg/default.nix
+++ pkgs/tools/filesystems/darling-dmg/default.nix
@@ -2,25 +2,23 @@
 
 stdenv.mkDerivation rec {
   pname = "darling-dmg";
-  version = "1.0.4+git20180914";
+  version = "1.0.4+git20200427";
 
   src = fetchFromGitHub {
     owner = "darlinghq";
     repo = "darling-dmg";
-    rev = "97a92a6930e43cdbc9dedaee62716e3223deb027";
-    sha256 = "1bngr4827qnl4s2f7z39wjp13nfm3zzzykjshb43wvjz536bnqdj";
+    rev = "71cc76c792db30328663272788c0b64aca27fdb0";
+    sha256 = "08iphkxlmjddrxpbm13gxyqwcrd0k65z3l1944n4pccb6qbyj8gv";
   };
 
   nativeBuildInputs = [ cmake ];
   buildInputs = [ fuse openssl zlib bzip2 libxml2 icu ];
 
-  # compat with icu61+ https://github.com/unicode-org/icu/blob/release-64-2/icu4c/readme.html#L554
-  CXXFLAGS = [ "-DU_USING_ICU_NAMESPACE=1" ];
 
-  meta = {
+  meta = with stdenv.lib; {
     homepage = "https://www.darlinghq.org/";
     description = "Darling lets you open macOS dmgs on Linux";
-    platforms = stdenv.lib.platforms.linux;
-    license = stdenv.lib.licenses.gpl3;
+    platforms = platforms.linux;
+    license = licenses.gpl3;
   };
 }
