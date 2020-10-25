ac466d3d9f11efecf52a5a9d4ccc0afc03747fed
Frederik Rietdijk <fridh@fridh.nl>
2020-10-25 10:06:53 +0100

    python3Packages.ipdb: 0.13.3 -> 0.13.4

--- pkgs/development/python-modules/ipdb/default.nix
+++ pkgs/development/python-modules/ipdb/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "ipdb";
-  version = "0.13.3";
+  version = "0.13.4";
   disabled = isPyPy;  # setupterm: could not find terminfo database
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0y3yk5k2yszcwxsjinvf40b1wl8wi8l6kv7pl9jmx9j53hk6vx6n";
+    sha256 = "c85398b5fb82f82399fc38c44fe3532c0dde1754abee727d8f5cfcc74547b334";
   };
 
   propagatedBuildInputs = [ ipython ];
