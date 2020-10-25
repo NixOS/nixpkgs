bf935d15e3fe8f6f1612d35d6f50e91e396af71f
Frederik Rietdijk <fridh@fridh.nl>
2020-10-25 10:06:53 +0100

    python3Packages.nest_asyncio: 1.4.0 -> 1.4.1

--- pkgs/development/python-modules/nest-asyncio/default.nix
+++ pkgs/development/python-modules/nest-asyncio/default.nix
@@ -5,13 +5,13 @@
 }:
 
 buildPythonPackage rec {
-  version = "1.4.0";
+  version = "1.4.1";
   pname = "nest_asyncio";
   disabled = !(pythonAtLeast "3.5");
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1j2rbb31wdq9k15cmj2y0kypzvfc3v05py1n0809nmqlpi5hawsp";
+    sha256 = "b86c3193abda5b2eeccf8c79894bc71c680369a178f4b068514ac00720b14e01";
   };
 
   # tests not packaged with source dist as of 1.3.2/1.3.2, and
