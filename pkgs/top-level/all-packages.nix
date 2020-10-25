42054073bb5563a0e8b1912bd01b826dd18b69ae
rnhmjoj <rnhmjoj@inventati.org>
2020-10-25 02:20:19 +0200

    arx-libertatis: remove old override
    (cherry picked from commit 9ae47f9e64d1f43da5fd7804ac308e76b1ba918b)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24606,9 +24606,7 @@ in
 
   arena = callPackage ../games/arena {};
 
-  arx-libertatis = libsForQt5.callPackage ../games/arx-libertatis {
-    stdenv = gcc6Stdenv;
-  };
+  arx-libertatis = libsForQt5.callPackage ../games/arx-libertatis { };
 
   asc = callPackage ../games/asc {
     lua = lua5_1;
