41e143915e3a17c9b10400cbb8c050eb81ccdd00
Ryan Burns <rtburns@protonmail.com>
2020-10-25 16:31:19 -0700

    goaccess: fix build on darwin

--- pkgs/tools/misc/goaccess/default.nix
+++ pkgs/tools/misc/goaccess/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, ncurses, glib, libmaxminddb }:
+{ stdenv, fetchurl, pkgconfig, ncurses, glib, libmaxminddb, fetchpatch }:
 
 stdenv.mkDerivation rec {
   version = "1.4";
@@ -8,6 +8,13 @@ stdenv.mkDerivation rec {
     url = "https://tar.goaccess.io/goaccess-${version}.tar.gz";
     sha256 = "1gkpjg39f3afdwm9128jqjsfap07p8s027czzlnxfmi5hpzvkyz8";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/allinurl/goaccess/commit/514618cdd69453497fbf67913ccb37a0a0b07391.patch";
+      sha256 = "11lp7mabfl6ibgzsd9nw10k2xvcm0hrimrwidl06r8dqn2jzjxf6";
+    })
+  ];
 
   configureFlags = [
     "--enable-geoip=mmdb"
