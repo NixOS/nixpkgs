7a80631a8bc6425138842ba9323a6ba51895cf62
Frederik Rietdijk <fridh@fridh.nl>
2020-10-25 10:06:50 +0100

    python3Packages.bayespy: 0.5.19 -> 0.5.20

--- pkgs/development/python-modules/bayespy/default.nix
+++ pkgs/development/python-modules/bayespy/default.nix
@@ -4,7 +4,7 @@
 
 buildPythonPackage rec {
   pname = "bayespy";
-  version = "0.5.19";
+  version = "0.5.20";
 
   # Python 2 not supported and not some old Python 3 because MPL doesn't support
   # them properly.
@@ -12,7 +12,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "24e1327ce241a0113abf217fbaf41ac25e04f5a01f9ed606610f2f1f2d82d34f";
+    sha256 = "8c16cdc73bbcd9a1124a0495056065b7ce938dbe6c2c780dc330c83fb4d2640a";
   };
 
   checkInputs = [ pytest nose glibcLocales ];
