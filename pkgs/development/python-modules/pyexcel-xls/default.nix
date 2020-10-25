cda7e319400b188afa619324d7c3ee20730c6f15
Frederik Rietdijk <fridh@fridh.nl>
2020-10-25 10:06:55 +0100

    python3Packages.pyexcel-xls: 0.6.0 -> 0.6.1

--- pkgs/development/python-modules/pyexcel-xls/default.nix
+++ pkgs/development/python-modules/pyexcel-xls/default.nix
@@ -11,11 +11,11 @@
 
 buildPythonPackage rec {
   pname = "pyexcel-xls";
-  version = "0.6.0";
+  version = "0.6.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "64bac180274c52efe970c664d3e8bb12402c9d10e0734d9fe87655646a876c45";
+    sha256 = "c4cc1fb4ac5d1682a44d9a368a43ec2e089ad6fc46884648ccfad46863e3da0a";
   };
 
   propagatedBuildInputs = [
