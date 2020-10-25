5f811aee02ae9fa6804cbf03b3b92ab3222dccaa
Frederik Rietdijk <fridh@fridh.nl>
2020-10-25 10:06:56 +0100

    python3Packages.pytest-quickcheck: 0.8.4 -> 0.8.5

--- pkgs/development/python-modules/pytest-quickcheck/default.nix
+++ pkgs/development/python-modules/pytest-quickcheck/default.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPythonPackage, fetchPypi, pytest, pytest-flakes, pytestpep8, tox }:
 buildPythonPackage rec {
   pname = "pytest-quickcheck";
-  version = "0.8.4";
+  version = "0.8.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e368390c9e3fd48eb3edec0c4eef08d7332f1143ad7b7190d32376b2fd2e62ff";
+    sha256 = "2427808b54ccdec26a40cdba934a6c042fab9ebadb60d563a01f367bef87fe58";
   };
 
   buildInputs = [ pytest ];
