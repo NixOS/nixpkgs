7e69e227fc69acf596f22957596a7defc9f99ca2
Frederik Rietdijk <fridh@fridh.nl>
2020-10-25 10:06:56 +0100

    python3Packages.python-rapidjson: 0.9.1 -> 0.9.3

--- pkgs/development/python-modules/python-rapidjson/default.nix
+++ pkgs/development/python-modules/python-rapidjson/default.nix
@@ -8,13 +8,13 @@
 }:
 
 buildPythonPackage rec {
-  version = "0.9.1";
+  version = "0.9.3";
   pname = "python-rapidjson";
   disabled = pythonOlder "3.4";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ad80bd7e4bb15d9705227630037a433e2e2a7982b54b51de2ebabdd1611394a1";
+    sha256 = "4e339edfbba9e5feed0cf44043b6befba0c780b34cdf446888dbce44de362a8a";
   };
 
   LC_ALL="en_US.utf-8";
