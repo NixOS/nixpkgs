76a38f069eca6299975af8365ec02086c11ef6ae
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-25 19:45:44 +0000

    livepeer: 0.5.10 -> 0.5.11

--- pkgs/servers/livepeer/default.nix
+++ pkgs/servers/livepeer/default.nix
@@ -4,16 +4,16 @@
 
 buildGoModule rec {
   pname = "livepeer";
-  version = "0.5.10";
+  version = "0.5.11";
 
   runVend = true;
-  vendorSha256 = "0i5977skw4b209zvdvgvzfnhqb574067mmfk2gf9y590lgnr86f7";
+  vendorSha256 = "13cgwpf3v4vlvb0mgdxsdybpghx1cp3fzkdwmq8b193a8dcl8s63";
 
   src = fetchFromGitHub {
     owner = "livepeer";
     repo = "go-livepeer";
     rev = "v${version}";
-    sha256 = "0qccvnk2yyly7ha9lkcpd6zj5n9xnhdjkl3nllb9xziidsq6p65h";
+    sha256 = "12vbnl74z6jk77bnws8a5z5n7bnhkbb4ngzxfir5l3g9zrpsc5p3";
   };
 
   # livepeer_cli has a vendoring problem
