04e586bba541d2cf153d75a2157b6d2db76c9a25
Frederik Rietdijk <fridh@fridh.nl>
2020-10-25 10:06:51 +0100

    python3Packages.djangorestframework: 3.11.1 -> 3.11.2

--- pkgs/development/python-modules/djangorestframework/default.nix
+++ pkgs/development/python-modules/djangorestframework/default.nix
@@ -1,13 +1,13 @@
 { stdenv, buildPythonPackage, fetchPypi, django, isPy27 }:
 
 buildPythonPackage rec {
-  version = "3.11.1";
+  version = "3.11.2";
   pname = "djangorestframework";
   disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "6dd02d5a4bd2516fb93f80360673bf540c3b6641fec8766b1da2870a5aa00b32";
+    sha256 = "a5967b68a04e0d97d10f4df228e30f5a2d82ba63b9d03e1759f84993b7bf1b53";
   };
 
   # Test settings are missing
