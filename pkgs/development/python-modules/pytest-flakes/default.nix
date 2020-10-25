1ed8b607c83948b26e48c6c178e0a7f425272560
Frederik Rietdijk <fridh@fridh.nl>
2020-10-25 10:06:56 +0100

    python3Packages.pytest-flakes: 4.0.1 -> 4.0.2

--- pkgs/development/python-modules/pytest-flakes/default.nix
+++ pkgs/development/python-modules/pytest-flakes/default.nix
@@ -5,11 +5,11 @@ buildPythonPackage rec {
   # upstream has abandoned project in favor of pytest-flake8
   # retaining package to not break other packages
   pname = "pytest-flakes";
-  version = "4.0.1";
+  version = "4.0.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "37113ac6c7ea5e0b648abf73937955a45f8b9214fe49413297c2ce6ce1808500";
+    sha256 = "6733db47937d9689032876359e5ee0ee6926e3638546c09220e2f86b3581d4c1";
   };
 
   checkInputs = [ pytestpep8 pytest ];
