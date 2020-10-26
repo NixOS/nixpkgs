16d0ee5aa0b630c9e0e2045f4e722901e76d5989
Nicolas Martin <penguwin@penguwin.eu>
2020-10-26 23:49:29 +0100

    nextinspace: init at 1.0.3

---
+++ pkgs/applications/science/misc/nextinspace/default.nix
@@ -0,0 +1,26 @@
+{ lib, fetchPypi, python3Packages }:
+
+python3Packages.buildPythonPackage rec {
+  pname = "nextinspace";
+  version = "1.0.3";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1435ajd5d8r0zqi3jy7ppf7s5q7pa3hxysvb5kxiz1k3c23bszw8";
+  };
+
+  pythonPath = with python3Packages; [
+    requests
+    tzlocal
+    colorama
+  ];
+
+  vendorSha256 = "0z3r8fvpy36ybgb18sr0lril1sg8z7s99xv1a6g1v3zdnj3zimav";
+
+  meta = with lib; {
+    description = "Print upcoming space-related events in your terminal";
+    homepage = "https://github.com/The-Kid-Gid/nextinspace";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ penguwin ];
+  };
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26812,6 +26812,8 @@ in
 
   netlogo = callPackage ../applications/science/misc/netlogo { };
 
+  nextinspace = python3Packages.callPackage ../applications/science/misc/nextinspace { };
+
   ns-3 = callPackage ../development/libraries/science/networking/ns-3 { python = python3; };
 
   root = callPackage ../applications/science/misc/root {
