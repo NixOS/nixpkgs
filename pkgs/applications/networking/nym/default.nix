0b3577c73f13a0b717daef1d58212fae415b4026
hyperfekt <git@hyperfekt.net>
2020-10-26 22:00:25 +0100

    nym: 0.7.0 -> 0.8.1
    The tests requiring network access are no longer called by the root
    package.

--- pkgs/applications/networking/nym/default.nix
+++ pkgs/applications/networking/nym/default.nix
@@ -9,36 +9,31 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "nym";
-  version = "0.7.0";
+  version = "0.8.1";
 
   src = fetchFromGitHub {
     owner = "nymtech";
     repo = "nym";
     rev = "v${version}";
-    sha256 = "05bxrpqwwf9spydac0q8sly65q8f1nk13i5fy3p5adr1phzxdnr8";
+    sha256 = "0wzk9qzjyax73lfjbbag412vw1fgk2wmhhry5hdlvdbkim42m5bn";
   };
 
-  cargoSha256 = "0mh8cwia86bm68b0wcrmnsq1af5cp6kj1j81nwxb03awnqpxc34n";
+  # fix outdated Cargo.lock
+  cargoPatches = [ (writeText "fix-nym-cargo-lock.patch" ''
+    --- a/Cargo.lock
+    +++ b/Cargo.lock
+    @@ -1826 +1826 @@
+    -version = "0.8.0"
+    +version = "0.8.1"
+  '') ];
+
+  cargoSha256 = "0zr5nzmglmvn6xfqgvipbzy8nw5cl3nf7zjmghkqdwi6zj9p9272";
 
   nativeBuildInputs = [ pkgconfig ];
 
   buildInputs = [ openssl ];
 
   checkType = "debug";
-
-  /*
-  Nym's test presence::converting_mixnode_presence_into_topology_mixnode::it_returns_resolved_ip_on_resolvable_hostname tries to resolve nymtech.net.
-  Since there is no external DNS resolution available in the build sandbox, we point cargo and its children (that's what we remove the 'unsetenv' call for) to a hosts file in which we statically resolve nymtech.net.
-  */
-  preCheck = ''
-    export LD_PRELOAD=${libredirect.overrideAttrs (drv: {
-      postPatch = "sed -i -e /unsetenv/d libredirect.c";
-    })}/lib/libredirect.so
-    export NIX_REDIRECTS=/etc/hosts=${writeText "nym_resolve_test_hosts" "127.0.0.1 nymtech.net"}
-  '';
-
-  postCheck = "unset NIX_REDIRECTS LD_PRELOAD";
-
 
   passthru.updateScript = ./update.sh;
 
