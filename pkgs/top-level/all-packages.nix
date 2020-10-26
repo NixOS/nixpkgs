9c80e9587db98e721f9259c0d9813c55b2ac694f
Doron Behar <doron.behar@gmail.com>
2020-10-26 22:48:27 +0200

    nixos/sddm: Use libsForQt514.sddm if needed (for lxqt)
    Currently lxqt is a desktop environment that's compiled against qt514.
    To avoid possible issues (#101369), we (hopefully) use the same qt
    version as the desktop environment at hand. LXQT should move to qt515,
    and for the long term the correct qt version should be inherited by the
    sddm module.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25934,7 +25934,8 @@ in
   lumina = recurseIntoAttrs (callPackage ../desktops/lumina { });
 
   lxqt = recurseIntoAttrs (import ../desktops/lxqt {
-    # TODO: Update these to qt515 at some point
+    # TODO: Update these to qt515 at some point. When doing it, please remove
+    # the choice of libsForQt5*.sddm in sddm's module.
     qt5 = qt514;
     libsForQt5 = libsForQt514;
     inherit pkgs;
