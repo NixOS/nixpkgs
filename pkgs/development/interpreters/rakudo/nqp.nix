4eb584c69c3e0a19524642a4892a01cb5578c7d2
Stig Palmquist <stig@stig.io>
2020-10-26 15:32:42 +0100

    nqp: 2020.09 -> 2020.10

--- pkgs/development/interpreters/rakudo/nqp.nix
+++ pkgs/development/interpreters/rakudo/nqp.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "nqp";
-  version = "2020.09";
+  version = "2020.10";
 
   src = fetchurl {
     url    = "https://github.com/raku/nqp/releases/download/${version}/nqp-${version}.tar.gz";
-    sha256 = "09kiy7r732yrh3nzdzdq9yki0jx2hamyqvrbahk4mlxmb6cl9qmh";
+    sha256 = "0nb0vdycz2wrd5yspvps2wy7a21066csr18v786085qim347830h";
   };
 
   buildInputs = [ perl ];
