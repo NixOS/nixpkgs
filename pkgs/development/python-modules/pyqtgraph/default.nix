f0087e297e9aa83ff46ed258a35f488a8b8f2332
Nicolai Kellerer <nicolai@kellerer.me>
2020-10-27 18:36:10 +0100

    pythonPackages.pyqtgraph: use PyQt5 and enable tests

--- pkgs/development/python-modules/pyqtgraph/default.nix
+++ pkgs/development/python-modules/pyqtgraph/default.nix
@@ -1,24 +1,49 @@
 { stdenv
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 , scipy
 , numpy
-, pyqt4
+, pyqt5
 , pyopengl
+, h5py
+, qt5
+, python
+, pytest
+, freefont_ttf
+, makeFontsConf
 }:
 
-buildPythonPackage rec {
+let
+  fontsConf = makeFontsConf {
+    fontDirectories = [ freefont_ttf ];
+  };
+in buildPythonPackage rec {
   pname = "pyqtgraph";
   version = "0.11.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "0p5k73wjfh0zzjvby8b5107cx7x0c2rdj66zh1nc8y95i0anf2na";
+  src = fetchFromGitHub {
+    owner = "pyqtgraph";
+    repo = "pyqtgraph";
+    rev = "pyqtgraph-${version}";
+    sha256 = "03fvpkqdn80ni51msvyivmghw41qk4vplwdqndkvzzzlppimdjbn";
   };
 
-  propagatedBuildInputs = [ scipy numpy pyqt4 pyopengl ];
+  propagatedBuildInputs = [ numpy pyqt5 scipy pyopengl h5py ];
 
-  doCheck = false;  # "PyQtGraph requires either PyQt4 or PySide; neither package could be imported."
+  checkInputs = [ pytest ];
+
+  checkPhase = ''
+    export QT_PLUGIN_PATH="${qt5.qtbase.bin}/${qt5.qtbase.qtPluginPrefix}"
+    export QT_QPA_PLATFORM=offscreen
+
+    export FONTCONFIG_FILE=${fontsConf}
+
+    # disable 6 tests which try to clone https://github.com/pyqtgraph/test-data
+    ${python.interpreter} test.py --pyqt5 \
+      -k "not test_ImageItem and not test_ImageItem_axisorder \
+      and not test_PlotCurveItem and not test_getArrayRegion \
+      and not test_getArrayRegion_axisorder and not test_PolyLineROI"
+  '';
 
   meta = with stdenv.lib; {
     description = "Scientific Graphics and GUI Library for Python";
