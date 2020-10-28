4f08de64486941e404e3b2655551e9aa748492f8
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-10-28 21:18:12 +0100

    python3Packages.pyobjc-framework-ServiceManagement: init at 6.2.2

---
+++ pkgs/development/python-modules/pyobjc-framework-ServiceManagement/default.nix
@@ -0,0 +1,44 @@
+{ stdenv, buildPythonPackage, pythonOlder, fetchPypi, darwin, python,
+pyobjc-core, pyobjc-framework-Cocoa }:
+
+buildPythonPackage rec {
+  pname = "pyobjc-framework-ServiceManagement";
+  version = "6.2.2";
+
+  disabled = pythonOlder "3.6";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1m7dbv1484vwm21d9233jrzy6widjv90a9b629lyrj1jy42k0j62";
+  };
+  
+  postPatch = ''
+    # Hard code correct SDK version
+    substituteInPlace pyobjc_setup.py \
+      --replace 'os.path.basename(data)[6:-4]' '"${darwin.apple_sdk.sdk.version}"'
+  '';
+
+  buildInputs = [
+  ] ++ (with darwin; [
+  ] ++ (with apple_sdk.frameworks;[
+  ]));
+
+  propagatedBuildInputs = [
+    pyobjc-core
+    pyobjc-framework-Cocoa
+  ];
+
+  # clang-7: error: argument unused during compilation: '-fno-strict-overflow'
+  hardeningDisable = [ "strictoverflow" ];
+
+  dontUseSetuptoolsCheck = true;
+  pythonImportcheck = [ "pyobjc-framework-ServiceManagement" ];
+
+  meta = with stdenv.lib; {
+    description = "Wrappers for the framework ServiceManagement on Mac OS X";
+    homepage = "https://pythonhosted.org/pyobjc-framework-ServiceManagement/";
+    license = licenses.mit;
+    platforms = platforms.darwin;
+    maintainers = with maintainers; [ SuperSandro2000 ];
+  };
+}
