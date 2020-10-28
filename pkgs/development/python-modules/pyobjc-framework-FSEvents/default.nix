34348d5595b8d62311e499961ad4075be8708803
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-10-28 20:40:49 +0100

    python3Packages.pyobjc-framework-FSEvents: init at 6.2.2

---
+++ pkgs/development/python-modules/pyobjc-framework-FSEvents/default.nix
@@ -0,0 +1,45 @@
+{ stdenv, buildPythonPackage, pythonOlder, fetchPypi, darwin, python,
+pyobjc-core, pyobjc-framework-Cocoa }:
+
+buildPythonPackage rec {
+  pname = "pyobjc-framework-FSEvents";
+  version = "6.2.2";
+
+  disabled = pythonOlder "3.6";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1dzjb3rcyk6fl0ypwbs259xw1ihyi6d22jzwczf57jhvissdyb4f";
+  };
+  
+  postPatch = ''
+    # Hard code correct SDK version
+    substituteInPlace pyobjc_setup.py \
+      --replace 'os.path.basename(data)[6:-4]' '"${darwin.apple_sdk.sdk.version}"'
+  '';
+
+  buildInputs = [
+  ] ++ (with darwin; [
+  ] ++ (with apple_sdk.frameworks;[
+    Foundation
+  ]));
+
+  propagatedBuildInputs = [
+    pyobjc-core
+    pyobjc-framework-Cocoa
+  ];
+
+  # clang-7: error: argument unused during compilation: '-fno-strict-overflow'
+  hardeningDisable = [ "strictoverflow" ];
+
+  dontUseSetuptoolsCheck = true;
+  pythonImportcheck = [ "pyobjc-framework-FSEvents" ];
+
+  meta = with stdenv.lib; {
+    description = "Wrappers for the framework FSEvents on Mac OS X";
+    homepage = "https://pythonhosted.org/pyobjc-framework-FSEvents/";
+    license = licenses.mit;
+    platforms = platforms.darwin;
+    maintainers = with maintainers; [ SuperSandro2000 ];
+  };
+}
