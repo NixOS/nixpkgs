083bfc5932d1b2f589b725d97dbab7fa9b281192
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-10-28 20:41:21 +0100

    python3Packages.pyobjc-framework-GameKit: init at 6.2.2

---
+++ pkgs/development/python-modules/pyobjc-framework-GameKit/default.nix
@@ -0,0 +1,47 @@
+{ stdenv, buildPythonPackage, pythonOlder, fetchPypi, darwin, python,
+pyobjc-core, pyobjc-framework-Quartz }:
+
+buildPythonPackage rec {
+  pname = "pyobjc-framework-GameKit";
+  version = "6.2.2";
+
+  disabled = pythonOlder "3.6";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "14yxazlblmg44b1faicg7his7lg74rbdnh4iydn0f0xqlma8v70x";
+  };
+  
+  postPatch = ''
+    # Hard code correct SDK version
+    substituteInPlace pyobjc_setup.py \
+      --replace 'os.path.basename(data)[6:-4]' '"${darwin.apple_sdk.sdk.version}"'
+  '';
+
+  buildInputs = [
+  ] ++ (with darwin; [
+  ] ++ (with apple_sdk.frameworks;[
+    AddressBook
+    Foundation
+    GameKit
+  ]));
+
+  propagatedBuildInputs = [
+    pyobjc-core
+    pyobjc-framework-Quartz
+  ];
+
+  # clang-7: error: argument unused during compilation: '-fno-strict-overflow'
+  hardeningDisable = [ "strictoverflow" ];
+
+  dontUseSetuptoolsCheck = true;
+  pythonImportcheck = [ "pyobjc-framework-GameKit" ];
+
+  meta = with stdenv.lib; {
+    description = "Wrappers for the framework GameKit on Mac OS X";
+    homepage = "https://pythonhosted.org/pyobjc-framework-GameKit/";
+    license = licenses.mit;
+    platforms = platforms.darwin;
+    maintainers = with maintainers; [ SuperSandro2000 ];
+  };
+}
