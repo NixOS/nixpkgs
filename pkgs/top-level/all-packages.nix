9f534d375984a9f07ab18daee72ec98a0e007d28
Sebastian Neubauer <sebastian.neubauer@amd.com>
2020-10-28 12:32:51 +0100

    piglit: init at unstable-2020-10-23
    piglit is a test runner for mesa.
    To check that it is working, run `piglit run sanity results/sanity`

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2471,6 +2471,8 @@ in
 
   photon = callPackage ../tools/networking/photon { };
 
+  piglit = callPackage ../tools/graphics/piglit { };
+
   playerctl = callPackage ../tools/audio/playerctl { };
 
   ps_mem = callPackage ../tools/system/ps_mem { };
