3f9744a968fe1c24f63f9eb82b96369e60a684d9
Aaron Andersen <aaron@fosslib.net>
2020-10-27 20:01:12 -0400

    php.packages.composer2: init at 2.0.0

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -85,6 +85,35 @@ in
       };
     };
 
+    composer2 = mkDerivation rec {
+      version = "2.0.0";
+      pname = "composer";
+
+      src = pkgs.fetchurl {
+        url = "https://getcomposer.org/download/${version}/composer.phar";
+        sha256 = "11fjplbrscnw0fs5hmw4bmszg5a87ig189175407i1ip5fm5g5hk";
+      };
+
+      dontUnpack = true;
+
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+
+      installPhase = ''
+        mkdir -p $out/bin
+        install -D $src $out/libexec/composer/composer.phar
+        makeWrapper ${php}/bin/php $out/bin/composer \
+          --add-flags "$out/libexec/composer/composer.phar" \
+          --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.unzip ]}
+      '';
+
+      meta = with pkgs.lib; {
+        description = "Dependency Manager for PHP";
+        license = licenses.mit;
+        homepage = "https://getcomposer.org/";
+        maintainers = with maintainers; [ offline ] ++ teams.php.members;
+      };
+    };
+
     php-cs-fixer = mkDerivation rec {
       version = "2.16.3";
       pname = "php-cs-fixer";
