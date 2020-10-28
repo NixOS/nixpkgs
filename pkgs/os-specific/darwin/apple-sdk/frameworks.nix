1bfb4880d86569de4f6b5157abb87cf7ebc24672
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-10-28 13:56:55 +0100

    apple_sdk.frameworks.Quartz: Fix Headers/QuartzFilter.h:10:9: fatal error: 'Cocoa/Cocoa.h' file not found

--- pkgs/os-specific/darwin/apple-sdk/frameworks.nix
+++ pkgs/os-specific/darwin/apple-sdk/frameworks.nix
@@ -120,7 +120,7 @@ with frameworks; with libs; {
   IOBluetooth         = [ IOKit ];
   JavaVM              = [];
   OpenDirectory       = [];
-  Quartz              = [ AppKit QuickLook QTKit ];
+  Quartz              = [ AppKit Cocoa QuickLook QTKit ];
   QuartzCore          = [ ApplicationServices CoreVideo OpenCL CoreImage Metal ];
   QuickTime           = [ ApplicationServices AudioUnit Carbon CoreAudio CoreServices OpenGL QuartzCore ];
 
