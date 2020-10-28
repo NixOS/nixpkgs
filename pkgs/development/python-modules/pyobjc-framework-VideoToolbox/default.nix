d7df066fb36bd28faf338b2b4e073c836be89350
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-10-28 21:19:05 +0100

    python3Packages.pyobjc-framework-VideoToolbox: init at 6.2.2

---
+++ pkgs/development/python-modules/pyobjc-framework-VideoToolbox/default.nix
@@ -0,0 +1,53 @@
+{ stdenv, buildPythonPackage, pythonOlder, fetchPypi, darwin, python,
+pyobjc-core, pyobjc-framework-Cocoa, pyobjc-framework-CoreMedia, pyobjc-framework-Quartz }:
+
+buildPythonPackage rec {
+  pname = "pyobjc-framework-VideoToolbox";
+  version = "6.2.2";
+
+  disabled = pythonOlder "3.6";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "11jyz43c8ciylbrznzg3lz18am8vyxywhd1bi0w2x34y7zygqq5f";
+  };
+  
+  postPatch = ''
+    # Hard code correct SDK version
+    substituteInPlace pyobjc_setup.py \
+      --replace 'os.path.basename(data)[6:-4]' '"${darwin.apple_sdk.sdk.version}"'
+  '';
+
+  buildInputs = [
+  ] ++ (with darwin; [
+  ] ++ (with apple_sdk.frameworks;[
+    Foundation
+    VideoToolbox
+  ]));
+
+  propagatedBuildInputs = [
+    pyobjc-core
+    pyobjc-framework-Cocoa
+    pyobjc-framework-CoreMedia
+    pyobjc-framework-Quartz
+  ];
+
+  # clang-7: error: argument unused during compilation: '-fno-strict-overflow'
+  hardeningDisable = [ "strictoverflow" ];
+
+  # show test names instead of just dots
+  setuptoolsCheckFlagsArray = [ "--verbosity=3" ];
+
+  preCheck = ''
+    # testConstants in PyObjCTest.test_cfsocket.TestSocket returns: Segmentation fault: 11
+    export DYLD_FRAMEWORK_PATH=/System/Library/Frameworks
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Wrappers for the framework VideoToolbox on Mac OS X";
+    homepage = "https://pythonhosted.org/pyobjc-framework-VideoToolbox/";
+    license = licenses.mit;
+    platforms = platforms.darwin;
+    maintainers = with maintainers; [ SuperSandro2000 ];
+  };
+}
