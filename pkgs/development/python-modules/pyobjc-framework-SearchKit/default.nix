05ffc7fbe1b7d52abd7c18a3e9f0fad4ee64006e
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-10-28 21:17:44 +0100

    python3Packages.pyobjc-framework-SearchKit: init at 6.2.2

---
+++ pkgs/development/python-modules/pyobjc-framework-SearchKit/default.nix
@@ -0,0 +1,44 @@
+{ stdenv, buildPythonPackage, pythonOlder, fetchPypi, darwin, python,
+pyobjc-core, pyobjc-framework-CoreServices }:
+
+buildPythonPackage rec {
+  pname = "pyobjc-framework-SearchKit";
+  version = "6.2.2";
+
+  disabled = pythonOlder "3.6";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1wq7m6hmswlp0k0vwn3f4lb1m2qqi3l1bw3k55qqgzzdlqhfhc18";
+  };
+  
+  postPatch = ''
+    # Hard code correct SDK version
+    substituteInPlace pyobjc_setup.py \
+      --replace 'os.path.basename(data)[6:-4]' '"${darwin.apple_sdk.sdk.version}"'
+  '';
+
+  buildInputs = [
+  ] ++ (with darwin; [
+  ] ++ (with apple_sdk.frameworks;[
+  ]));
+
+  propagatedBuildInputs = [
+    pyobjc-core
+    pyobjc-framework-CoreServices
+  ];
+
+  # clang-7: error: argument unused during compilation: '-fno-strict-overflow'
+  hardeningDisable = [ "strictoverflow" ];
+
+  dontUseSetuptoolsCheck = true;
+  pythonImportcheck = [ "pyobjc-framework-SearchKit" ];
+
+  meta = with stdenv.lib; {
+    description = "Wrappers for the framework SearchKit on Mac OS X";
+    homepage = "https://pythonhosted.org/pyobjc-framework-SearchKit/";
+    license = licenses.mit;
+    platforms = platforms.darwin;
+    maintainers = with maintainers; [ SuperSandro2000 ];
+  };
+}
