981c8bb5e24f25014c75cbe050dd16697ca68157
Ryan Burns <rtburns@protonmail.com>
2020-10-29 00:14:42 -0700

    soil: fix dead url, enable on darwin

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15704,7 +15704,9 @@ in
 
   sofia_sip = callPackage ../development/libraries/sofia-sip { };
 
-  soil = callPackage ../development/libraries/soil { };
+  soil = callPackage ../development/libraries/soil {
+    inherit (darwin.apple_sdk.frameworks) Carbon;
+  };
 
   sonic = callPackage ../development/libraries/sonic { };
 
