5d01fa2f3a0c042dfa3b2b934ffa958d47f96c8b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-10-29 11:22:53 +0100

    ssh-audit: 2.3.0 -> 2.3.1

--- pkgs/tools/security/ssh-audit/default.nix
+++ pkgs/tools/security/ssh-audit/default.nix
@@ -2,37 +2,17 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "ssh-audit";
-  version = "2.3.0";
+  version = "2.3.1";
 
   src = fetchFromGitHub {
     owner = "jtesta";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1k5nv2cdnzd3j2p729vjw6ya0gwwkxhqijs2b7p68wvp0n3y3m77";
+    sha256 = "1h739r5nv5zkmjyyjwkw8r6d4avddjjxsamc5rffwfxi1kjavpxm";
   };
-
-  postPatch = ''
-    cp ./README.md packages/sshaudit/
-    cp ./ssh-audit.py packages/sshaudit/sshaudit.py
-    mv packages/* .
-    ls -lah
-  '';
 
   checkInputs = with python3Packages; [
     pytestCheckHook
-  ];
-
-  disabledTests = [
-    "test_resolve_error"
-    "test_resolve_hostname_without_records"
-    "test_resolve_ipv4"
-    "test_resolve_ipv6"
-    "test_resolve_ipv46_both"
-    "test_resolve_ipv46_order"
-    "test_invalid_host"
-    "test_invalid_port"
-    "test_not_connected_socket"
-    "test_ssh2_server_simple"
   ];
 
   meta = with lib; {
