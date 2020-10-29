a15f07ad4ddde4b780495d0e1b31886caab90ae3
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-29 14:06:51 +0000

    emplace: 0.3.7 -> 0.3.8

--- pkgs/tools/package-management/emplace/default.nix
+++ pkgs/tools/package-management/emplace/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "emplace";
-  version = "0.3.7";
+  version = "0.3.8";
 
   src = fetchFromGitHub {
     owner = "tversteeg";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0ihx6hrzs7wkdz9wzgarmm73dz1fb2bhssmxrgv5nzmkhygn4xfy";
+    sha256 = "1g7xrsw0ji3nbrj1a2hywxwy7f33fgbvzp7k4l9ls2icg0xsw9a3";
   };
 
-  cargoSha256 = "0yqg2hagsjaxvrj96qg6k1llkmqdqp792c2844h7fhnhlx6v2wd2";
+  cargoSha256 = "1phr0k05dxwdhj1i71v8lr91qanjvp2zgi9wjpppzixpnpcsrbrb";
 
   meta = with lib; {
     description = "Mirror installed software on multiple machines";
