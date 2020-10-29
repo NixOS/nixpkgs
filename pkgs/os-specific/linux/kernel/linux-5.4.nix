27212188ee8072ee79a4633e95fb5c731d40e1c0
Tim Steinbach <tim@nequissimus.com>
2020-10-29 16:45:17 -0400

    linux: 5.4.72 -> 5.4.73

--- pkgs/os-specific/linux/kernel/linux-5.4.nix
+++ pkgs/os-specific/linux/kernel/linux-5.4.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.4.72";
+  version = "5.4.73";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0whi5kr1ziy9y20p42adnmqks41cavsraa36g9dbbrbgsmdn890f";
+    sha256 = "1cknwnzpimjfacjbb39ay9j4lv3767j2858xz9yvwsvj7d04nhjs";
   };
 } // (args.argsOverride or {}))
