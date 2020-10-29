3a7af9bcfd8122f66aaa7432c9bd226094440de0
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-29 17:54:46 +0000

    batsignal: 1.1.2 -> 1.1.3

--- pkgs/applications/misc/batsignal/default.nix
+++ pkgs/applications/misc/batsignal/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "batsignal";
-  version = "1.1.2";
+  version = "1.1.3";
 
   src = fetchFromGitHub {
     owner = "electrickite";
     repo = "batsignal";
     rev = "${version}";
-    sha256 = "0ss5dw7wpqsf96dig6r7x4fhf6brmjdy54jyyf5nk1h9kzw4d69r";
+    sha256 = "12hj0j18db34x0xzgj6xmhvxm966d05z0rl3d9rlrcd2q96lilwf";
   };
 
   buildInputs = [ libnotify glib ];
