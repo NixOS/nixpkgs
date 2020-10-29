166eadee60ade4793b8209ec1aca3b57e114df94
wheelsandmetal <jakob@schmutz.co.uk>
2020-10-29 10:24:59 +0000

    nuraft: init at 1.1.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14671,6 +14671,8 @@ in
 
   ntrack = callPackage ../development/libraries/ntrack { };
 
+  nuraft = callPackage ../development/libraries/nuraft { };
+
   nuspell = callPackage ../development/libraries/nuspell { };
   nuspellWithDicts = dicts: callPackage ../development/libraries/nuspell/wrapper.nix { inherit dicts; };
 
