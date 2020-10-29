5ea63c942e0db71f7370c4ab0a35fa9d16b39a04
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-10-29 01:01:54 +0100

    python3Packages.pyobjc-framework-AVKit: init at 6.2.2

---
+++ pkgs/development/python-modules/pyobjc-framework-AVKit/default.nix
@@ -0,0 +1,51 @@
+{ stdenv, buildPythonPackage, pythonOlder, fetchPypi, darwin, python,
+pyobjc-core, pyobjc-framework-Quartz }:
+
+buildPythonPackage rec {
+  pname = "pyobjc-framework-AVKit";
+  version = "6.2.2";
+
+  disabled = pythonOlder "3.6";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "08m15160fb42anrjfifzgvs0ji4fmzq9fxgn5bifyl4szlkbd59h";
+  };
+  
+  postPatch = ''
+    # Hard code correct SDK version
+    substituteInPlace pyobjc_setup.py \
+      --replace 'os.path.basename(data)[6:-4]' '"${darwin.apple_sdk.sdk.version}"'
+  '';
+
+  buildInputs = [
+  ] ++ (with darwin; [
+  ] ++ (with apple_sdk.frameworks;[
+    AVKit
+    Foundation
+  ]));
+
+  propagatedBuildInputs = [
+    pyobjc-core
+    pyobjc-framework-Quartz
+  ];
+
+  # clang-7: error: argument unused during compilation: '-fno-strict-overflow'
+  hardeningDisable = [ "strictoverflow" ];
+
+  # show test names instead of just dots
+  setuptoolsCheckFlagsArray = [ "--verbosity=3" ];
+
+  preCheck = ''
+    # testConstants in PyObjCTest.test_cfsocket.TestSocket returns: Segmentation fault: 11
+    export DYLD_FRAMEWORK_PATH=/System/Library/Frameworks
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Wrappers for the framework AVKit on Mac OS X";
+    homepage = "https://pythonhosted.org/pyobjc-framework-AVKit/";
+    license = licenses.mit;
+    platforms = platforms.darwin;
+    maintainers = with maintainers; [ SuperSandro2000 ];
+  };
+}
