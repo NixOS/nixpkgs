d8bc50396e98b80833b836bc2154d5a6569c0a3c
Ben Siraphob <bensiraphob@gmail.com>
2020-10-29 23:47:19 +0700

    bintools-wrapper refactor

--- pkgs/build-support/bintools-wrapper/default.nix
+++ pkgs/build-support/bintools-wrapper/default.nix
@@ -306,9 +306,7 @@ stdenv.mkDerivation {
     ''
 
     + optionalString (libc != null && targetPlatform.isZ80) ''
-      for isa in z80; do
-        echo "-L${getLib libc}/z80/lib/$isa" >> $out/nix-support/libc-cflags
-      done
+      echo "-L${getLib libc}/z80/lib/z80" >> $out/nix-support/libc-cflags
     ''
 
     + ''
