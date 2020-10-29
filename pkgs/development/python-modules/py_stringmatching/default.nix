7a99aacc29b8b0c6969d09a5d1ff91c1bf500662
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-29 07:41:57 +0000

    python37Packages.py_stringmatching: 0.4.1 -> 0.4.2

--- pkgs/development/python-modules/py_stringmatching/default.nix
+++ pkgs/development/python-modules/py_stringmatching/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "py_stringmatching";
-  version = "0.4.1";
+  version = "0.4.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "c46db1e855cef596dfbbe1bd48fcabb30736479eff602c2bf88af10f998f1532";
+    sha256 = "c87f62698fba1612a18f8f44bd57f0c4e70aac2d7ca6dfb6ed46dabd2194453c";
   };
 
   checkInputs = [ nose ];
