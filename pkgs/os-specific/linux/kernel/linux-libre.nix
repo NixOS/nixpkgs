bd92d30ce42125ca9987d090ca408459a4b657c4
Tim Steinbach <tim@nequissimus.com>
2020-10-30 09:33:44 -0400

    linux_latest-libre: 17724 -> 17744

--- pkgs/os-specific/linux/kernel/linux-libre.nix
+++ pkgs/os-specific/linux/kernel/linux-libre.nix
@@ -1,8 +1,8 @@
 { stdenv, lib, fetchsvn, linux
 , scripts ? fetchsvn {
     url = "https://www.fsfla.org/svn/fsfla/software/linux-libre/releases/branches/";
-    rev = "17724";
-    sha256 = "0aqg8il35a1mda4icqprvriham81vvnsa04rjc86b6x7khxdhiq2";
+    rev = "17744";
+    sha256 = "120jf6d1xrpwf8yfqzfqp8a1189lca0si8fxci16sf4bdg5q1had";
   }
 , ...
 }:
