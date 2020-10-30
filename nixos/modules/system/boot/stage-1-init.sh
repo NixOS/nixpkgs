a179781696271602ee317455aff5e7fa051f5218
Graham Christensen <graham@grahamc.com>
2020-10-30 11:05:48 -0400

    stage-1: add datestamps to logs
    When the stage-1 logs get imported in to the journal, they all get
    loaded with the same timestamp. This makes it difficult to identify
    what might be taking a long time in early boot.

--- nixos/modules/system/boot/stage-1-init.sh
+++ nixos/modules/system/boot/stage-1-init.sh
@@ -120,7 +120,7 @@ eval "exec $logOutFd>&1 $logErrFd>&2"
 if test -w /dev/kmsg; then
     tee -i < /tmp/stage-1-init.log.fifo /proc/self/fd/"$logOutFd" | while read -r line; do
         if test -n "$line"; then
-            echo "<7>stage-1-init: $line" > /dev/kmsg
+            echo "<7>stage-1-init: [$(date)] $line" > /dev/kmsg
         fi
     done &
 else
