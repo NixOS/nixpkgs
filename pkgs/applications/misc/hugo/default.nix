c84b9e52e099bdd4f8f1cd94807c1228df8064d2
Sergey Lukjanov <me@slukjanov.name>
2020-10-30 21:20:52 -0700

    hugo: 0.76.5 -> 0.77.0
    (cherry picked from commit 5c4342674b7e13e583bcb52ee328c719962cd313)
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/applications/misc/hugo/default.nix
+++ pkgs/applications/misc/hugo/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "hugo";
-  version = "0.76.5";
+  version = "0.77.0";
 
   src = fetchFromGitHub {
     owner = "gohugoio";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0p7zz9cif1vihhs21nvh4n3y1p2cwpga59rilpam7yvza4nzx7ay";
+    sha256 = "1vjqddcbk8afqkjzrj9wwvz697bxhv9vz0rk2vj2ji6lz1slhc56";
   };
 
-  vendorSha256 = "0kiqy8n2df52gsgsbmr96rph8lpnw06g622123hlwk7kqg0z9ifh";
+  vendorSha256 = "03xv188jw5scqd6a8xd2s13vkn721d37bgs6a6rik7pgqmjh46c6";
 
   doCheck = false;
 
