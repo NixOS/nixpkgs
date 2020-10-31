e6bc641ef45e773a2eba5ba35fda697faf79cac7
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2020-10-30 22:22:01 -0300

    iosevka: 3.2.2 -> 3.7.1; fetch source through nodePackages
    Previously we used nodePackages only to fetch Iosevka's build
    dependencies and then fetched the source code ourselves.  Updates
    involved changing the version and hashes in the `iosevka` derivation and
    then running node-packages/generate.sh to update the build dependencies,
    which in turns updates *all* of node-packages.nix.
    
    A new proposed policy for handling node-packages.nix updates involves
    batching those updates.  Previously, that would mean `iosevka` and its
    build dependencies could end up out of sync until the batched update was
    run.
    
    To work with the new policy, we now fetch Iosevka's source code (and
    not only its dependencies) through nodePackages.  Updates are done by
    changing the source URL in node-packages.json, which eventually becomes
    part of an update of node-packages.nix, which is then propagated to
    `iosevka` itself.
    
    One con of this strategy is that errors can not be caught directly
    after the update, but only after node-packages.nix is regenerated.

--- pkgs/data/fonts/iosevka/default.nix
+++ pkgs/data/fonts/iosevka/default.nix
@@ -27,21 +27,29 @@
 
 assert (privateBuildPlan != null) -> set != null;
 
+let
+  # We don't know the attribute name for the Iosevka package as it
+  # changes not when our update script is run (which in turn updates
+  # node-packages.json, but when node-packages/generate.sh is run
+  # (which updates node-packages.nix).
+  #
+  # Doing it this way ensures that the package can always be built,
+  # although possibly an older version than ioseva-bin.
+  nodeIosevka = (
+    lib.findSingle
+      (drv: drv ? packageName && drv.packageName == "iosevka")
+      (throw "no 'iosevka' package found in nodePackages")
+      (throw "multiple 'iosevka' packages found in nodePackages")
+      (lib.attrValues nodePackages)
+  ).override (drv: { dontNpmInstall = true; });
+in
 stdenv.mkDerivation rec {
   pname = if set != null then "iosevka-${set}" else "iosevka";
-
-  version = "3.2.2";
-
-  src = fetchFromGitHub {
-    owner = "be5invis";
-    repo = "Iosevka";
-    rev = "v${version}";
-    sha256 = "1wbnp6gr3ywvspwk6i0jn68zwjmsd38arn4n2dkh7mdkrmvah81k";
-  };
+  inherit (nodeIosevka) version src;
 
   nativeBuildInputs = [
     nodejs
-    nodePackages."iosevka-build-deps-../../data/fonts/iosevka"
+    nodeIosevka
     remarshal
     otfcc
     ttfautohint-nox
@@ -61,9 +69,7 @@ stdenv.mkDerivation rec {
       echo -e "\n" >> parameters.toml
       cat "$extraParametersPath" >> parameters.toml
     ''}
-    ln -s ${
-      nodePackages."iosevka-build-deps-../../data/fonts/iosevka"
-    }/lib/node_modules/iosevka-build-deps/node_modules .
+    ln -s ${nodeIosevka}/lib/node_modules/iosevka/node_modules .
     runHook postConfigure
   '';
 
@@ -74,9 +80,11 @@ stdenv.mkDerivation rec {
   '';
 
   installPhase = ''
+    runHook preInstall
     fontdir="$out/share/fonts/truetype"
     install -d "$fontdir"
     install "dist/$pname/ttf"/* "$fontdir"
+    runHook postInstall
   '';
 
   enableParallelBuilding = true;
