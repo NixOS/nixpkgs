f4af933f9382ea6ea8d791d6d05a7bf1eb56a0ed
Florian Pester <florian.pester@cyberus-technology.de>
2020-10-31 12:53:18 +0100

    lexicon: 3.4.3 -> 3.4.4

--- pkgs/tools/admin/lexicon/default.nix
+++ pkgs/tools/admin/lexicon/default.nix
@@ -1,51 +1,25 @@
 { lib
-, python3
+, python3Packages
 , fetchFromGitHub
 }:
 
-let
-  py = python3.override {
-    packageOverrides = self: super: {
-      # until https://github.com/ags-slc/localzone/issues/1 gets resolved
-      dnspython = super.dnspython.overridePythonAttrs(oldAttrs: rec {
-        pname = "dnspython";
-        version = "1.16.0";
-        # since name is defined from the previous derivation, need to override
-        # name explicity for correct version to show in drvName
-        name = "${pname}-${version}";
-
-        src = super.fetchPypi {
-          inherit pname version;
-          extension = "zip";
-          sha256 = "00cfamn97w2vhq3id87f10mjna8ag5yz5dw0cy5s0sa3ipiyii9n";
-        };
-      });
-
-      localzone = super.localzone.overridePythonAttrs(oldAttrs: rec {
-        meta = oldAttrs.meta // { broken = false; };
-      });
-    };
-  };
-in
-  with py.pkgs;
-
-buildPythonApplication rec {
+python3Packages.buildPythonApplication rec {
   pname = "lexicon";
-  version = "3.4.3";
+  version = "3.4.4";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "AnalogJ";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1ym4gj4xyd69rsc5niilvcb72gys22rjxhj4qd574vyx3ryl34za";
+    sha256 = "1hwxx93c68k3gn6bpbb8jl443bcc14pk9ndx8wkyaykyk9ham49k";
   };
 
-  nativeBuildInputs = [
+  nativeBuildInputs = with python3Packages; [
     poetry
   ];
 
-  propagatedBuildInputs = [
+  propagatedBuildInputs = with python3Packages; [
     beautifulsoup4
     boto3
     cryptography
@@ -62,7 +36,7 @@ buildPythonApplication rec {
     zeep
   ];
 
-  checkInputs = [
+  checkInputs = with python3Packages; [
     mock
     pytest
     pytestcov
