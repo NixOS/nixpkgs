a7d7f254528740210fb8059148af7f74a548616f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-10-31 15:48:16 +0100

    apple_sdk.frameworks.SpriteKit: Fix Headers/SpriteKitBase.h:18:9: fatal error: 'AppKit/AppKit.h' file not found

--- pkgs/os-specific/darwin/apple-sdk/frameworks.nix
+++ pkgs/os-specific/darwin/apple-sdk/frameworks.nix
@@ -107,7 +107,7 @@ with frameworks; with libs; {
   SecurityInterface       = { inherit Cocoa Security SecurityFoundation; };
   ServiceManagement       = { inherit Security; };
   Social                  = {};
-  SpriteKit               = {};
+  SpriteKit               = { inherit AppKit; };
   StoreKit                = {};
   SyncServices            = {};
   SystemConfiguration     = { inherit Security; };
