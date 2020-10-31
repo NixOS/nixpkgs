c665b0321595ea596621b3259b394dd10fa5d604
R. RyanTM <ryantm-bot@ryantm.com>
2020-10-31 18:04:36 +0000

    datovka: 4.15.2 -> 4.15.5

--- pkgs/applications/networking/datovka/default.nix
+++ pkgs/applications/networking/datovka/default.nix
@@ -11,11 +11,11 @@
 
 mkDerivation rec {
   pname = "datovka";
-  version = "4.15.2";
+  version = "4.15.5";
 
   src = fetchurl {
     url = "https://secure.nic.cz/files/datove_schranky/${version}/${pname}-${version}.tar.xz";
-    sha256 = "0vna3vaivi6w7nlkwpqhwmyly0s1d5y2yg51br2f918pjhp2cp7q";
+    sha256 = "1mnw1m3wjkw8rfh6fwwrhfmkna6j19pza9cs7kyp8qj1fzzqi8my";
   };
 
   buildInputs = [ libisds qmake qtbase qtsvg libxml2 ];
