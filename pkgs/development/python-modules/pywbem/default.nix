b8ba234c8c931ab437dbb8262e119ddee054139a
freezeboy <freezeboy@users.noreply.github.com>
2020-10-31 22:50:16 +0100

    python3Packages.pywbem: 1.0.2 at 1.1.1

--- pkgs/development/python-modules/pywbem/default.nix
+++ pkgs/development/python-modules/pywbem/default.nix
@@ -1,52 +1,43 @@
 { lib, buildPythonPackage, fetchPypi, libxml2
 , m2crypto, ply, pyyaml, six, pbr, pythonOlder, isPy37
+, nocasedict, nocaselist, yamlloader, requests-mock
 , httpretty, lxml, mock, pytest, requests, decorator, unittest2
+, FormEncode, testfixtures, pytz
 }:
 
 buildPythonPackage rec {
   pname = "pywbem";
-  version = "1.0.3";
-
-  # Support added in master https://github.com/pywbem/pywbem/commit/b2f2f1a151a30355bbc6652dca69a7b30bfe941e awaiting release
-  disabled = isPy37;
+  version = "1.1.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a6c53d9426326e0e722a5b1af3a1d55810259cb3afa92ca5e4029a6d533cab37";
+    sha256 = "9GpxbgNsXZJj2M5MvosNnEe+9pY+Qz64RD/7ZIDqmII=";
   };
 
   propagatedBuildInputs = [
     mock
+    nocasedict
+    nocaselist
     pbr
     ply
     pyyaml
     six
+    yamlloader
   ] ++ lib.optionals (pythonOlder "3.0") [ m2crypto ];
 
   checkInputs = [
     decorator
+    FormEncode
     httpretty
     libxml2
     lxml
     pytest
+    pytz
     requests
+    requests-mock
+    testfixtures
     unittest2
   ];
-
-  postPatch = ''
-    # Uses deprecated library yamlordereddictloader
-    rm testsuite/test_client.py
-
-    # Wants `wbemcli` in PATH
-    rm testsuite/test_wbemcli.py
-
-    # Disables tests that use testfixtures which is currently broken by nonbuilding zope_component
-    rm testsuite/{test_logging,test_recorder,test_wbemconnection_mock}.*
-  '';
-
-  checkPhase = ''
-    pytest testsuite/
-  '';
 
   meta = with lib; {
     description = "Support for the WBEM standard for systems management";
