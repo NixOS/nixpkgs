4024f244fac38993725236299f1e8ef5a550e4d6
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-11-01 01:45:43 +0100

    python3Packages.pyobjc-framework-FileProviderUI: init at 6.2.2

---
+++ pkgs/development/python-modules/pyobjc-framework-FileProviderUI/default.nix
@@ -0,0 +1,49 @@
+{ stdenv, buildPythonPackage, pythonOlder, fetchPypi, darwin, python,
+pyobjc-core, pyobjc-framework-FileProvider }:
+
+buildPythonPackage rec {
+  pname = "pyobjc-framework-FileProviderUI";
+  version = "6.2.2";
+
+  disabled = pythonOlder "3.6";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1dljhjk1awdl7imnlims51al4a2hq1wfkx26m9lqabmd3n5dq8hv";
+  };
+  
+  postPatch = ''
+    # Hard code correct SDK version
+    substituteInPlace pyobjc_setup.py \
+      --replace 'os.path.basename(data)[6:-4]' '"${darwin.apple_sdk.sdk.version}"'
+  '';
+
+  buildInputs = [
+  ] ++ (with darwin; [
+  ] ++ (with apple_sdk.frameworks;[
+  ]));
+
+  propagatedBuildInputs = [
+    pyobjc-core
+    pyobjc-framework-FileProvider
+  ];
+
+  # clang-7: error: argument unused during compilation: '-fno-strict-overflow'
+  hardeningDisable = [ "strictoverflow" ];
+
+  # show test names instead of just dots
+  setuptoolsCheckFlagsArray = [ "--verbosity=3" ];
+
+  preCheck = ''
+    # testConstants in PyObjCTest.test_cfsocket.TestSocket returns: Segmentation fault: 11
+    export DYLD_FRAMEWORK_PATH=/System/Library/Frameworks
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Wrappers for the framework FileProviderUI on Mac OS X";
+    homepage = "https://pythonhosted.org/pyobjc-framework-FileProviderUI/";
+    license = licenses.mit;
+    platforms = platforms.darwin;
+    maintainers = with maintainers; [ SuperSandro2000 ];
+  };
+}
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5506,6 +5506,8 @@ in {
 
   pyobjc-framework-FileProvider = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-FileProvider { });
 
+  pyobjc-framework-FileProviderUI = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-FileProviderUI { });
+
   pyobjc-framework-FinderSync = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-FinderSync { });
 
   pyobjc-framework-FSEvents = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-FSEvents { });
