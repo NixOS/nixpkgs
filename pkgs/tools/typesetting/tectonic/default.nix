2bbef4e359542d9dc2cf50c163831a3f2c004a3d
Doron Behar <doron.behar@gmail.com>
2020-11-01 22:30:44 +0200

    tectonic: Install desktop file.

--- pkgs/tools/typesetting/tectonic/default.nix
+++ pkgs/tools/typesetting/tectonic/default.nix
@@ -19,6 +19,11 @@ rustPlatform.buildRustPackage rec {
   buildInputs = [ fontconfig harfbuzz openssl ]
     ++ stdenv.lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [ ApplicationServices Cocoa Foundation ]);
 
+  postInstall = stdenv.lib.optionalString stdenv.isLinux ''
+    install -D dist/appimage/tectonic.desktop -t $out/share/applications/
+    install -D dist/appimage/tectonic.svg -t $out/share/icons/hicolor/scalable/apps/
+  '';
+
   doCheck = true;
 
   meta = with stdenv.lib; {
