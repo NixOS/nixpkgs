ecc73baa098482b59c2cc3b14fb986a09d3582cf
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-11-01 01:16:15 +0100

    python3Packages.pyobjc-framework-LinkPresentation: init at 6.2.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5538,6 +5538,8 @@ in {
 
   pyobjc-framework-libdispatch = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-libdispatch { });
 
+  pyobjc-framework-LinkPresentation = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-LinkPresentation { });
+
   pyobjc-framework-LocalAuthentication = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-LocalAuthentication { });
 
   pyobjc-framework-MapKit = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-MapKit { });
