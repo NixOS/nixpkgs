c53f0aa2ca9a199238f4ced50aa5cc147da26f23
Vladimír Čunát <v@cunat.cz>
2020-11-02 16:08:58 +0100

    lib.maitainers.lostnet: init at 1.0
    Picked from master (part of 0ef1be0aa).  Needed after PR #102477.

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4928,6 +4928,12 @@
     githubId = 1202012;
     name = "Ignat Loskutov";
   };
+  lostnet = {
+    email = "lost.networking@gmail.com";
+    github = "lostnet";
+    githubId = 1422781;
+    name = "Will Young";
+  };
   louisdk1 = {
     email = "louis@louis.dk";
     github = "louisdk1";
