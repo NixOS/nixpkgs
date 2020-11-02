20d24d0b15dfb24975d88b0a19b2a7ecea624705
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-11-02 19:33:35 +0100

    python3Packages.pyserial-asyncio: propagate pyserial
    Fixes errors like this:
    
    ERROR: Could not find a version that satisfies the requirement pyserial (from pyserial-asyncio>=0.4->pysml==0.0.2) (from versions: none)

--- pkgs/development/python-modules/pyserial-asyncio/default.nix
+++ pkgs/development/python-modules/pyserial-asyncio/default.nix
@@ -7,12 +7,12 @@ buildPythonPackage rec {
 
   disabled = !isPy3k; # Doesn't support python older than 3.4
 
-  buildInputs = [ pyserial ];
-
   src = fetchPypi {
     inherit pname version;
     sha256 = "1vlsb0d03krxlj7vpvyhpinnyxyy8s3lk5rs8ba2932dhyl7f1n4";
   };
+
+  propagatedBuildInputs = [ pyserial ];
 
   meta = with stdenv.lib; {
     description = "asyncio extension package for pyserial";
