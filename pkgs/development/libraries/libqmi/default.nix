717dea95a16aeda3b419e12b5555f88659ecf4b8
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-03 00:37:36 +0000

    libqmi: 1.26.2 -> 1.26.6

--- pkgs/development/libraries/libqmi/default.nix
+++ pkgs/development/libraries/libqmi/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "libqmi";
-  version = "1.26.2";
+  version = "1.26.6";
 
   src = fetchurl {
     url = "https://www.freedesktop.org/software/libqmi/${pname}-${version}.tar.xz";
-    sha256 = "00vvvfq74awg6mk0si1cdv79f6z6wqx11h47nl78a1h7zsr6fd0k";
+    sha256 = "1fbwz6534q6n4bgabdx4svbgkf4mdyisjh3y51jjd94p22xn66d7";
   };
 
   outputs = [ "out" "dev" "devdoc" ];
