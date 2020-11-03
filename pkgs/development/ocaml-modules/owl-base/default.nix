eaa481a79ae978c6a2477c62779fdd13494dcd8e
Vincent Laporte <Vincent.Laporte@gmail.com>
2020-11-03 11:43:56 +0100

    ocamlPackages.owl: 0.9.0 → 0.10.0

--- pkgs/development/ocaml-modules/owl-base/default.nix
+++ pkgs/development/ocaml-modules/owl-base/default.nix
@@ -1,23 +1,19 @@
-{ stdenv, buildDunePackage, fetchFromGitHub, stdlib-shims }:
+{ lib, buildDunePackage, fetchurl }:
 
 buildDunePackage rec {
   pname = "owl-base";
-  version = "0.9.0";
+  version = "0.10.0";
 
   useDune2 = true;
 
-  src = fetchFromGitHub {
-    owner = "owlbarn";
-    repo = "owl";
-    rev  = version;
-    sha256 = "0xxchsymmdbwszs6barqq8x4vqz5hbap64yxq82c2la9sdxgk0vv";
+  src = fetchurl {
+    url = "https://github.com/owlbarn/owl/releases/download/${version}/owl-${version}.tbz";
+    sha256 = "148ny2cdzga1l36kcibvlz5xlyi5zvkywifxaqn8lf79n1swmlzf";
   };
-
-  propagatedBuildInputs = [ stdlib-shims ];
 
   minimumOCamlVersion = "4.10";
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Numerical computing library for Ocaml";
     homepage = "https://ocaml.xyz";
     changelog = "https://github.com/owlbarn/owl/releases";
