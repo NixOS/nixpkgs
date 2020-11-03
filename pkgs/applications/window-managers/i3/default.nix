e90cfc722dceae9ac947e51fe33f66e972beb0ea
Michal Sojka <michal.sojka@cvut.cz>
2020-11-03 12:10:58 +0100

    i3: 4.18.2 -> 4.18.3

--- pkgs/applications/window-managers/i3/default.nix
+++ pkgs/applications/window-managers/i3/default.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   pname = "i3";
-  version = "4.18.2";
+  version = "4.18.3";
 
   src = fetchurl {
     url = "https://i3wm.org/downloads/${pname}-${version}.tar.bz2";
-    sha256 = "030jym6b8b07yf4y6pb806hg8k77zsprv569gy0r72rh5zb1g1mj";
+    sha256 = "03dijnwv2n8ak9jq59fhq0rc80m5wjc9d54fslqaivnnz81pkbjk";
   };
 
   nativeBuildInputs = [ which pkgconfig makeWrapper installShellFiles ];
