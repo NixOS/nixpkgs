0e9346b1597fd2627a632b19b6bfb0fa400cc96c
Daniël de Kok <me@danieldk.eu>
2020-11-03 07:53:32 +0100

    rocm-runtime: 3.8.0 -> 3.9.0

--- pkgs/development/libraries/rocm-runtime/default.nix
+++ pkgs/development/libraries/rocm-runtime/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "rocm-runtime";
-  version = "3.8.0";
+  version = "3.9.0";
 
   src = fetchFromGitHub {
     owner = "RadeonOpenCompute";
     repo = "ROCR-Runtime";
     rev = "rocm-${version}";
-    sha256 = "1lm4cbx1d727zll85vjc1kykc72mk82nfhyyhjljv82gd4mnz00c";
+    sha256 = "034qbqznfligg4lwd95zmqa7lwcda720zbfv066nqvarlcml0kr6";
   };
 
   sourceRoot = "source/src";
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
   buildInputs = [ clang-unwrapped elfutils llvm ];
 
   cmakeFlags = [
-   "-DBITCODE_DIR=${rocm-device-libs}/lib"
+   "-DBITCODE_DIR=${rocm-device-libs}/amdgcn/bitcode"
    "-DCMAKE_PREFIX_PATH=${rocm-thunk}"
   ];
 
