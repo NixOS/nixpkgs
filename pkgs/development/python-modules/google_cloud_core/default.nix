d24b9897dd51ca677fc4775722bfc6a7b926de1d
Austin Butler <austinabutler@gmail.com>
2020-11-02 17:15:39 -0800

    python3Packages.google_cloud_core: 1.4.1 -> 1.4.3

--- pkgs/development/python-modules/google_cloud_core/default.nix
+++ pkgs/development/python-modules/google_cloud_core/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, buildPythonPackage, fetchPypi, python
-, google_api_core, grpcio, pytest, mock, setuptools }:
+{ stdenv, buildPythonPackage, fetchPypi, pythonOlder, pytestCheckHook, python
+, google_api_core, grpcio, mock }:
 
 buildPythonPackage rec {
   pname = "google-cloud-core";
@@ -10,17 +10,21 @@ buildPythonPackage rec {
     sha256 = "21afb70c1b0bce8eeb8abb5dca63c5fd37fc8aea18f4b6d60e803bd3d27e6b80";
   };
 
-  propagatedBuildInputs = [ google_api_core grpcio setuptools ];
-  checkInputs = [ pytest mock ];
+  disabled = pythonOlder "3.5";
 
-  checkPhase = ''
-    cd tests
-    ${python.interpreter} -m unittest discover
+  propagatedBuildInputs = [ google_api_core grpcio ];
+  checkInputs = [ google_api_core mock pytestCheckHook ];
+
+  pythonImportsCheck = [ "google.cloud" ];
+
+  # prevent google directory from shadowing google imports
+  preCheck = ''
+    rm -r google
   '';
 
   meta = with stdenv.lib; {
     description = "API Client library for Google Cloud: Core Helpers";
-    homepage = "https://github.com/GoogleCloudPlatform/google-cloud-python";
+    homepage = "https://github.com/googleapis/python-cloud-core";
     license = licenses.asl20;
     maintainers = with maintainers; [ ];
   };
