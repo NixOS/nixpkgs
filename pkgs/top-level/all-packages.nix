9b5c36a3b1f4f4caf29610d9a9a7a18cc02d7d37
Andreas Wiese <aw-nixos@meterriblecrew.net>
2020-11-03 14:58:14 +0100

    chrysalis: init at 0.7.9
    (cherry picked from commit 0262af9cce20f30a163b2b17ba7962883b8c7302)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -172,6 +172,8 @@ in
 
   cereal = callPackage ../development/libraries/cereal { };
 
+  chrysalis = callPackage ../applications/misc/chrysalis { };
+
   clj-kondo = callPackage ../development/tools/clj-kondo { };
 
   cmark = callPackage ../development/libraries/cmark { };
