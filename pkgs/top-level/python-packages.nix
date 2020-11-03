023f70ef8ac5f3c0030941a4e2f5118091afb995
Masanori Ogino <167209+omasanori@users.noreply.github.com>
2020-11-03 20:09:45 +0900

    pythonPackages.smugpy: remove
    Upstream has stalled since 2013. The latest release supports up to
    Python 3.2.
    
    Reference: https://github.com/chrishoffman/smugpy
    Reference: https://pypi.org/project/smugpy/
    Signed-off-by: Masanori Ogino <167209+omasanori@users.noreply.github.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6654,8 +6654,6 @@ in {
 
   smpplib = callPackage ../development/python-modules/smpplib { };
 
-  smugpy = callPackage ../development/python-modules/smugpy { };
-
   snack = toPythonModule (pkgs.newt.override { inherit (self) python; });
 
   snakebite = callPackage ../development/python-modules/snakebite { };
