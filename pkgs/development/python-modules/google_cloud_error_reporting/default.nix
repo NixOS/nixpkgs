3305029ef16af369e2ee92f6a528405f39252623
Austin Butler <austinabutler@gmail.com>
2020-11-02 17:15:57 -0800

    python3Packages.google_cloud_error_reporting: 0.34.0 -> 1.0.0

--- pkgs/development/python-modules/google_cloud_error_reporting/default.nix
+++ pkgs/development/python-modules/google_cloud_error_reporting/default.nix
@@ -1,31 +1,31 @@
-{ stdenv
-, buildPythonPackage
-, fetchPypi
-, google_cloud_logging
-, pytest
-, mock
-}:
+{ stdenv, buildPythonPackage, fetchPypi, pytestCheckHook, pythonOlder
+, google_cloud_logging, google_cloud_testutils, libcst, mock, proto-plus
+, pytest-asyncio }:
 
 buildPythonPackage rec {
   pname = "google-cloud-error-reporting";
-  version = "0.34.0";
+  version = "1.0.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "34edd11601b17c87a89c2e1cefdc27d975e1e9243a88ba3c0c48bfe6a05c404f";
+    sha256 = "1y5vkkg1cmzshj5j68zk1876857z8a7sjm0wqhf4rzgqgkr2kcdd";
   };
 
-  checkInputs = [ pytest mock ];
-  propagatedBuildInputs = [ google_cloud_logging ];
+  disabled = pythonOlder "3.6";
 
-  checkPhase = ''
+  checkInputs = [ google_cloud_testutils mock pytestCheckHook pytest-asyncio ];
+  propagatedBuildInputs = [ google_cloud_logging libcst proto-plus ];
+
+  # Disable tests that require credentials
+  disabledTests = [ "test_report_error_event" "test_report_exception" ];
+  # prevent google directory from shadowing google imports
+  preCheck = ''
     rm -r google
-    pytest tests/unit
   '';
 
   meta = with stdenv.lib; {
     description = "Stackdriver Error Reporting API client library";
-    homepage = "https://github.com/GoogleCloudPlatform/google-cloud-python";
+    homepage = "https://github.com/googleapis/python-error-reporting";
     license = licenses.asl20;
     maintainers = [ maintainers.costrouc ];
   };
