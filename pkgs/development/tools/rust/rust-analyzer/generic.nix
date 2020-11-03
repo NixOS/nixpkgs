7ac7ce8b1ab5020639c4a3d53f85e8b1dcc36ac7
oxalica <oxalicc@pm.me>
2020-11-03 22:31:16 +0800

    rust-analyzer: 2020-10-19 -> 2020-11-02

--- pkgs/development/tools/rust/rust-analyzer/generic.nix
+++ pkgs/development/tools/rust/rust-analyzer/generic.nix
@@ -16,11 +16,6 @@ rustPlatform.buildRustPackage {
     inherit rev sha256;
   };
 
-  # FIXME: Temporary fixes for our rust 1.46.0
-  patches = [
-    ./no-track_env_var.patch # Requires rustc 1.47.0
-  ];
-
   buildAndTestSubdir = "crates/rust-analyzer";
 
   cargoBuildFlags = lib.optional useMimalloc "--features=mimalloc";
