1a7b5f7e4b0e4f307a4a97afccb3a7f4a9253323
Austin Butler <austinabutler@gmail.com>
2020-11-02 17:15:10 -0800

    python3Packages.google_cloud_bigquery: 1.26.1 -> 2.1.0

--- pkgs/development/python-modules/google_cloud_bigquery/default.nix
+++ pkgs/development/python-modules/google_cloud_bigquery/default.nix
@@ -1,42 +1,46 @@
-{ stdenv
-, buildPythonPackage
-, fetchPypi
-, freezegun
-, google_resumable_media
-, google_api_core
-, google_cloud_core
-, pandas
-, pyarrow
-, pytest
-, mock
-, ipython
-}:
+{ stdenv, buildPythonPackage, fetchPypi, pytestCheckHook, pythonOlder, freezegun
+, google_api_core, google_cloud_core, google_cloud_testutils
+, google_resumable_media, grpcio, ipython, mock, pandas, proto-plus, pyarrow }:
 
 buildPythonPackage rec {
   pname = "google-cloud-bigquery";
-  version = "1.26.1";
+  version = "2.1.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "51c29b95d460486d9e0210f63e8193691cd08480b69775270e84dd3db87c1bf2";
+    sha256 = "0x5g6n151rcdgq4s80f71zpsl7bsvyyrs07l58psdpyd3kwf4sbk";
   };
 
-  checkInputs = [ pytest mock ipython freezegun ];
-  propagatedBuildInputs = [ google_resumable_media google_api_core google_cloud_core pandas pyarrow ];
+  disabled = pythonOlder "3.6";
 
-  # prevent local directory from shadowing google imports
-  # call_api_applying_custom_retry_on_timeout requires credentials
+  checkInputs =
+    [ freezegun google_cloud_testutils ipython mock pytestCheckHook ];
+  propagatedBuildInputs = [
+    google_resumable_media
+    google_api_core
+    google_cloud_core
+    pandas
+    proto-plus
+    pyarrow
+  ];
+
+  # prevent google directory from shadowing google imports
   # test_magics requires modifying sys.path
-  checkPhase = ''
+  preCheck = ''
     rm -r google
-    pytest tests/unit \
-      -k 'not call_api_applying_custom_retry_on_timeout' \
-      --ignore=tests/unit/test_magics.py
+    rm tests/unit/test_magics.py
   '';
+
+  # call_api_applying_custom_retry_on_timeout requires credentials
+  # to_dataframe_timestamp_out_of_pyarrow_bounds has inconsistent results
+  disabledTests = [
+    "call_api_applying_custom_retry_on_timeout"
+    "to_dataframe_timestamp_out_of_pyarrow_bounds"
+  ];
 
   meta = with stdenv.lib; {
     description = "Google BigQuery API client library";
-    homepage = "https://github.com/GoogleCloudPlatform/google-cloud-python";
+    homepage = "https://pypi.org/project/google-cloud-bigquery";
     license = licenses.asl20;
     maintainers = [ maintainers.costrouc ];
   };
