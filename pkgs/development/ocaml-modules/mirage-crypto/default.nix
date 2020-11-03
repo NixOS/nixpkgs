50fa2dbc729b0a84e9f019c0a6fc9aa9255c6b88
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-11-03 19:30:09 +0100

    ocamlPackages.mirage-crypto: 0.8.6 → 0.8.7

--- pkgs/development/ocaml-modules/mirage-crypto/default.nix
+++ pkgs/development/ocaml-modules/mirage-crypto/default.nix
@@ -4,11 +4,11 @@ buildDunePackage rec {
   minimumOCamlVersion = "4.08";
 
   pname = "mirage-crypto";
-  version = "0.8.6";
+  version = "0.8.7";
 
   src = fetchurl {
     url = "https://github.com/mirage/mirage-crypto/releases/download/v${version}/mirage-crypto-v${version}.tbz";
-    sha256 = "1fghg89lpm1iks6nk1jhqcimpvb52jih0ys9bxbn2f343l0njbkq";
+    sha256 = "1gx86h6kk39zq3kvl854jc2ap2755paalp1f7iv8r9js2xnbxfxy";
   };
 
   useDune2 = true;
