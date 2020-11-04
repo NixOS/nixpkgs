f877a6c374ab18b629e88b5d48a214f39f2718c5
Finn Behrens <me@kloenk.de>
2020-11-04 17:49:46 +0100

    onefetch: 2.5.0 -> 2.6.0

--- pkgs/tools/misc/onefetch/default.nix
+++ pkgs/tools/misc/onefetch/default.nix
@@ -3,16 +3,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "onefetch";
-  version = "2.5.0";
+  version = "2.6.0";
 
   src = fetchFromGitHub {
     owner = "o2sh";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-F1NbzcMoct0mjWKY2itz4cBqVToY7HpoaRzk3l683fY=";
+    sha256 = "sha256-KdPr/6wGyoHhEwfPektL+B1khyoRfxDHI4aFcKNU0TM=";
   };
 
-  cargoSha256 = "sha256-eBY+34VEPEAv5bqerxR3i7cJrVb1GtvmWyrM7uuef2E=";
+  cargoSha256 = "sha256-MLlGz7Sid0G52//XekqkZgzLqFL7xoUwT6OvpaF4jm0=";
 
   buildInputs = with stdenv;
     lib.optionals isDarwin [ CoreFoundation libiconv libresolv Security ];
@@ -21,6 +21,6 @@ rustPlatform.buildRustPackage rec {
     description = "Git repository summary on your terminal";
     homepage = "https://github.com/o2sh/onefetch";
     license = licenses.mit;
-    maintainers = with maintainers; [ filalex77 ];
+    maintainers = with maintainers; [ filalex77 kloenk ];
   };
 }
