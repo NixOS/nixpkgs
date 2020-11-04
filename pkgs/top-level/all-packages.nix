4c4976a74f2bb769b1891e24de22b7b552c1946a
gnidorah <gnidorah@users.noreply.github.com>
2020-11-04 10:33:53 +0300

    kde2-decoration: move to libsForQt5 its a kdecoration plugin like breeze-qt5 or oxygen

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15259,6 +15259,8 @@ in
 
     kdb = callPackage ../development/libraries/kdb { };
 
+    kde2-decoration = callPackage ../data/themes/kde2 { };
+
     kdiagram = callPackage ../development/libraries/kdiagram { };
 
     kdsoap = callPackage ../development/libraries/kdsoap { };
@@ -19271,8 +19273,6 @@ in
   kanji-stroke-order-font = callPackage ../data/fonts/kanji-stroke-order-font {};
 
   kawkab-mono-font = callPackage ../data/fonts/kawkab-mono {};
-
-  kde2-decoration = libsForQt514.callPackage ../data/themes/kde2 { };
 
   kochi-substitute = callPackage ../data/fonts/kochi-substitute {};
 
