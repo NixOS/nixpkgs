aedbc02a974daaf038db584de3d1aead758b390f
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-04 06:07:07 +0000

    procs: 0.10.4 -> 0.10.5

--- pkgs/tools/admin/procs/default.nix
+++ pkgs/tools/admin/procs/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "procs";
-  version = "0.10.4";
+  version = "0.10.5";
 
   src = fetchFromGitHub {
     owner = "dalance";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1a28kkxcrdfmrq2mmsfkdxfp3msklwga5nbfhjb7a7s64xh8jmjv";
+    sha256 = "0w6a8pfk5s30yi5038kdqh4dncx2pskm7a23z66c4xj3842ci79c";
   };
 
-  cargoSha256 = "1xlxjr0pkwlzm7f5xlrsf76in28r9jj41n6gn44vxqbh4x161gs1";
+  cargoSha256 = "07fwznx2czk1ibb77xcfhma9dpqps0m7rsmbb90gah6f32gah617";
 
   buildInputs = stdenv.lib.optional stdenv.isDarwin Security;
 
