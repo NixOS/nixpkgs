1fbcadbc425667379fb3fdc22a9e5a5542ff7bb5
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-04 04:10:16 +0000

    poppler_data: 0.4.9 -> 0.4.10

--- pkgs/data/misc/poppler-data/default.nix
+++ pkgs/data/misc/poppler-data/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, cmake, ninja }:
 
 stdenv.mkDerivation rec {
-  name = "poppler-data-0.4.9";
+  name = "poppler-data-0.4.10";
 
   src = fetchurl {
     url = "https://poppler.freedesktop.org/${name}.tar.gz";
-    sha256 = "04i0wgdkn5lhda8cyxd1ll4a2p41pwqrwd47n9mdpl7cx5ypx70z";
+    sha256 = "0c3vjs3p7rjc4yfacnhd865r27czmzwcr4j2z4jldi68dvvcwbvf";
   };
 
   nativeBuildInputs = [ cmake ninja ];
