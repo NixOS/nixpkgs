b9d3bc0a0f17d2c705eadd53e1317f15dfa6d63e
freezeboy <freezeboy@users.noreply.github.com>
2020-11-04 13:49:32 +0100

    kwave: build requires Qt5.14
    (cherry picked from commit 0c441e26d0866b1fc8a1694fe548e8d6fe274fbd)

--- pkgs/applications/kde/kwave.nix
+++ pkgs/applications/kde/kwave.nix
@@ -1,14 +1,17 @@
-{ mkDerivation, lib, extra-cmake-modules, kdoctools, qtmultimedia, kcompletion, kconfig, kcrash, kiconthemes, kio, audiofile, libsamplerate
-, alsaLib, libpulseaudio, flac, id3lib, libogg, libmad, libopus, libvorbis, fftw, librsvg }:
+{ mkDerivation, lib, extra-cmake-modules, kdoctools, qtmultimedia, kcompletion, kconfig
+, kcrash, kiconthemes, kio, audiofile, libsamplerate, alsaLib, libpulseaudio, flac, id3lib
+, libogg, libmad, libopus, libvorbis, fftw, librsvg, qtbase }:
 
 mkDerivation {
   name = "kwave";
+
   meta = with lib; {
     homepage = "https://kde.org/applications/en/multimedia/org.kde.kwave";
     description = "KWave is a simple media player";
     maintainers = with maintainers; [ freezeboy ];
     license = licenses.gpl2Plus;
     platforms = platforms.linux;
+    broken = lib.versionOlder qtbase.version "5.14";
   };
   nativeBuildInputs = [
     extra-cmake-modules
