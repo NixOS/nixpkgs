eda5f6a83bd354d68fa2764fe8a900fbc3f3b519
Redvers Davies <red@infect.me>
2020-11-04 05:11:17 +0000

    mediawiki: 1.34.0 -> 1.34.4

--- pkgs/servers/web-apps/mediawiki/default.nix
+++ pkgs/servers/web-apps/mediawiki/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "mediawiki";
-  version = "1.34.0";
+  version = "1.34.4";
 
   src = with stdenv.lib; fetchurl {
     url = "https://releases.wikimedia.org/mediawiki/${versions.majorMinor version}/${pname}-${version}.tar.gz";
-    sha256 = "1lckjnharwxh9xb7gxdxrkb0r3xgd0dh4019cnbixn5mmzgc696y";
+    sha256 = "09sj7xsfsnki8pw1hx5dwikwqgr9pcwhfpklsszsdywdj35wqs9b";
   };
 
   prePatch = ''
