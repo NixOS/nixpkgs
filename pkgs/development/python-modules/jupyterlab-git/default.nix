cfb81cd55ebd294f2de64ef3f09e79482c19bcc3
Frederik Rietdijk <fridh@fridh.nl>
2020-11-04 15:04:23 +0100

    python3Packages.jupyterlab_git: 0.22.2 -> 0.22.3

--- pkgs/development/python-modules/jupyterlab-git/default.nix
+++ pkgs/development/python-modules/jupyterlab-git/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "jupyterlab_git";
-  version = "0.22.2";
+  version = "0.22.3";
   disabled = pythonOlder "3.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "9966a569428e67cc1688fbc2e05f51b0ceeefd8dfe30737e78a501ce3105103d";
+    sha256 = "434ff9edd1190809e02e0cbf50090c28de48a51e151a1f904ac66e902244398d";
   };
 
   propagatedBuildInputs = [ notebook nbdime git ];
