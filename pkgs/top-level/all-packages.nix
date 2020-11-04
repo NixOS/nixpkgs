445dde63041d516195926d2a2358541f9fbc8b7e
Ben Siraphob <bensiraphob@gmail.com>
2020-11-04 21:13:06 +0700

    Initial implementation of mmix cross-compile

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5642,6 +5642,8 @@ in
 
   mmake = callPackage ../tools/misc/mmake { };
 
+  mmixware = callPackage ../development/tools/mmixware { };
+
   modemmanager = callPackage ../tools/networking/modem-manager {};
 
   modem-manager-gui = callPackage ../applications/networking/modem-manager-gui {};
