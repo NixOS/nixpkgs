3030f39f41a70cc91a3c1ff9feec3abeb0294aa3
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-04 05:13:08 +0000

    php73Packages.composer2: 2.0.0 -> 2.0.4

--- pkgs/development/php-packages/composer/2.0.nix
+++ pkgs/development/php-packages/composer/2.0.nix
@@ -1,14 +1,14 @@
 { mkDerivation, fetchurl, pkgs, lib, php }:
 let
   pname = "composer";
-  version = "2.0.0";
+  version = "2.0.4";
 in
 mkDerivation {
   inherit pname version;
 
   src = fetchurl {
     url = "https://getcomposer.org/download/${version}/composer.phar";
-    sha256 = "11fjplbrscnw0fs5hmw4bmszg5a87ig189175407i1ip5fm5g5hk";
+    sha256 = "03bnaifywh8pmpzl0b8r3rm3radj0rz176vzkz327j99fi3vrcn3";
   };
 
   dontUnpack = true;
