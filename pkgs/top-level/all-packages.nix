efa1a8b5c43a8d8e9602480d5da313f2cd67573a
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2020-11-04 16:04:13 +0200

    pass-secret-service: init at unstable-2020-04-12

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21401,6 +21401,8 @@ in
 
   musikcube = callPackage ../applications/audio/musikcube {};
 
+  pass-secret-service = callPackage ../applications/misc/pass-secret-service { };
+
   pinboard = with python3Packages; toPythonApplication pinboard;
 
   pinboard-notes-backup = haskell.lib.overrideCabal
