765b5f9248c3904ddd7b606ac9d7138238d4328a
freezeboy <freezeboy@users.noreply.github.com>
2020-11-05 15:48:37 +0100

    vnote: 2.8.2 2.8.2 -> 2.10
    Also migrate to Qt5.15

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27998,7 +27998,7 @@ in
 
   ib-controller = callPackage ../applications/office/ib/controller { jdk=oraclejdk8; };
 
-  vnote = libsForQt514.callPackage ../applications/office/vnote { };
+  vnote = libsForQt5.callPackage ../applications/office/vnote { };
 
   ssh-audit = callPackage ../tools/security/ssh-audit { };
 
