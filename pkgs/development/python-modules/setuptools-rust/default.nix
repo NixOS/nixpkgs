ae614ffc30f73ae85339247c7f309ad11cdad0c7
Daniël de Kok <me@danieldk.eu>
2020-11-05 20:47:09 +0100

    python3Packages.setuptools-rust: 0.11.3 -> 0.11.4
    Changelog:
    
    https://github.com/PyO3/setuptools-rust/releases/tag/v0.11.4

--- pkgs/development/python-modules/setuptools-rust/default.nix
+++ pkgs/development/python-modules/setuptools-rust/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , buildPythonPackage
 , fetchPypi
+, isPy27
 , semantic-version
 , setuptools
 , setuptools_scm
@@ -9,11 +10,13 @@
 
 buildPythonPackage rec {
   pname = "setuptools-rust";
-  version = "0.11.3";
+  version = "0.11.4";
+
+  disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1p21sw77197m7pciy8g25bwwaakq1675h0x1lis9sypzr46p2s11";
+    sha256 = "9714fcb94c78e6ab1864ddac7750049e105fd4f7c52103aecf40d408e94a722f";
   };
 
   nativeBuildInputs = [ setuptools_scm ];
