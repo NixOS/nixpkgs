ea374ae4ecca3954fdf0424ec43c873403bbc478
Jonathan Ringer <jonringer117@gmail.com>
2020-11-05 15:26:41 -0800

    python3Packages.munkres: fix tests

--- pkgs/development/python-modules/munkres/default.nix
+++ pkgs/development/python-modules/munkres/default.nix
@@ -2,7 +2,7 @@
 , buildPythonPackage
 , fetchPypi
 , isPy3k
-, nose
+, pytestCheckHook
 }:
 
 buildPythonPackage rec {
@@ -16,9 +16,7 @@ buildPythonPackage rec {
     sha256 = "fc44bf3c3979dada4b6b633ddeeb8ffbe8388ee9409e4d4e8310c2da1792db03";
   };
 
-  checkInputs = [ nose ];
-
-  checkPhase = "nosetests";
+  checkInputs = [ pytestCheckHook ];
 
   meta = with stdenv.lib; {
     homepage = "http://bmc.github.com/munkres/";
