8296881706f519f18e1f8b6f4975cacc4587ed36
freezeboy <freezeboy@users.noreply.github.com>
2020-11-05 19:21:24 +0100

    psi-plus: migrate to Qt5.15
    remove useless wrapQtAppsHook and stdenv

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23181,7 +23181,7 @@ in
 
   psi = libsForQt5.callPackage ../applications/networking/instant-messengers/psi { };
 
-  psi-plus = libsForQt514.callPackage ../applications/networking/instant-messengers/psi-plus { };
+  psi-plus = libsForQt5.callPackage ../applications/networking/instant-messengers/psi-plus { };
 
   psol = callPackage ../development/libraries/psol { };
 
