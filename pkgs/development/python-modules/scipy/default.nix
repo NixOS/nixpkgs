f3ecc37a857e2dfe4f03dc508dfdf8300afa867c
Frederik Rietdijk <fridh@fridh.nl>
2020-11-05 10:38:35 +0100

    python3Packages.scipy: 1.5.3 -> 1.5.4

--- pkgs/development/python-modules/scipy/default.nix
+++ pkgs/development/python-modules/scipy/default.nix
@@ -9,11 +9,11 @@ let
   });
 in buildPythonPackage rec {
   pname = "scipy";
-  version = "1.5.3";
+  version = "1.5.4";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ddae76784574cc4c172f3d5edd7308be16078dd3b977e8746860c76c195fa707";
+    sha256 = "4a453d5e5689de62e5d38edf40af3f17560bfd63c9c5bd228c18c1f99afa155b";
   };
 
   checkInputs = [ nose pytest ];
