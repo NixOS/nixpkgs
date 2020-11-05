5435f648e9895bb656a212389f812c25b76e7b98
Arnout Engelen <arnout@bzzt.net>
2020-11-05 16:48:09 +0100

    seaweedfs: init at 2.07

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7045,6 +7045,8 @@ in
 
   skim = callPackage ../tools/misc/skim { };
 
+  seaweedfs = callPackage ../applications/networking/seaweedfs { };
+
   sec = callPackage ../tools/admin/sec { };
 
   seccure = callPackage ../tools/security/seccure { };
