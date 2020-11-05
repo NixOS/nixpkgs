d56a7e5dd8d8c05ec850074fd244479c458f931e
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-11-05 09:09:15 -0600

    lazygit: 0.23.6 -> 0.23.7

--- pkgs/development/tools/lazygit/default.nix
+++ pkgs/development/tools/lazygit/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "lazygit";
-  version = "0.23.6";
+  version = "0.23.7";
 
   src = fetchFromGitHub {
     owner = "jesseduffield";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0afs3h8npbzmcrzm6kzfdva5id3zyifhhsh7zgyrxkmw60qhl498";
+    sha256 = "05s0rj785dm9dqr30ak5hi26qy0z2w71zljyln5is4frckxkcvng";
   };
 
   vendorSha256 = null;
