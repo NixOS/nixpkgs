0f56f46e8f05f65d8368d9d0aa404b0e892f8040
Daniël de Kok <me@danieldk.eu>
2020-11-05 20:44:01 +0100

    _1password-gui: 0.9.1 -> 0.9.2-1
    Changes:
    
    https://1password.community/discussion/116663/1password-beta-0-9-2
    (cherry picked from commit 4653f62f44b1062c41eaad7d3bc5bbaf2f298d18)

--- pkgs/tools/security/1password-gui/default.nix
+++ pkgs/tools/security/1password-gui/default.nix
@@ -13,11 +13,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "1password";
-  version = "0.9.1";
+  version = "0.9.2-1";
 
   src = fetchurl {
     url = "https://onepassword.s3.amazonaws.com/linux/appimage/${pname}-${version}.AppImage";
-    sha256 = "04hv7hw4s3f65kf3ggh9l0nrx25k5yj2kqv83nl7pvam5lvnv8sz";
+    sha256 = "19m8qfhmdzgz76xba9wi5cb12jqwr17afqzajvgq681i52fij0lr";
   };
 
   nativeBuildInputs = [ makeWrapper ];
