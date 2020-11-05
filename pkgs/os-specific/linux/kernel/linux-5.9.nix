91bd628cdd2c5fad6b14ab24e5f3265c041d22b0
Tim Steinbach <tim@nequissimus.com>
2020-11-05 17:28:15 -0500

    linux: 5.9.5 -> 5.9.6

--- pkgs/os-specific/linux/kernel/linux-5.9.nix
+++ pkgs/os-specific/linux/kernel/linux-5.9.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.9.5";
+  version = "5.9.6";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1q427ryzh0dfg4lxyf04qfbyckxkcczxc4a6qd3ciwx2isv262ca";
+    sha256 = "0w2kcng09nzk09dwkx4azdfgnwzbd2mz8lyl4j69bwx837z85hbc";
   };
 } // (args.argsOverride or {}))
