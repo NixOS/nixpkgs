7623d0ba85cd6a4a96b61a3be5fc6ec2b75dbd25
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-11-06 13:33:43 +0100

    ocamlPackages.asn1-combinators: 0.2.3 → 0.2.4

--- pkgs/development/ocaml-modules/asn1-combinators/default.nix
+++ pkgs/development/ocaml-modules/asn1-combinators/default.nix
@@ -6,11 +6,11 @@ buildDunePackage rec {
   minimumOCamlVersion = "4.05";
 
   pname = "asn1-combinators";
-  version = "0.2.3";
+  version = "0.2.4";
 
   src = fetchurl {
     url = "https://github.com/mirleft/ocaml-asn1-combinators/releases/download/v${version}/asn1-combinators-v${version}.tbz";
-    sha256 = "1z73hc17f5m2i4bfxw0g94fsri67f8vha812mm8klz4ggs8y7d6r";
+    sha256 = "09rn5wwqhwg7x51b9ycl15s7007hgha6lwaz2bpw85fr70jq3i9r";
   };
 
   propagatedBuildInputs = [ cstruct zarith bigarray-compat stdlib-shims ptime ];
