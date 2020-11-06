NixOS/nixpkgs

commit af27072f1d35dcde78ab1e3b384082f5498725e6

python3Packages.pyelftools: 0.26 -> unstable-09-23
And fix the tests.

Signed-off-by: Pamplemousse <xav.maso@gmail.com>

pkgs/development/python-modules/pyelftools/default.nix
@@ -1,31 +1,34 @@
 { stdenv
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 , python
 }:
 
 buildPythonPackage rec {
   pname = "pyelftools";
-  version = "0.26";
+  version = "unstable-2020-09-23";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "86ac6cee19f6c945e8dedf78c6ee74f1112bd14da5a658d8c9d4103aed5756a2";
+  src = fetchFromGitHub {
+    owner = "eliben";
+    repo = pname;
+    rev = "ab84e68837113b2d700ad379d94c1dd4a73125ea";
+    sha256 = "sha256-O7l1kj0k8bOSOtZJVzS674oVnM+X3oP00Ybs0qjb64Q=";
   };
 
+  doCheck = stdenv.is64bit;
+
   checkPhase = ''
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" test/external_tools/readelf
     ${python.interpreter} test/all_tests.py
   '';
 
-  # Tests cannot pass against system-wide readelf
-  # https://github.com/eliben/pyelftools/issues/65
-  doCheck = false;
+  pythonImportsCheck = [ "elftools" ];
 
   meta = with stdenv.lib; {
     description = "A library for analyzing ELF files and DWARF debugging information";
     homepage = "https://github.com/eliben/pyelftools";
     license = licenses.publicDomain;
-    maintainers = [ maintainers.igsha ];
+    maintainers = with maintainers; [ igsha pamplemousse ];
   };
 
 }

commit 080e6ca45948189b8cbc0f82f38710ef07c9b1ca

python3Packages.pyelftools: disable tests on Darwin
af27072 broke the build on Darwin.

There does not seem to be any way to sanely enable tests on Darwin.

Tests initially fail because of missing 'greadelf'
expected by test/run_readelf_tests.py.

This is fixed by:

- adding 'binutils-unwrapped' to checkInputs
- hardlinking 'readelf' to 'greadelf' in a temp bin dir
- exporting temp bin dir to PATH

This leads to further test failures because of 'readelf' version mismatch
causing output inconsistencies.

See test/run_readelf_tests.py:31 for further details.

Signed-off-by: Sirio Balmelli <sirio@b-ad.ch>

pkgs/development/python-modules/pyelftools/default.nix
@@ -15,7 +15,7 @@ buildPythonPackage rec {
     sha256 = "sha256-O7l1kj0k8bOSOtZJVzS674oVnM+X3oP00Ybs0qjb64Q=";
   };
 
-  doCheck = stdenv.is64bit;
+  doCheck = stdenv.is64bit && !stdenv.isDarwin;
 
   checkPhase = ''
     patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" test/external_tools/readelf