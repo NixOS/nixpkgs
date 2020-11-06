5f4378ae012b50dee3d55c4ce414dbaf1f129de6
LightDiscord <root@arnaud.sh>
2020-11-06 21:58:11 +0100

    protonmail-bridge: 1.2.3-1 -> 1.5.0
    Provide a minimal cli only version see #97991.
    
    Reviewed-by: SuperSandro2000 <sandro.jaeckel@posteo.de>
    Reviewed-by: kalbasit <github@kalbas.it>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23266,7 +23266,7 @@ in
 
   properties-cpp = callPackage ../development/libraries/properties-cpp { };
 
-  protonmail-bridge = libsForQt5.callPackage ../applications/networking/protonmail-bridge { };
+  protonmail-bridge = callPackage ../applications/networking/protonmail-bridge { };
 
   protonvpn-cli = callPackage ../applications/networking/protonvpn-cli { };
 
