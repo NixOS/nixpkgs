f14e41ecf98b6e0201f400e2203852103e4d999d
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2020-11-05 21:36:19 -0500

    texlive.combine: pass --sort to mktexlsr
    Improves reproducibility

--- pkgs/tools/typesetting/tex/texlive/combine.nix
+++ pkgs/tools/typesetting/tex/texlive/combine.nix
@@ -223,7 +223,7 @@ in (buildEnv {
     sed "1s|$| -I $out/share/texmf/scripts/texlive|" -i "$out/bin/fmtutil"
     ln -sf fmtutil "$out/bin/mktexfmt"
 
-    perl `type -P mktexlsr.pl` ./share/texmf
+    perl `type -P mktexlsr.pl` --sort ./share/texmf
     ${bin.texlinks}/bin/texlinks "$out/bin" && wrapBin
     perl `type -P fmtutil.pl` --sys --all | grep '^fmtutil' # too verbose
     #${bin.texlinks}/bin/texlinks "$out/bin" && wrapBin # do we need to regenerate format links?
@@ -233,7 +233,7 @@ in (buildEnv {
     # Regenerate the map files (this is optional)
     perl `type -P updmap.pl` --sys --force
 
-    perl `type -P mktexlsr.pl` ./share/texmf-* # to make sure
+    perl `type -P mktexlsr.pl` --sort ./share/texmf-* # to make sure
   '' +
     # install (wrappers for) scripts, based on a list from upstream texlive
   ''
