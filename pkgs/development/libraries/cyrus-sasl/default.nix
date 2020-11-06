6d000a85a3f26638a8811eb377ed4d78c3f4fd11
Scott Worley <scottworley@scottworley.com>
2020-11-06 10:27:24 -0800

    cyrus-sasl: Tarballs now hosted at github

--- pkgs/development/libraries/cyrus-sasl/default.nix
+++ pkgs/development/libraries/cyrus-sasl/default.nix
@@ -9,7 +9,8 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     urls =
-      [ "http://www.cyrusimap.org/releases/${pname}-${version}.tar.gz"
+      [ "https://github.com/cyrusimap/${pname}/releases/download/${pname}-${version}/${pname}-${version}.tar.gz"
+        "http://www.cyrusimap.org/releases/${pname}-${version}.tar.gz"
         "http://www.cyrusimap.org/releases/old/${pname}-${version}.tar.gz"
       ];
     sha256 = "1m85zcpgfdhm43cavpdkhb1s2zq1b31472hq1w1gs3xh94anp1i6";
