a4b1f04419798c9bd6a4a4269bfa9c8f4ad722f1
Jonathan Ringer <jonringer117@gmail.com>
2020-11-05 21:23:53 -0800

    python3Packages.google_auth: 1.20.1 -> 1.23.0

--- pkgs/development/python-modules/google_auth/default.nix
+++ pkgs/development/python-modules/google_auth/default.nix
@@ -17,11 +17,11 @@
 
 buildPythonPackage rec {
   pname = "google-auth";
-  version = "1.20.1";
+  version = "1.23.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "2f34dd810090d0d4c9d5787c4ad7b4413d1fbfb941e13682c7a2298d3b6cdcc8";
+    sha256 = "0h6lpbz8inxj2kpdjwrk5v7lqh1w5kkyv76d8sk3gs77y62xnxji";
   };
 
   propagatedBuildInputs = [ six pyasn1-modules cachetools rsa setuptools ];
