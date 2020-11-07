2c03cf9491e1c29bd8a3777a18a20926da2119dc
Félix Baylac-Jacqué <felix@alternativebit.fr>
2020-11-07 18:41:44 +0100

    pleroma-otp: init at 2.2.2
    Co-authored-by: flokli <flokli@flokli.de>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7020,6 +7020,8 @@ in
 
   tautulli = python3Packages.callPackage ../servers/tautulli { };
 
+  pleroma-otp = callPackage ../servers/pleroma-otp { };
+
   ploticus = callPackage ../tools/graphics/ploticus {
     libpng = libpng12;
   };
