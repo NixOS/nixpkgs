231c5a576a95391d5e4cd5b8948bfa8a6224bed1
Félix Baylac-Jacqué <felix@alternativebit.fr>
2020-11-08 15:05:37 +0100

    nixos/pleroma: init
    Adding a new pleroma NixOS module and its associated documentation.
    
    co-authored-by: duponin <duponin@locahlo.st>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -725,6 +725,7 @@
   ./services/networking/owamp.nix
   ./services/networking/pdnsd.nix
   ./services/networking/pixiecore.nix
+  ./services/networking/pleroma.nix
   ./services/networking/polipo.nix
   ./services/networking/powerdns.nix
   ./services/networking/pdns-recursor.nix
