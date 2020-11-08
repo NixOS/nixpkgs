522319f6d04c0546cb35854cd10d3595bc19dd4d
Mario Rodas <marsam@users.noreply.github.com>
2020-11-08 04:20:00 +0000

    dogdns: install completions

--- pkgs/tools/networking/dogdns/default.nix
+++ pkgs/tools/networking/dogdns/default.nix
@@ -4,6 +4,7 @@
 , pkg-config
 , openssl
 , Security
+, installShellFiles
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -17,11 +18,16 @@ rustPlatform.buildRustPackage rec {
     sha256 = "088ib0sncv0vrvnqfvxf5zc79v7pnxd2cmgp4378r6pmgax9z9zy";
   };
 
-  nativeBuildInputs = stdenv.lib.optionals stdenv.isLinux [ pkg-config ];
+  nativeBuildInputs = [ installShellFiles ]
+    ++ stdenv.lib.optionals stdenv.isLinux [ pkg-config ];
   buildInputs = stdenv.lib.optionals stdenv.isLinux [ openssl ]
     ++ stdenv.lib.optionals stdenv.isDarwin [ Security ];
 
   cargoSha256 = "08scc6vh703245rg3xkffhalrk5pisd0wg54fd49d7gdbyjivgi6";
+
+  postInstall = ''
+    installShellCompletion completions/dog.{bash,fish,zsh}
+  '';
 
   meta = with stdenv.lib; {
     description = "Command-line DNS client";
