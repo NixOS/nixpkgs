17dd9bcbca948c7519f5b55c4b69fa827c0b2365
Felix Andreas <felix.andreas95@googlemail.com>
2020-11-09 03:44:08 +0100

    vala-language-server: init at 0.48.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11966,6 +11966,8 @@ in
 
   vagrant = callPackage ../development/tools/vagrant {};
 
+  vala-language-server = callPackage ../development/tools/vala-language-server {};
+
   bashdb = callPackage ../development/tools/misc/bashdb { };
 
   gdb = callPackage ../development/tools/misc/gdb {
