3ebcc1a8f44b6b9aaed9f022812d7e1c390eaa6b
Orivej Desh <orivej@gmx.fr>
2020-11-09 01:45:09 +0000

    convertall: init at 0.8.0 (#103108)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26927,6 +26927,8 @@ in
     inherit (pkgs.gnome2) gtkglext;
   };
 
+  convertall = qt5.callPackage ../applications/science/misc/convertall { };
+
   cytoscape = callPackage ../applications/science/misc/cytoscape {
     jre = openjdk11;
   };
