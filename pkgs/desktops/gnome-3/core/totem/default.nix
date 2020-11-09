1f7ae0f29385db3a8ae5209e61f30a9c97e29b5d
Jan Tojnar <jtojnar@gmail.com>
2020-11-09 14:02:59 +0100

    gnome3.totem: clean up
    * format with nixpkgs-fmt
    * reorder attributes to more cannonical order
    * narrow patchShebangs attributes
    * add hooks to checkPhase
    * correct license

--- pkgs/desktops/gnome-3/core/totem/default.nix
+++ pkgs/desktops/gnome-3/core/totem/default.nix
@@ -1,9 +1,34 @@
-{ stdenv, fetchurl, meson, ninja, gettext, gst_all_1
-, clutter-gtk, clutter-gst, python3Packages, shared-mime-info
-, pkgconfig, gtk3, glib, gobject-introspection, totem-pl-parser
-, wrapGAppsHook, itstool, libxml2, vala, gnome3, grilo, grilo-plugins
-, libpeas, adwaita-icon-theme, gnome-desktop, gsettings-desktop-schemas
-, gdk-pixbuf, tracker, nautilus, xvfb_run }:
+{ stdenv
+, fetchurl
+, meson
+, ninja
+, gettext
+, gst_all_1
+, clutter-gtk
+, clutter-gst
+, python3Packages
+, shared-mime-info
+, pkgconfig
+, gtk3
+, glib
+, gobject-introspection
+, totem-pl-parser
+, wrapGAppsHook
+, itstool
+, libxml2
+, vala
+, gnome3
+, grilo
+, grilo-plugins
+, libpeas
+, adwaita-icon-theme
+, gnome-desktop
+, gsettings-desktop-schemas
+, gdk-pixbuf
+, tracker
+, nautilus
+, xvfb_run
+}:
 
 stdenv.mkDerivation rec {
   pname = "totem";
@@ -14,16 +39,48 @@ stdenv.mkDerivation rec {
     sha256 = "0bs33ijvxbr2prb9yj4dxglsszslsn9k258n311sld84masz4ad8";
   };
 
-  doCheck = true;
+  nativeBuildInputs = [
+    meson
+    ninja
+    vala
+    pkgconfig
+    gettext
+    python3Packages.python
+    itstool
+    gobject-introspection
+    wrapGAppsHook
+  ];
 
-  nativeBuildInputs = [ meson ninja vala pkgconfig gettext python3Packages.python itstool gobject-introspection wrapGAppsHook ];
   buildInputs = [
-    gtk3 glib grilo clutter-gtk clutter-gst totem-pl-parser grilo-plugins
-    gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good gst_all_1.gst-plugins-bad
-    gst_all_1.gst-plugins-ugly gst_all_1.gst-libav libpeas shared-mime-info
-    gdk-pixbuf libxml2 adwaita-icon-theme gnome-desktop
-    gsettings-desktop-schemas tracker nautilus
-    python3Packages.pygobject3 python3Packages.dbus-python # for plug-ins
+    gtk3
+    glib
+    grilo
+    clutter-gtk
+    clutter-gst
+    totem-pl-parser
+    grilo-plugins
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav
+    libpeas
+    shared-mime-info
+    gdk-pixbuf
+    libxml2
+    adwaita-icon-theme
+    gnome-desktop
+    gsettings-desktop-schemas
+    tracker
+    nautilus
+    # for plug-ins
+    python3Packages.pygobject3
+    python3Packages.dbus-python
+  ];
+
+  checkInputs = [
+    xvfb_run
   ];
 
   mesonFlags = [
@@ -31,16 +88,22 @@ stdenv.mkDerivation rec {
     "-Dc_args=-I${glib.dev}/include/gio-unix-2.0"
   ];
 
+  doCheck = true;
+
   postPatch = ''
     chmod +x meson_compile_python.py meson_post_install.py # patchShebangs requires executable file
-    patchShebangs .
+    patchShebangs \
+      ./meson_compile_python.py \
+      ./meson_post_install.py
   '';
 
-  checkInputs = [ xvfb_run ];
-
   checkPhase = ''
+    runHook preCheck
+
     xvfb-run -s '-screen 0 800x600x24' \
       ninja test
+
+    runHook postCheck
   '';
 
   wrapPrefixVariables = [ "PYTHONPATH" ];
@@ -56,7 +119,7 @@ stdenv.mkDerivation rec {
     homepage = "https://wiki.gnome.org/Apps/Videos";
     description = "Movie player for the GNOME desktop based on GStreamer";
     maintainers = teams.gnome.members;
-    license = licenses.gpl2;
+    license = licenses.gpl2Plus; # with exception to allow use of non-GPL compatible plug-ins
     platforms = platforms.linux;
   };
 }
