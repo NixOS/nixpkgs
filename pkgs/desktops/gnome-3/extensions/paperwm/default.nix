beafea6499338bf41d9b15e5c8770c74f6e037dc
zowoq <59103226+zowoq@users.noreply.github.com>
2020-11-10 19:29:35 +1000

    gnomeExtensions.paperwm: 38.0 -> 38.1
    https://github.com/paperwm/PaperWM/releases/tag/38.1

--- pkgs/desktops/gnome-3/extensions/paperwm/default.nix
+++ pkgs/desktops/gnome-3/extensions/paperwm/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-shell-extension-paperwm";
-  version = "38.0";
+  version = "38.1";
 
   src = fetchFromGitHub {
     owner = "paperwm";
     repo = "PaperWM";
     rev = version;
-    sha256 = "01r2ifwrl8w735d0ckzlwhvclax9dxd2ld5y2svv5bp444zbjsag";
+    sha256 = "1jq15qrq3khqpjsjbcc17amdr1k53jkvambdacdf56xbqkycvlgs";
   };
 
   uuid = "paperwm@hedning:matrix.org";
