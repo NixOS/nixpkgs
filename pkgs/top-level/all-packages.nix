c240827cbe9c2dc067621faff1f30fb7f30f79fd
Doron Behar <doron.behar@gmail.com>
2020-11-10 19:07:51 +0200

    merkaator: comment why it needs qt5.14

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22329,6 +22329,7 @@ in
 
   mercurialFull = appendToName "full" (pkgs.mercurial.override { guiSupport = true; });
 
+  # Needs qtwebkit which is broken on qt5.15
   merkaartor = libsForQt514.callPackage ../applications/misc/merkaartor { };
 
   meshlab = libsForQt5.callPackage ../applications/graphics/meshlab { };
