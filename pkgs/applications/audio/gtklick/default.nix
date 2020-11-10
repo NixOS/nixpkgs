9f50f97173cc9ea29a6df5aebda861fb0f1192d2
Frederik Rietdijk <fridh@fridh.nl>
2020-11-10 17:12:32 +0100

    gtklick: fix expression

--- pkgs/applications/audio/gtklick/default.nix
+++ pkgs/applications/audio/gtklick/default.nix
@@ -10,12 +10,15 @@ pythonPackages.buildPythonApplication rec {
   };
 
   requiredPythonModules = with pythonPackages; [
-    klick
     pyliblo
     pyGtkGlade
   ];
 
   nativeBuildInputs = [ gettext ];
+
+  makeWrapperArgs = [
+    "--prefix PATH ${stdenv.lib.makeBinPath [ klick ]}"
+  ];
 
   # wrapPythonPrograms breaks gtklick in the postFixup phase.
   # To fix it, apply wrapPythonPrograms and then clean up the wrapped file.
