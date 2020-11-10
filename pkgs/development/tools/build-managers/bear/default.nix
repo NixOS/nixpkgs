bd402504a3e7a41ade0976387c9a96af4dc75d7e
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-10 22:20:26 +0000

    bear: 3.0.1 -> 3.0.2

--- pkgs/development/tools/build-managers/bear/default.nix
+++ pkgs/development/tools/build-managers/bear/default.nix
@@ -16,13 +16,13 @@
 
 stdenv.mkDerivation rec {
   pname = "bear";
-  version = "3.0.1";
+  version = "3.0.2";
 
   src = fetchFromGitHub {
     owner = "rizsotto";
     repo = pname;
     rev = version;
-    sha256 = "02vzlm48ywf6s3fly19j94k11dqx94x8pgmkq1ylx3z3d1y3b5zb";
+    sha256 = "0x1an9sxcr0f45c9kdpgpcrc773cc28zsffbxddpqp0h1qh9s4ns";
   };
 
   nativeBuildInputs = [ cmake pkg-config ];
