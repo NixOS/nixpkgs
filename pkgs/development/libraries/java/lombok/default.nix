fd9b4ec1f70a015754e9666f9e5c54d878246254
Robert Helgesson <robert@rycee.net>
2020-11-12 23:05:24 +0100

    lombok: add bin output with lombok wrapper script
    The wrapper can be used, e.g., to run the delombok command.

--- pkgs/development/libraries/java/lombok/default.nix
+++ pkgs/development/libraries/java/lombok/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, makeWrapper, jdk }:
 
 stdenv.mkDerivation rec {
   name = "lombok-1.18.16";
@@ -8,9 +8,17 @@ stdenv.mkDerivation rec {
     sha256 = "1msys7xkaj0d7fi112fmb2z50mk46db58agzrrdyimggsszwn1kj";
   };
 
+  buildInputs = [ makeWrapper ];
+
+  outputs = [ "out" "bin" ];
+
   buildCommand = ''
     mkdir -p $out/share/java
     cp $src $out/share/java/lombok.jar
+
+    makeWrapper ${jdk}/bin/java $bin/bin/lombok \
+      --add-flags "-cp ${jdk}/lib/openjdk/lib/tools.jar:$out/share/java/lombok.jar" \
+      --add-flags lombok.launch.Main
   '';
 
   meta = {
