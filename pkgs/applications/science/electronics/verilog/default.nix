767aaba3e95acc64f4ae7e7482be2a29938196cb
Austin Seipp <aseipp@pobox.com>
2020-11-12 14:14:08 -0600

    iverilog: unstable-2020-08-24 -> unstable-2020-10-24
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/applications/science/electronics/verilog/default.nix
+++ pkgs/applications/science/electronics/verilog/default.nix
@@ -6,40 +6,35 @@
 
 let
   iverilog-test = fetchFromGitHub {
-    owner = "steveicarus";
-    repo = "ivtest";
-    rev = "6882cb8ec08926c4e356c6092f0c5f8c23328d5c";
-    sha256 = "04sj5nqzwls1y760kgnd9c2whkcrr8kvj9lisd5rvk0w580kjb2x";
+    owner  = "steveicarus";
+    repo   = "ivtest";
+    rev    = "d4c80beb845cad92136c05074b3910b822a9315f";
+    sha256 = "13cpnkki3xmhsh2v4bp2s35mhwknapcikdh85g4q6925ka940r45";
   };
 in
 stdenv.mkDerivation rec {
-  pname = "iverilog";
-  version = "unstable-2020-08-24";
+  pname   = "iverilog";
+  version = "unstable-2020-10-24";
 
   src = fetchFromGitHub {
-    owner = "steveicarus";
-    repo = pname;
-    rev = "d8556e4c86e1465b68bdc8d5ba2056ba95a42dfd";
-    sha256 = "sha256-sT9j/0Q2FD5MOGpH/quMGvAuM7t7QavRHKD9lX7Elfs=";
+    owner  = "steveicarus";
+    repo   = pname;
+    rev    = "d6e01d0c557253414109a4dde46b2966a5a3fb08";
+    sha256 = "1bl75mbycj9zpjbpay8z12384yk9ih5q9agsrjh9pva0vv3h4y4y";
   };
 
-  enableParallelBuilding = true;
-
-  preConfigure = ''
-    chmod +x $PWD/autoconf.sh
-    $PWD/autoconf.sh
-  '';
-
   nativeBuildInputs = [ autoconf gperf flex bison ];
-
   buildInputs = [ readline ncurses bzip2 zlib ];
 
-  # tests from .travis.yml
-  doCheck = true; # runs ``make check``
-  # most tests pass, but some that rely on exact text of floating-point numbers fail on aarch64.
+  preConfigure = "bash $PWD/autoconf.sh";
+
+  enableParallelBuilding = true;
+  doCheck = true;
+
+  # most tests pass, but some that rely on exact text of floating-point numbers
+  # fail on aarch64.
   doInstallCheck = !stdenv.isAarch64;
   installCheckInputs = [ perl ];
-
   installCheckPhase = ''
     # copy tests to allow writing results
     export TESTDIR=$(mktemp -d)
@@ -58,9 +53,9 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Icarus Verilog compiler";
-    homepage = "http://iverilog.icarus.com/";
-    license = with licenses; [ gpl2Plus lgpl21Plus] ;
-    maintainers = with maintainers; [ winden ];
-    platforms = platforms.all;
+    homepage    = "http://iverilog.icarus.com/";
+    license     = with licenses; [ gpl2Plus lgpl21Plus ];
+    maintainers = with maintainers; [ winden thoughtpolice ];
+    platforms   = platforms.all;
   };
 }
