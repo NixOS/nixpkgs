6e5842c8c4e571be49353d53414adfbb1b4d6fb9
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-12 06:19:20 +0000

    stripe-cli: 1.5.3 -> 1.5.4

--- pkgs/tools/admin/stripe-cli/default.nix
+++ pkgs/tools/admin/stripe-cli/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "stripe-cli";
-  version = "1.5.3";
+  version = "1.5.4";
 
   src = fetchFromGitHub {
     owner = "stripe";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0anrn7dkxgbzilh45gyqrp2930bkg3g3diarb50qp0rlim302sgy";
+    sha256 = "1gb9xk40drj7z6sw6a2hva4kvim508q4f3cfjpmr7yjzspqy32p0";
   };
 
   vendorSha256 = "05cyn9cgmijj6dl075slwm5qc6fj6m5sm414wqm50xz2fjs0400r";
