1a4bce13c011bc5b13ea95f0d55a0e2a5c14cc69
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-12 15:11:25 +0000

    aws-c-common: 0.3.11 -> 0.4.63

--- pkgs/development/libraries/aws-c-common/default.nix
+++ pkgs/development/libraries/aws-c-common/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "aws-c-common";
-  version = "0.3.11";
+  version = "0.4.63";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0a7hi4crnc3j1j39qcnd44zqdfwzw1xghcf80marx5vdf1qdzy6p";
+    sha256 = "16bc6fn1gq3nqcrzgpi2kjphq7xkkr73aljakrg89ysm6hyzyim9";
   };
 
   nativeBuildInputs = [ cmake ];
