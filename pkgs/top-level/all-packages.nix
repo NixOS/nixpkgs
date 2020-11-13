2014ed7f37f16c27246a7fbe537c324f6752a585
Peter Simons <simons@cryp.to>
2020-11-13 22:01:48 +0100

    haskellPackages: switch default package set to ghc-8.10.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9228,7 +9228,7 @@ in
 
   # Please update doc/languages-frameworks/haskell.section.md, “Our
   # current default compiler is”, if you bump this:
-  haskellPackages = dontRecurseIntoAttrs haskell.packages.ghc884;
+  haskellPackages = dontRecurseIntoAttrs haskell.packages.ghc8102;
 
   inherit (haskellPackages) ghc;
 
