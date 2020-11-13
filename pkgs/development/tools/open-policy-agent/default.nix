4c4742cb583fcb98b582c1c1159bc98b30214486
06kellyjac <dev@j-k.io>
2020-11-13 18:56:15 +0000

    open-policy-agent: add a longDescription
    Added a long description of OPA

--- pkgs/development/tools/open-policy-agent/default.nix
+++ pkgs/development/tools/open-policy-agent/default.nix
@@ -22,6 +22,12 @@ buildGoModule rec {
 
   meta = with lib; {
     description = "General-purpose policy engine";
+    longDescription = ''
+      The Open Policy Agent (OPA, pronounced "oh-pa") is an open source, general-purpose policy engine that unifies
+      policy enforcement across the stack. OPA provides a high-level declarative language that let’s you specify policy
+      as code and simple APIs to offload policy decision-making from your software. You can use OPA to enforce policies
+      in microservices, Kubernetes, CI/CD pipelines, API gateways, and more.
+    '';
     homepage = "https://www.openpolicyagent.org";
     license = licenses.asl20;
     maintainers = with maintainers; [ lewo ];
