73ca6f3e306af4cdf925cf130e45fd1b3a99b8ff
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-13 19:03:00 +0000

    intel-media-sdk: 20.3.1 -> 20.4.1

--- pkgs/development/libraries/intel-media-sdk/default.nix
+++ pkgs/development/libraries/intel-media-sdk/default.nix
@@ -3,13 +3,13 @@
 
 stdenv.mkDerivation rec {
   pname = "intel-media-sdk";
-  version = "20.3.1";
+  version = "20.4.1";
 
   src = fetchFromGitHub {
     owner = "Intel-Media-SDK";
     repo = "MediaSDK";
     rev = "intel-mediasdk-${version}";
-    sha256 = "13b698zp9b228x81yly0qx3zvj821niz3yad74b229i3z7rf8wzd";
+    sha256 = "0qnq43qjcmzkn6v2aymzi3kycndk9xw6m5f5g5sz5x53nz556bp0";
   };
 
   nativeBuildInputs = [ cmake pkgconfig ];
