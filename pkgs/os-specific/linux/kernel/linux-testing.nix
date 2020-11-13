4d89e3b76857751b9093b753f5c7553aadbcd789
Tim Steinbach <tim@nequissimus.com>
2020-11-13 11:33:33 -0500

    linux: 5.10-rc2 -> 5.10-rc3

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.10-rc2";
+  version = "5.10-rc3";
   extraMeta.branch = "5.10";
 
   # modDirVersion needs to be x.y.z, will always add .0
@@ -11,7 +11,7 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "1qh353xd48mbvavpw61r2lz1cn8007yyc2xqiacbn8xsj7xdlxkj";
+    sha256 = "08zwz6rjpd6nzhzs1hwix3709w22495sxcs0ajizyq50pydn77vd";
   };
 
   # Should the testing kernels ever be built on Hydra?
