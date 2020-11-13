2a05329b4680d8b67139ff4f84ea7f008c5d4f7d
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-13 13:38:10 +0000

    intel-gmmlib: 20.3.2 -> 20.3.3

--- pkgs/development/libraries/intel-gmmlib/default.nix
+++ pkgs/development/libraries/intel-gmmlib/default.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation rec {
   pname = "intel-gmmlib";
-  version = "20.3.2";
+  version = "20.3.3";
 
   src = fetchFromGitHub {
     owner  = "intel";
     repo   = "gmmlib";
     rev    = "${pname}-${version}";
-    sha256 = "0727pr7sknqi859gb5z472kgbbwx40574iyls8fgirm7lcz6gbd9";
+    sha256 = "1diq1gqpfv5bawvag6bzwnd3ilhacwmwx3bl67abjvbb0m3jw4lk";
   };
 
   nativeBuildInputs = [ cmake ];
