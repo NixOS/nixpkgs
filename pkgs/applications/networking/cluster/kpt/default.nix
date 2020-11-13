feab318c6a43b37b07cecf49e8d2e94d2c314971
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-13 23:41:23 +0000

    kpt: 0.36.1 -> 0.37.0

--- pkgs/applications/networking/cluster/kpt/default.nix
+++ pkgs/applications/networking/cluster/kpt/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "kpt";
-  version = "0.36.1";
+  version = "0.37.0";
 
   src = fetchFromGitHub {
     owner = "GoogleContainerTools";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0j9a7lxspgw63xzz1f8r5fb67jxm5isdvfi5450v20virxch9afi";
+    sha256 = "1b1sgwax67pazcs1lly3h3bb7ww91lcd3nr0w3r3f46d1c6iy8mn";
   };
 
-  vendorSha256 = "06kx85bf8mjmyhz5gp0la4fr8psnfz6i2rchc22sz2pgmsng1dfr";
+  vendorSha256 = "0dn6nryf3vn7r0xna02va4wbgkq0z6x8sj6g2mskfywrk0mfhadv";
 
   subPackages = [ "." ];
 
