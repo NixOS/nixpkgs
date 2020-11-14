fcc4fce7d0cc9fcb84da9173391944bb6d1e2e96
Andrew Childs <lorne@cons.org.nz>
2020-11-15 02:32:52 +0900

    git: fix darwin build (#103774)
    The install step for the darwin specific $out/etc/gitconfig file
    assumed that $out/etc already existed. This assumption is no longer
    true after f5e2ff1ed80dac74bfb0afa92f34641cce098a49 in #103421. This
    change makes make sure the directory exists before adding to it.
    
    (cherry picked from commit d686ded08ce6b38dbb8316c46f505907a2a2a3ad)

--- pkgs/applications/version-management/git-and-tools/git/default.nix
+++ pkgs/applications/version-management/git-and-tools/git/default.nix
@@ -255,6 +255,7 @@ stdenv.mkDerivation {
      '')
    + stdenv.lib.optionalString stdenv.isDarwin ''
     # enable git-credential-osxkeychain by default if darwin
+    mkdir -p $out/etc
     cat > $out/etc/gitconfig << EOF
     [credential]
       helper = osxkeychain
