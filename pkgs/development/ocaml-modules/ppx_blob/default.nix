86ae793e8720742233a186d335dfbd04819da58a
Vincent Laporte <Vincent.Laporte@gmail.com>
2020-11-14 09:42:16 +0100

    ocamlPackages.ppx_blob: 0.4.0 → 0.7.1

--- pkgs/development/ocaml-modules/ppx_blob/default.nix
+++ pkgs/development/ocaml-modules/ppx_blob/default.nix
@@ -2,16 +2,18 @@
 
 buildDunePackage rec {
   pname = "ppx_blob";
-  version = "0.4.0";
+  version = "0.7.1";
+
+  useDune2 = true;
 
   src = fetchurl {
     url = "https://github.com/johnwhitington/${pname}/releases/download/${version}/ppx_blob-${version}.tbz";
-    sha256 = "1xmslk1mwdzhy1bydgsjlcb7h544c39hvxa8lywp8w72gaggjl16";
+    sha256 = "0m616ri6kmawflphiwm6j4djds27v0fjvi8xjz1fq5ydc1sq8d0l";
   };
 
-  checkInputs = lib.optional doCheck alcotest;
+  checkInputs = [ alcotest ];
   buildInputs = [ ocaml-migrate-parsetree ];
-  doCheck = lib.versionAtLeast ocaml.version "4.03";
+  doCheck = lib.versionAtLeast ocaml.version "4.05";
 
   meta = with lib; {
     homepage = "https://github.com/johnwhitington/ppx_blob";
