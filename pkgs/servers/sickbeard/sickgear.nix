5c33271eb9e0cfdafe25ab42a65dde4530b0f0ea
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-15 17:35:05 +0000

    sickgear: 0.22.15 -> 0.23.0

--- pkgs/servers/sickbeard/sickgear.nix
+++ pkgs/servers/sickbeard/sickgear.nix
@@ -4,13 +4,13 @@ let
   pythonEnv = python2.withPackages(ps: with ps; [ cheetah ]);
 in stdenv.mkDerivation rec {
   pname = "sickgear";
-  version = "0.22.15";
+  version = "0.23.0";
 
   src = fetchFromGitHub {
     owner = "SickGear";
     repo = "SickGear";
     rev = "release_${version}";
-    sha256 = "1hc0aahfxyv05d3bskfxcv7ik5zvd1j22r3z964idhch8csxw92l";
+    sha256 = "1cn5335zpcqwra3widwnwqan2r1ardxc6ac7z5nn98r7kd3kll5v";
   };
 
   dontBuild = true;
