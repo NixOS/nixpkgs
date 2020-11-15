cff3cbd0ea8e9b002f1006899e479388dbf9b110
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-15 16:22:22 +0000

    rpm-ostree: 2020.7 -> 2020.8

--- pkgs/tools/misc/rpm-ostree/default.nix
+++ pkgs/tools/misc/rpm-ostree/default.nix
@@ -40,13 +40,13 @@
 
 stdenv.mkDerivation rec {
   pname = "rpm-ostree";
-  version = "2020.7";
+  version = "2020.8";
 
   outputs = [ "out" "dev" "man" "devdoc" ];
 
   src = fetchurl {
     url = "https://github.com/coreos/${pname}/releases/download/v${version}/${pname}-${version}.tar.xz";
-    sha256 = "1f8ajri6k5ni2rm8c75nydl8wcza0q6bv2bia3aqd0mr7iw31pbm";
+    sha256 = "1iyl6bjkj3drlwds579bh25xcmlwj9lkkbdmcdanq5b3shbmpyhi";
   };
 
   nativeBuildInputs = [
