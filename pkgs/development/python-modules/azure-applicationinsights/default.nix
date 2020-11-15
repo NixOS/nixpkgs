ba4642eb6a2a4807e54881ff537bfcfae6cf85d7
Max Wilson <max@maxwilson.dev>
2020-11-15 09:35:46 -0500

    update contact info

--- pkgs/development/python-modules/azure-applicationinsights/default.nix
+++ pkgs/development/python-modules/azure-applicationinsights/default.nix
@@ -27,6 +27,6 @@ buildPythonPackage rec {
     description = "This is the Microsoft Azure Application Insights Client Library";
     homepage = "https://github.com/Azure/azure-sdk-for-python";
     license = licenses.mit;
-    maintainers = with maintainers; [ mwilsoninsight ];
+    maintainers = with maintainers; [ maxwilson ];
   };
 }
