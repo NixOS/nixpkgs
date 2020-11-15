99847951934f5a67d65cd69495721e78a28c36c1
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-15 17:20:39 +0000

    seaweedfs: 2.09 -> 2.10

--- pkgs/applications/networking/seaweedfs/default.nix
+++ pkgs/applications/networking/seaweedfs/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "seaweedfs";
-  version = "2.09";
+  version = "2.10";
 
   src = fetchFromGitHub {
     owner = "chrislusf";
     repo = "seaweedfs";
     rev = version;
-    sha256 = "0yy5a7hr597vj6xbn7f5vzqdwnr637b3l1d62cmk0h7qbmh4anji";
+    sha256 = "0ymkkj7nf70rwlzfj6x78dx07mv32qvcb6dp8m92hc4s2ckdz3ly";
   };
 
   vendorSha256 = "1r7k0rzizs61r4gqqll7l2j7mdpi3w1ja6l4w6vxgzb45h2sjhi7";
