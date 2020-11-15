794b8f9a9e51451157ff1dff44fb86d4cd20bd40
Yves Fischer <yvesf+git@xapek.org>
2020-11-15 20:22:04 +0100

    firefox: fix wayland-lto issue reference number

--- pkgs/applications/networking/browsers/firefox/common.nix
+++ pkgs/applications/networking/browsers/firefox/common.nix
@@ -23,7 +23,7 @@
 , ffmpegSupport ? true
 , gtk3Support ? true, gtk2, gtk3, wrapGAppsHook
 , waylandSupport ? true, libxkbcommon
-# LTO is disabled since it caused segfaults on wayland see https://github.com/NixOS/nixpkgs/issues/10142
+# LTO is disabled since it caused segfaults on wayland see https://github.com/NixOS/nixpkgs/issues/101429
 , ltoSupport ? false, overrideCC, buildPackages
 , gssSupport ? true, kerberos
 , pipewireSupport ? waylandSupport && webrtcSupport, pipewire
