df62fcad616c14fff8cf5ba13b18e0fb3d152cef
Max Wilson <max@maxwilson.dev>
2020-11-15 09:35:46 -0500

    update contact info

--- pkgs/development/python-modules/azure-mgmt-keyvault/default.nix
+++ pkgs/development/python-modules/azure-mgmt-keyvault/default.nix
@@ -37,6 +37,6 @@ buildPythonPackage rec {
     description = "This is the Microsoft Azure Key Vault Management Client Library";
     homepage = "https://github.com/Azure/azure-sdk-for-python";
     license = licenses.mit;
-    maintainers = with maintainers; [ jonringer mwilsoninsight ];
+    maintainers = with maintainers; [ jonringer maxwilson ];
   };
 }
