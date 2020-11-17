7f845978213369fd0b4b4ec267afeaddb9ac3a72
06kellyjac <dev@j-k.io>
2020-11-17 19:24:42 +0000

    terragrunt: 0.26.2 -> 0.26.3

--- pkgs/applications/networking/cluster/terragrunt/default.nix
+++ pkgs/applications/networking/cluster/terragrunt/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "terragrunt";
-  version = "0.26.2";
+  version = "0.26.3";
 
   src = fetchFromGitHub {
     owner = "gruntwork-io";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0bp43rsnkq2ysdl0v28i9agan738m0zk5yn8zza6js28sx0y7kns";
+    sha256 = "0414xmmx8lzv779zvr1k265c9kh3jlx9c5acg56hl5zzvifl774r";
   };
 
   vendorSha256 = "0l85jx02dj9qvxs8l0ln5fln8vssi0fisblm5i1scz9x4a1jqg9n";
