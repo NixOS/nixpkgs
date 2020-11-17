606e9a0cafab090a34d08080cbc4fa3a0d378ac7
Jan Tojnar <jtojnar@gmail.com>
2020-11-17 17:24:48 +0100

    gnome3.gnome-contacts: clean up

--- pkgs/desktops/gnome-3/core/gnome-contacts/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-contacts/default.nix
@@ -1,8 +1,30 @@
-{ stdenv, gettext, fetchurl, evolution-data-server, fetchpatch
-, pkgconfig, libxslt, docbook_xsl, docbook_xml_dtd_42, python3, gtk3, glib, cheese
-, libchamplain, clutter-gtk, geocode-glib, gnome-desktop, gnome-online-accounts
-, wrapGAppsHook, folks, libxml2, gnome3
-, vala, meson, ninja, libhandy, gsettings-desktop-schemas
+{ stdenv
+, gettext
+, fetchurl
+, evolution-data-server
+, fetchpatch
+, pkgconfig
+, libxslt
+, docbook_xsl
+, docbook_xml_dtd_42
+, python3
+, gtk3
+, glib
+, cheese
+, libchamplain
+, clutter-gtk
+, geocode-glib
+, gnome-desktop
+, gnome-online-accounts
+, wrapGAppsHook
+, folks
+, libxml2
+, gnome3
+, vala
+, meson
+, ninja
+, libhandy
+, gsettings-desktop-schemas
 }:
 
 stdenv.mkDerivation rec {
@@ -14,34 +36,44 @@ stdenv.mkDerivation rec {
     sha256 = "0hsq0dwxjahcaxnm1m4r1lync9k2fkwzybfmkchrmn95vqcwwvf9";
   };
 
-  propagatedUserEnvPkgs = [ evolution-data-server ];
+  propagatedUserEnvPkgs = [
+    evolution-data-server
+  ];
 
   nativeBuildInputs = [
-    meson ninja pkgconfig vala gettext libxslt docbook_xsl docbook_xml_dtd_42 python3 wrapGAppsHook
+    meson
+    ninja
+    pkgconfig
+    vala
+    gettext
+    libxslt
+    docbook_xsl
+    docbook_xml_dtd_42
+    python3
+    wrapGAppsHook
   ];
 
   buildInputs = [
-    gtk3 glib evolution-data-server gsettings-desktop-schemas
-    folks gnome-desktop libhandy
-    libxml2 gnome-online-accounts cheese
-    gnome3.adwaita-icon-theme libchamplain clutter-gtk geocode-glib
-  ];
-
-  mesonFlags = [
-    # Upstream does not seem to maintain this properly: https://gitlab.gnome.org/GNOME/gnome-contacts/issues/103
-    "-Dtelepathy=false"
+    gtk3
+    glib
+    evolution-data-server
+    gsettings-desktop-schemas
+    folks
+    gnome-desktop
+    libhandy
+    libxml2
+    gnome-online-accounts
+    cheese
+    gnome3.adwaita-icon-theme
+    libchamplain
+    clutter-gtk
+    geocode-glib
   ];
 
   postPatch = ''
     chmod +x build-aux/meson_post_install.py
     patchShebangs build-aux/meson_post_install.py
   '';
-
-  # In file included from src/gnome-contacts@exe/contacts-avatar-selector.c:30:0:
-  # /nix/store/*-cheese-3.28.0/include/cheese/cheese-widget.h:26:10: fatal error: clutter-gtk/clutter-gtk.h: No such file or directory
-  #  #include <clutter-gtk/clutter-gtk.h>
-  #           ^~~~~~~~~~~~~~~~~~~~~~~~~~~
-  NIX_CFLAGS_COMPILE = "-I${stdenv.lib.getDev clutter-gtk}/include/clutter-gtk-1.0";
 
   doCheck = true;
 
@@ -56,7 +88,7 @@ stdenv.mkDerivation rec {
     homepage = "https://wiki.gnome.org/Apps/Contacts";
     description = "GNOME’s integrated address book";
     maintainers = teams.gnome.members;
-    license = licenses.gpl2;
+    license = licenses.gpl2Plus;
     platforms = platforms.linux;
   };
 }
