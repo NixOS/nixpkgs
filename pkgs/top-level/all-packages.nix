75b029f33bd595f8c8fdfb21a28bddf413e2b4f3
Pavol Rusnak <pavol@rusnak.io>
2020-11-17 16:03:53 +0100

    electron_11: init at 11.0.2
    make electron point to electron_11

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10803,7 +10803,7 @@ in
   aws-adfs = with python3Packages; toPythonApplication aws-adfs;
 
   inherit (callPackages ../development/tools/electron { })
-    electron electron_3 electron_4 electron_5 electron_6 electron_7 electron_8 electron_9 electron_10;
+    electron electron_3 electron_4 electron_5 electron_6 electron_7 electron_8 electron_9 electron_10 electron_11;
 
   autobuild = callPackage ../development/tools/misc/autobuild { };
 
