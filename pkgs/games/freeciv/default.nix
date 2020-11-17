a9383489204cfecd1e9e580deb3d19d1e64364ec
Julien Moutinho <julm+nixpkgs@sourcephile.fr>
2020-11-17 06:31:10 +0100

    freeciv: fix Qt runtime

--- pkgs/games/freeciv/default.nix
+++ pkgs/games/freeciv/default.nix
@@ -28,7 +28,8 @@ in stdenv.mkDerivation rec {
     done
   '';
 
-  nativeBuildInputs = [ autoreconfHook pkg-config ];
+  nativeBuildInputs = [ autoreconfHook pkg-config ]
+    ++ optional qtClient [ qt5.wrapQtAppsHook ];
 
   buildInputs = [ lua5_3 zlib bzip2 curl lzma gettext libiconv ]
     ++ optionals sdlClient [ SDL SDL_mixer SDL_image SDL_ttf SDL_gfx freetype fluidsynth ]
