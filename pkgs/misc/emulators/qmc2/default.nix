8313dacb11a9f835fbfa8a9d785ad1e31171eae1
Jörg Thalheim <joerg@thalheim.io>
2020-11-18 09:42:33 +0100

    treewide: remove trailing whitespace

--- pkgs/misc/emulators/qmc2/default.nix
+++ pkgs/misc/emulators/qmc2/default.nix
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
       url = "mirror://sourceforge/project/qmc2/qmc2/${version}/${pname}-${version}.tar.gz";
       sha256 = "1dzmjlfk8pdspns6zg1jmd5fqzg8igd4q38cz4a1vf39lx74svns";
   };
-  
+
   preBuild = ''
     patchShebangs scripts
   '';
