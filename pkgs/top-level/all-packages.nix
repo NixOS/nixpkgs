e06bb8ea2307eaba57f9f28728f59e7bbc509038
zowoq <59103226+zowoq@users.noreply.github.com>
2020-11-19 08:50:02 +1000

    grobi: fix failing test on go 1.15

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2236,9 +2236,7 @@ in
 
   gringo = callPackage ../tools/misc/gringo { };
 
-  grobi = callPackage ../tools/X11/grobi {
-    buildGoModule = buildGo114Module;
-  };
+  grobi = callPackage ../tools/X11/grobi { };
 
   gscan2pdf = callPackage ../applications/graphics/gscan2pdf { };
 
