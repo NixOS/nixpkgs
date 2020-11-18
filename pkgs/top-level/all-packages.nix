c86f563e2a84ca5e53a8ca42de4c4d35cfeebe51
zowoq <59103226+zowoq@users.noreply.github.com>
2020-11-19 07:45:18 +1000

    Revert "prometheus: pin to go 1.14"
    This reverts commit ba17782524508c5e4f8af646bfeb7553185d4a6e.
    
    Fixed in https://github.com/prometheus/prometheus/releases/tag/v2.21.0
    
    (cherry picked from commit 8ec3ec72263e69f39039cab13359a64fb8eba63d)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16862,10 +16862,7 @@ in
   postgresql_jdbc = callPackage ../development/java-modules/postgresql_jdbc { };
 
   prom2json = callPackage ../servers/monitoring/prometheus/prom2json.nix { };
-  prometheus = callPackage ../servers/monitoring/prometheus {
-    buildGoPackage = buildGo114Package;
-    go = go_1_14;
-  };
+  prometheus = callPackage ../servers/monitoring/prometheus { };
   prometheus-alertmanager = callPackage ../servers/monitoring/prometheus/alertmanager.nix { };
   prometheus-apcupsd-exporter = callPackage ../servers/monitoring/prometheus/apcupsd-exporter.nix { };
   prometheus-aws-s3-exporter = callPackage ../servers/monitoring/prometheus/aws-s3-exporter.nix { };
