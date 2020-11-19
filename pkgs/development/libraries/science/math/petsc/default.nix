a1d06b98cea4c5cfd9b8db7f255758f22eaa6e56
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-19 19:49:01 +0000

    petsc: 3.14.0 -> 3.14.1

--- pkgs/development/libraries/science/math/petsc/default.nix
+++ pkgs/development/libraries/science/math/petsc/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "petsc";
-  version = "3.14.0";
+  version = "3.14.1";
 
   src = fetchurl {
     url = "http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-${version}.tar.gz";
-    sha256 = "1hq3igm90bnl44vyjdbkpqmqk7496pakcswzc2vq57l8d27nhdxz";
+    sha256 = "1y3fhg0l69qp9qn6vjyqdhqhhbvsa7jp8g7zcm0pwzm7gkcldqnx";
   };
 
   nativeBuildInputs = [ blas gfortran gfortran.cc.lib lapack python ];
