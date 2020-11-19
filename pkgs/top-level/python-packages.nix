f78411700d46a5157b6e8d498df60b0b2e2d3d42
freezeboy <freezeboy@users.noreply.github.com>
2020-11-19 01:09:05 +0100

    python3Packages.ndjson: init at 0.3.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4026,6 +4026,8 @@ in {
 
   ndg-httpsclient = callPackage ../development/python-modules/ndg-httpsclient { };
 
+  ndjson = callPackage ../development/python-modules/ndjson { };
+
   ndtypes = callPackage ../development/python-modules/ndtypes { };
 
   neo = callPackage ../development/python-modules/neo { };
