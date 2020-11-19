c81c3c3daff4a96980da2fce2d80a9e57f9db953
Tom McLaughlin <tom@codedown.io>
2020-11-19 12:29:02 -0700

    cling: init at 0.7.0 (#64319)
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10190,6 +10190,8 @@ in
 
   ceptre = callPackage ../development/interpreters/ceptre { };
 
+  cling = callPackage ../development/interpreters/cling { };
+
   clips = callPackage ../development/interpreters/clips { };
 
   clisp = callPackage ../development/interpreters/clisp { };
