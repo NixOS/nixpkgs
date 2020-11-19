2ed9eee12d0986303e5f8ef6a21bd0603a6cd3dd
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-11-19 15:13:31 +0100

    ocamlPackages.ca-certs: 0.1.2 → 0.1.3

--- pkgs/development/ocaml-modules/ca-certs/default.nix
+++ pkgs/development/ocaml-modules/ca-certs/default.nix
@@ -1,21 +1,21 @@
 { lib, buildDunePackage, fetchurl
-, bos, fpath, rresult, ptime, mirage-crypto, x509
+, bos, fpath, rresult, ptime, mirage-crypto, x509, astring, logs
 }:
 
 buildDunePackage rec {
   pname = "ca-certs";
-  version = "0.1.2";
+  version = "0.1.3";
 
   minimumOCamlVersion = "4.07";
 
   src = fetchurl {
     url = "https://github.com/mirage/ca-certs/releases/download/v${version}/ca-certs-v${version}.tbz";
-    sha256 = "10bbqbrsqcmslz56zc5407knalh3kqypbm7lblnzzbidbapa9wpz";
+    sha256 = "0jpghxjp2n8wx6ig0d2x87ycaql6mb92w8ai3xh3jb288m7g02zn";
   };
 
   useDune2 = true;
 
-  propagatedBuildInputs = [ bos fpath rresult ptime mirage-crypto x509 ];
+  propagatedBuildInputs = [ bos fpath rresult ptime mirage-crypto x509 astring logs ];
 
   # tests need access to network and systemwide ca cert chain
   doCheck = false;
