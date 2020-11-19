4b51d8ae6cd45feffbd8c03e37f86515959b9da6
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-11-19 21:31:27 +0100

    homeassistant: 0.118.0 -> 0.118.1

--- pkgs/servers/home-assistant/default.nix
+++ pkgs/servers/home-assistant/default.nix
@@ -62,7 +62,7 @@ let
   extraBuildInputs = extraPackages py.pkgs;
 
   # Don't forget to run parse-requirements.py after updating
-  hassVersion = "0.118.0";
+  hassVersion = "0.118.1";
 
 in with py.pkgs; buildPythonApplication rec {
   pname = "homeassistant";
@@ -78,7 +78,7 @@ in with py.pkgs; buildPythonApplication rec {
     owner = "home-assistant";
     repo = "core";
     rev = version;
-    sha256 = "0q3kjsxp82r11gqsnwcw8rnqhgi46214hw84ibw8956w7h75ik5m";
+    sha256 = "1l2jqvq3amwdr4v6xq7df19bf75s1dpgcgnaacsx7k7pfwsyn63f";
   };
 
   # leave this in, so users don't have to constantly update their downstream patch handling
