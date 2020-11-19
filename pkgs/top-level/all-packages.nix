90577e73561730c890a3868faa16461881497b6b
Jörg Thalheim <joerg@thalheim.io>
2020-11-19 13:58:53 +0100

    fmt_6: remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12685,8 +12685,7 @@ in
 
   flyway = callPackage ../development/tools/flyway { };
 
-  inherit (callPackages ../development/libraries/fmt { })
-    fmt_6 fmt_7;
+  inherit (callPackages ../development/libraries/fmt { }) fmt_7;
 
   fmt = fmt_7;
 
