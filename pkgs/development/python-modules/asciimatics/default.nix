b9049a50a8bafd3995f4547da0ca05ae88bf4334
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-11-20 01:46:46 +0100

    pythonPackages.asciimatics: add pythonImportsCheck

--- pkgs/development/python-modules/asciimatics/default.nix
+++ pkgs/development/python-modules/asciimatics/default.nix
@@ -39,6 +39,13 @@ buildPythonPackage rec {
   # which is too complicated to setup here
   doCheck = false;
 
+  pythonImportsCheck =  [
+    "asciimatics.effects"
+    "asciimatics.renderers"
+    "asciimatics.scene"
+    "asciimatics.screen"
+  ];
+
   meta = with lib; {
     description = "Helps to create full-screen text UIs (from interactive forms to ASCII animations) on any platform";
     homepage = "https://github.com/peterbrittain/asciimatics";
