43bfd7e5b1a70fe4be9b9c077eccb15fd50f6edc
Jack Kelly <jack@jackkelly.name>
2020-11-21 11:44:25 +1000

    {ec2,openstack}-metadata-fetcher: unconditionally fetch metadata
    The metadata fetcher scripts run each time an instance starts, and it
    is not safe to assume that responses from the instance metadata
    service (IMDS) will be as they were on first boot.
    
    Example: an EC2 instance can have its user data changed while
    the instance is stopped. When the instance is restarted, we want to
    see the new user data applied.

--- nixos/modules/virtualisation/openstack-metadata-fetcher.nix
+++ nixos/modules/virtualisation/openstack-metadata-fetcher.nix
@@ -1,7 +1,12 @@
 { targetRoot, wgetExtraOptions }:
+
+# OpenStack's metadata service aims to be EC2-compatible. Where
+# possible, try to keep the set of fetched metadata in sync with
+# ./ec2-metadata-fetcher.nix .
 ''
   metaDir=${targetRoot}etc/ec2-metadata
   mkdir -m 0755 -p "$metaDir"
+  rm -f "$metaDir/*"
 
   echo "getting instance metadata..."
 
@@ -9,19 +14,8 @@
     wget ${wgetExtraOptions} "$@"
   }
 
-  if ! [ -e "$metaDir/ami-manifest-path" ]; then
-    wget_imds -O "$metaDir/ami-manifest-path" http://169.254.169.254/1.0/meta-data/ami-manifest-path
-  fi
-
-  if ! [ -e "$metaDir/user-data" ]; then
-    wget_imds -O "$metaDir/user-data" http://169.254.169.254/1.0/user-data && chmod 600 "$metaDir/user-data"
-  fi
-
-  if ! [ -e "$metaDir/hostname" ]; then
-    wget_imds -O "$metaDir/hostname" http://169.254.169.254/1.0/meta-data/hostname
-  fi
-
-  if ! [ -e "$metaDir/public-keys-0-openssh-key" ]; then
-    wget_imds -O "$metaDir/public-keys-0-openssh-key" http://169.254.169.254/1.0/meta-data/public-keys/0/openssh-key
-  fi
+  wget_imds -O "$metaDir/ami-manifest-path" http://169.254.169.254/1.0/meta-data/ami-manifest-path
+  wget_imds -O "$metaDir/user-data" http://169.254.169.254/1.0/user-data && chmod 600 "$metaDir/user-data"
+  wget_imds -O "$metaDir/hostname" http://169.254.169.254/1.0/meta-data/hostname
+  wget_imds -O "$metaDir/public-keys-0-openssh-key" http://169.254.169.254/1.0/meta-data/public-keys/0/openssh-key
 ''
