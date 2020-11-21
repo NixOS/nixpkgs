85f08c086922f605ac4d83a2e77d790f3f46ecea
freezeboy <freezeboy@users.noreply.github.com>
2020-11-21 02:27:24 +0100

    mellowplayer: migrate to Qt5.15

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2874,7 +2874,7 @@ in
 
   clementineUnfree = clementine.unfree;
 
-  mellowplayer = libsForQt514.callPackage ../applications/audio/mellowplayer { };
+  mellowplayer = libsForQt5.callPackage ../applications/audio/mellowplayer { };
 
   ciopfs = callPackage ../tools/filesystems/ciopfs { };
 
