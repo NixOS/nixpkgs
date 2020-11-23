e5b105b4a883398556bbcf9083d892795eb24ac8
Kira Bruneau <kira.bruneau@gmail.com>
2020-11-22 23:16:23 -0500

    ccache: 4.0 -> 4.1

--- pkgs/development/tools/misc/ccache/default.nix
+++ pkgs/development/tools/misc/ccache/default.nix
@@ -1,7 +1,6 @@
 { lib
 , stdenv
 , fetchFromGitHub
-, fetchpatch
 , substituteAll
 , binutils
 , asciidoc
@@ -14,33 +13,16 @@
 
 let ccache = stdenv.mkDerivation rec {
   pname = "ccache";
-  version = "4.0";
+  version = "4.1";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "1frcplrv61m2iwc6jwycpbcz1101xl6s4sh8p87prdj98l60lyrx";
+    sha256 = "1az11q3wmr8wc7alx9l70wq9am41cm0y17g5gsaqmahws3dxfi8m";
   };
 
-  # TODO: Remove patches from upstream in next release
-  patches = [
-    # Fix badly named man page filename
-    (fetchpatch {
-      url = "https://github.com/ccache/ccache/commit/223e706fb24ce86eb0ad86079a97e6f345b9ef40.patch";
-      sha256 = "1h7amp3ka45a79zwlxh8qnzx6n371gnfpfgijcqljps7skhl5gjg";
-    })
-    # Build and install man page by default
-    (fetchpatch {
-      url = "https://github.com/ccache/ccache/commit/294ff2face26448afa68e3ef7b68bf4898d6dc77.patch";
-      sha256 = "0rx69qn41bgksc4m3p59nk5d6rz72rwnfska9mh5j62pzfm8axja";
-    })
-    # Fixes use of undeclared identifier 'CLONE_NOOWNERCOPY' on darwin
-    (fetchpatch {
-      url = "https://github.com/ccache/ccache/commit/411c010c3a5ee690cd42b23ffcf026ae009e2897.patch";
-      sha256 = "062s424d0d0psp6wjhmfnfn1s5dsrz403hdid5drm6l2san0jhq0";
-    })
-  ] ++ lib.optional stdenv.isDarwin (substituteAll {
+  patches = lib.optional stdenv.isDarwin (substituteAll {
     src = ./force-objdump-on-darwin.patch;
     objdump = "${binutils.bintools}/bin/objdump";
   });
