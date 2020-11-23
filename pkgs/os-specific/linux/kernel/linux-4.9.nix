856d7ae12bf3c99567e1636c1329f8048dd9b626
Tim Steinbach <tim@nequissimus.com>
2020-11-23 10:33:32 -0500

    linux: 4.9.244 -> 4.9.245

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.244";
+  version = "4.9.245";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "02givxp0y04qma5nlqbpyxdcl7xdb41p3gw7kgj2rmwdanhzaylr";
+    sha256 = "1vxsd3g96vbykrmfnj9qali0p868h678qzcqvf4yrhya47k6pnnb";
   };
 } // (args.argsOverride or {}))
