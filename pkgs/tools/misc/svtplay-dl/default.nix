18fab62368acb6f28b11cb2f37bf77ada0aa60c4
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2020-11-24 16:00:25 +0100

    svtplay-dl: Enable on macOS as well

--- pkgs/tools/misc/svtplay-dl/default.nix
+++ pkgs/tools/misc/svtplay-dl/default.nix
@@ -45,7 +45,7 @@ in stdenv.mkDerivation rec {
     homepage = "https://github.com/spaam/svtplay-dl";
     description = "Command-line tool to download videos from svtplay.se and other sites";
     license = licenses.mit;
-    platforms = stdenv.lib.platforms.linux;
+    platforms = stdenv.lib.platforms.unix;
     maintainers = [ maintainers.rycee ];
   };
 }
