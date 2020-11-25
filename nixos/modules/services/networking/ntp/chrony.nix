cafb08f6792ed46d1f3de58fe5d4138860b7063d
snicket2100 <57048005+snicket2100@users.noreply.github.com>
2020-11-25 22:57:11 +0100

    nixos/chrony: systemd service hardening
    The service can successfully work with more systemd sandboxing
    in place. Changing `ProtectSystem` from `full` to `strict` makes
    the entire filesystem read-only, with the exception of two
    directories listed under `ReadWritePaths`. Many other sandbox
    options added as well, unfortunately I couldn't source them
    from the project itself as they only provide a very basic
    systemd service template.
    
    The `/var/run/chrony` path is compiled in the `chronyd` binary,
    as well as in the `chronyc` client.

--- nixos/modules/services/networking/ntp/chrony.nix
+++ nixos/modules/services/networking/ntp/chrony.nix
@@ -5,6 +5,7 @@ with lib;
 let
   cfg = config.services.chrony;
 
+  runDir = "/var/run/chrony";
   stateDir = "/var/lib/chrony";
   driftFile = "${stateDir}/chrony.drift";
   keyFile = "${stateDir}/chrony.keys";
@@ -116,9 +117,34 @@ in
           { Type = "simple";
             ExecStart = "${pkgs.chrony}/bin/chronyd ${chronyFlags}";
 
-            ProtectHome = "yes";
-            ProtectSystem = "full";
+            KeyringMode = "private";
+            LockPersonality = true;
+            MemoryDenyWriteExecute = true;
+            NoNewPrivileges = true;
+            PrivateMounts = "yes";
             PrivateTmp = "yes";
+            ProtectControlGroups = true;
+            ProtectHome = "yes";
+            ProtectHostname = true;
+            ProtectKernelModules = true;
+            ProtectKernelTunables = true;
+            ProtectSystem = "strict";
+            ReadWritePaths = [ runDir stateDir ];
+            RemoveIPC = true;
+            RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" ];
+            RestrictNamespaces = true;
+            RestrictRealtime = true;
+            RestrictSUIDSGID = true;
+            SystemCallFilter = "@system-service @clock";
+            SystemCallArchitectures = "native";
+
+            # even though in the default configuration chrony does not access the rtc clock,
+            # it may be configured to so so either with the 'rtcfile' configuration option
+            # or using the '-s' flag. so we make sure rtc devices can still be used by it.
+            # at the same time there is no need for chrony to access any other device types.
+            DeviceAllow = "char-rtc";
+            DevicePolicy = "closed";
+
           };
 
       };
