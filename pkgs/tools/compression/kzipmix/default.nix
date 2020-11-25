6310923b28fef7a3724e3bd2ddc0d886f070b0d4
Ivar <ivar.scholten@protonmail.com>
2020-11-25 03:34:48 +0100

    kzipmix: 20091108 -> 20200115

--- pkgs/tools/compression/kzipmix/default.nix
+++ pkgs/tools/compression/kzipmix/default.nix
@@ -1,25 +1,25 @@
-{stdenv, fetchurl}:
+{ stdenv, fetchurl, autoPatchelfHook }:
 
-stdenv.mkDerivation {
-  name = "kzipmix-20091108";
+stdenv.mkDerivation rec {
+  pname = "kzipmix";
+  version = "20200115";
 
   src = fetchurl {
-    url = "http://static.jonof.id.au/dl/kenutils/kzipmix-20091108-linux.tar.gz";
-    sha256 = "19gyn8pblffdz1bf3xkbpzx8a8wn3xb0v411pqzmz5g5l6pm5gph";
+    url = "https://www.jonof.id.au/files/kenutils/kzipmix-${version}-linux.tar.gz";
+    sha256 = "086z8jzl56dk0hwgaj3vwy6n924xrgvwayiwvzwl1z7s81xk5y3q";
   };
+
+  nativeBuildInputs = [ autoPatchelfHook ];
 
   installPhase = ''
     mkdir -p $out/bin
-    cp kzip zipmix $out/bin
-    
-    patchelf --set-interpreter ${stdenv.glibc.out}/lib/ld-linux.so.2 $out/bin/kzip
-    patchelf --set-interpreter ${stdenv.glibc.out}/lib/ld-linux.so.2 $out/bin/zipmix
+    cp amd64/{kzip,zipmix} $out/bin
   '';
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "A tool that aggressively optimizes the sizes of Zip archives";
-    license = stdenv.lib.licenses.unfree;
-    homepage = "http://advsys.net/ken/utils.htm";
-    maintainers = [ stdenv.lib.maintainers.sander ];
+    homepage = "https://www.jonof.id.au/kenutils.html";
+    license = licenses.unfree;
+    maintainers = [ maintainers.sander ];
   };
 }
