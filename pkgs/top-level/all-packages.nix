f2e84260db34d062f54dfa1a12f5ce1021549928
bb2020 <bb2020@users.noreply.github.com>
2020-11-26 00:56:43 +0300

    pkgs/qemu: add qemu_full package
    qemu_full enables samba and ceph support because otherwise enabling
    them triggers recompilation of qemu package that takes a long time.
    Similar options can be enabled later on. qemu_full is based on complete
    qemu package, so hostCpuOnly is not enabled.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18870,6 +18870,7 @@ in
   prototool = callPackage ../development/tools/prototool { };
 
   qemu_kvm = lowPrio (qemu.override { hostCpuOnly = true; });
+  qemu_full = lowPrio (qemu.override { smbdSupport = true; cephSupport = true; });
 
   # See `xenPackages` source for explanations.
   # Building with `xen` instead of `xen-slim` is possible, but makes no sense.
