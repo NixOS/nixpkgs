NixOS/nixpkgs

commit 7798a4e19cc843874e6329e0a980de7515945923

eduke32: 20190330 -> 20200907

pkgs/games/eduke32/default.nix
@@ -1,10 +1,11 @@
 { stdenv, fetchurl, makeWrapper, pkgconfig, nasm, makeDesktopItem
-, flac, gtk2, libvorbis, libvpx, libGLU, libGL
+, alsaLib, flac, gtk2, libvorbis, libvpx, libGLU, libGL
 , SDL2, SDL2_mixer }:
 
 let
-  version = "20190330";
-  rev = "7470";
+  version = "20200907";
+  rev = "9527";
+  revExtra = "93f62bbad";
 
   desktopItem = makeDesktopItem {
     name = "eduke32";
@@ -22,11 +23,12 @@ in stdenv.mkDerivation {
   inherit version;
 
   src = fetchurl {
-    url = "http://dukeworld.duke4.net/eduke32/synthesis/latest/eduke32_src_${version}-${rev}.tar.xz";
-    sha256 = "09a7l23i6sygicc82w1in9hjw0jvivlf7q0vw8kcx9j98lm23mkn";
+    url = "http://dukeworld.duke4.net/eduke32/synthesis/latest/eduke32_src_20200907-9257-93f62bbad.tar.xz"; # Good
+    # url = "http://dukeworld.duke4.net/eduke32/synthesis/latest/eduke32_src_${version}-${rev}-${revExtra}.tar.xz";
+    sha256 = "972630059be61ef9564a241b84ef2ee4f69fc85c19ee36ce46052ff2f1ce3bf9";
   };
 
-  buildInputs = [ flac gtk2 libvorbis libvpx libGL libGLU SDL2 SDL2_mixer ];
+  buildInputs = [ alsaLib flac gtk2 libvorbis libvpx libGL libGLU SDL2 SDL2_mixer ];
 
   nativeBuildInputs = [ makeWrapper pkgconfig ]
     ++ stdenv.lib.optional (stdenv.hostPlatform.system == "i686-linux") nasm;

commit cf1efe2c7e49177a9faaccbcd5ad0e6d8139d711

eduke32: fix version number, use string interpolation for src.url
This fixes mistakes I've introduced in 7798a4e19cc843874e6329e0a980de7515945923.

pkgs/games/eduke32/default.nix
@@ -4,7 +4,7 @@
 
 let
   version = "20200907";
-  rev = "9527";
+  rev = "9257";
   revExtra = "93f62bbad";
 
   desktopItem = makeDesktopItem {
@@ -23,8 +23,7 @@ in stdenv.mkDerivation {
   inherit version;
 
   src = fetchurl {
-    url = "http://dukeworld.duke4.net/eduke32/synthesis/latest/eduke32_src_20200907-9257-93f62bbad.tar.xz"; # Good
-    # url = "http://dukeworld.duke4.net/eduke32/synthesis/latest/eduke32_src_${version}-${rev}-${revExtra}.tar.xz";
+    url = "http://dukeworld.duke4.net/eduke32/synthesis/latest/eduke32_src_${version}-${rev}-${revExtra}.tar.xz";
     sha256 = "972630059be61ef9564a241b84ef2ee4f69fc85c19ee36ce46052ff2f1ce3bf9";
   };
 