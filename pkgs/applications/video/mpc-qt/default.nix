4fa046add772d218bab173ff992b89e587f7640e
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-11-25 13:41:12 +0100

    mpc-qt: Fix compilation failure due to mpv changes

--- pkgs/applications/video/mpc-qt/default.nix
+++ pkgs/applications/video/mpc-qt/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, mkDerivation, fetchFromGitLab, pkgconfig, qmake, qtx11extras, qttools, mpv }:
+{ stdenv, mkDerivation, fetchFromGitLab, fetchurl, pkg-config, qmake, qtx11extras, qttools, mpv }:
 
 mkDerivation rec {
   pname = "mpc-qt";
@@ -11,7 +11,14 @@ mkDerivation rec {
     sha256 = "1cis8dl9pm91mpnp696zvwsfp96gkwr8jgs45anbwd7ldw78w4x5";
   };
 
-  nativeBuildInputs = [ pkgconfig qmake qttools ];
+  patches = [
+    (fetchurl {
+      url = "https://gitlab.com/mpc-qt/mpc-qt/-/commit/02f2bc7a22e863a89ba322b9acb61cf1aef23ba0.diff";
+      sha256 = "15gcpcs54k09pndlpcfwb3fa0cg9fszyyb6vbp0hpgspy43183lx";
+    })
+  ];
+
+  nativeBuildInputs = [ pkg-config qmake qttools ];
 
   buildInputs = [ mpv qtx11extras ];
 
