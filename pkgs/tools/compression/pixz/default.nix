e42db80fd28e20d536e306033f1c92b3a6efde6f
Ivar <ivar.scholten@protonmail.com>
2020-11-25 12:51:19 +0100

    pixz: cleanup

--- pkgs/tools/compression/pixz/default.nix
+++ pkgs/tools/compression/pixz/default.nix
@@ -1,37 +1,35 @@
-{
-  stdenv, fetchFromGitHub, autoconf, automake, libtool, pkgconfig
-  , asciidoc, libxslt, libxml2, docbook_xml_dtd_45, docbook_xsl
-  , libarchive, lzma
+{ stdenv, fetchFromGitHub, autoconf, automake, libtool, pkgconfig
+, asciidoc, libxslt, libxml2, docbook_xml_dtd_45, docbook_xsl
+, libarchive, lzma
 }:
-stdenv.mkDerivation rec {
-  baseName = "pixz";
-  version = "1.0.7";
-  name = "${baseName}-${version}";
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [
-    autoconf automake libtool asciidoc libxslt libxml2
-    docbook_xml_dtd_45 docbook_xsl
-    libarchive lzma
-  ];
-  preBuild = ''
-    echo "XML_CATALOG_FILES='$XML_CATALOG_FILES'"
-  '';
+stdenv.mkDerivation rec {
+  pname = "pixz";
+  version = "1.0.7";
+
   src = fetchFromGitHub {
     owner = "vasi";
-    repo = baseName;
+    repo = pname;
     rev = "v${version}";
     sha256 = "163axxs22w7pghr786hda22mnlpvmi50hzhfr9axwyyjl9n41qs2";
   };
+
+  nativeBuildInputs = [ pkgconfig autoconf automake ];
+
+  buildInputs = [
+    libtool asciidoc libxslt libxml2
+    docbook_xml_dtd_45 docbook_xsl
+    libarchive lzma
+  ];
+
   preConfigure = ''
     ./autogen.sh
   '';
 
-  meta = {
-    inherit version;
-    description = ''A parallel compressor/decompressor for xz format'';
-    license = stdenv.lib.licenses.bsd2;
-    maintainers = [stdenv.lib.maintainers.raskin];
-    platforms = stdenv.lib.platforms.unix;
+  meta = with stdenv.lib; {
+    description = "A parallel compressor/decompressor for xz format";
+    license = licenses.bsd2;
+    maintainers = [ maintainers.raskin ];
+    platforms = platforms.unix;
   };
 }
