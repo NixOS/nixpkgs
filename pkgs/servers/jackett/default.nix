109bcd82b0d7ca81a2a36fa1aac1f6abe0cce694
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-26 08:10:11 +0000

    jackett: 0.16.2152 -> 0.16.2236

--- pkgs/servers/jackett/default.nix
+++ pkgs/servers/jackett/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "jackett";
-  version = "0.16.2152";
+  version = "0.16.2236";
 
   src = fetchurl {
     url = "https://github.com/Jackett/Jackett/releases/download/v${version}/Jackett.Binaries.Mono.tar.gz";
-    sha256 = "1q1bydyhddhz110xzbhhjj3gm0rwvjrjgc8j8wsj7xd9k0laygqd";
+    sha256 = "1qra5qcm7bwplkrmy6haf1kk3gpj9jjvgl0y7rs81jn15cjp6fmq";
   };
 
   nativeBuildInputs = [ makeWrapper ];
