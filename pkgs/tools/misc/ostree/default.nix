f90d34443fbbe200b94d05405ef4190b88daba7f
R. RyanTM <ryantm-bot@ryantm.com>
2020-11-26 15:04:20 +0000

    ostree: 2020.7 -> 2020.8

--- pkgs/tools/misc/ostree/default.nix
+++ pkgs/tools/misc/ostree/default.nix
@@ -39,13 +39,13 @@ let
   ]));
 in stdenv.mkDerivation rec {
   pname = "ostree";
-  version = "2020.7";
+  version = "2020.8";
 
   outputs = [ "out" "dev" "man" "installedTests" ];
 
   src = fetchurl {
     url = "https://github.com/ostreedev/ostree/releases/download/v${version}/libostree-${version}.tar.xz";
-    sha256 = "0clriq2ypz1fycd6mpjyrhzid44svzpzw0amnank593h69b216ax";
+    sha256 = "16v73v63h16ika73kgh2cvgm0v27r2d48m932mbj3xm6s295kapx";
   };
 
   patches = [
