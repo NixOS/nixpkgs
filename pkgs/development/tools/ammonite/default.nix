34d2819831d356eb4353658fc7e290d2b0c26eec
Tim Steinbach <tim@nequissimus.com>
2020-11-27 09:58:00 -0500

    ammonite: Fix arguments

--- pkgs/development/tools/ammonite/default.nix
+++ pkgs/development/tools/ammonite/default.nix
@@ -23,7 +23,7 @@ let
         install -Dm755 $src $out/bin/amm
         sed -i '0,/java/{s|java|${jre}/bin/java|}' $out/bin/amm
       '' + optionalString (disableRemoteLogging) ''
-        sed -i '0,/ammonite.Main/{s|ammonite.Main|ammonite.Main --no-remote-logging|}' $out/bin/amm
+        sed -i "0,/ammonite.Main/{s|ammonite.Main'|ammonite.Main' --no-remote-logging|}" $out/bin/amm
         sed -i '1i #!/bin/sh' $out/bin/amm
       '';
 
