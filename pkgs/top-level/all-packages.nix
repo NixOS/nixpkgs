c5c2855647a7019c390d42ebf8451498057594d0
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2020-11-29 03:35:16 +0800

    pslib: Init at 0.4.6
    pslib is a C-libreary to create PostScript files.
    If applied, other nix package (e.g. the in-progress GNU Data Language)
    with such dependency can be packaged easier.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15425,6 +15425,8 @@ in
   gnupth = callPackage ../development/libraries/pth { };
   pth = if stdenv.hostPlatform.isMusl then npth else gnupth;
 
+  pslib = callPackage ../development/libraries/pslib { };
+
   pstreams = callPackage ../development/libraries/pstreams {};
 
   pugixml = callPackage ../development/libraries/pugixml { };
