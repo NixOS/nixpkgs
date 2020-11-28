b73c99ba14beaef73c2f5526791180f3c8f39d13
Sandro <sandro.jaeckel@gmail.com>
2020-11-28 05:41:51 +0100

    Apply suggestions from code review

--- pkgs/misc/tmux-plugins/default.nix
+++ pkgs/misc/tmux-plugins/default.nix
@@ -206,14 +206,13 @@ in rec {
   onedark-theme = mkDerivation {
     pluginName = "onedark-theme";
     version = "unstable-2020-06-07";
-    src = pkgs.fetchFromGitHub {
+    src = fetchFromGitHub {
       owner = "odedlaz";
       repo = "tmux-onedark-theme";
       rev = "3607ef889a47dd3b4b31f66cda7f36da6f81b85c";
       sha256 = "19jljshwp2p83b634cd1mw69091x42jj0dg40ipw61qy6642h2m5";
     };
   };
-
 
   pain-control = mkDerivation {
     pluginName = "pain-control";
