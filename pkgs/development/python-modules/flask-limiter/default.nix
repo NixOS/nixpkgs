01098517eaed090acec27d68f441d63dcf7c548c
Frederik Rietdijk <fridh@fridh.nl>
2020-11-29 15:04:28 +0100

    python3Packages.Flask-Limiter: 1.3.1 -> 1.4

--- pkgs/development/python-modules/flask-limiter/default.nix
+++ pkgs/development/python-modules/flask-limiter/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "Flask-Limiter";
-  version = "1.3.1";
+  version = "1.4";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "08d6d7534a847c532fd36d0df978f93908d8616813085941c862bbcfcf6811aa";
+    sha256 = "021279c905a1e24f181377ab3be711be7541734b494f4e6db2b8edeba7601e48";
   };
 
   propagatedBuildInputs = [ flask limits ];
