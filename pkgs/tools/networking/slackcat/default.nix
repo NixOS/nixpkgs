661129cb45da37b1411cf42a02eafda2aab1a583
Narek Asadorian <narek.asadorian@gmail.com>
2020-11-28 16:50:57 -0800

    Update pkgs/tools/networking/slackcat/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/tools/networking/slackcat/default.nix
+++ pkgs/tools/networking/slackcat/default.nix
@@ -3,14 +3,16 @@
 buildGoPackage rec {
   pname = "slackcat";
   version = "1.6";
-  goPackagePath = "github.com/bcicen/slackcat";
+
   src = fetchFromGitHub {
     owner = "bcicen";
     repo = "${pname}";
     rev = "v${version}";
     sha256 = "1ha9b2dffpxqs159ldqdh1aibn3316xab19vdn2yjgk8r508wm31";
   };
-  goDeps = ./deps.nix;
+
+  goPackagePath = "github.com/bcicen/slackcat";
+
   meta = with stdenv.lib; {
     description = "CLI utility to post files and command output to slack";
     homepage = "http://slackcat.chat/";
