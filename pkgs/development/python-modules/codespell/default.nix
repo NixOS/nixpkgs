a2db06c3f7e171fe553ce9a14c93fa81f09ae894
Jonathan Ringer <jonringer117@gmail.com>
2020-11-29 09:27:55 -0800

    python3Packages.codespell: fix tests

--- pkgs/development/python-modules/codespell/default.nix
+++ pkgs/development/python-modules/codespell/default.nix
@@ -1,4 +1,5 @@
 { lib, buildPythonApplication, fetchPypi, pytest, chardet }:
+
 buildPythonApplication rec {
   pname = "codespell";
   version = "2.0.0";
@@ -8,6 +9,8 @@ buildPythonApplication rec {
     sha256 = "dd9983e096b9f7ba89dd2d2466d1fc37231d060f19066331b9571341363c77b8";
   };
 
+  # no tests in pypi tarball
+  doCheck = false;
   checkInputs = [ pytest chardet ];
   checkPhase = ''
     # We don't want to be affected by the presence of these
@@ -15,6 +18,8 @@ buildPythonApplication rec {
     # test_command assumes too much about the execution environment
     pytest --pyargs codespell_lib.tests -k "not test_command"
   '';
+
+  pythonImportsCheck = [ "codespell_lib" ];
 
   meta = {
     description = "Fix common misspellings in source code";
