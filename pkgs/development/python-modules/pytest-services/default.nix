8cdf17768d23e786384387c217937e686b1060ce
Frederik Rietdijk <fridh@fridh.nl>
2020-11-29 15:04:39 +0100

    python3Packages.pytest-services: 2.1.0 -> 2.2.1

--- pkgs/development/python-modules/pytest-services/default.nix
+++ pkgs/development/python-modules/pytest-services/default.nix
@@ -11,11 +11,11 @@
 
 buildPythonPackage rec {
   pname = "pytest-services";
-  version = "2.1.0";
+  version = "2.2.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0037101eaa17e050542808ecb2e799e9b2b148f1867f62b2296329fdd2034cf5";
+    sha256 = "2da740487d08ea63dfdf718f5d4ba11e590c99ddf5481549edebf7a3a42ca536";
   };
 
   propagatedBuildInputs = [
