d33db239d557f8df16ccac8473b529e90c4a8d5e
Sage Raflik <neonfuz@gmail.com>
2020-11-30 17:21:59 -0600

    WIP: rpcs3: added neonfuz as maintainer

--- pkgs/misc/emulators/rpcs3/default.nix
+++ pkgs/misc/emulators/rpcs3/default.nix
@@ -49,7 +49,7 @@ mkDerivation {
   meta = with lib; {
     description = "PS3 emulator/debugger";
     homepage = "https://rpcs3.net/";
-    maintainers = with maintainers; [ abbradar nocent ];
+    maintainers = with maintainers; [ abbradar neonfuz nocent ];
     license = licenses.gpl2;
     platforms = [ "x86_64-linux" ];
   };
