01c6a08a6f335d7d615556cfc9e720c5f00bc98e
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-11-30 17:00:57 +0100

    botan2: 2.9.0 -> 2.17.2
    In botan 2.11.0 the upstream switched to tar.xz archives. To continue
    supporting botan1 the source package extension can now be overriden from
    within the specialized package.
    
    Addresses two advisories, neither of which received a CVE:
    - 2020-07-05: Failure to enforce name constraints on alternative names
    - 2020-03-24: Side channel during CBC padding
    
    (cherry picked from commit 4630fcf686b7b178e101e89d0bd8e8af1684ab97)

--- pkgs/development/libraries/botan/2.0.nix
+++ pkgs/development/libraries/botan/2.0.nix
@@ -1,9 +1,9 @@
 { callPackage, ... } @ args:
 
 callPackage ./generic.nix (args // {
-  baseVersion = "2.9";
-  revision = "0";
-  sha256 = "06fiyalvc68p11qqh953azx2vrbav5vr00yvcfp67p9l4csn8m9h";
+  baseVersion = "2.17";
+  revision = "2";
+  sha256 = "0v0yiq0qxcrsn5b34j6bz8i6pds8dih2ds90ylmy1msm5gz7vqpb";
   postPatch = ''
     sed -e 's@lang_flags "@&--std=c++11 @' -i src/build-data/cc/{gcc,clang}.txt
   '';
