f36ef8f4618fa4b441b9f3bfac95862f2c69c994
Dominik Xaver Hörl <hoe.dom@gmx.de>
2020-12-01 10:13:04 +0100

    nixos/installation-device: replace wpa_supplicant by iwd

--- nixos/modules/profiles/installation-device.nix
+++ nixos/modules/profiles/installation-device.nix
@@ -74,10 +74,8 @@ with lib;
       permitRootLogin = "yes";
     };
 
-    # Enable wpa_supplicant, but don't start it by default.
-    networking.wireless.enable = mkDefault true;
-    networking.wireless.userControlled.enable = true;
-    systemd.services.wpa_supplicant.wantedBy = mkOverride 50 [];
+    # Enable iwd by default for wireless networking
+    networking.wireless.iwd.enable = mkDefault true;
 
     # Tell the Nix evaluator to garbage collect more aggressively.
     # This is desirable in memory-constrained environments that don't
