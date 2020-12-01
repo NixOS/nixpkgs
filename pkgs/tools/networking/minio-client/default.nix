fe35c3067839333cb80841aef24b898314b0a68d
R. RyanTM <ryantm-bot@ryantm.com>
2020-12-01 02:23:52 +0000

    minio-client: 2020-11-17T00-39-14Z -> 2020-11-25T23-04-07Z

--- pkgs/tools/networking/minio-client/default.nix
+++ pkgs/tools/networking/minio-client/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "minio-client";
-  version = "2020-11-17T00-39-14Z";
+  version = "2020-11-25T23-04-07Z";
 
   src = fetchFromGitHub {
     owner = "minio";
     repo = "mc";
     rev = "RELEASE.${version}";
-    sha256 = "122fb9ghxwjifhvwiw07pm3szqfj3pc55m4bhq3v8nshzwrbcjf4";
+    sha256 = "0r4za7jnwmh2wna5l4iv5pz6s5c62f7v4hmbv7misff9qbk4p4k7";
   };
 
-  vendorSha256 = "148fb86v059skcf8dgcrqibwkl1h4lbwi60qnwmdi03q6rvaw33m";
+  vendorSha256 = "0zracgqwhqs3gabrqqm8fm3hyicaadcinsjp5qgprjwd9a3c1zfv";
 
   doCheck = false;
 
