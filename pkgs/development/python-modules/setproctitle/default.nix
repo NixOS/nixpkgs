9cb62449f62b447a1cb4be093bcd002d6b99b76b
Jonathan Ringer <jonringer117@gmail.com>
2020-11-30 17:32:45 -0800

    python3Packages.setproctitle: ignore impure tests

--- pkgs/development/python-modules/setproctitle/default.nix
+++ pkgs/development/python-modules/setproctitle/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , buildPythonPackage
 , fetchPypi
+, pytestCheckHook
 }:
 
 buildPythonPackage rec {
@@ -11,6 +12,11 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "9b4e48722dd96cbd66d5bf2eab930fff8546cd551dd8d774c8a319448bd381a6";
   };
+
+  checkInputs = [ pytestCheckHook ];
+
+  # tries to compile programs with dependencies that aren't available
+  pytestFlagsArray = [ "--ignore=tests/setproctitle_test.py" ];
 
   meta = with stdenv.lib; {
     description = "Allows a process to change its title (as displayed by system tools such as ps and top)";
