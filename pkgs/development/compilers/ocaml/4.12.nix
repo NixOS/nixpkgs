644be6c4ea1962c5beb0700cf4ffb6c006b81b7c
Vincent Laporte <Vincent.Laporte@gmail.com>
2020-12-02 08:27:16 +0100

    ocaml-ng.ocamlPackages_4_12.ocaml: 4.12.0~α1 → 4.12.0~α2

--- pkgs/development/compilers/ocaml/4.12.nix
+++ pkgs/development/compilers/ocaml/4.12.nix
@@ -1,9 +1,9 @@
 import ./generic.nix {
   major_version = "4";
   minor_version = "12";
-  patch_version = "0-alpha1";
+  patch_version = "0-alpha2";
   src = fetchTarball {
-    url = "http://caml.inria.fr/pub/distrib/ocaml-4.12/ocaml-4.12.0~alpha1.tar.xz";
-    sha256 = "1p9nnj7l43b697b6bm767znbf1h0s2lyc1qb8izr1vfpsmnm11ws";
+    url = "http://caml.inria.fr/pub/distrib/ocaml-4.12/ocaml-4.12.0~alpha2.tar.xz";
+    sha256 = "148vgjcfajjvrvh0q9kb2y7fszqd02cikb5wyznz7kjxka6xxyn9";
   };
 }
