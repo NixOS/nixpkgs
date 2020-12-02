e2b7cfedd6cf29db095dd469e4453c9138c87d1f
Matt McHenry <github@matt.mchenryfamily.org>
2020-12-02 10:45:03 -0500

    globalprotect-openconnect: init at 1.2.6
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>
    
    Co-authored-by: sterni <sternenseemann@systemli.org>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9485,6 +9485,8 @@ in
     openssl = null;
   };
 
+  globalprotect-openconnect = libsForQt5.callPackage ../tools/networking/globalprotect-openconnect { };
+
   ding-libs = callPackage ../tools/misc/ding-libs { };
 
   sssd = callPackage ../os-specific/linux/sssd {
