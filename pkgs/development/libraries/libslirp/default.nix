ae1f466248adf24f3204779e94d346510e4fa73a
zowoq <59103226+zowoq@users.noreply.github.com>
2020-12-03 06:11:26 +1000

    libslirp: 4.3.1 -> 4.4.0
    https://gitlab.freedesktop.org/slirp/libslirp/-/releases/v4.4.0

--- pkgs/development/libraries/libslirp/default.nix
+++ pkgs/development/libraries/libslirp/default.nix
@@ -1,6 +1,5 @@
 { stdenv
 , fetchFromGitLab
-, fetchpatch
 , meson
 , ninja
 , pkg-config
@@ -9,24 +8,15 @@
 
 stdenv.mkDerivation rec {
   pname = "libslirp";
-  version = "4.3.1";
+  version = "4.4.0";
 
   src = fetchFromGitLab {
     domain = "gitlab.freedesktop.org";
     owner = "slirp";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0pzgjj2x2vrjshrzrl2x39xp5lgwg4b4y9vs8xvadh1ycl10v3fv";
+    sha256 = "0abh337jvij664w65zszjql42n720zzfap0ab0amr4qcvkzw7bjx";
   };
-
-  patches = [
-    # remove >4.3.1
-    (fetchpatch {
-      name = "CVE-2020-29129_CVE-2020-29130.patch";
-      url = "https://gitlab.freedesktop.org/slirp/libslirp/-/commit/2e1dcbc0c2af64fcb17009eaf2ceedd81be2b27f.patch";
-      sha256 = "01vbjqgnc0kp881l5p6b31cyyirhwhavm6x36hlgkymswvl3wh9w";
-    })
-  ];
 
   nativeBuildInputs = [ meson ninja pkg-config ];
 
@@ -41,6 +31,6 @@ stdenv.mkDerivation rec {
     homepage = "https://gitlab.freedesktop.org/slirp/libslirp";
     license = licenses.bsd3;
     maintainers = with maintainers; [ orivej ];
-    platforms = platforms.linux;
+    platforms = platforms.unix;
   };
 }
