582fa3d237d9433e9fb3be36030bc72e7135bb97
Stevan Andjelkovic <stevana@users.noreply.github.com>
2020-12-03 19:40:55 +0100

    graalvm-ce-20.2.0 -> 20.3.0 + darwin support.
    (Also bumps babashka and clj-kondo.)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10253,9 +10253,9 @@ in
     inherit (darwin) libiconv libobjc libresolv;
   }) mx jvmci8 graalvm8;
 
-  inherit (callPackages ../development/compilers/graalvm/community-edition.nix { })
-    graalvm8-ce
-    graalvm11-ce;
+  inherit (callPackages ../development/compilers/graalvm/community-edition.nix {
+    inherit (darwin.apple_sdk.frameworks) Foundation;
+  }) graalvm8-ce graalvm11-ce;
 
   inherit (callPackages ../development/compilers/graalvm/enterprise-edition.nix { })
     graalvm8-ee
