87d88376bb1a43cb90aa56b5dd3271aeeb760daa
R. RyanTM <ryantm-bot@ryantm.com>
2020-12-03 05:34:13 +0000

    privoxy: 3.0.28 -> 3.0.29
    (cherry picked from commit c6fafc68795dcbbce63fcc597a021c19a5f4f951)

--- pkgs/tools/networking/privoxy/default.nix
+++ pkgs/tools/networking/privoxy/default.nix
@@ -3,11 +3,11 @@
 stdenv.mkDerivation rec {
 
   pname = "privoxy";
-  version = "3.0.28";
+  version = "3.0.29";
 
   src = fetchurl {
     url = "mirror://sourceforge/ijbswa/Sources/${version}%20%28stable%29/${pname}-${version}-stable-src.tar.gz";
-    sha256 = "0jl2yav1qzqnaqnnx8i6i53ayckkimcrs3l6ryvv7bda6v08rmxm";
+    sha256 = "17a8fbdyb0ixc0wwq68fg7xn7l6n7jq67njpq93psmxgzng0dii5";
   };
 
   hardeningEnable = [ "pie" ];
