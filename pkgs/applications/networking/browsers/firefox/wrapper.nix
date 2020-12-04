36436ee8ac23d2ecff5faa55f5dedf3499c7664a
ryneeverett <ryneeverett@gmail.com>
2020-12-04 15:54:00 +0000

    firefox: Prefix $PATH with xdg_utils
    This is required for certain URIs that require launching external
    programs (e.g. mailto:, magnet:, or irc:) or setting the default browser
    via xdg-settings.
    
    Resolve #92751.
    
    Comparable to #96922.

--- pkgs/applications/networking/browsers/firefox/wrapper.nix
+++ pkgs/applications/networking/browsers/firefox/wrapper.nix
@@ -1,5 +1,5 @@
 { stdenv, lib, makeDesktopItem, makeWrapper, lndir, config
-, replace, fetchurl, zip, unzip, jq
+, replace, fetchurl, zip, unzip, jq, xdg_utils
 
 ## various stuff that can be plugged in
 , flashplayer, hal-flash
@@ -249,6 +249,7 @@ let
             --suffix LD_LIBRARY_PATH ':' "$libs" \
             --suffix-each GTK_PATH ':' "$gtk_modules" \
             --suffix-each LD_PRELOAD ':' "$(cat $(filterExisting $(addSuffix /extra-ld-preload $plugins)))" \
+            --prefix PATH ':' "${xdg_utils}/bin" \
             --prefix-contents PATH ':' "$(filterExisting $(addSuffix /extra-bin-path $plugins))" \
             --suffix PATH ':' "$out${browser.execdir or "/bin"}" \
             --set MOZ_APP_LAUNCHER "${browserName}${nameSuffix}" \
