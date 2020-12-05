464912ee011c7a8ac49d347a766976433b3ebeb3
Vincent Laporte <Vincent.Laporte@gmail.com>
2020-12-05 17:27:44 +0100

    ocamlPackages.duration: use Dune 2

--- pkgs/development/ocaml-modules/duration/default.nix
+++ pkgs/development/ocaml-modules/duration/default.nix
@@ -1,16 +1,18 @@
-{ lib, buildDunePackage, fetchurl, alcotest }:
+{ lib, buildDunePackage, ocaml, fetchurl, alcotest }:
 
 buildDunePackage rec {
   pname = "duration";
   version = "0.1.3";
+
+  useDune2 = true;
 
   src = fetchurl {
     url = "https://github.com/hannesm/duration/releases/download/${version}/duration-${version}.tbz";
     sha256 = "0m9r0ayhpl98g9vdxrbjdcllns274jilic5v8xj1x7dphw21p95h";
   };
 
-  doCheck = true;
-  checkInputs = lib.optional doCheck alcotest;
+  doCheck = lib.versionAtLeast ocaml.version "4.05";
+  checkInputs = [ alcotest ];
 
   meta = {
     homepage = "https://github.com/hannesm/duration";
