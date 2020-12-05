bc2b132f98b48220fa5ec148aa2ba170aeb9a891
Jonathan Ringer <jonringer117@gmail.com>
2020-12-04 21:38:17 -0800

    onnxruntime: remove
    Poor cmake and git hygiene has made maintaining this difficult, see:
    https://github.com/microsoft/onnxruntime/issues/5966
    https://github.com/microsoft/onnxruntime/issues/5967

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2478,8 +2478,6 @@ in
 
   oneshot = callPackage ../tools/networking/oneshot { };
 
-  onnxruntime = callPackage ../development/libraries/onnxruntime { };
-
   xkbd = callPackage ../applications/misc/xkbd { };
 
   libpsm2 = callPackage ../os-specific/linux/libpsm2 { };
