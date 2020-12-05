886e497f5d44df4198dd6470afe09685ad233985
Michael Weiss <dev.primeos@gmail.com>
2020-12-05 13:18:27 +0100

    mesa: Switch from LLVM 9 to llvmPackages_latest
    The radeonsi and radv (without ACO) drivers requires LLVM 11 for
    GFX 10.3 [0],[1]. E.g. Arch Linux also compiles Mesa with LLVM 11 so
    this should be fine.
    
    [0]: https://docs.mesa3d.org/relnotes/20.2.0.html
    [1]: https://github.com/NixOS/nixpkgs/pull/105817#issuecomment-738888102

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14962,7 +14962,7 @@ in
   libGLU = mesa_glu;
 
   mesa = callPackage ../development/libraries/mesa {
-    llvmPackages = llvmPackages_9;
+    llvmPackages = llvmPackages_latest;
     inherit (darwin.apple_sdk.frameworks) OpenGL;
     inherit (darwin.apple_sdk.libs) Xplugin;
   };
