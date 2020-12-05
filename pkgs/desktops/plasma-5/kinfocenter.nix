43c8aac0e3087f0a0ce0ff86ad2c29612938ae75
Thomas Tuegel <ttuegel@mailbox.org>
2020-12-05 13:34:39 -0600

    kinfocenter: broken with Qt < 5.15

--- pkgs/desktops/plasma-5/kinfocenter.nix
+++ pkgs/desktops/plasma-5/kinfocenter.nix
@@ -1,6 +1,7 @@
 {
-  mkDerivation,
+  mkDerivation, lib,
   extra-cmake-modules, kdoctools,
+  qtbase,
   kcmutils, kcompletion, kconfig, kconfigwidgets, kcoreaddons, kdbusaddons,
   kdeclarative, kdelibs4support, ki18n, kiconthemes, kio, kirigami2, kpackage,
   kservice, kwayland, kwidgetsaddons, kxmlgui, libraw1394, libGLU, pciutils,
@@ -9,6 +10,7 @@
 
 mkDerivation {
   name = "kinfocenter";
+  meta.broken = lib.versionOlder qtbase.version "5.15.0";
   nativeBuildInputs = [ extra-cmake-modules kdoctools ];
   buildInputs = [
     kcmutils kcompletion kconfig kconfigwidgets kcoreaddons kdbusaddons
