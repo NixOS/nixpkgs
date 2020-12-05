716218a2b7ac49634f00f5958c691052db7826b0
Thomas Tuegel <ttuegel@mailbox.org>
2020-12-05 14:55:12 -0600

    xdg-desktop-portal-kde: broken with Qt < 5.15

--- pkgs/desktops/plasma-5/xdg-desktop-portal-kde.nix
+++ pkgs/desktops/plasma-5/xdg-desktop-portal-kde.nix
@@ -1,13 +1,15 @@
 {
-  mkDerivation,
+  mkDerivation, lib,
   extra-cmake-modules, gettext, kdoctools, python,
   cups, epoxy, mesa, pcre, pipewire, wayland, wayland-protocols,
   kcoreaddons, knotifications, kwayland, kwidgetsaddons, kwindowsystem,
-  kirigami2, kdeclarative, plasma-framework, plasma-wayland-protocols, kio
+  kirigami2, kdeclarative, plasma-framework, plasma-wayland-protocols, kio,
+  qtbase
 }:
 
 mkDerivation {
   name = "xdg-desktop-portal-kde";
+  meta.broken = lib.versionOlder qtbase.version "5.15.0";
   nativeBuildInputs = [ extra-cmake-modules gettext kdoctools python ];
   buildInputs = [
     cups epoxy mesa pcre pipewire wayland wayland-protocols
