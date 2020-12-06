c797dbc3e1d8ae6a2749c845ffceaaa3c373062a
R. RyanTM <ryantm-bot@ryantm.com>
2020-12-06 22:38:26 +0000

    python37Packages.aniso8601: 8.0.0 -> 8.1.0

--- pkgs/development/python-modules/aniso8601/default.nix
+++ pkgs/development/python-modules/aniso8601/default.nix
@@ -3,7 +3,7 @@
 
 buildPythonPackage rec {
   pname = "aniso8601";
-  version = "8.0.0";
+  version = "8.1.0";
 
   meta = with stdenv.lib; {
     description = "Parses ISO 8601 strings.";
@@ -17,6 +17,6 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "529dcb1f5f26ee0df6c0a1ee84b7b27197c3c50fc3a6321d66c544689237d072";
+    sha256 = "246bf8d3611527030889e6df970878969d3a2f760ba3eb694fa1fb10e6ce53f9";
   };
 }
