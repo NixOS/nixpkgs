4778f510a46599d037364405d3e7daee56871e75
Andreas Rammhold <andreas@rammhold.de>
2020-12-07 00:19:55 +0100

    tldextract: add missing filelock dependency
    As of version 3.0.0 tldextract requires filelock during runtime. [0] In
    nixpkgs tldextract was bumped from version 2.2.3 to 3.1.0 in
    5b2d82451f034b2ce30eae12b3cf3f860381279b without actually updating the
    dependencies breaking packages such as qutebrowser.
    
    This fix is part of this series to verify that bumping qt5 to 5.15.2
    actually fixes the crash caused by WebRTC [1].
    
    [0] https://github.com/john-kurkowski/tldextract/commit/2eba0e5ebd02adf9e382523c7c84b86c7f2656f7
    [1] https://bugreports.qt.io/browse/QTBUG-86752

--- pkgs/development/python-modules/tldextract/default.nix
+++ pkgs/development/python-modules/tldextract/default.nix
@@ -1,5 +1,5 @@
 { lib, fetchPypi, buildPythonPackage, setuptools_scm
-, requests, requests-file, idna, pytest
+, requests, requests-file, idna, pytest, filelock
 , responses
 }:
 
@@ -12,7 +12,7 @@ buildPythonPackage rec {
     sha256 = "cfae9bc8bda37c3e8c7c8639711ad20e95dc85b207a256b60b0b23d7ff5540ea";
   };
 
-  propagatedBuildInputs = [ requests requests-file idna ];
+  propagatedBuildInputs = [ requests requests-file idna filelock ];
   checkInputs = [ pytest responses ];
   nativeBuildInputs = [ setuptools_scm ];
 
