f78a7f1820e6f1a996b41445a2de9de6b0c25f8f
nyanloutre <paul@nyanlout.re>
2020-12-06 14:05:17 +0100

    nixos/navidrome: use upstream systemd unit

--- nixos/modules/services/audio/navidrome.nix
+++ nixos/modules/services/audio/navidrome.nix
@@ -4,12 +4,13 @@ with lib;
 
 let
   cfg = config.services.navidrome;
+  settingsFormat = pkgs.formats.json {};
 in {
   options = {
     services.navidrome = {
       enable = mkEnableOption pkgs.navidrome.meta.description;
       settings = mkOption rec {
-        type = types.attrs;
+        type = settingsFormat.type;
         apply = recursiveUpdate default;
         default = {
           Address = "127.0.0.1";
@@ -19,43 +20,35 @@ in {
           MusicFolder = "/mnt/music";
         };
         description = ''
-          Configuration for Navidrome, see <link xlink:href="https://www.navidrome.org/docs/usage/configuration-options/"/> for supported values
+          Configuration for Navidrome, see <link xlink:href="https://www.navidrome.org/docs/usage/configuration-options/"/> for supported values.
         '';
       };
     };
   };
 
   config = mkIf cfg.enable {
+    systemd.packages = [ pkgs.navidrome ];
+
     systemd.services.navidrome = {
-      description = "Navidrome Media Server";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
       serviceConfig = {
-        ExecStart = ''
-          ${pkgs.navidrome}/bin/navidrome --configfile ${pkgs.writeText "navidrome.json" (builtins.toJSON cfg.settings)}
-        '';
+        ExecStart = [
+          ""
+          "${pkgs.navidrome}/bin/navidrome --configfile ${settingsFormat.generate "navidrome.json" cfg.settings}"
+        ];
         DynamicUser = true;
         StateDirectory = "navidrome";
-        WorkingDirectory = "/var/lib/navidrome";
         RuntimeDirectory = "navidrome";
         RootDirectory = "/run/navidrome";
+        ReadWritePaths = "";
         BindReadOnlyPaths = [
           builtins.storeDir
         ] ++ lib.optional (cfg.settings ? MusicFolder) cfg.settings.MusicFolder;
         CapabilityBoundingSet = "";
-        RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" ];
-        RestrictNamespaces = true;
         PrivateDevices = true;
-        PrivateUsers = true;
         ProtectClock = true;
-        ProtectControlGroups = true;
-        ProtectHome = true;
         ProtectKernelLogs = true;
-        ProtectKernelModules = true;
-        ProtectKernelTunables = true;
+        SystemCallFilter = [ "~@resources" "~@cpu-emulation" ];
         SystemCallArchitectures = "native";
-        SystemCallFilter = [ "@system-service" "~@privileged" "~@resources" ];
-        RestrictRealtime = true;
         LockPersonality = true;
         MemoryDenyWriteExecute = true;
         UMask = "0066";
--- pkgs/servers/misc/navidrome/default.nix
+++ pkgs/servers/misc/navidrome/default.nix
@@ -2,9 +2,15 @@
 
 with stdenv.lib;
 
-stdenv.mkDerivation rec {
-  pname = "navidrome";
+let
   version = "0.39.0";
+  upstream-systemd-unit = fetchurl {
+    url = "https://raw.githubusercontent.com/deluan/navidrome/v${version}/contrib/navidrome.service";
+    sha256 = "065lcmv7carbi0vy08qh3q5rsii8hp8xrrnfk5dgqyvykpf97cz5";
+  };
+in stdenv.mkDerivation rec {
+  pname = "navidrome";
+  inherit version;
 
   src = fetchurl {
     url = "https://github.com/deluan/navidrome/releases/download/v${version}/navidrome_${version}_Linux_x86_64.tar.gz";
@@ -20,6 +26,8 @@ stdenv.mkDerivation rec {
   installPhase = ''
      mkdir -p $out/bin
      cp navidrome $out/bin
+     mkdir -p $out/lib/systemd/system
+     cp ${upstream-systemd-unit} $out/lib/systemd/system/navidrome.service
   '';
 
   postFixup = ''
