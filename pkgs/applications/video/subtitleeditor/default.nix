fef35c439a9214c78a96519a18c4760bf90ff97d
ilmari-lauhakangas <ilmari-lauhakangas@users.noreply.github.com>
2020-12-06 17:43:20 +0200

    subtitleeditor: add more gst-plugins

--- pkgs/applications/video/subtitleeditor/default.nix
+++ pkgs/applications/video/subtitleeditor/default.nix
@@ -35,6 +35,9 @@ stdenv.mkDerivation {
     gst_all_1.gstreamermm
     gst_all_1.gst-plugins-base
     gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav
     hicolor-icon-theme
     libsigcxx
     libxmlxx
