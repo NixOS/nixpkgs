6396d0265b56b9b8574cef6e13216977326ef580
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-12-06 17:48:18 +0100

    ocamlPackages.opam-file-format: 2.0.0 -> 2.1.2

--- pkgs/development/ocaml-modules/opam-file-format/default.nix
+++ pkgs/development/ocaml-modules/opam-file-format/default.nix
@@ -1,21 +1,29 @@
-{ stdenv, lib, fetchFromGitHub, ocaml, findlib }:
+{ stdenv, lib, fetchpatch, fetchFromGitHub, ocaml, findlib }:
 
 stdenv.mkDerivation rec {
-  version = "2.0.0";
+  version = "2.1.2";
   name = "ocaml${ocaml.version}-opam-file-format-${version}";
 
   src = fetchFromGitHub {
     owner = "ocaml";
     repo = "opam-file-format";
     rev = version;
-    sha256 = "0fqb99asnair0043hhc8r158d6krv5nzvymd0xwycr5y72yrp0hv";
+    sha256 = "19xppn2s3yjid8jc1wh8gdf5mgmlpzby2cf2slmnbyrgln3vj6i2";
   };
 
   buildInputs = [ ocaml findlib ];
 
   installFlags = [ "LIBDIR=$(OCAMLFIND_DESTDIR)" ];
 
-  patches = [ ./optional-static.patch ];
+  patches = [
+    ./optional-static.patch
+    # fix no implementation error for OpamParserTypes
+    # can be removed at next release presumably
+    (fetchpatch {
+      url = "https://github.com/ocaml/opam-file-format/pull/41/commits/2a9a92ec334e0bf2adf8d2b4c1b83f1f9f68df8f.patch";
+      sha256 = "090nl7yciyyidmbjfryw3wyx7srh6flnrr4zgyhv4kvjsvq944y2";
+    })
+  ];
 
   meta = {
     description = "Parser and printer for the opam file syntax";
