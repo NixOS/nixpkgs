d98fa2ee8a1c46c4810d7ba31873913eec4064ac
R. RyanTM <ryantm-bot@ryantm.com>
2020-12-06 00:19:15 +0000

    insomnia: 2020.4.2 -> 2020.5.1

--- pkgs/development/web/insomnia/default.nix
+++ pkgs/development/web/insomnia/default.nix
@@ -16,12 +16,12 @@ let
   ];
 in stdenv.mkDerivation rec {
   pname = "insomnia";
-  version = "2020.4.2";
+  version = "2020.5.1";
 
   src = fetchurl {
     url =
       "https://github.com/Kong/insomnia/releases/download/core%40${version}/Insomnia.Core-${version}.deb";
-    sha256 = "0pxf6nphb6k2szgapqkhjavdzq1blg34v1zj47l76r0iqxrpm3va";
+    sha256 = "18wbyf5qq2x0dzaf6bprs4nhv0ckrgq679l3w61p1yf4y3j7xwn4";
   };
 
   nativeBuildInputs =
