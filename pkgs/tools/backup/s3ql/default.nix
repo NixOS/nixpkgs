e405f809ee6b051b5a8506465f2a5010101d63dc
nyanloutre <paul@nyanlout.re>
2020-12-07 22:14:18 +0100

    s3ql: 3.3.2 -> 3.6.0

--- pkgs/tools/backup/s3ql/default.nix
+++ pkgs/tools/backup/s3ql/default.nix
@@ -1,31 +1,48 @@
-{ stdenv, fetchFromGitHub, python3Packages, sqlite, which }:
+{ stdenv, fetchFromGitHub, fetchpatch, python3Packages, sqlite, which, psmisc }:
 
 python3Packages.buildPythonApplication rec {
   pname = "s3ql";
-  version = "3.3.2";
+  version = "3.6.0";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "release-${version}";
-    sha256 = "1x0xj8clfs8fdczn8skc2wag5i4z47bsvlczn22iaf20hll1bb2w";
+    sha256 = "04w8b15mwvyxnq146diblgs83mwsz9m0xmnsss5d1z2vz32i1q3z";
   };
 
-  checkInputs = [ which ] ++ (with python3Packages; [ cython pytest ]);
+  prePatch = ''
+    substituteInPlace src/s3ql/umount.py \
+      --replace "'fuser'" "'${psmisc}/bin/fuser'"
+  '';
+
+  patches = [
+    # Fixes tests with pytest 6, to be removed in next stable version
+    (fetchpatch {
+      url = "https://github.com/s3ql/s3ql/commit/5316c60b447dbf85a8b80de523fb1a570bf01c11.patch";
+      sha256 = "142j8ffhg6w7pf8cpxj0i485fq9vhvac32zjwl9ka4r3fqcq79zb";
+    })
+  ];
+
+  nativeBuildInputs = with python3Packages; [
+    cython
+  ];
+
   propagatedBuildInputs = with python3Packages; [
-    sqlite apsw pycrypto requests defusedxml dugong llfuse
-    cython pytest pytest-catchlog google_auth google-auth-oauthlib
+    sqlite apsw pycrypto requests defusedxml dugong pyfuse3
+    google_auth google-auth-oauthlib
   ];
 
   preBuild = ''
     ${python3Packages.python.interpreter} ./setup.py build_cython build_ext --inplace
   '';
 
-  checkPhase = ''
-    # Removing integration tests
-    rm tests/t{4,5,6}_*
-    pytest tests
-  '';
+  checkInputs = [ which ] ++ (with python3Packages; [
+    pytestCheckHook
+    pytest-trio
+  ]);
+
+  pytestFlagsArray = [ "tests/" "--ignore-glob='*/t[456]_*'" ];
 
   meta = with stdenv.lib; {
     description = "A full-featured file system for online data storage";
