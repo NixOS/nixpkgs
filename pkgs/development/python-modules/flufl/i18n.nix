d30fe3708be7718e8669345e0fd87fee64bf68a8
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-12-07 10:47:59 +0100

    python37Packages.flufl_i18n: Disable tests

--- pkgs/development/python-modules/flufl/i18n.nix
+++ pkgs/development/python-modules/flufl/i18n.nix
@@ -6,6 +6,10 @@ buildPythonPackage rec {
 
   propagatedBuildInputs = [ atpublic ];
 
+  doCheck = false;
+
+  pythonImportsCheck = [ "flufl.i18n" ];
+
   src = fetchPypi {
     inherit pname version;
     sha256 = "dcca738be27f2c43ddf6f9307667a17478353190071f38a9f92c9af8d2252ba4";
