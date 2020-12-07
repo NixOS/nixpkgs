7e9c3ff8bae88eb1e3ce3d6989a9c443bf969088
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-12-07 14:03:15 +0100

    pythonPackages.awkward{,0}: 0.14.0 -> 0.15.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -504,7 +504,7 @@ in {
 
   awesome-slugify = callPackage ../development/python-modules/awesome-slugify { };
 
-  awkward = callPackage ../development/python-modules/awkward { };
+  awkward0 = callPackage ../development/python-modules/awkward0 { };
   awkward1 = callPackage ../development/python-modules/awkward1 { };
 
   aws-adfs = callPackage ../development/python-modules/aws-adfs { };
