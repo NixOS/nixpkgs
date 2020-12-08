3c50d74f501389b040e3f7fc4571fb15acd850b4
R. RyanTM <ryantm-bot@ryantm.com>
2020-12-08 06:38:07 +0000

    svls: 0.1.17 -> 0.1.18

--- pkgs/development/tools/misc/svls/default.nix
+++ pkgs/development/tools/misc/svls/default.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "svls";
-  version = "0.1.17";
+  version = "0.1.18";
 
   src = fetchFromGitHub {
     owner = "dalance";
     repo = "svls";
     rev = "v${version}";
-    sha256 = "0qcd9pkshk94c6skzld8cyzppl05hk4vcmmaya8r9l6kdi1f4b5m";
+    sha256 = "08n2sp0yj7ql5aagxw94dn8bx2jkigs9zlylf5zkg673bh54829x";
   };
 
-  cargoSha256 = "0dqa7iw0sffzh07qysznh7ma3d3vl5fhd0i2qmz7a3dvw8mvyvsm";
+  cargoSha256 = "0k8x6kgxc2w7ldi456s4kg4v80b064drfa26zg5chcmcrr5zgmyv";
 
   meta = with lib; {
     description = "SystemVerilog language server";
