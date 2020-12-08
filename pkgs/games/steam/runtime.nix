f3103fe2d1f087f36ba6d5b1787f3284a5f5781c
Jonathan Ringer <jonringer117@gmail.com>
2020-12-08 23:19:14 +0000

    steamPackages.steam-runtime: 0.20200720.0 → 0.20201203.1

--- pkgs/games/steam/runtime.nix
+++ pkgs/games/steam/runtime.nix
@@ -8,11 +8,11 @@ stdenv.mkDerivation rec {
 
   pname = "steam-runtime";
   # from https://repo.steampowered.com/steamrt-images-scout/snapshots/
-  version = "0.20200720.0";
+  version = "0.20201203.1";
 
   src = fetchurl {
     url = "https://repo.steampowered.com/steamrt-images-scout/snapshots/${version}/steam-runtime.tar.xz";
-    sha256 = "03qdlr1xk84jb4c60ilis00vjhj70bxc0bbgk5g5b1883l2frljd";
+    sha256 = "sha256-hOHfMi0x3K82XM3m/JmGYbVk5RvuHG+m275eAC0MoQc=";
     name = "scout-runtime-${version}.tar.gz";
   };
 
