f162839a1eda1a79df211c56660faf051d9511b9
Jonathan Ringer <jonringer117@gmail.com>
2020-12-08 18:04:49 -0800

    steam: fix electron launchers

--- pkgs/games/steam/fhsenv.nix
+++ pkgs/games/steam/fhsenv.nix
@@ -35,6 +35,10 @@ let
       # Steam VR
       procps
       usbutils
+
+      # electron based launchers need newer versions of these libraries than what runtime provides
+      mesa
+      sqlite
     ] ++ lib.optional withJava jdk8 # TODO: upgrade https://github.com/NixOS/nixpkgs/pull/89731
       ++ lib.optional withPrimus primus
       ++ extraPkgs pkgs;
@@ -175,7 +179,6 @@ in buildFHSUserEnv rec {
     libidn
     tbb
     wayland
-    mesa
     libxkbcommon
 
     # Other things from runtime
