d95f865152771585b24eb9395f12c7687c72f7de
IvarWithoutBones <ivar.scholten@protonmail.com>
2020-12-09 06:29:13 +0100

    pythonPackages.tldextract: add missing dependency
    (cherry picked from commit b7cf39083799736b505ccc1a9a4fa0bddde1eb84)

--- pkgs/development/python-modules/tldextract/default.nix
+++ pkgs/development/python-modules/tldextract/default.nix
@@ -1,5 +1,5 @@
 { lib, fetchPypi, buildPythonPackage, setuptools_scm
-, requests, requests-file, idna, pytest
+, requests, requests-file, idna, filelock, pytest
 , responses
 }:
 
@@ -12,14 +12,17 @@ buildPythonPackage rec {
     sha256 = "ab0e38977a129c72729476d5f8c85a8e1f8e49e9202e1db8dca76e95da7be9a8";
   };
 
-  propagatedBuildInputs = [ requests requests-file idna ];
+  propagatedBuildInputs = [ requests requests-file idna filelock ];
   checkInputs = [ pytest responses ];
   nativeBuildInputs = [ setuptools_scm ];
 
+  # No tests included
+  doCheck = false;
+  pythonImportsCheck = [ "tldextract" ];
+
   meta = {
     homepage = "https://github.com/john-kurkowski/tldextract";
-    description = "Accurately separate the TLD from the registered domain and subdomains of a URL, using the Public Suffix List.";
+    description = "Accurately separate the TLD from the registered domain and subdomains of a URL, using the Public Suffix List";
     license = lib.licenses.bsd3;
   };
-
 }
