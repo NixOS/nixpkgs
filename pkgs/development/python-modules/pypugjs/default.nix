2b8515d7706769d308326a74a734bb450f5f2310
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-12-08 22:16:33 -0500

    pythonPackages.pypugjs: 5.9.6 -> 5.9.7

--- pkgs/development/python-modules/pypugjs/default.nix
+++ pkgs/development/python-modules/pypugjs/default.nix
@@ -3,25 +3,16 @@
 
 buildPythonPackage rec {
   pname = "pypugjs";
-  version = "5.9.6";
+  version = "5.9.7";
 
-  # No source tarball on PyPI
+  # PyPI source tarball is missing files
   # https://github.com/kakulukia/pypugjs/issues/68
   src = fetchFromGitHub {
     owner = "kakulukia";
     repo = pname;
     rev = "v${version}";
-    sha256 = "14hsl4jfzy5hf5qiwpjdy6vzzj9mvd2rvkcvjgciyz06vfccqd7y";
+    sha256 = "1wvl0n5c3xpani5yn1740f9yd72pjcqg7zy7sj4553m0mlwj9pvc";
   };
-
-  patches = [
-    # Allow newer pyramid versions
-    # https://github.com/kakulukia/pypugjs/pull/69
-    (fetchpatch {
-      url = "https://github.com/kakulukia/pypugjs/commit/2a77a59bee8526f7e3eb9d71c8241470dfff11f5.patch";
-      sha256 = "0d2bjh0f8hmbvmzi8ilj1xbpgszhhkapiql8yi0by2lrd525qspn";
-    })
-  ];
 
   propagatedBuildInputs = [ six chardet ];
   checkInputs = [ nose django jinja2 tornado pyramid pyramid_mako Mako ];
