5ba727781fca02d122e656df0fe948e51371ee62
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-12-09 15:25:33 +0100

    curl: 7.73.0 -> 7.74.0
    https://curl.se/docs/CVE-2020-8284.html
    https://curl.se/docs/CVE-2020-8285.html
    https://curl.se/docs/CVE-2020-8286.html
    
    Fixes: CVE-2020-8284, CVE-2020-8285, CVE-2020-8286

--- pkgs/tools/networking/curl/default.nix
+++ pkgs/tools/networking/curl/default.nix
@@ -34,14 +34,14 @@ assert gssSupport -> libkrb5 != null;
 
 stdenv.mkDerivation rec {
   pname = "curl";
-  version = "7.73.0";
+  version = "7.74.0";
 
   src = fetchurl {
     urls = [
       "https://curl.haxx.se/download/${pname}-${version}.tar.bz2"
       "https://github.com/curl/curl/releases/download/${lib.replaceStrings ["."] ["_"] pname}-${version}/${pname}-${version}.tar.bz2"
     ];
-    sha256 = "sha256-zzT+Cwe4APHAGkmabosq9Uj20OBE3KSinYikvuFG0TE=";
+    sha256 = "19bp3d91xq9vqwlbzq261j23mk9lz4lyka4gr2fm6dhnd3k66k8g";
   };
 
   outputs = [ "bin" "dev" "out" "man" "devdoc" ];
