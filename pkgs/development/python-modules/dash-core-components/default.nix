c325b13724c19e99cb48b7520e41462cb845d989
R. RyanTM <ryantm-bot@ryantm.com>
2020-12-10 05:55:12 +0000

    python37Packages.dash-core-components: 1.13.0 -> 1.14.1

--- pkgs/development/python-modules/dash-core-components/default.nix
+++ pkgs/development/python-modules/dash-core-components/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "dash_core_components";
-  version = "1.13.0";
+  version = "1.14.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f92025b12931539cdda2173f2b4cd077119cbabbf3ddf62333f6302fd0d8a3ac";
+    sha256 = "55423dfe0ede92b2efdb6dee6b7d44be141ca1e2e06f1a0effd4e7c83c929d4d";
   };
 
   # No tests in archive
