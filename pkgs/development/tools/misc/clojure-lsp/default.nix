e0d253916af35fe33b46e500e53ae98e2cf05670
R. RyanTM <ryantm-bot@ryantm.com>
2020-12-10 13:47:40 +0000

    clojure-lsp: 20201205T230541 -> 20201207T142850

--- pkgs/development/tools/misc/clojure-lsp/default.nix
+++ pkgs/development/tools/misc/clojure-lsp/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "clojure-lsp";
-  version = "20201205T230541";
+  version = "20201207T142850";
 
   src = fetchurl {
     url = "https://github.com/snoe/clojure-lsp/releases/download/release-${version}/${pname}";
-    sha256 = "06sismbv7wrcwfgr0brw9gsdvl2plrryhxs1v08a2sd5rdf2qi3b";
+    sha256 = "0fxplldpxslm7f5xxazkl09gsj0ysppaal72hmlqbdj6rbgxlrnk";
   };
 
   dontUnpack = true;
