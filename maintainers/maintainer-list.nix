6aec75747c6703287350fe80229759661f3df593
Le Anh Duc <anhdle14@icloud.com>
2020-12-12 02:59:04 +0900

    Tfsec github location changed, also pumping the version to 0.36.10
    Signed-off-by: Le Anh Duc <anhdle14@icloud.com>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10152,4 +10152,14 @@
     github = "zupo";
     githubId = 311580;
   };
+  anhdle14 = {
+    name = "Le Anh Duc";
+    email = "anhdle14@icloud.com";
+    github = "anhdle14";
+    githubId = 9645992;
+    keys = [{
+      longkeyid = "rsa4096/0x0299AFF9ECBB5169";
+      fingerprint = "AA4B 8EC3 F971 D350 482E  4E20 0299 AFF9 ECBB 5169";
+    }];
+  };
 }
