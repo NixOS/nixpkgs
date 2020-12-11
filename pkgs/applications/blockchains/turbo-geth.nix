1e67bbac0a9b301db4c2356b35c30cbc14c17ad7
R. RyanTM <ryantm-bot@ryantm.com>
2020-12-11 04:21:37 +0000

    turbo-geth: 2020.12.01 -> 2020.12.02

--- pkgs/applications/blockchains/turbo-geth.nix
+++ pkgs/applications/blockchains/turbo-geth.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "turbo-geth";
-  version = "2020.12.01";
+  version = "2020.12.02";
 
   src = fetchFromGitHub {
     owner = "ledgerwatch";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0l1qj10vhfkrk66zihz8m24pnyj8jbb65a7amvphp07r199swy2a";
+    sha256 = "0ynnpvpd84qncvzmk4hmq8mn6m14a9p3zg4svijqwlsrr39amp3q";
   };
 
-  vendorSha256 = "16vawkky612zf45d8dhipjmhrprmi28z9wdcnjy07x3bxdyfbhfr";
+  vendorSha256 = "0sk064iyaxq9ig9xv3h1p1c4994hna9bky16g3hblbnh3v7mmqar";
   runVend = true;
 
   subPackages = [
