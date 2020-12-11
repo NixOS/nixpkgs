7f4bc12f1369030a9512ef324ec42d5ed4ad2097
Arnout Engelen <arnout@bzzt.net>
2020-12-11 10:43:00 +0100

    talloc: remove python2 as a build dependency
    This makes sure waf, which is used to build talloc, is ran
    with python3 rather than python2.
    
    (another option would be to make this the default for
    wafHook, see https://github.com/NixOS/nixpkgs/pull/104701)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16165,7 +16165,9 @@ in
 
   taglib-sharp = callPackage ../development/libraries/taglib-sharp { };
 
-  talloc = callPackage ../development/libraries/talloc { };
+  talloc = callPackage ../development/libraries/talloc {
+    wafHook = wafHook.override { python = python3; };
+  };
 
   tclap = callPackage ../development/libraries/tclap {};
 
