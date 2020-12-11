b52951e118271b5402a898e63e51faa85623d6a0
Mario Rodas <marsam@users.noreply.github.com>
2020-12-11 04:20:00 +0000

    mp4v2: 4.1.3 -> 4.1.5

--- pkgs/development/libraries/mp4v2/default.nix
+++ pkgs/development/libraries/mp4v2/default.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "mp4v2";
-  version = "4.1.3";
+  version = "4.1.5";
 
   src = fetchFromGitHub {
     # 2020-06-20: THE current upstream, maintained and used in distros fork.
     owner = "TechSmith";
     repo = "mp4v2";
     rev = "Release-ThirdParty-MP4v2-${version}";
-    sha256 = "053a0lgy819sbz92cfkq0vmkn2ky39bva554pj4ypky1j6vs04fv";
+    sha256 = "0i8fy7gdqcwwla6fw50rr159jjlx01b1g13x3325a128wh33ghbg";
   };
 
   patches = [
@@ -19,10 +19,16 @@ stdenv.mkDerivation rec {
       url = "https://git.archlinux.org/svntogit/packages.git/plain/trunk/libmp4v2-c++11.patch?id=203f5a72bc97ffe089b424c47b07dd9eaea35713";
       sha256 = "0sbn0il7lmk77yrjyb4f0a3z3h8gsmdkscvz5n9hmrrrhrwf672w";
     })
-  ];
+  ] ++ stdenv.lib.optionals stdenv.cc.isClang [ ./fix-build-clang.patch ];
+
+  NIX_CFLAGS_COMPILE = stdenv.lib.optionalString stdenv.cc.isClang "-Wno-error=c++11-narrowing";
 
   # `faac' expects `mp4.h'.
-  postInstall = "ln -s mp4v2/mp4v2.h $out/include/mp4.h";
+  postInstall = ''
+    ln -s $dev/include/mp4v2/mp4v2.h $dev/include/mp4v2/mp4.h
+  '';
+
+  outputs = [ "out" "dev" "lib" ];
 
   enableParallelBuilding = true;
 
