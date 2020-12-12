96ad9af555d545dc85bf9305f43905081353ca60
Matt Votava <mvnetbiz@gmail.com>
2020-12-12 10:52:12 -0800

    python3Packages.aiosqlite: 0.12.0 -> 0.16.0

--- pkgs/development/python-modules/aiosqlite/default.nix
+++ pkgs/development/python-modules/aiosqlite/default.nix
@@ -1,31 +1,37 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
-, setuptools
+, typing-extensions
 , aiounittest
-, isPy27
-, pytest
+, coverage
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "aiosqlite";
-  version = "0.12.0";
-  disabled = isPy27;
+  version = "0.16.0";
+  format = "flit";
+  disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
-    owner = "jreese";
+    owner = "omnilib";
     repo = pname;
     rev = "v${version}";
-    sha256 = "090vdv210zfry0bms5b3lmm06yhiyjb8ga96996cqs611l7c2a2j";
+    sha256 = "qdWD3yyN9E+SHecZrpD/MDGWBpsx95/14CY1ElBTbX4=";
   };
 
-  buildInputs = [
-    setuptools
+  propagatedBuildInputs = [
+    typing-extensions
   ];
 
   checkInputs = [
     aiounittest
+    coverage
   ];
+
+  checkPhase = ''
+    python -m coverage run -m aiosqlite.tests
+  '';
 
   meta = with lib; {
     description = "Asyncio bridge to the standard sqlite3 module";
