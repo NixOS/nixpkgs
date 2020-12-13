5142dd84f8685541a0ad3531f931096b3aad2e25
Florian Klink <flokli@flokli.de>
2020-12-13 22:36:40 +0100

    mlarchive2maildir: 0.0.8 -> 0.0.9

--- pkgs/applications/networking/mailreaders/mlarchive2maildir/default.nix
+++ pkgs/applications/networking/mailreaders/mlarchive2maildir/default.nix
@@ -2,11 +2,11 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "mlarchive2maildir";
-  version = "0.0.8";
+  version = "0.0.9";
 
   src = python3.pkgs.fetchPypi {
     inherit pname version;
-    sha256 = "1din3yay2sas85178v0xr0hbm2396y4dalkcqql1ny9vdm94h6sp";
+    sha256 = "02zjwa7zbcbqj76l0qmg7bbf3fqli60pl2apby3j4zwzcrrryczs";
   };
 
   nativeBuildInputs = with python3.pkgs; [ setuptools_scm ];
