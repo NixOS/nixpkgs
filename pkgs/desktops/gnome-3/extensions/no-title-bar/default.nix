b0cc66673c05a4ca3cf1b49c554e4baad130fba3
Maxine Aubrey <maxeaubrey@gmail.com>
2020-12-13 15:11:32 +0100

    gnomeExtensions.no-title-bar: 2020-05-14 -> 11

--- pkgs/desktops/gnome-3/extensions/no-title-bar/default.nix
+++ pkgs/desktops/gnome-3/extensions/no-title-bar/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-shell-extension-no-title-bar";
-  version = "2020-05-14";
+  version = "11";
 
   src = fetchFromGitHub {
     owner = "poehlerj";
     repo = "no-title-bar";
-    rev = "4115905e1d3df51072a2c6b173a471667181b31a";
-    sha256 = "0hcbbfapk76lr8yajacx59cyzs2c1dnccf8fq3gv3zk1z8jfqb1h";
+    rev = "V_${version}";
+    sha256 = "07ddw47binlsbyvgy4xkdjvd40zyp7nwd17r6k7w54d50vmnwhvb";
   };
 
   nativeBuildInputs = [ glib gettext ];
