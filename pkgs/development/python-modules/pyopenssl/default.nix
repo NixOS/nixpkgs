NixOS/nixpkgs

commit 13456d6ceb6552cd5e42b7a207a5902d96a29c3d

pyopenssl: 19.1.0 -> 20.0.0
This is needed to build 'master' of mitmproxy,
verified that this works with this change.

pkgs/development/python-modules/pyopenssl/default.nix
@@ -65,11 +65,11 @@ in
 
 buildPythonPackage rec {
   pname = "pyOpenSSL";
-  version = "19.1.0";
+  version = "20.0.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "9a24494b2602aaf402be5c9e30a0b82d4a5c67528fe8fb475e3f3bc00dd69507";
+    sha256 = "1i8ab5zn9i9iq2ksizp3rd42v157kacddzz88kviqw3kpp68xw4j";
   };
 
   outputs = [ "out" "dev" ];
@@ -80,27 +80,6 @@ buildPythonPackage rec {
     py.test tests ${testExpression}
     runHook postCheck
   '';
-
-  patches = [
-    # 4 patches for 2020 bug
-    # https://github.com/pyca/pyopenssl/pull/828
-    (fetchpatch {
-      url = "https://github.com/pyca/pyopenssl/commit/0d2fd1a24b30077ead6960bd63b4a9893a57c101.patch";
-      sha256 = "1c27g53qrwxddyx04sxf8yvj7xgbaabla7mc1cgbfd426rncbqf3";
-    })
-    (fetchpatch {
-      url = "https://github.com/pyca/pyopenssl/commit/d08a742573c3205348a4eec9a65abaf6c16110c4.patch";
-      sha256 = "18xn8s1wpycz575ivrbsbs0qd2q48z8pdzsjzh8i60xba3f8yj2f";
-    })
-    (fetchpatch {
-      url = "https://github.com/pyca/pyopenssl/commit/60b9e10e6da7ccafaf722def630285f54510ed12.patch";
-      sha256 = "0aw8qvy8m0bhgp39lmbcrpprpg4bhpssm327hyrk476wwgajk01j";
-    })
-    (fetchpatch {
-      url = "https://github.com/pyca/pyopenssl/commit/7a37cc23fcbe43abe785cd4badd14bdc7acfb175.patch";
-      sha256 = "1c7zb568rs71rsl16p6dq7aixwlkgzfnba4vzmfvbmy3zsnaslq2";
-    })
-  ];
 
   # Seems to fail unpredictably on Darwin. See https://hydra.nixos.org/build/49877419/nixlog/1
   # for one example, but I've also seen ContextTests.test_set_verify_callback_exception fail.

commit 242e3f30c1496ee93d74c5b687cf6767c252cdbb

pyopenssl: skip test that fails on i686
Found in https://github.com/NixOS/nixpkgs/pull/105454#issuecomment-743973848

Upstream issue https://github.com/pyca/pyopenssl/issues/974

pkgs/development/python-modules/pyopenssl/default.nix
@@ -54,6 +54,9 @@ let
     optionals (hasPrefix "libressl" openssl.meta.name) failingLibresslTests
   ) ++ (
     optionals (versionAtLeast (getVersion openssl.name) "1.1") failingOpenSSL_1_1Tests
+  ) ++ (
+    # https://github.com/pyca/pyopenssl/issues/974
+    optionals stdenv.isi686 [ "test_verify_with_time" ]
   );
 
   # Compose the final string expression, including the "-k" and the single quotes.