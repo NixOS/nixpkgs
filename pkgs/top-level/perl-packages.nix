43e2e22260212c6bd8568b93609619e9e04f6939
Stig Palmquist <stig@stig.io>
2020-12-13 13:26:43 +0100

    [nix-update-cpan] perlPackages.CPANPerlReleases: 5.20200820 -> 5.20201120

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -3851,10 +3851,10 @@ let
 
   CPANPerlReleases = buildPerlPackage {
     pname = "CPAN-Perl-Releases";
-    version = "5.20200820";
+    version = "5.20201120";
     src = fetchurl {
-      url = "mirror://cpan/authors/id/B/BI/BINGOS/CPAN-Perl-Releases-5.20200820.tar.gz";
-      sha256 = "07vsbsxygdbv0nk9389ng3jdsl1d3yk1z54xdci1gpy0lghbij70";
+      url = "mirror://cpan/authors/id/B/BI/BINGOS/CPAN-Perl-Releases-5.20201120.tar.gz";
+      sha256 = "1qsjm26556kqxyvj85c7j37mh2xw78fm3i7f58mpqkfawxip4q3k";
     };
     meta = {
       homepage = "https://github.com/bingos/cpan-perl-releases";
