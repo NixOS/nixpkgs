4ef64822612839af18c863192e5f4ddc638fd96b
Poscat <poscat@mail.poscat.moe>
2020-12-14 08:01:19 +0800

    fcitx5-table-other: Init at 5.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3948,6 +3948,8 @@ in
 
   fcitx5-table-extra = callPackage ../tools/inputmethods/fcitx5/fcitx5-table-extra.nix { };
 
+  fcitx5-table-other = callPackage ../tools/inputmethods/fcitx5/fcitx5-table-other.nix { };
+
   fcppt = callPackage ../development/libraries/fcppt { };
 
   fcrackzip = callPackage ../tools/security/fcrackzip { };
