34fcfc880bdc3c23c936e7b4d19bd0a93558d8f1
Erik Arvstedt <erik.arvstedt@gmail.com>
2020-12-15 20:24:57 +0100

    tests/containers-custom-pkgs: improve test
    - Fix name
    - Remove unneeded leftovers that were copied from containers-hosts.nix
    - Remove redundant 'start_all()'

--- nixos/tests/containers-custom-pkgs.nix
+++ nixos/tests/containers-custom-pkgs.nix
@@ -1,5 +1,3 @@
-# Test for NixOS' container support.
-
 import ./make-test-python.nix ({ pkgs, lib, ...} : let
 
   customPkgs = pkgs // {
@@ -9,34 +7,27 @@ import ./make-test-python.nix ({ pkgs, lib, ...} : let
   };
 
 in {
-  name = "containers-hosts";
+  name = "containers-custom-pkgs";
   meta = with lib.maintainers; {
     maintainers = [ adisbladis ];
   };
 
-  machine =
-    { ... }:
-    {
-      virtualisation.memorySize = 256;
-      virtualisation.vlans = [];
-
-      containers.simple = {
-        autoStart = true;
-        pkgs = customPkgs;
-        config = {pkgs, config, ... }: {
-          environment.systemPackages = [
-            pkgs.hello
-          ];
-        };
+  machine = { ... }: {
+    containers.test = {
+      autoStart = true;
+      pkgs = customPkgs;
+      config = {pkgs, config, ... }: {
+        environment.systemPackages = [
+          pkgs.hello
+        ];
       };
-
     };
+  };
 
   testScript = ''
-    start_all()
     machine.wait_for_unit("default.target")
     machine.succeed(
-        "test $(nixos-container run simple -- readlink -f /run/current-system/sw/bin/hello) = ${customPkgs.hello}/bin/hello"
+        "test $(nixos-container run test -- readlink -f /run/current-system/sw/bin/hello) = ${customPkgs.hello}/bin/hello"
     )
   '';
 })
