95fefe1226b86c574bdc7273c21fed723f56840c
Cole Mickens <cole.mickens@gmail.com>
2020-12-15 14:21:50 -0800

    vscode: 1.51.1->1.52.0; fix for darwin-x64

--- pkgs/applications/editors/vscode/vscode.nix
+++ pkgs/applications/editors/vscode/vscode.nix
@@ -5,16 +5,16 @@ let
 
   plat = {
     x86_64-linux = "linux-x64";
-    x86_64-darwin = "darwin";
+    x86_64-darwin = "darwin-x64";
     aarch64-linux = "linux-arm64";
   }.${system};
 
   archive_fmt = if system == "x86_64-darwin" then "zip" else "tar.gz";
 
   sha256 = {
-    x86_64-linux = "0yv6584y4idkl9vvmpxj5ix5brshm1vadiwf7ima84snm0fipb0n";
-    x86_64-darwin = "0igndxkwkxyjc9rkf9hbj8903hvfv7ab41q0s3gw8w5qh4b8s48x";
-    aarch64-linux = "17g7xnzjazw0bq22pf96faqqg3m0gk1iwsg5n4ybv6wskzhpqfbd";
+    x86_64-linux = "1r44gqg70b5w9g1ywwjsd5iy2hdp47ijx7gac4vbdkj38inmxpmp";
+    x86_64-darwin = "0gdzv1z7z6ilbfrrym7lmwkjc7gyrfhrj7x1g1ark4wqrh55p6hf";
+    aarch64-linux = "1r44gqg70b5w9g1ywwjsd5iy2hdp47ijx7gac4vbdkj38inmxpmp";
   }.${system};
 in
   callPackage ./generic.nix rec {
@@ -23,7 +23,7 @@ in
 
     # Please backport all compatible updates to the stable release.
     # This is important for the extension ecosystem.
-    version = "1.51.1";
+    version = "1.52.0";
     pname = "vscode";
 
     executableName = "code" + lib.optionalString isInsiders "-insiders";
