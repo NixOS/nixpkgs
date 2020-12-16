28f68fd831ed06c13c85f5519f05bd164b5cc696
Travis A. Everett <travis.a.everett@gmail.com>
2020-12-16 00:08:43 -0600

    bashup-events: init at it's-complicated

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12594,6 +12594,8 @@ in
 
   bamf = callPackage ../development/libraries/bamf { };
 
+  inherit (callPackages ../development/libraries/bashup-events { }) bashup-events32 bashup-events44;
+
   bcg729 = callPackage ../development/libraries/bcg729 { };
 
   bctoolbox = callPackage ../development/libraries/bctoolbox { };
