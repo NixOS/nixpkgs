731c516cce5679181d2727901cdf38fe5eaf9027
Marek Mahut <marek.mahut@fivebinaries.com>
2020-12-18 12:32:21 +0100

    terraform-providers.checkly: init at 0.7.1

---
+++ pkgs/applications/networking/cluster/terraform-providers/checkly/default.nix
@@ -0,0 +1,24 @@
+{ stdenv, buildGoModule, fetchFromGitHub }:
+
+buildGoModule rec {
+  pname = "terraform-provider-checkly";
+  version = "0.7.1";
+
+  src = fetchFromGitHub {
+    owner = "checkly";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "1wv7zdzzg8cjcz5h8vvnaznnbp8r7kbdc9hj10s8dmnmqkf9rf83";
+  };
+
+  vendorSha256 = "1k4rpin0ijs31hlfcrgyz97yll89ff6lbnkkscyfiw61xcsz6mhm";
+
+  postInstall = "mv $out/bin/terraform-provider-checkly{,_v${version}}";
+
+  meta = with stdenv.lib; {
+    homepage = "https://github.com/checkly/terraform-provider-checkly";
+    description = "Terraform provider for Checkly";
+    license = licenses.mit;
+    maintainers = with maintainers; [ mmahut ];
+  };
+}
--- pkgs/applications/networking/cluster/terraform-providers/default.nix
+++ pkgs/applications/networking/cluster/terraform-providers/default.nix
@@ -55,6 +55,7 @@ let
 
     # Packages that don't fit the default model
     ansible = callPackage ./ansible {};
+    checkly = callPackage ./checkly {};
     cloudfoundry = callPackage ./cloudfoundry {};
     elasticsearch = callPackage ./elasticsearch {};
     gandi = callPackage ./gandi {};
