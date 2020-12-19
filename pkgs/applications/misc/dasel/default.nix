6ea2fd15d881006b41ea5bbed0f76bffcd85f9f9
0x4A6F <0x4A6F@users.noreply.github.com>
2020-12-19 23:02:17 +0000

    dasel: 1.9.1 -> 1.10.0

--- pkgs/applications/misc/dasel/default.nix
+++ pkgs/applications/misc/dasel/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "dasel";
-  version = "1.9.1";
+  version = "1.10.0";
 
   src = fetchFromGitHub {
     owner = "TomWright";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-W95lMULucXcCDqSDWtRoXZM+zh8mmXhoEeFIukPFI0o=";
+    sha256 = "1n2c6lcn4gad7w1zrh69rjdn5slnf6p7s5rkkkbpvs0jjwri11md";
   };
 
   vendorSha256 = "1il1vnv0v97qh8f47md5i6qaac2k8par0pd0z7zqg67vxq6gim85";
