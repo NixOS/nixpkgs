3505c8bcd0932bf3efaba6f64452ce136a07be6f
Astro <astro@spaceboyz.net>
2020-12-19 22:59:07 +0100

    probe-run: 0.1.3 -> 0.1.8

--- pkgs/development/tools/rust/probe-run/default.nix
+++ pkgs/development/tools/rust/probe-run/default.nix
@@ -2,18 +2,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "probe-run";
-  version = "0.1.3";
+  version = "0.1.8";
 
   src = fetchFromGitHub {
     owner = "knurling-rs";
     repo = "probe-run";
     rev = "v${version}";
-    sha256 = "0r1asnsslzq0mxhnbwzc3fg6j1i1621p11923yp649lqmz598mcx";
+    sha256 = "1jgml8jgnv8a2giblx8jxvfh16y296xm9p6m291i0xcg3fccga70";
   };
 
   nativeBuildInputs = [ pkg-config rustfmt ];
   buildInputs = [ libusb1 ];
-  cargoSha256 = "14rw1q329r9xs28qyy5qhjmw9v8bjab74gdzslky1di6ynkwizd5";
+  cargoSha256 = "14hrvdmrv9ybz2v5pvz6m3ld074l1aiqb2m9y1pch01i1mg347mw";
 
   meta = with stdenv.lib; {
     description = "Custom Cargo runner that transparently runs Rust firmware on a remote device";
