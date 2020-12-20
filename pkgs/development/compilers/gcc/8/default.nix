0df75bc04e14e4af11e0124791bfd41bbff51b88
John Ericson <John.Ericson@Obsidian.Systems>
2020-12-20 06:26:04 +0000

    gcc 8, 10: tabs -> spaces

--- pkgs/development/compilers/gcc/8/default.nix
+++ pkgs/development/compilers/gcc/8/default.nix
@@ -134,10 +134,10 @@ stdenv.mkDerivation ({
         )
     else "")
       + stdenv.lib.optionalString targetPlatform.isAvr ''
-	        makeFlagsArray+=(
-	           'LIMITS_H_TEST=false'
-	        )
-	      '';
+            makeFlagsArray+=(
+               'LIMITS_H_TEST=false'
+            )
+          '';
 
   inherit noSysDirs staticCompiler crossStageStatic
     libcCross crossMingw;
