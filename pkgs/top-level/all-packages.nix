b2a1f176ed17e47f134366cba22cfb950629ff2b
con-f-use <con-f-use@gmx.net>
2020-12-21 21:42:48 +0100

    docopts: add to all-packages

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12725,6 +12725,8 @@ in
 
   docopt_cpp = callPackage ../development/libraries/docopt_cpp { };
 
+  docopts = callPackage ../development/tools/misc/docopts { };
+
   dotconf = callPackage ../development/libraries/dotconf { };
 
   draco = callPackage ../development/libraries/draco { };
