be3bf05085282c6edf0826218fa7a32944a8985c
Bastian Köcher <git@kchr.de>
2020-12-21 22:18:20 +0100

    rust-analyzer: 2020-11-09 -> 2020-12-21

--- pkgs/development/tools/rust/rust-analyzer/default.nix
+++ pkgs/development/tools/rust/rust-analyzer/default.nix
@@ -2,10 +2,10 @@
 
 {
   rust-analyzer-unwrapped = callPackage ./generic.nix rec {
-    rev = "2020-11-09";
+    rev = "2020-12-21";
     version = "unstable-${rev}";
-    sha256 = "sha256-SX9dvx2JtYZBxA3+dHQKX/jrjbAMy37/SAybDjlYcSs=";
-    cargoSha256 = "sha256-+td+wMmI+MyGz9oPC+SPO2TmAV0+3lOORNY7xf6s3vI=";
+    sha256 = "sha256:0yz4mc6vbqrgnziscziy2z0241zmx5kzsiy4hhw5p70hvmb5nm9h";
+    cargoSha256 = "sha256:1mqd0i397nwcdhm21rv4w38n3mqrz0cvwk89vmhi8mwv37pqsnxx";
   };
 
   rust-analyzer = callPackage ./wrapper.nix {} {
