6e9d407ba9dc60273fc9df3324f7e4b0d34a8dc9
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-12-21 04:18:36 +0100

    netdata: 1.26.0 -> 1.28.0
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- pkgs/tools/system/netdata/default.nix
+++ pkgs/tools/system/netdata/default.nix
@@ -15,14 +15,14 @@ with stdenv.lib;
 let
   go-d-plugin = callPackage ./go.d.plugin.nix {};
 in stdenv.mkDerivation rec {
-  version = "1.26.0";
+  version = "1.28.0";
   pname = "netdata";
 
   src = fetchFromGitHub {
     owner = "netdata";
     repo = "netdata";
     rev = "v${version}";
-    sha256 = "0pvl1y1qscwp1chrbmk43xf9ddjxgfm0hcslbdbljjis7ng4gacg";
+    sha256 = "1266jbfw55r1zh00xi6c90j2fs9hw8hmsb7686rh04l8mffny9f4";
   };
 
   nativeBuildInputs = [ autoreconfHook pkgconfig ];
