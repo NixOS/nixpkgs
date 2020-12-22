a926df739881461db3ee67ad8c6f7cd07aeedd7d
R. RyanTM <ryantm-bot@ryantm.com>
2020-12-22 07:29:26 +0000

    python37Packages.toposort: 1.5 -> 1.6

--- pkgs/development/python-modules/toposort/default.nix
+++ pkgs/development/python-modules/toposort/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "toposort";
-  version = "1.5";
+  version = "1.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "dba5ae845296e3bf37b042c640870ffebcdeb8cd4df45adaa01d8c5476c557dd";
+    sha256 = "a7428f56ef844f5055bb9e9e44b343983773ae6dce0fe5b101e08e27ffbd50ac";
   };
 
   meta = with stdenv.lib; {
