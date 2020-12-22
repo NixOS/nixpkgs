bc28967a1898f006119842a6ccd0484def2f72f6
Hunter Jones <hjones2199@gmail.com>
2020-12-22 11:36:25 -0600

    phd2: add GApps wrapper to fix breakage on non-nixos wayland systems

--- pkgs/applications/science/astronomy/phd2/default.nix
+++ pkgs/applications/science/astronomy/phd2/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, fetchFromGitHub, pkg-config, cmake, gtk3,
-  wxGTK30-gtk3, curl, gettext, glib, indilib, libnova }:
+{ stdenv, fetchFromGitHub, pkg-config, cmake, gtk3, wxGTK30-gtk3,
+  curl, gettext, glib, indilib, libnova, wrapGAppsHook }:
 
 stdenv.mkDerivation rec {
   pname = "phd2";
@@ -12,12 +12,17 @@ stdenv.mkDerivation rec {
     sha256 = "1ih7m9lilh12xbhmwm9kkicaqy72mi3firl6df7m5x38n2zj3zm4";
   };
 
-  nativeBuildInputs = [ cmake pkg-config ];
+  nativeBuildInputs = [ cmake pkg-config wrapGAppsHook ];
   buildInputs = [ gtk3 wxGTK30-gtk3 curl gettext glib indilib libnova ];
 
   cmakeFlags = [
     "-DOPENSOURCE_ONLY=1"
   ];
+
+  # Fix broken wrapped name scheme by moving wrapped binary to where wrapper expects it
+  postFixup = ''
+    mv $out/bin/.phd2.bin-wrapped $out/bin/.phd2-wrapped.bin
+  '';
 
   meta = with stdenv.lib; {
     homepage = "https://openphdguiding.org/";
