e210e98ff16be03e2f4bc3830c4ce55c331f55a3
remunds <raban.emunds@gmail.com>
2020-12-22 16:21:17 +0100

    this one does not work

--- pkgs/desktops/gnome-3/extensions/pop-os-shell/default.nix
+++ pkgs/desktops/gnome-3/extensions/pop-os-shell/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, nodePackages, glib }:
+{ stdenv, fetchFromGitHub, nodePackages, glib, substituteAll, gjs }:
 
 stdenv.mkDerivation rec {
   pname = "pop-os-shell";
@@ -7,13 +7,26 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "shell";
-    rev =  version;
+    rev = version;
     sha256 = "sha256-KOp/0R7P/iH52njr7JPDKd4fAoN88d/pfou2gWy5QPk=";
   };
 
   nativeBuildInputs = [ glib nodePackages.typescript ];
 
+  patches = [
+    (substituteAll {
+      src = ./fix-gjs.patch;
+      inherit gjs;
+    })
+  ];
+
   makeFlags = [ "INSTALLBASE=$(out)/share/gnome-shell/extensions PLUGIN_BASE=$(out)/share/pop-shell/launcher" ];
+
+  postInstall = ''
+    chmod +x $out/share/gnome-shell/extensions/pop-shell@system76.com/floating_exceptions/main.js
+    chmod +x $out/share/gnome-shell/extensions/pop-shell@system76.com/color_dialog/main.js
+    echo "heeeelo"
+  '';
 
   meta = with stdenv.lib; {
     description = "Keyboard-driven layer for GNOME Shell";
