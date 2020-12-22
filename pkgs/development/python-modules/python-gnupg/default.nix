6e680ab28888b06dce44aee356aa945fa2261030
Elliot Cameron <eacameron@gmail.com>
2020-12-22 13:46:26 -0500

    pythonPackages.python-gnupg: Patch to fix tests on some systems
    For some reason /etc/foo is writable during the nix build on some
    systems. I've observed it on an aarch64 Ubuntu 18.04 system.
    
    This patch is also filed as an issues against the upstream package
    so once it lands we can remove it here.

--- pkgs/development/python-modules/python-gnupg/default.nix
+++ pkgs/development/python-modules/python-gnupg/default.nix
@@ -1,16 +1,25 @@
-{ stdenv, buildPythonPackage, fetchPypi, gnupg }:
+{ stdenv, buildPythonPackage, fetchpatch, fetchPypi, gnupg }:
 
 buildPythonPackage rec {
   pname   = "python-gnupg";
-  version = "0.4.6";
+  version = "0.4.6"; # Please remove the patch below when >0.4.6
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "3aa0884b3bd414652c2385b9df39e7b87272c2eca1b8fcc3089bc9e58652019a";
   };
 
+  patches = [
+  ] ++ stdenv.lib.optionals (let cutoff = "0.4.6"; in version == cutoff || stdenv.lib.versionOlder version cutoff) [
+    # Remove on next version bump
+    (fetchpatch {
+      url = "https://bitbucket.org/vinay.sajip/python-gnupg/commits/443fc58ed3ce5ea928d90c16d6cfac25e40c05ba/raw";
+      sha256 = "1ppbg944227nj6yylhcxfr6bdrlhzripahmjh1a0c63vxzrcwg4m";
+    })
+  ];
+
   # Let's make the library default to our gpg binary
-  patchPhase = ''
+  postPatch = ''
     substituteInPlace gnupg.py \
     --replace "gpgbinary='gpg'" "gpgbinary='${gnupg}/bin/gpg'"
     substituteInPlace test_gnupg.py \
