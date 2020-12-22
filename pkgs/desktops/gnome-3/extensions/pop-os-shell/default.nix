b4f8e0120472735bd0d056f1e1c9c2a14408bdff
remunds <raban.emunds@gmail.com>
2020-12-22 17:21:38 +0100

    color switch still not working

--- pkgs/desktops/gnome-3/extensions/pop-os-shell/default.nix
+++ pkgs/desktops/gnome-3/extensions/pop-os-shell/default.nix
@@ -11,13 +11,12 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-KOp/0R7P/iH52njr7JPDKd4fAoN88d/pfou2gWy5QPk=";
   };
 
-  nativeBuildInputs = [ glib nodePackages.typescript ];
+  nativeBuildInputs = [ glib nodePackages.typescript gjs ];
+
+  buildInputs = [ gjs ];
 
   patches = [
-    (substituteAll {
-      src = ./fix-gjs.patch;
-      inherit gjs;
-    })
+    ./fix-gjs.patch
   ];
 
   makeFlags = [ "INSTALLBASE=$(out)/share/gnome-shell/extensions PLUGIN_BASE=$(out)/share/pop-shell/launcher" ];
@@ -25,7 +24,6 @@ stdenv.mkDerivation rec {
   postInstall = ''
     chmod +x $out/share/gnome-shell/extensions/pop-shell@system76.com/floating_exceptions/main.js
     chmod +x $out/share/gnome-shell/extensions/pop-shell@system76.com/color_dialog/main.js
-    echo "heeeelo"
   '';
 
    meta = with stdenv.lib; {
