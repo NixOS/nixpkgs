a9544e450db626ea33f67a935f6e888e01c57945
R. RyanTM <ryantm-bot@ryantm.com>
2020-12-23 14:08:26 +0000

    python37Packages.eliot: 1.12.0 -> 1.13.0

--- pkgs/development/python-modules/eliot/default.nix
+++ pkgs/development/python-modules/eliot/default.nix
@@ -15,12 +15,12 @@
 
 buildPythonPackage rec {
   pname = "eliot";
-  version = "1.12.0";
+  version = "1.13.0";
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0wabv7hk63l12881f4zw02mmj06583qsx2im0yywdjlj8f56vqdn";
+    sha256 = "5760194b308a7ab35514ae1b942d88e9f2359071556d82580383f09ca586fff7";
   };
 
   checkInputs = [
