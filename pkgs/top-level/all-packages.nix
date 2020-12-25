4dc9fff596a74d555fc8464029efa16e23702a0b
Bart Brouns <bart@magnetophon.nl>
2020-12-25 13:18:35 +0100

    stochas: init at 1.3.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23113,6 +23113,8 @@ in
 
   station = callPackage ../applications/networking/station { };
 
+  stochas = callPackage ../applications/audio/stochas { };
+
   synapse = callPackage ../applications/misc/synapse { };
 
   synapse-bt = callPackage ../applications/networking/p2p/synapse-bt {
