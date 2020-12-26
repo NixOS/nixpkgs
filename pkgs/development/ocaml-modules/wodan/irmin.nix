9c7c12f9f77a9b7afff0b023c51f2c3a6fa63f83
Emery Hemingway <ehmry@posteo.net>
2020-12-26 18:52:48 +0100

    ocamlPackages.wodan: init unstable-2020-11-20

---
+++ pkgs/development/ocaml-modules/wodan/irmin.nix
@@ -0,0 +1,19 @@
+{ lib, buildDunePackage, irmin-chunk, irmin-git, irmin-unix
+, mirage-block-ramdisk, mirage-block-unix, wodan }:
+
+buildDunePackage rec {
+  pname = "wodan-irmin";
+  inherit (wodan) version src useDune2;
+
+  propagatedBuildInputs = [
+    irmin-chunk
+    irmin-git
+    irmin-unix
+    mirage-block-ramdisk
+    mirage-block-unix
+    wodan
+  ];
+
+  meta = wodan.meta // { description = "Wodan as an Irmin store"; };
+
+}
