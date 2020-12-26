5595eeb61ff374f739dca6096e85f9539e5c235c
Andreas <hamburger1984@gmail.com>
2020-12-26 21:22:38 +0100

    frmeworkintegration: add dependencies

--- pkgs/development/libraries/kde-frameworks/frameworkintegration.nix
+++ pkgs/development/libraries/kde-frameworks/frameworkintegration.nix
@@ -2,7 +2,8 @@
   mkDerivation, lib,
   extra-cmake-modules,
   kbookmarks, kcompletion, kconfig, kconfigwidgets, ki18n, kiconthemes, kio,
-  knewstuff, knotifications, kpackage, kwidgetsaddons, libXcursor, qtx11extras
+  knewstuff, knotifications, kpackage, kwidgetsaddons, libXcursor, qtx11extras,
+  appstream-qt, packagekit-qt
 }:
 
 mkDerivation {
@@ -11,7 +12,7 @@ mkDerivation {
   nativeBuildInputs = [ extra-cmake-modules ];
   buildInputs = [
     kbookmarks kcompletion kconfig ki18n kio knewstuff knotifications kpackage
-    kwidgetsaddons libXcursor qtx11extras
+    kwidgetsaddons libXcursor qtx11extras appstream-qt packagekit-qt
   ];
   propagatedBuildInputs = [ kconfigwidgets kiconthemes ];
 }
