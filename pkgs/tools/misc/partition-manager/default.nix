36cdc6d656114699f72bd254c553f00314d64a97
Andreas <hamburger1984@gmail.com>
2020-12-26 21:23:23 +0100

    partition-manager: 3.3.1 -> 4.2.0

--- pkgs/tools/misc/partition-manager/default.nix
+++ pkgs/tools/misc/partition-manager/default.nix
@@ -8,19 +8,23 @@ let
   pname = "partitionmanager";
 in mkDerivation rec {
   name = "${pname}-${version}";
-  version = "3.3.1";
+  version = "4.2.0";
 
   src = fetchurl {
     url = "mirror://kde/stable/${pname}/${version}/src/${name}.tar.xz";
-    sha256 = "0jhggb4xksb0k0mj752n6pz0xmccnbzlp984xydqbz3hkigra1si";
+    sha256 = "e9096dd5ce3b11e93a4e45960734f2059609d637e1b70b02f57e6ae61e8884f8";
   };
 
   enableParallelBuilding = true;
 
   nativeBuildInputs = [ extra-cmake-modules kdoctools wrapGAppsHook wrapQtAppsHook ];
 
+  cmakeFlags = [
+    "-DKPMCORE_INCLUDE_DIR=${lib.getDev kpmcore}/include/kpmcore"
+  ];
+
   # refer to kpmcore for the use of eject
-  buildInputs = [ eject libatasmart util-linux ];
+  buildInputs = [ eject libatasmart util-linux kpmcore ];
   propagatedBuildInputs = [ kconfig kcrash kinit kpmcore ];
 
   meta = with lib; {
