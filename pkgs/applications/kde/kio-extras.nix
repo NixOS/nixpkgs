4213f6aaabc73d09036b8e13617c25b9c2aaed97
Andreas <hamburger1984@gmail.com>
2020-12-26 21:11:23 +0100

    kio-extras: add dependencies

--- pkgs/applications/kde/kio-extras.nix
+++ pkgs/applications/kde/kio-extras.nix
@@ -2,8 +2,8 @@
   mkDerivation, lib, extra-cmake-modules, kdoctools, shared-mime-info,
   exiv2, kactivities, karchive, kbookmarks, kconfig, kconfigwidgets,
   kcoreaddons, kdbusaddons, kdsoap, kguiaddons, kdnssd, kiconthemes, ki18n, kio,
-  khtml, kdelibs4support, kpty, syntax-highlighting, libmtp, libssh, openexr,
-  ilmbase, openslp, phonon, qtsvg, samba, solid, gperf
+  khtml, kdelibs4support, kpty, syntax-highlighting, libmtp, libssh, libtirpc, openexr,
+  ilmbase, openslp, phonon, qtsvg, samba, solid, gperf, taglib
 }:
 
 mkDerivation {
@@ -16,8 +16,8 @@ mkDerivation {
   buildInputs = [
     exiv2 kactivities karchive kbookmarks kconfig kconfigwidgets kcoreaddons
     kdbusaddons kdsoap kguiaddons kdnssd kiconthemes ki18n kio khtml
-    kdelibs4support kpty syntax-highlighting libmtp libssh openexr openslp
-    phonon qtsvg samba solid gperf
+    kdelibs4support kpty syntax-highlighting libmtp libssh libtirpc openexr openslp
+    phonon qtsvg samba solid gperf taglib
   ];
   CXXFLAGS = [ "-I${ilmbase.dev}/include/OpenEXR" ];
 }
