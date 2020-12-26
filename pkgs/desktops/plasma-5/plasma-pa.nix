5f40928e450b97125ef843fc03a3d33446d97335
Andreas <hamburger1984@gmail.com>
2020-12-26 21:22:22 +0100

    plasma-pa: add dependency

--- pkgs/desktops/plasma-5/plasma-pa.nix
+++ pkgs/desktops/plasma-5/plasma-pa.nix
@@ -3,14 +3,14 @@
   extra-cmake-modules, kdoctools,
   kconfigwidgets, kcoreaddons, kdeclarative, kglobalaccel, ki18n, kwindowsystem, plasma-framework,
   qtbase, qtdeclarative,
-  gconf, glib, libcanberra-gtk3, libpulseaudio, sound-theme-freedesktop
+  gconf, glib, pcre, libcanberra-gtk3, libpulseaudio, sound-theme-freedesktop
 }:
 
 mkDerivation {
   name = "plasma-pa";
   nativeBuildInputs = [ extra-cmake-modules kdoctools ];
   buildInputs = [
-    gconf glib libcanberra-gtk3 libpulseaudio sound-theme-freedesktop
+    gconf glib pcre libcanberra-gtk3 libpulseaudio sound-theme-freedesktop
 
     kconfigwidgets kcoreaddons kdeclarative kglobalaccel ki18n plasma-framework
     kwindowsystem
