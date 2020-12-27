c9e2ce3d837ec9e66a41dd69cd35517e2ed6f919
Mario Rodas <marsam@users.noreply.github.com>
2020-12-27 04:21:00 -0500

    pcaudiolib: fix homepage

--- pkgs/development/libraries/pcaudiolib/default.nix
+++ pkgs/development/libraries/pcaudiolib/default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
   version = "1.1";
 
   src = fetchFromGitHub {
-    owner = "rhdunn";
+    owner = "espeak-ng";
     repo = "pcaudiolib";
     rev = version;
     sha256 = "0c55hlqqh0m7bcb3nlgv1s4a22s5bgczr1cakjh3767rjb10khi0";
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Provides a C API to different audio devices";
-    homepage = "https://github.com/rhdunn/pcaudiolib";
+    homepage = "https://github.com/espeak-ng/pcaudiolib";
     license = licenses.gpl3;
     maintainers = with maintainers; [ aske ];
     platforms = platforms.all;
