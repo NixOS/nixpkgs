83cbd45c1e36b25762217a1c891c72b9897da969
Peter Hoeg <peter@hoeg.com>
2020-12-27 17:49:17 +0800

    neochat: clean up

--- pkgs/applications/networking/instant-messengers/neochat/default.nix
+++ pkgs/applications/networking/instant-messengers/neochat/default.nix
@@ -1,23 +1,28 @@
-{ mkDerivation, stdenv, fetchFromGitLab
-, pkgconfig, wrapQtAppsHook
+{ mkDerivation
+, stdenv
+, fetchFromGitLab
+, pkg-config
 , cmake
-, qtbase, qttools, qtquickcontrols2, qtmultimedia, qtkeychain
+, qtbase
+, qttools
+, qtquickcontrols2
+, qtmultimedia
+, qtkeychain
 , libpulseaudio
-# Not mentioned but seems needed
-, qtgraphicaleffects
-, qtdeclarative
-, qtmacextras
-, olm, libsecret, cmark, extra-cmake-modules, kirigami2, ki18n, knotifications, kdbusaddons, kconfig, libquotient
-, KQuickImageEdit, kitemmodels
+, olm
+, libsecret
+, cmark
+, extra-cmake-modules
+, kirigami2
+, ki18n
+, knotifications
+, kdbusaddons
+, kconfig
+, libquotient
+, kquickimageedit
 }:
 
-let
-qtkeychain-qt5 = qtkeychain.override {
-  inherit qtbase qttools;
-  withQt5 = true;
-};
-
-in mkDerivation rec {
+mkDerivation rec {
   pname = "neochat";
   version = "v1.0";
 
@@ -29,13 +34,29 @@ in mkDerivation rec {
     sha256 = "1r9n83kvc5v215lzmzh6hyc5q9i3w6znbf508qk0mdwdzxz4zry9";
   };
 
-  nativeBuildInputs = [ pkgconfig cmake extra-cmake-modules wrapQtAppsHook ];
-  buildInputs = [ qtbase qtkeychain-qt5 qtquickcontrols2 qtmultimedia qtgraphicaleffects qtdeclarative olm libsecret cmark kirigami2 ki18n knotifications kdbusaddons kconfig libquotient KQuickImageEdit kitemmodels libpulseaudio ];
+  nativeBuildInputs = [ cmake extra-cmake-modules pkg-config ];
+
+  buildInputs = [
+    qtkeychain
+    qtquickcontrols2
+    qtmultimedia
+    olm
+    libsecret
+    cmark
+    kirigami2
+    ki18n
+    knotifications
+    kdbusaddons
+    kconfig
+    libquotient
+    kquickimageedit
+    libpulseaudio
+  ];
 
   meta = with stdenv.lib; {
     description = "A client for matrix, the decentralized communication protocol.";
     homepage = "https://apps.kde.org/en/neochat";
-    license = licenses.gpl3;
+    license = licenses.gpl3Only;
     platforms = with platforms; linux;
   };
 }
