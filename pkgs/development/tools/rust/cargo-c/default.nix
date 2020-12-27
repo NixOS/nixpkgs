029235658720fe0ea48cfcc29b1e00a91d2308da
freezeboy <freezeboy@users.noreply.github.com>
2020-12-27 21:08:37 +0100

    cargo-c: 0.6.18 -> 0.7.0

--- pkgs/development/tools/rust/cargo-c/default.nix
+++ pkgs/development/tools/rust/cargo-c/default.nix
@@ -5,7 +5,7 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-c";
-  version = "0.6.18";
+  version = "0.7.0";
 
   src = stdenv.mkDerivation rec {
     name = "${pname}-source-${version}";
@@ -14,11 +14,11 @@ rustPlatform.buildRustPackage rec {
       owner = "lu-zero";
       repo = pname;
       rev = "v${version}";
-      sha256 = "1dh5z210nl8grjxb8zxch8h7799w61bah7r2j0s07091rcpfsrsb";
+      sha256 = "ckHm95nuDb5WcKBLNz1DWTW/TOt+7nuIq90l4a/doV0=";
     };
     cargoLock = fetchurl {
       url = "https://github.com/lu-zero/${pname}/releases/download/v${version}/Cargo.lock";
-      sha256 = "1h5wmfmm2a2ilyw3ar88rqm7yvdc2vhyx4pgg781615ax52fhjli";
+      sha256 = "MJlO/RQvxplsfLOUrhtHKlcwdKyykbzdQyLlYWppsIM=";
     };
 
     installPhase = ''
@@ -28,7 +28,7 @@ rustPlatform.buildRustPackage rec {
     '';
   };
 
-  cargoSha256 = "0ll9p2rbnw46zd9m2bmdmn99v9jjjf8i33xpkvd1rx42ki7sys62";
+  cargoSha256 = "mfTP4D1H11NZNA0ZttND6oLzhj7sYZRHdWXpjvIEr6s=";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ]
