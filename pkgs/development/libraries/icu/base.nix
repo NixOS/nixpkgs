1da03f0c4bed1c446e1db67350c76c5332258f8f
Nick Fraser <icanlosh@gmail.com>
2020-12-27 00:01:06 +0000

    icu: Fixed source download URL.

--- pkgs/development/libraries/icu/base.nix
+++ pkgs/development/libraries/icu/base.nix
@@ -9,8 +9,7 @@ let
 
   baseAttrs = {
     src = fetchurl {
-      url = "http://download.icu-project.org/files/${pname}/${version}/${pname}-"
-        + (stdenv.lib.replaceChars ["."] ["_"] version) + "-src.tgz";
+      url = "https://github.com/unicode-org/icu/releases/download/release-${lib.replaceChars [ "." ] [ "-" ] version}/icu4c-${lib.replaceChars [ "." ] [ "_" ] version}-src.tgz";
       inherit sha256;
     };
 
