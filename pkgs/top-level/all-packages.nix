dcab5b7d573210608728b65c314c482eac35b935
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2020-12-29 17:40:22 +0100

    ksmoothdock: qt514 -> qt515 (fix build)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5124,7 +5124,7 @@ in
 
   peruse = libsForQt5.callPackage ../tools/misc/peruse { };
 
-  ksmoothdock = libsForQt514.callPackage ../applications/misc/ksmoothdock { };
+  ksmoothdock = libsForQt5.callPackage ../applications/misc/ksmoothdock { };
 
   kstars = libsForQt514.callPackage ../applications/science/astronomy/kstars { };
 
