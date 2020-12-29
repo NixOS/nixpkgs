35ecda3c6caf9d04bd391e7164bde0d5c15ba4cd
Peter Hoeg <peter@hoeg.com>
2020-12-30 06:16:54 +0800

    libquotient: 0.6.2 -> 0.6.3

--- pkgs/development/libraries/libquotient/default.nix
+++ pkgs/development/libraries/libquotient/default.nix
@@ -2,13 +2,13 @@
 
 mkDerivation rec {
   pname = "libquotient";
-  version = "0.6.2";
+  version = "0.6.3";
 
   src = fetchFromGitHub {
     owner = "quotient-im";
     repo = "libQuotient";
     rev = version;
-    sha256 = "1721cy6zaq086nrwh9x4d7k1jiaygg1wkvyx486i9bj9z53lc3wd";
+    sha256 = "sha256-RYEcFClRdAippG0kspNi9QZIzZAuU4++9LOQTZcqpVc=";
   };
 
   buildInputs = [ qtbase qtmultimedia ];
@@ -18,7 +18,7 @@ mkDerivation rec {
   meta = with lib; {
     description = "A Qt5 library to write cross-platfrom clients for Matrix";
     homepage = "https://matrix.org/docs/projects/sdk/quotient";
-    maintainers = with maintainers; [ colemickens ];
     license = licenses.lgpl21;
+    maintainers = with maintainers; [ colemickens ];
   };
 }
