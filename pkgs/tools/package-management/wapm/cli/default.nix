558327712176f16d00b599d5c3422c4365115d2f
Luc Perkins <lucperkins@gmail.com>
2020-12-28 19:33:26 -0800

    Update pkgs/tools/package-management/wapm/cli/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/tools/package-management/wapm/cli/default.nix
+++ pkgs/tools/package-management/wapm/cli/default.nix
@@ -16,8 +16,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "0w0ck60xm47b93y803i2b2bpv7nsahzvzda3ll7pjg88f8qz5fx1";
   };
 
-  buildInputs = []
-    ++ stdenv.lib.optionals stdenv.isLinux [ openssl ]
+  buildInputs = stdenv.lib.optionals stdenv.isLinux [ openssl ]
     ++ stdenv.lib.optionals stdenv.isDarwin [ Security ];
 
   meta = with stdenv.lib; {
