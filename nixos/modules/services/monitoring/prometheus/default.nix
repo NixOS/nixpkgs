b03d74001b453d3a2d6b3a4deca310827583bbae
Phillip Cloud <cloud@standard.ai>
2020-12-29 11:53:17 -0500

    nixos/prometheus: update relabel_action list
    (cherry picked from commit 287939e2eddcc79c9cb87b9865f207382e810350)

--- nixos/modules/services/monitoring/prometheus/default.nix
+++ nixos/modules/services/monitoring/prometheus/default.nix
@@ -432,10 +432,10 @@ let
         regular expression matches.
       '';
 
-      action = mkDefOpt (types.enum ["replace" "keep" "drop"]) "replace" ''
+      action =
+        mkDefOpt (types.enum ["replace" "keep" "drop" "hashmod" "labelmap" "labeldrop" "labelkeep"]) "replace" ''
         Action to perform based on regex matching.
       '';
-
     };
   };
 
