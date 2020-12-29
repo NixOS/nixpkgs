497e07e9a3263015ae65b23de9f3bc018ac68785
Paul Peregud <paulperegud@protonmail.ch>
2020-12-29 13:17:41 +0100

    Update pkgs/applications/networking/bee/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/applications/networking/bee/default.nix
+++ pkgs/applications/networking/bee/default.nix
@@ -4,18 +4,16 @@ buildGoModule rec {
   pname = "bee";
   version = "0.1.0";
 
-  subPackages = [
-    "cmd/bee"
-  ];
-
-  vendorSha256 = "1a9fmf5c3asvylcb0ixlwb6jk2z5hzdaa6yffg1pjvq9rx92jbjy";
-
   src = fetchFromGitHub {
     owner = "ethersphere";
     repo = "bee";
     rev = "v${version}";
     sha256 = "1iwwhs2zm8ml484sc0d7smmmp1x893zjafr1mnaswl8pxg0lnm2l";
   };
+
+  vendorSha256 = "1a9fmf5c3asvylcb0ixlwb6jk2z5hzdaa6yffg1pjvq9rx92jbjy";
+
+  subPackages = [ "cmd/bee" ];  
 
   meta = with stdenv.lib; {
     homepage = "https://swarm-gateways.net/bzz:/docs.swarm.eth/";
