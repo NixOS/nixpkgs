003b1ae3dfaf2807d0ca39a41a909d701f7c8597
Vincent Laporte <Vincent.Laporte@gmail.com>
2020-12-29 16:13:07 +0100

    ocamlPackages.mew_vi: init at 0.5.0
    This is a vi-like modal editing engine generator.

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -545,6 +545,8 @@ let
 
     mew = callPackage ../development/ocaml-modules/mew { };
 
+    mew_vi = callPackage ../development/ocaml-modules/mew_vi { };
+
     mezzo = callPackage ../development/compilers/mezzo { };
 
     minisat = callPackage ../development/ocaml-modules/minisat { };
