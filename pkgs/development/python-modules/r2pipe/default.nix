NixOS/nixpkgs

commit 6556711c87f42e064ba01deb4c1e0aa917be8d66

python3Packages.r2pipe: 1.4.2 -> 1.5.3

pkgs/development/python-modules/r2pipe/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "r2pipe";
-  version = "1.4.2";
+  version = "1.5.3";
 
   postPatch = let
     r2lib = "${lib.getOutput "lib" radare2}/lib";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "4020754e3263ef28d3e74288537847bd8ae5fc1ddd74f34fb262ef1282c4d23c";
+    sha256 = "8f3708195c8a6e91c5753940fd348cd821df1389d23b889b01b3e88acf407485";
   };
 
   # Tiny sanity check to make sure r2pipe finds radare2 (since r2pipe doesn't

commit dfbedd66c68db1415332c085ce61fa3154d63cc1

pythonPackages.r2pipe: Fix build
Regression introduced by 6556711c87f42e064ba01deb4c1e0aa917be8d66.

The string start and end quoting styles have changed in the upstream
source code between version 1.4.2 and version 1.5.3, so the checkPhase
now results in the following error:

  ======================================================================
  ERROR: native (unittest.loader._FailedTest)
  ----------------------------------------------------------------------
  ImportError: Failed to import test module: native
  Traceback (most recent call last):
    File ".../unittest/loader.py", line 154, in loadTestsFromName
      module = __import__(module_name)
    File "/build/r2pipe-1.5.3/r2pipe/native.py", line 113, in <module>
      class RCore(Structure):  # 1
    File "/build/r2pipe-1.5.3/r2pipe/native.py", line 125, in RCore
      cmd_str, r_core_cmd_str = register(
    File "/build/r2pipe-1.5.3/r2pipe/native.py", line 108, in register
      method = WrappedRMethod(cname, args, ret)
    File "/build/r2pipe-1.5.3/r2pipe/native.py", line 53, in __init__
      r2 = r2lib()
    File "/build/r2pipe-1.5.3/r2pipe/native.py", line 27, in r2lib
      raise ImportError("No native r_core library")
  ImportError: No native r_core library

Signed-off-by: aszlig <aszlig@nix.build>

pkgs/development/python-modules/r2pipe/default.nix
@@ -18,10 +18,10 @@ buildPythonPackage rec {
   ''
     # Fix find_library, can be removed after
     # https://github.com/NixOS/nixpkgs/issues/7307 is resolved.
-    substituteInPlace r2pipe/native.py --replace "find_library('r_core')" "'${libr_core}'"
+    substituteInPlace r2pipe/native.py --replace 'find_library("r_core")' "'${libr_core}'"
 
     # Fix the default r2 executable
-    substituteInPlace r2pipe/open_sync.py --replace "r2e = 'radare2'" "r2e = '${radare2}/bin/radare2'"
+    substituteInPlace r2pipe/open_sync.py --replace 'r2e = "radare2"' "r2e = '${radare2}/bin/radare2'"
     substituteInPlace r2pipe/open_base.py --replace 'which("radare2")' "'${radare2}/bin/radare2'"
   '';
 