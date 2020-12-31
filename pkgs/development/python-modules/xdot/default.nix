91d551f73ece214d027b608401d6686ebaff54d5
Robert T. McGibbon <rmcgibbo@gmail.com>
2020-12-30 19:39:29 -0500

    xdot: unbreak build

--- pkgs/development/python-modules/xdot/default.nix
+++ pkgs/development/python-modules/xdot/default.nix
@@ -1,19 +1,25 @@
-{ lib, buildPythonPackage, fetchPypi, isPy3k
-, wrapGAppsHook, gobject-introspection, pygobject3, graphviz, gtk3 }:
+{ lib, buildPythonPackage, fetchFromGitHub, isPy3k
+, wrapGAppsHook, gobject-introspection, pygobject3, graphviz, gtk3
+, numpy, pytestCheckHook }:
 
 buildPythonPackage rec {
   pname = "xdot";
   version = "1.2";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "3df91e6c671869bd2a6b2a8883fa3476dbe2ba763bd2a7646cf848a9eba71b70";
+  src = fetchFromGitHub {
+    owner = "jrfonseca";
+    repo = "xdot.py";
+    rev = version;
+    sha256 = "0lignsx7yr2pq92q8dgyim7d3xlmmwq1l16waz3qj4p6g2yh8023";
   };
 
   disabled = !isPy3k;
 
   nativeBuildInputs = [ wrapGAppsHook ];
-  propagatedBuildInputs = [ gobject-introspection pygobject3 graphviz gtk3 ];
+  propagatedBuildInputs = [ numpy gobject-introspection pygobject3 graphviz gtk3 ];
+  checkPhase = ''
+    python ./test.py;
+  '';
 
   meta = with lib; {
     description = "An interactive viewer for graphs written in Graphviz's dot";
