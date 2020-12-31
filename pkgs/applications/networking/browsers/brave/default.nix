4848ad305de27f66395dc3e602b1d5641c5123e2
Daniël de Kok <me@danieldk.eu>
2020-12-31 11:10:14 +0100

    brave: 1.18.75 -> 1.18.77
    Changelog:
    https://community.brave.com/t/release-channel-1-18-77/191009
    (cherry picked from commit e2a9d90153ccb34d292bca9304335fc9668dbc54)

--- pkgs/applications/networking/browsers/brave/default.nix
+++ pkgs/applications/networking/browsers/brave/default.nix
@@ -88,11 +88,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "brave";
-  version = "1.18.75";
+  version = "1.18.77";
 
   src = fetchurl {
     url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
-    sha256 = "1njgdw7ml30xs517brc7z7piy6lcylrfjhz6wn1dp7gywsxfgx1h";
+    sha256 = "AV3bqtWaoy6AVnt8K/Qo+7hguAIsPJPZhgLSeOvJ7JY=";
   };
 
   dontConfigure = true;
