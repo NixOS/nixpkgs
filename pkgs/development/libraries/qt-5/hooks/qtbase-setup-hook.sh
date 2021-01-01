b868d93d621dc41c3c8866871295db48d2c371d1
Thomas Tuegel <ttuegel@mailbox.org>
2021-01-01 08:50:10 -0600

    fixup! qtbase: run setup hook once only

--- pkgs/development/libraries/qt-5/hooks/qtbase-setup-hook.sh
+++ pkgs/development/libraries/qt-5/hooks/qtbase-setup-hook.sh
@@ -1,11 +1,12 @@
 if [[ -n "$__nix_qtbase" ]]; then
+    # Throw an error if a different version of Qt was already set up.
     if [[ "$__nix_qtbase" != "@dev@" ]]; then
         echo >&2 "Error: detected mismatched Qt dependencies:"
         echo >&2 "    @dev@"
         echo >&2 "    $__nix_qtbase"
         exit 1
     fi
-else
+else # Only set up Qt once.
 __nix_qtbase="@dev@"
 
 qtPluginPrefix=@qtPluginPrefix@
