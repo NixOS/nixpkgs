1b0448136b40136c6c664be96e3b140c615c5750
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-01-01 14:56:20 +0100

    ocamlPackages.ppx_deriving_yojson: 3.5.3 -> 3.6.1

--- pkgs/development/ocaml-modules/ppx_deriving_yojson/default.nix
+++ pkgs/development/ocaml-modules/ppx_deriving_yojson/default.nix
@@ -1,25 +1,26 @@
-{ lib, buildDunePackage, fetchFromGitHub, ppxfind, ounit
-, ppx_deriving, yojson
+{ lib, buildDunePackage, fetchFromGitHub
+, ounit, ppxlib, ppx_deriving, yojson, result
 }:
 
 buildDunePackage rec {
   pname = "ppx_deriving_yojson";
-  version = "3.5.3";
+  version = "3.6.1";
 
-  minimumOCamlVersion = "4.04";
+  minimumOCamlVersion = "4.05";
+
+  useDune2 = true;
 
   src = fetchFromGitHub {
     owner = "ocaml-ppx";
     repo = "ppx_deriving_yojson";
     rev = "v${version}";
-    sha256 = "030638gp39mr4hkilrjhd98q4s8gjqxifm6fy6bwqrg74hmrl2y5";
+    sha256 = "1icz5h6p3pfj7my5gi7wxpflrb8c902dqa17f9w424njilnpyrbk";
   };
 
-  buildInputs = [ ppxfind ounit ];
-
-  propagatedBuildInputs = [ ppx_deriving yojson ];
+  propagatedBuildInputs = [ result ppx_deriving yojson ppxlib ];
 
   doCheck = true;
+  checkInputs = [ ounit ];
 
   meta = {
     description = "A Yojson codec generator for OCaml >= 4.04";
