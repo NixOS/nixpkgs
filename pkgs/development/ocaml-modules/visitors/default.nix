dfea44255dedcad0c9bdab9d825fd241154c9b95
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-01-01 16:24:39 +0100

    ocamlPackages.visitors: 20200210 -> unstable-2020-11-12
    Upgrade to an unstable version (which has a changelog, kind of a semi
    release?) which is compatible with ppx_deriving >= 5.0.

--- pkgs/development/ocaml-modules/visitors/default.nix
+++ pkgs/development/ocaml-modules/visitors/default.nix
@@ -1,24 +1,26 @@
-{ lib, buildDunePackage, fetchFromGitLab, ppx_tools, ppx_deriving, result, cppo }:
+{ lib, buildDunePackage, fetchFromGitLab
+, ppxlib, ppx_deriving, result
+, cppo, core_bench
+}:
 
 buildDunePackage rec {
   pname = "visitors";
-  version = "20200210";
+  version = "unstable-2020-11-12";
+  # 20201112 has a changelog entry, but no release tag
 
   useDune2 = true;
 
-  minimumOCamlVersion = "4.02.3";
+  minimumOCamlVersion = "4.07";
 
   src = fetchFromGitLab {
     owner = "fpottier";
     repo = pname;
-    rev = version;
+    rev = "ffa92a0fdfdfe7e8ea12a53cfc7d51d1f3f34d5b";
     domain = "gitlab.inria.fr";
-    sha256 = "12i099h1hc1walabiwqbinnpgcxkc1wn72913v7v6vvyif21rb5a";
+    sha256 = "14b86nh0jlsj71qxzzdkqp2zyah6czhnfb7djakghwl6633445ci";
   };
 
-  buildInputs = [ cppo ];
-
-  propagatedBuildInputs = [ ppx_tools ppx_deriving result ];
+  propagatedBuildInputs = [ ppxlib ppx_deriving result ];
 
   meta = with lib; {
     homepage = "https://gitlab.inria.fr/fpottier/visitors";
