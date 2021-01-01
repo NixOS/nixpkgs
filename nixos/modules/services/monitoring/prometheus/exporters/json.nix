3f94c66ee121a2fdb136741c3685195c6d1db643
WilliButz <wbutz@cyberfnord.de>
2021-01-01 23:59:06 +0100

    nixos/prometheus-json-exporter: update modules & tests, add release notes

--- nixos/modules/services/monitoring/prometheus/exporters/json.nix
+++ nixos/modules/services/monitoring/prometheus/exporters/json.nix
@@ -4,32 +4,42 @@ with lib;
 
 let
   cfg = config.services.prometheus.exporters.json;
+
 in
 {
   port = 7979;
   extraOpts = {
-    url = mkOption {
-      type = types.str;
-      description = ''
-        URL to scrape JSON from.
-      '';
-    };
     configFile = mkOption {
       type = types.path;
       description = ''
         Path to configuration file.
       '';
     };
-    listenAddress = {}; # not used
   };
   serviceOpts = {
     serviceConfig = {
       ExecStart = ''
-        ${pkgs.prometheus-json-exporter}/bin/prometheus-json-exporter \
-          --port ${toString cfg.port} \
-          ${cfg.url} ${escapeShellArg cfg.configFile} \
+        ${pkgs.prometheus-json-exporter}/bin/json_exporter \
+          --config.file ${escapeShellArg cfg.configFile} \
+          --web.listen-address="${cfg.listenAddress}:${toString cfg.port}" \
           ${concatStringsSep " \\\n  " cfg.extraFlags}
       '';
     };
   };
+  imports = [
+    (mkRemovedOptionModule [ "url" ] ''
+      This option was removed. The URL of the endpoint serving JSON
+      must now be provided to the exporter by prometheus via the url
+      parameter <literal>target</literal>.
+
+      In prometheus a scrape URL would look like this:
+      <programlisting>
+      http://some.json-exporter.host:7979/probe?target=https://example.com/some/json/endpoint
+      </programlisting>
+
+      For more information, take a look at the <link xlink:href="https://github.com/prometheus-community/json_exporter">
+      official documentation</link> of the json_exporter.
+    '')
+     ({ options.warnings = options.warnings; options.assertions = options.assertions; })
+  ];
 }
