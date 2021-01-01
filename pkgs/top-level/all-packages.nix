f90045d24c972570b627969574d7505843a8b3fe
AndersonTorres <torres.anderson.85@protonmail.com>
2021-01-01 13:49:48 -0300

    wio: init at unstable-2020-11-02

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24966,6 +24966,8 @@ in
 
   weston = callPackage ../applications/window-managers/weston { pipewire = pipewire_0_2; };
 
+  wio = callPackage ../applications/window-managers/wio { };
+
   whitebox-tools = callPackage ../applications/gis/whitebox-tools {
     inherit (darwin.apple_sdk.frameworks) Security;
   };
