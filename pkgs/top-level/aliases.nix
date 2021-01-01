fa6c8b656b72dfa97d7a5aa9892569fd6eb64dfc
midchildan <git@midchildan.org>
2021-01-02 01:02:32 +0900

    macfuse-stubs: init at 4.0.4
    replaces osxfuse 3.8.3

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -459,6 +459,7 @@ mapAliases ({
   openssh_with_kerberos = openssh; # added 2018-01-28
   onnxruntime = throw "onnxruntime has been removed due to poor maintainability"; # added 2020-12-04
   osquery = throw "osquery has been removed."; # added 2019-11-24
+  osxfuse = macfuse-stubs; # added 2021-03-20
   otter-browser = throw "otter-browser has been removed from nixpkgs, as it was unmaintained"; # added 2020-02-02
   owncloudclient = owncloud-client;  # added 2016-08
   p11_kit = p11-kit; # added 2018-02-25
