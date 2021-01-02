a500ff1c51966cdc64e66e6f40233c8130eb2d9d
0x4A6F <0x4A6F@users.noreply.github.com>
2021-01-02 15:15:09 +0000

    dasel: 1.11.0 -> 1.12.0
    Added
    - Add -c, --compact flag to remove pretty-print formatting from JSON output.
    - Defined storage.IndentOption(indent string) ReadWriteOption.
    - Defined storage.PrettyPrintOption(enabled bool) ReadWriteOption.
    
    Changed
    - Changed storage.Parser funcs to allow the passing of ...ReadWriteOption.

--- pkgs/applications/misc/dasel/default.nix
+++ pkgs/applications/misc/dasel/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "dasel";
-  version = "1.11.0";
+  version = "1.12.0";
 
   src = fetchFromGitHub {
     owner = "TomWright";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1xyh41vb2rypajjzbysw6k8x39bna8j3fmxcqyjcrqs0dzx78nk3";
+    sha256 = "69igz0Q7pT0f6PsbZWHcwUiTKRTTzj7r5E6E5ExUoJo=";
   };
 
-  vendorSha256 = "1il1vnv0v97qh8f47md5i6qaac2k8par0pd0z7zqg67vxq6gim85";
+  vendorSha256 = "BdX4DO77mIf/+aBdkNVFUzClsIml1UMcgvikDbbdgcY=";
 
   buildFlagsArray = ''
     -ldflags=-s -w -X github.com/tomwright/dasel/internal.Version=${version}
