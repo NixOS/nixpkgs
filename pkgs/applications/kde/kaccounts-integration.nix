0a922750bd77c648a803443ab281a7cd60ceac8a
Luke Granger-Brown <git@lukegb.com>
2021-01-03 20:22:15 +0000

    kdeApplications.kaccounts-integration: unbreak derivation

--- pkgs/applications/kde/kaccounts-integration.nix
+++ pkgs/applications/kde/kaccounts-integration.nix
@@ -1,7 +1,7 @@
 { mkDerivation, lib, extra-cmake-modules, kdoctools, kcmutils, kcoreaddons, kwallet, accounts-qt, signond }:
 
 mkDerivation {
-  name = "kaccounts-integration";
+  pname = "kaccounts-integration";
   meta = with lib; {
     homepage = "https://community.kde.org/KTp/Setting_up_KAccounts";
     description = "Online accounts integration";
