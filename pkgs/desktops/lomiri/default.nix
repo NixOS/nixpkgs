e6584ad7b1530dd3e12094b095603a78744ff666
OPNA2608 <christoph.neidahl@gmail.com>
2021-01-03 16:01:23 +0100

    libqtdbustest: init at unstable 2018-06-11

--- pkgs/desktops/lomiri/default.nix
+++ pkgs/desktops/lomiri/default.nix
@@ -4,5 +4,6 @@
 let
   packages = self: with self; {
     cmake-extras = callPackage ./cmake-extras { };
+    libqtdbustest = callPackage ./libqtdbustest { };
   };
 in makeScope libsForQt5.newScope packages
