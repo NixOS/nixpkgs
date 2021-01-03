96d50327bc590822210fae445fa6f3261a8f4642
Las <las@protonmail.ch>
2021-01-03 19:26:08 +0000

    libtiff: export private headers for freeimage
    They are exported under the dev_private output so
    that they aren't available automatically to dependening libraries.
    
    This also switches to cmake to simplify the patch.

--- pkgs/development/libraries/libtiff/default.nix
+++ pkgs/development/libraries/libtiff/default.nix
@@ -2,6 +2,7 @@
 , fetchurl
 
 , pkgconfig
+, cmake
 
 , zlib
 , libjpeg
@@ -17,15 +18,25 @@ stdenv.mkDerivation rec {
     sha256 = "0d46bdvxdiv59lxnb0xz9ywm8arsr6xsapi5s6y6vnys2wjz6aax";
   };
 
-  outputs = [ "bin" "dev" "out" "man" "doc" ];
+  # FreeImage needs this patch
+  patches = [ ./headers.patch ];
 
-  nativeBuildInputs = [ pkgconfig ];
+  outputs = [ "bin" "dev" "dev_private" "out" "man" "doc" ];
+
+  postFixup = ''
+    moveToOutput include/tif_dir.h $dev_private
+    moveToOutput include/tif_config.h $dev_private
+    moveToOutput include/tiffiop.h $dev_private
+  '';
+
+  nativeBuildInputs = [ cmake pkgconfig ];
 
   propagatedBuildInputs = [ zlib libjpeg xz ]; #TODO: opengl support (bogus configure detection)
 
   enableParallelBuilding = true;
 
-  doCheck = true; # not cross;
+  doInstallCheck = true;
+  installCheckTarget = "test";
 
   meta = with stdenv.lib; {
     description = "Library and utilities for working with the TIFF image file format";
