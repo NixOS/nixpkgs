ed92157b900b3aa4e0de49fafd235990064d3841
Justin Bedo <cu@cua0.org>
2021-01-05 10:18:55 +1100

    bedops: init 2.4.39

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26901,6 +26901,8 @@ in
 
   bayescan = callPackage ../applications/science/biology/bayescan { };
 
+  bedops = callPackage ../applications/science/biology/bedops { };
+
   bedtools = callPackage ../applications/science/biology/bedtools { };
 
   bcftools = callPackage ../applications/science/biology/bcftools { };
