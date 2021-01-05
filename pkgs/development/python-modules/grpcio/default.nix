2979502a46747f56fdffa3ab93ca443cd25ba082
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-01-05 13:33:26 +0100

    pythonPackages.grpcio: Disable tests, cleanup

--- pkgs/development/python-modules/grpcio/default.nix
+++ pkgs/development/python-modules/grpcio/default.nix
@@ -1,6 +1,18 @@
-{ stdenv, buildPythonPackage, darwin, grpc
-, six, protobuf, enum34, futures, isPy27, pkgconfig
-, cython, c-ares, openssl, zlib }:
+{ stdenv
+, buildPythonPackage
+, darwin
+, grpc
+, six
+, protobuf
+, enum34
+, futures
+, isPy27
+, pkg-config
+, cython
+, c-ares
+, openssl
+, zlib
+}:
 
 buildPythonPackage rec {
   inherit (grpc) src version;
@@ -8,12 +20,12 @@ buildPythonPackage rec {
 
   outputs = [ "out" "dev" ];
 
-  nativeBuildInputs = [ cython pkgconfig ]
-                    ++ stdenv.lib.optional stdenv.isDarwin darwin.cctools;
+  nativeBuildInputs = [ cython pkg-config ]
+    ++ stdenv.lib.optional stdenv.isDarwin darwin.cctools;
 
   buildInputs = [ c-ares openssl zlib ];
   propagatedBuildInputs = [ six protobuf ]
-                        ++ stdenv.lib.optionals (isPy27) [ enum34 futures ];
+    ++ stdenv.lib.optionals (isPy27) [ enum34 futures ];
 
   preBuild = stdenv.lib.optionalString stdenv.isDarwin "unset AR";
 
@@ -22,10 +34,15 @@ buildPythonPackage rec {
   GRPC_PYTHON_BUILD_SYSTEM_ZLIB = 1;
   GRPC_PYTHON_BUILD_SYSTEM_CARES = 1;
 
+  # does not contain any tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "grpc" ];
+
   meta = with stdenv.lib; {
     description = "HTTP/2-based RPC framework";
     license = licenses.asl20;
     homepage = "https://grpc.io/grpc/python/";
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ SuperSandro2000 ];
   };
 }
