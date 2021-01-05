a3a1091deefd749dcec184341a8129e644bb148c
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-01-05 15:44:21 +0100

    pythonPackages.google_cloud_resource_manager: Cleanups

--- pkgs/development/python-modules/google_cloud_resource_manager/default.nix
+++ pkgs/development/python-modules/google_cloud_resource_manager/default.nix
@@ -1,5 +1,11 @@
-{ stdenv, buildPythonPackage, fetchPypi, pytestCheckHook, pythonOlder
-, google_cloud_core, google_api_core, mock, pytest }:
+{ stdenv
+, buildPythonPackage
+, fetchPypi
+, pytestCheckHook
+, google_cloud_core
+, google_api_core
+, mock
+}:
 
 buildPythonPackage rec {
   pname = "google-cloud-resource-manager";
@@ -7,28 +13,24 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "3577bbf38f2c7c2f42306b8dfdeffbb0eedf45aaec947fd513d51937f72046d1";
+    sha256 = "1la643vkf6fm2gapz57cm92xzvmhzgpzv3bb6112yz1cizrvnxrm";
   };
 
-  disabled = pythonOlder "3.5";
-
-  checkInputs = [ mock pytestCheckHook ];
   propagatedBuildInputs = [ google_api_core google_cloud_core ];
 
-  # api_url test broken, fix not yet released
-  # https://github.com/googleapis/python-resource-manager/pull/31
-  disabledTests =
-    [ "api_url_no_extra_query_param" "api_url_w_custom_endpoint" ];
+  checkInputs = [ mock pytestCheckHook ];
 
   # prevent google directory from shadowing google imports
   preCheck = ''
     rm -r google
   '';
 
+  pythonImportsCheck = [ "google.cloud.resource_manager" ];
+
   meta = with stdenv.lib; {
     description = "Google Cloud Resource Manager API client library";
     homepage = "https://github.com/googleapis/python-resource-manager";
     license = licenses.asl20;
-    maintainers = [ maintainers.costrouc ];
+    maintainers = with maintainers; [ SuperSandro2000 ];
   };
 }
