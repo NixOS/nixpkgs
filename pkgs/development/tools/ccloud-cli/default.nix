9b05bad4ba234d7aa73aac84b3babd4e849d8fcd
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-01-04 20:24:33 -0800

    ccloud-cli: Declare the actual supported platforms
    Not all Linux and Darwin platforms are supported, only their x86_64
    variants are supported.

--- pkgs/development/tools/ccloud-cli/default.nix
+++ pkgs/development/tools/ccloud-cli/default.nix
@@ -28,6 +28,13 @@ stdenv.mkDerivation rec {
     homepage = "https://docs.confluent.io/current/cloud/cli/index.html";
     license = licenses.unfree;
     maintainers = with maintainers; [ kalbasit ];
-    platforms = platforms.linux ++ platforms.darwin;
+
+    # TODO: There's support for i686 systems but I do not have any such system
+    # to build it locally on, it's also unfree so I cannot rely on ofborg to
+    # build it. Get the list of supported system by looking at the list of
+    # files in the S3 bucket:
+    #
+    #   https://s3-us-west-2.amazonaws.com/confluent.cloud?prefix=ccloud-cli/archives/1.25.0/&delimiter=/%27
+    platforms = [ "x86_64-linux" "x86_64-darwin" ];
   };
 }
