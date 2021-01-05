11ef6fdd9c9e641b39e56bf13e6e644becc4fa26
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-01-05 16:30:32 +0100

    pythonPackages.google-auth-oauthlib: Cleanups

--- pkgs/development/python-modules/google-auth-oauthlib/default.nix
+++ pkgs/development/python-modules/google-auth-oauthlib/default.nix
@@ -1,12 +1,9 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, pythonOlder
-, isPy3k
 , click
 , mock
-, pytest
-, futures
+, pytestCheckHook
 , google_auth
 , requests_oauthlib
 }:
@@ -14,31 +11,27 @@
 buildPythonPackage rec {
   pname = "google-auth-oauthlib";
   version = "0.4.2";
-  disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "65b65bc39ad8cab15039b35e5898455d3d66296d0584d96fe0e79d67d04c51d9";
+    sha256 = "1nai9k86g7g7w1pxk105dllncgax8nc5hpmk758b3jnqkb1mpdk5";
   };
 
-  checkInputs = [
-    click mock pytest
-  ] ++ lib.optionals (!isPy3k) [ futures ];
-
   propagatedBuildInputs = [
-    google_auth requests_oauthlib
+    google_auth
+    requests_oauthlib
   ];
 
-  doCheck = isPy3k;
-  checkPhase = ''
-    rm -fr tests/__pycache__/ google
-    py.test
-  '';
+  checkInputs = [
+    click
+    mock
+    pytestCheckHook
+  ];
 
   meta = with lib; {
     description = "Google Authentication Library: oauthlib integration";
     homepage = "https://github.com/GoogleCloudPlatform/google-auth-library-python-oauthlib";
     license = licenses.asl20;
-    maintainers = with maintainers; [ terlar ];
+    maintainers = with maintainers; [ SuperSandro2000 terlar ];
   };
 }
