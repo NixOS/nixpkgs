0f52f3092889153a1aec19f650005359f2bba5fa
John Ericson <John.Ericson@Obsidian.Systems>
2021-01-05 13:40:34 -0500

    ott-mode: Init (version inherited from ott)
    This way it is pre-compiled.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21089,7 +21089,7 @@ in
       inherit
         autoconf automake editorconfig-core-c git libffi libpng pkgconfig
         poppler rtags w3m zlib substituteAll rustPlatform cmake llvmPackages
-        libtool zeromq openssl;
+        libtool zeromq openssl ott;
     };
   };
 
