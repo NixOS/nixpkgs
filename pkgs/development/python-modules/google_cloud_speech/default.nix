a40a35d8da599d367117f586e86f58e1dfa4c967
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-01-05 15:11:05 +0100

    pythonPackages.google_cloud_speech: Fix depedencies

--- pkgs/development/python-modules/google_cloud_speech/default.nix
+++ pkgs/development/python-modules/google_cloud_speech/default.nix
@@ -1,5 +1,13 @@
-{ stdenv, buildPythonPackage, fetchPypi
-, google_api_core, pytest, mock }:
+{ stdenv
+, buildPythonPackage
+, fetchPypi
+, google_api_core
+, libcst
+, mock
+, proto-plus
+, pytestCheckHook
+, pytest-asyncio
+}:
 
 buildPythonPackage rec {
   pname = "google-cloud-speech";
@@ -7,20 +15,29 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a7428190f4c10440148a273eb4c91480470b34180eec422b7325acdc0b2c0832";
+    sha256 = "0ch85h5xrb15fcml5v0f30s0niw02k4v8gi7i8a40161yj882hm7";
   };
 
-  propagatedBuildInputs = [ google_api_core ];
-  checkInputs = [ pytest mock ];
+  propagatedBuildInputs = [ libcst google_api_core proto-plus ];
 
-  checkPhase = ''
-    pytest tests/unit
-  '';
+  checkInputs = [ mock pytestCheckHook pytest-asyncio ];
+
+  pytestFlagsArray = [
+    # requrire credentials
+    "--ignore=tests/system/gapic/v1/test_system_speech_v1.py"
+    "--ignore=tests/system/gapic/v1p1beta1/test_system_speech_v1p1beta1.py"
+  ];
+
+  pythonImportsCheck = [
+    "google.cloud.speech"
+    "google.cloud.speech_v1"
+    "google.cloud.speech_v1p1beta1"
+  ];
 
   meta = with stdenv.lib; {
-    description = "Cloud Speech API enables integration of Google speech recognition into applications.";
-    homepage = "https://github.com/googleapis/google-cloud-python/tree/master/speech";
+    description = "Google Cloud Speech API client library";
+    homepage = "https://github.com/googleapis/python-speech";
     license = licenses.asl20;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ SuperSandro2000 ];
   };
 }
