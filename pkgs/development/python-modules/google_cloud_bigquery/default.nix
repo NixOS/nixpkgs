692345a57070f2a03b59ac66b62872717b3b2d99
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-01-06 01:43:03 +0100

    pythonPackages.google_cloud_bigquery: Cleanups

--- pkgs/development/python-modules/google_cloud_bigquery/default.nix
+++ pkgs/development/python-modules/google_cloud_bigquery/default.nix
@@ -1,6 +1,18 @@
-{ stdenv, buildPythonPackage, fetchPypi, pytestCheckHook, pythonOlder, freezegun
-, google_api_core, google_cloud_core, google_cloud_testutils
-, google_resumable_media, grpcio, ipython, mock, pandas, proto-plus, pyarrow }:
+{ stdenv
+, buildPythonPackage
+, fetchPypi
+, pytestCheckHook
+, freezegun
+, google_cloud_core
+, google_cloud_testutils
+, google_resumable_media
+, grpcio
+, ipython
+, mock
+, pandas
+, proto-plus
+, pyarrow
+}:
 
 buildPythonPackage rec {
   pname = "google-cloud-bigquery";
@@ -8,40 +20,39 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1f99fd0c0c5bde999e056a1be666e5d5bbf392f62c9e730dfcbaf6e8408d44ef";
+    sha256 = "1vs4im0fixmszh6p77icys9g7fymwmkfc6va0ng9kpjv1h6gv68z";
   };
 
-  disabled = pythonOlder "3.6";
-
-  checkInputs =
-    [ freezegun google_cloud_testutils ipython mock pytestCheckHook ];
   propagatedBuildInputs = [
     google_resumable_media
-    google_api_core
     google_cloud_core
-    pandas
     proto-plus
     pyarrow
   ];
 
+  checkInputs = [
+    freezegun
+    google_cloud_testutils
+    ipython
+    mock
+    pandas
+    pytestCheckHook
+  ];
+
   # prevent google directory from shadowing google imports
-  # test_magics requires modifying sys.path
   preCheck = ''
     rm -r google
-    rm tests/unit/test_magics.py
   '';
 
-  # call_api_applying_custom_retry_on_timeout requires credentials
-  # to_dataframe_timestamp_out_of_pyarrow_bounds has inconsistent results
-  disabledTests = [
-    "call_api_applying_custom_retry_on_timeout"
-    "to_dataframe_timestamp_out_of_pyarrow_bounds"
+  pythonImportsCheck = [
+    "google.cloud.bigquery"
+    "google.cloud.bigquery_v2"
   ];
 
   meta = with stdenv.lib; {
     description = "Google BigQuery API client library";
-    homepage = "https://pypi.org/project/google-cloud-bigquery";
+    homepage = "https://github.com/googleapis/python-bigquery";
     license = licenses.asl20;
-    maintainers = [ maintainers.costrouc ];
+    maintainers = with maintainers; [ SuperSandro2000 ];
   };
 }
