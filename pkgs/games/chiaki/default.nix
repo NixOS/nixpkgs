NixOS/nixpkgs

commit fe1151218cbb643b34fc938dfea66920dc141814

chiaki: cleanup package

pkgs/games/chiaki/default.nix
@@ -1,9 +1,21 @@
-{ lib, mkDerivation, fetchgit
-, cmake, ffmpeg, libevdev, libopus, udev, qtbase, qtmacextras, qtmultimedia
-, qtsvg , pkg-config, protobuf , python3Packages, SDL2, stdenv }:
+{ stdenv
+, fetchgit
+, cmake
+, pkg-config
+, protobuf
+, python3Packages
+, ffmpeg
+, libopus
+, qtbase
+, qtmultimedia
+, qtsvg
+, SDL2
+, libevdev
+, udev
+, qtmacextras
+}:
 
-with stdenv.lib;
-mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "chiaki";
   version = "2.0.1";
 
@@ -15,18 +27,35 @@ mkDerivation rec {
   };
 
   nativeBuildInputs = [
-    cmake pkg-config protobuf python3Packages.python python3Packages.protobuf
+    cmake
+    pkg-config
+    protobuf
+    python3Packages.protobuf
+    python3Packages.python
   ];
-  buildInputs = [ ffmpeg libopus qtbase qtmultimedia qtsvg protobuf SDL2 ]
-    ++ optionals stdenv.hostPlatform.isLinux [ libevdev udev]
-    ++ optionals (stdenv.isDarwin) [ qtmacextras ];
+
+  buildInputs = [
+    ffmpeg
+    libopus
+    qtbase
+    qtmultimedia
+    qtsvg
+    protobuf
+    SDL2
+  ] ++ stdenv.lib.optionals stdenv.isLinux [
+    libevdev
+    udev
+  ] ++ stdenv.lib.optionals stdenv.isDarwin [
+    qtmacextras
+  ];
 
   doCheck = true;
+
   installCheckPhase = "$out/bin/chiaki --help";
 
-  meta = with lib; {
-    homepage = "https://github.com/thestr4ng3r/chiaki";
-    description = "Free and Open Source PS4 Remote Play Client";
+  meta = with stdenv.lib; {
+    homepage = "https://git.sr.ht/~thestr4ng3r/chiaki";
+    description = "Free and Open Source PlayStation Remote Play Client";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ delroth ];
     platforms = platforms.all;

commit ca0e2aced040782fb7c320a4fcabfa2d7556b963

chiaki: use mkDerivation for QT5 application
Commit fe1151218cbb ("chiaki: cleanup package") replaced mkDerivation
with stdenv mkDerivation, which is wrong for QT5 applications and
results in the following error on start:

  qt.qpa.plugin: Could not find the Qt platform plugin "wayland" in ""
  qt.qpa.plugin: Could not find the Qt platform plugin "xcb" in ""
  This application failed to start because no Qt platform plugin could be initialized. Reinstalling the application may fix this problem.

also see [1] in the manual. Fix the package by using mkDerivation again.

[1]: https://nixos.org/manual/nixpkgs/unstable/#sec-language-qt

Fixes: fe1151218cbb ("chiaki: cleanup package")

pkgs/games/chiaki/default.nix
@@ -6,6 +6,7 @@
 , python3Packages
 , ffmpeg
 , libopus
+, mkDerivation
 , qtbase
 , qtmultimedia
 , qtsvg
@@ -15,7 +16,7 @@
 , qtmacextras
 }:
 
-stdenv.mkDerivation rec {
+mkDerivation rec {
   pname = "chiaki";
   version = "2.0.1";
 