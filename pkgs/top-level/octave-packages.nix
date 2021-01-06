545804e2e0d928f818be8c0f25333ded7e9af594
Karl Hallsby <karl@hallsby.com>
2021-01-06 12:06:36 -0600

    octave.pkgs.splines: init at 1.3.3

--- pkgs/top-level/octave-packages.nix
+++ pkgs/top-level/octave-packages.nix
@@ -187,6 +187,8 @@ makeScope newScope (self:
 
     stk = callPackage ../development/octave-modules/stk { };
 
+    splines = callPackage ../development/octave-modules/splines { };
+
     struct = callPackage ../development/octave-modules/struct { };
 
     symbolic = callPackage ../development/octave-modules/symbolic {
