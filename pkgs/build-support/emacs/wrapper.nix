NixOS/nixpkgs

commit dd5d2482c9dcdf5e35bf271b32303a515f923607

emacs: Fix accidental double wrapping
This makeWrapper invocation found it's way back via a rebase.

pkgs/build-support/emacs/wrapper.nix
@@ -185,11 +185,6 @@ runCommand
         --subst-var-by wrapperSiteLispNative "$deps/share/emacs/native-lisp:" \
         --subst-var prog
       chmod +x $out/bin/$progname
-
-      makeWrapper "$prog" "$out/bin/$progname" \
-        --suffix EMACSLOADPATH ":" "$deps/share/emacs/site-lisp:" \
-        --suffix EMACSNATIVELOADPATH ":" "$deps/share/emacs/native-lisp:"
-
     done
 
     # Wrap MacOS app

commit 9bd25333c9525247197ad0f64a8014fff17a1fe4

emacs: fix accidental double wrapper (Darwin)
This was already fixed on non-Darwin, but the fix missed that it was
also reintroduced for the Darwin code path at the same time.

Fixes: dd5d2482c9d ("emacs: Fix accidental double wrapping")

pkgs/build-support/emacs/wrapper.nix
@@ -203,10 +203,6 @@ runCommand
         --subst-var-by wrapperSiteLisp "$deps/share/emacs/site-lisp" \
         --subst-var-by prog "$emacs/Applications/Emacs.app/Contents/MacOS/Emacs"
       chmod +x $out/Applications/Emacs.app/Contents/MacOS/Emacs
-
-      makeWrapper $emacs/Applications/Emacs.app/Contents/MacOS/Emacs $out/Applications/Emacs.app/Contents/MacOS/Emacs \
-        --suffix EMACSLOADPATH ":" "$deps/share/emacs/site-lisp:" \
-        --suffix EMACSNATIVELOADPATH ":" "$deps/share/emacs/native-lisp:"
     fi
 
     mkdir -p $out/share