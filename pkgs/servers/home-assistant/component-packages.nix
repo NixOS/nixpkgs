37730adea7957b0bb26c1af7c3db7cbfa35c7dbb
Fabian Affolter <mail@fabian-affolter.ch>
2021-01-07 15:56:42 +0100

    home-assistant: update component-packages

--- pkgs/servers/home-assistant/component-packages.nix
+++ pkgs/servers/home-assistant/component-packages.nix
@@ -399,7 +399,7 @@
     "iperf3" = ps: with ps; [ ]; # missing inputs: iperf3
     "ipma" = ps: with ps; [ ]; # missing inputs: pyipma
     "ipp" = ps: with ps; [ pyipp ];
-    "iqvia" = ps: with ps; [ numpy ]; # missing inputs: pyiqvia
+    "iqvia" = ps: with ps; [ numpy pyiqvia ];
     "irish_rail_transport" = ps: with ps; [ ]; # missing inputs: pyirishrail
     "islamic_prayer_times" = ps: with ps; [ ]; # missing inputs: prayer_times_calculator
     "iss" = ps: with ps; [ ]; # missing inputs: pyiss
