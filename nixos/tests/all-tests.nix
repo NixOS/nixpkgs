41c50cc5052e80a7132880b28ff38c52b611c974
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-01-08 13:48:00 -0300

    nixos/opentabletdriver: add tests

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -281,6 +281,7 @@ in
   openssh = handleTest ./openssh.nix {};
   openstack-image-metadata = (handleTestOn ["x86_64-linux"] ./openstack-image.nix {}).metadata or {};
   openstack-image-userdata = (handleTestOn ["x86_64-linux"] ./openstack-image.nix {}).userdata or {};
+  opentabletdriver = handleTest ./opentabletdriver.nix {};
   image-contents = handleTest ./image-contents.nix {};
   orangefs = handleTest ./orangefs.nix {};
   os-prober = handleTestOn ["x86_64-linux"] ./os-prober.nix {};
