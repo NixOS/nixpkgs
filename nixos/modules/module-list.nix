30b4f71c1511978a14ef759e803e578eb30d9222
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2021-01-08 15:57:48 -0500

    nixos/auto-cpufreq: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -356,6 +356,7 @@
   ./services/games/terraria.nix
   ./services/hardware/acpid.nix
   ./services/hardware/actkbd.nix
+  ./services/hardware/auto-cpufreq.nix
   ./services/hardware/bluetooth.nix
   ./services/hardware/bolt.nix
   ./services/hardware/brltty.nix
