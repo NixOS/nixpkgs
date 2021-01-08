2f640f61988a38f885718ac11def87091c618ada
Mario Rodas <marsam@users.noreply.github.com>
2021-01-08 04:20:00 +0000

    pythonPackages.apprise: 0.8.9 -> 0.9.0
    https://github.com/caronc/apprise/releases/tag/v0.9.0

--- pkgs/development/python-modules/apprise/default.nix
+++ pkgs/development/python-modules/apprise/default.nix
@@ -1,15 +1,15 @@
 { lib, buildPythonPackage, fetchPypi
 , Babel, requests, requests_oauthlib, six, click, markdown, pyyaml
-, pytestrunner, coverage, flake8, mock, pytest, pytestcov, tox, gntp, sleekxmpp
+, pytestrunner, coverage, flake8, mock, pytestCheckHook, pytestcov, tox, gntp, sleekxmpp
 }:
 
 buildPythonPackage rec {
   pname = "apprise";
-  version = "0.8.9";
+  version = "0.9.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "024db00c6a80dbc8c9038b2de211c9fd32963046612882f3f54ad78930f3e0f7";
+    sha256 = "bab3563bc1e0c64938c4c7700112797bd99f20eb5d4a3e6038338bc8f060e153";
   };
 
   nativeBuildInputs = [ Babel ];
@@ -19,8 +19,10 @@ buildPythonPackage rec {
   ];
 
   checkInputs = [
-    pytestrunner coverage flake8 mock pytest pytestcov tox gntp sleekxmpp
+    pytestrunner coverage flake8 mock pytestCheckHook pytestcov tox gntp sleekxmpp
   ];
+
+  disabledTests = [ "test_apprise_cli_nux_env"  ];
 
   meta = with lib; {
     homepage = "https://github.com/caronc/apprise";
