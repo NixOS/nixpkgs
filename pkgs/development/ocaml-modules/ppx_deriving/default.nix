0fcbd37ab12003c862a3a5900c70f565590e2fb1
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-01-09 20:41:33 +0100

    ocamlPackages.ppx_deriving: use version 5.0 for OCaml 4.04

--- pkgs/development/ocaml-modules/ppx_deriving/default.nix
+++ pkgs/development/ocaml-modules/ppx_deriving/default.nix
@@ -1,19 +1,31 @@
-{ lib, fetchzip, buildDunePackage
+{ lib, fetchzip, buildDunePackage, ocaml
 , cppo, ppx_derivers, ppxlib, ocaml-migrate-parsetree, result
 , ounit
 }:
 
+let
+  param =
+    if lib.versionAtLeast ocaml.version "4.05"
+    then {
+      version = "5.1";
+      sha256 = "0as0hy606vsbc4bf72vh2v23vdfaqlay22mlw6wbkr5b2l6w5b4w";
+    } else {
+      version = "5.0";
+      sha256 = "0ihw9d75ic82knv1cvc38fr0wki7vrkavaja362ra9cqbwr1y0al";
+    };
+in
+
 buildDunePackage rec {
   pname = "ppx_deriving";
-  version = "5.1";
+  inherit (param) version;
 
   useDune2 = true;
 
-  minimumOCamlVersion = "4.05";
+  minimumOCamlVersion = "4.04";
 
   src = fetchzip {
-    url = "https://github.com/ocaml-ppx/ppx_deriving/archive/v${version}.tar.gz";
-    sha256 = "0as0hy606vsbc4bf72vh2v23vdfaqlay22mlw6wbkr5b2l6w5b4w";
+    url = "https://github.com/ocaml-ppx/ppx_deriving/archive/v${param.version}.tar.gz";
+    inherit (param) sha256;
   };
 
   buildInputs = [ cppo ];
