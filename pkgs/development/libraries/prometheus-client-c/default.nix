335fedb11b68567fdb0dac731fdb5c2f758db6c0
Carlos Fernandez Sanz <carlos@standard.ai>
2021-01-08 16:22:29 -0800

    libpromhttp: 0.1.1 -> 0.1.3
    Minor update, but they merged the patches we were including manually.
    They also fixed a floating point bug on histograms.

--- pkgs/development/libraries/prometheus-client-c/default.nix
+++ pkgs/development/libraries/prometheus-client-c/default.nix
@@ -13,35 +13,17 @@ let
     }:
     stdenv.mkDerivation rec {
       inherit pname;
-      version = "0.1.1";
+      version = "0.1.3";
 
       src = fetchFromGitHub {
         owner = "digitalocean";
         repo = "prometheus-client-c";
         rev = "v${version}";
-        sha256 = "0g69s24xwrv5974acshrhnp6i8rpby8c6bhz15m3d8kpgjw3cm8f";
+        sha256 = "0vsvng0nvvflcgick0bpyf8pynf0rzl2yk4mrydzbdkc6646s5sf";
       };
 
       nativeBuildInputs = [ cmake ];
       inherit buildInputs;
-
-      # These patches will be in 0.1.2
-      patches = [
-        # Required so CMAKE_INSTALL_PREFIX is honored, otherwise it
-        # installs headers in /usr/include (absolute)
-        (
-          fetchpatch {
-            url = "https://github.com/digitalocean/prometheus-client-c/commit/5fcedeb506b7d47dd7bab35797f2c3f23db6fe10.patch";
-            sha256 = "10hzg8v5jcgxz224kdq0nha9vs78wz098b0ys7gig2iwgrg018fy";
-          }
-        )
-        (
-          fetchpatch {
-            url = "https://github.com/digitalocean/prometheus-client-c/commit/0c15e7e45ad0c3726593591fdd7d8f2fde845fe3.patch";
-            sha256 = "06899v1xz3lpsdxww4p3q7pv8nrymnibncdc472056znr5fidlp0";
-          }
-        )
-      ];
 
       preConfigure = ''
         cd ${subdir}
