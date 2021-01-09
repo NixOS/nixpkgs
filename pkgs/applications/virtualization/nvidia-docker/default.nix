e67c679649a2f03baf161c3c89a01611045546ed
Phillip Cloud <cloud@standard.ai>
2021-01-09 09:40:45 -0500

    nvidia-docker: split out nvidia-container-toolkit into separate drv

--- pkgs/applications/virtualization/nvidia-docker/default.nix
+++ pkgs/applications/virtualization/nvidia-docker/default.nix
@@ -12,23 +12,6 @@
 }:
 
 with lib; let
-  nvidia-container-toolkit = buildGoModule rec {
-    pname = "nvidia-container-toolkit";
-    version = "1.3.0";
-    src = fetchFromGitHub {
-      owner = "NVIDIA";
-      repo = "nvidia-container-toolkit";
-      rev = "v${version}";
-      sha256 = "04284bhgx4j55vg9ifvbji2bvmfjfy3h1lq7q356ffgw3yr9n0hn";
-    };
-    vendorSha256 = "17zpiyvf22skfcisflsp6pn56y6a793jcx89kw976fq2x5br1bz7";
-    buildFlagsArray = [ "-ldflags=" "-s -w" ];
-    postInstall = ''
-      mv $out/bin/{pkg,${pname}}
-      cp $out/bin/{${pname},nvidia-container-runtime-hook}
-    '';
-  };
-
 in
 stdenv.mkDerivation rec {
   pname = "nvidia-docker";
@@ -46,8 +29,6 @@ stdenv.mkDerivation rec {
 
     cp nvidia-docker bin
     substituteInPlace bin/nvidia-docker --subst-var-by VERSION ${version}
-
-    cp ${nvidia-container-toolkit}/bin/nvidia-container-{toolkit,runtime-hook} bin
   '';
 
   installPhase = ''
