dcb5ce6317c3447141c0b0e3cfca9c3fab024259
Dominic Rubas <rubas@users.noreply.github.com>
2021-01-09 15:44:38 +0100

    yabai: 3.3.4 -> 3.3.6
    Update is needed for MacOS 11.1
    
    https://github.com/koekeishiya/yabai/blob/master/CHANGELOG.md

--- pkgs/os-specific/darwin/yabai/default.nix
+++ pkgs/os-specific/darwin/yabai/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "yabai";
-  version = "3.3.4";
+  version = "3.3.6";
 
   src = fetchFromGitHub {
     owner = "koekeishiya";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1pvyjdxgy7yxxz4x87f8an0dlxvxbnmv5kya8hkzw2na453ihvab";
+    sha256 = "0319k35c2rm0hsf0s5qdx4510g2n3nzg42cw1mhxcqrpi63604gg";
   };
 
   buildInputs = [ Carbon Cocoa ScriptingBridge xxd ];
