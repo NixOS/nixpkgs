f91f9eacd676ac5b54e8e2858bcbc0e3c39be9f4
Phillip Cloud <cloud@standard.ai>
2021-01-10 07:58:41 -0500

    nvidia-container-toolkit: format

--- pkgs/applications/virtualization/nvidia-container-toolkit/default.nix
+++ pkgs/applications/virtualization/nvidia-container-toolkit/default.nix
@@ -14,15 +14,18 @@ in
 buildGoModule rec {
   pname = "nvidia-container-toolkit";
   version = "1.3.0";
+
   src = fetchFromGitHub {
     owner = "NVIDIA";
     repo = pname;
     rev = "v${version}";
     sha256 = "04284bhgx4j55vg9ifvbji2bvmfjfy3h1lq7q356ffgw3yr9n0hn";
   };
+
   vendorSha256 = "17zpiyvf22skfcisflsp6pn56y6a793jcx89kw976fq2x5br1bz7";
   buildFlagsArray = [ "-ldflags=" "-s -w" ];
   nativeBuildInputs = [ makeWrapper ];
+
   postInstall = ''
     mv $out/bin/{pkg,${pname}}
     ln -s $out/bin/nvidia-container-{toolkit,runtime-hook}
