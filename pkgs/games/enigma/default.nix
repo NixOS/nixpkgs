725051e81cdd650329e948f481b93278d3ef0670
Ingo Blechschmidt <iblech@web.de>
2021-01-10 13:45:48 +0100

    enigma: 1.21 -> 1.30-alpha

--- pkgs/games/enigma/default.nix
+++ pkgs/games/enigma/default.nix
@@ -1,30 +1,31 @@
-{ stdenv, fetchurl, makeWrapper, pkg-config, curl, imagemagick, libpng, SDL, SDL_image, SDL_mixer, SDL_ttf, xercesc, xdg_utils, hicolor-icon-theme }:
+{ stdenv, lib, fetchurl, makeWrapper, pkg-config, gettext, imagemagick, curl, libpng, SDL2, SDL2_image, SDL2_mixer, SDL2_ttf, xercesc, xdg_utils, hicolor-icon-theme }:
 stdenv.mkDerivation rec {
   pname = "enigma";
-  version = "1.21";
+  version = "1.30-alpha";
 
   src = fetchurl {
-    url = "mirror://sourceforge/enigma-game/enigma-${version}.tar.gz";
-    sha256 = "d872cf067d8eb560d3bb1cb17245814bc56ac3953ae1f12e2229c8eb6f82ce01";
+    url = "https://github.com/Enigma-Game/Enigma/releases/download/${version}/${pname}-${version}.tar.gz";
+    sha256 = "1zyk3j43gzfr1lhc6g13j7qai5f33fv5xm5735nnznaqvaz17949";
   };
 
-  # patches lifted from Arch
-  patches = [ ./src_client.cc.patch ./src_lev_Proxy.cc.patch ./src_Value.cc.patch ];
+  nativeBuildInputs = [ pkg-config gettext makeWrapper imagemagick ];
+  buildInputs = [ SDL2 SDL2_image SDL2_mixer SDL2_ttf libpng xercesc curl xdg_utils ];
 
-  NIX_CFLAGS_COMPILE = [ "-Wno-deprecated-declarations" ];
-
-  nativeBuildInputs = [ pkg-config imagemagick makeWrapper ];
-  buildInputs = [ SDL SDL_image SDL_mixer SDL_ttf libpng xercesc curl xdg_utils ];
+  # For some reason (might be related to the alpha status), some includes
+  # which are required by lib-src/enigma-core are not picked up by the
+  # configure script. Hence we add them manually.
+  CPPFLAGS = "-I${SDL2.dev}/include/SDL2 -I${SDL2_ttf}/include/SDL2 -I${SDL2_image}/include/SDL2 -I${SDL2_mixer}/include/SDL2";
 
   postInstall = ''
     rm -r $out/include
     wrapProgram $out/bin/enigma --prefix PATH : "${stdenv.lib.makeBinPath [ xdg_utils ]}"
   '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Puzzle game inspired by Oxyd on the Atari ST and Rock'n'Roll on the Amiga";
-    license = with licenses; [ gpl2 free ];
+    license = with licenses; [ gpl2 free ];  # source + bundles libs + art
     platforms = platforms.unix;
+    broken = stdenv.targetPlatform.isDarwin;
     maintainers = with maintainers; [ iblech ];
     homepage = "https://www.nongnu.org/enigma/";
   };
