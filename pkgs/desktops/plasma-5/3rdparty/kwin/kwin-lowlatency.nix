c5bed7468f690cbde966659683a85ebb352596d6
InternetUnexplorer <internetunexplorer@gmail.com>
2021-01-10 15:46:29 -0800

    kwin-lowlatency: rename o to oldAttrs

--- pkgs/desktops/plasma-5/3rdparty/kwin/kwin-lowlatency.nix
+++ pkgs/desktops/plasma-5/3rdparty/kwin/kwin-lowlatency.nix
@@ -3,10 +3,10 @@
 let
   pname = "kwin-lowlatency";
   version = "5.20.5";
-in kwin.overrideAttrs (o: {
+in kwin.overrideAttrs (oldAttrs: {
   name = "${pname}-${version}";
 
-  patches = (o.patches or [ ]) ++ [
+  patches = (oldAttrs.patches or [ ]) ++ [
     (fetchpatch {
       url = "https://tildearrow.org/storage/kwin-lowlatency/kwin-lowlatency-${version}.patch";
       sha256 = "14lzv38kkypqjwgdcd8f7lm7366y7cj08l3lh8qq1b49pza0zb2w";
