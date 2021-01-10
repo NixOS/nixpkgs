629b34fe2ce632600962ade013f84c96c2d79adc
Mario Rodas <marsam@users.noreply.github.com>
2021-01-10 11:53:00 -0500

    tflint: 0.23.0 -> 0.23.1
    https://github.com/terraform-linters/tflint/releases/tag/v0.23.1

--- pkgs/development/tools/analysis/tflint/default.nix
+++ pkgs/development/tools/analysis/tflint/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "tflint";
-  version = "0.23.0";
+  version = "0.23.1";
 
   src = fetchFromGitHub {
     owner = "terraform-linters";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1j6a956cbmsc9fy500sl5z6l7q5sc4fna772v5m10w0hq0vw6sk5";
+    sha256 = "19vgan743xsnglwva93fs4sqcbpqbyrxbgn5qff809ns8hj8ir94";
   };
 
-  vendorSha256 = "1x7gxjl98i36vsch6b3w6iqyq6q8mj4x9gylk2fihq50c3qq4mk4";
+  vendorSha256 = "1y6ra9bkp6wl0pikvyqywhg5w18v3glnyvwqrddkkkabspl05nqx";
 
   doCheck = false;
 
