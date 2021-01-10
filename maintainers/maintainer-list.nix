6ae087f4b29c6d79b2c517e0143cb8053922d30f
Terin Stock <terinjokes@gmail.com>
2021-01-10 01:13:30 -0800

    maintainers: add terin

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8879,6 +8879,12 @@
     githubId = 649832;
     name = "Teo Klestrup Röijezon";
   };
+  terin = {
+    email = "terinjokes@gmail.com";
+    github = "terinjokes";
+    githubId = 273509;
+    name = "Terin Stock";
+  };
   terlar = {
     email = "terlar@gmail.com";
     github = "terlar";
