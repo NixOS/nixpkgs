a29eeb386efa82e51bf7af37ac4446932fc68083
Felix Singer <felixsinger@posteo.net>
2021-01-11 01:42:20 +0100

    maintainers: Add Felix Singer
    Signed-off-by: Felix Singer <felixsinger@posteo.net>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3043,6 +3043,12 @@
     githubId = 9959940;
     name = "Andreas Fehn";
   };
+  felixsinger = {
+    email = "felixsinger@posteo.net";
+    github = "felixsinger";
+    githubId = 628359;
+    name = "Felix Singer";
+  };
   felschr = {
     email = "dev@felschr.com";
     github = "felschr";
