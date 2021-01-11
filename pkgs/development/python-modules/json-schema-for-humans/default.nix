e917e5d15cb61b525d28616b52ac4e070f738317
Astro <astro@spaceboyz.net>
2021-01-11 22:45:53 +0100

    python3Packages.json-schema-for-humans: 0.27.0 -> 0.27.1

--- pkgs/development/python-modules/json-schema-for-humans/default.nix
+++ pkgs/development/python-modules/json-schema-for-humans/default.nix
@@ -1,25 +1,17 @@
-{ stdenv, buildPythonPackage, fetchFromGitHub, fetchurl
+{ stdenv, buildPythonPackage, fetchFromGitHub
 , pbr, click, dataclasses-json, htmlmin, jinja2, markdown2, pygments, pytz, pyyaml, requests, pytestCheckHook, beautifulsoup4, tox
 }:
 
 buildPythonPackage rec {
   pname = "json-schema-for-humans";
-  version = "0.27.0";
+  version = "0.27.1";
 
   src = fetchFromGitHub {
     owner = "coveooss";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1r40i192z6aasil5vsgcgp5yvx392dhhqnfc2qxbxvpja6l3p6p2";
+    sha256 = "0d2a4a2lcqssr5g9rmc76f86nkqc9grixh507vzc9fi1h3gbi765";
   };
-
-  patches = [ (fetchurl {
-    url = "https://github.com/coveooss/json-schema-for-humans/commit/1fe2e2391da5a796204fd1889e4a11a53f83f7c9.patch";
-    sha256 = "0kpydpddlg0rib9snl8albhbrrs6d3ds292gpgpg7bdpqrwamdib";
-  }) (fetchurl {
-    url = "https://github.com/astro/json-schema-for-humans/commit/9bcc9b461102062dff214ca1ec2375b8aea53711.patch";
-    sha256 = "142a07v8bn1j20b7177yb60f4944kbx4cdqqq2nz6xkxmamw704d";
-  }) ];
 
   nativeBuildInputs = [ pbr ];
   propagatedBuildInputs = [
