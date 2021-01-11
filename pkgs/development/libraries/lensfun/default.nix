124bb278229f7d1997a24e15fb2dd977b21e6cc1
Florian Klink <flokli@flokli.de>
2021-01-11 01:14:05 +0100

    lensfun: update website
    Project has moved to GitHub.

--- pkgs/development/libraries/lensfun/default.nix
+++ pkgs/development/libraries/lensfun/default.nix
@@ -19,6 +19,6 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ ];
     license = stdenv.lib.licenses.lgpl3;
     description = "An opensource database of photographic lenses and their characteristics";
-    homepage = "http://lensfun.sourceforge.net/";
+    homepage = "https://lensfun.github.io";
   };
 }
