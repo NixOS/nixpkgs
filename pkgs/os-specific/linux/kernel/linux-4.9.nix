a1f3d24a96d55b6659f05ce151fa745e901a8d09
kraem <ebrin.ronnie@protonmail.com>
2021-01-12 22:27:01 +0100

    linux: 4.9.250 -> 4.9.251

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.250";
+  version = "4.9.251";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "15vizxd2i2311skjank406ny3bc30c5rz2p9jvh5xz1yv12vzgcy";
+    sha256 = "03cn3yzyv8vwvv76nxj655i14s9avhr4hcc18mq2rh0qn6zcnkgg";
   };
 } // (args.argsOverride or {}))
