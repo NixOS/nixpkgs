2883e0bd1009937bcc14249a4882e465896c2a88
Fabian Affolter <mail@fabian-affolter.ch>
2021-01-12 18:59:11 +0100

    python3Packages.aioimaplib: update check part

--- pkgs/development/python-modules/aioimaplib/default.nix
+++ pkgs/development/python-modules/aioimaplib/default.nix
@@ -1,11 +1,22 @@
-{ lib, buildPythonPackage, fetchFromGitHub, isPy3k, pythonOlder, isPy36
-, nose, asynctest, mock, pytz, tzlocal, imaplib2, docutils, pyopenssl }:
+{ lib
+, asynctest
+, buildPythonPackage
+, docutils
+, fetchFromGitHub
+, imaplib2
+, mock
+, nose
+, pyopenssl
+, pytestCheckHook
+, pythonOlder
+, pytz
+, tzlocal
+}:
 
 buildPythonPackage rec {
   pname = "aioimaplib";
   version = "0.7.18";
 
-  # PyPI tarball doesn't ship tests
   src = fetchFromGitHub {
     owner = "bamthomas";
     repo = pname;
@@ -13,12 +24,22 @@ buildPythonPackage rec {
     sha256 = "037fxwmkdfb95cqcykrhn37p138wg9pvlsgdf45vyn1mhz5crky5";
   };
 
-  disabled = !(isPy3k && pythonOlder "3.7");
+  checkInputs = [
+    asynctest
+    docutils
+    imaplib2
+    mock
+    nose
+    pyopenssl
+    pytestCheckHook
+    pytz
+    tzlocal
+  ];
 
-  checkInputs = [ nose asynctest mock pytz tzlocal imaplib2 docutils pyopenssl ];
-
-  # https://github.com/bamthomas/aioimaplib/issues/35
-  doCheck = !isPy36;
+  # Project is using asynctest with doesn't work with Python 3.8 and above
+  # https://github.com/bamthomas/aioimaplib/issues/54
+  doCheck = pythonOlder "3.8";
+  pythonImportsCheck = [ "aioimaplib" ];
 
   meta = with lib; {
     description = "Python asyncio IMAP4rev1 client library";
