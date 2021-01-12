2ae40dd9e8c87c9aee970e1dda0d8fb52b351fad
Justin Humm <justin.humm@posteo.de>
2021-01-12 06:54:45 +0100

    openjpeg: use stable patch URLs

--- pkgs/development/libraries/openjpeg/2.x.nix
+++ pkgs/development/libraries/openjpeg/2.x.nix
@@ -13,9 +13,16 @@ callPackage ./generic.nix (args // rec {
 
   patches = [
     ./fix-cmake-config-includedir.patch
+
+    # Fix finding openjpeg.h: https://github.com/uclouvain/openjpeg/pull/1321
     (fetchpatch {
-      url = "https://patch-diff.githubusercontent.com/raw/uclouvain/openjpeg/pull/1321.patch";
-      sha256 = "1cjpr76nf9g65nqkfnxnjzi3bv7ifbxpc74kxxibh58pzjlp6al8";
+      url = "https://github.com/uclouvain/openjpeg/pull/1321/commits/14f4c27e7c91f745a1dda9991b5deea3cbef2072.patch";
+      sha256 = "sha256-lVnzMAy6pSKFnz9eEneDld6zpzfsAmGurPOjTU5Nqnw=";
     })
+    (fetchpatch {
+      url = "https://github.com/uclouvain/openjpeg/pull/1321/commits/4d0b49edad7fb31ebbf03c60a45b72aaa7b7412b.patch";
+      sha256 = "sha256-zU7t6SjmGjt35w875GSQnoTDl8V8yzG1LK2QY0jEnZM=";
+    })
+
   ];
 })
