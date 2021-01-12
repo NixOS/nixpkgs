740630e480198c4313a266015dc5e3a9ed72c3b3
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-01-12 11:24:44 +0100

    pythonPackages.check-manifest: Fix dependencies

--- pkgs/development/python-modules/check-manifest/default.nix
+++ pkgs/development/python-modules/check-manifest/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, buildPythonPackage, fetchPypi, pep517, toml, mock, breezy, git }:
+{ lib, stdenv, buildPythonPackage, fetchPypi, pep517, toml, mock, breezy, git, build }:
 
 buildPythonPackage rec {
   pname = "check-manifest";
@@ -14,7 +14,7 @@ buildPythonPackage rec {
     substituteInPlace tests.py --replace "test_build_sdist" "no_test_build_sdist"
   '';
 
-  propagatedBuildInputs = [ pep517 toml ];
+  propagatedBuildInputs = [ build pep517 toml ];
 
   checkInputs = [ mock breezy git ];
 
