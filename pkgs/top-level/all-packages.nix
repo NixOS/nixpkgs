27a490d4178af4da4cdd99a6a6b50e9360fe703e
Alyssa Ross <hi@alyssa.is>
2021-01-12 14:22:30 +0000

    firefox: remove unused override
    This isn't used:
    
        $ git grep -w python pkgs/applications/networking/browsers/firefox
        $

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21890,7 +21890,6 @@ in
     callPackage = pkgs.newScope {
       inherit (rustPackages) cargo rustc;
       libpng = libpng_apng;
-      python = python2;
       gnused = gnused_422;
       inherit (darwin.apple_sdk.frameworks) CoreMedia ExceptionHandling
                                             Kerberos AVFoundation MediaToolbox
