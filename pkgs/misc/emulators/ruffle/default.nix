b249fd1b46e48dc9a4b12e37073d949f5507ad77
Eduardo Sánchez Muñoz <esm@eduardosm.net>
2021-01-13 11:19:36 +0100

    ruffle: nightly-2020-11-30 -> nightly-2021-01-12

--- pkgs/misc/emulators/ruffle/default.nix
+++ pkgs/misc/emulators/ruffle/default.nix
@@ -1,5 +1,6 @@
 { alsaLib
 , fetchFromGitHub
+, makeWrapper
 , openssl
 , pkg-config
 , python3
@@ -7,20 +8,22 @@
 , lib, stdenv
 , wayland
 , xorg
+, vulkan-loader
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "ruffle";
-  version = "nightly-2020-11-30";
+  version = "nightly-2021-01-12";
 
   src = fetchFromGitHub {
     owner = "ruffle-rs";
     repo = pname;
     rev = version;
-    sha256 = "0z54swzy47laq3smficd3dyrs2zdi3cj2kb0b4hppjxpkkhiw4x0";
+    sha256 = "1lywxn61w0b3pb8vjpavd9f3v58gq35ypwp41b7rjkc4rjxmf3cd";
   };
 
   nativeBuildInputs = [
+    makeWrapper
     pkg-config
     python3
   ];
@@ -35,9 +38,14 @@ rustPlatform.buildRustPackage rec {
     xorg.libXi
     xorg.libxcb
     xorg.libXrender
+    vulkan-loader
   ];
 
-  cargoSha256 = "05kwfcbzjyyfhiqklhhlv06pinzw9bry4j8l9lk3k04c1q30gzkw";
+  postInstall = ''
+    wrapProgram $out/bin/ruffle_desktop --prefix LD_LIBRARY_PATH ':' ${vulkan-loader}/lib
+  '';
+
+  cargoSha256 = "113gh8nf2fs9shfvnzpwlc7zaq1l9l9jhlybcc4dq0wr4r8qpff5";
 
   meta = with lib; {
     description = "An Adobe Flash Player emulator written in the Rust programming language.";
