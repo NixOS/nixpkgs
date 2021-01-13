545ae40ba357cfe674eb8f38b2a0ec5b3b0b608e
ajs124 <git@ajs124.de>
2021-01-13 01:48:06 +0100

    vorbis-tools: 1.4.0 -> 1.4.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25426,7 +25426,9 @@ in
 
   volnoti = callPackage ../applications/misc/volnoti { };
 
-  vorbis-tools = callPackage ../applications/audio/vorbis-tools { };
+  vorbis-tools = callPackage ../applications/audio/vorbis-tools {
+    autoreconfHook = buildPackages.autoreconfHook269;
+  };
 
   vscode = callPackage ../applications/editors/vscode/vscode.nix { };
 
