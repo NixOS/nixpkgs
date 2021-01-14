c02807ece4ddd872dffa84b0b3fc4d847e4b96da
Monson Shao <holymonson@gmail.com>
2021-01-14 22:31:53 +0800

    darwin.launchd: add appleHeaders

--- pkgs/os-specific/darwin/apple-source-releases/launchd/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/launchd/default.nix
@@ -9,4 +9,18 @@ appleDerivation {
     cp liblaunch/bootstrap.h $out/include/servers
     cp liblaunch/bootstrap.h $out/include/servers/bootstrap_defs.h
   '';
+
+  appleHeaders = ''
+    bootstrap.h
+    bootstrap_priv.h
+    launch.h
+    launch_internal.h
+    launch_priv.h
+    reboot2.h
+    servers/bootstrap.h
+    servers/bootstrap_defs.h
+    vproc.h
+    vproc_internal.h
+    vproc_priv.h
+  '';
 }
