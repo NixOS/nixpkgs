aef86cffdd422452b2b1b910c7ff933324b24017
zowoq <59103226+zowoq@users.noreply.github.com>
2021-01-15 04:59:24 +1000

    gdu: 2.2.0 -> 2.3.0
    https://github.com/dundee/gdu/releases/tag/v2.3.0

--- pkgs/tools/system/gdu/default.nix
+++ pkgs/tools/system/gdu/default.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "gdu";
-  version = "2.2.0";
+  version = "2.3.0";
 
   src = fetchFromGitHub {
     owner = "dundee";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0ajkc0vbzyl56d6z03s5vb17frjrg5wl145x60asnrmh7lg8adsj";
+    sha256 = "1dc0z6daxpbid2ilpms0dw76qyyc84gx0bcqsx0b3s5p5p154xzq";
   };
 
-  vendorSha256 = "1jqbsda9bch3awdq816w4jybv7wz9mfflmvs5y2wsa2qnhn9nbyp";
+  vendorSha256 = "18a3qwshz8jmw0j29qvmzarxig0kj1n0fnmlx81qzswsyl85kncv";
 
   buildFlagsArray = [ "-ldflags=-s -w -X main.AppVersion=${version}" ];
 
