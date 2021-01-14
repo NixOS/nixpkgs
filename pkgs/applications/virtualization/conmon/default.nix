8a4b39330f80143b313bde8d88b6cc1264b31d18
zowoq <59103226+zowoq@users.noreply.github.com>
2021-01-15 04:44:13 +1000

    Revert "conmon: 2.0.22 -> 2.0.23"
    This reverts commit 59d4583cf3c9025372f7174fe16a30044eccff6b.
    
    Broken release.

--- pkgs/applications/virtualization/conmon/default.nix
+++ pkgs/applications/virtualization/conmon/default.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation rec {
   pname = "conmon";
-  version = "2.0.23";
+  version = "2.0.22";
 
   src = fetchFromGitHub {
     owner = "containers";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1agygycpdacxx0pmbyxyyps5nv4662xcdfkfxd19b5npam8iiwpx";
+    sha256 = "07wd3pns6x25dcnc1r84cwmrzg8xgzsfmidkclcpcagf97ad7jmc";
   };
 
   nativeBuildInputs = [ pkg-config ];
