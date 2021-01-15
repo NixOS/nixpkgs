f7f73b2c8de734200589576100a62949a0754ece
Phillip Cloud <cloud@standard.ai>
2021-01-15 12:04:42 -0500

    nomad: 1.0.1 -> 1.0.2

--- pkgs/applications/networking/cluster/nomad/1.0.nix
+++ pkgs/applications/networking/cluster/nomad/1.0.nix
@@ -6,6 +6,6 @@
 
 callPackage ./generic.nix {
   inherit buildGoPackage nvidia_x11 nvidiaGpuSupport;
-  version = "1.0.1";
-  sha256 = "07k81csyxhgc7bgn297zlqyvc55qb5fmiavi7dk81rdpg5m2zjvv";
+  version = "1.0.2";
+  sha256 = "1l9j6k5dzh9ym9j75mam10vd9b5qh4xqfj6d63bjp7gkk4hd1jxx";
 }
