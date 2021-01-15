cd0fa5ae486525cda83515a5bb52e1f444202bae
Wout Mertens <Wout.Mertens@gmail.com>
2021-01-15 15:18:55 +0100

    scponly: init at 4.8
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9168,6 +9168,8 @@ in
 
   pash = callPackage ../shells/pash { };
 
+  scponly = callPackage ../shells/scponly { };
+
   tcsh = callPackage ../shells/tcsh { };
 
   rush = callPackage ../shells/rush { };
