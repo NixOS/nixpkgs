7c1eee3b8e0f21129705a43cf20b5d7960e6d9b4
Robert Schütz <dev@schuetz-co.de>
2021-01-16 13:30:14 +0100

    etesync-dav: 0.30.6 -> 0.30.7

--- pkgs/applications/misc/etesync-dav/default.nix
+++ pkgs/applications/misc/etesync-dav/default.nix
@@ -2,11 +2,11 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "etesync-dav";
-  version = "0.30.6";
+  version = "0.30.7";
 
   src = python3Packages.fetchPypi {
     inherit pname version;
-    sha256 = "0cjz4p3a750fwvrxbzwda0sidw7nscahvppdshbsx49i6qrczpbg";
+    sha256 = "16b3105834dd6d9e374e976cad0978e1acfed0f0328c5054bc214550aea3e2c5";
   };
 
   propagatedBuildInputs = with python3Packages; [
