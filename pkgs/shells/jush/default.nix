813d1433ff311566662245d80d0dbcb8c499ab7b
Ben Siraphob <bensiraphob@gmail.com>
2021-01-17 09:15:29 +0700

    pkgs/shells: pkgconfig -> pkg-config

--- pkgs/shells/jush/default.nix
+++ pkgs/shells/jush/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, editline }:
+{ lib, stdenv, fetchFromGitHub, autoreconfHook, pkg-config, editline }:
 
 stdenv.mkDerivation rec {
   pname = "jush";
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
     sha256 = "1azvghrh31gawd798a254ml4id642qvbva64zzg30pjszh1087n8";
   };
 
-  nativeBuildInputs = [ autoreconfHook pkgconfig ];
+  nativeBuildInputs = [ autoreconfHook pkg-config ];
 
   buildInputs = [ editline ];
 
