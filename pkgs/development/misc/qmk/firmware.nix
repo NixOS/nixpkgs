304853bc41c80c17d7eed3f553ac44449a1b3e4b
AndersonTorres <torres.anderson.85@protonmail.com>
2021-01-17 11:35:21 -0300

    qmk_firmware: 0.6.114 -> 0.11.49

--- pkgs/development/misc/qmk/firmware.nix
+++ pkgs/development/misc/qmk/firmware.nix
@@ -12,20 +12,19 @@
 
 stdenv.mkDerivation rec {
   pname = "qmk_firmware";
-  version = "0.6.144";
+  version = "0.11.49";
 
   src = fetchFromGitHub {
     owner = "qmk";
     repo = pname;
     rev = version;
-    sha256 = "0m71f9w32ksqjkrwhqwhr74q5v3pr38bihjyb9ks0k5id0inhrjn";
+    sha256 = "E/oe5u6F9pxvavRLlrXmr9kCc6pSdSmQgtg4a3wbIuA=";
     fetchSubmodules = true;
   };
+
   postPatch = ''
     substituteInPlace tmk_core/arm_atsam.mk \
       --replace arm-none-eabi arm-none-eabihf
-    rm keyboards/handwired/frenchdev/rules.mk
-    rm keyboards/dk60/rules.mk
   '';
 
   nativeBuildInputs = [
@@ -46,14 +45,14 @@ stdenv.mkDerivation rec {
 
   checkTarget = "test:all";
 
-  installPhase = ''
+  preInstall = ''
     mkdir $out
   '';
 
   meta = with lib; {
     homepage = "https://qmk.fm";
     description = "Open-source keyboard firmware for ATMEL AVR and ARM USB families";
-    longDescription = ''      
+    longDescription = ''
       QMK (Quantum Mechanical Keyboard) is an open source community centered
       around developing computer input devices. The community encompasses all
       sorts of input devices, such as keyboards, mice, and MIDI devices. A core
