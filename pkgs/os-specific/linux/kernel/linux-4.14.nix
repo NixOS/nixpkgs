cf90ecd036293ae124b1d2e607091a286978567e
Tim Steinbach <tim@nequissimus.com>
2021-01-18 10:57:41 -0500

    linux: 4.14.215 -> 4.14.216

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.14.215";
+  version = "4.14.216";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1ni6p485dszi365rfflrwmjl6arbkhfvl1zm7p99vcv11hllx14s";
+    sha256 = "19dvxmqvs1ysl127zqdcqq2pyf7370jj66fd73zdx6ya2pplz1mp";
   };
 } // (args.argsOverride or {}))
