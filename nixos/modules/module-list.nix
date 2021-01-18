951f41fd41bcd00bf56b645e121cd26597e32e45
Yannick Markus <ym@ymarkus.dev>
2021-01-18 08:37:28 +0100

    nixos/bookstack: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -865,6 +865,7 @@
   ./services/web-apps/atlassian/confluence.nix
   ./services/web-apps/atlassian/crowd.nix
   ./services/web-apps/atlassian/jira.nix
+  ./services/web-apps/bookstack.nix
   ./services/web-apps/convos.nix
   ./services/web-apps/cryptpad.nix
   ./services/web-apps/documize.nix
