1f3f9053b912635f52726ca9b6006c9d7288dd12
Maximilian Bosch <maximilian@mbosch.me>
2021-01-18 16:56:36 +0100

    element-web: 1.7.16 -> 1.7.17
    ChangeLog: https://github.com/vector-im/element-web/releases/tag/v1.7.17

--- pkgs/applications/networking/instant-messengers/element/element-web.nix
+++ pkgs/applications/networking/instant-messengers/element/element-web.nix
@@ -12,11 +12,11 @@ let
 
 in stdenv.mkDerivation rec {
   pname = "element-web";
-  version = "1.7.16";
+  version = "1.7.17";
 
   src = fetchurl {
     url = "https://github.com/vector-im/element-web/releases/download/v${version}/element-v${version}.tar.gz";
-    sha256 = "sha256-/KLTD7IvIp1f1dSUEMMCoknQzZarcP2wDM211h+OJMM=";
+    sha256 = "sha256-9DyA3h0sQCzOFZ9gEReJrMQOnygFvEB7PeIpkG/F5H4=";
   };
 
   installPhase = ''
