f9ced01f4ac5c269e2ce73ec629cad115b0c725d
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-18 00:59:52 +0000

    python37Packages.ROPGadget: 6.3 -> 6.4

--- pkgs/development/python-modules/ROPGadget/default.nix
+++ pkgs/development/python-modules/ROPGadget/default.nix
@@ -3,11 +3,11 @@
 
 buildPythonPackage rec {
   pname = "ROPGadget";
-  version = "6.3";
+  version = "6.4";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0v34w88if3p4vn46aby24msfnxj6znmkf4848n4d24jnykxcsqk9";
+    sha256 = "51d7cbdf51ac8b3f3f00bc0d4ae44433ef58d3bf5495efb316ec918654f1e6c3";
   };
 
   propagatedBuildInputs = [ capstone ];
