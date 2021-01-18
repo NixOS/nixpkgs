7c70204558c0395bbf459cb3697fa63dc5dfbbb4
Tim Steinbach <tim@nequissimus.com>
2021-01-18 10:58:00 -0500

    linux: 4.4.251 -> 4.4.252

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.251";
+  version = "4.4.252";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "13mnlwwcwvbyqn8lafjymq66qjfj7nksdiyrcgymx8s03z1why86";
+    sha256 = "0lchvfvn0kvqh1yixwscz4wrzd965zsxjkpc7nqiw9rhmvma3paf";
   };
 } // (args.argsOverride or {}))
