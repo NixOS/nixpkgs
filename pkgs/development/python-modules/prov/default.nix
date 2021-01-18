b17123a0de80d424916cd25b8e9bd578593016fb
Robert Schütz <dev@schuetz-co.de>
2021-01-18 23:12:19 +0100

    python3Packages.prov: update dependencies

--- pkgs/development/python-modules/prov/default.nix
+++ pkgs/development/python-modules/prov/default.nix
@@ -4,11 +4,8 @@
 , lxml
 , networkx
 , dateutil
-, six
-, pydotplus
 , rdflib
 , pydot
-, glibcLocales
 }:
 
 buildPythonPackage rec {
@@ -20,27 +17,16 @@ buildPythonPackage rec {
     sha256 = "b6438f2195ecb9f6e8279b58971e02bc51814599b5d5383366eef91d867422ee";
   };
 
-  prePatch = ''
-    substituteInPlace setup.py --replace "six==1.10.0" "six>=1.10.0"
-  '';
-
   propagatedBuildInputs = [
     lxml
     networkx
     dateutil
-    six
-    pydotplus
     rdflib
   ];
 
   checkInputs = [
     pydot
-    glibcLocales
   ];
-
-  preCheck = ''
-    export LC_ALL="en_US.utf-8"
-  '';
 
   meta = with lib; {
     description = "A Python library for W3C Provenance Data Model (PROV)";
