68f95c3707cddf33defbc9bd79e4e3f4f86a2438
Soner Sayakci <s.sayakci@shopware.com>
2021-01-19 12:32:37 +0100

    chiaki: 2.0.1 -> 2.1.0

--- pkgs/games/chiaki/default.nix
+++ pkgs/games/chiaki/default.nix
@@ -18,13 +18,13 @@
 
 mkDerivation rec {
   pname = "chiaki";
-  version = "2.0.1";
+  version = "2.1.0";
 
   src = fetchgit {
     url = "https://git.sr.ht/~thestr4ng3r/chiaki";
     rev = "v${version}";
     fetchSubmodules = true;
-    sha256 = "0l532i9j6wmzbxqx7fg69kgfd1zy1r1wlw6f756vpxpgswivi892";
+    sha256 = "1hgh2j71rch53cnpm3f195gpfgrb4hd0yqa810k3ikar8zl4h7sd";
   };
 
   nativeBuildInputs = [
