8fd1e81e2129f38868e8c00d73d06bf2c35c9317
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-19 12:36:50 +0000

    doxygen: 1.8.20 -> 1.9.1

--- pkgs/development/tools/documentation/doxygen/default.nix
+++ pkgs/development/tools/documentation/doxygen/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "doxygen";
-  version = "1.8.20";
+  version = "1.9.1";
 
   src = fetchFromGitHub {
     owner = "doxygen";
     repo = "doxygen";
     rev = "Release_${stdenv.lib.replaceStrings [ "." ] [ "_" ] version}";
-    sha256 = "17chvi3i80rj4750smpizf562xjzd2xcv5rfyh997pyvc1zbq5rh";
+    sha256 = "sha256-sY/yFkpBpO3HbPsuaxvC0AXrpMsBa6j1UOh0Ra+RvHw=";
   };
 
   nativeBuildInputs = [
