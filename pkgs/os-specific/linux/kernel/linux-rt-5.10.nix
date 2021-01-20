c74614795b565052c260617fd9d7d80ea1cae1de
Tim Steinbach <tim@nequissimus.com>
2021-01-20 09:58:48 -0500

    linux-rt_5_10: 5.10.4-rt22 -> 5.10.8-rt24

--- pkgs/os-specific/linux/kernel/linux-rt-5.10.nix
+++ pkgs/os-specific/linux/kernel/linux-rt-5.10.nix
@@ -6,7 +6,7 @@
 , ... } @ args:
 
 let
-  version = "5.10.4-rt22"; # updated by ./update-rt.sh
+  version = "5.10.8-rt24"; # updated by ./update-rt.sh
   branch = lib.versions.majorMinor version;
   kversion = builtins.elemAt (lib.splitString "-" version) 0;
 in buildLinux (args // {
@@ -18,14 +18,14 @@ in buildLinux (args // {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${kversion}.tar.xz";
-    sha256 = "1v2nbpp21c3fkw23dgrrfznnnlvi0538kj8wrlb2m6g94rn3jklh";
+    sha256 = "1v83wm8xbhq1sgn7c84zi7l40vmd9k1gb653b686jp8n4na85z2w";
   };
 
   kernelPatches = let rt-patch = {
     name = "rt";
     patch = fetchurl {
       url = "mirror://kernel/linux/kernel/projects/rt/${branch}/older/patch-${version}.patch.xz";
-      sha256 = "1wnp7w3k1z10ipg8vzgyh22lpfya1p3ckabjadk9hadpa1ialma0";
+      sha256 = "06fqwx9flcxzbjr9gb0d7v4hidypzz69r6p2mfzhqh7ii0p89f30";
     };
   }; in [ rt-patch ] ++ lib.remove rt-patch kernelPatches;
 
