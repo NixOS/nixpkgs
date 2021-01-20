9f5d0aad8dd0cd0a78213b4441a6485fb268a992
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-20 09:47:40 +0000

    new-session-manager: 1.4.0 -> 1.5.0

--- pkgs/applications/audio/new-session-manager/default.nix
+++ pkgs/applications/audio/new-session-manager/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "new-session-manager";
-  version = "1.4.0";
+  version = "1.5.0";
 
   src = fetchFromGitHub {
     owner = "linuxaudio";
     repo = "new-session-manager";
     rev = "v${version}";
-    sha256 = "PqOv4tx3NLxL2+GWIUVgL72EQYMyDPIMrAkyby3TZ+0=";
+    sha256 = "sha256-YP5AHoFP8P1o0Y2jAipRcNPxPRuM7COs5tBMm6Eojoc=";
   };
 
   nativeBuildInputs = [ meson pkg-config ninja ];
