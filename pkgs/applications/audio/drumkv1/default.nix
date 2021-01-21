c5f566240e607da042d1697e1d17a0db2c70dded
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-21 22:49:30 +0000

    drumkv1: 0.9.18 -> 0.9.19

--- pkgs/applications/audio/drumkv1/default.nix
+++ pkgs/applications/audio/drumkv1/default.nix
@@ -2,11 +2,11 @@
 
 mkDerivation rec {
   pname = "drumkv1";
-  version = "0.9.18";
+  version = "0.9.19";
 
   src = fetchurl {
     url = "mirror://sourceforge/drumkv1/${pname}-${version}.tar.gz";
-    sha256 = "1bzkaz7sqx1pvirja8zm7i2ckzl5ad6xspr4840389ik3l8qpnr5";
+    sha256 = "sha256-LmOUXI9tBlUMeBfHzW4KjtDTjWtfE8Q8qOJ5MgzLLnE=";
   };
 
   buildInputs = [ libjack2 alsaLib libsndfile liblo lv2 qt5.qtbase qt5.qttools ];
