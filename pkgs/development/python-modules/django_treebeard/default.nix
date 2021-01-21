f169c19a1259465bcaed73073f0ef41c5802f7e9
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-21 05:19:48 +0000

    python37Packages.django_treebeard: 4.3.1 -> 4.4

--- pkgs/development/python-modules/django_treebeard/default.nix
+++ pkgs/development/python-modules/django_treebeard/default.nix
@@ -7,11 +7,11 @@
 
 buildPythonPackage rec {
   pname = "django-treebeard";
-  version = "4.3.1";
+  version = "4.4";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "83aebc34a9f06de7daaec330d858d1c47887e81be3da77e3541fe7368196dd8a";
+    sha256 = "f50e4eea146f7af6702decf7ef198ac1eee1fb9bb4af2c5dba276c3c48f76623";
   };
 
   buildInputs = [ pytest ];
