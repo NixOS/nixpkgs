abc9d1a554cffa78ba1e01a1d6a23e0bfb7efd15
Michael Lingelbach <m.j.lbach@gmail.com>
2021-01-21 14:05:01 -0800

    toolbox: init at 0.0.99

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7056,6 +7056,8 @@ in
 
   podiff = callPackage ../tools/text/podiff { };
 
+  toolbox = callPackage ../applications/virtualization/toolbox { };
+
   podman = if stdenv.isDarwin then
     callPackage ../applications/virtualization/podman { }
   else
