d96a69c9639b2af5e0bd36ac02f57309fbe16b5b
Peter Woodman <peter@shortbus.org>
2021-01-21 16:10:03 -0500

    musl-obstack: init at 1.2.2
    in service of getting elfutils building against musl-libc, this library
    is an extraction of the obstack functions from glibc/libiberty. this is
    the approach taken by void and alpine linux.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19490,6 +19490,7 @@ in
   musl = callPackage ../os-specific/linux/musl { };
 
   musl-fts = callPackage ../os-specific/linux/musl-fts { };
+  musl-obstack = callPackage ../os-specific/linux/musl-obstack { };
 
   nushell = callPackage ../shells/nushell {
     inherit (darwin.apple_sdk.frameworks) AppKit Security;
