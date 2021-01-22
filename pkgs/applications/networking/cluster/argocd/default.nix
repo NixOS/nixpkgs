5d5acadac5464e69301b7fbb8b91dccca60ec799
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-22 09:31:30 +0000

    argocd: 1.8.2 -> 1.8.3

--- pkgs/applications/networking/cluster/argocd/default.nix
+++ pkgs/applications/networking/cluster/argocd/default.nix
@@ -2,14 +2,14 @@
 
 buildGoModule rec {
   pname = "argocd";
-  version = "1.8.2";
+  version = "1.8.3";
   commit = "ef5010c3a0b5e027fd642732d03c5b0391b1e574";
 
   src = fetchFromGitHub {
     owner = "argoproj";
     repo = "argo-cd";
     rev = "v${version}";
-    sha256 = "sha256-qppJYpTuCA1+5dY8kbtUy2Rd+fG7u+BMxsY7IqFUOf8=";
+    sha256 = "sha256-do5DAxaQ1gBdvNN/YGKAkmkFcJ+j/ojBaWPwrXXQko0=";
   };
 
   vendorSha256 = "sha256-6DOay+aeXz7EQKe5SzQSmg/5KyUI0g6wzPgx/+F2RW4=";
