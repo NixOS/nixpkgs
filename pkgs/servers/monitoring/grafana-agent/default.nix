ce4a93e13a73a53028f9e86a8a8c7f21fbc93196
Florian Klink <flokli@flokli.de>
2021-01-22 12:31:24 +0100

    grafana-agent: 0.10.0 -> 0.11.0
    (cherry picked from commit f227aab6f350e3ff646569f8dfd9b29370647107)

--- pkgs/servers/monitoring/grafana-agent/default.nix
+++ pkgs/servers/monitoring/grafana-agent/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "grafana-agent";
-  version = "0.10.0";
+  version = "0.11.0";
 
   src = fetchFromGitHub {
     rev = "v${version}";
     owner = "grafana";
     repo = "agent";
-    sha256 = "1kliq6d3hg4bx9s5crdagirf2h3ljl0ikcyz0x0wb2ack6cgjsvm";
+    sha256 = "092ry9gq9fpkkgsdymfwzdxz982pp0ljf2gs6mp419ivvllzwhiv";
   };
 
   vendorSha256 = null;
