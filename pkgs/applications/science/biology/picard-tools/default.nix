e1ea0a237a76805050158a4568b2cdf0701480f8
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-22 19:10:27 +0000

    picard-tools: 2.24.0 -> 2.24.1

--- pkgs/applications/science/biology/picard-tools/default.nix
+++ pkgs/applications/science/biology/picard-tools/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "picard-tools";
-  version = "2.24.0";
+  version = "2.24.1";
 
   src = fetchurl {
     url = "https://github.com/broadinstitute/picard/releases/download/${version}/picard.jar";
-    sha256 = "sha256-cOkQObzMb222DxjEFxMhiozfRfWR8CwQEsBiFSsnzXs=";
+    sha256 = "sha256-acCsYHyZ358hYqa2haOxR+g6FQGtK+hstYfLBvHLwR8=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
