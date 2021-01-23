1b7583ee8f4dc8dd3a4e5f781eeeb4e1c126d144
Tobias Mayer <tobim@fastmail.fm>
2021-01-23 13:10:18 +0100

    simdjson: 0.7.0 -> 0.8.0

--- pkgs/development/libraries/simdjson/default.nix
+++ pkgs/development/libraries/simdjson/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "simdjson";
-  version = "0.7.0";
+  version = "0.8.0";
 
   src = fetchFromGitHub {
     owner = "simdjson";
     repo = "simdjson";
     rev = "v${version}";
-    sha256 = "14gi2zq430nfjy424q6r57imc2gnz30nhx4r0wbajzp9qvna819w";
+    sha256 = "0lpb8la74xwd78d5mgwnzx4fy632jbmh0ip19v0dydwm0kagm0a3";
   };
 
   nativeBuildInputs = [ cmake ];
