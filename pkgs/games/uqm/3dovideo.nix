NixOS/nixpkgs

commit 2e34288f0d8cf01eea228c7dbc50af9589b885f3

pkgs/games: stdenv.lib -> lib

pkgs/games/uqm/3dovideo.nix
@@ -1,6 +1,6 @@
 { stdenv, requireFile, writeText, fetchurl, haskellPackages }:
 
-with stdenv.lib;
+with lib;
 
 let
   makeSpin = num: let
@@ -30,7 +30,7 @@ let
     buildDepends = [ base binary bytestring filepath tar ];
 
     description = "Extract video files from a Star Control II 3DO image";
-    license = stdenv.lib.licenses.bsd3;
+    license = lib.licenses.bsd3;
   };
 
 in stdenv.mkDerivation {

pkgs/games/uqm/default.nix
@@ -102,7 +102,7 @@ in stdenv.mkDerivation rec {
           spin-offs, thereby making zillions more people happy!
     '';
     homepage = "http://sc2.sourceforge.net/";
-    license = stdenv.lib.licenses.gpl2;
+    license = lib.licenses.gpl2;
     maintainers = with lib.maintainers; [ jcumming aszlig ];
     platforms = with lib.platforms; linux;
   };

pkgs/games/abbaye-des-morts/default.nix
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
 
   makeFlags = [ "PREFIX=$(out)" "DESTDIR=" ];
 
-  preBuild = stdenv.lib.optionalString stdenv.cc.isClang
+  preBuild = lib.optionalString stdenv.cc.isClang
     ''
       substituteInPlace Makefile \
         --replace -fpredictive-commoning ""

pkgs/games/adom/default.nix
@@ -5,7 +5,7 @@ let
 
   inherit (xorg) libXext libX11;
 
-  lpath = "${stdenv.cc.cc.lib}/lib64:" + stdenv.lib.makeLibraryPath [
+  lpath = "${stdenv.cc.cc.lib}/lib64:" + lib.makeLibraryPath [
       zlib libmad libpng12 libcaca libXext libX11 libGLU libGL alsaLib libpulseaudio];
 
 in

pkgs/games/alephone/default.nix
@@ -93,7 +93,7 @@ in self // {
       '';
 
       meta = alephone.meta // {
-        license = stdenv.lib.licenses.free;
+        license = lib.licenses.free;
         hydraPlatforms = [ ];
       } // meta;
     });

pkgs/games/andyetitmoves/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, libvorbis, libogg, libtheora, SDL, libXft, SDL_image, zlib, libX11, libpng, openal, runtimeShell, requireFile, commercialVersion ? false }:
+{ lib, stdenv, fetchurl, libvorbis, libogg, libtheora, SDL, libXft, SDL_image, zlib, libX11, libpng, openal, runtimeShell, requireFile, commercialVersion ? false }:
 
 let plainName = "andyetitmoves";
     version   = "1.2.2";
@@ -68,8 +68,8 @@ stdenv.mkDerivation {
 
     homepage = "http://www.andyetitmoves.net/";
 
-    license = stdenv.lib.licenses.unfree;
+    license = lib.licenses.unfree;
 
-    maintainers = with stdenv.lib.maintainers; [bluescreen303];
+    maintainers = with lib.maintainers; [bluescreen303];
   };
 }

pkgs/games/arena/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, gtk2-x11, glib, pango, cairo, atk, gdk-pixbuf, libX11 }:
+{ lib, stdenv, fetchurl, gtk2-x11, glib, pango, cairo, atk, gdk-pixbuf, libX11 }:
 
 # Arena is free software in the sense of "free beer" but not as in "free
 # speech". We can install it as we please, but we cannot re-distribute it in
@@ -7,7 +7,7 @@
 
 let
 
-  inherit (stdenv.lib) makeLibraryPath;
+  inherit (lib) makeLibraryPath;
   libDir = "lib64";
 
 in
@@ -66,9 +66,9 @@ stdenv.mkDerivation rec {
       UCI protocol I, II. Furthermore, compatible to Chess960, DGT electronic
       chess board & DGT clocks and much more.
     '';
-    license = stdenv.lib.licenses.unfree;
+    license = lib.licenses.unfree;
     platforms = ["x86_64-linux"];
-    hydraPlatforms = stdenv.lib.platforms.none;
+    hydraPlatforms = lib.platforms.none;
   };
 
 }

pkgs/games/arx-libertatis/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, cmake, zlib, boost
+{ lib, stdenv, fetchFromGitHub, cmake, zlib, boost
 , openal, glm, freetype, libGLU, SDL2, epoxy
 , dejavu_fonts, inkscape, optipng, imagemagick
 , withCrashReporter ? !stdenv.isDarwin
@@ -8,7 +8,7 @@
 ,   gdb  ? null
 }:
 
-with stdenv.lib;
+with lib;
 
 stdenv.mkDerivation {
   pname = "arx-libertatis";

commit 2f30e32121691fa924dc13e6f09f569911723402

uqm: Fix evaluation of 3DO video package
Regression introduced by 2e34288f0d8cf01eea228c7dbc50af9589b885f3.

The commit in question just used the "lib" attribute, but it wasn't in
scope. In addition to bringeng the lib attribute into scope, I also
removed the "with lib;", which should make sure that we get an error
when just parsing (nix-instantiate --parse) the Nix expression.

Signed-off-by: aszlig <aszlig@nix.build>

pkgs/games/uqm/3dovideo.nix
@@ -1,10 +1,8 @@
-{ stdenv, requireFile, writeText, fetchurl, haskellPackages }:
-
-with lib;
+{ stdenv, lib, requireFile, writeText, fetchurl, haskellPackages }:
 
 let
   makeSpin = num: let
-    padded = (optionalString (lessThan num 10) "0") + toString num;
+    padded = (lib.optionalString (lib.lessThan num 10) "0") + toString num;
   in "slides.spins.${padded} = 3DOVID:" +
      "addons/3dovideo/spins/ship${padded}.duk:" +
      "addons/3dovideo/spins/spin.aif:" +
@@ -13,7 +11,7 @@ let
   videoRMP = writeText "3dovideo.rmp" (''
     slides.ending = 3DOVID:addons/3dovideo/ending/victory.duk
     slides.intro = 3DOVID:addons/3dovideo/intro/intro.duk
-  '' + concatMapStrings makeSpin (range 0 24));
+  '' + lib.concatMapStrings makeSpin (lib.range 0 24));
 
   helper = with haskellPackages; mkDerivation {
     pname = "uqm3donix";

pkgs/games/uqm/default.nix
@@ -12,7 +12,7 @@ assert use3DOVideos -> requireFile != null && writeText != null
 
 let
   videos = import ./3dovideo.nix {
-    inherit stdenv requireFile writeText fetchurl haskellPackages;
+    inherit stdenv lib requireFile writeText fetchurl haskellPackages;
   };
 
   remixPacks = lib.imap1 (num: sha256: fetchurl rec {