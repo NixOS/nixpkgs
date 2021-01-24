cee6a4d01e8c785ac91cd54c67d1b9d6f60c6d0a
Karl Hallsby <karl@hallsby.com>
2021-01-23 23:21:44 -0600

    python.pkgs.ufl: init at dolfin's version
    python.pkgs.ufl: get licenses correct
    
    python.pkgs.ufl: use with lib

---
+++ pkgs/development/python-modules/ufl/default.nix
@@ -0,0 +1,32 @@
+{ stdenv
+, lib
+, fetchurl
+, python3Packages
+, dolfin
+}:
+
+python3Packages.buildPythonPackage rec {
+  pname = "ufl";
+  inherit (dolfin) version;
+
+  src = fetchurl {
+    url = "https://bitbucket.org/fenics-project/ufl/downloads/ufl-${version}.tar.gz";
+    sha256 = "04daxwg4y9c51sdgvwgmlc82nn0fjw7i2vzs15ckdc7dlazmcfi1";
+  };
+
+  propagatedBuildInputs = with python3Packages; [ numpy six ];
+
+  checkInputs = with python3Packages; [ pytest ];
+
+  checkPhase = ''
+    runHook preCheck
+    py.test test/
+    runHook postCheck
+  '';
+
+  meta = with lib; {
+    description = "A domain-specific language for finite element variational forms";
+    homepage = "https://fenicsproject.org/";
+    license = with licenses; [ gpl3Plus lgpl3Plus ];
+  };
+}
