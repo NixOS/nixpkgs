NixOS/nixpkgs

commit 2f78ee7e816ae39f86bae2e6c61b2d14d3fe3ff4

pkgs/development: stdenv.lib -> lib

pkgs/development/beam-modules/default.nix
@@ -1,12 +1,12 @@
-{ stdenv, pkgs, erlang }:
+{ lib, stdenv, pkgs, erlang }:
 
 let
-  inherit (stdenv.lib) makeExtensible;
+  inherit (lib) makeExtensible;
 
-  lib = pkgs.callPackage ./lib.nix {};
+  lib' = pkgs.callPackage ./lib.nix {};
 
   # FIXME: add support for overrideScope
-  callPackageWithScope = scope: drv: args: stdenv.lib.callPackageWith scope drv args;
+  callPackageWithScope = scope: drv: args: lib'.callPackageWith scope drv args;
   mkScope = scope: pkgs // scope;
 
   packages = self:
@@ -38,27 +38,27 @@ let
         # BEAM-based languages.
         elixir = elixir_1_11;
 
-        elixir_1_11 = lib.callElixir ../interpreters/elixir/1.11.nix {
+        elixir_1_11 = lib'.callElixir ../interpreters/elixir/1.11.nix {
           inherit erlang;
           debugInfo = true;
         };
 
-        elixir_1_10 = lib.callElixir ../interpreters/elixir/1.10.nix {
+        elixir_1_10 = lib'.callElixir ../interpreters/elixir/1.10.nix {
           inherit erlang;
           debugInfo = true;
         };
 
-        elixir_1_9 = lib.callElixir ../interpreters/elixir/1.9.nix {
+        elixir_1_9 = lib'.callElixir ../interpreters/elixir/1.9.nix {
           inherit erlang;
           debugInfo = true;
         };
 
-        elixir_1_8 = lib.callElixir ../interpreters/elixir/1.8.nix {
+        elixir_1_8 = lib'.callElixir ../interpreters/elixir/1.8.nix {
           inherit erlang;
           debugInfo = true;
         };
 
-        elixir_1_7 = lib.callElixir ../interpreters/elixir/1.7.nix {
+        elixir_1_7 = lib'.callElixir ../interpreters/elixir/1.7.nix {
           inherit erlang;
           debugInfo = true;
         };
@@ -67,8 +67,8 @@ let
         # https://hexdocs.pm/elixir/compatibility-and-deprecations.html
 
         lfe = lfe_1_3;
-        lfe_1_2 = lib.callLFE ../interpreters/lfe/1.2.nix { inherit erlang buildRebar3 buildHex; };
-        lfe_1_3 = lib.callLFE ../interpreters/lfe/1.3.nix { inherit erlang buildRebar3 buildHex; };
+        lfe_1_2 = lib'.callLFE ../interpreters/lfe/1.2.nix { inherit erlang buildRebar3 buildHex; };
+        lfe_1_3 = lib'.callLFE ../interpreters/lfe/1.3.nix { inherit erlang buildRebar3 buildHex; };
 
         # Non hex packages. Examples how to build Rebar/Mix packages with and
         # without helper functions buildRebar3 and buildMix.

pkgs/development/androidndk-pkgs/default.nix
@@ -1,4 +1,4 @@
-{ androidenv, buildPackages, pkgs, targetPackages
+{ lib, androidenv, buildPackages, pkgs, targetPackages
 }:
 
 {
@@ -17,6 +17,7 @@
       };
     in
     import ./androidndk-pkgs.nix {
+      inherit lib;
       inherit (buildPackages)
         makeWrapper;
       inherit (pkgs)
@@ -46,6 +47,7 @@
       };
     in
     import ./androidndk-pkgs.nix {
+      inherit lib;
       inherit (buildPackages)
         makeWrapper;
       inherit (pkgs)

pkgs/development/beam-modules/hex/default.nix
@@ -45,13 +45,13 @@ let
 
     meta = {
       description = "Package manager for the Erlang VM https://hex.pm";
-      license = stdenv.lib.licenses.mit;
+      license = lib.licenses.mit;
       homepage = "https://github.com/hexpm/hex";
-      maintainers = with stdenv.lib.maintainers; [ ericbmerritt ];
+      maintainers = with lib.maintainers; [ ericbmerritt ];
     };
 
     passthru = {
       env = shell self;
     };
 };
-in stdenv.lib.fix pkg
+in lib.fix pkg

pkgs/development/beam-modules/pc/default.nix
@@ -7,7 +7,7 @@ buildHex {
 
   meta = {
     description = "a rebar3 port compiler for native code";
-    license = stdenv.lib.licenses.mit;
+    license = lib.licenses.mit;
     homepage = "https://github.com/blt/port_compiler";
   };
 }

pkgs/development/beam-modules/pgsql/default.nix
@@ -21,9 +21,9 @@ let
 
     meta = {
       description = "Erlang PostgreSQL Driver";
-      license = stdenv.lib.licenses.mit;
+      license = lib.licenses.mit;
       homepage = "https://github.com/semiocast/pgsql";
-      maintainers = with stdenv.lib.maintainers; [ ericbmerritt ];
+      maintainers = with lib.maintainers; [ ericbmerritt ];
     };
 
     passthru = {
@@ -31,4 +31,4 @@ let
     };
 
 };
-in stdenv.lib.fix pkg
+in lib.fix pkg

pkgs/development/beam-modules/webdriver/default.nix
@@ -27,9 +27,9 @@ let
 
     meta = {
       description = "WebDriver implementation in Erlang";
-      license = stdenv.lib.licenses.mit;
+      license = lib.licenses.mit;
       homepage = "https://github.com/Quviq/webdrv";
-      maintainers = with stdenv.lib.maintainers; [ ericbmerritt ];
+      maintainers = with lib.maintainers; [ ericbmerritt ];
     };
 
     passthru = {
@@ -37,4 +37,4 @@ let
     };
 
 };
-in stdenv.lib.fix pkg
+in lib.fix pkg

pkgs/development/java-modules/junit/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, pkgs, mavenbuild, fetchMaven }:
+{ lib, stdenv, pkgs, mavenbuild, fetchMaven }:
 
 with pkgs.javaPackages;
 
@@ -21,9 +21,9 @@ in rec {
     meta = {
       homepage = "https://junit.org/junit4/";
       description = "Simple framework to write repeatable tests. It is an instance of the xUnit architecture for unit testing frameworks";
-      license = stdenv.lib.licenses.epl10;
-      platforms = stdenv.lib.platforms.all;
-      maintainers = with stdenv.lib.maintainers;
+      license = lib.licenses.epl10;
+      platforms = lib.platforms.all;
+      maintainers = with lib.maintainers;
         [ nequissimus ];
     };
   };

pkgs/development/java-modules/maven-hello/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, pkgs, mavenbuild }:
+{ lib, stdenv, pkgs, mavenbuild }:
 
 with pkgs.javaPackages;
 
@@ -20,9 +20,9 @@ in rec {
     meta = {
       homepage = "https://github.com/NeQuissimus/maven-hello/";
       description = "Maven Hello World";
-      license = stdenv.lib.licenses.unlicense;
-      platforms = stdenv.lib.platforms.all;
-      maintainers = with stdenv.lib.maintainers;
+      license = lib.licenses.unlicense;
+      platforms = lib.platforms.all;
+      maintainers = with lib.maintainers;
         [ nequissimus ];
     };
   };

commit a35934f5d3d11ef0ee87f1ae62d7db2b27c5ad6e

beamPackages: use callPackagesWith from nixpkgs lib
2f78ee7e816ae39f86bae2e6c61b2d14d3fe3ff4 accidentally changed this from
(nixpkgs) stdenv.lib to (beam-modules) lib', which broke things.
This changes it back to (nixpkgs) lib.

pkgs/development/beam-modules/default.nix
@@ -6,7 +6,7 @@ let
   lib' = pkgs.callPackage ./lib.nix {};
 
   # FIXME: add support for overrideScope
-  callPackageWithScope = scope: drv: args: lib'.callPackageWith scope drv args;
+  callPackageWithScope = scope: drv: args: lib.callPackageWith scope drv args;
   mkScope = scope: pkgs // scope;
 
   packages = self: