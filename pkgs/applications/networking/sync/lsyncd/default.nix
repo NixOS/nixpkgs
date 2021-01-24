e63d0738a7e81c463342003810046c483c2ef745
Nicolas Mattia <nicolas@nmattia.com>
2021-01-24 12:22:36 +0100

    lsyncd: build on darwin (#109981)
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/applications/networking/sync/lsyncd/default.nix
+++ pkgs/applications/networking/sync/lsyncd/default.nix
@@ -25,6 +25,10 @@ stdenv.mkDerivation rec {
       --replace "/usr/bin/rsync" "${rsync}/bin/rsync"
   '';
 
+  # Special flags needed on Darwin:
+  # https://github.com/axkibe/lsyncd/blob/42413cabbedca429d55a5378f6e830f191f3cc86/INSTALL#L51
+  cmakeFlags = lib.optional stdenv.isDarwin [ "-DWITH_INOTIFY=OFF" "-DWITH_FSEVENTS=ON" ];
+
   dontUseCmakeBuildDir = true;
 
   buildInputs = [
@@ -36,8 +40,8 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     homepage = "https://github.com/axkibe/lsyncd";
     description = "A utility that synchronizes local directories with remote targets";
-    license = licenses.gpl2;
-    platforms = platforms.linux;
+    license = licenses.gpl2Plus;
+    platforms = platforms.all;
     maintainers = with maintainers; [ bobvanderlinden ];
   };
 }
