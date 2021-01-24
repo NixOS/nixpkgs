75d174b4108e8fefc612ac79e925ceee37221c21
Karl Hallsby <karl@hallsby.com>
2021-01-23 23:55:47 -0600

    python.pkgs.fenics: move python fenics bindings to be python module
    python.pkgs.fenics: correct pname and sourceRoot
    
    python2 packages were split off

---
+++ pkgs/development/python-modules/fenics/default.nix
@@ -0,0 +1,62 @@
+{ lib, stdenv
+, fetchurl
+, buildPythonPackage
+, isPy27
+, boost
+, cmake
+, mpi4py
+, pybind11
+, dijitso
+, numpy
+, sympy
+, ufl
+, pkgconfig
+, ffc
+, dolfin
+}:
+
+buildPythonPackage rec {
+  pname = "fenics";
+  inherit (dolfin) version;
+
+  inherit (dolfin) src;
+  sourceRoot = "source/python";
+
+  nativeBuildInputs = [
+    pybind11
+    cmake
+  ];
+
+  dontUseCmakeConfigure = true;
+  preConfigure = ''
+    export CMAKE_PREFIX_PATH=${pybind11}/share/cmake/pybind11:$CMAKE_PREFIX_PATH
+    substituteInPlace setup.py --replace "pybind11==2.2.4" "pybind11"
+    substituteInPlace dolfin/jit/jit.py \
+      --replace 'pkgconfig.exists("dolfin")' 'pkgconfig.exists("${dolfin}/lib/pkgconfig/dolfin.pc")' \
+      --replace 'pkgconfig.parse("dolfin")' 'pkgconfig.parse("${dolfin}/lib/pkgconfig/dolfin.pc")'
+  '';
+
+  buildInputs = [
+    dolfin
+    boost
+  ];
+
+  propagatedBuildInputs = [
+    dijitso
+    ffc
+    mpi4py
+    numpy
+    ufl
+    pkgconfig
+    pybind11
+  ];
+
+  doCheck = false; # Tries to orte_ess_init and call ssh to localhost
+
+  meta = {
+    description = "Python bindings for the DOLFIN FEM compiler";
+    homepage = "https://fenicsproject.org/";
+    platforms = lib.platforms.all;
+    license = lib.licenses.lgpl3;
+  };
+}
