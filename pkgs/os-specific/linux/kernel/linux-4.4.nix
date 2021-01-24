487a3944103c3469926b3946c0737a29596dffb2
Tim Steinbach <tim@nequissimus.com>
2021-01-24 12:23:38 -0500

    linux: 4.4.252 -> 4.4.253
    (cherry picked from commit 59bd2632d0af64de7a6ee99f1307485f78923db7)

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.252";
+  version = "4.4.253";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0lchvfvn0kvqh1yixwscz4wrzd965zsxjkpc7nqiw9rhmvma3paf";
+    sha256 = "0nlqnfhrkaj2s582kc0wxqi0881hgp6l9z85qx4ckflc8jwrh7k6";
   };
 } // (args.argsOverride or {}))
