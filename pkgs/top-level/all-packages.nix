6d6ae21535f90cde259b819301667ab0d9beb851
Tim Steinbach <tim@nequissimus.com>
2021-01-24 13:03:31 -0500

    linux-hardened: Remove tag patch
    (cherry picked from commit 071750d412c5ccbbbc84606c2f871881d97f067e)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17994,10 +17994,9 @@ in
         inherit (kernel) version;
       };
       kernelPatches = kernel.kernelPatches ++ [
-        kernelPatches.tag_hardened
         kernelPatches.hardened.${kernel.meta.branch}
       ];
-      modDirVersionArg = kernel.modDirVersion + (kernelPatches.hardened.${kernel.meta.branch}).extra + "-hardened";
+      modDirVersionArg = kernel.modDirVersion + (kernelPatches.hardened.${kernel.meta.branch}).extra;
       isHardened = true;
   });
 
