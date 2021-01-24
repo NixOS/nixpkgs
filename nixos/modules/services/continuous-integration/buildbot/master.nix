72691e14ec2616aa359c6395265464d7a57cd918
Fritz Otlinghaus <fritz@otlinghaus.it>
2021-01-24 13:20:35 +0100

    nixos/buildbot: add pythonPackages type

--- nixos/modules/services/continuous-integration/buildbot/master.nix
+++ nixos/modules/services/continuous-integration/buildbot/master.nix
@@ -223,6 +223,7 @@ in {
       };
 
       pythonPackages = mkOption {
+        type = types.listOf types.package;
         default = pythonPackages: with pythonPackages; [ ];
         defaultText = "pythonPackages: with pythonPackages; [ ]";
         description = "Packages to add the to the PYTHONPATH of the buildbot process.";
