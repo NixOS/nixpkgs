d98d76c2ae663e1a80a7e0f9e5bcec5da568ee3c
Milan Pässler <milan@petabyte.dev>
2021-01-25 03:40:06 +0100

    qemu: fix build when desktop file does not exist
    The qemu-user variants as used by binfmt emulation through
    `(lib.systems.elaborate lib.systems.examples.aarch64-multiplatform).emulator pkgs`
    does not install a .desktop file since qemu 5.2.0. This change allows
    the build to continue if deletion of the desktop file fails.

--- pkgs/applications/virtualization/qemu/default.nix
+++ pkgs/applications/virtualization/qemu/default.nix
@@ -143,7 +143,7 @@ stdenv.mkDerivation rec {
 
   postFixup = ''
     # the .desktop is both invalid and pointless
-    rm $out/share/applications/qemu.desktop
+    rm -f $out/share/applications/qemu.desktop
 
     # copy qemu-ga (guest agent) to separate output
     mkdir -p $ga/bin
