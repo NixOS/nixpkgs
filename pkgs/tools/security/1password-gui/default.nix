6fbb0f32ff58bda980c7c3450513304ef2c058df
Daniël de Kok <me@danieldk.eu>
2021-01-26 18:07:47 +0100

    _1password-gui: 0.9.9-3 -> 0.9.10-5
    Changes:
    https://releases.1password.com/linux/0.9/#1password-for-linux-0.9.10
    (cherry picked from commit 01776be3a8edfe5e38d179209b45048025001e1a)

--- pkgs/tools/security/1password-gui/default.nix
+++ pkgs/tools/security/1password-gui/default.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation rec {
   pname = "1password";
-  version = "0.9.9-3";
+  version = "0.9.10-5";
 
   src = fetchurl {
     url = "https://onepassword.s3.amazonaws.com/linux/appimage/${pname}-${version}.AppImage";
-    hash = "sha256-REuES0yTjTsBHapmj1YLCTIq2cIVtr7Z8ZiEgMCrwrs=";
+    hash = "sha256-eHQZjR3KUQ6SuacKwtV/5hAB0WxoJYulKU4LRn8hlmk=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
