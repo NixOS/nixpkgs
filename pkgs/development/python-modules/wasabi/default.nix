15a64b2facc1b91f4361bdd101576e8886ef834b
Daniël de Kok <me@danieldk.eu>
2021-01-26 19:01:31 +0100

    python3Packages.wasabi: 0.8.0 -> 0.8.1
    Changelog:
    https://github.com/ines/wasabi/releases/tag/v0.8.1

--- pkgs/development/python-modules/wasabi/default.nix
+++ pkgs/development/python-modules/wasabi/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "wasabi";
-  version = "0.8.0";
+  version = "0.8.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "75fec6db6193c8615d7f398ae4aa2c4ad294e6e3e81c6a6dbbbd3864ee2223c3";
+    sha256 = "6e5228a51f5550844ef5080e74759e7ecb6e344241989d018686ba968f0b4f5a";
   };
 
   checkInputs = [ pytestCheckHook ];
