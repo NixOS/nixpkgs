6f298e14a59164cc208b0b3132d829794d3aeb5e
Gabriel Gonzalez <Gabriel439@gmail.com>
2021-01-26 08:22:33 -0800

    Document why we don't enable createHome
    Co-authored-by: Cole Helbling <cole.e.helbling@outlook.com>

--- nixos/modules/services/continuous-integration/hydra/default.nix
+++ nixos/modules/services/continuous-integration/hydra/default.nix
@@ -231,6 +231,7 @@ in
     users.users.hydra =
       { description = "Hydra";
         group = "hydra";
+        # We don't enable `createHome` here because the creation of the home directory is handled by the hydra-init service below.
         home = baseDir;
         useDefaultShell = true;
         uid = config.ids.uids.hydra;
