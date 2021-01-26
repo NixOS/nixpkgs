33355cc5be73c6129559ba6b8a790948171bd60b
Manuel Bärenz <programming@manuelbaerenz.de>
2021-01-26 13:01:48 +0100

    agdaPackages: Build reverse dependencies on test

--- pkgs/build-support/agda/lib.nix
+++ pkgs/build-support/agda/lib.nix
@@ -7,4 +7,9 @@
   * interfaceFile "src/Everything.lagda.tex" == "src/Everything.agdai"
   */
   interfaceFile = agdaFile: lib.head (builtins.match ''(.*\.)l?agda(\.(md|org|rst|tex))?'' agdaFile) + "agdai";
+
+  /* Takes an arbitrary derivation and says whether it is an agda library package
+  *  that is not marked as broken.
+  */
+  isUnbrokenAgdaPackage = pkg: pkg.isAgdaDerivation or false && !pkg.meta.broken;
 }
