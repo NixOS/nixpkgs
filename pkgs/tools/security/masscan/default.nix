7055f4b42c9d83e13f563f37128759bd478c9dc8
Fabian Affolter <mail@fabian-affolter.ch>
2021-01-27 08:19:51 +0100

    masscan: 1.3.0 -> 1.3.1

--- pkgs/tools/security/masscan/default.nix
+++ pkgs/tools/security/masscan/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "masscan";
-  version = "1.3.0";
+  version = "1.3.1";
 
   src = fetchFromGitHub {
     owner  = "robertdavidgraham";
     repo   = "masscan";
     rev    = version;
-    sha256 = "04nlnficybgxa16kq9fwrrfjsbyiaps4mikfqgdr206fkqk9i05y";
+    sha256 = "sha256-gH0zOf2kl6cqws1nB3QPtaAjpvNAgbawXRx77bqJTIc=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
