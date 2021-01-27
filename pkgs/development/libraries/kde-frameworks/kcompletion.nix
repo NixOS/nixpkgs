137243869552c6f1d450398bb914021d8a7c93ba
Thomas Tuegel <ttuegel@mailbox.org>
2021-01-27 14:53:47 -0600

    kcompletion: backport upstream patch

--- pkgs/development/libraries/kde-frameworks/kcompletion.nix
+++ pkgs/development/libraries/kde-frameworks/kcompletion.nix
@@ -1,5 +1,5 @@
 {
-  mkDerivation, lib,
+  mkDerivation, lib, fetchpatch,
   extra-cmake-modules,
   kconfig, kwidgetsaddons, qtbase, qttools
 }:
@@ -7,6 +7,13 @@
 mkDerivation {
   name = "kcompletion";
   meta = { maintainers = [ lib.maintainers.ttuegel ]; };
+  patches = [
+    # https://mail.kde.org/pipermail/distributions/2021-January/000928.html
+    (fetchpatch {
+      url = "https://invent.kde.org/frameworks/kcompletion/commit/7acda936f06193e9fc85ae5cf9ccc8d65971f657.patch";
+      sha256 = "150ff506rhr5pin5363ks222vhv8qd77y5s5nyylcbdjry3ljd3n";
+    })
+  ];
   nativeBuildInputs = [ extra-cmake-modules ];
   buildInputs = [ kconfig kwidgetsaddons qttools ];
   propagatedBuildInputs = [ qtbase ];
