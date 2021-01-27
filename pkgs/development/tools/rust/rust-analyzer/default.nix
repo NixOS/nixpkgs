9afe3525f39693397ef016b00b04d333ef270351
Badi Abdul-Wahid <abdulwahidc@gmail.com>
2021-01-26 19:45:49 -0600

    rust-analyzer: 2021-01-18 -> 2021-01-25

--- pkgs/development/tools/rust/rust-analyzer/default.nix
+++ pkgs/development/tools/rust/rust-analyzer/default.nix
@@ -2,10 +2,10 @@
 
 {
   rust-analyzer-unwrapped = callPackage ./generic.nix rec {
-    rev = "2021-01-18";
+    rev = "2021-01-25";
     version = "unstable-${rev}";
-    sha256 = "sha256-eFiZdFBJZuBfwH8tqZTayNaWiq8fWUzlzBRRvdPbmW8=";
-    cargoSha256 = "sha256-rRoo0TrXa03okJ8wktzVSAn8tRO1d9kcDprotZ1hZ6w=";
+    sha256 = "1r42cnx5kplql810zc5bcpl0zzm9l8gls64h32nvd7fgad4ixapz";
+    cargoSha256 = "0ns26lddiaa1lanamcf8zawh287k4qg8n4brjpqi9s1bxbmd1kc2";
   };
 
   rust-analyzer = callPackage ./wrapper.nix {} {
