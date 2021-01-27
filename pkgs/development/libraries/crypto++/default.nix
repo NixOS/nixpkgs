e2768fdecf6505205c6c128ac552476e86ac1ae2
Justin Humm <justin.humm@posteo.de>
2021-01-27 14:21:45 +0100

    Revert "cryptopp: 8.2.0 -> 8.4.0"
    This reverts commit 490b3f2947ff2fc309a85454db9d6b690c7c39a4.
    
    See https://github.com/NixOS/nixpkgs/pull/110795#issuecomment-768279317.

--- pkgs/development/libraries/crypto++/default.nix
+++ pkgs/development/libraries/crypto++/default.nix
@@ -1,15 +1,16 @@
-{ lib, stdenv, fetchFromGitHub, }:
+{ stdenv, fetchFromGitHub, nasm, which }:
 
+with stdenv.lib;
 stdenv.mkDerivation rec {
   pname = "crypto++";
-  version = "8.4.0";
-  underscoredVersion = lib.strings.replaceStrings ["."] ["_"] version;
+  version = "8.2.0";
+  underscoredVersion = strings.replaceStrings ["."] ["_"] version;
 
   src = fetchFromGitHub {
     owner = "weidai11";
     repo = "cryptopp";
     rev = "CRYPTOPP_${underscoredVersion}";
-    sha256 = "1gwn8yh1mh41hkh6sgnhb9c3ygrdazd7645msl20i0zdvcp7f5w3";
+    sha256 = "01zrrzjn14yhkb9fzzl57vmh7ig9a6n6fka45f8za0gf7jpcq3mj";
   };
 
   postPatch = ''
@@ -18,6 +19,9 @@ stdenv.mkDerivation rec {
         --replace "ARFLAGS = -static -o" "ARFLAGS = -cru"
   '';
 
+  nativeBuildInputs = optionals stdenv.hostPlatform.isx86 [ nasm which ];
+
+  preBuild = optionalString stdenv.hostPlatform.isx86 "${stdenv.shell} rdrand-nasm.sh";
   makeFlags = [ "PREFIX=${placeholder "out"}" ];
   buildFlags = [ "shared" "libcryptopp.pc" ];
   enableParallelBuilding = true;
@@ -27,17 +31,17 @@ stdenv.mkDerivation rec {
   preInstall = "rm libcryptopp.a"; # built for checks but we don't install static lib into the nix store
   installTargets = [ "install-lib" ];
   installFlags = [ "LDCONF=true" ];
-  postInstall = lib.optionalString (!stdenv.hostPlatform.isDarwin) ''
-    ln -sr $out/lib/libcryptopp.so.${version} $out/lib/libcryptopp.so.${lib.versions.majorMinor version}
-    ln -sr $out/lib/libcryptopp.so.${version} $out/lib/libcryptopp.so.${lib.versions.major version}
+  postInstall = optionalString (!stdenv.hostPlatform.isDarwin) ''
+    ln -sr $out/lib/libcryptopp.so.${version} $out/lib/libcryptopp.so.${versions.majorMinor version}
+    ln -sr $out/lib/libcryptopp.so.${version} $out/lib/libcryptopp.so.${versions.major version}
   '';
 
   meta = {
     description = "Crypto++, a free C++ class library of cryptographic schemes";
     homepage = "https://cryptopp.com/";
     changelog = "https://raw.githubusercontent.com/weidai11/cryptopp/CRYPTOPP_${underscoredVersion}/History.txt";
-    license = with lib.licenses; [ boost publicDomain ];
-    platforms = lib.platforms.all;
-    maintainers = with lib.maintainers; [ c0bw3b ];
+    license = with licenses; [ boost publicDomain ];
+    platforms = platforms.all;
+    maintainers = with maintainers; [ c0bw3b ];
   };
 }
