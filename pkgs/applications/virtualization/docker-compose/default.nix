74df273c014a99cea454acda13f4e37b31c2d15c
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-28 00:45:55 +0000

    docker-compose: 1.28.0 -> 1.28.2

--- pkgs/applications/virtualization/docker-compose/default.nix
+++ pkgs/applications/virtualization/docker-compose/default.nix
@@ -8,12 +8,12 @@
 }:
 
 buildPythonApplication rec {
-  version = "1.28.0";
+  version = "1.28.2";
   pname = "docker-compose";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "947888fe9377b48c260d59b6511ba205655c6beb45a4b70fbce28f753aacf75a";
+    sha256 = "2f148b590414915d029dad7551f4cdf0b03a774dc9baa674480217236d260cc1";
   };
 
   # lots of networking and other fails
