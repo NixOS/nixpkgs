507e992263e0575598cafec68299e38de14b6cff
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-28 21:46:06 +0000

    argo: 2.12.5 -> 2.12.6

--- pkgs/applications/networking/cluster/argo/default.nix
+++ pkgs/applications/networking/cluster/argo/default.nix
@@ -19,13 +19,13 @@ let
 in
 buildGoModule rec {
   pname = "argo";
-  version = "2.12.5";
+  version = "2.12.6";
 
   src = fetchFromGitHub {
     owner = "argoproj";
     repo = "argo";
     rev = "v${version}";
-    sha256 = "sha256-uHec8/eGu2W3ZqXfnC/u+AOGY0nZL/GD/pZmz8aKgg0=";
+    sha256 = "sha256-28vePb/2CsDLNFYMqtsoptAaOgh4YdPSHzGTO+HLGLs=";
   };
 
   vendorSha256 = "sha256-CcSR2TZldeAbBE0yTR+4oa8mDCIx33pu9h8NE1LkRFQ=";
