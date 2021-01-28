d7b783a52e0dc6b7ddc454a538ee1294bb5ea33b
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-28 21:24:15 +0000

    aide: 0.16.2 -> 0.17

--- pkgs/tools/security/aide/default.nix
+++ pkgs/tools/security/aide/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "aide";
-  version = "0.16.2";
+  version = "0.17";
 
   src = fetchurl {
     url = "https://github.com/aide/aide/releases/download/v${version}/${pname}-${version}.tar.gz";
-    sha256 = "15xp47sz7kk1ciffw3f5xw2jg2mb2lqrbr3q6p4bkbz5dap9iy8p";
+    sha256 = "sha256-T9iNHV3ccMaYxlGeu8BcjTLD9tgTe7/e/q66r9bbhns=";
   };
 
   buildInputs = [ flex bison libmhash zlib acl attr libselinux pcre ];
