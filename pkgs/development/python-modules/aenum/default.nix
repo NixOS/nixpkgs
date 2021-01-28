608c5169fb01f44f34f82647c2d221b5a3d98fdb
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-28 01:04:09 +0000

    python37Packages.aenum: 2.2.6 -> 3.0.0

--- pkgs/development/python-modules/aenum/default.nix
+++ pkgs/development/python-modules/aenum/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "aenum";
-  version = "2.2.6";
+  version = "3.0.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "260225470b49429f5893a195a8b99c73a8d182be42bf90c37c93e7b20e44eaae";
+    sha256 = "17cd8cfed1ee4b617198c9fabbabd70ebd8f01e54ac29cd6c3a92df14bd86656";
   };
 
   # For Python 3, locale has to be set to en_US.UTF-8 for
