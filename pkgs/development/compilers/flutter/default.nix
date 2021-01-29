dc38b1567ef378464a305a99b8bc56cc1b0dabe6
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-01-29 14:50:13 -0300

    Revert "flutter: 1.22.4 -> 1.22.5"
    This reverts commit 5f72a823deeab9ef9f94bd7b208d11fab281f5d6.

--- pkgs/development/compilers/flutter/default.nix
+++ pkgs/development/compilers/flutter/default.nix
@@ -1,24 +1,17 @@
-{ callPackage, fetchurl, dart }:
-
+{ callPackage, dart }:
 let
   mkFlutter = opts: callPackage (import ./flutter.nix opts) { };
   getPatches = dir:
     let files = builtins.attrNames (builtins.readDir dir);
     in map (f: dir + ("/" + f)) files;
-  version = "1.22.5";
-  channel = "stable";
-  filename = "flutter_linux_${version}-${channel}.tar.xz";
 in
 {
   mkFlutter = mkFlutter;
   stable = mkFlutter rec {
-    inherit dart version;
+    inherit dart;
     pname = "flutter";
-    patches = getPatches ./patches;
-    src = fetchurl {
-      url = "https://storage.googleapis.com/flutter_infra/releases/${channel}/linux/${filename}";
-      sha256 = "1dv5kczcj9npf7xxlanmpc9ijnxa3ap46521cxn14c0i3y9295ja";
-    };
-    depsSha256 = "0d7vhk6axgqajy2d9ia9lc6awcnz6cc3n04r7hnh7bx4hb0jv0l1";
+    version = "1.22.4";
+    sha256Hash = "0qalgav9drqddcj8lfvl9ddf3325n953pvkmgha47lslg9sa88zw";
+    patches = getPatches ./patches/stable;
   };
 }
