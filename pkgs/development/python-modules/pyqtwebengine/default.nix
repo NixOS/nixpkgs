245718c351029ff902dd90f3d444ff95252a50f5
rnhmjoj <rnhmjoj@inventati.org>
2021-01-29 08:57:11 +0100

    pythonPackages.pyqtwebengine: 5.15.0 -> 5.15.2

--- pkgs/development/python-modules/pyqtwebengine/default.nix
+++ pkgs/development/python-modules/pyqtwebengine/default.nix
@@ -14,12 +14,12 @@ let
 
 in buildPythonPackage rec {
   pname = "PyQtWebEngine";
-  version = "5.15.0";
+  version = "5.15.2";
   format = "other";
 
   src = pythonPackages.fetchPypi {
     inherit pname version;
-    sha256 = "0xdzhl07x3mzfnr5cf4d640168vxi7fyl0fz1pvpbgs0irl14237";
+    sha256 = "0d56ak71r14w4f9r96vaj34qcn2rbln3s6ildvvyc707fjkzwwjd";
   };
 
   inherit patches;
