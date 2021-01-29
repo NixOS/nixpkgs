910d06bb9038c11b26bc7439c26afe6ef77fc1a3
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-01-29 15:03:58 -0300

    flutter: small refactorings in derivation

--- pkgs/development/compilers/flutter/default.nix
+++ pkgs/development/compilers/flutter/default.nix
@@ -1,17 +1,22 @@
-{ callPackage, dart }:
+{ callPackage, fetchurl, dart }:
 let
   mkFlutter = opts: callPackage (import ./flutter.nix opts) { };
   getPatches = dir:
     let files = builtins.attrNames (builtins.readDir dir);
     in map (f: dir + ("/" + f)) files;
+  version = "1.22.4";
+  channel = "stable";
+  filename = "flutter_linux_${version}-${channel}.tar.xz";
 in
 {
   mkFlutter = mkFlutter;
   stable = mkFlutter rec {
-    inherit dart;
+    inherit dart version;
     pname = "flutter";
-    version = "1.22.4";
-    sha256Hash = "0qalgav9drqddcj8lfvl9ddf3325n953pvkmgha47lslg9sa88zw";
-    patches = getPatches ./patches/stable;
+    src = fetchurl {
+      url = "https://storage.googleapis.com/flutter_infra/releases/${channel}/linux/${filename}";
+      sha256 = "0qalgav9drqddcj8lfvl9ddf3325n953pvkmgha47lslg9sa88zw";
+    };
+    patches = getPatches ./patches;
   };
 }
