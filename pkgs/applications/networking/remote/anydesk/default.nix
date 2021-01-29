901e699b614cb7a09903f6931e14744373f2ff62
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-29 11:00:11 +0000

    anydesk: 6.0.1 -> 6.1.0

--- pkgs/applications/networking/remote/anydesk/default.nix
+++ pkgs/applications/networking/remote/anydesk/default.nix
@@ -5,7 +5,7 @@
 
 let
   sha256 = {
-    x86_64-linux = "19751ygq1ng79aniqx91qawc0cw07cwdjdjd88azc9ww6z6nv0mp";
+    x86_64-linux = "sha256-eXgI6yzzmly5GiaKJZcYji9SMuL0eRT1vUpa5UE2eOE=";
     i386-linux   = "0dwc7v4p1dz51444zwn0kds23yi87r4h2d3isfj9xwkn90pxb7in";
   }.${stdenv.hostPlatform.system} or (throw "system ${stdenv.hostPlatform.system} not supported");
 
@@ -28,7 +28,7 @@ let
 
 in stdenv.mkDerivation rec {
   pname = "anydesk";
-  version = "6.0.1";
+  version = "6.1.0";
 
   src = fetchurl {
     urls = [
