7095a8cb9a60165303be821ddbe450b7bfb90de4
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-29 00:38:25 +0000

    dua: 2.10.9 -> 2.10.10

--- pkgs/tools/misc/dua/default.nix
+++ pkgs/tools/misc/dua/default.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "dua";
-  version = "2.10.9";
+  version = "2.10.10";
 
   src = fetchFromGitHub {
     owner = "Byron";
     repo = "dua-cli";
     rev = "v${version}";
-    sha256 = "sha256-Dx6yVgmi0CTKpBKQ5US+UpiI0d1S4vPZSbRWmmnHIMk=";
+    sha256 = "sha256-Rt+60j1+uwL6g4HGc+xdUzGK6nanGfRYbekO54iKsPE=";
     # Remove unicode file names which leads to different checksums on HFS+
     # vs. other filesystems because of unicode normalisation.
     extraPostFetch = ''
@@ -16,7 +16,7 @@ rustPlatform.buildRustPackage rec {
     '';
   };
 
-  cargoSha256 = "sha256-OFmDllEQKeByzra/XttQzdlBlahHvKsZM1ShsmV1OcM=";
+  cargoSha256 = "sha256-6L1J5RHI657Z03rMmhwEzgpQ0//dykqGi8bI2Enc3N8=";
 
   doCheck = false;
 
