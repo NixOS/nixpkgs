2b9fb043d18ecb4c6499c37c0d82e716c7645f8a
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-29 04:26:23 +0000

    ipscan: 3.7.3 -> 3.7.4

--- pkgs/tools/security/ipscan/default.nix
+++ pkgs/tools/security/ipscan/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "ipscan";
-  version = "3.7.3";
+  version = "3.7.4";
 
   src = fetchurl {
     url = "https://github.com/angryip/ipscan/releases/download/${version}/ipscan_${version}_all.deb";
-    sha256 = "18vvjqsxkz9g503k983cxdzzz6sdkv6qg3nwf8af9k34ynhhh0m7";
+    sha256 = "sha256-zNv5zoYcUDLFT6oZyK3ftqETrPxZWFGk4zBdlG8qvvU=";
   };
 
   sourceRoot = ".";
