cb05baabc0270fd44aac39516adeb53d974c4c9b
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-29 02:48:23 +0000

    gifski: 1.2.6 -> 1.3.1

--- pkgs/tools/graphics/gifski/default.nix
+++ pkgs/tools/graphics/gifski/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "gifski";
-  version = "1.2.6";
+  version = "1.3.1";
 
   src = fetchFromGitHub {
     owner = "ImageOptim";
     repo = "gifski";
     rev = version;
-    sha256 = "sha256-pnaNBjMKWfnCHG4MTLS2tJ2lrKxH6tcnvbOFZSDtPJY=";
+    sha256 = "sha256-sB8W5n3FvRAB9ygFg63NecSZgUw8FGk4pzviIbRF2vk=";
   };
 
-  cargoSha256 = "sha256-M5LEoEaWKT6nfQsnuqfyRBtDILewAxzMs7d6DvhkvFg=";
+  cargoSha256 = "sha256-0cFk1GnEJxMfY9GvQTdI5tkgtxGkH3ZQFTloo4/C+sY=";
 
   nativeBuildInputs = [ pkg-config ];
 
