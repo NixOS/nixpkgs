9102e70bb8a843c0b42d62123c96a66954962f2e
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-01-30 14:04:00 +0100

    libschrift: fix build on darwin
    On darwin unsigned long int != uint_fast32_t:
    https://opensource.apple.com/source/Libc/Libc-1439.40.11/include/_types/_uint32_t.h.auto.html
    
    Thus the build fails as SFT_Glyph is defined as unsigned long, but
    schrift.c uses uint_fast32_t everywhere instead of SFT_Glyph. We fix
    this by adjusting the int type in the header which should be fine for
    now.
    
    This patch is technically not required for Linux, but it doesn't hurt
    either as unsigned long == uint_fast32_t.

--- pkgs/development/libraries/libschrift/default.nix
+++ pkgs/development/libraries/libschrift/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch }:
 
 stdenv.mkDerivation rec {
   pname = "libschrift";
@@ -10,6 +10,14 @@ stdenv.mkDerivation rec {
     rev = "c207585486b3e78ec5506f55f5d56178f421a53c";
     sha256 = "13qrplsi2a53s84giwnzqmn0zbslyaagvjn89wsn9fd90m2v2bs1";
   };
+
+  # fix a compilation failure related to darwin integers, remove at next release
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/tomolt/libschrift/commit/1b1292f2cf4b582d66b2f6c87105997391f9fa08.patch";
+      sha256 = "076l3n28famgi74nr5bz47yn192bm76p8c8558fm5zj5c21pcfsv";
+    })
+  ];
 
   postPatch = ''
     substituteInPlace config.mk \
