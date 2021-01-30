8696e999ac0a9f735a085e57eb71cbfaf65f10f4
Kira Bruneau <kira.bruneau@gmail.com>
2021-01-30 12:51:09 -0500

    newsflash: 1.1.1 -> 1.2.0

--- pkgs/applications/networking/feedreaders/newsflash/default.nix
+++ pkgs/applications/networking/feedreaders/newsflash/default.nix
@@ -19,16 +19,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "newsflash";
-  version = "1.1.1";
+  version = "1.2.0";
 
   src = fetchFromGitLab {
     owner = "news-flash";
     repo = "news_flash_gtk";
     rev = version;
-    sha256 = "1z47h23g87dqmr9sfjl36fs5xjm2wj7z2bri9g0a4jcpwzl5awsd";
+    hash = "sha256-ilaWkwy3uL/GWUg5lTLVG9zbpGXvjSFyciY8M+vy4k4=";
   };
 
-  cargoSha256 = "0rnrdh9ganj63hf9j890yj9pahcgza95z7x020w72mbb4648hq26";
+  cargoHash = "sha256-kqhGoAMXt+KK5CJ1sKeMtattIQCcqV7YC7DD3TmaYyg=";
 
   patches = [
     # Post install tries to generate an icon cache & update the
@@ -40,7 +40,6 @@ rustPlatform.buildRustPackage rec {
   ];
 
   postPatch = ''
-    chmod +x build-aux/cargo.sh
     patchShebangs .
   '';
 
