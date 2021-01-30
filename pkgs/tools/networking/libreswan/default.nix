54b288ab35d7164ce8a1f7b47395e005c7104642
Ingo Blechschmidt <iblech@web.de>
2021-01-30 16:59:38 +0100

    libreswan: 3.32 -> 4.1

--- pkgs/tools/networking/libreswan/default.nix
+++ pkgs/tools/networking/libreswan/default.nix
@@ -15,25 +15,12 @@ assert stdenv.isLinux -> libselinux != null;
 
 stdenv.mkDerivation rec {
   pname = "libreswan";
-  version = "3.32";
+  version = "4.1";
 
   src = fetchurl {
     url = "https://download.libreswan.org/${pname}-${version}.tar.gz";
-    sha256 = "0bj3g6qwd3ir3gk6hdl9npy3k44shf56vcgjahn30qpmx3z5fsr3";
+    sha256 = "0z92v5a5j6lx2ahx8aqjnk28zk6rlmpqaj06hbavxrzdlb1l8r11";
   };
-
-  # These flags were added to compile v3.18. Try to lift them when updating.
-  NIX_CFLAGS_COMPILE = toString [ "-Wno-error=redundant-decls" "-Wno-error=format-nonliteral"
-    # these flags were added to build with gcc7
-    "-Wno-error=implicit-fallthrough"
-    "-Wno-error=format-truncation"
-    "-Wno-error=pointer-compare"
-    "-Wno-error=stringop-truncation"
-    # The following flag allows libreswan v3.32 to work with NSS 3.22, see
-    # https://github.com/libreswan/libreswan/issues/334.
-    # This flag should not be needed for libreswan v3.33 (which is not yet released).
-    "-DNSS_PKCS11_2_0_COMPAT=1"
-  ];
 
   nativeBuildInputs = [ makeWrapper pkg-config ];
   buildInputs = [ bash iproute iptables systemd coreutils gnused gawk gmp unbound bison flex pam libevent
@@ -47,9 +34,7 @@ stdenv.mkDerivation rec {
 
     # Fix systemd unit directory, and prevent the makefile from trying to reload the
     # systemd daemon or create tmpfiles
-    sed -i -e 's|UNITDIR=.*$|UNITDIR=$\{out}/etc/systemd/system/|g' \
-      -e 's|TMPFILESDIR=.*$|TMPFILESDIR=$\{out}/tmpfiles.d/|g' \
-      -e 's|systemctl|true|g' \
+    sed -i -e 's|systemctl|true|g' \
       -e 's|systemd-tmpfiles|true|g' \
       initsystems/systemd/Makefile
 
@@ -64,6 +49,7 @@ stdenv.mkDerivation rec {
   preBuild = "export INC_USRLOCAL=\${out}";
 
   makeFlags = [
+    "UNITDIR=$(out)/etc/systemd/system/" "TMPFILESDIR=$(out)/tmpfiles.d/"
     "INITSYSTEM=systemd"
     (if docs then "all" else "base")
   ];
