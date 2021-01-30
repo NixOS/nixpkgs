03ab4596f84ecd0151013ae1cac0788b9c96363a
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-30 11:25:14 +0000

    fastJson: 0.99.8 -> 0.99.9

--- pkgs/development/libraries/fastjson/default.nix
+++ pkgs/development/libraries/fastjson/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchFromGitHub, libtool, autoconf, automake }:
 
 stdenv.mkDerivation rec {
-  version = "0.99.8";
+  version = "0.99.9";
   pname = "fastjson";
   src = fetchFromGitHub {
     repo = "libfastjson";
     owner = "rsyslog";
     rev = "v${version}";
-    sha256 = "0qhs0g9slj3p0v2z4s3cnsx44msrlb4k78ljg7714qiziqbrbwyl";
+    sha256 = "sha256-2LyBdJR0dV1CElcGfrlmNwX52lVtx9X/Z4h/1XFjOIs=";
   };
 
   buildInputs = [ autoconf automake libtool ];
