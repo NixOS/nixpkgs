47e82e173347ce2d80ee582a309a71b4a060e621
AndersonTorres <torres.anderson.85@protonmail.com>
2021-01-31 01:37:31 -0300

    xfce4-systemload-plugin: 1.2.3 -> 1.2.4

--- pkgs/desktops/xfce/panel-plugins/xfce4-systemload-plugin/default.nix
+++ pkgs/desktops/xfce/panel-plugins/xfce4-systemload-plugin/default.nix
@@ -1,16 +1,24 @@
-{ lib, stdenv, fetchurl, pkg-config, intltool, libxfce4util, xfce4-panel, libxfce4ui, gtk3, xfce }:
+{ lib
+, stdenv
+, fetchurl
+, pkg-config
+, intltool
+, libxfce4util
+, xfce4-panel
+, libxfce4ui
+, gtk3
+, xfce
+}:
 
 let
   category = "panel-plugins";
-in
-
-stdenv.mkDerivation rec {
+in stdenv.mkDerivation rec {
   pname  = "xfce4-systemload-plugin";
-  version = "1.2.3";
+  version = "1.2.4";
 
   src = fetchurl {
     url = "mirror://xfce/src/${category}/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.bz2";
-    sha256 = "0x87a8h5l3ashz1ksfaxcpn9a392jzlsbx5n5pga8g90fp2hf905";
+    sha256 = "BTG435I8ujvo0GTLi2OLlU33SRXlpEciiZlReEd4mDU=";
   };
 
   nativeBuildInputs = [
@@ -36,6 +44,6 @@ stdenv.mkDerivation rec {
     description = "System load plugin for Xfce panel";
     license = licenses.bsd2;
     platforms = platforms.linux;
-    maintainers = [ maintainers.AndersonTorres ];
+    maintainers = [ ];
   };
 }
