1e01f697c2a8996765e3c6a8ebe08f8cab1c65d7
Robert Scott <code@humanleg.org.uk>
2021-01-31 16:30:23 +0000

    cloudfoundry-cli: relocate to pkgs/applications/networking/cluster

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11857,7 +11857,7 @@ in
 
   cloudcompare = libsForQt5.callPackage ../applications/graphics/cloudcompare {};
 
-  cloudfoundry-cli = callPackage ../development/tools/cloudfoundry-cli { };
+  cloudfoundry-cli = callPackage ../applications/networking/cluster/cloudfoundry-cli { };
 
   clpm = callPackage ../development/tools/clpm {};
 
