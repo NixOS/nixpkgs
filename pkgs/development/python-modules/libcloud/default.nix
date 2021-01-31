bf93bc8960d8e5080e4303f819b50995a6ad8aa7
Mario Rodas <marsam@users.noreply.github.com>
2021-01-30 20:51:10 -0500

    python37Packages.libcloud: update meta

--- pkgs/development/python-modules/libcloud/default.nix
+++ pkgs/development/python-modules/libcloud/default.nix
@@ -30,7 +30,8 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "A unified interface to many cloud providers";
-    homepage = "http://incubator.apache.org/libcloud/";
+    homepage = "https://libcloud.apache.org/";
+    changelog = "https://github.com/apache/libcloud/blob/v${version}/CHANGES.rst";
     license = licenses.asl20;
   };
 
