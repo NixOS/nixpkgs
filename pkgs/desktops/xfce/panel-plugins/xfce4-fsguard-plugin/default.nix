e1adcd80d58474125dff2de00f3b12447467bfc0
AndersonTorres <torres.anderson.85@protonmail.com>
2021-01-31 01:32:05 -0300

    xfce4-fsguard-plugin: 1.1.1 -> 1.1.2

--- pkgs/desktops/xfce/panel-plugins/xfce4-fsguard-plugin/default.nix
+++ pkgs/desktops/xfce/panel-plugins/xfce4-fsguard-plugin/default.nix
@@ -1,16 +1,25 @@
-{ lib, stdenv, fetchurl, pkg-config, intltool, libxfce4util, xfce4-panel, libxfce4ui, xfconf, gtk3, xfce }:
+{ lib
+, stdenv
+, fetchurl
+, pkg-config
+, intltool
+, libxfce4util
+, xfce4-panel
+, libxfce4ui
+, xfconf
+, gtk3
+, xfce
+}:
 
 let
   category = "panel-plugins";
-in
-
-stdenv.mkDerivation rec {
+in stdenv.mkDerivation rec {
   pname  = "xfce4-fsguard-plugin";
-  version = "1.1.1";
+  version = "1.1.2";
 
   src = fetchurl {
     url = "mirror://xfce/src/${category}/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.bz2";
-    sha256 = "05nmfkrmifm76bsywqmbjd1qdvzagv5cbvnwbkb57156j055vl6n";
+    sha256 = "Z9jmohmnEXxZaTrbxZw5puujHj8YpUmRie9O8otVQQU=";
   };
 
   nativeBuildInputs = [
@@ -37,6 +46,6 @@ stdenv.mkDerivation rec {
     description = "Filesystem usage monitor plugin for the Xfce panel";
     license = licenses.bsd2;
     platforms = platforms.linux;
-    maintainers = [ maintainers.AndersonTorres ];
+    maintainers = [ ];
   };
 }
