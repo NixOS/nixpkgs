289b6303e593be94e0c499e02919833e6bb837cc
R. RyanTM <ryantm-bot@ryantm.com>
2021-01-31 05:27:07 +0000

    python37Packages.flask_migrate: 2.5.3 -> 2.6.0

--- pkgs/development/python-modules/flask-migrate/default.nix
+++ pkgs/development/python-modules/flask-migrate/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "Flask-Migrate";
-  version = "2.5.3";
+  version = "2.6.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a69d508c2e09d289f6e55a417b3b8c7bfe70e640f53d2d9deb0d056a384f37ee";
+    sha256 = "8626af845e6071ef80c70b0dc16d373f761c981f0ad61bb143a529cab649e725";
   };
 
   checkInputs = [ flask_script ] ++ lib.optional isPy3k glibcLocales;
