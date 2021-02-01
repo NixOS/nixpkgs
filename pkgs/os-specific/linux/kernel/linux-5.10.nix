ab661e6635bfa694cbf3109e3d8955ee3c66de78
Tim Steinbach <tim@nequissimus.com>
2021-02-01 10:30:50 -0500

    linux: 5.10.11 -> 5.10.12

--- pkgs/os-specific/linux/kernel/linux-5.10.nix
+++ pkgs/os-specific/linux/kernel/linux-5.10.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.10.11";
+  version = "5.10.12";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "03viivkmz9kvn8vak26l80p7fr8jbqisa0y605bi9i8gn1b2pvq2";
+    sha256 = "1an460q3affd7gmd6fqv8g37j3z2fnmq19iy677k8kxb2wl4yi8x";
   };
 } // (args.argsOverride or {}))
