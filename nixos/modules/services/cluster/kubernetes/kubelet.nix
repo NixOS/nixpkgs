bb6e51107b480aa2e6607659a7a6ad30e1d5377c
Jörg Thalheim <joerg@thalheim.io>
2021-02-01 17:40:27 +0100

    kubernetes: fix startup by setting systemd as cgroup driver

--- nixos/modules/services/cluster/kubernetes/kubelet.nix
+++ nixos/modules/services/cluster/kubernetes/kubelet.nix
@@ -265,6 +265,7 @@ in
             --authentication-token-webhook \
             --authentication-token-webhook-cache-ttl="10s" \
             --authorization-mode=Webhook \
+            --cgroup-driver systemd \
             ${optionalString (cfg.clientCaFile != null)
               "--client-ca-file=${cfg.clientCaFile}"} \
             ${optionalString (cfg.clusterDns != "")
