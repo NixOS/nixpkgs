507b96eb3090963c394b8c4900dd04eef91a389d
Ivan Babrou <github@ivan.computer>
2021-01-31 17:11:03 -0800

    universal-ctags: downgrade to p5.9.20201206.0
    Newer versions fail on aarch64-linux:
    
    ```
      checking for aarch64-unknown-linux-gnu-gcc... aarch64-unknown-linux-gnu-gcc
      checking whether the compiler supports GNU C... yes
      checking whether aarch64-unknown-linux-gnu-gcc accepts -g... yes
      checking for aarch64-unknown-linux-gnu-gcc option to enable C11 features... (cached) none needed
      checking whether aarch64-unknown-linux-gnu-gcc understands -c and -o together... yes
      checking dependency style of aarch64-unknown-linux-gnu-gcc... none
      checking whether the C compiler works... no
      configure: error: in `/build/source':
      configure: error: C compiler cannot create executables
      See `config.log' for more details
    ```
    
    The following PR is likely culprit:
    
    * https://github.com/universal-ctags/ctags/pull/2747

--- pkgs/development/tools/misc/universal-ctags/default.nix
+++ pkgs/development/tools/misc/universal-ctags/default.nix
@@ -1,17 +1,17 @@
-{ lib, stdenv, fetchFromGitHub, autoreconfHook, pkg-config, perl, python3Packages, libiconv, jansson }:
+{ lib, stdenv, fetchFromGitHub, autoreconfHook, coreutils, pkg-config, perl, python3Packages, libiconv, jansson }:
 
 stdenv.mkDerivation rec {
   pname = "universal-ctags";
-  version = "5.9.20210124.0";
+  version = "5.9.20201206.0";
 
   src = fetchFromGitHub {
     owner = "universal-ctags";
     repo = "ctags";
     rev = "p${version}";
-    sha256 = "082yq629s2p4fc5kmjhmzx9q9grc32hwfr1x55ydvhi4x68dbx6d";
+    sha256 = "0w10zjyz46sjm6ypxmq550dkr84hvc4phm4vm9j53jp5s19x5q19";
   };
 
-  nativeBuildInputs = [ autoreconfHook pkg-config python3Packages.docutils ];
+  nativeBuildInputs = [ autoreconfHook coreutils pkg-config python3Packages.docutils ];
   buildInputs = [ jansson ] ++ lib.optional stdenv.isDarwin libiconv;
 
   # to generate makefile.in
@@ -25,6 +25,9 @@ stdenv.mkDerivation rec {
     # Remove source of non-determinism
     substituteInPlace main/options.c \
       --replace "printf (\"  Compiled: %s, %s\n\", __DATE__, __TIME__);" ""
+
+    substituteInPlace Tmain/utils.sh \
+      --replace /bin/echo ${coreutils}/bin/echo
   '';
 
   postConfigure = ''
