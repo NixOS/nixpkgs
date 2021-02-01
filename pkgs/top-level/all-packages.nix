f52c8862c9b1fc524bd1401185944de6274db43b
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-02-01 15:24:14 +0100

    mfc9140cdnlpr: init at 1.1.2-1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31132,6 +31132,8 @@ in
   mfcl8690cdwcupswrapper = callPackage ../misc/cups/drivers/mfcl8690cdwcupswrapper { };
   mfcl8690cdwlpr = callPackage ../misc/cups/drivers/mfcl8690cdwlpr { };
 
+  mfc9140cdnlpr = callPackage ../misc/cups/drivers/mfc9140cdnlpr { };
+
   samsung-unified-linux-driver_1_00_36 = callPackage ../misc/cups/drivers/samsung/1.00.36/default.nix { };
   samsung-unified-linux-driver_1_00_37 = callPackage ../misc/cups/drivers/samsung/1.00.37.nix { };
   samsung-unified-linux-driver_4_00_39 = callPackage ../misc/cups/drivers/samsung/4.00.39 { };
