eeab23f7aff1f4ed8afa50ece46ba96852faee6d
Matthias Beyer <mail@beyermatthias.de>
2021-02-01 20:28:54 +0100

    fixup! wolf-spectrum: init at 1.0.0

--- pkgs/applications/audio/wolf-spectrum/default.nix
+++ pkgs/applications/audio/wolf-spectrum/default.nix
@@ -41,7 +41,6 @@ stdenv.mkDerivation rec {
     mkdir -p $out/lib/vst
     mkdir -p $out/bin/
     cp -r bin/wolf-spectrum.lv2    $out/lib/lv2/
-    #cp -r bin/wolf-spectrum-dssi*  $out/lib/dssi/
     cp -r bin/wolf-spectrum-vst.so $out/lib/vst/
     cp -r bin/wolf-spectrum        $out/bin/
   '';
@@ -51,6 +50,6 @@ stdenv.mkDerivation rec {
     description = "Real-time 2D spectrogram plugin (LV2, VST and Jack)";
     license = licenses.gpl3Plus;
     maintainers = [ maintainers.matthiasbeyer ];
-    platforms = [ "i686-linux" "x86_64-linux" ];
+    platforms = platforms.linux;
   };
 }
