68afbf9d6385032be9fc368dbfa4ad4ba2cc45e0
David Arnold <dar@xoe.solutions>
2021-02-02 17:40:46 -0500

    installer: add deprecation warning about sd-card file move

--- nixos/modules/installer/cd-dvd/sd-image-raspberrypi4.nix
+++ nixos/modules/installer/cd-dvd/sd-image-raspberrypi4.nix
@@ -1,5 +1,14 @@
+{ config, ... }:
 {
   imports = [
     ../sd-card/sd-image-raspberrypi4-installer.nix
   ];
+  config = {
+    warnings = [
+      ''
+      .../cd-dvd/sd-image-raspberrypi4.nix is deprecated and will eventually be removed.
+      Please switch to .../sd-card/sd-image-raspberrypi4-installer.nix, instead.
+      ''
+    ];
+  };
 }
