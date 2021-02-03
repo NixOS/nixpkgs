d672ac1182d9b81cf3988098f8c0de3f73ce3134
Fabian Affolter <mail@fabian-affolter.ch>
2021-02-03 10:12:40 +0100

    python3Packages.pysyncobj: init at 0.3.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5989,6 +5989,8 @@ in {
 
   pysychonaut = callPackage ../development/python-modules/pysychonaut { };
 
+  pysyncobj = callPackage ../development/python-modules/pysyncobj { };
+
   pytabix = callPackage ../development/python-modules/pytabix { };
 
   pytado = callPackage ../development/python-modules/pytado { };
