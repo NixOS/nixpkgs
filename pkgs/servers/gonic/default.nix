44a42206702bc96fd704a8259d0a2a10cedf3c59
Alex McGrath <amk@amk.ie>
2021-02-03 23:49:16 +0000

    gonic: 0.11.0 -> 0.12.0

--- pkgs/servers/gonic/default.nix
+++ pkgs/servers/gonic/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildGoPackage, fetchFromGitHub
+{ lib, buildGoModule, fetchFromGitHub
 , pkg-config, taglib, alsaLib
 
 # Disable on-the-fly transcoding,
@@ -7,48 +7,26 @@
 # to the original file, but if transcoding is configured
 # that takes a while. So best to disable all transcoding
 # in the configuration if you disable transcodingSupport.
-, transcodingSupport ? true, ffmpeg
-
-# udpater
-, writers, vgo2nix }:
+, transcodingSupport ? true, ffmpeg }:
 
 assert transcodingSupport -> ffmpeg != null;
-
 let
-  # update these, then run `updateScript` to update dependencies
-  version = "0.11.0";
-  rev = "056fb54a703ef5b5194ce112cbbdd8fb53dbb1ea";
-  sha256 = "0hd794wrz29nh89lfnq67w1rc23sg085rqf1agwlgpqycns2djl9";
-
+  pname = "gonic-${version}";
+  version = "0.12.0";
+in buildGoModule {
+  inherit pname version;
   src = fetchFromGitHub {
     owner = "sentriz";
     repo = "gonic";
-    inherit rev sha256;
+    rev = "v${version}";
+    sha256 = "069hcsvx5rbdgxmkli0gkhgrvl47xb26vkfb7rr6dp06bx5nfhax";
   };
-
-in
-buildGoPackage {
-  pname = "gonic-${version}";
-  inherit version src;
-  goPackagePath = "go.senan.xyz/gonic";
-  goDeps = ./deps.nix;
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ taglib alsaLib ];
 
-  postPatch = lib.optionalString transcodingSupport ''
-    substituteInPlace \
-       server/encode/encode.go \
-      --replace \
-        'ffmpegPath = "/usr/bin/ffmpeg"' \
-        'ffmpegPath = "${ffmpeg}/bin/ffmpeg"' \
-  '';
-
-  passthru.updateScript = writers.writeDash "update-gonic" ''
-    ${vgo2nix}/bin/vgo2nix \
-      -dir ${src} \
-      -outfile ${lib.escapeShellArg (toString ./deps.nix)}
-  '';
+  vendorSha256 = "0inxlqxnkglz4j14jav8080718a80nqdcl866lkql8r6zcxb4fm9";
+  subPackages = [ "cmd/gonic" ];
 
   meta = {
     homepage = "https://github.com/sentriz/gonic";
