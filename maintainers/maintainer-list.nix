d78ddc828e0c99563ba6c997bf9fa8feefa2c9b0
Winter <winter@winter.cafe>
2021-02-02 19:03:29 -0500

    maintainers: add winterqt

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9940,6 +9940,12 @@
     githubId = 6016963;
     name = "Patrick Winter";
   };
+  winterqt = {
+    email = "nixos@winter.cafe";
+    github = "winterqt";
+    githubId = 78392041;
+    name = "Winter";
+  };
   wishfort36 = {
     email = "42300264+wishfort36@users.noreply.github.com";
     github = "wishfort36";
