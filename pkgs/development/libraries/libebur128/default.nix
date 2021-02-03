4dd63848b1d70cab375149a03ffb810f43393f34
R. RyanTM <ryantm-bot@ryantm.com>
2021-02-03 15:57:37 +0000

    libebur128: 1.2.4 -> 1.2.5

--- pkgs/development/libraries/libebur128/default.nix
+++ pkgs/development/libraries/libebur128/default.nix
@@ -1,14 +1,14 @@
 { lib, stdenv, fetchFromGitHub, cmake, speexdsp, pkg-config }:
 
 stdenv.mkDerivation rec {
-  version = "1.2.4";
+  version = "1.2.5";
   pname = "libebur128";
 
   src = fetchFromGitHub {
     owner = "jiixyj";
     repo = "libebur128";
     rev = "v${version}";
-    sha256 = "0n81rnm8dm1zmibkr2v3q79rsd609y0dbbsrbay18njcjva88p0g";
+    sha256 = "sha256-B6MOSbLfPvadXtXHSvxZCIpAH1Bnj6sItYRp+xH5HDA=";
   };
 
   nativeBuildInputs = [ cmake pkg-config ];
