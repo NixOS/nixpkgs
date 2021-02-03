f3522701a70a0eef15ef415420505b334ae1fd17
R. RyanTM <ryantm-bot@ryantm.com>
2021-02-03 13:21:54 +0000

    folly: 2021.01.25.00 -> 2021.02.01.00

--- pkgs/development/libraries/folly/default.nix
+++ pkgs/development/libraries/folly/default.nix
@@ -19,13 +19,13 @@
 
 stdenv.mkDerivation (rec {
   pname = "folly";
-  version = "2021.01.25.00";
+  version = "2021.02.01.00";
 
   src = fetchFromGitHub {
     owner = "facebook";
     repo = "folly";
     rev = "v${version}";
-    sha256 = "sha256-n2cpXdjPZYNjvSHBZFFn3JFwqYCFWc8qSKwet80LtJE=";
+    sha256 = "sha256-AnULyCtF07ldBNPcM4gaTmpSdbLnHHbIBhq+8l72cMw=";
   };
 
   nativeBuildInputs = [
