2edd63b82f4e77a0bdb3171f3dec99fa12c77a16
Tim Steinbach <tim@nequissimus.com>
2021-02-04 10:08:50 -0500

    python3Packages.boto3: 1.17.0 -> 1.17.1

--- pkgs/development/python-modules/boto3/default.nix
+++ pkgs/development/python-modules/boto3/default.nix
@@ -13,11 +13,11 @@
 
 buildPythonPackage rec {
   pname =  "boto3";
-  version = "1.17.0"; # N.B: if you change this, change botocore too
+  version = "1.17.1"; # N.B: if you change this, change botocore too
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-Kjm9Xl8tUM6SZ9aCzJJ1D4dxOZZlAh9H6A+cjS+4EqY=";
+    sha256 = "sha256-WKRA9MlqHy8HZXfHCFI3hUum20GnyR5KM/swBo+NRpI=";
   };
 
   propagatedBuildInputs = [ botocore jmespath s3transfer ] ++ lib.optionals (!isPy3k) [ futures ];
