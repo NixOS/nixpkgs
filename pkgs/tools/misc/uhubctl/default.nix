07cc794f577a30fa4012e9b3c2c532d2a87bf697
Pavol Rusnak <pavol@rusnak.io>
2021-02-04 17:29:29 +0100

    uhubctl: cleanup src.rev

--- pkgs/tools/misc/uhubctl/default.nix
+++ pkgs/tools/misc/uhubctl/default.nix
@@ -1,4 +1,5 @@
-{ lib, stdenv
+{ lib
+, stdenv
 , fetchFromGitHub
 , libusb1
 }:
@@ -10,13 +11,14 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "mvp";
     repo = "uhubctl";
-    rev = "refs/tags/v${version}";
+    rev = "v${version}";
     sha256 = "1wxsiygw6gwv1h90yassnxylkyi2dfz7y59qkmb7rs8a8javj7nv";
   };
 
   buildInputs = [ libusb1 ];
 
   installFlags = [ "prefix=${placeholder "out"}" ];
+
   meta = with lib; {
     homepage = "https://github.com/mvp/uhubctl";
     description = "Utility to control USB power per-port on smart USB hubs";
