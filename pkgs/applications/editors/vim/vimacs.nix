276ce86a108562f0bca3c779993a29b0419c0b64
Lily Ballard <lily@sb.org>
2021-02-04 13:27:38 -0800

    vimacs: don't assume vimPackage has a version attribute
    `vimPackage` may not have a version attribute if it was specified using
    a full name instead, such as with a `buildEnv` call. This can happen
    right now on Darwin when `vimPackage` is `macvim.configure {…}`.

--- pkgs/applications/editors/vim/vimacs.nix
+++ pkgs/applications/editors/vim/vimacs.nix
@@ -4,7 +4,7 @@
 
 stdenv.mkDerivation rec {
   pname = "vimacs";
-  version = vimPackage.version;
+  version = lib.getVersion vimPackage;
   vimPackage = if useMacvim then macvim else vim_configurable;
 
   buildInputs = [ vimPackage vimPlugins.vimacs ];
