0761e651c469ff00394945add40827a9c64bbb79
ajs124 <git@ajs124.de>
2021-02-06 18:30:45 +0100

    php74: 7.4.14 -> 7.4.15
    https://www.php.net/ChangeLog-7.php#7.4.15
    
    Fixes CVE-2021-21702 and a some regular bugs

--- pkgs/development/interpreters/php/7.4.nix
+++ pkgs/development/interpreters/php/7.4.nix
@@ -4,8 +4,8 @@ let
   generic = (import ./generic.nix) _args;
 
   base = callPackage generic (_args // {
-    version = "7.4.14";
-    sha256 = "1xm1s2w9fsd8q7kjbpqw8s4bs7ggziwws23m0ykkmvmd0l3cm2b8";
+    version = "7.4.15";
+    sha256 = "0mvp7b16sy9j36v9v1mhixwz16hi8mhax7rwpqy3sv24jc1bxmqv";
   });
 
 in base.withExtensions ({ all, ... }: with all; ([
