c6b99fddcc88fc391b4b701e91d8121f16a899b1
Daniel Wilson-Thomas <daniel@mojotech.com>
2021-02-06 01:10:36 -0500

    nixos/dunst: Add defaults and examples to config options
    I made the choice to merge provided options into the default values
    because the default values are so large.
    
    Default values taken from example dunstrc at dunst 1.5.0.

--- nixos/modules/services/x11/dunst.nix
+++ nixos/modules/services/x11/dunst.nix
@@ -8,6 +8,83 @@ let
     "global" "experimental" "frame" "shortcuts"
     "urgency_low" "urgency_normal" "urgency_critical"
   ];
+  defaultGlobalConfig = {
+    monitor = 0;
+    follow = "mouse";
+    geometry = "300x5-30+20";
+    progress_bar = true;
+    progress_bar_height = 10;
+    progress_bar_frame_width = 1;
+    progress_bar_min_width = 150;
+    progress_bar_max_width = 300;
+    indicate_hidden = true;
+    shrink = false;
+    transparency = 0;
+    notification_height = 0;
+    separator_height = 2;
+    padding = 8;
+    horizontal_padding = 8;
+    text_icon_padding = 0;
+    frame_width = 3;
+    frame_color = "#aaaaaa";
+    separator_color = "frame";
+    sort = true;
+    idle_threshold = 120;
+    font = "Monospace 8";
+    line_height = 0;
+    markup = "full";
+    format = "<b>%s</b>\\n%b";
+    alignment = "left";
+    vertical_alignment = "center";
+    show_age_threshold = 60;
+    word_wrap = true;
+    ellipsize = "middle";
+    ignore_newline = false;
+    stack_duplicates = true;
+    hide_duplicate_count = false;
+    show_indicators = true;
+    icon_position = "left";
+    min_icon_size = 0;
+    max_icon_size = 32;
+    icon_path = "";
+    sticky_history = true;
+    history_length = 20;
+    dmenu = "${pkgs.dmenu}/bin/dmenu -p dunst:";
+    browser = "${pkgs.firefox}/bin/firefox -new-tab";
+    always_run_script = true;
+    title = "Dunst";
+    class = "Dunst";
+    startup_notification = false;
+    verbosity = "mesg";
+    corner_radius = 0;
+    ignore_dbusclose = false;
+    force_xwayland = false;
+    force_xinerama = false;
+    mouse_left_click = "close_current";
+    mouse_middle_click = "close_current";
+    mouse_right_click = "close_all";
+  };
+  defaultShortcutConfig = {};
+  defaultFrameConfig = {};
+  defaultUrgencyConfigLow = {
+    background = "#222222";
+    foreground = "#888888";
+    timeout = 10;
+  };
+  defaultUrgencyConfigNormal = {
+    background = "#285577";
+    foreground = "#ffffff";
+    timeout = 10;
+  };
+  defaultUrgencyConfigCritical = {
+    background = "#900000";
+    foreground = "#ffffff";
+    frame_color = "#ff0000";
+    timeout = 0;
+  };
+  defaultExperimentalConfig = {
+    per_monitor_dpi = false;
+  };
 in {
 
 options.services.dunst = {
@@ -35,57 +112,101 @@ options.services.dunst = {
 
   globalConfig = mkOption {
     type = with types; attrsOf (oneOf [bool str int]);
-    default = {};
+    default = defaultGlobalConfig;
     description = ''
-      The global configuration section for dunst.
+      The global configuration section for dunst. Values will be merged into the defaults.
+    '';
+    example = ''
+      {
+        font = "Monospace 12";
+        sort = true;
+        idle_threshold = 0;
+      }
     '';
   };
 
   shortcutConfig = mkOption {
     type = with types; attrsOf (oneOf [bool str int]);
-    default = {};
+    default = defaultShortcutConfig;
     description = ''
-      The shortcut configuration for dunst.
+      The shortcut configuration for dunst. Values will be merged into the defaults.
+    '';
+    example = ''
+      {
+        close = "mod3+h";
+      }
     '';
   };
 
   frameConfig = mkOption {
     type = with types; attrsOf (oneOf [bool str int]);
-    default = {};
+    default = defaultFrameConfig;
     description = ''
-      The frame configuration for dunst.
+      The frame configuration for dunst. Values will be merged into the defaults.
+    '';
+    example = ''
+      {
+        width = 0;
+        color = "#000000";
+      }
     '';
   };
 
   urgencyConfig = {
     low = mkOption {
       type = with types; attrsOf (oneOf [bool str int]);
-      default = {};
+      default = defaultUrgencyConfigLow;
       description = ''
-        The low urgency section of the dunst configuration.
+        The low urgency section of the dunst configuration. Values will be merged into the defaults.
+      '';
+      example = ''
+        {
+          background = "#ffffff";
+          foreground = "#888888";
+          timeout = 60;
+        }
       '';
     };
     normal = mkOption {
       type = with types; attrsOf (oneOf [bool str int]);
-      default = {};
+      default = defaultUrgencyConfigNormal;
       description = ''
-        The normal urgency section of the dunst configuration.
+        The normal urgency section of the dunst configuration. Values will be merged into the defaults.
+      '';
+      example = ''
+        {
+          background = "#ffffff";
+          foreground = "#111111";
+          timeout = 60;
+        }
       '';
     };
     critical = mkOption {
       type = with types; attrsOf (oneOf [bool str int]);
-      default = {};
+      default = defaultUrgencyConfigCritical;
       description = ''
-        The critical urgency section of the dunst configuration.
+        The critical urgency section of the dunst configuration. Values will be merged into the defaults.
+      '';
+      example = ''
+        {
+          background = "#ff0000";
+          foreground = "#111111";
+          timeout = 0;
+        }
       '';
     };
   };
 
   experimentalConfig = mkOption {
     type = with types; attrsOf (oneOf [bool str int]);
-    default = {};
+    default = defaultExperimentalConfig;
     description = ''
-      The experimental configuration for dunst.
+      The experimental configuration for dunst. Values will be merged into the defaults.
+    '';
+    example = ''
+      {
+        per_monitor_dpi = true;
+      }
     '';
   };
 
@@ -132,13 +253,13 @@ config =
       } "=";
     } allOptions;
     allOptions = {
-      global = cfg.globalConfig;
-      shortcut = cfg.shortcutConfig;
-      frameConfig = cfg.frameConfig;
-      experimental = cfg.experimentalConfig;
-      urgency_normal = cfg.urgencyConfig.normal;
-      urgency_low = cfg.urgencyConfig.low;
-      urgency_critical = cfg.urgencyConfig.critical;
+      global = defaultGlobalConfig // cfg.globalConfig;
+      shortcut = defaultShortcutConfig // cfg.shortcutConfig;
+      frameConfig = defaultFrameConfig // cfg.frameConfig;
+      experimental = defaultExperimentalConfig // cfg.experimentalConfig;
+      urgency_normal = defaultUrgencyConfigNormal // cfg.urgencyConfig.normal;
+      urgency_low = defaultUrgencyConfigLow // cfg.urgencyConfig.low;
+      urgency_critical = defaultUrgencyConfigCritical // cfg.urgencyConfig.critical;
     } // cfg.rules;
 
     iconPath = concatStringsSep ":" cfg.iconDirs;
