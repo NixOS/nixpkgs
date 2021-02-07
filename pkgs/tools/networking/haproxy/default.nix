2e3cf03ab87412593719ae4b92dee301c2de60f7
R. RyanTM <ryantm-bot@ryantm.com>
2021-02-07 12:02:43 +0000

    haproxy: 2.3.4 -> 2.3.5

--- pkgs/tools/networking/haproxy/default.nix
+++ pkgs/tools/networking/haproxy/default.nix
@@ -11,11 +11,11 @@ assert usePcre -> pcre != null;
 
 stdenv.mkDerivation rec {
   pname = "haproxy";
-  version = "2.3.4";
+  version = "2.3.5";
 
   src = fetchurl {
     url = "https://www.haproxy.org/download/${lib.versions.majorMinor version}/src/${pname}-${version}.tar.gz";
-    sha256 = "sha256-YBSM3+3WsZxAHbzXXM12pTwgvHbEkDK6Mq+YoKXEle0=";
+    sha256 = "sha256-eSRTlTC79VWCnH9Yhr4Lf8+NnI/+CGe3AQvrZwq/vks=";
   };
 
   buildInputs = [ openssl zlib ]
