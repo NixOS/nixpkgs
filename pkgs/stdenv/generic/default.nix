c7942b0f8b3cf01b17794a53e883afe30020b8c7
Cole Helbling <cole.e.helbling@outlook.com>
2021-02-06 20:17:12 -0800

    stdenv/generic: allowAliases should default to true if unset
    Since the deprecation is fairly recent, we should warn by default.
    
    Also fix the wording of the comment: stdenv.lib will be removed for the 21.11
    release, not just deprecated (as it already is deprecated).

--- pkgs/stdenv/generic/default.nix
+++ pkgs/stdenv/generic/default.nix
@@ -152,8 +152,8 @@ let
         inherit lib config stdenv;
       }) mkDerivation;
 
-      # Slated for deprecation in 21.11
-      lib = if config.allowAliases or false then builtins.trace
+      # Slated for removal in 21.11
+      lib = if config.allowAliases or true then builtins.trace
         ( "Warning: `stdenv.lib` is deprecated and will be removed in the next release."
          + " Please use `pkgs.lib` instead."
          + " For more information see https://github.com/NixOS/nixpkgs/issues/108938")
