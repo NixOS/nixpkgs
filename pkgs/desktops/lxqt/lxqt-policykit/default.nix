591f129c6d19f5a65ed66b351597b6af394b7ab7
AndersonTorres <torres.anderson.85@protonmail.com>
2021-02-08 11:41:52 -0300

    lxqt: cosmetical changes
    Updated the GPL and LGPL licenses.

--- pkgs/desktops/lxqt/lxqt-policykit/default.nix
+++ pkgs/desktops/lxqt/lxqt-policykit/default.nix
@@ -48,9 +48,9 @@ mkDerivation rec {
   passthru.updateScript = lxqtUpdateScript { inherit pname version src; };
 
   meta = with lib; {
-    description = "The LXQt PolicyKit agent";
     homepage = "https://github.com/lxqt/lxqt-policykit";
-    license = licenses.lgpl21;
+    description = "The LXQt PolicyKit agent";
+    license = licenses.lgpl21Plus;
     platforms = platforms.linux;
     maintainers = with maintainers; [ romildo ];
   };
