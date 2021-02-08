76a29c2c79a078fd3debece84b6a72c7606211bd
Ivan Babrou <github@ivan.computer>
2021-02-08 13:42:49 -0800

    qemu: only apply autoPatchelfHook on linux
    It fails on darwin due to missing `patchelf` and the missing ELFs:
    
    ```
    /nix/store/...-auto-patchelf-hook/nix-support/setup-hook: line 220: -l: command not found
    ```
    
    (cherry picked from commit 251add14cf97aa893e9934c225ee7412861bc655)

--- pkgs/applications/virtualization/qemu/default.nix
+++ pkgs/applications/virtualization/qemu/default.nix
@@ -47,8 +47,9 @@ stdenv.mkDerivation rec {
     sha256 = "1g0pvx4qbirpcn9mni704y03n3lvkmw2c0rbcwvydyr8ns4xh66b";
   };
 
-  nativeBuildInputs = [ python python.pkgs.sphinx pkgconfig flex bison meson ninja autoPatchelfHook ]
-    ++ optionals gtkSupport [ wrapGAppsHook ];
+  nativeBuildInputs = [ python python.pkgs.sphinx pkgconfig flex bison meson ninja ]
+    ++ optionals gtkSupport [ wrapGAppsHook ]
+    ++ optionals stdenv.isLinux [ autoPatchelfHook ];
   buildInputs =
     [ zlib glib ncurses perl pixman
       vde2 texinfo makeWrapper lzo snappy
