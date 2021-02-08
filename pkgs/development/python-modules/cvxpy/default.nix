a07d4a406e9d286822394a30c42d148029d86866
R. RyanTM <ryantm-bot@ryantm.com>
2021-02-08 08:21:25 +0000

    python37Packages.cvxpy: 1.1.8 -> 1.1.10

--- pkgs/development/python-modules/cvxpy/default.nix
+++ pkgs/development/python-modules/cvxpy/default.nix
@@ -17,13 +17,13 @@
 
 buildPythonPackage rec {
   pname = "cvxpy";
-  version = "1.1.8";
+  version = "1.1.10";
 
   disabled = pythonOlder "3.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "444068d4eda9ffcd43578895174489d4cef36b28ba7ae8a96ab9ef9571d2b4ff";
+    sha256 = "ecd0a8b89f799cea254928dea8792d9c69c37db0bd830b4cda629b2b2be52270";
   };
 
   propagatedBuildInputs = [
