2d4fd31f834039c54b6029a8751c72a583eae04e
Stig Palmquist <stig@stig.io>
2021-02-08 12:33:54 +0100

    [nix-update-cpan] perlPackages.Catmandu: 1.2013 -> 1.2015

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -2368,10 +2368,10 @@ let
 
   Catmandu = buildPerlModule {
     pname = "Catmandu";
-    version = "1.2013";
+    version = "1.2015";
     src = fetchurl {
-      url = "mirror://cpan/authors/id/N/NI/NICS/Catmandu-1.2013.tar.gz";
-      sha256 = "0sh4qqjh53rhqcpbvq3pyg0k1ybs8qxivhc1an6w5hjar2bihwiq";
+      url = "mirror://cpan/authors/id/N/NI/NICS/Catmandu-1.2015.tar.gz";
+      sha256 = "1ix5gasnvn74vrk7pafqy29815s3fgbw3i7fjrgmzx74i0dczvpx";
     };
     propagatedBuildInputs = [ AnyURIEscape AppCmd CGIExpand ConfigOnion CpanelJSONXS DataCompare DataUtil IOHandleUtil LWP ListMoreUtils LogAny MIMETypes ModuleInfo MooXAliases ParserMGC PathIteratorRule PathTiny StringCamelCase TextCSV TextHogan Throwable TryTinyByClass URITemplate UUIDTiny YAMLLibYAML namespaceclean ];
     buildInputs = [ LogAnyAdapterLog4perl LogLog4perl TestDeep TestException TestLWPUserAgent TestPod ];
