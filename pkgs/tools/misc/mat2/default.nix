9bdb3a7bebff2f78a00e4fe3b11655a6b5da37db
nicoo <nicoo@mur.at>
2021-02-08 18:07:07 +0100

    mat2: Document that upstream signs their release artefacts

--- pkgs/tools/misc/mat2/default.nix
+++ pkgs/tools/misc/mat2/default.nix
@@ -4,6 +4,9 @@ python3Packages.buildPythonPackage rec {
 	pname = "mat2";
 	version = "0.12.0";
 
+	# TODO: Verify upstream's signature, to ensure maintainers get an authentic
+	#  archive when importing a new version and setting its hash.
+	# #43233 mentions adding support in Nix's tooling, but nothing seems to exist.
 	srcs = fetchurl {
 		url = "${meta.homepage}/-/archive/${version}/${pname}-${version}.tar.gz";
 		sha256 = "185shmq35y2qj8ydy9l6v53flv536b8hrmv35b6ly22bczfs99yj";
