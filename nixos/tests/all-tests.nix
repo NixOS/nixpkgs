10b3773db0582651572088a401510ad486d78826
Manuel Bärenz <programming@manuelbaerenz.de>
2021-02-08 13:54:24 +0100

    vscode/vscodium: Add integration test

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -413,6 +413,7 @@ in
   vector = handleTest ./vector.nix {};
   victoriametrics = handleTest ./victoriametrics.nix {};
   virtualbox = handleTestOn ["x86_64-linux"] ./virtualbox.nix {};
+  vscodium = handleTest ./vscodium.nix {};
   wasabibackend = handleTest ./wasabibackend.nix {};
   wireguard = handleTest ./wireguard {};
   wordpress = handleTest ./wordpress.nix {};
