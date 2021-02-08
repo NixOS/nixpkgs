fcdbbe1bf6a7a9cf5fe0731ed3b55d68b38db18e
R. RyanTM <ryantm-bot@ryantm.com>
2021-02-08 08:52:53 +0000

    python37Packages.docplex: 2.19.202 -> 2.20.204

--- pkgs/development/python-modules/docplex/default.nix
+++ pkgs/development/python-modules/docplex/default.nix
@@ -9,12 +9,12 @@
 
 buildPythonPackage rec {
   pname = "docplex";
-  version = "2.19.202";
+  version = "2.20.204";
 
   # No source available from official repo
   src = fetchPypi {
     inherit pname version;
-    sha256 = "2b606dc645f99feae67dfc528620dddc773ecef5d59bcaeae68bba601f25162b";
+    sha256 = "24d8c3f54b4b1ec306c13b97c9d67e2f9fa93d0d9ea1b92bdba62df6982cd6a0";
   };
 
   propagatedBuildInputs = [
