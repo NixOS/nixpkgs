a47fdc43d703708d5c683be31b0a506316c9e0b2
nicoo <nicoo@mur.at>
2021-02-08 18:28:48 +0100

    mat2: Use spaces rather than tabs, placate CI

--- pkgs/tools/misc/mat2/default.nix
+++ pkgs/tools/misc/mat2/default.nix
@@ -1,40 +1,40 @@
 { python3Packages, fetchurl, lib, pkgs }:
 
 python3Packages.buildPythonPackage rec {
-	pname = "mat2";
-	version = "0.12.0";
+  pname = "mat2";
+  version = "0.12.0";
 
-	# TODO: Verify upstream's signature, to ensure maintainers get an authentic
-	#  archive when importing a new version and setting its hash.
-	# #43233 mentions adding support in Nix's tooling, but nothing seems to exist.
-	srcs = fetchurl {
-		url = "${meta.homepage}/-/archive/${version}/${pname}-${version}.tar.gz";
-		sha256 = "185shmq35y2qj8ydy9l6v53flv536b8hrmv35b6ly22bczfs99yj";
-	};
+  # TODO: Verify upstream's signature, to ensure maintainers get an authentic
+  #  archive when importing a new version and setting its hash.
+  # #43233 mentions adding support in Nix's tooling, but nothing seems to exist.
+  srcs = fetchurl {
+    url = "${meta.homepage}/-/archive/${version}/${pname}-${version}.tar.gz";
+    sha256 = "185shmq35y2qj8ydy9l6v53flv536b8hrmv35b6ly22bczfs99yj";
+  };
 
-	propagatedBuildInputs =
-		(with python3Packages; [
-			mutagen
-			pygobject3
-		]) ++ (with pkgs; [
-			bubblewrap
-			cairo
-			exiftool
-			ffmpeg
-			librsvg
-			poppler_gi
-			gdk_pixbuf
-		]);
+  propagatedBuildInputs =
+    (with python3Packages; [
+      mutagen
+      pygobject3
+    ]) ++ (with pkgs; [
+      bubblewrap
+      cairo
+      exiftool
+      ffmpeg
+      librsvg
+      poppler_gi
+      gdk_pixbuf
+    ]);
 
-	# Test runner seems to have incorrect PATH
-	doCheck = false;
+  # Test runner seems to have incorrect PATH
+  doCheck = false;
 
-	meta = with lib; {
-		homepage = "https://0xacab.org/jvoisin/${pname}";
-		description = "Metadata Anonymisation Toolkit — remove metadata from various media formats";
+  meta = with lib; {
+    homepage = "https://0xacab.org/jvoisin/${pname}";
+    description = "Metadata Anonymisation Toolkit — remove metadata from various media formats";
 
-		license = licenses.lgpl3Plus;
-		platforms = platforms.unix;
-		maintainers = with maintainers; [ nicoo ];
-	};
+    license = licenses.lgpl3Plus;
+    platforms = platforms.unix;
+    maintainers = with maintainers; [ nicoo ];
+  };
 }
