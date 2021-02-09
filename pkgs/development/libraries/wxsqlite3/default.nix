94f9f61f64cec4cdb628ce633e7eea2a5cf83922
R. RyanTM <ryantm-bot@ryantm.com>
2021-02-09 19:02:58 +0000

    wxsqlite3: 4.6.2 -> 4.6.4
    Update license.

--- pkgs/development/libraries/wxsqlite3/default.nix
+++ pkgs/development/libraries/wxsqlite3/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "wxsqlite3";
-  version = "4.6.2";
+  version = "4.6.4";
 
   src = fetchFromGitHub {
     owner = "utelle";
     repo = "wxsqlite3";
     rev = "v${version}";
-    sha256 = "10jlb4p3ahck9apcy2c2mrrjynv4c1dfwwbf1vwd8dl17pgv4kgs";
+    sha256 = "sha256-fIm8xbNP7pjzvfBn7NgYmUtbVVh2aiaXQVANJQnrWCs=";
   };
 
   nativeBuildInputs = [ autoreconfHook ];
@@ -27,6 +27,6 @@ stdenv.mkDerivation rec {
     description = "A C++ wrapper around the public domain SQLite 3.x for wxWidgets";
     platforms = platforms.unix;
     maintainers = with maintainers; [ vrthra ];
-    license = [ licenses.lgpl2 ];
+    license = [ licenses.lgpl3Plus licenses.gpl3Plus ];
   };
 }
