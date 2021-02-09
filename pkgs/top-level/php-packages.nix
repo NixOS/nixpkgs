6ae0ce2deafe9f3e05a42bbba44e64d0d30040c0
Aaron Andersen <aaron@fosslib.net>
2021-02-09 09:06:29 -0500

    php.extensions.smbclient: init at 1.0.4

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -773,6 +773,20 @@ in
       meta.maintainers = lib.teams.php.members;
     };
 
+    smbclient = buildPecl {
+      pname = "smbclient";
+      version = "1.0.4";
+      sha256 = "07p72m5kbdyp3r1mfxhiayzdvymhc8afwcxa9s86m96sxbmlbbp8";
+
+      # TODO: remove this when upstream merges a fix - https://github.com/eduardok/libsmbclient-php/pull/66
+      LIBSMBCLIENT_INCDIR = "${pkgs.samba.dev}/include/samba-4.0";
+
+      nativeBuildInputs = [ pkgs.pkg-config ];
+      buildInputs = [ pkgs.samba ];
+
+      meta.maintainers = lib.teams.php.members;
+    };
+
     sqlsrv = buildPecl {
       version = "5.8.1";
       pname = "sqlsrv";
