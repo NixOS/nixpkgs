e605e6205fda7ec0e5e39ba7b405dca3ad1750da
Ben Siraphob <bensiraphob@gmail.com>
2021-02-09 18:24:09 +0700

    power-profiles-daemon: pkgs.stdenv.lib -> pkgs.lib

--- nixos/tests/power-profiles-daemon.nix
+++ nixos/tests/power-profiles-daemon.nix
@@ -2,7 +2,7 @@ import ./make-test-python.nix ({ pkgs, ... }:
 
 {
   name = "power-profiles-daemon";
-  meta = with pkgs.stdenv.lib.maintainers; {
+  meta = with pkgs.lib.maintainers; {
     maintainers = [ mvnetbiz ];
   };
   machine = { pkgs, ... }: {
