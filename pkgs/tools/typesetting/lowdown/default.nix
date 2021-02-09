79143cae19a15bd820984284ea0fb323c77448b7
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-02-09 14:20:20 +0100

    lowdown: 0.8.0 -> 0.8.1
    (cherry picked from commit 44f1a10aa0a5b51f451149583525eb4871c97efe)

--- pkgs/tools/typesetting/lowdown/default.nix
+++ pkgs/tools/typesetting/lowdown/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "lowdown";
-  version = "0.8.0";
+  version = "0.8.1";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-${version}.tar.gz";
-    sha512 = "17w0hldlg7x0f4946bz1pmgkpp08dha3myz8pk0rx8n2lqlw76f3n9giq69kw82sgzg7qqy31sl9mxvgaxyf6hcdgmxnkr7h8d9xmak";
+    sha512 = "28kwj053lm3510cq7pg4rqx6linv5zphhm2h6r9icigclfq7j9ybnd7vqbn2v4ay0r8ghac5cjbqab5zy8cjlgllwhwsxg0dnk75x2i";
   };
 
   nativeBuildInputs = [ which ];
