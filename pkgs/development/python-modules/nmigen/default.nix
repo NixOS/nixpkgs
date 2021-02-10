6a865bf06096c423156eab9d64749333eb62fe0e
David Anderson <dave@natulte.net>
2021-02-09 20:47:21 -0800

    nmigen: 2020-04-02 -> 2021-02-09
    Signed-off-by: David Anderson <dave@natulte.net>

--- pkgs/development/python-modules/nmigen/default.nix
+++ pkgs/development/python-modules/nmigen/default.nix
@@ -6,6 +6,8 @@
 , setuptools_scm
 , pyvcd
 , jinja2
+, importlib-resources
+, importlib-metadata
 
 # for tests
 , pytestCheckHook
@@ -16,21 +18,27 @@
 
 buildPythonPackage rec {
   pname = "nmigen";
-  version = "unstable-2020-04-02";
+  version = "unstable-2021-02-09";
   # python setup.py --version
-  realVersion = "0.2.dev49+g${lib.substring 0 7 src.rev}";
+  realVersion = "0.3.dev243+g${lib.substring 0 7 src.rev}";
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "nmigen";
     repo = "nmigen";
-    rev = "c79caead33fff14e2dec42b7e21d571a02526876";
-    sha256 = "sha256-3+mxHyg0a92/BfyePtKT5Hsk+ra+fQzTjCJ2Ech44/s=";
+    rev = "f7c2b9419f9de450be76a0e9cf681931295df65f";
+    sha256 = "0cjs9wgmxa76xqmjhsw4fsb2mhgvd85jgs2mrjxqp6fwp8rlgnl1";
   };
 
   nativeBuildInputs = [ setuptools_scm ];
 
-  propagatedBuildInputs = [ setuptools pyvcd jinja2 ];
+  propagatedBuildInputs = [
+    setuptools
+    pyvcd
+    jinja2
+  ] ++
+    lib.optional (pythonOlder "3.9") importlib-resources ++
+    lib.optional (pythonOlder "3.8") importlib-metadata;
 
   checkInputs = [ pytestCheckHook yosys symbiyosys yices ];
 
