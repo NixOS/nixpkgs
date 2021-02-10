63cdb44b488f735eb330a428f03e772f00f9144f
Daniël de Kok <me@danieldk.eu>
2021-02-10 13:22:47 +0100

    cudnn_cudatoolkit_11_0: 8.0.2 -> 8.1.0

--- pkgs/development/libraries/science/math/cudnn/default.nix
+++ pkgs/development/libraries/science/math/cudnn/default.nix
@@ -82,10 +82,12 @@ in rec {
   cudnn_cudatoolkit_10 = cudnn_cudatoolkit_10_2;
 
   cudnn_cudatoolkit_11_0 = generic rec {
-    version = "8.0.2";
+    version = "8.1.0";
     cudatoolkit = cudatoolkit_11_0;
-    srcName = "cudnn-${cudatoolkit.majorVersion}-linux-x64-v8.0.2.39.tgz";
-    sha256 = "0ib3v3bgcdxarqapkxngw1nwl0c2a7zz392ns7w9ipcficl4cbv7";
+    # 8.1.0 is compatible with CUDA 11.0, 11.1, and 11.2:
+    # https://docs.nvidia.com/deeplearning/cudnn/support-matrix/index.html#cudnn-cuda-hardware-versions
+    srcName = "cudnn-11.2-linux-x64-v8.1.0.77.tgz";
+    sha256 = "sha256-2+gvrwcdkbqbzwBIAUatM/RiSC3+5WyvRHnBuNq+Pss=";
   };
 
   cudnn_cudatoolkit_11 = cudnn_cudatoolkit_11_0;
