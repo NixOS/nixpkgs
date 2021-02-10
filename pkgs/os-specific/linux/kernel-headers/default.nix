60056c8ce72d7d1f22a8ed2f07409acde93509a9
TredwellGit <tredwell@tutanota.com>
2021-02-10 09:37:27 +0000

    linuxHeaders: 5.10.9 -> 5.10.15

--- pkgs/os-specific/linux/kernel-headers/default.nix
+++ pkgs/os-specific/linux/kernel-headers/default.nix
@@ -69,12 +69,12 @@ let
 in {
   inherit makeLinuxHeaders;
 
-  linuxHeaders = let version = "5.10.9"; in
+  linuxHeaders = let version = "5.10.15"; in
     makeLinuxHeaders {
       inherit version;
       src = fetchurl {
         url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-        sha256 = "0la7dklpy6xd79fkzavpmlfyrc60kmmwz491msd95dmvv06kwwvz";
+        sha256 = "00bf1v8zn4qngxhj6sca0lhv71xlnajw02iq6854s76my6y8flnq";
       };
       patches = [
          ./no-relocs.patch # for building x86 kernel headers on non-ELF platforms
