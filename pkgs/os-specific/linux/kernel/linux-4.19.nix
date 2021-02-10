a0996190f06ba77469e5b72fe49a9fad6420fbb0
Tim Steinbach <tim@nequissimus.com>
2021-02-10 09:54:02 -0500

    linux: 4.19.174 -> 4.19.175

--- pkgs/os-specific/linux/kernel/linux-4.19.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "4.19.174";
+  version = "4.19.175";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1rcy0hfbc3ny52mfrfysknm1q2scqz0g8l28j0qlyw8cx41wgxhg";
+    sha256 = "099b3dw9rj2z147dpjppd57g24paxw8x4fq1ir1ss5ibzy24pvnc";
   };
 } // (args.argsOverride or {}))
