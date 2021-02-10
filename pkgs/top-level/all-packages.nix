206dcee07dd5e9fcb3077b4247071df5b4bc87d8
David Anderson <dave@natulte.net>
2021-02-09 22:08:01 -0800

    openfpgaloader: init at 0.2.1.
    Signed-off-by: David Anderson <dave@natulte.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6677,6 +6677,8 @@ in
 
   opencorsairlink = callPackage ../tools/misc/opencorsairlink { };
 
+  openfpgaloader = callPackage ../development/tools/misc/openfpgaloader { };
+
   openfortivpn = callPackage ../tools/networking/openfortivpn { };
 
   obexfs = callPackage ../tools/bluetooth/obexfs { };
