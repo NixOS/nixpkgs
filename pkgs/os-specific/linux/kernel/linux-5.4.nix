6c56e6aaf1787d234917ddbaa9f5b70e11557722
Tim Steinbach <tim@nequissimus.com>
2021-02-10 09:54:28 -0500

    linux: 5.4.96 -> 5.4.97

--- pkgs/os-specific/linux/kernel/linux-5.4.nix
+++ pkgs/os-specific/linux/kernel/linux-5.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.4.96";
+  version = "5.4.97";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1q7mz69wzk1ps5770l9bj556qyndiz2frjjsl7pigsy5brlxwa7p";
+    sha256 = "0gs856v3q3m0q245mf8b9ivds8dizqrgjw27s9kbq3v31886da3i";
   };
 } // (args.argsOverride or {}))
