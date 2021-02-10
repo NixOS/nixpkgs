f9e6eb090ea75b5b0d68e849e94c0298b8a5894f
Ivan Babrou <github@ivan.computer>
2021-02-09 23:31:04 -0800

    llvmPackages: default to llvmPackages_11 on aarch64-darwin
    It doesn't make sense to use llvm7, which doesn't work on arm64.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11249,7 +11249,7 @@ in
     # This returns the minimum suported version for the platform. The
     # assumption is that or any later version is good.
     choose = platform:
-      /**/ if platform.isDarwin then 7
+      /**/ if platform.isDarwin then (if platform.isAarch64 then 11 else 7)
       else if platform.isFreeBSD then 7
       else if platform.isAndroid then 12
       else if platform.isLinux then 7
