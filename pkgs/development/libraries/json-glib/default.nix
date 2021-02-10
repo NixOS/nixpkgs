8b31cb4e70b13fdf645a02e60b932a089c43328a
Ivan Babrou <github@ivan.computer>
2021-02-09 21:07:29 -0800

    json-glib: fix build issue on clang 11
    Upgrade for the latest version is non-trivial, but lifting this
    one patch really helps with #105026.

--- pkgs/development/libraries/json-glib/default.nix
+++ pkgs/development/libraries/json-glib/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, glib, meson, ninja, pkg-config, gettext
+{ lib, stdenv, fetchurl, fetchpatch, glib, meson, ninja, pkg-config, gettext
 , gobject-introspection, fixDarwinDylibNames, gnome3
 }:
 
@@ -12,6 +12,14 @@ in stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${name}.tar.xz";
     sha256 = "0ixwyis47v5bkx6h8a1iqlw3638cxcv57ivxv4gw2gaig51my33j";
   };
+
+  patches = [
+    (fetchpatch {
+      # included in next release (> 1.4.4)
+      url = "https://github.com/GNOME/json-glib/commit/8c5fabe962b7337066dac7a697d23fce257a5d64.patch";
+      sha256 = "0y6jwvb52i8q0hpp58lx49nj59ii59rs980ib8i3v2nmjz9qfy34";
+    })
+  ];
 
   propagatedBuildInputs = [ glib ];
   nativeBuildInputs = [ meson ninja pkg-config gettext gobject-introspection glib ]
