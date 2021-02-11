a74e48c6d6384ebc2a36ba887a35d7585b17a02d
toonn <toonn@toonn.io>
2021-02-11 12:55:05 +0100

    bitlbee-facebook: Add note for removal of the patch
    This TODO note should remind whoever bumps the package next to check
    whether the patch can be dropped.

--- pkgs/applications/networking/instant-messengers/bitlbee-facebook/default.nix
+++ pkgs/applications/networking/instant-messengers/bitlbee-facebook/default.nix
@@ -12,6 +12,8 @@ stdenv.mkDerivation rec {
     sha256 = "1yjhjhk3jzjip13lq009vlg84lm2lzwhac5jy0aq3vkcz6rp94rc";
   };
 
+  # TODO: This patch should be included with the next release after v1.2.1
+  #       these lines and the patch should be removed when this happens.
   patches = [ ./0001-Bump-the-FB_ORCA_AGENT-version-once-again-208.patch ];
 
   nativeBuildInputs = [ autoconf automake libtool pkg-config ];
