0d4143e363a70dc4b243609a0e4c95d0160bde01
R. RyanTM <ryantm-bot@ryantm.com>
2021-02-11 00:17:47 +0000

    python37Packages.azure-synapse-accesscontrol: 0.4.0 -> 0.5.0

--- pkgs/development/python-modules/azure-synapse-accesscontrol/default.nix
+++ pkgs/development/python-modules/azure-synapse-accesscontrol/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "azure-synapse-accesscontrol";
-  version = "0.4.0";
+  version = "0.5.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a4f32423d9facaae512c433f5460b4ceec73a6c20b44b00e9de9de7a0e86dacd";
+    sha256 = "835e324a2072a8f824246447f049c84493bd43a1f6bac4b914e78c090894bb04";
     extension = "zip";
   };
 
