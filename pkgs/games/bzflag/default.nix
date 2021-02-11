753be21485579c942b7bce79e2bcd1f36da12695
Bryan Gardiner <bog@khumba.net>
2021-02-10 19:32:38 -0800

    bzflag: install desktop file and icon; enable parallel building

--- pkgs/games/bzflag/default.nix
+++ pkgs/games/bzflag/default.nix
@@ -11,9 +11,19 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-nmRlMwK2V72LX5b+EVCp/4Ch1Tptfoo1E4xrGwIAak0=";
   };
 
+  patches = [ ./bzflag-2.4.20-desktop-file.patch ];
+
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ curl SDL2 libGLU libGL glew ncurses c-ares ]
     ++ lib.optionals stdenv.isDarwin [ Carbon CoreServices ];
+
+  enableParallelBuilding = true;
+
+  postInstall = ''
+    mkdir -p $out/share/{applications,pixmaps}
+    ln -st $out/share/applications $out/share/bzflag/bzflag.desktop
+    ln -st $out/share/pixmaps $out/share/bzflag/bzflag-48x48.png
+  '';
 
   meta = with lib; {
     description = "Multiplayer 3D Tank game";
