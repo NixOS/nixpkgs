e67163e4ae43676a5f1f425589d4c7c4b59fa4e7
"jupblb" <"mpkielbowicz@gmail.com">
2021-02-12 09:19:05 +0100

    vimPlugins.vim-bbye: init at 2018-03-03

--- pkgs/misc/vim-plugins/generated.nix
+++ pkgs/misc/vim-plugins/generated.nix
@@ -4778,6 +4778,18 @@ let
     meta.homepage = "https://github.com/bazelbuild/vim-bazel/";
   };
 
+  vim-bbye = buildVimPluginFrom2Nix {
+    pname = "vim-bbye";
+    version = "2018-03-03";
+    src = fetchFromGitHub {
+      owner = "moll";
+      repo = "vim-bbye";
+      rev = "25ef93ac5a87526111f43e5110675032dbcacf56";
+      sha256 = "0dlifpbd05fcgndpkgb31ww8p90pwdbizmgkkq00qkmvzm1ik4y4";
+    };
+    meta.homepage = "https://github.com/moll/vim-bbye/";
+  };
+
   vim-beancount = buildVimPluginFrom2Nix {
     pname = "vim-beancount";
     version = "2020-08-06";
