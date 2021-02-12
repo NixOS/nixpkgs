73ac41146bd325ca0b0e32fc203486e3764215ba
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-02-12 16:54:50 +0100

    smenu: format, add SuperSandro2000 as maintainer

--- pkgs/tools/misc/smenu/default.nix
+++ pkgs/tools/misc/smenu/default.nix
@@ -5,25 +5,24 @@ stdenv.mkDerivation rec {
   pname = "smenu";
 
   src = fetchFromGitHub {
-    owner  = "p-gen";
-    repo   = "smenu";
-    rev    = "v${version}";
+    owner = "p-gen";
+    repo = "smenu";
+    rev = "v${version}";
     sha256 = "1p8y1fgrfb7jxmv5ycvvnqaz7ghdi50paisgzk71169fqwp1crfa";
   };
 
   buildInputs = [ ncurses ];
 
   meta = with lib; {
-    homepage        = "https://github.com/p-gen/smenu";
-    description     = "Terminal selection utility";
+    homepage = "https://github.com/p-gen/smenu";
+    description = "Terminal selection utility";
     longDescription = ''
       Terminal utility that allows you to use words coming from the standard
       input to create a nice selection window just below the cursor. Once done,
       your selection will be sent to standard output.
     '';
-    license     = licenses.gpl2;
-    maintainers = [ maintainers.matthiasbeyer ];
-    platforms   = platforms.unix;
+    license = licenses.gpl2Only;
+    maintainers = with maintainers; [ matthiasbeyer SuperSandro2000 ];
+    platforms = platforms.unix;
   };
 }
-
