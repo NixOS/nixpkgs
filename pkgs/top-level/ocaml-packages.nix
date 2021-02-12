7e8602236394e4710aaab64b9c3ea57e6cdde44c
Alyssa Ross <hi@alyssa.is>
2021-02-12 22:32:08 +0000

    glsurf: use current giflib
    This requires bumping the version of camlimages used by glsurf to a
    version that supports current giflib.  The most recent versions of
    camlimages (even of 4.x) don't support ocaml 4.01 any more, so I've
    upgraded to 4.1.2 here, the last version that supports ocaml 4.01 (and
    which happily supports current giflib).

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -114,12 +114,11 @@ let
       then callPackage ../development/ocaml-modules/camomile { }
       else callPackage ../development/ocaml-modules/camomile/0.8.5.nix { };
 
-    camlimages_4_0 =
+    camlimages_4_1_2 =
       if lib.versionOlder "4.02" ocaml.version
       then null
-      else callPackage ../development/ocaml-modules/camlimages/4.0.nix {
+      else callPackage ../development/ocaml-modules/camlimages/4.1.2.nix {
       libpng = pkgs.libpng12;
-      giflib = pkgs.giflib_4_1;
     };
     camlimages = callPackage ../development/ocaml-modules/camlimages { };
 
