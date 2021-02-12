2fb689018c10f058f634e0bed0faf816675a250b
R. RyanTM <ryantm-bot@ryantm.com>
2021-02-12 14:12:17 +0000

    jc: 1.14.2 -> 1.14.3

--- pkgs/development/python-modules/jc/default.nix
+++ pkgs/development/python-modules/jc/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "jc";
-  version = "1.14.2";
+  version = "1.14.3";
   disabled = isPy27;
 
   src = fetchFromGitHub {
     owner = "kellyjonbrazil";
     repo = "jc";
     rev = "v${version}";
-    sha256 = "sha256-rYH4m7h2SPB1Io83ZUsqZ+Ll4XEi4Feuj4QYvaOJ2lY=";
+    sha256 = "sha256-6ofw74onFECJYYSuZlOVcNVAVuJaJKV4pkaJ4cTrvPk=";
   };
 
   propagatedBuildInputs = [ ruamel_yaml xmltodict pygments ];
