7d69145adbd339220ce39a0bd63544b9e57f23b4
WORLDofPEACE <worldofpeace@protonmail.ch>
2021-02-12 04:37:03 -0500

    dbus-broker:  22 -> 26
    * add me as maintainer
    * add system-console-users
    * fix-paths.patch
      This I'm not 100% on.
      We make it so the systemd service isn't configured to have
      /run/current-system/sw/share in XDG_DATA_DIRS, and dbus-broker falls
      back to /usr/share. This makes it so nothing will get launched
      from /run/current-system/sw/share, and everything is limited to the
      configs (and how I believe this should be working). However, I have
      suspicion that maybe dbus-daemon knows about /run/current-system/sw/share.

--- pkgs/os-specific/linux/dbus-broker/default.nix
+++ pkgs/os-specific/linux/dbus-broker/default.nix
@@ -1,21 +1,50 @@
-{ lib, stdenv, fetchFromGitHub, docutils, meson, ninja, pkg-config
-, dbus, linuxHeaders, systemd }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, docutils
+, meson
+, ninja
+, pkg-config
+, dbus
+, linuxHeaders
+, systemd
+}:
 
 stdenv.mkDerivation rec {
   pname = "dbus-broker";
-  version = "22";
+  version = "26";
 
   src = fetchFromGitHub {
-    owner  = "bus1";
-    repo   = "dbus-broker";
-    rev    = "v${version}";
-    sha256 = "0vxr73afix5wjxy8g4cckwhl242rrlazm52673iwmdyfz5nskj2x";
+    owner = "bus1";
+    repo = "dbus-broker";
+    rev = "v${version}";
+    sha256 = "QjYjAvnLgMRL/4Jj1XBXl9z47zVHclOqZrAF2khC424=";
     fetchSubmodules = true;
   };
 
-  nativeBuildInputs = [ docutils meson ninja pkg-config ];
+  patches = [
+    # Make launcher.c use nixos dbus configuration paths
+    # In the future the buildsystem should allow us to configure the **default**
+    # (as there is a --config-file cmdline)
+    ./fix-paths.patch
+  ];
 
-  buildInputs = [ dbus linuxHeaders systemd ];
+  mesonFlags = [
+    "-D=system-console-users=gdm,sddm,lightdm"
+  ];
+
+  nativeBuildInputs = [
+    docutils
+    meson
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = [
+    dbus
+    linuxHeaders
+    systemd
+  ];
 
   PKG_CONFIG_SYSTEMD_SYSTEMDSYSTEMUNITDIR = "${placeholder "out"}/lib/systemd/system";
   PKG_CONFIG_SYSTEMD_SYSTEMDUSERUNITDIR = "${placeholder "out"}/lib/systemd/user";
@@ -32,9 +61,9 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "Linux D-Bus Message Broker";
-    homepage    = "https://github.com/bus1/dbus-broker/wiki";
-    license     = licenses.asl20;
-    platforms   = platforms.linux;
-    maintainers = with maintainers; [ peterhoeg ];
+    homepage = "https://github.com/bus1/dbus-broker/wiki";
+    license = licenses.asl20;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ peterhoeg worldofpeace ];
   };
 }
