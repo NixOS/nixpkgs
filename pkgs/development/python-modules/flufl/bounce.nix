7ff749465cb8c4b82fc84df14385f65a70254343
R. RyanTM <ryantm-bot@ryantm.com>
2021-02-13 09:57:27 +0000

    python37Packages.flufl_bounce: 3.0.1 -> 3.0.2

--- pkgs/development/python-modules/flufl/bounce.nix
+++ pkgs/development/python-modules/flufl/bounce.nix
@@ -2,13 +2,13 @@
 
 buildPythonPackage rec {
   pname = "flufl.bounce";
-  version = "3.0.1";
+  version = "3.0.2";
 
   buildInputs = [ nose2 ];
   propagatedBuildInputs = [ atpublic zope_interface ];
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e432fa1ca25ddbf23e2716b177d4d1c6ab6c078e357df56b0106b92bc10a8f06";
+    sha256 = "5014b23248fce72b13143c32da30073e6abc655b963e7739575608280c52c9a7";
   };
 }
