f3e2aecd32a0f3ed48575d366fd7b050fe4d4436
Fabian Affolter <mail@fabian-affolter.ch>
2021-02-13 18:45:46 +0100

    python3Packages.httplib2: enable tests

--- pkgs/development/python-modules/httplib2/default.nix
+++ pkgs/development/python-modules/httplib2/default.nix
@@ -1,27 +1,53 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
+, isPy27
+, mock
 , pyparsing
+, pytest-forked
+, pytest-randomly
+, pytest-timeout
+, pytest-xdist
+, pytestCheckHook
+, six
 }:
 
 buildPythonPackage rec {
   pname = "httplib2";
   version = "0.19.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-4NQo2tQ8ctvOfRY7d1P/x6OcCX5niO8Q9BmNtpuS8I4=";
+  src = fetchFromGitHub {
+    owner = pname;
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "04y2bc2yv3q84llxnafqrciqxjqpxbrd8glbnvvr16c20fwc3r4q";
   };
+
+  postPatch = ''
+    sed -i "/--cov/d" setup.cfg
+  '';
 
   propagatedBuildInputs = [ pyparsing ];
 
-  # Needs setting up
-  doCheck = false;
+  checkInputs = [
+    mock
+    pytest-forked
+    pytest-randomly
+    pytest-timeout
+    pytest-xdist
+    six
+    pytestCheckHook
+  ];
+
+  # Don't run tests for Python 2.7
+  doCheck = !isPy27;
+  pytestFlagsArray = [ "--ignore python2" ];
+  pythonImportsCheck = [ "httplib2" ];
 
   meta = with lib; {
-    homepage = "https://github.com/httplib2/httplib2";
     description = "A comprehensive HTTP client library";
+    homepage = "https://httplib2.readthedocs.io";
     license = licenses.mit;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ fab ];
   };
 }
