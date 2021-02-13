298a451eb65c006c550589861b62bbed82f28286
Daniel Nagy <danielnagy@posteo.de>
2021-02-14 00:43:42 +0100

    chickenEggs.foreigners: init at 1.5

--- pkgs/development/compilers/chicken/5/eggs.nix
+++ pkgs/development/compilers/chicken/5/eggs.nix
@@ -106,6 +106,20 @@ rec {
     ];
   };
 
+  foreigners = eggDerivation {
+    name = "foreigners-1.5";
+
+    src = fetchegg {
+      name = "foreigners";
+      version = "1.5";
+      sha256 = "1mm91y61nlawgb7iqdrkz2fi9sc3fic07f5m1ig541b2hbscfiqy";
+    };
+
+    buildInputs = [
+      matchable
+    ];
+  };
+
   intarweb = eggDerivation {
     name = "intarweb-2.0.1";
 
