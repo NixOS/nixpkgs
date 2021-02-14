551ac139afcec2368de5bd5ee3728f84f9d8f906
Fabian Affolter <mail@fabian-affolter.ch>
2021-02-14 09:34:32 +0100

    home-assistant: update component-packages

--- pkgs/servers/home-assistant/component-packages.nix
+++ pkgs/servers/home-assistant/component-packages.nix
@@ -41,7 +41,7 @@
     "apple_tv" = ps: with ps; [ aiohttp-cors netdisco pyatv zeroconf ];
     "apprise" = ps: with ps; [ apprise ];
     "aprs" = ps: with ps; [ geopy ]; # missing inputs: aprslib
-    "aqualogic" = ps: with ps; [ ]; # missing inputs: aqualogic
+    "aqualogic" = ps: with ps; [ aqualogic ];
     "aquostv" = ps: with ps; [ ]; # missing inputs: sharp_aquos_rc
     "arcam_fmj" = ps: with ps; [ ]; # missing inputs: arcam-fmj
     "arduino" = ps: with ps; [ ]; # missing inputs: PyMata
