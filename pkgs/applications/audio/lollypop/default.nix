368c1d9d7d62b48c96373a2ceb05f27e6634e4c6
Bernardo Meurer <bernardo@meurer.org>
2021-02-14 15:01:54 -0800

    lollypop: add lovesegfault to maintainers

--- pkgs/applications/audio/lollypop/default.nix
+++ pkgs/applications/audio/lollypop/default.nix
@@ -106,7 +106,7 @@ python3.pkgs.buildPythonApplication rec  {
     description = "A modern music player for GNOME";
     homepage = "https://wiki.gnome.org/Apps/Lollypop";
     license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ worldofpeace ];
+    maintainers = with maintainers; [ worldofpeace lovesegfault ];
     platforms = platforms.linux;
   };
 }
