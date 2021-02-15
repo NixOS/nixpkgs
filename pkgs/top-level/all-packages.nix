33cd29d1377338853b6ec65942b51c77fa3619a2
adisbladis <adisbladis@gmail.com>
2021-02-15 12:50:42 +0100

    linuxPackages_latest: Use kernel 5.11

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19423,7 +19423,7 @@ in
 
   # Update this when adding the newest kernel major version!
   # And update linux_latest_for_hardened below if the patches are already available
-  linuxPackages_latest = linuxPackages_5_10;
+  linuxPackages_latest = linuxPackages_5_11;
   linux_latest = linuxPackages_latest.kernel;
 
   # Realtime kernel packages.
