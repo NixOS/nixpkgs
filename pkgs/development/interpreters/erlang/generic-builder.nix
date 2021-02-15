fb0c10bbdb8368b38fb618115efe1fefb8f8c83f
Raphael Megzari <raphael@megzari.com>
2021-02-16 06:47:17 +0900

    Update pkgs/development/interpreters/erlang/generic-builder.nix
    Co-authored-by: Jörg Thalheim <Mic92@users.noreply.github.com>

--- pkgs/development/interpreters/erlang/generic-builder.nix
+++ pkgs/development/interpreters/erlang/generic-builder.nix
@@ -82,7 +82,7 @@ in stdenv.mkDerivation ({
     ./otp_build autoconf
   '';
 
-  configureFlags = [ "--with-ssl=${opensslPackage.dev}" ]
+  configureFlags = [ "--with-ssl=${lib.getDev opensslPackage}" ]
     ++ optional enableThreads "--enable-threads"
     ++ optional enableSmpSupport "--enable-smp-support"
     ++ optional enableKernelPoll "--enable-kernel-poll"
