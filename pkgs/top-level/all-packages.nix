04e04ead20d8f65618ce2db13533f93d5a00b1b3
Johannes Rosenberger <johannes@jorsn.eu>
2021-02-17 14:29:04 +0100

    lice: 0.4 -> 0.6 (#112895)
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29134,7 +29134,7 @@ in
 
   lkproof = callPackage ../tools/typesetting/tex/lkproof { };
 
-  lice = callPackage ../tools/misc/lice {};
+  lice = python3Packages.callPackage ../tools/misc/lice {};
 
   m33-linux = callPackage ../misc/drivers/m33-linux { };
 
