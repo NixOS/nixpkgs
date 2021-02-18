493e7f24413bfabb4150f900ee92bd11a6825f1e
Manoj Karthick <manojkarthick@users.noreply.github.com>
2021-02-18 11:38:08 -0800

    reddsaver: 0.2.3 -> 0.3.0 (#113571)

--- pkgs/applications/misc/reddsaver/default.nix
+++ pkgs/applications/misc/reddsaver/default.nix
@@ -7,27 +7,27 @@
 }:
 
 rustPlatform.buildRustPackage rec {
-  version = "0.2.3";
   pname = "reddsaver";
+  version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "manojkarthick";
     repo = "reddsaver";
     rev = "v${version}";
-    sha256 = "sha256-K6SyfYx8VG0t6yogHwd80AxQuj3TXofHLEqZcDsRs1s=";
+    sha256 = "0wiyzbl9vqx5aq3lpaaqkm3ivj77lqd8bmh8ipgshdflgm1z6yvp";
   };
 
-  cargoSha256 = "sha256-VDr7fcE13Wy7KoGG3U1GSbWqF5Oad4EobgzOL7dtJDo=";
+  cargoSha256 = "0kw5gk7pf4xkmjffs2jxm6sc4chybns88cii2wlgpyvgn4c3cwaa";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ]
     ++ lib.optional stdenv.isDarwin Security;
 
-  # package does not contain tests as of v0.2.3
+  # package does not contain tests as of v0.3.0
   docCheck = false;
 
   meta = with lib; {
-    description = "CLI tool to download saved images from Reddit";
+    description = "CLI tool to download saved media from Reddit";
     homepage = "https://github.com/manojkarthick/reddsaver";
     license = with licenses; [ mit /* or */ asl20 ];
     maintainers = [ maintainers.manojkarthick ];
