f16fcc0ba55c6d7df4244d29a80d1a1f37d519bb
Fabian Affolter <mail@fabian-affolter.ch>
2021-02-18 13:05:20 +0100

    python3Packages.librouteros: init at 3.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3704,6 +3704,8 @@ in {
 
   librosa = callPackage ../development/python-modules/librosa { };
 
+  librouteros = callPackage ../development/python-modules/librouteros { };
+
   libsass = (callPackage ../development/python-modules/libsass { inherit (pkgs) libsass; });
 
   libsavitar = callPackage ../development/python-modules/libsavitar { };
