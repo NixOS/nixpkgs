8f7ff0021334ac4346cc00c9244238e992bff7d0
Armijn Hemel <armijn@tjaldur.nl>
2021-02-18 14:05:50 +0100

    packageurl-python: init at 0.9.4

---
+++ pkgs/development/python-modules/packageurl-python/default.nix
@@ -0,0 +1,17 @@
+{ buildPythonPackage, fetchPypi, lib }:
+
+buildPythonPackage rec {
+  pname = "packageurl-python";
+  version = "0.9.4";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "0mpvj8imsaqhrgfq1cxx16flc5201y78kqa7bh2i5zxsc29843mx";
+  };
+
+  meta = with lib; {
+    description = "A parser and builder for purl aka. Package URLs for Python 2 and 3.";
+    homepage = "https://github.com/package-url/packageurl-python";
+    license = licenses.mit;
+  };
+}
