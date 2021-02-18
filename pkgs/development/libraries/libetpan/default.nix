5bb5d61154bb9de283c7b94254d17b2ded1088be
Alvar Penning <post@0x21.biz>
2021-02-18 09:51:21 +0100

    libetpan: Fix CVE-2020-15953
    This commit patches the vulnerable libetpan release 1.9.4 with its
    upstream patches against the CVE-2020-15953.
    
    Merging this will close #113463.
    
    (cherry picked from commit 2b344e02ad2cdb4696c3c56d01f4638b08e9fe97)

--- pkgs/development/libraries/libetpan/default.nix
+++ pkgs/development/libraries/libetpan/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub
+{ stdenv, fetchFromGitHub, fetchpatch
 , autoconf
 , automake
 , libtool
@@ -17,6 +17,28 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [ libtool autoconf automake ];
+
+  patches = [
+    # The following two patches are fixing CVE-2020-15953, as reported in the
+    # issue tracker: https://github.com/dinhvh/libetpan/issues/386
+    # They might be removed for the next version bump.
+
+    # CVE-2020-15953: Detect extra data after STARTTLS response and exit
+    # https://github.com/dinhvh/libetpan/pull/387
+    (fetchpatch {
+      name = "cve-2020-15953-imap.patch";
+      url = "https://github.com/dinhvh/libetpan/commit/1002a0121a8f5a9aee25357769807f2c519fa50b.patch";
+      sha256 = "1h9ds2z4jii40a0i3z6hsnzx1ldmd2jqidsxp2y2ksyp1ijcgabn";
+    })
+
+    # CVE-2020-15953: Detect extra data after STARTTLS responses in SMTP and POP3 and exit
+    # https://github.com/dinhvh/libetpan/pull/388
+    (fetchpatch {
+      name = "cve-2020-15953-pop3-smtp.patch";
+      url = "https://github.com/dinhvh/libetpan/commit/298460a2adaabd2f28f417a0f106cb3b68d27df9.patch";
+      sha256 = "0lq829djar7nb3fai3vdzirmks3w2lfagzqc809lx2lln6y213a0";
+    })
+  ];
 
   buildInputs = [ openssl ];
 
