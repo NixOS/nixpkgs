d327ac13f03ec7200e1ed7ab8b0f0d157468517b
Cyril Cohen <cohen@crans.org>
2021-02-19 19:34:30 +0100

    coqPackages: etc
    - updated coqPackage.heq (broken url)
    - fixed use of `DESTDIR` and `COQMF_COQLIB` in mkCoqDerivation

--- pkgs/development/coq-modules/coq-bits/default.nix
+++ pkgs/development/coq-modules/coq-bits/default.nix
@@ -1,4 +1,4 @@
-{ lib, mkCoqDerivation, coq, mathcomp, version ? null }:
+{ lib, mkCoqDerivation, coq, mathcomp-algebra, version ? null }:
 
 with lib; mkCoqDerivation {
   pname = "coq-bits";
@@ -9,12 +9,7 @@ with lib; mkCoqDerivation {
   release."20190812".rev    = "1.0.0";
   release."20190812".sha256 = "0nv5mdgrd075dpd8bc7h0xc5i95v0pkm0bfyq5rj6ii1s54dwcjl";
 
-  propagatedBuildInputs = [ mathcomp.algebra ];
-
-  installPhase = ''
-    make -f Makefile CoqMakefile
-    make -f CoqMakefile COQLIB=$out/lib/coq/${coq.coq-version}/ install
-  '';
+  propagatedBuildInputs = [ mathcomp-algebra ];
 
   meta = {
     description = "A formalization of bitset operations in Coq";
