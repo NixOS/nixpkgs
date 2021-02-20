b6c9bcfd6f1f29382a3c2cfa5007ca25957012de
Frederik Rietdijk <fridh@fridh.nl>
2021-02-20 10:09:18 +0100

    python3Packages.urllib3: 1.26.2 -> 1.26.3

--- pkgs/development/python-modules/urllib3/default.nix
+++ pkgs/development/python-modules/urllib3/default.nix
@@ -4,11 +4,11 @@
 
 buildPythonPackage rec {
   pname = "urllib3";
-  version = "1.26.2";
+  version = "1.26.3";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "19188f96923873c92ccb987120ec4acaa12f0461fa9ce5d3d0772bc965a39e08";
+    sha256 = "de3eedaad74a2683334e282005cd8d7f22f4d55fa690a2a1020a416cb0a47e73";
   };
 
   NOSE_EXCLUDE = lib.concatStringsSep "," [
