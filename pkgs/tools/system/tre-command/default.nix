7e711976bfbe93807dcd60dae81dca0fdc176a15
Daniel Duan <daniel@duan.ca>
2021-02-20 15:44:54 -0800

    tre-command: 0.3.3 -> 0.3.4
    Release note: https://github.com/dduan/tre/releases/tag/v0.3.4

--- pkgs/tools/system/tre-command/default.nix
+++ pkgs/tools/system/tre-command/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "tre-command";
-  version = "0.3.3";
+  version = "0.3.4";
 
   src = fetchFromGitHub {
     owner = "dduan";
     repo = "tre";
     rev = "v${version}";
-    sha256 = "10c8mpqzpw7m3vrm2vl2rx678z3c37hxpqyh3fn83dlh9f4f0j87";
+    sha256 = "0syvhpnw9c5csxv8c4gdfwif9a9vl4rjkwj4mfglgxk227k1y53q";
   };
 
-  cargoSha256 = "0jd6cfs2zi2n34kirpsy12l76whaqwm1pkqa57w1ms5z658z07wj";
+  cargoSha256 = "056wlxz8hzky8315rnn65nh7dd2yhx5323y3hq64g6aqj52vd734";
 
   nativeBuildInputs = [ installShellFiles ];
 
