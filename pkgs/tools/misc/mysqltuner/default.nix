4f1e0c7ec06b7c4a0bcacbce38127212ca94ae6b
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2021-02-20 17:46:39 +0100

    mysqltuner: add password list to output

--- pkgs/tools/misc/mysqltuner/default.nix
+++ pkgs/tools/misc/mysqltuner/default.nix
@@ -11,17 +11,29 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-Yv1XjD8sZcmGr2SVD6TEElUH7vspJ61WwQwfXLOrao0=";
   };
 
+  postPatch = ''
+    substituteInPlace mysqltuner.pl \
+      --replace '$basic_password_files = "/usr/share/mysqltuner/basic_passwords.txt"' "\$basic_password_files = \"$out/share/basic_passwords.txt\"" \
+      --replace '$opt{cvefile} = "/usr/share/mysqltuner/vulnerabilities.csv"' "\$opt{cvefile} = \"$out/share/vulnerabilities.csv\""
+  '';
+
   buildInputs = [ perl ];
 
   installPhase = ''
-    mkdir -p $out/bin
-    install -m0755 mysqltuner.pl $out/bin/mysqltuner
+    runHook preInstall
+
+    mkdir -p "$out/bin"
+    install -Dm 0755 mysqltuner.pl "$out/bin/mysqltuner"
+    install -Dm 0644 basic_passwords.txt "$out/share/basic_passwords.txt"
+    install -Dm 0644 vulnerabilities.csv "$out/share/vulnerabilities.csv"
+
+    runHook postInstall
   '';
 
   meta = with lib; {
     description = "Make recommendations for increased performance and stability of MariaDB/MySQL";
     homepage = "http://mysqltuner.com";
-    license = licenses.gpl3;
-    maintainers = with maintainers; [ peterhoeg ];
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ peterhoeg shamilton ];
   };
 }
