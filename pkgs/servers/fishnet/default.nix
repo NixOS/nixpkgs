05ab1773cce558218428bc62c5df8d8cb5f75d5a
R. RyanTM <ryantm-bot@ryantm.com>
2021-02-20 21:02:07 +0000

    fishnet: 2.2.4 -> 2.2.5

--- pkgs/servers/fishnet/default.nix
+++ pkgs/servers/fishnet/default.nix
@@ -12,16 +12,16 @@ let
 in
 rustPlatform.buildRustPackage rec {
   pname = "fishnet";
-  version = "2.2.4";
+  version = "2.2.5";
 
   src = fetchFromGitHub {
     owner = "niklasf";
     repo = pname;
     rev = "v${version}";
-    sha256 = "19dh69b6mqx16195w9d20fah4jl8hhbxm84xq4zwsgl4khmw7zqz";
+    sha256 = "sha256-SAEhb0R2wbo8xk/xknGv29F0/8ZaxTXkwX+l+hRPLj4=";
   };
 
-  cargoSha256 = "0zl2fnmqncyjd52wkn6dddx9lm9ywpw7swy895yq299z2bbbkv3h";
+  cargoSha256 = "sha256-sgSMYWXu8vvNk4jra/aZoCF+2KK5RzzJqKdm9y2AHkM=";
 
   preBuild = ''
     rmdir ./assets
