ef6de88e515e6ba1cfa8155f0515068420229cc2
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-02-21 00:01:29 +0100

    pythonPackages.gpsoauth: add pythonImportsCheck

--- pkgs/development/python-modules/gpsoauth/default.nix
+++ pkgs/development/python-modules/gpsoauth/default.nix
@@ -26,6 +26,11 @@ buildPythonPackage rec {
 
   propagatedBuildInputs = [ cffi cryptography enum34 idna ipaddress ndg-httpsclient pyopenssl pyasn1 pycparser pycryptodomex requests six ];
 
+  # no tests executed
+  doCheck = false;
+
+  pythonImportsCheck = [ "gpsoauth" ];
+
   meta = with lib; {
     description = "A python client library for Google Play Services OAuth";
     homepage = "https://github.com/simon-weber/gpsoauth";
