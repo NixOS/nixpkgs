e2abd532869a1a0efb25bf6ee16ec0352e3cce7e
Fabian Affolter <mail@fabian-affolter.ch>
2021-02-20 16:26:49 +0100

    python3Packages.pyvolumio: init 0.1.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6600,6 +6600,8 @@ in {
 
   pyvmomi = callPackage ../development/python-modules/pyvmomi { };
 
+  pyvolumio = callPackage ../development/python-modules/pyvolumio { };
+
   pyvoro = callPackage ../development/python-modules/pyvoro { };
 
   pywal = callPackage ../development/python-modules/pywal { };
