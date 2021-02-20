c69fe03bd9610ee77ba2af39470728c44baf2d3e
Frederik Rietdijk <fridh@fridh.nl>
2021-02-20 10:09:16 +0100

    python3Packages.rnc2rng: 2.6.4 -> 2.6.5

--- pkgs/development/python-modules/rnc2rng/default.nix
+++ pkgs/development/python-modules/rnc2rng/default.nix
@@ -7,11 +7,11 @@
 
 buildPythonPackage rec {
   pname = "rnc2rng";
-  version = "2.6.4";
+  version = "2.6.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1kmp3iwxxyzjsd47j2sprd47ihhkwhb3yydih3af5bbfq0ibh1w8";
+    sha256 = "d354afcf0bf8e3b1e8f8d37d71a8fe5b1c0cf75cbd4b71364a9d90b5108a16e5";
   };
 
   propagatedBuildInputs = [ rply ];
