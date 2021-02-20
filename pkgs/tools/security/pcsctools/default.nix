3d9cbfa449fbdb22bbaab36e7f9c8cbd0659284f
Eduardo Sánchez Muñoz <esm@eduardosm.net>
2021-02-20 15:36:11 +0100

    pcsctools: set eduardosm as maintainer

--- pkgs/tools/security/pcsctools/default.nix
+++ pkgs/tools/security/pcsctools/default.nix
@@ -35,7 +35,7 @@ in stdenv.mkDerivation rec {
     description = "Tools used to test a PC/SC driver, card or reader";
     homepage = "http://ludovic.rousseau.free.fr/softwares/pcsc-tools/";
     license = licenses.gpl2Plus;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ eduardosm ];
     platforms = platforms.linux;
   };
 }
