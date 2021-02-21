6b81e73c4b5dc2a13bd82700149fcdcef79d2976
Tim Steinbach <NeQuissimus@users.noreply.github.com>
2021-02-21 16:01:57 -0500

    Update pkgs/development/python-modules/sagemaker/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/development/python-modules/sagemaker/default.nix
+++ pkgs/development/python-modules/sagemaker/default.nix
@@ -26,8 +26,7 @@ buildPythonPackage rec {
   ];
 
   meta = with lib; {
-    description =
-      "Library for training and deploying machine learning models on Amazon SageMaker";
+    description = "Library for training and deploying machine learning models on Amazon SageMaker";
     homepage = "https://github.com/aws/sagemaker-python-sdk/";
     license = licenses.asl20;
     maintainers = with maintainers; [ nequissimus ];
