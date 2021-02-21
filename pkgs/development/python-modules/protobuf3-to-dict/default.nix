340eff8b0dc08a8d40d6ab7060b75f4cc75e689d
Tim Steinbach <NeQuissimus@users.noreply.github.com>
2021-02-21 16:01:50 -0500

    Update pkgs/development/python-modules/protobuf3-to-dict/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/development/python-modules/protobuf3-to-dict/default.nix
+++ pkgs/development/python-modules/protobuf3-to-dict/default.nix
@@ -16,8 +16,7 @@ buildPythonPackage rec {
   propagatedBuildInputs = [ protobuf six ];
 
   meta = with lib; {
-    description =
-      "A teeny Python library for creating Python dicts from protocol buffers and the reverse";
+    description = "A teeny Python library for creating Python dicts from protocol buffers and the reverse";
     homepage = "https://github.com/kaporzhu/protobuf-to-dict";
     license = licenses.publicDomain;
     maintainers = with maintainers; [ nequissimus ];
