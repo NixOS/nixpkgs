d87a39df15983248c6de698d5265d440f2755963
Eric Bailey <eric@ericb.me>
2021-02-21 22:47:14 -0600

    kubernetes-helmPlugins: build rather than download

--- pkgs/applications/networking/cluster/helm/plugins/default.nix
+++ pkgs/applications/networking/cluster/helm/plugins/default.nix
@@ -1,36 +1,11 @@
-{ stdenv, fetchFromGitHub, fetchurl, fetchzip }:
-let
+{ callPackage }:
 
-  buildHelmPlugin = { name, ... }@attrs:
-    fetchzip (attrs // {
-      stripRoot = false;
-    });
-in {
+{
 
-  helm-s3 = let
-    pname = "helm-s3";
-    version = "0.10.0";
-  in buildHelmPlugin rec {
-    name = "${pname}-${version}";
-    url = "https://github.com/hypnoglow/helm-s3/releases/download/v${version}/helm-s3_${version}_linux_amd64.tar.gz";
-    sha256 = "sha256-pTux7HArWB5yO1Oehfd+ZpeGUziI2+wfUart5WfkQW4=";
+  helm-diff = callPackage ./helm-diff.nix {};
 
-    extraPostFetch = ''
-      mkdir $out/${pname}
-      GLOBIGNORE="$out/${pname}"
-      mv $out/* -t $out/${pname}
-    '';
-  };
+  helm-s3 = callPackage ./helm-s3.nix {};
 
-  helm-diff = buildHelmPlugin {
-    name = "helm-diff";
-    url = "https://github.com/databus23/helm-diff/releases/download/v3.1.3/helm-diff-linux.tgz";
-    sha256 = "sha256-oGmBPcCyUgq2YD4+CkGrbf6/JhzXJjmkaiqX/3a03aE=";
-  };
+  helm-secrets = callPackage ./helm-secrets.nix {};
 
-  helm-secrets = buildHelmPlugin {
-    name = "helm-secrets";
-    url = "https://github.com/jkroepke/helm-secrets/releases/download/v3.4.1/helm-secrets.tar.gz";
-    sha256 = "sha256-HXwbs/bXJXF75FbLB/En0jirCQnz8HpU3o9LeMyV0e8=";
-  };
 }
