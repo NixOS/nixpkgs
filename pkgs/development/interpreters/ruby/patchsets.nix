9db5a83465a173fef7204302ac08533b411d6359
Mario Rodas <marsam@users.noreply.github.com>
2021-02-22 04:20:00 +0000

    ruby_2_5: remove patchset
    ruby_2_5 was removed from nixpkgs in 1c33c0ad6b1b9a335c9a2c6747849309fb817eca

--- pkgs/development/interpreters/ruby/patchsets.nix
+++ pkgs/development/interpreters/ruby/patchsets.nix
@@ -1,11 +1,6 @@
 { patchSet, useRailsExpress, ops, patchLevel, fetchpatch }:
 
 {
-  "2.5.8" = ops useRailsExpress [
-    "${patchSet}/patches/ruby/2.5/head/railsexpress/01-fix-broken-tests-caused-by-ad.patch"
-    "${patchSet}/patches/ruby/2.5/head/railsexpress/02-improve-gc-stats.patch"
-    "${patchSet}/patches/ruby/2.5/head/railsexpress/03-more-detailed-stacktrace.patch"
-  ];
   "2.6.6" = ops useRailsExpress [
     "${patchSet}/patches/ruby/2.6/head/railsexpress/01-fix-broken-tests-caused-by-ad.patch"
     "${patchSet}/patches/ruby/2.6/head/railsexpress/02-improve-gc-stats.patch"
