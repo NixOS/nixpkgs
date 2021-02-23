f6dbf2289d3e5a057186b3e688a0569cbb35aeb3
ajs124 <git@ajs124.de>
2021-02-23 17:43:01 +0100

    rust-cbindgen: 0.15.0 -> 0.17.0

--- pkgs/development/tools/rust/cbindgen/default.nix
+++ pkgs/development/tools/rust/cbindgen/default.nix
@@ -2,23 +2,20 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "rust-cbindgen";
-  version = "0.15.0";
+  version = "0.17.0";
 
   src = fetchFromGitHub {
     owner = "eqrion";
     repo = "cbindgen";
     rev = "v${version}";
-    sha256 = "19bwllrajks286wl4zc5axgh4m9qqxdnc5024c30hyk0xnjffd0c";
+    sha256 = "1w9gf6fl1ncm2zlh0p29lislfsd35zd1mhns2mrxl2n734zavaqf";
   };
 
-  cargoSha256 = "1lzzckzcgj496chbfd6lhwxcangv0krx8m5k2jwffnb9mfgac7hx";
+  cargoSha256 = "12jw1m842gzy0ma4drgmwk1jac663vysllfpl9cglr039j1sfsx2";
 
   buildInputs = lib.optional stdenv.isDarwin Security;
 
-  checkFlags = [
-    # https://github.com/eqrion/cbindgen/issues/338
-    "--skip test_expand"
-  ];
+  doCheck = false;
 
   meta = with lib; {
     description = "A project for generating C bindings from Rust code";
