c4b2992b9469840c8cd36de9063aa8b370c50071
Arthur Gautier <baloo@superbaloo.net>
2021-02-23 04:50:13 +0000

    pythonPackages.tpm2-pytss: init at 0.2.4
    Signed-off-by: Arthur Gautier <baloo@superbaloo.net>

---
+++ pkgs/development/python-modules/tpm2-pytss/default.nix
@@ -0,0 +1,42 @@
+{ lib, buildPythonPackage, fetchPypi, pythonOlder
+, pkg-config, swig
+, tpm2-tss
+, cryptography, pytest
+}:
+
+buildPythonPackage rec {
+  pname = "tpm2-pytss";
+
+  # Last version on github is 0.2.4, but it looks
+  # like a mistake (it's missing commits from 0.1.9)
+  version = "0.1.9";
+  disabled = pythonOlder "3.5";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-v5Xth0A3tFnLFg54nvWYL2TD201e/GWv+2y5Qc60CmU=";
+  };
+  postPatch = ''
+    substituteInPlace tpm2_pytss/config.py --replace \
+      'SYSCONFDIR = CONFIG.get("sysconfdir", "/etc")' \
+      'SYSCONFDIR = "${tpm2-tss}/etc"'
+  '';
+
+  nativeBuildInputs = [ pkg-config swig ];
+  buildInputs = [ tpm2-tss ];
+
+  checkInputs = [ cryptography pytest ];
+  checkPhase = ''
+    # Most of the tests requires a simulator (tpm_server from ibm-sw-tpm2)
+    # that simulator looks broken in tcti mode
+
+    pytest tests/test_binding.py tests/test_util_retry.py
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/tpm2-software/tpm2-pytss";
+    description = "TPM2 TSS Python bindings for Enhanced System API (ESYS)";
+    license = licenses.bsd2;
+    maintainers = with maintainers; [ baloo ];
+  };
+}
