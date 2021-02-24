4cc49b2b225e688db44d210470b83f941f2e181e
Maxine Aubrey <maxeaubrey@gmail.com>
2021-02-25 00:04:39 +0100

    yaru-theme: update licenses
    based on https://github.com/ubuntu/yaru/blob/1b74404c407ebf5a8487e9f138173701af8cb3e0/debian/copyright

--- pkgs/data/themes/yaru/default.nix
+++ pkgs/data/themes/yaru/default.nix
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Ubuntu community theme 'yaru' - default Ubuntu theme since 18.10";
     homepage = "https://github.com/ubuntu/yaru";
-    license = with licenses; [ cc-by-sa-40 gpl3 ];
+    license = with licenses; [ cc-by-sa-40 gpl3Plus lgpl21Only lgpl3Only ];
     platforms = platforms.linux;
     maintainers = [ maintainers.jD91mZM2 ];
   };
