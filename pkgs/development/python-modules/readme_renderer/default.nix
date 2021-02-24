62a76604dcedd879fcb2d2f54031f5b558dd17ce
Fabian Affolter <mail@fabian-affolter.ch>
2021-02-24 11:31:26 +0100

    python3Packages.readme_renderer: update check part

--- pkgs/development/python-modules/readme_renderer/default.nix
+++ pkgs/development/python-modules/readme_renderer/default.nix
@@ -1,14 +1,13 @@
 { lib
-, buildPythonPackage
-, fetchPypi
-, pytest
-, mock
-, cmarkgfm
 , bleach
+, buildPythonPackage
+, cmarkgfm
 , docutils
+, fetchPypi
 , future
+, mock
 , pygments
-, six
+, pytestCheckHook
 , pythonOlder
 }:
 
@@ -22,21 +21,25 @@ buildPythonPackage rec {
     sha256 = "sha256-kv1awr+Gd/MQ8zA6pLzludX58glKuYwp8TeR17gFo9s=";
   };
 
-  checkInputs = [ pytest mock ];
-
   propagatedBuildInputs = [
-    bleach cmarkgfm docutils future pygments six
+    bleach
+    cmarkgfm
+    docutils
+    future
+    pygments
   ];
 
-  checkPhase = ''
-    # disable one failing test case
-    # fixtures test is failing for incorrect class name
-    py.test -k "not test_invalid_link and not fixtures"
-  '';
+  checkInputs = [
+    mock
+    pytestCheckHook
+  ];
 
-  meta = {
-    description = "readme_renderer is a library for rendering readme descriptions for Warehouse";
+  pythonImportsCheck = [ "readme_renderer" ];
+
+  meta = with lib; {
+    description = "Python library for rendering readme descriptions";
     homepage = "https://github.com/pypa/readme_renderer";
-    license = lib.licenses.asl20;
+    license = with licenses; [ asl20 ];
+    maintainers = with maintainers; [ fab ];
   };
 }
