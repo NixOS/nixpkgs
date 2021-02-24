b4976c28fa8527965015a1e6580c874fa32ea918
Ivan Babrou <github@ivan.computer>
2021-02-24 09:02:59 -0800

    python3Packages.parso: fix flaky test due to slow moving time

--- pkgs/development/python-modules/parso/default.nix
+++ pkgs/development/python-modules/parso/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, fetchpatch
 , pythonOlder
 , pytestCheckHook
 }:
@@ -14,6 +15,15 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "2b6db14759c528d857eeb9eac559c2166b2554548af39f5198bdfb976f72aa64";
   };
+
+  patches = [
+    # Fix the flaky test due to slow moving time on Apple Silicon chips.
+    # Remove when https://github.com/davidhalter/parso/pull/177 is in the next release.
+    (fetchpatch {
+      url = "https://github.com/davidhalter/parso/pull/177/commits/2799a7a3c2cf87fdc2d0c19a0890acea425091ce.patch";
+      sha256 = "sha256-A5EQly1wR/7lo+L8Pp0UPSUIhC0WcblXEWQNvRMlZYA=";
+    })
+  ];
 
   checkInputs = [ pytestCheckHook ];
 
