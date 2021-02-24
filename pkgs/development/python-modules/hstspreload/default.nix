aed071d0bbd9c3ffb69f9538a983b19c9b5de090
Fabian Affolter <mail@fabian-affolter.ch>
2021-02-24 23:21:41 +0100

    python3Packages.hstspreload: 2021.2.1 -> 2021.2.15

--- pkgs/development/python-modules/hstspreload/default.nix
+++ pkgs/development/python-modules/hstspreload/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "hstspreload";
-  version = "2021.2.1";
+  version = "2021.2.15";
   disabled = isPy27;
 
   src = fetchFromGitHub {
     owner = "sethmlarson";
     repo = pname;
     rev = version;
-    sha256 = "sha256-R6tqGxGd6JymFgQX+deDPOtlKlwUjL7uf+zGdNxUW/s=";
+    sha256 = "sha256-vHq4DjDh7hBNAK/h/KdzqaEgrG5bg7VQ8fVWuxV7vOg=";
   };
 
   # tests require network connection
