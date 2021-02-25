9e9062b54c2b3bd6266d46b488d4a128c9582363
Jan Solanti <jhs@psonet.com>
2021-02-25 16:36:18 +0200

    spirv-headers: 1.5.3 -> 2020-01-27

--- pkgs/development/libraries/spirv-headers/default.nix
+++ pkgs/development/libraries/spirv-headers/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "spirv-headers";
-  version = "1.5.3";
+  version = "2020-01-27";
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "SPIRV-Headers";
-    rev = version;
-    sha256 = "069sivqajp7z4p44lmrz23lvf237xpkjxd4lzrg27836pwqcz9bj";
+    rev = "faa570afbc91ac73d594d787486bcf8f2df1ace0";
+    sha256 = "jgjBTkPTGl4aU4nk/ZdLHUneC5/o3PCCnlmbCtQjLsY=";
   };
 
   nativeBuildInputs = [ cmake ];
