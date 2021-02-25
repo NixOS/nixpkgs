c5bd117275d326b6a687f0d537f0519406bd2c09
Fabian Affolter <mail@fabian-affolter.ch>
2021-02-25 08:48:53 +0100

    python3Packages.pytest-subtests: 0.3.2 -> 0.4.0

--- pkgs/development/python-modules/pytest-subtests/default.nix
+++ pkgs/development/python-modules/pytest-subtests/default.nix
@@ -2,12 +2,12 @@
 
 buildPythonPackage rec {
   pname = "pytest-subtests";
-  version = "0.3.2";
+  version = "0.4.0";
   disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1mxg91mrn8672f8hwg0f31xkyarnq7q0hr4fvb9hcb09jshq2wk7";
+    sha256 = "sha256-jZ4sHR3OEfe30snQkgLr/HdXt/8MrJtyrTKO3+fuA3s=";
   };
 
   nativeBuildInputs = [ setuptools_scm ];
