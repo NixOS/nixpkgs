NixOS/nixpkgs

commit 81cf1908827b3921931bee827825ec1175daa406

sherpa: fix build w/glibc-2.32

pkgs/applications/science/physics/sherpa/default.nix
@@ -9,6 +9,10 @@ stdenv.mkDerivation rec {
     sha256 = "1iwa17s8ipj6a2b8zss5csb1k5y9s5js38syvq932rxcinbyjsl4";
   };
 
+  postPatch = ''
+    sed -ie '/sys\/sysctl.h/d' ATOOLS/Org/Run_Parameter.C
+  '';
+
   buildInputs = [ gfortran sqlite lhapdf rivet ];
 
   enableParallelBuilding = true;

commit 5b9a3edb205a76490ead177f49ceabadbe821bf4

sherpa: fix on darwin
Fixes: 81cf1908 ('sherpa: fix build w/glibc-2.32')

pkgs/applications/science/physics/sherpa/default.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
     sha256 = "1iwa17s8ipj6a2b8zss5csb1k5y9s5js38syvq932rxcinbyjsl4";
   };
 
-  postPatch = ''
+  postPatch = lib.optional (stdenv.hostPlatform.libc == "glibc") ''
     sed -ie '/sys\/sysctl.h/d' ATOOLS/Org/Run_Parameter.C
   '';
 