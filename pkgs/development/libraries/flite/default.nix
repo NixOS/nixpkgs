2af1630a9b4fb0f394e0c368dec87d13aabf286b
Rick van Schijndel <rol3517@gmail.com>
2021-02-27 00:17:51 +0100

    flite: improve cross-compilation support

--- pkgs/development/libraries/flite/default.nix
+++ pkgs/development/libraries/flite/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, alsaLib }:
+{ lib, stdenv, fetchFromGitHub, alsaLib, fetchpatch }:
 
 stdenv.mkDerivation rec {
   pname = "flite";
@@ -12,6 +12,16 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = lib.optionals stdenv.isLinux [ alsaLib ];
+
+  # https://github.com/festvox/flite/pull/60.
+  # Replaces `ar` with `$(AR)` in config/common_make_rules.
+  # Improves cross-compilation compatibility.
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/festvox/flite/commit/54c65164840777326bbb83517568e38a128122ef.patch";
+      sha256 = "sha256-hvKzdX7adiqd9D+9DbnfNdqEULg1Hhqe1xElYxNM1B8=";
+    })
+  ];
 
   configureFlags = [
     "--enable-shared"
