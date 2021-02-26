60785fe4dbedc4fd2a4a4980c59357a27b57aabf
Vincent Laporte <Vincent.Laporte@gmail.com>
2021-02-26 17:35:41 +0100

    ocamlPackages.dune: rename into dune_1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2220,7 +2220,7 @@ in
 
   duf = callPackage ../tools/misc/duf { };
 
-  inherit (ocamlPackages) dune dune_2 dune-release;
+  inherit (ocamlPackages) dune_1 dune_2 dune-release;
 
   duperemove = callPackage ../tools/filesystems/duperemove { };
 
