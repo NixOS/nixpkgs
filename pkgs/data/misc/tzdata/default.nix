109cbc400f4dd472d91f1f7768f42d7faac0cddb
Robert Schütz <dev@schuetz-co.de>
2021-02-26 10:56:10 +0100

    Revert "Fix tzdata cross-compilation"
    This reverts commit 963513e6540cb5ef85c1d6410bc7cdd937f95bdb.
    A better solution is a65208ca8e593e2478b0848e6b5cbda7530a1707.

--- pkgs/data/misc/tzdata/default.nix
+++ pkgs/data/misc/tzdata/default.nix
@@ -42,11 +42,10 @@ stdenv.mkDerivation rec {
 
   installFlags = [ "ZIC=./zic-native" ];
 
-  # NOTE: in cross-compilation scenarios, this is built for the build platform.
   preInstall = ''
      mv zic.o zic.o.orig
      mv zic zic.orig
-     make $makeFlags cc=cc AR=ar zic
+     make $makeFlags cc=${stdenv.cc.targetPrefix}cc AR=${stdenv.cc.targetPrefix}ar zic
      mv zic zic-native
      mv zic.o.orig zic.o
      mv zic.orig zic
