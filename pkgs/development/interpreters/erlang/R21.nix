7993fc70af9e8e1b8b91ec93aecf3e98377d1e8a
Ivan Babrou <github@ivan.computer>
2021-02-27 12:34:56 -0800

    erlangR21: 21.3.8.3 -> 21.3.8.21

--- pkgs/development/interpreters/erlang/R21.nix
+++ pkgs/development/interpreters/erlang/R21.nix
@@ -1,8 +1,8 @@
 { mkDerivation }:
 
 mkDerivation {
-  version = "21.3.8.3";
-  sha256 = "1szybirrcpqsl2nmlmpbkxjqnm6i7l7bma87m5cpwi0kpvlxwmcw";
+  version = "21.3.8.21";
+  sha256 = "sha256-zQCs2hOA66jxAaxl/B42EKCejAktIav2rpVQCNyKCh4=";
 
   prePatch = ''
     substituteInPlace configure.in --replace '`sw_vers -productVersion`' "''${MACOSX_DEPLOYMENT_TARGET:-10.12}"
