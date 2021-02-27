86702daaf2dc49f455582bb55994ac0e116b308f
Ivan Babrou <github@ivan.computer>
2021-02-26 20:25:15 -0800

    ocamlPackages.ocaml: 4.10.0 -> 4.10.2

--- pkgs/development/compilers/ocaml/4.10.nix
+++ pkgs/development/compilers/ocaml/4.10.nix
@@ -1,6 +1,6 @@
 import ./generic.nix {
   major_version = "4";
   minor_version = "10";
-  patch_version = "0";
-  sha256 = "1dzy7lbdapcmwn1wg8k87419khi54f8hb6n314bdv7v0gfqlswrh";
+  patch_version = "2";
+  sha256 = "sha256-locUYQeCgtXbAiB32JveJchfteN2YStE+MN9ToTwAOM=";
 }
