5bbfc16b281d7c6b87ef8b1b56e8b15f8ced47bd
R. RyanTM <ryantm-bot@ryantm.com>
2021-02-27 05:11:55 +0000

    python37Packages.adafruit-platformdetect: 3.1.1 -> 3.2.0

--- pkgs/development/python-modules/adafruit-platformdetect/default.nix
+++ pkgs/development/python-modules/adafruit-platformdetect/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "Adafruit-PlatformDetect";
-  version = "3.1.1";
+  version = "3.2.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-JcqDuTzR2sffEbmhJHRPJggLruc9lKQ4aO/Ab88yo/I=";
+    sha256 = "a14c642b1f6849e57b5c442e81e809c9e1e3a0bcfebfd54b15a9f9fd273f9611";
   };
 
   nativeBuildInputs = [ setuptools-scm ];
