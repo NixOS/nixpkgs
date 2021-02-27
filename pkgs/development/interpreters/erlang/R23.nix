428ff5779ae6b4335f2c1edfb1a7969d013f9700
Ivan Babrou <github@ivan.computer>
2021-02-27 12:32:51 -0800

    erlangR23: 23.1.4 -> 23.2.6

--- pkgs/development/interpreters/erlang/R23.nix
+++ pkgs/development/interpreters/erlang/R23.nix
@@ -3,8 +3,8 @@
 # How to obtain `sha256`:
 # nix-prefetch-url --unpack https://github.com/erlang/otp/archive/OTP-${version}.tar.gz
 mkDerivation {
-  version = "23.1.4";
-  sha256 = "16ssxmrgjgvzg06aa1l4wz9rrdjfy39k22amgabxwb5if1g8bg8z";
+  version = "23.2.6";
+  sha256 = "sha256-G930sNbr8h5ryI/IE+J6OKhR5ij68ZhGo1YIEjSOwGU=";
 
   prePatch = ''
     substituteInPlace make/configure.in --replace '`sw_vers -productVersion`' "''${MACOSX_DEPLOYMENT_TARGET:-10.12}"
