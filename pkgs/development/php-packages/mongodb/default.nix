NixOS/nixpkgs

commit 5ad7fb28f6a8732eee23fa33d47fa25444ff3837

treewide/php-packages: Drop pkgs from arguments to packages

pkgs/development/php-packages/mongodb/default.nix
@@ -1,4 +1,5 @@
-{ buildPecl, lib, pkgs, pcre' }:
+{ stdenv, buildPecl, lib, pcre', pkg-config, cyrus_sasl, icu64
+, openssl, snappy, zlib, darwin }:
 
 buildPecl {
   pname = "mongodb";
@@ -6,15 +7,15 @@ buildPecl {
   version = "1.9.0";
   sha256 = "16mbw3p80qxsj86nmjbfch8wv6jaq8wbz4rlpmixvhj9nwbp37hs";
 
-  nativeBuildInputs = [ pkgs.pkg-config ];
-  buildInputs = with pkgs; [
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [
     cyrus_sasl
     icu64
     openssl
     snappy
     zlib
     pcre'
-  ] ++ lib.optional (pkgs.stdenv.isDarwin) pkgs.darwin.apple_sdk.frameworks.Security;
+  ] ++ lib.optionals (stdenv.isDarwin) darwin.apple_sdk.frameworks.Security;
 
   meta.maintainers = lib.teams.php.members;
 }

pkgs/development/php-packages/box/default.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, fetchurl, pkgs, lib, php }:
+{ mkDerivation, fetchurl, makeWrapper, lib, php }:
 let
   pname = "box";
   version = "2.7.5";
@@ -12,7 +12,7 @@ mkDerivation {
   };
 
   phases = [ "installPhase" ];
-  nativeBuildInputs = [ pkgs.makeWrapper ];
+  nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
     mkdir -p $out/bin

pkgs/development/php-packages/composer/default.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, fetchurl, pkgs, lib, php }:
+{ mkDerivation, fetchurl, makeWrapper, unzip, lib, php }:
 let
   pname = "composer";
   version = "1.10.15";
@@ -13,17 +13,17 @@ mkDerivation {
 
   dontUnpack = true;
 
-  nativeBuildInputs = [ pkgs.makeWrapper ];
+  nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
     mkdir -p $out/bin
     install -D $src $out/libexec/composer/composer.phar
     makeWrapper ${php}/bin/php $out/bin/composer \
       --add-flags "$out/libexec/composer/composer.phar" \
-      --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.unzip ]}
+      --prefix PATH : ${lib.makeBinPath [ unzip ]}
   '';
 
-  meta = with pkgs.lib; {
+  meta = with lib; {
     description = "Dependency Manager for PHP";
     license = licenses.mit;
     homepage = "https://getcomposer.org/";

pkgs/development/php-packages/couchbase/default.nix
@@ -1,4 +1,4 @@
-{ buildPecl, lib, pkgs, php }:
+{ lib, buildPecl, fetchFromGitHub, writeText, libcouchbase, zlib, php }:
 let
   pname = "couchbase";
   version = "2.6.2";
@@ -6,7 +6,7 @@ in
 buildPecl {
   inherit pname version;
 
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "couchbase";
     repo = "php-couchbase";
     rev = "v${version}";
@@ -16,12 +16,12 @@ buildPecl {
   configureFlags = [ "--with-couchbase" ];
   broken = lib.versionAtLeast php.version "8.0";
 
-  buildInputs = with pkgs; [ libcouchbase zlib ];
+  buildInputs = [ libcouchbase zlib ];
   internalDeps = [] ++ lib.optionals (lib.versionOlder php.version "8.0") [ php.extensions.json ];
   peclDeps = [ php.extensions.igbinary ];
 
   patches = [
-    (pkgs.writeText "php-couchbase.patch" ''
+    (writeText "php-couchbase.patch" ''
       --- a/config.m4
       +++ b/config.m4
       @@ -9,7 +9,7 @@ if test "$PHP_COUCHBASE" != "no"; then
@@ -29,7 +29,7 @@ buildPecl {
          else
            AC_MSG_CHECKING(for libcouchbase in default path)
       -    for i in /usr/local /usr; do
-      +    for i in ${pkgs.libcouchbase}; do
+      +    for i in ${libcouchbase}; do
              if test -r $i/include/libcouchbase/couchbase.h; then
                LIBCOUCHBASE_DIR=$i
                AC_MSG_RESULT(found in $i)

pkgs/development/php-packages/event/default.nix
@@ -1,4 +1,4 @@
-{ buildPecl, lib, pkgs, php }:
+{ buildPecl, lib, php, pkg-config, openssl, libevent }:
 buildPecl {
   pname = "event";
 
@@ -6,7 +6,7 @@ buildPecl {
   sha256 = "1ws4l014z52vb23xbsfj6viwkf7fmh462af639xgbp0n6syf77dq";
 
   configureFlags = [
-    "--with-event-libevent-dir=${pkgs.libevent.dev}"
+    "--with-event-libevent-dir=${libevent.dev}"
     "--with-event-core"
     "--with-event-extra"
     "--with-event-pthreads"
@@ -18,11 +18,11 @@ buildPecl {
       ':'
   '';
 
-  nativeBuildInputs = [ pkgs.pkg-config ];
-  buildInputs = with pkgs; [ openssl libevent ];
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ openssl libevent ];
   internalDeps = [ php.extensions.sockets ];
 
-  meta = with pkgs.lib; {
+  meta = with lib; {
     description = ''
       This is an extension to efficiently schedule I/O, time and signal based
       events using the best I/O notification mechanism available for specific platform.

pkgs/development/php-packages/imagick/default.nix
@@ -1,4 +1,4 @@
-{ buildPecl, fetchpatch, lib, pkgs, pcre' }:
+{ buildPecl, fetchpatch, lib, imagemagick7, pkg-config, pcre' }:
 
 buildPecl {
   pname = "imagick";
@@ -19,8 +19,8 @@ buildPecl {
     })
   ];
 
-  configureFlags = [ "--with-imagick=${pkgs.imagemagick7.dev}" ];
-  nativeBuildInputs = [ pkgs.pkg-config ];
+  configureFlags = [ "--with-imagick=${imagemagick7.dev}" ];
+  nativeBuildInputs = [ pkg-config ];
   buildInputs = [ pcre' ];
 
   meta.maintainers = lib.teams.php.members;

pkgs/development/php-packages/maxminddb/default.nix
@@ -1,4 +1,4 @@
-{ buildPecl, lib, pkgs }:
+{ buildPecl, lib, fetchFromGitHub, libmaxminddb }:
 let
   pname = "maxminddb";
   version = "1.10.0";
@@ -6,17 +6,17 @@ in
 buildPecl {
   inherit pname version;
 
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "maxmind";
     repo = "MaxMind-DB-Reader-php";
     rev = "v${version}";
     sha256 = "sha256-2SnajDdO5uAYcuVpEbOuFlZzMxwo/EqFtUSr9XxT0KQ=";
   };
 
-  buildInputs = [ pkgs.libmaxminddb ];
+  buildInputs = [ libmaxminddb ];
   sourceRoot = "source/ext";
 
-  meta = with pkgs.lib; {
+  meta = with lib; {
     description = "C extension that is a drop-in replacement for MaxMind\\Db\\Reader";
     license = with licenses; [ asl20 ];
     maintainers = with maintainers; [ ajs124 das_j ] ++ teams.php.members;

pkgs/development/php-packages/memcached/default.nix
@@ -1,4 +1,4 @@
-{ buildPecl, lib, fetchgit, php, pkgs }:
+{ buildPecl, lib, fetchgit, php, cyrus_sasl, zlib, pkg-config, libmemcached }:
 let
   pname = "memcached";
   version = "3.1.5";
@@ -19,12 +19,12 @@ buildPecl {
   ];
 
   configureFlags = [
-    "--with-zlib-dir=${pkgs.zlib.dev}"
-    "--with-libmemcached-dir=${pkgs.libmemcached}"
+    "--with-zlib-dir=${zlib.dev}"
+    "--with-libmemcached-dir=${libmemcached}"
   ];
 
-  nativeBuildInputs = [ pkgs.pkg-config ];
-  buildInputs = with pkgs; [ cyrus_sasl zlib ];
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ cyrus_sasl zlib ];
 
   meta.maintainers = lib.teams.php.members;
 }

commit d2b35b0fc6da5771ebf8c9b5ff6081c18e58de86

php-packages/mongodb: fix evaluation on darwin
Regressed in 5ad7fb28 (PR #114551).
It was a channel blocker, through the tarball job:
https://hydra.nixos.org/build/137987705

pkgs/development/php-packages/mongodb/default.nix
@@ -15,7 +15,7 @@ buildPecl {
     snappy
     zlib
     pcre'
-  ] ++ lib.optionals (stdenv.isDarwin) darwin.apple_sdk.frameworks.Security;
+  ] ++ lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Security;
 
   meta.maintainers = lib.teams.php.members;
 }