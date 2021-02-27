8d782cc605582c82271bd613642037aa046e54df
Ryan Burns <rtburns@protonmail.com>
2021-02-27 01:45:06 -0800

    Revert "adoptopenjdk-bin: add support for GTK+ Look and Feel"
    This reverts commit 714eca6967a0017b3228966871a9a3803ab3effb.

--- pkgs/development/compilers/adoptopenjdk-bin/jdk-linux-base.nix
+++ pkgs/development/compilers/adoptopenjdk-bin/jdk-linux-base.nix
@@ -4,31 +4,16 @@ sourcePerArch:
 , lib
 , fetchurl
 , autoPatchelfHook
-, makeWrapper
-# minimum dependencies
 , alsaLib
-, fontconfig
 , freetype
-, libffi
-, xorg
+, fontconfig
 , zlib
-# runtime dependencies
-, cups
-# runtime dependencies for GTK+ Look and Feel
-, gtkSupport ? true
-, cairo
-, glib
-, gtk3
+, xorg
+, libffi
 }:
 
 let
   cpuName = stdenv.hostPlatform.parsed.cpu.name;
-  runtimeDependencies = [
-    cups
-  ] ++ lib.optionals gtkSupport [
-    cairo glib gtk3
-  ];
-  runtimeLibraryPath = lib.makeLibraryPath runtimeDependencies;
 in
 
 let result = stdenv.mkDerivation rec {
@@ -43,19 +28,11 @@ let result = stdenv.mkDerivation rec {
   };
 
   buildInputs = [
-    alsaLib # libasound.so wanted by lib/libjsound.so
-    fontconfig
-    freetype
-    stdenv.cc.cc.lib # libstdc++.so.6
-    xorg.libX11
-    xorg.libXext
-    xorg.libXi
-    xorg.libXrender
-    xorg.libXtst
-    zlib
+    alsaLib freetype fontconfig zlib xorg.libX11 xorg.libXext xorg.libXtst
+    xorg.libXi xorg.libXrender stdenv.cc.cc.lib
   ] ++ lib.optional stdenv.isAarch32 libffi;
 
-  nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
+  nativeBuildInputs = [ autoPatchelfHook ];
 
   # See: https://github.com/NixOS/patchelf/issues/10
   dontStrip = 1;
@@ -80,13 +57,6 @@ let result = stdenv.mkDerivation rec {
     cat <<EOF >> "$out/nix-support/setup-hook"
     if [ -z "\''${JAVA_HOME-}" ]; then export JAVA_HOME=$out; fi
     EOF
-
-    # We cannot use -exec since wrapProgram is a function but not a command.
-    for bin in $( find "$out" -executable -type f ); do
-      if patchelf --print-interpreter "$bin" &> /dev/null; then
-        wrapProgram "$bin" --prefix LD_LIBRARY_PATH : "${runtimeLibraryPath}"
-      fi
-    done
   '';
 
   preFixup = ''
