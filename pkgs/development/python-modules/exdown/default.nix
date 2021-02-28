31dcf063d8f20dcdac6c15e5808646cda2772301
Robert Schütz <dev@schuetz-co.de>
2021-02-28 10:10:51 +0100

    pythonPackages.exdown: requires Python 3

--- pkgs/development/python-modules/exdown/default.nix
+++ pkgs/development/python-modules/exdown/default.nix
@@ -1,10 +1,12 @@
-{ lib, buildPythonPackage, fetchPypi, pythonOlder
+{ lib, buildPythonPackage, isPy27, fetchPypi, pythonOlder
 , importlib-metadata }:
 
 buildPythonPackage rec {
   pname = "exdown";
   version = "0.7.1";
   format = "pyproject";
+
+  disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
