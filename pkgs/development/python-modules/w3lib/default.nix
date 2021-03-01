9e18a6780f6a7f929a38bbf1391fd30ca85c0d10
Anders Kaseorg <andersk@mit.edu>
2021-03-01 11:39:56 -0800

    python3Packages.w3lib: Fix build failure with Python 3.8.8
    In Python 3.6.13, 3.7.10, 3.8.8, and 3.9.2, urllib.parse.parse_qsl no
    longer treats ; as a separator by default to fix CVE-2021-23336
    (https://bugs.python.org/issue42967). This broke one of the tests for
    w3lib (https://github.com/scrapy/w3lib/issues/164).
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- pkgs/development/python-modules/w3lib/default.nix
+++ pkgs/development/python-modules/w3lib/default.nix
@@ -3,6 +3,7 @@
 , fetchPypi
 , six
 , pytest
+, fetchpatch
 }:
 
 buildPythonPackage rec {
@@ -13,6 +14,14 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "1pv02lvvmgz2qb61vz1jkjc04fgm4hpfvaj5zm4i3mjp64hd1mha";
   };
+
+  patches = [
+    # Remove test broken by Python CVE-2021-23336 fix
+    (fetchpatch {
+      url = "https://github.com/scrapy/w3lib/pull/165/commits/78054f19bfe20555792b0f336b423921fe88b994.patch";
+      sha256 = "1fb40gk2dk6rlap7nxm5sii3id054plx0yq2gkzcs10gpv1a8vpc";
+    })
+  ];
 
   buildInputs = [ six pytest ];
 
