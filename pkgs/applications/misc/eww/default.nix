f361275753a7fbd7bf18fac4bebdfb1f88dd9032
wunderbrick <wunderbrick@lambdafuzz.com>
2021-03-01 16:00:58 -0500

    Remove import for Rust overlay

--- pkgs/applications/misc/eww/default.nix
+++ pkgs/applications/misc/eww/default.nix
@@ -1,9 +1,26 @@
-{ lib, stdenv, callPackage, fetchFromGitHub, pkgconfig, rustPlatform, glib
-, cairo, pango, atk, gdk_pixbuf, gtk3-x11 }:
+{ lib, stdenv, callPackage, fetchFromGitHub, pkgconfig, makeRustPlatform
+, rustPlatform, glib, cairo, pango, atk, gdk_pixbuf, gtk3-x11 }:
 
 let
-  mkRustPlatform = callPackage ./nightly-rust-platform.nix { };
+  mkRustPlatform =
+    { callPackage, fetchFromGitHub, makeRustPlatform, date, channel }:
+
+    let
+      mozillaOverlay = fetchFromGitHub {
+        owner = "mozilla";
+        repo = "nixpkgs-mozilla";
+        rev = "8c007b60731c07dd7a052cce508de3bb1ae849b4";
+        sha256 = "1zybp62zz0h077zm2zmqs2wcg3whg6jqaah9hcl1gv4x8af4zhs6";
+      };
+      mozilla = callPackage "${mozillaOverlay.out}/package-set.nix" { };
+      rustSpecific = (mozilla.rustChannelOf { inherit date channel; }).rust;
+    in makeRustPlatform {
+      cargo = rustSpecific;
+      rustc = rustSpecific;
+    };
+
   rustPlatform = mkRustPlatform {
+    inherit callPackage fetchFromGitHub makeRustPlatform;
     date = "2020-10-28";
     channel = "nightly";
   };
