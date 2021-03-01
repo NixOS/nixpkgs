14797ce13e3213ff59245e03f4cdf5f765ef3f19
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-01 23:30:56 +0100

    gitleaks: 7.2.0 -> 7.2.2

--- pkgs/tools/security/gitleaks/default.nix
+++ pkgs/tools/security/gitleaks/default.nix
@@ -1,17 +1,17 @@
-{ buildGoModule
+{ lib
+, buildGoModule
 , fetchFromGitHub
-, lib
 }:
 
 buildGoModule rec {
   pname = "gitleaks";
-  version = "7.2.0";
+  version = "7.2.2";
 
   src = fetchFromGitHub {
     owner = "zricethezav";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1pdbkjx8h6ijypsxyv34lykymaqf8wnfyjk3ldp49apbx01bl34y";
+    sha256 = "sha256-G/7Ezyfp9vkG1QHTG9Xg6mZ3qhQpx952i7rsSr3fFwY=";
   };
 
   vendorSha256 = "0kk8ci7vprqw4v7cigspshfd13k2wyy4pdkxf11pqc2fz8j07kh9";
