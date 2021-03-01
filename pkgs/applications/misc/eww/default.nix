6f8f37c24d54a3401177f0ef18869754061332e3
wunderbrick <wunderbrick@lambdafuzz.com>
2021-03-01 15:17:13 -0500

    Run nixfmt to satisfy editorconfig-checker

--- pkgs/applications/misc/eww/default.nix
+++ pkgs/applications/misc/eww/default.nix
@@ -1,14 +1,13 @@
-{ lib, stdenv, callPackage
-, fetchFromGitHub, pkgconfig, rustPlatform
-, glib, cairo, pango
-, atk, gdk_pixbuf, gtk3-x11
-}:
+{ lib, stdenv, callPackage, fetchFromGitHub, pkgconfig, rustPlatform, glib
+, cairo, pango, atk, gdk_pixbuf, gtk3-x11 }:
 
 let
-  mkRustPlatform = callPackage ./nightly-rust-platform.nix {};
-  rustPlatform = mkRustPlatform { date = "2020-10-28"; channel = "nightly"; };
-in
-rustPlatform.buildRustPackage rec {
+  mkRustPlatform = callPackage ./nightly-rust-platform.nix { };
+  rustPlatform = mkRustPlatform {
+    date = "2020-10-28";
+    channel = "nightly";
+  };
+in rustPlatform.buildRustPackage rec {
   pname = "eww";
   version = "unstable-2021-02-26";
 
@@ -28,15 +27,15 @@ rustPlatform.buildRustPackage rec {
   # For the files Cargo nightly will put in $HOME
   preConfigure = ''
     export HOME=$(mktemp -d)
-    '';
+  '';
 
   meta = with lib; {
     homepage = "https://elkowar.github.io/eww/main/";
     description = "ElKowar's widgeting system written in Rust";
     longDescription = ''
-    Eww (ElKowar's Wacky Widgets, pronounced with sufficient amounts of disgust) is a widgeting system made in Rust, which lets you create your own widgets similarly to how you can in AwesomeWM. The key difference: it is independent of your window manager!
+      Eww (ElKowar's Wacky Widgets, pronounced with sufficient amounts of disgust) is a widgeting system made in Rust, which lets you create your own widgets similarly to how you can in AwesomeWM. The key difference: it is independent of your window manager!
 
-    Configured in XML and themed using CSS, it is easy to customize and provides all the flexibility you need!
+      Configured in XML and themed using CSS, it is easy to customize and provides all the flexibility you need!
     '';
     license = licenses.mit;
     maintainers = with maintainers; [ wunderbrick ];
--- pkgs/applications/misc/eww/nightly-rust-platform.nix
+++ pkgs/applications/misc/eww/nightly-rust-platform.nix
@@ -9,10 +9,9 @@ let
     rev = "8c007b60731c07dd7a052cce508de3bb1ae849b4";
     sha256 = "1zybp62zz0h077zm2zmqs2wcg3whg6jqaah9hcl1gv4x8af4zhs6";
   };
-  mozilla = callPackage "${mozillaOverlay.out}/package-set.nix" {};
+  mozilla = callPackage "${mozillaOverlay.out}/package-set.nix" { };
   rustSpecific = (mozilla.rustChannelOf { inherit date channel; }).rust;
-in
-makeRustPlatform {
+in makeRustPlatform {
   cargo = rustSpecific;
   rustc = rustSpecific;
 }
