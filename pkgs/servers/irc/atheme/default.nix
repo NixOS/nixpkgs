b5fc00619cb73d0042fd74db5936ec84883e7220
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-03-02 21:51:52 +0100

    atheme: 7.2.10-r2 -> 7.2.11

--- pkgs/servers/irc/atheme/default.nix
+++ pkgs/servers/irc/atheme/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   pname = "atheme";
-  version = "7.2.10-r2";
+  version = "7.2.11";
 
   src = fetchgit {
     url = "https://github.com/atheme/atheme.git";
     rev = "v${version}";
-    sha256 = "1yasfvbmixj4zzfv449hlcp0ms5c250lrshdy6x6l643nbnix4y9";
+    sha256 = "15fs48cgzxblh2g4abl5v647ndfx9hg8cih2x67v3y7s9wz68wk2";
     leaveDotGit = true;
   };
 
