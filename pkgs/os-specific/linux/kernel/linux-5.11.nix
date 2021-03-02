003fcee3cd06afd91183df40e7782b7dd9b58897
TredwellGit <tredwell@tutanota.com>
2021-03-02 11:19:31 +0000

    linux_5_11: 5.11.1 -> 5.11.2
    https://cdn.kernel.org/pub/linux/kernel/v5.x/ChangeLog-5.11.2

--- pkgs/os-specific/linux/kernel/linux-5.11.nix
+++ pkgs/os-specific/linux/kernel/linux-5.11.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.11.1";
+  version = "5.11.2";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1gmrckvl3039z80rr740c0d5knwgj6p1dmhw4x9gwc7rxli6az85";
+    sha256 = "186ha9fsk2qvrjkq7yvpmml938byz92m8ykcvbw4w9pmp8y5njlh";
   };
 } // (args.argsOverride or {}))
