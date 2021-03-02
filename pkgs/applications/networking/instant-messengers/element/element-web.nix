c3554253abb7e6aa137a4007752d0980ec12099c
TredwellGit <tredwell@tutanota.com>
2021-03-02 22:16:39 +0000

    element: 1.7.21 -> 1.7.22
    https://github.com/vector-im/element-web/releases/tag/v1.7.22
    https://nvd.nist.gov/vuln/detail/CVE-2021-21320

--- pkgs/applications/networking/instant-messengers/element/element-web.nix
+++ pkgs/applications/networking/instant-messengers/element/element-web.nix
@@ -12,11 +12,11 @@ let
 
 in stdenv.mkDerivation rec {
   pname = "element-web";
-  version = "1.7.21";
+  version = "1.7.22";
 
   src = fetchurl {
     url = "https://github.com/vector-im/element-web/releases/download/v${version}/element-v${version}.tar.gz";
-    sha256 = "sha256-JJXl+jDlXw8fZ1ZeeAACvilbqG9zanCmBsHy6BEla8M=";
+    sha256 = "1aaa986h38kkrnyhb1y65d73idsxmkmi201511az9zlz9210ih59";
   };
 
   installPhase = ''
