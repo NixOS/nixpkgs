993fb95e7832e62e8de93735ffcd2006bc693fc5
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-03-02 16:56:43 +0100

    qemu: remove not require enableParallelBuild

--- pkgs/applications/virtualization/qemu/default.nix
+++ pkgs/applications/virtualization/qemu/default.nix
@@ -77,7 +77,6 @@ stdenv.mkDerivation rec {
     ++ optionals libiscsiSupport [ libiscsi ]
     ++ optionals smbdSupport [ samba ];
 
-  enableParallelBuilding = true;
   dontUseMesonConfigure = true; # meson's configurePhase isn't compatible with qemu build
 
   outputs = [ "out" "ga" ];
