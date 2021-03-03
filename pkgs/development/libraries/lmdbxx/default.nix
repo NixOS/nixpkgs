4aa1fa8e83b5f64c6789eabcb5ccbe67667ddea6
Francesco Gazzetta <fgaz@fgaz.me>
2021-03-03 22:19:23 +0100

    lmdbxx: switch to active fork, 0.9.14.0 -> 1.0.0
    The original project is dead since 5 years. The fork was already adopted
    by gentoo and by the downstream application nheko.

--- pkgs/development/libraries/lmdbxx/default.nix
+++ pkgs/development/libraries/lmdbxx/default.nix
@@ -1,26 +1,27 @@
-{ lib, stdenv
+{ lib
+, stdenv
 , fetchFromGitHub
-, lmdb }:
+, lmdb
+}:
 
 stdenv.mkDerivation rec {
   pname = "lmdbxx";
-  version = "0.9.14.0";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
-    owner = "drycpp";
+    owner = "hoytech";
     repo = "lmdbxx";
     rev = version;
-    sha256 = "1jmb9wg2iqag6ps3z71bh72ymbcjrb6clwlkgrqf1sy80qwvlsn6";
+    sha256 = "12k5rz74d1l0skcks9apry1svkl96g9lf5dcgylgjmh7v1jm0b7c";
   };
 
   buildInputs = [ lmdb ];
   makeFlags = [ "PREFIX=$(out)" ];
 
   meta = {
-    homepage = "https://github.com/drycpp/lmdbxx#readme";
+    homepage = "https://github.com/hoytech/lmdbxx#readme";
     description = "C++11 wrapper for the LMDB embedded B+ tree database library";
     license = lib.licenses.unlicense;
     maintainers = with lib.maintainers; [ fgaz ];
   };
 }
-
