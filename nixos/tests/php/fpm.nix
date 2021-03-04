5e5f482f32ea399128893a359fec1a451f62b443
Maximilian Bosch <maximilian@mbosch.me>
2021-03-04 16:06:37 +0100

    nixos/tests/php: add apcu to php to actually test whether extensions are properly loaded

--- nixos/tests/php/fpm.nix
+++ nixos/tests/php/fpm.nix
@@ -50,7 +50,7 @@ import ../make-test-python.nix ({pkgs, lib, php, ...}: {
     assert "PHP Version ${php.version}" in response, "PHP version not detected"
 
     # Check so we have database and some other extensions loaded
-    for ext in ["json", "opcache", "pdo_mysql", "pdo_pgsql", "pdo_sqlite"]:
+    for ext in ["json", "opcache", "pdo_mysql", "pdo_pgsql", "pdo_sqlite", "apcu"]:
         assert ext in response, f"Missing {ext} extension"
         machine.succeed(f'test -n "$(php -m | grep -i {ext})"')
   '';
