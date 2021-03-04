9ee0c71e4f462207f4b9c846e4860e2b11d01d0d
R. RyanTM <ryantm-bot@ryantm.com>
2021-03-04 01:23:26 +0000

    nats-streaming-server: 0.20.0 -> 0.21.0

--- pkgs/servers/nats-streaming-server/default.nix
+++ pkgs/servers/nats-streaming-server/default.nix
@@ -4,14 +4,14 @@ with lib;
 
 buildGoPackage rec {
   pname   = "nats-streaming-server";
-  version = "0.20.0";
+  version = "0.21.0";
   goPackagePath = "github.com/nats-io/${pname}";
 
   src = fetchFromGitHub {
     rev    = "v${version}";
     owner  = "nats-io";
     repo   = pname;
-    sha256 = "sha256-vhUj3CVBM5jbwEtnzdgQD3eLguiHQguK01O69JZIUUk=";
+    sha256 = "sha256-vZFX5YDf722c7xc4oh4SoL91s7XuZsXzFxW+i//mOUw=";
   };
 
   meta = {
