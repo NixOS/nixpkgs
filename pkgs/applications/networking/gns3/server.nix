f6e94a2e21081298ef950f89b90fd7747adb564d
Michael Weiss <dev.primeos@gmail.com>
2021-03-05 18:46:02 +0100

    gns3-{gui,server}: Remove myself as maintainer

--- pkgs/applications/networking/gns3/server.nix
+++ pkgs/applications/networking/gns3/server.nix
@@ -62,6 +62,6 @@ in python.pkgs.buildPythonPackage {
     changelog = "https://github.com/GNS3/gns3-server/releases/tag/v${version}";
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
-    maintainers = with maintainers; [ primeos ];
+    maintainers = with maintainers; [ ];
   };
 }
