7d4f998b07b660da1a1744d5852797967c8db112
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-03-05 03:31:26 +0100

    pythonPackages.pyte: fix cross compile

--- pkgs/development/python-modules/pyte/default.nix
+++ pkgs/development/python-modules/pyte/default.nix
@@ -1,21 +1,19 @@
-{ lib, buildPythonPackage, fetchPypi, pytest, pytestrunner, wcwidth }:
+{ lib, buildPythonPackage, fetchPypi, pytestCheckHook, pytest-runner, wcwidth }:
 
 buildPythonPackage rec {
   pname = "pyte";
   version = "0.8.0";
+
   src = fetchPypi {
     inherit pname version;
     sha256 = "7e71d03e972d6f262cbe8704ff70039855f05ee6f7ad9d7129df9c977b5a88c5";
   };
 
+  nativeBuildInputs = [ pytest-runner ];
+
   propagatedBuildInputs = [ wcwidth ];
 
-  checkInputs = [ pytest pytestrunner ];
-
-  # tries to write to os.path.dirname(__file__) in test_input_output
-  checkPhase = ''
-    py.test -k "not test_input_output"
-  '';
+  checkInputs = [ pytestCheckHook ];
 
   meta = with lib; {
     description = "Simple VTXXX-compatible linux terminal emulator";
