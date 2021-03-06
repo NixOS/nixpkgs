a2100d3c78121cf723944f97319426c6d4d019b8
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-06 13:55:19 +0100

    python3Packages.defusedxml: 0.6.0 -> 0.7.0

--- pkgs/development/python-modules/defusedxml/default.nix
+++ pkgs/development/python-modules/defusedxml/default.nix
@@ -2,10 +2,10 @@
 
 buildPythonPackage rec {
   pname = "defusedxml";
-  version = "0.6.0";
+  version = "0.7.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f684034d135af4c6cbb949b8a4d2ed61634515257a67299e5f940fbaa34377f5";
+    sha256 = "sha256-hrFdnjxjneefTLOK7/6jKB9ir/eN3n15jhNSxjv6bqA=";
   };
 }
