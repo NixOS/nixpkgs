95bbeedede7c8ce4765641478c5ba321d1ed8633
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-03-06 12:07:57 +0100

    ocamlPackages.hxd: 0.2.0 -> 0.3.1
    ocamlPackages.duff: fix tests for hxd >= 0.3.0 by applying patch from
                        https://github.com/mirage/duff/pull/9

--- pkgs/development/ocaml-modules/duff/default.nix
+++ pkgs/development/ocaml-modules/duff/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchurl, buildDunePackage
+{ lib, fetchurl, buildDunePackage, fetchpatch
 , stdlib-shims, bigarray-compat, fmt
 , alcotest, hxd, crowbar, bigstringaf
 }:
@@ -13,6 +13,14 @@ buildDunePackage rec {
     url = "https://github.com/mirage/duff/releases/download/v${version}/duff-v${version}.tbz";
     sha256 = "1lb67yxk93ifj94p1i3swjbnj5xy8j6xzs72bwvq6cffx5xykznm";
   };
+
+  patches = [
+    # compatibility with hxd >= 0.3.0
+    (fetchpatch {
+      url = "https://github.com/mirage/duff/commit/da0737975aaca313ee4ad9f1e46db1e969672a5b.patch";
+      sha256 = "0qbqzfn2rv867fjcd607cjbnvrfh8fzxlp9mn5r7jx5v83l0zjjq";
+    })
+  ];
 
   propagatedBuildInputs = [ stdlib-shims bigarray-compat fmt ];
 
