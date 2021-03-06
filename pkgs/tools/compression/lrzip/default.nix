e0ba4d7673a67b962987928890d61379a9086c1e
ajs124 <git@ajs124.de>
2021-03-06 04:15:02 +0100

    lrzip: 0.640 -> 0.641

--- pkgs/tools/compression/lrzip/default.nix
+++ pkgs/tools/compression/lrzip/default.nix
@@ -1,12 +1,12 @@
 {lib, stdenv, fetchurl, zlib, lzo, bzip2, lz4, nasm, perl}:
 
 stdenv.mkDerivation rec {
-  version = "0.640";
+  version = "0.641";
   pname = "lrzip";
 
   src = fetchurl {
     url = "http://ck.kolivas.org/apps/lrzip/${pname}-${version}.tar.xz";
-    sha256 = "175466drfpz8rsfr0pzfn5rqrj3wmcmcs3i2sfmw366w2kbjm4j9";
+    sha256 = "0ziyanspd96dc3lp2qdcylc7aq8dhb511jhqrhxvlp502fjqjqrc";
   };
 
   buildInputs = [ zlib lzo bzip2 lz4 nasm perl ];
