9001fbc439737a68d9fdb2dc12e1114bd4c4d906
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-06 14:12:11 +0100

    python3Packages.defusedxml: add meta

--- pkgs/development/python-modules/defusedxml/default.nix
+++ pkgs/development/python-modules/defusedxml/default.nix
@@ -1,11 +1,25 @@
-{ buildPythonPackage, fetchPypi }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "defusedxml";
   version = "0.7.0";
+  disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-hrFdnjxjneefTLOK7/6jKB9ir/eN3n15jhNSxjv6bqA=";
+    sha256 = "183fz8xwclhkirwpvpldyypn47r8lgzfz2mk9jgyg7b37jg5vcc6";
+  };
+
+  pythonImportsCheck = [ "defusedxml" ];
+
+  meta = with lib; {
+    description = "Python module to defuse XML issues";
+    homepage = "https://github.com/tiran/defusedxml";
+    license = licenses.psfl;
+    maintainers = with maintainers; [ fab ];
   };
 }
