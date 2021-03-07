f22a0ec393e0366d90639999eaae5c3a71adf102
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-07 22:22:37 +0100

    python3Packages.sanic: allow building without tests

--- pkgs/development/python-modules/sanic/default.nix
+++ pkgs/development/python-modules/sanic/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, fetchPypi
+{ lib, buildPythonPackage, fetchPypi, doCheck ? true
 , aiofiles, httptools, httpx, multidict, ujson, uvloop, websockets
 , pytestCheckHook, beautifulsoup4, gunicorn, httpcore, uvicorn
 , pytest-asyncio, pytest-benchmark, pytest-dependency, pytest-sanic, pytest-sugar, pytestcov
@@ -13,7 +13,7 @@ buildPythonPackage rec {
     sha256 = "06p0lsxqbfbka2yaqlpp0bg5pf7ma44zi6kq7qbb6hhry48dp1w6";
   };
 
-  patchPhase = ''
+  postPatch = ''
     substituteInPlace setup.py \
       --replace '"multidict==5.0.0"' '"multidict"' \
       --replace '"httpx==0.15.4"' '"httpx"' \
@@ -30,13 +30,18 @@ buildPythonPackage rec {
     pytest-asyncio pytest-benchmark pytest-dependency pytest-sanic pytest-sugar pytestcov
   ];
 
+  inherit doCheck;
+
   disabledTests = [
     "test_gunicorn" # No "examples" directory in pypi distribution.
     "test_logo" # Fails to filter out "DEBUG asyncio:selector_events.py:59 Using selector: EpollSelector"
     "test_zero_downtime" # No "examples.delayed_response.app" module in pypi distribution.
+    "test_reloader_live" # OSError: [Errno 98] error while attempting to bind on address ('127.0.0.1', 42104)
   ];
 
   __darwinAllowLocalNetworking = true;
+
+  pythonImportsCheck = [ "sanic" ];
 
   meta = with lib; {
     description = "A microframework based on uvloop, httptools, and learnings of flask";
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6257,7 +6257,9 @@ in {
   pytest-runner = self.pytestrunner; # added 2021-01-04
   pytestrunner = callPackage ../development/python-modules/pytestrunner { };
 
-  pytest-sanic = callPackage ../development/python-modules/pytest-sanic { };
+  pytest-sanic = callPackage ../development/python-modules/pytest-sanic {
+    sanic = self.sanic.override { doCheck = false; };
+  };
 
   pytest-server-fixtures = callPackage ../development/python-modules/pytest-server-fixtures { };
 
