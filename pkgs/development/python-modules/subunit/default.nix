cf23ddab2721b0ddbba1fd0f0c6f26b502691cd9
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-03-07 02:00:54 +0100

    pythonPackages.subunit: switch to pytestCheckHook

--- pkgs/development/python-modules/subunit/default.nix
+++ pkgs/development/python-modules/subunit/default.nix
@@ -4,11 +4,10 @@
 , cppunit
 , pkg-config
 , subunit
-
 # python dependencies
 , fixtures
 , hypothesis
-, pytest
+, pytestCheckHook
 , testscenarios
 , testtools
 }:
@@ -17,15 +16,21 @@ buildPythonPackage {
   inherit (subunit) name src meta;
 
   nativeBuildInputs = [ pkg-config ];
+
   buildInputs = [ check cppunit ];
+
   propagatedBuildInputs = [ testtools ];
 
-  checkInputs = [ testscenarios hypothesis fixtures pytest ];
+  checkInputs = [ testscenarios hypothesis fixtures pytestCheckHook ];
+
+  pytestFlagsArray = [
+    "python/subunit"
+  ];
+
   # ignore tests which call shell code, or call methods which haven't been implemented
-  checkPhase = ''
-    pytest python/subunit \
-      --ignore=python/subunit/tests/test_{output_filter,test_protocol{,2}}.py
-  '';
+  disabledTestPaths = [
+      "python/subunit/tests/test_{output_filter,test_protocol{,2}}.py"
+  ];
 
   patchPhase = ''
     sed -i 's/version=VERSION/version="${subunit.version}"/' setup.py
