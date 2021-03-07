00f033ad8381749843f2ad49393533eae78038b8
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-07 09:21:42 +0100

    python3Packages.typing-inspect: fix build for Python > 3.9

--- pkgs/development/python-modules/typing-inspect/default.nix
+++ pkgs/development/python-modules/typing-inspect/default.nix
@@ -1,9 +1,11 @@
 { lib
 , buildPythonPackage
+, fetchpatch
 , fetchPypi
-, typing-extensions
 , mypy-extensions
-, isPy39
+, pytestCheckHook
+, typing-extensions
+, pythonAtLeast
 }:
 
 buildPythonPackage rec {
@@ -16,16 +18,26 @@ buildPythonPackage rec {
     sha256 = "1dzs9a1pr23dhbvmnvms2jv7l7jk26023g5ysf0zvnq8b791s6wg";
   };
 
-  propagatedBuildInputs = [
-    typing-extensions
-    mypy-extensions
+  patches = lib.optional (pythonAtLeast "3.9") [
+    (fetchpatch {
+      name = "fix-for-py39.patch";
+      url = "https://github.com/ilevkivskyi/typing_inspect/commit/16919e21936179e53df2f376c8b59b5fc44bd2dd.patch";
+      sha256 = "17gvha4z685q04lvy8r0xhddhvqh1bfk1avs5jqbwislpsd83396";
+    })
   ];
+
+  propagatedBuildInputs = [
+    mypy-extensions
+    pytestCheckHook
+    typing-extensions
+  ];
+
+  pythonImportsCheck = [ "typing_inspect" ];
 
   meta = with lib; {
     description = "Runtime inspection utilities for Python typing module";
     homepage = "https://github.com/ilevkivskyi/typing_inspect";
     license = licenses.mit;
     maintainers = with maintainers; [ albakham ];
-    broken = isPy39;  # see https://github.com/ilevkivskyi/typing_inspect/issues/65
   };
 }
