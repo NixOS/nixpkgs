b0bf039676a94e3015cd3bdf9e602b399236f926
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-03-07 20:27:56 +0100

    pythonPackages.semver: 2.10.2 -> 2.13.0

--- pkgs/development/python-modules/semver/default.nix
+++ pkgs/development/python-modules/semver/default.nix
@@ -7,20 +7,24 @@
 
 buildPythonPackage rec {
   pname = "semver";
-  version = "2.10.2";
+  version = "2.13.0";
 
   src = fetchFromGitHub {
     owner = "python-semver";
     repo = "python-semver";
     rev = version;
-    sha256 = "0yxjmcgk5iwp53l9z1cg0ajrj18i09ircs11ifpdrggzm8n1blf3";
+    sha256 = "sha256-IWTo/P9JRxBQlhtcH3JMJZZrwAA8EALF4dtHajWUc4w=";
   };
 
-  preCheck = "rm -rf dist"; # confuses source vs dist imports in pytest
+  # confuses source vs dist imports in pytest
+  preCheck = ''
+    rm -r dist
+  '';
+
   checkInputs = [ pytestCheckHook pytestcov ];
 
   meta = with lib; {
-    description = "Python package to work with Semantic Versioning (http://semver.org/)";
+    description = "Python package to work with Semantic Versioning";
     homepage = "https://python-semver.readthedocs.io/en/latest/";
     license = licenses.bsd3;
     maintainers = with maintainers; [ np ];
