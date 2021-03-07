e1d7e8b111aca6ec3989b0e66088d05821c76141
Michael Weiss <dev.primeos@gmail.com>
2021-03-07 12:04:12 +0100

    chromium: Make get-commit-message.py more robust
    Not all updates contain security fixes.
    
    (cherry picked from commit 17c4f6fa36b35b3bb92e465dd103bd927c8be8c9)

--- pkgs/applications/networking/browsers/chromium/get-commit-message.py
+++ pkgs/applications/networking/browsers/chromium/get-commit-message.py
@@ -21,15 +21,15 @@ for entry in feed.entries:
         continue
     #print(url)  # For debugging purposes
     version = re.search(r'\d+(\.\d+){3}', content).group(0)
-    fixes = re.search(r'This update includes .+ security fixes\.', content).group(0)
-    fixes = html_tags.sub('', fixes)
-    zero_days = re.search(r'Google is aware of reports that .+ in the wild\.', content)
-    if zero_days:
-        fixes += " " + zero_days.group(0)
-    cve_list = re.findall(r'CVE-[^: ]+', content)
-    cve_string = ' '.join(cve_list)
-    print('chromium: TODO -> ' + version + '\n')
-    print(url + '\n')
-    print('\n'.join(textwrap.wrap(fixes, width=72)) + '\n')
-    print("CVEs:\n" + '\n'.join(textwrap.wrap(cve_string, width=72)))
+    print('chromium: TODO -> ' + version)
+    print('\n' + url)
+    if fixes := re.search(r'This update includes .+ security fixes\.', content):
+        fixes = html_tags.sub('', fixes.group(0))
+        zero_days = re.search(r'Google is aware of reports that .+ in the wild\.', content)
+        if zero_days:
+            fixes += " " + zero_days.group(0)
+        print('\n' + '\n'.join(textwrap.wrap(fixes, width=72)))
+    if cve_list := re.findall(r'CVE-[^: ]+', content):
+        cve_string = ' '.join(cve_list)
+        print("\nCVEs:\n" + '\n'.join(textwrap.wrap(cve_string, width=72)))
     break  # We only care about the most recent stable channel update
