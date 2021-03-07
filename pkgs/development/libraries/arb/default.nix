9718302d11cf50b3ff7a9b8114a3fb4157dc660b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-03-07 15:46:04 +0100

    arb: format

--- pkgs/development/libraries/arb/default.nix
+++ pkgs/development/libraries/arb/default.nix
@@ -1,26 +1,32 @@
-{lib, stdenv, fetchFromGitHub, mpir, gmp, mpfr, flint}:
+{ lib, stdenv, fetchFromGitHub, mpir, gmp, mpfr, flint }:
+
 stdenv.mkDerivation rec {
   pname = "arb";
   version = "2.17.0";
+
   src = fetchFromGitHub {
     owner = "fredrik-johansson";
     repo = pname;
     rev = version;
     sha256 = "05lpy3hkl5f8ik19aw40cqydrb932xaf2n8hbq9ib5dnk7f010p1";
   };
-  buildInputs = [mpir gmp mpfr flint];
+
+  buildInputs = [ mpir gmp mpfr flint ];
+
   configureFlags = [
     "--with-gmp=${gmp}"
     "--with-mpir=${mpir}"
     "--with-mpfr=${mpfr}"
     "--with-flint=${flint}"
   ];
+
   doCheck = true;
+
   meta = with lib; {
     description = "A library for arbitrary-precision interval arithmetic";
     homepage = "https://arblib.org/";
-    license = lib.licenses.lgpl21Plus;
+    license = licenses.lgpl21Plus;
     maintainers = teams.sage.members;
-    platforms = lib.platforms.unix;
+    platforms = platforms.unix;
   };
 }
