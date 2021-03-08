3521dc45edbb76e13ec29d2a08b15d3f369413c3
Frederik Rietdijk <fridh@fridh.nl>
2021-03-08 19:36:51 +0100

    python3Packages.aiohttp: fixup package set
    In b1d8335904dbdee98696a3094e1d918f40a40367 pytestCheckHook was overridden.
    Overriding as is done in this commit breaks cross. Therefore, I attempted
    to use one of the spliced sets but that did not work and got accidentally
    committed and pushed.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -251,7 +251,7 @@ in {
   aiohomekit = callPackage ../development/python-modules/aiohomekit { };
 
   aiohttp = callPackage ../development/python-modules/aiohttp {
-    pytestCheckHook = self.pkgsBuildHost.pytestCheckHook.override { pytest = self.pytest_6_1; };
+    pytestCheckHook = self.pytestCheckHook.override { pytest = self.pytest_6_1; };
   };
 
   aiohttp-cors = callPackage ../development/python-modules/aiohttp-cors { };
