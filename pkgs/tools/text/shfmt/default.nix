3ebace1a8f8a6cfe8174cfce021c0ba7c1b29591
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-03-08 21:05:12 +0100

    shfmt: add SuperSandro2000 as maintainer

--- pkgs/tools/text/shfmt/default.nix
+++ pkgs/tools/text/shfmt/default.nix
@@ -32,6 +32,6 @@ buildGoModule rec {
       You can feed it standard input, any number of files or any number of directories to recurse into.
     '';
     license = licenses.bsd3;
-    maintainers = with maintainers; [ zowoq ];
+    maintainers = with maintainers; [ zowoq SuperSandro2000 ];
   };
 }
