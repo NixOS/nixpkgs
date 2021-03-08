babf859184dfdea20946eeabf8ff6bd5ae28bca0
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-03-08 13:50:40 +0100

    trezord: format

--- pkgs/servers/trezord/default.nix
+++ pkgs/servers/trezord/default.nix
@@ -11,16 +11,16 @@ buildGoModule rec {
   version = "2.0.30";
 
   src = fetchFromGitHub {
-    owner  = "trezor";
-    repo   = "trezord-go";
-    rev    = "v${version}";
+    owner = "trezor";
+    repo = "trezord-go";
+    rev = "v${version}";
     sha256 = "1hzvk0wfgg7b4wpqjk3738yqxlv3pj5i7zxwm0jady2h97hmrqrr";
   };
 
   vendorSha256 = "0wb959xzyvr5zzjvkfqc422frmf97q5nr460f02wwx0pj6ch0y61";
 
   propagatedBuildInputs = lib.optionals stdenv.isLinux [ trezor-udev-rules ]
-                       ++ lib.optionals stdenv.isDarwin [ AppKit ];
+    ++ lib.optionals stdenv.isDarwin [ AppKit ];
 
   meta = with lib; {
     description = "Trezor Communication Daemon aka Trezor Bridge";
