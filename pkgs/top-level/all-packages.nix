64f91849d483e8201e9a6fc888e72c8fac6a2446
Owen Shepherd <owen@owen.cafe>
2021-03-08 00:46:01 +0000

    oranchelo-icon-theme: init at 0.8.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20949,6 +20949,8 @@ in
 
   openzone-cursors = callPackage ../data/themes/openzone { };
 
+  oranchelo-icon-theme = callPackage ../data/icons/oranchelo-icon-theme { };
+
   orbitron = callPackage ../data/fonts/orbitron { };
 
   orchis = callPackage ../data/themes/orchis { };
