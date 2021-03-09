b3490960b9d32ef34f873b2437cf2346171d2565
R. RyanTM <ryantm-bot@ryantm.com>
2021-03-09 12:20:49 +0000

    sane-airscan: 0.99.16 -> 0.99.24

--- pkgs/applications/graphics/sane/backends/airscan/default.nix
+++ pkgs/applications/graphics/sane/backends/airscan/default.nix
@@ -2,7 +2,7 @@
 , sane-backends, meson, ninja }:
 stdenv.mkDerivation rec {
   pname = "sane-airscan";
-  version = "0.99.16";
+  version = "0.99.24";
 
   nativeBuildInputs = [ meson ninja pkg-config ];
   buildInputs = [ avahi libsoup libjpeg libpng gnutls sane-backends ];
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
     owner = "alexpevzner";
     repo = pname;
     rev = version;
-    sha256 = "00lgcdbpns0shwlpkvrpfm4z05s7v5q3al4kcw6ii7xnkzmv1721";
+    sha256 = "sha256-2zSLC9P7Q/GMefHvmrUz6nV2hgScb4BhPAkahNBouqk=";
   };
 
   meta = with lib; {
