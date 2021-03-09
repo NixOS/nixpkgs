158252e6ff371cc9f20ce43bf1ba7c9df1ccc1a2
R. RyanTM <ryantm-bot@ryantm.com>
2021-03-09 15:59:58 +0000

    snd: 20.3 -> 21.1

--- pkgs/applications/audio/snd/default.nix
+++ pkgs/applications/audio/snd/default.nix
@@ -4,11 +4,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "snd-20.3";
+  name = "snd-21.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/snd/${name}.tar.gz";
-    sha256 = "016slh34gb6qqb38m8k9yg48rbhc5p12084szcwvanhh5v7fc7mk";
+    sha256 = "sha256-Y8q9wUEg7mhSDjnFD51NTnItrMzff/obX0bhHfq7u8s=";
   };
 
   nativeBuildInputs = [ pkg-config ];
