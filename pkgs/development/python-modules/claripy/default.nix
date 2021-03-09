40568b4910dcb5a9f2289a1763c67a2dd5f9b58f
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-09 16:53:10 +0100

    python3Packages.claripy: 9.0.5903 -> 9.0.6281

--- pkgs/development/python-modules/claripy/default.nix
+++ pkgs/development/python-modules/claripy/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "claripy";
-  version = "9.0.5903";
+  version = "9.0.6281";
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "angr";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-NIKWUx1VT5TjnuqppuT6VzwNRwcBLc0xI5k3F2Nmj8A=";
+    sha256 = "sha256-gvo8I6LQRAEUa7QiV5Sugrt+e2SmGkkKfsGn/IKz+Mk=";
   };
 
   # Use upstream z3 implementation
