97b64bd65e08f8e6d937f9f360aa5365c356bb33
pacien <pacien.trangirard@pacien.net>
2021-03-09 13:08:17 +0100

    fishPlugins.clownfish: init at unstable-2021-01-17

--- pkgs/shells/fish/plugins/default.nix
+++ pkgs/shells/fish/plugins/default.nix
@@ -4,6 +4,8 @@ lib.makeScope newScope (self: with self; {
 
   buildFishPlugin = callPackage ./build-fish-plugin.nix { };
 
+  clownfish = callPackage ./clownfish.nix { };
+
   fishtape = callPackage ./fishtape.nix { };
 
   foreign-env = callPackage ./foreign-env { };
