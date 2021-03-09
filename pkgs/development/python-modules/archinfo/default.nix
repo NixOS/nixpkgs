97aab48b52180a921a2965db8473d69f658886d1
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-09 16:44:19 +0100

    python3Packages.archinfo: 9.0.5903 -> 9.0.6281

--- pkgs/development/python-modules/archinfo/default.nix
+++ pkgs/development/python-modules/archinfo/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage rec {
   pname = "archinfo";
-  version = "9.0.5903";
+  version = "9.0.6281";
 
   src = fetchFromGitHub {
     owner = "angr";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-4e+ZGIt/ouZj5rsmaVxUrz8gAq4Yq2+Qx4jdOojB4Sw=";
+    sha256 = "sha256-ZO2P53RdR3cYhDbtrdGJnadFZgKkBdDi5gR/CB7YTpI=";
   };
 
   checkInputs = [
