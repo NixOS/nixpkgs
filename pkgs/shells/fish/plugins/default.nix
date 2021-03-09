d7145807ada972df1ba22dba54a1fc4e4f23fd47
pacien <pacien.trangirard@pacien.net>
2021-03-09 13:10:36 +0100

    fishPlugins.fishtape_3: init at 3.0.1
    Keeping fishtape 2.x and 3.x separate because they aren't compatible
    with each other and are still both being used to test different packages.

--- pkgs/shells/fish/plugins/default.nix
+++ pkgs/shells/fish/plugins/default.nix
@@ -6,7 +6,10 @@ lib.makeScope newScope (self: with self; {
 
   clownfish = callPackage ./clownfish.nix { };
 
+  # Fishtape 2.x and 3.x aren't compatible,
+  # but both versions are used in the tests of different other plugins.
   fishtape = callPackage ./fishtape.nix { };
+  fishtape_3 = callPackage ./fishtape_3.nix { };
 
   foreign-env = callPackage ./foreign-env { };
 
