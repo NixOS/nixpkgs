86ee8dd8ff48f2da3577077720adf587b72908b8
Milan <me@pbb.lc>
2021-03-10 12:42:58 +0100

    gitlab: 13.8.4 -> 13.8.5 (#115393)
    https://about.gitlab.com/releases/2021/03/04/security-release-gitlab-13-9-2-released/

--- pkgs/applications/version-management/gitlab/gitlab-workhorse/default.nix
+++ pkgs/applications/version-management/gitlab/gitlab-workhorse/default.nix
@@ -3,13 +3,13 @@
 buildGoModule rec {
   pname = "gitlab-workhorse";
 
-  version = "8.59.0";
+  version = "8.59.2";
 
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitlab-workhorse";
     rev = "v${version}";
-    sha256 = "sha256-6JVt5hNWvnUm64SFqns1f1+xMP/KyPQ8H93jqXNT8n8=";
+    sha256 = "sha256-hMcE7dlUw34DyUO0v5JxwvvEh/HC2emrIKc1K1U4bPE=";
   };
 
   vendorSha256 = "0vkw12w7vr0g4hf4f0im79y7l36d3ah01n1vl7siy94si47g8ir5";
