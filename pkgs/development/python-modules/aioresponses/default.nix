ce961c0ea41d2fc1cf173b119f47c920d3ac9fb6
R. RyanTM <ryantm-bot@ryantm.com>
2021-03-10 09:55:30 +0000

    python38Packages.aioresponses: 0.7.1 -> 0.7.2

--- pkgs/development/python-modules/aioresponses/default.nix
+++ pkgs/development/python-modules/aioresponses/default.nix
@@ -11,12 +11,12 @@
 
 buildPythonPackage rec {
   pname = "aioresponses";
-  version = "0.7.1";
+  version = "0.7.2";
   disabled = pythonOlder "3.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f65bba2be1e9a4997ee166bc0161a50be0fef7350ad09e6afdb2adccf74dfefe";
+    sha256 = "82e495d118b74896aa5b4d47e17effb5e2cc783e510ae395ceade5e87cabe89a";
   };
 
   nativeBuildInputs = [
