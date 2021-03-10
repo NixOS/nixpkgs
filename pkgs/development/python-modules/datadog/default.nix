NixOS/nixpkgs

commit 5c7461478b3b0fcfa315876fd3ec915242d68566

pythonPackages.datadog: fix tests

pkgs/development/python-modules/datadog/default.nix
@@ -1,6 +1,19 @@
-{ lib, buildPythonPackage, fetchPypi, pythonOlder
-, decorator, requests, simplejson, pillow, typing
-, nose, mock, pytest, freezegun }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, pythonOlder
+, decorator
+, requests
+, typing
+, configparser
+, click
+, freezegun
+, mock
+, pytestCheckHook
+, pytest-vcr
+, python-dateutil
+, vcrpy
+}:
 
 buildPythonPackage rec {
   pname = "datadog";
@@ -15,13 +28,30 @@ buildPythonPackage rec {
     find . -name '*.pyc' -exec rm {} \;
   '';
 
-  propagatedBuildInputs = [ decorator requests simplejson pillow ]
-    ++ lib.optionals (pythonOlder "3.5") [ typing ];
+  propagatedBuildInputs = [ decorator requests ]
+    ++ lib.optional (pythonOlder "3.5") typing
+    ++ lib.optional (pythonOlder "3.0") configparser;
 
-  checkInputs = [ nose mock pytest freezegun ];
-  checkPhase = ''
-    pytest tests/unit
-  '';
+  checkInputs = [
+    click
+    freezegun
+    mock
+    pytestCheckHook
+    pytest-vcr
+    python-dateutil
+    vcrpy
+  ];
+
+  disabledTestPaths = [
+    "tests/unit/dogstatsd/test_statsd.py" # does not work in sandbox
+  ];
+
+  disabledTests = [
+    "test_default_settings_set"
+    "test_threadstats_thread_safety"
+  ];
+
+  pythonImportsCheck = [ "datadog" ];
 
   meta = with lib; {
     description = "The Datadog Python library";

commit 0fcf2841e279b37dd9113760f74f32544cf99fa7

python27Packages.datadog: fix evaluation
pytest-vcr and vcrpy indirectly depend on construct which doesn't
support python 2.7, breaking the evaluation for python27Packages.datadog.

Having this package evaluate is desireable since this breaks most
notably nixops. This breakage was introduced in
5c7461478b3b0fcfa315876fd3ec915242d68566 and not caught by CI.

pkgs/development/python-modules/datadog/default.nix
@@ -2,6 +2,7 @@
 , buildPythonPackage
 , fetchPypi
 , pythonOlder
+, isPy27
 , decorator
 , requests
 , typing
@@ -32,6 +33,7 @@ buildPythonPackage rec {
     ++ lib.optional (pythonOlder "3.5") typing
     ++ lib.optional (pythonOlder "3.0") configparser;
 
+  doCheck = !isPy27;
   checkInputs = [
     click
     freezegun