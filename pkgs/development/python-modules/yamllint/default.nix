8a4f3d7b9ee7e2fb27ef710617b190188042f3af
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-10 10:09:35 +0100

    python3Packages.yamllint: enable tests

--- pkgs/development/python-modules/yamllint/default.nix
+++ pkgs/development/python-modules/yamllint/default.nix
@@ -1,26 +1,42 @@
-{ lib, buildPythonPackage, fetchPypi
-, nose, pyyaml, pathspec }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, pathspec
+, pytestCheckHook
+, pythonOlder
+, pyyaml
+}:
 
 buildPythonPackage rec {
   pname = "yamllint";
   version = "1.25.0";
+  disabled = pythonOlder "3.5";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "b1549cbe5b47b6ba67bdeea31720f5c51431a4d0c076c1557952d841f7223519";
   };
 
-  checkInputs = [ nose ];
+  propagatedBuildInputs = [
+    pyyaml
+    pathspec
+  ];
 
-  propagatedBuildInputs = [  pyyaml pathspec ];
+  checkInputs = [
+    pytestCheckHook
+  ];
 
-  # Two test failures
-  doCheck = false;
+  disabledTests = [
+    # Test failure could be related to https://github.com/adrienverge/yamllint/issues/334
+    "test_find_files_recursively"
+  ];
+
+  pythonImportsCheck = [ "yamllint" ];
 
   meta = with lib; {
     description = "A linter for YAML files";
     homepage = "https://github.com/adrienverge/yamllint";
-    license = licenses.gpl3;
+    license = licenses.gpl3Plus;
     maintainers = with maintainers; [ jonringer mikefaille ];
   };
 }
