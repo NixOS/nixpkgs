bf2f6a99d5712e1ec6d2b49486f63e5a984ea835
Patrick Hilhorst <git@hilhorst.be>
2021-03-10 20:12:15 +0100

    plexamp: 3.4.2 -> 3.4.3

--- pkgs/applications/audio/plexamp/default.nix
+++ pkgs/applications/audio/plexamp/default.nix
@@ -2,13 +2,13 @@
 
 let
   pname = "plexamp";
-  version = "3.4.2";
+  version = "3.4.3";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-${version}.AppImage";
     name="${pname}-${version}.AppImage";
-    sha256 = "045yih6vd7ng90aa3fd5nqh7746plyaz72spflb8bcvl8vav57w1";
+    sha256 = "1rzhrc5yr5f6bxydgmcjwrg85vkbkn6lqj72512lyhq5gg7zmm1w";
   };
 
   appimageContents = appimageTools.extractType2 {
@@ -32,6 +32,7 @@ in appimageTools.wrapType2 {
   meta = with lib; {
     description = "A beautiful Plex music player for audiophiles, curators, and hipsters";
     homepage = "https://plexamp.com/";
+    changelog = "https://forums.plex.tv/t/plexamp-release-notes/221280/25";
     license = licenses.unfree;
     maintainers = with maintainers; [ killercup ];
     platforms = [ "x86_64-linux" ];
