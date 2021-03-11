NixOS/nixpkgs

commit 8ad96b7786134cba3d856ad5572080cef94f1640

mcfgthreads: enable cross-compiling on Darwin
using -Werror is problematic in general, remove for all platforms
fixes #97214

pkgs/os-specific/windows/mcfgthreads/default.nix
@@ -18,6 +18,10 @@ stdenv.mkDerivation {
     rm -r "$sourceRoot/debug" "$sourceRoot/release"
   '';
 
+  postPatch = ''
+    substituteInPlace Makefile.am --replace '-Werror' ''
+  '';
+
   nativeBuildInputs = [
     autoreconfHook
   ];

commit 81e5833fd1e7e023fc26a9acdbcff99d5ccd0353

mcfgthreads: fix syntax
PR #115603 / 8ad96b7786134cba3d856ad5572080cef94f1640 introduced a
syntax error by introducing an '' inside an indented string and thus a
syntax error. Was merged despite the failing ofborg check.

pkgs/os-specific/windows/mcfgthreads/default.nix
@@ -19,7 +19,7 @@ stdenv.mkDerivation {
   '';
 
   postPatch = ''
-    substituteInPlace Makefile.am --replace '-Werror' ''
+    substituteInPlace Makefile.am --replace '-Werror' ""
   '';
 
   nativeBuildInputs = [