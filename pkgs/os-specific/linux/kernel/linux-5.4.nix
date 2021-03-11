7513be6e65c234bb2140ba86b1bd50274a3884d6
Tim Steinbach <tim@nequissimus.com>
2021-03-11 09:26:25 -0500

    linux: 5.4.104 -> 5.4.105

--- pkgs/os-specific/linux/kernel/linux-5.4.nix
+++ pkgs/os-specific/linux/kernel/linux-5.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.4.104";
+  version = "5.4.105";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1p8459plp3a6xw7cib34p7bddjs0dlv2m34dy4z6bbsvgfg51m74";
+    sha256 = "1dmq1zkr4idbbvh7wln5hn5sl4d0mcfm1af5bvsmsa44c78lqki4";
   };
 } // (args.argsOverride or {}))
