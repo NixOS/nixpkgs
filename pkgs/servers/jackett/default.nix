2e491db3058425e0a324b82578bf0740d1e8c537
Andrei Pampu <pampu.s.andrei@gmail.com>
2021-03-11 15:55:15 +0200

    jackett: 0.17.668 -> 0.17.671
    Also replace mono with dotnet core

--- pkgs/servers/jackett/default.nix
+++ pkgs/servers/jackett/default.nix
@@ -1,12 +1,12 @@
-{ lib, stdenv, fetchurl, mono, makeWrapper, curl, icu60, openssl, zlib }:
+{ lib, stdenv, fetchurl, dotnetCorePackages, makeWrapper, curl, icu60, openssl, zlib }:
 
 stdenv.mkDerivation rec {
   pname = "jackett";
-  version = "0.17.668";
+  version = "0.17.671";
 
   src = fetchurl {
-    url = "https://github.com/Jackett/Jackett/releases/download/v${version}/Jackett.Binaries.Mono.tar.gz";
-    sha256 = "sha256-+cvUpWVpXEkW+d92aIOli+pNi+ZDHEbxDDQ67O6kOVA=";
+    url = "https://github.com/Jackett/Jackett/releases/download/v${version}/Jackett.Binaries.LinuxAMDx64.tar.gz";
+    sha256 = "19yhfyznkqdwj408hz5481lvaz7ri6sib7bsmh0lxg0mvx35rq9r";
   };
 
   nativeBuildInputs = [ makeWrapper ];
@@ -15,9 +15,10 @@ stdenv.mkDerivation rec {
     mkdir -p $out/{bin,share/${pname}-${version}}
     cp -r * $out/share/${pname}-${version}
 
-    makeWrapper "${mono}/bin/mono" $out/bin/Jackett \
-      --add-flags "$out/share/${pname}-${version}/JackettConsole.exe" \
-      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ curl icu60 openssl zlib ]}
+    makeWrapper "${dotnetCorePackages.netcore_3_1}/bin/dotnet" $out/bin/Jackett \
+      --add-flags "$out/share/${pname}-${version}/jackett.dll" \
+      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ 
+          curl icu60 openssl zlib ]}
   '';
 
   meta = with lib; {
