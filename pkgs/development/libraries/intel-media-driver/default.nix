90e466041a4546bae74b2b27f390f8b4bc854a02
Michael Weiss <dev.primeos@gmail.com>
2021-03-11 17:09:22 +0100

    intel-media-driver: 21.1.1 -> 21.1.2

--- pkgs/development/libraries/intel-media-driver/default.nix
+++ pkgs/development/libraries/intel-media-driver/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "intel-media-driver";
-  version = "21.1.1";
+  version = "21.1.2";
 
   src = fetchFromGitHub {
     owner  = "intel";
     repo   = "media-driver";
     rev    = "intel-media-${version}";
-    sha256 = "1cgmpy2wqhv8zljz73wm4rggpk9w1prpmab5qphfp7ljajfv7k8r";
+    sha256 = "17g9xilcrpmkn6higgpvd9bly1h61xxyp1kx9zwl6rgzs2pryby2";
   };
 
   cmakeFlags = [
