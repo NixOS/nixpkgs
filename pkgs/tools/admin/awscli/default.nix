1857d1bf70f78a877e0712550f54395dc59fb736
Tim Steinbach <tim@nequissimus.com>
2021-03-11 09:11:49 -0500

    awscli: 1.19.24 -> 1.19.25

--- pkgs/tools/admin/awscli/default.nix
+++ pkgs/tools/admin/awscli/default.nix
@@ -28,11 +28,11 @@ let
 in
 with py.pkgs; buildPythonApplication rec {
   pname = "awscli";
-  version = "1.19.24"; # N.B: if you change this, change botocore and boto3 to a matching version too
+  version = "1.19.25"; # N.B: if you change this, change botocore and boto3 to a matching version too
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-eB81VdFB09QuagANaZxQuOe8N0AnqgL4aIu5HDrYg2I=";
+    sha256 = "sha256-YL5MnlN+DODGgpi2qtpr6wwC0nuebl/VoBzXRk4l4R8=";
   };
 
   # https://github.com/aws/aws-cli/issues/4837
