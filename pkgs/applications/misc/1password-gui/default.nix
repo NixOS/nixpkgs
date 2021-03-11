be731b9d09865b3c3ec3a0dc9ffe780ba56805d7
R. RyanTM <ryantm-bot@ryantm.com>
2021-03-11 05:37:16 +0000

    _1password-gui: 0.9.26 -> 8.0.27
    (cherry picked from commit efd3c20d7bd0cc9fa40a9c081a20fc0aab9abfa6)

--- pkgs/applications/misc/1password-gui/default.nix
+++ pkgs/applications/misc/1password-gui/default.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation rec {
   pname = "1password";
-  version = "0.9.26";
+  version = "8.0.27";
 
   src = fetchurl {
     url = "https://onepassword.s3.amazonaws.com/linux/appimage/${pname}-${version}.AppImage";
-    hash = "sha256-LvHvWUS2iEm9m+v+kk7wf+P9xZkOyuoLk4xM4+P2vF8=";
+    hash = "sha256-qzZXs7ak4052Igq+YWuzgDqJ7143q5qw5P3b3eN3NkU=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
