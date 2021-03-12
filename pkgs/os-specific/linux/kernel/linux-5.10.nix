a1838934ec88ab0243bb3200268e769a24dbe97f
ajs124 <git@ajs124.de>
2021-03-12 04:07:52 +0100

    linux: 5.10.22 -> 5.10.23
    (cherry picked from commit 7760c71c9cd153b040e348d6e94e5ea344136922)

--- pkgs/os-specific/linux/kernel/linux-5.10.nix
+++ pkgs/os-specific/linux/kernel/linux-5.10.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.10.22";
+  version = "5.10.23";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1pv3661d1gvkdl2jg6wx95lr5lcp6q77jrmx0m4a6z6n6asicnr4";
+    sha256 = "0snvkrs95wbx611kcnap59whfiz6h6mzsnbwswk5py3gxh1irqpn";
   };
 } // (args.argsOverride or {}))
