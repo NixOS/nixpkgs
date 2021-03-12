ddc52d4a0d37240bf8b06570a4720bdbc943dd55
R. RyanTM <ryantm-bot@ryantm.com>
2021-03-12 22:51:05 +0000

    python38Packages.caldav: 0.7.1 -> 0.8.0

--- pkgs/development/python-modules/caldav/default.nix
+++ pkgs/development/python-modules/caldav/default.nix
@@ -15,13 +15,13 @@
 
 buildPythonPackage rec {
   pname = "caldav";
-  version = "0.7.1";
+  version = "0.8.0";
 
   src = fetchFromGitHub {
     owner = "python-caldav";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1shfj67kq6qzd0ngyfk09hpzfggybcfxv5s7hqs87nq9l51bssv8";
+    sha256 = "11q3svns3a2ywfci739krxbh67cx691qja772wq22606blyygyjy";
   };
 
   nativeBuildInputs = lib.optionals (pythonOlder "3.5") [ mock ];
