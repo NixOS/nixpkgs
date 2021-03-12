c1ced05ec4330c4241cfd2e32c071470feeb9e8f
John Ericson <John.Ericson@Obsidian.Systems>
2021-03-12 15:06:09 -0500

    tests.cude.cuda-library-samples.{cublas,cusolver}: init at master
    Well, strictly speaking, master +
    https://github.com/NVIDIA/CUDALibrarySamples/pull/29

--- pkgs/test/cuda/default.nix
+++ pkgs/test/cuda/default.nix
@@ -13,4 +13,14 @@ rec {
     cuda-samples_cudatoolkit_11_0
     cuda-samples_cudatoolkit_11_1
     cuda-samples_cudatoolkit_11_2;
+
+  cuda-library-samplesPackages = callPackage ./cuda-library-samples { };
+  inherit (cuda-library-samplesPackages)
+    cuda-library-samples_cudatoolkit_10
+    cuda-library-samples_cudatoolkit_10_1
+    cuda-library-samples_cudatoolkit_10_2
+    cuda-library-samples_cudatoolkit_11
+    cuda-library-samples_cudatoolkit_11_0
+    cuda-library-samples_cudatoolkit_11_1
+    cuda-library-samples_cudatoolkit_11_2;
 }
