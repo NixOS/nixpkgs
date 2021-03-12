562313118bf5d88d1f6acaa917d8682175d2f7bd
Anton-Latukha <anton.latukha@gmail.com>
2021-03-12 21:12:49 +0200

    haskellPackages.hnix-store-core: 0.4.2.0
    Release just was made, fixed the `tasty-discover`.

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -215,9 +215,6 @@ self: super: {
   # 2020-06-05: HACK: does not pass own build suite - `dontCheck`
   hnix = generateOptparseApplicativeCompletion "hnix" (dontCheck super.hnix);
 
-  # https://github.com/haskell-nix/hnix-store/issues/127
-  hnix-store-core = addTestToolDepend super.hnix-store-core self.tasty-discover;
-
   # Fails for non-obvious reasons while attempting to use doctest.
   search = dontCheck super.search;
 
