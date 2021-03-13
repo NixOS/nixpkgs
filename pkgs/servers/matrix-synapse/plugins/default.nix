ad44060f5683293f5767d6a28e335f9f22c51bf6
Milan <me@pbb.lc>
2021-03-13 08:45:15 +0100

    matrix-synapse-plugins.matrix-synapse-shared-secret-auth: init at 1.0.2 (#112671)

--- pkgs/servers/matrix-synapse/plugins/default.nix
+++ pkgs/servers/matrix-synapse/plugins/default.nix
@@ -3,4 +3,5 @@
 {
   matrix-synapse-ldap3 = callPackage ./ldap3.nix { };
   matrix-synapse-pam = callPackage ./pam.nix { };
+  matrix-synapse-shared-secret-auth = callPackage ./shared-secret-auth.nix { };
 }
