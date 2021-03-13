84b304348ebc7a4d7290b90bf43ad1e4f7f5948b
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-03-13 03:30:05 +0100

    python3Packages.broadlink: add imports check, remove unused inputs

--- pkgs/development/python-modules/broadlink/default.nix
+++ pkgs/development/python-modules/broadlink/default.nix
@@ -1,5 +1,8 @@
-{ lib, fetchPypi, buildPythonPackage
-, cryptography, pyaes, pycrc }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, cryptography
+}:
 
 buildPythonPackage rec {
   pname = "broadlink";
@@ -10,15 +13,16 @@ buildPythonPackage rec {
     sha256 = "bfd1ff007d0d1187c17ae52be938afc8137fbd1ed6a794426e975df10d167571";
   };
 
-  postPatch = ''
-    substituteInPlace setup.py \
-      --replace pyaes==1.6.0 pyaes
-    '';
-
-  propagatedBuildInputs = [ cryptography pyaes pycrc ];
+  propagatedBuildInputs = [
+    cryptography
+  ];
 
   # no tests available
   doCheck = false;
+
+  pythonImportsCheck = [
+    "broadlink"
+  ];
 
   meta = with lib; {
     description = "Python API for controlling Broadlink IR controllers";
