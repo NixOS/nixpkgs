NixOS/nixpkgs

commit e03c068af5c9ca950214cd6c3b5cfe67f595da88

treewide: makeWrapper buildInputs to nativeBuildInputs

pkgs/applications/editors/vim/macvim-configurable.nix
@@ -29,7 +29,7 @@ let
           "/Applications/MacVim.app/Contents/MacOS"
           "/Applications/MacVim.app/Contents/bin"
         ];
-        buildInputs = [ makeWrapper ];
+        nativeBuildInputs = [ makeWrapper ];
         # We need to do surgery on the resulting app. We can't just make a wrapper for vim because this
         # is a GUI app. We need to copy the actual GUI executable image as AppKit uses the loaded image's
         # path to locate the bundle. We can use symlinks for other executables and resources though.

commit b0fde15bb2f0880d17bb241a1491ae17d2131dc2

macvim: fix configuration
macvim.configure was broken in e03c068af5c because `buildEnv` doesn't take
`nativeBuildInputs`. Revert the change for `macvim`.

pkgs/applications/editors/vim/macvim-configurable.nix
@@ -29,7 +29,7 @@ let
           "/Applications/MacVim.app/Contents/MacOS"
           "/Applications/MacVim.app/Contents/bin"
         ];
-        nativeBuildInputs = [ makeWrapper ];
+        buildInputs = [ makeWrapper ];
         # We need to do surgery on the resulting app. We can't just make a wrapper for vim because this
         # is a GUI app. We need to copy the actual GUI executable image as AppKit uses the loaded image's
         # path to locate the bundle. We can use symlinks for other executables and resources though.