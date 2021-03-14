58bcc97dc24340041d0144ecd0457e9f80c05869
Mario Rodas <marsam@users.noreply.github.com>
2021-03-14 04:20:00 +0000

    deno: fix build on darwin

--- pkgs/development/web/deno/default.nix
+++ pkgs/development/web/deno/default.nix
@@ -1,13 +1,15 @@
 { stdenv
 , lib
 , callPackage
-, fetchurl
 , fetchFromGitHub
 , rust
 , rustPlatform
 , installShellFiles
+, libobjc
 , Security
 , CoreServices
+, Metal
+, Foundation
 , librusty_v8 ? callPackage ./librusty_v8.nix { }
 }:
 
@@ -26,7 +28,7 @@ rustPlatform.buildRustPackage rec {
   # Install completions post-install
   nativeBuildInputs = [ installShellFiles ];
 
-  buildInputs = lib.optionals stdenv.isDarwin [ Security CoreServices ];
+  buildInputs = lib.optionals stdenv.isDarwin [ libobjc Security CoreServices Metal Foundation ];
 
   # The rusty_v8 package will try to download a `librusty_v8.a` release at build time to our read-only filesystem
   # To avoid this we pre-download the file and place it in the locations it will require it in advance
