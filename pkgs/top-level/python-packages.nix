b3148b97313ffde81e9b4c65fabd2c58a093f25b
Frederik Rietdijk <fridh@fridh.nl>
2021-03-14 09:30:49 +0100

    pythonPackages.pytestCheckHook_6_1: only override for Python 3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -124,7 +124,15 @@ in {
     venvShellHook
     wheelUnpackHook;
 
-  pytestCheckHook_6_1 = self.pytestCheckHook.override { pytest = self.pytest_6_1; };
+  # Not all packages are compatible with the latest pytest yet.
+  # We need to override the hook to select an older pytest, however,
+  # it should not override the version of pytest that is used for say
+  # Python 2. This is an ugly hack that is needed now because the hook
+  # propagates the package.
+  pytestCheckHook_6_1 = if isPy3k then
+    self.pytestCheckHook.override { pytest = self.pytest_6_1; }
+  else
+    self.pytestCheckHook;
 
   # helpers
 
