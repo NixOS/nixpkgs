4e701b586ada7abbdce1a7add69cd7f9437b56b7
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-14 01:05:54 +0100

    python3Packages.xknx: 0.17.1 -> 0.17.2

--- pkgs/development/python-modules/xknx/default.nix
+++ pkgs/development/python-modules/xknx/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "xknx";
-  version = "0.17.1";
+  version = "0.17.2";
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "XKNX";
     repo = pname;
     rev = version;
-    sha256 = "sha256-FNI6zodwlYdJDxncCOubClG9L1U6HGkQxdEEp0LdW04=";
+    sha256 = "sha256-DJglvcpqGBVkycEjq+Oa9gz9Keop39+UR9SctX0lg/A=";
   };
 
   propagatedBuildInputs = [
