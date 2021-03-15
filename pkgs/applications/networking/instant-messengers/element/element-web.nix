12d9950bf47e0ac20d4d04e189448ee075242117
TredwellGit <tredwell@tutanota.com>
2021-03-15 18:04:00 +0000

    element: 1.7.22 -> 1.7.23
    https://github.com/vector-im/element-web/releases/tag/v1.7.23
    (cherry picked from commit 56da9300a3962bcabaf791a91b3536f0606a9d0c)

--- pkgs/applications/networking/instant-messengers/element/element-web.nix
+++ pkgs/applications/networking/instant-messengers/element/element-web.nix
@@ -12,11 +12,11 @@ let
 
 in stdenv.mkDerivation rec {
   pname = "element-web";
-  version = "1.7.22";
+  version = "1.7.23";
 
   src = fetchurl {
     url = "https://github.com/vector-im/element-web/releases/download/v${version}/element-v${version}.tar.gz";
-    sha256 = "1aaa986h38kkrnyhb1y65d73idsxmkmi201511az9zlz9210ih59";
+    sha256 = "10n899gc3qcjy2cskk0whwz60pnvh500x1b57kn22l9bhkg9xkvp";
   };
 
   installPhase = ''
