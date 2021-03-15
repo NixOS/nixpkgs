a88aa04c28d05895ee624db50154f1c2e06564b5
Bastian Köcher <info@kchr.de>
2021-03-15 19:18:11 +0100

    rust-analyzer: 2021-03-01 -> 2021-03-15

--- pkgs/development/tools/rust/rust-analyzer/default.nix
+++ pkgs/development/tools/rust/rust-analyzer/default.nix
@@ -2,10 +2,10 @@
 
 {
   rust-analyzer-unwrapped = callPackage ./generic.nix rec {
-    rev = "2021-03-01";
+    rev = "2021-03-15";
     version = "unstable-${rev}";
-    sha256 = "10x4fk1nxk548cfxrbfvz0kpa2r955d0bcnxxn8k8zmrdqxs3sph";
-    cargoSha256 = "02s6qnq61vifx59hnbaalqmfvp8anfik62y6rzy3rwm1l9r85qrz";
+    sha256 = "150gydm0mg72bbhgjjks8qc5ldiqyzhai9z4yfh4f1s2bwdfh3yf";
+    cargoSha256 = "10l0lk5p11002q59dqa5yrrz6n6s11i7bmr1wnl141bxqvm873q2";
   };
 
   rust-analyzer = callPackage ./wrapper.nix {} {
