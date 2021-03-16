b134d81781cea2a49df17ee8f9c3c32c3069a3a5
storvik <petterstorvik@gmail.com>
2021-03-16 22:49:58 +0100

    libplctag: use fetchFromGitHub

--- pkgs/development/libraries/libplctag/default.nix
+++ pkgs/development/libraries/libplctag/default.nix
@@ -1,25 +1,27 @@
 { lib
 , stdenv
 , cmake
-, fetchurl
+, fetchFromGitHub
 }:
 
 stdenv.mkDerivation rec {
   pname = "libplctag";
   version = "2.3.5";
 
-  nativeBuildInputs = [ cmake ];
-
-  src = fetchurl {
-    url = "https://github.com/libplctag/libplctag/archive/v${version}.tar.gz";
-    sha256 = "16azdxyl82js17xcav7v23zvi64w72181wylj489zim9x5h5sxsr";
+  src = fetchFromGitHub {
+    owner = "libplctag";
+    repo = "libpcltag";
+    rev = "v${version}";
+    sha256 = "0brmzr863chybm5y0q5hld5mhf6kx0bl4dddr7j69adlraak7x6s";
   };
+
+  nativeBuildInputs = [ cmake ];
 
   meta = with lib; {
     homepage = "https://github.com/libplctag/libplctag";
     description = "Library that uses EtherNet/IP or Modbus TCP to read and write tags in PLCs";
-    license = [ licenses.lgpl2Plus licenses.mpl20 ];
-    maintainers = [ maintainers.petterstorvik ];
+    license = with licenses; [ lgpl2Plus mpl20 ];
+    maintainers = with maintainers; [ petterstorvik ];
     platforms = platforms.all;
   };
 }
