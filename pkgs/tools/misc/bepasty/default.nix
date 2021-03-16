d31535f07344d24b1b7a13bead38f30001ef4489
Pablo Ovelleiro Corral <github@pablo.tools>
2021-03-16 01:03:12 +0100

    add missing libs to bepasty

--- pkgs/tools/misc/bepasty/default.nix
+++ pkgs/tools/misc/bepasty/default.nix
@@ -1,25 +1,25 @@
-{ python3
-, lib
-}:
+{ python3, lib }:
 
 let
   python = python3.override {
     self = python;
-    packageOverrides = self: super : {
-      xstatic-bootstrap = super.xstatic-bootstrap.overridePythonAttrs(oldAttrs: rec {
-        version = "3.3.7.1";
-        src = oldAttrs.src.override {
-          inherit version;
-          sha256 = "0cgihyjb9rg6r2ddpzbjm31y0901vyc8m9h3v0zrhxydx1w9x50c";
-        };
-      });
+    packageOverrides = self: super: {
+      xstatic-bootstrap = super.xstatic-bootstrap.overridePythonAttrs
+        (oldAttrs: rec {
+          version = "3.3.7.1";
+          src = oldAttrs.src.override {
+            inherit version;
+            sha256 = "0cgihyjb9rg6r2ddpzbjm31y0901vyc8m9h3v0zrhxydx1w9x50c";
+          };
+        });
     };
   };
 
-#We need to use buildPythonPackage here to get the PYTHONPATH build correctly.
-#This is needed for services.bepasty
-#https://github.com/NixOS/nixpkgs/pull/38300
-in with python.pkgs; buildPythonPackage rec {
+  #We need to use buildPythonPackage here to get the PYTHONPATH build correctly.
+  #This is needed for services.bepasty
+  #https://github.com/NixOS/nixpkgs/pull/38300
+in with python.pkgs;
+buildPythonPackage rec {
   pname = "bepasty";
   version = "1.0.0";
 
@@ -28,8 +28,10 @@ in with python.pkgs; buildPythonPackage rec {
     pygments
     setuptools
     xstatic
+    xstatic-asciinema-player
     xstatic-bootbox
     xstatic-bootstrap
+    xstatic-font-awesome
     xstatic-jquery
     xstatic-jquery-file-upload
     xstatic-jquery-ui
@@ -43,10 +45,7 @@ in with python.pkgs; buildPythonPackage rec {
     sha256 = "sha256-rgb/xFYvZ44JUGw+RYfJVfg6stE/y/YjY6XSwO32Qkc=";
   };
 
-  checkInputs = [
-    pytest
-    selenium
-  ];
+  checkInputs = [ pytest selenium ];
 
   # No tests in sdist
   doCheck = false;
