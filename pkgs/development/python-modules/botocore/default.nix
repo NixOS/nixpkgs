cbd0794e43afd71d3f6f394159c8365bb5c96f73
Tim Steinbach <tim@nequissimus.com>
2021-03-17 09:50:48 -0400

    python3Packages.botocore: 1.20.27 -> 1.20.29

--- pkgs/development/python-modules/botocore/default.nix
+++ pkgs/development/python-modules/botocore/default.nix
@@ -12,11 +12,11 @@
 
 buildPythonPackage rec {
   pname = "botocore";
-  version = "1.20.27"; # N.B: if you change this, change boto3 and awscli to a matching version
+  version = "1.20.29"; # N.B: if you change this, change boto3 and awscli to a matching version
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-RHeAPwdkn02AsX0FSCDnoJuyyweS0N7MKBIQi8N1nEo=";
+    sha256 = "sha256-GEt9JrBmn9ZayBk2YjdtEmfYAOAFtpQStXzILF/76TU=";
   };
 
   propagatedBuildInputs = [
