7dcceb329befbde997734b89d3c5cd70a02446f8
Markus S. Wamser <github-dev@mail2013.wamser.eu>
2021-03-17 11:42:31 +0100

    teams: reorder arguments produced by wrapper
    this fixes launching meetings from links

--- pkgs/applications/networking/instant-messengers/teams/default.nix
+++ pkgs/applications/networking/instant-messengers/teams/default.nix
@@ -82,6 +82,11 @@ stdenv.mkDerivation rec {
       echo "Adding runtime dependencies to RPATH of Node module $mod"
       patchelf --set-rpath "$runtime_rpath:$mod_rpath" "$mod"
     done;
+
+    # fix for https://docs.microsoft.com/en-us/answers/questions/298724/open-teams-meeting-link-on-linux-doens39t-work.html?childToView=309406#comment-309406
+    # while we create the wrapper ourselves, gappsWrapperArgs leads to the same issue
+    # another option would be to introduce gappsWrapperAppendedArgs, to allow control of positioning
+    substituteInPlace "$out/bin/teams" --replace '.teams-wrapped"  --disable-namespace-sandbox --disable-setuid-sandbox "$@"' '.teams-wrapped" "$@" --disable-namespace-sandbox --disable-setuid-sandbox'
   '';
 
   meta = with lib; {
