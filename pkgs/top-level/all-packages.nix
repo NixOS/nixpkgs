e53281a59b4abd0685ed98f3f16a24d77eb0601d
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2021-03-18 23:24:20 +0100

    usbview: init at 2.0 (#116450)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3325,6 +3325,8 @@ in
 
   usbsdmux = callPackage ../development/tools/misc/usbsdmux { };
 
+  usbview = callPackage ../tools/misc/usbview { };
+
   anthy = callPackage ../tools/inputmethods/anthy { };
 
   evdevremapkeys = callPackage ../tools/inputmethods/evdevremapkeys { };
