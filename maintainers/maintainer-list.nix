7f0ada8ae2cde8c5c58d587eda2540888cff196c
piegames <git@piegames.de>
2021-03-18 19:14:45 +0100

    maintainers: add piegames

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7403,6 +7403,12 @@
     githubId = 627831;
     name = "Hoang Xuan Phu";
   };
+  piegames = {
+    name = "piegames";
+    email = "nix@piegames.de";
+    github = "piegamesde";
+    githubId = 14054505;
+  };
   pierrechevalier83 = {
     email = "pierrechevalier83@gmail.com";
     github = "pierrechevalier83";
