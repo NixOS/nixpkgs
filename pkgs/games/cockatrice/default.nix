051db0e3a0a1bfd2c32e9d7e03ac2829cb8b836b
Facteur Pat <kevin.unger@protonmail.com>
2021-03-19 09:50:24 +0100

    cockatrice: 2.8.0 -> 2.8.1-beta
    change gpl2 into gpl2Plus

--- pkgs/games/cockatrice/default.nix
+++ pkgs/games/cockatrice/default.nix
@@ -22,7 +22,7 @@ mkDerivation rec {
   meta = {
     homepage = "https://github.com/Cockatrice/Cockatrice";
     description = "A cross-platform virtual tabletop for multiplayer card games";
-    license = lib.licenses.gpl2;
+    license = lib.licenses.gpl2Plus;
     maintainers = with lib.maintainers; [ evanjs ];
     platforms = with lib.platforms; linux;
   };
