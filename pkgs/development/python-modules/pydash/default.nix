d0a55e85bbb2777069638fd51f7d21a0f3ae9c33
Maximilian Bosch <maximilian@mbosch.me>
2021-03-19 23:25:50 +0100

    python3Packages.pydash: different homepage
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/development/python-modules/pydash/default.nix
+++ pkgs/development/python-modules/pydash/default.nix
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   checkInputs = [ mock pytestCheckHook invoke ];
 
   meta = with lib; {
-    homepage = "https://pypi.org/project/pydash/";
+    homepage = "https://github.com/dgilland/pydash";
     description = "The kitchen sink of Python utility libraries for doing \"stuff\" in a functional way. Based on the Lo-Dash Javascript library.";
     license = licenses.mit;
     maintainers = with maintainers; [ ma27 ];
