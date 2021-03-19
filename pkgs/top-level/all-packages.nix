efcafebb99559812f3d6022d05f840f248af6170
Anton Arapov <anton@deadbeef.mx>
2021-03-19 17:33:12 +0100

    meson64-tools: init
    an open alternative to aml_encrypt_g12a and aml_encrypt_g12b.
    Proprietary tool is not available for aarch64 architecture, which is a
    show stopper for building u-boot images for the boards like Odroid C4.
    
    Signed-off-by: Anton Arapov <anton@deadbeef.mx>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2749,6 +2749,8 @@ in
 
   meson-tools = callPackage ../misc/meson-tools { };
 
+  meson64-tools = callPackage ../misc/meson64-tools { };
+
   metabase = callPackage ../servers/metabase { };
 
   midicsv = callPackage ../tools/audio/midicsv { };
