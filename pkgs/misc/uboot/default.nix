e219927eb74eef419d2e7b130446b4bda27ec0cc
Anton Arapov <anton@deadbeef.mx>
2021-03-19 17:37:15 +0100

    uboot: (ubootOdroidC4) use meson64-tools instead of aml_encrypt_g12a
    This makes aarch64 builds possible.
    
    Signed-off-by: Anton Arapov <anton@deadbeef.mx>

--- pkgs/misc/uboot/default.nix
+++ pkgs/misc/uboot/default.nix
@@ -8,7 +8,6 @@
 , flex
 , openssl
 , swig
-, meson-tools
 , armTrustedFirmwareAllwinner
 , armTrustedFirmwareRK3328
 , armTrustedFirmwareRK3399
@@ -248,60 +247,27 @@ in {
     extraMeta.platforms = ["aarch64-linux"];
   };
 
-  ubootOdroidC4 = let in
-  assert
-    (lib.assertMsg (stdenv.buildPlatform.isx86_64)
-    "U-Boot for Odroid C4 must be cross-compiled on x86_64-linux system. Once built, U-Boot image must be signed by 'aml_encrypt_g12a' tool, which is available for x86_64-linux platform only.");
-  buildUBoot {
+  ubootOdroidC4 = buildUBoot {
     defconfig = "odroid-c4_defconfig";
 
     postBuild = ''
-      # blx_fix() resembles function from:
-      # - https://github.com/hardkernel/u-boot/blob/e0725c1dce0c3dbbae47478c13d968c41014fac8/fip/Makefile#L44
-      blx_fix() {
-        case $7 in
-          "bl30")
-            declare blx_bin_limit=40960
-            declare blx01_bin_limit=13312
-          ;;
-          "bl2")
-            declare blx_bin_limit=57344
-            declare blx01_bin_limit=4096
-          ;;
-        esac
+      ${buildPackages.meson64-tools}/bin/pkg --type bl30 --output bl30_new.bin \
+        ${firmwareOdroidC4}/bl30.bin ${firmwareOdroidC4}/bl301.bin
+      ${buildPackages.meson64-tools}/bin/pkg --type bl2 --output bl2_new.bin \
+        ${firmwareOdroidC4}/bl2.bin ${firmwareOdroidC4}/acs.bin
 
-        declare -i blx_size=`du -b $1 | awk '{print int($1)}'`
-        declare -i zero_size=$blx_bin_limit-$blx_size
-        dd if=/dev/zero of=$2 bs=1 count=$zero_size
-        cat $1 $2 > $3
-
-        declare -i blx01_size=`du -b $4 | awk '{print int($1)}'`
-        declare -i zero_size_01=$blx01_bin_limit-$blx01_size
-        dd if=/dev/zero of=$2 bs=1 count=$zero_size_01
-        cat $4 $2 > $5
-
-        cat $3 $5 > $6
-      }
-
-      blx_fix ${firmwareOdroidC4}/bl30.bin zero_tmp bl30_zero.bin \
-              ${firmwareOdroidC4}/bl301.bin bl301_zero.bin \
-              bl30_new.bin bl30
-      blx_fix ${firmwareOdroidC4}/bl2.bin zero_tmp bl2_zero.bin \
-              ${firmwareOdroidC4}/acs.bin bl21_zero.bin \
-              bl2_new.bin bl2
-
-      ${firmwareOdroidC4}/aml_encrypt_g12a --bl30sig --input bl30_new.bin \
+      ${buildPackages.meson64-tools}/bin/bl30sig --input bl30_new.bin \
         --output bl30_new.bin.g12a.enc --level v3
-      ${firmwareOdroidC4}/aml_encrypt_g12a --bl3sig --input bl30_new.bin.g12a.enc \
+      ${buildPackages.meson64-tools}/bin/bl3sig --input  bl30_new.bin.g12a.enc \
         --output bl30_new.bin.enc --level v3 --type bl30
-      ${firmwareOdroidC4}/aml_encrypt_g12a --bl3sig --input ${firmwareOdroidC4}/bl31.img \
+      ${buildPackages.meson64-tools}/bin/bl3sig --input ${firmwareOdroidC4}/bl31.img \
         --output bl31.img.enc --level v3 --type bl31
-      ${firmwareOdroidC4}/aml_encrypt_g12a --bl3sig --input u-boot.bin --compress lz4 \
+      ${buildPackages.meson64-tools}/bin/bl3sig --input u-boot.bin --compress lz4 \
         --output bl33.bin.enc --level v3 --type bl33 --compress lz4
-      ${firmwareOdroidC4}/aml_encrypt_g12a --bl2sig --input bl2_new.bin \
+      ${buildPackages.meson64-tools}/bin/bl2sig --input bl2_new.bin \
         --output bl2.n.bin.sig
 
-      ${firmwareOdroidC4}/aml_encrypt_g12a --bootmk --output u-boot.bin \
+      ${buildPackages.meson64-tools}/bin/bootmk --output u-boot.bin \
         --bl2 bl2.n.bin.sig --bl30 bl30_new.bin.enc --bl31 bl31.img.enc --bl33 bl33.bin.enc \
         --ddrfw1 ${firmwareOdroidC4}/ddr4_1d.fw \
         --ddrfw2 ${firmwareOdroidC4}/ddr4_2d.fw \
