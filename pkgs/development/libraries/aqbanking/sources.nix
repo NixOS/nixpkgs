23a3a52be57e8c8b6e976f34f3aa3bbfde18928f
Daniel Nagy <danielnagy@posteo.de>
2021-03-20 16:43:14 +0100

    gwenhywfar: 5.4.1 -> 5.6.0

--- pkgs/development/libraries/aqbanking/sources.nix
+++ pkgs/development/libraries/aqbanking/sources.nix
@@ -1,7 +1,7 @@
 {
-  gwenhywfar.version = "5.4.1";
-  gwenhywfar.sha256 = "16waq39mbhhjcma2ykdbqvpcw0ba3ksqqwsp55zczhg320s41zgv";
-  gwenhywfar.releaseId = "344";
+  gwenhywfar.version = "5.6.0";
+  gwenhywfar.sha256 = "1isbj4a7vdgagp3kkvx2pjcjy8lba6kzjr11fmr06aci1694dbsp";
+  gwenhywfar.releaseId = "364";
   libchipcard.version = "5.0.4";
   libchipcard.sha256 = "0fj2h39ll4kiv28ch8qgzdbdbnzs8gl812qnm660bw89rynpjnnj";
   libchipcard.releaseId = "158";
