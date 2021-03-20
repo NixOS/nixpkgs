561fa02f123fe83cccc8bc5c311df10e6988d982
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-20 09:53:47 +0100

    python3Packages.iaqualink: init at 0.4.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3184,6 +3184,8 @@ in {
 
   iapws = callPackage ../development/python-modules/iapws { };
 
+  iaqualink = callPackage ../development/python-modules/iaqualink { };
+
   ibis = callPackage ../development/python-modules/ibis { };
 
   ibis-framework = callPackage ../development/python-modules/ibis-framework { };
