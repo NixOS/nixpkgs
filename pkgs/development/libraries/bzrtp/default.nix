d00627d37c0a33031fd724760940c4e7cb2c0d02
R. RyanTM <ryantm-bot@ryantm.com>
2021-03-20 03:50:46 +0000

    bzrtp: 4.4.9 -> 4.4.34

--- pkgs/development/libraries/bzrtp/default.nix
+++ pkgs/development/libraries/bzrtp/default.nix
@@ -7,7 +7,7 @@
 
 stdenv.mkDerivation rec {
   pname = "bzrtp";
-  version = "4.4.9";
+  version = "4.4.34";
 
   src = fetchFromGitLab {
     domain = "gitlab.linphone.org";
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
     group = "BC";
     repo = pname;
     rev = version;
-    sha256 = "1yjmsbqmymzl4r7sba6w4a2yld8m6hzafr6jf7sj0syhwpnc3zv6";
+    sha256 = "sha256-Zv/B7OXQayD1cdJkpz40FTXqhSLcqKVPJvRXX/HSVfo=";
   };
 
   buildInputs = [ bctoolbox sqlite ];
