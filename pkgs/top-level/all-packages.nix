e2e6243508147e856959bde81d45b3d4324bbd81
Robert Schütz <dev@schuetz-co.de>
2021-03-20 14:08:29 +0100

    flashfocus: don't use python3Packages.callPackage
    Otherwise we can't override specific Python dependencies of it.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2390,7 +2390,7 @@ in
 
   fuzzel = callPackage ../applications/misc/fuzzel { };
 
-  flashfocus = python3Packages.callPackage ../misc/flashfocus { };
+  flashfocus = callPackage ../misc/flashfocus { };
 
   qt-video-wlr = libsForQt5.callPackage ../applications/misc/qt-video-wlr { };
 
