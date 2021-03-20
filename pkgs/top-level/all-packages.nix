bf4a3c208e54c909e2e86497854a3be0304abb89
Weihua Lu <luwh364@gmail.com>
2021-03-21 00:57:39 +0800

    logseq: init at 0.0.13

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -820,6 +820,10 @@ in
     flavour = "git";
   };
 
+  logseq = callPackage ../applications/misc/logseq {
+    electron = electron_11;
+  };
+
   lxterminal = callPackage ../applications/terminal-emulators/lxterminal { };
 
   microcom = callPackage ../applications/terminal-emulators/microcom { };
