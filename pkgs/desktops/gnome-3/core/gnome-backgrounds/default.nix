4cbb1f3cb9d30d15aabd541684d39ec20b4c7f21
Jan Tojnar <jtojnar@gmail.com>
2021-03-21 07:35:52 +0100

    gnome3.gnome-backgrounds: 3.38.0 → 40.0

--- pkgs/desktops/gnome-3/core/gnome-backgrounds/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-backgrounds/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-backgrounds";
-  version = "3.38.0";
+  version = "40.0";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gnome-backgrounds/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "1qqygm15rcdgm36vz2iy7b9axndjzvpi29lmygyakjc07a3jlwgp";
+    url = "mirror://gnome/sources/gnome-backgrounds/${lib.versions.major version}/${pname}-${version}.tar.xz";
+    sha256 = "1xm2cyhw88yna9a3inbw7c7my217i2zn4s9rmr3h666pbqx17hs6";
   };
 
   passthru = {
