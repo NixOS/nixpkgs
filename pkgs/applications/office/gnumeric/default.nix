a8093f0b641d802bbd880343a238e7ae855a3682
Jan Tojnar <jtojnar@gmail.com>
2021-03-21 01:19:18 +0000

    gnumeric: 1.12.48 → 1.12.49

--- pkgs/applications/office/gnumeric/default.nix
+++ pkgs/applications/office/gnumeric/default.nix
@@ -7,11 +7,11 @@ let
   inherit (python3Packages) python pygobject3;
 in stdenv.mkDerivation rec {
   pname = "gnumeric";
-  version = "1.12.48";
+  version = "1.12.49";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "14556b0vyxdvdwjlin0rv7jk0vq4nplbmvp9j89bhkfk84xf7k2p";
+    sha256 = "0mzdhhpa7kwkc51l344g6vgqwaxkjdf03s7zasqh0bn3jpn75h4i";
   };
 
   configureFlags = [ "--disable-component" ];
