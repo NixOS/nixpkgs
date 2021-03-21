b57434af002a82271e281e3d779300cfd66aa8c7
Jan Tojnar <jtojnar@gmail.com>
2021-03-21 07:26:52 +0100

    gnome-user-docs: 3.38.2 → 40.0

--- pkgs/data/documentation/gnome-user-docs/default.nix
+++ pkgs/data/documentation/gnome-user-docs/default.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-user-docs";
-  version = "3.38.2";
+  version = "40.0";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gnome-user-docs/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "1h9lyn80zccmgi6gpymabgrcj7km0sb1axll5z490qnx74xbn37m";
+    url = "mirror://gnome/sources/gnome-user-docs/${lib.versions.major version}/${pname}-${version}.tar.xz";
+    sha256 = "149s4qvqyx1jgqqzsrpmlavi6p8kgh8hbqbpncl5i8ld446lhx1i";
   };
 
   nativeBuildInputs = [
