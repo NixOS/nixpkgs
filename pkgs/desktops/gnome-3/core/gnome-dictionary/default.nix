0032ed66e0b2318ea9cbf0e2cb8d5fd0dafd81d6
Jan Tojnar <jtojnar@gmail.com>
2021-03-21 08:30:09 +0100

    gnome3.gnome-dictionary: 3.26.1 → 40.0

--- pkgs/desktops/gnome-3/core/gnome-dictionary/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-dictionary/default.nix
@@ -4,11 +4,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-dictionary";
-  version = "3.26.1";
+  version = "40.0";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gnome-dictionary/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "16b8bc248dcf68987826d5e39234b1bb7fd24a2607fcdbf4258fde88f012f300";
+    url = "mirror://gnome/sources/gnome-dictionary/${lib.versions.major version}/${pname}-${version}.tar.xz";
+    sha256 = "1d8dhcfys788vv27v34i3s3x3jdvdi2kqn2a5p8c937a9hm0qr9f";
   };
 
   patches = [
