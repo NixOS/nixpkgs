4a8cb3385da6c0749876a76e243def18dcb8e9d5
Jan Tojnar <jtojnar@gmail.com>
2021-03-21 08:30:12 +0100

    gnome3.gnome-sudoku: 3.38.0 → 40.0

--- pkgs/desktops/gnome-3/games/gnome-sudoku/default.nix
+++ pkgs/desktops/gnome-3/games/gnome-sudoku/default.nix
@@ -3,11 +3,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-sudoku";
-  version = "3.38.0";
+  version = "40.0";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gnome-sudoku/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0cpxx63liczmax6ry06r5k0f221xpg2rqh49vkdj2snmqq61swrq";
+    url = "mirror://gnome/sources/gnome-sudoku/${lib.versions.major version}/${pname}-${version}.tar.xz";
+    sha256 = "0byz0f8lylf5cy8hvb3203vbd1k0ljjbm8nv549m125z9nn5j66r";
   };
 
   nativeBuildInputs = [ meson ninja vala pkg-config gobject-introspection gettext itstool libxml2 python3 desktop-file-utils wrapGAppsHook ];
