480eae82cd26ec124ef07a96b597939424d923b2
Jan Tojnar <jtojnar@gmail.com>
2021-03-22 13:52:10 +0000

    tracker: 3.1.0.rc → 3.1.0

--- pkgs/development/libraries/tracker/default.nix
+++ pkgs/development/libraries/tracker/default.nix
@@ -29,13 +29,13 @@
 
 stdenv.mkDerivation rec {
   pname = "tracker";
-  version = "3.1.0.rc";
+  version = "3.1.0";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-+9xAnShxXDEGpaylBwv+VDwhwDMWzsgcyKwXlf54Xi8=";
+    sha256 = "sha256-ylXrlXWHR++AKvSzH8CFqNVvufbQA76P0+keMtLkKHU=";
   };
 
   patches = [
