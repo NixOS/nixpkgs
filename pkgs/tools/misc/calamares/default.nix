8671b73a727553cce0031b042595bd780b6f7f18
R. RyanTM <ryantm-bot@ryantm.com>
2021-03-22 05:31:17 +0000

    calamares: 3.2.36 -> 3.2.39

--- pkgs/tools/misc/calamares/default.nix
+++ pkgs/tools/misc/calamares/default.nix
@@ -6,12 +6,12 @@
 
 mkDerivation rec {
   pname = "calamares";
-  version = "3.2.36";
+  version = "3.2.39";
 
   # release including submodule
   src = fetchurl {
     url = "https://github.com/${pname}/${pname}/releases/download/v${version}/${pname}-${version}.tar.gz";
-    sha256 = "sha256-pgA0mRglIBObNNUQIozNy6EvJQSmHRF+kN4EjqL4tt4=";
+    sha256 = "sha256-QGdy49RndRIBR3B+Z7iXbFyx5gxXO2GHNYc+iv0z47w=";
   };
 
   nativeBuildInputs = [ cmake extra-cmake-modules ];
