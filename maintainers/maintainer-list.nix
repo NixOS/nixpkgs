2b8b6344b2cd278d7be5048961ee4f31aa876126
SEbbaDK <sebbadk@gmail.com>
2021-03-23 18:21:39 +0100

    maintainers: add sebbadk

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8675,6 +8675,12 @@
     githubId = 19472270;
     name = "Sebastian";
   };
+  sebbadk = {
+    email = "sebastian@sebba.dk";
+    github = "SEbbaDK";
+    githubId = 1567527;
+    name = "Sebastian Hyberts";
+  };
   sellout = {
     email = "greg@technomadic.org";
     github = "sellout";
