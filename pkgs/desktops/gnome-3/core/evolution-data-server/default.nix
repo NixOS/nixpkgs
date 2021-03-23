6ef020394140c50df1f3be0675e36b00fc60e304
R. RyanTM <ryantm-bot@ryantm.com>
2021-03-23 04:04:00 +0000

    evolution-data-server: 3.38.4 -> 3.40.0

--- pkgs/desktops/gnome-3/core/evolution-data-server/default.nix
+++ pkgs/desktops/gnome-3/core/evolution-data-server/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "evolution-data-server";
-  version = "3.38.4";
+  version = "3.40.0";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/evolution-data-server/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "rFPxay1R8+f/gCX5yhn0otTOOEHXKun+K7iX3ICZ1wU=";
+    sha256 = "7VcvDLaiNlgJlDRJqMy+5lJoHi2aGn9KVLYMu1PYdEU=";
   };
 
   patches = [
