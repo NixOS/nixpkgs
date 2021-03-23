ded9ada2b1b4b716508d28b1ab2529027837f7e6
Danielle <dani@builds.terrible.systems>
2021-03-23 15:46:04 +0100

    drone-runner-docker: init at 1.6.3 (#117239)
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3857,6 +3857,8 @@ in
 
   drone-runner-exec = callPackage ../development/tools/continuous-integration/drone-runner-exec { };
 
+  drone-runner-docker = callPackage ../development/tools/continuous-integration/drone-runner-docker { };
+
   dropbear = callPackage ../tools/networking/dropbear { };
 
   dsview = libsForQt5.callPackage ../applications/science/electronics/dsview { };
