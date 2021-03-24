f201344d0e4f001923060393b061a5b8bb5d9e64
Frederik Rietdijk <fridh@fridh.nl>
2021-03-24 10:28:18 +0100

    python3Packages.nose-timer: 1.0.0 -> 1.0.1

--- pkgs/development/python-modules/nose-timer/default.nix
+++ pkgs/development/python-modules/nose-timer/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "nose-timer";
-  version = "1.0.0";
+  version = "1.0.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "09hwjwbczi06bfqgiylb2yxs5h88jdl26zi1fdqxdzvamrkksf2c";
+    sha256 = "8f70d103b7ffd9122a589de0df9d037a7d967519bf6de122621d2186609b9e3a";
   };
 
   propagatedBuildInputs = [ nose ];
