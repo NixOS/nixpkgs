c7d220618fb152c3ccb50d883eda4d13abbca13f
Frederik Rietdijk <fridh@fridh.nl>
2021-03-24 10:28:09 +0100

    python3Packages.boto3: 1.17.33 -> 1.17.35

--- pkgs/development/python-modules/boto3/default.nix
+++ pkgs/development/python-modules/boto3/default.nix
@@ -13,11 +13,11 @@
 
 buildPythonPackage rec {
   pname = "boto3";
-  version = "1.17.33"; # N.B: if you change this, change botocore and awscli to a matching version
+  version = "1.17.35"; # N.B: if you change this, change botocore and awscli to a matching version
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-DKwv/8G6kV97tezuU5MYUy21HyGMkooij6/j5QHpRy4=";
+    sha256 = "40e84a5f7888924db74a2710dbe48d066b51fe1f5549efaffe90e6efe813f37b";
   };
 
   propagatedBuildInputs = [ botocore jmespath s3transfer ] ++ lib.optionals (!isPy3k) [ futures ];
