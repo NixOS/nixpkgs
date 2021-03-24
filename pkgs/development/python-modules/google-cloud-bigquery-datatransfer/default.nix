993f7d8bde56de8a9950eec071d986dcbab8c737
Frederik Rietdijk <fridh@fridh.nl>
2021-03-24 10:28:13 +0100

    python3Packages.google-cloud-bigquery-datatransfer: 3.0.1 -> 3.1.0

--- pkgs/development/python-modules/google-cloud-bigquery-datatransfer/default.nix
+++ pkgs/development/python-modules/google-cloud-bigquery-datatransfer/default.nix
@@ -11,11 +11,11 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-bigquery-datatransfer";
-  version = "3.0.1";
+  version = "3.1.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "c5669410018eb41cecf6f9c90136d24d0ca9ed141bda8fbb3d52cd3de7162960";
+    sha256 = "98065257189388b6cc797478c4b2bdf86e9e8cdcaca5d170ded027646444a586";
   };
 
   propagatedBuildInputs = [ google-api-core libcst proto-plus ];
