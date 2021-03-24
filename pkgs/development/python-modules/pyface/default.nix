6a0bd5c9ecbf968ebad0ab5019efcc4933dcbce1
Frederik Rietdijk <fridh@fridh.nl>
2021-03-24 10:28:22 +0100

    python3Packages.pyface: 7.1.0 -> 7.3.0

--- pkgs/development/python-modules/pyface/default.nix
+++ pkgs/development/python-modules/pyface/default.nix
@@ -4,11 +4,11 @@
 
 buildPythonPackage rec {
   pname = "pyface";
-  version = "7.1.0";
+  version = "7.3.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a3e22a4d31429f0d5b9ff50aaac3fb47e4f7da678b6b0439a7b91ef40675f88d";
+    sha256 = "a7031ec4cfff034affc822e47ff5e6c1a0272e576d79465cdbbe25f721740322";
   };
 
   propagatedBuildInputs = [ setuptools six traits ];
