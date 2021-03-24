87f83e65937b5b6b8b999210a6506ea4d466d9d4
Frederik Rietdijk <fridh@fridh.nl>
2021-03-24 10:28:20 +0100

    python3Packages.pikepdf: 2.8.0 -> 2.9.1

--- pkgs/development/python-modules/pikepdf/default.nix
+++ pkgs/development/python-modules/pikepdf/default.nix
@@ -23,12 +23,12 @@
 
 buildPythonPackage rec {
   pname = "pikepdf";
-  version = "2.8.0";
+  version = "2.9.1";
   disabled = ! isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "74ff96fddd21cd4c0830eb549137ea9eccbdbff8cef4f684322b9afb8e42ccb5";
+    sha256 = "99c19cf0dd0fc89fc9e6a0de61e974e34d4111dd69802aeaee3e61fb1a74a3d8";
   };
 
   buildInputs = [
