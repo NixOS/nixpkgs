abd15fed80a24a38a021cccbcb4266c2bca9ea6e
Jonathan Ringer <jonringer117@gmail.com>
2021-03-24 10:41:49 -0700

    python3Packages.habanero: 0.6.0 -> 0.7.4

--- pkgs/development/python-modules/habanero/default.nix
+++ pkgs/development/python-modules/habanero/default.nix
@@ -5,14 +5,14 @@
 
 buildPythonPackage rec {
   pname = "habanero";
-  version = "0.6.0";
+  version = "0.7.4";
 
   # Install from Pypi is failing because of a missing file (Changelog.rst)
   src = fetchFromGitHub {
     owner = "sckott";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1l2cgl6iiq8jff2w2pib6w8dwaj8344crhwsni2zzq0p44dwi13d";
+    sha256 = "1d8yj9xz5qabcj57rpjzvg0jcscvzrpb0739mll29nijbsaimfr1";
   };
 
   propagatedBuildInputs = [ requests ];
