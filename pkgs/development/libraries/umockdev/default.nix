2b9c8ba171107d734a0d414babd695e6a60a094c
Jan Tojnar <jtojnar@gmail.com>
2021-03-24 05:40:11 +0100

    umockdev: Fix build with Vala 0.52

--- pkgs/development/libraries/umockdev/default.nix
+++ pkgs/development/libraries/umockdev/default.nix
@@ -1,6 +1,7 @@
 { lib, stdenv
 , docbook_xsl
 , fetchurl
+, fetchpatch
 , glib
 , gobject-introspection
 , gtk-doc
@@ -25,6 +26,14 @@ stdenv.mkDerivation rec {
     url = "https://github.com/martinpitt/umockdev/releases/download/${version}/${pname}-${version}.tar.xz";
     sha256 = "09k8jwvsphd97hcagf0zaf0hwzlzq2r8jfgbmvj55k7ylrg8hjxg";
   };
+
+  patches = [
+    # Fix build with Vala 0.52
+    (fetchpatch {
+      url = "https://github.com/martinpitt/umockdev/commit/a236f0b55fbb6ff50a6429da9d404703d6637d94.patch";
+      sha256 = "sZs9Ove1r7te/a9vmWUmFetLVhyzhHmx7ijhkK/2S5o=";
+    })
+  ];
 
   mesonFlags = [
     "-Dgtk_doc=true"
