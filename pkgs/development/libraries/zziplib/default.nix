ecc9070c1aaab0915ad417612cae5c271029cd87
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2021-03-24 17:56:00 -0400

    zziplib: fix missing links on darwin

--- pkgs/development/libraries/zziplib/default.nix
+++ pkgs/development/libraries/zziplib/default.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , fetchFromGitHub
+, fetchpatch
 , cmake
 , perl
 , pkg-config
@@ -20,6 +21,22 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     hash = "sha256-Ht3fBgdrTm4mCi5uhgQPNtpGzADoRVOpSuGPsIS6y0Q=";
   };
+
+  patches = [
+    # apply https://github.com/gdraheim/zziplib/pull/113
+    (fetchpatch {
+      url = "https://github.com/gdraheim/zziplib/commit/82a7773cd17828a3b0a4f5f552ae80c1cc8777c7.diff";
+      sha256 = "0ifqdzxwb5d19mziy9j6lhl8wj95jpxzm0d2c6y3bgwa931avd3y";
+    })
+    (fetchpatch {
+      url = "https://github.com/gdraheim/zziplib/commit/1cd611514c5f9559eb9dfc191d678dfc991f66db.diff";
+      sha256 = "11w9qa46xq49l113k266dnv8izzdk1fq4y54yy5w8zps8zd3xfny";
+    })
+    (fetchpatch {
+      url = "https://github.com/gdraheim/zziplib/commit/e47b1e1da952a92f917db6fb19485b8a0b1a42f3.diff";
+      sha256 = "0d032hkmi3s3db12z2zbppl2swa3gdpbj0c6w13ylv2g2ixglrwg";
+    })
+  ];
 
   nativeBuildInputs = [
     cmake
