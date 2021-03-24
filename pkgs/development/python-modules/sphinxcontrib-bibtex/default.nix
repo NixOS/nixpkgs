8d4293706b74b49c2117a9ffa68d6e20a7d52bf9
Frederik Rietdijk <fridh@fridh.nl>
2021-03-24 10:28:29 +0100

    python3Packages.sphinxcontrib-bibtex: 2.1.4 -> 2.2.0

--- pkgs/development/python-modules/sphinxcontrib-bibtex/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-bibtex/default.nix
@@ -9,14 +9,14 @@
 }:
 
 buildPythonPackage rec {
-  version = "2.1.4";
+  version = "2.2.0";
   pname = "sphinxcontrib-bibtex";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f53ec0cd534d2c8f0a51b4b3473ced46e9cb0dd99a7c5019249fe0ef9cbef18e";
+    sha256 = "7500843e154d76983c23bca5ca7380965e0725c46b8f484c1322d0b58a6ce3b2";
   };
 
   propagatedBuildInputs = [ oset pybtex pybtex-docutils sphinx ];
