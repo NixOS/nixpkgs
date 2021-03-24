08c081d049d68e51ef040dd73a9b50c3af2ca098
Frederik Rietdijk <fridh@fridh.nl>
2021-03-24 10:28:15 +0100

    python3Packages.ipywidgets: 7.5.1 -> 7.6.3

--- pkgs/development/python-modules/ipywidgets/default.nix
+++ pkgs/development/python-modules/ipywidgets/default.nix
@@ -14,11 +14,11 @@
 
 buildPythonPackage rec {
   pname = "ipywidgets";
-  version = "7.5.1";
+  version = "7.6.3";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e945f6e02854a74994c596d9db83444a1850c01648f1574adf144fbbabe05c97";
+    sha256 = "9f1a43e620530f9e570e4a493677d25f08310118d315b00e25a18f12913c41f0";
   };
 
   # Tests are not distributed
