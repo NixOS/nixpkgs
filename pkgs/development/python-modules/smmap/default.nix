4853a3a7213e83dddbbc55d2c0b6f6d1362c81b3
Frederik Rietdijk <fridh@fridh.nl>
2021-03-24 10:28:28 +0100

    python3Packages.smmap: 3.0.5 -> 4.0.0

--- pkgs/development/python-modules/smmap/default.nix
+++ pkgs/development/python-modules/smmap/default.nix
@@ -2,10 +2,10 @@
 
 buildPythonPackage rec {
   pname = "smmap";
-  version = "3.0.5";
+  version = "4.0.0";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "84c2751ef3072d4f6b2785ec7ee40244c6f45eb934d9e543e2c51f1bd3d54c50";
+    sha256 = "7e65386bd122d45405ddf795637b7f7d2b532e7e401d46bbe3fb49b9986d5182";
   };
 
   checkInputs = [ nosexcover ];
