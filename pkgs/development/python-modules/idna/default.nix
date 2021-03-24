e8e1339137b78557af91ee998b3296b9619111db
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-03-24 17:39:10 +0100

    python3Packages.idna: enable tests

--- pkgs/development/python-modules/idna/default.nix
+++ pkgs/development/python-modules/idna/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pytestCheckHook
 }:
 
 buildPythonPackage rec {
@@ -11,6 +12,8 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "c5b02147e01ea9920e6b0a3f1f7bb833612d507592c837a6c49552768f4054e1";
   };
+
+  checkInputs = [ pytestCheckHook ];
 
   meta = {
     homepage = "https://github.com/kjd/idna/";
