42b37d87fdb2895c5f18d40f8dfc338a80b42d45
R. RyanTM <ryantm-bot@ryantm.com>
2021-03-25 00:43:03 +0000

    bitwig-studio: 3.3.3 -> 3.3.5

--- pkgs/applications/audio/bitwig-studio/bitwig-studio3.nix
+++ pkgs/applications/audio/bitwig-studio/bitwig-studio3.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   pname = "bitwig-studio";
-  version = "3.3.3";
+  version = "3.3.5";
 
   src = fetchurl {
     url = "https://downloads.bitwig.com/stable/${version}/${pname}-${version}.deb";
-    sha256 = "sha256-NDkGHJDr6TCHEhgSKK7jLYk5RjGEj8+lDYZ4ywvG20g=";
+    sha256 = "sha256-GHRGFvWxPnIWExIeRaIHTmOksQfC4JRxL24MndkE2dU=";
   };
 
   nativeBuildInputs = [ dpkg makeWrapper wrapGAppsHook ];
