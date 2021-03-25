5d90316080cc1af77861cdf1d7d71dfed0b5b293
happysalada <raphael@megzari.com>
2021-03-25 13:48:53 +0900

    remove trailing whitespace

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11206,7 +11206,7 @@ in
     jdk = oraclejdk;
   };
 
-  sqlx-cli = callPackage ../development/tools/rust/sqlx-cli { 
+  sqlx-cli = callPackage ../development/tools/rust/sqlx-cli {
     inherit (darwin.apple_sdk.frameworks) SystemConfiguration CoreFoundation Security;
   };
 
