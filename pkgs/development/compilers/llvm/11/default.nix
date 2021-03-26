0ae11824896c7a172bd60566900529bb4c154404
John Ericson <John.Ericson@Obsidian.Systems>
2021-03-26 05:06:15 +0000

    llvmPackages_*.libunwind: Move into own directory
    I expect to soon give it some patches that would go in there too, but
    either way it's good to match the others.

--- pkgs/development/compilers/llvm/11/default.nix
+++ pkgs/development/compilers/llvm/11/default.nix
@@ -187,7 +187,7 @@ let
 
     openmp = callPackage ./openmp.nix {};
 
-    libunwind = callPackage ./libunwind.nix ({} //
+    libunwind = callPackage ./libunwind ({} //
       (lib.optionalAttrs (stdenv.hostPlatform.useLLVM or false) {
         stdenv = overrideCC stdenv buildLlvmTools.lldClangNoLibcxx;
       }));
