cba7c22f61519ec483e839828a3e8ee8d31c5085
Michael Francis <edude03@gmail.com>
2021-03-27 04:51:32 +0800

    Update pkgs/development/ruby-modules/gem-config/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/development/ruby-modules/gem-config/default.nix
+++ pkgs/development/ruby-modules/gem-config/default.nix
@@ -380,7 +380,7 @@ in
   };
 
   mimemagic = attrs: {
-    FREEDESKTOP_MIME_TYPES_PATH="${shared-mime-info}";
+    FREEDESKTOP_MIME_TYPES_PATH="${shared-mime-info}/share/mime/packages/freedesktop.org.xml";
   };
 
   msgpack = attrs: {
