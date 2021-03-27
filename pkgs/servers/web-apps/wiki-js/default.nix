47cc45ad77ed0c771ab48764594f99e32f22c145
Maximilian Bosch <maximilian@mbosch.me>
2021-03-27 21:09:14 +0100

    wiki-js: 2.5.191 -> 2.5.197
    ChangeLog: https://docs.requarks.io/releases#hotfix-8-25197

--- pkgs/servers/web-apps/wiki-js/default.nix
+++ pkgs/servers/web-apps/wiki-js/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "wiki-js";
-  version = "2.5.191";
+  version = "2.5.197";
 
   src = fetchurl {
     url = "https://github.com/Requarks/wiki/releases/download/${version}/${pname}.tar.gz";
-    sha256 = "sha256-lEHelZTFZxcds7t3TCMcd9b3rKdml54A0/V7gcQIyPA=";
+    sha256 = "sha256-0xM9BtQvSt5WkbKBri+KxB+Ghc4wgY8/TUgI6PCFmm0=";
   };
 
   sourceRoot = ".";
