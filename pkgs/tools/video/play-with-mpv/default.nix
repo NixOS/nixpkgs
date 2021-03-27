dadad73c0b49c3a2951ed6a30129b8b1be4aff0b
dawidsowa <dawid_sowa@posteo.net>
2021-03-27 23:42:40 +0100

    !fixup play-with-mpv: init at 2020-05-18
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/tools/video/play-with-mpv/default.nix
+++ pkgs/tools/video/play-with-mpv/default.nix
@@ -1,5 +1,11 @@
 { lib, python3Packages, fetchFromGitHub, fetchurl, youtube-dl, git }:
 
+let
+  install_freedesktop = fetchurl {
+    url = "https://github.com/thann/install_freedesktop/tarball/2673e8da4a67bee0ffc52a0ea381a541b4becdd4";
+    sha256 = "0j8d5jdcyqbl5p6sc1ags86v3hr2sghmqqi99d1mvc064g90ckrv";
+  };
+in
 python3Packages.buildPythonApplication rec {
   pname = "play-with-mpv";
   version = "unstable-2020-05-18";
@@ -9,11 +15,6 @@ python3Packages.buildPythonApplication rec {
       repo = "play-with-mpv";
       rev = "656448e03fe9de9e8bd21959f2a3b47c4acb8c3e";
       sha256 = "1qma8b3lnkdhxdjsnrq7n9zgy53q62j4naaqqs07kjxbn72zb4p4";
-  };
-
-  install_freedesktop = fetchurl {
-    url = "https://github.com/thann/install_freedesktop/tarball/2673e8da4a67bee0ffc52a0ea381a541b4becdd4";
-    sha256 = "0j8d5jdcyqbl5p6sc1ags86v3hr2sghmqqi99d1mvc064g90ckrv";
   };
 
   nativeBuildInputs = [ git ];
