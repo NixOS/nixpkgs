c797e205b325aefdda4ebd1740d32688d9137b80
Sandro <sandro.jaeckel@gmail.com>
2021-03-27 02:36:30 +0100

    Update pkgs/development/python-modules/mxnet/default.nix

--- pkgs/development/python-modules/mxnet/default.nix
+++ pkgs/development/python-modules/mxnet/default.nix
@@ -9,7 +9,7 @@
 }:
 
 buildPythonPackage {
-  inherit (pkgs.mxnet) name pname version src meta;
+  inherit (pkgs.mxnet) pname version src meta;
 
   buildInputs = [ pkgs.mxnet ];
   propagatedBuildInputs = [ requests numpy graphviz ];
