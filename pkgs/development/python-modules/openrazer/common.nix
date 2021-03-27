6d41c7e4d50bb952b6d47815805430fd57affa1f
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2021-03-26 22:17:05 -0400

    openrazer: 2.9.0 -> 3.0.0

--- pkgs/development/python-modules/openrazer/common.nix
+++ pkgs/development/python-modules/openrazer/common.nix
@@ -1,12 +1,12 @@
 { lib
 , fetchFromGitHub
 }: rec {
-  version = "2.9.0";
+  version = "3.0.0";
   src = fetchFromGitHub {
     owner = "openrazer";
     repo = "openrazer";
     rev = "v${version}";
-    sha256 = "1js7hq7zx5kj99brffrfaaah283ydkffmmrzsxv4mkd3nnd6rykk";
+    sha256 = "sha256-gw6Qt9BntPcF3zw19PXftDbhoCeBr8hwrujy51rb5Fc=";
   };
   meta = with lib; {
     homepage = "https://openrazer.github.io/";
