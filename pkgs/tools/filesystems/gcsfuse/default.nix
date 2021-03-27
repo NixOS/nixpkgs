ba589e6d5812e0ba2e1403b1bb6431e511494305
R. RyanTM <ryantm-bot@ryantm.com>
2021-03-27 10:52:33 +0000

    gcsfuse: 0.33.2 -> 0.34.0

--- pkgs/tools/filesystems/gcsfuse/default.nix
+++ pkgs/tools/filesystems/gcsfuse/default.nix
@@ -2,13 +2,13 @@
 
 buildGoPackage rec {
   pname = "gcsfuse";
-  version = "0.33.2";
+  version = "0.34.0";
 
   src = fetchFromGitHub {
     owner = "googlecloudplatform";
     repo = "gcsfuse";
     rev = "v${version}";
-    sha256 = "sha256-y40JWfD6selBo2IP7VgASmlNUDhXwOdQIRlRHPi3Nh0=";
+    sha256 = "sha256-lnuf2Xvzqy50L2oY3x8Bpvf5GzkRgx2hRbvKGssGTc8=";
   };
 
   goPackagePath = "github.com/googlecloudplatform/gcsfuse";
