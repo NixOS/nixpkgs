d97d5ddd2432be1e31ebf94e0cbc768c939b70b9
gtgteq <gtgteq.operator@gmail.com>
2021-03-28 08:54:31 +0900

    haskellPackages.github: Loosen version bounds at 0.26

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -1717,4 +1717,11 @@ self: super: {
   # Issue reported upstream, no bug tracker url yet.
   darcs = doJailbreak super.darcs;
 
+  # Too strict version bounds on base16-bytestring and http-link-header.
+  # This patch will be merged when next release comes.
+  github = appendPatch super.github (pkgs.fetchpatch {
+    url = "https://github.com/phadej/github/commit/514b175851dd7c4a9722ff203dd6f652a15d33e8.patch";
+    sha256 = "0pmx54xd7ah85y9mfi5366wbnwrp918j0wbx8yw8hrdac92qi4gh";
+  });
+
 } // import ./configuration-tensorflow.nix {inherit pkgs haskellLib;} self super
