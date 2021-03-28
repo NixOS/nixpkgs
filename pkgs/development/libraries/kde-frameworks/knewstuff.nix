d6e4f4fa657b8e7f90f3262d9d59894d77bf39b3
Thomas Tuegel <ttuegel@mailbox.org>
2021-03-28 11:18:11 -0500

    knewstuff: remove backported patch

--- pkgs/development/libraries/kde-frameworks/knewstuff.nix
+++ pkgs/development/libraries/kde-frameworks/knewstuff.nix
@@ -16,10 +16,4 @@ mkDerivation {
     ktextwidgets kwidgetsaddons qtbase qtdeclarative kirigami2
   ];
   propagatedBuildInputs = [ attica kservice kxmlgui ];
-
-  patches = [ (fetchpatch {
-    url = "https://github.com/KDE/knewstuff/commit/dbf788c10130eaa3f5ea37a7f22eb4569471aa04.patch";
-    sha256 = "1225rgqg1j120nvhgsahvsq2xlkg91lr37zp14x19krixxgx521j";
-    revert = true;
-  }) ];
 }
