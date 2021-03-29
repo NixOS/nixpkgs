7d51a0cdb0f9299aac4b118b1086418ea4ef6409
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-03-29 12:13:06 +0200

    coq-elpi: 1.6.1_8.11 -> 1.6.2_8.11

--- pkgs/development/coq-modules/coq-elpi/default.nix
+++ pkgs/development/coq-modules/coq-elpi/default.nix
@@ -14,7 +14,7 @@ in mkCoqDerivation {
   defaultVersion = lib.switch coq.coq-version [
     { case = "8.13"; out = "1.9.5"; }
     { case = "8.12"; out = "1.8.2_8.12"; }
-    { case = "8.11"; out = "1.6.1_8.11"; }
+    { case = "8.11"; out = "1.6.2_8.11"; }
   ] null;
   release."1.9.5".sha256      = "0gjdwmb6bvb5gh0a6ra48bz5fb3pr5kpxijb7a8mfydvar5i9qr6";
   release."1.9.4".sha256      = "0nii7238mya74f9g6147qmpg6gv6ic9b54x5v85nb6q60d9jh0jq";
@@ -25,6 +25,8 @@ in mkCoqDerivation {
   release."1.8.1".sha256      = "1fbbdccdmr8g4wwpihzp4r2xacynjznf817lhijw6kqfav75zd0r";
   release."1.8.0".sha256      = "13ywjg94zkbki22hx7s4gfm9rr87r4ghsgan23xyl3l9z8q0idd1";
   release."1.7.0".sha256      = "1ws5cqr0xawv69prgygbl3q6dgglbaw0vc397h9flh90kxaqgyh8";
+  release."1.6.2_8.11".sha256 = "06xrx0ljilwp63ik2sxxr7h617dgbch042xfcnfpy5x96br147rn";
+  release."1.6.2_8.11".version = "1.6.2";
   release."1.6.1_8.11".sha256 = "0yyyh35i1nb3pg4hw7cak15kj4y6y9l84nwar9k1ifdsagh5zq53";
   release."1.6.1_8.11".version = "1.6.1";
   release."1.6.0_8.11".sha256 = "0ahxjnzmd7kl3gl38kyjqzkfgllncr2ybnw8bvgrc6iddgga7bpq";
