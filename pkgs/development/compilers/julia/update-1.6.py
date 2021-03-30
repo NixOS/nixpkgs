66a76e3bd728b3380276bab25314fb348baea8ff
Samuel Ainsworth <skainsworth@gmail.com>
2021-03-30 02:06:14 +0000

    julia: update-1.5.py -> update-1.6.py

--- pkgs/development/compilers/julia/update-1.6.py
+++ pkgs/development/compilers/julia/update-1.6.py
@@ -11,11 +11,11 @@ assert latest[0] == "v"
 major, minor, patch = latest[1:].split(".")
 assert major == "1"
 # When a new minor version comes out we'll have to refactor/copy this update script.
-assert minor == "5"
+assert minor == "6"
 
 sha256 = subprocess.check_output(["nix-prefetch-url", "--unpack", f"https://github.com/JuliaLang/julia/releases/download/v{major}.{minor}.{patch}/julia-{major}.{minor}.{patch}-full.tar.gz"], text=True).strip()
 
-nix_path = os.path.join(os.path.dirname(__file__), "1.5.nix")
+nix_path = os.path.join(os.path.dirname(__file__), "1.6.nix")
 nix0 = open(nix_path, "r").read()
 nix1 = re.sub("maintenanceVersion = \".*\";", f"maintenanceVersion = \"{patch}\";", nix0)
 nix2 = re.sub("src_sha256 = \".*\";", f"src_sha256 = \"{sha256}\";", nix1)
