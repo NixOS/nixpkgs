273010f6a2a7e611ebd4facf460d230d1d2f296c
Wael Nasreddine <wael.nasreddine@gmail.com>
2021-03-30 09:57:45 -0700

    terraforming: init at 0.18.0 (#117731)
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30006,6 +30006,8 @@ in
     callPackage ../applications/networking/cluster/terraform-providers {}
   );
 
+  terraforming = callPackage ../applications/networking/cluster/terraforming { };
+
   terraform-compliance = python3Packages.callPackage ../applications/networking/cluster/terraform-compliance {};
 
   terraform-docs = callPackage ../applications/networking/cluster/terraform-docs {};
