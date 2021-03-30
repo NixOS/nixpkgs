e0deff88c5a210ce3e3a5ec056d764f302da44a2
zowoq <59103226+zowoq@users.noreply.github.com>
2021-03-31 06:13:15 +1000

    youtube-dl: 2021.03.25 -> 2021.03.31
    https://github.com/ytdl-org/youtube-dl/releases/tag/2021.03.31
    (cherry picked from commit 124564d5d212c8c9a74252ed718e6f1d65f3a5a0)

--- pkgs/tools/misc/youtube-dl/default.nix
+++ pkgs/tools/misc/youtube-dl/default.nix
@@ -18,11 +18,11 @@ buildPythonPackage rec {
   # The websites youtube-dl deals with are a very moving target. That means that
   # downloads break constantly. Because of that, updates should always be backported
   # to the latest stable release.
-  version = "2021.03.25";
+  version = "2021.03.31";
 
   src = fetchurl {
     url = "https://yt-dl.org/downloads/${version}/${pname}-${version}.tar.gz";
-    sha256 = "0ps8ydx4hbj6sl0m760zdm9pvhccjmwvx680i4akz3lk4z9wy0x3";
+    sha256 = "1svcgrhq1yxpcd6k3piqs5paalrcsq9bm79h5ras1g7yjzid05gj";
   };
 
   nativeBuildInputs = [ installShellFiles makeWrapper ];
