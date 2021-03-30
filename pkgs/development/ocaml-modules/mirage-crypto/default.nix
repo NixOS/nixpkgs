318d98aa0deddb175d9c49c4bf8b3e2756e489e0
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-03-30 14:56:02 +0200

    ocamlPackages.mirage-crypto: allow compiling with ocaml-freestanding

--- pkgs/development/ocaml-modules/mirage-crypto/default.nix
+++ pkgs/development/ocaml-modules/mirage-crypto/default.nix
@@ -1,4 +1,7 @@
-{ lib, fetchurl, buildDunePackage, ounit, cstruct, dune-configurator, eqaf, pkg-config }:
+{ lib, fetchurl, buildDunePackage, ounit, cstruct, dune-configurator, eqaf, pkg-config
+, withFreestanding ? false
+, ocaml-freestanding
+}:
 
 buildDunePackage rec {
   minimumOCamlVersion = "4.08";
@@ -11,13 +14,21 @@ buildDunePackage rec {
     sha256 = "8a5976fe7837491d2fbd1917b77524776f70ae590e9f55cf757cc8951b5481fc";
   };
 
+  patches = [
+    ./makefile-no-opam.patch
+  ];
+
   useDune2 = true;
 
   doCheck = true;
   checkInputs = [ ounit ];
 
   nativeBuildInputs = [ dune-configurator pkg-config ];
-  propagatedBuildInputs = [ cstruct eqaf ];
+  propagatedBuildInputs = [
+    cstruct eqaf
+  ] ++ lib.optionals withFreestanding [
+    ocaml-freestanding
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/mirage/mirage-crypto";
