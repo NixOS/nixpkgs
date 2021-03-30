d82c027134b28afab6ffecc353dad81597bd7a16
milahu <milahu@gmail.com>
2021-03-30 18:54:28 +0200

    fix: version 3.0, add runHook preBuild, add -j$NIX_BUILD_CORES

--- pkgs/tools/filesystems/nullfs/default.nix
+++ pkgs/tools/filesystems/nullfs/default.nix
@@ -1,19 +1,22 @@
 { lib, stdenv, fetchFromGitHub, kernel }:
 
 stdenv.mkDerivation rec {
+
   pname = "nullfs";
-  version = "0.3-git";
+  version = "0.3";
+  name = "${pname}-${version}+linux-${kernel.version}";
+  moduleName = pname;
+
   # why rename from nullfsvfs to nullfs?
   # * shorter
   # * the module name is nullfs
   # * this is an improved version of https://github.com/xrgtn/nullfs
-  name = "${pname}-${version}+linux-${kernel.version}";
-  moduleName = "nullfs";
+
   src = fetchFromGitHub {
     owner = "abbbi";
     repo = "nullfsvfs";
-    rev = "802930fb0e66c3e0ba7c81f2c87caa1dcd874b3f";
-    sha256 = "13qqc29jaa9s8hb7bvkrv4vfzy9cfmjdpmd4rysppi8x6cdlzhva";
+    rev = version;
+    sha256 = "01jm22arnff1s58ky76a5061mpwhnz322mp3dva3lyl2vimf3ncw";
   };
 
   hardeningDisable = [ "pic" "format" ];
@@ -25,11 +28,15 @@ stdenv.mkDerivation rec {
     cp -r "$src" "$out/build"
     chmod +w "$out/build"
   '';
+
   buildPhase = ''
-    make -C "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build" M="$out/build" modules
+    runHook preBuild
+    # workaround for broken Makefile
+    make -j$NIX_BUILD_CORES -C "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build" M="$out/build" modules
   '';
+
   installPhase = ''
-    # install to userspace
+    # install to userspace (not initrd)
     mkdir -p $out/lib/modules/${kernel.modDirVersion}/extra
     xz $out/build/${moduleName}.ko
     mv $out/build/${moduleName}.ko.xz $out/lib/modules/${kernel.modDirVersion}/extra/
@@ -37,6 +44,7 @@ stdenv.mkDerivation rec {
     chmod -R +w $out/build
     rm -r $out/build
   '';
+
   meta = with lib; {
     description = "Virtual black hole file system that behaves like /dev/null";
     longDescription = ''
