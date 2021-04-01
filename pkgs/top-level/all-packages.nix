e3645c05ed592121c191611f228b19befcf6ebb4
Fabian Affolter <fabian@affolter-engineering.ch>
2021-04-01 20:13:15 +0200

    python3Packages.callPackage (#117406)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5766,6 +5766,8 @@ in
 
   knockknock = callPackage ../tools/security/knockknock { };
 
+  knockpy = callPackage ../tools/security/knockpy { };
+
   kore = callPackage ../development/web/kore { };
 
   krakenx = callPackage ../tools/system/krakenx { };
