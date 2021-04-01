0323299e92b86c77b86903e2d512652d07048d0e
"Timothy DeHerrera" <"tim.deh@pm.me">
2021-03-31 18:35:21 -0600

    kakounePlugins.kakoune-text-objects: init at 2019-10-12

--- pkgs/applications/editors/kakoune/plugins/generated.nix
+++ pkgs/applications/editors/kakoune/plugins/generated.nix
@@ -208,6 +208,18 @@ let
     meta.homepage = "https://github.com/h-youhei/kakoune-surround/";
   };
 
+  kakoune-text-objects = buildKakounePluginFrom2Nix {
+    pname = "kakoune-text-objects";
+    version = "2019-10-12";
+    src = fetchFromGitHub {
+      owner = "Delapouite";
+      repo = "kakoune-text-objects";
+      rev = "aacd554982ba9a7a77a788817bb8ee2e1b380ee7";
+      sha256 = "0qdzl0z32wgw6k4ssy598sdqk2jh13lwvczslzyg4x5d6z8jwhyf";
+    };
+    meta.homepage = "https://github.com/Delapouite/kakoune-text-objects/";
+  };
+
   kakoune-vertical-selection = buildKakounePluginFrom2Nix {
     pname = "kakoune-vertical-selection";
     version = "2019-04-11";
