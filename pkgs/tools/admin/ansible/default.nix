22662001c0bedd54ed3053b616d572991a543d4d
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-04-02 02:07:03 +0200

    ansible: 2.10.0 -> 2.10.7

--- pkgs/tools/admin/ansible/default.nix
+++ pkgs/tools/admin/ansible/default.nix
@@ -7,14 +7,14 @@ rec {
   # for now.
   ansible_2_10 = python3Packages.toPythonApplication (python3Packages.ansible.overridePythonAttrs (old: rec {
     pname = "ansible";
-    version = "2.10.0";
+    version = "2.10.7";
 
     # TODO: migrate to fetchurl, when release becomes available on releases.ansible.com
     src = fetchFromGitHub {
       owner = pname;
       repo = pname;
       rev = "v${version}";
-      sha256 = "0k9rs5ajx0chaq0xr1cj4x7fr5n8kd4y856miss6k01iv2m7yx42";
+      sha256 = "sha256-qDUc3mvsGmZHA2TCfKFJgGcB6e1pxlBP5Oc5QJjLzLA=";
     };
   }));
 
