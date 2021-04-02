053e6c2b6644b465a75079648a3b11e1509ff85f
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2021-04-02 11:03:16 +0300

    lime: 4.4.34 -> 4.5.1

--- pkgs/development/libraries/lime/default.nix
+++ pkgs/development/libraries/lime/default.nix
@@ -10,7 +10,7 @@
 
 stdenv.mkDerivation rec {
   pname = "lime";
-  version = "4.4.34";
+  version = "4.5.1";
 
   src = fetchFromGitLab {
     domain = "gitlab.linphone.org";
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
     group = "BC";
     repo = pname;
     rev = version;
-    sha256 = "14jg1zisjbzflw3scfqdbwy48wq3cp93l867vigb8l40lkc6n26z";
+    sha256 = "1fsldk7gxagxkbkzksz6dz8a8dmix1lxfy8nvvp0m355pzgqj6lb";
   };
 
   buildInputs = [ bctoolbox soci belle-sip sqlite ];
