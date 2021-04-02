93f9883a054b299400654adfa2851183f8648f00
Jappie Klooster <jappieklooster@hotmail.com>
2021-04-02 11:06:49 -0400

    iodash: init at 0.1.7
    IODash is required to build the latest
    version of ydotool.
    
    I'm sure as a library it has many other use
    cases. It seems to be a contender towards
    the well known boost library.
    
    This init points to a temporary fork which makes
    it build. The install directive was missing.
    I'm intending to upstream this change if I can.
    Or otherwise make this a permanent fork.
    
    Point iodash to the right version
    
    Add comment in iodash on repo change
    
    Fix undefined variable src
    
    Remove jappie from maintainer list
    
    Update pkgs/development/libraries/iodash/default.nix
    
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>
    
    Update pkgs/development/libraries/iodash/default.nix
    
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>
    
    Update pkgs/development/libraries/iodash/default.nix
    
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>
    
    Fix dangling in
    
    Format patch for IODash

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6096,6 +6096,7 @@ in
 
   ispell = callPackage ../tools/text/ispell {};
 
+  iodash = callPackage ../development/libraries/iodash { };
   jumanpp = callPackage ../tools/text/jumanpp {};
 
   jump = callPackage ../tools/system/jump {};
