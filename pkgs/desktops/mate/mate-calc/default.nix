43a72457e023b5534eb32dfdd34fd0f3bea39b9e
José Romildo Malaquias <malaquias@gmail.com>
2021-04-02 18:56:02 -0300

    mate.mate-calc: 1.24.1 -> 1.24.2

--- pkgs/desktops/mate/mate-calc/default.nix
+++ pkgs/desktops/mate/mate-calc/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "mate-calc";
-  version = "1.24.1";
+  version = "1.24.2";
 
   src = fetchurl {
     url = "https://pub.mate-desktop.org/releases/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0imdimq5d5rjq8mkjcrsd683a2bn9acmhc0lmvyw71y0040inbaw";
+    sha256 = "1yg8j0dqy37fljd20pwxdgna3f1v7k9wmdr9l4r1nqf4a7zwi96l";
   };
 
   nativeBuildInputs = [
