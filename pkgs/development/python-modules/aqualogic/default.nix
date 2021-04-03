8d0b81ca5d15669cf91ce8da7fe9167dc33ab0c5
Fabian Affolter <mail@fabian-affolter.ch>
2021-04-03 22:20:47 +0200

    python3Packages.aqualogic: 2.5 -> 2.6

--- pkgs/development/python-modules/aqualogic/default.nix
+++ pkgs/development/python-modules/aqualogic/default.nix
@@ -1,20 +1,29 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
+, fetchpatch
 , pyserial
 , pytestCheckHook
 }:
 
 buildPythonPackage rec {
   pname = "aqualogic";
-  version = "2.5";
+  version = "2.6";
 
   src = fetchFromGitHub {
     owner = "swilson";
     repo = pname;
     rev = version;
-    sha256 = "sha256-yxd+A5dsB9gBwVlPNjz+IgDHKTktNky84bWZMhA/xa4=";
+    sha256 = "sha256-dAC/0OjvrC8J/5pu5vcOKV/WqgkAlz0LuFl0up6FQRM=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "allow-iobase-objects.patch";
+      url = "https://github.com/swilson/aqualogic/commit/185fe25a86c82c497a55c78914b55ed39f5ca339.patch";
+      sha256 = "072jrrsqv86bn3skibjc57111jlpm8pq2503997fl3h4v6ziwdxg";
+    })
+  ];
 
   propagatedBuildInputs = [ pyserial ];
 
