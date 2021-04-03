0e31f60cad792e54441be0c5804611d48051522a
Justin Humm <justin.humm@posteo.de>
2021-04-03 17:48:11 +0200

    openimagedenoise: 1.2.2 -> 1.3.0
    Moved 1.2.2 to an extra file, as we still need it for luxcorerender to
    build.
    
    https://github.com/LuxCoreRender/LuxCore/pull/482#issuecomment-744343949
    
    Also build with python3.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6346,7 +6346,9 @@ in
 
   lzip = callPackage ../tools/compression/lzip { };
 
-  luxcorerender = callPackage ../tools/graphics/luxcorerender { };
+  luxcorerender = callPackage ../tools/graphics/luxcorerender {
+    openimagedenoise = openimagedenoise_1_2_x;
+  };
 
   xz = callPackage ../tools/compression/xz { };
 
@@ -7046,6 +7048,8 @@ in
   openhantek6022 = libsForQt5.callPackage ../applications/science/electronics/openhantek6022 { };
 
   openimagedenoise = callPackage ../development/libraries/openimagedenoise { };
+
+  openimagedenoise_1_2_x = callPackage ../development/libraries/openimagedenoise/1_2_x.nix { };
 
   openmvg = callPackage ../applications/science/misc/openmvg { };
 
