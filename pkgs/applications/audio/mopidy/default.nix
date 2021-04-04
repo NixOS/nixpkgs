56a9a8c659db2c92ef513ba522c346e2420d532d
Thomas Nixon <tom@tomn.co.uk>
2021-04-04 17:11:36 +0100

    mopidy: allow overriding in mopidyPackages scope
    In the previous implementation, overrideScope' etc. was not exposed in
    mopidyPackages, which made it annoying to override the mopidy package,
    as the other packages in the scope would not pick up the changes,
    causing conflicts.

--- pkgs/applications/audio/mopidy/default.nix
+++ pkgs/applications/audio/mopidy/default.nix
@@ -1,46 +1,39 @@
-{ newScope, python }:
+{ lib, newScope, python }:
 
 # Create a custom scope so we are consistent in which python version is used
+lib.makeScope newScope (self: with self; {
+  inherit python;
+  pythonPackages = python.pkgs;
 
-let
-  callPackage = newScope self;
+  mopidy = callPackage ./mopidy.nix { };
 
-  self = {
+  mopidy-iris = callPackage ./iris.nix { };
 
-    inherit python;
-    pythonPackages = python.pkgs;
+  mopidy-local = callPackage ./local.nix { };
 
-    mopidy = callPackage ./mopidy.nix { };
+  mopidy-moped = callPackage ./moped.nix { };
 
-    mopidy-iris = callPackage ./iris.nix { };
+  mopidy-mopify = callPackage ./mopify.nix { };
 
-    mopidy-local = callPackage ./local.nix { };
+  mopidy-mpd = callPackage ./mpd.nix { };
 
-    mopidy-moped = callPackage ./moped.nix { };
+  mopidy-mpris = callPackage ./mpris.nix { };
 
-    mopidy-mopify = callPackage ./mopify.nix { };
+  mopidy-musicbox-webclient = callPackage ./musicbox-webclient.nix { };
 
-    mopidy-mpd = callPackage ./mpd.nix { };
+  mopidy-scrobbler = callPackage ./scrobbler.nix { };
 
-    mopidy-mpris = callPackage ./mpris.nix { };
+  mopidy-somafm = callPackage ./somafm.nix { };
 
-    mopidy-musicbox-webclient = callPackage ./musicbox-webclient.nix { };
+  mopidy-soundcloud = callPackage ./soundcloud.nix { };
 
-    mopidy-scrobbler = callPackage ./scrobbler.nix { };
+  mopidy-spotify = callPackage ./spotify.nix { };
 
-    mopidy-somafm = callPackage ./somafm.nix { };
+  mopidy-spotify-tunigo = callPackage ./spotify-tunigo.nix { };
 
-    mopidy-soundcloud = callPackage ./soundcloud.nix { };
+  mopidy-tunein = callPackage ./tunein.nix { };
 
-    mopidy-spotify = callPackage ./spotify.nix { };
+  mopidy-youtube = callPackage ./youtube.nix { };
 
-    mopidy-spotify-tunigo = callPackage ./spotify-tunigo.nix { };
-
-    mopidy-tunein = callPackage ./tunein.nix { };
-
-    mopidy-youtube = callPackage ./youtube.nix { };
-
-    mopidy-subidy = callPackage ./subidy.nix { };
-  };
-
-in self
+  mopidy-subidy = callPackage ./subidy.nix { };
+})
