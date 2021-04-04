8dddb70bb97ff76c291d582295fba0791ed1c82a
talyz <kim.lindberger@gmail.com>
2021-04-04 13:42:18 +0200

    nixos/discourse: Init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -890,6 +890,7 @@
   ./services/web-apps/bookstack.nix
   ./services/web-apps/convos.nix
   ./services/web-apps/cryptpad.nix
+  ./services/web-apps/discourse.nix
   ./services/web-apps/documize.nix
   ./services/web-apps/dokuwiki.nix
   ./services/web-apps/engelsystem.nix
