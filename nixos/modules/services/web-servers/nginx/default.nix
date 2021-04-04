ac3abece2ea933c40f77e36e4c1d6262b24e03d4
Sandro <sandro.jaeckel@gmail.com>
2021-04-04 08:22:18 +0200

    nixos/nginx: add upstreams examples
    I am not fully sure if they are fully correct but they deployed the right syntax.

--- nixos/modules/services/web-servers/nginx/default.nix
+++ nixos/modules/services/web-servers/nginx/default.nix
@@ -662,6 +662,7 @@ in
                 Defines the address and other parameters of the upstream servers.
               '';
               default = {};
+              example = { "127.0.0.1:8000" = {}; };
             };
             extraConfig = mkOption {
               type = types.lines;
@@ -676,6 +677,14 @@ in
           Defines a group of servers to use as proxy target.
         '';
         default = {};
+        example = {
+          "backend_server" = {
+            servers = { "127.0.0.1:8000" = {}; };
+            extraConfig = ''
+              keepalive 16;
+            '';
+          };
+        };
       };
 
       virtualHosts = mkOption {
