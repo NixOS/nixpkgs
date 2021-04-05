NixOS/nixpkgs

commit 05e5f5d515b0766d3867b5eaf470a532b20e3c7f

Fix tarball
libiconvOrLibc is null on ppc

Signed-off-by: Shea Levy <shea@shealevy.com>

pkgs/applications/search/recoll/default.nix
@@ -3,6 +3,8 @@
 , djvulibre, groff, libxslt, unzip, xpdf, antiword, catdoc, lyx
 , ghostscript, gawk, gnugrep, gnused, gnutar, gzip, libiconvOrLibc }:
 
+assert stdenv.system != "powerpc-linux";
+
 stdenv.mkDerivation rec {
   ver = "1.18.1";
   name = "recoll-${ver}";

commit 97bb239d9d2a53565883f81b73f61cbaa419f459

recoll: drop assert on hostPlatform.system
This was introduced in 05e5f5d5 ('Fix tarball') and no longer serves
the original purpose.

pkgs/applications/search/recoll/default.nix
@@ -33,8 +33,6 @@
 , withGui ? true
 }:
 
-assert stdenv.hostPlatform.system != "powerpc-linux";
-
 mkDerivation rec {
   pname = "recoll";
   version = "1.28.6";