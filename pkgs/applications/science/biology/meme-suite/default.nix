c8239ccb358806e8ecbb1c055c469259b36787d6
Gregory Schwartz <GregorySchwartz@users.noreply.github.com>
2021-04-05 11:40:22 -0400

    Update pkgs/applications/science/biology/meme-suite/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/applications/science/biology/meme-suite/default.nix
+++ pkgs/applications/science/biology/meme-suite/default.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ perl python3 zlib ];
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Motif-based sequence analysis tools";
     license = licenses.unfree;
     maintainers = with maintainers; [ gschwartz ];
