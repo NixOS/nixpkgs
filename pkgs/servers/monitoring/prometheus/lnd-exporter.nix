39331740f8127a427941a8daf1d32a7cad0e253f
Martin Milata <martin@martinmilata.cz>
2021-04-05 23:38:18 +0200

    prometheus-lnd-exporter: unstable-2020-12-04 -> unstable-2021-03-26

--- pkgs/servers/monitoring/prometheus/lnd-exporter.nix
+++ pkgs/servers/monitoring/prometheus/lnd-exporter.nix
@@ -2,23 +2,21 @@
 
 buildGoModule rec {
   pname = "lndmon";
-  version = "unstable-2020-12-04";
+  version = "unstable-2021-03-26";
 
   src = fetchFromGitHub {
     owner = "lightninglabs";
     repo = "lndmon";
-    sha256 = "0q72jbkhw1vpwxd0r80l1v4ab71sakc315plfqbijy7al9ywq5nl";
-    rev = "f07d574320dd1a6a428fecd47f3a5bb46a0fc4d1";
+    sha256 = "14lmmjq61p8yhc86swigs43risqi31vlmz7ri8j0n0fyp8lm2kxs";
+    rev = "3aa925aa4f633a6c4d132601922e78f173ae8ac1";
   };
 
   vendorSha256 = "06if387b9m02ciqgcissih1x06l33djp87vgspwzz589f77vczk8";
 
-  doCheck = false;
-
   passthru.tests = { inherit (nixosTests.prometheus-exporters) lnd; };
 
   meta = with lib; {
-    inherit (src.meta) homepage;
+    homepage = "https://github.com/lightninglabs/lndmon";
     description = "Prometheus exporter for lnd (Lightning Network Daemon)";
     license = licenses.mit;
     maintainers = with maintainers; [ mmilata ];
