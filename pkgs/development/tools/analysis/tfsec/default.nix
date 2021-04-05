8935034d90f7621446363af8782764fc8a1b5b6d
R. RyanTM <ryantm-bot@ryantm.com>
2021-04-05 06:13:12 +0000

    tfsec: 0.39.14 -> 0.39.16

--- pkgs/development/tools/analysis/tfsec/default.nix
+++ pkgs/development/tools/analysis/tfsec/default.nix
@@ -2,13 +2,13 @@
 
 buildGoPackage rec {
   pname = "tfsec";
-  version = "0.39.14";
+  version = "0.39.16";
 
   src = fetchFromGitHub {
     owner = "tfsec";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-Peq51XN8EEvHWdSL3ngBx74t7V4RUrZa4RpGnvZ0Ml8=";
+    sha256 = "sha256-5We3Nk/AU5dj37vG4pvqzvNztK01PAPadQV/CgHZe8w=";
   };
 
   goPackagePath = "github.com/tfsec/tfsec";
