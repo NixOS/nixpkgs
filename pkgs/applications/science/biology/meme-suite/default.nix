1aaee5f62dbebfdd39c09e29cbfc28f09287b5b3
Gregory Schwartz <GregorySchwartz@users.noreply.github.com>
2021-04-05 11:40:33 -0400

    Update pkgs/applications/science/biology/meme-suite/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/applications/science/biology/meme-suite/default.nix
+++ pkgs/applications/science/biology/meme-suite/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
   version = "5.1.1";
 
   src = fetchurl {
-    url = "http://meme-suite.org/meme-software/5.1.1/meme-5.1.1.tar.gz";
+    url = "http://meme-suite.org/meme-software/${version}/meme-${version}.tar.gz";
     sha256 = "38d73d256d431ad4eb7da2c817ce56ff2b4e26c39387ff0d6ada088938b38eb5";
   };
 
