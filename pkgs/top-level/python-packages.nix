5ba1edf6c8aa148c11399b9b802af2a9a87c00f2
Fabian Affolter <mail@fabian-affolter.ch>
2021-04-06 22:15:50 +0200

    python3Packages.angr: init at 9.0.6281

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -378,6 +378,8 @@ in {
 
   androguard = callPackage ../development/python-modules/androguard { };
 
+  angr = callPackage ../development/python-modules/angr { };
+
   aniso8601 = callPackage ../development/python-modules/aniso8601 { };
 
   annexremote = callPackage ../development/python-modules/annexremote { };
@@ -1355,6 +1357,8 @@ in {
   claripy =  callPackage ../development/python-modules/claripy { };
 
   cld2-cffi = callPackage ../development/python-modules/cld2-cffi { };
+
+  cle = callPackage ../development/python-modules/cle { };
 
   cleo = callPackage ../development/python-modules/cleo { };
 
