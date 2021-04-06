e289a280f38eaf44b366e51dd56d845b162c4785
Robert Schütz <dev@schuetz-co.de>
2021-04-06 23:54:37 +0200

    pyspread: fix build and simplify expression (#117906)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7606,9 +7606,7 @@ in
 
   pympress = callPackage ../applications/office/pympress { };
 
-  pyspread = python3Packages.callPackage ../applications/office/pyspread {
-    inherit (qt5) qtsvg wrapQtAppsHook;
-  };
+  pyspread = libsForQt5.callPackage ../applications/office/pyspread { };
 
   pythonIRClib = pythonPackages.pythonIRClib;
 
