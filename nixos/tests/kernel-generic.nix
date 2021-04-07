eae2d6318993e0f077cfdc6329c4d4eada840edf
Tim Steinbach <tim@nequissimus.com>
2021-04-07 12:08:57 -0400

    kernel: Add test for 5.4

--- nixos/tests/kernel-generic.nix
+++ nixos/tests/kernel-generic.nix
@@ -25,6 +25,7 @@ let
   }));
 in
 with pkgs; {
+  linux_5_4 = makeKernelTest "5.4" linuxPackages_5_4;
   linux_5_10 = makeKernelTest "5.10" linuxPackages_5_10;
   linux_5_11 = makeKernelTest "5.11" linuxPackages_5_11;
 }
