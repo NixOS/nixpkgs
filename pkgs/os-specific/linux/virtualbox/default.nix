8aa84bc5794affa169d503c6ac5c3b91a251eb45
Ryan Trinkle <ryan@trinkle.org>
2021-04-07 12:07:09 -0400

    virtualbox: add patch to enable building with linux kernel 5.11

--- pkgs/os-specific/linux/virtualbox/default.nix
+++ pkgs/os-specific/linux/virtualbox/default.nix
@@ -1,6 +1,8 @@
-{ stdenv, virtualbox, kernel }:
+{ stdenv, virtualbox, kernel, lib }:
 
-stdenv.mkDerivation {
+let inherit (lib) optional versionAtLeast;
+
+in stdenv.mkDerivation {
   name = "virtualbox-modules-${virtualbox.version}-${kernel.version}";
   src = virtualbox.modsrc;
   hardeningDisable = [
@@ -13,6 +15,12 @@ stdenv.mkDerivation {
 
   makeFlags = [ "INSTALL_MOD_PATH=$(out)" ];
   installTargets = [ "install" ];
+
+  # VirtualBox 6.1.18 doesn't build with linux kernel 5.11.  This fix is based on
+  # the discussion at https://forums.virtualbox.org/viewtopic.php?f=7&t=101860
+  patches = optional (versionAtLeast kernel.version "5.11" && versionAtLeast "6.1.18" virtualbox.version) [
+    ./linux-kernel-5.11.patch
+  ];
 
   enableParallelBuilding = true;
 
