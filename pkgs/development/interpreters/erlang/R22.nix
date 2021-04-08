cbf73f0803eaea6c236caf258620022f9fd241fc
David <dlesl@users.noreply.github.com>
2021-04-08 19:04:13 +0200

    erlang: add updateScript and update versions

--- pkgs/development/interpreters/erlang/R22.nix
+++ pkgs/development/interpreters/erlang/R22.nix
@@ -3,6 +3,6 @@
 # How to obtain `sha256`:
 # nix-prefetch-url --unpack https://github.com/erlang/otp/archive/OTP-${version}.tar.gz
 mkDerivation {
-  version = "22.3.4.16";
-  sha256 = "sha256-V0RwEPfjnHtEzShNh6Q49yGC5fbt2mNR4xy6f6iWvck=";
+  version = "22.3.4.17";
+  sha256 = "sha256-YhKU9I4qN+TVG3t//t9htUBkOu8DS75vbn/qWvS1zc0=";
 }
