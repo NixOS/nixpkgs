6da2f72f69ff212ca7ee842744e122bacdc18788
Maximilian Bosch <maximilian@mbosch.me>
2021-04-09 00:01:14 +0200

    gitea: 1.13.6 -> 1.13.7
    ChangeLog: https://github.com/go-gitea/gitea/releases/tag/v1.13.7

--- pkgs/applications/version-management/gitea/default.nix
+++ pkgs/applications/version-management/gitea/default.nix
@@ -16,12 +16,12 @@ with lib;
 
 buildGoPackage rec {
   pname = "gitea";
-  version = "1.13.6";
+  version = "1.13.7";
 
   # not fetching directly from the git repo, because that lacks several vendor files for the web UI
   src = fetchurl {
     url = "https://github.com/go-gitea/gitea/releases/download/v${version}/gitea-src-${version}.tar.gz";
-    sha256 = "1f0fsqcmmqygv0r796ddr2fjhh333i9nr0cqk9x2b2kbs1z264vf";
+    sha256 = "sha256-jJbX+kcXqd1v8aXNhmt24mq9mxOpTogCVm263rHVGHw=";
   };
 
   unpackPhase = ''
