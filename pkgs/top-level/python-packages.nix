c69e6e52e8e9be2ae5f2de85c9698e030ca2d70a
Robert Schütz <dev@schuetz-co.de>
2021-04-09 13:43:23 +0200

    python3Packages.pytest-subprocess: init at 1.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5198,6 +5198,8 @@ in {
 
   pyshark = callPackage ../development/python-modules/pyshark { };
 
+  pytest-subprocess = callPackage ../development/python-modules/pytest-subprocess { };
+
   python-codon-tables = callPackage ../development/python-modules/python-codon-tables { };
 
   python-csxcad = callPackage ../development/python-modules/python-csxcad { };
