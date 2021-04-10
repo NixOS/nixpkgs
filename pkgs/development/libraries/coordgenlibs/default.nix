f8b50175c6d9e186b67409fbd0cba062993ae62f
R. RyanTM <ryantm-bot@ryantm.com>
2021-04-10 07:09:59 +0000

    coordgenlibs: 2.0.2 -> 2.0.3

--- pkgs/development/libraries/coordgenlibs/default.nix
+++ pkgs/development/libraries/coordgenlibs/default.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation rec {
   pname = "coordgenlibs";
-  version = "2.0.2";
+  version = "2.0.3";
 
   src = fetchFromGitHub {
     owner = "schrodinger";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-BcDqrOotqRPMnzvfjzbeCf8gT3yQzKihywKrpah1D7w=";
+    sha256 = "sha256-uperQnJ1Q+s15pAlg/f4XR5VJI484ygZ0F6pMvcVDv8=";
   };
 
   nativeBuildInputs = [ cmake ];
