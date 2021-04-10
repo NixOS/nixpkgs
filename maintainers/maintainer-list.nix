2cf806993270e86dd6238846e8c8531c2bbfb99e
Akshay <nerdy@peppe.rs>
2021-04-10 09:06:18 +0530

    maintainers: add nerdypepper

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6825,6 +6825,12 @@
     githubId = 628342;
     name = "Tim Steinbach";
   };
+  nerdypepper = {
+    email = "nerdy@peppe.rs";
+    github = "nerdypepper";
+    githubId = 23743547;
+    name = "Akshay Oppiliappan";
+  };
   netixx = {
     email = "dev.espinetfrancois@gmail.com";
     github = "netixx";
