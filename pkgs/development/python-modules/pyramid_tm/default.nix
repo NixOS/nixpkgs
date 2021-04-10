500740d1b15998d3c5e7cdb6657ea88f3c84b732
Holger Peters <holger.peters@posteo.de>
2021-04-10 12:41:27 +0200

    Add pyramid_tm

---
+++ pkgs/development/python-modules/pyramid_tm/default.nix
@@ -0,0 +1,28 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, transaction
+, pyramid
+, setuptools
+}:
+
+buildPythonPackage rec {
+  pname = "pyramid_chameleon";
+  version = "2.4";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "d176792a50eb015d7865b44bd9b24a7bd0489fa9a5cebbd17b9e05048cef9014";
+  };
+
+  propagatedBuildInputs = [ pyramid transaction setuptools ];
+
+  pythonImportsCheck = [ "pyramid_tm" ];
+
+  meta = with lib; {
+    description = "Transaction manager for pyramid";
+    homepage = "https://github.com/Pylons/pyramid_tm";
+    license = licenses.bsd0;
+    maintainers = with maintainers; [ holgerpeters ];
+  };
+}
