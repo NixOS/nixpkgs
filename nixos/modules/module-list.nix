a446a85bdce5c2035506c8084f7742ebfd22e539
Izorkin <izorkin@elven.pw>
2021-04-11 11:11:11 +0300

    nixos/peertube: init service

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -956,6 +956,7 @@
   ./services/web-apps/moodle.nix
   ./services/web-apps/nextcloud.nix
   ./services/web-apps/nexus.nix
+  ./services/web-apps/peertube.nix
   ./services/web-apps/plantuml-server.nix
   ./services/web-apps/plausible.nix
   ./services/web-apps/pgpkeyserver-lite.nix
