069bbc5b496d964aadc6c2690f0ccfbd04bdce59
Sandro <sandro.jaeckel@gmail.com>
2021-04-11 23:14:54 +0200

    Update pkgs/tools/inputmethods/touchegg/default.nix

--- pkgs/tools/inputmethods/touchegg/default.nix
+++ pkgs/tools/inputmethods/touchegg/default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "JoseExposito";
     repo = pname;
-    rev = "v${version}";
+    rev = version;
     sha256 = "1v1sskyxmvbc6j4l31zbabiklfmy2pm77bn0z0baj1dl3wy7xcj2";
   };
 
