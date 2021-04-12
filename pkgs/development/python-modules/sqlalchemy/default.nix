b56b10fc23691612a6f91bca345363e715a862fb
Ivan Solyankin <vanzef@gmail.com>
2021-04-12 11:05:44 +0300

    pythonPackages.sqlalchemy: 1.3.23 -> 1.4.7

--- pkgs/development/python-modules/sqlalchemy/default.nix
+++ pkgs/development/python-modules/sqlalchemy/default.nix
@@ -1,5 +1,8 @@
-{ stdenv, lib, fetchPypi, buildPythonPackage, isPy3k, isPy35, fetchpatch
+{ stdenv, lib, fetchPypi, buildPythonPackage, isPy27, isPy3k, pythonAtLeast
+, pythonOlder
 , mock
+, greenlet
+, importlib-metadata
 , pysqlite ? null
 , pytestCheckHook
 , pytest_xdist
@@ -7,20 +10,14 @@
 
 buildPythonPackage rec {
   pname = "SQLAlchemy";
-  version = "1.3.23";
+  version = "1.4.7";
+
+  disabled = !(isPy27 || pythonAtLeast "3.6");
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "6fca33672578666f657c131552c4ef8979c1606e494f78cd5199742dfb26918b";
+    sha256 = "1aghjm27k1q9kn2yz1871qvaxsaxbxy9kdw1vckcz34cv2bmy4c4";
   };
-
-  patches = [
-    # fix test_pyodbc_extra_connect_azure test failure
-    (fetchpatch {
-      url = "https://github.com/sqlalchemy/sqlalchemy/commit/7293b3dc0e9eb3dae84ffd831494b85355df8e73.patch";
-      sha256 = "1z61lzxamz74771ddlqmbxba1dcr77f016vqfcmb44dxb228w2db";
-    })
-  ];
 
   checkInputs = [
     pytestCheckHook
@@ -30,6 +27,9 @@ buildPythonPackage rec {
 
   pytestFlagsArray = [ "-n auto" ];
 
+  propagatedBuildInputs = lib.optional isPy3k greenlet
+    ++ lib.optional (pythonOlder "3.8") importlib-metadata;
+
   postInstall = ''
     sed -e 's:--max-worker-restart=5::g' -i setup.cfg
   '';
@@ -37,7 +37,7 @@ buildPythonPackage rec {
   dontUseSetuptoolsCheck = true;
 
   # disable mem-usage tests on mac, has trouble serializing pickle files
-  disabledTests = lib.optionals isPy35 [ "exception_persistent_flush_py3k "]
+  disabledTests = lib.optionals isPy3k [ "exception_persistent_flush_py3k "]
     ++ lib.optionals stdenv.isDarwin [ "MemUsageWBackendTest" "MemUsageTest" ];
 
   meta = with lib; {
