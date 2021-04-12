278d04f8ef9e14b37ebc24485b9c4606088fe6c6
Hernawan Fa'iz Abdillah <hernawan.faiz.abdillah@gmail.com>
2021-04-12 08:11:35 +0700

    gpaste: 3.38.5 → 3.40.1

--- pkgs/desktops/gnome-3/misc/gpaste/default.nix
+++ pkgs/desktops/gnome-3/misc/gpaste/default.nix
@@ -17,14 +17,14 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "3.38.5";
+  version = "3.40.1";
   pname = "gpaste";
 
   src = fetchFromGitHub {
     owner = "Keruspe";
     repo = "GPaste";
     rev = "v${version}";
-    sha256 = "sha256-hUqFijqUQ1W8OThpbioqcxkOgYvScKUBmXN84MbMPGE=";
+    sha256 = "sha256-coMye/arH0JYgcjf/y3ZS8Bijuivs9m5Z7EiJQJzThk=";
   };
 
   patches = [
