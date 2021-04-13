317188f03f7a6700f23d384731e202d546083e27
Stephane TEYSSIER <stephane.teyssier@wescale.fr>
2021-04-13 12:03:13 +0200

    terraform-providers.keycloak: 1.20.0 -> 3.0.0

--- pkgs/applications/networking/cluster/terraform-providers/keycloak/default.nix
+++ pkgs/applications/networking/cluster/terraform-providers/keycloak/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "terraform-provider-keycloak";
-  version = "1.20.0";
+  version = "3.0.0";
 
   src = fetchFromGitHub {
     owner = "mrparkers";
     repo = "terraform-provider-keycloak";
     rev = version;
-    sha256 = "1h8780k8345pf0s14k1pmwdjbv2j08h4rq3jwds81mmv6qgj1r2n";
+    sha256 = "1q9vzmj9c7mznv6al58d3rs5kk1fh28k1qccx46hcbk82z52da3a";
   };
 
-  vendorSha256 = "12iary7p5qsbl4xdhfd1wh92mvf2fiylnb3m1d3m7cdcn32rfimq";
+  vendorSha256 = "0kh6lljvqd577s19gx0fmfsmx9wm3ikla3jz16lbwwb8ahbqcw1f";
 
   doCheck = false;
 
