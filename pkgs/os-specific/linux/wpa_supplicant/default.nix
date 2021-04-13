aaa79e071c4f7871e9714b411a94363851f7c947
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-04-13 18:44:35 +0200

    wpa_supplicant: add patch for CVE-2021-30004
    In wpa_supplicant and hostapd 2.9, forging attacks may occur because
    AlgorithmIdentifier parameters are mishandled in tls/pkcs1.c and
    tls/x509v3.c.
    
    Fixes: CVE-2021-30004
    (cherry picked from commit 9f9ab6fffc3b87d7640d58f849fe1121e9f00fa0)

--- pkgs/os-specific/linux/wpa_supplicant/default.nix
+++ pkgs/os-specific/linux/wpa_supplicant/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, openssl, pkgconfig, libnl
+{ stdenv, fetchurl, fetchpatch, openssl, pkgconfig, libnl
 , dbus, readline ? null, pcsclite ? null
 }:
 
@@ -30,6 +30,12 @@ stdenv.mkDerivation rec {
       name = "CVE-2021-27803.patch";
       url = "https://w1.fi/security/2021-1/0001-P2P-Fix-a-corner-case-in-peer-addition-based-on-PD-R.patch";
       sha256 = "04cnds7hmbqc44jasabjvrdnh66i5hwvk2h2m5z94pmgbzncyh3z";
+    })
+    # In wpa_supplicant and hostapd 2.9, forging attacks may occur because AlgorithmIdentifier parameters are mishandled in tls/pkcs1.c and tls/x509v3.c.
+    (fetchpatch {
+      name = "CVE-2021-30004.patch";
+      url = "https://w1.fi/cgit/hostap/patch/?id=a0541334a6394f8237a4393b7372693cd7e96f15";
+      sha256 = "1gbhlz41x1ar1hppnb76pqxj6vimiypy7c4kq6h658637s4am3xg";
     })
   ];
 
