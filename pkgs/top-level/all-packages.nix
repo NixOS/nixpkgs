db529c23ccbcdea39c0b917088f97a92559708f6
Steven Roose <steven@stevenroose.org>
2021-04-13 11:09:54 +0100

    peertube: init at 3.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25093,6 +25093,11 @@ in
 
   peek = callPackage ../applications/video/peek { };
 
+  peertube = callPackage ../servers/peertube {
+    # until v15 is supported but using default version
+    nodejs = pkgs.nodejs-14_x;
+  };
+
   pflask = callPackage ../os-specific/linux/pflask {};
 
   pfsshell = callPackage ../tools/misc/pfsshell { };
