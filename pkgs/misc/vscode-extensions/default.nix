165db62d93abe0c9986a05e9d8071251d43658f3
Sandro <sandro.jaeckel@gmail.com>
2021-04-14 21:21:13 +0200

    Update pkgs/misc/vscode-extensions/default.nix

--- pkgs/misc/vscode-extensions/default.nix
+++ pkgs/misc/vscode-extensions/default.nix
@@ -391,17 +391,17 @@ let
       };
 
       foxundermoon.shell-format = buildVscodeMarketplaceExtension {
-        meta = with lib; {
-          downloadPage = "https://marketplace.visualstudio.com/items?itemName=foxundermoon.shell-format";
-          homepage = "https://github.com/foxundermoon/vs-shell-format";
-          license = licenses.mit;
-          maintainers = with maintainers; [ dbirks ];
-        };
         mktplcRef = {
           name = "shell-format";
           publisher = "foxundermoon";
           version = "7.1.0";
           sha256 = "09z72mdr5bfdcb67xyzlv7lb9vyjlc3k9ackj4jgixfk40c68cnj";
+        };
+        meta = with lib; {
+          downloadPage = "https://marketplace.visualstudio.com/items?itemName=foxundermoon.shell-format";
+          homepage = "https://github.com/foxundermoon/vs-shell-format";
+          license = licenses.mit;
+          maintainers = with maintainers; [ dbirks ];
         };
       };
 
