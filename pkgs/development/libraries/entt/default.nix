5f0fdcf3e38bcc96f8140da5ecf6b77784dca702
R. RyanTM <ryantm-bot@ryantm.com>
2021-04-14 23:30:23 +0000

    entt: 3.7.0 -> 3.7.1

--- pkgs/development/libraries/entt/default.nix
+++ pkgs/development/libraries/entt/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchFromGitHub, cmake }:
 stdenv.mkDerivation rec {
   pname = "entt";
-  version = "3.7.0";
+  version = "3.7.1";
 
   src = fetchFromGitHub {
     owner = "skypjack";
     repo = "entt";
     rev = "v${version}";
-    sha256 = "sha256-qDjt74nijZhXW7F7GW0CSv6JWOc/kXN7ndbkwSO0+1s=";
+    sha256 = "sha256-t1QRqasb82W277XEV2FG5JrsQWIWZ0G5V7wLI+p4MpQ=";
   };
 
   nativeBuildInputs = [ cmake ];
