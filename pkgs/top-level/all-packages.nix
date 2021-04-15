NixOS/nixpkgs

commit 22f1c746dfbd9f83f63002f5ad79bc48dec21189

stdenv: use gcc9 for aarch64
Several aarch64 builds fail with is referenced by DSO followed by
bin/ld: final link failed: bad value. This seems related to libgcc_s.so.

https://github.com/NixOS/nixpkgs/issues/108111

pkgs/top-level/all-packages.nix
@@ -9180,7 +9180,8 @@ in
     then ../development/compilers/gcc/6
     else ../development/compilers/gcc/10);
   gcc = if (with stdenv.targetPlatform; isVc4 || libc == "relibc")
-    then gcc6 else gcc10;
+    then gcc6 else
+      if stdenv.targetPlatform.isAarch64 then gcc9 else gcc10;
   gcc-unwrapped = gcc.cc;
 
   gccStdenv = if stdenv.cc.isGNU then stdenv else stdenv.override {

commit bcdaf43b38edd9c21afb11357d3d706f1d99e055

gccFun: use gcc9 for aarch64
I'm assuming this was accidentally omitted in 22f1c746dfb, because I
don't think it makes sense to use one version of gcc for gcc and another
for gccFun.

Fixes: 22f1c746dfb ("stdenv: use gcc9 for aarch64")

pkgs/top-level/all-packages.nix
@@ -10176,8 +10176,8 @@ in
   gerbil-support = callPackage ../development/compilers/gerbil/gerbil-support.nix { };
   gerbilPackages-unstable = gerbil-support.gerbilPackages-unstable; # NB: don't recurseIntoAttrs for (unstable!) libraries
 
-  gccFun = callPackage (if (with stdenv.targetPlatform; isVc4 || libc == "relibc")
-    then ../development/compilers/gcc/6
+  gccFun = callPackage (if (with stdenv.targetPlatform; isVc4 || libc == "relibc") then ../development/compilers/gcc/6
+    else if stdenv.targetPlatform.isAarch64 then ../development/compilers/gcc/9
     else ../development/compilers/gcc/10);
   gcc = if (with stdenv.targetPlatform; isVc4 || libc == "relibc")
     then gcc6 else