0f6104563cbcfce0b8dc85c040cd037a01e2803f
Tim Steinbach <tim@nequissimus.com>
2021-04-15 11:28:27 -0400

    linux: 5.10.29 -> 5.10.30
    (cherry picked from commit ed540048c898b898e8ad39d50609bd9995a14c3c)

--- pkgs/os-specific/linux/kernel/linux-5.10.nix
+++ pkgs/os-specific/linux/kernel/linux-5.10.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.10.29";
+  version = "5.10.30";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1v79wylb2kd9gadiqf7dr7jcgynr970bbga09mdn940sq536g30m";
+    sha256 = "0h06lavcbbj9a4dfzca9sprghiq9z33q8i4gh3n2912wmjsnj0nl";
   };
 } // (args.argsOverride or {}))
