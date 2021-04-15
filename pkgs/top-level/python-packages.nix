e2efd87f5ec6378dbbdd62dd8ae482fea6aaf7ab
Cole Helbling <cole.e.helbling@outlook.com>
2021-04-15 15:13:24 -0700

    python3.pkgs.liblzfse: init at 0.4.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3889,6 +3889,10 @@ in {
 
   liblarch = callPackage ../development/python-modules/liblarch { };
 
+  liblzfse = callPackage ../development/python-modules/liblzfse {
+    inherit (pkgs) lzfse;
+  };
+
   libmodulemd = pipe pkgs.libmodulemd [
     toPythonModule
     (p:
