2796cfa5f0851c54cb0fba68c356e89a9afb7622
Dmitry Bogatov <git#v1@kaction.cc>
2021-04-16 00:00:00 +0000

    gopls: fix privacy breach
    Upstream sources provide links to online documentation with Google Analytics
    marker.

--- pkgs/development/tools/gopls/default.nix
+++ pkgs/development/tools/gopls/default.nix
@@ -10,6 +10,11 @@ buildGoModule rec {
     sha256 = "0hbfxdsbfz044vw8zp223ni6m7gcwqpff4xpjiqmihhgga5849lf";
   };
 
+  # Fix privacy breach.
+  postPatch = ''
+    find -type f | xargs sed -i 's/utm_source=gopls//g'
+  '';
+
   modRoot = "gopls";
   vendorSha256 = "0r9bffgi9ainqrl4kraqy71rgwdfcbqmv3srs12h3xvj0w5ya5rz";
 
