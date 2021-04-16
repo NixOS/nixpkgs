f6a33cfe92127b233170487faf3c17200ffde700
Arnout Engelen <arnout@bzzt.net>
2021-04-16 17:49:23 +0200

    not for merge: test if ofborg tests passthru.tests
    please ignore :)

--- pkgs/applications/networking/seaweedfs/default.nix
+++ pkgs/applications/networking/seaweedfs/default.nix
@@ -1,24 +1,35 @@
-{ lib, fetchFromGitHub, buildGoModule }:
+{ lib
+, fetchFromGitHub
+, buildGoModule
+, runCommand
+}:
 
-buildGoModule rec {
-  pname = "seaweedfs";
-  version = "2.36";
+let
+  seaweedfs = buildGoModule rec {
+    pname = "seaweedfs";
+    version = "2.36";
 
-  src = fetchFromGitHub {
-    owner = "chrislusf";
-    repo = "seaweedfs";
-    rev = version;
-    sha256 = "sha256-BVn+mV5SjyODcT+O8LXfGA42/Si5+GrdkjP0tAPiuTM=";
+    src = fetchFromGitHub {
+      owner = "chrislusf";
+      repo = "seaweedfs";
+      rev = version;
+      sha256 = "sha256-BVn+mV5SjyODcT+O8LXfGA42/Si5+GrdkjP0tAPiuTM=";
+    };
+
+    vendorSha256 = "sha256-qdgnoh+53o3idCfpkEFGK88aUVb2F6oHlSRZncs2hyY=";
+
+    subPackages = [ "weed" ];
+
+    passthru.tests.check-version = runCommand "weed-version" {} ''
+      ${seaweedfs}/bin/weed version | grep -Fw ${version}x
+      mkdir $out
+    '';
+
+    meta = with lib; {
+      description = "Simple and highly scalable distributed file system";
+      homepage = "https://github.com/chrislusf/seaweedfs";
+      maintainers = [ maintainers.raboof ];
+      license = licenses.asl20;
+    };
   };
-
-  vendorSha256 = "sha256-qdgnoh+53o3idCfpkEFGK88aUVb2F6oHlSRZncs2hyY=";
-
-  subPackages = [ "weed" ];
-
-  meta = with lib; {
-    description = "Simple and highly scalable distributed file system";
-    homepage = "https://github.com/chrislusf/seaweedfs";
-    maintainers = [ maintainers.raboof ];
-    license = licenses.asl20;
-  };
-}
+in seaweedfs
