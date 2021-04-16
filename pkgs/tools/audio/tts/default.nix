a7ee5158ab295f1749b1e728b3a035e3d0f9ae71
Jörg Thalheim <joerg@thalheim.io>
2021-04-16 09:33:04 +0200

    tts: 0.0.11 -> 0.0.12

--- pkgs/tools/audio/tts/default.nix
+++ pkgs/tools/audio/tts/default.nix
@@ -2,6 +2,7 @@
 , python3Packages
 , fetchFromGitHub
 , python3
+, fetchpatch
 }:
 
 # USAGE:
@@ -9,22 +10,31 @@
 # # pick your favorite vocoder/tts model
 # $ tts-server --model_name tts_models/en/ljspeech/glow-tts --vocoder_name vocoder_models/universal/libri-tts/fullband-melgan
 #
+# If you upgrade from an old version you may have to delete old models from ~/.local/share/tts
+# Also note that your tts version might not support all available models so check:
+#   https://github.com/coqui-ai/TTS/releases/tag/v0.0.12
+#
 # For now, for deployment check the systemd unit in the pull request:
 #   https://github.com/NixOS/nixpkgs/pull/103851#issue-521121136
-#
-# Check the latest release for compatible models:
-#   https://github.com/coqui-ai/TTS/releases/tag/v0.0.11
 
 python3Packages.buildPythonApplication rec {
   pname = "tts";
-  version = "0.0.11";
+  version = "0.0.12";
 
   src = fetchFromGitHub {
     owner = "coqui-ai";
     repo = "TTS";
     rev = "v${version}";
-    sha256 = "0kk9bgiw2x5ybwz0v3zrfaxajl3lnccc9xmrwc295n2rfkmwxsis";
+    sha256 = "sha256-0M9wcdBmuTK+NvEGsXEdoYiVFjw8G2MRUwmi1PJgmzI=";
   };
+
+  patches = [
+    # https://github.com/coqui-ai/TTS/pull/435
+    (fetchpatch {
+      url = "https://github.com/coqui-ai/TTS/commit/97f98e4c4584ef14ed2f4885aa02c162d9364a00.patch";
+      sha256 = "sha256-DAZYOOAe+6TYBF5ukFq5HRwm49askEvNEivuwb/oCWM=";
+    })
+  ];
 
   preBuild = ''
     # numba jit tries to write to its cache directory
@@ -39,6 +49,8 @@ python3Packages.buildPythonApplication rec {
     sed -i -e 's!numpy==[^"]*!numpy!' requirements.txt setup.py
     sed -i -e 's!umap-learn==[^"]*!umap-learn!' requirements.txt setup.py
     # Not required for building/installation but for their development/ci workflow
+    sed -i -e '/black/d' requirements.txt
+    sed -i -e '/isor/d' requirements.txt
     sed -i -e '/pylint/d' requirements.txt
     sed -i -e '/cardboardlint/d' requirements.txt setup.py
   '';
