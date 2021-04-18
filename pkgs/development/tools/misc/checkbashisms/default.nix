785ba85deb67a79c432aad1e72c094eac3c64e74
Dmitry Bogatov <git#v1@kaction.cc>
2021-04-18 00:00:00 +0000

    checkbashisms: fix hammering warnings

--- pkgs/development/tools/misc/checkbashisms/default.nix
+++ pkgs/development/tools/misc/checkbashisms/default.nix
@@ -12,19 +12,28 @@ stdenv.mkDerivation rec {
   buildInputs = [ perl ];
 
   buildPhase = ''
+    runHook preBuild
+
     substituteInPlace ./scripts/checkbashisms.pl \
       --replace '###VERSION###' "$version"
+
+    runHook postBuild
   '';
   installPhase = ''
+    runHook preInstall
+
     installManPage scripts/$pname.1
     installShellCompletion --bash --name $pname scripts/$pname.bash_completion
     install -D -m755 scripts/$pname.pl $out/bin/$pname
+
+    runHook postInstall
   '';
 
   meta = {
     homepage = "https://sourceforge.net/projects/checkbaskisms/";
     description = "Check shell scripts for non-portable syntax";
-    license = lib.licenses.gpl2;
+    license = lib.licenses.gpl2Plus;
+    maintainers = with lib.maintainers; [ kaction ];
     platforms = lib.platforms.unix;
   };
 }
