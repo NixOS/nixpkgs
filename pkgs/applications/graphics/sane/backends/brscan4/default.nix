de766883f718aedad3f95dd39bde2c8eefdb2326
Antoine Eiche <lewo@abesis.fr>
2021-04-18 19:46:14 +0200

    brscan4: 0.4.9-1 -> 0.4.10-1
    (cherry picked from commit f907aadb1d8e5356c4d06867257e41507d4ba222)
    
    Reason: 0.4.9-1 can no longer be downloaded and is not is the binary cache.

--- pkgs/applications/graphics/sane/backends/brscan4/default.nix
+++ pkgs/applications/graphics/sane/backends/brscan4/default.nix
@@ -11,15 +11,15 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "brscan4";
-  version = "0.4.9-1";
+  version = "0.4.10-1";
   src = {
     "i686-linux" = fetchurl {
       url = "http://download.brother.com/welcome/dlf006646/${pname}-${version}.i386.deb";
-      sha256 = "0pkg9aqvnkpjnb9cgzf7lxw2g4jqrf2w98irkv22r0gfsfs3nwma";
+      sha256 = "sha256-ymIAg+rfSYP5uzsAM1hUYZacJ0PXmKEoljNtb0pgGMw=";
     };
     "x86_64-linux" = fetchurl {
       url = "https://download.brother.com/welcome/dlf006645/${pname}-${version}.amd64.deb";
-      sha256 = "0kakkl8rmsi2yr3f8vd1kk8vsl9g2ijhqil1cvvbwrhwgi0b7ai7";
+      sha256 = "sha256-Gpr5456MCNpyam3g2qPo7S3aEZFMaUGR8bu7YmRY8xk=";
     };
   }."${stdenv.hostPlatform.system}";
 
