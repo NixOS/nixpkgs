15cd5fc57ead4d312acbc3edad680ebe4fc0706a
Sandro <sandro.jaeckel@gmail.com>
2021-04-18 16:15:48 +0200

    nixos/nginx: set isSystemUser

--- nixos/modules/services/web-servers/nginx/default.nix
+++ nixos/modules/services/web-servers/nginx/default.nix
@@ -879,6 +879,7 @@ in
     users.users = optionalAttrs (cfg.user == "nginx") {
       nginx = {
         group = cfg.group;
+        isSystemUser = true;
         uid = config.ids.uids.nginx;
       };
     };
