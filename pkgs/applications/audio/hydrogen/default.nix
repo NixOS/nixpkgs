acfb7cb31a9adb2f146d95b8af2aae72ba421e0d
Antoine Eiche <lewo@abesis.fr>
2021-04-18 19:57:12 +0200

    hydrogen: set license to gpl2Only

--- pkgs/applications/audio/hydrogen/default.nix
+++ pkgs/applications/audio/hydrogen/default.nix
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Advanced drum machine";
     homepage = "http://www.hydrogen-music.org";
-    license = licenses.gpl2;
+    license = licenses.gpl2Only;
     platforms = platforms.linux;
     maintainers = with maintainers; [ goibhniu orivej ];
   };
