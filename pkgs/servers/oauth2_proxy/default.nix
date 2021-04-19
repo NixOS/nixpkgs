d266f7e938d41a899f3efce8d90464849bd73507
Robert Scott <code@humanleg.org.uk>
2021-04-19 21:42:43 +0100

    oauth2_proxy: add patch for CVE-2021-21291

--- pkgs/servers/oauth2_proxy/default.nix
+++ pkgs/servers/oauth2_proxy/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{ lib, buildGoModule, fetchFromGitHub, fetchpatch }:
 
 buildGoModule rec {
   pname = "oauth2-proxy";
@@ -10,6 +10,14 @@ buildGoModule rec {
     sha256 = "0mbjg0d0w173xpq69frjdvgyx5k74pkrfx3phc3lq8snvhnf1c2n";
     rev = "v${version}";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "CVE-2021-21291.patch";
+      url = "https://github.com/oauth2-proxy/oauth2-proxy/commit/780ae4f3c99b579cb2ea9845121caebb6192f725.patch";
+      sha256 = "066cr6ml69pinw3j9rspjy2h3dvqmf4bvmdxvalb1ws3z0xkn4ag";
+    })
+  ];
 
   vendorSha256 = "1hrk3h729kcc77fq44kiywmyzk5a78v7bm5d2yl76lfxxdcdric7";
 
