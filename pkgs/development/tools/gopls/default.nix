3466a06f29fc9329aba2d80f44377f12357d5620
Diego Pontoriero <74719+diegs@users.noreply.github.com>
2021-04-19 14:06:49 -0700

    gopls: 0.6.2 -> 0.6.10
    Quite a few critical bugfixes and some new features since 0.6.2.
    
    https://github.com/golang/tools/releases/tag/gopls%2Fv0.6.10

--- pkgs/development/tools/gopls/default.nix
+++ pkgs/development/tools/gopls/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "gopls";
-  version = "0.6.2";
+  version = "0.6.10";
 
   src = fetchgit {
     rev = "gopls/v${version}";
     url = "https://go.googlesource.com/tools";
-    sha256 = "0hbfxdsbfz044vw8zp223ni6m7gcwqpff4xpjiqmihhgga5849lf";
+    sha256 = "13mv6rvqlmgn1shx0hnlqxgqiiiz1ij37j30jz1jkr9kcrbxpacr";
   };
 
   modRoot = "gopls";
-  vendorSha256 = "0r9bffgi9ainqrl4kraqy71rgwdfcbqmv3srs12h3xvj0w5ya5rz";
+  vendorSha256 = "01apsvkds8f3m88inb37z4lgalrbjp12xr2jikwx7n10hjddgbqi";
 
   doCheck = false;
 
