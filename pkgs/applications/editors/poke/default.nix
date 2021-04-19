f915ea70a63ade8521d72c337e8aedf8f7b2ee7b
Lancelot SIX <lsix@lancelotsix.com>
2021-04-19 09:38:12 +0100

    poke: 1.1 -> 1.2
    See https://lists.gnu.org/archive/html/info-gnu/2021-04/msg00003.html
    for release information.

--- pkgs/applications/editors/poke/default.nix
+++ pkgs/applications/editors/poke/default.nix
@@ -19,11 +19,11 @@ let
   isCross = stdenv.hostPlatform != stdenv.buildPlatform;
 in stdenv.mkDerivation rec {
   pname = "poke";
-  version = "1.1";
+  version = "1.2";
 
   src = fetchurl {
     url = "mirror://gnu/${pname}/${pname}-${version}.tar.gz";
-    hash = "sha256-zWjfY8dBtBYLEsvqAvJ8RxWCeUZuNEOTDSU1pFLAatY=";
+    hash = "sha256-9hz42ltkwBoTWTc3JarRyiV/NcHJJp5NUN0GZBg932I=";
   };
 
   postPatch = ''
