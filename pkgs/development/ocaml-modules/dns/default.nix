6e748eefd8db6022c9412e0e1cee612504ed5d54
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-04-20 14:05:19 +0200

    ocamlPackages.dns*: 4.6.3 -> 5.0.0

--- pkgs/development/ocaml-modules/dns/default.nix
+++ pkgs/development/ocaml-modules/dns/default.nix
@@ -4,14 +4,14 @@
 
 buildDunePackage rec {
   pname = "dns";
-  version = "4.6.3";
+  version = "5.0.0";
 
   useDune2 = true;
   minimumOCamlVersion = "4.07";
 
   src = fetchurl {
     url = "https://github.com/mirage/ocaml-dns/releases/download/v${version}/dns-v${version}.tbz";
-    sha256 = "0g7xw43jm5hv0w9lsnhhi0w3243mxl615cdsvz29yh39fcqvqsdy";
+    sha256 = "ef93435407955a46e51e3be857a1b00a6765ec3b673d4a859a9536c7e3365111";
   };
 
   propagatedBuildInputs = [ rresult astring fmt logs ptime domain-name gmap cstruct ipaddr lru duration metrics ];
