fb8c527586523cdedc249ab4060809c7164711e3
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-04-20 13:10:36 +0200

    ocamlPackages.irmin: 2.5.2 -> 2.5.3
    https://github.com/mirage/irmin/releases/tag/2.5.3

--- pkgs/development/ocaml-modules/irmin/ppx.nix
+++ pkgs/development/ocaml-modules/irmin/ppx.nix
@@ -2,11 +2,11 @@
 
 buildDunePackage rec {
   pname = "ppx_irmin";
-  version = "2.5.2";
+  version = "2.5.3";
 
   src = fetchurl {
     url = "https://github.com/mirage/irmin/releases/download/${version}/irmin-${version}.tbz";
-    sha256 = "ac8d75144cafdaf4b5e106b540a27338245510b7e33a8c412d393c9d50cae490";
+    sha256 = "2c8ef24cc57379c3a138f121fea350ee7b6077abc22a4fdc6a47d0c81585f3f6";
   };
 
   minimumOCamlVersion = "4.08";
