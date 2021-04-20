eea858ccd4b8fcae067c343b7cdf7031c565696a
Victor SENE <victor@sene.ovh>
2021-04-21 00:04:56 +0200

    [WIP] mautrix-whatsapp: try to update PR ...

--- nixos/modules/services/misc/mautrix-whatsapp.nix
+++ nixos/modules/services/misc/mautrix-whatsapp.nix
@@ -4,69 +4,69 @@ with lib;
 
 let
   cfg = config.services.mautrix-whatsapp;
+  dataDir = "/var/lib/mautrix-whatsapp";
+  format = pkgs.formats.json {};
 
-  configFile = pkgs.runCommand "mautrix-whatsapp" {
-    buildInputs = [ pkgs.mautrix-whatsapp pkgs.remarshal ];
-    preferLocalBuild = true;
-  } ''
-    mkdir -p $out
-    ${pkgs.remarshal}/bin/json2yaml -i ${pkgs.writeText "config.json" (builtins.toJSON cfg.configOptions)} \
-      -o $out/config.yaml
-
-    ${pkgs.mautrix-whatsapp}/bin/mautrix-whatsapp -c $out/config.yaml -g -r $out/registration.yaml
-  '';
-
+  registrationFile = "${dataDir}/whatsapp-registration.yaml";
+  settingsFile = format.generate "config.json" cfg.settings;
 in
 {
   options.services.mautrix-whatsapp = {
     enable = mkEnableOption "Mautrix-whatsapp, a puppeting bridge between Matrix and WhatsApp.";
 
-    configOptions = mkOption {
-      type = types.attrs;
+    settings = mkOption rec {
+      apply = recursiveUpdate default;
+      inherit (format) type;
+
       description = ''
         This options will be transform in YAML configuration file for the bridge
 
         Look <link xlink:href="https://github.com/tulir/mautrix-whatsapp/wiki/Bridge-setup">here</link> for documentation.
       '';
+      default = {
+        homeserver = {
+          domain = config.services.matrix-synapse.server_name;
+        };
+        appservice = rec {
+          address = http://localhost:29318;
+          hostname = "0.0.0.0";
+          port = 29318;
+          database = {
+            type = "sqlite3";
+            uri = "${dataDir}/mautrix-whatsapp.db";
+          };
+          id = "whatsapp";
+          bot = {
+            username = "whatsappbot";
+            displayname = "WhatsApp Bot";
+          };
+          as_token = "";
+          hs_token = "";
+        };
+        bridge = {
+          username_template = "whatsapp_{{.}}";
+          displayname_template = "{{if .Notify}}{{.Notify}}{{else}}{{.Jid}}{{end}}";
+          command_prefix = "!wa";
+          permissions."*" = "relaybot";
+        };
+        relaybot = {
+          enabled = true;
+          management = "!whatsappbot:${toString(config.services.matrix-synapse.server_name)}";
+        };
+        logging = {
+          directory = "${dataDir}/logs";
+          file_name_format = "{{.Date}}-{{.Index}}.log";
+          file_date_format = "2006-01-02";
+          file_mode = 0384;
+          timestamp_format = "Jan _2, 2006 15:04:05";
+          print_level = "info";
+        };
+      };
       example = {
-        configOptions = {
-          homeserver = {
-            address = https://matrix.org;
-            domain = "matrix.org";
-          };
-          appservice = {
-            address = http://localhost:8080;
-            hostname = "0.0.0.0";
-            port = 8080;
-            database = {
-              type = "sqlite3";
-              uri = "/var/lib/mautrix-whatsapp/mautrix-whatsapp.db";
-            };
-            state_store_path = "/var/lib/mautrix-whatsapp/mx-state.json";
-            id = "whatsapp";
-            bot = {
-              username = "whatsappbot";
-              displayname = "WhatsApp bridge bot";
-              avatar = "mxc://maunium.net/NeXNQarUbrlYBiPCpprYsRqr";
-            };
-            as_token = "";
-            hs_token = "";
-          };
-          bridge = {
-            username_template = "whatsapp_{{.}}";
-            displayname_template = "{{if .Notify}}{{.Notify}}{{else}}{{.Jid}}{{end}} (WA)";
-            command_prefix = "!wa";
-            permissions = {
-              "@example:matrix.org" = 100;
-            };
-          };
-          logging = {
-            directory = "/var/lib/mautrix-whatsapp/logs";
-            file_name_format = "{{.Date}}-{{.Index}}.log";
-            file_date_format = "\"2006-01-02\"";
-            file_mode = 384;
-            timestamp_format = "Jan _2, 2006 15:04:05";
-            print_level = "debug";
+        settings = {
+          homeserver.address = https://matrix.org;
+          bridge.permissions = {
+            "@example:matrix.org" = 100;
           };
         };
       };
@@ -76,20 +76,48 @@ in
   config = mkIf cfg.enable {
     systemd.services.mautrix-whatsapp = {
       description = "Mautrix-WhatsApp Service - A WhatsApp bridge for Matrix";
-      after = [ "network.target" "matrix-synapse.service" ];
+      after = [ "network-online.target" "matrix-synapse.service" ];
+      wants = [ "network-online.target" ];
       wantedBy = [ "multi-user.target" ];
 
+      preStart = ''
+        # generate the appservice's registration file if absent
+        if [ ! -f '${registrationFile}' ]; then
+          ${pkgs.mautrix-whatsapp}/bin/mautrix-whatsapp \
+            --generate-registration \
+            --config='${settingsFile}' \
+            --registration='${registrationFile}'
+        fi
+        chmod 640 ${registrationFile}
+      '';
+
       serviceConfig = {
-        DynamicUser = true;
-        StateDirectory = "mautrix-whatsapp";
+        Type = "simple";
+        #DynamicUser = true;
+        PrivateTmp = true;
+        StateDirectory = baseNameOf dataDir;
+        WorkingDirectory = "${dataDir}";
+
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectControlGroups = true;
+        User = "mautrix-whatsapp";
+        Group = "matrix-synapse";
+        SupplementaryGroups = "matrix-synapse";
+        UMask = 0027;
+
         ExecStart = ''
-          ${pkgs.mautrix-whatsapp}/bin/mautrix-whatsapp -c "${configFile}/config.yaml"
+          ${pkgs.mautrix-whatsapp}/bin/mautrix-whatsapp \
+            --config='${settingsFile}'
         '';
-        Restart = "on-failure";
+        Restart = "always";
       };
     };
 
-    services.matrix-synapse.app_service_config_files = [ "${configFile}/registration.yaml" ];
+    users.users.mautrix-whatsapp = {};
+    services.matrix-synapse.app_service_config_files = [ "${registrationFile}" ];
 
   };
 }
