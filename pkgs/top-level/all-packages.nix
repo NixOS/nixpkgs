647960c60bfcefaf0b2199ab1a783a10298354c1
Thomas Gerbet <thomas@gerbet.me>
2021-04-20 08:53:31 +0200

    cosign: 0.2.0 -> 0.3.0
    Release notes:
    https://github.com/sigstore/cosign/releases/tag/v0.3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1274,7 +1274,9 @@ in
 
   corsmisc = callPackage ../tools/security/corsmisc { };
 
-  cosign = callPackage ../tools/security/cosign { };
+  cosign = callPackage ../tools/security/cosign {
+    inherit (darwin.apple_sdk.frameworks) PCSC;
+  };
 
   cozy = callPackage ../applications/audio/cozy-audiobooks { };
 
