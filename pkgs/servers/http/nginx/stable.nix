f4b6314e6041172da9ec011c59e4fa8fe88dc12f
Izorkin <izorkin@elven.pw>
2021-04-20 23:55:56 +0300

    nginxStable: 1.18.0 -> 1.20.0

--- pkgs/servers/http/nginx/stable.nix
+++ pkgs/servers/http/nginx/stable.nix
@@ -1,6 +1,6 @@
 { callPackage, ... } @ args:
 
 callPackage ./generic.nix args {
-  version = "1.18.0";
-  sha256 = "16azscl74ym1far0s0p6xsjin1k1cm4wk80i9x5d74dznmx3wdsc";
+  version = "1.20.0";
+  sha256 = "072dn2qhgx20y4pfa3mi97qszbifhcnwj28ccin4iamwivn93vsl";
 }
