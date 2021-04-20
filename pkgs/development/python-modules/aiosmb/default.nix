b3491eca0ccc4509f7d47d029575c17cb035c554
Fabian Affolter <mail@fabian-affolter.ch>
2021-04-20 22:23:49 +0200

    python3Packages.aiosmb: 0.2.37 -> 0.2.41

--- pkgs/development/python-modules/aiosmb/default.nix
+++ pkgs/development/python-modules/aiosmb/default.nix
@@ -1,6 +1,7 @@
 { lib
 , asysocks
 , buildPythonPackage
+, colorama
 , fetchPypi
 , minikerberos
 , prompt_toolkit
@@ -13,22 +14,23 @@
 
 buildPythonPackage rec {
   pname = "aiosmb";
-  version = "0.2.37";
+  version = "0.2.41";
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0daf1fk7406vpywc0yxv0wzf4nw986js9lc2agfyfxz0q7s29lf0";
+    sha256 = "sha256-hiLLoFswh0rm5f5TsaX+zyRDkOIyzGXVO0M5J5d/gtQ=";
   };
 
   propagatedBuildInputs = [
-    minikerberos
-    winsspi
-    six
     asysocks
-    tqdm
+    colorama
+    minikerberos
     prompt_toolkit
+    six
+    tqdm
     winacl
+    winsspi
   ];
 
   # Project doesn't have tests
