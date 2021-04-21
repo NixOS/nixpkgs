897c8e6982de78ba629a77ca4aedae73e0923f10
Taeer Bar-Yam <taeer@bar-yam.me>
2021-04-21 11:49:18 -0400

    mpg123: fix cross-compile to windows

--- pkgs/applications/audio/mpg123/default.nix
+++ pkgs/applications/audio/mpg123/default.nix
@@ -1,9 +1,9 @@
 { lib, stdenv
 , fetchurl
 , makeWrapper
-
 , alsaLib
 , perl
+, withConplay ? !stdenv.targetPlatform.isWindows
 }:
 
 stdenv.mkDerivation rec {
@@ -14,35 +14,36 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-UCqX4Nk1vn432YczgCHY8wG641wohPKoPVnEtSRm7wY=";
   };
 
-  outputs = [ "out" "conplay" ];
+  outputs = [ "out" ] ++ lib.optionals withConplay [ "conplay" ];
 
-  nativeBuildInputs = [ makeWrapper ];
+  nativeBuildInputs = lib.optionals withConplay [ makeWrapper ];
 
-  buildInputs = [ perl ] ++ lib.optional (!stdenv.isDarwin) alsaLib;
+  buildInputs = lib.optionals withConplay [ perl ]
+    ++ lib.optionals (!stdenv.isDarwin && !stdenv.targetPlatform.isWindows) [ alsaLib ];
 
   configureFlags = lib.optional
     (stdenv.hostPlatform ? mpg123)
     "--with-cpu=${stdenv.hostPlatform.mpg123.cpu}";
 
-  postInstall = ''
+  postInstall = lib.optionalString withConplay ''
     mkdir -p $conplay/bin
     mv scripts/conplay $conplay/bin/
   '';
 
-  preFixup = ''
+  preFixup = lib.optionalString withConplay ''
     patchShebangs $conplay/bin/conplay
   '';
 
-  postFixup = ''
+  postFixup = lib.optionalString withConplay ''
     wrapProgram $conplay/bin/conplay \
       --prefix PATH : $out/bin
   '';
 
-  meta = {
+  meta = with lib; {
     description = "Fast console MPEG Audio Player and decoder library";
-    homepage = "http://mpg123.org";
-    license = lib.licenses.lgpl21;
-    maintainers = [ lib.maintainers.ftrvxmtrx ];
-    platforms = lib.platforms.unix;
+    homepage = "https://mpg123.org";
+    license = licenses.lgpl21;
+    maintainers = [ maintainers.ftrvxmtrx ];
+    platforms = platforms.all;
   };
 }
