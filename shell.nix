b9bd4496673bdbf612ad632a7c2d2b56764703ee
David Arnold <dar@xoe.solutions>
2021-04-21 17:53:44 -0500

    devhsell: pin nixpkgs version
    it would be too costly for users to rebuild the devshell on every
    change in nixpkgs. Also, an unpinned nixpkgs version might suddenly
    become broken if nixpkgs changes in ways that devshell is not yet
    compatible with.

--- shell.nix
+++ shell.nix
@@ -1,9 +1,14 @@
 { system ? builtins.currentSystem }:
 let
   # nixpkgs / devshell is only used for development. Don't add it to the flake.lock.
+  nixpkgsGitRev = "5268ee2ebacbc73875be42d71e60c2b5c1b5a1c7";
   devshellGitRev = "709fe4d04a9101c9d224ad83f73416dce71baf21";
 
-  nixpkgsSrc = ./.;
+  nixpkgsSrc = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgsGitRev}.tar.gz";
+    sha256 = "080fvmg0i6z01h6adddfrjp1bbbjhhqk32ks6ch9gv689645ccfq";
+  };
+
 
   devshellSrc = fetchTarball {
     url = "https://github.com/numtide/devshell/archive/${devshellGitRev}.tar.gz";
