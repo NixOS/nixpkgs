d786f0d171516c284f208b251bde18d537b4adc6
JordiPlayGames <82941543+JordiPlayGames@users.noreply.github.com>
2021-04-21 12:13:39 +0200

    Add apt to all-packages.nix.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -758,6 +758,8 @@ in
   agda-pkg = callPackage ../development/tools/agda-pkg { };
 
   agi = callPackage ../tools/graphics/agi { };
+  
+  apt = callPackage ../tools/package-management/apt { };
 
   agrep = callPackage ../tools/text/agrep { };
 
