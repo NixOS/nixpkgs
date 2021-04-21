fbddb37ac7628b3b1bf40d50c92beb1dfd79ea57
Jonathan Ringer <jonringer117@gmail.com>
2021-04-21 12:01:01 -0700

    python3Packages.b2sdk: 1.4.0 -> 1.6.0, fix build

--- pkgs/development/python-modules/b2sdk/default.nix
+++ pkgs/development/python-modules/b2sdk/default.nix
@@ -1,20 +1,27 @@
-{ lib, buildPythonPackage, fetchPypi, setuptools_scm, isPy27, pytestCheckHook
+{ lib, buildPythonPackage, fetchPypi, setuptools-scm, isPy27, pytestCheckHook
 , requests, arrow, logfury, tqdm }:
 
 buildPythonPackage rec {
   pname = "b2sdk";
-  version = "1.4.0";
+  version = "1.6.0";
 
   disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "fb82cbaef5dd7499b62622010fc8e328944ca8cbdd00b485530ab6600de1129d";
+    sha256 = "sha256-6fjreuMUC056ljddfAidfBbJkvEDndB/dIkx1bF7efs=";
   };
+
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace 'setuptools_scm<6.0' 'setuptools_scm'
+    substituteInPlace requirements.txt \
+      --replace 'arrow>=0.8.0,<1.0.0' 'arrow'
+  '';
 
   pythonImportsCheck = [ "b2sdk" ];
 
-  nativebuildInputs = [ setuptools_scm ];
+  nativeBuildInputs = [ setuptools-scm ];
   propagatedBuildInputs = [ requests arrow logfury tqdm ];
 
   # requires unpackaged dependencies like liccheck
