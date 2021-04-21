c88a223e38d3b36f0d4d12beab10f98282ed449a
Michael Weiss <dev.primeos@gmail.com>
2021-04-21 18:54:54 +0200

    nvme-cli: Remove myself as maintainer

--- pkgs/os-specific/linux/nvme-cli/default.nix
+++ pkgs/os-specific/linux/nvme-cli/default.nix
@@ -35,6 +35,6 @@ stdenv.mkDerivation rec {
     '';
     license = licenses.gpl2Plus;
     platforms = platforms.linux;
-    maintainers = with maintainers; [ primeos tavyc ];
+    maintainers = with maintainers; [ tavyc ];
   };
 }
