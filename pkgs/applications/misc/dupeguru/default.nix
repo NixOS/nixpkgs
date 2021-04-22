97b15bee8e6963bf7e04860c32cd69074e0781f5
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2021-04-22 21:10:28 +0200

    dupeguru: 4.0.4 -> 4.1.1

--- pkgs/applications/misc/dupeguru/default.nix
+++ pkgs/applications/misc/dupeguru/default.nix
@@ -1,8 +1,24 @@
-{lib, python3Packages, fetchpatch, gettext, qt5, fetchFromGitHub}:
+{ lib
+, buildPythonApplication
+, fetchFromGitHub
+, wrapQtAppsHook
+, gettext
+, hsaudiotag3k
+, polib
+, pyqt5
+, send2trash
+, sip
+, sphinx
+, black
+, distro
+, flake8
+, pytest
+, tox
+}:
 
-python3Packages.buildPythonApplication rec {
+buildPythonApplication rec {
   pname = "dupeguru";
-  version = "4.0.4";
+  version = "4.1.1";
 
   format = "other";
 
@@ -10,44 +26,31 @@ python3Packages.buildPythonApplication rec {
     owner = "arsenetar";
     repo = "dupeguru";
     rev = version;
-    sha256 = "0ma4f1c6vmpz8gi4sdy43x1ik7wh42wayvk1iq520d3i714kfcpy";
+    sha256 = "1a34kkw2xyfdirv0nw5w6v7s2db4cfn17ksnnblkl7ahndr6hlnj";
     fetchSubmodules = true;
   };
 
-  patches = [
-    # already merged to master, remove next version bump
-    (fetchpatch {
-      name = "remove-m-from-so-var.patch";
-      url = "https://github.com/arsenetar/dupeguru/commit/bd0f53bcbe463c48fe141b73af13542da36d82ba.patch";
-      sha256 = "07iisz8kcr7v8lb21inzj1avlpfhh9k8wcivbd33w49cr3mmnr26";
-    })
-  ];
+  patches = [ ./tox-passenv-PYTHONPATH.patch ];
 
   nativeBuildInputs = [
     gettext
-    python3Packages.pyqt5
-    qt5.wrapQtAppsHook
+    pyqt5
+    wrapQtAppsHook
   ];
 
-  pythonPath = with python3Packages; [
+  propagatedBuildInputs = [
+    hsaudiotag3k
+    polib
     pyqt5
     send2trash
+    sip
     sphinx
-    polib
-    hsaudiotag3k
   ];
 
   makeFlags = [
     "PREFIX=${placeholder "out"}"
     "NO_VENV=1"
   ];
-
-  # TODO: package pytest-monkeyplus for running tests
-  # https://github.com/NixOS/nixpkgs/pull/75054/files#r357690123
-  doCheck = false;
-
-  # Avoid double wrapping Python programs.
-  dontWrapQtApps = true;
 
   # TODO: A bug in python wrapper
   # see https://github.com/NixOS/nixpkgs/pull/75054#discussion_r357656916
@@ -58,14 +61,29 @@ python3Packages.buildPythonApplication rec {
   # Executable in $out/bin is a symlink to $out/share/dupeguru/run.py
   # so wrapPythonPrograms hook does not handle it automatically.
   postFixup = ''
-    wrapPythonProgramsIn "$out/share/dupeguru" "$out $pythonPath"
+    wrapPythonProgramsIn "$out/share/dupeguru" "$out $propagatedBuildInputs"
+  '';
+
+  doCheck = true;
+
+  checkInputs = [
+    black
+    distro
+    flake8
+    pytest
+    send2trash
+    tox
+  ];
+
+  checkPhase = ''
+    tox
   '';
 
   meta = with lib; {
     description = "GUI tool to find duplicate files in a system";
-    homepage = "https://github.com/arsenetar/dupeguru";
-    license = licenses.bsd3;
+    homepage = "https://dupeguru.voltaicideas.net/";
+    license = licenses.gpl3Plus;
     platforms = platforms.linux;
-    maintainers = [ maintainers.novoxudonoser ];
+    maintainers = with maintainers; [ novoxudonoser shamilton ];
   };
 }
