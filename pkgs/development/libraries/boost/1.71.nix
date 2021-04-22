f76f6ffbac85a1b8a08d5453144a1e6879e0aad7
Hunter Jones <hjones2199@gmail.com>
2021-04-22 14:38:30 -0500

    boost: generalize version numbers

--- pkgs/development/libraries/boost/1.71.nix
+++ pkgs/development/libraries/boost/1.71.nix
@@ -4,10 +4,9 @@ callPackage ./generic.nix (args // rec {
   version = "1.71.0";
 
   src = fetchurl {
-    #url = "mirror://sourceforge/boost/boost_1_71_0.tar.bz2";
     urls = [
-      "mirror://sourceforge/boost/boost_1_71_0.tar.bz2"
-      "https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.tar.bz2"
+      "mirror://sourceforge/boost/boost_${builtins.replaceStrings ["."] ["_"] version}.tar.bz2"
+      "https://dl.bintray.com/boostorg/release/${version}/source/boost_${builtins.replaceStrings ["."] ["_"] version}.tar.bz2"
     ];
     # SHA256 from http://www.boost.org/users/history/version_1_71_0.html
     sha256 = "d73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee";
