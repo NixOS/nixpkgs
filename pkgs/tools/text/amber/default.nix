a754f10fa081a25e8dabbb3d021e8403a97d79ba
Sandro <sandro.jaeckel@gmail.com>
2021-04-22 02:36:33 +0200

    Update pkgs/tools/text/amber/default.nix

--- pkgs/tools/text/amber/default.nix
+++ pkgs/tools/text/amber/default.nix
@@ -15,7 +15,7 @@ rustPlatform.buildRustPackage rec {
 
   cargoSha256 = "sha256-opRinhTmhZxpAwHNiVOLXL8boQf09Y1NXrWQ6HWQYQ0=";
 
-  buildInputs = lib.optional stdenv.isDarwin [ libiconv Security ];
+  buildInputs = lib.optionals stdenv.isDarwin [ libiconv Security ];
 
   meta = with lib; {
     description = "A code search-and-replace tool";
