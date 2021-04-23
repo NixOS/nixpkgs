562a927b7e114691039ce1b4e81e10a3dfb230ce
piegames <git@piegames.de>
2021-04-24 01:09:14 +0200

    fixup! python3Packages.mung: init at unstable-2021-04-23

--- pkgs/development/python-modules/mung/default.nix
+++ pkgs/development/python-modules/mung/default.nix
@@ -1,29 +1,25 @@
 { lib
-, fetchgit
-# , fetchPypi
 , buildPythonPackage
-, pytest
+, fetchFromGitHub
 , lxml
+, matplotlib
+, midiutil
+, numpy
+, pytestCheckHook
 , scikitimage
 , scikitlearn
-, matplotlib
-, numpy
-, midiutil
 }:
 
 buildPythonPackage rec {
   pname = "mung";
   version = "unstable-2021-04-23";
 
-  src = fetchgit {
-    url = "https://github.com/OMR-Research/mung.git";
+  src = fetchFromGitHub {
+    owner = "OMR-Research";
+    repo = "mung";
     rev = "8fc816177e15889bd397eb20725441dbe21b8620";
     sha256 = "1sann02126aas61f67kn4038raaj9arwq302nmi6vihsawy40sm5";
   };
-#   src = fetchPypi {
-#     inherit pname version;
-#     sha256 = "17mjkiizfz3441h3xksw4bdaxv9962qsbdqip3vw39jglm78cpv1";
-#   };
 
   propagatedBuildInputs = [
     lxml
@@ -35,18 +31,8 @@ buildPythonPackage rec {
   ];
 
   checkInputs = [
-    pytest
+    pytestCheckHook
   ];
-
-  checkPhase = ''
-    pytest test/
-  '';
-
-  # The scripts in there don't look like they were meant to be packaged
-  removeBin = ''
-    rm -r $out/bin
-  '';
-  postPhases = ["removeBin"];
 
   meta = with lib; {
     description = "Music Notation Graph: a data model for optical music recognition";
