063e5abdd268cf993df7b8bda1d8dda2676ca345
Florian Franzen <Florian.Franzen@gmail.com>
2021-04-23 11:58:54 +0200

    sphinx-serve: init from python3Packages.sphinx-serve

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17474,6 +17474,8 @@ in
     fftw = fftwFloat;
   };
 
+  sphinx-serve = with python3Packages; toPythonApplication sphinx-serve;
+
   sphinxbase = callPackage ../development/libraries/sphinxbase { };
 
   sphinxsearch = callPackage ../servers/search/sphinxsearch { };
