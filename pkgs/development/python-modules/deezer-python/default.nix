59740b02a7a61c49c2e1a65c927148b8480b4aaf
Sandro <sandro.jaeckel@gmail.com>
2021-04-23 03:19:31 +0200

    Apply suggestions from code review

--- pkgs/development/python-modules/deezer-python/default.nix
+++ pkgs/development/python-modules/deezer-python/default.nix
@@ -13,6 +13,8 @@
 buildPythonPackage rec {
   pname = "deezer-python";
   version = "2.2.2";
+  disabled = pythonOlder "3.6";
+  format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "browniebroke";
@@ -20,10 +22,6 @@ buildPythonPackage rec {
     rev = "v${version}";
     sha256 = "1l8l4lxlqsj921gk1mxcilp11jx31addiyd9pk604aldgqma709y";
   };
-
-  disabled = pythonOlder "3.6";
-
-  format = "pyproject";
 
   nativeBuildInputs = [
     poetry-core
