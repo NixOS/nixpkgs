086ab5a8809763ca918ed012c52dee5831de95e2
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-04-22 23:57:11 -0300

    bloomrpc: init at 1.5.2

---
+++ pkgs/development/web/bloomrpc/default.nix
@@ -0,0 +1,92 @@
+{ lib, stdenv, makeWrapper, fetchurl, dpkg, alsaLib, atk, cairo, cups, dbus, expat
+, fontconfig, freetype, gdk-pixbuf, glib, gnome2, pango, nspr, nss, gtk3, gtk2
+, at-spi2-atk, gsettings-desktop-schemas, gobject-introspection, wrapGAppsHook
+, libX11, libXScrnSaver, libXcomposite, libXcursor, libXdamage, libXext
+, libXfixes, libXi, libXrandr, libXrender, libXtst, libxcb, nghttp2
+, libudev0-shim, glibc, curl, openssl, autoPatchelfHook }:
+
+let
+  runtimeLibs = lib.makeLibraryPath [
+    curl
+    glibc
+    libudev0-shim
+    nghttp2
+    openssl
+    stdenv.cc.cc
+  ];
+in stdenv.mkDerivation rec {
+  pname = "bloomrpc";
+  version = "1.5.2";
+
+  src = fetchurl {
+    url =
+      "https://github.com/uw-labs/bloomrpc/releases/download/${version}/bloomrpc_${version}_amd64.deb";
+    sha256 = "9da17bcd3ec579309661658849652e2bf8e0db6961716e94af299ee41f0fd778";
+  };
+
+  nativeBuildInputs =
+    [ autoPatchelfHook dpkg makeWrapper gobject-introspection wrapGAppsHook ];
+
+  buildInputs = [
+    alsaLib
+    at-spi2-atk
+    atk
+    cairo
+    cups
+    dbus
+    expat
+    fontconfig
+    freetype
+    gdk-pixbuf
+    glib
+    gnome2.GConf
+    pango
+    gtk2
+    gtk3
+    gsettings-desktop-schemas
+    libX11
+    libXScrnSaver
+    libXcomposite
+    libXcursor
+    libXdamage
+    libXext
+    libXfixes
+    libXi
+    libXrandr
+    libXrender
+    libXtst
+    libxcb
+    nspr
+    nss
+    stdenv.cc.cc
+  ];
+
+  dontBuild = true;
+  dontConfigure = true;
+
+  unpackPhase = "dpkg-deb -x $src .";
+
+  installPhase = ''
+    mkdir -p $out/share/bloomrpc $out/lib $out/bin
+
+    mv usr/share/* $out/share/
+    mv opt/BloomRPC/* $out/share/bloomrpc/
+    mv $out/share/bloomrpc/*.so $out/lib/
+
+    ln -s $out/share/bloomrpc/bloomrpc $out/bin/bloomrpc
+    sed -i 's|\/opt\/BloomRPC|'$out'/bin|g' $out/share/applications/bloomrpc.desktop
+  '';
+
+  preFixup = ''
+    wrapProgram "$out/bin/bloomrpc" --prefix LD_LIBRARY_PATH : ${runtimeLibs}
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/uw-labs/bloomrpc";
+    description = "GUI Client for GRPC Services";
+    license = licenses.lgpl3Only;
+    platforms = [ "x86_64-linux" ];
+    maintainers = with maintainers; [ Mdsp9070 ];
+  };
+
+}
