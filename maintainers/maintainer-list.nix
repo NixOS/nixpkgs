7e066a86cd090866f1e1ccd4b3e8f58e34cef8cd
French-isotope <83166352+French-isotope@users.noreply.github.com>
2021-04-25 22:55:44 +0200

    Update maintainer-list.nix
    Adding "french-isotope" as maintainer.

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3441,6 +3441,12 @@
     githubId = 13279982;
     name = "freezeboy";
   };
+  French-isotope = {
+    email = "french.isotope@gmail.com";
+    github = "French-isotope";
+    githubId = 83166352;
+    name = "French Isotope";
+  };
   Fresheyeball = {
     email = "fresheyeball@gmail.com";
     github = "Fresheyeball";
