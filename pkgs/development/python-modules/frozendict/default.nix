f532d682e48dffe0b2485eb8a5ecf40cb9dbd799
ajs124 <git@ajs124.de>
2021-04-26 14:30:59 +0200

    python3.pkgs.frozendict: 1.2 -> 2.0.3

--- pkgs/development/python-modules/frozendict/default.nix
+++ pkgs/development/python-modules/frozendict/default.nix
@@ -1,19 +1,23 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib, buildPythonPackage, fetchFromGitHub, isPy3k }:
 
 buildPythonPackage rec {
   pname = "frozendict";
-  version = "1.2";
+  version = "2.0.3";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "0ibf1wipidz57giy53dh7mh68f2hz38x8f4wdq88mvxj5pr7jhbp";
+  src = fetchFromGitHub {
+    owner = "Marco-Sulla";
+    repo = "python-${pname}";
+    rev = "v${version}";
+    sha256 = "0qvh36pdcsv0w963fwb1f6zlzwngjxyy6zd3n5925cgxaqxn54gj";
   };
+
+  disabled = !isPy3k;
 
   # frozendict does not come with tests
   doCheck = false;
 
   meta = with lib; {
-    homepage = "https://github.com/slezica/python-frozendict";
+    homepage = "https://github.com/Marco-Sulla/python-frozendict";
     description = "An immutable dictionary";
     license = licenses.mit;
   };
