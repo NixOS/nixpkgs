99de33bb1d8724c5a60369c4605b4f517bb78fc9
Eduardo Sánchez Muñoz <esm@eduardosm.net>
2021-04-26 12:48:10 +0200

    remmina: fix license

--- pkgs/applications/networking/remote/remmina/default.nix
+++ pkgs/applications/networking/remote/remmina/default.nix
@@ -51,7 +51,7 @@ stdenv.mkDerivation rec {
   '';
 
   meta = {
-    license = licenses.gpl2;
+    license = licenses.gpl2Plus;
     homepage = "https://gitlab.com/Remmina/Remmina";
     description = "Remote desktop client written in GTK";
     maintainers = with maintainers; [ melsigl ryantm ];
