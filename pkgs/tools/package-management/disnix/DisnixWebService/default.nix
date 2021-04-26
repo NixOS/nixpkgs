72b3c05545f87dc65a9cdea432f54393ddb01c5c
Robin Gloster <mail@glob.in>
2021-04-26 16:56:54 -0500

    DisnixWebService: structured-attrs build fix

--- pkgs/tools/package-management/disnix/DisnixWebService/default.nix
+++ pkgs/tools/package-management/disnix/DisnixWebService/default.nix
@@ -7,10 +7,10 @@ stdenv.mkDerivation {
     sha256 = "0m451msd127ay09yb8rbflg68szm8s4hh65j99f7s3mz375vc114";
   };
   buildInputs = [ apacheAnt jdk ];
-  PREFIX = "\${env.out}";
-  AXIS2_LIB = "${axis2}/lib";
-  AXIS2_WEBAPP = "${axis2}/webapps/axis2";
-  DBUS_JAVA_LIB = "${dbus_java}/share/java";
+  env.PREFIX = "\${env.out}";
+  env.AXIS2_LIB = "${axis2}/lib";
+  env.AXIS2_WEBAPP = "${axis2}/webapps/axis2";
+  env.DBUS_JAVA_LIB = "${dbus_java}/share/java";
   prePatch = ''
     sed -i -e "s|#JAVA_HOME=|JAVA_HOME=${jdk}|" \
        -e "s|#AXIS2_LIB=|AXIS2_LIB=${axis2}/lib|" \
