b9e3b986cd82e6c651df26e0c370dc914492c0e5
Tim Steinbach <tim@nequissimus.com>
2021-04-26 10:56:50 -0400

    linux-rt_5_4: 5.4.106-rt54 -> 5.4.109-rt56

--- pkgs/os-specific/linux/kernel/linux-rt-5.4.nix
+++ pkgs/os-specific/linux/kernel/linux-rt-5.4.nix
@@ -6,7 +6,7 @@
 , ... } @ args:
 
 let
-  version = "5.4.106-rt54"; # updated by ./update-rt.sh
+  version = "5.4.109-rt56"; # updated by ./update-rt.sh
   branch = lib.versions.majorMinor version;
   kversion = builtins.elemAt (lib.splitString "-" version) 0;
 in buildLinux (args // {
@@ -14,14 +14,14 @@ in buildLinux (args // {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${kversion}.tar.xz";
-    sha256 = "1ny8b69ngydh0iw53jwlmqlgv31wjhkybkgnqi5kv0n174n3p1yc";
+    sha256 = "1vmpc6yrr2zm4m3naflwik5111jr8hy0mnyddwk31l0p4xbg8smc";
   };
 
   kernelPatches = let rt-patch = {
     name = "rt";
     patch = fetchurl {
       url = "mirror://kernel/linux/kernel/projects/rt/${branch}/older/patch-${version}.patch.xz";
-      sha256 = "0xwbpn1k1b4bxq15sw7gicrzkfg32nkja308a5pcwx1ihv9khchf";
+      sha256 = "08cg8b7mwihs8zgdh0jwi8hrn3hnf9j0jyplsyc7644wd6mqby4a";
     };
   }; in [ rt-patch ] ++ lib.remove rt-patch kernelPatches;
 
