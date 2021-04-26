d274222c3b3861a6af79c058ff33f92d342ab52a
Maëlys Bras de fer <mae.bdf@outlook.com>
2021-04-26 17:46:02 +0200

    fcitx5-configtool: 5.0.2 -> 5.0.4
    Fixes fcitx/fcitx5-configtool#38 that prevented compiling.

--- pkgs/tools/inputmethods/fcitx5/fcitx5-configtool.nix
+++ pkgs/tools/inputmethods/fcitx5/fcitx5-configtool.nix
@@ -19,13 +19,13 @@
 
 mkDerivation rec {
   pname = "fcitx5-configtool";
-  version = "5.0.2";
+  version = "5.0.4";
 
   src = fetchFromGitHub {
     owner = "fcitx";
     repo = "fcitx5-configtool";
     rev = version;
-    sha256 = "sha256-kw0KIbS5SVMf6kR/9xsYiChHXQBM0enSVXyh0QfiiPY=";
+    sha256 = "sha256-UO3Ob+bFQ/2Vqb8YpD9tfmfZt5YLUyoqcbtsHLaVOzE=";
   };
 
   cmakeFlags = [
