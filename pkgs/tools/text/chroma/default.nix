85b653c9d4a54882907c8a4e31664ba523eb7e59
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-04-26 14:49:49 +0200

    chroma: 0.8.2 -> 0.9.1
    https://github.com/alecthomas/chroma/releases/tag/v0.9.0
    https://github.com/alecthomas/chroma/releases/tag/v0.9.1

--- pkgs/tools/text/chroma/default.nix
+++ pkgs/tools/text/chroma/default.nix
@@ -2,15 +2,14 @@
 
 buildGoModule rec {
   pname = "chroma";
-  version = "0.8.2";
+  version = "0.9.1";
 
   src = fetchFromGitHub {
     owner  = "alecthomas";
     repo   = pname;
     rev    = "v${version}";
-    sha256 = "0vzxd0jvjaakwjvkkkjppakjb00z44k7gb5ng1i4924agh24n5ka";
+    sha256 = "0zzk4wcjgxa9lsx8kwpmxvcw67f2fr7ai37jxmdahnws0ai2c2f7";
     leaveDotGit = true;
-    fetchSubmodules = true;
   };
 
   nativeBuildInputs = [ git ];
@@ -27,7 +26,7 @@ buildGoModule rec {
       --replace 'date    = "?"' "date = \"$date\""
   '';
 
-  vendorSha256 = "16cnc4scgkx8jan81ymha2q1kidm6hzsnip5mmgbxpqcc2h7hv9m";
+  vendorSha256 = "0y8mp08zccn9qxrsj9j7vambz8dwzsxbbgrlppzam53rg8rpxhrg";
 
   subPackages = [ "cmd/chroma" ];
 
