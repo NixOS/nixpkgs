1af6da6ae3664661cb27c604762bbf47892c1027
R. RyanTM <ryantm-bot@ryantm.com>
2021-04-26 00:47:41 +0000

    gruvbox-dark-gtk: 1.0.1 -> 1.0.2

--- pkgs/data/themes/gruvbox-dark-gtk/default.nix
+++ pkgs/data/themes/gruvbox-dark-gtk/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gruvbox-dark-gtk";
-  version = "1.0.1";
+  version = "1.0.2";
 
   src = fetchFromGitHub {
     owner = "jmattheis";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1wf4ybnjdp2kbbvz7pmqdnzk94axaqx5ws18f34hrg4y267n0w4g";
+    sha256 = "sha256-C681o89MTGNp1l3DLQsRpH9HQdmdCXZzk0F0rNhcyL4=";
   };
 
   installPhase = ''
