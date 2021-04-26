f5ef9d08b6bce617ea8a222086596286ad559e98
Robert Hensing <robert@roberthensing.nl>
2021-04-26 14:49:01 +0200

    hello: Reintroduce rec keyword

--- pkgs/applications/misc/hello/default.nix
+++ pkgs/applications/misc/hello/default.nix
@@ -7,12 +7,12 @@
 , hello
 }:
 
-stdenv.mkDerivation (self: {
+stdenv.mkDerivation (self: rec {
   pname = "hello";
   version = "2.10";
 
   src = fetchurl {
-    url = "mirror://gnu/hello/${self.pname}-${self.version}.tar.gz";
+    url = "mirror://gnu/hello/${pname}-${version}.tar.gz";
     sha256 = "0ssi1wpaf7plaswqqjwigppsg5fyh99vdlb9kzl7c9lng89ndq1i";
   };
 
@@ -35,7 +35,7 @@ stdenv.mkDerivation (self: {
       It is fully customizable.
     '';
     homepage = "https://www.gnu.org/software/hello/manual/";
-    changelog = "https://git.savannah.gnu.org/cgit/hello.git/plain/NEWS?h=v${self.version}";
+    changelog = "https://git.savannah.gnu.org/cgit/hello.git/plain/NEWS?h=v${version}";
     license = licenses.gpl3Plus;
     maintainers = [ maintainers.eelco ];
     platforms = platforms.all;
