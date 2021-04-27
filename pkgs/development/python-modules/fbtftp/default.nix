cc448f6a0ce6b89286e98a2e024734a77cf4a9c8
French-isotope <83166352+French-isotope@users.noreply.github.com>
2021-04-27 17:26:19 +0200

    Correcting trailing whitespace on default.nix

--- pkgs/development/python-modules/fbtftp/default.nix
+++ pkgs/development/python-modules/fbtftp/default.nix
@@ -2,14 +2,14 @@
 buildPythonPackage rec {
   pname = "fbtftp";
   version = "0.4";
-  
+
   doCheck = false;
-  
+
   src = fetchPypi {
     inherit pname version;
     sha256 = "0qbfwk2qbbv8z0mf3hyykkp32lisv7g64w26ki7rf8nifj2lgnn6";
   };
-  
+
   meta = {
     description = "Facebook's implementation of a dynamic TFTP server framework";
     homepage = "https://github.com/facebook/fbtftp";
