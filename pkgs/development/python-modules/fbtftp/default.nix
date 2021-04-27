1c93672d651dfd60003c97b883c936042029066d
French-isotope <83166352+French-isotope@users.noreply.github.com>
2021-04-27 15:47:20 +0200

    Create default.nix and folder for fbtftp
    Add the package fbtftp with buildPythonPackage

---
+++ pkgs/development/python-modules/fbtftp/default.nix
@@ -0,0 +1,19 @@
+{ lib, buildPythonPackage, fetchPypi }:
+buildPythonPackage rec {
+  pname = "fbtftp";
+  version = "0.4";
+  
+  doCheck = false;
+  
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "0qbfwk2qbbv8z0mf3hyykkp32lisv7g64w26ki7rf8nifj2lgnn6";
+  };
+  
+  meta = {
+    description = "Facebook's implementation of a dynamic TFTP server framework";
+    homepage = "https://github.com/facebook/fbtftp";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ guibou french-isotope ];
+  };
+}
