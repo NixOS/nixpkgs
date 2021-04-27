45995d8d9e4c9b1149c8a00137f0cb03ce876a3e
R. RyanTM <ryantm-bot@ryantm.com>
2021-04-27 17:45:54 +0000

    cargo-outdated: 0.9.14 -> 0.9.15

--- pkgs/tools/package-management/cargo-outdated/default.nix
+++ pkgs/tools/package-management/cargo-outdated/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-outdated";
-  version = "0.9.14";
+  version = "0.9.15";
 
   src = fetchFromGitHub {
     owner = "kbknapp";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-80H0yblEcxP6TTil0dJPZhvMivDLuyvoV0Rmcrykgjs=";
+    sha256 = "sha256-Cd0QWFeAAHSkeCVQvb+Fsg5nBoutV1k1kQpMkWpci2E=";
   };
 
-  cargoSha256 = "sha256-RACdzaCWfm5rrIX0wrvKSmhLQt1a+2MQqrjTx+etpGo=";
+  cargoSha256 = "sha256-VngJMDVKIV8+ODHia2U2gKKPKskyKiuKhSnO6NJsJHI=";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ]
