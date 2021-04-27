ba31f056f5399586c4ea406490e14eb2d08cfadc
Robin Gloster <mail@glob.in>
2021-04-27 18:53:45 -0500

    azureus: structured-attrs build fix

--- pkgs/tools/networking/p2p/azureus/default.nix
+++ pkgs/tools/networking/p2p/azureus/default.nix
@@ -8,7 +8,27 @@ stdenv.mkDerivation {
     sha256 = "1hwrh3n0b0jbpsdk15zrs7pw175418phhmg6pn4xi1bvilxq1wrd";
   };
 
-  inherit jdk swt;
+  dontUnpack = true;
+
+  installPhase = ''
+    mkdir -p $out/jars
+    cp $src $out/jars/azureus.jar
+
+    mkdir -p $out/bin
+    cat > $out/bin/azureus <<EOF
+    #! $SHELL -e
+    azureusHome=$out
+    if test -n "\$HOME"; then
+        azureusHome=\$HOME/.Azureus
+    fi
+    exec ${jdk}/bin/java -Xms16m -Xmx128m \
+      -cp $out/jars/azureus.jar:${swt}/jars/swt.jar \
+      -Djava.library.path=${swt}/lib \
+      -Dazureus.install.path=\$azureusHome \
+      org.gudy.azureus2.ui.swt.Main
+    EOF
+    chmod +x $out/bin/azureus
+  '';
 
   meta = {
     platforms = lib.platforms.linux;
