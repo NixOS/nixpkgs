3fe839e78cfb63661cf00cda0407a11ab8c05b76
Evils <evils.devils@protonmail.com>
2021-04-27 18:20:28 +0200

    kicad: 5.1.9 -> 5.1.10

--- pkgs/applications/science/electronics/kicad/versions.nix
+++ pkgs/applications/science/electronics/kicad/versions.nix
@@ -3,17 +3,17 @@
 {
   "kicad" = {
     kicadVersion = {
-      version =			"5.1.9";
+      version =			"5.1.10";
       src = {
-        rev =			"73d0e3b20dec05c4350efa5b69916eb29a7bfcb5";
-        sha256 =		"1cqh3bc9y140hbryfk9qavs2y3lj5sm9q0qjxcf4mm472afzckky";
+        rev =			"88a1d61d58fdd62149bd1e00984e01540148ca1b";
+        sha256 =		"10ix560bqy0lprnik1bprxw9ix4g8w2ipvyikx551ak9ryvgwjcc";
       };
     };
     libVersion = {
-      version =			"5.1.9";
+      version =			"5.1.10";
       libSources = {
-        i18n.rev =		"04f3231f60d55400cb81564b2cd465a57d5192d5";
-        i18n.sha256 =		"04jq1dcag6i2ljjfqrib65mn4wg4c4nmi7i946l3bywc0rkqsx1f";
+        i18n.rev =		"f081afe79be4660d5c49a9d674e3cb666d76d4d0";
+        i18n.sha256 =		"0y51l0r62cnxkvpc21732p3cx7pjvaqjih8193502hlv9kv1j9p6";
         symbols.rev =		"6dec5004b6a2679c19d4857bda2f90c5ab3a5726";
         symbols.sha256 =	"0n25rq32jwyigfw26faqraillwv6zbi2ywy26dkz5zqlf5xp56ad";
         templates.rev =		"1ccbaf3704e8ff4030d0915f71e051af621ef7d7";
