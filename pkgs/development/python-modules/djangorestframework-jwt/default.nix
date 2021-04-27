1c4234f3809a04df101d9e5ea149b013787f4b59
critbase <12738442+critbase@users.noreply.github.com>
2021-04-27 19:40:29 -0400

    djangorestframework-jwt: fix building error
    This package depends on an older version of 'pyjwt' than is packaged in
    nixpkgs.

--- pkgs/development/python-modules/djangorestframework-jwt/default.nix
+++ pkgs/development/python-modules/djangorestframework-jwt/default.nix
@@ -6,6 +6,18 @@
 , buildPythonPackage
 }:
 
+let
+
+  pyjwt' = pyjwt.overridePythonAttrs (oldAttrs: rec {
+    version = "1.7.1";
+    src = oldAttrs.src.override {
+      inherit version;
+      sha256 = "jVmpdvt3Pz5qOchWNjV8Tw4kJwc5TK2t2YFPXLqiDpY=";
+    };
+  });
+
+in
+
 buildPythonPackage rec {
   pname = "djangorestframework-jwt";
   version = "1.11.0";
@@ -15,7 +27,7 @@ buildPythonPackage rec {
     sha256 = "19rng6v1sw14mbjp5cplnrgxjnhlj8faalfw02iihi9s5w1k7zjy";
   };
 
-  propagatedBuildInputs = [ pyjwt django djangorestframework ];
+  propagatedBuildInputs = [ pyjwt' django djangorestframework ];
 
   # ./runtests.py fails because the project must be tested against a django
   # installation, there are missing database tables for User, that don't exist.
