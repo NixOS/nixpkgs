a482410e0baf244203c5051967e96c9aa66d8f85
Robin Gloster <mail@glob.in>
2021-04-27 18:52:47 -0500

    ncompress: structured-attrs build fix

--- pkgs/tools/compression/ncompress/default.nix
+++ pkgs/tools/compression/ncompress/default.nix
@@ -1,15 +1,26 @@
-{lib, stdenv, fetchurl}:
+{ lib, stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
   pname = "ncompress";
   version = "5.0";
 
-  builder = ./builder.sh;
-
   src = fetchurl {
     url = "mirror://sourceforge/project/ncompress/${pname}-${version}.tar.gz";
     sha256 = "004r086c11sw9vg2j3srgxpz98w8pycjl33bk3pgqnd0s92igrn4";
   };
+
+  installFlags = [ "PREFIX=${placeholder "out"}" ];
+
+  preBuild = ''
+    cp Makefile.def Makefile
+    sed -i GNUmakefile -e 's/compress %/%/g'
+  '';
+
+  postInstall = ''
+    rm $out/bin/uncompress* $out/bin/zcat*
+    ln -s compress $out/bin/uncompress
+    ln -s compress $out/bin/zcat
+  '';
 
   meta = {
     homepage = "http://ncompress.sourceforge.net/";
