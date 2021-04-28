ac630e8d2664c78dd988b25032f2ada1f99cf763
Robin Gloster <mail@glob.in>
2021-04-27 19:04:35 -0500

    driversi686Linux.vaapiIntel: structured-attrs build fix

--- pkgs/development/libraries/vaapi-intel/default.nix
+++ pkgs/development/libraries/vaapi-intel/default.nix
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
   };
 
   # Set the correct install path:
-  LIBVA_DRIVERS_PATH = "${placeholder "out"}/lib/dri";
+  env.LIBVA_DRIVERS_PATH = "${placeholder "out"}/lib/dri";
 
   postInstall = lib.optionalString enableHybridCodec ''
     ln -s ${vaapi-intel-hybrid}/lib/dri/* $out/lib/dri/
