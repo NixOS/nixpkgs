e6b8c94438b1a51f0fbd12baeb4e15052747c2d2
Tim Steinbach <tim@nequissimus.com>
2021-04-28 10:51:26 -0400

    linux: 5.11.16 -> 5.11.17

--- pkgs/os-specific/linux/kernel/linux-5.11.nix
+++ pkgs/os-specific/linux/kernel/linux-5.11.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.11.16";
+  version = "5.11.17";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,7 +13,7 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0hqgai4r40xxlfqp1paxhn2g4i4yqvi1k473dddcxjrhs60kc5i1";
+    sha256 = "18yld85jymb3wf71kd425zzzvriz9wxlw90pklgf91q37r9rxpa1";
   };
 
   kernelTests = args.kernelTests or [ nixosTests.kernel-generic.linux_5_11 ];
