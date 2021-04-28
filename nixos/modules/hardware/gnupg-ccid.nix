5bb8d89518ad5bfe54965ab9adc06444eba0011f
Cole Mickens <cole.mickens@gmail.com>
2021-04-28 22:03:29 +0000

    nixos/hardware/gnupg-ccid: use debians udev rules

--- nixos/modules/hardware/gnupg-ccid.nix
+++ nixos/modules/hardware/gnupg-ccid.nix
@@ -5,38 +5,35 @@ with lib;
 # https://www.gnupg.org/howtos/card-howto/en/gnupg-ccid.rules
 # https://www.gnupg.org/howtos/card-howto/en/gnupg-ccid
 
+# but then: https://dev.gnupg.org/T5409
+# https://salsa.debian.org/debian/gnupg2/-/blob/debian/main/debian/scdaemon.udev
+
+# per https://man7.org/linux/man-pages/man1/dh_installudev.1.html
+# it looks like the default level prefix is 60-...
+
 let
-  cfg = config.hardware.gnupg-ccid;
+  # from: https://salsa.debian.org/debian/gnupg2/-/blob/debian/main/debian/scdaemon.udev
+  # latest available on 2021-04-28 (file last commited on 2021-01-05 7817a03).
+  scdaemonUdevRev = "01898735a015541e3ffb43c7245ac1e612f40836";
 
-  # TODO: where the g-p-fuck-sticks is "gnupg-ccid.usermap" meant to come from
+  scdaemonRules = builtins.fetchurl {
+    url = "https://salsa.debian.org/debian/gnupg2/-/raw/${scdaemonUdevRev}/debian/scdaemon.udev";
+    sha256 = "08v0vp6950bz7galvc92zdss89y9vcwbinmbfcdldy8x72w6rqr3";
+  };
 
-  scardGroup = "scard";
-
-  gnupgRulesPkg = pkgs.writeTextDir "lib/udev/rules.d/gnupg-ccid.rules" ''
-    ACTION=="add", SUBSYSTEM=="usb", ENV{PRODUCT}=="4e6/e003/*", RUN+="${gnupgCcidScript}"
-    ACTION=="add", SUBSYSTEM=="usb", ENV{PRODUCT}=="4e6/5115/*", RUN+="${gnupgCcidScript}"
+  scdaemonUdevRulesPkg = pkgs.runCommandNoCC "scdaemon-udev-rules" {} ''
+    loc="$out/lib/udev/rules.d/"
+    mkdir -p "''${loc}"
+    cp "${scdaemonRules}" "''${loc}/60-scdaemon.rules"
   '';
 
-  gnupgCcidScript = pkgs.writeShellScript "gnupg-ccid" ''
-    GROUP="${scardGroup}"
-
-    if [ "${ACTION}" = "add" ] && [ -f "${DEVICE}" ]; then
-      ${pkgs.coreutils}/bin/chmod o-rwx "${DEVICE}"
-      ${pkgs.coreutils}/bin/chgrp "${GROUP}" "${DEVICE}"
-      ${pkgs.coreutils}/bin/chmod g+rw "${DEVICE}"
-    fi
-  '';
+  cfg = config.hardware.gpgSmartcards;
 in {
-  options.hardware.gnupg-ccid = {
-    enable = mkEnableOption "udev rules for Ledger devices";
-    group = mkOption {
-      description = "the name of the group to receive permission to smart cards";
-      type = pkgs.types.str;
-      default = "scard";
-    };
+  options.hardware.gpgSmartcards = {
+    enable = mkEnableOption "udev rules for gnupg smart cards";
   };
 
   config = mkIf cfg.enable {
-    services.udev.packages = [ gnupgRulesPkg ];
+    services.udev.packages = [ scdaemonUdevRulesPkg ];
   };
 }
