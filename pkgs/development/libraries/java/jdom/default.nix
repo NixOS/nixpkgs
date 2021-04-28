0c409d57ea97cfb24606fbc711f9afa393e749d2
Robin Gloster <mail@glob.in>
2021-04-28 14:29:19 -0500

    jdom: structured-attrs build fix

--- pkgs/development/libraries/java/jdom/default.nix
+++ pkgs/development/libraries/java/jdom/default.nix
@@ -2,12 +2,19 @@
 
 stdenv.mkDerivation {
   name = "jdom-1.0";
-  builder = ./builder.sh;
 
   src = fetchurl {
     url = "http://www.jdom.org/dist/binary/jdom-1.0.tar.gz";
     sha256 = "1igmxzcy0s25zcy9vmcw0kd13lh60r0b4qg8lnp1jic33f427pxf";
   };
+
+  dontUnpack = true;
+
+  installPhase = ''
+    tar zxvf $src
+    mkdir -p $out
+    mv * $out
+  '';
 
   meta = with lib; {
     description = "Java-based solution for accessing, manipulating, and outputting XML data from Java code";
