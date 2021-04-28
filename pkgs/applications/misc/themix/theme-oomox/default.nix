a798d35ec575efc29b55ed4f9b84402968496142
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-04-28 18:48:58 +0900

    themix-theme-oomox: 1.12 -> 1.12.r2.g0db28619

--- pkgs/applications/misc/themix/theme-oomox/default.nix
+++ pkgs/applications/misc/themix/theme-oomox/default.nix
@@ -1,19 +1,29 @@
 { stdenv, fetchFromGitHub, python3, bc, sassc, glib, libxml2, gdk-pixbuf
 , gtk-engine-murrine
+, fetchpatch
 }:
 
 stdenv.mkDerivation rec {
   pname = "themix-theme-oomox";
-  version = "1.12";
+  version = "1.12.r2.g0db28619";
 
   src = fetchFromGitHub {
     owner = "themix-project";
     repo = "oomox-gtk-theme";
-    rev = version;
-    sha256 = "015yj9hl283dsgphkva441r1fr580wiyssm4s2x4xfjprqksxg8w";
+    rev = "e0d729510791b4797e80610b73b5beea07673b10";
+    sha256 = "1i0pj3hwpxcrwrx1y1yf9xxp38zcaljh7f8na3z3k77f1pldch27";
   };
 
   patches = [
+    (let
+       rev = "b695fcb8d303f804c53d85ad7d6396b0cd2b29b4";
+       sha256 = "0696bvj8pddf34pnljkxbnl2za6ah80a5rmjj89qjs122xg50n0d";
+     in
+     fetchpatch {
+       url = "https://github.com/themix-project/oomox-gtk-theme/commit/${rev}.patch";
+       inherit sha256;
+     })
+
     # themix-gui generates customized theme by `cp -r` theme skeleton to
     # working directory and modifying it. As the skeleton is in nix store and
     # not writable, the copied one is not modifiable.
