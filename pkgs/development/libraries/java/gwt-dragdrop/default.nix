c0b788e97fa64a7df80b993d487b25a995d207b7
Robin Gloster <mail@glob.in>
2021-04-28 14:28:48 -0500

    gwtdragdrop: structured-attrs build fix

--- pkgs/development/libraries/java/gwt-dragdrop/default.nix
+++ pkgs/development/libraries/java/gwt-dragdrop/default.nix
@@ -2,12 +2,18 @@
 
 stdenv.mkDerivation {
   name = "gwt-dnd-2.6.5";
-  builder = ./builder.sh;
 
   src = fetchurl {
     url = "http://gwt-dnd.googlecode.com/files/gwt-dnd-2.6.5.jar";
     sha256 = "07zdlr8afs499asnw0dcjmw1cnjc646v91lflx5dv4qj374c97fw";
   };
+
+  dontUnpack = true;
+
+  installPhase = ''
+    mkdir -p $out/share/java
+    cp $src $out/share/java/$name.jar
+  '';
 
   meta = with lib; {
     platforms = platforms.unix;
