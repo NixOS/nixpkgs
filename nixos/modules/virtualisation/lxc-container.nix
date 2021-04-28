d262f6f59361fbc9c232b8ed5483d3258b42497d
Maciej Krüger <mkg20001@gmail.com>
2021-04-28 04:25:21 +0200

    virtualisation.lxc-container: overwrite tarball for lxd

--- nixos/modules/virtualisation/lxc-container.nix
+++ nixos/modules/virtualisation/lxc-container.nix
@@ -7,6 +7,34 @@ with lib;
     ../profiles/docker-container.nix # FIXME, shouldn't include something from profiles/
   ];
 
+  system.build.tarball = mkForce (pkgs.callPackage ../../lib/make-system-tarball.nix {
+    extraArgs = "--owner=0";
+
+    storeContents = [
+      config.system.build.toplevel
+    ];
+
+    contents = [
+      {
+        source = pkgs.writeText "metadata.yaml" ''
+          architecture: ${builtins.elemAt (builtins.match "^([a-z0-9_]+).+" (toString pkgs.system)) 0}
+          creation_date: 0
+          properties:
+            description: NixOS ${config.system.nixos.codeName} ${config.system.nixos.label} ${pkgs.system}
+            os: nixos
+            release: ${config.system.nixos.codeName}
+        '';
+        target = "/metadata.yaml";
+      }
+      {
+        source = config.system.build.toplevel + "/init";
+        target = "/sbin/init";
+      }
+    ];
+
+    extraCommands = "mkdir -p proc sys dev";
+  });
+
   # Allow the user to login as root without password.
   users.users.root.initialHashedPassword = mkOverride 150 "";
 
