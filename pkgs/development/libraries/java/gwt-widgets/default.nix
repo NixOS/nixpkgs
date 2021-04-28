e0cddfabe9393f8db973a7dd51b4034bb5977d11
Robin Gloster <mail@glob.in>
2021-04-28 14:29:05 -0500

    gwtwidgets: structured-attrs build fix

--- pkgs/development/libraries/java/gwt-widgets/default.nix
+++ pkgs/development/libraries/java/gwt-widgets/default.nix
@@ -2,12 +2,20 @@
 
 stdenv.mkDerivation {
   name = "gwt-widgets-0.2.0";
-  builder = ./builder.sh;
 
   src = fetchurl {
     url = "mirror://sourceforge/gwt-widget/gwt-widgets-0.2.0-bin.tar.gz";
     sha256 = "09isj4j6842rj13nv8264irkjjhvmgihmi170ciabc98911bakxb";
   };
+
+  dontUnpack = true;
+
+  installPhase = ''
+    tar xfvz $src
+    cd gwt-widgets-*
+    mkdir -p $out/share/java
+    cp gwt-widgets-*.jar $out/share/java
+  '';
 
   meta = with lib; {
     platforms = platforms.unix;
