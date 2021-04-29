486bb7c4a7735d9364e746fab23d0dec4167b21d
Vladimir Korolev <balodja@gmail.com>
2021-04-29 16:18:23 +0300

    openmodelica: patchelf on produced libraries

--- pkgs/applications/science/misc/openmodelica/omcompiler/default.nix
+++ pkgs/applications/science/misc/openmodelica/omcompiler/default.nix
@@ -1,9 +1,8 @@
 {stdenv, lib, mkDerivation, fetchFromGitHub, fetchgit, autoconf, automake, libtool,
 gfortran, clang, cmake, curl, gnumake, hwloc, jre, openblas, hdf5, expat, ncurses,
-readline, qtbase, qmake, qttools, qtwebkit, webkit, which, lp_solve, omniorb, sqlite, libatomic_ops,
-pkgconfig, file, gettext, flex, bison, doxygen, boost, openscenegraph, gnome2,
-ipopt, libuuid, qtxmlpatterns,
-xorg, git, bash, gtk2, makeWrapper, autoreconfHook }:
+readline, qmake, which, lp_solve, omniorb, sqlite, libatomic_ops,
+pkgconfig, file, gettext, flex, bison, doxygen, boost, qttools,
+ipopt, libuuid, git, bash, makeWrapper, autoreconfHook }:
 
 let omrepo = import ./src-main.nix;
 in
@@ -12,29 +11,42 @@ mkDerivation rec {
   version = "1.17.0";
   src = fetchgit omrepo;
 
-  nativeBuildInputs = [autoconf automake libtool cmake qttools gfortran clang makeWrapper
-    flex bison doxygen
+  nativeBuildInputs = [autoconf automake libtool cmake gfortran clang makeWrapper
+    flex bison doxygen qttools
     pkgconfig file
     autoreconfHook];
 
   buildInputs = [hwloc curl
-    jre openblas hdf5 expat ncurses readline qtbase qtwebkit webkit which lp_solve
+    jre openblas hdf5 expat ncurses readline which lp_solve
     omniorb sqlite libatomic_ops gettext boost
-    ipopt libuuid qtxmlpatterns
-    openscenegraph gnome2.gtkglext xorg.libXmu git gtk2 makeWrapper];
+    ipopt libuuid
+    git makeWrapper];
 
   hardeningDisable = [ "format" ];
 
   enableParallelBuilding = false;
 
-  #sourceRoot = "OpenModelica-" + builtins.substring 0 7 omrepo.rev + "/OMCompiler";
-  preAutoreconf = "cd OMCompiler";
+  # sourceRoot = "OpenModelica-" + builtins.substring 0 7 omrepo.rev + "/OMCompiler";
+  preAutoreconf = ''
+    cd OMCompiler
+    patchShebangs --build
+  '';
+
   patchPhase = ''
     sed -i ''$(find -L -name qmake.m4) -e '/^\s*LRELEASE=/ s|LRELEASE=.*$|LRELEASE=${lib.getDev qttools}/bin/lrelease|'
     sed -i OMCompiler/Makefile.common -e '/INSTALL_LOCALEDIR/ s|^|#|'
+    sed -i ''$(find -name CMakeLists.txt) -e 's|/''${CMAKE_LIBRARY_ARCHITECTURE}||'
+    sed -i ''$(find -name Makefile.in) -e 's|/@host_short@||'
   '';
 
   dontUseCmakeConfigure = true;
+
+  preFixup = ''
+    for entry in $(find $out -regex '.*\.so\.?.*' -type f); do
+      patchelf --shrink-rpath --allowed-rpath-prefixes /nix/store $entry
+      patchelf --set-rpath '$ORIGIN':"$(patchelf --print-rpath $entry)" $entry
+    done
+  '';
 
   postFixup = ''
     for e in $(cd $out/bin && ls); do
