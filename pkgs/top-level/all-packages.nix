8fda249f9cabb985688fa6d9f0e699973be8dd6d
Robert Schütz <dev@schuetz-co.de>
2021-04-29 20:02:05 +0200

    recapp: init at 1.1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25701,6 +25701,8 @@ in
 
   reaper = callPackage ../applications/audio/reaper { };
 
+  recapp = callPackage ../applications/video/recapp { };
+
   recode = callPackage ../tools/text/recode { };
 
   reddsaver = callPackage ../applications/misc/reddsaver {
