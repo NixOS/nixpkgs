e7835da83f14600a8fc713aa00023aeb461f82ca
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-04-30 15:27:43 +0200

    zoom-us: 5.6.16775.0418 -> 5.6.16888.0424

--- pkgs/applications/networking/instant-messengers/zoom-us/default.nix
+++ pkgs/applications/networking/instant-messengers/zoom-us/default.nix
@@ -29,11 +29,11 @@
 assert pulseaudioSupport -> libpulseaudio != null;
 
 let
-  version = "5.6.16775.0418";
+  version = "5.6.16888.0424";
   srcs = {
     x86_64-linux = fetchurl {
       url = "https://zoom.us/client/${version}/zoom_x86_64.pkg.tar.xz";
-      sha256 = "twtxzniojgyLTx6Kda8Ej96uyw2JQB/jIhLdTgTqpCo=";
+      sha256 = "H/G9cSVmxYM0AVfrdpXzm7ohssDbKq2xdvIBc4d+elc=";
     };
   };
 
