f37db5d583c5e0c87a847546cab0b6e49716e7aa
R. RyanTM <ryantm-bot@ryantm.com>
2021-04-30 17:46:03 +0000

    kronosnet: 1.20 -> 1.21

--- pkgs/development/libraries/kronosnet/default.nix
+++ pkgs/development/libraries/kronosnet/default.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   pname = "kronosnet";
-  version = "1.20";
+  version = "1.21";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-lP5W+4b9McU2Uqibh2SucIu2y4KluO3B1RpAJKgYq/M=";
+    sha256 = "sha256-qOeSYN+Ty6TQViGA/LbILBdNmZyY+/VM2R9olGJOHT4=";
   };
 
   nativeBuildInputs = [ autoreconfHook pkg-config doxygen ];
