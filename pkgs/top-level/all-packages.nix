53193ef71690039548c2f48c5a92af1217b82561
Zhaofeng Li <hello@zhaofeng.li>
2021-04-30 00:54:24 +0000

    phoc: init at 0.7.0
    Co-authored-by: Blaž Hrastnik <blaz@mxxn.io>
    Co-authored-by: Jan Tojnar <jtojnar@gmail.com>
    Co-authored-by: Jordi Masip <jordi@masip.cat>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7530,6 +7530,10 @@ in
 
   philter = callPackage ../tools/networking/philter { };
 
+  phoc = callPackage ../applications/misc/phoc {
+    wlroots = wlroots_0_12;
+  };
+
   phodav = callPackage ../tools/networking/phodav { };
 
   pim6sd = callPackage ../servers/pim6sd { };
