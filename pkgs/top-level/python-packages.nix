064943f8b053cd4d9d3f2c4f759d59a36c7c3300
Vojtěch Káně <vojtech.kane@gmail.com>
2021-04-30 14:46:53 +0200

    pythonPackages.rst2ansi: init at 0.1.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7144,6 +7144,8 @@ in {
 
   rsa = callPackage ../development/python-modules/rsa { };
 
+  rst2ansi = callPackage ../development/python-modules/rst2ansi { };
+
   rtmidi-python = callPackage ../development/python-modules/rtmidi-python { };
 
   Rtree = callPackage ../development/python-modules/Rtree {
