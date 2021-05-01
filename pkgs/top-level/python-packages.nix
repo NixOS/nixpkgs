d26d7713819ae913b575a45defd094fb7ee8e30a
Jan Tojnar <jtojnar@gmail.com>
2021-05-01 14:08:20 +0200

    python3.pkgs.click-configfile: init at 0.2.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1359,6 +1359,8 @@ in {
 
   click-completion = callPackage ../development/python-modules/click-completion { };
 
+  click-configfile = callPackage ../development/python-modules/click-configfile { };
+
   click-datetime = callPackage ../development/python-modules/click-datetime { };
 
   click-default-group = callPackage ../development/python-modules/click-default-group { };
