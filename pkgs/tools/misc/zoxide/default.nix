ab92ec63d29361d8632d89d318d915d974f40c54
Mario Rodas <marsam@users.noreply.github.com>
2021-05-01 20:33:00 -0500

    zoxide: install manpages

--- pkgs/tools/misc/zoxide/default.nix
+++ pkgs/tools/misc/zoxide/default.nix
@@ -4,6 +4,7 @@
 , rustPlatform
 , withFzf ? true
 , fzf
+, installShellFiles
 , libiconv
 }:
 
@@ -18,6 +19,8 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-yunKyCjJ/vWUcsodweLmxv0+QJQI2i5u5VIzp2U+VyU=";
   };
 
+  nativeBuildInputs = [ installShellFiles ];
+
   buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
 
   postPatch = lib.optionalString withFzf ''
@@ -27,9 +30,14 @@ rustPlatform.buildRustPackage rec {
 
   cargoSha256 = "sha256-eoal6z4wX1pNdAJfdamJgOSFCvGWdbVlq1X+vD9lraE=";
 
+  postInstall = ''
+    installManPage man/*
+  '';
+
   meta = with lib; {
     description = "A fast cd command that learns your habits";
     homepage = "https://github.com/ajeetdsouza/zoxide";
+    changelog = "https://github.com/ajeetdsouza/zoxide/raw/v${version}/CHANGELOG.md";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ ysndr cole-h SuperSandro2000 ];
   };
