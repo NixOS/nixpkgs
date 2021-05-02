884cf29501c424ee68d1f2791edd1ed4ef2a9361
Luke Granger-Brown <git@lukegb.com>
2021-05-02 11:10:40 +0000

    dcm2niix: 1.0.20201102 -> 1.0.20210317

--- pkgs/applications/science/biology/dcm2niix/default.nix
+++ pkgs/applications/science/biology/dcm2niix/default.nix
@@ -6,14 +6,14 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "1.0.20201102";
+  version = "1.0.20210317";
   pname = "dcm2niix";
 
   src = fetchFromGitHub {
     owner = "rordenlab";
     repo = "dcm2niix";
     rev = "v${version}";
-    sha256 = "0r21a55fd1fhkkrqqrynasvvnrbhzq0g3ifav2858hppdicw1j35";
+    sha256 = "05rjk0xsrzcxa979vlx25k1rdz1in84gkfm9l1h9f7k4a4aa5r6j";
   };
 
   nativeBuildInputs = [ cmake git ];
