8fe0143d88e87e529c3d143ae2fb589ce651dcb6
Guillaume Girol <symphorien+git@xlumurb.eu>
2021-05-02 12:00:00 +0000

    nixos: add option to load wireless regulatory database as firmware
    use it when networkmanager or wpa_supplicant is enabled.
    
    fixes #57053
    fixes "Direct firmware load for regulatory.db failed with error -2"
    in dmesg
    
    Note that all kernels on unstable are newer that 4.15, which is required
    for this to work.

--- nixos/modules/services/networking/networkmanager.nix
+++ nixos/modules/services/networking/networkmanager.nix
@@ -6,7 +6,6 @@ let
   cfg = config.networking.networkmanager;
 
   basePackages = with pkgs; [
-    crda
     modemmanager
     networkmanager
     networkmanager-fortisslvpn
@@ -366,6 +365,8 @@ in {
         '';
       }
     ];
+
+    hardware.wirelessRegulatoryDatabase = true;
 
     environment.etc = with pkgs; {
       "NetworkManager/NetworkManager.conf".source = configFile;
