88a43165bd780272c8a5c1b2764fea4873d68d40
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2021-05-03 11:23:53 +0100

    mautrix-signal: fix escape in postInstall

--- pkgs/servers/mautrix-signal/default.nix
+++ pkgs/servers/mautrix-signal/default.nix
@@ -37,7 +37,7 @@ python3Packages.buildPythonPackage rec {
     # Make a little wrapper for running mautrix-signal with its dependencies
     echo "$mautrixSignalScript" > $out/bin/mautrix-signal
     echo "#!/bin/sh
-      exec python -m mautrix_signal \"$@\"
+      exec python -m mautrix_signal \"\$@\"
     " > $out/bin/mautrix-signal
     chmod +x $out/bin/mautrix-signal
     wrapProgram $out/bin/mautrix-signal \
