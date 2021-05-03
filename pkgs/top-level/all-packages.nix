7731a02d813390ebb03ee289f6c1451e1f047b8c
Florian Franzen <Florian.Franzen@gmail.com>
2021-05-03 18:02:29 +0200

    segger-jlink: init at 7.20
    Co-authored-by: Brandon Black <freedom@reardencode.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16297,6 +16297,8 @@ with pkgs;
 
   scss-lint = callPackage ../development/tools/scss-lint { };
 
+  segger-jlink = callPackage ../development/tools/misc/segger-jlink { };
+
   segger-ozone = callPackage ../development/tools/misc/segger-ozone { };
 
   selene = callPackage ../development/tools/selene {
