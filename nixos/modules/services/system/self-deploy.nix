422f3a4ef34e1314da76b67951ea71cd6d0dea9b
Ashlynn Anderson <aanderson@kato.local>
2021-05-03 11:32:57 -0400

    nixos/self-deploy: make recommended changes
    Changes:
     - /var/lib/{self-deploy->nixos-self-deploy}
     - run nixpkgs-fmt

--- nixos/modules/services/system/self-deploy.nix
+++ nixos/modules/services/system/self-deploy.nix
@@ -3,7 +3,7 @@
 let
   cfg = config.services.self-deploy;
 
-  workingDirectory = "/var/lib/self-deploy";
+  workingDirectory = "/var/lib/nixos-self-deploy";
   repositoryDirectory = "${workingDirectory}/repo";
   outPath = "${workingDirectory}/system";
 
@@ -16,11 +16,12 @@ let
         then " --argstr ${lib.escapeShellArg key} ${lib.escapeShellArg value}"
         else " --arg ${lib.escapeShellArg key} ${lib.escapeShellArg (toString value)}";
     in
-      lib.concatStrings (lib.mapAttrsToList toArg args);
+    lib.concatStrings (lib.mapAttrsToList toArg args);
 
   isPathType = x: lib.strings.isCoercibleToString x && builtins.substring 0 1 (toString x) == "/";
 
-in {
+in
+{
   options.services.self-deploy = {
     enable = lib.mkEnableOption "self-deploy";
 
@@ -30,8 +31,8 @@ in {
       default = "/default.nix";
 
       description = ''
-      Path to nix file in repository. Leading '/' refers to root of
-      git repository.
+        Path to nix file in repository. Leading '/' refers to root of
+        git repository.
       '';
     };
 
@@ -39,18 +40,18 @@ in {
       type = lib.types.str;
 
       description = ''
-      Attribute of `nixFile` that builds the current system.
+        Attribute of `nixFile` that builds the current system.
       '';
     };
 
     nixArgs = lib.mkOption {
       type = lib.types.attrs;
 
-      default = {};
+      default = { };
 
       description = ''
-      Arguments to `nix-build` passed as `--argstr` or `--arg` depending on
-      the type.
+        Arguments to `nix-build` passed as `--argstr` or `--arg` depending on
+        the type.
       '';
     };
 
@@ -60,7 +61,7 @@ in {
       default = "switch";
 
       description = ''
-      The `switch-to-configuration` subcommand used.
+        The `switch-to-configuration` subcommand used.
       '';
     };
 
@@ -68,15 +69,15 @@ in {
       type = with lib.types; oneOf [ path str ];
 
       description = ''
-      The repository to fetch from. Must be properly formatted for git.
+        The repository to fetch from. Must be properly formatted for git.
 
-      If this value is set to a path (must begin with `/`) then it's
-      assumed that the repository is local and the resulting service
-      won't wait for the network to be up.
+        If this value is set to a path (must begin with `/`) then it's
+        assumed that the repository is local and the resulting service
+        won't wait for the network to be up.
 
-      If the repository will be fetched over SSH, you must add an
-      entry to `programs.ssh.knownHosts` for the SSH host for the fetch
-      to be successful.
+        If the repository will be fetched over SSH, you must add an
+        entry to `programs.ssh.knownHosts` for the SSH host for the fetch
+        to be successful.
       '';
     };
 
@@ -86,8 +87,8 @@ in {
       default = null;
 
       description = ''
-      Path to SSH private key used to fetch private repositories over
-      SSH.
+        Path to SSH private key used to fetch private repositories over
+        SSH.
       '';
     };
 
@@ -97,11 +98,11 @@ in {
       default = "master";
 
       description = ''
-      Branch to track
+        Branch to track
 
-      Technically speaking any ref can be specified here, as this is
-      passed directly to a `git fetch`, but for the use-case of
-      continuous deployment you're likely to want to specify a branch.
+        Technically speaking any ref can be specified here, as this is
+        passed directly to a `git fetch`, but for the use-case of
+        continuous deployment you're likely to want to specify a branch.
       '';
     };
 
@@ -111,12 +112,12 @@ in {
       default = "hourly";
 
       description = ''
-      The schedule on which to run the `self-deploy` service. Format
-      specified by `systemd.time 7`.
+        The schedule on which to run the `self-deploy` service. Format
+        specified by `systemd.time 7`.
 
-      This value can also be a list of `systemd.time 7` formatted
-      strings, in which case the service will be started on multiple
-      schedules.
+        This value can also be a list of `systemd.time 7` formatted
+        strings, in which case the service will be started on multiple
+        schedules.
       '';
     };
   };
@@ -133,34 +134,36 @@ in {
       restartIfChanged = false;
 
       path = with pkgs; [
-        git nix systemd
+        git
+        nix
+        systemd
       ];
 
       script = ''
-      if [ ! -e ${repositoryDirectory} ]; then
-        mkdir --parents ${repositoryDirectory}
-        git init ${repositoryDirectory}
-      fi
+        if [ ! -e ${repositoryDirectory} ]; then
+          mkdir --parents ${repositoryDirectory}
+          git init ${repositoryDirectory}
+        fi
 
-      ${gitWithRepo} fetch ${lib.escapeShellArg cfg.repository} ${lib.escapeShellArg cfg.branch}
+        ${gitWithRepo} fetch ${lib.escapeShellArg cfg.repository} ${lib.escapeShellArg cfg.branch}
 
-      ${gitWithRepo} checkout FETCH_HEAD
+        ${gitWithRepo} checkout FETCH_HEAD
 
-      nix-build${renderNixArgs cfg.nixArgs} ${lib.cli.toGNUCommandLineShell {} {
-        attr = cfg.nixAttribute;
-        out-link = outPath;
-      }} ${lib.escapeShellArg "${repositoryDirectory}${cfg.nixFile}"}
+        nix-build${renderNixArgs cfg.nixArgs} ${lib.cli.toGNUCommandLineShell { } {
+          attr = cfg.nixAttribute;
+          out-link = outPath;
+        }} ${lib.escapeShellArg "${repositoryDirectory}${cfg.nixFile}"}
 
-      ${lib.optionalString (cfg.switchCommand != "test")
-        "nix-env --profile /nix/var/nix/profiles/system --set ${outPath}"}
+        ${lib.optionalString (cfg.switchCommand != "test")
+          "nix-env --profile /nix/var/nix/profiles/system --set ${outPath}"}
 
-      ${outPath}/bin/switch-to-configuration ${cfg.switchCommand}
+        ${outPath}/bin/switch-to-configuration ${cfg.switchCommand}
 
-      rm ${outPath}
+        rm ${outPath}
 
-      ${gitWithRepo} gc --prune=all
+        ${gitWithRepo} gc --prune=all
 
-      ${lib.optionalString (cfg.switchCommand == "boot") "systemctl reboot"}
+        ${lib.optionalString (cfg.switchCommand == "boot") "systemctl reboot"}
       '';
     };
   };
