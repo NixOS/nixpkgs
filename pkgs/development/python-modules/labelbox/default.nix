a9558a193b2ee5b5040a4ba3fcdb4c383047e569
Fabian Affolter <mail@fabian-affolter.ch>
2021-05-04 11:43:07 +0200

    python3Packages.labelbox: 2.5.1 -> 2.5.4

--- pkgs/development/python-modules/labelbox/default.nix
+++ pkgs/development/python-modules/labelbox/default.nix
@@ -4,27 +4,35 @@
 , requests
 , jinja2
 , pillow
+, dataclasses
+, pythonOlder
 , rasterio
 , shapely
 , ndjson
 , backoff
+, pydantic
 , google-api-core
 , backports-datetime-fromisoformat
 }:
 
 buildPythonPackage rec {
   pname = "labelbox";
-  version = "2.5.1";
+  version = "2.5.4";
+  disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "7f2cbc5d4869d8acde865ad519fc1cc85338247cd7cf534334f988a040679219";
+    sha256 = "sha256-L1KjP8Yzx9adTK84+Nf9JnirT4p3D3lwulWw6W1L/88=";
   };
 
   propagatedBuildInputs = [
     jinja2 requests pillow rasterio shapely ndjson backoff
-    google-api-core backports-datetime-fromisoformat
+    google-api-core backports-datetime-fromisoformat pydantic
   ];
+
+  postPatch = ''
+    substituteInPlace setup.py --replace "pydantic==1.8" "pydantic>=1.8"
+  '';
 
   # Test cases are not running on pypi or GitHub
   doCheck = false;
