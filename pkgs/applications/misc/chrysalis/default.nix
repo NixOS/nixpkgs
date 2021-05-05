ea8f93eecf4acb07a87ff21f3942b29873a576f0
Bill Sun <billksun@gmail.com>
2021-05-04 17:04:47 -0700

    Fix GSettings schema not found

--- pkgs/applications/misc/chrysalis/default.nix
+++ pkgs/applications/misc/chrysalis/default.nix
@@ -1,4 +1,4 @@
-{ lib, appimageTools, fetchurl }:
+{ lib, appimageTools, fetchurl, gtk3, gsettings-desktop-schemas }:
 
 let
   pname = "chrysalis";
@@ -10,6 +10,10 @@ in appimageTools.wrapType2 rec {
     url = "https://github.com/keyboardio/${pname}/releases/download/v${version}/${pname}-${version}.AppImage";
     sha256 = "b41f3e23dac855b1588cff141e3d317f96baff929a0543c79fccee0c6f095bc7";
   };
+
+  profile = ''
+    export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS
+  '';
 
   multiPkgs = null;
   extraPkgs = p: (appimageTools.defaultFhsEnvArgs.multiPkgs p) ++ [
