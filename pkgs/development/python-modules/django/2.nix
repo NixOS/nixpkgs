177732cf2e7c0aed24766ec25ce344829415bb4a
Robert Schütz <dev@schuetz-co.de>
2021-05-06 15:18:50 +0200

    python3Packages.django_2_2: 2.2.20 -> 2.2.22
    https://www.djangoproject.com/weblog/2021/may/06/security-releases/
    (cherry picked from commit 2e0b8acd823c1166d48097891368d59aac821e0d)

--- pkgs/development/python-modules/django/2.nix
+++ pkgs/development/python-modules/django/2.nix
@@ -6,13 +6,13 @@
 
 buildPythonPackage rec {
   pname = "Django";
-  version = "2.2.20";
+  version = "2.2.22";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0r3a6gbhwngxl172yy6n0sq5knibl2vxc0wbk1g8licfbzfgjs95";
+    sha256 = "db2214db1c99017cbd971e58824e6f424375154fe358afc30e976f5b99fc6060";
   };
 
   patches = stdenv.lib.optional withGdal
