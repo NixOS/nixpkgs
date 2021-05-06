bcdf3fbeeb576512d8624abbbcd021c6ab998097
Jan Tojnar <jtojnar@gmail.com>
2021-05-06 21:01:39 +0200

    gnome3.gpaste: 3.40.1 → 3.40.2
    http://www.imagination-land.org/posts/2021-04-30-gpaste-3.40.2-released.html

--- pkgs/desktops/gnome-3/misc/gpaste/default.nix
+++ pkgs/desktops/gnome-3/misc/gpaste/default.nix
@@ -17,14 +17,14 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "3.40.1";
+  version = "3.40.2";
   pname = "gpaste";
 
   src = fetchFromGitHub {
     owner = "Keruspe";
     repo = "GPaste";
     rev = "v${version}";
-    sha256 = "sha256-coMye/arH0JYgcjf/y3ZS8Bijuivs9m5Z7EiJQJzThk=";
+    sha256 = "sha256-DUikcnkDBRkCwPLrl8lkNr+SeNpc3bPwPTWRn91nOo4=";
   };
 
   patches = [
