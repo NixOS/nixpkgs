2bee7fa739f2e0b68fb876bb70805af190b48da7
Mica <paperdigits@users.noreply.github.com>
2021-05-07 11:39:31 -0700

    Update pkgs/applications/video/prism/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/applications/video/prism/default.nix
+++ pkgs/applications/video/prism/default.nix
@@ -8,10 +8,10 @@ buildGoModule rec {
     owner = "muesli";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256:0q7q7aj3fm45bnx6hgl9c1ll8na16x6p7qapr0c4a6dhxwd7n511";
+    sha256 = "0q7q7aj3fm45bnx6hgl9c1ll8na16x6p7qapr0c4a6dhxwd7n511";
   };
 
-  vendorSha256 = "sha256:1mkd1s9zgzy9agy2rjjk8wfdga7nzv9cmwgiarfi4xrqzj4mbaxq";
+  vendorSha256 = "1mkd1s9zgzy9agy2rjjk8wfdga7nzv9cmwgiarfi4xrqzj4mbaxq";
 
   meta = with lib; {
     description = "An RTMP stream recaster/splitter";
