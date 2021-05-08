e6d63982d9069a02213bea7c5936cc52222463ba
Graham Christensen <graham@grahamc.com>
2021-05-08 12:18:22 -0400

    pgbadger: init at 11.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7185,6 +7185,8 @@ in
 
   pcmsolver = callPackage ../development/libraries/pcmsolver { };
 
+  pgbadger = perlPackages.callPackage ../tools/misc/pgbadger { };
+
   phash = callPackage ../development/libraries/phash { };
 
   pnmixer = callPackage ../tools/audio/pnmixer { };
