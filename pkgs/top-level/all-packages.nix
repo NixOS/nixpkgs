8c9a5a09f92f28e116723ba3d928af868f71474d
Joe Hermaszewski <git@monoid.al>
2021-05-08 15:08:21 +0800

    picoscope: init at 6.14.44-4r5870
    Based on https://github.com/wirew0rm/nixpkgs/tree/pkg/picoscope
    
    Sadly it comes with its own version of mono, problems with Nixpkgs' mono
    described here: https://discourse.nixos.org/t/picoscope-oscilloscope-drivers-and-mono-application/10899
    
    Currently only 2000 series scopes have been tested.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3980,6 +3980,8 @@ in
     tk = tk-8_5;
   };
 
+  picoscope = callPackage ../applications/science/electronics/picoscope { };
+
   picotts = callPackage ../tools/audio/picotts { };
 
   wgetpaste = callPackage ../tools/text/wgetpaste { };
