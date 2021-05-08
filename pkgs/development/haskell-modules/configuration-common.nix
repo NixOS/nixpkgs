3e3f1c02f24b4ba8d582bb0fb9ef256fa3293fd0
Dmitry Ivankov <divanorama@gmail.com>
2021-05-08 21:13:55 +0200

    haskellPackages.gi-gtk-declarative-app-simple: loosen haskell-gi version bound
    https://hydra.nixos.org/build/142721471/nixlog/1
    
    ZHF: #122042

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -1950,4 +1950,9 @@ self: super: {
   # https://github.com/merijn/paramtree/issues/4
   paramtree = dontCheck super.paramtree;
 
+  # Too strict version bounds on haskell-gi
+  # https://github.com/owickstrom/gi-gtk-declarative/issues/100
+  gi-gtk-declarative = doJailbreak super.gi-gtk-declarative;
+  gi-gtk-declarative-app-simple = doJailbreak super.gi-gtk-declarative-app-simple;
+
 } // import ./configuration-tensorflow.nix {inherit pkgs haskellLib;} self super
