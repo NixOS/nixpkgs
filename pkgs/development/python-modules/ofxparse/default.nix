24ae6995055ba5941b076c62d9eeafd995790681
Damien Cassou <damien@cassou.me>
2021-05-08 09:23:46 +0200

    pythonPackages.ofxparse: 0.20 -> 0.20-unstable-2020-02-05

--- pkgs/development/python-modules/ofxparse/default.nix
+++ pkgs/development/python-modules/ofxparse/default.nix
@@ -1,6 +1,6 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 , six
 , beautifulsoup4
 , lxml
@@ -8,11 +8,15 @@
 
 buildPythonPackage rec {
   pname = "ofxparse";
-  version = "0.20";
+  version = "unstable-2020-02-05";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "0zn3grc6xhgzcc81qc3dxkkwk731cjjqqhb46smw12lk09cdnigb";
+  # The newer changes haven't been released yet and ledger-autosync
+  # depends on them:
+  src = fetchFromGitHub {
+    owner = "jseutter";
+    repo = "ofxparse";
+    rev = "3236cfd96434feb6bc79a8b66f3400f18e2ad3c4";
+    sha256 = "1rkp174102q7hwjrg3na0qnfd612xb3r360b9blkbprjhzxy7gr7";
   };
 
   propagatedBuildInputs = [ six beautifulsoup4 lxml ];
