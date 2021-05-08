c0ae086d96de6f421298a34b7b567067177b3ddc
Dmitry Ivankov <divanorama@gmail.com>
2021-05-08 21:13:55 +0200

    haskellPackages.gi-gtk-declarative-app-simple: loosen haskell-gi version bound
    https://hydra.nixos.org/build/142721471/nixlog/1
    
    ZHF: #122042

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -1901,4 +1901,9 @@ self: super: {
 
   phonetic-languages-phonetics-basics = appendPatch super.phonetic-languages-phonetics-basics ./patches/phonetic-languages-phonetics-basics-haddock.patch;
 
+  # Too strict version bounds on haskell-gi
+  # https://github.com/owickstrom/gi-gtk-declarative/issues/100
+  gi-gtk-declarative = doJailbreak super.gi-gtk-declarative;
+  gi-gtk-declarative-app-simple = doJailbreak super.gi-gtk-declarative-app-simple;
+
 } // import ./configuration-tensorflow.nix {inherit pkgs haskellLib;} self super
