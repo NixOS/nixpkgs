5d54dd8e3e581ab952c9557dfaa48086423b924d
Eduardo Sánchez Muñoz <esm@eduardosm.net>
2021-05-09 01:32:36 +0200

    gnomeExtensions.appindicator: 36 -> 37

--- pkgs/desktops/gnome/extensions/appindicator/default.nix
+++ pkgs/desktops/gnome/extensions/appindicator/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-shell-extension-appindicator";
-  version = "36";
+  version = "37";
 
   src = fetchFromGitHub {
     owner = "Ubuntu";
     repo = "gnome-shell-extension-appindicator";
     rev = "v${version}";
-    sha256 = "1nx1lgrrp3w5z5hymb91frjdvdkk7x677my5v4jjd330ihqa02dq";
+    sha256 = "1yss91n94laakzhym409iyjs5gwhln2pkq0zrdrsxc3z70zlslxl";
   };
 
   # This package has a Makefile, but it's used for building a zip for
