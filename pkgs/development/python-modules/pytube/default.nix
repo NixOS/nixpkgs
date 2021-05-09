8f1e52e08fd6033a2baeb5cc084b5f87f4b65251
Robert Schütz <dev@schuetz-co.de>
2021-05-09 10:41:29 +0200

    python3Packages.pytube: 10.7.2 -> 10.8.0
    https://github.com/pytube/pytube/releases/tag/v10.8.0

--- pkgs/development/python-modules/pytube/default.nix
+++ pkgs/development/python-modules/pytube/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "pytube";
-  version = "10.7.2";
+  version = "10.8.0";
 
   disabled = pythonOlder "3.6";
 
@@ -15,7 +15,7 @@ buildPythonPackage rec {
     owner = "pytube";
     repo = "pytube";
     rev = "v${version}";
-    sha256 = "sha256-85pHzfQYyqwX8mQ5msIojM/0FSfeaC12KJw4mXmji3g=";
+    sha256 = "sha256-9E1NEG58Yps5JVnO0SAXfT37CzETSW8v5bVGCvJhlig=";
   };
 
   checkInputs = [
