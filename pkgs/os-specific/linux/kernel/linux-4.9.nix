10ac4bd55180ec1aebe638961cdcb0adaf941893
TredwellGit <tredwell@tutanota.com>
2021-05-09 16:51:32 +0000

    linux: 4.9.267 -> 4.9.268
    (cherry picked from commit 94ce86210197f75a8bcab9ff04f273f43ce1910f)

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.267";
+  version = "4.9.268";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0q0a49b3wsxk9mqyy8b55lr1gmiqxjpqh2nlhj4xwcfzd7z9lfwq";
+    sha256 = "0aknrlf5q0dsqib8c9klmf5c60dy7hg2zksb020qvyrp077gcrjv";
   };
 } // (args.argsOverride or {}))
