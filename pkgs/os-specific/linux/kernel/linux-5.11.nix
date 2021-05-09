654238cf6d64dd466cf7915295a4fc98d3c53d82
TredwellGit <tredwell@tutanota.com>
2021-05-09 16:51:47 +0000

    linux: 5.11.16 -> 5.11.19
    (cherry picked from commit d96c775d1af7b397099e8469f138908370db7f89)

--- pkgs/os-specific/linux/kernel/linux-5.11.nix
+++ pkgs/os-specific/linux/kernel/linux-5.11.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.11.16";
+  version = "5.11.19";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0hqgai4r40xxlfqp1paxhn2g4i4yqvi1k473dddcxjrhs60kc5i1";
+    sha256 = "0jrb8wbxj0dadyadggcn49hlxzxgz8mz8xr0ckgbnnvb8snikvjs";
   };
 } // (args.argsOverride or {}))
