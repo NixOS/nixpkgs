62bbd50331279335a6dd289101c0026681fb7521
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-05-10 13:39:49 +0200

    haskellPackages.large-hashable: mark as broken on aarch64
    See linked issue.

--- pkgs/development/haskell-modules/configuration-arm.nix
+++ pkgs/development/haskell-modules/configuration-arm.nix
@@ -66,6 +66,9 @@ self: super: {
   # https://github.com/ekmett/half/issues/35
   half = dontCheck super.half;
 
+  # https://github.com/factisresearch/large-hashable/issues/17
+  large-hashable = markBroken super.large-hashable;
+
 } // lib.optionalAttrs pkgs.stdenv.hostPlatform.isAarch32 {
   # AARCH32-SPECIFIC OVERRIDES
 
