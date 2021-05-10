d9d8d6145fe0c79ff2f4e1b5902b0dfacf7e62e1
happysalada <raphael@megzari.com>
2021-05-10 12:35:47 +0900

    nixpkgs-fmt

--- pkgs/development/beam-modules/build-rebar3.nix
+++ pkgs/development/beam-modules/build-rebar3.nix
@@ -1,30 +1,41 @@
-{ stdenv, writeText, erlang, rebar3, openssl, libyaml,
-  pc, lib }:
+{ stdenv
+, writeText
+, erlang
+, rebar3
+, openssl
+, libyaml
+, pc
+, lib
+}:
 
-{ name, version
+{ name
+, version
 , src
 , setupHook ? null
-, buildInputs ? [], beamDeps ? [], buildPlugins ? []
+, buildInputs ? [ ]
+, beamDeps ? [ ]
+, buildPlugins ? [ ]
 , postPatch ? ""
 , compilePorts ? false
 , installPhase ? null
 , buildPhase ? null
 , configurePhase ? null
-, meta ? {}
+, meta ? { }
 , enableDebugInfo ? false
-, ... }@attrs:
+, ...
+}@attrs:
 
 with lib;
 
 let
   debugInfoFlag = lib.optionalString (enableDebugInfo || erlang.debugInfo) "debug-info";
 
-  ownPlugins = buildPlugins ++ (if compilePorts then [pc] else []);
+  ownPlugins = buildPlugins ++ (if compilePorts then [ pc ] else [ ]);
 
   shell = drv: stdenv.mkDerivation {
-          name = "interactive-shell-${drv.name}";
-          buildInputs = [ drv ];
-    };
+    name = "interactive-shell-${drv.name}";
+    buildInputs = [ drv ];
+  };
 
   customPhases = filterAttrs
     (_: v: v != null)
@@ -46,7 +57,7 @@ let
     inherit src;
 
     setupHook = writeText "setupHook.sh" ''
-       addToSearchPath ERL_LIBS "$1/lib/erlang/lib/"
+      addToSearchPath ERL_LIBS "$1/lib/erlang/lib/"
     '';
 
     postPatch = ''
@@ -91,4 +102,4 @@ let
     };
   } // customPhases);
 in
-  fix pkg
+fix pkg
