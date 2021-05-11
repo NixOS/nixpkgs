8a9702b6529438f0738053ab990e82302699bd9a
Daniël de Kok <me@danieldk.eu>
2021-05-11 08:13:53 +0200

    python3Packages.pytorch-bin: 1.8.0 -> 1.8.1
    Changelog:
    
    https://github.com/pytorch/pytorch/releases/tag/v1.8.1

--- pkgs/development/python-modules/pytorch/bin.nix
+++ pkgs/development/python-modules/pytorch/bin.nix
@@ -18,7 +18,7 @@ let
   pyVerNoDot = builtins.replaceStrings [ "." ] [ "" ] python.pythonVersion;
   srcs = import ./binary-hashes.nix version;
   unsupported = throw "Unsupported system";
-  version = "1.8.0";
+  version = "1.8.1";
 in buildPythonPackage {
   inherit version;
 
