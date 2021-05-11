6f0158e60b5d3a89e5bd994d7f91a71a3ac9bae7
Thiago Franco de Moraes <totonixsame@gmail.com>
2021-05-11 19:25:09 -0300

    gdcm: 3.0.8 -> 3.0.9
    - make use of VTK optional (enabled by default)
    - use vtk instead of vtk_7
    - fetching source code from GitHub (sourceforge doesn't have the last
      version).
    - nixpkgs-fmt

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14636,7 +14636,9 @@ in
 
   gdal_2 = callPackage ../development/libraries/gdal/2.4.nix { };
 
-  gdcm = callPackage ../development/libraries/gdcm { };
+  gdcm = callPackage ../development/libraries/gdcm {
+    inherit (darwin.apple_sdk.frameworks) ApplicationServices Cocoa;
+  };
 
   ggz_base_libs = callPackage ../development/libraries/ggz_base_libs {};
 
