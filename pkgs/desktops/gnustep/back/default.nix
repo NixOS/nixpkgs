3c0f658262e9eee86520416ca50bc912f3e05120
R. RyanTM <ryantm-bot@ryantm.com>
2021-05-11 16:32:18 +0000

    gnustep.back: 0.28.0 -> 0.29.0

--- pkgs/desktops/gnustep/back/default.nix
+++ pkgs/desktops/gnustep/back/default.nix
@@ -8,13 +8,13 @@
 , libXmu
 }:
 let
-  version = "0.28.0";
+  version = "0.29.0";
 in
 gsmakeDerivation {
   name = "gnustep-back-${version}";
   src = fetchurl {
     url = "ftp://ftp.gnustep.org/pub/gnustep/core/gnustep-back-${version}.tar.gz";
-    sha256 = "1ynd27zwga17mp2qlym90k2xsypdvz24w6gyy2rfvmv0gkvlgrjr";
+    sha256 = "sha256-GN1OkgCr7xZXCzMehyXS7PgI+obRJaknzJd26LiKmJI=";
   };
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ cairo base gui freetype xlibsWrapper libXmu ];
