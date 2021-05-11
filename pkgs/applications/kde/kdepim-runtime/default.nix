52837a216c7e3f7228c450b778f2021ce792a50a
Thomas Tuegel <ttuegel@mailbox.org>
2021-05-11 15:36:51 -0500

    kdepim-runtime: remove kdelibs4support dependency

--- pkgs/applications/kde/kdepim-runtime/default.nix
+++ pkgs/applications/kde/kdepim-runtime/default.nix
@@ -3,7 +3,7 @@
   extra-cmake-modules, kdoctools,
   shared-mime-info,
   akonadi, akonadi-calendar, akonadi-contacts, akonadi-mime, akonadi-notes,
-  kalarmcal, kcalutils, kcontacts, kdav, kdelibs4support, kidentitymanagement,
+  kalarmcal, kcalutils, kcontacts, kdav, kidentitymanagement,
   kimap, kldap, kmailtransport, kmbox, kmime, knotifications, knotifyconfig,
   pimcommon, qtwebengine, libkgapi, qca-qt5, qtnetworkauth, qtspeech, qtxmlpatterns,
 }:
@@ -17,7 +17,7 @@ mkDerivation {
   nativeBuildInputs = [ extra-cmake-modules kdoctools shared-mime-info ];
   buildInputs = [
     akonadi akonadi-calendar akonadi-contacts akonadi-mime akonadi-notes
-    kalarmcal kcalutils kcontacts kdav kdelibs4support kidentitymanagement kimap
+    kalarmcal kcalutils kcontacts kdav kidentitymanagement kimap
     kldap kmailtransport kmbox kmime knotifications knotifyconfig qtwebengine
     pimcommon libkgapi qca-qt5 qtnetworkauth qtspeech qtxmlpatterns
   ];
