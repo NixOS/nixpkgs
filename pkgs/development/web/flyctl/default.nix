ef73616a1f7c906d051b54ea1a7464d7c147af5c
R. RyanTM <ryantm-bot@ryantm.com>
2021-05-11 03:34:13 +0000

    flyctl: 0.0.212 -> 0.0.217

--- pkgs/development/web/flyctl/default.nix
+++ pkgs/development/web/flyctl/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "flyctl";
-  version = "0.0.212";
+  version = "0.0.217";
 
   src = fetchFromGitHub {
     owner = "superfly";
     repo = "flyctl";
     rev = "v${version}";
-    sha256 = "sha256-4BL+IhwmKiPN5G/5ut0RJcCXWy4IKPichx2dD3/TRic=";
+    sha256 = "sha256-7WfN+fNmr6lUvKKMwCM6QL0rClsb1uX3qk+ZQZna6kc=";
   };
 
   preBuild = ''
