0ad041e5bcf5358ba100f806e2e925ef5c7be7bf
Francesco Gazzetta <fgaz@fgaz.me>
2021-05-11 11:50:23 +0200

    ocamlPackages.color: init at 0.2.0

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -183,6 +183,8 @@ let
 
     cohttp-mirage = callPackage ../development/ocaml-modules/cohttp/mirage.nix { };
 
+    color = callPackage ../development/ocaml-modules/color { };
+
     conduit = callPackage ../development/ocaml-modules/conduit { };
 
     conduit-async = callPackage ../development/ocaml-modules/conduit/async.nix { };
