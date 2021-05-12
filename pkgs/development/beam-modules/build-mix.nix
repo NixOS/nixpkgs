ffbdd8a2c2ec769c318b74b605259052cdd33dcd
Zach <zach@hipcreativeinc.com>
2021-05-11 21:28:35 -0500

    add comments to build-mix.nix

--- pkgs/development/beam-modules/build-mix.nix
+++ pkgs/development/beam-modules/build-mix.nix
@@ -33,6 +33,8 @@ let
     # stripping does not have any effect on beam files
     dontStrip = true;
 
+    # add to ERL_LIBS so other modules can find at runtime.
+    # http://erlang.org/doc/man/code.html#code-path
     setupHook = attrs.setupHook or
       writeText "setupHook.sh" ''
       addToSearchPath ERL_LIBS "$1/lib/erlang/lib"
@@ -50,8 +52,15 @@ let
       do
         for dir in $(ls $lib)
         do
+          # Strip version number for directory name if it exists, so naming of
+          # all libs matches what mix's expectation.
           dest=$(echo "$dir" | cut -d '-' -f1)
+
           [[ $MIX_DEBUG -eq 1 ]] && echo "Linking $lib/$dir to _build/$MIX_ENV/lib/$dest"
+
+          # Symlink libs to _build so that mix can find them when compiling.
+          # This is what allows mix to compile the package without searching
+          # for dependencies over the network.
           ln -s $lib/$dir _build/$MIX_ENV/lib/$dest
         done
       done <<< "$ERL_LIBS:"
@@ -69,15 +78,20 @@ let
 
     installPhase = attrs.installPhase or ''
       runHook preInstall
+
+      # Some packages will use _build/shared instead of honoring the $MIX_ENV
+      # variable.
       if [ -d "_build/shared" ]; then
         MIX_ENV=shared
       fi
+
+      # This uses the install path convention established by nixpkgs maintainers
+      # for all beam packages. There is no magic here.
       mkdir -p "$out/lib/erlang/lib/${name}-${version}"
       for reldir in src ebin priv include; do
         fd="_build/$MIX_ENV/lib/${name}/$reldir"
         [ -d "$fd" ] || continue
         cp -Hrt "$out/lib/erlang/lib/${name}-${version}" "$fd"
-        success=1
       done
       runHook postInstall
     '';
