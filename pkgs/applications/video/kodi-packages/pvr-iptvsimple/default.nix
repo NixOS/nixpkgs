4f8e95ac59eef5f516c497835f6f16917090ae4c
R. RyanTM <ryantm-bot@ryantm.com>
2021-05-12 04:31:01 +0000

    kodiPackages.pvr-iptvsimple: 7.6.2 -> 7.6.4

--- pkgs/applications/video/kodi-packages/pvr-iptvsimple/default.nix
+++ pkgs/applications/video/kodi-packages/pvr-iptvsimple/default.nix
@@ -6,13 +6,13 @@
 buildKodiBinaryAddon rec {
   pname = "pvr-iptvsimple";
   namespace = "pvr.iptvsimple";
-  version = "7.6.2";
+  version = "7.6.4";
 
   src = fetchFromGitHub {
     owner = "kodi-pvr";
     repo = "pvr.iptvsimple";
     rev = "${version}-${rel}";
-    sha256 = "sha256-MdgPUKkbqNt/WKUTrYNetlyUBQcYLSn0J8EHH2Z9I+g=";
+    sha256 = "sha256-F2uvf3BChN4p4VV1vTMAAPwQchVI5paTdxSFEi0u9a0=";
   };
 
   extraBuildInputs = [
