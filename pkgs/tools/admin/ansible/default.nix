af40f3666dc14becc216bacdc61f776aa11e220f
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-05-12 04:48:44 +0200

    ansible_2_8: drop
    The version went end of life on 2021/01/21.

--- pkgs/tools/admin/ansible/default.nix
+++ pkgs/tools/admin/ansible/default.nix
@@ -8,13 +8,5 @@ rec {
   # End of support 2021/10/02, End of life 2021/12/31
   ansible_2_9 = python3Packages.toPythonApplication python3Packages.ansible;
 
-  ansible_2_8 = python3Packages.toPythonApplication (python3Packages.ansible.overridePythonAttrs (old: rec {
-    pname = "ansible";
-    version = "2.8.14";
-
-    src = fetchurl {
-      url = "https://releases.ansible.com/ansible/${pname}-${version}.tar.gz";
-      sha256 = "19ga0c9qs2b216qjg5k2yknz8ksjn8qskicqspg2d4b8x2nr1294";
-    };
-  }));
+  ansible_2_8 = throw "Ansible 2.8 went end of life on 2021/01/03 and has subsequently been dropped";
 }
