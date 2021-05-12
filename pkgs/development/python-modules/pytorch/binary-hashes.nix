c8283cedb729c3849214570471553b479991d19a
Daniël de Kok <me@danieldk.eu>
2021-05-12 14:58:39 +0200

    python3Packages.pytorch-bin: use CUDA 11 wheels

--- pkgs/development/python-modules/pytorch/binary-hashes.nix
+++ pkgs/development/python-modules/pytorch/binary-hashes.nix
@@ -1,17 +1,17 @@
 version: {
   x86_64-linux-37 = {
     name = "torch-${version}-cp37-cp37m-linux_x86_64.whl";
-    url = "https://download.pytorch.org/whl/cu102/torch-${version}%2Bcu102-cp37-cp37m-linux_x86_64.whl";
-    hash = "sha256-79JK2gGlV5Lhbim7GgPCjk++brwpUQE7g7vsrBu06eE=";
+    url = "https://download.pytorch.org/whl/cu111/torch-${version}%2Bcu111-cp37-cp37m-linux_x86_64.whl";
+    hash = "sha256-lxj066PiB+S88GqQnXKgd9RCiKkMk3mctx/eBieZjAk=";
   };
   x86_64-linux-38 = {
     name = "torch-${version}-cp38-cp38-linux_x86_64.whl";
-    url = "https://download.pytorch.org/whl/cu102/torch-${version}%2Bcu102-cp38-cp38-linux_x86_64.whl";
-    hash = "sha256-16h90SdxAgFxmPpgUPA3rBGxs9dtFaCeAgeSpKmtnmQ=";
+    url = "https://download.pytorch.org/whl/cu111/torch-${version}%2Bcu111-cp38-cp38-linux_x86_64.whl";
+    hash = "sha256-qvTQMLz4CQPgalzUyYwz6rm+ExyWlIzI+FSMQhxM4eM=";
   };
   x86_64-linux-39 = {
     name = "torch-${version}-cp39-cp39-linux_x86_64.whl";
-    url = "https://download.pytorch.org/whl/cu102/torch-${version}%2Bcu102-cp39-cp39-linux_x86_64.whl";
-    hash = "sha256-E3RLWpGYbRuKsz0pQv7npmC7WTbdvlYFaxXQYBKT2/8=";
+    url = "https://download.pytorch.org/whl/cu111/torch-${version}%2Bcu111-cp39-cp39-linux_x86_64.whl";
+    hash = "sha256-z0pzEiljU+E2x2SuOEAg86dmVY1iFZSDOCfoOAFhLQo=";
   };
 }
