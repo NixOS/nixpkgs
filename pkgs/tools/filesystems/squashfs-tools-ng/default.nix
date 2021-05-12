d49e7dd0f4bd38ed50ea7818522314f3d1072851
Alyssa Ross <hi@alyssa.is>
2021-05-12 16:43:21 +0000

    squashfs-tools-ng: 1.1.0 -> 1.1.1; add bzip2 support; enableParallelBuilding (#122649)

--- pkgs/tools/filesystems/squashfs-tools-ng/default.nix
+++ pkgs/tools/filesystems/squashfs-tools-ng/default.nix
@@ -1,18 +1,20 @@
 { stdenv, lib, fetchurl, doxygen, graphviz, perl, pkg-config
-, lz4, lzo, xz, zlib, zstd
+, bzip2, lz4, lzo, xz, zlib, zstd
 }:
 
 stdenv.mkDerivation rec {
   pname = "squashfs-tools-ng";
-  version = "1.1.0";
+  version = "1.1.1";
 
   src = fetchurl {
     url = "https://infraroot.at/pub/squashfs/squashfs-tools-ng-${version}.tar.xz";
-    sha256 = "1swsw5j8rrjxdxsfyd446f6g8f0k3mwg15baivi953i69c9981qi";
+    sha256 = "07c8vpzgwvqr9ycww1769ya40cf077c6igdg1b4akwszz2nw0bxq";
   };
 
   nativeBuildInputs = [ doxygen graphviz pkg-config perl ];
-  buildInputs = [ zlib xz lz4 lzo zstd ];
+  buildInputs = [ bzip2 zlib xz lz4 lzo zstd ];
+
+  enableParallelBuilding = true;
 
   meta = with lib; {
     homepage = "https://github.com/AgentD/squashfs-tools-ng";
