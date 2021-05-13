19c1af43b2d2da1097328fca665ca3e6b04983a4
Robert T. McGibbon <rmcgibbo@gmail.com>
2021-05-13 10:59:09 -0400

    copper: 4.4 -> 4.5

--- pkgs/development/compilers/copper/default.nix
+++ pkgs/development/compilers/copper/default.nix
@@ -4,15 +4,17 @@
 }:
 stdenv.mkDerivation rec {
   pname = "copper";
-  version = "4.4";
+  version = "4.5";
+
   src = fetchurl {
     url = "https://tibleiz.net/download/copper-${version}-src.tar.gz";
-    sha256 = "1nf0bw143rjhd019yms3k6k531rahl8anidwh6bif0gm7cngfwfw";
+    sha256 = "133xavmpi8si9pybx57knpx7wvx59bpr7gvr8gwilcn9gs5g7yhj";
   };
   buildInputs = [
     libffi
   ];
   postPatch = ''
+    substituteInPlace Makefile --replace "CC=gcc" "CC?=gcc"
     substituteInPlace Makefile --replace "-s scripts/" "scripts/"
     patchShebangs .
   '';
@@ -27,6 +29,7 @@ stdenv.mkDerivation rec {
     description = "Simple imperative language, statically typed with type inference and genericity";
     homepage = "https://tibleiz.net/copper/";
     license = licenses.bsd2;
+    broken = stdenv.isDarwin;
     platforms = platforms.x86_64;
   };
 }
