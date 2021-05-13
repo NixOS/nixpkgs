4c957da9bb93bfb3d24da41e1786f861468cc112
Flakebi <flakebi@t-online.de>
2021-05-13 16:41:09 +0200

    python3Packages.inotifyrecursive: init at 0.3.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3577,6 +3577,8 @@ in {
 
   inotify-simple = callPackage ../development/python-modules/inotify-simple { };
 
+  inotifyrecursive = callPackage ../development/python-modules/inotifyrecursive { };
+
   inquirer = callPackage ../development/python-modules/inquirer { };
 
   intake = callPackage ../development/python-modules/intake { };
