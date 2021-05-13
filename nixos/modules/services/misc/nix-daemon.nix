d879e63b4a98463ab42dc91aea4ec67fbcca813f
Pamplemousse <xav.maso@gmail.com>
2021-05-13 08:47:57 -0700

    nixos/modules: Use `types.ints.between` for nice level
    Signed-off-by: Pamplemousse <xav.maso@gmail.com>

--- nixos/modules/services/misc/nix-daemon.nix
+++ nixos/modules/services/misc/nix-daemon.nix
@@ -175,7 +175,7 @@ in
       };
 
       daemonNiceLevel = mkOption {
-        type = types.int;
+        type = types.ints.between (-20) 19;
         default = 0;
         description = ''
           Nix daemon process priority. This priority propagates to build processes.
