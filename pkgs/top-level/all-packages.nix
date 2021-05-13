1f0748370f4e4aec7659ae5d8dc39b8181f711f1
Vojtěch Káně <vojtech.kane@gmail.com>
2021-05-13 16:57:37 +0200

    archivebox: init at 0.6.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1045,6 +1045,8 @@ in
 
   apitrace = libsForQt514.callPackage ../applications/graphics/apitrace {};
 
+  archivebox = callPackage ../applications/backup/archivebox { };
+
   argtable = callPackage ../development/libraries/argtable { };
 
   arguments = callPackage ../development/libraries/arguments { };
