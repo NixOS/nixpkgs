905f3041f56d9a16c05078ee4aa700c009c46f7b
J. Neto <jnetod@tuta.io>
2021-05-14 03:35:59 -0300

    pythonPackages.pyfuse3: 3.1.1 -> 3.2.0

--- pkgs/development/python-modules/pyfuse3/default.nix
+++ pkgs/development/python-modules/pyfuse3/default.nix
@@ -1,21 +1,13 @@
-{ lib, buildPythonPackage, fetchPypi, fetchpatch, pkg-config, fuse3, trio, pytestCheckHook, pytest-trio, which }:
+{ lib, buildPythonPackage, fetchPypi, pkg-config, fuse3, trio, pytestCheckHook, pytest-trio, which }:
 
 buildPythonPackage rec {
   pname = "pyfuse3";
-  version = "3.1.1";
+  version = "3.2.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "9feb42a8639dc4815522ee6af6f7221552cfd2df1c7a7e9df96767be65e18667";
+    sha256 = "45f0053ad601b03a36e2c283a5271403674245a66a0daf50e3deaab0ea4fa82f";
   };
-
-  patches = [
-    # Fixes tests with pytest 6, to be removed in next stable version
-    (fetchpatch {
-      url = "https://github.com/libfuse/pyfuse3/commit/0070eddfc33fc2fba8eb4fe9353a2d2fa1ae575b.patch";
-      sha256 = "0lb4x1j31ihs3qkn61x41k2vqwcjl2fp1c2qx2jg9br6yqhjmg3b";
-    })
-  ];
 
   nativeBuildInputs = [ pkg-config ];
 
@@ -36,7 +28,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python 3 bindings for libfuse 3 with async I/O support";
     homepage = "https://github.com/libfuse/pyfuse3";
-    license = licenses.gpl2;
+    license = licenses.lgpl2Plus;
     maintainers = with maintainers; [ nyanloutre ];
   };
 }
