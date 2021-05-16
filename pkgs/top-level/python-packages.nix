480c7c395bb6876d21ed35614c444def78c21f3e
Daniël de Kok <me@danieldk.eu>
2021-05-16 09:06:47 +0200

    python3Packages.pymorphy2-dicts-ru: init at 2.4.417127.4579844
    This package provides Russian dictionaries for pymorphy2.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5851,6 +5851,8 @@ in {
 
   pymongo = callPackage ../development/python-modules/pymongo { };
 
+  pymorphy2-dicts-ru = callPackage ../development/python-modules/pymorphy2/dicts-ru.nix { };
+
   pympler = callPackage ../development/python-modules/pympler { };
 
   pymsgbox = callPackage ../development/python-modules/pymsgbox { };
