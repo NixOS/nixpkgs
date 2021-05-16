1e6a687fd0dc633a156933ad876ed1e843220f0b
Atemu <atemu.main@gmail.com>
2021-05-16 10:11:39 +0200

    linux_lqx: 5.11.20 -> 5.11.21

--- pkgs/os-specific/linux/kernel/linux-lqx.nix
+++ pkgs/os-specific/linux/kernel/linux-lqx.nix
@@ -1,7 +1,7 @@
 { lib, fetchFromGitHub, buildLinux, linux_zen, ... } @ args:
 
 let
-  version = "5.11.20";
+  version = "5.11.21";
   suffix = "lqx1";
 in
 
@@ -14,7 +14,7 @@ buildLinux (args // {
     owner = "zen-kernel";
     repo = "zen-kernel";
     rev = "v${version}-${suffix}";
-    sha256 = "0pdn3p9v3jasgf4xffirijlx20qp9gglzlksk2gmyzg9lmf5dygi";
+    sha256 = "1klvik8nrqsqi5ijnga1li4gacsknmap75g2zp8f3hd3hz18d5n1";
   };
 
   extraMeta = {
