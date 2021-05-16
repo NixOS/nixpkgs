201e1a4fb870c8b904c078f3e22790b0d873adc3
Christian Kögler <ck3d@gmx.de>
2021-05-16 08:17:25 +0200

    vdr: Update license

--- pkgs/applications/video/vdr/default.nix
+++ pkgs/applications/video/vdr/default.nix
@@ -51,6 +51,6 @@
     description = "Video Disc Recorder";
     maintainers = [ maintainers.ck3d ];
     platforms = [ "i686-linux" "x86_64-linux" ];
-    license = licenses.gpl2;
+    license = licenses.gpl2Plus;
   };
 }
