30b143b1365fc9ba145c5ba1ecfcbf65d06a2c84
R. RyanTM <ryantm-bot@ryantm.com>
2021-05-17 04:44:00 +0000

    earthly: 0.5.12 -> 0.5.13

--- pkgs/development/tools/earthly/default.nix
+++ pkgs/development/tools/earthly/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "earthly";
-  version = "0.5.12";
+  version = "0.5.13";
 
   src = fetchFromGitHub {
     owner = "earthly";
     repo = "earthly";
     rev = "v${version}";
-    sha256 = "sha256-jG4KaDCzx0PAJu6Hr+xnKsAdz97LmGUF0El3rSiLQPo=";
+    sha256 = "sha256-XN3E1oCIlohALnaP17WOB7/1vaklmyAfVZLxrBOXhbo=";
   };
 
   vendorSha256 = "sha256-q3dDV0eop2NxXHFrlppWsZrO2Hz1q5xhs1DnB6PvG9g=";
