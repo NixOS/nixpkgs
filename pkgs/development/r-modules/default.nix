7ee9a6642bf96cc92a7a2d13ca57096759a60375
Florian Beeres <yuuki@protonmail.com>
2021-05-17 17:40:04 +0200

    rPackages.RcppArmadillo: fix darwin
    RcppArmadillo requires libiconv on darwin to build successfully

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -379,6 +379,7 @@ let
   packagesWithBuildInputs = {
     # sort -t '=' -k 2
     gam = lib.optionals stdenv.isDarwin [ pkgs.libiconv ];
+    RcppArmadillo = lib.optionals stdenv.isDarwin [ pkgs.libiconv ];
     quantreg = lib.optionals stdenv.isDarwin [ pkgs.libiconv ];
     rmutil = lib.optionals stdenv.isDarwin [ pkgs.libiconv ];
     robustbase = lib.optionals stdenv.isDarwin [ pkgs.libiconv ];
