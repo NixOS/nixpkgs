e1b2a44f5d74848d0e6008cf03b051b1bc7dd3af
Eduardo Sánchez Muñoz <esm@eduardosm.net>
2021-05-17 19:47:10 +0200

    gnomeExtensions.topicons-plus: enable GNOME 40 compatibility

--- pkgs/desktops/gnome/extensions/topicons-plus/default.nix
+++ pkgs/desktops/gnome/extensions/topicons-plus/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, glib, gettext }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch, glib, gettext }:
 
 stdenv.mkDerivation rec {
   pname = "gnome-shell-extension-topicons-plus";
@@ -10,6 +10,14 @@ stdenv.mkDerivation rec {
     rev = version;
     sha256 = "1p3jlvs4zgnrvy8am7myivv4rnnshjp49kg87rd22qqyvcz51ykr";
   };
+
+  patches = [
+    # GNOME 40 compatibility (from https://github.com/phocean/TopIcons-plus/pull/156)
+    (fetchpatch {
+      url = "https://github.com/phocean/TopIcons-plus/commit/98cd17aa324a031e2ee3d344582dfdafd1e4642f.diff";
+      sha256 = "1c2j0yy7zbn6jzpqkla150gxlhgfz85y2rscx9vz7cgyzdl0q112";
+    })
+  ];
 
   buildInputs = [ glib ];
 
