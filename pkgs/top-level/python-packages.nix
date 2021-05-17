658b63c41ba909779ae93b3cdcb1155cd881d59e
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-05-17 23:01:26 +0200

    python3Packages.resolvelib: init at 0.7.0
    (cherry picked from commit 23414c5026bd1a2c612786053aa7c5a21b26378f)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7208,6 +7208,8 @@ in {
 
   resampy = callPackage ../development/python-modules/resampy { };
 
+  resolvelib = callPackage ../development/python-modules/resolvelib { };
+
   responses = callPackage ../development/python-modules/responses { };
 
   respx = callPackage ../development/python-modules/respx { };
