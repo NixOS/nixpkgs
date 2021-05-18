85fce57861bec51fbf154ebb25b91cccab2f5976
Yorick van Pelt <yorick@yorickvanpelt.nl>
2021-05-18 13:32:48 +0200

    picoscope: use nixpkgs mono, generated sources.json

--- pkgs/applications/science/electronics/picoscope/default.nix
+++ pkgs/applications/science/electronics/picoscope/default.nix
@@ -1,104 +1,124 @@
-{ stdenv, lib, fetchurl, dpkg, makeWrapper, buildFHSUserEnvBubblewrap }:
-
-with lib;
+{ stdenv, lib, fetchurl, dpkg, makeWrapper , mono, gtk-sharp-2_0
+, glib, libusb1 , zlib, gtk2-x11, gnome2, callPackage
+, scopes ? [
+  "pl1000"
+  "ps2000"
+  "ps2000a"
+  "ps3000"
+  "ps3000a"
+  "ps4000"
+  "ps4000a"
+  "ps5000"
+  "ps5000a"
+  "ps6000"
+  "ps6000a"
+  "usbdrdaq"
+] }:
 
 let
-  pkg = stdenv.mkDerivation rec {
-    pname = "picoscope";
-    # Make sure to update the dependencies to the correct versions when
-    # updating here!
-    version = "6.14.44-4r5870";
+  shared_meta = lib:
+    with lib; {
+      homepage = "https://www.picotech.com/downloads/linux";
+      maintainers = with maintainers; [ expipiplus1 yorickvp wirew0rm ];
+      platforms = [ "x86_64-linux" "armv7l-linux" ];
+      license = licenses.unfree;
+    };
 
-    srcs = let base = "https://labs.picotech.com/debian/pool/main";
-    in [
-      (fetchurl {
-        url = "${base}/p/picoscope/${pname}_${version}_all.deb";
-        sha256 = "05ryzchk6xwcv6cssik05mxhi7bp8i7szc05gjrff60bnaygx324";
-      })
-      (fetchurl {
-        url = "${base}/libp/libpicoipp/libpicoipp_1.3.0-4r78_amd64.deb";
-        sha256 = "0qd7kdiw3dxfrnd9hyxab3bb3sahyrn0pqnv0x6ca7sg47955h67";
-      })
-      (fetchurl {
-        url = "${base}/libu/libusbdrdaq/libusbdrdaq_2.0.61-1r2597_amd64.deb";
-        sha256 = "1qacqzg8mh6f3pbjnkzdq1ffj9wbmgykf75x29vd9cbdhxpxpfj4";
-      })
-      (fetchurl {
-        url = "${base}/p/picomono/picomono_4.6.2.16-1r02_amd64.deb";
-        sha256 = "0cq4pfpsx2kfqn5x4v66zc7q5czcp4r5lm4mwgbfhzkj9rz93brb";
-      })
-      (fetchurl {
-        url = "${base}/libp/libpl1000/libpl1000_2.0.61-1r2597_amd64.deb";
-        sha256 = "1b4h6wl5kan5vrwgn88mrhiimb5r2hly8a1f3j1cz9r9c68vpdy6";
-      })
-      (fetchurl {
-        url = "${base}/libp/libps2000/libps2000_3.0.63-3r2621_amd64.deb";
-        sha256 = "1b2mj9825h7mvq12mxpqxga1j5bksgn3dq46cc2wrn2kgyhgfjlv";
-      })
-      (fetchurl {
-        url = "${base}/libp/libps2000a/libps2000a_2.1.61-5r2597_amd64.deb";
-        sha256 = "12ggs2j29av33pgyclr87fd28yig35rrk730x0wl82ik0inbzv6g";
-      })
-      (fetchurl {
-        url = "${base}/libp/libps3000/libps3000_4.0.63-3r2621_amd64.deb";
-        sha256 = "0pnq4igblvcilgmfk5sja3hmh1rbdia4sjcg267wg59y8rrbhlkh";
-      })
-      (fetchurl {
-        url = "${base}/libp/libps3000a/libps3000a_2.1.61-6r2597_amd64.deb";
-        sha256 = "03dvvw0bj0p1mgf89g6f589ff6c8aq9m3ff53mz8nshwfdv4iipv";
-      })
-      (fetchurl {
-        url = "${base}/libp/libps4000/libps4000_2.1.61-2r2597_amd64.deb";
-        sha256 = "1hc3nd2wvlrhvcbilm0r7alwh72fjhgnxwym27pp7zyj0ng2kk44";
-      })
-      (fetchurl {
-        url = "${base}/libp/libps4000a/libps4000a_2.1.61-2r2597_amd64.deb";
-        sha256 = "0c7fziq09vk4r6gn8hpl8p6fpvbqbprlv34ibx1pdvkx81adcf8q";
-      })
-      (fetchurl {
-        url = "${base}/libp/libps5000/libps5000_2.1.61-3r2597_amd64.deb";
-        sha256 = "12nhjpn10ap193kzmndb5lsgfzzwh674p0igla9qxncgy0kxcazd";
-      })
-      (fetchurl {
-        url = "${base}/libp/libps5000a/libps5000a_2.1.61-5r2597_amd64.deb";
-        sha256 = "0pkccacn81mkn200igy1441mxrknzr3yi1m8ggsrhw41qyxjizbq";
-      })
-      (fetchurl {
-        url = "${base}/libp/libps6000/libps6000_2.1.61-6r2597_amd64.deb";
-        sha256 = "1bk67w8plxbp9czpsws2kzcysymw2j18467nvss7p0qvcqawy1dv";
-      })
-      (fetchurl {
-        url = "${base}/libp/libps6000a/libps6000a_1.0.61-0r2608_amd64.deb";
-        sha256 = "1ircb3ilqavm4xbpnw1ic23k1ynqi1yzy5cyf8rnhbqcwckiglsg";
-      })
-    ];
+  libpicoipp = callPackage ({ stdenv, lib, fetchurl, autoPatchelfHook, dpkg }:
+    stdenv.mkDerivation rec {
+      pname = "libpicoipp";
+      inherit (sources.libpicoipp) version;
+      src = fetchurl { inherit (sources.libpicoipp) url sha256; };
+      nativeBuildInputs = [ dpkg autoPatchelfHook ];
+      buildInputs = [ stdenv.cc.cc.lib ];
+      sourceRoot = ".";
+      unpackCmd = "dpkg-deb -x $src .";
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out/lib
+        cp -d opt/picoscope/lib/* $out/lib
+        install -Dt $out/usr/share/doc/libpicoipp usr/share/doc/libpicoipp/copyright
+        runHook postInstall
+      '';
+      meta = with lib;
+        shared_meta lib // {
+          description = "library for picotech oscilloscope software";
+        };
+    }) { };
+  sources =
+    (builtins.fromJSON (builtins.readFile ./sources.json)).${stdenv.system};
+  scopePkg = name:
+    { url, version, sha256 }:
+    stdenv.mkDerivation rec {
+      pname = "lib${name}";
+      inherit version;
+      src = fetchurl { inherit url sha256; };
+      # picoscope does a signature check, so we can't patchelf these
+      nativeBuildInputs = [ dpkg ];
+      sourceRoot = ".";
+      unpackCmd = "dpkg-deb -x $src .";
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out/lib
+        cp -d opt/picoscope/lib/* $out/lib
+         runHook postInstall
+      '';
+      meta = with lib;
+        shared_meta lib // {
+          description = "library for picotech oscilloscope ${name} series";
+        };
+    };
 
-    nativeBuildInputs = [ dpkg makeWrapper ];
+  scopePkgs = lib.mapAttrs scopePkg sources;
 
-    dontConfigure = true;
-    dontBuild = true;
+in stdenv.mkDerivation rec {
+  pname = "picoscope";
+  inherit (sources.picoscope) version;
 
-    unpackPhase = ''
-      for source in ${toString srcs}; do
-        echo "unpacking $source"
-        dpkg-deb -x $source .
-      done
-    '';
+  src = fetchurl { inherit (sources.picoscope) url sha256; };
 
-    installPhase = ''
-      mkdir -p "$out"
-      mv etc opt usr "$out"
+  nativeBuildInputs = [ dpkg makeWrapper ];
+  buildInputs = [ gtk-sharp-2_0 mono glib libusb1 zlib ];
 
-      mkdir -p "$out/bin"
-      makeWrapper "$out/opt/picomono/bin/mono" "$out/bin/picoscope" \
-        --add-flags "$out/opt/picoscope/lib/PicoScope.GTK.exe" \
-        --prefix LD_LIBRARY_PATH : ${
-          lib.makeLibraryPath [ "$out/opt/picoscope" ]
-        }
-    '';
+  unpackCmd = "dpkg-deb -x $src .";
+  sourceRoot = ".";
+  scopeLibs = lib.attrVals (map (x: "lib${x}") scopes) scopePkgs;
+  MONO_PATH = "${gtk-sharp-2_0}/lib/mono/gtk-sharp-2.0:" + (lib.makeLibraryPath
+    ([
+      glib
+      gtk2-x11
+      gnome2.libglade
+      gtk-sharp-2_0
+      libpicoipp
+      libusb1
+      zlib
+      stdenv.cc.cc.lib
+    ] ++ scopeLibs));
 
-    meta = with lib; {
-      description = "Oscilloscope application that works with all PicoScope models";
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/
+    cp -dr usr/share $out/share
+    cp -dr opt/picoscope/* $out/
+    makeWrapper "$(command -v mono)" $out/bin/picoscope \
+      --add-flags $out/lib/PicoScope.GTK.exe \
+      --prefix MONO_PATH : "$MONO_PATH" \
+      --prefix LD_LIBRARY_PATH : "$MONO_PATH"
+    runHook postInstall
+  '';
+
+  # usage:
+  # services.udev.packages = [ pkgs.picoscope.rules ];
+  # users.groups.pico = {};
+  # users.users.you.extraGroups = [ "pico" ];
+  passthru.rules = lib.writeTextDir "lib/udev/rules.d/95-pico.rules" ''
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="0ce9", MODE="664",GROUP="pico"
+  '';
+
+  meta = with lib;
+    shared_meta lib // {
+      description =
+        "Oscilloscope application that works with all PicoScope models";
       longDescription = ''
         PicoScope for Linux is a powerful oscilloscope application that works
         with all PicoScope models. The most important features from PicoScope
@@ -110,22 +130,6 @@ let
         PicoScope for Linux, PicoScope for macOS and PicoScope for Windows
         users, or exported in text, CSV and MathWorks MATLAB 4 formats.
       '';
-      homepage = "https://www.picotech.com/downloads/linux";
-      license = licenses.unfree;
-      platforms = platforms.linux;
-      maintainers = with maintainers; [ expipiplus1 wirew0rm ];
     };
-  }
-
-  ;
-
-in buildFHSUserEnvBubblewrap {
-  name = "picoscope";
-  targetPkgs = pkgs:
-    (with pkgs; [ pkg glib gtk2 libusb1 gtk-sharp-2_0 libpng12 ]);
-  runScript = "${pkg}/bin/${pkg.pname}";
-  extraBuildCommands = ''
-    ln -s ${pkg}/opt
-  '';
 }
 
