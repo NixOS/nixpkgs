a3fd86a3ba098051edf7edae5c630829c0e1279e
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-05-18 16:59:23 +0200

    haskellPackages: disable some more failing doctests on aarch64
    This should conclude a pass of direct aarch64-linux failures related to
    this issue on hydra. Subsequent evaluation may of course reveal more.

--- pkgs/development/haskell-modules/configuration-arm.nix
+++ pkgs/development/haskell-modules/configuration-arm.nix
@@ -63,6 +63,25 @@ self: super: {
   openapi3 = dontCheck super.openapi3;
   strict-writer = dontCheck super.strict-writer;
   xml-html-qq = dontCheck super.xml-html-qq;
+  static = dontCheck super.static;
+  hhp = dontCheck super.hhp;
+  groupBy = dontCheck super.groupBy;
+  greskell = dontCheck super.greskell;
+  html-validator-cli = dontCheck super.html-validator-cli;
+  hw-fingertree-strict = dontCheck super.hw-fingertree-strict;
+  hw-prim = dontCheck super.hw-prim;
+  lens-regex = dontCheck super.lens-regex;
+  meep = dontCheck super.meep;
+  ranged-list = dontCheck super.ranged-list;
+  rank2classes = dontCheck super.rank2classes;
+  schedule = dontCheck super.schedule;
+  twiml = dontCheck super.twiml;
+  twitter-conduit = dontCheck super.twitter-conduit;
+  validationt = dontCheck super.validationt;
+  vgrep = dontCheck super.vgrep;
+  vulkan-utils = dontCheck super.vulkan-utils;
+  yaml-combinators = dontCheck super.yaml-combinators;
+  yesod-paginator = dontCheck super.yesod-paginator;
 
   # https://github.com/ekmett/half/issues/35
   half = dontCheck super.half;
