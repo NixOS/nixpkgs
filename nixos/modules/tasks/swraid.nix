fe49099ea418ff9956b92057b5a9d584b4f51708
Christian Theune <christian@theune.cc>
2021-05-18 10:30:03 +0200

    Work on fix for #72394

--- nixos/modules/tasks/swraid.nix
+++ nixos/modules/tasks/swraid.nix
@@ -1,17 +1,50 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 
+with lib;
+let cfg = config.services.mdadm;
+in
 {
 
-  environment.systemPackages = [ pkgs.mdadm ];
+  options = {
 
-  services.udev.packages = [ pkgs.mdadm ];
+    services.mdadm = {
 
-  systemd.packages = [ pkgs.mdadm ];
+      monitor = {
+        enable = mkEnableOption "Enable the mdadm monitoring daemon";
+      };
 
-  boot.initrd.availableKernelModules = [ "md_mod" "raid0" "raid1" "raid10" "raid456" ];
+      adminAddr = mkOption {
+        type = types.str;
+        example = "admin@example.org";
+        description = "E-mail address of the server administrator.";
+      };
+    };
+  };
 
-  boot.initrd.extraUdevRulesCommands = ''
-    cp -v ${pkgs.mdadm}/lib/udev/rules.d/*.rules $out/
-  '';
+  config = mkMerge [
+    {
+      environment.systemPackages = [ pkgs.mdadm ];
+
+      services.udev.packages = [ pkgs.mdadm ];
+
+      systemd.packages = [ pkgs.mdadm ];
+
+      boot.initrd.availableKernelModules = [ "md_mod" "raid0" "raid1" "raid10" "raid456" ];
+
+      boot.initrd.extraUdevRulesCommands = ''
+        cp -v ${pkgs.mdadm}/lib/udev/rules.d/*.rules $out/
+      '';
+    }
+
+    (mkIf cfg.monitor.enable
+        {
+          systemd.services.mdadm-monitor = {
+            description = "Monitor RAID disks";
+            wantedBy = [ "multi-user.target" ];
+            script = "${pkgs.mdadm}/bin/mdadm --monitor -m ${assert cfg.adminAddr != null; cfg.adminAddr} --scan";
+          };
+        }
+    )
+  ];
 
 }
