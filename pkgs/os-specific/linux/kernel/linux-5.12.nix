1e4ff1a71eea4eedf4916eceb9df01544a99cab9
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-05-19 11:55:45 +0200

    linux: 5.12.4 -> 5.12.5
    (cherry picked from commit a76a720665423d32db1edaf44915287c5860d536)

--- pkgs/os-specific/linux/kernel/linux-5.12.nix
+++ pkgs/os-specific/linux/kernel/linux-5.12.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.12.4";
+  version = "5.12.5";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,7 +13,7 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0wv89gwf5v8m7wi2f3bv9mdr8n9raq998sy4m1m2lwwjhkpgwq2s";
+    sha256 = "1x39sdswww4j8zr54wpjzy9dia52kihs11xwljxcnz8pck0vwja0";
   };
 
   kernelTests = args.kernelTests or [ nixosTests.kernel-generic.linux_5_12 ];
