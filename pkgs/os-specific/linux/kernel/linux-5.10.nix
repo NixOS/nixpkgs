eb14ecabeec2b2a34d1bc4f290995e4547f61780
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-05-19 11:55:35 +0200

    linux: 5.10.37 -> 5.10.38
    (cherry picked from commit 9a0a33f6b1ca2ae2c70e0c01722e3c359d667578)

--- pkgs/os-specific/linux/kernel/linux-5.10.nix
+++ pkgs/os-specific/linux/kernel/linux-5.10.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.10.37";
+  version = "5.10.38";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0xz01g017s9kcc9awlg6p9wrm8pzxyk4fizrf3mq9i5gklqf7md8";
+    sha256 = "0bzipgnfvd5w70rv1x6g81iwknn2qskw92p2bpyiks7r52qmmwij";
   };
 } // (args.argsOverride or {}))
