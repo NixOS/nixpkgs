9bc16d2c92c880d97218c87398dc872efe2dbb9c
Mica Semrick <mica@silentumbrella.com>
2021-05-18 19:42:40 -0700

    wlc: init at 1.11

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27039,6 +27039,8 @@ in
 
   write_stylus = libsForQt5.callPackage ../applications/graphics/write_stylus { };
 
+  wlc = callPackage  ../tools/misc/wlc { };
+
   wllvm = callPackage  ../development/tools/wllvm { };
 
   wmname = callPackage ../applications/misc/wmname { };
