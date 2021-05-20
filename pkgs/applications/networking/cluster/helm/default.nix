cc2f1d5bae49afc1257506c2491f09ff2996865d
Eric Bailey <e.bailey@sportradar.com>
2021-05-19 19:23:01 -0500

    kubernetes-helm: add updateScript
    Also fix meta.homepage.

--- pkgs/applications/networking/cluster/helm/default.nix
+++ pkgs/applications/networking/cluster/helm/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildGoModule, fetchFromGitHub, installShellFiles }:
+{ lib, buildGoModule, fetchFromGitHub, writeScript, installShellFiles }:
 
 buildGoModule rec {
   pname = "helm";
@@ -24,8 +24,17 @@ buildGoModule rec {
     installShellCompletion helm.{bash,zsh}
   '';
 
+  passthru.updateScript = writeScript "update-helm" ''
+    #! /usr/bin/env nix-shell
+    #! nix-shell -i bash -p common-updater-scripts curl findutils
+    set -eufo pipefail
+    curl -s https://api.github.com/repos/helm/helm/releases/latest |
+        jq -e -r '.tag_name | ltrimstr("v")' |
+        xargs update-source-version kubernetes-helm
+  '';
+
   meta = with lib; {
-    homepage = "https://github.com/kubernetes/helm";
+    inherit (src.meta) homepage;
     description = "A package manager for kubernetes";
     license = licenses.asl20;
     maintainers = with maintainers; [ rlupton20 edude03 saschagrunert Frostman Chili-Man ];
