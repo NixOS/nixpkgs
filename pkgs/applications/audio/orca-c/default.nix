cea842ddfa44da6ecdce37b64d7a9399ef9ae4e0
netcrns <jason.wing@gmx.de>
2021-05-20 19:49:37 +0200

    orca-c: explicitly call `preInstall` & `postInstall` hooks during `installPhase`

--- pkgs/applications/audio/orca-c/default.nix
+++ pkgs/applications/audio/orca-c/default.nix
@@ -17,8 +17,12 @@ stdenv.mkDerivation {
   '';
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/bin
     install build/orca $out/bin/orca
+
+    runHook postInstall
   '';
 
   meta = with lib; {
