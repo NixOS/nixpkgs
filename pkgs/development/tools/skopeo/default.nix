0afb6b137db4cb85264fafe6c75b2fb7c1d2a834
Sascha Grunert <sgrunert@redhat.com>
2021-05-20 10:23:39 +0200

    skopeo: 1.2.3 -> 1.3.0
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- pkgs/development/tools/skopeo/default.nix
+++ pkgs/development/tools/skopeo/default.nix
@@ -14,13 +14,13 @@
 
 buildGoModule rec {
   pname = "skopeo";
-  version = "1.2.3";
+  version = "1.3.0";
 
   src = fetchFromGitHub {
     rev = "v${version}";
     owner = "containers";
     repo = "skopeo";
-    sha256 = "sha256-GhLw8wt5eDixKNGtxGA0Fjw3auQ3AsjKa+0M4mLTQlg=";
+    sha256 = "sha256-ZHEujkl+GUk5WjgDWdbJwOIKuOqJnIpGnvD1SsrHuhI=";
   };
 
   outputs = [ "out" "man" ];
