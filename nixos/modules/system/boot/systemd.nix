b61e7a617ed76322dc5545a0501c04f22bf4841e
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-05-21 13:07:15 +0200

    Revert "Merge pull request #85073 from hyperfekt/systemd-pstore"
    This reverts commit 6c0058f47ffa35b4803ae092efbb4dc467676bb4, reversing
    changes made to fb637b899a9b6ccb1cb47e5916a7c94b686c554a.

--- nixos/modules/system/boot/systemd.nix
+++ nixos/modules/system/boot/systemd.nix
@@ -90,7 +90,6 @@ let
       "systemd-fsck@.service"
       "systemd-fsck-root.service"
       "systemd-remount-fs.service"
-      "systemd-pstore.service"
       "local-fs.target"
       "local-fs-pre.target"
       "remote-fs.target"
@@ -1184,7 +1183,6 @@ in
     systemd.targets.remote-fs.unitConfig.X-StopOnReconfiguration = true;
     systemd.targets.network-online.wantedBy = [ "multi-user.target" ];
     systemd.services.systemd-importd.environment = proxy_env;
-    systemd.services.systemd-pstore.wantedBy = [ "sysinit.target" ]; # see #81138
 
     # Don't bother with certain units in containers.
     systemd.services.systemd-remount-fs.unitConfig.ConditionVirtualization = "!container";
