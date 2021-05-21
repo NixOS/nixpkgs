6ff71447d9626285600a5b79436ddd622bf97a7a
Bart Brouns <bart@magnetophon.nl>
2021-05-21 23:13:33 +0200

    WIP: ChowKick: init at unstable-2021-05-17
    Standalone segfaults

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22673,6 +22673,8 @@ in
 
   bookworm = callPackage ../applications/office/bookworm { };
 
+  ChowKick  = callPackage ../applications/audio/ChowKick { };
+
   CHOWTapeModel = callPackage ../applications/audio/CHOWTapeModel { };
 
   chromium = callPackage ../applications/networking/browsers/chromium (config.chromium or {});
