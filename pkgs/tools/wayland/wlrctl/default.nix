49b31f8b00da0b42661752e56ab27ecd0700cfe7
Brian McKenna <brian@brianmckenna.org>
2021-05-21 10:13:05 +1000

    wlrctl: init at 0.2.1

---
+++ pkgs/tools/wayland/wlrctl/default.nix
@@ -0,0 +1,23 @@
+{ lib, stdenv, fetchFromSourcehut, cmake, meson, pkg-config, libxkbcommon, wayland, scdoc, ninja }:
+
+stdenv.mkDerivation rec {
+  name = "wlrctl-${version}";
+  version = "0.2.1";
+  src = fetchFromSourcehut {
+    owner = "~brocellous";
+    repo = "wlrctl";
+    rev = "v${version}";
+    sha256 = "039cxc82k7x473n6d65jray90rj35qmfdmr390zy0c7ic7vn4b78";
+  };
+  dontUseCmakeConfigure = true;
+  nativeBuildInputs = [ cmake meson pkg-config scdoc ninja ];
+  buildInputs = [ libxkbcommon wayland ];
+
+  meta = with lib; {
+    description = "Command line utility for miscellaneous wlroots Wayland extensions";
+    homepage = "https://git.sr.ht/~brocellous/wlrctl";
+    license = licenses.mit;
+    maintainers = with maintainers; [ puffnfresh ];
+    platforms = platforms.linux;
+  };
+}
