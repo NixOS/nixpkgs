9c7b0d0698d4830d37b58baf9bc9f193d3fdcfb7
R. RyanTM <ryantm-bot@ryantm.com>
2021-05-22 04:13:15 +0000

    git-quick-stats: 2.1.9 -> 2.2.0

--- pkgs/development/tools/git-quick-stats/default.nix
+++ pkgs/development/tools/git-quick-stats/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "git-quick-stats";
-  version = "2.1.9";
+  version = "2.2.0";
 
   src = fetchFromGitHub {
     repo = "git-quick-stats";
     owner = "arzzen";
     rev = version;
-    sha256 = "sha256-2rrwbEXwBBuussybCZFbAEjNwm/ztbXw1jUlTnxPINA=";
+    sha256 = "sha256-cE9eo8FUUNlhmBrHalFDTFK2LIQUaWFy7Gj/E2F15t4=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
