18e4425e23f85a7ecf56a3e7083664ef97ef74fa
Stéphan Kochen <git@stephank.nl>
2021-05-22 21:04:35 +0200

    reproxy: fix darwin build

--- pkgs/servers/reproxy/default.nix
+++ pkgs/servers/reproxy/default.nix
@@ -15,6 +15,12 @@ buildGoModule rec {
     # Requires network access
     substituteInPlace app/main_test.go \
       --replace "Test_Main" "Skip_Main"
+
+    # Fails on Darwin.
+    # https://github.com/umputun/reproxy/issues/77
+    substituteInPlace app/discovery/provider/file_test.go \
+      --replace "TestFile_Events" "SkipFile_Events" \
+      --replace "TestFile_Events_BusyListener" "SkipFile_Events_BusyListener"
   '';
 
   vendorSha256 = null;
