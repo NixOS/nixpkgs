b6d5ca37308bd56ddd9b5fd507ce95c7f4c96334
Enno Richter <enno@nerdworks.de>
2021-05-23 16:43:00 +0200

    ppsspp: fix build for ffmpeg>=4.3

--- pkgs/misc/emulators/ppsspp/default.nix
+++ pkgs/misc/emulators/ppsspp/default.nix
@@ -1,5 +1,6 @@
 { mkDerivation
 , fetchFromGitHub
+, fetchpatch
 , SDL2
 , cmake
 , ffmpeg
@@ -25,6 +26,14 @@ mkDerivation rec {
     fetchSubmodules = true;
     sha256 = "sha256-vfp/vacIItlPP5dR7jzDT7oOUNFnjvvdR46yi79EJKU=";
   };
+
+  patches = [
+    # adds ffmpeg 4.3 compatibility, remove for ppsspp 1.12
+    (fetchpatch {
+      url = "https://github.com/hrydgard/ppsspp/commit/8a69c3d1226fe174c49437514a2d3ca7e411c3fa.patch";
+      sha256 = "sha256-ecDoOydaLfL6+eFpahcO1TnRl866mZZVHlr6Qrib1mo=";
+    })
+  ];
 
   postPatch = ''
     substituteInPlace git-version.cmake --replace unknown ${src.rev}
