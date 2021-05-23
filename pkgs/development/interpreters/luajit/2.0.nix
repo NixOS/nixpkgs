e0b048da92b619303c97f9cf2f7c2f045d1b2743
Luka Blaskovic <lblasc@tvbeat.com>
2021-05-23 08:20:04 +0000

    luajit_2_0: 2.0.5-2020-12-28 -> 2.0.5-2021-05-17
    Changes since last update:
    https://github.com/LuaJIT/LuaJIT/compare/56c04accf975bff2519c34721dccbbdb7b8e6963...44684fa71d8af6fa8b3051c7d763bbfdcf7915d7

--- pkgs/development/interpreters/luajit/2.0.nix
+++ pkgs/development/interpreters/luajit/2.0.nix
@@ -1,10 +1,10 @@
 { self, callPackage, lib }:
 callPackage ./default.nix {
   inherit self;
-  version = "2.0.5-2020-12-28";
-  rev = "56c04accf975bff2519c34721dccbbdb7b8e6963";
+  version = "2.0.5-2021-05-17";
+  rev = "44684fa71d8af6fa8b3051c7d763bbfdcf7915d7";
   isStable = true;
-  sha256 = "0mmx7dy843iqdpyxxdfks860np0311gg58gi4zczx10h62y6jacm";
+  sha256 = "049d3l0miv4n0cnm35ml8flrb9vs12zvbda2743vypckymidliqp";
   extraMeta = { # this isn't precise but it at least stops the useless Hydra build
     platforms = with lib; filter (p: p != "aarch64-linux")
       (platforms.linux ++ platforms.darwin);
