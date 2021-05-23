0afbd6c86a29160386e5386332b65ba707a25340
Niklas Hambüchen <mail@nh2.me>
2021-05-23 16:25:52 +0200

    libredirect: Enable debug symbols

--- pkgs/build-support/libredirect/default.nix
+++ pkgs/build-support/libredirect/default.nix
@@ -28,6 +28,11 @@ stdenv.mkDerivation rec {
     runHook postBuild
   '';
 
+  # We want to retain debugging info to be able to use GDB on libredirect.so
+  # to more easily investigate which function overrides are missing or why
+  # existing ones do not have the intended effect.
+  dontStrip = true;
+
   installPhase = ''
     runHook preInstall
 
