b1581fc209667c57486a4ce2b4a23af56330502f
Tom McLaughlin <tom@codedown.io>
2021-05-23 13:15:14 -0600

    Add pythonImportsCheck and quote URL

--- pkgs/development/python-modules/powerline-mem-segment/default.nix
+++ pkgs/development/python-modules/powerline-mem-segment/default.nix
@@ -15,9 +15,11 @@ buildPythonPackage rec {
 
   propagatedBuildInputs = [ psutil ];
 
+  pythonImportsCheck = [ "powerlinemem" ];
+
   meta = with lib; {
     description = "Segment for Powerline showing the current memory usage in percent or absolute values.";
-    homepage = https://github.com/mKaloer/powerline_mem_segment;
+    homepage = "https://github.com/mKaloer/powerline_mem_segment";
     license = licenses.asl20;
     maintainers = with maintainers; [ thomasjm ];
   };
