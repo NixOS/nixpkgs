20f8b8ff343e0015352f1b2788fafc5f64c43051
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-05-24 18:53:35 +0200

    linux: 4.14.232 -> 4.14.233
    (cherry picked from commit c5545b7e423869aaabc1232e9f339a5cb18398db)

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.14.232";
+  version = "4.14.233";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0sa3sz7bznlhijd0iwv37nyrrnw34iq6dq1bqr6cj2wpyrhr7h8x";
+    sha256 = "0sqikz8cvrjfs46mcqx9gbdfkpdims0m5xxxzwr5vcfph5rdvj26";
   };
 } // (args.argsOverride or {}))
