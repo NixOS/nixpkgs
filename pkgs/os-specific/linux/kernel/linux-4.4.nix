d342aa61cd78db7265186da3f2e2406da7937a97
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-05-24 18:53:44 +0200

    linux: 4.4.268 -> 4.4.269
    (cherry picked from commit 7d15d507177349feee82e225a2ddbb09a9830d47)

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.268";
+  version = "4.4.269";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1srk08kaxq5jjlqx804cgjffhcsrdkv3idh8ipagl6v2w4kas5v8";
+    sha256 = "0qx5zwi2ijwv9jwhs4cz91z7yxy6nd0g8ryzrlg1ar2xyk8w4yh4";
   };
 } // (args.argsOverride or {}))
