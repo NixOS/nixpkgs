2a5cd921e2cc298f9689cf33718f332cefba2b1f
Jonathan Ringer <jonringer117@gmail.com>
2021-05-25 15:23:06 -0700

    linuxPackages.ati_drivers_x11: remove, causing docs to fail

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17823,8 +17823,6 @@ in
 
     bbswitch = callPackage ../os-specific/linux/bbswitch {};
 
-    ati_drivers_x11 = throw "ati drivers are no longer supported by any kernel >=4.1"; # added 2021-05-18
-
     chipsec = callPackage ../tools/security/chipsec {
       inherit kernel;
       withDriver = true;
