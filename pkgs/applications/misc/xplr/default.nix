47cb91e633ae6f4b28fb6abc8fc262ae483244cb
Arijit Basu <sayanarijit@gmail.com>
2021-05-25 15:12:25 +0530

    xplr: 0.10.1 -> 0.12.0

--- pkgs/applications/misc/xplr/default.nix
+++ pkgs/applications/misc/xplr/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "xplr";
-  version = "0.10.1";
+  version = "0.12.0";
 
   src = fetchCrate {
     inherit pname version;
-    sha256 = "17x514pwbkzkkrd47a66a4iz3bxrxvm8hk5hphsfbhgzqfnf9iy7";
+    sha256 = "16w1lkc13qaq9zqpyfkpalcsa2050v24klvpghvfxwrriixhl34f";
   };
 
   buildInputs = lib.optional stdenv.isDarwin libiconv;
 
-  cargoSha256 = "03y2fk174rdjvw8wdzwc0hhj0zqwpap7qcga51yhq877rgyxbxir";
+  cargoSha256 = "1xlmycf15j9igraw9s3qh2br46vyhlp128syh57zlssizqbgks16";
 
   meta = with lib; {
     description = "A hackable, minimal, fast TUI file explorer";
