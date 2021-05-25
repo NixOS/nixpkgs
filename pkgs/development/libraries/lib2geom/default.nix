ff8752a60361623bdb01f3534f12d503cc854c8e
Jordan Justen <jljusten@gmail.com>
2021-05-25 00:28:26 -0700

    lib2geom: update 2020-03-12 -> 1.1
    Reworks:
     * lgpl21 => lgpl21Only (jtojnar)
     * stdenv.lib => lib (jtojnar)
     * Use 1.1 tagged version rather than sha1 from inkscape 1.1 (jtojnar)
     * BUILD_SHARED_LIBS => 2GEOM_BUILD_SHARED (jtojnar)
     * Drop cmakeBuildType (jtojnar)

--- pkgs/development/libraries/lib2geom/default.nix
+++ pkgs/development/libraries/lib2geom/default.nix
@@ -1,6 +1,5 @@
 { stdenv
 , fetchFromGitLab
-, fetchpatch
 , cmake
 , ninja
 , pkg-config
@@ -10,30 +9,21 @@
 , cairo
 , double-conversion
 , gtest
+, lib
 }:
 
 stdenv.mkDerivation rec {
-  pname = "lib2geom-unstable";
-  version = "2020-03-12";
+  pname = "lib2geom";
+  version = "1.1";
 
   outputs = [ "out" "dev" ];
 
   src = fetchFromGitLab {
     owner = "inkscape";
     repo = "lib2geom";
-    rev = "226eb8c60f2af639d74a0229c0ba90e649e6451d";
-    sha256 = "BSuqasBfig6HiKY/xtJm7CjbSaV8cW45ip59iEO5Es4=";
+    rev = "refs/tags/${version}";
+    sha256 = "sha256-u9pbpwVzAXzrM2/tQnd1B6Jo9Fzg6UZBr9AtUsNMfQ0=";
   };
-
-  patches = [
-    # Re-enable assertions for tests to work
-    # https://gitlab.com/inkscape/lib2geom/issues/5
-    # https://gitlab.com/inkscape/lib2geom/merge_requests/17
-    (fetchpatch {
-      url = "https://gitlab.com/inkscape/lib2geom/commit/4aa78f52232682b353eb15c219171e466987bac7.patch";
-      sha256 = "XsX8SPft0RwDemJujc8lierBe4s3iw8YkW4CSlY5LsY=";
-    })
-  ];
 
   nativeBuildInputs = [
     cmake
@@ -53,19 +43,17 @@ stdenv.mkDerivation rec {
     gtest
   ];
 
-  cmakeBuildType = "RelWithDebugInfo"; # needed to keep assertions for tests working
-
   cmakeFlags = [
     "-DCMAKE_SKIP_BUILD_RPATH=OFF" # for tests
-    "-DBUILD_SHARED_LIBS=ON"
+    "-D2GEOM_BUILD_SHARED=ON"
   ];
 
   doCheck = true;
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Easy to use 2D geometry library in C++";
     homepage = "https://gitlab.com/inkscape/lib2geom";
-    license = [ licenses.lgpl21 licenses.mpl11 ];
+    license = [ licenses.lgpl21Only licenses.mpl11 ];
     maintainers = with maintainers; [ jtojnar ];
     platforms = platforms.unix;
   };
