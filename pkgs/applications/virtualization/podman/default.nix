e1d59ae8d4c839542607b76218bd9a66ce3f4e20
Nick Cao <nickcao@nichi.co>
2021-05-26 23:53:52 +0800

    Revert "podman: add systemd to LD_LIBRARY_PATH for journald log driver"
    This reverts commit b71086499de8644780bbe59dd31d1eb3d04f3c06.

--- pkgs/applications/virtualization/podman/default.nix
+++ pkgs/applications/virtualization/podman/default.nix
@@ -3,7 +3,6 @@
 , fetchFromGitHub
 , pkg-config
 , installShellFiles
-, makeWrapper
 , buildGoModule
 , gpgme
 , lvm2
@@ -37,7 +36,7 @@ buildGoModule rec {
 
   outputs = [ "out" "man" ];
 
-  nativeBuildInputs = [ pkg-config go-md2man installShellFiles makeWrapper ];
+  nativeBuildInputs = [ pkg-config go-md2man installShellFiles ];
 
   buildInputs = lib.optionals stdenv.isLinux [
     btrfs-progs
@@ -70,8 +69,6 @@ buildGoModule rec {
     installShellCompletion --zsh completions/zsh/*
     MANDIR=$man/share/man make install.man-nobuild
   '' + lib.optionalString stdenv.isLinux ''
-    wrapProgram $out/bin/podman \
-      --prefix LD_LIBRARY_PATH : "${lib.getLib systemd}/lib"
     install -Dm644 contrib/tmpfile/podman.conf -t $out/lib/tmpfiles.d
     install -Dm644 contrib/systemd/system/podman.{socket,service} -t $out/lib/systemd/system
   '' + ''
