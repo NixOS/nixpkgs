32c5e04919862b40b4048402a0f395750b2df906
zowoq <59103226+zowoq@users.noreply.github.com>
2021-05-27 09:59:49 +1000

    docker: add clientOnly / docker-client
    Currently the docker client is only available on non-linux platforms as `docker`,
    this makes the client available on linux and other platforms as `docker-client`.
    
    (cherry picked from commit 7233acd515546107e5902a8fef6832f4f319390c)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23020,6 +23020,7 @@ in
 
   docker = docker_20_10;
   docker-edge = docker_20_10;
+  docker-client = docker.override { clientOnly = true; };
 
   docker-proxy = callPackage ../applications/virtualization/docker/proxy.nix { };
 
