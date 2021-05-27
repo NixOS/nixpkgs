14e9c2bdd4ab7a82c6d77d9297ecb6a1cf09a8ce
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-05-27 06:27:51 +0200

    linux: 4.19.191 -> 4.19.192
    (cherry picked from commit bd12ba96439e67cf75ec94773b7b2b5fc5f6079b)

--- pkgs/os-specific/linux/kernel/linux-4.19.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.19.191";
+  version = "4.19.192";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1jd97rzv900mnyzps4xmf6k166zrg391vcrbmr5zvcway5qbn9mm";
+    sha256 = "1680dz4wjg721a8lqwh6ijd90zcr2f3wzcvhwgn3shbwgjkwsk87";
   };
 } // (args.argsOverride or {}))
