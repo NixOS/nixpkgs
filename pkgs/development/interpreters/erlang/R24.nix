8ed2973cb1a664d272de9fea25fc31906545cfa7
R. RyanTM <ryantm-bot@ryantm.com>
2021-05-28 14:08:33 +0000

    erlangR24: 24.0 -> 24.0.1

--- pkgs/development/interpreters/erlang/R24.nix
+++ pkgs/development/interpreters/erlang/R24.nix
@@ -3,6 +3,6 @@
 # How to obtain `sha256`:
 # nix-prefetch-url --unpack https://github.com/erlang/otp/archive/OTP-${version}.tar.gz
 mkDerivation {
-  version = "24.0";
-  sha256 = "0p4p920ncsvls9q3czdc7wz2p7m15bi3nr4306hqddnxz1kxcm4w";
+  version = "24.0.1";
+  sha256 = "z01gaKNkKsIPKdPKhX6IUUY9uBSuyA33E4aVM0MdXsg=";
 }
