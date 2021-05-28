3c27f597e6a63c176aa3e304c54df7c12b086eb6
R. RyanTM <ryantm-bot@ryantm.com>
2021-05-28 08:36:10 +0000

    aws-c-cal: 0.4.5 -> 0.5.8

--- pkgs/development/libraries/aws-c-cal/default.nix
+++ pkgs/development/libraries/aws-c-cal/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "aws-c-cal";
-  version = "0.4.5";
+  version = "0.5.8";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = pname;
     rev = "v${version}";
-    sha256 = "04acra1mnzw9q7jycs5966akfbgnx96hkrq90nq0dhw8pvarlyv6";
+    sha256 = "sha256-+4RNZ7q+KKlf23Bol4DWvDJq/r6WtCOi0356+YsbDa0=";
   };
 
   nativeBuildInputs = [ cmake ];
