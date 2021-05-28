2da72b888a449033705b1a7d3820f08ca716c6ca
happysalada <raphael@megzari.com>
2021-05-28 21:19:26 +0900

    relxExe: 3.32.1 -> 4.3.0

--- pkgs/development/tools/erlang/relx-exe/default.nix
+++ pkgs/development/tools/erlang/relx-exe/default.nix
@@ -2,18 +2,18 @@
 
 rebar3Relx rec {
   name = "relx-exe";
-  version = "3.32.1";
+  version = "4.3.0";
   releaseType = "escript";
 
   src = fetchHex {
     pkg = "relx";
-    sha256 = "0693k8ac7hvpm9jd3ysbdn8bk97d68ini22p1fsqdsi9qv9f7nq7";
+    sha256 = "sha256-c44JSab8fQ3p5FSdwPc9m24FtTnhURuySFkHArMiBEA=";
     inherit version;
   };
 
   checkouts = fetchRebar3Deps {
     inherit name version;
     src = "${src}/rebar.lock";
-    sha256 = "0l7r3x7zwcz49013zv8z5v2i06p7wqkgzdyzrl8jk0hglscvhpf6";
+    sha256 = "sha256-BY/c4+vqdnn4FBPzLExEZIZ27f1g46XISb8mIpMU8aI=";
   };
 }
