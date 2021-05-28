9589fb021345f9ba6d62adb69a3950af762c3ec0
R. RyanTM <ryantm-bot@ryantm.com>
2021-05-28 00:40:16 +0000

    agi: 1.1.0-dev-20210521 -> 1.1.0-dev-20210526

--- pkgs/tools/graphics/agi/default.nix
+++ pkgs/tools/graphics/agi/default.nix
@@ -14,11 +14,11 @@
 
 stdenv.mkDerivation rec {
   pname = "agi";
-  version = "1.1.0-dev-20210521";
+  version = "1.1.0-dev-20210526";
 
   src = fetchzip {
     url = "https://github.com/google/agi-dev-releases/releases/download/v${version}/agi-${version}-linux.zip";
-    sha256 = "sha256-otdthD5p+12JmBltFtXgVaa1fgsItluHv0S4k/GbB9Q=";
+    sha256 = "sha256-A2qMb7wUzy4FONdBYRLYsgLy7ffGvzu4OcKzWFTbsog=";
   };
 
   nativeBuildInputs = [
