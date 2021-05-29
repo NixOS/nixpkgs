0885a2d40fe1e01e80481b3d649ddb1defbe68ef
Devin Singh <drsingh2518@icloud.com>
2021-05-29 08:37:06 -0500

    bunnyfetch: init at unstable-2021-05-24

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31616,6 +31616,8 @@ in
 
   kodelife = callPackage ../applications/graphics/kodelife {};
 
+  bunnyfetch = callPackage ../tools/misc/bunnyfetch {};
+
   _3proxy = callPackage ../applications/networking/3proxy {};
 
   pigeon = callPackage ../development/tools/pigeon {};
