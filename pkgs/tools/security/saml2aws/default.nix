fe6ce7d950c1e49ff3cba7b819d9a489ee99fde6
Ari Becker <ari-becker@users.noreply.github.com>
2021-05-29 12:18:02 +0300

    saml2aws: 2.28.0 -> 2.30.0

--- pkgs/tools/security/saml2aws/default.nix
+++ pkgs/tools/security/saml2aws/default.nix
@@ -2,24 +2,26 @@
 
 buildGoModule rec {
   pname = "saml2aws";
-  version = "2.28.0";
+  version = "2.30.0";
 
   src = fetchFromGitHub {
     owner = "Versent";
     repo = "saml2aws";
     rev = "v${version}";
-    sha256 = "sha256-2t1MytLjAxhVVsWyMYcQZ9c+ox+X2OszG5mLAv8c7xE=";
+    sha256 = "0plwh7dswf756xy048dqycc2nlpnzk3d1s9m6ypgi77vydryzhj3";
   };
 
   runVend = true;
-  vendorSha256 = "sha256-8Kox01iyWhv/Fp7jHPeNXxc/K2TT1WPyWFieHZkqLho=";
+  vendorSha256 = lib.fakeSha256;
 
   doCheck = false;
 
   subPackages = [ "." "cmd/saml2aws" ];
 
   buildFlagsArray = ''
-    -ldflags=-X main.Version=${version}
+    -v
+    -trimpath
+    -ldflags=-s -w -X main.Version=${version} -X main.commit=${src.sha256}
   '';
 
   meta = with lib; {
