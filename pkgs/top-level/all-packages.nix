acf2a84d49f0e1da442be78864ac85f2b29328f9
Jacek Galowicz <jacek@galowicz.de>
2021-05-30 10:26:09 +0000

    ustreamer: init 3.27

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9361,6 +9361,8 @@ in
 
   usbmuxd = callPackage ../tools/misc/usbmuxd {};
 
+  ustreamer = callPackage ../applications/video/ustreamer { };
+
   usync = callPackage ../applications/misc/usync { };
 
   uwc = callPackage ../tools/text/uwc { };
