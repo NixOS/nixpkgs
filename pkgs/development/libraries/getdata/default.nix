1b77912d10ba5a5e8ddf7853f00ac3cb841d59bf
Sandro <sandro.jaeckel@gmail.com>
2021-05-30 03:43:56 +0200

    Update pkgs/development/libraries/getdata/default.nix

--- pkgs/development/libraries/getdata/default.nix
+++ pkgs/development/libraries/getdata/default.nix
@@ -7,10 +7,12 @@ stdenv.mkDerivation rec {
     sha256 = "18xbb32vygav9x6yz0gdklif4chjskmkgp06rwnjdf9myhia0iym";
   };
 
-  patches = [ (fetchpatch {
-    url = "https://sources.debian.org/data/main/libg/libgetdata/0.10.0-10/debian/patches/CVE-2021-20204.patch";
-    sha256 = "1lvp1c2pkk9kxniwlvax6d8fsmjrkpxawf71c7j4rfjm6dgvivzm";
-  })];
+  patches = [
+    (fetchpatch {
+      url = "https://sources.debian.org/data/main/libg/libgetdata/0.10.0-10/debian/patches/CVE-2021-20204.patch";
+      sha256 = "1lvp1c2pkk9kxniwlvax6d8fsmjrkpxawf71c7j4rfjm6dgvivzm";
+    })
+  ];
 
   buildInputs = [ libtool ];
 
