83099ae2b2429ab89082e557a48a3f8bc277468d
Izorkin <izorkin@elven.pw>
2021-05-31 14:23:06 +0300

    nixos/tests: add peertube test

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -338,6 +338,7 @@ in
   paperless-ng = handleTest ./paperless-ng.nix {};
   pdns-recursor = handleTest ./pdns-recursor.nix {};
   peerflix = handleTest ./peerflix.nix {};
+  peertube = handleTestOn ["x86_64-linux"] ./web-apps/peertube.nix {};
   pgjwt = handleTest ./pgjwt.nix {};
   pgmanage = handleTest ./pgmanage.nix {};
   php = handleTest ./php {};
