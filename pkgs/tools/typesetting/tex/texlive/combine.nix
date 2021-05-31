eacc64775443036d00e16cfaaccfbde950b8f921
Vincenzo Mantova <xworld21@users.sf.net>
2021-05-31 12:44:10 +0100

    texlive.combine: generate hyphens.sed only from packages with AddHyphen

--- pkgs/tools/typesetting/tex/texlive/combine.nix
+++ pkgs/tools/typesetting/tex/texlive/combine.nix
@@ -122,7 +122,8 @@ in (buildEnv {
   '' +
     # now filter hyphenation patterns, in a hacky way ATM
   (let
-    pnames = uniqueStrings (map (p: p.pname) pkgList.splitBin.wrong);
+    hyphens = with lib; filter (p: p ? executes && any (hasPrefix "AddHyphen") p.executes) pkgList.splitBin.wrong;
+    pnames = uniqueStrings (map (p: p.pname) hyphens);
     script =
       writeText "hyphens.sed" (
         # pick up the header
