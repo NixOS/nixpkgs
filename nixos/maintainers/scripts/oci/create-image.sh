e8fc4d22e9dcf37d5296225d4416945bda5c66e2
ilian <ilian@tuta.io>
2021-05-31 14:46:51 +0200

    maintainers/scripts/oci: Fix indentation

--- nixos/maintainers/scripts/oci/create-image.sh
+++ nixos/maintainers/scripts/oci/create-image.sh
@@ -4,19 +4,19 @@ export NIX_PATH=nixpkgs=$(dirname $(readlink -f $0))/../../../..
 export NIXOS_CONFIG=$(dirname $(readlink -f $0))/../../../modules/virtualisation/oci-image.nix
 
 if (( $# < 1 )); then
-	(
-	echo "Usage: create-image.sh <architecture>"
-	echo
-	echo "Where <architecture> is one of:"
-	echo "  x86_64-linux"
-	echo "  aarch64-linux"
-	) >&2
+    (
+    echo "Usage: create-image.sh <architecture>"
+    echo
+    echo "Where <architecture> is one of:"
+    echo "  x86_64-linux"
+    echo "  aarch64-linux"
+    ) >&2
 fi
 
 system="$1"; shift
 
 nix-build '<nixpkgs/nixos>' \
-   -A config.system.build.OCIImage \
-   --argstr system "$system" \
-   --option system-features kvm \
-   -o oci-image
+    -A config.system.build.OCIImage \
+    --argstr system "$system" \
+    --option system-features kvm \
+    -o oci-image
