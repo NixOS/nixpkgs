9c086cc6555068c2be915613ad4c9c89f6d49bcd
R. RyanTM <ryantm-bot@ryantm.com>
2021-05-31 05:02:54 +0000

    gitstatus: 1.4.4 -> 1.5.0

--- pkgs/applications/version-management/git-and-tools/gitstatus/default.nix
+++ pkgs/applications/version-management/git-and-tools/gitstatus/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gitstatus";
-  version = "1.4.4";
+  version = "1.5.0";
 
   src = fetchFromGitHub {
     owner = "romkatv";
     repo = "gitstatus";
     rev = "v${version}";
-    sha256 = "1w5kpca2v6iii912riywp1jscq7cpr5xv93mglr30pjnar1mk8gs";
+    sha256 = "sha256-ypB/9pyABWZKbFTVj3rW0Jt/gvBKAIo147PTSXgZygc=";
   };
 
   buildInputs = [ (callPackage ./romkatv_libgit2.nix {}) ];
