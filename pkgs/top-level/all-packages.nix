059950a2d49cb7089c6a9a1155973323afe35bf6
Tomas Bravo <tomas.bravo@protonmail.ch>
2021-06-01 15:27:34 +0000

    songrec: init at 0.1.8

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25486,6 +25486,8 @@ in
 
   softmaker-office = callPackage ../applications/office/softmaker/softmaker_office.nix {};
 
+  songrec = callPackage ../applications/audio/songrec {};
+
   spacegun = callPackage ../applications/networking/cluster/spacegun {};
 
   stride = callPackage ../applications/networking/instant-messengers/stride { };
