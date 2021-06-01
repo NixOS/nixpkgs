af47c4717e2ea01496d0e5f90bf0ccb283bfe103
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-05-31 22:12:14 -0400

    kirigami-addons: init at 21.05

--- pkgs/top-level/qt5-packages.nix
+++ pkgs/top-level/qt5-packages.nix
@@ -88,6 +88,8 @@ in (kdeFrameworks // plasma5 // plasma5.thirdParty // kdeGear // qt5 // {
 
   kf5gpgmepp = callPackage ../development/libraries/kf5gpgmepp { };
 
+  kirigami-addons = libsForQt5.callPackage ../development/libraries/kirigami-addons { };
+
   kimageannotator = callPackage ../development/libraries/kimageannotator { };
 
   kproperty = callPackage ../development/libraries/kproperty { };
