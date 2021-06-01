NixOS/nixpkgs

commit 00996b5e03f33bebafc2b17c41a175d3726a9bde

dockerTools: Do not lowercase image tag in buildImage
Closes #115455

pkgs/build-support/docker/default.nix
@@ -518,9 +518,9 @@ rec {
         };
       result = runCommand "docker-image-${baseName}.tar.gz" {
         nativeBuildInputs = [ jshon pigz coreutils findutils jq moreutils ];
-        # Image name and tag must be lowercase
+        # Image name must be lowercase
         imageName = lib.toLower name;
-        imageTag = if tag == null then "" else lib.toLower tag;
+        imageTag = if tag == null then "" else tag;
         inherit fromImage baseJson;
         layerClosure = writeReferencesToFile layer;
         passthru.buildArgs = args;

commit ff55c41facaa6812f9cc879a49f6929321881d4f

dockerTools: Fix passthru image tag
It should match the actual image tag.
This fixes the problem introduced in 00996b5e03f33bebafc2b17c41a175d3726a9bde
https://github.com/NixOS/nixpkgs/pull/115491#pullrequestreview-672789901

pkgs/build-support/docker/default.nix
@@ -536,7 +536,7 @@ rec {
         passthru.layer = layer;
         passthru.imageTag =
           if tag != null
-            then lib.toLower tag
+            then tag
             else
               lib.head (lib.strings.splitString "-" (baseNameOf result.outPath));
         # Docker can't be made to run darwin binaries