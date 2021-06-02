e3e37d20ceb5efbc98ecad66d10db4a11d3f700b
Patrick Hilhorst <git@hilhorst.be>
2021-06-02 14:19:08 +0200

    nixos/tests/test-driver: add shell_interact
    (cherry picked from commit 5a589b5ba8941d734e9c3aebbf2be2f50d7c32a5)

--- nixos/lib/test-driver/test-driver.py
+++ nixos/lib/test-driver/test-driver.py
@@ -21,6 +21,7 @@ import shutil
 import socket
 import subprocess
 import sys
+import telnetlib
 import tempfile
 import time
 import traceback
@@ -454,6 +455,15 @@ class Machine:
                 status_code = int(match[2])
                 return (status_code, output)
             output += chunk
+
+    def shell_interact(self) -> None:
+        """Allows you to interact with the guest shell
+
+        Should only be used during testing, not in the production test."""
+        self.connect()
+        telnet = telnetlib.Telnet()
+        telnet.sock = self.shell  # type: ignore
+        telnet.interact()
 
     def succeed(self, *commands: str) -> str:
         """Execute each command and check that it succeeds."""
