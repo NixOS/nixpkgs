a425b4fbe446e53f7adce108e780522f3cc4d916
R. RyanTM <ryantm-bot@ryantm.com>
2021-06-02 15:49:54 +0000

    jackett: 0.18.100 -> 0.18.225

--- pkgs/servers/jackett/default.nix
+++ pkgs/servers/jackett/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "jackett";
-  version = "0.18.100";
+  version = "0.18.225";
 
   src = fetchurl {
     url = "https://github.com/Jackett/Jackett/releases/download/v${version}/Jackett.Binaries.Mono.tar.gz";
-    sha256 = "sha256-qgIC1PCCEMmt83xqedwmiUttA85zE/frn/Qxo5Y1yoM=";
+    sha256 = "sha256-EvFu+v1Ltot5zuDWcuG8Z00NHjWjZTrI0JKv+rgVB7U=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
