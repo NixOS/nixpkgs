d7d8972abd1a95a89c4dc4d87f8efc79a8ca7873
Ate Eskola <ajieskola@gmail.com>
2021-06-02 18:44:39 +0300

    Modified roughly as suggested by SuperSandro2000

--- pkgs/development/compilers/dmd/sourcebuild.nix
+++ pkgs/development/compilers/dmd/sourcebuild.nix
@@ -1,6 +1,6 @@
 { stdenv, lib, fetchFromGitHub
 , makeWrapper, unzip, which, writeTextFile
-, curl, tzdata, gdb, darwin, git, callPackage
+, curl, tzdata, gdb, Foundation, git, callPackage
 , targetPackages, fetchpatch, bash
 , HOST_DMD
 , version
@@ -10,14 +10,13 @@
 }:
 
 let
-
   dmdConfFile = writeTextFile {
-      name = "dmd.conf";
-      text = (lib.generators.toINI {} {
-        Environment = {
-          DFLAGS = ''-I@out@/include/dmd -L-L@out@/lib -fPIC ${lib.optionalString (!targetPackages.stdenv.cc.isClang) "-L--export-dynamic"}'';
-        };
-      });
+    name = "dmd.conf";
+    text = (lib.generators.toINI {} {
+      Environment = {
+        DFLAGS = ''-I@out@/include/dmd -L-L@out@/lib -fPIC ${lib.optionalString (!targetPackages.stdenv.cc.isClang) "-L--export-dynamic"}'';
+      };
+    });
   };
 
 in
@@ -58,34 +57,29 @@ stdenv.mkDerivation rec {
   hardeningDisable = [ "fortify" ];
 
   postUnpack = ''
-      patchShebangs .
+    patchShebangs .
   '';
 
   postPatch = ''
-      substituteInPlace dmd/test/dshell/test6952.d --replace "/usr/bin/env bash" "${bash}/bin/bash"
+    substituteInPlace dmd/test/dshell/test6952.d --replace "/usr/bin/env bash" "${bash}/bin/bash"
 
-      rm dmd/test/runnable/gdb1.d
-      rm dmd/test/runnable/gdb10311.d
-      rm dmd/test/runnable/gdb14225.d
-      rm dmd/test/runnable/gdb14276.d
-      rm dmd/test/runnable/gdb14313.d
-      rm dmd/test/runnable/gdb14330.d
-      rm dmd/test/runnable/gdb15729.sh
-      rm dmd/test/runnable/gdb4149.d
-      rm dmd/test/runnable/gdb4181.d
-  ''
-  + lib.optionalString stdenv.hostPlatform.isLinux ''
-      substituteInPlace phobos/std/socket.d --replace "assert(ih.addrList[0] == 0x7F_00_00_01);" ""
-  ''
-  + lib.optionalString stdenv.hostPlatform.isDarwin ''
-      substituteInPlace phobos/std/socket.d --replace "foreach (name; names)" "names = []; foreach (name; names)"
+    rm dmd/test/runnable/gdb1.d
+    rm dmd/test/runnable/gdb10311.d
+    rm dmd/test/runnable/gdb14225.d
+    rm dmd/test/runnable/gdb14276.d
+    rm dmd/test/runnable/gdb14313.d
+    rm dmd/test/runnable/gdb14330.d
+    rm dmd/test/runnable/gdb15729.sh
+    rm dmd/test/runnable/gdb4149.d
+    rm dmd/test/runnable/gdb4181.d
+  '' + lib.optionalString stdenv.hostPlatform.isLinux ''
+    substituteInPlace phobos/std/socket.d --replace "assert(ih.addrList[0] == 0x7F_00_00_01);" ""
+  '' + lib.optionalString stdenv.hostPlatform.isDarwin ''
+    substituteInPlace phobos/std/socket.d --replace "foreach (name; names)" "names = []; foreach (name; names)"
   '';
 
   nativeBuildInputs = [ makeWrapper unzip which gdb git ]
-
-  ++ lib.optional stdenv.hostPlatform.isDarwin (with darwin.apple_sdk.frameworks; [
-    Foundation
-  ]);
+    ++ lib.optional stdenv.hostPlatform.isDarwin [ Foundation ];
 
   buildInputs = [ curl tzdata ];
 
@@ -99,18 +93,20 @@ stdenv.mkDerivation rec {
 
   # Buid and install are based on http://wiki.dlang.org/Building_DMD
   buildPhase = ''
-      cd dmd
-      make -j$NIX_BUILD_CORES -f posix.mak INSTALL_DIR=$out BUILD=release ENABLE_RELEASE=1 PIC=1 HOST_DMD=${HOST_DMD}
-      cd ../druntime
-      make -j$NIX_BUILD_CORES -f posix.mak BUILD=release ENABLE_RELEASE=1 PIC=1 INSTALL_DIR=$out DMD=${pathToDmd}
-      cd ../phobos
-      echo ${tzdata}/share/zoneinfo/ > TZDatabaseDirFile
-      echo ${curl.out}/lib/libcurl${stdenv.hostPlatform.extensions.sharedLibrary} > LibcurlPathFile
-      make -j$NIX_BUILD_CORES -f posix.mak BUILD=release ENABLE_RELEASE=1 PIC=1 INSTALL_DIR=$out DMD=${pathToDmd} DFLAGS="-version=TZDatabaseDir -version=LibcurlPath -J$(pwd)"
-      cd ..
+    cd dmd
+    make -j$NIX_BUILD_CORES -f posix.mak INSTALL_DIR=$out BUILD=release ENABLE_RELEASE=1 PIC=1 HOST_DMD=${HOST_DMD}
+    cd ../druntime
+    make -j$NIX_BUILD_CORES -f posix.mak BUILD=release ENABLE_RELEASE=1 PIC=1 INSTALL_DIR=$out DMD=${pathToDmd}
+    cd ../phobos
+    echo ${tzdata}/share/zoneinfo/ > TZDatabaseDirFile
+    echo ${curl.out}/lib/libcurl${stdenv.hostPlatform.extensions.sharedLibrary} > LibcurlPathFile
+    make -j$NIX_BUILD_CORES -f posix.mak BUILD=release ENABLE_RELEASE=1 PIC=1 INSTALL_DIR=$out DMD=${pathToDmd} DFLAGS="-version=TZDatabaseDir -version=LibcurlPath -J$(pwd)"
+    cd ..
   '';
 
   doCheck = true;
+
+  # many tests are disbled because they are failing
 
   # NOTE: Purity check is disabled for checkPhase because it doesn't fare well
   # with the DMD linker. See https://github.com/NixOS/nixpkgs/issues/97420
@@ -131,43 +127,42 @@ stdenv.mkDerivation rec {
   '';
 
   installPhase = ''
-      cd dmd
-      mkdir $out
-      mkdir $out/bin
-      cp ${pathToDmd} $out/bin
+    cd dmd
+    mkdir $out
+    mkdir $out/bin
+    cp ${pathToDmd} $out/bin
 
-      mkdir -p $out/share/man/man1
-      mkdir -p $out/share/man/man5
-      cp -r docs/man/man1/* $out/share/man/man1/
-      cp -r docs/man/man5/* $out/share/man/man5/
+    mkdir -p $out/share/man/man1
+    mkdir -p $out/share/man/man5
+    cp -r docs/man/man1/* $out/share/man/man1/
+    cp -r docs/man/man5/* $out/share/man/man5/
 
-      cd ../druntime
-      mkdir $out/include
-      mkdir $out/include/dmd
-      cp -r import/* $out/include/dmd
+    cd ../druntime
+    mkdir $out/include
+    mkdir $out/include/dmd
+    cp -r import/* $out/include/dmd
 
-      cd ../phobos
-      mkdir $out/lib
-      cp generated/${osname}/release/${bits}/libphobos2.* $out/lib
+    cd ../phobos
+    mkdir $out/lib
+    cp generated/${osname}/release/${bits}/libphobos2.* $out/lib
 
-      cp -r std $out/include/dmd
-      cp -r etc $out/include/dmd
+    cp -r std $out/include/dmd
+    cp -r etc $out/include/dmd
 
-      wrapProgram $out/bin/dmd \
-          --prefix PATH ":" "${targetPackages.stdenv.cc}/bin" \
-          --set-default CC "${targetPackages.stdenv.cc}/bin/cc"
+    wrapProgram $out/bin/dmd \
+      --prefix PATH ":" "${targetPackages.stdenv.cc}/bin" \
+      --set-default CC "${targetPackages.stdenv.cc}/bin/cc"
 
-      substitute ${dmdConfFile} "$out/bin/dmd.conf" --subst-var out
+    substitute ${dmdConfFile} "$out/bin/dmd.conf" --subst-var out
   '';
 
   meta = with lib; {
     description = "Official reference compiler for the D language";
-    homepage = "http://dlang.org/";
+    homepage = "https://dlang.org/";
     # Everything is now Boost licensed, even the backend.
     # https://github.com/dlang/dmd/pull/6680
     license = licenses.boost;
     maintainers = with maintainers; [ ThomasMader lionello ];
     platforms = [ "x86_64-linux" "i686-linux" "x86_64-darwin" ];
-    # many tests are failing
   };
 }
