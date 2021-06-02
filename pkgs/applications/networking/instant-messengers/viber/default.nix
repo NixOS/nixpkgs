d5b481dd69f1b53d649d49b379d34a85be2f5b3b
Artturin <Artturin@artturin.com>
2021-06-02 03:56:40 +0300

    viber: use webarchive for a stable source
    (cherry picked from commit 24ba97a40beb3bc913ab724c8e66b97a06d52db1)

--- pkgs/applications/networking/instant-messengers/viber/default.nix
+++ pkgs/applications/networking/instant-messengers/viber/default.nix
@@ -9,7 +9,8 @@ stdenv.mkDerivation {
   version = "13.3.1.22";
 
   src = fetchurl {
-    url = "https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb";
+    # Official link: https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
+    url = "http://web.archive.org/web/20210602004133/https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb";
     sha256 = "0rs26x0lycavybn6k1hbb5kzms0zzcmxlrmi4g8k7vyafj6s8dqh";
   };
 
