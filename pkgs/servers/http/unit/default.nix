c76bebc549f2bd81bf5c70617d3d564a1ae6e8d1
Elis Hirwing <elis@hirwing.se>
2021-06-03 19:17:27 +0200

    unit: Add php80 and use it as default

--- pkgs/servers/http/unit/default.nix
+++ pkgs/servers/http/unit/default.nix
@@ -2,7 +2,8 @@
 , pcre2
 , withPython2 ? false, python2
 , withPython3 ? true, python3, ncurses
-, withPHP74 ? true, php74
+, withPHP74 ? false, php74
+, withPHP80 ? true, php80
 , withPerl530 ? false, perl530
 , withPerl532 ? true, perl532
 , withPerldevel ? false, perldevel
@@ -26,6 +27,7 @@ let
   };
 
   php74-unit = php74.override phpConfig;
+  php80-unit = php80.override phpConfig;
 
 in stdenv.mkDerivation rec {
   version = "1.24.0";
@@ -44,6 +46,7 @@ in stdenv.mkDerivation rec {
     ++ optional withPython2 python2
     ++ optionals withPython3 [ python3 ncurses ]
     ++ optional withPHP74 php74-unit
+    ++ optional withPHP80 php80-unit
     ++ optional withPerl530 perl530
     ++ optional withPerl532 perl532
     ++ optional withPerldevel perldevel
@@ -62,11 +65,13 @@ in stdenv.mkDerivation rec {
 
   # Optionally add the PHP derivations used so they can be addressed in the configs
   usedPhp74 = optionals withPHP74 php74-unit;
+  usedPhp80 = optionals withPHP80 php80-unit;
 
   postConfigure = ''
     ${optionalString withPython2    "./configure python --module=python2  --config=python2-config  --lib-path=${python2}/lib"}
     ${optionalString withPython3    "./configure python --module=python3  --config=python3-config  --lib-path=${python3}/lib"}
     ${optionalString withPHP74      "./configure php    --module=php74    --config=${php74-unit.unwrapped.dev}/bin/php-config --lib-path=${php74-unit}/lib"}
+    ${optionalString withPHP80      "./configure php    --module=php80    --config=${php80-unit.unwrapped.dev}/bin/php-config --lib-path=${php80-unit}/lib"}
     ${optionalString withPerl530    "./configure perl   --module=perl530  --perl=${perl530}/bin/perl"}
     ${optionalString withPerl532    "./configure perl   --module=perl532  --perl=${perl532}/bin/perl"}
     ${optionalString withPerldevel  "./configure perl   --module=perldev  --perl=${perldevel}/bin/perl"}
