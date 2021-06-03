d370824442bfcd0f9192b67e774d7b3fc291bf7f
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-06-03 03:43:31 +0200

    home-assistant: 2021.5.5 -> 2021.6.0

--- pkgs/servers/home-assistant/frontend.nix
+++ pkgs/servers/home-assistant/frontend.nix
@@ -4,11 +4,11 @@ buildPythonPackage rec {
   # the frontend version corresponding to a specific home-assistant version can be found here
   # https://github.com/home-assistant/home-assistant/blob/master/homeassistant/components/frontend/manifest.json
   pname = "home-assistant-frontend";
-  version = "20210504.0";
+  version = "20210601.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-rNdqflWT8bmGHtAGGfq4xVxDcnK+EZU9qejRWQPJmUI=";
+    sha256 = "sha256-z2KvP/zMibhpDzBBuWVD0Qpb37NaX3kxyNW4bY9WJrE=";
   };
 
   # there is nothing to strip in this package
