c06b1086c0962a5909432c5b9590fc510926802a
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-06-03 23:41:07 +0200

    python3Packages.click-option-group: init at 0.5.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1417,6 +1417,8 @@ in {
 
   click-log = callPackage ../development/python-modules/click-log { };
 
+  click-option-group = callPackage ../development/python-modules/click-option-group { };
+
   click-plugins = callPackage ../development/python-modules/click-plugins { };
 
   click-spinner = callPackage ../development/python-modules/click-spinner { };
