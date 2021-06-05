718080c7a1c92d7376e934b37aed46092a0a642b
David Arnold <dar@xoe.solutions>
2021-06-05 18:59:29 -0500

    nixos/tests/test-driver: fixups and polishing

--- nixos/lib/testing-python.nix
+++ nixos/lib/testing-python.nix
@@ -100,31 +100,42 @@ rec {
   # Generate convenience wrappers for running the test driver
   # has vlans, vms and test script defaulted through env variables
   # also instantiates test script with nodes, if it's a function (contract)
-  mkDriver = { testScript, testDriverName, qemu_pkg ? pkgs.qemu_test, enableOCR ? false}:
+  mkDriver = {
+      testScript
+    , testName
+    , nodes
+    , qemu_pkg ? pkgs.qemu_test
+    , enableOCR ? false
+    , skipLint ? false
+    , passthru ? {}
+  }:
     let
-      build-vms = import ./build-vms.nix {
-        inherit system pkgs minimal specialArgs;
-        extraConfigurations = extraConfigurations ++ (pkgs.lib.optional (qemu_pkg != pkgs.qemu_test)
-          {
-            virtualisation.qemu.package = qemu_pkg;
-          }
-        ) ++ [(
-          {
-            # Ensure we do not use aliases. Ideally this is only set
-            # when the test framework is used by Nixpkgs NixOS tests.
-            nixpkgs.config.allowAliases = false;
-          }
-        )];
-      };
-
       # FIXME: get this pkg from the module system
       testDriver = mkTestDriver { inherit qemu_pkg enableOCR;};
 
-      nodes = build-vms.buildVirtualNetwork (
-        t.nodes or (if t ? machine then { machine = t.machine; } else { })
-      );
+      testDriverName =
+        let
+          # A standard store path to the vm monitor is built like this:
+          #   /tmp/nix-build-vm-test-run-$name.drv-0/vm-state-machine/monitor
+          # The max filename length of a unix domain socket is 108 bytes.
+          # This means $name can at most be 50 bytes long.
+          maxTestNameLen = 50;
+          testNameLen = builtins.stringLength testName;
+        in with builtins;
+          if testNameLen > maxTestNameLen then
+            abort
+              ("The name of the test '${testName}' must not be longer than ${toString maxTestNameLen} " +
+                "it's currently ${toString testNameLen} characters long.")
+          else
+            "nixos-test-driver-${testName}";
+
       vlans = map (m: m.config.virtualisation.vlans) (lib.attrValues nodes);
       vms = map (m: m.config.system.build.vm) (lib.attrValues nodes);
+
+      nodeHostNames = map (c: c.config.system.name) (lib.attrValues nodes);
+      invalidNodeNames = lib.filter
+        (node: builtins.match "^[A-z_]([A-z0-9_]+)?$" node == null)
+        (builtins.attrNames nodes);
 
       testScript' =
         # Call the test script with the computed nodes.
@@ -133,12 +144,20 @@ rec {
         else testScript;
 
     in
-    lib.warnIf skipLint "Linting is disabled" (runCommand testDriverName
+    if lib.length invalidNodeNames > 0 then
+      throw ''
+        Cannot create machines out of (${lib.concatStringsSep ", " invalidNodeNames})!
+        All machines are referenced as python variables in the testing framework which will break the
+        script when special characters are used.
+
+        Please stick to alphanumeric chars and underscores as separation.
+      ''
+    else lib.warnIf skipLint "Linting is disabled" (runCommand testDriverName
       {
+        inherit testName;
         nativeBuildInputs = [ makeWrapper ];
         testScript = testScript';
         preferLocalBuild = true;
-        testName = name;
         passthru = passthru // {
           inherit nodes;
         };
@@ -149,6 +168,8 @@ rec {
         vmStartScripts=($(for i in ${toString vms}; do echo $i/bin/run-*-vm; done))
         ln -s ${testDriver}/bin/nixos-test-driver $out/bin/nixos-test-driver
 
+        echo ${lib.concatStringsSep "," nodeHostNames} > $out/nodenames
+
         echo -n "$testScript" > $out/test-script
         ${lib.optionalString (!skipLint) ''
            PYFLAKES_BUILTINS="$(
@@ -157,10 +178,10 @@ rec {
            )" ${python3Packages.pyflakes}/bin/pyflakes $out/test-script
         ''}
 
-         # set defaults through environment
+        # set defaults through environment
         # see: ./test-driver/test-driver.py argparse implementation
         wrapProgram $out/bin/nixos-test-driver \
-           --set startScripts "''${vmStartScripts[*]}" \
+          --set startScripts "''${vmStartScripts[*]}" \
           --set testScript "$out/test-script" \
           --set vlans '${toString vlans}'
        ''); # "
@@ -180,55 +201,44 @@ rec {
     , ...
     } @ t:
     let
-      # A standard store path to the vm monitor is built like this:
-      #   /tmp/nix-build-vm-test-run-$name.drv-0/vm-state-machine/monitor
-      # The max filename length of a unix domain socket is 108 bytes.
-      # This means $name can at most be 50 bytes long.
-      maxTestNameLen = 50;
-      testNameLen = builtins.stringLength name;
-      testDriverName = with builtins;
-        if testNameLen > maxTestNameLen then
-          abort
-            ("The name of the test '${name}' must not be longer than ${toString maxTestNameLen} " +
-              "it's currently ${toString testNameLen} characters long.")
-        else
-          "nixos-test-driver-${name}";
-
-      passMeta = drv: drv // lib.optionalAttrs (t ? meta) {
-        meta = (drv.meta or { }) // t.meta;
-      };
+      nodes = qemu_pkg:
+        let
+          build-vms = import ./build-vms.nix {
+            inherit system pkgs minimal specialArgs;
+            extraConfigurations = extraConfigurations ++ [(
+              {
+                virtualisation.qemu.package = qemu_pkg;
+                # Ensure we do not use aliases. Ideally this is only set
+                # when the test framework is used by Nixpkgs NixOS tests.
+                nixpkgs.config.allowAliases = false;
+              }
+            )];
+          };
+        in
+          build-vms.buildVirtualNetwork (
+              t.nodes or (if t ? machine then { machine = t.machine; } else { })
+          );
 
       driver = mkDriver {
-        inherit testScript testDriverName enableOCR;
-        qemu_pkg = pkgs.qemu_test;
+        inherit testScript enableOCR skipLint;
+        testName = name;
+        nodes = nodes pkgs.qemu_test;
       };
       driverInteractive = mkDriver {
-        inherit testScript testDriverName enableOCR;
-        qemu_pkg = pkgs.qemu;
+        inherit testScript enableOCR skipLint;
+        testName = name;
+        nodes = nodes pkgs.qemu;
       };
 
-      test = passMeta (runTests { inherit driver pos; });
-
-      nodeNames = builtins.attrNames driver.nodes;
-      invalidNodeNames = lib.filter
-        (node: builtins.match "^[A-z_]([A-z0-9_]+)?$" node == null)
-        nodeNames;
-
-      nodeHostNames = map (c: c.config.system.name) (lib.attrValues driver.nodes);
-
+      test =
+        let
+          passMeta = drv: drv // lib.optionalAttrs (t ? meta) {
+            meta = (drv.meta or { }) // t.meta;
+          };
+        in passMeta (runTests { inherit driver pos; });
     in
-    if lib.length invalidNodeNames > 0 then
-      throw ''
-        Cannot create machines out of (${lib.concatStringsSep ", " invalidNodeNames})!
-        All machines are referenced as python variables in the testing framework which will break the
-        script when special characters are used.
-
-        Please stick to alphanumeric chars and underscores as separation.
-      ''
-    else
       test // {
-        inherit test driver driverInteractive;
-        inherit (driver) nodes;
+        inherit test driver driverInteractive nodes;
       };
 
   runInMachine =
