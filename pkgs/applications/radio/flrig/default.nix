b23e400fd0fac4e9ba575e0a3e8e7d8cc0a3c847
R. RyanTM <ryantm-bot@ryantm.com>
2021-06-06 09:36:51 +0000

    flrig: 1.3.54 -> 1.4.0

--- pkgs/applications/radio/flrig/default.nix
+++ pkgs/applications/radio/flrig/default.nix
@@ -7,12 +7,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "1.3.54";
+  version = "1.4.0";
   pname = "flrig";
 
   src = fetchurl {
     url = "mirror://sourceforge/fldigi/${pname}-${version}.tar.gz";
-    sha256 = "sha256-zw0xi4AVnm8Vi/oWkh13C9BjabOKUtZaVmLN++zKGug=";
+    sha256 = "sha256-CrIDk6cZlB+otO9CWFH7dIO6S3RUuhLLOcb+hkiGCwc=";
   };
 
   buildInputs = [
