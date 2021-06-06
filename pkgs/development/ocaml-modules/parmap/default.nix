3829f1e360540ef92e3b661ee73c20a7d6cfb890
superherointj <5861043+superherointj@users.noreply.github.com>
2021-06-05 21:07:19 -0300

    ocamlPackages.parmap: 1.2 -> 1.2.3

--- pkgs/development/ocaml-modules/parmap/default.nix
+++ pkgs/development/ocaml-modules/parmap/default.nix
@@ -1,24 +1,28 @@
-{ lib, buildDunePackage, fetchurl, dune-configurator }:
+{ lib, fetchurl, buildDunePackage, dune-configurator }:
 
 buildDunePackage rec {
   pname = "parmap";
-  version = "1.2";
-
-  useDune2 = true;
+  version = "1.2.3";
 
   src = fetchurl {
     url = "https://github.com/rdicosmo/${pname}/releases/download/${version}/${pname}-${version}.tbz";
-    sha256 = "sha256-XUXptzD0eytaypaBQ+EBp4iVFRE6/Y0inS93t/YZrM8=";
+    sha256 = "1wg81slp453jci0gi0rzvdjx74110mlf1n5qpsmxic6fqsyz9d2v";
   };
 
-  buildInputs = [ dune-configurator ];
+  minimalOCamlVersion = "4.03";
+  useDune2 = true;
+
+  buildInputs = [
+    dune-configurator
+  ];
 
   doCheck = true;
 
   meta = with lib; {
     description = "Library for multicore parallel programming";
+    downloadPage = "https://github.com/rdicosmo/parmap";
     homepage = "https://rdicosmo.github.io/parmap";
-    maintainers = [ maintainers.bcdarwin ];
     license = licenses.lgpl2;
+    maintainers = with maintainers; [ bcdarwin ];
   };
 }
