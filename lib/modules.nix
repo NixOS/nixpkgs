8fb9984690c878fcd768e967190957441de05d11
Janne Heß <janne@hess.ooo>
2021-06-06 20:41:37 +0200

    lib/modules: Drop mkStrict and mkFixStrictness
    This was deprecated in 2014 and is not used anywhere in the tree.

--- lib/modules.nix
+++ lib/modules.nix
@@ -713,10 +713,6 @@ rec {
   mkForce = mkOverride 50;
   mkVMOverride = mkOverride 10; # used by ‘nixos-rebuild build-vm’
 
-  mkStrict = builtins.trace "`mkStrict' is obsolete; use `mkOverride 0' instead." (mkOverride 0);
-
-  mkFixStrictness = id; # obsolete, no-op
-
   mkOrder = priority: content:
     { _type = "order";
       inherit priority content;
