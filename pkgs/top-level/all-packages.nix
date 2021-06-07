d4726d156e22afb397df4ad374d513b4c956fecb
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2021-06-08 01:01:42 +1000

    clapper: init at 0.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23922,6 +23922,8 @@ with pkgs;
 
   cipher = callPackage ../applications/misc/cipher { };
 
+  clapper = callPackage ../applications/video/clapper { };
+
   claws-mail-gtk2 = callPackage ../applications/networking/mailreaders/claws-mail {
     inherit (xorg) libSM;
     useGtk3 = false;
