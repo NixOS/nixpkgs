259b3b589dec9090deb0949d685531bc9c80aa2d
Ian Henry <ianthehenry@gmail.com>
2021-06-06 19:27:23 -0700

    python3: set an explicit high priority
    Currently there are multiple derivations named python3, none of which have an
    explicit meta.priority. This means that nix-env -i python3 will give a
    different result than nix-env -iA nixpkgs.python3, and nix-env -u is not useful
    when you have nixpkgs.python3 installed.
    
    So we explicitly raise the priority of nixpkgs.python3, so that nix-env -i
    python3 will no longer try to install an alpha version of python3 just because
    it has a higher version number.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12355,7 +12355,7 @@ in
   # When switching these sets, please update docs at ../../doc/languages-frameworks/python.md
   python = python2;
   python2 = python27;
-  python3 = python38;
+  python3 = python38.overrideAttrs lib.hiPrio;
   pypy = pypy2;
   pypy2 = pypy27;
   pypy3 = pypy36;
