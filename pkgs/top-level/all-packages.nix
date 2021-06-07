2f3e0cf3bae57b35e6653aad0b32d6343ae197b8
Ivar Scholten <ivar.scholten@protonmail.com>
2021-06-07 20:42:14 +0200

    gwe: init at 0.15.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11153,6 +11153,10 @@ in
 
   gprolog = callPackage ../development/compilers/gprolog { };
 
+  gwe = callPackage ../tools/misc/gwe {
+    nvidia_x11 = linuxPackages.nvidia_x11;
+  };
+
   gwt240 = callPackage ../development/compilers/gwt/2.4.0.nix { };
 
   idrisPackages = dontRecurseIntoAttrs (callPackage ../development/idris-modules {
