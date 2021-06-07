9de67a62677e41b166c02fe476ffbb5a4e1fa07e
superherointj <5861043+superherointj@users.noreply.github.com>
2021-06-07 11:35:32 -0300

    reason: add superherointj as maintainer

--- pkgs/development/compilers/reason/default.nix
+++ pkgs/development/compilers/reason/default.nix
@@ -50,6 +50,6 @@ stdenv.mkDerivation rec {
     description = "Facebook's friendly syntax to OCaml";
     license = licenses.mit;
     inherit (ocaml.meta) platforms;
-    maintainers = with maintainers; [ volth ];
+    maintainers = with maintainers; [ superherointj volth ];
   };
 }
