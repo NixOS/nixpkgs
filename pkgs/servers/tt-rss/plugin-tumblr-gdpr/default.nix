c5811ce005abcf2c4cebec0ea43bbd335ae115b0
Guillaume Girol <symphorien+git@xlumurb.eu>
2021-06-08 12:00:00 +0000

    tt-rss-plugin-tumblr-gdpr: 2.1 -> 2.2
    fixes adding a new feed to ttrss.
    the release only contains the fix, so it's low risk.
    It's not a backport because the plugin has become unnecessary so we are
    simply removing it from unstable.

--- pkgs/servers/tt-rss/plugin-tumblr-gdpr/default.nix
+++ pkgs/servers/tt-rss/plugin-tumblr-gdpr/default.nix
@@ -2,19 +2,22 @@
 
 stdenv.mkDerivation rec {
   pname = "tt-rss-plugin-tumblr-gdpr";
-  version = "2.1";
+  version = "2.2";
 
   src = fetchFromGitHub {
     owner = "GregThib";
     repo = "ttrss-tumblr-gdpr";
     rev = "v${version}";
-    sha256 = "09cbghi5b6ww4i5677i39qc9rhpq70xmygp0d7x30239r3i23rpq";
+    sha256 = "sha256-oCdpgfOvXNui2KaS48WeyVy8Of6IlPOxQR6gmPiyH0s=";
   };
 
   installPhase = ''
-    mkdir -p $out/tumblr_gdpr
+    runHook preInstall
 
+    mkdir -p $out/tumblr_gdpr
     cp init.php $out/tumblr_gdpr
+
+    runHook postInstall
   '';
 
   meta = with lib; {
@@ -24,7 +27,7 @@ stdenv.mkDerivation rec {
 
       The name of the plugin in TT-RSS is 'tumblr_gdpr'.
     '';
-    license = licenses.gpl3;
+    license = licenses.gpl3Only;
     homepage = "https://github.com/GregThib/ttrss-tumblr-gdpr";
     maintainers = with maintainers; [ das_j ];
     platforms = platforms.all;
