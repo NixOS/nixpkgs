632c33730fee3535b32acf07af2bed5e6d2fcc12
David Terry <me@xwvvvvwx.com>
2021-06-08 15:41:43 +0200

    erigon: 2021.05.02 -> 2021.06.03

--- pkgs/applications/blockchains/erigon.nix
+++ pkgs/applications/blockchains/erigon.nix
@@ -1,29 +1,50 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{ stdenv, lib, buildGoModule, fetchFromGitHub }:
+
+let
+  pname = "erigon";
+  version = "2021.06.03";
+
+  # this derivation uses the makefile to build mdbx-static.o and put in the
+  # right place for the standard gomodules build to succeed.
+  prepare-mdbx = stdenv.mkDerivation {
+    inherit pname version;
+    src = fetchFromGitHub {
+      owner = "ledgerwatch";
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "sha256-Y7qsPrH9ZZD2QDmguefs7S1q+YANl5u3WdpNtcnuRsI=";
+    };
+    buildPhase = ''
+      make mdbx
+    '';
+    installPhase = ''
+      mkdir -p $out
+      cp -r /build/source/* $out
+    '';
+  };
+in
 
 buildGoModule rec {
-  pname = "turbo-geth";
-  version = "2021.05.02";
+  inherit pname version;
+  src = prepare-mdbx;
 
-  src = fetchFromGitHub {
-    owner = "ledgerwatch";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-7sTRAAlKZOdwi/LRbIEDKWpBe1ol8pZfEf2KIC4s0xk=";
-  };
-
-  vendorSha256 = "1d0ahdb2b5v8nxq3kdxw151phnyv6habb8kr8qjaq3kyhcnyk6ng";
+  vendorSha256 = "1nlgby1vv23a0abgd85wdywx01v7a0i6i1797hhwazvsrm1lphb0";
   runVend = true;
 
+  # Build errors in mdbx when format hardening is enabled:
+  #   cc1: error: '-Wformat-security' ignored without '-Wformat' [-Werror=format-security]
+  hardeningDisable = [ "format" ];
+
   subPackages = [
-    "cmd/tg"
+    "cmd/erigon"
     "cmd/evm"
     "cmd/rpcdaemon"
     "cmd/rlpdump"
   ];
 
   meta = with lib; {
-    homepage = "https://github.com/ledgerwatch/turbo-geth/";
-    description = "Ethereum node and geth fork focused on scalability and modularity";
+    homepage = "https://github.com/ledgerwatch/erigon/";
+    description = "Ethereum node implementation focused on scalability and modularity";
     license = with licenses; [ lgpl3Plus gpl3Plus ];
     maintainers = with maintainers; [ xwvvvvwx ];
   };
