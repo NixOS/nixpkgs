62eb2776d470b67cd66742499e32bf5a4929d5d3
R. RyanTM <ryantm-bot@ryantm.com>
2021-06-08 21:28:43 +0000

    logseq: 0.1.3 -> 0.1.6

--- pkgs/applications/misc/logseq/default.nix
+++ pkgs/applications/misc/logseq/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "logseq";
-  version = "0.1.3";
+  version = "0.1.6";
 
   src = fetchurl {
     url = "https://github.com/logseq/logseq/releases/download/${version}/logseq-linux-x64-${version}.AppImage";
-    sha256 = "1akg3xjbh01nb7l06qpvz3xsjj64kf042xjnapn60jlgg5y34vbm";
+    sha256 = "6gZtBp8Xw5r2h3cS9++emA1Ny5xa9x4zsvlAR4kUfQ0=";
     name = "${pname}-${version}.AppImage";
   };
 
