e7928186d996d4427e83ce313930d4cf58b7a5f5
R. RyanTM <ryantm-bot@ryantm.com>
2021-06-09 04:00:20 +0000

    deno: 1.10.3 -> 1.11.0
    (cherry picked from commit c374fd03ffd37d400c6bad69a4cd386749df803b)

--- pkgs/development/web/deno/default.nix
+++ pkgs/development/web/deno/default.nix
@@ -17,15 +17,15 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "deno";
-  version = "1.10.3";
+  version = "1.11.0";
 
   src = fetchFromGitHub {
     owner = "denoland";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-25FfxGtPZ+KQCmXur6pwrb1l/xjCWgw69CMLPihnhAU=";
+    sha256 = "sha256-rsQ9MdMgYPqnLzMfW4rwDpx5QKVDm6TMCCHqJzvTpjw=";
   };
-  cargoSha256 = "sha256-CopfdjafWAhpbrdYSHJjKHKCLw94TSaiSAH4CVFOHi8=";
+  cargoSha256 = "sha256-nKbc73bEKb0XzuBZApu21E1OzkgU9sqbCaaWhUMiWdQ=";
 
   # Install completions post-install
   nativeBuildInputs = [ installShellFiles ];
