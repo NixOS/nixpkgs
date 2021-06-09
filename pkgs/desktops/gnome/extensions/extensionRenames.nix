f5cec70c8a075bd28d8893d498d9b6b018ad7680
Scott Worley <scottworley@scottworley.com>
2021-06-09 15:07:50 -0700

    gnomeExtensions: Auto-update

--- pkgs/desktops/gnome/extensions/extensionRenames.nix
+++ pkgs/desktops/gnome/extensions/extensionRenames.nix
@@ -13,6 +13,15 @@
   "lockkeys@vaina.lt" = "lock-keys";
   "lockkeys@fawtytoo" = "lock-keys-2";
 
+  "Hide_Activities@shay.shayel.org" = "hide-activities-button";
+  "hide-activities-button@nmingori.gnome-shell-extensions.org" = "hide-activities-button-nmingori";
+
+  "ShutdownTimer@neumann" = "shutdowntimer";
+  "shutdown-timer-gnome-shell-extension" = "shutdowntimer-mkrajnak";
+
+  "extension-list@tu.berry" = "extension-list";
+  "screen-lock@garciabaameiro.com" = "screen-lock";
+
 
   # These are conflicts for 3.38 extensions. They will very probably come back
   # once more of them support 40.
