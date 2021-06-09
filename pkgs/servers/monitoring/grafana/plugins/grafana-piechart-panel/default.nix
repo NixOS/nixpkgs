cd48bd0f9abc4486655965ffeb293b82eb55f8f0
Luke Granger-Brown <git@lukegb.com>
2021-06-09 10:18:04 +0000

    grafanaPlugins.grafana-piechart-panel: 1.6.1 → 1.6.2

--- pkgs/servers/monitoring/grafana/plugins/grafana-piechart-panel/default.nix
+++ pkgs/servers/monitoring/grafana/plugins/grafana-piechart-panel/default.nix
@@ -2,8 +2,8 @@
 
 grafanaPlugin rec {
   pname = "grafana-piechart-panel";
-  version = "1.6.1";
-  zipHash = "sha256-64K/efoBKuBFp8Jw79hTdMyTurTZsL0qfgPDcUWz2jg=";
+  version = "1.6.2";
+  zipHash = "sha256-xKyVT092Ffgzl0BewQw5iZ14I/q6CviUR5t9BVM0bf0=";
   meta = with lib; {
     description = "Pie chart panel for Grafana";
     license = licenses.mit;
