f0a007e780d1c4d5aa5fb4fee9d1ab8d469918dd
David McFarland <corngood@gmail.com>
2021-06-08 22:30:45 -0300

    p4: 2020.2.2057778 -> 2021.1.2126753

--- pkgs/applications/version-management/p4/default.nix
+++ pkgs/applications/version-management/p4/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "p4";
-  version = "2020.2.2057778";
+  version = "2021.1.2126753";
 
   src = fetchurl {
-    url = "https://www.perforce.com/downloads/perforce/r20.2/bin.linux26x86_64/helix-core-server.tgz";
-    sha256 = "02fcn8myqkj4xiwxy2xdyp9glc1xqyzblyn564vs1mjy99vm24ym";
+    url = "https://www.perforce.com/downloads/perforce/r21.1/bin.linux26x86_64/helix-core-server.tgz";
+    sha256 = "064cdx2r6aix0mawm1jklbfhwmzkj1146fkqjadz9sv79zj3g35p";
   };
 
   sourceRoot = ".";
