398c74fcd6d68be57a9eca1a84902f39becb6d62
Alvar Penning <post@0x21.biz>
2021-06-10 22:05:03 +0200

    olm: 3.2.2 -> 3.2.4
    (cherry picked from commit 660be4bfb228e4f3502443598edd25e2ab5304fa)

--- pkgs/development/libraries/olm/default.nix
+++ pkgs/development/libraries/olm/default.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "olm";
-  version = "3.2.2";
+  version = "3.2.4";
 
   src = fetchFromGitLab {
     domain = "gitlab.matrix.org";
     owner = "matrix-org";
     repo = pname;
     rev = version;
-    sha256 = "0qji25wiwmkxyfpraxj96c54hyayqmjkvwh0gsy5gb5pz5bp4mcy";
+    sha256 = "1rl7j26li1irb1lqnnkzan7jrj38kvmdn69rlwbbp390v3z15lvh";
   };
 
   nativeBuildInputs = [ cmake ];
