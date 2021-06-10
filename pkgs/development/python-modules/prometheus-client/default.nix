15a93f1268ba2fb441560c4d125ebc5790593c9b
Fabian Affolter <mail@fabian-affolter.ch>
2021-06-10 11:59:47 +0200

    python3Packages.prometheus-client: enable tests

--- pkgs/development/python-modules/prometheus-client/default.nix
+++ pkgs/development/python-modules/prometheus-client/default.nix
@@ -1,15 +1,25 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, pytestCheckHook
+}:
 
 buildPythonPackage rec {
-  pname = "prometheus_client";
+  pname = "prometheus-client";
   version = "0.11.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "3a8baade6cb80bcfe43297e33e7623f3118d660d41387593758e2fb1ea173a86";
+  src = fetchFromGitHub {
+    owner = "prometheus";
+    repo = "client_python";
+    rev = "v${version}";
+    sha256 = "14swmy4dgpk6cyjsm2advgc2c8api7xaca1sl7swznblh5fyzgzg";
   };
 
-  doCheck = false;
+  checkInputs = [
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [ "prometheus_client" ];
 
   meta = with lib; {
     description = "Prometheus instrumentation library for Python applications";
