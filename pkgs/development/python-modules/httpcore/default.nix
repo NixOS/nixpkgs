5466601bdd72525c59a081b222d438469bb996b4
Fabian Affolter <mail@fabian-affolter.ch>
2021-06-12 22:20:55 +0200

    python3Packages.httpcore: 0.13.0 -> 0.13.4

--- pkgs/development/python-modules/httpcore/default.nix
+++ pkgs/development/python-modules/httpcore/default.nix
@@ -2,31 +2,33 @@
 , buildPythonPackage
 , pythonOlder
 , fetchFromGitHub
+, anyio
 , h11
 , h2
 , pproxy
 , pytest-asyncio
-, pytest-trio
 , pytestCheckHook
 , pytestcov
 , sniffio
+, trio
 , trustme
 , uvicorn
 }:
 
 buildPythonPackage rec {
   pname = "httpcore";
-  version = "0.13.0";
+  version = "0.13.4";
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "encode";
     repo = pname;
     rev = version;
-    sha256 = "sha256-KvqBVQUaF3p2oJz0tt3Bkn2JiKEHqrZ3b6I9f0JK5h8=";
+    sha256 = "0vfdkd3bq14mnjd2gfg3ajsgygmd796md7pv96nicpx20prw3a2p";
   };
 
   propagatedBuildInputs = [
+    anyio
     h11
     h2
     sniffio
@@ -35,16 +37,23 @@ buildPythonPackage rec {
   checkInputs = [
     pproxy
     pytest-asyncio
-    pytest-trio
     pytestCheckHook
     pytestcov
+    trio
     trustme
     uvicorn
   ];
 
+  postPatch = ''
+    # The anyio 3.1.0 release is not picked-up proberly
+    substituteInPlace setup.py --replace "anyio==3.*" "anyio"
+  '';
+
+
   disabledTestPaths = [
     # these tests fail during dns lookups: httpcore.ConnectError: [Errno -2] Name or service not known
     "tests/test_threadsafety.py"
+    "tests/async_tests/"
     "tests/sync_tests/test_interfaces.py"
     "tests/sync_tests/test_retries.py"
   ];
