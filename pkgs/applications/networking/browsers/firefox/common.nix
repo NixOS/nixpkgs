1b74469cd087f4d08b9101fecc52f9ebae058e9f
Luke Granger-Brown <git@lukegb.com>
2021-06-12 19:29:26 +0000

    nixos/ca: use cacert package build for options and p11-kit output
    The cacert package can now generate p11-kit-compatible output itself,
    as well as generating the correct set of outputs for fully-joined
    and unbundled "traditional" outputs (in standard PEM and
    OpenSSL-compatible formats).

--- pkgs/applications/networking/browsers/firefox/common.nix
+++ pkgs/applications/networking/browsers/firefox/common.nix
@@ -117,9 +117,7 @@ let
                 then overrideCC stdenv llvmPackages.clangUseLLVM
                 else stdenv;
 
-  # Disable p11-kit support in nss until our cacert packages has caught up exposing CKA_NSS_MOZILLA_CA_POLICY
-  # https://github.com/NixOS/nixpkgs/issues/126065
-  nss_pkg = if lib.versionOlder version "83" then nss_3_53 else nss.override { useP11kit = false; };
+  nss_pkg = if lib.versionOlder version "83" then nss_3_53 else nss;
 
   # --enable-release adds -ffunction-sections & LTO that require a big amount of
   # RAM and the 32-bit memory space cannot handle that linking
