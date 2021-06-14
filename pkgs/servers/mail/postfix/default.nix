b57edd471a3ef3cc37d17eed2fd53711b52d781a
Robert Schütz <dev@schuetz-co.de>
2021-06-14 14:53:28 +0200

    postfix: 3.6.0 -> 3.6.1
    http://www.postfix.org/announcements/postfix-3.6.1.html

--- pkgs/servers/mail/postfix/default.nix
+++ pkgs/servers/mail/postfix/default.nix
@@ -26,11 +26,11 @@ in stdenv.mkDerivation rec {
 
   pname = "postfix";
 
-  version = "3.6.0";
+  version = "3.6.1";
 
   src = fetchurl {
     url = "ftp://ftp.cs.uu.nl/mirror/postfix/postfix-release/official/${pname}-${version}.tar.gz";
-    sha256 = "sha256-d0YolNdnHWPL5fwnM/lBCIUVptZxCLnxgIt9rjfoPC4=";
+    sha256 = "sha256-IKgFYlYB57lZiSIIMsj6FM43TwcR2gVBiPjOxqkv1xw=";
   };
 
   nativeBuildInputs = [ makeWrapper m4 ];
