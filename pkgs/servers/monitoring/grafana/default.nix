9f20e57d40c8173182fee8cf2389d00fe128c95b
Maximilian Bosch <maximilian@mbosch.me>
2021-06-15 12:16:55 +0200

    grafana: 8.0.1 -> 8.0.2
    ChangeLog: https://github.com/grafana/grafana/releases/tag/v8.0.2

--- pkgs/servers/monitoring/grafana/default.nix
+++ pkgs/servers/monitoring/grafana/default.nix
@@ -1,8 +1,8 @@
-{ lib, buildGoModule, fetchurl, fetchFromGitHub, nixosTests }:
+{ lib, buildGoModule, fetchurl, fetchFromGitHub, nixosTests, fetchpatch }:
 
 buildGoModule rec {
   pname = "grafana";
-  version = "8.0.1";
+  version = "8.0.2";
 
   excludedPackages = [ "release_publisher" ];
 
@@ -10,15 +10,24 @@ buildGoModule rec {
     rev = "v${version}";
     owner = "grafana";
     repo = "grafana";
-    sha256 = "sha256-Vs/4urH/XuoVly26YfhFbf/T3x5jdS4BgrVStuTHaHo=";
+    sha256 = "sha256-kCsrLZ0EbuMwqqDvUvhm8+B/vh6FpeJ5zkwste+qwyQ=";
   };
 
   srcStatic = fetchurl {
     url = "https://dl.grafana.com/oss/release/grafana-${version}.linux-amd64.tar.gz";
-    sha256 = "sha256-b05nUApLjdQW5vbS56HUK+/GXKcHo2UAHybfe6ZTr3U=";
+    sha256 = "sha256-QBMGLN3MjYJcv2vbs9GHfrCixcV7nH+Ox3o6/YtRYak=";
   };
 
-  vendorSha256 = "sha256-iwB1JtekxFYSHjaV+TqBDqnyE5zt3RJ4dQmf12AA53U=";
+  vendorSha256 = "sha256-x7sSVIim/TOhMTbnRK/fpgxiSRSO8KwGILTE2i1gU3U=";
+
+  patches = [
+    # https://github.com/grafana/grafana/pull/35635 (fixes declarative plugins for us)
+    (fetchpatch {
+      url = "https://github.com/grafana/grafana/commit/5b5cb948092bdb85e0378fd9ae01b564c4bf65f1.patch";
+      sha256 = "sha256-MnCjfLiHsBSWPcxVZ2dC4q8x1/TjzR8uyQhH2Jzgx7o=";
+      includes = [ "pkg/util/filepath.go" ];
+    })
+  ];
 
   preBuild = ''
     # The testcase makes an API call against grafana.com:
