9dc58496dd0d5657eccac4aea9031f39b9de59ab
Atemu <atemu.main@gmail.com>
2021-06-16 12:40:15 +0200

    openjdk: correct license

--- pkgs/development/compilers/openjdk/meta.nix
+++ pkgs/development/compilers/openjdk/meta.nix
@@ -1,6 +1,6 @@
 lib: with lib; {
   homepage = "http://openjdk.java.net/";
-  license = licenses.gpl2;
+  license = licenses.gpl2Only;
   description = "The open-source Java Development Kit";
   maintainers = with maintainers; [ edwtjo asbachb ];
   platforms = [ "i686-linux" "x86_64-linux" "aarch64-linux" "armv7l-linux" "armv6l-linux" ];
