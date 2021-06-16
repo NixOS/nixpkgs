0530cbdf2cf21933d37c4af1a21bef50bc5fbcfc
taku0 <mxxouy6x3m_github@tatapa.org>
2021-06-17 00:07:09 +0900

    firefox/update.nix: Use fingerprint instead of keyid
    (cherry picked from commit eaef28d6c42f01f459ef16c52d017ca8665fe70b)

--- pkgs/applications/networking/browsers/firefox/update.nix
+++ pkgs/applications/networking/browsers/firefox/update.nix
@@ -21,7 +21,7 @@ writeScript "update-${attrPath}" ''
   set -eux
   HOME=`mktemp -d`
   export GNUPGHOME=`mktemp -d`
-  gpg --receive-keys 61B7B526D98F0353
+  gpg --receive-keys 14F26682D0916CDD81E37B6D61B7B526D98F0353
 
   url=${baseUrl}
 
