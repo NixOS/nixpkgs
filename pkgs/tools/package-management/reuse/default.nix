77b31c9b4f2c3b1b20eee134f1b1995fa830a8c8
06kellyjac <dev@j-k.io>
2021-06-16 12:37:57 +0100

    reuse: 0.12.1 -> 0.13.0

--- pkgs/tools/package-management/reuse/default.nix
+++ pkgs/tools/package-management/reuse/default.nix
@@ -2,13 +2,13 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "reuse";
-  version = "0.12.1";
+  version = "0.13.0";
 
   src = fetchFromGitHub {
     owner = "fsfe";
     repo = "reuse-tool";
     rev = "v${version}";
-    sha256 = "0ql0krnz0fmq405r2qrm9ysm3cvmqfw14j06pny6na7qshibj78z";
+    sha256 = "sha256-AkOwsfTPVsOl+X0Eo1ErqWWyMBxUhnOpVwbYvJfGLTY=";
   };
 
   propagatedBuildInputs = with python3Packages; [
@@ -27,7 +27,12 @@ python3Packages.buildPythonApplication rec {
   meta = with lib; {
     description = "A tool for compliance with the REUSE Initiative recommendations";
     homepage = "https://github.com/fsfe/reuse-tool";
-    license = with licenses; [ asl20 cc-by-sa-40 cc0 gpl3Plus ];
+    license = with licenses; [
+      gpl3Plus # All original source code
+      asl20 # Some borrowed source code
+      cc-by-sa-40 # Documentation
+      cc0 # Some configuration and data files
+    ];
     maintainers = [ maintainers.FlorianFranzen ];
   };
 }
