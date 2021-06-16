34759acfaaf0e7b1001aec1298a1c03acdfed6a4
Joe Hermaszewski <git@monoid.al>
2021-06-16 13:16:23 +0800

    nix-linter: 2020-09-25 -> 2021-06-16
    Also add to haskellPackages

--- pkgs/development/tools/analysis/nix-linter/default.nix
+++ pkgs/development/tools/analysis/nix-linter/default.nix
@@ -17,18 +17,17 @@
 , containers
 , hnix
 , bytestring
-, fetchpatch
 }:
 
 mkDerivation rec {
   pname = "nix-linter-unstable";
-  version = "2020-09-25";
+  version = "2021-06-16";
 
   src = fetchFromGitHub {
     owner = "Synthetica9";
     repo = "nix-linter";
-    rev = "2516a8cda41f9bb553a1c3eca38e3dd94ebf53de";
-    sha256 = "07mn2c9v67wsm57jlxv9pqac9hahw4618vngmj2sfbgihx8997kb";
+    rev = "74707ed48dcc58dbfa27ae25ee0e044c072cc344";
+    sha256 = "17scghkinpx3pzlw3hw023ybnd8cy7bqfy8b48vwaq8a7bnm2rs3";
   };
 
   isLibrary = false;
@@ -36,14 +35,6 @@ mkDerivation rec {
   libraryHaskellDepends = [ fixplate ];
   executableHaskellDepends = [ streamly mtl path pretty-terminal text base aeson cmdargs containers hnix bytestring path-io ];
   testHaskellDepends = [ tasty tasty-hunit tasty-th ];
-
-  patches = [
-    # Fix compatibility with hnix≥0.13.0 https://github.com/Synthetica9/nix-linter/pull/51
-    (fetchpatch {
-      url = "https://github.com/Synthetica9/nix-linter/commit/f73acacd8623dc25c9a35f8e04e4ff33cc596af8.patch";
-      sha256 = "139fm21hdg3vcw8hv35kxj4awd52bjqbb76mpzx191hzi9plj8qc";
-    })
-  ];
 
   description = "Linter for Nix(pkgs), based on hnix";
   homepage = "https://github.com/Synthetica9/nix-linter";
