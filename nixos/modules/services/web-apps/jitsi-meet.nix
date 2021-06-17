27d38294c6a8cae5f5138225fca13e7498f06d20
illustris <rharikrishnan95@gmail.com>
2021-06-17 23:44:10 +0530

    nixos/jitsi-meet: Update jitsi prosody configs
    Changes made as per https://github.com/jitsi/jitsi-meet/commit/b6f7f8fba7ba81c5a0ab5477a1d103b7279bf38f
    
    (cherry picked from commit 34b9ba2e6161e68febe4312c96c089d707472d5e)

--- nixos/modules/services/web-apps/jitsi-meet.nix
+++ nixos/modules/services/web-apps/jitsi-meet.nix
@@ -187,8 +187,8 @@ in
       ];
       extraModules = [ "pubsub" ];
       extraConfig = mkAfter ''
-        Component "focus.${cfg.hostName}"
-          component_secret = os.getenv("JICOFO_COMPONENT_SECRET")
+        Component "focus.${cfg.hostName}" "client_proxy"
+          target_address = "focus@auth.${cfg.hostName}"
       '';
       virtualHosts.${cfg.hostName} = {
         enabled = true;
@@ -254,6 +254,7 @@ in
       + optionalString cfg.prosody.enable ''
         ${config.services.prosody.package}/bin/prosodyctl register focus auth.${cfg.hostName} "$(cat /var/lib/jitsi-meet/jicofo-user-secret)"
         ${config.services.prosody.package}/bin/prosodyctl register jvb auth.${cfg.hostName} "$(cat ${videobridgeSecret})"
+        ${config.services.prosody.package}/bin/prosodyctl mod_roster_command subscribe focus.${cfg.hostName} focus@auth.${cfg.hostName}
 
         # generate self-signed certificates
         if [ ! -f /var/lib/jitsi-meet.crt ]; then
