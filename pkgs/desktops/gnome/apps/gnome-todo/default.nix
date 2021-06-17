890449fca4647ecf66c80a8178fa726fdade3e03
Jan Tojnar <jtojnar@gmail.com>
2021-06-17 20:41:55 +0200

    gnome.gnome-todo: 40.0 -> 40.1
    https://ftp.gnome.org/pub/GNOME/sources/gnome-todo/40/gnome-todo-40.1.news

--- pkgs/desktops/gnome/apps/gnome-todo/default.nix
+++ pkgs/desktops/gnome/apps/gnome-todo/default.nix
@@ -25,11 +25,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-todo";
-  version = "40.0";
+  version = "40.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "aAl8lvBnXHFCZn0QQ0ToNHLdf8xTj+wKzb9gJrucobE=";
+    sha256 = "A0tCNN7BCCuZnngnkvn+4wUeHkOSCL9G6nBu+f5YLuQ=";
   };
 
   nativeBuildInputs = [
@@ -62,10 +62,6 @@ stdenv.mkDerivation rec {
   postPatch = ''
     chmod +x build-aux/meson/meson_post_install.py
     patchShebangs build-aux/meson/meson_post_install.py
-
-    # https://gitlab.gnome.org/GNOME/gnome-todo/merge_requests/103
-    substituteInPlace src/meson.build \
-      --replace 'Gtk-3.0' 'Gtk-4.0'
   '';
 
   passthru = {
