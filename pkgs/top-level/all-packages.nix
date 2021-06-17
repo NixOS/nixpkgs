e2fa7038adf8b15dd72df2708dca4786d835921c
adisbladis <adisbladis@gmail.com>
2021-06-17 13:46:06 -0500

    citrix_workspace: add 21.04.0.11
    ChangeLog: https://docs.citrix.com/en-us/citrix-workspace-app-for-linux/whats-new.html#whats-new-in-2104

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3604,7 +3604,7 @@ in
 
   circus = callPackage ../tools/networking/circus { };
 
-  citrix_workspace = citrix_workspace_21_03_0;
+  citrix_workspace = citrix_workspace_21_04_0;
 
   inherit (callPackage ../applications/networking/remote/citrix-workspace { })
     citrix_workspace_20_04_0
@@ -3614,6 +3614,7 @@ in
     citrix_workspace_20_12_0
     citrix_workspace_21_01_0
     citrix_workspace_21_03_0
+    citrix_workspace_21_04_0
   ;
 
   citra = libsForQt5.callPackage ../misc/emulators/citra { };
