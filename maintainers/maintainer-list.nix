444d3e4003db054912911a1d260c561385b72a41
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2021-06-17 15:32:37 +0530

    maintainer: add SubhrajyotiSen

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9864,6 +9864,12 @@
       githubId = 39732259;
       name = "Justus K";
   };
+  SubhrajyotiSen = {
+      email = "subhrajyoti12@gmail.com";
+      github = "SubhrajyotiSen";
+      githubId = 12984845;
+      name = "Subhrajyoti Sen";
+  };
   suhr = {
     email = "suhr@i2pmail.org";
     github = "suhr";
