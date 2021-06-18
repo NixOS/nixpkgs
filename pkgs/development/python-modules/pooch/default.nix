1d137424d62aa461ba47450b8fd6423ba4a44b8b
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-06-18 23:47:30 +0200

    python3Packages.pooch: 1.3.0 -> 1.4.0

--- pkgs/development/python-modules/pooch/default.nix
+++ pkgs/development/python-modules/pooch/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "pooch";
-  version = "1.3.0";
+  version = "1.4.0";
   disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "30d448e825904e2d763bbbe418831a788813c32f636b21c8d60ee5f474532898";
+    sha256 = "f827e79ab51b21a8964a4b1ea8972aa4a1079cb9c1ff8e9ec61893eb7dab50cb";
   };
 
   propagatedBuildInputs = [ packaging appdirs requests ];
