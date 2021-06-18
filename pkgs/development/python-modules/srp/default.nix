9190ad02fc732a80f3d585c17b97982a55b27a2f
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-06-18 23:47:39 +0200

    python3Packages.srp: 1.0.17 -> 1.0.18

--- pkgs/development/python-modules/srp/default.nix
+++ pkgs/development/python-modules/srp/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "srp";
-  version = "1.0.17";
+  version = "1.0.18";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f2e3fed4e5cbfd6b481edc9cdd51a8c39a609eae117210218556004bdc9016b2";
+    sha256 = "1582317ccd383dc39d54f223424c588254d73d1cfb2c5c24d945e018ec9516bb";
   };
 
   propagatedBuildInputs = [ six ];
