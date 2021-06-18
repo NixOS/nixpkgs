5e49c97d29455e39ba820228dc7ff7b034ecc6fb
illustris <rharikrishnan95@gmail.com>
2021-06-18 10:05:54 +0530

    jitsi-meet-prosody: init at 1.0.5056

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19345,6 +19345,8 @@ in
 
   jitsi-meet = callPackage ../servers/web-apps/jitsi-meet { };
 
+  jitsi-meet-prosody = callPackage ../misc/jitsi-meet-prosody { };
+
   jitsi-videobridge = callPackage ../servers/jitsi-videobridge { };
 
   kapowbang = callPackage ../servers/kapowbang { };
