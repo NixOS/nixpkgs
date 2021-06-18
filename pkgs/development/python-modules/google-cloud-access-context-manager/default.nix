43ca7708ce00c65f704481cdb0964cb6436f1aed
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-06-18 23:47:18 +0200

    python3Packages.google-cloud-access-context-manager: 0.1.2 -> 0.1.3

--- pkgs/development/python-modules/google-cloud-access-context-manager/default.nix
+++ pkgs/development/python-modules/google-cloud-access-context-manager/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-access-context-manager";
-  version = "0.1.2";
+  version = "0.1.3";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1qy7wv1xn7g3x5z0vvv0pwmxhin4hw2m9fs9iklnghy00vg37v0b";
+    sha256 = "5453e64ca885c302f54c7a630de15e1e0b2c5ef21107ea69bd96fd694d723d5e";
   };
 
   propagatedBuildInputs = [ google-api-core ];
