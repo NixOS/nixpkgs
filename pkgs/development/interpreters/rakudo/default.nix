24eceb46b78f1d1e0b8807dceb26e1589c346ecc
Stig Palmquist <stig@stig.io>
2021-06-18 22:02:37 +0200

    rakudo: remove references to stdenv.cc.cc
    Rakudo depends on gcc noticeably increasing closure size, this commit
    removes the references.
    
    Fixes #101097

--- pkgs/development/interpreters/rakudo/default.nix
+++ pkgs/development/interpreters/rakudo/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, perl, icu, zlib, gmp, lib, nqp }:
+{ stdenv, fetchurl, perl, icu, zlib, gmp, lib, nqp, removeReferencesTo }:
 
 stdenv.mkDerivation rec {
   pname = "rakudo";
@@ -9,12 +9,19 @@ stdenv.mkDerivation rec {
     sha256 = "11ixlqmvbb37abksdysg5r4lkbwzr486lkc0ssl3wca4iiy3mhgf";
   };
 
+  nativeBuildInputs = [ removeReferencesTo ];
+
   buildInputs = [ icu zlib gmp perl ];
   configureScript = "perl ./Configure.pl";
   configureFlags = [
     "--backends=moar"
     "--with-nqp=${nqp}/bin/nqp"
   ];
+
+  disallowedReferences = [ stdenv.cc.cc ];
+  postFixup = ''
+    remove-references-to -t ${stdenv.cc.cc} "$(readlink -f $out/share/perl6/runtime/dynext/libperl6_ops_moar.so)"
+  '';
 
   meta = with lib; {
     description = "Raku implementation on top of Moar virtual machine";
