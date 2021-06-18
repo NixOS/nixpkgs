64188f95d80aef8ddb1f7d5c7b86a1ed2bdd75fa
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-06-18 23:47:10 +0200

    python3Packages.bitarray: 2.1.0 -> 2.1.3

--- pkgs/development/python-modules/bitarray/default.nix
+++ pkgs/development/python-modules/bitarray/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "bitarray";
-  version = "2.1.0";
+  version = "2.1.3";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-lyJKGTJezuSaO/TfPuBTHTr5zyiLZ9CJp+9Eo8TqODk=";
+    sha256 = "a24aff72a7f1b09571b5daf9dbfcffd98481be1fe085ae5ef662cf11452a97e0";
   };
 
   checkPhase = ''
