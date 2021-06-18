7282b4fc0389217c60fe788cb80d10f3e80e447d
Michael Ashton <data@gtf.org>
2021-06-17 23:36:09 -0700

    unison: patch 2.51.3
    Add patch which fixes builds with (at least) ocaml 4.12.0, and
    remove build constraint for 4.09.  Necessary because unison built
    with 4.09 is not compatible with unison built with 4.12, e.g. on
    recent Homebrew.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27157,7 +27157,6 @@ in
   unipicker = callPackage ../applications/misc/unipicker { };
 
   unison = callPackage ../applications/networking/sync/unison {
-    ocamlPackages = ocaml-ng.ocamlPackages_4_09;
     enableX11 = config.unison.enableX11 or true;
   };
 
