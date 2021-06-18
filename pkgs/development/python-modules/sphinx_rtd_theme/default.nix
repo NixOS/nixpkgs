6a10bd91289799ba0b784454a40d5dfd99d9ff80
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-06-18 23:47:39 +0200

    python3Packages.sphinx_rtd_theme: 0.5.1 -> 0.5.2

--- pkgs/development/python-modules/sphinx_rtd_theme/default.nix
+++ pkgs/development/python-modules/sphinx_rtd_theme/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "sphinx_rtd_theme";
-  version = "0.5.1";
+  version = "0.5.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "19c31qhfiqbm6y7mamglrc2mc7l6n4lasb8jry01lc67l3nqk9pd";
+    sha256 = "32bd3b5d13dc8186d7a42fc816a23d32e83a4827d7d9882948e7b837c232da5a";
   };
 
   propagatedBuildInputs = [ sphinx ];
