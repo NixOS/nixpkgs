dd28b4c270f65fc2e4564dac3db086a64ef32711
Sandro <sandro.jaeckel@gmail.com>
2021-06-18 20:13:52 +0200

    Apply suggestions from code review

--- pkgs/development/libraries/libvori/default.nix
+++ pkgs/development/libraries/libvori/default.nix
@@ -4,12 +4,12 @@ stdenv.mkDerivation rec {
   pname = "libvori";
   version = "210412";
 
-  nativeBuildInputs = [ cmake ];
-
   src = fetchurl {
     url = "https://brehm-research.de/files/${pname}-${version}.tar.gz";
     sha256 = "1b4hpwibf3k7gl6n984l3wdi0zyl2fmpz84m9g2di4yhm6p8c61k";
   };
+
+  nativeBuildInputs = [ cmake ];
 
   meta = with lib; {
     description = "Library for Voronoi intergration of electron densities";
