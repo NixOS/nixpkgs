2b541568640f435e8fb573920540c766a5317f6a
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-06-18 23:47:31 +0200

    python3Packages.psycopg2: 2.8.6 -> 2.9.1

--- pkgs/development/python-modules/psycopg2/default.nix
+++ pkgs/development/python-modules/psycopg2/default.nix
@@ -2,7 +2,7 @@
 
 buildPythonPackage rec {
   pname = "psycopg2";
-  version = "2.8.6";
+  version = "2.9.1";
 
   # Extension modules don't work well with PyPy. Use psycopg2cffi instead.
   # c.f. https://github.com/NixOS/nixpkgs/pull/104151#issuecomment-729750892
@@ -10,7 +10,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "fb23f6c71107c37fd667cb4ea363ddeb936b348bbd6449278eb92c189699f543";
+    sha256 = "de5303a6f1d0a7a34b9d40e4d3bef684ccc44a49bbe3eb85e3c0bffb4a131b7c";
   };
 
   buildInputs = lib.optional stdenv.isDarwin openssl;
