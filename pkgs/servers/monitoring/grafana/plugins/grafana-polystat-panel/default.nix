874dc9e411cb05874533e3190d8ab4d14bd9f0da
Luke Granger-Brown <git@lukegb.com>
2021-06-18 10:18:11 +0000

    grafanaPlugins.grafana-polystat-panel: 1.2.3 → 1.2.4

--- pkgs/servers/monitoring/grafana/plugins/grafana-polystat-panel/default.nix
+++ pkgs/servers/monitoring/grafana/plugins/grafana-polystat-panel/default.nix
@@ -2,8 +2,8 @@
 
 grafanaPlugin rec {
   pname = "grafana-polystat-panel";
-  version = "1.2.3";
-  zipHash = "sha256-Eu3dTPfJxAWwqqGT6l4El4MqviBbzERarU30OXU+eEM=";
+  version = "1.2.4";
+  zipHash = "sha256-gXPnblLrhxUTAqVFOnzaMXyOAnUBbONTqWJXozbnD0M=";
   meta = with lib; {
     description = "Hexagonal multi-stat panel for Grafana";
     license = licenses.asl20;
