ecac186bb2afb3485995c341d896eed0cbfb9f8d
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-06-18 23:47:29 +0200

    python3Packages.pg8000: 1.19.4 -> 1.19.5

--- pkgs/development/python-modules/pg8000/default.nix
+++ pkgs/development/python-modules/pg8000/default.nix
@@ -8,12 +8,12 @@
 
 buildPythonPackage rec {
   pname = "pg8000";
-  version = "1.19.4";
+  version = "1.19.5";
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-fJxtV1QbDyFT4jqNdZzrPXy5MIkY+6atnpL44OWpC8g=";
+    sha256 = "dcb9afb9cb87acc79388ae633c45cf7f806aabe3bb5d8cf154572b699393f937";
   };
 
   propagatedBuildInputs = [
