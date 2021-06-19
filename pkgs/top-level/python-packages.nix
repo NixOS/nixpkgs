07d5520ce2e8f6ae4bad34fc4f8cb1db4004b6da
Robert Schütz <dev@schuetz-co.de>
2021-06-19 13:28:15 +0200

    python3Packages.hangups: init at 0.4.14

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3174,6 +3174,8 @@ in {
 
   handout = callPackage ../development/python-modules/handout { };
 
+  hangups = callPackage ../development/python-modules/hangups { };
+
   hap-python = callPackage ../development/python-modules/hap-python { };
 
   hass-nabucasa = callPackage ../development/python-modules/hass-nabucasa { };
