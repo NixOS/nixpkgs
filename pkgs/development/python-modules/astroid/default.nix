7fa6a626ab0c1b2fa4f7939743238b4b9562eafd
Jonathan Ringer <jonringer117@gmail.com>
2021-06-18 18:51:50 -0700

    Revert "python3Packages.astroid: 2.5.1 -> 2.5.8"
    This reverts commit 62006ddeb7d2879f5782f17c5a36afa9c5dbfa95.

--- pkgs/development/python-modules/astroid/default.nix
+++ pkgs/development/python-modules/astroid/default.nix
@@ -11,13 +11,13 @@
 
 buildPythonPackage rec {
   pname = "astroid";
-  version = "2.5.8";
+  version = "2.5.1";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "2476b7f0d6cec13f4c1f53b54bea2ce072310ac9fc7acb669d5270190c748042";
+    sha256 = "cfc35498ee64017be059ceffab0a25bedf7548ab76f2bea691c5565896e7128d";
   };
 
   # From astroid/__pkginfo__.py
