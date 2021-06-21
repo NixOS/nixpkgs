5d497fd7dde31053e6dcbb1288063e4307ab009c
Ethan Edwards <ethan@ethancedwards.com>
2021-06-20 21:15:49 -0400

    guile-ssh: init at 0.13.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12712,6 +12712,8 @@ in
 
   guile-sdl2 = callPackage ../development/guile-modules/guile-sdl2 { };
 
+  guile-ssh = callPackage ../development/guile-modules/guile-ssh { };
+
   guile-xcb = callPackage ../development/guile-modules/guile-xcb {
     guile = guile_2_0;
   };
