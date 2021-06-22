ca618d6401631cf2c82c81220c54e774b514297f
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-06-22 16:14:07 +0200

    ansible, ansible_2_10: internalize collections package
    This drops the python3Packages.ansible-collections attribute in favor of
    a local callPackage that overwrites the collections package per ansible
    version.

--- pkgs/development/python-modules/ansible/core.nix
+++ pkgs/development/python-modules/ansible/core.nix
@@ -1,8 +1,8 @@
 { lib
+, callPackage
 , buildPythonPackage
 , fetchPypi
 , installShellFiles
-, ansible-collections
 , cryptography
 , jinja2
 , junit-xml
@@ -21,6 +21,12 @@
 , xmltodict
 }:
 
+let
+  ansible-collections = callPackage ./collections.nix {
+    version = "4.1.0";
+    sha256 = "0rrivq1g0vizah8zmf012lzig2xxfk5x1371k16s3nn4zfkwqqgm";
+  };
+in
 buildPythonPackage rec {
   pname = "ansible-core";
   version = "2.11.2";
@@ -73,6 +79,10 @@ buildPythonPackage rec {
 
   # internal import errors, missing dependencies
   doCheck = false;
+
+  passthru = {
+    collections = ansible-collections;
+  };
 
   meta = with lib; {
     description = "Radically simple IT automation";
