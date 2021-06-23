5555f34784ef4013e207097f9da8f2861eb5b9e3
Fabian Affolter <mail@fabian-affolter.ch>
2021-06-23 23:14:29 +0200

    python3Packages.feedparser: 6.0.6 -> 6.0.8

--- pkgs/development/python-modules/feedparser/default.nix
+++ pkgs/development/python-modules/feedparser/default.nix
@@ -1,18 +1,18 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, isPy27
+, pythonOlder
 , sgmllib3k
 }:
 
 buildPythonPackage rec {
   pname = "feedparser";
-  version = "6.0.6";
-  disabled = isPy27;
+  version = "6.0.8";
+  disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1syd15460a6m397rajdpbr9q0hgxm1j6xf9ba18z9pighxdjmxkq";
+    sha256 = "sha256-XOBBCgWrJIyMfPyjoOoiA5aO6f9EhgZzea9IJ6WflmE=";
   };
 
   propagatedBuildInputs = [ sgmllib3k ];
@@ -21,11 +21,12 @@ buildPythonPackage rec {
     python -Wd tests/runtests.py
   '';
 
+  pythonImportsCheck = [ "feedparser" ];
+
   meta = with lib; {
     homepage = "https://github.com/kurtmckee/feedparser";
     description = "Universal feed parser";
     license = licenses.bsd2;
     maintainers = with maintainers; [ domenkozar ];
   };
-
 }
