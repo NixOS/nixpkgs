6d5dbb02a20dd387bb3047c6d4b3a26feb130671
R. RyanTM <ryantm-bot@ryantm.com>
2021-06-23 03:51:27 +0000

    fission: 1.13.0 -> 1.13.1

--- pkgs/development/tools/fission/default.nix
+++ pkgs/development/tools/fission/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "fission";
-  version = "1.13.0";
+  version = "1.13.1";
 
   src = fetchFromGitHub {
     owner = "fission";
     repo = "fission";
     rev = version;
-    sha256 = "sha256-WCynU5+d789d584JH99jmTZSEbyco2ysuBXIC2pW1r4=";
+    sha256 = "sha256-ayVEU2Dlqun8KLP+KeI0uU4p9N4aaYLZ/IHqfA2PGrI=";
   };
 
   vendorSha256 = "sha256-V3/IFCbW3wXfNiFzucLeyFDc6SA2nE+NwO0sNEBmIYg=";
