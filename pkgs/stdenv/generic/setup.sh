5f015430709fe441efdf60834cafe912c7dd91b7
happysalada <raphael@megzari.com>
2021-06-23 16:34:11 +0900

    setup.sh: fatal: Invalid integer constant 'relHostOffset'
    dereference variables passed to mapOffset

--- pkgs/stdenv/generic/setup.sh
+++ pkgs/stdenv/generic/setup.sh
@@ -405,7 +405,7 @@ findInputs() {
 
         # Host offset relative to the package currently being
         # built---as absolute an offset as will be used.
-        mapOffset relHostOffset
+        mapOffset "$relHostOffset"
         local hostOffsetNext="$mapOffsetResult"
 
         # Ensure we're in bounds relative to the package currently
@@ -424,7 +424,7 @@ findInputs() {
 
             # Target offset relative to the package currently being
             # built.
-            mapOffset relTargetOffset
+            mapOffset "$relTargetOffset"
             local targetOffsetNext="$mapOffsetResult"
 
             # Once again, ensure we're in bounds relative to the
