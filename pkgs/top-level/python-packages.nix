cb6b93e29963405ce41d3fd3734ffe31f9c0c8c4
Robert Schütz <dev@schuetz-co.de>
2021-06-24 00:10:16 +0200

    python3Packages.python-juicenet: init at 1.0.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5351,6 +5351,8 @@ in {
 
   python-izone = callPackage ../development/python-modules/python-izone { };
 
+  python-juicenet = callPackage ../development/python-modules/python-juicenet { };
+
   python-openems = callPackage ../development/python-modules/python-openems { };
 
   python-openzwave-mqtt = callPackage ../development/python-modules/python-openzwave-mqtt { };
