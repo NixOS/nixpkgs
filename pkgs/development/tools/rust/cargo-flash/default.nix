d1790a80b9a7e1009a9680bfd30c956130648154
R. RyanTM <ryantm-bot@ryantm.com>
2021-06-24 05:58:52 +0000

    cargo-flash: 0.10.1 -> 0.11.0

--- pkgs/development/tools/rust/cargo-flash/default.nix
+++ pkgs/development/tools/rust/cargo-flash/default.nix
@@ -11,16 +11,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-flash";
-  version = "0.10.1";
+  version = "0.11.0";
 
   src = fetchFromGitHub {
     owner = "probe-rs";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-aQ5647woODs/A4fcxSsQoQHL6YQ0TpfQFegtXETqlHk=";
+    sha256 = "sha256-yTtnRdDy3wGBe0SlO0165uooWu6ZMhUQw3hdDUK1e8A=";
   };
 
-  cargoSha256 = "sha256-P7xyg9I1MhmiKlyAI9cvABcYKNxB6TSvTgMsMk5KxAQ=";
+  cargoSha256 = "sha256-f5vUMdyz3vDh2yE0pMKZiknsqTAKkuvTCtlgb6/gaLc=";
 
   nativeBuildInputs = [ pkg-config rustfmt ];
   buildInputs = [ libusb1 openssl ] ++ lib.optionals stdenv.isDarwin [ Security ];
