ceb00f6ea663a509fff128ae7477d072e9563e13
Jonathan Ringer <jonringer117@gmail.com>
2021-06-24 00:14:57 -0700

    libpqxx_6: init at 6.4.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16804,6 +16804,7 @@ in
   libpfm = callPackage ../development/libraries/libpfm { };
 
   libpqxx = callPackage ../development/libraries/libpqxx { };
+  libpqxx_6 = callPackage ../development/libraries/libpqxx/6.nix { };
 
   inherit (callPackages ../development/libraries/prometheus-client-c {
     stdenv = gccStdenv; # Required for darwin
