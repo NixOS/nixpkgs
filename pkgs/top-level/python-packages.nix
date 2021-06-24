d8e767e7c67a413cdd9b4e8cd0dc8063e9479a4e
Fabian Affolter <mail@fabian-affolter.ch>
2021-06-24 10:03:10 +0200

    python3Packages.dacite: init at 1.6.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1763,6 +1763,8 @@ in {
 
   d2to1 = callPackage ../development/python-modules/d2to1 { };
 
+  dacite = callPackage ../development/python-modules/dacite { };
+
   daemonize = callPackage ../development/python-modules/daemonize { };
 
   daemonocle = callPackage ../development/python-modules/daemonocle { };
