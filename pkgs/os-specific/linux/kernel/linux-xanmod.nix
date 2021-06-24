ddced1169ea2fa6db47fa4f59ff5e3b8ed4276f0
fortuneteller2k <lythe1107@gmail.com>
2021-06-24 19:07:18 +0800

    linux_xanmod: enable preemption
    (cherry picked from commit 167fde46fc34e9cb8a7dc39b14c16be20e77ca9b)

--- pkgs/os-specific/linux/kernel/linux-xanmod.nix
+++ pkgs/os-specific/linux/kernel/linux-xanmod.nix
@@ -15,6 +15,11 @@ buildLinux (args // rec {
     sha256 = "sha256-99gVqdYhnBx3MDTCCHbxsljmvi+DixHp19vtNwCRM/M=";
   };
 
+  structuredExtraConfig = with lib.kernel; {
+    PREEMPT = lib.mkForce yes;
+    PREEMPT_VOLUNTARY = lib.mkForce no;
+  };
+
   extraMeta = {
     branch = "5.12-cacule";
     maintainers = with lib.maintainers; [ fortuneteller2k ];
