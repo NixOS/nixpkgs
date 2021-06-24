419e4299a6a393fb514048dfe57a391f939a721b
fortuneteller2k <lythe1107@gmail.com>
2021-06-24 11:45:02 +0800

    linux_xanmod: 5.12.12 -> 5.12.13

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20779,7 +20779,6 @@ in
     kernelPatches = [
       kernelPatches.bridge_stp_helper
       kernelPatches.request_key_helper
-      kernelPatches.rtnetlink_fix_regression_in_bridge_vlan_configuration
     ];
   };
 
