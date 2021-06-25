82e726bcca671a56ccd64d2bb58950af2178d479
Phillip <sheepforce@users.noreply.github.com>
2021-06-25 18:53:00 +0200

    pyscf: init at 1.7.6 (#127880)
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6461,6 +6461,8 @@ in {
     inherit (pkgs.darwin.apple_sdk.frameworks) PCSC;
   };
 
+  pyscf = callPackage ../development/python-modules/pyscf { };
+
   pyschedule = callPackage ../development/python-modules/pyschedule { };
 
   pyscreenshot = callPackage ../development/python-modules/pyscreenshot { };
