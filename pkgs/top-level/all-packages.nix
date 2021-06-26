0bef1bb91fee138649b26e46cb2c55da2ad47bf3
Leo Maroni <git@em0lar.de>
2021-06-26 21:56:45 +0200

    vikunja-api: init at 0.17.1
    vikunja-api: use mage in build (thanks to @jtojnar)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31699,6 +31699,7 @@ in
   vimb-unwrapped = callPackage ../applications/networking/browsers/vimb { };
   vimb = wrapFirefox vimb-unwrapped { };
 
+  vikunja-api = callPackage ../servers/web-apps/vikunja/api.nix { };
   vikunja-frontend = callPackage ../servers/web-apps/vikunja/frontend.nix { };
 
   vips = callPackage ../tools/graphics/vips {
