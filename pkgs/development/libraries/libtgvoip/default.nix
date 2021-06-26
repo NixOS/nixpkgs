0d509d366d4fc692a9dc81366445e29da81202e6
Michael Weiss <dev.primeos@gmail.com>
2021-06-26 16:17:07 +0200

    tdesktop: 2.7.5 -> 2.8.0

--- pkgs/development/libraries/libtgvoip/default.nix
+++ pkgs/development/libraries/libtgvoip/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, lib, fetchFromGitHub, pkg-config, autoreconfHook
+{ stdenv, lib, fetchFromGitHub, fetchpatch
+, pkg-config, autoreconfHook
 , openssl, libopus, alsa-lib, libpulseaudio
 }:
 
@@ -6,14 +7,30 @@ with lib;
 
 stdenv.mkDerivation rec {
   pname = "libtgvoip";
-  version = "unstable-2020-03-02";
+  version = "unstable-2021-01-01";
 
   src = fetchFromGitHub {
     owner = "telegramdesktop";
     repo = "libtgvoip";
-    rev = "e422d2a80546a32ab7166a9b1058bacfc5daeefc";
-    sha256 = "0n6f7215k74039j0zmicjzhj6f45mq6fvkrwzyzibcrv87ib17fc";
+    rev = "13a5fcb16b04472d808ce122abd695dbf5d206cd";
+    sha256 = "12p6s7vxkf1gh1spdckkdxrx7bjzw881ds9bky7l5fw751cwb3xd";
   };
+
+  # To fix the build without external webrtc:
+  patches = [
+    (fetchpatch {
+      # Use methods from updated webrtc.
+      url = "https://github.com/telegramdesktop/libtgvoip/commit/13a5fcb16b04472d808ce122abd695dbf5d206cd.patch";
+      sha256 = "0wapqvml3yyv5dlp2q8iih5rfvfnkngll69krhnw5xsdjy22sp7r";
+      revert = true;
+    })
+    (fetchpatch {
+      # Allow working with external webrtc.
+      url = "https://github.com/telegramdesktop/libtgvoip/commit/6e82b6e45664c1f80b9039256c99bebc76d34672.patch";
+      sha256 = "0m87ixja70vnm80a9z4gxk0yl7n64y59smczxb88lxnj6kdgih7x";
+      revert = true;
+    })
+  ];
 
   outputs = [ "out" "dev" ];
 
