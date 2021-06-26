0b99ed93f941d553c1d14d55a3c59708b445c701
Sandro <sandro.jaeckel@gmail.com>
2021-06-26 21:55:50 +0200

    Apply suggestions from code review

--- pkgs/development/python-modules/pycritty/default.nix
+++ pkgs/development/python-modules/pycritty/default.nix
@@ -3,7 +3,6 @@
 buildPythonPackage rec {
   pname = "pycritty";
   version = "0.3.5";
-
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
@@ -11,18 +10,18 @@ buildPythonPackage rec {
     sha256 = "1lrmd4a1ps3h9z0pndfjfrd2qa7v3abd6np75fd2q2ffsqv7ar6x";
   };
 
-  # The package does not include any tests to run
-  doCheck = false;
-
-  pythonImportsCheck = [ "pycritty" ];
-
-  propagatedBuildInputs = [ pyyaml ];
-
   postPatch = ''
     # remove custom install
     substituteInPlace setup.py \
       --replace "'install': PostInstallHook," ""
   '';
+
+  propagatedBuildInputs = [ pyyaml ];
+
+  # The package does not include any tests to run
+  doCheck = false;
+
+  pythonImportsCheck = [ "pycritty" ];
 
   meta = with lib; {
     description = "A CLI tool for changing your alacritty configuration on the fly";
