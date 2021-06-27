664d835d7ecbe173fe11d8089575a14c60f6f9ee
R. RyanTM <ryantm-bot@ryantm.com>
2021-06-27 18:09:41 +0000

    ginkgo: 1.16.3 -> 1.16.4

--- pkgs/development/tools/ginkgo/default.nix
+++ pkgs/development/tools/ginkgo/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "ginkgo";
-  version = "1.16.3";
+  version = "1.16.4";
 
   src = fetchFromGitHub {
     owner = "onsi";
     repo = "ginkgo";
     rev = "v${version}";
-    sha256 = "sha256-MDxDDZaNgjZz98lAoIIpWbIg7Cr/6kohSh+WL/1Ljyo=";
+    sha256 = "sha256-p9kam1pRP0Am02o7vM+VzeAht+Qtn4DZ12NM8TaA/2Y=";
   };
   vendorSha256 = "sha256-tS8YCGVOsfQp02vY6brmE3pxi70GG9DYcp1JDkcVG9Y=";
   doCheck = false;
