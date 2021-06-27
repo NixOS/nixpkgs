592643a217395ff15b1fea5318cd9a2166e0645d
Matt Wittmann <mcwitt@gmail.com>
2021-06-26 21:18:47 -0700

    Manually patch to add icu path

--- pkgs/applications/networking/aws-client-vpn/default.nix
+++ pkgs/applications/networking/aws-client-vpn/default.nix
@@ -1,9 +1,9 @@
 { stdenv
 , lib
-, autoPatchelfHook
 , curl
 , dpkg
 , fetchurl
+, icu
 , kerberos
 , lttng-ust
 , zlib
@@ -18,18 +18,7 @@ stdenv.mkDerivation rec {
     sha256 = "0nxmx83wbzgx648fcw5rqbdzi6v93qkpcnbb6qf1z6zzpg17c4br";
   };
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-    dpkg
-  ];
-
-  buildInputs = [
-    curl
-    kerberos
-    lttng-ust
-    stdenv.cc.cc.lib
-    zlib
-  ];
+  nativeBuildInputs = [ dpkg ];
 
   unpackPhase = ''
     dpkg -x $src .
@@ -42,6 +31,24 @@ stdenv.mkDerivation rec {
     ln -s "$out/awsvpnclient/AWS VPN Client" "$out/bin/AWS VPN Client"
     ln -s "$out/awsvpnclient/Service/ACVC.GTK.Service" "$out/bin/ACVC.GTK.Service"
   '';
+
+  postFixup = let libPath = lib.makeLibraryPath [
+    curl
+    icu
+    kerberos
+    lttng-ust
+    stdenv.cc.cc.lib
+    zlib
+  ]; in
+    ''
+      for file in "$out/awsvpnclient/AWS VPN Client" $out/awsvpnclient/Service/ACVC.GTK.Service; do
+        patchelf --set-interpreter "${stdenv.cc.bintools.dynamicLinker}" --set-rpath ${libPath} "$file"
+      done
+
+      while IFS= read -r -d $'\0' file; do
+        patchelf --set-rpath ${libPath} "$file"
+      done < <(find $out/awsvpnclient -name '*.so' -print0)
+    '';
 
   meta = with lib; {
     homepage = https://docs.aws.amazon.com/vpn/latest/clientvpn-user/client-vpn-user-what-is.html;
