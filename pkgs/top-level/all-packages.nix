a06aeca3bbefe5ea08604387eeeba68e64650acd
markuskowa <markus.kowalewski@gmail.com>
2021-06-27 18:06:11 +0200

    libint: init at 2.6.0 (#128083)
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>
    Co-authored-by: Robert Hensing <roberth@users.noreply.github.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6621,6 +6621,8 @@ in
 
   libinsane = callPackage ../development/libraries/libinsane { };
 
+  libint = callPackage ../development/libraries/libint {};
+
   libipfix = callPackage ../development/libraries/libipfix { };
 
   libircclient = callPackage ../development/libraries/libircclient { };
