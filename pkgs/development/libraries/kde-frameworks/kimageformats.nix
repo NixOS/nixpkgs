254a1f32a6a4664b7a6072c9f7419f1802b3a5a5
Vladimír Čunát <v@cunat.cz>
2021-06-28 17:19:39 +0200

    libsForQt5.kimageformats: fix build after libavif update

--- pkgs/development/libraries/kde-frameworks/kimageformats.nix
+++ pkgs/development/libraries/kde-frameworks/kimageformats.nix
@@ -1,5 +1,5 @@
 {
-  mkDerivation, lib,
+  mkDerivation, lib, fetchpatch,
   extra-cmake-modules,
   ilmbase, karchive, openexr, libavif, qtbase
 }:
@@ -8,6 +8,15 @@ let inherit (lib) getDev; in
 
 mkDerivation {
   name = "kimageformats";
+
+  patches = [
+    (fetchpatch { # included in kimageformats >= 5.83
+      name = "avif-0.9.2.diff";
+      url = "https://invent.kde.org/frameworks/kimageformats/-/commit/bf3f94da766d66a0470ab744dbe1ced4697b572d.diff";
+      sha256 = "18d67l5kj9sv88jdpi061k9rl3adzkx9l51ng7saylrkfddwc3ig";
+    })
+  ];
+
   nativeBuildInputs = [ extra-cmake-modules ];
   buildInputs = [ karchive openexr libavif qtbase ];
   outputs = [ "out" ]; # plugins only
