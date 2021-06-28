5ca20c29fa5c7311e0467a6783229474febb0685
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-06-28 10:43:40 +0200

    wireless-regdb: 2020.04.29 -> 2021.04.21

--- pkgs/data/misc/wireless-regdb/default.nix
+++ pkgs/data/misc/wireless-regdb/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "wireless-regdb";
-  version = "2020.04.29";
+  version = "2021.04.21";
 
   src = fetchurl {
     url = "https://www.kernel.org/pub/software/network/${pname}/${pname}-${version}.tar.xz";
-    sha256 = "0yicda474ygahv8da18h1p4yf42s6x2f208mlwcw4xsrxld07zc9";
+    sha256 = "sha256-nkwCsqlxDfTb2zJ8OWEujLuuZJWYev7drrqyjB6j2Po=";
   };
 
   dontBuild = true;
