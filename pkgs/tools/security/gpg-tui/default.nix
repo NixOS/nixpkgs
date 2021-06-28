775e849b0c8796ab43e8ecaebecc99acdc2b7cc8
R. RyanTM <ryantm-bot@ryantm.com>
2021-06-28 22:28:34 +0000

    gpg-tui: 0.6.1 -> 0.6.2

--- pkgs/tools/security/gpg-tui/default.nix
+++ pkgs/tools/security/gpg-tui/default.nix
@@ -9,16 +9,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "gpg-tui";
-  version = "0.6.1";
+  version = "0.6.2";
 
   src = fetchFromGitHub {
     owner = "orhun";
     repo = "gpg-tui";
     rev = "v${version}";
-    sha256 = "sha256-ajzx54uQFNZraDwqEKG9sdlMquJnluiaCqsR+JT79jw=";
+    sha256 = "sha256-Iv5A+o4TNSHJeTZgZ2e0SCHclz1mGMVRJDdBAWilyT8=";
   };
 
-  cargoSha256 = "sha256-UjrX+Z8slMBAiKhiFjxSoX74vIiFW9bMww49oPa18ag=";
+  cargoSha256 = "sha256-ISG/0WtgWwZoQd8PsvaQ9L8UKwerzEhY/84DxkdZV2g=";
 
   nativeBuildInputs = [
     gpgme # for gpgme-config
