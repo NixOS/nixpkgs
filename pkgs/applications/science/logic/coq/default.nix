a42defa1c2aecd81ecf50e175d94ee1abf674f04
Théo Zimmermann <theo.zimmermann@inria.fr>
2021-06-28 20:24:22 +0200

    coq: add dune_2 dependency
    For upcoming Coq >= 8.14

--- pkgs/applications/science/logic/coq/default.nix
+++ pkgs/applications/science/logic/coq/default.nix
@@ -129,7 +129,8 @@ self = stdenv.mkDerivation {
     ++ optionals buildIde
       (if versionAtLeast "8.10"
        then [ ocamlPackages.lablgtk3-sourceview3 glib gnome.adwaita-icon-theme wrapGAppsHook ]
-       else [ ocamlPackages.lablgtk ]);
+       else [ ocamlPackages.lablgtk ])
+    ++ optional (versionAtLeast "8.14") [ ocamlPackages.dune_2 ];
 
   postPatch = ''
     UNAME=$(type -tp uname)
