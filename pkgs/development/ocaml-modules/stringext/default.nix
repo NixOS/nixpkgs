ea3bd41061bb1ac9ba703ce82e70d564bd0a7aa9
Ulrik Strid <ulrik.strid@outlook.com>
2021-06-29 13:19:08 +0200

    Apply suggestions from code review
    Co-authored-by: Vincent Laporte <vbgl@users.noreply.github.com>

--- pkgs/development/ocaml-modules/stringext/default.nix
+++ pkgs/development/ocaml-modules/stringext/default.nix
@@ -1,6 +1,6 @@
-{ stdenv, lib, fetchurl, ocaml, buildDunePackage, ounit, qtest
+{ lib, fetchurl, ocaml, buildDunePackage, ounit, qtest
 # Optionally enable tests; test script use OCaml-4.01+ features
-, doCheck ? lib.versionAtLeast (lib.getVersion ocaml) "4.01"
+, doCheck ? lib.versionAtLeast ocaml.version "4.04"
 }:
 
 let version = "1.6.0"; in
@@ -8,7 +8,7 @@ let version = "1.6.0"; in
 buildDunePackage {
   pname = "stringext";
   version = version;
-
+  useDune2 = true;
   src = fetchurl {
     url = "https://github.com/rgrinberg/stringext/releases/download/${version}/stringext-${version}.tbz";
     sha256 = "1sh6nafi3i9773j5mlwwz3kxfzdjzsfqj2qibxhigawy5vazahfv";
