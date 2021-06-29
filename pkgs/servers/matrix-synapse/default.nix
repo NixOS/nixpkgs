9d694395a863e4b4a0441f792dbb320aa3c53fe4
Sumner Evans <me@sumnerevans.com>
2021-06-29 16:54:28 -0600

    matrix-synapse: 1.37.0 -> 1.37.1

--- pkgs/servers/matrix-synapse/default.nix
+++ pkgs/servers/matrix-synapse/default.nix
@@ -12,11 +12,11 @@ let
 in
 buildPythonApplication rec {
   pname = "matrix-synapse";
-  version = "1.37.0";
+  version = "1.37.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-BWGbfDBB1jX0PJSR8WCZE3FE1pDs0fRg8/fML3FsJHA=";
+    sha256 = "sha256-KxDHJrKm/QUZtOzI6MEpIUb4hTtxtIPLFuD1as3j4EA=";
   };
 
   patches = [
