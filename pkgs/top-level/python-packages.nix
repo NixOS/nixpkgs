1a658c26654448d7b5584acede72c33ce6693e57
Fabian Affolter <mail@fabian-affolter.ch>
2021-06-29 12:19:59 +0200

    python3Packages.py17track: init at 3.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5690,6 +5690,8 @@ in {
 
   py-ubjson = callPackage ../development/python-modules/py-ubjson { };
 
+  py17track = callPackage ../development/python-modules/py17track { };
+
   py2bit = callPackage ../development/python-modules/py2bit { };
 
   py3buddy = toPythonModule (callPackage ../development/python-modules/py3buddy { });
