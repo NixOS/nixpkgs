9ac61d128a7e5e3203383ee04dd9bb1d4bf2b736
Robert Schütz <dev@schuetz-co.de>
2021-06-29 20:45:56 +0200

    pythonPackages.google-i18n-address: fix compatibility with Python 3.9

--- pkgs/development/python-modules/google-i18n-address/default.nix
+++ pkgs/development/python-modules/google-i18n-address/default.nix
@@ -1,14 +1,21 @@
-{ buildPythonPackage, fetchPypi, pythonAtLeast, lib, requests, pytestCheckHook, mock }:
+{ buildPythonPackage, fetchPypi, fetchpatch, lib, requests, pytestCheckHook, mock }:
 
 buildPythonPackage rec {
   pname = "google-i18n-address";
   version = "2.4.0";
-  disabled = pythonAtLeast "3.9";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "8454a58f254a29988b8d1ca9ab663fd28a1f392a3d29b844d8824807db6333d7";
   };
+
+  patches = [
+    # fix compatibility with Python 3.9
+    (fetchpatch {
+      url = "https://github.com/mirumee/google-i18n-address/commit/b1d63d980e8b1a666e312e1c05c9037e2920685b.patch";
+      sha256 = "0lhsvkcgwz3i4az5hj8irbn8mj4b8yffq03isa18qp3z616adlrp";
+    })
+  ];
 
   propagatedBuildInputs = [ requests ];
 
