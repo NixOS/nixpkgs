c6482c12d9d38012b1fdb734113a1ff7b606a763
elseym <elseym@me.com>
2021-06-29 17:47:07 +0200

    atlassian-confluence: 7.11.0 -> 7.12.2
    (cherry picked from commit 50839b44984e0a0750226509b26a45e014828214)

--- pkgs/servers/atlassian/confluence.nix
+++ pkgs/servers/atlassian/confluence.nix
@@ -8,11 +8,11 @@ assert withMysql -> (mysql_jdbc != null);
 
 stdenvNoCC.mkDerivation rec {
   pname = "atlassian-confluence";
-  version = "7.11.0";
+  version = "7.12.2";
 
   src = fetchurl {
     url = "https://product-downloads.atlassian.com/software/confluence/downloads/${pname}-${version}.tar.gz";
-    sha256 = "sha256-/D/fMg+WRkGGV4NG73gvVDZwwi/8BB0w6IWK/OQ3caA=";
+    sha256 = "sha256-SZFyHU6Uy/opwfW0B+hnp+3wQkf+6w2/P25JH+BfLGY=";
   };
 
   buildPhase = ''
