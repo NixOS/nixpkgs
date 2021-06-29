577baa24a90660fcda9afe2843da7317441150ea
Fabian Affolter <mail@fabian-affolter.ch>
2021-06-29 13:45:59 +0200

    python3Packages.zwave-js-server-python: 0.26.1 -> 0.27.0

--- pkgs/development/python-modules/zwave-js-server-python/default.nix
+++ pkgs/development/python-modules/zwave-js-server-python/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "zwave-js-server-python";
-  version = "0.26.1";
+  version = "0.27.0";
   disabled = pythonOlder "3.8";
 
 
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "home-assistant-libs";
     repo = pname;
     rev = version;
-    sha256 = "1rh6wim5hr9lygvgria7lr6x61sdyrv3drrrirgv9p9gl3rxcj6m";
+    sha256 = "sha256-ClZ9B20NphN5XewqEtHpUMNHuuVxs1RJBcxWi6TzT1Y=";
   };
 
   propagatedBuildInputs = [
