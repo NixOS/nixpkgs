044733c30e168ebc538fb76f27caf2f325632114
John Ericson <John.Ericson@Obsidian.Systems>
2021-06-29 11:04:24 -0400

    binutils: Don't need earlier autoconf version for iOS
    Cherry-picked too much in f2c057684f4950d96dfcd993fe61645821cb5406.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12805,7 +12805,6 @@ in
   bingrep = callPackage ../development/tools/analysis/bingrep { };
 
   binutils-unwrapped = callPackage ../development/tools/misc/binutils {
-    autoreconfHook = if targetPlatform.isiOS then autoreconfHook269 else autoreconfHook;
     # FHS sys dirs presumably only have stuff for the build platform
     noSysDirs = (stdenv.targetPlatform != stdenv.hostPlatform) || noSysDirs;
   };
