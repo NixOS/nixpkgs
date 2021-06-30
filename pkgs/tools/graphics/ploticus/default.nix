eea3344e9171c80e59e157c2c5739b8150d782dc
Jan Tojnar <jtojnar@gmail.com>
2021-06-30 04:19:12 +0200

    ploticus: Modernize
    - Split pname & version.
    - Format the expression.
    - Do not use custom builder, the generic one is good enough.
    - Install the man pages to datadir instead of $out, where they would be moved anyway.
      – Allowing us to drop the postFixup.
    - Use patch instead of wrapper for pointing to prefabs. This is what Debian does and it will also apply to the pl binary, not just the ploticus alias.
      - Since the wrapping is no longer necessary, let’s create the alias using a symlink.

--- pkgs/tools/graphics/ploticus/default.nix
+++ pkgs/tools/graphics/ploticus/default.nix
@@ -1,34 +1,69 @@
-{lib, stdenv, fetchurl, zlib, libX11, libpng}:
+{ lib
+, stdenv
+, fetchurl
+, zlib
+, libX11
+, libpng
+}:
 
 stdenv.mkDerivation {
-  name = "ploticus-2.42";
+  pname = "ploticus";
+  version = "2.42";
 
-  builder = ./builder.sh;
   src = fetchurl {
     url = "mirror://sourceforge/ploticus/ploticus/2.41/pl241src.tar.gz";
     sha256 = "1065r0nizjixi9sxxfxrnwg10r458i6fgsd23nrxa200rypvdk7c";
   };
 
-  buildInputs = [ zlib libX11 libpng ];
+  patches = [
+    # Replace hardcoded FHS path with $out.
+    ./ploticus-install.patch
+
+    # Set the location of the PREFABS directory.
+    ./set-prefabs-dir.patch
+  ];
+
+  buildInputs = [
+    zlib
+    libX11
+    libpng
+  ];
 
   hardeningDisable = [ "format" ];
 
-  patches = [ ./ploticus-install.patch ];
+  preBuild = ''
+    cd src
+  '';
 
-  # Make the symlink relative instead of absolute.
-  # Otherwise it breaks when auto-moved to $out/share.
-  preFixup = ''
-    ln -sf pl.1 "$out"/man/man1/ploticus.1
+  preInstall = ''
+    mkdir -p "$out/bin"
+  '';
+
+  postInstall = ''
+    cd ..
+
+    # Install the “prefabs”.
+    mkdir -p "$out/share/ploticus/prefabs"
+    cp -rv prefabs/* "$out/share/ploticus/prefabs"
+
+    # Install the man pages.
+    cp -rv man $out/share
+
+    # Add aliases for backwards compatibility.
+    ln -s "pl.1" "$out/share/man/man1/ploticus.1"
+    ln -s "pl" "$out/bin/ploticus"
   '';
 
   meta = with lib; {
     description = "A non-interactive software package for producing plots and charts";
-    longDescription = ''Ploticus is a free, GPL'd, non-interactive
+    longDescription = ''
+      Ploticus is a free, GPL'd, non-interactive
       software package for producing plots, charts, and graphics from
       data.  Ploticus is good for automated or just-in-time graph
       generation, handles date and time data nicely, and has basic
       statistical capabilities.  It allows significant user control
-      over colors, styles, options and details.'';
+      over colors, styles, options and details.
+    '';
     license = licenses.gpl2Plus;
     maintainers = with maintainers; [ pSub ];
     homepage = "http://ploticus.sourceforge.net/";
