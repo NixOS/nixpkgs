dede942870e5c4e99bdb8a3182290e23f7728943
Raphael Megzari <raphael@megzari.com>
2021-06-30 13:23:26 +0900

    Update pkgs/stdenv/generic/setup.sh: group var declaration
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/stdenv/generic/setup.sh
+++ pkgs/stdenv/generic/setup.sh
@@ -511,8 +511,7 @@ activatePackage() {
 }
 
 _activatePkgs() {
-    local hostOffset targetOffset
-    local pkg
+    local hostOffset targetOffset pkg
 
     for hostOffset in "${allPlatOffsets[@]}"; do
         local pkgsVar="${pkgAccumVarVars[$(( hostOffset + 1 ))]}"
