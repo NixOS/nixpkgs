84b9c18a470ae731895fda251d6c8544cf731333
fortuneteller2k <lythe1107@gmail.com>
2021-06-30 23:21:22 +0800

    linuxPackages.tuxedo-keyboard: 3.0.5 -> 3.0.7
    (cherry picked from commit c7ec86b6894f7c216f4a381ebade4d717069a955)

--- pkgs/os-specific/linux/tuxedo-keyboard/default.nix
+++ pkgs/os-specific/linux/tuxedo-keyboard/default.nix
@@ -1,14 +1,14 @@
-{ lib, stdenv, fetchFromGitHub, kernel, linuxHeaders}:
+{ lib, stdenv, fetchFromGitHub, kernel, linuxHeaders }:
 
 stdenv.mkDerivation rec {
   pname = "tuxedo-keyboard-${kernel.version}";
-  version = "3.0.5";
+  version = "3.0.7";
 
   src = fetchFromGitHub {
     owner = "tuxedocomputers";
     repo = "tuxedo-keyboard";
     rev = "v${version}";
-    sha256 = "123ady2bi2dwbajy3pgv10l3g2pyhi5k31c1ii0zcrvl2qqhndck";
+    sha256 = "sha256-JloLwfJfDdVowx1hOehjxPbnaKBCAMn7SZe09SE03HU=";
   };
 
   buildInputs = [ linuxHeaders ];
