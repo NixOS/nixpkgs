550444d9b4cc309dc7da76e56555b6bc289a5507
Daniel Barter <danielbarter@gmail.com>
2021-07-01 10:06:31 -0700

    patching shared libs

--- pkgs/development/python-modules/mip/default.nix
+++ pkgs/development/python-modules/mip/default.nix
@@ -1,8 +1,10 @@
 { lib
 , fetchPypi
 , buildPythonPackage
-, setuptools_scm
+, setuptools-scm
 , cffi
+, zlib
+, autoPatchelfHook
 }:
 
 buildPythonPackage rec {
@@ -14,12 +16,14 @@ buildPythonPackage rec {
     sha256 = "0ynczcabpxqwg797wscw1gck4wg574wc2zyp77qsas8jv7xb6b5j";
   };
 
-  buildInputs = [ setuptools_scm cffi ];
+  buildInputs = [ setuptools-scm zlib cffi ];
+  nativeBuildInputs = [ autoPatchelfHook ];
+
   doCheck = false;
 
   meta = with lib; {
     homepage = "https://github.com/coin-or/python-mip";
-    description = "MIP is a collection of tools for Mixed-Integer Linear programs.";
+    description = "Collection of tools for Mixed-Integer Linear programs";
     maintainers = with maintainers; [ danielbarter ];
     license = licenses.mit;
   };
