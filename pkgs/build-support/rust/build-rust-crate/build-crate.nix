da95ae8e3da4d59e3275db2cff6d8fc49a68e669
John Ericson <John.Ericson@Obsidian.Systems>
2021-07-01 04:06:55 +0000

    buildRustCrate: Don't override the linker during cross
    lld is sometimes need. The caller can do that instead.

--- pkgs/build-support/rust/build-rust-crate/build-crate.nix
+++ pkgs/build-support/rust/build-rust-crate/build-crate.nix
@@ -18,7 +18,6 @@
         (mkRustcFeatureArgs crateFeatures)
       ] ++ lib.optionals (stdenv.hostPlatform != stdenv.buildPlatform) [
         "--target" (rust.toRustTargetSpec stdenv.hostPlatform)
-        "-C" "linker=${stdenv.hostPlatform.config}-ld"
       ] ++ extraRustcOpts
       # since rustc 1.42 the "proc_macro" crate is part of the default crate prelude
       # https://github.com/rust-lang/cargo/commit/4d64eb99a4#diff-7f98585dbf9d30aa100c8318e2c77e79R1021-R1022
