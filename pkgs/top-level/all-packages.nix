4a754d8e9fa024e40f7c89652bd490c785f3a215
dan4ik <6057430gu@gmail.com>
2021-07-01 12:50:47 +0700

    removing recent changes

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27243,10 +27243,6 @@ in
 
   tonelib-gfx = callPackage ../applications/audio/tonelib-gfx { };
 
-  tonelib-jam = callPackage ../applications/audio/tonelib-jam { };
-
-  tonelib-zoom = callPackage ../applications/audio/tonelib-zoom { };
-
   tony = libsForQt514.callPackage ../applications/audio/tony { };
 
   toot = callPackage ../applications/misc/toot { };
