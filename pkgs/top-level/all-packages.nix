2244f17ef18e2867de48bb85b254e15625a84682
Robert Helgesson <robert@rycee.net>
2021-07-01 11:51:23 +0200

    yajsv: init at 1.4.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10168,6 +10168,8 @@ in
 
   yafaray-core = callPackage ../tools/graphics/yafaray-core { };
 
+  yajsv = callPackage ../tools/misc/yajsv { };
+
   yapf = with python3Packages; toPythonApplication yapf;
 
   yarn = callPackage ../development/tools/yarn  { };
