0cde4bf18f77b2cc9c9fb3fdefa35492f1e905c8
Mario Rodas <marsam@users.noreply.github.com>
2021-07-01 04:20:00 +0000

    nodejs-16_x: 16.4.0 -> 16.4.1
    https://github.com/nodejs/node/releases/tag/v16.4.1
    (cherry picked from commit 4f90b61a11496e16d2e460301b73426ddd2be0b8)

--- pkgs/development/web/nodejs/v16.nix
+++ pkgs/development/web/nodejs/v16.nix
@@ -8,6 +8,6 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "16.4.0";
-    sha256 = "07f8g3hs0v7nsdvzlsr1p4pzgb04qn54pnhmbdsgmmb41cp227pr";
+    version = "16.4.1";
+    sha256 = "1a1aygksmbafxvrs8g2jv0y1jj3cwyclk0qbqxkn5qfq5r1i943n";
   }
