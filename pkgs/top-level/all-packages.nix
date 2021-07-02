1ead8774f92585b8f442442b09d300e9dd52d199
Maciej Krüger <mkg20001@gmail.com>
2021-07-02 14:22:12 +0200

    pgadmin4: init at 5.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31183,7 +31183,9 @@ in
 
   pgmanage = callPackage ../applications/misc/pgmanage { };
 
-  pgadmin = callPackage ../applications/misc/pgadmin {
+  pgadmin4 = callPackage ../tools/admin/pgadmin { };
+
+  pgadmin3 = callPackage ../applications/misc/pgadmin {
     openssl = openssl_1_0_2;
   };
 
