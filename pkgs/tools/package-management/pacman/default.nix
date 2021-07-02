666468be2e8cae4ffe17a1ec784b4ae1a041eeeb
Felix Singer <felixsinger@posteo.net>
2021-07-02 05:07:50 +0200

    pacman: 5.2.2 -> 6.0.0
    Update to latest stable release. Also, apply the following patches
    fixing various bugs.
    
    https://gitlab.archlinux.org/pacman/pacman/-/commit/3401f9e142ac4c701cd98c52618cb13164f2146b.patch
    https://gitlab.archlinux.org/pacman/pacman/-/commit/542910d684191eb7f25ddc5d3d8fe3060028a267.patch
    
    Signed-off-by: Felix Singer <felixsinger@posteo.net>

--- pkgs/tools/package-management/pacman/default.nix
+++ pkgs/tools/package-management/pacman/default.nix
@@ -1,30 +1,38 @@
-{ stdenv, lib, fetchurl, pkg-config, m4, perl, libarchive, openssl, zlib, bzip2,
-xz, curl, runtimeShell }:
+{ fetchpatch, fetchurl, lib, meson, ninja, stdenv, pkg-config, python3,
+  asciidoc, bzip2, curl, gpgme, libarchive, openssl, runtimeShell, xz, zlib }:
 
 stdenv.mkDerivation rec {
   pname = "pacman";
-  version = "5.2.2";
+  version = "6.0.0";
 
   src = fetchurl {
-    url = "https://sources.archlinux.org/other/${pname}/${pname}-${version}.tar.gz";
-    sha256 = "1829jcc300fxidr3cahx5kpnxkpg500daqgn2782hg5m5ygil85v";
+    url = "https://sources.archlinux.org/other/${pname}/${pname}-${version}.tar.xz";
+    sha256 = "0228bn8gk25hk816x80i064nnv2mnwgmvpd4l33vsivpb844hi00";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "fix-404-download.patch";
+      url = "https://gitlab.archlinux.org/pacman/pacman/-/commit/3401f9e142ac4c701cd98c52618cb13164f2146b.patch";
+      sha256 = "0fd07xs6ii57nlcd3zdaajbm9flhd68b34l32nqmnnlixrr430di";
+    })
+    (fetchpatch {
+      name = "fix-key-import-double-free.patch";
+      url = "https://gitlab.archlinux.org/pacman/pacman/-/commit/542910d684191eb7f25ddc5d3d8fe3060028a267.patch";
+      sha256 = "04mgrmg6a7s38830whhb0i4gkhi3p7b0v8yxzympsyg52hcxrla2";
+    })
+  ];
+
+  nativeBuildInputs = [ asciidoc meson ninja pkg-config python3 ];
+  buildInputs = [ bzip2 curl gpgme libarchive openssl xz zlib ];
 
   enableParallelBuilding = true;
 
-  configureFlags = [
-    # trying to build docs fails with a2x errors, unable to fix through asciidoc
-    "--disable-doc"
-
-    "--sysconfdir=/etc"
-    "--localstatedir=/var"
-    "--with-scriptlet-shell=${runtimeShell}"
+  mesonFlags = [
+    "--sysconfdir=etc"
+    "--localstatedir=var"
+    "-Dscriptlet-shell=${runtimeShell}"
   ];
-
-  installFlags = [ "sysconfdir=${placeholder "out"}/etc" ];
-
-  nativeBuildInputs = [ pkg-config m4 ];
-  buildInputs = [ curl perl libarchive openssl zlib bzip2 xz ];
 
   postFixup = ''
     substituteInPlace $out/bin/repo-add \
