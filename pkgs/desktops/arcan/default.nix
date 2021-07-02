09eacdb0dfb4d8eba59466dacf7714f7e5150f3f
AndersonTorres <torres.anderson.85@protonmail.com>
2021-07-02 06:01:46 -0300

    arcan.arcan: init at 0.6.1pre1+unstable=2021-07-07

--- pkgs/desktops/arcan/default.nix
+++ pkgs/desktops/arcan/default.nix
@@ -1,4 +1,13 @@
 { callPackage, lib, pkgs }:
 
 rec {
+  # Dependencies
+
+  espeak = pkgs.espeak-ng;
+  ffmpeg = pkgs.ffmpeg-full;
+  harfbuzz = pkgs.harfbuzzFull;
+
+  # Arcan
+
+  arcan = callPackage ./arcan.nix { };
 }
