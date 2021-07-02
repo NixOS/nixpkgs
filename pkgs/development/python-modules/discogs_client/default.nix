02de971fc24ad37eef13a914112cb5addf63381e
Fabian Affolter <mail@fabian-affolter.ch>
2021-07-02 21:54:43 +0200

    python3Packages.discogs_client: 2.3.0 -> 2.3.12

--- pkgs/development/python-modules/discogs_client/default.nix
+++ pkgs/development/python-modules/discogs_client/default.nix
@@ -1,19 +1,40 @@
-{ lib, buildPythonPackage, fetchPypi, requests, oauthlib }:
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, requests
+, oauthlib
+, python-dateutil
+, pytestCheckHook
+}:
 
 buildPythonPackage rec {
   pname = "discogs-client";
-  version = "2.3.0";
+  version = "2.3.12";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "cc979fcbb5283f74d388c7111c8ed6bef920b01614a014d6b1c5d6fbb554bfc3";
+
+  src = fetchFromGitHub {
+    owner = "joalla";
+    repo = "discogs_client";
+    rev = "v${version}";
+    sha256 = "0y553x8rkgmqqg980n62pwdxbp75xalkhlb6k5g0cms42ggy5fsc";
   };
 
-  propagatedBuildInputs = [ requests oauthlib ];
+  propagatedBuildInputs = [
+    requests
+    oauthlib
+    python-dateutil
+  ];
+
+  checkInputs = [
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [ "discogs_client" ];
 
   meta = with lib; {
-    description = "Official Python API client for Discogs";
+    description = "Unofficial Python API client for Discogs";
+    homepage = "https://github.com/joalla/discogs_client";
     license = licenses.bsd2;
-    homepage = "https://github.com/discogs/discogs_client";
+    maintainers = with maintainers; [ fab ];
   };
 }
