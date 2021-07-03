147ea7381f18c12dfd4157e89ece250ecb20624f
Fabian Affolter <mail@fabian-affolter.ch>
2021-07-03 17:04:18 +0200

    python3Packages.fastparquet: 0.5.0 -> 0.6.3

--- pkgs/development/python-modules/fastparquet/default.nix
+++ pkgs/development/python-modules/fastparquet/default.nix
@@ -1,27 +1,59 @@
-{ lib, buildPythonPackage, fetchFromGitHub, numba, numpy, pandas, pytestrunner
-, thrift, pytestCheckHook, python-snappy, lz4, zstandard, zstd }:
+{ lib
+, buildPythonPackage
+, cramjam
+, fetchFromGitHub
+, fsspec
+, numba
+, numpy
+, pandas
+, pytest-runner
+, thrift
+, pytestCheckHook
+, python-snappy
+, lz4
+, zstandard
+, zstd
+}:
 
 buildPythonPackage rec {
   pname = "fastparquet";
-  version = "0.5.0";
+  version = "0.6.3";
 
   src = fetchFromGitHub {
     owner = "dask";
     repo = pname;
     rev = version;
-    sha256 = "17i091kky34m2xivk29fqsyxxxa7v4352n79w01n7ni93za6wana";
+    sha256 = "05lfviijbd403gii4rjxznrkcqslv8akxbn11b4w0wxvllz7l8n1";
   };
 
-  nativeBuildInputs = [ pytestrunner ];
-  propagatedBuildInputs = [ numba numpy pandas thrift ];
-  checkInputs = [ pytestCheckHook python-snappy lz4 zstandard zstd ];
+  nativeBuildInputs = [
+    pytest-runner
+  ];
+
+  propagatedBuildInputs = [
+    cramjam
+    fsspec
+    lz4
+    numba
+    numpy
+    pandas
+    python-snappy
+    thrift
+    zstandard
+    zstd
+  ];
+
+  checkInputs = [
+    pytestCheckHook
+  ];
 
   # E   ModuleNotFoundError: No module named 'fastparquet.speedups'
   doCheck = false;
+
   pythonImportsCheck = [ "fastparquet" ];
 
   meta = with lib; {
-    description = "A python implementation of the parquet format";
+    description = "Python implementation of the parquet format";
     homepage = "https://github.com/dask/fastparquet";
     license = with licenses; [ asl20 ];
     maintainers = with maintainers; [ veprbl ];
