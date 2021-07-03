9ed4eacdfbadc2bf1eed17176508ef16db190dc6
Robert Schütz <dev@schuetz-co.de>
2021-07-03 12:12:58 +0200

    pythonPackages.parameterizedtestcase: init at 0.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5237,6 +5237,8 @@ in {
 
   openapi-core = callPackage ../development/python-modules/openapi-core { };
 
+  parameterizedtestcase = callPackage ../development/python-modules/parameterizedtestcase { };
+
   pdunehd = callPackage ../development/python-modules/pdunehd { };
 
   phonopy = callPackage ../development/python-modules/phonopy { };
