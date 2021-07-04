ff224c89bd023336b09a19e72150558925544744
Bernardo Meurer <bernardo@meurer.org>
2021-07-04 14:11:50 -0700

    firefox: increase silent timeout to 14400s (4h)
    Fixes: #129115
    Replaces: #129212
    (cherry picked from commit 919e2a98ac1090be9be5ed673bc5c402f347bae7)

--- pkgs/applications/networking/browsers/firefox/packages.nix
+++ pkgs/applications/networking/browsers/firefox/packages.nix
@@ -21,6 +21,7 @@ rec {
       badPlatforms = lib.platforms.darwin;
       broken = stdenv.buildPlatform.is32bit; # since Firefox 60, build on 32-bit platforms fails with "out of memory".
                                              # not in `badPlatforms` because cross-compilation on 64-bit machine might work.
+      maxSilent = 14400; # 4h, double the default of 7200s (c.f. #129212, #129115)
       license = lib.licenses.mpl20;
     };
     tests = [ nixosTests.firefox ];
