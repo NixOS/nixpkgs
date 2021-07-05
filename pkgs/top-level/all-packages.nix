5f4bbbffa4b7d0e94c00f674c7504b9d4a3ae3a5
Robert Schütz <dev@schuetz-co.de>
2021-07-06 00:34:35 +0200

    ytmdl: don't propagate ffmpeg

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28208,7 +28208,7 @@ in
 
   ytmdesktop = callPackage ../applications/audio/ytmdesktop { };
 
-  ytmdl = python3Packages.callPackage ../tools/misc/ytmdl { };
+  ytmdl = callPackage ../tools/misc/ytmdl { };
 
   zam-plugins = callPackage ../applications/audio/zam-plugins { };
 
