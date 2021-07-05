5427e9aea81da2e0ec017991cf534f9907867e6a
Justin Bedo <cu@cua0.org>
2021-07-05 22:01:41 +1000

    singularity: bump vendored deps to patch CVE-2021-29136

--- pkgs/applications/virtualization/singularity/default.nix
+++ pkgs/applications/virtualization/singularity/default.nix
@@ -1,7 +1,10 @@
 {stdenv
+, stdenvNoCC
 , removeReferencesTo
 , lib
 , fetchurl
+, fetchpatch
+, cacert
 , utillinux
 , gpgme
 , openssl
@@ -16,14 +19,62 @@
 
 with lib;
 
-buildGoPackage rec {
-  pname = "singularity";
+let
   version = "3.6.4";
 
-  src = fetchurl {
-    url = "https://github.com/hpcng/singularity/releases/download/v${version}/singularity-${version}.tar.gz";
-    sha256 = "17z7v7pjq1ibl64ir4h183sp58v2x7iv6dn6imnnhkdvss0kl8vi";
+  # Patch the singularity source by revendoring and backporting changes
+  # to support umoci v0.4.7; addresses CVE-2021-29136
+  patchedSrc = stdenvNoCC.mkDerivation {
+    name = "singularity-${version}-src";
+
+    src = fetchurl {
+      url = "https://github.com/hpcng/singularity/releases/download/v${version}/singularity-${version}.tar.gz";
+      sha256 = "17z7v7pjq1ibl64ir4h183sp58v2x7iv6dn6imnnhkdvss0kl8vi";
+    };
+
+    nativeBuildInputs = [ go cacert ];
+
+    phases = [ "unpackPhase" "patchPhase" "buildPhase" "installPhase" ];
+
+    patches = [((fetchpatch {
+      url = "https://github.com/hpcng/singularity/commit/6e59f31cb97ff59e323850aa1339a38a11358429.patch";
+      sha256 = "sha256-pypzgRr7LO589TBqhHEKZ+eP6/4w7OQQ1dBgsro47G0=";
+      includes = [ "internal/pkg/build/sources/oci_unpack.go" ];
+    }).overrideAttrs (attrs: {
+      postFetch = attrs.postFetch + ''
+        substituteInPlace $out --replace \
+          "internal/pkg/build/sources/oci_unpack.go" \
+          "internal/pkg/build/sources/oci_unpack_linux.go"
+      '';
+    }))];
+
+    buildPhase = ''
+      runHook preBuild
+      substituteInPlace go.mod --replace \
+        'github.com/opencontainers/umoci v0.4.6-0.20200622135030-30d116059d97' \
+        'github.com/opencontainers/umoci v0.4.7'
+      HOME=$TMPDIR GO111MODULE=on go mod vendor
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      mkdir $out
+      cp -r ./* $out
+      runHook postInstall
+    '';
+
+    outputHashMode = "recursive";
+    outputHashAlgo = "sha256";
+    outputHash = "sha256-IAtFklXI6x6jKV3uNYrFsQre4iepCz/hU8Z/X9QWBoY=";
+
   };
+
+in buildGoPackage rec {
+  pname = "singularity";
+  inherit version;
+
+  src = patchedSrc;
 
   goPackagePath = "github.com/sylabs/singularity";
   goDeps = ./deps.nix;
