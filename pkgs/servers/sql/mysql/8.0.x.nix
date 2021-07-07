457eb2fdb3d85dcf16b56964f4c98828d85c8aff
Giulio De Pasquale <peperunas@users.noreply.github.com>
2021-07-07 16:45:12 +0100

    mysql 8.0.22 -> 8.0.25
    (cherry picked from commit f6d3da9061a54576436906e201fa336f1fdda716)

--- pkgs/servers/sql/mysql/8.0.x.nix
+++ pkgs/servers/sql/mysql/8.0.x.nix
@@ -1,16 +1,16 @@
 { lib, stdenv, fetchurl, bison, cmake, pkg-config
 , boost, icu, libedit, libevent, lz4, ncurses, openssl, protobuf, re2, readline, zlib, zstd
-, numactl, perl, cctools, CoreServices, developer_cmds, libtirpc, rpcsvc-proto
+, numactl, perl, cctools, CoreServices, developer_cmds, libtirpc, rpcsvc-proto, curl
 }:
 
 let
 self = stdenv.mkDerivation rec {
   pname = "mysql";
-  version = "8.0.22";
+  version = "8.0.25";
 
   src = fetchurl {
     url = "https://dev.mysql.com/get/Downloads/MySQL-${self.mysqlVersion}/${pname}-${version}.tar.gz";
-    sha256 = "9fd85bb243940ef8234d21384ef421a0962fd4d13406fc1420efa902115ce17a";
+    sha256 = "c16aa9cf621bc028efba2bb11f3c36a323b125fa0d108ff92fab60e46309206e";
   };
 
   patches = [
@@ -27,7 +27,7 @@ self = stdenv.mkDerivation rec {
   '';
 
   buildInputs = [
-    boost icu libedit libevent lz4 ncurses openssl protobuf re2 readline zlib
+    boost curl icu libedit libevent lz4 ncurses openssl protobuf re2 readline zlib
     zstd
   ] ++ lib.optionals stdenv.isLinux [
     numactl libtirpc
