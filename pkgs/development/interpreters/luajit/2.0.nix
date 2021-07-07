1775ee342a4ca9636e40d9b646c08802272f49c7
Roman Volosatovs <rvolosatovs@riseup.net>
2021-07-07 16:24:43 +0200

    luajit_2_0: 2.1.0-2021-05-29 -> 2.1.0-2021-06-08

--- pkgs/development/interpreters/luajit/2.0.nix
+++ pkgs/development/interpreters/luajit/2.0.nix
@@ -1,10 +1,10 @@
 { self, callPackage, lib }:
 callPackage ./default.nix {
   inherit self;
-  version = "2.0.5-2021-05-29";
-  rev = "c2cfa04231785116d9d198462830f41ef94147c0";
+  version = "2.0.5-2021-06-08";
+  rev = "98f95f69180d48ce49289d6428b46a9ccdd67a46";
   isStable = true;
-  sha256 = "1fw64pv0dvzb9bgr2zwcv9q8gqgsmfnvrcrmrdfgj4ncamgdnilj";
+  sha256 = "1pdmhk5syp0nir80xcnkf6xy2w5rwslak8hgmjpgaxzlnrjcgs7p";
   extraMeta = { # this isn't precise but it at least stops the useless Hydra build
     platforms = with lib; filter (p: p != "aarch64-linux")
       (platforms.linux ++ platforms.darwin);
