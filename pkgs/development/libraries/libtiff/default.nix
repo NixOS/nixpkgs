190b0d6939560bc36830f5bd4d6047435cbf6add
Alyssa Ross <hi@alyssa.is>
2021-07-08 20:46:22 +0000

    libtiff: 4.2.0 -> 4.3.0; adopt; improve meta
    Tested by viewing a TIFF file in imv.
    
    (cherry picked from commit b7d70449e650e94f35e4c8158044809a1997e320)

--- pkgs/development/libraries/libtiff/default.nix
+++ pkgs/development/libraries/libtiff/default.nix
@@ -12,11 +12,11 @@
 
 stdenv.mkDerivation rec {
   pname = "libtiff";
-  version = "4.2.0";
+  version = "4.3.0";
 
   src = fetchurl {
     url = "https://download.osgeo.org/libtiff/tiff-${version}.tar.gz";
-    sha256 = "1jrkjv0xya9radddn8idxvs2gqzp3l2b1s8knlizmn7ad3jq817b";
+    sha256 = "1j3snghqjbhwmnm5vz3dr1zm68dj15mgbx1wqld7vkl7n2nfaihf";
   };
 
   cmakeFlags = if stdenv.isDarwin then [
@@ -47,7 +47,9 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "Library and utilities for working with the TIFF image file format";
-    homepage = "http://download.osgeo.org/libtiff";
+    homepage = "https://libtiff.gitlab.io/libtiff";
+    changelog = "https://libtiff.gitlab.io/libtiff/v${version}.html";
+    maintainers = with maintainers; [ qyliss ];
     license = licenses.libtiff;
     platforms = platforms.unix;
   };
