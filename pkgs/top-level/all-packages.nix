d44293e8470479666c9a83e23e83179aa00d45f9
Giulio De Pasquale <depasquale+git@giugl.io>
2021-07-08 15:55:35 +0100

    mysql 8.0.25: protobuf 3.7 -> 3.11

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19838,7 +19838,7 @@ in
     inherit (darwin) cctools developer_cmds;
     inherit (darwin.apple_sdk.frameworks) CoreServices;
     boost = boost173; # Configure checks for specific version.
-    protobuf = protobuf3_7;
+    protobuf = protobuf3_11;
     icu =  icu67;
   };
 
