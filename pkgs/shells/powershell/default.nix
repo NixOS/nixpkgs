72780d8179dc150fb3d443ac5d88ee6df11b31b5
Matt Votava <mvnetbiz@gmail.com>
2021-07-08 15:06:58 -0700

    powershell: Add keyutils

--- pkgs/shells/powershell/default.nix
+++ pkgs/shells/powershell/default.nix
@@ -1,21 +1,37 @@
-{ stdenv, lib, autoPatchelfHook, fetchzip, libunwind, libuuid, icu, curl
-, darwin, makeWrapper, less, openssl_1_1, pam, lttng-ust }:
+{ stdenv
+, lib
+, autoPatchelfHook
+, fetchzip
+, libunwind
+, libuuid
+, icu
+, curl
+, darwin
+, makeWrapper
+, less
+, openssl_1_1
+, pam
+, lttng-ust
+, keyutils
+}:
 
-let archString = if stdenv.isAarch64 then "arm64"
-                 else if stdenv.isx86_64 then "x64"
-                 else throw "unsupported platform";
-    platformString = if stdenv.isDarwin then "osx"
-                     else if stdenv.isLinux then "linux"
-                     else throw "unsupported platform";
-    platformSha = if stdenv.isDarwin then "0w44ws8b6zfixf7xz93hmplqsx18279n9x8j77y4rbzs13fldvsn"
-                     else if (stdenv.isLinux && stdenv.isx86_64) then "0xm7l49zhkz2fly3d751kjd5cy3ws9zji9i0061lkd06dvkch7jy"
-                     else if (stdenv.isLinux && stdenv.isAarch64) then "1axbi4kmb1ydys7c45jhp729w1srid3c8jgivb4bdmdp56rf6h32"
-                     else throw "unsupported platform";
-    platformLdLibraryPath = if stdenv.isDarwin then "DYLD_FALLBACK_LIBRARY_PATH"
-                     else if stdenv.isLinux then "LD_LIBRARY_PATH"
-                     else throw "unsupported platform";
-                     libraries = [ libunwind libuuid icu curl openssl_1_1 ] ++
-                       (if stdenv.isLinux then [ pam lttng-ust ] else [ darwin.Libsystem ]);
+let
+  archString = if stdenv.isAarch64 then "arm64"
+    else if stdenv.isx86_64 then "x64"
+    else throw "unsupported platform";
+  platformString = if stdenv.isDarwin then "osx"
+    else if stdenv.isLinux then "linux"
+    else throw "unsupported platform";
+  platformSha = if stdenv.isDarwin then "0w44ws8b6zfixf7xz93hmplqsx18279n9x8j77y4rbzs13fldvsn"
+    else if (stdenv.isLinux && stdenv.isx86_64) then "0xm7l49zhkz2fly3d751kjd5cy3ws9zji9i0061lkd06dvkch7jy"
+    else if (stdenv.isLinux && stdenv.isAarch64) then "1axbi4kmb1ydys7c45jhp729w1srid3c8jgivb4bdmdp56rf6h32"
+    else throw "unsupported platform";
+  platformLdLibraryPath = if stdenv.isDarwin then "DYLD_FALLBACK_LIBRARY_PATH"
+    else if stdenv.isLinux then "LD_LIBRARY_PATH"
+    else throw "unsupported platform";
+  libraries = [ libunwind libuuid icu curl openssl_1_1 ]
+    ++ lib.optionals stdenv.isLinux [ pam lttng-ust keyutils ]
+    ++ lib.optionals stdenv.isDarwin [ darwin.Libsystem ];
 in
 stdenv.mkDerivation rec {
   pname = "powershell";
