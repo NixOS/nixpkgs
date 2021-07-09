d0a4e3f82a89f83fca157502f68baec021593cd7
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2021-07-09 03:21:42 +0300

    psi-plus: Add “chatType” build option
    Psi+ supports these three options for chat log:
    
    1. Basic (without any web stuff involved)
    2. WebKit
    3. QtWebEngine

--- pkgs/applications/networking/instant-messengers/psi-plus/default.nix
+++ pkgs/applications/networking/instant-messengers/psi-plus/default.nix
@@ -1,5 +1,5 @@
 { lib, mkDerivation, fetchFromGitHub, cmake
-, qtbase, qtmultimedia, qtx11extras, qttools, qtwebengine
+, qtbase, qtmultimedia, qtx11extras, qttools
 , libidn, qca-qt5, libXScrnSaver, hunspell
 , libsecret, libgcrypt, libotr, html-tidy, libgpgerror, libsignal-protocol-c
 , usrsctp
@@ -7,7 +7,19 @@
 # Voice messages
 , voiceMessagesSupport ? true
 , gst_all_1
+
+, chatType ? "basic" # See the assertion below for available options
+, qtwebkit
+, qtwebengine
 }:
+
+assert builtins.isBool voiceMessagesSupport;
+
+assert builtins.elem (lib.toLower chatType) [
+  "basic"     # Basic implementation, no web stuff involved
+  "webkit"    # Legacy one, based on WebKit (see https://wiki.qt.io/Qt_WebKit)
+  "webengine" # QtWebEngine (see https://wiki.qt.io/QtWebEngine)
+];
 
 mkDerivation rec {
   pname = "psi-plus";
@@ -21,20 +33,24 @@ mkDerivation rec {
   };
 
   cmakeFlags = [
+    "-DCHAT_TYPE=${chatType}"
     "-DENABLE_PLUGINS=ON"
   ];
 
   nativeBuildInputs = [ cmake qttools ];
 
   buildInputs = [
-    qtbase qtmultimedia qtx11extras qtwebengine
+    qtbase qtmultimedia qtx11extras
     libidn qca-qt5 libXScrnSaver hunspell
     libsecret libgcrypt libotr html-tidy libgpgerror libsignal-protocol-c
     usrsctp
-  ] ++ lib.optionals voiceMessagesSupport [
+  ]
+  ++ lib.optionals voiceMessagesSupport [
     gst_all_1.gst-plugins-base
     gst_all_1.gst-plugins-good
-  ];
+  ]
+  ++ lib.optional (chatType == "webkit") qtwebkit
+  ++ lib.optional (chatType == "webengine") qtwebengine;
 
   preFixup = lib.optionalString voiceMessagesSupport ''
     qtWrapperArgs+=(
