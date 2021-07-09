aff1607b17da0e97b3b77a5ee640da29eadf9c9c
Yureka <yuka@yuka.dev>
2021-07-09 11:03:17 +0200

    gitlab: 14.0.2 -> 14.0.5
    https://about.gitlab.com/releases/2021/07/06/gitlab-14-0-3-released/
    https://about.gitlab.com/releases/2021/07/07/critical-security-release-gitlab-14-0-4-released/
    https://about.gitlab.com/releases/2021/07/08/gitlab-14-0-5-released/
    (cherry picked from commit 6510a136119d9c24ba67e4133ab1d561f72a46f6)

--- pkgs/applications/version-management/gitlab/gitaly/default.nix
+++ pkgs/applications/version-management/gitlab/gitaly/default.nix
@@ -21,14 +21,14 @@ let
       };
   };
 in buildGoModule rec {
-  version = "14.0.2";
+  version = "14.0.5";
   pname = "gitaly";
 
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitaly";
     rev = "v${version}";
-    sha256 = "sha256-0mLGtvRHgMN3TtH/g4XLDubwZvtB0xr2U30fufj//KY=";
+    sha256 = "sha256-aZ+EDF9BOdsbE+Xsde259eRmQhnb6H5piLQsvzgVpGM=";
   };
 
   vendorSha256 = "sha256-U962bMmXNnenCYkSdk0Uy7Bz+b9JGU5rJHfblZoyC/I=";
