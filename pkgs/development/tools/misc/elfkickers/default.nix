4446da0ceb1fcd64d9661c82ce55c81884c70ca1
Sandro <sandro.jaeckel@gmail.com>
2021-07-09 12:42:17 +0200

    Update pkgs/development/tools/misc/elfkickers/default.nix

--- pkgs/development/tools/misc/elfkickers/default.nix
+++ pkgs/development/tools/misc/elfkickers/default.nix
@@ -14,7 +14,7 @@ stdenv.mkDerivation rec {
   enableParallelBuilding = true;
 
   meta = with lib; {
-    homepage = "http://www.muppetlabs.com/~breadbox/software/elfkickers.html";
+    homepage = "https://www.muppetlabs.com/~breadbox/software/elfkickers.html";
     description = "A collection of programs that access and manipulate ELF files";
     platforms = platforms.linux;
     license = licenses.gpl2Plus;
