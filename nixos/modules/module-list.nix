3ade72d0ef66d58e162f832463279ccf0eb4b182
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2021-07-10 12:23:04 +0200

    nixos/prometheus.sachet: add module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -688,6 +688,7 @@
   ./services/monitoring/prometheus/alertmanager.nix
   ./services/monitoring/prometheus/exporters.nix
   ./services/monitoring/prometheus/pushgateway.nix
+  ./services/monitoring/prometheus/sachet.nix
   ./services/monitoring/prometheus/xmpp-alerts.nix
   ./services/monitoring/riemann.nix
   ./services/monitoring/riemann-dash.nix
