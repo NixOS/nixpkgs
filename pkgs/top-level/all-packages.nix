b7826b859b1ef02dfce367f9f994fa30f5e084a8
Ethan Edwards <ethan@ethancedwards.com>
2021-07-09 23:08:12 -0400

    difftastic: init at unstable-2021-07-09

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4165,6 +4165,8 @@ in
 
   diffstat = callPackage ../tools/text/diffstat { };
 
+  difftastic = callPackage ../tools/text/difftastic { };
+
   diffutils = callPackage ../tools/text/diffutils { };
 
   dir2opus = callPackage ../tools/audio/dir2opus {
