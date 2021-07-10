165f5fa142ffc5e3b809afeced906bce6ac87971
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2021-07-10 16:48:11 +0300

    psi-plus: Apply formatting to the Nix file

--- pkgs/applications/networking/instant-messengers/psi-plus/default.nix
+++ pkgs/applications/networking/instant-messengers/psi-plus/default.nix
@@ -1,7 +1,21 @@
-{ lib, mkDerivation, fetchFromGitHub, cmake
-, qtbase, qtmultimedia, qtx11extras, qttools
-, libidn, qca-qt5, libXScrnSaver, hunspell
-, libsecret, libgcrypt, libotr, html-tidy, libgpgerror, libsignal-protocol-c
+{ lib
+, mkDerivation
+, fetchFromGitHub
+, cmake
+, qtbase
+, qtmultimedia
+, qtx11extras
+, qttools
+, libidn
+, qca-qt5
+, libXScrnSaver
+, hunspell
+, libsecret
+, libgcrypt
+, libotr
+, html-tidy
+, libgpgerror
+, libsignal-protocol-c
 , usrsctp
 
 , chatType ? "basic" # See the assertion below for available options
@@ -10,19 +24,19 @@
 
 , enablePlugins ? true
 
-# Voice messages
+  # Voice messages
 , voiceMessagesSupport ? true
 , gst_all_1
 
 , enablePsiMedia ? false
 , pkg-config
 
-, extraCmakeFlags ? [] # In addition to existing ones
+, extraCmakeFlags ? [ ] # In addition to existing ones
 }:
 
 assert builtins.elem (lib.toLower chatType) [
-  "basic"     # Basic implementation, no web stuff involved
-  "webkit"    # Legacy one, based on WebKit (see https://wiki.qt.io/Qt_WebKit)
+  "basic" # Basic implementation, no web stuff involved
+  "webkit" # Legacy one, based on WebKit (see https://wiki.qt.io/Qt_WebKit)
   "webengine" # QtWebEngine (see https://wiki.qt.io/QtWebEngine)
 ];
 
@@ -50,23 +64,36 @@ mkDerivation rec {
     assert builtins.all builtins.isString extraCmakeFlags; extraCmakeFlags
   );
 
-  nativeBuildInputs =
-    [ cmake qttools ]
-    ++ lib.optional enablePsiMedia pkg-config;
+  nativeBuildInputs = [
+    cmake
+    qttools
+  ] ++ lib.optionals enablePsiMedia [
+    pkg-config
+  ];
 
-  buildInputs =
-    [
-      qtbase qtmultimedia qtx11extras
-      libidn qca-qt5 libXScrnSaver hunspell
-      libsecret libgcrypt libotr html-tidy libgpgerror libsignal-protocol-c
-      usrsctp
-    ]
-    ++ lib.optionals voiceMessagesSupport [
-      gst_all_1.gst-plugins-base
-      gst_all_1.gst-plugins-good
-    ]
-    ++ lib.optional (chatType == "webkit") qtwebkit
-    ++ lib.optional (chatType == "webengine") qtwebengine;
+  buildInputs = [
+    qtbase
+    qtmultimedia
+    qtx11extras
+    libidn
+    qca-qt5
+    libXScrnSaver
+    hunspell
+    libsecret
+    libgcrypt
+    libotr
+    html-tidy
+    libgpgerror
+    libsignal-protocol-c
+    usrsctp
+  ] ++ lib.optionals voiceMessagesSupport [
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+  ] ++ lib.optionals (chatType == "webkit") [
+    qtwebkit
+  ] ++ lib.optionals (chatType == "webengine") [
+    qtwebengine
+  ];
 
   preFixup = lib.optionalString voiceMessagesSupport ''
     qtWrapperArgs+=(
