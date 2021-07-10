c088c684c85532ed23c1715dd1586b742d10d27a
Fabian Affolter <mail@fabian-affolter.ch>
2021-07-10 11:16:51 +0200

    python3Packages.nexia: 0.9.8 -> 0.9.9

--- pkgs/development/python-modules/nexia/default.nix
+++ pkgs/development/python-modules/nexia/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "nexia";
-  version = "0.9.8";
+  version = "0.9.9";
   disabled = pythonOlder "3.5";
 
   src = fetchFromGitHub {
     owner = "bdraco";
     repo = pname;
     rev = version;
-    sha256 = "sha256-KjA3AnrMg4ySKSY2FDHVo0Zh9GoLUWiQDuUzo8d5At8=";
+    sha256 = "sha256-OamQ6p8o23lVeOB/KyNQI7G8xZaAaVNYacoRfbNKJtk=";
   };
 
   propagatedBuildInputs = [
