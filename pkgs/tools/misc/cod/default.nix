3d2ffb4e019ec1ba1e0cebbbac66119f5e3126bd
Sandro <sandro.jaeckel@gmail.com>
2021-07-11 19:40:56 +0200

    Update pkgs/tools/misc/cod/default.nix

--- pkgs/tools/misc/cod/default.nix
+++ pkgs/tools/misc/cod/default.nix
@@ -8,7 +8,7 @@ buildGoModule rec {
     owner = "dim-an";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0h95y2pci3czwn9dh2xblq5cah7znx526khnla34rkxghkrdd3ix";
+    sha256 = "0wi680sxpv0kp1ggy21qp4c4ms79hw4z9w9kvp278p8z3y8wwglr";
   };
 
   vendorSha256 = "1arllkiz1hk12hq5b2zpg3f8i9lxl66mil5sdv8gnhflmb37vbv3";
