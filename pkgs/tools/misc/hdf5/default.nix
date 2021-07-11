2da3c4adaad27af002a7f1cab80f928ff3d83278
R. RyanTM <ryantm-bot@ryantm.com>
2021-07-11 09:43:41 +0000

    hdf5-cpp: 1.12.0 -> 1.12.1

--- pkgs/tools/misc/hdf5/default.nix
+++ pkgs/tools/misc/hdf5/default.nix
@@ -20,11 +20,11 @@ assert !cpp || !mpiSupport;
 let inherit (lib) optional optionals; in
 
 stdenv.mkDerivation rec {
-  version = "1.12.0";
+  version = "1.12.1";
   pname = "hdf5";
   src = fetchurl {
     url = "https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-${lib.versions.majorMinor version}/${pname}-${version}/src/${pname}-${version}.tar.bz2";
-    sha256 = "0qazfslkqbmzg495jafpvqp0khws3jkxa0z7rph9qvhacil6544p";
+    sha256 = "sha256-qvn1MrPtqD09Otyfi0Cpt2MVIhj6RTScO8d1Asofjxw=";
   };
 
   passthru = {
