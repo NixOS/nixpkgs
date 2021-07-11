f638849080be56194a6e78f2f22dc039025c914a
Sandro <sandro.jaeckel@gmail.com>
2021-07-11 21:59:51 +0200

    Update pkgs/tools/misc/cod/default.nix

--- pkgs/tools/misc/cod/default.nix
+++ pkgs/tools/misc/cod/default.nix
@@ -11,7 +11,7 @@ buildGoModule rec {
     sha256 = "0wi680sxpv0kp1ggy21qp4c4ms79hw4z9w9kvp278p8z3y8wwglr";
   };
 
-  vendorSha256 = "1arllkiz1hk12hq5b2zpg3f8i9lxl66mil5sdv8gnhflmb37vbv3";
+  vendorSha256 = "0ann1fbh8rqys3rwbz5h9mfnvkpqiw5rgkd4c30y99706h2dzv4i";
 
   buildFlagsArray = [ "-ldflags=-s -w -X main.GitSha=${src.rev}" ];
 
