f53f5613aa837f11c58faea6a66dc9a132ef0228
Bob van der Linden <bobvanderlinden@gmail.com>
2021-07-11 17:45:06 +0200

    neo4j-desktop: rename bin/neo4j-desktop{-1.4.7,}

--- pkgs/applications/misc/neo4j-desktop/default.nix
+++ pkgs/applications/misc/neo4j-desktop/default.nix
@@ -9,13 +9,14 @@ let
     hash = "sha256-oEcSW7UIeiGY2fCPJQ5EtDBRtd73Q04HaSk+7ObLKJc=";
   };
 
-  appimageContents = appimageTools.extract { inherit src; name = pname; };
+  appimageContents = appimageTools.extract { inherit name src; };
 in appimageTools.wrapType2 {
   inherit name src;
 
   extraPkgs = pkgs: with pkgs; [ libsecret ];
 
   extraInstallCommands = ''
+    mv $out/bin/${name} $out/bin/${pname}
     install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
     substituteInPlace $out/share/applications/${pname}.desktop \
       --replace 'Exec=AppRun' 'Exec=${pname}'
