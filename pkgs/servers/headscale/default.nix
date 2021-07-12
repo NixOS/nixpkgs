43861af0f93f797ce33e6b950ac54045691b15f1
R. RyanTM <ryantm-bot@ryantm.com>
2021-07-12 04:49:50 +0000

    headscale: 0.2.2 -> 0.3.2

--- pkgs/servers/headscale/default.nix
+++ pkgs/servers/headscale/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "headscale";
-  version = "0.2.2";
+  version = "0.3.2";
 
   src = fetchFromGitHub {
     owner = "juanfont";
     repo = "headscale";
     rev = "v${version}";
-    sha256 = "0iij4hdbdplai5c8kvcwiw11idrk04xbpwb3njdhqcg6pdyx3216";
+    sha256 = "sha256-bq4WnLwNW8gvNMdTPN4lyWfwOYS/zf2EaSbenBXYfi4=";
   };
 
-  vendorSha256 = "sha256-8MCSN8wn2Y9ejHAIEwTdIXGITaa+FqX7WUMJEr0RcIk=";
+  vendorSha256 = "sha256-0Lqr/tWk31S01vi21sG2gtlGouOhecL4u8ScKG0nWLo=";
 
   # Ldflags are same as build target in the project's Makefile
   # https://github.com/juanfont/headscale/blob/main/Makefile
