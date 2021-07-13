54e1d5a3e6375bef22e046454e7eb3cb659f83bd
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-07-13 14:33:00 +0200

    haskell.compiler.ghc865Binary: mark as broken when built with musl
    The musl support for binary GHC 8.6.5 relied on ABI compat between musl
    and glibc which is no longer the case: https://github.com/NixOS/nixpkgs/issues/129247
    
    Since there is no upstream musl (alpine) bindist for GHC 8.6.5, we can
    only accept that binary 8.6.5 is not possible with musl.

--- pkgs/development/compilers/ghc/8.6.5-binary.nix
+++ pkgs/development/compilers/ghc/8.6.5-binary.nix
@@ -190,5 +190,7 @@ stdenv.mkDerivation rec {
     license = lib.licenses.bsd3;
     platforms = ["x86_64-linux" "aarch64-linux" "i686-linux" "x86_64-darwin"];
     hydraPlatforms = builtins.filter (p: p != "aarch64-linux") platforms;
+    # build segfaults, use ghc8102Binary which has proper musl support instead
+    broken = stdenv.hostPlatform.isMusl;
   };
 }
