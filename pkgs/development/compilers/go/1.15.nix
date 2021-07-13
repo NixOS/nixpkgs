11beeade2d9832eaba6cfceefa3edab60375de69
Maxine Aubrey <maxeaubrey@gmail.com>
2021-07-13 17:55:25 +0200

    go_1_15: 1.15.13 -> 1.15.14
    (cherry picked from commit 25eb6344d03d644084d9d3cb3c571f5664accf93)

--- pkgs/development/compilers/go/1.15.nix
+++ pkgs/development/compilers/go/1.15.nix
@@ -50,11 +50,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "go";
-  version = "1.15.13";
+  version = "1.15.14";
 
   src = fetchurl {
     url = "https://dl.google.com/go/go${version}.src.tar.gz";
-    sha256 = "sha256-mQaeciNHnM5FU/hPh0uTRfb0BF8nz1CJSJtUbaYZokQ=";
+    sha256 = "sha256-YKSlxI1j0KE+yohJAJtiRin/QpyLxdGmqMPE2p805wo=";
   };
 
   # perl is used for testing go vet
