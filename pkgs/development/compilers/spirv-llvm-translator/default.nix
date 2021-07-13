d83b9212503cbebdde691170f2dbc8d7705155ce
kvtb <76634406+kvtb@users.noreply.github.com>
2021-07-13 20:16:39 +0000

    spirv-llvm-translator: 8.0.1 -> 12.0.0

--- pkgs/development/compilers/spirv-llvm-translator/default.nix
+++ pkgs/development/compilers/spirv-llvm-translator/default.nix
@@ -1,33 +1,36 @@
-{ lib, stdenv
+{ lib
 , fetchFromGitHub
 , cmake
-, pkg-config
-
 , lit
-, llvm_8
+, llvmPackages_12
 }:
 
-stdenv.mkDerivation rec {
-  pname = "SPIRV-LLVM-Translator";
-  version = "8.0.1-2";
+llvmPackages_12.stdenv.mkDerivation rec {
+  pname = "spirv-llvm-translator";
+  version = "12.0.0";
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "SPIRV-LLVM-Translator";
     rev = "v${version}";
-    sha256 = "0hxalc3fkliqs61hpr97phbm3qsx4b8vgnlg30aimzr6aas403r5";
+    sha256 = "0d6v7h00z7ipcn2qlidma2fs5ywf85ji1k6gskw2mh5szwggcajg";
   };
 
-  nativeBuildInputs = [ pkg-config cmake llvm_8.dev ];
+  nativeBuildInputs = [ cmake ];
 
-  buildInputs = [ llvm_8 ];
+  buildInputs = [ llvmPackages_12.llvm ];
 
   checkInputs = [ lit ];
 
-  cmakeFlags = [ "-DLLVM_INCLUDE_TESTS=ON" ];
+  cmakeFlags = [ "-DLLVM_INCLUDE_TESTS=ON"
+                 "-DLLVM_EXTERNAL_LIT=${lit}/bin/lit"
+               ];
+  # also make `llvm-spirv`
+  postBuild = "make llvm-spirv";
 
-  # FIXME: CMake tries to run "/llvm-lit" which of course doesn't exist
-  doCheck = false;
+  doCheck = true;
+
+  postInstall = "install -Dm755 tools/llvm-spirv/llvm-spirv -t ${placeholder "out"}/bin";
 
   meta = with lib; {
     homepage    = "https://github.com/KhronosGroup/SPIRV-LLVM-Translator";
