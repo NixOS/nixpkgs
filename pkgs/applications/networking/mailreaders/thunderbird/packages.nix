0c19ac53e46f94b31a63fdd96f266dbb25ea4380
Yureka <yuka@yuka.dev>
2021-07-13 19:15:12 +0200

    thunderbird: 78.11.0 -> 90.0b3

--- pkgs/applications/networking/mailreaders/thunderbird/packages.nix
+++ pkgs/applications/networking/mailreaders/thunderbird/packages.nix
@@ -7,6 +7,34 @@ in
 rec {
   thunderbird = common rec {
     pname = "thunderbird";
+    version = "90.0b3";
+    application = "comm/mail";
+    binaryName = pname;
+    src = fetchurl {
+      url = "mirror://mozilla/thunderbird/releases/${version}/source/thunderbird-${version}.source.tar.xz";
+      sha512 = "be79ff41b1dd1bb8035512a79864da41be45514ec7feca4fe656532bedbee220bf61c3061b7e9e15ad06895c181ce1aa6c2effcecaad7877fa72e3cd4859ce01";
+    };
+    patches = [
+      ./no-buildconfig-90.patch
+    ];
+
+    meta = {
+      description = "A full-featured e-mail client";
+      homepage = "https://thunderbird.net/";
+      maintainers = with lib.maintainers; [ eelco lovesegfault pierron vcunat];
+      platforms = lib.platforms.unix;
+      badPlatforms = lib.platforms.darwin;
+      broken = stdenv.buildPlatform.is32bit; # since Firefox 60, build on 32-bit platforms fails with "out of memory".
+                                             # not in `badPlatforms` because cross-compilation on 64-bit machine might work.
+      license = lib.licenses.mpl20;
+    };
+    updateScript = callPackage ./update.nix {
+      attrPath = "thunderbird-unwrapped";
+    };
+  };
+
+  thunderbird-78 = common rec {
+    pname = "thunderbird";
     version = "78.12.0";
     application = "comm/mail";
     binaryName = pname;
