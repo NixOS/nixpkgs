e51348d672c74b0d848976182600a41f14752fac
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-07-14 00:37:09 -0400

    plasmaMobileGear: 21.05 -> 21.06
     - https://www.plasma-mobile.org/2021/06/10/plasma-mobile-update-june/
     - https://www.plasma-mobile.org/info/plasma-mobile-gear-21-06/
    
    New:
    
     - kasts
     - keysmith
     - kweather
     - tokodon

--- pkgs/applications/plasma-mobile/srcs.nix
+++ pkgs/applications/plasma-mobile/srcs.nix
@@ -4,115 +4,147 @@
 
 {
   alligator = {
-    version = "21.05";
+    version = "21.06";
     src = fetchurl {
-      url = "${mirror}/stable/plasma-mobile/21.05/alligator-21.05.tar.xz";
-      sha256 = "04zgxfx2zmn1p2ap08i5sfsnrly3smip4ylr07ghkhkiyjzbv9w6";
-      name = "alligator-21.05.tar.xz";
+      url = "${mirror}/stable/plasma-mobile/21.06/alligator-21.06.tar.xz";
+      sha256 = "18f1hsfl5glmrs778z8xkcszcs7fj148giz9bs5adg1sczjdja2j";
+      name = "alligator-21.06.tar.xz";
     };
   };
   angelfish = {
-    version = "21.05";
+    version = "21.06";
     src = fetchurl {
-      url = "${mirror}/stable/plasma-mobile/21.05/angelfish-21.05.tar.xz";
-      sha256 = "11jd5dwy0xa7kh5z5rc29xy3wfn20hm31908zjax4x83qqjrm075";
-      name = "angelfish-21.05.tar.xz";
+      url = "${mirror}/stable/plasma-mobile/21.06/angelfish-21.06.tar.xz";
+      sha256 = "0yvp307gbj3c8wk7cyk2hdh1r1gy1x7j2mbr8pkl6sfyy0djcy48";
+      name = "angelfish-21.06.tar.xz";
     };
   };
   calindori = {
-    version = "21.05";
+    version = "21.06";
     src = fetchurl {
-      url = "${mirror}/stable/plasma-mobile/21.05/calindori-21.05.tar.xz";
-      sha256 = "110f9ri9r1nb6q1ybhkfxljl4q5gqxikh9z0xkzjjbxjjqfscqcj";
-      name = "calindori-21.05.tar.xz";
+      url = "${mirror}/stable/plasma-mobile/21.06/calindori-21.06.tar.xz";
+      sha256 = "0ivfy9n7gcbxpyk6d6zlaj6xnzcvgrn01svild61cza5vcp1dp28";
+      name = "calindori-21.06.tar.xz";
     };
   };
   kalk = {
-    version = "21.05";
+    version = "21.06";
     src = fetchurl {
-      url = "${mirror}/stable/plasma-mobile/21.05/kalk-21.05.tar.xz";
-      sha256 = "04n65hx0j9rx6b3jq69zgypi8qjd4ig3rfw7d44c3q7dgh4k451l";
-      name = "kalk-21.05.tar.xz";
+      url = "${mirror}/stable/plasma-mobile/21.06/kalk-21.06.tar.xz";
+      sha256 = "06i9z3x8c5yiylzk272cbmp88q1bk6plspp6zckwzbmi19pkvmki";
+      name = "kalk-21.06.tar.xz";
+    };
+  };
+  kasts = {
+    version = "21.06";
+    src = fetchurl {
+      url = "${mirror}/stable/plasma-mobile/21.06/kasts-21.06.tar.xz";
+      sha256 = "0kayg137haqfjb2j7bc61vnqq2gp0f24n41lqygwnndh2wyl22x0";
+      name = "kasts-21.06.tar.xz";
     };
   };
   kclock = {
-    version = "21.05";
+    version = "21.06";
     src = fetchurl {
-      url = "${mirror}/stable/plasma-mobile/21.05/kclock-21.05.tar.xz";
-      sha256 = "0pa5hvax0y80l8yrqczh9mcknfm3z0vdq3xc35cxdiz1vc6fwqmd";
-      name = "kclock-21.05.tar.xz";
+      url = "${mirror}/stable/plasma-mobile/21.06/kclock-21.06.tar.xz";
+      sha256 = "1h91l2ip15kfd2p85r96npib34gms7zacnn8dqb4j2m6znxjh9ib";
+      name = "kclock-21.06.tar.xz";
+    };
+  };
+  keysmith = {
+    version = "21.06";
+    src = fetchurl {
+      url = "${mirror}/stable/plasma-mobile/21.06/keysmith-21.06.tar.xz";
+      sha256 = "01v05m327av4hwn50slay8w1flsgh2as06z582r08y4rrn2q6crg";
+      name = "keysmith-21.06.tar.xz";
     };
   };
   koko = {
-    version = "21.05";
+    version = "21.06";
     src = fetchurl {
-      url = "${mirror}/stable/plasma-mobile/21.05/koko-21.05.tar.xz";
-      sha256 = "00hnzkl8dvf15psrcfh96b8wfb3pbggd2f7xnadzcb87sbaml035";
-      name = "koko-21.05.tar.xz";
+      url = "${mirror}/stable/plasma-mobile/21.06/koko-21.06.tar.xz";
+      sha256 = "117v8m68i4yq0j1lr2xm1c08474xlscxdnxv979gjcz0ghw402q2";
+      name = "koko-21.06.tar.xz";
     };
   };
   kongress = {
-    version = "21.05";
+    version = "21.06";
     src = fetchurl {
-      url = "${mirror}/stable/plasma-mobile/21.05/kongress-21.05.tar.xz";
-      sha256 = "0qxgpi04ra9crc6drgbdm9arjbvcx52pprjr1dj8acch07f6i2gs";
-      name = "kongress-21.05.tar.xz";
+      url = "${mirror}/stable/plasma-mobile/21.06/kongress-21.06.tar.xz";
+      sha256 = "03nm9jnhv5nqq8l9qp6xxk96vl44dias9y9y59agcs9lz16vxxvz";
+      name = "kongress-21.06.tar.xz";
     };
   };
   krecorder = {
-    version = "21.05";
+    version = "21.06";
     src = fetchurl {
-      url = "${mirror}/stable/plasma-mobile/21.05/krecorder-21.05.tar.xz";
-      sha256 = "0ydaidxx2616bixihyaagvyym1r5s9rjkgg04vq9k4608d4vnn5c";
-      name = "krecorder-21.05.tar.xz";
+      url = "${mirror}/stable/plasma-mobile/21.06/krecorder-21.06.tar.xz";
+      sha256 = "1l2wq9hwd1dimc8n35hd7cckh8mcjabjv936qzk32c7pychxnz5f";
+      name = "krecorder-21.06.tar.xz";
     };
   };
   ktrip = {
-    version = "21.05";
+    version = "21.06";
     src = fetchurl {
-      url = "${mirror}/stable/plasma-mobile/21.05/ktrip-21.05.tar.xz";
-      sha256 = "0hxgnncyc2ir6i9p6s9fy1r4mhxgm643pxvp8lj3j5y0c5wk2kp9";
-      name = "ktrip-21.05.tar.xz";
+      url = "${mirror}/stable/plasma-mobile/21.06/ktrip-21.06.tar.xz";
+      sha256 = "172y49g7h60dksd96fim67c1lmqfpha83rvh5r0hljla8x4jjrip";
+      name = "ktrip-21.06.tar.xz";
+    };
+  };
+  kweather = {
+    version = "21.06";
+    src = fetchurl {
+      url = "${mirror}/stable/plasma-mobile/21.06/kweather-21.06.tar.xz";
+      sha256 = "0ja0bagsk5rbdmgpakc236vlx1dfk5bnr60cdb1da7pswvxz9gfn";
+      name = "kweather-21.06.tar.xz";
     };
   };
   plasma-dialer = {
-    version = "21.05";
+    version = "21.06";
     src = fetchurl {
-      url = "${mirror}/stable/plasma-mobile/21.05/plasma-dialer-21.05.tar.xz";
-      sha256 = "0kwkjn7ry6snc86qi1j7kcq5qa6rbyk5i7v6gqf7a7wywkk9n045";
-      name = "plasma-dialer-21.05.tar.xz";
+      url = "${mirror}/stable/plasma-mobile/21.06/plasma-dialer-21.06.tar.xz";
+      sha256 = "1b6zdswy5wfbzcmpy5hmblbv95hb5bwdh6bi7f1r196aw74j4hxl";
+      name = "plasma-dialer-21.06.tar.xz";
     };
   };
   plasma-phonebook = {
-    version = "21.05";
+    version = "21.06";
     src = fetchurl {
-      url = "${mirror}/stable/plasma-mobile/21.05/plasma-phonebook-21.05.tar.xz";
-      sha256 = "0aqqi3gvcsh4zg41zf8y0c626lwrabjchhr8pxj4n9la7y0wdvca";
-      name = "plasma-phonebook-21.05.tar.xz";
+      url = "${mirror}/stable/plasma-mobile/21.06/plasma-phonebook-21.06.tar.xz";
+      sha256 = "0a5vkvysjmp3lbwn9s1pfjijk9mc0yvyp3xshg0w8bz5ky06rs8f";
+      name = "plasma-phonebook-21.06.tar.xz";
     };
   };
   plasma-settings = {
-    version = "21.05";
+    version = "21.06";
     src = fetchurl {
-      url = "${mirror}/stable/plasma-mobile/21.05/plasma-settings-21.05.tar.xz";
-      sha256 = "16bhx0i8gvi9ina4jxzx02xyzypyjic9646lahxvzvzmd9hn9ipi";
-      name = "plasma-settings-21.05.tar.xz";
+      url = "${mirror}/stable/plasma-mobile/21.06/plasma-settings-21.06.tar.xz";
+      sha256 = "1lvjjdrgk5cxxzsb2knlbh27n7ji1plpcxr2lycfv0fjq51rjsjg";
+      name = "plasma-settings-21.06.tar.xz";
     };
   };
   qmlkonsole = {
-    version = "21.05";
+    version = "21.06";
     src = fetchurl {
-      url = "${mirror}/stable/plasma-mobile/21.05/qmlkonsole-21.05.tar.xz";
-      sha256 = "1ga48n09zlgvf5vvk2m26ak3ih5gjf361xxnyfprimmd7yj23han";
-      name = "qmlkonsole-21.05.tar.xz";
+      url = "${mirror}/stable/plasma-mobile/21.06/qmlkonsole-21.06.tar.xz";
+      sha256 = "15rwn0rhx4c7mn68494ylg8ng1pb4afhwwndiarr8fzmi29fgx8l";
+      name = "qmlkonsole-21.06.tar.xz";
     };
   };
   spacebar = {
-    version = "21.05";
+    version = "21.06";
     src = fetchurl {
-      url = "${mirror}/stable/plasma-mobile/21.05/spacebar-21.05.tar.xz";
-      sha256 = "16lvi5yzmvk6gb5m7csk44y2jbkk7psn1lkljmj1938p2475b94c";
-      name = "spacebar-21.05.tar.xz";
+      url = "${mirror}/stable/plasma-mobile/21.06/spacebar-21.06.tar.xz";
+      sha256 = "14wk35yk7crp9449x23yy8sxg8cnxb7gp6fhnws8zc89w3l2faqn";
+      name = "spacebar-21.06.tar.xz";
+    };
+  };
+  tokodon = {
+    version = "21.06";
+    src = fetchurl {
+      url = "${mirror}/stable/plasma-mobile/21.06/tokodon-21.06.tar.xz";
+      sha256 = "1xzjq3sqdccarq9a3w54hajqnhlg3lplg17f81sy9yld1as4rria";
+      name = "tokodon-21.06.tar.xz";
     };
   };
 }
