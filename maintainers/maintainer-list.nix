8b01f2c017591289dc4abc13021a41cb6b21c992
whonore <wolfhonore@gmail.com>
2021-07-15 15:11:14 -0400

    maintainers: add whonore

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11314,6 +11314,12 @@
     githubId = 19174984;
     name = "Alex Whitt";
   };
+  whonore = {
+    email = "wolfhonore@gmail.com";
+    github = "whonore";
+    githubId = 7121530;
+    name = "Wolf Honoré";
+  };
   wildsebastian = {
     name = "Sebastian Wild";
     email = "sebastian@wild-siena.com";
