89b566d562b69b6db74d2f6f1a95685688afb5be
dan4ik <6057430gu@gmail.com>
2021-07-16 11:05:24 +0700

    coreterminal: init at 4.2.0

---
+++ pkgs/applications/terminal-emulators/coreterminal/default.nix
@@ -0,0 +1,35 @@
+{ qt5
+, lib
+, fetchzip
+, cmake
+, ninja
+}:
+
+qt5.mkDerivation rec {
+  pname = "coreterminal";
+  version = "4.2.0";
+
+  src = fetchzip {
+    url = "https://gitlab.com/cubocore/coreapps/${pname}/-/archive/v${version}/${pname}-v${version}.tar.gz";
+    sha256 = "sha256-YXs6VTem3AaK4n1DYwKP/jqNuf09Srn2THHyJJnArlc=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+  ];
+
+  buildInputs = [
+    qt5.full
+    qtermwidget
+    libcprime
+  ];
+
+  meta = with lib; {
+    description = "A terminal emulator from the C Suite";
+    homepage = "https://gitlab.com/cubocore/coreapps/coreterminal";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ dan4ik605743 ];
+    platforms = platforms.linux;
+  };
+}
