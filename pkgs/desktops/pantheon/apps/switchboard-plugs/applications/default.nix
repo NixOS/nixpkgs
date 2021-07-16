627c9fc86d9108f7d669641b66e45d4c46d65bc0
Bobby Rong <rjl931189261@126.com>
2021-07-16 23:06:58 +0800

    pantheon.switchboard-plug-applications: 2.1.7 -> 6.0.0

--- pkgs/desktops/pantheon/apps/switchboard-plugs/applications/default.nix
+++ pkgs/desktops/pantheon/apps/switchboard-plugs/applications/default.nix
@@ -1,5 +1,6 @@
 { lib, stdenv
 , fetchFromGitHub
+, fetchpatch
 , nix-update-script
 , pantheon
 , meson
@@ -10,18 +11,28 @@
 , granite
 , gtk3
 , switchboard
+, flatpak
 }:
 
 stdenv.mkDerivation rec {
   pname = "switchboard-plug-applications";
-  version = "2.1.7";
+  version = "6.0.0";
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = pname;
     rev = version;
-    sha256 = "sha256-uiu19XyogNR0LX/2IIZM6B1JKAfqDMOHniQ29EAa9XM=";
+    sha256 = "0hgvmrgg6g2sjb3sda7kzfcd3zgngd5w982drl6ll44k1mh16gsj";
   };
+
+  patches = [
+    # Upstream code not respecting our localedir
+    # https://github.com/elementary/switchboard-plug-applications/pull/163
+    (fetchpatch {
+      url = "https://github.com/elementary/switchboard-plug-applications/commit/25db490654ab41694be7b7ba19218376f42fbb8d.patch";
+      sha256 = "16y8zcwnnjsh72ifpyqcdb9f5ajdj0iy8kb5sj6v77c1cxdhrv29";
+    })
+  ];
 
   passthru = {
     updateScript = nix-update-script {
@@ -37,6 +48,7 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
+    flatpak
     granite
     gtk3
     libgee
@@ -46,7 +58,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Switchboard Applications Plug";
     homepage = "https://github.com/elementary/switchboard-plug-applications";
-    license = licenses.gpl2Plus;
+    license = licenses.gpl3Plus;
     platforms = platforms.linux;
     maintainers = pantheon.maintainers;
   };
