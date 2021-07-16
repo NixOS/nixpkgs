fe7e1ec567c7255c5a91cbef30f4bb66400a706d
AndersonTorres <torres.anderson.85@protonmail.com>
2021-07-16 20:31:51 -0300

    org-packages 2021-07-16
    (cherry picked from commit e56573a734fe6eeb38352908aaf846e33e396e85)

--- pkgs/applications/editors/emacs/elisp-packages/org-generated.nix
+++ pkgs/applications/editors/emacs/elisp-packages/org-generated.nix
@@ -4,10 +4,10 @@
       elpaBuild {
         pname = "org";
         ename = "org";
-        version = "20210628";
+        version = "20210712";
         src = fetchurl {
-          url = "https://orgmode.org/elpa/org-20210628.tar";
-          sha256 = "1sn2yyynndk8qf43ss8bayll33r4ina8xfx4ywzcs3m1lm6xy1zl";
+          url = "https://orgmode.org/elpa/org-20210712.tar";
+          sha256 = "0xdxzfk7hvsmlyivn61ivci6hy2alxg2ysdm5xad4xxz337jrj7x";
         };
         packageRequires = [];
         meta = {
@@ -19,10 +19,10 @@
       elpaBuild {
         pname = "org-plus-contrib";
         ename = "org-plus-contrib";
-        version = "20210628";
+        version = "20210712";
         src = fetchurl {
-          url = "https://orgmode.org/elpa/org-plus-contrib-20210628.tar";
-          sha256 = "0r4kxp1hbhkwvi7939fglng8db4h4n7vigy8pd2gia3a02xcw8l5";
+          url = "https://orgmode.org/elpa/org-plus-contrib-20210712.tar";
+          sha256 = "1cdpwsfjmjplyik1r9kl4lvd5lm52zrixlfg2ml1mhh28s680k0q";
         };
         packageRequires = [];
         meta = {
