d06e921ec63cc63bc41bc7d02c3f14cb6eb538d5
happysalada <raphael@megzari.com>
2021-07-16 17:39:50 +0900

    vscode-extensions.coenraads.bracket-pair-colorizer-2: 0.2.0 -> 0.2.1

--- pkgs/misc/vscode-extensions/default.nix
+++ pkgs/misc/vscode-extensions/default.nix
@@ -270,8 +270,8 @@ let
         mktplcRef = {
           name = "bracket-pair-colorizer-2";
           publisher = "CoenraadS";
-          version = "0.2.0";
-          sha256 = "0nppgfbmw0d089rka9cqs3sbd5260dhhiipmjfga3nar9vp87slh";
+          version = "0.2.1";
+          sha256 = "0bfvzs4ac537zqhnqaa38jf4lhiy1fmqcv6lq89nnx8k963380z7";
         };
         meta = with lib; {
           license = licenses.mit;
