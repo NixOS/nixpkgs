6990d4cf12e4c64263bd24f77a2eed3ae1ed6e8c
Phillip Seeber <phillip.seeber@googlemail.com>
2021-07-16 11:34:12 +0200

    avogadrolibs: init at 1.94.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29746,6 +29746,8 @@ in
     eigen = eigen2;
   };
 
+  avogadrolibs = libsForQt5.callPackage ../development/libraries/science/chemistry/avogadrolibs { };
+
   molequeue = libsForQt5.callPackage ../development/libraries/science/chemistry/molequeue { };
 
   chemtool = callPackage ../applications/science/chemistry/chemtool { };
