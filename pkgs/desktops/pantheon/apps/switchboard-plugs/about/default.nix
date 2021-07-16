702d48b75782e3be106cb19a9feeea91ddae960e
Bobby Rong <rjl931189261@126.com>
2021-07-16 23:02:01 +0800

    pantheon.switchboard-plug-about: 2.6.3 -> 6.0.0

--- pkgs/desktops/pantheon/apps/switchboard-plugs/about/default.nix
+++ pkgs/desktops/pantheon/apps/switchboard-plugs/about/default.nix
@@ -1,6 +1,5 @@
 { lib, stdenv
 , fetchFromGitHub
-, fetchpatch
 , nix-update-script
 , pantheon
 , substituteAll
@@ -9,22 +8,26 @@
 , pkg-config
 , vala
 , libgee
+, libgtop
+, libhandy
 , granite
 , gtk3
 , switchboard
 , pciutils
 , elementary-feedback
+, fwupd
+, appstream
 }:
 
 stdenv.mkDerivation rec {
   pname = "switchboard-plug-about";
-  version = "2.6.3";
+  version = "6.0.0";
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = pname;
     rev = version;
-    sha256 = "sha256-wis6wNEOOjPLUCT9vRRhMxbKHR2Y2nZArKogSF/FQv8=";
+    sha256 = "0rlp52ylgz41c3s4q8zrrynfl0yihvbgb8cg9cz6ywdaaahi3asp";
   };
 
   passthru = {
@@ -41,27 +44,17 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
+    appstream
+    fwupd
     granite
     gtk3
     libgee
+    libgtop
+    libhandy
     switchboard
   ];
 
   patches = [
-    # Get OS Info from GLib.Environment
-    # https://github.com/elementary/switchboard-plug-about/pull/128
-    (fetchpatch {
-      url = "https://github.com/elementary/switchboard-plug-about/commit/5ed29988e3a895b2df66e5529df0f12a94d5517c.patch";
-      sha256 = "1ipDxnpDZjpSEzZdtOeNe5U+QOXiB5M+hC3yDAsl/rQ=";
-    })
-
-    # Use Pretty Name
-    # https://github.com/elementary/switchboard-plug-about/pull/134
-    (fetchpatch {
-      url = "https://github.com/elementary/switchboard-plug-about/commit/653d131dc8fac10ae7523f2bf6b179ffffa9c0fd.patch";
-      sha256 = "AsM49Dc9/yn2tG6fqjfedeOlDXUu+iEoyNUmNYLH+zE=";
-    })
-
     (substituteAll {
       src = ./fix-paths.patch;
       inherit pciutils;
