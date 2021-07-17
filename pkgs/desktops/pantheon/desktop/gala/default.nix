a72793822ce1d104e3f0e01813aeb8dd05db8008
Bobby Rong <rjl931189261@126.com>
2021-07-17 15:37:19 +0800

    pantheon.gala: 3.3.2 -> 6.0.0

--- pkgs/desktops/pantheon/desktop/gala/default.nix
+++ pkgs/desktops/pantheon/desktop/gala/default.nix
@@ -1,7 +1,6 @@
 { lib, stdenv
 , fetchFromGitHub
 , nix-update-script
-, fetchpatch
 , pantheon
 , pkg-config
 , meson
@@ -24,17 +23,18 @@
 , elementary-icon-theme
 , elementary-settings-daemon
 , wrapGAppsHook
+, gexiv2
 }:
 
 stdenv.mkDerivation rec {
   pname = "gala";
-  version = "3.3.2";
+  version = "6.0.0";
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = pname;
     rev = version;
-    sha256 = "sha256-BOarHUEgWqQM6jmVMauJi0JnsM+jE45MnPNnAqz1qOE=";
+    sha256 = "1a37bds7dwqb19wr1q8vxxpl95i7q3a0bxrih76iq41mzlqk00nj";
   };
 
   passthru = {
@@ -61,6 +61,7 @@ stdenv.mkDerivation rec {
     elementary-dock
     elementary-icon-theme
     elementary-settings-daemon
+    gexiv2
     gnome-desktop
     granite
     gtk3
@@ -68,18 +69,6 @@ stdenv.mkDerivation rec {
     libcanberra-gtk3
     libgee
     mutter
-  ];
-
-  patches = [
-    # https://github.com/elementary/gala/pull/869
-    # build failure in vala 0.48.7
-    # https://github.com/elementary/gala/pull/869#issuecomment-657147695
-    (fetchpatch {
-      url = "https://github.com/elementary/gala/commit/85d290c75eaa147b704ad34e6c67498071707ee8.patch";
-      sha256 = "19jkvmxidf453qfrxkvi35igxzfz2cm8srwkabvyn9wyd1yhiw0l";
-    })
-    ./plugins-dir.patch
-    ./use-new-notifications-default.patch
   ];
 
   postPatch = ''
