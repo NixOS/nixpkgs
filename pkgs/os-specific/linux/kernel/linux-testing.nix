8a64963e5ce1d3fb8456e785697b8cf9172debbf
arcnmx <arcnmx@users.noreply.github.com>
2021-07-18 10:49:32 -0700

    linux_testing: 5.13-rc6 -> 5.14-rc7

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,15 +3,15 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.13-rc6";
-  extraMeta.branch = "5.12";
+  version = "5.14-rc7";
+  extraMeta.branch = lib.versions.majorMinor version;
 
   # modDirVersion needs to be x.y.z, will always add .0
   modDirVersion = if (modDirVersionArg == null) then builtins.replaceStrings ["-"] [".0-"] version else modDirVersionArg;
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "sha256-PunFd6tOsmrsPItp2QX4TEVxHnvvi1BMSwWio/DTlMU=";
+    sha256 = "1q3l4vg6iw6i15ify93hd30kdfdvgl9a22ysdz1rs6aj589fzqfd";
   };
 
   # Should the testing kernels ever be built on Hydra?
