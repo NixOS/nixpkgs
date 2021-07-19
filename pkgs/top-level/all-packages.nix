1894e8d9452640610c531a31186261b97d5d8f1c
Guillaume Girol <symphorien+git@xlumurb.eu>
2021-07-19 12:00:00 +0000

    bitwuzla: init at unstable-2021-07-01

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30447,6 +30447,8 @@ in
 
   boolector = callPackage ../applications/science/logic/boolector {};
 
+  bitwuzla = callPackage ../applications/science/logic/bitwuzla {};
+
   symbiyosys = callPackage ../applications/science/logic/symbiyosys {};
 
   mcy = callPackage ../applications/science/logic/mcy {};
