1feec44d65fa98f59394cea0e7400de54fbcdceb
happysalada <raphael@megzari.com>
2021-07-19 11:50:27 +0900

    builder.sh: strict non null check

--- pkgs/stdenv/generic/builder.sh
+++ pkgs/stdenv/generic/builder.sh
@@ -16,6 +16,6 @@ cat "$setup" >> $out/setup
 # Allow the user to install stdenv using nix-env and get the packages
 # in stdenv.
 mkdir $out/nix-support
-if [ "$propagatedUserEnvPkgs" ]; then
+if [[ -n "${propagatedUserEnvPkgs:-}" ]]; then
     printf '%s ' $propagatedUserEnvPkgs > $out/nix-support/propagated-user-env-packages
 fi
