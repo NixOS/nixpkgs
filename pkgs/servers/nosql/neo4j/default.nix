a9abbd1040059053e471cb235a32916cc3ae5c0b
David R Connell <davidconnell12@gmail.com>
2021-07-20 14:54:58 -0500

    neo4j: resolve compgen dependency error
    - Fix #130112.
    - neo4j was unable to find `compgen` at runtime.
    - Need to use `bashInteractive` version of bash since default bash does
    not have `compgen`.
    - Add a compgen wrapper shell script to pass `compgen` calls to `bash
    -c 'compgen "$@"'` in order to run `compgen` outside of a `bash`
    shell.
    
    neo4j: remove compgen from global environment

--- pkgs/servers/nosql/neo4j/default.nix
+++ pkgs/servers/nosql/neo4j/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, makeWrapper, jre, which, gawk }:
+{ lib, stdenv, fetchurl, makeWrapper, jre, which, gawk, bashInteractive }:
 
 with lib;
 
@@ -18,11 +18,18 @@ stdenv.mkDerivation rec {
     cp -R * "$out/share/neo4j"
 
     mkdir -p "$out/bin"
+
+    compgen_wrapper="$out/share/neo4j/bin/compgen"
+    cat > $compgen_wrapper << _EOF_
+    "${bashInteractive}/bin/bash" -c 'compgen "\$@"'
+    _EOF_
+    chmod +x $compgen_wrapper
+
     for NEO4J_SCRIPT in neo4j neo4j-admin neo4j-import cypher-shell
     do
         makeWrapper "$out/share/neo4j/bin/$NEO4J_SCRIPT" \
             "$out/bin/$NEO4J_SCRIPT" \
-            --prefix PATH : "${lib.makeBinPath [ jre which gawk ]}" \
+            --prefix PATH : "${lib.makeBinPath [ jre which gawk ]}:$out/share/neo4j/bin/" \
             --set JAVA_HOME "${jre}"
     done
   '';
