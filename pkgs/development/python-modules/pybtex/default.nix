3443a7478eda35697b4dec3956882d795ce4a338
Sandro <sandro.jaeckel@gmail.com>
2021-07-22 01:11:45 +0200

    Update pkgs/development/python-modules/pybtex/default.nix
    Co-authored-by: Martin Weinelt <mweinelt@users.noreply.github.com>

--- pkgs/development/python-modules/pybtex/default.nix
+++ pkgs/development/python-modules/pybtex/default.nix
@@ -1,5 +1,10 @@
-{ lib, buildPythonPackage, fetchPypi
-, latexcodec, pyyaml, setuptools }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, latexcodec
+, pyyaml
+, setuptools
+}:
 
 buildPythonPackage rec {
   version = "0.24.0";
