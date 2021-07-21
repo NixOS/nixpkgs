39790153557ce5ff2355110ce40f17275ce631bf
Linus Heckemann <git@sphalerite.org>
2021-07-21 10:20:58 +0200

    meshcentral: init at 0.8.87

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25656,6 +25656,8 @@ in
 
   merkaartor = libsForQt5.callPackage ../applications/misc/merkaartor { };
 
+  meshcentral = callPackage ../tools/admin/meshcentral { };
+
   meshlab = libsForQt5.callPackage ../applications/graphics/meshlab { };
 
   metadata-cleaner = callPackage ../applications/misc/metadata-cleaner { };
