0ef6841aaae5bc48bd1b5add066f1a9a4ab8509e
urlordjames <urlordjames@gmail.com>
2021-07-21 00:14:49 -0400

    mnamer: init at 2.5.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25662,6 +25662,8 @@ in
 
   mmtc = callPackage ../applications/audio/mmtc { };
 
+  mnamer = callPackage ../applications/misc/mnamer { };
+
   moc = callPackage ../applications/audio/moc { };
 
   mod-distortion = callPackage ../applications/audio/mod-distortion { };
