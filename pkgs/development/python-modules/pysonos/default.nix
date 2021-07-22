4172b9822d243e5d7c372e60877f58bc4ec3b732
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-07-23 01:38:22 +0200

    python3Packages.pysonos: 0.0.52 -> 0.0.54

--- pkgs/development/python-modules/pysonos/default.nix
+++ pkgs/development/python-modules/pysonos/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "pysonos";
-  version = "0.0.52";
+  version = "0.0.54";
 
   disabled = !isPy3k;
 
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "amelchio";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-RX+VSy5DkMgpa0kZmTqguD4zHa76LJVeh7SquxEKviA=";
+    sha256 = "sha256-gBOknYHL5nQWFVhCbLN0Ah+1fovcNY4P2myryZnUadk=";
   };
 
   propagatedBuildInputs = [
