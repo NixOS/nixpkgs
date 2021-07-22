3343fecc74232f23a5175fce442190101f141fcc
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-07-22 15:11:01 +0200

    haskellPackages.ap-normalize: use hackage sdist

--- pkgs/development/misc/haskell/ap-normalize/default.nix
+++ pkgs/development/misc/haskell/ap-normalize/default.nix
@@ -1,15 +1,8 @@
-{ mkDerivation, base, fetchgit, inspection-testing, lib
-, transformers
-}:
+{ mkDerivation, base, inspection-testing, lib, transformers }:
 mkDerivation {
   pname = "ap-normalize";
   version = "0.1.0.1";
-  src = fetchgit {
-    url = "https://gitlab.com/lysxia/ap-normalize.git";
-    sha256 = "0xfr00dy5qiisiwkhbmmisbv3zmfak5i8fx3b9ymxswxwvvbhjnc";
-    rev = "ed521b63f02f1fe7d4e4ec5f8c5de2d4f76ab017";
-    fetchSubmodules = true;
-  };
+  sha256 = "820613b12ce759c8c8a254c78a0e4c474b2cd4cfd08fc0c1d4d5584c58ff2288";
   libraryHaskellDepends = [ base ];
   testHaskellDepends = [ base inspection-testing transformers ];
   description = "Self-normalizing applicative expressions";
