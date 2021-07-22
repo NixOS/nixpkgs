38bc6d4941e5272f7475b0313abd95d9375ec11a
Graham Christensen <graham@grahamc.com>
2021-07-21 20:35:43 -0400

    tidyp: document autoreconf issues with a source tarball
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/development/libraries/tidyp/default.nix
+++ pkgs/development/libraries/tidyp/default.nix
@@ -5,6 +5,8 @@ stdenv.mkDerivation rec {
   version = "1.04";
 
   src = fetchurl {
+    # downloads from a legacy GitHub download page from ~11 years ago
+    # project does not work with autoconf anymore and the configure script cannot be generated from the source download
     url = "https://github.com/downloads/petdance/tidyp/${pname}-${version}.tar.gz";
     sha256 = "0f5ky0ih4vap9c6j312jn73vn8m2bj69pl2yd3a5nmv35k9zmc10";
   };
