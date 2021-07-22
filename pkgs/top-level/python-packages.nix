4ebfdf3b88d036b5ebaf3833014ef03f940273d5
IvarWithoutBones <ivar.scholten@protonmail.com>
2021-07-22 22:22:30 +0200

    python3Packages.synergy: init at 0.5.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8436,6 +8436,8 @@ in {
 
   syncer = callPackage ../development/python-modules/syncer { };
 
+  synergy = callPackage ../development/python-modules/synergy { };
+
   synologydsm-api = callPackage ../development/python-modules/synologydsm-api { };
 
   systembridge = callPackage ../development/python-modules/systembridge { };
