ebabecfd2a62ddb3504bcc02f47bd1cc137b07e8
arjix <62168569+arjix@users.noreply.github.com>
2021-07-23 14:06:09 +0200

    nengo-gui: init at 0.4.8 (#130015)
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30082,6 +30082,8 @@ in
 
   ### SCIENCE/MACHINE LEARNING
 
+  nengo-gui = callPackage ../applications/science/machine-learning/nengo-gui { };
+
   sc2-headless = callPackage ../applications/science/machine-learning/sc2-headless { };
 
   ### SCIENCE/MATH
