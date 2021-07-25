b4b29c1c7b7c03e14d1d7b54fac702bc005753e9
Fabian Affolter <mail@fabian-affolter.ch>
2021-07-25 21:10:21 +0200

    python3Packages.millheater: 0.5.0 -> 0.5.2

--- pkgs/development/python-modules/millheater/default.nix
+++ pkgs/development/python-modules/millheater/default.nix
@@ -1,3 +1,4 @@
+
 { lib
 , aiohttp
 , async-timeout
@@ -9,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "millheater";
-  version = "0.5.0";
+  version = "0.5.2";
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "Danielhiversen";
     repo = "pymill";
     rev = version;
-    sha256 = "sha256-uMvCpXz+amb5mR9ebkAit21UFYpsTkjkZRXtmcvWt8k=";
+    sha256 = "0ndfxdg10m9mahnwbs66dnyc1lr8q7vs71y6zwxlc0h27hr3gr0d";
   };
 
   propagatedBuildInputs = [
