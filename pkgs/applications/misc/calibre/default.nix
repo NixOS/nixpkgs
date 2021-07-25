2fc9bbc44e0d7fe5681b4e4c48f4b3dcba889676
Mukund Lakshman <yaymukund@gmail.com>
2021-07-25 14:08:30 +0100

    calibre: Pin autoconf to 0.31
    This prevents https://bugs.launchpad.net/calibre/+bug/1936889 which has
    been patched [1] in calibre master but not yet released.
    
    [1]: https://github.com/kovidgoyal/calibre/commit/4f9e83e6426483b6cc0929c61f9207c33c573fec

--- pkgs/applications/misc/calibre/default.nix
+++ pkgs/applications/misc/calibre/default.nix
@@ -94,7 +94,7 @@ mkDerivation rec {
       python
       regex
       sip
-      zeroconf
+      (callPackage ./zeroconf.nix { })
       # the following are distributed with calibre, but we use upstream instead
       odfpy
     ] ++ lib.optional (unrarSupport) unrardll
