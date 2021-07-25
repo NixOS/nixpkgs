f293ad9eecdfca4dc9b536381cfae8af3bf174fb
Ben Siraphob <bensiraphob@gmail.com>
2021-07-25 14:09:43 +0700

    craftos2-lua

--- pkgs/misc/emulators/craftos-pc/default.nix
+++ pkgs/misc/emulators/craftos-pc/default.nix
@@ -1,6 +1,15 @@
 { lib, stdenv, fetchFromGitHub, poco, openssl, SDL2, SDL2_mixer, ncurses, libpng
 , libharu, ApplicationServices, pngpp, libX11, Cocoa, xlibsWrapper }:
 
+let
+  craftos2-lua = fetchFromGitHub {
+    owner = "MCJack123";
+    repo = "craftos2-lua";
+    rev = "v2.6";
+    sha256 = "sha256-82PAxwt50zGLul/HJ9Z1KUFd83F7hPVO8J70tdzYHy4=";
+  };
+in
+
 stdenv.mkDerivation rec {
   pname = "craftos-pc";
   version = "2.6";
@@ -9,14 +18,15 @@ stdenv.mkDerivation rec {
     owner = "MCJack123";
     repo = "craftos2";
     rev = "v${version}";
-    sha256 = "sha256-ml3RkZb4YGNGyJJkHW3wLRp1Rzpeb0tEJ0jS1Rmmzns=";
-    fetchSubmodules = true;
+    sha256 = "sha256-x3SBZwpgcTUOCJBck+dkPmN94T3xoRPIo3c5EYIZ8iQ=";
   };
 
   buildInputs = [ poco openssl SDL2 SDL2_mixer ncurses libpng libharu pngpp libX11 xlibsWrapper ]
      ++ lib.optionals stdenv.isDarwin [ ApplicationServices Cocoa ];
 
   preBuild = ''
+    cp -R ${craftos2-lua}/* ./craftos2-lua/
+    chmod -R u+w ./craftos2-lua
     make -C craftos2-lua ${if stdenv.isLinux then "linux" else "macosx"}
   '';
 
