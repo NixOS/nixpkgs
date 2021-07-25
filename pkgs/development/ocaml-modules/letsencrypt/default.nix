4cf4f3c454ed5f6c54afdfdf0cad23f8badabd0d
sterni <sternenseemann@systemli.org>
2021-07-25 15:45:06 +0200

    Revert "ocamlPackages.letsencrypt: 0.2.5 -> 0.3.0"

--- pkgs/development/ocaml-modules/letsencrypt/default.nix
+++ pkgs/development/ocaml-modules/letsencrypt/default.nix
@@ -6,6 +6,11 @@
 , uri
 , rresult
 , base64
+, cmdliner
+, cohttp
+, cohttp-lwt
+, cohttp-lwt-unix
+, zarith
 , logs
 , fmt
 , lwt
@@ -15,25 +20,38 @@
 , x509
 , yojson
 , ounit
+, dns
+, dns-tsig
 , ptime
+, bos
+, fpath
+, randomconv
 , domain-name
 }:
 
 buildDunePackage rec {
   pname = "letsencrypt";
-  version = "0.3.0";
+  version = "0.2.5";
 
   src = fetchurl {
     url = "https://github.com/mmaker/ocaml-letsencrypt/releases/download/v${version}/letsencrypt-v${version}.tbz";
-    sha256 = "8772b7e6dbda0559a03a7b23b75c1431d42ae09a154eefd64b4c7e23b8d92deb";
+    sha256 = "6e3bbb5f593823d49e83e698c06cf9ed48818695ec8318507b311ae74731e607";
   };
 
   minimumOCamlVersion = "4.08";
   useDune2 = true;
 
   buildInputs = [
+    cmdliner
+    cohttp
+    cohttp-lwt-unix
+    zarith
     fmt
+    mirage-crypto-rng
     ptime
+    bos
+    fpath
+    randomconv
     domain-name
   ];
 
@@ -47,8 +65,11 @@ buildDunePackage rec {
     asn1-combinators
     x509
     uri
+    dns
+    dns-tsig
     rresult
     astring
+    cohttp-lwt
   ];
 
   doCheck = true;
