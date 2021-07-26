87f2965a8bf4ee78fc6bc018a2f445b01547b77c
Angus Trau <me@angus.ws>
2021-07-26 10:53:29 +1000

    python3Packages.luddite: init at 1.0.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25518,6 +25518,8 @@ in
 
   lscolors = callPackage ../applications/misc/lscolors { };
 
+  luddite = with python3Packages; toPythonApplication luddite;
+
   lumail = callPackage ../applications/networking/mailreaders/lumail {
     lua = lua5_1;
   };
