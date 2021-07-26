9c765d4534e8b4a58c61f8fd0175c295a2bb86e4
ajs124 <git@ajs124.de>
2021-07-26 03:33:27 +0200

    varnishPackages.*: fix and update dynamic module

--- pkgs/servers/varnish/packages.nix
+++ pkgs/servers/varnish/packages.nix
@@ -1,12 +1,19 @@
-{ callPackage, varnish60, varnish65 }:
-
-{
+{ callPackage, varnish60, varnish65, fetchFromGitHub }: {
   varnish60Packages = {
     varnish = varnish60;
     digest  = callPackage ./digest.nix   { varnish = varnish60; };
-    dynamic = callPackage ./dynamic.nix  { varnish = varnish60; };
+    dynamic = callPackage ./dynamic.nix  {
+      varnish = varnish60;
+      version = "0.4";
+      sha256 = "1n94slrm6vn3hpymfkla03gw9603jajclg84bjhwb8kxsk3rxpmk";
+    };
   };
   varnish65Packages = {
     varnish = varnish65;
+    dynamic = callPackage ./dynamic.nix  {
+      varnish = varnish65;
+      version = "2.3.1";
+      sha256 = "060vkba7jwcvx5704hh6ds0g0kfzpkdrg8548frvkrkz2s5j9y88";
+    };
   };
 }
