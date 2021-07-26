87274431a5ba8c389c25779b33df4a467783e61a
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-07-26 08:32:08 +0200

    linux-testing: 5.13-rc6 -> 5.14-rc3

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,20 +3,22 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.13-rc6";
-  extraMeta.branch = "5.12";
+  version = "5.14-rc3";
 
   # modDirVersion needs to be x.y.z, will always add .0
-  modDirVersion = if (modDirVersionArg == null) then builtins.replaceStrings ["-"] [".0-"] version else modDirVersionArg;
+  modDirVersion = if (modDirVersionArg == null) then builtins.replaceStrings [ "-" ] [ ".0-" ] version else modDirVersionArg;
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "sha256-PunFd6tOsmrsPItp2QX4TEVxHnvvi1BMSwWio/DTlMU=";
+    sha256 = "sha256-pdu3cMS1+delgQD8V3OFYy0FGTMrRfAWmwIGl9ntsc4=";
   };
 
   kernelTests = args.kernelTests or [ nixosTests.kernel-generic.linux_testing ];
 
   # Should the testing kernels ever be built on Hydra?
-  extraMeta.hydraPlatforms = [];
+  extraMeta = {
+    hydraPlatforms = [ ];
+    branch = versions.majorMinor version;
+  };
 
-} // (args.argsOverride or {}))
+} // (args.argsOverride or { }))
