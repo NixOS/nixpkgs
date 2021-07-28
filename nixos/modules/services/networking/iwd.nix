fdca375160a7b6c281107f000ca4604eb257a911
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-07-28 22:15:51 +0300

    iwd: Make predictable link naming policy disabling configurable

--- nixos/modules/services/networking/iwd.nix
+++ nixos/modules/services/networking/iwd.nix
@@ -10,6 +10,14 @@ in {
   options.networking.wireless.iwd = {
     enable = mkEnableOption "iwd";
 
+    # https://github.com/NixOS/nixpkgs/issues/110898
+    enableLinkPolicy = mkOption {
+      default = true;
+      description = "Whether to enable iwd's link policy to disable predictable interface naming scheme";
+      example = true;
+      type = types.bool;
+    };
+
     settings = mkOption {
       type = ini.type;
       default = { };
@@ -30,7 +38,7 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf cfg.enable ({
     assertions = [{
       assertion = !config.networking.wireless.enable;
       message = ''
@@ -47,16 +55,16 @@ in {
 
     systemd.packages = [ pkgs.iwd ];
 
-    systemd.network.links."80-iwd" = {
-      matchConfig.Type = "wlan";
-      linkConfig.NamePolicy = "keep kernel";
-    };
-
     systemd.services.iwd = {
       wantedBy = [ "multi-user.target" ];
       restartTriggers = [ configFile ];
     };
-  };
+  } // optionalAttrs cfg.enableLinkPolicy {
+    systemd.network.links."80-iwd" = {
+      matchConfig.Type = "wlan";
+      linkConfig.NamePolicy = "keep kernel";
+    };
+  });
 
   meta.maintainers = with lib.maintainers; [ mic92 dtzWill ];
 }
