17ba2a8f6f9fc7805761896dac2b135eba4f4399
dan4ik <6057430gu@gmail.com>
2021-07-28 12:36:21 +0700

    corerenamer: init at 4.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3914,6 +3914,8 @@ in
 
   corehunt = libsForQt5.callPackage ../applications/misc/corehunt { };
 
+  corerenamer = libsForQt5.callPackage ../applications/misc/corerenamer { };
+
   certstrap = callPackage ../tools/security/certstrap { };
 
   cfssl = callPackage ../tools/security/cfssl { };
