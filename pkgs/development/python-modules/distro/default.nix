e4c0b56f797c08443238b86699a1fd7fb1292377
Fabian Affolter <mail@fabian-affolter.ch>
2021-08-01 12:47:15 +0200

    python3Packages.distro: enable tests

--- pkgs/development/python-modules/distro/default.nix
+++ pkgs/development/python-modules/distro/default.nix
@@ -1,4 +1,8 @@
-{ lib, fetchPypi, buildPythonPackage }:
+{ lib
+, fetchPypi
+, buildPythonPackage
+, pytestCheckHook
+}:
 
 buildPythonPackage rec {
   pname = "distro";
@@ -9,15 +13,34 @@ buildPythonPackage rec {
     sha256 = "09441261dd3c8b2gv15vhw1cryzg60lmgpkk07v6hpwwkyhfbxc3";
   };
 
-  # tests are very targeted at individual linux distributions
-  doCheck = false;
+  checkInputs = [
+    pytestCheckHook
+  ];
+
+  disabledTests = [
+    # Some distributions can't be tested
+    "test_debian8_release"
+    "test_linuxmint17_lsb_release"
+    "test_mageia5_release"
+    "test_mandriva2011_release"
+    "test_manjaro1512_lsb_release"
+    "test_manjaro1512_lsb_release"
+    "test_manjaro1512_release"
+    "test_sles12_release"
+    "test_trailingblanks_lsb_release"
+    "test_trailingblanks_lsb_release"
+    "test_ubuntu14nomodules_lsb_release"
+    "test_ubuntu14nomodules_lsb_release"
+    "test_ubuntu14normal_lsb_release"
+    "test_ubuntu14normal_lsb_release"
+  ];
 
   pythonImportsCheck = [ "distro" ];
 
   meta = with lib; {
+    description = "Python module for getting information about the OS distribution it runs on";
     homepage = "https://github.com/nir0s/distro";
-    description = "Linux Distribution - a Linux OS platform information API.";
     license = licenses.asl20;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ fab ];
   };
 }
