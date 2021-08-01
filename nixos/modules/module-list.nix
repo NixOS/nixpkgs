94f775024eec2cdf676ad1017a58660619fb3dd0
Jonas Heinrich <onny@project-insanity.org>
2021-08-01 16:06:53 +0200

    Opensnitch: Add module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -896,6 +896,7 @@
   ./services/security/nginx-sso.nix
   ./services/security/oauth2_proxy.nix
   ./services/security/oauth2_proxy_nginx.nix
+  ./services/security/opensnitch.nix
   ./services/security/privacyidea.nix
   ./services/security/physlock.nix
   ./services/security/shibboleth-sp.nix
