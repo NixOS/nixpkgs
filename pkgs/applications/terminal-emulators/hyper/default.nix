0554b39c49d894cbc3c2b36591f349da588639ca
Fabian George Dröge <fabian.g.droege@gmail.com>
2021-08-02 10:41:04 +0200

    (hyper): 3.0.2 -> 3.1.1 and fix for desktop files

--- pkgs/applications/terminal-emulators/hyper/default.nix
+++ pkgs/applications/terminal-emulators/hyper/default.nix
@@ -1,7 +1,8 @@
-{ stdenv, lib, fetchurl, dpkg, atk, glib, pango, gdk-pixbuf, gnome2, gtk3, cairo
-, freetype, fontconfig, dbus, libXi, libXcursor, libXdamage, libXrandr
-, libXcomposite, libXext, libXfixes, libXrender, libX11, libXtst, libXScrnSaver
-, libxcb, nss, nspr, alsa-lib, cups, expat, udev, libpulseaudio, at-spi2-atk }:
+{ stdenv, lib, fetchurl, makeDesktopItem, copyDesktopItems, dpkg, atk, glib, pango
+, gdk-pixbuf, gnome2, gtk3, cairo, freetype, fontconfig, dbus, libXi, libXcursor
+, libXdamage, libXrandr, libXcomposite, libXext, libXfixes, libXrender, libX11, libXtst
+, libXScrnSaver, libxcb, nss, nspr, alsa-lib, cups, expat, udev, libpulseaudio
+, at-spi2-atk }:
 
 let
   libPath = lib.makeLibraryPath [
@@ -10,27 +11,47 @@ let
     libXrender libX11 libXtst libXScrnSaver nss nspr alsa-lib cups expat udev libpulseaudio
     at-spi2-atk
   ];
+  name = "hyper";
+  desktopItem = makeDesktopItem {
+    type = "Application";
+    name = name;
+    desktopName = "Hyper";
+    genericName = "Hyper Terminal";
+    exec = name;
+    icon = "hyper";
+    categories = "System;TerminalEmulator;";
+  };
+
 in
 stdenv.mkDerivation rec {
-  version = "3.0.2";
-  pname = "hyper";
+  pname = name;
+  version = "3.1.1";
+
   src = fetchurl {
     url = "https://github.com/zeit/hyper/releases/download/${version}/hyper_${version}_amd64.deb";
     sha256 = "0fv4wv5f8nc739bna83qxmgrvvbyq4w9ch764q2f12wjygrz336p";
   };
+  nativeBuildInputs = [ copyDesktopItems ];
+
   buildInputs = [ dpkg ];
+
   unpackPhase = ''
     mkdir pkg
     dpkg-deb -x $src pkg
     sourceRoot=pkg
   '';
+
   installPhase = ''
     mkdir -p "$out/bin"
     mv opt "$out/"
     ln -s "$out/opt/Hyper/hyper" "$out/bin/hyper"
     patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" --set-rpath "${libPath}:$out/opt/Hyper:\$ORIGIN" "$out/opt/Hyper/hyper"
     mv usr/* "$out/"
+    copyDesktopItems ${desktopItem}/share/applications/* $out/share/applications/
   '';
+
+  desktopItems = [ desktopItem ];
+
   dontPatchELF = true;
   meta = with lib; {
     description = "A terminal built on web technologies";
