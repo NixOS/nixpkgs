3411c04449bf872d44234c6b452d09157c4e6f69
Manuel Bärenz <programming@manuelbaerenz.de>
2021-08-02 18:22:21 +0200

    elixir: 1.12.1 -> 1.12.2

--- pkgs/development/interpreters/elixir/1.12.nix
+++ pkgs/development/interpreters/elixir/1.12.nix
@@ -3,7 +3,7 @@
 # How to obtain `sha256`:
 # nix-prefetch-url --unpack https://github.com/elixir-lang/elixir/archive/v${version}.tar.gz
 mkDerivation {
-  version = "1.12.1";
-  sha256 = "sha256-gRgGXb4btMriQwT/pRIYOJt+NM7rtYBd+A3SKfowC7k=";
+  version = "1.12.2";
+  sha256 = "1rwmwnqxhjcdx9niva9ardx90p1qi4axxh72nw9k15hhlh2jy29x";
   minimumOTPVersion = "22";
 }
