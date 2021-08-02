102d57d5c524e56b352d50049e3d9caabd48933b
Silvan Mosberger <contact@infinisil.com>
2021-08-02 21:13:39 +0200

    lib/modules: Default to emptyValue of types if no definitions
    This patch makes it so that if an option has no definitions, but its
    type specifies an `emptyValue`, that is used instead of throwing an error.
    
    This means that for e.g. `attrsOf`, there's no need to specify a default
    of `default = {}` anymore. Other types that have such a default are
    `nullOr` (default `null`), `submodule` (default `{}`) and `listOf`
    (default `[]`).

--- lib/modules.nix
+++ lib/modules.nix
@@ -646,6 +646,8 @@ rec {
         if all (def: type.check def.value) defsFinal then type.merge loc defsFinal
         else let allInvalid = filter (def: ! type.check def.value) defsFinal;
         in throw "A definition for option `${showOption loc}' is not of type `${type.description}'. Definition values:${showDefs allInvalid}"
+      else if type.emptyValue ? value then
+        type.emptyValue.value
       else
         # (nixos-option detects this specific error message and gives it special
         # handling.  If changed here, please change it there too.)
