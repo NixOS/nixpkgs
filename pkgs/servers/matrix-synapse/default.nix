11cb4d06801ae3fb59d5345288abbab2c2cd95a2
Sumner Evans <me@sumnerevans.com>
2021-08-03 09:44:06 -0600

    matrix-synapse: fix homeserver script
    (cherry picked from commit 4444860f074e4530c25217a528748786f9334308)

--- pkgs/servers/matrix-synapse/default.nix
+++ pkgs/servers/matrix-synapse/default.nix
@@ -19,11 +19,6 @@ buildPythonApplication rec {
     sha256 = "sha256-5RCeKTAtuFERQSoz4WinGz36tMuKtijnupPR/X02hCU=";
   };
 
-  patches = [
-    # adds an entry point for the service
-    ./homeserver-script.patch
-  ];
-
   buildInputs = [ openssl ];
 
   propagatedBuildInputs = [
@@ -68,6 +63,19 @@ buildPythonApplication rec {
 
   checkPhase = ''
     PYTHONPATH=".:$PYTHONPATH" ${python3.interpreter} -m twisted.trial tests
+  '';
+
+  postFixup = ''
+    mkdir -p $out/bin
+
+    # Make a little wrapper for running Synapse with its dependencies
+    echo "#!/bin/sh
+      exec python -m synapse.app.homeserver \"\$@\"
+    " > $out/bin/homeserver
+    chmod +x $out/bin/homeserver
+    wrapProgram $out/bin/homeserver \
+      --set PATH ${python3}/bin \
+      --set PYTHONPATH $PYTHONPATH
   '';
 
   passthru.tests = { inherit (nixosTests) matrix-synapse; };
