bcd9520cb67eafaa24d5230832bed132912a7ef3
Benjamin Smith <polynomial@users.noreply.github.com>
2021-08-04 13:42:53 -0700

    Update default.nix

--- pkgs/servers/apache-kafka/default.nix
+++ pkgs/servers/apache-kafka/default.nix
@@ -5,24 +5,6 @@ let
   jre8 = jdk8_headless;
   jre11 = jdk11_headless;
   versionMap = {
-    "2.4" = {
-      kafkaVersion = "2.4.1";
-      scalaVersion = "2.12";
-      sha256 = "0ahsprmpjz026mhbr79187wfdrxcg352iipyfqfrx68q878wnxr1";
-      jre = jre8;
-    };
-    "2.5" = {
-      kafkaVersion = "2.5.1";
-      scalaVersion = "2.12";
-      sha256 = "1wn4iszrm2rvsfyyr515zx79k5m86davjkcwcwpxcgc4k3q0z7lv";
-      jre = jre8;
-    };
-    "2.6" = {
-      kafkaVersion = "2.6.1";
-      scalaVersion = "2.13";
-      sha256 = "1a2kd4r6f8z7qf886nnq9f350sblzzdi230j2hll7x156888573y";
-      jre = jre11;
-    };
     "2.7" = {
       kafkaVersion = "2.7.1";
       scalaVersion = "2.13";
