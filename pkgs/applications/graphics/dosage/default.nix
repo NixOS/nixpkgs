ddcb5f6b8a4fd01919780abe09f4cf41f6a9720e
toonn <toonn@toonn.io>
2021-08-04 22:58:39 +0200

    dosage: Re-add test dependencies

--- pkgs/applications/graphics/dosage/default.nix
+++ pkgs/applications/graphics/dosage/default.nix
@@ -9,7 +9,9 @@ python3Packages.buildPythonApplication rec {
     sha256 = "0vmxgn9wd3j80hp4gr5iq06jrl4gryz5zgfdd2ah30d12sfcfig0";
   };
 
-  checkInputs = with python3Packages; [ pytestCheckHook ];
+  checkInputs = with python3Packages; [
+    pytestCheckHook pytest-xdist responses
+  ];
 
   propagatedBuildInputs = with python3Packages; [
     colorama imagesize lxml requests setuptools
