064121ec5a2bd78acc0e9bb06ab7fe3858f2e3bb
Adomas Jatužis <adomixaszvers@gmail.com>
2021-08-04 13:47:30 +0300

    rambox: 0.7.7 -> 0.7.8
    (cherry picked from commit ff783132f0d9934fde046ed7067d2508e274df18)

--- pkgs/applications/networking/instant-messengers/rambox/default.nix
+++ pkgs/applications/networking/instant-messengers/rambox/default.nix
@@ -4,16 +4,16 @@ let
   mkRambox = opts: callPackage (import ./rambox.nix opts) { };
 in mkRambox rec {
   pname = "rambox";
-  version = "0.7.7";
+  version = "0.7.8";
 
   src = {
     x86_64-linux = fetchurl {
       url = "https://github.com/ramboxapp/community-edition/releases/download/${version}/Rambox-${version}-linux-x86_64.AppImage";
-      sha256 = "0f82hq0dzcjicdz6lkzj8889y100yqciqrwh8wjjy9pxkhjcdini";
+      sha256 = "1y3c9xh8594ay95rj9vaqxxzibwpc38n7ixxi2wnsrdbrqrwlc63";
     };
     i686-linux = fetchurl {
       url = "https://github.com/ramboxapp/community-edition/releases/download/${version}/Rambox-${version}-linux-i386.AppImage";
-      sha256 = "1nhgqjha10jvyf9nsghvlkibg7byj8qz140639ygag9qlpd52rfs";
+      sha256 = "07sv384nd2i701fkjgsrlib8jfsa01bvj60gnqdwlnpphlknga3h";
     };
   }.${stdenv.system} or (throw "Unsupported system: ${stdenv.system}");
 
@@ -24,8 +24,5 @@ in mkRambox rec {
     maintainers = with maintainers; [ ];
     platforms = ["i686-linux" "x86_64-linux"];
     hydraPlatforms = [];
-    knownVulnerabilities = [
-      "Electron 7.2.4 is EOL and contains at least the following vulnerabilities: CVE-2020-6458, CVE-2020-6460 and more (https://www.electronjs.org/releases/stable?version=7). Consider using an alternative such as `ferdi'."
-    ];
   };
 }
