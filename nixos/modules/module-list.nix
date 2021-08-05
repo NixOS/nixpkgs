99d73983c05bd6a8ac3e5cd97b088174c92a4fad
Cole Helbling <cole.helbling@determinate.systems>
2021-08-05 12:33:13 -0700

    NixOS: support external bootloader backends

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1199,6 +1199,7 @@
   ./system/boot/loader/grub/grub.nix
   ./system/boot/loader/grub/ipxe.nix
   ./system/boot/loader/grub/memtest.nix
+  ./system/boot/loader/external/external.nix
   ./system/boot/loader/init-script/init-script.nix
   ./system/boot/loader/loader.nix
   ./system/boot/loader/raspberrypi/raspberrypi.nix
