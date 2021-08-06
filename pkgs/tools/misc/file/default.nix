04d50d249fb23ca0cbda2720caed666518cdfb1f
marius851000 <marius851000@gmail.com>
2021-08-06 12:45:31 +0200

    file: fix detection of mimetype of xz files (#132809)

--- pkgs/tools/misc/file/default.nix
+++ pkgs/tools/misc/file/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, file, zlib, libgnurx }:
+{ lib, stdenv, fetchurl, file, zlib, libgnurx, fetchpatch }:
 
 stdenv.mkDerivation rec {
   pname = "file";
@@ -15,6 +15,14 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = lib.optional (stdenv.hostPlatform != stdenv.buildPlatform) file;
   buildInputs = [ zlib ]
               ++ lib.optional stdenv.hostPlatform.isWindows libgnurx;
+
+  patches = [
+    # Fix the mime type detection of xz file. Is merged in master.
+    (fetchpatch {
+      url = "https://github.com/file/file/commit/9b0459afab309a82aa4e46f73a4e50dd641f3d39.patch";
+      sha256 = "sha256-6vjyIn5gVbgmhUlfXJKFRVltm8YKATKmh0/X6+2lLnM=";
+    })
+  ];
 
   doCheck = true;
 
