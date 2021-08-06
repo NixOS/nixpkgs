a160dfbf354934bf756fca2cb7e7ff005c27f17b
ajs124 <git@ajs124.de>
2021-08-06 17:59:18 +0200

    mariadb: 10.5.11 -> 10.6.3
    new minor release https://mariadb.com/kb/en/changes-improvements-in-mariadb-106/

--- pkgs/servers/sql/mariadb/default.nix
+++ pkgs/servers/sql/mariadb/default.nix
@@ -22,14 +22,14 @@ mariadb = server // {
 };
 
 common = rec { # attributes common to both builds
-  version = "10.5.11";
+  version = "10.6.3";
 
   src = fetchurl {
     urls = [
       "https://downloads.mariadb.org/f/mariadb-${version}/source/mariadb-${version}.tar.gz"
       "https://downloads.mariadb.com/MariaDB/mariadb-${version}/source/mariadb-${version}.tar.gz"
     ];
-    sha256 = "0yn4bhqciy6jyig31rmkjc588l03k4bj3194yf9y6373bxh5643n";
+    sha256 = "1nqq1g6h2gvsraqziv2qq42v7y6fzbfw357mh3d1zv7md9h2bhav";
     name   = "mariadb-${version}.tar.gz";
   };
 
