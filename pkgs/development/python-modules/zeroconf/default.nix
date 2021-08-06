1d537947e73e3e155241a023e065142c81d0fa56
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-08-06 22:49:03 +0200

    python3Packages.zeroconf: 0.33.2 -> 0.33.4

--- pkgs/development/python-modules/zeroconf/default.nix
+++ pkgs/development/python-modules/zeroconf/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "zeroconf";
-  version = "0.33.2";
+  version = "0.33.4";
   format = "setuptools";
   disabled = pythonOlder "3.6";
 
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "jstasiak";
     repo = "python-zeroconf";
     rev = version;
-    sha256 = "sha256-LtLLPffwznND7zciSdXJGFKHxk0m2AnLMna0vDt9jl0=";
+    sha256 = "sha256-ld8Mo465fJTVNSv6YvKcsPafiIij4PEmeycWB3M3ewU=";
   };
 
   propagatedBuildInputs = [
