adfd9f5cc94d3e4744216373426696767baf6ef9
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-08-07 22:49:29 +0200

    kdsoap: format

--- pkgs/development/libraries/kdsoap/default.nix
+++ pkgs/development/libraries/kdsoap/default.nix
@@ -1,4 +1,6 @@
-{ mkDerivation, lib, fetchurl
+{ mkDerivation
+, lib
+, fetchurl
 , cmake
 , qtbase
 }:
@@ -6,7 +8,27 @@
 mkDerivation rec {
   pname = "kdsoap";
   version = "2.0.0";
-  meta = {
+
+  src = fetchurl {
+    url = "https://github.com/KDAB/KDSoap/releases/download/kdsoap-${version}/kdsoap-${version}.tar.gz";
+    sha256 = "sha256-0YljEE+m99ArBEYxzdvnjxj3DgbGB69oDHrOBNbPBO4=";
+  };
+
+  outputs = [ "out" "dev" ];
+
+  nativeBuildInputs = [ cmake ];
+
+  buildInputs = [ qtbase ];
+
+  postInstall = ''
+    moveToOutput bin/kdwsdl2cpp "$dev"
+    sed -i "$out/lib/cmake/KDSoap/KDSoapTargets.cmake" \
+        -e "/^  INTERFACE_INCLUDE_DIRECTORIES/ c   INTERFACE_INCLUDE_DIRECTORIES \"$dev/include\""
+    sed -i "$out/lib/cmake/KDSoap/KDSoapTargets-release.cmake" \
+        -e "s@$out/bin@$dev/bin@"
+  '';
+
+  meta = with lib; {
     description = "A Qt-based client-side and server-side SOAP component";
     longDescription = ''
       KD Soap is a Qt-based client-side and server-side SOAP component.
@@ -15,21 +37,7 @@ mkDerivation rec {
       provides the means to create web services without the need for any further
       component such as a dedicated web server.
     '';
-    license = with lib.licenses; [ gpl2 gpl3 lgpl21 ];
-    maintainers = [ lib.maintainers.ttuegel ];
+    license = with licenses; [ gpl2 gpl3 lgpl21 ];
+    maintainers = [ maintainers.ttuegel ];
   };
-  src = fetchurl {
-    url = "https://github.com/KDAB/KDSoap/releases/download/kdsoap-${version}/kdsoap-${version}.tar.gz";
-    sha256 = "sha256-0YljEE+m99ArBEYxzdvnjxj3DgbGB69oDHrOBNbPBO4=";
-  };
-  outputs = [ "out" "dev" ];
-  nativeBuildInputs = [ cmake ];
-  buildInputs = [ qtbase ];
-  postInstall = ''
-    moveToOutput bin/kdwsdl2cpp "$dev"
-    sed -i "$out/lib/cmake/KDSoap/KDSoapTargets.cmake" \
-        -e "/^  INTERFACE_INCLUDE_DIRECTORIES/ c   INTERFACE_INCLUDE_DIRECTORIES \"$dev/include\""
-    sed -i "$out/lib/cmake/KDSoap/KDSoapTargets-release.cmake" \
-        -e "s@$out/bin@$dev/bin@"
-  '';
 }
