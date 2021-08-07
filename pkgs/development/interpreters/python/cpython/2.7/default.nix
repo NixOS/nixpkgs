37962fc5fb3c7cd39b18448b28ecc8d259062db5
Guillaume Girol <symphorien+git@xlumurb.eu>
2021-08-07 12:00:00 +0000

    python27: fix static build

--- pkgs/development/interpreters/python/cpython/2.7/default.nix
+++ pkgs/development/interpreters/python/cpython/2.7/default.nix
@@ -26,8 +26,8 @@
 , sourceVersion
 , sha256
 , passthruFun
-, static ? false
-, stripBytecode ? true
+, static ? stdenv.hostPlatform.isStatic
+, stripBytecode ? reproducibleBuild
 , rebuildBytecode ? true
 , reproducibleBuild ? false
 , enableOptimizations ? false
@@ -187,8 +187,9 @@ let
 
   configureFlags = optionals enableOptimizations [
     "--enable-optimizations"
-  ] ++ [
+  ] ++ optionals (!static) [
     "--enable-shared"
+  ] ++ [
     "--with-threads"
     "--enable-unicode=ucs${toString ucsEncoding}"
   ] ++ optionals (stdenv.hostPlatform.isCygwin || stdenv.hostPlatform.isAarch64) [
