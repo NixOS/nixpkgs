2d4dd130c2c46c14af96a7aeb8185a776a25e74c
Guillaume Girol <symphorien+git@xlumurb.eu>
2021-08-07 12:00:00 +0000

    lzo: fix static build

--- pkgs/development/libraries/lzo/default.nix
+++ pkgs/development/libraries/lzo/default.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
     sha256 = "0wm04519pd3g8hqpjqhfr72q8qmbiwqaxcs3cndny9h86aa95y60";
   };
 
-  configureFlags = [ "--enable-shared" ];
+  configureFlags = lib.optional (!stdenv.hostPlatform.isStatic) "--enable-shared" ;
 
   enableParallelBuilding = true;
 
