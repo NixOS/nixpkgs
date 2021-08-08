2e8e8f2c92a76535fa0d44611a20b97ee6b75927
Alexandre Iooss <erdnaxe@crans.org>
2021-08-08 15:29:33 +0200

    nixos/nitter: test with CAP_NET_BIND_SERVICE

--- nixos/tests/nitter.nix
+++ nixos/tests/nitter.nix
@@ -6,11 +6,13 @@ import ./make-test-python.nix ({ pkgs, ... }:
 
   nodes.machine = {
     services.nitter.enable = true;
+    # Test CAP_NET_BIND_SERVICE
+    services.nitter.server.port = 80;
   };
 
   testScript = ''
     machine.wait_for_unit("nitter.service")
-    machine.wait_for_open_port("8080")
-    machine.succeed("curl --fail http://localhost:8080/")
+    machine.wait_for_open_port("80")
+    machine.succeed("curl --fail http://localhost:80/")
   '';
 })
