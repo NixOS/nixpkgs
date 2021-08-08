e923e42a84f66ff9e86384581a29b262fe1a7b37
Artturi <Artturin@artturin.com>
2021-08-08 03:29:15 +0300

    Update default.nix

--- pkgs/development/compilers/inform6/default.nix
+++ pkgs/development/compilers/inform6/default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
     owner = "DavidGriffith";
     repo = "inform6unix";
     rev = version;
-    sha256 = "1mfgr9vzm7nwxnh13hfpvpyc9dbzi9a4sfi1b4wypqc2rb5q7n6r";
+    sha256 = "sha256-VjlTu7IIY12qLlqfuV0DyzC3z6rFdR8iXo966TrOpGs=";
   };
 
   buildInputs = [ perl ];
