9cf7759f910074dcebfb4542ad6adc669445d2b0
Lein Matsumaru <applePrincess@applePrincess.io>
2021-08-09 06:03:42 +0000

    sherlock: init at 0.14.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26553,6 +26553,8 @@ with pkgs;
 
   gjay = callPackage ../applications/audio/gjay { };
 
+  sherlock = callPackage ../tools/security/sherlock { };
+
   rhythmbox = callPackage ../applications/audio/rhythmbox { };
 
   puddletag = libsForQt5.callPackage ../applications/audio/puddletag { };
