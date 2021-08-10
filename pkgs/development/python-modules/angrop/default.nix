36e0fc76ae6e0d68b6f40920a5daf910517aa361
Fabian Affolter <mail@fabian-affolter.ch>
2021-08-10 22:26:32 +0200

    python3Packages.angrop: 9.0.9355 -> 9.0.9438

--- pkgs/development/python-modules/angrop/default.nix
+++ pkgs/development/python-modules/angrop/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "angrop";
-  version = "9.0.9355";
+  version = "9.0.9438";
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "angr";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1fs31yd8hhkrrx4gsq13fb5fqi86688pazpmsnsga7hznv60s2ks";
+    sha256 = "sha256-+6fWdl5IcQTygFQbqPSfWvpqdooFN5yeBgPIblOyZEU=";
   };
 
   propagatedBuildInputs = [
