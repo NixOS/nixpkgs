e45dd4bffe1ddd8320c1b0ef59c0d80277348f8d
Fabian Affolter <mail@fabian-affolter.ch>
2021-08-10 11:29:33 +0200

    python3Packages.class-registry: enable tests

--- pkgs/development/python-modules/class-registry/default.nix
+++ pkgs/development/python-modules/class-registry/default.nix
@@ -1,16 +1,14 @@
-{
-  buildPythonPackage,
-  fetchFromGitHub,
-  lib,
-  nose,
-  six,
-  typing ? null,
-  isPy27,
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, nose
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "class-registry";
   version = "3.0.5";
+  disabled = pythonOlder "3.5";
 
   src = fetchFromGitHub {
     owner = "todofixthis";
@@ -19,16 +17,16 @@ buildPythonPackage rec {
     sha256 = "0gpvq4a6qrr2iki6b4vxarjr1jrsw560m2qzm5bb43ix8c8b7y3q";
   };
 
-  propagatedBuildInputs = [ six ] ++ lib.optional isPy27 typing;
-  checkInputs = [ nose ];
+  checkInputs = [
+    nose
+  ];
 
-  # Tests currently failing.
-  doCheck = false;
+  pythonImportsCheck = [ "class_registry" ];
 
-  meta = {
-    description = "Factory+Registry pattern for Python classes.";
+  meta = with lib; {
+    description = "Factory and registry pattern for Python classes";
     homepage = "https://class-registry.readthedocs.io/en/latest/";
-    license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ kevincox ];
+    license = licenses.mit;
+    maintainers = with maintainers; [ kevincox ];
   };
 }
