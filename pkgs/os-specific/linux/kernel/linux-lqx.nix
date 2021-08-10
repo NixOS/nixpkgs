71b9b198b156a5d6222d250bea61849db4949a91
Atemu <atemu.main@gmail.com>
2021-08-10 20:36:58 +0200

    linux_lqx: 5.12.19 -> 5.13.9
    (cherry picked from commit 2aaf41173d28c86c7037680b2ffbb91aec274d92)
    
    Closes https://github.com/NixOS/nixpkgs/pull/133525

--- pkgs/os-specific/linux/kernel/linux-lqx.nix
+++ pkgs/os-specific/linux/kernel/linux-lqx.nix
@@ -1,8 +1,8 @@
 { lib, fetchFromGitHub, buildLinux, linux_zen, ... } @ args:
 
 let
-  version = "5.12.19";
-  suffix = "lqx2";
+  version = "5.13.9";
+  suffix = "lqx1";
 in
 
 buildLinux (args // {
@@ -14,11 +14,11 @@ buildLinux (args // {
     owner = "zen-kernel";
     repo = "zen-kernel";
     rev = "v${version}-${suffix}";
-    sha256 = "sha256-r2DvKLlm1a1VuJwC81tRuRwCd6H21T3MsBAC3b9TUbs=";
+    sha256 = "sha256-aAnwPw1qoGhUdWN/uaQa+5bi0DFZB/wDfNow7FgMMFE=";
   };
 
   extraMeta = {
-    branch = "5.12/master";
+    branch = "5.13/master";
     maintainers = with lib.maintainers; [ atemu ];
     description = linux_zen.meta.description + " (Same as linux_zen but less aggressive release schedule)";
   };
