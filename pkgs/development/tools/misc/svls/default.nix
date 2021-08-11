2c94a5efded2cefb181aa0c0746d014d1a7931fb
Tom Repetti <trepetti@cs.columbia.edu>
2021-08-10 20:06:15 -0400

    svls: 0.1.27 -> 0.1.28

--- pkgs/development/tools/misc/svls/default.nix
+++ pkgs/development/tools/misc/svls/default.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "svls";
-  version = "0.1.27";
+  version = "0.1.28";
 
   src = fetchFromGitHub {
     owner = "dalance";
     repo = "svls";
     rev = "v${version}";
-    sha256 = "sha256-+/4D0pRZs1Gy6DJnsDZA8wWi1FKhr7gRS0oq1TyWpuE=";
+    sha256 = "0k3njqdhcbgj6l1rwyk6crylpwzgg70fsh5yfgp4p2zvynw4hrx2";
   };
 
-  cargoSha256 = "sha256-xkRlUXlkXQwvzIuhExf+tSSBi+8BZv58btvln05UI+k=";
+  cargoSha256 = "0aj5d0ca70ggiarjkrj4z34zbdpwlhbsssqf3l9ypblqhfvq2wvl";
 
   meta = with lib; {
     description = "SystemVerilog language server";
