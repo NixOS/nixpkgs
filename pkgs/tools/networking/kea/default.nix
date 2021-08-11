4dfa596f93f76c528e02856216b8f218cac4796c
Poscat <poscat@mail.poscat.moe>
2021-08-12 00:06:44 +0800

    kea: 1.9.9 -> 1.9.10

--- pkgs/tools/networking/kea/default.nix
+++ pkgs/tools/networking/kea/default.nix
@@ -14,11 +14,11 @@
 
 stdenv.mkDerivation rec {
   pname = "kea";
-  version = "1.9.9";
+  version = "1.9.10";
 
   src = fetchurl {
     url = "https://ftp.isc.org/isc/${pname}/${version}/${pname}-${version}.tar.gz";
-    sha256 = "sha256-iVSWBR1+SkXlkwMii2PXpcxFSXYigz4lfNnMZBvS2kM=";
+    sha256 = "08pr2qav87jmrf074v8zbqyjkl51wf6r9hhgbkzhdav9d4f9kny3";
   };
 
   patches = [ ./dont-create-var.patch ];
