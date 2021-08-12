7cd5d178fc6fd403907bfe00f502337c162d7607
Felix Buehler <account@buehler.rocks>
2021-08-12 21:47:47 +0200

    tools: replace name with pname&version

--- pkgs/tools/archivers/cabextract/default.nix
+++ pkgs/tools/archivers/cabextract/default.nix
@@ -1,10 +1,11 @@
 { lib, stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "cabextract-1.9.1";
+  pname = "cabextract";
+  version = "1.9.1";
 
   src = fetchurl {
-    url = "https://www.cabextract.org.uk/${name}.tar.gz";
+    url = "https://www.cabextract.org.uk/cabextract-${version}.tar.gz";
     sha256 = "19qwhl2r8ip95q4vxzxg2kp4p125hjmc9762sns1dwwf7ikm7hmg";
   };
 
