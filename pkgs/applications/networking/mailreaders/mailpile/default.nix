435465e89eb65db4ec279c6613c7ce19acc820b8
ostylk <ostylk@googlemail.com>
2021-08-13 14:23:44 +0200

    mailpile: add longDescription attribute

--- pkgs/applications/networking/mailreaders/mailpile/default.nix
+++ pkgs/applications/networking/mailreaders/mailpile/default.nix
@@ -58,5 +58,30 @@ python2Packages.buildPythonApplication rec {
     knownVulnerabilities = [
       "Numerous and uncounted, upstream has requested we not package it. See more: https://github.com/NixOS/nixpkgs/pull/23058#issuecomment-283515104"
     ];
+    longDescription = ''
+      Mailpile (https://www.mailpile.is/) is a modern, fast web-mail client with user-friendly
+      encryption and privacy features. The development of Mailpile is funded by a large community
+      of backers and all code related to the project is and will be released under an OSI approved
+      Free Software license.
+
+      Mailpile places great emphasis on providing a clean, elegant user interface and pleasant
+      user experience. In particular, Mailpile aims to make it easy and convenient to receive and
+      send PGP encrypted or signed e-mail.
+
+      Mailpile's primary user interface is web-based, but it also has a basic command-line interface
+      and an API for developers. Using web technology for the interface allows Mailpile to function
+      both as a local desktop application (accessed by visiting localhost in the browser) or a remote
+      web-mail on a personal server or VPS.
+
+      The core of Mailpile is a fast search engine, custom written to deal with large volumes of
+      e-mail on consumer hardware. The search engine allows e-mail to be organized using tags (similar
+      to GMail's labels) and the application can be configured to automatically tag incoming mail
+      either based on static rules or bayesian classifiers.
+
+      Note: We are currently working towards a 1.0 release candidate. Until it is ready, Mailpile is
+      really only suitable for developers who can help us find and fix the last few bugs. Our beta
+      releases are obsolete and should not be used, in part for security reasons! For more details
+      follow @MailpileTeam on Twitter or read our blog.
+    '';
   };
 }
