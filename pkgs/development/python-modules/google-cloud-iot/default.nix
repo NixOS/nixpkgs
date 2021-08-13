2368e780ddc89e83c7b90bf41ff169dbe43057ea
R. RyanTM <ryantm-bot@ryantm.com>
2021-08-13 21:32:06 +0000

    python38Packages.google-cloud-iot: 2.1.0 -> 2.2.1

--- pkgs/development/python-modules/google-cloud-iot/default.nix
+++ pkgs/development/python-modules/google-cloud-iot/default.nix
@@ -12,11 +12,11 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-iot";
-  version = "2.1.0";
+  version = "2.2.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f4d7c55636e8cb57a4dde41d933cd8663b1369ab3542b287957959ee59828559";
+    sha256 = "bccceae1f7c0efcefbcd1b5d67e56915d107534059843761831ba4e75e7884c5";
   };
 
   propagatedBuildInputs = [ grpc_google_iam_v1 google-api-core libcst proto-plus ];
