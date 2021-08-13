14b2250e419c0dac9fcbf04dd6895b66c84a3735
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-08-13 17:11:25 +0200

    fdk-aac: cleanup, format

--- pkgs/development/libraries/fdk-aac/default.nix
+++ pkgs/development/libraries/fdk-aac/default.nix
@@ -1,8 +1,9 @@
-{ lib, stdenv, fetchurl
+{ lib
+, stdenv
+, fetchurl
 , exampleSupport ? false # Example encoding program
 }:
 
-with lib;
 stdenv.mkDerivation rec {
   pname = "fdk-aac";
   version = "2.0.2";
@@ -12,14 +13,13 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-yehjDPnUM/POrXSQahUg0iI/ibzT+pJUhhAXRAuOsi8=";
   };
 
-  configureFlags = [ ]
-    ++ optional exampleSupport "--enable-example";
+  configureFlags = lib.optional exampleSupport "--enable-example";
 
-  meta = {
+  meta = with lib; {
     description = "A high-quality implementation of the AAC codec from Android";
-    homepage    = "https://sourceforge.net/projects/opencore-amr/";
-    license     = licenses.asl20;
+    homepage = "https://sourceforge.net/projects/opencore-amr/";
+    license = licenses.asl20;
     maintainers = with maintainers; [ codyopel ];
-    platforms   = platforms.all;
+    platforms = platforms.all;
   };
 }
