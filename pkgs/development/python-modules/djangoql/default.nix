e55b1b84c093671f7eb670960e42d2d4c66651e9
R. RyanTM <ryantm-bot@ryantm.com>
2021-08-13 20:09:50 +0000

    python38Packages.djangoql: 0.15.0 -> 0.15.2

--- pkgs/development/python-modules/djangoql/default.nix
+++ pkgs/development/python-modules/djangoql/default.nix
@@ -3,11 +3,11 @@
 
 buildPythonPackage rec {
   pname = "djangoql";
-  version = "0.15.0";
+  version = "0.15.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ddd876b640faf941aface6e8554dc7e79a79ec00fee12da8a20f9f74301f7943";
+    sha256 = "bd9d3accc5e84c439d22211676cf74ab746e557da9bc57afb27fee0ccd00e563";
   };
 
   propagatedBuildInputs = [ ply ];
