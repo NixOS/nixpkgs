90cc1994d0f5d9bb44f33c2c094571ccd70684d1
veleth <llathasa@outlook.com>
2021-08-13 09:36:00 +0300

    profetch: refined code
    + Added missing `"` to the `rev` variable
    + We use 4-space indentation in bash scripts inside nix code
    + License in GPL3-only
    + One-line args because not too much of them

--- pkgs/tools/misc/profetch/default.nix
+++ pkgs/tools/misc/profetch/default.nix
@@ -1,5 +1,4 @@
-{ stdenv, lib
-, fetchFromGitHub, gprolog }:
+{ stdenv, lib, fetchFromGitHub, gprolog }:
 
 stdenv.mkDerivation rec {
   pname = "profetch";
@@ -8,28 +7,28 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "RustemB";
     repo = pname;
-    rev = "v${version};
+    rev = "v${version}";
     sha256 = "1clh3l50wz6mlrw9kx0wh2bbhnz6bsksyh4ngz7givv4y3g9m702";
   };
 
   buildInputs = [ gprolog ];
 
   buildPhase = ''
-    gplc profetch.pl --no-top-level            \
-                     --no-debugger --no-fd-lib \
-                     --no-fd-lib-warn --min-size -o profetch
-    runHook postBuild
+      gplc profetch.pl --no-top-level --no-debugger    \
+                       --no-fd-lib    --no-fd-lib-warn \
+                       --min-size -o profetch
+      runHook postBuild
   '';
 
   installPhase = ''
-    runHook preInstall
-    install -Dm755 -t $out/bin profetch
+      runHook preInstall
+      install -Dm755 -t $out/bin profetch
   '';
 
   meta = with lib; {
     description = "System Information Fetcher Written in GNU/Prolog";
     homepage = "https://github.com/RustemB/profetch";
-    license = licenses.gpl3;
+    license = licenses.gpl3Only;
     platforms = platforms.all;
     maintainers = [ maintainers.vel ];
   };
