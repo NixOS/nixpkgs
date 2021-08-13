0f845cc4ecbe04d424215d79370d523cf02c8398
R. RyanTM <ryantm-bot@ryantm.com>
2021-08-13 09:42:28 +0000

    python38Packages.azure-mgmt-loganalytics: 10.0.0 -> 11.0.0

--- pkgs/development/python-modules/azure-mgmt-loganalytics/default.nix
+++ pkgs/development/python-modules/azure-mgmt-loganalytics/default.nix
@@ -12,12 +12,12 @@
 
 buildPythonPackage rec {
   pname = "azure-mgmt-loganalytics";
-  version = "10.0.0";
+  version = "11.0.0";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "29330984d0f084dff26cea239d7b733c1a26844da85d33bf3bb53b515ce0bc23";
+    sha256 = "41671fc6e95180fb6147cb40567410c34b85fb69bb0a9b3e09feae1ff370ee9d";
   };
 
   propagatedBuildInputs = [
