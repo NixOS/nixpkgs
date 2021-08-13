b46f10ec54dc3ac8f6c6a4c1bf638447b6a16fc4
R. RyanTM <ryantm-bot@ryantm.com>
2021-08-13 15:06:38 +0000

    python38Packages.azure-synapse-spark: 0.5.0 -> 0.6.0

--- pkgs/development/python-modules/azure-synapse-spark/default.nix
+++ pkgs/development/python-modules/azure-synapse-spark/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "azure-synapse-spark";
-  version = "0.5.0";
+  version = "0.6.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "2b037024dc7c034f47aac551cc918f78590a1e1ae30cd2370c8a14da15994970";
+    sha256 = "ac7564a61ba314e0a9406c0f73c3cede04091a131a0c58971bcba0c158b7455d";
     extension = "zip";
   };
 
