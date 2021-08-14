711b572d22a1f2dd285a86feb85781334b572948
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-08-14 12:33:23 +0200

    catgirl: fix warnings related to flock not being defined
    There were a few complaints about this, so ppl seem to care about C
    compiler's warnings.

--- pkgs/applications/networking/irc/catgirl/default.nix
+++ pkgs/applications/networking/irc/catgirl/default.nix
@@ -1,4 +1,4 @@
-{ ctags, fetchurl, lib, libressl, man, ncurses, pkg-config, stdenv }:
+{ ctags, fetchurl, fetchpatch, lib, libressl, man, ncurses, pkg-config, stdenv }:
 
 stdenv.mkDerivation rec {
   pname = "catgirl";
@@ -8,6 +8,13 @@ stdenv.mkDerivation rec {
     url = "https://git.causal.agency/catgirl/snapshot/${pname}-${version}.tar.gz";
     sha256 = "182l7yryqm1ffxqgz3i4lcnzwzpbpm2qvadddmj0xc8dh8513s0w";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://git.causal.agency/catgirl/patch/?id=3f3585d0f32e66ad5c8c6c713f315e14810230eb";
+      sha256 = "1vrgimvf007bxz8blxm3vjc7g3xwxplwxyrblnsryq54cqaw0xv3";
+    })
+  ];
 
   nativeBuildInputs = [ ctags pkg-config ];
   buildInputs = [ libressl man ncurses ];
