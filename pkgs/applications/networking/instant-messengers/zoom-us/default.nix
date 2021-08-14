c486a1117ba33d6afea5536458ee2a00fc0f2b08
R. RyanTM <ryantm-bot@ryantm.com>
2021-08-14 10:17:03 +0000

    zoom-us: 5.7.28991.0726 -> 5.7.29123.0808
    Old versions have been blacklisted serverside.
    
    (cherry picked from commit cc7a5e69e396a7860398f3b7b9b33f423b731577)

--- pkgs/applications/networking/instant-messengers/zoom-us/default.nix
+++ pkgs/applications/networking/instant-messengers/zoom-us/default.nix
@@ -29,11 +29,11 @@
 assert pulseaudioSupport -> libpulseaudio != null;
 
 let
-  version = "5.7.28991.0726";
+  version = "5.7.29123.0808";
   srcs = {
     x86_64-linux = fetchurl {
       url = "https://zoom.us/client/${version}/zoom_x86_64.pkg.tar.xz";
-      sha256 = "w1oeMKADG5+7EV1OXyuEbotrwcVywob82KOXKoRUifA=";
+      sha256 = "WAeE/2hUaQbWwDg/iqlKSZVoH3ruVHvh+9SEWdPwCIc=";
     };
   };
 
