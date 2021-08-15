6d9e4ada12c28757ba0f80b73cef55476925e8a5
Ilan Joselevich <personal@ilanjoselevich.com>
2021-08-15 11:06:02 +0300

    portfolio: 0.54.1 -> 0.54.2

--- pkgs/applications/office/portfolio/default.nix
+++ pkgs/applications/office/portfolio/default.nix
@@ -24,11 +24,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "PortfolioPerformance";
-  version = "0.54.1";
+  version = "0.54.2";
 
   src = fetchurl {
     url = "https://github.com/buchen/portfolio/releases/download/${version}/PortfolioPerformance-${version}-linux.gtk.x86_64.tar.gz";
-    sha256 = "16sv938sdbs01byqwngrfqmzb81zfhvk72ar53l68cg8qjvzs5ml";
+    sha256 = "sha256-fKUKVeR0q8oylpwF4d3jnkON4vbQ80Fc9WYWStb67ek=";
   };
 
   nativeBuildInputs = [
