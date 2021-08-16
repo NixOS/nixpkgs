cb899db2ee79390d5816a39f13ae97117dfd6ab4
Felix Buehler <account@buehler.rocks>
2021-08-16 21:45:06 +0200

    emacsPackages.perl-completion: deprecate phases

--- pkgs/applications/editors/emacs/elisp-packages/perl-completion/default.nix
+++ pkgs/applications/editors/emacs/elisp-packages/perl-completion/default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation {
     sha256 = "0x6qsgs4hm87k0z9q3g4p6508kc3y123j5jayll3jf3lcl2vm6ks";
   };
 
-  phases = [ "installPhase"];
+  dontUnpack = true;
 
   installPhase = ''
     install -d $out/share/emacs/site-lisp
