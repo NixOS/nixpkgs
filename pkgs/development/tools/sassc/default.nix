f7e477b0ff2c8418260ad19f1897537af94ffcfe
R. RyanTM <ryantm-bot@ryantm.com>
2021-08-17 22:16:26 +0000

    sassc: 3.6.1 -> 3.6.2

--- pkgs/development/tools/sassc/default.nix
+++ pkgs/development/tools/sassc/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "sassc";
-  version = "3.6.1";
+  version = "3.6.2";
 
   src = fetchFromGitHub {
     owner = "sass";
     repo = pname;
     rev = version;
-    sha256 = "1sxm54mkhs9m4vnl7vn11y17mhzamc403hv3966j1c7p2rbzg5pv";
+    sha256 = "sha256-jcs3+orRqKt9C3c2FTdeaj4H2rBP74lW3HF8CHSm7lQ=";
   };
 
   patchPhase = ''
