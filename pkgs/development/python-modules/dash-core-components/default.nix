6f2803d374559e7f1fc43305d866fb1e3b189840
Jonathan Ringer <jonringer117@gmail.com>
2021-08-17 11:44:14 -0700

    python3Packages.dash_core_components: 1.16.0 -> 1.17.1

--- pkgs/development/python-modules/dash-core-components/default.nix
+++ pkgs/development/python-modules/dash-core-components/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "dash_core_components";
-  version = "1.16.0";
+  version = "1.17.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e8cdfaf3580577670bb2d1c3168efa06f5a7b439fbe5527cfaefa3e32394542f";
+    sha256 = "7e503f5eddb63034dd20c23d218cc0d8a81ec88e9a15b7cbc60e266b0e2b60a9";
   };
 
   # No tests in archive
