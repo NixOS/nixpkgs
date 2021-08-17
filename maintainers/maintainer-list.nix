c96a5945a06d05809684b1db4a9738d6235bbf74
Arcade Wise <l3gacy.b3ta@disroot.org>
2021-08-17 14:13:07 -0400

    maintainers: add l3gacyb3ta

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -5917,6 +5917,12 @@
     githubId = 72546287;
     name = "L3af";
   };
+  l3gacyb3ta = {
+    email = "arcade@omg.lol";
+    github = "l3gacyb3ta";
+    githubId = 58434499;
+    name = "Arcade Wise";
+  };
   lach = {
     email = "iam@lach.pw";
     github = "CertainLach";
