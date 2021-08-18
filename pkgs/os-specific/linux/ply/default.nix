cebb66308668299b89f897e38ea469d31f271e31
Joseph Crail <jbcrail@gmail.com>
2021-08-17 22:42:23 -0700

    Clarify specific GPL license

--- pkgs/os-specific/linux/ply/default.nix
+++ pkgs/os-specific/linux/ply/default.nix
@@ -34,7 +34,7 @@ in stdenv.mkDerivation {
   meta = with lib; {
     description = "Dynamic tracing in Linux";
     homepage = "https://wkz.github.io/ply/";
-    license = [ licenses.gpl2 ];
+    license = [ licenses.gpl2Only ];
     maintainers = with maintainers; [ mic92 mbbx6spp ];
     broken = lib.versionOlder kernel.version "4.0";
   };
