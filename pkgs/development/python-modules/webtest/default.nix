062d47f93ea0e00816b59691cd2394d529fde6fb
Fabian Affolter <mail@fabian-affolter.ch>
2021-08-18 16:44:47 +0200

    python3Packages.webtest: add pythonImportsCheck

--- pkgs/development/python-modules/webtest/default.nix
+++ pkgs/development/python-modules/webtest/default.nix
@@ -28,17 +28,30 @@ buildPythonPackage rec {
     substituteInPlace setup.py --replace "nose<1.3.0" "nose"
   '';
 
-  propagatedBuildInputs = [ webob six beautifulsoup4 waitress ];
+  propagatedBuildInputs = [
+    webob
+    six
+    beautifulsoup4
+    waitress
+  ];
 
-  checkInputs = [ nose mock PasteDeploy wsgiproxy2 pyquery ];
+  checkInputs = [
+    nose
+    mock
+    PasteDeploy
+    wsgiproxy2
+    pyquery
+  ];
 
   # Some of the tests use localhost networking.
   __darwinAllowLocalNetworking = true;
+
+  pythonImportsCheck = [ "webtest" ];
 
   meta = with lib; {
     description = "Helper to test WSGI applications";
     homepage = "https://webtest.readthedocs.org/en/latest/";
     license = licenses.mit;
+    maintainers = with maintainers; [ ];
   };
-
 }
