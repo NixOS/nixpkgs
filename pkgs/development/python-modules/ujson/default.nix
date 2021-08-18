893473a3180ca3bf6d22d7fca3922fcf43e059e7
Fabian Affolter <mail@fabian-affolter.ch>
2021-08-18 13:51:32 +0200

    python3Packages.ujson: enable tests

--- pkgs/development/python-modules/ujson/default.nix
+++ pkgs/development/python-modules/ujson/default.nix
@@ -1,27 +1,36 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, isPy3k
 , isPyPy
+, pytestCheckHook
+, pythonOlder
 , setuptools-scm
 }:
 
 buildPythonPackage rec {
   pname = "ujson";
   version = "4.1.0";
-  disabled = isPyPy || (!isPy3k);
+  disabled = isPyPy || pythonOlder "3.5";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "sha256-IrY+xECfDS8sTJ1aozGZfgJHC3oVoyM/PMMvL5uS1Yw=";
   };
 
-  nativeBuildInputs = [ setuptools-scm ];
+  nativeBuildInputs = [
+    setuptools-scm
+  ];
+
+  checkInputs = [
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [ "ujson" ];
 
   meta = with lib; {
-    homepage = "https://pypi.python.org/pypi/ujson";
     description = "Ultra fast JSON encoder and decoder for Python";
+    homepage = "https://pypi.python.org/pypi/ujson";
     license = licenses.bsd3;
+    maintainers = with maintainers; [ ];
   };
-
 }
