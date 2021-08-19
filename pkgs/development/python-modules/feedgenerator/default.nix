7796697ca77de36554e7f7100d0b627b5b364988
Fabian Affolter <mail@fabian-affolter.ch>
2021-08-19 10:38:54 +0200

    python3Packages.feedgenerator: 1.9.1 -> 1.9.2

--- pkgs/development/python-modules/feedgenerator/default.nix
+++ pkgs/development/python-modules/feedgenerator/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "feedgenerator";
-  version = "1.9.1";
+  version = "1.9.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0m6fjnrx3sd0bm6pnbhxxx5ywlwqh8bx0lka386kj28mg3fmm2m2";
+    sha256 = "sha256-sG1pQej9aiyecXkQeehsvno3iMciRKzAbwWTtJzaN5s=";
   };
 
   buildInputs = [ glibcLocales ];
