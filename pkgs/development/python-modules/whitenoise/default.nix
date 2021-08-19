c0fe8642d40f691e7d3d8640c710b55ac652d97e
R. RyanTM <ryantm-bot@ryantm.com>
2021-08-19 20:51:26 +0000

    python38Packages.whitenoise: 5.2.0 -> 5.3.0

--- pkgs/development/python-modules/whitenoise/default.nix
+++ pkgs/development/python-modules/whitenoise/default.nix
@@ -2,12 +2,12 @@
 
 buildPythonPackage rec {
   pname = "whitenoise";
-  version = "5.2.0";
+  version = "5.3.0";
   disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "05ce0be39ad85740a78750c86a93485c40f08ad8c62a6006de0233765996e5c7";
+    sha256 = "d234b871b52271ae7ed6d9da47ffe857c76568f11dd30e28e18c5869dbd11e12";
   };
 
   # No tests
