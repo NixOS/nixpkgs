ff704e85bca351d456876b7dfa4c801a3cc78c3c
Bart Bakker <bart@thesoftwarecraft.com>
2021-08-20 11:59:50 +0200

    xcolor: unstable -> 0.5.0
    Also create a desktop item so xcolor shows up in application launchers.

--- pkgs/tools/graphics/xcolor/default.nix
+++ pkgs/tools/graphics/xcolor/default.nix
@@ -1,29 +1,46 @@
-{ lib, rustPlatform, fetchFromGitHub, fetchpatch, pkg-config, libX11, libXcursor, libxcb, python3 }:
+{ lib, rustPlatform, fetchFromGitHub, fetchpatch, pkg-config, libX11, libXcursor
+, libxcb, python3, makeDesktopItem }:
 
-rustPlatform.buildRustPackage rec {
+let
+  desktopItem = makeDesktopItem {
+    name = "XColor";
+    exec = "xcolor -s";
+    desktopName = "XColor";
+    comment =
+      "Select colors visible anywhere on the screen to get their RGB representation";
+    icon = "xcolor";
+    categories = "Graphics;";
+  };
+
+in rustPlatform.buildRustPackage rec {
   pname = "xcolor";
-  version = "unstable-2021-02-02";
+  version = "0.5.0";
 
   src = fetchFromGitHub {
     owner = "Soft";
     repo = pname;
-    rev = "0e99e67cd37000bf563aa1e89faae796ec25f163";
-    sha256 = "sha256-rHqK05dN5lrvDNbRCWGghI7KJwWzNCuRDEThEeMzmio=";
+    rev = version;
+    sha256 = "0i04jwvjasrypnsfwdnvsvcygp8ckf1a5sxvjxaivy73cdvy34vk";
   };
 
-  cargoPatches = [
-    # Update Cargo.lock, lexical_core doesn't build on Rust 1.52.1
-    (fetchpatch {
-      url = "https://github.com/Soft/xcolor/commit/324d80a18a39a11f2f7141b226f492e2a862d2ce.patch";
-      sha256 = "sha256-5VzXitpl/gMef40UQBh1EoHezXPyB08aflqp0mSMAVI=";
-    })
-  ];
-
-  cargoSha256 = "sha256-yD4pX+dCJvbDecsdB8tNt1VsEcyAJxNrB5WsZUhPGII=";
+  cargoSha256 = "1r2s4iy5ls0svw5ww51m37jhrbvnj690ig6n9c60hzw1hl4krk30";
 
   nativeBuildInputs = [ pkg-config python3 ];
 
   buildInputs = [ libX11 libXcursor libxcb ];
+
+  postInstall = ''
+    mkdir -p $out/share/applications
+    cp "${desktopItem}"/share/applications/* $out/share/applications/
+
+    install -D -m644 -- man/xcolor.1 $out/share/man/man1/xcolor.1
+    install -D -m644 -- extra/icons/xcolor-16.png $out/share/icons/hicolor/16x16/apps/xcolor.png
+    install -D -m644 -- extra/icons/xcolor-24.png $out/share/icons/hicolor/24x24/apps/xcolor.png
+    install -D -m644 -- extra/icons/xcolor-32.png $out/share/icons/hicolor/32x32/apps/xcolor.png
+    install -D -m644 -- extra/icons/xcolor-48.png $out/share/icons/hicolor/48x48/apps/xcolor.png
+    install -D -m644 -- extra/icons/xcolor-256.png $out/share/icons/hicolor/256x256/apps/xcolor.png
+    install -D -m644 -- extra/icons/xcolor-512.png $out/share/icons/hicolor/512x512/apps/xcolor.png
+  '';
 
   meta = with lib; {
     description = "Lightweight color picker for X11";
