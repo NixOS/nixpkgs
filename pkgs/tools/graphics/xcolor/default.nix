803790c1a665e9594a64738c4232fcea7557e48d
Bart Bakker <bart@thesoftwarecraft.com>
2021-08-20 13:42:21 +0200

    xcolor: cleanup install phase
    Use available utility functions for installing man pages and desktop items. Also
    cleanup redundant icon copy lines.

--- pkgs/tools/graphics/xcolor/default.nix
+++ pkgs/tools/graphics/xcolor/default.nix
@@ -1,18 +1,7 @@
-{ lib, rustPlatform, fetchFromGitHub, fetchpatch, pkg-config, libX11, libXcursor
-, libxcb, python3, makeDesktopItem }:
+{ lib, rustPlatform, fetchFromGitHub, pkg-config, libX11, libXcursor
+, libxcb, python3, installShellFiles, makeDesktopItem, copyDesktopItems }:
 
-let
-  desktopItem = makeDesktopItem {
-    name = "XColor";
-    exec = "xcolor -s";
-    desktopName = "XColor";
-    comment =
-      "Select colors visible anywhere on the screen to get their RGB representation";
-    icon = "xcolor";
-    categories = "Graphics;";
-  };
-
-in rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage rec {
   pname = "xcolor";
   version = "0.5.0";
 
@@ -25,21 +14,28 @@ in rustPlatform.buildRustPackage rec {
 
   cargoSha256 = "1r2s4iy5ls0svw5ww51m37jhrbvnj690ig6n9c60hzw1hl4krk30";
 
-  nativeBuildInputs = [ pkg-config python3 ];
+  nativeBuildInputs = [ pkg-config python3 installShellFiles copyDesktopItems ];
 
   buildInputs = [ libX11 libXcursor libxcb ];
 
+  desktopItems = [
+    (makeDesktopItem {
+      name = "XColor";
+      exec = "xcolor -s";
+      desktopName = "XColor";
+      comment = "Select colors visible anywhere on the screen to get their RGB representation";
+      icon = "xcolor";
+      categories = "Graphics;";
+    })
+  ];
+
   postInstall = ''
     mkdir -p $out/share/applications
-    cp "${desktopItem}"/share/applications/* $out/share/applications/
 
-    install -D -m644 -- man/xcolor.1 $out/share/man/man1/xcolor.1
-    install -D -m644 -- extra/icons/xcolor-16.png $out/share/icons/hicolor/16x16/apps/xcolor.png
-    install -D -m644 -- extra/icons/xcolor-24.png $out/share/icons/hicolor/24x24/apps/xcolor.png
-    install -D -m644 -- extra/icons/xcolor-32.png $out/share/icons/hicolor/32x32/apps/xcolor.png
-    install -D -m644 -- extra/icons/xcolor-48.png $out/share/icons/hicolor/48x48/apps/xcolor.png
-    install -D -m644 -- extra/icons/xcolor-256.png $out/share/icons/hicolor/256x256/apps/xcolor.png
-    install -D -m644 -- extra/icons/xcolor-512.png $out/share/icons/hicolor/512x512/apps/xcolor.png
+    installManPage man/xcolor.1
+    for x in 16 24 32 48 256 512; do
+        install -D -m644 extra/icons/xcolor-''${x}.png $out/share/icons/hicolor/''${x}x''${x}/apps/xcolor.png
+    done
   '';
 
   meta = with lib; {
