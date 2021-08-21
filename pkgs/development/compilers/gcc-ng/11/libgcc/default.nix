e411c828a5af7e3c504f0388b6522ee7c1f5a6a5
John Ericson <John.Ericson@Obsidian.Systems>
2021-08-21 22:18:15 +0000

    gccPackagesNg.libgcc: Fix Musl build

--- pkgs/development/compilers/gcc-ng/11/libgcc/default.nix
+++ pkgs/development/compilers/gcc-ng/11/libgcc/default.nix
@@ -116,6 +116,37 @@ stdenv.mkDerivation rec {
     export CC_FOR_TARGET=${stdenv.cc}/bin/$CC_FOR_TARGET
     export CPP_FOR_TARGET=${stdenv.cc}/bin/$CPP_FOR_TARGET
     export LD_FOR_TARGET=${stdenv.cc.bintools}/bin/$LD_FOR_TARGET
+  ''
+  # Fix build for Musl (even though musl isn't built yet!)
+  #
+  # Here's the situation: GCC's default serach patch usual includes an
+  # "include-fixed" directory where various hheaders that are non-comformant
+  # are overridden with comformant ones. This was probably created for all the
+  # old proprietary Unices GNU software had to build on, but also used for
+  # glibc for at least limits.h. Musl, however, doesn't hit this problem, and
+  # so GCC doesn't bother putting the include-next files on the CLI even though
+  # it does ship with them.
+  #
+  # While libgcc is built prior to libc, it's configure script does check to
+  # see if the C pre-processor works, and does so via trying include---you
+  # guessed it---limits.h. This works with glibc because it picks up the
+  # include-fixed version.  This doesn't work with Musl however because it
+  # doesn't pick up any limits.h, and thus thinks the C preprocessor is broken.
+  #
+  # BTW, we are just discovering this now this now because in a regular
+  # monolith build of GCC, the build-time include-fix is *unconditionally*
+  # added as an -isystem in CFLAGS_FOR_TARGET, even though it onluy
+  # conditionally becomes an installed default. People doing regular MUSL
+  # builds therefore haven't hit this issue.
+  #
+  # Our work-around is to manually add the -isystem ourselves, but only at
+  # configure time, by sneaking it in NIX_CLAGS_COMPILE behind the build
+  # system's back. While the include-fixed limits.h is probably safe to use, we
+  # rather use Musl's own for consistently with how GCC will be used
+  # "regularly".
+  + lib.optionalString stdenv.hostPlatform.isMusl ''
+    NIX_CFLAGS_COMPILE_OLD=$NIX_CFLAGS_COMPILE
+    NIX_CFLAGS_COMPILE+=' -isystem ${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${version}/include-fixed'
   '';
 
   topLevelConfigureFlags = [
@@ -153,6 +184,12 @@ stdenv.mkDerivation rec {
     "--enable-static"
     "--disable-shared"
   ];
+
+  # Set the variable back the way it was, see corresponding code in
+  # `preConfigure`.
+  postConfigure = lib.optionalString stdenv.hostPlatform.isMusl ''
+    NIX_CFLAGS_COMPILE=$NIX_CFLAGS_COMPILE_OLD
+  '';
 
   makeFlags = [ "MULTIBUILDTOP:=../" ];
 
