21cdf18de97ff61fad28baac5897fed531cdb944
R. RyanTM <ryantm-bot@ryantm.com>
2021-08-21 22:41:26 +0000

    drone-cli: 1.3.0 -> 1.3.1

--- pkgs/development/tools/continuous-integration/drone-cli/default.nix
+++ pkgs/development/tools/continuous-integration/drone-cli/default.nix
@@ -1,11 +1,11 @@
 { lib, fetchFromGitHub, buildGoModule }:
 
 buildGoModule rec {
-  version = "1.3.0";
+  version = "1.3.1";
   pname = "drone-cli";
   revision = "v${version}";
 
-  vendorSha256 = "sha256-I+UBa6gqkPRXNV72iyJcCBLYShZxMtHFHSK77mhDv+U=";
+  vendorSha256 = "sha256-IlQ83lhRiobjvXa4FvavwLAXe7Bi7oLXRAr+1kvIHhc=";
 
   doCheck = false;
 
@@ -17,7 +17,7 @@ buildGoModule rec {
     owner = "drone";
     repo = "drone-cli";
     rev = revision;
-    sha256 = "sha256-j6drDMxvAVfQ1aCFooc9g9HhMRMlFZXGZPiuJZKBbY4=";
+    sha256 = "sha256-EUvwKQgQTX8wX9h/rMlCYuB0S/OhPo4Ynlz5QQOWJlU=";
   };
 
   meta = with lib; {
