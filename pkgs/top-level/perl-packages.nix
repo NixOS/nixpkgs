ddf68a8faf4d2c6797c8b307d9f9de270d98dd7d
Vincenzo Mantova <1962985+xworld21@users.noreply.github.com>
2021-08-21 10:28:44 +0100

    Revert "perlPackages.PerlTidy: use shortenPerlShebang on darwin"

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -17118,10 +17118,6 @@ let
       description = "Indent and reformat perl scripts";
       license = lib.licenses.gpl2Plus;
     };
-    nativeBuildInputs = lib.optional stdenv.isDarwin shortenPerlShebang;
-    postInstall = lib.optionalString stdenv.isDarwin ''
-      shortenPerlShebang $out/bin/perltidy
-    '';
   };
 
   PHPSerialization = buildPerlPackage {
