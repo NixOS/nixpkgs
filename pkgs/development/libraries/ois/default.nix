72e32060ec5883df2c549a4f1596aab14e2a529c
R. RyanTM <ryantm-bot@ryantm.com>
2021-08-22 08:21:31 +0000

    ois: 1.5 -> 1.5.1

--- pkgs/development/libraries/ois/default.nix
+++ pkgs/development/libraries/ois/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "ois";
-  version = "1.5";
+  version = "1.5.1";
 
   src = fetchFromGitHub {
     owner = "wgois";
     repo = "OIS";
     rev = "v${version}";
-    sha256 = "0g8krgq5bdx2rw7ig0xva4kqv4x815672i7z6lljp3n8847wmypa";
+    sha256 = "sha256-ir6p+Tzf8L5VOW/rsG4yelsth7INbhABO2T7pfMHcFo=";
   };
 
   nativeBuildInputs = [ cmake ];
