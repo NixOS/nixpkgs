426569a04174720817242de5e8d9157582595055
Samuel Ainsworth <skainsworth@gmail.com>
2021-08-22 20:39:04 +0000

    python3Packages.jax: init at 0.2.19

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3716,6 +3716,8 @@ in {
 
   javaproperties = callPackage ../development/python-modules/javaproperties { };
 
+  jax = callPackage ../development/python-modules/jax { };
+
   jaxlib = callPackage ../development/python-modules/jaxlib { };
 
   JayDeBeApi = callPackage ../development/python-modules/JayDeBeApi { };
