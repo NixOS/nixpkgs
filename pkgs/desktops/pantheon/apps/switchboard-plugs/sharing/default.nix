468164175f7f9d02733b569f7ca617a73b7a3d24
R. RyanTM <ryantm-bot@ryantm.com>
2021-08-22 03:26:01 +0000

    pantheon.switchboard-plug-sharing: 2.1.4 -> 2.1.5

--- pkgs/desktops/pantheon/apps/switchboard-plugs/sharing/default.nix
+++ pkgs/desktops/pantheon/apps/switchboard-plugs/sharing/default.nix
@@ -15,13 +15,13 @@
 
 stdenv.mkDerivation rec {
   pname = "switchboard-plug-sharing";
-  version = "2.1.4";
+  version = "2.1.5";
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = pname;
     rev = version;
-    sha256 = "sha256-/M60w14zfAUXspabvTUWlOPRrHvKtCYUio82b034k6s=";
+    sha256 = "sha256-J9gKCSN4fIOYkbw9of4WaodTWoIZ/SiTFn58HnDPmAI=";
   };
 
   passthru = {
