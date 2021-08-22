d74f249f7677d43756cd34ccb960205856cd4e17
Mario Rodas <marsam@users.noreply.github.com>
2021-08-22 01:04:02 -0500

    python39Packages.unidiff: fetch from PyPI

--- pkgs/development/python-modules/unidiff/default.nix
+++ pkgs/development/python-modules/unidiff/default.nix
@@ -1,20 +1,20 @@
-{ lib, buildPythonPackage, fetchFromGitHub }:
+{ lib, buildPythonPackage, fetchPypi }:
 
 buildPythonPackage rec {
   pname = "unidiff";
   version = "0.7.0";
 
-  # PyPI tarball doesn't ship tests
-  src = fetchFromGitHub {
-    owner = "matiasb";
-    repo = "python-unidiff";
-    rev = "v${version}";
-    sha256 = "1s1l327jqm0r35pn9c83pbw15k66x8klw1lf45xqp8lrdc15cqv5";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "91bb13b4969514a400679d9ae5e29a6ffad85346087677f8b5e2e036af817447";
   };
+
+  pythonImportsCheck = [ "unidiff" ];
 
   meta = with lib; {
     description = "Unified diff python parsing/metadata extraction library";
     homepage = "https://github.com/matiasb/python-unidiff";
+    changelog = "https://github.com/matiasb/python-unidiff/raw/v${version}/HISTORY";
     license = licenses.mit;
     maintainers = [ maintainers.marsam ];
   };
