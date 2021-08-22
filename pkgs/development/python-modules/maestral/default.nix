48c079758a4b4d2a50966715571f2d7c9b53c06c
Stefan Frijters <sfrijters@gmail.com>
2021-08-22 22:55:38 +0200

    python3Packages.maestral: Add tests

--- pkgs/development/python-modules/maestral/default.nix
+++ pkgs/development/python-modules/maestral/default.nix
@@ -5,6 +5,7 @@
 , python
 , click, desktop-notifier, dropbox, fasteners, keyring, keyrings-alt, packaging, pathspec, Pyro5, requests, setuptools, sdnotify, survey, watchdog
 , importlib-metadata
+, pytestCheckHook
 }:
 
 buildPythonPackage rec {
@@ -44,8 +45,24 @@ buildPythonPackage rec {
     "--prefix" "PYTHONPATH" ":" "$out/lib/${python.libPrefix}/site-packages"
   ];
 
-  # no tests
-  doCheck = false;
+  checkInputs = [
+    pytestCheckHook
+  ];
+
+  disabledTests = [
+    # We don't want to benchmark
+    "test_performance"
+    # Requires systemd
+    "test_autostart"
+    # Requires network access
+    "test_check_for_updates"
+    # Tries to look at /usr
+    "test_filestatus"
+    "test_path_exists_case_insensitive"
+    "test_cased_path_candidates"
+  ];
+
+  pythonImportsCheck = [ "maestral" ];
 
   meta = with lib; {
     description = "Open-source Dropbox client for macOS and Linux";
