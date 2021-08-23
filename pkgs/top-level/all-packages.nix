3dd3c78ea9ef2c11f7ae713fd2d7f7dd2ce2fc70
Ivar Scholten <ivar.scholten@protonmail.com>
2021-08-23 23:29:31 +0200

    redtimer: init at unstable-2021-08-01

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -893,6 +893,8 @@ with pkgs;
 
   redfang = callPackage ../tools/networking/redfang { };
 
+  redtimer = callPackage ../tools/misc/redtimer { };
+
   sx-go = callPackage ../tools/security/sx-go { };
 
   tfk8s = callPackage ../tools/misc/tfk8s { };
