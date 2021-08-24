0a0775808ce12d353891bbf501d24dcff4a19df7
R. RyanTM <ryantm-bot@ryantm.com>
2021-08-24 13:05:34 +0000

    resvg: 0.15.0 -> 0.16.0

--- pkgs/tools/graphics/resvg/default.nix
+++ pkgs/tools/graphics/resvg/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "resvg";
-  version = "0.15.0";
+  version = "0.16.0";
 
   src = fetchFromGitHub {
     owner = "RazrFalcon";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-FjL/7SC1XtQyI+vlkDbQR2848vhV4Lvx3htSN3RSohw=";
+    sha256 = "sha256-A54KTToi69l0/Nrz4K8EqFpCodbomYUI/zTP++Y4FF0=";
   };
 
-  cargoSha256 = "sha256-FfTkturHQqnTAzkEHDn/M/UiLMH1L/+Kv/zov8n8sek=";
+  cargoSha256 = "sha256-RbgLZvNZSNjYImVm8Ax5cAL3R0XqlVz5ApPYKj93GEE=";
 
   buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
 
