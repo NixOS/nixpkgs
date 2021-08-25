cf32a8e5fa94513db7cfb26525417a116686d696
Fabian Affolter <mail@fabian-affolter.ch>
2021-08-25 08:51:31 +0200

    python3Packages.angr: 9.0.9506 -> 9.0.9572

--- pkgs/development/python-modules/angr/default.nix
+++ pkgs/development/python-modules/angr/default.nix
@@ -43,14 +43,14 @@ in
 
 buildPythonPackage rec {
   pname = "angr";
-  version = "9.0.9506";
+  version = "9.0.9572";
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-2bKsLmZeFs7N4YUYxIktDoOn/H8waaOaOJOzyVumuf8=";
+    sha256 = "sha256-ZA2PKyJVXrSs2IvpjMyHGrtAPUpUZFhUzlKURLEWm5o=";
   };
 
   propagatedBuildInputs = [
