b720376c7c4da1663e155b0b2142348c8caddae9
Joachim Breitner <mail@joachim-breitner.de>
2021-08-25 20:26:02 +0200

    ocamlPackages.vlq: init at 0.2.1
    (cherry picked from commit 91a55816dda6ac8caef47bfa3ff4bcf832ff4b3b)

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1214,6 +1214,8 @@ let
 
     vg = callPackage ../development/ocaml-modules/vg { };
 
+    vlq = callPackage ../development/ocaml-modules/vlq { };
+
     visitors = callPackage ../development/ocaml-modules/visitors { };
 
     wasm = callPackage ../development/ocaml-modules/wasm { };
