d01533ceabdd84674a90691969bf4294e9d97ce1
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-08-25 21:05:28 +0200

    pkgsCross.avr.buildPackages.gcc{10,11}: avoid log limit on hydra
    When upgrading from gcc 9 to 10, avr-gcc started to hit the hydra log
    limit, preventing the binary cache from being populated.
    
    This commit tries to workaround this issue by passing `-s` to make for
    avr-gcc 10 and 11 which seem to exhibit this problem.
    
    Reference #135605.

--- pkgs/development/compilers/gcc/11/default.nix
+++ pkgs/development/compilers/gcc/11/default.nix
@@ -152,6 +152,7 @@ stdenv.mkDerivation ({
     else "")
       + lib.optionalString targetPlatform.isAvr ''
             makeFlagsArray+=(
+               '-s' # workaround for hitting hydra log limit
                'LIMITS_H_TEST=false'
             )
           '';
