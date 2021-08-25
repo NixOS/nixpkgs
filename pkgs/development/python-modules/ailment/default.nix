a9ac94752c7b71dec005d4a0feaae0948270a4f6
Fabian Affolter <mail@fabian-affolter.ch>
2021-08-25 08:51:20 +0200

    python3Packages.ailment: 9.0.9506 -> 9.0.9572

--- pkgs/development/python-modules/ailment/default.nix
+++ pkgs/development/python-modules/ailment/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "ailment";
-  version = "9.0.9506";
+  version = "9.0.9572";
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "angr";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-ikIO6AhoBkmz4+8BLOC55Yh6HbzHJOjlktSDMiC0I38=";
+    sha256 = "sha256-cjVKIlvGu1SCiVkegJ36GevZ9bihYF7n3P/xNqtAapw=";
   };
 
   propagatedBuildInputs = [ pyvex ];
