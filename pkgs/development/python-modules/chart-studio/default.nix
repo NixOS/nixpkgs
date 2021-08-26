e5099b5f44f1c3301048b6fb2c724d252ffcfc4f
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-08-26 21:38:06 +0200

    python3Packages.chart-studio: 4.14.3 -> 5.2.2

--- pkgs/development/python-modules/chart-studio/default.nix
+++ pkgs/development/python-modules/chart-studio/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "chart-studio";
-  version = "4.14.3";
+  version = "5.2.2";
 
   # chart-studio was split from plotly
   src = fetchFromGitHub {
     owner = "plotly";
     repo = "plotly.py";
     rev = "v${version}";
-    sha256 = "1yrrcn9032r3l8y2cc1wvpydg4rkqvgkpyv1s7znby5mrxa1rjma";
+    sha256 = "14msi6i2caca5riirfv81nskh8w3avnflyr2phm62kygk125h1v7";
   };
 
   sourceRoot = "source/packages/python/chart-studio";
