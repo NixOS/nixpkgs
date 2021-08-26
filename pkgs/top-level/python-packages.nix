d60f63ec966a52108a8a22a9aa53d0c0f31eac5a
Michael Colicchia <michael.colicchia@imio.be>
2021-08-26 21:53:55 +0200

    Python3Packages.versiontag: init at 1.2.0
    fix(typo)
    
    fix(typo)
    
    fix(typo)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9204,6 +9204,8 @@ in {
 
   versioneer = callPackage ../development/python-modules/versioneer { };
 
+  versiontag = callPackage ../development/python-modules/versiontag { };
+
   versiontools = callPackage ../development/python-modules/versiontools { };
 
   vertica-python = callPackage ../development/python-modules/vertica-python { };
