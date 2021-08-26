138321d3738a77653b9f642731980acb21175e33
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-08-26 19:09:41 +0200

    python39Packages.django_redis: format, add import check, update meta

--- pkgs/development/python-modules/django_redis/default.nix
+++ pkgs/development/python-modules/django_redis/default.nix
@@ -1,5 +1,11 @@
-{ lib, fetchPypi, buildPythonPackage,
-  mock, django, redis, msgpack }:
+{ lib
+, fetchPypi
+, buildPythonPackage
+, mock
+, django
+, redis
+, msgpack
+}:
 buildPythonPackage rec {
   pname = "django-redis";
   version = "5.0.0";
@@ -9,19 +15,21 @@ buildPythonPackage rec {
     sha256 = "048f665bbe27f8ff2edebae6aa9c534ab137f1e8fa7234147ef470df3f3aa9b8";
   };
 
-  doCheck = false;
-
-  buildInputs = [ mock ];
-
   propagatedBuildInputs = [
     django
     redis
     msgpack
   ];
 
+  # django.core.exceptions.ImproperlyConfigured: Requested setting DJANGO_REDIS_SCAN_ITERSIZE, but settings are not configured. You must either define the environment variable DJANGO_SETTINGS_MODULE or call settings.configure() before accessing settings.
+  doCheck = false;
+
+  pythonImportsCheck = [ "django_redis" ];
+
   meta = with lib; {
     description = "Full featured redis cache backend for Django";
     homepage = "https://github.com/niwibe/django-redis";
     license = licenses.bsd3;
+    maintainers = with maintainers; [ ];
   };
 }
