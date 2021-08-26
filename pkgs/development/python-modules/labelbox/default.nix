04a0156ce6aa98841b58b89f4b58ffe0b0da0c7b
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-08-26 21:38:08 +0200

    python3Packages.labelbox: 2.7.0 -> 3.1.0

--- pkgs/development/python-modules/labelbox/default.nix
+++ pkgs/development/python-modules/labelbox/default.nix
@@ -19,14 +19,14 @@
 
 buildPythonPackage rec {
   pname = "labelbox";
-  version = "2.7.0";
+  version = "3.1.0";
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "Labelbox";
     repo = "labelbox-python";
     rev = "V${version}";
-    sha256 = "0bvfxs4g9cxj9npy7qckxsgxwgfg0cdmyywdw5lrfs926iq4ydq1";
+    sha256 = "0hqjwilz1d5jidgsambr2zrp4czy53rq01p2ib6mndzsdh86nv7w";
   };
 
   propagatedBuildInputs = [
