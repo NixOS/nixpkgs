97712f7537e809a0e3f8d82d89f46ba0411e5556
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-08-26 21:38:08 +0200

    python3Packages.plotly: 4.14.3 -> 5.2.2

--- pkgs/development/python-modules/plotly/default.nix
+++ pkgs/development/python-modules/plotly/default.nix
@@ -11,11 +11,11 @@
 
 buildPythonPackage rec {
   pname = "plotly";
-  version = "4.14.3";
+  version = "5.2.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "14cxlfl79i9bh3awsf4xgwr14ywm7lnrz2a81s7gp0if77nsx2kx";
+    sha256 = "809f0674a7991daaf4f287964d617d24e9fa44463acd5a5352ebd874cfd98b07";
   };
 
   propagatedBuildInputs = [
