c54165ed6c0b3ca8c3e968cad0f52ad54fa048a0
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-08-26 19:04:59 +0200

    python39Packages.django_reversion: add import check, update meta

--- pkgs/development/python-modules/django_reversion/default.nix
+++ pkgs/development/python-modules/django_reversion/default.nix
@@ -18,10 +18,12 @@ buildPythonPackage rec {
 
   propagatedBuildInputs = [ django ];
 
+  pythonImportsCheck = [ "reversion" ];
+
   meta = with lib; {
     description = "An extension to the Django web framework that provides comprehensive version control facilities";
     homepage = "https://github.com/etianen/django-reversion";
     license = licenses.bsd3;
+    maintainers = with maintainers; [ ];
   };
-
 }
