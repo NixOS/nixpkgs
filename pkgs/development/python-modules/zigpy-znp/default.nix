f2b7e0192a4283c3f1da704a991803f04666df6e
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-08-26 21:32:48 +0200

    python3Packages.zigpy-znp: 0.5.3 -> 0.5.4

--- pkgs/development/python-modules/zigpy-znp/default.nix
+++ pkgs/development/python-modules/zigpy-znp/default.nix
@@ -18,13 +18,13 @@
 
 buildPythonPackage rec {
   pname = "zigpy-znp";
-  version = "0.5.3";
+  version = "0.5.4";
 
   src = fetchFromGitHub {
     owner = "zigpy";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-nnA/gVXBpCZFkspcO6kF3ZkEDu0vV0d9p1WNGVrN0u8=";
+    sha256 = "0jki9qmjazh0by02c0w17dyaz0nl3gzjiy064mj6pi502d175831";
   };
 
   propagatedBuildInputs = [
