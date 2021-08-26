2a5f007e94292bb3b61b2844bd18cbe767abf422
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-08-26 21:38:08 +0200

    python3Packages.plaid-python: 7.5.0 -> 8.0.0

--- pkgs/development/python-modules/plaid-python/default.nix
+++ pkgs/development/python-modules/plaid-python/default.nix
@@ -1,12 +1,12 @@
 { lib, buildPythonPackage, fetchPypi, requests, pytest }:
 
 buildPythonPackage rec {
-  version = "7.5.0";
+  version = "8.0.0";
   pname = "plaid-python";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f4781a54d18c8a70bf0b9206004574c524e63e6a07e3fa480964a489e2d759e1";
+    sha256 = "ce11a832e04889972809b257e0351e4cc70fd3b5d31de2b97cc9df732a3dcc9b";
   };
 
   checkInputs = [ pytest ];
