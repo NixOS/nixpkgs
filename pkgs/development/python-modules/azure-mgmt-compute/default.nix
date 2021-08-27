929905f8c7ce713709c6f04596a4b3e53bed1292
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-08-27 05:30:04 +0200

    python39Packages.azure-mgmt-compute: add import check

--- pkgs/development/python-modules/azure-mgmt-compute/default.nix
+++ pkgs/development/python-modules/azure-mgmt-compute/default.nix
@@ -25,6 +25,8 @@ buildPythonPackage rec {
   # has no tests
   doCheck = false;
 
+  pythonImportsCheck = [ "azure.mgmt.compute" ];
+
   meta = with lib; {
     description = "This is the Microsoft Azure Compute Management Client Library";
     homepage = "https://github.com/Azure/azure-sdk-for-python";
