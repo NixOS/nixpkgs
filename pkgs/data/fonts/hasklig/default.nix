387ae7251ea8c8bd08f44c49237a7c2776200635
Tim Steinbach <tim@nequissimus.com>
2021-08-27 08:50:01 -0400

    hasklig: 1.1 -> 1.2

--- pkgs/data/fonts/hasklig/default.nix
+++ pkgs/data/fonts/hasklig/default.nix
@@ -1,18 +1,18 @@
 { lib, fetchzip }:
 
 let
-  version = "1.1";
+  version = "1.2";
 in fetchzip {
   name = "hasklig-${version}";
 
-  url = "https://github.com/i-tu/Hasklig/releases/download/${version}/Hasklig-${version}.zip";
+  url = "https://github.com/i-tu/Hasklig/releases/download/v${version}/Hasklig-${version}.zip";
 
   postFetch = ''
     unzip $downloadedFile
-    install -m444 -Dt $out/share/fonts/opentype *.otf
+    install -m444 -Dt $out/share/fonts/opentype OTF/*.otf
   '';
 
-  sha256 = "0xxyx0nkapviqaqmf3b610nq17k20afirvc72l32pfspsbxz8ybq";
+  sha256 = "sha256-8lY1m0P1qQIbR4e7kX68jVoKZmEfPWNwK3AFLoaBEak=";
 
   meta = with lib; {
     homepage = "https://github.com/i-tu/Hasklig";
