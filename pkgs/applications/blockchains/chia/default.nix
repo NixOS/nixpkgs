a46e2c439059fbf074920dab43e7af7abccd6e21
Louis Bettens <louis@bettens.info>
2021-08-27 11:45:20 +0200

    chia: 1.2.3 -> 1.2.5

--- pkgs/applications/blockchains/chia/default.nix
+++ pkgs/applications/blockchains/chia/default.nix
@@ -7,14 +7,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "chia";
-  version = "1.2.3";
+  version = "1.2.5";
 
   src = fetchFromGitHub {
     owner = "Chia-Network";
     repo = "chia-blockchain";
     rev = version;
     fetchSubmodules = true;
-    sha256 = "sha256-nK/Zk2zgIdrRtw3+VkUXQWfI9j29XFDOR95Dvbn07eA=";
+    sha256 = "sha256-3TRjOAlZfcvDKHecagA+hXRg7HqiD6NNegi956LEXS8=";
   };
 
   patches = [
@@ -45,15 +45,18 @@ python3Packages.buildPythonApplication rec {
     clvm
     clvm-rs
     clvm-tools
+    colorama
     colorlog
     concurrent-log-handler
     cryptography
     dnspython
+    fasteners
     keyrings-cryptfile
     pyyaml
     setproctitle
     setuptools # needs pkg_resources at runtime
     sortedcontainers
+    watchdog
     websockets
   ];
 
@@ -64,6 +67,8 @@ python3Packages.buildPythonApplication rec {
   disabledTests = [
     "test_spend_through_n"
     "test_spend_zero_coin"
+    "test_default_cached_master_passphrase"
+    "test_using_legacy_keyring"
   ];
 
   postPatch = ''
@@ -72,9 +77,7 @@ python3Packages.buildPythonApplication rec {
       --replace "aiohttp==3.7.4" "aiohttp>=3.7.4" \
       --replace "sortedcontainers==2.3.0" "sortedcontainers>=2.3.0" \
       --replace "click==7.1.2" "click>=7.1.2" \
-      --replace "clvm_rs==0.1.8" "clvm_rs>=0.1.8" \
       --replace "clvm==0.9.7" "clvm>=0.9.7" \
-      --replace "bitstring==3.1.7" "bitstring>=3.1.9" \
 
     ln -sf ${cacert}/etc/ssl/certs/ca-bundle.crt mozilla-ca/cacert.pem
   '';
