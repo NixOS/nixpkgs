843eebbd8e0a951e5c2c3f1cd2e17a39f6c08701
Mihai Fufezan <fufexan@pm.me>
2021-08-27 10:33:50 +0300

    ocamlPackages.noise: init at 0.2.0

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -808,6 +808,8 @@ let
 
     nocrypto =  callPackage ../development/ocaml-modules/nocrypto { };
 
+    noise = callPackage ../development/ocaml-modules/noise { };
+
     nonstd =  callPackage ../development/ocaml-modules/nonstd { };
 
     notty = callPackage ../development/ocaml-modules/notty { };
