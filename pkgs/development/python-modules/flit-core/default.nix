9409588dd6517626420ada379b4e29550974891b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-08-27 02:35:00 +0200

    python39Packages.flit-core: break infinite recursion with setuptools-scm by splitting tests, add SuperSandro2000 as maintainer

--- pkgs/development/python-modules/flit-core/default.nix
+++ pkgs/development/python-modules/flit-core/default.nix
@@ -1,7 +1,7 @@
 { lib
 , buildPythonPackage
+, callPackage
 , flit
-, isPy3k
 , toml
 , pytestCheckHook
 , testpath
@@ -11,6 +11,11 @@ buildPythonPackage rec {
   pname = "flit-core";
   version = "3.2.0";
   format = "pyproject";
+
+  outputs = [
+    "out"
+    "testsout"
+  ];
 
   inherit (flit) src patches;
 
@@ -22,19 +27,23 @@ buildPythonPackage rec {
     toml
   ];
 
-  checkInputs = [
-    pytestCheckHook
-    testpath
-  ];
+  postInstall = ''
+    mkdir $testsout
+    cp -R ../tests $testsout/tests
+  '';
+
+  # check in passthru.tests.pytest to escape infinite recursion with setuptools-scm
+  doCheck = false;
 
   passthru.tests = {
     inherit flit;
+    pytest = callPackage ./tests.nix { };
   };
 
-  meta = {
+  meta = with lib; {
     description = "Distribution-building parts of Flit. See flit package for more information";
     homepage = "https://github.com/takluyver/flit";
-    license = lib.licenses.bsd3;
-    maintainers = [ lib.maintainers.fridh ];
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ fridh SuperSandro2000 ];
   };
 }
