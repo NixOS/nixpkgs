c617466cadc6c725b532a233d2f06b1f178d537e
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-08-30 00:18:44 +0200

    python39Packages.sphinxcontrib-tikz: replace patch by substituteInPlace to prevent apply errors

--- pkgs/development/python-modules/sphinxcontrib-tikz/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-tikz/default.nix
@@ -16,12 +16,11 @@ buildPythonPackage rec {
     sha256 = "1614a45c960b80009dd42f96689122c9c0781018a0c5ec5828f4cdc5e35b11ad";
   };
 
-  patches = [
-    (substituteAll {
-      src = ./binary-paths.patch;
-      inherit texLive pdf2svg;
-    })
-  ];
+  postPatch = ''
+    substituteInPlace sphinxcontrib/tikz.py \
+      --replace "config.latex_engine" "${texLive}/bin/pdflatex" \
+      --replace "system(['pdf2svg'" "system(['${pdf2svg}/bin/pdf2svg'"
+  '';
 
   propagatedBuildInputs = [ sphinx ];
 
