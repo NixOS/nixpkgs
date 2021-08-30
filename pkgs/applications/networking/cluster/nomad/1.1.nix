00d1b4f193401c05382c7cd67c9dd5525c043e9d
Maxine Aubrey <maxeaubrey@gmail.com>
2021-08-30 20:09:50 +0200

    nomad_1_1: 1.1.3 -> 1.1.4
    - https://github.com/hashicorp/nomad/releases/tag/v1.1.4

--- pkgs/applications/networking/cluster/nomad/1.1.nix
+++ pkgs/applications/networking/cluster/nomad/1.1.nix
@@ -6,7 +6,7 @@
 
 callPackage ./genericModule.nix {
   inherit buildGoModule nvidia_x11 nvidiaGpuSupport;
-  version = "1.1.3";
-  sha256 = "0jpc8ff56k9q2kv9l86y3p8h3gqbvx6amvs0cw8sp4i7dqd2ihz2";
-  vendorSha256 = "0az4gr7292lfr5wrwbkdknrigqm15lkbnf5mh517hl3yzv4pb8yr";
+  version = "1.1.4";
+  sha256 = "182f3sxw751s8qg16vbssplhl92i9gshgzvflwwvnxraz2795y7l";
+  vendorSha256 = "1nddknnsvb05sapbj1c52cv2fmibvdg48f88malxqblzw33wfziq";
 }
