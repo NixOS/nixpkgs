c93de5d1d584438c163229652156a12276d4083a
Maxine Aubrey <maxeaubrey@gmail.com>
2021-08-30 20:21:53 +0200

    bluez: 5.60 -> 5.61
    - http://www.bluez.org/release-of-bluez-5-61/
    - https://github.com/bluez/bluez/blob/5.61/ChangeLog

--- pkgs/os-specific/linux/bluez/default.nix
+++ pkgs/os-specific/linux/bluez/default.nix
@@ -21,11 +21,11 @@
   ];
 in stdenv.mkDerivation rec {
   pname = "bluez";
-  version = "5.60";
+  version = "5.61";
 
   src = fetchurl {
     url = "mirror://kernel/linux/bluetooth/${pname}-${version}.tar.xz";
-    sha256 = "sha256-cQmZWA0B7lnsWF5efAf9lO3e3AAaom/nRkxUb52UUwQ=";
+    sha256 = "sha256-g6/WxSF5VUv+q7y1OP7C62vpCorDxAhxtJ162LScQjs=";
   };
 
   buildInputs = [
