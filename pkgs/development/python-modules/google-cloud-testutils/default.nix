ee887e0e57836689572acb38237652a2ebec14e1
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-08-31 00:17:07 +0200

    python3Packages.google-cloud-testutils: 1.0.0 -> 1.1.0

--- pkgs/development/python-modules/google-cloud-testutils/default.nix
+++ pkgs/development/python-modules/google-cloud-testutils/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-testutils";
-  version = "1.0.0";
+  version = "1.1.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-2RaRhRvgnmr/trjAE+RBlVO5AZ54YEFcoRwcPCKCTKs=";
+    sha256 = "3d79051b6ca170c2a4f159bf56ac3f66c1e5360486121e72c06fdaa3911154bf";
   };
 
   propagatedBuildInputs = [ click google-auth six ];
