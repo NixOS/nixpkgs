4780757785e8a5f93c849016d2625e4b556d2dec
Maxine Aubrey <maxeaubrey@gmail.com>
2021-08-30 20:42:06 +0200

    vault: 1.7.2 -> 1.7.4
    - https://github.com/hashicorp/vault/releases/tag/v1.7.3
    - https://github.com/hashicorp/vault/releases/tag/v1.7.4

--- pkgs/tools/security/vault/default.nix
+++ pkgs/tools/security/vault/default.nix
@@ -6,13 +6,13 @@
 
 buildGoPackage rec {
   pname = "vault";
-  version = "1.7.2";
+  version = "1.7.4";
 
   src = fetchFromGitHub {
     owner = "hashicorp";
     repo = "vault";
     rev = "v${version}";
-    sha256 = "0nd77lfccl71qn98cq1yz85aiafplxbr58nafbbflijs1fz1771q";
+    sha256 = "sha256-SODweNtIwjme00/cm9YQG7HDRaRf9bGrOV77txd0iuw=";
   };
 
   goPackagePath = "github.com/hashicorp/vault";
