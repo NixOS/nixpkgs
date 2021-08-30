1d22aed0417ef945552d4dc2abcf04e806162351
Matthieu Coudron <mcoudron@hotmail.com>
2021-08-31 00:17:26 +0200

    luarocks-nix: bumped luarocks-nix
    this new version removes support of src.rock but now supports rockspecs embedded in a repo. The goal is to be able to support neovim plugins that provide a rockspec, even when they are not registered on luarocks.

--- pkgs/development/tools/misc/luarocks/luarocks-nix.nix
+++ pkgs/development/tools/misc/luarocks/luarocks-nix.nix
@@ -2,18 +2,17 @@
 luarocks.overrideAttrs(old: {
   pname = "luarocks-nix";
   version = "2021-01-22";
-  src = /home/teto/luarocks;
-  # src = fetchFromGitHub {
-  #   owner = "nix-community";
-  #   repo = "luarocks-nix";
-  #   rev = "test-speedup";
-  #   sha256 = "sha256-AN4D69IUfCnIWmM7CXWmXeNcTgRCIOamfjYEL4ti9CQ=";
-  # };
+  src = fetchFromGitHub {
+    owner = "nix-community";
+    repo = "luarocks-nix";
+    rev = "test-speedup";
+    sha256 = "sha256-WfzLSpIp0V7Ib4sjYvoJHF+/vHaieccvfVAr5W47QsQ=";
+  };
   patches = [];
   # src = builtins.fetchGit {
   #   url = "https://github.com/nix-community/luarocks-nix.git";
   #   ref = "test-speedup";
-  #   rev = "f9cb2285fda1d73bfa8ae33b6102b286dbe1f098";
+  #   rev = "68ebd5356206a625021b9add4ec2f916692a55eb";
   # };
 
   meta.mainProgram = "luarocks";
