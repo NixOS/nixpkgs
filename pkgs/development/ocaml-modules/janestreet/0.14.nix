f81c41fa8f9643e8b2dedd61dfbf438d8bcdc704
Vincent Laporte <Vincent.Laporte@gmail.com>
2021-08-30 20:37:22 +0200

    ocamlPackages.parsexp: 0.14.0 → 0.14.1

--- pkgs/development/ocaml-modules/janestreet/0.14.nix
+++ pkgs/development/ocaml-modules/janestreet/0.14.nix
@@ -351,7 +351,8 @@ with self;
 
   parsexp = janePackage {
     pname = "parsexp";
-    hash = "0rvbrf8ggh2imsbhqi15jzyyqbi3m5hzvy2iy2r4skx6m102mzpd";
+    version = "0.14.1";
+    hash = "1nr0ncb8l2mkk8pqzknr7fsqw5kpz8y102kyv5bc0x7c36v0d4zy";
     minimumOCamlVersion = "4.04.2";
     meta.description = "S-expression parsing library";
     propagatedBuildInputs = [ base sexplib0 ];
