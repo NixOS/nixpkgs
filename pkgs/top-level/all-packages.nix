db4b3ef15a3fed741820affdb35154b8af9c8b11
Joseph Crail <jbcrail@gmail.com>
2021-08-30 16:27:43 -0700

    dwz: init at 0.14 (#136078)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13747,6 +13747,8 @@ with pkgs;
 
   drush = callPackage ../development/tools/misc/drush { };
 
+  dwz = callPackage ../development/tools/misc/dwz { };
+
   easypdkprog = callPackage ../development/embedded/easypdkprog { };
 
   editorconfig-checker = callPackage ../development/tools/misc/editorconfig-checker { };
