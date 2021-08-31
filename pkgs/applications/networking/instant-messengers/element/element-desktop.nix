7ddb48c45fe022dd72e9f3167379463eb4f72bf4
Sumner Evans <me@sumnerevans.com>
2021-08-31 10:44:11 -0600

    element-{web,desktop}: 1.8.1 -> 1.8.2
    (cherry picked from commit 29c70e8b88e1f2ae25f44691d34ffdb073d575f5)

--- pkgs/applications/networking/instant-messengers/element/element-desktop.nix
+++ pkgs/applications/networking/instant-messengers/element/element-desktop.nix
@@ -8,12 +8,12 @@
 
 let
   executableName = "element-desktop";
-  version = "1.8.1";
+  version = "1.8.2";
   src = fetchFromGitHub {
     owner = "vector-im";
     repo = "element-desktop";
     rev = "v${version}";
-    sha256 = "sha256-FIKbyfnRuHBbmtjwxNC//n5UiGTCQNr+PeiZEi3+RGI=";
+    sha256 = "sha256-6DPMfx3LF45YWn2do02zDMLYZGBgBrOMJx3XBAO0ZyM=";
   };
 in mkYarnPackage rec {
   name = "element-desktop-${version}";
