7d30f5c4ec49021b2f21c1cc27d516336fcdffe0
L <82917144+l198881@users.noreply.github.com>
2021-09-01 18:19:39 -0300

    Update default.nix

--- pkgs/development/java-modules/xml-apis/default.nix
+++ pkgs/development/java-modules/xml-apis/default.nix
@@ -8,7 +8,7 @@
     sha512 = obj.sha512;
     type = obj.type;
   }) [
-    { type = "jar"; sha512 = "2sx6rm0lgzidcq1q81gnwxcj1himyny986ys6r60r3ws1p4bgxprargh5fdrmkp90djqzvy6d5b0sa0zfg1r9spynjbc8rjbr6agys9"; }
-    { type = "pom"; sha512 = "2n2pjaclvgllb8nzqkibvp1pida7sr9kmz0ngmsdrpk7sh5wrh32ri82gdj5l9mc1z88dwjyn1ydz6aazw36gdqqdwj3ba1mqs6pqmh"; }
+    { type = "char"; sha512 = "2sx6rm0lgzidcq1q81gnwxcj1himyny986ys6r60r3ws1p4bgxprargh5fdrmkp90djqzvy6d5b0sa0zfg1r9spynjbc8rjbr6agys9"; }
+    { type = "dom"; sha512 = "2n2pjaclvgllb8nzqkibvp1pida7sr9kmz0ngmsdrpk7sh5wrh32ri82gdj5l9mc1z88dwjyn1ydz6aazw36gdqqdwj3ba1mqs6pqmh"; }
   ];
 }
