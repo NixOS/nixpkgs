beba9f4200b63f1ebca305efb92dd2407caaf40e
R. RyanTM <ryantm-bot@ryantm.com>
2021-09-01 14:42:32 +0000

    python38Packages.django-mailman3: 1.3.5 -> 1.3.6

--- pkgs/development/python-modules/django-mailman3/default.nix
+++ pkgs/development/python-modules/django-mailman3/default.nix
@@ -4,11 +4,11 @@
 
 buildPythonPackage rec {
   pname = "django-mailman3";
-  version = "1.3.5";
+  version = "1.3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "368595b3c2623edeaca5beea5f12887424c384edd6f7052cf442443075084313";
+    sha256 = "cd3cb1ac26a3658bd694b82f00eaab98903bd2edff34956e90c187c9ab5c0dae";
   };
 
   propagatedBuildInputs = [
