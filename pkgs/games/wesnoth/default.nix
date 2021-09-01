96f02afbe58d243835c9b7da811c6bab61690ed9
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-09-01 21:49:06 +0200

    wesnoth: fix license information
    source: https://github.com/wesnoth/wesnoth/blob/master/copyright

--- pkgs/games/wesnoth/default.nix
+++ pkgs/games/wesnoth/default.nix
@@ -34,7 +34,7 @@ stdenv.mkDerivation rec {
     '';
 
     homepage = "https://www.wesnoth.org/";
-    license = licenses.gpl2;
+    license = licenses.gpl2Plus;
     maintainers = with maintainers; [ abbradar ];
     platforms = platforms.unix;
   };
