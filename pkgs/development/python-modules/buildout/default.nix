59b2e853d59a6de3cf8c059c7acab97a0e0b7a77
superherointj <5861043+superherointj@users.noreply.github.com>
2021-09-02 15:22:59 -0300

    pythonPackages.zc_buildout: 2.13.4 -> 3.0.0b2

--- pkgs/development/python-modules/buildout/default.nix
+++ pkgs/development/python-modules/buildout/default.nix
@@ -1,18 +1,42 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib, buildPythonPackage, fetchFromGitHub
+, setuptools, pip, wheel
+# , zope_testing, manuel, zope_bobo, zdaemon, zope_zc_zdaemonrecipe, zope_zc_recipe_deployment
+}:
 
 buildPythonPackage rec {
-  pname = "zc.buildout";
-  version = "2.13.4";
+  pname = "zc_buildout";
+  version = "3.0.0b2";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "b978b2f9317b317ee4191f78fcc4f05b1ac41bdaaae47f0956f14c8285feef63";
+  src = fetchFromGitHub {
+    owner = "buildout";
+    repo = "buildout";
+    rev = version;
+    sha256 = "01sj09xx5kmkzynhq1xd8ahn6xqybfi8lrqjqr5lr45aaxjk2pid";
   };
 
+  propagatedBuildInputs = [
+    setuptools
+    pip
+    wheel
+  ];
+
+  # checkInputs = [
+  #   zope_testing
+  #   manuel
+  #   zope_bobo
+  #   zdaemon
+  #   #zope_zc_zdaemonrecipe # Missing package & Blocked on `zc.recipe.egg`.
+  #   (zope_zc_recipe_deployment.overridePythonAttrs (oldAttrs: rec { doCheck = false; }))
+  # ];
+  checkPhase = ''
+    $out/bin/buildout --version | grep ${version} > /dev/null
+  '';
+
   meta = with lib; {
-    homepage = "http://www.buildout.org";
     description = "A software build and configuration system";
+    downloadPage = "https://github.com/buildout/buildout";
+    homepage = "https://www.buildout.org";
     license = licenses.zpl21;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ superherointj ];
   };
 }
