45366e20d05e17f5636995a4ec655b7e856b873c
superherointj <5861043+superherointj@users.noreply.github.com>
2021-09-02 09:43:52 -0300

    pythonPackages.stestr: init 3.2.0

---
+++ pkgs/development/python-modules/stestr/default.nix
@@ -0,0 +1,45 @@
+{ lib, buildPythonPackage, fetchPypi, callPackage
+, future, pbr, cliff, subunit, fixtures, testtools, pyyaml, voluptuous
+#, coverage, ddt, doc8, openstack-hacking, sphinx
+}:
+
+buildPythonPackage rec {
+  pname = "stestr";
+  version = "3.2.0";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "fb492cbdf3d3fdd6812645804efc84a99a68bb60dd7705f15c1a2949c8172bc4";
+  };
+
+  propagatedBuildInputs = [
+    future
+    pbr
+    cliff
+    subunit
+    fixtures
+    testtools
+    pyyaml
+    voluptuous
+  ];
+
+  # FIXME: Tests disabled until "openstack-hacking" is merged.
+  # checkInputs = [
+  #   coverage
+  #   ddt
+  #   doc8
+  #   (openstack-hacking.overridePythonAttrs (oldAttrs: rec { doCheck = false; }))
+  #   sphinx
+  # ];
+  checkPhase = ''
+    $out/bin/stestr --version | grep ${version} > /dev/null
+  '';
+
+  meta = with lib; {
+    description = "A parallel Python test runner built around subunit";
+    downloadPage = "https://pypi.org/project/stestr/";
+    homepage = "https://github.com/mtreinish/stestr/";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ superherointj ];
+  };
+}
