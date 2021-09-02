97d3febd2e3bb550a0240d33a7cf16e61bf8b57b
Emery Hemingway <ehmry@posteo.net>
2021-09-02 19:03:01 +0200

    hottext: convert to buildNimPackage

---
+++ pkgs/development/nim-packages/vmath/default.nix
@@ -0,0 +1,7 @@
+{ fetchNimble }:
+
+fetchNimble {
+  pname = "vmath";
+  version = "1.0.3";
+  hash = "sha256-zzSKXjuTZ46HTFUs0N47mxEKTKIdS3dwr+60sQYSdn0=";
+}
--- pkgs/top-level/nim-packages.nix
+++ pkgs/top-level/nim-packages.nix
@@ -9,4 +9,22 @@ lib.makeScope newScope (self:
       callPackage ../development/nim-packages/build-nim-package { };
     fetchNimble = callPackage ../development/nim-packages/fetch-nimble { };
 
+    bumpy = callPackage ../development/nim-packages/bumpy { };
+
+    chroma = callPackage ../development/nim-packages/chroma { };
+
+    flatty = callPackage ../development/nim-packages/flatty { };
+
+    nimsimd = callPackage ../development/nim-packages/nimsimd { };
+
+    pixie = callPackage ../development/nim-packages/pixie { };
+
+    sdl2 = callPackage ../development/nim-packages/sdl2 { };
+
+    typography = callPackage ../development/nim-packages/typography { };
+
+    vmath = callPackage ../development/nim-packages/vmath { };
+
+    zippy = callPackage ../development/nim-packages/zippy { };
+
   })
