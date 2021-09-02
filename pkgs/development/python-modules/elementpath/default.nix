f8a59f157fcad2d667d5607d8281f46894875a74
R. RyanTM <ryantm-bot@ryantm.com>
2021-09-02 09:48:16 +0000

    python38Packages.elementpath: 2.2.3 -> 2.3.0

--- pkgs/development/python-modules/elementpath/default.nix
+++ pkgs/development/python-modules/elementpath/default.nix
@@ -1,7 +1,7 @@
 { lib, buildPythonPackage, fetchFromGitHub, isPy27 }:
 
 buildPythonPackage rec {
-  version = "2.2.3";
+  version = "2.3.0";
   pname = "elementpath";
   disabled = isPy27; # uses incompatible class syntax
 
@@ -9,7 +9,7 @@ buildPythonPackage rec {
     owner = "sissaschool";
     repo = "elementpath";
     rev = "v${version}";
-    sha256 = "142w8xbxdvz6949nqd4malqijyxfcv917cls4lsv4x7164b4naf7";
+    sha256 = "1zghj0v1s03ahynx7sczag7iaam757ypwl1lm0ym6j37dnq8vnxk";
   };
 
   # avoid circular dependency with xmlschema which directly depends on this
