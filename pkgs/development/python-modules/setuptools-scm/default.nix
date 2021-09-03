8fa32dafcc3157f03bc3a4ba349c0b58d5fc4f6d
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-09-04 01:28:18 +0200

    python3Packages.setuptools-scm: 6.0.1 -> 6.3.1, enable tests

--- pkgs/development/python-modules/setuptools-scm/default.nix
+++ pkgs/development/python-modules/setuptools-scm/default.nix
@@ -1,29 +1,36 @@
-{ lib, buildPythonPackage, fetchPypi, toml, tomli }:
+{ buildPythonPackage
+, callPackage
+, fetchPypi
+, packaging
+, tomli
+, lib
+}:
 
 buildPythonPackage rec {
   pname = "setuptools-scm";
-  # don't update to 6.1.0 or 6.2.0, releases were pulled because of regression
-  # https://github.com/pypa/setuptools_scm/issues/615
-  version = "6.0.1";
+  version = "6.3.1";
 
   src = fetchPypi {
     pname = "setuptools_scm";
     inherit version;
-    sha256 = "sha256-0ZJaacsH6bKUFqJ1ufrbAJojwUis6QWy+yIGSabBjpI=";
+    sha256 = "sha256-D2omORKxN5jAKLmicdka873g5CeECRx5fezMOtOn9ZY=";
   };
 
-  postPatch = ''
-    substituteInPlace setup.cfg \
-      --replace "tomli~=1.0.0" "tomli>=1.0.0"
-  '';
+  propagatedBuildInputs = [
+    packaging
+    tomli
+  ];
 
-  # TODO: figure out why both toml and tomli are needed when only tomli is listed in setuptools-scm
-  # if not both are listed some packages like zipp silently fallback to a 0.0.0 version number and break version pins in other packages
-  propagatedBuildInputs = [ toml tomli ];
+  pythonImportsCheck = [
+    "setuptools_scm"
+  ];
 
-  # Requires pytest, circular dependency
+  # check in passhtru.tests.pytest to escape infinite recursion on pytest
   doCheck = false;
-  pythonImportsCheck = [ "setuptools_scm" ];
+
+  passthru.tests = {
+    pytest = callPackage ./tests.nix { };
+  };
 
   meta = with lib; {
     homepage = "https://github.com/pypa/setuptools_scm/";
