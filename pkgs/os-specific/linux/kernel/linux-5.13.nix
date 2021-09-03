ff64785c049beaaa8b1c16d5ffe322ea8d68bb62
Tim Steinbach <tim@nequissimus.com>
2021-09-03 06:53:27 -0400

    linux: 5.13.13 -> 5.13.14

--- pkgs/os-specific/linux/kernel/linux-5.13.nix
+++ pkgs/os-specific/linux/kernel/linux-5.13.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.13.13";
+  version = "5.13.14";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0d1lr3rivgf9j3bn2a9hpzdf74nq8kybf3rfxxvw68vr1hhd4cam";
+    sha256 = "06lbjsbr86qa8yai5gfclbfxvcqsw33kxj9b4r93hh6z1wajmx82";
   };
 } // (args.argsOverride or { }))
