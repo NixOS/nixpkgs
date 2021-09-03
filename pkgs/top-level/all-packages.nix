dbb78328cff22b32ca401d86d3fb3fe51ff4353c
Fabian Affolter <mail@fabian-affolter.ch>
2021-09-03 21:02:38 +0200

    dalfox: init at 2.4.9

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30373,6 +30373,8 @@ with pkgs;
 
   conglomerate = callPackage ../applications/science/biology/conglomerate { };
 
+  dalfox = callPackage ../tools/security/dalfox { };
+
   dcm2niix = callPackage ../applications/science/biology/dcm2niix { };
 
   deepdiff = with python3Packages; toPythonApplication deepdiff;
