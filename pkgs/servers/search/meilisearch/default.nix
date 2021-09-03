9bea6f08294ea9a4538d3a6d4d1dac45b08c06c1
happysalada <raphael@megzari.com>
2021-09-03 12:39:22 +0900

    meilisearch: add wrapper derivation for renaming

--- pkgs/servers/search/meilisearch/default.nix
+++ pkgs/servers/search/meilisearch/default.nix
@@ -1,251 +1,48 @@
 { lib
 , stdenv
-, rustPlatform
 , buildRustCrate
 , defaultCrateOverrides
 , fetchFromGitHub
 , Security
-, libiconv
-, pkg-config
-, zlib
 , features ? [ ]
 }:
 
 let
+  version = "0.21.1";
   src = fetchFromGitHub {
     owner = "meilisearch";
     repo = "MeiliSearch";
-    rev = "v0.21.1";
+    rev = "v${version}";
     sha256 = "sha256-wyyhTNhVw8EJhahstLK+QuEhufQC68rMpw/ngK8FL8Y=";
   };
-  custom = pkgs: buildRustCrate.override {
+  customBuildRustCrateForPkgs = pkgs: buildRustCrate.override {
     defaultCrateOverrides = defaultCrateOverrides // {
       meilisearch-http = attrs: {
         src = "${src}/meilisearch-http";
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv Security ];
+        buildInputs = lib.optionals stdenv.isDarwin [ Security ];
       };
       meilisearch-error = attrs: {
         src = "${src}/meilisearch-error";
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      bitflags = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      const_fn = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      encoding_rs = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      futures-core = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      futures-task = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      httparse = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      indexmap = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      libc = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      log = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      memchr = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      paste = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      futures-util = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      proc-macro-hack = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      serde = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      tokio = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      typenum = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      proc-macro2 = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      generic-array = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      ahash = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      standback = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      syn = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      anyhow = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      crc32fast = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      crossbeam-utils = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      futures-channel = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      fst = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      getrandom = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      hashbrown = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      memoffset = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      miniz_oxide = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      crossbeam-epoch = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      num-traits = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      num-integer = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      proc-macro-error-attr = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      proc-macro-nested = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      rayon-core = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      rayon = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      brotli-sys = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      lmdb-rkv-sys = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      libz-sys = attrs: {
-        nativeBuildInputs = [ pkg-config ];
-        buildInputs = [ zlib ] ++ lib.optionals stdenv.isDarwin [ libiconv ];
-        extraLinkFlags = [ "-L${zlib.out}/lib" ];
-      };
-      rustversion = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      ryu = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      snap = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      ring = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      jieba-rs = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      logging_timer_proc_macros = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      zstd-sys = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      zstd-safe = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      async-trait = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      actix-macros = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      actix-web-codegen = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      concat-arrays = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      async-stream-impl = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      enum-iterator-derive = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      futures-macro = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      proc-macro-error = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      pest_derive = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      serde_derive = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      getset = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      pin-project-internal = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      structopt-derive = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      derive_more = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      thiserror-impl = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      time-macros-impl = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      tokio-macros = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      time = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      zerocopy-derive = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      cookie = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      serde_json = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-      };
-      vergen = attrs: {
-        buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
       };
     };
   };
   cargo_nix = import ./Cargo.nix {
-    buildRustCrateForPkgs = custom;
+    nixpkgs = ../../../..;
+    buildRustCrateForPkgs = customBuildRustCrateForPkgs;
+  };
+  meilisearch-http = cargo_nix.workspaceMembers."meilisearch-http".build.override {
+    inherit features;
   };
 in
-cargo_nix.workspaceMembers."meilisearch-http".build.override {
-  inherit features;
+stdenv.mkDerivation {
+  pname = "meilisearch";
+  inherit version src;
+  dontUnpack = true;
+  dontBuild = true;
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${meilisearch-http}/bin/meilisearch $out/bin/meilisearch
+  '';
+  dontCheck = true;
+  dontFixup = true;
 }
