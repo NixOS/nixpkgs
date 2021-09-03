8b0fc1f7f5fd37b2c7ca419ff48919c9403a0beb
Matthieu Coudron <mcoudron@hotmail.com>
2021-09-03 16:50:36 +0200

    vimPlugins: split doc generation into a hook
    so that the hook can be used for lua modules as well. Improves
    composition.

---
+++ pkgs/misc/vim-plugins/update-shell.nix
@@ -0,0 +1,13 @@
+{ nixpkgs ? import ../../.. { } }:
+with nixpkgs;
+let
+  pyEnv = python3.withPackages(ps: [ ps.GitPython ]);
+in
+mkShell {
+  packages = [
+    bash
+    pyEnv
+    nix-prefetch-scripts
+  ];
+}
+
