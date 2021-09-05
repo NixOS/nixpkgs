72800727fe743f47525df882c89e14990abba32a
R. RyanTM <ryantm-bot@ryantm.com>
2021-09-05 02:43:50 +0000

    xmrig-proxy: 6.14.0 -> 6.15.0

--- pkgs/applications/misc/xmrig/proxy.nix
+++ pkgs/applications/misc/xmrig/proxy.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation rec {
   pname = "xmrig-proxy";
-  version = "6.14.0";
+  version = "6.15.0";
 
   src = fetchFromGitHub {
     owner = "xmrig";
     repo = "xmrig-proxy";
     rev = "v${version}";
-    sha256 = "sha256-QCjXtn7O4jcPybzMsu2j7jQqWoGzeqjwessZC/dG86s=";
+    sha256 = "sha256-ZNyD0P1vuX/FBPW6tg4WRcNCJpz9/gY+Bhipqngv3jY=";
   };
 
   nativeBuildInputs = [ cmake ];
