a9fe9e8a327d0a7bd8d3ab81c21533200fa10679
0x4A6F <0x4A6F@users.noreply.github.com>
2021-09-06 19:56:04 +0200

    age: set and check version

--- pkgs/tools/security/age/default.nix
+++ pkgs/tools/security/age/default.nix
@@ -12,6 +12,20 @@ buildGoModule rec {
     sha256 = "sha256-MfyW8Yv8swKqA7Hl45l5Zn4wZrQmE661eHsKIywy36U=";
   };
 
+  ldflags = [
+    "-X main.Version=${version}"
+  ];
+
+  doInstallCheck = true;
+  installCheckPhase = ''
+    if [[ "$("$out/bin/${pname}" --version)" == "${version}" ]]; then
+      echo '${pname} smoke check passed'
+    else
+      echo '${pname} smoke check failed'
+      return 1
+    fi
+  '';
+
   meta = with lib; {
     homepage = "https://age-encryption.org/";
     description = "Modern encryption tool with small explicit keys";
