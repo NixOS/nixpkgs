4e1c9126a32d0418c80644b37f3ac97d0c55fd1e
Maxine Aubrey <maxeaubrey@gmail.com>
2021-09-06 14:14:33 +0200

    pangomm: 2.42.2 -> 2.46.1
    - https://gitlab.gnome.org/GNOME/pangomm/-/blob/2.46.1/NEWS

--- pkgs/development/libraries/pangomm/default.nix
+++ pkgs/development/libraries/pangomm/default.nix
@@ -3,11 +3,11 @@
 
 stdenv.mkDerivation rec {
   pname = "pangomm";
-  version= "2.42.2";
+  version= "2.46.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-GyTJJiSuEnXMtXdYF10198Oa0zQtjAtLpg8NmEnS0Io=";
+    sha256 = "sha256-yIUBP+YaTFEX/aOVdw1QdWNBHGPkn0o6ztTJ7+NNmXU=";
   };
 
   outputs = [ "out" "dev" ];
