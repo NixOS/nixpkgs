bf8e3e079d41e2d8c59dd3cd09dcebd4dd235c72
Maxine Aubrey <maxeaubrey@gmail.com>
2021-09-06 12:30:17 +0200

    atkmm: 2.28.0 -> 2.28.2
    - https://gitlab.gnome.org/GNOME/atkmm/-/blob/2.28.2/NEWS

--- pkgs/development/libraries/atkmm/2.36.nix
+++ pkgs/development/libraries/atkmm/2.36.nix
@@ -1,19 +1,19 @@
-{ lib, stdenv, fetchurl, atk, glibmm, pkg-config, gnome, meson, ninja, python3 }:
+{ lib, stdenv, fetchurl, atk, glibmm, pkg-config, gnome }:
 
 stdenv.mkDerivation rec {
   pname = "atkmm";
-  version = "2.28.2";
+  version = "2.36.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-oLtJdlzszCk6ssZzW6EAQxgH04T/oUwuvTDgeZP9L6Q=";
+    sha256 = "sha256-4RMkv+0bbjMKAtslzswUXcoD+w3/R/BxDIXjF2h9pFg=";
   };
 
   outputs = [ "out" "dev" ];
 
   propagatedBuildInputs = [ atk glibmm ];
 
-  nativeBuildInputs = [ pkg-config meson python3 ninja ];
+  nativeBuildInputs = [ pkg-config ];
 
   doCheck = true;
 
