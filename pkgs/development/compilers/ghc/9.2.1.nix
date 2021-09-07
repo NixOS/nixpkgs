89c37a0988787966983727f354e0f281e1070d97
(cdep)illabout <cdep.illabout@gmail.com>
2021-09-07 09:49:54 +0900

    haskell.compiler.ghc921: remove darwin from hydraPlatforms since it is broken
    This was marked broken in 6f1a319c4567d1c4273f38949ba.

--- pkgs/development/compilers/ghc/9.2.1.nix
+++ pkgs/development/compilers/ghc/9.2.1.nix
@@ -313,6 +313,7 @@ stdenv.mkDerivation (rec {
     # https://gitlab.haskell.org/ghc/ghc/-/issues/19950#note_373726
     broken = (enableIntegerSimple && hostPlatform.isMusl)
       || stdenv.hostPlatform.isDarwin;
+    hydraPlatforms = lib.remove "x86_64-darwin" ghc.meta.platforms;
   };
 
 } // lib.optionalAttrs targetPlatform.useAndroidPrebuilt {

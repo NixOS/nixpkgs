NixOS/nixpkgs

commit 6f1a319c4567d1c4273f38949ba65586585f2144

haskell.compiler.ghc921: mark as broken on darwin
Seems like there is either a fundamental issue with how our derivation
works or an upstream bug prevent GHC 9.2.1 from compiling on macOS using
nixpkgs. Until someone fixes that or rc2 comes around, we can save the
build time.

pkgs/development/compilers/ghc/9.2.1.nix
@@ -307,7 +307,10 @@ stdenv.mkDerivation (rec {
 
     # integer-simple builds are broken when GHC links against musl.
     # See https://github.com/NixOS/nixpkgs/pull/129606#issuecomment-881323743.
-    broken = enableIntegerSimple && hostPlatform.isMusl;
+    # Linker failure on macOS:
+    # https://gitlab.haskell.org/ghc/ghc/-/issues/19950#note_373726
+    broken = (enableIntegerSimple && hostPlatform.isMusl)
+      || stdenv.hostPlatform.isDarwin;
   };
 
 } // lib.optionalAttrs targetPlatform.useAndroidPrebuilt {

commit 89c37a0988787966983727f354e0f281e1070d97

haskell.compiler.ghc921: remove darwin from hydraPlatforms since it is broken
This was marked broken in 6f1a319c4567d1c4273f38949ba.

pkgs/development/compilers/ghc/9.2.1.nix
@@ -313,6 +313,7 @@ stdenv.mkDerivation (rec {
     # https://gitlab.haskell.org/ghc/ghc/-/issues/19950#note_373726
     broken = (enableIntegerSimple && hostPlatform.isMusl)
       || stdenv.hostPlatform.isDarwin;
+    hydraPlatforms = lib.remove "x86_64-darwin" ghc.meta.platforms;
   };
 
 } // lib.optionalAttrs targetPlatform.useAndroidPrebuilt {