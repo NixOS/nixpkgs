509b969a1804c5060bc9c34183b0752e1df4588a
Robert Scott <code@humanleg.org.uk>
2021-09-08 00:25:12 +0100

    botan2: 2.18.0 -> 2.18.1

--- pkgs/development/libraries/botan/2.0.nix
+++ pkgs/development/libraries/botan/2.0.nix
@@ -2,8 +2,8 @@
 
 callPackage ./generic.nix (args // {
   baseVersion = "2.18";
-  revision = "0";
-  sha256 = "09z3fy31q1pvnvpy4fswrsl2aq8ksl94lbh5rl7b6nqc3qp8ar6c";
+  revision = "1";
+  sha256 = "0adf53drhk1hlpfih0175c9081bqpclw6p2afn51cmx849ib9izq";
   postPatch = ''
     sed -e 's@lang_flags "@&--std=c++11 @' -i src/build-data/cc/{gcc,clang}.txt
   '';
