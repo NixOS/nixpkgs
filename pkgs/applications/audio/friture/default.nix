a008d288c388c7414ff7755e3350c4b7d072cb1c
alyaeanyx <alexandra.hollmeier@mailbox.org>
2021-09-08 13:45:52 +0200

    friture: fix desktop item
    friture: remove trailing whitespace
    
    friture: use substituteInPlace instead of patch file
    
    friture: fix

--- pkgs/applications/audio/friture/default.nix
+++ pkgs/applications/audio/friture/default.nix
@@ -36,6 +36,18 @@ in py.buildPythonApplication rec {
     makeWrapperArgs+=("''${qtWrapperArgs[@]}")
   '';
 
+  postInstall = ''
+    substituteInPlace $out/share/applications/friture.desktop --replace usr/bin/friture friture
+
+    for size in 16 32 128 256 512
+    do
+      mkdir -p $out/share/icons/hicolor/$size\x$size
+      cp $src/resources/images/friture.iconset/icon_$size\x$size.png $out/share/icons/hicolor/$size\x$size/friture.png
+    done
+    mkdir -p $out/share/icons/hicolor/scalable/apps/
+    cp $src/resources/images-src/window-icon.svg $out/share/icons/hicolor/scalable/apps/friture.svg
+  '';
+
   meta = with lib; {
     description = "A real-time audio analyzer";
     homepage = "https://friture.org/";
