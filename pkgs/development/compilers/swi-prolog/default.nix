2e0d1231584451741f62fe810a8385040cfc6a9f
Ben Siraphob <bensiraphob@gmail.com>
2021-09-07 22:51:03 -0500

    swi-prolog: set meta.mainProgram

--- pkgs/development/compilers/swi-prolog/default.nix
+++ pkgs/development/compilers/swi-prolog/default.nix
@@ -81,7 +81,7 @@ stdenv.mkDerivation {
     homepage = "https://www.swi-prolog.org";
     description = "A Prolog compiler and interpreter";
     license = lib.licenses.bsd2;
-
+    mainProgram = "swipl";
     platforms = lib.platforms.linux ++ lib.optionals (!withGui) lib.platforms.darwin;
     maintainers = [ lib.maintainers.meditans ];
   };
