1f9733a07780e5e2f5e10d1b92efcf9a0fed8fb6
Matthew Leach <matthew@mattleach.net>
2021-09-08 21:33:08 +0100

    ite-backlight: init at v1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6212,6 +6212,8 @@ with pkgs;
 
   itm-tools = callPackage ../development/tools/misc/itm-tools { };
 
+  ite-backlight = callPackage ../misc/ite-backlight { };
+
   iwgtk = callPackage ../tools/networking/iwgtk { };
 
   ix = callPackage ../tools/misc/ix { };
