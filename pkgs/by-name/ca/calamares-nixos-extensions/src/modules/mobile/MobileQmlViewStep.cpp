418cf9b5897ae0e73a71d1bad95d7127aee038c9
Adriaan de Groot <groot@kde.org>
2021-09-09 10:57:21 +0200

    CMake: adjust extensions to changes in Calamares core

--- pkgs/by-name/ca/calamares-nixos-extensions/src/modules/mobile/MobileQmlViewStep.cpp
+++ pkgs/by-name/ca/calamares-nixos-extensions/src/modules/mobile/MobileQmlViewStep.cpp
@@ -2,15 +2,13 @@
  * SPDX-License-Identifier: GPL-3.0-or-later */
 #include "MobileQmlViewStep.h"
 
+#include "Branding.h"
 #include "GlobalStorage.h"
-
-#include "locale/LabelModel.h"
+#include "locale/TranslationsModel.h"
+#include "modulesystem/ModuleManager.h"
 #include "utils/Dirs.h"
 #include "utils/Logger.h"
 #include "utils/Variant.h"
-
-#include "Branding.h"
-#include "modulesystem/ModuleManager.h"
 
 #include <QProcess>
 
