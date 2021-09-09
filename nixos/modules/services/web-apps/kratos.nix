bd60117cee74f251dbb4a02555b892166b2ab4b2
Jacob Hrbek <kreyren@fsfe.org>
2021-09-09 11:24:44 +0200

    Changed indent on 2 spaces as requested
    Signed-off-by: Jacob Hrbek <kreyren@fsfe.org>

--- nixos/modules/services/web-apps/kratos.nix
+++ nixos/modules/services/web-apps/kratos.nix
@@ -1,34 +1,34 @@
 { config, lib, pkgs, ... }:
 let
-    cfg = config.services.kratos;
+  cfg = config.services.kratos;
 
-    upstreamKratosVersion = lib.getVersion pkgs.kratos;
+  upstreamKratosVersion = lib.getVersion pkgs.kratos;
 
-    kratosPackage = if config.services.kratos.enable
-        then
-            config.services.kratos.package
-        else
-            pkgs.kratos;
+  kratosPackage = if config.services.kratos.enable
+    then
+      config.services.kratos.package
+    else
+      pkgs.kratos;
 
-    kratosVersion = lib.getVersion kratosPackage;
+  kratosVersion = lib.getVersion kratosPackage;
 in {
-    options = {
-        services.kratos = {
-            enable = lib.mkEnableOption "Kratos, Libre and open source identity server";
+  options = {
+    services.kratos = {
+      enable = lib.mkEnableOption "Kratos, Libre and open source identity server";
 
-            package = lib.mkOption {
-                type = lib.types.package;
-                default = pkgs.kratos;
-                defaultText = "pkgs.kratos";
-                description = ''
-                    The kratos package to use
-                '';
-            };
-        };
+      package = lib.mkOption {
+        type = lib.types.package;
+        default = pkgs.kratos;
+        defaultText = "pkgs.kratos";
+        description = ''
+          The kratos package to use
+        '';
+      };
     };
+  };
 
-    # config = lib.mkIf cfg.enable {
-    # };
+  # config = lib.mkIf cfg.enable {
+  # };
 
-    meta.maintainers = [ ];
+  meta.maintainers = [ ];
 }
