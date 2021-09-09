71b7b77cbe1928561f4e8e595aef4fa4ac20e0cf
Mario Rodas <marsam@users.noreply.github.com>
2021-09-09 04:20:00 +0000

    mcfly: replace executable path in init scripts

--- pkgs/tools/misc/mcfly/default.nix
+++ pkgs/tools/misc/mcfly/default.nix
@@ -11,6 +11,12 @@ rustPlatform.buildRustPackage rec {
     sha256 = "0i3qjgq1b8h3bzc7rxa60kq1yc2im9m6dgzrvial086a1zk8s81r";
   };
 
+  postPatch = ''
+    substituteInPlace mcfly.bash --replace '$(which mcfly)' '${placeholder "out"}/bin/mcfly'
+    substituteInPlace mcfly.zsh  --replace '$(which mcfly)' '${placeholder "out"}/bin/mcfly'
+    substituteInPlace mcfly.fish --replace '(which mcfly)'  '${placeholder "out"}/bin/mcfly'
+  '';
+
   cargoSha256 = "084v4fsdi25ahz068ssq29z7d5d3k3jh3s8b07irwybdsy18c629";
 
   meta = with lib; {
