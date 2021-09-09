32c5fca4d8497c02c9754b23951d599f4b4afae2
Kreyren <kreyren@fsfe.org>
2021-09-09 10:24:05 +0200

    modules/../kratos: Initial commit
    Define bare minimum module that only deploys kratos for further contributions

---
+++ nixos/modules/services/web-apps/kratos.nix
@@ -0,0 +1,34 @@
+{ config, lib, pkgs, ... }:
+let
+	cfg = config.services.kratos;
+
+	upstreamKratosVersion = lib.getVersion pkgs.kratos;
+
+	kratosPackage = if config.services.kratos.enable
+		then
+			config.services.kratos.package
+		else
+			pkgs.kratos;
+
+	kratosVersion = lib.getVersion kratosPackage;
+in {
+	options = {
+		services.kratos = {
+			enable = lib.mkEnableOption "Kratos, Libre and open source identity server";
+
+			package = lib.mkOption {
+				type = lib.types.package;
+				default = pkgs.kratos;
+				defaultText = "pkgs.kratos";
+				description = ''
+					The kratos package to use
+				'';
+			};
+		};
+	};
+
+	# config = lib.mkIf cfg.enable {
+	# };
+
+	meta.maintainers = [ ];
+}
