de1ff3208fd5396d061bc755981029720ad7927d
Niklas Hambüchen <mail@nh2.me>
2021-09-09 22:07:22 +0200

    thunderbird-78: 78.13.0 -> 78.14.0
    Fixes CVE-2021-38495:
    
    https://www.mozilla.org/en-US/security/advisories/mfsa2021-41/#CVE-2021-38495

--- pkgs/applications/networking/mailreaders/thunderbird/default.nix
+++ pkgs/applications/networking/mailreaders/thunderbird/default.nix
@@ -73,13 +73,13 @@ assert waylandSupport -> gtk3Support == true;
 
 stdenv.mkDerivation rec {
   pname = "thunderbird";
-  version = "78.13.0";
+  version = "78.14.0";
 
   src = fetchurl {
     url =
       "mirror://mozilla/thunderbird/releases/${version}/source/thunderbird-${version}.source.tar.xz";
     sha512 =
-      "daee9ea9e57bdfce231a35029807f279a06f8790d71efc8998c78eb42d99a93cf98623170947df99202da038f949ba9111a7ff7adbd43c161794deb6791370a0";
+      "0zan30jvv45pd6i59l2kfyfjwivqk5qq6vyf77xhss2dk8qhk3mfrfxpfbkrab676l14b9hs09nr6ni1h1iwn82zx5k7fx5x8sh5dx6";
   };
 
   nativeBuildInputs = [
