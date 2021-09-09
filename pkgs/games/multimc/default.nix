9b0e1cc2a6623504e1448012411ec377c5fa05c2
leo60228 <leo@60228.dev>
2021-09-08 20:50:37 -0400

    multimc: unstable-2021-06-21 -> unstable-2021-09-08
    (cherry picked from commit fd48ed5022b6c367adbffcfc7aa89a694f5508e6)

--- pkgs/games/multimc/default.nix
+++ pkgs/games/multimc/default.nix
@@ -4,12 +4,12 @@ let
   libpath = with xorg; lib.makeLibraryPath [ libX11 libXext libXcursor libXrandr libXxf86vm libpulseaudio libGL ];
 in mkDerivation rec {
   pname = "multimc";
-  version = "unstable-2021-06-21";
+  version = "unstable-2021-09-08";
   src = fetchFromGitHub {
     owner = "MultiMC";
     repo = "MultiMC5";
-    rev = "8179a89103833805d5374399d80a4305be1b8355";
-    sha256 = "lPz6ZM7TjaixfwWMPaXijKZJQKFPrCegBhvbJ8Xg4P8=";
+    rev = "e2355eb276bf355ca4acf526a0f3cc390aa88f8b";
+    sha256 = "3G9QPoAbC+uVfUYR0Kq6hnxl9c2mvCzIEYGjwfarQJ8=";
     fetchSubmodules = true;
   };
   nativeBuildInputs = [ cmake file makeWrapper ];
@@ -19,7 +19,7 @@ in mkDerivation rec {
 
   postPatch = ''
     # hardcode jdk paths
-    substituteInPlace api/logic/java/JavaUtils.cpp \
+    substituteInPlace launcher/java/JavaUtils.cpp \
       --replace 'scanJavaDir("/usr/lib/jvm")' 'javas.append("${jdk}/lib/openjdk/bin/java")' \
       --replace 'scanJavaDir("/usr/lib32/jvm")' 'javas.append("${jdk8}/lib/openjdk/bin/java")'
   '';
@@ -27,8 +27,8 @@ in mkDerivation rec {
   cmakeFlags = [ "-DMultiMC_LAYOUT=lin-system" ];
 
   postInstall = ''
-    install -Dm644 ../application/resources/multimc/scalable/multimc.svg $out/share/pixmaps/multimc.svg
-    install -Dm755 ../application/package/linux/multimc.desktop $out/share/applications/multimc.desktop
+    install -Dm644 ../launcher/resources/multimc/scalable/multimc.svg $out/share/pixmaps/multimc.svg
+    install -Dm755 ../launcher/package/linux/multimc.desktop $out/share/applications/multimc.desktop
 
     # xorg.xrandr needed for LWJGL [2.9.2, 3) https://github.com/LWJGL/lwjgl/issues/128
     wrapProgram $out/bin/multimc \
