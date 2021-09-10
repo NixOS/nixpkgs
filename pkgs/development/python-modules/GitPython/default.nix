c1aadda3d3ea1b69312f00559151ed680987d3a7
Fabian Affolter <mail@fabian-affolter.ch>
2021-09-10 08:12:56 +0200

    python3Packages.GitPython: 3.1.20 -> 3.1.23

--- pkgs/development/python-modules/GitPython/default.nix
+++ pkgs/development/python-modules/GitPython/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "gitpython";
-  version = "3.1.20";
+  version = "3.1.23";
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "gitpython-developers";
     repo = "GitPython";
     rev = version;
-    sha256 = "1ygrxn8br2ff87j02ibijxzxa0ax9lgjdviwddslqxiarfggik9h";
+    sha256 = "sha256-1+jMg5pOrYJBgv/q9FFnR5Ujc8pwEPZHfLssNnt8nmA=";
   };
 
   patches = [
