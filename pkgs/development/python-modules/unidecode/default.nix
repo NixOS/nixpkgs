c59947fdd16b526b3e7303e86afd30af254e440d
Fabian Affolter <mail@fabian-affolter.ch>
2021-09-10 18:04:16 +0200

    python3Packages.unidecode: enable tests

--- pkgs/development/python-modules/unidecode/default.nix
+++ pkgs/development/python-modules/unidecode/default.nix
@@ -1,22 +1,33 @@
-{ lib, buildPythonPackage, fetchPypi, glibcLocales }:
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, pytestCheckHook
+, pythonOlder
+}:
 
 buildPythonPackage rec {
-  pname = "Unidecode";
+  pname = "unidecode";
   version = "1.3.1";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-bvrAkL+PKZcK/JDK9Nquh7FycJt4bLG02i0MBiRDHsw=";
+  disabled = pythonOlder "3.5";
+
+  src = fetchFromGitHub {
+    owner = "avian2";
+    repo = pname;
+    rev = "${pname}-${version}";
+    sha256 = "07789mrq0gjxrg1b9a3ypzzfww224sbj25wl0h9nik22sjwi8qhh";
   };
 
-  LC_ALL="en_US.UTF-8";
+  checkInputs = [
+    pytestCheckHook
+  ];
 
-  buildInputs = [ glibcLocales ];
+  pythonImportsCheck = [ "unidecode" ];
 
   meta = with lib; {
     homepage = "https://pypi.python.org/pypi/Unidecode/";
     description = "ASCII transliterations of Unicode text";
-    license = licenses.gpl2;
+    license = licenses.gpl2Plus;
     maintainers = with maintainers; [ domenkozar ];
   };
 }
