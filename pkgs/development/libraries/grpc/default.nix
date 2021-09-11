d126476d4060d997bfb440d2fa3756eca3feeefa
Mario Rodas <marsam@users.noreply.github.com>
2021-09-11 17:00:00 -0500

    grpc: add patch to revert alias to abseil mutex

--- pkgs/development/libraries/grpc/default.nix
+++ pkgs/development/libraries/grpc/default.nix
@@ -33,6 +33,13 @@ stdenv.mkDerivation rec {
       url = "https://github.com/grpc/grpc/commit/2f4cf1d9265c8e10fb834f0794d0e4f3ec5ae10e.patch";
       sha256 = "0ams3jmgh9yzwmxcg4ifb34znamr7pb4qm0609kvil9xqvkqz963";
     })
+
+    # Revert gRPC C++ Mutex to be an alias of Abseil, because it breaks dependent packages
+    (fetchpatch {
+      url = "https://github.com/grpc/grpc/commit/931f91b745cd5b2864a0d1787815871d0bd844ae.patch";
+      sha256 = "0vc93g2i4982ys4gzyaxdv9ni25yk10sxq3n7fkz8dypy8sylck7";
+      revert = true;
+    })
   ];
 
   nativeBuildInputs = [ cmake pkg-config ]
