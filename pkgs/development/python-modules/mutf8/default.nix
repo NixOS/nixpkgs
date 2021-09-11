28e7cd989082bbfae88ac3c6fd473a8bfcc87bb4
Fabian Affolter <mail@fabian-affolter.ch>
2021-09-11 09:07:58 +0200

    python3Packages.mutf8: 1.0.3 -> 1.0.4

--- pkgs/development/python-modules/mutf8/default.nix
+++ pkgs/development/python-modules/mutf8/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "mutf8";
-  version = "1.0.3";
+  version = "1.0.4";
 
   disabled = pythonOlder "3.6";
 
@@ -15,7 +15,7 @@ buildPythonPackage rec {
     owner = "TkTech";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0p9xczkhrf9d3n44k6kxbnk9sm831k5gkiagk6vm75vcmzm7zdqc";
+    sha256 = "sha256-99NL4mCvzQmDvaOI2M9PA8UUwIrrEtVHRmJqkd6hNd0=";
   };
 
   checkInputs = [
