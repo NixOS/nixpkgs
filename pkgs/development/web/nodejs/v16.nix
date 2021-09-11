24201b18485787f3a5eabd6549e44b68eab62888
Mario Rodas <marsam@users.noreply.github.com>
2021-09-11 04:20:00 +0000

    nodejs-16_x: 16.9.0 -> 16.9.1
    https://github.com/nodejs/node/releases/tag/v16.9.1
    (cherry picked from commit 35f805cf9fcc5788aab8f59cfd963d62ffee8203)

--- pkgs/development/web/nodejs/v16.nix
+++ pkgs/development/web/nodejs/v16.nix
@@ -8,7 +8,7 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "16.9.0";
-    sha256 = "0vv6igmnz4fkr4i8gczxxw2qgcvydkpy71w3lskah8zw1lh69rqs";
+    version = "16.9.1";
+    sha256 = "070k8i9a65r03xdchr200qixv053mim5irfvgg4pl3h57k2hxxcp";
     patches = [ ./disable-darwin-v8-system-instrumentation.patch ];
   }
