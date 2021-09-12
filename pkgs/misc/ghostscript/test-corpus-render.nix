57692f6d3e48766a7c50fa00f6157ece953595ca
Robert Scott <code@humanleg.org.uk>
2021-09-12 13:13:19 +0100

    ghostscript: add passthru.tests.test-corpus-render
    this simply attempts rendering every ps/eps/pdf file in the ghostscript
    test corpus

---
+++ pkgs/misc/ghostscript/test-corpus-render.nix
@@ -0,0 +1,38 @@
+{ lib
+, stdenv
+, fetchgit
+, ghostscript
+}:
+
+stdenv.mkDerivation {
+  pname = "ghostscript-test-corpus-render";
+  version = "unstable-2020-02-19";
+
+  src = fetchgit {
+    url = "git://git.ghostscript.com/tests.git";
+    rev = "efdd224340d9a407ed3ec22afa1cb127c8fee73c";
+    sha256 = "1v1iqz897zzrwa8ng22zcf3y61ab5798jdwidgv10w1r9mjrl7ax";
+  };
+
+  dontConfigure = true;
+  dontBuild = true;
+
+  doCheck = true;
+  checkPhase = ''
+    find . -iregex '.*\.\(ps\|eps\|pdf\)' | while read f; do
+      echo "Rendering $f"
+      ${ghostscript}/bin/gs \
+        -dNOPAUSE \
+        -dBATCH \
+        -sDEVICE=bitcmyk \
+        -sOutputFile=/dev/null \
+        -r600 \
+        -dBufferSpace=100000 \
+        $f
+    done
+  '';
+
+  installPhase = ''
+    touch $out
+  '';
+}
