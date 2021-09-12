f882fbcee0b076fd6f964643af9d5ae690c610c7
zimbatm <zimbatm@zimbatm.com>
2021-09-12 06:23:30 +0200

    lib.generators.toINI: serialize derivations to string
    This is the common case when passing a derivation, we want to access the
    store path.

--- lib/generators.nix
+++ lib/generators.nix
@@ -35,6 +35,8 @@ rec {
           ("generators.mkValueStringDefault: " +
            "${t} not supported: ${toPretty {} v}");
     in   if isInt      v then toString v
+    # convert derivations to store paths
+    else if lib.isDerivation v then toString v
     # we default to not quoting strings
     else if isString   v then v
     # isString returns "1", which is not a good default
