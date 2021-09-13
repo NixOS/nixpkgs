765dd20a6c29a9ce7d0691d25c0770986dcfc2df
Emily <vcs@emily.moe>
2021-09-13 04:20:37 +0100

    pv: 1.6.6 -> 1.6.20

--- pkgs/tools/misc/pv/default.nix
+++ pkgs/tools/misc/pv/default.nix
@@ -1,11 +1,11 @@
 { lib, stdenv, fetchurl } :
 
 stdenv.mkDerivation rec {
-  name = "pv-1.6.6";
+  name = "pv-1.6.20";
 
   src = fetchurl {
     url = "https://www.ivarch.com/programs/sources/${name}.tar.bz2";
-    sha256 = "1wbk14xh9rfypiwyy68ssl8dliyji30ly70qki1y2xx3ywszk3k0";
+    sha256 = "00y6zla8h653sn4axgqz7rr0x79vfwl62a7gn6lzn607zwg9acg8";
   };
 
   meta = {
