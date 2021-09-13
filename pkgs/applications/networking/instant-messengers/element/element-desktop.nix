c81983ec9f228b9b1405f492429a33cb3976201b
Maximilian Bosch <maximilian@mbosch.me>
2021-09-13 14:17:13 +0200

    element-desktop: 1.8.2 -> 1.8.4
    ChangeLog: https://github.com/vector-im/element-desktop/releases/tag/v1.8.4

--- pkgs/applications/networking/instant-messengers/element/element-desktop.nix
+++ pkgs/applications/networking/instant-messengers/element/element-desktop.nix
@@ -19,12 +19,12 @@
 
 let
   executableName = "element-desktop";
-  version = "1.8.2";
+  version = "1.8.4";
   src = fetchFromGitHub {
     owner = "vector-im";
     repo = "element-desktop";
     rev = "v${version}";
-    sha256 = "sha256-6DPMfx3LF45YWn2do02zDMLYZGBgBrOMJx3XBAO0ZyM=";
+    sha256 = "sha256-MmrO9Ref/qpW7ssjw8IAb7dYZHMRBfdfH2whsZJq/14=";
   };
   electron_exec = if stdenv.isDarwin then "${electron}/Applications/Electron.app/Contents/MacOS/Electron" else "${electron}/bin/electron";
 in
