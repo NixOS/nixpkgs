01bd54a0aa3f4f6ca23efbe0a9592fe578eefe16
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-09-14 13:54:10 +0200

    python3Packages: rename passthru test for openstack team

--- pkgs/development/python-modules/pbr/default.nix
+++ pkgs/development/python-modules/pbr/default.nix
@@ -20,7 +20,7 @@ buildPythonPackage rec {
   doCheck = false;
 
   passthru.tests = {
-    pytest = callPackage ./tests.nix { };
+    tests = callPackage ./tests.nix { };
   };
 
   pythonImportsCheck = [ "pbr" ];
