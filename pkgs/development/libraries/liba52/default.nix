70a24553d399df00d23f3390bbbf955af28624ca
Emily <vcs@emily.moe>
2021-09-14 22:33:41 +0100

    liba52: don't disable PIC
    I'm hoping this fixes the test failure on i686, though I haven't tested
    it yet.

--- pkgs/development/libraries/liba52/default.nix
+++ pkgs/development/libraries/liba52/default.nix
@@ -16,17 +16,15 @@ stdenv.mkDerivation rec {
     sha256 = "1ynxh4s1pdqjfirdy3j0kcbbl1pfl5sdkq2w91s2npsfh74jsc8x";
   };
 
+  patches = [
+    ./no-disable-pic.patch
+  ];
+
   makeFlags = [
     "AR=${stdenv.cc.targetPrefix}ar"
   ];
 
   nativeBuildInputs = [ autoreconfHook ];
-
-  # fails 1 out of 1 tests with "BAD GLOBAL SYMBOLS" on i686
-  # which can also be fixed with
-  # hardeningDisable = lib.optional stdenv.isi686 "pic";
-  # but it's better to disable tests than loose ASLR on i686
-  doCheck = !stdenv.isi686;
 
   meta = with lib; {
     description = "ATSC A/52 stream decoder";
