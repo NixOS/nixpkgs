e2cce9e0ca53578229099d08baf0b52794bbad4b
R. RyanTM <ryantm-bot@ryantm.com>
2021-09-14 23:32:37 +0000

    sniffglue: 0.13.0 -> 0.13.1

--- pkgs/tools/networking/sniffglue/default.nix
+++ pkgs/tools/networking/sniffglue/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "sniffglue";
-  version = "0.13.0";
+  version = "0.13.1";
 
   src = fetchFromGitHub {
     owner = "kpcyrd";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-UZLVkTkuJPzju9fl4N6Lgdn3gNBpKruNFxDcoTwdtYc=";
+    sha256 = "sha256-P8ubw523sw1O6Gpruy1Wa/Y0I3qJgvMdT53wBBoEGhE=";
   };
 
-  cargoSha256 = "sha256-bHwp0U83uvhFOi9oaeVwNrqHkxK2IQs/bvxqUl/rpjI=";
+  cargoSha256 = "sha256-+Wh+/C9LvYppCghv11e8NKNhiMy0SV4S3nEWA6b1hQk=";
 
   nativeBuildInputs = [ pkg-config ];
 
