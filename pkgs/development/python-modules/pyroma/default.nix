6b4002f4be8fe2e645101705d9da06af02da29d3
Sergei Trofimovich <slyich@gmail.com>
2021-09-14 21:39:18 +0100

    python37Packages.pyroma: backport upstream python-3.7 test fix
    Closes: https://github.com/NixOS/nixpkgs/issues/136901

--- pkgs/development/python-modules/pyroma/default.nix
+++ pkgs/development/python-modules/pyroma/default.nix
@@ -1,8 +1,10 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
+, fetchpatch
 , docutils
 , python
+, pythonOlder
 , pygments
 , setuptools
 , requests
@@ -18,6 +20,17 @@ buildPythonPackage rec {
     rev = version;
     sha256 = "0ln9w984n48nyxwzd1y48l6b18lnv52radcyizaw56lapcgxrzdr";
   };
+
+  patches = lib.optionals (pythonOlder "3.8") [
+    # upstream fix for python-3.7 test failures:
+    #  https://github.com/NixOS/nixpkgs/issues/136901
+    (fetchpatch {
+      name = "fix-py37-tests.patch";
+      url = "https://github.com/regebro/pyroma/commit/d30bc41da7d17a0737eb8ad2267457eff4cac82c.patch";
+      sha256 = "003vvp360cc05kas6bm6pdd6wgw4x3399sprr6pl7f654s730psq";
+      excludes = [ "HISTORY.txt" ];
+    })
+  ];
 
   propagatedBuildInputs = [
     docutils
