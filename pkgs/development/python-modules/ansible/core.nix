31f932af46864ad5b18d8837586b9e6b9e5f89a8
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-09-14 19:44:59 +0200

    ansible_2_11: 2.11.4 -> 2.11.5

--- pkgs/development/python-modules/ansible/core.nix
+++ pkgs/development/python-modules/ansible/core.nix
@@ -29,11 +29,11 @@ let
 in
 buildPythonPackage rec {
   pname = "ansible-core";
-  version = "2.11.4";
+  version = "2.11.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-Iuqnwt/myHXprjgDI/HLpiWcYFCl5MiBn4X5KzaD6kk=";
+    sha256 = "sha256-fTzkcBQSKQdFRwQ2NIXkhRP4rQ8AE4uIhw622IlT0SE=";
   };
 
   # ansible_connection is already wrapped, so don't pass it through
