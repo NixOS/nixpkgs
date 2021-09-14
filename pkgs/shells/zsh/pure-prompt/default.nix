82367d8543e8c3b703911e1a429896d54b805c84
R. RyanTM <ryantm-bot@ryantm.com>
2021-09-14 21:17:03 +0000

    pure-prompt: 1.17.1 -> 1.17.2

--- pkgs/shells/zsh/pure-prompt/default.nix
+++ pkgs/shells/zsh/pure-prompt/default.nix
@@ -4,13 +4,13 @@ with lib;
 
 stdenv.mkDerivation rec {
   pname = "pure-prompt";
-  version = "1.17.1";
+  version = "1.17.2";
 
   src = fetchFromGitHub {
     owner = "sindresorhus";
     repo = "pure";
     rev = "v${version}";
-    sha256 = "sha256-bWp04xT+/Xhgxj1Rm0FgTkRtLH9nuSFtqBsO3B7Exvo=";
+    sha256 = "sha256-sk7uAYVw/9OLKi3ClBXwEEnfaiprM/gQcNJMDFGqhE0=";
   };
 
   installPhase = ''
