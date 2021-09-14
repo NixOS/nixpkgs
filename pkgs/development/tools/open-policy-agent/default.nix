9c81cbace1a74dc599af642f7e9e87954a184114
R. RyanTM <ryantm-bot@ryantm.com>
2021-09-14 18:15:03 +0000

    open-policy-agent: 0.31.0 -> 0.32.0

--- pkgs/development/tools/open-policy-agent/default.nix
+++ pkgs/development/tools/open-policy-agent/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "open-policy-agent";
-  version = "0.31.0";
+  version = "0.32.0";
 
   src = fetchFromGitHub {
     owner = "open-policy-agent";
     repo = "opa";
     rev = "v${version}";
-    sha256 = "sha256-SUMTyqzeaCTj/fVdDHFZkh43t+mIFGNt6wendOt9gfw=";
+    sha256 = "sha256-RCB9PSUvfIqDAuZFw8HAF35ZhWwTiS+dAHgwlLPwc9I=";
   };
   vendorSha256 = null;
 
