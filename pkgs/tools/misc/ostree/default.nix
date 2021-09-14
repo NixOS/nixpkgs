5683d7f6fa7d27b3153c6cfd584a169aff0a3a6e
R. RyanTM <ryantm-bot@ryantm.com>
2021-09-14 19:18:20 +0000

    ostree: 2021.3 -> 2021.4

--- pkgs/tools/misc/ostree/default.nix
+++ pkgs/tools/misc/ostree/default.nix
@@ -41,13 +41,13 @@ let
   ]));
 in stdenv.mkDerivation rec {
   pname = "ostree";
-  version = "2021.3";
+  version = "2021.4";
 
   outputs = [ "out" "dev" "man" "installedTests" ];
 
   src = fetchurl {
     url = "https://github.com/ostreedev/ostree/releases/download/v${version}/libostree-${version}.tar.xz";
-    sha256 = "sha256-D6Wjnww+WMIEATPkIpyyhmDGG5eM1KKj0vbpfvTI0LM=";
+    sha256 = "sha256-21zgGhYGrzRm7HcjZwD1OmPOIOSYSlSdDquj9mo9k2E=";
   };
 
   patches = [
