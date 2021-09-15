1dcf36a2539c0493315ff50aea2b0c1c38e4c707
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-15 09:30:12 -0300

    Create RouterBrazil.py

---
+++ pkgs/Router/Brasil/RouterBrazil.py
@@ -0,0 +1,13 @@
+Import mundi
+ Export to onucloud
+
+Const mundi = ('/mundi.py')
+ New router = mapamundi Brazil.router.mundi
+  Brazil.router.mundi = new file(router.brl.py)
+
+Const router.brl = ('/router.brl.py')
+  Import to DRF and DPF and DPC 
+ 
+Export to Monaco
+         
+
