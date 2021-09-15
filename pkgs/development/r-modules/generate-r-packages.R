29fdc0ce2d98641fc1ae0ce3057dbc7fd1bdd3d6
Phil Dyer <phildyer@protonmail.com>
2021-09-15 11:09:17 +1000

    rPackages: autoset bioconductor ver to match R ver
    BioConductor releases are tied to specific R releases.
    
    Updating the package set now automatically selects the
    correct version of bioconductor to match the current version of
    R.

--- pkgs/development/r-modules/generate-r-packages.R
+++ pkgs/development/r-modules/generate-r-packages.R
@@ -1,9 +1,16 @@
 #!/usr/bin/env Rscript
 library(data.table)
 library(parallel)
+library(BiocManager)
 cl <- makeCluster(10)
 
-biocVersion <- 3.12
+biocVersion <- BiocManager:::.version_map()
+biocVersion <- biocVersion[biocVersion$R == getRversion()[, 1:2],c("Bioc", "BiocStatus")]
+if ("release" %in% biocVersion$BiocStatus) {
+  biocVersion <-  as.numeric(as.character(biocVersion[biocVersion$BiocStatus == "release", "Bioc"]))
+} else {
+  biocVersion <-  max(as.numeric(as.character(biocVersion$Bioc)))
+}
 snapshotDate <- Sys.Date()-1
 
 mirrorUrls <- list( bioc=paste0("http://bioconductor.statistik.tu-dortmund.de/packages/", biocVersion, "/bioc/src/contrib/")
