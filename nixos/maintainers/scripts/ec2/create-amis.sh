5e53b98e6553b6f2307ea326778bb49f0eb276a9
Timothy DeHerrera <tim.deh@pm.me>
2021-09-14 18:29:18 -0600

    create-amis.sh: make vars overridable from env
    (cherry picked from commit 0ffd7d80cab0e5e3e152d678e0107be3438cfda8)

--- nixos/maintainers/scripts/ec2/create-amis.sh
+++ nixos/maintainers/scripts/ec2/create-amis.sh
@@ -15,18 +15,22 @@
 # set -x
 set -euo pipefail
 
-# configuration
-state_dir=$HOME/amis/ec2-images
-home_region=eu-west-1
-bucket=nixos-amis
-service_role_name=vmimport
+var () { true; }
 
-regions=(eu-west-1 eu-west-2 eu-west-3 eu-central-1 eu-north-1
+# configuration
+var ${state_dir:=$HOME/amis/ec2-images}
+var ${home_region:=eu-west-1}
+var ${bucket:=nixos-amis}
+var ${service_role_name:=vmimport}
+
+var ${regions:=eu-west-1 eu-west-2 eu-west-3 eu-central-1 eu-north-1
          us-east-1 us-east-2 us-west-1 us-west-2
          ca-central-1
          ap-southeast-1 ap-southeast-2 ap-northeast-1 ap-northeast-2
          ap-south-1 ap-east-1
-         sa-east-1)
+         sa-east-1}
+
+regions=($regions)
 
 log() {
     echo "$@" >&2
