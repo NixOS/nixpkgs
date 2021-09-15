c9b0ede4483458a6d5fa383b3ebfc78b7af886ea
Fabian Affolter <mail@fabian-affolter.ch>
2021-09-15 21:05:01 +0200

    python3Packages.velbus-aio: init at 2021.9.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9304,6 +9304,8 @@ in {
 
   venusian = callPackage ../development/python-modules/venusian { };
 
+  velbus-aio = callPackage ../development/python-modules/velbus-aio { };
+
   verboselogs = callPackage ../development/python-modules/verboselogs { };
 
   versioneer = callPackage ../development/python-modules/versioneer { };
