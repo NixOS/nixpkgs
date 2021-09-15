11ca2ab73618ff16ef3529a6565e71490fb1a6a2
Tim Steinbach <tim@nequissimus.com>
2021-09-15 04:51:44 -0400

    linux: 5.4.145 -> 5.4.146

--- pkgs/os-specific/linux/kernel/linux-5.4.nix
+++ pkgs/os-specific/linux/kernel/linux-5.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.4.145";
+  version = "5.4.146";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1yb8vk5sbnyswylkpqw5i4n9cmnmlrfmbrnmy3nif579q8p7ixsw";
+    sha256 = "1bjkfk24j86akm6df3c5b28viyq363drhssml7rcrl69286f92dk";
   };
 } // (args.argsOverride or {}))
