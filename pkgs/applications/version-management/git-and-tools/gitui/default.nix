964b74f6b47b0ffb82d59856eaf527b3b1a8c129
happysalada <raphael@megzari.com>
2021-09-15 09:47:17 +0900

    gitui: do not compile openssl

--- pkgs/applications/version-management/git-and-tools/gitui/default.nix
+++ pkgs/applications/version-management/git-and-tools/gitui/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, rustPlatform, fetchFromGitHub, libiconv, perl, python3, Security, AppKit, openssl, xclip }:
+{ lib, stdenv, rustPlatform, fetchFromGitHub, libiconv, perl, python3, Security, AppKit, openssl, xclip, pkg-config }:
 rustPlatform.buildRustPackage rec {
   pname = "gitui";
   version = "0.17.1";
@@ -12,10 +12,13 @@ rustPlatform.buildRustPackage rec {
 
   cargoSha256 = "sha256-9uRvxax0SrvRkD89mbZPxsfRItde11joA/ZgnXK9XBE=";
 
-  nativeBuildInputs = [ python3 perl ];
+  nativeBuildInputs = [ python3 perl pkg-config ];
   buildInputs = [ openssl ]
     ++ lib.optional stdenv.isLinux xclip
     ++ lib.optionals stdenv.isDarwin [ libiconv Security AppKit ];
+
+  # Needed to get openssl-sys to use pkg-config.
+  OPENSSL_NO_VENDOR = 1;
 
   meta = with lib; {
     description = "Blazing fast terminal-ui for git written in rust";
