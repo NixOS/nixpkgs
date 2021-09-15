fa9c5db3ed80dd9c7ca7d3aa6a500facc11667e3
AndersonTorres <torres.anderson.85@protonmail.com>
2021-09-15 00:58:49 -0300

    emacsPackages.isearch-prop: use trivialBuild

--- pkgs/applications/editors/emacs/elisp-packages/isearch-prop/default.nix
+++ pkgs/applications/editors/emacs/elisp-packages/isearch-prop/default.nix
@@ -1,10 +1,10 @@
 { lib
 , stdenv
-, fetchFromGitHub
+, trivialBuild
 , emacs
 }:
 
-stdenv.mkDerivation {
+trivialBuild {
   pname = "isearch-prop";
   version = "0.0.0+unstable=2019-05-01";
 
@@ -15,28 +15,11 @@ stdenv.mkDerivation {
     hash = "sha256-A1Kt4nm7iRV9J5yaLupwiNL5g7ddZvQs79dggmqZ7Rk=";
   };
 
-  buildInputs = [
-    emacs
-  ];
-
-  buildPhase = ''
-    runHook preBuild
-    emacs -L . --batch -f batch-byte-compile *.el
-    runHook postBuild
-  '';
-
-  installPhase = ''
-    runHook preInstall
-    install -d $out/share/emacs/site-lisp
-    install *.el *.elc $out/share/emacs/site-lisp
-    runHook postInstall
-  '';
-
   meta = with lib; {
     homepage = "https://www.emacswiki.org/emacs/IsearchPlus";
     description = "Search text- or overlay-property contexts";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ leungbk ];
-    platforms = emacs.meta.platforms;
+    inherit (emacs.meta) platforms;
   };
 }
