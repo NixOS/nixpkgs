94683a072a6f7fc488e76f5cbf835bf80af1f117
Aaron Andersen <aaron@fosslib.net>
2021-09-16 09:35:38 -0400

    zabbix50: 5.0.12 -> 5.0.15

--- pkgs/servers/monitoring/zabbix/versions.nix
+++ pkgs/servers/monitoring/zabbix/versions.nix
@@ -1,7 +1,7 @@
 generic: {
   v50 = generic {
-    version = "5.0.12";
-    sha256 = "1gxlsmvz3m3dm4qqgx5qg5zjs680salah17mdzm3m27w8srg13i5";
+    version = "5.0.15";
+    sha256 = "1q9xwixzdj3w9qmarpp8xbkjphsz4m185br8g6i9f96676hrw5ww";
   };
 
   v40 = generic {
