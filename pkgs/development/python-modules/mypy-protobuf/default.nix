8ae61d08be82ddf2cdf50d4f38edec4d608cddda
Anders Kaseorg <andersk@mit.edu>
2021-09-16 15:29:19 -0700

    python3Packages.mypy-protobuf: Add missing build inputs
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- pkgs/development/python-modules/mypy-protobuf/default.nix
+++ pkgs/development/python-modules/mypy-protobuf/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchPypi, buildPythonApplication, protobuf, pythonOlder }:
+{ lib, fetchPypi, buildPythonApplication, protobuf, types-protobuf, grpcio-tools, pythonOlder }:
 
 buildPythonApplication rec {
   pname = "mypy-protobuf";
@@ -11,7 +11,7 @@ buildPythonApplication rec {
     sha256 = "278172935d7121c2f8c7c0a05518dd565a2b76d9e9c4a0a3fcd08a21fa685d43";
   };
 
-  propagatedBuildInputs = [ protobuf ];
+  propagatedBuildInputs = [ protobuf types-protobuf grpcio-tools ];
 
   meta = with lib; {
     description = "Generate mypy stub files from protobuf specs";
