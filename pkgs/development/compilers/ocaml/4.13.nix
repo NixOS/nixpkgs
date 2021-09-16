f837a27a8a7ae6e6bfeedc74fa6e39aea7a11f5d
Vincent Laporte <Vincent.Laporte@gmail.com>
2021-09-16 11:27:43 +0200

    ocaml-ng.ocamlPackages_4_13.ocaml: 4.13.0-rc1 → 4.13.0-rc2

--- pkgs/development/compilers/ocaml/4.13.nix
+++ pkgs/development/compilers/ocaml/4.13.nix
@@ -1,9 +1,9 @@
 import ./generic.nix {
   major_version = "4";
   minor_version = "13";
-  patch_version = "0-rc1";
+  patch_version = "0-rc2";
   src = fetchTarball {
-    url = "https://caml.inria.fr/pub/distrib/ocaml-4.13/ocaml-4.13.0~rc1.tar.xz";
-    sha256 = "0vp19qwdny5z428yjvdn0yxvf3i5l23axjb83y5ccj0rpza1k0im";
+    url = "https://caml.inria.fr/pub/distrib/ocaml-4.13/ocaml-4.13.0~rc2.tar.xz";
+    sha256 = "1w4sdrs5s1bhbisgz44ysi2j1n13qd3slgs34ppglpwmqqw6ply2";
   };
 }
