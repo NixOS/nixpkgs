ecfeb4d4c323278e9cd278975b4e0a8569966f6a
R. RyanTM <ryantm-bot@ryantm.com>
2021-09-16 05:22:09 +0000

    chezmoi: 2.1.6 -> 2.4.0

--- pkgs/tools/misc/chezmoi/default.nix
+++ pkgs/tools/misc/chezmoi/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "chezmoi";
-  version = "2.1.6";
+  version = "2.4.0";
 
   src = fetchFromGitHub {
     owner = "twpayne";
     repo = "chezmoi";
     rev = "v${version}";
-    sha256 = "sha256-6BzocbG7I3ZHlPGZ2GRY/n9ezRF2OER9BFaoIq0yBro=";
+    sha256 = "sha256-eSAkm7BQHpE/DaYG4WOPqrlAqlSnPzn9RlxkH43Yle0=";
   };
 
-  vendorSha256 = "sha256-i20Zt1ZP1ij1Qp4moNZqUTQOHPlchonFF7ag5qjMoqg=";
+  vendorSha256 = "sha256-cwIPytFgakTMMkM15lIc6PUknzHdtsm3AH3jpO7qvlg=";
 
   doCheck = false;
 
