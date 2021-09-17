cd28312313dff2ef8a2458d9405f8db2b0a23382
Vincent Laporte <Vincent.Laporte@gmail.com>
2021-09-17 22:40:07 +0200

    coqPackages.flocq: 3.3.1 → 3.4.2

--- pkgs/development/coq-modules/flocq/default.nix
+++ pkgs/development/coq-modules/flocq/default.nix
@@ -7,9 +7,10 @@ with lib; mkCoqDerivation {
   domain = "gitlab.inria.fr";
   inherit version;
   defaultVersion = with versions; switch coq.coq-version [
-    { case = isGe "8.7";        out = "3.3.1"; }
+    { case = range "8.7" "8.14"; out = "3.4.2"; }
     { case = range "8.5" "8.8"; out = "2.6.1"; }
   ] null;
+  release."3.4.2".sha256 = "1s37hvxyffx8ccc8mg5aba7ivfc39p216iibvd7f2cb9lniqk1pw";
   release."3.3.1".sha256 = "1mk8adhi5hrllsr0hamzk91vf2405sjr4lh5brg9201mcw11abkz";
   release."2.6.1".sha256 = "0q5a038ww5dn72yvwn5298d3ridkcngb1dik8hdyr3xh7gr5qibj";
   releaseRev = v: "flocq-${v}";
