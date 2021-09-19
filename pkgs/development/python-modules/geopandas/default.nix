7e6325efd413bd72df9b9a89af9669f843120b11
Robert Schütz <nix@dotlambda.de>
2021-09-19 08:50:21 -0700

    python3Packages.geopandas: fix tests

--- pkgs/development/python-modules/geopandas/default.nix
+++ pkgs/development/python-modules/geopandas/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, buildPythonPackage, fetchFromGitHub, pythonOlder
+{ lib, stdenv, buildPythonPackage, fetchFromGitHub, fetchpatch, pythonOlder
 , pandas, shapely, fiona, pyproj
 , pytestCheckHook, Rtree }:
 
@@ -13,6 +13,19 @@ buildPythonPackage rec {
     rev = "v${version}";
     sha256 = "sha256-58X562OkRzZ4UTNMTwXW4U5czoa5tbSMBCcE90DqbaE=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "skip-pandas-master-fillna-test.patch";
+      url = "https://github.com/geopandas/geopandas/pull/1878.patch";
+      sha256 = "1yw3i4dbhaq7f02n329b9y2cqxbwlz9db81mhgrfc7af3whwysdb";
+    })
+    (fetchpatch {
+      name = "fix-proj4strings-test.patch";
+      url = "https://github.com/geopandas/geopandas/pull/1958.patch";
+      sha256 = "0kzmpq5ry87yvhqr6gnh9p2606b06d3ynzjvw0hpp9fncczpc2yn";
+    })
+  ];
 
   propagatedBuildInputs = [
     pandas
