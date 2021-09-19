ea71d92e4e1055cd29536e9c1747712cd3fe4162
Harold Leboulanger <hleboulanger@protonmail.com>
2021-09-19 13:44:39 +0200

    maintainers: add hleboulanger

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4441,6 +4441,12 @@
       fingerprint = "D618 7A03 A40A 3D56 62F5  4B46 03EF BF83 9A5F DC15";
     }];
   };
+  hleboulanger = {
+    email = "hleboulanger@protonmail.com";
+    name = "Harold Leboulanger";
+    github = "thbkrhsw";
+    githubId = 33122;
+  };
   hlolli = {
     email = "hlolli@gmail.com";
     github = "hlolli";
