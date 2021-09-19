da0426a3b385f74878dea39d36f6f0694999cd64
(cdep)illabout <cdep.illabout@gmail.com>
2021-09-19 09:29:56 +0900

    haskell.compiler.ghcjs: mark hydraPlatforms as none because output is too large
    (cherry picked from commit 3389aab889719081e240ce169ec5bc0d5ccd60d0)

--- pkgs/development/compilers/ghcjs/8.10/default.nix
+++ pkgs/development/compilers/ghcjs/8.10/default.nix
@@ -108,7 +108,15 @@ in stdenv.mkDerivation {
 
     inherit passthru;
 
-    # The emscripten is broken on darwin
-    meta.platforms = lib.platforms.linux;
-    meta.maintainers = with lib.maintainers; [ obsidian-systems-maintenance ];
+    meta = {
+      # The emscripten is broken on darwin
+      platforms = lib.platforms.linux;
+
+      # Hydra limits jobs to only outputting 1 gigabyte worth of files.
+      # GHCJS outputs over 3 gigabytes.
+      # https://github.com/NixOS/nixpkgs/pull/137066#issuecomment-922335563
+      hydraPlatforms = lib.platforms.none;
+
+      maintainers = with lib.maintainers; [ obsidian-systems-maintenance ];
+    };
   }
