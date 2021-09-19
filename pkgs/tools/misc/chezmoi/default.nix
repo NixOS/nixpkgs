d5cef0b6b06b9b6bfe59f7a4322f2464b48a345b
James Hillyerd <james@hillyerd.com>
2021-09-19 15:14:50 -0700

    chezmoi: 2.1.6 -> 2.2.0

--- pkgs/tools/misc/chezmoi/default.nix
+++ pkgs/tools/misc/chezmoi/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "chezmoi";
-  version = "2.1.6";
+  version = "2.2.0";
 
   src = fetchFromGitHub {
     owner = "twpayne";
     repo = "chezmoi";
     rev = "v${version}";
-    sha256 = "sha256-6BzocbG7I3ZHlPGZ2GRY/n9ezRF2OER9BFaoIq0yBro=";
+    sha256 = "sha256-Iv/SuuE4LMTe+UlOG3iSyWjaN7A4pkSeCk/rYnMu7qc=";
   };
 
-  vendorSha256 = "sha256-i20Zt1ZP1ij1Qp4moNZqUTQOHPlchonFF7ag5qjMoqg=";
+  vendorSha256 = "sha256-ECdsuKvBVyzRo9XviVldHnD4nND9P1v4esLz0+L+c7o=";
 
   doCheck = false;
 
