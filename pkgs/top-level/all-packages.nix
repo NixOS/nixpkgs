ac78ae80a43c9ab27a434c3d1c62176cf93b5296
Robert Hensing <robert@roberthensing.nl>
2021-09-19 20:23:52 +0200

    invalidateFetcherByDrvHash move docs to manual

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -614,35 +614,8 @@ with pkgs;
 
   installShellFiles = callPackage ../build-support/install-shell-files {};
 
-  /*
-    Use the derivation hash to invalidate the output via name, for testing
-    purposes.
-
-    Type: (a@{ name, ... } -> Derivation) -> a -> Derivation
-
-    Normally, fixed output derivations can and should be cached by their output
-    hash only, but for testing we want to re-fetch everytime the fetcher changes.
-
-    Changes to the fetcher become apparent in the drvPath, which is a hash of
-    how to fetch, rather than a fixed store path.
-    By inserting this hash into the name, we can make sure to re-run the fetcher
-    every time the fetcher changes.
-
-    This relies on the assumption that Nix isn't clever enough to reuse its
-    database of local store contents to optimize fetching.
-
-    Note that the "salt" derives from a different drv than the final fetcher
-    drv. This is necessary, because it can not be defined recursively.
-
-    Example:
-
-      tests.fetchgit = invalidateFetcherByDrvHash fetchgit {
-        name = "nix-source";
-        url = "https://github.com/NixOS/nix";
-        rev = "9d9dbe6ed05854e03811c361a3380e09183f4f4a";
-        sha256 = "sha256-7DszvbCNTjpzGRmpIVAWXk20P0/XTrWZ79KSOGLrUWY=";
-      };
-  */
+  # See doc/builders/special/invalidateFetcherByDrvHash.section.md or
+  # https://nixos.org/manual/nixpkgs/unstable/#sec-pkgs-invalidateFetcherByDrvHash
   invalidateFetcherByDrvHash = f: args:
     let
       drvPath = (f args).drvPath;
