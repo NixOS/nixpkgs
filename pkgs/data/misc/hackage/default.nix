253a88f39355ee63a33e5b2d92ca1d5d188bfd3c
sternenseemann <sternenseemann@systemli.org>
2021-09-20 18:29:02 +0200

    maintainers/haskell: unpack hackage data in all-cabal-hashes

--- pkgs/data/misc/hackage/default.nix
+++ pkgs/data/misc/hackage/default.nix
@@ -1,10 +1,15 @@
 # Hackage database snapshot, used by maintainers/scripts/regenerate-hackage-packages.sh
 # and callHackage
-{ fetchurl }:
+{ fetchurl, runCommand, all-cabal-hashes }:
 let
   pin = builtins.fromJSON (builtins.readFile ./pin.json);
 in
 fetchurl {
   inherit (pin) url sha256;
-  passthru.updateScript = ../../../../maintainers/scripts/haskell/update-hackage.sh;
+  passthru = {
+    updateScript = ../../../../maintainers/scripts/haskell/update-hackage.sh;
+    unpacked = runCommand "unpacked-cabal-hashes" {} ''
+      tar xf ${all-cabal-hashes} --strip-components=1 --one-top-level=$out
+    '';
+  };
 }
