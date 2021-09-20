76bdc20f22a0586a31f0d80ec75c5acf9e083145
TredwellGit <tredwell@tutanota.com>
2021-09-20 21:40:12 +0000

    linux: 5.14.5 -> 5.14.6
    (cherry picked from commit 51b0e44980a4939b21a6431d652169426840a335)

--- pkgs/os-specific/linux/kernel/linux-5.14.nix
+++ pkgs/os-specific/linux/kernel/linux-5.14.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.14.5";
+  version = "5.14.6";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1zbcai5q1b5hjc3xhg05pghj5iyb0xsvrqyrazzmhrfbsqf8s5k5";
+    sha256 = "1v6cicakpg0fdp74r2a4h46sj7rzpjdf4cscbr8y67kpd098r12l";
   };
 } // (args.argsOverride or { }))
