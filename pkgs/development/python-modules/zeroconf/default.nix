2dfbd6499943c11ae5548a465fe1c663720ee510
Fabian Affolter <mail@fabian-affolter.ch>
2021-09-20 21:58:22 +0200

    python3Packages.zeroconf: 0.36.5 -> 0.36.6

--- pkgs/development/python-modules/zeroconf/default.nix
+++ pkgs/development/python-modules/zeroconf/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "zeroconf";
-  version = "0.36.5";
+  version = "0.36.6";
   format = "setuptools";
   disabled = pythonOlder "3.6";
 
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "jstasiak";
     repo = "python-zeroconf";
     rev = version;
-    sha256 = "sha256-FMiSxUj+RTJe0vvYzyRTXhuDal/Dnx61pb0rUCbvFS8=";
+    sha256 = "sha256-Ignbms6/M36cK1fwm2ejMPDkYrANmQ7CcSqM+ISoZig=";
   };
 
   propagatedBuildInputs = [
