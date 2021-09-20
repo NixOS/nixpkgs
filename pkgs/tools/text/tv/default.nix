0cd345bcc470627d057eb94afa5a470fbb24830a
figsoda <figsoda@pm.me>
2021-09-20 15:12:25 -0400

    tv: 0.5.3 -> 0.6.0

--- pkgs/tools/text/tv/default.nix
+++ pkgs/tools/text/tv/default.nix
@@ -2,20 +2,21 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "tv";
-  version = "0.5.3";
+  version = "0.6.0";
 
   src = fetchFromGitHub {
     owner = "uzimaru0000";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-mh/+MX0MZM1fsi9HGTioRRH1DVatmkdyiwAgG/42cVU=";
+    sha256 = "sha256-4PcD0keG3OVZPv6MA+rNSL9lysrseJUA6C5cd2f6LRY=";
   };
 
-  cargoSha256 = "sha256-8uxW0EIeMPvgffYW55Ov1euoVi8Zz9fZ4F44ktxvj9Q=";
+  cargoSha256 = "sha256-E4qMxCqgJYIA8E6A0d8iUYTbKif5T51zcFdc+Ptq7qc=";
 
   meta = with lib; {
     description = "Format json into table view";
     homepage = "https://github.com/uzimaru0000/tv";
+    changelog = "https://github.com/uzimaru0000/tv/blob/v${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ figsoda ];
   };
