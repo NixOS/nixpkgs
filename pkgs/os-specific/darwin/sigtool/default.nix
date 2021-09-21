bf060b81900ac51c7fa7df8c50a005ed18ba1584
Michael Stone <m@mstone.info>
2021-09-21 11:18:17 -0400

    sigtool: 2a13539d -> 0.1.0

--- pkgs/os-specific/darwin/sigtool/default.nix
+++ pkgs/os-specific/darwin/sigtool/default.nix
@@ -1,13 +1,14 @@
 { lib, stdenv, fetchFromGitHub, pkg-config, openssl }:
 
-stdenv.mkDerivation {
-  name = "sigtool";
+stdenv.mkDerivation rec {
+  pname = "sigtool";
+  version = "0.1.0";
 
   src = fetchFromGitHub {
     owner = "thefloweringash";
     repo = "sigtool";
-    rev = "2a13539dc4893f39412a3fb810afc78b183df3df";
-    sha256 = "sha256-iCsdklN3crFx6CKsMIUP/fA3twLh4ArQh7OsVug5UjE=";
+    rev = "refs/tags/v${version}";
+    sha256 = "sha256-GSVkF1BkZEkIF+q0SsoCuE0q48DRclE3E+qKc6JEc/0=";
   };
 
   nativeBuildInputs = [ pkg-config ];
