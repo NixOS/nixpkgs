0ebf740d2d641d3b28c116c361ebfa47b8f1cbef
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2021-09-21 10:46:30 +0800

    libfabric: platforms = platforms.all

--- pkgs/development/libraries/libfabric/default.nix
+++ pkgs/development/libraries/libfabric/default.nix
@@ -1,4 +1,5 @@
-{ lib, stdenv, fetchFromGitHub, pkg-config, autoreconfHook, libpsm2 }:
+{ lib, stdenv, fetchFromGitHub, pkg-config, autoreconfHook, libpsm2
+, enablePsm2 ? (stdenv.isx86_64 && stdenv.isLinux) }:
 
 stdenv.mkDerivation rec {
   pname = "libfabric";
@@ -13,17 +14,17 @@ stdenv.mkDerivation rec {
     sha256 = "0USQMBXZrbz4GtXLNsSti9ohUOqqo0OCtVz+0Uk9ndI=";
   };
 
-  nativeBuildInputs = [ pkg-config autoreconfHook ] ;
+  nativeBuildInputs = [ pkg-config autoreconfHook ];
 
-  buildInputs = [ libpsm2 ] ;
+  buildInputs = lib.optional enablePsm2 libpsm2;
 
-  configureFlags = [ "--enable-psm2=${libpsm2}" ] ;
+  configureFlags = [ (if enablePsm2 then "--enable-psm2=${libpsm2}" else "--disable-psm2") ];
 
   meta = with lib; {
     homepage = "https://ofiwg.github.io/libfabric/";
     description = "Open Fabric Interfaces";
     license = with licenses; [ gpl2 bsd2 ];
-    platforms = [ "x86_64-linux" ];
+    platforms = platforms.all;
     maintainers = [ maintainers.bzizou ];
   };
 }
