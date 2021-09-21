f661c05a11fe2984ecf8e2c50274ad58f9e301a6
Sumner Evans <me@sumnerevans.com>
2021-09-21 13:55:45 -0600

    heisenbridge: 1.1.1 -> 1.1.2

--- pkgs/servers/heisenbridge/default.nix
+++ pkgs/servers/heisenbridge/default.nix
@@ -2,13 +2,13 @@
 
 python3Packages.buildPythonPackage rec {
   pname = "heisenbridge";
-  version = "1.1.1";
+  version = "1.1.2";
 
   # Use the release tarball because it has the version set correctly using the
   # version.txt file.
   src = fetchurl {
-    url = "https://github.com/hifi/heisenbridge/releases/download/v${version}/heisenbridge-v${version}.tar.gz";
-    sha256 = "sha256-thI3NYYnLHmlfs5mmH2SQcREBLWtnilYlxriKWnamPM=";
+    url = "https://github.com/hifi/heisenbridge/releases/download/v${version}/heisenbridge-${version}.tar.gz";
+    sha256 = "sha256-hY0dB4QT9W18ubUytvJwRUWKpNQMpyYdSLbmu+c8BCo=";
   };
 
   propagatedBuildInputs = with python3Packages; [
@@ -16,8 +16,6 @@ python3Packages.buildPythonPackage rec {
     irc
     pyyaml
   ];
-
-  checkInputs = [ python3Packages.pytestCheckHook ];
 
   meta = with lib; {
     description = "A bouncer-style Matrix-IRC bridge.";
