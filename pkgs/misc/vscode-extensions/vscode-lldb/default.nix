f3db53c98e317e0a65c83258be9dbc8b8bd1866a
oxalica <oxalicc@pm.me>
2021-09-22 00:27:23 +0800

    vscode-extensions.vscode-lldb: 1.6.5 -> 1.6.7

--- pkgs/misc/vscode-extensions/vscode-lldb/default.nix
+++ pkgs/misc/vscode-extensions/vscode-lldb/default.nix
@@ -5,7 +5,7 @@ assert lib.versionAtLeast python3.version "3.5";
 let
   publisher = "vadimcn";
   pname = "vscode-lldb";
-  version = "1.6.5";
+  version = "1.6.7";
 
   vscodeExtUniqueId = "${publisher}.${pname}";
 
@@ -13,7 +13,7 @@ let
     owner = "vadimcn";
     repo = "vscode-lldb";
     rev = "v${version}";
-    sha256 = "sha256-ppiEWFKJiUtlF8LSqBb8Xvg26B+wHcIZJhU+ANE4J2k=";
+    sha256 = "sha256-9rqdqpxUWcUV9RnZOTxg+zMW7wlTXZVkoKYHuv/lE7c=";
   };
 
   lldb = callPackage ./lldb.nix {};
@@ -25,7 +25,7 @@ let
     # It will pollute the build environment of `buildRustPackage`.
     cargoPatches = [ ./reset-cargo-config.patch ];
 
-    cargoSha256 = "sha256-ksRFlbtrFAbcX/Pc6rgWUHVl859GVUOvNckxM7Q971U=";
+    cargoSha256 = "sha256-KeZpjMCBdOJTLj8pA5WWi3EMyhhWw/+aik4IJqIs/mk=";
 
     nativeBuildInputs = [ makeWrapper ];
 
