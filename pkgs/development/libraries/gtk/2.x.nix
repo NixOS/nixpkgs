44b93146951024508371ad083d621ab9d562f8c3
Jan Tojnar <jtojnar@gmail.com>
2021-09-21 13:20:46 +0200

    gtk2: 2.24.32 → 2.24.33
    Should improve MacOS compat.
    
    https://gitlab.gnome.org/GNOME/gtk/-/commit/68631945733158f164427db84f01301d7e875763

--- pkgs/development/libraries/gtk/2.x.nix
+++ pkgs/development/libraries/gtk/2.x.nix
@@ -21,11 +21,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "gtk+";
-  version = "2.24.32";
+  version = "2.24.33";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gtk+/2.24/${pname}-${version}.tar.xz";
-    sha256 = "b6c8a93ddda5eabe3bfee1eb39636c9a03d2a56c7b62828b359bf197943c582e";
+    sha256 = "rCrHV/WULTGKMRpUsMgLXvKV8pnCpzxjL2v7H/Scxto=";
   };
 
   outputs = [ "out" "dev" "devdoc" ];
