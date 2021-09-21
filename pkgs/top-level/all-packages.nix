5e94e24b14a4cfb02b7ca1ec3b0141c4a8a4b534
figsoda <figsoda@pm.me>
2021-09-21 08:01:11 -0400

    hors: fix darwin build

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24888,7 +24888,9 @@ with pkgs;
     gtk = gtk3;
   };
 
-  hors = callPackage ../development/tools/hors { };
+  hors = callPackage ../development/tools/hors {
+    inherit (darwin.apple_sdk.frameworks) Security;
+  };
 
   hover = callPackage ../development/tools/hover { };
 
