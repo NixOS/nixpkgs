5d3f726f0bab0fc367b30f6183a80542bdc5bd84
R. RyanTM <ryantm-bot@ryantm.com>
2021-09-22 01:24:03 +0000

    yq-go: 4.13.0 -> 4.13.2

--- pkgs/development/tools/yq-go/default.nix
+++ pkgs/development/tools/yq-go/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "yq-go";
-  version = "4.13.0";
+  version = "4.13.2";
 
   src = fetchFromGitHub {
     owner = "mikefarah";
     repo = "yq";
     rev = "v${version}";
-    sha256 = "sha256-9yKCFEtGg6TiHo3PGobVzQSbqc27ptKhH+2WgbLKXRI=";
+    sha256 = "sha256-manTuR7/3FE+q08WTVAtKilPCQBK136O8w1r5OX9T08=";
   };
 
   vendorSha256 = "sha256-u7elWOW/tz1ISM/KC1njkZmPi8AEEssZ5QtxK/+1/1I=";
