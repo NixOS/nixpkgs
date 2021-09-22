c71894f17eae29e3e1dfd49f9615bb947100e557
Simarra <loic.martel@protonmail.com>
2021-09-22 12:45:27 +0200

    cozy-drive: init. Fix maintenair name

--- pkgs/applications/networking/cozy-drive/default.nix
+++ pkgs/applications/networking/cozy-drive/default.nix
@@ -19,8 +19,8 @@ appimageTools.wrapType2 {
   inherit name src;
   extraInstallCommands = ''
     mv $out/bin/${name} $out/bin/${pname}
-    install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
-    substituteInPlace $out/share/applications/${pname}.desktop \
+    install -m 444 -D ${appimageContents}/cozydrive.desktop -t $out/share/applications
+    substituteInPlace $out/share/applications/cozydrive.desktop \
       --replace 'Exec=AppRun' 'Exec=${pname}'
     cp -r ${appimageContents}/usr/share/icons $out/share
   '';
@@ -29,7 +29,7 @@ appimageTools.wrapType2 {
     description = "Cozy Drive is a synchronization tool for your files and folders with Cozy Cloud.";
     homepage = "https://cozy.io";
     license = licenses.gpl3Only;
-    maintainers = with maintainers; [ Simarra ];
+    maintainers = with maintainers; [ "Simarra" ];
     platforms = [ "x86_64-linux" ];
   };
 }
