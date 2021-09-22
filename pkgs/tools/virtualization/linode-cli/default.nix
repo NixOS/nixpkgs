80ea69bdc8b45f0b0f7d22a2beca539d6d384d8f
superherointj <5861043+superherointj@users.noreply.github.com>
2021-09-22 16:41:14 -0300

    linode-cli: add updateScript (#138547)
    * linode-cli: add updateScript
    
    * Update pkgs/tools/virtualization/linode-cli/default.nix
    
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/tools/virtualization/linode-cli/default.nix
+++ pkgs/tools/virtualization/linode-cli/default.nix
@@ -11,11 +11,13 @@
 }:
 
 let
+  sha256 = "08j7j9fpxh08vkczjqahb8r55bwks8p3ykrv6a2kfi309629nm96";
   # specVersion taken from: https://www.linode.com/docs/api/openapi.yaml at `info.version`.
   specVersion = "4.104.0";
+  specSha256 = "0lbky0djqsndp33067q6yir5525m3zcgkpvf1z16mkspkixzadqs";
   spec = fetchurl {
     url = "https://raw.githubusercontent.com/linode/linode-api-docs/v${specVersion}/openapi.yaml";
-    sha256 = "0lbky0djqsndp33067q6yir5525m3zcgkpvf1z16mkspkixzadqs";
+    sha256 = specSha256;
   };
 
 in
@@ -28,7 +30,7 @@ buildPythonApplication rec {
     owner = "linode";
     repo = pname;
     rev = version;
-    sha256 = "08j7j9fpxh08vkczjqahb8r55bwks8p3ykrv6a2kfi309629nm96";
+    inherit sha256;
   };
 
   # remove need for git history
@@ -59,6 +61,8 @@ buildPythonApplication rec {
   postInstall = ''
     installShellCompletion --cmd linode-cli --bash <($out/bin/linode-cli --skip-config completion bash)
   '';
+
+  passthru.updateScript = ./update.sh;
 
   meta = with lib; {
     description = "The Linode Command Line Interface";
