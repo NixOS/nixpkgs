70e7ae9e600ae4bd9bd0a28b93077cc623e0e8d4
alternateved <alternateved@gmail.com>
2021-09-23 14:24:42 +0200

    discord: 0.0.15 → 0.0.16

--- pkgs/applications/networking/instant-messengers/discord/default.nix
+++ pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -1,6 +1,5 @@
 { branch ? "stable", pkgs }:
-let
-  inherit (pkgs) callPackage fetchurl;
+let inherit (pkgs) callPackage fetchurl;
 in {
   stable = callPackage ./base.nix rec {
     pname = "discord";
@@ -8,8 +7,9 @@ in {
     desktopName = "Discord";
     version = "0.0.16";
     src = fetchurl {
-      url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-      sha256 = "UTVKjs/i7C/m8141bXBsakQRFd/c//EmqqhKhkr1OOk=";
+      url =
+        "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
+      sha256 = "1s9qym58cjm8m8kg3zywvwai2i3adiq6sdayygk2zv72ry74ldai";
     };
   };
   ptb = callPackage ./base.nix rec {
@@ -18,7 +18,8 @@ in {
     desktopName = "Discord PTB";
     version = "0.0.26";
     src = fetchurl {
-      url = "https://dl-ptb.discordapp.net/apps/linux/${version}/discord-ptb-${version}.tar.gz";
+      url =
+        "https://dl-ptb.discordapp.net/apps/linux/${version}/discord-ptb-${version}.tar.gz";
       sha256 = "1rlj76yhxjwwfmdln3azjr69hvfx1bjqdg9jhdn4fp6mlirkrcq4";
     };
   };
@@ -28,7 +29,8 @@ in {
     desktopName = "Discord Canary";
     version = "0.0.131";
     src = fetchurl {
-      url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
+      url =
+        "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
       sha256 = "087rzyivk0grhc73v7ldxxghks0n16ifrvpmk95vzaw99l9xv0v5";
     };
   };
