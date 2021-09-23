41ea7ebd8df978d2388bad40579e726d57529182
R. RyanTM <ryantm-bot@ryantm.com>
2021-09-23 14:20:19 +0000

    python38Packages.elasticsearch: 7.13.1 -> 7.15.0

--- pkgs/development/python-modules/elasticsearch/default.nix
+++ pkgs/development/python-modules/elasticsearch/default.nix
@@ -12,11 +12,11 @@ buildPythonPackage (rec {
   # there's a clear path forward. See
   # https://github.com/elastic/elasticsearch-py/issues/1639 for more
   # info.
-  version = "7.13.1";
+  version = "7.15.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "d6bcca0b2e5665d08e6fe6fadc2d4d321affd76ce483603078fc9d3ccd2bc0f9";
+    sha256 = "6c47eb68f643f3d7d6a6d971e571448b900b9470dc481421c9af9d28a1303911";
   };
 
   # Check is disabled because running them destroy the content of the local cluster!
