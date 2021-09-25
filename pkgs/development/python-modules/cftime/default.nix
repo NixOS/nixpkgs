7d4a548aeab82a89cdf6a91d6452d1d8df1760d4
R. RyanTM <ryantm-bot@ryantm.com>
2021-09-25 02:08:17 +0000

    python38Packages.cftime: 1.5.0 -> 1.5.1

--- pkgs/development/python-modules/cftime/default.nix
+++ pkgs/development/python-modules/cftime/default.nix
@@ -10,11 +10,11 @@
 
 buildPythonPackage rec {
   pname = "cftime";
-  version = "1.5.0";
+  version = "1.5.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b644bcb53346b6f4fe2fcc9f3b574740a2097637492dcca29632c817e0604f29";
+    sha256 = "8a398caed78389b366f1037ca62939ff01af2f1789c77bce05eb903f19ffd840";
   };
 
   checkInputs = [
