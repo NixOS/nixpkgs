d7b9b64571528a20d4e9c6e5a6b61975184a1a50
R. RyanTM <ryantm-bot@ryantm.com>
2021-09-25 11:06:31 +0000

    python38Packages.translatepy: 2.0 -> 2.1

--- pkgs/development/python-modules/translatepy/default.nix
+++ pkgs/development/python-modules/translatepy/default.nix
@@ -10,13 +10,13 @@
 
 buildPythonPackage rec {
   pname = "translatepy";
-  version = "2.0";
+  version = "2.1";
 
   src = fetchFromGitHub {
     owner = "Animenosekai";
     repo = "translate";
     rev = "v${version}";
-    sha256 = "Rt6FvB4kZVaB/jxxqOHsnkReTFCCyiEaZf240n0zVZs=";
+    sha256 = "0xj97s6zglvq2894wpq3xbjxgfkrfk2414vmcszap8h9j2zxz8gf";
   };
 
   propagatedBuildInputs = [
