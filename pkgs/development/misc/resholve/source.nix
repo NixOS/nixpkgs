777b821d38352ea440d426a61db83f35e23cdaa2
Travis A. Everett <travis.a.everett@gmail.com>
2021-09-26 08:15:17 -0500

    resholve: 0.6.4 -> 0.6.5

--- pkgs/development/misc/resholve/source.nix
+++ pkgs/development/misc/resholve/source.nix
@@ -3,7 +3,7 @@
 }:
 
 rec {
-  version = "0.6.4";
+  version = "0.6.5";
   rSrc =
     # local build -> `make ci`; `make clean` to restore
     # return to remote source
@@ -14,6 +14,6 @@ rec {
         owner = "abathur";
         repo = "resholve";
         rev = "v${version}";
-        hash = "sha256-Wq9m67xkTs/8FA1XlwNuNXYSpseQdCa/GtgmsMSLtXI=";
+        hash = "sha256-4fTACLDsHxVGYMOm08wQs4gFWMu8kv0O12IyOZSROqw=";
       };
 }
