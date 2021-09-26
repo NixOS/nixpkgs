cc0ac2c9d43886a16ef637e9b4811f961e0186d7
lucasew <lucas59356@gmail.com>
2021-09-25 21:15:23 -0300

    cisco-packet-tracer: init at 7.3.1 and 8.0.1
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24078,6 +24078,10 @@ with pkgs;
 
   clapper = callPackage ../applications/video/clapper { };
 
+  ciscoPacketTracer7 = callPackage ../applications/networking/cisco-packet-tracer/7.nix { };
+
+  ciscoPacketTracer8 = callPackage ../applications/networking/cisco-packet-tracer/8.nix { };
+
   claws-mail-gtk2 = callPackage ../applications/networking/mailreaders/claws-mail {
     inherit (xorg) libSM;
     useGtk3 = false;
