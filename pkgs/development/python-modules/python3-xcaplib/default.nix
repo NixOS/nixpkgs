d46bc8c173b8b65a462638e1b27514692d851124
Charlie Hanley <charles.scott.hanley+git@gmail.com>
2021-09-26 15:52:14 -0400

    python3Packages.python3-xcaplib: init at 2.0.1

---
+++ pkgs/development/python-modules/python3-xcaplib/default.nix
@@ -0,0 +1,39 @@
+{ lib, fetchFromGitHub, buildPythonPackage, isPy3k, python3-application, lxml
+, gevent, python3-eventlib }:
+
+buildPythonPackage rec {
+  pname = "python3-xcaplib";
+  version = "2.0.1";
+
+  disabled = !isPy3k;
+
+  src = fetchFromGitHub {
+    owner = "AGProjects";
+    repo = "python3-xcaplib";
+    rev = version;
+    sha256 = "sha256-tIHDkseFkLqrdXFPSemYfOMRRhNcqJxCgMSr2xkI/DA=";
+  };
+
+  propagatedBuildInputs = [ lxml gevent python3-eventlib python3-application ];
+
+  pythonImportsCheck = [ "xcaplib" ];
+
+  meta = with lib; {
+    description = "Python XCAP client library";
+    homepage = "https://github.com/AGProjects/python3-xcaplib";
+    license = licenses.lgpl21Plus;
+    maintainers = with maintainers; [ chanley ];
+    longDescription = ''
+      XCAP protocol, defined in RFC 4825, allows a client to read, write, and
+      modify application configuration data stored in XML format on a server. XCAP
+      maps XML document sub-trees and element attributes to HTTP URIs, so that
+      these components can be directly accessed by HTTP. An XCAP server used by
+      XCAP clients to store data like presence policy in combination with a SIP
+      Presence server that supports PUBLISH/SUBSCRIBE/NOTIFY SIP methods can
+      provide a complete SIP SIMPLE solution.
+
+      The XCAP client example script provided by this package can be used to
+      manage documents on an XCAP server.
+    '';
+  };
+}
