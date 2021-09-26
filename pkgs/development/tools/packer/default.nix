fe6701ec3ab3ede6f53d6f014fe1db80b83d0fa7
Maxine Aubrey <maxeaubrey@gmail.com>
2021-09-26 13:01:30 +0200

    packer: 1.7.4 -> 1.7.5
    - https://github.com/hashicorp/packer/releases/tag/v1.7.5
    - https://github.com/hashicorp/packer/blob/v1.7.5/CHANGELOG.md

--- pkgs/development/tools/packer/default.nix
+++ pkgs/development/tools/packer/default.nix
@@ -1,17 +1,25 @@
-{ lib, buildGoModule, fetchFromGitHub, installShellFiles }:
+{ lib, buildGoModule, fetchFromGitHub, installShellFiles, fetchpatch }:
 
 buildGoModule rec {
   pname = "packer";
-  version = "1.7.4";
+  version = "1.7.5";
 
   src = fetchFromGitHub {
     owner = "hashicorp";
     repo = "packer";
     rev = "v${version}";
-    sha256 = "sha256-VNOq9uhtzf1hdEn+bkAOYy4gZxP5ek0WaaS/71uJzrA=";
+    sha256 = "15kw4zy0p7hr6jm0202s0fk5ja3ff0pdir37qdifngm1x7id1vxc";
   };
 
-  vendorSha256 = "sha256-WYA/wZJg93+X4IAX9hOMRHVRQRyA4N4aDaScDgkGUIE=";
+  vendorSha256 = "1785yv48sn504zcig9szjw9s4dxb55dg9idh10i2gzfgbda2c3nf";
+
+  patches = [
+    # https://github.com/hashicorp/packer/pull/11282
+    (fetchpatch {
+      url = "https://github.com/hashicorp/packer/commit/dbf13803217e18c6cb567ffefc9476c4e0149e02.patch";
+      sha256 = "1n038x6qnr75c5ci2jp8jcwp6yvlchcf2nydksb2s75ffvidjrsa";
+    })
+  ];
 
   subPackages = [ "." ];
 
@@ -28,6 +36,7 @@ buildGoModule rec {
     homepage    = "https://www.packer.io";
     license     = licenses.mpl20;
     maintainers = with maintainers; [ cstrahan zimbatm ma27 ];
+    changelog   = "https://github.com/hashicorp/packer/blob/v${version}/CHANGELOG.md";
     platforms   = platforms.unix;
   };
 }
