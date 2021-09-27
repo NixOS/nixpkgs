a0be5029d60267033997d5a90ae6b9841d7f6ffd
Adriaan de Groot <groot@kde.org>
2021-09-27 13:46:40 +0200

    [os-nixos] Replace useless stub implementation
    The existing stub just did the same as slow-python, logging
    some values and doing nothing useful. It didn't even act
    as an example of the original os-nixos plan, which was
    to write the config file.
    
    Do a very minimal effort example with variable substitution
    from GS.

--- pkgs/by-name/ca/calamares-nixos-extensions/src/modules/os-nixos/main.py
+++ pkgs/by-name/ca/calamares-nixos-extensions/src/modules/os-nixos/main.py
@@ -18,7 +18,6 @@
 #
 #   SPDX-FileCopyrightText: 2019 Adriaan de Groot <groot@kde.org>
 #   SPDX-License-Identifier: GPL-3.0-or-later
-#   License-Filename: LICENSE
 #
 
 """
@@ -44,39 +43,219 @@ _ = gettext.translation("calamares-python",
                         fallback=True).gettext
 
 
+# The following long **long** string is the entire text of
+# a nix-configuration file. It is cribbed from, and adapted from,
+# the sample file in https://github.com/itamar567/dotnix .
+#
+# We are going to substitute values into this text. However,
+# Python's .format() function wants parens { } around variable
+# names, and Nix's config file wants to use parens { } for block
+# structure. So we have a compromise format here:
+#
+# - Write the config file as you would normally,
+# - Write @@variable@@ instead of {variable}
+#
+# Some minor trickery later will massage this and substitute variables.
+#
+configuration_nix_sample = """# Nix configuration file
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./command-not-found/command-not-found.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use Zen Kernel
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+
+  networking.hostName = "@@hostname@@"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "@@timezone@@";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  networking.useDHCP = false;
+  networking.interfaces.enp42s0.useDHCP = true;
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Configure X11
+  services.xserver = {
+    enable = true;
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+    };
+
+    # Set i3 to the default session in the display manager
+    displayManager.defaultSession = "none+i3";
+  };
+
+  # SSH fix
+  programs.ssh.askPassword = pkgs.lib.mkForce "";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.username = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "libvirtd" ];
+  };
+
+  # Disable password for sudo
+  security.sudo.extraRules= [{
+    groups = [ "wheel" ];
+    commands = [{
+        command = "ALL" ;
+        options= [ "NOPASSWD" ];
+    }];
+  }];
+
+  # Set ZSH as the default shell
+  users.defaultUserShell = pkgs.zsh;
+
+  # clean /tmp on boot
+  boot.cleanTmpDir=true;
+
+  # Config packages
+  nixpkgs.config = {
+    allowUnfree = true;
+
+    chromium = {
+      enableWideVine = true;
+    };
+  };
+
+   # Automatically upgrade the system
+   # This service is a modified version of https://github.com/NixOS/nixpkgs/blob/nixos-21.05/nixos/modules/tasks/auto-upgrade.nix#L122
+   systemd = {
+     services.nixos-upgrade = {
+       description = "NixOS Upgrade";
+
+       # We use --upgrade, so we need internet access
+       wants = [ "network-online.target" ];
+
+       restartIfChanged = false;
+       unitConfig.X-StopOnRemoval = false;
+
+       serviceConfig.Type = "oneshot";
+
+       environment = config.nix.envVars // {
+         inherit (config.environment.sessionVariables) NIX_PATH;
+         HOME = "/root";
+       } // config.networking.proxy.envVars;
+
+       path = with pkgs; [
+         coreutils
+         gnutar
+         xz.bin
+         gzip
+         gitMinimal
+         config.nix.package.out
+       ];
+
+       script = let
+         nixos-rebuild =
+           "${config.system.build.nixos-rebuild}/bin/nixos-rebuild";
+       in ''
+         ${nixos-rebuild} boot --upgrade
+         booted="$(readlink /run/booted-system/{initrd,kernel,kernel-modules})"
+         built="$(readlink /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules})"
+         if [ "$booted" = "$built" ]; then
+           ${nixos-rebuild} switch
+         fi
+       '';
+     };
+
+     # To start the service at boot, we will use a systemd timer
+     timers.nixos-upgrade = {
+       wantedBy = [ "timers.target" ];
+       partOf = [ "nixos-upgrade.service" ];
+       timerConfig.OnBootSec = "5s";
+     };
+   };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+}
+"""
+
+
 def pretty_name():
     return _("NixOS Configuration.")
 
 
 def run():
     """NixOS Configuration."""
-    libcalamares.utils.debug("LocaleDir=" +
-                             str(libcalamares.utils.gettext_path()))
-    libcalamares.utils.debug("Languages=" +
-                             str(libcalamares.utils.gettext_languages()))
 
-    # TODO: probably want to use the job configuration
-    #       with a key "stage" to distinguish generate-config
-    #       from execute-config; maybe it wants an "all" as well
-    #       to do both.
-    accumulator = "*** Job configuration\n"
-    accumulator += str(libcalamares.job.configuration)
-    libcalamares.utils.debug(accumulator)
+    gs = libcalamares.globalstorage
+    text = configuration_nix_sample
 
-    accumulator = "*** GlobalStorage configuration\n"
-    accumulator += "count: " + str(libcalamares.globalstorage.count()) + "\n"
-    accumulator += "keys: {}\n".format(str(libcalamares.globalstorage.keys()))
-    libcalamares.utils.debug(accumulator)
+    # Collect variables to substitute into the main text
+    variables = dict()
+    variables["hostname"] = gs.value("hostname")
+    variables["timezone"] = gs.value("locationRegion") + "/" + gs.value("locationZone")
 
-    libcalamares.utils.debug("Run NixOS tools.")
+    # Check that all variables are used
+    for key in variables.keys():
+        pattern = "@@{key}@@".format(key=key)
+        if not pattern in text:
+            libcalamares.utils.warn("Variable '{key}' is not used.".format(key=key))
 
-    libcalamares.job.setprogress( 0.1 )
-    sleep(1)
-    libcalamares.job.setprogress( 0.5 )
-    sleep(1)
-    libcalamares.job.setprogress( 1.0 )
+    # Check that all patterns exist
+    import re
+    variable_pattern = re.compile("@@\w+@@")
+    for match in variable_pattern.finditer(text):
+        variable_name = text[match.start()+2:match.end()-2]
+        if not variable_name in variables:
+            libcalamares.utils.warn("Variable '{key}' is used but not defined.".format(key=variable_name))
 
-    sleep(3)
+    # Do the substitutions
+    for key in variables.keys():
+        pattern = "@@{key}@@".format(key=key)
+        text = text.replace(pattern, str(variables[key]))
+
+    # Write the result to a temp-file, then run the main tool.
+    # There is no progress reporting from the tool, so it's going
+    # to seem like the module is hanging (see issue #1740).
+    configuration_filename = "/tmp/configuration.nix"
+    with open(configuration_filename, "w") as f:
+        f.write(text)
+
+    libcalamares.job.setprogress(0.1)
+    libcalamares.utils.check_target_env_call(["nix", configuration_filename])
 
     # To indicate an error, return a tuple of:
     # (message, detailed-error-message)
