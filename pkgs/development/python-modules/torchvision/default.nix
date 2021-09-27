a9f3a5c02199574b894614ab1504119fffba7185
R. RyanTM <ryantm-bot@ryantm.com>
2021-09-27 09:23:41 +0000

    python38Packages.torchvision: 0.10.0 -> 0.10.1

--- pkgs/development/python-modules/torchvision/default.nix
+++ pkgs/development/python-modules/torchvision/default.nix
@@ -24,13 +24,13 @@ let
   cudaArchStr = lib.optionalString cudaSupport lib.strings.concatStringsSep ";" pytorch.cudaArchList;
 in buildPythonPackage rec {
   pname = "torchvision";
-  version = "0.10.0";
+  version = "0.10.1";
 
   src = fetchFromGitHub {
     owner = "pytorch";
     repo = "vision";
     rev = "v${version}";
-    sha256 = "13j04ij0jmi58nhav1p69xrm8dg7jisg23268i3n6lnms37n02kc";
+    sha256 = "0dw4q4yf86wwkm38bpsjf0yfzai46icvaly861ymh5v9f90q60jw";
   };
 
   nativeBuildInputs = [ libpng ninja which ]
