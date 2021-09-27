dd74170ce49788447f34fbe9a63972735ecb7d4b
Mikael Voss <mvs@nyantec.com>
2021-09-27 14:46:41 +0200

    flex-ndax: init at 0.1-20210714.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24671,6 +24671,8 @@ with pkgs;
 
   flex-ncat = callPackage ../applications/radio/flex-ncat { };
 
+  flex-ndax = callPackage ../applications/radio/flex-ndax { };
+
   fluxbox = callPackage ../applications/window-managers/fluxbox { };
 
   fme = callPackage ../applications/misc/fme {
