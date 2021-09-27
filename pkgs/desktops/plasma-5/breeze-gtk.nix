697277d7da81d43d7d56eff96969a0cfb0610ff6
Yaroslav Bolyukin <iam@lach.pw>
2021-09-27 22:53:33 +0300

    breeze-gtk: fix build
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- pkgs/desktops/plasma-5/breeze-gtk.nix
+++ pkgs/desktops/plasma-5/breeze-gtk.nix
@@ -6,6 +6,9 @@ mkDerivation {
   name = "breeze-gtk";
   nativeBuildInputs = [ extra-cmake-modules sassc python3 python3.pkgs.pycairo breeze-qt5 ];
   buildInputs = [ qtbase ];
+  patches = [
+    ./patches/0001-fix-add-executable-bit.patch
+  ];
   postPatch = ''
     sed -i cmake/FindGTKEngine.cmake \
       -e "s|\''${KDE_INSTALL_FULL_LIBDIR}|${getLib gtk2}/lib|"
