2efc7710a3663e75353cf78292034b3c05a52a25
Adriaan de Groot <groot@kde.org>
2021-09-27 14:07:27 +0200

    SPDX: tag modules

--- pkgs/by-name/ca/calamares-nixos-extensions/src/modules/filekeeper/FileKeeper.h
+++ pkgs/by-name/ca/calamares-nixos-extensions/src/modules/filekeeper/FileKeeper.h
@@ -1,19 +1,9 @@
 /* === This file is part of Calamares - <https://github.com/calamares> ===
  *
- *   Copyright 2018, Adriaan de Groot <groot@kde.org>
+ *   SPDX-FileCopyrightText: 2018 Adriaan de Groot <groot@kde.org>
+ *   SPDX-License-Identifier: GPL-3.0-or-later
  *
- *   Calamares is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   Calamares is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+ *   Calamares is Free Software: see the License-Identifier above.
  */
 
 #ifndef FILEKEEPER_H
