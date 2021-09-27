0c5376e27a43d0ecc3305e9e5ec293931b732c37
Fabian Affolter <mail@fabian-affolter.ch>
2021-09-27 10:34:57 +0200

    olm: 3.2.4 -> 3.2.6

--- pkgs/development/libraries/olm/default.nix
+++ pkgs/development/libraries/olm/default.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "olm";
-  version = "3.2.4";
+  version = "3.2.6";
 
   src = fetchFromGitLab {
     domain = "gitlab.matrix.org";
     owner = "matrix-org";
     repo = pname;
     rev = version;
-    sha256 = "1rl7j26li1irb1lqnnkzan7jrj38kvmdn69rlwbbp390v3z15lvh";
+    sha256 = "sha256-Uwcq9/XNnUFdwNkuph2oGJR0E9CzqdSKFx/Ebs3gNes=";
   };
 
   nativeBuildInputs = [ cmake ];
