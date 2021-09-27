baa7d5b8cec33f19c032befcaa74abd08bf9c1a8
Théo Zimmermann <theo.zimmermann@inria.fr>
2021-09-27 21:52:40 +0200

    coq_8_14: init at 8.14+rc1

--- pkgs/development/coq-modules/stdpp/default.nix
+++ pkgs/development/coq-modules/stdpp/default.nix
@@ -6,8 +6,8 @@ with lib; mkCoqDerivation rec {
   domain = "gitlab.mpi-sws.org";
   owner = "iris";
   defaultVersion = with versions; switch coq.coq-version [
-    { case = isGe "8.11";        out = "1.5.0"; }
-    { case = range "8.8" "8.11"; out = "1.4.0"; }
+    { case = range "8.11" "8.13"; out = "1.5.0"; }
+    { case = range "8.8" "8.10";  out = "1.4.0"; }
   ] null;
   release."1.5.0".sha256 = "1ym0fy620imah89p8b6rii8clx2vmnwcrbwxl3630h24k42092nf";
   release."1.4.0".sha256 = "1m6c7ibwc99jd4cv14v3r327spnfvdf3x2mnq51f9rz99rffk68r";
