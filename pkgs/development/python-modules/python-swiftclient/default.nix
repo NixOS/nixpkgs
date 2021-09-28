d3d3b91455681168f62cd78b35404a63e99d47f6
Sandro Jäckel <sandro.jaeckel@sap.com>
2021-09-28 17:58:48 +0200

    python39Packages.python-swiftclient: install shell completion

--- pkgs/development/python-modules/python-swiftclient/default.nix
+++ pkgs/development/python-modules/python-swiftclient/default.nix
@@ -25,6 +25,10 @@ buildPythonApplication rec {
     stestr
   ];
 
+  postInstall = ''
+    install -Dm644 tools/swift.bash_completion $out/share/bash_completion.d/swift
+  '';
+
   checkPhase = ''
     stestr run
   '';
