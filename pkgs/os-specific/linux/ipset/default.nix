40cd6666331a881f8c40a19225fcd5efe7b33aa8
thomassdk <thomassdk@pm.me>
2021-09-28 12:26:22 +0100

    ipset: extend meta

--- pkgs/os-specific/linux/ipset/default.nix
+++ pkgs/os-specific/linux/ipset/default.nix
@@ -19,5 +19,12 @@ stdenv.mkDerivation rec {
     description = "Administration tool for IP sets";
     license = licenses.gpl2;
     platforms = platforms.linux;
+    longDescription = ''
+      IP sets are a framework inside the Linux 2.4.x and later kernel,
+      which can be administered by the ipset utility.
+      Depending on the type, currently an IP set may store IP addresses,
+      (TCP/UDP) port numbers or IP addresses with MAC addresses in a way,
+      which ensures lightning speed when matching an entry against a set.
+    '';
   };
 }
