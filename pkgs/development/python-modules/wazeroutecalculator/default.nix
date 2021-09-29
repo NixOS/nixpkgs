fcceea469fd8086e52265d958d0f71d20a6ba217
Fabian Affolter <mail@fabian-affolter.ch>
2021-09-29 16:20:27 +0200

    python3Packages.wazeroutecalculator: rename

--- pkgs/development/python-modules/wazeroutecalculator/default.nix
+++ pkgs/development/python-modules/wazeroutecalculator/default.nix
@@ -1,24 +1,32 @@
-{ lib, buildPythonPackage, fetchPypi
-, requests }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, requests
+}:
 
 buildPythonPackage rec {
-  pname = "WazeRouteCalculator";
+  pname = "wazeroutecalculator";
   version = "0.13";
 
   src = fetchPypi {
-    inherit pname version;
+    pname = "WazeRouteCalculator";
+    inherit version;
     sha256 = "sha256-Ex9yglaJkk0+Uo3Y+xpimb5boXz+4QdbJS2O75U6dUg=";
   };
 
-  propagatedBuildInputs = [ requests ];
+  propagatedBuildInputs = [
+    requests
+  ];
 
   # there are no tests
   doCheck = false;
 
+  pythonImportsCheck = [ "WazeRouteCalculator" ];
+
   meta = with lib; {
     description = "Calculate actual route time and distance with Waze API";
     homepage = "https://github.com/kovacsbalu/WazeRouteCalculator";
-    license = licenses.gpl3;
+    license = licenses.gpl3Only;
     maintainers = with maintainers; [ peterhoeg ];
   };
 }
