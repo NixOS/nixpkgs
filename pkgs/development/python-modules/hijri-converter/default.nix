4791e7de6b94d19ebac398af74052bb6733a877d
Fabian Affolter <mail@fabian-affolter.ch>
2021-09-29 10:12:31 +0200

    python3Packages.hijri-converter: 2.2.0 -> 2.2.2

--- pkgs/development/python-modules/hijri-converter/default.nix
+++ pkgs/development/python-modules/hijri-converter/default.nix
@@ -6,14 +6,16 @@
 
 buildPythonPackage rec {
   pname = "hijri-converter";
-  version = "2.2.0";
+  version = "2.2.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-25pfMciEJUFjr2ocOb6ByAel6Je6lYdiTWcG3RBI8WA=";
+    sha256 = "sha256-1KENsAnBQXWSu/s96+yt+gTY2NXVG2Spcelp12Gp8+E=";
   };
 
   checkInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "hijri_converter" ];
 
   meta = with lib; {
     description = "Accurate Hijri-Gregorian date converter based on the Umm al-Qura calendar";
