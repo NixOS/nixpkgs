3e02d644758f3e17df129712eb098539aa0f6688
Fabian Affolter <mail@fabian-affolter.ch>
2021-09-30 22:34:47 +0200

    python3Packages.pyvicare: 2.8.1 -> 2.9.1

--- pkgs/development/python-modules/pyvicare/default.nix
+++ pkgs/development/python-modules/pyvicare/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "pyvicare";
-  version = "2.8.1";
+  version = "2.9.1";
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "somm15";
     repo = "PyViCare";
     rev = version;
-    sha256 = "sha256-SmbsEN6vZ28ihgUggtcF2AjbmUVaqLLweh7cKipr6u4=";
+    sha256 = "sha256-Uzz2mWBT5BaMxYeR6YFIP1BqTWye1Hz9CTTg/bg4kSU=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
