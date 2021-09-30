947dc6cbc0156e394f22e7db3413239f9803553b
Samuel Ainsworth <skainsworth@gmail.com>
2021-09-29 18:35:21 -0700

    python3Packages.tensorflow: don't use "sha256:" prefix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/development/python-modules/gast/default.nix
+++ pkgs/development/python-modules/gast/default.nix
@@ -2,11 +2,11 @@ self: rec {
   # Used by tensorflow 2.6.0
   gast_0_4 = self.callPackage ./common.nix {
     version = "0.4.0";
-    sha256 = "sha256:0zhca08ij3rgrpcr3w385dfiwng129s44wsnmcxg7jhbqpyz2s5y";
+    sha256 = "0zhca08ij3rgrpcr3w385dfiwng129s44wsnmcxg7jhbqpyz2s5y";
   };
   gast_0_5 = self.callPackage ./common.nix {
     version = "0.5.2";
-    sha256 = "sha256:05vysdd9hlivq91xyspsjj560xfsjcsjp8491cl0j4p5b1lsmzxa";
+    sha256 = "05vysdd9hlivq91xyspsjj560xfsjcsjp8491cl0j4p5b1lsmzxa";
   };
   gast = gast_0_5;
 }
