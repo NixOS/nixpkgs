3b8e97b4f721ef14846c43df4c82143977113156
Artturin <Artturin@artturin.com>
2021-10-02 02:00:50 +0300

    libdecor: init at 0.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16959,6 +16959,8 @@ with pkgs;
     libGL = null;
   };
 
+  libdecor = callPackage ../development/libraries/libdecor { };
+
   libdigidoc = callPackage ../development/libraries/libdigidoc {
     inherit (darwin.apple_sdk.frameworks) Security;
   };
