ba2dd06680f576c7f969526ab6ccf0f09854805d
Felix Singer <felixsinger@posteo.net>
2021-10-01 14:39:21 +0200

    mumble,murmur: 1.3.4 -> 1.4.230
    Update version to 1.4.230
    
    Signed-off-by: Felix Singer <felixsinger@posteo.net>
    Signed-off-by: Franz Pletz <fpletz@fnordicwalking.de>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27695,6 +27695,7 @@ with pkgs;
       jackSupport = config.mumble.jackSupport or false;
       speechdSupport = config.mumble.speechdSupport or false;
       pulseSupport = config.pulseaudio or stdenv.isLinux;
+      pipewireSupport = stdenv.isLinux;
     }).mumble;
 
   mumble_overlay = callPackage ../applications/networking/mumble/overlay.nix {
