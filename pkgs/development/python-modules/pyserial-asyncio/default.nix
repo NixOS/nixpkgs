dedd0f6308443d85d7117bc78698a4761d868ca9
Fabian Affolter <mail@fabian-affolter.ch>
2021-10-02 23:20:23 +0200

    python3Packages.pyserial-asyncio: 0.5 -> 0.6

--- pkgs/development/python-modules/pyserial-asyncio/default.nix
+++ pkgs/development/python-modules/pyserial-asyncio/default.nix
@@ -3,13 +3,13 @@
 
 buildPythonPackage rec {
   pname = "pyserial-asyncio";
-  version = "0.5";
+  version = "0.6";
 
   disabled = !isPy3k; # Doesn't support python older than 3.4
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1641e5433a866eeaf6464b3ab88b741e7a89dd8cd0f851b3343b15f425138d33";
+    sha256 = "sha256-tgMpI+BenXXsF6WvmphCnEbSg5rfr4BgTVLg+qzXoy8=";
   };
 
   propagatedBuildInputs = [ pyserial ];
