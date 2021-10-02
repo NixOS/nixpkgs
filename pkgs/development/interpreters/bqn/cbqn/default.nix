90275e88fa0d9b6396e7cb1f9dca7a9e9df46b7a
sternenseemann <sternenseemann@systemli.org>
2021-10-02 13:36:30 +0200

    cbqn: move cbqn-bytecode out of derivation environment
    This way if bqn-path != null, the derivation won't depend on cbqn-bytecode.

--- pkgs/development/interpreters/bqn/cbqn/default.nix
+++ pkgs/development/interpreters/bqn/cbqn/default.nix
@@ -11,8 +11,16 @@ let
     rev = "97cbdc67fe6a9652c42daefadd658cc41c1e5ae3";
     hash = "sha256-F2Bv3n3C7zAhqKCMB6hT2iIWTjEqFdLBMyX6/w7V1SY=";
   };
+
+  cbqn-bytecode = fetchFromGitHub {
+    owner = "dzaima";
+    repo = "CBQN";
+    rev = "fdf0b93409d68d5ffd86c5670db27c240e6039e0";
+    hash = "sha256-A0zvpg+G37WNgyfrJuc5rH6L7Wntdbrz8pYEPreqgKE=";
+  };
 in
-stdenv.mkDerivation rec {
+
+stdenv.mkDerivation {
   pname = "cbqn";
   version = "0.0.0+unstable=2021-09-29";
 
@@ -21,13 +29,6 @@ stdenv.mkDerivation rec {
     repo = "CBQN";
     rev = "1c83483d5395e097f60de299274ebe0df590217e";
     hash = "sha256-C34DpXab08mBm2oCQuaeq4fJPtQ5rVa/HlpL/nB9XjQ=";
-  };
-
-  cbqn-bytecode = fetchFromGitHub {
-    owner = "dzaima";
-    repo = "CBQN";
-    rev = "fdf0b93409d68d5ffd86c5670db27c240e6039e0";
-    hash = "sha256-A0zvpg+G37WNgyfrJuc5rH6L7Wntdbrz8pYEPreqgKE=";
   };
 
   dontConfigure = true;
