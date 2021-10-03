726fd18cc88fc0bf82ae4569a3c156ef6ea4c4b1
Ivan <ivan.d.balashov@gmail.com>
2021-10-03 21:05:43 +0300

    add myself @almostnobody to maintainer-list.nix

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12638,5 +12638,11 @@
     github = "mbprtpmnr";
     githubId = 88109321;
   };
+  almostnobody = {
+    name = "AlmostNobody";
+    email = "almost0nobody@gmail.com";
+    github = "almostnobody";
+    githubId = 1583339;
+  };
 
-}
+  }
