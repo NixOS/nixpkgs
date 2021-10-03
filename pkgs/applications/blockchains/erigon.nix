8233973640336b61208902c299e9b0b6da0e35e2
Sandro <sandro.jaeckel@gmail.com>
2021-10-03 19:23:25 +0200

    Apply suggestions from code review

--- pkgs/applications/blockchains/erigon.nix
+++ pkgs/applications/blockchains/erigon.nix
@@ -8,10 +8,10 @@ buildGoModule rec {
     owner = "ledgerwatch";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256:0l0w1badhvlh1rgqzvlmy5k7xhb1nf4f5dmhkl935a5ila08aak3";
+    sha256 = "0l0w1badhvlh1rgqzvlmy5k7xhb1nf4f5dmhkl935a5ila08aak3";
   };
 
-  vendorSha256 = "sha256:1hbfmq76zm50zwmlh3jblriwq2k1mp99d8lg8xzxwy56hncgfj8k";
+  vendorSha256 = "1hbfmq76zm50zwmlh3jblriwq2k1mp99d8lg8xzxwy56hncgfj8k";
   runVend = true;
 
   # Build errors in mdbx when format hardening is enabled:
