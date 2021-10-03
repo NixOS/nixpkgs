c74221839b5e20a1260a77a9823ab7d3d9d1a5fa
taku0 <mxxouy6x3m_github@tatapa.org>
2021-10-03 20:45:43 +0900

    temurin-bin, semeru-bin: init at 17.0.3, adoptopenjdk-bin: remove 13, 14, 17
    AdoptOpenJDK Hotspot is now Eclipse Temurin and AdoptOpenJDK OpenJ9 is
    now IBM Semeru Runtime.
    
    Adds `temurin-bin` and `semeru-bin` packages.
    
    AdoptOpenJDK 13, 14, and 15 is no longer supported.  AdoptOpenJDK 13 and
    14 are removed.  AdoptOpenJDK 15 is left only for some packages
    depending it.

--- pkgs/development/compilers/adoptopenjdk-bin/jdk17-darwin.nix
+++
@@ -1,8 +0,0 @@
-{ lib }:
-
-let
-  sources = lib.importJSON ./sources.json;
-in
-{
-  jdk-hotspot = import ./jdk-darwin-base.nix { sourcePerArch = sources.openjdk17.mac.jdk.hotspot; };
-}
--- pkgs/development/compilers/adoptopenjdk-bin/jdk17-linux.nix
+++
@@ -1,8 +0,0 @@
-{ lib }:
-
-let
-  sources = lib.importJSON ./sources.json;
-in
-{
-  jdk-hotspot = import ./jdk-linux-base.nix { sourcePerArch = sources.openjdk17.linux.jdk.hotspot; };
-}
