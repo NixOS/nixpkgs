131b5eb8c3a41b886660888974df673a905e3a7e
Merlin Göttlinger <megoettlinger@gmail.com>
2021-10-04 11:32:04 +0200

    Apply suggestions from code review
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/applications/networking/mhost/default.nix
+++ pkgs/applications/networking/mhost/default.nix
@@ -11,7 +11,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "1j0378f8gj8hdcdhpj6lqlnriasmjxzri42wjj9pygzkmpd3ym86";
   };
 
-  cargoSha256 = "sha256:0gqrya0bpdd67k2sxib7f4npnrx84d9r4hjq2sg2xz4j8pmgs018";
+  cargoSha256 = "0gqrya0bpdd67k2sxib7f4npnrx84d9r4hjq2sg2xz4j8pmgs018";
 
   CARGO_CRATE_NAME = "mhost";
 
@@ -20,7 +20,7 @@ rustPlatform.buildRustPackage rec {
   meta = with lib; {
     description = "A modern take on the classic host DNS lookup utility including an easy to use and very fast Rust lookup library";
     homepage = "https://github.com/lukaspustina/mhost";
-    license = [ licenses.asl20 licenses.mit ]; # dual licensed
+    license = with licenses; [ asl20 /* or */ mit ];
     maintainers = [ maintainers.mgttlinger ];
   };
 }
