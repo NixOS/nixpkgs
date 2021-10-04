33e06197bf2b8d8dcc6bd560f12f61886531f001
Bryan A. S <bryanasdev000@gmail.com>
2021-10-04 00:17:06 -0300

    rancher: init at 2.4.13

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32116,6 +32116,8 @@ with pkgs;
 
   rargs = callPackage ../tools/misc/rargs { };
 
+  rancher = callPackage ../applications/networking/cluster/rancher { };
+
   rauc = callPackage ../tools/misc/rauc { };
 
   redprl = callPackage ../applications/science/logic/redprl { };
