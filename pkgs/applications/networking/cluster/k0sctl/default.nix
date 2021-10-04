e22edea5545a1ec7a1caecae6d92bf5fe009caea
Nick Cao <nickcao@nichi.co>
2021-10-04 22:02:33 +0800

    k0sctl: 0.10.2 -> 0.10.3

--- pkgs/applications/networking/cluster/k0sctl/default.nix
+++ pkgs/applications/networking/cluster/k0sctl/default.nix
@@ -5,16 +5,23 @@
 
 buildGoModule rec {
   pname = "k0sctl";
-  version = "0.10.2";
+  version = "0.10.3";
 
   src = fetchFromGitHub {
     owner = "k0sproject";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-0vsOZbSQtoWvU81wnT7QWNhvIclwGAu441lTOuZnXho=";
+    sha256 = "sha256-hlJfgNFHEjIrvHhaAje1XQbNO6e3D/qcCmdVFhklwqs=";
   };
 
   vendorSha256 = "sha256-3OTkigryWsyCytyNMyumJJtc/BwtdryvDQRan2dzqfg=";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X github.com/k0sproject/k0sctl/version.Environment=production"
+    "-X github.com/k0sproject/k0sctl/version.Version=${version}"
+  ];
 
   meta = with lib; {
     description = "A bootstrapping and management tool for k0s clusters.";
