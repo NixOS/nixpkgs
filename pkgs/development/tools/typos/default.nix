476b094f2998b2364e5116060d88abad491fc78e
Merlin Göttlinger <megoettlinger@gmail.com>
2021-10-04 11:28:46 +0200

    Apply suggestions from code review
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/development/tools/typos/default.nix
+++ pkgs/development/tools/typos/default.nix
@@ -8,15 +8,15 @@ rustPlatform.buildRustPackage rec {
     owner = "crate-ci";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256:10ydsp77v4kf1qsq5wyc02iyfsdy0rpcyxycp2lqz9qy3g3ih7vm";
+    sha256 = "10ydsp77v4kf1qsq5wyc02iyfsdy0rpcyxycp2lqz9qy3g3ih7vm";
   };
 
-  cargoSha256 = "sha256:1i6999nmg4pahpp4fz4qm4rx8iixa13zjwlhyixwjwbag1w8l3gp";
+  cargoSha256 = "1i6999nmg4pahpp4fz4qm4rx8iixa13zjwlhyixwjwbag1w8l3gp";
 
   meta = with lib; {
     description = "Source code spell checker";
-    homepage = "https://https://github.com/crate-ci/typos/";
-    license = [ licenses.asl20 licenses.mit ]; # dual licensed
+    homepage = "https://github.com/crate-ci/typos/";
+    license = with licenses; [ asl20 /* or */ mit ];
     maintainers = [ maintainers.mgttlinger ];
   };
 }
