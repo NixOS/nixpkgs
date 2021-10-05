362cdf2cc60c9a7a32c02ed785a8ba9aa2882a68
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2021-10-06 04:37:24 +0800

    davix: 0.7.6 -> 0.8.0

--- pkgs/tools/networking/davix/default.nix
+++ pkgs/tools/networking/davix/default.nix
@@ -1,19 +1,24 @@
 { lib, stdenv, fetchurl, cmake, pkg-config, openssl, libxml2, boost, python3, libuuid }:
 
 stdenv.mkDerivation rec {
-  version = "0.7.6";
+  version = "0.8.0";
   pname = "davix";
   nativeBuildInputs = [ cmake pkg-config python3 ];
   buildInputs = [ openssl libxml2 boost libuuid ];
 
   # using the url below since the github release page states
   # "please ignore the GitHub-generated tarballs, as they are incomplete"
-  # https://github.com/cern-fts/davix/releases/tag/R_0_7_6
+  # https://github.com/cern-fts/davix/releases/tag/R_0_8_0
   src = fetchurl {
     url = "https://github.com/cern-fts/${pname}/releases/download/R_${lib.replaceStrings ["."] ["_"] version}/${pname}-${version}.tar.gz";
-    sha256 = "0wq66spnr616cns72f9dvr2xfvkdvfqqmc6d7dx29fpp57zzvrx2";
+    sha256 = "LxCNoECKg/tbnwxoFQ02C6cz5LOg/imNRbDTLSircSQ=";
   };
 
+  preConfigure = ''
+    find . -mindepth 1 -maxdepth 1 -type f -name "patch*.sh" -print0 | while IFS= read -r -d ''' file; do
+      patchShebangs "$file"
+    done
+  '';
 
   meta = with lib; {
     description = "Toolkit for Http-based file management";
@@ -22,10 +27,10 @@ stdenv.mkDerivation rec {
     operations with Http based protocols (WebDav, Amazon S3, ...).
     Davix provides an API and a set of command line tools";
 
-    license     = licenses.lgpl2Plus;
-    homepage    = "http://dmc.web.cern.ch/projects/davix/home";
-    maintainers = [ maintainers.adev ];
-    platforms   = platforms.all;
+    license = licenses.lgpl2Plus;
+    homepage = "http://dmc.web.cern.ch/projects/davix/home";
+    changelog = "https://github.com/cern-fts/davix/blob/devel/RELEASE-NOTES.md";
+    maintainers = with maintainers; [ adev ];
+    platforms = platforms.all;
   };
 }
-
