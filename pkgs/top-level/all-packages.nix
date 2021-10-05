bf8e9d00040b8cef113c6d036a5a8779bf2884f1
Fabian Affolter <mail@fabian-affolter.ch>
2021-10-05 21:13:04 +0200

    spyre: init at 1.2.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31247,6 +31247,8 @@ with pkgs;
 
   spyder = with python3.pkgs; toPythonApplication spyder;
 
+  spyre = callPackage ../tools/security/spyre { };
+
   openspace = callPackage ../applications/science/astronomy/openspace { };
 
   stellarium = libsForQt5.callPackage ../applications/science/astronomy/stellarium { };
