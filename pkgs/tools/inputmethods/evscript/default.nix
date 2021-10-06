a72e0d2ab3fcd1c4d0d1899cd7da36071644210a
Miles Breslin <milesbreslin@gmail.com>
2021-10-05 23:53:14 -0700

    evscript: Update repository owner to unrelentingtech

--- pkgs/tools/inputmethods/evscript/default.nix
+++ pkgs/tools/inputmethods/evscript/default.nix
@@ -5,16 +5,16 @@ rustPlatform.buildRustPackage rec {
   version = "unstable-2021-06-16";
 
   src = fetchFromGitHub {
-    owner = "myfreeweb";
+    owner = "unrelentingtech";
     repo = pname;
     rev = "410603abf4810320bf79bde90cc85367b71a8b12";
     sha256 = "sha256-apq3kHipEX6zOTNwqpIQR46JqmeE7EKVSOGrNNSkyu8=";
   };
 
-  cargoHash = "sha256-h1Znx69mFIXCs0x3AtFT2brSbdfMJGB0Ht7v4HDfEqM=";
+  cargoHash = "sha256-1aR9/fhJQ+keRIxSG2cpY1HTalE6nM+MTb1Za3Tot28=";
 
   meta = with lib; {
-    homepage = "https://github.com/myfreeweb/evscript";
+    homepage = "https://github.com/unrelentingtech/evscript";
     description = "A tiny sandboxed Dyon scripting environment for evdev input devices";
     license = licenses.unlicense;
     maintainers = with maintainers; [ milesbreslin ];
