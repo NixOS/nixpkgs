a24b5e511cf2c6859dda725af48f8c296f777613
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-10-06 17:27:56 -0400

    kweathercore: 0.4 -> 0.5

--- pkgs/development/libraries/kweathercore/default.nix
+++ pkgs/development/libraries/kweathercore/default.nix
@@ -8,14 +8,14 @@
 
 mkDerivation rec {
   pname = "kweathercore";
-  version = "0.4";
+  version = "0.5";
 
   src = fetchFromGitLab {
     domain = "invent.kde.org";
     owner = "libraries";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0mwmkr1xg0mcgbjgzp04chf55xyrgf0gndg3hs4c21bfjpy1gp3k";
+    sha256 = "08ipabskhsbspkzzdlpwl89r070q8d0vc9500ma6d5i9fnpmkz6d";
   };
 
   buildInputs = [
