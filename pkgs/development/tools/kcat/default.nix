01a85ce48b156973cb0aa9f5a898a6975d20b1cf
Souvik Sen <senso@deshaw.com>
2021-10-06 23:14:34 -0400

    kcat(previously kafkacat): 1.6.0 -> 1.7.0
    Fixed the 1.7.0 build and renamed the package per upstream.
    (https://github.com/edenhill/kcat/commit/3fc21aa1e9facc5fa546b9c922fc26a41666a0c1)

--- pkgs/development/tools/kcat/default.nix
+++ pkgs/development/tools/kcat/default.nix
@@ -1,18 +1,18 @@
-{ lib, stdenv, fetchFromGitHub, pkg-config, zlib, rdkafka, yajl, avro-c, libserdes }:
+{ lib, stdenv, fetchFromGitHub, pkg-config, zlib, rdkafka, yajl, avro-c, libserdes, which }:
 
 stdenv.mkDerivation rec {
-  pname = "kafkacat";
+  pname = "kcat";
 
-  version = "1.6.0";
+  version = "1.7.0";
 
   src = fetchFromGitHub {
     owner = "edenhill";
-    repo = "kafkacat";
+    repo = "kcat";
     rev = version;
-    sha256 = "0z3bw00s269myfd1xqksjyznmgp74xfs09xqlq347adsgby3cmfs";
+    sha256 = "sha256-koDhj/RQc9fhfqjrJylhURw6tppPELhLlBGbNVJsii8=";
   };
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [ pkg-config which ];
 
   buildInputs = [ zlib rdkafka yajl avro-c libserdes ];
 
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "A generic non-JVM producer and consumer for Apache Kafka";
-    homepage = "https://github.com/edenhill/kafkacat";
+    homepage = "https://github.com/edenhill/kcat";
     license = licenses.bsd2;
     platforms = platforms.linux ++ platforms.darwin;
     maintainers = with maintainers; [ nyarly ];
