5627b8ae5b927351693f27e96738b7787567d606
sterni <sternenseemann@systemli.org>
2021-10-07 04:13:51 +0200

    cbqn: 2021-10-01 -> 2021-10-05 (#140795)

--- pkgs/development/interpreters/bqn/cbqn/default.nix
+++ pkgs/development/interpreters/bqn/cbqn/default.nix
@@ -11,20 +11,20 @@ let
     name = "cbqn-bytecode-files";
     owner = "dzaima";
     repo = "CBQN";
-    rev = "94bb312d20919f942eabed3dca33c514de3c3227";
-    hash = "sha256-aFw5/F7/sYkYmxAnGeK8EwkoVrbEcjuJAD9YT+iW9Rw=";
+    rev = "4d23479cdbd5ac6eb512c376ade58077b814b2b7";
+    sha256 = "1il6pxbllf4rs0wf2s6q6h72m3p1d6ymgsllpkmadnw1agif0fri";
   };
 in
 assert genBytecode -> ((bqn-path != null) && (mbqn-source != null));
 stdenv.mkDerivation rec {
   pname = "cbqn" + lib.optionalString (!genBytecode) "-standalone";
-  version = "0.0.0+unstable=2021-10-01";
+  version = "0.0.0+unstable=2021-10-05";
 
   src = fetchFromGitHub {
     owner = "dzaima";
     repo = "CBQN";
-    rev = "3725bd58c758a749653080319766a33169551536";
-    hash = "sha256-xWp64inFZRqGGTrH6Hqbj7aA0vYPyd+FdetowTMTjPs=";
+    rev = "e23dab20daff9c0dacc2561c616174af72029a3e";
+    sha256 = "17h8fb9a0hjindbxgkljajl1hjr8rdqrb85s5lz903v17wl4lrba";
   };
 
   dontConfigure = true;
@@ -34,6 +34,9 @@ stdenv.mkDerivation rec {
   '';
 
   preBuild = ''
+    # otherwise cbqn defaults to clang
+    makeFlagsArray+=("CC=$CC")
+
     # inform make we are providing the runtime ourselves
     touch src/gen/customRuntime
   '' + (if genBytecode then ''
@@ -41,10 +44,6 @@ stdenv.mkDerivation rec {
   '' else ''
     cp ${cbqn-bytecode-files}/src/gen/{compiler,formatter,runtime0,runtime1,src} src/gen/
   '');
-
-  makeFlags = [
-    "CC=${stdenv.cc.targetPrefix}cc"
-  ];
 
   installPhase = ''
      runHook preInstall
