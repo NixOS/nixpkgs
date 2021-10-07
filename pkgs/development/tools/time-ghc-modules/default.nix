d121e933116b2937eaf5b57626e3a01607da5b27
Tom McLaughlin <tom@codedown.io>
2021-10-07 03:20:17 -0600

    time-ghc-modules: init at 1.0.0

---
+++ pkgs/development/tools/time-ghc-modules/default.nix
@@ -0,0 +1,47 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, makeWrapper
+, sqlite
+, python3
+, coreutils
+, findutils
+, gnused
+}:
+
+stdenv.mkDerivation rec {
+  pname = "time-ghc-modules";
+  version = "1.0.0";
+
+  src = fetchFromGitHub {
+    owner = "codedownio";
+    repo = "time-ghc-modules";
+    rev = version;
+    sha256 = "sha256:1fa1lz0r8s8q664z4303y3b9jqa6d7wv53bnh7pifj0fm45xqznp";
+    fetchSubmodules = true;
+  };
+
+  buildInputs = [makeWrapper];
+
+  buildPhase = ''
+    mkdir -p $out/bin
+    cp ./time-ghc-modules $out/bin/time-ghc-modules
+    wrapProgram $out/bin/time-ghc-modules --prefix PATH : ${lib.makeBinPath [ sqlite python3 coreutils findutils gnused ]}
+
+    mkdir -p $out/bin/scripts
+    cp ./scripts/process $out/bin/scripts/process
+
+    mkdir -p $out/bin/dist
+    cp ./dist/index.html $out/bin/dist/index.html
+  '';
+
+  dontInstall = true;
+
+  meta = with lib; {
+    description = "Analyze GHC .dump-timings files";
+    homepage = "https://github.com/codedownio/time-ghc-modules";
+    license = licenses.mit;
+    maintainers = [ maintainers.thomasjm ];
+    platforms = platforms.all;
+  };
+}
