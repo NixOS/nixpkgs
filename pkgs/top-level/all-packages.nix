d66a5122227d4610efe7846babcc58a4b20064b9
Felix Singer <felixsinger@posteo.net>
2021-10-07 23:41:01 +0200

    libjaylink: Init at 0.2.0
    Add package for libjaylink. libjaylink is a shared library written in C
    to access SEGGER J-Link and compatible devices.
    
    Signed-off-by: Felix Singer <felixsinger@posteo.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17956,6 +17956,8 @@ with pkgs;
 
   liburcu = callPackage ../development/libraries/liburcu { };
 
+  libjaylink = callPackage ../development/libraries/libjaylink { };
+
   libusb-compat-0_1 = callPackage ../development/libraries/libusb-compat/0.1.nix {};
 
   libusb1 = callPackage ../development/libraries/libusb1 {
