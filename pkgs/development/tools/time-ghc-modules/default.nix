1f414a22071a47064965ba1acac6b561c5ca23cd
Tom McLaughlin <tom@codedown.io>
2021-10-07 23:33:55 -0600

    Take a stab at using install

--- pkgs/development/tools/time-ghc-modules/default.nix
+++ pkgs/development/tools/time-ghc-modules/default.nix
@@ -28,19 +28,19 @@ stdenv.mkDerivation rec {
     mkdir -p $out/bin
     cp ./time-ghc-modules $out/bin/time-ghc-modules
     wrapProgram $out/bin/time-ghc-modules --prefix PATH : ${lib.makeBinPath [ sqlite python3 coreutils findutils gnused ]} \
-                                          --set PROCESS_SCRIPT $out/lib/scripts/process \
-                                          --set HTML_FILE $out/lib/dist/index.html
-
-    mkdir -p $out/lib/scripts
-    cp ./scripts/process $out/lib/scripts/process
-
-    mkdir -p $out/lib/dist
-    cp ./dist/index.html $out/lib/dist/index.html
-
-    runHook postBuild
+                                          --set PROCESS_SCRIPT $out/lib/process \
+                                          --set HTML_FILE $out/lib/index.html
   '';
 
-  dontInstall = true;
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/lib
+    install -m 444 ./dist/index.html $out/lib
+    install ./scripts/process $out/lib
+
+    runHook postInstall
+  '';
 
   meta = with lib; {
     description = "Analyze GHC .dump-timings files";
