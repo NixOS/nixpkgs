da78d399e3c3006baf2a7657249c4930fd90ce50
Tom McLaughlin <tom@codedown.io>
2021-10-07 23:26:45 -0600

    Move non-binaries out of the bin folder

--- pkgs/development/tools/time-ghc-modules/default.nix
+++ pkgs/development/tools/time-ghc-modules/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "time-ghc-modules";
-  version = "1.0.0";
+  version = "1.0.1";
 
   src = fetchFromGitHub {
     owner = "codedownio";
     repo = "time-ghc-modules";
     rev = version;
-    sha256 = "1fa1lz0r8s8q664z4303y3b9jqa6d7wv53bnh7pifj0fm45xqznp";
+    sha256 = "0s6540gllhjn7366inhwa70rdnngnhbi07jn1h6x8a0pi71wdfm9";
   };
 
   nativeBuildInputs = [makeWrapper];
@@ -27,13 +27,15 @@ stdenv.mkDerivation rec {
 
     mkdir -p $out/bin
     cp ./time-ghc-modules $out/bin/time-ghc-modules
-    wrapProgram $out/bin/time-ghc-modules --prefix PATH : ${lib.makeBinPath [ sqlite python3 coreutils findutils gnused ]}
+    wrapProgram $out/bin/time-ghc-modules --prefix PATH : ${lib.makeBinPath [ sqlite python3 coreutils findutils gnused ]} \
+                                          --set PROCESS_SCRIPT $out/lib/scripts/process \
+                                          --set HTML_FILE $out/lib/dist/index.html
 
-    mkdir -p $out/bin/scripts
-    cp ./scripts/process $out/bin/scripts/process
+    mkdir -p $out/lib/scripts
+    cp ./scripts/process $out/lib/scripts/process
 
-    mkdir -p $out/bin/dist
-    cp ./dist/index.html $out/bin/dist/index.html
+    mkdir -p $out/lib/dist
+    cp ./dist/index.html $out/lib/dist/index.html
 
     runHook postBuild
   '';
