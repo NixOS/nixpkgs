9a817a9a759e99881715e1d5f4373f32771d9405
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-10-09 00:23:12 +0200

    python3Packages.pyexcel: 0.6.6 -> 0.6.7

--- pkgs/development/python-modules/pyexcel/default.nix
+++ pkgs/development/python-modules/pyexcel/default.nix
@@ -10,13 +10,13 @@
 
 buildPythonPackage rec {
   pname = "pyexcel";
-  version = "0.6.6";
+  version = "0.6.7";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "39b0bb8f033d9b5523b126cf5a5259d1990ea82b8a23c8eab7aa5e23116803df";
+    sha256 = "cbbd9875729767564b3b64b6ed6a9870b14631184943d13646833d94157dd10f";
   };
 
   propagatedBuildInputs = [
