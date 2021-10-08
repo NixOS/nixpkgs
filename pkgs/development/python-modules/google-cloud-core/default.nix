86f27d349a7dd3238762456d3f8c064d7593fc8b
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-10-09 00:23:02 +0200

    python3Packages.google-cloud-core: 2.0.0 -> 2.1.0

--- pkgs/development/python-modules/google-cloud-core/default.nix
+++ pkgs/development/python-modules/google-cloud-core/default.nix
@@ -11,11 +11,11 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-core";
-  version = "2.0.0";
+  version = "2.1.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-kO6ZZIzPnhGhZ4Gn/FjRPlj2YrQ5xzfUjCTvGGYsJwI=";
+    sha256 = "35a1f5f02a86e0fa2e28c669f0db4a76d928671a28fbbbb493ab59ba9d1cb9a9";
   };
 
   propagatedBuildInputs = [ google-api-core ];
