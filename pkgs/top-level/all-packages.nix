d532260775458198b8eafbfacf324d698b2773d3
zowoq <59103226+zowoq@users.noreply.github.com>
2021-10-09 09:48:28 +1000

    podman: remove darwin wrapper
    - wrapping gvproxy doesn't work since 3.4
    - store paths are written to config files which then break when GCed

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8413,7 +8413,10 @@ with pkgs;
 
   podiff = callPackage ../tools/text/podiff { };
 
-  podman = callPackage ../applications/virtualization/podman/wrapper.nix { };
+  podman = if stdenv.isDarwin then
+    callPackage ../applications/virtualization/podman { }
+  else
+    callPackage ../applications/virtualization/podman/wrapper.nix { };
   podman-unwrapped = callPackage ../applications/virtualization/podman { };
 
   podman-compose = python3Packages.callPackage ../applications/virtualization/podman-compose {};
