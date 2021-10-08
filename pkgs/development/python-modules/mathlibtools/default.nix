74a115cffd4308af0b8006c3439869bb6d933bd5
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-10-09 00:23:09 +0200

    python3Packages.mathlibtools: 1.0.0 -> 1.1.0

--- pkgs/development/python-modules/mathlibtools/default.nix
+++ pkgs/development/python-modules/mathlibtools/default.nix
@@ -3,11 +3,11 @@
 
 buildPythonPackage rec {
   pname = "mathlibtools";
-  version = "1.0.0";
+  version = "1.1.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "da41c65e206f55b1faea303581fc11215e52d6e6990b827336b2e1eb82aad96c";
+    sha256 = "789f070f35424e89e4f2e2c007382250133cc48877627e37c5c463bcf4a1b58a";
   };
 
   propagatedBuildInputs = [
