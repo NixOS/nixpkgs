7fbefa1b061936495729de3dba328aed6a578f48
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-10-09 17:20:35 +0200

    python3Packages.virtualenv: update disabled tests

--- pkgs/development/python-modules/virtualenv/default.nix
+++ pkgs/development/python-modules/virtualenv/default.nix
@@ -72,11 +72,8 @@ buildPythonPackage rec {
   ];
 
   disabledTests = [
-    "test_can_build_c_extensions"
-    "test_xonsh" # imports xonsh, which is not in pythonPackages
-    # tests search `python3`, fail on python2, pypy
-    "test_python_via_env_var"
-    "test_python_multi_value_prefer_newline_via_env_var"
+    # Permission Error
+    "test_bad_exe_py_info_no_raise"
   ];
 
   pythonImportsCheck = [ "virtualenv" ];
