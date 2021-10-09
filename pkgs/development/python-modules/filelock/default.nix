877cca04d1f8fb6081808fff16b2d50352a4f6fe
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-10-09 16:53:49 +0200

    python3Packages.filelock: migrate to pyproject and setuptools-scm

--- pkgs/development/python-modules/filelock/default.nix
+++ pkgs/development/python-modules/filelock/default.nix
@@ -1,13 +1,29 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib
+, buildPythonPackage
+, pythonOlder
+, fetchPypi
+, setuptools-scm
+, pytestCheckHook
+}:
 
 buildPythonPackage rec {
   pname = "filelock";
   version = "3.3.0";
+  format = "pyproject";
+  disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "8c7eab13dc442dc249e95158bcc12dec724465919bdc9831fdbf0660f03d1785";
   };
+
+  nativeBuildInputs = [
+    setuptools-scm
+  ];
+
+  checkInputs = [
+    pytestCheckHook
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/benediktschmitt/py-filelock";
