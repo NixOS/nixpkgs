1799181fd58b5ce43ef955267023f979902c6f5d
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2021-10-11 07:29:51 +0800

    vscode-registries.openvsx: init

--- pkgs/applications/editors/vscode/extension-registries/default.nix
+++ pkgs/applications/editors/vscode/extension-registries/default.nix
@@ -1,5 +1,6 @@
 { lib, callPackage }:
 {
+  openvsx = lib.recurseIntoAttrs (callPackage ./openvsx { });
   standalone = lib.recurseIntoAttrs (callPackage ./standalone { });
   upstream-releases = lib.recurseIntoAttrs (callPackage ./upstream-releases { });
 }
