aa4b31870c61a4d6fe1c8ed490a39d86b9c89495
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2021-10-10 09:51:10 +0200

    python3Packages.pytest-rerunfailures: 10.1 → 10.2

--- pkgs/development/python-modules/pytest-rerunfailures/default.nix
+++ pkgs/development/python-modules/pytest-rerunfailures/default.nix
@@ -2,13 +2,13 @@
 
 buildPythonPackage rec {
   pname = "pytest-rerunfailures";
-  version = "10.1";
+  version = "10.2";
 
   disabled = pythonOlder "3.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "7617c06de13ee6dd2df9add7e275bfb2bcebbaaf3e450f5937cd0200df824273";
+    sha256 = "9e1e1bad51e07642c5bbab809fc1d4ec8eebcb7de86f90f1a26e6ef9de446697";
   };
 
   buildInputs = [ pytest ];
