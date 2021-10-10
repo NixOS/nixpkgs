ffa4f5818afdc88bb4fe2ad4e5438d84f14bdf9c
Emile <mail@emile.space>
2021-10-10 15:36:57 +0200

    updated the indentation to 2 spaces

--- pkgs/tools/misc/mapcidr/default.nix
+++ pkgs/tools/misc/mapcidr/default.nix
@@ -4,31 +4,31 @@
 }:
 
 buildGoModule rec {
-    pname = "mapcidr";
-    version = "0.0.8";
+  pname = "mapcidr";
+  version = "0.0.8";
 
-    src = fetchFromGitHub {
-        owner = "projectdiscovery";
-        repo = pname;
-        rev = "v${version}";
-        sha256 = "sha256-hlMIgSsSqvMx6Y7JnR7L9muTLWPfxDN5raJRezt99G0=";
-    };
+  src = fetchFromGitHub {
+    owner = "projectdiscovery";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-hlMIgSsSqvMx6Y7JnR7L9muTLWPfxDN5raJRezt99G0=";
+  };
 
-    vendorSha256 = "sha256-zp+XaSZgSMwJK+EEiTaJKBTPiKYaYpTtArnGBmHUGzE=";
+  vendorSha256 = "sha256-zp+XaSZgSMwJK+EEiTaJKBTPiKYaYpTtArnGBmHUGzE=";
 
-    modRoot = ".";
-    subPackages = [
-        "cmd/mapcidr"
-    ];
+  modRoot = ".";
+  subPackages = [
+    "cmd/mapcidr"
+  ];
 
-    meta = with lib; {
-        description = "Small utility program to perform multiple operations for a given subnet/CIDR ranges.";
-        longDescription = ''
-            mapCIDR is developed to ease load distribution for mass scanning
-            operations, it can be used both as a library and as independent CLI tool.
-        '';
-        homepage = "https://github.com/projectdiscovery/mapcidr";
-        license = licenses.mit;
-        maintainers = with maintainers; [ hanemile ];
-    };
+  meta = with lib; {
+    description = "Small utility program to perform multiple operations for a given subnet/CIDR ranges";
+    longDescription = ''
+      mapCIDR is developed to ease load distribution for mass scanning
+      operations, it can be used both as a library and as independent CLI tool.
+    '';
+    homepage = "https://github.com/projectdiscovery/mapcidr";
+    license = licenses.mit;
+    maintainers = with maintainers; [ hanemile ];
+  };
 }
