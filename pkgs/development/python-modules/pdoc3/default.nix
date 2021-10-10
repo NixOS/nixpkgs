b4781ea8cd9b74b2116f37174e87a0fdfe4baf26
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-10-10 20:17:21 +0200

    python3Packages.pdoc3: apply patch to fix test_Class_params

--- pkgs/development/python-modules/pdoc3/default.nix
+++ pkgs/development/python-modules/pdoc3/default.nix
@@ -1,4 +1,7 @@
-{ lib, buildPythonPackage, fetchPypi
+{ lib
+, buildPythonPackage
+, fetchPypi
+, fetchpatch
 , pythonOlder
 , Mako
 , markdown
@@ -16,8 +19,24 @@ buildPythonPackage rec {
     sha256 = "5f22e7bcb969006738e1aa4219c75a32f34c2d62d46dc9d2fb2d3e0b0287e4b7";
   };
 
-  nativeBuildInputs = [ setuptools-git setuptools-scm ];
-  propagatedBuildInputs = [ Mako markdown ];
+  patches = [
+    (fetchpatch {
+      # test_Class_params fails in 0.10.0
+      # https://github.com/pdoc3/pdoc/issues/355
+      url = "https://github.com/pdoc3/pdoc/commit/4aa70de2221a34a3003a7e5f52a9b91965f0e359.patch";
+      sha256 = "07sbf7bh09vgd5z1lbay604rz7rhg88414whs6iy60wwbvkz5c2v";
+    })
+  ];
+
+  nativeBuildInputs = [
+    setuptools-git
+    setuptools-scm
+  ];
+
+  propagatedBuildInputs = [
+    Mako
+    markdown
+  ];
 
   meta = with lib; {
     description = "Auto-generate API documentation for Python projects.";
