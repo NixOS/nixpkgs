3096f0a6026a5086070d31fb1dba50a7b38c100c
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-10-11 16:43:35 +0200

    home-assistant: 2021.10.2 -> 2021.10.3

--- pkgs/servers/home-assistant/default.nix
+++ pkgs/servers/home-assistant/default.nix
@@ -114,7 +114,7 @@ let
   extraBuildInputs = extraPackages py.pkgs;
 
   # Don't forget to run parse-requirements.py after updating
-  hassVersion = "2021.10.2";
+  hassVersion = "2021.10.3";
 
 in with py.pkgs; buildPythonApplication rec {
   pname = "homeassistant";
@@ -131,7 +131,7 @@ in with py.pkgs; buildPythonApplication rec {
     owner = "home-assistant";
     repo = "core";
     rev = version;
-    sha256 = "0nds4491v8wy4d8w842asjpjj7xhqghlq0h61i7z6wp8jln7m418";
+    sha256 = "039wi74hx6jy5hqj9cpp8kdifnfyipbzl31f4s1wg3g4fxjq6167";
   };
 
   # leave this in, so users don't have to constantly update their downstream patch handling
