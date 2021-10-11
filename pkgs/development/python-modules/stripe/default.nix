c25721601dbe5c0d2c2436204ba1f391f71ffa20
R. RyanTM <ryantm-bot@ryantm.com>
2021-10-11 15:54:00 +0000

    python38Packages.stripe: 2.60.0 -> 2.61.0

--- pkgs/development/python-modules/stripe/default.nix
+++ pkgs/development/python-modules/stripe/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "stripe";
-  version = "2.60.0";
+  version = "2.61.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "8966b7793014380f60c6f121ba333d6f333a55818edaf79c8d70464ce0a7a808";
+    sha256 = "8131addd3512a22c4c539dda2d869a8f488e06f1b02d1f3a5f0f4848fc56184e";
   };
 
   propagatedBuildInputs = [ requests ];
