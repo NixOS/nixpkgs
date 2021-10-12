d3ce8e4c6ce3de3d52ff9b16dcb29d04ceb2aa2e
Fabian Affolter <mail@fabian-affolter.ch>
2021-10-12 23:39:23 +0200

    git-privacy: init at 2.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1658,6 +1658,8 @@ with pkgs;
 
   git-fire = callPackage ../tools/misc/git-fire { };
 
+  git-privacy = callPackage ../development/tools/git-privacy { };
+
   git-repo-updater = python3Packages.callPackage ../development/tools/git-repo-updater { };
 
   git-revise = with python3Packages; toPythonApplication git-revise;
