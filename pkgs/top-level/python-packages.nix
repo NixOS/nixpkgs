3c787b91e7f66acabb5606883d3b904b2f9c5bb1
Zak B. Elep <zakame@zakame.net>
2021-10-13 22:54:35 +0800

    python3Packages.versionfinder: init at 1.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9530,6 +9530,8 @@ in {
 
   versioneer = callPackage ../development/python-modules/versioneer { };
 
+  versionfinder = callPackage ../development/python-modules/versionfinder { };
+
   versiontag = callPackage ../development/python-modules/versiontag { };
 
   versiontools = callPackage ../development/python-modules/versiontools { };
