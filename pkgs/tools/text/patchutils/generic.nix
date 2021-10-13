bfe02b232050af01600bd0a6bd215e2358765ebd
Artturin <Artturin@artturin.com>
2021-10-13 23:53:28 +0300

    patchutils: enable tests

--- pkgs/tools/text/patchutils/generic.nix
+++ pkgs/tools/text/patchutils/generic.nix
@@ -14,7 +14,16 @@ stdenv.mkDerivation rec {
   buildInputs = [ perl ] ++ extraBuildInputs;
   hardeningDisable = [ "format" ];
 
-  doCheck = false; # fails
+  doCheck = lib.versionAtLeast version "0.3.4";
+
+  preCheck = ''
+    patchShebangs tests
+    chmod +x scripts/*
+  '' + lib.optionalString (lib.versionOlder version "0.4.2") ''
+    find tests -type f -name 'run-test' \
+      -exec echo "Patching {}" \; \
+      -exec sed -i '{}' -e 's|/bin/echo|echo|g' \;
+  '';
 
   meta = with lib; {
     description = "Tools to manipulate patch files";
