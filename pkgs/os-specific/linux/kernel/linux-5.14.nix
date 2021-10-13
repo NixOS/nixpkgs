694a6aeeb2b6a57da25c42540ba43ecd3f87e082
TredwellGit <tredwell@tutanota.com>
2021-10-13 13:06:07 +0000

    linux: 5.14.11 -> 5.14.12

--- pkgs/os-specific/linux/kernel/linux-5.14.nix
+++ pkgs/os-specific/linux/kernel/linux-5.14.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.14.11";
+  version = "5.14.12";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0capilz3wx29pw7n2m5cn229vy9psrccmdspp27znhjkvwj0m0wk";
+    sha256 = "0dswxf1qk70lms5lph15i7nz3ybwiia58v8zzrmi71ajviwjc9wd";
   };
 } // (args.argsOverride or { }))
