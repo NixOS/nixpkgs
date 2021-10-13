aa0a596e6c2399cc1bc5f29e850d49ef6eccbc7e
sternenseemann <sternenseemann@systemli.org>
2021-10-13 15:44:10 +0200

    csound: build with fluidsynth 2.x
    csound has supported FluidSynth 2 since 6.13.0:
    https://github.com/csound/csound/pull/1106
    
    Since fluidsynth_1 has finally been broken by glib 2.70.0, so now is a
    good opportunity to get rid of this dependency.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24004,9 +24004,7 @@ with pkgs;
 
   csa = callPackage ../applications/audio/csa { };
 
-  csound = callPackage ../applications/audio/csound {
-    fluidsynth = fluidsynth_1;
-  };
+  csound = callPackage ../applications/audio/csound { };
 
   csound-manual = callPackage ../applications/audio/csound/csound-manual {
     python = python27;
