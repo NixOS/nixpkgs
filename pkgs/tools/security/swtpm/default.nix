5bd3236442ee1f2a0d3190e18db0f6dceeab61e3
Luflosi <luflosi@luflosi.de>
2021-10-14 21:27:54 +0200

    swtpm: 0.6.0 -> 0.6.1
    https://github.com/stefanberger/swtpm/releases/tag/v0.6.1
    Also fix a small mistake I made in a comment in a previous commit.

--- pkgs/tools/security/swtpm/default.nix
+++ pkgs/tools/security/swtpm/default.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation rec {
   pname = "swtpm";
-  version = "0.6.0";
+  version = "0.6.1";
 
   src = fetchFromGitHub {
     owner = "stefanberger";
     repo = "swtpm";
     rev = "v${version}";
-    sha256 = "sha256-7YzdwGAGECj7PhaCOf/dLSILPXqtbylCkN79vuFBw5Y=";
+    sha256 = "sha256-iy8xjKnPLq1ntZa9x+KtLDznzu6m+1db3NPeGQESUVo=";
   };
 
   patches = [
@@ -46,7 +46,7 @@ stdenv.mkDerivation rec {
   ];
 
   prePatch = ''
-    # Makefile tries to create the directory /var/lib/swtpm-localcafor, which fails
+    # Makefile tries to create the directory /var/lib/swtpm-localca, which fails
     substituteInPlace samples/Makefile.am \
         --replace 'install-data-local:' 'do-not-execute:'
 
