0c5419923c0a05d1ed20ff6816293c2b2d8628e6
Piper McCorkle <contact@piperswe.me>
2021-10-13 23:13:05 -0500

    stdenv: re-add useMusl to busyboxMinimal

--- pkgs/stdenv/linux/make-bootstrap-tools.nix
+++ pkgs/stdenv/linux/make-bootstrap-tools.nix
@@ -19,6 +19,7 @@ in with pkgs; rec {
   tarMinimal = gnutar.override { acl = null; };
 
   busyboxMinimal = busybox.override {
+    useMusl = !stdenv.targetPlatform.isSparc;
     enableStatic = true;
     enableMinimal = true;
     extraConfig = ''
