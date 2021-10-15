867e614f97623ed7d75857fc9e4cfde19b206e89
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-10-16 00:27:22 +0200

    nodePackages.node2nix: backport patch to reduce derivation size

--- pkgs/development/node-packages/default.nix
+++ pkgs/development/node-packages/default.nix
@@ -243,6 +243,11 @@ let
             url = "https://github.com/svanderburg/node2nix/commit/58736093161f2d237c17e75a96529b018cd0ac64.patch";
             sha256 = "0sif7803c9g6gjmmdniw5qxrq5igiz9nqdmdrcf1hxfi5x43a32h";
           })
+          # Extract common logic from composePackage to a shell function
+          (fetchpatch {
+            url = "https://github.com/svanderburg/node2nix/commit/e4c951971df6c9f9584c7252971c13b55c369916.patch";
+            sha256 = "0w8fcyr12g2340rn06isv40jkmz2khmak81c95zpkjgipzx7hp7w";
+          })
         ];
       };
       postInstall = ''
