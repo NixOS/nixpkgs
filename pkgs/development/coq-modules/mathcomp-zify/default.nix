51ed94db8bcb57e0ec82cf9c0ab6325a256df864
Théo Zimmermann <theo.zimmermann@inria.fr>
2021-10-15 12:10:59 +0200

    coqPackages.mathcomp-zify: 1.0.0+1.12+8.13 -> 1.1.0+1.12+8.13

--- pkgs/development/coq-modules/mathcomp-zify/default.nix
+++ pkgs/development/coq-modules/mathcomp-zify/default.nix
@@ -8,10 +8,11 @@ with lib; mkCoqDerivation rec {
 
   defaultVersion = with versions;
      switch [ coq.coq-version mathcomp-algebra.version ] [
-       { cases = [ (isEq "8.13") (isEq "1.12") ]; out = "1.0.0+1.12+8.13"; }
+       { cases = [ (range "8.13" "8.14") (isEq "1.12") ]; out = "1.1.0+1.12+8.13"; }
      ] null;
 
   release."1.0.0+1.12+8.13".sha256 = "1j533vx6lacr89bj1bf15l1a0s7rvrx4l00wyjv99aczkfbz6h6k";
+  release."1.1.0+1.12+8.13".sha256 = "1plf4v6q5j7wvmd5gsqlpiy0vwlw6hy5daq2x42gqny23w9mi2pr";
 
   propagatedBuildInputs = [ mathcomp-algebra ];
 
