91a115abfa847b043ba81a841323ac2f804fd108
R. RyanTM <ryantm-bot@ryantm.com>
2021-10-15 08:59:05 +0000

    python38Packages.google-cloud-redis: 2.2.4 -> 2.3.0

--- pkgs/development/python-modules/google-cloud-redis/default.nix
+++ pkgs/development/python-modules/google-cloud-redis/default.nix
@@ -11,11 +11,11 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-redis";
-  version = "2.2.4";
+  version = "2.3.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-3gbCBb+jnz4hE2T02JsSGxoFSIVg2UDTOaRMKCXh1vg=";
+    sha256 = "3b53fde67a97718642d29ac26b1b7608e7581b37d1e468f3c2ae38ea6cf7308f";
   };
 
   propagatedBuildInputs = [ google-api-core libcst proto-plus ];
