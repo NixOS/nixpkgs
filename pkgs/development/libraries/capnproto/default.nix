6ecdd5dadea26b48ba01fe7a12872b5d7c7e24f5
Jim Garrison <jim@garrison.cc>
2021-10-16 13:29:23 -0400

    ekam: simplify comment in capnproto/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/development/libraries/capnproto/default.nix
+++ pkgs/development/libraries/capnproto/default.nix
@@ -4,10 +4,7 @@ stdenv.mkDerivation rec {
   pname = "capnproto";
   version = "0.9.0";
 
-  # We fetch from GitHub, rather than use a release tarball, so that
-  # ekam (pkgs/development/tools/build-managers/ekam/default.nix) will
-  # build successfully.  Building ekam requires certain ekam rule
-  # files that are in capnproto.git but are not in release tarballs.
+  # release tarballs are missing some ekam rules
   src = fetchFromGitHub {
     owner = "capnproto";
     repo = "capnproto";
