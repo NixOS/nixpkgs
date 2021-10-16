d73a07ba57ead389d4e4418245ab0214aec35fb3
milahu <milahu@gmail.com>
2021-10-16 15:01:15 +0200

    fetch-kde-qt.sh: use mirrorlist files to get sha256 sums

--- maintainers/scripts/fetch-kde-qt.sh
+++ maintainers/scripts/fetch-kde-qt.sh
@@ -14,16 +14,24 @@ fi
 
 tmp=$(mktemp -d)
 pushd $tmp >/dev/null
+
+# use mirrorlist files to get sha256 sums
+# patch WGET_ARGS from nixpkgs/pkgs/development/libraries/qt-6/6.2/fetch.sh etc
+[ "${WGET_ARGS[-1]}" = '*.tar.xz' ] && WGET_ARGS[-1]='*.tar.xz.mirrorlist'
+
+echo 'fetching mirrorlist files ...'
+echo wget -nH -r -c --no-parent "${WGET_ARGS[@]}"
 wget -nH -r -c --no-parent "${WGET_ARGS[@]}" >/dev/null
 
 csv=$(mktemp)
-find . -type f | while read src; do
+find . -type f -name '*.mirrorlist' | while read mirrorlist; do
+    src="${mirrorlist%.*}" # remove extension
     # Sanitize file name
     filename=$(basename "$src" | tr '@' '_')
     nameVersion="${filename%.tar.*}"
     name=$(echo "$nameVersion" | sed -e 's,-[[:digit:]].*,,' | sed -e 's,-opensource-src$,,' | sed -e 's,-everywhere-src$,,')
     version=$(echo "$nameVersion" | sed -e 's,^\([[:alpha:]][[:alnum:]]*-\)\+,,')
-    echo "$name,$version,$src,$filename" >>$csv
+    echo "$name,$version,$src,$filename,$mirrorlist" >>$csv
 done
 
 cat >"$SRCS" <<EOF
@@ -39,8 +47,9 @@ gawk -F , "{ print \$1 }" $csv | sort | uniq | while read name; do
     latestVersion=$(echo "$versions" | sort -rV | head -n 1)
     src=$(gawk -F , "/^$name,$latestVersion,/ { print \$3 }" $csv)
     filename=$(gawk -F , "/^$name,$latestVersion,/ { print \$4 }" $csv)
+    mirrorlist=$(gawk -F , "/^$name,$latestVersion,/ { print \$5 }" $csv)
     url="${src:2}"
-    sha256=$(nix-hash --type sha256 --base32 --flat "$src")
+    sha256=$(grep -F '>SHA-256 Hash<' "$mirrorlist" | sed -E 's|^.*<tt>([0-9a-f]{64})</tt>.*|\1|')
     cat >>"$SRCS" <<EOF
   $name = {
     version = "$latestVersion";
