29e14f8a72530d059a1eed65ab2c772cc91e76be
Maximilian Bosch <maximilian@mbosch.me>
2021-10-17 19:04:58 +0200

    polyml*: fix build w/glibc-2.34
    Failing Hydra build: https://hydra.nixos.org/build/156205993

--- pkgs/development/compilers/polyml/5.7.nix
+++ pkgs/development/compilers/polyml/5.7.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, autoreconfHook, gmp, libffi }:
+{ lib, stdenv, fetchFromGitHub, autoreconfHook, gmp, libffi, fetchpatch }:
 
 stdenv.mkDerivation rec {
   pname = "polyml";
@@ -8,7 +8,15 @@ stdenv.mkDerivation rec {
     substituteInPlace configure.ac --replace stdc++ c++
   '';
 
-  patches = [ ./5.7-new-libffi-FFI_SYSV.patch ];
+  patches = [
+    ./5.7-new-libffi-FFI_SYSV.patch
+
+    # glibc 2.34 compat
+    (fetchpatch {
+      url = "https://src.fedoraproject.org/rpms/polyml/raw/4d8868ca5a1ce3268f212599a321f8011c950496/f/polyml-pthread-stack-min.patch";
+      sha256 = "1h5ihg2sxld9ymrl3f2mpnbn2242ka1fsa0h4gl9h90kndvg6kby";
+    })
+  ];
 
   buildInputs = [ libffi gmp ];
 
