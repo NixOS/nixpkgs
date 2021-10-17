f378420360a278e317b6cb14bfce1a305acbbedb
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2021-10-17 11:40:07 -0500

    binutils: 2.35.1 → 2.35.2
    CVEs:
    
    - https://nvd.nist.gov/vuln/detail/CVE-2020-35448 (3.3 Low)
    - https://nvd.nist.gov/vuln/detail/CVE-2021-20284 (5.5 Medium)
    - https://nvd.nist.gov/vuln/detail/CVE-2021-20294 (7.8 High)

--- pkgs/development/tools/misc/binutils/default.nix
+++ pkgs/development/tools/misc/binutils/default.nix
@@ -27,7 +27,7 @@ assert gold -> execFormatIsELF stdenv.targetPlatform;
 let
   reuseLibs = enableShared && withAllTargets;
 
-  version = "2.35.1";
+  version = "2.35.2";
   basename = "binutils";
   # The targetPrefix prepended to binary names to allow multiple binuntils on the
   # PATH to both be usable.
@@ -42,7 +42,7 @@ let
   # HACK to ensure that we preserve source from bootstrap binutils to not rebuild LLVM
   normal-src = stdenv.__bootPackages.binutils-unwrapped.src or (fetchurl {
     url = "mirror://gnu/binutils/${basename}-${version}.tar.bz2";
-    sha256 = "sha256-Mg56HQ9G/Nn0E/EEbiFsviO7K85t62xqYzBEJeSLGUI=";
+    sha256 = "sha256-z6dkTb7PRZHhNutAfBwdoWV4vSsD8MLorNzroZS7nWE=";
   });
 in
 
