3c5ffd288e80020a781ff1101dc223488ef46e88
Artturin <Artturin@artturin.com>
2021-10-19 00:59:16 +0300

    python3Packages.mujson: init at 1.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4942,6 +4942,8 @@ in {
 
   mutf8 = callPackage ../development/python-modules/mutf8 { };
 
+  mujson = callPackage ../development/python-modules/mujson { };
+
   mwclient = callPackage ../development/python-modules/mwclient { };
 
   mwdblib = callPackage ../development/python-modules/mwdblib { };
