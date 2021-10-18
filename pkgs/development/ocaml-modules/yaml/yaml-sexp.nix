5f13a342524ad192ecfb3229f8e10fc485d89757
Jules Aguillon <juloo.dsi@gmail.com>
2021-10-18 16:05:31 +0200

    ocamlPackages.yaml-sexp: Inherit from the main package

--- pkgs/development/ocaml-modules/yaml/yaml-sexp.nix
+++ pkgs/development/ocaml-modules/yaml/yaml-sexp.nix
@@ -2,22 +2,12 @@
 
 buildDunePackage rec {
   pname = "yaml-sexp";
-  version = "3.0.0";
 
-  useDune2 = true;
-
-  src = fetchurl {
-    url =
-      "https://github.com/avsm/ocaml-yaml/releases/download/v${version}/yaml-v${version}.tbz";
-    sha256 = "1iws6lbnrrd5hhmm7lczfvqp0aidx5xn7jlqk2s5rjfmj9qf4j2c";
-  };
+  inherit (yaml) version src useDune2;
 
   propagatedBuildInputs = [ yaml ppx_sexp_conv sexplib ];
 
-  meta = {
+  meta = yaml.meta // {
     description = "ocaml-yaml with sexp support";
-    homepage = "https://github.com/avsm/ocaml-yaml";
-    license = lib.licenses.isc;
-    maintainers = [ lib.maintainers.vbgl ];
   };
 }
