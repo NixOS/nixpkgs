38e49939da26fbe6b0315c65e1c65021ead382e8
Artur Cygan <arczicygan@gmail.com>
2021-10-18 17:41:19 +0200

    protobuf: 3.18.0 -> 3.18.1

--- pkgs/development/libraries/protobuf/3.18.nix
+++ pkgs/development/libraries/protobuf/3.18.nix
@@ -1,6 +1,6 @@
 { callPackage, ... }:
 
 callPackage ./generic-v3.nix {
-  version = "3.18.0";
-  sha256 = "0nhjw4m4dm6wqwwsi0b18js5wbh3ibrpsq195g6mk9cx54fx097f";
+  version = "3.18.1";
+  sha256 = "sha256-5PK0uuk7Du9hX/hHy0gninHIKu6b8THdfVh87Gn7y2Q=";
 }
