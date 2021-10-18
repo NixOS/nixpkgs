9e7a6dfe375d1ae6df78ca3a17b24556e9dd5e74
Fabian Affolter <mail@fabian-affolter.ch>
2021-10-18 23:49:20 +0200

    kubescape: 1.0.120 -> 1.0.123

--- pkgs/tools/security/kubescape/default.nix
+++ pkgs/tools/security/kubescape/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "kubescape";
-  version = "1.0.120";
+  version = "1.0.123";
 
   src = fetchFromGitHub {
     owner = "armosec";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-aFFJAib0/FTOaPtSLYXIFV3+QfIpzy8fC7rWAQW5yh0=";
+    sha256 = "sha256-mrpQXIcV1KxOLDhWAS9Og76k18jsey7vPAcbhGe+cN4=";
   };
 
-  vendorSha256 = "sha256-vN+ci2vCbtDuEEVzZQiFkdi1QkMgnnbbJgD9g6DS7qs=";
+  vendorSha256 = "sha256-4v/7JrSGZKOyLwU6U3+hc9P+kzxGGj4aXG/wXmNdo+M=";
 
   # One test is failing, disabling for now
   doCheck = false;
