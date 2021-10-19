735f1744220162fcb719305c425a6b67f7cc7a6d
Peter Hoeg <peter@hoeg.com>
2021-10-19 14:02:30 +0800

    ddcutil: 1.1.0 -> 1.2.0

--- pkgs/tools/misc/ddcutil/default.nix
+++ pkgs/tools/misc/ddcutil/default.nix
@@ -1,33 +1,49 @@
-{ lib, stdenv, fetchFromGitHub, autoreconfHook, pkg-config
-, glib, i2c-tools, udev, libgudev, libusb1, libdrm, xorg }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, autoreconfHook
+, pkg-config
+, glib
+, i2c-tools
+, udev
+, kmod
+, libgudev
+, libusb1
+, libdrm
+, xorg
+}:
 
 stdenv.mkDerivation rec {
   pname = "ddcutil";
-  version = "1.1.0";
+  version = "1.2.0";
 
   src = fetchFromGitHub {
-    owner  = "rockowitz";
-    repo   = "ddcutil";
-    rev    = "v${version}";
-    sha256 = "0wv8a8zjahzmi4qx0lc24mwyi3jklj1yxqq26fwklmfh5dv1y8yc";
+    owner = "rockowitz";
+    repo = "ddcutil";
+    rev = "v${version}";
+    sha256 = "sha256-MH7WnJgROgLvttQEbd/21mLRFElObp4KINy8RarqkRo=";
   };
 
-  patches = [
-    # Look for kernel modules in /run/booted-system/kernel-modules/lib/modules/*
-    ./nixos-paths.diff
+  nativeBuildInputs = [ autoreconfHook pkg-config ];
+
+  buildInputs = [
+    glib
+    i2c-tools
+    kmod
+    libdrm
+    libgudev
+    libusb1
+    udev
+    xorg.libXrandr
   ];
 
-  nativeBuildInputs = [ autoreconfHook pkg-config ];
-  buildInputs = [
-    i2c-tools udev libgudev
-    glib libusb1 libdrm xorg.libXrandr
-  ];
+  enableParallelBuilding = true;
 
   meta = with lib; {
-    homepage    = "http://www.ddcutil.com/";
+    homepage = "http://www.ddcutil.com/";
     description = "Query and change Linux monitor settings using DDC/CI and USB";
-    license     = licenses.gpl2;
-    platforms   = platforms.linux;
+    license = licenses.gpl2;
+    platforms = platforms.linux;
     maintainers = with maintainers; [ rnhmjoj ];
   };
 }
