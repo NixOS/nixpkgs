aa7fb09313460cce452e008b4ca79d1f5eaf937d
superherointj <5861043+superherointj@users.noreply.github.com>
2021-10-19 16:05:53 -0300

    grass: mark as broken

--- pkgs/applications/gis/grass/default.nix
+++ pkgs/applications/gis/grass/default.nix
@@ -1,35 +1,55 @@
-{ lib, stdenv, fetchFromGitHub, flex, bison, pkg-config, zlib, libtiff, libpng, fftw
-, cairo, readline, ffmpeg_3, makeWrapper, wxGTK30, netcdf, blas
-, proj, gdal, geos, sqlite, postgresql, libmysqlclient, python2Packages, libLAS, proj-datumgrid
+{ bison
+, blas
+, cairo
+, fetchFromGitHub
+, ffmpeg
+, fftw
+, flex
+, gdal
+, geos
+, lib
+, libLAS
+, libpng
+, libmysqlclient
+, libtiff
+, makeWrapper
+, netcdf
+, pkg-config
+, postgresql
+, proj_7
+, proj-datumgrid
+, python3Packages
+, readline
+, sqlite
+, stdenv
+, wxGTK30
+, zstd
 }:
 
+let proj = proj_7; in
 stdenv.mkDerivation rec {
   name = "grass";
-  version = "7.6.1";
+  version = "7.8.6";
 
-  src = with lib; fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "OSGeo";
     repo = "grass";
-    rev = "${name}_${replaceStrings ["."] ["_"] version}";
-    sha256 = "1amjk9rz7vw5ha7nyl5j2bfwj5if9w62nlwx5qbp1x7spldimlll";
+    rev = version;
+    sha256 = "1cs9zrg0gmfp37frmc8gnk776ms1bcj353gghqz20dxidcanmxnf";
   };
 
-  nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ flex bison zlib proj gdal libtiff libpng fftw sqlite cairo proj
-  readline ffmpeg_3 makeWrapper wxGTK30 netcdf geos postgresql libmysqlclient blas
-  libLAS proj-datumgrid ]
-    ++ (with python2Packages; [ python python-dateutil wxPython30 numpy ]);
-
+  # FIXME:
   # On Darwin the installer tries to symlink the help files into a system
   # directory
-  patches = [ ./no_symbolic_links.patch ];
+  # patches = [ ./no_symbolic_links.patch ];
 
-  # Correct mysql_config query
-  patchPhase = ''
-      substituteInPlace configure --replace "--libmysqld-libs" "--libs"
+  postPatch = ''
+    # Correct mysql_config query
+    substituteInPlace configure --replace "--libmysqld-libs" "--libs"
   '';
 
   configureFlags = [
+    "--with-proj=${proj}/bin/proj"
     "--with-proj-share=${proj}/share/proj"
     "--with-proj-includes=${proj.dev}/include"
     "--with-proj-lib=${proj}/lib"
@@ -37,6 +57,7 @@ stdenv.mkDerivation rec {
     "--with-readline"
     "--with-wxwidgets"
     "--with-netcdf"
+    "--with-bzlib"
     "--with-geos"
     "--with-postgres"
     "--with-postgres-libs=${postgresql.lib}/lib/"
@@ -51,6 +72,7 @@ stdenv.mkDerivation rec {
   # Otherwise a very confusing "Can't load GDAL library" error
   makeFlags = lib.optional stdenv.isDarwin "GDAL_DYNAMIC=";
 
+  # FIXME: postConfigure needs fixing. List of files may have changed.
   /* Ensures that the python script run at build time are actually executable;
    * otherwise, patchShebangs ignores them.  */
   postConfigure = ''
@@ -87,22 +109,60 @@ stdenv.mkDerivation rec {
 
   NIX_CFLAGS_COMPILE = "-DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1";
 
+  enableParallelBuilding = true;
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    bison
+    blas
+    cairo
+    ffmpeg
+    fftw
+    flex
+    gdal
+    geos
+    libLAS
+    libmysqlclient
+    libpng
+    libtiff
+    makeWrapper
+    netcdf
+    postgresql
+    proj
+    proj-datumgrid
+    readline
+    sqlite
+    wxGTK30
+    zstd
+  ] ++ (with python3Packages; [
+    python
+    dateutil
+    wxPython
+    numpy
+    pyproj
+    #vecLib # Missing: https://pypi.org/project/vecLib/
+    zstandard
+    zstd
+  ]);
+
   postInstall = ''
-    wrapProgram $out/bin/grass76 \
+    wrapProgram $out/bin/grass${lib.versions.majorMinor version} \
     --set PYTHONPATH $PYTHONPATH \
-    --set GRASS_PYTHON ${python2Packages.python}/bin/${python2Packages.python.executable} \
+    --set GRASS_PYTHON ${python3Packages.python}/bin/${python3Packages.python.executable} \
     --suffix LD_LIBRARY_PATH ':' '${gdal}/lib'
     ln -s $out/grass*/lib $out/lib
     ln -s $out/grass*/include $out/include
   '';
 
-  enableParallelBuilding = true;
-
   meta = {
+    broken = true; # 2021-10-19
     homepage = "https://grass.osgeo.org/";
     description = "GIS software suite used for geospatial data management and analysis, image processing, graphics and maps production, spatial modeling, and visualization";
     license = lib.licenses.gpl2Plus;
     platforms = lib.platforms.all;
-    maintainers = with lib.maintainers; [mpickering];
+    maintainers = with lib.maintainers; [ mpickering ];
   };
 }
