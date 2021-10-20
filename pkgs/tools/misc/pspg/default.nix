b5780d4d3919a760f1b083db9b1377339db1a1c9
Mario Rodas <marsam@users.noreply.github.com>
2021-10-19 20:51:20 -0500

    pspg: 5.3.5 -> 5.4.1

--- pkgs/tools/misc/pspg/default.nix
+++ pkgs/tools/misc/pspg/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "pspg";
-  version = "5.3.5";
+  version = "5.4.1";
 
   src = fetchFromGitHub {
     owner = "okbob";
     repo = pname;
     rev = version;
-    sha256 = "sha256-xJ7kgEvIsTufAZa5x3YpElTc74nEs9C+baVjbheHySM=";
+    sha256 = "1pi9dbjhd2mj11p3hx74f3rr8ysriq7sy2x012r0kil3b5xzpv2p";
   };
 
   nativeBuildInputs = [ pkg-config installShellFiles ];
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
   makeFlags = [ "PREFIX=${placeholder "out"}" ];
 
   postInstall = ''
-    installShellCompletion --bash --name pspg.bash bash-completion.sh
+    installShellCompletion --bash --cmd pspg bash-completion.sh
   '';
 
   meta = with lib; {
