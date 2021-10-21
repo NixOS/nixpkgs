aa2d3c77a53940a606062b7aa23357db3d21b001
AndersonTorres <torres.anderson.85@protonmail.com>
2021-10-21 16:58:52 -0300

    rakshasa.rtorrent: 0.9.8 -> 0.9.8+date=2021-08-07

--- pkgs/tools/networking/p2p/rakshasa-rtorrent/rtorrent.nix
+++ pkgs/tools/networking/p2p/rakshasa-rtorrent/rtorrent.nix
@@ -1,39 +1,54 @@
-{ lib, stdenv, fetchurl, fetchFromGitHub, pkg-config
-, libtool, autoconf, automake, cppunit
-, libtorrent, ncurses, libsigcxx, curl
-, zlib, openssl, xmlrpc_c
-
-# This no longer works
-, colorSupport ? false
+{ lib
+, stdenv
+, fetchurl
+, fetchFromGitHub
+, autoreconfHook
+, autoconf-archive
+, cppunit
+, curl
+, libsigcxx
+, libtool
+, libtorrent
+, ncurses
+, openssl
+, pkg-config
+, xmlrpc_c
+, zlib
 }:
 
 stdenv.mkDerivation rec {
-  pname = "rtorrent";
-  version = "0.9.8";
+  pname = "rakshasa-rtorrent";
+  version = "0.9.8+date=2021-08-07";
 
   src = fetchFromGitHub {
     owner = "rakshasa";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "0hcaf1brk402caa7hhlb2r1c93mjzxkm8gb19xfl33gkp3jpf372";
+    repo = "rtorrent";
+    rev = "a6bc99bb821d86b3b0633552db3fbd0a22497657";
+    hash = "sha256-HTwAs8dfZVXfLRNiT6QpjKGnuahHfoMfYWqdKkedUL0=";
   };
 
-  nativeBuildInputs = [ pkg-config ];
-  buildInputs = [
-    libtool autoconf automake cppunit
-    libtorrent ncurses libsigcxx curl zlib openssl xmlrpc_c
+  nativeBuildInputs = [
+    autoconf-archive
+    autoreconfHook
+    pkg-config
   ];
 
-  # Optional patch adds support for custom configurable colors
-  # https://github.com/Chlorm/chlorm_overlay/blob/master/net-p2p/rtorrent/README.md
-  patches = lib.optional colorSupport (fetchurl {
-    url = "https://gist.githubusercontent.com/codyopel/a816c2993f8013b5f4d6/raw/b952b32da1dcf14c61820dfcf7df00bc8918fec4/rtorrent-color.patch";
-    sha256 = "00gcl7yq6261rrfzpz2k8bd7mffwya0ifji1xqcvhfw50syk8965";
-  });
+  buildInputs = [
+    cppunit
+    curl
+    libsigcxx
+    libtool
+    libtorrent
+    ncurses
+    openssl
+    xmlrpc_c
+    zlib
+  ];
 
-  preConfigure = "./autogen.sh";
-
-  configureFlags = [ "--with-xmlrpc-c" "--with-posix-fallocate" ];
+  configureFlags = [
+    "--with-xmlrpc-c"
+    "--with-posix-fallocate"
+  ];
 
   enableParallelBuilding = true;
 
@@ -46,9 +61,8 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     homepage = "https://rakshasa.github.io/rtorrent/";
     description = "An ncurses client for libtorrent, ideal for use with screen, tmux, or dtach";
-
-    platforms = platforms.unix;
-    maintainers = with maintainers; [ ebzzry codyopel ];
     license = licenses.gpl2Plus;
+    maintainers = with maintainers; [ ebzzry codyopel ];
+    platforms = platforms.unix;
   };
 }
