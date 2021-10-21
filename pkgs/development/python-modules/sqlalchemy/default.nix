5332488c849275c39cc1e7e96b5faf69221a032e
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-10-21 21:03:00 +0200

    python3Packages.sqlalchemy: 1.4.25 -> 1.4.26

--- pkgs/development/python-modules/sqlalchemy/default.nix
+++ pkgs/development/python-modules/sqlalchemy/default.nix
@@ -13,11 +13,11 @@
 
 buildPythonPackage rec {
   pname = "SQLAlchemy";
-  version = "1.4.25";
+  version = "1.4.26";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-Gt89JeLjOvvNSM+tgHb5N4eTvkPn/sPkM0MGysa+wTg=";
+    sha256 = "sha256-a8f519kO9V6MbbEwioYZzY9A4ko091kRm5XnKE3KNRo=";
   };
 
   propagatedBuildInputs = [
