af97bf5624a4bf22d7e50fded4a23e32231ab4f7
Bobby Rong <rjl931189261@126.com>
2021-10-21 17:07:23 +0800

    pantheon.file-roller: init

--- pkgs/desktops/gnome/apps/file-roller/default.nix
+++ pkgs/desktops/gnome/apps/file-roller/default.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , fetchurl
+, fetchpatch
 , desktop-file-utils
 , gettext
 , glibcLocales
@@ -20,7 +21,9 @@
 , libarchive
 , libnotify
 , nautilus
+, pantheon
 , unzip
+, withPantheon ? false
 }:
 
 stdenv.mkDerivation rec {
@@ -31,6 +34,15 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
     sha256 = "039w1dcpa5ypmv6sm634alk9vbcdkyvy595vkh5gn032jsiqca2a";
   };
+
+  patches = lib.optionals withPantheon [
+    # Make this respect dark mode settings from Pantheon
+    # https://github.com/elementary/fileroller/
+    (fetchpatch {
+      url = "https://raw.githubusercontent.com/elementary/fileroller/f183eac36c68c9c9441e72294d4e305cf5fe36ed/fr-application-prefers-color-scheme.patch";
+      sha256 = "sha256-d/sqf4Oen9UrzYqru7Ck15o/6g6WfxRDH/iAGFXgYAA=";
+    })
+  ];
 
   LANG = "en_US.UTF-8"; # postinstall.py
 
@@ -57,6 +69,8 @@ stdenv.mkDerivation rec {
     libarchive
     libnotify
     nautilus
+  ] ++ lib.optionals withPantheon [
+    pantheon.granite
   ];
 
   PKG_CONFIG_LIBNAUTILUS_EXTENSION_EXTENSIONDIR = "${placeholder "out"}/lib/nautilus/extensions-3.0";
@@ -86,6 +100,6 @@ stdenv.mkDerivation rec {
     description = "Archive manager for the GNOME desktop environment";
     license = licenses.gpl2Plus;
     platforms = platforms.linux;
-    maintainers = teams.gnome.members;
+    maintainers = teams.gnome.members ++ teams.pantheon.members;
   };
 }
