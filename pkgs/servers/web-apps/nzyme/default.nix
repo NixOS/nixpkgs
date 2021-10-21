7afe5467b68c1720cffad103d9c5efde00533b5c
Jakob Hagl <jakob.hagl@hellothere.at>
2021-10-21 15:39:18 +0200

    nzyme: restructure default.nix

--- pkgs/servers/web-apps/nzyme/default.nix
+++ pkgs/servers/web-apps/nzyme/default.nix
@@ -1,26 +1,35 @@
-# a simple derivation for nyzme, a wIDS for the raspberry pi
-
-
 { stdenv, lib, fetchurl, jre }:
 stdenv.mkDerivation rec {
-    pname = "nzyme";
-    version = "1.1.1";
+  pname = "nzyme";
+  version = "1.1.1";
 
-    builder = ./builder.sh;
+  src = fetchurl {
+    url = "https://assets.nzyme.org/releases/${pname}-${version}.jar";
+    sha256 = "1ygbblgqqwad389g2szabdacc218b4bvm240fk4l73x7w8halr9b";
+  };
 
-    src = fetchurl {
-        url = "https://assets.nzyme.org/releases/${pname}-${version}.jar";
-        sha256 = "1ygbblgqqwad389g2szabdacc218b4bvm240fk4l73x7w8halr9b";
-    };
+  buildPhase = ''
+    jar=$src
 
-    buildInputs = [ ];
-    dontUnpack = true;
-    inherit jre;
+    mkdir -p $out/share/java
+    cp $jar $out/share/java/nzyme.jar
 
-    meta = {
-        homepage = "https://www.nzyme.org/";
-        description = "A program to detect attacks against wireless networks";
-        license = lib.licenses.sspl;
-        maintainers = with lib.maintainers; [ jakobu5 ];
-    };
+    mkdir -p $out/bin
+    cat > $out/bin/nzyme <<EOF
+    #! $SHELL -e
+    exec $jre/bin/java -jar $out/share/java/nzyme.jar --config-file \$1
+    EOF
+    chmod +x $out/bin/nzyme
+  '';
+
+  inherit jre;
+  dontInstall = true;
+  dontUnpack = true;
+
+  meta = with lib; {
+    homepage = "https://www.nzyme.org/";
+    description = "A program to detect attacks against wireless networks";
+    license = licenses.sspl;
+    maintainers = with maintainers; [ jakobu5 ];
+  };
 }
