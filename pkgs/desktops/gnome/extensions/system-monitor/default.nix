defa45a3bede25020b93fad7e4e3da3410b9f9d5
Anders Kaseorg <andersk@mit.edu>
2021-10-22 02:20:22 -0700

    gnomeExtensions.system-monitor: Change maintainer to myself
    At the previous maintainer’s request.
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- pkgs/desktops/gnome/extensions/system-monitor/default.nix
+++ pkgs/desktops/gnome/extensions/system-monitor/default.nix
@@ -52,7 +52,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Display system informations in gnome shell status bar";
     license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ tiramiseb ];
+    maintainers = with maintainers; [ andersk ];
     homepage = "https://github.com/paradoxxxzero/gnome-shell-system-monitor-applet";
   };
 }
