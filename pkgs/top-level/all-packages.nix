8a9447d7fe1cc760dbf4c1baa6ccb41703f0d91e
Yannik Rödel <hey@yannik.info>
2021-10-22 11:13:04 +0200

    winbox: init at 3.31

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -968,6 +968,11 @@ with pkgs;
 
   vopono = callPackage ../tools/networking/vopono { };
 
+  winbox = callPackage ../tools/admin/winbox {
+    wine = wineWowPackages.staging;
+    use64 = true;
+  };
+
   xcd = callPackage ../tools/misc/xcd { };
 
   xtrt = callPackage ../tools/archivers/xtrt { };
