NixOS/nixpkgs

commit ec4a1591007265384c0f036e265e5e38eff83231

transmission: fixes to make one test work again
Broken by 37134b607 (PR #134007).

pkgs/tools/networking/miniupnpc/default.nix
@@ -19,6 +19,8 @@ let
 
       makeFlags = [ "PREFIX=$(out)" "INSTALLPREFIX=$(out)" ];
 
+      postInstall = ''chmod +x "$out"/lib/libminiupnpc.so'';
+
       meta = with lib; {
         homepage = "http://miniupnp.free.fr/";
         description = "A client that implements the UPnP Internet Gateway Device (IGD) specification";

pkgs/applications/networking/p2p/transmission/default.nix
@@ -97,7 +97,7 @@ in stdenv.mkDerivation {
       include <abstractions/nameservice>
       include <abstractions/ssl_certs>
       include "${apparmorRulesFromClosure { name = "transmission-daemon"; } ([
-        curl libevent openssl pcre zlib
+        curl libevent openssl pcre zlib libnatpmp miniupnpc
       ] ++ lib.optionals enableSystemd [ systemd ]
         ++ lib.optionals stdenv.isLinux [ inotify-tools ]
       )}"
@@ -116,6 +116,7 @@ in stdenv.mkDerivation {
   '';
 
   passthru.tests = {
+    apparmor = nixosTests.transmission; # starts the service with apparmor enabled
     smoke-test = nixosTests.bittorrent;
   };
 

commit d82c73ce17d7610bafb0d891e32bb69f390d2111

miniupnpc: fix build on darwin again
Broken by ec4a15910072 (PR #139638); I forgot darwin again.

pkgs/tools/networking/miniupnpc/default.nix
@@ -19,7 +19,9 @@ let
 
       makeFlags = [ "PREFIX=$(out)" "INSTALLPREFIX=$(out)" ];
 
-      postInstall = ''chmod +x "$out"/lib/libminiupnpc.so'';
+      postInstall = ''
+        chmod +x "$out"/lib/libminiupnpc${stdenv.hostPlatform.extensions.sharedLibrary}
+      '';
 
       meta = with lib; {
         homepage = "http://miniupnp.free.fr/";