1c6b48a4234869f4db35d96240b834e5e9137c01
Ryan Burns <rtburns@protonmail.com>
2021-10-22 18:21:09 -0700

    build2: 0.13.0 -> 0.14.0
    This is a breaking update (at least for the internal C++ APIs) so the
    packages all need to be updated in lockstep.
    
    Misc notes:
    
    * build2
      * Remove unnecessary pkg-config patch
      * Trivially refresh remove-config-store-paths.patch
    * libbutl
      * Enable tests now that they work with NDEBUG
    * libodb
      * update 2.5.0-b.19 -> 2.5.0-b.21

--- pkgs/development/libraries/libodb/default.nix
+++ pkgs/development/libraries/libodb/default.nix
@@ -6,13 +6,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "libodb";
-  version = "2.5.0-b.19";
+  version = "2.5.0-b.21";
 
   outputs = [ "out" "dev" "doc" ];
 
   src = fetchurl {
     url = "https://pkg.cppget.org/1/beta/odb/libodb-${version}.tar.gz";
-    sha256 = "8180d9d40d8e74ed25b1712953f19379a29abdee3896ae98ba9ade35846adb39";
+    sha256 = "sha256-Q4HZ8zU5osZ9Phz59ZAjXh0dbB8ELBY5gMRbDnawCWs=";
   };
 
   nativeBuildInputs = [ build2 ];
