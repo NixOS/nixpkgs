65253522a92963322acc1fb46c2f7e5db6c55fd0
P. R. d. O <d.ol.rod@protonmail.com>
2021-10-24 23:21:15 -0600

    pocket-casts: init at 0.5.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3389,6 +3389,8 @@ with pkgs;
 
   pn = callPackage ../tools/text/pn { };
 
+  pocket-casts = callPackage ../applications/audio/pocket-casts { };
+
   poweralertd = callPackage ../tools/misc/poweralertd { };
 
   ps_mem = callPackage ../tools/system/ps_mem { };
