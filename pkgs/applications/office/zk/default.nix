e748f6762d14852a27ab6245d1d021762efc032b
Pablo Ovelleiro Corral <github@pablo.tools>
2021-10-26 16:25:03 +0200

    Update pkgs/applications/office/zk/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/applications/office/zk/default.nix
+++ pkgs/applications/office/zk/default.nix
@@ -4,9 +4,6 @@ buildGoModule rec {
   pname = "zk";
   version = "0.7.0";
 
-  vendorSha256 = "sha256-m7QGv8Vx776TsN7QHXtO+yl3U1D573UMZVyg1B4UeIk=";
-
-  doCheck = false;
 
   src = fetchFromGitHub {
     owner = "mickael-menu";
@@ -14,6 +11,10 @@ buildGoModule rec {
     rev = "v${version}";
     sha256 = "sha256-C3/V4v8lH4F3S51egEw5d51AI0n5xzBQjwhrI64FEGA=";
   };
+
+  vendorSha256 = "sha256-m7QGv8Vx776TsN7QHXtO+yl3U1D573UMZVyg1B4UeIk=";
+
+  doCheck = false;
 
   buildInputs = [ icu ];
 
