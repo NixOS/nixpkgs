78d0ded5e5bb8d9a83023e5a8d53fb08fb58288e
Jeremy Kolb <kjeremy@gmail.com>
2021-10-26 08:32:14 -0400

    uniscribe: init at 1.7.0
    Fixes #141890

---
+++ pkgs/tools/text/uniscribe/default.nix
@@ -0,0 +1,30 @@
+{ stdenv, lib, bundlerEnv, bundlerUpdateScript, makeWrapper }:
+
+let
+  rubyEnv = bundlerEnv {
+    name = "uniscribe";
+    gemdir = ./.;
+  };
+in
+stdenv.mkDerivation rec {
+  pname = "uniscribe";
+  version = (import ./gemset.nix).uniscribe.version;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  dontUnpack = true;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    makeWrapper ${rubyEnv}/bin/uniscribe $out/bin/uniscribe
+  '';
+
+  passthru.updateScript = bundlerUpdateScript "uniscribe";
+
+  meta = {
+    description = "Explains  Unicode characters/code points: Displays their name, category, and shows compositions";
+    homepage = "https://github.com/janlelis/uniscribe";
+    license = lib.licenses.mit;
+    maintainers = [];
+  };
+}
