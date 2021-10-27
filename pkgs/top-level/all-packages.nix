3ed95b14e289fdc6ffc13f94cf61e50500cb2140
nick black <dankamongmen@gmail.com>
2021-10-27 05:03:50 -0400

    notcurses: 2.4.2 -> 2.4.8 (#143031)
    * libraries/notcurses: 2.4.2 -> 2.4.8
    
    * notcurses: fix sri hash
    
    Co-authored-by: Jörg Thalheim <joerg@thalheim.io>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18579,9 +18579,7 @@ with pkgs;
 
   notify-sharp = callPackage ../development/libraries/notify-sharp { };
 
-  notcurses = callPackage ../development/libraries/notcurses {
-    readline = readline81;
-  };
+  notcurses = callPackage ../development/libraries/notcurses { };
 
   ncurses5 = ncurses.override {
     abiVersion = "5";
