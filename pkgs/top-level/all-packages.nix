5977dda6b5f695263feee0ab9df05f84eb3335ef
Lara <lara@uwu.is>
2021-10-27 07:46:58 +0000

    snipe-it: init at 5.4.3
    Snipe-IT is a web-based open source asset management system.
    
    https://snipeitapp.com/

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21972,6 +21972,8 @@ with pkgs;
 
   smcroute = callPackage ../servers/smcroute { };
 
+  snipe-it = callPackage ../servers/web-apps/snipe-it { };
+
   sogo = callPackage ../servers/web-apps/sogo { };
 
   spacecookie =
