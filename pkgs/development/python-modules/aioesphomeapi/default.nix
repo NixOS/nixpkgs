0f75792ef4da7f703b78036d8e6be59d87519380
R. Ryantm <ryantm-bot@ryantm.com>
2021-10-27 11:58:42 +0000

    python38Packages.aioesphomeapi: 10.0.3 -> 10.1.0

--- pkgs/development/python-modules/aioesphomeapi/default.nix
+++ pkgs/development/python-modules/aioesphomeapi/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "aioesphomeapi";
-  version = "10.0.3";
+  version = "10.1.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "esphome";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-9K67vmPaPSR6ODl3tpwwfRMY9DDD/2cCaDRRO38mHVU=";
+    sha256 = "1l97vlyyh12kxkvgfn2bsgc54zkv9aigjn833bhmcgnycxkzfzw0";
   };
 
   propagatedBuildInputs = [
