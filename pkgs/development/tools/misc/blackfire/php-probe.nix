dbee63ce914f652a01f9eeca78b3e97372ead463
R. Ryantm <ryantm-bot@ryantm.com>
2021-10-27 13:38:34 +0000

    php74Extensions.blackfire: 1.67.0 -> 1.69.0

--- pkgs/development/tools/misc/blackfire/php-probe.nix
+++ pkgs/development/tools/misc/blackfire/php-probe.nix
@@ -18,11 +18,11 @@ let
   }.${lib.versions.majorMinor php.version} or (throw "Unsupported PHP version.");
 in stdenv.mkDerivation rec {
   pname = "php-blackfire";
-  version = "1.67.0";
+  version = "1.69.0";
 
   src = fetchurl {
     url = "https://packages.blackfire.io/debian/pool/any/main/b/blackfire-php/blackfire-php_${version}_amd64.deb";
-    sha256 = "KeTNdI+zCRAdXqvIX3tnAZtTbcH0WAKUcRIOiCJn2aE=";
+    sha256 = "5wE6yCl4N6PJiL2up9y/me/Sg2hZ4HnIKsbuhDzyFco=";
   };
 
   nativeBuildInputs = [
