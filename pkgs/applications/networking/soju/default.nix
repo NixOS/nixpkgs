0582654eb22c99cdd3c1ced4624888eb2244679a
R. Ryantm <ryantm-bot@ryantm.com>
2021-10-28 18:01:09 +0000

    soju: 0.1.2 -> 0.2.2

--- pkgs/applications/networking/soju/default.nix
+++ pkgs/applications/networking/soju/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "soju";
-  version = "0.1.2";
+  version = "0.2.2";
 
   src = fetchFromSourcehut {
     owner = "~emersion";
     repo = "soju";
     rev = "v${version}";
-    sha256 = "sha256-dauhGfwSjjRt1vl2+OPhtcme/QaRNTs43heQVnI7oRU=";
+    sha256 = "sha256-ssq4fED7YIJkSHhxybBIqOr5qVEHGordBxuJOmilSOY=";
   };
 
-  vendorSha256 = "sha256-0JLbqqybLZ/cYyHAyNR4liAVJI2oIsHELJLWlQy0qjE=";
+  vendorSha256 = "sha256-60b0jhyXQg9RG0mkvUOmJOEGv96FZq/Iwv1S9c6C35c=";
 
   subPackages = [
     "cmd/soju"
