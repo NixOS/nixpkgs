5f0bce982f7621ad43ec240ecc9ef5d6fe37fd74
R. Ryantm <ryantm-bot@ryantm.com>
2021-10-28 15:24:16 +0000

    greetd.tuigreet: 0.6.1 -> 0.7.0

--- pkgs/os-specific/linux/tuigreet/default.nix
+++ pkgs/os-specific/linux/tuigreet/default.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "tuigreet";
-  version = "0.6.1";
+  version = "0.7.0";
 
   src = fetchFromGitHub {
     owner = "apognu";
     repo = pname;
     rev = version;
-    sha256 = "sha256-Exw3HPNFh1yiUfDfaIDiz2PemnVLRmefD4ydgMiHQAc=";
+    sha256 = "sha256-wiGoaVSTX8+lAc7GCHqN7dsp+uUD755uqMXxxPpbd1k=";
   };
 
-  cargoSha256 = "sha256-/JNGyAEZlb4YilsoXtaXekXNVev6sdVxS4pEcPFh7Bg=";
+  cargoSha256 = "sha256-63T3ibgRa+/nykqRfzsG4ib1RHa2s2jNHlfZcmaOV7Q=";
 
   meta = with lib; {
     description = "Graphical console greter for greetd";
