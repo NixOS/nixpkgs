cb4a7d49a517b5cb2df68c40193b465fe176ed63
Sauyon Lee <2347889+sauyon@users.noreply.github.com>
2021-10-28 05:44:21 +0000

    python3Packages.simple-di: bring up to standard

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8470,7 +8470,7 @@ in {
 
   simplekml = callPackage ../development/python-modules/simplekml { };
 
-  simple_di = callPackage ../development/python-modules/simple_di { };
+  simple-di = callPackage ../development/python-modules/simple_di { };
 
   simple-rest-client = callPackage ../development/python-modules/simple-rest-client { };
 
