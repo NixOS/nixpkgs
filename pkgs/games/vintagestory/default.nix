cbfdee81ba8a7ab64b2a38867edfeb81f04e455f
R. Ryantm <ryantm-bot@ryantm.com>
2021-10-28 06:57:09 +0000

    vintagestory: 1.15.5 -> 1.15.7

--- pkgs/games/vintagestory/default.nix
+++ pkgs/games/vintagestory/default.nix
@@ -17,11 +17,11 @@
 
 stdenv.mkDerivation rec {
   pname = "vintagestory";
-  version = "1.15.5";
+  version = "1.15.7";
 
   src = fetchurl {
     url = "https://cdn.vintagestory.at/gamefiles/stable/vs_archive_${version}.tar.gz";
-    sha256 = "sha256-38vLkH8B1yYC1I8P8uCsbC8CK8Btpfm9tNxgiuswsa8=";
+    sha256 = "sha256-V1aoCu62WFGbGTB9jt6iTYmEcgO8OvmFTX4eNuPTPeY=";
   };
 
   nativeBuildInputs = [ makeWrapper copyDesktopItems ];
