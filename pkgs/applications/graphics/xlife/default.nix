73892924e0b9b75b236713b2ded962df2153fc31
R. Ryantm <ryantm-bot@ryantm.com>
2021-10-28 03:29:40 +0000

    xlife: 6.7.5 -> 6.7.6

--- pkgs/applications/graphics/xlife/default.nix
+++ pkgs/applications/graphics/xlife/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation {
   pname = "xlife";
-  version = "6.7.5";
+  version = "6.7.6";
 
   src = fetchsvn {
     url = "https://svn.code.sf.net/p/xlife-cal/xlife/trunk";
     rev = "365";
-    sha256 = "1gadlcp32s179kd7ypxr8cymd6s060p6z4c2vnx94i8bmiw3nn8h";
+    sha256 = "sha256-EFk7eKwLRZK63YKRby4wQJtWPUO5X3/aTCdoMS6jTb0=";
   };
 
   nativeBuildInputs = with xorg; [ imake gccmakedep ];
