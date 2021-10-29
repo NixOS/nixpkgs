9e65eb4cee6aea2f96e6593d0990481b44ff5ff7
Thomas Tuegel <ttuegel@mailbox.org>
2021-10-29 09:34:03 -0500

    qt515: 5.15.2 -> 5.15.3
    Qt 5.15.3 does not have an official open source release, but the KDE team
    maintains a collection of patches (pulled from Qt upstream) that they expect us
    to carry.

--- pkgs/development/libraries/qt-5/5.15/fetch.sh
+++ pkgs/development/libraries/qt-5/5.15/fetch.sh
@@ -1,2 +1,59 @@
-WGET_ARGS=( http://download.qt.io/official_releases/qt/5.15/5.15.2/submodules/ \
-            -A '*.tar.xz' )
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p nix-prefetch-scripts jq
+
+set -x -e -o pipefail
+
+module_list=( \
+    qt3d \
+    qtactiveqt \
+    qtandroidextras \
+    qtbase \
+    qtcharts \
+    qtconnectivity \
+    qtdatavis3d \
+    qtdeclarative \
+    qtdoc \
+    qtgamepad \
+    qtgraphicaleffects \
+    qtimageformats \
+    qtlocation \
+    qtlottie \
+    qtmacextras \
+    qtmultimedia \
+    qtnetworkauth \
+    qtpurchasing \
+    qtquick3d \
+    qtquickcontrols \
+    qtquickcontrols2 \
+    qtquicktimeline \
+    qtremoteobjects \
+    qtscript \
+    qtscxml \
+    qtsensors \
+    qtserialbus \
+    qtserialport \
+    qtspeech \
+    qtsvg \
+    qttools \
+    qttranslations \
+    qtvirtualkeyboard \
+    qtwayland \
+    qtwebchannel \
+    qtwebglplugin \
+    qtwebsockets \
+    qtwebview \
+    qtwinextras \
+    qtx11extras \
+    qtxmlpatterns \
+    )
+
+srcs="$(dirname "${BASH_SOURCE[0]}")/srcs"
+mkdir -p "$srcs"
+
+for module in "${module_list[@]}"
+do
+    url="https://invent.kde.org/qt/qt/${module}.git"
+    nix-prefetch-git --url $url --rev refs/heads/kde/5.15 \
+        | jq '{url,rev,sha256,fetchLFS,fetchSubmodules,deepClone,leaveDotGit}' \
+        > "${srcs}/${module}.json"
+done
