21acfcd96353f793d069abd8a3b757d3fa015bfe
Mario Rodas <marsam@users.noreply.github.com>
2021-10-30 04:20:00 +0000

    sarasa-gothic: 0.32.9 -> 0.34.7

--- pkgs/data/fonts/sarasa-gothic/default.nix
+++ pkgs/data/fonts/sarasa-gothic/default.nix
@@ -1,26 +1,27 @@
 { lib, fetchurl, libarchive }:
 
 let
-  version = "0.32.9";
+  version = "0.34.7";
 in fetchurl {
   name = "sarasa-gothic-${version}";
 
-  url = "https://github.com/be5invis/Sarasa-Gothic/releases/download/v${version}/sarasa-gothic-ttc-${version}.7z";
-  sha256 = "0mwaj9dq26f36ddywjm7m0is1jml2kpmqm46b16c8avvr97c65z5";
+  url = "https://github.com/be5invis/Sarasa-Gothic/releases/download/v${version}/sarasa-gothic-ttf-${version}.7z";
+  sha256 = "094sl6gklrdv9pk4r6451dvz0fjyjmwys7i81qrz4ik1km5dfq9b";
 
   recursiveHash = true;
   downloadToTemp = true;
 
   postFetch = ''
-    mkdir -p $out/share/fonts
-    ${libarchive}/bin/bsdtar -xf $downloadedFile -C $out/share/fonts
+    mkdir -p $out/share/fonts/truetype
+    ${libarchive}/bin/bsdtar -xf $downloadedFile -C $out/share/fonts/truetype
   '';
 
   meta = with lib; {
-    description = "SARASA GOTHIC is a Chinese & Japanese programming font based on Iosevka and Source Han Sans";
+    description = "A CJK programming font based on Iosevka and Source Han Sans";
     homepage = "https://github.com/be5invis/Sarasa-Gothic";
     license = licenses.ofl;
     maintainers = [ maintainers.ChengCat ];
+    hydraPlatforms = [ ]; # disabled from hydra because it's so big
     platforms = platforms.all;
   };
 }
