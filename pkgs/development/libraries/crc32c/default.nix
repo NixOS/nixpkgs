6a2e74c3614ece46acdbac9b75ed27b23b65b61c
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-10-30 08:05:54 -0400

    crc32c: disable unnecessary build flags

--- pkgs/development/libraries/crc32c/default.nix
+++ pkgs/development/libraries/crc32c/default.nix
@@ -21,7 +21,10 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ cmake ];
   buildInputs = [ gflags ];
   NIX_CFLAGS_COMPILE = lib.optionalString stdenv.isAarch64 "-march=armv8-a+crc";
-  cmakeFlags = lib.optionals (!staticOnly) [ "-DBUILD_SHARED_LIBS=1" ];
+  cmakeFlags = [
+    "-DCRC32C_BUILD_BENCHMARKS=0"
+    "-DCRC32C_USE_GLOG=0"
+  ] ++ lib.optionals (!staticOnly) [ "-DBUILD_SHARED_LIBS=1" ];
 
   meta = with lib; {
     homepage = "https://github.com/google/crc32c";
