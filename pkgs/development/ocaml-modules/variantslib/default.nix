a264971bd6560de6c837235916dd0e6420ccd411
Felix Buehler <account@buehler.rocks>
2021-10-30 16:57:53 +0200

    ocamlPackages.variantslib: switch to fetchFromGitHub

--- pkgs/development/ocaml-modules/variantslib/default.nix
+++ pkgs/development/ocaml-modules/variantslib/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildOcaml, ocaml, fetchurl, type_conv }:
+{ lib, buildOcaml, ocaml, fetchFromGitHub, type_conv }:
 
 if lib.versionAtLeast ocaml.version "4.06"
 then throw "variantslib-109.15.03 is not available for OCaml ${ocaml.version}"
@@ -10,9 +10,11 @@ buildOcaml rec {
 
   minimumSupportedOcamlVersion = "4.00";
 
-  src = fetchurl {
-    url = "https://github.com/janestreet/variantslib/archive/${version}.tar.gz";
-    sha256 = "a948dcdd4ca54786fe0646386b6e37a9db03bf276c6557ea374d82740bf18055";
+  src = fetchFromGitHub {
+    owner = "janestreet";
+    repo = "variantslib";
+    rev = version;
+    sha256 = "sha256-pz3i3od2CqKSrm7uTQ2jdidFFwx7m7g1QuG4UdLk01k=";
   };
 
   propagatedBuildInputs = [ type_conv ];
