ba4354d54aaaa5eb42023ebb45d8c07c2d2c5228
Filippo Berto <berto.f@protonmail.com>
2021-11-01 00:30:29 +0100

    Applied revision suggestions

--- pkgs/servers/nfd/default.nix
+++ pkgs/servers/nfd/default.nix
@@ -4,41 +4,39 @@
 , libpcap
 , fetchFromGitHub
 , boost
-, pkgconfig
+, pkg-config
 , openssl
 , doxygen
 , wafHook
 , systemd
 , python3
 , python3Packages
-, withSystemd ? true
+, websocketpp
+, withSystemd ? stdenv.isLinux
 , withWebSocket ? true
 }:
 let
-  pname = "nfd";
-  version = "0.7.1";
   pythonPackages = p: with p; [ sphinx ];
 in
-stdenv.mkDerivation {
-  inherit pname version;
+stdenv.mkDerivation rec {
+  pname = "nfd";
+  version = "0.7.1";
 
   src = fetchFromGitHub {
     owner = "named-data";
     repo = lib.toUpper pname;
     rev = "${lib.toUpper pname}-${version}";
     sha256 = "1l9bchj8c68r6qw4vr1kc96jgxl0vpqa2vjkvy1xmhz92sivr6gi";
-    fetchSubmodules = withWebSocket;
   };
 
-  nativeBuildInputs = [ wafHook doxygen pkgconfig python3 python3Packages.sphinx ];
-  buildInputs = [ libpcap boost openssl ndn-cxx ] ++ lib.optional withSystemd systemd;
+  nativeBuildInputs = [ wafHook doxygen pkg-config (python3.withPackages pythonPackages) ];
+  buildInputs = [ libpcap boost openssl ndn-cxx ] ++ lib.optional withSystemd systemd ++ lib.optional withSystemd websocketpp;
 
   wafConfigureFlags = [
     "--boost-includes=${boost.dev}/include"
     "--boost-libs=${boost.out}/lib"
   ] ++ lib.optional (!withWebSocket) "--without-websocket";
 
-  outputs = [ "out" "dev" ];
   meta = with lib; {
     homepage = "http://named-data.net/";
     description = "Named Data Neworking (NDN) Forwarding Daemon";
