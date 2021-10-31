828f1c7df3c62503300d091fe57ceff869db7d39
Fabian Affolter <fabian@affolter-engineering.ch>
2021-10-31 07:24:57 +0100

    python3Packages.upass: add pythonImportsCheck

--- pkgs/development/python-modules/upass/default.nix
+++ pkgs/development/python-modules/upass/default.nix
@@ -6,8 +6,9 @@
 }:
 
 buildPythonPackage rec {
-  version = "0.2.1";
   pname = "upass";
+  version = "0.2.1";
+  format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "Kwpolska";
@@ -16,14 +17,22 @@ buildPythonPackage rec {
     sha256 = "0bgplq07dmlld3lp6jag1w055glqislfgwwq2k7cb2bzjgvysdnj";
   };
 
-  propagatedBuildInputs = [ pyperclip urwid ];
+  propagatedBuildInputs = [
+    pyperclip
+    urwid
+  ];
 
+  # Project has no tests
   doCheck = false;
+
+  pythonImportsCheck = [
+    "upass"
+  ];
 
   meta = with lib; {
     description = "Console UI for pass";
     homepage = "https://github.com/Kwpolska/upass";
     license = licenses.bsd3;
+    maintainers = with maintainers; [ ];
   };
-
 }
