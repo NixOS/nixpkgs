af3444ffc5ad73d5efc351acae8bd0fa79a8c1df
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-10-31 06:02:08 -0400

    abseil-cpp: add cxxStandard input

--- pkgs/development/libraries/abseil-cpp/default.nix
+++ pkgs/development/libraries/abseil-cpp/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, fetchpatch, cmake, static ? stdenv.hostPlatform.isStatic }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch, cmake, static ? stdenv.hostPlatform.isStatic, cxxStandard ? null }:
 
 stdenv.mkDerivation rec {
   pname = "abseil-cpp";
@@ -21,8 +21,9 @@ stdenv.mkDerivation rec {
   ];
 
   cmakeFlags = [
-    "-DCMAKE_CXX_STANDARD=17"
     "-DBUILD_SHARED_LIBS=${if static then "OFF" else "ON"}"
+  ] ++ lib.optionals (cxxStandard != null) [
+    "-DCMAKE_CXX_STANDARD=${toString cxxStandard}"
   ];
 
   nativeBuildInputs = [ cmake ];
