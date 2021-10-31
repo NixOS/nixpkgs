2f5c78458b146d1fa9f3d49625108ce416be090f
Mihai-Drosi Câju <cajum.bugs@yandex.com>
2021-10-31 18:33:16 +0200

    waydroid: Add missing dependencies
    * Also add pyclip for clipboard support
    * And symlink Waydroid.desktop to share/applications

--- pkgs/os-specific/linux/waydroid/default.nix
+++ pkgs/os-specific/linux/waydroid/default.nix
@@ -3,9 +3,15 @@
 , fetchFromGitHub
 , python3Packages
 , dnsmasq
+, getent
+, kmod
 , lxc
+, iproute2
+, iptables
 , nftables
-, python
+, util-linux
+, which
+, xclip
 }:
 
 python3Packages.buildPythonApplication rec {
@@ -21,6 +27,7 @@ python3Packages.buildPythonApplication rec {
 
   propagatedBuildInputs = with python3Packages; [
     gbinder-python
+    pyclip
     pygobject3
   ];
 
@@ -36,20 +43,33 @@ python3Packages.buildPythonApplication rec {
 
     cp -ra data $out/${python3Packages.python.sitePackages}/data
     wrapProgram $out/${python3Packages.python.sitePackages}/data/scripts/waydroid-net.sh \
-       --prefix PATH ":" ${lib.makeBinPath [ dnsmasq nftables ]}
+       --prefix PATH ":" ${lib.makeBinPath [ dnsmasq getent iproute2 iptables nftables ]}
 
     mkdir -p $out/share/waydroid/gbinder.d
     cp gbinder/anbox.conf $out/share/waydroid/gbinder.d/anbox.conf
+
+    mkdir -p $out/share/applications
+    ln -s $out/${python3Packages.python.sitePackages}/data/Waydroid.desktop $out/share/applications/Waydroid.desktop
 
     mkdir $out/bin
     cp -a waydroid.py $out/${python3Packages.python.sitePackages}/waydroid.py
     ln -s $out/${python3Packages.python.sitePackages}/waydroid.py $out/bin/waydroid
 
-    wrapPythonProgramsIn $out/${python3Packages.python.sitePackages} "$out ${python3Packages.gbinder-python} ${python3Packages.pygobject3} ${lxc}"
+    wrapPythonProgramsIn $out/${python3Packages.python.sitePackages} "${lib.concatStringsSep " " [
+      "$out"
+      python3Packages.gbinder-python
+      python3Packages.pygobject3
+      python3Packages.pyclip
+      kmod
+      lxc
+      util-linux
+      which
+      xclip
+    ]}"
   '';
 
   meta = with lib; {
-    description = "Waydroid is a container-based approach to boot a full Android system on a regular GNU/Linux system like Ubuntu.";
+    description = "Waydroid is a container-based approach to boot a full Android system on a regular GNU/Linux system like Ubuntu";
     homepage = "https://github.com/waydroid/waydroid";
     license = licenses.gpl3;
     platforms = platforms.linux;
