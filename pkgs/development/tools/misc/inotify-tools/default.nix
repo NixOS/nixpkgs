0d016a086ed1100fbb5b79b5c62e99ffe6fe613e
R. Ryantm <ryantm-bot@ryantm.com>
2021-10-31 00:16:35 +0000

    inotify-tools: 3.21.9.5 -> 3.21.9.6

--- pkgs/development/tools/misc/inotify-tools/default.nix
+++ pkgs/development/tools/misc/inotify-tools/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "inotify-tools";
-  version = "3.21.9.5";
+  version = "3.21.9.6";
 
   src = fetchFromGitHub {
     repo = "inotify-tools";
     owner = "rvoicilas";
     rev = version;
-    sha256 = "sha256-2eMYCFqecpY/yvhwl5+kvQ+pkdWzhX6Xsb+rcJEK37c=";
+    sha256 = "sha256-oKcVmF39N43g8O1S+xwUhVJryFcW+ZUteyoe3fUkRH8=";
   };
 
   nativeBuildInputs = [ autoreconfHook ];
