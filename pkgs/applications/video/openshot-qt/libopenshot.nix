2721652f034536a8c0a760565b7afbe3a6ce6ad4
Jonathan Ringer <jonringer117@gmail.com>
2021-11-02 09:01:35 -0700

    libopenshot: 0.2.5 -> 0.2.7

--- pkgs/applications/video/openshot-qt/libopenshot.nix
+++ pkgs/applications/video/openshot-qt/libopenshot.nix
@@ -1,54 +1,70 @@
 { lib, stdenv, fetchFromGitHub, fetchpatch
-, pkg-config, cmake, doxygen
-, libopenshot-audio, imagemagick, ffmpeg
-, swig, python3, jsoncpp
-, cppzmq, zeromq
-, qtbase, qtmultimedia
+, alsa-lib
+, cmake
+, cppzmq
+, doxygen
+, ffmpeg
+, imagemagick
+, jsoncpp
+, libopenshot-audio
 , llvmPackages
+, pkg-config
+, python3
+, qtbase
+, qtmultimedia
+, swig
+, zeromq
 }:
 
 with lib;
 stdenv.mkDerivation rec {
   pname = "libopenshot";
-  version = "0.2.5";
+  version = "0.2.7";
 
   src = fetchFromGitHub {
     owner = "OpenShot";
     repo = "libopenshot";
     rev = "v${version}";
-    sha256 = "1mxjkgjmjzgf628y3rscc6rqf55hxgjpmvwxlncfk1216i5xskwp";
+    sha256 = "sha256-aF8wrPxFIjCy5gw72e/WyL/Wcx9tUGDkrqHS+ZDVK0U=";
   };
 
-  patches = [
-    # Fix build with GCC 10.
-    (fetchpatch {
-      name = "fix-build-with-gcc-10.patch";
-      url = "https://github.com/OpenShot/libopenshot/commit/13290364e7bea54164ab83d973951f2898ad9e23.diff";
-      sha256 = "0i7rpdsr8y9dphil8yq75qbh20vfqjc2hp5ahv0ws58z9wj6ngnz";
-    })
-  ];
+  patches = [ ];
 
   postPatch = ''
     sed -i 's/{UNITTEST++_INCLUDE_DIR}/ENV{UNITTEST++_INCLUDE_DIR}/g' tests/CMakeLists.txt
-    sed -i 's/{_REL_PYTHON_MODULE_PATH}/ENV{_REL_PYTHON_MODULE_PATH}/g' src/bindings/python/CMakeLists.txt
+    sed -i 's/{_REL_PYTHON_MODULE_PATH}/ENV{_REL_PYTHON_MODULE_PATH}/g' bindings/python/CMakeLists.txt
     export _REL_PYTHON_MODULE_PATH=$(toPythonPath $out)
   '';
 
-  nativeBuildInputs = [ pkg-config cmake doxygen swig ];
+  nativeBuildInputs = [
+    alsa-lib
+    cmake
+    doxygen
+    pkg-config
+    swig
+  ];
 
-  buildInputs =
-  [ imagemagick ffmpeg python3 jsoncpp
-    cppzmq zeromq qtbase qtmultimedia ]
-    ++ optional stdenv.isDarwin llvmPackages.openmp
-  ;
+  buildInputs = [
+    cppzmq
+    ffmpeg
+    imagemagick
+    jsoncpp
+    libopenshot-audio
+    python3
+    qtbase
+    qtmultimedia
+    zeromq
+  ] ++ optionals stdenv.isDarwin [
+    llvmPackages.openmp
+  ];
 
   dontWrapQtApps = true;
-
-  LIBOPENSHOT_AUDIO_DIR = libopenshot-audio;
 
   doCheck = false;
 
   cmakeFlags = [ "-DENABLE_RUBY=OFF" ];
+
+  passthru = { inherit libopenshot-audio; };
 
   meta = {
     homepage = "http://openshot.org/";
