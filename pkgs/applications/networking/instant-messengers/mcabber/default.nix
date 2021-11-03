72aca77ae534742ce3fc670f29b3ae10deb0b940
Sergei Trofimovich <slyich@gmail.com>
2021-11-03 09:30:28 +0000

    mcabber: pull upstream fix for upcoming ncurses-6.3

--- pkgs/applications/networking/instant-messengers/mcabber/default.nix
+++ pkgs/applications/networking/instant-messengers/mcabber/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, openssl, ncurses, pkg-config, glib, loudmouth, libotr
+{ lib, stdenv, fetchurl, fetchpatch, openssl, ncurses, pkg-config, glib, loudmouth, libotr
 , gpgme
 }:
 
@@ -10,6 +10,16 @@ stdenv.mkDerivation rec {
     url = "https://mcabber.com/files/mcabber-${version}.tar.bz2";
     sha256 = "0q1i5acyghsmzas88qswvki8kkk2nfpr8zapgnxbcd3lwcxl38f4";
   };
+
+  patches = [
+    # Pull upstream patch for ncurses-6.3.
+    (fetchpatch {
+      name = "ncurses-6.3.patch";
+      url = "https://github.com/McKael/mcabber/commit/5a0893d69023b77b7671731defbdca5d47731130.patch";
+      sha256 = "01bc23z0mva9l9jv587sq2r9w3diachgkmb9ad99hlzgj02fmq4v";
+      stripLen = 1;
+    })
+  ];
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ncurses glib loudmouth libotr gpgme ];
