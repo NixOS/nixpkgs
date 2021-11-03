1fcaa65c896c7fbe374201b753697041d854dcc0
R. Ryantm <ryantm-bot@ryantm.com>
2021-11-03 16:14:29 +0000

    nats-server: 2.6.0 -> 2.6.3

--- pkgs/servers/nats-server/default.nix
+++ pkgs/servers/nats-server/default.nix
@@ -4,7 +4,7 @@ with lib;
 
 buildGoPackage rec {
   pname   = "nats-server";
-  version = "2.6.0";
+  version = "2.6.3";
 
   goPackagePath = "github.com/nats-io/${pname}";
 
@@ -12,7 +12,7 @@ buildGoPackage rec {
     rev    = "v${version}";
     owner  = "nats-io";
     repo   = pname;
-    sha256 = "sha256-DggzXYPyu0dQ40L98VzxgN9S/35vLJJow9UjDtMz9rY=";
+    sha256 = "sha256-7srDyTsIyac4AYwTFpDji4Czg6rRK9evb4W25CqQgGk=";
   };
 
   meta = {
