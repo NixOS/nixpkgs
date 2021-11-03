8165ee70ca98ed062f40de8590f32264a15ebaf2
Dylan Arbour <arbourd@users.noreply.github.com>
2021-11-03 14:25:13 -0400

    cloud-sql-proxy: 1.26.0 -> 1.27.0

--- pkgs/tools/misc/cloud-sql-proxy/default.nix
+++ pkgs/tools/misc/cloud-sql-proxy/default.nix
@@ -2,23 +2,27 @@
 
 buildGoModule rec {
   pname = "cloud-sql-proxy";
-  version = "1.26.0";
+  version = "1.27.0";
 
   src = fetchFromGitHub {
     owner = "GoogleCloudPlatform";
     repo = "cloudsql-proxy";
     rev = "v${version}";
-    sha256 = "sha256-Xgz8ku6szzbgL3cyiggHJYaj4aUhaqgIOUnwKUE1AeY=";
+    sha256 = "sha256-x44nG5M2ycBaf/Fbw5crmAV//yv/WtIYbTjJ7/6TnoI=";
   };
 
   subPackages = [ "cmd/cloud_sql_proxy" ];
 
-  vendorSha256 = "sha256-7KiLJoQ0xH35ae4NGODF4t1S9h86L0TJbCqFVm+bBmk=";
+  vendorSha256 = "sha256-Uw8YJ1qzLYlTkx6wR/FKeDRHGSwZm2za/c0f/OKHiE0=";
+
+  # Disables tests that require running fuse with a hardcoded path
+  doCheck = false;
 
   meta = with lib; {
     description = "An authenticating proxy for Second Generation Google Cloud SQL databases";
     homepage = "https://github.com/GoogleCloudPlatform/cloudsql-proxy";
     license = licenses.asl20;
     maintainers = with maintainers; [ nicknovitski ];
+    mainProgram = "cloud_sql_proxy";
   };
 }
