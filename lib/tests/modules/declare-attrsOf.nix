e8d61a25fcb5e11da8af0792da27a896bf87ba65
Robert Hensing <robert@roberthensing.nl>
2021-11-03 19:19:41 +0100

    lib/tests/modules: Test non-strictness some more
    Doesn't seem to have been a problem actually, but now it won't
    regress.

--- lib/tests/modules/declare-attrsOf.nix
+++ lib/tests/modules/declare-attrsOf.nix
@@ -1,6 +1,13 @@
-{ lib, ... }: {
+{ lib, ... }:
+let
+  deathtrapArgs = lib.mapAttrs
+    (k: _: throw "The module system is too strict, accessing an unused option's ${k} mkOption-attribute.")
+    (lib.functionArgs lib.mkOption);
+in
+{
   options.value = lib.mkOption {
     type = lib.types.attrsOf lib.types.str;
     default = {};
   };
+  options.testing-laziness-so-don't-read-me = lib.mkOption deathtrapArgs;
 }
