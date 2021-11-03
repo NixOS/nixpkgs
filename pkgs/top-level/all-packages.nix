1fcac2a148e81cf6e65855cb1c46ca9c5259d0b9
Dominik Xaver Hörl <hoe.dom@gmx.de>
2021-11-03 08:08:53 +0100

    linux_latest: 5.14 -> 5.15

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20782,7 +20782,7 @@ in
 
   # Update this when adding the newest kernel major version!
   # And update linux_latest_for_hardened below if the patches are already available
-  linuxPackages_latest = linuxPackages_5_14;
+  linuxPackages_latest = linuxPackages_5_15;
   linux_latest = linuxPackages_latest.kernel;
 
   # Realtime kernel packages.
