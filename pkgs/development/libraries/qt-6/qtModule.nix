f025d6f94aa4a980d1c6664ec00b6c0116ebf480
Milan Hauth <milahu@gmail.com>
2021-11-03 11:58:41 +0100

    qt6.qtModule: replace _IMPORT_PREFIX with nix output paths

--- pkgs/development/libraries/qt-6/qtModule.nix
+++ pkgs/development/libraries/qt-6/qtModule.nix
@@ -22,7 +22,9 @@ mkDerivation (args // {
   outputs = args.outputs or [ "out" "dev" ];
   setOutputFlags = args.setOutputFlags or false;
 
+  # FIXME only needed for qt6
   # set attributes for qt-6/hooks/qmake-hook.sh
+  # TODO better. we do not want to set this for every libsForQt6.callPackage target
   inherit (self.qtbase) qtDocPrefix qtQmlPrefix qtPluginPrefix;
 
   preHook = ''
@@ -38,6 +40,7 @@ mkDerivation (args // {
 
   dontWrapQtApps = args.dontWrapQtApps or true;
 
+  # TODO verify for qt6: patching of pkgconfig files
   postFixup = ''
     if [ -d "''${!outputDev}/lib/pkgconfig" ]; then
         find "''${!outputDev}/lib/pkgconfig" -name '*.pc' | while read pc; do
@@ -47,6 +50,43 @@ mkDerivation (args // {
                 -e "/^includedir=/ c includedir=''${!outputDev}/include"
         done
     fi
+
+    # TODO refactor. same code in qtbase.nix
+    echo "patching output paths in cmake files ..."
+    (
+    cd $dev/lib/cmake
+    moduleNAME="${lib.toUpper pname}"
+    outEscaped=$(echo $out | sed 's,/,\\/,g')
+    devEscaped=$(echo $dev | sed 's,/,\\/,g')
+    if [ -n "$bin" ]; then
+    binEscaped=$(echo $bin | sed 's,/,\\/,g') # optional plugins
+    else binEscaped=""; fi
+
+    # TODO build the perlRegex string with nix?
+    perlRegex="s/^# Compute the installation prefix relative to this file\..*?set\(_IMPORT_PREFIX \"\"\)\nendif\(\)/# NixOS was here\nset(_''${moduleNAME}_NIX_OUT \"$outEscaped\")\nset(_''${moduleNAME}_NIX_DEV \"$devEscaped\")\nset(_''${moduleNAME}_NIX_BIN \"$binEscaped\")/s;"
+    perlRegex+="s/\\\''${_IMPORT_PREFIX}\/(\.\/)?include/\\\''${_''${moduleNAME}_NIX_DEV}\/include/g;"
+    perlRegex+="s/\\\''${_IMPORT_PREFIX}\/(\.\/)?libexec/\\\''${_''${moduleNAME}_NIX_DEV}\/libexec/g;" # both in $dev and $out, should be only in $dev, maybe
+    perlRegex+="s/\\\''${_IMPORT_PREFIX}\/(\.\/)?lib/\\\''${_''${moduleNAME}_NIX_OUT}\/lib/g;" # must come after libexec
+    perlRegex+="s/\\\''${_IMPORT_PREFIX}\/(\.\/)?plugins/\\\''${_''${moduleNAME}_NIX_BIN}\/lib\/qt-${version}\/plugins/g;"
+    perlRegex+="s/\\\''${_IMPORT_PREFIX}\/(\.\/)?bin/\\\''${_''${moduleNAME}_NIX_DEV}\/bin/g;"
+    perlRegex+="s/\\\''${_IMPORT_PREFIX}\/(\.\/)?mkspecs/\\\''${_''${moduleNAME}_NIX_OUT}\/mkspecs/g;" # should be in $dev, maybe
+    perlRegex+="s/set\(_IMPORT_PREFIX\)/set(_''${moduleNAME}_NIX_OUT)\nset(_''${moduleNAME}_NIX_DEV)\nset(_''${moduleNAME}_NIX_BIN)/g;"
+
+    echo "debug: perlRegex = $perlRegex"
+    find . -name '*.cmake' -exec perl -00 -p -i -e "$perlRegex" '{}' \;
+    echo "rc of find = $?" # zero when perl returns nonzero?
+    # FIXME catch errors from perl: find -> xargs
+    echo "patching output paths in cmake files done"
+
+    echo "verify that all _IMPORT_PREFIX are replaced ..."
+    matches="$(find . -name '*.cmake' -exec grep -HnF _IMPORT_PREFIX '{}' \;)"
+    if [ -n "$matches" ]; then
+      echo "fatal: _IMPORT_PREFIX was not replaced in:"
+      echo "$matches"
+      exit 1
+    fi
+    echo "verify that all _IMPORT_PREFIX are replaced done"
+    )
 
     moveQtDevTools
 
