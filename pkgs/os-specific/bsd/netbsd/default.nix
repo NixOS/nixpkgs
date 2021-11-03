afa8670aa78737a665d0deffeb135b1356f99cce
Alyssa Ross <hi@alyssa.is>
2021-11-03 16:55:36 +0000

    netbsd.compat: don't use musl's sys/cdefs.h
    When building glib statically, a Meson check would fail, because the
    check would interpret any warning as failure, and it would see the
    warning that the musl sys/cdefs.h emits about the file being
    deprecated.
    
    (cherry picked from commit 2b9c5958a10affa63551166c281bcd36ea021674)

--- pkgs/os-specific/bsd/netbsd/default.nix
+++ pkgs/os-specific/bsd/netbsd/default.nix
@@ -192,6 +192,12 @@ in lib.makeScopeWithSplicing
     configurePlatforms = [ "build" "host" ];
     configureFlags = [
       "--cache-file=config.cache"
+    ] ++ lib.optionals stdenv.hostPlatform.isMusl [
+      # We include this header in our musl package only for legacy
+      # compatibility, and compat works fine without it (and having it
+      # know about sys/cdefs.h breaks packages like glib when built
+      # statically).
+      "ac_cv_header_sys_cdefs_h=no"
     ];
 
     nativeBuildInputs = with buildPackages.netbsd; commonDeps ++ [
