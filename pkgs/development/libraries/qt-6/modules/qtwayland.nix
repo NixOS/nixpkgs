4403f26156679096b1695f2368dc5fc2c62a152f
Milan Hauth <milahu@gmail.com>
2021-11-03 17:23:07 +0100

    qt6.qtwayland: add dependencies TODO rebase: fixup to e58d329

--- pkgs/development/libraries/qt-6/modules/qtwayland.nix
+++ pkgs/development/libraries/qt-6/modules/qtwayland.nix
@@ -1,14 +1,18 @@
 { qtModule
 , qtbase
-, libglvnd, libxkbcommon, vulkan-headers # TODO should be inherited from qtbase
-, wayland
-}:
 
-# TODO? enable more Qt Wayland Drivers
+, libglvnd, libxkbcommon, vulkan-headers, libX11, libXcomposite
+# TODO should be inherited from qtbase
+
+, wayland
+, pkg-config
+, xlibs
+}:
 
 qtModule {
   pname = "qtwayland";
   qtInputs = [ qtbase ];
-  buildInputs = [ wayland libglvnd libxkbcommon vulkan-headers ];
+  buildInputs = [ wayland pkg-config xlibs.libxcb xlibs.libxcb.dev
+    libglvnd libxkbcommon vulkan-headers libX11 libXcomposite ];
   hasPlugins = true;
 }
