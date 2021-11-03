419cee225d7d951f2b8c38d3db43c311efdf6147
Milan Hauth <milahu@gmail.com>
2021-11-03 07:57:01 +0100

    qt6: add dependency cmake

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19753,7 +19753,7 @@ with pkgs;
     (import ../development/libraries/qt-6/6.2) {
       inherit newScope;
       inherit lib stdenv fetchurl fetchpatch fetchgit fetchFromGitHub makeSetupHook makeWrapper;
-      inherit bison cups dconf harfbuzz libGL perl gtk3;
+      inherit bison cups dconf harfbuzz libGL perl cmake gtk3;
       inherit (gst_all_1) gstreamer gst-plugins-base;
       inherit llvmPackages_5 darwin;
     });
