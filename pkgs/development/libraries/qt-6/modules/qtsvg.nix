2eb5a7d950801201f72a77257c1eae211f824532
Milan Hauth <milahu@gmail.com>
2021-11-03 08:19:40 +0100

    qt6.qtsvg: init

--- pkgs/development/libraries/qt-6/modules/qtsvg.nix
+++ pkgs/development/libraries/qt-6/modules/qtsvg.nix
@@ -1,7 +1,20 @@
-{ qtModule, qtbase }:
+{ pkg-config
+, qtModule
+, qtbase
+, libwebp
+# FIXME not found by cmake: libwebp-1.1.0/lib/libwebp.so.7.1.0
+, jasper
+, libmng
+, zlib
+, libglvnd
+, libxkbcommon
+, vulkan-headers
+}:
 
 qtModule {
   pname = "qtsvg";
-  qtInputs = [ qtbase ];
-  outputs = [ "out" "dev" "bin" ];
+  qtInputs = [ qtbase libwebp jasper libmng zlib pkg-config
+    libglvnd libxkbcommon # TODO these should be inherited from qtbase
+  ];
+  outputs = [ "out" "dev" ];
 }
