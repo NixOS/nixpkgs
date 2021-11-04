16873acde8b421eda63a529333753071d7c17b31
ajs124 <git@ajs124.de>
2021-11-04 15:47:36 +0100

    nginxMainline: 1.21.3 -> 1.21.4

--- pkgs/servers/http/nginx/mainline.nix
+++ pkgs/servers/http/nginx/mainline.nix
@@ -1,6 +1,6 @@
 { callPackage, ... }@args:
 
 callPackage ./generic.nix args {
-  version = "1.21.3";
-  sha256 = "0nhps7igdqcpcy1r8677ar807rfclpylmz3y858a678m1np4lxql";
+  version = "1.21.4";
+  sha256 = "1ziv3xargxhxycd5hp6r3r5mww54nvvydiywcpsamg3i9r3jzxyi";
 }
