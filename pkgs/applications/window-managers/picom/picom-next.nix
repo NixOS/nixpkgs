c28690b0913afe5ed087b07d3628cd9637dbf3fb
GKasparov <mizozahr@gmail.com>
2021-11-05 10:41:43 +1100

    picom-next: addid lib, fetchFromGithub imports, fixed indents

--- pkgs/applications/window-managers/picom/picom-next.nix
+++ pkgs/applications/window-managers/picom/picom-next.nix
@@ -2,12 +2,12 @@
 
 picom.overrideAttrs (oldAttrs: rec {
   pname = "picom-next";
-    version = "unstable-2021-11-01";
-    src = fetchFromGitHub {
-      owner = "yshui";
-      repo = "picom";
-      rev = "fade045eadf171d2c732820d6ebde7d1943a1397";
-      sha256 = "fPiLZ63+Bw5VCxVNqj9i5had2YLa+jFMMf85MYdqvHU=";
-      };
-    meta.maintainers = with lib.maintainers; oldAttrs.meta.maintainers ++ [ GKasparov ];
-  })
+  version = "unstable-2021-11-01";
+  src = fetchFromGitHub {
+    owner = "yshui";
+    repo = "picom";
+    rev = "fade045eadf171d2c732820d6ebde7d1943a1397";
+    sha256 = "fPiLZ63+Bw5VCxVNqj9i5had2YLa+jFMMf85MYdqvHU=";
+  };
+  meta.maintainers = with lib.maintainers; oldAttrs.meta.maintainers ++ [ GKasparov ];
+})
