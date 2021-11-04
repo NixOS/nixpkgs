1549123435034220872ddf8e11edbad56b5cd9b3
Malo Bourgon <mbourgon@gmail.com>
2021-11-04 12:59:32 -0700

    thefuck: fix build on darwin

--- pkgs/tools/misc/thefuck/default.nix
+++ pkgs/tools/misc/thefuck/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchFromGitHub, buildPythonApplication
+{ lib, stdenv, fetchFromGitHub, buildPythonApplication
 , colorama, decorator, psutil, pyte, six
 , go, mock, pytestCheckHook, pytest-mock
 }:
@@ -17,6 +17,23 @@ buildPythonApplication rec {
   propagatedBuildInputs = [ colorama decorator psutil pyte six ];
 
   checkInputs = [ go mock pytestCheckHook pytest-mock ];
+
+  disabledTests = lib.optional stdenv.isDarwin [
+    "test_settings_defaults"
+    "test_from_file"
+    "test_from_env"
+    "test_settings_from_args"
+    "test_get_all_executables_exclude_paths"
+    "test_with_blank_cache"
+    "test_with_filled_cache"
+    "test_when_etag_changed"
+    "test_for_generic_shell"
+    "test_on_first_run"
+    "test_on_run_after_other_commands"
+    "test_when_cant_configure_automatically"
+    "test_when_already_configured"
+    "test_when_successfully_configured"
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/nvbn/thefuck";
