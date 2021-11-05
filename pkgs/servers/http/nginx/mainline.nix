ffa6cb97ff76a25489e83ba409e4a0c7dbbfd93c
ajs124 <git@ajs124.de>
2021-11-05 13:10:58 +0100

    nginxMainline: use openssl_3_0

--- pkgs/servers/http/nginx/mainline.nix
+++ pkgs/servers/http/nginx/mainline.nix
@@ -1,6 +1,6 @@
-{ callPackage, ... }@args:
+{ callPackage, openssl_3_0, ... }@args:
 
-callPackage ./generic.nix args {
+callPackage ./generic.nix (args // { openssl = openssl_3_0; }) {
   version = "1.21.4";
   sha256 = "1ziv3xargxhxycd5hp6r3r5mww54nvvydiywcpsamg3i9r3jzxyi";
 }
