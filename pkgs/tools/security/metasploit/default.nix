3e368b176a28e0f985d790d72167bc5881b5aff1
Fabian Affolter <mail@fabian-affolter.ch>
2021-11-05 21:27:30 +0100

    metasploit: 6.1.12 -> 6.1.13

--- pkgs/tools/security/metasploit/default.nix
+++ pkgs/tools/security/metasploit/default.nix
@@ -14,13 +14,13 @@ let
   };
 in stdenv.mkDerivation rec {
   pname = "metasploit-framework";
-  version = "6.1.12";
+  version = "6.1.13";
 
   src = fetchFromGitHub {
     owner = "rapid7";
     repo = "metasploit-framework";
     rev = version;
-    sha256 = "sha256-I7wk8DBN7i4zE4bEIMVGcZi4OMIsbh0Ay2RsAh0VRrw=";
+    sha256 = "sha256-ncmLizpX1yEtr8biUoWVVK4Ziv3QMiKWsj694yubA1M=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
