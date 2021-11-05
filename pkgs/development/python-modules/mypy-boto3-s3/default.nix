ff02ceb9153636d9f6f97e5dc6b0f28350ec2a1c
Fabian Affolter <mail@fabian-affolter.ch>
2021-11-06 00:52:50 +0100

    python3Packages.mypy-boto3-s3: 1.19.8 -> 1.19.12

--- pkgs/development/python-modules/mypy-boto3-s3/default.nix
+++ pkgs/development/python-modules/mypy-boto3-s3/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage rec {
   pname = "mypy-boto3-s3";
-  version = "1.19.8";
+  version = "1.19.12";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "60481cae38e01273d09a6159e4ff8c36d5d7e335319117a16cdb3d887928a7b4";
+    sha256 = "sha256-xgcGbWcrdFwFPQLJHmgRdY2XbwAiInZxmw6RiFVJ5F4=";
   };
 
   propagatedBuildInputs = [
