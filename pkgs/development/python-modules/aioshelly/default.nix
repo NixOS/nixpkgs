7be1b371026529af5fe16f5ea5ab3c50eb2f0b06
Fabian Affolter <mail@fabian-affolter.ch>
2021-11-06 23:16:37 +0100

    python3Packages.aioshelly: 1.0.3 -> 1.0.4

--- pkgs/development/python-modules/aioshelly/default.nix
+++ pkgs/development/python-modules/aioshelly/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage rec {
   pname = "aioshelly";
-  version = "1.0.3";
+  version = "1.0.4";
 
   src = fetchFromGitHub {
     owner = "home-assistant-libs";
     repo = pname;
     rev = version;
-    sha256 = "sha256-3ml1krSVaDlHirk0Mi36EDqhgNLJdyH9ZExbGPABJ5k=";
+    sha256 = "sha256-Om+v+cGisfx2PkH/J08loZioUO6y9wi5+6kd2T+dfOo=";
   };
 
   propagatedBuildInputs = [
