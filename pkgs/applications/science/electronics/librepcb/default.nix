7a51cf9c1dcfd4e987c59ad3a93337000c2916ee
Luz <luz666@daum.net>
2021-11-06 17:35:58 +0100

    librepcb: 0.1.5 -> 0.1.6

--- pkgs/applications/science/electronics/librepcb/default.nix
+++ pkgs/applications/science/electronics/librepcb/default.nix
@@ -1,36 +1,21 @@
 { stdenv, lib, fetchFromGitHub
-, qtbase, qttools, qmake, wrapQtAppsHook
+, qtbase, qttools, cmake, wrapQtAppsHook
 }:
 
 stdenv.mkDerivation rec {
   pname = "librepcb";
-  version = "0.1.5";
+  version = "0.1.6";
 
   src = fetchFromGitHub {
     owner  = pname;
     repo   = pname;
     rev    = version;
-    sha256 = "0ag8h3id2c1k9ds22rfrvyhf2vjhkv82xnrdrz4n1hnlr9566vcx";
+    sha256 = "0gzf3asdgdicpikb412134ybqnbbark948yrfhvba2w4i9cwbk2r";
     fetchSubmodules = true;
   };
 
-  nativeBuildInputs = [ qmake qttools wrapQtAppsHook ];
+  nativeBuildInputs = [ cmake qttools wrapQtAppsHook ];
   buildInputs = [ qtbase ];
-
-  qmakeFlags = ["-r"];
-
-  # the build system tries to use 'git' at build time to find the HEAD hash.
-  # that's a no-no, so replace it with a quick hack. NOTE: the # adds a comment
-  # at the end of the line to remove the git call.
-  postPatch = ''
-    substituteInPlace ./libs/librepcb/common/common.pro \
-      --replace 'GIT_COMMIT_SHA' 'GIT_COMMIT_SHA="\\\"${src.rev}\\\"" # '
-  '';
-
-  postInstall = ''
-    mkdir -p $out/share/librepcb/fontobene
-    cp share/librepcb/fontobene/newstroke.bene $out/share/librepcb/fontobene/
-  '';
 
   meta = with lib; {
     description = "A free EDA software to develop printed circuit boards";
