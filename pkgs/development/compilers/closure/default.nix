02dfbde63938f077e21d7880781132ea141b0b21
Alyssa Ross <hi@alyssa.is>
2021-11-06 01:56:39 +0000

    treewide: use mirror URLs where available
    Candidates identified with
    
    	git grep -F "$(grep -v '#' pkgs/build-support/fetchurl/mirrors.nix |
    		grep -v nixos.org | grep :// | cut -d '"' -f 2)"
    
    and then manually reviewed and tested.

--- pkgs/development/compilers/closure/default.nix
+++ pkgs/development/compilers/closure/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
   version = "20210808";
 
   src = fetchurl {
-    url = "https://repo1.maven.org/maven2/com/google/javascript/closure-compiler/v${version}/closure-compiler-v${version}.jar";
+    url = "mirror://maven/com/google/javascript/closure-compiler/v${version}/closure-compiler-v${version}.jar";
     sha256 = "1cvibvm8l4mp64ml6lpsh3w62bgbr42pi3i7ga8ss0prhr0dsk3y";
   };
 
