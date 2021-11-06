b3f59f2089722ec4f0d4a032d329d33ddd63a226
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-11-06 03:58:17 +0100

    helm-docs: init at 1.5.0
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26188,6 +26188,8 @@ with pkgs;
 
   chart-testing = callPackage ../applications/networking/cluster/helm/chart-testing { };
 
+  helm-docs = callPackage ../applications/networking/cluster/helm-docs { };
+
   kubetail = callPackage ../applications/networking/cluster/kubetail { } ;
 
   kupfer = callPackage ../applications/misc/kupfer { };
