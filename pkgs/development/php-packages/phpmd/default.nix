NixOS/nixpkgs

commit e76ccc6b2798e50ef959a8cf830035d76a92106b

php.extensions: Add missing meta info

pkgs/development/php-packages/phpmd/default.nix
@@ -2,8 +2,6 @@
 let
   pname = "phpmd";
   version = "2.8.2";
-
-  isPhp74 = lib.versionAtLeast php.version "7.4";
 in
 mkDerivation {
   inherit pname version;
@@ -28,6 +26,6 @@ mkDerivation {
     license = licenses.bsd3;
     homepage = "https://phpmd.org/";
     maintainers = teams.php.members;
-    broken = !isPhp74;
+    broken = versionAtLeast php.version "7.4";
   };
 }

pkgs/development/php-packages/apcu/default.nix
@@ -13,5 +13,10 @@ buildPecl {
   makeFlags = [ "phpincludedir=$(dev)/include" ];
   outputs = [ "out" "dev" ];
 
-  meta.maintainers = lib.teams.php.members;
+  meta = with lib; {
+    description = "Userland cache for PHP";
+    license = licenses.php301;
+    homepage = "https://pecl.php.net/package/APCu";
+    maintainers = teams.php.members;
+  };
 }

pkgs/development/php-packages/apcu_bc/default.nix
@@ -14,6 +14,11 @@ buildPecl {
     mv $out/lib/php/extensions/apc.so $out/lib/php/extensions/apcu_bc.so
   '';
 
-  meta.maintainers = lib.teams.php.members;
-  meta.broken = lib.versionAtLeast php.version "8";
+  meta = with lib; {
+    description = "APCu Backwards Compatibility Module";
+    license = licenses.php301;
+    homepage = "https://pecl.php.net/package/apcu_bc";
+    maintainers = teams.php.members;
+    broken = versionAtLeast php.version "8";
+  };
 }

pkgs/development/php-packages/ast/default.nix
@@ -6,5 +6,10 @@ buildPecl {
   version = "1.0.10";
   sha256 = "13s5r1szd80g1mqickghdd38mvjkwss221322mmbrykcfgp4fs30";
 
-  meta.maintainers = lib.teams.php.members;
+  meta = with lib; {
+    description = "Exposes the abstract syntax tree generated by PHP";
+    license = licenses.bsd3;
+    homepage = "https://pecl.php.net/package/ast";
+    maintainers = teams.php.members;
+  };
 }

pkgs/development/php-packages/couchbase/default.nix
@@ -44,6 +44,11 @@ buildPecl {
     '')
   ];
 
-  meta.broken = lib.versionAtLeast php.version "8.0";
-  meta.maintainers = lib.teams.php.members;
+  meta = with lib; {
+    description = "Couchbase Server PHP extension";
+    license = licenses.asl20;
+    homepage = "https://docs.couchbase.com/php-sdk/current/project-docs/sdk-release-notes.html";
+    maintainers = teams.php.members;
+    broken = versionAtLeast php.version "8.0";
+  };
 }

pkgs/development/php-packages/igbinary/default.nix
@@ -10,5 +10,10 @@ buildPecl {
   makeFlags = [ "phpincludedir=$(dev)/include" ];
   outputs = [ "out" "dev" ];
 
-  meta.maintainers = lib.teams.php.members;
+  meta = with lib; {
+    description = "Binary serialization for PHP";
+    license = licenses.bsd3;
+    homepage = "https://github.com/igbinary/igbinary/";
+    maintainers = teams.php.members;
+  };
 }

pkgs/development/php-packages/imagick/default.nix
@@ -23,5 +23,10 @@ buildPecl {
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ pcre2 ];
 
-  meta.maintainers = lib.teams.php.members;
+  meta = with lib; {
+    description = "Imagick is a native php extension to create and modify images using the ImageMagick API";
+    license = licenses.php301;
+    homepage = "https://pecl.php.net/package/imagick";
+    maintainers = teams.php.members;
+  };
 }

pkgs/development/php-packages/mailparse/default.nix
@@ -11,5 +11,10 @@ buildPecl {
     echo "#define HAVE_MBSTRING 1" >> config.h
   '';
 
-  meta.maintainers = lib.teams.php.members;
+  meta = with lib; {
+    description = "Mailparse is an extension for parsing and working with email messages";
+    license = licenses.php301;
+    homepage = "https://pecl.php.net/package/mailparse";
+    maintainers = lib.teams.php.members;
+  };
 }

commit b06550ed1feaf4f538b44001645052e0de87286e

phpmd: Fix meta.broken
In commit e76ccc6b279 ("php.extensions: Add missing meta info",
2021-06-08), Nix expressions for PHP packages were refactored and in
case of phpmd, the value of meta.broken was negated (likely by
mistake). The result was that phpmd is marked as broken, but it seems
to work well, at least for my use case.

Here, we correct the mentioned commit by negating meta.broken again.

pkgs/development/php-packages/phpmd/default.nix
@@ -27,6 +27,6 @@ mkDerivation {
     license = licenses.bsd3;
     homepage = "https://phpmd.org/";
     maintainers = teams.php.members;
-    broken = versionAtLeast php.version "7.4";
+    broken = versionOlder php.version "7.4";
   };
 }