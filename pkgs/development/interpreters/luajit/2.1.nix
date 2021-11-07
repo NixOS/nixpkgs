90a564a133e1a1ebfed26666567182ae60c666ee
Luka Blaskovic <lblasc@tvbeat.com>
2021-11-07 07:36:49 +0000

    luajit_2_1: 2.1.0-2021-08-12 -> 2.1.0-2021-10-27

--- pkgs/development/interpreters/luajit/2.1.nix
+++ pkgs/development/interpreters/luajit/2.1.nix
@@ -1,8 +1,8 @@
 { self, callPackage }:
 callPackage ./default.nix {
   inherit self;
-  version = "2.1.0-2021-08-12";
-  rev = "8ff09d9f5ad5b037926be2a50dc32b681c5e7597";
+  version = "2.1.0-2021-10-27";
+  rev = "b4b2dce9fc3ffaaaede39b36d06415311e2aa516";
   isStable = false;
-  sha256 = "18wp8sgmiwlslnvgs35cy35ji2igksyfm3f8hrx07hqmsq2d77vr";
+  sha256 = "185s071aa0yffz8npgdxj7l98cs987vddb2l5pzfcdqfj41gn55q";
 }
