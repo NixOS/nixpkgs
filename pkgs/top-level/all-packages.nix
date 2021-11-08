78f07d0f899b750e1a1fcdbe5aeef9bb4536805c
Mark Mulder <markmulder@gmail.com>
2021-11-08 17:20:33 +0100

    termscp: fix darwin build
    I was running into Foundation.h errors during compilation on mac/darwin,
    adding Foundation fixes that for me.
    
    Error:
    
    ```
    The following warnings were emitted during compilation:
    
    warning: In file included from objc/notify.m:1:
    warning: objc/notify.h:1:9: fatal error: 'Foundation/Foundation.h' file not found
    warning: #import <Foundation/Foundation.h>
    warning:         ^~~~~~~~~~~~~~~~~~~~~~~~~
    warning: 1 error generated.
    
    error: failed to run custom build command for `mac-notification-sys v0.3.0`
    
    Caused by:
      process didn't exit successfully: `/tmp/cargo-installEt3UWW/release/build/mac-notification-sys-f414f8995c05e541/build-script-build` (exit status: 1)
      --- stdout
      TARGET = Some("x86_64-apple-darwin")
      OPT_LEVEL = Some("3")
      HOST = Some("x86_64-apple-darwin")
      CC_x86_64-apple-darwin = None
      CC_x86_64_apple_darwin = None
      HOST_CC = None
      CC = Some("clang")
      CFLAGS_x86_64-apple-darwin = None
      CFLAGS_x86_64_apple_darwin = None
      HOST_CFLAGS = None
      CFLAGS = None
      CRATE_CC_NO_DEFAULTS = None
      DEBUG = Some("false")
      running: "clang" "-O3" "-ffunction-sections" "-fdata-sections" "-fPIC" "--target=x86_64-apple-darwin" "-fmodules" "-o" "/tmp/cargo-installEt3UWW/release/build/mac-notification-sys-ce5dd87fdfef4721/out/objc/notify.o" "-c" "objc/notify.m"
      cargo:warning=In file included from objc/notify.m:1:
      cargo:warning=objc/notify.h:1:9: fatal error: 'Foundation/Foundation.h' file not found
      cargo:warning=#import <Foundation/Foundation.h>
      cargo:warning=        ^~~~~~~~~~~~~~~~~~~~~~~~~
      cargo:warning=1 error generated.
      exit status: 1
    
      --- stderr
    
      error occurred: Command "clang" "-O3" "-ffunction-sections" "-fdata-sections" "-fPIC" "--target=x86_64-apple-darwin" "-fmodules" "-o" "/tmp/cargo-installEt3UWW/release/build/mac-notification-sys-ce5dd87fdfef4721/out/objc/notify.o" "-c" "objc/notify.m" with args "clang" did not execute successfully (status code exit status: 1).
    
    warning: build failed, waiting for other jobs to finish...
    error: failed to compile `termscp v0.7.0`, intermediate artifacts can be found at `/tmp/cargo-installEt3UWW`
    
    Caused by:
      build failed
    ```

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9857,7 +9857,7 @@ with pkgs;
   telescope = callPackage ../applications/networking/browsers/telescope { };
 
   termscp = callPackage ../tools/networking/termscp {
-    inherit (darwin.apple_sdk.frameworks) Security;
+    inherit (darwin.apple_sdk.frameworks) Foundation Security;
   };
 
   termius = callPackage ../applications/networking/termius { };
