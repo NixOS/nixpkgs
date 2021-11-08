17e2591d9b216634ab501293efa2ba4c55ff6bc9
Ellie Hermaszewska <git@monoid.al>
2021-11-08 23:05:38 +0800

    haskell.packages.ghc901.autoapply: jailbreak

--- pkgs/development/haskell-modules/configuration-ghc-9.0.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-9.0.x.nix
@@ -76,7 +76,8 @@ self: super: {
   generic-lens-core = self.generic-lens-core_2_2_0_0;
   generic-lens = self.generic-lens_2_2_0_0;
   th-desugar = self.th-desugar_1_13;
-  autoapply = self.autoapply_0_4_1_1;
+  # 2021-11-08: Fixed in autoapply-0.4.2
+  autoapply = doJailbreak self.autoapply_0_4_1_1;
 
   # Doesn't allow Dhall 1.39.*
   weeder_2_3_0 = super.weeder_2_3_0.override {
