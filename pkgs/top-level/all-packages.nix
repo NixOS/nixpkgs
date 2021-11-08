aac65657d336985e5e7a01f64dca2c0878324c0a
legendofmiracles <legendofmiracles@protonmail.com>
2021-11-08 12:32:00 -0600

    present: init at 0.6.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18990,6 +18990,8 @@ with pkgs;
 
   presage = callPackage ../development/libraries/presage { };
 
+  present = callPackage ../misc/present { };
+
   prime-server = callPackage ../development/libraries/prime-server { };
 
   primesieve = callPackage ../development/libraries/science/math/primesieve { };
