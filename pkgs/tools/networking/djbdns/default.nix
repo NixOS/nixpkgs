7a8b4907e2d5fe95b31e8f4f1cb1f176419fa9d7
Bas van Dijk <bas@van.dijk.ch>
2021-11-08 17:28:41 +0000

    djbdns: fix crash due to data ulimit
    This fixes: https://github.com/NixOS/nixpkgs/issues/119066.
    
    When `djbdns` is repeatedly queried it will crash with:
    
    ```
    mmap: tinydns (842): VmData 331776 exceed data ulimit 300000. Update limits or use boot option ignore_rlimit_data.
    ```
    
    So we increase the softlimit from 300000 to 4500000 as suggested in
    the issue.
    
    The `tinydns` NixOS test has been extended with testing for the faulty
    behaviour.

--- pkgs/tools/networking/djbdns/default.nix
+++ pkgs/tools/networking/djbdns/default.nix
@@ -19,7 +19,14 @@ stdenv.mkDerivation {
     sha256 = "0j3baf92vkczr5fxww7rp1b7gmczxmmgrqc8w2dy7kgk09m85k9w";
   };
 
-  patches = [ ./hier.patch ./fix-nix-usernamespace-build.patch ];
+  patches = [
+    ./hier.patch
+    ./fix-nix-usernamespace-build.patch
+
+    # To fix https://github.com/NixOS/nixpkgs/issues/119066.
+    # Note that the NixOS test <nixpkgs/nixos/tests/tinydns.nix> tests for this.
+    ./softlimit.patch
+  ];
 
   postPatch = ''
     echo gcc -O2 -include ${glibc.dev}/include/errno.h > conf-cc
