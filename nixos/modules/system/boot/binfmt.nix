18f515bee79e3145f75b8a8831e3ce674dad93ad
Christian Kögler <ck3d@gmx.de>
2021-11-09 21:37:45 +0100

    Apply suggestions from code review
    Co-authored-by: markuskowa <markus.kowalewski@gmail.com>

--- nixos/modules/system/boot/binfmt.nix
+++ nixos/modules/system/boot/binfmt.nix
@@ -249,7 +249,6 @@ in {
           List of systems to emulate. Will also configure Nix to
           support your new systems.
           Warning: the builder can execute all emulated systems within the same build, which introduces impurities in the case of cross compilation.
-          In case of cross compilation this introduces impurity.
         '';
         type = types.listOf types.str;
       };
