5ffdc94b8ea24fa6f11cc1c5c8f7bf92d6670283
Milan Hauth <milahu@gmail.com>
2021-11-09 12:22:03 +0100

    qt6.qtModule: add maintainer milahu

--- pkgs/development/libraries/qt-6/qtModule.nix
+++ pkgs/development/libraries/qt-6/qtModule.nix
@@ -162,7 +162,7 @@ drv1 = mkDerivation (args // {
     homepage = "https://www.qt.io/";
     description = "A cross-platform application framework for C++";
     license = with licenses; [ fdl13 gpl2 lgpl21 lgpl3 ];
-    maintainers = with maintainers; [ qknight ttuegel periklis bkchr ];
+    maintainers = with maintainers; [ qknight ttuegel periklis bkchr milahu ];
     platforms = platforms.unix;
   } // (args.meta or {});
 });
