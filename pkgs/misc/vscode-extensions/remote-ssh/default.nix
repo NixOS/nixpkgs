580374635d73a5b0ec737f43842c5d39f3611a26
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-11-09 20:54:11 +0100

    vscode-extensions.ms-vscode-remote.remote-ssh: 0.65.7 -> 0.66.1

--- pkgs/misc/vscode-extensions/remote-ssh/default.nix
+++ pkgs/misc/vscode-extensions/remote-ssh/default.nix
@@ -1,6 +1,7 @@
 { lib
 , vscode-utils
-, useLocalExtensions ? false}:
+, useLocalExtensions ? false
+}:
 # Note that useLocalExtensions requires that vscode-server is not running
 # on host. If it is, you'll need to remove $HOME/.vscode-server,
 # and redo the install by running "Connect to host" on client
@@ -32,24 +33,22 @@ let
     ''}
   '';
 in
-  buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "remote-ssh";
-      publisher = "ms-vscode-remote";
-      version = "0.65.7";
-      sha256 = "ae86c4be79fc5af747bb1f1aa5841221af80ee7476cc2f1c9ac277fa2fa1d683";
-    };
+buildVscodeMarketplaceExtension {
+  mktplcRef = {
+    name = "remote-ssh";
+    publisher = "ms-vscode-remote";
+    version = "0.66.1";
+    sha256 = "sha256-+v4UnGRG5xOc8k0IzvHUBHa128fhgd3jcmEuciiMQmI=";
+  };
 
-    postPatch = ''
-      substituteInPlace "out/extension.js" \
-        --replace "# install extensions" '${patch}'
-    '';
+  postPatch = ''
+    substituteInPlace "out/extension.js" \
+      --replace "# install extensions" '${patch}'
+  '';
 
-    meta = with lib; {
-      description ="Use any remote machine with a SSH server as your development environment.";
-      license = licenses.unfree;
-      maintainers = with maintainers; [
-        tbenst
-      ];
-    };
-  }
+  meta = with lib; {
+    description = "Use any remote machine with a SSH server as your development environment.";
+    license = licenses.unfree;
+    maintainers = with maintainers; [ SuperSandro2000 tbenst ];
+  };
+}
