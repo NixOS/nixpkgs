692768dadb485010f5d8faf850fa0fa348f75797
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-11-09 10:22:31 +0100

    subdl: fix attribute ordering, fix version

--- pkgs/applications/video/subdl/default.nix
+++ pkgs/applications/video/subdl/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation {
   pname = "subdl";
-  version = "0.0pre.2017.11.06";
+  version = "unstable-2017-11.06";
 
   src = fetchFromGitHub {
     owner = "alexanderwink";
@@ -11,6 +11,12 @@ stdenv.mkDerivation {
     sha256 = "0kmk5ck1j49q4ww0lvas2767kwnzhkq0vdwkmjypdx5zkxz73fn8";
   };
 
+  buildInputs = [ python3 ];
+
+  installPhase = ''
+    install -vD subdl $out/bin/subdl
+  '';
+
   meta = {
     homepage = "https://github.com/alexanderwink/subdl";
     description = "A command-line tool to download subtitles from opensubtitles.org";
@@ -18,10 +24,4 @@ stdenv.mkDerivation {
     license = lib.licenses.gpl3;
     maintainers = [ lib.maintainers.exfalso ];
   };
-
-  buildInputs = [ python3 ];
-
-  installPhase = ''
-    install -vD subdl $out/bin/subdl
-  '';
 }
