3871db4b2b37e40de8a812c94fe2ed01a91d93e5
x10an14 <x10an14@users.noreply.github.com>
2021-11-09 15:59:49 +0100

    fix/vimPlugin: vim-fzf-coauthorship dependencies' had invalid whitespace.
    Begone trixy non-ascii whitespace.
    
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/misc/vim-plugins/overrides.nix
+++ pkgs/misc/vim-plugins/overrides.nix
@@ -773,7 +773,7 @@ self: super: {
   });
 
   vim-fzf-coauthorship = super.vim-fzf-coauthorship.overrideAttrs (old: {
-    dependencies = with self; [ vim-fzf ];
+    dependencies = with self; [ vim-fzf ];
   });
 
   # change the go_bin_path to point to a path in the nix store. See the code in
