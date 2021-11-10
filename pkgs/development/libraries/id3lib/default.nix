1c07bb514ffda3fbd50fc43d9f3f6d0fc019859e
Winter <winter@winter.cafe>
2021-11-09 20:00:10 -0500

    id3lib: fix build on aarch64-darwin

--- pkgs/development/libraries/id3lib/default.nix
+++ pkgs/development/libraries/id3lib/default.nix
@@ -1,4 +1,4 @@
-{lib, stdenv, fetchurl, zlib}:
+{ lib, stdenv, fetchurl, libiconv, zlib }:
 
 stdenv.mkDerivation rec {
   pname = "id3lib";
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
     ./patch_id3lib_3.8.3_UTF16_writing_bug.diff
   ];
 
-  buildInputs = [ zlib ];
+  buildInputs = [ libiconv zlib ];
 
   src = fetchurl {
     url = "mirror://sourceforge/id3lib/${pname}-${version}.tar.gz";
