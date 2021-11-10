ea25bea65ac46d8ea61de35daa26d50ab8cc6718
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-11-10 20:43:19 +0100

    iaca: cleanup with lib

--- pkgs/development/tools/iaca/3.0.nix
+++ pkgs/development/tools/iaca/3.0.nix
@@ -1,20 +1,24 @@
 { lib, stdenv, requireFile, unzip }:
-with lib;
 
 stdenv.mkDerivation {
-  name = "iaca-3.0";
+  pname = "iaca";
+  version = "3.0";
+
   src = requireFile {
     name = "iaca-version-v3.0-lin64.zip";
     sha256 = "0qd81bxg269cwwvfmdp266kvhcl3sdvhrkfqdrbmanawk0w7lvp1";
     url = "https://software.intel.com/en-us/articles/intel-architecture-code-analyzer-download";
   };
+
   unpackCmd = ''${unzip}/bin/unzip "$src"'';
+
   installPhase = ''
     mkdir -p $out/bin
     cp iaca $out/bin
     patchelf --set-interpreter ${stdenv.glibc}/lib/ld-linux-x86-64.so.2 $out/bin/iaca
   '';
-  meta = {
+
+  meta = with lib; {
     description = "Intel Architecture Code Analyzer";
     homepage = "https://software.intel.com/en-us/articles/intel-architecture-code-analyzer/";
     license = licenses.unfree;
