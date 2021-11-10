284681784712e73991c8c5d1f3643d61fcc261e9
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-11-10 20:42:45 +0100

    tokyo-tyrant: format, remove arbitrary choice

--- pkgs/development/libraries/tokyo-tyrant/default.nix
+++ pkgs/development/libraries/tokyo-tyrant/default.nix
@@ -11,31 +11,27 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ tokyocabinet ];
 
-  doCheck = false;                                # FIXME
+  doCheck = false; # FIXME
 
-  meta = {
+  meta = with lib; {
     description = "Network interface of the Tokyo Cabinet DBM";
+    longDescription = ''
+      Tokyo Tyrant is a package of network interface to the DBM called
+      Tokyo Cabinet.  Though the DBM has high performance, you might
+      bother in case that multiple processes share the same database, or
+      remote processes access the database.  Thus, Tokyo Tyrant is
+      provided for concurrent and remote connections to Tokyo Cabinet.  It
+      is composed of the server process managing a database and its access
+      library for client applications.
 
-    longDescription =
-      '' Tokyo Tyrant is a package of network interface to the DBM called
-         Tokyo Cabinet.  Though the DBM has high performance, you might
-         bother in case that multiple processes share the same database, or
-         remote processes access the database.  Thus, Tokyo Tyrant is
-         provided for concurrent and remote connections to Tokyo Cabinet.  It
-         is composed of the server process managing a database and its access
-         library for client applications.
-
-         Tokyo Tyrant is written in the C language, and provided as API of C,
-         Perl, and Ruby.  Tokyo Tyrant is available on platforms which have
-         API conforming to C99 and POSIX.  Tokyo Tyrant is a free software
-         licensed under the GNU Lesser General Public License.
-       '';
-
+      Tokyo Tyrant is written in the C language, and provided as API of C,
+      Perl, and Ruby.  Tokyo Tyrant is available on platforms which have
+      API conforming to C99 and POSIX.  Tokyo Tyrant is a free software
+      licensed under the GNU Lesser General Public License.
+    '';
     homepage = "https://fallabs.com/tokyotyrant/";
-
-    license = lib.licenses.lgpl21Plus;
-
-    platforms = lib.platforms.gnu ++ lib.platforms.linux;         # arbitrary choice
-    maintainers = [ ];
+    license = licenses.lgpl21Plus;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ ];
   };
 }
