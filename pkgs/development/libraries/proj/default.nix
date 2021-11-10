bd23ca01dd6a35b7569255a6e059fd41cac4723e
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2021-11-10 16:02:56 -0500

    proj: minor formatting fix

--- pkgs/development/libraries/proj/default.nix
+++ pkgs/development/libraries/proj/default.nix
@@ -21,8 +21,9 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-Z2nruyowC3NG4Wb8AFBL0PME/zp9D7SwQdMSl6VjH/w=";
   };
 
-  patches = [ (fetchpatch {
-  name = "Make-CApi-test-cross-platform.patch";
+  patches = [
+    (fetchpatch {
+      name = "Make-CApi-test-cross-platform.patch";
       url = "https://github.com/OSGeo/PROJ/commit/ac113a8898cded7f5359f1edd3abc17a78eee9b4.patch";
       sha256 = "0gz2xa5nxzck5c0yr7cspv3kw4cz3fxb2yic76w7qfvxidi7z1s1";
     })
