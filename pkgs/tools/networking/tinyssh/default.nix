005a4a1704f3a975ee579052421716aa818f0f7f
Sandro <sandro.jaeckel@gmail.com>
2021-11-11 17:18:04 +0100

    Apply suggestions from code review

--- pkgs/tools/networking/tinyssh/default.nix
+++ pkgs/tools/networking/tinyssh/default.nix
@@ -3,17 +3,21 @@
 stdenv.mkDerivation rec {
   pname = "tinyssh";
   version = "20210601";
+
   src = fetchFromGitHub {
     owner = "janmojzis";
     repo = "tinyssh";
     rev = version;
     sha256 = "sha256-+THoPiD6dW5ZuiQmmLckOJGyjhzdF3qF0DgC51zjGY8=";
   };
+
   preConfigure = ''
     echo /bin       > conf-bin
     echo /share/man > conf-man
   '';
+
   DESTDIR = placeholder "out";
+
   meta = with lib; {
     description = "minimalistic SSH server";
     homepage = "https://tinyssh.org";
