e5d1ac060f0af563cf632461b70d9b475e3f53cd
TredwellGit <tredwell@tutanota.com>
2021-11-12 19:02:44 +0000

    linux: 5.15.1 -> 5.15.2

--- pkgs/os-specific/linux/kernel/linux-5.15.nix
+++ pkgs/os-specific/linux/kernel/linux-5.15.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.15.1";
+  version = "5.15.2";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1j0lnrsj5y2bsmmym8pjc5wk4wb11y336zr9gad1nmxcr0rwvz9j";
+    sha256 = "0xdi799k15l7l9kxlq4qbp79mp1c38vxal4z4p9l5gl194x06d2n";
   };
 } // (args.argsOverride or { }))
