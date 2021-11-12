61c121424daf3efdbeb2fb3172ef6afc2cbb5e3a
Uri Baghin <uri@canva.com>
2021-11-12 21:58:45 +1100

    bazel_4: link to issue about aarch64-darwin

--- pkgs/development/tools/build-managers/bazel/bazel_4/default.nix
+++ pkgs/development/tools/build-managers/bazel/bazel_4/default.nix
@@ -617,6 +617,8 @@ stdenv.mkDerivation rec {
       ./bazel_src/output/bazel-complete.fish
   '';
 
+  # Install check fails on `aarch64-darwin`
+  # https://github.com/NixOS/nixpkgs/issues/145587
   doInstallCheck = stdenv.hostPlatform.system != "aarch64-darwin";
   installCheckPhase = ''
     export TEST_TMPDIR=$(pwd)
