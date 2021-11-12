c6d6891185632d000076a4f42e5298773c2497e8
TredwellGit <tredwell@tutanota.com>
2021-11-12 19:02:52 +0000

    linux: 5.4.158 -> 5.4.159

--- pkgs/os-specific/linux/kernel/linux-5.4.nix
+++ pkgs/os-specific/linux/kernel/linux-5.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.4.158";
+  version = "5.4.159";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0ncdkf1iz49458k3dvq3dc5b2r9dp21jsn3ffm9lbhlgvkn8y0bf";
+    sha256 = "0hw68yjf0c8kahwra8hq863318cbyqc89f429z75scmb9rgk466p";
   };
 } // (args.argsOverride or {}))
