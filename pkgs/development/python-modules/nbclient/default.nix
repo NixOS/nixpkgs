214033c0840d2fe5e1294ac8bbb55af2bb87b39d
Kerstin Humm <kerstin@erictapen.name>
2021-11-12 17:48:25 +0100

    python3Packages.nbclient: 0.5.4 -> 0.5.8

--- pkgs/development/python-modules/nbclient/default.nix
+++ pkgs/development/python-modules/nbclient/default.nix
@@ -6,12 +6,12 @@
 
 buildPythonPackage rec {
   pname = "nbclient";
-  version = "0.5.4";
+  version = "0.5.8";
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "6c8ad36a28edad4562580847f9f1636fe5316a51a323ed85a24a4ad37d4aefce";
+    sha256 = "sha256-NPUsycuDGl2MzXAxU341THXcYaJEh/mYcS0Sid4yCiU=";
   };
 
   inherit doCheck;
