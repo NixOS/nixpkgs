95947f6dd765d51729bb7e60bbb202b3ef5a1a9d
Alyssa Ross <hi@alyssa.is>
2021-11-12 16:52:50 +0000

    python2.pkgs.pycairo: fix cross

--- pkgs/development/python-modules/pycairo/1.18.nix
+++ pkgs/development/python-modules/pycairo/1.18.nix
@@ -37,7 +37,10 @@ buildPythonPackage rec {
   ];
 
   mesonFlags = [
-    "-Dpython=${python.interpreter}"
+    # This is only used for figuring out what version of Python is in
+    # use, and related stuff like figuring out what the install prefix
+    # should be, but it does need to be able to execute Python code.
+    "-Dpython=${python.pythonForBuild.interpreter}"
   ];
 
   meta = with lib; {
