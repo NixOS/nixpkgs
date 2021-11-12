4b47c55d39713bfacc1a474aa79c1810755d4282
Nikolay Korotkiy <sikmir@gmail.com>
2021-11-12 18:27:39 +0300

    gmnisrv: unstable-2021-05-16 → 1.0

--- pkgs/servers/gemini/gmnisrv/default.nix
+++ pkgs/servers/gemini/gmnisrv/default.nix
@@ -2,14 +2,19 @@
 
 stdenv.mkDerivation rec {
   pname = "gmnisrv";
-  version = "unstable-2021-05-16";
+  version = "1.0";
 
   src = fetchFromSourcehut {
     owner = "~sircmpwn";
     repo = "gmnisrv";
-    rev = "b9a92193e96bbe621ebc8430d8308d45a5b86cef";
-    sha256 = "sha256-eMKsoq3Y+eS20nxI7EoDLbdwdoB6shbGt6p8wS+uoPc=";
+    rev = version;
+    sha256 = "sha256-V9HXXYQIo3zeqZjJEn+dhemNg6AU+ee3FRmBmXgLuYQ=";
   };
+
+  postPatch = ''
+    substituteInPlace config.sh \
+      --replace "pkg-config" "${stdenv.cc.targetPrefix}pkg-config"
+  '';
 
   MIMEDB = "${mime-types}/etc/mime.types";
   nativeBuildInputs = [ pkg-config scdoc ];
