f51d931ec837f953b8997c7572b94fa8498ce84d
zseri <zseri.devel@ytrizja.de>
2021-11-13 17:34:13 +0100

    checksec: run nixpkgs-fmt

--- pkgs/os-specific/linux/checksec/default.nix
+++ pkgs/os-specific/linux/checksec/default.nix
@@ -1,5 +1,14 @@
-{ lib, stdenv, fetchFromGitHub, makeWrapper, file, findutils
-, binutils-unwrapped, glibc, coreutils, sysctl, openssl
+{ lib
+, stdenv
+, fetchFromGitHub
+, makeWrapper
+, binutils-unwrapped
+, coreutils
+, file
+, findutils
+, glibc
+, openssl
+, sysctl
 }:
 
 stdenv.mkDerivation rec {
@@ -16,18 +25,24 @@ stdenv.mkDerivation rec {
   patches = [ ./0001-attempt-to-modprobe-config-before-checking-kernel.patch ];
   nativeBuildInputs = [ makeWrapper ];
 
-  installPhase = let
-    path = lib.makeBinPath [
-      findutils file binutils-unwrapped sysctl openssl
-    ];
-  in ''
-    mkdir -p $out/bin
-    install checksec $out/bin
-    substituteInPlace $out/bin/checksec --replace /lib/libc.so.6 ${glibc.out}/lib/libc.so.6
-    substituteInPlace $out/bin/checksec --replace "/usr/bin/id -" "${coreutils}/bin/id -"
-    wrapProgram $out/bin/checksec \
-      --prefix PATH : ${path}
-  '';
+  installPhase =
+    let
+      path = lib.makeBinPath [
+        binutils-unwrapped
+        findutils
+        file
+        openssl
+        sysctl
+      ];
+    in
+    ''
+      mkdir -p $out/bin
+      install checksec $out/bin
+      substituteInPlace $out/bin/checksec --replace /lib/libc.so.6 ${glibc.out}/lib/libc.so.6
+      substituteInPlace $out/bin/checksec --replace "/usr/bin/id -" "${coreutils}/bin/id -"
+      wrapProgram $out/bin/checksec \
+        --prefix PATH : ${path}
+    '';
 
   meta = with lib; {
     description = "A tool for checking security bits on executables";
