df183ff1112b4321065f1800c1be82f512d039cb
Victor Engmark <victor@engmark.name>
2021-11-14 15:22:12 +1300

    onlykey: Indicate shell to ShellCheck
    ShellCheck SC1008.

--- pkgs/tools/security/onlykey/generate.sh
+++ pkgs/tools/security/onlykey/generate.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash -p nodePackages.node2nix
+# shellcheck shell=bash
 
 # XXX: --development is given here because we need access to gulp in order to build OnlyKey.
 exec node2nix --nodejs-14 --development -i package.json -c onlykey.nix -e ../../../development/node-packages/node-env.nix --no-copy-node-env
