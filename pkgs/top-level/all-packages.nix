dbf0c72627598a2eb09aed0ee1c330c195b1a240
Emery Hemingway <ehmry@posteo.net>
2021-11-14 16:42:56 +0000

    hjson-go: init at version 3.1.0
    There already exists a package "hjson" but it's written in Python.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32545,6 +32545,8 @@ with pkgs;
 
   hjson = with python3Packages; toPythonApplication hjson;
 
+  hjson-go = callPackage ../development/tools/hjson-go { };
+
   epkowa = callPackage ../misc/drivers/epkowa { };
 
   utsushi = callPackage ../misc/drivers/utsushi { };
