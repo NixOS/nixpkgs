939213640179baba0669190abdc1d14f044e3b1f
Victor Engmark <victor@engmark.name>
2021-11-14 15:08:13 +1300

    1password-gui: Indicate shell to ShellCheck
    ShellCheck SC1008.

--- pkgs/applications/misc/1password-gui/update.sh
+++ pkgs/applications/misc/1password-gui/update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p curl gnused common-updater-scripts
+# shellcheck shell=bash
 
 version="$(curl -sL https://onepassword.s3.amazonaws.com/linux/debian/dists/edge/main/binary-amd64/Packages | sed -r -n 's/^Version: (.*)/\1/p' | head -n1)"
 update-source-version _1password-gui "$version"
