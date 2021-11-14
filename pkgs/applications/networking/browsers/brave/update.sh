54b890c5652c8feb928c7709dd76c48c231b16d2
Victor Engmark <victor@engmark.name>
2021-11-14 15:08:26 +1300

    brave: Indicate shell to ShellCheck
    ShellCheck SC1008.

--- pkgs/applications/networking/browsers/brave/update.sh
+++ pkgs/applications/networking/browsers/brave/update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p curl gnused common-updater-scripts
+# shellcheck shell=bash
 
 version="$(curl -sL https://brave-browser-apt-release.s3.brave.com/dists/stable/main/binary-amd64/Packages | sed -r -n 's/^Version: (.*)/\1/p' | head -n1)"
 update-source-version brave "$version"
