f3e8eb217c4b619f6d4cd4a85e84a406b3c8526e
Jichao Ouyang <oyanglulu@gmail.com>
2021-11-14 19:07:40 +1100

    synology-drive-client: fix editorconfig lint: rm whitespaces

--- pkgs/applications/networking/synology-drive-client/default.nix
+++ pkgs/applications/networking/synology-drive-client/default.nix
@@ -3,7 +3,7 @@ let
   pname = "synology-drive-client";
   buildNumber = "12682";
   version = "3.0.2";
-  baseUrl = "https://global.download.synology.com/download/Utility/SynologyDriveClient";    
+  baseUrl = "https://global.download.synology.com/download/Utility/SynologyDriveClient";
   meta = with lib; {
     description = "Desktop application to synchronize files and folders between the computer and the Synology Drive server.";
     homepage = "https://www.synology.com/en-global/dsm/feature/drive";
@@ -44,7 +44,7 @@ let
 
   darwin = stdenv.mkDerivation {
     inherit pname version;
-    
+
     src = fetchurl {
       url = "${baseUrl}/${version}-${buildNumber}/Mac/Installer/synology-drive-client-${buildNumber}.dmg";
       sha256 = "1mlv8gxzivgxm59mw1pd63yq9d7as79ihm7166qyy0h0b0m04q2m";
