7327fe85f50c26b58e69f33d18d73422c323cd14
Victor Engmark <victor@engmark.name>
2021-11-14 15:17:12 +1300

    base16-builder: Indicate shell to ShellCheck
    ShellCheck SC1008.

--- pkgs/misc/base16-builder/generate.sh
+++ pkgs/misc/base16-builder/generate.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash -p nodePackages.node2nix
+# shellcheck shell=bash
 exec node2nix --nodejs-10 \
      --input node-packages.json \
      --output node-packages-generated.nix \
