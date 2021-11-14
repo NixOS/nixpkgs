ec2b7e9120da5838393266cb87f6acd6d4b8775f
Victor Engmark <victor@engmark.name>
2021-11-14 15:08:19 +1300

    logseq: Indicate shell to ShellCheck
    ShellCheck SC1008.

--- pkgs/applications/misc/logseq/update.sh
+++ pkgs/applications/misc/logseq/update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p curl jq common-updater-scripts
+# shellcheck shell=bash
 
 version="$(curl -sL "https://api.github.com/repos/logseq/logseq/releases" | jq '.[0].tag_name' --raw-output)"
 update-source-version logseq "$version"
