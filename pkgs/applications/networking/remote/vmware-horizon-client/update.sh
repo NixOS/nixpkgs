e9714e266c494251dac437beb95e74626c86bdb5
Victor Engmark <victor@engmark.name>
2021-11-14 15:10:34 +1300

    vmware-horizon-client: Indicate shell to ShellCheck
    ShellCheck SC1008.

--- pkgs/applications/networking/remote/vmware-horizon-client/update.sh
+++ pkgs/applications/networking/remote/vmware-horizon-client/update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -p curl -p jq -p common-updater-scripts -i bash
+# shellcheck shell=bash
 set -e
 
 entryPointURL='https://customerconnect.vmware.com/channel/public/api/v1.0/products/getRelatedDLGList?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&version=horizon_8&dlgType=PRODUCT_BINARY'
