ccd305dad7920c9217d661ac7f86278dadd74c56
Victor Engmark <victor@engmark.name>
2021-11-14 15:12:44 +1300

    elm: Indicate shell to ShellCheck
    ShellCheck SC1008.

--- pkgs/development/compilers/elm/update.sh
+++ pkgs/development/compilers/elm/update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -p cabal2nix elm2nix -i bash ../../..
+# shellcheck shell=bash
 
 cabal2nix https://github.com/elm/compiler --revision c9aefb6230f5e0bda03205ab0499f6e4af924495 > packages/elm.nix
 echo "need to manually copy registry.dat from an existing elm project"
