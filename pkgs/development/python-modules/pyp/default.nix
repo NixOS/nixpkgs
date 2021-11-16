c0066eb9cd2bdd3acbaba230b20d936ccb82918b
Fabian Affolter <fabian@affolter-engineering.ch>
2021-11-16 12:14:54 +0100

    python3Packages.pyp: add pythonImportsCheck

--- pkgs/development/python-modules/pyp/default.nix
+++ pkgs/development/python-modules/pyp/default.nix
@@ -12,6 +12,9 @@
 buildPythonPackage rec {
   pname = "pyp";
   version = "1.0.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "hauntsaninja";
@@ -27,6 +30,7 @@ buildPythonPackage rec {
   preCheck = ''
     export PATH=$out/bin:$PATH
   '';
+
   checkInputs = [
     pytestCheckHook
     coreutils
@@ -34,8 +38,12 @@ buildPythonPackage rec {
     bc
   ];
 
+  pythonImportsCheck = [
+    "pyp"
+  ];
+
   meta = with lib; {
-    description = "Easily run Python at the shell! Magical, but never mysterious.";
+    description = "Easily run Python at the shell! Magical, but never mysterious";
     homepage = "https://github.com/hauntsaninja/pyp";
     license = licenses.mit;
     maintainers = with maintainers; [ rmcgibbo ];
