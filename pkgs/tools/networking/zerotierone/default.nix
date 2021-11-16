75b645529450fd4bac3b897dc1fab1701ade3465
misuzu <bakalolka@gmail.com>
2021-11-16 22:50:30 +0200

    zerotierone: 1.8.2 -> 1.8.3 (#146293)

--- pkgs/tools/networking/zerotierone/default.nix
+++ pkgs/tools/networking/zerotierone/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "zerotierone";
-  version = "1.8.2";
+  version = "1.8.3";
 
   src = fetchFromGitHub {
     owner = "zerotier";
     repo = "ZeroTierOne";
     rev = version;
-    sha256 = "sha256-Itq06zE2AVQ1HSUEQdNUuvEazfU56Hs86T0DEVuF8d4=";
+    sha256 = "sha256-668KZ2E0jx/s+w4pl+oJbPlfdRGr6ypP2/FoFEBReIk=";
   };
 
   preConfigure = ''
