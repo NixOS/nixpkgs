82a58da60babeaac51b0e65b523f0588130175ee
Fabian Affolter <mail@fabian-affolter.ch>
2021-11-16 22:54:07 +0100

    python3Packages.oocsi: init at 0.4.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5289,6 +5289,8 @@ in {
 
   onnx = callPackage ../development/python-modules/onnx { };
 
+  oocsi = callPackage ../development/python-modules/oocsi { };
+
   open-garage = callPackage ../development/python-modules/open-garage { };
 
   openant = callPackage ../development/python-modules/openant { };
