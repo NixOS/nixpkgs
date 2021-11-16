b505012a58167c4834a7d9e5be1669e0c6837f18
Adriaan de Groot <groot@kde.org>
2021-11-16 01:47:36 +0100

    CI: fix build-script with new-style version reporting

--- pkgs/by-name/ca/calamares-nixos-extensions/src/ci/RELEASE.sh
+++ pkgs/by-name/ca/calamares-nixos-extensions/src/ci/RELEASE.sh
@@ -140,7 +140,7 @@ fi
 ### Get version number for this release
 #
 #
-V=$( cmake -DVERSION_STYLE=short -P CMakeLists.txt )
+V=$( cmake -DVERSION_STYLE=short -P CMakeLists.txt 2>&1 )
 test -n "$V" || { echo "Could not obtain version in $BUILDDIR ." ; exit 1 ; }
 
 ### Create signed tag
