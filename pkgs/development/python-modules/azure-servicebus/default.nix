56c240e6ba0e56e3ca618ae98804629938d2de03
Robert Schütz <nix@dotlambda.de>
2021-11-16 14:12:03 -0800

    python3Packages.azure-servicebus: 7.3.3 -> 7.4.0

--- pkgs/development/python-modules/azure-servicebus/default.nix
+++ pkgs/development/python-modules/azure-servicebus/default.nix
@@ -11,12 +11,12 @@
 
 buildPythonPackage rec {
   pname = "azure-servicebus";
-  version = "7.3.3";
+  version = "7.4.0";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "1c2133909a086bd4329135d6affcc05628e3a7da27afca584a0de8c21fc4e1ac";
+    sha256 = "sha256-nlt4wNHI613tK7JB85fBW2LE/FOa8+2aLeT6wzP1PQ4=";
   };
 
   propagatedBuildInputs = [
