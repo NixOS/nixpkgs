86630d9a5fbcecf04178e2d1f110f075ff3f8763
Madoura <madouura@gmail.com>
2021-11-16 20:45:07 -0600

    maintainers: add Madouura

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6934,6 +6934,12 @@
     githubId = 109141;
     name = "Georges Dubus";
   };
+  Madouura = {
+    email = "madouura@gmail.com";
+    github = "Madouura";
+    githubId = 93990818;
+    name = "Madoura";
+  };
   mafo = {
     email = "Marc.Fontaine@gmx.de";
     github = "MarcFontaine";
