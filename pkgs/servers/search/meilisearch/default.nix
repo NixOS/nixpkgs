56331b29444c660d2adf348b655ef74497452159
happysalada <raphael@megzari.com>
2021-11-17 08:33:59 -0500

    meilisearch: 0.23.1 -> 0.24.0

--- pkgs/servers/search/meilisearch/default.nix
+++ pkgs/servers/search/meilisearch/default.nix
@@ -7,7 +7,7 @@
 , Foundation
 }:
 
-let version = "0.23.1";
+let version = "0.24.0";
 in
 rustPlatform.buildRustPackage {
   pname = "meilisearch";
@@ -16,14 +16,15 @@ rustPlatform.buildRustPackage {
     owner = "meilisearch";
     repo = "MeiliSearch";
     rev = "v${version}";
-    sha256 = "sha256-4F7noByC9ZgqYwPfkm8VE15QU2jbBvUAH4Idxa+J+Aw=";
+    sha256 = "sha256-0peKFNB/w8FDowiRo3v/dakxUSPnKbZi0ZA5ALoEzHQ=";
   };
+  cargoSha256 = "sha256-U9bIXDtE71vydfSyZDznQrRsYYny/jXo97gb2CAoXy4=";
   cargoPatches = [
     # feature mini-dashboard tries to download a file from the internet
     # feature analitycs should be opt-in
     ./remove-default-feature.patch
   ];
-  cargoSha256 = "sha256-dz+1IQZRSeMEagI2dnOtR3A8prg4UZ2Om0pd1BUhuhE=";
+  cargoBuildFlags = [ "--no-default-features" ];
   buildInputs = lib.optionals stdenv.isDarwin [ Security DiskArbitration Foundation ];
   meta = with lib; {
     description = "Powerful, fast, and an easy to use search engine ";
