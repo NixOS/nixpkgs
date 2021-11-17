6979c7334a59218878b7577d3ed73e8188f4d21a
Fabian Affolter <mail@fabian-affolter.ch>
2021-11-17 09:31:27 +0100

    python3Packages.justbackoff: init at 0.6.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4109,6 +4109,8 @@ in {
 
   jupytext = callPackage ../development/python-modules/jupytext { };
 
+  justbackoff = callPackage ../development/python-modules/justbackoff { };
+
   jwcrypto = callPackage ../development/python-modules/jwcrypto { };
 
   jxmlease = callPackage ../development/python-modules/jxmlease { };
