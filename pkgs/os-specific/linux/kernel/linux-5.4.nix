fdb4ba60acfdb7ab30b66d91e102c06b8cf2af5f
TredwellGit <tredwell@tutanota.com>
2021-11-18 21:11:12 +0000

    linux: 5.4.159 -> 5.4.160
    (cherry picked from commit 83c1df7574c9fc93776ea798349113d6b1c7c08b)

--- pkgs/os-specific/linux/kernel/linux-5.4.nix
+++ pkgs/os-specific/linux/kernel/linux-5.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.4.159";
+  version = "5.4.160";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,7 +13,7 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0hw68yjf0c8kahwra8hq863318cbyqc89f429z75scmb9rgk466p";
+    sha256 = "0n04nlg44l7p855lxkdz80x2avwm1pmrx1761cjmqv4w1qlq1c6l";
   };
 
   kernelTests = args.kernelTests or [ nixosTests.kernel-generic.linux_5_4 ];
