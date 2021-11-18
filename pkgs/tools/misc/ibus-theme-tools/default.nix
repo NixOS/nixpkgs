f77e5fd2d38628f140f295c2596a96f210deee1e
Hollow Man <hollowman@hollowman.ml>
2021-11-19 00:55:46 +0800

    ibus-theme-tools: Fix according to review @SuperSandro2000
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- pkgs/tools/misc/ibus-theme-tools/default.nix
+++ pkgs/tools/misc/ibus-theme-tools/default.nix
@@ -3,7 +3,6 @@
 python3Packages.buildPythonApplication rec {
   pname = "ibus-theme-tools";
   version = "4.2.0";
-  disabled = python3Packages.pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "openSUSE";
@@ -12,12 +11,14 @@ python3Packages.buildPythonApplication rec {
     sha256 = "0i8vwnikwd1bfpv4xlgzc51gn6s18q58nqhvcdiyjzcmy3z344c2";
   };
 
-  BuildInputs = [ gettext ];
+  buildInputs = [ gettext ];
 
   propagatedBuildInputs = with python3Packages; [ tinycss2 pygobject3 ];
 
   # No test.
   doCheck = false;
+
+  pythonImportsCheck = [ "ibus_theme_tools" ];
 
   meta = with lib; {
     description = "Generate the IBus GTK or GNOME Shell theme from existing themes";
