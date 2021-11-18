1384c851de19cbf733cfc39ea375627d83083104
Bernardo Meurer <bernardo@meurer.org>
2021-11-17 22:33:23 -0800

    linuxPackages_5_15.ddcci-driver: fix build
    We apply the patch from this MR: https://gitlab.com/ddcci-driver-linux/ddcci-driver-linux/-/merge_requests/8

--- pkgs/os-specific/linux/ddcci/default.nix
+++ pkgs/os-specific/linux/ddcci/default.nix
@@ -1,14 +1,17 @@
-{ lib, stdenv, fetchFromGitLab, kernel }:
+{ lib, stdenv, fetchpatch, fetchFromGitLab, kernel }:
 
 stdenv.mkDerivation rec {
   pname = "ddcci-driver";
-  version = "0.4.1";
+  # XXX: We apply a patch for the upcoming version to the source of version 0.4.1
+  # XXX: When 0.4.2 is actually released, don't forget to remove this comment,
+  # XXX: fix the rev in fetchFromGitLab, and remove the patch.
+  version = "0.4.2";
   name = "${pname}-${kernel.version}-${version}";
 
   src = fetchFromGitLab {
     owner = "${pname}-linux";
     repo = "${pname}-linux";
-    rev = "v${version}";
+    rev = "v0.4.1";
     sha256 = "1qhsm0ccwfmwn0r6sbc6ms4lf4a3iqfcgqmbs6afr6hhxkqll3fg";
   };
 
@@ -25,6 +28,13 @@ stdenv.mkDerivation rec {
       --replace depmod \#
   '';
 
+  patches = [
+    (fetchpatch {
+      url = "https://gitlab.com/ddcci-driver-linux/ddcci-driver-linux/-/commit/bf9d79852cbd0aa5c2e288ce51b8280f74a1f5d2.patch";
+      sha256 = "sha256-ShqVzkoRnlX4Y5ARY11YVYatFI1K7bAtLulP3/8/nwg=";
+    })
+  ];
+
   makeFlags = kernel.makeFlags ++ [
     "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
     "KVER=${kernel.modDirVersion}"
@@ -38,6 +48,6 @@ stdenv.mkDerivation rec {
     license = licenses.gpl2Plus;
     maintainers = with maintainers; [ ];
     platforms = platforms.linux;
-    broken = kernel.kernelOlder "5.1" || kernel.kernelAtLeast "5.15";
+    broken = kernel.kernelOlder "5.1";
   };
 }
