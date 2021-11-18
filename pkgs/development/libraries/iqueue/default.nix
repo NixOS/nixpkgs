1c7f6562e20af6f7c75e8e8e666784f6ed8c2394
Sandro <sandro.jaeckel@gmail.com>
2021-11-18 13:10:23 +0100

    Update pkgs/development/libraries/iqueue/default.nix

--- pkgs/development/libraries/iqueue/default.nix
+++ pkgs/development/libraries/iqueue/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   pname = "iqueue";
   version = "0.1.0";
   src = fetchurl {
-    url = "https://github.com/twosigma/${pname}/releases/download/v${version}/${pname}-${version}.tar.gz";
+    url = "https://github.com/twosigma/iqueue/releases/download/v${version}/iqueue-${version}.tar.gz";
     sha256 = "0049fnr02k15gr21adav33swrwxrpbananilnrp63vp5zs5v9m4x";
   };
 
