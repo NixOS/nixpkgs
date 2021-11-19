cb96cea42d98c6466376f50ec1d4f168ec8f068a
imlonghao <git@imlonghao.com>
2021-11-19 23:13:05 +0800

    borgmatic: 1.5.12 -> 1.5.18

--- pkgs/tools/backup/borgmatic/default.nix
+++ pkgs/tools/backup/borgmatic/default.nix
@@ -2,30 +2,26 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "borgmatic";
-  version = "1.5.12";
+  version = "1.5.18";
 
   src = python3Packages.fetchPypi {
     inherit pname version;
-    sha256 = "sha256-XLbBJvNRmH8W9SnOjF7zUbazRYFCMW6SEO2wKN/2VTY=";
+    sha256 = "sha256-dX1U1zza8zMhDiTLE+DgtN6RLRciLks4NDOukpKH/po=";
   };
 
   checkInputs = with python3Packages; [ flexmock pytestCheckHook pytest-cov ];
 
   # - test_borgmatic_version_matches_news_version
   # The file NEWS not available on the pypi source, and this test is useless
-  # - test_collect_configuration_run_summary_logs_outputs_merged_json_results
-  # Upstream fixed in the next version, see
-  # https://github.com/witten/borgmatic/commit/ea6cd53067435365a96786b006aec391714501c4
   disabledTests = [
     "test_borgmatic_version_matches_news_version"
-    "test_collect_configuration_run_summary_logs_outputs_merged_json_results"
   ];
 
   propagatedBuildInputs = with python3Packages; [
     borgbackup
     colorama
-    pykwalify
-    ruamel_yaml
+    jsonschema
+    ruamel-yaml
     requests
     setuptools
   ];
