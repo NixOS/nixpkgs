f76bc9cc95479816685bcc87c83c1658b5abe0cd
Fabian Affolter <mail@fabian-affolter.ch>
2021-11-19 12:05:42 +0100

    python3Packages.related: init at 0.7.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8090,6 +8090,8 @@ in {
 
   reikna = callPackage ../development/python-modules/reikna { };
 
+  related = callPackage ../development/python-modules/related { };
+
   relatorio = callPackage ../development/python-modules/relatorio { };
 
   remarshal = callPackage ../development/python-modules/remarshal { };
