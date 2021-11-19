8e4ef58acbadbeba439ad6030502d27f1724fd1b
Sebastián Mancilla <smancill@smancill.dev>
2021-11-19 17:04:40 -0300

    python3Packages.m3u8: use pytestCheckHook

--- pkgs/development/python-modules/m3u8/default.nix
+++ pkgs/development/python-modules/m3u8/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, fetchFromGitHub, requests, iso8601, bottle, pytest, pytest-cov }:
+{ lib, buildPythonPackage, fetchFromGitHub, requests, iso8601, bottle, pytestCheckHook }:
 
 buildPythonPackage rec {
   pname = "m3u8";
@@ -11,13 +11,15 @@ buildPythonPackage rec {
     sha256 = "0cmg993icpsa1b19kljxvjwhs167bsqrs0ad4wnwsi8qq6na5d4p";
   };
 
-  checkInputs = [ bottle pytest pytest-cov ];
-
-  checkPhase = ''
-    pytest tests/test_{parser,model,variant_m3u8}.py
-  '';
+  checkInputs = [ bottle pytestCheckHook ];
 
   propagatedBuildInputs = [ requests iso8601 ];
+
+  pytestFlagsArray = [
+    "tests/test_parser.py"
+    "tests/test_model.py"
+    "tests/test_variant_m3u8.py"
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/globocom/m3u8";
