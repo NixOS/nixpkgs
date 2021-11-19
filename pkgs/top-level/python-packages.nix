12647a0a977d14d131a7d58204a9d2ab3d0ddeac
Fabian Affolter <mail@fabian-affolter.ch>
2021-11-19 14:10:35 +0100

    python3Packages.luxtronik: init at 0.3.9

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4537,6 +4537,8 @@ in {
 
   luftdaten = callPackage ../development/python-modules/luftdaten { };
 
+  luxtronik = callPackage ../development/python-modules/luxtronik { };
+
   lupa = callPackage ../development/python-modules/lupa { };
 
   lupupy = callPackage ../development/python-modules/lupupy { };
