ca052ac444b4b0a1efb52dfef9ef0dea257e62fd
Matthias Beyer <mail@beyermatthias.de>
2021-11-19 14:35:04 +0100

    libisl: add 0.24
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

---
+++ pkgs/development/libraries/isl/0.24.0.nix
@@ -0,0 +1,16 @@
+{ callPackage, lib, stdenv, fetchurl, gmp }:
+
+callPackage ./generic.nix rec {
+  version = "0.24";
+  urls = [
+    "mirror://sourceforge/libisl/isl-${version}.tar.xz"
+    "https://libisl.sourceforge.io/isl-${version}.tar.xz"
+  ];
+  sha256 = "1ak1gq0rbqbah5517blg2zlnfvjxfcl9cjrfc75nbcx5p2gnlnd5";
+  homepage = "https://sourceforge.net/projects/libisl/";
+} // {
+  configureFlags = [
+    "--with-gcc-arch=generic" # don't guess -march=/mtune=
+  ];
+}
+
