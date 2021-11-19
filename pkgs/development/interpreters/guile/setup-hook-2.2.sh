918323afb8a4260421c6f655d13bae1c99a7854c
Victor Engmark <victor@engmark.name>
2021-11-19 22:36:01 +1300

    guile: Indicate shell to ShellCheck
    ShellCheck SC2148.

--- pkgs/development/interpreters/guile/setup-hook-2.2.sh
+++ pkgs/development/interpreters/guile/setup-hook-2.2.sh
@@ -1,3 +1,4 @@
+# shellcheck shell=bash
 addGuileLibPath () {
     if test -d "$1/share/guile/site/2.2"; then
         export GUILE_LOAD_PATH="${GUILE_LOAD_PATH-}${GUILE_LOAD_PATH:+:}$1/share/guile/site/2.2"
--- pkgs/development/interpreters/guile/setup-hook-3.0.sh
+++ pkgs/development/interpreters/guile/setup-hook-3.0.sh
@@ -1,3 +1,4 @@
+# shellcheck shell=bash
 addGuileLibPath () {
     if test -d "$1/share/guile/site/3.0"; then
         export GUILE_LOAD_PATH="${GUILE_LOAD_PATH-}${GUILE_LOAD_PATH:+:}$1/share/guile/site/3.0"
