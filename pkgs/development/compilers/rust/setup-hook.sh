7386170c46a1918f08132f2a1434827d9a89a54d
Victor Engmark <victor@engmark.name>
2021-11-19 22:36:00 +1300

    rust: Indicate shell to ShellCheck
    ShellCheck SC2148.

--- pkgs/development/compilers/rust/setup-hook.sh
+++ pkgs/development/compilers/rust/setup-hook.sh
@@ -1,3 +1,4 @@
+# shellcheck shell=bash
 # Fix 'failed to open: /homeless-shelter/.cargo/.package-cache' in rust 1.36.
 if [[ -z ${IN_NIX_SHELL-} && -z ${CARGO_HOME-} ]]; then
     export CARGO_HOME=$TMPDIR
