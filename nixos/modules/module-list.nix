c42db55ba6f55fd3f132879488dc5b29f641b0b0
Sumner Evans <me@sumnerevans.com>
2021-11-20 09:15:38 -0700

    nixos/matrix-synapse: move to services/matrix

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -488,6 +488,7 @@
   ./services/mail/nullmailer.nix
   ./services/matrix/matrix-appservice-discord.nix
   ./services/matrix/matrix-appservice-irc.nix
+  ./services/matrix/matrix-synapse.nix
   ./services/matrix/mjolnir.nix
   ./services/matrix/pantalaimon.nix
   ./services/misc/ananicy.nix
@@ -551,7 +552,6 @@
   ./services/misc/libreddit.nix
   ./services/misc/lifecycled.nix
   ./services/misc/mame.nix
-  ./services/misc/matrix-synapse.nix
   ./services/misc/mautrix-facebook.nix
   ./services/misc/mautrix-telegram.nix
   ./services/misc/mbpfan.nix
