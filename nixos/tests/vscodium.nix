6b9977942adb40518d74eead610d5e92b7067863
Patrick Hilhorst <git@hilhorst.be>
2021-11-20 11:26:30 +0100

    nixosTests.vscodium: add comments

--- nixos/tests/vscodium.nix
+++ nixos/tests/vscodium.nix
@@ -39,24 +39,29 @@ let
         machine.wait_for_unit('graphical.target')
         machine.wait_until_succeeds('pgrep -x codium')
 
+        # Wait until vscodium is visible. "File" is in the menu bar.
         machine.wait_for_text('File')
         machine.screenshot('start_screen')
 
         test_string = 'testfile'
 
+        # Create a new file
         machine.send_key('ctrl-n')
         machine.wait_for_text('Untitled')
         machine.screenshot('empty_editor')
 
+        # Type a string
         machine.send_chars(test_string)
         machine.wait_for_text(test_string)
         machine.screenshot('editor')
 
+        # Save the file
         machine.send_key('ctrl-s')
         machine.wait_for_text('Save')
         machine.screenshot('save_window')
-
         machine.send_key('ret')
+
+        # (the default filename is the first line of the file)
         machine.wait_for_file(f'/home/alice/{test_string}')
       '';
     });
