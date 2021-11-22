f9dc3b8bc79b085e580dff55a62876406139428a
Amy de Buitléir <amy@nualeargais.ie>
2021-11-22 18:52:39 +0000

    yambar: support both wayland and x11

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26985,9 +26985,7 @@ with pkgs;
     i3GapsSupport = false;
   };
 
-  yambar = callPackage ../applications/misc/yambar { x11Support = true; waylandSupport = false; };
-
-  yambar-wayland = callPackage ../applications/misc/yambar { x11Support = false; waylandSupport = true; };
+  yambar = callPackage ../applications/misc/yambar { x11Support = true; waylandSupport = true; };
 
   polyphone = libsForQt514.callPackage ../applications/audio/polyphone { };
 
