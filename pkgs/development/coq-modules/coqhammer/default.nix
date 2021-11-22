1201977ef7eb010557e942c1b444f56868adfac9
Vincent Laporte <Vincent.Laporte@gmail.com>
2021-11-22 18:43:25 +0000

    coqPackages.coqhammer: 1.3.1 → 1.3.2
    (cherry picked from commit e5f41e735d8e6b41f27dd3dd7cf40cd740b8d6f2)

--- pkgs/development/coq-modules/coqhammer/default.nix
+++ pkgs/development/coq-modules/coqhammer/default.nix
@@ -5,13 +5,19 @@ with lib; mkCoqDerivation {
   pname = "coqhammer";
   owner = "lukaszcz";
   defaultVersion = with versions; switch coq.coq-version [
-    { case = "8.13"; out = "1.3.1-coq8.13"; }
-    { case = "8.12"; out = "1.3.1-coq8.12"; }
-    { case = "8.11"; out = "1.3.1-coq8.11"; }
-    { case = "8.10"; out = "1.3.1-coq8.10"; }
+    { case = "8.14"; out = "1.3.2-coq8.14"; }
+    { case = "8.13"; out = "1.3.2-coq8.13"; }
+    { case = "8.12"; out = "1.3.2-coq8.12"; }
+    { case = "8.11"; out = "1.3.2-coq8.11"; }
+    { case = "8.10"; out = "1.3.2-coq8.10"; }
     { case = "8.9";  out = "1.1.1-coq8.9"; }
     { case = "8.8";  out = "1.1-coq8.8"; }
   ] null;
+  release."1.3.2-coq8.14".sha256 = "sha256:1pvs4p95lr31jb86f33p2q9v8zq3xbci1fk6s6a2g2snfxng1574";
+  release."1.3.2-coq8.13".sha256 = "sha256:0krsm8qj9lgfbggxv2jhkbk3vy2cz63qypnarnl31fdmpykchi4b";
+  release."1.3.2-coq8.12".sha256 = "sha256:08mnr13lrdnpims6kf8pk6axf4s8qqs0a71hzg3frkx21d6nawhh";
+  release."1.3.2-coq8.11".sha256 = "sha256:1z54lmr180rdkv549f0dygxlmamsx3fygvsm0d7rz9j88f2z8kc5";
+  release."1.3.2-coq8.10".sha256 = "sha256:08d63ckiwjx07hy5smg5c7a6b3m3a8ra4ljk3z6597633dx85cd0";
   release."1.3.1-coq8.13".sha256 = "033j6saw24anb1lqbgsg1zynxi2rnxq7pgqwh11k8r8y3xisz78w";
   release."1.3.1-coq8.12".sha256 = "0xy3vy4rv8w5ydwb9nq8y4dcimd91yr0hak2j4kn02svssg1kv1y";
   release."1.3.1-coq8.11".sha256 = "0i9nlcayq0ac95vc09d1w8sd221gdjs0g215n086qscqjwimnz8j";
