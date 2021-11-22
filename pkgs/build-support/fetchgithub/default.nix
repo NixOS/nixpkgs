4f3bdc0b240a104b28f4d03b7a0c6c3546506416
Giulio De Pasquale <depasquale+git@giugl.io>
2021-11-22 12:39:20 +0100

    make rev optional for fetchFromGitHub. no proper checks if useFetchGit yet.

--- pkgs/build-support/fetchgithub/default.nix
+++ pkgs/build-support/fetchgithub/default.nix
@@ -1,6 +1,6 @@
 { lib, fetchgit, fetchzip }:
 
-{ owner, repo, rev, name ? "source"
+{ owner, repo, rev ? null, name ? "source"
 , fetchSubmodules ? false, leaveDotGit ? null
 , deepClone ? false, private ? false, forceFetchGit ? false
 , githubBase ? "github.com", varPrefix ? null
@@ -30,8 +30,10 @@ let
   };
   fetcherArgs = (if useFetchGit
     then {
-      inherit rev deepClone fetchSubmodules; url = "${baseUrl}.git";
-    } // lib.optionalAttrs (leaveDotGit != null) { inherit leaveDotGit; }
+      inherit deepClone fetchSubmodules; url = "${baseUrl}.git";
+    } // lib.optionalAttrs (leaveDotGit != null) { inherit leaveDotGit; } 
+      # rev is not compulsory for fetchGit.
+      // lib.optionalAttrs (rev != null) { inherit rev; }
     else { url = "${baseUrl}/archive/${rev}.tar.gz"; }
   ) // privateAttrs // passthruAttrs // { inherit name; };
 in
