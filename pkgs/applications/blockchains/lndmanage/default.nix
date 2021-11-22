3a58cd414fcca910a9fba428e74fa64c2438071f
Martin Milata <martin@martinmilata.cz>
2021-11-22 18:07:27 +0100

    lndmanage: 0.13.0 -> 0.14.0
    https://github.com/bitromortac/lndmanage/releases/tag/v0.14.0

--- pkgs/applications/blockchains/lndmanage/default.nix
+++ pkgs/applications/blockchains/lndmanage/default.nix
@@ -2,13 +2,13 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "lndmanage";
-  version = "0.13.0";
+  version = "0.14.0";
 
   src = fetchFromGitHub {
     owner = "bitromortac";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1vnv03k2d11rw6mry6fmspiy3hqsza8y3daxnn4lp038gw1y0f4z";
+    hash = "sha256-wPr/R+WGACyhv2Qh9JeLJwvr2vQfxpqj2XjEkrRoSX4=";
   };
 
   propagatedBuildInputs = with python3Packages; [
