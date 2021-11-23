b8b08eb07ee6ed03d32f78b775171966cbd52122
Robert Schütz <nix@dotlambda.de>
2021-11-22 19:41:57 -0800

    python3Packages.total-connect-client: 2021.11.4 -> 2021.11.5
    https://github.com/craigjmidwinter/total-connect-client/releases/tag/2021.11.5

--- pkgs/development/python-modules/total-connect-client/default.nix
+++ pkgs/development/python-modules/total-connect-client/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "total-connect-client";
-  version = "2021.11.4";
+  version = "2021.11.5";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "craigjmidwinter";
     repo = "total-connect-client";
     rev = version;
-    sha256 = "sha256-iggxKefrVZP4ljf8jGRrCAq0/xQLsr54L42KkdjBqgI=";
+    sha256 = "sha256-6hyeU0jigQZsZtdb4E8/iD8YfQeNOKm6IK1CHBizZQs=";
   };
 
   propagatedBuildInputs = [
@@ -26,15 +26,6 @@ buildPythonPackage rec {
 
   checkInputs = [
     pytestCheckHook
-  ];
-
-  preCheck = ''
-    export PYTHONPATH="total_connect_client:$PYTHONPATH"
-  '';
-
-  disabledTests = [
-    # Tests require network access
-    "tests_request"
   ];
 
   pythonImportsCheck = [
