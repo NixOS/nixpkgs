8b8bd0e23effca7cb845e1089de9ceb3d1715690
pasqui23 <pasqui23@users.noreply.github.com>
2021-11-23 01:32:12 +0000

    systemdgenie: init at  0.99.0 (#143984)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9846,6 +9846,8 @@ with pkgs;
     inherit (xorg) xauth xorgserver;
   };
 
+  systemdgenie = libsForQt5.callPackage ../applications/system/systemdgenie { };
+
   t = callPackage ../tools/misc/t { };
 
   tabnine = callPackage ../development/tools/tabnine { };
