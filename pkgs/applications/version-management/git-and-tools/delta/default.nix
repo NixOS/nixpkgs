3cb164e0f04966ac567ca793242be8496d9d6fe5
Sandro <sandro.jaeckel@gmail.com>
2021-11-25 00:28:48 +0100

    delta: add SuperSandro2000 as maintainer
    (cherry picked from commit 31b46dd7f9af9c4360c5796a885c8ec7e8594e51)

--- pkgs/applications/version-management/git-and-tools/delta/default.nix
+++ pkgs/applications/version-management/git-and-tools/delta/default.nix
@@ -34,6 +34,6 @@ rustPlatform.buildRustPackage rec {
     description = "A syntax-highlighting pager for git";
     changelog = "https://github.com/dandavison/delta/releases/tag/${version}";
     license = licenses.mit;
-    maintainers = with maintainers; [ marsam zowoq ];
+    maintainers = with maintainers; [ marsam zowoq SuperSandro2000 ];
   };
 }
