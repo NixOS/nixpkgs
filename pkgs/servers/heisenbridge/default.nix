f8fddafe3c8a48dc55a35e0693c61b243eb8a36a
Sumner Evans <me@sumnerevans.com>
2021-11-24 16:54:11 -0700

    heisenbridge: add patch for compatibility with aiohttp 3.8.0

--- pkgs/servers/heisenbridge/default.nix
+++ pkgs/servers/heisenbridge/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchFromGitHub, python3Packages }:
+{ lib, fetchFromGitHub, fetchpatch, python3Packages }:
 
 python3Packages.buildPythonApplication rec {
   pname = "heisenbridge";
@@ -10,6 +10,14 @@ python3Packages.buildPythonApplication rec {
     rev = "v${version}";
     sha256 = "sha256-q1Rj8BehvYnV/Kah5YKAxBUz4j9WziSqn1fVeaKpy7g=";
   };
+
+  patches = [
+    # Compatibility with aiohttp 3.8.0
+    (fetchpatch {
+      url = "https://github.com/hifi/heisenbridge/commit/cff5d33e0b617e6cf3a44dc00c72b98743175c9e.patch";
+      sha256 = "sha256-y5X4mWvX1bq0XNZNTYUc0iK3SzvaHpS7px53I7xC9c8=";
+    })
+  ];
 
   postPatch = ''
     echo "${version}" > heisenbridge/version.txt
