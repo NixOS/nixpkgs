9a41b4088beddddf4805d6e13700c0788013435f
Jan Tojnar <jtojnar@gmail.com>
2021-11-24 20:51:46 +0100

    gnome.sushi: Fix runtime
    (cherry picked from commit fda27cf38e94511f8b8ee091e7189375493dec74)

--- pkgs/desktops/gnome/core/sushi/default.nix
+++ pkgs/desktops/gnome/core/sushi/default.nix
@@ -8,7 +8,8 @@
 , gnome
 , gtksourceview4
 , gjs
-, webkitgtk_4_1
+, libsoup
+, webkitgtk
 , icu
 , wrapGAppsHook
 , gst_all_1
@@ -48,7 +49,8 @@ stdenv.mkDerivation rec {
     gtksourceview4
     gdk-pixbuf
     librsvg
-    webkitgtk_4_1
+    libsoup
+    webkitgtk
     libepoxy
     gst_all_1.gstreamer
     gst_all_1.gst-plugins-base
