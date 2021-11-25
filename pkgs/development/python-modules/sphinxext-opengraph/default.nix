f846276916394e7ee37bcc290bbb5c86b14b98e5
Jonathan Ringer <jonringer117@gmail.com>
2021-11-25 14:47:16 -0800

    python3Packages.sphinxext-opengraph: 0.4.2 -> 0.5.0

--- pkgs/development/python-modules/sphinxext-opengraph/default.nix
+++ pkgs/development/python-modules/sphinxext-opengraph/default.nix
@@ -6,13 +6,13 @@
 
 buildPythonPackage rec {
   pname = "sphinxext-opengraph";
-  version = "0.4.2";
+  version = "0.5.0";
 
   src = fetchFromGitHub {
     owner = "wpilibsuite";
     repo = "sphinxext-opengraph";
     rev = "v${version}";
-    sha256 = "sha256-978aPtaqUDHcswDdFynzi+IjDYaBmCZDZk+dmDkhajY=";
+    sha256 = "0iri6sh6h7l1p8xg04bj3bphs1hwxh43sfnrv1c8b1wy84w988y9";
   };
 
   propagatedBuildInputs = [
