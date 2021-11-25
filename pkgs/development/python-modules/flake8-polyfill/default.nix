3fee224dcc9c754fe1bc15fb60889fc3b0670f61
Vikram Narayanan <vikram186@gmail.com>
2021-11-24 21:42:13 -0800

    flake8-polyfill: fix build

--- pkgs/development/python-modules/flake8-polyfill/default.nix
+++ pkgs/development/python-modules/flake8-polyfill/default.nix
@@ -1,5 +1,5 @@
 { lib, fetchPypi, buildPythonPackage
-, flake8
+, flake8, fetchpatch
 , mock, pep8, pytest }:
 
 buildPythonPackage rec {
@@ -10,6 +10,16 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "1nlf1mkqw856vi6782qcglqhaacb23khk9wkcgn55npnjxshhjz4";
   };
+
+  patches = [
+    # allow newer flake8 versions
+    # update when there is a version upgrade to flake8.
+    (fetchpatch {
+      url = "https://github.com/PyCQA/flake8-polyfill/pull/14/commits/649ee70574e5f64c09ada38da38dab6761c34549.patch";
+      sha256 = "sha256-WhASXEyVFQwDWq/sOclE9SQzrT5TDLk/s5yg326slKM=";
+      name = "fix-version.patch";
+    })
+  ];
 
   postPatch = ''
     # Failed: [pytest] section in setup.cfg files is no longer supported, change to [tool:pytest] instead.
