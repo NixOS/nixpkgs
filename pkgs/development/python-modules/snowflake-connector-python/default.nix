087153560642095e5f26a0da33976ca5df7f5d7f
Jonathan Ringer <jonringer117@gmail.com>
2021-11-25 14:47:15 -0800

    python3Packages.snowflake-connector-python: 2.6.2 -> 2.7.1

--- pkgs/development/python-modules/snowflake-connector-python/default.nix
+++ pkgs/development/python-modules/snowflake-connector-python/default.nix
@@ -24,12 +24,12 @@
 
 buildPythonPackage rec {
   pname = "snowflake-connector-python";
-  version = "2.6.2";
+  version = "2.7.1";
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ce131b1dd059a4d081e78595d618654bf9b9fc184d78352f24512375467257d1";
+    sha256 = "81175ff9fe0355a7e9007a087458dddc0816985ec10dc122e690de03549afbff";
   };
 
   propagatedBuildInputs = [
