38f4c87e6484a8156fb2d36207cd14ce6980e3e2
sternenseemann <sternenseemann@systemli.org>
2021-11-25 19:58:55 +0100

    haskell.compiler.*: disable useLLVM also for SPARC and PowerPC
    These targets also have NCG support, but they are tested less (in fact
    SPARC seems to be untested atm) and may have issues. In such cases being
    able to fallback to -fllvm without rebuilding the compiler could be
    useful. OTOH GHC will default to -fasm and the backends probably work
    well enough in most cases.

--- pkgs/development/compilers/ghc/9.0.1.nix
+++ pkgs/development/compilers/ghc/9.0.1.nix
@@ -12,7 +12,9 @@
 , # GHC can be built with system libffi or a bundled one.
   libffi ? null
 
-, useLLVM ? !stdenv.targetPlatform.isx86
+, useLLVM ? !(stdenv.targetPlatform.isx86
+              || stdenv.targetPlatform.isPowerPC
+              || stdenv.targetPlatform.isSparc)
 , # LLVM is conceptually a run-time-only depedendency, but for
   # non-x86, we need LLVM to bootstrap later stages, so it becomes a
   # build-time dependency too.
