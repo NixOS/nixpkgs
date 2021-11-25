ae1995c05d921a0a166a1fbb5178e9fb6988747b
Pasquale <p3dimaria@hotmail.it>
2021-11-25 21:38:49 +0100

    nixos/shellPkgs: refactoring

--- nixos/modules/config/shells-pkgs.nix
+++ nixos/modules/config/shells-pkgs.nix
@@ -1,92 +1,93 @@
 { config, pkgs, lib, ... }:
-with lib;
 let
-  lsMod = m: with types; listOf submodule m;
-  inherit (attrsets) nameValuePair mapAttrs;
-  attrFIelds = mapAttrs (n: v: n);
+  cfg = config.environment.shellPkgs;
+  cfgSh = sh: config.programs."${sh}".shellPkgs;
   inherit (builtins) filter;
-  phases = [ "shellInit" "promptInit" "loginShellInit" "interactiveShellInit" ];
-  shells = [ "bash" "fish" "zsh" ];
+  inherit (lib) types mkOption;
+  lsMod = m: with types; listOf submodule m;
+  shPkgBaseType = lsMod { freeformType = with types; oneOf [ package (submodule { }) ]; };
+  assertOutputsArePresent = outputNames: map (p: {
+    assertion = lib.lists.any (x: (p."${x}" or null) != null) outputNames;
+    message = ''
+      ${p.pname or p.name or (builtins.toJSON p)} must have at least one of ${strings.concatStringSep " , " outputNames} outputs.
+    '';
+  });
 
-  sh_phaseModule = sh: phase:
+
+  modByShAndPhase = sh: phase:
     let
       outputName = phase + "_" + sh;
-      getPlugin = attrset.attrByPath [ outputName ] null;
-      plugins = builtins.filter (p: null != (getPlugin p)) cfg;
-    in
-      {
-        options = {
-          environment.shellPkgs = mkOption {
-            type = lsMod {
-              options = nameValuePair outputName (
-                mkOption {
-                  type = with types; nullOr path;
-                  default = null;
-                  description = ''
-                    Path to a script that will be added to ${sh}.${phase}.
-                  '';
-                }
-              );
-            };
-          };
-          programs."${sh}".shellPkgs = mkOption {
-            type = lsMod {
-              options = nameValuePair outputName (
-                mkOption {
-                  type = with types; nullOr path;
-                  default = null;
-                  description = ''
-                    Path to a script that will be added to ${sh}.${phase}.
-                  '';
-                }
-              );
-            };
+      o.shellPkgs = mkOption {
+        type = lsMod {
+          options."${outputName}" = mkOption {
+            type = with types; nullOr path;
+            default = null;
+            description = ''
+              Path to a script that will be added to programs.${sh}.${phase}.
+            '';
           };
         };
-
-        config.programs."${sh}" = mkMerge [
-          {
-            shellPkgs = filter (p: null != p."${outputName}") config.environment.shellPkgs;
-          }
-        ] ++ (
-          map
-            (p: nameValuePair phase "source ${p."${outputName}"}")
-            config.programs."${sh}".shellPkgs
-        );
       };
-  shModule = sh: let
-    outputNames = map (phase: phase + "_" + sh) phases;
-  in
+      sourced = pkgs.runCommand outputName
+        {
+          files = map (p: p."${outputName}") (cfgSh sh);
+        } "cat $files > $out";
+    in
     {
-      imports = map (sh_phaseModule sh) phases;
+      options = {
+        environment = o;
+        programs."${sh}" = o;
+      };
+      config.environment.shellPkgsOutputs = [ outputName ];
+      config.programs."${sh}" = {
+        shellPkgs = filter (p: null != (p."${outputName}"or null)) cfg;
+        "${phase}" = "source ${sourced}";
+      };
+    };
+
+
+  modByPhasesAndSh = phases: sh:
+    let
+      outputNames = map (phase: phase + "_" + sh) phases;
+    in
+    {
+      imports = map (modByShAndPhase sh) phases;
       options.programs."${sh}".shellPkgs = mkOption {
-        default = [];
+        default = [ ];
         description = ''
           A list of packages that will be added to the various phases of ${sh}.
         '';
-        type =
-          lsMod { freeformType = with types; oneOf [ package (submodule {}) ]; };
+        type = shPkgBaseType // {
+          description = "A list of packages with outputs for the phases of ${sh}.";
+        };
       };
-      config.assertions = map (
-        p: {
-          assertion =
-            (lists.intersect outputNames (filter (f: null != (p."${f}"or null)) (attrFields p))) != [];
-          message = ''
-            ${toString p} must have at least one of ${strings.concatStringSep " , " outputNames} outputs.
-          '';
-        }
-      ) config.programs."${sh}".shellPkgs;
+      config.assertions = assertOutputsArePresent outputNames (cfgSh sh);
     };
 
 in
+
 {
-  imports = map shModule shells;
-  options.environment.shellPkgs = mkOption {
-    default = [];
-    description = ''
-      A list of packages that will be added to the various phases of various shells.
-    '';
-    type =
-      lsMod { freeformType = with types; oneOf [ package (submodule {}) ]; };
+  imports =
+    map (modByPhasesAndSh [ "shellInit" "promptInit" "loginShellInit" "interactiveShellInit" ])
+      [ "bash" "fish" "zsh" ];
+
+  options.environment = {
+    shellPkgs = mkOption {
+      default = [ ];
+      description = ''
+        A list of packages that will be added to the various phases of various shells.
+      '';
+      type = shPkgBaseType // {
+        description = "A list of packages with outputs for the phases of various shells.";
+      };
+    };
+
+    shellPkgsOutputs = mkOption {
+      internal = true;
+      type = with types; listOf string;
+      description = "A list of build outputs taken in consideration for environment.shellPkgs";
+    };
   };
+
+  config.assertions = assertOutputsArePresent config.environment.shellPkgsOutputs cfg;
 }
