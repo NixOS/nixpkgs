0f09e25b7b1ff3582ae84fb2ca4562a30d95e89b
Jonathan Ringer <jonringer117@gmail.com>
2021-11-25 14:47:15 -0800

    python3Packages.shortuuid: 1.0.1 -> 1.0.8

--- pkgs/development/python-modules/shortuuid/default.nix
+++ pkgs/development/python-modules/shortuuid/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage rec {
   pname = "shortuuid";
-  version = "1.0.1";
+  version = "1.0.8";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "3c11d2007b915c43bee3e10625f068d8a349e04f0d81f08f5fa08507427ebf1f";
+    sha256 = "9435e87e5a64f3b92f7110c81f989a3b7bdb9358e22d2359829167da476cfc23";
   };
 
   buildInputs = [pep8];
