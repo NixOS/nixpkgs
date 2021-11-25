a37d5183204a2587780691b52955a31487a17753
pasqui23 <pasqui23@users.noreply.github.com>
2021-11-25 16:57:45 +0000

    xxh: formatting
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/tools/networking/xxh/default.nix
+++ pkgs/tools/networking/xxh/default.nix
@@ -1,14 +1,18 @@
 { lib, fetchFromGitHub, buildPythonApplication, pexpect, pyyaml, openssh }:
+
 buildPythonApplication rec{
   pname = "xxh";
   version = "0.8.7";
+
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = version;
     hash = "sha256-AKfiFBaV8DC/Z7Bc+ZpwcJor/mzYomUaQKKobKXICn4=";
   };
+
   propagatedBuildInputs = [ pexpect pyyaml openssh ];
+
   meta = with lib; {
     description = "Bring your favorite shell wherever you go through the ssh";
     homepage = "https://github.com/xxh/xxh";
