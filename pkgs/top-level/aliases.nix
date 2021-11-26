e3b7448f2301459365d3e6833b9321a6dbf1ae73
Tom Bereknyei <tomberek@gmail.com>
2021-11-26 11:48:29 -0500

    nixStable: 2.4 -> 2.3.16
    Revert due to regressions. This is meant to be only for the 21.11
    release. See
    https://discourse.nixos.org/t/nix-2-4-and-what-s-next/16257 for
    additional information.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -600,7 +600,7 @@ mapAliases ({
   nilfs_utils = nilfs-utils; # added 2018-04-25
   nix-direnv-flakes = nix-direnv;
   nix-review = nixpkgs-review; # added 2019-12-22
-  nixFlakes = nixStable; # added 2021-05-21
+  nixFlakes = nix_2_4; # added 2021-05-21
   nmap_graphical = nmap-graphical;  # added 2017-01-19
   nmap-unfree = nmap; # added 2021-04-06
   nologin = shadow; # added 2018-04-25
