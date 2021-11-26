334a4e3dfb86fd518adec983b144b484e89ab6de
AndersonTorres <torres.anderson.85@protonmail.com>
2021-11-26 18:37:03 -0300

    zydis: 3.2.0 -> 3.2.1
    (cherry picked from commit 51ab665ad7d29ace29dd1b3702fa2a44d48e4e56)

--- pkgs/development/libraries/zydis/default.nix
+++ pkgs/development/libraries/zydis/default.nix
@@ -1,23 +1,28 @@
-{ lib, stdenv, fetchFromGitHub, cmake }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+}:
 
 stdenv.mkDerivation rec {
   pname = "zydis";
-  version = "3.2.0";
+  version = "3.2.1";
 
   src = fetchFromGitHub {
     owner = "zyantific";
     repo = "zydis";
     rev = "v${version}";
     fetchSubmodules = true;
-    sha256 = "sha256-1XGELwMuFlIt6Z3+kfD6VAgDZOwhhCSG42dkYh7WLf8=";
+    sha256 = "sha256-FB7hGQ9vI3ZE376iROEpdtZm91IiccBhtAFa94JgnUY=";
   };
 
   nativeBuildInputs = [ cmake ];
 
   meta = with lib; {
+    homepage = "https://zydis.re/";
     description = "Fast and lightweight x86/x86-64 disassembler library";
     license = licenses.mit;
-    maintainers = [ maintainers.jbcrail ];
+    maintainers = with maintainers; [ jbcrail ];
     platforms = platforms.all;
   };
 }
