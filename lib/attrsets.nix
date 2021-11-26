59d9f1a8e6e2986d3c2fa64d37d670f13024d0eb
Sandro <sandro.jaeckel@gmail.com>
2021-11-26 01:09:41 +0100

    lib.attrsets: remove unused inherit

--- lib/attrsets.nix
+++ lib/attrsets.nix
@@ -9,7 +9,7 @@ let
 in
 
 rec {
-  inherit (builtins) attrNames listToAttrs hasAttr isAttrs getAttr;
+  inherit (builtins) attrNames listToAttrs isAttrs getAttr;
 
 
   /* Return an attribute from nested attribute sets.
