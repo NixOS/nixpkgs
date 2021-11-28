63a3695759bc8640ee9782637c21fe9b91726ffe
figsoda <figsoda@pm.me>
2021-11-28 12:56:54 -0500

    csview: 0.3.8 -> 0.3.9

--- pkgs/tools/text/csview/default.nix
+++ pkgs/tools/text/csview/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "csview";
-  version = "0.3.8";
+  version = "0.3.9";
 
   src = fetchFromGitHub {
     owner = "wfxr";
     repo = pname;
     rev = "v${version}";
-    sha256 = "18bz12yn85h9vj0b18iaziix9km2iwh8gwfs93fddjv6kg87p38q";
+    sha256 = "sha256-FrdW3f/ydjClgySEa2AIlAC9NOAr9cE4W67zXmlrUrQ=";
   };
 
-  cargoSha256 = "1my6gl8zq5k7clzapgbf1mmcgq8mmdbhl250rdd1fvfd59wkrwra";
+  cargoSha256 = "sha256-cew6czpBGNF3kulgdmfoWl/4f1AyKvHTIk/3eGEwkhE=";
 
   meta = with lib; {
     description = "A high performance csv viewer with cjk/emoji support";
