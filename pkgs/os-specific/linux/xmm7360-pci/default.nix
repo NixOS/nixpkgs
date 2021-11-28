14efc0258a212cc656779ad6482069ca25faf265
Florian Klink <flokli@flokli.de>
2021-11-28 14:08:16 +0100

    linuxPackages*.xmm7360-pci: unbreak for kernels > 5.14
    As of v5.15, the `put_tty_driver` alias has been dropped in favor of
    directly calling `tty_driver_kref_put` (9f90a4ddef4e). Apply this change
    in the xmm7360 driver in order to allow building on recent kernels:
    https://github.com/xmm7360/xmm7360-pci/pull/152
    
    Also, apply some [incompatible pointer type fixes](https://github.com/xmm7360/xmm7360-pci/pull/139)
    and [stability fixes](https://github.com/xmm7360/xmm7360-pci/pull/149)
    which are queued in that repository.
    
    I successfully built this for 5.14 and 5.15, so let's remove the
    isBroken for these versions.

--- pkgs/os-specific/linux/xmm7360-pci/default.nix
+++ pkgs/os-specific/linux/xmm7360-pci/default.nix
@@ -11,6 +11,20 @@ stdenv.mkDerivation rec {
     sha256 = "1wdb0phqg9rj9g9ycqdya0m7lx24kzjlh25yw0ifp898ddxrrr0c";
   };
 
+  patches = [
+    # Change xmm7360_tty_write_room to unsigned in xmm7360.c
+    # https://github.com/xmm7360/xmm7360-pci/pull/139
+    ./516617f9bb63900f1e1a96d8ef58105edb9808a8.patch
+
+    # xmm7360: Drop put_tty_driver
+    # https://github.com/xmm7360/xmm7360-pci/pull/152
+    ./72e2d597d19d7b2a4e172637715aa0c14deaee71.patch
+
+    # xmm7360: Add irq handler before device init
+    # https://github.com/xmm7360/xmm7360-pci/pull/149
+    ./704e1ca6421947183be4b1fe9f2f6c54213b60c2.patch
+  ];
+
   makeFlags = [ "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build" ];
 
   nativeBuildInputs = kernel.moduleBuildDependencies;
@@ -24,6 +38,6 @@ stdenv.mkDerivation rec {
     license = licenses.isc;
     maintainers = with maintainers; [ flokli hexa ];
     platforms = platforms.linux;
-    broken = kernel.kernelOlder "4.10" || kernel.kernelAtLeast "5.14";
+    broken = kernel.kernelOlder "4.10";
   };
 }
