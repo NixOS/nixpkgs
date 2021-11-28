b26b92159957f18b3133b5e2d5695736f9e1173d
K900 <me@0upti.me>
2021-11-28 20:47:12 +0300

    matplotlib: 3.4.3 -> 3.5.0
    Some new deps were added, and a dependency cycle along with them.
    Break it in a most ugly-yet-efficient way possible.
    
    Also, set MPLSETUPCFG instead of renaming setup.cfg so older
    versions keep building with stuff like poetry2nix.

--- pkgs/development/python-modules/matplotlib/default.nix
+++ pkgs/development/python-modules/matplotlib/default.nix
@@ -1,7 +1,7 @@
 { lib, stdenv, fetchPypi, writeText, buildPythonPackage, isPy3k, pycairo
 , which, cycler, python-dateutil, numpy, pyparsing, sphinx, tornado, kiwisolver
 , freetype, qhull, libpng, pkg-config, mock, pytz, pygobject3, gobject-introspection
-, certifi, pillow
+, certifi, pillow, fonttools, setuptools-scm, setuptools-scm-git-archive
 , enableGhostscript ? true, ghostscript, gtk3
 , enableGtk3 ? false, cairo
 # darwin has its own "MacOSX" backend
@@ -17,27 +17,41 @@ let
 in
 
 buildPythonPackage rec {
-  version = "3.4.3";
+  version = "3.5.0";
   pname = "matplotlib";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "06032j0ccjxldx4z9kf97qps2g36mfgvy1nap3b9n75kzmnm4kzw";
+    sha256 = "1ym6v4d36lsd5bf8p83d1g1yksd5c9hjfxsh52a5bn908hjjm29q";
   };
 
   XDG_RUNTIME_DIR = "/tmp";
+  MPLSETUPCFG = "setup.cfg";
 
   nativeBuildInputs = [ pkg-config ];
 
-  buildInputs = [ which sphinx ]
+  buildInputs = [ which sphinx setuptools-scm setuptools-scm-git-archive ]
     ++ lib.optional enableGhostscript ghostscript
     ++ lib.optional stdenv.isDarwin [ Cocoa ];
 
   propagatedBuildInputs =
     [ cycler python-dateutil numpy pyparsing tornado freetype qhull
-      kiwisolver certifi libpng mock pytz pillow ]
+      kiwisolver certifi libpng mock pytz pillow
+
+      # HACK: there's a dependency cycle:
+      # matplotlib -> fonttools -> pytest-randomly (check) ->
+      # -> factory_boy (check) -> flask (check) -> python-dotenv ->
+      # -> ipython (check) -> matplotlib-inline -> matplotlib
+      #
+      # Instead of trying to unbundle this trainwreck, just build
+      # a second copy of fonttools without the optional deps.
+      (fonttools.overridePythonAttrs(old: {
+        doCheck = false;
+        checkInputs = [];
+      }))
+    ]
     ++ lib.optionals enableGtk3 [ cairo pycairo gtk3 gobject-introspection pygobject3 ]
     ++ lib.optionals enableTk [ tcl tk tkinter libX11 ]
     ++ lib.optionals enableQt [ pyqt5 ];
