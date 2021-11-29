819d9f9a60705ccc15ebabdfb4f6bdee6c693e65
Jonathan Ringer <jonringer117@gmail.com>
2021-11-28 23:46:45 -0800

    python3Packages.matplotlib: fix build, disable network fetch of deps

--- pkgs/development/python-modules/matplotlib/default.nix
+++ pkgs/development/python-modules/matplotlib/default.nix
@@ -1,7 +1,7 @@
 { lib, stdenv, fetchPypi, writeText, buildPythonPackage, isPy3k, pycairo
 , which, cycler, python-dateutil, numpy, pyparsing, sphinx, tornado, kiwisolver
 , freetype, qhull, libpng, pkg-config, mock, pytz, pygobject3, gobject-introspection
-, certifi, pillow
+, certifi, pillow, fonttools, setuptools-scm, setuptools-scm-git-archive
 , enableGhostscript ? true, ghostscript, gtk3
 , enableGtk3 ? false, cairo
 # darwin has its own "MacOSX" backend
@@ -29,14 +29,18 @@ buildPythonPackage rec {
 
   XDG_RUNTIME_DIR = "/tmp";
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [
+    pkg-config
+    setuptools-scm
+    setuptools-scm-git-archive
+  ];
 
   buildInputs = [ which sphinx ]
     ++ lib.optional enableGhostscript ghostscript
     ++ lib.optional stdenv.isDarwin [ Cocoa ];
 
   propagatedBuildInputs =
-    [ cycler python-dateutil numpy pyparsing tornado freetype qhull
+    [ cycler fonttools python-dateutil numpy pyparsing tornado freetype qhull
       kiwisolver certifi libpng mock pytz pillow ]
     ++ lib.optionals enableGtk3 [ cairo pycairo gtk3 gobject-introspection pygobject3 ]
     ++ lib.optionals enableTk [ tcl tk tkinter libX11 ]
@@ -74,6 +78,13 @@ buildPythonPackage rec {
       substituteInPlace src/_c_internal_utils.c \
         --replace libX11.so.6 ${libX11}/lib/libX11.so.6 \
         --replace libwayland-client.so.0 ${wayland}/lib/libwayland-client.so.0
+    '' + ''
+      # avoid matplotlib trying to download dependencies
+      echo "[libs]
+      system_freetype=true
+      system_qhull=true" > mplsetup.cfg
+      cat mplsetup.cfg
+      echo FFFFFFFFFFFFFFFFFFFFFFFFFFFFF
     '';
 
   # Matplotlib needs to be built against a specific version of freetype in
