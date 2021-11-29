4679a09afd1df837eaece7beffcfb827f677afc0
Jonathan Ringer <jonringer117@gmail.com>
2021-11-29 10:07:33 -0800

    python3Packages.django_compressor: fix build

--- pkgs/development/python-modules/django_compressor/default.nix
+++ pkgs/development/python-modules/django_compressor/default.nix
@@ -1,5 +1,6 @@
 { lib, buildPythonPackage, fetchPypi,
   rcssmin, rjsmin, django_appconf }:
+
 buildPythonPackage rec {
     pname = "django_compressor";
     version = "2.4.1";
@@ -9,8 +10,9 @@ buildPythonPackage rec {
       sha256 = "3358077605c146fdcca5f9eaffb50aa5dbe15f238f8854679115ebf31c0415e0";
     };
     postPatch = ''
-      substituteInPlace setup.py --replace 'rcssmin == 1.0.6' 'rcssmin' \
-        --replace 'rjsmin == 1.0.12' 'rjsmin'
+      substituteInPlace setup.py \
+        --replace 'rcssmin == 1.0.6' 'rcssmin' \
+        --replace 'rjsmin == 1.1.0' 'rjsmin'
     '';
 
     # requires django-sekizai, which we don't have packaged yet
