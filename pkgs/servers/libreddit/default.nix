374dc22d7febd1e11bf1e886915807a418829ded
Johannes Schleifenbaum <johannes@js-webcoding.de>
2021-11-29 09:03:47 +0100

    libreddit: 0.19.1 -> 0.21.2

--- pkgs/servers/libreddit/default.nix
+++ pkgs/servers/libreddit/default.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "libreddit";
-  version = "0.19.1";
+  version = "0.21.2";
 
   src = fetchFromGitHub {
     owner = "spikecodes";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-d6MDyFwQGvPQ1o+oYzZWhG2vFhBrFwMoV7a9vqFj+gs=";
+    sha256 = "sha256-NlAalhKsku9dJ3xZjXdI70QAltwWaHGLMYpWCRqU2Lk=";
   };
 
-  cargoSha256 = "sha256-Dpfs4kC8wijmEd2HejT/RfWAbfxqO701uFcUtBGmCjo=";
+  cargoSha256 = "sha256-FzV61UtSn86llAlp4/ceVkFaN6W88OjrVsaLA7LoUhs=";
 
   buildInputs = lib.optional stdenv.isDarwin Security;
 
