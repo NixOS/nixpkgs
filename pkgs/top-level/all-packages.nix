2253021b7e911016509bae8ad922f9b8db1d27ac
P. R. d. O <d.ol.rod@protonmail.com>
2021-11-29 19:59:54 -0600

    alfaview: init at 8.32.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23989,6 +23989,8 @@ with pkgs;
 
   alembic = callPackage ../development/libraries/alembic {};
 
+  alfaview = callPackage ../applications/networking/instant-messengers/alfaview { };
+
   alchemy = callPackage ../applications/graphics/alchemy { };
 
   alock = callPackage ../misc/screensavers/alock { };
