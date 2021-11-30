19fc2292942cfcb7c07dade739989a64545d812f
sternenseemann <sternenseemann@systemli.org>
2021-11-30 23:00:11 +0100

    haskell.compiler.*: use targetCC for hasGold check
    This is a bit shorter and more consistent with the rest of the file.

--- pkgs/development/compilers/ghc/8.8.4.nix
+++ pkgs/development/compilers/ghc/8.8.4.nix
@@ -142,7 +142,7 @@ let
   # But we cannot avoid BFD when using musl libc due to https://sourceware.org/bugzilla/show_bug.cgi?id=23856
   # see #84670 and #49071 for more background.
   useLdGold = targetPlatform.linker == "gold" ||
-    (targetPlatform.linker == "bfd" && (targetPackages.stdenv.cc.bintools.bintools.hasGold or false) && !targetPlatform.isMusl);
+    (targetPlatform.linker == "bfd" && (targetCC.bintools.bintools.hasGold or false) && !targetPlatform.isMusl);
 
   # Makes debugging easier to see which variant is at play in `nix-store -q --tree`.
   variantSuffix = lib.concatStrings [
