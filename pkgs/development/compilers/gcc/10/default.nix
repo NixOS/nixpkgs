0d1750e9c041fa24752adb128625fee0952a6086
oxalica <oxalicc@pm.me>
2021-11-30 15:17:55 +0800

    gcc{10,11}: add no-sys-dir patch for RiscV
    RiscV has an individual STARTFILE_PREFIX_SPEC macro which also need to
    be patched.

--- pkgs/development/compilers/gcc/10/default.nix
+++ pkgs/development/compilers/gcc/10/default.nix
@@ -64,6 +64,7 @@ let majorVersion = "10";
     patches =
          optional (targetPlatform != hostPlatform) ../libstdc++-target.patch
       ++ optional noSysDirs ../no-sys-dirs.patch
+      ++ optional (noSysDirs && hostPlatform.isRiscV) ../no-sys-dirs-riscv.patch
       /* ++ optional (hostPlatform != buildPlatform) (fetchpatch { # XXX: Refine when this should be applied
         url = "https://git.busybox.net/buildroot/plain/package/gcc/${version}/0900-remove-selftests.patch?id=11271540bfe6adafbc133caf6b5b902a816f5f02";
         sha256 = ""; # TODO: uncomment and check hash when available.
