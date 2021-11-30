a008c576cf0d006646253fbb5f506c1e3237ca3c
Fabian Affolter <mail@fabian-affolter.ch>
2021-11-30 15:40:31 +0100

    python3Packages.apsw: switch to pytestCheckHook, disable failing tests

--- pkgs/development/python-modules/apsw/default.nix
+++ pkgs/development/python-modules/apsw/default.nix
@@ -1,8 +1,15 @@
-{ lib, buildPythonPackage, fetchFromGitHub, sqlite, isPyPy }:
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, sqlite
+, isPyPy
+, pytestCheckHook
+}:
 
 buildPythonPackage rec {
   pname = "apsw";
   version = "3.33.0-r1";
+  format = "setuptools";
 
   disabled = isPyPy;
 
@@ -13,11 +20,35 @@ buildPythonPackage rec {
     sha256 = "05mxcw1382xx22285fnv92xblqby3adfrvvalaw4dc6rzsn6kcan";
   };
 
-  buildInputs = [ sqlite ];
+  buildInputs = [
+    sqlite
+  ];
+
+  checkInputs = [
+    pytestCheckHook
+  ];
+
+  pytestFlagsArray = [
+    "tests.py"
+  ];
+
+  disabledTests = [
+    "testCursor"
+    "testLoadExtension"
+    "testShell"
+    "testVFS"
+    "testVFSWithWAL"
+    "testdb"
+  ];
+
+  pythonImportsCheck = [
+    "apsw"
+  ];
 
   meta = with lib; {
     description = "A Python wrapper for the SQLite embedded relational database engine";
     homepage = "https://github.com/rogerbinns/apsw";
     license = licenses.zlib;
+    maintainers = with maintainers; [ ];
   };
 }
