4e4743cba672db8e0fee1826f2b206335d2a19b4
Bruno Bzeznik <Bruno.Bzeznik@univ-grenoble-alpes.fr>
2021-11-30 19:32:45 +0100

    Update pkgs/applications/science/biology/repseek/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/applications/science/biology/repseek/default.nix
+++ pkgs/applications/science/biology/repseek/default.nix
@@ -10,8 +10,9 @@ stdenv.mkDerivation rec {
 
   preConfigure = ''
     mkdir -p $out/bin
-    substituteInPlace Makefile --replace "INSTALLDIR = \$\$HOME/bin" "INSTALLDIR = $out/bin/"
-    substituteInPlace Makefile --replace "CC= gcc" "CC = $CC"
+    substituteInPlace Makefile \
+      --replace "INSTALLDIR = \$\$HOME/bin" "INSTALLDIR = $out/bin/" \
+      --replace "CC= gcc" "CC = $CC"
   '' + lib.optionalString stdenv.isLinux ''
     substituteInPlace Makefile --replace "MACHINE = MACOSX" "MACHINE = LINUX"
   '';
