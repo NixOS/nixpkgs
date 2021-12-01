2f444f12c8c6dceba20db54b4afe33192a929a7d
GRBurst <GRBurst@protonmail.com>
2021-12-01 10:59:45 +0100

    runescape-launcher: use package from archive.org
    Signed-off-by: GRBurst <GRBurst@protonmail.com>

--- pkgs/games/runescape-launcher/default.nix
+++ pkgs/games/runescape-launcher/default.nix
@@ -10,8 +10,9 @@ let
     pname = "runescape-launcher";
     version = "2.2.9";
 
+    # upstream is https://content.runescape.com/downloads/ubuntu/pool/non-free/r/${pname}/${pname}_${version}_amd64.deb
     src = fetchurl {
-      url = "https://content.runescape.com/downloads/ubuntu/pool/non-free/r/${pname}/${pname}_${version}_amd64.deb";
+      url = "https://archive.org/download/${pname}_${version}_amd64/${pname}_${version}_amd64.deb";
       sha256 = "10ly44lbgbbqc65lx7mhfzfjaiz1xj0mfjqlwlxz0sdjyw4a6ij6";
     };
 
@@ -89,23 +90,23 @@ in
   * FHS simulates a classic linux shell
   */
   buildFHSUserEnv {
-   name = "RuneScape";
-   targetPkgs = pkgs: [
-     runescape
-     dpkg glibc gcc-unwrapped
-     libSM libXxf86vm libX11 glib pango cairo gtk2-x11 zlib openssl
-     libpulseaudio
-     xorg.libX11
-     SDL2 xorg_sys_opengl libGL
-   ];
-   multiPkgs = pkgs: [ libGL ];
-   runScript = "runescape-launcher";
+    name = "RuneScape";
+    targetPkgs = pkgs: [
+      runescape
+      dpkg glibc gcc-unwrapped
+      libSM libXxf86vm libX11 glib pango cairo gtk2-x11 zlib openssl
+      libpulseaudio
+      xorg.libX11
+      SDL2 xorg_sys_opengl libGL
+    ];
+    multiPkgs = pkgs: [ libGL ];
+    runScript = "runescape-launcher";
 
-   meta = with lib; {
-     description = "Launcher for RuneScape 3";
-     homepage = "https://www.runescape.com/";
-     license = licenses.unfree;
-     maintainers = with maintainers; [ grburst ];
-     platforms = [ "x86_64-linux" ];
-   };
-}
+    meta = with lib; {
+      description = "RuneScape Game Client (NXT) - Launcher for RuneScape 3";
+      homepage = "https://www.runescape.com/";
+      license = licenses.unfree;
+      maintainers = with maintainers; [ grburst ];
+      platforms = [ "x86_64-linux" ];
+    };
+  }
