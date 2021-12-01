36afb5849371a1898dc605b0905f4d351cee4b39
Pontus Stenetorp <pontus@stenetorp.se>
2021-12-01 17:11:15 +0000

    julia_10-bin and julia_16-bin: update LTS alias
    In line with the upstream announcement:
    
        https://discourse.julialang.org/t/julia-v1-7-0-has-been-released-and-v1-6-is-now-lts/72336

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12442,7 +12442,7 @@ with pkgs;
   julia_16-bin = callPackage ../development/compilers/julia/1.6-bin.nix { };
   julia_17-bin = callPackage ../development/compilers/julia/1.7-bin.nix { };
 
-  julia-lts-bin = julia_10-bin;
+  julia-lts-bin = julia_16-bin;
   julia-stable-bin = julia_17-bin;
   julia-bin = julia-stable-bin;
 
