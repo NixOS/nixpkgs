3151b3d7174ef35219dd63896503990ae9623a04
Kira Bruneau <kira.bruneau@pm.me>
2021-11-30 23:16:03 -0500

    goverlay: 0.6.4 → 0.7

--- pkgs/tools/graphics/goverlay/default.nix
+++ pkgs/tools/graphics/goverlay/default.nix
@@ -20,6 +20,7 @@
 , util-linux
 , vulkan-tools
 , which
+, nix-update-script
 }:
 
 let
@@ -36,13 +37,13 @@ let
   '';
 in stdenv.mkDerivation rec {
   pname = "goverlay";
-  version = "0.6.4";
+  version = "0.7";
 
   src = fetchFromGitHub {
     owner = "benjamimgois";
     repo = pname;
     rev = version;
-    sha256 = "sha256-xuv7u2lLQAB0Zmu7UHGXP/sJwcb8vHDf9hFL+pF+818=";
+    sha256 = "sha256-LdpgEfCNbf0/sY8v8D3KiapYEd23tVy4nQ7RuGwl7jM=";
   };
 
   outputs = [ "out" "man" ];
@@ -101,6 +102,10 @@ in stdenv.mkDerivation rec {
     # See https://github.com/benjamimgois/goverlay/issues/107
     "--set QT_QPA_PLATFORM xcb"
   ];
+
+  passthru.updateScript = nix-update-script {
+    attrPath = pname;
+  };
 
   meta = with lib; {
     description = "An opensource project that aims to create a Graphical UI to help manage Linux overlays";
