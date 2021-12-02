1a768ef4da5450ce3e5afad447d238c54e66d103
Justin Bedo <cu@cua0.org>
2021-12-03 09:33:30 +1100

    genmap: init 1.3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5507,6 +5507,8 @@ with pkgs;
 
   genimage = callPackage ../tools/filesystems/genimage { };
 
+  genmap = callPackage ../applications/science/biology/genmap { };
+
   geonkick = callPackage ../applications/audio/geonkick {};
 
   gerrit = callPackage ../applications/version-management/gerrit { };
