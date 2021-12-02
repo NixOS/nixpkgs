7a779a23845fcdb6cbf980c0fac6f285d078d44a
TredwellGit <tredwell@tutanota.com>
2021-12-02 07:52:30 +0000

    linux: 4.14.255 -> 4.14.256

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "4.14.255";
+  version = "4.14.256";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1mxjwqijnlvrdbpd973kdycz1dwklfkj9vpnj1cihgsf880q02zh";
+    sha256 = "180s2zmkfxk7af9nnkmfi2cs56af6vwyd21hjcfdxiygjm7j114p";
   };
 } // (args.argsOverride or {}))
