60252466babdc28e647f6d7fb4544233cadacf33
zowoq <59103226+zowoq@users.noreply.github.com>
2021-12-04 07:45:10 +1000

    vault-bin: set dontStrip for darwin
    immediately errors unless this is set

--- pkgs/tools/security/vault/vault-bin.nix
+++ pkgs/tools/security/vault/vault-bin.nix
@@ -51,6 +51,8 @@ in stdenv.mkDerivation {
     runHook postInstall
   '';
 
+  dontStrip = stdenv.isDarwin;
+
   meta = with lib; {
     homepage = "https://www.vaultproject.io";
     description = "A tool for managing secrets, this binary includes the UI";
