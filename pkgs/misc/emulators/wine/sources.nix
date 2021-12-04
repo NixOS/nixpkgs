bee62e0efe677d5b7c690ee01f41513125e01ada
Stefan Frijters <sfrijters@gmail.com>
2021-12-05 00:15:49 +0100

    wine{Unstable,Staging}: 6.22 -> 6.23

--- pkgs/misc/emulators/wine/sources.nix
+++ pkgs/misc/emulators/wine/sources.nix
@@ -44,9 +44,9 @@ in rec {
 
   unstable = fetchurl rec {
     # NOTE: Don't forget to change the SHA256 for staging as well.
-    version = "6.22";
+    version = "6.23";
     url = "https://dl.winehq.org/wine/source/6.x/wine-${version}.tar.xz";
-    sha256 = "sha256-gmBCoYGph5cyo9rLv4FnYF8wUpG/z6r16qDslSXXZO8=";
+    sha256 = "sha256-2Wx/MXQeQlH7UPkZMJxgRmUmfPrO4T+PRQowyVNiDg0=";
     inherit (stable) gecko32 gecko64;
 
     ## see http://wiki.winehq.org/Mono
@@ -65,7 +65,7 @@ in rec {
   staging = fetchFromGitHub rec {
     # https://github.com/wine-staging/wine-staging/releases
     inherit (unstable) version;
-    sha256 = "sha256-dGyaos6xITiAQdU65/PJQKRl5Rr5xBpgQhXii9iwb+E=";
+    sha256 = "sha256-ml4dPdkKFxeWf/hAFw64Xg9kppe2Az0vyx7WFr/ZhR0=";
     owner = "wine-staging";
     repo = "wine-staging";
     rev = "v${version}";
