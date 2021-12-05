2fde20d45c5e40c837b89c60a2232eac58da40bd
Patrick Chilton <chpatrick@gmail.com>
2021-12-05 18:20:35 +0100

    gnome.gnome-flashback: 3.42.0 → 3.42.1
    https://ftp.gnome.org/pub/GNOME/sources/gnome-flashback/3.42/gnome-flashback-3.42.1.news

--- pkgs/desktops/gnome/misc/gnome-flashback/default.nix
+++ pkgs/desktops/gnome/misc/gnome-flashback/default.nix
@@ -30,7 +30,7 @@
 }:
 let
   pname = "gnome-flashback";
-  version = "3.42.0";
+  version = "3.42.1";
 
   # From data/sessions/Makefile.am
   requiredComponentsCommon = enableGnomePanel:
@@ -61,7 +61,7 @@ let
 
     src = fetchurl {
       url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${name}.tar.xz";
-      sha256 = "1CcdwHrHOyceS07cgfMfZPVzGqbUSOehXX2TOXbc3Us=";
+      sha256 = "sha256:0kl4m05whm03m2v0y3jd69lghkggn8s0hxdhvchcas7jmhh940n8";
     };
 
     # make .desktop Execs absolute
