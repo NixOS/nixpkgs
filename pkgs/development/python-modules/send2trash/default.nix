c9f0477ef3f0de8f73b1de421d624f258c43e765
Nick Cao <nickcao@nichi.co>
2021-12-05 17:37:05 +0800

    pythonPackages.send2trash: 1.5.0 -> 1.8.0

--- pkgs/development/python-modules/send2trash/default.nix
+++ pkgs/development/python-modules/send2trash/default.nix
@@ -6,13 +6,13 @@
 
 buildPythonPackage rec {
   pname = "Send2Trash";
-  version = "1.5.0";
+  version = "1.8.0";
 
   src = fetchFromGitHub {
-    owner = "hsoft";
+    owner = "arsenetar";
     repo = "send2trash";
     rev = version;
-    sha256 = "1c76zldhw2ay7q7r00nnzcampjz9lkqfcbzqpm0iqp5i6bmmv30v";
+    sha256 = "sha256-HZeN/kpisPRrVwg1xGGUjxspztZKRbacGY5gpa537cw=";
   };
 
   doCheck = !stdenv.isDarwin;
@@ -21,7 +21,7 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Send file to trash natively under macOS, Windows and Linux";
-    homepage = "https://github.com/hsoft/send2trash";
+    homepage = "https://github.com/arsenetar/send2trash";
     license = licenses.bsd3;
   };
 }
