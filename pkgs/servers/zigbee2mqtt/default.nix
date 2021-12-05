b7907f7e19b1c3320c6bf5e41407fe2dab3d30d8
Simon Weber <sweber2342@gmail.com>
2021-12-05 17:24:29 +0100

    zigbee2mqtt: 1.16.2 -> 1.22.1

--- pkgs/servers/zigbee2mqtt/default.nix
+++ pkgs/servers/zigbee2mqtt/default.nix
@@ -5,14 +5,14 @@ in
 package.override rec {
   # don't upgrade! Newer versions cause stack overflows and fail trunk-combined
   # see https://github.com/NixOS/nixpkgs/pull/118400
-  version = "1.16.2";
+  version = "1.22.1";
   reconstructLock = true;
 
   src = pkgs.fetchFromGitHub {
     owner = "Koenkk";
     repo = "zigbee2mqtt";
     rev = version;
-    sha256 = "0rpmm4pwm8s4i9fl26ql0czg5kijv42k9wwik7jb3ppi5jzxrakd";
+    sha256 = "HoheB+/K4THFqgcC79QZM71rDPv2JB+S6y4K1+sdASo=";
   };
 
   passthru.tests.zigbee2mqtt = nixosTests.zigbee2mqtt;
