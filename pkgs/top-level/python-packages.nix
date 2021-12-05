5bd4dadb172ab9829c44021d8b599b75bff60422
nkalupahana <avishkank@gmail.com>
2021-12-05 00:58:01 -0600

    python3Packages.empy: init at 3.3.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2470,6 +2470,8 @@ in {
 
   emoji = callPackage ../development/python-modules/emoji { };
 
+  empy = callPackage ../development/python-modules/empy { };
+
   emulated-roku = callPackage ../development/python-modules/emulated-roku { };
 
   enaml = callPackage ../development/python-modules/enaml { };
