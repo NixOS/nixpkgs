7cccd548fd78833a3b5b6bc51a95c5740428cf28
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-06 14:31:58 +0000

    smcroute: 2.5.3 -> 2.5.5

--- pkgs/servers/smcroute/default.nix
+++ pkgs/servers/smcroute/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "smcroute";
-  version = "2.5.3";
+  version = "2.5.5";
 
   src = fetchFromGitHub {
     owner = "troglobit";
     repo = "smcroute";
     rev = version;
-    sha256 = "sha256-0s4BIJbbygt7Wpxlp13QGbXpvZsdIBszE7TOaN2aq/E=";
+    sha256 = "sha256-kMK2XLyI9GG83gxjVh4I7wSMEPM5iJSmgtwVJNHn9OQ=";
   };
 
   nativeBuildInputs = [ autoreconfHook pkg-config ];
