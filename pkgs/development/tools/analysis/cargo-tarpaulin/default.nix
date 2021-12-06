aea02423ee49a9162d7ca137dc8b57319793bead
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-06 07:43:24 +0000

    cargo-tarpaulin: 0.18.3 -> 0.18.5

--- pkgs/development/tools/analysis/cargo-tarpaulin/default.nix
+++ pkgs/development/tools/analysis/cargo-tarpaulin/default.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-tarpaulin";
-  version = "0.18.3";
+  version = "0.18.5";
 
   src = fetchFromGitHub {
     owner = "xd009642";
     repo = "tarpaulin";
     rev = version;
-    sha256 = "sha256-n43n9rmLWmGF7JXz/jspT6Stn0fQnkbkxqbuqXiZLHA=";
+    sha256 = "sha256-vYfoCKHN7kaXSkZI7cdh9pzlX3LqYQNeENaoztKwcII=";
   };
 
   nativeBuildInputs = [
@@ -17,7 +17,7 @@ rustPlatform.buildRustPackage rec {
   buildInputs = [ openssl ]
     ++ lib.optionals stdenv.isDarwin [ curl Security ];
 
-  cargoSha256 = "sha256-dJey7z0gRUu5MRKOfR909eQNTt3zpZnoWYvRhHzZlGs=";
+  cargoSha256 = "sha256-I7a3Vm/+oUqVLPYXvlz+I0Epuems7swePmV8vmmP6TU=";
   #checkFlags = [ "--test-threads" "1" ];
   doCheck = false;
 
