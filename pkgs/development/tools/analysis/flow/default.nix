942263df96dbbe2461b9f2a76f7f16e93ef87cf8
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-06 22:26:54 +0000

    flow: 0.163.0 -> 0.166.1

--- pkgs/development/tools/analysis/flow/default.nix
+++ pkgs/development/tools/analysis/flow/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "flow";
-  version = "0.163.0";
+  version = "0.166.1";
 
   src = fetchFromGitHub {
     owner = "facebook";
     repo = "flow";
     rev = "v${version}";
-    sha256 = "sha256-8O7EuYMNEa0M3HS0U10VbkevxL/cnySXHkT1WdI4Z84=";
+    sha256 = "sha256-n07P7C+RehZJ+s7ZAgtHBMUwG62rxbjcXKLjKJf+rDA=";
   };
 
   installPhase = ''
