35535a379b02ad514195adcc181e9217601edc2d
0x4A6F <0x4A6F@users.noreply.github.com>
2021-12-06 16:22:19 +0100

    wiretrustee: enable tests

--- pkgs/tools/networking/wiretrustee/default.nix
+++ pkgs/tools/networking/wiretrustee/default.nix
@@ -17,7 +17,10 @@ buildGoModule rec {
   };
 
   vendorSha256 = "sha256-BLiunzoUr7FcwfGlwoWXaK1D9NKytinWrV2+bZd+YK8=";
+
   patches = [
+    # Fails with:
+    # - Failed to write to log, can't make directories for new logfile: mkdir /var: permission denied
     ./log-path.patch
   ];
 
@@ -27,10 +30,31 @@ buildGoModule rec {
     "-X main.version=${version}"
   ];
 
-  # Fails with:
-  # - Failed to write to log, can't make directories for new logfile: mkdir /var: permission denied
-  doCheck = false;
+  #doCheck = false;
+  checkPhase = ''
+    runHook preCheck
 
+    # Failing tests:
+    #
+    # client/cmd:
+    # up_test.go:80: expected wireguard interface wt0 to be created
+    # "TestUp"
+    # "TestUp_Start"
+    #
+    # client/internal:
+    # === RUN   TestEngine_MultiplePeers
+    # level=error msg="failed creating interface wt3: [CreateTUN(\"wt3\") failed; /dev/net/tun does not exist]"
+    #
+    # iface:
+    # iface_test.go:186: CreateTUN("utun1003") failed; /dev/net/tun does not exist
+    # TestUp
+
+    for pkg in $(getGoDirs test | grep -Ev 'client\/cmd|client\/internal|iface'); do
+      buildGoDir test $checkFlags "$pkg"
+    done
+
+    runHook postCheck
+  '';
   passthru.tests.version = testVersion { package = wiretrustee; command = "client version"; };
 
   meta = with lib; {
