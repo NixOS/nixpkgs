a1aefcf9046f0087d3e0332c9042d17a4c8a5944
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-06 04:31:17 +0000

    bazel-kazel: 0.2.2 -> 0.2.3

--- pkgs/development/tools/bazel-kazel/default.nix
+++ pkgs/development/tools/bazel-kazel/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "bazel-kazel";
-  version = "0.2.2";
+  version = "0.2.3";
 
   src = fetchFromGitHub {
     owner = "kubernetes";
     repo = "repo-infra";
     rev = "v${version}";
-    sha256 = "sha256-EfK8uJQvZkB5V/SGOLRznAFGsgVGwFv6MWkLPWePYvM=";
+    sha256 = "sha256-px9HXtF9n60H7FBpNfYVZ/3MFkk4KJENg0xVWZbzf5M=";
   };
 
   vendorSha256 = "sha256-1+7Mx1Zh1WolqTpWNe560PRzRYaWVUVLvNvUOysaW5I=";
