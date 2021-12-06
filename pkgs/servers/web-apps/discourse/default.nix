1373cfd5b93065b2f3874342b3ca883ce6229490
talyz <kim.lindberger@gmail.com>
2021-12-06 14:21:39 +0100

    discourse: Don't patch the public path
    Instead of patching the path to /public in Discourse's sources, make
    the nginx configuration refer to the symlink in the discourse
    package which points to the real path.
    
    When there is a mismatch between the path nginx serves and the path
    Discourse thinks it serves, we can run into issues like files not
    being served - at least when sendfile requests from the ruby app are
    processed by nginx. The issue I ran into most recently is that backup
    downloads don't work.
    
    Since Discourse refers to the public directory relative to the Rails
    root in many places, it's much easier to just sync this path to the
    nginx configuration than trying to patch all occurrences in the
    sources. This should hopefully mean less potential for breakage in
    future Discourse releases, too.
    
    (cherry picked from commit 125bb7dac1fcf9829ff93a33ed19665a72872ecd)

--- pkgs/servers/web-apps/discourse/default.nix
+++ pkgs/servers/web-apps/discourse/default.nix
@@ -264,11 +264,6 @@ let
 
       # Make sure the notification email setting applies
       ./notification_email.patch
-
-      # Change the path to the public directory reported by Discourse
-      # to its real path instead of the symlink in the store, since
-      # the store path won't be matched by any nginx rules
-      ./public_dir_path.patch
     ];
 
     postPatch = ''
