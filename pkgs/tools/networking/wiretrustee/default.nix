9ea12c00d8206905768cac8d383653cbda5fc0a9
0x4A6F <0x4A6F@users.noreply.github.com>
2021-12-06 11:05:22 +0100

    wiretrustee: 0.3.0 -> 0.3.1

--- pkgs/tools/networking/wiretrustee/default.nix
+++ pkgs/tools/networking/wiretrustee/default.nix
@@ -1,24 +1,34 @@
 { lib
 , buildGoModule
 , fetchFromGitHub
+, wiretrustee
+, testVersion
 }:
 
 buildGoModule rec {
   pname = "wiretrustee";
-  version = "0.3.0";
+  version = "0.3.1";
 
   src = fetchFromGitHub {
     owner = "wiretrustee";
     repo = "wiretrustee";
     rev = "v${version}";
-    sha256 = "11crspy55kgsqnh64xa4dbmagyiy871q7klzassgpiqlcpbsfz88";
+    sha256 = "0wyi8n29q8gsza6cwwp8qqri8wfp1rlfhizw4y65r0dq3xc7g1wm";
   };
 
   vendorSha256 = "13yfh3ylm9pyqjrhwdkl8hk5rr7d2bgi6n2xcy1aw1b674fk5713";
 
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.version=${version}"
+  ];
+
   # Fails with:
   # - Failed to write to log, can't make directories for new logfile: mkdir /var: permission denied
   doCheck = false;
+
+  passthru.tests.version = testVersion { package = wiretrustee; command = "client version"; };
 
   meta = with lib; {
     description = "Connect your devices into a single secure private WireGuard-based mesh network";
