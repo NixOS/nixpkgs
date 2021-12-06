511caf3cc3f72fda74e7b39a08647d008a97a507
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-06 03:31:54 +0000

    evolution-data-server: 3.42.1 -> 3.42.2
    (cherry picked from commit 74d8cc01903dc3391fafa13be4148985b93c3491)

--- pkgs/desktops/gnome/core/evolution-data-server/default.nix
+++ pkgs/desktops/gnome/core/evolution-data-server/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "evolution-data-server";
-  version = "3.42.1";
+  version = "3.42.2";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/evolution-data-server/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "5uZ/KnfQ/z5tpQsD3F+iZucWYmvNou9EFE4xfjXy9Sg=";
+    sha256 = "HBTYbnoNjm5PGpYTfjGmGdK8+/yArR8OrDje4sAkerw=";
   };
 
   patches = [
