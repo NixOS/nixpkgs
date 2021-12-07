55af3549a32a4e95b05addb499d9cd7ccf6108a5
Silvan Mosberger <contact@infinisil.com>
2021-12-07 17:58:03 +0100

    scyther: remove since it only supports Python 2
    See https://github.com/cascremers/scyther/issues/20

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31592,8 +31592,6 @@ with pkgs;
 
   plm = callPackage ../applications/science/programming/plm { };
 
-  scyther = callPackage ../applications/science/programming/scyther { };
-
   ### SCIENCE/LOGIC
 
   abc-verifier = callPackage ../applications/science/logic/abc {};
