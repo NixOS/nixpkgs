1da57a8c496a3ea308efa89b7cf00463b6f6049d
Robert Schütz <nix@dotlambda.de>
2021-12-07 12:47:17 -0800

    authenticator: not broken anymore

--- pkgs/applications/misc/authenticator/default.nix
+++ pkgs/applications/misc/authenticator/default.nix
@@ -91,7 +91,6 @@ stdenv.mkDerivation rec {
   ];
 
   meta = with lib; {
-    broken = true; # https://gitlab.gnome.org/World/Authenticator/-/issues/271
     description = "Two-factor authentication code generator for GNOME";
     homepage = "https://gitlab.gnome.org/World/Authenticator";
     license = licenses.gpl3Plus;
