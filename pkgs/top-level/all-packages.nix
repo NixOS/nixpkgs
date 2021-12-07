1e4281bbfd34b0badf9e6fae45f8fb36a994fccc
sternenseemann <sternenseemann@systemli.org>
2021-12-07 09:33:51 +0100

    gitin: remove at 0.2.3
    gitin depends on a precise version of git2go. Not even the latest
    gitin/git2go version is compatible with an up to date libgit2. As the
    upstream project hasn't seen activity in some time and doesn't respond
    to the related issue (https://github.com/isacikgoz/gitin/issues/71),
    drop this package.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5767,8 +5767,6 @@ with pkgs;
 
   github-runner = callPackage ../development/tools/continuous-integration/github-runner { };
 
-  gitin = callPackage ../applications/version-management/git-and-tools/gitin { };
-
   gitinspector = callPackage ../applications/version-management/gitinspector { };
 
   gitkraken = callPackage ../applications/version-management/gitkraken { };
