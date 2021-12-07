d8edf418c11872e39a3e8df75b9f5fc4d7b59741
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2021-12-07 12:36:30 -0500

    python3Packages.mask-rcnn: fix for recent keras

--- pkgs/development/python-modules/mask-rcnn/default.nix
+++ pkgs/development/python-modules/mask-rcnn/default.nix
@@ -1,6 +1,7 @@
 { buildPythonPackage
 , cython
 , fetchFromGitHub
+, fetchpatch
 , h5py
 , imgaug
 , ipython
@@ -25,6 +26,22 @@ buildPythonPackage rec {
     rev = "3deaec5d902d16e1daf56b62d5971d428dc920bc";
     sha256 = "13s3q9yh2q9m9vyksd269mww3bni4q2w7q5l419q70ca075qp8zp";
   };
+
+  patches = [
+    # Fix for TF2:
+    # https://github.com/matterport/Mask_RCNN/issues/2734
+    (fetchpatch {
+      url = "https://github.com/BupyeongHealer/Mask_RCNN_tf_2.x/commit/7957839fe2b248f2f22c7e991ead12068ddc6cfc.diff";
+      excludes = [ "mrcnn/model.py" ];
+      sha256 = "sha256-70BGrx6X1uJDA2025f0YTlreT2uB3n35yIzuhf+ypVc=";
+    })
+  ];
+
+  # Fix for recent Keras
+  postPatch = ''
+    substituteInPlace mrcnn/model.py \
+      --replace "KE." "KL."
+  '';
 
   nativeBuildInputs = [ cython ];
 
