10f572f5c872d8965adcaeebf60eb4075ddfb927
Jonathan Ringer <jonringer117@gmail.com>
2021-12-07 00:56:14 -0800

    python3Packages.pint: 0.14 -> 0.18

--- pkgs/development/python-modules/pint/default.nix
+++ pkgs/development/python-modules/pint/default.nix
@@ -14,12 +14,12 @@
 
 buildPythonPackage rec {
   pname = "pint";
-  version = "0.14";
+  version = "0.18";
 
   src = fetchPypi {
     inherit version;
     pname = "Pint";
-    sha256 = "0wkzb7g20wzpqr3xaqpq96dlfv6irw202icsz81ys8npp7mm194s";
+    sha256 = "8c4bce884c269051feb7abc69dbfd18403c0c764abc83da132e8a7222f8ba801";
   };
 
   disabled = pythonOlder "3.6";
