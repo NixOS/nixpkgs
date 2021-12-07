c0a65548471077d382ff66f8e5a56de69c94e25f
Sumner Evans <me@sumnerevans.com>
2021-12-07 08:28:55 -0700

    matrix-synapse: 1.48.0 -> 1.49.0

--- pkgs/servers/matrix-synapse/default.nix
+++ pkgs/servers/matrix-synapse/default.nix
@@ -11,17 +11,12 @@ in
 with python3.pkgs;
 buildPythonApplication rec {
   pname = "matrix-synapse";
-  version = "1.48.0";
+  version = "1.49.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-G09VbfC9mZ0+shLHRNutR91URewvLW4l4lQaVrsZYaQ=";
+    sha256 = "sha256-vvLcRy2qt/qOgKkQpj2eHP4691OX0AW58dD3EecluZM=";
   };
-
-  patches = [
-    ./0001-setup-add-homeserver-as-console-script.patch
-    ./0002-Expose-generic-worker-as-binary-under-NixOS.patch
-  ];
 
   buildInputs = [ openssl ];
 
