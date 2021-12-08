b8a644565e2fdf8d3392e5b18f554c7ad11914cd
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-07 23:04:01 -0400

    python3.pkgs.alectryon: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/alectryon/default.nix
+++ pkgs/development/python-modules/alectryon/default.nix
@@ -1,13 +1,15 @@
-{ lib, buildPythonPackage, fetchPypi
+{ lib, buildPythonPackage, fetchFromGitHub
 , pygments, dominate, beautifulsoup4, docutils, sphinx }:
 
 buildPythonPackage rec {
   pname = "alectryon";
   version = "1.4.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "00cxzfifvgcf3d3s8lsj1yxcwyf3a1964p86fj7b42q8pa0b4r3i";
+  src = fetchFromGitHub {
+     owner = "cpitclaudel";
+     repo = "alectryon";
+     rev = "v1.4.0";
+     sha256 = "1vpsddfjxpvylq70r7ip6c0iaqn10jdkxmwd93r1zzkxg30hzsf3";
   };
 
   propagatedBuildInputs = [
