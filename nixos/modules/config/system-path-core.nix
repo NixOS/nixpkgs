c6ea188fb9ebaa5622f8976616549787b03c246c
Robert Hensing <robert@roberthensing.nl>
2021-12-08 10:29:10 +0000

    nixos/system-path-core.nix: Extract module

---
+++ nixos/modules/config/system-path-core.nix
@@ -0,0 +1,87 @@
+{ config, lib, pkgs, ... }:
+let
+  inherit (lib)
+    mkOption
+    types
+    literalExpression
+    ;
+in
+{
+  options = {
+
+    environment = {
+
+      systemPackages = mkOption {
+        type = types.listOf types.package;
+        default = [ ];
+        example = literalExpression "[ pkgs.firefox pkgs.htop ]";
+        description = ''
+          The set of packages that appear in
+          /run/current-system/sw.  These packages are
+          automatically available to all users, and are
+          automatically updated every time you rebuild the system
+          configuration.  (The latter is the main difference with
+          installing them in the default profile,
+          <filename>/nix/var/nix/profiles/default</filename>.
+        '';
+      };
+
+      pathsToLink = mkOption {
+        type = types.listOf types.str;
+        # Note: We need `/lib' to be among `pathsToLink' for NSS modules
+        # to work.
+        default = [ ];
+        example = [ "/" ];
+        description = "List of directories to be symlinked in <filename>/run/current-system/sw</filename>.";
+      };
+
+      extraOutputsToInstall = mkOption {
+        type = types.listOf types.str;
+        default = [ ];
+        example = [ "doc" "info" "devdoc" ];
+        description = "List of additional package outputs to be symlinked into <filename>/run/current-system/sw</filename>.";
+      };
+
+      extraSetup = mkOption {
+        type = types.lines;
+        default = "";
+        description = "Shell fragments to be run after the system environment has been created. This should only be used for things that need to modify the internals of the environment, e.g. generating MIME caches. The environment being built can be accessed at $out.";
+      };
+
+    };
+
+    system = {
+
+      path = mkOption {
+        internal = true;
+        description = ''
+          The packages you want in the boot environment.
+        '';
+      };
+
+    };
+
+  };
+  config = {
+    environment.pathsToLink = [
+      "/bin"
+    ];
+
+    system.path = pkgs.buildEnv {
+      name = "system-path";
+      paths = config.environment.systemPackages;
+      inherit (config.environment) pathsToLink extraOutputsToInstall;
+      ignoreCollisions = true;
+      # !!! Hacky, should modularise.
+      # outputs TODO: note that the tools will often not be linked by default
+      postBuild =
+        ''
+          # Remove wrapped binaries, they shouldn't be accessible via PATH.
+          find $out/bin -maxdepth 1 -name ".*-wrapped" -type l -delete
+
+          ${config.environment.extraSetup}
+        '';
+    };
+
+  };
+}
--- nixos/modules/config/system-path.nix
+++ nixos/modules/config/system-path.nix
@@ -51,24 +51,11 @@ let
 in
 
 {
+  imports = [ ./system-path-core.nix ];
+
   options = {
 
     environment = {
-
-      systemPackages = mkOption {
-        type = types.listOf types.package;
-        default = [];
-        example = literalExpression "[ pkgs.firefox pkgs.thunderbird ]";
-        description = ''
-          The set of packages that appear in
-          /run/current-system/sw.  These packages are
-          automatically available to all users, and are
-          automatically updated every time you rebuild the system
-          configuration.  (The latter is the main difference with
-          installing them in the default profile,
-          <filename>/nix/var/nix/profiles/default</filename>.
-        '';
-      };
 
       defaultPackages = mkOption {
         type = types.listOf types.package;
@@ -93,39 +80,6 @@ in
         '';
       };
 
-      pathsToLink = mkOption {
-        type = types.listOf types.str;
-        # Note: We need `/lib' to be among `pathsToLink' for NSS modules
-        # to work.
-        default = [];
-        example = ["/"];
-        description = "List of directories to be symlinked in <filename>/run/current-system/sw</filename>.";
-      };
-
-      extraOutputsToInstall = mkOption {
-        type = types.listOf types.str;
-        default = [ ];
-        example = [ "doc" "info" "devdoc" ];
-        description = "List of additional package outputs to be symlinked into <filename>/run/current-system/sw</filename>.";
-      };
-
-      extraSetup = mkOption {
-        type = types.lines;
-        default = "";
-        description = "Shell fragments to be run after the system environment has been created. This should only be used for things that need to modify the internals of the environment, e.g. generating MIME caches. The environment being built can be accessed at $out.";
-      };
-
-    };
-
-    system = {
-
-      path = mkOption {
-        internal = true;
-        description = ''
-          The packages you want in the boot environment.
-        '';
-      };
-
     };
 
   };
@@ -135,8 +89,7 @@ in
     environment.systemPackages = requiredPackages ++ config.environment.defaultPackages;
 
     environment.pathsToLink =
-      [ "/bin"
-        "/etc/xdg"
+      [ "/etc/xdg"
         "/etc/gtk-2.0"
         "/etc/gtk-3.0"
         "/lib" # FIXME: remove and update debug-info.nix
@@ -155,25 +108,11 @@ in
         "/share/thumbnailers"
       ];
 
-    system.path = pkgs.buildEnv {
-      name = "system-path";
-      paths = config.environment.systemPackages;
-      inherit (config.environment) pathsToLink extraOutputsToInstall;
-      ignoreCollisions = true;
-      # !!! Hacky, should modularise.
-      # outputs TODO: note that the tools will often not be linked by default
-      postBuild =
-        ''
-          # Remove wrapped binaries, they shouldn't be accessible via PATH.
-          find $out/bin -maxdepth 1 -name ".*-wrapped" -type l -delete
-
-          if [ -x $out/bin/glib-compile-schemas -a -w $out/share/glib-2.0/schemas ]; then
-              $out/bin/glib-compile-schemas $out/share/glib-2.0/schemas
-          fi
-
-          ${config.environment.extraSetup}
-        '';
-    };
+    environment.extraSetup = ''
+      if [ -x $out/bin/glib-compile-schemas -a -w $out/share/glib-2.0/schemas ]; then
+          $out/bin/glib-compile-schemas $out/share/glib-2.0/schemas
+      fi
+    '';
 
   };
 }
