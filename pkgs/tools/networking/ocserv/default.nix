bf25e198eff064c71009c61f2a95853ce9be2274
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-08 23:13:22 +0000

    ocserv: 1.1.2 -> 1.1.4

--- pkgs/tools/networking/ocserv/default.nix
+++ pkgs/tools/networking/ocserv/default.nix
@@ -5,13 +5,13 @@
 
 stdenv.mkDerivation rec {
   pname = "ocserv";
-  version = "1.1.2";
+  version = "1.1.4";
 
   src = fetchFromGitLab {
     owner = "openconnect";
     repo = "ocserv";
     rev = version;
-    sha256 = "gXolG4zTnJpgI32SuudhvlP9snI0k4Oa1mqE7eGbdE0=";
+    sha256 = "sha256-0/BDB7vuLRnp/xhsRTdd0z3FrFZcXwohKCDvQNkbVaI=";
   };
 
   nativeBuildInputs = [ autoreconfHook pkg-config ];
