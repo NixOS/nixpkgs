c76e1c7d14441ce29c20b7b9e4d743b77f70ab9c
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-08 15:57:22 -0400

    python3.pkgs.colorclass: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/colorclass/default.nix
+++ pkgs/development/python-modules/colorclass/default.nix
@@ -1,12 +1,14 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib, buildPythonPackage, fetchFromGitHub }:
 
 buildPythonPackage rec {
   pname = "colorclass";
   version = "2.2.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "b05c2a348dfc1aff2d502527d78a5b7b7e2f85da94a96c5081210d8e9ee8e18b";
+  src = fetchFromGitHub {
+     owner = "Robpol86";
+     repo = "colorclass";
+     rev = "v2.2.0";
+     sha256 = "1c9r7v888wavaq8mhzihg42rlfynwzvgw95r5h7sjkqz2zd1pf4b";
   };
 
   # No tests in archive
