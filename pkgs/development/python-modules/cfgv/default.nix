bbe35133af4d8faa5ea339b95619ff0540ec4912
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-08 14:52:12 -0400

    python3.pkgs.cfgv: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/cfgv/default.nix
+++ pkgs/development/python-modules/cfgv/default.nix
@@ -1,13 +1,15 @@
-{ lib, buildPythonPackage, fetchPypi, isPy27, six }:
+{ lib, buildPythonPackage, fetchFromGitHub, isPy27, six }:
 
 buildPythonPackage rec {
   pname = "cfgv";
   version = "3.3.1";
   disabled = isPy27;
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "f5a830efb9ce7a445376bb66ec94c638a9787422f96264c98edc6bdeed8ab736";
+  src = fetchFromGitHub {
+     owner = "asottile";
+     repo = "cfgv";
+     rev = "v3.3.1";
+     sha256 = "1pci97cmn3v45sfch9s3lshidrl0309ls9byidic0l8drkwnkwcj";
   };
 
   propagatedBuildInputs = [ six ];
