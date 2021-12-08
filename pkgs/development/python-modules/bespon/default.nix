7834e8b620e3dd9b4b8ead23080a03255d0b928d
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-08 10:26:57 -0400

    python3.pkgs.bespon: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/bespon/default.nix
+++ pkgs/development/python-modules/bespon/default.nix
@@ -1,15 +1,17 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 }:
 
 buildPythonPackage rec {
   version = "0.6.0";
   pname = "BespON";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "2f2bda67fea8ee95c8aa7e885835ab88bdbfa392a94077ce1c9d29017420ce7a";
+  src = fetchFromGitHub {
+     owner = "gpoore";
+     repo = "bespon_py";
+     rev = "v0.6.0";
+     sha256 = "0x1ifklhh88fa6i693zgpb63646jxsyhj4j64lrvarckrb31wk23";
   };
 
   propagatedBuildInputs = [ ];
