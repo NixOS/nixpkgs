3f15fdc21ac44c172a1d0e2b7e8ef2019f825a1b
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-08 01:21:01 -0400

    python3.pkgs.asn1crypto: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/asn1crypto/default.nix
+++ pkgs/development/python-modules/asn1crypto/default.nix
@@ -1,15 +1,17 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 }:
 
 buildPythonPackage rec {
   pname = "asn1crypto";
   version = "1.4.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "f4f6e119474e58e04a2b1af817eb585b4fd72bdd89b998624712b5c99be7641c";
+  src = fetchFromGitHub {
+     owner = "wbond";
+     repo = "asn1crypto";
+     rev = "1.4.0";
+     sha256 = "19abibn6jw20mzi1ln4n9jjvpdka8ygm4m439hplyrdfqbvgm01r";
   };
 
   # No tests included
