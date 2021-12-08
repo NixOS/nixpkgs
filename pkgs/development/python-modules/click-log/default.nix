c7142cbe3ba1c0afb860b61700819032e2093fca
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-08 15:46:26 -0400

    python3.pkgs.click-log: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/click-log/default.nix
+++ pkgs/development/python-modules/click-log/default.nix
@@ -1,12 +1,14 @@
-{ lib, buildPythonPackage, fetchPypi, click }:
+{ lib, buildPythonPackage, fetchFromGitHub, click }:
 
 buildPythonPackage rec {
   pname = "click-log";
   version = "0.3.2";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "16fd1ca3fc6b16c98cea63acf1ab474ea8e676849dc669d86afafb0ed7003124";
+  src = fetchFromGitHub {
+     owner = "click-contrib";
+     repo = "click-log";
+     rev = "0.3.2";
+     sha256 = "01zpjyqnyzrx6xi7cii2180bqdcg2a6b1sdbjijri8jv755r7ray";
   };
 
   propagatedBuildInputs = [ click ];
