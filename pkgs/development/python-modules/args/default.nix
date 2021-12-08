77e4853de4911c03464c80030337e0d2c6b78106
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-08 00:37:08 -0400

    python3.pkgs.args: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/args/default.nix
+++ pkgs/development/python-modules/args/default.nix
@@ -1,12 +1,14 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib, buildPythonPackage, fetchFromGitHub }:
 
 buildPythonPackage rec {
   pname = "args";
   version = "0.1.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "a785b8d837625e9b61c39108532d95b85274acd679693b71ebb5156848fcf814";
+  src = fetchFromGitHub {
+     owner = "kennethreitz";
+     repo = "args";
+     rev = "v0.1.0";
+     sha256 = "1n8ayb1gc6zp32qicj3y5b20sniqgb24i0gq33dp6wmv9yddm805";
   };
 
   meta = with lib; {
