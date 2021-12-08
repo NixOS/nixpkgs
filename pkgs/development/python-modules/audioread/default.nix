935d6ade4feee0d245adea70e9fd8f01a286dc5a
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-08 03:59:16 -0400

    python3.pkgs.audioread: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/audioread/default.nix
+++ pkgs/development/python-modules/audioread/default.nix
@@ -1,15 +1,17 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 }:
 
 buildPythonPackage rec {
   pname = "audioread";
   version = "2.1.9";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "a3480e42056c8e80a8192a54f6729a280ef66d27782ee11cbd63e9d4d1523089";
+  src = fetchFromGitHub {
+     owner = "sampsyo";
+     repo = "audioread";
+     rev = "v2.1.9";
+     sha256 = "1s1x2xcym69mbcgp6sf5a1dk1kqfn91hd8c5d3sv4r9j0d1skv4p";
   };
 
   # No tests, need to disable or py3k breaks
