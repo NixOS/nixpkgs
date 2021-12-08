f323d9574b27596322635d593ad7119a5e2fdb81
Izorkin <izorkin@elven.pw>
2021-12-08 13:25:28 +0300

    modsecurity-crs: init at 3.3.2
    (cherry picked from commit 8b37c4d5c4dc0bbe3126a098fc345d9518e8d1bc)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7653,6 +7653,8 @@ with pkgs;
 
   modsecurity_standalone = callPackage ../tools/security/modsecurity { };
 
+  modsecurity-crs = callPackage ../tools/security/modsecurity-crs { };
+
   molly-guard = callPackage ../os-specific/linux/molly-guard { };
 
   molotov = callPackage ../applications/video/molotov {};
