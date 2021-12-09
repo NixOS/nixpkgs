a52b1a30d87be2592760b9bd260a4fb1788c79b8
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-09 09:13:39 +0000

    mpg123: 1.28.2 -> 1.29.2

--- pkgs/applications/audio/mpg123/default.nix
+++ pkgs/applications/audio/mpg123/default.nix
@@ -18,11 +18,11 @@
 
 stdenv.mkDerivation rec {
   pname = "mpg123";
-  version = "1.28.2";
+  version = "1.29.2";
 
   src = fetchurl {
     url = "mirror://sourceforge/${pname}/${pname}-${version}.tar.bz2";
-    sha256 = "006v44nz4nkpgvxz1k2vbbrfpa2m47hyydscs0wf3iysiyvd9vvy";
+    sha256 = "sha256-kHEhTr38G27QwKhdUwAQu7jrwETP5a5ZMOg/fmt5N+Y=";
   };
 
   outputs = [ "out" ] ++ lib.optionals withConplay [ "conplay" ];
