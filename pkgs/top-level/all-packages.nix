a288cf20cb7c614a56964d4d8fe13def5055b3ab
Doron Behar <doron.behar@gmail.com>
2021-12-09 23:33:06 +0200

    asciidoctor: Remove some extensions and cleanup
    Since we have `asciidoctor-with-extensions` now, `asciidoctor` can
    include less extensions to reduce closure space.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3805,10 +3805,7 @@ with pkgs;
     enableExtraPlugins = true;
   });
 
-  asciidoctor = callPackage ../tools/typesetting/asciidoctor {
-    # epubcheck pulls in Java, which is problematic on some platforms
-    epubcheck = null;
-  };
+  asciidoctor = callPackage ../tools/typesetting/asciidoctor { };
 
   asciidoctor-with-extensions = callPackage ../tools/typesetting/asciidoctor-with-extensions { };
 
