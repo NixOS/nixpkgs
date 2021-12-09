0376dd24cf2a9bd2a012d8fa13128eb413b925f3
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-09 23:37:48 +0000

    libwpe: 1.10.1 -> 1.12.0
    (cherry picked from commit c6201cd8e42a7eb433d4dc6ca10b5300d2d78c09)

--- pkgs/development/libraries/libwpe/default.nix
+++ pkgs/development/libraries/libwpe/default.nix
@@ -10,11 +10,11 @@
 
 stdenv.mkDerivation rec {
   pname = "libwpe";
-  version = "1.10.1";
+  version = "1.12.0";
 
   src = fetchurl {
     url = "https://wpewebkit.org/releases/${pname}-${version}.tar.xz";
-    sha256 = "sha256-oyez/88jVtvoFIwdtLvcU1nX5xY9F39nVRqDk7FErfA=";
+    sha256 = "sha256-6O7KIoprTDYpTPtj99O6mtpHpDCQSlqXOzyZyWpEwYw=";
   };
 
   nativeBuildInputs = [
