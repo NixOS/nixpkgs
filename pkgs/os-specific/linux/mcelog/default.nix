ecca16a5b4191c2e0001803c0d3d0ae0d9168f40
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-09 16:28:50 +0000

    mcelog: 178 -> 179

--- pkgs/os-specific/linux/mcelog/default.nix
+++ pkgs/os-specific/linux/mcelog/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "mcelog";
-  version = "178";
+  version = "179";
 
   src = fetchFromGitHub {
     owner  = "andikleen";
     repo   = "mcelog";
     rev    = "v${version}";
-    sha256 = "sha256-++R/R3oRw0O0MoDnASdVr9UIiI0yS4UcIO8nV62PLzE=";
+    sha256 = "sha256-zwBVKXq7m735g+nKlYRrseXdYGOIQs0TqOR6iS6Q6Ts=";
   };
 
   postPatch = ''
