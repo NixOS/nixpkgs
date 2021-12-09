2f3e9f4dde8d95254e9e2cfe08857dcde8356384
Robert Hensing <robert@roberthensing.nl>
2021-12-09 11:54:21 +0000

    nixos/environment-variables.nix: Extract module

--- nixos/modules/config/shells-environment.nix
+++ nixos/modules/config/shells-environment.nix
@@ -30,21 +30,9 @@ in
 
 {
 
-  options = {
+  imports = [ ./environment-variables.nix ];
 
-    environment.variables = mkOption {
-      default = {};
-      example = { EDITOR = "nvim"; VISUAL = "nvim"; };
-      description = ''
-        A set of environment variables used in the global environment.
-        These variables will be set on shell initialisation (e.g. in /etc/profile).
-        The value of each variable can be either a string or a list of
-        strings.  The latter is concatenated, interspersed with colon
-        characters.
-      '';
-      type = with types; attrsOf (either str (listOf str));
-      apply = mapAttrs (n: v: if isList v then concatStringsSep ":" v else v);
-    };
+  options = {
 
     environment.profiles = mkOption {
       default = [];
