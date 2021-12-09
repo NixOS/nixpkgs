d4db04ce91d09ac9141fba2af3e07be8d42f07a4
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-12-09 21:37:56 +0100

    python3Packages.freezegun: Fix python3.10 compat
    Fixes decorate_class for Python 3.10, where the staticmethod decorator
    is callable.

--- pkgs/development/python-modules/freezegun/default.nix
+++ pkgs/development/python-modules/freezegun/default.nix
@@ -2,6 +2,7 @@
 , buildPythonPackage
 , pythonOlder
 , fetchPypi
+, fetchpatch
 , python-dateutil
 , pytestCheckHook
 }:
@@ -15,6 +16,14 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "177f9dd59861d871e27a484c3332f35a6e3f5d14626f2bf91be37891f18927f3";
   };
+
+  patches = [
+    (fetchpatch {
+      # https://github.com/spulec/freezegun/issues/396
+      url = "https://gitweb.gentoo.org/repo/gentoo.git/plain/dev-python/freezegun/files/freezegun-1.1.0-py310.patch?id=d030418412529d09801fc2fda2201e525c25ad2d";
+      sha256 = "sha256:1ybxqbsn3cz2w25x9wwxi0kzfn9q5ldagb1crbhxy25wyw8s1801";
+    })
+  ];
 
   propagatedBuildInputs = [ python-dateutil ];
   checkInputs = [ pytestCheckHook ];
