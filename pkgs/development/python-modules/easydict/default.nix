37200469f9ff68ff728655fce03daa0d56952c07
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-09 06:03:14 -0400

    python3.pkgs.easydict: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/easydict/default.nix
+++ pkgs/development/python-modules/easydict/default.nix
@@ -1,12 +1,14 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib, buildPythonPackage, fetchFromGitHub }:
 
 buildPythonPackage rec {
   pname = "easydict";
   version = "1.9";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "3f3f0dab07c299f0f4df032db1f388d985bb57fa4c5be30acd25c5f9a516883b";
+  src = fetchFromGitHub {
+     owner = "makinacorpus";
+     repo = "easydict";
+     rev = "1.9";
+     sha256 = "0nv0asrdixi16qmafvasmxprgja9g07gxk5fz87lw13n9bfjb9z7";
   };
 
   docheck = false; # No tests in archive
