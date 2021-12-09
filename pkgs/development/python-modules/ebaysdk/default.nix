e9c0fb36d5cb6559997ee13df461d1426822baed
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-09 06:04:00 -0400

    python3.pkgs.ebaysdk: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/ebaysdk/default.nix
+++ pkgs/development/python-modules/ebaysdk/default.nix
@@ -1,6 +1,6 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 , lxml
 , requests
 }:
@@ -9,9 +9,11 @@ buildPythonPackage rec {
   pname = "ebaysdk";
   version = "2.2.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-Lrh11wa0gfWcqN0wdFON9+UZaBT5zhLQ74RpA0Opx/M=";
+  src = fetchFromGitHub {
+     owner = "timotheus";
+     repo = "ebaysdk-python";
+     rev = "v2.2.0";
+     sha256 = "1ym2qa0q89cd5agw07kkbzw0i5jyj1nm2zm58g94vjsg7acsbdkq";
   };
 
   propagatedBuildInputs = [
