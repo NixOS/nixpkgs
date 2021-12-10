4e51a80a4688a771c73f3f7b4134e846c29c3dd2
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-10 03:45:02 +0000

    libavif: 0.9.2 -> 0.9.3

--- pkgs/development/libraries/libavif/default.nix
+++ pkgs/development/libraries/libavif/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libavif";
-  version = "0.9.2";
+  version = "0.9.3";
 
   src = fetchFromGitHub {
     owner = "AOMediaCodec";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-0/5JLynYzr7ZewBbrNoxr26UkVzpSP9RqlmH2ql8tfs=";
+    sha256 = "sha256-ME/mkaHhFeHajTbc7zhg9vtf/8XgkgSRu9I/mlQXnds=";
   };
 
   # reco: encode libaom slowest but best, decode dav1d fastest
