fa35b83c7928823261215c4163006d6755c83105
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-10 07:26:26 -0400

    python3.pkgs.feedgen: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/feedgen/default.nix
+++ pkgs/development/python-modules/feedgen/default.nix
@@ -1,12 +1,14 @@
-{ lib, buildPythonPackage, fetchPypi, python-dateutil, lxml }:
+{ lib, buildPythonPackage, fetchFromGitHub, python-dateutil, lxml }:
 
 buildPythonPackage rec {
   pname = "feedgen";
   version = "0.9.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "0jl0b87l7v6c0f1nx6k81skjhdj5i11kmchdjls00mynpvdip0cf";
+  src = fetchFromGitHub {
+     owner = "lkiesow";
+     repo = "python-feedgen";
+     rev = "v0.9.0";
+     sha256 = "1plfhxj44avkd3qx4gsv678dnckj1yr0r5ci5ivhsin09qymz8zf";
   };
 
   propagatedBuildInputs = [ python-dateutil lxml ];
