18dd2a807a5cb8d8a1bc5a4ade641f54ec280cad
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-10 11:50:07 -0400

    python3.pkgs.glob2: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/glob2/default.nix
+++ pkgs/development/python-modules/glob2/default.nix
@@ -1,6 +1,6 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 , python
 }:
 
@@ -8,9 +8,11 @@ buildPythonPackage rec {
   pname = "glob2";
   version = "0.7";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "135bj8gm6vn45vv0phrvhyir36kfm17y7kmasxinv8lagk8dphw5";
+  src = fetchFromGitHub {
+     owner = "miracle2k";
+     repo = "python-glob2";
+     rev = "v0.7";
+     sha256 = "160nh2ay9lw2hi0rixpzb2k87r6ql56k0j2cm87lqz8xc8zbw919";
   };
 
   checkPhase = ''
