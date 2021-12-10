228e1ba47e4a9a58e3bdd95c9041f5730b0bfeea
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-10 08:01:27 -0400

    python3.pkgs.flask-swagger-ui: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/flask-swagger-ui/default.nix
+++ pkgs/development/python-modules/flask-swagger-ui/default.nix
@@ -1,12 +1,14 @@
-{ lib, buildPythonPackage, fetchPypi, flask }:
+{ lib, buildPythonPackage, fetchFromGitHub, flask }:
 
 buildPythonPackage rec {
   pname = "flask-swagger-ui";
   version = "3.36.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "f329752a65b2940ada8eeb57bce613f7c0a12856a9c31063bb9e33798554c9ed";
+  src = fetchFromGitHub {
+     owner = "sveint";
+     repo = "flask-swagger-ui";
+     rev = "3.36.0";
+     sha256 = "0mg3skydq15gv5gv3i2f6131fijdvq9607is01fx26dczs57nii2";
   };
 
   doCheck = false;  # there are no tests
