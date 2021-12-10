867b8e2188cbe46cee7385b11750dde16f805570
Joel <joel@joel.tokyo>
2021-12-10 21:45:19 +1000

    minecraft-server: package major versions

--- pkgs/games/minecraft-server/default.nix
+++ pkgs/games/minecraft-server/default.nix
@@ -1,40 +1,17 @@
-{ lib, stdenv, fetchurl, nixosTests, jre_headless }:
-stdenv.mkDerivation {
-  pname = "minecraft-server";
-  version = "1.18.1";
+{ callPackage, lib }:
+# if you add more versions make sure to add to all-packages.nix
+let
+  versions = lib.importJSON ./versions.json;
+  latestVersion = lib.last (builtins.sort lib.versionOlder (builtins.attrNames versions));
+  escapeVersion = builtins.replaceStrings [ "." ] [ "_" ];
 
-  src = fetchurl {
-    url = "https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar";
-    # sha1 because that comes from mojang via api
-    sha1 = "125e5adf40c659fd3bce3e66e67a16bb49ecc1b9";
-  };
-
-  preferLocalBuild = true;
-
-  installPhase = ''
-    mkdir -p $out/bin $out/lib/minecraft
-    cp -v $src $out/lib/minecraft/server.jar
-
-    cat > $out/bin/minecraft-server << EOF
-    #!/bin/sh
-    exec ${jre_headless}/bin/java \$@ -jar $out/lib/minecraft/server.jar nogui
-    EOF
-
-    chmod +x $out/bin/minecraft-server
-  '';
-
-  dontUnpack = true;
-
-  passthru = {
-    tests = { inherit (nixosTests) minecraft-server; };
-    updateScript = ./update.sh;
-  };
-
-  meta = with lib; {
-    description = "Minecraft Server";
-    homepage = "https://minecraft.net";
-    license = licenses.unfreeRedistributable;
-    platforms = platforms.unix;
-    maintainers = with maintainers; [ thoughtpolice tomberek costrouc ];
-  };
+  packages = lib.mapAttrs'
+    (version: value: {
+      name = "minecraft-server_${escapeVersion version}";
+      value = callPackage ./derivation.nix { inherit (value) version url sha1; };
+    })
+    versions;
+in
+packages // {
+  minecraft-server = builtins.getAttr "minecraft-server_${escapeVersion latestVersion}" packages;
 }
