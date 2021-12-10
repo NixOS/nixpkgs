5030bcc5637e68b62ffb6cac098861c8f8193d7c
Fabian Affolter <mail@fabian-affolter.ch>
2021-12-10 11:08:54 +0100

    python3Packages.ms-active-directory: init at 1.12.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4994,6 +4994,8 @@ in {
 
   mrkd = callPackage ../development/python-modules/mrkd { };
 
+  ms-active-directory = callPackage ../development/python-modules/ms-active-directory { };
+
   ms-cv = callPackage ../development/python-modules/ms-cv { };
 
   msal = callPackage ../development/python-modules/msal { };
