81f9463b9b7f2118879728250d92bc51528d2e80
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-10 11:31:41 -0400

    python3.pkgs.ghp-import: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/ghp-import/default.nix
+++ pkgs/development/python-modules/ghp-import/default.nix
@@ -1,6 +1,6 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 , python-dateutil
 }:
 
@@ -8,9 +8,11 @@ buildPythonPackage rec {
   pname = "ghp-import";
   version = "2.0.2";
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-lHs3cfEb6FDIUsZLVhxgD93feUurNjBghUwe560F4HE=";
+  src = fetchFromGitHub {
+     owner = "c-w";
+     repo = "ghp-import";
+     rev = "2.0.2";
+     sha256 = "0i4lxsgqri1y8sw4k44bkwbzmdmk4vpmdi882mw148j8gk4i7vvj";
   };
 
   propagatedBuildInputs = [ python-dateutil ];
