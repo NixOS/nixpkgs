24fd3a67250010383d298390a79b3e25d1308958
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-10 07:31:53 -0400

    python3.pkgs.flake8-blind-except: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/flake8-blind-except/default.nix
+++ pkgs/development/python-modules/flake8-blind-except/default.nix
@@ -1,11 +1,13 @@
-{ lib, fetchPypi, buildPythonPackage }:
+{ lib, fetchFromGitHub, buildPythonPackage }:
 
 buildPythonPackage rec {
   pname = "flake8-blind-except";
   version = "0.2.0";
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "02a860a1a19cb602c006a3fe0778035b0d14d3f57929b4b798bc7d6684f204e5";
+  src = fetchFromGitHub {
+     owner = "elijahandrews";
+     repo = "flake8-blind-except";
+     rev = "v0.2.0";
+     sha256 = "1qgfg77x6xgg0db416drak1yw15lpfsg9mfzj7n0qbmh6qbvdw4i";
   };
   meta = {
     homepage = "https://github.com/elijahandrews/flake8-blind-except";
