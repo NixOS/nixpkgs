8fe2589f6bc93328c00f56ce0294c9dd13d769bc
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-10 08:51:06 +0000

    libsForQt5.kquickimageedit: 0.1.3 -> 0.2.0

--- pkgs/development/libraries/kquickimageedit/default.nix
+++ pkgs/development/libraries/kquickimageedit/default.nix
@@ -5,14 +5,14 @@
 
 mkDerivation rec {
   pname = "kquickimageeditor";
-  version = "0.1.3";
+  version = "0.2.0";
 
   src = fetchFromGitLab {
     domain = "invent.kde.org";
     owner = "libraries";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-p2uOssS5MQSkmppNKOjTxp866Qx1rIB6ZPhcmVvfBxs=";
+    sha256 = "sha256-g7+BAWjpQBJPbrwnIwSudjBFtwaj4JKemV+BLfPzl4I=";
   };
 
   nativeBuildInputs = [ extra-cmake-modules ];
