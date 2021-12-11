51508817f8231e589a9edb57116145112ef14897
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-11 03:32:14 +0000

    flyway: 7.13.0 -> 8.2.0

--- pkgs/development/tools/flyway/default.nix
+++ pkgs/development/tools/flyway/default.nix
@@ -1,10 +1,10 @@
 { lib, stdenv, fetchurl, jre_headless, makeWrapper }:
 stdenv.mkDerivation rec{
   pname = "flyway";
-  version = "7.13.0";
+  version = "8.2.0";
   src = fetchurl {
     url = "mirror://maven/org/flywaydb/flyway-commandline/${version}/flyway-commandline-${version}.tar.gz";
-    sha256 = "sha256-rZUVxswJdCFKwuXlzko+t+ZO1plRgH2VcZFJ5kkiM2s=";
+    sha256 = "sha256-YIujiaBQB5fJBzluWQRYgjcjrUUpYVEpZfTX9YFRZq4=";
   };
   nativeBuildInputs = [ makeWrapper ];
   dontBuild = true;
