0834f91d8a2a652019d91040054f6ff29ff1650f
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-11 06:13:02 +0000

    evemu: 2.6.0 -> 2.7.0

--- pkgs/tools/system/evemu/default.nix
+++ pkgs/tools/system/evemu/default.nix
@@ -4,14 +4,14 @@
 
 stdenv.mkDerivation rec {
   pname = "evemu";
-  version = "2.6.0";
+  version = "2.7.0";
 
   # We could have downloaded a release tarball from cgit, but it changes hash
   # each time it is downloaded :/
   src = fetchgit {
     url = "git://git.freedesktop.org/git/evemu";
     rev = "refs/tags/v${version}";
-    sha256 = "1m38fxwy2s82vb2qm9aqxinws12akmqqq7q66is931lc3awqkbah";
+    sha256 = "sha256-SQDaARuqBMBVlUz+Nw6mjdxaZfVOukmzTlIqy8U2rus=";
   };
 
   nativeBuildInputs = [ pkg-config autoreconfHook python3Packages.python ];
