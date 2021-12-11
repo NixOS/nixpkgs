3e34a00f6c57033aeeb0e18f17e7e8742c4a06f3
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-10 22:05:05 -0400

    python3.pkgs.hieroglyph: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/hieroglyph/default.nix
+++ pkgs/development/python-modules/hieroglyph/default.nix
@@ -1,13 +1,15 @@
-{ lib, fetchPypi, buildPythonPackage, isPy27, sphinx }:
+{ lib, fetchFromGitHub, buildPythonPackage, isPy27, sphinx }:
 
 buildPythonPackage rec {
   pname = "hieroglyph";
   version = "2.1.0";
   disabled = isPy27; # python2 compatible sphinx is too low
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "b4b5db13a9d387438e610c2ca1d81386ccd206944d9a9dd273f21874486cddaf";
+  src = fetchFromGitHub {
+     owner = "nyergler";
+     repo = "hieroglyph";
+     rev = "hieroglyph-2.1.0";
+     sha256 = "1q84q8dz1p291y9yfh3md2418qqa8z7a1kpr8aink0qbblf5rgly";
   };
 
   propagatedBuildInputs = [ sphinx ];
