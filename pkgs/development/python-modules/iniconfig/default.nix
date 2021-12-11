cf8f630c70cf60e85e026ec26bdd468b251d2c76
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-11 00:46:36 -0400

    python3.pkgs.iniconfig: fetch source from git, not PyPI
    Uploads to PyPI often do not include tests or documentation.

--- pkgs/development/python-modules/iniconfig/default.nix
+++ pkgs/development/python-modules/iniconfig/default.nix
@@ -1,12 +1,14 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib, buildPythonPackage, fetchFromGitHub }:
 
 buildPythonPackage rec {
   pname = "iniconfig";
   version = "1.1.1";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "bc3af051d7d14b2ee5ef9969666def0cd1a000e121eaea580d4a313df4b37f32";
+  src = fetchFromGitHub {
+     owner = "RonnyPfannschmidt";
+     repo = "iniconfig";
+     rev = "v1.1.1";
+     sha256 = "0ysy6945513cbfnhzmvlk78qar1zk4pilmw50zsvpy3xd7nc14mz";
   };
 
   doCheck = false; # avoid circular import with pytest
