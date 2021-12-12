0acfd0c1e179c1fa276fd931bdd22f207e7d5a48
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-12 13:58:20 +0000

    ugrep: 3.3.10 -> 3.3.12

--- pkgs/tools/text/ugrep/default.nix
+++ pkgs/tools/text/ugrep/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "ugrep";
-  version = "3.3.10";
+  version = "3.3.12";
 
   src = fetchFromGitHub {
     owner = "Genivia";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-Z+cNI/053/FFwNxYkbH5Zlp+BOWhP1o8xZ2LSmDaI5M=";
+    sha256 = "sha256-gQWLHZPU3pWgMpnl4BdgiWsml+meoN6lLP7KU9KluCc=";
   };
 
   buildInputs = [
