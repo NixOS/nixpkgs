a7c1bcec091b0ae7b755ad9b5421c5aa8f018ff1
Robert Schütz <nix@dotlambda.de>
2021-12-13 15:05:44 -0800

    python3Packages.vpk: init at 1.3.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10014,6 +10014,8 @@ in {
 
   vowpalwabbit = callPackage ../development/python-modules/vowpalwabbit { };
 
+  vpk = callPackage ../development/python-modules/vpk { };
+
   vsts = callPackage ../development/python-modules/vsts { };
 
   vsts-cd-manager = callPackage ../development/python-modules/vsts-cd-manager { };
