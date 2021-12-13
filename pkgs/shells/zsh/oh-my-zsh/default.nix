91d8fa7cd1c21b15229f461e3ca7d3bc18311851
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-13 17:54:07 +0000

    oh-my-zsh: 2021-12-07 -> 2021-12-13

--- pkgs/shells/zsh/oh-my-zsh/default.nix
+++ pkgs/shells/zsh/oh-my-zsh/default.nix
@@ -5,15 +5,15 @@
 , git, nix, nixfmt, jq, coreutils, gnused, curl, cacert }:
 
 stdenv.mkDerivation rec {
-  version = "2021-12-07";
+  version = "2021-12-13";
   pname = "oh-my-zsh";
-  rev = "5b987e59d0fce1a74bcfd51750c6f52d7c29c647";
+  rev = "9a3d853481645ae0f961e9cc8421fc5d84e2c3c3";
 
   src = fetchFromGitHub {
     inherit rev;
     owner = "ohmyzsh";
     repo = "ohmyzsh";
-    sha256 = "JNAuWsD03F8fbhHwwDnDh+2pPjJsyFnT/oboZIhk3rc=";
+    sha256 = "TFktV7xBm3KaRfW+cUGdwIZZD7TfU0gaq4J8cKBjtMM=";
   };
 
   installPhase = ''
