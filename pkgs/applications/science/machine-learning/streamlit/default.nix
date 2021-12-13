8544813627a13efd258c6c89681bee498b579b09
Bojan Baros <baros@twosigma.com>
2021-12-13 12:09:43 -0600

    streamlit: 0.50.2 -> 1.2.0

--- pkgs/applications/science/machine-learning/streamlit/default.nix
+++ pkgs/applications/science/machine-learning/streamlit/default.nix
@@ -1,23 +1,37 @@
 {   lib, buildPythonApplication, fetchPypi
-  , altair, astor, base58, blinker, boto3, botocore, click, enum-compat
-  , future, pillow, protobuf, requests, toml, tornado_5, tzlocal, validators, watchdog
-  , jinja2, setuptools
+  , altair, astor, base58, blinker, click, cachetools, GitPython, pillow, protobuf
+  , pyarrow, pydeck, pympler, requests, toml, tornado, tzlocal, validators, watchdog
+  , jinja2, setuptools 
 }:
 
 buildPythonApplication rec {
   pname = "streamlit";
-  version = "0.50.2";
+  version = "1.2.0";
   format = "wheel"; # the only distribution available
 
   src = fetchPypi {
     inherit pname version format;
-    sha256 = "1wymv7qckafs0p2jdjlxjaf1xrhm3iyd185jkldanbb0na5n3ndz";
+    sha256 = "1dzb68a8n8wvjppcmqdaqnh925b2dg6rywv51ac9q09zjxb6z11n";
   };
 
+  postPatch = ''
+    chmod u+rwx -R ./dist
+    pushd dist
+    wheel unpack --dest unpacked ./*.whl
+    pushd unpacked/${pname}-${version}
+
+    substituteInPlace ${pname}-${version}.dist-info/METADATA \
+      --replace "click (<8.0,>=7.0)" "click" 
+
+    popd
+    wheel pack ./unpacked/${pname}-${version}
+    popd
+  '';
+
   propagatedBuildInputs = [
-    altair astor base58 blinker boto3 botocore click enum-compat
-    future pillow protobuf requests toml tornado_5 tzlocal validators watchdog
-    jinja2 setuptools
+    altair astor base58 blinker click cachetools GitPython pillow protobuf
+    pyarrow pydeck pympler requests toml tornado tzlocal validators watchdog
+    jinja2 setuptools 
   ];
 
   postInstall = ''
