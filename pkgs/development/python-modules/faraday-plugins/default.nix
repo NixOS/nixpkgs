d1292f8bfabaa509fbbfbac4d78678e177a323a7
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-13 21:05:11 +0000

    python38Packages.faraday-plugins: 1.5.7 -> 1.5.8

--- pkgs/development/python-modules/faraday-plugins/default.nix
+++ pkgs/development/python-modules/faraday-plugins/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "faraday-plugins";
-  version = "1.5.7";
+  version = "1.5.8";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "infobyte";
     repo = "faraday_plugins";
     rev = "v${version}";
-    sha256 = "sha256-EW9p5r7RwWohNGwbITtDrEd1FYLtOwWXhVWFgPCG+Po=";
+    sha256 = "1r415g2f0cid8nr3y27ipx9hvwzh70l5wp0d7nv25qblc7g38mms";
   };
 
   propagatedBuildInputs = [
