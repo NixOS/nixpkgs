0fdcf6418ac831172a761786ba909cd22d7681ef
Anders Kaseorg <andersk@mit.edu>
2021-12-13 03:51:32 -0500

    openafs: 1.8.8 → 1.8.8.1
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>
    (cherry picked from commit 43c93f6131ec33bb6859249f6328df0eddd8564f)

--- pkgs/servers/openafs/1.8/srcs.nix
+++ pkgs/servers/openafs/1.8/srcs.nix
@@ -1,14 +1,14 @@
 { fetchurl }:
 rec {
-  version = "1.8.8";
+  version = "1.8.8.1";
   src = fetchurl {
     url = "https://www.openafs.org/dl/openafs/${version}/openafs-${version}-src.tar.bz2";
-    sha256 = "sha256-2qjvhqdyf6z83jvJemrRQxKcHCXuNfM0cIDsfp0oTaA=";
+    sha256 = "sha256-58S+1wdbzWQC4/DC1bnb52rS7jxf1d3DlzozVsoj70Q=";
   };
 
   srcs = [ src
     (fetchurl {
       url = "https://www.openafs.org/dl/openafs/${version}/openafs-${version}-doc.tar.bz2";
-      sha256 = "sha256-3cxODH1KvOTxrGB+acEudxGCX1iBPjZcTfjpfraOm+U=";
+      sha256 = "sha256-y17O3C4WS+o7SMayydbxw2v96R0GikxiqciF30j+jms=";
     })];
 }
