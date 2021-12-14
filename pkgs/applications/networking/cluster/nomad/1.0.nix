5c5068cb1028c5da921685f7b163b80c52e711a8
Robert Scott <code@humanleg.org.uk>
2021-12-14 21:00:24 +0000

    nomad_1_0: 1.0.12 -> 1.0.13

--- pkgs/applications/networking/cluster/nomad/1.0.nix
+++ pkgs/applications/networking/cluster/nomad/1.0.nix
@@ -6,6 +6,6 @@
 
 callPackage ./generic.nix {
   inherit buildGoPackage nvidia_x11 nvidiaGpuSupport;
-  version = "1.0.12";
-  sha256 = "04fqliz7y4zzs4xraid54mqxwgrzh138nmfcs876vp534slvikpi";
+  version = "1.0.13";
+  sha256 = "19wlma2y8lpb7p01wb0l20rb6nvrvldz0mm3qfisx33y56ykjyh8";
 }
