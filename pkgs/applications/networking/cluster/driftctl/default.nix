a6e6e454582264ce5d9c3859ebe77b4f7080fd17
Elie <elie.charra@snyk.io>
2021-12-14 13:55:48 +0100

    Moved from cloudskiff to snyk

--- pkgs/applications/networking/cluster/driftctl/default.nix
+++ pkgs/applications/networking/cluster/driftctl/default.nix
@@ -5,7 +5,7 @@ buildGoModule rec {
   version = "0.18.0";
 
   src = fetchFromGitHub {
-    owner = "cloudskiff";
+    owner = "snyk";
     repo = "driftctl";
     rev = "v${version}";
     sha256 = "sha256:1k9mx3yh5qza5rikg38ls78gbi4mw8ar4c1x9ij863w1c28fdzlb";
@@ -14,14 +14,14 @@ buildGoModule rec {
   vendorSha256 = "sha256:0dajz1xbf607l9y5kby4kh7h28v4b3jjmnjsf6cys46pcgxa0zw3";
 
   ldflags = [
-    "-X github.com/cloudskiff/driftctl/build.enableUsageReporting=false"
-    "-X github.com/cloudskiff/driftctl/build.env=release"
-    "-X github.com/cloudskiff/driftctl/pkg/version.version=v${version}"
+    "-X github.com/snyk/driftctl/build.enableUsageReporting=false"
+    "-X github.com/snyk/driftctl/build.env=release"
+    "-X github.com/snyk/driftctl/pkg/version.version=v${version}"
   ];
 
   meta = with lib; {
     description = "Tool to track infrastructure drift";
-    homepage = "https://github.com/cloudskiff/driftctl";
+    homepage = "https://github.com/snyk/driftctl";
     license = licenses.asl20;
     maintainers = with maintainers; [ kaction ];
   };
