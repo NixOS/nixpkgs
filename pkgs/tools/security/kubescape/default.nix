4531dadac8235af1f327a4bb726845ac155f334f
Fabian Affolter <mail@fabian-affolter.ch>
2021-12-14 17:05:31 +0100

    kubescape: 1.0.134 -> 1.0.135

--- pkgs/tools/security/kubescape/default.nix
+++ pkgs/tools/security/kubescape/default.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "kubescape";
-  version = "1.0.134";
+  version = "1.0.135";
 
   src = fetchFromGitHub {
     owner = "armosec";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-jcMaLp3SOeH6kE9V5fYSNP5klJwqWt3mrKz1VHOzjx4=";
+    sha256 = "sha256-8nl4DSftiv0ZT6aUfAk0eLdpf/ScqDQqntK/2xbOn7k=";
   };
 
   nativeBuildInputs = [
