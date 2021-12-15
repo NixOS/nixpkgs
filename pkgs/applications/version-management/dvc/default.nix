5213654f35b5e3fe55c7568eb26133d1abb91a31
Tracy Wadleigh <tracy@compoundeye.com>
2021-12-15 09:36:27 -0800

    dvc: 0.24.3 -> 2.9.2

--- pkgs/applications/version-management/dvc/default.nix
+++ pkgs/applications/version-management/dvc/default.nix
@@ -1,60 +1,226 @@
 { lib
 , python3Packages
 , fetchFromGitHub
-, enableGoogle ? false
-, enableAWS ? false
-, enableAzure ? false
-, enableSSH ? false
 }:
 
 with python3Packages;
+
+let
+
+  aiohttp-retry = buildPythonPackage rec {
+    pname = "aiohttp_retry";
+    version = "2.4.6";
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "KIwaDZO0s62SkQxWoDJsawVcfhNFAnsm8XOsGFlKl9o=";
+    };
+
+    propagatedBuildInputs = [ aiohttp ];
+  };
+
+  commonmark = buildPythonPackage rec {
+    pname = "commonmark";
+    version = "0.9.1";
+
+    propagatedBuildInputs = [
+    ];
+    doCheck = false;
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "RS+dyFm+fwZjHdyzKLaRnGeYSsplTl/vs5FNVGka7WA=";
+    };
+  };
+
+  dictdiffer = buildPythonPackage rec {
+    pname = "dictdiffer";
+    version = "0.9.0";
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "F7rPX7/mE8zxttUSvXZuayH7eYgioTOqhgmLismZdXg=";
+    };
+
+    propagatedBuildInputs = [ setuptools-scm pytest-runner ];
+    checkInputs = [ pytest ];
+    doCheck = false;
+  };
+
+  flatten-dict = buildPythonPackage rec {
+    pname = "flatten-dict";
+    version = "0.4.2";
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "UGqWtub4Bbga5GoPnzEpC+tfp53tnYDb4bf6I2q0MHY=";
+    };
+
+    propagatedBuildInputs = [ six ];
+  };
+
+  mailchecker = buildPythonPackage rec {
+    pname = "mailchecker";
+    version = "4.1.6";
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "v1blkN3lnrdJjYIm5G+8WjnhNhVIe9l9LLKd3SNA4Gk=";
+    };
+  };
+
+  python-benedict = buildPythonPackage rec {
+    pname = "python-benedict";
+    version = "0.24.3";
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "+de0HL5/55s98MEiherPKXoVf6e12dpGETKQirE8OQ8=";
+    };
+
+    propagatedBuildInputs = [
+      ftfy
+      mailchecker
+      phonenumbers
+      python-dateutil
+      python-fsutil
+      python-slugify
+      pyyaml
+      requests
+      six
+      toml
+      xmltodict
+    ];
+
+    checkInputs = [ pytest ];
+    doCheck = false;
+  };
+
+  python-fsutil = buildPythonPackage rec {
+    pname = "python-fsutil";
+    version = "0.5.0";
+
+    propagatedBuildInputs = [ requests ];
+
+    checkInputs = [ pytest codecov coverage tox ];
+    doCheck = false;
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "AqNHVA0QwWFjkKU2ztc/1n340BxJn0l9CrPeP7sjbw4=";
+    };
+  };
+
+  rich = buildPythonPackage rec {
+    pname = "rich";
+    version = "10.16.1";
+
+    propagatedBuildInputs = [
+      pygments
+      commonmark
+      colorama
+    ];
+    doCheck = false;
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "SUnnPeMheE72Zk67yFSsgrIP9gsoZQl7k/O5tB4w2ic=";
+    };
+  };
+
+  scmrepo = buildPythonPackage
+    rec {
+      pname = "scmrepo";
+      version = "0.0.4";
+
+      format = "pyproject";
+
+      src = fetchPypi {
+        inherit pname version;
+        sha256 = "4ApG8cX1YMNbr9eS/RSwcUThotHDhOIKtEol9fHXwzU=";
+      };
+
+      propagatedBuildInputs = [
+        GitPython
+        dulwich
+        pygit2
+        pygtrie
+        fsspec
+        pathspec
+        asyncssh
+        funcy
+      ];
+    };
+
+  shtab = buildPythonPackage rec {
+    pname = "shtab";
+    version = "1.5.2";
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "N4ucFl1QqLv0N5tZhxA2UHuEexS14BT31gthU6xR6tA=";
+    };
+
+    nativeBuildInputs = [ setuptools setuptools_scm ];
+    doCheck = false;
+  };
+
+in
+
 buildPythonApplication rec {
   pname = "dvc";
-  version = "0.24.3";
+  version = "2.9.2";
 
-  # PyPi only has wheel
-  src = fetchFromGitHub {
-    owner = "iterative";
-    repo = "dvc";
-    rev = version;
-    sha256 = "1wqq4i23hppilp20fx5a5nj93xwf3wwwr2f8aasvn6jkv2l22vpl";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "VDJXkHqEOVLES9seqE8q4z2Gk1hMEXXrSeuV2gwqLkk=";
   };
+
+  nativeBuildInputs = [
+    setuptools-scm
+    setuptools-scm-git-archive
+  ];
 
   propagatedBuildInputs = [
     ply
-    configparser
-    zc_lockfile
-    future
     colorama
     configobj
-    networkx
-    pyyaml
-    GitPython
-    setuptools
     nanotime
     pyasn1
-    schema
-    jsonpath_rw
+    voluptuous
     requests
     grandalf
-    asciimatics
     distro
     appdirs
-  ]
-  ++ lib.optional enableGoogle google-cloud-storage
-  ++ lib.optional enableAWS boto3
-  ++ lib.optional enableAzure azure-storage-blob
-  ++ lib.optional enableSSH paramiko;
+    ruamel-yaml
+    toml
+    funcy
+    pathspec
+    shortuuid
+    tqdm
+    packaging
+    zc_lockfile
+    flufl_lock
+    networkx
+    psutil
+    pydot
+    flatten-dict
+    tabulate
+    pygtrie
+    dpath
+    shtab
+    rich
+    dictdiffer
+    python-benedict
+    pyparsing
+    typing-extensions
+    fsspec
+    aiohttp-retry
+    diskcache
+    scmrepo
+  ];
 
-  # tests require access to real cloud services
-  # nix build tests have to be isolated and run locally
   doCheck = false;
-
-  patches = [ ./dvc-daemon.patch ];
-
-  postPatch = ''
-    substituteInPlace dvc/daemon.py --subst-var-by dvc "$out/bin/dcv"
-  '';
 
   meta = with lib; {
     description = "Version Control System for Machine Learning Projects";
