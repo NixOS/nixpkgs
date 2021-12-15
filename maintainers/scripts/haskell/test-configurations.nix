65de104e6c540e4dfab3c7fb07dbcd95c8731231
sternenseemann <sternenseemann@systemli.org>
2021-12-15 11:21:57 +0100

    maintainers/scripts/test-configuration.nix: warn about broken pkgs
    The warnings help when removing the broken flags when all packages turn
    out to build and could also be helpful when investigating failures.

--- maintainers/scripts/haskell/test-configurations.nix
+++ maintainers/scripts/haskell/test-configurations.nix
@@ -98,7 +98,10 @@ let
   # overlays. For common, nix, darwin etc. only the derivation from the default
   # package set will be emitted.
   packages = builtins.filter (v:
-    v != null && (skipEvalErrors -> (builtins.tryEval (v.outPath or v)).success)
+    lib.warnIf (v.meta.broken or false) "${v.pname} is marked as broken" (
+      v != null
+      && (skipEvalErrors -> (builtins.tryEval (v.outPath or v)).success)
+    )
   ) (
     lib.concatMap (fileName:
       let
