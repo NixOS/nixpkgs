e0843a80e2a77888866d10482372894ad051ec8a
Joel <joel@joel.tokyo>
2021-12-15 15:29:56 +1000

    minecraft-server: fix using latest jre for all minecraft server versions

--- pkgs/games/minecraft-server/default.nix
+++ pkgs/games/minecraft-server/default.nix
@@ -1,14 +1,20 @@
-{ callPackage, lib }:
+{ callPackage, lib, javaPackages }:
 # if you add more versions make sure to add to all-packages.nix
 let
   versions = lib.importJSON ./versions.json;
+
   latestVersion = lib.last (builtins.sort lib.versionOlder (builtins.attrNames versions));
   escapeVersion = builtins.replaceStrings [ "." ] [ "_" ];
+
+  getJavaVersion = v: (builtins.getAttr "openjdk${toString v}" javaPackages.compiler).headless;
 
   packages = lib.mapAttrs'
     (version: value: {
       name = "minecraft-server_${escapeVersion version}";
-      value = callPackage ./derivation.nix { inherit (value) version url sha1; };
+      value = callPackage ./derivation.nix {
+        inherit (value) version url sha1;
+        jre_headless = getJavaVersion (if value.javaVersion == null then 8 else value.javaVersion); # versions <= 1.6 will default to 8
+      };
     })
     versions;
 in
