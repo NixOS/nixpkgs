44e6109fb42b3168edc53c09f8fbb27c86e66a8a
sternenseemann <sternenseemann@systemli.org>
2021-12-15 09:31:28 +0100

    python3Packages.pypandoc: 1.6.4 -> 1.7.0
    https://github.com/NicklasTegner/pypandoc/releases/tag/v1.7.0

--- pkgs/development/python-modules/pypandoc/default.nix
+++ pkgs/development/python-modules/pypandoc/default.nix
@@ -4,13 +4,13 @@
 
 buildPythonPackage rec {
   pname = "pypandoc";
-  version = "1.6.4";
+  version = "1.7.0";
 
   src = fetchFromGitHub {
     owner = "NicklasTegner";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0rssjig3nwdi4qvsjq7v7k8jyv6l9szfl5dp1a8s54c4j4dw37nh";
+    sha256 = "00r88qcvc9jpi8jvd6rpizz9gm33aq8hc3mf8lrarrjiq2fsxmk9";
   };
 
   patches = [
