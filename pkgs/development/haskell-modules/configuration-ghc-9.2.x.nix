6edf0b0b67aaabad935a93b1e5a6f805f2c4bcea
sternenseemann <sternenseemann@systemli.org>
2021-12-15 10:31:57 +0100

    haskell.compiler.ghc921.hlint: 3.3.4 -> unstable-2021-12-12
    hlint introduced GHC 9.2.1 support on its development branch recently.

--- pkgs/development/haskell-modules/configuration-ghc-9.2.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-9.2.x.nix
@@ -209,11 +209,21 @@ self: super: {
   # Tests have a circular dependency on quickcheck-instances
   text-short = dontCheck super.text-short_0_1_4;
 
-  # hlint 3.3 needs a ghc-lib-parser newer than the one from stackage
-  hlint = super.hlint_3_3_4.overrideScope (self: super: {
+  # Use hlint from git for GHC 9.2.1 support
+  hlint = overrideCabal {
+    version = "unstable-2021-12-12";
+    src = pkgs.fetchFromGitHub {
+      owner = "ndmitchell";
+      repo = "hlint";
+      rev = "77a9702e10b772a7695c08682cd4f450fd0e9e46";
+      sha256 = "0hpp3iw7m7w2abr8vb86gdz3x6c8lj119zxln933k90ia7bmk8jc";
+    };
+    revision = null;
+    editedCabalFile = null;
+  } (super.hlint_3_3_4.overrideScope (self: super: {
     ghc-lib-parser = self.ghc-lib-parser_9_2_1_20211101;
     ghc-lib-parser-ex = self.ghc-lib-parser-ex_9_2_0_1;
-  });
+  }));
 
   # https://github.com/sjakobi/bsb-http-chunked/issues/38
   bsb-http-chunked = dontCheck super.bsb-http-chunked;
--- pkgs/top-level/release-haskell.nix
+++ pkgs/top-level/release-haskell.nix
@@ -311,6 +311,7 @@ let
       funcmp = released ++ [ compilerNames.ghc921 ];
       haskell-language-server = released;
       hoogle = released ++ [ compilerNames.ghc921 ];
+      hlint = released ++ [ compilerNames.ghc921 ];
       hsdns = released ++ [ compilerNames.ghc921 ];
       jailbreak-cabal = released ++ [ compilerNames.ghc921 ];
       language-nix = released ++ [ compilerNames.ghc921 ];
