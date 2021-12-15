788c11592816d8455e722895eaa80c6ef8551f00
Fabian Affolter <mail@fabian-affolter.ch>
2021-12-15 10:21:20 +0100

    log4j-vuln-scanner: 0.6 -> 0.7.1

--- pkgs/tools/security/log4j-vuln-scanner/default.nix
+++ pkgs/tools/security/log4j-vuln-scanner/default.nix
@@ -5,16 +5,21 @@
 
 buildGoModule rec {
   pname = "log4j-vuln-scanner";
-  version = "0.6";
+  version = "0.7.1";
 
   src = fetchFromGitHub {
     owner = "hillu";
     repo = "local-log4j-vuln-scanner";
     rev = "v${version}";
-    sha256 = "sha256-6LbKwDu3YZFRaIUOcepbLVZC9OYnqb0Tl0ElGDIzW48=";
+    sha256 = "sha256-YEXYZtjcZTl+9IgRuSRK/pvnISuW6Jbwuv+dqr4pork=";
   };
 
   vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+
+  postInstall = ''
+    mv $out/bin/scanner $out/bin/$pname
+    mv $out/bin/patcher $out/bin/log4j-vuln-patcher
+  '';
 
   meta = with lib; {
     description = "Local log4j vulnerability scanner";
