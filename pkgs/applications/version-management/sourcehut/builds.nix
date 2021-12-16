2e2e0b3779484ab4a6b8448596f470e2aad61500
Julien Moutinho <julm+nixpkgs@sourcephile.fr>
2021-12-16 05:27:41 +0100

    sourcehut.buildsrht: 0.74.12 -> 0.74.13

--- pkgs/applications/version-management/sourcehut/builds.nix
+++ pkgs/applications/version-management/sourcehut/builds.nix
@@ -11,13 +11,13 @@
 , python
 }:
 let
-  version = "0.74.12";
+  version = "0.74.13";
 
   src = fetchFromSourcehut {
     owner = "~sircmpwn";
     repo = "builds.sr.ht";
     rev = version;
-    sha256 = "sha256-lGMfJqpAB5evwDrNWgeSmAEAlJ8lb2V/daCLD1VpxKg=";
+    sha256 = "sha256-Wraw4lRKqCthd4jfwq81zRNo2hrIeGHUATAl0QRRl4o=";
   };
 
   buildWorker = src: buildGoModule {
