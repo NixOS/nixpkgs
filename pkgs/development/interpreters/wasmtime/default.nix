969fcef5c1eda8247345ebd0376b626ae6b9061f
Gabor Greif <ggreif@gmail.com>
2021-12-16 18:56:08 +0100

    wasmtime: 0.28.0 -> 0.32.0

--- pkgs/development/interpreters/wasmtime/default.nix
+++ pkgs/development/interpreters/wasmtime/default.nix
@@ -2,17 +2,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "wasmtime";
-  version = "0.28.0";
+  version = "0.32.0";
 
   src = fetchFromGitHub {
     owner = "bytecodealliance";
     repo = pname;
     rev = "v${version}";
-    sha256 = "12d993yjzmji6vji7j9dzy9z72hgk9xs5x1c9hykli6mayxmc1q0";
+    sha256 = "sha256-iko2G2cUIQYv7Sia8fLtb7d6XCbpOKz31ST62eE19B0";
     fetchSubmodules = true;
   };
 
-  cargoSha256 = "1n5wgakx0mk468p3w9nnkcfmyy26gk8kkchdiy70sfbf4846x7cl";
+  cargoSha256 = "sha256-z8x004BbRWi9cRf2I27uiFuu2Jnr1Z3Ey992S5hdyNs";
 
   nativeBuildInputs = [ python cmake clang ];
   buildInputs = [ llvmPackages.libclang ] ++
