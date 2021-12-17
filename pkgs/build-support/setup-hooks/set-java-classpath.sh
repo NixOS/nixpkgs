ed328874971f06f3dbc5e95e45d0db0d5659ae33
happysalada <raphael@megzari.com>
2021-12-16 19:12:21 -0500

    openjdk: setup hook: add nullglob

--- pkgs/build-support/setup-hooks/set-java-classpath.sh
+++ pkgs/build-support/setup-hooks/set-java-classpath.sh
@@ -5,9 +5,13 @@ export CLASSPATH
 
 addPkgToClassPath () {
     local jar
+    shopt -u failglob
+    shopt -s nullglob
     for jar in $1/share/java/*.jar; do
         export CLASSPATH=''${CLASSPATH-}''${CLASSPATH:+:}''${jar}
     done
+    shopt -u nullglob
+    shopt -s failglob
 }
 
 addEnvHooks "$targetOffset" addPkgToClassPath
