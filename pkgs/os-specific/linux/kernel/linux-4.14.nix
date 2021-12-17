f69589612734e35b6dff7cb288229cdd7168e4b0
TredwellGit <tredwell@tutanota.com>
2021-12-17 23:07:50 +0000

    linux: 4.14.257 -> 4.14.258
    (cherry picked from commit 860b4c92b87238dc7caba9c6b93f961bfa77e218)

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "4.14.257";
+  version = "4.14.258";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0jnw02jphvm9zcviwwymxyhq8kd0bk0v1827ninnv6bdy3140izv";
+    sha256 = "162bzhm0k8kipgk0ma745rjcl33rqhpwxdfdz3q6rkp48b82kbvi";
   };
 } // (args.argsOverride or {}))
