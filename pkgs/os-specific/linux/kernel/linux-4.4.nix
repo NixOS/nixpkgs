72166d1378b3bd22e53ee512245ffe90e1f677de
TredwellGit <tredwell@tutanota.com>
2021-12-17 23:08:02 +0000

    linux: 4.4.294 -> 4.4.295
    (cherry picked from commit 2deb4377d5ff1eee430327766df17a0974020099)

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,12 +1,12 @@
 { buildPackages, fetchurl, perl, buildLinux, nixosTests, stdenv, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.294";
+  version = "4.4.295";
   extraMeta.branch = "4.4";
   extraMeta.broken = stdenv.isAarch64;
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0k0h5m1ng2049d5ggrq4q81vgsfmdpkqla73vg2a3bf2v6ycjmc7";
+    sha256 = "1h3i2lgm2sy325f58jildip7m6sb4hr2n9pm3gc3h2gash65kc2r";
   };
 } // (args.argsOverride or {}))
