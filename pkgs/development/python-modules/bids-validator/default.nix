3975d65462dc7c975bfd6ec5a5308722ebf25c24
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-17 04:57:50 +0000

    python38Packages.bids-validator: 1.8.8 -> 1.8.9

--- pkgs/development/python-modules/bids-validator/default.nix
+++ pkgs/development/python-modules/bids-validator/default.nix
@@ -4,12 +4,12 @@
 }:
 
 buildPythonPackage rec {
-  version = "1.8.8";
+  version = "1.8.9";
   pname = "bids-validator";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e9ebc4cf1004bd343bbb56105b1eb2be833200e466dbaff7dfd267fca044d55e";
+    sha256 = "01fcb5a8fe6de1280cdfd5b37715103ffa0bafb3c739ca7f5ffc41e46549612e";
   };
 
   # needs packages which are not available in nixpkgs
