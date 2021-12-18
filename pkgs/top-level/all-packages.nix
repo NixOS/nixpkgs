3fce038da7b0babc7ba6f280b5880433b65998fc
Emery Hemingway <ehmry@posteo.net>
2021-12-18 09:52:01 +0000

    nncp: 7.7.0 -> 8.0.2
    This update introduces an encrypted packet format that is
    incompatible with releases older than 8.0.0.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27242,9 +27242,7 @@ with pkgs;
 
   ninjas2 = callPackage ../applications/audio/ninjas2 {};
 
-  nncp = callPackage ../tools/misc/nncp {
-    go = go_1_15;
-  };
+  nncp = callPackage ../tools/misc/nncp { };
 
   notion = callPackage ../applications/window-managers/notion { };
 
