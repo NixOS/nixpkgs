26f623b85606778c503021759355941c98a550ea
Las Safin <me@las.rs>
2021-12-19 19:12:54 +0000

    alsa-ucm-conf: 1.2.5.1 -> 1.2.6.3

--- pkgs/os-specific/linux/alsa-project/alsa-ucm-conf/default.nix
+++ pkgs/os-specific/linux/alsa-project/alsa-ucm-conf/default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://alsa/lib/${pname}-${version}.tar.bz2";
-    sha256 = "sha256-uKA6o4emJKL2XtwgG/d3QhGQtgUpqSCHZGgjr72Wxc0=";
+    sha256 = "uKA6o4emJKL2XtwgG/d3QhGQtgUpqSCHZGgjr72Wxc0=";
   };
 
   dontBuild = true;
@@ -30,7 +30,7 @@ stdenv.mkDerivation rec {
     '';
 
     license = licenses.bsd3;
-    maintainers = [ maintainers.roastiek ];
+    maintainers = [ maintainers.roastiek maintainers.l-as ];
     platforms = platforms.linux;
   };
 }
