8182ec51572a9224cae5fbaf72cbaec29c112c2d
Alexander Nortung <alex_nortung@live.dk>
2021-12-20 21:51:30 +0100

    session-desktop-appimage: 1.7.5 -> 1.7.6

--- pkgs/applications/networking/instant-messengers/session-desktop-appimage/default.nix
+++ pkgs/applications/networking/instant-messengers/session-desktop-appimage/default.nix
@@ -5,10 +5,10 @@
 
 appimageTools.wrapType2 rec {
   pname = "session-desktop-appimage";
-  version = "1.7.5";
+  version = "1.7.6";
   src = fetchurl {
     url = "https://github.com/oxen-io/session-desktop/releases/download/v${version}/session-desktop-linux-x86_64-${version}.AppImage";
-    sha256 = "We1/1UIMJ8JnI9V4Z7TA0Vx3YNOfqLBW9DtklwwqUVE=";
+    sha256 = "PNjUslqLcSxkRSXFpesBr2sfre4wetZWfUQTjywdClU=";
   };
 
   meta = with lib; {
