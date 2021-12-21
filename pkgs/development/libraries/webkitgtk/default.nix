eff0dee5f6b598e0119a4074b692e98b937fd4e2
Philipp <philipp+dev@xndr.de>
2021-12-21 19:52:31 +0100

    webkitgtk: 2.34.2 -> 2.34.3
    (cherry picked from commit f6cd576e4504201140f0bf324418040d6bc825e5)

--- pkgs/development/libraries/webkitgtk/default.nix
+++ pkgs/development/libraries/webkitgtk/default.nix
@@ -64,7 +64,7 @@ assert enableGeoLocation -> geoclue2 != null;
 
 stdenv.mkDerivation rec {
   pname = "webkitgtk";
-  version = "2.34.2";
+  version = "2.34.3";
 
   outputs = [ "out" "dev" ];
 
@@ -72,7 +72,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://webkitgtk.org/releases/${pname}-${version}.tar.xz";
-    sha256 = "sha256-WEZ31ufK4S4nzcyOBbTPc7VISaJK/D16QM7JEBbe/wA=";
+    sha256 = "sha256-DS83qjLiGjbk3Vpc565c4nQ1wp1oA7liuMkMsMxJxS0=";
   };
 
   patches = lib.optionals stdenv.isLinux [
