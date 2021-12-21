4ea6bfadfbdcf22fd4a6b939ecfc7f1f0a4b036d
Zhaofeng Li <hello@zhaofeng.li>
2021-12-20 21:26:59 -0800

    wlroots_0_12: drop
    phoc was the last package that depended on this.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26192,7 +26192,6 @@ with pkgs;
   };
 
   wlroots = wlroots_0_15;
-  wlroots_0_12 = callPackage ../development/libraries/wlroots/0.12.nix {};
   wlroots_0_14 = callPackage ../development/libraries/wlroots/0.14.nix {
     inherit (xorg) xcbutilrenderutil;
   };
