943096eaff6dc15ed913da9c312c5c4fcd693fa5
jmininger <no email>
2021-12-23 18:02:56 -0500

    bump ledger-live-desktop to 2.36.3

--- pkgs/applications/blockchains/ledger-live-desktop/default.nix
+++ pkgs/applications/blockchains/ledger-live-desktop/default.nix
@@ -2,12 +2,12 @@
 
 let
   pname = "ledger-live-desktop";
-  version = "2.36.2";
+  version = "2.36.3";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://github.com/LedgerHQ/${pname}/releases/download/v${version}/${pname}-${version}-linux-x86_64.AppImage";
-    hash = "sha256-cZwKL5G7CmQBw2x0wPYHZCiAABxPatvfIcOAf0d2+Dg=";
+    hash = "sha256:1sxsr70nm86fyx2m3wf694x1p6kvmkwxw6fqnslpbyjpyi9x73cd";
   };
 
   appimageContents = appimageTools.extractType2 {
