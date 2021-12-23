508fd27d57eff8d7a2cbcfd3df1cd3e067c29443
Bernardo Meurer <bernardo@meurer.org>
2021-12-23 17:47:05 -0300

    linux: 4.9.293 -> 4.9.294

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,12 +1,12 @@
 { buildPackages, fetchurl, perl, buildLinux, nixosTests, stdenv, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.293";
+  version = "4.9.294";
   extraMeta.branch = "4.9";
   extraMeta.broken = stdenv.isAarch64;
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0l64rz6ly5ls23lqq0cv98xb4z2mimp2jrsjrs6kq3zm4k2mm4gs";
+    sha256 = "0s527wr1zngyfz4p7nss1id14amc448g19i1wy20s13n43gm1jii";
   };
 } // (args.argsOverride or {}))
