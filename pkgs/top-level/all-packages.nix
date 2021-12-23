375b48a52df6d2230493a71d549807402eed318f
Alexander Nortung <alex_nortung@live.dk>
2021-12-23 10:43:49 +0100

    sagetex: init at 3.6
    sagetex: init at 3.6
    
    sagetex: refactor
    
    Will now build from source
    
    sagetex: etc
    
    Changed description.
    
    Co-authored-by: legendofmiracles <30902201+legendofmiracles@users.noreply.github.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31676,6 +31676,8 @@ with pkgs;
   sage = callPackage ../applications/science/math/sage { };
   sageWithDoc = sage.override { withDoc = true; };
 
+  sagetex = callPackage ../misc/sagetex { };
+
   subread = callPackage ../applications/science/biology/subread { };
 
   suitesparse_4_2 = callPackage ../development/libraries/science/math/suitesparse/4.2.nix { };
