8dd791e6ede64a1501d1adf294bbf9c8469059c3
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-24 01:05:30 +0000

    clojure: 1.10.3.1040 -> 1.10.3.1053

--- pkgs/development/interpreters/clojure/default.nix
+++ pkgs/development/interpreters/clojure/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   pname = "clojure";
-  version = "1.10.3.1040";
+  version = "1.10.3.1053";
 
   src = fetchurl {
     # https://clojure.org/releases/tools
     url = "https://download.clojure.org/install/clojure-tools-${version}.tar.gz";
-    sha256 = "+21o3usbxeOoac7umPzZaDNVify1fjHuTujgNKtNeXc=";
+    sha256 = "p91+Ylx8HQL/o3Pq4Pd1c9GAMiOXvJSw+09LGYLj5Wo=";
   };
 
   nativeBuildInputs = [
