2cc754a7baa72659430ec961608f0fc1dbe128df
Doron Behar <doron.behar@gmail.com>
2021-12-24 10:00:23 +0200

    cc-wrapper: Fortran: disable stackprotector hardening on darwin aarch64

--- pkgs/build-support/cc-wrapper/fortran-hook.sh
+++ pkgs/build-support/cc-wrapper/fortran-hook.sh
@@ -5,7 +5,7 @@ export FC${role_post}=@named_fc@
 
 # If unset, assume the default hardening flags.
 # These are different for fortran.
-: ${NIX_HARDENING_ENABLE="stackprotector pic strictoverflow relro bindnow"}
+: ${NIX_HARDENING_ENABLE="@fortran_hardening@"}
 export NIX_HARDENING_ENABLE
 
 unset -v role_post
