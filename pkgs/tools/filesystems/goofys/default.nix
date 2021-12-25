34ae1959b7c0a8d64556a4a6de87210b7d6d9e96
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2021-12-24 22:36:36 -0500

    goofys: mark as broken on darwin

--- pkgs/tools/filesystems/goofys/default.nix
+++ pkgs/tools/filesystems/goofys/default.nix
@@ -29,6 +29,7 @@ buildGoModule {
     description = "A high-performance, POSIX-ish Amazon S3 file system written in Go.";
     license = [ lib.licenses.mit ];
     maintainers = [ lib.maintainers.adisbladis ];
+    broken = stdenv.isDarwin; # needs to update gopsutil to at least v3.21.3 to include https://github.com/shirou/gopsutil/pull/1042
   };
 
 }
