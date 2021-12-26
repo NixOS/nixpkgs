02c725db8508f1305eb44214f3e914a721a496ff
cirno-999 <reverene@pm.me>
2021-12-26 14:13:29 +0100

    maintainers: add cirno-999

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -2134,6 +2134,12 @@
     githubId = 3956062;
     name = "Simon Lackerbauer";
   };
+  cirno-999 = {
+    email = "reverene@protonmail.com";
+    github = "cirno-999";
+    githubId = 73712874;
+    name = "cirno-999";
+  };
   citadelcore = {
     email = "alex@arctarus.co.uk";
     github = "citadelcore";
