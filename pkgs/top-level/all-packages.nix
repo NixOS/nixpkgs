388b7ecfa0e71efa3d87695e19aedda502baa29d
Michael Raskin <7c6f434c@mail.ru>
2021-12-26 11:19:08 +0100

    monotone: ping boost_170 to fix build

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27123,6 +27123,7 @@ with pkgs;
 
   monotone = callPackage ../applications/version-management/monotone {
     lua = lua5;
+    boost = boost170;
   };
 
   monotoneViz = callPackage ../applications/version-management/monotone-viz {
