e9f4ecd90e8f067a14ac57eb4d2947248a2cd35f
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-12-27 20:32:48 +0200

    libpulseaudio: fix aarch64-darwin build
    Removes darwin Libc buildInput, which does not appear to be needed on
    x86_64, and which does not support aarch64 at all (too old)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21323,7 +21323,6 @@ with pkgs;
   };
 
   pulseaudio = callPackage ../servers/pulseaudio ({
-    inherit (darwin) Libc;
     inherit (darwin.apple_sdk.frameworks) CoreServices AudioUnit Cocoa;
   } // lib.optionalAttrs stdenv.isDarwin {
     # Default autoreconfHook (2.70) fails on darwin,
