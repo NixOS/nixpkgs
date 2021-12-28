NixOS/nixpkgs

commit ae9337179b73290cfe9a057d40a0f1c08c8365ed

vm: deprecate phases

pkgs/build-support/vm/default.nix
@@ -494,7 +494,9 @@ rec {
      tarball must contain an RPM specfile. */
 
   buildRPM = attrs: runInLinuxImage (stdenv.mkDerivation ({
-    phases = "prepareImagePhase sysInfoPhase buildPhase installPhase";
+    prePhases = [ prepareImagePhase sysInfoPhase ];
+    dontUnpack = true;
+    dontConfigure = true;
 
     outDir = "rpms/${attrs.diskImage.name}";
 

pkgs/build-support/vm/test.nix
@@ -1,4 +1,4 @@
-with import ../../.. {};
+with import ../../.. { };
 with vmTools;
 
 {
@@ -30,7 +30,7 @@ with vmTools;
     src = patchelf.src;
     diskImage = diskImages.ubuntu1204i386;
     memSize = 512;
-    phases = "sysInfoPhase unpackPhase patchPhase configurePhase buildPhase checkPhase installPhase fixupPhase distPhase";
+    prePhases = [ sysInfoPhase ];
     sysInfoPhase = ''
       dpkg-query --list
     '';

commit e0455e1e29bd21d267368e8bfc59ae7505f751df

vmTools: fix phases
These gave missing attribute errors.  They should have been written as
strings in ae9337179b7.

Fixes: ae9337179b7 ("vm: deprecate phases")
Fixes: 52b10ee8725 ("vmTools refactor: don't use huge `with pkgs;`")

pkgs/build-support/vm/default.nix
@@ -510,7 +510,7 @@ rec {
      tarball must contain an RPM specfile. */
 
   buildRPM = attrs: runInLinuxImage (stdenv.mkDerivation ({
-    prePhases = [ pkgs.prepareImagePhase pkgs.sysInfoPhase ];
+    prePhases = [ "prepareImagePhase" "sysInfoPhase" ];
     dontUnpack = true;
     dontConfigure = true;
 