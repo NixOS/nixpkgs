b9103dbea5d1cdae382acd214f71429c9bd47870
Fabian Affolter <mail@fabian-affolter.ch>
2021-12-28 22:56:55 +0100

    python3Packages.flux-led: 0.27.19 -> 0.27.20

--- pkgs/development/python-modules/flux-led/default.nix
+++ pkgs/development/python-modules/flux-led/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "flux-led";
-  version = "0.27.19";
+  version = "0.27.20";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "Danielhiversen";
     repo = "flux_led";
     rev = version;
-    sha256 = "sha256-QJCRBwsfQQzXQ7DDvzb9NKnZS2LqrQUygdzJ+ccjWt4=";
+    sha256 = "sha256-bkVhM6Em0kyIhfSCFtotZR5Ob4i8IWBL1oSc+BTVFuw=";
   };
 
   propagatedBuildInputs = [
