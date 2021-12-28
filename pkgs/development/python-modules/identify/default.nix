fceda0ab7eca4b15aea7e276fcb3e5e5b966814e
Fabian Affolter <mail@fabian-affolter.ch>
2021-12-28 10:30:39 +0100

    python3Packages.identify: 2.4.0 -> 2.4.1

--- pkgs/development/python-modules/identify/default.nix
+++ pkgs/development/python-modules/identify/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "identify";
-  version = "2.4.0";
+  version = "2.4.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "pre-commit";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-0J3P3RawafVAfOUhK9qSz5K8y0goMqTjMh5PL60sqME=";
+    sha256 = "sha256-+kfIpmJ6Gnb33MZ7NZrE8oVSBbZLuRfIvfCbstxJFX0=";
   };
 
   checkInputs = [
