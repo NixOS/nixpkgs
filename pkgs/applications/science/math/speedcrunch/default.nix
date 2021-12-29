5ca1d45da35a6c564fb69e29ba584fe9d8681530
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-12-29 17:52:11 +0100

    speedcrunch: use proper fetcher

--- pkgs/applications/science/math/speedcrunch/default.nix
+++ pkgs/applications/science/math/speedcrunch/default.nix
@@ -1,13 +1,13 @@
-{ mkDerivation, lib, fetchgit, cmake, qtbase, qttools }:
+{ mkDerivation, lib, fetchFromBitbucket, cmake, qtbase, qttools }:
 
 mkDerivation rec {
   pname = "speedcrunch";
   version = "0.12.0";
 
-  src = fetchgit {
-    # the tagging is not standard, so you probably need to check this when updating
-    rev = "refs/tags/release-${version}";
-    url = "https://bitbucket.org/heldercorreia/speedcrunch";
+  src = fetchFromBitbucket {
+    owner = "heldercorreia";
+    repo = pname;
+    rev = "release-${version}";
     sha256 = "0vh7cd1915bjqzkdp3sk25ngy8cq624mkh8c53c5bnzk357kb0fk";
   };
 
@@ -29,7 +29,7 @@ mkDerivation rec {
       precisions, unlimited variable storage, intelligent automatic completion
       full keyboard-friendly and more than 15 built-in math function.
     '';
-    maintainers = with maintainers; [ gebner ];
+    maintainers = with maintainers; [ gebner j0hax ];
     inherit (qtbase.meta) platforms;
     # works with qt 5.6 and qt 5.8
     broken = builtins.compareVersions qtbase.version "5.7.0" == 0;
