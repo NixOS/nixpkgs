8a54f4c0bbef28976f69347c458ce74ed98e0234
ajs124 <ajs124@users.noreply.github.com>
2021-12-29 05:41:04 +0100

    heisenbridge: 1.7.1 -> 1.8.2 (#152301)

--- pkgs/servers/heisenbridge/default.nix
+++ pkgs/servers/heisenbridge/default.nix
@@ -2,22 +2,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "heisenbridge";
-  version = "1.7.1";
+  version = "1.8.2";
 
   src = fetchFromGitHub {
     owner = "hifi";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-q1Rj8BehvYnV/Kah5YKAxBUz4j9WziSqn1fVeaKpy7g=";
+    sha256 = "173prcd56rwlxjxlw67arnm12k1l317xi5s6m7jhmp8zbbrj5vwr";
   };
-
-  patches = [
-    # Compatibility with aiohttp 3.8.0
-    (fetchpatch {
-      url = "https://github.com/hifi/heisenbridge/commit/cff5d33e0b617e6cf3a44dc00c72b98743175c9e.patch";
-      sha256 = "sha256-y5X4mWvX1bq0XNZNTYUc0iK3SzvaHpS7px53I7xC9c8=";
-    })
-  ];
 
   postPatch = ''
     echo "${version}" > heisenbridge/version.txt
