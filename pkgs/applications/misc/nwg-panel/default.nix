536e006f827b6a6865bf512560e42165b77aba38
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-30 15:05:40 +0000

    nwg-panel: 0.5.1 -> 0.5.4

--- pkgs/applications/misc/nwg-panel/default.nix
+++ pkgs/applications/misc/nwg-panel/default.nix
@@ -13,13 +13,13 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "nwg-panel";
-  version = "0.5.1";
+  version = "0.5.4";
 
   src = fetchFromGitHub {
     owner = "nwg-piotr";
     repo = "nwg-panel";
     rev = "v${version}";
-    sha256 = "0i3g6brw8y17lzq6yzqc91x5w8na8wpqj57zq72zhgdji39n0g0d";
+    sha256 = "0vl9vfgpa1byf9vxn03j58h9zcbjd1y83b0kgv97cxm2mhqz12dg";
   };
 
   # No tests
