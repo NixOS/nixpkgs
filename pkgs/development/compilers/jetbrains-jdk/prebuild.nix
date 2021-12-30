b701063283e55a764761c4f28fc45b398ecc294c
Moritz Lutz <moritz.lutz@gmail.com>
2021-12-30 18:23:36 +0100

    added meta
    removed it because i thought it inherits from sourcebuild but brain lag ...

--- pkgs/development/compilers/jetbrains-jdk/prebuild.nix
+++ pkgs/development/compilers/jetbrains-jdk/prebuild.nix
@@ -20,7 +20,21 @@ openjdk11.overrideAttrs (oldAttrs: rec {
   '';
 
   meta = with lib; {
-    maintainers = with maintainers; [ lutzmor ];
+    description = "An OpenJDK fork to better support Jetbrains's products.";
+    longDescription = ''
+     JetBrains Runtime is a runtime environment for running IntelliJ Platform
+     based products on Windows, Mac OS X, and Linux. JetBrains Runtime is
+     based on OpenJDK project with some modifications. These modifications
+     include: Subpixel Anti-Aliasing, enhanced font rendering on Linux, HiDPI
+     support, ligatures, some fixes for native crashes not presented in
+     official build, and other small enhancements.
+
+     JetBrains Runtime is not a certified build of OpenJDK. Please, use at
+     your own risk.
+    '';
+    homepage = "https://confluence.jetbrains.com/display/JBR/JetBrains+Runtime";
+    inherit (openjdk11.meta) license platforms mainProgram;
+    maintainers = with maintainers; [ edwtjo petabyteboy ];
   };
 
   passthru = oldAttrs.passthru // {
