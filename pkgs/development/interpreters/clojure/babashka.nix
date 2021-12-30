192cd3cd39a90c590d2a35ae787c6755e3367ea6
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-12-30 12:56:47 -0300

    babashka: 0.7.0 -> 0.7.3

--- pkgs/development/interpreters/clojure/babashka.nix
+++ pkgs/development/interpreters/clojure/babashka.nix
@@ -2,11 +2,11 @@
 
 buildGraalvmNativeImage rec {
   pname = "babashka";
-  version = "0.7.0";
+  version = "0.7.3";
 
   src = fetchurl {
     url = "https://github.com/babashka/${pname}/releases/download/v${version}/${pname}-${version}-standalone.jar";
-    sha256 = "sha256-zSjiHacetJ68U0GciIbuGET9I/51EM8JnPPUGemDfEI=";
+    sha256 = "sha256-zbxFMc02hbsU2ERlUzqMBHwHYfORB7TkMINrKC52PPU=";
   };
 
   executable = "bb";
