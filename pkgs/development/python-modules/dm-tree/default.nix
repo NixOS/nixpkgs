5e3cbff2e07163dfed501b99132670cc56c1c8c4
Alexander Tsvyashchenko <ndl@endl.ch>
2021-12-30 18:17:07 +0100

    python3Packages.dm-tree: refactor to build from sources

--- pkgs/development/python-modules/dm-tree/default.nix
+++ pkgs/development/python-modules/dm-tree/default.nix
@@ -1,47 +1,56 @@
-{ autoPatchelfHook
+{ lib
+, cmake
+, fetchFromGitHub
 , buildPythonPackage
-, fetchPypi
-, isPy39
-, lib
-, six
-, stdenv
+, abseil-cpp
+, absl-py
+, attrs
+, numpy
+, pybind11
+, wrapt
 }:
 
 buildPythonPackage rec {
   pname = "dm-tree";
-  version = "0.1.6";
-  format = "wheel";
+  # As of 2021-12-29, the latest stable version still builds with Bazel.
+  version = "42e87fda83278e2eb32bb55225e1d1511e77c10c";
 
-  # At the time of writing (8/19/21), there are releases for 3.6-3.9. Supporting
-  # all of them is a pain, so we focus on 3.9, the current nixpkgs python3
-  # version.
-  disabled = !isPy39;
-
-  src = fetchPypi {
-    inherit version format;
-    sha256 = "1f71dy5xa5ywa5chbdhpdf8k0w1v9cvpn3qyk8nnjm79j90la9c4";
-    pname = "dm_tree";
-    dist = "cp39";
-    python = "cp39";
-    abi = "cp39";
-    platform = "manylinux_2_24_x86_64";
+  src = fetchFromGitHub {
+    owner = "deepmind";
+    repo = "tree";
+    rev = "${version}";
+    sha256 = "0bnvnn6m54jp5sr7lbvakdmhbrh2myv79ffmrgl7yjp13mz03lqi";
   };
 
-  # Prebuilt wheels are dynamically linked against things that nix can't find.
-  # Run `autoPatchelfHook` to automagically fix them.
-  nativeBuildInputs = [ autoPatchelfHook ];
-  # Dynamic link dependencies
-  buildInputs = [ stdenv.cc.cc ];
+  patches = [
+    ./cmake.patch
+  ];
 
-  propagatedBuildInputs = [ six ];
+  dontUseCmakeConfigure = true;
+
+  nativeBuildInputs = [
+    cmake
+    pybind11
+  ];
+
+  buildInputs = [
+    abseil-cpp
+    pybind11
+  ];
+
+  checkInputs = [
+    absl-py
+    attrs
+    numpy
+    wrapt
+  ];
 
   pythonImportsCheck = [ "tree" ];
 
   meta = with lib; {
     description = "Tree is a library for working with nested data structures.";
-    homepage    = "https://github.com/deepmind/tree";
-    license     = licenses.asl20;
-    maintainers = with maintainers; [ samuela ];
-    platforms = [ "x86_64-linux" ];
+    homepage = "https://github.com/deepmind/tree";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ samuela ndl ];
   };
 }
