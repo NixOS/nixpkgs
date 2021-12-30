c05389b3c595e7ba9d103f5c0924fb3f00722c37
Andreas Rammhold <andreas@rammhold.de>
2021-12-30 16:11:50 +0100

    libfido2: split outputs & remove static archive
    This helps cut down the closure of depending programs by a tiny but. We
    don't need the static library or the binaries in every closure of a
    program that depends on the library. We can bring back the static
    library as-needed (if needed).

--- pkgs/development/libraries/libfido2/default.nix
+++ pkgs/development/libraries/libfido2/default.nix
@@ -28,12 +28,16 @@ stdenv.mkDerivation rec {
 
   cmakeFlags = [
     "-DUDEV_RULES_DIR=${placeholder "out"}/etc/udev/rules.d"
-    "-DCMAKE_INSTALL_LIBDIR=lib"
+    "-DCMAKE_INSTALL_LIBDIR=${placeholder "lib"}/lib"
+    "-DBUILD_SHARED=ON"
+    "-DBUILD_STATIC=OFF"
   ] ++ lib.optionals stdenv.isDarwin [
     "-DUSE_HIDAPI=1"
   ] ++ lib.optionals stdenv.isLinux [
     "-DNFC_LINUX=1"
   ];
+
+  outputs = [ "out" "lib" "dev" ];
 
   meta = with lib; {
     description = ''
