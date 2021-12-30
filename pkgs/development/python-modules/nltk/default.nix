d58a493e94dc5014e6298c1ee6c755823c5791e2
R. Ryantm <ryantm-bot@ryantm.com>
2021-12-30 23:16:44 +0000

    python38Packages.nltk: 3.6.6 -> 3.6.7

--- pkgs/development/python-modules/nltk/default.nix
+++ pkgs/development/python-modules/nltk/default.nix
@@ -6,13 +6,13 @@
 }:
 
 buildPythonPackage rec {
-  version = "3.6.6";
+  version = "3.6.7";
   pname = "nltk";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "0f8ff4e261c78605bca284e8d2025e562304766156af32a1731f56b396ee364b";
+    sha256 = "51bf1aef5304740a708be7c8e683f7798f03dc5c7a7e7feb758be9e95f4585e3";
   };
 
   propagatedBuildInputs = [
