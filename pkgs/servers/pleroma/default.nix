99fc898a93964480ec4340eace064a2f826ae143
Finn Behrens <me@kloenk.dev>
2021-12-31 16:46:58 +0100

    pleroma: digest static files at build time

--- pkgs/servers/pleroma/default.nix
+++ pkgs/servers/pleroma/default.nix
@@ -199,6 +199,10 @@ beamPackages.mixRelease rec {
     });
   };
 
+  postBuild = ''
+    mix phx.digest --no-deps-check
+  '';
+
   passthru = {
     tests.pleroma = nixosTests.pleroma;
     inherit mixNixDeps;
