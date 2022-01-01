495c3025979b8daf40f40439dd29f1cd21447a96
Patrick Hilhorst <git@hilhorst.be>
2022-01-01 12:57:53 +0100

    pipectl: init at 0.2.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8627,6 +8627,8 @@ with pkgs;
 
   pipe-rename = callPackage ../tools/misc/pipe-rename { };
 
+  pipectl = callPackage ../tools/misc/pipectl { };
+
   pitivi = callPackage ../applications/video/pitivi { };
 
   prism = callPackage ../applications/video/prism { };
