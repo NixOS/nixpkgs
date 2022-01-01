3cd704d85fc1abdc8acc477e8e57bc0242bbf06b
legendofmiracles <legendofmiracles@protonmail.com>
2022-01-01 11:55:14 -0600

    wiki-tui: 0.4.2 -> 0.4.3

--- pkgs/misc/wiki-tui/default.nix
+++ pkgs/misc/wiki-tui/default.nix
@@ -1,25 +1,29 @@
-{ lib, rustPlatform, fetchFromGitHub, ncurses, openssl, pkg-config, stdenv, nix-update-script, Security }:
+{ lib, rustPlatform, fetchFromGitHub, ncurses, openssl, pkg-config, stdenv, Security, fetchpatch }:
 
 rustPlatform.buildRustPackage rec {
   pname = "wiki-tui";
-  version = "0.4.2";
+  version = "0.4.3";
 
   src = fetchFromGitHub {
     owner = "Builditluc";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-TWpCmHG07dv0/hXGpo71Ie0uDRqs6yywHzcv0Hpi8Sc=";
+    sha256 = "sha256-maN/0lJx6/lj3Zn+IZcPJFPPFVLbnpwxeMSTyzKYX6s=";
   };
+
+  # latest update forgot to include cargo.lock update
+  cargoPatches = [
+    (fetchpatch {
+      url = "https://github.com/Builditluc/wiki-tui/commit/87993eaca35a14afc1fb557482b099a6dd2da911.patch";
+      sha256 = "sha256-n04FCZwQ9pPanz9QQY/7Apyoy6lG0t/23S40p4c/TXw=";
+    })
+  ];
 
   buildInputs = [ ncurses openssl ] ++ lib.optional stdenv.isDarwin Security;
 
   nativeBuildInputs = [ pkg-config ];
 
-  cargoSha256 = "sha256-tJhbIsmh4zw1Dhvc2jE0N1cTE4//DOe3rDzDVNiTigA=";
-
-  passthru.updateScript = nix-update-script {
-    attrPath = pname;
-  };
+  cargoSha256 = "sha256-x4oS9IBF2GMcilv9Oi/IeFaCM3sxWn7PpkKcaeSqIog=";
 
   meta = with lib; {
     description = "A simple and easy to use Wikipedia Text User Interface";
