9bdfbf001039dff08b6f055603dd3ab5d0b4650a
Fabian Affolter <fabian@affolter-engineering.ch>
2022-01-01 10:31:18 +0100

    python3Packages.line_profiler: update meta

--- pkgs/development/python-modules/line_profiler/default.nix
+++ pkgs/development/python-modules/line_profiler/default.nix
@@ -7,14 +7,18 @@
 , python
 , scikit-build
 , cmake
+, pythonOlder
 }:
 
 buildPythonPackage rec {
-  pname = "line_profiler";
+  pname = "line-profiler";
   version = "3.4.0";
 
+  disabled = pythonOlder "3.6";
+
   src = fetchPypi {
-    inherit pname version;
+    pname = "line_profiler";
+    inherit version;
     sha256 = "b6b0a8100a2829358e31ef7c6f427b1dcf2b1d8e5d38b55b219719ecf758aee5";
   };
 
@@ -44,10 +48,10 @@ buildPythonPackage rec {
     PYTHONPATH=$out/${python.sitePackages}:$PYTHONPATH cd tests && ${python.interpreter} -m unittest discover -s .
   '';
 
-  meta = {
+  meta = with lib; {
     description = "Line-by-line profiler";
-    homepage = "https://github.com/rkern/line_profiler";
-    license = lib.licenses.bsd3;
-    maintainers = with lib.maintainers; [ fridh ];
+    homepage = "https://github.com/pyutils/line_profiler";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ fridh ];
   };
 }
