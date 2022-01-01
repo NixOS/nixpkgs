44866de4d11506208d7b0b5e35fe1ffaaa0516a2
Alexander Tsvyashchenko <ndl@endl.ch>
2022-01-01 22:35:46 +0100

    python3Packages.jmp: init at unstable-2021-10-03 (#152972)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4119,6 +4119,8 @@ in {
 
   jmespath = callPackage ../development/python-modules/jmespath { };
 
+  jmp = callPackage ../development/python-modules/jmp { };
+
   joblib = callPackage ../development/python-modules/joblib { };
 
   johnnycanencrypt = callPackage ../development/python-modules/johnnycanencrypt {
