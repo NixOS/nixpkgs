705b82d61fbf9aaf460fde0b35339ec2a3ed7240
R. Ryantm <ryantm-bot@ryantm.com>
2022-01-02 07:56:37 +0000

    ocamlPackages.ppx_fields_conv: 0.14.2 -> 0.15

--- pkgs/development/ocaml-modules/janestreet/0.14.nix
+++ pkgs/development/ocaml-modules/janestreet/0.14.nix
@@ -463,8 +463,8 @@ with self;
 
   ppx_fields_conv = janePackage {
     pname = "ppx_fields_conv";
-    version = "0.14.2";
-    hash = "1zwirwqry24b48bg7d4yc845hvcirxyymzbw95aaxdcck84d30n8";
+    version = "0.15";
+    hash = "sha256-5MDokQ3XAfvAxufhUKNuoHAT2ZVWKD7tE9nQ9G0naiY=";
     minimumOCamlVersion = "4.04.2";
     meta.description = "Generation of accessor and iteration functions for ocaml records";
     propagatedBuildInputs = [ fieldslib ppxlib ];
@@ -637,7 +637,7 @@ with self;
 
   ppx_typerep_conv = janePackage {
     pname = "ppx_typerep_conv";
-    version = "0.14.2";
+    version = "0.15";
     minimumOCamlVersion = "4.04.2";
     hash = "0yk9vkpnwr8labgfncqdi4rfkj88d8mb3cr8m4gdqpi3f2r27hf0";
     meta.description = "Generation of runtime types from type declarations";
@@ -646,7 +646,7 @@ with self;
 
   ppx_variants_conv = janePackage {
     pname = "ppx_variants_conv";
-    version = "0.14.2";
+    version = "0.15";
     minimumOCamlVersion = "4.04.2";
     hash = "1p11fiz4m160hs0xzg4g9rxchp053sz3s3d1lyciqixad1xi47a4";
     meta.description = "Generation of accessor and iteration functions for ocaml variant types";
