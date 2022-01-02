482634d5ca393aac313daee6a42b3ffc6454bc07
Robert Schütz <nix@dotlambda.de>
2022-01-02 00:24:45 +0000

    awscli: 1.22.14 -> 1.22.21

--- pkgs/tools/admin/awscli/default.nix
+++ pkgs/tools/admin/awscli/default.nix
@@ -35,11 +35,11 @@ let
 in
 with py.pkgs; buildPythonApplication rec {
   pname = "awscli";
-  version = "1.22.14"; # N.B: if you change this, change botocore and boto3 to a matching version too
+  version = "1.22.21"; # N.B: if you change this, change botocore and boto3 to a matching version too
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-FTGtUqdjZel8XqSrO3s3XQNqR6fyTO3mc1gyIQfk9n8=";
+    hash = "sha256-yzfy6MjXC6LeydLNVXQvcK4UmpVQP/jJ+W2jMgpNMgw=";
   };
 
   # https://github.com/aws/aws-cli/issues/4837
