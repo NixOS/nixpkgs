7fbd1bbb1129302fc6f71ae7548732a4ca4a0eba
Fabian Affolter <mail@fabian-affolter.ch>
2022-01-02 01:30:24 +0100

    python3Packages.pycognito: 2021.03.1 -> 2021.12.0

--- pkgs/development/python-modules/pycognito/default.nix
+++ pkgs/development/python-modules/pycognito/default.nix
@@ -3,23 +3,25 @@
 , buildPythonPackage
 , envs
 , fetchFromGitHub
-, isPy27
 , mock
 , pytestCheckHook
 , python-jose
+, pythonOlder
 , requests
 }:
 
 buildPythonPackage rec {
   pname = "pycognito";
-  version = "2021.03.1";
-  disabled = isPy27;
+  version = "2021.12.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "pvizeli";
     repo = pname;
     rev = version;
-    sha256 = "sha256-V3R6i1/FZrjcfRqJhczjURr/+x++iCvZ3aCK9wdEL1A=";
+    sha256 = "sha256-LHLFbr25DivdkdD2patGSljAf7VbeTh+DfmQqkbbKa0=";
   };
 
   propagatedBuildInputs = [
@@ -39,9 +41,13 @@ buildPythonPackage rec {
       --replace 'python-jose[cryptography]' 'python-jose'
   '';
 
-  pytestFlagsArray = [ "tests.py" ];
+  pytestFlagsArray = [
+    "tests.py"
+  ];
 
-  pythonImportsCheck = [ "pycognito" ];
+  pythonImportsCheck = [
+    "pycognito"
+  ];
 
   meta = with lib; {
     description = "Python class to integrate Boto3's Cognito client so it is easy to login users. With SRP support";
