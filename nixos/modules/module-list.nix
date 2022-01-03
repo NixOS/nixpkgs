a9efae4087938c77a668e78b9b13c8549474f9c4
George Shammas <george@shamm.as>
2022-01-03 12:07:17 -0500

    nixos/glorytun: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -748,6 +748,7 @@
   ./services/networking/ghostunnel.nix
   ./services/networking/git-daemon.nix
   ./services/networking/globalprotect-vpn.nix
+  ./services/networking/glorytun.nix
   ./services/networking/gnunet.nix
   ./services/networking/go-neb.nix
   ./services/networking/go-shadowsocks2.nix
