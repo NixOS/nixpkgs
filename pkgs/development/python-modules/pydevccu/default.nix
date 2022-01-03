69f136d18c59387f68f5159c810d5ad8d8a06423
Fabian Affolter <mail@fabian-affolter.ch>
2022-01-03 20:13:41 +0100

    python3Packages.pydevccu: fix file name

--- pkgs/development/python-modules/pydevccu/default.nix
+++ pkgs/development/python-modules/pydevccu/default.nix
@@ -18,6 +18,11 @@ buildPythonPackage rec {
     sha256 = "sha256-/4sJ5T17nCcTjg1Me4zTlOEOkK1py9kl2YeLGv4X6us=";
   };
 
+  postPatch = ''
+    # Fix file name, https://github.com/danielperna84/pydevccu/pull/8
+    mv pydevccu/paramset_descriptions/HmIP-STDH.json pydevccu/paramset_descriptions/HmIP-STHD.json
+  '';
+
   # Module has no tests
   doCheck = false;
 
