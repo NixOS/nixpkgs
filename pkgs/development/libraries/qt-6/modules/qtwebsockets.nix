e9516493b8b6de278d6d00b2f5db123c6e6edace
Milan Hauth <milahu@gmail.com>
2022-01-04 23:49:04 +0100

    qt6.qtwebsockets: init at 6.2.2

--- pkgs/development/libraries/qt-6/modules/qtwebsockets.nix
+++ pkgs/development/libraries/qt-6/modules/qtwebsockets.nix
@@ -1,7 +1,22 @@
-{ qtModule, qtbase, qtdeclarative }:
+{ qtModule
+, qtbase
+, qtdeclarative
+, openssl
+
+, libglvnd, libxkbcommon, vulkan-headers, libX11, libXcomposite
+}:
 
 qtModule {
   pname = "qtwebsockets";
   qtInputs = [ qtbase qtdeclarative ];
-  outputs = [ "out" "dev" "bin" ];
+  outputs = [ "out" "dev" ];
+  buildInputs = [
+    openssl
+    # FIXME should be propagated by qtbase
+    libglvnd libxkbcommon vulkan-headers libX11 libXcomposite
+  ];
+  # FIXME qt6: set this automatically
+  preConfigure = ''
+    export QT_ADDITIONAL_PACKAGES_PREFIX_PATH="${qtdeclarative.dev}"
+  '';
 }
