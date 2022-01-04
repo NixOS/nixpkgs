20a0975e268399d04cd261cd28170cf7bc26efdd
Milan Hauth <milahu@gmail.com>
2022-01-04 23:49:22 +0100

    qt6.qtwebchannel: init at 6.2.2

--- pkgs/development/libraries/qt-6/modules/qtwebchannel.nix
+++ pkgs/development/libraries/qt-6/modules/qtwebchannel.nix
@@ -1,8 +1,23 @@
-{ qtModule, qtbase, qtdeclarative }:
+{ qtModule
+, qtbase
+, qtdeclarative
+, qtwebsockets
+, openssl
+
+, libglvnd, libxkbcommon, vulkan-headers, libX11, libXcomposite
+}:
 
 qtModule {
   pname = "qtwebchannel";
-  qtInputs = [ qtbase qtdeclarative ];
-  outputs = [ "out" "dev" "bin" ];
+  qtInputs = [ qtbase qtdeclarative qtwebsockets ];
+  buildInputs = [
+    openssl
+    # FIXME should be propagated by qtbase
+    libglvnd libxkbcommon vulkan-headers libX11 libXcomposite
+  ];
+  outputs = [ "out" "dev" ];
+  # FIXME qt6: set this automatically
+  preConfigure = ''
+    export QT_ADDITIONAL_PACKAGES_PREFIX_PATH="${qtdeclarative.dev}:${qtwebsockets.dev}"
+  '';
 }
-
