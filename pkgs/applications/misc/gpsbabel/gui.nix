c0bd90086d872222cd6b65aef38b9b89d137e871
Jonathan Ringer <jonringer117@gmail.com>
2022-01-03 19:15:51 -0800

    gpsbable: make src independent

--- pkgs/applications/misc/gpsbabel/gui.nix
+++ pkgs/applications/misc/gpsbabel/gui.nix
@@ -5,7 +5,7 @@ mkDerivation {
 
   inherit (gpsbabel) src version;
 
-  sourceRoot = "${src.name}/gui";
+  prePatch = "cd gui";
 
   nativeBuildInputs = [ qmake qttools ];
   buildInputs = [ qtwebkit ];
