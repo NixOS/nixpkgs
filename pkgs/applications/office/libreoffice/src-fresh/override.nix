1f64b214291bb25d27b8643d6f28ca7543443309
TredwellGit <tredwell@tutanota.com>
2022-01-04 22:09:36 +0000

    libreoffice-fresh: fix build
    Fixes https://github.com/NixOS/nixpkgs/issues/153436

--- pkgs/applications/office/libreoffice/src-fresh/override.nix
+++ pkgs/applications/office/libreoffice/src-fresh/override.nix
@@ -4,6 +4,7 @@ attrs:
   postConfigure = attrs.postConfigure + ''
     sed -e '/CPPUNIT_TEST(Import_Export_Import);/d' -i './sw/qa/inc/swmodeltestbase.hxx'
   '';
+  doCheck = false;
   configureFlags = attrs.configureFlags ++ [
     (lib.enableFeature kdeIntegration "kf5")
     "--without-system-zxing"
