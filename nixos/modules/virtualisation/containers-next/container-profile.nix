f93309de4d435cc0a804370ae1231b44bce17bdc
Maximilian Bosch <maximilian@mbosch.me>
2022-01-04 13:03:22 +0100

    nixos/containers-next: fix `systemd-networkd-wait-online.service` hanging indefinetely
    See https://github.com/NixOS/nixpkgs/pull/140669#discussion_r768911989
    for further context.
    
    Co-authored-by: Franz Pletz <fpletz@fnordicwalking.de>
    Co-authored-by: zseri <zseri.devel@ytrizja.de>

--- nixos/modules/virtualisation/containers-next/container-profile.nix
+++ nixos/modules/virtualisation/containers-next/container-profile.nix
@@ -7,18 +7,6 @@
     # `/nix/store`) and this doesn't like sudo.
     security.sudo.package = mkDefault pkgs.sudo-nspawn;
 
-    # FIXME get rid of this hack!
-    # On a test-system I experienced that this service was hanging for no reason.
-    # After a config-activation in ExecReload which affected larger parts of the OS in the
-    # container, `nixops` waited until the timeout was reached. However, the networkd
-    # was routable and the `host0` interface reached the state `configured`. Hence I'd guess
-    # that this is a networkd bug that requires investigation. Until then, I'll leave
-    # this as-is.
-    systemd.services.systemd-networkd-wait-online.serviceConfig.ExecStart = mkForce [
-      ""
-      "/run/current-system/sw/bin/true"
-    ];
-
     # Containers are supposed to use systemd-networkd to have a proper
     # networking stack even during boot-up.
     networking = {
@@ -31,7 +19,6 @@
         Virtualization = "container";
         Name = "host0";
       };
-      linkConfig.RequiredForOnline = "no";
       dhcpConfig.UseTimezone = "yes";
       networkConfig = {
         DHCP = "yes";
