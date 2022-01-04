6c46314a203a40b80c04f820bccc108ae0109be6
R. Ryantm <ryantm-bot@ryantm.com>
2022-01-04 06:18:58 +0000

    jc: 1.17.5 -> 1.17.6

--- pkgs/development/python-modules/jc/default.nix
+++ pkgs/development/python-modules/jc/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "jc";
-  version = "1.17.5";
+  version = "1.17.6";
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "kellyjonbrazil";
     repo = pname;
     rev = "v${version}";
-    sha256 = "004773a1wsip1gnqvas78k0snv7yq83qv1spir891sz4mmg7fyin";
+    sha256 = "sha256-JWipvhMsVI/AX7Pj4iHJh5GIgfwGLc7s1hZZJnsYGIw=";
   };
 
   propagatedBuildInputs = [ ruamel-yaml xmltodict pygments ];
