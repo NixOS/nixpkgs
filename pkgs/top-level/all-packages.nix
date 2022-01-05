1e674af07ccb89c1d8388d1e5a01a2584b214206
Moritz Clasmeier <moritz@stackrox.com>
2022-01-05 11:08:21 +0100

    kube-linter: init at 0.2.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14820,6 +14820,8 @@ with pkgs;
 
   kubectx = callPackage ../development/tools/kubectx { };
 
+  kube-linter = callPackage ../development/tools/kube-linter { };
+
   kube-prompt = callPackage ../development/tools/kube-prompt { };
 
   kubei = callPackage ../tools/security/kubei { };
