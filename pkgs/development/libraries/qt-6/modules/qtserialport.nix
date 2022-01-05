12a126310ee5fb17c0d38db6de73fb1b5f9373b4
Milan Hauth <milahu@gmail.com>
2022-01-05 01:02:10 +0100

    qt6.qtserialport: init at 6.2.2

--- pkgs/development/libraries/qt-6/modules/qtserialport.nix
+++ pkgs/development/libraries/qt-6/modules/qtserialport.nix
@@ -1,7 +1,23 @@
-{ qtModule, stdenv, lib, qtbase, systemd }:
+{ qtModule
+, stdenv
+, lib
+, qtbase
+, systemd
+, pkg-config
+
+, libglvnd, libxkbcommon, vulkan-headers, libX11, libXcomposite
+}:
 
 qtModule {
   pname = "qtserialport";
   qtInputs = [ qtbase ];
   NIX_CFLAGS_COMPILE = lib.optionalString stdenv.isLinux "-DNIXPKGS_LIBUDEV=\"${lib.getLib systemd}/lib/libudev\"";
+
+  nativeBuildInputs = [ pkg-config ]; # find systemd = udev
+
+  buildInputs = [
+    systemd
+    # FIXME should be propagated by qtbase
+    libglvnd libxkbcommon vulkan-headers libX11 libXcomposite
+  ];
 }
