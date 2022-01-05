NixOS/nixpkgs

commit ff7990087c63623eb31ef8e22bcfdc2254927cb2

python3Packages.nidaqmx: mark disabled for 3.10+

pkgs/development/python-modules/nidaqmx/default.nix
@@ -7,6 +7,7 @@
 , pykka
 , enum34
 , pythonOlder
+, pythonAtLeast
 }:
 
 # Note we currently do not patch the path to the drivers
@@ -16,6 +17,9 @@
 buildPythonPackage rec {
   pname = "nidaqmx";
   version = src.rev;
+
+  # 3.10 is not supported, upstream inactive
+  disabled = pythonAtleast "3.10";
 
   src = fetchFromGitHub {
     owner = "ni";

commit fba4cce694d76ec190c28bfd8365cb4da8503ffa

python3Packages.nidaqmx: fix eval failing due to misspelling
Introduced in commit ff7990087c63623eb31ef8e22bcfdc22.

pkgs/development/python-modules/nidaqmx/default.nix
@@ -19,7 +19,7 @@ buildPythonPackage rec {
   version = src.rev;
 
   # 3.10 is not supported, upstream inactive
-  disabled = pythonAtleast "3.10";
+  disabled = pythonAtLeast "3.10";
 
   src = fetchFromGitHub {
     owner = "ni";