1a0ed943e97747c3a4e1b52174194bc8042a3235
Daniel Olsen <daniel.olsen99@gmail.com>
2022-01-07 00:56:51 +0100

    hydrus: 467 -> 468
    (cherry picked from commit 0e5389c1f8449d1a1cba9fab479f4452fdb32bbe)

--- pkgs/applications/graphics/hydrus/default.nix
+++ pkgs/applications/graphics/hydrus/default.nix
@@ -10,14 +10,14 @@
 
 python3Packages.buildPythonPackage rec {
   pname = "hydrus";
-  version = "467";
+  version = "468";
   format = "other";
 
   src = fetchFromGitHub {
     owner = "hydrusnetwork";
     repo = "hydrus";
     rev = "v${version}";
-    sha256 = "sha256-ijIOCabpnaK9ww1cR+HNpCOn8uSwSEuyLWwnT2ypdD4=";
+    sha256 = "sha256-3G5Lxzt1zy51o5ugx14mKR+HXrk5pt0IwmO77Guqexk=";
   };
 
   nativeBuildInputs = [
