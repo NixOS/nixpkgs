789434b790d4cade0eceaff4716fe53b24223072
Fabian Affolter <fabian@affolter-engineering.ch>
2022-01-07 10:02:01 +0100

    python3Packages.striprtf: add pythonImportsCheck

--- pkgs/development/python-modules/striprtf/default.nix
+++ pkgs/development/python-modules/striprtf/default.nix
@@ -6,11 +6,16 @@
 buildPythonPackage rec {
   pname = "striprtf";
   version = "0.0.19";
+  format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "b7f15e11306e466dbe91665409233a06d9fdb4ee156489a3d879579891b04c25";
   };
+
+  pythonImportsCheck = [
+    "striprtf"
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/joshy/striprtf";
