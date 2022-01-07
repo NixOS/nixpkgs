56238790ce9048b03463383828708dbb6d1a638e
sternenseemann <sternenseemann@systemli.org>
2022-01-07 12:58:21 +0100

    llvmPackages_*.clang: pick clangUseLLVM if targetPlatform.useLLVM
    libcxxClang still depends on cc wrapper's gccForLibs for libgcc which is
    not available when useLLVM is set. In such cases we need to switch to
    clangUseLLVM and (try) to use compiler-rt instead.
    
    Resolves #153759: pkgsLLVM.llvmPackages.stdenv now correctly
    clangUseLLVM as cc, allowing compilation to work as expected.
    
    (cherry picked from commit e238f456b8d643d5afe5370e942e82204907d0ef)

--- pkgs/development/compilers/llvm/5/default.nix
+++ pkgs/development/compilers/llvm/5/default.nix
@@ -67,8 +67,8 @@ let
 
     # pick clang appropriate for package set we are targeting
     clang =
-      if (pkgs.targetPackages.stdenv or stdenv).cc.isGNU
-      then tools.libstdcxxClang
+      /**/ if stdenv.targetPlatform.useLLVM or false then tools.clangUseLLVM
+      else if (pkgs.targetPackages.stdenv or stdenv).cc.isGNU then tools.libstdcxxClang
       else tools.libcxxClang;
 
     libstdcxxClang = wrapCCWith rec {
