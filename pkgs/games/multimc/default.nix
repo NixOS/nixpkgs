5b254c17cfe87af57fbca4649e19301ee81d6ff2
Mustafa Çalışkan <muscaln@protonmail.com>
2022-01-08 10:03:20 +0300

    multimc: format

--- pkgs/games/multimc/default.nix
+++ pkgs/games/multimc/default.nix
@@ -1,10 +1,35 @@
-{ lib, mkDerivation, fetchFromGitHub, cmake, jdk8, jdk, zlib, file, makeWrapper, xorg, libpulseaudio, qtbase, libGL, msaClientID ? "" }:
+{ lib
+, mkDerivation
+, fetchFromGitHub
+, cmake
+, jdk8
+, jdk
+, zlib
+, file
+, makeWrapper
+, xorg
+, libpulseaudio
+, qtbase
+, libGL
+, msaClientID ? ""
+}:
 
 let
-  libpath = with xorg; lib.makeLibraryPath [ libX11 libXext libXcursor libXrandr libXxf86vm libpulseaudio libGL ];
-in mkDerivation rec {
+  gameLibraryPath = with xorg; lib.makeLibraryPath [
+    libX11
+    libXext
+    libXcursor
+    libXrandr
+    libXxf86vm
+    libpulseaudio
+    libGL
+  ];
+in
+  
+mkDerivation rec {
   pname = "multimc";
   version = "unstable-2021-09-08";
+  
   src = fetchFromGitHub {
     owner = "MultiMC";
     repo = "MultiMC5";
@@ -12,6 +37,7 @@ in mkDerivation rec {
     sha256 = "3G9QPoAbC+uVfUYR0Kq6hnxl9c2mvCzIEYGjwfarQJ8=";
     fetchSubmodules = true;
   };
+  
   nativeBuildInputs = [ cmake file makeWrapper ];
   buildInputs = [ qtbase jdk8 zlib ];
 
@@ -36,7 +62,7 @@ in mkDerivation rec {
 
     # xorg.xrandr needed for LWJGL [2.9.2, 3) https://github.com/LWJGL/lwjgl/issues/128
     wrapProgram $out/bin/multimc \
-      --set GAME_LIBRARY_PATH /run/opengl-driver/lib:${libpath} \
+      --set GAME_LIBRARY_PATH /run/opengl-driver/lib:${gameLibraryPath} \
       --prefix PATH : ${lib.makeBinPath [ xorg.xrandr ]}
   '';
 
@@ -44,7 +70,9 @@ in mkDerivation rec {
     homepage = "https://multimc.org/";
     description = "A free, open source launcher for Minecraft";
     longDescription = ''
-      Allows you to have multiple, separate instances of Minecraft (each with their own mods, texture packs, saves, etc) and helps you manage them and their associated options with a simple interface.
+      Allows you to have multiple, separate instances of
+      Minecraft (each with their own mods, texture packs, saves, etc)
+      and helps you manage them and their associated options with a simple interface.
     '';
     platforms = platforms.linux;
     license = licenses.asl20;
