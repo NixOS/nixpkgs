ca3bc92bc48aa13dcd5d6a59d57c1d3493a222e5
Ilan Joselevich <personal@ilanjoselevich.com>
2022-01-09 22:31:50 +0200

    legendary-gl: 0.20.18 -> 0.20.24

--- pkgs/games/legendary-gl/default.nix
+++ pkgs/games/legendary-gl/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonApplication rec {
   pname = "legendary-gl"; # Name in pypi
-  version = "0.20.18";
+  version = "0.20.24";
 
   src = fetchFromGitHub {
     owner = "derrod";
     repo = "legendary";
     rev = version;
-    sha256 = "0d31c8grvcw7y3sh2x90cxhj612k6f491w2r12j1q33d2v9sqm4j";
+    sha256 = "sha256-4VN/2FoAXTaumPsplV9wgdUSQsFitC1hqWveAW/yt58=";
   };
 
   propagatedBuildInputs = [ requests ];
