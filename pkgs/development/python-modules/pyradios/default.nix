37bb0ba9f41a2825bb9821d20cf7c72c76344565
R. Ryantm <ryantm-bot@ryantm.com>
2022-01-09 03:11:24 +0000

    python38Packages.pyradios: 0.0.22 -> 1.0.1

--- pkgs/development/python-modules/pyradios/default.nix
+++ pkgs/development/python-modules/pyradios/default.nix
@@ -1,11 +1,11 @@
 { lib, buildPythonPackage, fetchPypi, appdirs, requests }:
 buildPythonPackage rec {
   pname = "pyradios";
-  version = "0.0.22";
+  version = "1.0.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1bgfb8vz7jybswss16pdzns0qpqfrwa9f2g8qrh1r4mig4xh2dmi";
+    sha256 = "0fd3b234c635d9e628bdadb9dc3a820405631b54977402719a641d2e1cc3f7b6";
   };
 
   propagatedBuildInputs = [
