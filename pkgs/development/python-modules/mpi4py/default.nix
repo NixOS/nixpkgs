b37f9552574c20f84f42ccdf01c700e95c2a5ea6
Robert T. McGibbon <rmcgibbo@gmail.com>
2022-01-09 17:39:59 -0500

    python3Packages.mpi4py: 3.0.3 -> 3.1.3

--- pkgs/development/python-modules/mpi4py/default.nix
+++ pkgs/development/python-modules/mpi4py/default.nix
@@ -1,19 +1,21 @@
-{ lib, fetchPypi, fetchpatch, python, buildPythonPackage, mpi, openssh }:
+{ lib
+, fetchFromGitHub
+, python
+, cython
+, buildPythonPackage
+, mpi
+, openssh }:
 
 buildPythonPackage rec {
   pname = "mpi4py";
-  version = "3.0.3";
+  version = "3.1.3";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "012d716c8b9ed1e513fcc4b18e5af16a8791f51e6d1716baccf988ad355c5a1f";
+  src = fetchFromGitHub {
+    owner = "mpi4py";
+    repo = "mpi4py";
+    rev = version;
+    sha256 = "sha256-CUpRh63xmqRUveVGJoHVaOdrqDM3D+P14aV0IrwJIVw=";
   };
-
-  patches = [ (fetchpatch {
-    name = "disable-broken-test"; # upstream patch
-    url = "https://github.com/mpi4py/mpi4py/commit/e13cc3ee59ec6ec2c6ee20e384e1e649d5027e8a.patch";
-    sha256 = "0iwknrhxnfmsqjj8ahpn50c8pcdyv9p3wmcqi1jhr4i5y7lnmvvx";
-  })];
 
   passthru = {
     inherit mpi;
@@ -23,31 +25,12 @@ buildPythonPackage rec {
     substituteInPlace test/test_spawn.py --replace \
                       "unittest.skipMPI('openmpi(<3.0.0)')" \
                       "unittest.skipMPI('openmpi')"
-  '';
-
-  configurePhase = "";
-
-  installPhase = ''
-    mkdir -p "$out/lib/${python.libPrefix}/site-packages"
-    export PYTHONPATH="$out/lib/${python.libPrefix}/site-packages:$PYTHONPATH"
-
-    ${python}/bin/${python.executable} setup.py install \
-      --install-lib=$out/lib/${python.libPrefix}/site-packages \
-      --prefix="$out"
-
-    # --install-lib:
-    # sometimes packages specify where files should be installed outside the usual
-    # python lib prefix, we override that back so all infrastructure (setup hooks)
-    # work as expected
-
-    # Needed to run the tests reliably. See:
-    # https://bitbucket.org/mpi4py/mpi4py/issues/87/multiple-test-errors-with-openmpi-30
-    export OMPI_MCA_rmaps_base_oversubscribe=yes
+    conf/cythonize.sh
   '';
 
   setupPyBuildFlags = ["--mpicc=${mpi}/bin/mpicc"];
 
-  nativeBuildInputs = [ mpi openssh ];
+  nativeBuildInputs = [ cython mpi openssh ];
 
   meta = with lib; {
     description = "Python bindings for the Message Passing Interface standard";
