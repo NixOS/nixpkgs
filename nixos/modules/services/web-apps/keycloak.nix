67bcf3575603008a20cfeed17923ea54881b6db2
Nikolay Amiantov <ab@fmap.me>
2022-01-10 00:42:23 +0300

    keycloak service: use 'attrsOf anything' for extraConfig
    (cherry picked from commit a42abe27c0b58749f1c563fc77305d145c739746)

--- nixos/modules/services/web-apps/keycloak.nix
+++ nixos/modules/services/web-apps/keycloak.nix
@@ -227,7 +227,7 @@ in
     };
 
     extraConfig = lib.mkOption {
-      type = lib.types.attrs;
+      type = lib.types.attrsOf lib.types.anything;
       default = { };
       example = lib.literalExpression ''
         {
