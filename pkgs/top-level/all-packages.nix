addbaf25a16c9edd48d426c2fc07ef8abdd8cc8d
zowoq <59103226+zowoq@users.noreply.github.com>
2022-01-09 17:27:12 +1000

    terraform-providers: updates, switch go_1_17

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33506,7 +33506,9 @@ with pkgs;
   terraform-full = terraform.full;
 
   terraform-providers = recurseIntoAttrs (
-    callPackage ../applications/networking/cluster/terraform-providers {}
+    callPackage ../applications/networking/cluster/terraform-providers {
+      buildGoModule = buildGo117Module;
+    }
   );
 
   terraforming = callPackage ../applications/networking/cluster/terraforming { };
