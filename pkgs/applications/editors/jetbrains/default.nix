af8f7432763798c5dea7cf176e073250aa2902c9
diogox <diogoxavierpinto@gmail.com>
2022-01-09 17:35:01 +0000

    Update pkgs/applications/editors/jetbrains/default.nix
    Co-authored-by: Jörg Thalheim <Mic92@users.noreply.github.com>
    (cherry picked from commit 4cb152fceaf8d3a93227b6965325fc81a689951c)

--- pkgs/applications/editors/jetbrains/default.nix
+++ pkgs/applications/editors/jetbrains/default.nix
@@ -93,7 +93,7 @@ let
 
         # fortify source breaks build since delve compiles with -O0
         wrapProgram $out/bin/goland \
-          --set CGO_CPPFLAGS "-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0"
+          --prefix CGO_CPPFLAGS " " "-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0"
       '';
     });
 
