265a3053596e59815e728e4409e5d9663ac7ab6c
Alyssa Ross <hi@alyssa.is>
2022-01-10 03:04:08 +0000

    linuxPackages.vhba: 20211023 -> 20211218
    This release adds Linux 5.16 compatibility.

--- pkgs/misc/emulators/cdemu/vhba.nix
+++ pkgs/misc/emulators/cdemu/vhba.nix
@@ -1,12 +1,12 @@
-{ lib, stdenv, fetchurl, kernel }:
+{ lib, stdenv, fetchurl, fetchpatch, kernel }:
 
 stdenv.mkDerivation rec {
   pname = "vhba";
-  version = "20211023";
+  version = "20211218";
 
   src  = fetchurl {
     url = "mirror://sourceforge/cdemu/vhba-module-${version}.tar.xz";
-    sha256 = "sha256-YAh7qqkozvoG1WhHBv7z1IcSrP75LLMq/FB6sZrevxA=";
+    sha256 = "1dkprnnya0i8121p7ip4g8cww99drk7fzbwcxx65x02jqk0siibj";
   };
 
   makeFlags = [ "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build" "INSTALL_MOD_PATH=$(out)" ];
