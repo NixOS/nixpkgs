4e6877097b1d54034d40c0c1d90010156761e70e
Denys Pavlov <me@denys.me>
2022-01-10 16:16:09 +0000

    python3Packages.sqlite-utils: 3.19 -> 3.22
    Clean up package:
    - remove test patch, as it's included in 3.20 (simonw/sqlite-utils@ee13f98c2c7ca3b819bd0fc55da3108cb6a6434a)
    - pytest-runner is removed from setup.py in (simonw/sqlite-utils@f3fd8613113d21d44238a6ec54b375f5aa72c4e0)
    - replace `dateutils` with `python-dateutil` (simonw/sqlite-utils@bc4c42d68879c710c851dba3c98deda96ca6caa8)

--- pkgs/development/python-modules/sqlite-utils/default.nix
+++ pkgs/development/python-modules/sqlite-utils/default.nix
@@ -5,7 +5,7 @@
 , pythonOlder
 , click
 , click-default-group
-, dateutils
+, python-dateutil
 , sqlite-fts4
 , tabulate
 , pytestCheckHook
@@ -14,32 +14,18 @@
 
 buildPythonPackage rec {
   pname = "sqlite-utils";
-  version = "3.19";
+  version = "3.22";
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "509099fce5f25faada6e76b6fb90e8ef5ba0f1715177933a816718be0c8e7244";
+    sha256 = "24803ea4d63e2123d2040db2da43fea95fabada80e1af1fe1da69643ae376689";
   };
-
-  patches = [
-    # https://github.com/simonw/sqlite-utils/pull/347
-    (fetchpatch {
-      name = "sqlite-utils-better-test_rebuild_fts.patch";
-      url = "https://github.com/simonw/sqlite-utils/pull/347/commits/1a7ef2fe2064ace01d5535fb771f941296fb642a.diff";
-      sha256 = "sha256-WKCQGMqr8WYjG7cmAH5pYBhgikowbt3r6hObwtMDDUY=";
-    })
-  ];
-
-  postPatch = ''
-    substituteInPlace setup.py \
-      --replace '"pytest-runner"' ""
-  '';
 
   propagatedBuildInputs = [
     click
     click-default-group
-    dateutils
+    python-dateutil
     sqlite-fts4
     tabulate
   ];
