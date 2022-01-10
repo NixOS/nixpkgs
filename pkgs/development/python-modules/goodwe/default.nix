c91c09ec25605c22d570c32dbb3944cb00528ed2
R. Ryantm <ryantm-bot@ryantm.com>
2022-01-10 06:03:29 +0000

    python310Packages.goodwe: 0.2.12 -> 0.2.13

--- pkgs/development/python-modules/goodwe/default.nix
+++ pkgs/development/python-modules/goodwe/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "goodwe";
-  version = "0.2.12";
+  version = "0.2.13";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -16,7 +16,7 @@ buildPythonPackage rec {
     owner = "marcelblijleven";
     repo = pname;
     rev = "v${version}";
-    sha256 = "153qv1x7yphfpf2nkcbd4fl6i7fjc3j5dvmyr7f59f1cm469sp10";
+    sha256 = "189szff4sl5pc670wv8syl6zcv3p748s33w11biig0vbd59kdr1l";
   };
 
   checkInputs = [
