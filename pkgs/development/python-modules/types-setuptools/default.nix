3fcdc51354a3e919dfe50b8ab04d525c67e64d55
Fabian Affolter <mail@fabian-affolter.ch>
2022-01-11 23:23:07 +0100

    python3Packages.types-setuptools: 57.4.6 -> 57.4.7

--- pkgs/development/python-modules/types-setuptools/default.nix
+++ pkgs/development/python-modules/types-setuptools/default.nix
@@ -5,12 +5,12 @@
 
 buildPythonPackage rec {
   pname = "types-setuptools";
-  version = "57.4.6";
+  version = "57.4.7";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "65ef8946fc31aed946177629e681861217aceb8fc9b75a44ba987d7eea9388aa";
+    sha256 = "sha256-lnfZabAOwcFFUvW+KytHpvvqTQ7U3g/c7hir2qDMkmc=";
   };
 
   # Module doesn't have tests
