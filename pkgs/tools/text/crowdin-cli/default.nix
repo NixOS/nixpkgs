24fa256045310dac062f09989620c11ababc8450
R. Ryantm <ryantm-bot@ryantm.com>
2022-01-11 05:17:41 +0000

    crowdin-cli: 3.7.4 -> 3.7.5

--- pkgs/tools/text/crowdin-cli/default.nix
+++ pkgs/tools/text/crowdin-cli/default.nix
@@ -14,11 +14,11 @@
 
 stdenv.mkDerivation rec {
   pname = "crowdin-cli";
-  version = "3.7.4";
+  version = "3.7.5";
 
   src = fetchurl {
     url = "https://github.com/crowdin/${pname}/releases/download/${version}/${pname}.zip";
-    sha256 = "sha256-zsd95dkKzuhqtWFwc84tjZ05MnzE25UvfF459gfp+lA=";
+    sha256 = "sha256-p2lfE3fxUpgTGgIP6KojQ5uC3kF7KWDIU2ILpi90Sso=";
   };
 
   nativeBuildInputs = [ installShellFiles makeWrapper unzip ];
