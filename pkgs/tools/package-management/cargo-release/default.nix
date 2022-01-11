18ec76afecd588a9b06e305a967e9b3ef6f3b6e3
R. Ryantm <ryantm-bot@ryantm.com>
2022-01-11 03:23:17 +0000

    cargo-release: 0.18.6 -> 0.19.0

--- pkgs/tools/package-management/cargo-release/default.nix
+++ pkgs/tools/package-management/cargo-release/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-release";
-  version = "0.18.6";
+  version = "0.19.0";
 
   src = fetchFromGitHub {
     owner = "crate-ci";
     repo = "cargo-release";
     rev = "v${version}";
-    sha256 = "sha256-4ZXur3Hja5nlJ2GLGei9r7ZPz+bvw41rm9xzaIo68gw=";
+    sha256 = "sha256-u1XXkenMIflbXACxOeeq7Mg5ubel4oFXpL/uy6McQf0=";
   };
 
-  cargoSha256 = "sha256-vBr3RYBFoeCJquS8ugVpl29J7CUraN/HuxtKv8Dqi/k=";
+  cargoSha256 = "sha256-7QOaSEaX2JXtTQQo3RXbMpZg6V2wzfoQbId9QOD9sCA=";
 
   nativeBuildInputs = [ pkg-config ];
 
