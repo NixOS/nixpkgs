6b98aa0d5469f20c01835bbf36f0400e19ff0c08
Fabian Affolter <mail@fabian-affolter.ch>
2022-01-11 09:28:14 +0100

    python3Packages.requests-toolbelt: disable warnings

--- pkgs/development/python-modules/requests-toolbelt/default.nix
+++ pkgs/development/python-modules/requests-toolbelt/default.nix
@@ -1,6 +1,7 @@
 { lib
 , betamax
 , buildPythonPackage
+, fetchpatch
 , fetchPypi
 , mock
 , pyopenssl
@@ -29,7 +30,17 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
+  patches = [
+    (fetchpatch {
+      # Fix collections.abc deprecation warning, https://github.com/requests/toolbelt/pull/246
+      name = "fix-collections-abc-deprecation.patch";
+      url = "https://github.com/requests/toolbelt/commit/7188b06330e5260be20bce8cbcf0d5ae44e34eaf.patch";
+      sha256 = "sha256-pRkG77sNglG/KsRX6JaPgk4QxmmSBXypFRp/vNA3ot4=";
+    })
+  ];
+
   disabledTests = [
+    # https://github.com/requests/toolbelt/issues/306
     "test_no_content_length_header"
     "test_read_file"
     "test_reads_file_from_url_wrapper"
@@ -41,10 +52,10 @@ buildPythonPackage rec {
     "requests_toolbelt"
   ];
 
-  meta = {
+  meta = with lib; {
     description = "Toolbelt of useful classes and functions to be used with requests";
     homepage = "http://toolbelt.rtfd.org";
-    license = lib.licenses.asl20;
-    maintainers = with lib.maintainers; [ matthiasbeyer ];
+    license = licenses.asl20;
+    maintainers = with maintainers; [ matthiasbeyer ];
   };
 }
