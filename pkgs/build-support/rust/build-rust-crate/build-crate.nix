20c39449eee2be0594f6d5fbfb2bfcaa79e0ef26
John Ericson <John.Ericson@Obsidian.Systems>
2022-01-12 08:48:49 +0000

    buildRustCrate: Add support for standard library deps
    We are replicating one mechanism behind `-Z build-std`.
    
    There isn't yet crate2nix support for this, but one can (and I do) add
    the missing stdlib deps (for this feature to pick up) with overrides.
    
    (cherry picked from commit cc29693a0979c3b81da6942c214841ef11de95b5)
    (cherry picked from commit 5aef865cefc2a906296b9a256bf597f70e4ee243)

--- pkgs/build-support/rust/build-rust-crate/build-crate.nix
+++ pkgs/build-support/rust/build-rust-crate/build-crate.nix
@@ -1,4 +1,8 @@
-{ lib, stdenv, mkRustcDepArgs, mkRustcFeatureArgs, rust }:
+{ lib, stdenv
+, mkRustcDepArgs, mkRustcFeatureArgs, needUnstableCLI
+, rust
+}:
+
 { crateName,
   dependencies,
   crateFeatures, crateRenames, libName, release, libPath,
@@ -17,6 +21,8 @@
         (mkRustcFeatureArgs crateFeatures)
       ] ++ lib.optionals (stdenv.hostPlatform != stdenv.buildPlatform) [
         "--target" (rust.toRustTargetSpec stdenv.hostPlatform)
+      ] ++ lib.optionals (needUnstableCLI dependencies) [
+        "-Z" "unstable-options"
       ] ++ extraRustcOpts
       # since rustc 1.42 the "proc_macro" crate is part of the default crate prelude
       # https://github.com/rust-lang/cargo/commit/4d64eb99a4#diff-7f98585dbf9d30aa100c8318e2c77e79R1021-R1022
