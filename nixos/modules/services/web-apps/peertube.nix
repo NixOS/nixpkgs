f9065c605692195325e0a44f280e14a4932b8d25
Izorkin <izorkin@elven.pw>
2022-01-13 21:21:06 +0300

    nixos/peertube: fix youtube-dl import
    (cherry picked from commit c2296c3ec277d21786ddd289dca93b1433f277e8)

--- nixos/modules/services/web-apps/peertube.nix
+++ nixos/modules/services/web-apps/peertube.nix
@@ -302,6 +302,7 @@ in {
         };
         storage = {
           tmp = lib.mkDefault "/var/lib/peertube/storage/tmp/";
+          bin = lib.mkDefault "/var/lib/peertube/storage/bin/";
           avatars = lib.mkDefault "/var/lib/peertube/storage/avatars/";
           videos = lib.mkDefault "/var/lib/peertube/storage/videos/";
           streaming_playlists = lib.mkDefault "/var/lib/peertube/storage/streaming-playlists/";
@@ -362,7 +363,7 @@ in {
 
       environment = env;
 
-      path = with pkgs; [ bashInteractive ffmpeg nodejs-16_x openssl yarn youtube-dl ];
+      path = with pkgs; [ bashInteractive ffmpeg nodejs-16_x openssl yarn python3 ];
 
       script = ''
         #!/bin/sh
