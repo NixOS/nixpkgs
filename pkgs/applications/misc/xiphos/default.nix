569eea8ec1ea42a83dda23fe00bc6c0d080bc93f
Jan Tojnar <jtojnar@gmail.com>
2022-01-13 21:27:26 +0100

    xiphos: do not use patchFlags
    Those apply to all patches making applying patches in the future problematic.
    
    Also pin the patch while at it.

--- pkgs/applications/misc/xiphos/default.nix
+++ pkgs/applications/misc/xiphos/default.nix
@@ -65,12 +65,11 @@ stdenv.mkDerivation rec {
     # https://github.com/crosswire/xiphos/issues/1083#issuecomment-820304874
     (fetchpatch {
       name ="xiphos-glibc.patch";
-      url = "https://aur.archlinux.org/cgit/aur.git/plain/xiphos-glibc.patch?h=xiphos";
-      sha256 = "sha256-0WadztJKXW2adqsDP8iSAYVShbdqHoDvP+aVJC0cQB0=";
+      url = "https://aur.archlinux.org/cgit/aur.git/plain/xiphos-glibc.patch?h=xiphos&id=bb816f43ba764ffac1287ab1e2a649c2443e3ce8";
+      sha256 = "he3U7phU2/QCrZidHviupA7YwzudnQ9Jbb8eMZw6/ck=";
+      extraPrefix = "";
     })
   ];
-
-  patchFlags = [ "-p0" ];
 
   nativeBuildInputs = [
     appstream-glib # for appstream-util
