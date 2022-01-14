dc57826da0a797d3ae115e7067473bfddd0adcb3
Wout Mertens <Wout.Mertens@gmail.com>
2022-01-14 12:19:36 +0200

    sqlcipher: grab CFLAGS from sqlite
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>
    (cherry picked from commit 3fc8be277da9ef16fded052863df1d336176ca54)

--- pkgs/development/libraries/sqlcipher/default.nix
+++ pkgs/development/libraries/sqlcipher/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, fetchFromGitHub, openssl, tcl, installShellFiles, buildPackages, readline, ncurses, zlib }:
+{ stdenv, lib, fetchFromGitHub, openssl, tcl, installShellFiles, buildPackages, readline, ncurses, zlib, sqlite }:
 
 stdenv.mkDerivation rec {
   pname = "sqlcipher";
@@ -21,24 +21,8 @@ stdenv.mkDerivation rec {
   ];
 
   CFLAGS = [
-    # Keep these in sync with ../sqlite/default.nix
-    "-DSQLITE_ENABLE_COLUMN_METADATA"
-    "-DSQLITE_ENABLE_DBSTAT_VTAB"
-    "-DSQLITE_ENABLE_JSON1"
-    "-DSQLITE_ENABLE_FTS3"
-    "-DSQLITE_ENABLE_FTS3_PARENTHESIS"
-    "-DSQLITE_ENABLE_FTS3_TOKENIZER"
-    "-DSQLITE_ENABLE_FTS4"
-    "-DSQLITE_ENABLE_FTS5"
-    "-DSQLITE_ENABLE_RTREE"
-    "-DSQLITE_ENABLE_STMT_SCANSTATUS"
-    "-DSQLITE_ENABLE_UNLOCK_NOTIFY"
-    "-DSQLITE_SOUNDEX"
-    "-DSQLITE_SECURE_DELETE"
-    "-DSQLITE_MAX_VARIABLE_NUMBER=250000"
-    "-DSQLITE_MAX_EXPR_DEPTH=10000"
-
-    # Additional flags for sqlcipher
+    # We want feature parity with sqlite
+    sqlite.NIX_CFLAGS_COMPILE
     "-DSQLITE_HAS_CODEC"
   ];
 
