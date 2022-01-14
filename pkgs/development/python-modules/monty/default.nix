5ffbd4d07617e00a8c00375cd590188519b5bec3
Jonathan Ringer <jonringer117@gmail.com>
2022-01-13 16:58:33 -0800

    python3Packages.monty: 2021.12.1 -> 2022.1.12.1

--- pkgs/development/python-modules/monty/default.nix
+++ pkgs/development/python-modules/monty/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "monty";
-  version = "2021.12.1";
+  version = "2022.1.12.1";
   disabled = pythonOlder "3.5"; # uses type annotations
 
   src = fetchFromGitHub {
     owner = "materialsvirtuallab";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0zcbdh7pqv4dq3fan0zh912w9bvmf2p0zj1fhp0ayhdsc50cwldh";
+    sha256 = "11bp3zkmqffc6rqw93qfk8d905mzc10nqbjmvj2bay8h197q0dg2";
   };
 
   postPatch = ''
