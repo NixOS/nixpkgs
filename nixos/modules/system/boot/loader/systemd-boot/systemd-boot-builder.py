604c44137d97b5111be1ca5c0d97f6e24fbc5c2c
Jonathan Ringer <jonringer117@gmail.com>
2022-01-14 14:52:52 -0800

    nixos/systemd-boot: fix error output
    (cherry picked from commit 87502df43b246c020ff47e99e1d06c4fbb36022f)

--- nixos/modules/system/boot/loader/systemd-boot/systemd-boot-builder.py
+++ nixos/modules/system/boot/loader/systemd-boot/systemd-boot-builder.py
@@ -281,7 +281,7 @@ def main() -> None:
             if os.readlink(system_dir(*gen)) == args.default_config:
                 write_loader_conf(*gen)
         except OSError as e:
-            print("ignoring profile '{}' in the list of boot entries because of the following error:\n{}".format(profile, e), file=sys.stderr)
+            print("ignoring generation '{}' in the list of boot entries because of the following error:\n{}".format(*gen, e), file=sys.stderr)
 
     memtest_entry_file = "@efiSysMountPoint@/loader/entries/memtest86.conf"
     if os.path.exists(memtest_entry_file):
