93acc1fd8d1bea800e3df4918475ef0e0b85a030
Fabian Affolter <mail@fabian-affolter.ch>
2022-01-14 09:30:10 +0100

    python3Packages.pytest-httpbin: fix src

--- pkgs/development/python-modules/pytest-httpbin/default.nix
+++ pkgs/development/python-modules/pytest-httpbin/default.nix
@@ -12,9 +12,11 @@ buildPythonPackage rec {
   pname = "pytest-httpbin";
   version = "1.0.1";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "9f73320f97d33cc20bc8a08cb945372948de51402559e87e74e92c56b48da0d7";
+  src = fetchFromGitHub {
+    owner = "kevin1024";
+    repo = "pytest-httpbin";
+    rev = "v${version}";
+    hash = "sha256-Vngd8Vum96+rdG8Nz1+aHrO6WZjiAz+0CeIovaH8N+s=";
   };
 
   buildInputs = [
