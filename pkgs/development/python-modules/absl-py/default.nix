02dcdf714b134441708fc4633ebebea3ed269ae7
Jonathan Ringer <jonringer117@gmail.com>
2022-01-13 16:56:50 -0800

    python3Packages.absl-py: 0.15.0 -> 1.0.0

--- pkgs/development/python-modules/absl-py/default.nix
+++ pkgs/development/python-modules/absl-py/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "absl-py";
-  version = "0.15.0";
+  version = "1.0.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "72d782fbeafba66ba3e525d46bccac949b9a174dbf66233e50ece09ee688dc81";
+    sha256 = "ac511215c01ee9ae47b19716599e8ccfa746f2e18de72bdf641b79b22afa27ea";
   };
 
   propagatedBuildInputs = [
