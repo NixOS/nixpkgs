1ab055c4fb806a4df446e28416d84b46aa23bffd
Fabian Affolter <mail@fabian-affolter.ch>
2022-01-14 22:38:36 +0100

    checkSSLCert: add homepage

--- pkgs/servers/monitoring/nagios/plugins/check_ssl_cert.nix
+++ pkgs/servers/monitoring/nagios/plugins/check_ssl_cert.nix
@@ -35,6 +35,7 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "Nagios plugin to check the CA and validity of an X.509 certificate";
+    homepage = "https://github.com/matteocorti/check_ssl_cert";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ ];
     platforms = platforms.all;
