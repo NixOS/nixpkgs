72fb0a7231f2d10db2de4754dcb117ff92dcdb21
Renaud <c0bw3b@users.noreply.github.com>
2022-01-14 16:00:21 +0100

    ocamlPackages.fix: 20211125 -> 20211231
    and fix license

--- pkgs/development/ocaml-modules/fix/default.nix
+++ pkgs/development/ocaml-modules/fix/default.nix
@@ -2,14 +2,14 @@
 
 buildDunePackage rec {
   pname = "fix";
-  version = "20211125";
+  version = "20211231";
 
   src = fetchFromGitLab {
     domain = "gitlab.inria.fr";
     owner = "fpottier";
     repo = "fix";
     rev = "${version}";
-    sha256 = "sha256-gaYUUudCXH3FkUwoKIfXTghTvDz//BGAYh3RoFUTeAM=";
+    sha256 = "sha256-T/tbiC95yzPb60AiEcvMRU47D8xUZNN5C4X33Y1VB9E=";
   };
 
   minimumOCamlVersion = "4.03";
@@ -18,7 +18,7 @@ buildDunePackage rec {
   meta = with lib; {
     homepage = "https://gitlab.inria.fr/fpottier/fix/";
     description = "A simple OCaml module for computing the least solution of a system of monotone equations";
-    license = licenses.cecill-c;
+    license = licenses.lgpl2Only;
     maintainers = with maintainers; [ vbgl ];
   };
 }
