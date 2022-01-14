4a2619dee67a1afb1201cfd5e4ac8ae61763689b
Brian McKenna <bmckenna@atlassian.com>
2022-01-15 07:29:24 +1000

    onnxruntime: init at 1.10.0 (resurrected)

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -936,7 +936,6 @@ mapAliases ({
   odpdown = throw "odpdown has been removed because it lacks python3 support"; # Added 2022-04-25
   ofp = throw "ofp is not compatible with odp-dpdk";
   olifant = throw "olifant has been removed from nixpkgs, as it was unmaintained"; # Added 2021-08-05
-  onnxruntime = throw "onnxruntime has been removed due to poor maintainability"; # Added 2020-12-04
   openbazaar = throw "openbazzar has been removed from nixpkgs as upstream has abandoned the project"; # Added 2022-01-06
   openbazaar-client = throw "openbazzar-client has been removed from nixpkgs as upstream has abandoned the project"; # Added 2022-01-06
   opencascade_oce = throw "'opencascade_oce' has been renamed to/replaced by 'opencascade'"; # Converted to throw 2022-02-22
