fb8aba36b0d9aef299f8cecd42116625d554ba0c
Jonathan Ringer <jonringer117@gmail.com>
2022-01-13 16:58:25 -0800

    python3Packages.huey: 2.2.0 -> 2.4.2

--- pkgs/development/python-modules/huey/default.nix
+++ pkgs/development/python-modules/huey/default.nix
@@ -2,13 +2,13 @@
 
 buildPythonPackage rec {
   pname = "huey";
-  version = "2.2.0";
+  version = "2.4.2";
 
   src = fetchFromGitHub {
     owner = "coleifer";
     repo = pname;
     rev = version;
-    sha256 = "1hgic7qrmb1kxvfgf2qqiw39nqyknf17pjvli8jfzvd9mv7cb7hh";
+    sha256 = "00fi04991skq61gjrmig8ry6936pc8zs7p8py8spfipbxf1irkjg";
   };
 
   propagatedBuildInputs = [ redis ];
