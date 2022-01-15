741e04e580c216eaaae00a7cb677eb03221a421a
Jörg Thalheim <joerg@thalheim.io>
2022-01-15 13:51:44 +0100

    love_0_8: drop

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13527,6 +13527,8 @@ with pkgs;
 
   lolcode = callPackage ../development/interpreters/lolcode { };
 
+  love_0_7 = callPackage ../development/interpreters/love/0.7.nix { lua=lua5_1; };
+  love_0_9 = callPackage ../development/interpreters/love/0.9.nix { };
   love_0_10 = callPackage ../development/interpreters/love/0.10.nix { };
   love_11 = callPackage ../development/interpreters/love/11.nix { };
   love = love_0_10;
