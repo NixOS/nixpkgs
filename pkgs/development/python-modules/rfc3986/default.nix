245cde952a184c59e340062104594b25bddd3eeb
Fabian Affolter <mail@fabian-affolter.ch>
2022-01-15 15:47:50 +0100

    python3Packages.rfc3986: add pythonImportsCheck

--- pkgs/development/python-modules/rfc3986/default.nix
+++ pkgs/development/python-modules/rfc3986/default.nix
@@ -1,19 +1,34 @@
-{ lib, buildPythonPackage, fetchPypi, pythonOlder, idna, pytestCheckHook }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, idna
+, pytestCheckHook
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "rfc3986";
   version = "2.0.0";
+  format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "sha256-l6rPnb1L/YKbqtbmMJ+mVzqvG+P2+nNcirBeRs7LJhw=";
   };
 
-  propagatedBuildInputs = [ idna ];
+  propagatedBuildInputs = [
+    idna
+  ];
 
-  checkInputs = [ pytestCheckHook ];
+  checkInputs = [
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [
+    "rfc3986"
+  ];
 
   meta = with lib; {
     description = "Validating URI References per RFC 3986";
