512483d9108b430db9d6fba09ae2676b095c5473
Seong Yong-ju <sei40kr@gmail.com>
2022-01-16 01:26:33 +0900

    nwg-bar: init at unstable-2021-09-23

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27744,6 +27744,8 @@ with pkgs;
 
   novnc = callPackage ../applications/networking/novnc { };
 
+  nwg-bar = callPackage ../applications/misc/nwg-bar { };
+
   nwg-drawer = callPackage ../applications/misc/nwg-drawer { };
 
   nwg-launchers = callPackage ../applications/misc/nwg-launchers { };
