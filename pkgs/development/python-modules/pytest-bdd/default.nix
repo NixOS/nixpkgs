8bcd96e711dfbbd544add9edf9b5c78637690450
Fabian Affolter <mail@fabian-affolter.ch>
2022-01-15 21:36:50 +0100

    python3Packages.pytest-bdd: 4.0.2 -> 5.0.0

--- pkgs/development/python-modules/pytest-bdd/default.nix
+++ pkgs/development/python-modules/pytest-bdd/default.nix
@@ -1,4 +1,6 @@
-{ lib, buildPythonPackage, fetchFromGitHub, fetchpatch
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
 , execnet
 , glob2
 , Mako
@@ -8,41 +10,51 @@
 , py
 , pytest
 , pytestCheckHook
-, six
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "pytest-bdd";
-  version = "4.0.2";
+  version = "5.0.0";
+  format = "setuptools";
 
-  # tests are not included in pypi tarball
+  disabled = pythonOlder "3.6";
+
   src = fetchFromGitHub {
     owner = "pytest-dev";
     repo = pname;
     rev = version;
-    sha256 = "0pxx4c8lm68rw0jshbr09fnadg8zz8j73q0qi49yw9s7yw86bg5l";
+    sha256 = "sha256-3P9ongMAsLITOCRPmME492xnkdVc8IdOVrINK57gfOY=";
   };
 
-  patches = [
-    # Fixed compatibility with pytest > 6.1
-    (fetchpatch {
-      url = "https://github.com/pytest-dev/pytest-bdd/commit/e1dc0cad9a1c1ba563ccfbc24f9993d83ac59293.patch";
-      sha256 = "1p3gavh6nir2a8crd5wdf0prfrg0hmgar9slvn8a21ils3k5pm5y";
-    })
+  buildInputs = [
+    pytest
   ];
 
+  propagatedBuildInputs = [
+    glob2
+    Mako
+    parse
+    parse-type
+    py
+  ];
 
-  buildInputs = [ pytest ];
+  checkInputs = [
+    pytestCheckHook
+    execnet
+    mock
+  ];
 
-  propagatedBuildInputs = [ glob2 Mako parse parse-type py six ];
-
-  checkInputs = [ pytestCheckHook execnet mock ];
   preCheck = ''
     export PATH=$PATH:$out/bin
   '';
 
+  pythonImportsCheck = [
+    "pytest_bdd"
+  ];
+
   meta = with lib; {
-    description = "BDD library for the py.test runner";
+    description = "BDD library for the pytest";
     homepage = "https://github.com/pytest-dev/pytest-bdd";
     license = licenses.mit;
     maintainers = with maintainers; [ jm2dev ];
