e2293412d10bb090c93c5d692180f8862849f6d2
Fabian Affolter <mail@fabian-affolter.ch>
2022-01-15 21:18:41 +0100

    python3Packages.zipp: add pythonImportsCheck

--- pkgs/development/python-modules/zipp/default.nix
+++ pkgs/development/python-modules/zipp/default.nix
@@ -1,25 +1,37 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, setuptools-scm
 , more-itertools
+, pythonOlder
+, setuptools-scm
 }:
 
 buildPythonPackage rec {
   pname = "zipp";
   version = "3.7.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "9f50f446828eb9d45b267433fd3e9da8d801f614129124863f9c51ebceafb87d";
   };
 
-  nativeBuildInputs = [ setuptools-scm ];
+  nativeBuildInputs = [
+    setuptools-scm
+  ];
 
-  propagatedBuildInputs = [ more-itertools ];
+  propagatedBuildInputs = [
+    more-itertools
+  ];
 
   # Prevent infinite recursion with pytest
   doCheck = false;
+
+  pythonImportsCheck = [
+    "zipp"
+  ];
 
   meta = with lib; {
     description = "Pathlib-compatible object wrapper for zip files";
