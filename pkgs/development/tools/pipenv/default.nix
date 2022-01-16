d32d994cc195858ec6569011a642d29350241edc
sudosubin <sudosubin@gmail.com>
2022-01-16 22:31:42 +0900

    pipenv: add pipenv shell completions

--- pkgs/development/tools/pipenv/default.nix
+++ pkgs/development/tools/pipenv/default.nix
@@ -38,6 +38,17 @@ in buildPythonApplication rec {
 
   propagatedBuildInputs = runtimeDeps python3.pkgs;
 
+  postInstall = ''
+    mkdir -p "$out/share/bash-completion/completions"
+    _PIPENV_COMPLETE=bash_source "$out/bin/pipenv" > "$out/share/bash-completion/completions/pipenv"
+
+    mkdir -p "$out/share/zsh/vendor-completions"
+    _PIPENV_COMPLETE=zsh_source "$out/bin/pipenv" > "$out/share/zsh/vendor-completions/_pipenv"
+
+    mkdir -p "$out/share/fish/vendor_completions.d"
+    _PIPENV_COMPLETE=fish_source "$out/bin/pipenv" > "$out/share/fish/vendor_completions.d/pipenv.fish"
+  '';
+
   doCheck = true;
   checkPhase = ''
     export HOME=$(mktemp -d)
