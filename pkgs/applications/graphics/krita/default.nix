35a71711ab23e34ba562533c00acbe9fc1eb412f
wackbyte <wackbyte@pm.me>
2022-01-15 20:07:56 -0500

    krita: 5.0.0 -> 5.0.2

--- pkgs/applications/graphics/krita/default.nix
+++ pkgs/applications/graphics/krita/default.nix
@@ -1,7 +1,7 @@
 { callPackage, ... } @ args:
 
 callPackage ./generic.nix (args // {
-  version = "5.0.0";
+  version = "5.0.2";
   kde-channel = "stable";
-  sha256 = "sha256-hNWDPbyrP9OkGPTDdnDYKtkZQw8MbQpXuZOQdHHuzFc=";
+  sha256 = "sha256-5nUfx+tQSXekiAo3brvTmVyH2tFUSGCE6COX5l1JnL8=";
 })
