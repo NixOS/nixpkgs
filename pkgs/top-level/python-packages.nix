e87b979ddf6c6c69781fc3a7a851a67b34cffe7b
Patrick Hilhorst <git@hilhorst.be>
2022-01-16 16:22:00 +0100

    python3Packages.mutmut: init at 2.2.0
    (cherry picked from commit 4326ef50cfee1d36b026e9f79744466080494eab)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5007,6 +5007,8 @@ in {
 
   mutf8 = callPackage ../development/python-modules/mutf8 { };
 
+  mutmut = callPackage ../development/python-modules/mutmut { };
+
   mujson = callPackage ../development/python-modules/mujson { };
 
   mwclient = callPackage ../development/python-modules/mwclient { };
