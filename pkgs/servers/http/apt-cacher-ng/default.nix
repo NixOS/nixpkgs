7ec6c926e960fad41c546b3a4c64cd2fa08c7b58
embr <git@liclac.eu>
2022-01-17 21:01:27 +0100

    apt-cacher-ng: 3.6.3 -> 3.7.4

--- pkgs/servers/http/apt-cacher-ng/default.nix
+++ pkgs/servers/http/apt-cacher-ng/default.nix
@@ -11,19 +11,20 @@
 , systemd
 , tcp_wrappers
 , zlib
+, c-ares
 }:
 
 stdenv.mkDerivation rec {
   pname = "apt-cacher-ng";
-  version = "3.6.3";
+  version = "3.7.4";
 
   src = fetchurl {
     url = "http://ftp.debian.org/debian/pool/main/a/apt-cacher-ng/apt-cacher-ng_${version}.orig.tar.xz";
-    sha256 = "sha256-P4ArWpxjOjBi9EiDp/ord17GfUOFwpiTKGvSEuZljGA=";
+    sha256 = "0pwsj9rf6a6q7cnfbpcrfq2gjcy7sylqzqqr49g2zi39lrrh8533";
   };
 
   nativeBuildInputs = [ cmake doxygen pkg-config ];
-  buildInputs = [ bzip2 fuse libevent xz openssl systemd tcp_wrappers zlib ];
+  buildInputs = [ bzip2 fuse libevent xz openssl systemd tcp_wrappers zlib c-ares ];
 
   meta = with lib; {
     description = "A caching proxy specialized for linux distribution files";
