0a85191a3ba37c8c8be5dbd447dca186bc464e28
florian on nixos (Florian Brandes) <florian.brandes@posteo.de>
2022-01-17 21:59:14 +0100

    python3Packages.sentinels: init at 1.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8819,6 +8819,8 @@ in {
 
   sentinel = callPackage ../development/python-modules/sentinel { };
 
+  sentinels = callPackage ../development/python-modules/sentinels { };
+
   sentry-sdk = callPackage ../development/python-modules/sentry-sdk { };
 
   sepaxml = callPackage ../development/python-modules/sepaxml { };
