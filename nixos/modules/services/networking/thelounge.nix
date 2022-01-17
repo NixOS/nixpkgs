9ae8b13009850cb8552868a36c7e83750e9e3790
Winter <winter@winter.cafe>
2022-01-16 21:08:57 -0500

    nixos/thelounge: add ensureUsers option

--- nixos/modules/services/networking/thelounge.nix
+++ nixos/modules/services/networking/thelounge.nix
@@ -18,6 +18,33 @@ let
     ln -s ${pkg}/lib/node_modules/${getName pkg} $out/node_modules/${getName pkg}
     '') cfg.plugins}
   '';
+  userOptions = {
+    options =
+      let
+        passwordDesc = ''
+          This can be generated with <literal>mkpasswd -m bcrypt -R 11</literal>.
+        '';
+      in
+      {
+        hashedPassword = mkOption {
+          type = types.nullOr types.nonEmptyStr;
+          default = null;
+          description = ''
+            The hashed password for this user. ${passwordDesc}
+          '';
+        };
+
+        hashedPasswordFile = mkOption {
+          type = types.nullOr types.path;
+          default = null;
+          description = ''
+            The path to a file containing the hashed password for this user. ${passwordDesc}
+          '';
+        };
+
+        enableLogging = (mkEnableOption "message logging for this user") // { default = true; };
+      };
+  };
 in
 {
   imports = [ (mkRemovedOptionModule [ "services" "thelounge" "private" ] "The option was renamed to `services.thelounge.public` to follow upstream changes.") ];
@@ -74,9 +101,32 @@ in
         <literal>pkgs.theLoungePlugins.plugins</literal> and <literal>pkgs.theLoungePlugins.themes</literal>.
       '';
     };
+
+    ensureUsers = mkOption {
+      default = { };
+      type = types.attrsOf (types.submodule userOptions);
+      example = literalExpression ''
+        john = {
+          password = "$2b$05$fK.qf8vOxvOeSA4D3W0znOhkASuXTceypm6uwqPkrAHh7bWaji7U2";
+        };
+      '';
+      description = ''
+        Ensures that the specified users exist. If one does not, a user with the specified options will be created.
+        This option will not delete or update existing users.
+      '';
+    };
   };
 
   config = mkIf cfg.enable {
+    assertions = mapAttrsToList
+      (name: user:
+        {
+          assertion = (user.hashedPassword != null && user.hashedPasswordFile == null) || (user.hashedPasswordFile != null && user.hashedPassword == null);
+          message = "Exactly one of services.thelounge.ensureUsers.${name}.hashedPassword or services.thelounge.ensureUsers.${name}.hashedPasswordFile must be set.";
+        }
+      )
+      cfg.ensureUsers;
+
     users.users.thelounge = {
       description = "The Lounge service user";
       group = "thelounge";
@@ -88,7 +138,33 @@ in
     systemd.services.thelounge = {
       description = "The Lounge web IRC client";
       wantedBy = [ "multi-user.target" ];
-      preStart = "ln -sf ${pkgs.writeText "config.js" configJsData} ${dataDir}/config.js";
+      preStart = ''
+        set -o errexit -o pipefail -o nounset -o errtrace
+        shopt -s inherit_errexit
+
+        ln -sf ${pkgs.writeText "config.js" configJsData} ${dataDir}/config.js
+
+        mkdir ${dataDir}/users
+
+        ${concatStrings (mapAttrsToList (user: options:
+          let
+            path = "${dataDir}/users/${user}.json";
+          in
+          ''
+            if [ ! -e ${path} ]; then
+              ${if options.hashedPassword != null then ''
+                PASSWORD=${escapeShellArg options.hashedPassword}
+              '' else ''
+                PASSWORD="$(<${escapeShellArg options.hashedPasswordFile})"
+              ''}
+
+              export PASSWORD
+
+              ${pkgs.jq}/bin/jq --null-input \
+                '{password: $ENV.PASSWORD, log: ${if options.enableLogging then "true" else "false"}}' > ${path}
+            fi
+          '') cfg.ensureUsers)}
+      '';
       environment.THELOUNGE_PACKAGES = mkIf (cfg.plugins != [ ]) "${plugins}";
       serviceConfig = {
         User = "thelounge";
--- nixos/tests/thelounge.nix
+++ nixos/tests/thelounge.nix
@@ -1,19 +1,108 @@
 import ./make-test-python.nix {
-  nodes = {
-    private = { config, pkgs, ... }: {
-      services.thelounge = {
-        enable = true;
-        plugins = [ pkgs.theLoungePlugins.themes.solarized ];
-      };
-    };
+  nodes =
+    let
+      mkTestRunner =
+        cfg: pkgs: pkgs.writers.writePython3Bin "test-runner"
+          {
+            libraries = [ pkgs.python3Packages.aiohttp ];
+            flakeIgnore = [ "E271" "E303" "E501" "W293" ];
+          }
+          ''
+            import aiohttp
+            import asyncio
+            import json
+            import sys
 
-    public = { config, pkgs, ... }: {
-      services.thelounge = {
-        enable = true;
-        public = true;
+
+            async def main():
+                user = None
+                password = None
+
+                if len(sys.argv) > 2:
+                    user = sys.argv[1]
+                    password = sys.argv[2]
+
+                async with aiohttp.ClientSession() as session:
+                    async with session.ws_connect("http://localhost:9000/socket.io/?EIO=4&transport=websocket") as ws:
+                        async for msg in ws:
+                            if msg.type == aiohttp.WSMsgType.TEXT:
+                                typ = msg.data[0]
+                                data = msg.data[1:]
+
+                                if typ == "2":
+                                    await ws.send_str("3")
+                                elif typ == "0":
+                                    await ws.send_str("40")
+                                elif typ == "4":
+                                    typ = data[0]
+                                    data = data[1:]
+
+                                    if typ == "2":
+                                        data = json.loads(data)
+                                        event = data[0]
+                                        payload = None
+
+                                        if len(data) > 1:
+                                            payload = data[1]
+
+                                        if event == "configuration":
+                                            assert ${pkgs.lib.optionalString (!cfg.public) "not"} payload["public"]
+
+                                            ${pkgs.lib.optionalString ((builtins.length cfg.plugins) > 0) ''
+                                              assert any([theme["name"] == "thelounge-theme-solarized" for theme in payload["themes"]])
+                                            ''}
+
+                                            break
+                                        elif event == "auth:start":
+                                            payload = ["auth:perform", {"user": user, "password": password}]
+                                            await ws.send_str("42" + json.dumps(payload))
+
+
+            asyncio.run(main())
+          '';
+    in
+    {
+      private = { config, pkgs, ... }: {
+        services.thelounge = {
+          enable = true;
+          plugins = [ pkgs.theLoungePlugins.themes.solarized ];
+          ensureUsers = {
+            john = {
+              hashedPassword = "$2b$11$SqnMvKNEERan67wC9pQZX.MnCX2yXsTjh/EM0RLwhuZWq.Y/rGFLu"; # password123
+            };
+            jane =
+              let
+                file = pkgs.writeTextFile {
+                  name = "jane-password";
+                  text = config.services.thelounge.ensureUsers.john.hashedPassword;
+                };
+              in
+              {
+                hashedPasswordFile = "${file}";
+              };
+          };
+        };
+
+        environment.systemPackages =
+          let
+            testRunner = mkTestRunner config.services.thelounge pkgs;
+          in
+          [ testRunner ];
+      };
+
+      public = { config, pkgs, ... }: {
+        services.thelounge = {
+          enable = true;
+          public = true;
+        };
+
+        environment.systemPackages =
+          let
+            testRunner = mkTestRunner config.services.thelounge pkgs;
+          in
+          [ testRunner ];
       };
     };
-  };
 
   testScript = ''
     start_all()
@@ -22,8 +111,8 @@ import ./make-test-python.nix {
       machine.wait_for_unit("thelounge.service")
       machine.wait_for_open_port(9000)
 
-    private.wait_until_succeeds("journalctl -u thelounge.service | grep thelounge-theme-solarized")
-    private.wait_until_succeeds("journalctl -u thelounge.service | grep 'in private mode'")
-    public.wait_until_succeeds("journalctl -u thelounge.service | grep 'in public mode'")
+    public.succeed("PYTHONUNBUFFERED=1 test-runner")
+    private.succeed("PYTHONUNBUFFERED=1 test-runner john password123")
+    private.succeed("PYTHONUNBUFFERED=1 test-runner jane password123")
   '';
 }
