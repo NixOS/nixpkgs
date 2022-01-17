c3cbfb738b33c0345a3051e910a906d6ef46ca25
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-01-17 08:07:49 +0100

    php: 7.4.26 -> 7.4.27
    (cherry picked from commit 6e4afa39a5952a5d834cbe2d5d7f03a469b8d1c9)

--- pkgs/development/interpreters/php/7.4.nix
+++ pkgs/development/interpreters/php/7.4.nix
@@ -2,8 +2,8 @@
 
 let
   base = callPackage ./generic.nix (_args // {
-    version = "7.4.26";
-    sha256 = "0k803j5wf4jv72px0zqz2z2hxyk2w3jr6xyczy568dx4z2l8i2yn";
+    version = "7.4.27";
+    sha256 = "184aaef313fbf28c9987f6aa07b655cd1b0eae9e7e17061775a3e7d880185563";
   });
 
 in
