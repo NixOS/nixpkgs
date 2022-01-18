102dba80cf4acd8459630aeca2d4168120ca98c6
Johannes Schleifenbaum <johannes@js-webcoding.de>
2022-01-18 08:58:58 +0100

    dnsdist: 1.5.2 -> 1.7.0

--- pkgs/servers/dns/dnsdist/default.nix
+++ pkgs/servers/dns/dnsdist/default.nix
@@ -1,7 +1,7 @@
 { lib, stdenv, fetchurl, pkg-config, systemd
 , boost, libsodium, libedit, re2
 , net-snmp, lua, protobuf, openssl, zlib, h2o
-, nghttp2, nixosTests
+, nghttp2
 }:
 
 stdenv.mkDerivation rec {
