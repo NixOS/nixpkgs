77c1f235d343741eab0bc6cea0961127c12afdeb
Fabian Affolter <mail@fabian-affolter.ch>
2022-01-18 23:09:37 +0100

    python3Packages.types-freezegun: add pythonImportsCheck

--- pkgs/development/python-modules/types-freezegun/default.nix
+++ pkgs/development/python-modules/types-freezegun/default.nix
@@ -6,14 +6,19 @@
 buildPythonPackage rec {
   pname = "types-freezegun";
   version = "1.1.6";
+  format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0kxiv0yjbbvp1zx694ir149b26kjzvb6600fh397v32b8jvs8w2w";
+    hash = "sha256-XHCkt0RLjH3SgA4AY9b+chqxEgk5kmT6D3evJT3YsU8=";
   };
 
   # Module doesn't have tests
   doCheck = false;
+
+  pythonImportsCheck = [
+    "freezegun-stubs"
+  ];
 
   meta = with lib; {
     description = "Typing stubs for freezegun";
