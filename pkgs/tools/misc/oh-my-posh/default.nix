6d730e5a2cff8a273ee72fac1f7b2060eaca1c10
^x3ro <git@x3ro.dev>
2022-01-19 14:12:45 +0100

    oh-my-posh: Add tests

--- pkgs/tools/misc/oh-my-posh/default.nix
+++ pkgs/tools/misc/oh-my-posh/default.nix
@@ -2,6 +2,8 @@
 , lib
 , fetchFromGitHub
 , buildGoModule
+, runCommand
+, oh-my-posh
 }:
 
 buildGoModule rec {
@@ -22,6 +24,27 @@ buildGoModule rec {
     "-s" "-w"
     "-X main.Version=${version}"
   ];
+
+  passthru.tests = {
+    test-version = runCommand "${pname}-test-version" {} ''
+      version=$(${oh-my-posh}/bin/oh-my-posh --version)
+      [ "$version" = '${version}' ]
+      touch $out
+    '';
+    # To generate the expected-prompt.out, add `echo $prompt; exit 1` after the
+    # line `prompt=$(...)`.
+    # Then execute the test and in the error message, look for the line
+    #   For full logs, run 'nix log /nix/store/<HASH>-oh-my-posh-test-parse-config.drv'
+    # Now pipe the output of this command to the `.out` file:
+    #   $ nix log /nix/store/<HASH>-oh-my-posh-test-parse-config.drv > test/expected-prompt.out
+    test-parse-config = runCommand "${pname}-test-parse-config" {} ''
+      config='${./test/themes/agnoster.omp.json}'
+      prompt=$(${oh-my-posh}/bin/oh-my-posh -shell sh --config "$config")
+      expected=$(cat '${./test/expected-prompt.agnoster.out}')
+      [ "$prompt" = "$expected" ]
+      touch $out
+    '';
+  };
 
   meta = with lib; {
     homepage = "https://ohmyposh.dev/";
