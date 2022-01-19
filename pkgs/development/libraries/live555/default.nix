8bd7d610556ee07fca8c46546af32603748d25cd
FliegendeWurst <2012gdwu+github@posteo.de>
2022-01-19 22:19:05 +0100

    live555: 2019.11.22 -> 2022.01.21
    (cherry picked from commit 460fdfce74c739e774f8ac8e26447977cf63adad)

--- pkgs/development/libraries/live555/default.nix
+++ pkgs/development/libraries/live555/default.nix
@@ -1,20 +1,23 @@
-{ stdenv, fetchurl, lib, darwin }:
+{ stdenv, fetchurl, lib, darwin, openssl }:
 
 # Based on https://projects.archlinux.org/svntogit/packages.git/tree/trunk/PKGBUILD
 stdenv.mkDerivation rec {
   pname = "live555";
-  version = "2019.11.22";
+  version = "2022.01.21";
 
   src = fetchurl { # the upstream doesn't provide a stable URL
     urls = [
       "mirror://sourceforge/slackbuildsdirectlinks/live.${version}.tar.gz"
       "https://download.videolan.org/contrib/live555/live.${version}.tar.gz"
     ];
-    sha256 = "144y2wsfpaclkj7srx85f3y3parzn7vbjmzc2afc62wdsb9gn46d";
+    sha256 = "03m9wgw06vs93nbd55yq8qjpm92mcg9j7cih8j6blfnv8b0pj9bn";
   };
 
   postPatch = ''
     sed 's,/bin/rm,rm,g' -i genMakefiles
+    substituteInPlace config.macosx-catalina \
+      --replace '/usr/lib/libssl.46.dylib' "${openssl.out}/lib/libssl.dylib" \
+      --replace '/usr/lib/libcrypto.44.dylib' "${openssl.out}/lib/libcrypto.dylib"
     sed \
       -e 's/$(INCLUDES) -I. -O2 -DSOCKLEN_T/$(INCLUDES) -I. -O2 -I. -fPIC -DRTSPCLIENT_SYNCHRONOUS_INTERFACE=1 -DSOCKLEN_T/g' \
       -i config.linux
@@ -27,7 +30,7 @@ stdenv.mkDerivation rec {
     runHook preConfigure
 
     ./genMakefiles ${{
-      x86_64-darwin = "macosx";
+      x86_64-darwin = "macosx-catalina";
       i686-linux = "linux";
       x86_64-linux = "linux-64bit";
       aarch64-linux = "linux-64bit";
@@ -49,6 +52,8 @@ stdenv.mkDerivation rec {
   '';
 
   nativeBuildInputs = lib.optional stdenv.isDarwin darwin.cctools;
+
+  buildInputs = [ openssl ];
 
   enableParallelBuilding = true;
 
