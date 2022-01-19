fde0b2e2f28aeac074e424bb0a17917ceeee43b9
FliegendeWurst <2012gdwu+github@posteo.de>
2022-01-19 10:28:22 +0100

    vim: 8.2.3877 -> 8.2.4186 (security)
    (cherry picked from staging commit ea5a1dd5e7c0afb3c1, PR #155587)
    I believe this qualifies for staging-next; rebuilds aren't that huge,
    and they look relatively cheap (mostly vim plugins).

--- pkgs/applications/editors/vim/common.nix
+++ pkgs/applications/editors/vim/common.nix
@@ -1,12 +1,12 @@
 { lib, fetchFromGitHub }:
 rec {
-  version = "8.2.3877";
+  version = "8.2.4186";
 
   src = fetchFromGitHub {
     owner = "vim";
     repo = "vim";
     rev = "v${version}";
-    sha256 = "sha256-NqTO2TdhOs63eP7CdWY9U9nbR7No3hqPV5rGhYF9arA=";
+    sha256 = "0g276mbmq69z7c4kgj59r0azxmx9ih2sd8v83dx2gfph6wgw65ph";
   };
 
   enableParallelBuilding = true;
