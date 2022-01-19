259955a975f1861551e8471347519ff3ace2953c
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-01-19 14:43:35 +0100

    docker-compose_2: add SuperSandro2000 as maintainer, strip more

--- pkgs/applications/virtualization/docker/compose.nix
+++ pkgs/applications/virtualization/docker/compose.nix
@@ -13,7 +13,7 @@ buildGoModule rec {
 
   vendorSha256 = "sha256-tBm4yPeH02kie77A8KvylfH/wI2OsLyAlUb1zBJgT0g=";
 
-  ldflags = ["-X github.com/docker/compose/v2/internal.Version=${version}"];
+  ldflags = [ "-X github.com/docker/compose/v2/internal.Version=${version}" "-s" "-w" ];
 
   doCheck = false;
   installPhase = ''
@@ -22,7 +22,8 @@ buildGoModule rec {
 
   meta = with lib; {
     description = "Docker CLI plugin to define and run multi-container applications with Docker";
+    homepage = "https://github.com/docker/compose";
     license = licenses.asl20;
-    maintainers = [ maintainers.babariviere ];
+    maintainers = with maintainers; [ babariviere SuperSandro2000 ];
   };
 }
