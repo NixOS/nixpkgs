64b6d7337527ef2323fa9134d584918e682c2d4a
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-01-21 00:52:59 +0100

    python3Packages.pyphen: fix build

--- pkgs/development/python-modules/pyphen/default.nix
+++ pkgs/development/python-modules/pyphen/default.nix
@@ -1,17 +1,23 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, flit
 , pytestCheckHook
 }:
 
 buildPythonPackage rec {
   pname = "pyphen";
   version = "0.12.0";
+  format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "b7d3dfc24b6f2178cdb2b1757ace0bd5d222de3e62c28d22ac578c5f22a13e9b";
   };
+
+  nativeBuildInputs = [
+    flit
+  ];
 
   preCheck = ''
     sed -i '/addopts/d' pyproject.toml
