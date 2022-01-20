8a18354a3247a714d99275c20243e492525a647d
Luke Amdor <luke.amdor@gmail.com>
2022-01-20 09:23:08 -0600

    python3Packages.s3transfer: ignore test_compat on darwin
    This tests fails on darwin due to a switch on python 3.8 on MacOS from
    fork to spawn. See
    https://github.com/NixOS/nixpkgs/pull/154280 for more information.

--- pkgs/development/python-modules/s3transfer/default.nix
+++ pkgs/development/python-modules/s3transfer/default.nix
@@ -1,13 +1,5 @@
-{ lib
-, botocore
-, buildPythonPackage
-, docutils
-, fetchFromGitHub
-, mock
-, pytestCheckHook
-, pythonOlder
-, wheel
-}:
+{ lib, botocore, buildPythonPackage, docutils, fetchFromGitHub, mock
+, pytestCheckHook, pythonOlder, stdenv, wheel }:
 
 buildPythonPackage rec {
   pname = "s3transfer";
@@ -42,11 +34,13 @@ buildPythonPackage rec {
     "tests/integration/test_processpool.py"
     "tests/integration/test_s3transfer.py"
     "tests/integration/test_upload.py"
-  ];
+  ] ++
+    # There was a change in python 3.8 that defaults multiprocessing to spawn instead of fork on macOS
+    # See https://bugs.python.org/issue33725 and https://github.com/python/cpython/pull/13603.
+    # I suspect the underlying issue here is that upstream tests aren't compatible with spawn multiprocessing, and pass on linux where the default is still fork
+    lib.optionals stdenv.isDarwin [ "tests/unit/test_compat.py" ];
 
-  pythonImportsCheck = [
-    "s3transfer"
-  ];
+  pythonImportsCheck = [ "s3transfer" ];
 
   meta = with lib; {
     description = "Library for managing Amazon S3 transfers";
