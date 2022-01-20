f0e71f2ccc879fc84261e8ff2dd2dbcac40c20d2
R. Ryantm <ryantm-bot@ryantm.com>
2022-01-20 07:24:09 +0000

    python310Packages.pyee: 8.2.2 -> 9.0.3

--- pkgs/development/python-modules/pyee/default.nix
+++ pkgs/development/python-modules/pyee/default.nix
@@ -12,13 +12,13 @@
 
 buildPythonPackage rec {
   pname = "pyee";
-  version = "8.2.2";
+  version = "9.0.3";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-XH5g+N+VcQ2+F1UOFs4BU/g5kMAO90SEG0Pzce1T6+o=";
+    sha256 = "ab60ed0f00eb465b32e527df2159d4f32cf06f2239b511a45f5e80184ceb9d6f";
   };
 
   buildInputs = [
