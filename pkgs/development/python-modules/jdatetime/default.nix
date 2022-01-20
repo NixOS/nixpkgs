38e16d7c76761055096b0dd7e8353e6401f75571
R. Ryantm <ryantm-bot@ryantm.com>
2022-01-20 00:28:02 +0000

    python310Packages.jdatetime: 3.8.0 -> 3.8.1

--- pkgs/development/python-modules/jdatetime/default.nix
+++ pkgs/development/python-modules/jdatetime/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "jdatetime";
-  version = "3.8.0";
+  version = "3.8.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "389a0723a8011379a5e34386ec466cb3f65b2d5cb5422702c1d3aecb6ac192d0";
+    sha256 = "db57ee517356b1bfc1603ef412f5da61eae92241ba0bcaf0851028cae424780c";
   };
 
   propagatedBuildInputs = [ six ];
