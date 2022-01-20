50e48a57c51252c5b1207101889e8fd29cb35f41
Robert Hensing <robert@roberthensing.nl>
2022-01-20 15:04:08 +0100

    nixos: Make system.build a submodule with freeformType
    This allows the values below it to be specified as options, while
    remaining compatible with existing code.

--- nixos/modules/system/build.nix
+++ nixos/modules/system/build.nix
@@ -6,12 +6,15 @@ in
   options = {
 
     system.build = mkOption {
-      internal = true;
       default = {};
-      type = types.lazyAttrsOf types.unspecified;
       description = ''
         Attribute set of derivations used to set up the system.
       '';
+      type = types.submoduleWith {
+        modules = [{
+          freeformType = types.lazyAttrsOf types.unspecified;
+        }];
+      };
     };
 
   };
