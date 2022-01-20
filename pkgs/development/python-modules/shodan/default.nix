aca9f3526d561fd6b3b7f24c2301f5f76a59e948
R. Ryantm <ryantm-bot@ryantm.com>
2022-01-20 13:49:24 +0000

    python310Packages.shodan: 1.26.0 -> 1.26.1

--- pkgs/development/python-modules/shodan/default.nix
+++ pkgs/development/python-modules/shodan/default.nix
@@ -10,11 +10,11 @@
 
 buildPythonPackage rec {
   pname = "shodan";
-  version = "1.26.0";
+  version = "1.26.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "4f2ee19bdcad41a5f4618c8e7e1759f62c337cc2214416b53ad3d0c04a1146bc";
+    sha256 = "f2827b40d6918988ef9f5f16dcb8a13383aaae1a2845898f7012eac8906bbb87";
   };
 
   propagatedBuildInputs = [
