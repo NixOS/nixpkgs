f7d2ba2942d7a6ce704cf08989e462c82836db49
Alyssa Ross <hi@alyssa.is>
2022-01-20 00:48:44 +0000

    virtiofsd: init at 1.0.0
    (cherry picked from commit dc78ae3dc453384049994e41cd8ad7b29e6d644e)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21671,6 +21671,8 @@ with pkgs;
 
   victoriametrics = callPackage ../servers/nosql/victoriametrics { };
 
+  virtiofsd = callPackage ../servers/misc/virtiofsd { };
+
   virtlyst = libsForQt5.callPackage ../servers/web-apps/virtlyst { };
 
   virtualenv = with python3Packages; toPythonApplication virtualenv;
