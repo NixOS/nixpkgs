033c8141ab6ec7f57d0a9bb507a7af01e8b1a978
Jochen Kiemes <jochen@kiemes.de>
2022-01-21 22:12:10 +0100

    wg-netmanager: fix service

--- nixos/modules/services/networking/wg-netmanager.nix
+++ nixos/modules/services/networking/wg-netmanager.nix
@@ -10,20 +10,12 @@ in
   options = {
     services.wg-netmanager = {
       enable = mkEnableOption "Wireguard network manager";
-      package = mkOption {
-        type = types.package;
-        default = pkgs.wg-netmanager;
-        defaultText = literalExpression "pkgs.wg-netmanager";
-        description = ''
-          wg-netmanager derivation to use.
-        '';
-      };
     };
   };
 
   ###### implementation
   config = mkIf cfg.enable {
-
+    # NOTE: wg-netmanager runs as root
     systemd.services.wg-netmanager = {
       description = "Wireguard network manager";
       wantedBy = [ "multi-user.target" ];
@@ -31,18 +23,18 @@ in
       serviceConfig = {
         Type = "simple";
         Restart = "on-failure";
-        ExecStart = "${cfg.package}/bin/wg_netmanager";
+        ExecStart = "${pkgs.wg-netmanager}/bin/wg_netmanager";
         ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
         ExecStop = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
 
         ReadWritePaths = [
           "/tmp"  # wg-netmanager creates files in /tmp before deleting them after use
         ];
+      };
       unitConfig =  {
         ConditionPathExists = ["/etc/wg_netmanager/network.yaml" "/etc/wg_netmanager/peer.yaml"];
       };
     };
-   };
   };
 
   meta.maintainers = with maintainers; [ gin66 ];
