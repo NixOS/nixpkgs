7a1c1b7a9ccce301f76151cf7657c802c7d0ff7b
Robert Hensing <robert@roberthensing.nl>
2022-01-22 14:17:25 +0100

    netflix: init
    (cherry picked from commit 9aa8863a8ffcad0626a9e160da16a95f82e08f5c)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21462,6 +21462,8 @@ with pkgs;
 
   nemo-qml-plugin-dbus = libsForQt5.callPackage ../development/libraries/nemo-qml-plugin-dbus { };
 
+  netflix = callPackage ../applications/video/netflix { };
+
   nifticlib = callPackage ../development/libraries/science/biology/nifticlib { };
 
   notify-sharp = callPackage ../development/libraries/notify-sharp { };
