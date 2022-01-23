8731e9b02815727e6b93b6e1e9b8ff006d1eea56
R. Ryantm <ryantm-bot@ryantm.com>
2022-01-23 00:23:16 +0000

    python310Packages.pdm-pep517: 0.9.4 -> 0.10.1

--- pkgs/development/python-modules/pdm-pep517/default.nix
+++ pkgs/development/python-modules/pdm-pep517/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage rec {
   pname = "pdm-pep517";
-  version = "0.9.4";
+  version = "0.10.1";
   format = "pyproject";
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-2o2FuuvS5PW7uhxl3EGBP75CZ3dcyjPoug1k0irl51c=";
+    sha256 = "46e2bbb8ad3875818575840d97f2d7120ddebcd1f5ef2dc26025256581505e32";
   };
 
   preCheck = ''
