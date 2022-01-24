040139e4a8fb94326c2ad4bee6eec16137d343cc
Fabian Affolter <mail@fabian-affolter.ch>
2022-01-24 16:40:49 +0100

    python3Packages.cyclonedx-python-lib: 1.2.0 -> 1.3.0

--- pkgs/development/python-modules/cyclonedx-python-lib/default.nix
+++ pkgs/development/python-modules/cyclonedx-python-lib/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "cyclonedx-python-lib";
-  version = "1.2.0";
+  version = "1.3.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.6";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "CycloneDX";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-CrRtFo4wb/+4S0A4A68G4fxRWHi9pumXGNOsyW6t2Zc=";
+    hash = "sha256-/1kWvhTUS0JT0RwodiivJSUiWIDwQyXxdjF/KUlCNds=";
   };
 
   nativeBuildInputs = [
