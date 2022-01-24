d406dc84158c9a62a16acc938169b6f6d936eaac
Patrick Hilhorst <git@hilhorst.be>
2022-01-24 18:30:34 +0100

    trivial-builders/testGraphical: add
    Allows writing really simple NixOS tests for graphical programs that check that a program starts and displays a certain text with very little effort.

--- pkgs/build-support/trivial-builders.nix
+++ pkgs/build-support/trivial-builders.nix
@@ -814,4 +814,31 @@ rec {
       ${command} |& grep -Fw ${version}
       touch $out
     '';
+
+  testGraphical =
+    { package
+    , command ? "${package.meta.mainProgram or package.pname or package.name}"
+    , expectedText ? null
+    }: import ../../nixos/tests/make-test-python.nix ({ pkgs, ... }: {
+      machine = { ... }: {
+        imports = [ ../../nixos/tests/common/wayland-cage.nix ];
+        environment.systemPackages = [ package ];
+        services.cage.program = "${package}/bin/${command}";
+      };
+
+      enableOCR = expectedText != null;
+
+      testScript = ''
+        @polling_condition
+        def cage_running():
+          machine.require_unit_state("cage-tty1.service", "active")
+
+
+        machine.wait_for_unit("cage-tty1.service")
+        with cage_running:
+            machine.wait_until_succeeds("pgrep -f ${command}")
+            machine.wait_for_text("${expectedText}", verbose=True)
+            machine.screenshot("screen")
+      '';
+    });
 }
