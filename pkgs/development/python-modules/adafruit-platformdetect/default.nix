23b3acf554106b8417722e1b9d4df056f327ca1d
Fabian Affolter <mail@fabian-affolter.ch>
2022-01-26 01:05:04 +0100

    python3Packages.adafruit-platformdetect: 3.19.3 -> 3.19.4

--- pkgs/development/python-modules/adafruit-platformdetect/default.nix
+++ pkgs/development/python-modules/adafruit-platformdetect/default.nix
@@ -6,13 +6,13 @@
 
 buildPythonPackage rec {
   pname = "adafruit-platformdetect";
-  version = "3.19.3";
+  version = "3.19.4";
   format = "setuptools";
 
   src = fetchPypi {
     pname = "Adafruit-PlatformDetect";
     inherit version;
-    sha256 = "sha256-kjXHEXC+xTaQ7bEDROpqM/fNwacXlbVbhxRs2o62W20=";
+    sha256 = "sha256-b/uAmrFdAtmXUjaW038mKeZgWHCSIEzCZvCy/9Z3ghw=";
   };
 
   nativeBuildInputs = [
