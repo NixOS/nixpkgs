6012c686544dc1731dc1c03d7a985f91789ea204
schnusch <schnusch@users.noreply.github.com>
2022-01-26 02:30:46 +0100

    opentracker: add overridable build flags

--- pkgs/applications/networking/p2p/opentracker/default.nix
+++ pkgs/applications/networking/p2p/opentracker/default.nix
@@ -1,4 +1,46 @@
-{ lib, stdenv, fetchgit, libowfat, zlib, nixosTests }:
+{ lib
+, stdenv
+, fetchgit
+, libowfat
+, zlib
+, nixosTests
+, ipv6 ? false
+, blacklist ? false
+, whitelist ? false
+, syncLive ? false
+, ipFromQueryString ? false
+, restrictStats ? true
+, ipFromProxy ? false
+, fullLogNetwork ? false
+, logNumWant ? false
+, modestFullScrape ? fullscrape
+, spotWoodpecker ? false
+, fullscrape ? false
+}:
+
+assert !blacklist || !whitelist;
+
+let
+  features = {
+    V6 = ipv6;
+    ACCESSLIST_BLACK = blacklist;
+    ACCESSLIST_WHITE = whitelist;
+    SYNC_LIVE = syncLive;
+    IP_FROM_QUERY_STRING = ipFromQueryString;
+    COMPRESSION_GZIP = true;
+    COMPRESSION_GZIP_ALWAYS = false;
+    LOG_NETWORKS = false;
+    RESTRICT_STATS = restrictStats;
+    IP_FROM_PROXY = ipFromProxy;
+    FULLLOG_NETWORKS = fullLogNetwork;
+    LOG_NUMWANT = logNumWant;
+    MODEST_FULLSCRAPES = modestFullScrape;
+    SPOT_WOODPECKER = spotWoodpecker;
+    SYSLOGS = false;
+    DEV_RANDOM = false;
+    FULLSCRAPE = fullscrape;
+  };
+in
 
 stdenv.mkDerivation {
   pname = "opentracker";
@@ -11,6 +53,18 @@ stdenv.mkDerivation {
   };
 
   buildInputs = [ libowfat zlib ];
+
+  /* FEATURES flag contains spaces so we have to add it this way
+     see https://nixos.org/manual/nixpkgs/stable/#var-stdenv-makeFlagsArray
+  */
+  preBuild = ''
+    makeFlagsArray+=('FEATURES=${
+      lib.concatMapStringsSep
+        " "
+        (name: "-DWANT_${name}")
+        (lib.attrNames (lib.filterAttrs (_: use: use) features))
+    }')
+  '';
 
   makeFlags = [
     "LIBOWFAT_HEADERS=${libowfat}/include/libowfat"
