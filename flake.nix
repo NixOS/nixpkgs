6fe8d1fed20ae52cbfea44e590d36628ab0bdb91
Naïm Favier <n@monade.li>
2022-01-27 16:37:32 +0100

    flake/nixosSystem: pass the final lib to eval-config.nix
    Makes lib extensions available in modules by default

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
 
         nixosSystem = args:
           import ./nixos/lib/eval-config.nix (
-            args // {
+            { lib = final; } // args // {
               modules = args.modules ++ [{
                 system.nixos.versionSuffix =
                   ".${final.substring 0 8 (self.lastModifiedDate or self.lastModified or "19700101")}.${self.shortRev or "dirty"}";
