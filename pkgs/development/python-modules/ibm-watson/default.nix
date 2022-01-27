d746a9c2d8498750ffb84067f6e3fe75bfa3b3dd
Fabian Affolter <fabian@affolter-engineering.ch>
2022-01-27 09:30:14 +0100

    python3Packages.ibm-watson: add pythonImportsCheck

--- pkgs/development/python-modules/ibm-watson/default.nix
+++ pkgs/development/python-modules/ibm-watson/default.nix
@@ -10,11 +10,15 @@
 , python-dateutil
 , websocket-client
 , ibm-cloud-sdk-core
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "ibm-watson";
   version = "5.3.1";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "watson-developer-cloud";
@@ -22,6 +26,13 @@ buildPythonPackage rec {
     rev = "v${version}";
     sha256 = "1x6r8j0xyi81jb0q4pzr6l7aglykrwqz8nw45clv79v33i2sgdcs";
   };
+
+  propagatedBuildInputs = [
+    requests
+    python-dateutil
+    websocket-client
+    ibm-cloud-sdk-core
+  ];
 
   checkInputs = [
     responses
@@ -31,17 +42,14 @@ buildPythonPackage rec {
     tox
   ];
 
-  propagatedBuildInputs = [
-    requests
-    python-dateutil
-    websocket-client
-    ibm-cloud-sdk-core
-  ];
-
   postPatch = ''
     substituteInPlace setup.py \
       --replace websocket-client==1.1.0 websocket-client>=1.1.0
   '';
+
+  pythonImportsCheck = [
+    "ibm_watson"
+  ];
 
   meta = with lib; {
     description = "Client library to use the IBM Watson Services";
