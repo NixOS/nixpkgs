5e3d09b3b0b488b574b2122817153770098b861a
Mustafa Çalışkan <muscaln@protonmail.com>
2022-01-27 17:01:52 +0300

    tinygo: 0.16.0 -> 0.23.0

--- pkgs/development/compilers/tinygo/default.nix
+++ pkgs/development/compilers/tinygo/default.nix
@@ -1,64 +1,147 @@
-{ lib, buildGoModule, fetchFromGitHub, llvm, clang-unwrapped, lld, avrgcc
-, avrdude, openocd, gcc-arm-embedded, makeWrapper, fetchurl }:
+{ stdenv
+, lib
+, buildPackages
+, buildGoModule
+, fetchFromGitHub
+, makeWrapper
+, substituteAll
+, llvmPackages
+, go
+, libffi
+, zlib
+, ncurses
+, libxml2
+, xar
+, wasi-libc
+, avrgcc
+, binaryen
+, avrdude
+, gdb
+, openocd
+}:
 
-let main = ./main.go;
-    gomod = ./go.mod;
+let
+  llvmMajor = lib.versions.major llvm.version;
+  inherit (llvmPackages) llvm clang compiler-rt lld;
 in
+
 buildGoModule rec {
   pname = "tinygo";
-  version = "0.16.0";
+  version = "0.23.0";
 
   src = fetchFromGitHub {
     owner = "tinygo-org";
     repo = "tinygo";
     rev = "v${version}";
-    sha256 = "063aszbsnr0myq56kms1slmrfs7m4nmg0zgh2p66lxdsifrfly7j";
+    sha256 = "sha256-YgQGAQJw9Xyw5BF2d9uZTQHfjHsu2evZGo4RV9DtStE=";
     fetchSubmodules = true;
   };
 
-  overrideModAttrs = (_: {
-      patches = [];
-      preBuild = ''
-      rm -rf *
-      cp ${main} main.go
-      cp ${gomod} go.mod
-      chmod +w go.mod
-      '';
-  });
+  vendorSha256 = "sha256-fK8BlCh+1NtHW6MwW68iSIB+Sw6AK+g3y4lMyMYrXkk=";
 
-  preBuild = "cp ${gomod} go.mod";
+  patches = [
+    ./0001-Makefile.patch
 
-  postBuild = "make gen-device";
+    (substituteAll {
+      src = ./0002-Add-clang-header-path.patch;
+      clang_include = "${clang.cc.lib}/lib/clang/${clang.cc.version}/include";
+    })
+  ];
 
-  vendorSha256 = "12k2gin0v7aqz5543m12yhifc0xsz26qyqra5l4c68xizvzcvkxb";
+  checkInputs = [ avrgcc binaryen ];
+  nativeBuildInputs = [ go makeWrapper ];
+  buildInputs = [ llvm clang.cc ]
+    ++ lib.optionals stdenv.isDarwin [ zlib ncurses libffi libxml2 xar ];
 
-  doCheck = false;
+  doCheck = stdenv.buildPlatform == stdenv.hostPlatform;
 
-  prePatch = ''
-    sed -i s/', "-nostdlibinc"'// builder/builtins.go
-    sed -i s/'"-nostdlibinc", '// compileopts/config.go builder/picolibc.go
-  '';
-
+  allowGoReference = true;
+  tags = [ "llvm${llvmMajor}" ];
   subPackages = [ "." ];
-  nativeBuildInputs = [ makeWrapper ];
-  buildInputs = [ llvm clang-unwrapped ];
-  propagatedBuildInputs = [ lld avrgcc avrdude openocd gcc-arm-embedded ];
 
-  postInstall = ''
-    mkdir -p $out/share/tinygo
-    cp -a lib src targets $out/share/tinygo
-    wrapProgram $out/bin/tinygo --prefix "TINYGOROOT" : "$out/share/tinygo" \
-      --prefix "PATH" : "$out/libexec/tinygo"
-    mkdir -p $out/libexec/tinygo
-    ln -s ${clang-unwrapped}/bin/clang $out/libexec/tinygo/clang-10
-    ln -s ${lld}/bin/lld $out/libexec/tinygo/ld.lld-10
-    ln -sf $out/bin $out/share/tinygo
+  # Output contains static libraries for different arm cpus
+  # and stripping could mess up these so only strip the compiler
+  stripDebugList = [ "bin" ];
+
+  postConfigure = lib.optionalString stdenv.isDarwin ''
+    for i in vendor/tinygo.org/x/go-llvm/llvm_config_darwin*; do
+      substituteInPlace $i --replace "curses" "ncurses"
+    done
   '';
+
+  postPatch = ''
+    # Copy wasi-libc, symlink seems not working
+    rm -rf lib/wasi-libc/*
+    mkdir -p lib/wasi-libc/sysroot/lib/wasm32-wasi lib/wasi-libc/sysroot/include
+    cp -a ${wasi-libc}/lib/* lib/wasi-libc/sysroot/lib/wasm32-wasi/
+    cp -a ${wasi-libc.dev}/include/* lib/wasi-libc/sysroot/include/
+
+    # Borrow compiler-rt builtins from our source
+    # See https://github.com/tinygo-org/tinygo/pull/2471
+    mkdir -p lib/compiler-rt-builtins
+    cp -a ${compiler-rt.src}/compiler-rt/lib/builtins/* lib/compiler-rt-builtins/
+
+    substituteInPlace Makefile \
+      --replace "\$(TINYGO)" "$(pwd)/build/tinygo" \
+      --replace "build/release/tinygo/bin" "$out/bin" \
+      --replace "build/release/" "$out/share/"
+
+    # TODO: Fix mingw and darwin
+    # Disable windows and darwin cross-compile tests
+    sed -i "/GOOS=windows/d" Makefile
+    sed -i "/GOOS=darwin/d" Makefile
+
+    # tinygo needs versioned binaries
+    mkdir -p $out/libexec/tinygo
+    ln -s ${lib.getBin clang.cc}/bin/clang $out/libexec/tinygo/clang-${llvmMajor}
+    ln -s ${lib.getBin lld}/bin/ld.lld $out/libexec/tinygo/ld.lld-${llvmMajor}
+    ln -s ${lib.getBin lld}/bin/wasm-ld $out/libexec/tinygo/wasm-ld-${llvmMajor}
+    ln -s ${gdb}/bin/gdb $out/libexec/tinygo/gdb-multiarch
+  '' + lib.optionalString (stdenv.buildPlatform != stdenv.hostPlatform) ''
+    substituteInPlace Makefile \
+      --replace "./build/tinygo" "${buildPackages.tinygo}/bin/tinygo"
+  '';
+
+  preBuild = ''
+    export HOME=$TMPDIR
+    export GOCACHE=$TMPDIR/go-cache
+    export GOPATH=$TMPDIR/go
+    export PATH=$out/libexec/tinygo:$PATH
+  '';
+
+  postBuild = ''
+    # Move binary
+    mkdir -p build
+    mv $GOPATH/bin/tinygo build/tinygo
+
+    make gen-device
+  '';
+
+  checkPhase = ''
+    runHook preCheck
+    make smoketest XTENSA=0
+    runHook postCheck
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    make build/release
+
+    wrapProgram $out/bin/tinygo \
+      --set TINYGOROOT $out/share/tinygo \
+      --prefix PATH : ${lib.makeBinPath [ go avrdude openocd avrgcc binaryen ]}:$out/libexec/tinygo
+
+    runHook postInstall
+  '';
+
+  disallowedReferences = [ wasi-libc ];
 
   meta = with lib; {
     homepage = "https://tinygo.org/";
     description = "Go compiler for small places";
     license = licenses.bsd3;
-    maintainers = with maintainers; [ Madouura ];
+    maintainers = with maintainers; [ Madouura muscaln ];
+    broken = stdenv.isDarwin;
   };
 }
