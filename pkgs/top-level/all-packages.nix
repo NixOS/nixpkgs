dd04a90ee9480a54a714b3452f767cd66da3a247
zowoq <59103226+zowoq@users.noreply.github.com>
2022-01-27 10:19:43 +1000

    mandown: init at 0.1.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3366,6 +3366,8 @@ with pkgs;
 
   maliit-keyboard = libsForQt5.callPackage ../applications/misc/maliit-keyboard { };
 
+  mandown = callPackage ../tools/misc/mandown { };
+
   mapcidr = callPackage ../tools/misc/mapcidr { };
 
   mapproxy = callPackage ../applications/misc/mapproxy { };
