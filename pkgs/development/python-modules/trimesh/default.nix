8ca27a4fafdebeb621de994ebd8898afd69b7ad7
R. Ryantm <ryantm-bot@ryantm.com>
2022-01-27 04:19:10 +0000

    python310Packages.trimesh: 3.9.42 -> 3.9.43

--- pkgs/development/python-modules/trimesh/default.nix
+++ pkgs/development/python-modules/trimesh/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "trimesh";
-  version = "3.9.42";
+  version = "3.9.43";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "d9a6e977989f1f8344b1f86dbce0511dc431733deb0ad21ff8375f6f47282b39";
+    sha256 = "f7d4adf2df0fe19ea49c5f3268c33ffe28b3be818d280bb4c113d7463c58ddf9";
   };
 
   propagatedBuildInputs = [ numpy ];
