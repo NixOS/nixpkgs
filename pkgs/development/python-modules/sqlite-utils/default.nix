f329ced6f2fac212e341d00a6ff08502b36e7f8d
R. Ryantm <ryantm-bot@ryantm.com>
2022-01-27 15:22:16 +0000

    python310Packages.sqlite-utils: 3.22 -> 3.22.1

--- pkgs/development/python-modules/sqlite-utils/default.nix
+++ pkgs/development/python-modules/sqlite-utils/default.nix
@@ -14,12 +14,12 @@
 
 buildPythonPackage rec {
   pname = "sqlite-utils";
-  version = "3.22";
+  version = "3.22.1";
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "24803ea4d63e2123d2040db2da43fea95fabada80e1af1fe1da69643ae376689";
+    sha256 = "35ebb4f7e2b09f1818b36e7da6dc62f1ca57e2c0d99a9cbc8151e634d75a7906";
   };
 
   propagatedBuildInputs = [
