93c2a076113610f06851d0a699ef6c28bbf2c8fa
R. Ryantm <ryantm-bot@ryantm.com>
2022-01-29 03:20:19 +0000

    python310Packages.hahomematic: 0.27.2 -> 0.28.1

--- pkgs/development/python-modules/hahomematic/default.nix
+++ pkgs/development/python-modules/hahomematic/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "hahomematic";
-  version = "0.27.2";
+  version = "0.28.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.9";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "danielperna84";
     repo = pname;
     rev = version;
-    sha256 = "sha256-XBGA3wRZdl8rJ1hLLPPLK7E87Ggoly+kePbLY4x9/ZE=";
+    sha256 = "sha256-zYSJMP/uwgyIHdI8E7NHLaHrpAQeLpaEcXXXlzIO5ns=";
   };
 
   propagatedBuildInputs = [
