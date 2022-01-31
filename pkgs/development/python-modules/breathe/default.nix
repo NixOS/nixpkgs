6ef5dc851031a7015c5a59c7da33b9a2167cf8fd
R. Ryantm <ryantm-bot@ryantm.com>
2022-01-31 06:56:03 +0000

    python310Packages.breathe: 4.31.0 -> 4.32.0

--- pkgs/development/python-modules/breathe/default.nix
+++ pkgs/development/python-modules/breathe/default.nix
@@ -1,13 +1,13 @@
 { lib, fetchPypi, buildPythonPackage, docutils, six, sphinx, isPy3k, isPy27 }:
 
 buildPythonPackage rec {
-  version = "4.31.0";
+  version = "4.32.0";
   pname = "breathe";
   disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "925eeff96c6640cd857e4ddeae6f75464a1d5e2e08ee56dccce4043583ae2050";
+    sha256 = "sha256-U/OOoTT2GuisoBk7jYoe5KE+i7n4y/zz964bDGXUzTI=";
   };
 
   propagatedBuildInputs = [ docutils six sphinx ];
