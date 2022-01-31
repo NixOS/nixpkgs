28e0a553c58db369f83587f61a0b3074d8e1ebbf
Jakub Kądziołka <kuba@kadziolka.net>
2022-01-31 01:33:16 +0100

    ghidra: 10.1.1 -> 10.1.2

--- pkgs/tools/security/ghidra/default.nix
+++ pkgs/tools/security/ghidra/default.nix
@@ -24,12 +24,12 @@ let
 
 in stdenv.mkDerivation rec {
   pname = "ghidra";
-  version = "10.1.1";
-  versiondate = "20211221";
+  version = "10.1.2";
+  versiondate = "20220125";
 
   src = fetchzip {
     url = "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_${version}_build/ghidra_${version}_PUBLIC_${versiondate}.zip";
-    sha256 = "1aib24hjfavy31vq0pasbzix9lpqrb90m3hp4n0iakg6ck8jcl5r";
+    sha256 = "sha256-KSD+yJUqnMvWjnRNxv9S5echogBwcljGrjt0gNhtgGg=";
   };
 
   nativeBuildInputs = [
