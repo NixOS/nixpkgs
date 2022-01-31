be0b9a0b1d3d59f074276869f4d2838028114f8b
zowoq <59103226+zowoq@users.noreply.github.com>
2022-02-01 08:08:40 +1000

    shadowfox: update vendorSha256

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28756,7 +28756,9 @@ with pkgs;
 
   sfxr-qt = libsForQt5.callPackage ../applications/audio/sfxr-qt { };
 
-  shadowfox = callPackage ../tools/networking/shadowfox { };
+  shadowfox = callPackage ../tools/networking/shadowfox {
+    buildGoModule = buildGo117Module;
+  };
 
   shfmt = callPackage ../tools/text/shfmt { };
 
