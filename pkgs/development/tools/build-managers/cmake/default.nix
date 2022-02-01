29c409f82423042a367f73c14d6595c032a8302a
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-02-01 12:08:04 +0100

    cmake: 3.22.1 -> 3.22.2

--- pkgs/development/tools/build-managers/cmake/default.nix
+++ pkgs/development/tools/build-managers/cmake/default.nix
@@ -17,11 +17,11 @@ stdenv.mkDerivation rec {
     + lib.optionalString isBootstrap "-boot"
     + lib.optionalString useNcurses "-cursesUI"
     + lib.optionalString withQt5 "-qt5UI";
-  version = "3.22.1";
+  version = "3.22.2";
 
   src = fetchurl {
     url = "https://cmake.org/files/v${lib.versions.majorMinor version}/cmake-${version}.tar.gz";
-    sha256 = "sha256-DpmCKVSdez82hwPSDiSOfuH4U5ENQnBKqHkYwhPqgsA=";
+    sha256 = "sha256-PBxHi5ZQsQfUUsW9VFxy4vrU43wJuJoZhLmi9G32rO0=";
   };
 
   patches = [
