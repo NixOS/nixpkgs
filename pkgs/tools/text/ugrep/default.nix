76846dccc2a5be9ce3d5c0f700b6940b0eee7a5d
Renaud <c0bw3b@users.noreply.github.com>
2022-02-01 13:42:03 +0100

    ugrep: 3.7.0 -> 3.7.1

--- pkgs/tools/text/ugrep/default.nix
+++ pkgs/tools/text/ugrep/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "ugrep";
-  version = "3.7.0";
+  version = "3.7.1";
 
   src = fetchFromGitHub {
     owner = "Genivia";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-hQ4wgRT9Sft+ldmmrop8Kv195pVA6KHEuaovJB3RWhs=";
+    hash = "sha256-5ZlZ/nCUOiyOWagF1Vla945d7zKbMsHp56ZE4HwdEP4=";
   };
 
   buildInputs = [
