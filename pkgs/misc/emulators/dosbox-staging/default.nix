31b7fe92c030b3843d51d13225f1d384d23bb818
AndersonTorres <torres.anderson.85@protonmail.com>
2022-02-01 17:42:56 -0300

    dosbox-staging: munt is now libmt32emu

--- pkgs/misc/emulators/dosbox-staging/default.nix
+++ pkgs/misc/emulators/dosbox-staging/default.nix
@@ -1,6 +1,21 @@
-{ lib, fetchFromGitHub, stdenv
-, gtest, makeWrapper, meson, ninja, pkg-config
-, alsa-lib, fluidsynth, libGL, libGLU, libogg, libpng, munt, opusfile, SDL2, SDL2_net
+{ lib
+, stdenv
+, fetchFromGitHub
+, SDL2
+, SDL2_net
+, alsa-lib
+, fluidsynth
+, gtest
+, libGL
+, libGLU
+, libogg
+, libpng
+, makeWrapper
+, meson
+, libmt32emu
+, ninja
+, opusfile
+, pkg-config
 }:
 
 stdenv.mkDerivation rec {
@@ -14,34 +29,53 @@ stdenv.mkDerivation rec {
     sha256 = "07jwmmm1bhfxavlhl854cj8l5iy5hqx5hpwkkjbcwqg7yh9jfs2x";
   };
 
-  nativeBuildInputs = [ gtest makeWrapper meson ninja pkg-config ];
-  buildInputs = [ alsa-lib fluidsynth libGL libGLU libogg libpng munt opusfile SDL2 SDL2_net ];
+  nativeBuildInputs = [
+    gtest
+    makeWrapper
+    meson
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = [
+    SDL2
+    SDL2_net
+    alsa-lib
+    fluidsynth
+    libGL
+    libGLU
+    libmt32emu
+    libogg
+    libpng
+    opusfile
+  ];
 
   hardeningDisable = [ "format" ];
 
   mesonFlags = [
     "--buildtype=release"
-    "-Ddefault_library=static"
     "-Db_asneeded=true"
-    "-Dtry_static_libs=png"
+    "-Ddefault_library=static"
     "-Dfluidsynth:enable-floats=true"
     "-Dfluidsynth:try-static-deps=true"
+    "-Dtry_static_libs=png"
   ];
 
   postFixup = ''
-    # Rename binary, add a wrapper, and copy manual to avoid conflict with vanilla dosbox.
-    # Doing it this way allows us to work with frontends and launchers that expect the
-    # binary to be named dosbox, but get out of the way of vanilla dosbox if the user
-    # desires to install that as well.
+    # Rename binary, add a wrapper, and copy manual to avoid conflict with
+    # vanilla dosbox. Doing it this way allows us to work with frontends and
+    # launchers that expect the binary to be named dosbox, but get out of the
+    # way of vanilla dosbox if the user desires to install that as well.
+
     mv $out/bin/dosbox $out/bin/${pname}
     makeWrapper $out/bin/dosbox-staging $out/bin/dosbox
     cp $out/share/man/man1/dosbox.1.gz $out/share/man/man1/${pname}.1.gz
   '';
 
   meta = with lib; {
-    description = "A modernized DOS emulator";
     homepage = "https://dosbox-staging.github.io/";
-    license = licenses.gpl2;
+    description = "A modernized DOS emulator";
+    license = licenses.gpl2Plus;
     maintainers = with maintainers; [ joshuafern ];
     platforms = platforms.unix;
     priority = 101;
