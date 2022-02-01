9b3724dc64e686af720dfd2e3a5e72f628b92b06
Travis A. Everett <travis.a.everett@gmail.com>
2022-01-31 23:17:50 -0600

    sharness: use resholvePackage

--- pkgs/development/libraries/sharness/default.nix
+++ pkgs/development/libraries/sharness/default.nix
@@ -1,12 +1,19 @@
-{ stdenv
+{ resholvePackage
 , lib
 , fetchFromGitHub
 , fetchurl
 , perl
 , perlPackages
-, sharnessExtensions ? {} }:
+, sharnessExtensions ? {}
+, bash
+, coreutils
+, diffutils
+, findutils
+, ncurses
+, gnused
+}:
 
-stdenv.mkDerivation rec {
+resholvePackage rec {
   pname = "sharness";
   version = "1.1.0-dev";
 
@@ -40,6 +47,49 @@ stdenv.mkDerivation rec {
   '';
 
   doCheck = true;
+
+  solutions = {
+    sharness = {
+      scripts = [
+        "share/sharness/sharness.sh"
+        "share/sharness/lib-sharness/functions.sh"
+        "share/sharness/lib-sharness/aggregate-results.sh"
+      ];
+      # not bash specific, but the source seems to ~prefer bash?
+      # it sounds like you just source it, so this is mostly doc anyways
+      interpreter = "${bash}/bin/bash";
+      inputs = [
+        coreutils
+        bash
+        diffutils
+        findutils
+        ncurses
+        gnused
+      ];
+      fix = {
+        "$SHELL_PATH" = [ "sh" ];
+        "$TEST_CMP" = [ "'diff -u'" ];
+      };
+      keep = {
+        source = [
+          /*
+          not 100% sure, but it looks like it'll dynamically locate either the
+          store directory it's running from, or similar ~override paths in the
+          code under test...
+          */
+          "$SHARNESS_TEST_SRCDIR"
+          # file == individual files loaded from srcdir above
+          "$file"
+        ];
+      };
+      # TODO: drop when diffutils & ncurses are triaged in binlore / resholve
+      # manually checked for likely-execed arguments for now
+      execer = [
+        "cannot:${diffutils}/bin/diff"
+        "cannot:${ncurses}/bin/tput"
+      ];
+    };
+  };
 
   meta = with lib; {
     description = "Portable shell library to write, run and analyze automated tests adhering to Test Anything Protocol (TAP)";
