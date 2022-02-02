8e02e37bb25c007874fba4cd5578722a119e4535
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-02-02 21:16:29 +0100

    python3Packages.rflink: 0.0.58 -> 0.0.62

--- pkgs/development/python-modules/rflink/default.nix
+++ pkgs/development/python-modules/rflink/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "rflink";
-  version = "0.0.58";
+  version = "0.0.62";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "aequitas";
     repo = "python-rflink";
     rev = version;
-    sha256 = "1zab55lsw419gg0jfrl69ap6128vbi3wdmg5z7qin65ijpjdhasc";
+    sha256 = "sha256-dEzkYE8xtUzvdsnPaSiQR8960WLOEcr/QhwDiQlobcs=";
   };
 
   propagatedBuildInputs = [
@@ -32,15 +32,6 @@ buildPythonPackage rec {
 
   checkInputs = [
     pytestCheckHook
-  ];
-
-  patches = [
-    # Remove loop, https://github.com/aequitas/python-rflink/pull/61
-    (fetchpatch {
-      name = "remove-loop.patch";
-      url = "https://github.com/aequitas/python-rflink/commit/777e19b5bde3398df5b8f142896c34a01ae18d52.patch";
-      sha256 = "sJmihxY3fNSfZVFhkvQ/+9gysQup/1jklKDMyDDLOs8=";
-    })
   ];
 
   postPatch = ''
