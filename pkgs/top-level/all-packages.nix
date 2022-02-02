d48f5e54db1c0eccfe6f33c0469dba705a794c62
xrelkd <46590321+xrelkd@users.noreply.github.com>
2022-02-03 00:48:59 +0800

    fcitx5-hangul: init at 5.0.7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5332,6 +5332,8 @@ with pkgs;
 
   fcitx5-gtk = callPackage ../tools/inputmethods/fcitx5/fcitx5-gtk.nix { };
 
+  fcitx5-hangul = callPackage ../tools/inputmethods/fcitx5/fcitx5-hangul.nix { };
+
   fcitx5-rime = callPackage ../tools/inputmethods/fcitx5/fcitx5-rime.nix { };
 
   fcitx5-table-extra = callPackage ../tools/inputmethods/fcitx5/fcitx5-table-extra.nix { };
