1f699119beffcb3ea7700c8a73e3e00302800666
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-02 15:35:28 +0000

    python39Packages.pyefergy: 0.1.5 -> 22.1.1

--- pkgs/development/python-modules/pyefergy/default.nix
+++ pkgs/development/python-modules/pyefergy/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "pyefergy";
-  version = "0.1.5";
+  version = "22.1.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "tkdrob";
     repo = pname;
     rev = version;
-    sha256 = "sha256-8xcKgsZ6buaQdrKD8Qn7jB5IlQ0NkR0nZGuFk+Dd8Q8=";
+    sha256 = "sha256-AdoM+PcVoajxhnEfkyN9UuNufChu8XGmZDLNC3mjrps=";
   };
 
   propagatedBuildInputs = [
