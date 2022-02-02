a4c8d1308d8004efb96f7ecb1d5c35427aeece9e
Ashish SHUKLA <ashish.is@lostca.se>
2022-02-03 04:53:53 +0530

    tasktimer: init at 1.9.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30801,6 +30801,8 @@ with pkgs;
 
   tasknc = callPackage ../applications/misc/tasknc { };
 
+  tasktimer = callPackage ../applications/misc/tasktimer { };
+
   taskwarrior = callPackage ../applications/misc/taskwarrior { };
 
   taskwarrior-tui = callPackage ../applications/misc/taskwarrior-tui { };
