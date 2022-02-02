88326c18bcb4aefa2b7c85637bb841da389641ed
Fabian Affolter <mail@fabian-affolter.ch>
2022-02-02 18:07:09 +0100

    python3Packages.grapheme: disable tests

--- pkgs/development/python-modules/grapheme/default.nix
+++ pkgs/development/python-modules/grapheme/default.nix
@@ -1,8 +1,13 @@
-{ lib, buildPythonPackage, fetchPypi, pythonOlder }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "grapheme";
   version = "0.6.0";
+  format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
@@ -11,12 +16,18 @@ buildPythonPackage rec {
     sha256 = "1jiwc3w05c8kh22s3zk7a8km8na3plqc5zimb2qcyxxy3grbkhj4";
   };
 
-  pythonImportsCheck = [ "grapheme" ];
+  # Tests are no available on PyPI
+  # https://github.com/alvinlindstam/grapheme/issues/18
+  doCheck = false;
+
+  pythonImportsCheck = [
+    "grapheme"
+  ];
 
   meta = with lib; {
+    description = "Python package for grapheme aware string handling";
     homepage = "https://github.com/alvinlindstam/grapheme";
-    description = "A python package for grapheme aware string handling";
-    maintainers = with maintainers; [ creator54 ];
     license = licenses.mit;
+    maintainers = with maintainers; [ creator54 ];
   };
 }
