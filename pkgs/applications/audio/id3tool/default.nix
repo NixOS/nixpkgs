ab81ed6e634bd5251075e729397a226502e65204
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2022-02-02 00:19:01 -0500

    id3tool: minor style fixes

--- pkgs/applications/audio/id3tool/default.nix
+++ pkgs/applications/audio/id3tool/default.nix
@@ -1,21 +1,27 @@
-{ stdenv, lib, fetchurl }:
+{ lib, stdenv, fetchurl }:
 
 stdenv.mkDerivation {
-  name = "id3tool-1.2a";
+  pname = "id3tool";
+  version = "1.2a";
+
   src = fetchurl {
     url = "http://nekohako.xware.cx/id3tool/id3tool-1.2a.tar.gz";
     sha256 = "eQjWbFqr4qU66AGegjT0IxSF2Avksv5yydBAE8/xyuw=";
   };
 
   installPhase = ''
-    mkdir -p $out/bin
-    mv id3tool $out/bin
+    runHook preInstall
+
+    install -D id3tool "$out/bin/id3tool"
+
+    runHook postInstall
   '';
 
   meta = with lib; {
     description = "ID3 editing tool";
     homepage = "http://nekohako.xware.cx/id3tool/";
     license = licenses.bsd3;
+    maintainers = with maintainers; [  ];
     platforms = with platforms; linux ++ darwin;
   };
 }
