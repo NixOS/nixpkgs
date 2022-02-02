34c30740e910061e6bda19f3cb8ffe5d256b20b1
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-02 10:55:36 +0000

    python310Packages.pyrogram: 1.3.6 -> 1.4.0

--- pkgs/development/python-modules/pyrogram/default.nix
+++ pkgs/development/python-modules/pyrogram/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "pyrogram";
-  version = "1.3.6";
+  version = "1.4.0";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     pname = "Pyrogram";
     inherit version;
-    hash = "sha256-Q3JGTNSfqEMUqEru0crcbE8Ut/n+9MjFIDRKRSZ7tlE=";
+    hash = "sha256-VKhsC12xihe7omm/V0WENUU5M0weC6/02+raZh99Af8=";
   };
 
   propagatedBuildInputs = [
