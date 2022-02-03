9d38d2534fc2f3c5687f35deac9b649b45bfdf20
zowoq <59103226+zowoq@users.noreply.github.com>
2022-02-03 12:36:30 +1000

    linuxPackages.ena: 2.5.0 -> 2.6.1
    also add patch for kernel 5.15 support
    
    https://github.com/amzn/amzn-drivers/issues/200
    https://github.com/amzn/amzn-drivers/pull/204

--- pkgs/os-specific/linux/ena/default.nix
+++ pkgs/os-specific/linux/ena/default.nix
@@ -1,15 +1,23 @@
-{ lib, stdenv, fetchFromGitHub, kernel }:
+{ lib, stdenv, fetchFromGitHub, kernel, fetchpatch }:
 
 stdenv.mkDerivation rec {
-  version = "2.5.0";
+  version = "2.6.1";
   name = "ena-${version}-${kernel.version}";
 
   src = fetchFromGitHub {
     owner = "amzn";
     repo = "amzn-drivers";
     rev = "ena_linux_${version}";
-    sha256 = "sha256-uOf/1624UtjaZtrk7XyQpeUGdTNVDnzZJZMgU86i+SM=";
+    sha256 = "sha256-lvFsZGzgiD0YRrPFDa6EjM2b/IBRzN6nhc8I1j2Ptm8=";
   };
+
+  patches = [
+    # kernel 5.15 support https://github.com/amzn/amzn-drivers/pull/204
+    (fetchpatch {
+      url = "https://github.com/amzn/amzn-drivers/pull/204/commits/0f971febfa14bdd461e89c5ba940b429275720a5.patch";
+      sha256 = "sha256-Hyr3e0jPEy9/e8Fr5hgnLq0HZuVmyQBWWPLWh+GmqBc=";
+    })
+  ];
 
   hardeningDisable = [ "pic" ];
 
@@ -41,6 +49,6 @@ stdenv.mkDerivation rec {
     license = licenses.gpl2Only;
     maintainers = [ maintainers.eelco ];
     platforms = platforms.linux;
-    broken = kernel.kernelOlder "4.5" || kernel.kernelAtLeast "5.15";
+    broken = kernel.kernelOlder "4.5";
   };
 }
