193ae96eb29e5285cd848cfb8e0c0005ad5a6c77
Fabian Affolter <mail@fabian-affolter.ch>
2022-02-04 09:01:55 +0100

    python3Packages.weconnect-mqtt: 0.28.0 -> 0.28.1

--- pkgs/development/python-modules/weconnect-mqtt/default.nix
+++ pkgs/development/python-modules/weconnect-mqtt/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "weconnect-mqtt";
-  version = "0.28.0";
+  version = "0.28.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "tillsteinbach";
     repo = "WeConnect-mqtt";
     rev = "v${version}";
-    sha256 = "sha256-WrKJy4CxRJ/X599+95mfCQIhVcsLPnSPNtv3oAISSfU=";
+    sha256 = "sha256-YKZr+F64QMJQSURwvc86iaJqYBQrBNEHt8uJWfwz29Q=";
   };
 
   propagatedBuildInputs = [
@@ -33,7 +33,7 @@ buildPythonPackage rec {
       --replace "--cov=weconnect_mqtt --cov-config=.coveragerc --cov-report html" "" \
       --replace "pytest-cov" ""
     substituteInPlace requirements.txt \
-      --replace "weconnect[Images]~=0.33.0" "weconnect"
+      --replace "weconnect[Images]~=0.35.1" "weconnect"
   '';
 
   checkInputs = [
