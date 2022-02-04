4c8a021ea3eb08be8e477782502bddf0258a45ad
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-02-04 15:06:20 +0100

    php.packages.psysh: 0.11.0 -> 0.11.1

--- pkgs/development/php-packages/psysh/default.nix
+++ pkgs/development/php-packages/psysh/default.nix
@@ -1,10 +1,8 @@
 { mkDerivation, fetchurl, makeWrapper, lib, php }:
-let
+
+mkDerivation rec {
   pname = "psysh";
   version = "0.11.1";
-in
-mkDerivation {
-  inherit pname version;
 
   src = fetchurl {
     url = "https://github.com/bobthecow/psysh/releases/download/v${version}/psysh-v${version}.tar.gz";
@@ -16,16 +14,18 @@ mkDerivation {
   nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
+    runHook preInstall
     mkdir -p $out/bin
     tar -xzf $src -C $out/bin
     chmod +x $out/bin/psysh
     wrapProgram $out/bin/psysh --prefix PATH : "${lib.makeBinPath [ php ]}"
+    runHook postInstall
   '';
 
   meta = with lib; {
     description = "PsySH is a runtime developer console, interactive debugger and REPL for PHP.";
+    homepage = "https://github.com/bobthecow/psysh";
     license = licenses.mit;
-    homepage = "https://psysh.org/";
     maintainers = with maintainers; [ caugner ] ++ teams.php.members;
   };
 }
