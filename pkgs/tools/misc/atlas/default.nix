41c73868c9394a97cd0e5193c941bba95e46261e
Akira Komamura <akira.komamura@gmail.com>
2022-02-04 18:45:43 +0900

    atlas: 0.3.2 -> 0.3.3

--- pkgs/tools/misc/atlas/default.nix
+++ pkgs/tools/misc/atlas/default.nix
@@ -1,12 +1,12 @@
 { lib, buildGoPackage, fetchFromGitHub }:
 buildGoPackage rec {
   pname = "atlas";
-  version = "0.3.2";
+  version = "0.3.3";
   src = fetchFromGitHub {
     owner = "ariga";
     repo = "atlas";
     rev = "v${version}";
-    sha256 = "1wxfji28r100qnmryfixhg2n2mcvvl65vv50fqzhh2q576c0sh5h";
+    sha256 = "18ilqrnhm48bi511l9wb2vhcyyp12ajn9ks4s3jk567v0j8bph6b";
   };
 
   goPackagePath = "ariga.io/atlas";
