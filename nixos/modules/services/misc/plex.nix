f4cadcfcae5a81948ddb940233634b492b4b6219
afreakk <laderud@hotmail.com>
2022-02-06 18:35:58 +0100

    plex: extraPlugins example

--- nixos/modules/services/misc/plex.nix
+++ nixos/modules/services/misc/plex.nix
@@ -63,6 +63,19 @@ in
           this behavior is undesired, set <option>managePlugins</option> to
           false.
         '';
+        example = literalExpression ''
+          [
+            (builtins.path {
+              name = "Audnexus.bundle";
+              path = pkgs.fetchFromGitHub {
+                owner = "djdembeck";
+                repo = "Audnexus.bundle";
+                rev = "v0.2.8";
+                sha256 = "sha256-IWOSz3vYL7zhdHan468xNc6C/eQ2C2BukQlaJNLXh7E=";
+              };
+            })
+          ]
+        '';
       };
 
       extraScanners = mkOption {
