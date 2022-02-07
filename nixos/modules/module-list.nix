0a6d22c6c3f51407317512cf4c44335f6db664ef
06kellyjac <dev@j-k.io>
2022-02-07 16:03:44 +0000

    nixos/agate: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1055,6 +1055,7 @@
   ./services/web-apps/wordpress.nix
   ./services/web-apps/youtrack.nix
   ./services/web-apps/zabbix.nix
+  ./services/web-servers/agate.nix
   ./services/web-servers/apache-httpd/default.nix
   ./services/web-servers/caddy/default.nix
   ./services/web-servers/darkhttpd.nix
