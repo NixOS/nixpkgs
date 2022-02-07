6df6f276a36d2cc18472e467f94226dd2795f6ba
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2022-02-06 16:15:14 -0800

    witness: init at 0.1.1
    Co-authored-by: zowoq <59103226+zowoq@users.noreply.github.com>
    Co-authored-by: Patrick Hilhorst <git@hilhorst.be>
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10851,6 +10851,8 @@ with pkgs;
     SDL = SDL_sixel;
   };
 
+  witness = callPackage ../tools/security/witness { };
+
   openconnect = openconnect_gnutls;
 
   openconnect_openssl = callPackage ../tools/networking/openconnect {
