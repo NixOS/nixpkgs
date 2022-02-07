a64c0831b3a6405dba3b94f55ecd72de6f0d76c1
zoedsoupe <zoey.spessanha@outlook.com>
2022-02-07 09:33:52 -0300

    fix: pr issues

--- pkgs/development/web/bloomrpc/default.nix
+++ pkgs/development/web/bloomrpc/default.nix
@@ -1,9 +1,8 @@
-{ lib, fetchurl, appimageTools, pkgs }:
+{ lib, fetchurl, appimageTools }:
 
 let
   pname = "bloomrpc";
   version = "1.5.3";
-  name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://github.com/uw-labs/${pname}/releases/download/${version}/BloomRPC-${version}.AppImage";
@@ -21,7 +20,7 @@ in appimageTools.wrapType2 {
   extraPkgs = pkgs: appimageTools.defaultFhsEnvArgs.multiPkgs pkgs ++ [ pkgs.bash ];
 
   extraInstallCommands = ''
-    ln -s $out/bin/${name} $out/bin/${pname}
+    ln -s $out/bin/${pname}-${version} $out/bin/${pname}
     install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
     install -m 444 -D ${appimageContents}/${pname}.png \
       $out/share/icons/hicolor/512x512/apps/${pname}.png
@@ -30,8 +29,10 @@ in appimageTools.wrapType2 {
   '';
 
   meta = with lib; {
-    longDescription = "Inspired by Postman and GraphQL Playground
-BloomRPC aims to provide the simplest and most efficient developer experience for exploring and querying your GRPC services.";
+    description = "GUI Client for GRPC Services";
+    longDescription = ''
+    Inspired by Postman and GraphQL Playground BloomRPC aims to provide the simplest and most efficient developer experience for exploring and querying your GRPC services.
+    '';
     homepage = "https://github.com/uw-labs/bloomrpc";
     license = licenses.lgpl3Plus;
     maintainers = with maintainers; [ zoedsoupe ];
