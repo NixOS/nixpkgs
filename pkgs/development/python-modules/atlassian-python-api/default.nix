092fe80925dc09e2fd187d90f5f21712381e72e3
Fabian Affolter <mail@fabian-affolter.ch>
2022-02-08 17:55:18 +0100

    python3Packages.atlassian-python-api: add pythonImportsCheck

--- pkgs/development/python-modules/atlassian-python-api/default.nix
+++ pkgs/development/python-modules/atlassian-python-api/default.nix
@@ -7,11 +7,15 @@
 , requests_oauthlib
 , six
 , pytestCheckHook
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "atlassian-python-api";
   version = "3.19.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "atlassian-api";
@@ -20,16 +24,26 @@ buildPythonPackage rec {
     sha256 = "sha256-SJsqk8TM+5UztN1ZDyYrOjNIWDLhm5XtLxPflIGPxKQ=";
   };
 
+  propagatedBuildInputs = [
+    deprecated
+    oauthlib
+    requests
+    requests_oauthlib
+    six
+  ];
+
   checkInputs = [
     pytestCheckHook
   ];
 
-  propagatedBuildInputs = [ deprecated oauthlib requests requests_oauthlib six ];
+  pythonImportsCheck = [
+    "atlassian"
+  ];
 
   meta = with lib; {
     description = "Python Atlassian REST API Wrapper";
     homepage = "https://github.com/atlassian-api/atlassian-python-api";
     license = licenses.asl20;
-    maintainers = [ maintainers.arnoldfarkas ];
+    maintainers = with maintainers; [ arnoldfarkas ];
   };
 }
