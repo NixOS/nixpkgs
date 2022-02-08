c72b0cdab84595b98025b83c12ccd7fbad01d3a9
Jörg Thalheim <joerg@thalheim.io>
2022-02-08 15:32:29 +0100

    jetbrains: allow to override JAVA_HOME/ANDROID_JAVA_HOME/JDK_HOME
    Otherwise one cannot use a different java version i.e. jdk17

--- pkgs/applications/editors/jetbrains/common.nix
+++ pkgs/applications/editors/jetbrains/common.nix
@@ -82,10 +82,10 @@ with stdenv; lib.makeOverridable mkDerivation (rec {
         libnotify
       ] ++ extraLdPath)}" \
       ${lib.concatStringsSep " " extraWrapperArgs} \
-      --set JDK_HOME "$jdk" \
+      --set-default JDK_HOME "$jdk" \
+      --set-default ANDROID_JAVA_HOME "$jdk" \
+      --set-default JAVA_HOME "$jdk" \
       --set ${hiName}_JDK "$jdk" \
-      --set ANDROID_JAVA_HOME "$jdk" \
-      --set JAVA_HOME "$jdk" \
       --set ${hiName}_VM_OPTIONS ${vmoptsFile}
 
     ln -s "$item/share/applications" $out/share
