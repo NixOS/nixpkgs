d43e4c0a9a86c5520d0890a8ac2a8052b50ad757
Robert Schütz <nix@dotlambda.de>
2022-02-08 20:56:19 +0000

    python3Packages.django_3: fix pname
    (cherry picked from commit 8b4354e860c87415b731af4f7fda2ed4204fb904)

--- pkgs/development/python-modules/django/3.nix
+++ pkgs/development/python-modules/django/3.nix
@@ -12,13 +12,14 @@
 }:
 
 buildPythonPackage rec {
-  pname = "Django";
+  pname = "django";
   version = "3.2.12";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
-    inherit pname version;
+    pname = "Django";
+    inherit version;
     sha256 = "sha256-l3Lmk1cD5Z6ZOWCDLWamFM8CM6HFEjvGIk7MataeQeI=";
   };
 
