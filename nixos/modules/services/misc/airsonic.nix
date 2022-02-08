00a0c8ba0978dfb3e6af13899a2015bfddadc29e
Jonathan Strickland <djanatyn@gmail.com>
2022-02-08 15:18:56 -0500

    airsonic: new docs for airsonic.listenAddress
    Co-authored-by: Ted Reed <ted.reed@gmail.com>

--- nixos/modules/services/misc/airsonic.nix
+++ nixos/modules/services/misc/airsonic.nix
@@ -38,6 +38,11 @@ in {
         default = "127.0.0.1";
         description = ''
           The host name or IP address on which to bind Airsonic.
+          The default value is appropriate for first launch, when the
+          default credentials are easy to guess. It is also appropriate
+          if you intend to use the virtualhost option in the service
+          module. In other cases, you may want to change this to a
+          specific IP or 0.0.0.0 to listen on all interfaces.
         '';
       };
 
