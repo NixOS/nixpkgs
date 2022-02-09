f066dddaa5cb6aea4429cd9386d4aa6f3963a889
Robert Hensing <robert@roberthensing.nl>
2022-02-09 12:35:35 +0100

    hello: Make pname overridable without breaking src.url

--- pkgs/applications/misc/hello/default.nix
+++ pkgs/applications/misc/hello/default.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation (finalAttrs: {
   version = "2.12";
 
   src = fetchurl {
-    url = "mirror://gnu/hello/${finalAttrs.pname}-${finalAttrs.version}.tar.gz";
+    url = "mirror://gnu/hello/hello-${finalAttrs.version}.tar.gz";
     sha256 = "1ayhp9v4m4rdhjmnl2bq3cibrbqqkgjbl3s7yk2nhlh8vj3ay16g";
   };
 
