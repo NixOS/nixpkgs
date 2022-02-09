e646b64ebbfd11f901e6a601a1ad422cd71f2da3
Jean-Francois Chevrette <jfchevrette@gmail.com>
2022-02-09 18:03:04 -0500

    jless: init at 0.7.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6770,6 +6770,8 @@ with pkgs;
 
   jl = haskellPackages.callPackage ../development/tools/jl { };
 
+  jless = callPackage ../development/tools/jless { };
+
   jmespath = callPackage ../development/tools/jmespath { };
 
   jmtpfs = callPackage ../tools/filesystems/jmtpfs { };
