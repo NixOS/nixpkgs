b3adf9bbb6137cd211be0a4078fe9e58129b9ef5
Seong Yong-ju <sei40kr@gmail.com>
2022-02-11 00:00:53 +0900

    vscode-extensions.rust-lang.rust: init at 0.7.8

--- pkgs/misc/vscode-extensions/default.nix
+++ pkgs/misc/vscode-extensions/default.nix
@@ -1497,6 +1497,18 @@ let
         };
       };
 
+      rust-lang.rust = buildVscodeMarketplaceExtension {
+        mktplcRef = {
+          name = "rust";
+          publisher = "rust-lang";
+          version = "0.7.8";
+          sha256 = "sha256-Y33agSNMVmaVCQdYd5mzwjiK5JTZTtzTkmSGTQrSNg0=";
+        };
+        meta = {
+          license = lib.licenses.mit;
+        };
+      };
+
       matklad.rust-analyzer = callPackage ./rust-analyzer { };
 
       ocamllabs.ocaml-platform = buildVscodeMarketplaceExtension {
