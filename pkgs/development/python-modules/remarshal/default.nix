35cd881e3f259836f034330634920aef9c303490
zowoq <59103226+zowoq@users.noreply.github.com>
2022-02-11 09:16:45 +1000

    remarshal: fix build

--- pkgs/development/python-modules/remarshal/default.nix
+++ pkgs/development/python-modules/remarshal/default.nix
@@ -31,7 +31,8 @@ buildPythonApplication rec {
   postPatch = ''
     substituteInPlace pyproject.toml \
       --replace "poetry.masonry.api" "poetry.core.masonry.api" \
-      --replace 'PyYAML = "^5.3"' 'PyYAML = "*"'
+      --replace 'PyYAML = "^5.3"' 'PyYAML = "*"' \
+      --replace 'tomlkit = "^0.7"' 'tomlkit = "*"'
   '';
 
   nativeBuildInputs = [
