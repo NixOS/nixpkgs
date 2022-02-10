ec5c5720ab3b9b6f3f92c332cefe5b76b297228c
zowoq <59103226+zowoq@users.noreply.github.com>
2022-02-10 17:56:58 +1000

    nixos/amazon-image: use 5_10 kernel and add assert

--- nixos/modules/virtualisation/amazon-image.nix
+++ nixos/modules/virtualisation/amazon-image.nix
@@ -37,7 +37,12 @@ in
       { assertion = cfg.efi -> cfg.hvm;
         message = "EC2 instances using EFI must be HVM instances.";
       }
+      { assertion = versionOlder config.boot.kernelPackages.kernel.version "5.15";
+        message = "ENA driver fails to build with kernel >= 5.15";
+      }
     ];
+
+    boot.kernelPackages = pkgs.linuxKernel.packages.linux_5_10;
 
     boot.growPartition = cfg.hvm;
 
