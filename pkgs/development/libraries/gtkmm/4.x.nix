526f85503ca0f8357618e4ef51d22037dcb3ae22
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-10 06:52:47 +0000

    gtkmm4: 4.4.0 -> 4.6.0

--- pkgs/development/libraries/gtkmm/4.x.nix
+++ pkgs/development/libraries/gtkmm/4.x.nix
@@ -17,13 +17,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gtkmm";
-  version = "4.4.0";
+  version = "4.6.0";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "LrRkMmCW5qQMgunNB0Fk2BA/teB4ZWecCmSeQXRwDdo=";
+    sha256 = "E1OgkJPLVx71rAXZPSALrxMq1gS19JQHd2VuFQWBTB8=";
   };
 
   nativeBuildInputs = [
