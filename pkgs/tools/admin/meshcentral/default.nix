3c111976387854a43d8ca8cb37105b5715d18d31
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-10 09:08:18 +0000

    meshcentral: 0.9.59 -> 0.9.79
    (cherry picked from commit 15a8db204eb6145fd3abd2ce92762bcac4d1e5e0)

--- pkgs/tools/admin/meshcentral/default.nix
+++ pkgs/tools/admin/meshcentral/default.nix
@@ -1,11 +1,11 @@
 { lib, fetchpatch, fetchzip, yarn2nix-moretea, nodejs, jq, dos2unix }:
 
 yarn2nix-moretea.mkYarnPackage rec {
-  version = "0.9.59";
+  version = "0.9.79";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/meshcentral/-/meshcentral-${version}.tgz";
-    sha256 = "05dalrm82mspqrjqb3ya7cjd3vbn1a4wij8cdndfyh0rrbwvglys";
+    sha256 = "17f34ifzdrkbap2hhd0y0rdcn8j0svxzsqw0qhcp3h68z3098hdv";
   };
 
   packageJSON = ./package.json;
