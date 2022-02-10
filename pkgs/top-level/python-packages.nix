bde9bd867b29d29445231cb60ab7a4f8757fbc19
Fabian Affolter <mail@fabian-affolter.ch>
2022-02-10 13:24:20 +0100

    python310Packages.strenum: init at 0.4.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9494,6 +9494,8 @@ in {
 
   streamz = callPackage ../development/python-modules/streamz { };
 
+  strenum =  callPackage ../development/python-modules/strenum { };
+
   strict-rfc3339 = callPackage ../development/python-modules/strict-rfc3339 { };
 
   strictyaml = callPackage ../development/python-modules/strictyaml { };
