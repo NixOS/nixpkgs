a164fb2eddd21b83001520d7204647ff0280a72a
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-11 04:55:31 +0000

    python310Packages.roonapi: 0.0.39 -> 0.1.1

--- pkgs/development/python-modules/roonapi/default.nix
+++ pkgs/development/python-modules/roonapi/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "roonapi";
-  version = "0.0.39";
+  version = "0.1.1";
   format = "pyproject";
   disabled = pythonOlder "3.7";
 
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "pavoni";
     repo = "pyroon";
     rev = version;
-    sha256 = "03m00qbdkm0vdflc48ds186kh9qjxqr6z602yn3l6fzj5f93zgh5";
+    sha256 = "sha256-GEgm250uALTXIEMBWmluqGw/dw2TfGmUIcItfzonGkU=";
   };
 
   nativeBuildInputs = [
