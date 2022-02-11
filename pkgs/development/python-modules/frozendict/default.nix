16056114170a9944506393d780570ac1657a41f4
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-11 04:18:04 +0000

    python310Packages.frozendict: 2.2.0 -> 2.3.0

--- pkgs/development/python-modules/frozendict/default.nix
+++ pkgs/development/python-modules/frozendict/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "frozendict";
-  version = "2.2.0";
+  version = "2.3.0";
   format = "setuptools";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-jj1HNfmhPRB3Vn5WhHFmPzJE+FrImyP4yzHPIx2+Rbk=";
+    sha256 = "sha256-2kIxre/Fko54ENonMiadOte1YWKVs+aTdGOSqCBeoLU=";
   };
 
   pythonImportsCheck = [
