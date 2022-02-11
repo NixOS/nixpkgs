cbd85bcda4781101476fe22d98190ec9eec04576
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-11 14:14:29 +0000

    elementary-planner: 2.7 -> 3.0.5

--- pkgs/applications/office/elementary-planner/default.nix
+++ pkgs/applications/office/elementary-planner/default.nix
@@ -1,4 +1,5 @@
-{ lib, stdenv
+{ stdenv
+, lib
 , fetchFromGitHub
 , meson
 , ninja
@@ -7,32 +8,26 @@
 , python3
 , vala
 , wrapGAppsHook
-, evolution-data-server
-, libical
 , libgee
 , json-glib
 , glib
-, glib-networking
 , sqlite
-, libsoup
 , libgdata
 , gtk3
-, pantheon /* granite, icons, maintainers */
+, pantheon /* granite, icons */
 , webkitgtk
-, libpeas
 , libhandy
-, curl
 }:
 
 stdenv.mkDerivation rec {
   pname = "elementary-planner";
-  version = "2.7";
+  version = "3.0.5";
 
   src = fetchFromGitHub {
     owner = "alainm23";
     repo = "planner";
     rev = version;
-    sha256 = "sha256-3eFPGRcZWhzFYi52TbHmpFNLI0pWYcHbbBI7efqZwYE=";
+    sha256 = "sha256-Gba99ZNaQLAkHO/xGbXKljpph5Ckl+kXOsHVARRHkCo=";
   };
 
   nativeBuildInputs = [
@@ -46,21 +41,15 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
-    evolution-data-server
     glib
-    glib-networking
     gtk3
     json-glib
     libgee
-    libical
-    libpeas
-    libsoup
+    libhandy
     pantheon.elementary-icon-theme
     pantheon.granite
     sqlite
     webkitgtk
-    libhandy
-    curl
   ];
 
   postPatch = ''
@@ -80,12 +69,11 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with lib; {
-    description = "Task manager with Todoist support designed for GNU/Linux 🚀️";
+    description = "Task manager with Todoist support designed for GNU/Linux";
     homepage = "https://planner-todo.web.app";
-    license = licenses.gpl3;
+    license = licenses.gpl3Plus;
     maintainers = with maintainers; [ dtzWill ] ++ teams.pantheon.members;
     platforms = platforms.linux;
     mainProgram = "com.github.alainm23.planner";
   };
 }
-
