db1bda5a61f09ad468f71566cbbb47c100567b6d
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-11 09:43:44 +0000

    duckdb: 0.3.1 -> 0.3.2

--- pkgs/development/libraries/duckdb/default.nix
+++ pkgs/development/libraries/duckdb/default.nix
@@ -5,13 +5,13 @@
 
 stdenv.mkDerivation rec {
   pname = "duckdb";
-  version = "0.3.1";
+  version = "0.3.2";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-apTU7WgKw/YEnT4maibyffqOrCoVwHPOkNINlAmtYYI=";
+    sha256 = "sha256-F5YOqDeY3rgcnuu5SNqOfUxhsaXgqvdJZTnD1unI0tc=";
   };
 
   nativeBuildInputs = [ cmake ];
