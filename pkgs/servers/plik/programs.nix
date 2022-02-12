ba27973aaf3ec2ec589077947aad18df7f5d0652
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-12 22:59:32 +0000

    plik: 1.3.4 -> 1.3.5

--- pkgs/servers/plik/programs.nix
+++ pkgs/servers/plik/programs.nix
@@ -1,13 +1,13 @@
 { lib, buildGoModule, fetchFromGitHub, fetchurl, makeWrapper, runCommand }:
 
 let
-  version = "1.3.4";
+  version = "1.3.5";
 
   src = fetchFromGitHub {
     owner = "root-gg";
     repo = "plik";
     rev = version;
-    sha256 = "0kmcidnjw26vnxx9h3swcg72i507awg89s4nfxw6rwbyw36iiiqf";
+    sha256 = "sha256-YT+hIh68foHD+WcprFL+Z1eddUYmay0IJ3YGGy7NZbY=";
   };
 
   vendorSha256 = null;
