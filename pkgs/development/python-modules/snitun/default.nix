aa1eede1a15d2bc8c38bc6ff54c074ea456eadb1
Fabian Affolter <mail@fabian-affolter.ch>
2022-02-12 13:51:09 +0100

    python3Packages.snitun: 0.30.0 -> 0.31.0

--- pkgs/development/python-modules/snitun/default.nix
+++ pkgs/development/python-modules/snitun/default.nix
@@ -7,17 +7,21 @@
 , fetchFromGitHub
 , pytest-aiohttp
 , pytestCheckHook
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "snitun";
-  version = "0.30.0";
+  version = "0.31.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "NabuCasa";
     repo = pname;
     rev = version;
-    sha256 = "sha256-IjdgxX6ed9IWMFaMXIXQWZWoODrZBzXtMAcMOIhPFVQ=";
+    sha256 = "sha256-Ehafb35H462Ffn6omGh/MDJKQX5qJJZeiIBO3n0IGlA=";
   };
 
   propagatedBuildInputs = [
@@ -31,6 +35,10 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
+  pythonImportsCheck = [
+    "snitun"
+  ];
+
   disabledTests = lib.optionals stdenv.isDarwin [
     "test_multiplexer_data_channel_abort_full" # https://github.com/NabuCasa/snitun/issues/61
     # port binding conflicts
@@ -40,13 +48,11 @@ buildPythonPackage rec {
     "test_peer_listener_timeout"
   ];
 
-  pythonImportsCheck = [ "snitun" ];
-
   meta = with lib; {
-    homepage = "https://github.com/nabucasa/snitun";
     description = "SNI proxy with TCP multiplexer";
+    homepage = "https://github.com/nabucasa/snitun";
     license = licenses.gpl3Only;
-    maintainers = with maintainers; [ Scriptkiddi ];
+    maintainers = with maintainers; [ fab Scriptkiddi ];
     platforms = platforms.linux;
   };
 }
