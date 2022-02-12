272820e6c45bb78e7bba61f4b54abfd31d87eda4
Julien Moutinho <julm+nixpkgs@sourcephile.fr>
2022-02-12 01:42:40 +0100

    sourcehut.listssrht: 0.51.0 -> 0.51.7

--- pkgs/applications/version-management/sourcehut/lists.nix
+++ pkgs/applications/version-management/sourcehut/lists.nix
@@ -12,13 +12,13 @@
 
 buildPythonPackage rec {
   pname = "listssrht";
-  version = "0.51.0";
+  version = "0.51.7";
 
   src = fetchFromSourcehut {
     owner = "~sircmpwn";
     repo = "lists.sr.ht";
     rev = version;
-    sha256 = "sha256-iywZ6G5E4AJevg/Q1LoB7JMJxBcsAnbhiND++mFy/bw=";
+    sha256 = "sha256-oNY5A98oVoL2JKO0fU/8YVl8u7ywmHb/RHD8A6z9yIM=";
   };
 
   patches = [
