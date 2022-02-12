b8e58a22600676e995158f78c37701354469affa
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-12 06:11:16 +0000

    python310Packages.types-protobuf: 3.19.11 -> 3.19.12

--- pkgs/development/python-modules/types-protobuf/default.nix
+++ pkgs/development/python-modules/types-protobuf/default.nix
@@ -6,12 +6,12 @@
 
 buildPythonPackage rec {
   pname = "types-protobuf";
-  version = "3.19.11";
+  version = "3.19.12";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-KveYV1TMFtD9Zpfk/aTol87721npYkqtZXLRwucAeHs=";
+    sha256 = "sha256-sCIkc0dHEhmsvi/mjS82I9eIJS6Z6blzWLerkE0ming=";
   };
 
   propagatedBuildInputs = [
