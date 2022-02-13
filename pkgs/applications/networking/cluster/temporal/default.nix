4892a1fea933a2bce33f28b42a554aa6de45e46d
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-13 09:54:24 +0000

    temporal: 1.12.0 -> 1.14.5

--- pkgs/applications/networking/cluster/temporal/default.nix
+++ pkgs/applications/networking/cluster/temporal/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "temporal";
-  version = "1.12.0";
+  version = "1.14.5";
 
   src = fetchFromGitHub {
     owner = "temporalio";
     repo = "temporal";
     rev = "v${version}";
-    sha256 = "1gdks7pzaqrsdihh2m3v597x0dw2qww95jlznj0h112jgicanimj";
+    sha256 = "sha256-unRJkJzi1l7GGkLMb3clIaumcb1Jsi1mUox9kevqIjg=";
   };
 
-  vendorSha256 = "sha256-dGmd6tCUKoK4uwhB5kXGOpXemtLn0VssabDE4iQWEAw=";
+  vendorSha256 = "sha256-k9F1cY1FCNLPGoCd/i4dg7CfPsuiq6Y9nni8v69Lra8=";
 
   # Errors:
   #  > === RUN   TestNamespaceHandlerGlobalNamespaceDisabledSuite
