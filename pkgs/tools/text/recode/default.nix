ca63f0b6bab125367d9ae4983e4109851c8366c6
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-13 02:39:14 +0000

    recode: 3.7.9 -> 3.7.11

--- pkgs/tools/text/recode/default.nix
+++ pkgs/tools/text/recode/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   pname = "recode";
-  version = "3.7.9";
+  version = "3.7.11";
 
   # Use official tarball, avoid need to bootstrap/generate build system
   src = fetchurl {
     url = "https://github.com/rrthomas/${pname}/releases/download/v${version}/${pname}-${version}.tar.gz";
-    sha256 = "sha256-5DIKaw9c2DfNtFT7WFQBjd+pcJEWCOHwHMLGX2M2csQ=";
+    sha256 = "sha256-lyZ6Dm7j2Fm39NFZMoKQDbx5gVG3Cm0fc3GIgFY7SF4=";
   };
 
   nativeBuildInputs = [ python3 python3.pkgs.cython perl intltool flex texinfo libiconv ];
