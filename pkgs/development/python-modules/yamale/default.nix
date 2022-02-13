de8a764559cf56bbedd16fa443ca1756c964946f
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-13 12:40:54 +0000

    yamale: 4.0.2 -> 4.0.3

--- pkgs/development/python-modules/yamale/default.nix
+++ pkgs/development/python-modules/yamale/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "yamale";
-  version = "4.0.2";
+  version = "4.0.3";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "23andMe";
     repo = pname;
     rev = version;
-    sha256 = "sha256-hFBU3o3HpL0Schgzcx3oYq0IAUVGKThIfEteYcFbLnk=";
+    sha256 = "sha256-EkCKUSPRrj3g2AY17tquBDxf+nWfpdnILu5AS/2SsLo=";
   };
 
   propagatedBuildInputs = [
