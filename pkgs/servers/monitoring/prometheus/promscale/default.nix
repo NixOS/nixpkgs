bfcd989454921a20a81ab16d2032204afe5697df
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-13 00:19:34 +0000

    promscale: 0.8.0 -> 0.9.0

--- pkgs/servers/monitoring/prometheus/promscale/default.nix
+++ pkgs/servers/monitoring/prometheus/promscale/default.nix
@@ -7,20 +7,20 @@
 
 buildGoModule rec {
   pname = "promscale";
-  version = "0.8.0";
+  version = "0.9.0";
 
   src = fetchFromGitHub {
     owner = "timescale";
     repo = pname;
     rev = version;
-    sha256 = "sha256-h76NHEPY3ABq2NbRQXNR+zSkriBasi550rfSkl3Xdas=";
+    sha256 = "sha256-snbQVkJ4J5ElVNfHuSfb7VCZ64TqJ8Lx5uUaJPqBHl4=";
   };
 
   patches = [
     ./0001-remove-jaeger-test-dep.patch
   ];
 
-  vendorSha256 = "sha256-PxmTS8fSh21BcLS4PsSfHhKOXWWJLboPR6E8/Jx/UGY=";
+  vendorSha256 = "sha256-1t4WNoJrfKTtrpwi9p+L1WQR7mTsD70CRW+RYT7E9Lo=";
 
   ldflags = [
     "-s"
