1eb1e3bf959fde09708c5f09c72067ddfc62097b
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-02-13 03:20:52 +0100

    prometheus-pihole-exporter: 0.1.1 -> 0.2.0
    https://github.com/eko/pihole-exporter/releases/tag/v0.2.0

--- pkgs/servers/monitoring/prometheus/pihole-exporter.nix
+++ pkgs/servers/monitoring/prometheus/pihole-exporter.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "pihole-exporter";
-  version = "0.1.1";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "eko";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-MkWGJks0Ol4bEbD+k72zEFP09f5eSN7y9Jhhzpu/Uyc=";
+    sha256 = "sha256-JxznxE4Pq1fhlt3l1jbGWD5eUg5VF0GmewkuSYECG0Y=";
   };
 
-  vendorSha256 = "sha256-+zI0cGzTs4+Fco/qh8bhWLkfgxpEdcX4BOIVdV1f8ew=";
+  vendorSha256 = "sha256-jfpM192LtFGVgsVv+F+P8avTGD5c8I+7JFsn4oVoqr0=";
 
   meta = with lib; {
     description = "Prometheus exporter for PI-Hole's Raspberry PI ad blocker";
