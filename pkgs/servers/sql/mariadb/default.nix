fed91582ae90f3f496c7ccac1a27bb0f8a3adeb7
ajs124 <git@ajs124.de>
2022-02-14 22:45:59 +0100

    mariadb_106: 10.6.6 -> 10.6.7
    (cherry picked from commit 45ab3705c8b651bf386b1c7ee47c45f92a2600e6)

--- pkgs/servers/sql/mariadb/default.nix
+++ pkgs/servers/sql/mariadb/default.nix
@@ -28,11 +28,11 @@ mariadb = server // {
 };
 
 common = rec { # attributes common to both builds
-  version = "10.6.5";
+   version = "10.6.7";
 
   src = fetchurl {
     url = "https://downloads.mariadb.com/MariaDB/mariadb-${version}/source/mariadb-${version}.tar.gz";
-    sha256 = "sha256-4L4EBCjZpCqLtL0iG1Z/8lIs1vqJBjhic9pPA8XCCo8=";
+    sha256 = "1idjnkjfkjvyr6r899xbiwq9wwbs84cm85mbc725yxjshqghzvkm";
   };
 
   nativeBuildInputs = [ cmake pkg-config ]
