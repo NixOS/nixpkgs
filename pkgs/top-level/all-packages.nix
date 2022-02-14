88cea4d9726ba43e1759a07b45ef1aaf018fc468
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2022-02-15 05:41:37 +0800

    xrootd: init at 5.4.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1083,6 +1083,8 @@ with pkgs;
 
   xcd = callPackage ../tools/misc/xcd { };
 
+  xrootd = callPackage ../tools/networking/xrootd { };
+
   xtrt = callPackage ../tools/archivers/xtrt { };
 
   yabridge = callPackage ../tools/audio/yabridge {
