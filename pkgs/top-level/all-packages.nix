0391279c2479fa7dcf744238ac071b6cad0673b6
Jan van Brügge <supermanitu@gmail.com>
2022-02-14 12:40:49 +0100

    isabelle: Add isabelle-linter as optional component

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32646,6 +32646,7 @@ with pkgs;
     java = openjdk17;
     z3 = z3_4_4_0;
   };
+  isabelle-components = recurseIntoAttrs (callPackage ../applications/science/logic/isabelle/components { });
 
   iprover = callPackage ../applications/science/logic/iprover { };
 
