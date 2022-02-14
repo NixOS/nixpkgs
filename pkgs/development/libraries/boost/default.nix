ad2191e279c71b428031e5ff1a5e9497e27b7049
Andrew Childs <lorne@cons.org.nz>
2022-02-14 18:49:15 +0900

    boost: use clang 7 for versions older than 1.6.9
    Older versions of boost don't build with newer versions of clang
    (see #85156), and the default clang version on x86_64-darwin is now
    new enough that these older versions of boost no longer build.

--- pkgs/development/libraries/boost/default.nix
+++ pkgs/development/libraries/boost/default.nix
@@ -1,10 +1,18 @@
 { lib
+, stdenv
+, llvmPackages_7
 , callPackage
 , boost-build
 , fetchurl
 }:
 
 let
+  # Older boost versions (< 1.6.9) require clang < 8
+  compatibleStdenv =
+    if stdenv.cc.isClang && !(lib.versionOlder stdenv.cc.version "8.0.0")
+    then llvmPackages_7.stdenv
+    else stdenv;
+
   # for boost 1.55 we need to use 1.56's b2
   # since 1.55's build system is not working
   # with our derivation
@@ -16,9 +24,9 @@ let
     };
   };
 
-  makeBoost = file:
+  makeBoost = file: args:
     lib.fix (self:
-      callPackage file {
+      callPackage file (args // {
         boost-build = boost-build.override {
           # useBoost allows us passing in src and version from
           # the derivation we are building to get a matching b2 version.
@@ -27,22 +35,22 @@ let
             then self
             else useBoost156; # see above
         };
-      }
+      })
     );
 in {
-  boost155 = makeBoost ./1.55.nix;
-  boost159 = makeBoost ./1.59.nix;
-  boost160 = makeBoost ./1.60.nix;
-  boost165 = makeBoost ./1.65.nix;
-  boost166 = makeBoost ./1.66.nix;
-  boost167 = makeBoost ./1.67.nix;
-  boost168 = makeBoost ./1.68.nix;
-  boost169 = makeBoost ./1.69.nix;
-  boost170 = makeBoost ./1.70.nix;
-  boost171 = makeBoost ./1.71.nix;
-  boost172 = makeBoost ./1.72.nix;
-  boost173 = makeBoost ./1.73.nix;
-  boost174 = makeBoost ./1.74.nix;
-  boost175 = makeBoost ./1.75.nix;
-  boost177 = makeBoost ./1.77.nix;
+  boost155 = makeBoost ./1.55.nix { stdenv = compatibleStdenv; };
+  boost159 = makeBoost ./1.59.nix { stdenv = compatibleStdenv; };
+  boost160 = makeBoost ./1.60.nix { stdenv = compatibleStdenv; };
+  boost165 = makeBoost ./1.65.nix { stdenv = compatibleStdenv; };
+  boost166 = makeBoost ./1.66.nix { stdenv = compatibleStdenv; };
+  boost167 = makeBoost ./1.67.nix { stdenv = compatibleStdenv; };
+  boost168 = makeBoost ./1.68.nix { stdenv = compatibleStdenv; };
+  boost169 = makeBoost ./1.69.nix {};
+  boost170 = makeBoost ./1.70.nix {};
+  boost171 = makeBoost ./1.71.nix {};
+  boost172 = makeBoost ./1.72.nix {};
+  boost173 = makeBoost ./1.73.nix {};
+  boost174 = makeBoost ./1.74.nix {};
+  boost175 = makeBoost ./1.75.nix {};
+  boost177 = makeBoost ./1.77.nix {};
 }
