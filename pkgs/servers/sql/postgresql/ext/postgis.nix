f9012ac8ce5461b73f7b647dfbbec64cc7281974
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-14 13:22:58 +0000

    postgresql11Packages.postgis: 3.2.0 -> 3.2.1

--- pkgs/servers/sql/postgresql/ext/postgis.nix
+++ pkgs/servers/sql/postgresql/ext/postgis.nix
@@ -15,13 +15,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "postgis";
-  version = "3.2.0";
+  version = "3.2.1";
 
   outputs = [ "out" "doc" ];
 
   src = fetchurl {
     url = "https://download.osgeo.org/postgis/source/postgis-${version}.tar.gz";
-    sha256 = "sha256-ernhVMaUfAz/uPoS1wgGrdmqBg5itshqLiBvnUtQfP0=";
+    sha256 = "sha256-+6to3ebKOTSyS6CMirDP8llPV/k96rQaFcgq4btpiT4=";
   };
 
   buildInputs = [ libxml2 postgresql geos proj gdal json_c protobufc ]
