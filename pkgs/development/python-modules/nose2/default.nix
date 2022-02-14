f2c0615ee7e1267bfde094f0356bd3e8d9c7ffaf
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-14 06:28:29 +0000

    python39Packages.nose2: 0.10.0 -> 0.11.0

--- pkgs/development/python-modules/nose2/default.nix
+++ pkgs/development/python-modules/nose2/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "nose2";
-  version = "0.10.0";
+  version = "0.11.0";
 
   # Requires mock 2.0.0 if python < 3.6, but NixPkgs has mock 3.0.5.
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "886ba617a96de0130c54b24479bd5c2d74d5c940d40f3809c3a275511a0c4a60";
+    sha256 = "sha256-bSCNfW7J+dVcdNrIHJOUvDkG2++BqMpUILK5t/jmnek=";
   };
 
   propagatedBuildInputs = [ six coverage ];
