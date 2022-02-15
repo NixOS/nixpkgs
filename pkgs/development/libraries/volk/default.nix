a67172ebf667d248c85397835e1e5f9b47d6c4c1
Matthew Leach <dev@mattleach.net>
2022-02-15 12:12:38 +0000

    volk: fix build for apple silicon

--- pkgs/development/libraries/volk/default.nix
+++ pkgs/development/libraries/volk/default.nix
@@ -5,6 +5,7 @@
 , python3
 , enableModTool ? true
 , removeReferencesTo
+, fetchpatch
 }:
 
 stdenv.mkDerivation rec {
@@ -18,6 +19,14 @@ stdenv.mkDerivation rec {
     sha256 = "XvX6emv30bSB29EFm6aC+j8NGOxWqHCNv0Hxtdrq/jc=";
     fetchSubmodules = true;
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://raw.githubusercontent.com/macports/macports-ports/e83a55ef196d4283be438c052295b2fc44f3df5b/science/volk/files/patch-cpu_features-add-support-for-ARM64.diff";
+      sha256 = "sha256-MNUntVvKZC4zuQsxGQCItaUaaQ1d31re2qjyPFbySmI=";
+      extraPrefix = "";
+    })
+  ];
 
   cmakeFlags = lib.optionals (!enableModTool) [ "-DENABLE_MODTOOL=OFF" ];
   postInstall = lib.optionalString (!stdenv.isDarwin) ''
