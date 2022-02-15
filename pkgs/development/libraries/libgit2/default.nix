fca1c9165c7a005ac662d39298828490e36a9c6e
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-02-15 12:31:31 +0100

    libgit2: 1.3.0 -> 1.4.0, update meta
    Enable SSH backend which several libraries depend on which was disabled by default for some reason.

--- pkgs/development/libraries/libgit2/default.nix
+++ pkgs/development/libraries/libgit2/default.nix
@@ -1,23 +1,42 @@
-{ lib, stdenv, fetchFromGitHub, cmake, pkg-config, python3
-, zlib, libssh2, openssl, pcre, http-parser
-, libiconv, Security
+{ lib
+, stdenv
+, fetchFromGitHub
+, fetchpatch
+, cmake
+, pkg-config
+, python3
+, zlib
+, libssh2
+, openssl
+, pcre
+, http-parser
+, libiconv
+, Security
 }:
 
 stdenv.mkDerivation rec {
   pname = "libgit2";
-  version = "1.3.0";
-  # keep the version in sync with python3.pkgs.pygit2 and libgit2-glib
+  version = "1.4.0";
+  # also check the following packages for updates: python3.pkgs.pygit2 and libgit2-glib
 
   src = fetchFromGitHub {
     owner = "libgit2";
     repo = "libgit2";
     rev = "v${version}";
-    sha256 = "sha256-7atNkOBzX+nU1gtFQEaE+EF1L+eex+Ajhq2ocoJY920=";
+    sha256 = "sha256-21t7fD/5O+HIHUDEv8MqloDmAIm9sSpJYqreCD3Co2k=";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/libgit2/libgit2/commit/8bc9eda779b2e2602fc74944aba5d39198e0642f.patch";
+      sha256 = "sha256-r2i4+WsrxIpSwH0g/AikBdAajBncXb1zz0uOQB0h1Jk=";
+    })
+  ];
 
   cmakeFlags = [
     "-DTHREADSAFE=ON"
     "-DUSE_HTTP_PARSER=system"
+    "-DUSE_SSH=ON"
   ];
 
   nativeBuildInputs = [ cmake python3 pkg-config ];
@@ -30,9 +49,10 @@ stdenv.mkDerivation rec {
   doCheck = false; # hangs. or very expensive?
 
   meta = {
-    description = "The Git linkable library";
-    homepage = "https://libgit2.github.com/";
-    license = lib.licenses.gpl2;
-    platforms = with lib.platforms; all;
+    description = "Linkable library implementation of Git that you can use in your application";
+    homepage = "https://libgit2.org/";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ ];
   };
 }
