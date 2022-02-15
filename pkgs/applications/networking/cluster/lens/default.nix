66f62621a9b2a619903d838a7b490242b5fe900e
Otavio Salvador <otavio@ossystems.com.br>
2022-02-15 08:00:52 -0300

    RFH: lens: build from source instead of appimage
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- pkgs/applications/networking/cluster/lens/default.nix
+++ pkgs/applications/networking/cluster/lens/default.nix
@@ -1,53 +1,36 @@
 { lib
-, fetchurl
-, appimageTools
-, wrapGAppsHook
-, gsettings-desktop-schemas
-, gtk3
+, fetchFromGitHub
+, yarn2nix-moretea
+, nodejs-14_x
+, yarn
+, fetchYarnDeps
 }:
-
 let
+  yarn2nix = yarn2nix-moretea.override { nodejs = nodejs-14_x; };
+in
+yarn2nix.mkYarnPackage rec {
   pname = "lens";
   version = "5.3.4";
-  build = "${version}-latest.20220120.1";
-  name = "${pname}-${version}";
 
-  src = fetchurl {
-    url = "https://api.k8slens.dev/binaries/Lens-${build}.x86_64.AppImage";
-    sha256 = "sha256-9vRLQFSocVkHAfgwdKSPhSAO4G/v/ANd0WQmilcZiVw=";
-    name = "${pname}.AppImage";
+  src = fetchFromGitHub {
+    owner = "lensapp";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-dVanSi6VkLVUoEK0r8vqkJztCjD9ulx8LeNsMnikh7Q=";
   };
 
-  appimageContents = appimageTools.extractType2 {
-    inherit name src;
+  offlineCache = fetchYarnDeps {
+    yarnLock = src + "/yarn.lock";
+    sha256 = "sha256-KryL1Q3Sar14FHJq1cnH/WgnqZCWryrjnB3wvOCslAE=";
   };
 
-in
-appimageTools.wrapType2 {
-  inherit name src;
-
-  profile = ''
-    export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS
-  '';
-
-  extraInstallCommands =
-    ''
-      mv $out/bin/${name} $out/bin/${pname}
-
-      install -m 444 -D ${appimageContents}/lens.desktop $out/share/applications/${pname}.desktop
-      install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/512x512/apps/lens.png \
-         $out/share/icons/hicolor/512x512/apps/${pname}.png
-
-      substituteInPlace $out/share/applications/${pname}.desktop \
-        --replace 'Icon=lens' 'Icon=${pname}' \
-        --replace 'Exec=AppRun' 'Exec=${pname}'
-    '';
+  distPhase = ":"; # disable useless $out/tarballs directory
 
   meta = with lib; {
     description = "The Kubernetes IDE";
     homepage = "https://k8slens.dev/";
     license = licenses.mit;
-    maintainers = with maintainers; [ dbirks ];
-    platforms = [ "x86_64-linux" ];
+    maintainers = with maintainers; [ dbirks otavio ];
+    platforms = platforms.linux;
   };
 }
