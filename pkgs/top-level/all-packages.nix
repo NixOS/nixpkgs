8e2ddf7c0e466d716fe157137a048c23d6ef26ab
Átila Saraiva <atilasaraiva@gmail.com>
2022-02-15 22:04:21 -0300

    droidmote: init at 3.0.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2107,6 +2107,8 @@ with pkgs;
 
   droidcam = callPackage ../applications/video/droidcam { };
 
+  droidmote = callPackage ../tools/inputmethods/droidmote { };
+
   ecdsautils = callPackage ../tools/security/ecdsautils { };
 
   echidna = haskell.lib.compose.justStaticExecutables (haskellPackages.callPackage (../tools/security/echidna) { });
