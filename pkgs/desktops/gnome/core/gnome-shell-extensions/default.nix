acae4ecf95710b974d407d4b9cf99dff94ac57c1
Jan Tojnar <jtojnar@gmail.com>
2022-02-16 18:35:00 +0000

    gnome.gnome-shell-extensions: 41.1 → 42.beta
    https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/compare/41.1...42.beta

--- pkgs/desktops/gnome/core/gnome-shell-extensions/default.nix
+++ pkgs/desktops/gnome/core/gnome-shell-extensions/default.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-shell-extensions";
-  version = "41.2";
+  version = "42.beta";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-shell-extensions/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "rZuoL0y9BHJpc7XoevrRiv2qMjhgoK8+ckDzp00iLxk=";
+    sha256 = "QtHztEMRGe344a66JKAMgC9ixQBn4j2xKIynwSWsIu8=";
   };
 
   patches = [
