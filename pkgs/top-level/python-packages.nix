ca3704931f821a8b846a903648d5a81d838bcad3
florian on nixos (Florian Brandes) <florian.brandes@posteo.de>
2022-02-16 22:16:38 +0100

    python3Packages.pipenv-poetry-migrate: init at 0.2.0
    Signed-off-by: florian on nixos (Florian Brandes) <florian.brandes@posteo.de>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6082,6 +6082,8 @@ in {
 
   pipdate = callPackage ../development/python-modules/pipdate { };
 
+  pipenv-poetry-migrate = callPackage ../development/python-modules/pipenv-poetry-migrate { };
+
   pip-tools = callPackage ../development/python-modules/pip-tools { };
 
   pipx = callPackage ../development/python-modules/pipx { };
