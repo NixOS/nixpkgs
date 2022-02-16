2f8d707463882f8e4c478b05a432b32affa7b3e1
Fabian Affolter <mail@fabian-affolter.ch>
2022-02-16 10:06:32 +0100

    python3Packages.limits: enable tests

--- pkgs/development/python-modules/limits/default.nix
+++ pkgs/development/python-modules/limits/default.nix
@@ -1,23 +1,64 @@
 { lib
-, fetchPypi
 , buildPythonPackage
+, fetchFromGitHub
+, hiro
+, pymemcache
+, pymongo
+, pytest-asyncio
+, pytest-lazy-fixture
+, pytestCheckHook
+, pythonOlder
+, redis
 , setuptools
 }:
 
 buildPythonPackage rec {
   pname = "limits";
   version = "2.3.3";
+  format = "setuptools";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-1CcNKVkcxezqsZvgU0VaTmGbo5UGJQK94rVoGvfcG+g=";
+  disabled = pythonOlder "3.7";
+
+  src = fetchFromGitHub {
+    owner = "alisaifee";
+    repo = pname;
+    rev = version;
+    hash = "sha256-ndENV9QBmOecyxFA9rikQ5+5la9k0OfRSTAOLg0aULU=";
   };
 
   propagatedBuildInputs = [
     setuptools
+    redis
+    pymemcache
+    pymongo
   ];
 
-  doCheck = false; # ifilter
+  checkInputs = [
+    hiro
+    pytest-asyncio
+    pytest-lazy-fixture
+    pytestCheckHook
+  ];
+
+  postPatch = ''
+    substituteInPlace pytest.ini \
+      --replace "--cov=limits" "" \
+      --replace "-K" ""
+    # redis-py-cluster doesn't support redis > 4
+    substituteInPlace tests/conftest.py \
+      --replace "import rediscluster" ""
+  '';
+
+  pythonImportsCheck = [
+    "limits"
+  ];
+
+  pytestFlagsArray = [
+    # All other tests require a running Docker instance
+    "tests/test_limits.py"
+    "tests/test_ratelimit_parser.py"
+    "tests/test_limit_granularities.py"
+  ];
 
   meta = with lib; {
     description = "Rate limiting utilities";
