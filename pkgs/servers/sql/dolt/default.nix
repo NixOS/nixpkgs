964afce4ac2eed76ae14edad85a303faa9a107c0
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-16 07:15:23 +0000

    dolt: 0.36.2 -> 0.37.0

--- pkgs/servers/sql/dolt/default.nix
+++ pkgs/servers/sql/dolt/default.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "dolt";
-  version = "0.36.2";
+  version = "0.37.0";
 
   src = fetchFromGitHub {
     owner = "liquidata-inc";
     repo = "dolt";
     rev = "v${version}";
-    sha256 = "sha256-ED0+ETS1E0rw3nDoKNQkBOLMjyx5w/MzvmkjUiH65Ho=";
+    sha256 = "sha256-uAZlQpuNUpg2ZZJBU7A49fCm6GlrYaFyn6ZrmNBW1Y0=";
   };
 
   modRoot = "./go";
   subPackages = [ "cmd/dolt" "cmd/git-dolt" "cmd/git-dolt-smudge" ];
-  vendorSha256 = "sha256-wMM0S7G81lrVAwPTCi3JaVEMmDqbw1AL3JJ+C1tZJWw=";
+  vendorSha256 = "sha256-YqlOzHsdkRrBdQ/mWGs2JIq5JttdRhFeug9HRbl6qlw=";
 
   doCheck = false;
 
