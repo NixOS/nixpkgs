19bb871aa66bbac8e626ff0d855366356d289f6e
Jan Tojnar <jtojnar@gmail.com>
2022-02-16 18:34:59 +0000

    gnome.gnome-settings-daemon: 41.0 → 42.beta
    https://gitlab.gnome.org/GNOME/gnome-settings-daemon/-/compare/41.0...42.beta

--- pkgs/desktops/gnome/core/gnome-settings-daemon/default.nix
+++ pkgs/desktops/gnome/core/gnome-settings-daemon/default.nix
@@ -42,11 +42,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-settings-daemon";
-  version = "41.0";
+  version = "42.beta";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-settings-daemon/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "5spjYfvR3qst4aHjkNTxQWfPR7HFR9u4tlpdielmOIQ=";
+    sha256 = "8ZRFgIrGSYdkMwxbAqnAer70tj7/1G0HX85IjZ4Ahl8=";
   };
 
   patches = [
