f4797bd4524f63e2972699b8a99ec9899354aef9
Jan Tojnar <jtojnar@gmail.com>
2022-02-16 18:37:02 +0000

    orca: 41.2 → 42.beta
    https://gitlab.gnome.org/GNOME/orca/-/compare/41.2...42.beta

--- pkgs/applications/misc/orca/default.nix
+++ pkgs/applications/misc/orca/default.nix
@@ -34,13 +34,13 @@
 
 buildPythonApplication rec {
   pname = "orca";
-  version = "41.2";
+  version = "42.beta";
 
   format = "other";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "1/Jy6ps3+9ZFTkAh5GU4okcibhwKxXDW4rhOlxmqKv4=";
+    sha256 = "lhL6XiigaN4lAi0d5/OWrBRjArySLTTvGG5YeEJR96M=";
   };
 
   patches = [
