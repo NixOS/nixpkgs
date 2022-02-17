aff8e0c87d21950a1674510ab84f92e643369f50
Fabian Affolter <mail@fabian-affolter.ch>
2022-02-17 11:21:11 +0100

    python3Packages.pyoverkiz: init at 1.3.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6149,6 +6149,8 @@ in {
 
   pynx584 = callPackage ../development/python-modules/pynx584 { };
 
+  pyoverkiz = callPackage ../development/python-modules/pyoverkiz { };
+
   pyownet = callPackage ../development/python-modules/pyownet { };
 
   pypoint = callPackage ../development/python-modules/pypoint { };
