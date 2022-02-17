8983324350f5ccb7caa8908b52472304f54dda42
Renaud <c0bw3b@users.noreply.github.com>
2022-02-17 22:36:40 +0100

    metabase: 0.42.0 -> 0.42.1
    and use SPDX3 license identifier

--- pkgs/servers/metabase/default.nix
+++ pkgs/servers/metabase/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "metabase";
-  version = "0.42.0";
+  version = "0.42.1";
 
   src = fetchurl {
     url = "https://downloads.metabase.com/v${version}/metabase.jar";
-    sha256 = "sha256-OSyRpjJW34Ltq2whGpTq48Oa4Mp/BCs/T9f9Ft7uNOE=";
+    hash = "sha256-PmcVVAS/5mDhmOSoFvkZeYkbvFD/KOcgVYuScwD4Olg=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
@@ -14,13 +14,15 @@ stdenv.mkDerivation rec {
   dontUnpack = true;
 
   installPhase = ''
+    runHook preInstall
     makeWrapper ${jdk11}/bin/java $out/bin/metabase --add-flags "-jar $src"
+    runHook postInstall
   '';
 
   meta = with lib; {
     description = "The easy, open source way for everyone in your company to ask questions and learn from data";
     homepage    = "https://metabase.com";
-    license     = licenses.agpl3;
+    license     = licenses.agpl3Only;
     platforms   = platforms.all;
     maintainers = with maintainers; [ schneefux thoughtpolice mmahut ];
   };
