a59afced91da5622e41a79e88331ff18ad8f4f15
Bart Brouns <bart@magnetophon.nl>
2022-02-17 17:56:26 +0100

    Cardinal: init at 22.02

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24745,6 +24745,8 @@ with pkgs;
 
   carddav-util = callPackage ../tools/networking/carddav-util { };
 
+  cardinal = callPackage ../applications/audio/cardinal { };
+
   carla = libsForQt5.callPackage ../applications/audio/carla { };
 
   castor = callPackage ../applications/networking/browsers/castor { };
