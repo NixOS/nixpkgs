c9a176d475c38ea6e241eadf6244c6b64acaaf02
wrvsrx <wrvsrx@outlook.com>
2022-02-18 20:37:24 +0800

    taskwarrior-tui: 0.18.5 -> 0.19.4

--- pkgs/applications/misc/taskwarrior-tui/default.nix
+++ pkgs/applications/misc/taskwarrior-tui/default.nix
@@ -5,19 +5,19 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "taskwarrior-tui";
-  version = "0.18.5";
+  version = "0.19.4";
 
   src = fetchFromGitHub {
     owner = "kdheepak";
     repo = "taskwarrior-tui";
     rev = "v${version}";
-    sha256 = "sha256-Rztz/qye+VsZ0czPt6xk4PwK0e6Aq1GC404+843W55Y=";
+    sha256 = "sha256-Aj3KbrAxohGNE4+TCaX+N95zpqRL4lMDye6aUcgR6t0=";
   };
 
   # Because there's a test that requires terminal access
   doCheck = false;
 
-  cargoSha256 = "sha256-H2A78ACz4TxHxCCtcOuzrfAk4awU6/HC/TlHLNtb/bk=";
+  cargoSha256 = "sha256-i38kLP80ZfTlvFuoFezJD4qQ0Za8VS9S1sd4WKZKvMI=";
 
   meta = with lib; {
     description = "A terminal user interface for taskwarrior ";
