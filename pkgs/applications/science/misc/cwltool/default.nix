b715fcd9d9e26b99182d902c6b5694be0daae6d5
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-18 04:45:38 +0000

    cwltool: 3.1.20220210171524 -> 3.1.20220217190813

--- pkgs/applications/science/misc/cwltool/default.nix
+++ pkgs/applications/science/misc/cwltool/default.nix
@@ -7,14 +7,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "cwltool";
-  version = "3.1.20220210171524";
+  version = "3.1.20220217190813";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "common-workflow-language";
     repo = pname;
     rev = version;
-    sha256 = "sha256-Z0Btg2NklOwwv9c9lIwE6ONpfwCqZfraNWZd3qxzpGs=";
+    sha256 = "sha256-2Zd1Z/Tv8wAiqlaYkZRflsUVl8OAOXdufq9k4j4L7f0=";
   };
 
   postPatch = ''
