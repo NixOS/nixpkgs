f571bcf454631a9f58db57f7dadec62a597be84e
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-18 04:55:13 +0000

    python310Packages.fastapi: 0.73.0 -> 0.74.0

--- pkgs/development/python-modules/fastapi/default.nix
+++ pkgs/development/python-modules/fastapi/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "fastapi";
-  version = "0.73.0";
+  version = "0.74.0";
   format = "flit";
 
   disabled = pythonOlder "3.6";
@@ -28,7 +28,7 @@ buildPythonPackage rec {
     owner = "tiangolo";
     repo = pname;
     rev = version;
-    sha256 = "0v3w9b8107b3g2rgy5y58f0p64inhwl1j9cybp627myypwpqx4b7";
+    sha256 = "sha256-pA+oaZpyRDeQ+b9jrzpYdm3LZQ5QWNjvfFRtUe8eDSo=";
   };
 
   propagatedBuildInputs = [
