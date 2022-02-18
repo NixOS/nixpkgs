7105bba8358821911b317bed558a9a2bc59b51f8
R. RyanTM <ryantm-bot@ryantm.com>
2022-02-18 01:29:29 -0800

    buildah: 1.24.1 -> 1.24.2

--- pkgs/development/tools/buildah/default.nix
+++ pkgs/development/tools/buildah/default.nix
@@ -14,13 +14,13 @@
 
 buildGoModule rec {
   pname = "buildah";
-  version = "1.24.1";
+  version = "1.24.2";
 
   src = fetchFromGitHub {
     owner = "containers";
     repo = "buildah";
     rev = "v${version}";
-    sha256 = "sha256-Dl1ZTYzwZ3tl5k9uPnnKHObmTP6Xsw0P1LdPqCfd/d0=";
+    sha256 = "sha256-gBO+H26YGmOtP3CUHZjynAaOb0h+MJbJnWqxOZdif6w=";
   };
 
   outputs = [ "out" "man" ];
