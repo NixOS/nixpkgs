499da440d7c684ad5ac3cdf6011ef7841d61e245
LeixB <abone9999@gmail.com>
2022-02-18 20:49:35 +0100

    libyuv: init at 1787

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18924,6 +18924,8 @@ with pkgs;
 
   libyubikey = callPackage ../development/libraries/libyubikey { };
 
+  libyuv = callPackage ../development/libraries/libyuv { };
+
   libzapojit = callPackage ../development/libraries/libzapojit { };
 
   libzen = callPackage ../development/libraries/libzen { };
