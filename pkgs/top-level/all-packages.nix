82a6c5b16232cd5fb64a6b7d9d46325b4cf4b4f0
Jan Tojnar <jtojnar@gmail.com>
2022-02-19 02:52:52 +0100

    vala_0_56: init at 0.55.3
    https://gitlab.gnome.org/GNOME/vala/-/blob/0.55.3/NEWS

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13604,6 +13604,7 @@ with pkgs;
   inherit (callPackage ../development/compilers/vala { })
     vala_0_48
     vala_0_54
+    vala_0_56
     vala;
 
   vyper = with python3Packages; toPythonApplication vyper;
