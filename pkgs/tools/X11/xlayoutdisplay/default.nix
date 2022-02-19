88a6b6bd0f8177fd84ebc6dcb0ca629bcd3d7a1b
Renaud <c0bw3b@users.noreply.github.com>
2022-02-19 10:31:57 +0100

    xlayoutdisplay: check target is gtest
    and build in parallel

--- pkgs/tools/X11/xlayoutdisplay/default.nix
+++ pkgs/tools/X11/xlayoutdisplay/default.nix
@@ -15,13 +15,15 @@ stdenv.mkDerivation rec {
   checkInputs = [ gtest ];
 
   doCheck = true;
+  checkTarget = "gtest";
 
   # Fixup reference to hardcoded boost path, dynamically link as seems fine and we don't have static for this
   postPatch = ''
     substituteInPlace config.mk --replace '/usr/lib/libboost_program_options.a' '-lboost_program_options'
   '';
 
-  makeFlags = [ "DESTDIR=/" "PREFIX=${placeholder "out"}" ];
+  makeFlags = [ "PREFIX=${placeholder "out"}" ];
+  enableParallelBuilding = true;
 
   meta = with lib; {
     description = "Detects and arranges linux display outputs, using XRandR for detection and xrandr for arrangement";
