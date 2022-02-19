a0b9894a479adbee2ee252b24afa948d94223cb3
Adam Joseph <adam@westernsemico.com>
2022-02-19 14:50:14 -0800

    seatd: honor systemdSupport
    This commit exposes support for compilation without systemd,
    controlled by the global systemdSupport argument.  This argument is
    understood by many other nixpkgs expressions and can be set globally
    in ~/.config/nixpkgs/config.nix.

--- pkgs/applications/misc/seatd/default.nix
+++ pkgs/applications/misc/seatd/default.nix
@@ -5,7 +5,7 @@
 , pkg-config
 , scdoc
 , stdenv
-, systemd
+, systemdSupport ? stdenv.isLinux, systemd
 }:
 
 stdenv.mkDerivation rec {
@@ -25,9 +25,9 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ meson ninja pkg-config scdoc ];
 
-  buildInputs = [ systemd ];
+  buildInputs = lib.optionals systemdSupport [ systemd ];
 
-  mesonFlags = [ "-Dlibseat-logind=systemd" "-Dlibseat-builtin=enabled" ];
+  mesonFlags = [ "-Dauto_features=auto" "-Dlibseat-builtin=enabled" ];
 
   meta = with lib; {
     description = "A universal seat management library";
