63ccd0b0bba0049d189b28f906a4f84859373643
Arya K <aryakiran@zohomail.eu>
2022-02-19 13:25:17 +0530

    Add a package for ble.sh

---
+++ pkgs/misc/blesh/default.nix
@@ -0,0 +1,28 @@
+{ lib, stdenv, fetchgit }:
+stdenv.mkDerivation rec {
+  pname = "blesh";
+  version = "0.3.3"; 
+
+  src = pkgs.fetchgit {
+    url = "https://github.com/akinomyoga/ble.sh.git";
+    rev = "398e404d624ef6365d9175882e24f6e180d8b6b2";
+    sha256 = "09i6a45lcd8gb0397hcffwsc5fqam8f7wk2yw9qgjjrb7i9n1235";
+  };
+  configurePhase = ''
+ mkdir contrib/.git 
+'';
+  installPhase = ''
+    mkdir -p $out/share/doc
+    mv out/doc $out/share/doc/blesh
+    mv out $out/share/blesh
+  '';
+
+ meta = with lib; {
+    homepage = "https://github.com/akinomyoga/ble.sh";
+    description = "A full-featured line editor written in pure Bash";
+    longDescription = "A full-featured line editor written in pure Bash with Syntax highlighting, auto suggestions, vim modes, etc. in Bash interactive sessions! ";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ aryak ];
+    platforms = platforms.linux;
+  };
+}
