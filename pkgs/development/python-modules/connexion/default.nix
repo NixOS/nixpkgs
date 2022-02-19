907d19292c09ff2445bf111d0ea5c71db626c068
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-19 13:45:09 +0000

    python310Packages.connexion: 2.11.1 -> 2.11.2

--- pkgs/development/python-modules/connexion/default.nix
+++ pkgs/development/python-modules/connexion/default.nix
@@ -22,7 +22,7 @@
 
 buildPythonPackage rec {
   pname = "connexion";
-  version = "2.11.1";
+  version = "2.11.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -31,7 +31,7 @@ buildPythonPackage rec {
     owner = "zalando";
     repo = pname;
     rev = version;
-    sha256 = "sha256-m/r09VNp/AMssOJH9RKMhPcObGHl9uIAoS1PwrjpKaE=";
+    sha256 = "sha256-kFNKRWl/Q8vxIELQURfzCRbVs2JYUwbKro/zKlzNcHU=";
   };
 
   propagatedBuildInputs = [
