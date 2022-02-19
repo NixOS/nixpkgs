6d4aab34c2ff95db52da71443fc794f6b41bb237
Arya K <aryakiran@zohomail.eu>
2022-02-19 13:33:56 +0530

    Fix minor issues with default.nix

--- pkgs/misc/blesh/default.nix
+++ pkgs/misc/blesh/default.nix
@@ -3,7 +3,7 @@ stdenv.mkDerivation rec {
   pname = "blesh";
   version = "0.3.3"; 
 
-  src = pkgs.fetchgit {
+  src = fetchgit {
     url = "https://github.com/akinomyoga/ble.sh.git";
     rev = "398e404d624ef6365d9175882e24f6e180d8b6b2";
     sha256 = "09i6a45lcd8gb0397hcffwsc5fqam8f7wk2yw9qgjjrb7i9n1235";
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
  meta = with lib; {
     homepage = "https://github.com/akinomyoga/ble.sh";
     description = "A full-featured line editor written in pure Bash";
-    longDescription = "A full-featured line editor written in pure Bash with Syntax highlighting, auto suggestions, vim modes, etc. in Bash interactive sessions! ";
+    longDescription = "A full-featured line editor written in pure Bash with Syntax highlighting, auto suggestions, vim modes, etc. in Bash interactive sessions!";
     license = licenses.bsd3;
     maintainers = with maintainers; [ aryak ];
     platforms = platforms.linux;
