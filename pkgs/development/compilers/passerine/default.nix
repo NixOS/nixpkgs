7a065b79f2c4307cb5eb29683ea967d160847de9
Renaud <c0bw3b@users.noreply.github.com>
2022-02-20 00:25:17 +0100

    passerine: use upstream tag

--- pkgs/development/compilers/passerine/default.nix
+++ pkgs/development/compilers/passerine/default.nix
@@ -7,15 +7,15 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "vrtbl";
     repo = "passerine";
-    rev = "30757a528c1656c15ed3403d355d2b9dee028991";
-    sha256 = "1vffnl5472gsskq9065cifh4fk52z602sb448ghf1xq9n98drdjf";
+    rev = "v${version}";
+    hash = "sha256-TrbcULIJ9+DgQ4QsLYD5okxHoIusGJDw1PqJQwq1zu0=";
   };
 
-  cargoSha256 = "0bd8d5k6d6vwmlsbfkvxx1xbkb9wqihpnmym1lnhk875997hxsq3";
+  cargoHash = "sha256-A+sOT0rloAktDdVXe2HEPK25euh9T7c0rXybZmZpqC0=";
 
   meta = with lib; {
     description = "A small extensible programming language designed for concise expression with little code";
-    homepage = "https://github.com/vrtbl/passerine";
+    homepage = "https://www.passerine.io/";
     license = licenses.mit;
     maintainers = with maintainers; [ siraben ];
   };
