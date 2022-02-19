16b6a98bff27101a18b03d48a5cc7d760b55323c
Someone Serge <sergei.kozlukov@aalto.fi>
2022-02-19 16:03:14 +0200

    opensfm: ceres usage compatible with old nixpkgs
    In 21.05 ceres hasn't been split yet

--- pkgs/development/python-modules/opensfm/default.nix
+++ pkgs/development/python-modules/opensfm/default.nix
@@ -34,80 +34,88 @@
   matplotlib,
   fpdf,
 }:
-buildPythonPackage rec {
-  pname = "OpenSfM";
-  version = "0.5.2";
 
-  src = fetchFromGitHub {
-    owner = "mapillary";
-    repo = pname;
-    rev = "79aa4bdd8bd08dc0cd9e3086d170cedb29ac9760";
-    sha256 = "sha256-dHBrkYwLA1OUxUSoe7DysyeEm9Yy70tIJvAsXivdjrM=";
-  };
-  patches = [
-    ./0001-fix-cmake-Eigen-Ceres-via-native-cmake-targets.patch
-    ./0002-cmake-find-system-distributed-gtest.patch
-    ./0003-cmake-use-system-pybind11.patch
-    ./0004-pybind_utils.h-conflicts-with-nixpkgs-pybind.patch
-    ./fix-scripts.patch
-  ];
-  postPatch = ''
-    rm opensfm/src/cmake/FindGlog.cmake
-    rm opensfm/src/cmake/FindGflags.cmake
+let
+  ceresSplit = (builtins.length ceres-solver.outputs) > 1;
+  ceres' =
+    if ceresSplit
+    then ceres-solver.dev
+    else ceres-solver;
+in
+  buildPythonPackage rec {
+    pname = "OpenSfM";
+    version = "0.5.2";
 
-    # HAHOG is the default descriptor.
-    # We'll test both HAHOG and SIFT because this is
-    # where segfaults might be introduced in future
-    echo 'feature_type: SIFT' >> data/berlin/config.yaml
-    echo 'feature_type: HAHOG' >> data/lund/config.yaml
-  '';
+    src = fetchFromGitHub {
+      owner = "mapillary";
+      repo = pname;
+      rev = "79aa4bdd8bd08dc0cd9e3086d170cedb29ac9760";
+      sha256 = "sha256-dHBrkYwLA1OUxUSoe7DysyeEm9Yy70tIJvAsXivdjrM=";
+    };
+    patches = [
+      ./0001-fix-cmake-Eigen-Ceres-via-native-cmake-targets.patch
+      ./0002-cmake-find-system-distributed-gtest.patch
+      ./0003-cmake-use-system-pybind11.patch
+      ./0004-pybind_utils.h-conflicts-with-nixpkgs-pybind.patch
+      ./fix-scripts.patch
+    ];
+    postPatch = ''
+      rm opensfm/src/cmake/FindGlog.cmake
+      rm opensfm/src/cmake/FindGflags.cmake
 
-  nativeBuildInputs = [cmake pkg-config sphinx];
-  buildInputs = [
-    opencv
-    ceres-solver.dev
-    suitesparse
-    metis
-    eigen
-    lapack
-    gflags
-    gtest
-    glog
-    pybind11
-  ];
-  propagatedBuildInputs = [
-    numpy
-    scipy
-    pyyaml
-    opencv4
-    networkx
-    pillow
-    matplotlib
-    fpdf
-    exifread
-    gpxpy
-    pyproj
-    python-dateutil
-    joblib
-    repoze_lru
-    xmltodict
-    cloudpickle
-  ];
-  checkInputs = [pytestCheckHook];
+      # HAHOG is the default descriptor.
+      # We'll test both HAHOG and SIFT because this is
+      # where segfaults might be introduced in future
+      echo 'feature_type: SIFT' >> data/berlin/config.yaml
+      echo 'feature_type: HAHOG' >> data/lund/config.yaml
+    '';
 
-  dontUseCmakeBuildDir = true;
-  cmakeFlags = [
-    "-Bcmake_build"
-    "-Sopensfm/src"
-  ];
+    nativeBuildInputs = [cmake pkg-config sphinx];
+    buildInputs = [
+      opencv
+      ceres'
+      suitesparse
+      metis
+      eigen
+      lapack
+      gflags
+      gtest
+      glog
+      pybind11
+    ];
+    propagatedBuildInputs = [
+      numpy
+      scipy
+      pyyaml
+      opencv4
+      networkx
+      pillow
+      matplotlib
+      fpdf
+      exifread
+      gpxpy
+      pyproj
+      python-dateutil
+      joblib
+      repoze_lru
+      xmltodict
+      cloudpickle
+    ];
+    checkInputs = [pytestCheckHook];
 
-  pythonImportsCheck = ["opensfm"];
+    dontUseCmakeBuildDir = true;
+    cmakeFlags = [
+      "-Bcmake_build"
+      "-Sopensfm/src"
+    ];
 
-  meta = {
-    maintainers = [lib.maintainers.SomeoneSerge];
-    license = lib.licenses.bsd2;
-    description = "Open source Structure-from-Motion pipeline from Mapillary";
-    homepage = "https://opensfm.org/";
-    platforms = lib.platforms.linux;
-  };
-}
+    pythonImportsCheck = ["opensfm"];
+
+    meta = {
+      maintainers = [lib.maintainers.SomeoneSerge];
+      license = lib.licenses.bsd2;
+      description = "Open source Structure-from-Motion pipeline from Mapillary";
+      homepage = "https://opensfm.org/";
+      platforms = lib.platforms.linux;
+    };
+  }
