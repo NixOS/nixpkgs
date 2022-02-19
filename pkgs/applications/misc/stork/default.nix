d9f089eecf73e4ee58c2f3d9dd6e07a711b3752c
Chua Hou <human+github@chuahou.dev>
2022-02-19 22:34:01 +0000

    stork: add Security framework and mark broken for darwin build
    - The Security framework is needed to build on darwin.
    - It currently does not build since the SDK is too old.
    - For more details, see #157416.

--- pkgs/applications/misc/stork/default.nix
+++ pkgs/applications/misc/stork/default.nix
@@ -3,6 +3,8 @@
 , fetchFromGitHub
 , openssl
 , pkg-config
+, stdenv
+, darwin
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -20,12 +22,16 @@ rustPlatform.buildRustPackage rec {
 
   nativeBuildInputs = [ pkg-config ];
 
-  buildInputs = [ openssl ];
+  buildInputs = [ openssl ]
+    ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
 
   meta = with lib; {
     description = "Impossibly fast web search, made for static sites";
     homepage = "https://github.com/jameslittle230/stork";
     license = with licenses; [ asl20 ];
     maintainers = with maintainers; [ chuahou ];
+    # TODO: Remove once nixpkgs uses macOS SDK 10.14+ for x86_64-darwin
+    # Undefined symbols for architecture x86_64: "_SecTrustEvaluateWithError"
+    broken = stdenv.isDarwin && stdenv.isx86_64;
   };
 }
