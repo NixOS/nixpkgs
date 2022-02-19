61cfa946fc0d6b81d9fec06948f4a86cba75fe38
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-19 07:38:31 +0000

    python310Packages.dash: 2.1.0 -> 2.2.0

--- pkgs/development/python-modules/dash/default.nix
+++ pkgs/development/python-modules/dash/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "dash";
-  version = "2.1.0";
+  version = "2.2.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "plotly";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-qEHB90xzettkX/9ojCQ8AZYnTJaL+8n87SmlBOBSDWQ=";
+    sha256 = "sha256-Ul3j1BJd3lKzGCEhPFYxZG0CcAqAZqmOEDi8vGPFItw=";
   };
 
   propagatedBuildInputs = [
