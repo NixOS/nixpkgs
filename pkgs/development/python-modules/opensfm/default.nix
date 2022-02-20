4803121aa758de7ba3b2fd2dbd8098116ebd081f
Someone Serge <sergei.kozlukov@aalto.fi>
2022-02-20 03:56:38 +0200

    opensfm: back to nixpkgs-fmt

--- pkgs/development/python-modules/opensfm/default.nix
+++ pkgs/development/python-modules/opensfm/default.nix
@@ -1,38 +1,38 @@
-{
-  lib,
-  buildPythonPackage,
-  fetchFromGitHub,
-  fetchpatch,
-  cmake,
-  opencv,
-  opencv4,
-  ceres-solver,
-  suitesparse,
-  metis,
-  eigen,
-  pkg-config,
-  pybind11,
-  numpy,
-  pyyaml,
-  lapack,
-  gtest,
-  gflags,
-  glog,
-  pytestCheckHook,
-  networkx,
-  pillow,
-  exifread,
-  gpxpy,
-  pyproj,
-  python-dateutil,
-  joblib,
-  repoze_lru,
-  xmltodict,
-  cloudpickle,
-  scipy,
-  sphinx,
-  matplotlib,
-  fpdf,
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, fetchpatch
+, cmake
+, opencv
+, opencv4
+, ceres-solver
+, suitesparse
+, metis
+, eigen
+, pkg-config
+, pybind11
+, numpy
+, pyyaml
+, lapack
+, gtest
+, gflags
+, glog
+, pytestCheckHook
+, networkx
+, pillow
+, exifread
+, gpxpy
+, pyproj
+, python-dateutil
+, joblib
+, repoze_lru
+, xmltodict
+, cloudpickle
+, scipy
+, sphinx
+, matplotlib
+, fpdf
+,
 }:
 
 let
@@ -42,80 +42,80 @@ let
     then ceres-solver.dev
     else ceres-solver;
 in
-  buildPythonPackage rec {
-    pname = "OpenSfM";
-    version = "0.5.2";
+buildPythonPackage rec {
+  pname = "OpenSfM";
+  version = "0.5.2";
 
-    src = fetchFromGitHub {
-      owner = "mapillary";
-      repo = pname;
-      rev = "79aa4bdd8bd08dc0cd9e3086d170cedb29ac9760";
-      sha256 = "sha256-dHBrkYwLA1OUxUSoe7DysyeEm9Yy70tIJvAsXivdjrM=";
-    };
-    patches = [
-      ./0001-fix-cmake-Eigen-Ceres-via-native-cmake-targets.patch
-      ./0002-cmake-find-system-distributed-gtest.patch
-      ./0003-cmake-use-system-pybind11.patch
-      ./0004-pybind_utils.h-conflicts-with-nixpkgs-pybind.patch
-      ./fix-scripts.patch
-    ];
-    postPatch = ''
-      rm opensfm/src/cmake/FindGlog.cmake
-      rm opensfm/src/cmake/FindGflags.cmake
+  src = fetchFromGitHub {
+    owner = "mapillary";
+    repo = pname;
+    rev = "79aa4bdd8bd08dc0cd9e3086d170cedb29ac9760";
+    sha256 = "sha256-dHBrkYwLA1OUxUSoe7DysyeEm9Yy70tIJvAsXivdjrM=";
+  };
+  patches = [
+    ./0001-fix-cmake-Eigen-Ceres-via-native-cmake-targets.patch
+    ./0002-cmake-find-system-distributed-gtest.patch
+    ./0003-cmake-use-system-pybind11.patch
+    ./0004-pybind_utils.h-conflicts-with-nixpkgs-pybind.patch
+    ./fix-scripts.patch
+  ];
+  postPatch = ''
+    rm opensfm/src/cmake/FindGlog.cmake
+    rm opensfm/src/cmake/FindGflags.cmake
 
-      # HAHOG is the default descriptor.
-      # We'll test both HAHOG and SIFT because this is
-      # where segfaults might be introduced in future
-      echo 'feature_type: SIFT' >> data/berlin/config.yaml
-      echo 'feature_type: HAHOG' >> data/lund/config.yaml
-    '';
+    # HAHOG is the default descriptor.
+    # We'll test both HAHOG and SIFT because this is
+    # where segfaults might be introduced in future
+    echo 'feature_type: SIFT' >> data/berlin/config.yaml
+    echo 'feature_type: HAHOG' >> data/lund/config.yaml
+  '';
 
-    nativeBuildInputs = [cmake pkg-config sphinx];
-    buildInputs = [
-      opencv
-      ceres'
-      suitesparse
-      metis
-      eigen
-      lapack
-      gflags
-      gtest
-      glog
-      pybind11
-    ];
-    propagatedBuildInputs = [
-      numpy
-      scipy
-      pyyaml
-      opencv4
-      networkx
-      pillow
-      matplotlib
-      fpdf
-      exifread
-      gpxpy
-      pyproj
-      python-dateutil
-      joblib
-      repoze_lru
-      xmltodict
-      cloudpickle
-    ];
-    checkInputs = [pytestCheckHook];
+  nativeBuildInputs = [ cmake pkg-config sphinx ];
+  buildInputs = [
+    opencv
+    ceres'
+    suitesparse
+    metis
+    eigen
+    lapack
+    gflags
+    gtest
+    glog
+    pybind11
+  ];
+  propagatedBuildInputs = [
+    numpy
+    scipy
+    pyyaml
+    opencv4
+    networkx
+    pillow
+    matplotlib
+    fpdf
+    exifread
+    gpxpy
+    pyproj
+    python-dateutil
+    joblib
+    repoze_lru
+    xmltodict
+    cloudpickle
+  ];
+  checkInputs = [ pytestCheckHook ];
 
-    dontUseCmakeBuildDir = true;
-    cmakeFlags = [
-      "-Bcmake_build"
-      "-Sopensfm/src"
-    ];
+  dontUseCmakeBuildDir = true;
+  cmakeFlags = [
+    "-Bcmake_build"
+    "-Sopensfm/src"
+  ];
 
-    pythonImportsCheck = ["opensfm"];
+  pythonImportsCheck = [ "opensfm" ];
 
-    meta = {
-      maintainers = [lib.maintainers.SomeoneSerge];
-      license = lib.licenses.bsd2;
-      description = "Open source Structure-from-Motion pipeline from Mapillary";
-      homepage = "https://opensfm.org/";
-      platforms = lib.platforms.linux;
-    };
-  }
+  meta = {
+    maintainers = [ lib.maintainers.SomeoneSerge ];
+    license = lib.licenses.bsd2;
+    description = "Open source Structure-from-Motion pipeline from Mapillary";
+    homepage = "https://opensfm.org/";
+    platforms = lib.platforms.linux;
+  };
+}
