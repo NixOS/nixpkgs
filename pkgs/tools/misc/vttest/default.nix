5d1c9a54eb09603e9267ffe19f61418642c0b3c8
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-20 19:52:36 +0000

    vttest: 20210210 -> 20220215

--- pkgs/tools/misc/vttest/default.nix
+++ pkgs/tools/misc/vttest/default.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "vttest";
-  version = "20210210";
+  version = "20220215";
 
   src = fetchurl {
     urls = [
       "https://invisible-mirror.net/archives/${pname}/${pname}-${version}.tgz"
       "ftp://ftp.invisible-island.net/${pname}/${pname}-${version}.tgz"
     ];
-    sha256 = "sha256-D5ii4wWYKRXxUgmEw+hpjjrNUI7iEHEVKMifWn6n8EY=";
+    sha256 = "sha256-SmWZjF4SzwjO0s/OEZrbRPqEKsFJXQ8VDyHIpnhZFaE=";
   };
 
   meta = with lib; {
