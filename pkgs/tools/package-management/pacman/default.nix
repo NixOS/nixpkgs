3cc426eb9ea55fd457f5e4c7f624511c3be458bc
Renaud <c0bw3b@users.noreply.github.com>
2022-02-20 22:32:07 +0100

    pacman: 5.2.2 -> 6.0.1

--- pkgs/tools/package-management/pacman/default.nix
+++ pkgs/tools/package-management/pacman/default.nix
@@ -1,40 +1,83 @@
-{ stdenv, lib, fetchurl, pkg-config, m4, perl, libarchive, openssl, zlib, bzip2,
-xz, curl, runtimeShell }:
+{ lib
+, stdenv
+, fetchurl
+, asciidoc
+, bzip2
+, coreutils
+, curl
+, gpgme
+, installShellFiles
+, libarchive
+, meson
+, ninja
+, openssl
+, perl
+, pkg-config
+, python3
+, runtimeShell
+, xz
+, zlib
+}:
 
 stdenv.mkDerivation rec {
   pname = "pacman";
-  version = "5.2.2";
+  version = "6.0.1";
 
   src = fetchurl {
-    url = "https://sources.archlinux.org/other/${pname}/${pname}-${version}.tar.gz";
-    sha256 = "1829jcc300fxidr3cahx5kpnxkpg500daqgn2782hg5m5ygil85v";
+    url = "https://sources.archlinux.org/other/${pname}/${pname}-${version}.tar.xz";
+    hash = "sha256-DbYUVuVqpJ4mDokcCwJb4hAxnmKxVSHynT6TsA079zE=";
   };
 
-  enableParallelBuilding = true;
-
-  configureFlags = [
-    # trying to build docs fails with a2x errors, unable to fix through asciidoc
-    "--disable-doc"
-
-    "--sysconfdir=/etc"
-    "--localstatedir=/var"
-    "--with-scriptlet-shell=${runtimeShell}"
+  nativeBuildInputs = [
+    asciidoc
+    coreutils
+    installShellFiles
+    meson
+    ninja
+    pkg-config
+    python3
   ];
 
-  installFlags = [ "sysconfdir=${placeholder "out"}/etc" ];
+  buildInputs = [
+    bzip2
+    curl
+    gpgme
+    libarchive
+    openssl
+    perl
+    xz
+    zlib
+  ];
 
-  nativeBuildInputs = [ pkg-config m4 ];
-  buildInputs = [ curl perl libarchive openssl zlib bzip2 xz ];
+  postPatch = ''
+    substituteInPlace meson.build \
+      --replace "install_dir : SYSCONFDIR" "install_dir : '${placeholder "out"}/etc'" \
+      --replace "join_paths(LOCALSTATEDIR, 'lib/pacman/')," "'${placeholder "out"}/var/lib/pacman/'," \
+      --replace "join_paths(LOCALSTATEDIR, 'cache/pacman/pkg/')," "'${placeholder "out"}/var/cache/pacman/pkg/',"
 
-  postFixup = ''
-    substituteInPlace $out/bin/repo-add \
+    substituteInPlace doc/meson.build \
+      --replace "/bin/true" "${coreutils}/bin/true"
+
+    substituteInPlace scripts/repo-add.sh.in \
       --replace bsdtar "${libarchive}/bin/bsdtar"
+  '';
+
+  mesonFlags = [
+    "--sysconfdir=/etc"
+    "--localstatedir=/var"
+    "-Dmakepkg-template-dir=${placeholder "out"}/share/makepkg-template"
+    "-Dscriptlet-shell=${runtimeShell}"
+  ];
+
+  postInstall = ''
+    installShellCompletion --bash scripts/pacman --zsh scripts/_pacman
   '';
 
   meta = with lib; {
     description = "A simple library-based package manager";
-    homepage = "https://www.archlinux.org/pacman/";
-    license = licenses.gpl2;
+    homepage = "https://archlinux.org/pacman/";
+    changelog = "https://gitlab.archlinux.org/pacman/pacman/-/raw/v${version}/NEWS";
+    license = licenses.gpl2Plus;
     platforms = platforms.linux;
     maintainers = with maintainers; [ mt-caret ];
   };
