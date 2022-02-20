dd0c4562f9f049d06df21a49bbdc034ef2d26d6a
sternenseemann <sternenseemann@systemli.org>
2022-02-20 22:41:18 +0100

    haskellPackages.xmonad: use appropriate patch for >= 0.17
    We already had a xmonad-nix.patch for XMonad 0.17, so it's just a matter
    of using it. The old patch for 0.16 can be dropped as well, since we no
    longer ship that version.

--- pkgs/development/haskell-modules/configuration-nix.nix
+++ pkgs/development/haskell-modules/configuration-nix.nix
@@ -212,18 +212,7 @@ self: super: builtins.intersectAttrs super {
   }) super.tz;
 
   # Nix-specific workaround
-  xmonad = appendPatch ./patches/xmonad-nix.patch (dontCheck super.xmonad);
-  xmonad_0_17_0 = doDistribute (appendPatch ./patches/xmonad_0_17_0-nix.patch (super.xmonad_0_17_0));
-
-  # Need matching xmonad version
-  xmonad-contrib_0_17_0 = doDistribute (super.xmonad-contrib_0_17_0.override {
-    xmonad = self.xmonad_0_17_0;
-  });
-
-  xmonad-extras_0_17_0 = doDistribute (super.xmonad-extras_0_17_0.override {
-    xmonad = self.xmonad_0_17_0;
-    xmonad-contrib = self.xmonad-contrib_0_17_0;
-  });
+  xmonad = appendPatch ./patches/xmonad_0_17_0-nix.patch (dontCheck super.xmonad);
 
   # wxc supports wxGTX >= 3.0, but our current default version points to 2.8.
   # http://hydra.cryp.to/build/1331287/log/raw
