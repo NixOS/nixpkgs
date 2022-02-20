d299475aab0ff2c69adcd7d3c48d5fdf6b487676
jx11r <jx11r@hotmail.com>
2022-02-20 11:03:02 -0600

    nixos/modules: add more options to qtile

--- nixos/modules/services/x11/window-managers/qtile.nix
+++ nixos/modules/services/x11/window-managers/qtile.nix
@@ -1,25 +1,48 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 with lib;
 
 let
   cfg = config.services.xserver.windowManager.qtile;
+  qtile = cfg.package;
 in
 
 {
   options = {
-    services.xserver.windowManager.qtile.enable = mkEnableOption "qtile";
+    services.xserver.windowManager.qtile = {
+      enable = mkEnableOption "qtile";
+
+      package = mkOption {
+        type = types.package;
+        default = pkgs.qtile;
+        defaultText = literalExpression "pkgs.qtile";
+        example = literalExpression "pkgs.unstable.qtile";
+        description = "Qtile package to use.";
+      };
+
+      configFile = mkOption {
+        type = with types; nullOr path;
+        default = null;
+        example = literalExpression "/path/to/your/config.py";
+        description = ''
+          Path to the qtile configuration file.
+          If null, $HOME/.config/qtile/config.py will be used.
+        '';
+      };
+    };
   };
 
   config = mkIf cfg.enable {
     services.xserver.windowManager.session = [{
       name = "qtile";
       start = ''
-        ${pkgs.qtile}/bin/qtile start &
+        ${qtile}/bin/qtile start ${optionalString (cfg.configFile != null)
+          "--config \"${cfg.configFile}\""
+        } &
         waitPID=$!
       '';
     }];
 
-    environment.systemPackages = [ pkgs.qtile ];
+    environment.systemPackages = [ qtile ];
   };
 }
