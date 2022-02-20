02ae3defed9086d07963fcecb287f13da1cfe49b
Adam Joseph <adam@westernsemico.com>
2022-02-20 14:28:17 -0800

    chromium: use pkgsBuildHost.jre8_headless instead of pkgsBuildHost.jre8
    Chromium appears to require bin/java at build-time.  This patch causes
    the chromium derivation to use jre8_headless instead of jre8, in order
    to avoid dragging in all of gnome.

--- pkgs/applications/networking/browsers/chromium/common.nix
+++ pkgs/applications/networking/browsers/chromium/common.nix
@@ -220,7 +220,7 @@ let
       # Link to our own Node.js and Java (required during the build):
       mkdir -p third_party/node/linux/node-linux-x64/bin
       ln -s "${pkgsBuildHost.nodejs}/bin/node" third_party/node/linux/node-linux-x64/bin/node
-      ln -s "${pkgsBuildHost.jre8}/bin/java" third_party/jdk/current/bin/
+      ln -s "${pkgsBuildHost.jre8_headless}/bin/java" third_party/jdk/current/bin/
 
       # Allow building against system libraries in official builds
       sed -i 's/OFFICIAL_BUILD/GOOGLE_CHROME_BUILD/' tools/generate_shim_headers/generate_shim_headers.py
