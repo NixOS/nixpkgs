0d6d753fa17121f61e35926adcda91385cb1fb70
Fabian Affolter <mail@fabian-affolter.ch>
2022-02-21 16:26:59 +0100

    globe-cli: remove blank line

--- pkgs/applications/misc/globe-cli/default.nix
+++ pkgs/applications/misc/globe-cli/default.nix
@@ -6,7 +6,7 @@ rustPlatform.buildRustPackage rec {
 
   src = fetchCrate {
     inherit pname version;
-    sha256 = "sha256-Np1f/mSMIMZU3hE0Fur8bOHhOH3rZyroGiVAqfiIs7g=";
+    hash = "sha256-Np1f/mSMIMZU3hE0Fur8bOHhOH3rZyroGiVAqfiIs7g=";
   };
 
   cargoHash = "sha256-qoCOYk7hyjMx07l48IkxE6zsG58NkF72E3OvoZHz5d0=";
@@ -17,5 +17,4 @@ rustPlatform.buildRustPackage rec {
     license = licenses.gpl3Only;
     maintainers = with maintainers; [ devhell ];
   };
-
 }
