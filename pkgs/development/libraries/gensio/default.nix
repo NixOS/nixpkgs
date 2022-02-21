3c962d11e476788aff46425d57629262f13e4390
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-21 07:51:55 +0000

    gensio: 2.2.9 -> 2.3.6

--- pkgs/development/libraries/gensio/default.nix
+++ pkgs/development/libraries/gensio/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gensio";
-  version = "2.2.9";
+  version = "2.3.6";
 
   src = fetchFromGitHub {
     owner = "cminyard";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-SN8zMMBX02kIS9q1/7DO+t826DpmbZBO37TDZtvRT1A=";
+    sha256 = "sha256-aPTwN5DuUFPqLDwq73KvmBR8tEN5bIX6/JOZMfOAoow=";
   };
 
   passthru = {
