826c21549169faf268cba920dda90860e7618496
Felix Buehler <account@buehler.rocks>
2022-02-21 22:17:57 +0100

    treewide: rename name to pname&version

--- pkgs/development/libraries/physics/geant4/datasets.nix
+++ pkgs/development/libraries/physics/geant4/datasets.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchurl, geant_version }:
 
 let
-  mkDataset = { name, version, sha256, envvar }:
+  mkDataset = { pname, version, sha256, envvar }:
     stdenv.mkDerivation {
-      inherit name version;
+      inherit pname version;
       inherit geant_version;
 
       src = fetchurl {
-        url = "https://cern.ch/geant4-data/datasets/${name}.${version}.tar.gz";
+        url = "https://cern.ch/geant4-data/datasets/${pname}.${version}.tar.gz";
         inherit sha256;
       };
 
@@ -15,7 +15,7 @@ let
       dontBuild = true;
       dontConfigure = true;
 
-      datadir = "${placeholder "out"}/share/Geant4-${geant_version}/data/${name}${version}";
+      datadir = "${placeholder "out"}/share/Geant4-${geant_version}/data/${pname}${version}";
       installPhase = ''
         mkdir -p $datadir
         mv ./* $datadir
@@ -32,86 +32,86 @@ let
       };
     };
 in
-  builtins.listToAttrs (map (a: { inherit (a) name; value = mkDataset a; }) [
+  builtins.listToAttrs (map (a: { inherit (a) pname; value = mkDataset a; }) [
     {
-      name = "G4NDL";
+      pname = "G4NDL";
       version = "4.6";
       sha256 = "sha256-nSh88q4PuIeirc6AHudPub4hsNFm2rSby+6UCKUUVAg=";
       envvar = "NEUTRONHP";
     }
 
     {
-      name = "G4EMLOW";
+      pname = "G4EMLOW";
       version = "8.0";
       sha256 = "sha256-2Rmo5YOGiCV7kkimE5EOsqdjMFngMMi1DAosKtn9Kzs=";
       envvar = "LE";
     }
 
     {
-      name = "G4PhotonEvaporation";
+      pname = "G4PhotonEvaporation";
       version = "5.7";
       sha256 = "sha256-dh5C5W/93j2YOfn52BAmB8a0wDKRUe5Rggb07p535+U=";
       envvar = "LEVELGAMMA";
     }
 
     {
-      name = "G4RadioactiveDecay";
+      pname = "G4RadioactiveDecay";
       version = "5.6";
       sha256 = "sha256-OIYHfJyOWph4PmcY4cMlZ4me6y27M+QC1Edrwv5PDfE=";
       envvar = "RADIOACTIVE";
     }
 
     {
-      name = "G4SAIDDATA";
+      pname = "G4SAIDDATA";
       version = "2.0";
       sha256 = "sha256-HSao55uqceRNV1m59Vpn6Lft4xdRMWqekDfYAJDHLpE=";
       envvar = "SAIDXS";
     }
 
     {
-      name = "G4PARTICLEXS";
+      pname = "G4PARTICLEXS";
       version = "4.0";
       sha256 = "sha256-k4EDlwPD8rD9NqtJmTYqLItP+QgMMi+QtOMZKBEzypU=";
       envvar = "PARTICLEXS";
     }
 
     {
-      name = "G4ABLA";
+      pname = "G4ABLA";
       version = "3.1";
       sha256 = "sha256-dpiwUrWL8bmIa+rNvWr2B63B4Jn8cwq2shz38JDAJ+0=";
       envvar = "ABLA";
     }
 
     {
-      name = "G4INCL";
+      pname = "G4INCL";
       version = "1.0";
       sha256 = "sha256-cWFhghrp89BWX788LPNPTgLj5RnrQZqCI27vIsLENn0=";
       envvar = "INCL";
     }
 
     {
-      name = "G4PII";
+      pname = "G4PII";
       version = "1.3";
       sha256 = "sha256-YiWtkCZ19DgcmMa6JfxaBs6HVJqpeWNNPQNJHWYW6SY=";
       envvar = "PII";
     }
 
     {
-      name = "G4ENSDFSTATE";
+      pname = "G4ENSDFSTATE";
       version = "2.3";
       sha256 = "sha256-lETF4IIHkavTzKrOEFsOR3kPrc4obhEUmDTnnEqOkgM=";
       envvar = "ENSDFSTATE";
     }
 
     {
-      name = "G4RealSurface";
+      pname = "G4RealSurface";
       version = "2.2";
       sha256 = "sha256-mVTe4AEvUzEmf3g2kOkS5y21v1Lqm6vs0S6iIoIXaCA=";
       envvar = "REALSURFACE";
     }
 
     {
-      name = "G4TENDL";
+      pname = "G4TENDL";
       version = "1.4";
       sha256 = "sha256-S3J0AgzItO1Wm4ku8YwuCI7c22tm850lWFzO4l2XIeA=";
       envvar = "PARTICLEHP";
--- pkgs/development/libraries/qimageblitz/default.nix
+++ pkgs/development/libraries/qimageblitz/default.nix
@@ -1,15 +1,11 @@
 {lib, stdenv, fetchurl, cmake, qt4}:
 
-let
-  pn = "qimageblitz";
-  v = "0.0.4";
-in
-
-stdenv.mkDerivation {
-  name = "${pn}-${v}";
+stdenv.mkDerivation rec {
+  pname = "qimageblitz";
+  version = "0.0.4";
 
   src = fetchurl {
-    url = "mirror://sourceforge/${pn}/${pn}-${v}.tar.bz2";
+    url = "mirror://sourceforge/qimageblitz/qimageblitz-${version}.tar.bz2";
     sha256 = "0pnaf3qi7rgkxzs2mssmslb3f9ya4cyx09wzwlis3ppyvf72j0p9";
   };
 
@@ -21,7 +17,7 @@ stdenv.mkDerivation {
   meta = {
     description = "Graphical effect and filter library for KDE4";
     license = lib.licenses.bsd2;
-    homepage = "http://${pn}.sourceforge.net";
+    homepage = "http://qimageblitz.sourceforge.net";
     platforms = lib.platforms.linux;
   };
 }
