d9b80ac69a186fed06a95f1dcfa64802b08e5b19
Renaud <c0bw3b@users.noreply.github.com>
2022-02-23 21:10:24 +0100

    ibus-m17n: 1.4.8 -> 1.4.9
    and use SPDX 3.0 license identifier

--- pkgs/tools/inputmethods/ibus-engines/ibus-m17n/default.nix
+++ pkgs/tools/inputmethods/ibus-engines/ibus-m17n/default.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation rec {
   pname = "ibus-m17n";
-  version = "1.4.8";
+  version = "1.4.9";
 
   src = fetchFromGitHub {
     owner = "ibus";
     repo = "ibus-m17n";
     rev = version;
-    sha256 = "sha256-Rdwvm9PC+WZcmksJEEFAd40XruCWBUZ+A2rh7/Hnjg8=";
+    sha256 = "sha256-N9hzyu2etbxlJPD2yUc2T0jxAfhEhshQ8X5R27JBg1E=";
   };
 
   nativeBuildInputs = [
@@ -48,7 +48,7 @@ stdenv.mkDerivation rec {
     isIbusEngine = true;
     description = "m17n engine for ibus";
     homepage = "https://github.com/ibus/ibus-m17n";
-    license = licenses.gpl2;
+    license = licenses.gpl2Plus;
     platforms = platforms.linux;
     maintainers = with maintainers; [ ericsagnes ];
   };
