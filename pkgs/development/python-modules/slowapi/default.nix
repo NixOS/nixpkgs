be5344745add1193ee8d1a8064c3f864ea8415bc
Fabian Affolter <mail@fabian-affolter.ch>
2022-02-23 19:50:42 +0100

    python310Packages.slowapi: disable failing tests on Python 3.10

--- pkgs/development/python-modules/slowapi/default.nix
+++ pkgs/development/python-modules/slowapi/default.nix
@@ -7,6 +7,7 @@
 , hiro
 , poetry-core
 , pytestCheckHook
+, pythonAtLeast
 , pythonOlder
 , redis
 , starlette
@@ -50,9 +51,11 @@ buildPythonPackage rec {
   '';
 
   disabledTests = [
-    # E       AssertionError: Regex pattern 'parameter `request` must be an instance of starlette.requests.Request' does not match 'This portal is not running'.
+    # AssertionError: Regex pattern 'parameter `request` must be an instance of starlette.requests.Request' does not match 'This portal is not running'.
     "test_endpoint_request_param_invalid"
     "test_endpoint_response_param_invalid"
+  ] ++ lib.optionals (pythonAtLeast "3.10") [
+    "test_multiple_decorators"
   ];
 
   pythonImportsCheck = [
