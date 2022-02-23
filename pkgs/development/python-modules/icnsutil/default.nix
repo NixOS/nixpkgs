88ac8585ba2e443c1075d28f3d7761f18193eb1b
Randy Eckenrode <randy@largeandhighquality.com>
2022-02-22 23:40:01 -0500

    icnsutil: init at 1.0.1

---
+++ pkgs/development/python-modules/icnsutil/default.nix
@@ -0,0 +1,30 @@
+{ lib
+, python3Packages
+, fetchFromGitHub
+}:
+
+python3Packages.buildPythonPackage rec {
+  pname = "icnsutil";
+  version = "1.0.1";
+
+  src = fetchFromGitHub {
+    owner = "relikd";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-TfQvAbP7iCpRQg2G+ejl245NCYo9DpYwMgiwY2BuJnY=";
+  };
+
+  doCheck = true;
+
+  checkPhase = ''
+    ${python3Packages.python}/bin/python3 tests/test_icnsutil.py
+    ${python3Packages.python}/bin/python3 tests/test_cli.py
+  '';
+
+  meta = {
+    homepage = "https://github.com/relikd/icnsutil";
+    description = "Create and extract .icns files.";
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.reckenrode ];
+  };
+}
