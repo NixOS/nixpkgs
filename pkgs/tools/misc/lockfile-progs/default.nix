7281a936b485412deef49dd6173fca66c86ae2b9
Renaud <c0bw3b@users.noreply.github.com>
2022-02-23 22:17:33 +0100

    lockfile-progs: refactor

--- pkgs/tools/misc/lockfile-progs/default.nix
+++ pkgs/tools/misc/lockfile-progs/default.nix
@@ -1,27 +1,28 @@
 { lib, stdenv, fetchurl, liblockfile }:
 
 stdenv.mkDerivation rec {
-  _name   = "lockfile-progs";
+  pname   = "lockfile-progs";
   version = "0.1.19";
-  name    = "${_name}-${version}";
 
   src = fetchurl {
-    url = "mirror://debian/pool/main/l/${_name}/${_name}_${version}.tar.gz";
+    url = "mirror://debian/pool/main/l/${pname}/${pname}_${version}.tar.gz";
     sha256 = "sha256-LFcEsByPR0+CkheA5Fkqknsr9qbXYWNUpsXXzVZkhX4=";
   };
 
   buildInputs = [ liblockfile ];
 
   installPhase = ''
+    runHook preInstall
     mkdir -p $out/bin $out/man/man1
     install -s bin/* $out/bin
     install man/*.1 $out/man/man1
+    runHook postInstall
   '';
 
   meta = {
     description = "Programs for locking and unlocking files and mailboxes";
     homepage = "http://packages.debian.org/sid/lockfile-progs";
-    license = lib.licenses.gpl2;
+    license = lib.licenses.gpl2Only;
 
     maintainers = [ lib.maintainers.bluescreen303 ];
     platforms = lib.platforms.all;
