fd4f33424e84c36af0de4e49897a2806ac7ceba6
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-24 14:16:11 +0000

    python310Packages.google-resumable-media: 2.2.1 -> 2.3.0

--- pkgs/development/python-modules/google-resumable-media/default.nix
+++ pkgs/development/python-modules/google-resumable-media/default.nix
@@ -12,11 +12,11 @@
 
 buildPythonPackage rec {
   pname = "google-resumable-media";
-  version = "2.2.1";
+  version = "2.3.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-se37mIZ8n6Jap68S1kaGZbg8UytzSe/6uAWgJ+qLvuU=";
+    sha256 = "sha256-Gn3OV5CwRRjtwCws4zllVWZg1klXEG1mqUUIbitkJXI=";
   };
 
   propagatedBuildInputs = [ google-auth google-crc32c requests ];
