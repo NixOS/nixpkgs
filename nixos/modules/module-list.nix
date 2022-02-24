5721945070e61e6d92c8a2391624673deb733105
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2022-02-24 17:09:56 +0100

    nixos/tetrd: add to module list and fix enable description
    The module was dangling and and not listed.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -898,6 +898,7 @@
   ./services/networking/tcpcrypt.nix
   ./services/networking/teamspeak3.nix
   ./services/networking/tedicross.nix
+  ./services/networking/tetrd.nix
   ./services/networking/teleport.nix
   ./services/networking/thelounge.nix
   ./services/networking/tinc.nix
