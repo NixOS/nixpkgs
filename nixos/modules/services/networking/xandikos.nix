49cf508f4f59dc8e42c5a4ddf595874ebba7d023
schnusch <schnusch@users.noreply.github.com>
2022-02-24 18:09:44 +0100

    nixos/xandikos: use systemd socket activation
    Xandikos' handling of listening addresses is quite buggy. There were multiple
    bugs regarding socket activation, which caused it to also listen on unwanted
    default ports. Additionally the metrics port reuses the listen address.
    
    To avoid all this unexpected behaviour we run Xandikos in a private network
    namespace and let systemd handle our *desired* listening adresses. Optionally
    the metrics are made accessible in the host's network namespace through
    `xandikos-metrics.service` using systemd-socket-proxyd.

--- nixos/modules/services/networking/xandikos.nix
+++ nixos/modules/services/networking/xandikos.nix
@@ -9,8 +9,18 @@ with lib;
 
 let
   cfg = config.services.xandikos;
+
+  nginxProxyAddress =
+    let
+      first = head (toList cfg.address);
+    in
+    if hasInfix "/" first then "unix:${first}" else first;
 in
 {
+
+  imports = [
+    (mkRemovedOptionModule [ "services" "xandikos" "port" ] "Use services.xandikos.address")
+  ];
 
   options = {
     services.xandikos = {
@@ -19,18 +29,24 @@ in
       package = mkPackageOption pkgs "xandikos" { };
 
       address = mkOption {
-        type = types.str;
-        default = "localhost";
         description = ''
-          The IP address on which Xandikos will listen.
-          By default listens on localhost.
+          systemd ListenStream where Xandikos shall listen, see {manpage}`systemd.socket(5)`
         '';
-      };
-
-      port = mkOption {
-        type = types.port;
-        default = 8080;
-        description = "The port of the Xandikos web application";
+        type =
+          with types;
+          oneOf [
+            str
+            (listOf str)
+          ];
+        default = [
+          "127.0.0.1:8080"
+          "[::1]:8080"
+        ];
+        example = [
+          "0.0.0.0:8080"
+          "[::]:8080"
+          "/run/xandikos/socket"
+        ];
       };
 
       routePrefix = mkOption {
@@ -83,6 +99,31 @@ in
         };
       };
 
+      metrics = {
+        enable = mkEnableOption "Xandikos' metrics";
+
+        address = mkOption {
+          description = ''
+            systemd ListenStream where the Xandikos metrics shall listen, see {manpage}`systemd.socket(5)`
+          '';
+          type =
+            with types;
+            oneOf [
+              str
+              (listOf str)
+            ];
+          default = [
+            "127.0.0.1:8081"
+            "[::1]:8081"
+          ];
+          example = [
+            "0.0.0.0:8081"
+            "[::]:8081"
+            "/run/xandikos/metrics.socket"
+          ];
+        };
+      };
+
     };
 
   };
@@ -92,10 +133,15 @@ in
   config = mkIf cfg.enable (mkMerge [
     {
 
+      systemd.sockets.xandikos = {
+        wantedBy = [ "sockets.target" ];
+        socketConfig.ListenStream = cfg.address;
+      };
+
       systemd.services.xandikos = {
         description = "A Simple Calendar and Contact Server";
-        after = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
+        requires = [ "xandikos.socket" ];
+        after = [ "xandikos.socket" ];
 
         serviceConfig = {
           User = "xandikos";
@@ -105,6 +151,19 @@ in
           StateDirectory = "xandikos";
           StateDirectoryMode = "0700";
           PrivateDevices = true;
+          # Systemd socket activation was broken time and again and bugs with
+          # default listening addresses introduced. See
+          #   * https://github.com/jelmer/xandikos/issues/134
+          #   * https://github.com/jelmer/xandikos/pull/133
+          #   * https://github.com/jelmer/xandikos/pull/136
+          #   * https://github.com/jelmer/xandikos/pull/155
+          #   * https://github.com/jelmer/xandikos/issues/260
+          #   * https://github.com/jelmer/xandikos/pull/262
+          # Additionally --metrics-port reuses --listen-address, which
+          # defaults to 0.0.0.0/::. To avoid all this behaviour having
+          # unwanted side-effects we run it in a private network namespace.
+          # The metrics are made accesible through `xandikos-metrics.socket`.
+          PrivateNetwork = true;
           # Sandboxing
           CapabilityBoundingSet = "CAP_NET_RAW CAP_NET_ADMIN";
           ProtectSystem = "strict";
@@ -122,21 +181,51 @@ in
           ExecStart = ''
             ${cfg.package}/bin/xandikos \
               --directory /var/lib/xandikos \
-              --listen-address ${cfg.address} \
-              --port ${toString cfg.port} \
               --route-prefix ${cfg.routePrefix} \
-              ${lib.concatStringsSep " " cfg.extraOptions}
+              ${lib.concatStringsSep " " (
+                lib.optionals cfg.metrics.enable [
+                  "--metrics-port"
+                  "8081"
+                ]
+                ++ cfg.extraOptions
+              )}
           '';
         };
       };
     }
+
+    (mkIf cfg.metrics.enable {
+      systemd.sockets.xandikos-metrics = {
+        wantedBy = [ "sockets.target" ];
+        socketConfig.ListenStream = cfg.metrics.address;
+      };
+
+      systemd.services.xandikos-metrics = {
+        description = "A proxy to Xandikos' metrics";
+        # see https://www.freedesktop.org/software/systemd/man/systemd-socket-proxyd.html#Namespace%20Example
+        requires = [
+          "xandikos.service"
+          "xandikos-metrics.socket"
+        ];
+        after = [
+          "xandikos.service"
+          "xandikos-metrics.socket"
+        ];
+        unitConfig.JoinsNamespaceOf = "xandikos.service";
+        serviceConfig = {
+          Type = "notify";
+          ExecStart = "${config.systemd.package}/lib/systemd/systemd-socket-proxyd localhost:8081";
+          PrivateNetwork = true; # required by JoinsNamespaceOf
+        };
+      };
+    })
 
     (mkIf cfg.nginx.enable {
       services.nginx = {
         enable = true;
         virtualHosts."${cfg.nginx.hostName}" = {
           locations."/" = {
-            proxyPass = "http://${cfg.address}:${toString cfg.port}/";
+            proxyPass = "http://${nginxProxyAddress}";
           };
         };
       };
