b119781b2d63524fdf742f78e1b6a7568253e042
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-25 14:21:55 +0000

    nats-server: 2.7.2 -> 2.7.3

--- pkgs/servers/nats-server/default.nix
+++ pkgs/servers/nats-server/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname   = "nats-server";
-  version = "2.7.2";
+  version = "2.7.3";
 
   src = fetchFromGitHub {
     rev    = "v${version}";
     owner  = "nats-io";
     repo   = pname;
-    sha256 = "0w4hjz1x6zwcxhnd1y3874agyn8nsdra4fky6kc2rrfikjcw003y";
+    sha256 = "sha256-8XCk447Ow+lPo7cTSWQFytsoi0XQZygi+8H0RRtA1bA=";
   };
 
-  vendorSha256 = "1gvvjwx1g8mhcqi3ssb3k5ylkz0afpmnf6h2zfny9rc4dk2cp2dy";
+  vendorSha256 = "sha256-ES56ARMAOLQPqc8/xeqB0cpPFq1fb1ShLXbSeVX9yXs=";
 
   doCheck = false;
 
