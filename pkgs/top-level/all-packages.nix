97e38be9f68ee4fc04413bc71ed9dfbefda98aa6
P. R. d. O <d.ol.rod@tutanota.com>
2022-02-25 13:34:32 -0600

    gometer: init at 5.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22391,6 +22391,8 @@ with pkgs;
 
   gmailctl = callPackage ../applications/networking/gmailctl { };
 
+  gometer = callPackage ../applications/misc/gometer { };
+
   gomp = callPackage ../applications/version-management/gomp { };
 
   gomplate = callPackage ../development/tools/gomplate {};
