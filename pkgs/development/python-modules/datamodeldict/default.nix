3ab90617d3f597004882fee9af51478ab9a74210
Fabian Affolter <mail@fabian-affolter.ch>
2022-02-25 11:32:34 +0100

    python3Packages.datamodeldict: clean-up

--- pkgs/development/python-modules/datamodeldict/default.nix
+++ pkgs/development/python-modules/datamodeldict/default.nix
@@ -1,27 +1,38 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
 , xmltodict
 }:
 
 buildPythonPackage rec {
+  pname = "datamodeldict";
   version = "0.9.9";
-  pname = "DataModelDict";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-DadBRsc8qEu9PWgMNllGS2ESKL7kgBLDhg4yDr87WRk=";
+    pname = "DataModelDict";
+    inherit version;
+    hash = "sha256-DadBRsc8qEu9PWgMNllGS2ESKL7kgBLDhg4yDr87WRk=";
   };
 
-  propagatedBuildInputs = [ xmltodict ];
+  propagatedBuildInputs = [
+    xmltodict
+  ];
 
   # no tests
   doCheck = false;
 
+  pythonImportsCheck = [
+    "DataModelDict"
+  ];
+
   meta = with lib; {
-    homepage = "https://github.com/usnistgov/DataModelDict/";
     description = "Class allowing for data models equivalently represented as Python dictionaries, JSON, and XML";
+    homepage = "https://github.com/usnistgov/DataModelDict/";
     license = licenses.mit;
-    maintainers = [ maintainers.costrouc ];
+    maintainers = with maintainers; [ costrouc ];
   };
 }
