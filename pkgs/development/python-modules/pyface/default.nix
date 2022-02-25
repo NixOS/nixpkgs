00e569b437ddb0538cd445ca481ef6e48b170937
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-25 08:15:51 +0000

    python310Packages.pyface: 7.4.0 -> 7.4.1

--- pkgs/development/python-modules/pyface/default.nix
+++ pkgs/development/python-modules/pyface/default.nix
@@ -4,11 +4,11 @@
 
 buildPythonPackage rec {
   pname = "pyface";
-  version = "7.4.0";
+  version = "7.4.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-r8Awj9dOYPWxh1Ar2JK/nhuY8hAGFO4+6yr9yq7Pb6s=";
+    sha256 = "sha256-UtzzZ5yj5hCjynxLmQSpbGkWiASNtdflKvjlAZ5HrbY=";
   };
 
   propagatedBuildInputs = [ importlib-metadata importlib-resources six traits ];
