02cebc91b95b3e8ca10b38953a4a4e3a6f3742af
Fabian Affolter <mail@fabian-affolter.ch>
2022-02-25 08:41:05 +0100

    python3Packages.b2sdk: add format
    (cherry picked from commit c8d355433e47067c2cab8a9049a060cc9f1699eb)

--- pkgs/development/python-modules/b2sdk/default.nix
+++ pkgs/development/python-modules/b2sdk/default.nix
@@ -3,7 +3,6 @@
 , buildPythonPackage
 , fetchPypi
 , importlib-metadata
-, isPy27
 , logfury
 , pytestCheckHook
 , pytest-lazy-fixture
@@ -17,11 +16,13 @@
 buildPythonPackage rec {
   pname = "b2sdk";
   version = "1.14.1";
-  disabled = isPy27;
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-fYOeyhKm9mRT61NcQVaXFKeRC8AS9lfIZMO/s6iFaeg=";
+    hash = "sha256-fYOeyhKm9mRT61NcQVaXFKeRC8AS9lfIZMO/s6iFaeg=";
   };
 
   nativeBuildInputs = [
@@ -65,5 +66,6 @@ buildPythonPackage rec {
     description = "Client library and utilities for access to B2 Cloud Storage (backblaze)";
     homepage = "https://github.com/Backblaze/b2-sdk-python";
     license = licenses.mit;
+    maintainers = with maintainers; [ ];
   };
 }
