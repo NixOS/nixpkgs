5bd0ba079e137cfb8ce90d6a2af235e67a6048f1
sternenseemann <sternenseemann@systemli.org>
2022-02-26 10:28:42 +0100

    haskellPackages.ema: fix eval

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -2281,7 +2281,8 @@ self: super: {
   ];
 
   # 2022-02-25: Not compatible with relude 1.0
-  ema = assert super.ema.version == 0.6.0.0; super.ema.overrideScope (self: super: { relude = doJailbreak self.relude_0_7_0_0; });
+  ema = assert super.ema.version == "0.6.0.0";
+    super.ema.overrideScope (self: super: { relude = doJailbreak self.relude_0_7_0_0; });
 
   # attoparsec bump is on v2 branch, but not released yet
   irc-core = assert super.irc-core.version == "2.10"; doJailbreak super.irc-core;
