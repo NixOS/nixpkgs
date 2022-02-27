8c2ba9c5818c1d510748eeaa50efab6d929c23ea
Ben Siraphob <bensiraphob@gmail.com>
2022-02-27 15:54:56 -0600

    lfs: 2.2.0 -> 2.3.0

--- pkgs/tools/filesystems/lfs/default.nix
+++ pkgs/tools/filesystems/lfs/default.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "lfs";
-  version = "2.2.0";
+  version = "2.3.0";
 
   src = fetchFromGitHub {
     owner = "Canop";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-q8ekLh0pw+kMIE2SP3cBgrMTfMjY5zRWR58mq5bbB8U=";
+    sha256 = "sha256-c52ArXTpzGlY4+yXY1hQfZxoFvgRfpzMRqnF36DHaaI=";
   };
 
-  cargoSha256 = "sha256-eZM48OyYgEJl71I2iu52hKwjDrbSelR4JH/se79bBDs=";
+  cargoSha256 = "sha256-iQqEkyG+tg0l7I9fp2bT5KKzR9047c5xj4QuhWncNuE=";
 
   meta = with lib; {
     description = "Get information on your mounted disks";
