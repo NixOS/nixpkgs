2c08ee56441e81fdde4bece06a03a333bcfcba2e
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-28 10:26:11 +0000

    lrzip: 0.641 -> 0.650
    (cherry picked from commit 833aa51924cc1d817b0cba4da85932eac21e0f0d)

--- pkgs/tools/compression/lrzip/default.nix
+++ pkgs/tools/compression/lrzip/default.nix
@@ -5,13 +5,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "lrzip";
-  version = "0.641";
+  version = "0.650";
 
   src = fetchFromGitHub {
     owner = "ckolivas";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-253CH6TiHWyr13C76y9PXjyB7gj2Bhd2VRgJ5r+cm/g=";
+    sha256 = "sha256-rHjaTgNVGfnGio4geuWRfPds5BqcXJu7p8XJh83rRTs=";
   };
 
   postPatch = lib.optionalString stdenv.isDarwin ''
