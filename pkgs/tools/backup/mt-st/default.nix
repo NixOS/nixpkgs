57438d4861910030069d7cab4ea6ed93892864aa
Ľuboš Lendáč <Arcanum417@users.noreply.github.com>
2022-02-28 19:24:38 +0100

    Update mt-st to verision 1.4

--- pkgs/tools/backup/mt-st/default.nix
+++ pkgs/tools/backup/mt-st/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "mt-st";
-  version = "1.3";
+  version = "1.4";
 
   src = fetchurl {
-    url = "https://github.com/iustin/mt-st/releases/download/mt-st-${version}/mt-st-${version}.tar.gz";
-    sha256 = "b552775326a327cdcc076c431c5cbc4f4e235ac7c41aa931ad83f94cccb9f6de";
+    url = "https://github.com/iustin/mt-st/releases/download/v${version}/mt-st-${version}.tar.gz";
+    sha256 = "f98a4ff02a00f2588ea370bb9560484bf48eb841e2c80a382fc0cd8f1d9a2b4c";
   };
 
   installFlags = [ "PREFIX=$(out)" "EXEC_PREFIX=$(out)" ];
