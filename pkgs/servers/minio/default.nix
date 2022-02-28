c1cf73c9c6d0dc4c3fb00f672f7cb50ac2472151
R. Ryantm <ryantm-bot@ryantm.com>
2022-02-28 11:26:48 +0000

    minio: 2022-02-24T22-12-01Z -> 2022-02-26T02-54-46Z

--- pkgs/servers/minio/default.nix
+++ pkgs/servers/minio/default.nix
@@ -15,16 +15,16 @@ let
 in
 buildGoModule rec {
   pname = "minio";
-  version = "2022-02-24T22-12-01Z";
+  version = "2022-02-26T02-54-46Z";
 
   src = fetchFromGitHub {
     owner = "minio";
     repo = "minio";
     rev = "RELEASE.${version}";
-    sha256 = "sha256-7bnT+rxQw2h1wCkXjsYm8ystU9F4EUL0KASkJmqLgXQ=";
+    sha256 = "sha256-+XiormhPdbQRzlqD4Y/+zcPnlIxNJnz9dU53eKYdG5Q=";
   };
 
-  vendorSha256 = "sha256-fYpnYMt6VrC2eem8XvK8oAR1B0rQ4DV6ZWo0cLNCjCs=";
+  vendorSha256 = "sha256-omX4nT85+ZT71cQXdiPJHyR+/afEbb43GqeJuxzV0mA=";
 
   doCheck = false;
 
