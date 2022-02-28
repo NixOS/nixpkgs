ab34433bc0dcdf9b3cfbc8bb187030920b4c0e7b
Alexander Nortung <alex_nortung@live.dk>
2022-02-28 13:18:39 +0100

    emojipick: init at 2021-01-27 (#158187)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23601,6 +23601,8 @@ with pkgs;
     inherit (nodePackages) svgo;
   };
 
+  emojipick = callPackage ../applications/misc/emojipick { };
+
   encode-sans = callPackage ../data/fonts/encode-sans { };
 
   envypn-font = callPackage ../data/fonts/envypn-font
