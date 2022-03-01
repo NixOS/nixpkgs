5d4321b70390eba3ef64cc4154d22e51cb0daaba
Kylie McClain <kylie@somas.is>
2022-03-01 01:12:13 -0500

    linja-pi-pu-lukin: init at unstable-2021-10-29

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26398,6 +26398,8 @@ with pkgs;
 
   line-awesome = callPackage ../data/fonts/line-awesome { };
 
+  linja-pi-pu-lukin = callPackage ../data/fonts/linja-pi-pu-lukin {};
+
   linux-manual = callPackage ../data/documentation/linux-manual { };
 
   lklug-sinhala = callPackage ../data/fonts/lklug-sinhala {};
