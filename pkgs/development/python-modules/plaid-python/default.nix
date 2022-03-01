55b2c3c4c501e3a3d4c538846d06e87d63d30ad0
Fabian Affolter <mail@fabian-affolter.ch>
2022-03-01 08:38:19 +0100

    python3Packages.plaid-python: disable on older Python releases

--- pkgs/development/python-modules/plaid-python/default.nix
+++ pkgs/development/python-modules/plaid-python/default.nix
@@ -4,15 +4,19 @@
 , nulltype
 , python-dateutil
 , urllib3
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "plaid-python";
   version = "8.11.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-zvwqMpI/aufZLf9dSVEDY2Letiyso8oSf9o5kanXW7U=";
+    hash = "sha256-zvwqMpI/aufZLf9dSVEDY2Letiyso8oSf9o5kanXW7U=";
   };
 
   propagatedBuildInputs = [
