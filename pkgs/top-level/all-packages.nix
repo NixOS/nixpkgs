18a489144183bb6097bca37fb4e093c1cd55ca0c
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-03-02 00:09:44 +0100

    shellspec: init at 0.28.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9728,6 +9728,8 @@ with pkgs;
 
   shelldap = callPackage ../tools/misc/shelldap { };
 
+  shellspec = callPackage ../tools/misc/shellspec { };
+
   schema2ldif = callPackage ../tools/text/schema2ldif { };
 
   sharedown = callPackage ../tools/misc/sharedown { };
