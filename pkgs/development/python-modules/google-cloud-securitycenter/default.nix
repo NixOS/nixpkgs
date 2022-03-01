f1ba48e24289a2b572157e4e9a1cfcdb934425f1
Fabian Affolter <mail@fabian-affolter.ch>
2022-03-01 08:47:36 +0100

    python3Packages.google-cloud-securitycenter: disable on older Python releases

--- pkgs/development/python-modules/google-cloud-securitycenter/default.nix
+++ pkgs/development/python-modules/google-cloud-securitycenter/default.nix
@@ -8,20 +8,33 @@
 , proto-plus
 , pytestCheckHook
 , pytest-asyncio
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "google-cloud-securitycenter";
   version = "1.9.0";
+  format = "setuptools";
+  
+  disabled = pythonOlder "3.6";
 
-  src = fetchPypi {
+src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-qtaZs6mgcYeEEdf6KGQNx7c+RP8SqSNyTCZuhTVAq38=";
+    hash = "sha256-qtaZs6mgcYeEEdf6KGQNx7c+RP8SqSNyTCZuhTVAq38=";
   };
 
-  propagatedBuildInputs = [ grpc-google-iam-v1 google-api-core libcst proto-plus ];
+  propagatedBuildInputs = [
+    grpc-google-iam-v1
+    google-api-core
+    libcst
+    proto-plus
+  ];
 
-  checkInputs = [ mock pytestCheckHook pytest-asyncio ];
+  checkInputs = [
+    mock
+    pytestCheckHook
+    pytest-asyncio
+  ];
 
   pythonImportsCheck = [
     "google.cloud.securitycenter"
