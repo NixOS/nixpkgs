7d34bb2077ab77233d925992c1f57616578f8823
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-01 08:11:55 +0000

    python310Packages.python-vlc: 3.0.12118 -> 3.0.16120

--- pkgs/development/python-modules/python-vlc/default.nix
+++ pkgs/development/python-modules/python-vlc/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "python-vlc";
-  version = "3.0.12118";
+  version = "3.0.16120";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-Vm8vfDA/aACFHKzAFt8cbu7AlK1j4KSdh9udaYCU8fs=";
+    hash = "sha256-kvmP7giPcr1tBjs7MxLQvSmzfnrWXd6zpzAzIDAMKAc=";
   };
 
   patches = [
