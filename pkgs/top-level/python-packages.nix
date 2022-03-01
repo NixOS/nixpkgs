e3e5e0199ee33e17850493b5d98f6feea7c56231
jacobi petrucciani <j@cobi.dev>
2022-03-01 00:44:47 -0500

    python3Packages.types-enum34: init at 1.1.8

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10165,6 +10165,8 @@ in {
 
   types-decorator = callPackage ../development/python-modules/types-decorator { };
 
+  types-enum34 = callPackage ../development/python-modules/types-enum34 { };
+
   types-freezegun = callPackage ../development/python-modules/types-freezegun { };
 
   types-futures = callPackage ../development/python-modules/types-futures { };
