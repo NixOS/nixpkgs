5bf27e9c710ca86fa9f223ed60ed90613e1b89bb
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-02 22:42:30 +0100

    python3Packages.twilio: 7.5.0 -> 7.7.0

--- pkgs/development/python-modules/twilio/default.nix
+++ pkgs/development/python-modules/twilio/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "twilio";
-  version = "7.5.0";
+  version = "7.7.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "twilio";
     repo = "twilio-python";
     rev = version;
-    sha256 = "0h6r9nz7dcvagrjhzvnirpnjazcy9r64cwlr2bnmlrbjhwdni9rq";
+    sha256 = "sha256-PxLDAP/6Ddvf58eEyX3DHkdBNuLE5DlLdCEaRguqOy0=";
   };
 
   propagatedBuildInputs = [
