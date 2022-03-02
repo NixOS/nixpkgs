e31838f0e38a8893753cd23fe14b6f63f43d57bd
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-02 22:42:02 +0100

    python3Packages.intbitset: 2.4.1 -> 3.0.0

--- pkgs/development/python-modules/intbitset/default.nix
+++ pkgs/development/python-modules/intbitset/default.nix
@@ -1,35 +1,22 @@
 { lib
 , fetchPypi
 , buildPythonPackage
-, six
-, nose
+, pytestCheckHook
 }:
+
 buildPythonPackage rec {
   pname = "intbitset";
-  version = "2.4.1";
+  version = "3.0.0";
+  format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "44bca80b8cc702d5a56f0686f2bb5e028ab4d0c2c1761941589d46b7fa2c701c";
+    sha256 = "sha256-tDG3CAlTZvz9Pi2pLq0TEPhl3DyYuWQS1N6VNNNokEE=";
   };
 
-  patches = [
-    # fixes compilation on aarch64 and determinism (uses -march=core2 and
-    # -mtune=native)
-    ./remove-impure-tuning.patch
-  ];
-
-  propagatedBuildInputs = [
-    six
-  ];
-
   checkInputs = [
-    nose
+    pytestCheckHook
   ];
-
-  checkPhase = ''
-    nosetests
-  '';
 
   pythonImportsCheck = [
     "intbitset"
