975788e3d3886e37d38f08d227a5aa35561274bc
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-02 22:42:14 +0100

    python3Packages.pyfaidx: 0.6.3.1 -> 0.6.4

--- pkgs/development/python-modules/pyfaidx/default.nix
+++ pkgs/development/python-modules/pyfaidx/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "pyfaidx";
-  version = "0.6.3.1";
+  version = "0.6.4";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "93adf036a75e08dc9b1dcd59de6a4db2f65a48c603edabe2e499764b6535ed50";
+    sha256 = "sha256-e6O9yx30unSfdmWzTmoFKqToQkBqDflebfRxfMEj85I=";
   };
 
   nativeBuildInputs = [
