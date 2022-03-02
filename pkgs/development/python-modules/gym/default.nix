a9d44fd71618486a621fd3c42df551b7a06ce67f
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-02 22:42:00 +0100

    python3Packages.gym: 0.21.0 -> 0.22.0

--- pkgs/development/python-modules/gym/default.nix
+++ pkgs/development/python-modules/gym/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage rec {
   pname = "gym";
-  version = "0.21.0";
+  version = "0.22.0";
 
   src = fetchFromGitHub {
     owner = "openai";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "12b545xz0r2g4z5r7f8amxl7nm0lqymkzwcwhg1bni9h0sxwpv6c";
+    rev = version;
+    sha256 = "sha256-JbPWLuQGo+fErUlCKKpMwWdu0KvXBDuH2MeAHdJCTgM=";
   };
 
   propagatedBuildInputs = [
