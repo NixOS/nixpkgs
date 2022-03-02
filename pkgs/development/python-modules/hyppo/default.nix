7d297f1167d83947a2210481ef450555d69317bd
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-02 22:42:01 +0100

    python3Packages.hyppo: 0.2.2 -> 0.3.2

--- pkgs/development/python-modules/hyppo/default.nix
+++ pkgs/development/python-modules/hyppo/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "hyppo";
-  version = "0.2.2";
+  version = "0.3.2";
 
   disabled = pythonOlder "3.6";
 
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "neurodata";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1wrzrppyjq0pc03bn6qcslxzcnwn7fr2z5lm71gfpli5k05i26nr";
+    sha256 = "sha256-DQ5DrQrFBJ3dnGAjD1c/7GCJeR3g+aL2poR4hwOvmPA=";
   };
 
   propagatedBuildInputs = [
