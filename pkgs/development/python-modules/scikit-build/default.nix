ebba81c52002a08ab3452bb172e4773e319a5439
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-02 22:42:25 +0100

    python3Packages.scikit-build: 0.12.0 -> 0.13.1

--- pkgs/development/python-modules/scikit-build/default.nix
+++ pkgs/development/python-modules/scikit-build/default.nix
@@ -25,11 +25,11 @@
 
 buildPythonPackage rec {
   pname = "scikit-build";
-  version = "0.12.0";
+  version = "0.13.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f851382c469bcd9a8c98b1878bcfdd13b68556279d2fd9a329be41956ae5a7fe";
+    sha256 = "sha256-XRd0ousVmI4IHFgsJUq0qXUgluajTyNUEct5vWFmDDc=";
   };
 
   propagatedBuildInputs = [
@@ -66,6 +66,8 @@ buildPythonPackage rec {
     "test_install_command" # tries to alter out path
     "test_test_command" # tries to alter out path
     "test_setup" # tries to install using distutils
+    "test_pep518" # pip exits with code 1
+    "test_dual_pep518" # pip exits with code 1
   ]);
 
   checkPhase = ''
