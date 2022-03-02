1ceb560bfd6542b87e55b4e24f86009055f731e0
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-02 22:42:21 +0100

    python3Packages.pytools: 2021.2.9 -> 2022.1

--- pkgs/development/python-modules/pytools/default.nix
+++ pkgs/development/python-modules/pytools/default.nix
@@ -11,12 +11,12 @@
 
 buildPythonPackage rec {
   pname = "pytools";
-  version = "2021.2.9";
+  version = "2022.1";
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "db6cf83c9ba0a165d545029e2301621486d1e9ef295684072e5cd75316a13755";
+    sha256 = "sha256-GXqs9uH11gxxW5JDh5Kst3Aq7Vnrv7FH+oTtp4DlT+4=";
   };
 
   checkInputs = [ pytest ];
