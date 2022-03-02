c4343111279db1d33a63c6b68a21010aea10ca55
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-02 22:41:59 +0100

    python3Packages.ftfy: 6.0.3 -> 6.1.1

--- pkgs/development/python-modules/ftfy/default.nix
+++ pkgs/development/python-modules/ftfy/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage rec {
   pname = "ftfy";
-  version = "6.0.3";
+  version = "6.1.1";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ba71121a9c8d7790d3e833c6c1021143f3e5c4118293ec3afb5d43ed9ca8e72b";
+    sha256 = "sha256-v8IBn4T82FFBkVIyCmN1YEoPFFnCgbWxmbLNDS5yf48=";
   };
 
   propagatedBuildInputs = [
