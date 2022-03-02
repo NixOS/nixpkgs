addee10e2bfcde41a4de8871e5dd4ab67b99dbf4
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-02 15:55:55 +0000

    signalbackup-tools: 20220227 -> 20220301

--- pkgs/applications/networking/instant-messengers/signalbackup-tools/default.nix
+++ pkgs/applications/networking/instant-messengers/signalbackup-tools/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "signalbackup-tools";
-  version = "20220227";
+  version = "20220301";
 
   src = fetchFromGitHub {
     owner = "bepaald";
     repo = pname;
     rev = version;
-    sha256 = "sha256-TpemAMBh3XN71C4vYr9TmKBw2W5esgtL2dKLHN4sQd8=";
+    sha256 = "sha256-1Z6jl4trAzKrI9WO3MpCJtg0R3Riyd5wvunErCkkiig=";
   };
 
   # Remove when Apple SDK is >= 10.13
