3855b2d0c3b6a6e9f1be95233e2582ad1acda6ad
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-02 22:41:48 +0100

    python3Packages.adblock: 0.5.1 -> 0.5.2

--- pkgs/development/python-modules/adblock/default.nix
+++ pkgs/development/python-modules/adblock/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "adblock";
-  version = "0.5.1";
+  version = "0.5.2";
   format = "pyproject";
 
   disabled = pythonOlder "3.6";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "ArniDagur";
     repo = "python-adblock";
     rev = version;
-    sha256 = "sha256-f6PmEHVahQv8t+WOkE8DO2emivHG2t14hUSIf/l8omY=";
+    sha256 = "sha256-6FH+AVK7+Yg1a6oKbFV80TuGGE4Y7I3mMVzwVHdHYO4=";
   };
 
   cargoDeps = rustPlatform.fetchCargoTarball {
