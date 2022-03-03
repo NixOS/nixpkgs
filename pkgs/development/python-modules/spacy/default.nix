7f1eb5ba09f17a1a4d00e5c54c2193f2348f7760
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-03 06:51:48 +0000

    python310Packages.spacy: 3.2.2 -> 3.2.3

--- pkgs/development/python-modules/spacy/default.nix
+++ pkgs/development/python-modules/spacy/default.nix
@@ -31,13 +31,13 @@
 
 buildPythonPackage rec {
   pname = "spacy";
-  version = "3.2.2";
+  version = "3.2.3";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-s9mjYeHjwcGtD38kTyaH+s5CD+LWQtkpic01eUSn1w8=";
+    sha256 = "sha256-JdAz/Ae4+/yb3Te3cLilhtxBTb1gMShEmvMldqOJFnM=";
   };
 
   propagatedBuildInputs = [
