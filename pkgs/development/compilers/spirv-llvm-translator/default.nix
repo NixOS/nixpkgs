266b01d1d421844a42571dc9b09ca27c28c211d9
Jordan Justen <jljusten@gmail.com>
2022-03-03 00:57:05 -0800

    SPIRV-LLVM-Translator: Build and install llvm-spirv tool
    This will make ${self.spirv-llvm-translator}/bin/llvm-spirv available.
    
    As of llvmorg-12.0.0, libclc can now build SPIR-V based libraries, but
    requires the llvm-spirv build tool for this.
    
    Ref: https://github.com/llvm/llvm-project/blob/llvmorg-12.0.0/libclc/CMakeLists.txt#L98
    Signed-off-by: Jordan Justen <jljusten@gmail.com>

--- pkgs/development/compilers/spirv-llvm-translator/default.nix
+++ pkgs/development/compilers/spirv-llvm-translator/default.nix
@@ -30,6 +30,12 @@ stdenv.mkDerivation rec {
   # FIXME: CMake tries to run "/llvm-lit" which of course doesn't exist
   doCheck = false;
 
+  makeFlags = [ "all" "llvm-spirv" ];
+
+  postInstall = ''
+    install -D tools/llvm-spirv/llvm-spirv $out/bin/llvm-spirv
+  '';
+
   meta = with lib; {
     homepage    = "https://github.com/KhronosGroup/SPIRV-LLVM-Translator";
     description = "A tool and a library for bi-directional translation between SPIR-V and LLVM IR";
