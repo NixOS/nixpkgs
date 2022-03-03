371463be00b5a2121811daeb36898efa468cb3ed
Malo Bourgon <mbourgon@gmail.com>
2022-03-03 13:24:14 -0800

    Revert "darwin.apple_sdk.frameworks.IOSurface: use Libsystem instead of xpc (#161561)"
    This reverts commit 92074a45f308a1c42de2c57233471bfd7cc6189c.

--- pkgs/os-specific/darwin/apple-sdk-11.0/frameworks.nix
+++ pkgs/os-specific/darwin/apple-sdk-11.0/frameworks.nix
@@ -89,7 +89,7 @@
   IOBluetooth                      = { inherit CoreBluetooth IOKit; };
   IOBluetoothUI                    = { inherit IOBluetooth; };
   IOKit                            = {};
-  IOSurface                        = { inherit IOKit Libsystem; };
+  IOSurface                        = { inherit IOKit xpc; };
   IOUSBHost                        = {};
   IdentityLookup                   = {};
   ImageCaptureCore                 = {};
