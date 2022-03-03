d2c3410efdfd1642f52f9c67246791895534333a
Robert Hensing <robert@roberthensing.nl>
2022-03-03 12:49:31 +0100

    nixopsUnstable -> nixops_unstable
    Conform to https://nixos.org/manual/nixpkgs/unstable/#sec-package-naming

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33307,7 +33307,7 @@ with pkgs;
 
   nixops = callPackage ../tools/package-management/nixops { };
 
-  nixopsUnstable = lowPrio (callPackage ../applications/networking/cluster/nixops { });
+  nixops_unstable = lowPrio (callPackage ../applications/networking/cluster/nixops { });
 
   nixops-dns = callPackage ../tools/package-management/nixops/nixops-dns.nix { };
 
