0c8d67761f1cfc01b0e5da32532393d3dab1fe2f
Sandro <sandro.jaeckel@gmail.com>
2022-03-05 00:03:18 +0100

    Update pkgs/games/vimgolf/default.nix

--- pkgs/games/vimgolf/default.nix
+++ pkgs/games/vimgolf/default.nix
@@ -7,12 +7,11 @@ bundlerApp {
 
   passthru.updateScript = bundlerUpdateScript "vimgolf";
 
-  meta = with lib;
-    {
-      description = "A game that tests Vim efficiency";
-      homepage = "https://vimgolf.com";
-      license = licenses.mit;
-      maintainers = with maintainers; [ leungbk ];
-      platforms = platforms.unix;
-    };
+  meta = with lib; {
+    description = "A game that tests Vim efficiency";
+    homepage = "https://vimgolf.com";
+    license = licenses.mit;
+    maintainers = with maintainers; [ leungbk ];
+    platforms = platforms.unix;
+  };
 }
