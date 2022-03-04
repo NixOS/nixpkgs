fd2c89dc6b589ac097a871ecbcdd69cc3b712c5d
Fabian Affolter <mail@fabian-affolter.ch>
2022-03-04 09:42:49 +0100

    python3Packages.pyobihai: add format

--- pkgs/development/python-modules/pyobihai/default.nix
+++ pkgs/development/python-modules/pyobihai/default.nix
@@ -8,12 +8,14 @@
 buildPythonPackage rec {
   pname = "pyobihai";
   version = "1.3.2";
-  disabled = pythonOlder "3.6";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   # GitHub release, https://github.com/dshokouhi/pyobihai/issues/10
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-zhsnJyhXlugK0nJ7FJZZcrq2VDQt1a9uCgsJAIABZ28=";
+    hash = "sha256-zhsnJyhXlugK0nJ7FJZZcrq2VDQt1a9uCgsJAIABZ28=";
   };
 
   propagatedBuildInputs = [
@@ -23,7 +25,9 @@ buildPythonPackage rec {
   # Project has no tests
   doCheck = false;
 
-  pythonImportsCheck = [ "pyobihai" ];
+  pythonImportsCheck = [
+    "pyobihai"
+  ];
 
   meta = with lib; {
     description = "Python package to interact with Obihai devices";
