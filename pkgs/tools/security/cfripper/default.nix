d0a9b738d5e3b1dce39a880bf430410fb9e58233
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-04 02:35:35 +0000

    cfripper: 1.4.2 -> 1.5.0

--- pkgs/tools/security/cfripper/default.nix
+++ pkgs/tools/security/cfripper/default.nix
@@ -5,13 +5,13 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "cfripper";
-  version = "1.4.2";
+  version = "1.5.0";
 
   src = fetchFromGitHub {
     owner = "Skyscanner";
     repo = pname;
     rev = version;
-    hash = "sha256-WSDCYKiU1VTi3j7vZ87HjHOShIN+PF7pwR4IVTalbmo=";
+    hash = "sha256-nomNjt/BV/Tu/9w4IEu3tXWQw4/wLTc64khdnQeliH0=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
