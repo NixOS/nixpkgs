edbc343ccbafa4fea972ef6ed023bf6b404e80ef
Michal Sojka <michal.sojka@cvut.cz>
2022-03-04 18:53:07 +0100

    nixos-rebuild: add --no-flake switch
    When a NixOS system uses flakes, i.e., /etc/nixos/flake.nix exists, it
    is impossible to use nixos-rebuild to build a pre-flake
    configuration.nix. Of course, one can directly use nix command to
    build the configuration, but not everybody remembers the correct nix
    options to do that.
    
    With the new option, it is possible to build a pre-flake configuration
    with command like this:
    
        nixos-rebuild build-vm -I nixos-config=./vm.nix --no-flake
    
    The option might be useful for people following older pre-flake
    tutorials on a flake-based system.

--- pkgs/os-specific/linux/nixos-rebuild/nixos-rebuild.sh
+++ pkgs/os-specific/linux/nixos-rebuild/nixos-rebuild.sh
@@ -32,6 +32,7 @@ buildHost=localhost
 targetHost=
 remoteSudo=
 verboseScript=
+noFlake=
 # comma separated list of vars to preserve when using sudo
 preservedSudoVars=NIXOS_INSTALL_BOOTLOADER
 
@@ -114,6 +115,9 @@ while [ "$#" -gt 0 ]; do
         flake="$1"
         flakeFlags=(--extra-experimental-features 'nix-command flakes')
         shift 1
+        ;;
+      --no-flake)
+        noFlake=1
         ;;
       --recreate-lock-file|--no-update-lock-file|--no-write-lock-file|--no-registries|--commit-lock-file)
         lockFlags+=("$i")
@@ -339,7 +343,7 @@ fi
 
 # Use /etc/nixos/flake.nix if it exists. It can be a symlink to the
 # actual flake.
-if [[ -z $flake && -e /etc/nixos/flake.nix ]]; then
+if [[ -z $flake && -e /etc/nixos/flake.nix && -z $noFlake ]]; then
     flake="$(dirname "$(readlink -f /etc/nixos/flake.nix)")"
 fi
 
