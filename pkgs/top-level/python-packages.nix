75e47c35288f416bd46869386ed49ccc72be640e
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-05 21:26:48 +0100

    python3Packages.hatchling: init at 0.18.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3688,6 +3688,8 @@ in {
 
   hatasmota = callPackage ../development/python-modules/hatasmota { };
 
+  hatchling = callPackage ../development/python-modules/hatchling { };
+
   haversine = callPackage ../development/python-modules/haversine { };
 
   hawkauthlib = callPackage ../development/python-modules/hawkauthlib { };
