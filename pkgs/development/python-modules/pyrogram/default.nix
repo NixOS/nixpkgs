1578d6a8df8dc2aa8bc9aa09a3bc0d780cf48f28
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-05 00:47:16 +0000

    python310Packages.pyrogram: 1.4.7 -> 1.4.8

--- pkgs/development/python-modules/pyrogram/default.nix
+++ pkgs/development/python-modules/pyrogram/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "pyrogram";
-  version = "1.4.7";
+  version = "1.4.8";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     pname = "Pyrogram";
     inherit version;
-    hash = "sha256-2kBlTaP2tkUgP4TiP+9zv5pgCap9VnyB8BEHI6SY+uc=";
+    hash = "sha256-mUJEV8DLYhXNvYiuiVDCfgMmZRDCNZuydngj9C9FvQU=";
   };
 
   propagatedBuildInputs = [
