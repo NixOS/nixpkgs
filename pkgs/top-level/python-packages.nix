6959e91eb6b1f1b5f34aee87bf3e4c2182e44ee5
Ember 'n0emis' Keske <git@n0emis.eu>
2022-03-05 19:23:58 +0100

    python3Packages.social-auth-core: init at 4.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9379,6 +9379,8 @@ in {
 
   socketio-client = callPackage ../development/python-modules/socketio-client { };
 
+  social-auth-core = callPackage ../development/python-modules/social-auth-core { };
+
   socialscan = callPackage ../development/python-modules/socialscan { };
 
   socid-extractor =  callPackage ../development/python-modules/socid-extractor { };
