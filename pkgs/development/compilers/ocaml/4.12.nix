edab12c6228e15f42bf38812044d41782319ee48
Vincent Laporte <Vincent.Laporte@gmail.com>
2022-03-05 07:40:39 +0100

    ocaml-ng.ocamlPackages_4_12.ocaml: 4.12.0 → 4.12.1

--- pkgs/development/compilers/ocaml/4.12.nix
+++ pkgs/development/compilers/ocaml/4.12.nix
@@ -1,6 +1,6 @@
 import ./generic.nix {
   major_version = "4";
   minor_version = "12";
-  patch_version = "0";
-  sha256 = "1hxy349jfa2vkfgmxf6pvd9w4z5bmcgsg0fxfdabcghyvjw9vvir";
+  patch_version = "1";
+  sha256 = "1jbjjnmqq6ymsy81x188i256bz4z5jrz1pws8g1qf59c32ganjkf";
 }
