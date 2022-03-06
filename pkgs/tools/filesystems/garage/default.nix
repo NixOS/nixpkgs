47b93c97a277c7f65310e540bf5716c0063d2a5f
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-06 00:25:27 +0000

    garage: 0.6.0 -> 0.6.1

--- pkgs/tools/filesystems/garage/default.nix
+++ pkgs/tools/filesystems/garage/default.nix
@@ -1,17 +1,17 @@
 { lib, rustPlatform, fetchFromGitea, testVersion, garage }:
 rustPlatform.buildRustPackage rec {
   pname = "garage";
-  version = "0.6.0";
+  version = "0.6.1";
 
   src = fetchFromGitea {
     domain = "git.deuxfleurs.fr";
     owner = "Deuxfleurs";
     repo = "garage";
     rev = "v${version}";
-    sha256 = "sha256-NNjqDOkMMRyXce+Z7RQpuffCuVhA1U3qH30rSv939ks=";
+    sha256 = "sha256-BEFxPU4yPtctN7H+EcxJpXnf4tyqBseskls0ZA9748k=";
   };
 
-  cargoSha256 = "sha256-eKJxRcC43D8qVLORer34tlmsWhELTbcJbZLyf0MB618=";
+  cargoSha256 = "sha256-/mOH7VOfIHEydnJUUSts44aGb8tS1/Faxiu4pQDeobY=";
 
   passthru = {
     tests.version = testVersion { package = garage; };
