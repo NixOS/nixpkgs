6e3286cc63654d3c279424f55ba4118f6fccf6a8
Ian M. Jones <ian@ianmjones.com>
2022-03-06 00:34:46 +0000

    wails: init at 2.0.0-beta.33

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15882,6 +15882,8 @@ with pkgs;
 
   vtable-dumper = callPackage ../development/tools/misc/vtable-dumper { };
 
+  wails = callPackage ../development/tools/wails { };
+
   whatsapp-for-linux = callPackage ../applications/networking/instant-messengers/whatsapp-for-linux { };
 
   whatstyle = callPackage ../development/tools/misc/whatstyle {
