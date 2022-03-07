a6b28fcad7994aebb1e9b5de2d58c79c4aa26d62
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-07 05:23:58 +0000

    fits-cloudctl: 0.10.10 -> 0.10.11

--- pkgs/tools/admin/fits-cloudctl/default.nix
+++ pkgs/tools/admin/fits-cloudctl/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "fits-cloudctl";
-  version = "0.10.10";
+  version = "0.10.11";
 
   src = fetchFromGitHub {
     owner = "fi-ts";
     repo = "cloudctl";
     rev = "v${version}";
-    sha256 = "sha256-US3Wvr6uBwsaIqjsbWeZtYucXVybMjKu8AuPYvykCvA=";
+    sha256 = "sha256-hGKnQk2OPpHsjbRh/xx3MidbUMio6tYn+oJB0t1a/yM=";
   };
 
   vendorSha256 = "sha256-f35Asf9l6ZfixpjMGzesTsxmANreilMxH2CULMH3b2o=";
