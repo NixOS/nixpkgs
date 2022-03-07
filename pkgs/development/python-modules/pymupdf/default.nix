9a35d5dc886edde0771571387e39a71030c216cc
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-07 05:41:25 +0000

    python310Packages.pymupdf: 1.19.5 -> 1.19.6

--- pkgs/development/python-modules/pymupdf/default.nix
+++ pkgs/development/python-modules/pymupdf/default.nix
@@ -13,12 +13,12 @@
 
 buildPythonPackage rec {
   pname = "pymupdf";
-  version = "1.19.5";
+  version = "1.19.6";
 
   src = fetchPypi {
     pname = "PyMuPDF";
     inherit version;
-    sha256 = "sha256-OTybnLJmmoaRoBew/bmuDs86Smbypc6jfQqqGikKVaU=";
+    sha256 = "sha256-7z0T4n8Vhdd29qJZfxE6q9KNNrZIuYOnKFCyHFOZqwg=";
   };
 
   postPatch = ''
