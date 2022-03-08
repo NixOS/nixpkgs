c5f465e4c58b957fc967640260e43b7046f66644
Jonathan Ringer <jonringer117@gmail.com>
2022-03-07 18:35:42 -0800

    python3Packages.lektor: fix src and build

--- pkgs/development/python-modules/lektor/default.nix
+++ pkgs/development/python-modules/lektor/default.nix
@@ -2,6 +2,7 @@
 , buildPythonPackage
 , fetchFromGitHub
 , click
+, filetype
 , watchdog
 , exifread
 , requests
@@ -17,6 +18,7 @@
 , pytest-mock
 , pytest-pylint
 , pytest-click
+, python-slugify
 , isPy27
 , functools32
 , setuptools
@@ -25,17 +27,18 @@
 buildPythonPackage rec {
   pname = "lektor";
   version = "3.3.2";
+  format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "lektor";
     repo = "lektor";
-    rev = version;
+    rev = "v${version}";
     sha256 = "sha256-PNHQ87aO+b1xseupIOsO7MXdr16s0gjoHGnZhPlKKRY=";
   };
 
   propagatedBuildInputs = [
-    click watchdog exifread requests mistune inifile Babel jinja2
-    flask pyopenssl ndg-httpsclient setuptools
+    click filetype watchdog exifread requests mistune inifile Babel jinja2
+    flask pyopenssl python-slugify ndg-httpsclient setuptools
   ] ++ lib.optionals isPy27 [ functools32 ];
 
   checkInputs = [
