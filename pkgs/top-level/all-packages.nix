2f80a58bdd2f724cb0a373febf47e81c2f2495ee
Francesco Gazzetta <fgaz@fgaz.me>
2022-03-08 18:29:32 +0100

    faircamp: init at unstable-2022-01-19

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25449,6 +25449,8 @@ with pkgs;
 
   f1viewer = callPackage ../applications/video/f1viewer {};
 
+  faircamp = callPackage ../applications/misc/faircamp { };
+
   fasttext = callPackage ../applications/science/machine-learning/fasttext { };
 
   fbmenugen = callPackage ../applications/misc/fbmenugen { };
