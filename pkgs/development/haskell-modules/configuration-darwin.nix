71f2c74efbad1ec0f5802356e0bc2119cb3121cc
Robert Hensing <robert@roberthensing.nl>
2022-03-08 11:55:46 +0100

    haskellPackages.cryptonite: Disable Argon2 on aarch64-darwin
    A workaround for packages that don't use this hash function.
    See https://github.com/haskell-crypto/cryptonite/issues/360

--- pkgs/development/haskell-modules/configuration-darwin.nix
+++ pkgs/development/haskell-modules/configuration-darwin.nix
@@ -289,4 +289,7 @@ self: super: ({
 
   # https://github.com/fpco/inline-c/issues/127
   inline-c-cpp = dontCheck super.inline-c-cpp;
+
+  # https://github.com/haskell-crypto/cryptonite/issues/360
+  cryptonite = appendPatch ./patches/cryptonite-remove-argon2.patch super.cryptonite;
 })
