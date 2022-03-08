c553c7b8fa6f6984328fb7a92722445e6b026123
Fabian Affolter <mail@fabian-affolter.ch>
2022-03-08 10:55:15 +0100

    arping: clarify license

--- pkgs/tools/networking/arping/default.nix
+++ pkgs/tools/networking/arping/default.nix
@@ -1,25 +1,36 @@
-{ lib, stdenv, fetchFromGitHub, autoreconfHook, libnet, libpcap }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, autoreconfHook
+, libnet
+, libpcap
+}:
 
 stdenv.mkDerivation rec {
-  version = "2.23";
   pname = "arping";
-
-  buildInputs = [ libnet libpcap ];
+  version = "2.23";
 
   src = fetchFromGitHub {
     owner = "ThomasHabets";
     repo = pname;
     rev = "${pname}-${version}";
-    sha256 = "sha256-Yn0EFb23VJvcVluQhwGHg9cdnZ8LKlBEds7cq8Irftc=";
+    hash = "sha256-Yn0EFb23VJvcVluQhwGHg9cdnZ8LKlBEds7cq8Irftc=";
   };
 
-  nativeBuildInputs = [ autoreconfHook ];
+  nativeBuildInputs = [
+    autoreconfHook
+  ];
+
+  buildInputs = [
+    libnet
+    libpcap
+  ];
 
   meta = with lib; {
     description = "Broadcasts a who-has ARP packet on the network and prints answers";
     homepage = "https://github.com/ThomasHabets/arping";
-    license = with licenses; [ gpl2 ];
-    maintainers = [ maintainers.michalrus ];
+    license = with licenses; [ gpl2Plus ];
+    maintainers = with maintainers; [ michalrus ];
     platforms = platforms.unix;
   };
 }
