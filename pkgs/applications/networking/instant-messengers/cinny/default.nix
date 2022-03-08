8ef51651b212e3e67dd2f2aff93289e0ccc2dd07
Ashish SHUKLA <ashish.is@lostca.se>
2022-03-08 19:59:28 +0530

    cinny: 1.7.0 -> 1.8.0

--- pkgs/applications/networking/instant-messengers/cinny/default.nix
+++ pkgs/applications/networking/instant-messengers/cinny/default.nix
@@ -4,11 +4,11 @@ let
   configOverrides = writeText "cinny-config-overrides.json" (builtins.toJSON conf);
 in stdenv.mkDerivation rec {
   pname = "cinny";
-  version = "1.7.0";
+  version = "1.8.0";
 
   src = fetchurl {
     url = "https://github.com/ajbura/cinny/releases/download/v${version}/cinny-v${version}.tar.gz";
-    sha256 = "0133dbzxy0n0i6bn2p3lx33kpabnf9kzs9mv4xws30hbns25q99k";
+    sha256 = "0pbapzl3pfx87ns4vp7088kkhl34c0ihbq90r3d0iz6sa16mcs79";
   };
 
   installPhase = ''
