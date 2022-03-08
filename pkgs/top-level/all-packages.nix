73d9e53ff3c1458c79d61b588c93d005b2f36735
linsui <36977733+linsui@users.noreply.github.com>
2022-03-08 12:56:51 +0000

    weylus: build from source and fix desktop shortcut (#161052)
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1054,7 +1054,9 @@ with pkgs;
 
   weidu = callPackage ../tools/games/weidu { };
 
-  weylus = callPackage ../applications/graphics/weylus { };
+  weylus = callPackage ../applications/graphics/weylus  {
+    inherit (darwin.apple_sdk.frameworks) ApplicationServices Carbon Cocoa VideoToolbox;
+  };
 
   gfshare = callPackage ../tools/security/gfshare { };
 
