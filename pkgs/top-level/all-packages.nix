e02b127611c9d340c5c0d7de711f17c86b11fd02
Emery Hemingway <ehmry@c3d2.de>
2022-03-09 08:27:52 -0600

    littlefs-fuse: init at 2.4.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7164,6 +7164,8 @@ with pkgs;
 
   lfs = callPackage ../tools/filesystems/lfs { };
 
+  littlefs-fuse = callPackage ../tools/filesystems/littlefs-fuse { };
+
   lksctp-tools = callPackage ../os-specific/linux/lksctp-tools { };
 
   lldpd = callPackage ../tools/networking/lldpd { };
