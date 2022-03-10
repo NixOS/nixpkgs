883b5931a6ee733568cc3f62824dc167041e9203
Vonfry <mail@vonfry.name>
2022-03-10 09:49:38 +0800

    fcitx5-hangul: 5.0.7 -> 5.0.8

--- pkgs/tools/inputmethods/fcitx5/fcitx5-hangul.nix
+++ pkgs/tools/inputmethods/fcitx5/fcitx5-hangul.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation rec {
   pname = "fcitx5-hangul";
-  version = "5.0.7";
+  version = "5.0.8";
 
   src = fetchFromGitHub {
     owner = "fcitx";
     repo = pname;
     rev = version;
-    sha256 = "sha256-vjKQ0h85gvlE+inHuzkIBLgluwA6c/XADtyMi6nQNc8=";
+    sha256 = "sha256-b8vxPzTmvmvWkO5H/yf+Kg9xPQ/53tR9jQsD11KjzJw=";
   };
 
   nativeBuildInputs = [
