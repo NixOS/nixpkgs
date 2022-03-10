2582c10f6fd8f0bc96295ed6c6e4afdb703c62d3
Fabian Affolter <mail@fabian-affolter.ch>
2022-03-10 23:28:21 +0100

    python3Packages.BTrees: disable on older Python releases

--- pkgs/development/python-modules/btrees/default.nix
+++ pkgs/development/python-modules/btrees/default.nix
@@ -6,15 +6,19 @@
 , transaction
 , zope_testrunner
 , python
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "BTrees";
   version = "4.10.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-1qsONBDQdNcVQkXW3GSTrobxtQvWCA0TEOuz7N6l3rY=";
+    hash = "sha256-1qsONBDQdNcVQkXW3GSTrobxtQvWCA0TEOuz7N6l3rY=";
   };
 
   propagatedBuildInputs = [
