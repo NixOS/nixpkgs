b03bcbd4989a9818654f59c4fb806dae50d83c8a
Bryan A. S <bryanasdev000@gmail.com>
2022-03-09 21:07:10 -0300

    kn: 1.2.0 -> 1.3.0
    Signed-off-by: Bryan A. S <bryanasdev000@gmail.com>

--- pkgs/applications/networking/cluster/kn/default.nix
+++ pkgs/applications/networking/cluster/kn/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "kn";
-  version = "1.2.0";
+  version = "1.3.0";
 
   src = fetchFromGitHub {
     owner = "knative";
     repo = "client";
     rev = "knative-v${version}";
-    sha256 = "sha256-yWzrMkkusRueHyWT4LwrflH4N7BNri2ycHIuAyvXceo=";
+    sha256 = "sha256-PxiYxDHcA95MinNpEcXul2cYrV/VB6gThhYCZANqAGY=";
   };
 
   vendorSha256 = null;
