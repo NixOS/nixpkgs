b420440aa9308a4a379f7047c2c462b40a1d49ab
Сухарик <65870+suhr@users.noreply.github.com>
2022-03-11 21:56:52 +0300

    ssb-patchbay: init at 8.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10132,6 +10132,8 @@ with pkgs;
 
   ssb-patchwork = callPackage ../applications/networking/ssb-patchwork { };
 
+  ssb-patchbay = callPackage ../applications/networking/ssb-patchbay { };
+
   ssdeep = callPackage ../tools/security/ssdeep { };
 
   ssh-ident = callPackage ../tools/networking/ssh-ident { };
