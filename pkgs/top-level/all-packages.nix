90fdf944bd61a2bf6530259f75a30c07b59a3d47
Jonas Heinrich <onny@project-insanity.org>
2022-03-11 18:18:30 +0100

    shopify-cli: init at 2.14.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9836,6 +9836,8 @@ with pkgs;
 
   shocco = callPackage ../tools/text/shocco { };
 
+  shopify-cli = callPackage ../development/web/shopify-cli { };
+
   shopify-themekit = callPackage ../development/web/shopify-themekit { };
 
   shorewall = callPackage ../tools/networking/shorewall { };
