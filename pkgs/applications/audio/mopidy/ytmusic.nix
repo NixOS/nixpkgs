1b9bb54e713a22bae98109a2460a151c1ef7630e
be7a <bela.stoyan@gmail.com>
2022-03-11 12:09:06 +0100

    mopidy-ytmusic: 0.3.2 -> 0.3.5

--- pkgs/applications/audio/mopidy/ytmusic.nix
+++ pkgs/applications/audio/mopidy/ytmusic.nix
@@ -2,12 +2,12 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "mopidy-ytmusic";
-  version = "0.3.2";
+  version = "0.3.5";
 
   src = python3Packages.fetchPypi {
     inherit version;
     pname = "Mopidy-YTMusic";
-    sha256 = "sha256-BZtW+qHsTnOMj+jdAFI8ZMwGxJc9lNosgPJZGbt4JgU=";
+    sha256 = "0pncyxfqxvznb9y4ksndbny1yf5mxh4089ak0yz86dp2qi5j99iv";
   };
 
   propagatedBuildInputs = [
@@ -18,7 +18,7 @@ python3Packages.buildPythonApplication rec {
 
   postPatch = ''
     substituteInPlace setup.py \
-      --replace 'ytmusicapi>=0.19.1,<0.20.0' 'ytmusicapi>=0.19.1'
+      --replace 'ytmusicapi>=0.20.0,<0.21.0' 'ytmusicapi>=0.20.0'
   '';
 
   doCheck = true;
