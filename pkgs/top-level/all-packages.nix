bebc236daf292c42380b29dc75ddfaeb086c86b6
Vincent Laporte <Vincent.Laporte@gmail.com>
2022-03-11 08:12:25 +0100

    reason: 3.7.0 → 3.8.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14080,7 +14080,7 @@ with pkgs;
 
   regina = callPackage ../development/interpreters/regina { };
 
-  inherit (ocaml-ng.ocamlPackages_4_12) reason;
+  inherit (ocamlPackages) reason;
 
   pixie = callPackage ../development/interpreters/pixie { };
   dust = callPackage ../development/interpreters/pixie/dust.nix { };
