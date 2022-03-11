bab25a7a3a12e5de1c0f9176a5827460733d6447
Mauricio Collares <mauricio@collares.org>
2022-03-11 11:40:27 -0300

    lean: 3.40.0 -> 3.41.0

--- pkgs/applications/science/logic/lean/default.nix
+++ pkgs/applications/science/logic/lean/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "lean";
-  version = "3.40.0";
+  version = "3.41.0";
 
   src = fetchFromGitHub {
     owner  = "leanprover-community";
@@ -11,8 +11,8 @@ stdenv.mkDerivation rec {
     # from. this is then used to check whether an olean file should be
     # rebuilt. don't use a tag as rev because this will get replaced into
     # src/githash.h.in in preConfigure.
-    rev    = "82216a493580a309a04b02c2b6eb5f82f0aef192";
-    sha256 = "0cdk124riz8g43n54yx4qyjn7n6qxj5zgn9r4znqs7iyb3djs286";
+    rev    = "154ac72f4ff674bc4486ac611f926a3d6b999f9f";
+    sha256 = "0mpxlfjq460x1vi3v6qzgjv74asg0qlhykd51pj347795x5b1hf1";
   };
 
   nativeBuildInputs = [ cmake ];
