998540aa8db8ac5fefa2106971a52dbf4c111b05
Sandro <sandro.jaeckel@gmail.com>
2022-03-12 23:39:02 +0100

    Update pkgs/applications/science/computer-architecture/qtrvsim/default.nix

--- pkgs/applications/science/computer-architecture/qtrvsim/default.nix
+++ pkgs/applications/science/computer-architecture/qtrvsim/default.nix
@@ -1,15 +1,20 @@
 { lib, stdenv, fetchFromGitHub, cmake, wrapQtAppsHook, qtbase }:
+
 stdenv.mkDerivation rec {
   pname = "QtRVSim";
   version = "0.9.1";
+
   src = fetchFromGitHub {
     owner = "cvut";
     repo = "qtrvsim";
     rev = "refs/tags/v${version}";
     sha256 = "AOksVS0drIBnK4RCxZw40yVxf4E8GjG9kU0rIZsY9gA=";
   };
+
   nativeBuildInputs = [ cmake wrapQtAppsHook ];
+
   buildInputs = [ qtbase ];
+
   meta = with lib; {
     description = "RISC-V CPU simulator for education purposes";
     longDescription = ''
