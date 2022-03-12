2a601248e7181aade89d542afaa42e3343b4bea5
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-12 15:04:05 +0000

    python310Packages.plugwise: 0.16.6 -> 0.16.7

--- pkgs/development/python-modules/plugwise/default.nix
+++ pkgs/development/python-modules/plugwise/default.nix
@@ -19,14 +19,14 @@
 
 buildPythonPackage rec {
   pname = "plugwise";
-  version = "0.16.6";
+  version = "0.16.7";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = "python-plugwise";
     rev = "v${version}";
-    sha256 = "sha256-hAYbYsLpiiJYdg9Rx5BjqNA9JTtKGu3DE0SpwOxlTWw=";
+    sha256 = "sha256-JKfSevbFmspxTVRSzJl7M78E64AtC8ipsbanWi7avTs=";
   };
 
   postPatch = ''
