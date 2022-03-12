8d2b4a00180a6a01a6e38e399e1577f223acc602
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-12 07:15:49 +0000

    python310Packages.python-juicenet: 1.0.2 -> 1.1.0

--- pkgs/development/python-modules/python-juicenet/default.nix
+++ pkgs/development/python-modules/python-juicenet/default.nix
@@ -6,13 +6,13 @@
 
 buildPythonPackage rec {
   pname = "python-juicenet";
-  version = "1.0.2";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "jesserockz";
     repo = "python-juicenet";
     rev = "v${version}";
-    sha256 = "04547pj51ds31yhyc7ng47v9giz16h2s3wgb6szc8ivhb5rclqz2";
+    sha256 = "sha256-5RKnVwOfEHzFZCiC8OUpS8exKrENK+I3Ok45HlKEvtU=";
   };
 
   propagatedBuildInputs = [
