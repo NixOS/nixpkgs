1d589b1f3c845d23d9ea366e1991aff5a56fbdd3
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-12 02:58:14 +0000

    dictdDBs.wiktionary: 20210920 -> 20220301

--- pkgs/servers/dict/wiktionary/default.nix
+++ pkgs/servers/dict/wiktionary/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "dict-db-wiktionary";
-  version = "20210920";
+  version = "20220301";
 
   src = fetchurl {
     url = "https://dumps.wikimedia.org/enwiktionary/${version}/enwiktionary-${version}-pages-articles.xml.bz2";
-    sha256 = "UeufbpSpRL+JrU3SkhxzWJncEsrM1es88grRmFwGABk=";
+    sha256 = "Gobilm9Rlb7qtZU+hlsYOl1/BAjj/MtNp5z2GQx8NN8=";
   };
 
   # script in nixpkgs does not support python2
