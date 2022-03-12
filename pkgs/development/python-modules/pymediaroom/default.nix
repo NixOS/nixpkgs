fd0f34dbe6e08ed08c5b387a64743a949cc7d483
Fabian Affolter <mail@fabian-affolter.ch>
2022-03-12 12:52:52 +0100

    python3Packages.pymediaroom: 0.6.4.1 -> 0.6.5

--- pkgs/development/python-modules/pymediaroom/default.nix
+++ pkgs/development/python-modules/pymediaroom/default.nix
@@ -8,14 +8,16 @@
 
 buildPythonPackage rec {
   pname = "pymediaroom";
-  version = "0.6.4.1";
-  disabled = pythonOlder "3.5";
+  version = "0.6.5";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "dgomes";
     repo = pname;
     rev = version;
-    sha256 = "1klf2dxd8rlq3n4b9m03lzwcsasn9vi6m3hzrjqhqnprhrnp0xmy";
+    sha256 = "sha256-iRxPCunbgU4kbTLBKNFS6IlTP/6jGoQfWx0TDqTXymE=";
   };
 
   propagatedBuildInputs = [
@@ -25,7 +27,10 @@ buildPythonPackage rec {
 
   # Project has no tests
   doCheck = false;
-  pythonImportsCheck = [ "pymediaroom" ];
+
+  pythonImportsCheck = [
+    "pymediaroom"
+  ];
 
   meta = with lib; {
     description = "Python Remote Control for Mediaroom STB";
