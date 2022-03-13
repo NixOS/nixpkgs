fbc9c00e4f5e3fe07b90c95346c8c236d34ca5ed
Aaron Jheng <wentworth@outlook.com>
2022-03-13 14:17:42 +0000

    clash: 1.6.0 -> 1.10.0

--- pkgs/tools/networking/clash/default.nix
+++ pkgs/tools/networking/clash/default.nix
@@ -1,23 +1,35 @@
-{ lib, fetchFromGitHub, buildGoModule }:
+{ lib, fetchFromGitHub, buildGoModule, testVersion, clash }:
 
 buildGoModule rec {
   pname = "clash";
-  version = "1.6.0";
+  version = "1.10.0";
 
   src = fetchFromGitHub {
     owner = "Dreamacro";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-XG/nci8Sj0vfa/SFPpJwl1Zmt/23LfKxocejplZtS0E=";
+    sha256 = "sha256-cAJjW+NzG48HcDRx12LLdj8VYyIroL+GWqrUrpHOOIk=";
   };
 
-  vendorSha256 = "sha256-WR1CpjEMHRkpd0/iqrOm0oVXvyQO+r6GyeP0L0zx8aA=";
+  vendorSha256 = "sha256-hE2MgjaVme+4vG7+rmJXfjycd3N2R6cA5iSUUTFcQXE=";
 
-  doCheck = false;
+  postPatch = ''
+    # Do not build testing suit
+    rm -rf ./test
+  '';
+
+  CGO_ENABLED = 0;
 
   ldflags = [
+    "-s"
+    "-w"
     "-X github.com/Dreamacro/clash/constant.Version=${version}"
   ];
+
+  passthru.tests.version = testVersion {
+    package = clash;
+    command = "clash -v";
+  };
 
   meta = with lib; {
     description = "A rule-based tunnel in Go";
