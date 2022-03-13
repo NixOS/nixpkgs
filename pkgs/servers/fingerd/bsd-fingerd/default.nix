f3e1e37041b33d658c1d606658c7b74caaad0333
AndersonTorres <torres.anderson.85@protonmail.com>
2022-03-13 17:27:02 -0300

    bsd-fingerd: ibiblioPubLinux mirror

--- pkgs/servers/fingerd/bsd-fingerd/default.nix
+++ pkgs/servers/fingerd/bsd-fingerd/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec {
   version = "0.17";
 
   src = fetchurl {
-    url = "mirror://metalab/system/network/finger/bsd-finger-${version}.tar.gz";
+    url = "mirror://ibiblioPubLinux/system/network/finger/bsd-finger-${version}.tar.gz";
     sha256 = "1yhkiv0in588il7f84k2xiy78g9lv12ll0y7047gazhiimk5v244";
   };
 
