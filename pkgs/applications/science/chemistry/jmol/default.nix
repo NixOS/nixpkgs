6d61669d69c9e4724eff3d8d32fef40d2399cff6
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-13 16:20:57 +0000

    jmol: 14.32.30 -> 14.32.33

--- pkgs/applications/science/chemistry/jmol/default.nix
+++ pkgs/applications/science/chemistry/jmol/default.nix
@@ -25,14 +25,14 @@ let
   };
 in
 stdenv.mkDerivation rec {
-  version = "14.32.30";
+  version = "14.32.33";
   pname = "jmol";
 
   src = let
     baseVersion = "${lib.versions.major version}.${lib.versions.minor version}";
   in fetchurl {
     url = "mirror://sourceforge/jmol/Jmol/Version%20${baseVersion}/Jmol%20${version}/Jmol-${version}-binary.tar.gz";
-    sha256 = "sha256-VpOoduUA0iD+nI83GSQYQDHoK2Snog0NHkHWHfpLqFM=";
+    sha256 = "sha256-aImV5zgXjsXdRJLrYIomDymXHRhxuOwGYAwpMIr7wak=";
   };
 
   patchPhase = ''
