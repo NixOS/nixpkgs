4339b99d6b8cfd8b9277a8fe614ae6f0661d5d53
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-13 11:30:45 +0000

    python310Packages.symengine: 0.9.0 -> 0.9.2

--- pkgs/development/python-modules/symengine/default.nix
+++ pkgs/development/python-modules/symengine/default.nix
@@ -11,13 +11,13 @@
 
 buildPythonPackage rec {
   pname = "symengine";
-  version = "0.9.0";
+  version = "0.9.2";
 
   src = fetchFromGitHub {
     owner = "symengine";
     repo = "symengine.py";
     rev = "v${version}";
-    sha256 = "sha256-kz4M4ghR9Mi8Ig5K+pZC4zHt8XxoP3vU4ATImejqbgg=";
+    sha256 = "sha256-ZHplYEG97foy/unOdSokFFkDl4LK5TI4kypHSLpcCM4=";
   };
 
   postConfigure = ''
