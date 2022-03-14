a8d171fee556609b0a0ae341de1780426a32b5de
Victor Nawothnig <dminuoso+git@icloud.com>
2022-03-14 12:25:04 +0100

    nixos/nginx: Always symlink nginx configuration to /etc/nginx/nginx.conf

--- nixos/modules/services/web-servers/nginx/default.nix
+++ nixos/modules/services/web-servers/nginx/default.nix
@@ -929,9 +929,7 @@ in
       };
     };
 
-    environment.etc."nginx/nginx.conf" = mkIf cfg.enableReload {
-      source = configFile;
-    };
+    environment.etc."nginx/nginx.conf".source = configFile;
 
     # This service waits for all certificates to be available
     # before reloading nginx configuration.
