e0cce3d7bffa8e6b5bc6f931b341d66fd5548c48
AndersonTorres <torres.anderson.85@protonmail.com>
2022-03-14 15:47:56 -0300

    goobook: export to all-packages.nix

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27292,6 +27292,8 @@ with pkgs;
 
   luddite = with python3Packages; toPythonApplication luddite;
 
+  goobook = with python3Packages; toPythonApplication goobook;
+
   lumail = callPackage ../applications/networking/mailreaders/lumail {
     lua = lua5_1;
   };
