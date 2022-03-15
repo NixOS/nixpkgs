0e0e43c7f8c3957e81d4642b1569e2543f97b816
Franz Pletz <fpletz@fnordicwalking.de>
2022-03-15 18:38:09 +0100

    libressl: 3.4.2 -> 3.4.3
    Fixes CVE-2022-0778.
    
    (cherry picked from commit c201e773c62c31afb20811b3ec28bb918f99dcee)

--- pkgs/development/libraries/libressl/default.nix
+++ pkgs/development/libraries/libressl/default.nix
@@ -94,7 +94,7 @@ in {
     ];
   };
   libressl_3_4 = generic {
-    version = "3.4.2";
-    sha256 = "sha256-y4LKfVRzNpFzUvvSPbL8SDxsRNNRV7MngCFOx0GXs84=";
+    version = "3.4.3";
+    sha256 = "sha256-/4i//jVIGLPM9UXjyv5FTFAxx6dyFwdPUzJx1jw38I0=";
   };
 }
