be2b7ec452e64ed2db7acfcf52b768210139276d
Robert Scott <code@humanleg.org.uk>
2022-03-15 22:20:09 +0000

    separate-debug-info setup hook: strip debug symbols from static libs
    this should make it less risky to enable separateDebugInfo as
    otherwise it may result in massive static libraries

--- pkgs/build-support/setup-hooks/separate-debug-info.sh
+++ pkgs/build-support/setup-hooks/separate-debug-info.sh
@@ -16,28 +16,40 @@ _separateDebugInfo() {
     # Find executables and dynamic libraries.
     local i
     while IFS= read -r -d $'\0' i; do
-        if ! isELF "$i"; then continue; fi
+        if isELF "$i"; then
 
-        # Extract the Build ID. FIXME: there's probably a cleaner way.
-        local id="$($READELF -n "$i" | sed 's/.*Build ID: \([0-9a-f]*\).*/\1/; t; d')"
-        if [ "${#id}" != 40 ]; then
-            echo "could not find build ID of $i, skipping" >&2
-            continue
+            # Extract the Build ID. FIXME: there's probably a cleaner way.
+            local id="$($READELF -n "$i" | sed 's/.*Build ID: \([0-9a-f]*\).*/\1/; t; d')"
+            if [ "${#id}" != 40 ]; then
+                echo "could not find build ID of $i, skipping" >&2
+                continue
+            fi
+
+            # Extract the debug info.
+            header "separating debug info from $i (build ID $id)"
+            mkdir -p "$dst/${id:0:2}"
+
+            # This may fail, e.g. if the binary is for a different
+            # architecture than we're building for.  (This happens with
+            # firmware blobs in QEMU.)
+            (
+                $OBJCOPY --only-keep-debug "$i" "$dst/${id:0:2}/${id:2}.debug"
+                $STRIP --strip-debug "$i"
+
+                # Also a create a symlink <original-name>.debug.
+                ln -sfn ".build-id/${id:0:2}/${id:2}.debug" "$dst/../$(basename "$i")"
+            ) || rmdir -p "$dst/${id:0:2}"
+
+        elif isAR "$i" && [ "${i##*.}" = "a" ]; then
+            # possibly a static library - build will likely have
+            # added debugging symbols to any static libraries produced
+            # too, making them huge. separate debug outputs have limited
+            # value for static libraries, so attempt to strip any
+            # found. a user can just use a `dontStrip` build if they
+            # need a static library with debug info.
+
+            header "stripping $i"
+            $STRIP --strip-debug -D "$i" || true
         fi
-
-        # Extract the debug info.
-        header "separating debug info from $i (build ID $id)"
-        mkdir -p "$dst/${id:0:2}"
-
-        # This may fail, e.g. if the binary is for a different
-        # architecture than we're building for.  (This happens with
-        # firmware blobs in QEMU.)
-        (
-            $OBJCOPY --only-keep-debug "$i" "$dst/${id:0:2}/${id:2}.debug"
-            $STRIP --strip-debug "$i"
-
-            # Also a create a symlink <original-name>.debug.
-            ln -sfn ".build-id/${id:0:2}/${id:2}.debug" "$dst/../$(basename "$i")"
-        ) || rmdir -p "$dst/${id:0:2}"
     done < <(find "$prefix" -type f -print0)
 }
