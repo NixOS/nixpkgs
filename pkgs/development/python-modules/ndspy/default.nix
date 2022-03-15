b3559b7817d057ee04dfdfe8dcf8da2d892bc6ba
Konrad Borowski <konrad@borowski.pw>
2022-03-15 21:50:39 +0100

    python3Packages.ndspy: 3.0.0 -> 4.0.0

--- pkgs/development/python-modules/ndspy/default.nix
+++ pkgs/development/python-modules/ndspy/default.nix
@@ -1,18 +1,19 @@
-{ lib, buildPythonPackage, fetchPypi, crcmod }:
+{ lib, buildPythonPackage, fetchFromGitHub, crcmod, pytestCheckHook }:
 
 buildPythonPackage rec {
   pname = "ndspy";
-  version = "3.0.0";
+  version = "4.0.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "1s0i81gspas22bjwk9vhy3x5sw1svyybk7c2j1ixc77drr9ym20a";
+  src = fetchFromGitHub {
+    owner = "RoadrunnerWMC";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "0x3sp10had1mq192m7kgjivvs8kpjagxjgj9d4z95dfjhzzbjh70";
   };
 
   propagatedBuildInputs = [ crcmod ];
-
-  doCheck = false; # there are no tests
-  pythonImportsCheck = [ "ndspy" ];
+  checkInputs = [ pytestCheckHook ];
+  preCheck = "cd tests";
 
   meta = with lib; {
     homepage = "https://github.com/RoadrunnerWMC/ndspy";
