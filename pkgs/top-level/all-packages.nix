b39792d141429e203edcd737307e9cdeb56a7419
Ben Siraphob <bensiraphob@gmail.com>
2022-03-14 22:07:51 -0500

    nuXmv: init at 2.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32463,6 +32463,8 @@ with pkgs;
 
   monosat = callPackage ../applications/science/logic/monosat {};
 
+  nuXmv = callPackage ../applications/science/logic/nuXmv {};
+
   opensmt = callPackage ../applications/science/logic/opensmt { };
 
   ott = callPackage ../applications/science/logic/ott { };
