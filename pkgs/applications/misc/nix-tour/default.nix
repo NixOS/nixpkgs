01dd7667016871c639bd9022b1d86df715c6ea7f
yuu <yuuyin@protonmail.com>
2022-03-15 14:04:15 -0300

    nix-tour: Add XDG desktop entry
    Co-authored-by: Sandro Jäckel <sandro.jaeckel@gmail.com>

--- pkgs/applications/misc/nix-tour/default.nix
+++ pkgs/applications/misc/nix-tour/default.nix
@@ -4,6 +4,8 @@
 , electron
 , runtimeShell
 , makeWrapper
+, copyDesktopItems
+, makeDesktopItem
 }:
 
 stdenv.mkDerivation rec {
@@ -17,7 +19,7 @@ stdenv.mkDerivation rec {
     sha256 = "09b1vxli4zv1nhqnj6c0vrrl51gaira94i8l7ww96fixqxjgdwvb";
   };
 
-  nativeBuildInputs = [ makeWrapper ];
+  nativeBuildInputs = [ makeWrapper copyDesktopItems ];
   buildInputs = [ electron ];
 
   installPhase = ''
@@ -26,6 +28,18 @@ stdenv.mkDerivation rec {
     makeWrapper ${electron}/bin/electron $out/bin/nix-tour \
       --add-flags $out/share/nix-tour/electron-main.js
   '';
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = pname;
+      desktopName = "Tour of Nix";
+      genericName = "Tour of Nix";
+      comment =
+        "Interactive programming guide dedicated to the nix programming language";
+      categories = [ "Development" "Documentation" ];
+      exec = "nix-tour";
+    })
+  ];
 
   meta = with lib; {
     description = "'the tour of nix' from nixcloud.io/tour as offline version";
