3c11b2df706c4b8169bb1172035c48060a9188ba
Kylie McClain <kylie@somas.is>
2022-03-15 03:29:56 -0400

    maintainers: add somasis

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11999,6 +11999,12 @@
     githubId = 26806;
     name = "Scott Olson";
   };
+  somasis = {
+    email = "kylie@somas.is";
+    github = "somasis";
+    githubId = 264788;
+    name = "Kylie McClain";
+  };
   SomeoneSerge = {
     email = "sergei.kozlukov@aalto.fi";
     matrix = "@ss:someonex.net";
