987dd131ffda5e86a5ca2e31924bfef125aec58b
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-15 16:37:58 +0100

    openssl_1_1: 1.1.1m -> 1.1.1n
    https://github.com/openssl/openssl/blob/OpenSSL_1_1_1n/CHANGES#L10
    
    Fixes: CVE-2022-0778
    (cherry picked from commit 72bb369245a84beaa5596f8ed551be470a20d6b2)

--- pkgs/development/libraries/openssl/default.nix
+++ pkgs/development/libraries/openssl/default.nix
@@ -190,8 +190,8 @@ in {
   };
 
   openssl_1_1 = common rec {
-    version = "1.1.1m";
-    sha256 = "sha256-+JGZvosjykX8fLnx2NPuZzEjGChq0DD1MWrKZGLbbJY=";
+    version = "1.1.1n";
+    sha256 = "sha256-QNzrUaT2pSdb3g5r8g70uRv8Mu1XwFUuLo4VRjNysXo=";
     patches = [
       ./1.1/nix-ssl-cert-file.patch
 
