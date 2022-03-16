a2e6d1799687c7a337ff6af31387cb73f24197ad
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-16 07:25:54 +0000

    python310Packages.python-box: 5.4.1 -> 6.0.0

--- pkgs/development/python-modules/python-box/default.nix
+++ pkgs/development/python-modules/python-box/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "python-box";
-  version = "5.4.1";
+  version = "6.0.0";
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "cdgriffith";
     repo = "Box";
     rev = version;
-    sha256 = "sha256-SkteajcWG7rBFMm6Xp6QCfkZfwthRituGL/RtICbtYk=";
+    sha256 = "sha256-YOYcI+OAuTumNtTylUc6dSY9shOE6eTr8M3rVbcy5hs=";
   };
 
   propagatedBuildInputs = [
