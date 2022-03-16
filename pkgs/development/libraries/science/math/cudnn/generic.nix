2000db2e332b552c3a7237b8fafb60be2c6687d2
Samuel Ainsworth <skainsworth@gmail.com>
2022-03-16 00:50:29 +0000

    cudnn: add samuela as a maintainer

--- pkgs/development/libraries/science/math/cudnn/generic.nix
+++ pkgs/development/libraries/science/math/cudnn/generic.nix
@@ -91,6 +91,6 @@ stdenv.mkDerivation {
     homepage = "https://developer.nvidia.com/cudnn";
     license = licenses.unfree;
     platforms = [ "x86_64-linux" ];
-    maintainers = with maintainers; [ mdaiter ];
+    maintainers = with maintainers; [ mdaiter samuela ];
   };
 }
