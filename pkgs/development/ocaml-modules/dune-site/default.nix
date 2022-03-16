45741500ed244c4643cdd8b211b41c3479be5899
Vincent Laporte <Vincent.Laporte@gmail.com>
2022-03-16 17:30:55 +0100

    ocamlPackages.dune-private-libs: 2.9.3 → 3.0.3
    ocamlPackages.dune-site: 2.9.3 → 3.0.3
    ocamlPackages.dune-glob: 2.9.3 → 3.0.3
    ocamlPackages.dune-action-plugin: 2.9.3 → 3.0.3

--- pkgs/development/ocaml-modules/dune-site/default.nix
+++ pkgs/development/ocaml-modules/dune-site/default.nix
@@ -1,10 +1,10 @@
-{ lib, buildDunePackage, dune_2, dune-private-libs }:
+{ lib, buildDunePackage, dune_3, dune-private-libs }:
 
 buildDunePackage rec {
   pname = "dune-site";
-  inherit (dune_2) src version patches;
+  inherit (dune_3) src version;
 
-  useDune2 = true;
+  duneVersion = "3";
 
   dontAddPrefix = true;
 
@@ -12,7 +12,7 @@ buildDunePackage rec {
 
   meta = with lib; {
     description = "A library for embedding location information inside executable and libraries";
-    inherit (dune_2.meta) homepage;
+    inherit (dune_3.meta) homepage;
     maintainers = with lib.maintainers; [ ];
     license = licenses.mit;
   };
