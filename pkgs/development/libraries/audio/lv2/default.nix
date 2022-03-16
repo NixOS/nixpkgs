c586cccd7cb90b479ecb7bc10585968bc3bda5bb
Jan Tojnar <jtojnar@gmail.com>
2022-03-16 01:27:11 +0100

    lv2: format the expression

--- pkgs/development/libraries/audio/lv2/default.nix
+++ pkgs/development/libraries/audio/lv2/default.nix
@@ -1,4 +1,12 @@
-{ lib, stdenv, fetchurl, gtk2, libsndfile, pkg-config, python3, wafHook }:
+{ stdenv
+, lib
+, fetchurl
+, gtk2
+, libsndfile
+, pkg-config
+, python3
+, wafHook
+}:
 
 stdenv.mkDerivation rec {
   pname = "lv2";
@@ -9,16 +17,26 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-TokfvHRMBYVb6136gugisUkX3Wbpj4K4Iw29HHqy4F4=";
   };
 
-  nativeBuildInputs = [ pkg-config wafHook ];
-  buildInputs = [ gtk2 libsndfile python3 ];
+  nativeBuildInputs = [
+    pkg-config
+    wafHook
+  ];
 
-  wafConfigureFlags = lib.optionals stdenv.isDarwin [ "--lv2dir=${placeholder "out"}/lib/lv2" ];
+  buildInputs = [
+    gtk2
+    libsndfile
+    python3
+  ];
+
+  wafConfigureFlags = lib.optionals stdenv.isDarwin [
+    "--lv2dir=${placeholder "out"}/lib/lv2"
+  ];
 
   meta = with lib; {
     homepage = "https://lv2plug.in";
     description = "A plugin standard for audio systems";
     license = licenses.mit;
-    maintainers = [ maintainers.goibhniu ];
+    maintainers = with maintainers; [ goibhniu ];
     platforms = platforms.unix;
   };
 }
