dd2c1fc3f06929dc64a592d11a6d64551cc061ba
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-16 13:36:18 +0100

    esphome: 2022.2.6 -> 2022.3.0
    https://github.com/esphome/esphome/releases/tag/2022.3.0

--- pkgs/tools/misc/esphome/default.nix
+++ pkgs/tools/misc/esphome/default.nix
@@ -15,14 +15,14 @@ let
 in
 with python.pkgs; buildPythonApplication rec {
   pname = "esphome";
-  version = "2022.2.6";
+  version = "2022.3.0";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = version;
-    sha256 = "sha256-AoSFtEg7GpUBdIrFCYlOMBRc2nBBtiFEkxoZnKC/xyQ=";
+    sha256 = "sha256-i+d1gPhB9LKzsHOVSHa22ZYhLxc/pY/a9XLlctysJ9Q=";
   };
 
   postPatch = ''
