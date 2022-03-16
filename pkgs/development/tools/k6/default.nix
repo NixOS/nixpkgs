021c5a6f3bf8d2620d790b8a766111d2a2c01977
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-16 07:17:09 +0000

    k6: 0.36.0 -> 0.37.0

--- pkgs/development/tools/k6/default.nix
+++ pkgs/development/tools/k6/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "k6";
-  version = "0.36.0";
+  version = "0.37.0";
 
   src = fetchFromGitHub {
     owner = "grafana";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-yWEh0sPMGe6mNcLKhbmJEUCHzZKFGMcTRNQrHgiQ+BQ=";
+    sha256 = "sha256-5pxOg+pwa2VrEWinDadx2ZFYXiQgochbU4bCkJEezQw=";
   };
 
   subPackages = [ "./" ];
