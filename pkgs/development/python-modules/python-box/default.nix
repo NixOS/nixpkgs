15e19f12a999f92132f8d1456a773c3490ea200f
Fabian Affolter <mail@fabian-affolter.ch>
2022-03-16 10:21:23 +0100

    python3Packages.python-box: add format

--- pkgs/development/python-modules/python-box/default.nix
+++ pkgs/development/python-modules/python-box/default.nix
@@ -12,13 +12,15 @@
 buildPythonPackage rec {
   pname = "python-box";
   version = "6.0.0";
+  format = "setuptools";
+
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "cdgriffith";
     repo = "Box";
     rev = version;
-    sha256 = "sha256-YOYcI+OAuTumNtTylUc6dSY9shOE6eTr8M3rVbcy5hs=";
+    hash = "sha256-YOYcI+OAuTumNtTylUc6dSY9shOE6eTr8M3rVbcy5hs=";
   };
 
   propagatedBuildInputs = [
@@ -32,7 +34,9 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
-  pythonImportsCheck = [ "box" ];
+  pythonImportsCheck = [
+    "box"
+  ];
 
   meta = with lib; {
     description = "Python dictionaries with advanced dot notation access";
