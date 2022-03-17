8d1664a7a8225eccb3d860b0819f90d2812d536e
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-17 11:05:33 +0000

    hostctl: 1.1.1 -> 1.1.2

--- pkgs/tools/system/hostctl/default.nix
+++ pkgs/tools/system/hostctl/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "hostctl";
-  version = "1.1.1";
+  version = "1.1.2";
 
   src = fetchFromGitHub {
     owner = "guumaster";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-VjFjGvIoymGVVRiZUk/qoq/PTYoklp+Jz89zndX0e5A=";
+    sha256 = "sha256-rvUm31WRSLusM9VGsIHKGTH6Vs8LWPtzPDs3azA710w=";
   };
 
   vendorSha256 = "sha256-rGDWrivIdl5FTu/kNR8nAfE2+1hE4cm3uDg7oBobE9M=";
