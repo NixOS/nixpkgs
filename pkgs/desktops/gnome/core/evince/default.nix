9bd780f8d54f9a1ee43c428bc940bdb8fe881b9f
Bobby Rong <rjl931189261@126.com>
2022-03-18 19:50:07 +0800

    pantheon.evince: drop
    The vanilla evince already support dark style
    preference in Pantheon, so no need to keep the patch.

--- pkgs/desktops/gnome/core/evince/default.nix
+++ pkgs/desktops/gnome/core/evince/default.nix
@@ -1,7 +1,6 @@
 { lib
 , stdenv
 , fetchurl
-, fetchpatch
 , meson
 , ninja
 , pkg-config
@@ -32,7 +31,6 @@
 , gsettings-desktop-schemas
 , gnome-desktop
 , dbus
-, pantheon
 , python3
 , texlive
 , gst_all_1
@@ -40,7 +38,6 @@
 , supportMultimedia ? true # PDF multimedia
 , libgxps
 , supportXPS ? true # Open XML Paper Specification via libgxps
-, withPantheon ? false
 , withLibsecret ? true
 }:
 
@@ -54,16 +51,6 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/evince/${lib.versions.major version}/${pname}-${version}.tar.xz";
     sha256 = "rGD/FKr1bWQ5lcO0um9XJcyc4k4nkIc7XIfttKIALUI=";
   };
-
-  patches = lib.optionals withPantheon [
-    # Make this respect dark mode settings from Pantheon
-    # https://github.com/elementary/evince/pull/21
-    # https://github.com/elementary/evince/pull/31
-    (fetchpatch {
-      url = "https://raw.githubusercontent.com/elementary/evince/c8364019ee2c2dffd2a1bccf79b8f4e526aa22af/dark-style.patch";
-      sha256 = "sha256-nKELRXnM6gMRTGmWdO1Qqlo9ciy+4HOK5z2CYOoi2Lo=";
-    })
-  ];
 
   postPatch = ''
     chmod +x meson_post_install.py
@@ -115,9 +102,7 @@ stdenv.mkDerivation rec {
     gst-plugins-bad
     gst-plugins-ugly
     gst-libav
-  ]) ++ lib.optionals withPantheon [
-    pantheon.granite
-  ];
+  ]);
 
   mesonFlags = [
     "-Dnautilus=false"
