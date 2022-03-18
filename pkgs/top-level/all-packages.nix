9983028488e8f893c21676c6f73946dd6aba80bd
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-03-18 13:40:01 -0500

    disper: remove
    Motivated by #148779 and an unmaintained upstream.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4857,8 +4857,6 @@ with pkgs;
 
   dirvish  = callPackage ../tools/backup/dirvish { };
 
-  disper = callPackage ../tools/misc/disper { };
-
   dleyna-connector-dbus = callPackage ../development/libraries/dleyna-connector-dbus { };
 
   dleyna-core = callPackage ../development/libraries/dleyna-core { };
