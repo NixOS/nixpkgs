6e664858190ee8aff007f46dba9df9985e33143a
Bobby Rong <rjl931189261@126.com>
2022-03-18 20:19:55 +0800

    pantheon.gnome-bluetooth-contract: mark as broken
    Sendto device selection is removed in gnome-bluetooth 42.
    I decide it is not worth to maintain a legacy gnome-bluetooth
    package for the contract.
    This should also be broken in elementary OS 7.

--- nixos/modules/services/x11/desktop-managers/pantheon.nix
+++ nixos/modules/services/x11/desktop-managers/pantheon.nix
@@ -302,7 +302,6 @@ in
       environment.systemPackages = with pkgs.pantheon; [
         contractor
         file-roller-contract
-        gnome-bluetooth-contract
       ];
 
       environment.pathsToLink = [
