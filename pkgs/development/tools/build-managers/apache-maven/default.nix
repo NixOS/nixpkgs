84bbe973cc066b4007af8e65248f37db06b7e52a
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-18 08:37:56 +0000

    maven: 3.8.4 -> 3.8.5

--- pkgs/development/tools/build-managers/apache-maven/default.nix
+++ pkgs/development/tools/build-managers/apache-maven/default.nix
@@ -4,13 +4,13 @@ assert jdk != null;
 
 stdenv.mkDerivation rec {
   pname = "apache-maven";
-  version = "3.8.4";
+  version = "3.8.5";
 
   builder = ./builder.sh;
 
   src = fetchurl {
     url = "mirror://apache/maven/maven-3/${version}/binaries/${pname}-${version}-bin.tar.gz";
-    sha256 = "sha256-LNycUZQnuyD9wlvvWpBjt5Dkq9kw57FLTp9IY9b58Tw=";
+    sha256 = "sha256-iOMHAPMqP2Dg0o0PEqNSXSm3wgxy0TAVPfW11tiQxnM=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
