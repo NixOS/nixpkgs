1e8f2716bcd3578f40730a365e7b6571c39401ae
Jan Tojnar <jtojnar@gmail.com>
2022-03-18 23:22:17 +0000

    gnome-builder: 42.rc1 → 42.0
    https://gitlab.gnome.org/GNOME/gnome-builder/-/compare/42.rc1...42.0

--- pkgs/applications/editors/gnome-builder/default.nix
+++ pkgs/applications/editors/gnome-builder/default.nix
@@ -5,6 +5,7 @@
 , appstream-glib
 , desktop-file-utils
 , fetchurl
+, fetchpatch
 , flatpak
 , gnome
 , libgit2-glib
@@ -40,14 +41,22 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-builder";
-  version = "42.rc1";
+  version = "42.0";
 
   outputs = [ "out" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "s0rehJp+0lOpf+I6ObFpqgnatnQXIawWOORa/IA+D8M=";
+    sha256 = "Uu/SltaLL/GCNBwEgdz9cGVMQIvbZ5/Ot225cDwiQo8=";
   };
+
+  patches = [
+    # Fix appstream validation
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/gnome-builder/-/commit/d7151679e0c925d27216256dc32fe67fb298d059.patch";
+      sha256 = "vdNJawkqSBaFGRZvxzvjOryQpBL4jcN7tr1t3ihD7LA=";
+    })
+  ];
 
   nativeBuildInputs = [
     appstream-glib
