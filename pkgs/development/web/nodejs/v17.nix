4265aaa6a0e40fabc64d0444f53930c5340da623
Mario Rodas <marsam@users.noreply.github.com>
2022-03-19 04:20:00 +0000

    nodejs-17_x: 17.5.0 -> 17.7.2
    - https://github.com/nodejs/node/releases/tag/v17.6.0
    - https://github.com/nodejs/node/releases/tag/v17.7.0
    - https://github.com/nodejs/node/releases/tag/v17.7.1
    - https://github.com/nodejs/node/releases/tag/v17.7.2

--- pkgs/development/web/nodejs/v17.nix
+++ pkgs/development/web/nodejs/v17.nix
@@ -1,4 +1,4 @@
-{ callPackage, fetchpatch, python3, enableNpm ? true }:
+{ callPackage, python3, enableNpm ? true }:
 
 let
   buildNodejs = callPackage ./nodejs.nix {
@@ -7,15 +7,9 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "17.5.0";
-  sha256 = "sha256-myTmgwV2xX7ja6SDM974vldSMph7Tak5Vot7ifdzzcM=";
+  version = "17.7.2";
+  sha256 = "sha256-OuXnTgsWIoz37faIU1mp0uAZrIQ5BsXgGUjXRvq6Sq8=";
   patches = [
     ./disable-darwin-v8-system-instrumentation.patch
-    # Fixes node incorrectly building vendored OpenSSL when we want system OpenSSL.
-    # https://github.com/nodejs/node/pull/40965
-    (fetchpatch {
-      url = "https://github.com/nodejs/node/commit/65119a89586b94b0dd46b45f6d315c9d9f4c9261.patch";
-      sha256 = "sha256-dihKYEdK68sQIsnfTRambJ2oZr0htROVbNZlFzSAL+I=";
-    })
   ];
 }
