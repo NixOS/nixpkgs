161b19e07ee95943bd7b5e7da4c9524b75f80cf3
Fabian Affolter <mail@fabian-affolter.ch>
2022-03-19 10:50:04 +0100

    python3Packages.flake8-blind-except: add pythonImportsCheck

--- pkgs/development/python-modules/flake8-blind-except/default.nix
+++ pkgs/development/python-modules/flake8-blind-except/default.nix
@@ -1,16 +1,32 @@
-{ lib, fetchPypi, buildPythonPackage }:
+{ lib
+, fetchPypi
+, buildPythonPackage
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "flake8-blind-except";
   version = "0.2.1";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
+
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-8lpXWp3LPus8dgv5wi22C4taIxICJO0fqppD913X3RY=";
+    hash = "sha256-8lpXWp3LPus8dgv5wi22C4taIxICJO0fqppD913X3RY=";
   };
-  meta = {
-    homepage = "https://github.com/elijahandrews/flake8-blind-except";
+
+  # Module has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [
+    "flake8_blind_except"
+  ];
+
+  meta = with lib; {
     description = "A flake8 extension that checks for blind except: statements";
-    maintainers = with lib.maintainers; [ johbo ];
-    license = lib.licenses.mit;
+    homepage = "https://github.com/elijahandrews/flake8-blind-except";
+    license = licenses.mit;
+    maintainers = with maintainers; [ johbo ];
   };
 }
