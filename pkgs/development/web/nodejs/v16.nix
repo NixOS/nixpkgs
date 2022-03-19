05164d273ac49d0b1d90303d655dc9b5d5fa8d39
Mario Rodas <marsam@users.noreply.github.com>
2022-03-19 04:20:00 +0000

    nodejs-16_x: 16.14.0 -> 16.14.2
    - https://github.com/nodejs/node/releases/tag/v16.14.1
    - https://github.com/nodejs/node/releases/tag/v16.14.2
    
    (cherry picked from commit 9516465000b423929900143258ed03fa861de5e3)

--- pkgs/development/web/nodejs/v16.nix
+++ pkgs/development/web/nodejs/v16.nix
@@ -8,8 +8,8 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "16.14.0";
-    sha256 = "sha256-BetkGT45H6iiwVnA9gwXGCRxUWX4DGf8q528lE4wxiM=";
+    version = "16.14.2";
+    sha256 = "sha256-6SLiFcxo61+U0z6KC2HiyGO3cxzIYAq5VdOCLakP+NE=";
     patches = [
       ./disable-darwin-v8-system-instrumentation.patch
       # Fixes node incorrectly building vendored OpenSSL when we want system OpenSSL.
