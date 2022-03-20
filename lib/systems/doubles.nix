222c569b76ba091c7253c81707705a00a1596654
Jörg Thalheim <joerg@thalheim.io>
2022-03-20 22:02:37 +0100

    platforms.linux: add mips-linux and mips64-linux

--- lib/systems/doubles.nix
+++ lib/systems/doubles.nix
@@ -28,6 +28,7 @@ let
     "aarch64-linux" "armv5tel-linux" "armv6l-linux" "armv7a-linux"
     "armv7l-linux" "i686-linux" "m68k-linux" "mipsel-linux" "mips64el-linux"
     "powerpc64-linux" "powerpc64le-linux" "riscv32-linux"
+    "mips-linux" "mips64-linux"
     "riscv64-linux" "s390-linux" "s390x-linux" "x86_64-linux"
 
     # MMIXware
