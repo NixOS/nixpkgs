ecbf5ae27a9ee6e877a20e3abce85c21e4bda9c2
Robert Hensing <robert@roberthensing.nl>
2022-03-21 00:14:06 +0100

    nixosTest: Simplify doc by deprecating syntax sugar

--- nixos/lib/testing-python.nix
+++ nixos/lib/testing-python.nix
@@ -206,6 +206,7 @@ rec {
             )];
           };
         in
+          lib.warnIf (t?machine) "In test `${name}': The `machine' attribute in NixOS tests (pkgs.nixosTest / make-test-pyton.nix / testing-python.nix / makeTest) is deprecated. Please use the equivalent `nodes.machine'."
           build-vms.buildVirtualNetwork (
               nodes // lib.optionalAttrs (machine != null) { inherit machine; }
           );
