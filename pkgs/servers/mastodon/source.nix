a8896bca5e47c16566d35067d0c5ccd11261e685
Kerstin Humm <kerstin@erictapen.name>
2022-03-21 00:35:17 +0100

    mastodon: 3.4.6 -> 3.5.0
    Co-authored-by: Izorkin <izorkin@elven.pw>

--- pkgs/servers/mastodon/source.nix
+++ pkgs/servers/mastodon/source.nix
@@ -2,10 +2,10 @@
 { fetchgit, applyPatches }: let
   src = fetchgit {
     url = "https://github.com/tootsuite/mastodon.git";
-    rev = "v3.4.6";
-    sha256 = "1lg25m6wsnb7iabbn1vpvn85csv6ywyvcm0ji6d8iq7wwgyq77xs";
+    rev = "v3.5.0";
+    sha256 = "1181zqz7928b6mnp4p502gy2rrwxyv5ysgfydx0n04y8wiq00g48";
   };
 in applyPatches {
   inherit src;
-  patches = [ ];
+  patches = [];
 }
