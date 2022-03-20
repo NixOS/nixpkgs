aeb7d568c004d948df9efd4788c9f1d7caa3a19a
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-20 14:59:40 +0000

    insomnia: 2021.7.2 -> 2022.2.0

--- pkgs/development/web/insomnia/default.nix
+++ pkgs/development/web/insomnia/default.nix
@@ -15,12 +15,12 @@ let
   ];
 in stdenv.mkDerivation rec {
   pname = "insomnia";
-  version = "2021.7.2";
+  version = "2022.2.0";
 
   src = fetchurl {
     url =
       "https://github.com/Kong/insomnia/releases/download/core%40${version}/Insomnia.Core-${version}.deb";
-    sha256 = "sha256-HkQWW4h2+XT5Xi4oiIiMPnrRKw+GIyjGMQ5B1NrBARU=";
+    sha256 = "sha256-nZjTef61FkLQNxq0rA4a5UHNO4XURWoUNGMe1B5UqtE=";
   };
 
   nativeBuildInputs =
