16b5d56a0839edb621f7e938c84d7008277579f7
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-21 07:58:05 +0000

    headscale: 0.14.0 -> 0.15.0
    headscale: Go 1.17 -> Go 1.18

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6551,7 +6551,9 @@ with pkgs;
 
   heimdall-gui = heimdall.override { enableGUI = true; };
 
-  headscale = callPackage ../servers/headscale { };
+  headscale = callPackage ../servers/headscale {
+    buildGoModule = buildGo118Module;
+  };
 
   heisenbridge = callPackage ../servers/heisenbridge { };
 
