c13013049eb1b9f2faed3d41740ccdfa049b9be2
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-03-21 15:14:41 -0500

    bitbucket-cli: remove
    Uses unsupported API and Python 2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2299,8 +2299,6 @@ with pkgs;
 
   birdtray = libsForQt5.callPackage ../applications/misc/birdtray { };
 
-  bitbucket-cli = callPackage ../tools/misc/bitbucket-cli { };
-
   bitbucket-server-cli = callPackage ../applications/version-management/git-and-tools/bitbucket-server-cli { };
 
   blitz = callPackage ../development/libraries/blitz { };
