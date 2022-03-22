1f7cc8a6e17f2434a2d750b9774a1658175a435b
Felix Buehler <account@buehler.rocks>
2022-03-22 13:58:51 +0100

    iana-etc: replace name with pname&version

--- pkgs/data/misc/iana-etc/default.nix
+++ pkgs/data/misc/iana-etc/default.nix
@@ -1,9 +1,8 @@
 { lib, fetchzip, stdenvNoCC, writeText }:
 
-let
+stdenvNoCC.mkDerivation rec {
+  pname = "iana-etc";
   version = "20211124";
-in stdenvNoCC.mkDerivation {
-  name = "iana-etc-${version}";
   src = fetchzip {
     url = "https://github.com/Mic92/iana-etc/releases/download/${version}/iana-etc-${version}.tar.gz";
     sha256 = "sha256-4mM/ZeGd91e1AklGHFK5UB4llg9IgCo9DKcM0iXcBls=";
