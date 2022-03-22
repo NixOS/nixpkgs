8fd85d69403565ed6bc23ca0290918821e205f19
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-22 23:21:05 +0000

    verilator: 4.218 -> 4.220

--- pkgs/applications/science/electronics/verilator/default.nix
+++ pkgs/applications/science/electronics/verilator/default.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation rec {
   pname = "verilator";
-  version = "4.218";
+  version = "4.220";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-FukC60z7Y3bb3I/dgzqCh6kFP6DDBOGi0M8IIZ50P3g=";
+    sha256 = "sha256-Y0j6j8kPCONlegFoWl46LXtfoiLhzMsHCVv+kLe7UyE=";
   };
 
   enableParallelBuilding = true;
