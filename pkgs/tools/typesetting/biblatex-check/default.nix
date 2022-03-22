b4346e1e23793cdb3bbe6e94ce19752160acfb5c
Will Dietz <w@wdtz.org>
2022-03-22 08:58:50 -0500

    biblatex-check: 1.0.0 -> 1.0.1 (official, patches incl)

--- pkgs/tools/typesetting/biblatex-check/default.nix
+++ pkgs/tools/typesetting/biblatex-check/default.nix
@@ -2,29 +2,16 @@
 
 stdenv.mkDerivation rec {
   pname = "biblatex-check";
-  version = "1.0.0";
+  version = "1.0.1";
 
   src = fetchFromGitHub {
     owner = "Pezmc";
     repo = "BibLatex-Check";
     rev = "v${version}";
-    sha256 = "sha256-SNftPSwkG8mOddwHJCguyPPOjQ3/tBYFsEUbr0Z7cuk=";
+    sha256 = "sha256-Pe6Ume7vH8WJG2EqOw31g3VYilfFsDBmNHtHcUXxqf0=";
   };
 
   buildInputs = [ python3 ];
-
-  patches = [
-    # Merged, but not in tagged release yet
-    (fetchpatch {
-      url = "https://github.com/Pezmc/BibLatex-Check/pull/55.patch";
-      sha256 = "sha256-W2CpYAGrWVYALe55H7Q2GtGiN+6EGU2Y3wLldFo+BAY=";
-    })
-    # Submitted
-    (fetchpatch {
-      url = "https://github.com/Pezmc/BibLatex-Check/pull/56.patch";
-      sha256 = "sha256-Xp/8HZHZMjgyl66GlLBNFslo8fVXgUE+VNvy2XHwQC8=";
-    })
-  ];
 
   strictDeps = true;
 
