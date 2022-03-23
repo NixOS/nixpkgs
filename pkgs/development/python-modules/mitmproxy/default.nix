6f44d085118d1375742ef801ee7073adccb6ab53
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-03-23 11:25:54 +0100

    python39Packages.mitmproxy: remove unused dependencies

--- pkgs/development/python-modules/mitmproxy/default.nix
+++ pkgs/development/python-modules/mitmproxy/default.nix
@@ -74,7 +74,6 @@ buildPythonPackage rec {
     passlib
     protobuf
     publicsuffix2
-    pyasn1
     pyopenssl
     pyparsing
     pyperclip
@@ -87,8 +86,6 @@ buildPythonPackage rec {
   ];
 
   checkInputs = [
-    beautifulsoup4
-    glibcLocales
     hypothesis
     parver
     pytest-asyncio
