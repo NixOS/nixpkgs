e541a5bddcab82eba00ec545ffed976415d39281
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-23 22:56:00 +0000

    python310Packages.azure-mgmt-resource: 20.1.0 -> 21.0.0

--- pkgs/development/python-modules/azure-mgmt-resource/default.nix
+++ pkgs/development/python-modules/azure-mgmt-resource/default.nix
@@ -8,14 +8,14 @@
 
 
 buildPythonPackage rec {
-  version = "20.1.0";
+  version = "21.0.0";
   pname = "azure-mgmt-resource";
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "4fbb2158320e0bdd367882642f266a6dfb3b4b8610792b3afbbca39089f212d7";
+    sha256 = "sha256-y9J/UhxwtA/YO/Y88XsStbwD5ecNwrbnpxtevYuQDQM=";
   };
 
   propagatedBuildInputs = [
