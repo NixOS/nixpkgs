8d862c57024792a67ce6a07bafb16e0bb611fef9
Fabian Affolter <mail@fabian-affolter.ch>
2022-03-23 10:39:27 +0100

    python3Packages.asgiref: update supported Python releases

--- pkgs/development/python-modules/asgiref/default.nix
+++ pkgs/development/python-modules/asgiref/default.nix
@@ -11,8 +11,9 @@
 buildPythonPackage rec {
   version = "3.5.0";
   pname = "asgiref";
+  format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "django";
