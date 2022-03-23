ab3d94d381872f26d3e515a125632f077bea71af
Sandro <sandro.jaeckel@gmail.com>
2022-03-23 05:52:19 +0100

    Update pkgs/misc/autoadb/default.nix

--- pkgs/misc/autoadb/default.nix
+++ pkgs/misc/autoadb/default.nix
@@ -11,7 +11,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-9Sv38dCtvbqvxSnRpq+HsIwF/rfLUVZbi0J+mltLres=";
   };
 
-  cargoSha256 = "sha256-fmNySuOW+2HKIOIqIv/8W41ZXSmq3hbi+11yZBbhW5Q=";
+  cargoSha256 = "1gzg1lhq8gp790mrc8fw8dg146k8lg20pnk45m2ssnmdka0826f7";
 
   meta = with lib; {
     description = "Execute a command whenever a device is adb-connected";
