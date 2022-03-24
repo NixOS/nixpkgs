a0a4650d5426760c8693f47ddaae80fcf67982d6
barinov274 <barinov274@gmail.com>
2022-03-24 16:42:28 +1000

    unigine-heaven: init at 4.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29667,6 +29667,8 @@ with pkgs;
 
   unigine-valley = callPackage ../applications/graphics/unigine-valley { };
 
+  unigine-heaven = callPackage ../applications/graphics/unigine-heaven { };
+
   unipicker = callPackage ../applications/misc/unipicker { };
 
   unison = callPackage ../applications/networking/sync/unison {
