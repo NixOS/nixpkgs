0489a21577032f24bad6e90fcaaf445a32e013c4
Connor Baker <connorbaker01@gmail.com>
2022-03-24 14:50:14 -0400

    z3: init 4.8.15, 4.7.1, 4.6.0, 4.5.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32784,7 +32784,13 @@ with pkgs;
     gmp-static = gmp.override { withStatic = true; };
   };
 
-  z3 = callPackage ../applications/science/logic/z3 { python = python2; };
+
+  inherit (callPackages ../applications/science/logic/z3 { python = python2; })
+    z3_4_8
+    z3_4_7
+    z3_4_6
+    z3_4_5;
+  z3 = z3_4_8;
   z3_4_4_0 = callPackage ../applications/science/logic/z3/4.4.0.nix {
     python = python2;
     stdenv = gcc49Stdenv;
