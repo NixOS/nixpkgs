fb8ed438e859ae25f3f70b25c2f04bd0b6babada
Josh Holland <josh@inv.alid.pw>
2022-03-24 10:37:16 +0100

    cfunits: init at 2.3.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1632,6 +1632,8 @@ in {
 
   cftime = callPackage ../development/python-modules/cftime { };
 
+  cfunits = callPackage ../development/python-modules/cfunits { };
+
   cgen = callPackage ../development/python-modules/cgen { };
 
   cgroup-utils = callPackage ../development/python-modules/cgroup-utils { };
