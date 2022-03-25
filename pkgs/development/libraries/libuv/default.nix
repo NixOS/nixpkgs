173de4efbf4a475175beb0ae9b93a760efcf264a
Vladimír Čunát <v@cunat.cz>
2022-03-25 10:01:51 +0100

    libuv: drop a test that seems flaky on Hydra
    https://hydra.nixos.org/log/lg744sdh93maxbb68bk1p29a9rpnx2pw-libuv-1.44.1.drv

--- pkgs/development/libraries/libuv/default.nix
+++ pkgs/development/libraries/libuv/default.nix
@@ -20,6 +20,7 @@ stdenv.mkDerivation rec {
       "threadpool_multiple_event_loops" # times out on slow machines
       "get_passwd" # passed on NixOS but failed on other Linuxes
       "tcp_writealot" "udp_multicast_join" "udp_multicast_join6" # times out sometimes
+      "fs_fstat" # https://github.com/libuv/libuv/issues/2235#issuecomment-1012086927
     ] ++ lib.optionals stdenv.isDarwin [
         # Sometimes: timeout (no output), failed uv_listen. Someone
         # should report these failures to libuv team. There tests should
