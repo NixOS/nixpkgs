1abf65b169051381ef1f131b947fc12de46571cd
Fabian Affolter <mail@fabian-affolter.ch>
2022-03-25 13:45:30 +0100

    python3Packages.azure-mgmt-containerservice: disable on older Python releases

--- pkgs/development/python-modules/azure-mgmt-containerservice/default.nix
+++ pkgs/development/python-modules/azure-mgmt-containerservice/default.nix
@@ -6,12 +6,15 @@
 , azure-common
 , azure-mgmt-core
 , azure-mgmt-nspkg
-, isPy3k
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "azure-mgmt-containerservice";
   version = "18.0.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
@@ -24,14 +27,14 @@ buildPythonPackage rec {
     msrestazure
     azure-common
     azure-mgmt-core
-  ] ++ lib.optionals (!isPy3k) [
-    azure-mgmt-nspkg
   ];
 
   # has no tests
   doCheck = false;
 
-  pythonImportsCheck = [ "azure.mgmt.containerservice" ];
+  pythonImportsCheck = [
+    "azure.mgmt.containerservice"
+  ];
 
   meta = with lib; {
     description = "This is the Microsoft Azure Container Service Management Client Library";
