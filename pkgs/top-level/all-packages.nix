83a42f0242f39d93710bb9f081416ed7f32cd5d6
barinov274 <barinov274@gmail.com>
2022-03-25 15:37:31 +1000

    unigine-superposition: init at 1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29671,6 +29671,8 @@ with pkgs;
 
   unigine-heaven = callPackage ../applications/graphics/unigine-heaven { };
 
+  unigine-superposition = libsForQt5.callPackage ../applications/graphics/unigine-superposition { };
+
   unipicker = callPackage ../applications/misc/unipicker { };
 
   unison = callPackage ../applications/networking/sync/unison {
