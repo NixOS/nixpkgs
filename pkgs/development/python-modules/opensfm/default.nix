9354aafaf6fe69aa54bd5b26f4ca26c55511b813
Serge K <newkozlukov@gmail.com>
2022-03-26 16:41:12 +0200

    opensfm: don't use obscure internals in changelog url
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/development/python-modules/opensfm/default.nix
+++ pkgs/development/python-modules/opensfm/default.nix
@@ -117,7 +117,7 @@ buildPythonPackage rec {
   meta = {
     maintainers = [ lib.maintainers.SomeoneSerge ];
     license = lib.licenses.bsd2;
-    changelog = "${src.meta.homepage}/blob/${src.rev}/CHANGELOG.md";
+    changelog = "https://github.com/mapillary/OpenSfM/blob/${src.rev}/CHANGELOG.md";
     description = "Open source Structure-from-Motion pipeline from Mapillary";
     homepage = "https://opensfm.org/";
   };
