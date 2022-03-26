a7eedbe82983608616ab8cc4e5723ca804fe0182
Jan Tojnar <jtojnar@gmail.com>
2022-03-26 18:33:57 +0100

    d-spy: init at 1.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14952,6 +14952,8 @@ with pkgs;
 
   dfeet = callPackage ../development/tools/misc/d-feet { };
 
+  d-spy = callPackage ../development/tools/misc/d-spy { };
+
   dfu-programmer = callPackage ../development/tools/misc/dfu-programmer { };
 
   dfu-util = callPackage ../development/tools/misc/dfu-util { };
