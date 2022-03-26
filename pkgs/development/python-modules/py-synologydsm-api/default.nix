b2fa8a507054f86f1fb18cebae6e7c93ed0faf6e
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-26 00:18:26 +0000

    python310Packages.py-synologydsm-api: 1.0.7 -> 1.0.8

--- pkgs/development/python-modules/py-synologydsm-api/default.nix
+++ pkgs/development/python-modules/py-synologydsm-api/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "py-synologydsm-api";
-  version = "1.0.7";
+  version = "1.0.8";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "mib1185";
     repo = "synologydsm-api";
     rev = "v${version}";
-    sha256 = "sha256-VhAzR/knvun6hJj8/YREqMfNvOKpTyYNI9fk9hsbHDQ=";
+    sha256 = "sha256-9bh7uLt9+uda6yFCWV6xUh//jFC4DgiS+KtRXQrU3A8=";
   };
 
   nativeBuildInputs = [
