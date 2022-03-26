882df60bfb78d3fc1e4c580400a5b9c37a804863
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-03-26 13:55:54 +0100

    mullvad.openvpn-wireguard: init at 2.5.3

--- pkgs/applications/networking/mullvad/default.nix
+++ pkgs/applications/networking/mullvad/default.nix
@@ -4,4 +4,5 @@
 lib.makeScope newScope (self: {
   libwg = self.callPackage ./libwg.nix { };
   mullvad = self.callPackage ./mullvad.nix { };
+  openvpn-mullvad = self.callPackage ./openvpn.nix { };
 })
