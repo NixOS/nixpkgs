501f6439760dab7c3ae329728762d98c4bbfcd58
techknowlogick <techknowlogick@gitea.io>
2022-03-26 08:29:58 -0400

    buildkite-agent: 3.33.3 -> 3.34.1 (#163110)

--- pkgs/development/tools/continuous-integration/buildkite-agent/default.nix
+++ pkgs/development/tools/continuous-integration/buildkite-agent/default.nix
@@ -3,13 +3,13 @@
   nixosTests }:
 buildGoModule rec {
   pname = "buildkite-agent";
-  version = "3.33.3";
+  version = "3.34.1";
 
   src = fetchFromGitHub {
     owner = "buildkite";
     repo = "agent";
     rev = "v${version}";
-    sha256 = "sha256-RCKHVFYYcWraUwsGuD/anmWpNwc7cHc9jm0LwR9WRzA=";
+    sha256 = "sha256-OxZcMPJx83hBQOe4Pc8ERhO9QOc4euVVs+OMbPjA4U0=";
   };
 
   vendorSha256 = "sha256-n3XRxpEKjHf7L7fcGscWTVKBtot9waZbLoS9cG0kHfI=";
