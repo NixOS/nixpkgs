de48c26357b26d42b0ed82072c4fa0ca2c10d06d
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-27 06:07:03 +0000

    pyinfra: 1.7 -> 1.7.1

--- pkgs/development/python-modules/pyinfra/default.nix
+++ pkgs/development/python-modules/pyinfra/default.nix
@@ -17,14 +17,14 @@
 
 buildPythonPackage rec {
   pname = "pyinfra";
-  version = "1.7";
+  version = "1.7.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-r+7ka3WKE6uHP//p1N71hgTGit7Eo3x9INpbKPYbFMI=";
+    sha256 = "sha256-NS1kuIHgSNHU5IWPvZkIUejaYEWxG0CB6EtwCUkKq1I=";
   };
 
   propagatedBuildInputs = [
