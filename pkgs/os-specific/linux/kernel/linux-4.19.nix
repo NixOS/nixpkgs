eb143c2918ce1bfb82b2ada1ab30e82371f449ce
TredwellGit <tredwell@tutanota.com>
2022-03-27 10:33:25 +0000

    linux: 4.19.235 -> 4.19.236
    (cherry picked from commit 8af99c4aab9d5fd4084120c707b85109458bd750)

--- pkgs/os-specific/linux/kernel/linux-4.19.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "4.19.235";
+  version = "4.19.236";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1615y3ma9icmqqr7lisl8nd8zvvkh77a81yl39yvy6qi9345l32k";
+    sha256 = "1h1ipqpcpi5ga1dg78y9gjmd2a7gkc52i0l83xbr13gyranzcbh0";
   };
 } // (args.argsOverride or {}))
