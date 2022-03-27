92754995454f62a400aa9993398a9a1277c85d11
TredwellGit <tredwell@tutanota.com>
2022-03-27 10:33:35 +0000

    linux: 5.10.107 -> 5.10.108
    (cherry picked from commit 7bfe0eec2b7abae215b55604852852865e2fb1e5)

--- pkgs/os-specific/linux/kernel/linux-5.10.nix
+++ pkgs/os-specific/linux/kernel/linux-5.10.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.10.107";
+  version = "5.10.108";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1snzzhkzdjlj92gqig3sanxlhv0xc0xk2xwjdjr0yds6g43w6ry4";
+    sha256 = "1n216rdlmb0x3az4hl224m8c8sddlqwzmqnds4s8z2wiw3bc4v5z";
   };
 } // (args.argsOverride or {}))
