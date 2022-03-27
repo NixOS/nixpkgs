35634410eabac5ca53c6cd20a72b21889a72302e
TredwellGit <tredwell@tutanota.com>
2022-03-27 10:33:29 +0000

    linux: 4.9.307 -> 4.9.308
    (cherry picked from commit 2dec7aec6d5f5837276f6638c3da5e55df95993c)

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,12 +1,12 @@
 { buildPackages, fetchurl, perl, buildLinux, nixosTests, stdenv, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.307";
+  version = "4.9.308";
   extraMeta.branch = "4.9";
   extraMeta.broken = stdenv.isAarch64;
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1xyhz7hq8yyclxyavzk36sbl41vlb74pccd56240kq34ma1hyis7";
+    sha256 = "11rf3zr31ap6rzy3s489c1dqzkfn5cfkm579y4dnssdyzmyams3v";
   };
 } // (args.argsOverride or {}))
