6a188241e789a9eb02230ea63bb13616227ffcbb
Thomas Watson <twatson52@icloud.com>
2022-03-27 11:28:43 -0500

    python3Packages.sip: fix builds of dependents on non-x86 Linux

--- pkgs/development/python-modules/sip/default.nix
+++ pkgs/development/python-modules/sip/default.nix
@@ -10,6 +10,13 @@ buildPythonPackage rec {
     sha256 = "a1cf8431a8eb9392b3ff6dc61d832d0447bfdcae5b3e4256a5fa74dbc25b0734";
   };
 
+  patches = [
+    # on non-x86 Linux platforms, sip incorrectly detects the manylinux version
+    # and PIP will refuse to install the resulting wheel.
+    # remove once upstream fixes this, hopefully in 6.5.2
+    ./fix-manylinux-version.patch
+  ];
+
   propagatedBuildInputs = [ packaging toml ];
 
   # There aren't tests
