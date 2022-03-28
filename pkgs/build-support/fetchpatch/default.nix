45c75a4cde87cfa7c889fd593073ec1badac7de6
Artturin <Artturin@artturin.com>
2022-03-29 01:37:00 +0300

    fetchpatch: add throwBool, reason, condition, path
    `throwBool = lib.versionAtLeast version "2.1.0";`
    so people won't forget to remove conditional patches
    
    `condition = stdenv.isAarch64;`
    so no `lib.optional` needed so we can evaluate the throwBool
    
    reason = "${pname}: upstream fix for ncurses-6.3";
    so we dont have to use comments
    
    `path = ./some.patch;`
    to allow using the above options with vendored patches

--- pkgs/build-support/fetchpatch/default.nix
+++ pkgs/build-support/fetchpatch/default.nix
@@ -15,6 +15,10 @@ in
 , excludes ? []
 , includes ? []
 , revert ? false
+, throwBool ? false
+, reason ? ""
+, condition ? true
+, path ? ""
 , postFetch ? ""
 , ...
 }@args:
@@ -27,57 +31,63 @@ let
   };
 in let
   inherit (args') stripLen extraPrefix;
+in let
+  fetch = if !condition then ""
+  else if builtins.isPath path then path
+  else fetchurl ({
+    postFetch = ''
+      tmpfile="$TMPDIR/patch"
+
+      if [ ! -s "$out" ]; then
+        echo "error: Fetched patch file '$out' is empty!" 1>&2
+        exit 1
+      fi
+
+      "${patchutils}/bin/lsdiff" \
+        ${lib.optionalString (relative != null) "-p1 -i ${lib.escapeShellArg relative}/'*'"} \
+        "$out" \
+      | sort -u | sed -e 's/[*?]/\\&/g' \
+      | xargs -I{} \
+        "${patchutils}/bin/filterdiff" \
+        --include={} \
+        --strip=${toString stripLen} \
+        ${lib.optionalString (extraPrefix != null) ''
+            --addoldprefix=a/${lib.escapeShellArg extraPrefix} \
+            --addnewprefix=b/${lib.escapeShellArg extraPrefix} \
+        ''} \
+        --clean "$out" > "$tmpfile"
+
+      if [ ! -s "$tmpfile" ]; then
+        echo "error: Normalized patch '$tmpfile' is empty (while the fetched file was not)!" 1>&2
+        echo "Did you maybe fetch a HTML representation of a patch instead of a raw patch?" 1>&2
+        echo "Fetched file was:" 1>&2
+        cat "$out" 1>&2
+        exit 1
+      fi
+
+      ${patchutils}/bin/filterdiff \
+        -p1 \
+        ${builtins.toString (builtins.map (x: "-x ${lib.escapeShellArg x}") excludes)} \
+        ${builtins.toString (builtins.map (x: "-i ${lib.escapeShellArg x}") includes)} \
+        "$tmpfile" > "$out"
+
+      if [ ! -s "$out" ]; then
+        echo "error: Filtered patch '$out' is empty (while the original patch file was not)!" 1>&2
+        echo "Check your includes and excludes." 1>&2
+        echo "Normalized patch file was:" 1>&2
+        cat "$tmpfile" 1>&2
+        exit 1
+      fi
+    '' + lib.optionalString revert ''
+      ${patchutils}/bin/interdiff "$out" /dev/null > "$tmpfile"
+      mv "$tmpfile" "$out"
+    '' + postFetch;
+  } // builtins.removeAttrs args [
+    "relative" "stripLen" "extraPrefix" "excludes" "includes" "revert" "postFetch" "reason" "throwBool" "condition" "path"
+  ]);
 in
-lib.throwIfNot (excludes == [] || includes == [])
-  "fetchpatch: cannot use excludes and includes simultaneously"
-fetchurl ({
-  postFetch = ''
-    tmpfile="$TMPDIR/patch"
-
-    if [ ! -s "$out" ]; then
-      echo "error: Fetched patch file '$out' is empty!" 1>&2
-      exit 1
-    fi
-
-    "${patchutils}/bin/lsdiff" \
-      ${lib.optionalString (relative != null) "-p1 -i ${lib.escapeShellArg relative}/'*'"} \
-      "$out" \
-    | sort -u | sed -e 's/[*?]/\\&/g' \
-    | xargs -I{} \
-      "${patchutils}/bin/filterdiff" \
-      --include={} \
-      --strip=${toString stripLen} \
-      ${lib.optionalString (extraPrefix != null) ''
-          --addoldprefix=a/${lib.escapeShellArg extraPrefix} \
-          --addnewprefix=b/${lib.escapeShellArg extraPrefix} \
-      ''} \
-      --clean "$out" > "$tmpfile"
-
-    if [ ! -s "$tmpfile" ]; then
-      echo "error: Normalized patch '$tmpfile' is empty (while the fetched file was not)!" 1>&2
-      echo "Did you maybe fetch a HTML representation of a patch instead of a raw patch?" 1>&2
-      echo "Fetched file was:" 1>&2
-      cat "$out" 1>&2
-      exit 1
-    fi
-
-    ${patchutils}/bin/filterdiff \
-      -p1 \
-      ${builtins.toString (builtins.map (x: "-x ${lib.escapeShellArg x}") excludes)} \
-      ${builtins.toString (builtins.map (x: "-i ${lib.escapeShellArg x}") includes)} \
-      "$tmpfile" > "$out"
-
-    if [ ! -s "$out" ]; then
-      echo "error: Filtered patch '$out' is empty (while the original patch file was not)!" 1>&2
-      echo "Check your includes and excludes." 1>&2
-      echo "Normalized patch file was:" 1>&2
-      cat "$tmpfile" 1>&2
-      exit 1
-    fi
-  '' + lib.optionalString revert ''
-    ${patchutils}/bin/interdiff "$out" /dev/null > "$tmpfile"
-    mv "$tmpfile" "$out"
-  '' + postFetch;
-} // builtins.removeAttrs args [
-  "relative" "stripLen" "extraPrefix" "excludes" "includes" "revert" "postFetch"
-])
+  lib.throwIfNot (excludes == [] || includes == [])
+    "fetchpatch: cannot use excludes and includes simultaneously"
+  lib.throwIf (throwBool && reason == "") "to use throwBool a reason why the patch is required is needed"
+  lib.throwIf throwBool "patch '${reason}' is no longer required"
+  fetch
