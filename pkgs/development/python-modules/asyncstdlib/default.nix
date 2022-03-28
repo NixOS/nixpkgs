2f2049b1fd78bff3395c823baf91da815ddcc116
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-28 14:51:53 +0000

    python310Packages.asyncstdlib: 3.10.3 -> 3.10.4

--- pkgs/development/python-modules/asyncstdlib/default.nix
+++ pkgs/development/python-modules/asyncstdlib/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "asyncstdlib";
-  version = "3.10.3";
+  version = "3.10.4";
   disabled = pythonOlder "3.7";
   format = "flit";
 
@@ -16,7 +16,7 @@ buildPythonPackage rec {
     owner = "maxfischer2781";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-Q547XKsn4/U7XbDhZADF4qPpFxAGPmv9bAXSQZnNUIo=";
+    sha256 = "sha256-zPWXI5iHMCkSVjyNRcXplTadobYCeMv3Unjt+QVF8D8=";
   };
 
   propagatedBuildInputs = [
