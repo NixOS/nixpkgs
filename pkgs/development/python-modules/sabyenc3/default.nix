32ee651ea94214b8c769b1482b2bf44da533cf7a
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-28 06:20:25 +0000

    python310Packages.sabyenc3: 5.1.3 -> 5.1.5

--- pkgs/development/python-modules/sabyenc3/default.nix
+++ pkgs/development/python-modules/sabyenc3/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "sabyenc3";
-  version = "5.1.3";
+  version = "5.1.5";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-p/7uNtarig1lfPF+dL4OPPdKnfpkveZC8wgwsQy2o2s=";
+    hash = "sha256-JwCzy3QRCzxT8B0VM5SXIIWlYi08tT8eLj/QKtMYLRE=";
   };
 
   # Tests are not included in pypi distribution
