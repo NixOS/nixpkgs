262b164ec092be7121a764feb0efdde2409badc4
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-03-30 00:31:39 +0200

    python39Packages.itsdangerous: 2.1.0 -> 2.1.2, add SuperSandro2000 as maintainer

--- pkgs/development/python-modules/itsdangerous/default.nix
+++ pkgs/development/python-modules/itsdangerous/default.nix
@@ -8,12 +8,12 @@
 
 buildPythonPackage rec {
   pname = "itsdangerous";
-  version = "2.1.0";
-  disabled = pythonOlder "3.6";
+  version = "2.1.2";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-2Ej8uLx9UHxFRrRIV06KRPxOorqE6/jXgykNU+gZkvU=";
+    sha256 = "sha256-XbvGizF+XkLzJ/kCF2NUXcP8O/4i5t65aq8fw4h0FWo=";
   };
 
   checkInputs = [
@@ -21,10 +21,12 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
+  pythonImportsCheck = [ "itsdangerous" ];
+
   meta = with lib; {
     description = "Safely pass data to untrusted environments and back";
     homepage = "https://itsdangerous.palletsprojects.com";
     license = licenses.bsd3;
+    maintainers = with maintainers; [ SuperSandro2000 ];
   };
-
 }
