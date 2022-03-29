ea35502fe4db6f9be94ae8461bf14c223ccef180
Ben Siraphob <bensiraphob@gmail.com>
2022-03-29 09:36:30 -0500

    xtris: fix darwin build

--- pkgs/games/xtris/default.nix
+++ pkgs/games/xtris/default.nix
@@ -18,10 +18,12 @@ stdenv.mkDerivation rec {
   '';
   buildInputs = [ xorg.libX11 ];
 
+  makeFlags = [ "CC=${stdenv.cc.targetPrefix}cc" ];
+
   meta = with lib; {
     description = "A multi-player version of the classical game of Tetris, for the X Window system";
     homepage = "https://web.archive.org/web/20120315061213/http://www.iagora.com/~espel/xtris/xtris.html";
     license = licenses.gpl2;
-    platforms = platforms.linux;
+    platforms = platforms.unix;
   };
 }
