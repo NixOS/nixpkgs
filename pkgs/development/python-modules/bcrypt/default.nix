c7fa67725a918662beb99cbbb5110f65a715db4b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-03-29 22:14:32 +0200

    python39Packages.bcrypt: remove unused dependencies

--- pkgs/development/python-modules/bcrypt/default.nix
+++ pkgs/development/python-modules/bcrypt/default.nix
@@ -1,5 +1,5 @@
 { lib, buildPythonPackage, isPyPy, fetchPypi, pythonOlder
-, cffi, pycparser, mock, pytest, py, six }:
+, cffi, pytestCheckHook, six }:
 
 buildPythonPackage rec {
   version = "3.2.0";
@@ -11,11 +11,11 @@ buildPythonPackage rec {
     sha256 = "5b93c1726e50a93a033c36e5ca7fdcd29a5c7395af50a6892f5d9e7c6cfbfb29";
   };
 
-  buildInputs = [ pycparser mock pytest py ];
-
   propagatedBuildInputs = [ six ] ++ lib.optional (!isPyPy) cffi;
 
   propagatedNativeBuildInputs = lib.optional (!isPyPy) cffi;
+
+  checkInputs = [ pytestCheckHook ];
 
   meta = with lib; {
     maintainers = with maintainers; [ domenkozar ];
