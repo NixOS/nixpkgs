c0691c5bac4204405d797c5863f27b832d80d490
Ben Siraphob <bensiraphob@gmail.com>
2022-03-30 00:49:38 -0500

    ledger-web: expand platforms to unix

--- pkgs/applications/office/ledger-web/default.nix
+++ pkgs/applications/office/ledger-web/default.nix
@@ -18,6 +18,6 @@ bundlerApp {
     homepage = "https://github.com/peterkeen/ledger-web";
     license = licenses.mit;
     maintainers = with maintainers; [ peterhoeg manveru nicknovitski ];
-    platforms = platforms.linux;
+    platforms = platforms.unix;
   };
 }
