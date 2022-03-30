422d3bba01fb11492a323066ecc5dd15130ca517
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-31 01:05:11 +0200

    python3Packages.mccabe: 0.6.1 -> 0.7.0

--- pkgs/development/python-modules/mccabe/default.nix
+++ pkgs/development/python-modules/mccabe/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "mccabe";
-  version = "0.6.1";
+  version = "0.7.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "07w3p1qm44hgxf3vvwz84kswpsx6s7kvaibzrsx5dzm0hli1i3fx";
+    sha256 = "sha256-NI4CQMM7YLvfTlIxku+RnyjLLD19XHeU90AJKQ8jYyU=";
   };
 
   buildInputs = [ pytest pytest-runner ];
