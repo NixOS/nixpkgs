98a276576885a920310d877305901eaba1a79b00
Terje Larsen <terlar@gmail.com>
2022-03-30 10:53:45 +0200

    python310Packages.amazon-ion: init at 0.8.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -470,6 +470,8 @@ in {
 
   altair = callPackage ../development/python-modules/altair { };
 
+  amazon-ion = callPackage ../development/python-modules/amazon-ion { };
+
   amazon_kclpy = callPackage ../development/python-modules/amazon_kclpy { };
 
   ambee = callPackage ../development/python-modules/ambee { };
