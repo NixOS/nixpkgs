68824fbc89d6105cf2bf997bae51bc281d8bad52
Bobby Rong <rjl931189261@126.com>
2022-03-30 16:54:57 +0800

    gnome.gnome-screenshot: fix build with meson 0.61

--- pkgs/desktops/gnome/core/gnome-screenshot/default.nix
+++ pkgs/desktops/gnome/core/gnome-screenshot/default.nix
@@ -4,6 +4,7 @@
 , libxml2
 , libhandy
 , fetchurl
+, fetchpatch
 , pkg-config
 , libcanberra-gtk3
 , gtk3
@@ -26,6 +27,15 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
     sha256 = "Stt97JJkKPdCY9V5ZnPPFC5HILbnaPVGio0JM/mMlZc=";
   };
+
+  patches = [
+    # Fix build with meson 0.61
+    # https://gitlab.gnome.org/GNOME/gnome-screenshot/-/issues/186
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/gnome-screenshot/-/commit/b60dad3c2536c17bd201f74ad8e40eb74385ed9f.patch";
+      sha256 = "Js83h/3xxcw2hsgjzGa5lAYFXVrt6MPhXOTh5dZTx/w=";
+    })
+  ];
 
   nativeBuildInputs = [
     meson
