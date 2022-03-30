66665c09442e59921aab88ab97121ed9497c6572
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-31 01:02:16 +0200

    python3Packages.alembic: 1.7.6 -> 1.7.7

--- pkgs/development/python-modules/alembic/default.nix
+++ pkgs/development/python-modules/alembic/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "alembic";
-  version = "1.7.6";
+  version = "1.7.7";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-bAwF6XaKiW2AQ4fiCymYgP4BvFZIQkaw3/6AddbT2Ec=";
+    sha256 = "sha256-SWEkgXPq186KIe+z3jePE7g5jmYw+rDrJY3HSoryTFg=";
   };
 
   propagatedBuildInputs = [
