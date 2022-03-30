92bb62c5a5e002c2be6d5cd398b159fef26b5039
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-03-30 02:06:53 +0200

    python39Packages.flask-sqlalchemy: update homepage, switch to pytestCheckHook, normalize pname

--- pkgs/development/python-modules/flask-sqlalchemy/default.nix
+++ pkgs/development/python-modules/flask-sqlalchemy/default.nix
@@ -1,24 +1,21 @@
-{ lib, buildPythonPackage, fetchPypi, flask, mock, sqlalchemy, pytest }:
+{ lib, buildPythonPackage, fetchPypi, flask, mock, sqlalchemy, pytestCheckHook }:
 
 buildPythonPackage rec {
-  pname = "Flask-SQLAlchemy";
+  pname = "flask-sqlalchemy";
   version = "2.5.1";
 
   src = fetchPypi {
-    inherit pname version;
+  pname = "Flask-SQLAlchemy";
+    inherit version;
     sha256 = "2bda44b43e7cacb15d4e05ff3cc1f8bc97936cc464623424102bfc2c35e95912";
   };
 
   propagatedBuildInputs = [ flask sqlalchemy ];
-  checkInputs = [ mock pytest ];
-
-  checkPhase = ''
-    pytest
-  '';
+  checkInputs = [ mock pytestCheckHook ];
 
   meta = with lib; {
     description = "SQLAlchemy extension for Flask";
-    homepage = "http://flask-sqlalchemy.pocoo.org/";
+    homepage = "https://flask-sqlalchemy.palletsprojects.com/";
     license = licenses.bsd3;
     maintainers = with maintainers; [ gerschtli ];
   };
