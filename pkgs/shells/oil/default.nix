288c8f8fbe3453f1bb5fb717a26f94ac50754067
R. Ryantm <ryantm-bot@ryantm.com>
2022-03-30 15:43:01 +0000

    oil: 0.9.8 -> 0.9.9

--- pkgs/shells/oil/default.nix
+++ pkgs/shells/oil/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "oil";
-  version = "0.9.8";
+  version = "0.9.9";
 
   src = fetchurl {
     url = "https://www.oilshell.org/download/oil-${version}.tar.xz";
-    sha256 = "sha256-OsrxfJ5dF9Anpg1r6Hj+aD194l99X9Yh4vIZ+R+aH8E=";
+    sha256 = "sha256-b2tMS5z4oejh3C/3vznIWhG4cd3anp5RuffhoORrKCQ=";
   };
 
   postPatch = ''
