fea4c06edeed4ba4ff899c15b76fa713043d3637
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-03-30 12:53:18 +0100

    libretro: unstable-2022-03-09 -> unstable-2022-04-05

--- pkgs/applications/emulators/retroarch/cores.nix
+++ pkgs/applications/emulators/retroarch/cores.nix
@@ -55,7 +55,7 @@ let
     , license
     , src ? (getCoreSrc core)
     , broken ? false
-    , version ? "unstable-2022-03-09"
+    , version ? "unstable-2022-04-05"
     , platforms ? retroarch.meta.platforms
       # The resulting core file is based on core name
       # Setting `normalizeCore` to `true` will convert `-` to `_` on the core filename
