57fae33c291bc720c8cd61d289e90d267bf48305
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-31 01:05:06 +0200

    python3Packages.django-modelcluster: 5.2 -> 6.0

--- pkgs/development/python-modules/django_modelcluster/default.nix
+++ pkgs/development/python-modules/django_modelcluster/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "django-modelcluster";
-  version = "5.2";
+  version = "6.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e541a46a0a899ef4778a4708be22e71cac3efacc09a6ff44bc065c5c9194c054";
+    sha256 = "sha256-zc/+9br103We4ExbYP+vGgyV/A8mXnYvPd+t3jOU5ds=";
   };
 
   disabled = pythonOlder "3.5";
