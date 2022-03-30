faee35ce35eca08e3a18e10d8583fe54de222fea
ajs124 <git@ajs124.de>
2022-03-30 02:17:35 +0200

    nss_latest: 3.76 -> 3.76.1

--- pkgs/development/libraries/nss/latest.nix
+++ pkgs/development/libraries/nss/latest.nix
@@ -5,6 +5,6 @@
 #       Example: nix-shell ./maintainers/scripts/update.nix --argstr package cacert
 
 import ./generic.nix {
-  version = "3.76";
-  sha256 = "0c0nmajcvnm8gqz2v6wrlq04yzy3y7hcs806wjnx4r6kml8073hv";
+  version = "3.76.1";
+  sha256 = "0ai37ncg50n4s5243bfvsip8isqq1y6w2swg1n4xgqg2fk1h8cg1";
 }
