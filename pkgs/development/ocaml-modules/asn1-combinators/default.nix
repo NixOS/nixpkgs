NixOS/nixpkgs

commit fbac0f4a6d367974be3a8ee015fd403a679a3402

ocamlPackages.asn1-combinators: 0.2.5 -> 0.2.6

pkgs/development/ocaml-modules/asn1-combinators/default.nix
@@ -6,13 +6,13 @@ buildDunePackage rec {
   minimumOCamlVersion = "4.05";
 
   pname = "asn1-combinators";
-  version = "0.2.5";
+  version = "0.2.6";
 
   useDune2 = true;
 
   src = fetchurl {
     url = "https://github.com/mirleft/ocaml-asn1-combinators/releases/download/v${version}/asn1-combinators-v${version}.tbz";
-    sha256 = "1pbcdwm12hnfpd1jv2b7cjfkj5r7h61xp2gr8dysb8waa455kwln";
+    sha256 = "sha256-ASreDYhp72IQY3UsHPjqAm9rxwL+0Q35r1ZojikbGpE=";
   };
 
   propagatedBuildInputs = [ cstruct zarith bigarray-compat stdlib-shims ptime ];

commit 163becf75fdff80ad48f86cb84c7067fc2254ea5

ocamlPackages.asn1-combinators: disable with OCaml < 4.08
Fixes build failures introduced by fbac0f4a6d367974be3a8ee015fd403a679a3402

pkgs/development/ocaml-modules/asn1-combinators/default.nix
@@ -3,12 +3,10 @@
 }:
 
 buildDunePackage rec {
-  minimumOCamlVersion = "4.05";
+  minimalOCamlVersion = "4.08";
 
   pname = "asn1-combinators";
   version = "0.2.6";
-
-  useDune2 = true;
 
   src = fetchurl {
     url = "https://github.com/mirleft/ocaml-asn1-combinators/releases/download/v${version}/asn1-combinators-v${version}.tbz";