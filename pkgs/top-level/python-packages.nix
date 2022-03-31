9894b6355e6af72a69fea42a86797b525d9accf6
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-03-31 11:50:51 +0200

    python310Packages.sparqlwrapper: normalize attr

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9475,7 +9475,7 @@ in {
 
   sparklines = callPackage ../development/python-modules/sparklines { };
 
-  SPARQLWrapper = callPackage ../development/python-modules/sparqlwrapper { };
+  sparqlwrapper = callPackage ../development/python-modules/sparqlwrapper { };
 
   sparse = callPackage ../development/python-modules/sparse { };
 
