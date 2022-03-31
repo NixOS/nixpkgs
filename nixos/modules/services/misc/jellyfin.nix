dd082d1f63727413bb86b931869fbf09ef55b0fb
Giulio De Pasquale <giulio@depasquale.eu>
2022-03-31 17:48:51 +0100

    nixfmt formatting

--- nixos/modules/services/misc/jellyfin.nix
+++ nixos/modules/services/misc/jellyfin.nix
@@ -2,10 +2,8 @@
 
 with lib;
 
-let
-  cfg = config.services.jellyfin;
-in
-{
+let cfg = config.services.jellyfin;
+in {
   options = {
     services.jellyfin = {
       enable = mkEnableOption "Jellyfin Media Server";
@@ -34,15 +32,16 @@ in
       dataDir = mkOption {
         type = types.str;
         default = "/var/lib/jellyfin";
-        description = "This is the directory that will hold all Jellyfin data, and is also used as a default base directory.";
+        description =
+          "This is the directory that will hold all Jellyfin data, and is also used as a default base directory.";
       };
-      
+
       cacheDir = mkOption {
         type = types.str;
         default = "/var/cache/jellyfin";
         description = "This is the directory containing the server cache.";
       };
-      
+
       openFirewall = mkOption {
         type = types.bool;
         default = false;
@@ -64,7 +63,8 @@ in
       serviceConfig = rec {
         User = cfg.user;
         Group = cfg.group;
-        ExecStart = "${cfg.package}/bin/jellyfin --datadir '${cfg.dataDir}' --cachedir '${cfg.cacheDir}'";
+        ExecStart =
+          "${cfg.package}/bin/jellyfin --datadir '${cfg.dataDir}' --cachedir '${cfg.cacheDir}'";
         Restart = "on-failure";
 
         # Security options:
@@ -96,7 +96,8 @@ in
 
         RestrictNamespaces = true;
         # AF_NETLINK needed because Jellyfin monitors the network connection
-        RestrictAddressFamilies = [ "AF_NETLINK" "AF_INET" "AF_INET6" "AF_UNIX" ];
+        RestrictAddressFamilies =
+          [ "AF_NETLINK" "AF_INET" "AF_INET6" "AF_UNIX" ];
         RestrictRealtime = true;
         RestrictSUIDSGID = true;
 
@@ -104,7 +105,13 @@ in
         SystemCallErrorNumber = "EPERM";
         SystemCallFilter = [
           "@system-service"
-          "~@cpu-emulation" "~@debug" "~@keyring" "~@memlock" "~@obsolete" "~@privileged" "~@setuid"
+          "~@cpu-emulation"
+          "~@debug"
+          "~@keyring"
+          "~@memlock"
+          "~@obsolete"
+          "~@privileged"
+          "~@setuid"
         ];
       };
     };
@@ -116,9 +123,7 @@ in
       };
     };
 
-    users.groups = mkIf (cfg.group == "jellyfin") {
-      jellyfin = {};
-    };
+    users.groups = mkIf (cfg.group == "jellyfin") { jellyfin = { }; };
 
     networking.firewall = mkIf cfg.openFirewall {
       # from https://jellyfin.org/docs/general/networking/index.html
