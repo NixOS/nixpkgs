1e26d1efbce0e06e6460bd75754e3d09c99c4294
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-31 18:17:14 +0200

    python3Packages.Mako: add babel extra, disable lingua tests

--- pkgs/development/python-modules/Mako/default.nix
+++ pkgs/development/python-modules/Mako/default.nix
@@ -1,8 +1,16 @@
 { lib
 , buildPythonPackage
+, pythonOlder
 , fetchPypi
 , isPyPy
+
+# propagates
 , markupsafe
+
+# extras: Babel
+, Babel
+
+# tests
 , mock
 , pytestCheckHook
 }:
@@ -11,13 +19,27 @@ buildPythonPackage rec {
   pname = "Mako";
   version = "1.2.0";
 
+  disabled = pythonOlder "3.7";
+
   src = fetchPypi {
     inherit pname version;
     sha256 = "sha256-mnx+kiuH2zaGIQz0nV12cDOkHUAQsoTnR2gskr3dizk=";
   };
 
-  propagatedBuildInputs = [ markupsafe ];
-  checkInputs = [ pytestCheckHook markupsafe mock ];
+  propagatedBuildInputs = [
+    markupsafe
+  ];
+
+  passthru.extras-require = {
+    babel = [
+      Babel
+    ];
+  };
+
+  checkInputs = [
+    pytestCheckHook
+    mock
+  ] ++ passthru.extras-require.babel;
 
   disabledTests = lib.optionals isPyPy [
     # https://github.com/sqlalchemy/mako/issues/315
@@ -27,6 +49,11 @@ buildPythonPackage rec {
     "test_stdin_success"
     # fails on pypy2.7
     "test_bytestring_passthru"
+  ];
+
+  disabledTestPaths = [
+    # lingua dependency is not packaged
+    "test/ext/test_linguaplugin.py"
   ];
 
   meta = with lib; {
