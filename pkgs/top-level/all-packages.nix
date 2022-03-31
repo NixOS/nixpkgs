357bf5e7fe076995e8d9e96857855998ade96fe5
ajs124 <git@ajs124.de>
2022-03-31 22:04:33 +0200

    notbit: remove after being marked broken for over two years
    It was marked in commit 5aa4b1994629ed0701b8b6d6e094da9d7d912721 by Linus Heckemann on 2019-10-07 (commited on 2019-10-08)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28222,8 +28222,6 @@ with pkgs;
 
   notepadqq = libsForQt514.callPackage ../applications/editors/notepadqq { };
 
-  notbit = callPackage ../applications/networking/mailreaders/notbit { };
-
   notmuch = callPackage ../applications/networking/mailreaders/notmuch {
     gmime = gmime3;
     pythonPackages = python3Packages;
