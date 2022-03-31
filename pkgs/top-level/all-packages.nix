5d652d056ca31ef5623de9aa80a827eec293c539
Jamie Quigley <jamie@quigley.xyz>
2022-03-31 12:42:00 +0100

    ograc: init at 0.1.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13470,6 +13470,8 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Security;
   };
 
+  ograc = callPackage ../development/tools/rust/ograc { };
+
   rhack = callPackage ../development/tools/rust/rhack { };
   inherit (rustPackages) rls;
   roogle = callPackage ../development/tools/rust/roogle { };
