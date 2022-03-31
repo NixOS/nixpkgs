f31d9457551315abad6174c08646e39e09f8db70
toonn <toonn@toonn.io>
2022-03-31 14:04:52 +0200

    desktopToDarwinBundle: Implement %f and %u Exec field codes
    `%f` and `%u` are used to signal the program only accepts a single file
    or URI argument. I do not believe there's a way to signal this
    information to macOS but it is possible the program really won't work if
    multiple files are passed and it's possible the relative position of
    `%i`, `%c` or `%k` matters. So we replace `%f` or `%u` with `$1`. That
    way we only pass one file in the (possibly significant) position of the
    field code.

--- pkgs/build-support/setup-hooks/desktop-to-darwin-bundle.sh
+++ pkgs/build-support/setup-hooks/desktop-to-darwin-bundle.sh
@@ -178,7 +178,8 @@ processExecFieldCodes() {
   local -r execNoKC="${execNoK/\%c/$(getDesktopParam "${file}" "Name")}"
   local -r icon=$(getDesktopParam "${file}" "Icon")
   local -r execNoKCI="${execNoKC/\%i/${icon:+--icon }${icon}}"
-  local -r exec="${execNoKCI// %[fFuU]}"
+  local -r execNoKCIfu="${execNoKCI/\%[fu]/\$1}"
+  local -r exec="${execNoKCIfu// %[FU]}"
   if [[ "$exec" != "$execRaw" ]]; then
     echo 1>&2 "desktopToDarwinBundle: Application bundles do not understand desktop entry field codes. Changed '$execRaw' to '$exec'."
   fi
