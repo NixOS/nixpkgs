998316ff952f42ae7a0de52e79a93847ecb0d798
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-31 02:24:41 +0200

    python3Packages.ephemeral-port-reserve: init at 1.1.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2661,6 +2661,8 @@ in {
 
   ephem = callPackage ../development/python-modules/ephem { };
 
+  ephemeral-port-reserve = callPackage ../development/python-modules/ephemeral-port-reserve { };
+
   epson-projector = callPackage ../development/python-modules/epson-projector { };
 
   eradicate = callPackage ../development/python-modules/eradicate { };
