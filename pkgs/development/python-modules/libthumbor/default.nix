af09feefd543087646128c196c3a805c614a7d99
Fabian Affolter <mail@fabian-affolter.ch>
2022-04-01 20:03:29 +0200

    python3Packages.libthumbor: disable on older Python releases
    add pythonImportsCheck

--- pkgs/development/python-modules/libthumbor/default.nix
+++ pkgs/development/python-modules/libthumbor/default.nix
@@ -9,21 +9,34 @@
 buildPythonPackage rec {
   pname = "libthumbor";
   version = "2.0.2";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-1PsiFZrTDVQqy8A3nkaM5LdPiBoriRgHkklTOiczN+g=";
+    hash = "sha256-1PsiFZrTDVQqy8A3nkaM5LdPiBoriRgHkklTOiczN+g=";
   };
 
-  buildInputs = [ django ];
-  propagatedBuildInputs = [ six pycrypto ];
+  buildInputs = [
+    django 
+  ];
+
+  propagatedBuildInputs = [
+    six
+    pycrypto
+  ];
 
   doCheck = false;
 
+  pythonImportsCheck = [
+    "libthumbor"
+  ];
+
   meta = with lib; {
-    description = "libthumbor is the python extension to thumbor";
+    description = "Python extension to thumbor";
     homepage = "https://github.com/heynemann/libthumbor";
     license = licenses.mit;
+    maintainers = with maintainers; [ ];
   };
-
 }
