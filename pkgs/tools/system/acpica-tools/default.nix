d1207c1887041feb7937c83859696ca838378549
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-01 08:17:26 +0000

    acpica-tools: 20211217 -> 20220331

--- pkgs/tools/system/acpica-tools/default.nix
+++ pkgs/tools/system/acpica-tools/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "acpica-tools";
-  version = "20211217";
+  version = "20220331";
 
   src = fetchurl {
     url = "https://acpica.org/sites/acpica/files/acpica-unix-${version}.tar.gz";
-    sha256 = "14jrrdrl3sw438791zf2v6rjvhiq78yl7hz2ldzp83c251cgh495";
+    sha256 = "sha256-rK/2ixTx4IBOu/xLlyaKTMvvz6BTsC7Zkk8rFNipjiE=";
   };
 
   NIX_CFLAGS_COMPILE = "-O3";
