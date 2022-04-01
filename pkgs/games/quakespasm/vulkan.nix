9d1d14e0dd96440ca2547dea775f1c1e76354082
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-01 06:23:44 +0000

    vkquake: 1.12.2 -> 1.13.0

--- pkgs/games/quakespasm/vulkan.nix
+++ pkgs/games/quakespasm/vulkan.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "vkquake";
-  version = "1.12.2";
+  version = "1.13.0";
 
   src = fetchFromGitHub {
     owner = "Novum";
     repo = "vkQuake";
     rev = version;
-    sha256 = "sha256-+AUSsqarDW40JKgDUIF3G9RNOKqQLuQHOGF23L8anPQ=";
+    sha256 = "sha256-dRPeUsBLliBevjMOSMU+uPSAivrQ0tbuh4QeLmowrAI=";
   };
 
   sourceRoot = "source/Quake";
