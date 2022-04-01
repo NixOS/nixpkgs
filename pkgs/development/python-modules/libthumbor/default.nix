f7e7f925ec4ec91ac03c88c1bd1e83ab8e715054
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-01 11:48:38 +0000

    python310Packages.libthumbor: 2.0.1 -> 2.0.2

--- pkgs/development/python-modules/libthumbor/default.nix
+++ pkgs/development/python-modules/libthumbor/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "libthumbor";
-  version = "2.0.1";
+  version = "2.0.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ed4fe5f27f8f90e7285b7e6dce99c1b67d43a140bf370e989080b43d80ce25f0";
+    sha256 = "sha256-1PsiFZrTDVQqy8A3nkaM5LdPiBoriRgHkklTOiczN+g=";
   };
 
   buildInputs = [ django ];
