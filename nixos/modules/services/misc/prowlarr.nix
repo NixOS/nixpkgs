d0a727229ab929457ee8611fbb04e74c88cd07ed
Thomas Boerger <thomas@webhippie.de>
2022-04-01 20:16:41 +0200

    nixos/prowlarr: add a module similar to sonarr/radarr/lidarr

--- nixos/modules/services/misc/prowlarr.nix
+++ nixos/modules/services/misc/prowlarr.nix
@@ -11,15 +11,37 @@ in
     services.prowlarr = {
       enable = mkEnableOption "Prowlarr";
 
+      dataDir = mkOption {
+        type = types.str;
+        default = "/var/lib/prowlarr";
+        description = "The directory where Prowlarr stores its data files.";
+      };
+
       openFirewall = mkOption {
         type = types.bool;
         default = false;
         description = "Open ports in the firewall for the Prowlarr web interface.";
       };
+
+      user = mkOption {
+        type = types.str;
+        default = "prowlarr";
+        description = "User account under which Prowlarr runs.";
+      };
+
+      group = mkOption {
+        type = types.str;
+        default = "prowlarr";
+        description = "Group under which Prowlarr runs.";
+      };
     };
   };
 
   config = mkIf cfg.enable {
+    systemd.tmpfiles.rules = [
+      "d '${cfg.dataDir}' 0700 ${cfg.user} ${cfg.group} - -"
+    ];
+
     systemd.services.prowlarr = {
       description = "Prowlarr";
       after = [ "network.target" ];
@@ -27,15 +49,29 @@ in
 
       serviceConfig = {
         Type = "simple";
-        DynamicUser = true;
-        StateDirectory = "prowlarr";
-        ExecStart = "${pkgs.prowlarr}/bin/Prowlarr -nobrowser -data=/var/lib/prowlarr";
+        User = cfg.user;
+        Group = cfg.group;
+        ExecStart = "${pkgs.prowlarr}/bin/Prowlarr -nobrowser -data=${cfg.dataDir}";
         Restart = "on-failure";
       };
     };
 
     networking.firewall = mkIf cfg.openFirewall {
       allowedTCPPorts = [ 9696 ];
+    };
+
+    users.users = mkIf (cfg.user == "prowlarr") {
+      prowlarr = {
+        group = cfg.group;
+        home = "/var/lib/prowlarr";
+        uid = config.ids.uids.prowlarr;
+      };
+    };
+
+    users.groups = mkIf (cfg.group == "prowlarr") {
+      prowlarr = {
+        gid = config.ids.gids.prowlarr;
+      };
     };
   };
 }
