6c5fc227d9fb0e29fb4faa2d83d9c47006c8a8fe
Fabian Affolter <mail@fabian-affolter.ch>
2022-04-02 22:17:07 +0200

    python3Packages.dotmap: disable on older Python releases

--- pkgs/development/python-modules/dotmap/default.nix
+++ pkgs/development/python-modules/dotmap/default.nix
@@ -2,24 +2,32 @@
 , buildPythonPackage
 , fetchPypi
 , pytestCheckHook
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "dotmap";
   version = "1.3.27";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-gHCQIN8CIeF8TgHWeQu8GCRxK1aQFJJ/d7jZurxxMik=";
+    hash = "sha256-gHCQIN8CIeF8TgHWeQu8GCRxK1aQFJJ/d7jZurxxMik=";
   };
 
   checkInputs = [
     pytestCheckHook
   ];
 
-  pytestFlagsArray = [ "dotmap/test.py" ];
+  pytestFlagsArray = [
+    "dotmap/test.py"
+  ];
 
-  pythonImportsCheck = [ "dotmap" ];
+  pythonImportsCheck = [
+    "dotmap"
+  ];
 
   meta = with lib; {
     description = "Python for dot-access dictionaries";
