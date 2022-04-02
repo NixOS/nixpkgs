008fbda841b9b008de6583bc40d5543dae9755f9
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-04-02 06:16:42 +0200

    python3Packages.pytest-xdist: fix hook being applied multiple times

--- pkgs/development/python-modules/pytest-xdist/setup-hook.sh
+++ pkgs/development/python-modules/pytest-xdist/setup-hook.sh
@@ -1,7 +1,10 @@
 pytestXdistHook() {
-    pytestFlagsArray+=("--numprocesses=$NIX_BUILD_CORES" "--forked" )
+    pytestFlagsArray+=(
+        "--numprocesses=$NIX_BUILD_CORES"
+        "--forked"
+    )
 }
 
 if [ -z "${dontUsePytestXdist-}" ] && [ -z "${dontUsePytestCheck-}" ]; then
-    addEnvHooks "$hostOffset" pytestXdistHook
+    preDistPhases+=" pytestXdistHook"
 fi
