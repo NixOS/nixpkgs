8ba849db8c829f4fa0789d9577fc1be03b56ac1e
Robert Scott <code@humanleg.org.uk>
2022-04-04 00:27:33 +0100

    python3Packages.asgiref: fix tests on darwin
    our macos sdk version doesn't have SOCK_NONBLOCK

--- pkgs/development/python-modules/asgiref/default.nix
+++ pkgs/development/python-modules/asgiref/default.nix
@@ -6,6 +6,7 @@
 , pytest-asyncio
 , pytestCheckHook
 , pythonOlder
+, fetchpatch
 }:
 
 buildPythonPackage rec {
@@ -21,6 +22,14 @@ buildPythonPackage rec {
     rev = version;
     sha256 = "sha256-eWDsd8iWK1C/X3t/fKAM1i4hyTM/daGTd8CDSgDTL/U=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "remove-sock-nonblock-in-tests.patch";
+      url = "https://github.com/django/asgiref/commit/d451a724c93043b623e83e7f86743bbcd9a05c45.patch";
+      sha256 = "0whdsn5isln4dqbqqngvsy4yxgaqgpnziz0cndj1zdxim8cdicj7";
+    })
+  ];
 
   propagatedBuildInputs = [
     async-timeout
