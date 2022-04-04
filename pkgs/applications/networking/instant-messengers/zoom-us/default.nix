7647f191559c0325caf01419a434cd84f1e24f36
Will Dietz <w@wdtz.org>
2022-04-04 14:00:40 -0500

    zoom: add dep for udev, fix launching
    (cherry picked from commit 215155b44065e6b2652535f720fa04ac20e8980f)

--- pkgs/applications/networking/instant-messengers/zoom-us/default.nix
+++ pkgs/applications/networking/instant-messengers/zoom-us/default.nix
@@ -25,6 +25,7 @@
 , wayland
 , xorg
 , libxkbcommon
+, udev
 , zlib
   # Runtime
 , coreutils
@@ -85,11 +86,12 @@ let
     xorg.libXrandr
     xorg.libXrender
     xorg.libxshmfence
-    zlib
     xorg.xcbutilimage
     xorg.xcbutilkeysyms
     xorg.libXfixes
     xorg.libXtst
+    udev
+    zlib
   ] ++ lib.optional (pulseaudioSupport) libpulseaudio);
 
 in
