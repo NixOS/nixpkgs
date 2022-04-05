007e79ab42cf2ca596feec895fedd26cd7c9b636
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-04-05 13:43:01 +0200

    python3Packages.botocore: 1.24.30 -> 1.24.33

--- pkgs/development/python-modules/botocore/default.nix
+++ pkgs/development/python-modules/botocore/default.nix
@@ -13,11 +13,11 @@
 
 buildPythonPackage rec {
   pname = "botocore";
-  version = "1.24.30"; # N.B: if you change this, change boto3 and awscli to a matching version
+  version = "1.24.33"; # N.B: if you change this, change boto3 and awscli to a matching version
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-r0vcUe7svp/c2tvtmtWMXJE4DvMPNWACK7wu4dePCtY=";
+    sha256 = "sha256-6l/RgAggMKbDP6Gb8BHXKXDz7SPP/xtBQTBp4yV2gQM=";
   };
 
   propagatedBuildInputs = [
