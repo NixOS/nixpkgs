894d1602bbe1f0eb26be5843403a1f6b5337faca
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-05 13:14:55 +0000

    python310Packages.chiavdf: 1.0.5 -> 1.0.6

--- pkgs/development/python-modules/chiavdf/default.nix
+++ pkgs/development/python-modules/chiavdf/default.nix
@@ -14,12 +14,12 @@
 
 buildPythonPackage rec {
   pname = "chiavdf";
-  version = "1.0.5";
+  version = "1.0.6";
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-2pC6sFRwgAwIEanZXe99848XMxK/lyLGxiA+UA+q3H0=";
+    hash = "sha256-Ri7j/T0nnZFml4kC0qIQkyYRJBPZMhTYxolW/A25030=";
   };
 
   patches = [
