d8d21367180ba7a3d21db6fb04196adfcd0421ab
Vincent Laporte <Vincent.Laporte@gmail.com>
2022-04-05 20:53:07 +0200

    ocaml 4.00 – 4.09: make compatible with glibc-2.34

--- pkgs/development/compilers/ocaml/4.03.nix
+++ pkgs/development/compilers/ocaml/4.03.nix
@@ -3,4 +3,10 @@ import ./generic.nix {
   minor_version = "03";
   patch_version = "0";
   sha256 = "09p3iwwi55r6rbrpyp8f0wmkb0ppcgw67yxw6yfky60524wayp39";
+
+  patches = [
+    # Compatibility with Glibc 2.34
+    { url = "https://github.com/ocaml/ocaml/commit/a8b2cc3b40f5269ce8525164ec2a63b35722b22b.patch";
+      sha256 = "sha256:1rrknmrk86xrj2k3hznnjk1gwnliyqh125zabg1hvy6dlvml9b0x"; }
+  ];
 }
