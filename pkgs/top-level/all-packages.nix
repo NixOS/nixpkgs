e67e81d8e383591c9375c26ddf2bcfaa2f74f74d
OPNA2608 <christoph.neidahl@gmail.com>
2022-04-05 23:47:27 +0200

    asap: init at 5.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24750,6 +24750,8 @@ with pkgs;
 
   inherit (atomPackages) atom atom-beta;
 
+  asap = callPackage ../tools/audio/asap { };
+
   aseprite = callPackage ../applications/editors/aseprite { };
   aseprite-unfree = aseprite.override { unfree = true; };
 
