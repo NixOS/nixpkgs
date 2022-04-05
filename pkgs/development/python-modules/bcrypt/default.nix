740c80c32e07e4d7604754ed95590105ed353f7a
Frederik Rietdijk <fridh@fridh.nl>
2022-04-05 20:35:57 +0200

    python3.pkgs.bcrypt: fix build
    also use pyproject format as we're heading that direction.

--- pkgs/development/python-modules/bcrypt/default.nix
+++ pkgs/development/python-modules/bcrypt/default.nix
@@ -1,5 +1,6 @@
 { lib
 , buildPythonPackage
+, setuptools
 , isPyPy
 , fetchPypi
 , pythonOlder
@@ -11,7 +12,7 @@
 buildPythonPackage rec {
   pname = "bcrypt";
   version = "3.2.0";
-  format = "setuptools";
+  format = "pyproject";
 
   disabled = pythonOlder "3.6";
 
@@ -20,13 +21,16 @@ buildPythonPackage rec {
     sha256 = "5b93c1726e50a93a033c36e5ca7fdcd29a5c7395af50a6892f5d9e7c6cfbfb29";
   };
 
+  nativeBuildInputs = [
+    setuptools
+  ];
+
   propagatedBuildInputs = [
     six
-  ] ++ lib.optional (!isPyPy) [
     cffi
   ];
 
-  propagatedNativeBuildInputs = lib.optional (!isPyPy) [
+  propagatedNativeBuildInputs = [
     cffi
   ];
 
