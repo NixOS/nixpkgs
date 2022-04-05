b7b2c002ed6b821d95f93ef3463313fc12175169
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-05 05:59:36 +0000

    moonlight-qt: 3.2.0 -> 4.0.0

--- pkgs/applications/misc/moonlight-qt/default.nix
+++ pkgs/applications/misc/moonlight-qt/default.nix
@@ -20,13 +20,13 @@
 
 stdenv.mkDerivation rec {
   pname = "moonlight-qt";
-  version = "3.2.0";
+  version = "4.0.0";
 
   src = fetchFromGitHub {
     owner = "moonlight-stream";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-nvVBjBcuHKSn66Q7iTzslGOCkH6zMFf62zx5dDXSosI=";
+    sha256 = "sha256-CfOphr8QILCZg+UrImp5JO/1DTqoan5EwiQeTKR15Fo=";
     fetchSubmodules = true;
   };
 
