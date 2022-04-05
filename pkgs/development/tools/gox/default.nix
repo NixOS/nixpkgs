b77e76decf7eb2ad09a08d693460933ed1e0ed18
Aaron Jheng <wentworth@outlook.com>
2022-04-05 02:18:02 +0000

    gox: Switch to buildGoModule

--- pkgs/development/tools/gox/default.nix
+++ pkgs/development/tools/gox/default.nix
@@ -1,21 +1,24 @@
-{ lib, buildGoPackage, fetchFromGitHub }:
+{ lib, buildGoModule, fetchFromGitHub }:
 
-buildGoPackage rec {
+buildGoModule rec {
   pname = "gox";
   version = "1.0.1";
-
-  goPackagePath = "github.com/mitchellh/gox";
 
   src = fetchFromGitHub {
     owner = "mitchellh";
     repo = "gox";
     rev = "v${version}";
-    sha256 = "0mkh81hd7kn45dz7b6yhzqsg2mvg1g6pwx89jjigxrnqhyg9vrl7";
+    sha256 = "sha256-h+adnofY5v6ilAl1fs0Lb1fxNP7Qm3V+K8TO02BAcFY=";
   };
+
+  vendorSha256 = null;
+
+  ldflags = [ "-s" "-w" ];
 
   meta = with lib; {
     homepage = "https://github.com/mitchellh/gox";
     description = "A dead simple, no frills Go cross compile tool";
     license = licenses.mpl20;
+    maintainers = with maintainers; [ aaronjheng ];
   };
 }
