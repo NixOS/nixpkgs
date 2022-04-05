63dbf4174a267e85a0a4daa336dc9e3093a57e44
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-04-05 16:10:36 +0200

    python3Packages.pdm-pep517: 0.12.1 -> 0.12.3

--- pkgs/development/python-modules/pdm-pep517/default.nix
+++ pkgs/development/python-modules/pdm-pep517/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage rec {
   pname = "pdm-pep517";
-  version = "0.12.1";
+  version = "0.12.3";
   format = "pyproject";
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-w/ms/ceDJjVijpQjUyDg9sGcvNkm6wQcRU+xJGO8dQQ=";
+    sha256 = "sha256-EXv7Fhm4a+s0/gCbjUGsrPeQ0fxSQMzBH2ytoVHUaIc=";
   };
 
   preCheck = ''
