9de5f9f5bba9724669a979c4d8daf0aad79dc320
Loïc Reynier <loic@loicreynier.fr>
2022-04-06 15:38:43 +0200

    toipe: init at 0.3.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29457,6 +29457,8 @@ with pkgs;
 
   toot = callPackage ../applications/misc/toot { };
 
+  toipe = callPackage ../applications/misc/toipe { };
+
   tootle = callPackage ../applications/misc/tootle { };
 
   toxic = callPackage ../applications/networking/instant-messengers/toxic { };
