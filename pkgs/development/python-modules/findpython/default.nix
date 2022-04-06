887a2ccdef1440ec6223fb4ef9eef97008b5f891
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-06 14:35:20 +0000

    python310Packages.findpython: 0.1.3 -> 0.1.5

--- pkgs/development/python-modules/findpython/default.nix
+++ pkgs/development/python-modules/findpython/default.nix
@@ -15,7 +15,7 @@
 
 let
   pname = "findpython";
-  version = "0.1.3";
+  version = "0.1.5";
 in
 buildPythonPackage {
   inherit pname version;
@@ -25,7 +25,7 @@ buildPythonPackage {
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-tVpBa5/PLShyG/vqHOsqbLZ6APmexLlKdtoix6IAKHA=";
+    hash = "sha256-AjTmTKIhWhl39O18S2XRVxeDiIzCpKH2qdjkY2h+i8M=";
   };
 
   nativeBuildInputs = [
