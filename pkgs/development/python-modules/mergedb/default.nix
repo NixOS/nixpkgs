16e14a8acb6f9ea4fabdeb0fa9b1c7cda3adf5c0
Fabian Affolter <mail@fabian-affolter.ch>
2022-04-06 11:10:06 +0200

    python3Package.mergedb: add some blank lines

--- pkgs/development/python-modules/mergedb/default.nix
+++ pkgs/development/python-modules/mergedb/default.nix
@@ -12,8 +12,10 @@
 buildPythonPackage rec {
   pname = "mergedb";
   version = "0.1.1";
+  format = "setuptools";
 
   disabled = pythonOlder "3.7";
+
   src = fetchPypi {
     inherit pname version;
     sha256 = "2034c18dca23456c5b166b63d94300bcd8ec9f386e6cd639c2f66e141c0313f9";
@@ -22,22 +24,26 @@ buildPythonPackage rec {
   nativeBuildInputs = [
     setuptools-scm
   ];
+
   propagatedBuildInputs = [
     pyyaml
     colorama
     jinja2
   ];
+
   checkInputs = [
     pytestCheckHook
   ];
 
-  pythonImportsCheck = [ "mergedb" ];
+  pythonImportsCheck = [
+    "mergedb"
+  ];
 
   meta = with lib; {
     description = "A tool/library for deep merging YAML files";
     homepage = "https://github.com/graysonhead/mergedb";
     license = licenses.gpl3Only;
-    maintainers = [ maintainers.graysonhead ];
+    maintainers = with maintainers; [ graysonhead ];
   };
 }
 
