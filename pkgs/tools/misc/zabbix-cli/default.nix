09c2baedb03516a28895d2311bd71f2bcdf1d13e
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-06 18:01:15 +0000

    zabbix-cli: 2.2.1 -> 2.2.1-1

--- pkgs/tools/misc/zabbix-cli/default.nix
+++ pkgs/tools/misc/zabbix-cli/default.nix
@@ -5,14 +5,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "zabbix-cli";
-  version = "2.2.1";
+  version = "2.2.1-1";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "usit-gd";
     repo = "zabbix-cli";
-    rev = version;
-    sha256 = "0wzmrn8p09ksqhhgawr179c4az7p2liqr0l4q2dra62bxliawyqz";
+    rev = "refs/tags/debian/${version}";
+    sha256 = "sha256-Fjz6k7pAj075jUwfPc9S0I5wtG+80tvPGnp1xcvcYy0=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
