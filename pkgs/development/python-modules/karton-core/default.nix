46de35bff05e2e4412ce5a220cc8e624d1417026
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-06 01:57:20 +0000

    python310Packages.karton-core: 4.3.0 -> 4.4.0

--- pkgs/development/python-modules/karton-core/default.nix
+++ pkgs/development/python-modules/karton-core/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage rec {
   pname = "karton-core";
-  version = "4.3.0";
+  version = "4.4.0";
 
   src = fetchFromGitHub {
     owner = "CERT-Polska";
     repo = "karton";
-    rev = "v${version}";
-    sha256 = "sha256-pIYDY+pie4xqH11UHBal7/+MVmJDgNCFVpSD9we9ZPA=";
+    rev = "refs/tags/v${version}";
+    sha256 = "sha256-TwTq44l/Nx+FQ6tFZHat4SPGOmHSwYfg7ShbGnxpkVw=";
   };
 
   propagatedBuildInputs = [ minio redis ];
