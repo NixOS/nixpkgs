687ff7ac8db5c033911ff93b7416b9ce0edce74a
superherointj <5861043+superherointj@users.noreply.github.com>
2022-04-07 07:09:12 -0300

    powerline-go: unstable-2021-07-15 -> 1.21.0

--- pkgs/tools/misc/powerline-go/default.nix
+++ pkgs/tools/misc/powerline-go/default.nix
@@ -5,18 +5,16 @@
 
 buildGoModule rec {
   pname = "powerline-go";
-  version = "unstable-2021-07-15";
+  version = "1.21.0";
 
   src = fetchFromGitHub {
     owner = "justjanne";
     repo = pname;
-    rev = "f27435b26b5001c52ffb1aee454572c59494c81b";
-    sha256 = "sha256-YB/WMprjXA5ZN6baT5nWahNj0xwbP8kzS7X/1tCwWiE=";
+    rev = "v${version}";
+    sha256 = "sha256-IO3I5lvPdN73EF+S5Xo+TMEYaBtd1pOGMs+aQtRnHjE=";
   };
 
   vendorSha256 = "sha256-HYF6aKz+P241EKmupEoretadlrh9FBRx6nIER66jofg=";
-
-  doCheck = false;
 
   meta = with lib; {
     description = "A Powerline like prompt for Bash, ZSH and Fish";
