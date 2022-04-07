16fe3fbef08320f15a6e0bbf9c10d0023ed2c0f5
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-07 13:25:01 +0000

    python310Packages.ledgerblue: 0.1.41 -> 0.1.42

--- pkgs/development/python-modules/ledgerblue/default.nix
+++ pkgs/development/python-modules/ledgerblue/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "ledgerblue";
-  version = "0.1.41";
+  version = "0.1.42";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "7246a1a0442a63aff0b5de2796d306f0033e1937b3c9b9c2a92c9101cde4fe8d";
+    sha256 = "sha256-UNquetZ1sCLO9T5p5b3jTSu+52xuc5XdyHNKsvvPdck=";
   };
 
   propagatedBuildInputs = [
