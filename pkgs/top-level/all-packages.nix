05c65cd3641ed51f95879d1125e2f777f3f658b7
matthewcroughan <matt@croughan.sh>
2022-04-07 23:26:40 +0100

    tinystatus: init at unstable-2021-07-09
    Co-authored-by: j-k <dev@j-k.io>
    
    Co-authored-by: ckie <25263210+ckiee@users.noreply.github.com>
    (cherry picked from commit 294742618e5348c54b6eb2cbc77fa783699835de)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8998,6 +8998,8 @@ with pkgs;
 
   tinyssh = callPackage ../tools/networking/tinyssh { };
 
+  tinystatus = callPackage ../tools/networking/tinystatus { };
+
   opensshPackages = dontRecurseIntoAttrs (callPackage ../tools/networking/openssh {});
 
   openssh = opensshPackages.openssh.override {
