7d24be4b1434c33d2330268d55379b8ed99b6265
Thomas Boerger <thomas@webhippie.de>
2022-04-08 22:50:53 +0200

    nixos/godns: use treeform configuration instead of detailed configuration

--- nixos/modules/services/misc/godns.nix
+++ nixos/modules/services/misc/godns.nix
@@ -5,27 +5,14 @@ with lib;
 let
   cfg = config.services.godns;
 
-  cfgFile = pkgs.writeText "godns.json" (builtins.toJSON (
-    (filterAttrsRecursive (n: v: v != null && v != []) ({
-      provider = cfg.provider;
-      email = cfg.email;
-      password = cfg.password;
-      password_file = cfg.passwordFile;
-      login_token = cfg.loginToken;
-      login_token_file = cfg.loginTokenFile;
-      domains = (map
-        (elem: {
-          domain_name = elem.domainName;
-          sub_domains = elem.subDomains;
-        })
-        cfg.domains);
-      ip_url = cfg.ipv4Url;
-      ipv6_url = cfg.ipv6Url;
-      ip_type = cfg.ipType;
-      interval = cfg.interval;
-      resolver = cfg.resolver;
-    } // cfg.extraConf)
-  )));
+  settingsFormat = pkgs.formats.json {};
+  settingsFile = settingsFormat.generate "godns.json" ({
+    ip_url = "https://ip4.seeip.org";
+    ipv6_url = "https://ip6.seeip.org";
+    ip_type = "IPv4";
+    interval = 300;
+    resolver = "8.8.8.8";
+  } // cfg.settings);
 
 in
 {
@@ -40,91 +27,20 @@ in
         description = "GoDNS package to use";
       };
 
-      provider = mkOption {
-        type = types.enum [ "Cloudflare" "DNSPod" "Dreamhost" "Dynv6" "Google" "AliDNS" "DuckDNS" "NoIP" "HE" "Scaleway" "Linode" ];
-        default = "Cloudflare";
-        description = "One of the supported DNS providers.";
-      };
-
-      email = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = "Email of the DNS provider.";
-      };
-
-      password = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = "Password of the DNS provider.";
-      };
-
-      passwordFile = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = "Path to file containing password of the DNS provider.";
-      };
-
-      loginToken = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = "API token of the DNS provider.";
-      };
-
-      loginTokenFile = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = "Path to file containing API token of the DNS provider.";
-      };
-
-      domains = mkOption {
-        type = with types; listOf (submodule { options = {
-          domainName = mkOption {
-            type = str;
-            description = "Name of the domain to update.";
-          };
-          subDomains = mkOption {
-            type = listOf str;
-            description = "List of subdomains to update.";
-          };
-        }; });
-        default = [];
-        description = "Domains list, with your sub domains.";
-      };
-
-      ipv4Url = mkOption {
-        type = types.str;
-        default = "https://ip4.seeip.org";
-        description = "A URL for fetching one's public IPv4 address.";
-      };
-
-      ipv6Url = mkOption {
-        type = types.str;
-        default = "https://ip6.seeip.org";
-        description = "A URL for fetching one's public IPv6 address.";
-      };
-
-      ipType = mkOption {
-        type = types.enum [ "IPv4" "IPv6" ];
-        default = "IPv4";
-        description = "Switch deciding if IPv4 or IPv6 should be used.";
-      };
-
-      interval = mkOption {
-        type = types.int;
-        default = 300;
-        description = "How often (in seconds) the public IP should be updated.";
-      };
-
-      resolver = mkOption {
-        type = types.str;
-        default = "8.8.8.8";
-        description = "Address of a public DNS server to use.";
-      };
-
-      extraConf = mkOption {
-        type = types.attrs;
-        default = {};
-        description = "GoDNS extra configuration";
+      settings = mkOption {
+        type = types.submodule {
+          freeformType = settingsFormat.type;
+        };
+        example = {
+          provider = "Cloudflare";
+          email = "example@example.com";
+          login_token = "";
+          domains = [{
+            domain_name = "example.com";
+            sub_domains = ["www"];
+          }];
+        };
+        description = "Settings used by GoDNS.";
       };
 
       user = mkOption {
@@ -157,11 +73,10 @@ in
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
 
-      serviceConfig = mkForce {
-        WorkingDirectory = cfg.dataDir;
+      serviceConfig = {
         User = cfg.user;
         Group = cfg.group;
-        ExecStart = "${cfg.package}/bin/godns -c ${cfgFile}";
+        ExecStart = "${cfg.package}/bin/godns -c ${settingsFile}";
         Restart = "on-failure";
       };
     };
