17fc78ea58790e0f6616145149de74d171c10641
Gabriel Fontes <eu@misterio.me>
2022-04-08 15:00:27 -0300

    nixos/argonone: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -428,6 +428,7 @@
   ./services/games/terraria.nix
   ./services/hardware/acpid.nix
   ./services/hardware/actkbd.nix
+  ./services/hardware/argonone.nix
   ./services/hardware/auto-cpufreq.nix
   ./services/hardware/bluetooth.nix
   ./services/hardware/bolt.nix
