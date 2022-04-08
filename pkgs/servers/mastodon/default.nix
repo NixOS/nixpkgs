fbe4ef868fdd352acdcd2e33d3dedcc452c7f45b
techknowlogick <techknowlogick@gitea.io>
2022-04-08 18:57:24 -0400

    mastodon: 3.5.0 -> 3.5.1
    Co-authored-by: Kerstin Humm <kerstin@erictapen.name>
    (cherry picked from commit 6a441683a0556f29fc416413b41aab88a3226a62)

--- pkgs/servers/mastodon/default.nix
+++ pkgs/servers/mastodon/default.nix
@@ -1,7 +1,6 @@
 { lib, stdenv, nodejs-slim, mkYarnPackage, fetchFromGitHub, bundlerEnv, nixosTests
 , yarn, callPackage, imagemagick, ffmpeg, file, ruby_3_0, writeShellScript
 , fetchYarnDeps, fixup_yarn_lock
-, fetchpatch
 
   # Allow building a fork or custom version of Mastodon:
 , pname ? "mastodon"
@@ -19,16 +18,8 @@ stdenv.mkDerivation rec {
 
   yarnOfflineCache = fetchYarnDeps {
     yarnLock = "${src}/yarn.lock";
-    sha256 = "sha256-Ngfs15YKLfSBOKju3BzpZFnenB370jId2G1g9Qy1y5w=";
+    sha256 = "sha256-Swe7AH/j1+N1T20xaQ+U0Ajtoe9BGzsghAjN1QakOp8=";
   };
-
-  patches = [
-    # Fix indexing statuses in ElasticSearch
-    (fetchpatch {
-      url = "https://github.com/mastodon/mastodon/commit/ef196c913c77338be5ebb1e02af2f6225f857080.patch";
-      sha256 = "sha256-uw8m6j4BzMQtev0LNLeIHW0xOJEmj3JikT/6gVfmvzs=";
-    })
-  ];
 
   mastodon-gems = bundlerEnv {
     name = "${pname}-gems-${version}";
