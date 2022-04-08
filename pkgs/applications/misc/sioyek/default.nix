5c0481109c0d7fe2a19dfbdb6bd18f94b1c21a35
Jia Xiaodong <xdjiaxd@gmail.com>
2022-04-08 19:42:52 +0800

    Update pkgs/applications/misc/sioyek/default.nix
    typo!
    
    Co-authored-by: ajs124 <ajs124@users.noreply.github.com>

--- pkgs/applications/misc/sioyek/default.nix
+++ pkgs/applications/misc/sioyek/default.nix
@@ -52,6 +52,6 @@ stdenv.mkDerivation rec {
     changelog = "https://github.com/ahrm/sioyek/releases";
     license = licenses.gpl3Only;
     platforms = platforms.linux;
-    maintainers = [ mainterners.podocarp ];
+    maintainers = [ maintainers.podocarp ];
   };
 }
