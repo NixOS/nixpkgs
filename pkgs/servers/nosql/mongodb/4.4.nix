7a246abcf1181d86c7ca1babf2b9623f351a4eec
Pascal Bach <pascal.bach@nextrem.ch>
2022-04-09 09:42:36 +0200

    mongodb: 4.4.10 -> 4.4.13

--- pkgs/servers/nosql/mongodb/4.4.nix
+++ pkgs/servers/nosql/mongodb/4.4.nix
@@ -6,8 +6,8 @@ let
   };
 in
 buildMongoDB {
-  version = "4.4.10";
-  sha256 = "1rn9w3wwb2kbixqx39zb7cczzw5qjbh7d9yhlbivrs0f9bywl8nx";
+  version = "4.4.13";
+  sha256 = "sha256-ebg3R6P+tjRvizDzsl7mZzhTfqIaRJPfHBu0IfRvtS8=";
   patches = [
     ./forget-build-dependencies-4-4.patch
   ] ++ lib.optionals stdenv.isDarwin [ ./asio-no-experimental-string-view-4-4.patch ];
