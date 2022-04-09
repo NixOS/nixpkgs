5e334e3296d9e317c778787cc4ef757cd364455d
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-09 18:53:16 +0000

    cudatext: 1.160.0 -> 1.160.2

--- pkgs/applications/editors/cudatext/default.nix
+++ pkgs/applications/editors/cudatext/default.nix
@@ -38,13 +38,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "cudatext";
-  version = "1.160.0";
+  version = "1.160.2";
 
   src = fetchFromGitHub {
     owner = "Alexey-T";
     repo = "CudaText";
     rev = version;
-    sha256 = "sha256-42V6RFa+mAXyaUuKeDQa9Voi1MjnzcVl+cOA65VabxM=";
+    sha256 = "sha256-moKuiW5kd0jdAk0lBLX8ZWeT/locxGAvM4oOqvMGsr4=";
   };
 
   postPatch = ''
