46e8fed971b33b0e8cd08d200a23a709fe4a8b71
TredwellGit <tredwell@tutanota.com>
2022-04-10 09:41:06 +0000

    linux: 5.10.109 -> 5.10.110

--- pkgs/os-specific/linux/kernel/linux-5.10.nix
+++ pkgs/os-specific/linux/kernel/linux-5.10.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.10.109";
+  version = "5.10.110";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1p0k46isy2wzzms801lrnb59f1nb9mhywjj7fnkrwrj9nbn25yqq";
+    sha256 = "14sn906f1bd87ngq1g2hx458v42d4nlzr36ba4alhcsl6836mvyv";
   };
 } // (args.argsOverride or {}))
