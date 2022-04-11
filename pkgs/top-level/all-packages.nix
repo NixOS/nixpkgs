99f40c2c13e48dc9c22221462d4d8b06d0930790
Erik Arvstedt <erik.arvstedt@gmail.com>
2022-04-11 17:06:01 +0200

    paperless-ng: delete pkg, add alias to `paperless-ngx`

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8802,8 +8802,6 @@ with pkgs;
 
   pantheon-tweaks = callPackage ../desktops/pantheon/third-party/pantheon-tweaks { };
 
-  paperless-ng = callPackage ../applications/office/paperless-ng { };
-
   paperless-ngx = callPackage ../applications/office/paperless-ngx { };
 
   paperwork = callPackage ../applications/office/paperwork/paperwork-gtk.nix { };
