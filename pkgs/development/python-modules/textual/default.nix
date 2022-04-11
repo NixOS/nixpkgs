a1665d0f41096c3582a70256b29c5a7ad57b1849
Joel <joel@joel.tokyo>
2022-04-11 15:16:42 +1000

    python3Packages.textual: 0.1.15 -> 0.1.17

--- pkgs/development/python-modules/textual/default.nix
+++ pkgs/development/python-modules/textual/default.nix
@@ -10,24 +10,16 @@
 
 buildPythonPackage rec {
   pname = "textual";
-  version = "0.1.15";
+  version = "0.1.17";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "Textualize";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "1jmjais0yq8dwi9yikgrxdw4rwp8aq1981nhfxn0v97jb07i4cj6";
+    # tag is incorrect
+    rev = "e13479bf72e8717b6c1c23a308465458297a6b33";
+    sha256 = "1c9qzqb8l1kh9znam7mix0silaxz5bkrgwss3sd280hhgbsray9c";
   };
-
-  patches = [
-    (fetchpatch {
-      # v0.1.15 git tag has 0.1.14 in pyproject.toml
-      name = "version.patch";
-      url = "https://github.com/Textualize/textual/commit/1b8d7d184e10889002425641222702afba508aea.patch";
-      sha256 = "1nfqp5f8ba3fg0ar3lghrlqypbjbsaywxaz3iiff8fy8j2wgsppp";
-    })
-  ];
 
   nativeBuildInputs = [ poetry-core ];
 
