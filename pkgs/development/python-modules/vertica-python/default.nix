a0257674da77e84bb33d655e3be0f92cbd4fe217
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-11 21:33:06 +0000

    python310Packages.vertica-python: 1.0.4 -> 1.0.5

--- pkgs/development/python-modules/vertica-python/default.nix
+++ pkgs/development/python-modules/vertica-python/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "vertica-python";
-  version = "1.0.4";
+  version = "1.0.5";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-IpdrR9mDG+8cNnXgSXkmXahSEP4EGnEBJqZk5SNu9pA=";
+    hash = "sha256-m9r6P8GTehx33QO/aCuKArrpJ/ycVHWPkQ9sPc3tmeo=";
   };
 
   propagatedBuildInputs = [
