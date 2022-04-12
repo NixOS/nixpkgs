4f9857143ccd4b4ddf972cae7a1002be01b6fcdf
Vincent Haupert <mail@vincent-haupert.de>
2022-04-12 17:12:14 +0200

    github-runner: 2.289.2 -> 2.290.0

--- pkgs/development/tools/continuous-integration/github-runner/default.nix
+++ pkgs/development/tools/continuous-integration/github-runner/default.nix
@@ -43,13 +43,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "github-runner";
-  version = "2.289.2";
+  version = "2.290.0";
 
   src = fetchFromGitHub {
     owner = "actions";
     repo = "runner";
     rev = "v${version}";
-    hash = "sha256-yISJ/qGJ+tGnJOqpTUe+rkG4UGzTZ5tIXix6wuDKXO0=";
+    hash = "sha256-5ASKWDtASVtGDPn68tjjx8ZTVv1E14M26OCDpMJ+nJU=";
   };
 
   nativeBuildInputs = [
