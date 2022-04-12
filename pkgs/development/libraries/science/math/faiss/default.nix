79046b7a5ebd2f882104d9eec1732c8a6612f4e2
Someone Serge <sergei.kozlukov@aalto.fi>
2022-04-13 02:39:53 +0300

    faiss: prefer optionals over optional

--- pkgs/development/libraries/science/math/faiss/default.nix
+++ pkgs/development/libraries/science/math/faiss/default.nix
@@ -16,8 +16,8 @@
 , addOpenGLRunpath
 , optLevel ? let
     optLevels =
-      lib.optional stdenv.hostPlatform.avx2Support "avx2"
-      ++ lib.optional stdenv.hostPlatform.sse4_1Support "sse4"
+      lib.optionals stdenv.hostPlatform.avx2Support [ "avx2" ]
+      ++ lib.optionals stdenv.hostPlatform.sse4_1Support [ "sse4" ]
       ++ [ "generic" ];
   in
   # Choose the maximum available optimization level
