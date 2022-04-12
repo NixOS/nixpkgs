29c687aa1ae85c075ea413f04e73da8e842b299f
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-12 02:31:40 +0000

    python310Packages.zigpy: 0.44.1 -> 0.44.2

--- pkgs/development/python-modules/zigpy/default.nix
+++ pkgs/development/python-modules/zigpy/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "zigpy";
-  version = "0.44.1";
+  version = "0.44.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -23,8 +23,8 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "zigpy";
     repo = "zigpy";
-    rev = version;
-    sha256 = "sha256-7X3uaxzvVMhSucCGA+rZsgt+fJSNjYQkJLpCGyHOIlc=";
+    rev = "refs/tags/${version}";
+    sha256 = "sha256-E6SeuVu5UdWL5Tx39UQymNhABltR+qVHANYWuCh+h6I=";
   };
 
   propagatedBuildInputs = [
