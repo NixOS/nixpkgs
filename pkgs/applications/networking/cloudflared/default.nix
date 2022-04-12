a31f1df7572b36088ada45cbf5d69e43491b0a7e
techknowlogick <techknowlogick@gitea.io>
2022-04-12 15:30:24 -0400

    cloudflared: 2022.4.0 -> 2022.4.1

--- pkgs/applications/networking/cloudflared/default.nix
+++ pkgs/applications/networking/cloudflared/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "cloudflared";
-  version = "2022.4.0";
+  version = "2022.4.1";
 
   src = fetchFromGitHub {
     owner  = "cloudflare";
     repo   = "cloudflared";
     rev    = version;
-    hash   = "sha256-+40OK2q4WdvlLhoPfZH6q+pghgS7ZLmaZl2VbZK4rdA=";
+    hash   = "sha256-dgvXbWtLP6sXBlqcx/xpw9LIbcE4VlYZQO5rrS34+9I=";
   };
 
   vendorSha256 = null;
