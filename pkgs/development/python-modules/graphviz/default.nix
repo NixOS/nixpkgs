1155186a2b72dd8df1d701f40fedf8c84766061a
Robert Schütz <nix@dotlambda.de>
2022-04-12 01:03:39 +0000

    python3Packages.graphviz: 0.19.1 -> 0.19.2
    https://github.com/xflr6/graphviz/blob/0.19.2/CHANGES.rst

--- pkgs/development/python-modules/graphviz/default.nix
+++ pkgs/development/python-modules/graphviz/default.nix
@@ -16,16 +16,16 @@
 
 buildPythonPackage rec {
   pname = "graphviz";
-  version = "0.19.1";
+  version = "0.19.2";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   # patch does not apply to PyPI tarball due to different line endings
   src = fetchFromGitHub {
     owner = "xflr6";
     repo = "graphviz";
     rev = version;
-    sha256 = "sha256-pE1lsx/r/BjvW5W2niDx/UeRXxx4kvCyHzAUAG3bdGc=";
+    hash = "sha256-LxXi0Es6ZJT/nSS6SjGg/3stmR25T4R7TZC34mbT+EA=";
   };
 
   patches = [
@@ -65,6 +65,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Simple Python interface for Graphviz";
     homepage = "https://github.com/xflr6/graphviz";
+    changelog = "https://github.com/xflr6/graphviz/blob/${src.rev}/CHANGES.rst";
     license = licenses.mit;
     maintainers = with maintainers; [ dotlambda ];
   };
