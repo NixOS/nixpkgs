66db6b56160eb3ef2c13f6bb301046fce932dab1
techknowlogick <techknowlogick@gitea.io>
2022-04-11 21:56:39 -0400

    nomad-pack: 2022-02-11 -> 2022-04-12

--- pkgs/applications/networking/cluster/nomad-pack/default.nix
+++ pkgs/applications/networking/cluster/nomad-pack/default.nix
@@ -5,17 +5,17 @@
 
 buildGoModule rec {
   pname = "nomad-pack";
-  version = "2022-02-11";
-  rev   = "568ac5e42bc41172a1fa3c8b18af2f42b9e341ff";
+  version = "2022-04-12";
+  rev   = "50ad747d2a5a2b90af1b3564483510cb04fefbea";
 
   src = fetchFromGitHub {
     owner = "hashicorp";
     repo = pname;
     inherit rev;
-    sha256 = "sha256-0hvnGdUT72sWvMER67ZBxcC+VTbuFMIos2NudOjeTB8=";
+    sha256 = "sha256-VG6Dmx5WD2AzKReMbmrpzXCNhrJqaZY3aQV9P+4ET68=";
   };
 
-  vendorSha256 = "sha256-wmoXZIogKj4i9+AsEjY7QaT2Tn4LQyGQcEFHrRO0W9s=";
+  vendorSha256 = "sha256-7ovR2F9N94iFK/B5OXRcqfykOYHST3354+Ge2L8yzq0=";
 
   # skip running go tests as they require network access
   doCheck = false;
