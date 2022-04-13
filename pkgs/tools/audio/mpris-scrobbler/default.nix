4dd2e406feb4d350a2e239be5b63ad92dbdb64ce
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-13 09:17:17 +0000

    mpris-scrobbler: 0.4.0.1 -> 0.4.90

--- pkgs/tools/audio/mpris-scrobbler/default.nix
+++ pkgs/tools/audio/mpris-scrobbler/default.nix
@@ -15,13 +15,13 @@
 
 stdenv.mkDerivation rec {
   pname = "mpris-scrobbler";
-  version = "0.4.0.1";
+  version = "0.4.90";
 
   src = fetchFromGitHub {
     owner  = "mariusor";
     repo   = "mpris-scrobbler";
     rev    = "v${version}";
-    sha256 = "0jzmgcb9a19hl8y7iwy8l3cc2vgzi0scw7r5q72kszfyxn0yk2gs";
+    sha256 = "sha256-+Y5d7yFOnSk2gQS/m/01ofbNeDCLXb+cTTlHj4bgO0M=";
   };
 
   postPatch = ''
