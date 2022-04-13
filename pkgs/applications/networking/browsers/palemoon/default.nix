e08cac733c3fe4bb1bf1bf95d64d0d7b557637e6
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-13 10:29:55 +0000

    palemoon: 29.4.5.1 -> 29.4.6
    (cherry picked from commit 93b9c6708e7dfdb1ad18a7854fdb3bb14346f354)

--- pkgs/applications/networking/browsers/palemoon/default.nix
+++ pkgs/applications/networking/browsers/palemoon/default.nix
@@ -46,12 +46,12 @@ assert with lib.strings; (
 
 stdenv.mkDerivation rec {
   pname = "palemoon";
-  version = "29.4.5.1";
+  version = "29.4.6";
 
   src = fetchzip {
     name = "${pname}-${version}";
     url = "http://archive.palemoon.org/source/${pname}-${version}.source.tar.xz";
-    sha256 = "sha256-IC7E88dECAz2diVLEEdjMltpNMBhPTlPvbz05BniBMI=";
+    sha256 = "sha256-6bI3AnIhp0x3BCgTvmbOXDBGrJXg3cN+AmwI8XCKD8g=";
   };
 
   nativeBuildInputs = [
