4d9a820dc791b641611cd73bd52d0e974e22604b
Matthias Beyer <mail@beyermatthias.de>
2022-04-13 09:06:20 +0200

    mutt: 2.2.2 -> 2.2.3
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/applications/networking/mailreaders/mutt/default.nix
+++ pkgs/applications/networking/mailreaders/mutt/default.nix
@@ -27,11 +27,11 @@ with lib;
 
 stdenv.mkDerivation rec {
   pname = "mutt";
-  version = "2.2.2";
+  version = "2.2.3";
 
   src = fetchurl {
     url = "http://ftp.mutt.org/pub/mutt/${pname}-${version}.tar.gz";
-    sha256 = "1k0ghbpc4gn3sydbw10xv9djin9grk1pkxdwynrw0iknyc68gphh";
+    sha256 = "12cds5qm0x51wj1bz1a2f4q4qwbyfssq9pnisxz48ks5mg6xv2lp";
   };
 
   patches = optional smimeSupport (fetchpatch {
