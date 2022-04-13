7cf8a80c3a46860e9d07d14f75ba7c766b102f2b
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-04-13 03:10:23 +0200

    home-assistant: 2022.4.2 -> 2022.4.3
    https://github.com/home-assistant/core/releases/tag/2022.4.3
    
    Fixes a small statistical mishap in the requirements parser.

--- pkgs/servers/home-assistant/default.nix
+++ pkgs/servers/home-assistant/default.nix
@@ -168,7 +168,7 @@ let
   extraPackagesFile = writeText "home-assistant-packages" (lib.concatMapStringsSep "\n" (pkg: pkg.pname) extraBuildInputs);
 
   # Don't forget to run parse-requirements.py after updating
-  hassVersion = "2022.4.2";
+  hassVersion = "2022.4.3";
 
 in python.pkgs.buildPythonApplication rec {
   pname = "homeassistant";
@@ -186,7 +186,7 @@ in python.pkgs.buildPythonApplication rec {
     owner = "home-assistant";
     repo = "core";
     rev = version;
-    hash = "sha256-jFETlFgxS1qlUYnqVUj0Duki6Met21NQbZQOc5pWxpE=";
+    hash = "sha256-kubW0JhG9ervVHVl65YmD5jd/0oWenacAyfSP0EPmsU=";
   };
 
   # leave this in, so users don't have to constantly update their downstream patch handling
