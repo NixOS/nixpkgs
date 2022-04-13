7ad5fe3d4346d054312637ea1a212cea28441e13
yuu <yuuyin@protonmail.com>
2022-04-13 17:42:31 -0300

    pythonPackages.levenshtein: fix 0.18.1 build
    Co-authored-by: Martin Weinelt <hexa@darmstadt.ccc.de>

--- pkgs/development/python-modules/levenshtein/default.nix
+++ pkgs/development/python-modules/levenshtein/default.nix
@@ -2,28 +2,51 @@
 , buildPythonPackage
 , fetchFromGitHub
 , pythonOlder
-, pytestCheckHook
+, python
+, cmake
+, ninja
+, cython
+, scikit-build
+, setuptools
+, rapidfuzz-cpp
 , rapidfuzz
+, pytestCheckHook
 }:
 
 buildPythonPackage rec {
   pname = "levenshtein";
   version = "0.18.1";
-  format = "setuptools";
+  format = "pyproject";
 
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "maxbachmann";
-    repo = "Levenshtein";
+    repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-3p9LM4tv45bqeTsuyngivqfd5uml7uqGB2ICKqPa0qY=";
+    fetchSubmodules = true;
+    sha256 = "sha256-WREYdD5MFOpCzH4BSceRpzQZdpi3Xxxn0DpMvDsNlGo=";
   };
 
   postPatch = ''
-    substituteInPlace setup.cfg \
-      --replace "rapidfuzz >= 1.8.2, < 1.9" "rapidfuzz"
+    substituteInPlace setup.py \
+      --replace ", < 3.0.0" ""
+
+    substituteInPlace pyproject.toml \
+      --replace "Cython==3.0.0a10" "Cython>=0.29.0"
   '';
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+    cython
+    scikit-build
+    setuptools
+  ];
+
+  buildInputs = [
+    rapidfuzz-cpp
+  ];
 
   propagatedBuildInputs = [
     rapidfuzz
@@ -31,6 +54,12 @@ buildPythonPackage rec {
 
   checkInputs = [
     pytestCheckHook
+  ];
+
+  dontUseCmakeConfigure = true;
+
+  cmakeFlags = [
+    "-DCMAKE_MODULE_PATH=${python.pkgs.scikit-build}/${python.sitePackages}/skbuild/resources/cmake"
   ];
 
   pythonImportsCheck = [
@@ -42,5 +71,6 @@ buildPythonPackage rec {
     homepage = "https://github.com/maxbachmann/Levenshtein";
     license = licenses.gpl2Plus;
     maintainers = with maintainers; [ fab ];
+    platforms = platforms.all;
   };
 }
