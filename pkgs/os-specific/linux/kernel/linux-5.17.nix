50b9d2dc0db6145e7063f127d2934f8e3636bc8c
TredwellGit <tredwell@tutanota.com>
2022-04-14 06:02:31 +0000

    linux: 5.17.2 -> 5.17.3
    (cherry picked from commit d061104f96b538697aea9044505b265403ccb2a1)

--- pkgs/os-specific/linux/kernel/linux-5.17.nix
+++ pkgs/os-specific/linux/kernel/linux-5.17.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.17.2";
+  version = "5.17.3";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "168b7zzw72k6hxbw0q402vvfir79qki9bjvxxvfi7s6g6y20z8id";
+    sha256 = "0b0nb807r2pwrifc7yk0p9q6cm472ahggfaix6yiqzmqcvisil1j";
   };
 } // (args.argsOverride or { }))
