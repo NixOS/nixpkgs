df30f5b75bdaae8dfd1316531c922277b22357f1
Bobby Rong <rjl931189261@126.com>
2022-04-14 18:52:37 +0800

    gnome-firmware: 3.34.0 → 42.1
    https://gitlab.gnome.org/World/gnome-firmware/-/compare/3.34.0...3.36.0
    https://gitlab.gnome.org/World/gnome-firmware/-/compare/3.36.0...41.0
    https://gitlab.gnome.org/World/gnome-firmware/-/compare/41.0...42.1

--- pkgs/applications/misc/gnome-firmware/default.nix
+++ pkgs/applications/misc/gnome-firmware/default.nix
@@ -1,45 +1,33 @@
-{ lib, stdenv
+{ stdenv
+, lib
 , fetchFromGitLab
-, fetchpatch
 , appstream-glib
 , desktop-file-utils
 , fwupd
 , gettext
 , glib
-, gtk3
-, libsoup
+, gtk4
+, libadwaita
 , libxmlb
 , meson
 , ninja
 , pkg-config
 , systemd
 , help2man
-, wrapGAppsHook
+, wrapGAppsHook4
 }:
 
 stdenv.mkDerivation rec {
   pname = "gnome-firmware";
-  version = "3.34.0";
+  version = "42.1";
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
     owner = "World";
     repo = "gnome-firmware";
     rev = version;
-    sha256 = "04pll0fzf4nr276kfw89r0524s6ppmls5rz4vq2j8c8gb50g0b6l";
+    sha256 = "9QZ98EElENWsME/jXoj9YJl2e+ipyLm0g4grQUwmnuE=";
   };
-
-  patches = [
-    # Fixes manual build
-    (fetchpatch {
-      url = "https://gitlab.gnome.org/World/gnome-firmware/commit/006b64dcb401d8c81a33222bc4be8274c23f3c9c.patch";
-      sha256 = "02303ip4ri5pv1bls8c0njb00qhn0jd0d8rmvsrig0fmacwfvc06";
-    })
-    (fetchpatch {
-      url = "https://gitlab.gnome.org/World/gnome-firmware/commit/c4f076f2c902080618e0c27dec924fd0019f68a3.patch";
-      sha256 = "1yfxd7qsg3gwpamg0m2sbcfrgks59w70r9728arrc4pwx1hia2q1";
-    })
-  ];
 
   nativeBuildInputs = [
     appstream-glib # for ITS rules
@@ -49,14 +37,14 @@ stdenv.mkDerivation rec {
     meson
     ninja
     pkg-config
-    wrapGAppsHook
+    wrapGAppsHook4
   ];
 
   buildInputs = [
     fwupd
     glib
-    gtk3
-    libsoup
+    gtk4
+    libadwaita
     libxmlb
     systemd
   ];
