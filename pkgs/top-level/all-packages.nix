8679b216a69e1414c306d29f19a860e787de1d93
Kierán Meinhardt <kmein@posteo.de>
2022-04-14 22:07:43 +0200

    ezra-sil: init at 2.51

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24162,6 +24162,8 @@ with pkgs;
 
   execline-man-pages = skawarePackages.execline-man-pages;
 
+  ezra-sil = callPackage ../data/fonts/ezra-sil { };
+
   fantasque-sans-mono = callPackage ../data/fonts/fantasque-sans-mono {};
 
   fira = callPackage ../data/fonts/fira { };
