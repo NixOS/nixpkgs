a837d70cac26db2aa8965e57a4e65be5960df7a5
Mario Rodas <marsam@users.noreply.github.com>
2022-04-14 04:20:00 +0000

    python310Packages.mlflow: 1.25.0 -> 1.25.1
    https://github.com/mlflow/mlflow/releases/tag/v1.25.1

--- pkgs/development/python-modules/mlflow/default.nix
+++ pkgs/development/python-modules/mlflow/default.nix
@@ -27,12 +27,12 @@
 
 buildPythonPackage rec {
   pname = "mlflow";
-  version = "1.25.0";
+  version = "1.25.1";
   disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-nROcV5HpCZtZa31nTmnc9rkE3YLKtRquYlf0oe1xuWI=";
+    sha256 = "sha256-jDePRRbWNz+VvFe2x8LcpD8mUNSc7lq2ucNFf8RvBgA=";
   };
 
   # run into https://stackoverflow.com/questions/51203641/attributeerror-module-alembic-context-has-no-attribute-config
