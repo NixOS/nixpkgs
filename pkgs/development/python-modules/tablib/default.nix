b0d433e931bb69e75b3a8db7057c0157c1d6642f
Mario Rodas <marsam@users.noreply.github.com>
2022-04-14 04:20:00 +0000

    python310Packages.tablib: update meta

--- pkgs/development/python-modules/tablib/default.nix
+++ pkgs/development/python-modules/tablib/default.nix
@@ -1,9 +1,12 @@
-{ buildPythonPackage, lib, fetchPypi, isPy27
+{ buildPythonPackage
+, lib
+, fetchPypi
+, isPy27
 , odfpy
 , openpyxl
 , pandas
 , setuptools-scm
-, pytest
+, pytestCheckHook
 , pytest-cov
 , pyyaml
 , unicodecsv
@@ -23,16 +26,17 @@ buildPythonPackage rec {
 
   nativeBuildInputs = [ setuptools-scm ];
   propagatedBuildInputs = [ xlwt openpyxl pyyaml xlrd odfpy ];
-  checkInputs = [ pytest pytest-cov unicodecsv pandas ];
+  checkInputs = [ pytestCheckHook pytest-cov unicodecsv pandas ];
 
   # test_tablib needs MarkupPy, which isn't packaged yet
-  checkPhase = ''
-    pytest --ignore tests/test_tablib.py
-  '';
+  pytestFlagsArray = [ "--ignore=tests/test_tablib.py" ];
+
+  pythonImportsCheck = [ "tablib" ];
 
   meta = with lib; {
     description = "Format-agnostic tabular dataset library";
-    homepage = "https://python-tablib.org";
+    homepage = "https://tablib.readthedocs.io/";
+    changelog = "https://github.com/jazzband/tablib/raw/v${version}/HISTORY.md";
     license = licenses.mit;
   };
 }
