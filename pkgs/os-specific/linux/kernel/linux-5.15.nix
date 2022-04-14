f497168eb87b6e92b44e13460f2978de3aaa7548
TredwellGit <tredwell@tutanota.com>
2022-04-14 06:02:18 +0000

    linux: 5.15.33 -> 5.15.34
    (cherry picked from commit 34a4c9124c31c09899784ccb36f0d0ab860ab0a4)

--- pkgs/os-specific/linux/kernel/linux-5.15.nix
+++ pkgs/os-specific/linux/kernel/linux-5.15.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.15.33";
+  version = "5.15.34";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -15,6 +15,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1i590npi00w5d1znmamkm3sb293k5k4xd37miwnvz7hg17k1f2n3";
+    sha256 = "0sfviwwp7qy8b5h15lg84dyskih4l082l9gs6yrqj3rg762lcld7";
   };
 } // (args.argsOverride or { }))
