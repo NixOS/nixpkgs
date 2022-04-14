e5fe7999879627f170efcb97df2cedb67402f88c
Bobby Rong <rjl931189261@126.com>
2022-04-14 18:51:25 +0800

    gnome-firmware: rename from gnome-firmware-updater

--- pkgs/applications/misc/gnome-firmware/default.nix
+++ pkgs/applications/misc/gnome-firmware/default.nix
@@ -18,13 +18,13 @@
 }:
 
 stdenv.mkDerivation rec {
-  pname = "gnome-firmware-updater";
+  pname = "gnome-firmware";
   version = "3.34.0";
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
-    owner = "hughsie";
-    repo = "gnome-firmware-updater";
+    owner = "World";
+    repo = "gnome-firmware";
     rev = version;
     sha256 = "04pll0fzf4nr276kfw89r0524s6ppmls5rz4vq2j8c8gb50g0b6l";
   };
@@ -32,11 +32,11 @@ stdenv.mkDerivation rec {
   patches = [
     # Fixes manual build
     (fetchpatch {
-      url = "https://gitlab.gnome.org/hughsie/gnome-firmware-updater/commit/006b64dcb401d8c81a33222bc4be8274c23f3c9c.patch";
+      url = "https://gitlab.gnome.org/World/gnome-firmware/commit/006b64dcb401d8c81a33222bc4be8274c23f3c9c.patch";
       sha256 = "02303ip4ri5pv1bls8c0njb00qhn0jd0d8rmvsrig0fmacwfvc06";
     })
     (fetchpatch {
-      url = "https://gitlab.gnome.org/hughsie/gnome-firmware-updater/commit/c4f076f2c902080618e0c27dec924fd0019f68a3.patch";
+      url = "https://gitlab.gnome.org/World/gnome-firmware/commit/c4f076f2c902080618e0c27dec924fd0019f68a3.patch";
       sha256 = "1yfxd7qsg3gwpamg0m2sbcfrgks59w70r9728arrc4pwx1hia2q1";
     })
   ];
@@ -66,7 +66,7 @@ stdenv.mkDerivation rec {
   ];
 
   meta = with lib; {
-    homepage = "https://gitlab.gnome.org/hughsie/gnome-firmware-updater";
+    homepage = "https://gitlab.gnome.org/World/gnome-firmware";
     description = "Tool for installing firmware on devices";
     license = licenses.gpl2Plus;
     maintainers = teams.gnome.members;
