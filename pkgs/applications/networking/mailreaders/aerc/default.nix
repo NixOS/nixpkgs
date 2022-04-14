83287e4e30b3516b1c16db241097fae645f2db0d
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-04-14 22:56:25 +0100

    aerc: simplify derivation

--- pkgs/applications/networking/mailreaders/aerc/default.nix
+++ pkgs/applications/networking/mailreaders/aerc/default.nix
@@ -40,7 +40,7 @@ buildGoModule rec {
     substituteAllInPlace doc/aerc-config.5.scd
   '';
 
-  makeFlags = [ "PREFIX=$out" ];
+  makeFlags = [ "PREFIX=${placeholder "out"}" ];
 
   pythonPath = [
     python3.pkgs.colorama
@@ -48,16 +48,11 @@ buildGoModule rec {
 
   buildInputs = [ python3 notmuch ];
 
-  buildPhase = "
-    runHook preBuild
-    # we use make instead of go build
-    runHook postBuild
-  ";
-
   installPhase = ''
     runHook preInstall
-    make PREFIX=$out GOFLAGS="$GOFLAGS -tags=notmuch" install
-    wrapPythonProgramsIn $out/share/aerc/filters "$out $pythonPath"
+
+    make $makeFlags GOFLAGS="$GOFLAGS -tags=notmuch" install
+
     runHook postInstall
   '';
 
