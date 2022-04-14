b4a160ca1250da648e8da709a55b38901e289590
TredwellGit <tredwell@tutanota.com>
2022-04-14 06:02:24 +0000

    linux: 5.16.19 -> 5.16.20
    (cherry picked from commit e70511248b567d91eefffe92b918063acc39b097)

--- pkgs/os-specific/linux/kernel/linux-5.16.nix
+++ pkgs/os-specific/linux/kernel/linux-5.16.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.16.19";
+  version = "5.16.20";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0zlvsnbmcx4l8a5cqciwaabm1267q5zn4ah3gfhxi0jvigx2qvfs";
+    sha256 = "09dz8zp8cxvsc5amrswqqrkxd3i92ay2samlcspalaw6iz40s1nq";
   };
 } // (args.argsOverride or { }))
