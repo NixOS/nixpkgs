6595b056b47b5c7b22135b54a65075590adc0e36
Luflosi <luflosi@luflosi.de>
2022-04-15 17:32:21 +0200

    ipfs: 0.11.0 -> 0.11.1
    https://github.com/ipfs/go-ipfs/releases/tag/v0.11.1

--- pkgs/applications/networking/ipfs/default.nix
+++ pkgs/applications/networking/ipfs/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "ipfs";
-  version = "0.11.0";
+  version = "0.11.1";
   rev = "v${version}";
 
   # go-ipfs makes changes to it's source tarball that don't match the git source.
   src = fetchurl {
     url = "https://github.com/ipfs/go-ipfs/releases/download/${rev}/go-ipfs-source.tar.gz";
-    sha256 = "lTPGnFqDgyMWmSCPmLHguGNnJQMWi9LPrOZfDgeS9Y4=";
+    sha256 = "CbleheuKg58aDdML6UmucJ3R85eh5Ob8jVyHEtG1pHQ=";
   };
 
   # tarball contains multiple files/directories
