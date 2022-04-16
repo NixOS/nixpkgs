c1f27518045e6e61b74ad8e9a039221ae1ae8438
sternenseemann <sternenseemann@systemli.org>
2022-04-16 20:25:23 +0200

    haskellPackages.cryptostore: 0.2.1.0 -> 0.2.2.0
    This upgrade fixes a genuine bug caught by the test suite (which wasn't
    flaky as it turns out), so we'll upgrade manually.

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -2540,8 +2540,14 @@ self: super: {
   # 2022-03-16: Upstream stopped updating bounds https://github.com/haskell-hvr/base-noprelude/pull/15
   base-noprelude = doJailbreak super.base-noprelude;
 
-  # 2022-04-12: Flaky test suite: https://github.com/ocheron/cryptostore/issues/7
-  cryptostore = dontCheck super.cryptostore;
+  # Manually upgrade cryptostore to work around
+  # https://github.com/ocheron/cryptostore/issues/7
+  cryptostore = assert super.cryptostore.version == "0.2.1.0"; overrideCabal {
+    version = "0.2.2.0";
+    sha256 = "0n70amg7y2qwfjhj4xaqjia46fbabba9l2g19ry191m7c4zp1skx";
+    revision = null;
+    editedCabalFile = null;
+  } super.cryptostore;
 
   # 2022-03-16: Bounds need to be loosened https://github.com/obsidiansystems/dependent-sum-aeson-orphans/issues/10
   dependent-sum-aeson-orphans = doJailbreak super.dependent-sum-aeson-orphans;
