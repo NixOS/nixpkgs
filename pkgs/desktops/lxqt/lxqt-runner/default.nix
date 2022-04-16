ff44f4913e4ac385e0542d0fb37c8f07a3627f95
José Romildo <malaquias@gmail.com>
2022-04-16 16:16:31 -0300

    lxqt.lxqt-runner: 1.0.0 -> 1.1.0

--- pkgs/desktops/lxqt/lxqt-runner/default.nix
+++ pkgs/desktops/lxqt/lxqt-runner/default.nix
@@ -20,13 +20,13 @@
 
 mkDerivation rec {
   pname = "lxqt-runner";
-  version = "1.0.0";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "lxqt";
     repo = pname;
     rev = version;
-    sha256 = "06b7l2jkh0h4ikddh82nxkz7qhg5ap7l016klg3jl2x659z59hpj";
+    sha256 = "hnuzoHKXqM6xEzN0jvHVjVWUXRxuwdhD3BiBfFMmZSk=";
   };
 
   nativeBuildInputs = [
