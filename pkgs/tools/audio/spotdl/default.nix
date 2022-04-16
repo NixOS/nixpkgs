5861c9e5975f63aea8850b89f0ff8f6d4db0fce1
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-16 20:44:27 +0000

    spotdl: 3.9.4 -> 3.9.5

--- pkgs/tools/audio/spotdl/default.nix
+++ pkgs/tools/audio/spotdl/default.nix
@@ -6,13 +6,13 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "spotdl";
-  version = "3.9.4";
+  version = "3.9.5";
 
   src = fetchFromGitHub {
     owner = "spotDL";
     repo = "spotify-downloader";
-    rev = "v${version}";
-    sha256 = "sha256-PJ9m+697bdrhHZ80wJvL6V366Vn3tmPfioK1sZAyB/Q=";
+    rev = "refs/tags/v${version}";
+    sha256 = "sha256-Zdm+OVcFgOO8kbJDNQSVDLnIHzhvm9EPbcG95mNrnTk=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
