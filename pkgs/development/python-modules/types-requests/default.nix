3feca9381d0653aaff903c089acbfe6736d22481
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-17 00:55:19 +0000

    python310Packages.types-requests: 2.27.16 -> 2.27.19

--- pkgs/development/python-modules/types-requests/default.nix
+++ pkgs/development/python-modules/types-requests/default.nix
@@ -6,12 +6,12 @@
 
 buildPythonPackage rec {
   pname = "types-requests";
-  version = "2.27.16";
+  version = "2.27.19";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-yAEMGLKRp++2CxRS2+ElMLwlaT3WV+cMYoA/zcS//ps=";
+    sha256 = "sha256-eV43gRcIjR5L9BosAaFTtz1upAqp18CsdTq96EwNOo8=";
   };
 
   propagatedBuildInputs = [
