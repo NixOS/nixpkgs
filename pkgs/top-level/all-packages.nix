47586fec7e92d30480411d323a88a92ff3477070
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-04-17 02:56:04 +0200

    SDL_audiolib: init at unstable-2022-04-17

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20033,6 +20033,8 @@ with pkgs;
     libGLU = null;
   });
 
+  SDL_audiolib = callPackage ../development/libraries/SDL_audiolib { };
+
   SDL_sixel = callPackage ../development/libraries/SDL_sixel { };
 
   SDL_gfx = callPackage ../development/libraries/SDL_gfx { };
