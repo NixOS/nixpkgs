e6b6a9d2a2df011dec0ef39774a7bc3cbda7a866
Nick Cao <nickcao@nichi.co>
2022-04-18 23:19:05 +0800

    fcitx5-qt: add support for qt6 applications

--- pkgs/tools/inputmethods/fcitx5/fcitx5-qt.nix
+++ pkgs/tools/inputmethods/fcitx5/fcitx5-qt.nix
@@ -8,6 +8,7 @@
 , libxcb
 , libXdmcp
 , qtbase
+, qt6
 }:
 
 mkDerivation rec {
@@ -24,11 +25,15 @@ mkDerivation rec {
   preConfigure = ''
     substituteInPlace qt5/platforminputcontext/CMakeLists.txt \
       --replace \$"{CMAKE_INSTALL_QT5PLUGINDIR}" $out/${qtbase.qtPluginPrefix}
+    substituteInPlace qt6/platforminputcontext/CMakeLists.txt \
+      --replace \$"{CMAKE_INSTALL_QT6PLUGINDIR}" $out/${qt6.qtbase.qtPluginPrefix}
   '';
 
   cmakeFlags = [
+    # adding qt6 to buildInputs would result in error: detected mismatched Qt dependencies
+    "-DCMAKE_PREFIX_PATH=${qt6.qtbase.dev}"
     "-DENABLE_QT4=0"
-    "-DENABLE_QT6=0"
+    "-DENABLE_QT6=1"
   ];
 
   nativeBuildInputs = [
