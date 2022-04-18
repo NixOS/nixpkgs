edc25332f47bafc510ffe4aa14a0e495d4afb190
AndersonTorres <torres.anderson.85@protonmail.com>
2022-04-18 00:29:00 -0300

    ltris: 1.0.19 -> 1.2.4

--- pkgs/games/lgames/ltris/default.nix
+++ pkgs/games/lgames/ltris/default.nix
@@ -2,21 +2,25 @@
 
 stdenv.mkDerivation rec {
   pname = "ltris";
-  version = "1.0.19";
-  buildInputs = [ SDL SDL_mixer ];
+  version = "1.2.4";
 
   src = fetchurl {
     url = "mirror://sourceforge/lgames/${pname}-${version}.tar.gz";
-    sha256 = "1895wv1fqklrj4apkz47rnkcfhfav7zjknskw6p0886j35vrwslg";
+    hash = "sha256-1ut7MBAjJ4YE2SkqkvL9L1ED7kEqdaEm0lUOLsI2j4M=";
   };
 
-  patchPhase = "patch -p0 < ${./gcc5_compliance.diff}";
+  buildInputs = [
+    SDL
+    SDL_mixer
+  ];
+
+  hardeningDisable = [ "format" ];
 
   meta = with lib; {
+    homepage = "https://lgames.sourceforge.io/LTris/";
     description = "Tetris clone from the LGames series";
-    homepage = "http://lgames.sourceforge.net/LBreakout2/";
-    license = licenses.gpl2;
-    maintainers = [ maintainers.ciil ];
-    platforms = platforms.linux;
+    license = licenses.gpl2Plus;
+    maintainers = with maintainers; [ AndersonTorres ciil ];
+    inherit (SDL.meta) platforms;
   };
 }
