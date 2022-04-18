6e8e1faabeb492f70404c95e33b7d314ae3017e2
Izorkin <izorkin@elven.pw>
2022-04-18 13:22:39 +0300

    nixos/tests: add nginx-http3 test

--- pkgs/servers/http/nginx/generic.nix
+++ pkgs/servers/http/nginx/generic.nix
@@ -165,7 +165,7 @@ stdenv.mkDerivation {
   passthru = {
     modules = modules;
     tests = {
-      inherit (nixosTests) nginx nginx-auth nginx-etag nginx-pubhtml nginx-sandbox nginx-sso;
+      inherit (nixosTests) nginx nginx-auth nginx-etag nginx-http3 nginx-pubhtml nginx-sandbox nginx-sso;
       variants = lib.recurseIntoAttrs nixosTests.nginx-variants;
       acme-integration = nixosTests.acme;
     } // passthru.tests;
