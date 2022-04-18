b19b12d33569c2bccabc1dd2d3046a9937f94c1c
Mitchell Amihod <mitchell@amihod.com>
2022-04-17 23:54:01 -0400

    asdf-vm: 0.9.0 -> 0.10.0

--- pkgs/tools/misc/asdf-vm/default.nix
+++ pkgs/tools/misc/asdf-vm/default.nix
@@ -37,13 +37,13 @@ ${asdfReshimFile}
   '';
 in stdenv.mkDerivation rec {
   pname = "asdf-vm";
-  version = "0.9.0";
+  version = "0.10.0";
 
   src = fetchFromGitHub {
     owner = "asdf-vm";
     repo = "asdf";
     rev = "v${version}";
-    sha256 = "sha256-uvf3WVflUdh+M90zMDVFVAXi7TZytc8oZ/DexJqpNV4=";
+    sha256 = "sha256-yWQ74tVYBZszuuE3PaDB758kCjKO6oBOG/Z+T3+82po=";
   };
 
   nativeBuildInputs = [
