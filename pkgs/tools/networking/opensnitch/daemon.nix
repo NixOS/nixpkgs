c8b86b83ff9f7d2f5ad28dde7f63aec977746ec5
Jonas Heinrich <onny@project-insanity.org>
2022-04-19 15:21:57 +0200

    opensnitch: 1.5.0 -> 1.5.1

--- pkgs/tools/networking/opensnitch/daemon.nix
+++ pkgs/tools/networking/opensnitch/daemon.nix
@@ -15,13 +15,13 @@
 
 buildGoModule rec {
   pname = "opensnitch";
-  version = "1.5.0";
+  version = "1.5.1";
 
   src = fetchFromGitHub {
     owner = "evilsocket";
     repo = "opensnitch";
     rev = "v${version}";
-    sha256 = "sha256-vtD82v0VlaJtCICXduD3IxJ0xjlBuzGKLWLoCiwPX2I=";
+    sha256 = "sha256-8IfupmQb1romGEvv/xqFkYhp0gGoY4ZEllX6rZYIkqw=";
   };
 
   patches = [
