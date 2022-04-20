f99e505cccc5d3bb862ed60e8c861eb576950b27
Artturin <Artturin@artturin.com>
2022-04-20 18:43:21 +0300

    testers: convert to a format that is kind of compatible with nixdoc
    Examples: support has to be added to https://github.com/nix-community/nixdoc
    
    'nixdoc --category testers --description "nixpkgs testers" --file ./pkgs/build-support/testers/default.nix'

--- pkgs/build-support/testers/default.nix
+++ pkgs/build-support/testers/default.nix
@@ -1,31 +1,47 @@
 { pkgs, lib, callPackage, runCommand }:
 {
+
+  /* Checks that two packages produce the exact same build instructions.
+
+     This can be used to make sure that a certain difference of configuration,
+     such as the presence of an overlay does not cause a cache miss.
+
+     When the derivations are equal, the return value is an empty file.
+     Otherwise, the build log explains the difference via `nix-diff`.
+
+     Example:
+
+         testEqualDerivation
+           "The hello package must stay the same when enabling checks."
+           hello
+           (hello.overrideAttrs(o: { doCheck = true; }))
+  */
   testEqualDerivation = callPackage ./test-equal-derivation.nix { };
 
   /* Checks the command output contains the specified version
-   *
-   * Although simplistic, this test assures that the main program
-   * can run. While there's no substitute for a real test case,
-   * it does catch dynamic linking errors and such. It also provides
-   * some protection against accidentally building the wrong version,
-   * for example when using an 'old' hash in a fixed-output derivation.
-   *
-   * Examples:
-   *
-   * passthru.tests.version = testVersion { package = hello; };
-   *
-   * passthru.tests.version = testVersion {
-   *   package = seaweedfs;
-   *   command = "weed version";
-   * };
-   *
-   * passthru.tests.version = testVersion {
-   *   package = key;
-   *   command = "KeY --help";
-   *   # Wrong '2.5' version in the code. Drop on next version.
-   *   version = "2.5";
-   * };
-   */
+
+     Although simplistic, this test assures that the main program
+     can run. While there's no substitute for a real test case,
+     it does catch dynamic linking errors and such. It also provides
+     some protection against accidentally building the wrong version,
+     for example when using an 'old' hash in a fixed-output derivation.
+
+     Examples:
+
+       passthru.tests.version = testVersion { package = hello; };
+
+       passthru.tests.version = testVersion {
+         package = seaweedfs;
+         command = "weed version";
+       };
+
+       passthru.tests.version = testVersion {
+         package = key;
+         command = "KeY --help";
+         # Wrong '2.5' version in the code. Drop on next version.
+         version = "2.5";
+       };
+  */
   testVersion =
     { package,
       command ? "${package.meta.mainProgram or package.pname or package.name} --version",
