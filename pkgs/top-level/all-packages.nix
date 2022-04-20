b043af988c84066221cbbe5a9e2e9278221f472d
Anna Aurora <anna@annaaurora.eu>
2022-04-20 16:31:17 +0200

    peazip: init at 9.9.0
    Co-authored-by: linsui <36977733+linsui@users.noreply.github.com>
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>
    Co-authored-by: Nikolay Korotkiy <sikmir@disroot.org>
    Co-authored-by: Thiago Kenji Okada <thiagokokada@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34495,6 +34495,8 @@ with pkgs;
     stdenv = gccStdenv;
   };
 
+  peazip = libsForQt5.callPackage ../tools/archivers/peazip { };
+
   peek = callPackage ../applications/video/peek { };
 
   peertube = callPackage ../servers/peertube {
