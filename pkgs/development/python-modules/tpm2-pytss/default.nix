cca6307ae4f22177d47d9b9aebedbebb090f46bb
Congee <congeec@qq.com>
2022-04-21 13:01:09 -0400

    python3Packages.tpm2-pytss: fix build 1.1.0

--- pkgs/development/python-modules/tpm2-pytss/default.nix
+++ pkgs/development/python-modules/tpm2-pytss/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, fetchPypi, pythonOlder
+{ lib, buildPythonPackage, fetchPypi, pythonOlder, python3, python3Packages
 , pkg-config, swig
 , tpm2-tss
 , cryptography, ibm-sw-tpm2
@@ -7,22 +7,17 @@
 buildPythonPackage rec {
   pname = "tpm2-pytss";
 
-  # Last version on github is 0.2.4, but it looks
-  # like a mistake (it's missing commits from 0.1.9)
   version = "1.1.0";
   disabled = pythonOlder "3.5";
+  format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "sha256-O0d1b99/V8b3embg8veerTrJGSVb/prlPVb7qSHErdQ=";
   };
-  postPatch = ''
-    substituteInPlace tpm2_pytss/config.py --replace \
-      'SYSCONFDIR = CONFIG.get("sysconfdir", "/etc")' \
-      'SYSCONFDIR = "${tpm2-tss}/etc"'
-  '';
 
-  nativeBuildInputs = [ pkg-config swig ];
+  buildInputs = with python3.pkgs; [ pkgconfig asn1crypto ];
+  nativeBuildInputs = [ swig ];
   # The TCTI is dynamically loaded from tpm2-tss, we have to provide the library to the end-user
   propagatedBuildInputs = [ tpm2-tss ];
 
