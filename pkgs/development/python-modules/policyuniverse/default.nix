0779a04aadd6ac02864f1d71ca0c320815a847b5
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-21 06:09:31 +0000

    python310Packages.policyuniverse: 1.5.0.20220416 -> 1.5.0.20220420

--- pkgs/development/python-modules/policyuniverse/default.nix
+++ pkgs/development/python-modules/policyuniverse/default.nix
@@ -6,12 +6,12 @@
 
 buildPythonPackage rec {
   pname = "policyuniverse";
-  version = "1.5.0.20220416";
+  version = "1.5.0.20220420";
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-RGXOqhXPsEQR5wcXDWz00J6KcdJOY5ZXSLVXT18wDDk=";
+    sha256 = "sha256-HWyzwvsn3ikL5cbEPljMHShF2vQEOSP6umk08UZgQsI=";
   };
 
   # Tests are not shipped and there are no GitHub tags
