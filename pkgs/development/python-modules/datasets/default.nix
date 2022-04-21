4d33027198a83923ddf384275457488e41533fea
Fabian Affolter <mail@fabian-affolter.ch>
2022-04-21 10:25:52 +0200

    python3Packages.datasets: update inputs

--- pkgs/development/python-modules/datasets/default.nix
+++ pkgs/development/python-modules/datasets/default.nix
@@ -1,15 +1,19 @@
 { lib
+, aiohttp
 , buildPythonPackage
-, fetchFromGitHub
 , dill
-, filelock
+, fetchFromGitHub
 , fsspec
 , huggingface-hub
+, importlib-metadata
 , multiprocess
 , numpy
+, packaging
 , pandas
 , pyarrow
+, pythonOlder
 , requests
+, responses
 , tqdm
 , xxhash
 }:
@@ -17,26 +21,33 @@
 buildPythonPackage rec {
   pname = "datasets";
   version = "1.18.3";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "huggingface";
     repo = pname;
     rev = version;
-    sha256 = "sha256-2x6DpsDcVF2O5iJKeMEGw/aJwZPc7gSGaK2947c3B6s=";
+    hash = "sha256-2x6DpsDcVF2O5iJKeMEGw/aJwZPc7gSGaK2947c3B6s=";
   };
 
   propagatedBuildInputs = [
+    aiohttp
     dill
-    filelock
     fsspec
     huggingface-hub
     multiprocess
     numpy
+    packaging
     pandas
     pyarrow
     requests
+    responses
     tqdm
     xxhash
+  ] ++ lib.optionals (pythonOlder "3.8") [
+    importlib-metadata
   ];
 
   # Tests require pervasive internet access.
@@ -45,11 +56,13 @@ buildPythonPackage rec {
   # Module import will attempt to create a cache directory.
   postFixup = "export HF_MODULES_CACHE=$TMPDIR";
 
-  pythonImportsCheck = [ "datasets" ];
+  pythonImportsCheck = [
+    "datasets"
+  ];
 
   meta = with lib; {
+    description = "Open-access datasets and evaluation metrics for natural language processing";
     homepage = "https://github.com/huggingface/datasets";
-    description = "Fast, efficient, open-access datasets and evaluation metrics for natural language processing";
     changelog = "https://github.com/huggingface/datasets/releases/tag/${version}";
     license = licenses.asl20;
     platforms = platforms.unix;
