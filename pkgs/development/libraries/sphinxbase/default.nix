054c075c2e5ebc9fb4bd82ef88510adc2cb8184f
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-04-21 12:20:19 -0500

    sphinxbase: 5prealpha -> unstable-2022-02-21

--- pkgs/development/libraries/sphinxbase/default.nix
+++ pkgs/development/libraries/sphinxbase/default.nix
@@ -1,27 +1,30 @@
 { lib, stdenv
-, fetchurl
+, autoreconfHook
+, fetchFromGitHub
 , bison
 , pkg-config
-, python27 # >= 2.6
+, python3
 , swig2 # 2.0
 , multipleOutputs ? false #Uses incomplete features of nix!
 }:
 
 stdenv.mkDerivation (rec {
   pname = "sphinxbase";
-  version = "5prealpha";
+  version = "unstable-2022-02-21";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/cmusphinx/sphinxbase-${version}.tar.gz";
-    sha256 = "0vr4k8pv5a8nvq9yja7kl13b5lh0f9vha8fc8znqnm8bwmcxnazp";
+  src = fetchFromGitHub {
+    owner = "cmusphinx";
+    repo = "sphinxbase";
+    rev = "3612ab4e8de9172e2b7e2401f4ca1ad122c29fde";
+    hash = "sha256-O/Olx7S+g4DftN/ahVXr2dQUtoQe4hxQUsz8Z7We0RI=";
   };
 
-  nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ swig2 python27 bison ];
+  nativeBuildInputs = [ pkg-config autoreconfHook ];
+  buildInputs = [ swig2 python3 bison ];
 
   meta = {
     description = "Support Library for Pocketsphinx";
-    homepage = "http://cmusphinx.sourceforge.net";
+    homepage = "https://cmusphinx.github.io";
     license = lib.licenses.bsd2;
     platforms = lib.platforms.unix;
     maintainers = with lib.maintainers; [ ];
