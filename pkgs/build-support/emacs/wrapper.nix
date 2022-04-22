8a615fb52c19ee614f11d1ddb88feb927ef641d2
Mario Rodas <marsam@users.noreply.github.com>
2022-04-22 04:20:00 +0000

    emacsWrapper: add missing variable substitutions to Darwin app

--- pkgs/build-support/emacs/wrapper.nix
+++ pkgs/build-support/emacs/wrapper.nix
@@ -227,6 +227,8 @@ runCommand
       substitute ${./wrapper.sh} $out/Applications/Emacs.app/Contents/MacOS/Emacs \
         --subst-var-by bash ${emacs.stdenv.shell} \
         --subst-var-by wrapperSiteLisp "$deps/share/emacs/site-lisp" \
+        --subst-var-by wrapperSiteLispNative "$deps/share/emacs/native-lisp:" \
+        --subst-var-by autoloadExpression "-l cl-loaddefs -l nix-generated-autoload" \
         --subst-var-by prog "$emacs/Applications/Emacs.app/Contents/MacOS/Emacs"
       chmod +x $out/Applications/Emacs.app/Contents/MacOS/Emacs
     fi
