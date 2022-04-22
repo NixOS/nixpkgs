06e28994aa5cd36b9714f17d4703200632f5a91f
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-22 14:46:09 +0000

    python310Packages.pytest-mpl: 0.14.0 -> 0.15.0

--- pkgs/development/python-modules/pytest-mpl/default.nix
+++ pkgs/development/python-modules/pytest-mpl/default.nix
@@ -12,11 +12,11 @@
 
 buildPythonPackage rec {
   pname = "pytest-mpl";
-  version = "0.14.0";
+  version = "0.15.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-iE4HjS1TgK9WQzhOIzw1jpZZgl+y2X/9r48YXENMjYk=";
+    sha256 = "sha256-p5/UKLVoDYclp2o/MBb2oX1pHzxsQpHjmfwU1kFSKbw=";
   };
 
   nativeBuildInputs = [
