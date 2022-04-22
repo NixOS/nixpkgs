2c6f925ea9a1631b06106873e6293ca9b442e5ba
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-04-22 17:48:10 -0500

    pyrseas: 0.8.0 -> 0.9.1

--- pkgs/development/tools/database/pyrseas/default.nix
+++ pkgs/development/tools/database/pyrseas/default.nix
@@ -1,39 +1,39 @@
-{ lib, python2Packages, fetchFromGitHub }:
+{ lib, python3Packages, fetchFromGitHub }:
 
 let
-  pgdbconn = python2Packages.buildPythonPackage rec {
+  pgdbconn = python3Packages.buildPythonPackage rec {
     pname = "pgdbconn";
     version = "0.8.0";
     src = fetchFromGitHub {
       owner = "perseas";
       repo = "pgdbconn";
       rev = "v${version}";
-      sha256 = "09r4idk5kmqi3yig7ip61r6js8blnmac5n4q32cdcbp1rcwzdn6z";
+      hash = "sha256-39j2OcvhLtaYGJjYwlS1dCEtTQ7mxvOiHxHXWWaLJCc=";
     };
     # The tests are impure (they try to access a PostgreSQL server)
     doCheck = false;
     propagatedBuildInputs = [
-      python2Packages.psycopg2
-      python2Packages.pytest
+      python3Packages.psycopg2
+      python3Packages.pytest
     ];
   };
 in
 
-python2Packages.buildPythonApplication {
+python3Packages.buildPythonApplication rec {
   pname = "pyrseas";
-  version = "0.8.0";
+  version = "0.9.1";
   src = fetchFromGitHub {
     owner = "perseas";
     repo = "Pyrseas";
-    rev = "2e9be763e61168cf20d28bd69010dc5875bd7b97";
-    sha256 = "1h9vahplqh0rzqjsdq64qqar6hj1bpbc6nl1pqwwgca56385br8r";
+    rev = version;
+    hash = "sha256-+MxnxvbLMxK1Ak+qKpKe3GHbzzC+XHO0eR7rl4ON9H4=";
   };
   # The tests are impure (they try to access a PostgreSQL server)
   doCheck = false;
   propagatedBuildInputs = [
-    python2Packages.psycopg2
-    python2Packages.pytest
-    python2Packages.pyyaml
+    python3Packages.psycopg2
+    python3Packages.pytest
+    python3Packages.pyyaml
     pgdbconn
   ];
   meta = {
