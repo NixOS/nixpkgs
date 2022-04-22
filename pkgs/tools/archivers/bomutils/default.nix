38b069872dcea45079f9989a56df1cb168d96427
Alexander Bich <code@quyse.net>
2022-04-23 00:20:38 +0500

    bomutils: fix build with gcc 11

--- pkgs/tools/archivers/bomutils/default.nix
+++ pkgs/tools/archivers/bomutils/default.nix
@@ -18,6 +18,8 @@ stdenv.mkDerivation rec {
     "CXX=${stdenv.cc.targetPrefix}c++"
   ];
 
+  NIX_CFLAGS_COMPILE = ["-std=c++14"];
+
   meta = with lib; {
     homepage = "https://github.com/hogliux/bomutils";
     description = "Open source tools to create bill-of-materials files used in macOS installers";
