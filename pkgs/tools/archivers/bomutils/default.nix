2226a08e910c5ce40792503a46f7d38af527b9b9
Pavol Rusnak <pavol@rusnak.io>
2022-04-23 10:27:43 +0200

    Update default.nix

--- pkgs/tools/archivers/bomutils/default.nix
+++ pkgs/tools/archivers/bomutils/default.nix
@@ -18,7 +18,10 @@ stdenv.mkDerivation rec {
     "CXX=${stdenv.cc.targetPrefix}c++"
   ];
 
-  NIX_CFLAGS_COMPILE = ["-std=c++14"];
+  # fix
+  # src/lsbom.cpp:70:10: error: reference to 'data' is ambiguous
+  # which refers to std::data from C++17
+  NIX_CFLAGS_COMPILE = [ "-std=c++14" ];
 
   meta = with lib; {
     homepage = "https://github.com/hogliux/bomutils";
