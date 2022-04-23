0bf4f7d46f5a89977b986e66eecb90f864ab789c
Giulio De Pasquale <depasquale+git@giugl.io>
2022-04-24 00:50:07 +0200

    libvorbis: Remove flags not recognized by clang

--- pkgs/development/libraries/libvorbis/default.nix
+++ pkgs/development/libraries/libvorbis/default.nix
@@ -5,7 +5,8 @@ stdenv.mkDerivation rec {
   version = "1.3.7";
 
   src = fetchurl {
-    url = "http://downloads.xiph.org/releases/vorbis/${pname}-${version}.tar.xz";
+    url =
+      "http://downloads.xiph.org/releases/vorbis/${pname}-${version}.tar.xz";
     sha256 = "0jwmf87x5sdis64rbv0l87mdpah1rbilkkxszipbzg128f9w8g5k";
   };
 
@@ -15,6 +16,10 @@ stdenv.mkDerivation rec {
   propagatedBuildInputs = [ libogg ];
 
   doCheck = true;
+
+  preConfigure = lib.optionalString (stdenv.cc.isClang or false) ''
+    sed s/\-mno\-ieee\-fp// -i {configure,configure.ac}
+  '';
 
   meta = with lib; {
     description = "Vorbis audio compression reference implementation";
