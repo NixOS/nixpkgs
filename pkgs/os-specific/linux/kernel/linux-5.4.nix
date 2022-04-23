b0d8ca58e54242b4c6bffb7aea39de9fbc293c40
Maximilian Bosch <maximilian@mbosch.me>
2022-04-23 13:36:07 +0200

    linux: 5.4.188 -> 5.4.190

--- pkgs/os-specific/linux/kernel/linux-5.4.nix
+++ pkgs/os-specific/linux/kernel/linux-5.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.4.188";
+  version = "5.4.190";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1g7xf2jx1hx580f42yirfgv9v0f9f88wzxxx0wiwx7wcqbyqpg4z";
+    sha256 = "157ifcl59xxj721r302hg82vmbqzx5hjrlihrc5s4maxfw3ygm41";
   };
 } // (args.argsOverride or {}))
