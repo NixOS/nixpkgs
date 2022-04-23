c7b93bcdd1899399f92f9e5eb1f1fbf8b38e62c0
Tom <twitchyliquid64@ciphersink.net>
2022-04-22 21:16:55 -0700

    tailscale: 1.22.2 -> 1.24.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22463,7 +22463,9 @@ with pkgs;
 
   systemd-journal2gelf = callPackage ../tools/system/systemd-journal2gelf { };
 
-  tailscale = callPackage ../servers/tailscale { };
+  tailscale = callPackage ../servers/tailscale {
+    buildGoModule = buildGo118Module;
+  };
 
   thanos = callPackage ../servers/monitoring/thanos { };
 
