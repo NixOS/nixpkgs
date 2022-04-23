1f083b9f45a2b42c762f9819e6ae278719adfdcf
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-04-23 09:34:17 -0500

    apacheHttpdPackages.mod_python: 3.5.0 -> unstable-2022-04-18

--- pkgs/servers/http/apache-modules/mod_python/default.nix
+++ pkgs/servers/http/apache-modules/mod_python/default.nix
@@ -1,31 +1,33 @@
-{ lib, stdenv, fetchurl, apacheHttpd, python2, libintl }:
+{ lib, stdenv, fetchFromGitHub, apacheHttpd, python3, libintl }:
 
 stdenv.mkDerivation rec {
   pname = "mod_python";
-  version = "3.5.0";
+  version = "unstable-2022-04-18";
 
-  src = fetchurl {
-    url = "http://dist.modpython.org/dist/${pname}-${version}.tgz";
-    sha256 = "146apll3yfqk05s8fkf4acmxzqncl08bgn4rv0c1rd4qxmc91w0f";
+  src = fetchFromGitHub {
+    owner = "grisha";
+    repo = "mod_python";
+    rev = "8fded887b664a5c57b6b015fbd82ef7ca9c31021";
+    hash = "sha256-IFSWZoVqTwNaW9qh78uojy5GFG4mn4vfgG5OKN+1bjw=";
   };
-
-  patches = [ ./install.patch ];
 
   postPatch = ''
     substituteInPlace dist/version.sh \
-        --replace 'GIT=`git describe --always`' "" \
-        --replace '-$GIT' ""
+        --replace '$GIT' ""
   '';
 
-  installFlags = [ "LIBEXECDIR=${placeholder "out"}/modules" ];
+  installFlags = [
+    "DESTDIR=${placeholder "out"}"
+  ];
 
-  preInstall = ''
-    mkdir -p $out/modules $out/bin
+  postInstall = ''
+    mv $out/nix/store/*/* $out/
+    rm -rf $out/nix
   '';
 
   passthru = { inherit apacheHttpd; };
 
-  buildInputs = [ apacheHttpd python2 ]
+  buildInputs = [ apacheHttpd python3 ]
     ++ lib.optional stdenv.isDarwin libintl;
 
   meta = {
