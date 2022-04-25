9b4f18aeee4ef233d3b1da78f8000f8c227bb524
Jonas Heinrich <onny@project-insanity.org>
2022-04-25 15:15:56 +0200

    libsForQt5.mauikit-accounts: init at 2.1.1

--- pkgs/top-level/qt5-packages.nix
+++ pkgs/top-level/qt5-packages.nix
@@ -146,6 +146,8 @@ in (kdeFrameworks // plasmaMobileGear // plasma5 // plasma5.thirdParty // kdeGea
 
   mauikit = callPackage ../development/libraries/mauikit { };
 
+  mauikit-accounts = callPackage ../development/libraries/mauikit-accounts { };
+
   mauikit-filebrowsing = callPackage ../development/libraries/mauikit-filebrowsing { };
 
   mlt = callPackage ../development/libraries/mlt/qt-5.nix { };
