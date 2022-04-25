0e4e53559c9099e28607e807b7e0611ec9c30b5f
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-04-25 09:56:43 -0500

    lvtk: 1.2.0 -> unstable-2022-01-19

--- pkgs/development/libraries/audio/lvtk/default.nix
+++ pkgs/development/libraries/audio/lvtk/default.nix
@@ -1,30 +1,20 @@
-{ lib, stdenv, fetchFromGitHub, boost, gtkmm2, lv2, pkg-config, python2, wafHook }:
+{ lib, stdenv, fetchFromGitHub, boost, gtkmm2, lv2, pkg-config, python3, wafHook }:
 
 stdenv.mkDerivation rec {
   pname = "lvtk";
-  version = "1.2.0";
+  version = "unstable-2022-01-19";
 
   src = fetchFromGitHub {
     owner = "lvtk";
     repo = "lvtk";
-    rev = version;
-    sha256 = "sha256-6IoyhBig3Nvc4Y8F0w8b1up6sn8O2RmoUVaBQ//+Aaw=";
+    rev = "a73feabe772f9650aa071e6a4df660e549ab7c48";
+    hash = "sha256-UukdDortjSAgMKknRtkfaXGUvIQM4xZEQ6hSSRbalWk=";
   };
 
-  nativeBuildInputs = [ pkg-config python2 wafHook ];
-  buildInputs = [ boost gtkmm2 lv2 ];
+  nativeBuildInputs = [ pkg-config python3 wafHook ];
+  buildInputs = [ lv2 ];
 
   enableParallelBuilding = true;
-
-  # Fix including the boost libraries during linking
-  postPatch = ''
-    sed -i '/target[ ]*= "ttl2c"/ ilib=["boost_system"],' tools/wscript_build
-  '';
-
-  wafConfigureFlags = [
-    "--boost-includes=${boost.dev}/include"
-    "--boost-libs=${boost.out}/lib"
-  ];
 
   meta = with lib; {
     description = "A set C++ wrappers around the LV2 C API";
