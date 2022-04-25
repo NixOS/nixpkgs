fece5997c0cd1a18fa8cd1466fa3a49503cef2d4
superherointj <5861043+superherointj@users.noreply.github.com>
2022-04-25 17:56:49 -0300

    mockgen: fix tests

--- pkgs/development/tools/mockgen/default.nix
+++ pkgs/development/tools/mockgen/default.nix
@@ -1,18 +1,23 @@
-{ buildGoModule, lib, fetchFromGitHub }:
+{ buildGoModule, fetchFromGitHub, lib }:
+
 buildGoModule rec {
   pname = "mockgen";
   version = "1.6.0";
+
   src = fetchFromGitHub {
     owner = "golang";
     repo = "mock";
     rev = "v${version}";
     sha256 = "sha256-5Kp7oTmd8kqUN+rzm9cLqp9nb3jZdQyltGGQDiRSWcE=";
   };
+
   vendorSha256 = "sha256-5gkrn+OxbNN8J1lbgbxM8jACtKA7t07sbfJ7gVJWpJM=";
 
-  doCheck = false;
-
   subPackages = [ "mockgen" ];
+
+  preCheck = ''
+    export GOROOT="$(go env GOROOT)"
+  '';
 
   meta = with lib; {
     description = "GoMock is a mocking framework for the Go programming language";
