c6bb3f7e55d5bf2075eb04640aebfd58dc38d90c
lxb <liuxiaobo666233@gmail.com>
2022-04-25 15:02:46 +0800

    Add imbox in python-modules.

---
+++ pkgs/development/python-modules/imbox/default.nix
@@ -0,0 +1,33 @@
+{ lib, buildPythonPackage, fetchFromGitHub, setuptools, chardet }:
+
+buildPythonPackage rec {
+  pname = "imbox";
+  version = "0.1.0";
+
+  # pypi source doesn't contain tests
+  src = fetchFromGitHub {
+    owner = "martinrusev";
+    repo = "imbox";
+    rev = "73ef2267ee6f9af927c30be68009552fc2b0bdbe";
+    sha256 = "sha256-4qMchhpkZK3cmrtRSd18PLaz/vaIE+jBIjfJnI11SWk=";
+  };
+
+  patches = [
+    ./patches/Support-to-connect-163-mail-server.patch
+    ./patches/Support-to-parse-chinese-attachment-file-name.patch
+  ];
+
+  propagatedBuildInputs = [ setuptools chardet ];
+
+  pytestFlagsArray = [ "-n" "$NIX_BUILD_CORES" ];
+
+  doCheck = false;
+
+  meta = with lib; {
+    description =
+      "Python library for reading IMAP mailboxes and converting email content to machine readable data";
+    homepage = "https://github.com/martinrusev/imbox";
+    license = licenses.mit;
+    maintainers = with maintainers; [ lhh ];
+  };
+}
