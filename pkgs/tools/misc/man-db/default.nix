108cac596ef1597d2c22bc0700bdfc4f1ee28985
Sandro <sandro.jaeckel@gmail.com>
2022-04-26 01:16:16 +0200

    man-db: simplify wrapper

--- pkgs/tools/misc/man-db/default.nix
+++ pkgs/tools/misc/man-db/default.nix
@@ -68,8 +68,7 @@ stdenv.mkDerivation rec {
     # make sure that we don't wrap symlinks (since that changes argv[0] to the -wrapped name)
     find "$out/bin" -type f | while read file; do
       wrapProgram "$file" \
-        --prefix PATH : "${lib.getBin groff}/bin" \
-        --prefix PATH : "${lib.getBin zstd}/bin"
+        --prefix PATH : "${lib.makeBinPath [ groff zstd ]}"
     done
   '';
 
