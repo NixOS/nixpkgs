7ac6f7279d7a37664f68b6c8d7b61ad638d558fa
Kasper Gałkowski <kpg@posteo.net>
2022-04-25 23:12:22 +0200

    add alternative lisp-modules implementation
    based on https://github.com/Uthar/nix-cl/commit/4c4cef812e67677a55a2a949e49dd109ae1fcf93

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21331,6 +21331,9 @@ with pkgs;
   quicklispPackagesGCL = dontRecurseIntoAttrs (quicklispPackagesFor (wrapLisp gcl));
   quicklispPackages = quicklispPackagesSBCL;
 
+  # Alternative lisp-modules implementation
+  lispPackages_new = callPackage ../development/lisp-modules-new/lisp-packages.nix {};
+
   ### DEVELOPMENT / PERL MODULES
 
   perlInterpreters = callPackages ../development/interpreters/perl {};
