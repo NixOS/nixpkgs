410de20eaeca782ad8fe623cf465d37359221f93
Fabian Affolter <mail@fabian-affolter.ch>
2022-04-25 23:49:57 +0200

    python3Packages.flake8-bugbear: disable on older Python releases

--- pkgs/development/python-modules/flake8-bugbear/default.nix
+++ pkgs/development/python-modules/flake8-bugbear/default.nix
@@ -11,12 +11,15 @@
 buildPythonPackage rec {
   pname = "flake8-bugbear";
   version = "22.4.25";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "PyCQA";
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-y/hpBlIQ3aJj3Y1snpArOCIv2w1ncQNMSYJ+G0CeM84=";
+    hash = "sha256-y/hpBlIQ3aJj3Y1snpArOCIv2w1ncQNMSYJ+G0CeM84=";
   };
 
   propagatedBuildInputs = [
@@ -32,9 +35,10 @@ buildPythonPackage rec {
   ];
 
   meta = with lib; {
+    description = "Plugin for Flake8 to find bugs and design problems";
     homepage = "https://github.com/PyCQA/flake8-bugbear";
     changelog = "https://github.com/PyCQA/flake8-bugbear/blob/${version}/README.rst#change-log";
-    description = ''
+    longDescription = ''
       A plugin for flake8 finding likely bugs and design problems in your
       program.
     '';
