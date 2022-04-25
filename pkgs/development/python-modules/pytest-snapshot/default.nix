b2e2247f8bc7d92a08a41eec2f9ca860ff597fba
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-25 05:11:30 +0000

    python310Packages.pytest-snapshot: 0.8.1 -> 0.9.0

--- pkgs/development/python-modules/pytest-snapshot/default.nix
+++ pkgs/development/python-modules/pytest-snapshot/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "pytest-snapshot";
-  version = "0.8.1";
+  version = "0.9.0";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "joseph-roitman";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-ymB9h3Mc6GSMRsxIh6uuf111qLvIkNAnFsxR6O2SF7s=";
+    rev = "refs/tags/v${version}";
+    sha256 = "sha256-0PZu9wL29iEppLxxbl4D0E4WfOHe61KUUld003cRBRU=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
