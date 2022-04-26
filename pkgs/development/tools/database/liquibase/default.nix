3ffe1ccca03d732f4b5cb435ed4be7cef86a29c2
Julien Debon <julien.debon@tweag.io>
2022-04-26 09:55:14 +0200

    liquibase: add support for Amazon Redshift
    The Liquibase package already provided PostgreSQL and MySQL support, this commit
    also adds support for Amazon Redshift.
    
    As per Liquibase Redshift documentation, 2 JARs must be added to the classpath:
    * Redshift JDBC
    * Liquibase Redshift extension JAR

--- pkgs/development/tools/database/liquibase/default.nix
+++ pkgs/development/tools/database/liquibase/default.nix
@@ -1,11 +1,25 @@
-{ lib, stdenv, fetchurl, jre, makeWrapper
-, mysqlSupport ? true, mysql_jdbc
-, postgresqlSupport ? true, postgresql_jdbc }:
+{ lib
+, stdenv
+, fetchurl
+, jre
+, makeWrapper
+, mysqlSupport ? true
+, mysql_jdbc
+, postgresqlSupport ? true
+, postgresql_jdbc
+, redshiftSupport ? true
+, redshift_jdbc
+, liquibase_redshift_extension
+}:
 
 let
   extraJars =
     lib.optional mysqlSupport mysql_jdbc
-    ++ lib.optional postgresqlSupport postgresql_jdbc;
+    ++ lib.optional postgresqlSupport postgresql_jdbc
+    ++ lib.optionals redshiftSupport [
+      redshift_jdbc
+      liquibase_redshift_extension
+    ];
 in
 
 stdenv.mkDerivation rec {
@@ -30,7 +44,8 @@ stdenv.mkDerivation rec {
         CP="\$CP":"\$jar"
       done
     '';
-    in ''
+    in
+    ''
       mkdir -p $out
       mv ./{lib,licenses,liquibase.jar} $out/
 
@@ -54,7 +69,7 @@ stdenv.mkDerivation rec {
         liquibase.integration.commandline.Main \''${1+"\$@"}
       EOF
       chmod +x $out/bin/liquibase
-  '';
+    '';
 
   meta = with lib; {
     description = "Version Control for your database";
