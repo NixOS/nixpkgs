fb18ee26ee9c634c6bcc8482c0a9755775a9b4e9
José Romildo <malaquias@gmail.com>
2022-04-26 20:02:23 -0300

    xfce.xfce4-terminal: 1.0.1 -> 1.0.2

--- pkgs/desktops/xfce/applications/xfce4-terminal/default.nix
+++ pkgs/desktops/xfce/applications/xfce4-terminal/default.nix
@@ -3,9 +3,9 @@
 mkXfceDerivation {
   category = "apps";
   pname = "xfce4-terminal";
-  version = "1.0.1";
+  version = "1.0.2";
 
-  sha256 = "sha256-+tuJGA/g0ddDop5mTbRZGVGUKKZ87fmdVMOaaBhMpFQ=";
+  sha256 = "sha256-d69SjV7dkiYmc0R5OyU5DV5RM3h51an4idWV872QoE4=";
 
   nativeBuildInputs = [ libxslt docbook_xml_dtd_45 docbook_xsl ];
 
