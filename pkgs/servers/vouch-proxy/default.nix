7665c2e599ee8247d662de92d9412931c626b053
squalus <squalus@squalus.net>
2022-04-25 20:48:28 -0700

    vouch-proxy: 0.36.0 -> 0.37.0

--- pkgs/servers/vouch-proxy/default.nix
+++ pkgs/servers/vouch-proxy/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "vouch-proxy";
-  version = "0.36.0";
+  version = "0.37.0";
 
   src = fetchFromGitHub {
     owner = "vouch";
     repo = "vouch-proxy";
     rev = "v${version}";
-    sha256 = "sha256-pgoxRzYc5PIrxnRwWpthFmpsxDfvWTmLT7upQVIFoQo=";
+    sha256 = "0rcc5b3v5d9v4y78z5fnjbn1k10xy8cpgxjhqc7j22k9wkic05mh";
   };
 
-  vendorSha256 = "sha256-ifH+420FIrib+zQtzzHtMMYd84BED+vgnRw4xToYIl4=";
+  vendorSha256 = "0pi230xcaf0wkphfn3s4h3riviihxlqwyb9lzfdvh8h5dpizxwc9";
 
   ldflags = [
     "-s" "-w"
