554a16c410f2bf141e8b7132aa073ec260acd858
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-26 02:28:53 +0000

    evolution: 3.44.0 -> 3.44.1

--- pkgs/applications/networking/mailreaders/evolution/evolution/default.nix
+++ pkgs/applications/networking/mailreaders/evolution/evolution/default.nix
@@ -46,11 +46,11 @@
 
 stdenv.mkDerivation rec {
   pname = "evolution";
-  version = "3.44.0";
+  version = "3.44.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/evolution/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "3yHT31Ik36hC6ikO/82QKv1LFBhgik37aQejt9TZlPk=";
+    sha256 = "dEx+CK0R4bYQPO60u/2Jo7Yo4SbOOGe7AI80F8wEnqk=";
   };
 
   nativeBuildInputs = [
