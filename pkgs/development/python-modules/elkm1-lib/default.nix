7302d016713db843d2918949e7225e65d76c3e5a
Fabian Affolter <mail@fabian-affolter.ch>
2022-04-26 22:19:17 +0200

    python39Packages.elkm1-lib: 1.3.4 -> 1.3.5

--- pkgs/development/python-modules/elkm1-lib/default.nix
+++ pkgs/development/python-modules/elkm1-lib/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "elkm1-lib";
-  version = "1.3.4";
+  version = "1.3.5";
   format = "pyproject";
 
   disabled = pythonOlder "3.6";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "gwww";
     repo = "elkm1";
     rev = version;
-    hash = "sha256-g8tMVH1MZihEEHGQjxX/Mcn5Mu3N9VA3AGdk2avlOoE=";
+    hash = "sha256-sYE7bZcMXsf+4r9Rs4d0XaUqdWVZfDU9/xPqIiiJkUQ=";
   };
 
   nativeBuildInputs = [
