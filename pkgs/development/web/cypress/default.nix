4d12f939d4cd5e7cde66a5a92363d6cb61b7ca9a
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-26 01:53:07 +0000

    cypress: 9.5.4 -> 9.6.0

--- pkgs/development/web/cypress/default.nix
+++ pkgs/development/web/cypress/default.nix
@@ -16,11 +16,11 @@
 
 stdenv.mkDerivation rec {
   pname = "cypress";
-  version = "9.5.4";
+  version = "9.6.0";
 
   src = fetchzip {
     url = "https://cdn.cypress.io/desktop/${version}/linux-x64/cypress.zip";
-    sha256 = "F4BSIA3ImXwmmki8/FK0t08Gf5S8KMpXNNBIPPJQNsM=";
+    sha256 = "Mac6lmwQqbj9EPfpG0iLI8Qx04q7E0swmTqXx0J+Sdc=";
   };
 
   # don't remove runtime deps
