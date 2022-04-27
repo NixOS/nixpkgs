0c494de2fd173be1abcf6ce8356221fb16a43a07
Bart Brouns <bart@magnetophon.nl>
2022-04-27 19:04:09 +0200

    airwindows-lv2: init at 1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1071,6 +1071,8 @@ with pkgs;
 
   airspyhf = callPackage ../applications/radio/airspyhf { };
 
+  airwindows-lv2 = callPackage ../applications/audio/airwindows-lv2 { };
+
   aj-snapshot  = callPackage ../applications/audio/aj-snapshot { };
 
   ajour = callPackage ../tools/games/ajour {
