8d8f0eafdeece480db9f8078e913b17ccf4209ae
zowoq <59103226+zowoq@users.noreply.github.com>
2022-04-28 07:40:38 +1000

    fx: 20.0.2 -> 22.0.10
    https://github.com/antonmedv/fx/releases/tag/21.0.0
    > This is the first version of fx implemented in go.
    
    https://github.com/antonmedv/fx/releases/tag/22.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3474,7 +3474,7 @@ with pkgs;
 
   futhark = haskell.lib.compose.justStaticExecutables haskellPackages.futhark;
 
-  inherit (nodePackages) fx;
+  fx = callPackage ../development/tools/fx { };
 
   tllist = callPackage ../development/libraries/tllist { };
 
