650633fc0861777a0939506c6ab46a3a197061c6
Jayesh Bhoot <jayesh@bhoot.sh>
2022-04-27 15:34:47 +0530

    kmousetool: init at 21.12.3

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -5604,6 +5604,11 @@
     githubId = 488556;
     name = "Javier Aguirre";
   };
+  jayesh-bhoot = {
+    name = "Jayesh Bhoot";
+    email = "jayesh@bhoot.sh";
+    github = "jayesh-bhoot";
+  };
   jb55 = {
     email = "jb55@jb55.com";
     github = "jb55";
