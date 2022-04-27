b20934a65c0541c2b83b688de24e3991b3db426e
Vincent Laporte <Vincent.Laporte@gmail.com>
2022-04-27 11:22:33 +0200

    easycrypt: init at 2022.04

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3362,6 +3362,8 @@ with pkgs;
 
   earlyoom = callPackage ../os-specific/linux/earlyoom { };
 
+  easycrypt = callPackage ../applications/science/logic/easycrypt { };
+
   EBTKS = callPackage ../development/libraries/science/biology/EBTKS { };
 
   ecasound = callPackage ../applications/audio/ecasound { };
