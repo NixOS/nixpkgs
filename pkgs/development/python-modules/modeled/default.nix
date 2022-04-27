845fb25789e207861b8bccb3f91420ab46be722a
Fabian Affolter <mail@fabian-affolter.ch>
2022-04-27 18:01:22 +0200

    python3Packages.modeled: adjust dependency name

--- pkgs/development/python-modules/modeled/default.nix
+++ pkgs/development/python-modules/modeled/default.nix
@@ -1,16 +1,20 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, zetup
-, six
 , moretools
 , path
 , pytestCheckHook
+, pythonOlder
+, six
+, zetup
 }:
 
 buildPythonPackage rec {
   pname = "modeled";
   version = "0.1.8";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     extension = "zip";
@@ -18,18 +22,37 @@ buildPythonPackage rec {
     sha256 = "1wcl3r02q10gxy4xw7g8x2wg2sx4sbawzbfcl7a5xdydrxl4r4v4";
   };
 
-  buildInputs = [ zetup ];
+  buildInputs = [
+    zetup
+  ];
 
-  propagatedBuildInputs = [ six moretools path ];
+  propagatedBuildInputs = [
+    moretools
+    path
+    six
+  ];
 
-  checkInputs = [ pytestCheckHook ];
+  checkInputs = [
+    pytestCheckHook
+  ];
 
-  pythonImportsCheck = [ "modeled" ];
+  postPatch = ''
+    # path.py was renamed to path
+    substituteInPlace requirements.txt \
+      --replace "path.py" "path"
+    # Issue with module name detection (path.py)
+    substituteInPlace modeled/__init__.py  \
+      --replace "__import__('zetup').annotate(__name__)" ""
+  '';
+
+  pythonImportsCheck = [
+    "modeled"
+  ];
 
   meta = with lib; {
     description = "Universal data modeling for Python";
     homepage = "https://github.com/modeled/modeled";
-    license = licenses.lgpl3Only;
-    maintainers = [ maintainers.costrouc ];
+    license = licenses.lgpl3Plus;
+    maintainers = with maintainers; [ costrouc ];
   };
 }
