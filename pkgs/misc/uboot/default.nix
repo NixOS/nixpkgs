059427718385d23e125ddfbb630eda0d6fbc4178
Bernardo Meurer <bernardo@meurer.org>
2022-04-26 19:09:06 -0700

    ubootRaspberryPi4_64bit: remove NVMe support patches

--- pkgs/misc/uboot/default.nix
+++ pkgs/misc/uboot/default.nix
@@ -412,31 +412,6 @@ in {
     defconfig = "rpi_4_defconfig";
     extraMeta.platforms = ["aarch64-linux"];
     filesToInstall = ["u-boot.bin"];
-    extraConfig = ''
-      CONFIG_CMD_NVME=y
-      CONFIG_NVME=y
-      CONFIG_NVME_PCI=y
-      CONFIG_USB_STORAGE=y
-      CONFIG_USB_FUNCTION_MASS_STORAGE=y
-      CONFIG_USB_EHCI_HCD=y
-      CONFIG_USB_EHCI_GENERIC=y
-      CONFIG_USB_OHCI_HCD=y
-
-      CONFIG_USB_XHCI_BRCM=y
-    '';
-    # These were taken from:
-    # https://github.com/home-assistant/operating-system/tree/dev/buildroot-external/board/raspberrypi/patches/uboot
-    # https://github.com/home-assistant/operating-system/tree/dev/buildroot-external/board/raspberrypi/yellow/patches/uboot
-    extraPatches = [
-      ./rpi-cm4/0001-rpi-add-NVMe-to-boot-order.patch
-      ./rpi-cm4/0002-Revert-nvme-Correct-the-prps-per-page-calculation-me.patch
-      ./rpi-cm4/0003-usb-xhci-brcm-Make-driver-compatible-with-downstream.patch
-      ./rpi-cm4/0004-drivers-bcm283x-don-t-explicitly-disable-init.patch
-      ./rpi-cm4/0005-drivers-bcm283x-allow-to-spawn-any-PL011-UART.patch
-      ./rpi-cm4/0006-nvme-improve-readability-of-nvme_setup_prps.patch
-      ./rpi-cm4/0007-nvme-Use-pointer-for-CPU-addressed-buffers.patch
-      ./rpi-cm4/0008-nvme-translate-virtual-addresses-into-the-bus-s-addr.patch
-    ];
   };
 
   ubootRaspberryPiZero = buildUBoot {
