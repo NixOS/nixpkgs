e2e571f6b93bdff718b113e38b878c7b9ba2f3d2
Jayesh Bhoot <jayesh@bhoot.sh>
2022-04-28 10:28:09 +0530

    kmousetool: move meta attrset to the bottom

--- pkgs/applications/kde/kmousetool.nix
+++ pkgs/applications/kde/kmousetool.nix
@@ -14,12 +14,6 @@
 
 mkDerivation {
   pname = "kmousetool";
-  meta = {
-    homepage = "https://github.com/KDE/kmousetool";
-    description = "Program that clicks the mouse for you";
-    license = with lib.licenses; [ gpl2 fdl12 ];
-    maintainers = [ lib.maintainers.jayesh-bhoot ];
-  };
   nativeBuildInputs = [ extra-cmake-modules kdoctools ];
   buildInputs = [
     ki18n
@@ -31,5 +25,11 @@ mkDerivation {
     libXtst
     libXt
   ];
+  meta = {
+    homepage = "https://github.com/KDE/kmousetool";
+    description = "Program that clicks the mouse for you";
+    license = with lib.licenses; [ gpl2 fdl12 ];
+    maintainers = [ lib.maintainers.jayesh-bhoot ];
+  };
 }
 
