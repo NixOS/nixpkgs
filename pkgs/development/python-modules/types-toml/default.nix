c7282002e7ad24552330817b6f674c76345ab2f6
R. Ryantm <ryantm-bot@ryantm.com>
2022-04-28 05:33:28 +0000

    python310Packages.types-toml: 0.10.5 -> 0.10.6

--- pkgs/development/python-modules/types-toml/default.nix
+++ pkgs/development/python-modules/types-toml/default.nix
@@ -5,12 +5,12 @@
 
 buildPythonPackage rec {
   pname = "types-toml";
-  version = "0.10.5";
+  version = "0.10.6";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-+UlGOjiCAL45Lvk5nbrT4eO75tRUPYStk9lsP9oCgIA=";
+    sha256 = "sha256-aus+17+oaTgVUcd07WqRQuPu2YqD76UQipYy6v1W3NM=";
   };
 
   # Module doesn't have tests
