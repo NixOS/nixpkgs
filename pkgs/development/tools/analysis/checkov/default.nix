7a5cbdf6f739626e11c52d0a1775445f5cc08c5a
Fabian Affolter <mail@fabian-affolter.ch>
2022-04-28 23:50:30 +0200

    checkov: 2.0.1088 -> 2.0.1098

--- pkgs/development/tools/analysis/checkov/default.nix
+++ pkgs/development/tools/analysis/checkov/default.nix
@@ -32,13 +32,13 @@ with py.pkgs;
 
 buildPythonApplication rec {
   pname = "checkov";
-  version = "2.0.1088";
+  version = "2.0.1098";
 
   src = fetchFromGitHub {
     owner = "bridgecrewio";
     repo = pname;
     rev = version;
-    hash = "sha256-g4kn8tdLgTnOFmeX8nywBvmAXL7WqCfX6ylgp7Z6Alk=";
+    hash = "sha256-CFH7zb3f50tlYhRiZ/q0b8PDhithVhaeRTtsXvan+bw=";
   };
 
   nativeBuildInputs = with py.pkgs; [
