e659d57c66074edfcbd0820d8a92fe1aa5d3d9b8
Sandro <sandro.jaeckel@gmail.com>
2022-04-28 14:55:29 +0200

    Update pkgs/tools/misc/dsq/default.nix

--- pkgs/tools/misc/dsq/default.nix
+++ pkgs/tools/misc/dsq/default.nix
@@ -58,8 +58,7 @@ buildGoModule rec {
   };
 
   meta = with lib; {
-    description =
-      "Commandline tool for running SQL queries against JSON, CSV, Excel, Parquet, and more";
+    description = "Commandline tool for running SQL queries against JSON, CSV, Excel, Parquet, and more";
     homepage = "https://github.com/multiprocessio/dsq";
     license = licenses.asl20;
     maintainers = with maintainers; [ liff ];
