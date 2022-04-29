c2ff9af725a5b4f90d209f7100bb6787ab30dc53
Matthew Penner <me@matthewp.io>
2022-04-29 13:20:29 -0600

    mutagen-compose: init at 0.14.0

---
+++ pkgs/tools/misc/mutagen-compose/default.nix
@@ -0,0 +1,27 @@
+{ lib, buildGo118Module, fetchFromGitHub, fetchzip }:
+
+buildGo118Module rec {
+  pname = "mutagen-compose";
+  version = "0.14.0";
+
+  src = fetchFromGitHub {
+    owner = "mutagen-io";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-FNV4X9/BdnfFGKVJXNpCJLdr3Y29LrGi+zUuQ07xUbE=";
+  };
+
+  vendorSha256 = "sha256-5nt9YHMgaRpkFdOnBTU4gSdOtB3h9Cj5CCUjx9PJ/m8=";
+
+  doCheck = false;
+
+  subPackages = [ "cmd/mutagen-compose" ];
+
+  meta = with lib; {
+    description = "Compose with Mutagen integration";
+    homepage = "https://mutagen.io/";
+    changelog = "https://github.com/mutagen-io/mutagen-compose/releases/tag/v${version}";
+    maintainers = [ maintainers.matthewpi ];
+    license = licenses.mit;
+  };
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8405,6 +8405,8 @@ with pkgs;
 
   mutagen = callPackage ../tools/misc/mutagen { };
 
+  mutagen-compose = callPackage ../tools/misc/mutagen-compose { };
+
   mycli = callPackage ../tools/admin/mycli { };
 
   mycrypto = callPackage ../applications/blockchains/mycrypto { };
