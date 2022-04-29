c2fe259aa6d2254b25853f4cafdf1567e7329bba
sternenseemann <sternenseemann@systemli.org>
2022-04-29 11:21:18 +0200

    release-haskell.nix: clean up testing of static haskell packages
    * Test the default pkgsStatic.haskellPackages set, as it should be
      working properly now.
    
    * Instead of GHC 8.10.7 which hasn't gotten some workarounds applied,
      test GHC 9.2.2 additionally which should work as well as GHC 9.0.
    
    * Test building an actual executable, cabal2nix.
    
    * Simplify aggregate job definition.
    
    * Make sure GHC 9.2 lens gets built on Hydra.

--- pkgs/development/haskell-modules/configuration-ghc-9.2.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-9.2.x.nix
@@ -171,7 +171,7 @@ self: super: {
   ];
 
   # lens >= 5.1 supports 9.2.1
-  lens = super.lens_5_1;
+  lens = doDistribute self.lens_5_1;
 
   # Syntax error in tests fixed in https://github.com/simonmar/alex/commit/84b29475e057ef744f32a94bc0d3954b84160760
   alex = dontCheck super.alex;
