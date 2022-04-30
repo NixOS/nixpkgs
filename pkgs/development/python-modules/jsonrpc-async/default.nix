31b631cbb52e787bb4611a45c07ad33131ee177e
Fabian Affolter <mail@fabian-affolter.ch>
2022-04-30 13:19:47 +0200

    python310Packages.jsonrpc-async: 2.0.0 -> 2.1.0

--- pkgs/development/python-modules/jsonrpc-async/default.nix
+++ pkgs/development/python-modules/jsonrpc-async/default.nix
@@ -1,32 +1,53 @@
 { lib
+, aiohttp
 , buildPythonPackage
 , fetchFromGitHub
-, aiohttp
+, fetchpatch
 , jsonrpc-base
 , pytest-aiohttp
 , pytestCheckHook
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "jsonrpc-async";
-  version = "2.0.0";
+  version = "2.1.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "emlove";
     repo = pname;
     rev = version;
-    sha256 = "1ff3523rwgira5llmf5iriwqag7b6ln9vmj0s70yyc6k98yg06rp";
+    hash = "sha256-Lr8gvQR0Q46b/e1K/XyvqtJo18nBpHjlDdNq4vjCMyU=";
   };
 
-  propagatedBuildInputs = [ aiohttp jsonrpc-base ];
+  propagatedBuildInputs = [
+    aiohttp
+    jsonrpc-base
+  ];
 
   checkInputs = [
     pytest-aiohttp
     pytestCheckHook
   ];
 
+  patches = [
+    # Fix tests with later pytest-aiohttp, https://github.com/emlove/jsonrpc-async/pull/9
+    (fetchpatch {
+      name = "support-later-pytest-aiohttp.patch";
+      url = "https://github.com/emlove/jsonrpc-async/commit/8b790f23af0d898df90460029d5ba3bcfb0423ed.patch";
+      sha256 = "sha256-rthHRF90hywMIbvIHo3Do/uzXKe+STPOoZIa80H4b/g=";
+    })
+  ];
+
   pytestFlagsArray = [
     "tests.py"
+  ];
+
+  pythonImportsCheck = [
+    "jsonrpc_async"
   ];
 
   meta = with lib; {
