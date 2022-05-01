03cac69a04d76e1d468ec8ed8bf47b1a6a7d3a17
Zhaofeng Li <hello@zhaofeng.li>
2022-05-01 00:46:23 -0700

    python3Packages.preprocess-cancellation: Fetch from PyPi

--- pkgs/development/python-modules/preprocess-cancellation/default.nix
+++ pkgs/development/python-modules/preprocess-cancellation/default.nix
@@ -1,26 +1,20 @@
-{ lib, fetchFromGitHub, buildPythonPackage, pythonOlder, poetry-core
-, pytestCheckHook, pytest-cov
+{ lib, fetchPypi, buildPythonPackage, pythonOlder
 , shapely }:
 
 buildPythonPackage rec {
   pname = "preprocess-cancellation";
   version = "0.2.0";
   disabled = pythonOlder "3.6"; # >= 3.6
-  format = "pyproject";
 
-  # No tests in PyPI
-  src = fetchFromGitHub {
-    owner = "kageurufu";
-    repo = "cancelobject-preprocessor";
-    rev = version;
-    hash = "sha256-mn3/etXA5dkL+IsyxwD4/XjU/t4/roYFVyqQxlLOoOI=";
+  src = fetchPypi {
+    inherit version;
+    pname = "preprocess_cancellation";
+    hash = "sha256-62hJTjXAof6DcW8qFOErPhze35RYdSvhys4A+UTZB2A=";
   };
-
-  nativeBuildInputs = [ poetry-core ];
 
   propagatedBuildInputs = [ shapely ];
 
-  checkInputs = [ pytestCheckHook pytest-cov ];
+  pythonImportsCheck = [ "preprocess_cancellation" ];
 
   meta = with lib; {
     description = "Klipper GCode Preprocessor for Object Cancellation";
