5561de6b331d5f2d3c57dc5ca41807fb0b1e719d
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-01 22:27:33 +0200

    python310Packages.sphinxcontrib-spelling: add pythonImportsCheck

--- pkgs/development/python-modules/sphinxcontrib-spelling/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-spelling/default.nix
@@ -6,28 +6,43 @@
 , sphinx
 , pyenchant
 , pbr
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "sphinxcontrib-spelling";
   version = "7.3.3";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-OBnRJinZXgyQkiT6QLRipn4K2zIdUCg9f8DRFobIrH4=";
+    hash = "sha256-OBnRJinZXgyQkiT6QLRipn4K2zIdUCg9f8DRFobIrH4=";
   };
 
-  propagatedBuildInputs = [ sphinx pyenchant pbr ]
-    ++ lib.optionals (pythonOlder "3.8") [ importlib-metadata ];
+  nativeBuildInputs = [
+    pbr
+  ];
+
+  propagatedBuildInputs = [
+    sphinx
+    pyenchant
+  ] ++ lib.optionals (pythonOlder "3.8") [
+    importlib-metadata
+  ];
 
   # No tests included
   doCheck = false;
 
+  pythonImportsCheck = [
+    "sphinxcontrib.spelling"
+  ];
+
   meta = with lib; {
     description = "Sphinx spelling extension";
-    homepage = "https://bitbucket.org/dhellmann/sphinxcontrib-spelling";
-    maintainers = with maintainers; [ ];
+    homepage = "https://github.com/sphinx-contrib/spelling";
     license = licenses.bsd2;
+    maintainers = with maintainers; [ ];
   };
-
 }
