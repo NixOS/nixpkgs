c9cb28e97cfe0320459f0364c6f3484cb69a7abb
milahu <milahu@gmail.com>
2022-05-02 17:30:38 +0200

    gstreamermm: 1.10.0 -> unstable-2022-05-03

--- pkgs/development/libraries/gstreamer/gstreamermm/default.nix
+++ pkgs/development/libraries/gstreamer/gstreamermm/default.nix
@@ -1,26 +1,63 @@
-{ lib, stdenv, fetchurl, pkg-config, file, glibmm, gst_all_1, gnome }:
+{ lib
+, stdenv
+, fetchFromGitLab
+, autoconf
+, automake
+, libtool
+, perl
+, mm-common
+, pkg-config
+, file
+, glibmm
+, gst_all_1
+, nix-update-script
+}:
+
 stdenv.mkDerivation rec {
   pname = "gstreamermm";
-  version = "1.10.0";
+  version = "unstable-2022-05-03";
 
-  src = fetchurl {
-    url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0q4dx9sncqbwgpzma0zvj6zssc279yl80pn8irb95qypyyggwn5y";
+  src = fetchFromGitLab {
+    # https://gitlab.gnome.org/GNOME/gstreamermm
+    domain = "gitlab.gnome.org";
+    owner = "GNOME";
+    repo = "gstreamermm";
+    # https://gitlab.gnome.org/GNOME/gstreamermm/-/merge_requests/5
+    rev = "89b2ece5cab32b5094686bb96426a004029a9449";
+    sha256 = "sha256-b6719eTiBJ21eNA/tgul6GuAkKF0gmsbbG2x9NAtW3s=";
   };
 
   outputs = [ "out" "dev" ];
 
-  nativeBuildInputs = [ pkg-config file ];
+  nativeBuildInputs = [
+    autoconf
+    automake
+    libtool
+    mm-common
+    pkg-config
+    file
+    (perl.withPackages (p: [ p.XMLParser ]))
+  ];
 
-  propagatedBuildInputs = [ glibmm gst_all_1.gst-plugins-base ];
+  propagatedBuildInputs = [
+    glibmm
+    gst_all_1.gst-plugins-base
+  ];
 
   enableParallelBuilding = true;
 
+  preConfigure = ''
+    NOCONFIGURE=1 ./autogen.sh
+  '';
+
+  configureFlags = [
+    # Enable generating of binding code (pre-generated C++ files not available in Git).
+    "--enable-maintainer-mode"
+  ];
+
   passthru = {
-    updateScript = gnome.updateScript {
+    updateScript = nix-update-script {
       attrPath = "gst_all_1.gstreamermm";
-      packageName = "gstreamermm";
-      versionPolicy = "odd-unstable";
     };
   };
 
@@ -31,5 +68,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.linux;
     maintainers = with maintainers; [ romildo ];
   };
-
 }
