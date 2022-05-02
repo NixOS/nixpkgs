2a0b704f16f1984e65c661345f267e72014a1d33
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-02 21:57:08 +0200

    python310Packages.pims: ignore DeprecationWarning

--- pkgs/development/python-modules/pims/default.nix
+++ pkgs/development/python-modules/pims/default.nix
@@ -38,6 +38,11 @@ buildPythonPackage rec {
     "pims"
   ];
 
+  pytestFlagsArray = [
+    "-W"
+    "ignore::DeprecationWarning"
+  ];
+
   disabledTests = [
     # NotImplementedError: Do not know how to deal with infinite readers
     "TestVideo_ImageIO"
