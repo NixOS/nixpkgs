2efd1090cf5931bd2e0f328713919ca4bc8adad7
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-04 16:23:18 +0200

    rich-cli: 1.5.1 -> 1.7.0

--- pkgs/misc/rich-cli/default.nix
+++ pkgs/misc/rich-cli/default.nix
@@ -1,17 +1,20 @@
-{ lib, python3 }:
+{ lib
+, python3
+}:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "rich-cli";
-  version = "1.5.1";
+  version = "1.7.0";
+  format = "pyproject";
 
   src = python3.pkgs.fetchPypi {
     inherit pname version;
-    sha256 = "18qpdaw4drkwq71xikngwaarkjxhfc0nrb1zm36rw31b8dz0ij2k";
+    sha256 = "sha256-fporylec9H+9G2v8D0O32ek7OQs3YRSma1xOpakClqk=";
   };
 
-  format = "pyproject";
-
-  nativeBuildInputs = [ python3.pkgs.poetry-core ];
+  nativeBuildInputs = with python3.pkgs; [
+    poetry-core
+  ];
 
   propagatedBuildInputs = with python3.pkgs; [
     rich
@@ -21,11 +24,18 @@ python3.pkgs.buildPythonApplication rec {
     rich-rst
   ];
 
-  buildInputs = [ python3 ];
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace 'rich = "^12.3.0"' 'rich = "*"'
+  '';
+
+  pythonImportsCheck = [
+    "rich_cli"
+  ];
 
   meta = with lib; {
-    homepage = "https://github.com/Textualize/rich-cli";
     description = "Command Line Interface to Rich";
+    homepage = "https://github.com/Textualize/rich-cli";
     license = licenses.mit;
     maintainers = with maintainers; [ jyooru ];
   };
