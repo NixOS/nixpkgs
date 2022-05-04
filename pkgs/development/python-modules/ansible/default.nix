b4049c2bc99ccddc81df2afce011d49f10a0c8f5
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-05-04 23:49:59 +0200

    python3Packages.ansible: add meta.changelog
    This is annoying to find so add it here and now.

--- pkgs/development/python-modules/ansible/default.nix
+++ pkgs/development/python-modules/ansible/default.nix
@@ -78,6 +78,7 @@ buildPythonPackage {
   meta = with lib; {
     description = "Radically simple IT automation";
     homepage = "https://www.ansible.com";
+    changelog = "https://github.com/ansible-community/ansible-build-data/blob/${version}/${lib.versions.major version}/CHANGELOG-v${lib.versions.major version}.rst";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ hexa ];
   };
