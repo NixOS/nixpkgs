e2100d5148b83b66d498cb22540a04ea3b28ca2f
Samuel Ainsworth <skainsworth@gmail.com>
2022-05-04 18:26:08 +0000

    python39Packages.nbclient: 0.5.13 -> 0.6.2

--- pkgs/development/python-modules/nbclient/default.nix
+++ pkgs/development/python-modules/nbclient/default.nix
@@ -1,27 +1,65 @@
-{ lib, buildPythonPackage, fetchPypi, pythonOlder,
-  async_generator, traitlets, nbformat, nest-asyncio, jupyter-client,
-  pytest, xmltodict, nbconvert, ipywidgets
+{ async_generator
+, buildPythonPackage
 , doCheck ? true
+, fetchFromGitHub
+, ipywidgets
+, jupyter-client
+, lib
+, nbconvert
+, nbformat
+, nest-asyncio
+, pytest-asyncio
+, pytestCheckHook
+, pythonOlder
+, traitlets
+, xmltodict
 }:
 
 buildPythonPackage rec {
   pname = "nbclient";
-  version = "0.5.13";
+  version = "0.6.2";
+  # As of version 0.6.2, there is both a setup.py and pyproject.toml. However,
+  # the pyproject.toml does not appear to be the main entry point.
+  format = "setuptools";
+
   disabled = pythonOlder "3.7";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-QMUsm148MfrsruafICs/U+ONfBxWPeD63enX7aD9r+g=";
+  src = fetchFromGitHub {
+    owner = "jupyter";
+    repo = pname;
+    rev = "refs/tags/v${version}";
+    hash = "sha256-5mBlc/DhjBiBAytwOQvB5WO2AHbIJQSpyww7ar8ONbo=";
   };
 
+  propagatedBuildInputs = [
+    async_generator
+    jupyter-client
+    nbformat
+    nest-asyncio
+    traitlets
+  ];
+
   inherit doCheck;
-  checkInputs = [ pytest xmltodict nbconvert ipywidgets ];
-  propagatedBuildInputs = [ async_generator traitlets nbformat nest-asyncio jupyter-client ];
+
+  preCheck = ''
+    export HOME=$(mktemp -d)
+  '';
+
+  checkInputs = [
+    ipywidgets
+    nbconvert
+    pytest-asyncio
+    pytestCheckHook
+    xmltodict
+  ];
+
+  # For some reason this still doesn't fix https://github.com/NixOS/nixpkgs/issues/171493.
+  pythonImportsCheck = [ "nbclient" ];
 
   meta = with lib; {
     homepage = "https://github.com/jupyter/nbclient";
     description = "A client library for executing notebooks";
     license = licenses.bsd3;
-    maintainers = [ maintainers.erictapen ];
+    maintainers = with maintainers; [ erictapen ];
   };
 }
