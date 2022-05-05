62793de6f40f3ef3643565a36eea91fad6aec598
Robert Schütz <nix@dotlambda.de>
2022-05-05 06:25:48 +0000

    wapiti: update propagatedBuildInputs

--- pkgs/tools/security/wapiti/default.nix
+++ pkgs/tools/security/wapiti/default.nix
@@ -29,20 +29,18 @@ python3.pkgs.buildPythonApplication rec {
     dnspython
     httpcore
     httpx
-    httpx-ntlm
-    httpx-socks
     humanize
     loguru
     Mako
     markupsafe
-    pysocks
     six
     sqlalchemy
     tld
     yaswfp
   ] ++ lib.optionals (python3.pythonOlder "3.8") [
     importlib-metadata
-  ];
+  ] ++ httpx.extras-require.brotli
+    ++ httpx.extras-require.socks;
 
   checkInputs = with python3.pkgs; [
     respx
@@ -52,8 +50,6 @@ python3.pkgs.buildPythonApplication rec {
 
   postPatch = ''
     # Ignore pinned versions
-    substituteInPlace setup.py \
-      --replace "httpx-socks[asyncio] == 0.6.0" "httpx-socks[asyncio]"
     sed -i -e "s/==[0-9.]*//;s/>=[0-9.]*//" setup.py
     substituteInPlace setup.cfg \
       --replace " --cov --cov-report=xml" ""
