6bffe188e6f85409612273a8088817d6833de660
R. Ryantm <ryantm-bot@ryantm.com>
2022-05-05 15:02:57 +0000

    python310Packages.databricks-connect: 9.1.14 -> 9.1.15

--- pkgs/development/python-modules/databricks-connect/default.nix
+++ pkgs/development/python-modules/databricks-connect/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "databricks-connect";
-  version = "9.1.14";
+  version = "9.1.15";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-l+mTqiQPuPJfGbEVSILpCTlxAka0GeCgIXjMG4Vs82o=";
+    sha256 = "sha256-qXS/hgF2qKUtTfo9UZ5KBa9N0PHJqKA8SC/vgE46LmA=";
   };
 
   sourceRoot = ".";
