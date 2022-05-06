36c879d18cb3f4e2f6dcdd5e7d646c69138187c1
Jeroen Versteeg <jv@1nnovat.io>
2022-05-06 18:23:47 +0200

    python3Packages.pandoc-xnos: init at 2.5.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5886,6 +5886,8 @@ in {
 
   pandoc-attributes = callPackage ../development/python-modules/pandoc-attributes { };
 
+  pandoc-xnos = callPackage ../development/python-modules/pandoc-xnos { };
+
   pandocfilters = callPackage ../development/python-modules/pandocfilters { };
 
   panel = callPackage ../development/python-modules/panel { };
