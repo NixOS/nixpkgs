f4e5bd806439fb7010c4962295a786d61d4af305
Maximilian Bosch <maximilian@mbosch.me>
2022-05-06 16:56:47 +0200

    nixos/nixos-generate-config: update comment for useDHCP

--- nixos/modules/installer/tools/nixos-generate-config.pl
+++ nixos/modules/installer/tools/nixos-generate-config.pl
@@ -585,8 +585,8 @@ sub generateNetworkingDhcpConfig {
     my $config = <<EOF;
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to explicitly set it
-  # per-interface using `networking.interfaces.<interface>.useDHCP`.
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
 EOF
 
