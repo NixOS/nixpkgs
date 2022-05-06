a081afe9e978a09ec99219cf704ff3082dc45313
Samuel Ainsworth <skainsworth@gmail.com>
2022-05-06 01:48:31 +0000

    python3Packages.nbconvert: clean up

--- pkgs/development/python-modules/nbconvert/default.nix
+++ pkgs/development/python-modules/nbconvert/default.nix
@@ -1,25 +1,19 @@
-{ lib
-, buildPythonPackage
-, fetchPypi
-, pytestCheckHook
-, glibcLocales
-, entrypoints
+{ beautifulsoup4
 , bleach
+, buildPythonPackage
+, defusedxml
+, fetchPypi
+, ipywidgets
+, jinja2
+, jupyterlab-pygments
+, lib
+, markupsafe
 , mistune
 , nbclient
-, jinja2
-, pygments
-, traitlets
-, jupyter_core
-, jupyterlab-pygments
-, nbformat
-, ipykernel
 , pandocfilters
-, tornado
-, jupyter-client
-, defusedxml
+, pyppeteer
+, pytestCheckHook
 , tinycss2
-, beautifulsoup4
 }:
 
 buildPythonPackage rec {
@@ -42,37 +36,38 @@ buildPythonPackage rec {
     substituteAllInPlace ./nbconvert/exporters/templateexporter.py
   '';
 
-  checkInputs = [ pytestCheckHook glibcLocales ];
-
   propagatedBuildInputs = [
-    entrypoints bleach mistune jinja2 pygments traitlets
-    jupyter_core nbformat ipykernel pandocfilters tornado jupyter-client
-    defusedxml tinycss2 beautifulsoup4
-    nbclient
+    beautifulsoup4
+    bleach
+    defusedxml
+    jinja2
     jupyterlab-pygments
+    markupsafe
+    mistune
+    nbclient
+    pandocfilters
+    tinycss2
   ];
 
-  # disable preprocessor tests for ipython 7
-  # see issue https://github.com/jupyter/nbconvert/issues/898
   preCheck = ''
-    export LC_ALL=en_US.UTF-8
-    HOME=$(mktemp -d)
+    export HOME=$(mktemp -d)
   '';
 
-  pytestFlagsArray = [
-    "--ignore=nbconvert/preprocessors/tests/test_execute.py"
-    # can't resolve template paths within sandbox
-    "--ignore=nbconvert/tests/base.py"
-    "--ignore=nbconvert/tests/test_nbconvertapp.py"
+  checkInputs = [
+    ipywidgets
+    pyppeteer
+    pytestCheckHook
   ];
 
+  pytestFlagsArray = [
+    # DeprecationWarning: Support for bleach <5 will be removed in a future version of nbconvert
+    "-W ignore::DeprecationWarning"
+  ];
 
   disabledTests = [
+    # Attempts network access (Failed to establish a new connection: [Errno -3] Temporary failure in name resolution)
     "test_export"
-    "test_webpdf_without_chromium"
-    #"test_cell_tag_output"
-    #"test_convert_from_stdin"
-    #"test_convert_full_qualified_name"
+    "test_webpdf_with_chromium"
   ];
 
   # Some of the tests use localhost networking.
