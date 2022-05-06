51512b35da1f86bfc0e6dafb3ef7f597fa1a9e1d
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-06 09:56:08 +0200

    python310Packages.ttp-templates: init at 0.1.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10357,6 +10357,8 @@ in {
 
   ttp = callPackage ../development/python-modules/ttp { };
 
+  ttp-templates = callPackage ../development/python-modules/ttp-templates { };
+
   tubes = callPackage ../development/python-modules/tubes { };
 
   tunigo = callPackage ../development/python-modules/tunigo { };
