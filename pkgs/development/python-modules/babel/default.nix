93d09055f55befe0c8a802c5a12f2e201fc85d14
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-05-06 21:56:54 +0200

    python310Packages.babel: adopt, update meta

--- pkgs/development/python-modules/babel/default.nix
+++ pkgs/development/python-modules/babel/default.nix
@@ -1,11 +1,12 @@
 { stdenv, lib, buildPythonPackage, fetchPypi, pytz, pytestCheckHook, freezegun }:
 
 buildPythonPackage rec {
-  pname = "Babel";
+  pname = "babel";
   version = "2.9.1";
 
   src = fetchPypi {
-    inherit pname version;
+    pname = "Babel";
+    inherit version;
     sha256 = "bc0c176f9f6a994582230df350aa6e05ba2ebe4b3ac317eab29d9be5d2768da0";
   };
 
@@ -16,9 +17,9 @@ buildPythonPackage rec {
   doCheck = !stdenv.isDarwin;
 
   meta = with lib; {
-    homepage = "http://babel.edgewall.org";
-    description = "A collection of tools for internationalizing Python applications";
+    homepage = "http://babel.pocoo.org/";
+    description = "Collection of internationalizing tools";
     license = licenses.bsd3;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ SuperSandro2000 ];
   };
 }
