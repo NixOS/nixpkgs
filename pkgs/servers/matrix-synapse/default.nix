f20bdf43157f09b46eb824e65d28425ec6c15a8b
R. Ryantm <ryantm-bot@ryantm.com>
2022-05-06 07:20:27 +0000

    matrix-synapse: 1.58.0 -> 1.58.1

--- pkgs/servers/matrix-synapse/default.nix
+++ pkgs/servers/matrix-synapse/default.nix
@@ -11,11 +11,11 @@ in
 with python3.pkgs;
 buildPythonApplication rec {
   pname = "matrix-synapse";
-  version = "1.58.0";
+  version = "1.58.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-cY3rtmaaAimEQPU4wcMEy/QysPNCdk7yptrkctnLfDA=";
+    sha256 = "sha256-6pRaF5tRKh1ocNvyF9uCeJTkLgwX47IOiLwTb8YTxH8=";
   };
 
   buildInputs = [ openssl ];
