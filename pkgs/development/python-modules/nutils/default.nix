f174d308de4fe8a3854bbb4ca4f8aca5384c119b
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-07 15:49:07 +0200

    python310Packages.nutils: add format

--- pkgs/development/python-modules/nutils/default.nix
+++ pkgs/development/python-modules/nutils/default.nix
@@ -1,8 +1,18 @@
-{ lib, buildPythonPackage, fetchFromGitHub, numpy, treelog, stringly, pytestCheckHook, pythonOlder }:
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, numpy
+, treelog
+, stringly
+, pytestCheckHook
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "nutils";
   version = "7.0";
+  format = "setuptools";
+
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
@@ -12,26 +22,29 @@ buildPythonPackage rec {
     hash = "sha256-V7lSMhwzc9+36uXMCy5uF241XwJ62Pdf59RUulOt4i8=";
   };
 
-  pythonImportsCheck = [ "nutils" ];
-
   propagatedBuildInputs = [
     numpy
     treelog
     stringly
   ];
 
-  checkInputs = [ pytestCheckHook ];
+  checkInputs = [
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [
+    "nutils"
+  ];
 
   disabledTestPaths = [
     # AttributeError: type object 'setup' has no attribute '__code__'
     "tests/test_cli.py"
   ];
 
-
   meta = with lib; {
     description = "Numerical Utilities for Finite Element Analysis";
     homepage = "https://www.nutils.org/";
     license = licenses.mit;
-    maintainers = [ maintainers.Scriptkiddi ];
+    maintainers = with maintainers; [ Scriptkiddi ];
   };
 }
