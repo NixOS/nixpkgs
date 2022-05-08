439d5e2a368096e61ddf8a79ee751f2af4fda943
Dmitry Bogatov <github-v1@kaction.cc>
2022-05-08 17:20:54 -0400

    Update pkgs/applications/version-management/rcshist/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/applications/version-management/rcshist/default.nix
+++ pkgs/applications/version-management/rcshist/default.nix
@@ -2,21 +2,21 @@
 , stdenv
 , fetchurl
 }:
-stdenv.mkDerivation
-  {
-    pname = "rcshist";
-    version = "1.04";
-    src =
-      fetchurl
-        {
-          url = "https://invisible-island.net/datafiles/release/rcshist.tar.gz";
-          sha256 = "01ab3xwgm934lxr8bm758am3vxwx4hxx7cc9prbgqj5nh30vdg1n";
-        };
-    meta = {
-      description = "Utitity to display complete revision history of a set of RCS files";
-      homepage = "https://invisible-island.net/rcshist/rcshist.html";
-      license = lib.licenses.bsd2;
-      maintainers = [ lib.maintainers.kaction ];
-      platforms = lib.platforms.unix;
-    };
-  }
+
+stdenv.mkDerivation {
+  pname = "rcshist";
+  version = "1.04";
+
+  src = fetchurl {
+    url = "https://invisible-island.net/datafiles/release/rcshist.tar.gz";
+    sha256 = "01ab3xwgm934lxr8bm758am3vxwx4hxx7cc9prbgqj5nh30vdg1n";
+  };
+
+  meta = {
+    description = "Utitity to display complete revision history of a set of RCS files";
+    homepage = "https://invisible-island.net/rcshist/rcshist.html";
+    license = lib.licenses.bsd2;
+    maintainers = [ lib.maintainers.kaction ];
+    platforms = lib.platforms.unix;
+  };
+}
