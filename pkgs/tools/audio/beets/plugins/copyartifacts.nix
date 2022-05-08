874994bcb84119bbdf6588a5d565d36c320d860a
Bernardo Meurer <bernardo@meurer.org>
2022-05-08 14:44:13 -0700

    beetsPackages.beets-copyartifacts: use pytest

--- pkgs/tools/audio/beets/plugins/copyartifacts.nix
+++ pkgs/tools/audio/beets/plugins/copyartifacts.nix
@@ -1,4 +1,4 @@
-{ lib, fetchFromGitHub, beets, python3Packages, glibcLocales }:
+{ lib, fetchFromGitHub, beets, python3Packages }:
 
 python3Packages.buildPythonApplication {
   pname = "beets-copyartifacts";
@@ -15,20 +15,20 @@ python3Packages.buildPythonApplication {
     sed -i -e '/install_requires/,/\]/{/beets/d}' setup.py
     sed -i -e '/namespace_packages/d' setup.py
     printf 'from pkgutil import extend_path\n__path__ = extend_path(__path__, __name__)\n' >beetsplug/__init__.py
-
-    # Skip test which is already failing upstream.
-    sed -i -e '1i import unittest' \
-           -e 's/\(^ *\)# failing/\1@unittest.skip/' \
-           tests/test_reimport.py
   '';
 
-  nativeBuildInputs = [ beets python3Packages.nose glibcLocales ];
+  pytestFlagsArray = [ "-r fEs" ];
 
-  checkPhase = "LANG=en_US.UTF-8 nosetests";
+  checkInputs = with python3Packages; [
+    pytestCheckHook
+    beets
+    six
+  ];
 
   meta = {
     description = "Beets plugin to move non-music files during the import process";
     homepage = "https://github.com/sbarakat/beets-copyartifacts";
     license = lib.licenses.mit;
+    inherit (beets.meta) platforms;
   };
 }
