194f81efa1aeac917ca27a1d09d9065d66ad8317
Joel <joel@joel.tokyo>
2022-05-08 13:19:30 +1000

    rich-cli: 1.7.0 -> 1.8.0

--- pkgs/misc/rich-cli/default.nix
+++ pkgs/misc/rich-cli/default.nix
@@ -1,15 +1,18 @@
 { lib
+, fetchFromGitHub
 , python3
 }:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "rich-cli";
-  version = "1.7.0";
+  version = "1.8.0";
   format = "pyproject";
 
-  src = python3.pkgs.fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-fporylec9H+9G2v8D0O32ek7OQs3YRSma1xOpakClqk=";
+  src = fetchFromGitHub {
+    owner = "Textualize";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-mV5b/J9wX9niiYtlmAUouaAm9mY2zTtDmex7FNWcezQ=";
   };
 
   nativeBuildInputs = with python3.pkgs; [
@@ -26,7 +29,7 @@ python3.pkgs.buildPythonApplication rec {
 
   postPatch = ''
     substituteInPlace pyproject.toml \
-      --replace 'rich = "^12.3.0"' 'rich = "*"'
+      --replace 'rich = "^12.4.0"' 'rich = "*"'
   '';
 
   pythonImportsCheck = [
