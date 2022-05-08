31b1d66bfe0a88c424ac93437bc570c4ac99bc9a
K900 <me@0upti.me>
2022-05-08 21:29:29 +0300

    powerdevil: drop ddcutil dependency

--- pkgs/desktops/plasma-5/powerdevil.nix
+++ pkgs/desktops/plasma-5/powerdevil.nix
@@ -3,7 +3,7 @@
   extra-cmake-modules, kdoctools,
   bluez-qt, kactivities, kauth, kconfig, kdbusaddons,
   kglobalaccel, ki18n, kidletime, kio, knotifyconfig, kwayland, libkscreen,
-  ddcutil, networkmanager-qt, plasma-workspace, qtx11extras, solid, udev
+  networkmanager-qt, plasma-workspace, qtx11extras, solid, udev
 }:
 
 mkDerivation {
@@ -13,9 +13,5 @@ mkDerivation {
     kconfig kdbusaddons knotifyconfig solid udev bluez-qt kactivities kauth
     kglobalaccel ki18n kio kidletime kwayland libkscreen
     networkmanager-qt plasma-workspace qtx11extras
-    ddcutil
-  ];
-  cmakeFlags = [
-    "-DHAVE_DDCUTIL=On"
   ];
 }
