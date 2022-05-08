62e689da1287eea56ec571e4d5baa619e401f341
Florian Brandes <florian.brandes@posteo.de>
2022-05-08 21:38:56 +0200

    libreoffice-still: apply review comments
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- pkgs/applications/office/libreoffice/src-still/override.nix
+++ pkgs/applications/office/libreoffice/src-still/override.nix
@@ -4,7 +4,7 @@ attrs:
   postConfigure = attrs.postConfigure + ''
     sed -e '/CPPUNIT_TEST(Import_Export_Import);/d' -i './sw/qa/inc/swmodeltestbase.hxx'
   '';
-    configureFlags = attrs.configureFlags ++ [
+  configureFlags = attrs.configureFlags ++ [
     (lib.enableFeature kdeIntegration "kf5")
     "--without-system-zxing"
   ];
