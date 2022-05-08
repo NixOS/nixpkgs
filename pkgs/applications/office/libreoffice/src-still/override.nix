dfd3754f612a2e563ada9eaa4554e451040939a1
Florian Brandes <florian.brandes@posteo.de>
2022-05-08 10:47:47 +0200

    libreoffice-still: 7.1.8.1 -> 7.2.6.2
    fixes #171907
    
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- pkgs/applications/office/libreoffice/src-still/override.nix
+++ pkgs/applications/office/libreoffice/src-still/override.nix
@@ -4,10 +4,10 @@ attrs:
   postConfigure = attrs.postConfigure + ''
     sed -e '/CPPUNIT_TEST(Import_Export_Import);/d' -i './sw/qa/inc/swmodeltestbase.hxx'
   '';
-  configureFlags = attrs.configureFlags ++ [
+    configureFlags = attrs.configureFlags ++ [
     (lib.enableFeature kdeIntegration "kf5")
-    "--with-commons-logging-jar=${commonsLogging}/share/java/commons-logging-1.2.jar"
-    "--without-system-qrcodegen"
+    "--without-system-zxing"
   ];
-  patches = attrs.patches or [] ++ [ ../xdg-open-brief.patch ]; # drop this when switching to 7.2
+
+  patches = attrs.patches or [];
 }
