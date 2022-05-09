21e569e2039280ec7509138be72e334267f5fd6f
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2022-05-09 10:15:52 -0400

    Apply suggestions from code review
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/development/python-modules/validobj/default.nix
+++ pkgs/development/python-modules/validobj/default.nix
@@ -11,7 +11,6 @@ buildPythonPackage rec {
   version = "0.5.1";
   format = "pyproject";
 
-  # tests are missing from the PyPi tarball
   src = fetchPypi {
     inherit pname version;
     sha256 = "430b0b56931a2cebdb857a9fe9da2467c06a3b4db37b728e7f1a8706e8887705";
@@ -19,8 +18,9 @@ buildPythonPackage rec {
 
   buildInputs = [ flit ];
 
-  pythonImportsCheck = [ "validobj" ];
   checkInputs = [ hypothesis pytestCheckHook ];
+
+  pythonImportsCheck = [ "validobj" ];
 
   meta = with lib; {
     description = "Validobj is library that takes semistructured data (for example JSON and YAML configuration files) and converts it to more structured Python objects";
