74b96d41d5b48f0cdc0220bce7c8f510e5e0be34
Jayesh Bhoot <jayesh@bhoot.sh>
2022-05-09 19:11:46 +0530

    promise_jsoo: init at 0.3.1

---
+++ pkgs/development/ocaml-modules/promise_jsoo/default.nix
@@ -0,0 +1,32 @@
+{ lib, fetchurl, buildDunePackage, ocaml, js_of_ocaml, ppxlib, js_of_ocaml-ppx, gen_js_api, ojs }:
+
+let
+  owner = "mnxn";
+in
+buildDunePackage rec {
+  pname = "promise_jsoo";
+  version = "0.3.1";
+  minimumOCamlVersion = "4.02";
+  useDune2 = lib.versionAtLeast ocaml.version "4.08";
+
+  src = fetchurl {
+    url = "https://github.com/${owner}/${pname}/releases/download/v${version}/${pname}-v${version}.tbz";
+    sha256 = "00pjnsbv0yv3hhxbbl8dsljgr95kjgi9w8j1x46gjyxg9zayrxzl";
+  };
+
+  buildInputs = [
+    js_of_ocaml
+    ppxlib
+    js_of_ocaml-ppx
+    gen_js_api
+  ];
+  propagatedBuildInputs = [ ojs ];
+
+  meta = {
+    homepage = "https://github.com/${owner}/${pname}";
+    description = "Js_of_ocaml bindings to JS Promises with supplemental functions";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ jayesh-bhoot ];
+  };
+}
+
