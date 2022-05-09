0a9f6d1d9c7db38317db2d9a2cd2a1ef06edacfa
David Tchekachev <contact@tchekda.fr>
2022-05-09 03:34:56 +0300

    bird-lg: init at 2022-05-09

---
+++ pkgs/servers/bird-lg/default.nix
@@ -0,0 +1,43 @@
+{ buildGoModule, fetchFromGitHub, lib, symlinkJoin }:
+let
+  generic = { modRoot, vendorSha256 }:
+    buildGoModule rec {
+      pname = "bird-lg-${modRoot}";
+      version = "2022-05-08";
+
+      doDist = false;
+
+      ldflags = [
+        "-s"
+        "-w"
+      ];
+
+      inherit modRoot;
+      inherit vendorSha256;
+
+      src = fetchFromGitHub {
+        owner = "xddxdd";
+        repo = "bird-lg-go";
+        rev = "348295b9aa954a92df2cf6b1179846a9486dafc0";
+        sha256 = "sha256-2t8ZP9Uc0sJlqWiJMq3MVoARfMKsuTXJkuOid0oWgyY=";
+      };
+
+      meta = with lib; {
+        description = "Bird Looking Glass";
+        homepage = "https://github.com/xddxdd/bird-lg-go";
+        license = licenses.gpl3Plus;
+        maintainers = with maintainers; [ tchekda ];
+      };
+    };
+
+  bird-lg-frontend = generic {
+    modRoot = "frontend";
+    vendorSha256 = "sha256-WKuVGiSV5LZrJ8/672TRN6tZNQxdCktHV6nx0ZxCP4A=";
+  };
+
+  bird-lg-proxy = generic {
+    modRoot = "proxy";
+    vendorSha256 = "sha256-7LZeCY4xSxREsQ+Dc2XSpu2ZI8CLE0mz0yoThP7/OO4=";
+  };
+in
+symlinkJoin { name = "bird-lg"; paths = [ bird-lg-frontend bird-lg-proxy ]; }
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21598,6 +21598,8 @@ with pkgs;
 
   bird = callPackage ../servers/bird { };
 
+  bird-lg = callPackage ../servers/bird-lg { };
+
   bloat = callPackage ../servers/bloat { };
 
   bosun = callPackage ../servers/monitoring/bosun { };
