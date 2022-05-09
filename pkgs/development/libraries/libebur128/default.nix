62011ffac400f8fea217f548ee4031afa6a65eb7
Alexander Shpilkin <ashpilkin@gmail.com>
2022-05-09 11:18:19 +0300

    libebur128: fix paths in pkg-config file

--- pkgs/development/libraries/libebur128/default.nix
+++ pkgs/development/libraries/libebur128/default.nix
@@ -14,6 +14,12 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ cmake pkg-config ];
   buildInputs = [ speexdsp ];
 
+  # https://github.com/jiixyj/libebur128/issues/121
+  postPatch = ''
+    substituteInPlace ebur128/libebur128.pc.cmake \
+      --replace '$'{prefix}/@CMAKE_INSTALL_LIBDIR@ @CMAKE_INSTALL_FULL_LIBDIR@
+  '';
+
   meta = with lib; {
     description = "Implementation of the EBU R128 loudness standard";
     homepage = "https://github.com/jiixyj/libebur128";
