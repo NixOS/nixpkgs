0bc70ad43489682489adfcf0678cec700c6d5a20
K900 <me@0upti.me>
2022-05-10 09:33:38 +0300

    zeroc-ice: 3.7.6 -> 3.7.7, drop Darwin
    Fixes build on Linux. Still doesn't build on Darwin, but at least it builds on Linux now.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30892,9 +30892,7 @@ with pkgs;
 
   zerobin = callPackage ../applications/networking/zerobin { };
 
-  zeroc-ice = callPackage ../development/libraries/zeroc-ice {
-    inherit (darwin.apple_sdk.frameworks) Security;
-  };
+  zeroc-ice = callPackage ../development/libraries/zeroc-ice {};
 
   zeroc-ice-cpp11 = zeroc-ice.override { cpp11 = true; };
 
