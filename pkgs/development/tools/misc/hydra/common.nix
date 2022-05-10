c07919c76caadd5915cbef41786f595e33cc263b
Linus Heckemann <git@sphalerite.org>
2022-05-10 14:08:18 +0200

    hydra: fix evaluation with aliases disabled

--- pkgs/development/tools/misc/hydra/common.nix
+++ pkgs/development/tools/misc/hydra/common.nix
@@ -36,7 +36,7 @@ let
         CatalystPluginSessionStateCookie
         CatalystPluginSessionStoreFastMmap
         CatalystPluginStackTrace
-        CatalystPluginUnicodeEncoding
+        CatalystRuntime
         CatalystTraitForRequestProxyBase
         CatalystViewDownload
         CatalystViewJSON
