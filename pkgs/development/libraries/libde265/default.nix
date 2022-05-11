efa0fa9e589688f2aa9965033862c3d16c1b40e4
Sandro <sandro.jaeckel@gmail.com>
2022-05-11 16:57:53 +0200

    Update pkgs/development/libraries/libde265/default.nix
    Co-authored-by: Linus Heckemann <git@sphalerite.org>
    (cherry picked from commit fe4ca85c970a7bbd1746869865040787a8e6a5ec)

--- pkgs/development/libraries/libde265/default.nix
+++ pkgs/development/libraries/libde265/default.nix
@@ -12,8 +12,8 @@ stdenv.mkDerivation rec {
   };
 
   patches = [
-    # CVE-2022-1253
     (fetchpatch {
+      name = "CVE-2022-1253.patch"
       url = "https://github.com/strukturag/libde265/commit/8e89fe0e175d2870c39486fdd09250b230ec10b8.patch";
       sha256 = "sha256-F1BOWFx9oXR2trM22atyD3AJ5x6vVfURQ/PTlYP2Ibg=";
     })
