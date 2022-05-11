dca0c4a803e1b3840236e72278575c0e2f27b92d
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-11 09:38:21 +0200

    python310Packages.praw: 7.5.0 -> 7.6.0

--- pkgs/development/python-modules/praw/default.nix
+++ pkgs/development/python-modules/praw/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "praw";
-  version = "7.5.0";
+  version = "7.6.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "praw-dev";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-xcITJ349ek9Y0HvJwzKJ7xDUV74w2v3yTBaj5n8YJ58=";
+    sha256 = "sha256-y2eynMsjF4wZd31YoLdtk8F+ga7Z3R+IQkQK0x0RAGA=";
   };
 
   propagatedBuildInputs = [
