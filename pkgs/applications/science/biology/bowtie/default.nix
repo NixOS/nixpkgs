802a8416b6f0b34afd2eec9d6249aa9763b9924d
Theodore Ni <3806110+tjni@users.noreply.github.com>
2022-05-11 02:33:57 -0500

    bowtie: add arm64 patch to build on M1 Macs
    This does not fix the build on Macs, but it brings the build on M1 Macs
    to parity with non-M1 Macs.

--- pkgs/applications/science/biology/bowtie/default.nix
+++ pkgs/applications/science/biology/bowtie/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, zlib }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch, zlib }:
 
 stdenv.mkDerivation rec {
   pname = "bowtie";
@@ -10,6 +10,16 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     sha256 = "sha256-mWItmrTMPst/NnzSpxxTHcBztDqHPCza9yOsZPwp7G4=";
   };
+
+  patches = [
+    # Without this patch, compiling with clang on an M1 Mac fails because
+    # 'cpuid.h' is included. It only works on x86 and throws an error.
+    (fetchpatch {
+      name = "fix_compilation_on_arm64";
+      url = "https://github.com/BenLangmead/bowtie/commit/091d72f4cb69ca0713704d38bd7f9b37e6c4ff2d.patch";
+      sha256 = "sha256-XBvgICUBnE5HKpJ36IHTDiKjJgLFKETsIaJC46uN+2I=";
+    })
+  ];
 
   buildInputs = [ zlib ];
 
