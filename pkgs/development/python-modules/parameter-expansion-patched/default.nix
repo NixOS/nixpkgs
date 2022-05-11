d96494b8dbe758406a1b8500f7246876886ba222
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-11 11:14:11 +0200

    python310Packages.parameter-expansion-patched: 0.2.1b4 -> 0.3.1

--- pkgs/development/python-modules/parameter-expansion-patched/default.nix
+++ pkgs/development/python-modules/parameter-expansion-patched/default.nix
@@ -3,18 +3,26 @@
 , fetchPypi
 , pytestCheckHook
 , pythonOlder
+, setuptools-scm
 }:
 
 buildPythonPackage rec {
   pname = "parameter-expansion-patched";
-  version = "0.2.1b4";
+  version = "0.3.1";
+  format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1vhshscjifi78qapzwn29gln6p8jhyc7cccszl8ai2jamhcph5zs";
+    hash = "sha256-/128ifveWC8zNlYtGWtxB3HpK6p7bVk1ahSwhaC2dAs=";
   };
+
+  SETUPTOOLS_SCM_PRETEND_VERSION = version;
+
+  nativeBuildInputs = [
+    setuptools-scm
+  ];
 
   checkInputs = [
     pytestCheckHook
@@ -26,7 +34,7 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "POSIX parameter expansion in Python";
-    homepage = "https://github.com/nexB/commoncode";
+    homepage = "https://github.com/nexB/parameter_expansion_patched";
     license = licenses.asl20;
     maintainers = with maintainers; [ fab ];
   };
