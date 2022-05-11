dfb16513230ec348f9ba299916c92b0b5d3ebc68
Maximilian Bosch <maximilian@mbosch.me>
2022-05-11 08:30:35 +0200

    linux: 5.10.113 -> 5.10.114
    (cherry picked from commit 8869941b2716c2d45d1fb13acd0ca2b49209d118)

--- pkgs/os-specific/linux/kernel/linux-5.10.nix
+++ pkgs/os-specific/linux/kernel/linux-5.10.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.10.113";
+  version = "5.10.114";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1z3dd5hrdbn2axsi2n70n41q1dq2dvg7s8aph1p6yiajpc16llc2";
+    sha256 = "09h5ngcl8clsvsv11q6ksvgcs01whlwbbrlpj1jz8mg3gjsrzl07";
   };
 } // (args.argsOverride or {}))
