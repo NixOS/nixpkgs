905b7e85ea92101defc12a76ebe10ff016f149f3
Alyssa Ross <hi@alyssa.is>
2022-05-11 17:11:36 +0000

    cgiserver: init at 1.0.0
    (cherry picked from commit b97e0d1cbc4c07934c4162502c637824818b58f0)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21589,6 +21589,8 @@ with pkgs;
 
   cayley = callPackage ../servers/cayley { };
 
+  cgiserver = callPackage ../servers/http/cgiserver { };
+
   charybdis = callPackage ../servers/irc/charybdis {
     autoreconfHook = buildPackages.autoreconfHook269;
   };
