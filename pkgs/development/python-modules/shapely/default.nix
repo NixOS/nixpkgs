795b29319cc8e8f1e0dc5ab98f514e7dca14f205
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-05-12 05:50:18 -0400

    python3Packages.shapely: 1.8.1.post1 -> 1.8.2

--- pkgs/development/python-modules/shapely/default.nix
+++ pkgs/development/python-modules/shapely/default.nix
@@ -12,12 +12,12 @@
 
 buildPythonPackage rec {
   pname = "Shapely";
-  version = "1.8.1.post1";
+  version = "1.8.2";
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-k/8G/wX74r6EO5PHsa2CkuVuZlugG0cI91rop1eXLp8=";
+    sha256 = "sha256-Vyr51QBv1eMhPjfuVIkSsDQfsmck1tyKTjlQwQGX67Y=";
   };
 
   nativeBuildInputs = [
