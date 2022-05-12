00b605f670ac77963006aa903c1ec55d75f26e8c
Theodore Ni <3806110+tjni@users.noreply.github.com>
2022-05-12 02:35:30 -0500

    adlplug,opnplug: patch JUCE to fix build on Darwin
    This will no longer be necessary after updating ADLplug to the latest
    version on its master branch.

--- pkgs/applications/audio/adlplug/default.nix
+++ pkgs/applications/audio/adlplug/default.nix
@@ -45,12 +45,93 @@ stdenv.mkDerivation rec {
     sha256 = "0mqx4bzri8s880v7jwd24nb93m5i3aklqld0b3h0hjnz0lh2qz0f";
   };
 
+  # All of these patches can be cleaned up if we can update ADLplug to the
+  # latest, unreleased version. (It updates its bundled version of JUCE so
+  # that it contains all of these changes.)
+  #
   patches = [
     (fetchpatch {
       url = "https://raw.githubusercontent.com/jpcima/ADLplug/83636c55bec1b86cabf634b9a6d56d07f00ecc61/resources/patch/juce-gcc9.patch";
       sha256 = "15hkdb76n9lgjsrpczj27ld9b4804bzrgw89g95cj4sc8wwkplyy";
       extraPrefix = "thirdparty/JUCE/";
       stripLen = 1;
+    })
+
+    (fetchpatch {
+      #
+      # Commit message:
+      #
+      #   macOS: Added missing OS versions to SystemStats::OperatingSystemType
+      #
+      # This is present by default in JUCE version 5.4.4.
+      #
+      # Needed so that subsequent patches apply cleanly.
+      #
+      url = "https://github.com/juce-framework/JUCE/commit/730fd6955fb1397c6da6774aeda1c7fa39792ce5.patch";
+      sha256 = "UT01GnnxGVIHupxPXD4HNifcPZR+zauqQQ3mzWOr3yQ=";
+      extraPrefix = "thirdparty/JUCE/";
+      stripLen = 1;
+    })
+    (fetchpatch {
+      #
+      # Commit message:
+      #
+      #   Prevented the Apple system headers from including some unnecessary C library headers
+      #
+      # This is present by default in JUCE version 5.4.4.
+      #
+      # Needed so that subsequent patches apply cleanly.
+      #
+      url = "https://github.com/juce-framework/JUCE/commit/2830ecec0a24035ddfe383475243b397841af289.patch";
+      sha256 = "I3mS9HE10L0v5IWchvyL6cNozrvw60np0EYK+SEblxM=";
+      extraPrefix = "thirdparty/JUCE/";
+      stripLen = 1;
+    })
+    (fetchpatch {
+      # Commit message:
+      #
+      #   Build: Fix Xcode 11.4 compatibility issues
+      #
+      # This is present by default in JUCE version 6.0.0.
+      #
+      url = "https://github.com/juce-framework/JUCE/commit/dddeb1ad68f8539f5ea2fbac36c07532f12bf9cf.patch";
+      sha256 = "B51UqvyHuc3beqkMp1mXE0aQ/yHbVioH0MPwAW4KHt8=";
+      extraPrefix = "thirdparty/JUCE/";
+      stripLen = 1;
+    })
+    (fetchpatch {
+      # Commit message:
+      #
+      #   Cleanup: Remove redundant inlines
+      #
+      # This is present by default in JUCE version 6.0.0.
+      #
+      # Needed so that subsequent patches apply cleanly.
+      #
+      url = "https://github.com/juce-framework/JUCE/commit/4cf66d65221bee100c3de490ca3fe6fafc72f508.patch";
+      sha256 = "Jw4OeNcRNgNT9hYdl/juo2w81G4QWNoplyoZZ1m4VMo=";
+      extraPrefix = "thirdparty/JUCE/";
+      stripLen = 1;
+      includes = [
+        "thirdparty/JUCE/modules/juce_core/native/juce_osx_ObjCHelpers.h"
+      ];
+    })
+    (fetchpatch {
+      # Commit message:
+      #
+      #   macOS: Initial support for macOS 11 and arm64
+      #
+      # This fixes objc_msgSend_fpret is an unknown symbol errors.
+      #
+      # This is present by default in JUCE version 6.0.2.
+      #
+      url = "https://github.com/juce-framework/JUCE/commit/b27017a5e3db1d6acf11d89f35adfe7de8bdb600.patch";
+      sha256 = "NxOawDc3c6JFf9g0rg5KK9j2aS1VkTwel8WfYTfOYcs=";
+      extraPrefix = "thirdparty/JUCE/";
+      stripLen = 1;
+      excludes = [
+        "thirdparty/JUCE/docs/CMake API.md"
+      ];
     })
   ];
 
