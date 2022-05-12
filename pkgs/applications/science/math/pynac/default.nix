738c576e115c6b84cb9f802ce3a6c3043c45244a
Ivan Kozik <ivan@ludios.org>
2022-05-12 02:53:26 +0000

    pynac: cleanup alignment

--- pkgs/applications/science/math/pynac/default.nix
+++ pkgs/applications/science/math/pynac/default.nix
@@ -52,9 +52,9 @@ stdenv.mkDerivation rec {
       It is a lite version of GiNaC as well, not implementing all the features
       of the full GiNaC, and it is *only* meant to be used as a Python library.
     '';
-    homepage    = "http://pynac.org";
+    homepage = "http://pynac.org";
     license = licenses.gpl2Plus;
     maintainers = teams.sage.members;
-    platforms   = platforms.unix;
+    platforms = platforms.unix;
   };
 }
