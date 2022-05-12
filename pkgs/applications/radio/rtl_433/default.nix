d949097fcd7c4a2f33523244d6af9ed38be90daa
Markus Kowalewski <markus.kowalewski@gmail.com>
2022-05-12 23:34:01 +0200

    rtl_433: patch for CVE-2022-27419

--- pkgs/applications/radio/rtl_433/default.nix
+++ pkgs/applications/radio/rtl_433/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, cmake, pkg-config
+{ lib, stdenv, fetchFromGitHub, cmake, pkg-config, fetchpatch
 , libusb1, rtl-sdr, soapysdr-with-plugins
 }:
 
@@ -12,6 +12,12 @@ stdenv.mkDerivation rec {
     rev = version;
     sha256 = "sha256-KoDKyI7KDdGSe79ZTuL9ObKnOJsqTN4wrMq+/cvQ/Xk=";
   };
+
+  patches = [( fetchpatch {
+    name = "CVE-2022-27419";
+    url = "https://github.com/merbanan/rtl_433/commit/37455483889bd1c641bdaafc493d1cc236b74904.patch";
+    sha256 = "172jndh8x5nlcbx2jp5y8fgfxsawwfz95037pcjp170gf93ijy88";
+  })];
 
   nativeBuildInputs = [ pkg-config cmake ];
 
