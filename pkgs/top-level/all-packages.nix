5c94298f876ec71b07abc669569305a951c3d6e1
Berry Phillips <berryphillips@gmail.com>
2022-05-14 03:25:00 +0900

    civo: init at 1.0.28

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33787,6 +33787,8 @@ with pkgs;
 
   calaos_installer = libsForQt5.callPackage ../misc/calaos/installer {};
 
+  civo = callPackage ../applications/networking/cluster/civo { };
+
   click = callPackage ../applications/networking/cluster/click { };
 
   clinfo = callPackage ../tools/system/clinfo {
