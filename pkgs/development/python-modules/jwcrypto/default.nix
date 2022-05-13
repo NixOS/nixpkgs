d47523375d3a6c3f8d29d27b7bbaab7bba2f6c34
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-13 08:48:35 +0200

    python310Packages.jwcrypto: add pythonImportsCheck

--- pkgs/development/python-modules/jwcrypto/default.nix
+++ pkgs/development/python-modules/jwcrypto/default.nix
@@ -3,15 +3,19 @@
 , fetchPypi
 , cryptography
 , deprecated
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "jwcrypto";
   version = "1.3";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-DWRhuhP3wnHYusUBjuYN28rl/zlAP6+kI3X1fQjjmLs=";
+    hash = "sha256-DWRhuhP3wnHYusUBjuYN28rl/zlAP6+kI3X1fQjjmLs=";
   };
 
   propagatedBuildInputs = [
@@ -19,10 +23,14 @@ buildPythonPackage rec {
     deprecated
   ];
 
+  pythonImportsCheck = [
+    "jwcrypto"
+  ];
+
   meta = with lib; {
     description = "Implementation of JOSE Web standards";
     homepage = "https://github.com/latchset/jwcrypto";
     license = licenses.lgpl3Plus;
-    maintainers = [ maintainers.costrouc ];
+    maintainers = with maintainers; [ costrouc ];
   };
 }
