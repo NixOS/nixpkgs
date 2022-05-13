744ca87ed01ef0ee02e62c12340c1071351e561f
piegames <git@piegames.de>
2022-05-14 00:03:59 +0200

    npins: Init at 0.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4019,6 +4019,8 @@ with pkgs;
 
   notify = callPackage ../tools/misc/notify { };
 
+  npins = callPackage ../tools/nix/npins { };
+
   nrsc5 = callPackage ../applications/misc/nrsc5 { };
 
   nsync = callPackage ../development/libraries/nsync { };
