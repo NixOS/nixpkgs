4a6744b2b210d1f04d354aa31a638a172a6d70f0
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-05-13 19:47:48 +0200

    proxychains-ng: install default config, zsh completion

--- pkgs/tools/networking/proxychains-ng/default.nix
+++ pkgs/tools/networking/proxychains-ng/default.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , fetchFromGitHub
+, fetchpatch
 }:
 
 stdenv.mkDerivation rec {
@@ -13,6 +14,18 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     sha256 = "sha256-uu/zN6W0ue526/3a9QeYg6J4HLaovZJVOYXksjouYok=";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/rofl0r/proxychains-ng/commit/04023d3811d8ee34b498b429bac7a871045de59c.patch";
+      sha256 = "sha256-Xcg2kmAhj/OJn/RKJAxb9MOJNJQY7FXmxEIzQ5dvabo=";
+    })
+  ];
+
+  installFlags = [
+    "install-config"
+    "install-zsh-completion"
+  ];
 
   meta = with lib; {
     description = "A preloader which hooks calls to sockets in dynamically linked programs and redirects it through one or more socks/http proxies";
