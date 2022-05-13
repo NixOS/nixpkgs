d1db18bc69da8246071b247f12f9e1b84fa83812
Luflosi <luflosi@luflosi.de>
2022-05-13 12:24:41 +0200

    yosys: 0.16 -> 0.17
    https://github.com/YosysHQ/yosys/releases/tag/yosys-0.17

--- pkgs/development/compilers/yosys/default.nix
+++ pkgs/development/compilers/yosys/default.nix
@@ -72,13 +72,13 @@ let
 
 in stdenv.mkDerivation rec {
   pname   = "yosys";
-  version = "0.16";
+  version = "0.17";
 
   src = fetchFromGitHub {
     owner = "YosysHQ";
     repo  = "yosys";
     rev   = "${pname}-${version}";
-    hash  = "sha256-X1yygoat6ezJt9jLO+W528ryf381nKGDQ3cfrG1ZbIk=";
+    hash  = "sha256-IjT+G3figWe32tTkIzv/RFjy0GBaNaZMQ1GA8mRHkio=";
   };
 
   enableParallelBuilding = true;
