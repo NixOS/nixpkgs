b419e38ea8e2ee131aee41d2bd7496367aaa50c0
Robert Schütz <nix@dotlambda.de>
2022-05-14 15:32:18 +0000

    python3Packages.pyrogram: 2.0.19 -> 2.0.21

--- pkgs/development/python-modules/pyrogram/default.nix
+++ pkgs/development/python-modules/pyrogram/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "pyrogram";
-  version = "2.0.19";
+  version = "2.0.21";
 
   disabled = pythonOlder "3.7";
 
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "pyrogram";
     repo = "pyrogram";
     rev = "v${version}";
-    hash = "sha256-HcUawiKlPS0zqB5kEScfV82xnLG0n62owzK5upOLn4I=";
+    hash = "sha256-o5VzsxJeVqscQqdT1EX7Xj0u6nCDzWTk6eObEqID7JY=";
   };
 
   propagatedBuildInputs = [
