8eb3f281dc1bd1a0599d29eca38e9bd9cfae3590
R. Ryantm <ryantm-bot@ryantm.com>
2022-05-14 12:13:26 +0000

    python310Packages.poetry-dynamic-versioning: 0.16.0 -> 0.17.0

--- pkgs/development/python-modules/poetry-dynamic-versioning/default.nix
+++ pkgs/development/python-modules/poetry-dynamic-versioning/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "poetry-dynamic-versioning";
-  version = "0.16.0";
+  version = "0.17.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "mtkennerly";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-dLtZSm10OYWLRK4eRo83RczW0zBVAUF0HQXLQbeDJCk=";
+    hash = "sha256-u4rqkwy5C1+OrHJUsc/9Sy3YyxWlsTv8cbB1bBKh2K4=";
   };
 
   nativeBuildInputs = [
