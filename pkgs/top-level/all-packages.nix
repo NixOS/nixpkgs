c9cb8e3b0c018ff3482652e72af4e5c9fdc26a8c
Austin Butler <austinabutler@gmail.com>
2022-05-14 18:00:46 -0700

    gyb: init at 1.62

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6628,6 +6628,8 @@ with pkgs;
 
   grin = callPackage ../tools/text/grin { };
 
+  gyb = callPackage ../tools/backup/gyb { };
+
   igrep = callPackage ../tools/text/igrep {
     inherit (darwin.apple_sdk.frameworks) Security;
   };
