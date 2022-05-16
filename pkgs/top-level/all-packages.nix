669283f06a78741ac640f0700e06794ef7db4cc0
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-05-17 00:52:31 +0200

    python3Packages.ansible-core: 2.12.5 -> 2.13.0
    Creates the ansible_2_13 attribute and keeps ansible_2_12 using an override.
    
    https://groups.google.com/g/ansible-announce/c/AEjCH6lMkZ0
    https://github.com/ansible/ansible/blob/v2.13.0/changelogs/CHANGELOG-v2.13.rst

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14805,7 +14805,14 @@ with pkgs;
   autoadb = callPackage ../misc/autoadb { };
 
   ansible = ansible_2_12;
-  ansible_2_12 = python3Packages.toPythonApplication python3Packages.ansible-core;
+  ansible_2_13 = python3Packages.toPythonApplication python3Packages.ansible-core;
+  ansible_2_12 = python3Packages.toPythonApplication (python3Packages.ansible-core.overridePythonAttrs (oldAttrs: rec {
+    version = "2.12.5";
+    src = oldAttrs.src.override {
+      inherit version;
+      hash = "sha256-HMyZRPEBMxra0e1A1axmqBSRMwUq402wJnp0qnO+67M=";
+    };
+  }));
 
   ansible-doctor = with python3.pkgs; toPythonApplication ansible-doctor;
 
