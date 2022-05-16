70d6b109b47b540f042f8f024fa136ac91a61879
Jan Tojnar <jtojnar@gmail.com>
2022-05-17 00:05:07 +0200

    gnomeExtensions.dash-to-dock: 71+date=2022-02-23 → 72
    Supports GNOME 42.
    
    https://github.com/micheleg/dash-to-dock/compare/extensions.gnome.org-v71...extensions.gnome.org-v72

--- pkgs/desktops/gnome/extensions/dash-to-dock/default.nix
+++ pkgs/desktops/gnome/extensions/dash-to-dock/default.nix
@@ -4,19 +4,19 @@
 , glib
 , gettext
 , sassc
-, unstableGitUpdater
+, gitUpdater
 }:
 
 stdenv.mkDerivation rec {
   pname = "gnome-shell-extension-dash-to-dock";
-  version = "71+date=2022-02-23";
+  version = "72";
 
   # Temporarily switched to commit hash because stable version is buggy.
   src = fetchFromGitHub {
     owner = "micheleg";
     repo = "dash-to-dock";
-    rev = "6f717302747931de6bf35bc9839fb3bd946e2c2f";
-    sha256 = "1J8t0R43jBbqpXyH2uVyEK+OvhrCw18WWheflqwe100=";
+    rev = "extensions.gnome.org-v${version}";
+    sha256 = "Cds5Fc+rnvoy01GTZBS7qPh8UC9ekrNBOs+IEkDNkJw=";
   };
 
   nativeBuildInputs = [
@@ -33,9 +33,10 @@ stdenv.mkDerivation rec {
     extensionUuid = "dash-to-dock@micxgx.gmail.com";
     extensionPortalSlug = "dash-to-dock";
 
-    updateScript = unstableGitUpdater {
-      stableVersion = true;
-      tagPrefix = "extensions.gnome.org-v";
+    updateScript = gitUpdater {
+      pname = "gnomeExtensions.dash-to-dock";
+      inherit version;
+      rev-prefix = "extensions.gnome.org-v";
     };
   };
 
