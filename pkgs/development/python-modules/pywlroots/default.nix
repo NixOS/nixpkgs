7d62b028b729ffc5cc483188fddd6326e68e3793
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2022-05-16 14:43:42 +0200

    python3Packages.pywlroots: 0.15.13 -> 0.15.14
    https://github.com/flacjacket/pywlroots/releases/tag/v0.15.14

--- pkgs/development/python-modules/pywlroots/default.nix
+++ pkgs/development/python-modules/pywlroots/default.nix
@@ -19,14 +19,14 @@
 
 buildPythonPackage rec {
   pname = "pywlroots";
-  version = "0.15.13";
+  version = "0.15.14";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "teZ8udox+4NefgA73gD5d6hPur/zjCapoMYPW36ax0s=";
+    sha256 = "z0PVjVzmAb5cgBdXWMxOdsy0VMMkmewHZaPNBYivXSA=";
   };
 
   nativeBuildInputs = [ pkg-config ];
