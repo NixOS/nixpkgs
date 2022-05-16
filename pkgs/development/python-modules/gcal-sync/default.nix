c867b1d76cecd0b1e681763c67c6dc1e405b380b
Robert Schütz <nix@dotlambda.de>
2022-05-16 00:07:52 +0000

    python3Packages.gcal-sync: 0.7.1 -> 0.8.0
    https://github.com/allenporter/gcal_sync/releases/tag/0.8.0

--- pkgs/development/python-modules/gcal-sync/default.nix
+++ pkgs/development/python-modules/gcal-sync/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "gcal-sync";
-  version = "0.7.1";
+  version = "0.8.0";
 
   disabled = pythonOlder "3.9";
 
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "allenporter";
     repo = "gcal_sync";
     rev = version;
-    hash = "sha256-NOB74P+5i63FEeHJsPXRdRgY6iyIUEn7BogbVKm8P5M=";
+    hash = "sha256-Qk4WlMVyzOK2uyrUlJw9Q8ENtNT3PZ7FKTlCtPsm2v0=";
   };
 
   propagatedBuildInputs = [
