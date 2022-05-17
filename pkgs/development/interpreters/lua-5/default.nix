980d1262a0e1c92f94efe923d3c5a5b99e898346
Robin Gloster <mail@glob.in>
2022-05-17 14:55:20 +0200

    lua5_2: add patch for CVE-2022-28805
    Derived from https://github.com/lua/lua/commit/1f3c6f4534c6411313361697d98d1145a1f030fa
    
    (cherry picked from commit 04d41ba8cc770aecc76a72b50f09c281d88a5022)

--- pkgs/development/interpreters/lua-5/default.nix
+++ pkgs/development/interpreters/lua-5/default.nix
@@ -32,7 +32,9 @@ rec {
     sourceVersion = { major = "5"; minor = "2"; patch = "4"; };
     hash = "0jwznq0l8qg9wh5grwg07b5cy3lzngvl5m2nl1ikp6vqssmf9qmr";
     makeWrapper = makeBinaryWrapper;
-    patches = lib.optional stdenv.isDarwin ./5.2.darwin.patch;
+    patches = [
+      ./CVE-2022-28805.patch
+    ] ++ lib.optional stdenv.isDarwin ./5.2.darwin.patch;
   };
 
   lua5_2_compat = lua5_2.override({
