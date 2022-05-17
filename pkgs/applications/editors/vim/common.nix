6c822e25b66f78d5051d101d366c81b663946b72
Robin Gloster <mail@glob.in>
2022-05-17 19:37:17 +0200

    vim: 8.2.4816 -> 8.2.4874
    fixes:
     - 7.8 https://nvd.nist.gov/vuln/detail/CVE-2022-1619
     - 7.5 https://nvd.nist.gov/vuln/detail/CVE-2022-1620
     - 7.8 https://nvd.nist.gov/vuln/detail/CVE-2022-1621
     - 7.8 https://nvd.nist.gov/vuln/detail/CVE-2022-1629

--- pkgs/applications/editors/vim/common.nix
+++ pkgs/applications/editors/vim/common.nix
@@ -1,12 +1,12 @@
 { lib, fetchFromGitHub }:
 rec {
-  version = "8.2.4816";
+  version = "8.2.4874";
 
   src = fetchFromGitHub {
     owner = "vim";
     repo = "vim";
     rev = "v${version}";
-    sha256 = "1lgqr3ki50hwkz4vhdyaryirrs99qq4kgkhmpx7ygvn6aj2wapg5";
+    sha256 = "sha256-0uVOIkm+hRJG0aU9kqFoWB78WdwyqDI34vhA6VQLglc=";
   };
 
   enableParallelBuilding = true;
