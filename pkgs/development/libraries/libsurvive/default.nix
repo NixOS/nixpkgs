32851a9a93301d8fb8b0a46da3eb6fa4594ce9d4
Alexander Shpilkin <ashpilkin@gmail.com>
2022-05-17 15:39:47 +0300

    libsurvive: fix path in pkg-config file

--- pkgs/development/libraries/libsurvive/default.nix
+++ pkgs/development/libraries/libsurvive/default.nix
@@ -30,6 +30,12 @@ stdenv.mkDerivation rec {
     zlib
   ];
 
+  # https://github.com/cntools/libsurvive/issues/272
+  postPatch = ''
+    substituteInPlace survive.pc.in \
+      --replace '$'{exec_prefix}/@CMAKE_INSTALL_LIBDIR@ @CMAKE_INSTALL_FULL_LIBDIR@
+  '';
+
   meta = with lib; {
     description = "Open Source Lighthouse Tracking System";
     homepage = "https://github.com/cntools/libsurvive";
