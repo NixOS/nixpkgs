ab8a7cae2c0eb3be8bf0175f9f53affd193983fa
Maximilian Bosch <maximilian@mbosch.me>
2022-05-17 18:11:40 +0200

    nixos/nextcloud: remove unneeded `log_level`-param
    Actually it's called `loglevel` on both v22 and v23.

--- nixos/modules/services/web-apps/nextcloud.nix
+++ nixos/modules/services/web-apps/nextcloud.nix
@@ -467,7 +467,6 @@ in {
               'skeletondirectory' => '${cfg.skeletonDirectory}',
               ${optionalString cfg.caching.apcu "'memcache.local' => '\\OC\\Memcache\\APCu',"}
               'log_type' => 'syslog',
-              'log_level' => '${builtins.toString cfg.logLevel}',
               'loglevel' => '${builtins.toString cfg.logLevel}',
               ${optionalString (c.overwriteProtocol != null) "'overwriteprotocol' => '${c.overwriteProtocol}',"}
               ${optionalString (c.dbname != null) "'dbname' => '${c.dbname}',"}
