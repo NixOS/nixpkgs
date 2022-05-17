ad03d121f2bf81fabeac5825ffc6b8139efb1834
Sumner Evans <me@sumnerevans.com>
2022-05-17 08:01:00 -0600

    matrix-synapse: 1.58.0 -> 1.59.1
    (cherry picked from commit 2836ac6b83a443f06cc7b851618aec92edaa4c08)

--- pkgs/servers/matrix-synapse/default.nix
+++ pkgs/servers/matrix-synapse/default.nix
@@ -11,11 +11,11 @@ in
 with python3.pkgs;
 buildPythonApplication rec {
   pname = "matrix-synapse";
-  version = "1.58.0";
+  version = "1.59.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-cY3rtmaaAimEQPU4wcMEy/QysPNCdk7yptrkctnLfDA=";
+    sha256 = "sha256-EASd+tlPIYQpQP2OOHJSPDzgJZyVoigVfcC2b2c2A2o=";
   };
 
   buildInputs = [ openssl ];
