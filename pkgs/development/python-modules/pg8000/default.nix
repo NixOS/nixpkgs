9cdd4e03aecc332128f61bdb26364cebc0be0e7c
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-17 07:57:11 +0200

    python310Packages.pg8000: 1.26.1 -> 1.27.1

--- pkgs/development/python-modules/pg8000/default.nix
+++ pkgs/development/python-modules/pg8000/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "pg8000";
-  version = "1.26.1";
+  version = "1.27.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-zNK2/hkK3ddMCTpivgcwuemfPqA6oO96uV7Rt/9p0lc=";
+    sha256 = "sha256-1qWDg0hZM0TyDrNa2kcqdy0yFFgm8u/ljb4bZeqZ6JA=";
   };
 
   propagatedBuildInputs = [
