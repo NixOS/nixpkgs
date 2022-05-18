20dc1995934cf4dee47a418e9ee62298bde84d8d
Alexander Shpilkin <ashpilkin@gmail.com>
2022-05-18 17:48:26 +0300

    orcania: fix paths in pkg-config file

--- pkgs/development/libraries/orcania/default.nix
+++ pkgs/development/libraries/orcania/default.nix
@@ -18,6 +18,13 @@ stdenv.mkDerivation rec {
 
   doCheck = true;
 
+  # https://github.com/babelouest/orcania/issues/27
+  postPatch = ''
+    substituteInPlace liborcania.pc.in \
+      --replace '$'{exec_prefix}/@CMAKE_INSTALL_LIBDIR@ @CMAKE_INSTALL_FULL_LIBDIR@ \
+      --replace '$'{prefix}/@CMAKE_INSTALL_INCLUDEDIR@ @CMAKE_INSTALL_FULL_INCLUDEDIR@
+  '';
+
   preCheck = ''
     export LD_LIBRARY_PATH="$(pwd)''${LD_LIBRARY_PATH:+:}$LD_LIBRARY_PATH"
     export DYLD_FALLBACK_LIBRARY_PATH="$(pwd):$DYLD_FALLBACK_LIBRARY_PATH"
