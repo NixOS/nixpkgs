b377ca57ab02462cb08edf0a0371e5723b7dc337
Alexander Shpilkin <ashpilkin@gmail.com>
2022-05-18 21:29:07 +0300

    rinutils: fix path in pkg-config file

--- pkgs/development/libraries/rinutils/default.nix
+++ pkgs/development/libraries/rinutils/default.nix
@@ -18,4 +18,11 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [ cmake perl ];
+
+  # https://github.com/shlomif/rinutils/issues/5
+  postPatch = ''
+    substituteInPlace librinutils.pc.in \
+      --replace '$'{exec_prefix}/@RINUTILS_INSTALL_MYLIBDIR@ @CMAKE_INSTALL_FULL_LIBDIR@ \
+      --replace '$'{prefix}/@CMAKE_INSTALL_INCLUDEDIR@ @CMAKE_INSTALL_FULL_INCLUDEDIR@
+  '';
 }
