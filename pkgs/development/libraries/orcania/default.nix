bf76af55353b160fab6c0af1f9cd424ddd36d977
Alexander Shpilkin <ashpilkin@gmail.com>
2022-05-18 17:48:26 +0300

    orcania: fix paths in pkg-config file

--- pkgs/development/libraries/orcania/default.nix
+++ pkgs/development/libraries/orcania/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, cmake, check, subunit }:
+{ lib, stdenv, fetchpatch, fetchFromGitHub, cmake, check, subunit }:
 stdenv.mkDerivation rec {
   pname = "orcania";
   version = "2.3.0";
@@ -9,6 +9,15 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     sha256 = "sha256-QAq/6MGVj+iBHLElHuqokF1v3LU1TZ9hVVJE1s3y6f0=";
   };
+
+  # in master post 2.2.2, see https://github.com/babelouest/orcania/issues/27
+  patches = [
+    (fetchpatch {
+      name = "fix-pkg-config.patch";
+      url = "https://github.com/babelouest/orcania/commit/4eac7d5ff76bb3bec8250fef300b723c8891552a.patch";
+      sha256 = "01bsxay1ca8d08ac3ddcqyvjwgz5mgs68jz5y3gzq4qnzl3q1i54";
+    })
+  ];
 
   nativeBuildInputs = [ cmake ];
 
