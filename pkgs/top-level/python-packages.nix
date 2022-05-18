0997b7fc2997ba913604f2df6f11c054212c4452
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-05-19 01:42:02 +0200

    python310Packages.django-polymorphic: 2.1.2 -> 3.1, adopt, normalise attr

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2358,7 +2358,7 @@ in {
 
   django-picklefield = callPackage ../development/python-modules/django-picklefield { };
 
-  django_polymorphic = callPackage ../development/python-modules/django-polymorphic { };
+  django-polymorphic = callPackage ../development/python-modules/django-polymorphic { };
 
   django-postgresql-netfields = callPackage ../development/python-modules/django-postgresql-netfields { };
 
