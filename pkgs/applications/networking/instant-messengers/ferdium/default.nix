583325d6e6126f44bc1982f7541e3d66dc483480
magnouvean <fd9d54f6-bb7d-4ce3-a4b3-42a3f60e088c@aleeas.com>
2022-05-19 09:46:23 +0200

    ferdium: init at 6.0.0-nightly.40

---
+++ pkgs/applications/networking/instant-messengers/ferdium/default.nix
@@ -0,0 +1,34 @@
+{ lib, mkFranzDerivation, fetchurl, xorg, xdg-utils, buildEnv, writeShellScriptBin }:
+
+let
+  mkFranzDerivation' = mkFranzDerivation.override {
+    xdg-utils = buildEnv {
+      name = "xdg-utils-for-ferdi";
+      paths = [
+        xdg-utils
+        (lib.hiPrio (writeShellScriptBin "xdg-open" ''
+          unset GDK_BACKEND
+          exec ${xdg-utils}/bin/xdg-open "$@"
+        ''))
+      ];
+    };
+  };
+in
+mkFranzDerivation' rec {
+  pname = "ferdium";
+  name = "Ferdium";
+  version = "6.0.0-nightly.40";
+  src = fetchurl {
+    url = "https://github.com/ferdium/ferdium-app/releases/download/v6.0.0-nightly.40/ferdium_6.0.0-nightly.40_amd64.deb";
+    sha256 = "sha256-pDVWVUQRJqFyaMGIhMekNzz8DVq+B8M3fD0JC8+7x2E=";
+  };
+  extraBuildInputs = [ xorg.libxshmfence ];
+  meta = with lib; {
+    description = "All your services in one place built by the community";
+    homepage = "https://ferdium.org/";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ magnouvean ];
+    platforms = [ "x86_64-linux" ];
+    hydraPlatforms = [ ];
+  };
+}
