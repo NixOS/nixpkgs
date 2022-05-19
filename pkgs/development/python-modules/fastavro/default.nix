3f9c0a658575464579b62b5c7bfa06c848b4b494
R. Ryantm <ryantm-bot@ryantm.com>
2022-05-19 08:51:27 +0000

    python310Packages.fastavro: 1.4.11 -> 1.4.12

--- pkgs/development/python-modules/fastavro/default.nix
+++ pkgs/development/python-modules/fastavro/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "fastavro";
-  version = "1.4.11";
+  version = "1.4.12";
 
   disabled = pythonOlder "3.6";
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-PK8+NFJurJ0nD5coEFj8lXWg4VMcpX0954qDb1GW6Gk=";
+    sha256 = "sha256-t3SM7pTsl3JLWeJSESzXGONyjufwCHMaqK95JI3isYY=";
   };
 
   preBuild = ''
