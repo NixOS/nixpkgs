2eaf7db3528230f76385e200c0b23c72443998f1
Rick van Schijndel <rol3517@gmail.com>
2022-05-19 21:48:44 +0200

    binwalk: mark broken on x86_64-darwin
    Probably some issues with signatures.
    Disabling this test may or may not also help.
    
    (cherry picked from commit ce7a64713eb631c08e7d403893023fb87be95abd)

--- pkgs/development/python-modules/binwalk/default.nix
+++ pkgs/development/python-modules/binwalk/default.nix
@@ -67,5 +67,7 @@ buildPythonPackage rec {
     description = "A tool for searching a given binary image for embedded files";
     maintainers = [ maintainers.koral ];
     license = licenses.mit;
+    # Signature Exception: [Errno 1] Operation not permitted: '/testing/tests/input-vectors/_dirtraversal.tar.extracted'
+    broken = (stdenv.isDarwin && stdenv.isx86_64); # broken on hydra since 2021-11-02
   };
 }
