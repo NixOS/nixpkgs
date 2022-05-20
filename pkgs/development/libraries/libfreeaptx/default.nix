NixOS/nixpkgs

commit 152765ca8b05c79c6a87683b1b6ce8589d82dd9e

libfreeaptx: fix build on darwin

pkgs/development/libraries/libfreeaptx/default.nix
@@ -11,6 +11,12 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-eEUhOrKqb2hHWanY+knpY9FBEnjkkFTB+x6BZgMBpbo=";
   };
 
+  postPatch = lib.optionalString stdenv.isDarwin ''
+    substituteInPlace Makefile \
+      --replace '-soname' '-install_name' \
+      --replace 'lib$(NAME).so' 'lib$(NAME).dylib'
+  '';
+
   makeFlags = [
     "PREFIX=${placeholder "out"}"
     # disable static builds
@@ -21,11 +27,18 @@ stdenv.mkDerivation rec {
 
   enableParallelBuilding = true;
 
+  postInstall = lib.optionalString stdenv.isDarwin ''
+    install_name_tool -change libfreeaptx.dylib.0 $out/lib/libfreeaptx.dylib.0 $out/bin/freeaptxdec
+    install_name_tool -change libfreeaptx.dylib.0 $out/lib/libfreeaptx.dylib.0 $out/bin/freeaptxenc
+    install_name_tool -id $out/lib/libfreeaptx.dylib $out/lib/libfreeaptx.dylib
+    install_name_tool -id $out/lib/libfreeaptx.dylib.0 $out/lib/libfreeaptx.dylib.0
+  '';
+
   meta = with lib; {
     description = "Free Implementation of Audio Processing Technology codec (aptX)";
     license = licenses.lgpl21Plus;
     homepage = "https://github.com/iamthehorker/libfreeaptx";
-    platforms = platforms.linux;
+    platforms = platforms.unix;
     maintainers = with maintainers; [ kranzes ];
   };
 }

commit 39f9c2dcfebcf1dc7607e8dcf8ce620ab76c6899

libfreeaptx: avoid rebuilding on Linux for now
Commit 152765ca8b caused thousands of rebuilds;
let's save doing that work on nixpkgs master.

pkgs/development/libraries/libfreeaptx/default.nix
@@ -11,11 +11,11 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-eEUhOrKqb2hHWanY+knpY9FBEnjkkFTB+x6BZgMBpbo=";
   };
 
-  postPatch = lib.optionalString stdenv.isDarwin ''
+  postPatch = if stdenv.isDarwin then ''
     substituteInPlace Makefile \
       --replace '-soname' '-install_name' \
       --replace 'lib$(NAME).so' 'lib$(NAME).dylib'
-  '';
+  '' else null;
 
   makeFlags = [
     "PREFIX=${placeholder "out"}"
@@ -27,12 +27,12 @@ stdenv.mkDerivation rec {
 
   enableParallelBuilding = true;
 
-  postInstall = lib.optionalString stdenv.isDarwin ''
+  postInstall = if stdenv.isDarwin then ''
     install_name_tool -change libfreeaptx.dylib.0 $out/lib/libfreeaptx.dylib.0 $out/bin/freeaptxdec
     install_name_tool -change libfreeaptx.dylib.0 $out/lib/libfreeaptx.dylib.0 $out/bin/freeaptxenc
     install_name_tool -id $out/lib/libfreeaptx.dylib $out/lib/libfreeaptx.dylib
     install_name_tool -id $out/lib/libfreeaptx.dylib.0 $out/lib/libfreeaptx.dylib.0
-  '';
+  '' else null;
 
   meta = with lib; {
     description = "Free Implementation of Audio Processing Technology codec (aptX)";