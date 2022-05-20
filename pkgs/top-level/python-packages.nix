055652935e2f81c70db8e932137dc5c37436c913
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-20 17:16:25 +0200

    python310Packages.types-docutils: init at 0.18.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10570,6 +10570,8 @@ in {
 
   types-decorator = callPackage ../development/python-modules/types-decorator { };
 
+  types-docutils = callPackage ../development/python-modules/types-docutils { };
+
   types-enum34 = callPackage ../development/python-modules/types-enum34 { };
 
   types-freezegun = callPackage ../development/python-modules/types-freezegun { };
