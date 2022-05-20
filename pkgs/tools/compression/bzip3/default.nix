c9e173fe09e33545ba936a0ee681df5200bae8f1
Robert Schütz <nix@dotlambda.de>
2022-05-20 05:31:51 +0000

    bzip3: init at 1.1.3

---
+++ pkgs/tools/compression/bzip3/default.nix
@@ -0,0 +1,42 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, autoreconfHook
+}:
+
+stdenv.mkDerivation rec {
+  pname = "bzip3";
+  version = "1.1.3";
+
+  outputs = [ "bin" "dev" "out" ];
+
+  src = fetchFromGitHub {
+    owner = "kspalaiologos";
+    repo = "bzip3";
+    rev = version;
+    hash = "sha256-puGtaL76p4BzSiTPf3qFgXN4pz90CDU9dziGIszk3to=";
+  };
+
+  postPatch = ''
+    echo -n "${version}" > .tarball-version
+    patchShebangs build-aux
+  '';
+
+  nativeBuildInputs = [
+    autoreconfHook
+  ];
+
+  configureFlags = [
+    "--disable-arch-native"
+  ];
+
+  meta = {
+    description = "A better and stronger spiritual successor to BZip2";
+    homepage = "https://github.com/kspalaiologos/bzip3";
+    changelog = "https://github.com/kspalaiologos/bzip3/blob/${src.rev}/NEWS";
+    license = lib.licenses.lgpl3Plus;
+    maintainers = with lib.maintainers; [ dotlambda ];
+    # upstream supports Darwin but I couldn't get it to work
+    platforms = lib.platforms.linux;
+  };
+}
