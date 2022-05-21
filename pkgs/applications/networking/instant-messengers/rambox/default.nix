939b6fbea06334e3578b6228ca698e5c4f6e701c
GabrielePuliti <gabriele.puliti@protonmail.com>
2022-05-21 14:46:51 +0000

    rambox: 1.5.0 -> 2.0.5 and remove deprecated CE

--- pkgs/applications/networking/instant-messengers/rambox/default.nix
+++ pkgs/applications/networking/instant-messengers/rambox/default.nix
@@ -5,16 +5,12 @@ let
 in
 mkRambox rec {
   pname = "rambox";
-  version = "0.7.9";
+  version = "2.0.5";
 
   src = {
     x86_64-linux = fetchurl {
-      url = "https://github.com/ramboxapp/community-edition/releases/download/${version}/Rambox-${version}-linux-x86_64.AppImage";
+      url = "https://github.com/ramboxapp/download/releases/download/v${version}/Rambox-${version}-linux-x64.AppImage";
       sha256 = "19y4cmrfp79dr4hgl698imp4f3l1nhgvhh76j5laxg46ld71knil";
-    };
-    i686-linux = fetchurl {
-      url = "https://github.com/ramboxapp/community-edition/releases/download/${version}/Rambox-${version}-linux-i386.AppImage";
-      sha256 = "13wiciyshyrabq2mvnssl2d6svia1kdvwx3dl26249iyif96xxvq";
     };
   }.${stdenv.system} or (throw "Unsupported system: ${stdenv.system}");
 
@@ -23,7 +19,7 @@ mkRambox rec {
     homepage = "https://rambox.pro";
     license = licenses.mit;
     maintainers = with maintainers; [];
-    platforms = [ "i686-linux" "x86_64-linux" ];
+    platforms = [ "x86_64-linux" ];
     hydraPlatforms = [];
   };
 }
