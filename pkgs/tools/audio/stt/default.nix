227c07597ea379b236a3820b19f22ef343192c5d
Jan Tojnar <jtojnar@gmail.com>
2022-05-21 09:09:13 +0200

    stt: 0.9.3 → 1.3.0
    https://github.com/coqui-ai/STT/releases/tag/v1.0.0 – dropped protobuf model support
    https://github.com/coqui-ai/STT/releases/tag/v1.1.0
    https://github.com/coqui-ai/STT/releases/tag/v1.2.0
    https://github.com/coqui-ai/STT/releases/tag/v1.3.0

--- pkgs/tools/audio/stt/default.nix
+++ pkgs/tools/audio/stt/default.nix
@@ -1,12 +1,18 @@
-{ stdenv, lib, fetchurl, autoPatchelfHook }:
+{ stdenv
+, lib
+, fetchurl
+, autoPatchelfHook
+, xz
+, bzip2
+}:
 
 stdenv.mkDerivation rec {
   pname = "stt";
-  version = "0.9.3";
+  version = "1.3.0";
 
   src = fetchurl {
-    url = "https://github.com/coqui-ai/STT/releases/download/v${version}/native_client.tf.Linux.tar.xz";
-    sha256 = "0axwys8vis4f0m7d1i2r3dfqlc8p3yj2nisvc7pdi5qs741xgy8w";
+    url = "https://github.com/coqui-ai/STT/releases/download/v${version}/native_client.tflite.Linux.tar.xz";
+    sha256 = "JsMrSNE0KlKpI998wPY6dMTyTggRtqu4JmucBmgirNs=";
   };
   setSourceRoot = "sourceRoot=`pwd`";
 
@@ -16,12 +22,20 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     stdenv.cc.cc.lib
+    xz # for lzma
+    bzip2
   ];
 
   installPhase = ''
+    runHook preInstall
+
     install -D stt $out/bin/stt
     install -D coqui-stt.h $out/include/coqui-stt.h
+    install -D libsox.so.3 $out/lib/libsox.so.3
+    install -D libkenlm.so $out/lib/libkenlm.so
     install -D libstt.so $out/lib/libstt.so
+
+    runHook postInstall
   '';
 
   meta = with lib; {
