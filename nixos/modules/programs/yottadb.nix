52b010298cd1a2eeb6ffba8050a0cbfb655dd8b3
Tomas Morstein <nix@fooboo.org>
2022-05-22 00:53:02 +0100

    yottadb: nixos-specific fixes
    YottaDB does not work correctly on NixOS out of the box
    in multiuser environments and requires a special SUID
    binary to allow processes to communicate with each other.
    
    This commit is trying to patch YDB to make this happen.
    
    These changes, however, are meant to enable people to use
    YottaDB for development on NixOS, but should not be used
    in production.

--- nixos/modules/programs/yottadb.nix
+++ nixos/modules/programs/yottadb.nix
@@ -6,15 +6,31 @@ let
 in {
   options.programs.yottadb = {
     enable = mkEnableOption "YottaDB";
+
+    package = mkPackageOption pkgs "YottaDB" {
+      default = [ "yottadb" ];
+    };
   };
 
-  config = mkIf cfg.enable {
-      environment.systemPackages = [ pkgs.yottadb ];
-      security.wrappers.gtmsecshr = {
+  config =
+    let
+      package = cfg.package;
+    in
+    mkIf cfg.enable {
+      environment.systemPackages = [ package ];
+      security.wrappers."${package.gtmsecshrSuidName}" = {
         setuid = true;
         owner = "root";
-        group = "root";
-        source = "${pkgs.yottadb}/dist/gtmsecshr.nosuid";
+        group = "${package.ydbGroup}";
+        permissions = "u+rx";
+        source = "${package}/${package.gtmsecshrSuidTargetRelative}";
       };
-  };
+      security.wrappers."${package.gtmsecshrWrapperSuidName}" = {
+        setuid = true;
+        owner = "root";
+        group = "${package.ydbGroup}";
+        permissions = "a+rx";
+        source = "${package}/${package.gtmsecshrWrapperSuidTargetRelative}";
+      };
+    };
 }
--- pkgs/development/yottadb/default.nix
+++ pkgs/development/yottadb/default.nix
@@ -1,7 +1,9 @@
 {
   lib,
+  config,
   makeWrapper,
   stdenv,
+  wrapperDir ? "/run/wrappers/bin",
   writeText,
   writeScript,
 
@@ -20,6 +22,10 @@
   zlib
 }:
 
+# To test this package, go to $NIXPKGS/nixos/tests and run:
+#   $ $(nix-build -A driverInteractive yottadb.nix)/bin/nixos-test-driver -I
+#   >>> test_script()
+
 stdenv.mkDerivation rec {
   pname = "yottadb";
   version = "1.34";
@@ -29,7 +35,7 @@ stdenv.mkDerivation rec {
     repo = "DB/YDB";
     rev = "r${version}";
     leaveDotGit = true;
-    sha256 = "sha256-4J0skA8Ru6rcvHMqN5nTIKI75tsnB+pYaSECDe8qRzE=";
+    hash = "sha256-i9kK2TsOaMpEMFpyQ1OCpF/cl/DHwEqgBtPNgyPHzPY=";
   };
 
   enableParallelBuilding = true;
@@ -72,6 +78,59 @@ stdenv.mkDerivation rec {
 
     # Revert any potential repo modifications to keep the release clean
     git reset --hard
+
+    # =========
+    # The following changes are not done as a conventional
+    # patchset because we need to hardcode a fixed Nix store path
+    # that is only known at package build time ($out).
+    #
+    # Background and motivation:
+    #
+    # gtmsecshr and gtmsecshr wrapper are two binaries that are
+    # assumed to be SUID and owned by root user, they are also
+    # assumed to be in $ydb_dist ($out/dist/gtmsecshr or
+    # $out/dist/utf8/gtmsecshr) in case of gtmsecshr wrapper
+    # and in $ydb_dist/gtmsecshrdir/gtmsecshr (or
+    # $ydb_dist/utf8/gtmsecshrdir/gtmsecshr) in case of the
+    # gtmsecshr daemon itself. The gtmsecshrdir is assumed to
+    # be 0500 (but Nix store has all directories as 0555).
+    #
+    # The problem is that NixOS does not allow SUID binaries
+    # in the Nix store itself, and all SUID has to be done
+    # using security wrappers. These are stored outside of
+    # the Nix store, which causes issues when the binary
+    # assumes to be in a directory relative to the package
+    # directory. Hardlinks are not possible because security
+    # wrapper does not exist before package is installed
+    # (chicken and egg problem), and softlinks are causing
+    # realpath syscall to return path to the wrapper that
+    # is outside of the package directory, breaking all the
+    # runtime assertions.
+    # Using makeBinWrapper that calls the security wrapper
+    # does not fix this, because it only manipulates argv[0],
+    # but YottaDB is using /proc/self/comm instead.
+    #
+    # Note that the changes below are developer enablers only,
+    # and are NOT meant for production use until this is solved
+    # properly. We don't know how is the YDB security model
+    # affected by these changes...
+    #
+    # Changes required:
+    #
+    # (1) Nix Store does not support 500 on directories, only 555
+    sed -ie s,0277,0222,g ../sr_unix/gtmsecshr_wrapper.c
+    #
+    # (2) gtmsecshr is SUID-wrapped and symlinked, so the realpath
+    # and ydb_dist checks are not working correctly, so we need
+    # to hack this a bit...
+    fullSecshrPath="$out/${gtmsecshrSuidTargetRelative}"
+    suffix="${gtmsecshrNosuidSuffix}"
+    rndirLen=$((''${#fullSecshrPath}-''${#suffix}))
+    sed -i \
+        -e 's@rndir = realpath(PROCSELF, gtmsecshr_realpath);@rndir = realpath("'"$out/${gtmsecshrSuidTargetRelative}"'", gtmsecshr_realpath); rndir['$rndirLen'] = NULL;@g' \
+        -e 's,path = ydb_dist,path = "'$out/dist'",g' \
+        ../sr_unix/gtmsecshr.c
+    # =========
   '';
 
   # NOTE: Setting exec_prefix=@@YDB_EXEDIR@@, includedir=@@YDB_INCDIR@@
@@ -91,22 +150,50 @@ stdenv.mkDerivation rec {
     Libs: -L''${libdir} -lyottadb -Wl,-rpath,''${libdir}
   '';
 
+  gtmsecshrNosuidSuffix = ".nosuid";
+  gtmsecshrSuidName = "gtmsecshr";
+  gtmsecshrSuidTargetRelative = "dist/gtmsecshrdir/gtmsecshr${gtmsecshrNosuidSuffix}";
+  gtmsecshrWrapperSuidName = "gtmsecshr-wrapper";
+  gtmsecshrWrapperSuidTargetRelative = "dist/gtmsecshr${gtmsecshrNosuidSuffix}";
+
+  ydbGroup = "root";
+
   yottadbGde = writeScript "gde" ''
     #!/usr/bin/env bash
-    exec ''${0%/*}/yottadb -r GDE "$@"
+    # Use bin/yottadb, not dist/yottadb (see the note on yottadb-shell wrapper)
+    exec @@YDB_PKG_DIR@@/bin/yottadb -r GDE "$@"
   '';
 
   postInstall = ''
+    set -x
+    # NOTE: In future, consider using a patched versions
+    # of `ydbinstall` and `configure` scripts while
+    # adding Nix-specific changes on top of that.
+
     mkdir -p $out/bin $out/lib $out/dist/utf8
     mkdir -p $out/lib/pkgconfig $out/include
 
-    # We cannot set SUID for gtmsecshr, it has to be done using Nix Security Wrapper
+    # We cannot set SUID for gtmsecshr wrapper, it has to be done
+    # using Nix Security Wrapper (wrapping the wrapper)
     cd $out/dist
-    mv gtmsecshr gtmsecshr.nosuid
-    ln -rfs /run/wrappers/bin/gtmsecshr .
+    # This is the gtmsecshr wrapper:
+    mv gtmsecshr "$out/${gtmsecshrWrapperSuidTargetRelative}"
+    # Can be symlink? We can't do hard-link before the wrapper is created!
+    ln -rs "${wrapperDir}/${gtmsecshrWrapperSuidName}" gtmsecshr
+
+    chmod 700 gtmsecshrdir
+    cd $out/dist/gtmsecshrdir
+    mv gtmsecshr "$out/${gtmsecshrSuidTargetRelative}"
+    # Can be symlink? We can't do hard-link before the wrapper is created!
+    ln -rs "${wrapperDir}/${gtmsecshrSuidName}" gtmsecshr
+    chmod 500 $out/dist/gtmsecshrdir  # is this needed?
+    cd $out/dist
 
     # Make sure we have ICU in library path (why is this needed only for ICU?)
-    wrapProgram $out/dist/yottadb \
+    # NOTE: make{Binary}Wrapper can manipulate argv0, but not /proc/self/comm
+    # so we will cheat a bit and have the following call structure:
+    #   $out/bin/yottadb -> $out/dist/yottadb-shell -> $out/dist/yottadb
+    makeWrapper "$out/dist/yottadb" "$out/dist/yottadb-shell" \
       --prefix LD_LIBRARY_PATH ":" "${lib.makeLibraryPath [ icu ]}" \
       --set-default ydb_dist $out/dist/utf8 \
       --set-default ydb_chset UTF-8 \
@@ -115,17 +202,24 @@ stdenv.mkDerivation rec {
       --set-default LC_ALL en_US.UTF-8
 
     cd $out/dist
-    ln -rfs yottadb mumps
-    cp ${yottadbGde} gde
-    for utl in dse ftok gde gtcm_gnp_server gtcm_pkdisp gtcm_play gtcm_server gtcm_shmclean gtmsecshr lke mupip semstat2 yottadb mumps ; do
-      ln -rfs $utl ../bin/
-      ln -rfs $utl ./utf8/
+    ln -rs yottadb mumps
+    sed -e "s,@@YDB_PKG_DIR@@,$out,g" "${yottadbGde}" > gde
+    chmod a+x gde
+    for utl in dse ftok gde gtcm_gnp_server gtcm_pkdisp gtcm_play gtcm_server gtcm_shmclean lke mupip semstat2 yottadb mumps ; do
+      ln -rs $utl ../bin/
+      ln -rs $utl ./utf8/
     done
-    ln -rfs libyottadb.so ../lib/
+    # The bin/yottadb will point to the yottadb-shell wrapper that
+    # has most of the runtime environment vars set automatically
+    # (also see the note on yottadb-shell makeWrapper)
+    ln -rsf "$out/dist/yottadb-shell" $out/bin/yottadb
+    ln -rsf "$out/dist/yottadb-shell" $out/bin/mumps
+    ln -rs libyottadb.so ../lib/
     for inc in $(find -maxdepth 1 -mindepth 1 -name '*.h') ; do
-      ln -rfs $inc ../include/
-      ln -rfs $inc ./utf8/
+      ln -rs $inc ../include/
+      ln -rs $inc ./utf8/
     done
+    ln -rs $out/dist/gtmsecshrdir $out/dist/utf8/gtmsecshrdir
 
     sed -e s,@@YDB_DIST@@,$out/dist,g \
         -e s,@@YDB_EXEDIR@@,$out/bin,g \
@@ -145,17 +239,17 @@ stdenv.mkDerivation rec {
     # Build M-mode dist
     export ydb_dist=$out/dist ydb_chset=M
     cd $ydb_dist
-    faketty ./yottadb -r %XCMD 'zhalt $s($zyre["r${version}":0,1:127)&$s($zch="M":0,1:126)'
-    faketty ./yottadb -r %XCMD 'd SILENT^%RSEL("*","SRC") s x="" f  s x=$o(%ZR(x)) q:x=""  w "["_$zch_"] Building "_x_" ...",! zl $tr(x,"%","_")'
+    faketty $out/bin/yottadb -r %XCMD 'zhalt $s($zyre["r${version}":0,1:127)&$s($zch="M":0,1:126)'
+    faketty $out/bin/yottadb -r %XCMD 'd SILENT^%RSEL("*","SRC") s x="" f  s x=$o(%ZR(x)) q:x=""  w "["_$zch_"] Building "_x_" ...",! zl $tr(x,"%","_")'
 
     # Build UTF8-mode dist
     for rtn in *.m *.gld *.dat ; do
-      ln -rfs $rtn utf8/
+      ln -rs $rtn utf8/
     done
     unset ydb_chset ydb_icu_version ydb_dist
     cd $out/dist/utf8
-    faketty ./yottadb -r %XCMD 'zhalt $s($zyre["$r{version}":0,1:127)&$s($zch="UTF-8":0,1:126)'
-    faketty ./yottadb -r %XCMD 'd SILENT^%RSEL("*","SRC") s x="" f  s x=$o(%ZR(x)) q:x=""  w "["_$zch_"] Building "_x_" ...",! zl $tr(x,"%","_")'
+    faketty $out/bin/yottadb -r %XCMD 'zhalt $s($zyre["$r{version}":0,1:127)&$s($zch="UTF-8":0,1:126)'
+    faketty $out/bin/yottadb -r %XCMD 'd SILENT^%RSEL("*","SRC") s x="" f  s x=$o(%ZR(x)) q:x=""  w "["_$zch_"] Building "_x_" ...",! zl $tr(x,"%","_")'
 
     # Wipe SRCs as Nix is read-only and sets both .o and .m to the same mtime, so compiler tries to compile all the time, but cannot write
     rm -f $out/dist/*.m $out/dist/utf8/*.m
@@ -171,14 +265,14 @@ stdenv.mkDerivation rec {
     fi
     cd $out/dist
     $ldcmd $ldflags -o $libYdbUtil.so *.o
-    ln -rfs $libYdbUtil.so ../lib/$libYdbUtil-m.so
+    ln -rs $libYdbUtil.so ../lib/$libYdbUtil-m.so
     cd $out/dist/utf8
     $ldcmd $ldflags -o $libYdbUtil.so *.o
-    ln -rfs $libYdbUtil.so ../../lib/$libYdbUtil-utf8.so
+    ln -rs $libYdbUtil.so ../../lib/$libYdbUtil-utf8.so
 
     # Default is UTF-8
     cd $out/lib
-    ln -rfs $libYdbUtil-utf8.so $libYdbUtil.so
+    ln -rs $libYdbUtil-utf8.so $libYdbUtil.so
   '';
 
   meta = with lib; {
