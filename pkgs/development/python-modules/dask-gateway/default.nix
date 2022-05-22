c464614b804feccc6bac6dfad8b7e3123c395d40
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-22 22:29:25 +0200

    python310Packages.dask-gateway: 0.9.0 -> 2022.4.0

--- pkgs/development/python-modules/dask-gateway/default.nix
+++ pkgs/development/python-modules/dask-gateway/default.nix
@@ -1,36 +1,55 @@
 { lib
 , buildPythonPackage
-, fetchPypi
-, aiohttp
+, click
 , dask
 , distributed
+, fetchFromGitHub
+, pythonOlder
+, pyyaml
+, tornado
 }:
 
 buildPythonPackage rec {
   pname = "dask-gateway";
   # update dask-gateway lock step with dask-gateway-server
-  version = "0.9.0";
+  version = "2022.4.0";
+  format = "setuptools";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "743f3b88dabe7d1503ac08aadf399eb9205df786b12c5175ea2e10c6ded7df22";
+  disabled = pythonOlder "3.7";
+
+  src = fetchFromGitHub {
+    owner = "dask";
+    repo = "dask-gateway";
+    rev = version;
+    hash = "sha256-Grjp7gt3Pos4cQSGV/Rynz6W/zebRI0OqDiWT4cTh8I=";
   };
 
+  sourceRoot = "${src.name}/${pname}";
+
   propagatedBuildInputs = [
-    aiohttp
+    click
     dask
     distributed
+    pyyaml
+    tornado
   ];
 
-  # tests requires cluster for testing
+  postPatch = ''
+    substituteInPlace requirements.txt \
+      --replace "click<8.1.0" "click"
+  '';
+
+  # Tests requires cluster for testing
   doCheck = false;
 
-  pythonImportsCheck = [ "dask_gateway" ];
+  pythonImportsCheck = [
+    "dask_gateway"
+  ];
 
   meta = with lib; {
     description = "A client library for interacting with a dask-gateway server";
     homepage = "https://gateway.dask.org/";
     license = licenses.bsd3;
-    maintainers = [ maintainers.costrouc ];
+    maintainers = with maintainers; [ costrouc ];
   };
 }
