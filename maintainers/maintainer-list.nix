f9b1cf5dddc09bb4e55a16e89063e48ce5fc8c99
Skyler Grey <skyler3665@gmail.com>
2022-05-23 09:47:30 +0100

    maintainers: add minion3665

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8473,6 +8473,17 @@
       fingerprint = "3196 83D3 9A1B 4DE1 3DC2  51FD FEA8 88C9 F5D6 4F62";
     }];
   };
+  minion3665 = {
+    name = "Skyler Grey";
+    email = "skyler3665@gmail.com";
+    matrix = "@minion3665:matrix.org";
+    github = "Minion3665";
+    githubId = 34243578;
+    keys = [{
+      longkeyid = "rsa4096/0x1AFD10256B3C714D";
+      fingerprint = "D520 AC8D 7C96 9212 5B2B  BD3A 1AFD 1025 6B3C 714D";
+    }];
+  };
   mir06 = {
     email = "armin.leuprecht@uni-graz.at";
     github = "mir06";
