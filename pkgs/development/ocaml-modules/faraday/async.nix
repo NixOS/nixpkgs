4f6ddfdd23f0c3571af65465c8937b2640f988ff
Aaron L. Zeng <me@bcc32.com>
2022-05-23 01:18:37 -0400

    Add Jane Street ocaml packages version 0.15
    - ocamlPackages.tls*: 0.15.2 -> 0.15.3
    - ocamlPackages.bistro: unstable-2021-11-13 -> unstable-2022-05-07
    - ocamlPackages.phylogenetics: 0.1.0 -> unstable-2022-05-06

--- pkgs/development/ocaml-modules/faraday/async.nix
+++ pkgs/development/ocaml-modules/faraday/async.nix
@@ -1,8 +1,13 @@
-{ buildDunePackage, faraday, core, async }:
+{ buildDunePackage, fetchpatch, faraday, core, async }:
 
 buildDunePackage rec {
   pname = "faraday-async";
   inherit (faraday) version src useDune2;
+
+  patches = fetchpatch {
+    url = "https://github.com/inhabitedtype/faraday/commit/31c3fc7f91ecca0f1deea10b40fd5e33bcd35f75.patch";
+    sha256 = "05z5gk7hxq7qvwg6f73hdhfcnx19p1dq6wqh8prx667y8zsaq2zj";
+  };
 
   minimumOCamlVersion = "4.08";
 
