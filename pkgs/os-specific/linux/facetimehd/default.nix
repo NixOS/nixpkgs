cd6418bfe124c8feb54a523ebdf49b81b8e06a43
Alyssa Ross <hi@alyssa.is>
2022-05-23 12:01:51 +0000

    linuxPackages.facetimehd: mark broken on Linux 5.18

--- pkgs/os-specific/linux/facetimehd/default.nix
+++ pkgs/os-specific/linux/facetimehd/default.nix
@@ -39,5 +39,6 @@ stdenv.mkDerivation rec {
     license = licenses.gpl2;
     maintainers = with maintainers; [ womfoo grahamc kraem ];
     platforms = [ "i686-linux" "x86_64-linux" ];
+    broken = kernel.kernelAtLeast "5.18";
   };
 }
