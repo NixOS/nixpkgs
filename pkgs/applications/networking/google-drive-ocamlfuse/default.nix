f1992e9c973719985067fade76da1d7bd6566dcf
Vincent Laporte <Vincent.Laporte@gmail.com>
2022-05-24 20:51:46 +0200

    ocamlPackages.google-drive-ocamlfuse: 0.7.27 → 0.7.28

--- pkgs/applications/networking/google-drive-ocamlfuse/default.nix
+++ pkgs/applications/networking/google-drive-ocamlfuse/default.nix
@@ -1,23 +1,24 @@
 { lib, buildDunePackage, fetchFromGitHub
 , ocaml_extlib, ocamlfuse, gapi_ocaml, ocaml_sqlite3
+, tiny_httpd
 , ounit
 }:
 
 buildDunePackage rec {
   pname = "google-drive-ocamlfuse";
-  version = "0.7.27";
+  version = "0.7.28";
 
   src = fetchFromGitHub {
     owner = "astrada";
     repo = "google-drive-ocamlfuse";
     rev = "v${version}";
-    sha256 = "sha256:0dnllnzdc1lg742a4rqhwscnhwm4kv0sqq35bmg59fyws08cj2z8";
+    sha256 = "sha256:1layxqz5iz6wnvk83579m1im9vzq608h7n1h04znpkan0fls6nad";
   };
 
   doCheck = true;
   checkInputs = [ ounit ];
 
-  buildInputs = [ ocaml_extlib ocamlfuse gapi_ocaml ocaml_sqlite3 ];
+  buildInputs = [ ocaml_extlib ocamlfuse gapi_ocaml ocaml_sqlite3 tiny_httpd ];
 
   meta = {
     inherit (src.meta) homepage;
