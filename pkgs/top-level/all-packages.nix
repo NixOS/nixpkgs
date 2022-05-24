985e152050e7e97c55d2da7102492841fd0cb920
Noah Snelson <noah.snelson@protonmail.com>
2022-05-24 15:36:32 -0700

    selectdefaultapplication: init at unstable-2021-08-12
    Creates the package definition for `selectdefaultapplication`, an
    application to select default applications for various file types on
    Linux systems.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10457,6 +10457,8 @@ with pkgs;
 
   seexpr = callPackage ../development/compilers/seexpr { };
 
+  selectdefaultapplication = libsForQt5.callPackage ../applications/misc/selectdefaultapplication { };
+
   semgrep = python3.pkgs.callPackage ../tools/security/semgrep { };
   semgrep-core = callPackage ../tools/security/semgrep/semgrep-core.nix { };
 
