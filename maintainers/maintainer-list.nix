23a5274eb7c4850085a64c92b3e33097a73b2655
Mars <mars@possums.xyz>
2022-05-24 23:27:24 +0000

    Update maintainer-list.nix

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1,53 +1,43 @@
 /* List of NixOS maintainers.
-  ```nix
-  handle = {
-  # Required
-  name = "Your name";
-  email = "address@example.org";
-
-  # Optional
-  matrix = "@user:example.org";
-  github = "GithubUsername";
-  githubId = your-github-id;
-  keys = [{
-  longkeyid = "rsa2048/0x0123456789ABCDEF";
-  fingerprint = "AAAA BBBB CCCC DDDD EEEE  FFFF 0000 1111 2222 3333";
-  }];
-  };
-  ```
-
-  where
-
-  - `handle` is the handle you are going to use in nixpkgs expressions,
-  - `name` is your, preferably real, name,
-  - `email` is your maintainer email address,
-  - `matrix` is your Matrix user ID,
-  - `github` is your GitHub handle (as it appears in the URL of your profile page, `https://github.com/<userhandle>`),
-  - `githubId` is your GitHub user ID, which can be found at `https://api.github.com/users/<userhandle>`,
-  - `keys` is a list of your PGP/GPG key IDs and fingerprints.
-
-  `handle == github` is strongly preferred whenever `github` is an acceptable attribute name and is short and convenient.
-
-  If `github` begins with a numeral, `handle` should be prefixed with an underscore.
-  ```nix
-  _1example = {
-  github = "1example";
-  };
-  ```
-
-  Add PGP/GPG keys only if you actually use them to sign commits and/or mail.
-
-  To get the required PGP/GPG values for a key run
-  ```shell
-  gpg --keyid-format 0xlong --fingerprint <email> | head -n 2
-  ```
-
-  !!! Note that PGP/GPG values stored here are for informational purposes only, don't use this file as a source of truth.
-
-  More fields may be added in the future, however, in order to comply with GDPR this file should stay as minimal as possible.
-
-  Please keep the list alphabetically sorted.
-  See `./scripts/check-maintainer-github-handles.sh` for an example on how to work with this data.
+    ```nix
+    handle = {
+      # Required
+      name = "Your name";
+      email = "address@example.org";
+      # Optional
+      matrix = "@user:example.org";
+      github = "GithubUsername";
+      githubId = your-github-id;
+      keys = [{
+        longkeyid = "rsa2048/0x0123456789ABCDEF";
+        fingerprint = "AAAA BBBB CCCC DDDD EEEE  FFFF 0000 1111 2222 3333";
+      }];
+    };
+    ```
+    where
+    - `handle` is the handle you are going to use in nixpkgs expressions,
+    - `name` is your, preferably real, name,
+    - `email` is your maintainer email address,
+    - `matrix` is your Matrix user ID,
+    - `github` is your GitHub handle (as it appears in the URL of your profile page, `https://github.com/<userhandle>`),
+    - `githubId` is your GitHub user ID, which can be found at `https://api.github.com/users/<userhandle>`,
+    - `keys` is a list of your PGP/GPG key IDs and fingerprints.
+    `handle == github` is strongly preferred whenever `github` is an acceptable attribute name and is short and convenient.
+    If `github` begins with a numeral, `handle` should be prefixed with an underscore.
+    ```nix
+    _1example = {
+      github = "1example";
+    };
+    ```
+    Add PGP/GPG keys only if you actually use them to sign commits and/or mail.
+    To get the required PGP/GPG values for a key run
+    ```shell
+    gpg --keyid-format 0xlong --fingerprint <email> | head -n 2
+    ```
+    !!! Note that PGP/GPG values stored here are for informational purposes only, don't use this file as a source of truth.
+    More fields may be added in the future, however, in order to comply with GDPR this file should stay as minimal as possible.
+    Please keep the list alphabetically sorted.
+    See `./scripts/check-maintainer-github-handles.sh` for an example on how to work with this data.
 */
 {
   _0qq = {
