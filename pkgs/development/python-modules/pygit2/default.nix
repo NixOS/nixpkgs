a6d35e3b7c99ad1baf84c4cb1d9f0e9b18a5b8b6
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-26 00:53:10 +0200

    python310Packages.pygit2: disable on older Python releases
    - add pythonImportsCheck
    - fix license entry

--- pkgs/development/python-modules/pygit2/default.nix
+++ pkgs/development/python-modules/pygit2/default.nix
@@ -1,12 +1,26 @@
-{ stdenv, lib, buildPythonPackage, fetchPypi, isPyPy, isPy3k, libgit2, cached-property, pytestCheckHook, cffi, cacert }:
+{ lib
+, stdenv
+, buildPythonPackage
+, cacert
+, cached-property
+, cffi
+, fetchPypi
+, isPyPy
+, libgit2
+, pytestCheckHook
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "pygit2";
   version = "1.9.2";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-IIlEM98RRkgarK434rDzu7/eoCbbL1UGEXC9mCPkCxk=";
+    hash = "sha256-IIlEM98RRkgarK434rDzu7/eoCbbL1UGEXC9mCPkCxk=";
   };
 
   preConfigure = lib.optionalString stdenv.isDarwin ''
@@ -19,14 +33,20 @@ buildPythonPackage rec {
 
   propagatedBuildInputs = [
     cached-property
-  ] ++ lib.optional (!isPyPy) cffi;
+  ] ++ lib.optional (!isPyPy) [
+    cffi
+  ];
 
-  propagatedNativeBuildInputs = lib.optional (!isPyPy) cffi;
+  propagatedNativeBuildInputs = lib.optional (!isPyPy) [
+    cffi
+  ];
 
-  checkInputs = [ pytestCheckHook ];
+  checkInputs = [
+    pytestCheckHook
+  ];
 
   disabledTestPaths = [
-    # disable tests that require networking
+    # Disable tests that require networking
     "test/test_repository.py"
     "test/test_credentials.py"
     "test/test_submodule.py"
@@ -44,10 +64,14 @@ buildPythonPackage rec {
   # https://github.com/NixOS/nixpkgs/pull/72544#issuecomment-582681068
   doCheck = false;
 
+  pythonImportsCheck = [
+    "pygit2"
+  ];
+
   meta = with lib; {
     description = "A set of Python bindings to the libgit2 shared library";
-    homepage = "https://pypi.python.org/pypi/pygit2";
-    license = licenses.gpl2;
+    homepage = "https://github.com/libgit2/pygit2";
+    license = licenses.gpl2Only;
     maintainers = with maintainers; [ ];
   };
 }
