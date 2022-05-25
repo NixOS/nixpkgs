aab7f92eb4d2647ea84c4861b3190c3fe23a7e14
R. Ryantm <ryantm-bot@ryantm.com>
2022-05-25 09:02:59 +0000

    mobile-broadband-provider-info: 20220315 -> 20220511

--- pkgs/data/misc/mobile-broadband-provider-info/default.nix
+++ pkgs/data/misc/mobile-broadband-provider-info/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "mobile-broadband-provider-info";
-  version = "20220315";
+  version = "20220511";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-H82sctF52xQnl4Nm9wG+HDx1Nf1aZYvFzIKCR8b2RcY=";
+    sha256 = "sha256-dfk+iGZh8DWYMsPigjyvqG505AgEJbjOCpw7DQqyp3Q=";
   };
 
   nativeBuildInputs = [
