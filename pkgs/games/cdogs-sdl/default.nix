a3a08aba665067b4eb03ab31ee3935f606ab94a1
R. Ryantm <ryantm-bot@ryantm.com>
2022-05-25 11:12:19 +0000

    cdogs-sdl: 0.13.0 -> 1.3.0

--- pkgs/games/cdogs-sdl/default.nix
+++ pkgs/games/cdogs-sdl/default.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cdogs";
-  version = "0.13.0";
+  version = "1.3.0";
 
   src = fetchFromGitHub {
     repo = "cdogs-sdl";
     owner = "cxong";
     rev = version;
-    sha256 = "sha256-7wNiDA/gOpqzSFWw8Bn6suC/f5RXdDTxPV49nCvOxas=";
+    sha256 = "sha256-fS+D/KfzVL66eY4eS/pHFuI9vx4wHHdzCDmtaiXg43Q=";
   };
 
   postPatch = ''
