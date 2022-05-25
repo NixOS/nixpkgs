11e68558021ac270697894c8d2b30f1e654e357d
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-05-25 14:03:38 +0200

    plex: 1.26.1.5798-99a4a6ac9 -> 1.26.2.5797-5bd057d2b

--- pkgs/servers/plex/raw.nix
+++ pkgs/servers/plex/raw.nix
@@ -12,16 +12,16 @@
 # server, and the FHS userenv and corresponding NixOS module should
 # automatically pick up the changes.
 stdenv.mkDerivation rec {
-  version = "1.26.1.5798-99a4a6ac9";
+  version = "1.26.2.5797-5bd057d2b";
   pname = "plexmediaserver";
 
   # Fetch the source
   src = if stdenv.hostPlatform.system == "aarch64-linux" then fetchurl {
     url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_arm64.deb";
-    hash = "sha256-Y2LW7NNdwD4t/R7dHOAEG2mxC4qFV6Z8VUh0APo8QNM=";
+    hash = "sha256-PycyEqRJrwndJvN90qFATkk3dmMThJqTPGOhZAtmmvE=";
   } else fetchurl {
     url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-    hash = "sha256-Chu4IULIvkmfMEV0LSg50i6usZJZI3UWOgCHQakbhaY=";
+    hash = "sha256-TbjRItXTJPZXwiNV3RFzAAC/AiFqXd9JNM6QEdPF2CI=";
   };
 
   outputs = [ "out" "basedb" ];
