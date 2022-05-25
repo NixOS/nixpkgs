2b89c6403a8bb4b89ec5d56ebb841d00e0be5c67
Thibault Gagnaux <tgagnaux@gmail.com>
2022-05-25 16:10:10 +0200

    libreoffice: refactor and document version reset workaround
    (cherry picked from commit 9f80b6d8fe87eccd39e7018512de6998c2765551)

--- pkgs/applications/office/libreoffice/darwin/update-utils.nix
+++ pkgs/applications/office/libreoffice/darwin/update-utils.nix
@@ -1,45 +1,54 @@
 { lib }:
 let
-# extractLatestVersionFromHtml :: String -> String
+  # extractLatestVersionFromHtml :: String -> String
   extractLatestVersionFromHtml = htmlString:
     let
       majorMinorPatchGroup = "([0-9]+\\.[0-9]+\\.[0-9]+)";
       splittedVersions = builtins.split "href=\"${majorMinorPatchGroup}" htmlString;
-      stableVersions = builtins.concatLists (builtins.filter (e: builtins.isList e) splittedVersions);
+      stableVersions = builtins.concatLists
+        (builtins.filter (e: builtins.isList e)
+          splittedVersions);
     in
-      if stableVersions == []
-        then abort "Failed to extract versions from html."
-        else lib.last (builtins.sort builtins.lessThan stableVersions);
+    if stableVersions == [ ]
+    then abort "Failed to extract versions from html."
+    else lib.last (builtins.sort builtins.lessThan stableVersions);
 
-# getHtml :: String -> String
+  # getHtml :: String -> String
   getHtml = url:
     builtins.readFile (builtins.fetchurl url);
 
-# getLatestStableVersion :: String
+  # getLatestStableVersion :: String
   getLatestStableVersion =
-    extractLatestVersionFromHtml (getHtml "https://download.documentfoundation.org/libreoffice/stable/");
+    extractLatestVersionFromHtml
+      (getHtml "https://download.documentfoundation.org/libreoffice/stable/");
 
-# extractSha256FromHtml :: String -> String
+  # extractSha256FromHtml :: String -> String
   extractSha256FromHtml = htmlString:
     let
       sha256 = (builtins.match ".*([0-9a-fA-F]{64}).*" htmlString);
     in
-      if sha256 == []
-        then abort "Failed to extract sha256 from html."
-        else builtins.head sha256;
+    if sha256 == [ ]
+    then abort "Failed to extract sha256 from html."
+    else builtins.head sha256;
 
-# getSha256 :: String -> String
+  # getSha256 :: String -> String
   getSha256 = dmgUrl: oldVersion: newVersion:
     extractSha256FromHtml (getHtml (getSha256Url dmgUrl oldVersion newVersion));
 
-# getSha256Url :: String -> String -> String -> String
+  # getSha256Url :: String -> String -> String -> String
   getSha256Url = dmgUrl: oldVersion: newVersion:
     (builtins.replaceStrings [ oldVersion ] [ newVersion ] dmgUrl) + ".sha256";
 
-# nullHash :: String
+  # nullHash :: String
   nullHash = "0000000000000000000000000000000000000000000000000000000000000000";
 in
 {
-  inherit extractLatestVersionFromHtml getHtml getLatestStableVersion
-    extractSha256FromHtml getSha256 getSha256Url nullHash;
+  inherit
+    extractLatestVersionFromHtml
+    getHtml
+    getLatestStableVersion
+    extractSha256FromHtml
+    getSha256
+    getSha256Url
+    nullHash;
 }
