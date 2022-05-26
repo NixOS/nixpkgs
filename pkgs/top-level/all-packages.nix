95d624394e73cb0b107e0f0214307979a018a997
ajs124 <git@ajs124.de>
2022-05-26 03:48:48 +0200

    appleseed: remove
    hasn't built on hydra since 2020-10-09 16:57:25

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5037,8 +5037,6 @@ with pkgs;
 
   apparix = callPackage ../tools/misc/apparix { };
 
-  appleseed = callPackage ../tools/graphics/appleseed { };
-
   apc-temp-fetch = with python3.pkgs; callPackage ../tools/networking/apc-temp-fetch { };
 
   arping = callPackage ../tools/networking/arping { };
