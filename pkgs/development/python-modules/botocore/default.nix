8925923f55f40313e8c1dcd374b7a659039eab5c
Bryan A. S <bryanasdev000@gmail.com>
2022-05-26 17:56:27 -0300

    python3Packages.botocore: 1.24.33 -> 1.26.9

--- pkgs/development/python-modules/botocore/default.nix
+++ pkgs/development/python-modules/botocore/default.nix
@@ -13,11 +13,11 @@
 
 buildPythonPackage rec {
   pname = "botocore";
-  version = "1.24.33"; # N.B: if you change this, change boto3 and awscli to a matching version
+  version = "1.26.9"; # N.B: if you change this, change boto3 and awscli to a matching version
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-6l/RgAggMKbDP6Gb8BHXKXDz7SPP/xtBQTBp4yV2gQM=";
+    sha256 = "sha256-/vOuGqniFNnae0QDXxOv4fcjmlsnJO12xuyaBigQIwc=";
   };
 
   propagatedBuildInputs = [
