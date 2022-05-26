be6214cd36d6456ed012c6eeb42abe0611daae9e
Bryan A. S <bryanasdev000@gmail.com>
2022-05-26 17:58:07 -0300

    python3Packages.aiobotocore: 2.3.0 -> 2.3.2

--- pkgs/development/python-modules/aiobotocore/default.nix
+++ pkgs/development/python-modules/aiobotocore/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "aiobotocore";
-  version = "2.3.0";
+  version = "2.3.2";
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-/D09YGFBC8GU0J7FReMLRGnV90dw+TespfaqReYqG/4=";
+    sha256 = "sha256-S26IMSFwpNJfb60AR+oFX9FfwZz/yRIK91z8ReHSvQc=";
   };
 
   # relax version constraints: aiobotocore works with newer botocore versions
