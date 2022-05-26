ef97620b668a5c8d1ac7fee56be7feab9c5e53cf
midchildan <git@midchildan.org>
2022-05-27 03:01:16 +0900

    xkcd-font: fix build

--- pkgs/data/fonts/xkcd-font/default.nix
+++ pkgs/data/fonts/xkcd-font/default.nix
@@ -11,11 +11,18 @@ in fetchFromGitHub {
   rev = "5632fde618845dba5c22f14adc7b52bf6c52d46d";
 
   postFetch = ''
-    tar xf $downloadedFile --strip=1
+    pushd $out
+
     install -Dm444 -t $out/share/fonts/opentype/ xkcd/build/xkcd.otf
     install -Dm444 -t $out/share/fonts/truetype/ xkcd-script/font/xkcd-script.ttf
+    for f in *; do
+      [[ "$f" == share ]] && continue
+      rm -rf "$f"
+    done
+
+    popd
   '';
-  sha256 = "0xhwa53aiz20763jb9nvbr2zq9k6jl69p07dc4b0apwrrwz0jfr1";
+  sha256 = "sha256-oBDP4LG8csjvzSk1+EcPH6hcp44dWtPzVFSifzBq8lU=";
 
   meta = with lib; {
     description = "The xkcd font";
