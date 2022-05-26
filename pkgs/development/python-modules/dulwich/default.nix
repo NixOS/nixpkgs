9c890c48f571450a8fbedb79b32bccf6954e4ae5
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-26 16:48:11 +0200

    python310Packages.dulwich: 0.20.41 -> 0.20.42

--- pkgs/development/python-modules/dulwich/default.nix
+++ pkgs/development/python-modules/dulwich/default.nix
@@ -17,7 +17,7 @@
 }:
 
 buildPythonPackage rec {
-  version = "0.20.41";
+  version = "0.20.42";
   pname = "dulwich";
   format = "setuptools";
 
@@ -25,7 +25,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-DKeu9YEhf1wNUklgZ7XFNSmIc/6xr384PsB5o4GwVgI=";
+    hash = "sha256-cro7YK5qVU0TMrO0CjRf6+FuxGnPYBS7RDtxmQLjPvA=";
   };
 
   LC_ALL = "en_US.UTF-8";
