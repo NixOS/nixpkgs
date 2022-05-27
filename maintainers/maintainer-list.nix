b7facc7d3b421a9e1d8abf55201ed4fce6632669
Mars <mars@possums.xyz>
2022-05-27 00:52:10 +0000

    Update maintainer-list.nix

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4,6 +4,7 @@
       # Required
       name = "Your name";
       email = "address@example.org";
+
       # Optional
       matrix = "@user:example.org";
       github = "GithubUsername";
@@ -13,8 +14,10 @@
         fingerprint = "AAAA BBBB CCCC DDDD EEEE  FFFF 0000 1111 2222 3333";
       }];
     };
+
     ```
     where
+
     - `handle` is the handle you are going to use in nixpkgs expressions,
     - `name` is your, preferably real, name,
     - `email` is your maintainer email address,
@@ -22,20 +25,27 @@
     - `github` is your GitHub handle (as it appears in the URL of your profile page, `https://github.com/<userhandle>`),
     - `githubId` is your GitHub user ID, which can be found at `https://api.github.com/users/<userhandle>`,
     - `keys` is a list of your PGP/GPG key IDs and fingerprints.
+
     `handle == github` is strongly preferred whenever `github` is an acceptable attribute name and is short and convenient.
+
     If `github` begins with a numeral, `handle` should be prefixed with an underscore.
     ```nix
     _1example = {
       github = "1example";
     };
     ```
+
     Add PGP/GPG keys only if you actually use them to sign commits and/or mail.
+
     To get the required PGP/GPG values for a key run
     ```shell
     gpg --keyid-format 0xlong --fingerprint <email> | head -n 2
     ```
+
     !!! Note that PGP/GPG values stored here are for informational purposes only, don't use this file as a source of truth.
+
     More fields may be added in the future, however, in order to comply with GDPR this file should stay as minimal as possible.
+
     Please keep the list alphabetically sorted.
     See `./scripts/check-maintainer-github-handles.sh` for an example on how to work with this data.
 */
