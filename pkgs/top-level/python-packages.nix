a2fd5a0c232cf55cf613357cb83f64a19ad9f460
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-05-27 23:55:43 +0200

    python310Packages.django-otp: init at 1.1.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2360,6 +2360,8 @@ in {
 
   django-oauth-toolkit = callPackage ../development/python-modules/django-oauth-toolkit { };
 
+  django-otp = callPackage ../development/python-modules/django-otp { };
+
   django-paintstore = callPackage ../development/python-modules/django-paintstore { };
 
   django-pglocks = callPackage ../development/python-modules/django-pglocks { };
