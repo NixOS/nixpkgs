1c01698dca8ac56b4a24937799d1fea1999665e5
sudosubin <sudosubin@gmail.com>
2022-05-28 15:33:44 +0900

    yabai: 3.3.10 -> 4.0.1

--- pkgs/os-specific/darwin/yabai/default.nix
+++ pkgs/os-specific/darwin/yabai/default.nix
@@ -1,17 +1,24 @@
-{ lib, stdenv, fetchFromGitHub, darwin, xxd }:
+{ lib, stdenv, fetchFromGitHub, runCommand, darwin, xxd }:
 
+let
+  buildSymlinks = runCommand "yabai-build-symlinks" { } ''
+    mkdir -p $out/bin
+    ln -s /usr/bin/xcrun /usr/bin/xcodebuild /usr/bin/tiffutil /usr/bin/qlmanage $out/bin
+  '';
+
+in
 stdenv.mkDerivation rec {
   pname = "yabai";
-  version = "3.3.10";
+  version = "4.0.1";
 
   src = fetchFromGitHub {
     owner = "koekeishiya";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-8O6//T894C32Pba3F2Z84Z6VWeCXlwml3xsXoIZGqL0=";
+    sha256 = "sha256-H1zMg+/VYaijuSDUpO6RAs/KLAAZNxhkfIC6CHk/xoI=";
   };
 
-  nativeBuildInputs = [ xxd ];
+  nativeBuildInputs = [ buildSymlinks xxd ];
 
   buildInputs = with darwin.apple_sdk.frameworks; [
     Carbon
@@ -25,6 +32,12 @@ stdenv.mkDerivation rec {
     mkdir -p $out/share/man/man1/
     cp ./bin/yabai $out/bin/yabai
     cp ./doc/yabai.1 $out/share/man/man1/yabai.1
+  '';
+
+  sandboxProfile = ''
+    (allow file-read* file-write* process-exec mach-lookup)
+    ; block homebrew dependencies
+    (deny file-read* file-write* process-exec mach-lookup (subpath "/usr/local") (with no-log))
   '';
 
   meta = with lib; {
