28be75ad0265fc22791c965f2e49547b96f4b8cf
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-28 11:58:02 +0200

    python310Packages.watermark: update meta

--- pkgs/development/python-modules/watermark/default.nix
+++ pkgs/development/python-modules/watermark/default.nix
@@ -10,27 +10,33 @@
 buildPythonPackage rec {
   pname = "watermark";
   version = "2.3.1";
+  format = "setuptools";
+
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "rasbt";
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-E3UxdGlxTcvkiKa3RoG9as6LybyW+QrCUZvA9VHwxlk=";
+    hash = "sha256-E3UxdGlxTcvkiKa3RoG9as6LybyW+QrCUZvA9VHwxlk=";
   };
 
   propagatedBuildInputs = [
     ipython
-  ] ++ lib.optionals (pythonOlder "3.8") [ importlib-metadata ];
+  ] ++ lib.optionals (pythonOlder "3.8") [
+    importlib-metadata
+  ];
 
   checkInputs =  [
     pytestCheckHook
   ];
 
-  pythonImportsCheck = [ "watermark" ];
+  pythonImportsCheck = [
+    "watermark"
+  ];
 
   meta = with lib; {
-    description = "IPython extension for printing date and time stamps, version numbers, and hardware information.";
+    description = "IPython extension for printing date and timestamps, version numbers, and hardware information";
     homepage = "https://github.com/rasbt/watermark";
     license = licenses.bsd3;
     maintainers = with maintainers; [ nphilou ];
