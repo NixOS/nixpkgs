8985f7e681ecfb048c7fdeeb4f7cfe1c5dd1be86
Rahul Butani <rrbutani@users.noreply.github.com>
2022-05-27 19:31:36 -0500

    maintainers: add rrbutani

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11681,6 +11681,15 @@
     githubId = 373566;
     name = "Ronuk Raval";
   };
+  rrbutani = {
+    email = "rrbutani+nix@gmail.com";
+    github = "rrbutani";
+    githubId = 7833358;
+    keys = [{
+      fingerprint = "7DCA 5615 8AB2 621F 2F32  9FF4 1C7C E491 479F A273";
+    }];
+    name = "Rahul Butani";
+  };
   rski = {
     name = "rski";
     email = "rom.skiad+nix@gmail.com";
