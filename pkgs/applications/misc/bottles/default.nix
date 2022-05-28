aea37b0a99dcbf00cb25d93ac1d559e7506ef2c7
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-05-28 17:01:07 +0200

    bottles: 2022.5.14-trento-1 -> 2022.5.28-trento

--- pkgs/applications/misc/bottles/default.nix
+++ pkgs/applications/misc/bottles/default.nix
@@ -20,13 +20,13 @@ let
 in
 python3Packages.buildPythonApplication rec {
   pname = "bottles";
-  version = "2022.5.14-trento-1";
+  version = "2022.5.28-trento";
 
   src = fetchFromGitHub {
     owner = "bottlesdevs";
     repo = pname;
     rev = version;
-    sha256 = "sha256-w5nSMJnt4WO1KOJvdjM1TYSOvPnogERgQWp1JVr3TZY=";
+    sha256 = "sha256-kStg/Ou/Eb7fQonyc3z+TCGpOBgZqfi2cEyu6lnuI7w=";
   };
 
   postPatch = ''
