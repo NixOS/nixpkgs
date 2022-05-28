254d2ff6a125dd99175c52f24282d76c81f21ab7
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-28 11:09:20 +0200

    python310Packages.pysqueezebox: enable tests

--- pkgs/development/python-modules/pysqueezebox/default.nix
+++ pkgs/development/python-modules/pysqueezebox/default.nix
@@ -1,22 +1,43 @@
-{ lib, fetchPypi, buildPythonPackage, pythonOlder, aiohttp }:
+{ lib
+, aiohttp
+, buildPythonPackage
+, fetchFromGitHub
+, pytest-asyncio
+, pytestCheckHook
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "pysqueezebox";
   version = "0.6.0";
+  format = "setuptools";
+
   disabled = pythonOlder "3.6";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-qc6ffWk62EF+IOLb2XVWtDrrZ0LVs7VtxJG1qrrUPPg=";
+  src = fetchFromGitHub {
+    owner = "rajlaud";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-0ArKVRy4H0NWShlQMziKvbHp9OjpAkEKp4zrvpVlXOk=";
   };
 
   propagatedBuildInputs = [
     aiohttp
   ];
 
-  # No tests in the Pypi distribution
-  doCheck = false;
-  pythonImportsCheck = [ "pysqueezebox" ];
+  checkInputs = [
+    pytest-asyncio
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [
+    "pysqueezebox"
+  ];
+
+  disabledTestPaths = [
+    # Tests require network access
+    "tests/test_integration.py"
+  ];
 
   meta = with lib; {
     description = "Asynchronous library to control Logitech Media Server";
