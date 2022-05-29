17cca50520f19e793ff1a9cb962193c21f4d277b
Alexander Bantyev <balsoft@balsoft.ru>
2022-05-29 18:54:03 +0400

    tracebox: stdenv -> gcc10Stdenv

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11035,7 +11035,7 @@ with pkgs;
 
   traceroute = callPackage ../tools/networking/traceroute { };
 
-  tracebox = callPackage ../tools/networking/tracebox { };
+  tracebox = callPackage ../tools/networking/tracebox { stdenv = gcc10StdenvCompat; };
 
   tracee = callPackage ../tools/security/tracee { };
 
