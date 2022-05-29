e8ba7bd1e43833ed2487d765ec1d081d9a2bea86
Alexander Bantyev <balsoft@balsoft.ru>
2022-05-29 17:21:27 +0400

    djv: stdenv -> gcc10Stdenv
    (cherry picked from commit a484f4f331e658b55776ad8577048c19be7427d5)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3319,7 +3319,7 @@ with pkgs;
     gst-plugins-good = gst_all_1.gst-plugins-good.override { gtkSupport = true; };
   };
 
-  djv = callPackage ../applications/graphics/djv { };
+  djv = callPackage ../applications/graphics/djv { stdenv = gcc10StdenvCompat; };
 
   dnschef = python3Packages.callPackage ../tools/networking/dnschef { };
 
