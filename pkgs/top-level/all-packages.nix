c6af26acfc7e15420c288dcc488a79afc816036d
Alexander Bantyev <balsoft@balsoft.ru>
2022-05-29 17:18:37 +0400

    clingcon: stdenv -> gcc10Stdenv

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3006,7 +3006,7 @@ with pkgs;
 
   clingo = callPackage ../applications/science/logic/potassco/clingo.nix { };
 
-  clingcon = callPackage ../applications/science/logic/potassco/clingcon.nix { };
+  clingcon = callPackage ../applications/science/logic/potassco/clingcon.nix { stdenv = gcc10StdenvCompat; };
 
   clprover = callPackage ../applications/science/logic/clprover/clprover.nix { };
 
