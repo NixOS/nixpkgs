7a073fa2a9d3656294297188748a44fb70330b95
Alexander Bantyev <balsoft@balsoft.ru>
2022-05-29 18:20:37 +0400

    openclonk: stdenv -> gcc10Stdenv
    (cherry picked from commit e86878e1b1b502e6ae73d91c615f5b87921edfa9)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31963,7 +31963,7 @@ with pkgs;
 
   openlierox = callPackage ../games/openlierox { };
 
-  openclonk = callPackage ../games/openclonk { };
+  openclonk = callPackage ../games/openclonk { stdenv = gcc10StdenvCompat; };
 
   openjk = callPackage ../games/openjk { };
 
