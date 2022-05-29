e2bb4ac359ff75d11e886b0ae752fc3c30539edb
Alexander Bantyev <balsoft@balsoft.ru>
2022-05-29 19:04:13 +0400

    yafaray-core: stdenv -> gcc10Stdenv
    (cherry picked from commit 8a6dbc8b7d4ea08956e651b02a824520fda7aed2)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11862,7 +11862,7 @@ with pkgs;
 
   xwinwrap = callPackage ../tools/X11/xwinwrap {};
 
-  yafaray-core = callPackage ../tools/graphics/yafaray-core { };
+  yafaray-core = callPackage ../tools/graphics/yafaray-core { stdenv = gcc10StdenvCompat; };
 
   yajsv = callPackage ../tools/misc/yajsv { };
 
