b29dbdf474b0e84e1a1d3b983ec5f83827af4e2d
Rick van Schijndel <rol3517@gmail.com>
2022-05-29 10:31:53 +0200

    treewide: pkgs/servers: mark broken for darwin
    (cherry picked from commit 879d278253424b2e63f8b1b7269a244d4e14aa9f)

--- pkgs/servers/rpiplay/default.nix
+++ pkgs/servers/rpiplay/default.nix
@@ -40,6 +40,7 @@ stdenv.mkDerivation rec {
   ];
 
   meta = with lib; {
+    broken = stdenv.isDarwin;
     homepage = "https://github.com/FD-/RPiPlay";
     description = "An open-source implementation of an AirPlay mirroring server.";
     license = licenses.gpl3Plus;
