3bd7bf6b8915748d5f0e583022470f1eec26a9e4
Emery Hemingway <ehmry@posteo.net>
2022-05-30 17:03:58 -0500

    ncdc: 1.22.1 -> 1.23

--- pkgs/applications/networking/p2p/ncdc/default.nix
+++ pkgs/applications/networking/p2p/ncdc/default.nix
@@ -1,25 +1,19 @@
-{ lib, stdenv, fetchurl, fetchpatch, ncurses, zlib, bzip2, sqlite, pkg-config, glib, gnutls }:
+{ lib, stdenv, fetchurl, fetchpatch, ncurses, zlib, bzip2, sqlite, pkg-config
+, glib, gnutls, perl, libmaxminddb }:
 
 stdenv.mkDerivation rec {
   pname = "ncdc";
-  version = "1.22.1";
+  version = "1.23";
 
   src = fetchurl {
     url = "https://dev.yorhel.nl/download/ncdc-${version}.tar.gz";
-    sha256 = "1bdgqd07f026qk6vpbxqsin536znd33931m3b4z44prlm9wd6pyi";
+    hash = "sha256-gEq65B/MqWnof2UEg65+OiN0Gdq70yCJfiX+iFHwoss=";
   };
 
-  patches = [
-    # Upstream fix for ncurses-6.3 support:
-    (fetchpatch {
-      name = "ncurses-6.3.patch";
-      url = "https://g.blicky.net/ncdc.git/patch/?id=4126dd51e90deb9e22dfd139cc4518a7812fcad6";
-      sha256 = "13hqkmhmbazj6cllb5b2ccgf51vsn5lri7jqkqc5xwivgcisfrij";
-    })
-  ];
+  nativeBuildInputs = [ perl pkg-config ];
+  buildInputs = [ ncurses zlib bzip2 sqlite glib gnutls libmaxminddb ];
 
-  nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ ncurses zlib bzip2 sqlite glib gnutls ];
+  configureFlags = [ "--with-geoip" ];
 
   meta = with lib; {
     description = "Modern and lightweight direct connect client with a friendly ncurses interface";
