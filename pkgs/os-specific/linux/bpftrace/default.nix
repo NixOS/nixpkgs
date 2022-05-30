d8b840a60f70d07de496572fa706ace44f90ef78
Dominique Martinet <asmadeus@codewreck.org>
2022-05-30 20:41:33 +0900

    bpftrace: 0.14.1 -> 0.15.0

--- pkgs/os-specific/linux/bpftrace/default.nix
+++ pkgs/os-specific/linux/bpftrace/default.nix
@@ -9,7 +9,7 @@
 
 stdenv.mkDerivation rec {
   pname = "bpftrace";
-  version = "0.14.1";
+  version = "0.15.0";
 
   # Cherry-picked from merged PR, remove this hook on next update
   # https://github.com/iovisor/bpftrace/pull/2242
@@ -33,13 +33,8 @@ stdenv.mkDerivation rec {
     owner  = "iovisor";
     repo   = "bpftrace";
     rev    = "v${version}";
-    sha256 = "sha256-QDqHAEVM/XHCFMS0jMLdKJfDUOpkUqONOf8+Fbd5dCY=";
+    sha256 = "sha256-9adZAKSn00W2yNwVDbVB1/O5Y+10c4EkVJGCHyd4Tgg=";
   };
-
-  # libbpf 0.6.0 relies on typeof in bpf/btf.h to pick the right version of
-  # btf_dump__new() but that's not valid c++.
-  # see https://github.com/iovisor/bpftrace/issues/2068
-  patches = [ ./btf-dump-new-0.6.0.patch ];
 
   buildInputs = with llvmPackages;
     [ llvm libclang
