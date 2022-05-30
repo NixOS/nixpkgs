80cc41b0999f1360c2ce7a790fd2c1139008aeff
Rick van Schijndel <rol3517@gmail.com>
2022-05-30 22:03:50 +0200

    mars: mark broken for x86_64

--- pkgs/games/mars/default.nix
+++ pkgs/games/mars/default.nix
@@ -30,6 +30,7 @@ stdenv.mkDerivation rec {
     chmod +x "$out/bin/mars"
   '';
   meta = with lib; {
+    broken = stdenv.isx86_64;
     homepage = "http://mars-game.sourceforge.net/";
     description = "A game about fighting with ships in a 2D space setting";
     license = licenses.gpl3Plus;
