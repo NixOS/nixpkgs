a27f49a4493c551aa7f4a3e31ec0bc3b865c4089
Robert Scott <code@humanleg.org.uk>
2022-05-31 00:09:50 +0100

    python3Packages.celery: skip hydra-failing tests on darwin
    these tests cause darwin hydra to hit open file limits
    
    (cherry picked from commit 71c7efa3877218ea3cd6ed07f95e804dd10d7daf)

--- pkgs/development/python-modules/celery/default.nix
+++ pkgs/development/python-modules/celery/default.nix
@@ -74,6 +74,11 @@ buildPythonPackage rec {
   disabledTests = [
     "msgpack"
     "test_check_privileges_no_fchown"
+  ] ++ lib.optionals stdenv.isDarwin [
+    # too many open files on hydra
+    "test_cleanup"
+    "test_with_autoscaler_file_descriptor_safety"
+    "test_with_file_descriptor_safety"
   ];
 
   pythonImportsCheck = [
