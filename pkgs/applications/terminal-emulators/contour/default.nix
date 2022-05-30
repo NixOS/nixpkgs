8873e330d28d6e2871ae07f9e5f869c1b642de4f
Rick van Schijndel <rol3517@gmail.com>
2022-05-30 21:38:19 +0200

    contour: mark broken

--- pkgs/applications/terminal-emulators/contour/default.nix
+++ pkgs/applications/terminal-emulators/contour/default.nix
@@ -20,7 +20,7 @@ mkDerivation rec {
 
   meta = with lib; {
     # never built on Hydra https://hydra.nixos.org/job/nixpkgs/staging-next/contour.x86_64-darwin
-    broken = (stdenv.isLinux && stdenv.isAarch64) || stdenv.isDarwin;
+    broken = true;
     description = "Modern C++ Terminal Emulator";
     homepage = "https://github.com/christianparpart/contour";
     changelog = "https://github.com/christianparpart/contour/blob/HEAD/Changelog.md";
