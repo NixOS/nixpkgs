f667f7fd6353d9fc2e36ff57fbf60fc305eeec88
R. Ryantm <ryantm-bot@ryantm.com>
2022-05-31 16:24:29 +0000

    python310Packages.approvaltests: 5.0.2 -> 5.2.0

--- pkgs/development/python-modules/approvaltests/default.nix
+++ pkgs/development/python-modules/approvaltests/default.nix
@@ -16,7 +16,7 @@
 }:
 
 buildPythonPackage rec {
-  version = "5.0.2";
+  version = "5.2.0";
   pname = "approvaltests";
   format = "setuptools";
 
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "approvals";
     repo = "ApprovalTests.Python";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-yEzfDbYHGm3Za4+yIk5lIWM4I+5TnqfluZj8OLN9oK0=";
+    sha256 = "sha256-PrO6NC+ARv0o1KHv+ekPwkEi4VpBIj+YjWRrCSFMHI8=";
   };
 
   propagatedBuildInputs = [
