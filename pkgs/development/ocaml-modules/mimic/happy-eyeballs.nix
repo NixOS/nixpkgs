f127fc6dbf7bbe09ffa4841f7d4eedd7e680ad61
Ulrik Strid <ulrik.strid@outlook.com>
2022-05-31 21:52:19 +0200

    ocamlPackages.mimic: 0.0.4 -> 0.0.5

---
+++ pkgs/development/ocaml-modules/mimic/happy-eyeballs.nix
@@ -0,0 +1,24 @@
+{ lib, buildDunePackage, mimic, happy-eyeballs-mirage
+}:
+
+buildDunePackage {
+  pname = "mimic-happy-eyeballs";
+
+  inherit (mimic) src version;
+
+  minimumOCamlVersion = "4.08";
+
+  strictDeps = true;
+
+  buildInputs = [
+    mimic
+    happy-eyeballs-mirage
+  ];
+  doCheck = false;
+
+  meta = {
+    description = "A happy-eyeballs integration into mimic";
+    maintainers = [ lib.maintainers.ulrikstrid ];
+    inherit (mimic.meta) license homepage;
+  };
+}
