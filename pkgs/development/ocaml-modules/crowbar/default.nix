d5bbac93fcb8507f08712e6d7d25918e3ebe98bc
Ulrik Strid <ulrik.strid@outlook.com>
2022-05-31 21:37:57 +0200

    ocamlPackages.crowbar: 0.2 -> 0.2.1

--- pkgs/development/ocaml-modules/crowbar/default.nix
+++ pkgs/development/ocaml-modules/crowbar/default.nix
@@ -1,28 +1,23 @@
-{ lib, stdenv, buildDunePackage, fetchFromGitHub, ocplib-endian, cmdliner, afl-persistent
+{ lib, stdenv, buildDunePackage, fetchFromGitHub, ocplib-endian, cmdliner_1_1, afl-persistent
 , calendar, fpath, pprint, uutf, uunf, uucp }:
 
 buildDunePackage rec {
   pname = "crowbar";
-  version = "0.2";
-
-  useDune2 = true;
+  version = "0.2.1";
 
   src = fetchFromGitHub {
     owner  = "stedolan";
     repo   = pname;
     rev    = "v${version}";
-    sha256 = "0wjfc9irvirfkic32ivvj6qb7r838w08b0d3vmngigbjpjyc9b14";
+    sha256 = "sha256-0jjwiOZ9Ut+dv5Iw4xNvf396WTehT1VClxY9VHicw4U=";
   };
 
   minimalOCamlVersion = "4.08";
 
-  # Fix tests with pprint ≥ 20220103
-  patches = [ ./pprint.patch ];
-
   # disable xmldiff tests, so we don't need to package unmaintained and legacy pkgs
   postPatch = "rm -rf examples/xmldiff";
 
-  propagatedBuildInputs = [ ocplib-endian cmdliner afl-persistent ];
+  propagatedBuildInputs = [ ocplib-endian cmdliner_1_1 afl-persistent ];
   checkInputs = [ calendar fpath pprint uutf uunf uucp ];
   # uunf is broken on aarch64
   doCheck = !stdenv.isAarch64;
