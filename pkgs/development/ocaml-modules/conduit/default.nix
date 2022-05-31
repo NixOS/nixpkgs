69bd123475cd11239ac09e856ca9e8c195413848
Ulrik Strid <ulrik.strid@outlook.com>
2022-05-31 21:36:58 +0200

    ocamlPackages.conduit: 4.0.2 -> 5.1.0

--- pkgs/development/ocaml-modules/conduit/default.nix
+++ pkgs/development/ocaml-modules/conduit/default.nix
@@ -5,14 +5,14 @@
 
 buildDunePackage rec {
   pname = "conduit";
-  version = "4.0.2";
+  version = "5.1.0";
   useDune2 = true;
 
   minimumOCamlVersion = "4.03";
 
   src = fetchurl {
-    url = "https://github.com/mirage/ocaml-conduit/releases/download/v${version}/conduit-v${version}.tbz";
-    sha256 = "2a37ffaa352a1e145ef3d80ac28661213c69a741b238623e59f29e3d5a12c537";
+    url = "https://github.com/mirage/ocaml-conduit/releases/download/v${version}/conduit-${version}.tbz";
+    sha256 = "sha256-5RyMPoecu+ngmYmwBZUJODLchmQgiAcuA+Wlmiojkc0=";
   };
 
   propagatedBuildInputs = [ astring ipaddr ipaddr-sexp sexplib uri logs ppx_sexp_conv ];
