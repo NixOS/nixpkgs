eee0e75af00cce8fb142e702e3c46bda8abb642f
Ulrik Strid <ulrik.strid@outlook.com>
2022-05-31 21:37:18 +0200

    ocamlPackages.awa: 0.0.5 -> 0.1.0

--- pkgs/development/ocaml-modules/awa/mirage.nix
+++ pkgs/development/ocaml-modules/awa/mirage.nix
@@ -1,5 +1,5 @@
 { buildDunePackage, awa
-, cstruct, mtime, lwt, mirage-flow, mirage-clock, logs
+, cstruct, mtime, lwt, mirage-flow, mirage-clock, mirage-time, logs
 }:
 
 buildDunePackage {
@@ -8,7 +8,7 @@ buildDunePackage {
   inherit (awa) version src useDune2;
 
   propagatedBuildInputs = [
-    awa cstruct mtime lwt mirage-flow mirage-clock logs
+    awa cstruct mtime lwt mirage-flow mirage-clock mirage-time logs
   ];
 
   inherit (awa) meta;
