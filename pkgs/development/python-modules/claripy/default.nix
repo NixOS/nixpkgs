ed0de8992c68ff4f8086965ed5c7c9b007e88aaa
Fabian Affolter <mail@fabian-affolter.ch>
2022-06-01 00:22:10 +0200

    python3Packages.claripy: 9.2.5 -> 9.2.6

--- pkgs/development/python-modules/claripy/default.nix
+++ pkgs/development/python-modules/claripy/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "claripy";
-  version = "9.2.5";
+  version = "9.2.6";
   format = "pyproject";
 
   disabled = pythonOlder "3.6";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-o5aNHYysIpccWhf09xuJ1WT2Hac4OPyztnKqPne1Vng=";
+    hash = "sha256-mBpAwKUclwvgOeGD6BvPUKZHdjngfvuHdtKY3nx7jzM=";
   };
 
   propagatedBuildInputs = [
