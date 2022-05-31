666ed16e091d80d6dfdb6e4c5e3bc56b3a0fea94
Ulrik Strid <ulrik.strid@outlook.com>
2022-05-31 21:34:09 +0200

    ocamlPackages.happy-eyeballs: 0.1.2 -> 0.3.0

--- pkgs/development/ocaml-modules/happy-eyeballs/lwt.nix
+++ pkgs/development/ocaml-modules/happy-eyeballs/lwt.nix
@@ -2,15 +2,32 @@
 , happy-eyeballs
 , cmdliner
 , dns-client
+, duration
+, domain-name
+, ipaddr
+, fmt
 , logs
 , lwt
+, mtime
 }:
 
 buildDunePackage {
   pname = "happy-eyeballs-lwt";
+
   inherit (happy-eyeballs) src version;
 
-  buildInputs = [ cmdliner ];
+  minimalOCamlVersion = "4.08";
+
+  strictDeps = true;
+
+  buildInputs = [
+    cmdliner
+    duration
+    domain-name
+    ipaddr
+    fmt
+    mtime
+  ];
 
   propagatedBuildInputs = [
     dns-client
@@ -19,8 +36,10 @@ buildDunePackage {
     lwt
   ];
 
+  doCheck = true;
+
   meta = happy-eyeballs.meta // {
+    mainProgram = "happy_eyeballs_client";
     description = "Connecting to a remote host via IP version 4 or 6 using Lwt_unix";
   };
-
 }
