89af76699ce325b61baa89cd6eeed31ff2378c3e
Ulrik Strid <ulrik.strid@outlook.com>
2022-05-31 12:43:41 +0200

    ocamlPackages.bls12-381: 1.1.0 -> 3.0.1

--- pkgs/development/ocaml-modules/bls12-381/default.nix
+++ pkgs/development/ocaml-modules/bls12-381/default.nix
@@ -1,23 +1,34 @@
-{ lib, buildDunePackage, fetchFromGitLab, ff-sig, zarith }:
+{ lib, buildDunePackage, fetchFromGitLab, ff-sig, ff-pbt, zarith
+, zarith_stubs_js, integers, integers_stubs_js, hex, alcotest }:
 
 buildDunePackage rec {
   pname = "bls12-381";
-  version = "1.1.0";
+  version = "3.0.1";
 
   src = fetchFromGitLab {
     owner = "dannywillems";
     repo = "ocaml-bls12-381";
     rev = "22247018c0651ea62ae898c8ffcc388cc73f758f";
-    sha256 = "ku6Rc+/lwFDoHTZTxgkhiF+kLkagi7944ntcu9vXWgI=";
+    sha256 = "sha256-ScKEkv+a83XJgcK9xiUqVQECoGT3PPx9stzz9QReu5I=";
   };
 
-  useDune2 = true;
+  strictDeps = true;
 
   minimalOCamlVersion = "4.08";
 
-  propagatedBuildInputs = [
+  buildInputs = [
     ff-sig
+    hex
+    integers
+    integers_stubs_js
     zarith
+    zarith_stubs_js
+  ]
+  ++ checkInputs; # dune builds tests at build-time
+
+  checkInputs = [
+    alcotest
+    ff-pbt
   ];
 
   doCheck = true;
