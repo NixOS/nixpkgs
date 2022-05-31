bbe40bf6958d0ef4d109eadf1a0242e423802201
Ulrik Strid <ulrik.strid@outlook.com>
2022-05-31 22:44:51 +0200

    ocamlPackages.mirage-xen: 6.0.0 -> 7.0.0

--- pkgs/development/ocaml-modules/mirage-xen/default.nix
+++ pkgs/development/ocaml-modules/mirage-xen/default.nix
@@ -19,26 +19,14 @@
 
 buildDunePackage rec {
   pname = "mirage-xen";
-  version = "6.0.0";
+  version = "7.0.0";
 
   useDune2 = true;
 
   src = fetchurl {
-    url = "https://github.com/mirage/mirage-xen/releases/download/v${version}/mirage-xen-v${version}.tbz";
-    sha256 = "f991e972059b27993c287ad010d9281fee061efaa1dd475d0955179f93710fbd";
+    url = "https://github.com/mirage/mirage-xen/releases/download/v${version}/mirage-xen-${version}.tbz";
+    sha256 = "sha256-zNEZSVM7e7y+XzXozdGJBx6aRi1M/E7kbMeJGzL2iI8=";
   };
-
-  patches = [
-    ./makefile-no-opam.patch
-    ./pkg-config.patch
-  ];
-
-  # can't handle OCAMLFIND_DESTDIR with substituteAll
-  postPatch = ''
-    substituteInPlace lib/bindings/mirage-xen.pc \
-      --replace "@out@" "$out" \
-      --replace "@OCAMLFIND_DESTDIR@" "$OCAMLFIND_DESTDIR"
-  '';
 
   minimumOCamlVersion = "4.08";
 
@@ -61,13 +49,6 @@ buildDunePackage rec {
     duration
     (ocaml-freestanding.override { target = "xen"; })
   ];
-
-  # Move pkg-config files into their well-known location.
-  # This saves us an extra setup hook and causes no issues
-  # since we patch all relative paths out of the .pc file.
-  postInstall = ''
-    mv $OCAMLFIND_DESTDIR/pkgconfig $out/lib/pkgconfig
-  '';
 
   meta = with lib; {
     description = "Xen core platform libraries for MirageOS";
