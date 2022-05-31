d81b3f58647e3127329b801f0210a00f2445020b
Robert Schütz <nix@dotlambda.de>
2022-05-31 03:00:27 +0000

    python310Packages.overly: init at 0.1.85

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6380,6 +6380,8 @@ in {
 
   openapi-core = callPackage ../development/python-modules/openapi-core { };
 
+  overly = callPackage ../development/python-modules/overly { };
+
   pandas-stubs = callPackage ../development/python-modules/pandas-stubs { };
 
   parameterizedtestcase = callPackage ../development/python-modules/parameterizedtestcase { };
