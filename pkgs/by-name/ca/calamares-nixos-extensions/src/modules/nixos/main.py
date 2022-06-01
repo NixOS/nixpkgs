d4ba4e3dc34fdba7eb6965e0e4e5106812ff7e91
Victor Fuentes <vmfuentes64@gmail.com>
2022-06-01 10:38:28 -0400

    Remove typo in config

--- pkgs/by-name/ca/calamares-nixos-extensions/src/modules/nixos/main.py
+++ pkgs/by-name/ca/calamares-nixos-extensions/src/modules/nixos/main.py
@@ -302,7 +302,7 @@ cfgtail = """  # Some programs need SUID wrappers, can be configured further or 
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leavecatenate(variables, "bootdev", bootdev)
+  # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
