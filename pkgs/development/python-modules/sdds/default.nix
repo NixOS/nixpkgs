cff8ae568806200e2484ef7017e8fae322aef26f
Fabian Affolter <mail@fabian-affolter.ch>
2022-06-01 15:42:07 +0200

    python310Packages.sdds: disable on older Python releases
    (cherry picked from commit 3a15f059278768e6920a27d1fd85a80f4917c693)

--- pkgs/development/python-modules/sdds/default.nix
+++ pkgs/development/python-modules/sdds/default.nix
@@ -3,6 +3,7 @@
 , fetchFromGitHub
 , numpy
 , pytestCheckHook
+, pythonOlder
 }:
 
 buildPythonPackage rec {
@@ -10,20 +11,29 @@ buildPythonPackage rec {
   version = "0.3.0";
   format = "setuptools";
 
+  disabled = pythonOlder "3.7";
+
   src = fetchFromGitHub {
     owner = "pylhc";
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-l9j+YJ5VNMzL6JW59kq0hQS7XIj53UxW5bNnfdURz/o=";
+    hash = "sha256-l9j+YJ5VNMzL6JW59kq0hQS7XIj53UxW5bNnfdURz/o=";
   };
 
-  propagatedBuildInputs = [ numpy ];
+  propagatedBuildInputs = [
+    numpy
+  ];
 
-  pythonImportsCheck = [ "sdds" ];
-  checkInputs = [ pytestCheckHook ];
+  checkInputs = [
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [
+    "sdds"
+  ];
 
   meta = with lib; {
-    description = "Python 3 package to handle SDDS files";
+    description = "Module to handle SDDS files";
     homepage = "https://pylhc.github.io/sdds/";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ veprbl ];
