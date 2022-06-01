eae11baeafa591f344eeef3c0db9d7afe0c78e8b
Sandro Jäckel <sandro.jaeckel@sap.com>
2022-06-02 01:18:04 +0200

    python310Packages.tempest: 29.2.0 -> 30.1.0

--- pkgs/development/python-modules/tempest/default.nix
+++ pkgs/development/python-modules/tempest/default.nix
@@ -1,7 +1,6 @@
 { lib
 , buildPythonApplication
 , fetchPypi
-, fetchpatch
 , pbr
 , cliff
 , jsonschema
@@ -29,20 +28,12 @@
 
 buildPythonApplication rec {
   pname = "tempest";
-  version = "29.2.0";
+  version = "30.1.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0521d3042360c0fb469b16f99174a9abddbae8a2d2a81268cfc664f1ccfdd0f9";
+    sha256 = "sha256-yaNw3c3d8TsSK9cXF9Lw9rKEcqx8YC6cigPURNdJy0A=";
   };
-
-  patches = [
-    # remove need for unittest2
-    (fetchpatch {
-      url = "https://github.com/openstack/tempest/commit/cd3745c27b7d8fcdaffc72b965a3d803d9ee12c2.patch";
-      sha256 = "sha256-UwUmyFZokH66Xqfsj982MBHb0w7x6v4SAtXlqA5dpnk=";
-    })
-  ];
 
   propagatedBuildInputs = [
     pbr
