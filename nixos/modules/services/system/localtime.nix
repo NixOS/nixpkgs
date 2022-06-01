ffae8569b01f478dd5e863ccd0ef783a573e3f70
Bernardo Meurer <bernardo@meurer.org>
2022-06-01 13:20:16 -0700

    nixos/localtimed: hopefully fix geoclue

--- nixos/modules/services/system/localtime.nix
+++ nixos/modules/services/system/localtime.nix
@@ -3,30 +3,26 @@
 with lib;
 
 let
-  cfg = config.services.localtime;
+  cfg = config.services.localtimed;
 in {
   options = {
-    services.localtime = {
+    services.localtimed = {
       enable = mkOption {
         type = types.bool;
         default = false;
         description = ''
-          Enable <literal>localtime</literal>, simple daemon for keeping the system
-          timezone up-to-date based on the current location. It uses geoclue2 to
-          determine the current location and systemd-timedated to actually set
-          the timezone.
+          Enable <literal>localtimed</literal>, a simple daemon for keeping the
+          system timezone up-to-date based on the current location. It uses
+          geoclue2 to determine the current location.
         '';
       };
     };
   };
 
   config = mkIf cfg.enable {
-    services.geoclue2 = {
-      enable = true;
-      appConfig.localtime = {
-        isAllowed = true;
-        isSystem = true;
-      };
+    services.geoclue2.appConfig.localtimed = {
+      isAllowed = true;
+      isSystem = true;
     };
 
     # Install the polkit rules.
@@ -34,16 +30,6 @@ in {
     # Install the systemd unit.
     systemd.packages = [ pkgs.localtime ];
 
-    users.users.localtimed = {
-      description = "localtime daemon";
-      isSystemUser = true;
-      group = "localtimed";
-    };
-    users.groups.localtimed = {};
-
-    systemd.services.localtime = {
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig.Restart = "on-failure";
-    };
+    systemd.services.localtime.wantedBy = [ "multi-user.target" ];
   };
 }
