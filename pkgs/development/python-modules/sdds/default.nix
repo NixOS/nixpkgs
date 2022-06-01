631756fefab2db7664ca690a3f64137814687793
R. Ryantm <ryantm-bot@ryantm.com>
2022-06-01 08:28:14 +0000

    python310Packages.sdds: 0.2.1 -> 0.3.0
    (cherry picked from commit 7b3aa7abd6c648ee58bbac9ac955d4e8ebdf4ef5)

--- pkgs/development/python-modules/sdds/default.nix
+++ pkgs/development/python-modules/sdds/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "sdds";
-  version = "0.2.1";
+  version = "0.3.0";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "pylhc";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-JcxcF0tDigZz3upzE7rPDynCH45dnLk/zpS0a2dOwRU=";
+    rev = "refs/tags/${version}";
+    sha256 = "sha256-l9j+YJ5VNMzL6JW59kq0hQS7XIj53UxW5bNnfdURz/o=";
   };
 
   propagatedBuildInputs = [ numpy ];
