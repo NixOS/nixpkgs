9488ef9504802bb417d40e2935069b9e4c4ed0bb
Aaron Jheng <wentworth@outlook.com>
2022-06-02 12:39:25 +0000

    i3cat: remove

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -571,6 +571,7 @@ mapAliases ({
 
   ### I ###
 
+  i3cat = throw "i3cat has been dropped due to the lack of maintanence from upstream since 2016"; # Added 2022-06-02
   iana_etc = throw "'iana_etc' has been renamed to/replaced by 'iana-etc'"; # Converted to throw 2022-02-22
   iasl = throw "iasl has been removed, use acpica-tools instead"; # Added 2021-08-08
   icecat-bin = throw "icecat-bin has been removed, the binary builds are not maintained upstream"; # Added 2022-02-15
