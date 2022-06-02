cccdb0558d6929177c2fafd2f92ac2cc84e32011
phaer <hello@phaer.org>
2022-06-02 12:39:53 +0200

    python3Packages.cron-descriptor: init at 1.2.24

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1974,6 +1974,8 @@ in {
 
   criticality-score = callPackage ../development/python-modules/criticality-score { };
 
+  cron-descriptor = callPackage ../development/python-modules/cron-descriptor { };
+
   croniter = callPackage ../development/python-modules/croniter { };
 
   crossplane = callPackage ../development/python-modules/crossplane { };
