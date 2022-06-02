713b7b04045268f155dc27b3a5e42ec3fef09104
Vincent Laporte <Vincent.Laporte@gmail.com>
2022-06-02 09:33:44 +0200

    ocamlPackages.wasm: 1.1.1 → 2.0.0
    (cherry picked from commit 6c36c16a81b03f73a4ab91a83588a05b72341b54)

--- pkgs/development/ocaml-modules/wasm/default.nix
+++ pkgs/development/ocaml-modules/wasm/default.nix
@@ -1,19 +1,18 @@
 { stdenv, lib, fetchFromGitHub, ocaml, findlib, ocamlbuild }:
 
-if lib.versionOlder ocaml.version "4.03"
-|| lib.versionOlder "4.13" ocaml.version
+if lib.versionOlder ocaml.version "4.08"
 then throw "wasm is not available for OCaml ${ocaml.version}"
 else
 
 stdenv.mkDerivation rec {
   pname = "ocaml${ocaml.version}-wasm";
-  version = "1.1.1";
+  version = "2.0.0";
 
   src = fetchFromGitHub {
     owner = "WebAssembly";
     repo = "spec";
     rev = "opam-${version}";
-    sha256 = "1kp72yv4k176i94np0m09g10cviqp2pnpm7jmiq6ik7fmmbknk7c";
+    sha256 = "sha256:09s0v79x0ymzcp2114zkm3phxavdfnkkq67qz1ndnknbkziwqf3v";
   };
 
   nativeBuildInputs = [ ocaml findlib ocamlbuild ];
