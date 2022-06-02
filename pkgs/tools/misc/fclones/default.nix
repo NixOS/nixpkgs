58aef42768cb7c72b1da9f6ef1367fbcca52d88f
Harsh Shandilya <me@msfjarvis.dev>
2022-06-02 14:11:27 +0530

    fclones: 0.24.0 -> 0.25.0

--- pkgs/tools/misc/fclones/default.nix
+++ pkgs/tools/misc/fclones/default.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "fclones";
-  version = "0.24.0";
+  version = "0.25.0";
 
   src = fetchFromGitHub {
     owner = "pkolaczk";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-yiOoknhKGRGUgZShz9gGOsaX1+IOr23UFviIx/Uva6M=";
+    sha256 = "sha256-7MNVLfBH6hWoNy+UQzK6gwZuPbBPMfG660my+E6FT5Q=";
   };
 
-  cargoSha256 = "sha256-CEfFCYUmFv1cG2DEAfgK+wr3UTgFMZlEQVqUaWnqzdk=";
+  cargoSha256 = "sha256-8Ur3KwGuIY8QAGNTcyTpFg2C1CBcIpZJp6EO9g8XuE8=";
 
   buildInputs = lib.optionals stdenv.isDarwin [
     AppKit
