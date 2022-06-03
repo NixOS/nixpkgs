759cf488a87ed74b5d7ab0c2ec7eee8b95686eb8
John Soo <john.soo@arista.com>
2022-06-03 15:31:03 -0700

    maintainers: add jsoo1

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6360,6 +6360,12 @@
     github = "jsimonetti";
     githubId = 5478838;
   };
+  jsoo1 = {
+    email = "jsoo1@asu.edu";
+    github = "jsoo1";
+    name = "John Soo";
+    githubId = 10039785;
+  };
   jtcoolen = {
     email = "jtcoolen@pm.me";
     name = "Julien Coolen";
