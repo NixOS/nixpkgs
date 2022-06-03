4cd69118d1307685c1555837ac2091ab2973711c
Martino Fontana <tinozzo123@tutanota.com>
2022-06-03 20:16:29 +0200

    tinyxml-2: 6.0.0 -> 9.0.0

--- pkgs/development/libraries/tinyxml-2/default.nix
+++ pkgs/development/libraries/tinyxml-2/default.nix
@@ -2,16 +2,24 @@
 
 stdenv.mkDerivation rec {
   pname = "tinyxml-2";
-  version = "6.0.0";
+  version = "9.0.0";
 
   src = fetchFromGitHub {
     repo = "tinyxml2";
     owner = "leethomason";
     rev = version;
-    sha256 = "031fmhpah449h3rkyamzzdpzccrrfrvjb4qn6vx2vjm47jwc54qv";
+    sha256 = "sha256-AQQOctXi7sWIH/VOeSUClX6hlm1raEQUOp+VoPjLM14=";
   };
 
   nativeBuildInputs = [ cmake ];
+
+  cmakeFlags = [
+    # the cmake package does not handle absolute CMAKE_INSTALL_INCLUDEDIR correctly
+    # (setting it to an absolute path causes include files to go to $out/$out/include,
+    #  because the absolute path is interpreted with root at $out).
+    "-DCMAKE_INSTALL_INCLUDEDIR=include"
+    "-DCMAKE_INSTALL_LIBDIR=lib"
+  ];
 
   meta = {
     description = "A simple, small, efficient, C++ XML parser";
