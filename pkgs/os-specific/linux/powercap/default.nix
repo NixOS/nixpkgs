e935bf72b52536f347cb5283334e3299e2c9d9be
Alexander Shpilkin <ashpilkin@gmail.com>
2022-06-03 20:54:33 +0300

    fixup f8c8b28d130 use upstream fix

--- pkgs/os-specific/linux/powercap/default.nix
+++ pkgs/os-specific/linux/powercap/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, cmake }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch, cmake }:
 
 stdenv.mkDerivation rec {
   pname = "powercap";
@@ -11,18 +11,20 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-VvepbABc7daRE0/sJqsCb+m2my8O3B1ICXywBqsjSO8=";
   };
 
+  # in master post 0.6.0, see https://github.com/powercap/powercap/issues/8
+  patches = [
+    (fetchpatch {
+      name = "fix-pkg-config.patch";
+      url = "https://github.com/powercap/powercap/commit/278dceb51635686e343edfc357b6020533fff299.patch";
+      sha256 = "0h62j63xdn0iqyx4xbia6hlmdjn45camb82z4vv6sb37x9sph7rg";
+    })
+  ];
+
   nativeBuildInputs = [ cmake ];
 
   cmakeFlags = [
     "-DBUILD_SHARED_LIBS=On"
   ];
-
-  # https://github.com/powercap/powercap/issues/8
-  postPatch = ''
-    substituteInPlace CMakeLists.txt \
-      --replace '\$'{prefix}/'$'{CMAKE_INSTALL_LIBDIR} '$'{CMAKE_INSTALL_FULL_LIBDIR} \
-      --replace '\$'{prefix}/'$'{CMAKE_INSTALL_INCLUDEDIR} '$'{CMAKE_INSTALL_FULL_INCLUDEDIR}
-  '';
 
   meta = with lib; {
     description = "Tools and library to read/write to the Linux power capping framework (sysfs interface)";
