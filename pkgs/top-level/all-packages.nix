5b898eb8deadc8ba46342c715615af60e62a1e2b
jevin <jevin@quickjack.ca>
2022-06-03 06:53:10 -0400

    sunpaper: init at unstable-2022-04-01

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10496,6 +10496,8 @@ with pkgs;
 
   sunwait = callPackage ../applications/misc/sunwait { };
 
+  sunpaper = callPackage ../tools/X11/sunpaper { };
+
   surface-control = callPackage ../applications/misc/surface-control { };
 
   syntex = callPackage ../tools/graphics/syntex {};
