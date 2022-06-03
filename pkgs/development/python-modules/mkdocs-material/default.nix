f2b9dde8606af198ffe71becf537b8aac6fb8f8b
R. Ryantm <ryantm-bot@ryantm.com>
2022-06-03 05:28:37 +0000

    python310Packages.mkdocs-material: 8.2.16 -> 8.3.0

--- pkgs/development/python-modules/mkdocs-material/default.nix
+++ pkgs/development/python-modules/mkdocs-material/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonApplication rec {
   pname = "mkdocs-material";
-  version = "8.2.16";
+  version = "8.3.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -22,7 +22,7 @@ buildPythonApplication rec {
     owner = "squidfunk";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-ZRk1PGLUg3StD7JhuI+3hRc4lWwEO3CrMUfLSqFVgVk=";
+    hash = "sha256-0N/IZ5fSK7xefwkROoG/udP3qTGJSQKbe+wcRiU752M=";
   };
 
   propagatedBuildInputs = [
