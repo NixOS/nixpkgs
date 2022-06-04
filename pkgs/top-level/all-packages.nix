436ca289c402472c782e20888c3a458fdbbca243
Robert Schütz <nix@dotlambda.de>
2022-06-04 23:10:15 +0000

    poetry: don't use poetry2nix

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14510,9 +14510,8 @@ with pkgs;
 
   pew = callPackage ../development/tools/pew {};
 
-  poetry = callPackage ../development/tools/poetry2nix/poetry2nix/pkgs/poetry {
-    python = python3;
-  };
+  poetry = with python3.pkgs; toPythonApplication poetry;
+
   poetry2nix = callPackage ../development/tools/poetry2nix/poetry2nix {
     inherit pkgs lib;
   };
