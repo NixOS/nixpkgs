8c1d9e4a2c8c1f90d867726b41c1708d82c2be1c
Fabian Affolter <mail@fabian-affolter.ch>
2022-06-04 23:46:14 +0200

    caf: 0.18.5 -> 0.18.6

--- pkgs/development/libraries/caf/default.nix
+++ pkgs/development/libraries/caf/default.nix
@@ -1,33 +1,44 @@
-{ lib, stdenv, fetchFromGitHub, cmake, openssl }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, openssl
+}:
 
 stdenv.mkDerivation rec {
   pname = "actor-framework";
-  version = "0.18.5";
+  version = "0.18.6";
 
   src = fetchFromGitHub {
     owner = "actor-framework";
     repo = "actor-framework";
     rev = version;
-    sha256 = "04b4kjisb5wzq6pilh8xzbxn7qcjgppl8k65hfv0zi0ja8fyp1xk";
+    hash = "sha256-AW8AXX9t9vYv8tZvFJvrghmz6tZdfbX4hVc2QoBAvhQ=";
   };
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [
+    cmake
+  ];
 
-  buildInputs = [ openssl ];
+  buildInputs = [
+    openssl
+  ];
 
   cmakeFlags = [
     "-DCAF_ENABLE_EXAMPLES:BOOL=OFF"
   ];
 
   doCheck = true;
+
   checkTarget = "test";
+
   preCheck = ''
     export LD_LIBRARY_PATH=$PWD/libcaf_core:$PWD/libcaf_io
     export DYLD_LIBRARY_PATH=$PWD/libcaf_core:$PWD/libcaf_io
   '';
 
   meta = with lib; {
-    description = "An open source implementation of the actor model in C++";
+    description = "Implementation of the actor model";
     homepage = "http://actor-framework.org/";
     license = licenses.bsd3;
     platforms = platforms.unix;
