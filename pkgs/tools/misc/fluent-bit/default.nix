576e3b5aec1db848cc3095b5f18d5304e1b0b74a
Mario Rodas <marsam@users.noreply.github.com>
2022-06-04 04:20:00 +0000

    fluent-bit: 1.8.11 -> 1.9.3
    https://fluentbit.io/announcements/v1.9.3/

--- pkgs/tools/misc/fluent-bit/default.nix
+++ pkgs/tools/misc/fluent-bit/default.nix
@@ -1,34 +1,15 @@
-{ lib, stdenv, fetchFromGitHub, fetchpatch, cmake, flex, bison, systemd, openssl }:
+{ lib, stdenv, fetchFromGitHub, cmake, flex, bison, systemd, openssl }:
 
 stdenv.mkDerivation rec {
   pname = "fluent-bit";
-  version = "1.8.11";
+  version = "1.9.3";
 
   src = fetchFromGitHub {
     owner = "fluent";
     repo = "fluent-bit";
     rev = "v${version}";
-    sha256 = "sha256-DULXfkddBdCvTWkuWXjSTEujRZ3mVVzy//qeB3j0Vz8=";
+    sha256 = "sha256-CMkVIWaD4Zt6SJ/4PLGrFDhirqeLbXcVa+96wsAYN/k=";
   };
-
-  patches = lib.optionals stdenv.isDarwin [
-    # Fix compilations errors on darwin
-    (fetchpatch {
-      url = "https://github.com/calyptia/cmetrics/commit/4f0f7ae2eeec148a69156f9fcc05d64bf249d11e.patch";
-      sha256 = "sha256-M1+28mHxpMvcFkOoKxkMMo1VCQsG33ncFZkFalOq2FQ=";
-      stripLen = 1;
-      extraPrefix = "lib/cmetrics/";
-    })
-    (fetchpatch {
-      url = "https://github.com/calyptia/cmetrics/commit/a97999cb6d7299ef230d216b7a1c584b43c64de9.patch";
-      sha256 = "sha256-RuyPEeILc86n/klPIb334XpX0F71nskQ8s/ya0rE2zI=";
-      stripLen = 1;
-      extraPrefix = "lib/cmetrics/";
-    })
-
-    # Fix bundled luajit compilation args
-    ./fix-luajit-darwin.patch
-  ];
 
   nativeBuildInputs = [ cmake flex bison ];
 
