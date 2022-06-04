6f0b4a92dc262f68cdf235ebe231cabff6ef4c5d
Simon Schoeters <b2282e9d-46e8-4ec4-946a-b6ad84078b1c@astil.be>
2022-06-04 14:53:26 +0200

    bluewalker: init at 0.3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2710,6 +2710,8 @@ with pkgs;
 
   bluemix-cli = callPackage ../tools/admin/bluemix-cli { };
 
+  bluewalker = callPackage ../tools/bluetooth/bluewalker { };
+
   blur-effect = callPackage ../tools/graphics/blur-effect { };
 
   bootiso = callPackage ../tools/cd-dvd/bootiso { };
