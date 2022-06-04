d515058727c54474ae7b1ea79036b003d9b79615
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-06-04 06:20:54 +0200

    python310Packages.eth-rlp: init at 0.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2819,6 +2819,8 @@ in {
 
   eth-keys = callPackage ../development/python-modules/eth-keys { };
 
+  eth-rlp = callPackage ../development/python-modules/eth-rlp { };
+
   eth-typing = callPackage ../development/python-modules/eth-typing { };
 
   eth-utils = callPackage ../development/python-modules/eth-utils { };
