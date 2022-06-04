b1595423bee7efb6cc7c633fd9287d4ab90ee248
Robert Schütz <nix@dotlambda.de>
2022-06-04 17:15:20 +0000

    python310Packages.softlayer: 5.9.9 -> unstable-2022-05-31

--- pkgs/development/python-modules/softlayer/default.nix
+++ pkgs/development/python-modules/softlayer/default.nix
@@ -3,54 +3,65 @@
 , buildPythonPackage
 , click
 , fetchFromGitHub
-, mock
+, prettytable
 , prompt-toolkit
-, ptable
 , pygments
 , pytestCheckHook
 , pythonOlder
 , requests
-, sphinx
-, testtools
+, rich
+, softlayer-zeep
 , tkinter
 , urllib3
 }:
 
 buildPythonPackage rec {
   pname = "softlayer";
-  version = "5.9.9";
-  disabled = pythonOlder "3.5";
+  version = "unstable-2022-05-31";
+  disabled = pythonOlder "3.6";
+
+  format = "setuptools";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = "softlayer-python";
-    rev = "v${version}";
-    sha256 = "sha256-LskPz5KXOi7olb3+DUP9uEFESQeo6ec/ZLx9B/w6Ni0=";
+    rev = "dae2ec73563a740c4670cc0718fd4b90c6d42eed";
+    hash = "sha256-JfdCm+Q40Nrt73R9w9bNMoUMrhDPP6Do0Lj1tXaznpw=";
   };
+
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace "==" ">="
+  '';
 
   propagatedBuildInputs = [
     click
+    prettytable
     prompt-toolkit
-    ptable
     pygments
     requests
+    rich
     urllib3
   ];
 
   checkInputs = [
-    mock
     pytestCheckHook
-    sphinx
-    testtools
+    softlayer-zeep
     tkinter
+  ];
+
+  disabledTestPaths = [
+    # requires network access
+    "tests/transports/soap_tests.py"
   ];
 
   pythonImportsCheck = [ "SoftLayer" ];
 
   meta = with lib; {
-    broken = (stdenv.isLinux && stdenv.isAarch64) || stdenv.isDarwin;
+    broken = stdenv.isDarwin;
     description = "Python libraries that assist in calling the SoftLayer API";
     homepage = "https://github.com/softlayer/softlayer-python";
+    changelog = "https://github.com/softlayer/softlayer-python/blob/${src.rev}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ ];
   };
