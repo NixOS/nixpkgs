8d1e34700b075ec0c67b6002110627bd737f4467
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-06-04 03:58:22 +0200

    python310Packages.eth-account: init at 0.6.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2805,6 +2805,8 @@ in {
 
   eth-abi = callPackage ../development/python-modules/eth-abi { };
 
+  eth-account = callPackage ../development/python-modules/eth-account { };
+
   eth-hash = callPackage ../development/python-modules/eth-hash { };
 
   eth-typing = callPackage ../development/python-modules/eth-typing { };
