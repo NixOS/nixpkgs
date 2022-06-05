54fcba5b3bb4d77dd58aa08a489aced93ff5da18
Victor Fuentes <vmfuentes64@gmail.com>
2022-06-05 13:24:33 -0700

    installation-cd: prevent gnome from sleeping

--- nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix
+++ nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix
@@ -18,7 +18,8 @@
     extraGSettingsOverrides = ''
       [org.gnome.shell]
       welcome-dialog-last-shown-version='9999999999'
-
+      [org.gnome.desktop.session]
+      idle-delay=0
       [org.gnome.settings-daemon.plugins.power]
       sleep-inactive-ac-type='nothing'
       sleep-inactive-battery-type='nothing'
