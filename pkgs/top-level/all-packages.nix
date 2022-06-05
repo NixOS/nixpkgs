c47cd973b68f9571ea3232db8e788a557a18575a
Azat Bahawi <azat@bahawi.net>
2022-06-05 22:12:24 +0300

    rss-glx: fix build
    (cherry picked from commit 8b501a1089c346ea6efcddd6de18015512467493)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34544,7 +34544,7 @@ with pkgs;
     stdenv = gccStdenv;
   };
 
-  rss-glx = callPackage ../misc/screensavers/rss-glx { stdenv = gcc10StdenvCompat; };
+  rss-glx = callPackage ../misc/screensavers/rss-glx { };
 
   run-scaled = callPackage ../tools/X11/run-scaled { };
 
