d5e616a3f410e082e7dd362b1548b1b322633205
Victor Fuentes <vmfuentes64@gmail.com>
2022-06-05 14:07:25 -0700

    calamares-nixos-extensions: 0.3.8 -> 0.3.10

--- pkgs/tools/misc/calamares-nixos-extensions/default.nix
+++ pkgs/tools/misc/calamares-nixos-extensions/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "calamares-nixos-extensions";
-  version = "0.3.8";
+  version = "0.3.10";
 
   src = fetchFromGitHub {
     owner = "NixOS";
     repo = "calamares-nixos-extensions";
     rev = version;
-    sha256 = "MtqAOwlY5euVNAGRl2pRkbg/OolJPNOSQcR4DS5gFz4=";
+    sha256 = "YJyK0rsrftrCwYD+aCAkPe/kAqUXsP/4WBAGtNKIGj8=";
   };
 
   installPhase = ''
