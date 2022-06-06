4e121bb72cfec86975a751ed14bc13d5924c4a3b
Robert Hensing <robert@roberthensing.nl>
2022-06-06 15:53:37 +0200

    nixosTests.hostname: Convert to module and plug into the matrix

--- nixos/tests/hostname.nix
+++ nixos/tests/hostname.nix
@@ -1,72 +1,86 @@
-{ system ? builtins.currentSystem,
-  config ? {},
-  pkgs ? import ../.. { inherit system config; }
+{ system ? builtins.currentSystem
+, config ? { }
+, pkgs ? import ../.. { inherit system config; }
 }:
 
 with import ../lib/testing-python.nix { inherit system pkgs; };
 with pkgs.lib;
-
 let
-  makeHostNameTest = hostName: domain: fqdnOrNull:
-    let
-      fqdn = hostName + (optionalString (domain != null) ".${domain}");
-      getStr = str: # maybeString2String
-        let res = builtins.tryEval str;
-        in if (res.success && res.value != null) then res.value else "null";
-    in
-      makeTest {
-        name = "hostname-${fqdn}";
-        meta = with pkgs.lib.maintainers; {
-          maintainers = [ primeos blitz ];
-        };
+  getStr = str: # maybeString2String
+    let res = builtins.tryEval str;
+    in if (res.success && res.value != null) then res.value else "null";
+in
 
-        nodes.machine = { lib, ... }: {
-          networking.hostName = hostName;
-          networking.domain = domain;
+runTest (test@{ config, ... }:
+  let
+    inherit (config.params) domain fqdn fqdnOrNull;
+    hostName = "ahost";
+  in
+  {
 
-          environment.systemPackages = with pkgs; [
-            inetutils
-          ];
-        };
+    meta = with pkgs.lib.maintainers; {
+      maintainers = [ primeos blitz ];
+    };
 
-        testScript = { nodes, ... }: ''
-          start_all()
+    options.params = {
+      domain = mkOption { };
+      fqdn = mkOption { readOnly = true; default = hostName + (optionalString (domain != null) ".${domain}"); };
+      fqdnOrNull = mkOption {
+        readOnly = true;
+        default =
+          if domain == null then null else fqdn;
+      };
+    };
 
-          machine = ${hostName}
+    config = {
+      name = "hostname-${fqdn}";
 
-          machine.wait_for_unit("network-online.target")
+      matrix.domain.value.explicit =
+        { params.domain = "adomain"; };
+      matrix.domain.value.implicit =
+        { params.domain = null; };
 
-          # Test if NixOS computes the correct FQDN (either a FQDN or an error/null):
-          assert "${getStr nodes.machine.config.networking.fqdn}" == "${getStr fqdnOrNull}"
+      nodes.machine = { lib, ... }: {
+        networking.hostName = hostName;
+        networking.domain = domain;
 
-          # The FQDN, domain name, and hostname detection should work as expected:
-          assert "${fqdn}" == machine.succeed("hostname --fqdn").strip()
-          assert "${optionalString (domain != null) domain}" == machine.succeed("dnsdomainname").strip()
-          assert (
-              "${hostName}"
-              == machine.succeed(
-                  'hostnamectl status | grep "Static hostname" | cut -d: -f2'
-              ).strip()
-          )
-
-          # 127.0.0.1 and ::1 should resolve back to "localhost":
-          assert (
-              "localhost" == machine.succeed("getent hosts 127.0.0.1 | awk '{print $2}'").strip()
-          )
-          assert "localhost" == machine.succeed("getent hosts ::1 | awk '{print $2}'").strip()
-
-          # 127.0.0.2 should resolve back to the FQDN and hostname:
-          fqdn_and_host_name = "${optionalString (domain != null) "${hostName}.${domain} "}${hostName}"
-          assert (
-              fqdn_and_host_name
-              == machine.succeed("getent hosts 127.0.0.2 | awk '{print $2,$3}'").strip()
-          )
-        '';
+        environment.systemPackages = with pkgs; [
+          inetutils
+        ];
       };
 
-in
-{
-  noExplicitDomain = makeHostNameTest "ahost" null null;
+      testScript = { nodes, ... }: ''
+        start_all()
 
-  explicitDomain = makeHostNameTest "ahost" "adomain" "ahost.adomain";
-}
+        machine = ${hostName}
+
+        machine.wait_for_unit("network-online.target")
+
+        # Test if NixOS computes the correct FQDN (either a FQDN or an error/null):
+        assert "${getStr nodes.machine.config.networking.fqdn}" == "${getStr fqdnOrNull}"
+
+        # The FQDN, domain name, and hostname detection should work as expected:
+        assert "${fqdn}" == machine.succeed("hostname --fqdn").strip()
+        assert "${optionalString (domain != null) domain}" == machine.succeed("dnsdomainname").strip()
+        assert (
+            "${hostName}"
+            == machine.succeed(
+                'hostnamectl status | grep "Static hostname" | cut -d: -f2'
+            ).strip()
+        )
+
+        # 127.0.0.1 and ::1 should resolve back to "localhost":
+        assert (
+            "localhost" == machine.succeed("getent hosts 127.0.0.1 | awk '{print $2}'").strip()
+        )
+        assert "localhost" == machine.succeed("getent hosts ::1 | awk '{print $2}'").strip()
+
+        # 127.0.0.2 should resolve back to the FQDN and hostname:
+        fqdn_and_host_name = "${optionalString (domain != null) "${hostName}.${domain} "}${hostName}"
+        assert (
+            fqdn_and_host_name
+            == machine.succeed("getent hosts 127.0.0.2 | awk '{print $2,$3}'").strip()
+        )
+      '';
+    };
+  })
