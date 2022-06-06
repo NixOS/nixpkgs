b9bbe8bfd872bdd0459b7c73f5c5277652eb262f
Fabian Affolter <mail@fabian-affolter.ch>
2022-06-06 14:52:49 +0200

    python310Packages.pyroute2-ethtool: 0.6.9 -> 0.6.10

--- pkgs/development/python-modules/pyroute2-ethtool/default.nix
+++ pkgs/development/python-modules/pyroute2-ethtool/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "pyroute2-ethtool";
-  version = "0.6.9";
+  version = "0.6.10";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "pyroute2.ethtool";
     inherit version;
-    hash = "sha256-6o75plLeSle6ZFgoIZGKLCjx1hg4Z2okOq2RqAT2g8E=";
+    hash = "sha256-5i1zbKs40RXCQB1cn3jrDOX+Fgl0nknTFLsvI69udxw=";
   };
 
   propagatedBuildInputs = [
