c08942e1ad495ec838965a886cb07c42b7e09f75
Winter <winter@winter.cafe>
2022-06-06 16:39:55 -0400

    nixos/tests/radarr: fix type of argument in test script

--- nixos/tests/radarr.nix
+++ nixos/tests/radarr.nix
@@ -12,7 +12,7 @@ with lib;
 
   testScript = ''
     machine.wait_for_unit("radarr.service")
-    machine.wait_for_open_port("7878")
+    machine.wait_for_open_port(7878)
     machine.succeed("curl --fail http://localhost:7878/")
   '';
 })
