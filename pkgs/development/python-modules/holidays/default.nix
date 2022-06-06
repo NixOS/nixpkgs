135028b610b7c1254617d3092ccbfb5333f56c1c
Fabian Affolter <mail@fabian-affolter.ch>
2022-06-06 14:22:30 +0200

    python310Packages.holidays: update disabled

--- pkgs/development/python-modules/holidays/default.nix
+++ pkgs/development/python-modules/holidays/default.nix
@@ -14,11 +14,11 @@ buildPythonPackage rec {
   version = "0.14.2";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-DnD9F0gErqHIcLFRMZ+uvNXNsNlVt4IwQ04a/Rd4SY4=";
+    hash = "sha256-DnD9F0gErqHIcLFRMZ+uvNXNsNlVt4IwQ04a/Rd4SY4=";
   };
 
   propagatedBuildInputs = [
