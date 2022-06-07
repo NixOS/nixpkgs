1851e682ce84b52b4a8cc5cbe1123573ffeecfa7
Dzmitry Malyshau <kvark@fastmail.com>
2022-06-07 00:40:54 -0700

    Remove kvark from the maintainers list

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7056,13 +7056,6 @@
     githubId = 449813;
     name = "Roman Kuznetsov";
   };
-  kvark = {
-    name = "Dzmitry Malyshau";
-    email = "kvark@fastmail.com";
-    matrix = "@kvark:matrix.org";
-    github = "kvark";
-    githubId = 107301;
-  };
   kwohlfahrt = {
     email = "kai.wohlfahrt@gmail.com";
     github = "kwohlfahrt";
