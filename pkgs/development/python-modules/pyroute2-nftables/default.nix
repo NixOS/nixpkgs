aacc1079c7d7f3546cc4e02f0f84fd45024dd0ee
Fabian Affolter <mail@fabian-affolter.ch>
2022-06-07 23:22:32 +0200

    python310Packages.pyroute2-nftables: 0.6.10 -> 0.6.11

--- pkgs/development/python-modules/pyroute2-nftables/default.nix
+++ pkgs/development/python-modules/pyroute2-nftables/default.nix
@@ -6,12 +6,12 @@
 
 buildPythonPackage rec {
   pname = "pyroute2-nftables";
-  version = "0.6.10";
+  version = "0.6.11";
 
   src = fetchPypi {
     pname = "pyroute2.nftables";
     inherit version;
-    sha256 = "sha256-dUuCTGs7RaEehsAgK7yNw11tE3VGtT5LxDilMVWpCNU=";
+    sha256 = "sha256-2SHAH4HBcXknCa2rdHk1s+ju+IN5ndcLPD8dgSGagqU=";
   };
 
   propagatedBuildInputs = [
