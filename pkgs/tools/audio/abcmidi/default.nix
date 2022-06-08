81a758031f974061abfcad84aafbc019a7dcf065
Robert Schütz <nix@dotlambda.de>
2022-06-08 02:25:45 +0000

    abcmidi: 2022.05.20 -> 2022.06.07

--- pkgs/tools/audio/abcmidi/default.nix
+++ pkgs/tools/audio/abcmidi/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "abcMIDI";
-  version = "2022.05.20";
+  version = "2022.06.07";
 
   src = fetchzip {
     url = "https://ifdo.ca/~seymour/runabc/${pname}-${version}.zip";
-    hash = "sha256-mTIpy5HHKQxpqN5mHnDvmq6lA0++etj93WCcX1i046I=";
+    hash = "sha256-gMEqcdpJ4dFnxmGJHayM6ZH+n6osHvZ8Tlxk0Vvm1qI=";
   };
 
   meta = with lib; {
