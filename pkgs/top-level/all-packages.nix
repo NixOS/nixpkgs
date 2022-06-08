38cc2b70ef3a06804907977fa98c09a5abc701f2
Majiir Paktu <majiir@nabaal.net>
2022-06-07 21:32:34 -0400

    streamdeck-ui: init at 2.0.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27679,6 +27679,8 @@ with pkgs;
 
   srain = callPackage ../applications/networking/irc/srain { };
 
+  streamdeck-ui = libsForQt5.callPackage ../applications/misc/streamdeck-ui { };
+
   super-productivity = callPackage ../applications/office/super-productivity {
     electron = electron_17;
   };
