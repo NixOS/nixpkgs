5d437463cd68bce24c75a878381d28bf2f9cf096
R. Ryantm <ryantm-bot@ryantm.com>
2022-06-08 09:26:17 +0000

    python310Packages.ansible: 5.8.0 -> 5.9.0

--- pkgs/development/python-modules/ansible/default.nix
+++ pkgs/development/python-modules/ansible/default.nix
@@ -20,7 +20,7 @@
 
 let
   pname = "ansible";
-  version = "5.8.0";
+  version = "5.9.0";
 in
 buildPythonPackage {
   inherit pname version;
@@ -30,7 +30,7 @@ buildPythonPackage {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-+gVkdiAfQGJfs22VxQQe9GOIC+GL5cc7mYtXtAGWeGM=";
+    sha256 = "sha256-by92L8ps/wQBptIRm4ujsvER6hqT/SA8ht8JvHVXDxg=";
   };
 
   postPatch = ''
