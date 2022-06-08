4f660634072bf422e574c7ee585b3ab57c1efe34
R. Ryantm <ryantm-bot@ryantm.com>
2022-06-08 03:35:28 +0000

    python310Packages.matrix-common: 1.1.0 -> 1.2.1

--- pkgs/development/python-modules/matrix-common/default.nix
+++ pkgs/development/python-modules/matrix-common/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "matrix_common";
-  version = "1.1.0";
+  version = "1.2.1";
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-qCOHSK/Cs3B5gYNn/tUVbzVXcbB8j/ChdZNPR+D/MnY=";
+    sha256 = "sha256-qZ3PAqa9lbJKWmGzVIiKKskr8rS4OccnuN2dos36OFM=";
   };
 
   propagatedBuildInputs = [ attrs ];
