390d78cc2396a5ae650426bb89757c9316b6d2b3
Rick van Schijndel <rol3517@gmail.com>
2022-06-09 20:42:16 +0200

    ffmpeg: use prefixed pkg-config if needed
    Helpful for when we introduce an always prefixed pkg-config.

--- pkgs/development/libraries/ffmpeg/generic.nix
+++ pkgs/development/libraries/ffmpeg/generic.nix
@@ -71,6 +71,7 @@ stdenv.mkDerivation rec {
   configureFlags = filter (v: v != null) ([
       "--arch=${stdenv.hostPlatform.parsed.cpu.name}"
       "--target_os=${stdenv.hostPlatform.parsed.kernel.name}"
+      "--pkg-config=${pkg-config.targetPrefix}pkg-config"
     # License
       "--enable-gpl"
       "--enable-version3"
