09f919447626c25b6aefa80254f60721425593d6
R. Ryantm <ryantm-bot@ryantm.com>
2022-06-10 07:30:20 +0000

    nodejs: 16.15.0 -> 16.15.1

--- pkgs/development/web/nodejs/v16.nix
+++ pkgs/development/web/nodejs/v16.nix
@@ -8,8 +8,8 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "16.15.0"; # Do not upgrade until #176127 is solved
-    sha256 = "sha256-oPgS78Q/eDIeygiVeWCkj15r+XAE1QWMjdOwPGRupPc=";
+    version = "16.15.1"; # Do not upgrade until #176127 is solved
+    sha256 = "0zcv2raa9d4g7dr7v3i2pkfrq076b085f9bmlq4i2wb93wy9vsfl";
     patches = [
       ./disable-darwin-v8-system-instrumentation.patch
     ];
