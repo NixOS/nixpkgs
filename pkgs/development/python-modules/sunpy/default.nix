3abb144ebf3b2e18c8ef8a80ca7265628054d241
R. Ryantm <ryantm-bot@ryantm.com>
2022-06-10 00:52:16 +0000

    python310Packages.sunpy: 4.0.0 -> 4.0.1

--- pkgs/development/python-modules/sunpy/default.nix
+++ pkgs/development/python-modules/sunpy/default.nix
@@ -31,14 +31,14 @@
 
 buildPythonPackage rec {
   pname = "sunpy";
-  version = "4.0.0";
+  version = "4.0.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-01eGYls8eE2e3vzDYXz5D1xQs7pxpmHt89aBKCvVdLg=";
+    hash = "sha256-TKOJcEg5A3zjuJbH/tugoX7A7vxSwcE20jJ5QuvWTu8=";
   };
 
   nativeBuildInputs = [
