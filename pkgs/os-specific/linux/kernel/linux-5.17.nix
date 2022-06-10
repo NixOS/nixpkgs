e29461d9fff12766b81b13f5b513b6fc976b66b9
Izorkin <izorkin@elven.pw>
2022-06-10 10:43:18 +0300

    linux: 5.17.11 -> 5.17.14

--- pkgs/os-specific/linux/kernel/linux-5.17.nix
+++ pkgs/os-specific/linux/kernel/linux-5.17.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.17.11";
+  version = "5.17.14";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0c8vz02lbfm0zkgsr1gvdp8bzxz255dk863pnakw6d77z9bfc22p";
+    sha256 = "sha256-C+RjcXNeSkpIzLuPTgOD/X/mDk/x5JqEZYLw39+aWmQ=";
   };
 } // (args.argsOverride or { }))
