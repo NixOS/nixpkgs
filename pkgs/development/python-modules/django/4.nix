7845d74f15d0075f62a5ee2ca8e1d079d5b80147
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-06-10 23:07:30 +0200

    python3Packages.django_4: enable tests

--- pkgs/development/python-modules/django/4.nix
+++ pkgs/development/python-modules/django/4.nix
@@ -25,14 +25,13 @@
 , bcrypt
 , docutils
 , geoip2
+, glibcLocales
 , jinja2
-, python-memcached
 , numpy
 , pillow
 , pylibmc
 , pymemcache
 , python
-, pytz
 , pywatchman
 , pyyaml
 , redis
@@ -58,6 +57,8 @@ buildPythonPackage rec {
       src = ./django_4_set_zoneinfo_dir.patch;
       zoneinfo = tzdata + "/share/zoneinfo";
     })
+    # make sure the tests don't remove packages from our pythonpath
+    ./django_4_pythonpath.patch
   ] ++ lib.optionals withGdal [
     (substituteAll {
       src = ./django_4_set_geos_gdal_lib.patch;
@@ -66,6 +67,11 @@ buildPythonPackage rec {
       extension = stdenv.hostPlatform.extensions.sharedLibrary;
     })
   ];
+
+  postPatch = ''
+    substituteInPlace tests/utils_tests/test_autoreload.py \
+      --replace "/usr/bin/python" "${python.interpreter}"
+  '';
 
   nativeBuildInputs = [
     setuptools
@@ -78,36 +84,48 @@ buildPythonPackage rec {
     backports-zoneinfo
   ];
 
-  # Fails to import asgiref in ~200 tests
-  # ModuleNotFoundError: No module named 'asgiref'
-  doCheck = false;
+  passthru.optional-dependencies = {
+    argon2 = [
+      argon2-cffi
+    ];
+    bcrypt = [
+      bcrypt
+    ];
+  };
 
   nativeCheckInputs = [
     aiosmtpd
-    argon2-cffi
     asgiref
-    bcrypt
     docutils
     geoip2
+    glibcLocales
     jinja2
-    python-memcached
     numpy
     pillow
     pylibmc
     pymemcache
-    pytz
     pywatchman
     pyyaml
     redis
     selenium
     tblib
     tzdata
-  ];
+  ] ++ lib.flatten (builtins.attrValues passthru.optional-dependencies);
+
+  preCheck = ''
+    # make sure the installed library gets imported
+    rm -rf django
+
+    export LC_TIME=UTC
+    export TZDIR=${tzdata}/${python.sitePackages}/tzdata/zoneinfo
+  '';
 
   checkPhase = ''
     runHook preCheck
 
-    ${python.interpreter} tests/runtests.py
+    pushd tests
+    ${python.interpreter} runtests.py --settings=test_sqlite
+    popd
 
     runHook postCheck
   '';
