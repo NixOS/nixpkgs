764759283dde59447ddab13c855c63677d8390cb
Robert Schütz <nix@dotlambda.de>
2022-06-11 17:30:25 +0000

    python310Packages.pyjwt: fix CVE-2022-29217

--- pkgs/development/python-modules/pyjwt/default.nix
+++ pkgs/development/python-modules/pyjwt/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, fetchpatch
 , cryptography
 , ecdsa
 , pytest-cov
@@ -18,6 +19,18 @@ buildPythonPackage rec {
     inherit version;
     sha256 = "sha256-uIi01W8G9tzXdyEMM05pxze+dHVdPl6e4/5n3Big7kE=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "CVE-2022-29217.patch";
+      url = "https://github.com/jpadilla/pyjwt/commit/9c528670c455b8d948aff95ed50e22940d1ad3fc.patch";
+      excludes = [
+        ".gitignore"
+        "jwt/__init__.py" # changes version
+      ];
+      hash = "sha256-9fgCn2Pb3q72c4w5utP16KhDsAe50EnHd7Z57Zrj7wA=";
+    })
+  ];
 
   propagatedBuildInputs = [
     cryptography
