ccad0503ba519acda6647e439ab8555cd44e5c4e
Fabian Affolter <mail@fabian-affolter.ch>
2022-06-13 11:25:06 +0200

    python310Packages.pysmb: add format
    - disable on older Python releases

--- pkgs/development/python-modules/pysmb/default.nix
+++ pkgs/development/python-modules/pysmb/default.nix
@@ -1,20 +1,22 @@
-{ buildPythonPackage
+{ lib
+, buildPythonPackage
 , fetchPypi
-, lib
-
-# pythonPackages
 , pyasn1
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "pysmb";
   version = "1.2.8";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     format = "setuptools";
     extension = "zip";
-    sha256 = "sha256-OwfbFiF0ZQOdDCVpTAcFuDZjyoIlniCfNWbVd1Nqc5U=";
+    hash = "sha256-OwfbFiF0ZQOdDCVpTAcFuDZjyoIlniCfNWbVd1Nqc5U=";
   };
 
   propagatedBuildInputs = [
@@ -22,17 +24,18 @@ buildPythonPackage rec {
   ];
 
   # Tests require Network Connectivity and a server up and running
-  #   https://github.com/miketeo/pysmb/blob/master/python3/tests/README_1st.txt
+  # https://github.com/miketeo/pysmb/blob/master/python3/tests/README_1st.txt
   doCheck = false;
 
-  pythonImportsCheck = [ "nmb" "smb" ];
+  pythonImportsCheck = [
+    "nmb"
+    "smb"
+  ];
 
-  meta = {
+  meta = with lib; {
     description = "Experimental SMB/CIFS library written in Python to support file sharing between Windows and Linux machines";
     homepage = "https://miketeo.net/wp/index.php/projects/pysmb";
-    license = lib.licenses.zlib;
-    maintainers = with lib.maintainers; [
-      kamadorueda
-    ];
+    license = licenses.zlib;
+    maintainers = with maintainers; [ kamadorueda ];
   };
 }
