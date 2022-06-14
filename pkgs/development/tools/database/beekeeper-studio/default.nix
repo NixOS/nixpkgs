456e9c58d09506c8d09965757e619ae087bb79d0
Milo Gertjejansen <milo@milogert.com>
2022-06-14 08:47:57 -0400

    Update linux sha

--- pkgs/development/tools/database/beekeeper-studio/default.nix
+++ pkgs/development/tools/database/beekeeper-studio/default.nix
@@ -3,18 +3,19 @@ let
   version = "3.4.3";
   appimage = callPackage ./appimage.nix {
     inherit version;
-    sha256 = "1xfr3i4gsi13wj374yr5idhgs0q71s4h33yxdr7b7xjdg2gb8lp1";
+    sha256 = "sha256-9t3fCZJeftUCEgWknGBSSdcLRhfWyspz/ArWTciqkmg=";
   };
   dmg-x86_64 = callPackage ./dmg.nix {
     inherit version;
     sha256 = "sha256-EzqVQemNVitgprN2o/0+BBPEundNACIl2Y2izAKbuEw=";
     isArm = false;
   };
-  dmg-aarch64 = callPackage ./dmg.nix {
-    inherit version;
-    sha256 = "1axki47hrxx5m0hrmjpxcya091lahqfnh2pd3zhn5dd496slq8an";
-    isArm = true;
-  };
+  # I don't have an arm64 machine, so I can't test this myself.
+  /* dmg-aarch64 = callPackage ./dmg.nix { */
+  /*   inherit version; */
+  /*   sha256 = "1axki47hrxx5m0hrmjpxcya091lahqfnh2pd3zhn5dd496slq8an"; */
+  /*   isArm = true; */
+  /* }; */
 in (
   if stdenvNoCC.isDarwin && stdenvNoCC.hostPlatform.isAarch64 then
     dmg-aarch64
@@ -34,6 +35,6 @@ in (
     platforms =
       appimage.meta.platforms
       ++ dmg-x86_64.meta.platforms
-      ++ dmg-aarch64.meta.platforms;
+      /* ++ dmg-aarch64.meta.platforms; */
   };
 })
