e009ce79a4002286eea8c790a0259dbbe5bc63d3
R. Ryantm <ryantm-bot@ryantm.com>
2022-06-15 05:22:59 +0000

    python310Packages.plaid-python: 9.5.0 -> 9.6.0

--- pkgs/development/python-modules/plaid-python/default.nix
+++ pkgs/development/python-modules/plaid-python/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "plaid-python";
-  version = "9.5.0";
+  version = "9.6.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-gjYlr2rp2xRMcFKbA4Y/Pb7YYOUR4LTzMtVoG1iMlx0=";
+    hash = "sha256-LijBSAnRwXoeddWwolamM3QYSTwFvgodmaIbayco8fY=";
   };
 
   propagatedBuildInputs = [
