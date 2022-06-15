a4fa94965acf7b15d15816f5705d43e6afd08447
Malo Bourgon <mbourgon@gmail.com>
2022-06-15 12:44:17 -0700

    python3Packages.pytorch: unbreak on Darwin
    * Add missing `buildInputs`.
    * Enable Grand Central Dispatch support for `aarch64-darwin`.
    * Remove `postFixup` steps for .dylib files that aren't present.
    
    (cherry picked from commit c1d5efbee20eaf5a2748dbb5e08eaf304b7b3f34)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8629,6 +8629,8 @@ in {
 
   pytorch = callPackage ../development/python-modules/pytorch {
     cudaSupport = pkgs.config.cudaSupport or false;
+    inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices;
+    inherit (pkgs.darwin) libobjc;
   };
 
   pytorch-bin = callPackage ../development/python-modules/pytorch/bin.nix { };
