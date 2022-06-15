11af729cd0535c48b22f65edc5112ec1fb04d7c7
Raymond Gauthier <jraygauthier@gmail.com>
2022-06-15 16:20:59 -0400

    tiscamera: 0.13.1 -> 1.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20962,7 +20962,7 @@ with pkgs;
 
   tinyxml-2 = callPackage ../development/libraries/tinyxml-2 { };
 
-  tiscamera = callPackage ../os-specific/linux/tiscamera { stdenv = gcc10StdenvCompat; };
+  tiscamera = callPackage ../os-specific/linux/tiscamera { };
 
   tivodecode = callPackage ../applications/video/tivodecode { };
 
