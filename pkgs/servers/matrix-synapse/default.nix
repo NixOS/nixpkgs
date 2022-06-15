0a86e1562328a81319b740cf82441fdd814a8259
Nick Cao <nickcao@nichi.co>
2022-06-15 12:54:38 +0800

    matrix-synapse: 1.60.0 -> 1.61.0
    (cherry picked from commit 402807041adc40950854a38ee6a850f5812ee7ad)

--- pkgs/servers/matrix-synapse/default.nix
+++ pkgs/servers/matrix-synapse/default.nix
@@ -11,11 +11,11 @@ in
 with python3.pkgs;
 buildPythonApplication rec {
   pname = "matrix-synapse";
-  version = "1.60.0";
+  version = "1.61.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-sR+DZhpAkPpurPs6jSBVphYp12z8qulcQSl3ngcCrcs=";
+    sha256 = "sha256-tEYvbR7uQe7WLtkYt0xXFGLu8w4q8bhf9HqDbGXF+T8=";
   };
 
   buildInputs = [ openssl ];
