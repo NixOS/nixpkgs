56b97e0acafdd7e68a2f7b35fa894d1fd514c2b6
Milo Gertjejansen <milo@milogert.com>
2022-06-15 09:57:05 -0400

    Apply suggestions from code review
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/servers/tt-rss/plugin-feediron/default.nix
+++ pkgs/servers/tt-rss/plugin-feediron/default.nix
@@ -1,24 +1,20 @@
-{ lib, stdenv, fetchFromGitHub, ... }:
+{ lib, stdenv, fetchFromGitHub, tt-rss }:
 
 stdenv.mkDerivation rec {
   pname = "tt-rss-plugin-feediron";
-  version = "v1.32";
+  version = "1.32";
 
   src = fetchFromGitHub {
     owner = "feediron";
     repo = "ttrss_plugin-feediron";
-    rev = version;
+    rev = "v${version}";
     sha256 = "0a4nq0k0zmgmx7dzz70smhp0yxp7jynk9djz0nnbaa138h8888pr";
   };
 
   installPhase = ''
     mkdir -p $out/feediron
 
-    cp -R bin $out/feediron/bin
-    cp -R filters $out/feediron/filters
-    cp -R preftab $out/feediron/preftab
-    cp -R recipes $out/feediron/recipes
-    cp init.php $out/feediron/init.php
+    cp -r bin filters init.php preftab recipes $out/feediron/
   '';
 
   meta = with lib; {
@@ -32,6 +28,7 @@ stdenv.mkDerivation rec {
     license = licenses.mit;
     homepage = "https://github.com/feediron/ttrss_plugin-feediron";
     maintainers = with maintainers; [ milogert ];
-    platforms = platforms.all;
+    inherit (tt-rss.meta) platforms;
+    ;
   };
 }
