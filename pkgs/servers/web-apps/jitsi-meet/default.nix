548cfced57bebb2f89a4108e562d9b5027ffb73e
R. Ryantm <ryantm-bot@ryantm.com>
2022-06-17 05:37:16 +0000

    jitsi-meet: 1.0.6155 -> 1.0.6239

--- pkgs/servers/web-apps/jitsi-meet/default.nix
+++ pkgs/servers/web-apps/jitsi-meet/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "jitsi-meet";
-  version = "1.0.6155";
+  version = "1.0.6239";
 
   src = fetchurl {
     url = "https://download.jitsi.org/jitsi-meet/src/jitsi-meet-${version}.tar.bz2";
-    sha256 = "baCRVYe1z2uH3vSb5KbWs0y4KyQYO3JpTyoGFZIZQqo=";
+    sha256 = "r47uteWSVKn0YnGfzO0Aiv6i4hLSjN6ihGjN5HgilC0=";
   };
 
   dontBuild = true;
