767d31165ad7442518e761521e2f1f17d8336abd
Dmitry Bogatov <git#v1@kaction.cc>
2022-06-17 17:00:42 -0400

    texinfo4: use recent config.guess script

--- pkgs/development/tools/misc/texinfo/4.13a.nix
+++ pkgs/development/tools/misc/texinfo/4.13a.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, texinfo, ncurses, xz }:
+{ stdenv, fetchurl, texinfo, ncurses, xz, autoconf }:
 
 stdenv.mkDerivation rec {
   pname = "texinfo";
@@ -8,6 +8,18 @@ stdenv.mkDerivation rec {
     url = "mirror://gnu/texinfo/${pname}-${version}.tar.lzma";
     sha256 = "1rf9ckpqwixj65bw469i634897xwlgkm5i9g2hv3avl6mv7b0a3d";
   };
+
+  autoconfSrc = autoconf.src;
+
+  # We need texinfo4 to build documentation of GNU m4, and version of
+  # config.guess bundled in texinfo4 is too old to recognize some CI
+  # platforms (see #172225).
+  #
+  # Using autoreconfHook results in infinity recursion.
+  postPatch = ''
+    tar xf $autoconfSrc
+    cp -v ./autoconf-*/build-aux/config.guess ./build-aux/config.guess
+  '';
 
   buildInputs = [ ncurses ];
   nativeBuildInputs = [ xz ];
