f38d2ae25a095fc031f5d85b7ceb95fa4f420aa5
ajs124 <git@ajs124.de>
2022-06-17 18:01:35 +0200

    dogdns: pin to openssl_1_1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5610,6 +5610,7 @@ with pkgs;
   dog = callPackage ../tools/system/dog { };
 
   dogdns = callPackage ../tools/networking/dogdns {
+    openssl = openssl_1_1;
     inherit (darwin.apple_sdk.frameworks) Security;
   };
 
