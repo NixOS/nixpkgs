4b31cc7551cbc795e30670d09845acdeb0f41651
Alok Parlikar <alok@cobaltspeech.com>
2022-06-18 14:18:24 +0530

    grpc-gateway: init at 2.10.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6757,6 +6757,8 @@ with pkgs;
 
   grpcurl = callPackage ../tools/networking/grpcurl { };
 
+  grpc-gateway = callPackage ../development/tools/grpc-gateway { };
+
   grpcui = callPackage ../tools/networking/grpcui { };
 
   grpc-tools = callPackage ../development/tools/misc/grpc-tools { };
