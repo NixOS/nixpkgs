5147a3e4c262bde42c7ded1aeb21fc2cd095e629
Matt Melling <mattmelling@fastmail.com>
2022-06-20 22:46:10 +0100

    js8call: init at 2.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27640,6 +27640,8 @@ with pkgs;
 
   josm = callPackage ../applications/misc/josm { };
 
+  js8call = qt5.callPackage ../applications/radio/js8call { };
+
   jwm = callPackage ../applications/window-managers/jwm { };
 
   jwm-settings-manager = callPackage ../applications/window-managers/jwm/jwm-settings-manager.nix { };
