e0516cb6ab416265ae53f045ee27127052603fbc
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-06-21 17:57:43 +0200

    openssl_1_1: 1.1.1o -> 1.1.1p
    Fixes additional sanitization issues in the c_rehash script.
    
    https://mta.openssl.org/pipermail/openssl-announce/2022-June/000226.html
    
    Fixes: CVE-2022-2068
    (cherry picked from commit 0c213829222e20c83eb2044c36ff205854a583b9)

--- pkgs/development/libraries/openssl/default.nix
+++ pkgs/development/libraries/openssl/default.nix
@@ -190,8 +190,8 @@ in {
   };
 
   openssl_1_1 = common rec {
-    version = "1.1.1o";
-    sha256 = "sha256-k4SisFcN2ANYhBRkZ3EV33he25QccSEfdQdtcv5rQ48=";
+    version = "1.1.1p";
+    sha256 = "sha256-v2G2Kqpmx8djmUKpTeTJroKAwI8X1OrC5EZE2fyKzm8=";
     patches = [
       ./1.1/nix-ssl-cert-file.patch
 
