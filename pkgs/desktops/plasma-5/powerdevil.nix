868b2b1fbe6d0e6ca3ffe10ac9bf4afc620a1dea
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2022-06-21 19:05:28 -0300

    Revert "powerdevil: fix brightness wonkiness on some laptops"
    This reverts commit a29f64c22f331e1a1c650d4289dd891303c3766e.

--- pkgs/desktops/plasma-5/powerdevil.nix
+++ pkgs/desktops/plasma-5/powerdevil.nix
@@ -1,5 +1,5 @@
 {
-  mkDerivation, fetchpatch,
+  mkDerivation,
   extra-cmake-modules, kdoctools,
   bluez-qt, kactivities, kauth, kconfig, kdbusaddons,
   kglobalaccel, ki18n, kidletime, kio, knotifyconfig, kwayland, libkscreen,
@@ -9,15 +9,6 @@
 mkDerivation {
   pname = "powerdevil";
   nativeBuildInputs = [ extra-cmake-modules kdoctools ];
-  patches = [
-    # Backported fix for https://bugs.kde.org/show_bug.cgi?id=454161
-    # FIXME: remove for next release
-    (fetchpatch {
-      name = "brightness-overflow-fix";
-      url = "https://invent.kde.org/plasma/powerdevil/-/commit/2ebe655d220c9167b66893a823b2fff2e2b8a531.patch";
-      sha256 = "sha256-Sf2q0CImLYjy1fTp9AWbCeRG05liUkemhfEXL/0MIQI=";
-    })
-  ];
   buildInputs = [
     kconfig kdbusaddons knotifyconfig solid udev bluez-qt kactivities kauth
     kglobalaccel ki18n kio kidletime kwayland libkscreen
