c534d056feceec34ddefc41adbc4a355e5c89ec9
Sergei Trofimovich <slyich@gmail.com>
2022-06-21 19:03:43 +0100

    pkgs/stdenv/generic/make-derivation.nix: add a bug reference to strictDepsByDefault TODO

--- pkgs/stdenv/generic/make-derivation.nix
+++ pkgs/stdenv/generic/make-derivation.nix
@@ -124,7 +124,7 @@ let
 # InstallCheck phase
 , doInstallCheck ? config.doCheckByDefault or false
 
-, # TODO(@Ericson2314): Make always true and remove
+, # TODO(@Ericson2314): Make always true and remove / resolve #178468
   strictDeps ? if config.strictDepsByDefault then true else stdenv.hostPlatform != stdenv.buildPlatform
 
 , enableParallelBuilding ? config.enableParallelBuildingByDefault
