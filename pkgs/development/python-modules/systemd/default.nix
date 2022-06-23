3f328a5108746708201815f6515d700dadc94f21
Jan Tojnar <jtojnar@gmail.com>
2022-06-23 19:54:08 +0200

    python3.pkgs.systemd: Fix compatibility with Python 3.10

--- pkgs/development/python-modules/systemd/default.nix
+++ pkgs/development/python-modules/systemd/default.nix
@@ -1,5 +1,6 @@
 { lib
 , buildPythonPackage
+, fetchpatch
 , fetchFromGitHub
 , systemd
 , pkg-config
@@ -15,6 +16,15 @@ buildPythonPackage rec {
     rev = "v${version}";
     sha256 = "1fakw7qln44mfd6pj4kqsgyrhkc6cyr653id34kv0rdnb1bvysrz";
   };
+
+  patches = [
+    # Fix runtime issues on Python 3.10
+    # https://github.com/systemd/python-systemd/issues/107
+    (fetchpatch {
+      url = "https://github.com/systemd/python-systemd/commit/c71bbac357f0ac722e1bcb2edfa925b68cca23c9.patch";
+      sha256 = "22s72Wa/BCwNNvwbxEUh58jhHlbA00SNwNVchVDovcc=";
+    })
+  ];
 
   nativeBuildInputs = [
     pkg-config
