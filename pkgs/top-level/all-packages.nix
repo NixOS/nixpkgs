6cab8dd7be4c0bb6e0e49c1b30d07f457f4e8407
hqurve <53281855+hqurve@users.noreply.github.com>
2022-06-23 05:34:08 -0400

    lorien: init at 0.5.0 (#164178)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8230,6 +8230,8 @@ with pkgs;
 
   longview = callPackage ../servers/monitoring/longview { };
 
+  lorien = callPackage ../applications/graphics/lorien { };
+
   lout = callPackage ../tools/typesetting/lout { };
 
   lr = callPackage ../tools/system/lr { };
