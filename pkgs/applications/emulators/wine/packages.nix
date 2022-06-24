c2dbbe706b8199efbfefb4c835a1c4a572b1ee02
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2022-06-24 13:37:46 +0800

    wine64Packages: fix mainProgram

--- pkgs/applications/emulators/wine/packages.nix
+++ pkgs/applications/emulators/wine/packages.nix
@@ -28,6 +28,7 @@ in with src; {
     monos =  [ mono ];
     configureFlags = [ "--enable-win64" ];
     platforms = [ "x86_64-linux" "x86_64-darwin" ];
+    mainProgram = "wine64";
   };
   wineWow = callPackage ./base.nix {
     pname = "wine-wow";
