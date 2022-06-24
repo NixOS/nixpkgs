727cc6aacfb612600e5d762703c49ab63535db18
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-06-24 14:16:56 -0400

    duckdb: add patch to fix list type inference

--- pkgs/development/libraries/duckdb/default.nix
+++ pkgs/development/libraries/duckdb/default.nix
@@ -33,6 +33,11 @@ stdenv.mkDerivation rec {
       url = "https://github.com/duckdb/duckdb/commit/82e13a4bb9f0683af6c52468af2fb903cce4286d.patch";
       sha256 = "sha256-m0Bs0DOJQtkadbKZKk88NHyBFJkjxXUsiWYciuRIJLU=";
     })
+    (fetchpatch {
+      name = "fix-list-type-metadata.patch";
+      url = "https://patch-diff.githubusercontent.com/raw/duckdb/duckdb/pull/3953.patch";
+      sha256 = "sha256-ttqs5EjeSLhZQOXc43Y5/N5IYSESQTD1FZWV1uJ15Fo=";
+    })
   ];
 
   postPatch = ''
