c2742ae1a4ca4bf80ddd7db44ab6505f84a83809
R. Ryantm <ryantm-bot@ryantm.com>
2022-06-25 11:27:26 +0000

    bitwig-studio: 4.2.3 -> 4.2.5

--- pkgs/applications/audio/bitwig-studio/bitwig-studio4.nix
+++ pkgs/applications/audio/bitwig-studio/bitwig-studio4.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   pname = "bitwig-studio";
-  version = "4.2.3";
+  version = "4.2.5";
 
   src = fetchurl {
     url = "https://downloads.bitwig.com/stable/${version}/${pname}-${version}.deb";
-    sha256 = "sha256-UCafrjrEwwHkhPum7sTOjtXzy7PNeK5/aeKg+b3CGJU=";
+    sha256 = "sha256-Fw3ll/Vjtq/ZwH/nBs+D1i3JC6Q1ALCnLvCZgAjQdmw=";
   };
 
   nativeBuildInputs = [ dpkg makeWrapper wrapGAppsHook ];
