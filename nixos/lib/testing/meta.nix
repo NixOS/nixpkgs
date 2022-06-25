8f03658713371a7f897ed7d9586fbcbae4be9125
Robert Hensing <robert@roberthensing.nl>
2022-06-26 00:13:03 +0200

    nixos/testing/meta.nix: Add options, some optional

--- nixos/lib/testing/meta.nix
+++ nixos/lib/testing/meta.nix
@@ -4,9 +4,25 @@ let
 in
 {
   options = {
-    meta.maintainers = lib.mkOption {
-      type = types.listOf types.raw;
-      default = [];
+    meta = lib.mkOption {
+      apply = lib.filterAttrs (k: v: v != null);
+      type = types.submodule {
+        options = {
+          maintainers = lib.mkOption {
+            type = types.listOf types.raw;
+            default = [];
+          };
+          timeout = lib.mkOption {
+            type = types.nullOr types.int;
+            default = null;
+          };
+          broken = lib.mkOption {
+            type = types.bool;
+            default = false;
+          };
+        };
+      };
+      default = {};
     };
   };
 }
