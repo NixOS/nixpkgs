d6f9b499b2302759bb1283f1459ea7eb687ab911
R. Ryantm <ryantm-bot@ryantm.com>
2022-06-25 16:33:04 +0000

    python310Packages.gdown: 4.4.0 -> 4.5.0

--- pkgs/development/python-modules/gdown/default.nix
+++ pkgs/development/python-modules/gdown/default.nix
@@ -11,12 +11,12 @@
 
 buildPythonApplication rec {
   pname = "gdown";
-  version = "4.4.0";
+  version = "4.5.0";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-GPw6TaSiJz3reqKcdIa+TfORnZBBWK1qaj4lyBFUcNc=";
+    sha256 = "sha256-rJ7CoZDOl+cW1FsPzkE7+AiBSpzDD9J8RkiQW25Xkno=";
   };
 
   propagatedBuildInputs = [
