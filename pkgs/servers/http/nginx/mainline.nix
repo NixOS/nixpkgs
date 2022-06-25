d5b329ea86c3c528c7942d958e681ec87af42361
Izorkin <izorkin@elven.pw>
2022-06-25 20:56:58 +0300

    nginxMainline: 1.22.0 -> 1.23.0
    (cherry picked from commit 7a8c541412ff6c714c06f4c8f953e0e250879a8b)

--- pkgs/servers/http/nginx/mainline.nix
+++ pkgs/servers/http/nginx/mainline.nix
@@ -1,6 +1,6 @@
 { callPackage, ... }@args:
 
 callPackage ./generic.nix args {
-  version = "1.22.0";
-  sha256 = "0lzb4sn8hv491zad9kbpvka3m5ayjf1pxqbwllri980idyd5cgdk";
+  version = "1.23.0";
+  sha256 = "sha256-ggrKo1uScr6ennL23vpKXykhgkcJ+KpHcseKsx7ZTNE=";
 }
