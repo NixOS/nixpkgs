a0153f4964c1c021f858ae7d5173cc4f5db23e41
Arthur Gautier <arthur.gautier@arista.com>
2022-06-25 19:30:07 -0700

    qemu-utils: remove qemu dependency
    qemu-utils was pulling qemu which is a 900MB dependency. By removing
    reference to it (unneeded), we're saving space on our deployments.
    qemu-utils is a dependency of cloud-utils

--- pkgs/applications/virtualization/qemu/utils.nix
+++ pkgs/applications/virtualization/qemu/utils.nix
@@ -1,4 +1,4 @@
-{ stdenv, installShellFiles, qemu }:
+{ stdenv, installShellFiles, qemu, removeReferencesTo }:
 
 stdenv.mkDerivation rec {
   pname = "qemu-utils";
@@ -13,6 +13,7 @@ stdenv.mkDerivation rec {
     cp "${qemu}/bin/qemu-img" "$out/bin/qemu-img"
     cp "${qemu}/bin/qemu-io"  "$out/bin/qemu-io"
     cp "${qemu}/bin/qemu-nbd" "$out/bin/qemu-nbd"
+    ${removeReferencesTo}/bin/remove-references-to -t ${qemu} $out/bin/*
 
     installManPage ${qemu}/share/man/man1/qemu-img.1.gz
     installManPage ${qemu}/share/man/man8/qemu-nbd.8.gz
