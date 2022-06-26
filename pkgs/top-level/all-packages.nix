fb9e656d48784ef32353c06330558161abefde60
Robert Hensing <robert@roberthensing.nl>
2022-06-26 18:35:55 +0200

    git-autofixup: init as alias
    A top-level attribute makes this program easier to find
    for nix repl junkies like me.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26919,6 +26919,8 @@ with pkgs;
   # Git with SVN support, but without GUI.
   gitSVN = lowPrio (git.override { svnSupport = true; });
 
+  git-autofixup = perlPackages.GitAutofixup;
+
   git-doc = lib.addMetaAttrs {
     description = "Additional documentation for Git";
     longDescription = ''
