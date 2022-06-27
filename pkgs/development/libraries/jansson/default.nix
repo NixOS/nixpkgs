NixOS/nixpkgs

commit 938f2ce1017ff1fdb25abec3f22a0821618af8cb

jansson: enable shared library installation
Without shared libraries metworkmanager fails to build:

    $ nix build -f. networkmanager -L
    ...
    networkmanager> meson.build:269:2: ERROR: Assert failed: Unable to determine Jansson SONAME

pkgs/development/libraries/jansson/default.nix
@@ -13,6 +13,10 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ cmake ];
 
+  # networkmanager relies on libjansson.so:
+  #   https://github.com/NixOS/nixpkgs/pull/176302#issuecomment-1150239453
+  cmakeFlags = [ "-DJANSSON_BUILD_SHARED_LIBS=ON" ];
+
   meta = with lib; {
     homepage = "https://github.com/akheron/jansson";
     description = "C library for encoding, decoding and manipulating JSON data";

commit 50c258cc1b05fda9d1f9222ddbaa71c3cbd68977

pkgsStatic.jansson: fix build
Fixes: 938f2ce1017 ("jansson: enable shared library installation")

pkgs/development/libraries/jansson/default.nix
@@ -13,9 +13,11 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ cmake ];
 
-  # networkmanager relies on libjansson.so:
-  #   https://github.com/NixOS/nixpkgs/pull/176302#issuecomment-1150239453
-  cmakeFlags = [ "-DJANSSON_BUILD_SHARED_LIBS=ON" ];
+  cmakeFlags = [
+    # networkmanager relies on libjansson.so:
+    #   https://github.com/NixOS/nixpkgs/pull/176302#issuecomment-1150239453
+    "-DJANSSON_BUILD_SHARED_LIBS=${if stdenv.hostPlatform.isStatic then "OFF" else "ON"}"
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/akheron/jansson";