ca5ee125844048125663f96c18bb685b775ea697
R. Ryantm <ryantm-bot@ryantm.com>
2022-06-27 03:29:24 +0000

    python310Packages.cachelib: 0.8.0 -> 0.9.0

--- pkgs/development/python-modules/cachelib/default.nix
+++ pkgs/development/python-modules/cachelib/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "cachelib";
-  version = "0.8.0";
+  version = "0.9.0";
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "pallets";
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-MtfBiis0oXF4sxCUI/uneZM/OFzmTy+pj8ddTxamKrs=";
+    sha256 = "sha256-LO1VdirKWXIAy3U8oRtnFI58qO+yn6Vm5bZdCjdgKwo=";
   };
 
   checkInputs = [
