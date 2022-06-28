2068a7086ed75cb5e806e0914af5b002d649d67e
Maximilian Bosch <maximilian@mbosch.me>
2022-06-28 19:14:23 +0200

    linux: 4.9.319 -> 4.9.320
    (cherry picked from commit 21c39a09698ce94f0be180027520c3b5169fb68d)

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,12 +1,12 @@
 { buildPackages, fetchurl, perl, buildLinux, nixosTests, stdenv, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.9.319";
+  version = "4.9.320";
   extraMeta.branch = "4.9";
   extraMeta.broken = stdenv.isAarch64;
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "11242bn95k51knm9da7xk7r10vk7iji06wix1cq4g5nzldrfp9sp";
+    sha256 = "16wq86i8ch488372v94r88xr9anda477d46xq43wkpmbj912gn9h";
   };
 } // (args.argsOverride or {}))
