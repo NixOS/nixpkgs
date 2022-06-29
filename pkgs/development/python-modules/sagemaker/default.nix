ccef4edeaf5175edb5c6a9fa3f81c7d833fb69d5
R. Ryantm <ryantm-bot@ryantm.com>
2022-06-29 23:16:15 +0000

    python310Packages.sagemaker: 2.96.0 -> 2.97.0

--- pkgs/development/python-modules/sagemaker/default.nix
+++ pkgs/development/python-modules/sagemaker/default.nix
@@ -17,14 +17,14 @@
 
 buildPythonPackage rec {
   pname = "sagemaker";
-  version = "2.96.0";
+  version = "2.97.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-40xvL7EwCDx/zsYHJhczx1MqVVrwQiDhlcv3QrEuv/E=";
+    hash = "sha256-iHh6WOefWmoLDzZYk1p2TtR0CAudP+paQOOj3RVFlRE=";
   };
 
   propagatedBuildInputs = [
