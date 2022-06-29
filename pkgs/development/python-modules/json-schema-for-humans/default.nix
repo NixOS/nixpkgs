f804d136074bb3ad43c8d3d2f83349044769603c
Fabian Affolter <mail@fabian-affolter.ch>
2022-06-29 20:02:33 +0200

    python310Packages.json-schema-for-humans: 0.41.1 -> 0.41.3

--- pkgs/development/python-modules/json-schema-for-humans/default.nix
+++ pkgs/development/python-modules/json-schema-for-humans/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "json-schema-for-humans";
-  version = "0.41.1";
+  version = "0.41.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.6";
@@ -28,7 +28,7 @@ buildPythonPackage rec {
     owner = "coveooss";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-JQqoQiug4n1o4PbGT/Ry/Qib11KmaTmkhPtZjhwmpc4=";
+    hash = "sha256-PEialHz5MJxi4PbtHYpx2CY78KgAKclijbF6cPr36vY=";
   };
 
   postPatch = ''
