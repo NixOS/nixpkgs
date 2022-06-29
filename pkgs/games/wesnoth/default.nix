17fada2a783027896c463343f452c239c554ce03
Evgeny Kurnevsky <kurnevsky@gmail.com>
2022-06-30 01:15:19 +0300

    wesnoth: 1.16.1 -> 1.16.3

--- pkgs/games/wesnoth/default.nix
+++ pkgs/games/wesnoth/default.nix
@@ -5,13 +5,13 @@
 
 stdenv.mkDerivation rec {
   pname = "wesnoth";
-  version = "1.16.1";
+  version = "1.16.3";
 
   src = fetchFromGitHub {
     rev = version;
     owner = "wesnoth";
     repo = "wesnoth";
-    sha256 = "sha256-eRegE+Q5dEtuT2XRKAK0sycRuK6sdSXKS6aLCsBDU8g=";
+    sha256 = "sha256-om6Tps5ZdAWq1WE9lq9v2oGwfPDPSRJ08pmMlT6DjGk=";
   };
 
   nativeBuildInputs = [ cmake pkg-config ];
