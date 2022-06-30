e0ff7e97b86cffc9771fcdc1b381651f2e6b9260
Vincent Laporte <Vincent.Laporte@gmail.com>
2022-06-30 06:29:07 +0100

    ocamlPackages.happy-eyeballs: init at 0.1.3

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -507,6 +507,8 @@ let
     hacl-star = callPackage ../development/ocaml-modules/hacl-star { };
     hacl-star-raw = callPackage ../development/ocaml-modules/hacl-star/raw.nix { };
 
+    happy-eyeballs = callPackage ../development/ocaml-modules/happy-eyeballs { };
+
     hashcons = callPackage ../development/ocaml-modules/hashcons { };
 
     herelib = callPackage ../development/ocaml-modules/herelib { };
