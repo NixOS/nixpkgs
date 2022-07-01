1c661285d08c09664b0f2f029450013d8dc342b9
Ilan Joselevich <personal@ilanjoselevich.com>
2022-07-01 18:04:16 +0300

    cryptsetup:Allow reading tokens from relative path

--- pkgs/os-specific/linux/cryptsetup/default.nix
+++ pkgs/os-specific/linux/cryptsetup/default.nix
@@ -1,5 +1,13 @@
-{ lib, stdenv, fetchurl, lvm2, json_c
-, openssl, libuuid, pkg-config, popt }:
+{ lib
+, stdenv
+, fetchurl
+, lvm2
+, json_c
+, openssl
+, libuuid
+, pkg-config
+, popt
+}:
 
 stdenv.mkDerivation rec {
   pname = "cryptsetup";
@@ -13,8 +21,12 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-/A35RRiBciZOxb8dC9oIJk+tyKP4VtR+upHzH+NUtQc=";
   };
 
-  # Disable 4 test cases that fail in a sandbox
-  patches = [ ./disable-failing-tests.patch ];
+  patches = [
+    # Disable 4 test cases that fail in sandbox
+    ./disable-failing-tests.patch
+    # Allow reading tokens from a relative path, see #167994
+    ./relative-token-path.patch
+  ];
 
   postPatch = ''
     patchShebangs tests
