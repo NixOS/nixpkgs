f86c463e0bee03c5f03041a136abde9a7563fa0b
Zoey de Souza Pessanha <zoey.spessanha@outlook.com>
2022-07-02 15:04:04 -0300

    update: hashes

--- pkgs/applications/virtualization/nixpacks/default.nix
+++ pkgs/applications/virtualization/nixpacks/default.nix
@@ -4,13 +4,13 @@ rustPlatform.buildRustPackage rec {
   pname = "nixpacks";
   version = "v0.1.7";
   doCheck = true;
-  cargoSha256 = "lEoCROgmRqcueQby4vR5GJOrQxCrjF3bBNeHsPmrAUk=";
+  cargoSha256 = "iA8ODh1NmUBfyH6syvVD32jDeLYG2LeLTo2lQoO93lc=";
   nativeBuildInputs = [ clippy ];
   src = fetchFromGitHub {
     owner = "railwayapp";
     repo = pname;
     rev = version;
-    sha256 = "";
+    sha256 = "UxdK2e5VYcOEYdZn0oGRFIVGiwnPixiZ3rOnqJDSQO8=";
   };
   # skip `cargo test` due tests FHS dependency
   checkPhase = ''
