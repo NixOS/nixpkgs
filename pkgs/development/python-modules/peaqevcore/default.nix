bd6fe08cc047416e9b991dffa2aaf8506282ab99
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-02 14:35:31 +0000

    python310Packages.peaqevcore: 2.1.1 -> 3.0.5

--- pkgs/development/python-modules/peaqevcore/default.nix
+++ pkgs/development/python-modules/peaqevcore/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "peaqevcore";
-  version = "2.1.1";
+  version = "3.0.5";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-MX4qWvM7V5xCibpwsaf/2JGOrWYfETHxNmZ6tyeFmKc=";
+    hash = "sha256-nRbpQxf2I+X9tgm7wZ1CGdlepDPvvwPpRjjLoDAR/k0=";
   };
 
   postPatch = ''
