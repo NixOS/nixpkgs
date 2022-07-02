f590e7d9432b099db5c3c6fa33d99a9a93401258
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-02 09:13:28 +0000

    orca: 42.1 -> 42.2

--- pkgs/applications/misc/orca/default.nix
+++ pkgs/applications/misc/orca/default.nix
@@ -34,13 +34,13 @@
 
 buildPythonApplication rec {
   pname = "orca";
-  version = "42.1";
+  version = "42.2";
 
   format = "other";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "6p6dLehwg4ewUqe+FqXmnOqfZ2jfrrRee9meRmNxYt4=";
+    sha256 = "mXHp/aGqK9ZKdrMxcxNcZBWaSQssS8/757lUj+38eCw=";
   };
 
   patches = [
