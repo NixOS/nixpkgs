2ec65a88fa2d2658813db120160d8be5ae2f114c
Jakub Kozłowski <kubukoz@gmail.com>
2022-07-03 17:52:36 +0200

    update

--- pkgs/applications/editors/vscode/extensions/generated.nix
+++ pkgs/applications/editors/vscode/extensions/generated.nix
@@ -1,45 +1,11 @@
 { buildVscodeMarketplaceExtension, lib }:
 {
-  Arjun.swagger-viewer = buildVscodeMarketplaceExtension {
+  _1Password.op-vscode = buildVscodeMarketplaceExtension {
     mktplcRef = {
-      publisher = "Arjun";
-      name = "swagger-viewer";
-      version = "3.1.2";
-      sha256 = "1cjvc99x1q5w3i2vnbxrsl5a1dr9gb3s6s9lnwn6mq5db6iz1nlm";
-    };
-  };
-  alexdima.copy-relative-path = buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      publisher = "alexdima";
-      name = "copy-relative-path";
-      version = "0.0.2";
-      sha256 = "06g601n9d6wyyiz659w60phgm011gn9jj5fy0gf5wpi2bljk3vcn";
-    };
-  };
-  alygin.vscode-tlaplus = buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      publisher = "alygin";
-      name = "vscode-tlaplus";
-      version = "1.5.4";
-      sha256 = "0mf98244z6wzb0vj6qdm3idgr2sr5086x7ss2khaxlrziif395dx";
-    };
-    meta.license = [ lib.licenses.mit ];
-  };
-  antfu.icons-carbon = buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      publisher = "antfu";
-      name = "icons-carbon";
-      version = "0.2.5";
-      sha256 = "12bs9i8fj11irnfk1j5jxcly91xql3nspwxf0rpfgy7fx1dyanmx";
-    };
-    meta.license = [ lib.licenses.mit ];
-  };
-  antfu.slidev = buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      publisher = "antfu";
-      name = "slidev";
-      version = "0.3.3";
-      sha256 = "0pqiwcvn5c8kwqlmz4ribwwra69gbiqvz41ig4fh29hkyh078rfk";
+      publisher = "1Password";
+      name = "op-vscode";
+      version = "1.0.0";
+      sha256 = "1q4wp3gkgv6lb0kq8qqvd2bzx52w8ya790dwzs8jp3waflzr7qk5";
     };
     meta.license = [ lib.licenses.mit ];
   };
