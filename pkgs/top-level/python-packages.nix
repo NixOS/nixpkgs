d9d731345e3c5480fa8e1b620968bcfd4b858a3d
Fabian Affolter <mail@fabian-affolter.ch>
2022-07-04 19:55:57 +0200

    python310Packages.dvc-objects: init at 0.0.18

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2647,9 +2647,11 @@ in {
 
   durus = callPackage ../development/python-modules/durus {  };
 
-  dvclive = callPackage ../development/python-modules/dvclive {  };
+  dvc-objects = callPackage ../development/python-modules/dvc-objects {  };
 
   dvc-render = callPackage ../development/python-modules/dvc-render {  };
+
+  dvclive = callPackage ../development/python-modules/dvclive {  };
 
   dwdwfsapi = callPackage ../development/python-modules/dwdwfsapi { };
 
