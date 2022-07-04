67b230bd08146e4a822ff1e6bc314501dc7b768c
Maximilian Bosch <maximilian@mbosch.me>
2022-07-04 07:24:07 +0200

    linux: 4.19.249 -> 4.19.250

--- pkgs/os-specific/linux/kernel/linux-4.19.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "4.19.249";
+  version = "4.19.250";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "14aiypira32hsw7wy9bhdw9rvfn705r0sb4415n9pfvi091bsjyf";
+    sha256 = "0p3mnl49708inhnrh461s6cnyn20qsqb9dw541h7crgsyd30mqvf";
   };
 } // (args.argsOverride or {}))
