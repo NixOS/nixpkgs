1dbf7b45e26a6ef990ea72387dc1195e185c5fa0
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-07-05 22:44:41 +0200

    openssl_3: 3.0.4 -> 3.0.5
    https://www.openssl.org/news/secadv/20220705.txt
    
    We already acted on the first public disclosure, so this release removes
    the previous patch and upgrades to the release including the fix.
    
    Related: CVE-2022-2274
    Fixes: CVE-2022-2097

--- pkgs/development/libraries/openssl/default.nix
+++ pkgs/development/libraries/openssl/default.nix
@@ -201,18 +201,14 @@ in {
   };
 
   openssl_3 = common {
-    version = "3.0.4";
-    sha256 = "sha256-KDGEPppmigq0eOcCCtY9LWXlH3KXdHLcc+/O+6/AwA8=";
+    version = "3.0.5";
+    sha256 = "sha256-qn2Nm+9xrWUlxVuhHl9Dl4ic5Jwsk0nc6m0+TwsCSno=";
     patches = [
       ./3.0/nix-ssl-cert-file.patch
 
       # openssl will only compile in KTLS if the current kernel supports it.
       # This patch disables build-time detection.
       ./3.0/openssl-disable-kernel-detection.patch
-
-      # https://guidovranken.com/2022/06/27/notes-on-openssl-remote-memory-corruption/
-      # https://github.com/openssl/openssl/commit/4d8a88c134df634ba610ff8db1eb8478ac5fd345.patch
-      3.0/rsa-fix-bn_reduce_once_in_place-call-for-rsaz_mod_exp_avx512_x2.patch
 
       (if stdenv.hostPlatform.isDarwin
        then ./use-etc-ssl-certs-darwin.patch
