447e562f815e8c89c3fb1824a3316dc9058cf0ea
Ryan Burns <rtburns@protonmail.com>
2022-07-05 19:21:49 -0700

    wibo: init at 0.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -35350,6 +35350,8 @@ with pkgs;
 
   websocketd = callPackage ../applications/networking/websocketd { };
 
+  wibo = pkgsi686Linux.callPackage ../applications/emulators/wibo { };
+
   wike = callPackage ../applications/misc/wike { };
 
   wikicurses = callPackage ../applications/misc/wikicurses {
