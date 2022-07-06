1087880e0b8f022cabbc9a8129ca7a2aa8f16bc6
Soham S Gumaste <sgumas2@uic.edu>
2022-07-05 22:31:09 -0500

    ocs-url: init at 3.1.0
    This package is used by DE store pages to one click install themes/fonts and other assets.
    
    Signed-off-by: Soham S Gumaste <sgumas2@uic.edu>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14604,6 +14604,7 @@
     email = "sohamg2@gmail.com";
     name = "Soham S Gumaste";
     github = "SohamG";
+    githubId = 7116239;
     keys = [{
       fingerprint = "E067 520F 5EF2 C175 3F60  50C0 BA46 725F 6A26 7442";
     }];
