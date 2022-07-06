NixOS/nixpkgs

commit 37c633f7ae518456af1f3064bd5c386e2de92c37

treewide: pkgs/applications: mark broken for darwin

pkgs/applications/version-management/git-and-tools/stgit/default.nix
@@ -1,4 +1,5 @@
-{ lib
+{ stdenv
+, lib
 , fetchFromGitHub
 , installShellFiles
 , python3Packages
@@ -65,6 +66,7 @@ python3Packages.buildPythonApplication rec {
   '';
 
   meta = with lib; {
+    broken = stdenv.isDarwin;
     description = "A patch manager implemented on top of Git";
     homepage = "https://stacked-git.github.io/";
     license = licenses.gpl2Only;

pkgs/applications/audio/muso/default.nix
@@ -29,6 +29,7 @@ rustPlatform.buildRustPackage rec {
   cargoSha256 = "1hgdzyz005244f2mh97js9ga0a6s2hcd6iydz07f1hmhsh1j2bwy";
 
   meta = with lib; {
+    broken = stdenv.isDarwin;
     description = "An automatic music sorter (based on ID3 tags)";
     homepage = "https://github.com/quebin31/muso";
     license = with licenses; [ gpl3Plus ];

pkgs/applications/audio/pithos/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchFromGitHub, meson, ninja, pkg-config, appstream-glib
+{ stdenv, lib, fetchFromGitHub, meson, ninja, pkg-config, appstream-glib
 , wrapGAppsHook, pythonPackages, gtk3, gnome, gobject-introspection
 , libnotify, libsecret, gst_all_1 }:
 
@@ -28,6 +28,7 @@ pythonPackages.buildPythonApplication rec {
     (with pythonPackages; [ pygobject3 pylast ]);
 
   meta = with lib; {
+    broken = stdenv.isDarwin;
     description = "Pandora Internet Radio player for GNOME";
     homepage = "https://pithos.github.io/";
     license = licenses.gpl3;

pkgs/applications/audio/sfxr/default.nix
@@ -46,6 +46,7 @@ stdenv.mkDerivation rec {
   makeFlags = [ "DESTDIR=$(out)" ];
 
   meta = with lib; {
+    broken = stdenv.isDarwin;
     homepage = "http://www.drpetter.se/project_sfxr.html";
     description = "A videogame sound effect generator";
     license = licenses.mit;

pkgs/applications/blockchains/openethereum/default.nix
@@ -41,6 +41,7 @@ rustPlatform.buildRustPackage rec {
   checkFlags = "--skip configuration::tests::should_resolve_external_nat_hosts";
 
   meta = with lib; {
+    broken = stdenv.isDarwin;
     description = "Fast, light, robust Ethereum implementation";
     homepage = "http://parity.io/ethereum";
     license = licenses.gpl3;

pkgs/applications/blockchains/particl-core/default.nix
@@ -43,6 +43,7 @@ stdenv.mkDerivation rec {
   enableParallelBuilding = true;
 
   meta = {
+    broken = stdenv.isDarwin;
     description = "Privacy-Focused Marketplace & Decentralized Application Platform";
     longDescription = ''
       An open source, decentralized privacy platform built for global person to person eCommerce.

pkgs/applications/blockchains/pivx/default.nix
@@ -64,6 +64,7 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with lib; {
+    broken = stdenv.isDarwin;
     description = "An open source crypto-currency focused on fast private transactions";
     longDescription = ''
       PIVX is an MIT licensed, open source, blockchain-based cryptocurrency with

pkgs/applications/editors/qxmledit/default.nix
@@ -22,6 +22,7 @@ stdenv.mkDerivation rec {
   dontWrapQtApps = true;
 
   meta = with lib; {
+    broken = stdenv.isDarwin;
     description = "Simple XML editor based on qt libraries" ;
     homepage = "https://sourceforge.net/projects/qxmledit";
     license = licenses.lgpl2;

commit bc068a39753d9fd74fa7521c311e47ad6757fe0d

stgit: mark as unbroken on darwin
Marked as broken by 37c633f7ae5 (treewide: pkgs/applications: mark
broken for darwin, 2022-05-28) probably by mistake. It builds and works
just fine.

pkgs/applications/version-management/git-and-tools/stgit/default.nix
@@ -66,7 +66,6 @@ python3Packages.buildPythonApplication rec {
   '';
 
   meta = with lib; {
-    broken = stdenv.isDarwin;
     description = "A patch manager implemented on top of Git";
     homepage = "https://stacked-git.github.io/";
     license = licenses.gpl2Only;