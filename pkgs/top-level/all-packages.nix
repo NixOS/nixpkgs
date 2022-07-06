c2c8e8fdd2e30937fc746c54a7b76c6ee87417b1
AndersonTorres <torres.anderson.85@protonmail.com>
2022-07-06 00:57:55 -0300

    xosview: init at 1.23

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -35452,6 +35452,8 @@ with pkgs;
 
   xosd = callPackage ../misc/xosd { };
 
+  xosview = callPackage ../tools/X11/xosview { };
+
   xosview2 = callPackage ../tools/X11/xosview2 { };
 
   xow_dongle-firmware = callPackage ../os-specific/linux/firmware/xow_dongle-firmware { };
