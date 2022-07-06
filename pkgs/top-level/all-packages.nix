95e8f32206c801ee9ac57e2e61848d3178168bee
Azat Bahawi <azat@bahawi.net>
2022-07-06 17:21:07 +0300

    linux_logo: init at 6.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23932,6 +23932,8 @@ with pkgs;
 
   quicktemplate = callPackage ../development/tools/quicktemplate { };
 
+  linux_logo = callPackage ../tools/misc/linux-logo { };
+
   linux-pam = callPackage ../os-specific/linux/pam { };
 
   nss_ldap = callPackage ../os-specific/linux/nss_ldap { };
