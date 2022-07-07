0750ba8925770360c8f59d40bf5f8ac382eadec7
Zoey de Souza Pessanha <zoey.spessanha@outlook.com>
2022-07-07 11:47:54 -0300

    Update pkgs/applications/virtualization/nixpacks/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/applications/virtualization/nixpacks/default.nix
+++ pkgs/applications/virtualization/nixpacks/default.nix
@@ -2,16 +2,20 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "nixpacks";
-  version = "v0.1.7";
-  cargoSha256 = "iA8ODh1NmUBfyH6syvVD32jDeLYG2LeLTo2lQoO93lc=";
-  # skip test due FHS dependency
-  doCheck = false;
+  version = "0.1.7";
+
   src = fetchFromGitHub {
     owner = "railwayapp";
     repo = pname;
-    rev = version;
+    rev = "v${version}";
     sha256 = "UxdK2e5VYcOEYdZn0oGRFIVGiwnPixiZ3rOnqJDSQO8=";
   };
+
+  cargoSha256 = "iA8ODh1NmUBfyH6syvVD32jDeLYG2LeLTo2lQoO93lc=";
+
+  # skip test due FHS dependency
+  doCheck = false;
+
   meta = with lib; {
     description = "App source + Nix packages + Docker = Image Resources";
     homepage = "https://github.com/railwayapp/nixpacks";
