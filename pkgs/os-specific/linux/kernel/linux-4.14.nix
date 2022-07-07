f0238d60982b865fef8097908e8d62b59f744bb8
Maximilian Bosch <maximilian@mbosch.me>
2022-07-07 21:19:28 +0200

    linux: 4.14.286 -> 4.14.287

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "4.14.286";
+  version = "4.14.287";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1x26fys9c2zai69wvvwpcxhdbbl02vijn4h7g6k6nq4y36fvkfyx";
+    sha256 = "05pnz2wch5b430j82sjqhxyfbpln4p1569avj2qkk62x4164kdbq";
   };
 } // (args.argsOverride or {}))
