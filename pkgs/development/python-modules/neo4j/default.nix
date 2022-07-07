bf55a19ca9dbcca86185674ef84bce93a4b9283a
Fabian Affolter <mail@fabian-affolter.ch>
2022-07-07 23:29:19 +0200

    python310Packages.neo4j: 4.4.4 -> 4.4.5

--- pkgs/development/python-modules/neo4j/default.nix
+++ pkgs/development/python-modules/neo4j/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "neo4j";
-  version = "4.4.4";
+  version = "4.4.5";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -16,7 +16,7 @@ buildPythonPackage rec {
     owner = "neo4j";
     repo = "neo4j-python-driver";
     rev = version;
-    hash = "sha256-Sd+ZeyJCzqGsBl3rdxfKPD0gYZ49qAfiRbuXaNGpj8M=";
+    hash = "sha256-BtftIpVKnIAwgLgdZUwHiVsKOpgy2bSb+9fC3ycpM4Y=";
   };
 
   propagatedBuildInputs = [
