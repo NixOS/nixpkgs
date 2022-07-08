5905c0de691efb4ec01a7a827b0ce4e83d2231f2
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-08 05:49:50 +0000

    clash: 1.11.0 -> 1.11.4

--- pkgs/tools/networking/clash/default.nix
+++ pkgs/tools/networking/clash/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "clash";
-  version = "1.11.0";
+  version = "1.11.4";
 
   src = fetchFromGitHub {
     owner = "Dreamacro";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-8g5rrkRA31wHvwY3JDFIU+slU0cLHFpADyd897hVyw0=";
+    sha256 = "sha256-NLGX72eZCfyh6y3mud/btMh15rNXnss7S0P7ujCX1ms=";
   };
 
-  vendorSha256 = "sha256-iW14KxtUY2nhpShcdrHLiCRZxsoXyLSPt01dB0Ds28Y=";
+  vendorSha256 = "sha256-zaWN/zI4WhHnEK12k1tWZ/qjLGvaZbJ4WfEvCZJ0+ms=";
 
   # Do not build testing suit
   excludedPackages = [ "./test" ];
