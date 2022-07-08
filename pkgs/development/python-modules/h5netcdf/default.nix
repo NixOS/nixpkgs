ab77b42008c31c21eec66b7d6ff079a1ed7d12d4
Jonas Heinrich <onny@project-insanity.org>
2022-07-08 11:04:52 +0200

    python310Packages.h5netcdf: 0.8.1 -> 1.0.1

--- pkgs/development/python-modules/h5netcdf/default.nix
+++ pkgs/development/python-modules/h5netcdf/default.nix
@@ -6,23 +6,21 @@
 , pytestCheckHook
 , netcdf4
 , pythonOlder
+, setuptools-scm
 }:
 
 buildPythonPackage rec {
   pname = "h5netcdf";
-  version = "0.8.1";
+  version = "1.0.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "09bbnpsvwksb07wijn7flyyza56h5b2g2cw0hb3slmwxz6cgcjmr";
+    sha256 = "sha256-d2cE+s2LgiRtbUBoxQXDibO3C5v5kgzPusfzXNxjTaw=";
   };
 
-  patches = [
-    (fetchpatch{
-      url = "https://patch-diff.githubusercontent.com/raw/h5netcdf/h5netcdf/pull/82.patch";
-    sha256 = "0x9bq9jl4kvw152adkpcyqslhpi7miv80hrnpl2w2y798mmbs0s4";
-    })
-  ];
+ nativeBuildInputs = [
+   setuptools-scm
+ ];
 
   propagatedBuildInputs = [
     h5py
