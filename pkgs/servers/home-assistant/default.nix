5baeaf5d6fdc10f1bf28147e12b98a74550f9b56
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-07-08 13:33:43 +0200

    home-assistant: 2022.7.0 -> 2022.7.1
    https://github.com/home-assistant/core/releases/tag/2022.7.1

--- pkgs/servers/home-assistant/default.nix
+++ pkgs/servers/home-assistant/default.nix
@@ -190,7 +190,7 @@ let
   extraPackagesFile = writeText "home-assistant-packages" (lib.concatMapStringsSep "\n" (pkg: pkg.pname) extraBuildInputs);
 
   # Don't forget to run parse-requirements.py after updating
-  hassVersion = "2022.7.0";
+  hassVersion = "2022.7.1";
 
 in python.pkgs.buildPythonApplication rec {
   pname = "homeassistant";
@@ -208,7 +208,7 @@ in python.pkgs.buildPythonApplication rec {
     owner = "home-assistant";
     repo = "core";
     rev = version;
-    hash = "sha256-DDRut+3wJutXcQVOf2KU+XuHs5XuKkd5R7dQIXwOIrU=";
+    hash = "sha256-p5Ht8poSUqaK3pluRuwmyk/tphihYI0H3XNu8YI06R4=";
   };
 
   # leave this in, so users don't have to constantly update their downstream patch handling
