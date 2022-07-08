40d692f7395760725e332fe4b464a2a879a0c276
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-08 19:51:25 +0000

    python310Packages.google-cloud-monitoring: 2.9.2 -> 2.10.0

--- pkgs/development/python-modules/google-cloud-monitoring/default.nix
+++ pkgs/development/python-modules/google-cloud-monitoring/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-monitoring";
-  version = "2.9.2";
+  version = "2.10.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-ePJGEBzo5lXBnCDMuZQPVoU9gT4yfDs8YZB50x4xb90=";
+    hash = "sha256-hnDzNkFluRjq/Bxk6NIXezGLDEMAcW4MLQG7F5U9Ez8=";
   };
 
   propagatedBuildInputs = [
