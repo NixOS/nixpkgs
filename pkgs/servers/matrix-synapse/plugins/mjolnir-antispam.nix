aa922e6648c51e401e9fe3a12964d4cdbb2cb34c
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-08 07:03:18 +0000

    matrix-synapse-plugins.matrix-synapse-mjolnir-antispam: 1.4.2 -> 1.5.0

--- pkgs/servers/matrix-synapse/plugins/mjolnir-antispam.nix
+++ pkgs/servers/matrix-synapse/plugins/mjolnir-antispam.nix
@@ -2,13 +2,13 @@
 
 buildPythonPackage rec {
   pname = "matrix-synapse-mjolnir-antispam";
-  version = "1.4.2";
+  version = "1.5.0";
 
   src = fetchFromGitHub {
     owner = "matrix-org";
     repo = "mjolnir";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-887azmXT5PGpcOqtWtKwdoyEtsXGm5DzpNRgEMlgSfM=";
+    sha256 = "sha256-YmP+r9W5e63Aw66lSQeTTbYwSF/vjPyHkoehJxtcRNw=";
   };
 
   sourceRoot = "./source/synapse_antispam";
