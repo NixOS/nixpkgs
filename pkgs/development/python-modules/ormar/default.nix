db01a66901e48cf5210a1ac679cb54592bcb2457
Fabian Affolter <mail@fabian-affolter.ch>
2022-07-10 00:24:52 +0200

    python310Packages.ormar: update stale substituteInPlace

--- pkgs/development/python-modules/ormar/default.nix
+++ pkgs/development/python-modules/ormar/default.nix
@@ -70,8 +70,8 @@ buildPythonPackage rec {
 
   postPatch = ''
     substituteInPlace pyproject.toml \
-      --replace 'SQLAlchemy = ">=1.3.18,<=1.4.31"' 'SQLAlchemy = ">=1.3.18"' \
-      --replace 'databases = ">=0.3.2,!=0.5.0,!=0.5.1,!=0.5.2,!=0.5.3,<=0.5.5"' 'databases = ">=0.5.5"'
+      --replace 'SQLAlchemy = ">=1.3.18,<1.4.39"' 'SQLAlchemy = ">=1.3.18"' \
+      --replace 'databases = ">=0.3.2,!=0.5.0,!=0.5.1,!=0.5.2,!=0.5.3,<0.6.1"' 'databases = ">=0.5.5"'
   '';
 
   disabledTests = [
