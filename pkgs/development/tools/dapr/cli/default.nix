661c78879f026922596d052e04542e7b8147b668
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-09 07:10:21 +0000

    dapr-cli: 1.7.1 -> 1.8.0

--- pkgs/development/tools/dapr/cli/default.nix
+++ pkgs/development/tools/dapr/cli/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "dapr-cli";
-  version = "1.7.1";
+  version = "1.8.0";
 
   src = fetchFromGitHub {
     owner = "dapr";
     repo = "cli";
     rev = "v${version}";
-    sha256 = "sha256-gEYN9r5hHRya1nqle8qHaUaOpuBN8cSLJx2FBRqyztw=";
+    sha256 = "sha256-ytn7fG84Wu4+fcgkV5B9djCw8KgAJWgffoNbV7wveK4=";
   };
 
-  vendorSha256 = "sha256-RGEoewLDKo+D9Wp/v8PI/LPjCh2rFrdLO/AS4RWFliY=";
+  vendorSha256 = "sha256-ZsuDaFcBPZuyt5rmjeBkzkrphCCcraLZCrMiQ2FtAUc=";
 
   nativeBuildInputs = [ installShellFiles ];
 
