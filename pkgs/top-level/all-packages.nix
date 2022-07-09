9ac130850aedd0bedad59998165d55868ad9ac35
Alexandre Acebedo <alexandre@acebedo.fr>
2022-07-09 21:54:04 +0200

    rofi-top: init at unstable-2017-10-16

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28727,6 +28727,8 @@ with pkgs;
 
   rofi-rbw = python3Packages.callPackage ../applications/misc/rofi-rbw { };
 
+  rofi-top = callPackage ../applications/misc/rofi-top { };
+
   rofi-vpn = callPackage ../applications/networking/rofi-vpn { };
 
   ympd = callPackage ../applications/audio/ympd { };
