f97b02c505df199277e2512f012af1fdf56e2e36
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-10 04:07:40 +0000

    python310Packages.jarowinkler: 1.1.0 -> 1.1.1

--- pkgs/development/python-modules/jarowinkler/default.nix
+++ pkgs/development/python-modules/jarowinkler/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "jarowinkler";
-  version = "1.1.0";
+  version = "1.1.1";
 
   disabled = pythonOlder "3.6";
 
@@ -24,8 +24,8 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "maxbachmann";
     repo = "JaroWinkler";
-    rev = "v${version}";
-    hash = "sha256-ilkslDrTOaq6mgg+nYMMqxrjvZHXMIcipu25wvV3+Mk=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-Sl12wxWCfwT0iu2EsabJUmOCRGqFXODYVFAb/KKK8gk=";
   };
 
   nativeBuildInputs = [
