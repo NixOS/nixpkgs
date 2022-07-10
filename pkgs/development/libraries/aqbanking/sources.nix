e5e2d53f6484bf9abf474fe10915e0c4c2bf30f7
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-07-10 19:36:57 +0200

    aqbanking: 6.3.0 -> 6.5.0
    (cherry picked from commit 1e68a02e4aeb4bdcd65882bc0aab20d04bc78651)

--- pkgs/development/libraries/aqbanking/sources.nix
+++ pkgs/development/libraries/aqbanking/sources.nix
@@ -5,7 +5,11 @@
   libchipcard.version = "5.0.4";
   libchipcard.sha256 = "0fj2h39ll4kiv28ch8qgzdbdbnzs8gl812qnm660bw89rynpjnnj";
   libchipcard.releaseId = "158";
-  aqbanking.version = "6.3.0";
-  aqbanking.sha256 = "1k2mhdnk0jc0inq1hmp74m3y7azxrjm8r07x5k1pp4ic0yi5vs50";
-  aqbanking.releaseId = "372";
+
+  # https://www.aquamaniac.de/rdm/projects/aqbanking/files
+  aqbanking = {
+    version = "6.5.0";
+    hash = "sha256-TS076ghulq2ntoGSBtTrQWjOt+Mtzppo3Gxuq8yetj4=";
+    releaseId = "435";
+  };
 }
