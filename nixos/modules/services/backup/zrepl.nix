2a69871cb80d360ce27160e527cb07a8c61d04cb
Sandro <sandro.jaeckel@gmail.com>
2022-07-10 16:01:49 +0200

    Update nixos/modules/services/backup/zrepl.nix

--- nixos/modules/services/backup/zrepl.nix
+++ nixos/modules/services/backup/zrepl.nix
@@ -15,9 +15,9 @@ in
 
       package = mkOption {
         type = types.package;
-        description = "Which package to use for zrepl";
         default = pkgs.zrepl;
         defaultText = literalExpression "pkgs.zrepl";
+        description = "Which package to use for zrepl";
       };
 
       settings = mkOption {
