50e68e5f4309654302c4cb6718333a72e10d585e
Konrad Borowski <konrad@borowski.pw>
2022-07-10 14:46:45 +0200

    codeql: 2.8.5 -> 2.10.0

--- pkgs/development/tools/analysis/codeql/default.nix
+++ pkgs/development/tools/analysis/codeql/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "codeql";
-  version = "2.8.5";
+  version = "2.10.0";
 
   dontConfigure = true;
   dontBuild = true;
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
 
   src = fetchzip {
     url = "https://github.com/github/codeql-cli-binaries/releases/download/v${version}/codeql.zip";
-    sha256 = "sha256-HZJBqm196RgWR/14mfrLYQlU+4W3t0b4TXme04XkfKw=";
+    sha256 = "sha256-kKQsPxyAdtCDV8x6Upl2e3NUzB7I8gMBogPyCfEMLMU=";
   };
 
   nativeBuildInputs = [
@@ -34,7 +34,11 @@ stdenv.mkDerivation rec {
 
     ln -sf $out/codeql/tools/linux64/lib64trace.so $out/codeql/tools/linux64/libtrace.so
 
-    sed -i 's%\$CODEQL_DIST/tools/\$CODEQL_PLATFORM/java%\${jdk11}%g' $out/codeql/codeql
+    # Many extractors use CODEQL_DIST + CODEQL_PLATFORM to resolve Java home
+    for path in $out/codeql/tools/{linux,osx}64/java{,-aarch64}; do
+      rm -rf "$path"
+      ln -s ${jdk11} "$path"
+    done
 
     ln -s $out/codeql/codeql $out/bin/
   '';
