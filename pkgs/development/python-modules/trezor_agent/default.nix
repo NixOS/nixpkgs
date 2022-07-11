15238ceaa3aa997c0b378f92a7858b1bd9a4c006
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-11 08:38:06 +0000

    python310Packages.trezor_agent: 0.11.0 -> 0.12.0

--- pkgs/development/python-modules/trezor_agent/default.nix
+++ pkgs/development/python-modules/trezor_agent/default.nix
@@ -15,11 +15,11 @@
 
 buildPythonPackage rec {
   pname = "trezor_agent";
-  version = "0.11.0";
+  version = "0.12.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "139d917d6495bf290bcc21da457f84ccd2e74c78b4d59a649e0cdde4288cd20c";
+    sha256 = "sha256-4IylpUvXZYAXFkyFGNbN9iPTsHff3M/RL2Eq9f7wWFU=";
   };
 
   propagatedBuildInputs = [ setuptools trezor libagent ecdsa ed25519 mnemonic keepkey semver wheel pinentry ];
