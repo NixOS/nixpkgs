5e297d07aa2d6047c130d14403d256a0912a78fe
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-07-11 03:48:00 +0200

    nixos/onlyoffice: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1085,6 +1085,7 @@
   ./services/web-apps/nifi.nix
   ./services/web-apps/node-red.nix
   ./services/web-apps/phylactery.nix
+  ./services/web-apps/onlyoffice.nix
   ./services/web-apps/pict-rs.nix
   ./services/web-apps/peertube.nix
   ./services/web-apps/plantuml-server.nix
