cfa1a2bc6377940c534ad65fbfbb19b5dfe03c81
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-11 06:17:06 +0000

    esbuild: 0.14.48 -> 0.14.49

--- pkgs/development/tools/esbuild/default.nix
+++ pkgs/development/tools/esbuild/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "esbuild";
-  version = "0.14.48";
+  version = "0.14.49";
 
   src = fetchFromGitHub {
     owner = "evanw";
     repo = "esbuild";
     rev = "v${version}";
-    sha256 = "sha256-ziZOIPBIUGyuS3SEKKAYDH7Tv1mLOegw9YqTpGV2plI=";
+    sha256 = "sha256-bQt9dLkwvXT4pLxTwYp+5Bg6IgwBnB8uu1gdAmhPn0A=";
   };
 
   vendorSha256 = "sha256-QPkBR+FscUc3jOvH7olcGUhM6OW4vxawmNJuRQxPuGs=";
