93f5569bd15246f3d7ae5058ef8c8ff011adefd7
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-11 03:06:01 +0000

    python310Packages.teslajsonpy: 2.2.1 -> 2.3.0

--- pkgs/development/python-modules/teslajsonpy/default.nix
+++ pkgs/development/python-modules/teslajsonpy/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "teslajsonpy";
-  version = "2.2.1";
+  version = "2.3.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "zabuldon";
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-fDHtdznxawX+gmFedxQLTTEvpDhhZLSbEndtXOgahUM=";
+    sha256 = "sha256-wAhi8TW0rOeJ3QWjmfLqJ3cKnLZShMekyQ6j7I2uwGY=";
   };
 
   nativeBuildInputs = [
