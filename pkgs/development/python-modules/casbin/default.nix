67258310baaa0d6fd42e1d6efa8e36c161ce3605
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-11 11:25:26 +0000

    python310Packages.casbin: 1.16.8 -> 1.16.9

--- pkgs/development/python-modules/casbin/default.nix
+++ pkgs/development/python-modules/casbin/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "casbin";
-  version = "1.16.8";
+  version = "1.16.9";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = pname;
     repo = "pycasbin";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-l98QfrRg7ghZ+jT9J2BNILUcinOKwhpnIMS+W8NQFr4=";
+    sha256 = "sha256-1xxjFNkCb50ndmXuRjt7svPOvSyzZbw+J49Zpyy1FUc=";
   };
 
   propagatedBuildInputs = [
