62840ca1d90c6255b838abe54d0f4c303d4f66af
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-12 16:34:09 +0000

    python310Packages.cssutils: 2.4.2 -> 2.5.0

--- pkgs/development/python-modules/cssutils/default.nix
+++ pkgs/development/python-modules/cssutils/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "cssutils";
-  version = "2.4.2";
+  version = "2.5.0";
 
   disabled = pythonOlder "3.7";
 
@@ -25,7 +25,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-h3gYv6lmjMU1dz9G5rakbeKENhkSEXQbP3tKqmTZr7s=";
+    hash = "sha256-1H5N1nsowm/5oeVBEV3u05YX/5JlERxtJQD3qBcHeVs=";
   };
 
   nativeBuildInputs = [
