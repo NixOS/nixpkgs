b66edf00b0884f2983a1b17d0df8dc8a7f150a28
José Romildo <malaquias@gmail.com>
2022-07-12 20:09:29 -0300

    numix-icon-theme-circle: 22.06.14 -> 22.07.11

--- pkgs/data/icons/numix-icon-theme-circle/default.nix
+++ pkgs/data/icons/numix-icon-theme-circle/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "numix-icon-theme-circle";
-  version = "22.06.14";
+  version = "22.07.11";
 
   src = fetchFromGitHub {
     owner = "numixproject";
     repo = pname;
     rev = version;
-    sha256 = "sha256-TPJKBDQXwZnYOfDqeCDvjNzwj9lWFKPrUTd9F07wTvQ=";
+    sha256 = "sha256-0wDSK1sBYEQ+3wr5BkZvvtFdG66uMzHYAtyvpaZZlTI=";
   };
 
   nativeBuildInputs = [ gtk3 ];
