2ae2e9500d1206c988bee22a8b369e04c3e81ea8
Alex Griffin <a@ajgrf.com>
2022-07-12 13:46:17 -0500

    xmonadctl: Add runHooks and supported platforms

--- pkgs/applications/window-managers/xmonad/xmonadctl.nix
+++ pkgs/applications/window-managers/xmonad/xmonadctl.nix
@@ -13,12 +13,15 @@ in stdenv.mkDerivation rec {
   };
 
   installPhase = ''
+    runHook preInstall
     mkdir -p $out/bin
     ${xmonadctlEnv}/bin/ghc -o $out/bin/xmonadctl \
       --make scripts/xmonadctl.hs
+    runHook postInstall
   '';
 
   meta = with lib; {
+    platforms = platforms.unix;
     description = "Send commands to a running instance of xmonad";
     homepage = "https://github.com/xmonad/xmonad-contrib";
     license = licenses.bsd3;
