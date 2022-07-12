cecee2a6d90c2147d3b60f06f6b26a3c133bbabf
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-07-12 08:47:46 +0200

    tautulli: 2.10.1 -> 2.10.2

--- pkgs/servers/tautulli/default.nix
+++ pkgs/servers/tautulli/default.nix
@@ -2,7 +2,7 @@
 
 buildPythonApplication rec {
   pname = "Tautulli";
-  version = "2.10.1";
+  version = "2.10.2";
   format = "other";
 
   pythonPath = [ setuptools ];
@@ -12,7 +12,7 @@ buildPythonApplication rec {
     owner = "Tautulli";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-qM3PiBZD0AfbhIdJFYFUGYhsB4U6ZZEW4i7S9waP7VE=";
+    sha256 = "sha256-nEiyYpj5J95tQAFcyRlaF5VEfosCkk4cmdYKLjfeA98=";
   };
 
   installPhase = ''
