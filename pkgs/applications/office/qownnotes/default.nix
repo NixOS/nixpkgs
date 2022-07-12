6b0546c40b215b8b0b5363a8cfbeb8bccfa62414
Matthias Thym <git@thym.at>
2022-07-12 14:27:23 +0200

    qownnotes: 22.6.1 -> 22.7.1

--- pkgs/applications/office/qownnotes/default.nix
+++ pkgs/applications/office/qownnotes/default.nix
@@ -5,13 +5,13 @@
 
 mkDerivation rec {
   pname = "qownnotes";
-  version = "22.6.1";
+  version = "22.7.1";
 
   src = fetchurl {
     url = "https://download.tuxfamily.org/${pname}/src/${pname}-${version}.tar.xz";
     # Fetch the checksum of current version with curl:
     # curl https://download.tuxfamily.org/qownnotes/src/qownnotes-<version>.tar.xz.sha256
-    sha256 = "c5b2075d42298d28f901ad2df8eb65f5a61aa59727fae9eeb1f92dac1b63d8ba";
+    sha256 = "9431a3315a533799525217e5ba03757b3c39e8259bf307c81330304f043b8b77";
   };
 
   nativeBuildInputs = [ qmake qttools ];
