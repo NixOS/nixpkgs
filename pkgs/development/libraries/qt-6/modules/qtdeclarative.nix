db57e21e3bde7164543d3c4ad8da252ed8e06431
Milan Hauth <milahu@gmail.com>
2022-07-13 12:39:59 +0200

    qt6.qtdeclarative: add patch: fix qt6_add_qml_module

--- pkgs/development/libraries/qt-6/modules/qtdeclarative.nix
+++ pkgs/development/libraries/qt-6/modules/qtdeclarative.nix
@@ -9,6 +9,11 @@ qtModule {
   pname = "qtdeclarative";
   qtInputs = [ qtbase qtshadertools ];
   propagatedBuildInputs = [ openssl python3 ];
+  patches = [
+    # TODO remove on qt 6.4
+    # https://bugreports.qt.io/browse/QTBUG-104928
+    ./qtdeclarative.fix-cmake-foreach-qt_additional_packages_prefix_paths.patch
+  ];
   preConfigure = ''
     export LD_LIBRARY_PATH="$PWD/build/lib''${LD_LIBRARY_PATH:+:}$LD_LIBRARY_PATH"
   '';
