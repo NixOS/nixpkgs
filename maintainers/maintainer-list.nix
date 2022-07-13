758d9a06e4ba59af26da7e93f9e98f7ad7971a11
Netali <me@netali.de>
2022-07-13 19:49:09 +0200

    maintainers: add netali

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9048,6 +9048,15 @@
     github = "nessdoor";
     githubId = 25993494;
   };
+  netali = {
+    name = "Jennifer Graul";
+    email = "me@netali.de";
+    github = "NetaliDev";
+    githubId = 15304894;
+    keys = [{
+      fingerprint = "F729 2594 6F58 0B05 8FB3  F271 9C55 E636 426B 40A9";
+    }];
+  };
   netcrns = {
     email = "jason.wing@gmx.de";
     github = "netcrns";
