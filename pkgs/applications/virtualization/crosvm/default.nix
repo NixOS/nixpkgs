b9c5058bf43b1c8d3a6c35468b91c75611df427d
Alyssa Ross <hi@alyssa.is>
2022-07-14 21:53:36 +0000

    crosvm: enable debug info

--- pkgs/applications/virtualization/crosvm/default.nix
+++ pkgs/applications/virtualization/crosvm/default.nix
@@ -20,6 +20,8 @@ in
 
     src = fetchgit (builtins.removeAttrs upstreamInfo.src [ "date" "path" ]);
 
+    separateDebugInfo = true;
+
     patches = [
       ./default-seccomp-policy-dir.diff
     ];
