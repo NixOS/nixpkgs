23d15582324e9df4315f565d16264d1d633451fc
Maximilian Bosch <maximilian@mbosch.me>
2022-07-14 21:58:44 +0200

    linux: 5.4.204 -> 5.4.205
    (cherry picked from commit e21d1188e0dc69096de8ef3bc00f66e6d5b2fb8c)

--- pkgs/os-specific/linux/kernel/linux-5.4.nix
+++ pkgs/os-specific/linux/kernel/linux-5.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.4.204";
+  version = "5.4.205";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0ivsfdw55mysihylk5n46r7limf1rqddipap17mkfnyq345b3hgx";
+    sha256 = "1m8ms5nizw3iimhw61kr11a09b6h8cfi8az3wwg4mcpb0k58lqrp";
   };
 } // (args.argsOverride or {}))
