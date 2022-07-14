729fb87ae3b56619333849c53687606e170ec7cc
Elis Hirwing <elis@hirwing.se>
2022-07-14 14:39:58 +0200

    treewide: Drop myself as maintainer for some packages I don't use

--- pkgs/development/python-modules/zigpy-cc/default.nix
+++ pkgs/development/python-modules/zigpy-cc/default.nix
@@ -54,7 +54,7 @@ buildPythonPackage rec {
     description = "Library which communicates with Texas Instruments CC2531 radios for zigpy";
     homepage = "https://github.com/zigpy/zigpy-cc";
     license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ etu mvnetbiz ];
+    maintainers = with maintainers; [ mvnetbiz ];
     platforms = platforms.linux;
   };
 }
