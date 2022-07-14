867c47e611cfe29f2afef692e3748b601e11ad17
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-14 01:07:38 +0000

    brave: 1.40.113 -> 1.41.96

--- pkgs/applications/networking/browsers/brave/default.nix
+++ pkgs/applications/networking/browsers/brave/default.nix
@@ -90,11 +90,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "brave";
-  version = "1.40.113";
+  version = "1.41.96";
 
   src = fetchurl {
     url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
-    sha256 = "sha256-+lJjLfxEOf82uvcVaRbWYQ93KEzWGVrzXvI9Rt1U9Bc=";
+    sha256 = "sha256-sPIzQkaa6PGxwLkCHYz2GdSDns5x/Q2YwWcXBWBx+Fo=";
   };
 
   dontConfigure = true;
