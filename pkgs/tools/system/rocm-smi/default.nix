9fee9397f93452a699b6fe099fb37681dfa7df61
Sebastian Neubauer <Sebastian.Neubauer@amd.com>
2022-07-14 11:56:03 +0000

    rocm-smi: 5.1.0 → 5.2.0

--- pkgs/tools/system/rocm-smi/default.nix
+++ pkgs/tools/system/rocm-smi/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "rocm-smi";
-  version = "5.1.0";
+  version = "5.2.0";
 
   src = fetchFromGitHub {
     owner = "RadeonOpenCompute";
     repo = "rocm_smi_lib";
     rev = "rocm-${version}";
-    hash = "sha256-11o4xUyeQ3W/RPY62r8ahwcljKh/rkVSyTk5ruTU66U=";
+    hash = "sha256-7e8nCCyFhecfC4TJrpqMI5buVZstynYgDWaa+LF85GA=";
   };
 
   nativeBuildInputs = [ cmake wrapPython ];
