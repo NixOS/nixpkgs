76ec68c7e4209f7692d83c15446baa158a952187
Sophie Taylor <sophie@spacekitteh.moe>
2022-07-15 10:48:14 +1000

    Stellarsolver: 2.2 -> 2.3

--- pkgs/development/libraries/science/astronomy/stellarsolver/default.nix
+++ pkgs/development/libraries/science/astronomy/stellarsolver/default.nix
@@ -3,13 +3,13 @@
 
 mkDerivation rec {
   pname = "stellarsolver";
-  version = "2.2";
+  version = "2.3";
 
   src = fetchFromGitHub {
     owner = "rlancaste";
     repo = pname;
     rev = version;
-    sha256 = "sha256-Ay7bszR4D5KKFiVLXfweJcc8jgUSZljnZVblEx7xh8o=";
+    sha256 = "sha256-1g5rs9bzbadbm6ss0c4b0491jqha7yifpfd48y0ilfl5mwwp6x62=";
   };
 
   nativeBuildInputs = [ cmake ];
