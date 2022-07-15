d4a98a7649599992df9c0186da6dfc0f024483d0
Maria <tuy_mearly@aleeas.com>
2022-07-15 08:14:10 -0400

    check-zfs: init at 2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22740,6 +22740,8 @@ with pkgs;
 
   check_systemd = callPackage ../servers/monitoring/nagios/plugins/check_systemd.nix { };
 
+  check_zfs = callPackage ../servers/monitoring/nagios/plugins/zfs.nix { };
+
   neo4j = callPackage ../servers/nosql/neo4j {
     jre = jre8_headless;
   };
