e1c0a925e065484d4963ad27700dead40a35863a
Charlotte Van Petegem <charlotte@vanpetegem.me>
2022-07-15 10:23:07 +0200

    xapian: Add warning about xapian-omega and python3Packages.xapian

--- pkgs/development/libraries/xapian/default.nix
+++ pkgs/development/libraries/xapian/default.nix
@@ -38,5 +38,8 @@ let
     };
   };
 in {
+  # Don't forget to change the hashes in xapian-omega and
+  # python3Packages.xapian. They inherit the version from this package, and
+  # should always be built with the equivalent xapian version.
   xapian_1_4 = generic "1.4.20" "sha256-zivl7/cgdcgQbANA5wsQk9vOviq0LcHBvgjdOtQZRC0=";
 }
