b306581d1056dd3f0b4afb9c79783c75170cf28d
Jonas Heinrich <onny@project-insanity.org>
2022-07-15 14:20:46 +0200

    python310Packages.distrax: init at 0.1.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2322,6 +2322,8 @@ in {
 
   distorm3 = callPackage ../development/python-modules/distorm3 { };
 
+  distrax = callPackage ../development/python-modules/distrax { };
+
   distributed = callPackage ../development/python-modules/distributed { };
 
   distro = callPackage ../development/python-modules/distro { };
