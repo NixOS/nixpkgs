01be5227ea12d56c916cf3ee7ab047cfe79d7034
Shane Sveller <shane@sveller.dev>
2022-07-15 09:38:54 -0500

    erlang: 25.0 -> 25.0.2
    https://www.erlang.org/patches/otp-25.0.1
    https://www.erlang.org/patches/otp-25.0.2
    (cherry picked from commit ddcc8217dcd4989f6af9b66375d9f3c739f4c974)

--- pkgs/development/interpreters/erlang/R25.nix
+++ pkgs/development/interpreters/erlang/R25.nix
@@ -3,6 +3,6 @@
 # How to obtain `sha256`:
 # nix-prefetch-url --unpack https://github.com/erlang/otp/archive/OTP-${version}.tar.gz
 mkDerivation {
-  version = "25.0";
-  sha256 = "1nf72yiirdpxcs8m10xc04ryghxxc73x954x38m5a6fhv2hfhp2n";
+  version = "25.0.2";
+  sha256 = "01jgmrskm04vdx560wry4xjy7xj57b82fhwyhn42hpv5k3dz4lp7";
 }
