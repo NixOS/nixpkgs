a2ec1c5fca5f131a26411dd0c18a59fcda3bc974
Guanpeng Xu <herberteuler@gmail.com>
2022-07-15 10:03:43 +0800

    nixos/modules/flake/source-info: improve example and description

--- nixos/modules/flake/source-info.nix
+++ nixos/modules/flake/source-info.nix
@@ -11,14 +11,26 @@ in
     system.nixos.configuration.sourceInfo = mkOption {
       type = types.attrsOf types.anything;
       default = {};
-      example = "self.sourceInfo";
-      description = ''
+      example = lib.literalExpression ''
+      {
+        inputs.nixpkgs.url = ...;
+        outputs = { self, nixpkgs }: {
+          nixosConfigurations = {
+            host = nixpkgs.lib.nixosSystem {
+              modules = [
+                { system.nixos.configuration.sourceInfo = self.sourceInfo; }
+              ] ++ ...;
+            };
+          };
+        };
+      }
+      '';
+      description = lib.mdDoc ''
         The source information of a flake-based NixOS configuration.
-        If set, the attribute <literal>configurationRevision</literal>
-        will appear properly in the output of
-        <literal>nixos-version --json</literal>.
+        If set, the attribute `configurationRevision` will appear
+        properly in the output of `nixos-version --json`.
 
-        Caution: Setting this option may result in unnecessary
+        **Caution:** Setting this option may result in unnecessary
         re-deployments if the flake repository changes but the
         specific system configuration does not change.
       '';
