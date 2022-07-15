a9126de8eaea3ea6f0ea8d2747e7a2aebd0025c3
Fabian Affolter <mail@fabian-affolter.ch>
2022-07-15 09:34:23 +0200

    python310Packages.pubnub: 6.4.0 -> 6.4.1

--- pkgs/development/python-modules/pubnub/default.nix
+++ pkgs/development/python-modules/pubnub/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "pubnub";
-  version = "6.4.0";
+  version = "6.4.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = pname;
     repo = "python";
     rev = "refs/tags/v${version}";
-    hash = "sha256-bOytN0HVJpqO87GLVntHlhJZm3En2h8o6LcvTyQlGNs=";
+    hash = "sha256-NpGhZrfUXKpz520l1S+cCfTrI2T/7XlO0Kk1e+XR8Ew=";
   };
 
   propagatedBuildInputs = [
