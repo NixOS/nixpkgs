f7741bf93c5e2490b2e88a7c170e954429c99639
zowoq <59103226+zowoq@users.noreply.github.com>
2022-07-16 13:55:51 +1000

    tilt: build on `x86_64-darwin`

--- pkgs/applications/networking/cluster/tilt/default.nix
+++ pkgs/applications/networking/cluster/tilt/default.nix
@@ -24,13 +24,5 @@ buildGoModule rec {
     homepage = "https://tilt.dev/";
     license = licenses.asl20;
     maintainers = with maintainers; [ anton-dessiatov ];
-
-    # TODO: Remove once nixpkgs uses macOS SDK 10.14+ for x86_64-darwin
-    # Undefined symbols for architecture x86_64:
-    # "_SecTrustEvaluateWithError", referenced from:
-    #     _crypto/x509/internal/macos.x509_SecTrustEvaluateWithError_trampoline.abi0 in go.o
-    # "_utimensat", referenced from:
-    #     _syscall.libc_utimensat_trampoline.abi0 in go.o
-    broken = stdenv.isDarwin && stdenv.isx86_64;
   };
 }
