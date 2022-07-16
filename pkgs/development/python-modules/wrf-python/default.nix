4f363b8af5407b2a113cad8f4b3a8520d97d4d94
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-07-16 12:59:45 +0200

    python3Packages.wrf-python: 1.3.3 -> 1.3.4.1

--- pkgs/development/python-modules/wrf-python/default.nix
+++ pkgs/development/python-modules/wrf-python/default.nix
@@ -3,17 +3,19 @@
 , fetchFromGitHub
 , pythonOlder
 , buildPythonPackage
+, basemap
 , gfortran
+, netcdf4
+, numpy
+, python
+, setuptools
 , xarray
 , wrapt
-, numpy
-, netcdf4
-, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "wrf-python";
-  version = "1.3.3";
+  version = "1.3.4.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -21,19 +23,20 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "NCAR";
     repo = "wrf-python";
-    rev = version;
-    hash = "sha256-+v4FEK0FVE0oAIb18XDTOInHKfxXyykb1ngk9Uxwf0c=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-4iIs/M9fzGJsnKCDSl09OTUoh7j6REBXuutE5uXFe3k=";
   };
-
-  propagatedBuildInputs = [
-    wrapt
-    numpy
-    setuptools
-    xarray
-  ];
 
   nativeBuildInputs = [
     gfortran
+  ];
+
+  propagatedBuildInputs = [
+    basemap
+    numpy
+    setuptools
+    xarray
+    wrapt
   ];
 
   checkInputs = [
@@ -43,7 +46,7 @@ buildPythonPackage rec {
   checkPhase = ''
     runHook preCheck
     cd ./test/ci_tests
-    python utests.py
+    ${python.interpreter} utests.py
     runHook postCheck
   '';
 
@@ -52,10 +55,10 @@ buildPythonPackage rec {
   ];
 
   meta = with lib; {
-    broken = (stdenv.isLinux && stdenv.isAarch64) || stdenv.isDarwin;
     description = "WRF postprocessing library for Python";
     homepage = "http://wrf-python.rtfd.org";
     license = licenses.asl20;
     maintainers = with maintainers; [ mhaselsteiner ];
+    broken = (stdenv.isLinux && stdenv.isAarch64) || stdenv.isDarwin;
   };
 }
