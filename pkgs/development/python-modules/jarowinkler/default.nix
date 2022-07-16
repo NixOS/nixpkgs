57a0185e88dafb00cbf993459433de221c735540
Mario Rodas <marsam@users.noreply.github.com>
2022-07-16 04:20:00 +0000

    python310Packages.jarowinkler: 1.1.1 -> 1.1.2
    https://github.com/maxbachmann/JaroWinkler/releases/tag/v1.1.2

--- pkgs/development/python-modules/jarowinkler/default.nix
+++ pkgs/development/python-modules/jarowinkler/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "jarowinkler";
-  version = "1.1.1";
+  version = "1.1.2";
 
   disabled = pythonOlder "3.6";
 
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "maxbachmann";
     repo = "JaroWinkler";
     rev = "refs/tags/v${version}";
-    hash = "sha256-Sl12wxWCfwT0iu2EsabJUmOCRGqFXODYVFAb/KKK8gk=";
+    hash = "sha256-yaJcmHkLxiRqrBiIel97mFxpav3N5QQSM+L9jA1kn5A=";
   };
 
   nativeBuildInputs = [
