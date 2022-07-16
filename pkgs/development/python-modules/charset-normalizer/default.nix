1d01cc396d57b37e99463dc6c845ab77dc4f6cf8
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-07-16 12:59:01 +0200

    python3Packages.charset-normalizer: 2.0.12 -> 2.1.0

--- pkgs/development/python-modules/charset-normalizer/default.nix
+++ pkgs/development/python-modules/charset-normalizer/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "charset-normalizer";
-  version = "2.0.12";
+  version = "2.1.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.5";
@@ -15,8 +15,8 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "Ousret";
     repo = "charset_normalizer";
-    rev = version;
-    hash = "sha256-d5vWnZtFR669l1Meg4ZSsYIyBlJZya7SpXJMx2AP8NU=";
+    rev = "refs/tags/${version}";
+    hash = "sha256-ntNMHjkQJqzSElEeyFmPIjUh6ZxQkTktPipfPHiJ/Vc=";
   };
 
   checkInputs = [
