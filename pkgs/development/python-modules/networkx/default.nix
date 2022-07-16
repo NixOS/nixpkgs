01ee8c8dd9e5325798a55953e4351fd3b849d42d
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-07-16 12:59:20 +0200

    python3Packages.networkx: 2.8.2 -> 2.8.4

--- pkgs/development/python-modules/networkx/default.nix
+++ pkgs/development/python-modules/networkx/default.nix
@@ -11,12 +11,12 @@
 buildPythonPackage rec {
   pname = "networkx";
   # upgrade may break sage, please test the sage build or ping @timokau on upgrade
-  version = "2.8.2";
+  version = "2.8.4";
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-rpnJsNNeW0pizxz+oB5bNjPY0C9KDq1paFtufeW4Xqs=";
+    sha256 = "sha256-XlPwJ8DVZ88fiE27KDIk31JWROQ6/RFF1kydiKNYR2I=";
   };
 
   propagatedBuildInputs = [ decorator setuptools ];
