13b3da1035ce87d5061b9cd68b6ff63e115188e2
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-07-16 12:59:21 +0200

    python3Packages.onlykey-solo-python: 0.0.28 -> 0.0.32

--- pkgs/development/python-modules/onlykey-solo-python/default.nix
+++ pkgs/development/python-modules/onlykey-solo-python/default.nix
@@ -13,12 +13,18 @@
 
 buildPythonPackage rec {
   pname = "onlykey-solo-python";
-  version = "0.0.28";
+  version = "0.0.32";
+  format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-Mbi5So2OgeXjg4Fzg7v2gAJuh1Y7ZCYu8Lrha/7PQfY=";
+    sha256 = "sha256-88DuhgX4FCwzIKzw4RqWgMtjRdf5huVlKEHAAEminuQ=";
   };
+
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace "fido2 == 0.9.3" "fido2"
+  '';
 
   patches = [
     # https://github.com/trustcrypto/onlykey-solo-python/pull/2
