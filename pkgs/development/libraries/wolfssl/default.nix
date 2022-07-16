9e94c6ed47d527a18ea253a21401d6ace471ccbc
Robert Scott <code@humanleg.org.uk>
2022-07-16 21:58:52 +0100

    wolfssl: add patches for CVE-2022-34293 & encrypted memory improvements
    these are the actionable security issues announced with 5.4.0

--- pkgs/development/libraries/wolfssl/default.nix
+++ pkgs/development/libraries/wolfssl/default.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , fetchFromGitHub
+, fetchpatch
 , autoreconfHook
 , openssl
 }:
@@ -15,6 +16,19 @@ stdenv.mkDerivation rec {
     rev = "v${version}-stable";
     sha256 = "sha256-KteArWAgDohlqEYaNfzLPuBn6uy5ABA8vV/LRCVIPGA=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "CVE-2022-34293.patch";
+      url = "https://github.com/wolfSSL/wolfssl/commit/17d7098bf63b2716ef36a72754c7c93baacc9cee.patch";
+      sha256 = "sha256-BgBMtOdyPV94aptlpt9IvcW9eJ0hlvlgp+H8LxQ2shs=";
+    })
+    (fetchpatch {
+      name = "ecc-dh-improve-encrypted-memory-implementations.patch";
+      url = "https://github.com/wolfSSL/wolfssl/commit/3c634e1f593586ff011623dd746f5a37d5659faf.patch";
+      sha256 = "sha256-AUATjk/GIWuh+qUw21Ruzj9yzYU11wNiLvOzagkILcw=";
+    })
+  ];
 
   postPatch = ''
     patchShebangs ./scripts
