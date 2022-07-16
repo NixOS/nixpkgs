NixOS/nixpkgs

commit 3a4ea08942255b92cbef5800efed748e2c5c80e3

imagemagick: 7.1.0-39 -> 7.1.0.43

pkgs/applications/graphics/ImageMagick/default.nix
@@ -45,13 +45,13 @@ in
 
 stdenv.mkDerivation rec {
   pname = "imagemagick";
-  version = "7.1.0-39";
+  version = "7.1.0.43";
 
   src = fetchFromGitHub {
     owner = "ImageMagick";
     repo = "ImageMagick";
     rev = version;
-    hash = "sha256-2KSsRkzaC3muNwH4GJfIiMy4pnSjh8waDpYRTuu6GG0=";
+    hash = "sha256-SOy7Ci1rzLB12ofSQBWmX86dfbr/ywsRPunHRswlAt4=";
   };
 
   outputs = [ "out" "dev" "doc" ]; # bin/ isn't really big

commit c82890e2e2c480c3e21701eaf6875edfafb2c926

imagemagick: fix version
imagemagick.passthru.tests tests that the Nixpkgs version for
imagemagick matches the one produced by magick --version, so the
recent upgrade that changed the dash to a dot broke the test.

Fixes: 3a4ea089422 ("imagemagick: 7.1.0-39 -> 7.1.0.43")

pkgs/applications/graphics/ImageMagick/default.nix
@@ -45,12 +45,12 @@ in
 
 stdenv.mkDerivation rec {
   pname = "imagemagick";
-  version = "7.1.0.43";
+  version = "7.1.0-43";
 
   src = fetchFromGitHub {
     owner = "ImageMagick";
     repo = "ImageMagick";
-    rev = version;
+    rev = builtins.replaceStrings [ "-" ] [ "." ] version;
     hash = "sha256-SOy7Ci1rzLB12ofSQBWmX86dfbr/ywsRPunHRswlAt4=";
   };
 