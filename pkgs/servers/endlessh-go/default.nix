d654eccd4acd1dda2e3478a4949a185da31f7f3a
Azat Bahawi <azat@bahawi.net>
2022-07-16 18:38:34 +0300

    endlessh-go: 20220308.1 -> 20220710

--- pkgs/servers/endlessh-go/default.nix
+++ pkgs/servers/endlessh-go/default.nix
@@ -5,22 +5,25 @@
 
 buildGoModule rec {
   pname = "endlessh-go";
-  version = "20220308.1";
+  version = "20220710";
 
   src = fetchFromGitHub {
     owner = "shizunge";
     repo = "endlessh-go";
     rev = version;
-    hash = "sha256-U+h/WmTVwwUIBEOiNa/EKS6HvkeoGNmP3NpeP1fcqYw=";
+    sha256 = "sha256-T8DLzHfITMLeHJtKuK4AjEzGGCIDJUPlqF2Lj56xPxY=";
   };
 
-  vendorSha256 = "sha256-5a/V2OQGVRIJVl/YG+lvtbLeHCy/1eiZu69zKIFqRKM=";
+  vendorSha256 = "sha256-hMCjYAqsI6h9B8iGVYQcNbKU5icalOHavvPKwOmvf/w=";
 
   proxyVendor = true;
+
+  ldflags = [ "-s" "-w" ];
 
   meta = with lib; {
     description = "An implementation of endlessh exporting Prometheus metrics";
     homepage = "https://github.com/shizunge/endlessh-go";
+    changelog = "https://github.com/shizunge/endlessh-go/releases/tag/${version}";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ azahi ];
   };
