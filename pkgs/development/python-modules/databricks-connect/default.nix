7f9d245894e08c457bcb1eba379ea8f91baaedf1
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-07-16 12:59:03 +0200

    python3Packages.databricks-connect: 9.1.17 -> 10.4.6

--- pkgs/development/python-modules/databricks-connect/default.nix
+++ pkgs/development/python-modules/databricks-connect/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "databricks-connect";
-  version = "9.1.17";
+  version = "10.4.6";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-QBGeXZ8zvRRjmKGs0vyBWTvcug3PF/BFV2asYTqsScw=";
+    sha256 = "sha256-Dezqn6rZysRhDQFUuTgXdoJL9dn21Bx2QlryBG9MtR0=";
   };
 
   sourceRoot = ".";
