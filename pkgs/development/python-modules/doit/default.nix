fa94bb4a4a8402497b11d56111a20a6d741a509c
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-07-16 12:59:05 +0200

    python3Packages.doit: 0.35.0 -> 0.36.0

--- pkgs/development/python-modules/doit/default.nix
+++ pkgs/development/python-modules/doit/default.nix
@@ -13,13 +13,13 @@
 
 buildPythonPackage rec {
   pname = "doit";
-  version = "0.35.0";
+  version = "0.36.0";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-cVoyLIdMTLhiOU46DWn/MlcrUln1cDb7/cEFPEwB00g=";
+    sha256 = "sha256-cdB8zJUUyyL+WdmJmVd2ZeqrV+FvZE0EM2rgtLriNLw=";
   };
 
   propagatedBuildInputs = [
