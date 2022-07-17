9560f94ef2d8d3274805df9d00a1bcda6d845935
Peder Bergebakken Sundt <pbsds@hotmail.com>
2022-07-18 01:32:12 +0200

    python3Packages.ffmpy: init at 0.3.1
    Co-authored-by: Yt <happysalada@tuta.io>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3674,6 +3674,8 @@ self: super: with self; {
 
   ffmpeg-progress-yield = callPackage ../development/python-modules/ffmpeg-progress-yield { };
 
+  ffmpy = callPackage ../development/python-modules/ffmpy { };
+
   fiblary3-fork = callPackage ../development/python-modules/fiblary3-fork { };
 
   fido2 = callPackage ../development/python-modules/fido2 { };
