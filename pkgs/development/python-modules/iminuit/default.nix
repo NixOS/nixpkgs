fab39d7fc0577c4b001dd745a902174eb2f813eb
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-17 17:23:45 +0000

    python310Packages.iminuit: 2.12.1 -> 2.13.0

--- pkgs/development/python-modules/iminuit/default.nix
+++ pkgs/development/python-modules/iminuit/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "iminuit";
-  version = "2.12.1";
+  version = "2.13.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-+le1b3wpze7QL5U1p7ZYB6zWoZfyCIUQlIIiLxoCPt4=";
+    hash = "sha256-40eFwqLArqb/hmcv6BuAoErJ1Cp57YJJYw8lKaj2oPo=";
   };
 
   nativeBuildInputs = [
