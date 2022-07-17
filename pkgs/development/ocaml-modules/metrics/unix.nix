1f7ab584cf86c10466c35046f4880a388f8dcf21
Vincent Laporte <Vincent.Laporte@gmail.com>
2022-07-17 14:33:14 +0200

    ocamlPackages.metrics: 0.2.0 → 0.4.0
    ocamlPackages.metrics-mirage: remove at 0.2.0
    ocamlPackages.metrics-rusage: init at 0.4.0

--- pkgs/development/ocaml-modules/metrics/unix.nix
+++ pkgs/development/ocaml-modules/metrics/unix.nix
@@ -1,12 +1,12 @@
-{ buildDunePackage, gnuplot, ocaml_lwt, metrics, metrics-lwt, mtime, uuidm }:
+{ buildDunePackage, gnuplot, lwt, metrics, metrics-lwt, mtime, uuidm }:
 
 buildDunePackage rec {
 
   pname = "metrics-unix";
 
-  inherit (metrics) version useDune2 src;
+  inherit (metrics) version src;
 
-  propagatedBuildInputs = [ gnuplot ocaml_lwt metrics mtime uuidm ];
+  propagatedBuildInputs = [ gnuplot lwt metrics mtime uuidm ];
 
   checkInputs = [ metrics-lwt ];
 
