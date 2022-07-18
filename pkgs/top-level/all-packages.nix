0005ee6060ba02bee022d06117a49d6ee0d9a7cd
sternenseemann <sternenseemann@systemli.org>
2022-07-18 12:39:33 +0200

    josh: init at 22.06.22

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27309,6 +27309,8 @@ with pkgs;
 
   jmusicbot = callPackage ../applications/audio/jmusicbot { };
 
+  josh = callPackage ../applications/version-management/josh { };
+
   junction = callPackage ../applications/misc/junction { };
 
   lemonade = callPackage ../applications/misc/lemonade { };
