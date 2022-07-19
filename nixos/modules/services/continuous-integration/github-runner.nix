def43d9f925c894c427bd636adb3a65223c9eef6
Vincent Haupert <mail@vincent-haupert.de>
2022-07-19 13:00:15 +0200

    nixos/github-runner: use state instead of runtime dir as `RUNNER_ROOT`
    Using the runtime directory as `RUNNER_ROOT` is wrong. We should always
    use the state directory like we already do when invoking the runner
    configure script. Otherwise, the runner constructs the wrong path for
    some files (.credentials, .runner, ...).

--- nixos/modules/services/continuous-integration/github-runner.nix
+++ nixos/modules/services/continuous-integration/github-runner.nix
@@ -137,7 +137,7 @@ in
 
       environment = {
         HOME = runtimeDir;
-        RUNNER_ROOT = runtimeDir;
+        RUNNER_ROOT = stateDir;
       };
 
       path = (with pkgs; [
@@ -214,7 +214,7 @@ in
                 echo "Configuring GitHub Actions Runner"
 
                 token=$(< "$STATE_DIRECTORY"/${newConfigTokenFilename})
-                RUNNER_ROOT="$STATE_DIRECTORY" ${cfg.package}/bin/config.sh \
+                ${cfg.package}/bin/config.sh \
                   --unattended \
                   --disableupdate \
                   --work "$RUNTIME_DIRECTORY" \
