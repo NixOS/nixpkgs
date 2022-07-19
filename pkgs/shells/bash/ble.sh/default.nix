7da3f228c483fc7b53a988cd00203bbb330b60f0
aiotter <git@aiotter.com>
2022-07-19 10:16:19 +0900

    ble.sh: refactor
    Follow more standard approach

--- pkgs/shells/bash/ble.sh/default.nix
+++ pkgs/shells/bash/ble.sh/default.nix
@@ -1,27 +1,32 @@
-{ lib, stdenv, git, fetchgit }:
+{ lib, stdenvNoCC, fetchgit, git, runtimeShell }:
 
-stdenv.mkDerivation {
+stdenvNoCC.mkDerivation {
   name = "ble.sh";
   version = "0.3.3";
-
-  nativeBuildInputs = [ git ];
 
   src = fetchgit {
     url = "https://github.com/akinomyoga/ble.sh.git";
     rev = "refs/tags/v0.3.3";
     hash = "sha256-Gfo2S1t5Kdy+8TEDS4M5yhyRShvzQIljdE0MQK1CL+4=";
     fetchSubmodules = true;
+    leaveDotGit = true;
   };
+
+  nativeBuildInputs = [ git ];
 
   doCheck = true;
 
+  installFlags = [ "INSDIR=$(out)/share" ];
   postInstall = ''
     mkdir -p "$out/bin"
-    echo 'source "$(dirname "$BASH_SOURCE")/../share/ble.sh"' >"$out/bin/ble.sh"
-    chmod +x "$out/bin/ble.sh"
-    '';
-
-  makeFlags = [ "INSDIR=$(out)/share" ];
+    cat <<SCRIPT > "$out/bin/ble.sh-share"
+    #!${runtimeShell}
+    # Run this script to find the ble.sh shared folder
+    # where all the shell scripts are living.
+    echo "$out/share/ble.sh"
+    SCRIPT
+    chmod +x "$out/bin/ble.sh-share"
+  '';
 
   meta = with lib; {
     homepage = "https://github.com/akinomyoga/ble.sh";
