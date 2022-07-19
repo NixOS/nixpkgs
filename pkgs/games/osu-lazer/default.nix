3817a25e42b4d0f75f8198016bca0dd1f423cba1
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-07-19 23:37:18 +0100

    osu-lazer: add thiagokokada as maintainer

--- pkgs/games/osu-lazer/default.nix
+++ pkgs/games/osu-lazer/default.nix
@@ -73,7 +73,7 @@ buildDotnetModule rec {
       cc-by-nc-40
       unfreeRedistributable # osu-framework contains libbass.so in repository
     ];
-    maintainers = with maintainers; [ oxalica ];
+    maintainers = with maintainers; [ oxalica thiagokokada ];
     platforms = [ "x86_64-linux" ];
     mainProgram = "osu!";
   };
