e11fb00b4ea36c4444785864f7f739f59788e880
Yu Matsuzawa <ymtszw@gmail.com>
2022-07-20 02:43:11 +0900

    feat: upgrade R24 to 24.3.4.2

--- pkgs/development/interpreters/erlang/R24.nix
+++ pkgs/development/interpreters/erlang/R24.nix
@@ -3,6 +3,6 @@
 # How to obtain `sha256`:
 # nix-prefetch-url --unpack https://github.com/erlang/otp/archive/OTP-${version}.tar.gz
 mkDerivation {
-  version = "24.2";
-  sha256 = "10s57v2i2qqyg3gddm85n3crzrkikl4zfwgzqmxjzdynsyb4xg68";
+  version = "24.3.4.2";
+  sha256 = "10gym0bdmrk0dph8gyy3nf38lgapxnwmpjdkpaak31d74caf230a";
 }
