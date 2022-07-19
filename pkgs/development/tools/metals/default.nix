f49a09b5a2effde9bc1cdbab8133935a9ab3b9ea
Kevin Rauscher <kevin.rauscher@tomahna.fr>
2022-07-19 21:51:19 +0200

    metals: remove client specific wrappers

--- pkgs/development/tools/metals/default.nix
+++ pkgs/development/tools/metals/default.nix
@@ -29,23 +29,8 @@ stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/bin
 
-    # This variant is not targeted at any particular client, clients are
-    # expected to declare their supported features in initialization options.
     makeWrapper ${jre}/bin/java $out/bin/metals \
       --add-flags "${extraJavaOpts} -cp $CLASSPATH scala.meta.metals.Main"
-
-    # Further variants targeted at clients with featuresets pre-set.
-    makeWrapper ${jre}/bin/java $out/bin/metals-emacs \
-      --add-flags "${extraJavaOpts} -Dmetals.client=emacs -cp $CLASSPATH scala.meta.metals.Main"
-
-    makeWrapper ${jre}/bin/java $out/bin/metals-vim \
-      --add-flags "${extraJavaOpts} -Dmetals.client=coc.nvim -cp $CLASSPATH scala.meta.metals.Main"
-
-    makeWrapper ${jre}/bin/java $out/bin/metals-vim-lsc \
-      --add-flags "${extraJavaOpts} -Dmetals.client=vim-lsc -cp $CLASSPATH scala.meta.metals.Main"
-
-    makeWrapper ${jre}/bin/java $out/bin/metals-sublime \
-      --add-flags "${extraJavaOpts} -Dmetals.client=sublime -cp $CLASSPATH scala.meta.metals.Main"
   '';
 
   meta = with lib; {
