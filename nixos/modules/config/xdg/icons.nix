8a79dfd94a77a07cc5afab2e2aaeb7b99f0b8039
pennae <github@quasiparticle.net>
2022-07-19 15:05:45 +0200

    nixos/config: invariant option docs MD conversions

--- nixos/modules/config/xdg/icons.nix
+++ nixos/modules/config/xdg/icons.nix
@@ -10,9 +10,9 @@ with lib;
     xdg.icons.enable = mkOption {
       type = types.bool;
       default = true;
-      description = ''
+      description = lib.mdDoc ''
         Whether to install files to support the
-        <link xlink:href="https://specifications.freedesktop.org/icon-theme-spec/icon-theme-spec-latest.html">XDG Icon Theme specification</link>.
+        [XDG Icon Theme specification](https://specifications.freedesktop.org/icon-theme-spec/icon-theme-spec-latest.html).
       '';
     };
   };
