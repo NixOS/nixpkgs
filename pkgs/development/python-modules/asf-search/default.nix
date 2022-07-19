73b3613021e7d2082ba22e7fe3bcefaf7a3d23fe
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-19 10:49:09 +0000

    python310Packages.asf-search: 4.0.3 -> 5.0.0

--- pkgs/development/python-modules/asf-search/default.nix
+++ pkgs/development/python-modules/asf-search/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "asf-search";
-  version = "4.0.3";
+  version = "5.0.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "asfadmin";
     repo = "Discovery-asf_search";
     rev = "refs/tags/v${version}";
-    hash = "sha256-Af6Nyrl1mrYTG24/nIv+x5Znk20HOubjjPAUvbdnj+Y=";
+    hash = "sha256-/E4Lzwyb7dxbuRZGfkRg2erVccWQhRHp6NEbbw1XDYw=";
   };
 
   propagatedBuildInputs = [
