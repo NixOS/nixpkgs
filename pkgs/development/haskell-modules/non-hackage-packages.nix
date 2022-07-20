8080821eeefa6360535dac3a5b920401c992f1b0
sternenseemann <sternenseemann@systemli.org>
2022-07-20 12:05:09 +0200

    haskellPackages.distribution-nixpkgs: 1.6.2 -> 1.7.0
    Since stable cabal2nix now supports the release, we can finally upgrade.

--- pkgs/development/haskell-modules/non-hackage-packages.nix
+++ pkgs/development/haskell-modules/non-hackage-packages.nix
@@ -13,9 +13,7 @@ self: super: {
 
   # Used by maintainers/scripts/regenerate-hackage-packages.sh, and generated
   # from the latest master instead of the current version on Hackage.
-  cabal2nix-unstable = self.callPackage ./cabal2nix-unstable.nix {
-    distribution-nixpkgs = self.distribution-nixpkgs_1_7_0;
-  };
+  cabal2nix-unstable = self.callPackage ./cabal2nix-unstable.nix { };
 
   # https://github.com/channable/vaultenv/issues/1
   vaultenv = self.callPackage ../tools/haskell/vaultenv { };
