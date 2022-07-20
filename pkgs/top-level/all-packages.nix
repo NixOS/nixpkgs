e36bd3c473bf86f0ec18faee2f5aa310316ec7cd
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-07-20 02:01:43 +0200

    libxcrypt: build using perl without libcrypt support

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20709,7 +20709,9 @@ with pkgs;
 
   libx86 = callPackage ../development/libraries/libx86 {};
 
-  libxcrypt = callPackage ../development/libraries/libxcrypt { };
+  libxcrypt = callPackage ../development/libraries/libxcrypt {
+    perl = perl.override { enableCrypt = false; };
+  };
 
   libxdg_basedir = callPackage ../development/libraries/libxdg-basedir { };
 
