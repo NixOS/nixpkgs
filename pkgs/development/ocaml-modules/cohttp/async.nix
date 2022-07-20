a003d6f971075eee849261d0119a285ec448996e
Aaron L. Zeng <me@bcc32.com>
2022-07-20 18:03:50 -0400

    ocamlPackages.cohttp-async: Remove redundant patch that doesn't apply

--- pkgs/development/ocaml-modules/cohttp/async.nix
+++ pkgs/development/ocaml-modules/cohttp/async.nix
@@ -1,6 +1,4 @@
 { lib
-, fetchpatch
-, fetchurl
 , buildDunePackage
 , ppx_sexp_conv
 , base
@@ -56,13 +54,6 @@ buildDunePackage {
     mirage-crypto
     core
   ];
-
-  # Compatibility with core 0.15.  No longer needed after updating cohttp to 5.0.0.
-  patches = fetchpatch {
-    url = "https://github.com/mirage/ocaml-cohttp/commit/5a7124478ed31c6b1fa6a9a50602c2ec839083b5.patch";
-    sha256 = "0i99rl8604xqwb6d0yzk9ws4dflbn0j4hv2nba2qscbqrrn22rw3";
-  };
-  patchFlags = "-p1 -F3";
 
   meta = cohttp.meta // {
     description = "CoHTTP implementation for the Async concurrency library";
