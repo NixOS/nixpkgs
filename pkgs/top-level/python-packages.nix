54ad27d1564ac328eac1e3d67df7d921a676dfa5
Sandro Jäckel <sandro.jaeckel@sap.com>
2022-07-20 14:36:14 +0200

    python310Packages.safeio: init at 1.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9483,6 +9483,8 @@ in {
 
   safe = callPackage ../development/python-modules/safe { };
 
+  safeio = callPackage ../development/python-modules/safeio { };
+
   safety = callPackage ../development/python-modules/safety { };
 
   sagemaker = callPackage ../development/python-modules/sagemaker { };
