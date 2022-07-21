accf1514b6bbd19cee8cf949d236fce11cc8264a
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-07-21 15:02:21 +0200

    python3Packages.mailsuite: 1.8.0 -> 1.9.2

--- pkgs/development/python-modules/mailsuite/default.nix
+++ pkgs/development/python-modules/mailsuite/default.nix
@@ -4,31 +4,43 @@
 , lib
 
 # pythonPackages
+, hatchling
 , dnspython
+, expiringdict
 , html2text
 , mail-parser
 , imapclient
+, publicsuffix2
 }:
 
 buildPythonPackage rec {
   pname = "mailsuite";
-  version = "1.8.0";
+  version = "1.9.2";
+  format = "pyproject";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-ItGOHnYL5rJvllcU/xAie95f09TnCf4OF4Y9oN65FTY=";
+    hash = "sha256-UV9cdWdUnUt4j1Puf1H0UxXsCHi3t4uNiKHwYNfTfa4=";
   };
+
+  nativeBuildInputs = [
+    hatchling
+  ];
 
   propagatedBuildInputs = [
     dnspython
+    expiringdict
     html2text
     mail-parser
     imapclient
+    publicsuffix2
   ];
 
   pythonImportsCheck = [ "mailsuite" ];
+
+  doCheck = false;
 
   meta = {
     description = "A Python package to simplify receiving, parsing, and sending email";
