ef4940ba155f762faa9b3711764272f3208e89b8
superherointj <5861043+superherointj@users.noreply.github.com>
2022-07-21 19:27:21 -0300

    obs-studio-plugins.looking-glass: normalized name and folder

--- pkgs/applications/video/obs-studio/plugins/default.nix
+++ pkgs/applications/video/obs-studio/plugins/default.nix
@@ -6,12 +6,14 @@ rec {
   gstreamer = callPackage ./gstreamer {};
     obs-gstreamer = gstreamer; # Alias added 2022-07-21
 
+  looking-glass = callPackage ./looking-glass {};
+    looking-glass-obs = looking-glass; # Alias added 2022-07-21
+
   obs-move-transition = callPackage ./obs-move-transition.nix {};
   obs-multi-rtmp = libsForQt5.callPackage ./obs-multi-rtmp.nix {};
   obs-ndi = libsForQt5.callPackage ./obs-ndi.nix {};
   obs-websocket = libsForQt5.callPackage ./obs-websocket.nix {};
   wlrobs = callPackage ./wlrobs.nix {};
-  looking-glass-obs = callPackage ./looking-glass-obs.nix {};
   obs-nvfbc = callPackage ./obs-nvfbc.nix {};
   obs-pipewire-audio-capture = callPackage ./obs-pipewire-audio-capture.nix {};
   obs-vkcapture = callPackage ./obs-vkcapture.nix {};
