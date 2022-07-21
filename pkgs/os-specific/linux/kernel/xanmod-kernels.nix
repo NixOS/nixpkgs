8aced39e55ba6983cd31d0387152ea143800b2da
fortuneteller2k <lythe1107@gmail.com>
2022-07-21 19:49:41 +0000

    xanmod-kernels: remove explicit WERROR=n
    (cherry picked from commit e5d17fcc91cb7c6b110174027eb2234c7a6a8269)

--- pkgs/os-specific/linux/kernel/xanmod-kernels.nix
+++ pkgs/os-specific/linux/kernel/xanmod-kernels.nix
@@ -24,51 +24,45 @@ let
       inherit hash;
     };
 
-    structuredExtraConfig =
-      with lib.kernel;
-      with (lib.kernel.whenHelpers version);
-      {
-        # TODO: remove this once https://github.com/NixOS/nixpkgs/pull/175433 is in master
-        WERROR = no;
+    structuredExtraConfig = with lib.kernel; {
+      # removed options
+      CFS_BANDWIDTH = lib.mkForce (option no);
+      RT_GROUP_SCHED = lib.mkForce (option no);
+      SCHED_AUTOGROUP = lib.mkForce (option no);
 
-        # removed options
-        CFS_BANDWIDTH = lib.mkForce (option no);
-        RT_GROUP_SCHED = lib.mkForce (option no);
-        SCHED_AUTOGROUP = lib.mkForce (option no);
+      # AMD P-state driver
+      X86_AMD_PSTATE = yes;
 
-        # AMD P-state driver
-        X86_AMD_PSTATE = yes;
+      # Paragon's NTFS3 driver
+      NTFS3_FS = module;
+      NTFS3_LZX_XPRESS = yes;
+      NTFS3_FS_POSIX_ACL = yes;
 
-        # Paragon's NTFS3 driver
-        NTFS3_FS = module;
-        NTFS3_LZX_XPRESS = yes;
-        NTFS3_FS_POSIX_ACL = yes;
+      # Preemptive Full Tickless Kernel at 500Hz
+      SCHED_CORE = lib.mkForce (option no);
+      PREEMPT_VOLUNTARY = lib.mkForce no;
+      PREEMPT = lib.mkForce yes;
+      NO_HZ_FULL = yes;
+      HZ_500 = yes;
 
-        # Preemptive Full Tickless Kernel at 500Hz
-        SCHED_CORE = lib.mkForce (option no);
-        PREEMPT_VOLUNTARY = lib.mkForce no;
-        PREEMPT = lib.mkForce yes;
-        NO_HZ_FULL = yes;
-        HZ_500 = yes;
+      # Google's BBRv2 TCP congestion Control
+      TCP_CONG_BBR2 = yes;
+      DEFAULT_BBR2 = yes;
 
-        # Google's BBRv2 TCP congestion Control
-        TCP_CONG_BBR2 = yes;
-        DEFAULT_BBR2 = yes;
+      # FQ-PIE Packet Scheduling
+      NET_SCH_DEFAULT = yes;
+      DEFAULT_FQ_PIE = yes;
 
-        # FQ-PIE Packet Scheduling
-        NET_SCH_DEFAULT = yes;
-        DEFAULT_FQ_PIE = yes;
+      # Graysky's additional CPU optimizations
+      CC_OPTIMIZE_FOR_PERFORMANCE_O3 = yes;
 
-        # Graysky's additional CPU optimizations
-        CC_OPTIMIZE_FOR_PERFORMANCE_O3 = yes;
+      # Futex WAIT_MULTIPLE implementation for Wine / Proton Fsync.
+      FUTEX = yes;
+      FUTEX_PI = yes;
 
-        # Futex WAIT_MULTIPLE implementation for Wine / Proton Fsync.
-        FUTEX = yes;
-        FUTEX_PI = yes;
-
-        # WineSync driver for fast kernel-backed Wine
-        WINESYNC = module;
-      };
+      # WineSync driver for fast kernel-backed Wine
+      WINESYNC = module;
+    };
 
     extraMeta = {
       branch = lib.versions.majorMinor version;
