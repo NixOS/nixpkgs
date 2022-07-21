a6ebda87e37dea20815a811a4570de4d0fd12aa8
superherointj <5861043+superherointj@users.noreply.github.com>
2022-07-21 19:17:59 -0300

    obs-studio-plugins.gstreamer: normalized name and folder

--- pkgs/applications/video/obs-studio/plugins/default.nix
+++ pkgs/applications/video/obs-studio/plugins/default.nix
@@ -1,7 +1,11 @@
 { callPackage, libsForQt5 }:
 
-{
-  obs-gstreamer = callPackage ./obs-gstreamer.nix {};
+# Normalization: Remove 'obs' from plugin name (since all are 'obs' plugins)
+
+rec {
+  gstreamer = callPackage ./gstreamer {};
+    obs-gstreamer = gstreamer; # Alias added 2022-07-21
+
   obs-move-transition = callPackage ./obs-move-transition.nix {};
   obs-multi-rtmp = libsForQt5.callPackage ./obs-multi-rtmp.nix {};
   obs-ndi = libsForQt5.callPackage ./obs-ndi.nix {};
