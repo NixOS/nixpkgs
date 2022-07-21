6553ad1fbed0e7c53298ade3ddeb3767096ddf7a
superherointj <5861043+superherointj@users.noreply.github.com>
2022-07-21 19:42:45 -0300

    obs-studio-plugins.ndi: normalize name and folder

--- pkgs/applications/video/obs-studio/plugins/default.nix
+++ pkgs/applications/video/obs-studio/plugins/default.nix
@@ -15,7 +15,9 @@ rec {
   multi-rtmp = libsForQt5.callPackage ./multi-rtmp {};
     obs-multi-rtmp = multi-rtmp; # Alias added 2022-07-21
 
-  obs-ndi = libsForQt5.callPackage ./obs-ndi.nix {};
+  ndi = libsForQt5.callPackage ./ndi {};
+    obs-ndi = ndi; # Alias added 2022-07-21
+
   obs-websocket = libsForQt5.callPackage ./obs-websocket.nix {};
   wlrobs = callPackage ./wlrobs.nix {};
   obs-nvfbc = callPackage ./obs-nvfbc.nix {};
