459221b4a1b9d5e09bbd148999bf098799709e5d
Vincent Laporte <Vincent.Laporte@gmail.com>
2022-07-22 17:59:51 +0200

    ocamlPackages.omd: 1.3.1 → 1.3.2

--- pkgs/development/ocaml-modules/omd/default.nix
+++ pkgs/development/ocaml-modules/omd/default.nix
@@ -1,25 +1,15 @@
-{ stdenv, lib, fetchurl, ocaml, findlib, ocamlbuild }:
+{ lib, buildDunePackage, fetchurl }:
 
-stdenv.mkDerivation rec {
-  pname = "ocaml${ocaml.version}-omd";
-  version = "1.3.1";
+buildDunePackage rec {
+  pname = "omd";
+  version = "1.3.2";
+
+  minimalOCamlVersion = "4.03";
 
   src = fetchurl {
-    url = "https://github.com/Chris00/omd/releases/download/${version}/omd-${version}.tar.gz";
-    sha256 = "1sgdgzpx96br7npj8mh91cli5mqmzsjpngwm7x4212n3k1d0ivwa";
+    url = "https://github.com/ocaml/omd/releases/download/${version}/omd-${version}.tbz";
+    sha256 = "sha256-YCPhZCYx8I9njrVyWCCHnte7Wj/+53fN7evCjB+F+ts=";
   };
-
-  nativeBuildInputs = [ ocaml findlib ocamlbuild ];
-
-  strictDeps = true;
-
-  createFindlibDestdir = true;
-
-  configurePhase = ''
-    runHook preConfigure
-    ocaml setup.ml -configure --prefix $out
-    runHook postConfigure
-  '';
 
   meta = {
     description = "Extensible Markdown library and tool in OCaml";
@@ -27,6 +17,5 @@ stdenv.mkDerivation rec {
     license = lib.licenses.isc;
     maintainers = [ lib.maintainers.vbgl ];
     mainProgram = "omd";
-    inherit (ocaml.meta) platforms;
   };
 }
