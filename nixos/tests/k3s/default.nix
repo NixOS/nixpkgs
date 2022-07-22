b6da1d8198e47033027750440551b4ff427068dd
Euan Kemp <euank@euank.com>
2022-07-21 23:50:25 -0700

    nixos/tests/k3s: add multi-node test
    This adds a basic multi-node test and verifies networking works between
    em.

--- nixos/tests/k3s/default.nix
+++ nixos/tests/k3s/default.nix
@@ -4,4 +4,6 @@
 {
   # Run a single node k3s cluster and verify a pod can run
   single-node = import ./single-node.nix { inherit system pkgs; };
+  # Run a multi-node k3s cluster and verify pod networking works across nodes
+  multi-node = import ./multi-node.nix { inherit system pkgs; };
 }
