8fd2f23bb5fdc9e6f5d2c7326abc4cd33f5f8276
aiotter <git@aiotter.com>
2022-07-22 18:15:13 +0900

    ble.sh: disable self-update

--- pkgs/shells/bash/ble.sh/default.nix
+++ pkgs/shells/bash/ble.sh/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenvNoCC, fetchgit, git, runtimeShell }:
+{ lib, stdenvNoCC, fetchgit, git, curl, jq, nix-prefetch, runtimeShell }:
 
 stdenvNoCC.mkDerivation {
   name = "ble.sh";
@@ -13,19 +13,47 @@ stdenvNoCC.mkDerivation {
   };
 
   nativeBuildInputs = [ git ];
+  buildInputs = [ curl jq nix-prefetch ];
 
   doCheck = true;
 
   installFlags = [ "INSDIR=$(out)/share" ];
   postInstall = ''
     mkdir -p "$out/bin"
-    cat <<SCRIPT > "$out/bin/ble.sh-share"
+    cat <<SCRIPT >"$out/bin/ble.sh-share"
     #!${runtimeShell}
     # Run this script to find the ble.sh shared folder
     # where all the shell scripts are living.
     echo "$out/share/ble.sh"
     SCRIPT
     chmod +x "$out/bin/ble.sh-share"
+
+    mkdir -p "$out/share/lib"
+    cat <<SCRIPT >"$out/share/lib/_package.sh"
+    _ble_base_package_type=nix
+
+    function ble/base/package:nix/update {
+      local rev=\$(${curl}/bin/curl -sL 'https://api.github.com/repos/akinomyoga/ble.sh/branches/master' | ${jq}/bin/jq -r '.commit.sha')
+      local hash="\$(${nix-prefetch}/bin/nix-prefetch fetchgit --url 'https://github.com/akinomyoga/ble.sh.git' --branchName master --rev \$rev --leaveDotGit --fetchSubmodules 2>/dev/null)"
+
+      cat <<-EOS >/dev/stderr
+    	You cannot update ble.sh automatically via Nix.
+    	The latest ble.sh from master branch can be installed by overriding nixpkgs like following.
+
+    	  pkgs.ble-sh.overrideAttrs (_: {
+    	    src = pkgs.fetchgit {
+    	      url = "https://github.com/akinomyoga/ble.sh.git";
+    	      rev = "\$rev";
+    	      hash = "\$hash";
+    	      fetchSubmodules = true;
+    	      leaveDotGit = true;
+    	    };
+    	    doCheck = false;
+    	  });
+    	EOS
+      return 1
+    }
+    SCRIPT
   '';
 
   meta = with lib; {
