540f8d2015f9c65eb619bd6ddf38086ec06afc84
Jan Tojnar <jtojnar@gmail.com>
2022-07-23 15:46:12 +0000

    gnome.gnome-software: 42.4 → 43.alpha
    https://gitlab.gnome.org/GNOME/gnome-software/-/compare/42.4...43.alpha
    
    Enable malcontent and gudev support. We did not have the former packaged when it was added (https://github.com/NixOS/nixpkgs/commit/59816457b6222ae4ed0ef3c41c5b32a026a2e60a) and probably forgot to introduce the latter (https://github.com/NixOS/nixpkgs/commit/1c1d677149797d6be7e484b020aa4ac69efc55be).
    
    Also do not build tests since they require extra dependencies and do not work anyway.
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/gnome-software/default.nix
+++ pkgs/desktops/gnome/core/gnome-software/default.nix
@@ -24,7 +24,9 @@
 , gtk4
 , gsettings-desktop-schemas
 , gnome-desktop
+, libgudev
 , libxmlb
+, malcontent
 , json-glib
 , libsecret
 , valgrind-light
@@ -42,11 +44,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "gnome-software";
-  version = "42.4";
+  version = "43.alpha";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-software/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "cRgp7mf58qG2S/oXQTdzuY8NxdIZ649sohfNZXK7SnQ=";
+    sha256 = "Xx913hkGKXJyBeubS886hP1tpwxAdZAXB6V3sOh5P6g=";
   };
 
   patches = [
@@ -87,18 +89,19 @@ stdenv.mkDerivation rec {
     ostree
     polkit
     flatpak
+    libgudev
     libxmlb
+    malcontent
     libsysprof-capture
   ] ++ lib.optionals withFwupd [
     fwupd
   ];
 
   mesonFlags = [
-    "-Dgudev=false"
-    # FIXME: package malcontent parental controls
-    "-Dmalcontent=false"
     # Needs flatpak to upgrade
     "-Dsoup2=true"
+    # Requires /etc/machine-id, D-Bus system bus, etc.
+    "-Dtests=false"
   ] ++ lib.optionals (!withFwupd) [
     "-Dfwupd=false"
   ];
