d39035792eba19c58dc744f9a33ed493177f458c
Jan Tojnar <jtojnar@gmail.com>
2022-07-23 14:16:52 +0200

    gnome.sushi: Fix video previews
    GStreamer was missing gtksink element.
    
    Fixes: https://github.com/NixOS/nixpkgs/issues/182542
    (cherry picked from commit 09e38a1d8a8c034ec8be0fc2fd1b22139855a66d)

--- pkgs/desktops/gnome/core/sushi/default.nix
+++ pkgs/desktops/gnome/core/sushi/default.nix
@@ -54,7 +54,7 @@ stdenv.mkDerivation rec {
     libepoxy
     gst_all_1.gstreamer
     gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
+    (gst_all_1.gst-plugins-good.override { gtkSupport = true; })
     gst_all_1.gst-plugins-bad
     gst_all_1.gst-plugins-ugly
   ];
