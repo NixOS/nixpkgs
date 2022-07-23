0211bd8a216255569f3bcdfd6fe607209bcb7e9b
Jan Tojnar <jtojnar@gmail.com>
2022-07-23 15:46:04 +0000

    gnome.gnome-remote-desktop: 42.4 → 43.alpha
    https://gitlab.gnome.org/GNOME/gnome-remote-desktop/-/compare/42.4...43.alpha
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/gnome-remote-desktop/default.nix
+++ pkgs/desktops/gnome/core/gnome-remote-desktop/default.nix
@@ -5,6 +5,7 @@
 , ninja
 , pkg-config
 , python3
+, asciidoc
 , wrapGAppsHook
 , glib
 , libepoxy
@@ -28,11 +29,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-remote-desktop";
-  version = "42.4";
+  version = "43.alpha";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    hash = "sha256-TU0jPvov+lRnMGo8w86Le6IyUtQtSxJy1crJ1d5Fy5o=";
+    hash = "sha256-MGLnrsKcyQYoPZ0hY0q3cv8NGVxS8n4aLFzMdtehKXE=";
   };
 
   nativeBuildInputs = [
@@ -40,6 +41,7 @@ stdenv.mkDerivation rec {
     ninja
     pkg-config
     python3
+    asciidoc
     wrapGAppsHook
   ];
 
