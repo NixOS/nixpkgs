39feddf58b2e04283d2d9913291878587e0f4bf9
Jan Tojnar <jtojnar@gmail.com>
2022-07-23 15:49:09 +0000

    vte: 0.68.0 → 0.69.90
    https://gitlab.gnome.org/GNOME/vte/-/compare/0.68.0...0.69.90
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/development/libraries/vte/default.nix
+++ pkgs/development/libraries/vte/default.nix
@@ -27,13 +27,13 @@
 
 stdenv.mkDerivation rec {
   pname = "vte";
-  version = "0.68.0";
+  version = "0.69.90";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-E+fUeJyiFqM3gAMNJGybE92/0ECUxjFu6n/5IoTdF0k=";
+    sha256 = "sha256-FB6AvuUExv5yWVrqkP2FSvxPNEkXsbW/34OqPIOWBM0=";
   };
 
   patches = [
