35d6ac96461e5fe217c5c3c376185e2eea44df79
Jan Tojnar <jtojnar@gmail.com>
2022-07-23 15:44:52 +0000

    gjs: 1.72.1 → 1.73.1
    https://gitlab.gnome.org/GNOME/gjs/-/compare/1.72.1...1.73.1

--- pkgs/development/libraries/gjs/default.nix
+++ pkgs/development/libraries/gjs/default.nix
@@ -31,13 +31,13 @@ let
   ];
 in stdenv.mkDerivation rec {
   pname = "gjs";
-  version = "1.72.1";
+  version = "1.73.1";
 
   outputs = [ "out" "dev" "installedTests" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gjs/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-F8Cx7D8JZnH/i/q6bku/FBmMcBPGBL/Wd6mFjaB5wKs=";
+    sha256 = "sha256-k/XuvxCLrR8uHb6sPBf9NHGs2o7acfQecMhdkDNR8As=";
   };
 
   patches = [
