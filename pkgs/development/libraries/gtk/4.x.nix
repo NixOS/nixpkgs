155fbef3cc547738f3b3658fd68e71ef6b827af5
Jan Tojnar <jtojnar@gmail.com>
2022-07-23 15:47:49 +0000

    gtk4: 4.6.6 → 4.7.1
    https://gitlab.gnome.org/GNOME/gtk/-/compare/4.6.6...4.7.1
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/development/libraries/gtk/4.x.nix
+++ pkgs/development/libraries/gtk/4.x.nix
@@ -62,7 +62,7 @@ in
 
 stdenv.mkDerivation rec {
   pname = "gtk4";
-  version = "4.6.6";
+  version = "4.7.1";
 
   outputs = [ "out" "dev" ] ++ lib.optionals x11Support [ "devdoc" ];
   outputBin = "dev";
@@ -74,7 +74,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/gtk/${lib.versions.majorMinor version}/gtk-${version}.tar.xz";
-    sha256 = "e7/k0TVp98KX7UmDSscmPjGLe/EC0ycctGbVlx9ZrnA=";
+    sha256 = "2+BJXSkzxGFRW/7YhrV/TRC05woMfObJM9hyC0ANCbQ=";
   };
 
   nativeBuildInputs = [
