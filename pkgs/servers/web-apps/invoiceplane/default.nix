0434c8fbaa14c7096d8a290d3839b48393f08597
Jonas Heinrich <onny@project-insanity.org>
2022-07-23 11:15:46 +0200

    invoiceplane: Mark as broken, missing php 8 support

--- pkgs/servers/web-apps/invoiceplane/default.nix
+++ pkgs/servers/web-apps/invoiceplane/default.nix
@@ -16,27 +16,30 @@ stdenv.mkDerivation rec {
     # https://github.com/NixOS/nixpkgs/issues/166655
     # https://github.com/InvoicePlane/InvoicePlane/pull/754
     (fetchpatch {
-      url = "https://patch-diff.githubusercontent.com/raw/InvoicePlane/InvoicePlane/pull/754.patch";
+      url = "https://github.com/InvoicePlane/InvoicePlane/commit/5db67d65ccaaec661885bd029b1e516bf3ca6c2a.patch";
       sha256 = "sha256-EHXw7Zqli/nA3tPIrhxpt8ueXvDtshz0XRzZT78sdQk=";
+      name = "cve-2021-29024-fix.patch";
     })
 
     # Fix CVE-2021-29023, password reset rate-limiting
-    # Should be included in a later release > 1.5.11
-    # https://github.com/NixOS/nixpkgs/issues/166655
-    # https://github.com/InvoicePlane/InvoicePlane/pull/739
-    (fetchpatch {
-      url = "https://patch-diff.githubusercontent.com/raw/InvoicePlane/InvoicePlane/pull/739.patch";
-      sha256 = "sha256-6ksJjW6awr3lZsDRxa22pCcRGBVBYyV8+TbhOp6HBq0=";
-    })
-
-    # Fix CVE-2021-29022, full path disclosure
     # Should be included in a later release > 1.5.11
     # https://github.com/NixOS/nixpkgs/issues/166655
     # https://github.com/InvoicePlane/InvoicePlane/pull/767
     #(fetchpatch {
     #  url = "https://patch-diff.githubusercontent.com/raw/InvoicePlane/InvoicePlane/pull/767.patch";
     #  sha256 = "sha256-rSWDH8KeHSRWLyQEa7RSwv+8+ja9etTz+6Q9XThuwUo=";
+    #  name = "cve-2021-29023-fix.patch";
     #})
+
+    # Fix CVE-2021-29022, full path disclosure
+    # Should be included in a later release > 1.5.11
+    # https://github.com/NixOS/nixpkgs/issues/166655
+    # https://github.com/InvoicePlane/InvoicePlane/pull/739
+    (fetchpatch {
+      url = "https://github.com/InvoicePlane/InvoicePlane/commit/694ba97d6e207bd4357ce4d2992601f0320f69fe.patch";
+      sha256 = "sha256-6ksJjW6awr3lZsDRxa22pCcRGBVBYyV8+TbhOp6HBq0=";
+      name = "cve-2021-29022-fix.patch"
+    })
 
   ];
 
@@ -54,10 +57,16 @@ stdenv.mkDerivation rec {
   };
 
   meta = with lib; {
-    description = "Self-hosted open source application for managing your invoices, clients and payments";
+    description = ''
+      Self-hosted open source application for managing your invoices, clients
+      and payments
+    '';
     license = licenses.mit;
     homepage = "https://www.invoiceplane.com";
     platforms = platforms.all;
     maintainers = with maintainers; [ onny ];
+    # Not yet working with php >= 8
+    # https://github.com/InvoicePlane/InvoicePlane/issues/798
+    broken = true;
   };
 }
