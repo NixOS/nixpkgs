91efc7d6644baa04cd0f99768072828b1ccfc12d
Jan Tojnar <jtojnar@gmail.com>
2022-07-23 15:45:25 +0000

    gnome.gedit: 42.2 → 43.alpha
    https://gitlab.gnome.org/GNOME/gedit/-/compare/42.2...43.alpha
    
    Changelog-Reviewed-By: Bobby Rong <rjl931189261@126.com>

--- pkgs/desktops/gnome/apps/gedit/default.nix
+++ pkgs/desktops/gnome/apps/gedit/default.nix
@@ -25,11 +25,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gedit";
-  version = "42.2";
+  version = "43.alpha";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gedit/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "PGIpER8KwGauRJZJIHkdEmX1u7VrC9lJppt7EmH8j8o=";
+    sha256 = "K3iOqhbYeeOVdGmS/OdmC5WtIpQ5OgHs0t5CzdWW95o=";
   };
 
   patches = [
