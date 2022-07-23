e91ddec77ea57c4f214b1913810592560c2aca15
Jan Tojnar <jtojnar@gmail.com>
2022-07-23 15:44:06 +0000

    at-spi2-core: 2.44.1 → 2.45.1
    atk and at-spi2-atk were merged into the repo.
    
    https://gitlab.gnome.org/GNOME/at-spi2-core/-/compare/AT_SPI2_CORE_2_44_1...AT_SPI2_CORE_2_45_1
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/development/libraries/at-spi2-core/default.nix
+++ pkgs/development/libraries/at-spi2-core/default.nix
@@ -11,6 +11,7 @@
 , glib
 , dconf
 , libX11
+, libxml2
 , libXtst
 , libXi
 , libXext
@@ -19,13 +20,13 @@
 
 stdenv.mkDerivation rec {
   pname = "at-spi2-core";
-  version = "2.44.1";
+  version = "2.45.1";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "S+sjJwumz3yvILWXNU11GU2Jr7adLvzxX0JxaIum90Y=";
+    sha256 = "upXzRukxCPuzRixiQ3CB1YIVTbJ5tAUt7cUqcGgosZI=";
   };
 
   nativeBuildInputs = [
@@ -38,6 +39,7 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     libX11
+    libxml2
     # at-spi2-core can be build without X support, but due it is a client-side library, GUI-less usage is a very rare case
     libXtst
     libXi
@@ -46,6 +48,7 @@ stdenv.mkDerivation rec {
   ];
 
   # In atspi-2.pc dbus-1 glib-2.0
+  # In atk.pc gobject-2.0
   propagatedBuildInputs = [
     dbus
     glib
@@ -55,6 +58,7 @@ stdenv.mkDerivation rec {
   doCheck = false;
 
   mesonFlags = [
+    "-Dintrospection=${if stdenv.buildPlatform == stdenv.hostPlatform then "yes" else "no"}"
     # Provide dbus-daemon fallback when it is not already running when
     # at-spi2-bus-launcher is executed. This allows us to avoid
     # including the entire dbus closure in libraries linked with
@@ -80,7 +84,7 @@ stdenv.mkDerivation rec {
     description = "Assistive Technology Service Provider Interface protocol definitions and daemon for D-Bus";
     homepage = "https://gitlab.gnome.org/GNOME/at-spi2-core";
     license = licenses.lgpl21Plus;
-    maintainers = teams.gnome.members;
+    maintainers = teams.gnome.members ++ (with maintainers; [ raskin ]);
     platforms = platforms.unix;
   };
 }
