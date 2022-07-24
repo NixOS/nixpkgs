a67bbc746517d6d7028f6f1ba5de33b5406320a9
Nick Cao <nickcao@nichi.co>
2022-07-24 13:35:45 +0800

    bpf-linker: init at 0.9.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23379,6 +23379,8 @@ with pkgs;
 
   bolt = callPackage ../os-specific/linux/bolt { };
 
+  bpf-linker = callPackage ../development/tools/bpf-linker { };
+
   bpfmon = callPackage ../os-specific/linux/bpfmon { };
 
   bridge-utils = callPackage ../os-specific/linux/bridge-utils { };
