1e7c97691c497c7a440fa76def9498d6707cf9b3
Jan Tojnar <jtojnar@gmail.com>
2022-07-24 07:47:58 +0200

    fixup! gnome.gnome-control-center: 42.3 → 43.alpha

--- pkgs/desktops/gnome/core/gnome-control-center/default.nix
+++ pkgs/desktops/gnome/core/gnome-control-center/default.nix
@@ -74,7 +74,6 @@ stdenv.mkDerivation rec {
     (substituteAll {
       src = ./paths.patch;
       gcm = gnome-color-manager;
-      gnome_desktop = gnome-desktop;
       inherit glibc libgnomekbd tzdata;
       inherit cups networkmanagerapplet;
     })
