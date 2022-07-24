3afb2daf367b48352061e88d1fec94ef7f3e9f0d
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-24 15:10:06 +0000

    python310Packages.geopandas: 0.11.0 -> 0.11.1

--- pkgs/development/python-modules/geopandas/default.nix
+++ pkgs/development/python-modules/geopandas/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "geopandas";
-  version = "0.11.0";
+  version = "0.11.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -22,8 +22,8 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "geopandas";
     repo = "geopandas";
-    rev = "v${version}";
-    hash = "sha256-w3F2Uqr/+DdG2Td5YmHdF/LII2y29rQkGK5ooMUcfRk=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-+9hPRPR9AJZqITc3CINEKWyFb+GQiRwbsdvlXxQBRu8=";
   };
 
   propagatedBuildInputs = [
