45702c76fce78c1bd790d615297980eccc0ef287
Jan Tojnar <jtojnar@gmail.com>
2022-07-24 08:16:12 +0200

    fixup! libpanel: init at 1.0.alpha

--- pkgs/development/libraries/libpanel/default.nix
+++ pkgs/development/libraries/libpanel/default.nix
@@ -1,19 +1,14 @@
 { lib
 , stdenv
 , fetchurl
-, ninja
 , meson
+, ninja
 , pkg-config
 , vala
 , gobject-introspection
-, libxml2
-, gtk-doc
-, docbook_xsl
-, docbook_xml_dtd_43
-, dbus
-, xvfb-run
 , glib
-, gtk3
+, libadwaita
+, gtk4
 , gnome
 }:
 
@@ -21,7 +16,7 @@ stdenv.mkDerivation rec {
   pname = "libpanel";
   version = "1.0.alpha";
 
-  outputs = [ "out" "dev" "devdoc" ];
+  outputs = [ "out" "dev" ];
   outputBin = "dev";
 
   src = fetchurl {
@@ -30,36 +25,22 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [
-    ninja
     meson
+    ninja
     pkg-config
     vala
     gobject-introspection
-    libxml2
-    gtk-doc
-    docbook_xsl
-    docbook_xml_dtd_43
-    dbus
-    xvfb-run
-    glib
   ];
 
   buildInputs = [
     glib
-    gtk3
+    libadwaita
+    gtk4
   ];
 
   mesonFlags = [
-    "-Denable_gtk_doc=true"
+    "-Dinstall-examples=true"
   ];
-
-  doCheck = true;
-
-  checkPhase = ''
-    xvfb-run -s '-screen 0 800x600x24' dbus-run-session \
-      --config-file=${dbus.daemon}/share/dbus-1/session.conf \
-      meson test --print-errorlogs
-  '';
 
   passthru = {
     updateScript = gnome.updateScript {
@@ -70,7 +51,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Dock/panel library for GTK 4";
     homepage = "https://gitlab.gnome.org/chergert/libpanel";
-    license = licenses.lgpl3Plus;
+    license = licenses.lgpl21Plus;
     maintainers = teams.gnome.members;
     platforms = platforms.unix;
   };
