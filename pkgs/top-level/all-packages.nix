74611c4dc4a66bb128b505b3664f0b0f121d4ded
Manuel Mendez <git@i.m.mmlb.dev>
2022-07-25 17:23:00 -0400

    tcat: init at 1.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36501,6 +36501,8 @@ with pkgs;
 
   tagref = callPackage ../tools/misc/tagref { };
 
+  tcat = callPackage ../tools/misc/tcat { };
+
   tellico = libsForQt5.callPackage ../applications/misc/tellico { };
 
   termpdfpy = python3Packages.callPackage ../applications/misc/termpdf.py {};
