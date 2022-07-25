930628f0228a1f69889eb39f4f3809259b7c7fbe
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2022-07-25 16:06:51 +0300

    midi-trigger: init at 0.0.4 (#182015)
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1247,6 +1247,8 @@ with pkgs;
 
   midimonster = callPackage ../tools/audio/midimonster { };
 
+  midi-trigger = callPackage ../applications/audio/midi-trigger { };
+
   mprocs = callPackage ../tools/misc/mprocs { };
 
   nominatim = callPackage ../servers/nominatim { };
