aadf0b4f03730127dce36dea1f45d90e78774105
Evan Stoll <evanjsx@gmail.com>
2022-07-26 10:17:32 -0400

    Use `find` to perform batch `install` invocations

--- pkgs/tools/games/ps3iso-utils/default.nix
+++ pkgs/tools/games/ps3iso-utils/default.nix
@@ -24,10 +24,7 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     mkdir -p $out/bin
-    install -m 0755 extractps3iso/extractps3iso $out/bin
-    install -m 0755 makeps3iso/makeps3iso $out/bin
-    install -m 0755 patchps3iso/patchps3iso $out/bin
-    install -m 0755 splitps3iso/splitps3iso $out/bin
+    find -name "*ps3iso" -type f -exec install -Dm 755 \{} $out/bin \;
   '';
 
 
