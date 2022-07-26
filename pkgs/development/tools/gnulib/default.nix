9eafe4319c247d3211c6c0783bb59003357f6064
Adam Joseph <adam@westernsemico.com>
2022-07-25 21:33:04 -0700

    gnulib: add gnulib-longdouble-redirect.patch
    This patch adds gnulib-longdouble-redirect.patch, which is a subset of
    gnulib commit 776af40e09b476a41073131a90022572f448c189.
    
    This patch must be applied to the copy of gnulib which is vendored
    within a few packages, when compiling for platforms that use
    longdouble-redirection (a glibc-specific feature).  Without it,
    compile failures mentioning "__LDBL_REDIR1_DECL" will occur.
    
    Currently there are two packages which need this patch to their
    vendored gnulib: gettext and gnused.  Both projects have merged a
    fixed version of gnulib but have not yet made a release with the fix.
    We can drop this patch as soon as both of those projects are
    version-bumped.

--- pkgs/development/tools/gnulib/default.nix
+++ pkgs/development/tools/gnulib/default.nix
@@ -26,6 +26,17 @@ stdenv.mkDerivation {
   # do not change headers to not update all vendored build files
   dontFixup = true;
 
+  passthru = {
+    # This patch is used by multiple other packages (currently:
+    # gnused, gettext) which contain vendored copies of gnulib.
+    # Without it, compilation will fail with error messages about
+    # "__LDBL_REDIR1_DECL" or similar on platforms with longdouble
+    # redirects (currently powerpc64).  Once all of those other
+    # packages make a release with a newer gnulib we can drop this
+    # patch.
+    longdouble-redirect-patch = ./gnulib-longdouble-redirect.patch;
+  };
+
   meta = with lib; {
     description = "Central location for code to be shared among GNU packages";
     homepage = "https://www.gnu.org/software/gnulib/";
