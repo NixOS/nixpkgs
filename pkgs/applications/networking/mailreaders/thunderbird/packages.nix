f92f7078b8b681f5a036d7d294c14bdb8665b53c
Vladimír Čunát <v@cunat.cz>
2022-07-26 10:56:03 +0200

    thunderbird: 102.0.2 -> 102.0.3
    https://www.thunderbird.net/en-US/thunderbird/102.0.3/releasenotes/
    (cherry picked from commit 7d84ef8ad9d248bf4dae17dcb35423fef3e2309f)

--- pkgs/applications/networking/mailreaders/thunderbird/packages.nix
+++ pkgs/applications/networking/mailreaders/thunderbird/packages.nix
@@ -38,13 +38,13 @@ rec {
   };
   thunderbird-102 = (buildMozillaMach rec {
     pname = "thunderbird";
-    version = "102.0.2";
+    version = "102.0.3";
     application = "comm/mail";
     applicationName = "Mozilla Thunderbird";
     binaryName = pname;
     src = fetchurl {
       url = "mirror://mozilla/thunderbird/releases/${version}/source/thunderbird-${version}.source.tar.xz";
-      sha512 = "c757543aff8b0acc48f1e2d5fb94024fa894a34f27275eb9c9193d8d2c6aee7643d8b762e7b511c2217130a93ffb0d5e7a405b2bfb15ea129b2a6f9ae9c7e9f2";
+      sha512 = "ac9f22935ef558890c95cf7fbbbe32a5bb1b7140acb10088ed0d037d1ca5c6e11695c131eb40844807003b77e83b1dd2d9008df420ec394fed5008d5c4c6c3cb";
     };
     extraPatches = [
       # The file to be patched is different from firefox's `no-buildconfig-ffx90.patch`.
