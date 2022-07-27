fc88905bac65bb3f6f0cce6f512578a47216f9be
kilianar <mail@kilianar.de>
2022-07-27 10:09:47 +0200

    logseq: 0.7.8 -> 0.7.9
    https://github.com/logseq/logseq/releases/tag/0.7.9

--- pkgs/applications/misc/logseq/default.nix
+++ pkgs/applications/misc/logseq/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "logseq";
-  version = "0.7.8";
+  version = "0.7.9";
 
   src = fetchurl {
     url = "https://github.com/logseq/logseq/releases/download/${version}/logseq-linux-x64-${version}.AppImage";
-    sha256 = "sha256-5NEx4OEISa7q0tp6v4AWK9eMn58dje7non653iJhipE=";
+    sha256 = "sha256-tca9jWMlBT+r1CFyGKe8xMA6O0T/tnbmU7uVk+3o87o=";
     name = "${pname}-${version}.AppImage";
   };
 
