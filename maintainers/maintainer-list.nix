80e96c8b69ad8cfd71d361a8f169412114ba501b
YisuiDenghua <bileiner@gmail.com>
2022-07-27 11:28:13 +0800

    maintainers: add yisuidenghua

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14799,4 +14799,10 @@
     github = "nikstur";
     githubId = 61635709;
   };
+  yisuidenghua = {
+    email = "bileiner@gmail.com";
+    name = "Milena Yisui";
+    github = "yisuidenghua";
+    githubId = 102890144;
+  };
 }
