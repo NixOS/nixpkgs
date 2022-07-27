b446b0451486e31cf05e2e9a363ec2c7eb1af1d8
Sebastián Mancilla <smancill@smancill.dev>
2022-07-26 22:12:03 -0400

    opencl-clhpp: 2.0.15 -> 2.0.17
    Also fix license.

--- pkgs/development/libraries/opencl-clhpp/default.nix
+++ pkgs/development/libraries/opencl-clhpp/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "opencl-clhpp";
-  version = "2.0.15";
+  version = "2.0.17";
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "OpenCL-CLHPP";
-    rev = "v${version}";
-    sha256 = "sha256-A12GdevbMaO2QkGAk3VsXzwcDkF+6dEapse2xfdqzPM=";
+    rev = "v2022.05.18"; # XXX: tag uses different name than current release
+    hash = "sha256-u4hbcBw3KEPo0+RC7lgPlExS5ZyDubxiLXN9ya0Ork8=";
   };
 
   nativeBuildInputs = [ cmake python3 ];
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "OpenCL Host API C++ bindings";
     homepage = "http://github.khronos.org/OpenCL-CLHPP/";
-    license = licenses.mit;
+    license = licenses.asl20;
     platforms = platforms.unix;
   };
 }
