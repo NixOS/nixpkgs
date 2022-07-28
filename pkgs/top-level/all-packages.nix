eb1f9ccfb44a0c34ea3cd80da9397ab803d657e5
Ben Darwin <bcdarwin@gmail.com>
2022-07-28 14:32:15 -0400

    tcping-go: init at unstable-2022-05-28

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29273,6 +29273,8 @@ with pkgs;
 
   taxi-cli = with python3Packages; toPythonApplication taxi;
 
+  tcping-go = callPackage ../applications/networking/tcping-go { };
+
   librep = callPackage ../development/libraries/librep { };
 
   rep-gtk = callPackage ../development/libraries/rep-gtk { };
