5bda48a78ee1814ce577cc59c23c13fa6fca1b1c
Tom Wieczorek <tom@bibbu.net>
2022-07-28 16:47:11 +0200

    gnome-commander: init at 1.14.3
    GNOME Commander is yet another two-pane file manager targeting the GNOME
    desktop. In addition to performing the basic filemanager functions the
    program is also an FTP-client and it can browse SMB-networks.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6759,6 +6759,8 @@ with pkgs;
 
   gnome-builder = callPackage ../applications/editors/gnome-builder { };
 
+  gnome-commander = callPackage ../applications/file-managers/gnome-commander { };
+
   gnome-desktop = callPackage ../development/libraries/gnome-desktop { };
 
   gnome-feeds = callPackage ../applications/networking/feedreaders/gnome-feeds {};
