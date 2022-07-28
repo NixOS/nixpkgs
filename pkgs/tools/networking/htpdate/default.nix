71d5dd7bbeee41480058dcf2b18f596c56209f1a
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-28 14:41:41 +0000

    htpdate: 1.3.3 -> 1.3.5

--- pkgs/tools/networking/htpdate/default.nix
+++ pkgs/tools/networking/htpdate/default.nix
@@ -1,14 +1,14 @@
 { stdenv, lib, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
-  version = "1.3.3";
+  version = "1.3.5";
   pname = "htpdate";
 
   src = fetchFromGitHub {
     owner = "twekkel";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-/xZxwEui8V5kyfGsmwRRkiyhj7lcJQaTmOjBihvdWg8=";
+    sha256 = "sha256-L3CKBgGk9R8qJFWOS98Tm1j/s/5t6+/Vt2EcZ+or0Ng=";
   };
 
   makeFlags = [
