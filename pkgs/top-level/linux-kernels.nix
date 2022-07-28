8687bf7e9c2aa523d11fccd3a731a452a34a9315
Florian Klink <flokli@flokli.de>
2022-07-28 17:10:47 +0700

    linuxPackages*.xmm7360-pci: drop
    Support for 7360 got enabled via iosm in mainline kernel:
    
    https://github.com/torvalds/linux/commit/1f52d7b622854b8bd7a1be3de095ca2e1f77098e
    
    There's few reason to still ship this out-of-tree kernel module, which
    broke whenever the laptop was suspended anyways.

--- pkgs/top-level/linux-kernels.nix
+++ pkgs/top-level/linux-kernels.nix
@@ -483,7 +483,7 @@ in {
 
     x86_energy_perf_policy = callPackage ../os-specific/linux/x86_energy_perf_policy { };
 
-    xmm7360-pci = callPackage ../os-specific/linux/xmm7360-pci { };
+    xmm7360-pci = throw "Support for 7360 was added to the iosm mainline kernel module";
 
     xone = if lib.versionAtLeast kernel.version "5.4" then callPackage ../os-specific/linux/xone { } else null;
 
