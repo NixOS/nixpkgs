fb7a06e635aa63a9e171290ce0a1200c897fa461
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-28 06:23:35 +0000

    fastly: 3.1.1 -> 3.2.1

--- pkgs/misc/fastly/default.nix
+++ pkgs/misc/fastly/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "fastly";
-  version = "3.1.1";
+  version = "3.2.1";
 
   src = fetchFromGitHub {
     owner = "fastly";
     repo = "cli";
     rev = "v${version}";
-    sha256 = "sha256-mUWGvFyei5Ep3xRDVVZoa6tMiqfV89hn82/Uai+Gh8Y=";
+    sha256 = "sha256-OK1xhsn30JE/IeKnOLE+0AmulEAeFM0Z7TyRqd6hTGY=";
     # The git commit is part of the `fastly version` original output;
     # leave that output the same in nixpkgs. Use the `.git` directory
     # to retrieve the commit SHA, and remove the directory afterwards,
