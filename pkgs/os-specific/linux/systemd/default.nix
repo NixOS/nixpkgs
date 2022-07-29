4a487f4c337a4b2a7a0e9399c08d2ddc1f0651e7
Alyssa Ross <hi@alyssa.is>
2022-07-29 16:06:16 +0000

    pkgsMusl.systemd: fix build with latest oe patches

--- pkgs/os-specific/linux/systemd/default.nix
+++ pkgs/os-specific/linux/systemd/default.nix
@@ -173,30 +173,24 @@ stdenv.mkDerivation {
   ] ++ lib.optional stdenv.hostPlatform.isMusl (
     let
       oe-core = fetchzip {
-        url = "https://git.openembedded.org/openembedded-core/snapshot/openembedded-core-7e35a575ef09a85e625a81e0b4d80b020e3e3a92.tar.bz2";
-        sha256 = "0dvz4685nk0y7nnq3sr2q8ab3wfx0bi8ilwcgn0h6kagwcnav2n8";
+        url = "https://git.openembedded.org/openembedded-core/snapshot/openembedded-core-86a33f98a7c0d6f2c2b51d02ba9e01b63062cf98.tar.bz2";
+        sha256 = "081j01sw21hl405l7g9z4bavvq0q0k4g80365677m0ykhiqlx3am";
       };
       musl-patches = oe-core + "/meta/recipes-core/systemd/systemd";
     in
     [
-      (musl-patches + "/0002-don-t-use-glibc-specific-qsort_r.patch")
-      (musl-patches + "/0003-missing_type.h-add-__compare_fn_t-and-comparison_fn_.patch")
+      (musl-patches + "/0003-missing_type.h-add-comparison_fn_t.patch")
       (musl-patches + "/0004-add-fallback-parse_printf_format-implementation.patch")
       (musl-patches + "/0005-src-basic-missing.h-check-for-missing-strndupa.patch")
       (musl-patches + "/0007-don-t-fail-if-GLOB_BRACE-and-GLOB_ALTDIRFUNC-is-not-.patch")
       (musl-patches + "/0008-add-missing-FTW_-macros-for-musl.patch")
-      (musl-patches + "/0009-fix-missing-of-__register_atfork-for-non-glibc-build.patch")
       (musl-patches + "/0010-Use-uintmax_t-for-handling-rlim_t.patch")
       (musl-patches + "/0011-test-sizeof.c-Disable-tests-for-missing-typedefs-in-.patch")
       (musl-patches + "/0012-don-t-pass-AT_SYMLINK_NOFOLLOW-flag-to-faccessat.patch")
       (musl-patches + "/0013-Define-glibc-compatible-basename-for-non-glibc-syste.patch")
       (musl-patches + "/0014-Do-not-disable-buffering-when-writing-to-oom_score_a.patch")
       (musl-patches + "/0015-distinguish-XSI-compliant-strerror_r-from-GNU-specif.patch")
-      (musl-patches + "/0016-Hide-__start_BUS_ERROR_MAP-and-__stop_BUS_ERROR_MAP.patch")
-      (musl-patches + "/0017-missing_type.h-add-__compar_d_fn_t-definition.patch")
       (musl-patches + "/0018-avoid-redefinition-of-prctl_mm_map-structure.patch")
-      (musl-patches + "/0019-Handle-missing-LOCK_EX.patch")
-      (musl-patches + "/0021-test-json.c-define-M_PIl.patch")
       (musl-patches + "/0022-do-not-disable-buffer-in-writing-files.patch")
       (musl-patches + "/0025-Handle-__cpu_mask-usage.patch")
       (musl-patches + "/0026-Handle-missing-gshadow.patch")
