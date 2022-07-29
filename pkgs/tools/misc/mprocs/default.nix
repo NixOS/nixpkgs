512807154e2878b0c307ccf7a9b2cd16b24b5976
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-29 01:12:19 +0000

    mprocs: 0.4.0 -> 0.6.1

--- pkgs/tools/misc/mprocs/default.nix
+++ pkgs/tools/misc/mprocs/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "mprocs";
-  version = "0.4.0";
+  version = "0.6.1";
 
   src = fetchFromGitHub {
     owner = "pvolok";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-IzaXcEm4eUpLWsn59ZSiIJ0mCegLhBiA3ONKI1Djemk=";
+    sha256 = "sha256-Ob1d8ZbWSArDhFuIvy+VlPyhkRJ7moMtIvn/odEoP2Y=";
   };
 
-  cargoSha256 = "sha256-nTFCFmmS3IIm+D2PjvDxUKQGTn2h0ajXtxLuosa9rRY=";
+  cargoSha256 = "sha256-q3elc20w0RFCFxZchZyves/5dGY9O9+TnXUw7MIfSOw=";
 
   meta = with lib; {
     description = "A TUI tool to run multiple commands in parallel and show the output of each command separately";
