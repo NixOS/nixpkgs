60cf5957c5a13bcba67eefbd00201ae3b516118c
Luc Perkins <lucperkins@gmail.com>
2022-07-29 13:15:47 +0200

    materialize: remove designation materialize: remove designation as broken

--- pkgs/servers/sql/materialize/default.nix
+++ pkgs/servers/sql/materialize/default.nix
@@ -91,7 +91,6 @@ rustPlatform.buildRustPackage rec {
     homepage    = "https://materialize.com";
     description = "A streaming SQL materialized view engine for real-time applications";
     license     = licenses.bsl11;
-    platforms   = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" ];
     maintainers = [ maintainers.petrosagg ];
   };
 }
