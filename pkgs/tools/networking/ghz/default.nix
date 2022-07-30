46eb4c4e3a7735793981445cc4c0783447507600
Ross Light <ross@zombiezen.com>
2022-07-29 19:35:12 -0700

    Use `with lib;`

--- pkgs/tools/networking/ghz/default.nix
+++ pkgs/tools/networking/ghz/default.nix
@@ -10,18 +10,18 @@ buildGoModule rec {
     rev = "v${version}";
     sha256 = "sha256-5l2PeN+VxTaORAkmAfI9TCGd4W6y8BFs/eY4T9nYJuc=";
   };
-  
+
   proxyVendor = true;
   vendorSha256 = "sha256-R1OB4jTmyyS2O91vqSNjmSA19KutHzPLcLQ7R7tCsIA=";
 
   subPackages = ["cmd/ghz" "cmd/ghz-web"];
 
-ldflags = [ "-s" "-w" ];
+  ldflags = [ "-s" "-w" ];
 
-  meta = {
+  meta = with lib; {
     description = "Simple gRPC benchmarking and load testing tool";
     homepage = "https://ghz.sh";
-    license = lib.licenses.asl20;
-    maintainers = [ lib.maintainers.zombiezen ];
+    license = licenses.asl20;
+    maintainers = [ maintainers.zombiezen ];
   };
 }
