720249407bac757012d9c2e6280f33afc8b76686
Robert Scott <code@humanleg.org.uk>
2022-07-30 19:23:16 +0100

    python3Packages.mistune_2_0: 2.0.2 -> 2.0.4
    (cherry picked from commit 0a9621088aecdc0fc6c7a0afaacb8cf2bb69d108)

--- pkgs/development/python-modules/mistune/default.nix
+++ pkgs/development/python-modules/mistune/default.nix
@@ -4,8 +4,8 @@ self: rec {
     sha256 = "59a3429db53c50b5c6bcc8a07f8848cb00d7dc8bdb431a4ab41920d201d4756e";
   };
   mistune_2_0 = self.callPackage ./common.nix {
-    version = "2.0.2";
-    sha256 = "sha256-b8iMPLSduosWaHtBcl5mHPhXhMEuiXSim50zbdWWw6E=";
+    version = "2.0.4";
+    sha256 = "sha256-nuCmYFPiJnq6dyxx4GiR+o8a9tSwHV6E4me0Vw1NmAg=";
     format = "pyproject";
   };
   mistune = mistune_0_8;
