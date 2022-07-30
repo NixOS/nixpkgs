bd9d8791cb0bc2397c01115314307f4f641987b3
Arnout Engelen <arnout@bzzt.net>
2022-07-30 14:15:56 +0200

    seaweedfs: drop myself as maintainer
    Since I'm not using it actively and there is someone else listed

--- pkgs/applications/networking/seaweedfs/default.nix
+++ pkgs/applications/networking/seaweedfs/default.nix
@@ -26,7 +26,7 @@ buildGoModule rec {
   meta = with lib; {
     description = "Simple and highly scalable distributed file system";
     homepage = "https://github.com/chrislusf/seaweedfs";
-    maintainers = with maintainers; [ cmacrae raboof ];
+    maintainers = with maintainers; [ cmacrae ];
     mainProgram = "weed";
     license = licenses.asl20;
   };
