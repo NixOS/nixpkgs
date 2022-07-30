d88d65b1756d684d6f96304b3c71bc5e95fcb023
Mario Rodas <marsam@users.noreply.github.com>
2022-07-30 04:20:00 +0000

    flow: 0.176.3 -> 0.183.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15942,7 +15942,6 @@ with pkgs;
 
   flow = callPackage ../development/tools/analysis/flow {
     inherit (darwin.apple_sdk.frameworks) CoreServices;
-    ocamlPackages = ocaml-ng.ocamlPackages_4_12;
   };
 
   fly = callPackage ../development/tools/continuous-integration/fly { };
