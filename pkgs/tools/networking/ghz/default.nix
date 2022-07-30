eeec043db4502f980c28091f0295b1d7a59f3ae7
Ross Light <ross@zombiezen.com>
2022-07-29 19:32:15 -0700

    Add ldflags
    Co-authored-by: Azat Bahawi <azat+github@bahawi.net>

--- pkgs/tools/networking/ghz/default.nix
+++ pkgs/tools/networking/ghz/default.nix
@@ -10,10 +10,13 @@ buildGoModule rec {
     rev = "v${version}";
     sha256 = "sha256-5l2PeN+VxTaORAkmAfI9TCGd4W6y8BFs/eY4T9nYJuc=";
   };
+  
   proxyVendor = true;
   vendorSha256 = "sha256-R1OB4jTmyyS2O91vqSNjmSA19KutHzPLcLQ7R7tCsIA=";
 
   subPackages = ["cmd/ghz" "cmd/ghz-web"];
+
+ldflags = [ "-s" "-w" ];
 
   meta = {
     description = "Simple gRPC benchmarking and load testing tool";
