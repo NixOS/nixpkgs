700a843b44dbbe4ff89242d25117225f5f12f121
Netali <me@netali.de>
2022-07-31 23:40:48 +0200

    nixos/mysql-auth: add VM-Test
    (cherry picked from commit e23ace62686fb974353e86299c7003e089f323dd)

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -41,6 +41,7 @@ in
   apparmor = handleTest ./apparmor.nix {};
   atd = handleTest ./atd.nix {};
   atop = handleTest ./atop.nix {};
+  auth-mysql = handleTest ./auth-mysql.nix {};
   avahi = handleTest ./avahi.nix {};
   avahi-with-resolved = handleTest ./avahi.nix { networkd = true; };
   babeld = handleTest ./babeld.nix {};
