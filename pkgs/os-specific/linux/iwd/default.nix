5dfbe0ac192e541685d9cff0381729a1c9dc9ef5
Maxine Aubrey <maxeaubrey@gmail.com>
2022-07-31 13:45:03 +0200

    iwd: 1.28 -> 1.29

--- pkgs/os-specific/linux/iwd/default.nix
+++ pkgs/os-specific/linux/iwd/default.nix
@@ -12,12 +12,12 @@
 
 stdenv.mkDerivation rec {
   pname = "iwd";
-  version = "1.28";
+  version = "1.29";
 
   src = fetchgit {
     url = "https://git.kernel.org/pub/scm/network/wireless/iwd.git";
     rev = version;
-    sha256 = "sha256-UAhgmXTbCgxja8nniexr6+jkzHIOMn9k1Cp8oMuskk0=";
+    sha256 = "sha256-W2MOK6aIa1whkj13OeuibNjL/2LWt7TO8h4JeoUrZnQ=";
   };
 
   outputs = [ "out" "man" "doc" ]
