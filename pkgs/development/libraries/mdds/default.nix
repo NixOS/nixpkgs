3f92e5944a02394b5f32a6882f501981b6bf8df9
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-31 21:57:20 +0000

    mdds: 2.0.2 -> 2.0.3

--- pkgs/development/libraries/mdds/default.nix
+++ pkgs/development/libraries/mdds/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "mdds";
-  version = "2.0.2";
+  version = "2.0.3";
 
   src = fetchurl {
     url = "https://kohei.us/files/${pname}/src/${pname}-${version}.tar.bz2";
-    sha256 = "sha256-EyEfLy44fvO3TXOh3O5Soa1c4G34+OZkdnnfknijEWo=";
+    sha256 = "sha256-l3H+QuEzRDwTyhhyU3Y+F8i8lqGgKuyeHoiTNn/6nOU=";
   };
 
   postInstall = ''
