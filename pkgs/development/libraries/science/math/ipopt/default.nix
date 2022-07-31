8c53477f36850279da7be9884684c9b91c925454
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-31 13:59:21 +0000

    ipopt: 3.14.5 -> 3.14.9

--- pkgs/development/libraries/science/math/ipopt/default.nix
+++ pkgs/development/libraries/science/math/ipopt/default.nix
@@ -12,13 +12,13 @@ assert (!blas.isILP64) && (!lapack.isILP64);
 
 stdenv.mkDerivation rec {
   pname = "ipopt";
-  version = "3.14.5";
+  version = "3.14.9";
 
   src = fetchFromGitHub {
     owner = "coin-or";
     repo = "Ipopt";
     rev = "releases/${version}";
-    sha256 = "sha256-eqOacZGuuGAjWMs2H6RntQ+WJmGTi+EqydHUQXEpY54=";
+    sha256 = "sha256-mlRr1BjkWiJZSgpxQIc0k1tXGewJkHwxf6xe0edUHaY=";
   };
 
   CXXDEFS = [ "-DHAVE_RAND" "-DHAVE_CSTRING" "-DHAVE_CSTDIO" ];
