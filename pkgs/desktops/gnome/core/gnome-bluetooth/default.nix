069698c4291a57186baacf8e724119c98e540a63
Artturin <Artturin@artturin.com>
2022-07-31 04:23:08 +0300

    gnome.gnome-bluetooth: fix cross

--- pkgs/desktops/gnome/core/gnome-bluetooth/default.nix
+++ pkgs/desktops/gnome/core/gnome-bluetooth/default.nix
@@ -3,6 +3,7 @@
 , fetchurl
 , gnome
 , meson
+, mesonEmulatorHook
 , ninja
 , pkg-config
 , gtk4
@@ -49,9 +50,12 @@ stdenv.mkDerivation rec {
     docbook-xsl-nons
     docbook_xml_dtd_43
     python3
+  ] ++ lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+    mesonEmulatorHook
   ];
 
   buildInputs = [
+    gobject-introspection
     glib
     gtk4
     libadwaita
