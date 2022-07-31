a071ead1ae8fe5698315e281d6a3a18259a09a04
R. Ryantm <ryantm-bot@ryantm.com>
2022-07-31 03:45:20 +0000

    trillian: 1.4.0 -> 1.4.2

--- pkgs/tools/misc/trillian/default.nix
+++ pkgs/tools/misc/trillian/default.nix
@@ -5,14 +5,14 @@
 
 buildGoModule rec {
   pname = "trillian";
-  version = "1.4.0";
-  vendorSha256 = "sha256-n5PD3CGgNGrqSYLJS+3joGFNue2fF/tdimC6CtO15yU=";
+  version = "1.4.2";
+  vendorSha256 = "sha256-/5IBb/cYY6o49WmG7LmLZ4AwOjZ54Uy9bALb1pn0qGo=";
 
   src = fetchFromGitHub {
     owner = "google";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-c7Sii6GMWZOeD46OwdkXU/wt9zY+EyPSnahYPJQKJcA=";
+    sha256 = "sha256-7R/E9BXPla90Q7LEOtLBMz2LKok7gsAnXrfJ1E8urf4=";
   };
 
   subPackages = [
