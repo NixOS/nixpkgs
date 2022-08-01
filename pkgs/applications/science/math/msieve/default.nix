243ed9798728f2a0bc1affb36a5e3317503f0b30
Emily Trau <me@angus.ws>
2022-08-01 21:12:31 +1000

    msieve: add lib outputs and fix i686-linux

--- pkgs/applications/science/math/msieve/default.nix
+++ pkgs/applications/science/math/msieve/default.nix
@@ -3,11 +3,17 @@
 stdenv.mkDerivation rec {
   pname = "msieve";
   version = "1.53";
+  outputs = [ "out" "lib" "dev" ];
 
   src = fetchurl {
     url = "mirror://sourceforge/msieve/msieve/Msieve%20v${version}/msieve${lib.replaceStrings ["."] [""] version}_src.tar.gz";
     sha256 = "1d1vv7j4rh3nnxsmvafi73qy7lw7n3akjlm5pjl3m936yapvmz65";
   };
+
+  patches = [
+    # Disable i686 SIMD optimisations that are unsupported by nix
+    ./disable-i686-assembly.patch
+  ];
 
   buildInputs = [ zlib gmp ecm ];
 
@@ -17,15 +23,20 @@ stdenv.mkDerivation rec {
   makeFlags = [ "CC=${stdenv.cc.targetPrefix}cc" "all" ];
 
   installPhase = ''
-    mkdir -p $out/bin/
-    cp msieve $out/bin/
+    runHook preInstall
+
+    install -D msieve $out/bin/msieve
+    install -D libmsieve.a $lib/lib/libmsieve.a
+    install -D include/msieve.h $dev/include/msieve.h
+
+    runHook postInstall
   '';
 
-  meta = {
+  meta = with lib; {
     description = "A C library implementing a suite of algorithms to factor large integers";
-    license = lib.licenses.publicDomain;
+    license = licenses.publicDomain;
     homepage = "http://msieve.sourceforge.net/";
-    maintainers = [ lib.maintainers.roconnor ];
-    platforms = [ "x86_64-linux" ] ++ lib.platforms.darwin;
+    maintainers = with maintainers; [ roconnor emilytrau ];
+    platforms = platforms.unix;
   };
 }
