22530442d2d4d5372bf709ab9cda2a100f6faec7
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-01 05:45:15 +0000

    cinnamon.pix: 2.8.4 -> 2.8.7

--- pkgs/desktops/cinnamon/pix/default.nix
+++ pkgs/desktops/cinnamon/pix/default.nix
@@ -29,13 +29,13 @@
 
 stdenv.mkDerivation rec {
   pname = "pix";
-  version = "2.8.4";
+  version = "2.8.7";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    sha256 = "sha256-yB8nLGvJZOViD+i4IkKN0yCDl5wRCvEbjzPDs7ZRMNA=";
+    sha256 = "sha256-aID0jxOMFegkcAflTY0VevZyN1HFivkTFi3has8MOY0=";
   };
 
   nativeBuildInputs = [
