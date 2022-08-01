1216b9f6a18a1b4a58bb0ed4fff2e7060cb7d6a4
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-01 03:55:55 +0000

    ocamlPackages.yojson: 1.7.0 -> 2.0.1

--- pkgs/development/ocaml-modules/yojson/default.nix
+++ pkgs/development/ocaml-modules/yojson/default.nix
@@ -2,12 +2,12 @@
 
 buildDunePackage rec {
   pname = "yojson";
-  version = "1.7.0";
+  version = "2.0.1";
   useDune2 = true;
 
   src = fetchurl {
     url = "https://github.com/ocaml-community/yojson/releases/download/${version}/yojson-${version}.tbz";
-    sha256 = "1iich6323npvvs8r50lkr4pxxqm9mf6w67cnid7jg1j1g5gwcvv5";
+    sha256 = "sha256-i8i8cipKlGVqWDlPF/n4pAYmc0Orh9dyAtuTBYSgyDY=";
   };
 
   nativeBuildInputs = [ cppo ];
