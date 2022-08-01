6f07109fbfc8bd72ecf358364ecb66c3d9a80c89
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-01 02:22:39 +0000

    ocamlPackages.fmt: 0.8.9 -> 0.9.0

--- pkgs/development/ocaml-modules/fmt/default.nix
+++ pkgs/development/ocaml-modules/fmt/default.nix
@@ -5,12 +5,12 @@ then throw "fmt is not available for OCaml ${ocaml.version}"
 else
 
 stdenv.mkDerivation rec {
-  version = "0.8.9";
+  version = "0.9.0";
   pname = "ocaml${ocaml.version}-fmt";
 
   src = fetchurl {
     url = "https://erratique.ch/software/fmt/releases/fmt-${version}.tbz";
-    sha256 = "0gkkkj4x678vxdda4xaw2dd44qjacavsvn5nx8gydfwah6pjbkxk";
+    sha256 = "sha256-8fsggFoi3XWhN9cnBKNw53ic9r32OUjmgX0cImwUEmE=";
   };
 
   nativeBuildInputs = [ ocaml findlib ocamlbuild topkg ];
