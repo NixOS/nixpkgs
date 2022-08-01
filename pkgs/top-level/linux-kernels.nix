NixOS/nixpkgs

commit 8687bf7e9c2aa523d11fccd3a731a452a34a9315

linuxPackages*.xmm7360-pci: drop
Support for 7360 got enabled via iosm in mainline kernel:

https://github.com/torvalds/linux/commit/1f52d7b622854b8bd7a1be3de095ca2e1f77098e

There's few reason to still ship this out-of-tree kernel module, which
broke whenever the laptop was suspended anyways.

pkgs/top-level/linux-kernels.nix
@@ -483,7 +483,7 @@ in {
 
     x86_energy_perf_policy = callPackage ../os-specific/linux/x86_energy_perf_policy { };
 
-    xmm7360-pci = callPackage ../os-specific/linux/xmm7360-pci { };
+    xmm7360-pci = throw "Support for 7360 was added to the iosm mainline kernel module";
 
     xone = if lib.versionAtLeast kernel.version "5.4" then callPackage ../os-specific/linux/xone { } else null;
 

pkgs/os-specific/linux/xmm7360-pci/default.nix
@@ -1,31 +1,0 @@
-{ lib, stdenv, fetchFromGitHub, fetchpatch, kernel, perl, bc, breakpointHook }:
-
-stdenv.mkDerivation rec {
-  pname = "xmm7360-pci";
-  version = "unstable-2021-07-19";
-
-  src = fetchFromGitHub {
-    owner = "xmm7360";
-    repo = "xmm7360-pci";
-    rev = "7086b80bb609180b1b89fb478751e5e8414ab64f";
-    sha256 = "1wdb0phqg9rj9g9ycqdya0m7lx24kzjlh25yw0ifp898ddxrrr0c";
-  };
-
-  makeFlags = kernel.makeFlags ++ [
-    "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
-    "INSTALL_MOD_PATH=${placeholder "out"}"
-  ];
-
-  nativeBuildInputs = kernel.moduleBuildDependencies;
-  installFlags = [ "DEPMOD=true" ];
-
-  meta = with lib; {
-    homepage = "https://github.com/xmm7360/xmm7360-pci";
-    description = "PCI driver for Fibocom L850-GL modem based on Intel XMM7360 modem";
-    downloadPage = "https://github.com/xmm7360/xmm7360-pci";
-    license = licenses.isc;
-    maintainers = with maintainers; [ flokli hexa ];
-    platforms = platforms.linux;
-    broken = kernel.kernelOlder "4.10" || kernel.kernelAtLeast "5.14";
-  };
-}

commit 017b80f490207e07bd09240fa0c667c5816c5856

linuxPackages.xmm7360-pci: respect allowAliases
Fixes: 8687bf7e9c2 ("linuxPackages*.xmm7360-pci: drop")

pkgs/top-level/linux-kernels.nix
@@ -483,8 +483,6 @@ in {
 
     x86_energy_perf_policy = callPackage ../os-specific/linux/x86_energy_perf_policy { };
 
-    xmm7360-pci = throw "Support for the XMM7360 WWAN card was added to the iosm kmod in mainline kernel version 5.18";
-
     xone = if lib.versionAtLeast kernel.version "5.4" then callPackage ../os-specific/linux/xone { } else null;
 
     xpadneo = callPackage ../os-specific/linux/xpadneo { };
@@ -507,6 +505,7 @@ in {
 
   } // lib.optionalAttrs config.allowAliases {
     ati_drivers_x11 = throw "ati drivers are no longer supported by any kernel >=4.1"; # added 2021-05-18;
+    xmm7360-pci = throw "Support for the XMM7360 WWAN card was added to the iosm kmod in mainline kernel version 5.18";
   });
 
   hardenedPackagesFor = kernel: overrides: packagesFor (hardenedKernelFor kernel overrides);