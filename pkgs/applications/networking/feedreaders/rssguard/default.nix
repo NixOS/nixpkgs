780e0d69e7e840217548b43792e40c11c5ffa879
kilianar <mail@kilianar.de>
2022-08-01 11:14:55 +0200

    rssguard: 4.1.2 -> 4.2.3
    - https://github.com/martinrotter/rssguard/releases/tag/4.2.0
    - https://github.com/martinrotter/rssguard/releases/tag/4.2.1
    - https://github.com/martinrotter/rssguard/releases/tag/4.2.2
    - https://github.com/martinrotter/rssguard/releases/tag/4.2.3
    
    Upstream now uses cmake instead of qmake.

--- pkgs/applications/networking/feedreaders/rssguard/default.nix
+++ pkgs/applications/networking/feedreaders/rssguard/default.nix
@@ -1,18 +1,18 @@
-{ lib, stdenv, fetchFromGitHub, qmake, qtwebengine, qttools, wrapGAppsHook, wrapQtAppsHook }:
+{ lib, stdenv, fetchFromGitHub, cmake, qtwebengine, qttools, wrapGAppsHook, wrapQtAppsHook }:
 
 stdenv.mkDerivation rec {
   pname = "rssguard";
-  version = "4.1.2";
+  version = "4.2.3";
 
   src = fetchFromGitHub {
     owner = "martinrotter";
     repo = pname;
     rev = version;
-    sha256 = "sha256-aG7Wkn2CHe7Dumskd0+DMja95lzvBWnFXACSqfU7Ow0=";
+    sha256 = "sha256-s5SrQgu1PHNuuLeVKhLdFdLkxsPMFXGo09NBPe6/sv0=";
   };
 
   buildInputs =  [ qtwebengine qttools ];
-  nativeBuildInputs = [ qmake wrapGAppsHook wrapQtAppsHook ];
+  nativeBuildInputs = [ cmake wrapGAppsHook wrapQtAppsHook ];
   qmakeFlags = [ "CONFIG+=release" ];
 
   meta = with lib; {
