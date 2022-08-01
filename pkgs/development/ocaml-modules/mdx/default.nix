d29c029c185350784b2e19be9934e0d246313d70
bezmuth <benkel97@protonmail.com>
2022-08-01 01:39:33 +0100

    ocamlPackages.mds: 2.1.0 -> unstable-2022-02-28 Adds support for cmdliner_1_1

--- pkgs/development/ocaml-modules/mdx/default.nix
+++ pkgs/development/ocaml-modules/mdx/default.nix
@@ -1,22 +1,32 @@
-{ lib, fetchurl, buildDunePackage, ocaml
+{ lib, fetchFromGitHub, buildDunePackage, ocaml
 , alcotest
-, astring, cmdliner, cppo, fmt, logs, ocaml-version, odoc-parser, ocaml_lwt, re, result, csexp
+, astring, cmdliner_1_1, cppo, fmt, logs, ocaml-version, odoc-parser, ocaml_lwt, re, result, csexp
 , pandoc}:
 
 buildDunePackage rec {
   pname = "mdx";
-  version = "2.1.0";
+  version = "unstable-2022-02-28";
   useDune2 = true;
 
   minimalOCamlVersion = "4.08";
 
-  src = fetchurl {
-    url = "https://github.com/realworldocaml/mdx/releases/download/${version}/mdx-${version}.tbz";
-    sha256 = "sha256-ol1zy8LODDYdcnv/jByE0pnqJ5ujQuMALq3v9y7td/o=";
-  };
+  src = fetchFromGitHub {
+      owner = "realworldocaml";
+      repo = "mdx";
+      rev = "cf176d7dc258b40b2e2491aa1df7e8a16ac7452e";
+      sha256 = "sha256-SUvcF1JaeD2aCCZvMkUopo/W/meQtYoystht+TYXn9c=";
+    };
+
+
+  # this can be switched back to when the cmdliner_1_1 patch is stabilised
+  #src = fetchurl {
+  #  url = "https://github.com/realworldocaml/mdx/releases/download/${version}/mdx-${version}.tbz";
+  #  sha256 = "sha256-SUvcF1JaeD2aCCZvMkUopo/W/meQtYoystht+TYXn9c=";
+  #}
+
 
   nativeBuildInputs = [ cppo ];
-  buildInputs = [ cmdliner ];
+  buildInputs = [ cmdliner_1_1 ];
   propagatedBuildInputs = [ astring fmt logs result csexp ocaml-version odoc-parser re ];
   checkInputs = [ alcotest ocaml_lwt pandoc ];
 
