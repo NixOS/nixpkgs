364b4f09bbf3c89872a76aa63fe7ec66417a0639
Tom Houlé <tom@tomhoule.com>
2022-08-02 13:32:56 +0200

    prisma-engines: add tomhoule maintainer

--- pkgs/development/tools/database/prisma-engines/default.nix
+++ pkgs/development/tools/database/prisma-engines/default.nix
@@ -59,7 +59,7 @@ rustPlatform.buildRustPackage rec {
     homepage = "https://www.prisma.io/";
     license = licenses.asl20;
     platforms = platforms.unix;
-    maintainers = with maintainers; [ pamplemousse pimeys superherointj ];
+    maintainers = with maintainers; [ pamplemousse pimeys superherointj tomhoule ];
   };
 }
 
