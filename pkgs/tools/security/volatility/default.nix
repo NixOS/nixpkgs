8ee9a2c60a2aa9723d13ca738f9626b1938b10d2
Emily Trau <me@angus.ws>
2022-08-02 15:04:18 +1000

    volatility: 2.6.1 -> unstable-2020-12-12

--- pkgs/tools/security/volatility/default.nix
+++ pkgs/tools/security/volatility/default.nix
@@ -2,23 +2,32 @@
 
 python2Packages.buildPythonApplication rec {
   pname = "volatility";
-  version = "2.6.1";
+  version = "unstable-2020-12-12";
 
   src = fetchFromGitHub {
     owner = "volatilityfoundation";
     repo = pname;
-    rev = version;
-    sha256 = "1v92allp3cv3akk71kljcwxr27h1k067dsq7j9h8jnlwk9jxh6rf";
+    rev = "a438e768194a9e05eb4d9ee9338b881c0fa25937";
+    sha256 = "U/iWPztn47/Tcz/etVduG7V/Nu5Z1jb+i0mutaKr9sM=";
   };
 
   doCheck = false;
+  pythonImportsCheck = [ "volatility" ];
 
-  propagatedBuildInputs = with python2Packages; [ pycrypto distorm3 pillow ];
+  propagatedBuildInputs = with python2Packages; [
+    pycrypto
+    distorm3
+    yara-python
+    openpyxl
+    # pillow is needed for the screenshot plugin but is marked insecure
+    # pillow
+  ];
 
   meta = with lib; {
     homepage = "https://www.volatilityfoundation.org/";
     description = "Advanced memory forensics framework";
-    maintainers = with maintainers; [ bosu ];
+    mainProgram = "vol.py";
+    maintainers = with maintainers; [ bosu emilytrau ];
     license = lib.licenses.gpl2Plus;
   };
 }
