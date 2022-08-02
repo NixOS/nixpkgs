1ee503710c5d5b5b1933fde0114d4be3df1dcdea
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-02 08:12:20 +0000

    cinnamon.xreader: 3.3.0 -> 3.4.3

--- pkgs/desktops/cinnamon/xreader/default.nix
+++ pkgs/desktops/cinnamon/xreader/default.nix
@@ -26,13 +26,13 @@
 
 stdenv.mkDerivation rec {
   pname = "xreader";
-  version = "3.3.0";
+  version = "3.4.3";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    sha256 = "sha256-wBrP5SHGPvH/Gz9QY253zQuf8WSjV19oNB5aIqXGLZ8=";
+    sha256 = "sha256-GkJo/wc5StyeQv0pv5XK0Qy3o8EGpfPYY8gOMq0Afgs=";
   };
 
   nativeBuildInputs = [
