d4c96363c4d1213ff7c10215c234a093996c050b
bezmuth <benkel97@protonmail.com>
2022-08-03 16:02:00 +0100

    ocamlPackages.data-encoding: 0.4.0 -> 0.5.3

--- pkgs/development/ocaml-modules/data-encoding/default.nix
+++ pkgs/development/ocaml-modules/data-encoding/default.nix
@@ -3,31 +3,35 @@
 , buildDunePackage
 , ezjsonm
 , zarith
+, zarith_stubs_js
 , hex
 , json-data-encoding
 , json-data-encoding-bson
+, either
 , alcotest
 , crowbar
 }:
 
 buildDunePackage {
   pname = "data-encoding";
-  version = "0.4.0";
+  version = "0.5.3";
 
   src = fetchFromGitLab {
     owner = "nomadic-labs";
     repo = "data-encoding";
-    rev = "v0.4";
-    sha256 = "1f88l9azpfk730hps5v6zlg4yyyyhj1gl27qy3cbbkzjc82d2rhx";
+    rev = "v0.5.3";
+    sha256 = "0626pvwjbhfk6iiyqdk88c8a3x1sdlr4c3blj4zzbvki3s76khqw";
   };
   useDune2 = true;
 
   propagatedBuildInputs = [
     ezjsonm
     zarith
+    zarith_stubs_js
     hex
     json-data-encoding
     json-data-encoding-bson
+    either
   ];
 
   checkInputs = [
