f1333c112e7f8666cc11b1b16fdb867dc07c5a4a
Mostly Void <7rat13@gmail.com>
2022-08-03 19:51:28 +0530

    use llvm_11 for compiling wasmedge with AOT

--- pkgs/development/tools/wasmedge/default.nix
+++ pkgs/development/tools/wasmedge/default.nix
@@ -1,13 +1,12 @@
-{
-  lib,
-  stdenv,
-  fetchFromGitHub,
-  llvm_12,
-  lld,
-  boost,
-  cmake,
-  gtest,
-  spdlog,
+{ lib
+, stdenv
+, fetchFromGitHub
+, llvm_11
+, boost
+, cmake
+, lld
+, gtest
+, spdlog
 }:
 stdenv.mkDerivation rec {
   pname = "wasmedge";
@@ -22,21 +21,23 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     boost
-    llvm_12
+    spdlog
+    llvm_11
   ];
 
-  nativeBuildInputs = [cmake lld boost gtest spdlog];
+  nativeBuildInputs = [ cmake lld ];
+
+  checkInputs = [ gtest ];
 
   cmakeFlags = [
     "-DCMAKE_BUILD_TYPE=Release"
     "-DWASMEDGE_BUILD_TESTS=OFF" # Tests are downloaded using git
-    "-DWASMEDGE_BUILD_AOT_RUNTIME=OFF" # Fails otherwise
   ];
 
   meta = with lib; {
     homepage = "https://wasmedge.org/";
-    license = with licenses; [asl20];
+    license = with licenses; [ asl20 ];
     description = "A lightweight, high-performance, and extensible WebAssembly runtime for cloud native, edge, and decentralized applications";
-    maintainers = with maintainers; [dit7ya];
+    maintainers = with maintainers; [ dit7ya ];
   };
 }
