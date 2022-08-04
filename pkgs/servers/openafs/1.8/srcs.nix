995ce4323c60f5b9f996e33f8845fc171854a469
Anders Kaseorg <andersk@mit.edu>
2022-08-03 20:30:20 -0700

    openafs: 1.8.8.1 → 1.8.9pre1
    This is needed for new Linux kernel support.
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- pkgs/servers/openafs/1.8/srcs.nix
+++ pkgs/servers/openafs/1.8/srcs.nix
@@ -1,16 +1,16 @@
 { fetchurl }:
 rec {
-  version = "1.8.8.1";
+  version = "1.8.9pre1";
   src = fetchurl {
-    url = "https://www.openafs.org/dl/openafs/${version}/openafs-${version}-src.tar.bz2";
-    sha256 = "sha256-58S+1wdbzWQC4/DC1bnb52rS7jxf1d3DlzozVsoj70Q=";
+    url = "https://www.openafs.org/dl/openafs/candidate/${version}/openafs-${version}-src.tar.bz2";
+    sha256 = "sha256-O51fZGse/SSuGLpr5FSQuwG0VHclYh+zNTefV1TGy3o=";
   };
 
   srcs = [
     src
     (fetchurl {
-      url = "https://www.openafs.org/dl/openafs/${version}/openafs-${version}-doc.tar.bz2";
-      sha256 = "sha256-y17O3C4WS+o7SMayydbxw2v96R0GikxiqciF30j+jms=";
+      url = "https://www.openafs.org/dl/openafs/candidate/${version}/openafs-${version}-doc.tar.bz2";
+      sha256 = "sha256-9ldv29R5atzj96sRTJ40xMynF5JKsC5djy3Fvw+pPy4=";
     })
   ];
 }
