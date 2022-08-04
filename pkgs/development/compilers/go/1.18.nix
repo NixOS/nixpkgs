1bc47147c08f6bf2c19aab1d4e03bc9b0e199195
zowoq <59103226+zowoq@users.noreply.github.com>
2022-08-04 13:40:29 +1000

    go: minor cleanup

--- pkgs/development/compilers/go/1.18.nix
+++ pkgs/development/compilers/go/1.18.nix
@@ -18,12 +18,8 @@
 , runtimeShell
 , buildPackages
 , pkgsBuildTarget
-, callPackage
-, threadsCross ? null # for MinGW
+, threadsCross
 }:
-
-# threadsCross is just for MinGW
-assert threadsCross != null -> stdenv.targetPlatform.isWindows;
 
 let
   go_bootstrap = buildPackages.callPackage ./bootstrap116.nix { };
@@ -37,19 +33,19 @@ let
   '';
 
   goarch = platform: {
-    "i686" = "386";
-    "x86_64" = "amd64";
     "aarch64" = "arm64";
     "arm" = "arm";
     "armv5tel" = "arm";
     "armv6l" = "arm";
     "armv7l" = "arm";
+    "i686" = "386";
     "mips" = "mips";
+    "mips64el" = "mips64le";
     "mipsel" = "mipsle";
+    "powerpc64le" = "ppc64le";
     "riscv64" = "riscv64";
     "s390x" = "s390x";
-    "powerpc64le" = "ppc64le";
-    "mips64el" = "mips64le";
+    "x86_64" = "amd64";
   }.${platform.parsed.cpu.name} or (throw "Unsupported system: ${platform.parsed.cpu.name}");
 
   # We need a target compiler which is still runnable at build time,
@@ -58,7 +54,6 @@ let
 
   isCross = stdenv.buildPlatform != stdenv.targetPlatform;
 in
-
 stdenv.mkDerivation rec {
   pname = "go";
   version = "1.18.4";
@@ -81,7 +76,7 @@ stdenv.mkDerivation rec {
 
   depsBuildTarget = lib.optional isCross targetCC;
 
-  depsTargetTarget = lib.optional (threadsCross != null) threadsCross;
+  depsTargetTarget = lib.optional stdenv.targetPlatform.isWindows threadsCross;
 
   hardeningDisable = [ "all" ];
 
@@ -288,10 +283,10 @@ stdenv.mkDerivation rec {
   disallowedReferences = [ goBootstrap ];
 
   meta = with lib; {
-    homepage = "https://go.dev/";
     description = "The Go Programming language";
+    homepage = "https://go.dev/";
     license = licenses.bsd3;
     maintainers = teams.golang.members;
-    platforms = platforms.linux ++ platforms.darwin;
+    platforms = platforms.darwin ++ platforms.linux;
   };
 }
