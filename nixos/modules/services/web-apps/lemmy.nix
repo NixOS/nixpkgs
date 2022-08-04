a39ae5cfd5dc7958bc178328e2b7cdbbed191bd8
Ilan Joselevich <personal@ilanjoselevich.com>
2022-08-04 21:14:11 +0300

    fixup! lemmy: improve module and add test

--- nixos/modules/services/web-apps/lemmy.nix
+++ nixos/modules/services/web-apps/lemmy.nix
@@ -91,105 +91,105 @@ in
         };
       };
     };
+  };
 
-    config = mkIf cfg.enable {
-      services.lemmy.settings = (mapAttrs (name: mkDefault)
-        {
-          bind = cfg.settings.listenAddress;
-          tls_enabled = true;
-          pictrs_url = with config.services.pict-rs; "http://${address}:${toString port}";
-          actor_name_max_length = 20;
+  config = mkIf cfg.enable {
+    services.lemmy.settings = (mapAttrs (name: mkDefault)
+      {
+        bind = cfg.settings.listenAddress;
+        tls_enabled = true;
+        pictrs_url = with config.services.pict-rs; "http://${address}:${toString port}";
+        actor_name_max_length = 20;
 
-          rate_limit.message = 180;
-          rate_limit.message_per_second = 60;
-          rate_limit.post = 6;
-          rate_limit.post_per_second = 600;
-          rate_limit.register = 3;
-          rate_limit.register_per_second = 3600;
-          rate_limit.image = 6;
-          rate_limit.image_per_second = 3600;
-        } // {
-        database = mapAttrs (name: mkDefault) {
-          user = "lemmy";
-          host = "/run/postgresql";
-          port = 5432;
-          database = "lemmy";
-          pool_size = 5;
-        };
-      });
+        rate_limit.message = 180;
+        rate_limit.message_per_second = 60;
+        rate_limit.post = 6;
+        rate_limit.post_per_second = 600;
+        rate_limit.register = 3;
+        rate_limit.register_per_second = 3600;
+        rate_limit.image = 6;
+        rate_limit.image_per_second = 3600;
+      } // {
+      database = mapAttrs (name: mkDefault) {
+        user = "lemmy";
+        host = "/run/postgresql";
+        port = 5432;
+        database = "lemmy";
+        pool_size = 5;
+      };
+    });
 
-      services.pict-rs.enable = true;
+    services.pict-rs.enable = true;
 
-      systemd.services.lemmy = {
-        description = "Lemmy server";
+    systemd.services.lemmy = {
+      description = "Lemmy server";
 
-        environment = {
-          LEMMY_CONFIG_LOCATION = "/run/lemmy/config.hjson";
+      environment = {
+        LEMMY_CONFIG_LOCATION = "/run/lemmy/config.hjson";
 
-          # Verify how this is used, and don't put the password in the nix store
-          LEMMY_DATABASE_URL = with cfg.settings.database;"postgres:///${database}?host=${host}";
-        };
-
-        documentation = [
-          "https://join-lemmy.org/docs/en/administration/from_scratch.html"
-          "https://join-lemmy.org/docs"
-        ];
-
-        wantedBy = [ "multi-user.target" ];
-
-        after = [ "pict-rs.service" ] ++ lib.optionals cfg.settings.database.createLocally [ "postgresql.service" ];
-
-        requires = lib.optionals cfg.settings.database.createLocally [ "postgresql.service" ];
-
-        serviceConfig = {
-          DynamicUser = true;
-          RuntimeDirectory = "lemmy";
-          ExecStartPre = "install -m 600 ${settingsFormat.generate "config.hjson" cfg.settings} /run/lemmy/config.hjson";
-          ExecStart = "${cfg.package}/bin/lemmy-server";
-        };
+        # Verify how this is used, and don't put the password in the nix store
+        LEMMY_DATABASE_URL = with cfg.settings.database;"postgres:///${database}?host=${host}";
       };
 
-      systemd.services.lemmy-ui = {
-        description = "Lemmy ui";
+      documentation = [
+        "https://join-lemmy.org/docs/en/administration/from_scratch.html"
+        "https://join-lemmy.org/docs"
+      ];
 
-        environment = {
-          LEMMY_UI_HOST = "${cfg.ui.listenAddress}:${toString cfg.ui.port}";
-          LEMMY_INTERNAL_HOST = "${cfg.settings.listenAddress}:${toString cfg.settings.port}";
-          LEMMY_EXTERNAL_HOST = cfg.settings.hostname;
-          LEMMY_HTTPS = "false";
-        };
+      wantedBy = [ "multi-user.target" ];
 
-        documentation = [
-          "https://join-lemmy.org/docs/en/administration/from_scratch.html"
-          "https://join-lemmy.org/docs"
-        ];
+      after = [ "pict-rs.service" ] ++ lib.optionals cfg.settings.database.createLocally [ "postgresql.service" ];
 
-        wantedBy = [ "multi-user.target" ];
+      requires = lib.optionals cfg.settings.database.createLocally [ "postgresql.service" ];
 
-        after = [ "lemmy.service" ];
+      serviceConfig = {
+        DynamicUser = true;
+        RuntimeDirectory = "lemmy";
+        ExecStartPre = "install -m 600 ${settingsFormat.generate "config.hjson" cfg.settings} /run/lemmy/config.hjson";
+        ExecStart = "${cfg.package}/bin/lemmy-server";
+      };
+    };
 
-        requires = [ "lemmy.service" ];
+    systemd.services.lemmy-ui = {
+      description = "Lemmy ui";
 
-        serviceConfig = {
-          DynamicUser = true;
-          WorkingDirectory = "${cfg.ui.package}";
-          ExecStart = "${pkgs.nodejs}/bin/node ${cfg.ui.package}/dist/js/server.js";
-        };
+      environment = {
+        LEMMY_UI_HOST = "${cfg.ui.listenAddress}:${toString cfg.ui.port}";
+        LEMMY_INTERNAL_HOST = "${cfg.settings.listenAddress}:${toString cfg.settings.port}";
+        LEMMY_EXTERNAL_HOST = cfg.settings.hostname;
+        LEMMY_HTTPS = "false";
       };
 
-      services.postgresql = mkIf cfg.settings.database.createLocally {
-        enable = true;
-        ensureDatabases = [ cfg.settings.database.database ];
-        ensureUsers = [{
-          name = cfg.settings.database.user;
-          ensurePermissions."DATABASE ${cfg.settings.database.database}" = "ALL PRIVILEGES";
-        }];
-      };
+      documentation = [
+        "https://join-lemmy.org/docs/en/administration/from_scratch.html"
+        "https://join-lemmy.org/docs"
+      ];
 
-      assertions = [{
-        assertion = cfg.settings.database.createLocally -> (cfg.settings.database.host == "localhost" || cfg.settings.database.host == "/run/postgresql");
-        message = "In order to use database.createLocally, you need to use a local database";
+      wantedBy = [ "multi-user.target" ];
+
+      after = [ "lemmy.service" ];
+
+      requires = [ "lemmy.service" ];
+
+      serviceConfig = {
+        DynamicUser = true;
+        WorkingDirectory = "${cfg.ui.package}";
+        ExecStart = "${pkgs.nodejs}/bin/node ${cfg.ui.package}/dist/js/server.js";
+      };
+    };
+
+    services.postgresql = mkIf cfg.settings.database.createLocally {
+      enable = true;
+      ensureDatabases = [ cfg.settings.database.database ];
+      ensureUsers = [{
+        name = cfg.settings.database.user;
+        ensurePermissions."DATABASE ${cfg.settings.database.database}" = "ALL PRIVILEGES";
       }];
     };
+
+    assertions = [{
+      assertion = cfg.settings.database.createLocally -> (cfg.settings.database.host == "localhost" || cfg.settings.database.host == "/run/postgresql");
+      message = "In order to use database.createLocally, you need to use a local database";
+    }];
   };
 }
