e4be613a8bb2e73acf810c37ca0bec6d4436d34c
K900 <me@0upti.me>
2022-08-04 08:13:09 +0300

    linux_latest-libre: 18825 -> 18837

--- pkgs/os-specific/linux/kernel/linux-libre.nix
+++ pkgs/os-specific/linux/kernel/linux-libre.nix
@@ -1,8 +1,8 @@
 { stdenv, lib, fetchsvn, linux
 , scripts ? fetchsvn {
     url = "https://www.fsfla.org/svn/fsfla/software/linux-libre/releases/branches/";
-    rev = "18825";
-    sha256 = "0gqmdlcxq3ws3mafbf47j40vrrfr5966nily6l7gj8hq2j0016cj";
+    rev = "18837";
+    sha256 = "0645lkbh5bi9a8nhdyh21h7rrw8x8pmb7la08zn7gpkmwvk3wnwx";
   }
 , ...
 }:
