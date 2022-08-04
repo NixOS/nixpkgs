b8dc7a5e88a82d218fddf6c14a6ccfc830fd296e
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-08-05 00:49:57 +0100

    audacious: migrate to meson build system, refactor
    Separate the derivation in `audacious` and `audacious-plugins`. Since
    one derivation depends on the other, we first build `audacious` without
    the `audacious-plugins`, them we build `audacious-plugins` and finally
    we build the final version of `audacious`.
    
    Also, add myself as maintainer.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25959,6 +25959,10 @@ with pkgs;
   aucatctl = callPackage ../applications/audio/aucatctl { };
 
   audacious = libsForQt5.callPackage ../applications/audio/audacious { };
+  audacious-plugins = libsForQt5.callPackage ../applications/audio/audacious/plugins.nix {
+    # Avoid circular dependency
+    audacious = audacious.override { audacious-plugins = null; };
+  };
   audaciousQt5 = audacious;
 
   audacity-gtk2 = callPackage ../applications/audio/audacity { wxGTK = wxGTK31-gtk2; };
