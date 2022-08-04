0081536ce57bbfc86462358b55bfbf4a27fbb4e9
Ilan Joselevich <personal@ilanjoselevich.com>
2022-08-04 20:59:20 +0300

    python3Packages.cryptolyzer: init at 0.8.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2027,6 +2027,8 @@ in {
     inherit (pkgs.darwin.apple_sdk.frameworks) Security;
   };
 
+  cryptolyzer = callPackage ../development/python-modules/cryptolyzer { };
+
   cryptoparser = callPackage ../development/python-modules/cryptoparser { };
 
   crytic-compile = callPackage ../development/python-modules/crytic-compile { };
