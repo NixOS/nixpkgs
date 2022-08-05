58ca06ca1b03f1437e2d2c3fc488dfbb7361afe0
Austin Seipp <aseipp@pobox.com>
2022-08-04 23:27:15 -0500

    spicedb: init at 1.11.0
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13526,6 +13526,7 @@ with pkgs;
 
   remarkable2-toolchain = callPackage ../development/tools/misc/remarkable/remarkable2-toolchain { };
 
+  spicedb     = callPackage ../servers/spicedb { };
   spicedb-zed = callPackage ../servers/spicedb/zed.nix { };
 
   tacacsplus = callPackage ../servers/tacacsplus { };
