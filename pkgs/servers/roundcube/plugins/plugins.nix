ef731d5040a50758299e9021e6efc8f75f5be08a
Stephane Schitter <stephane@frozenkiwi.net>
2022-08-06 19:35:47 +0200

    roundcubePlugins.thunderbird_labels: init at 1.6.0
    https://github.com/mike-kfed/roundcube-thunderbird_labels
    https://github.com/mike-kfed/roundcube-thunderbird_labels/releases/tag/v1.6.0

--- pkgs/servers/roundcube/plugins/plugins.nix
+++ pkgs/servers/roundcube/plugins/plugins.nix
@@ -8,4 +8,5 @@
   carddav = callPackage ./carddav { };
   contextmenu = callPackage ./contextmenu { };
   persistent_login = callPackage ./persistent_login { };
+  thunderbird_labels = callPackage ./thunderbird_labels { };
 }
