fb6659387d13d5245d5edde8c52e4be5ac5aaf53
Fabian Affolter <mail@fabian-affolter.ch>
2022-08-06 18:28:52 +0200

    python310Packages.pylibmc: add pythonImportsCheck

--- pkgs/development/python-modules/pylibmc/default.nix
+++ pkgs/development/python-modules/pylibmc/default.nix
@@ -1,23 +1,44 @@
-{ buildPythonPackage, fetchPypi, lib, libmemcached, zlib, cyrus_sasl }:
+{ lib
+, buildPythonPackage
+, cyrus_sasl
+, fetchPypi
+, libmemcached
+, zlib
+}:
 
 buildPythonPackage rec {
-  version = "1.6.2";
   pname = "pylibmc";
+  version = "1.6.2";
+  format = "setuptools";
+
+  disabled = pythonAtLeast "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-QatJ05VAdnN0iRvvC+tSkcqXvrcEi3r3dSEGSVPATcA=";
+    hash = "sha256-QatJ05VAdnN0iRvvC+tSkcqXvrcEi3r3dSEGSVPATcA=";
   };
 
-  buildInputs = [ libmemcached zlib cyrus_sasl ];
-  setupPyBuildFlags = [ "--with-sasl2" ];
+  buildInputs = [
+    cyrus_sasl
+    libmemcached
+    zlib
+  ];
 
-  # requires an external memcached server running
+  setupPyBuildFlags = [
+    "--with-sasl2"
+  ];
+
+  # Requires an external memcached server running
   doCheck = false;
+
+  pythonImportsCheck = [
+    "pylibmc"
+  ];
 
   meta = with lib; {
     description = "Quick and small memcached client for Python";
     homepage = "http://sendapatch.se/projects/pylibmc/";
     license = licenses.bsd3;
+    maintainers = with maintainers; [ ];
   };
 }
