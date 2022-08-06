98ba23d04346c36eb359bd254834dad132e88434
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-06 04:19:33 +0000

    python310Packages.mailchecker: 4.1.18 -> 5.0.0

--- pkgs/development/python-modules/mailchecker/default.nix
+++ pkgs/development/python-modules/mailchecker/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "mailchecker";
-  version = "4.1.18";
+  version = "5.0.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-zBikruHBTXKK4Yk7euTp0lTcdCG5AQ/kMZrECAvQ7Zw=";
+    hash = "sha256-d8RG/PSWPHvQy7o9tPLPGrEp0r/Y3JWwWoT3cS4iR38=";
   };
 
   # Module has no tests
