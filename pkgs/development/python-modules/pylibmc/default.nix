4244408aa6ea264f4b054799bfda7b29632d0fc1
Fabian Affolter <mail@fabian-affolter.ch>
2022-08-06 19:26:13 +0200

    python310Packages.pylibmc: remove whitespace

--- pkgs/development/python-modules/pylibmc/default.nix
+++ pkgs/development/python-modules/pylibmc/default.nix
@@ -12,7 +12,7 @@ buildPythonPackage rec {
   version = "1.6.2";
   format = "setuptools";
 
-  disabled = pythonOlder "3.7"; 
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
