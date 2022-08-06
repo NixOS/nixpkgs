234eb04495155d1e6d7e297f023f6070c55f1918
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-06 07:44:22 +0000

    trino-cli: 390 -> 392

--- pkgs/development/tools/database/trino-cli/default.nix
+++ pkgs/development/tools/database/trino-cli/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "trino-cli";
-  version = "390";
+  version = "392";
 
   jarfilename = "${pname}-${version}-executable.jar";
 
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://maven/io/trino/${pname}/${version}/${jarfilename}";
-    sha256 = "sha256-rqs2rWmr5hv4F/tc7xWBgkNht/l3meJUnpEyOn2cU48=";
+    sha256 = "sha256-yqTKXmcRgsSSr4KAZ2NV7FrCGIxCU/V14XFEZmUTj1s=";
   };
 
   dontUnpack = true;
