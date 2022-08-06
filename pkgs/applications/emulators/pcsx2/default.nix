d6905d065a6521653488b8f7ad96198d1072e27c
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-06 02:11:30 +0000

    pcsx2: 1.7.3128 -> 1.7.3165

--- pkgs/applications/emulators/pcsx2/default.nix
+++ pkgs/applications/emulators/pcsx2/default.nix
@@ -32,14 +32,14 @@
 
 stdenv.mkDerivation rec {
   pname = "pcsx2";
-  version = "1.7.3128";
+  version = "1.7.3165";
 
   src = fetchFromGitHub {
     owner = "PCSX2";
     repo = "pcsx2";
     fetchSubmodules = true;
     rev = "v${version}";
-    hash = "sha256-OVKxVyUkTpyVvQ1oA8G6W77ssC1NmUAu/VanBM8Z168=";
+    hash = "sha256-FdLmLZLpS8zPmHVn4k0nE6vS/omYVIOal9ej0h3bE/Y=";
   };
 
   cmakeFlags = [
