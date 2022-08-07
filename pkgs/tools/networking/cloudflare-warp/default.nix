2ed95928b72d135eef8b2fc9429ef7baa03b763b
Ahren Stevens-Taylor <git@stevenstaylor.dev>
2022-08-07 11:53:29 +0000

    chore(bump): cloudflare-warp bump 2022.02.24 -> 2022.7.421

--- pkgs/tools/networking/cloudflare-warp/default.nix
+++ pkgs/tools/networking/cloudflare-warp/default.nix
@@ -2,11 +2,12 @@
 
 stdenv.mkDerivation rec {
   pname = "cloudflare-warp";
-  version = "2022.02.24";
+  version = "2022.7.421";
 
   src = fetchurl {
-    url = "https://pkg.cloudflareclient.com/uploads/cloudflare_warp_2022_2_288_1_amd64_a0be7b47b3.deb";
-    sha256 = "sha256-gBXF0EfFMT6BC6ts/6PQYJH3AAQSDsFoZGK3RZIqmOA=";
+    #https://pkg.cloudflareclient.com/packages/cloudflare-warp Ubuntu Focal amd64
+    url = "https://pkg.cloudflareclient.com/pool/dists/focal/main/cloudflare_warp_2022_7_472_1_amd64_77aa79eba3_amd64.deb";
+    sha256 = "sha256-lbT3uH0kUbFpSvEYcfdh5jnpaKINwXotD3iePwXHAsY=";
   };
 
   nativeBuildInputs = [
@@ -27,7 +28,7 @@ stdenv.mkDerivation rec {
     runHook preInstall
 
     mv usr $out
-    mv lib $out
+    cp -a lib $out && rm -rf lib
     mv bin $out
 
     runHook postInstall
@@ -36,6 +37,8 @@ stdenv.mkDerivation rec {
   postInstall = ''
     substituteInPlace $out/lib/systemd/system/warp-svc.service \
       --replace "ExecStart=" "ExecStart=$out"
+    substituteInPlace $out/lib/systemd/user/warp-taskbar.service \
+      --replace "ExecStart=" "ExecStart=$out"
   '';
 
   meta = with lib; {
@@ -43,7 +46,7 @@ stdenv.mkDerivation rec {
     homepage = "https://pkg.cloudflareclient.com/packages/cloudflare-warp";
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license = licenses.unfree;
-    maintainers = with maintainers; [ wolfangaukang ];
+    maintainers = with maintainers; [ wolfangaukang astevenstaylor ];
     platforms = [ "x86_64-linux" ];
   };
 }
