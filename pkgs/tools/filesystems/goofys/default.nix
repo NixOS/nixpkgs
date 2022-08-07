NixOS/nixpkgs

commit caa2b2ee685f71f9c4d96de02234c42eb4e9343d

goofys: mark as broken

pkgs/tools/filesystems/goofys/default.nix
@@ -30,7 +30,8 @@ buildGoModule {
     description = "A high-performance, POSIX-ish Amazon S3 file system written in Go.";
     license = [ lib.licenses.mit ];
     maintainers = [ lib.maintainers.adisbladis ];
-    broken = stdenv.isDarwin; # needs to update gopsutil to at least v3.21.3 to include https://github.com/shirou/gopsutil/pull/1042
+    # does not build with go 1.17
+    broken = true;
   };
 
 }

pkgs/top-level/all-packages.nix
@@ -6512,9 +6512,7 @@ with pkgs;
 
   go-mtpfs = callPackage ../tools/filesystems/go-mtpfs { };
 
-  goofys = callPackage ../tools/filesystems/goofys {
-    buildGoModule = buildGo116Module;
-  };
+  goofys = callPackage ../tools/filesystems/goofys { };
 
   go-sct = callPackage ../tools/X11/go-sct { };
 

commit 87909c704f8f9db54d642c7e2cedbbd9cad4724d

goofys: unstable-2021-03-26 -> unstable-2022-04-21
Cherry-picked from 9e2d1c849242a6c26902230ae292464dd27f0f69
Package has been broken since caa2b2ee685f71f9c4d96de02234c42eb4e9343d

pkgs/tools/filesystems/goofys/default.nix
@@ -6,17 +6,17 @@
 
 buildGoModule {
   pname = "goofys";
-  version = "unstable-2021-03-26";
+  version = "unstable-2022-04-21";
 
   src = fetchFromGitHub {
     owner = "kahing";
     repo = "goofys";
     # Same as v0.24.0 but migrated to Go modules
-    rev = "0c993271269b539196330a18716a33fbeeebd624";
-    sha256 = "18is5sv2a9wmsm0qpakly988z1qyl2b2hf2105lpxrgl659sf14p";
+    rev = "829d8e5ce20faa3f9f6f054077a14325e00e9249";
+    sha256 = "sha256-6yVMNSwwPZlADXuPBDRlgoz4Stuz2pgv6r6+y2/C8XY=";
   };
 
-  vendorSha256 = "15yq0msh9icxd5n2zkkqrlwxifizhpa99d4aznv8clg32ybs61fj";
+  vendorSha256 = "sha256-2N8MshBo9+2q8K00eTW5So6d8ZNRzOfQkEKmxR428gI=";
 
   subPackages = [ "." ];
 
@@ -30,8 +30,7 @@ buildGoModule {
     description = "A high-performance, POSIX-ish Amazon S3 file system written in Go.";
     license = [ lib.licenses.mit ];
     maintainers = [ lib.maintainers.adisbladis ];
-    # does not build with go 1.17
-    broken = true;
+    broken = stdenv.isDarwin; # needs to update gopsutil to at least v3.21.3 to include https://github.com/shirou/gopsutil/pull/1042
   };
 
 }