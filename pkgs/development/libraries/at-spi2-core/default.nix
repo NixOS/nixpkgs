1c733c34132060dde626980bf165e0170d3b5d4e
Jan Tojnar <jtojnar@gmail.com>
2022-08-07 17:10:15 +0000

    at-spi2-core: 2.45.1 → 2.45.90
    https://gitlab.gnome.org/GNOME/at-spi2-core/-/compare/AT_SPI2_CORE_2_45_1...AT_SPI2_CORE_2_45_90
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/development/libraries/at-spi2-core/default.nix
+++ pkgs/development/libraries/at-spi2-core/default.nix
@@ -20,13 +20,13 @@
 
 stdenv.mkDerivation rec {
   pname = "at-spi2-core";
-  version = "2.45.1";
+  version = "2.45.90";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "upXzRukxCPuzRixiQ3CB1YIVTbJ5tAUt7cUqcGgosZI=";
+    sha256 = "6QUK08JJN1SDlrI3fy/NuTIc4tr/rTXnVU6PathQqw0=";
   };
 
   nativeBuildInputs = [
