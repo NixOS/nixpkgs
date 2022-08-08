f493ca6457dfa1aeca5344c1334bc5c97b79d1e6
Mihai Fufezan <fufexan@protonmail.com>
2022-08-08 23:03:48 +0300

    electron_20: init at 20.0.1
    (cherry picked from commit 93ac320de69e314519161e0dc85c3d823445b315)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14963,7 +14963,8 @@ with pkgs;
     electron_16
     electron_17
     electron_18
-    electron_19;
+    electron_19
+    electron_20;
 
   autobuild = callPackage ../development/tools/misc/autobuild { };
 
