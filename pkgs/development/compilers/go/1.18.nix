bf9a88db617fed06a6303373bbdc9201a36b1b8b
zowoq <59103226+zowoq@users.noreply.github.com>
2022-08-08 21:24:57 +1000

    go: add smoke test to passthru

--- pkgs/development/compilers/go/1.18.nix
+++ pkgs/development/compilers/go/1.18.nix
@@ -11,10 +11,15 @@
 , buildPackages
 , pkgsBuildTarget
 , threadsCross
+, testers
+, skopeo
+, buildGo118Module
 }:
 
 let
   goBootstrap = buildPackages.callPackage ./bootstrap116.nix { };
+
+  skopeoTest = skopeo.override { buildGoModule = buildGo118Module; };
 
   goarch = platform: {
     "aarch64" = "arm64";
@@ -160,7 +165,10 @@ stdenv.mkDerivation rec {
   disallowedReferences = [ goBootstrap ];
 
   passthru = {
-    inherit goBootstrap;
+    inherit goBootstrap skopeoTest;
+    tests = {
+      skopeo = testers.testVersion { package = skopeoTest; };
+    };
   };
 
   meta = with lib; {
