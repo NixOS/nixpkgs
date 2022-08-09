9f980dee7f3a386fd5df9348a9330b1e6999da9f
tobim <tobim@fastmail.fm>
2022-08-10 00:34:22 +0200

    pkgsStatic.rapidjson: disable checks

--- pkgs/development/libraries/rapidjson/default.nix
+++ pkgs/development/libraries/rapidjson/default.nix
@@ -38,7 +38,7 @@ stdenv.mkDerivation rec {
 
   cmakeFlags = [
     "-DGTEST_SOURCE_DIR=${gtest.dev}/include"
-  ] ++ lib.optionals stdenv.hostPlatform.isStatic [
+  ] ++ lib.optionals (!doCheck) [
     "-DRAPIDJSON_BUILD_TESTS=OFF"
   ];
 
@@ -54,7 +54,7 @@ stdenv.mkDerivation rec {
     runHook postCheck
   '';
 
-  doCheck = true;
+  doCheck = !stdenv.hostPlatform.isStatic;
 
   meta = with lib; {
     description = "Fast JSON parser/generator for C++ with both SAX/DOM style API";
