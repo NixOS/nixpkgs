af1f959f935fb9c34a1fd1135043cbdad302205d
Malo Bourgon <mbourgon@gmail.com>
2022-08-10 11:33:02 -0700

    signalbackup-tools: 20220711 -> 20220810

--- pkgs/applications/networking/instant-messengers/signalbackup-tools/default.nix
+++ pkgs/applications/networking/instant-messengers/signalbackup-tools/default.nix
@@ -1,15 +1,23 @@
-{ lib, stdenv, fetchFromGitHub, openssl, sqlite }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch, openssl, sqlite }:
 
 stdenv.mkDerivation rec {
   pname = "signalbackup-tools";
-  version = "20220711";
+  version = "20220810";
 
   src = fetchFromGitHub {
     owner = "bepaald";
     repo = pname;
     rev = version;
-    sha256 = "sha256-dKU8oTQ6ECwycDN3k7NY/pKpNWH16ceJIFDnRNEA90c=";
+    sha256 = "sha256-z/RAvNUss9rNuBQvxjJQl66ZMrlxvmS9at8L/vSG0XU=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "fix-platform-checks.patch";
+      url = "https://github.com/bepaald/signalbackup-tools/compare/20220810..a81baf25b6ba63da7d30d9a239e5b4bbc8d1ab4f.patch";
+      sha256 = "sha256-i7fuPBil8zB+V3wHHdcbmP79OZoTfG2ZpXPQ3m7X06c=";
+    })
+  ];
 
   buildInputs = [ openssl sqlite ];
   buildFlags = [
