3803658ab75bce78e2b52fdf941bce04124023a8
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-10 21:05:52 +0000

    nvc: 1.6.2 -> 1.7.0

--- pkgs/applications/science/electronics/nvc/default.nix
+++ pkgs/applications/science/electronics/nvc/default.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation rec {
   pname = "nvc";
-  version = "1.6.2";
+  version = "1.7.0";
 
   src = fetchFromGitHub {
     owner = "nickg";
     repo = pname;
     rev = "r${version}";
-    sha256 = "sha256-BtUMpT1MKRFGRlIbCEGo4OBZ/r9es1VRmJdgmk1oZFQ=";
+    sha256 = "sha256-U9VxpHzrAQPMqnSs0YcEnc9dlQUutTuZCJP5F1v7eaA=";
   };
 
   nativeBuildInputs = [
