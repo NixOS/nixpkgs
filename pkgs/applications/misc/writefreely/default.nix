d5fe6a062cb8bde559bfc0efc7c42abc5ca48d4c
f0x <f0x@cthu.lu>
2022-08-12 00:18:29 +0200

    writefreely: refactor package to include templates and assets

--- pkgs/applications/misc/writefreely/default.nix
+++ pkgs/applications/misc/writefreely/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildGoModule, fetchFromGitHub, go-bindata }:
+{ lib, buildGoModule, fetchFromGitHub, go-bindata, lessc, nodePackages, stdenv }:
 
 buildGoModule rec {
   pname = "writefreely";
@@ -13,10 +13,35 @@ buildGoModule rec {
 
   vendorSha256 = "sha256-CBPvtc3K9hr1oEmC+yUe3kPSWx20k6eMRqoxsf3NfCE=";
 
+  writefreely-assets = stdenv.mkDerivation {
+    pname = "writefreely-assets";
+    inherit src version;
+
+    nativeBuildInputs = [ lessc ];
+
+    buildPhase = ''
+      export NODE_PATH=${nodePackages.less-plugin-clean-css}/lib/node_modules
+      (
+        cd less
+        make all
+      )
+    '';
+
+    installPhase = ''
+      mkdir $out
+      cp -r templates pages static $out/
+    '';
+  };
+
   nativeBuildInputs = [ go-bindata ];
 
   preBuild = ''
     make assets
+  '';
+
+  postBuild = ''
+    mkdir $out
+    ln -s ${writefreely-assets}/templates ${writefreely-assets}/pages ${writefreely-assets}/static $out/
   '';
 
   ldflags = [ "-s" "-w" "-X github.com/writeas/writefreely.softwareVer=${version}" ];
