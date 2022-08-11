8ccf4e05df7b7aa87940117a0915f3969ed5cdef
Jörg Thalheim <joerg@thalheim.io>
2022-08-11 13:28:37 +0200

    nixos/consul: use signal instead of api for reload
    consul might require authentication if acls are enabled for `consul
    reload` to work. Signals never require authentication.

--- nixos/modules/services/networking/consul.nix
+++ nixos/modules/services/networking/consul.nix
@@ -201,7 +201,7 @@ in
         serviceConfig = {
           ExecStart = "@${cfg.package}/bin/consul consul agent -config-dir /etc/consul.d"
             + concatMapStrings (n: " -config-file ${n}") configFiles;
-          ExecReload = "${cfg.package}/bin/consul reload";
+          ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
           PermissionsStartOnly = true;
           User = if cfg.dropPrivileges then "consul" else null;
           Restart = "on-failure";
