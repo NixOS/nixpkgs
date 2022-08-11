a6b0f5faaa2ea543868af8e67e39960dc0c672f4
Robert Schütz <nix@dotlambda.de>
2022-08-11 12:01:29 +0000

    python310Packages.pylama: 8.3.8 -> 8.4.1
    https://github.com/klen/pylama/blob/8.4.1/Changelog

--- pkgs/development/python-modules/pylama/default.nix
+++ pkgs/development/python-modules/pylama/default.nix
@@ -17,7 +17,7 @@
 
 let pylama = buildPythonPackage rec {
   pname = "pylama";
-  version = "8.3.8";
+  version = "8.4.1";
 
   format = "setuptools";
 
@@ -26,7 +26,7 @@ let pylama = buildPythonPackage rec {
     owner = "klen";
     repo = "pylama";
     rev = version;
-    hash = "sha256-g6Lq5NaieUI/alxqoVFfL5VaCHwB/jLcp02/N1W69yE=";
+    hash = "sha256-WOGtZ412tX3YH42JCd5HIngunluwtMmQrOSUZp23LPU=";
   };
 
   patches = [
