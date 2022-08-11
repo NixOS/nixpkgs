d2ed2e2f0b3cbfdd0f8c06aa75d4b349f1153027
ajs124 <git@ajs124.de>
2022-08-11 15:19:09 +0200

    jenkins: add myself to maintainers

--- pkgs/development/tools/continuous-integration/jenkins/default.nix
+++ pkgs/development/tools/continuous-integration/jenkins/default.nix
@@ -70,7 +70,7 @@ stdenv.mkDerivation rec {
     homepage = "https://jenkins-ci.org";
     sourceProvenance = with sourceTypes; [ binaryBytecode ];
     license = licenses.mit;
-    maintainers = with maintainers; [ coconnor earldouglas nequissimus ];
+    maintainers = with maintainers; [ coconnor earldouglas nequissimus ajs124 ];
     mainProgram = "jenkins-cli";
     platforms = platforms.all;
   };
