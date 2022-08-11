22b4ec6c4222a39acaec41a75e082a4fa67bf48d
ajs124 <git@ajs124.de>
2022-08-11 23:26:39 +0200

    tzdata: 2022a -> 2022b
    https://mm.icann.org/pipermail/tz-announce/2022-August/000071.html
    (cherry picked from commit 15323f2c050c46a6a81230442a0388c59aefdb1d)

--- pkgs/data/misc/tzdata/default.nix
+++ pkgs/data/misc/tzdata/default.nix
@@ -2,18 +2,18 @@
 
 stdenv.mkDerivation rec {
   pname = "tzdata";
-  version = "2022a";
+  version = "2022b";
 
-  srcs =
-    [ (fetchurl {
-        url = "https://data.iana.org/time-zones/releases/tzdata${version}.tar.gz";
-        sha256 = "0r0nhwpk9nyxj5kkvjy58nr5d85568m04dcb69c4y3zmykczyzzg";
-      })
-      (fetchurl {
-        url = "https://data.iana.org/time-zones/releases/tzcode${version}.tar.gz";
-        sha256 = "1iysv8fdkm79k8wh8jizmjmq075q4qjhk090vxjy57my6dz5wmzq";
-      })
-    ];
+  srcs = [
+    (fetchurl {
+      url = "https://data.iana.org/time-zones/releases/tzdata${version}.tar.gz";
+      hash = "sha256-9ZDq8Eo5UkVCbCvk+uccFDrqXOvBEIi3oKVwRGHfOX0=";
+    })
+    (fetchurl {
+      url = "https://data.iana.org/time-zones/releases/tzcode${version}.tar.gz";
+      hash = "sha256-urINlD5ZoyGENfSNhopOVS8Y1tfz3RKGYMVmDIC4oF8=";
+    })
+  ];
 
   sourceRoot = ".";
 
