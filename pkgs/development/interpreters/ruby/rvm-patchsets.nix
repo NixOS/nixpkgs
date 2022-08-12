b171096af9bd881274f0bea5411c7368ff8103e3
Mario Rodas <marsam@users.noreply.github.com>
2022-08-12 04:20:00 +0000

    ruby: update railsexpress patchset

--- pkgs/development/interpreters/ruby/rvm-patchsets.nix
+++ pkgs/development/interpreters/ruby/rvm-patchsets.nix
@@ -3,6 +3,6 @@
 fetchFromGitHub {
   owner  = "skaes";
   repo   = "rvm-patchsets";
-  rev    = "0251817e2b9d5f73370bbbb12fdf7f7089bd1ac3";
-  sha256 = "1biiq5xzzdfb4hr1sgmx14i2nr05xa9w21pc7dl8c5n4f2ilg8ss";
+  rev    = "a6429bb1a7fb9b5798c22f43338739a6c192b42d";
+  sha256 = "sha256-NpSa+uGQA1rfHNcLzPNTK65J+Wk9ZlzhHFePDA4uuo0=";
 }
