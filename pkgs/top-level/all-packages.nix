eb642f80f9aecc19312909e08601a3c2020b5ce2
Robert Schütz <nix@dotlambda.de>
2022-08-12 11:37:14 +0000

    m2r: use toPythonApplication

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8076,7 +8076,7 @@ with pkgs;
 
   mcfly = callPackage ../tools/misc/mcfly { };
 
-  m2r = python3Packages.callPackage ../tools/text/m2r { };
+  m2r = with python3Packages; toPythonApplication m2r;
 
   md2gemini = with python3.pkgs; toPythonApplication md2gemini;
 
