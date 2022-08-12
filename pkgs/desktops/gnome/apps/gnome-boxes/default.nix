e44a262125d05e0c3a9b9fff51e38458296ffd6a
Jan Tojnar <jtojnar@gmail.com>
2022-08-12 18:52:58 +0000

    gnome.gnome-boxes: 43.alpha → 43.beta
    https://gitlab.gnome.org/GNOME/gnome-boxes/-/compare/43.alpha...43.beta
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>
    
    gnome.gnome-boxes: 43.alpha → 43.beta
    
    - Drop GtkSourceView, it has not been used since 42: https://gitlab.gnome.org/GNOME/gnome-boxes/-/commit/858c29417c71f6f1f6f77f47519d9bdccac9bfd0
    
    https://gitlab.gnome.org/GNOME/gnome-boxes/-/compare/43.alpha...43.beta
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/apps/gnome-boxes/default.nix
+++ pkgs/desktops/gnome/apps/gnome-boxes/default.nix
@@ -12,7 +12,6 @@
 , gobject-introspection
 , libxml2
 , gtk3
-, gtksourceview4
 , libvirt
 , spice-gtk
 , python3
@@ -54,11 +53,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-boxes";
-  version = "43.alpha";
+  version = "43.beta";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "BEUHGaCDN8/fF1D18N82gwc7Xg6kisynSb4j0Ev7+yY=";
+    sha256 = "WqNgpIepZCy/qJLNVFlgCXCRLT6FjyCKJDlz0Jev7Ts=";
   };
 
   patches = [
@@ -97,7 +96,6 @@ stdenv.mkDerivation rec {
     gmp
     gnome.adwaita-icon-theme
     gtk3
-    gtksourceview4
     json-glib
     libapparmor
     libarchive
