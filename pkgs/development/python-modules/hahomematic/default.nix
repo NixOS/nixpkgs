633d0afe3eb9476a9c0b385364ce8b18d2186a8a
Fabian Affolter <mail@fabian-affolter.ch>
2022-08-12 14:03:12 +0200

    python310Packages.hahomematic: 2022.8.6 -> 2022.8.7

--- pkgs/development/python-modules/hahomematic/default.nix
+++ pkgs/development/python-modules/hahomematic/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "hahomematic";
-  version = "2022.8.6";
+  version = "2022.8.7";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "danielperna84";
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-e56nZ/+B7npUM3AVFbCnJOln59GdUvJ0AlRm3kBmrCU=";
+    sha256 = "sha256-HSxGCytQ099GCG3LanExOEp9DYeboQ2fS0QxHbtSI3s=";
   };
 
   propagatedBuildInputs = [
