3994068efd4091a8746b196e178486d1955430da
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-12 05:30:06 +0000

    cinny: 2.1.1 -> 2.1.2
    (cherry picked from commit 2db29e2df54bf8cc28ce8b39bf31fb6c9d7c8bfd)

--- pkgs/applications/networking/instant-messengers/cinny/default.nix
+++ pkgs/applications/networking/instant-messengers/cinny/default.nix
@@ -4,11 +4,11 @@ let
   configOverrides = writeText "cinny-config-overrides.json" (builtins.toJSON conf);
 in stdenv.mkDerivation rec {
   pname = "cinny";
-  version = "2.1.1";
+  version = "2.1.2";
 
   src = fetchurl {
     url = "https://github.com/ajbura/cinny/releases/download/v${version}/cinny-v${version}.tar.gz";
-    sha256 = "0qzg790yd23ja3a782ga085vgd5vvh7rgmdy8kpvc3wf8plvw5nm";
+    sha256 = "sha256-UbL9HP90zfsoj/ClUoBF27n5zkvvN4UU4pxQu8QsoUA=";
   };
 
   installPhase = ''
