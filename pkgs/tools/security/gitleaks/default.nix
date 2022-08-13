d7de2d6dabf7e03ee6914c76d89d7cfb14d11ee0
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-13 06:52:10 +0000

    gitleaks: 8.10.2 -> 8.10.3

--- pkgs/tools/security/gitleaks/default.nix
+++ pkgs/tools/security/gitleaks/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "gitleaks";
-  version = "8.10.2";
+  version = "8.10.3";
 
   src = fetchFromGitHub {
     owner = "zricethezav";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-FiR1zJ9aOdysp5807vZ9aX3O7l8GhDXlFDWuyk5zJQw=";
+    sha256 = "sha256-uLCV0eSsQhx9/U/iaAPI25w4Q6pI0PeHAPAQvOJwSWI=";
   };
 
   vendorSha256 = "sha256-X8z9iKRR3PptNHwy1clZG8QsClsjbW45nZb2fHGfSYk=";
