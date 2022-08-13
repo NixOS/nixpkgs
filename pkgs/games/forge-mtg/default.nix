42eb5286fa93a8f22c06aa0dea1867b0f4bcf6fa
Sebastian Reuße <seb@schattenkopie.de>
2022-08-13 10:40:26 +0200

    forge-mtg: build package from source

--- pkgs/games/forge-mtg/default.nix
+++ pkgs/games/forge-mtg/default.nix
@@ -1,30 +1,87 @@
-{ lib, bzip2, fetchurl, makeWrapper, coreutils, gnused, openjdk, stdenv }:
-stdenv.mkDerivation rec {
-  pname = "forge-mtg";
+{ coreutils
+, fetchFromGitHub
+, gnused
+, lib
+, maven
+, makeWrapper
+, openjdk
+, stdenv
+}:
+
+let
   version = "1.6.53";
 
-  src = fetchurl {
-    url = "https://releases.cardforge.org/forge/forge-gui-desktop/${version}/forge-gui-desktop-${version}.tar.bz2";
-    sha256 = "sha256-rRb4PevxKMw4GN0q1oMRRsXQfgZhrNrJ3S++CuctRvA=";
+  src = fetchFromGitHub {
+    owner = "Card-Forge";
+    repo = "forge";
+    rev = "forge-${version}";
+    sha256 = "sha256-tNPG90mw8HZjp37YJ9JQlOBiVNPRo6xuNur651Adva8=";
   };
-  sourceRoot = ".";
 
-  dontBuild = true;
+  # launch4j downloads and runs a native binary during the package phase.
+  patches = [ ./no-launch4j.patch ];
 
-  nativeBuildInputs = [ bzip2 makeWrapper ];
+  mavenRepository = stdenv.mkDerivation {
+    pname = "forge-mtg-maven-repository";
+    inherit version src patches;
+
+    nativeBuildInputs = [ maven ];
+
+    buildPhase = ''
+      runHook preBuild
+      # Tests need a running Xorg.
+      mvn package -Dmaven.repo.local=$out -DskipTests
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      find $out -type f \( \
+        -name \*.lastUpdated \
+        -o -name resolver-status.properties \
+        -o -name _remote.repositories \) \
+        -delete
+      runHook postInstall
+    '';
+
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+    outputHash = "sha256-6FTbYXaF3rBIZov2WJxjG/ovmvimjXFPaFchAduVzI8=";
+  };
+
+in stdenv.mkDerivation {
+  pname = "forge-mtg";
+  inherit version src patches;
+
+  nativeBuildInputs = [ maven makeWrapper ];
+
+  buildPhase = ''
+    runHook preBuild
+    # Tests need a running Xorg.
+    mvn --offline -Dmaven.repo.local=${mavenRepository} -DskipTests package;
+    runHook postBuild
+  '';
 
   installPhase = ''
     runHook preInstall
-    mkdir -vp $out/{bin,share/forge}
-    cp -var . $out/share/forge
+    mkdir -pv $out/{bin,share/forge}
+    cp -av \
+      forge-gui-desktop/target/forge.sh \
+      forge-gui-desktop/target/forge-gui-desktop-${version}-jar-with-dependencies.jar \
+      forge-adventure/target/forge-adventure.sh \
+      forge-adventure/target/forge-adventure-${version}-jar-with-dependencies.jar \
+      forge-gui/res \
+      $out/share/forge
     runHook postInstall
   '';
+
   preFixup = ''
     for commandToInstall in forge forge-adventure; do
       chmod 555 $out/share/forge/$commandToInstall.sh
       makeWrapper $out/share/forge/$commandToInstall.sh $out/bin/$commandToInstall \
         --prefix PATH : ${lib.makeBinPath [ coreutils openjdk gnused ]} \
-        --set JAVA_HOME ${openjdk}/lib/openjdk
+        --set JAVA_HOME ${openjdk}/lib/openjdk \
+        --set SENTRY_DSN ""
     done
   '';
 
@@ -33,7 +90,6 @@ stdenv.mkDerivation rec {
     homepage = "https://www.slightlymagic.net/forum/viewforum.php?f=26";
     platforms = openjdk.meta.platforms;
     license = licenses.gpl3Plus;
-    sourceProvenance = with sourceTypes; [ binaryBytecode ];
     maintainers = with maintainers; [ eigengrau ];
   };
 }
