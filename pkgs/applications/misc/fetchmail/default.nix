4e817e1d1a7f1e4c794e2a485feab9c1e04bb7ce
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-13 17:16:22 +0000

    fetchmail: 6.4.30 -> 6.4.32

--- pkgs/applications/misc/fetchmail/default.nix
+++ pkgs/applications/misc/fetchmail/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "fetchmail";
-  version = "6.4.30";
+  version = "6.4.32";
 
   src = fetchurl {
     url = "mirror://sourceforge/fetchmail/fetchmail-${version}.tar.xz";
-    sha256 = "sha256-NYJMIKodjZfj8WEf70m59/aDlvfLxNOoB/7SeunqRFo=";
+    sha256 = "sha256-PudERmVgatBvtfexX5HjFzuEW8mLZGsaRRSkaqSLcig=";
   };
 
   buildInputs = [ openssl ];
