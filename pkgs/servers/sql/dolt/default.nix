b626ce773cf4644421762a0836c8de572b81ef6e
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-13 12:28:10 +0000

    dolt: 0.40.21 -> 0.40.25

--- pkgs/servers/sql/dolt/default.nix
+++ pkgs/servers/sql/dolt/default.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "dolt";
-  version = "0.40.21";
+  version = "0.40.25";
 
   src = fetchFromGitHub {
     owner = "dolthub";
     repo = "dolt";
     rev = "v${version}";
-    sha256 = "sha256-oqFOAFJheVJOsFmMo5o0RDaPCPrGrGYnNcUgv4Ead8E=";
+    sha256 = "sha256-MPfi6rfqlbrnl3CE0YKtIAUagqCB6f3cJtZ7mGUQ5Ng=";
   };
 
   modRoot = "./go";
   subPackages = [ "cmd/dolt" "cmd/git-dolt" "cmd/git-dolt-smudge" ];
-  vendorSha256 = "sha256-3W5ZNpZ9TNsqQ/atwMUPHbViyQ5YolwwTNznaRXNoso=";
+  vendorSha256 = "sha256-Iy01voXaCzbBEbJRxcLDzr5BKj4PHfcib3KH21VzFS4=";
 
   doCheck = false;
 
