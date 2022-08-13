df8c06b833d16cf9160c672ba040cbeb1e97204e
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-13 01:39:31 +0000

    python310Packages.awscrt: 0.13.14 -> 0.14.0

--- pkgs/development/python-modules/awscrt/default.nix
+++ pkgs/development/python-modules/awscrt/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "awscrt";
-  version = "0.13.14";
+  version = "0.14.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-K2x0Up3H6kIWcYeWvVegd1CkTjq8RoM0AOm0SX5u6wQ=";
+    hash = "sha256-MGLTFcsWVC/gTdgjny6LwyOO6QRc1QcLkVzy677Lqqw=";
   };
 
   buildInputs = lib.optionals stdenv.isDarwin [
