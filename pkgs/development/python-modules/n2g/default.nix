fb09c57f7792ae643dff403541965dfb66f71bac
Jonas Heinrich <onny@project-insanity.org>
2022-08-13 11:30:01 +0200

    python310Packages.n2g: init at 0.3.2

--- pkgs/development/python-modules/n2g/default.nix
+++ pkgs/development/python-modules/n2g/default.nix
@@ -1,18 +1,32 @@
-{ lib, fetchPypi, buildPythonPackage }:
+{ lib
+, fetchFromGitHub
+, buildPythonPackage
+, poetry
+, pytestCheckHook
+}:
 
 buildPythonPackage rec {
   pname = "n2g";
   version = "0.3.2";
+  format = "pyproject";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "0n1k83ww0pr4q6z0h7p8hvy21hcgb96jvgllfbwhvvyf37h3wlll";
+  src = fetchFromGitHub {
+    owner = "dmulyalin";
+    repo = "N2G";
+    rev = version;
+    sha256 = "sha256-Ur/P8hG0bvZx2AqWEvf1PwWLZRxJ8GduUjuFPIWS4NM=";
   };
 
+  propagatedBuildInputs = [ poetry ];
+
+  checkInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "n2g" ];
+
   meta = with lib; {
-    description = "Ed25519 public-key signatures";
-    homepage = "https://github.com/warner/python-ed25519";
+    description = "Library to generate diagrams in yWorks graphml or drawio formats or JSON data compatible with 3d-force-graph JSON input";
+    homepage = "https://github.com/dmulyalin/N2G";
     license = licenses.mit;
-    maintainers = with maintainers; [ np ];
+    maintainers = with maintainers; [ onny ];
   };
 }
