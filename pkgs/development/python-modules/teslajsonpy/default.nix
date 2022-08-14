6e99500faa4419247a9c14148408b56c67d99c61
Fabian Affolter <mail@fabian-affolter.ch>
2022-08-14 23:11:26 +0200

    python310Packages.teslajsonpy: 2.4.0 -> 2.4.1

--- pkgs/development/python-modules/teslajsonpy/default.nix
+++ pkgs/development/python-modules/teslajsonpy/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "teslajsonpy";
-  version = "2.4.0";
+  version = "2.4.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "zabuldon";
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-BAayVUmp2dsaWzH8dvTjZCKGnpc6uY6Y/6gWYIgaES8=";
+    sha256 = "sha256-YkutsaxPxspIclRlcr5dzk7JKvlX6IXbccca3NLFtSo=";
   };
 
   nativeBuildInputs = [
