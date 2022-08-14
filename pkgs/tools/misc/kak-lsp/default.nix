96c5cdbefe26d8f9c3459d7f36afd42ee97d6836
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-14 11:42:19 +0000

    kak-lsp: 12.2.0 -> 13.0.0

--- pkgs/tools/misc/kak-lsp/default.nix
+++ pkgs/tools/misc/kak-lsp/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "kak-lsp";
-  version = "12.2.0";
+  version = "13.0.0";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-Il3eF9bVrAaJkTDPB1DzEjROnJxIAnnk27qdT9qsp1k";
+    sha256 = "sha256-i+oi5lCNIWPVPP5o7IamSVPvCB/FhO/v08zOipmaE+c=";
   };
 
-  cargoSha256 = "sha256-wRjPjCKsvqnJkybNVAdVMgBA9RaviFyCJPv3D5hipSs";
+  cargoSha256 = "sha256-acxdGhExfE4g1YNp3bRQlEuUqz79y1IhDnqTaYh/nxc=";
 
   buildInputs = lib.optionals stdenv.isDarwin [ Security SystemConfiguration ];
 
