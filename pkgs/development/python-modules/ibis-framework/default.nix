37ef4030062204ca0ba1bfd316f18d773aacc789
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-08-14 05:56:14 -0400

    python3Packages.ibis-framework: 3.0.2 -> 3.1.0

--- pkgs/development/python-modules/ibis-framework/default.nix
+++ pkgs/development/python-modules/ibis-framework/default.nix
@@ -5,7 +5,6 @@
 , pythonOlder
 , pytestCheckHook
 , atpublic
-, cached-property
 , click
 , clickhouse-cityhash
 , clickhouse-driver
@@ -13,10 +12,10 @@
 , datafusion
 , duckdb
 , duckdb-engine
+, filelock
 , geoalchemy2
 , geopandas
 , graphviz-nox
-, importlib-metadata
 , lz4
 , multipledispatch
 , numpy
@@ -37,6 +36,7 @@
 , python
 , pytz
 , regex
+, rsync
 , shapely
 , sqlalchemy
 , sqlite
@@ -55,14 +55,14 @@ let
   ibisTestingData = fetchFromGitHub {
     owner = "ibis-project";
     repo = "testing-data";
-    rev = "a88a4b3c3b54a88e7f77e59de70f5bf20fb62f19";
-    sha256 = "sha256-BnRhVwPcWFwiBJ2ySgiiuUdnF4gesnTq1/dLcuvc868=";
+    rev = "3c39abfdb4b284140ff481e8f9fbb128b35f157a";
+    sha256 = "sha256-BZWi4kEumZemQeYoAtlUSw922p+R6opSWp/bmX0DjAo=";
   };
 in
 
 buildPythonPackage rec {
   pname = "ibis-framework";
-  version = "3.0.2";
+  version = "3.1.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -71,14 +71,15 @@ buildPythonPackage rec {
     repo = "ibis";
     owner = "ibis-project";
     rev = version;
-    hash = "sha256-7ywDMAHQAl39kiHfxVkq7voUEKqbb9Zq8qlaug7+ukI=";
+    hash = "sha256-/mQWQLiJa1DRZiyiA6F0/lMyn3wSY1IUwJl2S0IFkvs=";
   };
 
   patches = [
     (fetchpatch {
-      url = "https://github.com/ibis-project/ibis/commit/a6f64c6c32b49098d39bb205952cbce4bdfea657.patch";
-      sha256 = "sha256-puVMjiJXWk8C9yhuXPD9HKrgUBYcYmUPacQz5YO5xYQ=";
-      includes = [ "pyproject.toml" ];
+      name = "xfail-datafusion-0.4.0";
+      url = "https://github.com/ibis-project/ibis/compare/c162abba4df24e0d531bd2e6a3be3109c16b43b9...6219d6caee19b6fd3171983c49cd8d6872e3564b.patch";
+      hash = "sha256-pCYPntj+TwzqCtYWRf6JF5/tJC4crSXHp0aepRocHck=";
+      excludes = ["poetry.lock"];
     })
   ];
 
@@ -86,8 +87,6 @@ buildPythonPackage rec {
 
   propagatedBuildInputs = [
     atpublic
-    cached-property
-    importlib-metadata
     multipledispatch
     numpy
     packaging
@@ -104,14 +103,17 @@ buildPythonPackage rec {
   checkInputs = [
     pytestCheckHook
     click
+    filelock
     pytest-benchmark
     pytest-mock
     pytest-randomly
     pytest-xdist
+    rsync
   ] ++ lib.concatMap (name: passthru.optional-dependencies.${name}) testBackends;
 
   preBuild = ''
     # setup.py exists only for developer convenience and is automatically generated
+    # it gets in the way in nixpkgs so we remove it
     rm setup.py
   '';
 
@@ -119,6 +121,10 @@ buildPythonPackage rec {
     "--dist=loadgroup"
     "-m"
     "'${lib.concatStringsSep " or " testBackends} or core'"
+    # this test fails on nixpkgs datafusion version (0.4.0), but works on
+    # datafusion 0.6.0
+    "-k"
+    "'not datafusion-no_op'"
   ];
 
   preCheck = ''
@@ -127,16 +133,8 @@ buildPythonPackage rec {
     export IBIS_TEST_DATA_DIRECTORY
     IBIS_TEST_DATA_DIRECTORY="$(mktemp -d)"
 
-    # copy the test data to a writable directory
-    cp -r ${ibisTestingData}/* "$IBIS_TEST_DATA_DIRECTORY"
-
-    find "$IBIS_TEST_DATA_DIRECTORY" -type d -exec chmod u+rwx {} +
-    find "$IBIS_TEST_DATA_DIRECTORY" -type f -exec chmod u+rw {} +
-
-    # load data
-    for backend in ${lib.concatStringsSep " " testBackends}; do
-      ${python.interpreter} ci/datamgr.py load "$backend"
-    done
+    # copy the test data to a directory
+    rsync --chmod=Du+rwx,Fu+rw --archive "${ibisTestingData}/" "$IBIS_TEST_DATA_DIRECTORY"
   '';
 
   postCheck = ''
