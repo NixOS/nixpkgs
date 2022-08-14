0c4ffbfa61396934947dbb5ca561752825e015a9
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-14 00:35:10 +0000

    swaynotificationcenter: 0.6.1 -> 0.6.3

--- pkgs/applications/misc/swaynotificationcenter/default.nix
+++ pkgs/applications/misc/swaynotificationcenter/default.nix
@@ -21,13 +21,13 @@
 
 stdenv.mkDerivation rec {
   pname = "SwayNotificationCenter";
-  version = "0.6.1";
+  version = "0.6.3";
 
   src = fetchFromGitHub {
     owner = "ErikReider";
     repo = "SwayNotificationCenter";
     rev = "v${version}";
-    hash = "sha256-+vMlhBCLxvqfPRO2U9015srhY/2sd1DoV27kzNDjsqs=";
+    hash = "sha256-79Kda2Mi2r38f0J12bRm9wbHiZCy9+ojPDxwlFG8EYw=";
   };
 
   nativeBuildInputs = [ gobject-introspection meson ninja pkg-config scdoc vala wrapGAppsHook ];
