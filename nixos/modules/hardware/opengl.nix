9ceed2ee98b644ba464efc4a0a9cd86149804ec1
Juuso Haavisto <juuso@mail.com>
2022-08-14 10:36:30 +0300

    use kernelConfig over kernel features to determine emulation capabilities

--- nixos/modules/hardware/opengl.nix
+++ nixos/modules/hardware/opengl.nix
@@ -129,7 +129,7 @@ in
       { assertion = cfg.driSupport32Bit -> pkgs.stdenv.isx86_64;
         message = "Option driSupport32Bit only makes sense on a 64-bit system.";
       }
-      { assertion = cfg.driSupport32Bit -> (config.boot.kernelPackages.kernel.features.ia32Emulation or false);
+      { assertion = cfg.driSupport32Bit -> (with config.lib.kernelConfig; [(isEnabled "IA32_EMULATION")] or false);
         message = "Option driSupport32Bit requires a kernel that supports 32bit emulation";
       }
     ];
