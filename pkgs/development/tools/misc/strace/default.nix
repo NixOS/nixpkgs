e3f61ba99275acab18d716f3b39119a24c92a549
Sergei Trofimovich <slyich@gmail.com>
2022-08-14 12:42:49 +0100

    strace: 5.18 -> 5.19
    While at it added trivial updater.
    Changes: https://github.com/strace/strace/releases/tag/v5.19

--- pkgs/development/tools/misc/strace/default.nix
+++ pkgs/development/tools/misc/strace/default.nix
@@ -1,12 +1,12 @@
-{ lib, stdenv, fetchurl, perl, libunwind, buildPackages }:
+{ lib, stdenv, fetchurl, perl, libunwind, buildPackages, gitUpdater }:
 
 stdenv.mkDerivation rec {
   pname = "strace";
-  version = "5.18";
+  version = "5.19";
 
   src = fetchurl {
     url = "https://strace.io/files/${version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-YCk+p5rJJT1gDNyb4HetKYjKIihKQ5yeZr5RUNs9EYc=";
+    sha256 = "sha256-qj3ByOYOT2/z05ZRSqJH88e/cZ2KjcTdT6eTvnhr7KM=";
   };
 
   depsBuildBuild = [ buildPackages.stdenv.cc ];
@@ -17,6 +17,13 @@ stdenv.mkDerivation rec {
   buildInputs = [ libunwind ]; # support -k
 
   configureFlags = [ "--enable-mpers=check" ];
+
+  passthru.updateScript = gitUpdater {
+    inherit pname version;
+    # No nicer place to find latest release.
+    url = "https://github.com/strace/strace.git";
+    rev-prefix = "v";
+  };
 
   meta = with lib; {
     homepage = "https://strace.io/";
