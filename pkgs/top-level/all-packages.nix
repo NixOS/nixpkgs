ffc279b64c0b026ee22eda01f586ad5383623f0b
oluceps <i@oluceps.uk>
2022-08-15 11:28:28 +0800

    hysteria: 1.1.0 -> 1.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4009,6 +4009,8 @@ with pkgs;
     });
   };
 
+  hysteria = callPackage ../tools/networking/hysteria { };
+
   hyx = callPackage ../tools/text/hyx { };
 
   icdiff = callPackage ../tools/text/icdiff {};
