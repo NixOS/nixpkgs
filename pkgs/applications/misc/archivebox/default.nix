3dddd5d83207b22985a2377fe74982a469f5f312
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-08-15 19:01:17 +0200

    archivebox: relax django3 constraint
    Doing overrides like that prevents us from shipping django security
    updates, which is how these CVEs accumulated here.

--- pkgs/applications/misc/archivebox/default.nix
+++ pkgs/applications/misc/archivebox/default.nix
@@ -2,41 +2,21 @@
 , python3
 }:
 
-let
-  python = python3.override {
-    packageOverrides = self: super: {
-      django = super.django_3.overridePythonAttrs (old: rec {
-        version = "3.1.14";
-        src = old.src.override {
-          inherit version;
-          sha256 = "72a4a5a136a214c39cf016ccdd6b69e2aa08c7479c66d93f3a9b5e4bb9d8a347";
-        };
-        meta = old.meta // {
-          knownVulnerabilities = [
-            "CVE-2021-45115"
-            "CVE-2021-45116"
-            "CVE-2021-45452"
-            "CVE-2022-23833"
-            "CVE-2022-22818"
-            "CVE-2022-28347"
-            "CVE-2022-28346"
-          ];
-        };
-      });
-    };
-  };
-in
-
-python.pkgs.buildPythonApplication rec {
+python3.pkgs.buildPythonApplication rec {
   pname = "archivebox";
   version = "0.6.2";
 
-  src = python.pkgs.fetchPypi {
+  src = python3.pkgs.fetchPypi {
     inherit pname version;
     sha256 = "sha256-zHty7lTra6yab9d0q3EqsPG3F+lrnZL6PjQAbL1A2NY=";
   };
 
-  propagatedBuildInputs = with python.pkgs; [
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace "django>=3.1.3,<3.2" "django<4"
+  '';
+
+  propagatedBuildInputs = with python3.pkgs; [
     requests
     mypy-extensions
     django
@@ -48,6 +28,9 @@ python.pkgs.buildPythonApplication rec {
     w3lib
     ipython
   ];
+
+  # TODO: implement tests
+  doCheck = false;
 
   meta = with lib; {
     description = "Open source self-hosted web archiving";
