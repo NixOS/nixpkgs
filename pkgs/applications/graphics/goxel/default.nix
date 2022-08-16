89daf6f4ecae4144b47fbfe18ac4fa14dc2d856d
Francesco Gazzetta <fgaz@fgaz.me>
2022-08-16 11:36:00 +0200

    goxel: 0.10.8 -> 0.11.0
    https://github.com/guillaumechereau/goxel/releases/tag/v0.11.0

--- pkgs/applications/graphics/goxel/default.nix
+++ pkgs/applications/graphics/goxel/default.nix
@@ -3,20 +3,19 @@
 
 stdenv.mkDerivation rec {
   pname = "goxel";
-  version = "0.10.8";
+  version = "0.11.0";
 
   src = fetchFromGitHub {
     owner = "guillaumechereau";
     repo = "goxel";
     rev = "v${version}";
-    sha256 = "sha256-M9H9SV8xmU7Jw5rEdV0gfloIEBvWmWSuH+BCrowpf2M=";
+    sha256 = "sha256-FrPoKJXzWzfUF0ITYxIUdHi0kCh2VJ+A093HaXxMofA=";
   };
 
   patches = [ ./disable-imgui_ini.patch ];
 
   nativeBuildInputs = [ scons pkg-config wrapGAppsHook ];
   buildInputs = [ glfw3 gtk3 libpng12 ];
-  NIX_LDFLAGS = "-lpthread";
 
   buildPhase = ''
     make release
