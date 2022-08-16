d0545e25a4dc33e39806cd934bdb02ba6da7f816
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-16 20:21:47 +0000

    python310Packages.pymavlink: 2.4.31 -> 2.4.33

--- pkgs/development/python-modules/pymavlink/default.nix
+++ pkgs/development/python-modules/pymavlink/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "pymavlink";
-  version = "2.4.31";
+  version = "2.4.33";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-p7cvwMpW1fS9Ml72vsD9L35wqPjtsQHW5lclw5SJ4P0=";
+    sha256 = "sha256-YGoKKcKSGTTowNlrT8Vxo8jYM2GwoQCj+qIsx3/HwxM=";
   };
 
   propagatedBuildInputs = [ future lxml ];
