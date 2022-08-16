1af6f5a2220b2d1a88a1a8c8bf7e9972b3315c89
Grace Dinh <git@contact.dinh.ai>
2022-08-16 15:59:41 -0700

    hmat-oss: init at 1.7.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18479,6 +18479,8 @@ with pkgs;
     autoreconfHook = buildPackages.autoreconfHook269;
   };
 
+  hmat-oss = callPackage ../development/libraries/hmat-oss { };
+
   hound = callPackage ../development/tools/misc/hound { };
 
   hpx = callPackage ../development/libraries/hpx {
