324494135392d8e9ff26a70b970165f86731e37c
Robert Schütz <nix@dotlambda.de>
2022-08-17 11:15:44 +0000

    python310Packages.keyring: 23.7.0 -> 23.8.2
    https://github.com/jaraco/keyring/blob/v23.8.2/CHANGES.rst

--- pkgs/development/python-modules/keyring/default.nix
+++ pkgs/development/python-modules/keyring/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "keyring";
-  version = "23.7.0";
+  version = "23.8.2";
   disabled = pythonOlder "3.7";
 
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-eC4c0RMukb9Fn80kO88lsyYBXBrAsZjkQI+R+meRBis=";
+    sha256 = "0d9973f8891850f1ade5f26aafd06bb16865fbbae3fc56b0defb6a14a2624003";
   };
 
   nativeBuildInputs = [
@@ -56,6 +56,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Store and access your passwords safely";
     homepage    = "https://github.com/jaraco/keyring";
+    changelog   = "https://github.com/jaraco/keyring/blob/v${version}/CHANGES.rst";
     license     = licenses.mit;
     maintainers = with maintainers; [ lovek323 dotlambda ];
     platforms   = platforms.unix;
