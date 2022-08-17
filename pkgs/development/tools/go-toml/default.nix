a10b7b89713e1f160ebf41d2f200537afbda0378
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-17 11:07:42 +0000

    go-toml: 2.0.2 -> 2.0.3

--- pkgs/development/tools/go-toml/default.nix
+++ pkgs/development/tools/go-toml/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "go-toml";
-  version = "2.0.2";
+  version = "2.0.3";
 
   src = fetchFromGitHub {
     owner = "pelletier";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-lZUM31lA6l35EHEZnw6i+WR7qBo692RvlOBkxxBq6Vs=";
+    sha256 = "sha256-roEJMaRalvk/XT1f15R4DPnlkxo3hPDHdzOfDtZAa8Y=";
   };
 
-  vendorSha256 = "sha256-/F/ZbeNkiiO2+QibpoKUi1kC3Wv5Jujx6r468irlea0=";
+  vendorSha256 = "sha256-yDPCfJtYty4aaoDrn3UWFcs1jHJHMJqzc5f06AWQmRc=";
 
   excludedPackages = [ "cmd/gotoml-test-decoder" "cmd/tomltestgen" ];
 
