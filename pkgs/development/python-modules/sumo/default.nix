0c39386f13442bd3d95d2a51ab53af067d308e03
Jonas Heinrich <onny@project-insanity.org>
2022-08-18 13:11:35 +0200

    python310Packages.sumo: 2.3.2 -> 2.3.3

--- pkgs/development/python-modules/sumo/default.nix
+++ pkgs/development/python-modules/sumo/default.nix
@@ -13,11 +13,12 @@
 , spglib
 , castepxbin
 , pytestCheckHook
+, colormath
 }:
 
 buildPythonPackage rec {
   pname = "sumo";
-  version = "2.3.2";
+  version = "2.3.3";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -26,38 +27,36 @@ buildPythonPackage rec {
     owner = "SMTG-UCL";
     repo = "sumo";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-hY1rQG4s5j/lVvu5e+5e+GamKrYpviqxaWmq1qB6ejU=";
+    sha256 = "sha256-XEs4lLbVwN00UDnDC0kUNlut9RFXPfcyevBA1f1UqSU=";
   };
+
+  postPatch = ''
+    # Loosen castepxbin dependency version pinning
+    # https://github.com/SMTG-UCL/sumo/issues/173
+    substituteInPlace setup.py \
+      --replace "castepxbin==0.1.0" "castepxbin>=0.1.0"
+  '';
 
   nativeBuildInputs = [
     cython
   ];
 
   propagatedBuildInputs = [
-    spglib
-    numpy
-    scipy
-    h5py
-    pymatgen
-    phonopy
-    matplotlib
-    seekpath
     castepxbin
+    colormath
+    h5py
+    matplotlib
+    numpy
+    phonopy
+    pymatgen
+    scipy
+    seekpath
+    spglib
   ];
 
   checkInputs = [
     pytestCheckHook
   ];
-
-  disabledTests = [
-    # slight disagreement between caastepxbin versions
-    "test_castep_phonon_read_bands"
-  ];
-
-  postPatch = ''
-    substituteInPlace setup.py \
-      --replace "castepxbin==0.1.0" "castepxbin>=0.1.0"
-  '';
 
   pythonImportsCheck = [
     "sumo"
