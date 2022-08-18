3d20e2fcdf0042181459b0221c94d362f01a1fa1
jacobi petrucciani <j@cobi.dev>
2022-08-18 10:13:50 -0400

    python3Packages.bucketstore: init at 0.2.2

---
+++ pkgs/development/python-modules/bucketstore/default.nix
@@ -0,0 +1,38 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, pythonOlder
+, pytestCheckHook
+, boto3
+, moto
+}:
+
+buildPythonPackage rec {
+  pname = "bucketstore";
+  version = "0.2.2";
+  disabled = pythonOlder "3.7";
+
+  src = fetchFromGitHub {
+    owner = "jpetrucciani";
+    repo = "bucketstore";
+    rev = version;
+    sha256 = "sha256-BtoyGqFbeBhGQeXnmeSfiuJLZtXFrK26WO0SDlAtKG4=";
+  };
+
+  checkInputs = [
+    pytestCheckHook
+    moto
+  ];
+  propagatedBuildInputs = [ boto3 ];
+
+  pythonImportsCheck = [
+    "bucketstore"
+  ];
+
+  meta = with lib; {
+    description = "Library for interacting with Amazon S3";
+    homepage = "https://github.com/jpetrucciani/bucketstore";
+    license = licenses.mit;
+    maintainers = with maintainers; [ jpetrucciani ];
+  };
+}
