329d99f48496345abad18b0f5fd21d5e7e643d19
Winter <winter@winter.cafe>
2022-08-17 22:38:51 -0400

    dendrite: 0.8.5 -> 0.9.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21738,7 +21738,9 @@ with pkgs;
 
   dcnnt = python3Packages.callPackage ../servers/dcnnt { };
 
-  dendrite = callPackage ../servers/dendrite { };
+  dendrite = callPackage ../servers/dendrite {
+    buildGoModule = buildGo118Module;
+  };
 
   dex-oidc = callPackage ../servers/dex { };
 
