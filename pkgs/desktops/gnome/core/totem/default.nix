ec089c92ea04a05aefe153bbc21dfa03c5e8d773
Jan Tojnar <jtojnar@gmail.com>
2022-08-18 16:58:39 +0000

    gnome.totem: 43.alpha → 43.beta
    - Requires libx11 1.8
    
    https://gitlab.gnome.org/GNOME/totem/-/compare/43.alpha...43.beta
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/totem/default.nix
+++ pkgs/desktops/gnome/core/totem/default.nix
@@ -30,11 +30,11 @@
 
 stdenv.mkDerivation rec {
   pname = "totem";
-  version = "43.alpha";
+  version = "43.beta";
 
   src = fetchurl {
     url = "mirror://gnome/sources/totem/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "9pKUwVa0vZU+AQBdY+M+RWOTX5kmMDj65LxNlMEOuII=";
+    sha256 = "qkgMw4JT7Y07M0/qfj550hQ/J6N27dXbZjvIccKGSHA=";
   };
 
   nativeBuildInputs = [
