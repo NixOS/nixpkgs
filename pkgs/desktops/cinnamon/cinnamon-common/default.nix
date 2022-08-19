8ec26bcfa5b27e4e7d04b5be92582a4f10648b67
Bobby Rong <rjl931189261@126.com>
2022-08-19 18:21:06 +0800

    cinnamon.cinnamon-common: ensure xapp is available for cinnamon-desktop-editor
    Closes #129946
    Closes #177041
    
    (cherry picked from commit d00b39e1747295b6be6b3dce7a64a80c6e3503da)

--- pkgs/desktops/cinnamon/cinnamon-common/default.nix
+++ pkgs/desktops/cinnamon/cinnamon-common/default.nix
@@ -121,6 +121,7 @@ stdenv.mkDerivation rec {
     wrapGAppsHook
     intltool
     gtk-doc
+    python3.pkgs.wrapPython
   ];
 
   # use locales from cinnamon-translations (not using --localedir because datadir is used)
@@ -161,6 +162,10 @@ stdenv.mkDerivation rec {
     gappsWrapperArgs+=(
       --prefix XDG_DATA_DIRS : "${gnome.caribou}/share"
     )
+
+    # https://github.com/NixOS/nixpkgs/issues/129946
+    buildPythonPath "${python3.pkgs.xapp}"
+    patchPythonScript $out/share/cinnamon/cinnamon-desktop-editor/cinnamon-desktop-editor.py
   '';
 
   passthru = {
