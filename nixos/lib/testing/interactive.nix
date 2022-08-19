312497649b0d27a7c2157a7db3fd3c218d3792d6
Robert Hensing <robert@roberthensing.nl>
2022-08-19 12:09:58 +0200

    nixos/testing: Improve interactive docs

--- nixos/lib/testing/interactive.nix
+++ nixos/lib/testing/interactive.nix
@@ -6,11 +6,31 @@ in
   options = {
     interactive = mkOption {
       description = mdDoc ''
-        Tests [can be run interactively](#sec-running-nixos-tests-interactively).
+        Tests [can be run interactively](#sec-running-nixos-tests-interactively)
+        using the program in the test derivation's `.driverInteractive` attribute.
 
         When they are, the configuration will include anything set in this submodule.
 
         You can set any top-level test option here.
+
+        Example test module:
+
+        ```nix
+        { config, lib, ... }: {
+
+          nodes.rabbitmq = {
+            services.rabbitmq.enable = true;
+          };
+
+          # When running interactively ...
+          interactive.nodes.rabbitmq = {
+            # ... enable the web ui.
+            services.rabbitmq.managementPlugin.enable = true;
+          };
+        }
+        ```
+
+        For details, see the section about [running tests interactively](#sec-running-nixos-tests-interactively).
       '';
       type = moduleType;
       visible = "shallow";
