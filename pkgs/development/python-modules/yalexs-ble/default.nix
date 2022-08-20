460754f583e50b5ee9ad3c7f439c11ff8b68cfa9
Fabian Affolter <mail@fabian-affolter.ch>
2022-08-20 09:43:14 +0200

    python310Packages.yalexs-ble: 1.5.0 -> 1.6.0

--- pkgs/development/python-modules/yalexs-ble/default.nix
+++ pkgs/development/python-modules/yalexs-ble/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "yalexs-ble";
-  version = "1.5.0";
+  version = "1.6.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "bdraco";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-nRrJhS2E0j73RNRr+fyxWJISyAB+KSUbBzWejsu/km0=";
+    hash = "sha256-OL8waDz4bhY5rtKJXVzu0UeTJfC0eX8bEdfD7zkE/PE=";
   };
 
   nativeBuildInputs = [
