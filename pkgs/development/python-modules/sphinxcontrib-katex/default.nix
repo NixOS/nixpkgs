289cbd9b66f4d0d5f0c1b2519eea190804fbcfca
Fabian Affolter <mail@fabian-affolter.ch>
2022-08-20 08:55:50 +0200

    python310Packages.sphinxcontrib-katex: disable on older Python releases

--- pkgs/development/python-modules/sphinxcontrib-katex/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-katex/default.nix
@@ -1,23 +1,32 @@
-{ lib, buildPythonPackage, fetchPypi, pythonOlder, sphinx }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, pythonOlder
+, sphinx
+}:
 
 buildPythonPackage rec {
   pname = "sphinxcontrib-katex";
   version = "0.9.0";
+  format = "setuptools";
 
-  # pkgutil namespaces are broken in nixpkgs (because they can't scan multiple
-  # directories). But python2 is EOL, so not supporting it should be ok.
-  disabled = pythonOlder "3";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-HFs1+9tWl1D5VWY14dPCk+Ewv+ubedhd9DcCSrPQZnQ=";
+    hash = "sha256-HFs1+9tWl1D5VWY14dPCk+Ewv+ubedhd9DcCSrPQZnQ=";
   };
 
-  propagatedBuildInputs = [ sphinx ];
+  propagatedBuildInputs = [
+    sphinx
+  ];
 
   # There are no unit tests
   doCheck = false;
-  pythonImportsCheck = [ "sphinxcontrib.katex" ];
+
+  pythonImportsCheck = [
+    "sphinxcontrib.katex"
+  ];
 
   meta = with lib; {
     description = "Sphinx extension using KaTeX to render math in HTML";
