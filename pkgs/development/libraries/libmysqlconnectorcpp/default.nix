08a27f789cb4990beaf640559b24218fc953c40b
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-20 22:48:35 +0000

    libmysqlconnectorcpp: 8.0.29 -> 8.0.30

--- pkgs/development/libraries/libmysqlconnectorcpp/default.nix
+++ pkgs/development/libraries/libmysqlconnectorcpp/default.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation rec {
   pname = "libmysqlconnectorcpp";
-  version = "8.0.29";
+  version = "8.0.30";
 
   src = fetchurl {
     url = "https://cdn.mysql.com/Downloads/Connector-C++/mysql-connector-c++-${version}-src.tar.gz";
-    sha256 = "sha256-mmI2oovKM66VHYzKq7j/UaGIhj6FmfkJb0rgodoZ+H8=";
+    sha256 = "sha256-WyzuvjmG/m1rDG8ptpEss6HKv5mNLExBJ0UnaN51qws=";
   };
 
   nativeBuildInputs = [
