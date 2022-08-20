744963c24aa1121e461aa6f0a64184905916ae0b
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-08-21 01:10:33 +0200

    borgbackup: move manpages into man output
    By creating the man output the multiple-outputs hook will automatically
    move manpages in the correct path into it.

--- pkgs/tools/backup/borgbackup/default.nix
+++ pkgs/tools/backup/borgbackup/default.nix
@@ -120,7 +120,7 @@ python3.pkgs.buildPythonApplication rec {
     inherit (nixosTests) borgbackup;
   };
 
-  outputs = [ "out" "doc" ];
+  outputs = [ "out" "doc" "man" ];
 
   meta = with lib; {
     description = "Deduplicating archiver with compression and encryption";
