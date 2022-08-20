1d8db1854d09197a13416b5bf0fbda43945cb5c5
Jayesh Bhoot <jayesh@bhoot.sh>
2022-08-20 22:34:25 +0530

    promise_jsoo: make derivation more readable

--- pkgs/development/ocaml-modules/promise_jsoo/default.nix
+++ pkgs/development/ocaml-modules/promise_jsoo/default.nix
@@ -1,8 +1,5 @@
 { lib, fetchurl, buildDunePackage, ocaml, js_of_ocaml, ppxlib, js_of_ocaml-ppx, gen_js_api }:
 
-let
-  owner = "mnxn";
-in
 buildDunePackage rec {
   pname = "promise_jsoo";
   version = "0.3.1";
@@ -10,7 +7,7 @@ buildDunePackage rec {
   duneVersion = "3";
 
   src = fetchurl {
-    url = "https://github.com/${owner}/${pname}/releases/download/v${version}/${pname}-v${version}.tbz";
+    url = "https://github.com/mnxn/promise_jsoo/releases/download/v${version}/promise_jsoo-v${version}.tbz";
     sha256 = "00pjnsbv0yv3hhxbbl8dsljgr95kjgi9w8j1x46gjyxg9zayrxzl";
   };
 
@@ -22,10 +19,9 @@ buildDunePackage rec {
   ];
 
   meta = {
-    homepage = "https://github.com/${owner}/${pname}";
+    homepage = "https://github.com/mnxn/promise_jsoo";
     description = "Js_of_ocaml bindings to JS Promises with supplemental functions";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ jayesh-bhoot ];
   };
 }
-
