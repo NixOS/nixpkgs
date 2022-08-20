dec98445de051d3cfd13da67c0865e2db2c0366d
riceicetea <104317939+riceicetea@users.noreply.github.com>
2022-08-20 07:10:58 +0000

    Update with changes from SuperSandro2000's review

--- pkgs/applications/misc/wofi-emoji/default.nix
+++ pkgs/applications/misc/wofi-emoji/default.nix
@@ -16,13 +16,13 @@ stdenv.mkDerivation rec {
     sha256 = "1wq276bhf9x24ds13b2dwa69cjnr207p6977hr4bsnczryg609rh";
   };
 
-  buildInputs = [ wofi wtype wl-clipboard ];
   nativeBuildInputs = [ jq ];
+  buildInputs = [ wofi wtype wl-clipboard ];
 
   postPatch = ''
     substituteInPlace build.sh \
-      --replace 'curl https://raw.githubusercontent.com/muan/emojilib/v3.0.6/dist/emoji-en-US.json' 'cat emoji-en-US.json'
-  '';
+      --replace 'curl ${emojiJSON.url}' 'cat emoji-en-US.json'
+      '';
 
   buildPhase = ''
     runHook preBuild
