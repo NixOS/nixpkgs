9f54f1d09e21a46c719801ff780fc70e006b3913
Ivan Kozik <ivan@ludios.org>
2022-08-20 03:51:09 +0000

    rsync: 3.2.4 -> 3.2.5
    This release fixes CVE-2022-29154:
    https://download.samba.org/pub/rsync/NEWS#3.2.5
    
    Remove enableCopyDevicesPatch because --copy-devices was included in rsync 3.2.4:
    https://download.samba.org/pub/rsync/NEWS#3.2.4:~:text=Added%20the%20%2D%2Dcopy%2Ddevices%20option
    
    (cherry picked from commit 457e2672066c8dc0df2df1a3193139c659581639)

--- pkgs/applications/networking/sync/rsync/default.nix
+++ pkgs/applications/networking/sync/rsync/default.nix
@@ -1,7 +1,6 @@
 { lib
 , stdenv
 , fetchurl
-, fetchpatch
 , perl
 , libiconv
 , zlib
@@ -16,27 +15,18 @@
 , xxHash
 , enableZstd ? true
 , zstd
-, enableCopyDevicesPatch ? false
 , nixosTests
 }:
 
 stdenv.mkDerivation rec {
   pname = "rsync";
-  version = "3.2.4";
+  version = "3.2.5";
 
-  srcs = [
-    (fetchurl {
-      # signed with key 0048 C8B0 26D4 C96F 0E58  9C2F 6C85 9FB1 4B96 A8C5
-      url = "mirror://samba/rsync/src/rsync-${version}.tar.gz";
-      sha256 = "sha256-b3YYONCAUrC2V5z39nN9k+R/AfTaBMXSTTRHt/Kl+tE=";
-    })
-  ] ++ lib.optional enableCopyDevicesPatch (fetchurl {
+  src = fetchurl {
     # signed with key 0048 C8B0 26D4 C96F 0E58  9C2F 6C85 9FB1 4B96 A8C5
-    url = "mirror://samba/rsync/rsync-patches-${version}.tar.gz";
-    sha256 = "1wj21v57v135n6fnm2m2dxmb9lhrrg62jgkggldp1gb7d6s4arny";
-  });
-
-  patches = lib.optional enableCopyDevicesPatch "./patches/copy-devices.diff";
+    url = "mirror://samba/rsync/src/rsync-${version}.tar.gz";
+    sha256 = "sha256-KsTSFjXN95GGe8N3w1ym3af1DZGaWL5FBX/VFgDGmro=";
+  };
 
   nativeBuildInputs = [ perl ];
 
@@ -69,6 +59,6 @@ stdenv.mkDerivation rec {
     homepage = "https://rsync.samba.org/";
     license = licenses.gpl3Plus;
     platforms = platforms.unix;
-    maintainers = with lib.maintainers; [ ehmry kampfschlaefer ];
+    maintainers = with lib.maintainers; [ ehmry kampfschlaefer ivan ];
   };
 }
