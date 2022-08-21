8032ed26f3903a3c4f74357a4cf13a53dcf9bb19
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-21 20:45:36 +0000

    shellhub-agent: 0.9.5 -> 0.9.6
    (cherry picked from commit 8546d723f3bfb01ef23b2207d9eea4a75a529cc3)

--- pkgs/applications/networking/shellhub-agent/default.nix
+++ pkgs/applications/networking/shellhub-agent/default.nix
@@ -8,13 +8,13 @@
 
 buildGoModule rec {
   pname = "shellhub-agent";
-  version = "0.9.5";
+  version = "0.9.6";
 
   src = fetchFromGitHub {
     owner = "shellhub-io";
     repo = "shellhub";
     rev = "v${version}";
-    sha256 = "EBtQOli8jh5Ful39tR4U/H3KH517LmoYfqVyydQQTpA=";
+    sha256 = "Ag1rwBWeVUBKxnsIGNDwz9UmHwpklwF6UjM8IPudZTs=";
   };
 
   modRoot = "./agent";
