2005fc2f9d115594d5768f373b931df2a7e23e6d
Reed Riley <reed@riley.engineer>
2022-08-20 20:54:31 -0400

    fix randstructSeed support for 5.19

--- pkgs/os-specific/linux/kernel/manual-config.nix
+++ pkgs/os-specific/linux/kernel/manual-config.nix
@@ -127,11 +127,16 @@ let
         # the buildFlags, but that would require also patching the kernel's
         # toplevel Makefile to add a variable export. This would be likely to
         # cause future patch conflicts.
-        if [ -f scripts/gcc-plugins/gen-random-seed.sh ]; then
-          substituteInPlace scripts/gcc-plugins/gen-random-seed.sh \
-            --replace NIXOS_RANDSTRUCT_SEED \
-            $(echo ${randstructSeed}${src} ${configfile} | sha256sum | cut -d ' ' -f 1 | tr -d '\n')
-        fi
+        for file in \
+          scripts/gcc-plugins/gen-random-seed.sh \
+          scripts/gen-randstruct-seed.sh \
+        ;do
+          if [ -f "$file" ]; then
+            substituteInPlace "$file" \
+              --replace NIXOS_RANDSTRUCT_SEED \
+              $(echo ${randstructSeed}${src} ${configfile} | sha256sum | cut -d ' ' -f 1 | tr -d '\n')
+          fi
+        done
 
         patchShebangs scripts
 
