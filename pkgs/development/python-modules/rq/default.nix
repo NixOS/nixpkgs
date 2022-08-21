40cedc109eb5b1f9a571c119d8bddd095466df3f
Fabian Affolter <mail@fabian-affolter.ch>
2022-08-21 16:47:53 +0200

    python310Packages.rq: update disabled

--- pkgs/development/python-modules/rq/default.nix
+++ pkgs/development/python-modules/rq/default.nix
@@ -1,29 +1,42 @@
-{ lib, fetchFromGitHub, buildPythonPackage, isPy27, click, redis }:
+{ lib
+, fetchFromGitHub
+, buildPythonPackage
+, pythonOlder
+, click
+, redis
+}:
 
 buildPythonPackage rec {
   pname = "rq";
   version = "1.11";
-  disabled = isPy27;
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "rq";
     repo = "rq";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-fv+b5WmODaQkd8T+O8MuJ+XVC3dQ5hZwxMHtBBuqQ7Y=";
+    hash = "sha256-fv+b5WmODaQkd8T+O8MuJ+XVC3dQ5hZwxMHtBBuqQ7Y=";
   };
 
-  # test require a running redis rerver, which is something we can't do yet
+  propagatedBuildInputs = [
+    click
+    redis
+  ];
+
+  # Tests require a running Redis rerver
   doCheck = false;
 
-  pythonImportsCheck = [ "rq" ];
-
-  propagatedBuildInputs = [ click redis ];
+  pythonImportsCheck = [
+    "rq"
+  ];
 
   meta = with lib; {
-    description = "A simple, lightweight library for creating background jobs, and processing them";
+    description = "Library for creating background jobs and processing them";
     homepage = "https://github.com/nvie/rq/";
-    maintainers = with maintainers; [ mrmebelman ];
     license = licenses.bsd2;
+    maintainers = with maintainers; [ mrmebelman ];
   };
 }
 
