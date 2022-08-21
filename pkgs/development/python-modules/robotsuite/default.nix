ee08b8ce6a702bdc19cb0e6bd10af93326e328cf
Anthony Roussel <anthony@roussel.dev>
2022-08-21 22:28:55 +0200

    python310Packages.robotsuite: fix deprecated gpl3 license

--- pkgs/development/python-modules/robotsuite/default.nix
+++ pkgs/development/python-modules/robotsuite/default.nix
@@ -32,6 +32,6 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python unittest test suite for Robot Framework";
     homepage = "https://github.com/collective/robotsuite/";
-    license = licenses.gpl3;
+    license = licenses.gpl3Only;
   };
 }
