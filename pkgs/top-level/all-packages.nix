6b45d2a1d2c68a437b28b2a0bdfc28dcd7ae4485
Doron Behar <doron.behar@gmail.com>
2022-08-21 13:11:15 +0300

    video-trimmer: init at 0.7.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11742,6 +11742,8 @@ with pkgs;
     pythonPackages = python3Packages;
   };
 
+  video-trimmer = callPackage ../applications/video/video-trimmer { };
+
   via = callPackage ../tools/misc/via {};
 
   vial = callPackage ../tools/misc/vial {};
