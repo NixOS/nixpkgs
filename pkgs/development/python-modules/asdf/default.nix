f54e0689235d518d29d5620d8b5a8641d3ac066a
Fabian Affolter <mail@fabian-affolter.ch>
2022-08-21 10:03:44 +0200

    python310Packages.asdf: update disabled

--- pkgs/development/python-modules/asdf/default.nix
+++ pkgs/development/python-modules/asdf/default.nix
@@ -23,7 +23,7 @@ buildPythonPackage rec {
   version = "2.13.0";
   format = "pyproject";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
