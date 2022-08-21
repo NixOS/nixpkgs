fab54f268a8b0a966a9e07b2d453df529aacbcd5
Jacek Generowicz <jacg@my-post-office.net>
2022-08-21 11:02:52 +0200

    Fetch tarball from web.archive.org

--- pkgs/applications/office/banana-accounting/default.nix
+++ pkgs/applications/office/banana-accounting/default.nix
@@ -16,11 +16,11 @@ stdenv.mkDerivation {
   version = "10.0.12";
 
   srcs = fetchurl {
-    url = "https://www.banana.ch/accounting/files/bananaplus/exe/bananaplus.tgz";
+    url = "https://web.archive.org/web/20220821013214/https://www.banana.ch/accounting/files/bananaplus/exe/bananaplus.tgz";
     hash = "sha256-Xs7K/Z6qM1fKKfYMkwAGznNR0Kt/gY7qTr8ZOriIdYw=";
   };
 
-  sourceRoot = "./";
+  sourceRoot = ".";
 
   dontConfigure = true;
   dontBuild = true;
