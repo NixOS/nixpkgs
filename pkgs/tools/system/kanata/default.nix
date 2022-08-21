b1c5515a1ce89226431804984143e917db479f51
Lin Jian <me@linj.tech>
2022-08-21 09:06:28 +0800

    kanata: 1.0.6 -> 1.0.7

--- pkgs/tools/system/kanata/default.nix
+++ pkgs/tools/system/kanata/default.nix
@@ -1,41 +1,26 @@
 { fetchFromGitHub
-, fetchpatch
 , lib
-, libevdev
-, pkg-config
 , rustPlatform
 , withCmd ? false
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "kanata";
-  version = "1.0.6";
+  version = "1.0.7";
 
   src = fetchFromGitHub {
     owner = "jtroo";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-0S27dOwtHxQi5ERno040RWZNo5+ao0ULFwHKJz27wWw=";
+    sha256 = "sha256-2gGFAz0zXea+27T4ayDj6KdoI0ThwXV7U0CspHduTiQ=";
   };
 
-  cargoHash = "sha256-Ge9CiYIl6R8cjfUAY4B9ggjNZv5vpjmQKMPv93wGJwc=";
-
-  cargoPatches = [
-    (fetchpatch {
-      name = "update-cargo.lock-for-1.0.6.patch";
-      url = "https://github.com/jtroo/kanata/commit/29a7669ac230571c30c9113e5c82e8440c8b89af.patch";
-      sha256 = "sha256-s4R7vUFlrL1XTNpgXRyIpIq4rDuM5A85ECzbMUX4MAw=";
-    })
-  ];
+  cargoHash = "sha256-0NvZATdPABIboL5xvmBmDbqPPWvO4mM6wVB3FrOVHIQ=";
 
   buildFeatures = lib.optional withCmd "cmd";
 
-  nativeBuildInputs = [ pkg-config ];
-
-  buildInputs = [ libevdev ];
-
   meta = with lib; {
-    description = "A cross-platform advanced keyboard customization tool";
+    description = "A tool to improve keyboard comfort and usability with advanced customization";
     homepage = "https://github.com/jtroo/kanata";
     license = licenses.lgpl3Only;
     maintainers = with maintainers; [ linj ];
