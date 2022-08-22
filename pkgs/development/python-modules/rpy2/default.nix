411a0f1fb8703af547f1fc478e3c08154e1a1b35
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-22 03:33:00 +0000

    python310Packages.rpy2: 3.5.3 -> 3.5.4

--- pkgs/development/python-modules/rpy2/default.nix
+++ pkgs/development/python-modules/rpy2/default.nix
@@ -24,13 +24,13 @@
 }:
 
 buildPythonPackage rec {
-    version = "3.5.3";
+    version = "3.5.4";
     pname = "rpy2";
 
     disabled = isPyPy;
     src = fetchPypi {
       inherit version pname;
-      sha256 = "sha256-U6CS1ItE9GQo+zDLMVVmTW0vevCOvExF35jfTEWkLMs=";
+      sha256 = "sha256-ugqHeyuW4n0gkTg9RlK4KqInHP9KUFJD1F2kMLcSqvU=";
     };
 
     patches = [
