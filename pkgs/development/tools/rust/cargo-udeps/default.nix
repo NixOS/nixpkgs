147b6003012f23f608b8f8d469e415d2edbaf1c8
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-23 06:12:13 +0000

    cargo-udeps: 0.1.30 -> 0.1.32

--- pkgs/development/tools/rust/cargo-udeps/default.nix
+++ pkgs/development/tools/rust/cargo-udeps/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-udeps";
-  version = "0.1.30";
+  version = "0.1.32";
 
   src = fetchFromGitHub {
     owner = "est31";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-CTP8ENvaryZsoSp+9wx5SoWYXqMpwWfzjMn5kvL+gxk=";
+    sha256 = "sha256-Ev7hLtE5/PqeM39nyWaMyIhFsEZnXbdyU8Q5PET98lQ=";
   };
 
-  cargoSha256 = "sha256-Dzbogu/VqaASz7FEACavvrOIGZK2vn84IjQlCIx/Ey4=";
+  cargoSha256 = "sha256-3+6hZCYiyG6fgyJpjCcY1dzDK0kwVjsyckPIq/8Zfm0=";
 
   nativeBuildInputs = [ pkg-config ];
 
