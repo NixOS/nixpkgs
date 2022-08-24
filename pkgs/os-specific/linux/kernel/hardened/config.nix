99c5d7edbf4976a2045296399f9b069af09a2a18
PedroHLC ☭ <root@pedrohlc.com>
2022-08-24 16:25:54 -0300

    linux_5_4: no REFCOUNT_FULL

--- pkgs/os-specific/linux/kernel/hardened/config.nix
+++ pkgs/os-specific/linux/kernel/hardened/config.nix
@@ -45,7 +45,7 @@ assert (versionAtLeast version "4.9");
   DEBUG_SG              = yes;
   SCHED_STACK_END_CHECK = yes;
 
-  REFCOUNT_FULL = whenBetween "4.13" "5.5" yes;
+  REFCOUNT_FULL = whenBetween "4.13" "5.4" yes;
 
   # Randomize page allocator when page_alloc.shuffle=1
   SHUFFLE_PAGE_ALLOCATOR = whenAtLeast "5.2" yes;
