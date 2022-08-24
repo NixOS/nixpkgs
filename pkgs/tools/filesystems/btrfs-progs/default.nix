2c300d6918825be6954346b02bea208ccd983c2d
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2022-08-24 14:52:59 +0200

    btrfs-progs: 5.18.1 -> 5.19

--- pkgs/tools/filesystems/btrfs-progs/default.nix
+++ pkgs/tools/filesystems/btrfs-progs/default.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation rec {
   pname = "btrfs-progs";
-  version = "5.18.1";
+  version = "5.19";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v${version}.tar.xz";
-    sha256 = "sha256-bpinXM/1LpNU2qGtKExhTEkPhEJzovpSTLrJ64QcclU=";
+    sha256 = "sha256-H7zwbksvgOehJ/1oftRiWlt0+mdP4hLINv9w4O38zPk=";
   };
 
   nativeBuildInputs = [
