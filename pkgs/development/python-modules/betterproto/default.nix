406d2283ccc78bc036b1a108e2356c88ae6da341
Sandro <sandro.jaeckel@gmail.com>
2022-08-24 23:36:41 +0200

    Apply suggestions from code review

--- pkgs/development/python-modules/betterproto/default.nix
+++ pkgs/development/python-modules/betterproto/default.nix
@@ -9,13 +9,14 @@
 , black
 , jinja2
 , isort
-, python3
+, python
 , pytestCheckHook
 , pytest-asyncio
 , pytest-mock
 , tomlkit
 , grpcio-tools
 }:
+
 buildPythonPackage rec {
   pname = "betterproto";
   version = "2.0.0b5";
@@ -58,7 +59,7 @@ buildPythonPackage rec {
   # the protoc-gen-python_betterproto script from the packge to be on PATH.
   preCheck = ''
     export PATH=$PATH:$out/bin
-    ${python3.interpreter} -m tests.generate
+    ${python.interpreter} -m tests.generate
   '';
 
   meta = with lib; {
