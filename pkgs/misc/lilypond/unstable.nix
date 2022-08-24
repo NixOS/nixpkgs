882559e779026a556b622b03a6d7f08d7c2d9b34
usertam <heiyiutam@gmail.com>
2022-08-24 19:04:57 +0800

    lilypond-unstable: 2.23.11 -> 2.23.12

--- pkgs/misc/lilypond/unstable.nix
+++ pkgs/misc/lilypond/unstable.nix
@@ -3,9 +3,9 @@
 (lilypond.override {
   inherit guile;
 }).overrideAttrs (oldAttrs: rec {
-  version = "2.23.11";
+  version = "2.23.12";
   src = fetchurl {
     url = "https://lilypond.org/download/sources/v${lib.versions.majorMinor version}/lilypond-${version}.tar.gz";
-    sha256 = "sha256-4VjcuZvRmpPmiZ42zyk9xYPlsSN6kEsBSRe30P+raQ8=";
+    sha256 = "sha256-SLZ9/Jybltd8+1HANk8pTGHRb7MuZSJJDDY/S4Kwz/k=";
   };
 })
