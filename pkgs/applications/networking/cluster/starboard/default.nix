2a355e25f021c734e519a787ed4f10aec423f55e
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-24 01:08:37 +0000

    starboard: 0.15.4 -> 0.15.8

--- pkgs/applications/networking/cluster/starboard/default.nix
+++ pkgs/applications/networking/cluster/starboard/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "starboard";
-  version = "0.15.4";
+  version = "0.15.8";
 
   src = fetchFromGitHub {
     owner = "aquasecurity";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-GhcHvKqYktBAPB443ZjJ3LYRJZorO7IH1OEnEQKUO0g=";
+    sha256 = "sha256-AgAcN1KtMW7yHi12Gg1M3hu7DQA7WdWtY3qf4K7c1YA=";
     # populate values that require us to use git. By doing this in postFetch we
     # can delete .git afterwards and maintain better reproducibility of the src.
     leaveDotGit = true;
@@ -20,7 +20,7 @@ buildGoModule rec {
       find "$out" -name .git -print0 | xargs -0 rm -rf
     '';
   };
-  vendorSha256 = "sha256-ozbejIFR0mDgCgca+2yaPRiMMHLOEsC9u+kQUe69spc=";
+  vendorSha256 = "sha256-aQrKppIJOCTIDrKvtE6WTkQY8mfYzUh7mBKUd6z19E0=";
 
   nativeBuildInputs = [ installShellFiles ];
 
