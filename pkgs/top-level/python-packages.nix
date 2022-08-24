0dde2e2206ad4326aeb45caf3a91366b2f1b64a6
Jonathan Ringer <jonringer117@gmail.com>
2022-08-23 21:58:57 -0700

    localstack: init at 1.0.4
    closes #50445

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5333,6 +5333,8 @@ in {
 
   localimport = callPackage ../development/python-modules/localimport { };
 
+  localstack = callPackage ../development/python-modules/localstack { };
+
   localstack-client = callPackage ../development/python-modules/localstack-client { };
 
   localstack-ext = callPackage ../development/python-modules/localstack-ext { };
