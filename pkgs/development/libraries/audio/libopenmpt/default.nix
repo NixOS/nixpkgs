4db470acb63e4990c0c5b693561671d4375e62bd
OPNA2608 <christoph.neidahl@gmail.com>
2022-08-24 23:18:02 +0200

    libopenmpt: 0.6.4 -> 0.6.5

--- pkgs/development/libraries/audio/libopenmpt/default.nix
+++ pkgs/development/libraries/audio/libopenmpt/default.nix
@@ -16,13 +16,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libopenmpt";
-  version = "0.6.4";
+  version = "0.6.5";
 
   outputs = [ "out" "dev" "bin" ];
 
   src = fetchurl {
     url = "https://lib.openmpt.org/files/libopenmpt/src/libopenmpt-${version}+release.autotools.tar.gz";
-    sha256 = "4J+4RcMpJwCnrBPDsx1mns072+vL/hMo66I3bOvkAWI=";
+    sha256 = "8iq+l3za5AX2hbdRUOf7FVsseJa0cA/VSr5ohA9m6cA=";
   };
 
   enableParallelBuilding = true;
