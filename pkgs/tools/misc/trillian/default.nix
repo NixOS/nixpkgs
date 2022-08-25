a69d2301a4bdf2e614504b15913e73011d13f6a5
adisbladis <adisbladis@gmail.com>
2022-08-26 01:43:15 +1200

    trillian: re-package using gomod2nix

--- pkgs/tools/misc/trillian/default.nix
+++ pkgs/tools/misc/trillian/default.nix
@@ -1,32 +1,15 @@
 { lib
-, buildGoModule
+, gomod2nix
 , fetchFromGitHub
 }:
 
-buildGoModule rec {
+gomod2nix.buildGoApplication {
   pname = "trillian";
-  version = "1.5.0";
-  vendorSha256 = "sha256-235uQK4E/GLl5XLBd6lkTIgWIjT9MZZGnyfZbOoTFo0=";
-
-  src = fetchFromGitHub {
-    owner = "google";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-XZHVGuIN+5mFbaxOprhdHlpgz2NE2NsJxGWJciDMUqI=";
-  };
-
-  subPackages = [
-    "cmd/trillian_log_server"
-    "cmd/trillian_log_signer"
-    "cmd/createtree"
-    "cmd/deletetree"
-    "cmd/updatetree"
-  ];
-
-  meta = with lib; {
+  pwd = ./.;
+  meta = {
     homepage = "https://github.com/google/trillian";
     description = "A transparent, highly scalable and cryptographically verifiable data store.";
-    license = [ licenses.asl20 ];
-    maintainers = [ maintainers.adisbladis ];
+    license = [ lib.licenses.asl20 ];
+    maintainers = [ lib.maintainers.adisbladis ];
   };
 }
