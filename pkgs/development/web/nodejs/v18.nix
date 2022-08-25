dd845813dbea7850b7d295752d27542f7d58300e
Mario Rodas <marsam@users.noreply.github.com>
2022-08-25 04:20:00 +0000

    nodejs-18_x: 18.7.0 -> 18.8.0
    https://github.com/nodejs/node/releases/tag/v18.8.0

--- pkgs/development/web/nodejs/v18.nix
+++ pkgs/development/web/nodejs/v18.nix
@@ -1,4 +1,4 @@
-{ callPackage, python3, fetchpatch, enableNpm ? true }:
+{ callPackage, python3, enableNpm ? true }:
 
 let
   buildNodejs = callPackage ./nodejs.nix {
@@ -7,17 +7,9 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "18.7.0";
-  sha256 = "sha256-iDSjPJLf5rqJA+ZxXK6qJd/0ZX5wPFTNBuwRNJPiw8I=";
+  version = "18.8.0";
+  sha256 = "sha256-K12YJdBe3mYU8WaKjZfXdP6S68gQiOxf31gYTc48hrk=";
   patches = [
     ./disable-darwin-v8-system-instrumentation.patch
-    # Fix npm silently fail without a HOME directory https://github.com/npm/cli/issues/4996
-    (fetchpatch {
-      url = "https://github.com/npm/cli/commit/9905d0e24c162c3f6cc006fa86b4c9d0205a4c6f.patch";
-      sha256 = "sha256-RlabXWtjzTZ5OgrGf4pFkolonvTDIPlzPY1QcYDd28E=";
-      includes = [ "deps/npm/lib/npm.js" "deps/npm/lib/utils/log-file.js" ];
-      stripLen = 1;
-      extraPrefix = "deps/npm/";
-    })
   ];
 }
