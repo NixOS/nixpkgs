fb39efa0e8de4680fd0e7ac8a3c947abe284c878
Fabian Affolter <mail@fabian-affolter.ch>
2022-08-26 08:43:25 +0200

    python310Packages.qingping-ble: 0.5.0 -> 0.6.0

--- pkgs/development/python-modules/qingping-ble/default.nix
+++ pkgs/development/python-modules/qingping-ble/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "qingping-ble";
-  version = "0.5.0";
+  version = "0.6.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "bluetooth-devices";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-dPF+yo6Ji6z8WnbCNl/VkjiwIJ09FQK/3nkHfIUVbnk=";
+    hash = "sha256-Kf4DYRiDeJtViWlzoP79K94DhWOX6O2YvVTkYhLVkKM=";
   };
 
   nativeBuildInputs = [
