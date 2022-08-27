f3f50f1c2e096d721eeca88f5f3f3c045315d37a
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-27 10:40:57 +0000

    python310Packages.ipyvuetify: 1.8.2 -> 1.8.3

--- pkgs/development/python-modules/ipyvuetify/default.nix
+++ pkgs/development/python-modules/ipyvuetify/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "ipyvuetify";
-  version = "1.8.2";
+  version = "1.8.3";
 
   # GitHub version tries to run npm (Node JS)
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-uFjS7lv8kDRultRqqu2++1eieLs67dLolVurTXWls8A=";
+    sha256 = "sha256-JHeH8AFc3uThLbHV0jQexuHouDcZj+/4u4+dQ9akoq8=";
   };
 
   propagatedBuildInputs = [ ipyvue ];
