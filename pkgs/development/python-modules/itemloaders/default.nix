e723d27a1c8630853893bac4c722f15f606c6fcd
Fabian Affolter <mail@fabian-affolter.ch>
2022-08-28 11:27:16 +0200

    python310Packages.itemloaders: add format

--- pkgs/development/python-modules/itemloaders/default.nix
+++ pkgs/development/python-modules/itemloaders/default.nix
@@ -12,19 +12,27 @@
 buildPythonPackage rec {
   pname = "itemloaders";
   version = "1.0.5";
+  format = "setuptools";
+
   disabled = pythonOlder "3.6";
 
-  # Tests not included in PyPI tarball
   src = fetchFromGitHub {
     owner = "scrapy";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-ueq1Rsuae+wz4eFc1O7luBVR4XWGbefpDr124H6j56g=";
+    hash = "sha256-ueq1Rsuae+wz4eFc1O7luBVR4XWGbefpDr124H6j56g=";
   };
 
-  propagatedBuildInputs = [ w3lib parsel jmespath itemadapter ];
+  propagatedBuildInputs = [
+    w3lib
+    parsel
+    jmespath
+    itemadapter
+  ];
 
-  checkInputs = [ pytestCheckHook ];
+  checkInputs = [
+    pytestCheckHook
+  ];
 
   disabledTests = [
     # Test are failing (AssertionError: Lists differ: ...)
@@ -32,13 +40,15 @@ buildPythonPackage rec {
     "test_nested_xpath"
   ];
 
-  pythonImportsCheck = [ "itemloaders" ];
+  pythonImportsCheck = [
+    "itemloaders"
+  ];
 
   meta = with lib; {
     description = "Base library for scrapy's ItemLoader";
     homepage = "https://github.com/scrapy/itemloaders";
     changelog = "https://github.com/scrapy/itemloaders/raw/v${version}/docs/release-notes.rst";
     license = licenses.bsd3;
-    maintainers = [ maintainers.marsam ];
+    maintainers = with maintainers; [ marsam ];
   };
 }
