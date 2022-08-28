f640cb3cc03d11c7e79448d3a71e070b00833040
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-28 04:44:33 +0000

    aws-c-s3: 0.1.39 -> 0.1.46

--- pkgs/development/libraries/aws-c-s3/default.nix
+++ pkgs/development/libraries/aws-c-s3/default.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation rec {
   pname = "aws-c-s3";
-  version = "0.1.39";
+  version = "0.1.46";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = "aws-c-s3";
     rev = "v${version}";
-    sha256 = "sha256-UbYh3awI7Y3mJnVQrltU+rZ1TI1TDaMF5TOcvxbzNa8=";
+    sha256 = "sha256-OUrMdIWWnk+yAJTVJ/UlUNFABLlTkmYac5g125Zq+fQ=";
   };
 
   nativeBuildInputs = [
