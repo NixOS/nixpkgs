08480c5b0f703aa983591e81467ab622262e3d0e
Anthony Roussel <anthony@roussel.dev>
2022-08-28 20:17:01 +0200

    asouldocs: 0.9.8 -> 1.0.0

--- pkgs/servers/asouldocs/default.nix
+++ pkgs/servers/asouldocs/default.nix
@@ -1,22 +1,22 @@
 { lib, buildGoModule, fetchFromGitHub }:
 
 buildGoModule rec {
-  pname = "peach";
-  version = "0.9.8";
+  pname = "asouldocs";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
-    owner = "peachdocs";
-    repo = "peach";
+    owner = "asoul-sig";
+    repo = "asouldocs";
     rev = "v${version}";
-    sha256 = "1pzk3sah39hz8n1kswxl35djh6wm0qcfcwrbfi50nd4k3bdgz7xl";
+    hash = "sha256-ctRE7aF3Qj+fI/m0CuLA6x7E+mY6s1+UfBJI5YFea4g=";
   };
 
-  vendorSha256 = "0f215hd5a9d4fyvdirp2j14ghap5vwv12i28jmzm6wxjihj8nn1g";
+  vendorSha256 = "sha256-T/KLiSK6bxXGkmVJ5aGrfHTUfLs/ElGyWSoCL5kb/KU=";
 
   meta = with lib; {
     description = "Web server for multi-language, real-time synchronization and searchable documentation";
-    homepage = "https://peachdocs.org/";
-    license = licenses.asl20;
-    maintainers = [ maintainers.ivar ];
+    homepage = "https://asouldocs.dev/";
+    license = licenses.mit;
+    maintainers = with maintainers; [ ivar ];
   };
 }
