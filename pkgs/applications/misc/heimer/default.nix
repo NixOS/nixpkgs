126f8a0394cc6737f8ce7096ccdc8af8a67c676e
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-28 17:47:43 +0000

    heimer: 3.5.0 -> 3.6.0

--- pkgs/applications/misc/heimer/default.nix
+++ pkgs/applications/misc/heimer/default.nix
@@ -2,13 +2,13 @@
 
 mkDerivation rec {
   pname = "heimer";
-  version = "3.5.0";
+  version = "3.6.0";
 
   src = fetchFromGitHub {
     owner = "juzzlin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-rxvfBgbMVKNiv+F2fyqcieGamTUwFKScEky918oU8lc=";
+    sha256 = "sha256-GQf5S7mYuucJS5jsz0upe6DiniY8YXZXYQV7nyrjVmE=";
   };
 
   nativeBuildInputs = [ cmake ];
