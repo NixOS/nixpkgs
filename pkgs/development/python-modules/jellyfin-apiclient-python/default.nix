c5692172b344a37fca263d775924b771c70cffff
Fabian Affolter <mail@fabian-affolter.ch>
2022-08-28 11:01:41 +0200

    python310Packages.jellyfin-apiclient-python: adjust inputs

--- pkgs/development/python-modules/jellyfin-apiclient-python/default.nix
+++ pkgs/development/python-modules/jellyfin-apiclient-python/default.nix
@@ -1,36 +1,42 @@
 { lib
 , buildPythonPackage
-, pythonOlder
-, fetchPypi
 , certifi
+, fetchPypi
+, pythonOlder
 , requests
-, six
+, urllib3
 , websocket-client
 }:
 
 buildPythonPackage rec {
   pname = "jellyfin-apiclient-python";
   version = "1.9.1";
+  format = "setuptools";
+
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-fS+NQUTKNxHuE+qsV91mpTlYt7DfXQVsA9ybfLlHYtc=";
+    hash = "sha256-fS+NQUTKNxHuE+qsV91mpTlYt7DfXQVsA9ybfLlHYtc=";
   };
 
   propagatedBuildInputs = [
     certifi
     requests
-    six
+    urllib3
     websocket-client
   ];
 
-  doCheck = false; # no tests
-  pythonImportsCheck = [ "jellyfin_apiclient_python" ];
+  # Module has no test
+  doCheck = false;
+
+  pythonImportsCheck = [
+    "jellyfin_apiclient_python"
+  ];
 
   meta = with lib; {
-    homepage = "https://github.com/jellyfin/jellyfin-apiclient-python";
     description = "Python API client for Jellyfin";
+    homepage = "https://github.com/jellyfin/jellyfin-apiclient-python";
     license = licenses.gpl3Only;
     maintainers = with maintainers; [ jojosch ];
   };
