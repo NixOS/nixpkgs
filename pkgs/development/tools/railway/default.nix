314d7814e6d7e8867730733ea929e96e2b5bc99c
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-28 23:29:08 +0000

    railway: 2.0.8 -> 2.0.10

--- pkgs/development/tools/railway/default.nix
+++ pkgs/development/tools/railway/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "railway";
-  version = "2.0.8";
+  version = "2.0.10";
 
   src = fetchFromGitHub {
     owner = "railwayapp";
     repo = "cli";
     rev = "v${version}";
-    sha256 = "sha256-E6DCY+xM796pQb48xkVQ6d3qgIB528hbCU96SCu2eYI=";
+    sha256 = "sha256-g/QBsWWVjhmn5slNav7j+vrzwf/0mMAERJaDLRrbxGI=";
   };
 
   ldflags = [ "-s" "-w" ];
