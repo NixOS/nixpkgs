NixOS/nixpkgs

commit e01c41205234a68a55903e05800e62d0de302e1a

fuzzel: fixup build after librsvg update
Fixes #166121

pkgs/applications/misc/fuzzel/default.nix
@@ -60,6 +60,8 @@ stdenv.mkDerivation rec {
     "-Dsvg-backend=${withSVGBackend}"
   ];
 
+  CFLAGS = "-Wno-error=comment"; # https://gitlab.gnome.org/GNOME/librsvg/-/issues/856
+
   meta = with lib; {
     description = "Wayland-native application launcher, similar to rofi’s drun mode";
     homepage = "https://codeberg.org/dnkl/fuzzel";

commit 72dc88a9aa24586d088d037839e462098645e18f

fuzzel: revert CFLAGS workaround for librsvg regression
The '-Wno-error=comment' cflag was introduced in https://github.com/NixOS/nixpkgs/commit/e01c41205234a68a55903e05800e62d0de302e1a
as a workaround for the librsvg build regression[1] when '-Werror' is used. The issue has
been fixed since release v2.54.1[2].

[1] - https://gitlab.gnome.org/GNOME/librsvg/-/issues/856
[2] - https://gitlab.gnome.org/GNOME/librsvg/-/commit/39a6d3418c0df8ca5ea4d0df3e1b14b915c75608

pkgs/applications/misc/fuzzel/default.nix
@@ -62,8 +62,6 @@ stdenv.mkDerivation rec {
     "-Dsvg-backend=${if svgSupport then "librsvg" else "none"}"
   ];
 
-  CFLAGS = "-Wno-error=comment"; # https://gitlab.gnome.org/GNOME/librsvg/-/issues/856
-
   meta = with lib; {
     description = "Wayland-native application launcher, similar to rofi’s drun mode";
     homepage = "https://codeberg.org/dnkl/fuzzel";