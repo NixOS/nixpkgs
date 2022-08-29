a8fa628a882d8c07cd300e6066e3149837526161
Niko Strijbol <strijbol.niko@gmail.com>
2022-08-29 22:48:11 +0200

    Update pkgs/tools/security/kwalletcli/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/tools/security/kwalletcli/default.nix
+++ pkgs/tools/security/kwalletcli/default.nix
@@ -38,12 +38,11 @@ mkDerivation rec {
   '';
 
   postInstall = ''
-    wrapProgram $out/bin/pinentry-kwallet \
-      --prefix PATH : $out/bin:${lib.makeBinPath [ pinentry-qt ]} \
-      --set-default PINENTRY pinentry-qt
-    wrapProgram $out/bin/kwalletcli_getpin \
-      --prefix PATH : $out/bin:${lib.makeBinPath [ pinentry-qt ]} \
-      --set-default PINENTRY pinentry-qt
+    for program in pinentry-kwallet kwalletcli_getpin; do
+      wrapProgram $out/bin/$program \
+        --prefix PATH : $out/bin:${lib.makeBinPath [ pinentry-qt ]} \
+        --set-default PINENTRY pinentry-qt
+    done
   '';
 
   meta = with lib; {
