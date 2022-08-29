b2ecbfeff29e21a6aaa97d8c3c521b677cee4119
Sandro <sandro.jaeckel@gmail.com>
2022-08-29 22:15:14 +0200

    Apply suggestions from code review

--- pkgs/development/tools/rust/cargo-hakari/default.nix
+++ pkgs/development/tools/rust/cargo-hakari/default.nix
@@ -12,17 +12,14 @@ rustPlatform.buildRustPackage rec {
   cargoHash = "sha256-eQrRBmlP206MKDlXxcJ64jD6/6mv3V/sv9TsybIx+8Q=";
 
   meta = with lib; {
-    description =
-      "Manage workspace-hack packages to speed up builds in large workspaces.";
+    description = "Manage workspace-hack packages to speed up builds in large workspaces.";
     longDescription = ''
       cargo hakari is a command-line application to manage workspace-hack crates.
       Use it to speed up local cargo build and cargo check commands by 15-95%,
       and cumulatively by 20-25% or more.
     '';
     homepage = "https://crates.io/crates/cargo-hakari";
-
     license = with licenses; [ mit asl20 ];
     maintainers = with maintainers; [ macalinao ];
   };
-
 }
