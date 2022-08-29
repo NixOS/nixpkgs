0aaca0a6204d5fdd823fda311a91367d2d9d8097
Matthias Bartelmeß <mba@fourplusone.de>
2022-08-29 23:25:50 +0200

    fpc: support darwin

---
+++ pkgs/development/compilers/fpc/binary-builder-darwin.sh
@@ -0,0 +1,17 @@
+source $stdenv/setup
+
+pkgdir=$(pwd)/pkg
+deploydir=$(pwd)/deploy
+
+undmg $src
+mkdir $out
+mkdir $pkgdir
+mkdir $deploydir
+
+pkg=*.mpkg/Contents/Packages/*.pkg
+xar -xf $pkg -C $pkgdir
+pushd $deploydir
+cat $pkgdir/Payload | gunzip -dc | cpio -i
+popd
+echo $deploydir
+cp -r $deploydir/usr/local/* $out
