f9926df6de17be8077e5fc6cc62a0197d7659ebf
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-29 09:58:33 +0000

    fluidd: 1.19.1 -> 1.20.0

--- pkgs/applications/misc/fluidd/default.nix
+++ pkgs/applications/misc/fluidd/default.nix
@@ -2,12 +2,12 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "fluidd";
-  version = "1.19.1";
+  version = "1.20.0";
 
   src = fetchurl {
     name = "fluidd-v${version}.zip";
     url = "https://github.com/cadriel/fluidd/releases/download/v${version}/fluidd.zip";
-    sha256 = "sha256-ARBi+8IAheEJ5keyiDCub6Y83hT/dU/BajPChi3f1po=";
+    sha256 = "sha256-zDCvRwAtjBbBOa5D1k7khZGXeql5COMlWMi/dpwiiYM=";
   };
 
   nativeBuildInputs = [ unzip ];
