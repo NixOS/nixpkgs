d2969188461a3f29da7d32172c327af43d72b575
Fabian Affolter <mail@fabian-affolter.ch>
2022-08-29 08:56:02 +0200

    python310Packages.debian: specify license

--- pkgs/development/python-modules/debian/default.nix
+++ pkgs/development/python-modules/debian/default.nix
@@ -8,26 +8,31 @@
 buildPythonPackage rec {
   pname = "python-debian";
   version = "0.1.47";
+  format = "setuptools";
 
   disabled = pythonOlder "3.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-UeICgjd3o9cWqEO4pUD7oroL7Z9QeofAwPnu/N7DNCw=";
+    hash = "sha256-UeICgjd3o9cWqEO4pUD7oroL7Z9QeofAwPnu/N7DNCw=";
   };
 
-  propagatedBuildInputs = [ chardet ];
+  propagatedBuildInputs = [
+    chardet
+  ];
 
   # No tests in archive
   doCheck = false;
 
-  pythonImportsCheck = [ "debian" ];
+  pythonImportsCheck = [
+    "debian"
+  ];
 
   meta = with lib; {
     description = "Debian package related modules";
     homepage = "https://salsa.debian.org/python-debian-team/python-debian";
     changelog = "https://salsa.debian.org/python-debian-team/python-debian/-/blob/master/debian/changelog";
-    license = licenses.gpl2;
+    license = licenses.gpl2Plus;
     maintainers = with maintainers; [ SuperSandro2000 ];
   };
 }
