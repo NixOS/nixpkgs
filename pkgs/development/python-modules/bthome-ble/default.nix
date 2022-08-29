082ac9d2bf3c99d10deaddb16ecea17237e246bb
Fabian Affolter <mail@fabian-affolter.ch>
2022-08-29 20:26:42 +0200

    python310Packages.bthome-ble: 0.5.0 -> 0.5.1

--- pkgs/development/python-modules/bthome-ble/default.nix
+++ pkgs/development/python-modules/bthome-ble/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "bthome-ble";
-  version = "0.5.0";
+  version = "0.5.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-Ipi3XXPWxtQJIE2hzFdF/gnp+HqYdwtFtRrHfDcGgew=";
+    hash = "sha256-G3EZ23bpvt0HBjHtxJnpXgOzv+3/Rl8yWrVR5s3iZQA=";
   };
 
   nativeBuildInputs = [
