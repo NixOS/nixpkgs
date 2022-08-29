bb0eb96d45cfcb964a35b713b60c063ffc04ffd7
superherointj <5861043+superherointj@users.noreply.github.com>
2022-08-29 20:22:06 -0300

    linux: 5.19.4 -> 5.19.5

--- pkgs/os-specific/linux/kernel/linux-5.19.nix
+++ pkgs/os-specific/linux/kernel/linux-5.19.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.19.4";
+  version = "5.19.5";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "11xzz5glbc7p3j9mhv6dvi9iv345ykpyihm8mby8vyas12bln8d9";
+    sha256 = "1g9p4m9w9y0y1gk6vzqvsxzwqspbm10mmhd8n1mhal1yz721qgwc";
   };
 } // (args.argsOverride or { }))
