6088bad88a7a05c460d9549beb013bddd72c1e36
Anthony Roussel <anthony@roussel.dev>
2022-08-30 19:04:07 +0200

    bundler: add passthru.updateScript

--- pkgs/development/ruby-modules/bundler/default.nix
+++ pkgs/development/ruby-modules/bundler/default.nix
@@ -1,4 +1,4 @@
-{ buildRubyGem, ruby }:
+{ buildRubyGem, ruby, writeScript }:
 
 buildRubyGem rec {
   inherit ruby;
@@ -7,6 +7,16 @@ buildRubyGem rec {
   version = "2.3.20";
   source.sha256 = "sha256-gJJ3vHzrJo6XpHS1iwLb77jd9ZB39GGLcOJQSrgaBHw=";
   dontPatchShebangs = true;
+
+  passthru.updateScript = writeScript "gem-update-script" ''
+    #!/usr/bin/env nix-shell
+    #!nix-shell -i bash -p curl common-updater-scripts jq
+
+    set -eu -o pipefail
+
+    latest_version=$(curl -s https://rubygems.org/api/v1/gems/${gemName}.json | jq --raw-output .version)
+    update-source-version ${gemName} "$latest_version"
+  '';
 
   postFixup = ''
     sed -i -e "s/activate_bin_path/bin_path/g" $out/bin/bundle
