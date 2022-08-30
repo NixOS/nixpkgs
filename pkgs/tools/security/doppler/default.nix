a61f70f8972b957712bb3dbdefca77083a2180f1
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-30 09:46:42 +0000

    doppler: 3.41.0 -> 3.42.0

--- pkgs/tools/security/doppler/default.nix
+++ pkgs/tools/security/doppler/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "doppler";
-  version = "3.41.0";
+  version = "3.42.0";
 
   src = fetchFromGitHub {
     owner = "dopplerhq";
     repo = "cli";
     rev = version;
-    sha256 = "sha256-nYw3ZEGrqmuo6UKtlE+xS/mX1P5VXZ0zn8s/3K3k0jk=";
+    sha256 = "sha256-bIcBn6BjRerbIb+2/own51avbp6rUr4kClrY3iyTKso=";
   };
 
   vendorSha256 = "sha256-evG1M0ZHfn9hsMsSncwxF5Hr/VJ7y6Ir0D2gHJaunBo=";
