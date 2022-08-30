c41ebc16cdf6aedde27a4ad59d0797a7afca8ffd
R. Ryantm <ryantm-bot@ryantm.com>
2022-08-30 01:12:58 +0000

    sensu-go-backend: 6.7.5 -> 6.8.0

--- pkgs/servers/monitoring/sensu-go/default.nix
+++ pkgs/servers/monitoring/sensu-go/default.nix
@@ -4,19 +4,19 @@ let
   generic = { subPackages, pname, postInstall ? "", mainProgram }:
     buildGoModule rec {
       inherit pname;
-      version = "6.7.5";
+      version = "6.8.0";
       shortRev = "3a1ac58"; # for internal version info
 
       src = fetchFromGitHub {
         owner = "sensu";
         repo = "sensu-go";
         rev = "v${version}";
-        sha256 = "sha256-xzx6MaTkmCNDJiroaLFlQmaxUF0Zo4ZNT/7huzef61U=";
+        sha256 = "sha256-T9SR3Ec7Q51Q2L/xJHx35eA0/KcFB3ZxqimIYKwAJLU=";
       };
 
       inherit subPackages postInstall;
 
-      vendorSha256 = "sha256-ptBGCI3mLAsWhnIA1kUVZxHeBlEpsYoFTSF2kPTq2PM=";
+      vendorSha256 = "sha256-Y8gYh770p22O1ZLcqZi5NNKWOP4sXHSP3b0d4klrSHg=";
 
       doCheck = false;
 
