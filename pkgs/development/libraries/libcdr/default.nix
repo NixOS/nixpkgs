f02964978cdc18b6c54fedec8c0e7ecc05e5532a
Nikolay Korotkiy <sikmir@disroot.org>
2022-08-30 14:42:11 +0300

    libcdr: 0.1.6 → 0.1.7

--- pkgs/development/libraries/libcdr/default.nix
+++ pkgs/development/libraries/libcdr/default.nix
@@ -2,22 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "libcdr";
-  version = "0.1.6";
+  version = "0.1.7";
 
   src = fetchurl {
     url = "https://dev-www.libreoffice.org/src/${pname}-${version}.tar.xz";
-    sha256 = "0qgqlw6i25zfq1gf7f6r5hrhawlrgh92sg238kjpf2839aq01k81";
+    hash = "sha256-VmYknWE0ZrmqHph+pBCcBDZYZuknfYD2zZZj6GuOzdQ=";
   };
 
-  patches = [
-    # Fix build with icu 68
-    # Remove in next release
-    (fetchpatch {
-      name = "libcdr-fix-icu-68";
-      url = "https://cgit.freedesktop.org/libreoffice/libcdr/patch/?id=bf3e7f3bbc414d4341cf1420c99293debf1bd894";
-      sha256 = "0cgra10p8ibgwn8y5q31jrpan317qj0ribzjs4jq0bwavjq92w2k";
-    })
-  ];
+  strictDeps = true;
 
   buildInputs = [ libwpg libwpd lcms librevenge icu boost cppunit ];
 
