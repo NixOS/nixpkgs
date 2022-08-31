e9eee8ff51c0e020d5ba95dabea69b1f44e812be
zowoq <59103226+zowoq@users.noreply.github.com>
2022-08-31 17:54:32 +1000

    curl: 7.84.0 -> 7.85.0
    https://curl.se/changes.html#7_85_0

--- pkgs/tools/package-management/nix/default.nix
+++ pkgs/tools/package-management/nix/default.nix
@@ -1,7 +1,6 @@
 { lib
 , aws-sdk-cpp
 , boehmgc
-, curl
 , callPackage
 , fetchFromGitHub
 , fetchurl
@@ -32,7 +31,7 @@ let
 
   common = args:
     callPackage
-      (import ./common.nix ({ inherit lib fetchFromGitHub curl; } // args))
+      (import ./common.nix ({ inherit lib fetchFromGitHub; } // args))
       {
         inherit Security storeDir stateDir confDir;
         boehmgc = boehmgc-nix;
