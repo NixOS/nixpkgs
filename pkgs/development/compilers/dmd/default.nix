96f3189b9acfaf7727a5d3ddda67a8c6fc90864b
Sergei Trofimovich <slyich@gmail.com>
2022-08-31 08:46:41 +0100

    dmd: disable whitespace-sensitive tests for binutils-2.39
    Without the change two tests fail on `binutils-2.39` due to whitespace
    change in `objdump` output as:
    
        FAILED targets:
        - runnable/cdvecfill.sh
        - compilable/cdcmp.d
    
    Let's disable the tests until they are fixed upstream:
        https://issues.dlang.org/show_bug.cgi?id=23317

--- pkgs/development/compilers/dmd/default.nix
+++ pkgs/development/compilers/dmd/default.nix
@@ -84,6 +84,11 @@ stdenv.mkDerivation rec {
   postPatch =
   ''
     patchShebangs .
+
+    # Disable tests that rely on objdump whitespace until fixed upstream:
+    #   https://issues.dlang.org/show_bug.cgi?id=23317
+    rm dmd/test/runnable/cdvecfill.sh
+    rm dmd/test/compilable/cdcmp.d
   ''
 
   # This one has tested against a hardcoded year, then against a current year on
