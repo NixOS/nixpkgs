7de978027d5499d3db4843486ea8e9df43304c9e
Jan Tojnar <jtojnar@gmail.com>
2022-08-31 06:11:55 +0200

    gnome-info-collect: init at 0.1-7
    https://blogs.gnome.org/aday/2022/08/25/help-improve-gnome/

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7001,6 +7001,8 @@ with pkgs;
 
   gnome-frog = callPackage ../applications/misc/gnome-frog { };
 
+  gnome-info-collect = callPackage ../desktops/gnome/misc/gnome-info-collect { };
+
   gnome-keysign = callPackage ../tools/security/gnome-keysign { };
 
   gnome-secrets = callPackage ../applications/misc/gnome-secrets { };
