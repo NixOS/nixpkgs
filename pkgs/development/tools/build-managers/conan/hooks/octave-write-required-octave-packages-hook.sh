e7ab54a2449a8c3ae8261ef4a37b5cf78ad989ea
Luna Nova <git@lunnova.dev>
2022-08-30 17:22:07 -0700

    fixup! wip it's all fscked

--- pkgs/development/tools/build-managers/conan/hooks/octave-write-required-octave-packages-hook.sh
+++
@@ -1,6 +0,0 @@
-#TODO: Make a hook which does equivalent to https://docs.conan.io/en/latest/reference/conanfile/tools/cmake/cmakedeps.html
-# https://github.com/conan-io/conan/blob/develop/conan/tools/cmake/cmakedeps/templates/config.py
-# so can use as a dep in conan projects
-
-
-https://blog.karatos.in/a?ID=01000-1cfb4754-689d-453a-8ac6-3b711d59186b - use cmake right
