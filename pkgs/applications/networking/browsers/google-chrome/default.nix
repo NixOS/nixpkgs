aa56edf300652a2faaece1f61b8efe372b8e9e8a
Stefan Radziuk <stefan@radziuk.pl>
2022-09-01 23:31:31 +0100

    google-chrome: add commandLineArgs after wayland flags (#189199)

--- pkgs/applications/networking/browsers/google-chrome/default.nix
+++ pkgs/applications/networking/browsers/google-chrome/default.nix
@@ -148,8 +148,8 @@ in stdenv.mkDerivation {
       --suffix PATH            : "${lib.makeBinPath [ xdg-utils ]}" \
       --prefix XDG_DATA_DIRS   : "$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH:${addOpenGLRunpath.driverLink}/share" \
       --set CHROME_WRAPPER  "google-chrome-$dist" \
-      --add-flags ${escapeShellArg commandLineArgs} \
-      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--enable-features=UseOzonePlatform --ozone-platform=wayland}}"
+      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--enable-features=UseOzonePlatform --ozone-platform=wayland}}" \
+      --add-flags ${escapeShellArg commandLineArgs}
 
     for elf in $out/share/google/$appname/{chrome,chrome-sandbox,${crashpadHandlerBinary},nacl_helper}; do
       patchelf --set-rpath $rpath $elf
