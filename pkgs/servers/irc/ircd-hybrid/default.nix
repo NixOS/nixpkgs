0762cbf11624c232ac3425a233d12947cc43e5a6
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-01 18:28:54 +0000

    ircdHybrid: 8.2.41 -> 8.2.42

--- pkgs/servers/irc/ircd-hybrid/default.nix
+++ pkgs/servers/irc/ircd-hybrid/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "ircd-hybrid";
-  version = "8.2.41";
+  version = "8.2.42";
 
   src = fetchurl {
     url = "mirror://sourceforge/ircd-hybrid/ircd-hybrid-${version}.tgz";
-    sha256 = "sha256-zg6mllRgEEe9hkI93AAjwHlVcCSZkAEeqmxB1jcrBFQ=";
+    sha256 = "sha256-aS0he/x+o3xOQ5H7hrCpJ4C8MusUT2liM0lyF1W4H2o=";
   };
 
   buildInputs = [ openssl zlib ];
