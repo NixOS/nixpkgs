3b80d3febbdd35cadf1a204f274ceb583df84b4f
Fabian Affolter <mail@fabian-affolter.ch>
2022-09-01 05:26:23 +0200

    python310Packages.gspread: add pythonImportsCheck
    - disable on older Python releases

--- pkgs/development/python-modules/gspread/default.nix
+++ pkgs/development/python-modules/gspread/default.nix
@@ -4,26 +4,38 @@
 , requests
 , google-auth
 , google-auth-oauthlib
+, pythonOlder
 }:
 
 buildPythonPackage rec {
-  version = "5.5.0";
   pname = "gspread";
+  version = "5.5.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-hiDph+U0AxXyuNjSbPl+RzaoSzMloXx9m8/3BSXcMAM=";
+    hash = "sha256-hiDph+U0AxXyuNjSbPl+RzaoSzMloXx9m8/3BSXcMAM=";
   };
 
-  propagatedBuildInputs = [ requests google-auth google-auth-oauthlib ];
+  propagatedBuildInputs = [
+    requests
+    google-auth
+    google-auth-oauthlib
+  ];
+
+  # No tests included
+  doCheck = false;
+
+  pythonImportsCheck = [
+    "gspread"
+  ];
 
   meta = with lib; {
     description = "Google Spreadsheets client library";
     homepage = "https://github.com/burnash/gspread";
     license = licenses.mit;
+    maintainers = with maintainers; [ ];
   };
-
-  # No tests included
-  doCheck = false;
-
 }
