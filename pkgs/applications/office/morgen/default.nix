d7ce838ec54447d1aaa15fa1e503063663114142
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-01 22:13:20 +0000

    morgen: 2.5.10 -> 2.5.13

--- pkgs/applications/office/morgen/default.nix
+++ pkgs/applications/office/morgen/default.nix
@@ -3,11 +3,11 @@
 
 stdenv.mkDerivation rec {
   pname = "morgen";
-  version = "2.5.10";
+  version = "2.5.13";
 
   src = fetchurl {
     url = "https://download.todesktop.com/210203cqcj00tw1/morgen-${version}.deb";
-    sha256 = "sha256-uKHLKZtvaaklJMYqAHtu4ULwcfbut1L2h6bjZkqI91o=";
+    sha256 = "sha256-n9lZfg+0JRRaO3H+sDXRIrELTs6NE5G78Ni0RKWzYuI=";
   };
 
   nativeBuildInputs = [
