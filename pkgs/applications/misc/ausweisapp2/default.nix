af057ad908c6c5d2e40d841e37ca79994505f3c8
kilianar <mail@kilianar.de>
2022-09-01 15:47:13 +0200

    AusweisApp2: 1.24.0 -> 1.24.1
    https://github.com/Governikus/AusweisApp2/releases/tag/1.24.1

--- pkgs/applications/misc/ausweisapp2/default.nix
+++ pkgs/applications/misc/ausweisapp2/default.nix
@@ -3,13 +3,13 @@
 
 mkDerivation rec {
   pname = "AusweisApp2";
-  version = "1.24.0";
+  version = "1.24.1";
 
   src = fetchFromGitHub {
     owner = "Governikus";
     repo = "AusweisApp2";
     rev = version;
-    sha256 = "sha256-2yEUMD3FQ9JrecCnWSr0Fej4ngk7HVCZUgbRqmGbCK8=";
+    sha256 = "sha256-Uoi12odfJGyNc8Ap7E/zm4xklYdPPQvCGNxjb9AWJOI=";
   };
 
   nativeBuildInputs = [ cmake pkg-config ];
