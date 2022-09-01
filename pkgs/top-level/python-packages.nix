d69a00a2b01764b406985fd54f4122bc68a5bc79
Jonas Heinrich <onny@project-insanity.org>
2022-09-01 14:41:52 +0200

    python310Packages.aioquic: init at 0.9.20

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -284,6 +284,8 @@ self: super: with self; {
 
   aioqsw = callPackage ../development/python-modules/aioqsw { };
 
+  aioquic = callPackage ../development/python-modules/aioquic { };
+
   aiorecollect = callPackage ../development/python-modules/aiorecollect { };
 
   aioredis = callPackage ../development/python-modules/aioredis { };
