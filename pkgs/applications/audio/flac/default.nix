9380655f6da3b8e05ab2fa0dd6db9198c48c45de
OPNA2608 <christoph.neidahl@gmail.com>
2022-09-02 17:11:06 +0200

    flac: apply patch for ppc64

--- pkgs/applications/audio/flac/default.nix
+++ pkgs/applications/audio/flac/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, fetchpatch, libogg }:
+{ lib, stdenv, fetchurl, fetchpatch, autoreconfHook, libogg }:
 
 stdenv.mkDerivation rec {
   pname = "flac";
@@ -8,6 +8,20 @@ stdenv.mkDerivation rec {
     url = "http://downloads.xiph.org/releases/flac/${pname}-${version}.tar.xz";
     sha256 = "0dz7am8kbc97a6afml1h4yp085274prg8j7csryds8m3fmz61w4g";
   };
+
+  patches = [
+    # Remove on next bump
+    (fetchpatch {
+      name = "flac-Add-POWER-AltiVec-VSX-checks.patch";
+      url = "https://github.com/xiph/flac/commit/d15bcf80a06bd5300e33d5f5c94d69fbf879f45a.patch";
+      sha256 = "sha256-Pex4zAZiP2WsHbZ3SmsDY+taN5jNLOItN2FaBa+mebM=";
+    })
+  ];
+
+  nativeBuildInputs = [
+    # Needed for patching of configure.ac, can be removed on next bump
+    autoreconfHook
+  ];
 
   buildInputs = [ libogg ];
 
