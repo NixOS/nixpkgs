065c8a0fc3ff8e9295c2ce8a6039a4a6f686ec97
kilianar <mail@kilianar.de>
2022-09-02 17:12:23 +0200

    super-productivity: 7.11.6 -> 7.12.0
    https://github.com/johannesjo/super-productivity/releases/tag/v7.12.0

--- pkgs/applications/office/super-productivity/default.nix
+++ pkgs/applications/office/super-productivity/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "super-productivity";
-  version = "7.11.6";
+  version = "7.12.0";
 
   src = fetchurl {
     url = "https://github.com/johannesjo/super-productivity/releases/download/v${version}/superProductivity-${version}.AppImage";
-    sha256 = "sha256-BXRnc6c/PpFdF4gPemU6IzVQvr+Tl5Fbxz7mY6Z5u7o=";
+    sha256 = "sha256-Ms7wt79iZz0xnrp+yrFs/FzVqINe6BSOEmYG9Oiz5JU=";
     name = "${pname}-${version}.AppImage";
   };
 
