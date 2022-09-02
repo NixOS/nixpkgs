57f646e4c4cf38d1655825d914bf3c0431c8a4b5
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-02 20:47:28 +0000

    oh-my-posh: 8.36.7 -> 8.37.1

--- pkgs/development/tools/oh-my-posh/default.nix
+++ pkgs/development/tools/oh-my-posh/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "oh-my-posh";
-  version = "8.36.7";
+  version = "8.37.1";
 
   src = fetchFromGitHub {
     owner = "jandedobbeleer";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-K4+d7HIH7FDvXkjkf6YCJjbTgPYBBMFqKDHXUNkL2Jo=";
+    sha256 = "sha256-ZiDrG1GkUple+Q+MoNQlHdbq3P8PeQFWvxMC9dh7VTM=";
   };
 
   vendorSha256 = "sha256-t4FpvXsGVsTYoGM8wY2JelscnlmDzrLMPYk7zGUfo58=";
