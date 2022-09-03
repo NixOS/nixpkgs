0d6dfd25110645a2f80d2bc7de1f0545c5ff8831
David Lahm <david@klinger-lahm.de>
2022-09-03 20:55:15 +0200

    minetestWithMods: introduced factory for modded minetest.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31878,6 +31878,14 @@ with pkgs;
 
   minetest-mods = callPackage ../games/minetest/mods { };
 
+  minetestWithMods = import ../games/minetest/minetest-with-mods.nix 
+                                {
+                                    inherit symlinkJoin;
+                                    inherit makeWrapper;
+                                    inherit minetest-mods;
+                                    inherit (pkgs) minetest;
+                                  };
+
   mnemosyne = callPackage ../games/mnemosyne {
     python = python3;
   };
