6aaf666aaab76d6bd545222e8d79df6cc2c16f4b
Matthieu Coudron <mcoudron@hotmail.com>
2022-09-03 23:01:00 +0200

    bumped luarocks-nix

--- pkgs/development/tools/misc/luarocks/luarocks-nix.nix
+++ pkgs/development/tools/misc/luarocks/luarocks-nix.nix
@@ -1,20 +1,24 @@
-{ luarocks, fetchFromGitHub, nix-update-script }:
+{ luarocks, fetchFromGitHub, unstableGitUpdater }:
 luarocks.overrideAttrs(old: {
   pname = "luarocks-nix";
+  version = "unstable-2022-09-04";
 
   src = fetchFromGitHub {
     owner = "nix-community";
     repo = "luarocks-nix";
-    rev = "b1ff9eeb64c7c1dc5fc177008d6f2be9191c6aa2";
-    sha256 = "sha256-mkzrf/6yMyLMIEDwsuCIxi1HJvg57ybyZPXVheFAAHE=";
+    rev = "cfc1439a29ac666fb0fcf440224adf73004655d4";
+    sha256 = "sha256-uHkE9ztOZDs2pi7to3kZ0iELRhv/gPQgTK+qyYpFZ/Y=";
   };
+
   patches = [];
 
   passthru = {
-    updateScript = nix-update-script {
-      attrPath = "luarocks-nix";
+    updateScript = unstableGitUpdater {
+      branch = "use-fetchzip";
     };
   };
 
-  meta.mainProgram = "luarocks";
+  meta =  {
+    mainProgram = "luarocks";
+  };
 })
