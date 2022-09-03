ea45fcb8358ad63e0c8aaafdbd84f17dda9d06db
Thomas Gerbet <thomas@gerbet.me>
2022-09-03 15:13:30 +0200

    wasmer: enable LLVM and singlepass compilers on supported environments
    More information about those compilers can be found here:
    https://github.com/wasmerio/wasmer/tree/2.3.0/lib/compiler-singlepass
    https://github.com/wasmerio/wasmer/tree/2.3.0/lib/compiler-llvm

--- pkgs/development/interpreters/wasmer/default.nix
+++ pkgs/development/interpreters/wasmer/default.nix
@@ -1,10 +1,18 @@
 { stdenv
 , lib
 , rustPlatform
+, libffi
+, libxml2
+, ncurses
+, zlib
 , fetchFromGitHub
 , cmake
+, llvmPackages_12 # See version specified in https://github.com/wasmerio/wasmer/tree/master/lib/compiler-llvm#requirements
 , llvmPackages
 , pkg-config
+# See matrix https://github.com/wasmerio/wasmer/blob/master/Makefile#L22
+, withLlvmCompiler ? ((stdenv.isDarwin || stdenv.isLinux) && stdenv.isx86_64)
+, withSinglepassCompiler ? ((stdenv.isDarwin || stdenv.isLinux) && stdenv.isx86_64)
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -21,11 +29,14 @@ rustPlatform.buildRustPackage rec {
 
   cargoSha256 = "sha256-tswsbijNN5UcSZovVmy66yehcEOpQDGMdRgR/1mkuE8=";
 
-  nativeBuildInputs = [ cmake pkg-config ];
+  buildInputs = [] ++ lib.optionals withLlvmCompiler [ libffi libxml2 ncurses zlib ];
+  nativeBuildInputs = [ cmake pkg-config ] ++ lib.optionals withLlvmCompiler [ llvmPackages_12.llvm ];
 
   # cranelift+jit works everywhere, see:
   # https://github.com/wasmerio/wasmer/blob/master/Makefile#L22
-  buildFeatures = [ "cranelift" "jit" ];
+  buildFeatures = [ "cranelift" "jit" ]
+    ++ lib.optionals withLlvmCompiler [ "llvm" ]
+    ++ lib.optionals withSinglepassCompiler [ "singlepass" ];
   cargoBuildFlags = [
     # must target manifest and desired output bin, otherwise output is empty
     "--manifest-path" "lib/cli/Cargo.toml"
@@ -36,6 +47,7 @@ rustPlatform.buildRustPackage rec {
   # error: Package `wasmer-workspace v2.3.0 (/build/source)` does not have the feature `test-jit`
   checkFeatures = [ "test-cranelift" ];
 
+  # Using llvmPackages_12 makes the test wast::spec::unreachable::cranelift::universal fail on aarch64
   LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
 
   meta = with lib; {
