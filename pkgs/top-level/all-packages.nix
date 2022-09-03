0f9cea8a3649cc189a9f69b39cf47f385cb01df8
David Lahm <david@klinger-lahm.de>
2022-09-03 21:21:17 +0200

    minetestWithMods: use override pattern

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31879,11 +31879,12 @@ with pkgs;
   minetest-mods = callPackage ../games/minetest/mods { };
 
   minetestWithMods = import ../games/minetest/minetest-with-mods.nix 
+                                pkgs
                                 {
                                     inherit symlinkJoin;
                                     inherit makeWrapper;
                                     inherit minetest-mods;
-                                    inherit (pkgs) minetest;
+                                    inherit minetest;
                                   };
 
   mnemosyne = callPackage ../games/mnemosyne {
