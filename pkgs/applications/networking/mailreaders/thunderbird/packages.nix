f4ef4dbd6e384f43a78f28483abdab765ad2060e
Vladimír Čunát <vladimir.cunat@nic.cz>
2022-09-03 18:44:39 +0200

    thunderbird*: 91 -> 102
    The upstream support of 91 branch is already ending around now,
    so let's switch the default.

--- pkgs/applications/networking/mailreaders/thunderbird/packages.nix
+++ pkgs/applications/networking/mailreaders/thunderbird/packages.nix
@@ -1,7 +1,7 @@
 { stdenv, lib, buildMozillaMach, callPackage, fetchurl, fetchpatch, nixosTests }:
 
 rec {
-  thunderbird = thunderbird-91;
+  thunderbird = thunderbird-102;
   thunderbird-91 = (buildMozillaMach rec {
     pname = "thunderbird";
     version = "91.13.0";
