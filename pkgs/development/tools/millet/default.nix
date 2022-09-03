29b333f3af22c685a5a77feadd6ee656f23609c0
Mario Rodas <marsam@users.noreply.github.com>
2022-09-03 04:20:00 +0000

    millet: 0.3.5 -> 0.3.6

--- pkgs/development/tools/millet/default.nix
+++ pkgs/development/tools/millet/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "millet";
-  version = "0.3.5";
+  version = "0.3.6";
 
   src = fetchFromGitHub {
     owner = "azdavis";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-6laSFo8aBmZpeN5V3utZQHIWuoQcdy1hd1yU8LxpEJQ=";
+    sha256 = "sha256-HV/0ORI3siAnVyLcNDzu4BwK/sFO841ptQoxK6c+1Xs=";
   };
 
-  cargoSha256 = "sha256-bbyeI/cr5aBejiEcYyyyJO8UX5QoIlT9MFgnpDr/z1M=";
+  cargoSha256 = "sha256-jhn795bTcqOG60yzydWJ5Y9eR2UTVUBteuMw03M7wM0=";
 
   cargoBuildFlags = [ "--package" "lang-srv" ];
 
@@ -19,7 +19,7 @@ rustPlatform.buildRustPackage rec {
     description = "A language server for Standard ML";
     homepage = "https://github.com/azdavis/millet";
     changelog = "https://github.com/azdavis/millet/raw/v${version}/docs/changelog.md";
-    license = licenses.mit;
+    license = [ licenses.mit /* or */ licenses.asl20 ];
     maintainers = with maintainers; [ marsam ];
   };
 }
