bcf086196c6ae9e70bb18a0bb7c0a4fe92277377
Daniel Nagy <danielnagy@posteo.de>
2022-09-03 10:04:00 +0200

    iosevka-comfy: 0.3.1 -> 0.4.0

--- pkgs/data/fonts/iosevka/comfy.nix
+++ pkgs/data/fonts/iosevka/comfy.nix
@@ -1,32 +1,42 @@
-{ callPackage, lib, fetchFromGitHub }:
+{ callPackage, lib, fetchFromSourcehut }:
 
 let
-  sets = [ "comfy" "comfy-fixed" "comfy-duo" "comfy-wide" "comfy-wide-fixed" ];
-  version = "0.3.1";
-  src = fetchFromGitHub {
-    owner = "protesilaos";
+  sets = [
+    "comfy"
+    "comfy-fixed"
+    "comfy-duo"
+    "comfy-wide"
+    "comfy-wide-fixed"
+    "comfy-motion"
+    "comfy-motion-duo"
+  ];
+  version = "0.4.0";
+  src = fetchFromSourcehut {
+    owner = "~protesilaos";
     repo = "iosevka-comfy";
     rev = version;
-    sha256 = "sha256-SMy3Kqve65ZU13lf1vZQR61mH7gcl1DvIZt3yD6tIf4=";
+    sha256 = "sha256-d3C5HNiZCd0xva6zvMP9NmvmeU4uXuaO04pbgIepwfw=";
   };
   privateBuildPlan = src.outPath + "/private-build-plans.toml";
   overrideAttrs = (attrs: {
     inherit version;
 
     meta = with lib; {
-      homepage = "https://github.com/protesilaos/iosevka-comfy";
+      inherit (src.meta) homepage;
       description = ''
-      Custom build of Iosevka with a rounded style and open shapes,
-      adjusted metrics, and overrides for almost all individual glyphs
-      in both roman (upright) and italic (slanted) variants.
-    '';
+        Custom build of Iosevka with a rounded style and open shapes,
+        adjusted metrics, and overrides for almost all individual glyphs
+        in both roman (upright) and italic (slanted) variants.
+      '';
       license = licenses.ofl;
       platforms = attrs.meta.platforms;
       maintainers = [ maintainers.DamienCassou ];
     };
   });
-  makeIosevkaFont = set: (callPackage ./. {
-    inherit set privateBuildPlan;
-  }).overrideAttrs overrideAttrs;
-in
-builtins.listToAttrs (builtins.map (set: {name=set; value=makeIosevkaFont set;}) sets)
+  makeIosevkaFont = set:
+    (callPackage ./. { inherit set privateBuildPlan; }).overrideAttrs
+    overrideAttrs;
+in builtins.listToAttrs (builtins.map (set: {
+  name = set;
+  value = makeIosevkaFont set;
+}) sets)
