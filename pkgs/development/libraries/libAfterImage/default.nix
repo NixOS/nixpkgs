60a7d2e58dc4da15dd5f8d2e9c87ec94845a3a68
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2022-09-03 22:25:49 -0400

    libAfterImage: fix compatibility with giflib 5

--- pkgs/development/libraries/libAfterImage/default.nix
+++ pkgs/development/libraries/libAfterImage/default.nix
@@ -54,6 +54,13 @@ stdenv.mkDerivation {
       # workaround '-p0' patchflags below.
       stripLen = 1;
     })
+
+    # fix https://github.com/root-project/root/issues/10990
+    (fetchpatch {
+      url = "https://github.com/root-project/root/pull/11243/commits/e177a477b0be05ef139094be1e96a99ece06350a.diff";
+      hash = "sha256-2DQmJGHmATHawl3dk9dExncVe1sXzJQyy4PPwShoLTY=";
+      stripLen = 5;
+    })
   ];
   patchFlags = [ "-p0" ];
 
