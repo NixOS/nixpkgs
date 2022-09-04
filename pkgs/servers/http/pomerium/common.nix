b1246a98010dedb01eb0623b83cdcd02da178d21
Luke Granger-Brown <git@lukegb.com>
2022-09-04 22:20:58 +0100

    pomerium: 0.18.0 -> 0.19.0

--- pkgs/servers/http/pomerium/common.nix
+++ pkgs/servers/http/pomerium/common.nix
@@ -1,14 +1,12 @@
 { fetchFromGitHub
 , lib
 }:
-let
-  version = "0.18.0";
-  srcSha256 = "sha256-sM4kM8CqbZjl+RIsezWYVCmjoDKfGl+EQcdEaPKvVHs=";
-  vendorSha256 = "sha256-1EWcjfrO3FEypUUKwNwDisogERCuKOvtC7z0mC2JZn4=";
-  yarnSha256 = "sha256-Uh0y2Zmy6bSoyL5WMTce01hoH7EvSIniHyIBMxfMvhg=";
-in
+
 {
-  inherit version vendorSha256 yarnSha256;
+  version = "0.19.0";
+  srcSha256 = "sha256:0s5ji1iywymzxlv89y3ivl5vngkifhbpidpwxdrh969l3c5r4klf";
+  vendorSha256 = "sha256:1p78nb7bryvs7p5iq6ihylflyjia60x4hd9c62ffwz37dwqlbi33";
+  yarnSha256 = "sha256:1n6swanrds9hbd4yyfjzpnfhsb8fzj1pwvvcg3w7b1cgnihclrmv";
 
   src = fetchFromGitHub {
     owner = "pomerium";
