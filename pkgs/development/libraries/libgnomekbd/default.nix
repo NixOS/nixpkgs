edc49c5699eb625b3e195b259453d69721344ee0
Jan Tojnar <jtojnar@gmail.com>
2022-09-04 18:56:51 +0000

    libgnomekbd: 3.28.0 → 3.28.1
    https://gitlab.gnome.org/GNOME/libgnomekbd/-/compare/3.28.0...3.28.1
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/development/libraries/libgnomekbd/default.nix
+++ pkgs/development/libraries/libgnomekbd/default.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libgnomekbd";
-  version = "3.28.0";
+  version = "3.28.1";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "swV6RD5MJvkf41Lc0RVKVs3VHxz9fLas4wDDzBKGWck=";
+    sha256 = "ItxZVm1zwAZTUPWpc0DmLsx7CMTfGRg4BLuL4kyP6HA=";
   };
 
   nativeBuildInputs = [
@@ -29,6 +29,7 @@ stdenv.mkDerivation rec {
     pkg-config
     gobject-introspection
     wrapGAppsHook
+    glib
   ];
 
   # Requires in libgnomekbd.pc
@@ -37,6 +38,11 @@ stdenv.mkDerivation rec {
     libxklavier
     glib
   ];
+
+  postInstall = ''
+    # Missing post-install script.
+    glib-compile-schemas "$out/share/glib-2.0/schemas"
+  '';
 
   passthru = {
     updateScript = gnome.updateScript {
