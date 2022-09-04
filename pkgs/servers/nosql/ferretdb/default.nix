d4742adab51c8a4b314d4c27d422bcd68eac54dd
Sandro <sandro.jaeckel@gmail.com>
2022-09-04 14:50:03 +0200

    Apply suggestions from code review

--- pkgs/servers/nosql/ferretdb/default.nix
+++ pkgs/servers/nosql/ferretdb/default.nix
@@ -13,17 +13,15 @@ buildGoModule rec {
     sha256 = "sha256-WSdscZ1/Dix83RE95Iv61rdaSBWx1GMi6qOIPNus+ZI=";
   };
 
+  postPatch = ''
+    echo ${version} > internal/util/version/gen/version.txt
+  '';
+
   vendorSha256 = "sha256-fGmGE08w9w2QnBVdMZ2IKo8Zq3euJGCBVTTHNKYFY3U=";
 
   CGO_ENABLED = 0;
 
   subPackages = [ "cmd/ferretdb" ];
-
-  # We cannot run the `go generate` command since it requires full git history
-  # (deepClone and leaveDotGit does not seem to help)
-  preBuild = ''
-    echo ${version} > internal/util/version/gen/version.txt
-  '';
 
   meta = with lib; {
     description = "A truly Open Source MongoDB alternative";
