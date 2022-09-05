88190f5443f2d4c43bf53a15e24c5c6e2228248d
Morgan Helton <mhelton@gmail.com>
2022-09-05 14:28:23 -0500

    pomerium: consolidate ui and base package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23099,9 +23099,7 @@ with pkgs;
   };
   pflogsumm = callPackage ../servers/mail/postfix/pflogsumm.nix { };
 
-  pomerium = callPackage ../servers/http/pomerium {
-    pomerium-ui = callPackage ../servers/http/pomerium/ui.nix { };
-  };
+  pomerium = callPackage ../servers/http/pomerium { };
   pomerium-cli = callPackage ../tools/security/pomerium-cli { };
 
   postgrey = callPackage ../servers/mail/postgrey { };
