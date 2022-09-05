6bcea10f5830f0c9377c565fe4e9cb1d9d16563f
PedroHLC ☭ <root@pedrohlc.com>
2022-09-05 19:21:42 -0300

    SDL2: 2.0.22 -> 2.24.0

--- pkgs/development/libraries/SDL2/default.nix
+++ pkgs/development/libraries/SDL2/default.nix
@@ -59,11 +59,11 @@ with lib;
 
 stdenv.mkDerivation rec {
   pname = "SDL2";
-  version = "2.0.22";
+  version = "2.24.0";
 
   src = fetchurl {
     url = "https://www.libsdl.org/release/${pname}-${version}.tar.gz";
-    sha256 = "sha256-/ny/MSeILj/HJZp1oMtYViAnLFF0XThSq53YeWBpfy4=";
+    sha256 = "sha256-keTDSxdo+S05mweOFxRIxq8Yyv2nQ5h+0gZKKJVNbZc=";
   };
   dontDisableStatic = withStatic;
   outputs = [ "out" "dev" ];
