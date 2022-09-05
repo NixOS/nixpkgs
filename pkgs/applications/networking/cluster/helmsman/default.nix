448a951e4c87b585b281ea4bdd776c24b26a547a
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-05 16:10:15 +0000

    helmsman: 3.14.0 -> 3.15.0

--- pkgs/applications/networking/cluster/helmsman/default.nix
+++ pkgs/applications/networking/cluster/helmsman/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "helmsman";
-  version = "3.14.0";
+  version = "3.15.0";
 
   src = fetchFromGitHub {
     owner = "Praqma";
     repo = "helmsman";
     rev = "v${version}";
-    sha256 = "sha256-6ISQsCLiyW4GDFuVnQ+UWwfN2ZqW3m3ZN/FrYOd83DE=";
+    sha256 = "sha256-wKemvUk5UP4LweX9JxbsYlrjuVRgchkOvlHqRYvAB9A=";
   };
 
-  vendorSha256 = "sha256-AjRKPsCPHCDEPjqMJtl/hkrcGepj3neC9eSgsbT/DIc=";
+  vendorSha256 = "sha256-swUZqrQ0nJS66oyqn0ONmFutLR2mEnDviC8siQB19c8=";
 
   doCheck = false;
 
