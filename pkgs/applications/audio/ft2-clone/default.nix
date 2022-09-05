e4e77dc451b41595ab77dfe1aed09a3d063b13bc
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-05 14:12:18 +0000

    ft2-clone: 1.56 -> 1.57

--- pkgs/applications/audio/ft2-clone/default.nix
+++ pkgs/applications/audio/ft2-clone/default.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation rec {
   pname = "ft2-clone";
-  version = "1.56";
+  version = "1.57";
 
   src = fetchFromGitHub {
     owner = "8bitbubsy";
     repo = "ft2-clone";
     rev = "v${version}";
-    sha256 = "sha256-kSnsep6abE07Q1EpGEeX8e/2APc60TxR2MhLZxqW9WY=";
+    sha256 = "sha256-47MDroiO8zvYDdHe0350ukczzemRbesEzIXZ2KoXZUI=";
   };
 
   # Adapt the linux-only CMakeLists to darwin (more reliable than make-macos.sh)
