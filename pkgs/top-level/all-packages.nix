16f6463c6c095c775a7301f88c7a9ac7ba1e8ea6
Andrei Boar <andrey.boar@gmail.com>
2022-09-05 12:43:56 +0300

    parent 3c0c2a1b87a8988236ecb77acbbcaf111401fbc4 author Andrei Boar <andrey.boar@gmail.com> 1662371036 +0300 committer Andrei Boar <andrey.boar@gmail.com> 1662451006 +0300
    bob: init at 0.5.3
    
    bob remove trailing whitespace
    
    bob: update rev to match github
    
    bob: remove unintentional changes
    
    bob: add comment to explain why tests are disabled
    
    bob: exclude tui-example
    
    bob: remove git
    
    bob: change sha256 to hash
    
    bob: change vendorSha256 to vendorHash
    
    bob: add zuzuleinen to maintainers
    
    bob: add ldflags to add version + CGO_ENABLED = 0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15845,6 +15845,8 @@ with pkgs;
     wxGTK = wxGTK30;
   };
 
+  bob  = callPackage ../development/tools/build-managers/bob { };
+
   buck = callPackage ../development/tools/build-managers/buck { };
 
   build2 = callPackage ../development/tools/build-managers/build2 {
