d5d441d2c7d18a3582d4bdc076411ed957008d19
zendo <linzway@qq.com>
2022-09-05 15:14:30 +0800

    yacreader: 9.8.2 -> 9.9.1

--- pkgs/applications/graphics/yacreader/default.nix
+++ pkgs/applications/graphics/yacreader/default.nix
@@ -1,22 +1,22 @@
 { mkDerivation, lib, fetchFromGitHub, qmake, poppler, pkg-config, libunarr
-, libGLU, qtdeclarative, qtgraphicaleffects, qtmultimedia, qtquickcontrols
+, libGLU, qtdeclarative, qtgraphicaleffects, qtmultimedia, qtquickcontrols2
 , qtscript
 }:
 
 mkDerivation rec {
   pname = "yacreader";
-  version = "9.8.2";
+  version = "9.9.1";
 
   src = fetchFromGitHub {
     owner = "YACReader";
     repo = pname;
     rev = version;
-    sha256 = "sha256-Xvf0xXtMs3x1fPgAvS4GJXrZgDZWhzIgrOF4yECr7/g=";
+    sha256 = "sha256-D+ZmFMg9ZixZNUAMjPHwz7gcwKjG49lm5hTEqftbIrY=";
   };
 
   nativeBuildInputs = [ qmake pkg-config ];
   buildInputs = [ poppler libunarr libGLU qtmultimedia qtscript ];
-  propagatedBuildInputs = [ qtquickcontrols qtgraphicaleffects qtdeclarative ];
+  propagatedBuildInputs = [ qtquickcontrols2 qtgraphicaleffects qtdeclarative ];
 
   meta = {
     description = "A comic reader for cross-platform reading and managing your digital comic collection";
