45119d888016930b6a531941b9c34bca963f19bf
Fabian Affolter <mail@fabian-affolter.ch>
2022-09-05 09:46:48 +0200

    python310Packages.sensorpro-ble: init at 0.5.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9897,6 +9897,8 @@ in {
 
   sensor-state-data = callPackage ../development/python-modules/sensor-state-data { };
 
+  sensorpro-ble = callPackage ../development/python-modules/sensorpro-ble { };
+
   sensorpush-ble = callPackage ../development/python-modules/sensorpush-ble { };
 
   sentencepiece = callPackage ../development/python-modules/sentencepiece {
