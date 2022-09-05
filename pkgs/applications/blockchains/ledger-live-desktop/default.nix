a1b37896b5f286cfd5abf5083a6b1fa5559e49b8
Marek Generowicz <generowicz@gmail.com>
2022-09-05 14:36:14 +0300

    ledger-live-desktop: 2.46.0 -> 2.46.2
     ledger-live-desktop: 2.46.0 -> 2.46.2

--- pkgs/applications/blockchains/ledger-live-desktop/default.nix
+++ pkgs/applications/blockchains/ledger-live-desktop/default.nix
@@ -2,11 +2,11 @@
 
 let
   pname = "ledger-live-desktop";
-  version = "2.46.0";
+  version = "2.46.2";
 
   src = fetchurl {
     url = "https://download.live.ledger.com/${pname}-${version}-linux-x86_64.AppImage";
-    hash = "sha256-QbCiOzASqicd0Tns1sV9ZVoc/GmuoohB9wB/g0Z6uFA=";
+    hash = "sha256-fSfES+6+LCRTLdOy3RJaHzv0zRXkqT+0Rsdt2kSeU18=";
   };
 
   appimageContents = appimageTools.extractType2 {
