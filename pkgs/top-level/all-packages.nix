454a4cb1de0e59994b67dc78cdf9f5f78c61e039
Ethin Probst <harlydavidsen@gmail.com>
2022-09-06 14:05:40 -0500

    zecwallet: init at 1.8.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32600,6 +32600,8 @@ with pkgs;
 
   zecwallet-lite = callPackage ../applications/blockchains/zecwallet-lite { };
 
+  zecwallet = callPackage ../applications/blockchains/zecwallet { };
+
   lightwalletd = callPackage ../applications/blockchains/lightwalletd { };
 
   openethereum = callPackage ../applications/blockchains/openethereum { };
