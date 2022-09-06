7dd73c63a9890c06da4b7904314109f9bda7a377
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-09-06 02:56:33 +0200

    python3Packages.psycopg: 3.1 -> 3.1.1
    https://www.psycopg.org/psycopg3/docs/news.html#psycopg-3-1-1

--- pkgs/development/python-modules/psycopg/default.nix
+++ pkgs/development/python-modules/psycopg/default.nix
@@ -32,19 +32,20 @@
 
 let
   pname = "psycopg";
-  version = "3.1";
+  version = "3.1.1";
 
   src = fetchFromGitHub {
     owner = "psycopg";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-N0Qc8pSWN2NFZn06lYZ7DKMbk6H8aIByS+wDnOQ/O+Y=";
+    hash = "sha256-PrWHjs8PLmx7bgKtyhXaiSKmz9oT2OhXDkKd4xi7e0A=";
   };
 
   patches = [
     (substituteAll {
-      src = ./libpq.patch;
+      src = ./ctypes.patch;
       libpq = "${postgresql.lib}/lib/libpq${stdenv.hostPlatform.extensions.sharedLibrary}";
+      libc = "${stdenv.cc.libc}/lib/libc.so.6";
     })
   ];
 
