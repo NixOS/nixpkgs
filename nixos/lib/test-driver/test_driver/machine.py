02add9f600d38eba08d319a6c9adf18bde7873d8
Simon Bruder <simon@sbruder.de>
2022-09-06 14:32:09 +0200

    nixos/test-driver: fix formatting of closed port
    Co-authored-by: a-kenji <aks.kenji@protonmail.com>

--- nixos/lib/test-driver/test_driver/machine.py
+++ nixos/lib/test-driver/test_driver/machine.py
@@ -712,7 +712,7 @@ class Machine:
             status, _ = self.execute("nc -z localhost {}".format(port))
             return status != 0
 
-        with self.nested("waiting for TCP port {} to be closed"):
+        with self.nested("waiting for TCP port {} to be closed".format(port)):
             retry(port_is_closed)
 
     def start_job(self, jobname: str, user: Optional[str] = None) -> Tuple[int, str]:
