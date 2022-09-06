5c70ec42c561ed115375c930e8c185f9bf2d8ca6
Ivar Scholten <ivar.scholten@protonmail.com>
2022-09-06 02:57:08 +0200

    nodejs-18_x: fix cross compilation to aarch64-linux
    This adds a patch reverting https://github.com/nodejs/node/pull/43200 because it breaks
    cross compilation to aarch64-linux. Gcc would not recognize the
    `-msign-return-address=all` flag causing compilation to fail.

--- pkgs/development/web/nodejs/v18.nix
+++ pkgs/development/web/nodejs/v18.nix
@@ -1,4 +1,4 @@
-{ callPackage, python3, enableNpm ? true }:
+{ callPackage, fetchpatch, python3, enableNpm ? true }:
 
 let
   buildNodejs = callPackage ./nodejs.nix {
@@ -10,6 +10,14 @@ buildNodejs {
   version = "18.8.0";
   sha256 = "sha256-K12YJdBe3mYU8WaKjZfXdP6S68gQiOxf31gYTc48hrk=";
   patches = [
+    (fetchpatch {
+      # Fixes cross compilation to aarch64-linux by reverting https://github.com/nodejs/node/pull/43200
+      name = "revert-arm64-pointer-auth.patch";
+      url = "https://github.com/nodejs/node/pull/43200/commits/d42c42cc8ac652ab387aa93205aed6ece8a5040a.patch";
+      sha256 = "sha256-ipGzg4lEoftTJbt6sW+0QJO/AZqHvUkFKe0qlum+iLY=";
+      revert = true;
+    })
+
     ./disable-darwin-v8-system-instrumentation.patch
   ];
 }
