5c5d26b9e769bdfceaaa2d53423afe937bb7dca0
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-09-06 15:57:52 +0200

    maintainers: remove longkeyid for akkesm

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -496,7 +496,6 @@
     github = "akkesm";
     githubId = 56970006;
     keys = [{
-      longkeyid = "ed25519/0x0D6BFC01D45EDADD";
       fingerprint = "50E2 669C AB38 2F4A 5F72  1667 0D6B FC01 D45E DADD";
     }];
   };
