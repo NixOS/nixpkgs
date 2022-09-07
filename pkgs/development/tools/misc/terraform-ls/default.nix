2cee143ba9a6e6684b401cf7b519ec8d0298e0b5
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-07 20:45:48 +0000

    terraform-ls: 0.28.1 -> 0.29.2

--- pkgs/development/tools/misc/terraform-ls/default.nix
+++ pkgs/development/tools/misc/terraform-ls/default.nix
@@ -2,15 +2,15 @@
 
 buildGoModule rec {
   pname = "terraform-ls";
-  version = "0.28.1";
+  version = "0.29.2";
 
   src = fetchFromGitHub {
     owner = "hashicorp";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-6K4aOp5mjX+qhG/OS/Gs1kAOpDGUPdgRNx4zp3i/c2A=";
+    sha256 = "sha256-oPBk5mTCq8nn4olC9Z7ROvrfAUXDxnWhm8w20sh5Wkw=";
   };
-  vendorSha256 = "sha256-YouAdTo7huco35er84MRfI1gmq11VbFwRGSovs1XDYo=";
+  vendorSha256 = "sha256-5Pb1mr3rYPcWFLjETAZp8rLW32n+RyCm7NbfooM4hZs=";
 
   ldflags = [ "-s" "-w" "-X main.version=v${version}" "-X main.prerelease=" ];
 
