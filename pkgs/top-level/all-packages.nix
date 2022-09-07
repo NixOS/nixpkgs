546414f7f3bff8c38f52c689390cfd53c4bc5ef9
Alain Zscheile <zseri.devel@ytrizja.de>
2022-09-08 01:50:12 +0200

    zix: init at unstable-2022-09-08

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22314,6 +22314,8 @@ with pkgs;
 
   zita-resampler = callPackage ../development/libraries/audio/zita-resampler { };
 
+  zix = callPackage ../development/libraries/zix { };
+
   zz = callPackage ../development/compilers/zz { };
 
   zziplib = callPackage ../development/libraries/zziplib { };
