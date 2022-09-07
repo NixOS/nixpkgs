f548decfff50e5e011a6d87f205e1a9a4931daa5
night <903bdd24-af4f-4d5a-8499-d5008a128f26@potato.io>
2022-09-07 02:30:52 -0700

    gvisor: 20210518.0 -> 20220905.0
    switch gvisor to build with synthetic go branch instead of bazel.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28076,8 +28076,6 @@ with pkgs;
 
   gvisor = callPackage ../applications/virtualization/gvisor { };
 
-  gvisor-containerd-shim = callPackage ../applications/virtualization/gvisor/containerd-shim.nix { };
-
   guvcview = libsForQt5.callPackage ../os-specific/linux/guvcview { };
 
   gwc = callPackage ../applications/audio/gwc { };
