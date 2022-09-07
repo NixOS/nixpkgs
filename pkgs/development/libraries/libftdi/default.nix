e536e3e822fbaaa3f5cc8e4dcb6154b973e39520
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-09-07 20:06:01 +0200

    libftdi: enable strictDeps, fix build, cleanup

--- pkgs/development/libraries/libftdi/default.nix
+++ pkgs/development/libraries/libftdi/default.nix
@@ -9,25 +9,33 @@ stdenv.mkDerivation rec {
     sha256 = "13l39f6k6gff30hsgh0wa2z422g9pyl91rh8a8zz6f34k2sxaxii";
   };
 
-  buildInputs = [ libusb-compat-0_1 ];
-
-  propagatedBuildInputs = [ libusb-compat-0_1 ];
-
-  # Hack to avoid TMPDIR in RPATHs.
-  preFixup = ''rm -rf "$(pwd)" '';
-  configureFlags = lib.optional (!stdenv.isDarwin) "--with-async-mode";
-
-  # allow async mode. from ubuntu. see:
-  #   https://bazaar.launchpad.net/~ubuntu-branches/ubuntu/trusty/libftdi/trusty/view/head:/debian/patches/04_async_mode.diff
-  patchPhase = ''
+  postPatch = ''
+    # allow async mode. from ubuntu. see:
+    #   https://bazaar.launchpad.net/~ubuntu-branches/ubuntu/trusty/libftdi/trusty/view/head:/debian/patches/04_async_mode.diff
     substituteInPlace ./src/ftdi.c \
       --replace "ifdef USB_CLASS_PTP" "if 0"
   '';
 
-  meta = {
+  strictDeps = true;
+
+  nativeBuildInputs = [ libusb-compat-0_1 ];
+
+  buildInputs = [ libusb-compat-0_1 ];
+
+  propagatedBuildInputs = [ libusb-compat-0_1 ];
+
+  configureFlags = lib.optional (!stdenv.isDarwin) "--with-async-mode";
+
+  preFixup = ''
+    # Hack to avoid TMPDIR in RPATHs.
+    rm -rf "$(pwd)"
+  '';
+
+  meta = with lib; {
     description = "A library to talk to FTDI chips using libusb";
     homepage = "https://www.intra2net.com/en/developer/libftdi/";
-    license = lib.licenses.lgpl21;
-    platforms = lib.platforms.all;
+    license = licenses.lgpl21;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ ];
   };
 }
