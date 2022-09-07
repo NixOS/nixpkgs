22540cbab9ec1e2e01e229b67fca8306f742202c
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-07 17:02:34 +0000

    nextcloud-client: 3.5.4 -> 3.6.0

--- pkgs/applications/networking/nextcloud-client/default.nix
+++ pkgs/applications/networking/nextcloud-client/default.nix
@@ -26,7 +26,7 @@
 
 mkDerivation rec {
   pname = "nextcloud-client";
-  version = "3.5.4";
+  version = "3.6.0";
 
   outputs = [ "out" "dev" ];
 
@@ -34,7 +34,7 @@ mkDerivation rec {
     owner = "nextcloud";
     repo = "desktop";
     rev = "v${version}";
-    sha256 = "sha256-ennKNUuoyhpVfG1XPzJKrop0i77BAB3C2MNqZPOXNkc=";
+    sha256 = "sha256-wAxq5xFlofn2xEguvewMvGcel9O+CN/1AycR3tv/xMA=";
   };
 
   patches = [
