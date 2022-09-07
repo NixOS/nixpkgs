f98c14998891d4d9e69c210095b0cce05631a184
Enno Richter <enno@nerdworks.de>
2022-09-07 12:00:10 +0200

    python3Packages.django-stubs-ext: init at 0.5.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2616,6 +2616,8 @@ in {
 
   django-storages = callPackage ../development/python-modules/django-storages { };
 
+  django-stubs-ext = callPackage ../development/python-modules/django-stubs-ext { };
+
   django-tables2 = callPackage ../development/python-modules/django-tables2 { };
 
   django_tagging = callPackage ../development/python-modules/django_tagging { };
