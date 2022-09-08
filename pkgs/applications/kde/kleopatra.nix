cbffc686f43e6307a52ffc9a1bb1766954b3ceef
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2022-09-08 12:03:24 -0300

    Revert "kleopatra: fix broken build"
    This reverts commit a4fda24f16d374ce70eb97f2186f417b5753221a.

--- pkgs/applications/kde/kleopatra.nix
+++ pkgs/applications/kde/kleopatra.nix
@@ -1,5 +1,5 @@
 {
-  mkDerivation, fetchpatch, lib, kdepimTeam,
+  mkDerivation, lib, kdepimTeam,
   extra-cmake-modules, kdoctools,
   boost, gpgme, kcmutils, kdbusaddons, kiconthemes, kitemmodels, kmime,
   knotifications, kwindowsystem, kxmlgui, libkleo, kcrash
@@ -7,13 +7,6 @@
 
 mkDerivation {
   pname = "kleopatra";
-
-  patches = [
-    (fetchpatch {
-      url = "https://invent.kde.org/pim/kleopatra/-/commit/87d8b00d4b2286489d5fadc9cfa07f1d721cdfe3.patch";
-      sha256 = "sha256-s1tXB7h0KtFwwZHx8rhpI0nLZmwhWAiraHEF3KzncMc=";
-    })
-  ];
 
   nativeBuildInputs = [ extra-cmake-modules kdoctools ];
 
