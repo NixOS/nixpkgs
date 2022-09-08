17b08ee3c2306033f9b805be025cf85b1fbe1bd9
Nick Cao <nickcao@nichi.co>
2022-09-08 22:02:02 +0800

    hydra_unstable: 2022-08-08 -> 2022-09-08

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18767,7 +18767,7 @@ with pkgs;
 
   hwloc = callPackage ../development/libraries/hwloc {};
 
-  hydra_unstable = callPackage ../development/tools/misc/hydra/unstable.nix { nix = nixVersions.nix_2_9; };
+  hydra_unstable = callPackage ../development/tools/misc/hydra/unstable.nix { nix = nixVersions.nix_2_11; };
 
   hydra-cli = callPackage ../development/tools/misc/hydra-cli { };
 
