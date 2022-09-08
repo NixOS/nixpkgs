936891fbdfb55c16c3af741459fdb17ee0f55c0a
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-08 10:32:49 +0000

    librsvg: 2.54.4 -> 2.54.5

--- pkgs/development/libraries/librsvg/default.nix
+++ pkgs/development/libraries/librsvg/default.nix
@@ -27,7 +27,7 @@
 
 stdenv.mkDerivation rec {
   pname = "librsvg";
-  version = "2.54.4";
+  version = "2.54.5";
 
   outputs = [ "out" "dev" "installedTests" ] ++ lib.optionals withIntrospection [
     "devdoc"
@@ -35,7 +35,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "6hUqJD9qQ8DgNqKMcN4/y83qVmTGgRx4WSvCKezCSDM=";
+    sha256 = "TwMZD0UyTR+h9Sp5383tH2Tq9Js64viO7asMB2F8rm4=";
   };
 
   cargoVendorDir = "vendor";
