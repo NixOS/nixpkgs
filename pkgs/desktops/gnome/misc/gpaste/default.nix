09e57173e6aaa068c58904c95c85983c053d3bc8
Jan Tojnar <jtojnar@gmail.com>
2022-09-08 15:21:56 +0200

    gnome.gpaste: Upgrade for GNOME 43

--- pkgs/desktops/gnome/misc/gpaste/default.nix
+++ pkgs/desktops/gnome/misc/gpaste/default.nix
@@ -9,6 +9,7 @@
 , gobject-introspection
 , gtk3
 , gtk4
+, gcr_4
 , libadwaita
 , meson
 , mutter
@@ -16,6 +17,7 @@
 , pango
 , pkg-config
 , vala
+, desktop-file-utils
 , wrapGAppsHook
 }:
 
@@ -32,6 +34,28 @@ stdenv.mkDerivation rec {
 
   patches = [
     ./fix-paths.patch
+
+    # Fix GNOME 43 compatibility.
+    (fetchpatch {
+      url = "https://github.com/Keruspe/GPaste/commit/eece9c374a823234bc20e58bf95bb10ace9590f4.patch";
+      sha256 = "+GoiNsge+ki2X3OIoFA+r3K8WLN00nExYj8gDuCSNiA=";
+    })
+    (fetchpatch {
+      url = "https://github.com/Keruspe/GPaste/commit/614bbe65e7d8f2ddca447daaada86156334bb71a.patch";
+      sha256 = "q6fjBTVRx6/5xf5ZhQJpvnwvroLiNxF73P7vTFTmNqI=";
+    })
+    (fetchpatch {
+      url = "https://github.com/Keruspe/GPaste/commit/971e690a74b4de6b78bdaf1300507a6190ffb474.patch";
+      sha256 = "bCLnuLqLgq7BlZN49NFrQjbCJBneVHl1t+WnAAIofkY=";
+    })
+    (fetchpatch {
+      url = "https://github.com/Keruspe/GPaste/commit/0378cb4a657042ce5321f1d9728cff31e55bede6.patch";
+      sha256 = "0Ngr+/fS5/wICR84GEiE0pXEXQ/f/3G59lDivH167m8=";
+    })
+    (fetchpatch {
+      url = "https://github.com/Keruspe/GPaste/commit/6d29385f8c935b1def28340bc165ca6a7756f29b.patch";
+      sha256 = "crkkIrLwogRzr1JfJEt5TPK65Y4zY1H5Gt+s0z3A8Dg=";
+    })
   ];
 
   # TODO: switch to substituteAll with placeholder
@@ -52,6 +76,7 @@ stdenv.mkDerivation rec {
     ninja
     pkg-config
     vala
+    desktop-file-utils
     wrapGAppsHook
   ];
 
@@ -61,6 +86,7 @@ stdenv.mkDerivation rec {
     glib
     gtk3
     gtk4
+    gcr_4
     libadwaita
     mutter
     pango
@@ -71,10 +97,6 @@ stdenv.mkDerivation rec {
     "-Ddbus-services-dir=${placeholder "out"}/share/dbus-1/services"
     "-Dsystemd-user-unit-dir=${placeholder "out"}/etc/systemd/user"
   ];
-
-  postInstall = ''
-    ${glib.dev}/bin/glib-compile-schemas "$out/share/glib-2.0/schemas"
-  '';
 
   meta = with lib; {
     homepage = "https://github.com/Keruspe/GPaste";
