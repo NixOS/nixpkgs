1aa243c67fe13dc82fd2d7a2f263d8550e5e4b76
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-08 03:45:06 +0000

    gnome-desktop: 42.3 -> 42.4

--- pkgs/development/libraries/gnome-desktop/default.nix
+++ pkgs/development/libraries/gnome-desktop/default.nix
@@ -27,13 +27,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-desktop";
-  version = "42.3";
+  version = "42.4";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-desktop/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-2lBBC48Z/X53WwDR/g26Z/xeEVHe0pkVjcJd2tw/qKk=";
+    sha256 = "sha256-HOLJ1QZ5advgsoLqWprPuGmHUfA80H4scwJA+F3JrSU=";
   };
 
   patches = lib.optionals stdenv.isLinux [
