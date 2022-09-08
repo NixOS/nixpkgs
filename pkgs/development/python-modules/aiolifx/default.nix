e63137d0c644e841d34c11909d8a50224171c191
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-08 17:26:09 +0000

    python310Packages.aiolifx: 0.8.2 -> 0.8.4

--- pkgs/development/python-modules/aiolifx/default.nix
+++ pkgs/development/python-modules/aiolifx/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "aiolifx";
-  version = "0.8.2";
+  version = "0.8.4";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-k47cXi2CDtFIV3gzfdYU4i17ry0ABXcWK5CcWhwTdT0=";
+    hash = "sha256-r5bBomAoRwlII8pT/2o+0va/oBr3s8b8v2jq9n6WDwI=";
   };
 
   propagatedBuildInputs = [
