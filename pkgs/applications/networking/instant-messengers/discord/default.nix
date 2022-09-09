09e010b4996cc75a3dd91eb76687a44e30f2e973
Infinidoge <infinidoge@doge-inc.net>
2022-09-09 08:34:03 -0400

    discord-canary: 0.0.138 -> 0.0.139

--- pkgs/applications/networking/instant-messengers/discord/default.nix
+++ pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -3,7 +3,7 @@ let
   versions = if stdenv.isLinux then {
     stable = "0.0.19";
     ptb = "0.0.29";
-    canary = "0.0.138";
+    canary = "0.0.139";
   } else {
     stable = "0.0.264";
     ptb = "0.0.59";
@@ -22,7 +22,7 @@ let
       };
       canary = fetchurl {
         url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-        sha256 = "sha256-NojoHrrgdvLiMgWYPClXzWjWXuvHz7urhyHzMnZwvBY=";
+        sha256 = "sha256-/PfO0TWRxMrK+V1XkYmdaXQ6SfyJNBFETaR9oV90itI=";
       };
     };
     aarch64-darwin = {
