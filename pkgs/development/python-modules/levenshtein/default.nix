567e9a6fc734ddeeead8c6a4a0dcbf8c0615aa53
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-09 07:43:29 +0000

    python310Packages.levenshtein: 0.20.2 -> 0.20.3

--- pkgs/development/python-modules/levenshtein/default.nix
+++ pkgs/development/python-modules/levenshtein/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "levenshtein";
-  version = "0.20.2";
+  version = "0.20.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.6";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "maxbachmann";
     repo = "Levenshtein";
     rev = "refs/tags/v${version}";
-    hash = "sha256-zVYfErh9tts3yPgXqqeX6xp8o+gLd7nN64+Ml6YZfjE=";
+    hash = "sha256-oPG7qAzsUPKfLxjriS4/1fFjRu+FkceP87h2vC0OTBE=";
   };
 
   nativeBuildInputs = [
