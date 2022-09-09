7199d45dd9a3cfc352e7924cd0c07df3b41757dd
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-09 03:31:28 +0000

    v2ray-geoip: 202208180100 -> 202209080101

--- pkgs/data/misc/v2ray-geoip/default.nix
+++ pkgs/data/misc/v2ray-geoip/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "v2ray-geoip";
-  version = "202208180100";
+  version = "202209080101";
 
   src = fetchFromGitHub {
     owner = "v2fly";
     repo = "geoip";
-    rev = "005c33be4dd95339596ddd5ce792e8f97dd168a3";
-    sha256 = "sha256-KvEmgtbelZOauE2WBTzJkwJkaUVW2x8ezgmTE+Gbwu8=";
+    rev = "2e77e5d149f0a8f9c284333b206d0f017b0b66ef";
+    sha256 = "sha256-vkWRBSwLpCqZWMlfwOyPWn2MF+/lG+VXnSrDCSR+dak=";
   };
 
   installPhase = ''
