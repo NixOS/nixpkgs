9546e430be1cd76e8c6fe432943556da7b5a670e
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-09 18:47:51 +0000

    go-task: 3.14.1 -> 3.15.2

--- pkgs/development/tools/go-task/default.nix
+++ pkgs/development/tools/go-task/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "go-task";
-  version = "3.14.1";
+  version = "3.15.2";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = "task";
     rev = "v${version}";
-    sha256 = "sha256-GbCrMsMxhSjJOsZX4Gq9ZzBJ+F5vXDMi9vSyFrHNt44=";
+    sha256 = "sha256-UeKb+v9mHKCwQAGzaYQ0aRi7oCZOOIP1dal0ro3iwzI=";
   };
 
   vendorSha256 = "sha256-xp1s1aixPyXq9oVD8IZYSlUiL8UkIx5c8gYJRpIRD7I=";
