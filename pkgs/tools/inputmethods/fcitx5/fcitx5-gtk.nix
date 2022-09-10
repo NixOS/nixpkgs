b7029e3c81682170acab831a5c4594c05a3f14f6
Vonfry <mail@vonfry.name>
2022-09-10 08:50:28 +0800

    fcitx5-gtk: 5.0.17 -> 5.0.18

--- pkgs/tools/inputmethods/fcitx5/fcitx5-gtk.nix
+++ pkgs/tools/inputmethods/fcitx5/fcitx5-gtk.nix
@@ -26,13 +26,13 @@
 
 stdenv.mkDerivation rec {
   pname = "fcitx5-gtk";
-  version = "5.0.17";
+  version = "5.0.18";
 
   src = fetchFromGitHub {
     owner = "fcitx";
     repo = pname;
     rev = version;
-    sha256 = "sha256-sAPbbMoZ4NGiE7lbtcdzQQsPib6i52JwWsLe+bmTshU=";
+    sha256 = "sha256-rQ2HLiI0dIerxRV+fbHpTJy4aGmFKmGd6YckKxXmp4s=";
   };
 
   cmakeFlags = [
