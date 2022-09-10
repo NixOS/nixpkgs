8445a54f9c155dc170f47644c15b9b64ba771f0a
Arik Grahl <mail@arik-grahl.de>
2022-09-10 14:44:01 +0200

    reformat `rev` and `ldflags`

--- pkgs/tools/misc/octosql/default.nix
+++ pkgs/tools/misc/octosql/default.nix
@@ -12,13 +12,13 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner  = "cube2222";
     repo   = pname;
-    rev    = "v" + version;
+    rev    = "v${version}";
     sha256 = "sha256-Y6kKYW79415nCJkcIKQjcBQiFZrRCJ8If65lV9wmNFA=";
   };
 
   vendorSha256 = "sha256-ukNjLk1tTdw0bwXaYAEDuHfzxHuAX1xyqRqC6wmW/H4=";
 
-  ldflags = [ "-s" "-w" "-X" "github.com/cube2222/octosql/cmd.VERSION=${version}" ];
+  ldflags = [ "-s" "-w" "-X github.com/cube2222/octosql/cmd.VERSION=${version}" ];
 
   postInstall = ''
     rm -v $out/bin/tester
