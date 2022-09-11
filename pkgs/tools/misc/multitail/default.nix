fe87f6ee53bd8093fce5b4a7dcfbf9f4d9f9204b
Matthias Beyer <mail@beyermatthias.de>
2022-09-11 10:44:12 +0200

    multitail: 6.5.0 -> 6.5.2
    This patch also moves to use the github fork of multitail, which seems
    to be the maintained version of the project now.
    
    This update removes the need for patches.
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/tools/misc/multitail/default.nix
+++ pkgs/tools/misc/multitail/default.nix
@@ -1,23 +1,15 @@
-{ lib, stdenv, fetchurl, fetchpatch, ncurses, pkg-config }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch, ncurses, pkg-config }:
 
 stdenv.mkDerivation rec {
-  version = "6.5.0";
+  version = "6.5.2";
   pname = "multitail";
 
-  src = fetchurl {
-    url = "https://www.vanheusden.com/multitail/${pname}-${version}.tgz";
-    sha256 = "1vd9vdxyxsccl64ilx542ya5vlw2bpg6gnkq1x8cfqy6vxvmx7dj";
+  src = fetchFromGitHub {
+    owner = "halturin";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-DMYcoHOaX4bToDE8qpXmg5tbTYGkiwtPJd0/qy4uD54=";
   };
-
-  patches = [
-    # Fix pending upstream inclusion for ncurses-6.3:
-    #  https://github.com/halturin/multitail/pull/4
-    (fetchpatch {
-      name = "ncurses-6.3.patch";
-      url = "https://github.com/halturin/multitail/commit/d7d10f3bce261074c116eba9f924b61f43777662.patch";
-      sha256 = "0kyp9l6v92mz6d3h34j11gs5kh3sf2nv76mygqfxb800vd8r0cgg";
-    })
-  ];
 
   nativeBuildInputs = [ pkg-config ];
 
