47324bf9644095836459fc8715f0085698dc030f
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-09-12 14:41:18 +0200

    python3Packages.wxPython_4_0: propagate six

--- pkgs/development/python-modules/wxPython/4.0.nix
+++ pkgs/development/python-modules/wxPython/4.0.nix
@@ -20,6 +20,7 @@
 , CoreFoundation
 , pillow
 , numpy
+, six
 }:
 
 buildPythonPackage rec {
@@ -44,7 +45,11 @@ buildPythonPackage rec {
     [ wxGTK.gtk ]
   );
 
-  propagatedBuildInputs = [ pillow numpy ];
+  propagatedBuildInputs = [
+    numpy
+    pillow
+    six
+  ];
 
   DOXYGEN = "${doxygen}/bin/doxygen";
 
