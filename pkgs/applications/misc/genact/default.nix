174e579bd529667fddb76b00594d77f4ea614bc7
figsoda <figsoda@pm.me>
2022-09-13 11:03:15 -0400

    genact: 1.0.2 -> 1.1.1

--- pkgs/applications/misc/genact/default.nix
+++ pkgs/applications/misc/genact/default.nix
@@ -1,17 +1,32 @@
-{ lib, rustPlatform, fetchFromGitHub }:
+{ lib, rustPlatform, fetchFromGitHub, jq }:
 
 rustPlatform.buildRustPackage rec {
   pname = "genact";
-  version = "1.0.2";
+  version = "1.1.1";
 
   src = fetchFromGitHub {
     owner = "svenstaro";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-lZNVXBIYl9niqdwNcSzQwQTdxlA4kKQ/WrEt93cQDJU=";
+    sha256 = "sha256-Mw6mPOxiWnYu2QgqL4VccwwJhdxZ7zLJyX/oJWfGUhw=";
   };
 
-  cargoSha256 = "sha256-9IiA7KAaj9bLJ7QSB/ojLEiUVv0FGYsu9by4NSfMtiE=";
+  cargoSha256 = "sha256-ygQklcRjdffGl0s77MwKsyHVJWqWJZHq4SU38cSMVug=";
+
+  depsExtraArgs = {
+    nativeBuildInputs = [ jq ];
+    postBuild = ''
+      pushd $name/humansize
+
+      [ -d feature-tests ] && rm -r feature-tests
+
+      jq '.files |= with_entries(select(.key | startswith("feature-tests") | not))' \
+        -c .cargo-checksum.json > .cargo-checksum.json.new
+      mv .cargo-checksum.json{.new,}
+
+      popd
+    '';
+  };
 
   meta = with lib; {
     description = "A nonsense activity generator";
