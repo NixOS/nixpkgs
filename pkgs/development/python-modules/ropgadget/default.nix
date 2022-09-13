57ff57107036272f1cc9c3198937f9fb005d9063
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-13 08:21:21 +0000

    ropgadget: 7.0 -> 7.1

--- pkgs/development/python-modules/ropgadget/default.nix
+++ pkgs/development/python-modules/ropgadget/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "ropgadget";
-  version = "7.0";
+  version = "7.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -16,7 +16,7 @@ buildPythonPackage rec {
     owner = "JonathanSalwan";
     repo = "ROPgadget";
     rev = "refs/tags/v${version}";
-    hash = "sha256-7pGPi1efgxLoxoiqtxzfcSWuNw0otAVgamEzs8stBtc=";
+    hash = "sha256-eVRQQ8VmjT8wTpiTK2Emr1tEpSwFJPbMc1fa2Zh3mT0=";
   };
 
   propagatedBuildInputs = [
