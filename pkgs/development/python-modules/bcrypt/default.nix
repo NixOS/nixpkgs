aec89c8c93142d791b1cf48347b2c547d7f0e652
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-09-14 23:36:03 +0200

    python3Packages.bcrypt: 3.2.2 -> 4.0.0

--- pkgs/development/python-modules/bcrypt/default.nix
+++ pkgs/development/python-modules/bcrypt/default.nix
@@ -1,32 +1,45 @@
 { lib
 , buildPythonPackage
+, rustPlatform
 , setuptools
+, setuptools-rust
 , isPyPy
 , fetchPypi
 , pythonOlder
 , cffi
 , pytestCheckHook
-, six
 }:
 
 buildPythonPackage rec {
   pname = "bcrypt";
-  version = "3.2.2";
+  version = "4.0.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-QzxBDCF3BXcF2iqfLNAd0VdJOyp6wUyFk6FrPatra/s=";
+    hash = "sha256-xZwXD8kiX6rQTd4bph2FtBOUbozi5fX1/zDf1nKD8xk=";
+  };
+
+  cargoRoot = "src/_bcrypt";
+  cargoDeps = rustPlatform.fetchCargoTarball {
+    inherit src;
+    sourceRoot = "${pname}-${version}/${cargoRoot}";
+    name = "${pname}-${version}";
+    hash = "sha256-HvfRLyUhlXVuvxWrtSDKx3rMKJbjvuiMcDY6g+pYFS0=";
   };
 
   nativeBuildInputs = [
     setuptools
-  ];
+    setuptools-rust
+    rustPlatform.cargoSetupHook
+  ] ++ (with rustPlatform; [
+    rust.cargo
+    rust.rustc
+  ]);
 
   propagatedBuildInputs = [
-    six
     cffi
   ];
 
