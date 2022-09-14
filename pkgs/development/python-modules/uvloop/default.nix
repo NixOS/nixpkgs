72fcca65884a975393affbc507467c2dc736cb16
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-09-14 23:29:34 +0200

    python3Packages.uvloop: 0.16.0 -> 0.17.0
    https://github.com/MagicStack/uvloop/releases/tag/v0.17.0

--- pkgs/development/python-modules/uvloop/default.nix
+++ pkgs/development/python-modules/uvloop/default.nix
@@ -3,10 +3,12 @@
 , buildPythonPackage
 , pythonOlder
 , fetchPypi
+, cython
 , libuv
 , CoreServices
 , ApplicationServices
-  # Check Inputs
+
+# Check Inputs
 , aiohttp
 , psutil
 , pyopenssl
@@ -15,13 +17,18 @@
 
 buildPythonPackage rec {
   pname = "uvloop";
-  version = "0.16.0";
+  version = "0.17.0";
+  format = "setuptools";
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f74bc20c7b67d1c27c72601c78cf95be99d5c2cdd4514502b4f3eb0933ff1228";
+    hash = "sha256-Dd9rr5zxGhoixxSH858Vss9461vefltF+7meip2RueE=";
   };
+
+  nativeBuildInputs = [
+    cython
+  ];
 
   buildInputs = [
     libuv
@@ -32,9 +39,10 @@ buildPythonPackage rec {
 
   dontUseSetuptoolsCheck = true;
   checkInputs = [
-    aiohttp
     pytestCheckHook
     psutil
+  ] ++ lib.optionals (pythonOlder "3.11") [
+    aiohttp
   ];
 
   LIBUV_CONFIGURE_HOST = stdenv.hostPlatform.config;
@@ -46,16 +54,21 @@ buildPythonPackage rec {
     "--strict"
     "--tb=native"
     # Depend on pyopenssl
-    "--deselect tests/test_tcp.py::Test_UV_TCPSSL::test_flush_before_shutdown"
-    "--deselect tests/test_tcp.py::Test_UV_TCPSSL::test_renegotiation"
+    "--deselect=tests/test_tcp.py::Test_UV_TCPSSL::test_flush_before_shutdown"
+    "--deselect=tests/test_tcp.py::Test_UV_TCPSSL::test_renegotiation"
     # test gets stuck in epoll_pwait on hydras aarch64 builders
     # https://github.com/MagicStack/uvloop/issues/412
-    "--deselect tests/test_tcp.py::Test_AIO_TCPSSL::test_remote_shutdown_receives_trailing_data"
+    "--deselect=tests/test_tcp.py::Test_AIO_TCPSSL::test_remote_shutdown_receives_trailing_data"
+    # Tries to import cythonized file for which the .pyx file is not shipped via PyPi
+    "--deselect=tests/test_libuv_api.py::Test_UV_libuv::test_libuv_get_loop_t_ptr"
+    # Tries to run "env", but fails to find it
+    "--deselect=tests/test_process.py::Test_UV_Process::test_process_env_2"
+    "--deselect=tests/test_process.py::Test_AIO_Process::test_process_env_2"
   ] ++ lib.optionals (stdenv.isDarwin && stdenv.isAarch64) [
     # Flaky test: https://github.com/MagicStack/uvloop/issues/412
-    "--deselect tests/test_tcp.py::Test_UV_TCPSSL::test_shutdown_timeout_handler_not_set"
+    "--deselect=tests/test_tcp.py::Test_UV_TCPSSL::test_shutdown_timeout_handler_not_set"
     # Broken: https://github.com/NixOS/nixpkgs/issues/160904
-    "--deselect tests/test_context.py::Test_UV_Context::test_create_ssl_server_manual_connection_lost"
+    "--deselect=tests/test_context.py::Test_UV_Context::test_create_ssl_server_manual_connection_lost"
   ];
 
   disabledTestPaths = [
