6026c06b47c03006d5964cad84645b02bdd847da
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-09-14 23:36:02 +0200

    python3Packages.azure-mgmt-datafactory: 2.7.0 -> 2.8.0

--- pkgs/development/python-modules/azure-mgmt-datafactory/default.nix
+++ pkgs/development/python-modules/azure-mgmt-datafactory/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "azure-mgmt-datafactory";
-  version = "2.7.0";
+  version = "2.8.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    hash = "sha256-g7av5MFy4QNg+81PqDGznHXUZsHhnaauaJV/B6GMy4A=";
+    hash = "sha256-LH2YdWuRC5mzImF6ImkOixHBcgc7Yoi0Rai5m64/aiU=";
   };
 
   propagatedBuildInputs = [
