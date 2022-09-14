a95999ea7f2973823d2c5ecdbb72196b0a87bf4b
nanashi0x74 <rian.lindenberger@gmail.com>
2022-09-14 19:44:07 +0200

    flutter: 3.3.0->3.3.3, flutter.dart: 2.18.0->2.18.2

--- pkgs/development/compilers/flutter/default.nix
+++ pkgs/development/compilers/flutter/default.nix
@@ -4,20 +4,20 @@ let
   getPatches = dir:
     let files = builtins.attrNames (builtins.readDir dir);
     in map (f: dir + ("/" + f)) files;
-  version = "3.3.0";
+  version = "3.3.3";
   channel = "stable";
   filename = "flutter_linux_${version}-${channel}.tar.xz";
 
   # Decouples flutter derivation from dart derivation,
   # use specific dart version to not need to bump dart derivation when bumping flutter.
-  dartVersion = "2.18.0";
+  dartVersion = "2.18.2";
   dartSourceBase = "https://storage.googleapis.com/dart-archive/channels";
   dartForFlutter = dart.override {
     version = dartVersion;
     sources = {
       "${dartVersion}-x86_64-linux" = fetchurl {
         url = "${dartSourceBase}/stable/release/${dartVersion}/sdk/dartsdk-linux-x64-release.zip";
-        sha256 = "sha256-45HE7Y9iO5dI+JfLWF1ikFfBFB+er46bK+EYkyuhFjI=";
+        sha256 = "sha256-C3+YjecXLvSmJrLwi9H7TgD9Np0AArRWx3EdBrfQpTU";
       };
     };
   };
@@ -30,7 +30,7 @@ in
     pname = "flutter";
     src = fetchurl {
       url = "https://storage.googleapis.com/flutter_infra_release/releases/${channel}/linux/${filename}";
-      sha256 = "sha256-qSonqm1EVNehz5+KClbg5daGXyz80hz1Llf3kirV1QQ=";
+      sha256 = "sha256-MTZeWQUp4/TcPzYIT6eqIKSPUPvn2Mp/thOQzNgpTXg=";
     };
     patches = getPatches ./patches;
   };
