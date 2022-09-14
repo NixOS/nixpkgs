0deef1278741ae354864d388e627f987e0eb5ee3
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-09-14 23:36:26 +0200

    python3Packages.xmlsec: 1.3.12 -> 1.3.13

--- pkgs/development/python-modules/xmlsec/default.nix
+++ pkgs/development/python-modules/xmlsec/default.nix
@@ -15,18 +15,13 @@
 
 buildPythonPackage rec {
   pname = "xmlsec";
-  version = "1.3.12";
+  version = "1.3.13";
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "2c86ac6ce570c9e04f04da0cd5e7d3db346e4b5b1d006311606368f17c756ef9";
+    sha256 = "sha256-kW9deOgEH2zZORq7plnajJSk/vcZbRJtQK8f9Bfyz4Y=";
   };
-
-  # https://github.com/mehcode/python-xmlsec/issues/84#issuecomment-632930116
-  patches = [
-    ./reset-lxml-in-tests.patch
-  ];
 
   nativeBuildInputs = [ pkg-config pkgconfig setuptools-scm ];
 
