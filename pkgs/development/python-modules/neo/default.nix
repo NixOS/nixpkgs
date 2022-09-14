fbfa383ecca08ebd903100abddf9bd04d1dd21b9
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-09-14 23:36:13 +0200

    python3Packages.neo: 0.10.2 -> 0.11.0

--- pkgs/development/python-modules/neo/default.nix
+++ pkgs/development/python-modules/neo/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "neo";
-  version = "0.10.2";
+  version = "0.11.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-LUIYsIJtruqIDhVSJwYAKew4oAI4zrXwlxONlGfGOZs=";
+    sha256 = "sha256-zfjhMko/u9Hv1WGNzTfPxJexmXkjvXELWYRywdhGZ0o=";
   };
 
   propagatedBuildInputs = [
