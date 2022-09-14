62a8bd56c34cfd76c681eab66198949ec2c327b9
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-09-14 23:36:18 +0200

    python3Packages.pytest-check: 1.0.5 -> 1.0.9

--- pkgs/development/python-modules/pytest-check/default.nix
+++ pkgs/development/python-modules/pytest-check/default.nix
@@ -7,17 +7,21 @@
 
 buildPythonPackage rec {
   pname = "pytest-check";
-  version = "1.0.5";
+  version = "1.0.9";
   format = "flit";
 
   src = fetchPypi {
-    pname = "pytest_check";
-    inherit version;
-    sha256 = "sha256-ucjbax3uPB5mFivQebgcCDKWy3Ex6YQVGcKKQIGKSHU=";
+    inherit pname version;
+    hash = "sha256-zVMQTzpPw2RPcCi1XiOHZvbWWhKiB9MbzUyLoA2yP9k=";
   };
 
-  buildInputs = [ pytest ];
-  checkInputs = [ pytestCheckHook ];
+  buildInputs = [
+    pytest
+  ];
+
+  checkInputs = [
+    pytestCheckHook
+  ];
 
   meta = with lib; {
     description = "pytest plugin allowing multiple failures per test";
