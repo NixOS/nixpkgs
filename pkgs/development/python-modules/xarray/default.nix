a65fa982b1c3b48b64ac319b8a0c9d6f6318e80f
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-09-14 23:36:25 +0200

    python3Packages.xarray: 2022.3.0 -> 2022.6.0

--- pkgs/development/python-modules/xarray/default.nix
+++ pkgs/development/python-modules/xarray/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "xarray";
-  version = "2022.3.0";
+  version = "2022.6.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-OYNEv30XBHeqzv9wIQ4R69aa9rFW/hOXgFTSXEhylEA=";
+    sha256 = "sha256-ECjRmEk/ZrsVvTXc/dEd79gxy+469lif/xb0G93WfoQ=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION="${version}";
