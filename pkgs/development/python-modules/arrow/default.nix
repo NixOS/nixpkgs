c869d701e0ea230286cfb548661bf5cce05d233b
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-09-14 23:36:01 +0200

    python3Packages.arrow: 1.2.2 -> 1.2.3

--- pkgs/development/python-modules/arrow/default.nix
+++ pkgs/development/python-modules/arrow/default.nix
@@ -12,13 +12,13 @@
 
 buildPythonPackage rec {
   pname = "arrow";
-  version = "1.2.2";
+  version = "1.2.3";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-Bcrx/T2aEaETWytvCYh0IRU7lFWOXvTQkLVntHFzrCs=";
+    sha256 = "sha256-OTSzDKG58pI3bZ2xWxlEYIjRLsWGKbw/DaKP1V+2M6E=";
   };
 
   postPatch = ''
