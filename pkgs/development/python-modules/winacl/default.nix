252a188b0553aee7f3a4247929fc5e35473d175d
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-09-14 23:36:25 +0200

    python3Packages.winacl: 0.1.3 -> 0.1.4

--- pkgs/development/python-modules/winacl/default.nix
+++ pkgs/development/python-modules/winacl/default.nix
@@ -1,23 +1,29 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, cryptography
 , pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "winacl";
-  version = "0.1.3";
+  version = "0.1.4";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-G6xWep0hMACCqiJGuw+UpZH8qOIY4WO6sY3w4y7v6gY=";
+    hash = "sha256-sgpTQovEps19e05sgfznyiP9k2SwKsjcC1ZTfAHeQqQ=";
   };
+
+  propagatedBuildInputs = [
+    cryptography
+  ];
 
   # Project doesn't have tests
   doCheck = false;
+
   pythonImportsCheck = [ "winacl" ];
 
   meta = with lib; {
