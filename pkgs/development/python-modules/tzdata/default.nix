68936c07d4a7686140c0e4b7cbaf1e5aeed3d04a
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-09-14 23:36:24 +0200

    python3Packages.tzdata: 2022.1 -> 2022.2

--- pkgs/development/python-modules/tzdata/default.nix
+++ pkgs/development/python-modules/tzdata/default.nix
@@ -9,12 +9,12 @@
 
 buildPythonPackage rec {
   pname = "tzdata";
-  version = "2022.1";
+  version = "2022.2";
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-i1NqjsY9wHUTQrOYQZOjEY+Pyir+JXUrubf//TmFUtM=";
+    hash = "sha256-IfTw1yQVcu+n96T9q7BS5htV3EgnTmhCaXzN9SU+VFE=";
   };
 
   checkInputs = [
