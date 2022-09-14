e31374099020eb9951fbef016dbd9b77603e7ad5
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-09-14 14:32:59 +0200

    python3Packages.dulwich: 0.20.45 -> 0.20.46
    - Move from implicit testing to pytestCheckHook.
    - Inherit gnupg instead of passing pkgs around
    - Disable three failing tests
    - Disable test paths where test inputs are missing from the package

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2806,7 +2806,9 @@ in {
 
   dugong = callPackage ../development/python-modules/dugong { };
 
-  dulwich = callPackage ../development/python-modules/dulwich { };
+  dulwich = callPackage ../development/python-modules/dulwich {
+    inherit (pkgs) gnupg;
+  };
 
   dunamai = callPackage ../development/python-modules/dunamai { };
 
