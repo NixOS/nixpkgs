00650ff6ea96eeaf3e675458f2b26afe8cd4b6fb
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-15 20:18:52 +0000

    jpegoptim: 1.4.7 -> 1.5.0

--- pkgs/applications/graphics/jpegoptim/default.nix
+++ pkgs/applications/graphics/jpegoptim/default.nix
@@ -1,14 +1,14 @@
 { lib, stdenv, fetchFromGitHub, libjpeg }:
 
 stdenv.mkDerivation rec {
-  version = "1.4.7";
+  version = "1.5.0";
   pname = "jpegoptim";
 
   src = fetchFromGitHub {
     owner = "tjko";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-qae3OEG4CC/OGkmNdHrXFUv9CkoaB1ZJnFHX3RFoxhk=";
+    sha256 = "sha256-fTtNDjswxHv2kHU55RCzz9tdlXw+RUCSoe3qF4hQ7u4=";
   };
 
   # There are no checks, it seems.
