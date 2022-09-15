d71f2f87c995fb5e1274b6de0afbd4090080c591
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-15 09:45:22 +0000

    galene: 0.5.5 -> 0.6

--- pkgs/servers/web-apps/galene/default.nix
+++ pkgs/servers/web-apps/galene/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "galene";
-  version = "0.5.5";
+  version = "0.6";
 
   src = fetchFromGitHub {
     owner = "jech";
     repo = "galene";
     rev = "galene-${version}";
-    hash = "sha256-J1cX8mm1yZTvsaz7DOAbmjmXkdoIrs793aDnmHm8BFk=";
+    hash = "sha256-0KzUOFU5n80IcU/Hb05qDwgLsipOq51R7dk3isEPr64=";
   };
 
   vendorSha256 = "sha256-HZQeVa4UB/1jpPbfrh3XgWQe2S3qA8CM268KghgJA0w=";
