091ba87395abbb5cc686e162feee4ac8e95d67f7
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-09-15 22:30:20 +0200

    python3Packages.radicale_infcloud: 2017-07-27 -> unstable-2022-04-18

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9398,7 +9398,9 @@ in {
 
   rachiopy = callPackage ../development/python-modules/rachiopy { };
 
-  radicale_infcloud = callPackage ../development/python-modules/radicale_infcloud { };
+  radicale_infcloud = callPackage ../development/python-modules/radicale_infcloud {
+    radicale = pkgs.radicale.override { python3 = python; };
+  };
 
   radio_beam = callPackage ../development/python-modules/radio_beam { };
 
