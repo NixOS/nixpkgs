30b4d26971ded3655dc805afc7e869c346448896
Rickard Nilsson <rickynils@gmail.com>
2022-09-15 22:29:09 +0200

    beam-modules/rebar3Relx: Make sure executable flag is preserved when copying checkouts

--- pkgs/development/beam-modules/rebar3-release.nix
+++ pkgs/development/beam-modules/rebar3-release.nix
@@ -61,8 +61,10 @@ let
 
       configurePhase = ''
         runHook preConfigure
-        ${lib.optionalString (checkouts != null)
-        "cp --no-preserve=all -R ${checkouts}/_checkouts ."}
+        ${lib.optionalString (checkouts != null) ''
+          cp -R ${checkouts}/_checkouts .
+          chmod -R u+w _checkouts
+        ''}
         runHook postConfigure
       '';
 
