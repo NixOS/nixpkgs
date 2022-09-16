7f3b2bc06018d0f7458857e2388ed9c5e9b64bd1
Timothy Stott <stott.timothy@gmail.com>
2022-09-16 15:58:48 +0100

    rubyPackages.tty-command: init at 0.10.1

--- pkgs/top-level/ruby-packages.nix
+++ pkgs/top-level/ruby-packages.nix
@@ -2152,6 +2152,17 @@
     };
     version = "1.0.3";
   };
+  pastel = {
+    dependencies = ["tty-color"];
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0xash2gj08dfjvq4hy6l1z22s5v30fhizwgs10d6nviggpxsj7a8";
+      type = "gem";
+    };
+    version = "0.8.0";
+  };
   pathutil = {
     dependencies = ["forwardable-extended"];
     groups = ["default"];
@@ -3096,6 +3107,27 @@
       type = "gem";
     };
     version = "1.6.11";
+  };
+  tty-color = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0aik4kmhwwrmkysha7qibi2nyzb4c8kp42bd5vxnf8sf7b53g73g";
+      type = "gem";
+    };
+    version = "0.6.0";
+  };
+  tty-command = {
+    dependencies = ["pastel"];
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "14hi8xiahfrrnydw6g3i30lxvvz90wp4xsrlhx8mabckrcglfv0c";
+      type = "gem";
+    };
+    version = "0.10.1";
   };
   tty-option = {
     groups = ["default"];
