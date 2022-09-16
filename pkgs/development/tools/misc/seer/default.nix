fdb85bf063ca267e4387142561debc3dd6e48ca5
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-16 03:34:53 +0000

    seer: 1.9 -> 1.10

--- pkgs/development/tools/misc/seer/default.nix
+++ pkgs/development/tools/misc/seer/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "seer";
-  version = "1.9";
+  version = "1.10";
 
   src = fetchFromGitHub {
     owner = "epasveer";
     repo = "seer";
     rev = "v${version}";
-    sha256 = "sha256-YIxKjykhuxTAr0kEmlj2JFA2NRs1vknpoJAGEoshwtg=";
+    sha256 = "sha256-G8kiLZBRS8Ec8LYsbppmyYZcNk3By0bcfWQFyI5epZ4=";
   };
 
   preConfigure = ''
