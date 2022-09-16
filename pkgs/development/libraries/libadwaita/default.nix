3445e9aeab43bc1e6eae58a8e62d91a5deb9e614
Jan Tojnar <jtojnar@gmail.com>
2022-09-16 23:40:32 +0000

    libadwaita: 1.2.rc → 1.2.0
    https://gitlab.gnome.org/GNOME/libadwaita/-/compare/1.2.rc...1.2.0

--- pkgs/development/libraries/libadwaita/default.nix
+++ pkgs/development/libraries/libadwaita/default.nix
@@ -20,7 +20,7 @@
 
 stdenv.mkDerivation rec {
   pname = "libadwaita";
-  version = "1.2.rc";
+  version = "1.2.0";
 
   outputs = [ "out" "dev" "devdoc" ];
   outputBin = "devdoc"; # demo app
@@ -30,7 +30,7 @@ stdenv.mkDerivation rec {
     owner = "GNOME";
     repo = "libadwaita";
     rev = version;
-    hash = "sha256-p7nsaAqrzQKeUiu7aGlgoKu0AH9KV/sMsVcLLpgl4Lo=";
+    hash = "sha256-3lH7Vi9M8k+GSrCpvruRpLrIpMoOakKbcJlaAc/FK+U=";
   };
 
   nativeBuildInputs = [
