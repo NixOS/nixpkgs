38b688f59b06a618da1011725d51693f3aeff8f5
Ethin Probst <harlydavidsen@gmail.com>
2022-09-16 06:22:23 -0500

    blightmud: 3.5.0 -> 4.0.0

--- pkgs/games/blightmud/default.nix
+++ pkgs/games/blightmud/default.nix
@@ -3,16 +3,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "blightmud";
-  version = "3.5.0";
+  version = "4.0.0";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-DaICzwBew90YstV42wiY0IbvR1W4Hm8dzo3xY2qlMGQ=";
+    hash = "sha256-80uTwbZjXoQnfP0VWM/CnvaHyRsPA1puASJwBm3GqJk=";
   };
 
-  cargoSha256 = "sha256-BamMTPh+GN9GG4puxyTauPhjCC8heCu1wsgFaw98s9U=";
+  cargoSha256 = "sha256-uXr/5G0TH3EKqPwUKTwOguBExBEYB1D3/hr8nzOwCcM=";
 
   buildFeatures = lib.optional withTTS "tts";
 
@@ -56,6 +56,7 @@ rustPlatform.buildRustPackage rec {
       "test_lua_script"
       "timer_test"
       "validate_assertion_fail"
+      "test_is_connected"
     ];
     skipFlag = test: "--skip " + test;
   in builtins.concatStringsSep " " (builtins.map skipFlag skipList);
