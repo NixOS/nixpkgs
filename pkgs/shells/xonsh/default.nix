92112872cd8bd0aeaf02ae241bfc26717d260081
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-16 09:28:28 +0000

    xonsh: 0.13.1 -> 0.13.3

--- pkgs/shells/xonsh/default.nix
+++ pkgs/shells/xonsh/default.nix
@@ -8,14 +8,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "xonsh";
-  version = "0.13.1";
+  version = "0.13.3";
 
   # fetch from github because the pypi package ships incomplete tests
   src = fetchFromGitHub {
     owner = "xonsh";
     repo = "xonsh";
-    rev = version;
-    sha256 = "sha256-Q9FJXccpTW3nPUOCf5UD8ZWJW25QX8PNHHpsVYjesYE=";
+    rev = "refs/tags/${version}";
+    sha256 = "sha256-COm+MZUbiFTB5EaOB+1+lIef1IfhQ95Ya1MmnJXGu6A=";
   };
 
   LC_ALL = "en_US.UTF-8";
