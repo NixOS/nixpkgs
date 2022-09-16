4f079c273a6dd61951d0d4144e4535a175f0b1b9
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-16 01:11:57 +0000

    prometheus-fastly-exporter: 7.2.4 -> 7.2.5

--- pkgs/servers/monitoring/prometheus/fastly-exporter.nix
+++ pkgs/servers/monitoring/prometheus/fastly-exporter.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "fastly-exporter";
-  version = "7.2.4";
+  version = "7.2.5";
 
   src = fetchFromGitHub {
     owner = "peterbourgon";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-dg2JPVZJSjbBirvKvfQHGi06Fah48RHk5vbHgn5Q59M=";
+    sha256 = "sha256-W/P4jUBNDR3t7FESNyUUnNWfGR0PI/dG03EVKYt8S2Y=";
   };
 
-  vendorSha256 = "sha256-wsOgZTeErUQkt+yJ7P0Oi8Ks7WBj/e457lZNs+ZwJgY=";
+  vendorSha256 = "sha256-exoDUxcOXVn7wUqfLLtJpklPYFHjLodEYa3lF+qFD+A=";
 
   meta = with lib; {
     description = "Prometheus exporter for the Fastly Real-time Analytics API";
