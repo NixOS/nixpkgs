551fa1fe57fbdcfab7773bd115cd76e56c3f0889
Brendon Boldt <brendon.mb@protonmail.com>
2022-09-17 09:03:26 -0400

    qmltermwidget: 2018-11-24 -> unstable-2022-09-16

--- pkgs/development/libraries/qmltermwidget/default.nix
+++ pkgs/development/libraries/qmltermwidget/default.nix
@@ -1,27 +1,19 @@
-{ lib, stdenv, fetchFromGitHub, qtbase, qtquick1, qmake, qtmultimedia, utmp, fetchpatch }:
+{ lib, stdenv, fetchFromGitHub, qtbase, qtquick1, qmake, qtmultimedia, utmp }:
 
 stdenv.mkDerivation {
-  version = "2018-11-24";
   pname = "qmltermwidget-unstable";
+  version = "unstable-2022-09-16";
 
   src = fetchFromGitHub {
     repo = "qmltermwidget";
     owner = "Swordfish90";
-    rev = "48274c75660e28d44af7c195e79accdf1bd44963";
-    sha256 = "028nb1xp84jmakif5mmzx52q3rsjwckw27jdpahyaqw7j7i5znq6";
+    rev = "63228027e1f97c24abb907550b22ee91836929c5";
+    sha256 = "aVaiRpkYvuyomdkQYAgjIfi6a3wG2a6hNH1CfkA2WKQ=";
   };
 
   buildInputs = [ qtbase qtquick1 qtmultimedia ]
                 ++ lib.optional stdenv.isDarwin utmp;
   nativeBuildInputs = [ qmake ];
-
-  patches = [
-    (fetchpatch {
-      name = "fix-missing-includes.patch";
-      url = "https://github.com/Swordfish90/qmltermwidget/pull/27/commits/485f8d6d841b607ba49e55a791f7f587e4e193bc.diff";
-      sha256 = "186s8pv3642vr4lxsds919h0y2vrkl61r7wqq9mc4a5zk5vprinj";
-    })
-  ];
 
   postPatch = ''
     substituteInPlace qmltermwidget.pro \
