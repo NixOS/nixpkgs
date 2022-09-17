7ade6e7023a1ff5cb0e87f5312f66e078268998d
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-17 01:34:31 +0000

    python310Packages.fastcore: 1.5.26 -> 1.5.27

--- pkgs/development/python-modules/fastcore/default.nix
+++ pkgs/development/python-modules/fastcore/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "fastcore";
-  version = "1.5.26";
+  version = "1.5.27";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "fastai";
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-WA6EgdyZ6zQGCzeQsHUD304WMCarjhGEpqXXBhBsxNw=";
+    sha256 = "sha256-LFkjxcotJoHIX0GdKKqUSFF4/HSWc/sLwb34iuBrQIg=";
   };
 
   propagatedBuildInputs = [
