2383d17fc2e15d79b3b298c9694eaac1cbc0f5f3
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-17 10:22:34 +0000

    rofimoji: 5.5.0 -> 5.6.0

--- pkgs/applications/misc/rofimoji/default.nix
+++ pkgs/applications/misc/rofimoji/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonApplication rec {
   pname = "rofimoji";
-  version = "5.5.0";
+  version = "5.6.0";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "fdw";
     repo = "rofimoji";
     rev = "refs/tags/${version}";
-    sha256 = "sha256-rYqEeAoHCx0j83R1vmtj+CVuR0QFEd3e1c5O454mANM=";
+    sha256 = "sha256-6W/59DjxrgejHSkNxpruDAws812Vjyf+GePDPbXzVbc=";
   };
 
   # `rofi` and the `waylandSupport` and `x11Support` dependencies
