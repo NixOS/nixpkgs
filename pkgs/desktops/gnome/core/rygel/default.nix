06c2b7d2206d504b9c90b62f125c5522d05df0d2
Jan Tojnar <jtojnar@gmail.com>
2022-09-17 20:56:57 +0000

    gnome.rygel: 0.41.2 → 0.42.0
    https://gitlab.gnome.org/GNOME/rygel/-/compare/rygel-0.41.2...rygel-0.42.0
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/rygel/default.nix
+++ pkgs/desktops/gnome/core/rygel/default.nix
@@ -28,14 +28,14 @@
 
 stdenv.mkDerivation rec {
   pname = "rygel";
-  version = "0.41.2";
+  version = "0.42.0";
 
   # TODO: split out lib
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "sRhMYUsSYYrFgHqCgflD9/c7SypKZw1HViUEC4FpO3I=";
+    sha256 = "AG2nhnffKMruKHxEwp/NntyxshYkI7x/LdLchWuOK7s=";
   };
 
   patches = [
