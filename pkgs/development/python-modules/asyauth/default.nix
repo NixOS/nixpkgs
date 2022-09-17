24b4c08a1e013f4be1d578a651dfb7ab825d24a1
Fabian Affolter <mail@fabian-affolter.ch>
2022-09-17 13:02:54 +0200

    python310Packages.asyauth: init at 0.0.3

---
+++ pkgs/development/python-modules/asyauth/default.nix
@@ -0,0 +1,43 @@
+{ lib
+, asn1crypto
+, asysocks
+, buildPythonPackage
+, fetchPypi
+, minikerberos
+, pythonOlder
+, unicrypto
+}:
+
+buildPythonPackage rec {
+  pname = "asyauth";
+  version = "0.0.3";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-8qSYaPgidfDH8aebLFtLhLxt253xTHTkC0xwV346KdM=";
+  };
+
+  propagatedBuildInputs = [
+    asn1crypto
+    asysocks
+    minikerberos
+    unicrypto
+  ];
+
+  # Project doesn't have tests
+  doCheck = false;
+
+  pythonImportsCheck = [
+    "asyauth"
+  ];
+
+  meta = with lib; {
+    description = "Unified authentication library";
+    homepage = "https://github.com/skelsec/asyauth";
+    license = with licenses; [ mit ];
+    maintainers = with maintainers; [ fab ];
+  };
+}
