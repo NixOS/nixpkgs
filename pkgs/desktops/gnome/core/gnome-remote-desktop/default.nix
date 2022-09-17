56926182650e7ec207ac6bcddf436e938176cf45
Jan Tojnar <jtojnar@gmail.com>
2022-09-17 20:51:06 +0000

    gnome.gnome-remote-desktop: 43.rc → 43.0
    https://gitlab.gnome.org/GNOME/gnome-remote-desktop/-/compare/43.rc...43.0

--- pkgs/desktops/gnome/core/gnome-remote-desktop/default.nix
+++ pkgs/desktops/gnome/core/gnome-remote-desktop/default.nix
@@ -30,11 +30,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-remote-desktop";
-  version = "43.rc";
+  version = "43.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    hash = "sha256-dLT4QCC3pgMu3Clh3691sqHVHYE/eW1zSbOtMg+78I0=";
+    hash = "sha256-NrWdIeyuRLllzZHmynwMAJ1gcOwxyEt3wERZXddSeAs=";
   };
 
   nativeBuildInputs = [
