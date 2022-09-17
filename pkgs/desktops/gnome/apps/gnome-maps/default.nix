3eba6bd218c4eb4dfaf450a1e5bf0d3fc4bbd3da
Jan Tojnar <jtojnar@gmail.com>
2022-09-17 20:51:01 +0000

    gnome.gnome-maps: 43.rc → 43.0
    https://gitlab.gnome.org/GNOME/gnome-maps/-/compare/v43.rc...v43.0
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/apps/gnome-maps/default.nix
+++ pkgs/desktops/gnome/apps/gnome-maps/default.nix
@@ -1,7 +1,6 @@
 { stdenv
 , lib
 , fetchurl
-, fetchpatch
 , meson
 , ninja
 , gettext
@@ -28,25 +27,12 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-maps";
-  version = "43.rc";
+  version = "43.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-NBqS8OTo+lOaOggrdYXYA/4Od7byiF7ztq9zZxKSQ5k=";
+    sha256 = "sha256-CGjPz7eMOiesW+YM2E0wuz08KMiFqY+qCeN/o6UyNOQ=";
   };
-
-  patches = [
-    # Fix build without GTK 3.
-    # https://gitlab.gnome.org/GNOME/gnome-maps/-/merge_requests/248
-    (fetchpatch {
-      url = "https://gitlab.gnome.org/GNOME/gnome-maps/-/merge_requests/248.patch";
-      sha256 = "koVpvz021b+rvauIc3kvEBdTttRSuYiPvDH/dVbs5pc=";
-      postFetch = ''
-        # Make the patch apply.
-        substituteInPlace "$out" --replace "version: '43.0'," "version: '43.rc',"
-      '';
-    })
-  ];
 
   doCheck = true;
 
