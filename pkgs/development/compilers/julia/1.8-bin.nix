e992f35ba872aab78c980c153d01a67288bb8937
Nick Cao <nickcao@nichi.co>
2022-09-18 21:17:31 +0800

    julia_18-bin: add support for aarch64-linux

--- pkgs/development/compilers/julia/1.8-bin.nix
+++ pkgs/development/compilers/julia/1.8-bin.nix
@@ -9,6 +9,10 @@ stdenv.mkDerivation rec {
       url = "https://julialang-s3.julialang.org/bin/linux/x64/${lib.versions.majorMinor version}/julia-${version}-linux-x86_64.tar.gz";
       sha256 = "sha256-MwVO5kfuik+1T8BREOB+C1PgRZH+U9Cky0x+16BekfE=";
     };
+    aarch64-linux = fetchurl {
+      url = "https://julialang-s3.julialang.org/bin/linux/aarch64/${lib.versions.majorMinor version}/julia-${version}-linux-aarch64.tar.gz";
+      sha256 = "sha256-ugaDesKJlUe7t5mYnxFGT+zWeCImhxw7ekhhlIEEJnk=";
+    };
   }.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 
   postPatch = ''
@@ -54,7 +58,7 @@ stdenv.mkDerivation rec {
     # Bundled and linked with various GPL code, although Julia itself is MIT.
     license = lib.licenses.gpl2Plus;
     maintainers = with lib.maintainers; [ ninjin raskin ];
-    platforms = [ "x86_64-linux" ];
+    platforms = [ "x86_64-linux" "aarch64-linux" ];
     mainProgram = "julia";
   };
 }
