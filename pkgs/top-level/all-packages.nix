b6d0414b229541c79597b0abdfb302412897f3d9
Fabian Affolter <mail@fabian-affolter.ch>
2022-09-18 17:01:44 +0200

    ldapnomnom: init at unstable-2022-09-18

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8515,6 +8515,8 @@ with pkgs;
 
   ldapmonitor = callPackage ../tools/security/ldapmonitor { };
 
+  ldapnomnom = callPackage ../tools/security/ldapnomnom { };
+
   ldapvi = callPackage ../tools/misc/ldapvi { };
 
   ldeep = python3Packages.callPackage ../tools/security/ldeep { };
