a8c8cfd042f1bf58594282048dbaf2495c5c0207
Nikolay Korotkiy <sikmir@disroot.org>
2022-09-18 19:47:52 +0300

    s2geometry: 0.9.0 → 0.10.0

--- pkgs/development/libraries/s2geometry/default.nix
+++ pkgs/development/libraries/s2geometry/default.nix
@@ -1,32 +1,24 @@
-{ stdenv, lib, fetchFromGitHub, fetchpatch, cmake, pkg-config, openssl, gtest }:
+{ stdenv, lib, fetchFromGitHub, cmake, pkg-config, openssl, gtest, abseil-cpp }:
 
 stdenv.mkDerivation rec {
   pname = "s2geometry";
-  version = "0.9.0";
+  version = "0.10.0";
 
   src = fetchFromGitHub {
     owner = "google";
     repo = "s2geometry";
     rev = "v${version}";
-    sha256 = "1mx61bnn2f6bd281qlhn667q6yfg1pxzd2js88l5wpkqlfzzhfaz";
+    hash = "sha256-FV3F4mkgl5O8dPUFzsYzaSksp412W5TREmNasPHQ2fg=";
   };
 
-  patches = [
-    # Fix build https://github.com/google/s2geometry/issues/165
-    (fetchpatch {
-      url = "https://github.com/google/s2geometry/commit/a4dddf40647c68cd0104eafc31e9c8fb247a6308.patch";
-      sha256 = "0fp3w4bg7pgf5vv4vacp9g06rbqzhxc2fg6i5appp93q6phiinvi";
-    })
-  ];
-
   nativeBuildInputs = [ cmake pkg-config ];
-  buildInputs = [ openssl gtest ];
+  buildInputs = [ openssl gtest abseil-cpp ];
 
   meta = with lib; {
     description = "Computational geometry and spatial indexing on the sphere";
     homepage = "http://s2geometry.io/";
     license = licenses.asl20;
     maintainers = [ maintainers.Thra11 ];
-    platforms = platforms.linux;
+    platforms = platforms.unix;
   };
 }
