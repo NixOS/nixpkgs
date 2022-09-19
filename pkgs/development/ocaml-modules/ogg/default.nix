721bbcd50e6df4811ae3a9594da69280498ce82f
superherointj <5861043+superherointj@users.noreply.github.com>
2022-09-19 09:26:20 -0300

    ocamlPackages.ogg: set minimalOCamlVersion to 4.08

--- pkgs/development/ocaml-modules/ogg/default.nix
+++ pkgs/development/ocaml-modules/ogg/default.nix
@@ -4,14 +4,14 @@ buildDunePackage rec {
   pname = "ogg";
   version = "0.7.3";
 
-  useDune2 = true;
-
   src = fetchFromGitHub {
     owner = "savonet";
     repo = "ocaml-ogg";
     rev = "v${version}";
     sha256 = "sha256-D6tLKBSGfWBoMfQrWmamd8jo2AOphJV9xeSm+l06L5c=";
   };
+
+  minimalOCamlVersion = "4.08";
 
   buildInputs = [ dune-configurator ];
   propagatedBuildInputs = [ libogg ];
