6e4e3252359f07bd009b16849c24a56cf7c4b544
Robert Scott <code@humanleg.org.uk>
2022-09-19 01:48:32 +0100

    lief: enable on darwin

--- pkgs/development/libraries/lief/default.nix
+++ pkgs/development/libraries/lief/default.nix
@@ -68,7 +68,7 @@ stdenv.mkDerivation rec {
     description = "Library to Instrument Executable Formats";
     homepage = "https://lief.quarkslab.com/";
     license = [ licenses.asl20 ];
-    platforms = platforms.linux;
+    platforms = platforms.unix;
     maintainers = [ maintainers.lassulus ];
   };
 }
