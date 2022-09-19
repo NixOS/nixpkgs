NixOS/nixpkgs

commit 4c510f32feb018050c85868b176cac91a5363d65

file: 5.42 -> 5.43
https://mailman.astron.com/pipermail/file/2022-September/000857.html

pkgs/tools/misc/file/default.nix
@@ -7,14 +7,14 @@
 
 stdenv.mkDerivation rec {
   pname = "file";
-  version = "5.42";
+  version = "5.43";
 
   src = fetchurl {
     urls = [
       "https://astron.com/pub/file/${pname}-${version}.tar.gz"
       "https://distfiles.macports.org/file/${pname}-${version}.tar.gz"
     ];
-    sha256 = "sha256-wHb7TQKcdAc/FcQzYe9XLPuGhAfTRxkLqDSvOxY5sOQ=";
+    sha256 = "sha256-jIAV6Rrg6NAyHZTHgjmJLvnbxwxK3gAIwOlYlKv7GZE=";
   };
 
   strictDeps = true;

commit e8b82e912f813f24516fffa31b81edfd01bfd7f1

file: disable tests on musl
Fixes: 4c510f32feb ("file: 5.42 -> 5.43")

pkgs/tools/misc/file/default.nix
@@ -24,7 +24,8 @@ stdenv.mkDerivation rec {
   buildInputs = [ zlib ]
     ++ lib.optional stdenv.hostPlatform.isWindows libgnurx;
 
-  doCheck = true;
+  # https://bugs.astron.com/view.php?id=382
+  doCheck = !stdenv.hostPlatform.isMusl;
 
   makeFlags = lib.optional stdenv.hostPlatform.isWindows "FILE_COMPILE=file";
 