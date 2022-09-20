3014744349013b6ea01e6a808ed1d8422026864f
Jan Tojnar <jtojnar@gmail.com>
2022-09-20 21:31:32 +0000

    gnome.gdm: 42.0 → 43.0
    https://gitlab.gnome.org/GNOME/gdm/-/compare/42.0...43.0
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/gdm/default.nix
+++ pkgs/desktops/gnome/core/gdm/default.nix
@@ -44,13 +44,13 @@ in
 
 stdenv.mkDerivation rec {
   pname = "gdm";
-  version = "42.0";
+  version = "43.0";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gdm/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "oyisl2k3vsF5lx/weCmhJGuYznJBgcEorjKguketOFU=";
+    sha256 = "lNcNbtffWfp/3k/QL+0RaFk6itzhD87hE8FI1Ss5IpQ=";
   };
 
   mesonFlags = [
