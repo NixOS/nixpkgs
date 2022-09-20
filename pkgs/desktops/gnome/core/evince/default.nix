619892bab4a2f465d7d235002800bae86fe05c10
Jan Tojnar <jtojnar@gmail.com>
2022-09-20 11:35:46 +0000

    evince: 43.alpha → 43.0
    https://gitlab.gnome.org/GNOME/evince/-/compare/43.alpha...43.0
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/evince/default.nix
+++ pkgs/desktops/gnome/core/evince/default.nix
@@ -31,7 +31,6 @@
 , gsettings-desktop-schemas
 , gnome-desktop
 , dbus
-, python3
 , texlive
 , gst_all_1
 , gi-docgen
@@ -43,19 +42,14 @@
 
 stdenv.mkDerivation rec {
   pname = "evince";
-  version = "43.alpha";
+  version = "43.0";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/evince/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "PPVNKPeX74sFBL67HBaKGA1N35JcuDqfnGn+DRie9hE=";
+    sha256 = "Zr4N5LR7ETBIYQOYjMFSwErqlZULo+8W3CDC72toHUc=";
   };
-
-  postPatch = ''
-    chmod +x meson_post_install.py
-    patchShebangs meson_post_install.py
-  '';
 
   nativeBuildInputs = [
     appstream
@@ -66,7 +60,6 @@ stdenv.mkDerivation rec {
     meson
     ninja
     pkg-config
-    python3
     wrapGAppsHook
     yelp-tools
   ];
