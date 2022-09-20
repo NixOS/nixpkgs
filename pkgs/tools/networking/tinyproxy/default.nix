b11d6bafbe7142e318f01934578df9bede959a0d
Robert Scott <code@humanleg.org.uk>
2022-09-20 21:49:53 +0100

    tinyproxy: add patch for CVE-2022-40468
    (cherry picked from commit 32f4104b6610d31a17cc06f71a88bc27a677f538)

--- pkgs/tools/networking/tinyproxy/default.nix
+++ pkgs/tools/networking/tinyproxy/default.nix
@@ -1,4 +1,11 @@
-{ lib, stdenv, fetchFromGitHub, autoreconfHook, perl, withDebug ? false }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, fetchpatch
+, autoreconfHook
+, perl
+, withDebug ? false
+}:
 
 stdenv.mkDerivation rec {
   pname = "tinyproxy";
@@ -10,6 +17,14 @@ stdenv.mkDerivation rec {
     repo = "tinyproxy";
     owner = "tinyproxy";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "CVE-2022-40468.patch";
+      url = "https://github.com/tinyproxy/tinyproxy/commit/3764b8551463b900b5b4e3ec0cd9bb9182191cb7.patch";
+      sha256 = "sha256-P0c4mUK227ld3703ss5MQhi8Vo2QVTCVXhKmc9fcufk=";
+    })
+  ];
 
   # perl is needed for man page generation.
   nativeBuildInputs = [ autoreconfHook perl ];
