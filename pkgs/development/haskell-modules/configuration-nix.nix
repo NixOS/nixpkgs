cbb22921dbf0b48ede636d816ad1b64cef13ce1c
maralorn <mail@maralorn.de>
2022-09-20 02:12:09 +0200

    haskellPackages.hie-bios: Remove unused 0.5.0 override

--- pkgs/development/haskell-modules/configuration-nix.nix
+++ pkgs/development/haskell-modules/configuration-nix.nix
@@ -679,7 +679,6 @@ self: super: builtins.intersectAttrs super {
 
   # Tests access homeless-shelter.
   hie-bios = dontCheck super.hie-bios;
-  hie-bios_0_5_0 = dontCheck super.hie-bios_0_5_0;
 
   # Compiling the readme throws errors and has no purpose in nixpkgs
   aeson-gadt-th =
