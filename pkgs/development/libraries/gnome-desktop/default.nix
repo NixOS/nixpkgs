5b1ed07d2fcd7118dfb71b4a3751565caa669cdc
Jan Tojnar <jtojnar@gmail.com>
2022-09-20 11:37:30 +0000

    gnome-desktop: 43.rc → 43
    https://gitlab.gnome.org/GNOME/gnome-desktop/-/compare/43.rc...43
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/development/libraries/gnome-desktop/default.nix
+++ pkgs/development/libraries/gnome-desktop/default.nix
@@ -27,13 +27,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-desktop";
-  version = "43.rc";
+  version = "43";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-desktop/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-KiA9duBPDruj3Re/M0dKwZ/EdotuhPyRdxMu1M7KbO0=";
+    sha256 = "sha256-PW4VMxdIYVdZaqOAL4dnZBTFcHOPRQqUoEH+iDVCCmk=";
   };
 
   patches = lib.optionals stdenv.isLinux [
