e2df2572d6a8f1d206e405ba4b57c44467883556
yuu <yuuyin@protonmail.com>
2022-09-21 12:13:59 -0300

    python310Packages.python-decouple: init at 3.6

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9014,6 +9014,8 @@ in {
 
   python-dbusmock = callPackage ../development/python-modules/python-dbusmock { };
 
+  python-decouple = callPackage ../development/python-modules/python-decouple { };
+
   pythondialog = callPackage ../development/python-modules/pythondialog { };
 
   python-didl-lite = callPackage ../development/python-modules/python-didl-lite { };
