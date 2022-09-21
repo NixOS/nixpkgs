718d77d3b84ab78ec92899bb818d061eb6e269bc
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-21 19:35:46 +0000

    git-cliff: 0.9.0 -> 0.9.1

--- pkgs/applications/version-management/git-and-tools/git-cliff/default.nix
+++ pkgs/applications/version-management/git-and-tools/git-cliff/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "git-cliff";
-  version = "0.9.0";
+  version = "0.9.1";
 
   src = fetchFromGitHub {
     owner = "orhun";
     repo = "git-cliff";
     rev = "v${version}";
-    sha256 = "sha256-xmpT4xzajarc/1Iu0GqLthQlh0N9tJ1LdqJXrB4EN9Q=";
+    sha256 = "sha256-dtvA7wH2RNqaFnT8SbHHe3dCEBjWdEC21SWEWxI8XrU=";
   };
 
-  cargoSha256 = "sha256-f4tB37WiHompXjXqdVy+dNEIn5rhNqda8JTSVUOOlcE=";
+  cargoSha256 = "sha256-e6YifzTPZ6XS50F7rLomhgdXYY2WC24xLvnLkTj494U=";
 
   # attempts to run the program on .git in src which is not deterministic
   doCheck = false;
