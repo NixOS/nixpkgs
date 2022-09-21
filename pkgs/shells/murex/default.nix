d05298f61a4a9d728b3b7027135d17a895996f1d
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-21 23:44:50 +0000

    murex: 2.11.2000 -> 2.11.2030

--- pkgs/shells/murex/default.nix
+++ pkgs/shells/murex/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "murex";
-  version = "2.11.2000";
+  version = "2.11.2030";
 
   src = fetchFromGitHub {
     owner = "lmorg";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-PQFckkoNIpeYrPKuhg7K1y1MFYhmF7tCH+Dnh2RIsUI=";
+    sha256 = "sha256-/W0nNmMdjtkktwkNGHrxHHibqqbujC591nC4NwEG47U=";
   };
 
   vendorSha256 = "sha256-hLz36ESf6To6sT/ha/yXyhG0U1gGw8HDfnrPJnws25g=";
