a164ccd367625365bd86cb6c1f578fcf69d804ef
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-21 08:24:12 +0000

    kapp: 0.52.0 -> 0.53.0

--- pkgs/tools/networking/kapp/default.nix
+++ pkgs/tools/networking/kapp/default.nix
@@ -1,13 +1,13 @@
 { lib, buildGoModule, fetchFromGitHub, installShellFiles }:
 buildGoModule rec {
   pname = "kapp";
-  version = "0.52.0";
+  version = "0.53.0";
 
   src = fetchFromGitHub {
     owner = "vmware-tanzu";
     repo = "carvel-kapp";
     rev = "v${version}";
-    sha256 = "sha256-sCQD0hZTJaeueBTu4XPiPVC5dhPWkTQn0mKwWVT5H/0=";
+    sha256 = "sha256-y4afSca87vPWPPWbSMS/Q3Y6TZg8fl0eu0Vw3udp3zA=";
   };
 
   vendorSha256 = null;
