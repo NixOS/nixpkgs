41c914edb2d18f4892bbde263a66e65b2ffcfeb5
figsoda <figsoda@pm.me>
2022-09-21 09:48:25 -0400

    changie: fix version output, add figsoda to maintainers

--- pkgs/development/tools/changie/default.nix
+++ pkgs/development/tools/changie/default.nix
@@ -13,12 +13,14 @@ buildGoModule rec {
 
   vendorSha256 = "sha256-9Cpyemq/f62rVMvGwOtgDGd9XllvICXL2dqNwUoFQmg=";
 
+  ldflags = [ "-s" "-w" "-X=main.version=${version}" ];
+
   meta = with lib; {
     homepage = "https://changie.dev";
+    changelog = "https://github.com/miniscruff/changie/blob/v${version}/CHANGELOG.md";
     description = "Automated changelog tool for preparing releases with lots of customization options";
     license = licenses.mit;
     platforms = platforms.unix;
-    maintainers = with maintainers; [ matthiasbeyer ];
+    maintainers = with maintainers; [ figsoda matthiasbeyer ];
   };
 }
-
