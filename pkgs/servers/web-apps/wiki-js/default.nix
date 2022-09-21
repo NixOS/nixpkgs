076fd9b23592ee436f87e2b6a4dd4735ba4b74c1
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-21 07:54:48 +0000

    wiki-js: 2.5.288 -> 2.5.289
    (cherry picked from commit c15ada5b918ab5239a3d80558fa1268479fa66af)

--- pkgs/servers/web-apps/wiki-js/default.nix
+++ pkgs/servers/web-apps/wiki-js/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "wiki-js";
-  version = "2.5.288";
+  version = "2.5.289";
 
   src = fetchurl {
     url = "https://github.com/Requarks/wiki/releases/download/v${version}/${pname}.tar.gz";
-    sha256 = "sha256-3Bfkmv/DP7L37puhHarfe6ntwJ1d/2v0UMAabcv0Gco=";
+    sha256 = "sha256-hHUVHsRRlwFxHriaf7uHsaxRvQmeOKFHvz/taooK4YM=";
   };
 
   sourceRoot = ".";
