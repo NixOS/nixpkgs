b0e0e80ddc637603482cebe4c17d7ea87b765a8c
quasigod-io <quasigod-io@protonmail.com>
2022-09-21 01:34:20 -0400

    nitch: init at 0.1.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4451,6 +4451,8 @@ with pkgs;
 
   nfstrace = callPackage ../tools/networking/nfstrace { };
 
+  nitch = callPackage ../tools/misc/nitch { };
+
   nix-direnv = callPackage ../tools/misc/nix-direnv { };
 
   nixel = callPackage ../tools/nix/nixel { };
