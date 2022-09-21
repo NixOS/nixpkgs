ec9576773ae1cf9bc496c0f19c41fe74e1ac8f76
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-21 08:34:32 +0000

    libfilezilla: 0.38.1 -> 0.39.1

--- pkgs/development/libraries/libfilezilla/default.nix
+++ pkgs/development/libraries/libfilezilla/default.nix
@@ -11,11 +11,11 @@
 
 stdenv.mkDerivation rec {
   pname = "libfilezilla";
-  version = "0.38.1";
+  version = "0.39.1";
 
   src = fetchurl {
     url = "https://download.filezilla-project.org/${pname}/${pname}-${version}.tar.bz2";
-    hash = "sha256-1AGotagKfBexo2DdnMy23Fb9jTlEE6n7K2uxvF2Y/Uw=";
+    hash = "sha256-89bA3yjzP1LPJTywP8UVeXUq5NrvKqxzRaa9feYjpsU=";
   };
 
   nativeBuildInputs = [ autoreconfHook pkg-config ];
