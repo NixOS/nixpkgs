c03b4fb965f3ae76886ea64a9bf7c609e5c6695c
Fabian Affolter <mail@fabian-affolter.ch>
2022-09-23 00:25:51 +0200

    python310Packages.pyatome: 0.1.1 -> 0.1.2

--- pkgs/development/python-modules/pyatome/default.nix
+++ pkgs/development/python-modules/pyatome/default.nix
@@ -8,12 +8,12 @@
 
 buildPythonPackage rec {
   pname = "pyatome";
-  version = "0.1.1";
+  version = "0.1.2";
 
   src = fetchPypi {
     pname = "pyAtome";
     inherit version;
-    sha256 = "7282e7ec258c69d4ddf2a5754ff07680a1ac92f9bfb478d601fd9e944fccd834";
+    sha256 = "sha256-DGkgW6emh/esZa/alUjBbpLXlU4EVIPkysn9a0LgcJ4=";
   };
 
   propagatedBuildInputs = [ requests simplejson fake-useragent ];
