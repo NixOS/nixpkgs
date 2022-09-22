f7ca286ce2de87f08cc1187e1633b2a17e66e2c8
Ian M. Jones <ian@ianmjones.com>
2022-09-22 08:41:42 +0100

    wails: 2.0.0-beta.44.2 -> 2.0.0

--- pkgs/development/tools/wails/default.nix
+++ pkgs/development/tools/wails/default.nix
@@ -14,16 +14,16 @@
 
 buildGoModule rec {
   pname = "wails";
-  version = "2.0.0-beta.44.2";
+  version = "2.0.0";
 
   src = fetchFromGitHub {
     owner = "wailsapp";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-RBl/RlNxowKrtLRSMK8KW9vEB58XPK455nqC6Urfdpk=";
+    sha256 = "sha256-mAHRjIi4/cC29NqTdUF9sMLyHhFANw+QBibk1ENo1BA=";
   } + "/v2";
 
-  vendorSha256 = "sha256-KB6fzyeeHCl164ZnDIy9auUC7Qzm8rVMo3XxXbeYCgY=";
+  vendorSha256 = "sha256-jvjFV7StHCYn85iLWTRBXjPSUeWM+Yr5F3H0rFciCOc=";
 
   proxyVendor = true;
 
