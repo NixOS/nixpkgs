16e7785179939647e3a585eeee9f27887f061210
Ilan Joselevich <personal@ilanjoselevich.com>
2022-09-22 18:29:35 +0300

    nixos/oauth2_proxy_nginx:re-do virtualHosts option
    Co-authored-by: Mason Mackaman <masondeanm@aol.com>

--- nixos/modules/services/security/oauth2_proxy_nginx.nix
+++ nixos/modules/services/security/oauth2_proxy_nginx.nix
@@ -4,6 +4,11 @@ let
   cfg = config.services.oauth2_proxy.nginx;
 in
 {
+  imports = [
+    (mkRemovedOptionModule [ "services" "oauth2_proxy" "nginx" "virtualHosts" ]
+      "This option has been removed in favor of 'services.oauth2_proxy.nginx.virtualHostLocations' which allows setting the proxy protection at a per-location level.")
+  ];
+
   options.services.oauth2_proxy.nginx = {
     proxy = mkOption {
       type = types.str;
@@ -13,54 +18,66 @@ in
         The address of the reverse proxy endpoint for oauth2_proxy
       '';
     };
-    virtualHosts = mkOption {
-      type = types.listOf types.str;
-      default = [];
-      description = lib.mdDoc ''
-        A list of nginx virtual hosts to put behind the oauth2 proxy
+    virtualHostLocations = mkOption {
+      type = types.attrsOf (types.listOf types.str);
+      example = literalExpression ''
+        {
+          "example.com" = [ "/" "/assets/" ];
+        };
       '';
+      default = { };
+      description = lib.mdDoc "List of locations per virtual host to put behind the oauth2_proxy";
     };
   };
-  config.services.oauth2_proxy = mkIf (cfg.virtualHosts != [] && (hasPrefix "127.0.0.1:" cfg.proxy)) {
-    enable = true;
-  };
-  config.services.nginx = mkIf config.services.oauth2_proxy.enable (mkMerge
-  ((optional (cfg.virtualHosts != []) {
-    recommendedProxySettings = true; # needed because duplicate headers
-  }) ++ (map (vhost: {
-    virtualHosts.${vhost} = {
-      locations."/oauth2/" = {
-        proxyPass = cfg.proxy;
-        extraConfig = ''
-          proxy_set_header X-Scheme                $scheme;
-          proxy_set_header X-Auth-Request-Redirect $scheme://$host$request_uri;
-        '';
-      };
-      locations."/oauth2/auth" = {
-        proxyPass = cfg.proxy;
-        extraConfig = ''
-          proxy_set_header X-Scheme         $scheme;
-          # nginx auth_request includes headers but not body
-          proxy_set_header Content-Length   "";
-          proxy_pass_request_body           off;
-        '';
-      };
-      locations."/".extraConfig = ''
-        auth_request /oauth2/auth;
-        error_page 401 = /oauth2/sign_in;
+  config = {
+    assertions = [{
+      assertion = !lib.any (loc: lib.elem loc (flatten (lib.attrValues cfg.virtualHostLocations))) [ "/oauth2/" "/oauth2/auth" ];
+      message = "services.oauth2_proxy.nginx.virtualHostLocations: The OAuth2 specific locations cannot be protected.";
+    }];
 
-        # pass information via X-User and X-Email headers to backend,
-        # requires running with --set-xauthrequest flag
-        auth_request_set $user   $upstream_http_x_auth_request_user;
-        auth_request_set $email  $upstream_http_x_auth_request_email;
-        proxy_set_header X-User  $user;
-        proxy_set_header X-Email $email;
+    services.nginx = mkIf (cfg.virtualHostLocations != { }) {
+      recommendedProxySettings = true; # needed because duplicate headers
+      virtualHosts = lib.mapAttrs
+        (vhost: locations: mkMerge [
+          {
+            locations."/oauth2/" = {
+              proxyPass = cfg.proxy;
+              extraConfig = ''
+                proxy_set_header X-Scheme                $scheme;
+                proxy_set_header X-Auth-Request-Redirect $scheme://$host$request_uri;
+              '';
+            };
+            locations."/oauth2/auth" = {
+              proxyPass = cfg.proxy;
+              extraConfig = ''
+                proxy_set_header X-Scheme         $scheme;
+                # nginx auth_request includes headers but not body
+                proxy_set_header Content-Length   "";
+                proxy_pass_request_body           off;
+              '';
+            };
+          }
+          {
+            locations = genAttrs locations (_: {
+              extraConfig = ''
+                auth_request /oauth2/auth;
+                error_page 401 = /oauth2/sign_in;
 
-        # if you enabled --cookie-refresh, this is needed for it to work with auth_request
-        auth_request_set $auth_cookie $upstream_http_set_cookie;
-        add_header Set-Cookie $auth_cookie;
-      '';
+                # pass information via X-User and X-Email headers to backend,
+                # requires running with --set-xauthrequest flag
+                auth_request_set $user   $upstream_http_x_auth_request_user;
+                auth_request_set $email  $upstream_http_x_auth_request_email;
+                proxy_set_header X-User  $user;
+                proxy_set_header X-Email $email;
 
+                # if you enabled --cookie-refresh, this is needed for it to work with auth_request
+                auth_request_set $auth_cookie $upstream_http_set_cookie;
+                add_header Set-Cookie $auth_cookie;
+              '';
+            });
+          }
+        ])
+        cfg.virtualHostLocations;
     };
-  }) cfg.virtualHosts)));
+  };
 }
