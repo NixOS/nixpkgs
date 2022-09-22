733ce794b6e7b68773b654d6d9b00c237accb824
Mostly Void <7rat13@gmail.com>
2022-09-22 14:14:23 +0530

    litefs: init at 0.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16647,6 +16647,8 @@ with pkgs;
 
   litecli = callPackage ../development/tools/database/litecli {};
 
+  litefs = callPackage ../development/tools/database/litefs {};
+
   litestream = callPackage ../development/tools/database/litestream {};
 
   lsof = callPackage ../development/tools/misc/lsof { };
