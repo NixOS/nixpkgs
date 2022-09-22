749f0fd1f6d3d011fdd37f48bf4dd8f1e7b8c12b
Florian Brandes <florian.brandes@posteo.de>
2022-09-22 10:35:44 +0200

    python3Packages.apsw: 3.39.2.1 -> 3.39.3.0
    - remove now-included patch
    
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- pkgs/development/python-modules/apsw/default.nix
+++ pkgs/development/python-modules/apsw/default.nix
@@ -5,12 +5,11 @@
 , sqlite
 , isPyPy
 , python
-, fetchpatch
 }:
 
 buildPythonPackage rec {
   pname = "apsw";
-  version = "3.39.2.1";
+  version = "3.39.3.0";
   format = "setuptools";
 
   disabled = isPyPy;
@@ -19,20 +18,11 @@ buildPythonPackage rec {
     owner = "rogerbinns";
     repo = "apsw";
     rev = "refs/tags/${version}";
-    hash = "sha256-W1uQFya/IQUBAPAjwdCJ1K5LVc4spcYj0dN2YP2vtN0=";
+    hash = "sha256-rUG6TXGdF+XaRTFn2luffYw+1EEChxtLgQx2Gn+7J6A=";
   };
 
   buildInputs = [
     sqlite
-  ];
-
-  patches = [
-    # ongoing issue: https://github.com/rogerbinns/apsw/issues/363
-    # apsw needs to know the compile flags of sqlite to match features
-    (fetchpatch {
-      url = "https://github.com/rogerbinns/apsw/commit/e92f019ff785d8e52d381dc541d3f4f8236fb356.patch";
-      hash = "sha256-Zdy0ukfWkak9lTdU5WMNzWNp7uDROJgXLcfvQdfm2Oo=";
-    })
   ];
 
   # Project uses custom test setup to exclude some tests by default, so using pytest
