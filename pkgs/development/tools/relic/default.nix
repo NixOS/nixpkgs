100d91af3acb9f73fa42f96da0a815b89890e0c9
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-22 02:34:00 +0000

    relic: 7.4.0 -> 7.5.3

--- pkgs/development/tools/relic/default.nix
+++ pkgs/development/tools/relic/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "relic";
-  version = "7.4.0";
+  version = "7.5.3";
 
   src = fetchFromGitHub {
     owner = "sassoftware";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-3YzZUwS2rU+OROMXuIbVeLDQMIpEmZz+PNnI4dbQs+Y=";
+    sha256 = "sha256-urueDWvHhDsIiLIAatAforBM//UEJz8wUHEVyhwV7JY=";
   };
 
-  vendorSha256 = "sha256-aguirMJgh/uAGl0l3wKBMH2QEIH2N8pq7Dl9Ngfkc90=";
+  vendorSha256 = "sha256-wlylASK2RES0nbf6UZHJBrYQaz9jrq5j7/KF1wYOqE0=";
 
   meta = with lib; {
     homepage = "https://github.com/sassoftware/relic";
