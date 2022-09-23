aefcb9ca4b8275d8d5ad8f4addbe15dce9274fb3
bezmuth <benkel97@protonmail.com>
2022-09-23 18:07:34 +0100

    fetchNuGet: add support for sri hashes

--- pkgs/build-support/dotnet/make-nuget-deps/default.nix
+++ pkgs/build-support/dotnet/make-nuget-deps/default.nix
@@ -1,10 +1,10 @@
 { linkFarmFromDrvs, fetchurl }:
 { name, nugetDeps }:
 linkFarmFromDrvs "${name}-nuget-deps" (nugetDeps {
-  fetchNuGet = { pname, version, sha256
+  fetchNuGet = { pname, version, sha256 ? "", hash ? ""
     , url ? "https://www.nuget.org/api/v2/package/${pname}/${version}" }:
     fetchurl {
       name = "${pname}.${version}.nupkg";
-      inherit url sha256;
+      inherit url sha256 hash;
     };
 })
