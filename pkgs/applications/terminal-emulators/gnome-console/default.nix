63075847ae30fc7623d5005b35726c03a4ae83b6
Jan Tojnar <jtojnar@gmail.com>
2022-09-23 15:41:58 +0200

    gnome-console: 43.rc → 42.2
    There is a regression in VTE for GTK 4, let’s downgrade to a version using GTK 3.
    https://gitlab.gnome.org/GNOME/vte/-/issues/2584
    
    This reverts commit 71439b7977ec205fa9e2d1dec8e0a768d8d88b94.
    This reverts commit 36c49c22cfc22cde5003f91496f7c982abedbf68.
    
    Also disable Nautilus extension since it is now part of Nautilus itself.

--- pkgs/applications/terminal-emulators/gnome-console/default.nix
+++ pkgs/applications/terminal-emulators/gnome-console/default.nix
@@ -1,49 +1,67 @@
 { lib
 , stdenv
 , fetchurl
+, fetchpatch
 , gettext
 , gnome
 , libgtop
-, gtk4
-, libadwaita
+, gtk3
+, libhandy
 , pcre2
-, vte-gtk4
+, vte
 , appstream-glib
 , desktop-file-utils
+, git
 , meson
 , ninja
 , pkg-config
 , python3
-, wrapGAppsHook4
+, sassc
+, wrapGAppsHook
 , nixosTests
 }:
 
 stdenv.mkDerivation rec {
   pname = "gnome-console";
-  version = "43.rc";
+  # Do not upgrade until https://gitlab.gnome.org/GNOME/vte/-/issues/2584 is resolved!
+  version = "42.2";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-console/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "DCLFqoO+3qxYxYGCBBSrBPuzeOUmKzDKT0+Qqg4SLiw=";
+    sha256 = "fSbmwYdExXWnhykyY/YM7/YwEHCY6eWKd2WwCsdDcEk=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "fix-clang-build-issues.patch";
+      url = "https://gitlab.gnome.org/GNOME/console/-/commit/0e29a417d52e27da62f5cac461400be6a764dc65.patch";
+      sha256 = "sha256-5ORNZOxjC5dMk9VKaBcJu5OV1SEZo9SNUbN4Ob5hVJs=";
+    })
+  ];
 
   buildInputs = [
     gettext
     libgtop
-    gtk4
-    libadwaita
+    gtk3
+    libhandy
     pcre2
-    vte-gtk4
+    vte
   ];
 
   nativeBuildInputs = [
     appstream-glib
     desktop-file-utils
+    git
     meson
     ninja
     pkg-config
     python3
-    wrapGAppsHook4
+    sassc
+    wrapGAppsHook
+  ];
+
+  mesonFlags = [
+    "-Dnautilus=disabled"
   ];
 
   passthru = {
