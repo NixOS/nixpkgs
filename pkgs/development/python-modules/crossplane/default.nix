b81573950ea92eef4ee8ac2e55af4dffd9b5dcbb
Fabian Affolter <mail@fabian-affolter.ch>
2022-09-23 08:45:40 +0200

    python310Packages.crossplane: disable on older Python releases

--- pkgs/development/python-modules/crossplane/default.nix
+++ pkgs/development/python-modules/crossplane/default.nix
@@ -1,23 +1,35 @@
-{ lib, buildPythonPackage, pytestCheckHook, fetchFromGitHub }:
+{ lib
+, buildPythonPackage
+, pytestCheckHook
+, fetchFromGitHub
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "crossplane";
   version = "0.5.8";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "nginxinc";
     repo = "crossplane";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-DfIF+JvjIREi7zd5ZQ7Co/CIKC5iUeOgR/VLDPmrtTQ=";
+    hash = "sha256-DfIF+JvjIREi7zd5ZQ7Co/CIKC5iUeOgR/VLDPmrtTQ=";
   };
 
-  checkInputs = [ pytestCheckHook ];
+  checkInputs = [
+    pytestCheckHook
+  ];
 
-  pythonImportsCheck = [ "crossplane" ];
+  pythonImportsCheck = [
+    "crossplane"
+  ];
 
   meta = with lib; {
-    homepage = "https://github.com/nginxinc/crossplane";
     description = "NGINX configuration file parser and builder";
+    homepage = "https://github.com/nginxinc/crossplane";
     license = licenses.asl20;
     maintainers = with maintainers; [ kaction ];
   };
