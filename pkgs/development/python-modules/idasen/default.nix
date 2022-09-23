3028a781ff17f181e2fbb0a70a60031ff4e06a83
Alex Martens <alex@thinglab.org>
2022-09-23 12:47:21 -0700

    idasen: 0.9.2 -> 0.9.3

--- pkgs/development/python-modules/idasen/default.nix
+++ pkgs/development/python-modules/idasen/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "idasen";
-  version = "0.9.2";
+  version = "0.9.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "newAM";
     repo = "idasen";
     rev = "v${version}";
-    hash = "sha256-Nxg0r90EQTICstjbVp9nxJKkNRQEUhB2oEwc7r58t5k=";
+    hash = "sha256-O5EquX2zoSCsPdXRarTcqwt4aTBS6YF8SBq+/Ft3P1A=";
   };
 
   nativeBuildInputs = [
