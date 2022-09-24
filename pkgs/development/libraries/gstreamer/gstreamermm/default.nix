84f8194038f8b03e5a70606ee765ab9d2afe584f
José Romildo <malaquias@gmail.com>
2022-09-24 11:00:58 -0300

    gst_all_1.gstreamermm: fix build against glib 2.68

--- pkgs/development/libraries/gstreamer/gstreamermm/default.nix
+++ pkgs/development/libraries/gstreamer/gstreamermm/default.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , fetchurl
+, fetchpatch
 , pkg-config
 , file
 , glibmm
@@ -16,6 +17,14 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
     sha256 = "0q4dx9sncqbwgpzma0zvj6zssc279yl80pn8irb95qypyyggwn5y";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "${pname}-${version}.fix-build-against-glib-2.68.patch";
+      url = "https://gitlab.gnome.org/GNOME/gstreamermm/-/commit/37116547fb5f9066978e39b4cf9f79f2154ad425.patch";
+      sha256 = "sha256-YHtmOiOl4POwas3eWHsew3IyGK7Aq22MweBm3JPwyBM=";
+    })
+  ];
 
   outputs = [ "out" "dev" ];
 
@@ -45,11 +54,6 @@ stdenv.mkDerivation rec {
     license = licenses.lgpl21Plus;
     platforms = platforms.linux;
     maintainers = with maintainers; [ romildo ];
-    broken = true; # at 2022-09-24, logs at: https://termbin.com/n5hb
-      # ../gstreamer/gstreamermm/register.h: In function 'GType Gst::register_mm_type(const gchar*)':
-      # /nix/store/rwr0ly4girpnrq3sqvp3v2k40wb4hh3s-glib-2.72.3-dev/include/glib-2.0/glib/gatomic.h:113:19:
-      #   error: argument 2 of '__atomic_load' must not be a pointer to a 'volatile' type
-      #   113 |     __atomic_load (gapg_temp_atomic, &gapg_temp_newval, __ATOMIC_SEQ_CST);
   };
 
 }
