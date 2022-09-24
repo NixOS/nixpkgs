8b4bbd6919fe4e0834aef18905683c7722c87604
zowoq <59103226+zowoq@users.noreply.github.com>
2022-09-24 17:38:37 +1000

    nixosTests.cfssl: enable on aarch64-linux

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -117,7 +117,7 @@ in {
   ceph-single-node = handleTestOn ["x86_64-linux"] ./ceph-single-node.nix {};
   ceph-single-node-bluestore = handleTestOn ["x86_64-linux"] ./ceph-single-node-bluestore.nix {};
   certmgr = handleTest ./certmgr.nix {};
-  cfssl = handleTestOn ["x86_64-linux"] ./cfssl.nix {};
+  cfssl = handleTestOn ["aarch64-linux" "x86_64-linux"] ./cfssl.nix {};
   charliecloud = handleTest ./charliecloud.nix {};
   chromium = (handleTestOn ["aarch64-linux" "x86_64-linux"] ./chromium.nix {}).stable or {};
   cinnamon = handleTest ./cinnamon.nix {};
