fc6517a55b5049156ebf92bb0e1ad7fc40fc22f4
Sebastian Reuße <seb@schattenkopie.de>
2022-09-24 06:14:27 +0000

    ballerina: 2201.1.0 -> 2201.2.1
    Additionally, we now force Java 11. While Ballerina documentation says to use
    “Java 11 or above”, comments on GitHub indicate that Java 17 is currently
    unsupported.
    
    Using Ballerina 2201.2.1 on Java 17 yields me a ConcurrentModificationException
    arising from the Ballerina compiler when compiling a simple test service. This
    exception is avoided when running Ballerina on Java 11 instead.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13300,7 +13300,7 @@ with pkgs;
 
   avra = callPackage ../development/compilers/avra { };
 
-  ballerina = callPackage ../development/compilers/ballerina { };
+  ballerina = callPackage ../development/compilers/ballerina { openjdk = openjdk11; };
 
   beekeeper-studio = callPackage ../development/tools/database/beekeeper-studio { };
 
