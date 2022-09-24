a4d08decd0cbdc264fe9408bcab4e0f20cf75c5b
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-24 06:08:23 +0000

    armadillo: 11.2.4 -> 11.4.0

--- pkgs/development/libraries/armadillo/default.nix
+++ pkgs/development/libraries/armadillo/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "armadillo";
-  version = "11.2.4";
+  version = "11.4.0";
 
   src = fetchurl {
     url = "mirror://sourceforge/arma/armadillo-${version}.tar.xz";
-    sha256 = "sha256-3EyRlUqxFJC/ZNLfzFSoAvFDk8dWqNVFrBVe7v+n/ZM=";
+    sha256 = "sha256-AWUxFSwCcppgKqbxb0ujsEdXQNXBvSjTTt1yZPR2Rhw=";
   };
 
   nativeBuildInputs = [ cmake ];
