2905d2a32083c1028996b3db9bd8297906b390c2
Alexandre Iooss <erdnaxe@crans.org>
2022-09-24 09:41:02 +0200

    envypn-font: rename name to pname&version

--- pkgs/data/fonts/envypn-font/default.nix
+++ pkgs/data/fonts/envypn-font/default.nix
@@ -2,11 +2,12 @@
 , fonttosfnt, mkfontscale
 }:
 
-stdenv.mkDerivation {
-  name = "envypn-font-1.7.1";
+stdenv.mkDerivation rec {
+  pname = "envypn-font";
+  version = "1.7.1";
 
   src = fetchurl {
-    url = "https://ywstd.fr/files/p/envypn-font/envypn-font-1.7.1.tar.gz";
+    url = "https://ywstd.fr/files/p/envypn-font/envypn-font-${version}.tar.gz";
     sha256 = "bda67b6bc6d5d871a4d46565d4126729dfb8a0de9611dae6c68132a7b7db1270";
   };
 
