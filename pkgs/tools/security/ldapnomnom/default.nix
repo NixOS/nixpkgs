f83c3790b31a55ebfa194deacc0989bea321dad4
Fabian Affolter <mail@fabian-affolter.ch>
2022-09-24 18:18:52 +0200

    ldapnomnom: 1.0.5 -> 1.0.6

--- pkgs/tools/security/ldapnomnom/default.nix
+++ pkgs/tools/security/ldapnomnom/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "ldapnomnom";
-  version = "1.0.5";
+  version = "1.0.6";
 
   src = fetchFromGitHub {
     owner = "lkarlslund";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-+lUEsnSUcGs8knkYaI6l1r5YJU82iMlcI6E/+aqyCoY=";
+    hash = "sha256-fZlxDc/CgjGqTSM9TpFcE0yxb7YxzZmQJ1FoUdM7Bgs=";
   };
 
   vendorSha256 = "sha256-psITrOXWh+3XXLSdo862ln8n7AkO00KB4mHyTdbDCkY=";
