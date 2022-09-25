c0fe1c0eaeb21e4e5112d2754ff0cd2b0a21df0f
figsoda <figsoda@pm.me>
2022-09-25 12:46:28 -0400

    ruff: init at 0.0.46

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36273,6 +36273,10 @@ with pkgs;
 
   rucksack = callPackage ../development/tools/rucksack { };
 
+  ruff = callPackage ../development/tools/ruff {
+    inherit (darwin.apple_sdk.frameworks) CoreServices Security;
+  };
+
   sam-ba = callPackage ../tools/misc/sam-ba { };
 
   sndio = callPackage ../misc/sndio { };
