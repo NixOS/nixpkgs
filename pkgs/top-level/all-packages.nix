46539f80fd65bd1007ed090129533959f312eb6f
wentam <wentam42@gmail.com>
2022-09-25 05:33:19 -0400

    proycon-wayout: init at 0.1.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1351,6 +1351,8 @@ with pkgs;
 
   pferd = callPackage ../tools/misc/pferd {};
 
+  proycon-wayout = callPackage ../tools/wayland/proycon-wayout {};
+
   q = callPackage ../tools/networking/q {};
 
   qFlipper = libsForQt515.callPackage ../tools/misc/qflipper { };
