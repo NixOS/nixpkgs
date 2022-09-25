eccbe1413dd2c5776f6fc959bb4a3ceb99afa5a7
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-25 16:28:10 +0000

    openapi-generator-cli: 6.1.0 -> 6.2.0

--- pkgs/tools/networking/openapi-generator-cli/default.nix
+++ pkgs/tools/networking/openapi-generator-cli/default.nix
@@ -1,7 +1,7 @@
 { callPackage, lib, stdenv, fetchurl, jre, makeWrapper }:
 
 let this = stdenv.mkDerivation rec {
-  version = "6.1.0";
+  version = "6.2.0";
   pname = "openapi-generator-cli";
 
   jarfilename = "${pname}-${version}.jar";
@@ -12,7 +12,7 @@ let this = stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://maven/org/openapitools/${pname}/${version}/${jarfilename}";
-    sha256 = "sha256-O8v/d2By5OUWEwf4N9NKtHE6E+2e3CDkmDwjIXkeoDc=";
+    sha256 = "sha256-YHB+LIk4qUJ49iFggdcGfQ8b7O2MjrEnfmJemlnM0to=";
   };
 
   dontUnpack = true;
