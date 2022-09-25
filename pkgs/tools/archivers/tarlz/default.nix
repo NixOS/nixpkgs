498e91576a5b0972200e092cfdbba0653868a61d
Emery Hemingway <ehmry@posteo.net>
2022-09-25 18:53:41 -0500

    tarlz: 0.22 -> 0.23

--- pkgs/tools/archivers/tarlz/default.nix
+++ pkgs/tools/archivers/tarlz/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "tarlz";
-  version = "0.22";
+  version = "0.23";
   outputs = [ "out" "man" "info" ];
 
   nativeBuildInputs = [ lzip texinfo ];
@@ -10,12 +10,18 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://savannah/lzip/${pname}/${pname}-${version}.tar.lz";
-    sha256 = "sha256-/M9yJvoktV0ybKsT926jSb7ERsWo33GkbTQwmaBQkdw=";
+    sha256 = "3cefb4f889da25094f593b43a91fd3aaba33a02053a51fb092e9b5e8adb660a3";
+    # upstream publishes digests in hex
   };
 
   enableParallelBuilding = true;
   makeFlags = [ "CXX:=$(CXX)" ];
   doCheck = !stdenv.isDarwin;
+
+  preCheck = ''
+    substituteInPlace testsuite/check.sh \
+      --replace '1969-12-31T23:59:59' '''
+  ''; # do not use this date as a mtime with this package, this is your only warning
 
   meta = with lib; {
     homepage = "https://www.nongnu.org/lzip/${pname}.html";
