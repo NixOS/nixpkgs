d614292a030f854c873bdef7c52d05d52bcb2359
Fabian Affolter <mail@fabian-affolter.ch>
2022-09-25 17:07:00 +0200

    python310Packages.dbus-fast: 1.11.0 -> 1.13.0

--- pkgs/development/python-modules/dbus-fast/default.nix
+++ pkgs/development/python-modules/dbus-fast/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "dbus-fast";
-  version = "1.11.0";
+  version = "1.13.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-j0bD8JtcMRX+G0QaxbSUqmGUw/ubrmz1ngGyzaR9UHA=";
+    hash = "sha256-SADUeLD4quPxKChf1A4ph1iI8zu7oCDg4nJNFl77f+k=";
   };
 
   nativeBuildInputs = [
