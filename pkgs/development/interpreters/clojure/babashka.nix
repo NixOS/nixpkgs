2239172ea0f49fcf6457361692dac5523187a3d6
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-25 10:02:07 +0000

    babashka: 0.9.162 -> 0.10.163

--- pkgs/development/interpreters/clojure/babashka.nix
+++ pkgs/development/interpreters/clojure/babashka.nix
@@ -2,11 +2,11 @@
 
 buildGraalvmNativeImage rec {
   pname = "babashka";
-  version = "0.9.162";
+  version = "0.10.163";
 
   src = fetchurl {
     url = "https://github.com/babashka/${pname}/releases/download/v${version}/${pname}-${version}-standalone.jar";
-    sha256 = "sha256-eSn3E5opJO+7dVay+j5xEkCIKJvmymozZGVD+vAzO4A=";
+    sha256 = "sha256-54RqqjhKBNSmIIomyhgjujC4CsY33Mkd3QSIc2w9fRg=";
   };
 
   executable = "bb";
