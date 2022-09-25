ec9354d03a86233ad8f103f2ea0e39d2885b4755
superherointj <5861043+superherointj@users.noreply.github.com>
2022-09-25 14:09:27 -0300

    konf: 0.3.0 -> 0.4.0
    * Fix version
    * Add version test
    * Add shell completion for bash, zsh

--- pkgs/development/tools/konf/default.nix
+++ pkgs/development/tools/konf/default.nix
@@ -1,22 +1,44 @@
 { lib
 , buildGoModule
 , fetchFromGitHub
+, installShellFiles
+, testers
+, konf
 }:
 
 buildGoModule rec {
   pname = "konf";
-  version = "0.3.0";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "SimonTheLeg";
     repo = "konf-go";
     rev = "v${version}";
-    hash = "sha256-n8WJyihpbtci8Q6zUapwrpTrVZVAS53OpnwAsv6w1FY=";
+    hash = "sha256-8TXr/jYMl3NLERtLkm7qG97IL/idz4xxP0g0LEy4/j8=";
   };
 
   vendorHash = "sha256-sB3j19HrTtaRqNcooqNy8vBvuzxxyGDa7MOtiGoVgN8=";
 
-  ldflags = [ "-s" "-w" ];
+  ldflags = [
+    "-s" "-w"
+    "-X github.com/simontheleg/konf-go/cmd.gitcommit=unknown"
+    "-X github.com/simontheleg/konf-go/cmd.builddate=unknown"
+    "-X github.com/simontheleg/konf-go/cmd.gitversion=${version}"
+  ];
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  postInstall = ''
+    export HOME=$(mktemp -d)
+    $out/bin/konf-go completion bash > konf-go.bash
+    $out/bin/konf-go completion zsh > konf-go.zsh
+    installShellCompletion --cmd konf-go --bash konf-go.bash --zsh konf-go.zsh
+  '';
+
+  passthru.tests.version = testers.testVersion {
+    package = konf;
+    command = "HOME=$(mktemp -d) ${konf}/bin/konf-go version";
+  };
 
   meta = with lib; {
     description = "Lightweight and blazing fast kubeconfig manager which allows to use different kubeconfigs at the same time";
