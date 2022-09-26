49179007042f1603995d9c68fc75c1cf728e0f25
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-26 00:47:47 +0000

    plantuml: 1.2022.7 -> 1.2022.8

--- pkgs/tools/misc/plantuml/default.nix
+++ pkgs/tools/misc/plantuml/default.nix
@@ -1,12 +1,12 @@
 { lib, stdenv, fetchurl, makeWrapper, jre, graphviz }:
 
 stdenv.mkDerivation rec {
-  version = "1.2022.7";
+  version = "1.2022.8";
   pname = "plantuml";
 
   src = fetchurl {
     url = "https://github.com/plantuml/plantuml/releases/download/v${version}/plantuml-pdf-${version}.jar";
-    sha256 = "sha256-GnFueIK4RDGViHmyBtfQqNpId+4ufCsci7c1YCcZjAQ=";
+    sha256 = "sha256-9K9d7dQnMFTq1zpZU2t0NIhzQVIyk8b6PnI0cRJJ5t0=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
