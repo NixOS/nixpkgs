f36fc5937eb7068c1bf062e00c184f15765559d8
figsoda <figsoda@pm.me>
2022-09-26 10:31:44 -0400

    cargo-hack: 0.5.20 -> 0.5.21

--- pkgs/development/tools/rust/cargo-hack/default.nix
+++ pkgs/development/tools/rust/cargo-hack/default.nix
@@ -2,22 +2,22 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-hack";
-  version = "0.5.20";
+  version = "0.5.21";
 
   src = fetchCrate {
     inherit pname version;
-    sha256 = "sha256-V6ENO9KzZXkBF19stySSTLXSWnZym6FXaWjAEHuluQs=";
+    sha256 = "sha256-E0YhpfFT1JQzXWK3cQfieZ8TVg+BRGwHL6cTwOrNVSQ=";
   };
 
-  cargoSha256 = "sha256-5kIzTO02zurjoU6H+iDif9UV3KY0tPFvvZlg6sNRJwg=";
+  cargoSha256 = "sha256-AfILqelDRJuVVEoOT2FLKHq4QVEZXbwPSM0s4fpP00A=";
 
   # some necessary files are absent in the crate version
   doCheck = false;
 
   meta = with lib; {
     description = "Cargo subcommand to provide various options useful for testing and continuous integration";
-    changelog = "https://github.com/taiki-e/cargo-hack/blob/v${version}/CHANGELOG.md";
     homepage = "https://github.com/taiki-e/cargo-hack";
+    changelog = "https://github.com/taiki-e/cargo-hack/blob/v${version}/CHANGELOG.md";
     license = with licenses; [ asl20 /* or */ mit ];
     maintainers = with maintainers; [ figsoda ];
   };
