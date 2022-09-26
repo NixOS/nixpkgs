af411622fd0eea4d0485d1a0588fb53274c9bbde
Sefa Eyeoglu <contact@scrumplex.net>
2022-09-26 16:11:45 +0200

    tomlplusplus: init at 3.2.0
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22166,6 +22166,8 @@ with pkgs;
 
   tomlcpp = callPackage ../development/libraries/tomlcpp { };
 
+  tomlplusplus = callPackage ../development/libraries/tomlplusplus { };
+
   tokyocabinet = callPackage ../development/libraries/tokyo-cabinet { };
 
   tokyotyrant = callPackage ../development/libraries/tokyo-tyrant { };
