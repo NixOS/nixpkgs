6a07ce71b6830a5fe0b6574bd23e0cc6151689d9
figsoda <figsoda@pm.me>
2022-09-26 17:17:53 -0400

    fclones: skip flaky test

--- pkgs/tools/misc/fclones/default.nix
+++ pkgs/tools/misc/fclones/default.nix
@@ -27,6 +27,11 @@ rustPlatform.buildRustPackage rec {
   # device::test_physical_device_name test fails on Darwin
   doCheck = !stdenv.isDarwin;
 
+  checkFlags = [
+    # ofborg sometimes fails with "Resource temporarily unavailable"
+    "--skip=cache::test::return_none_if_different_transform_was_used"
+  ];
+
   meta = with lib; {
     description = "Efficient Duplicate File Finder and Remover";
     homepage = "https://github.com/pkolaczk/fclones";
