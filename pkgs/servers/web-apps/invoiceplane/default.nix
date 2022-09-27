6f1f7b15c93435b8457b948a2a8cd4cbf7f634f0
Arnout Engelen <arnout@bzzt.net>
2022-09-27 11:50:31 +0200

    invoiceplane: mark as broken (missing php8 support)
    And we have already dropped php7 in nixpkgs.

--- pkgs/servers/web-apps/invoiceplane/default.nix
+++ pkgs/servers/web-apps/invoiceplane/default.nix
@@ -1,48 +1,17 @@
-{ lib, stdenv, fetchurl, writeText, unzip, nixosTests, fetchpatch }:
+{ lib, stdenv, fetchFromGitHub, writeText, unzip, nixosTests, fetchpatch }:
 
 stdenv.mkDerivation rec {
   pname = "invoiceplane";
   version = "1.5.11";
 
-  src = fetchurl {
-    url = "https://github.com/InvoicePlane/InvoicePlane/releases/download/v${version}/v${version}.zip";
-    sha256 = "137g0xps4kb3j7f5gz84ql18iggbya6d9dnrfp05g2qcbbp8kqad";
+  src = fetchFromGitHub {
+    owner = "InvoicePlane";
+    repo = "InvoicePlane";
+    rev = "7402b3761614d2cdd09836022b195e247922d509";
+    sha256 = "sha256-iiJ1BUjWYD33zzG1vpo2LJ/Tks+MT2x+cR1+PIik46U=";
   };
 
-  patches = [
-
-    # Fix CVE-2021-29024, unauthenticated directory listing
-    # Should be included in a later release > 1.5.11
-    # https://github.com/NixOS/nixpkgs/issues/166655
-    # https://github.com/InvoicePlane/InvoicePlane/pull/754
-    (fetchpatch {
-      url = "https://patch-diff.githubusercontent.com/raw/InvoicePlane/InvoicePlane/pull/754.patch";
-      sha256 = "sha256-EHXw7Zqli/nA3tPIrhxpt8ueXvDtshz0XRzZT78sdQk=";
-    })
-
-    # Fix CVE-2021-29023, password reset rate-limiting
-    # Should be included in a later release > 1.5.11
-    # https://github.com/NixOS/nixpkgs/issues/166655
-    # https://github.com/InvoicePlane/InvoicePlane/pull/739
-    (fetchpatch {
-      url = "https://patch-diff.githubusercontent.com/raw/InvoicePlane/InvoicePlane/pull/739.patch";
-      sha256 = "sha256-6ksJjW6awr3lZsDRxa22pCcRGBVBYyV8+TbhOp6HBq0=";
-    })
-
-    # Fix CVE-2021-29022, full path disclosure
-    # Should be included in a later release > 1.5.11
-    # https://github.com/NixOS/nixpkgs/issues/166655
-    # https://github.com/InvoicePlane/InvoicePlane/pull/767
-    #(fetchpatch {
-    #  url = "https://patch-diff.githubusercontent.com/raw/InvoicePlane/InvoicePlane/pull/767.patch";
-    #  sha256 = "sha256-rSWDH8KeHSRWLyQEa7RSwv+8+ja9etTz+6Q9XThuwUo=";
-    #})
-
-  ];
-
   nativeBuildInputs = [ unzip ];
-
-  sourceRoot = ".";
 
   installPhase = ''
     mkdir -p $out/
@@ -59,5 +28,8 @@ stdenv.mkDerivation rec {
     homepage = "https://www.invoiceplane.com";
     platforms = platforms.all;
     maintainers = with maintainers; [ onny ];
+    # Not yet working with php >= 8
+    # https://github.com/InvoicePlane/InvoicePlane/issues/798
+    broken = true;
   };
 }
