0c804972f562b757c371ef445b06caa4ebbad44d
Rehno Lindeque <rehno.lindeque@gmail.com>
2022-09-27 19:44:17 +0000

    python310Packages.kornia: init at 0.6.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5252,6 +5252,8 @@ self: super: with self; {
 
   korean-lunar-calendar = callPackage ../development/python-modules/korean-lunar-calendar { };
 
+  kornia = callPackage ../development/python-modules/kornia { };
+
   krakenex = callPackage ../development/python-modules/krakenex { };
 
   kubernetes = callPackage ../development/python-modules/kubernetes { };
