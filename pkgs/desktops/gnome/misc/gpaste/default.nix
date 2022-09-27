1a5ed6aef347263bf1a0890b39b938834ebc61f0
Jan Tojnar <jtojnar@gmail.com>
2022-09-27 17:44:29 +0200

    gnome.gpaste: 42.1 → 43.0
    https://www.imagination-land.org/posts/2022-09-27-gpaste-43.0-released.html
    https://github.com/Keruspe/GPaste/compare/v42.1...v43.0
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/misc/gpaste/default.nix
+++ pkgs/desktops/gnome/misc/gpaste/default.nix
@@ -22,39 +22,25 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "42.1";
+  version = "43.0";
   pname = "gpaste";
 
   src = fetchFromGitHub {
     owner = "Keruspe";
     repo = "GPaste";
     rev = "v${version}";
-    sha256 = "sha256-A5NZ4NiPVZUr7vPdDuNywLsLrejZ4SCg7+3//ZNRmLY=";
+    sha256 = "sha256-F+AWTYVK145RzJ1Zldh4Q4R/hN/D7aXO3SIJ1t6ClWs=";
   };
 
   patches = [
     ./fix-paths.patch
 
-    # Fix GNOME 43 compatibility.
-    (fetchpatch {
-      url = "https://github.com/Keruspe/GPaste/commit/eece9c374a823234bc20e58bf95bb10ace9590f4.patch";
-      sha256 = "+GoiNsge+ki2X3OIoFA+r3K8WLN00nExYj8gDuCSNiA=";
-    })
-    (fetchpatch {
-      url = "https://github.com/Keruspe/GPaste/commit/614bbe65e7d8f2ddca447daaada86156334bb71a.patch";
-      sha256 = "q6fjBTVRx6/5xf5ZhQJpvnwvroLiNxF73P7vTFTmNqI=";
-    })
-    (fetchpatch {
-      url = "https://github.com/Keruspe/GPaste/commit/971e690a74b4de6b78bdaf1300507a6190ffb474.patch";
-      sha256 = "bCLnuLqLgq7BlZN49NFrQjbCJBneVHl1t+WnAAIofkY=";
-    })
+    # Build against GCR 4.
+    # Patch was temporarily reverted.
+    # https://github.com/Keruspe/GPaste/pull/409
     (fetchpatch {
       url = "https://github.com/Keruspe/GPaste/commit/0378cb4a657042ce5321f1d9728cff31e55bede6.patch";
       sha256 = "0Ngr+/fS5/wICR84GEiE0pXEXQ/f/3G59lDivH167m8=";
-    })
-    (fetchpatch {
-      url = "https://github.com/Keruspe/GPaste/commit/6d29385f8c935b1def28340bc165ca6a7756f29b.patch";
-      sha256 = "crkkIrLwogRzr1JfJEt5TPK65Y4zY1H5Gt+s0z3A8Dg=";
     })
   ];
 
