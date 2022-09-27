0b627ad0ee0390be9b49a46e6d446e5ce1e3b305
Francesco Gazzetta <fgaz@fgaz.me>
2022-09-27 09:47:40 +0200

    minetest: add fgaz to maintainers
    (cherry picked from commit 2819931e413b241fce9ef43647cc30a7a12f813e)

--- pkgs/games/minetest/default.nix
+++ pkgs/games/minetest/default.nix
@@ -123,7 +123,7 @@ let
       description = "Infinite-world block sandbox game";
       license = licenses.lgpl21Plus;
       platforms = platforms.linux ++ platforms.darwin;
-      maintainers = with maintainers; [ pyrolagus fpletz ];
+      maintainers = with maintainers; [ pyrolagus fpletz fgaz ];
       # never built on Hydra
       # https://hydra.nixos.org/job/nixpkgs/trunk/minetestclient_4.x86_64-darwin
       # https://hydra.nixos.org/job/nixpkgs/trunk/minetestserver_4.x86_64-darwin
