400c17120597d009bb3d48ce902cbbbd96974af3
Morgan Wolfe <m@m0rg.dev>
2022-09-27 22:32:29 -0700

    maintainers: add mwolfe

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9338,6 +9338,12 @@
     githubId = 772914;
     name = "Mikael Voss";
   };
+  mwolfe = {
+    email = "corp@m0rg.dev";
+    github = "m0rg-dev";
+    githubId = 38578268;
+    name = "Morgan Wolfe";
+  };
   maxwilson = {
     email = "nixpkgs@maxwilson.dev";
     github = "mwilsoncoding";
