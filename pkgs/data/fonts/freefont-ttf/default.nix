f9bbe9a03c619bc15ec1f32494034fc4e80718c2
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2022-09-28 18:41:57 -0400

    freefont_ttf: fix build

--- pkgs/data/fonts/freefont-ttf/default.nix
+++ pkgs/data/fonts/freefont-ttf/default.nix
@@ -6,8 +6,11 @@ fetchzip rec {
   url = "mirror://gnu/freefont/${name}.zip";
 
   postFetch = ''
-    mkdir -p $out/share/fonts
-    unzip -j $downloadedFile \*.ttf -d $out/share/fonts/truetype
+    pushd "$out"
+    mkdir -p share/fonts/truetype
+    mv *.ttf share/fonts/truetype
+    rm AUTHORS COPYING CREDITS ChangeLog INSTALL README TROUBLESHOOTING USAGE
+    popd
   '';
 
   sha256 = "0h0x2hhr7kvjiycf7fv800xxwa6hcpiz54bqx06wsqc7z61iklvd";
