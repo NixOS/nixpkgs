4078880ab4ee02cfcfee683f66bba95c8acbb81d
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-28 16:41:51 +0000

    pinniped: 0.19.0 -> 0.20.0

--- pkgs/applications/networking/cluster/pinniped/default.nix
+++ pkgs/applications/networking/cluster/pinniped/default.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec{
   pname = "pinniped";
-  version = "0.19.0";
+  version = "0.20.0";
 
   src = fetchFromGitHub {
     owner = "vmware-tanzu";
     repo = "pinniped";
     rev = "v${version}";
-    sha256 = "sha256-VGvT0jj2f4+jiaPQkebL0GE+W4U8+RfSEsGr2sNBu2Q=";
+    sha256 = "sha256-JKZij9f6zlBzbYolxz03R5j95zKELVmEhX+QRTxF/cc=";
   };
 
   subPackages = "cmd/pinniped";
 
-  vendorSha256 = "sha256-7jQRUYJiRdYuAy3x+VPK4Nh5GF6ME7juloagm85oOVg=";
+  vendorSha256 = "sha256-szv/B7LG/In0j6MT6KCnuUfaCnK7RsJOLeuOtJ/ig9w=";
 
   ldflags = [ "-s" "-w" ];
 
