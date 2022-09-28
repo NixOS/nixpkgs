7dafcde4520b699d7fca712ca738902879c2f863
Nikola Knezevic <nikola.knezevic@imc.com>
2022-09-28 12:10:20 +0200

    refurb: init at 1.7.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16219,6 +16219,8 @@ with pkgs;
     llvmPackages = llvmPackages_latest;
   };
 
+  refurb = callPackage ../development/tools/refurb { };
+
   srandrd = callPackage ../tools/X11/srandrd { };
 
   srecord = callPackage ../development/tools/misc/srecord { };
