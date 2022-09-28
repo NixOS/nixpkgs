90791dc4e1c08347347645cdd353ab5a74bc9ea2
Pavol Rusnak <pavol@rusnak.io>
2022-09-28 23:03:32 +0200

    taro: init at 0.1.0-alpha

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33078,6 +33078,8 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Security;
   };
 
+  taro = callPackage ../applications/blockchains/taro { };
+
   terra-station = callPackage ../applications/blockchains/terra-station { };
 
   tessera = callPackage ../applications/blockchains/tessera { };
