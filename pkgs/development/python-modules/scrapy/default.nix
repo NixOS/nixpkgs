9ef2357f77ce704c062abc9dc4ec8faf9569a45b
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-28 07:52:55 +0000

    python310Packages.scrapy: 2.6.2 -> 2.6.3

--- pkgs/development/python-modules/scrapy/default.nix
+++ pkgs/development/python-modules/scrapy/default.nix
@@ -30,7 +30,7 @@
 
 buildPythonPackage rec {
   pname = "scrapy";
-  version = "2.6.2";
+  version = "2.6.3";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -38,7 +38,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     inherit version;
     pname = "Scrapy";
-    sha256 = "55e21181165f25337105fff1efc8393296375cea7de699a7e703bbd265595f26";
+    sha256 = "sha256-vf8arzVHwuVAQ206uGgLIQOTJ71dOi74nDQWWZLT5fM=";
   };
 
   nativeBuildInputs = [
