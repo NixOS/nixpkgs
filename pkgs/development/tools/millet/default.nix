dac4a4cf5c2168420f4b28802cf7251b7c2ab3a0
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-28 20:58:14 +0000

    millet: 0.3.9 -> 0.3.10

--- pkgs/development/tools/millet/default.nix
+++ pkgs/development/tools/millet/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "millet";
-  version = "0.3.9";
+  version = "0.3.10";
 
   src = fetchFromGitHub {
     owner = "azdavis";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-mG8vpcBcFACfVa8IFuax81pDeiloi0ustbAy6MOYpZs=";
+    sha256 = "sha256-7dTEir6U1FzI87XkxMXLX8HSVYiB95o25Bz993Eb8Jo=";
   };
 
-  cargoSha256 = "sha256-QoD5c2QZRKRO0gV+buvJN6d2RgfkA65AHibwJ4Kg/q8=";
+  cargoSha256 = "sha256-sHdAZBUk+PgF3i2dvKSbt5z0S/7W1nKbSIQXtPB5ino=";
 
   postPatch = ''
     rm .cargo/config.toml
