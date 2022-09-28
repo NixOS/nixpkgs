2aa8ca66187644d351428c4157bac79851a9396d
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-28 19:01:09 +0000

    hydrus: 497 -> 500

--- pkgs/applications/graphics/hydrus/default.nix
+++ pkgs/applications/graphics/hydrus/default.nix
@@ -10,14 +10,14 @@
 
 python3Packages.buildPythonPackage rec {
   pname = "hydrus";
-  version = "497";
+  version = "500";
   format = "other";
 
   src = fetchFromGitHub {
     owner = "hydrusnetwork";
     repo = "hydrus";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-dQ6a3jys6V1ihT6q8FUaX7jOA1ZDZdX5EUy03ILk7vM=";
+    sha256 = "sha256-gsOto37++++ucpDC0ri3HhROp7v6qlHENjFvsbuyM6k=";
   };
 
   nativeBuildInputs = [
