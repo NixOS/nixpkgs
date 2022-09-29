8c06f11a99d6f3014af007646efa8c160be5afc9
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-29 02:50:49 +0000

    python310Packages.minio: 7.1.11 -> 7.1.12

--- pkgs/development/python-modules/minio/default.nix
+++ pkgs/development/python-modules/minio/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "minio";
-  version = "7.1.11";
+  version = "7.1.12";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "minio";
     repo = "minio-py";
     rev = "refs/tags/${version}";
-    sha256 = "sha256-Mf6ZisUCZaLznCMlmKDNwJ695P6Ut45no1lzX5w5CA8=";
+    sha256 = "sha256-9BjKoBQdkqkNK6StsiP0L3S5Dn8y53K5VghUIpIt46k=";
   };
 
   propagatedBuildInputs = [
