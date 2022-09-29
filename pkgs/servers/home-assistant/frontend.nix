5746170ad7f2d84004f09c5cfc122b0d53c158c6
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-09-29 02:13:15 +0200

    home-assistant: 2022.9.1 -> 2022.9.7
    https://github.com/home-assistant/core/releases/tag/2022.9.7
    https://github.com/home-assistant/core/releases/tag/2022.9.6
    https://github.com/home-assistant/core/releases/tag/2022.9.5
    https://github.com/home-assistant/core/releases/tag/2022.9.4
    https://github.com/home-assistant/core/releases/tag/2022.9.3
    https://github.com/home-assistant/core/releases/tag/2022.9.2

--- pkgs/servers/home-assistant/frontend.nix
+++ pkgs/servers/home-assistant/frontend.nix
@@ -4,7 +4,7 @@ buildPythonPackage rec {
   # the frontend version corresponding to a specific home-assistant version can be found here
   # https://github.com/home-assistant/home-assistant/blob/master/homeassistant/components/frontend/manifest.json
   pname = "home-assistant-frontend";
-  version = "20220907.0";
+  version = "20220907.2";
   format = "wheel";
 
   src = fetchPypi {
@@ -12,7 +12,7 @@ buildPythonPackage rec {
     pname = "home_assistant_frontend";
     dist = "py3";
     python = "py3";
-    sha256 = "sha256-G/2lOUffxKnEmd4mz/qAlNkzTNAUTfjKaGgvj6vG9ro=";
+    sha256 = "sha256-ykId53EMPRXmMmoS55ZtjF6UR/JVPtBXFqjwuK2E2F4=";
   };
 
   # there is nothing to strip in this package
