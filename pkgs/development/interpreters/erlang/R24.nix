4afe51e57155cd49d2fc6d988ac7ebad0cad0172
Robert Scott <code@humanleg.org.uk>
2022-09-29 20:05:25 +0100

    erlangR24: 24.2 -> 24.3.4.5

--- pkgs/development/interpreters/erlang/R24.nix
+++ pkgs/development/interpreters/erlang/R24.nix
@@ -1,6 +1,6 @@
 { mkDerivation }:
 
 mkDerivation {
-  version = "24.2";
-  sha256 = "10s57v2i2qqyg3gddm85n3crzrkikl4zfwgzqmxjzdynsyb4xg68";
+  version = "24.3.4.5";
+  sha256 = "sha256-MGmuge79Dh/HXF+tkvubM979K0At5v1B1RhM5Koy8oY=";
 }
