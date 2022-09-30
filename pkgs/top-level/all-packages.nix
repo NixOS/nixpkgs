a941ed551305a3b21489b59bda2330b9faafcbdb
waelwindows <waelwindows9922@gmail.com>
2022-09-30 17:55:47 +0300

    ytcast: init at version 1.3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32948,6 +32948,8 @@ with pkgs;
 
   ytarchive = callPackage ../tools/misc/ytarchive { };
 
+  ytcast = callPackage ../tools/misc/ytcast { };
+
   ytcc = callPackage ../tools/networking/ytcc { };
 
   ytmdesktop = callPackage ../applications/audio/ytmdesktop { };
