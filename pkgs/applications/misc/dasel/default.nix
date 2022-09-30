44caf5ea11fd14b90113441e06f6f6f0f6f4de2e
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-30 11:34:58 +0000

    dasel: 1.27.0 -> 1.27.1

--- pkgs/applications/misc/dasel/default.nix
+++ pkgs/applications/misc/dasel/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "dasel";
-  version = "1.27.0";
+  version = "1.27.1";
 
   src = fetchFromGitHub {
     owner = "TomWright";
     repo = "dasel";
     rev = "v${version}";
-    sha256 = "sha256-bVAW8DrLqZKm9/iLleFxIMVkxaqhgdKwQUSEPpYhwt8=";
+    sha256 = "sha256-ebz6JUifkw0FQ88dKvIYwC83zuzGsXFaN9PdGDxHmJc=";
   };
 
   vendorSha256 = "sha256-zli9SEBU6n0JusAquqb9+O2W4yPZS7zmC5PCebVSeIA=";
