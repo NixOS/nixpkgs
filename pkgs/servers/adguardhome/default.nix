b335bb07922525ea3a17149d0fb66d1d99306a10
R. Ryantm <ryantm-bot@ryantm.com>
2022-09-30 08:30:17 +0000

    adguardhome: 0.107.12 -> 0.107.14

--- pkgs/servers/adguardhome/default.nix
+++ pkgs/servers/adguardhome/default.nix
@@ -7,7 +7,7 @@ in
 
 stdenv.mkDerivation rec {
   pname = "adguardhome";
-  version = "0.107.12";
+  version = "0.107.14";
   src = sources.${system} or (throw "Source for ${pname} is not available for ${system}");
 
   installPhase = ''
