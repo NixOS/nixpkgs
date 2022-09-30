963a660fd51beacab245cad8eb022f37caac5e23
Matthieu Coudron <mcoudron@hotmail.com>
2022-09-30 13:19:08 +0200

    libvterm-neovim: 0.1.3 -> 0.3
    changed the url to upstream, neovim stopped using its own fork of libvterm https://github.com/neovim/neovim/wiki/Deps#forks

--- pkgs/development/libraries/libvterm-neovim/default.nix
+++ pkgs/development/libraries/libvterm-neovim/default.nix
@@ -5,16 +5,12 @@
 , libtool
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (drv: {
   pname = "libvterm-neovim";
-  # Releases are not tagged, look at commit history to find latest release
-  version = "0.1.3";
-
-  src = fetchFromGitHub {
-    owner = "neovim";
-    repo = "libvterm";
-    rev = "65dbda3ed214f036ee799d18b2e693a833a0e591";
-    sha256 = "0r6yimzbkgrsi9aaxwvxahai2lzgjd1ysblr6m6by5w459853q3n";
+  version = "0.3";
+  src = builtins.fetchTarball {
+    url = "https://www.leonerd.org.uk/code/libvterm/libvterm-${drv.version}.tar.gz";
+    sha256 = "0zg6sn5brwrnqaab883pdj0l2swk5askbbwbdam0zq55ikbrzgar";
   };
 
   nativeBuildInputs = [ perl libtool ];
@@ -31,4 +27,4 @@ stdenv.mkDerivation {
     maintainers = with maintainers; [ rvolosatovs ];
     platforms = platforms.unix;
   };
-}
+})
