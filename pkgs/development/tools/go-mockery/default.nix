257eb8ea53817cc1399317b4fbef08b01e3daef1
Mathieu Post <mathieupost@gmail.com>
2022-09-30 15:48:56 +0200

    go-mockery: 2.9.2 -> 2.14.1
    fixes errors with go 1.18

--- pkgs/development/tools/go-mockery/default.nix
+++ pkgs/development/tools/go-mockery/default.nix
@@ -2,16 +2,29 @@
 
 buildGoModule rec {
   pname = "go-mockery";
-  version = "2.9.2";
+  version = "2.14.1";
 
   src = fetchFromGitHub {
     owner = "vektra";
     repo = "mockery";
     rev = "v${version}";
-    sha256 = "sha256-+r9he3rlANGusv0vIZPSninaouMftRsfJWnm3VngcXU=";
+    sha256 = "sha256-FgDjuiBFzOaT8GlJYI7xNfxC9uhyZtBAIBFXZgW0BDU=";
   };
 
-  vendorSha256 = "sha256-//V3ia3YP1hPgC1ipScURZ5uXU4A2keoG6dGuwaPBcA=";
+  patches = [ ./fix-tests.patch ];
+
+  preCheck = ''
+    substituteInPlace ./pkg/generator_test.go --replace 0.0.0-dev ${version}
+  '';
+
+  ldflags = [
+    "-s" "-w"
+    "-X" "github.com/vektra/mockery/v2/pkg/config.SemVer=v${version}"
+  ];
+
+  CGO_ENABLED = false;
+
+  vendorSha256 = "sha256-+40n7OoP8TLyjj4ehBHOD6/SqzJMCHsISE0FrXUL3Q8=";
 
   meta = with lib; {
     homepage = "https://github.com/vektra/mockery";
