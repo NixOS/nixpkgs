NixOS/nixpkgs

commit bf0962385217abd159088b7eb9ce982a02379a02

llvmPackages: Update Linux default to 11
Update the default LLVM version on Linux to stay synchronized with
Darwin, where LLVM is tied to the stdenv.

pkgs/top-level/all-packages.nix
@@ -12607,10 +12607,7 @@ with pkgs;
       /**/ if platform.isDarwin then 11
       else if platform.isFreeBSD then 7
       else if platform.isAndroid then 12
-      else if platform.isLinux then
-        /**/ if platform.isRiscV then 11
-        else if platform.isMusl then 11
-        else 7
+      else if platform.isLinux then 11
       else if platform.isWasm then 8
       else latest_version;
     # We take the "max of the mins". Why? Since those are lower bounds of the

commit 348d19ad66fa9dfb9c6b9a63a830fc188cac864a

llvmPackages: set version 7 for armv6
In later versions the compiler-rt seems to be producing invalid code
and ultimately doesn't build on armv6.

Note: armv6 builds have been broken since bf096238.

pkgs/top-level/all-packages.nix
@@ -14259,6 +14259,7 @@ with pkgs;
       /**/ if platform.isDarwin then 11
       else if platform.isFreeBSD then 7
       else if platform.isAndroid then 12
+      else if platform.system == "armv6l-linux" then 7  # This fixes armv6 cross-compilation
       else if platform.isLinux then 11
       else if platform.isWasm then 12
       else latest_version;