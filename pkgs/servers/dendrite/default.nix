7414171dd1b127a961efb18a76296b61831e929e
Winter <winter@winter.cafe>
2022-09-30 16:49:36 -0400

    dendrite: 0.9.9 -> 0.10.1

--- pkgs/servers/dendrite/default.nix
+++ pkgs/servers/dendrite/default.nix
@@ -3,16 +3,16 @@
 
 buildGoModule rec {
   pname = "matrix-dendrite";
-  version = "0.9.9";
+  version = "0.10.1";
 
   src = fetchFromGitHub {
     owner = "matrix-org";
     repo = "dendrite";
     rev = "v${version}";
-    sha256 = "sha256-HPcPaI0JACyDJAHTGSXHoIuWU34A8pV7blmt/zKOUB4=";
+    sha256 = "sha256-o+vdxjxbgTPhXtmjrMXf/kBGYaaF3nvVnrG+PGnaM70=";
   };
 
-  vendorSha256 = "sha256-jX8NJYrhUHRUiZRTqzgMK3mgRhzPJGFNTCK4SuEHKzg=";
+  vendorSha256 = "sha256-sd2frDxtMd0YUVCCQGaNYU7KopZQC1Ld5OHnjttTrgA=";
 
   # some tests are racy, re-enable once upstream has fixed them
   doCheck = false;
