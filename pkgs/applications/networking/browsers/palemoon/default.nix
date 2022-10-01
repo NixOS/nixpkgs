26661728168b2f5af812bf7b219f152d0d444df0
OPNA2608 <christoph.neidahl@gmail.com>
2022-10-02 01:05:38 +0200

    palemoon: 31.2.0.1 -> 31.3.0.1

--- pkgs/applications/networking/browsers/palemoon/default.nix
+++ pkgs/applications/networking/browsers/palemoon/default.nix
@@ -45,7 +45,7 @@ assert with lib.strings; (
 
 stdenv.mkDerivation rec {
   pname = "palemoon";
-  version = "31.2.0.1";
+  version = "31.3.0.1";
 
   src = fetchFromGitea {
     domain = "repo.palemoon.org";
@@ -53,7 +53,7 @@ stdenv.mkDerivation rec {
     repo = "Pale-Moon";
     rev = "${version}_Release";
     fetchSubmodules = true;
-    sha256 = "sha256-ytJC3QW9grbI6DusYITACc40+xUJ94+ATVGaOzWAwHU=";
+    sha256 = "sha256-qspzTlhK9MRJBsXxmYzl5K6in09LZPvaVc+ibovJAD8=";
   };
 
   nativeBuildInputs = [
