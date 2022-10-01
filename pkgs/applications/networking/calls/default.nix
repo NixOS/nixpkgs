4eb5696d61ebc9ea5832ae00cb6b86dbb008d6f4
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2022-10-01 12:55:47 +1000

    calls: update meta.homepage to gitlab.gnome.org

--- pkgs/applications/networking/calls/default.nix
+++ pkgs/applications/networking/calls/default.nix
@@ -106,7 +106,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "A phone dialer and call handler";
     longDescription = "GNOME Calls is a phone dialer and call handler. Setting NixOS option `programs.calls.enable = true` is recommended.";
-    homepage = "https://source.puri.sm/Librem5/calls";
+    homepage = "https://gitlab.gnome.org/GNOME/calls";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ craigem lheckemann tomfitzhenry ];
     platforms = platforms.linux;
