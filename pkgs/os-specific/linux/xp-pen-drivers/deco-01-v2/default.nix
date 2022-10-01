09be1945591957150e999e11788fc19eb9ca47c7
Alexandre Iooss <erdnaxe@crans.org>
2022-10-01 19:06:55 +0200

    xp-pen-drivers-deco-01-v2: fix src style

--- pkgs/os-specific/linux/xp-pen-drivers/deco-01-v2/default.nix
+++ pkgs/os-specific/linux/xp-pen-drivers/deco-01-v2/default.nix
@@ -24,8 +24,7 @@ stdenv.mkDerivation rec {
 
   src = fetchzip {
     url = "https://www.xp-pen.com/download/file/id/1936/pid/440/ext/gz.html#.tar.gz";
-    name = "xp-pen-deco-01-v2-driver-${version}.tar.gz";
-    sha256 = "sha256-n/yutkRsjcIRRhB4q1yqEmaa03/1SO8RigJi/ZkfLbk=";
+    hash = "sha256-n/yutkRsjcIRRhB4q1yqEmaa03/1SO8RigJi/ZkfLbk=";
   };
 
   nativeBuildInputs = [
