23434b89f9022fe909dc777e60b8573c215458b7
Curtis John <admin@curtisrayjohn.com>
2022-10-01 18:37:00 -0400

    kns: init at unstable-2023-04-25

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32952,6 +32952,8 @@ with pkgs;
 
   kn = callPackage ../applications/networking/cluster/kn { };
 
+  kns = callPackage ../applications/networking/cluster/kns { };
+
   kondo = callPackage ../applications/misc/kondo { };
 
   kooha = callPackage ../applications/video/kooha { };
