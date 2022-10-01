4264812cf35e02d0c2f39f43a6db69d3792096c1
Alexandre Iooss <erdnaxe@crans.org>
2022-10-01 18:29:14 +0200

    ipaexfont: use mkDerivation

--- pkgs/data/fonts/ipaexfont/default.nix
+++ pkgs/data/fonts/ipaexfont/default.nix
@@ -1,16 +1,23 @@
-{ lib, fetchzip }:
+{ lib, stdenv, fetchzip }:
 
-fetchzip {
-  name = "ipaexfont-004.01";
+stdenv.mkDerivation {
+  pname = "ipaexfont";
+  version = "4.01";
 
-  url = "https://moji.or.jp/wp-content/ipafont/IPAexfont/IPAexfont00401.zip";
+  src = fetchzip {
+    url = "https://moji.or.jp/wp-content/ipafont/IPAexfont/IPAexfont00401.zip";
+    hash = "sha256-/87qJIb+v4qrtDy+ApfXxh59reOk+6RhGqFN98mc+8Q=";
+  };
 
-  postFetch = ''
-    mkdir -p $out/share/fonts
-    unzip -j $downloadedFile \*.ttf -d $out/share/fonts/opentype
+  dontConfigure = true;
+  dontBuild = true;
+
+  installPhase = ''
+    install -D -m 644 -t "$out/share/fonts/truetype" *.ttf
   '';
 
-  sha256 = "0wp369kri33kb1mmiq4lpl9i4xnacw9fj63ycmkmlkq64s8qnjnx";
+  outputHashMode = "recursive";
+  outputHash = "sha256-11NYDZ3umJqU+oqkfJgrmNHb5nJmOM0ShuAmhCZ+fb4=";
 
   meta = with lib; {
     description = "Japanese font package with Mincho and Gothic fonts";
