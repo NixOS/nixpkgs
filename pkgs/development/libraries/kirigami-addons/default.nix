19e66a67b0b1090b0d9c9623a688465efe4a428f
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-10-01 00:36:42 -0400

    kirigami-addons: 21.05 -> 0.4

--- pkgs/development/libraries/kirigami-addons/default.nix
+++ pkgs/development/libraries/kirigami-addons/default.nix
@@ -12,14 +12,14 @@
 
 mkDerivation rec {
   pname = "kirigami-addons";
-  version = "21.05";
+  version = "0.4";
 
   src = fetchFromGitLab {
     domain = "invent.kde.org";
     owner = "libraries";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0pwkpag15mvzhd3hvdwx0a8ajwq9j30r6069vsx85bagnag3zanh";
+    sha256 = "sha256-3RPOab10kLcMVBngcRILDXbhOBI/BhjkMZqWVC0IPlM=";
   };
 
   nativeBuildInputs = [
