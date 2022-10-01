b912f34820c153456348b55e51e00a4c9774a6dc
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-01 18:46:00 +0000

    doctl: 1.82.0 -> 1.82.2

--- pkgs/development/tools/doctl/default.nix
+++ pkgs/development/tools/doctl/default.nix
@@ -2,7 +2,7 @@
 
 buildGoModule rec {
   pname = "doctl";
-  version = "1.82.0";
+  version = "1.82.2";
 
   vendorSha256 = null;
 
@@ -31,7 +31,7 @@ buildGoModule rec {
     owner = "digitalocean";
     repo = "doctl";
     rev = "v${version}";
-    sha256 = "sha256-w8km6Ze2qAoYGZZxfHucQSBEOv4d+mWatfeYYI2lrRw=";
+    sha256 = "sha256-o8Wp9zrpIkeigQhbsaMURIiL6coOX8OwUDQmRfO8VoY=";
   };
 
   meta = with lib; {
