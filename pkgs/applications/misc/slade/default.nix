5be149a0ae10069f2845173bf5a752236c4914e8
Mrinal Purohit <github@mrinalpurohit.in>
2022-10-02 01:27:46 +0530

    slade: 3.1.1.5 -> 3.2.1

--- pkgs/applications/misc/slade/default.nix
+++ pkgs/applications/misc/slade/default.nix
@@ -1,18 +1,26 @@
-{ lib, stdenv, fetchFromGitHub, cmake, pkg-config, wxGTK, gtk2, sfml, fluidsynth, curl, freeimage, ftgl, glew, zip }:
+{ lib, stdenv, fetchFromGitHub, cmake, pkg-config, wxGTK, gtk2, sfml, fluidsynth
+, curl, freeimage, ftgl, glew, zip, lua, mpg123 }:
 
 stdenv.mkDerivation rec {
   pname = "slade";
-  version = "3.1.1.5";
+  version = "3.2.1";
 
   src = fetchFromGitHub {
     owner = "sirjuddington";
     repo = "SLADE";
     rev = version;
-    sha256 = "0mdn59jm6ab4cdh99bgvadif3wdlqmk5mq635gg7krq35njgw6f6";
+    sha256 = "sha256-KFRX3sfI//Op/h/EfEuAZOY22RO5qNXmvhSksC0aS4U=";
   };
 
   nativeBuildInputs = [ cmake pkg-config zip ];
-  buildInputs = [ wxGTK gtk2 sfml fluidsynth curl freeimage ftgl glew ];
+  buildInputs =
+    [ wxGTK gtk2 sfml fluidsynth curl freeimage ftgl glew lua mpg123 ];
+
+  cmakeFlags = [
+    "-DwxWidgets_CONFIG_EXECUTABLE=${wxGTK}/bin/wx-config"
+    "-DWX_GTK3=OFF"
+    "-DNO_WEBVIEW=1"
+  ];
 
   meta = with lib; {
     description = "Doom editor";
