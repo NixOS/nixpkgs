ffb43186dc018f1a83587542bbb6bcefb50dfead
Nick Cao <nickcao@nichi.co>
2022-10-02 14:37:56 +0800

    stellarium: 0.22.2 -> 1.0

--- pkgs/applications/science/astronomy/stellarium/default.nix
+++ pkgs/applications/science/astronomy/stellarium/default.nix
@@ -1,25 +1,51 @@
-{ stdenv, lib, fetchFromGitHub
-, cmake, freetype, libpng, libGLU, libGL, openssl, perl, libiconv
-, qtscript, qtserialport, qttools, qtcharts
-, qtmultimedia, qtlocation, qtbase, wrapQtAppsHook
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, perl
+, wrapQtAppsHook
+, qtbase
+, qtcharts
+, qtlocation
+, qtmultimedia
+, qtscript
+, qtserialport
+, qtwebengine
+, calcmysky
+, qxlsx
+, indilib
+, libnova
 }:
 
 stdenv.mkDerivation rec {
   pname = "stellarium";
-  version = "0.22.2";
+  version = "1.0";
 
   src = fetchFromGitHub {
     owner = "Stellarium";
     repo = "stellarium";
     rev = "v${version}";
-    sha256 = "sha256-FBH5IB1keMzRP06DQK2e7HX8rwm5/sdTX+cB80uG0vw=";
+    sha256 = "sha256-6EAykJ0yWeU1EBR5+7JjWGUVBE1DKW+W8yJOt0smkaE=";
   };
 
-  nativeBuildInputs = [ cmake perl wrapQtAppsHook ];
+  nativeBuildInputs = [
+    cmake
+    perl
+    wrapQtAppsHook
+  ];
 
   buildInputs = [
-    freetype libpng libGLU libGL openssl libiconv qtscript qtserialport qttools
-    qtmultimedia qtlocation qtbase qtcharts
+    qtbase
+    qtcharts
+    qtlocation
+    qtmultimedia
+    qtscript
+    qtserialport
+    qtwebengine
+    calcmysky
+    qxlsx
+    indilib
+    libnova
   ];
 
   preConfigure = lib.optionalString stdenv.isDarwin ''
