946474ca0fdcde59fc05027fbd511ab3ce46131a
nanashi0x74 <rian.lindenberger@gmail.com>
2022-10-02 20:07:18 +0200

    firmware-updater: build with flutter2

--- pkgs/os-specific/linux/firmware/firmware-updater/default.nix
+++ pkgs/os-specific/linux/firmware/firmware-updater/default.nix
@@ -1,13 +1,13 @@
 { lib
-, flutter
+, flutter2
 , fetchFromGitHub
 }:
 
-flutter.mkFlutterApp {
+flutter2.mkFlutterApp {
   pname = "firmware-updater";
   version = "unstable";
 
-  vendorHash = "sha256-3wVA9BLCnMijC0gOmskz+Hv7NQIGu/jhBDbWjmoq1Tc=";
+  vendorHash = "sha256-7uOiebGBcX61oUyNCi1h9KldTRTrCfYaHUQSH4J5OoQ=";
 
   src = fetchFromGitHub {
     owner = "canonical";
