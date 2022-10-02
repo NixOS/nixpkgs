f3416858078bf519609b8559312bf18e1ba1fe3b
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-10-02 15:15:35 +0200

    python3Packages.python-glanceclient: Use buildPythonPackage
    and add a top-level attribute.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21180,6 +21180,7 @@ with pkgs;
   openslp = callPackage ../development/libraries/openslp {};
 
   openstackclient = with python3Packages; toPythonApplication python-openstackclient;
+  glanceclient = with python3Packages; toPythonApplication python-glanceclient;
   heatclient = with python3Packages; toPythonApplication python-heatclient;
 
   openvdb = callPackage ../development/libraries/openvdb {};
