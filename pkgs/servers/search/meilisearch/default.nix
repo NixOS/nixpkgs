f72c3c04b16ff5074488a8605b31d0c331260d5b
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-03 18:17:05 +0000

    meilisearch: 0.28.1 -> 0.29.0

--- pkgs/servers/search/meilisearch/default.nix
+++ pkgs/servers/search/meilisearch/default.nix
@@ -8,7 +8,7 @@
 , nixosTests
 }:
 
-let version = "0.28.1";
+let version = "0.29.0";
 in
 rustPlatform.buildRustPackage {
   pname = "meilisearch";
@@ -17,9 +17,9 @@ rustPlatform.buildRustPackage {
     owner = "meilisearch";
     repo = "MeiliSearch";
     rev = "v${version}";
-    sha256 = "sha256-AjSzzOQ1K47tyc8Zn7kAU3B1UE9Tfvd3SvP7W13m6/o=";
+    sha256 = "sha256-/B44rqw4oilR7w2GnJ5LYFIWh9qv2N5Bwa+ZCgugTpQ=";
   };
-  cargoSha256 = "sha256-zRSAjOKKmL79eXrA6/ZNE3lo8MFdOqYJkcagcA51c6M=";
+  cargoSha256 = "sha256-70Nn2yvIM05K1TWh4umbh0ZHkrjPFogQ4DfCjQmgOcM=";
   # Default features include mini dashboard which downloads something from the internet.
   buildNoDefaultFeatures = true;
   buildInputs = lib.optionals stdenv.isDarwin [ Security DiskArbitration Foundation ];
