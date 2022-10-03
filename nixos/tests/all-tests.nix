78f929c5a6ac6320f5c99eadd805bac4d208c863
oxalica <oxalicc@pm.me>
2022-10-04 01:10:03 +0800

    nixos/tests/systemd-initrd-luks-fido2: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -595,6 +595,7 @@ in {
   systemd-cryptenroll = handleTest ./systemd-cryptenroll.nix {};
   systemd-escaping = handleTest ./systemd-escaping.nix {};
   systemd-initrd-btrfs-raid = handleTest ./systemd-initrd-btrfs-raid.nix {};
+  systemd-initrd-luks-fido2 = handleTest ./systemd-initrd-luks-fido2.nix {};
   systemd-initrd-luks-keyfile = handleTest ./systemd-initrd-luks-keyfile.nix {};
   systemd-initrd-luks-password = handleTest ./systemd-initrd-luks-password.nix {};
   systemd-initrd-luks-tpm2 = handleTest ./systemd-initrd-luks-tpm2.nix {};
