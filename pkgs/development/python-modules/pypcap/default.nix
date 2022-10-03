53e7c51ddcac1b05d03b2e166101293996e97d1f
Alvar Penning <post@0x21.biz>
2022-10-03 18:35:23 +0200

    pythonPackage.pypcap: 1.2.3 -> 1.3.0

--- pkgs/development/python-modules/pypcap/default.nix
+++ pkgs/development/python-modules/pypcap/default.nix
@@ -2,31 +2,21 @@
 , buildPythonPackage
 , dpkt
 , fetchFromGitHub
-, fetchpatch
 , libpcap
 , pytestCheckHook
 }:
 
 buildPythonPackage rec {
   pname = "pypcap";
-  version = "1.2.3";
-
+  version = "1.3.0";
 
   src = fetchFromGitHub {
     owner = "pynetwork";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "1zscfk10jpqwxgc8d84y8bffiwr92qrg2b24afhjwiyr352l67cf";
+    # No release was tagged and PyPI doesn't contain tests.
+    rev = "968859f0ffb5b7c990506dffe82457b7de23a026";
+    sha256 = "sha256-NfyEC3qEBm6TjebcDIsoz8tJWaJ625ZFPfx7AMyynWE=";
   };
-
-  patches = [
-    # Support for Python 3.9, https://github.com/pynetwork/pypcap/pull/102
-    (fetchpatch {
-      name = "support-python-3.9.patch";
-      url = "https://github.com/pynetwork/pypcap/pull/102/commits/e22f5d25f0d581d19ef337493434e72cd3a6ae71.patch";
-      sha256 = "0n1syh1vcplgsf6njincpqphd2w030s3b2jyg86d7kbqv1w5wk0l";
-    })
-  ];
 
   postPatch = ''
     # Add the path to libpcap in the nix-store
