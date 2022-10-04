c7f2e5359babdf02478403a737e479c6f459f99a
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-04 09:00:43 +0200

    python310Packages.google-cloud-secret-manager: update disabled

--- pkgs/development/python-modules/google-cloud-secret-manager/default.nix
+++ pkgs/development/python-modules/google-cloud-secret-manager/default.nix
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   version = "2.12.5";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
