a9668e31ee8451dea5226c3ad4ed8f82f78cd92e
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-04 08:38:19 +0000

    datree: 1.6.33 -> 1.6.37

--- pkgs/development/tools/datree/default.nix
+++ pkgs/development/tools/datree/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "datree";
-  version = "1.6.33";
+  version = "1.6.37";
 
   src = fetchFromGitHub {
     owner = "datreeio";
     repo = "datree";
     rev = version;
-    hash = "sha256-VuTa1bnwxk8SUvOK3jLEvgJTPf3eABUyBssUYtoAxTs=";
+    hash = "sha256-oDwI4rlpTkriPD2YC8AnlPYHUchC7btYyX/X8sxmvac=";
   };
 
   vendorSha256 = "sha256-gjD24nyQ8U1WwhUbq8N4dvzFK74t3as7wWZK7rh9yiw=";
