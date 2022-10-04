545de296e01d54b995b08361c7a72a517b68acbf
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-04 08:57:26 +0200

    python310Packages.google-cloud-speech: update disabled

--- pkgs/development/python-modules/google-cloud-speech/default.nix
+++ pkgs/development/python-modules/google-cloud-speech/default.nix
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   version = "2.16.0";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
