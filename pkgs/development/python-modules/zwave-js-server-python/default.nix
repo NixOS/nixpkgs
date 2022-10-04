0d3161da8f740c5bc8b82ff1c4b9c4ecf39714c7
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-04 21:28:22 +0000

    python310Packages.zwave-js-server-python: 0.41.1 -> 0.43.0

--- pkgs/development/python-modules/zwave-js-server-python/default.nix
+++ pkgs/development/python-modules/zwave-js-server-python/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "zwave-js-server-python";
-  version = "0.41.1";
+  version = "0.43.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "home-assistant-libs";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-V4QEsHkakqYsZ09Q8BJG9fhREdGboirF5sNxm8t+dAQ=";
+    hash = "sha256-qzAM3vcVySJB6OfOHKXYYXDR+xOSbGpyH7b9TaYkOLM=";
   };
 
   propagatedBuildInputs = [
