6f8a7852e2f70f44e444c272044536800a1f5eda
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-04 09:04:31 +0200

    python310Packages.google-cloud-datacatalog: update disabled

--- pkgs/development/python-modules/google-cloud-datacatalog/default.nix
+++ pkgs/development/python-modules/google-cloud-datacatalog/default.nix
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   version = "3.9.2";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
