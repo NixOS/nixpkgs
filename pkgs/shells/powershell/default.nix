aa760dbbf0a3b4ebdb826efa996cc1ee1a05b160
Anselm Schüler <mail@anselmschueler.com>
2022-10-05 00:37:43 +0200

    powershell: 7.2.4 -> 7.2.6

--- pkgs/shells/powershell/default.nix
+++ pkgs/shells/powershell/default.nix
@@ -7,10 +7,10 @@ let archString = if stdenv.isAarch64 then "arm64"
     platformString = if stdenv.isDarwin then "osx"
                      else if stdenv.isLinux then "linux"
                      else throw "unsupported platform";
-    platformSha = if (stdenv.isDarwin && stdenv.isx86_64) then "sha256-bcLyf/sIaFoS1xS4RLPPC9lVFa94IfQlWeXhyXUTsd0="
-                     else if (stdenv.isDarwin && stdenv.isAarch64) then "sha256-2UACjUtyQ611iXmwyiWrGwRVA0FT1cLLMKnY0y4SgoQ="
-                     else if (stdenv.isLinux && stdenv.isx86_64) then "sha256-5AZGwxpEqn3X20rCxPcvuqcQib689ui+e0jvri92EdA="
-                     else if (stdenv.isLinux && stdenv.isAarch64) then "sha256-90Sz32hm+EcK3nFJOGGCSqIEtW7w48G8mizXvcLb8WU="
+    platformSha = if (stdenv.isDarwin && stdenv.isx86_64) then "sha256-XyEzF7wt4k+F/IMV0ydpr29NdMNw0opmR6o7XFi2scI="
+                     else if (stdenv.isDarwin && stdenv.isAarch64) then "sha256-N1Y20jf4yZmuTRNHMc/PUvfuVs90eNgP8WF1lt06CR8="
+                     else if (stdenv.isLinux && stdenv.isx86_64) then "sha256-AR1b/EC64rZSf/YbObpOZLO83EjF4ZIGlYfvSzEDpJw="
+                     else if (stdenv.isLinux && stdenv.isAarch64) then "sha256-HNFzJA+b8H2oschTpCYW2NyI4a43sW05wwxXBnurvTU="
                      else throw "unsupported platform";
     platformLdLibraryPath = if stdenv.isDarwin then "DYLD_FALLBACK_LIBRARY_PATH"
                      else if stdenv.isLinux then "LD_LIBRARY_PATH"
@@ -20,7 +20,7 @@ let archString = if stdenv.isAarch64 then "arm64"
 in
 stdenv.mkDerivation rec {
   pname = "powershell";
-  version = "7.2.4";
+  version = "7.2.6";
 
   src = fetchzip {
     url = "https://github.com/PowerShell/PowerShell/releases/download/v${version}/powershell-${version}-${platformString}-${archString}.tar.gz";
