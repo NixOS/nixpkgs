5e4543e9f0ebffdfde9b6a512e9d99924beaa3ad
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-04 09:06:35 +0200

    python310Packages.google-cloud-appengine-logging: update disabled

--- pkgs/development/python-modules/google-cloud-appengine-logging/default.nix
+++ pkgs/development/python-modules/google-cloud-appengine-logging/default.nix
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   version = "1.1.5";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
