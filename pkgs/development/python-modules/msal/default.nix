9b4117a29b8827bc638261a943607a7d659d51a0
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-10-05 01:56:07 +0200

    python3Packages.msal: propagate pyjwt[crypto]

--- pkgs/development/python-modules/msal/default.nix
+++ pkgs/development/python-modules/msal/default.nix
@@ -21,12 +21,8 @@ buildPythonPackage rec {
   propagatedBuildInputs = [
     pyjwt
     requests
-  ];
-
-  postPatch = ''
-    substituteInPlace setup.py \
-      --replace "PyJWT[crypto]>=1.0.0,<3" "PyJWT"
-  '';
+  ]
+  ++ pyjwt.optional-dependencies.crypto;
 
   # Tests assume Network Connectivity:
   # https://github.com/AzureAD/microsoft-authentication-library-for-python/blob/e2958961e8ec16d0af4199f60c36c3f913497e48/tests/test_authority.py#L73
