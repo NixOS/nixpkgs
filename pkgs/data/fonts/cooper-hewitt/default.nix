fc95752d6f7edd65d443c9dd5fd28b4a7b54cf4d
Sandro Jäckel <sandro.jaeckel@sap.com>
2022-10-04 16:49:08 +0200

    Revert "cooper-hewitt: use mkDerivation"
    This reverts commit 9debb9217892cca73acc61232f6116fa4da6b045.

--- pkgs/data/fonts/cooper-hewitt/default.nix
+++ pkgs/data/fonts/cooper-hewitt/default.nix
@@ -1,20 +1,16 @@
-{ lib, stdenv, fetchzip }:
+{ lib, fetchzip }:
 
-stdenv.mkDerivation {
-  pname = "cooper-hewitt";
-  version = "unstable-2014-06-09";
+fetchzip {
+  name = "cooper-hewitt-2014-06-09";
 
-  src = fetchzip {
-    url = "https://www.cooperhewitt.org/wp-content/uploads/fonts/CooperHewitt-OTF-public.zip";
-    hash = "sha256-bTlEXQeYNNspvnNdvQhJn6CNBrcSKYWuNWF/N6+3Vb0=";
-  };
+  url = "https://www.cooperhewitt.org/wp-content/uploads/fonts/CooperHewitt-OTF-public.zip";
 
-  dontConfigure = true;
-  dontBuild = true;
-
-  installPhase = ''
-    install -D -m 644 -t "$out/share/fonts/opentype" *.otf
+  postFetch = ''
+    mkdir -p $out/share/fonts/opentype
+    unzip -j $downloadedFile \*.otf -d $out/share/fonts/opentype/
   '';
+
+  sha256 = "01iwqmjvqkc6fmc2r0486vk06s6f51n9wxzl1pf9z48n0igj4gqd";
 
   meta = with lib; {
     homepage = "https://www.cooperhewitt.org/open-source-at-cooper-hewitt/cooper-hewitt-the-typeface-by-chester-jenkins/";
