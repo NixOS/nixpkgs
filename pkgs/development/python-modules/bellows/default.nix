3c2e3c568642d6d6c8b9ad10dc2b720f63af2212
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-10-05 22:02:17 +0200

    python3Packages.bellows: 0.33.1 -> 0.34.1
    https://github.com/zigpy/bellows/releases/tag/0.34.1
    https://github.com/zigpy/bellows/releases/tag/0.34.0

--- pkgs/development/python-modules/bellows/default.nix
+++ pkgs/development/python-modules/bellows/default.nix
@@ -17,14 +17,14 @@
 
 buildPythonPackage rec {
   pname = "bellows";
-  version = "0.33.1";
+  version = "0.34.1";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "zigpy";
     repo = "bellows";
     rev = "refs/tags/${version}";
-    sha256 = "sha256-cpWQdsuW3CA/8HowhMoVV++rrDnjFQcgp+A5CCElj6o=";
+    sha256 = "sha256-a2skDJVqbct1+Ky2D8LXv8VMqFwqznUzXh+o+M6GtvQ=";
   };
 
   propagatedBuildInputs = [
