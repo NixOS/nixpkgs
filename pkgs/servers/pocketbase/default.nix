6b5c4a3b1b10d7a59c195f011ba50b9860f86680
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-05 20:36:33 +0000

    pocketbase: 0.7.7 -> 0.7.9

--- pkgs/servers/pocketbase/default.nix
+++ pkgs/servers/pocketbase/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "pocketbase";
-  version = "0.7.7";
+  version = "0.7.9";
 
   src = fetchFromGitHub {
     owner = "pocketbase";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-pHYbz1LuPZDOtg3xrvgr+16ziZMZHCtSbY685E8RA1E=";
+    sha256 = "sha256-3dsgBWpYuNIVLTdxbaanMVnXYpw5f1ct9ksIrJqpURw=";
   };
 
   vendorSha256 = "sha256-i3CRba2HA7dOEh4PU1rNZUl05pZqIm946lIjP7ZcFEc=";
