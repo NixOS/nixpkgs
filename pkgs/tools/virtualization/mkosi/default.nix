fac8a0a521481f81c1c9dd81b32f135c16148b13
Jonas Heinrich <onny@project-insanity.org>
2022-10-06 13:24:04 +0200

    testing

--- pkgs/tools/virtualization/mkosi/default.nix
+++ pkgs/tools/virtualization/mkosi/default.nix
@@ -1,6 +1,8 @@
 { lib
 , fetchFromGitHub
 , python3
+, stdenv
+, glibc
 }:
 
 python3.pkgs.buildPythonApplication rec {
@@ -8,15 +10,28 @@ python3.pkgs.buildPythonApplication rec {
   version = "13";
   format = "setuptools";
 
-  src = fetchFromGitHub {
-    owner = "systemd";
-    repo = "mkosi";
-    rev = "v${version}";
-    hash = "sha256-4eBxxCPpMcbnTtGpYw0FuxdNZ7sxSbaaavoJZ6Kboa0=";
-  };
+  #src = fetchFromGitHub {
+  #  owner = "systemd";
+  #  repo = "mkosi";
+  #  rev = "v${version}";
+  #  hash = "sha256-4eBxxCPpMcbnTtGpYw0FuxdNZ7sxSbaaavoJZ6Kboa0=";
+  #};
+  src = /home/onny/projects/mkosi;
+  unpackPhase = ''cp -r --no-preserve=mode $src/* .'';
 
   propagatedBuildInputs = with python3.pkgs; [
     pexpect
+  ];
+
+  postInstall = ''
+    wrapProgram $out/bin/mkosi \
+      --prefix PYTHONPATH : "$PYTHONPATH" \
+      --prefix LD_LIBRARY_PATH :"${lib.makeLibraryPath [ stdenv.cc.libc.out glibc.out ]}"
+  '';
+
+  disabledTests = [
+    "test_os_distribution"
+    "test_centos_brtfs"
   ];
 
   checkInputs = with python3.pkgs; [
