a6bffb6e446655f69e694e04905389eef6ab7785
techknowlogick <techknowlogick@gitea.io>
2022-10-05 23:28:50 -0400

    nomad_1_3: 1.3.5 -> 1.3.6
    (cherry picked from commit 19b7aae97774ff4161c806447eba84bb67ea44ce)

--- pkgs/applications/networking/cluster/nomad/1.3.nix
+++ pkgs/applications/networking/cluster/nomad/1.3.nix
@@ -6,7 +6,7 @@
 
 callPackage ./generic.nix {
   inherit buildGoModule nvidia_x11 nvidiaGpuSupport;
-  version = "1.3.5";
-  sha256 = "sha256-WKS7EfZxysy/oyq1fa8rKvmfgHRiB7adSVhALZNFYgo=";
-  vendorSha256 = "sha256-byc6wAxpqhxlN3kyHyFQeBS9/oIjHeoz6ldYskizgaI=";
+  version = "1.3.6";
+  sha256 = "sha256-E1+QFaakAsqeXxAfY80ExWVIud7Q/q2TaUVsmADjsgo=";
+  vendorSha256 = "sha256-kgTRjPr7GsoBeE/s9wrmUWE5jv1ZmszfVDsVaRbdx14=";
 }
