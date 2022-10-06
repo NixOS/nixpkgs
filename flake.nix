33c8c0fb00c5a5ca6a13cd0e61c43175ad73d21d
Robert Hensing <robert@roberthensing.nl>
2022-10-06 16:52:38 +0200

    flake.nix: Improve nixosModules.notDetected error location reporting
    This is mostly equivalent, but `import` was hiding the location
    from the module system, breaking error location reporting and
    breaking `disabledModules` support for this module (unlikely).

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
       legacyPackages = forAllSystems (system: import ./. { inherit system; });
 
       nixosModules = {
-        notDetected = import ./nixos/modules/installer/scan/not-detected.nix;
+        notDetected = ./nixos/modules/installer/scan/not-detected.nix;
       };
     };
 }
