b0265aadac2813d5e86605f9ff7da141ca9e4bb2
Maximilian Bosch <maximilian@mbosch.me>
2022-10-06 12:25:52 +0200

    gcc10: fix build w/glibc-2.36
    Failing Hydra build: https://hydra.nixos.org/build/193327405
    Same fix as for gcc11.

--- pkgs/development/compilers/gcc/10/default.nix
+++ pkgs/development/compilers/gcc/10/default.nix
@@ -58,6 +58,7 @@ let majorVersion = "10";
         url = "https://gcc.gnu.org/git/?p=gcc.git;a=patch;h=de31f5445b12fd9ab9969dc536d821fe6f0edad0";
         sha256 = "0sd52c898msqg7m316zp0ryyj7l326cjcn2y19dcxqp15r74qj0g";
       })
+      ../11/fix-struct-redefinition-on-glibc-2.36.patch
     ] ++ optional (targetPlatform != hostPlatform) ../libstdc++-target.patch
       ++ optional noSysDirs ../no-sys-dirs.patch
       ++ optional (noSysDirs && hostPlatform.isRiscV) ../no-sys-dirs-riscv.patch
