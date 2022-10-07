dfe88a09d1929f43fa296d113b9cc2ae93a0e744
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-07 13:14:25 +0200

    rust-motd: modernize
    - add changelog to meta

--- pkgs/tools/misc/rust-motd/default.nix
+++ pkgs/tools/misc/rust-motd/default.nix
@@ -15,20 +15,27 @@ rustPlatform.buildRustPackage rec {
     owner = "rust-motd";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-w984vvjjieSv4eM3jT8zJIIR7/7pmADhR3Esj+2dCTs=";
+    hash = "sha256-w984vvjjieSv4eM3jT8zJIIR7/7pmADhR3Esj+2dCTs=";
   };
 
-  cargoSha256 = "sha256-L/QdFjSYm3PekKS3tdsUl8XBVyIBE044EHOIB+aEltI=";
+  cargoHash = "sha256-L/QdFjSYm3PekKS3tdsUl8XBVyIBE044EHOIB+aEltI=";
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [
+    pkg-config
+  ];
 
-  buildInputs = [ openssl ] ++ lib.optional stdenv.isDarwin Security;
+  buildInputs = [
+    openssl
+  ] ++ lib.optional stdenv.isDarwin [
+    Security
+  ];
 
   OPENSSL_NO_VENDOR = 1;
 
   meta = with lib; {
     description = "Beautiful, useful MOTD generation with zero runtime dependencies";
     homepage = "https://github.com/rust-motd/rust-motd";
+    changelog = "https://github.com/rust-motd/rust-motd/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ figsoda ];
   };
