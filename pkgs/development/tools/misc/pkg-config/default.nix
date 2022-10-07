0e8dee61f0f6f205cb81858fcd627bb41c89e648
cidkidnix <cidkidnix@protonmail.com>
2022-10-06 19:59:39 -0500

    pkg-config: fix aarch32 android

--- pkgs/development/tools/misc/pkg-config/default.nix
+++ pkgs/development/tools/misc/pkg-config/default.nix
@@ -35,7 +35,10 @@ stdenv.mkDerivation rec {
          "glib_cv_uscore=no"
          "ac_cv_func_posix_getpwuid_r=yes"
          "ac_cv_func_posix_getgrgid_r=yes"
-       ];
+       ]
+    ++ optional (stdenv.hostPlatform.useAndroidPrebuilt && stdenv.targetPlatform.isAarch32) [
+      "CFLAGS=-Wno-implicit-function-declaration"
+    ];
 
   enableParallelBuilding = true;
   doCheck = true;
