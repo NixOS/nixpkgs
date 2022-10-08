78fe5b0a4fabb9b7e1bd560b146fde572ebed36d
Anthony Roussel <anthony@roussel.dev>
2022-10-08 15:16:36 +0200

    gns3-gui,gns3-server: add anthonyroussel to maintainers

--- pkgs/applications/networking/gns3/server.nix
+++ pkgs/applications/networking/gns3/server.nix
@@ -80,6 +80,6 @@ in python.pkgs.buildPythonApplication {
     changelog = "https://github.com/GNS3/gns3-server/releases/tag/v${version}";
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ anthonyroussel ];
   };
 }
