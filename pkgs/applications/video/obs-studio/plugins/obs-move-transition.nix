ddffb609f5d509c90d72d39525dce439a9f2362b
Sebastian Sellmeier <mail@sebastian-sellmeier.de>
2022-10-08 09:43:27 +0200

    obs-studio-plugins.obs-move-transition: 2.4.3 -> 2.6.4

--- pkgs/applications/video/obs-studio/plugins/obs-move-transition.nix
+++ pkgs/applications/video/obs-studio/plugins/obs-move-transition.nix
@@ -9,35 +9,26 @@
 
 stdenv.mkDerivation rec {
   pname = "obs-move-transition";
-  version = "2.4.3";
+  version = "2.6.4";
 
   src = fetchFromGitHub {
     owner = "exeldro";
     repo = "obs-move-transition";
     rev = version;
-    sha256 = "sha256-/6PcNLOnBBqLZHVKMg1tdX9OktcllEEqnL93nXpuXL0=";
+    sha256 = "sha256-+kAdCM5PEFNxKNmJmf2ASTyUKA7xnbMAA7kP/emoaeI=";
   };
 
   nativeBuildInputs = [ cmake ];
   buildInputs = [ obs-studio ];
 
-  cmakeFlags = with lib; [
-    "-DLIBOBS_INCLUDE_DIR=${obs-studio.src}/libobs"
+  preConfigure = ''
+    cp ${obs-studio.src}/cmake/external/ObsPluginHelpers.cmake cmake/
+  '';
+
+  cmakeFlags = [
+    "-DBUILD_OUT_OF_TREE=On"
     "-Wno-dev"
   ];
-
-  preConfigure = ''
-    cp ${obs-studio.src}/cmake/external/FindLibobs.cmake FindLibobs.cmake
-  '';
-
-  patches = [ ./obs-move-transition-use-FindLibobs.cmake.patch ];
-
-  postPatch = ''
-    substituteInPlace move-source-filter.c --replace '<../UI/obs-frontend-api/obs-frontend-api.h>' '<obs-frontend-api.h>'
-    substituteInPlace move-value-filter.c --replace '<../UI/obs-frontend-api/obs-frontend-api.h>' '<obs-frontend-api.h>'
-    substituteInPlace move-transition.c --replace '<../UI/obs-frontend-api/obs-frontend-api.h>' '<obs-frontend-api.h>'
-    substituteInPlace audio-move.c --replace '<../UI/obs-frontend-api/obs-frontend-api.h>' '<obs-frontend-api.h>'
-  '';
 
   meta = with lib; {
     description = "Plugin for OBS Studio to move source to a new position during scene transition";
