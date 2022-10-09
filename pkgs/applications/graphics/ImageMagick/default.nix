8b5b0e23fe745e42c8cfe321cb8cfe9f8c1b8a4e
Kerstin Humm <kerstin@erictapen.name>
2022-10-09 12:01:38 +0200

    imagemagick: 7.1.0-49 -> 7.1.0-50

--- pkgs/applications/graphics/ImageMagick/default.nix
+++ pkgs/applications/graphics/ImageMagick/default.nix
@@ -46,13 +46,13 @@ in
 
 stdenv.mkDerivation rec {
   pname = "imagemagick";
-  version = "7.1.0-49";
+  version = "7.1.0-50";
 
   src = fetchFromGitHub {
     owner = "ImageMagick";
     repo = "ImageMagick";
     rev = version;
-    hash = "sha256-5EbmAPEUPFMY8VDJmXfcFuUzFI0xVw7fpVteSI9gotg=";
+    hash = "sha256-L5IiqHa776i7O6iw6Rj1CJOjsEIUDGjAxwpzhgzGdtE=";
   };
 
   outputs = [ "out" "dev" "doc" ]; # bin/ isn't really big
