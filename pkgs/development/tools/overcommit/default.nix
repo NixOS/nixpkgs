ddb0ac635709866853d4037367449ef61455ef62
Anthony Roussel <anthony@roussel.dev>
2022-10-09 21:17:46 +0200

    overcommit: add anthonyroussel to maintainers

--- pkgs/development/tools/overcommit/default.nix
+++ pkgs/development/tools/overcommit/default.nix
@@ -13,7 +13,7 @@ bundlerApp {
     description = "Tool to manage and configure Git hooks";
     homepage    = "https://github.com/sds/overcommit";
     license     = licenses.mit;
-    maintainers = with maintainers; [ Br1ght0ne ];
+    maintainers = with maintainers; [ Br1ght0ne anthonyroussel ];
     platforms   = platforms.unix;
   };
 }
