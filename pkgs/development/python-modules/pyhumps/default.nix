06c4dbb4cdcbd0ed7046dde3c655d228d6fe2fa5
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-10-09 22:09:09 +0200

    python3Packages.pyhumps: Fix regression in symbols export

--- pkgs/development/python-modules/pyhumps/default.nix
+++ pkgs/development/python-modules/pyhumps/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
+, fetchpatch
 , poetry-core
 , pytestCheckHook
 , pythonOlder
@@ -19,6 +20,14 @@ buildPythonPackage rec {
     rev = "v${version}";
     hash = "sha256-7jkwf4qGQ+AD4/hOrEe/oAPY+gnSySUVBWFf70rU7xc=";
   };
+
+  patches = [
+    (fetchpatch {
+      # https://github.com/nficano/humps/pull/281
+      url = "https://github.com/nficano/humps/commit/e248c26195804fa04c43e88c5682528f367e27b3.patch";
+      hash = "sha256-+TCVfuMgfkDaS1tPu4q6PIOC3Kn1MBWyuoyAO6W0/h4=";
+    })
+  ];
 
   nativeBuildInputs = [
     poetry-core
