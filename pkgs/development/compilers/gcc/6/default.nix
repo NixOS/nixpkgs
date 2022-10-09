89fda21faeccce95f523e3b1c20327a137c83014
Maximilian Bosch <maximilian@mbosch.me>
2022-10-09 11:37:29 +0200

    gccgo6: fix build w/glibc-2.36
    Failing Hydra build: https://hydra.nixos.org/build/193810432

--- pkgs/development/compilers/gcc/6/default.nix
+++ pkgs/development/compilers/gcc/6/default.nix
@@ -78,6 +78,7 @@ let majorVersion = "6";
       ++ optional langAda ./gnat-glibc234.patch
       ++ optional langFortran ../gfortran-driving.patch
       ++ optional (targetPlatform.libc == "musl") ../libgomp-dont-force-initial-exec.patch
+      ++ optional langGo ./gogcc-workaround-glibc-2.36.patch
 
       # Obtain latest patch with ../update-mcfgthread-patches.sh
       ++ optional (!crossStageStatic && targetPlatform.isMinGW && threadsCross.model == "mcf") ./Added-mcf-thread-model-support-from-mcfgthread.patch
