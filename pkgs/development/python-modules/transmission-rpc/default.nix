282ac9121fbd772ff461d16bc57ccdb8e8db8d84
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-10 12:01:46 +0000

    python310Packages.transmission-rpc: 3.3.2 -> 3.4.0

--- pkgs/development/python-modules/transmission-rpc/default.nix
+++ pkgs/development/python-modules/transmission-rpc/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "transmission-rpc";
-  version = "3.3.2";
+  version = "3.4.0";
   disabled = pythonOlder "3.6";
 
   format = "pyproject";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "Trim21";
     repo = "transmission-rpc";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-GkhNOKatT/hJFw1l1xrf43jtgxvJ+WVvhz83Oe0MZ6w=";
+    sha256 = "sha256-O+VimSIVsO4P7v+8HHdYujaKpPx4FV8bF/Nn4EHP2vo=";
   };
 
   # remove once upstream has tagged version with dumped typing-extensions
