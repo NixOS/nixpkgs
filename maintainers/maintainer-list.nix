3556c180757708247f9a4834093ff605adb38034
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2022-10-10 09:44:33 +0700

    maintainers: add bigzilla

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1745,6 +1745,12 @@
     githubId = 28444296;
     name = "Benjamin Hougland";
   };
+  bigzilla = {
+    email = "m.billyzaelani@gmail.com";
+    github = "bigzilla";
+    githubId = 20436235;
+    name = "Billy Zaelani Malik";
+  };
   billewanick = {
     email = "bill@ewanick.com";
     github = "billewanick";
