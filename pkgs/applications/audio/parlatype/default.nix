56651c23da795d089bfe8437c11fb82cc8e657a5
Alexander Shpilkin <ashpilkin@gmail.com>
2022-10-10 18:23:19 +0300

    parlatype: 2.1 -> 3.1
    (cherry picked from commit 0aa7db6030704cfef15de8e3fd90bfe26b01793c)

--- pkgs/applications/audio/parlatype/default.nix
+++ pkgs/applications/audio/parlatype/default.nix
@@ -1,14 +1,14 @@
-{ lib, stdenv, fetchFromGitHub, pkg-config, meson, gtk3, at-spi2-core, dbus, gst_all_1, sphinxbase, pocketsphinx, ninja, gettext, appstream-glib, python3, glib, gobject-introspection, gsettings-desktop-schemas, itstool, wrapGAppsHook, hicolor-icon-theme }:
+{ lib, stdenv, fetchFromGitHub, pkg-config, meson, gtk3, dbus, gst_all_1, ninja, gettext, appstream-glib, python3, desktop-file-utils, glib, gobject-introspection, gsettings-desktop-schemas, isocodes, itstool, libxml2, wrapGAppsHook, hicolor-icon-theme }:
 
 stdenv.mkDerivation rec {
   pname = "parlatype";
-  version = "2.1";
+  version = "3.1";
 
   src = fetchFromGitHub {
     owner  = "gkarsay";
     repo   = pname;
     rev    = "v${version}";
-    sha256 = "1k53q0kbwpnbgyr0lmfzf5sm4f93d8nbjrzdz9pdhzpxgihndg25";
+    sha256 = "1a4xlsbszb50vnz1g7kf7hl7aywp7s7xaravkcx13csn0a7l3x45";
   };
 
   nativeBuildInputs = [
@@ -18,14 +18,15 @@ stdenv.mkDerivation rec {
     gettext
     appstream-glib
     python3
+    desktop-file-utils
     gobject-introspection
     itstool
+    libxml2
     wrapGAppsHook
   ];
 
   buildInputs = [
     gtk3
-    at-spi2-core
     dbus
     gst_all_1.gstreamer
     gst_all_1.gst-plugins-base
@@ -36,20 +37,15 @@ stdenv.mkDerivation rec {
     glib
     gsettings-desktop-schemas
     hicolor-icon-theme
+    isocodes
   ];
 
   postPatch = ''
-    chmod +x data/meson_post_install.py
     patchShebangs data/meson_post_install.py
+    patchShebangs libparlatype/tests/data/generate_config_data
   '';
 
   doCheck = false;
-
-  mesonFlags = [ "-Dasr=false" ];
-
-  buildPhase = ''
-    export GST_PLUGIN_SYSTEM_PATH_1_0="$out/lib/gstreamer-1.0/:$GST_PLUGIN_SYSTEM_PATH_1_0"
-  '';
 
   meta = with lib; {
     description = "GNOME audio player for transcription";
@@ -58,7 +54,7 @@ stdenv.mkDerivation rec {
       It plays audio sources to transcribe them in your favourite text application.
       It’s intended to be useful for journalists, students, scientists and whoever needs to transcribe audio files.
     '';
-    homepage = "https://gkarsay.github.io/parlatype/";
+    homepage = "https://www.parlatype.org/";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ alexshpilkin melchips ];
     platforms = platforms.linux;
