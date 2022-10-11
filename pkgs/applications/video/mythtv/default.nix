0df309aa832b140eac20fc82d110d941c734dea3
zero_one <zero_one_01@outlook.de>
2022-10-11 17:17:56 +0200

    bumped mythtv version to 32.0

--- pkgs/applications/video/mythtv/default.nix
+++ pkgs/applications/video/mythtv/default.nix
@@ -1,18 +1,18 @@
 { lib, mkDerivation, fetchFromGitHub, which, qtbase, qtwebkit, qtscript, xlibsWrapper
 , libpulseaudio, fftwSinglePrec , lame, zlib, libGLU, libGL, alsa-lib, freetype
 , perl, pkg-config , libsamplerate, libbluray, lzo, libX11, libXv, libXrandr, libXvMC, libXinerama, libXxf86vm
-, libXmu , yasm, libuuid, taglib, libtool, autoconf, automake, file, exiv2, linuxHeaders
+, libXmu , yasm, libuuid, taglib, libtool, autoconf, automake, file, exiv2, linuxHeaders, soundtouch, libzip
 }:
 
 mkDerivation rec {
   pname = "mythtv";
-  version = "31.0";
+  version = "32.0";
 
   src = fetchFromGitHub {
     owner = "MythTV";
     repo = "mythtv";
     rev = "v${version}";
-    sha256 = "092w5kvc1gjz6jd2lk2jhcazasz2h3xh0i5iq80k8x3znyp4i6v5";
+    sha256 = "0i4fs3rbk1jggh62wflpa2l03na9i1ihpz2vsdic9vfahqqjxff1";
   };
 
   patches = [
@@ -25,7 +25,7 @@ mkDerivation rec {
   buildInputs = [
     freetype qtbase qtwebkit qtscript lame zlib xlibsWrapper libGLU libGL
     perl libsamplerate libbluray lzo alsa-lib libpulseaudio fftwSinglePrec libX11 libXv libXrandr libXvMC
-    libXmu libXinerama libXxf86vm libXmu libuuid taglib exiv2
+    libXmu libXinerama libXxf86vm libXmu libuuid taglib exiv2 soundtouch libzip
   ];
   nativeBuildInputs = [ pkg-config which yasm libtool autoconf automake file ];
 
