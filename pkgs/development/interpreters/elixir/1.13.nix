1630a875e7137798000f7ebd4d5ea740cc12b126
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-11 19:08:57 +0000

    elixir: 1.13.4 -> 1.14.1

--- pkgs/development/interpreters/elixir/1.13.nix
+++ pkgs/development/interpreters/elixir/1.13.nix
@@ -3,7 +3,7 @@
 # How to obtain `sha256`:
 # nix-prefetch-url --unpack https://github.com/elixir-lang/elixir/archive/v${version}.tar.gz
 mkDerivation {
-  version = "1.13.4";
-  sha256 = "sha256-xGKq62wzaIfgZN2j808fL3b8ykizQVPuePWzsy2HKfw=";
+  version = "1.14.1";
+  sha256 = "sha256-/QQckiRvwmD3gdIo19TXM0bIgdxNx8eQwpd1RnEo35A=";
   minimumOTPVersion = "22";
 }
