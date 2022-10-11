47326b2a2369f3c7c9f50974809f45026376260e
Vincent Laporte <Vincent.Laporte@gmail.com>
2022-10-11 08:51:38 +0200

    coq_8_16: use OCaml 4.14

--- pkgs/top-level/coq-packages.nix
+++ pkgs/top-level/coq-packages.nix
@@ -1,6 +1,7 @@
 { lib, stdenv, fetchzip
 , callPackage, newScope, recurseIntoAttrs, ocamlPackages_4_05, ocamlPackages_4_09
-, ocamlPackages_4_10, ocamlPackages_4_12, fetchpatch, makeWrapper, coq2html
+, ocamlPackages_4_10, ocamlPackages_4_12, ocamlPackages_4_14
+, fetchpatch, makeWrapper, coq2html
 }@args:
 let lib = import ../build-support/coq/extra-lib.nix {inherit (args) lib;}; in
 let
@@ -130,6 +131,7 @@ let
       ocamlPackages_4_09
       ocamlPackages_4_10
       ocamlPackages_4_12
+      ocamlPackages_4_14
     ;
   };
 in rec {
