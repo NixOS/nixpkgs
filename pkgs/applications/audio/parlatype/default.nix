0ec9204ab69bae22c8f4833440ead7c3b56f8dd2
Alexander Shpilkin <ashpilkin@gmail.com>
2022-10-12 14:41:20 +0300

    parlatype: sort dependencies and reformat
    (cherry picked from commit 16ead09b5e3ab9b1c7a5dc43bb6ad497ccba8f4d)

--- pkgs/applications/audio/parlatype/default.nix
+++ pkgs/applications/audio/parlatype/default.nix
@@ -1,41 +1,62 @@
-{ lib, stdenv, fetchFromGitHub, pkg-config, meson, gtk3, dbus, gst_all_1, ninja, gettext, appstream-glib, python3, desktop-file-utils, glib, gobject-introspection, gsettings-desktop-schemas, isocodes, itstool, libxml2, wrapGAppsHook, hicolor-icon-theme }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, appstream-glib
+, dbus
+, desktop-file-utils
+, gettext
+, glib
+, gobject-introspection
+, gsettings-desktop-schemas
+, gst_all_1
+, gtk3
+, hicolor-icon-theme
+, isocodes
+, itstool
+, libxml2
+, meson
+, ninja
+, pkg-config
+, python3
+, wrapGAppsHook
+}:
 
 stdenv.mkDerivation rec {
   pname = "parlatype";
   version = "3.1";
 
   src = fetchFromGitHub {
-    owner  = "gkarsay";
-    repo   = pname;
-    rev    = "v${version}";
+    owner = "gkarsay";
+    repo = pname;
+    rev = "v${version}";
     sha256 = "1a4xlsbszb50vnz1g7kf7hl7aywp7s7xaravkcx13csn0a7l3x45";
   };
 
   nativeBuildInputs = [
-    pkg-config
-    meson
-    ninja
-    gettext
     appstream-glib
-    python3
     desktop-file-utils
+    gettext
     gobject-introspection
     itstool
     libxml2
+    meson
+    ninja
+    pkg-config
+    python3
     wrapGAppsHook
   ];
 
   buildInputs = [
-    gtk3
     dbus
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
-    gst_all_1.gst-plugins-ugly
-    gst_all_1.gst-libav
     glib
     gsettings-desktop-schemas
+    gst_all_1.gst-libav
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gstreamer
+    gtk3
     hicolor-icon-theme
     isocodes
   ];
@@ -50,9 +71,11 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "GNOME audio player for transcription";
     longDescription = ''
-      Parlatype is a minimal audio player for manual speech transcription, written for the GNOME desktop environment.
-      It plays audio sources to transcribe them in your favourite text application.
-      It’s intended to be useful for journalists, students, scientists and whoever needs to transcribe audio files.
+      Parlatype is a minimal audio player for manual speech transcription,
+      written for the GNOME desktop environment. It plays audio sources to
+      transcribe them in your favourite text application. It’s intended to be
+      useful for journalists, students, scientists and whoever needs to
+      transcribe audio files.
     '';
     homepage = "https://www.parlatype.org/";
     license = licenses.gpl3Plus;
