81ff0b89188f9988c7a20f911c1bbca56acdea58
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-13 12:30:07 +0000

    httm: 0.14.10 -> 0.15.1

--- pkgs/tools/filesystems/httm/default.nix
+++ pkgs/tools/filesystems/httm/default.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "httm";
-  version = "0.14.10";
+  version = "0.15.1";
 
   src = fetchFromGitHub {
     owner = "kimono-koans";
     repo = pname;
     rev = version;
-    sha256 = "sha256-izJMypTB7JCvSdTbsS85ez9HL7hM8DtPvnPXA0MvQC8=";
+    sha256 = "sha256-WaDODucXP/U0d6RRWPt9XWV3JtDxEjl39ZMMhdtN+wQ=";
   };
 
-  cargoSha256 = "sha256-3B1+pV7FyOD/e9fIKiAheIbb1vSFooc2qdnbL7LmzdQ=";
+  cargoSha256 = "sha256-mYhBq7PDG1xoMkN3OQ/hPoQ4x8+B/rlnUAX4O7B3GJY=";
 
   nativeBuildInputs = [ installShellFiles ];
 
