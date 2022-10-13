1ee0e58e2285085014e90691d8a3dbfc72066c13
misuzu <bakalolka@gmail.com>
2022-10-13 15:39:37 +0300

    xterm: 373 -> 374

--- pkgs/applications/terminal-emulators/xterm/default.nix
+++ pkgs/applications/terminal-emulators/xterm/default.nix
@@ -4,14 +4,14 @@
 
 stdenv.mkDerivation rec {
   pname = "xterm";
-  version = "373";
+  version = "374";
 
   src = fetchurl {
     urls = [
       "ftp://ftp.invisible-island.net/xterm/${pname}-${version}.tgz"
       "https://invisible-mirror.net/archives/xterm/${pname}-${version}.tgz"
     ];
-    sha256 = "sha256-3rCYlHOmOQi1qNRN/uqDAchxD2zgH7V86MMAAjdXRrY=";
+    sha256 = "sha256-EdTWJmcNTW17aft0Z+nsIxgX5a0iUC+RZ3aP2IrBvfU=";
   };
 
   strictDeps = true;
