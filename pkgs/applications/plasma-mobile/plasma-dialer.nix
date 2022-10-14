139173a984ad51c39fc3c2ee07329a95960714e7
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-10-13 23:57:50 -0400

    plasma-dialer: Disable overlay support
    See:
    
     - https://invent.kde.org/plasma-mobile/plasma-dialer/-/issues/66
     - https://invent.kde.org/plasma-mobile/plasma-dialer/-/commit/3a91964828fb2b9f54d9f9eb5f81865af5676a42
    
    When Plasma Gear 22.09 was released, KWin 5.26 wasn't released yet.
    
    The linked commit was not part of Gear 22.09.

--- pkgs/applications/plasma-mobile/plasma-dialer.nix
+++ pkgs/applications/plasma-mobile/plasma-dialer.nix
@@ -68,6 +68,13 @@ mkDerivation rec {
       --replace "/usr/bin/" "$out/bin/"
   '';
 
+  # Plasma gear 22.09 shipped before KWin 5.26 was made available.
+  # This feature requires 5.26. Otherwise plasma-dialer segfaults.
+  # Note that we may need to keep it disabled until it stops segfaulting outside of KWin.
+  cmakeFlags = [
+    "-DDIALER_BUILD_SHELL_OVERLAY=OFF"
+  ];
+
   meta = with lib; {
     description = "Dialer for Plasma Mobile";
     homepage = "https://invent.kde.org/plasma-mobile/plasma-dialer";
