997b80d632ad6d4c27e4093fac59e6cd3b7fd3c8
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-10-14 19:00:34 +0200

    python39: 3.9.14 -> 3.9.15
    http://docs.python.org/release/3.9.15/whatsnew/changelog.html

--- pkgs/development/interpreters/python/default.nix
+++ pkgs/development/interpreters/python/default.nix
@@ -131,10 +131,10 @@ with pkgs;
       sourceVersion = {
         major = "3";
         minor = "9";
-        patch = "14";
+        patch = "15";
         suffix = "";
       };
-      sha256 = "sha256-ZRME0hbIID/grfGoCvRy2OksOw4KeJIiKuTZ865N688=";
+      sha256 = "sha256-Etr/aAlSjZ9hVCFpUEI8njDw5HM2y1fGqgtDh91etLI=";
     };
     python310 = {
       sourceVersion = {
