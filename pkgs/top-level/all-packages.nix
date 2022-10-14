1a07eaa6dd7116dd9043f2e40317cb418fc7f3ac
Mario Rodas <marsam@users.noreply.github.com>
2022-10-14 04:20:00 +0000

    llhttp: init at 8.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22335,6 +22335,8 @@ with pkgs;
 
   live555 = callPackage ../development/libraries/live555 { };
 
+  llhttp = callPackage ../development/libraries/llhttp { };
+
   log4cpp = callPackage ../development/libraries/log4cpp { };
 
   log4cxx = callPackage ../development/libraries/log4cxx { };
