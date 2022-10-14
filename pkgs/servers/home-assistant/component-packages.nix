f2cc5866e8566db380d34af8a7bcdd394fa03210
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-10-15 01:24:46 +0200

    home-assistant: 2022.10.3 -> 2022.10.4
    https://github.com/home-assistant/core/releases/tag/2022.10.4

--- pkgs/servers/home-assistant/component-packages.nix
+++ pkgs/servers/home-assistant/component-packages.nix
@@ -2,7 +2,7 @@
 # Do not edit!
 
 {
-  version = "2022.10.3";
+  version = "2022.10.4";
   components = {
     "abode" = ps: with ps; [
       abodepy
@@ -635,10 +635,10 @@
       zeroconf
     ];
     "dlib_face_detect" = ps: with ps; [
-      face_recognition
+      face-recognition
     ];
     "dlib_face_identify" = ps: with ps; [
-      face_recognition
+      face-recognition
     ];
     "dlink" = ps: with ps; [
     ]; # missing inputs: pyW215
@@ -1276,12 +1276,12 @@
       sqlalchemy
     ];
     "homekit" = ps: with ps; [
-      hap-python
       pyqrcode
       pyturbojpeg
       aiohttp-cors
       base36
       fnvhash
+      hap-python
       ha-ffmpeg
       ifaddr
       zeroconf
@@ -2369,7 +2369,7 @@
       aiohttp-cors
     ];
     "pushbullet" = ps: with ps; [
-      pushbullet
+      pushbullet-py
     ];
     "pushover" = ps: with ps; [
       pushover-complete
