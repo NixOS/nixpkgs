d30723ee4ad9002523448982948972e28af7c290
Danielle Lancashire <dani@builds.terrible.systems>
2022-10-15 10:12:34 +0000

    bindle: init at v0.8.1

---
+++ pkgs/servers/bindle/default.nix
@@ -0,0 +1,34 @@
+{ lib, rustPlatform, fetchFromGitHub, pkgs, stdenv, Security }:
+
+rustPlatform.buildRustPackage rec {
+  pname = "bindle";
+  version = "0.8.1";
+
+  src = fetchFromGitHub {
+    owner = "deislabs";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-Mc3LaEOWx8cN7g0r8CtWkGZ746gAXTaFmAZhEIkbWgM=";
+  };
+
+  doCheck = false; # Tests require a network
+
+  buildInputs = with pkgs; [ pkg-config openssl_3 ] ++ lib.optional stdenv.isDarwin Security;
+
+  cargoSha256 = "sha256-brsemnw/9YEsA2FEIdYGmQMdlIoT1ZEMjvOpF44gcRE=";
+
+  cargoBuildFlags = [
+    "--bin" "bindle"
+    "--bin" "bindle-server"
+    "--all-features"
+  ];
+
+  meta = with lib; {
+    description = "Bindle: Aggregate Object Storage";
+    homepage = "https://github.com/deislabs/bindle";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ endocrimes ];
+    platforms = platforms.unix;
+  };
+}
+
