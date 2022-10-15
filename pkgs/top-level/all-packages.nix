8fe2576f464e2917e534f17f69d0808ebe6aeaa8
Azat Bahawi <azat@bahawi.net>
2022-10-15 13:21:24 +0300

    sigslot: init at 1.2.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4756,6 +4756,8 @@ with pkgs;
 
   shisho = callPackage ../tools/security/shisho { };
 
+  sigslot = callPackage ../development/libraries/sigslot { };
+
   siglo = callPackage ../applications/misc/siglo { };
 
   simg2img = callPackage ../tools/filesystems/simg2img { };
