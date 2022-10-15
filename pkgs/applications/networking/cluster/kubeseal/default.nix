1b59272940f5f5fec7773b67c9070ffbd9d62cc3
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-15 08:33:58 +0000

    kubeseal: 0.18.5 -> 0.19.0

--- pkgs/applications/networking/cluster/kubeseal/default.nix
+++ pkgs/applications/networking/cluster/kubeseal/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "kubeseal";
-  version = "0.18.5";
+  version = "0.19.0";
 
   src = fetchFromGitHub {
     owner = "bitnami-labs";
     repo = "sealed-secrets";
     rev = "v${version}";
-    sha256 = "sha256-Ij+NNaAq3woHze7o14WT3cqKYLD99dU8C6TUsdG2U54=";
+    sha256 = "sha256-CQlyAgnEWeAfOn6xXeDFEGuSnaGZpGewg1tYYDCw8qE=";
   };
 
-  vendorSha256 = "sha256-Iry8ZE/HwZEnro7p36KTdy3phydA+fjM4EFg8DneSuA=";
+  vendorSha256 = "sha256-505nUMuFteOfIurGYRGHqo9diTSEa56tmQZ3jIGtULQ=";
 
   subPackages = [ "cmd/kubeseal" ];
 
