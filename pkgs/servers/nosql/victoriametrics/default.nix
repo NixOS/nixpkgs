81938058dcf88241a75b9c4634d384c0136d855e
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-15 23:22:03 +0000

    victoriametrics: 1.82.0 -> 1.82.1

--- pkgs/servers/nosql/victoriametrics/default.nix
+++ pkgs/servers/nosql/victoriametrics/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "VictoriaMetrics";
-  version = "1.82.0";
+  version = "1.82.1";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-JIl2WeveDoAHzqJ2cqMxpWeNf4yQC9fIdfECOJywJ2A=";
+    sha256 = "sha256-Lor4rpL19xc/eA2quJ1D0WrJiviEcKnArBKUQhb9Z0k=";
   };
 
   vendorSha256 = null;
