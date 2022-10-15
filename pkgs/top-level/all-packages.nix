ec50286d53e92229a987733f71ea91b7ee106e51
Azat Bahawi <azat@bahawi.net>
2022-10-15 13:16:29 +0300

    ctre: init at 3.7.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2458,6 +2458,8 @@ with pkgs;
 
   msr = callPackage ../os-specific/linux/msr { };
 
+  ctre = callPackage ../development/libraries/ctre { };
+
   ctrtool = callPackage ../tools/archivers/ctrtool { };
 
   crowbar = callPackage ../tools/security/crowbar { };
