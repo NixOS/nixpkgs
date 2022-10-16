f59e4d4f3637e071e00b9e041d55cbedf97656ad
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-16 23:55:48 +0200

    python310Packages.aiohomekit: 2.0.2 -> 2.1.0

--- pkgs/development/python-modules/aiohomekit/default.nix
+++ pkgs/development/python-modules/aiohomekit/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "aiohomekit";
-  version = "2.0.2";
+  version = "2.1.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "Jc2k";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-pZEZHhsU/1tEX1VOFQ8b+ERJ8tU1pzRJMRYD28nfTb0=";
+    hash = "sha256-QRJqtqe6x2kN5hQ6q35k2qFKjWS3fk6JN+AABAFvVts=";
   };
 
   nativeBuildInputs = [
