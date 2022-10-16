c2687f97958d07e9dfb655b33d180e74c5bc34f3
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-16 09:06:32 +0200

    python310Packages.led-ble: 0.10.1 -> 1.0.0

--- pkgs/development/python-modules/led-ble/default.nix
+++ pkgs/development/python-modules/led-ble/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "led-ble";
-  version = "0.10.1";
+  version = "1.0.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-GyVj9g4tqPaR5Gd8N76TtkldaAATnEBsSs/F+2iQqGM=";
+    hash = "sha256-XAb/tJPUe/sNvcU7t63inMPBIz+AREioWXBuQa/c9T0=";
   };
 
   postPatch = ''
