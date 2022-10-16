9192937092150208dfd4171e157bb318bf1a91c3
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-16 16:38:59 +0000

    kubedb-cli: 0.28.0 -> 0.29.0

--- pkgs/applications/networking/cluster/kubedb-cli/default.nix
+++ pkgs/applications/networking/cluster/kubedb-cli/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "kubedb-cli";
-  version = "0.28.0";
+  version = "0.29.0";
 
   src = fetchFromGitHub {
     owner = "kubedb";
     repo = "cli";
     rev = "v${version}";
-    sha256 = "sha256-fSJ9IIuyOuKmpklw7uh1NKM3Pr5epJefYBJ3udeC5sE=";
+    sha256 = "sha256-MVvhXvf/R7Cku8S4zbs1MlH4NuEwkxCsbbc2ASCF67c=";
   };
 
   vendorSha256 = null;
